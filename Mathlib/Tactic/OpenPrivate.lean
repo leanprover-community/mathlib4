00a0e78d57a1cb65422d8c97f2f7973e43af6054
Mario Carneiro <di.gama@gmail.com>
2021-05-10 13:57:13 -0400

    feat(Tactic/OpenPrivate): make export private also open

--- Mathlib/Tactic/OpenPrivate.lean
+++ Mathlib/Tactic/OpenPrivate.lean
@@ -1,8 +1,10 @@
-import Mathlib.Tactic.Split
+/-
+Copyright (c) 2021 Microsoft Corporation. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Author: Mario Carneiro
+-/
 import Lean.Elab.Command
-import Lean.Elab.Match
 import Lean.Util.FoldConsts
-import Lean.Data.OpenDecl
 
 open Lean Parser.Tactic Elab Command
 
@@ -11,19 +13,26 @@ namespace Lean
 def Meta.collectPrivateIn [Monad m] [MonadEnv m] [MonadError m]
   (n : Name) (set := NameSet.empty) : m NameSet := do
   let c ← getConstInfo n
-  let tgt := `elabMatchAux
   pure $ Expr.foldConsts c.value! set fun c a =>
-    if isPrivateName c && tgt.isSuffixOf c then a.insert c else a
+    if isPrivateName c then a.insert c else a
 
 namespace Elab.Command
 
 def elabOpenPrivateLike (ids tgts : Array Syntax)
-  (a : α) (f : (priv full user : Name) → α → CommandElabM α) : CommandElabM α := do
+  (f : (priv full user : Name) → CommandElabM Name) : CommandElabM Unit := do
   let mut names := NameSet.empty
   for tgt in tgts do
     let n ← resolveGlobalConstNoOverload tgt.getId
     names ← Meta.collectPrivateIn n names
-  let mut a := a
+  let appendNames (msg : String) : String := do
+    let mut msg := msg
+    for c in names do
+      if let some name := privateToUserName? c then
+        msg := msg ++ s!"{name}\n"
+    msg
+  if ids.isEmpty && !names.isEmpty then
+    logInfo (appendNames "found private declarations:\n")
+  let mut decls := #[]
   for n in ids do
     let n := n.getId
     let mut found := []
@@ -31,18 +40,18 @@ def elabOpenPrivateLike (ids tgts : Array Syntax)
       if n.isSuffixOf c then
         found := c::found
     match found with
-    | [] =>
-      let mut msg := s!"'{n}' not found in the provided declarations:\n"
-      for c in names do
-        if let some name := privateToUserName? c then
-          msg := msg ++ s!"{name}\n"
-      throwError msg
+    | [] => throwError (appendNames s!"'{n}' not found in the provided declarations:\n")
     | [c] =>
       if let some name := privateToUserName? c then
-         a ← f c name n a
+        let new ← f c name n
+        decls := decls.push (OpenDecl.explicit n new)
       else unreachable!
     | _ => throwError s!"provided name is ambiguous: found {found.map privateToUserName?}"
-  a
+  modifyScope fun scope => do
+    let mut openDecls := scope.openDecls
+    for decl in decls do
+      openDecls := decl::openDecls
+    { scope with openDecls := openDecls }
 
 syntax (name := openPrivate) "open private " ident* " in " ident* : command
 
@@ -55,14 +64,8 @@ instead of the (unnameable) internal name for the private declaration, something
 name component.
 -/
 @[commandElab openPrivate] def elabOpenPrivate : CommandElab
-| `(open private $ids* in $tgts*) => do
-  let decls ← elabOpenPrivateLike ids tgts #[] fun c _ n decls =>
-    decls.push (OpenDecl.explicit n c)
-  modifyScope fun scope => do
-    let mut openDecls := scope.openDecls
-    for decl in decls do
-      openDecls := decl::openDecls
-    { scope with openDecls := openDecls }
+| `(open private $ids* in $tgts*) =>
+  elabOpenPrivateLike ids tgts fun c _ _ => c
 | _ => throwUnsupportedSyntax
 
 syntax (name := exportPrivate) "export private " ident* " in " ident* : command
@@ -72,10 +75,13 @@ The command `export private a b c in foo bar` is similar to `open private`, but 
 them in the current scope it will create public aliases to the private definition. The definition
 will exist at exactly the original location and name, as if the `private` keyword was not used
 originally.
+
+It will also open the newly created alias definition under the provided short name, like
+`open private`.
 -/
 @[commandElab exportPrivate] def elabExportPrivate : CommandElab
 | `(export private $ids* in $tgts*) =>
-  elabOpenPrivateLike ids tgts () fun c name _ _ => do
+  elabOpenPrivateLike ids tgts fun c name _ => do
     let cinfo ← getConstInfo c
     if (← getEnv).contains name then
       throwError s!"'{name}' has already been declared"
@@ -89,6 +95,7 @@ originally.
     }
     addDecl decl
     compileDecl decl
+    name
 | _ => throwUnsupportedSyntax
 
 end Elab.Command
