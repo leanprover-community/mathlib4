21ff8c55f76592b22b79d312c59756d88e53b8cd
Mario Carneiro <di.gama@gmail.com>
2021-05-11 23:07:07 -0400

    feat(Data/List): versions of list functions with lean 3 style defeq

--- Mathlib/Data/List/Basic.lean
+++ Mathlib/Data/List/Basic.lean
@@ -1,20 +1,44 @@
 import Mathlib.Logic.Basic
+import Mathlib.Data.Nat.Basic
 
 namespace List
 
-theorem concat_eq_append : ∀ (l : List α) a, concat l a = l ++ [a]
+/-- The same as append, but with simpler defeq. (The one in the standard library is more efficient,
+because it is implemented in a tail recursive way.) -/
+@[simp] def append' : List α → List α → List α
+| [], r => r
+| a::l, r => a :: append' l r
+
+theorem append'_eq_append : (l r : List α) → append' l r = l ++ r
+| [], r => rfl
+| a::l, r => by simp only [append', cons_append, append'_eq_append]; rfl
+
+/-- The same as length, but with simpler defeq. (The one in the standard library is more efficient,
+because it is implemented in a tail recursive way.) -/
+@[simp] def length' : List α → ℕ
+| [] => 0
+| a::l => l.length'.succ
+
+theorem length'_eq_length : (l : List α) → length' l = l.length
+| [] => rfl
+| a::l => by simp only [length', length_cons, length'_eq_length]; rfl
+
+theorem concat_eq_append' : ∀ (l : List α) a, concat l a = l.append' [a]
 | [], a => (append_nil _).symm
-| x::xs, a => by simp [concat, concat_eq_append xs]
+| x::xs, a => by simp only [concat, append', concat_eq_append' xs]; rfl
+
+theorem concat_eq_append (l : List α) (a) : concat l a = l ++ [a] :=
+(concat_eq_append' _ _).trans (append'_eq_append _ _)
 
 theorem get_cons_drop : ∀ (l : List α) i h,
   List.get l i h :: List.drop (i + 1) l = List.drop i l
 | _::_, 0, h => rfl
 | _::_, i+1, h => get_cons_drop _ i _
 
-theorem drop_eq_nil_of_le : ∀ {l : List α} {k : Nat} (h : l.length ≤ k), l.drop k = []
+theorem drop_eq_nil_of_le : ∀ {l : List α} {k : Nat} (h : l.length' ≤ k), l.drop k = []
 | [], k, _ => by cases k <;> rfl
-| a::l, 0, h => by simp at h; exact nomatch h
-| a::l, k+1, h => by simp at h; exact drop_eq_nil_of_le (l := l) h
+| a::l, 0, h => by cases h
+| a::l, k+1, h => drop_eq_nil_of_le (l := l) h
 
 /-- List membership. -/
 def mem (a : α) : List α → Prop
