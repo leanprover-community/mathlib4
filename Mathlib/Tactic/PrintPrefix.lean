0b5516247cd74b6409a32195e08cd3bd2be4525b
Mario Carneiro <di.gama@gmail.com>
2021-05-12 12:29:56 -0400

    fix(Tactic/PrintPrefix): fix attribution

--- Mathlib/Tactic/PrintPrefix.lean
+++ Mathlib/Tactic/PrintPrefix.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2021 Kevin Buzzard. All rights reserved.
+Copyright (c) 2021 Shing Tak Lam. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Kevin Buzzard, Mario Carneiro
+Authors: Shing Tak Lam, Daniel Selsam, Mario Carneiro
 -/
 import Lean.Elab.Command
 
