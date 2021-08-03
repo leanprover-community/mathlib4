7822926636f86c197bf3b9d14b4ad3b4a34701a1
Aurélien Saue <47693595+AurelienSaue@users.noreply.github.com>
2021-08-03 14:34:21 +0200

    Few tactic implementations (#27)
    * aligned tactic names to mathport
    
    * added byContra tactic
    
    * added guardExprEq guardTarget guardHyp
    
    * added introv
    
    * Update Mathlib/Tactic/Basic.lean
    
    Co-authored-by: Gabriel Ebner <gebner@gebner.org>
    
    * Update Mathlib/Tactic/Basic.lean
    
    Co-authored-by: Gabriel Ebner <gebner@gebner.org>
    
    * added exacts
    
    * use of elab and examples
    
    * matchTarget
    
    * byContra all cases
    
    * sorry & iterate
    
    * fix : get to build
    
    * fix : get to build
    
    * fix : get to build 3
    
    Co-authored-by: Aurélien SAUE <aurelien.saue@polytechnique.edu>
    Co-authored-by: Gabriel Ebner <gebner@gebner.org>

--- Mathlib/Tactic/Basic.lean
+++ Mathlib/Tactic/Basic.lean
@@ -8,7 +8,7 @@ import Mathlib.Tactic.NoMatch
 import Mathlib.Tactic.Block
 import Lean.Elab.Command
 
-open Lean Parser.Tactic Elab Command
+open Lean Parser.Tactic Elab Command Elab.Tactic Meta
 
 syntax (name := «variables») "variables" (bracketedBinder)* : command
 
@@ -54,3 +54,125 @@ set_option hygiene false in
 macro_rules
   | `(tactic| transitivity) => `(tactic| apply Nat.lt_trans)
   | `(tactic| transitivity $e) => `(tactic| apply Nat.lt_trans (m := $e))
+
+syntax (name := introv) "introv " (colGt ident)* : tactic
+@[tactic introv] partial def evalIntrov : Tactic := fun stx => do
+  match stx with
+  | `(tactic| introv)                     => introsDep
+  | `(tactic| introv $h:ident $hs:ident*) => evalTactic (← `(tactic| introv; intro $h:ident; introv $hs:ident*))
+  | _ => throwUnsupportedSyntax
+where
+  introsDep : TacticM Unit := do
+    let t ← getMainTarget
+    match t with
+    | Expr.forallE _ _ e _ =>
+      if e.hasLooseBVars then
+        intro1PStep
+        introsDep
+    | _ => ()
+  intro1PStep : TacticM Unit :=
+    liftMetaTactic fun mvarId => do
+      let (_, mvarId) ← Meta.intro1P mvarId
+      pure [mvarId]
+
+example : ∀ a b : Nat, a = b → b = a := by
+  introv h
+  exact h.symm
+
+macro "assumption'" : tactic => `(allGoals assumption)
+
+elab "exacts" "[" hs:term,* "]" : tactic => do
+  for stx in hs.getElems do
+    evalTactic (← `(tactic| exact $stx))
+  evalTactic (← `(tactic| done))
+
+example (n : Nat) : n = n := by
+  induction n
+  exacts [rfl, rfl]
+  exacts []
+
+--TODO : which expr equality to use?
+elab "guardExprEq " r:term " := " p:term : tactic => do
+  let r ← elabTerm r none
+  let p ← elabTerm p none
+  if not (r == p) then throwError "failed: {r} != {p}"
+
+elab "guardTarget" r:term : tactic => do
+  let r ← elabTerm r none
+  let t ← getMainTarget
+  if not (← isDefEq r t) then throwError m!"target of main goal is {t}"
+
+syntax (name := guardHyp) "guardHyp " ident (" : " term)? (" := " term)? : tactic
+@[tactic guardHyp] def evalGuardHyp : Lean.Elab.Tactic.Tactic := fun stx =>
+  match stx with
+  | `(tactic| guardHyp $h $[: $ty]? $[:= $val]?) => do
+    withMainContext do
+      let fvarid ← getFVarId h
+      let lDecl ←
+        match (← getLCtx).find? fvarid with
+        | none => throwError m!"hypothesis {h} not found"
+        | some lDecl => lDecl
+      if let some p ← ty then
+        let e ← elabTerm p none
+        let hty ← instantiateMVars lDecl.type
+        if not (← isDefEq e hty) then throwError m!"hypothesis {h} has type {lDecl.type}"
+      match lDecl.value?, val with
+      | none, some _        => throwError m!"{h} is not a let binding"
+      | some _, none        => throwError m!"{h} is a let binding"
+      | some hval, some val =>
+          let e ← elabTerm val none
+          let hval ← instantiateMVars hval
+          if not (← isDefEq e hval) then throwError m!"hypothesis {h} has value {hval}"
+      | none, none          => ()
+  | _ => throwUnsupportedSyntax
+
+example (n : Nat) : Nat := by
+  guardHyp n : Nat
+  let m : Nat := 1
+  guardHyp m := 1
+  guardHyp m : Nat := 1
+  guardTarget Nat
+  exact 0
+
+elab "matchTarget" t:term : tactic  => do
+  withMainContext do
+    let (val) ← elabTerm t (← inferType (← getMainTarget))
+    if not (← isDefEq val (← getMainTarget)) then
+      throwError "failed"
+
+syntax (name := byContra) "byContra " (colGt ident)? : tactic
+macro_rules
+  | `(tactic| byContra) => `(tactic| (matchTarget Not _; intro))
+  | `(tactic| byContra $e) => `(tactic| (matchTarget Not _; intro $e))
+macro_rules
+  | `(tactic| byContra) => `(tactic| (apply Decidable.byContradiction; intro))
+  | `(tactic| byContra $e) => `(tactic| (apply Decidable.byContradiction; intro $e))
+macro_rules
+  | `(tactic| byContra) => `(tactic| (apply Classical.byContradiction; intro))
+  | `(tactic| byContra $e) => `(tactic| (apply Classical.byContradiction; intro $e))
+
+example (a b : Nat) : a ≠ b → ¬ a = b := by
+  intros
+  byContra H
+  contradiction
+
+example (a b : Nat) : ¬¬ a = b → a = b := by
+  intros
+  byContra H
+  contradiction
+
+example (p q : Prop) : ¬¬ p → p := by
+  intros
+  byContra H
+  contradiction
+
+macro "sorry" : tactic => `(exact sorry)
+
+elab "iterate " n:num seq:tacticSeq : tactic => do
+  for i in [:n.toNat] do
+    evalTactic seq
+
+example (n m : Nat) : Unit := by
+  cases n
+  cases m
+  iterate 3 exact ()
