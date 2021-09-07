94dfcfa94c6f89d86ea3546c64058a5e9b61843b
Scott Morrison <scott@tqft.net>
2021-09-07 15:45:56 +1000

    clean up

--- Mathlib/Tactic/SplitIfs.lean
+++
@@ -1,9 +0,0 @@
-import Mathlib.Tactic.Basic
-import Mathlib.Tactic.Block
-
-example (P : Prop) [Decidable P] (w : if P then False else False) : False := by
-  (by_cases P)
-  - rw [if_pos h] at w
-    assumption
-  - rw [if_neg h] at w
-    assumption
