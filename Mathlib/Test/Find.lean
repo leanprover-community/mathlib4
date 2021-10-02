0caa2ddcd86df80db03a12a530ac14b9cb46816a
Scott Morrison <scott@tqft.net>
2021-10-02 11:42:02 +1000

    chore(Test/Find): silence tests

--- Mathlib/Test/Find.lean
+++ Mathlib/Test/Find.lean
@@ -1,7 +1,10 @@
 import Mathlib.Tactic.Find
 
-#find _ + _ = _ + _
-#find ?n + _ = _ + ?n
-#find (_ : Nat) + _ = _ + _
-#find Nat → Nat
-#find ?n ≤ ?m → ?n + _ ≤ ?m + _
+-- TODO Write an "internal" version of `find` that returns a list of results,
+-- rather than printing, and use this to set up silent tests.
+
+-- #find _ + _ = _ + _
+-- #find ?n + _ = _ + ?n
+-- #find (_ : Nat) + _ = _ + _
+-- #find Nat → Nat
+-- #find ?n ≤ ?m → ?n + _ ≤ ?m + _
