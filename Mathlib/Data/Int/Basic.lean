leanprover-community/mathlib4

commit b05d1f205209aa08422d0aecc8a8768787f49d91

chore(lean-toolchain): bump to nightly-2021-10-26 (#79)
Co-authored-by: Scott Morrison <scott.morrison@gmail.com>

Mathlib/Data/Int/Basic.lean
@@ -246,8 +246,8 @@ def gcd (m n : ℤ) : ℕ := Nat.gcd (natAbs m) (natAbs n)
 
 protected lemma add_comm : ∀ a b : ℤ, a + b = b + a
 | (n : ℕ), (m : ℕ) => by simp [Nat.add_comm]
-| (_ : ℕ), -[1+ _] => rfl
-| -[1+ _], (_ : ℕ) => rfl
+| (n : ℕ), -[1+ _] => rfl
+| -[1+ _], (m : ℕ) => rfl
 | -[1+ _], -[1+ _] => by simp [Nat.add_comm]
 
 protected lemma add_zero : ∀ a : ℤ, a + 0 = a

lean-toolchain
@@ -1,1 +1,1 @@
-leanprover/lean4:nightly-2021-10-18
+leanprover/lean4:nightly-2021-10-26

commit a8cf6b30a3fee60c52b7ccc5b045ad8f11d1ffb6

chore: bump to nightly-2021-10-28 and revert regression (#81)
This bumps Lean, and reverses the change made to deal with the regression from https://github.com/leanprover-community/mathlib4/pull/79.



Co-authored-by: Scott Morrison <scott.morrison@gmail.com>
Co-authored-by: Gabriel Ebner <gebner@gebner.org>

Mathlib/Data/Int/Basic.lean
@@ -246,8 +246,8 @@ def gcd (m n : ℤ) : ℕ := Nat.gcd (natAbs m) (natAbs n)
 
 protected lemma add_comm : ∀ a b : ℤ, a + b = b + a
 | (n : ℕ), (m : ℕ) => by simp [Nat.add_comm]
-| (n : ℕ), -[1+ _] => rfl
-| -[1+ _], (m : ℕ) => rfl
+| (_ : ℕ), -[1+ _] => rfl
+| -[1+ _], (_ : ℕ) => rfl
 | -[1+ _], -[1+ _] => by simp [Nat.add_comm]
 
 protected lemma add_zero : ∀ a : ℤ, a + 0 = a

Mathlib/Tactic/NoMatch.lean
@@ -43,7 +43,8 @@ elab tk:"fun" "." : term <= expectedType =>
     for _ in args do
       let n ← mkFreshIdent tk
       binders := binders.push n
-      discrs := discrs.push $ Syntax.node ``Lean.Parser.Term.matchDiscr #[mkNullNode, n]
+      discrs := discrs.push $
+        mkNode ``Lean.Parser.Term.matchDiscr #[mkNullNode, n]
     elabTerm (← `(@fun $binders* => match $discrs,* with.)) (some expectedType)
 
 macro tk:"λ" "." : term => `(fun%$tk .)

Mathlib/Tactic/TryThis.lean
@@ -22,8 +22,8 @@ partial def replaceMVarsByUnderscores [Monad m] [MonadQuotation m]
     (s : Syntax) : m Syntax := do
   if s matches `(?$mvar:ident) then
     `(?_)
-  else if let Syntax.node kind args := s then
-    Syntax.node kind (← args.mapM replaceMVarsByUnderscores)
+  else if let Syntax.node _ kind args := s then
+    mkNode kind (← args.mapM replaceMVarsByUnderscores)
   else
     s
 

lean-toolchain
@@ -1,1 +1,1 @@
-leanprover/lean4:nightly-2021-10-26
+leanprover/lean4:nightly-2021-10-28