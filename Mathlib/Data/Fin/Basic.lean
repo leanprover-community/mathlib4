bd1999777f93bfae5b4a55091f906459bd219b49
ammkrn <ammkrn@tuta.io>
2021-11-10 17:24:24 -0600

    feat(Data/UInt, Data/Fin/Basic) declarations
    Add new Fin instances, extend the UInt declarations to other UInt types using a macro. This incorporates @gebner's suggestions to add boolean methods for checking under/overflow, and adding separate instances of `Fin size`. I still can't figure out the elaboration issue mentioned [here on zulip](https://leanprover.zulipchat.com/#narrow/stream/270676-lean4/topic/simp.20type.20annotations/near/260877092), so I can't use the macro on USize yet.

--- Mathlib/Data/Fin/Basic.lean
+++ Mathlib/Data/Fin/Basic.lean
@@ -1,5 +1,6 @@
 import Mathlib.Data.Nat.Basic
 import Mathlib.Algebra.Group.Defs
+import Mathlib.Algebra.GroupWithZero.Defs
 
 section Fin
 
@@ -8,21 +9,23 @@ variable {n : Nat}
 instance : Zero (Fin n.succ) where
   zero := Fin.ofNat 0
 
-@[simp]
-lemma Fin.val_zero : (0 : Fin n.succ).val = (0 : Nat) :=
+@[simp] lemma Fin.val_zero : (0 : Fin n.succ).val = (0 : Nat) :=
   show (Fin.ofNat 0).val = 0 by simp [Fin.ofNat]
 
 instance : One (Fin n.succ) where
   one := ⟨1 % n.succ, Nat.mod_lt 1 (Nat.zero_lt_succ n)⟩
 
-@[simp]
-lemma Fin.of_nat_zero : (Fin.ofNat 0 : Fin n.succ) = (0 : Fin n.succ) := by
+@[simp] lemma Fin.val_one: (1 : Fin n.succ).val = (1 % n.succ : Nat) :=
+  show (Fin.ofNat 1).val = 1 % n.succ by simp [Fin.ofNat]
+
+@[simp] lemma Fin.of_nat_zero : (Fin.ofNat 0 : Fin n.succ) = (0 : Fin n.succ) := by
   apply Fin.eq_of_val_eq; simp only [Fin.ofNat, Nat.zero_mod, Fin.val_zero]
 
-@[simp]
-lemma Fin.val_one : (1 : Fin n.succ).val = (1 % n.succ : Nat) := by
-  have h0 : ∀ x, (OfNat.ofNat x : Fin n.succ) = Fin.ofNat x := by simp [OfNat.ofNat]
-  simp only [h0, Fin.ofNat]
+instance : Neg (Fin n) where
+  neg a := ⟨(n - a) % n, Nat.mod_lt _ (lt_of_le_of_lt (Nat.zero_le _) a.isLt)⟩
+
+lemma Fin.val_eq_of_lt : a < n.succ → (@Fin.ofNat n a).val = a := Nat.mod_eq_of_lt
+
 
 /-- If you actually have an element of `Fin n`, then the `n` is always positive -/
 lemma Fin.positive_size : ∀ (x : Fin n), 0 < n
@@ -33,6 +36,9 @@ lemma Fin.positive_size : ∀ (x : Fin n), 0 < n
 
 lemma Fin.modn_def : ∀ (a : Fin n) (m : Nat), a % m = Fin.mk ((a.val % m) % n) (Nat.mod_lt (a.val % m) (Fin.positive_size a))
 | ⟨a, pa⟩, m => by simp only [HMod.hMod, Fin.modn]
+
+lemma Fin.mod_def : ∀ (a m : Fin n), a % m = Fin.mk ((a.val % m.val) % n) (Nat.mod_lt (a.val % m.val) (Fin.positive_size a))
+| ⟨a, pa⟩, ⟨m, pm⟩ => by simp only [HMod.hMod, Mod.mod, Fin.mod]
 
 lemma Fin.add_def : ∀ (a b : Fin n), a + b = (Fin.mk ((a.val + b.val) % n) (Nat.mod_lt _ (Fin.positive_size a)))
 | ⟨a, pa⟩, ⟨b, pb⟩ => by simp only [HAdd.hAdd, Add.add, Fin.add]
@@ -48,34 +54,74 @@ lemma Fin.sub_def : ∀ (a b : Fin n), a - b = (Fin.mk ((a + (n - b)) % n) (Nat.
   simp only [Fin.modn_def, Nat.mod_mod]
   exact Nat.mod_eq_of_lt a.isLt
 
+@[simp] lemma Fin.mod_eq_val (a : Fin n) : a.val % n = a.val := by
+  simp only [Fin.modn_def, Nat.mod_mod]
+  exact Nat.mod_eq_of_lt a.isLt
+
+theorem Fin.mod_eq_of_lt {a b : Fin n} (h : a < b) : a % b = a := by
+  apply Fin.eq_of_val_eq
+  simp only [Fin.mod_def]
+  rw [Nat.mod_eq_of_lt h, Nat.mod_eq_of_lt a.isLt]
+
+theorem Fin.mod_lt : ∀ (i : Fin n.succ) {m : Fin n.succ}, (0 : Fin n.succ) < m → (i % m) < m
+  | ⟨a, aLt⟩, ⟨m, mLt⟩, hp =>  by
+    have _ : (0 : Nat) < m := Fin.val_zero ▸ hp
+    have _ : a % m < m := Nat.mod_lt _ ‹0 < m›
+    simp only [Fin.mod_def, LT.lt]
+    rw [(Nat.mod_eq_of_lt (Nat.lt_trans ‹a % m < m› mLt) : a % m % n.succ = a % m)]
+    exact Nat.mod_lt _ ‹0 < m›
+
 lemma Fin.add_comm (a b : Fin n) : a + b = b + a := by
-apply Fin.eq_of_val_eq
-simp only [Fin.add_def, Nat.add_comm]
+  apply Fin.eq_of_val_eq
+  simp only [Fin.add_def, Nat.add_comm]
 
 @[simp] lemma Fin.add_zero (a : Fin n.succ) : a + 0 = a := by
-apply Fin.eq_of_val_eq
-simp only [Fin.add_def, Fin.val_zero, Nat.add_zero]
-exact Nat.mod_eq_of_lt a.isLt
+  apply Fin.eq_of_val_eq
+  simp only [Fin.add_def, Fin.val_zero, Nat.add_zero]
+  exact Nat.mod_eq_of_lt a.isLt
 
-@[simp] lemma Fin.zero_add (a : Fin n.succ) : 0 + a = a := by
-rw [Fin.add_comm]
-exact Fin.add_zero a
-
-@[simp] lemma Fin.zero_mul (a : Fin n.succ) : 0 * a = 0 := by
-apply Fin.eq_of_val_eq
-simp only [Fin.mul_def, Fin.val_zero, Nat.zero_mul]
-simp [Nat.zero_mod]
+@[simp] lemma Fin.zero_add (a : Fin n.succ) : 0 + a = a := (Fin.add_comm _ _) ▸ Fin.add_zero a
 
 lemma Fin.mul_comm (a b : Fin n) : a * b = b * a := by
+  apply Fin.eq_of_val_eq
+  simp only [Fin.mul_def, Nat.mul_comm]
+
+@[simp] lemma Fin.zero_mul (a : Fin n.succ) : 0 * a = 0 := by
+  apply Fin.eq_of_val_eq
+  simp only [Fin.mul_def, Fin.val_zero, Nat.zero_mul]
+  simp [Nat.zero_mod]
+
+@[simp] lemma Fin.mul_zero (a : Fin n.succ) : a * 0 = 0 := by
+  rw [Fin.mul_comm]
+  exact Fin.zero_mul a
+
+@[simp] lemma Fin.one_mul (a : Fin n.succ) : 1 * a = a := by
 apply Fin.eq_of_val_eq
-simp only [Fin.mul_def, Nat.mul_comm]
+simp only [Fin.mul_def]
+cases n with
+| zero => simp only [Nat.eq_zero_of_le_zero (Nat.le_of_succ_le_succ a.isLt)]
+| succ n =>
+  have _ : (1 : Fin n.succ.succ).val = 1 := Nat.mod_eq_of_lt (Nat.succ_le_succ (Nat.succ_le_succ (Nat.zero_le n)))
+  rw [‹(1 : Fin n.succ.succ).val = 1›, Nat.one_mul, Nat.mod_eq_of_lt a.isLt]
+
+@[simp] lemma Fin.mul_one (a : Fin n.succ) : a * 1 = a := by
+  rw [Fin.mul_comm, Fin.one_mul]
 
 lemma Fin.add_assoc (a b c : Fin n) : (a + b) + c = a + (b + c) := by
 apply Fin.eq_of_val_eq
-simp only [Fin.mul_def, Fin.add_def, Nat.mod_add_mod, Nat.add_mod_mod, Nat.add_assoc]
+simp only [Fin.add_def, Nat.mod_add_mod, Nat.add_mod_mod, Nat.add_assoc]
 
-instance : Neg (Fin n) where
-  neg a := ⟨(n - a) % n, Nat.mod_lt _ (lt_of_le_of_lt (Nat.zero_le _) a.isLt)⟩
+lemma Fin.mul_assoc (a b c : Fin n) : (a * b) * c = a * (b * c) := by
+  apply Fin.eq_of_val_eq
+  simp only [Fin.mul_def]
+  generalize lhs : ((a.val * b.val) % n * c.val) % n = l
+  have hl : (a.val * b.val % n) * (c.val % n) % n = (a.val * b.val * c.val) % n := (Nat.mul_mod (a.val * b.val) c.val n).symm
+  rw [<- Nat.mod_eq_of_lt c.isLt, hl] at lhs
+  generalize rhs : a.val * (b.val * c.val % n) % n = r
+  have hr :  a.val % n * ((b.val * c.val) % n) % n = a.val * (b.val * c.val) % n := (Nat.mul_mod a.val (b.val * c.val) n).symm
+  rw [<- Nat.mod_eq_of_lt a.isLt, hr] at rhs
+  simp only [<- Nat.mul_assoc a.val b.val c.val] at rhs
+  rw [<- rhs, <- lhs]
 
 @[simp] lemma Fin.add_left_neg : ∀ (a : Fin n.succ), -a + a = 0
 | ⟨a, isLt⟩ => by
@@ -86,7 +132,7 @@ instance : Neg (Fin n) where
   | succ a =>
     have h1 : (n.succ - a.succ) % n.succ = (n.succ - a.succ) :=
       Nat.mod_eq_of_lt (Nat.sub_lt (Nat.succ_pos _) (Nat.succ_pos _))
-    have h_aux : (Nat.succ n - Nat.succ a + Nat.succ a) = Nat.succ n := by
+    have h_aux : (n.succ - a.succ + a.succ) = n.succ := by
       rw [Nat.add_comm]
       exact Nat.add_sub_of_le (Nat.le_of_lt isLt)
     rw [h1, h_aux, Nat.mod_self]
@@ -104,7 +150,7 @@ lemma Fin.nsmuls_eq (x : Nat) : ∀ (a : Fin n.succ), Fin.nsmul x a = Fin.ofNat 
   apply Fin.eq_of_val_eq
   simp only [Fin.nsmul, Fin.ofNat, Fin.mul_def]
   have hh : a % n.succ = a := Nat.mod_eq_of_lt isLt
-  generalize hq : x * a % Nat.succ n = q
+  generalize hq : x * a % n.succ = q
   rw [<- hh, <- Nat.mul_mod, hq]
 
 lemma Fin.nsmul_succ' (x : Nat) (a : Fin n.succ) : Fin.nsmul x.succ a = a + (Fin.nsmul x a) := by
@@ -134,6 +180,9 @@ instance : AddMonoid (Fin n.succ) where
   nsmul_zero' := Fin.zero_mul
   nsmul_succ' := Fin.nsmul_succ'
 
+instance : AddCommMonoid (Fin n.succ) where
+  add_comm := Fin.add_comm
+
 instance : SubNegMonoid (Fin n.succ) where
   sub_eq_add_neg := Fin.sub_eq_add_neg
   gsmul := Fin.gsmul
@@ -143,6 +192,19 @@ instance : SubNegMonoid (Fin n.succ) where
 
 instance : AddGroup (Fin n.succ) where
   add_left_neg := Fin.add_left_neg
+
+instance : Monoid (Fin n.succ) where
+  mul_one := Fin.mul_one
+  one_mul := Fin.one_mul
+  mul_assoc := Fin.mul_assoc
+  npow_zero' := fun _ => rfl
+  npow_succ' := fun _ _ => rfl
+
+def Fin.addOverflows? (a b : Fin n) : Bool := n <= a.val + b.val
+
+def Fin.mulOverflows? (a b : Fin n) : Bool := n <= a.val * b.val
+
+def Fin.subUnderflows? (a b : Fin n) : Bool := a.val < b.val
 
 def Fin.overflowingAdd (a b : Fin n) : (Bool × Fin n) := (n <= a.val + b.val, a + b)
 
@@ -186,5 +248,12 @@ lemma Fin.checked_sub_spec (a b : Fin n) : (Fin.checkedSub a b).isSome = true <-
     case pos => simp only [(decide_eq_true_iff (a.val < b.val)).mpr hx] at h
     case neg => exact Nat.le_of_not_lt hx
   case mpr => simp only [decide_eq_false (Nat.not_lt_of_le h : ¬a.val < b.val)]
+
+instance : CommMonoid (Fin n.succ) where
+  mul_comm := Fin.mul_comm
+
+instance : MonoidWithZero (Fin n.succ) where
+  zero_mul := Fin.zero_mul
+  mul_zero := Fin.mul_zero
 
 end Fin
--- Mathlib/Data/UInt.lean
+++ Mathlib/Data/UInt.lean
@@ -1,9 +1,230 @@
+import Mathlib.Data.Nat.Basic
+import Mathlib.Data.Fin.Basic
+import Mathlib.Algebra.Group.Defs
+import Mathlib.Algebra.GroupWithZero.Defs
 
-theorem Fin.val_eq_of_lt : a < n.succ → (@Fin.ofNat n a).val = a := Nat.mod_eq_of_lt
+lemma UInt8.size_positive : 0 < UInt8.size := by decide
 
-theorem UInt32.val_eq_of_lt : a < UInt32.size → (UInt32.ofNat a).val = a := Fin.val_eq_of_lt
+lemma UInt16.size_positive : 0 < UInt16.size := by decide
+
+lemma UInt32.size_positive : 0 < UInt32.size := by decide
+
+lemma UInt64.size_positive : 0 < UInt64.size := by decide
+
+lemma USize.size_positive : 0 < USize.size := by
+  simp only [USize.size]
+  cases System.Platform.numBits_eq with
+  | inl eq32 => simp [eq32]
+  | inr eq64 => simp [eq64]
+
+set_option hygiene false
+macro "genIntDeclars" typeName:ident : command => do
+  `(
+    namespace $typeName
+      instance : Zero (Fin size) := (Nat.succ_pred_eq_of_pos size_positive) ▸ inferInstance
+
+      instance : One (Fin size) := (Nat.succ_pred_eq_of_pos size_positive) ▸ inferInstance
+
+      instance : Neg (Fin size) := (Nat.succ_pred_eq_of_pos size_positive) ▸ inferInstance
+
+      instance : AddSemigroup (Fin size) := (Nat.succ_pred_eq_of_pos size_positive) ▸ inferInstance
+
+      instance : AddMonoid (Fin size) := (Nat.succ_pred_eq_of_pos size_positive) ▸ inferInstance
+
+      instance : AddCommMonoid (Fin size) := (Nat.succ_pred_eq_of_pos size_positive) ▸ inferInstance
+
+      instance : SubNegMonoid (Fin size) := (Nat.succ_pred_eq_of_pos size_positive) ▸ inferInstance
+
+      instance : AddGroup (Fin size) := (Nat.succ_pred_eq_of_pos size_positive) ▸ inferInstance
+
+      instance : Monoid (Fin size) := (Nat.succ_pred_eq_of_pos size_positive) ▸ inferInstance
+
+      instance : CommMonoid (Fin size) := (Nat.succ_pred_eq_of_pos size_positive) ▸ inferInstance
+
+      instance : MonoidWithZero (Fin size) := (Nat.succ_pred_eq_of_pos size_positive) ▸ inferInstance
+
+      lemma val_eq_of_lt : a < size → (ofNat a).val = a := Fin.val_eq_of_lt
+
+      lemma sub_def (a b : $typeName) : a - b = ⟨a.val - b.val⟩ := rfl
+
+      lemma mul_def (a b : $typeName) : a * b = ⟨a.val * b.val⟩ := rfl
+
+      lemma mod_def (a b : $typeName) : a % b = ⟨a.val % b.val⟩ := rfl
+
+      lemma add_def (a b : $typeName) : a + b = ⟨a.val + b.val⟩ := rfl
+
+      lemma eq_of_val_eq : ∀ {a b : $typeName}, a.val = b.val -> a = b
+      | ⟨f1⟩, ⟨f2⟩, h => congrArg mk h
+
+      lemma val_eq_of_eq : ∀ {a b : $typeName}, a = b -> a.val = b.val
+      | ⟨f1⟩, ⟨f2⟩, h => congrArg val h
+
+      @[simp] lemma mk_val_eq (a : $typeName) : mk a.val = a := by
+        cases h:a with
+        | mk aFin =>
+          have _ : a.val = aFin := h ▸ rfl
+          simp only [‹a.val = aFin›]
+
+      lemma add_assoc (a b c : $typeName) : (a + b) + c = a + (b + c) :=
+        congrArg mk (Fin.add_assoc _ _ _)
+
+      lemma mul_comm (a b : $typeName) : a * b = b * a := congrArg mk (Fin.mul_comm _ _)
+
+      lemma add_comm (a b : $typeName) : a + b = b + a := congrArg mk (Fin.add_comm _ _)
+
+      @[simp] lemma add_zero (a : $typeName) : a + 0 = a := by
+        have h0 : a.val + (0 : $typeName).val = a.val := AddMonoid.add_zero a.val
+        simp only [add_def, h0, mk_val_eq]
+
+      @[simp] lemma zero_add (a : $typeName) : 0 + a = a :=
+        (add_comm _ _) ▸ add_zero _
+
+      @[simp] lemma zero_mul (a : $typeName) : 0 * a = 0 := by
+        have h0 : (0 : $typeName).val * a.val = (0 : Fin size) := MonoidWithZero.zero_mul a.val
+        simp only [mul_def, h0, mk_val_eq]
+
+      @[simp] lemma mul_zero (a : $typeName) : a * 0 = 0 :=
+        (mul_comm a _) ▸ zero_mul _
+
+      @[simp] lemma one_mul (a : $typeName) : 1 * a = a := by
+        have h0 : (1 : $typeName).val * a.val = a.val := Monoid.one_mul a.val
+        simp only [mul_def, h0, mk_val_eq]
+
+      @[simp] lemma mul_one (a : $typeName) : a * 1 = a := (mul_comm _ _) ▸ one_mul _
+
+      instance : Neg $typeName where
+        neg a := mk (-a.val)
+
+      lemma neg_def (a : $typeName) : -a = mk (-a.val) := rfl
+
+      lemma sub_eq_add_neg (a b : $typeName) : a - b = a + -b :=
+        congrArg mk (SubNegMonoid.sub_eq_add_neg _ _)
+
+      def nsmul (x : Nat) (a : $typeName) : $typeName := mk (AddMonoid.nsmul x a.val)
+
+      def gsmul (x : Int) (a : $typeName) : $typeName := mk (SubNegMonoid.gsmul x a.val)
+
+      @[simp] lemma gsmul_zero' (a : $typeName) : gsmul 0 a = (0 : $typeName) :=
+        congrArg mk (SubNegMonoid.gsmul_zero' a.val)
+
+      lemma gsmul_succ' (m : ℕ) (a : $typeName) : gsmul (Int.ofNat m.succ) a = a + gsmul (Int.ofNat m) a :=
+        congrArg mk (SubNegMonoid.gsmul_succ' m a.val)
+
+      lemma gsmul_neg' (m : ℕ) (a : $typeName) : gsmul (Int.negSucc m) a = -(gsmul m.succ a) :=
+        congrArg mk (SubNegMonoid.gsmul_neg' m a.val)
+
+      instance : AddSemigroup $typeName where
+        add_assoc := add_assoc
+
+      @[simp] lemma nsmul_zero' (x : $typeName) : nsmul 0 x = 0 :=
+        congrArg mk (AddMonoid.nsmul_zero' x.val)
+
+      lemma nsmul_succ' (n : ℕ) (x : $typeName) : nsmul n.succ x = x + nsmul n x :=
+        congrArg mk (AddMonoid.nsmul_succ' n x.val)
+
+      @[simp] lemma add_left_neg (a : $typeName) : -a + a = 0 :=
+        congrArg mk (AddGroup.add_left_neg a.val)
+
+      lemma mul_assoc (a b c : $typeName) : (a * b) * c = a * (b * c) :=
+        congrArg mk (Semigroup.mul_assoc _ _ _)
+
+      instance : AddMonoid $typeName where
+        add_zero := add_zero
+        zero_add := zero_add
+        nsmul := nsmul
+        nsmul_zero' := nsmul_zero'
+        nsmul_succ' := nsmul_succ'
+
+      instance : AddCommMonoid $typeName where
+        add_comm := add_comm
+
+      instance : SubNegMonoid $typeName where
+        sub_eq_add_neg := sub_eq_add_neg
+        gsmul := gsmul
+        gsmul_zero' := gsmul_zero'
+        gsmul_succ' := gsmul_succ'
+        gsmul_neg' := gsmul_neg'
+
+      instance : Monoid $typeName where
+        mul_one := mul_one
+        one_mul := one_mul
+        mul_assoc := mul_assoc
+        npow_zero' := fun _ => rfl
+        npow_succ' := fun _ _ => rfl
+
+      instance : CommMonoid $typeName where
+        mul_comm := mul_comm
+
+      instance : MonoidWithZero $typeName where
+        zero_mul := zero_mul
+        mul_zero := mul_zero
+
+      def addOverflows? (a b : $typeName) : Bool := size <= a.toNat + b.toNat
+
+      def mulOverflows? (a b : $typeName) : Bool := size <= a.toNat * b.toNat
+
+      def subUnderflows? (a b : $typeName) : Bool := a.toNat < b.toNat
+
+      def overflowingAdd (a b : $typeName) : (Bool × $typeName) := (size <= a.toNat + b.toNat, a + b)
+
+      def overflowingMul (a b : $typeName) : (Bool × $typeName) := (size <= a.toNat * b.toNat, a * b)
+
+      def underflowingSub (a b : $typeName) : (Bool × $typeName) := (a.toNat < b.toNat, a - b)
+
+      def checkedAdd (a b : $typeName) : Option $typeName :=
+        match a.overflowingAdd b with
+        | (true, _) => none
+        | (false, sum) => some (sum)
+
+      def checkedMul (a b : $typeName) : Option $typeName :=
+        match a.overflowingMul b with
+        | (true, _) => none
+        | (false, prod) => some (prod)
+
+      def checkedSub (a b : $typeName) : Option $typeName :=
+        match a.underflowingSub b with
+        | (true, _) => none
+        | (false, diff) => some (diff)
+
+      lemma checked_add_spec (a b : $typeName) : (a.checkedAdd b).isSome = true <-> a.toNat + b.toNat < size := by
+        by_cases size <= a.toNat + b.toNat <;>
+          simp_all [checkedAdd, Option.isSome, overflowingAdd, decide_eq_true, decide_eq_false]
+
+      lemma checked_mul_spec (a b : $typeName) : (a.checkedMul b).isSome = true <-> a.toNat * b.toNat < size := by
+        simp only [checkedMul, overflowingMul, Option.isSome]
+        refine Iff.intro ?mp ?mpr <;> intro h
+        case mp =>
+          by_cases hx : size <= a.toNat * b.toNat
+          case pos => simp only [(decide_eq_true_iff (size <= a.toNat * b.toNat)).mpr hx] at h
+          case neg => exact Nat.lt_of_not_le hx
+        case mpr => simp only [decide_eq_false (Nat.not_le_of_lt h : ¬size <= a.toNat * b.toNat)]
+
+      lemma checked_sub_spec (a b : $typeName) : (a.checkedSub b).isSome = true <-> b.toNat <= a.toNat := by
+        simp only [checkedSub, underflowingSub, Option.isSome]
+        refine Iff.intro ?mp ?mpr <;> intro h
+        case mp =>
+          by_cases hx : a.val < b.val
+          case pos => simp only [(decide_eq_true_iff (a.toNat < b.toNat)).mpr hx] at h
+          case neg => exact Nat.le_of_not_lt hx
+        case mpr => simp only [decide_eq_false (Nat.not_lt_of_le h : ¬a.toNat < b.toNat)]
+
+      lemma mod_lt (x : $typeName) {y : $typeName} : 0 < y -> x % y < y :=
+        fun y_pos => Fin.mod_lt x.val y_pos
+
+      lemma mod_eq_of_lt {a b : $typeName} : a < b -> a % b = a :=
+        fun a_lt_b => eq_of_val_eq (Fin.mod_eq_of_lt a_lt_b)
+
+    end $typeName
+  )
+
+genIntDeclars UInt8
+genIntDeclars UInt16
+genIntDeclars UInt32
+genIntDeclars UInt64
+--genIntDeclars USize
 
 namespace UInt8
+
 
 /-- Is this an uppercase ASCII letter? -/
 def isUpper (c : UInt8) : Bool :=
