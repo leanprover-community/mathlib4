8fe680846b7f4dd1b93688a6355c1c1a0cba7f06
Gabriel Ebner <gebner@gebner.org>
2021-11-30 09:44:11 +0000

    chore: bump to Lean 4 nightly 2021-11-30 (#110)

--- Mathlib/Data/Char.lean
+++ Mathlib/Data/Char.lean
@@ -2,10 +2,9 @@
 /-- Convert a character into a `UInt8`, by truncating (reducing modulo 256) if necessary. -/
 def Char.toUInt8 (n : Char) : UInt8 := n.1.toUInt8
 
-theorem Char.utf8Size_pos (c : Char) : 0 < c.utf8Size :=
-  let this {c} {t e : UInt32} : [Decidable c] → 0 < t → 0 < e → 0 < ite c t e
-  | Decidable.isTrue _, h, _ => h
-  | Decidable.isFalse _, _, h => h
-  this (by decide) $ this (by decide) $ this (by decide) (by decide)
+theorem Char.utf8Size_pos (c : Char) : 0 < c.utf8Size := by
+  simp only [utf8Size]
+  repeat (split; decide)
+  decide
 
 theorem String.csize_pos : (c : Char) → 0 < String.csize c := Char.utf8Size_pos
