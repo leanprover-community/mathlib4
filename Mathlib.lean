e9a05cbb5d8bce3b791cbc9aebf35c0dec9a9195
Floris van Doorn <fpvdoorn@gmail.com>
2021-11-30 16:58:24 +0100

    fix

--- Mathlib.lean
+++ Mathlib.lean
@@ -29,8 +29,8 @@ import Mathlib.Init.Function
 import Mathlib.Init.Logic
 import Mathlib.Init.Set
 import Mathlib.Init.SetNotation
-import Mathlib.Lean.LocalContext
 import Mathlib.Lean.Expr
+import Mathlib.Lean.LocalContext
 import Mathlib.Logic.Basic
 import Mathlib.Logic.Function.Basic
 import Mathlib.Mathport.Attributes
