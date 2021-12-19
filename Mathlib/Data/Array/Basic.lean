560f08f7faa727bf49a4a722e8ef31b080c32a06
Mario Carneiro <di.gama@gmail.com>
2021-12-18 19:28:14 -0800

    feat: more theorems on arrays and binary heaps (#138)

--- Mathlib/Data/Array/Basic.lean
+++ Mathlib/Data/Array/Basic.lean
@@ -29,4 +29,47 @@ where
     suffices _::_ = _ by rw [this]; apply aux
     apply List.get_cons_drop
 
+theorem get_eq_get (a : Array α) (i : Nat) (h : i < a.size) :
+  a.get ⟨i, h⟩ = a.data.get i h := rfl
+
+theorem get?_eq_get (a : Array α) (i : Nat) (h : i < a.size) :
+  a.get? i = some (a.get ⟨i, h⟩) := by simp [get?, h]
+
+theorem get?_len_le (a : Array α) (i : Nat) (h : a.size ≤ i) :
+  a.get? i = none := by simp [get?, not_lt_of_ge h]
+
+theorem get?_eq_get? (a : Array α) (i : Nat) :
+  a.get? i = a.data.get? i := by
+  simp [get?]; split <;> rename_i h
+  · simp [get, List.get?_eq_get h]
+  · simp [List.get?_len_le (le_of_not_lt h)]
+
+theorem get?_push_lt (a : Array α) (x : α) (i : Nat) (h : i < a.size) :
+  (a.push x).get? i = some (a.get ⟨i, h⟩) := by
+  simp [push, get?_eq_get?, ← List.get?_eq_get, get_eq_get, List.concat_eq_append]
+  exact List.get?_append h
+
+theorem get?_push_eq (a : Array α) (x : α) :
+  (a.push x).get? a.size = some x := by
+  simp [push, get?_eq_get?, ← List.get?_eq_get, get_eq_get, List.concat_eq_append]
+
+theorem get_push (a : Array α) (x : α) (i) :
+  (a.push x).get i = if h : i < a.size then a.get ⟨i, h⟩ else x := by
+  split <;> (rename_i h; apply Option.some.inj; rw [← get?_eq_get])
+  · apply get?_push_lt
+  · match i with | ⟨i, hi⟩ => ?_
+    simp at hi ⊢
+    rw [le_antisymm (Nat.le_of_lt_succ hi) (le_of_not_lt h), get?_push_eq]
+
+@[simp] lemma get?_set_eq (a : Array α) (i) (v : α) : (a.set i v).get? i = v := by
+  simp [set, get?_eq_get?, List.get?_set_of_lt _ i.2]
+
+@[simp] lemma get?_set_ne (a : Array α) {i j} (v : α)
+  (h : i.1 ≠ j) : (a.set i v).get? j = a.get? j := by
+  simp [set, get?_eq_get?, List.get?_set_ne _ _ h]
+
+lemma get?_set (a : Array α) (i j) (v : α) :
+  (a.set i v).get? j = if i.1 = j then some v else a.get? j := by
+  split; {subst j; simp}; simp_all
+
 end Array
