ff8c4c036b3c661ff3c8ff39029450f5f13b86fa
Gabriel Ebner <gebner@gebner.org>
2022-01-17 12:00:09 +0100

    put clearer fixme in normcast

--- Mathlib/Tactic/NormCast.lean
+++ Mathlib/Tactic/NormCast.lean
@@ -42,7 +42,7 @@ structure NormCastExtension where
   squash : SimpExtension
   deriving Inhabited
 
-builtin_initialize normCastExt : NormCastExtension ← pure {
+initialize normCastExt : NormCastExtension ← pure {
   up := ← mkSimpExt `Tactic.NormCast.normCastExt.up
   down := ← mkSimpExt `Tactic.NormCast.normCastExt.down
   squash := ← mkSimpExt `Tactic.NormCast.normCastExt.squash
@@ -79,7 +79,8 @@ def addInfer (decl : Name)
   -- TODO: not just elim
   addElim decl kind prio
 
--- FIXME: Lean crashes here now
+-- FIXME: put this in a different file, because we can't access attributes in
+-- the file they're defined in.
 -- run_cmd Elab.Command.liftCoreM $ MetaM.run' do
 --   addElim ``ne_eq
 
