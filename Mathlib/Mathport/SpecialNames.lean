7e60eeeb7a57c2a107f5dbb4b324dc67723c173a
Daniel Selsam <daniel.selsam@protonmail.com>
2022-01-20 13:16:43 -0800

    fix: align has_[zero|one]

--- Mathlib/Mathport/SpecialNames.lean
+++ Mathlib/Mathport/SpecialNames.lean
@@ -51,6 +51,8 @@ namespace Mathlib.Prelude
 
 -- Generic 'has'-stripping
 -- Note: we don't currently strip automatically for various reasons.
+#align has_zero      Zero
+#align has_one       One
 #align has_add       Add
 #align has_sub       Sub
 #align has_mul       Mul
