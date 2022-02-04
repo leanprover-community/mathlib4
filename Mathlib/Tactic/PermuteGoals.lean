0d08a71bee7ca5197279b8fb161bddaae1002a2c
Arthur Paulino <arthurleonardo.ap@gmail.com>
2022-02-04 17:25:17 -0300

    renaming tactics

--- Mathlib/Tactic/PermuteGoals.lean
+++ Mathlib/Tactic/PermuteGoals.lean
@@ -16,15 +16,15 @@ open Lean Elab Elab.Tactic
 
 `rotate -` moves the last goal to the front. `rotate -n` will do this `n` times.
 
-See also `Tactic.pick_goal`, which moves the `n`-th goal to the front.
+See also `Tactic.raise`, which moves the `n`-th goal to the front.
 -/
-syntax (name := rotate_goals) "rotate_goals" ppSpace "-"? (num)? : tactic
-@[tactic rotate_goals] def evalRotateGoals : Tactic := fun stx => do
+syntax (name := rotate) "rotate" ppSpace "-"? (num)? : tactic
+@[tactic rotate] def evalRotate : Tactic := fun stx => do
 match stx with
-  | `(tactic|rotate_goals)     => setGoals $ (← getGoals).rotateLeft 1
-  | `(tactic|rotate_goals -)   => setGoals $ (← getGoals).rotateRight 1
-  | `(tactic|rotate_goals $n)  => setGoals $ (← getGoals).rotateLeft n.toNat
-  | `(tactic|rotate_goals -$n) => setGoals $ (← getGoals).rotateRight n.toNat
+  | `(tactic|rotate)     => setGoals $ (← getGoals).rotateLeft 1
+  | `(tactic|rotate -)   => setGoals $ (← getGoals).rotateRight 1
+  | `(tactic|rotate $n)  => setGoals $ (← getGoals).rotateLeft n.toNat
+  | `(tactic|rotate -$n) => setGoals $ (← getGoals).rotateRight n.toNat
   | _ => throwUnsupportedSyntax
 
 /-- Computes the round-tripping `n`-th goal. -/
@@ -36,20 +36,20 @@ private def roundTripNth (n : ℕ) (reverse : Bool) : TacticM (ℕ × List MVarI
   (nth, goals)
 
 /--
-`pick_goal n` will move the `n`-th goal to the front.
+`raise n` will move the `n`-th goal to the front.
 
-`pick_goal -n` will move the `n`-th goal (counting from the bottom) to the front.
+`raise -n` will move the `n`-th goal (counting from the bottom) to the front.
 
 See also `Tactic.rotate`, which moves goals from the front to the back and vice-versa.
 -/
-syntax (name := pick_goal) "pick_goal" ppSpace ppSpace "-"? num : tactic
-@[tactic pick_goal] def evalPickGoal : Tactic := fun stx => do
+syntax (name := raise) "raise" ppSpace ppSpace "-"? num : tactic
+@[tactic raise] def evalRaise : Tactic := fun stx => do
 match stx with
-  | `(tactic|pick_goal $n)  => pick_goal n.toNat false
-  | `(tactic|pick_goal -$n) => pick_goal n.toNat true
+  | `(tactic|raise $n)  => raise n.toNat false
+  | `(tactic|raise -$n) => raise n.toNat true
   | _ => throwUnsupportedSyntax
 where
-  pick_goal (n : ℕ) (reverse : Bool) : TacticM Unit := do
+  raise (n : ℕ) (reverse : Bool) : TacticM Unit := do
     let (nth, goals) ← roundTripNth n reverse
     match nth with
       | 0 => throwError "goals are 1-indexed"
@@ -58,16 +58,18 @@ where
           | (_, []) => throwError "not enough goals"
           | (gls, g :: grs) => setGoals $ g :: (gls ++ grs)
 
-
-/-- `swap` is a shortcut for `pick_goal 2`, which interchanges the 1st and 2nd goals. -/
-macro "swap" : tactic => `(pick_goal 2)
+/-- `swap` is a shortcut for `raise 2`, which interchanges the 1st and 2nd goals. -/
+macro "swap" : tactic => `(raise 2)
 
 /--
 `work_on_goal n tacSeq` creates a block scope for the `n`-th goal and tries the sequence
 of tactics `tacSeq` on it.
 
+`work_on_goal -n tacSeq` does the same, but the `n`-th goal is chosen by counting from the
+bottom.
+
 The goal is not required to be solved and any resulting subgoals are inserted back into the
-list of goals, replacing the `n`-th goal.
+list of goals, replacing the chosen goal.
 -/
 syntax (name := work_on_goal) "work_on_goal" ppSpace "-"? num ppSpace tacticSeq : tactic
 @[tactic work_on_goal] def evalWorkOnGoal : Tactic := fun stx => do
