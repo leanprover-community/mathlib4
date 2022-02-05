7a499c19025020abee64cd326d101df4fc131772
Arthur Paulino <arthurleonardo.ap@gmail.com>
2022-02-04 22:28:27 -0300

    fixing copyrights

--- test/SimpRw.lean
+++ test/SimpRw.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2022 Arthur Paulino. All rights reserved.
+Copyright (c) 2020 Anne Baanen. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Anne Baanen, Mario Carneiro
 -/
