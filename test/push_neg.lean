c0206ac8fa34de4b262c4cb6443fc95ec933c167
Alice <aliceetampes@gmail.com>
2022-02-13 23:29:42 +0100

    Changing arrow, tab, and a function name

--- test/push_neg.lean
+++ test/push_neg.lean
@@ -1,4 +1,3 @@
-
 /-
 Copyright (c) 2022 Alice Laroche. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
@@ -7,24 +6,24 @@ Author: Alice Laroche
 
 import Mathlib.Tactic.Pushneg
 
-variable {α : Type} {p q : Prop} {p' q' : α -> Prop}
+variable {α : Type} {p q : Prop} {p' q' : α → Prop}
 
-example : (¬p ∧ ¬q) -> ¬(p ∨ q) :=
-by intro h
-   push_neg
-   exact h
+example : (¬p ∧ ¬q) → ¬(p ∨ q) := by
+  intro h
+  push_neg
+  exact h
 
-example : ¬(p ∧ q) -> (p → ¬q) :=
-by intro h
-   push_neg at h
-   exact h
+example : ¬(p ∧ q) → (p → ¬q) :=by
+  intro h
+  push_neg at h
+  exact h
 
-example : (∀(x : α), ¬ p' x) -> ¬ ∃(x : α), p' x:=
-by intro h
-   push_neg
-   exact h
+example : (∀(x : α), ¬ p' x) → ¬ ∃(x : α), p' x:= by
+  intro h
+  push_neg
+  exact h
 
-example : (¬ ∀(x : α), p' x) -> (∃(x : α), ¬ p' x) :=
-by intro h
-   push_neg at h
-   exact h
+example : (¬ ∀(x : α), p' x) → (∃(x : α), ¬ p' x) :=by
+  intro h
+  push_neg at h
+  exact h
