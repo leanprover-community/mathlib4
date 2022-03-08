a382ac90435c9fb2e54134d47e3ea08cf681742f
Henrik Böving <hargonix@gmail.com>
2022-03-08 09:05:33 +0000

    feat: Implement Control.Random without streams (#218)

--- Mathlib.lean
+++ Mathlib.lean
@@ -2,6 +2,7 @@ import Mathlib.Algebra.Group.Basic
 import Mathlib.Algebra.Group.Defs
 import Mathlib.Algebra.GroupWithZero.Defs
 import Mathlib.Algebra.Ring.Basic
+import Mathlib.Control.Random
 import Mathlib.Data.Array.Basic
 import Mathlib.Data.Array.Defs
 import Mathlib.Data.BinaryHeap
