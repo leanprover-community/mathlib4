6a1b3e938f30cc4c0f2607f9b5b1d96ddc915f20
Mario Carneiro <di.gama@gmail.com>
2022-03-09 18:11:30 -0500

    review changes

--- Mathlib/Util/IncludeStr.lean
+++ Mathlib/Util/IncludeStr.lean
@@ -10,9 +10,6 @@ namespace Mathlib.Util
 elab (name := includeStr) "include_str " str:str : term => do
   let some str := str.isStrLit? | Lean.Elab.throwUnsupportedSyntax
   let srcPath := System.FilePath.mk (← read).fileName
-  let currentDir ← IO.currentDir
   let some srcDir := srcPath.parent | throwError "{srcPath} not in a valid directory"
   let path := srcDir / str
-  unless ← path.pathExists do throwError "{path} does not exist as a file"
-  if ← path.isDir then throwError "{str} is a directory"
   Lean.mkStrLit <$> IO.FS.readFile path
