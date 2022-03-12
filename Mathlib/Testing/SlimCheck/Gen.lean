4f2f2abb82aa7d219d4c855e3dd883e39fc51896
Henrik Böving <hargonix@gmail.com>
2022-03-12 20:57:49 +0100

    doc: Doc and copyright header for Gen

--- Mathlib/Testing/SlimCheck/Gen.lean
+++ Mathlib/Testing/SlimCheck/Gen.lean
@@ -1,12 +1,33 @@
+/-
+Copyright (c) 2021 Henrik Böving. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Henrik Böving
+-/
 import Mathlib.Control.Random
 import Mathlib.Data.List.Perm
 import Mathlib.Data.Subtype
 import Mathlib.Data.Nat.Basic
 
+/-!
+# `Gen` Monad
+This monad is used to formulate randomized computations with a parameter
+to specify the desired size of the result.
+This is a port of the Haskell QuickCheck library.
+## Main definitions
+  * `Gen` monad
+## Tags
+random testing
+## References
+  * https://hackage.haskell.org/package/QuickCheck
+-/
+
 namespace SlimCheck
 
 open Random
 
+/-- Monad to generate random examples to test properties with.
+It has a `Nat` parameter so that the caller can decide on the
+size of the examples. -/
 abbrev Gen (α : Type u) := ReaderT (ULift Nat) Rand α
 
 /-- Lift `Random.random` to the `Gen` monad. -/
@@ -27,18 +48,20 @@ lemma chooseNatLt_aux {lo hi : Nat} (a : Nat) (h : Nat.succ lo ≤ a ∧ a ≤ h
      this
     )
 
+/-- Generate a `Nat` example between `x` and `y` (exclusively). -/
 def chooseNatLt (lo hi : Nat) (h : lo < hi) : Gen {a // lo ≤ a ∧ a < hi} :=
   Subtype.map Nat.pred chooseNatLt_aux <$> choose Nat (lo.succ) hi (Nat.succ_le_of_lt h)
 
+/-- Get access to the size parameter of the `Gen` monad. -/
 def getSize : Gen Nat :=
   read >>= pure ∘ ULift.down
 
-def sized (f : Nat → Gen α) : Gen α :=
-  getSize >>= f
-
+/-- Apply a function to the size parameter. -/
 def resize (f : Nat → Nat) (x : Gen α) : Gen α :=
   withReader (ULift.up ∘ f ∘ ULift.down) x
 
+/-- Create an `Array` of examples using `x`. The size is controlled
+by the size parameter of `Gen`. -/
 def arrayOf (x : Gen α) : Gen (Array α) := do
   let sz ← choose Nat 0 (←getSize) (Nat.zero_le _)
   let mut res := #[]
@@ -46,18 +69,23 @@ def arrayOf (x : Gen α) : Gen (Array α) := do
     res := res.push (←x)
   pure res
 
+/-- Create an `List` of examples using `x`. The size is controlled
+by the size parameter of `Gen`. -/
 def listOf (x : Gen α) : Gen (List α) :=
   arrayOf x >>= pure ∘ Array.toList
 
+/-- Given a list of example generators, choose one to create an example. -/
 def oneOf (xs : List (Gen α)) (pos : 0 < xs.length) : Gen α := do
   let ⟨x, h1, h2⟩ ← chooseNatLt 0 xs.length pos
   xs.get ⟨x, h2⟩
 
+/-- Given a list of examples, choose one to create an example. -/
 def elements (xs : List α) (pos : 0 < xs.length) : Gen α := do
   let ⟨x, h1, h2⟩ ← chooseNatLt 0 xs.length pos
   pure $ xs.get ⟨x, h2⟩
 
 open List in
+/-- Generate a random permutation of a given list. -/
 def permutationOf : (xs : List α) → Gen { ys // ys ~ xs }
 | [] => pure ⟨[], Perm.nil⟩
 | x::xs => do
