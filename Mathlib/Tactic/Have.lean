7043e868faab007245ab381416359e501061e537
Arthur Paulino <arthurleonardo.ap@gmail.com>
2022-03-17 09:45:08 -0300

    fix syntax tree

--- Mathlib/Tactic/Have.lean
+++ Mathlib/Tactic/Have.lean
@@ -5,31 +5,28 @@ Authors: Arthur Paulino
 -/
 import Lean
 
+def Lean.Parser.Term.haveIdLhs' :=
+  optional (ident >> many (ppSpace >>
+    checkColGt "expected to be indented" >>
+    (simpleBinderWithoutType <|> bracketedBinder))) >> optType
+
 namespace Mathlib.Tactic
 
 open Lean Elab.Tactic Meta Parser.Term
 
-syntax "have " haveIdLhs : tactic
+syntax "have " haveIdLhs' : tactic
 
-private def haveDef (n? t? : Option Syntax) : TacticM Unit :=
+elab_rules : tactic
+| `(tactic|have $[$n:ident]? $[: $t:term]?) =>
   withMainContext do
-    let name : Name := match n? with
+    let name : Name := match n with
       | none   => `this
       | some n => n.getId
-    let t ← match t? with
-    | none   => mkFreshTypeMVar
-    | some t => elabTerm t none
+    let t ← match t with
+      | none   => mkFreshTypeMVar
+      | some t => elabTerm t none
     liftMetaTactic fun mvarId => do
       let p ← mkFreshExprMVar (userName := name) t
       let mvarIdNew ← assert mvarId name t p
       let (_, mvarIdNew) ← intro1P mvarIdNew
       return [p.mvarId!, mvarIdNew]
-
-elab_rules : tactic
-  | `(tactic|have $[$n?:ident]? : $t:term) => haveDef n? (some t)
-  | `(tactic|have $[$n?:ident]?)           => haveDef n? none
-
-example : True := by
-  have h;
-  exact 5
-  simp
