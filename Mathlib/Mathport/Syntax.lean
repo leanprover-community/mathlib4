8429fdb79d6a8ca7735ebe53f4248cbb0591fdf3
Beanway144 <ian.benway@gmail.com>
2022-03-25 00:14:41 +0000

    feat: set tactic (#242)
    Adding `set` tactic
    
    Co-authored-by: Beanway144 <65977044+Beanway144@users.noreply.github.com>
    Co-authored-by: Mario Carneiro <di.gama@gmail.com>

--- Mathlib/Mathport/Syntax.lean
+++ Mathlib/Mathport/Syntax.lean
@@ -15,6 +15,7 @@ import Mathlib.Tactic.NormCast
 import Mathlib.Tactic.NormNum
 import Mathlib.Tactic.RCases
 import Mathlib.Tactic.Ring
+import Mathlib.Tactic.Set
 import Mathlib.Tactic.ShowTerm
 import Mathlib.Tactic.Simps
 import Mathlib.Tactic.SolveByElim
@@ -259,8 +260,6 @@ syntax (name := guardExprEq') "guard_expr " term:51 " = " term : tactic -- defin
 syntax (name := guardTarget') "guard_target" " = " term : tactic -- definitional equality
 syntax (name := triv) "triv" : tactic
 syntax (name := clearAuxDecl) "clear_aux_decl" : tactic
-syntax (name := set) "set " ident (" : " term)? " := " term (" with " "←"? ident)? : tactic
-syntax (name := set!) "set! " ident (" : " term)? " := " term (" with " "←"? ident)? : tactic
 syntax (name := clearExcept) "clear " "*" " - " ident* : tactic
 syntax (name := extractGoal) "extract_goal" (ppSpace ident)?
   (" with" (ppSpace (colGt ident))*)? : tactic
