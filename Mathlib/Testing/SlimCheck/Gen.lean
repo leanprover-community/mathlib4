ce376ba742e00dae5b4d9fef72fca6c71270bdee
Henrik Böving <hargonix@gmail.com>
2022-03-31 13:57:00 +0200

    fix: Add Simon as co author
    He implemented the original variant and basically all comments
    are 1:1 copied from him.

--- Mathlib/Testing/SlimCheck/Gen.lean
+++ Mathlib/Testing/SlimCheck/Gen.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2021 Henrik Böving. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Henrik Böving
+Authors: Henrik Böving, Simon Hudon
 -/
 import Mathlib.Control.Random
 import Mathlib.Data.List.Perm
