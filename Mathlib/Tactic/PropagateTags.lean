acabf4b9c9687d949adc3dfe242e19150d49d6c3
Ian Benway <ian.benway@gmail.com>
2022-04-04 23:43:16 -0400

    added guard_tags

--- Mathlib/Tactic/PropagateTags.lean
+++ Mathlib/Tactic/PropagateTags.lean
@@ -3,9 +3,15 @@ Copyright (c) 2022 Ian Benway. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Author: Ian Benway.
 -/
+
 import Lean
 namespace Mathlib.Tactic
 open Lean Elab.Tactic Meta
+
+/-- Test that `t` is the tag of the main goal. -/
+elab (name := guardTags) "guard_tags" ts:(ppSpace ident) : tactic => do
+  guard ((← getMainTag) == ts.getId)
+
 /--
 `propagate_tags tac` carries the tag of the main goal to the main goal of the result of doing `tac`.
 -/
--- test/PropagateTags.lean
+++ test/PropagateTags.lean
@@ -8,6 +8,26 @@ import Mathlib.Tactic.PropagateTags
 import Mathlib.Tactic.Basic
 open Lean Elab.Tactic Meta
 
+/-tests for guard_tags-/
+example : True := by
+  cases true
+  guard_tags false; trivial
+  guard_tags true; trivial
+
+example : True ∧ True := by
+  apply And.intro
+  guard_tags left; trivial
+  guard_tags right; trivial
+
+example : True ∧ True := by
+  cases true
+  all_goals apply And.intro
+  guard_tags false.left; trivial
+  guard_tags false.right; trivial
+  guard_tags true.left; trivial
+  guard_tags true.right; trivial
+
+/-tests for propagate_tags-/
 /-Rids the first goal of any tags, for testing.-/
 elab "retag" x:(ident <|> "_") : tactic => liftMetaTactic1 fun g => do
   let g2 ← mkFreshExprMVar (← getMVarType g) (userName := x.getId)
@@ -17,8 +37,10 @@ elab "retag" x:(ident <|> "_") : tactic => liftMetaTactic1 fun g => do
 example (h : Nat → Nat → Nat) : Nat := by
   cases true
   propagate_tags retag _ -- does nothing
+  guard_tags false
   propagate_tags
     apply h; retag _; rotate_left; retag _; rotate_right
+  guard_tags false
   propagate_tags
     rotate_left;
   all_goals {apply h 4 4}
@@ -28,6 +50,9 @@ example (h g : Prop) (a : h) (b : g) : h ∧ g := by
   cases true
   propagate_tags
     retag _; rotate_right; rotate_left
+  guard_tags left.false
   case left.false => apply a
+  guard_tags left.true
   case left.true => apply a
+  guard_tags right
   case right => apply b
