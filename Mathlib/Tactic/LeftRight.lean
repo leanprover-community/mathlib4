d49c0d8555d660175de52999d3a1d25c6c57fa17
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2022-04-08 20:29:00 +0530

    avoided code duplication

--- Mathlib/Tactic/LeftRight.lean
+++ Mathlib/Tactic/LeftRight.lean
@@ -2,10 +2,10 @@ import Lean
 import Lean.Meta
 import Lean.Elab
 
-namespace Mathlib.Tactic.Left
+namespace Mathlib.Tactic.LeftRight
 open Lean Meta Elab
 open Tactic
-def leftMeta (mvarId : MVarId) : MetaM (List MVarId) := do
+def leftRightMeta (pickLeft: Bool)(mvarId : MVarId) : MetaM (List MVarId) := do
   withMVarContext mvarId do
     checkNotAssigned mvarId `left
     let target ← getMVarType' mvarId
@@ -14,38 +14,27 @@ def leftMeta (mvarId : MVarId) : MetaM (List MVarId) := do
       fun ival us => do
         unless ival.ctors.length == 2 do
           throwTacticEx `constructor mvarId "left target applies for inductive types with exactly two constructors"
-        let ctor := ival.ctors.toArray[0]
+        let ctor :=
+          if pickLeft then ival.ctors.toArray[0]
+          else ival.ctors.toArray[1]
         try
           return ← apply mvarId (Lean.mkConst ctor us)
         catch _ =>
           pure ()
         throwTacticEx `constructor mvarId "no applicable constructor found"
+
+end LeftRight
+
+open LeftRight
+namespace Mathlib.Tactic.Left
 
 elab "left" : tactic => do
-  Lean.Elab.Tactic.liftMetaTactic <| fun mvarId => leftMeta mvarId
+  Lean.Elab.Tactic.liftMetaTactic <| fun mvarId => leftRightMeta true mvarId
 
 end Left
-
 namespace Mathlib.Tactic.Right
-open Lean Meta Elab
-open Tactic
-def rightMeta (mvarId : MVarId) : MetaM (List MVarId) := do
-  withMVarContext mvarId do
-    checkNotAssigned mvarId `left
-    let target ← getMVarType' mvarId
-    matchConstInduct target.getAppFn
-      (fun _ => throwTacticEx `constructor mvarId "target is not an inductive datatype")
-      fun ival us => do
-        unless ival.ctors.length == 2 do
-          throwTacticEx `constructor mvarId "left target applies for inductive types with exactly two constructors"
-        let ctor := ival.ctors.toArray[1]
-        try
-          return ← apply mvarId (Lean.mkConst ctor us)
-        catch _ =>
-          pure ()
-        throwTacticEx `constructor mvarId "no applicable constructor found"
 
 elab "right" : tactic => do
-  Lean.Elab.Tactic.liftMetaTactic <| fun mvarId => rightMeta mvarId
+  Lean.Elab.Tactic.liftMetaTactic <| fun mvarId => leftRightMeta false mvarId
 
 end Right
