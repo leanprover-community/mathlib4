59e76ca5a53ac014445f97cb4dc735582dd7210f
E.W.Ayers <edward.ayers@outlook.com>
2022-04-26 17:19:31 -0400

    remove dsimp from wishlist

--- Mathlib/Mathport/Syntax.lean
+++ Mathlib/Mathport/Syntax.lean
@@ -189,8 +189,6 @@ syntax caseArg := binderIdent,+ (" :" (ppSpace (ident <|> "_"))+)?
   (" [" simpArg,* "]")? (" with " (colGt ident)+)? (ppSpace location)? : tactic
 /- N -/ syntax (name := simpIntro) "simp_intro" (config)?
   (ppSpace colGt (ident <|> "_"))* (&" only")? (" [" simpArg,* "]")? (" with " ident+)? : tactic
-/- N -/ syntax (name := dsimp) "dsimp" (config)? (&" only")?
-  (" [" simpArg,* "]")? (" with " (colGt ident)+)? (ppSpace location)? : tactic
 /- E -/ syntax (name := symm) "symm" : tactic
 /- E -/ syntax (name := trans) "trans" (ppSpace colGt term)? : tactic
 /- B -/ syntax (name := acRfl) "ac_rfl" : tactic
