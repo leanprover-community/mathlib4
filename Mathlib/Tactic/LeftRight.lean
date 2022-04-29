7d454e12825c489586bea1a91f44111631143744
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2022-04-29 13:49:27 +0530

    Improved following @gebner's suggestions

--- Mathlib/Tactic/LeftRight.lean
+++ Mathlib/Tactic/LeftRight.lean
@@ -1,40 +1,32 @@
+/-
+Copyright (c) 2022 Siddhartha Gadgil. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Author: Siddhartha Gadgil
+-/
 import Lean
-import Lean.Meta
-import Lean.Elab
 
 namespace Mathlib.Tactic.LeftRight
 open Lean Meta Elab
 open Tactic
-def leftRightMeta (pickLeft: Bool)(mvarId : MVarId) : MetaM (List MVarId) := do
+def leftRightMeta (pickLeft: Bool) (mvarId : MVarId) : MetaM (List MVarId) := do
   withMVarContext mvarId do
-    checkNotAssigned mvarId `left
+    let name := if pickLeft then `left else `right
+    checkNotAssigned mvarId name
     let target ← getMVarType' mvarId
     matchConstInduct target.getAppFn
       (fun _ => throwTacticEx `constructor mvarId "target is not an inductive datatype")
       fun ival us => do
         unless ival.ctors.length == 2 do
-          throwTacticEx `constructor mvarId "left target applies for inductive types with exactly two constructors"
-        let ctor :=
-          if pickLeft then ival.ctors.toArray[0]
-          else ival.ctors.toArray[1]
-        try
-          return ← apply mvarId (Lean.mkConst ctor us)
-        catch _ =>
-          pure ()
-        throwTacticEx `constructor mvarId "no applicable constructor found"
+          throwTacticEx `constructor mvarId s!"{name} target applies for inductive types with exactly two constructors"
+        let ctor := ival.ctors.get! (if pickLeft then 0 else 1)
+        apply mvarId (Lean.mkConst ctor us)
 
 end LeftRight
 
 open LeftRight
-namespace Mathlib.Tactic.Left
 
 elab "left" : tactic => do
-  Lean.Elab.Tactic.liftMetaTactic <| fun mvarId => leftRightMeta true mvarId
-
-end Left
-namespace Mathlib.Tactic.Right
+  Lean.Elab.Tactic.liftMetaTactic (leftRightMeta true)
 
 elab "right" : tactic => do
-  Lean.Elab.Tactic.liftMetaTactic <| fun mvarId => leftRightMeta false mvarId
-
-end Right
+  Lean.Elab.Tactic.liftMetaTactic (leftRightMeta false)
