20064709eb3c6131b71601709b5529b14d4d568a
E.W.Ayers <edward.ayers@outlook.com>
2022-06-02 14:48:42 -0400

    fix: unbundle WriterT

--- Mathlib/Control/Writer.lean
+++ Mathlib/Control/Writer.lean
@@ -6,8 +6,8 @@ Authors: Simon Hudon, E.W.Ayers
 The writer monad transformer for passing immutable state.
 -/
 
-structure WriterT (ω : Type u) (M : Type u → Type v) (α : Type u) : Type (max u v) where
-  run : M (α × ω)
+def WriterT (ω : Type u) (M : Type u → Type v) (α : Type u) : Type v :=
+  M (α × ω)
 
 @[reducible]
 def Writer ω := WriterT ω Id
@@ -21,29 +21,44 @@ export MonadWriter (tell listen pass)
 
 namespace WriterT
 
-protected def runThe (ω : Type u) {M : Type u → Type v} (cmd : WriterT ω M α) : M (α × ω) := WriterT.run cmd
+protected def mk {ω : Type u} (cmd :  M (α × ω)) : WriterT ω M α:= cmd
+protected def run {ω : Type u} (cmd : WriterT ω M α) : M (α × ω) := cmd
+protected def runThe (ω : Type u) (cmd : WriterT ω M α) : M (α × ω) := cmd
 
-variable {ω : Type u} {M : Type u → Type v} [Monad M] {α β : Type u}
+variable {ω : Type u} {α β : Type u}
 
+/-- Creates an instance of Monad, with an explicitly given empty and append operation.
+
+Previously, this would have used an instance of `[Monoid ω]` as input.
+In practice, however, WriterT is used for logging and creating lists so restricting to
+monoids with `Mul` and `One` can make `WriterT` cumbersome to use.
+
+This is used to derive instances for both `[EmptyCollection ω] [Append ω]` and `[Monoid ω]`.
+-/
 def monad (empty : ω) (append : ω → ω → ω) : Monad (WriterT ω M) where
-  map := fun f ⟨cmd⟩ => ⟨(fun (a,w) => (f a, w)) <$> cmd⟩
-  pure := fun a => ⟨pure (a, empty)⟩
-  bind := fun ⟨cmd⟩ f => ⟨cmd >>= fun (a, w₁) => (fun (b, w₂) => (b, append w₁ w₂)) <$> (f a).run⟩
+  map := fun f (cmd : M _) => WriterT.mk $ (fun (a,w) => (f a, w)) <$> cmd
+  pure := fun a => pure (f := M) (a, empty)
+  bind := fun (cmd : M _) f =>
+    WriterT.mk $ cmd >>= fun (a, w₁) =>
+      (fun (b, w₂) => (b, append w₁ w₂)) <$> (f a)
 
-protected def liftTell (item : ω) : MonadLift M (WriterT ω M) where
-  monadLift := fun cmd => ⟨(fun a => (a, item)) <$> cmd⟩
+/-- Lift an `M` to a `WriterT ω M`, using the given `empty` as the monoid unit. -/
+protected def liftTell (empty : ω) : MonadLift M (WriterT ω M) where
+  monadLift := fun cmd => WriterT.mk $ (fun a => (a, empty)) <$> cmd
 
 instance [EmptyCollection ω] [Append ω] : Monad (WriterT ω M) := monad ∅ (· ++ ·)
--- instance [Monoid ω] : Monad (WriterT ω M) := monad 1 (· * ·)
 instance [EmptyCollection ω] : MonadLift M (WriterT ω M) := WriterT.liftTell ∅
+instance [Monoid ω] : Monad (WriterT ω M) := monad 1 (· * ·)
+instance [Monoid ω] : MonadLift M (WriterT ω M) := WriterT.liftTell 1
 
 instance : MonadWriter ω (WriterT ω M) where
-  tell := fun w => ⟨pure (⟨⟩, w)⟩
-  listen := fun ⟨cmd⟩ => ⟨(fun (a,w) => ((a,w), w)) <$> cmd⟩
-  pass := fun ⟨cmd⟩ => ⟨(fun ((a,f), w) => (a, f w)) <$> cmd⟩
+  tell := fun w => WriterT.mk $ pure (⟨⟩, w)
+  listen := fun cmd => WriterT.mk $ (fun (a,w) => ((a,w), w)) <$> cmd
+  pass := fun cmd => WriterT.mk $ (fun ((a,f), w) => (a, f w)) <$> cmd
 
 instance [MonadExcept ε M] : MonadExcept ε (WriterT ω M) where
-  throw := fun e => ⟨throw e⟩
-  tryCatch := fun ⟨cmd⟩ c => ⟨tryCatch cmd fun e => (c e).run⟩
+  throw := fun e => WriterT.mk $ throw e
+  tryCatch := fun cmd c => WriterT.mk $ tryCatch cmd fun e => (c e).run
+
 
 end WriterT
