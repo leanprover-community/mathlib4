5c951924850fb1fb3d8a656129d9637c401cc6b4
Gabriel Ebner <gebner@gebner.org>
2022-06-13 18:50:54 +0000

    perf: compile runLinter.lean (#285)
    This gets us from 1630ms to 635ms runtime.

--- lakefile.lean
+++ lakefile.lean
@@ -6,3 +6,7 @@ package mathlib
 
 @[defaultTarget]
 lean_lib Mathlib
+
+lean_exe runLinter where
+  root := `scripts.runLinter
+  supportInterpreter := true
--- scripts/runLinter.lean
+++ scripts/runLinter.lean
@@ -1,40 +1,60 @@
-import Mathlib
+import Mathlib.Tactic.Lint
+import Mathlib.Data.Array.Defs
 
-open Lean in
-private abbrev NoLints := Array (Name × Name)
+open Lean Core Elab Command Mathlib.Tactic.Lint
 
-open Lean in
-private def readJsonFile (α) [FromJson α] (path : System.FilePath) : IO α := do
-  let inst : MonadExceptOf String IO := ⟨throw ∘ IO.userError, fun x _ => x⟩
+abbrev NoLints := Array (Name × Name)
+
+def readJsonFile (α) [FromJson α] (path : System.FilePath) : IO α := do
+  let _ : MonadExceptOf String IO := ⟨throw ∘ IO.userError, fun x _ => x⟩
   liftExcept <| fromJson? <|<- liftExcept <| Json.parse <|<- IO.FS.readFile path
 
-open Lean in
-private def writeJsonFile (α) [ToJson α] (path : System.FilePath) (a : α) : IO Unit :=
+def writeJsonFile (α) [ToJson α] (path : System.FilePath) (a : α) : IO Unit :=
   IO.FS.writeFile path <| toJson a |>.pretty
 
-local elab "#runLinters" update:(&"update")? : command =>
-  open Lean Elab Command Mathlib.Tactic.Lint in do
+open System in
+instance : ToExpr FilePath where
+  toTypeExpr := mkConst ``FilePath
+  toExpr path := mkApp (mkConst ``FilePath.mk) (toExpr path.1)
+
+elab "compileTimeSearchPath" : term =>
+  return toExpr (← searchPathRef.get)
+
+unsafe def main (args : List String) : IO Unit := do
+  let (update, args) :=
+    match args with
+    | "--update" :: args => (true, args)
+    | _ => (false, args)
+  let some module :=
+      match args with
+      | [] => some `Mathlib
+      | [mod] => Syntax.decodeNameLit s!"`{mod}"
+      | _ => none
+    | IO.eprintln "Usage: runLinter [--update] [Mathlib.Data.Nat]" *> IO.Process.exit 1
   let nolintsFile := "scripts/nolints.json"
   let nolints ← readJsonFile NoLints nolintsFile
-  let decls ← liftCoreM getDeclsInMathlib
-  let linters ← liftCoreM (getChecks (slow := true) (extra := []) (useOnly := false))
-  let results ← liftCoreM (lintCore decls linters.toArray)
-  if update.isSome then
-    writeJsonFile NoLints nolintsFile <|
-      .qsort (lt := fun (a,b) (c,d) => a.lt c || (a == c && b.lt d)) <|
-      .flatten <| results.map fun (linter, decls) =>
-      decls.fold (fun res decl _ => res.push (linter.name, decl)) #[]
-  let results := results.map fun (linter, decls) =>
-    .mk linter <| nolints.foldl (init := decls) fun decls (linter', decl') =>
-      if linter.name == linter' then decls.erase decl' else decls
-  let failed := results.any (!·.2.isEmpty)
-  if failed then
-    let fmtResults ← liftCoreM do
-      formatLinterResults results decls (groupByFilename := true)
-        "in mathlib" (runSlowLinters := true) .medium linters.length
-    logError fmtResults
-  else
-    logInfo "-- Linting passed."
-
-#runLinters
--- update
+  searchPathRef.set compileTimeSearchPath
+  withImportModules [{module}] {} (trustLevel := 1024) fun env =>
+    let ctx := {fileName := "", fileMap := default}
+    let state := {env}
+    Prod.fst <$> (CoreM.toIO · ctx state) do
+      let decls ← getDeclsInMathlib
+      let linters ← getChecks (slow := true) (extra := []) (useOnly := false)
+      let results ← lintCore decls linters.toArray
+      if update then
+        writeJsonFile NoLints nolintsFile <|
+          .qsort (lt := fun (a,b) (c,d) => a.lt c || (a == c && b.lt d)) <|
+          .flatten <| results.map fun (linter, decls) =>
+          decls.fold (fun res decl _ => res.push (linter.name, decl)) #[]
+      let results := results.map fun (linter, decls) =>
+        .mk linter <| nolints.foldl (init := decls) fun decls (linter', decl') =>
+          if linter.name == linter' then decls.erase decl' else decls
+      let failed := results.any (!·.2.isEmpty)
+      if failed then
+        let fmtResults ←
+          formatLinterResults results decls (groupByFilename := true)
+            "in mathlib" (runSlowLinters := true) .medium linters.length
+        IO.print (← fmtResults.toString)
+        IO.Process.exit 1
+      else
+        IO.print "-- Linting passed."
