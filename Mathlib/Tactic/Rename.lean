2121105c420e079efc97594a16b9ea759512f6f3
Yury G. Kudryashov <urkud@urkud.name>
2022-07-18 14:32:36 -0400

    docs(Tactic/Rename): improve the docstring

--- Mathlib/Tactic/Rename.lean
+++ Mathlib/Tactic/Rename.lean
@@ -11,7 +11,9 @@ open Lean Elab.Tactic Meta
 
 syntax renameArg := term " => " ident
 
-/-- `rename' h => hnew` renames the hypothesis named `h` to `hnew`. -/
+/-- `rename' h => hnew` renames the hypothesis named `h` to `hnew`.
+To rename several hypothesis, use `rename' h₁ => h₁new, h₂ => h₂new`.
+You can use `rename' a => b, b => a` to swap two variables. -/
 syntax (name := rename') "rename'" (ppSpace renameArg),* : tactic
 
 elab_rules : tactic
