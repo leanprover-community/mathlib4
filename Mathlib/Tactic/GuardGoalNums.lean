794d34c4d0fbb113d6691b9f0df5047ad9b42f8c
Rob Lewis <rob.y.lewis@gmail.com>
2022-07-18 15:24:14 -0400

    camelCase

--- Mathlib/Tactic/GuardGoalNums.lean
+++ Mathlib/Tactic/GuardGoalNums.lean
@@ -4,6 +4,6 @@ open Lean Meta Elab Tactic
 
 /-- `guard_goal_nums n` succeeds if there are exactly `n` goals and fails otherwise. -/
 elab (name := guardGoalNums) "guard_goal_nums " n:num : tactic => do
-  let num_goals := (← getGoals).length
-  guard (num_goals = n.getNat) <|>
-    throwError "expected {n.getNat} goals but found {num_goals}"
+  let numGoals := (← getGoals).length
+  guard (numGoals = n.getNat) <|>
+    throwError "expected {n.getNat} goals but found {numGoals}"
