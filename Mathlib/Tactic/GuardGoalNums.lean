fdb155f7a5fed9b8a7a68f8bb8f2e14f3267903b
Rob Lewis <rob.y.lewis@gmail.com>
2022-07-18 15:24:53 -0400

    copyright header

--- Mathlib/Tactic/GuardGoalNums.lean
+++ Mathlib/Tactic/GuardGoalNums.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2022 Robert Y. Lewis. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Robert Y. Lewis
+-/
+
 import Lean
 
 open Lean Meta Elab Tactic
