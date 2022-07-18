fbc5f2910c240e0236ee3b9db1f39bc503f1f2dc
Rob Lewis <rob.y.lewis@gmail.com>
2022-07-18 15:23:49 -0400

    rename to what this tactic actually is

--- Mathlib/Tactic/GuardGoalNums.lean
+++ Mathlib/Tactic/GuardGoalNums.lean
@@ -2,7 +2,8 @@ import Lean
 
 open Lean Meta Elab Tactic
 
-elab (name := guardHypNums) "guard_hyp_nums " n:num : tactic => do
+/-- `guard_goal_nums n` succeeds if there are exactly `n` goals and fails otherwise. -/
+elab (name := guardGoalNums) "guard_goal_nums " n:num : tactic => do
   let num_goals := (← getGoals).length
   guard (num_goals = n.getNat) <|>
     throwError "expected {n.getNat} goals but found {num_goals}"
--- test/GuardGoalNums.lean
+++ test/GuardGoalNums.lean
@@ -2,10 +2,10 @@ import Mathlib.Tactic.GuardHypNums
 
 example : true ∧ true := by
   constructor
-  guard_hyp_nums 2
+  guard_goal_nums 2
   all_goals {constructor}
 
 example : (true ∧ true) ∧ (true ∧ true) := by
   constructor <;> constructor
-  guard_hyp_nums 4
+  guard_goal_nums 4
   all_goals {constructor}
