58b000a833db746830c1fe58a4e790662e5a0738
Arthur Paulino <arthurleonardo.ap@gmail.com>
2022-07-19 09:06:44 -0300

    adding helper functions and simplifying 'swap_var' code

--- Mathlib/Tactic/SwapVar.lean
+++ Mathlib/Tactic/SwapVar.lean
@@ -4,11 +4,9 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Arthur Paulino
 -/
 
-import Lean
+import Mathlib.Tactic.Utils
 
 namespace Mathlib.Tactic
-
-open Lean Meta Elab.Tactic
 
 /--
 `swap_var swap_rule₁, swap_rule₂, ⋯` applies `swap_rule₁` then `swap_rule₂` then `⋯`.
@@ -17,31 +15,14 @@ A *swap_rule* is of the form `x y` or `x ↔ y`, and "applying it" means swappin
 `x` by `y` and vice-versa on all hypotheses and the goal.
 
 ```lean
-example {a b : Nat} (h : a = b) : a = b ∧ a = a := by
-  swap_var a ↔ b
--- b a: Nat
--- h: b = a
--- ⊢ b = a ∧ b = b
-  swap_var b y, y ↔ x
--- x a: Nat
--- h: x = a
--- ⊢ x = a ∧ x = x
-  exact .intro h $ .refl x
+example {P Q : Prop} (q : P) (p : Q) : P ∧ Q := by
+  swap_var p ↔ q
+  exact ⟨p, q⟩
 ```
 -/
 elab "swap_var " vars:(colGt (ident " ↔ "? ident)),+ : tactic =>
-  liftMetaTactic fun mvarId => do
-    let mut ctx ← getLCtx
-    for stx in vars.getElems do
-      let getDecl (stx : Syntax) :=
-        match ctx.findFromUserName? stx.getId with
-        | some decl => pure (stx.getId, decl.fvarId)
-        | none => throwErrorAt stx "swap_var: unknown variable {stx.getId}"
-      let (n1, fvar1) ← getDecl stx.raw[0]
-      let (n2, fvar2) ← getDecl stx.raw[2]
-      ctx := ctx.setUserName fvar1 n2
-      ctx := ctx.setUserName fvar2 n1
-    let mvarIdNew ← mkFreshExprMVarAt ctx (← getLocalInstances) (← getMVarType mvarId)
-      .syntheticOpaque (← getMVarTag mvarId)
-    assignExprMVar mvarId mvarIdNew
-    return [mvarIdNew.mvarId!]
+  modifyLocalContext fun ctx =>
+    vars.getElems.foldlM (init := ctx) fun ctx stx => do
+      let (n1, fvar1) ← getNameAndFVarId ctx stx.raw[0]
+      let (n2, fvar2) ← getNameAndFVarId ctx stx.raw[2]
+      pure $ ctx.setUserName fvar1 n2 |>.setUserName fvar2 n1
--- test/SwapVar.lean
+++ test/SwapVar.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Arthur Paulino
 -/
 
+import Mathlib.Tactic.Basic
 import Mathlib.Tactic.SwapVar
 
 example {P Q : Prop} (q : P) (p : Q) : P ∧ Q := by
