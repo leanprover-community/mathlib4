284758dd9b7ae957649367c9fb0cdc684dffc3de
Mario Carneiro <di.gama@gmail.com>
2022-07-18 23:29:51 -0400

    use logInfoAt

--- Mathlib/Tactic/Trace.lean
+++ Mathlib/Tactic/Trace.lean
@@ -1,7 +1,6 @@
 import Mathlib.Util.TermUnsafe
 
 open Lean Meta Elab Tactic
-elab (name := Lean.Parser.Tactic.trace) tk:"trace " val:term : tactic =>
-  withRef tk do
-    let e ← elabTerm (← `(toString $val)) (some (mkConst `String))
-    logInfo <|← unsafe evalExpr String (mkConst `String) e
+elab (name := Lean.Parser.Tactic.trace) tk:"trace " val:term : tactic => do
+  let e ← elabTerm (← `(toString $val)) (some (mkConst `String))
+  logInfoAt tk <|← unsafe evalExpr String (mkConst `String) e
