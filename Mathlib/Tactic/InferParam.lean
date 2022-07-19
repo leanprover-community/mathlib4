2a85e51fbe5b155504877a46d4babb585fae706f
Yury G. Kudryashov <urkud@urkud.name>
2022-07-19 13:30:54 -0400

    Update Mathlib/Tactic/InferParam.lean
    Co-authored-by: Mario Carneiro <di.gama@gmail.com>

--- Mathlib/Tactic/InferParam.lean
+++ Mathlib/Tactic/InferParam.lean
@@ -22,10 +22,10 @@ namespace Mathlib.Tactic
 open Lean Elab Tactic Meta
 
 /-- Close a goal of the form `optParam α a` by using `a`. -/
-elab (name := inferOptParam) "infer_opt_param" : tactic => do
-  withMainContext do
-    let tgt_expr ← getMainTarget
-    let goal ← getMainGoal
+elab (name := inferOptParam) "infer_opt_param" : tactic =>
+  liftMetaTactic fun goal => do
+    let tgt_expr ← getMVarType goal
     match tgt_expr.getAppFnArgs with
-    | (``optParam, #[_ty, val]) => assignExprMVar goal val; replaceMainGoal []
-    | _ => throwError "`infer_opt_param` only solves goals of the form `optParam _ _`, not {tgt_expr}"
+    | (``optParam, #[_ty, val]) => assignExprMVar goal val; pure []
+    | _ => throwError
+      "`infer_opt_param` only solves goals of the form `optParam _ _`, not {tgt_expr}"
