1d941da5e79a3c83af79e76942fd97c517123e00
Jireh Loreaux <loreaujy@gmail.com>
2022-07-18 22:57:36 -0500

    Update test/TypeCheck.lean
    Co-authored-by: Mario Carneiro <di.gama@gmail.com>

--- test/TypeCheck.lean
+++ test/TypeCheck.lean
@@ -1,11 +1,11 @@
 import Mathlib.Tactic.TypeCheck
 
-example : True := by {
-  type_check Nat; -- Type
-  type_check Bool.true; -- Bool
-  type_check nat_lit 1; -- Nat
-  type_check (1 : Nat); -- Nat
-  type_check (True : _); -- Prop
-  type_check ∀ x y : Nat, x = y; -- Prop
-  type_check fun x : Nat => 2 * x + 1; -- Nat -> Nat
-  exact True.intro }
+example : True := by
+  type_check Nat -- Type
+  type_check Bool.true -- Bool
+  type_check nat_lit 1 -- Nat
+  type_check (1 : Nat) -- Nat
+  type_check (True : _) -- Prop
+  type_check ∀ x y : Nat, x = y -- Prop
+  type_check fun x : Nat => 2 * x + 1 -- Nat -> Nat
+  trivial
