50abcc7e3f385590c7d5f38074bc0f95dc239c9d
Mario Carneiro <di.gama@gmail.com>
2022-07-18 22:10:03 -0700

    Update Mathlib/Tactic/Trace.lean
    Co-authored-by: Scott Morrison <scott@tqft.net>

--- Mathlib/Tactic/Trace.lean
+++ Mathlib/Tactic/Trace.lean
@@ -1,6 +1,8 @@
 import Mathlib.Util.TermUnsafe
 
 open Lean Meta Elab Tactic
+
+/-- Evaluates a term to a string (when possible), and prints it as a trace message. -/
 elab (name := Lean.Parser.Tactic.trace) tk:"trace " val:term : tactic => do
   let e ← elabTerm (← `(toString $val)) (some (mkConst `String))
   logInfoAt tk <|← unsafe evalExpr String (mkConst `String) e
