7630145f131740b1ac0cb1ab6bf44c5be1480d67
Arthur Paulino <arthurleonardo.ap@gmail.com>
2022-07-19 09:09:14 -0300

    moving 'Tactic.Utils' to 'Util.Tactic'

--- Mathlib/Tactic/SwapVar.lean
+++ Mathlib/Tactic/SwapVar.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Arthur Paulino
 -/
 
-import Mathlib.Tactic.Utils
+import Mathlib.Util.Tactic
 
 namespace Mathlib.Tactic
 
