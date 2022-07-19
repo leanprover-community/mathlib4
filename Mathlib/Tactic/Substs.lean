85d33aae4f847827a12411622fd5265d4970b208
evanlohn <evan.lohn@berkeley.edu>
2022-07-19 11:50:48 -0400

    added file header

--- Mathlib/Tactic/Substs.lean
+++ Mathlib/Tactic/Substs.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2022 Evan Lohn. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Author: Evan Lohn, Mario Carneiro
+-/
 import Lean
 
 namespace Mathlib.Tactic.Substs
@@ -7,5 +12,4 @@ open Tactic
 syntax (name := substs) "substs" (colGt ppSpace ident)* : tactic
 
 macro_rules
-| `(tactic| substs $xs:ident*) =>
-  `(tactic| ($[subst $xs;]*))
+| `(tactic| substs $xs:ident*) => `(tactic| ($[subst $xs;]*))
