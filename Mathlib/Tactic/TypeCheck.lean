338a5200d2c3ccaaff2bae9b1d6b94618bf6d0f4
Jireh Loreaux <loreaujy@gmail.com>
2022-07-18 20:00:48 -0500

    add docstring

--- Mathlib/Tactic/TypeCheck.lean
+++ Mathlib/Tactic/TypeCheck.lean
@@ -2,6 +2,7 @@ import Lean.Elab.Tactic.Basic
 
 syntax (name := typeCheck) "type_check " term : tactic
 
+/-- Type check the given expression, and trace its type. -/
 elab tk:"type_check " e:term : tactic => do
   let e ← Lean.Elab.Term.elabTerm e none
   let type ← Lean.Meta.inferType e
