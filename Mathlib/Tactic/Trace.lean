a47c51f57db7c829f42b9f11fcb8d480dde5950b
Mario Carneiro <di.gama@gmail.com>
2022-07-19 01:13:17 -0400

    chore: add header

--- Mathlib/Tactic/Trace.lean
+++ Mathlib/Tactic/Trace.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2022 Mario Carneiro. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Author: Mario Carneiro
+-/
 import Mathlib.Util.TermUnsafe
 
 open Lean Meta Elab Tactic
