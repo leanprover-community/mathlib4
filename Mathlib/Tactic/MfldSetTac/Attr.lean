ab36e3b5cd98325bb1245bd280ec70e90d3f4a39
Frédéric Dupuis <dupuisf@iro.umontreal.ca>
2022-07-19 17:52:58 -0400

    linter

--- Mathlib/Tactic/MfldSetTac/Attr.lean
+++ Mathlib/Tactic/MfldSetTac/Attr.lean
@@ -16,6 +16,16 @@ enough.
 "
 
 namespace Attr
+/-- The simpset `mfld_simps` records several simp lemmas that are
+especially useful in manifolds. It is a subset of the whole set of simp lemmas, but it makes it
+possible to have quicker proofs (when used with `squeeze_simp` or `simp only`) while retaining
+readability.
+
+The typical use case is the following, in a file on manifolds:
+If `simp [foo, bar]` is slow, replace it with `squeeze_simp [foo, bar, mfld_simps]` and paste
+its output. The list of lemmas should be reasonable (contrary to the output of
+`squeeze_simp [foo, bar]` which might contain tens of lemmas), and the outcome should be quick
+enough. -/
 syntax (name := mfldSetTacAttr) "mfld_simps" : attr
 end Attr
 
