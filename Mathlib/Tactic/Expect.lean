b10ea9eee7744df404a3efc21fb26a056bf699d6
Arthur Paulino <arthurleonardo.ap@gmail.com>
2022-07-20 08:30:56 -0300

    improve docstring for 'expect_failure'

--- Mathlib/Tactic/Expect.lean
+++ Mathlib/Tactic/Expect.lean
@@ -10,7 +10,13 @@ namespace Mathlib.Tactic
 
 open Lean Meta Elab Tactic
 
-/-- `expect_failure tacs` succeeds iff `tacs` fails. -/
+/--
+`expect_failure tacs` succeeds iff `tacs` fails.
+
+`expect_failure` is similar to `success_if_failure` from core, except that `success_if_failure`
+omits `withoutRecover`. When this is fixed, `expect_failure` can be deprecated or turned into a
+macro.
+-/
 elab "expect_failure " tacs:tacticSeq : tactic => do
   if ← try withoutRecover (evalTactic tacs); pure true catch _ => pure false then
     throwErrorAt tacs "tactic sequence succeeded"
