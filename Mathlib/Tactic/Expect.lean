47932632483b8ad2a56293dcf25232417b3b77b3
Arthur Paulino <arthurleonardo.ap@gmail.com>
2022-07-20 08:18:28 -0300

    dropping 'expect_hyps' and 'expect_goals' because their 'guard_' sibilings are already at least as powerful in terms of flexibility

--- Mathlib/Tactic/Expect.lean
+++ Mathlib/Tactic/Expect.lean
@@ -13,41 +13,14 @@ open Lean Meta Elab Tactic
 /-- `expect_failure tacs` succeeds iff `tacs` fails. -/
 elab "expect_failure " tacs:tacticSeq : tactic => do
   if ← try withoutRecover (evalTactic tacs); pure true catch _ => pure false then
-    throwErrorAt tacs "tactic succeeded"
+    throwErrorAt tacs "tactic sequence succeeded"
 
 /-- `expect_failure_msg msg tacs` succeeds iff `tacs` fails with `msg` as the error message. -/
 elab "expect_failure_msg " msg:str tacs:tacticSeq : tactic => do
   match ← try withoutRecover (evalTactic tacs); pure none catch e => pure (some e) with
-  | none => throwErrorAt tacs "tactic succeeded"
+  | none => throwErrorAt tacs "tactic sequence succeeded"
   | some e =>
     let expectedMsg := msg.getString
     let eMsg ← e.toMessageData.toString
     if eMsg != expectedMsg then
       throwErrorAt msg "expected failure message \"{expectedMsg}\" but got \"{eMsg}\""
-
-/-- `expect_goals t₁, t₂, ⋯` succeeds iff the list of goals start with types `t₁`, `t₂` ... -/
-elab "expect_goals " types:(colGt term),+ : tactic => do
-  let typesList := types.getElems.data
-  let goalsList ← getUnsolvedGoals
-  if typesList.length > goalsList.length then
-    throwError "too many expected goals"
-  typesList.zip goalsList |>.forM fun (type, mvarId) => do
-    let goalType ← instantiateMVars (← getMVarDecl mvarId).type
-    let expectedType ← elabTerm type none
-    if goalType != expectedType then
-      throwErrorAt type s!"expected '{expectedType}' but got '{goalType}'"
-
-/-- The parsing rule for defining hypotheses expectations. -/
-syntax hypRule := ident " : " term
-
-/-- `expect_hyps h₁ : t₁, h₂ : t₂, ⋯` succeeds iff every `hᵢ` in the context has type `tᵢ`. -/
-elab "expect_hyps " rules:(hypRule),+ : tactic => withMainContext do
-  rules.getElems.forM fun rule => do
-    let `(hypRule| $hyp:ident : $type:term) := rule | unreachable!
-    match (← getLCtx).findFromUserName? hyp.getId with
-      | some decl => do
-        let goalType ← instantiateMVars decl.type
-        let expectedType ← elabTerm type none
-        if goalType != expectedType then
-        throwErrorAt type s!"expected '{expectedType}' but got '{goalType}'"
-      | none => throwErrorAt hyp "unknown identifier '{hyp}'"
--- test/Expect.lean
+++ test/Expect.lean
@@ -6,31 +6,8 @@ Authors: Arthur Paulino
 
 import Mathlib.Tactic.Expect
 
-example : True ∧ True := by
-  expect_goals True ∧ True
-  constructor
-  expect_goals True, True
-  expect_failure_msg "too many expected goals" expect_goals True, True, Foo
-  expect_failure expect_goals True ∧ True
-  expect_goals True
-  repeat simp
-
 example (p : P) : P := by
   expect_failure_msg "unknown identifier 'x'" have h := x
   expect_failure expect_failure_msg "unknown identifier 'y'" have h := x
   exact p
-
-example (h : a = b) : a = b ∧ b = a := by
-  constructor
-  expect_goals a = b
-  have _five := 5
-  expect_hyps _five : Nat, h : a = b
-  expect_failure_msg "expected 'String' but got 'Nat'" expect_hyps _five : String
-  expect_failure_msg "unknown identifier 'six'" expect_hyps six : Nat
-  expect_hyps h : a = b
-  exact h
-  expect_goals b = a
-  have _six := 6
-  expect_hyps _six : Nat, h : a = b
-  expect_failure_msg "unknown identifier 'five'" expect_hyps five : Nat
-  simp [h]
+  expect_failure_msg "no goals to be solved" simp
