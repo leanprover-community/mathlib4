42692eb6de9c666372b8cba3d077f4f8f0a2a34d
Frédéric Dupuis <dupuisf@iro.umontreal.ca>
2022-07-20 15:58:20 -0400

    add version of replace without `:=`

--- Mathlib/Tactic/Replace.lean
+++ Mathlib/Tactic/Replace.lean
@@ -4,12 +4,14 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Arthur Paulino, Mario Carneiro
 -/
 import Lean
+import Mathlib.Tactic.Have
 
 namespace Mathlib.Tactic
 
 open Lean Elab.Tactic
 
 syntax "replace " haveDecl : tactic
+syntax (name := replace') "replace " Parser.Term.haveIdLhs' : tactic
 
 /--
 Acts like `have`, but removes a hypothesis with the same name as
@@ -38,6 +40,18 @@ h : β
 ⊢ goal
 ```
 
+Likewise, after `replace h : β`, the state will be:
+```lean
+case h
+f : α → β
+h : α
+⊢ β
+
+f: α → β
+h: β
+⊢ goal
+```
+
 This can be used to simulate the `specialize` and `apply at` tactics of Coq.
 -/
 elab_rules : tactic
@@ -53,3 +67,16 @@ elab_rules : tactic
         try replaceMainGoal [← Meta.clear (← getMainGoal) hId]
         catch | _ => pure ()
       | none     => pure ()
+
+elab_rules : tactic
+| `(tactic| replace $[$n:ident $bs*]? $[: $t:term]?) => withMainContext do
+    let (mvar1, mvar2) ← haveLetCore (← getMainGoal) n (bs.getD #[]) t false
+    let name : Name := match n with
+    | none   => `this
+    | some n => n.getId
+    let hId? := (← getLCtx).findFromUserName? name |>.map fun d => d.fvarId
+    match hId? with
+    | some hId =>
+      try replaceMainGoal [mvar1, ← Meta.clear mvar2 hId]
+      catch | _ => pure ()
+    | none     => replaceMainGoal [mvar1, mvar2]
