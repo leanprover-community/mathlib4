e45a4d6d08bbb1781b9c36989d6329da7889150a
Arthur Paulino <arthurleonardo.ap@gmail.com>
2022-07-21 09:48:50 -0300

    enhance 'Mathlib.Util.Tactic' API

--- Mathlib/Util/Tactic.lean
+++ Mathlib/Util/Tactic.lean
@@ -22,29 +22,41 @@ def getNameAndFVarId [Monad m] [MonadError m] (ctx : LocalContext) (stx : Syntax
   else
     throwErrorAt stx "not an identifier"
 
-/-- Updates the current `LocalContext` with a map function in `MetaM`. -/
-def modifyLocalContext (fLCtx : LocalContext → MetaM LocalContext) : TacticM Unit :=
+/-- Updates the current goal with a `Expr` map function in `MetaM`. -/
+def modifyGoal (f : Expr → MetaM Expr) : TacticM Unit :=
   liftMetaTactic fun mvarId => do
-    let newGoal ← mkFreshExprMVarAt (← fLCtx (← getLCtx)) (← getLocalInstances)
+    let newGoal ← mkFreshExprMVarAt (← getLCtx) (← getLocalInstances)
+      (← f (← getMVarType mvarId)) .syntheticOpaque (← getMVarTag mvarId)
+    assignExprMVar mvarId newGoal
+    return [newGoal.mvarId!]
+
+/-- Updates the current `LocalContext` with a map function in `MetaM`. -/
+def modifyLocalContext (f : LocalContext → MetaM LocalContext) : TacticM Unit :=
+  liftMetaTactic fun mvarId => do
+    let newGoal ← mkFreshExprMVarAt (← f (← getLCtx)) (← getLocalInstances)
       (← getMVarType mvarId) .syntheticOpaque (← getMVarTag mvarId)
     assignExprMVar mvarId newGoal
     return [newGoal.mvarId!]
 
-/-- Updates the current goal with a `Expr` map function in `MetaM`. -/
-def modifyGoal (fExpr : Expr → MetaM Expr) : TacticM Unit :=
-  liftMetaTactic fun mvarId => do
-    let newGoal ← mkFreshExprMVarAt (← getLCtx) (← getLocalInstances)
-      (← fExpr (← getMVarType mvarId)) .syntheticOpaque (← getMVarTag mvarId)
-    assignExprMVar mvarId newGoal
-    return [newGoal.mvarId!]
+/-- Updates a `LocalDecl` to be found by its `FVarId` with a map function in `MetaM`. -/
+def modifyLocalDeclById (fvarId : FVarId) (f : (Option LocalDecl) → MetaM LocalDecl) :
+    TacticM Unit :=
+  modifyLocalContext fun lctx => do
+    let decl ← f (lctx.find? fvarId)
+    return ⟨lctx.fvarIdToDecl.insert decl.fvarId decl, lctx.decls.set decl.index decl⟩
+
+/-- Updates a `LocalDecl` to be found by its `Name` with a map function in `MetaM`. -/
+def modifyLocalDeclByName (name : Name) (f : (Option LocalDecl) → MetaM LocalDecl) : TacticM Unit :=
+  modifyLocalContext fun lctx => do
+    let decl ← f (lctx.findFromUserName? name)
+    return ⟨lctx.fvarIdToDecl.insert decl.fvarId decl, lctx.decls.set decl.index decl⟩
 
 /-- Renames bound variables on a hypothesis. -/
-def renameBVarHyp (old new : Name) (hyp : FVarId) : TacticM Unit :=
-  modifyLocalContext fun ctx =>
-    return ctx.modifyLocalDecl hyp fun decl =>
-      decl.setType $ decl.type.renameBVar old new
+def renameBVarHyp (old new : Name) (fvarId : FVarId) : TacticM Unit :=
+  modifyLocalDeclById fvarId fun decl => match decl with
+    | some decl => return decl.setType $ decl.type.renameBVar old new
+    | none      => throwError "invalid FVarId: {repr fvarId}"
 
 /-- Renames bound variables on the goal. -/
 def renameBVarGoal (old new : Name) : TacticM Unit :=
-  modifyGoal fun e =>
-    return e.renameBVar old new
+  modifyGoal fun e => return e.renameBVar old new
