05169ce1ec4f05af7154381f9f55b3707ff682ed
Arthur Paulino <arthurleonardo.ap@gmail.com>
2022-07-23 18:42:50 -0300

    move 'renameBVar' to 'Mathlib/Lean/Expr/Basic.lean'

--- Mathlib/Util/Tactic.lean
+++ Mathlib/Util/Tactic.lean
@@ -4,8 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Arthur Paulino
 -/
 
-import Lean
-import Mathlib.Util.Expr
+import Mathlib.Lean.Expr.Basic
 
 namespace Mathlib.Tactic
 
