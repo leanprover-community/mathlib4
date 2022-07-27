b3e10f775a6f17d0405108347b7728e8d0c356bd
Scott Morrison <scott.morrison@gmail.com>
2022-07-27 12:50:36 +1000

    chore: record failing tests for Use from mathlib3

--- test/Use.lean
+++ test/Use.lean
@@ -5,6 +5,7 @@ Authors: Arthur Paulino
 -/
 
 import Mathlib.Tactic.Use
+import Mathlib.Tactic.Basic
 
 example : ∃ x : Nat, x = x := by use 42
 
@@ -16,3 +17,33 @@ example : ∃ x : Nat, x = x := by
   use ?_
   exact 42
   rfl
+
+example (α : Type) : ∃ S : List α, S = S :=
+by use ∅
+
+example : ∃ x : Int, x = x :=
+by use 42
+
+example : ∃ a b c : Int, a + b + c = 6 :=
+by use 1, 2, 3
+
+example : ∃ p : Int × Int, p.1 = 1 :=
+by use ⟨1, 42⟩
+
+-- FIXME Failing tests ported from mathlib3
+
+-- example : ∃ (n : Nat) (h : n > 0), n = n :=
+-- by
+--   use 1
+--   -- goal should now be `1 > 0 ∧ 1 = 1`, whereas it would be `∃ (H : 1 > 0), 1 = 1` after existsi 1.
+--   guard_target == 1 > 0 ∧ 1 = 1
+--   exact ⟨Nat.zero_lt_one, rfl⟩
+
+-- example : Σ x y : Int, (Int × Int) × Int :=
+-- by use 1, 2, 3, 4, 5
+
+-- inductive foo
+-- | mk : Nat → Bool × Nat → Nat → foo
+
+-- example : foo :=
+-- by use 100, true, 4, 3
