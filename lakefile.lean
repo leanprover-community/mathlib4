6417437bcb571695a981a57b25e32f810d079d8c
Henrik Böving <hargonix@gmail.com>
2022-08-12 00:00:31 +0200

    feat: Add support for building documentation via facets

--- lakefile.lean
+++ lakefile.lean
@@ -11,3 +11,6 @@ lean_lib Mathlib
 lean_exe runLinter where
   root := `scripts.runLinter
   supportInterpreter := true
+
+meta if get_config? env = some "dev" then -- dev is so not everyone has to build it
+require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
