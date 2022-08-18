202a0404a2fe885d880f856fd7ccec4c26f63e9d
Henrik Böving <hargonix@gmail.com>
2022-08-18 13:43:40 +0200

    Update lakefile.lean
    Co-authored-by: Gabriel Ebner <gebner@gebner.org>

--- lakefile.lean
+++ lakefile.lean
@@ -12,5 +12,5 @@ lean_exe runLinter where
   root := `scripts.runLinter
   supportInterpreter := true
 
-meta if get_config? env = some "dev" then -- dev is so not everyone has to build it
+meta if get_config? doc = some "on" then -- do not download and build doc-gen4 by default
 require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
