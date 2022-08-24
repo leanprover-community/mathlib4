6c4e23a27a5470fe14d8214d14e52ad673f8da62
David Renshaw <dwrenshaw@gmail.com>
2022-08-24 08:50:14 -0400

    better renaming of ih

--- Mathlib/Data/List/Basic.lean
+++ Mathlib/Data/List/Basic.lean
@@ -243,13 +243,13 @@ lemma length_injective_iff : injective (List.length : List α → ℕ) ↔ Subsi
   constructor
   · intro h; refine ⟨λ x y => ?_⟩; (suffices [x] = [y] by simpa using this); apply h; rfl
   · intros hα l1 l2 hl
-    induction l1 generalizing l2 with | nil => ?_ | cons _ _ ih => ?_ <;> cases l2
-    · rfl
-    · cases hl
-    · cases hl
-    · congr
-      · exact Subsingleton.elim _ _
-      · apply ih; simpa using hl
+    induction l1 generalizing l2 <;> cases l2
+    case nil.nil => rfl
+    case nil.cons => cases hl
+    case cons.nil => cases hl
+    case cons.cons ih _ _ => congr
+                             · exact Subsingleton.elim _ _
+                             · apply ih; simpa using hl
 
 -- TODO this is @[simp] in mathlib3, but here would run into the simpNF linter.
 lemma length_injective [Subsingleton α] : injective (length : List α → ℕ) :=
