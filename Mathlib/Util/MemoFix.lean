e32627db3d05d77a414a096ae1f9f8c3fcb0c543
Gabriel Ebner <gebner@gebner.org>
2022-09-02 15:27:03 +0200

    chore: bump to 2022-09-01

--- Mathlib/Util/MemoFix.lean
+++ Mathlib/Util/MemoFix.lean
@@ -3,7 +3,7 @@ Copyright (c) 2022 Gabriel Ebner. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Gabriel Ebner, E.W.Ayers
 -/
-import Bootstrap
+import Lean.Data.HashMap
 
 /-!
 # Fixpoint function with memoisation
