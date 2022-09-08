71390c02f4d304479e2d47a14a81fc0d1e85390a
Jireh Loreaux <loreaujy@gmail.com>
2022-09-08 12:25:32 -0500

    make the tactic *actually* fail instead of catching the error

--- Mathlib/Tactic/TypeCheck.lean
+++ Mathlib/Tactic/TypeCheck.lean
@@ -1,11 +1,10 @@
 import Lean.Elab.Tactic.Basic
 
+open Lean Meta
+
 /-- Type check the given expression, and trace its type. -/
 elab tk:"type_check " e:term : tactic => do
   let e ← Lean.Elab.Term.elabTerm e none
-  try
-    Lean.Meta.check e
-    let type ← Lean.Meta.inferType e
-    Lean.logInfoAt tk f!"{← Lean.instantiateMVars type}"
-  catch _ =>
-    Lean.logInfoAt tk "Term does not type check"
+  check e
+  let type ← inferType e
+  Lean.logInfoAt tk f!"{← Lean.instantiateMVars type}"
