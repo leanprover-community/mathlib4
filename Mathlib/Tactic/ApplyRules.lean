acde493aec07f241fe1eebb46eca724109ac8410
Scott Morrison <scott.morrison@gmail.com>
2022-09-15 15:24:27 +1000

    oops

--- Mathlib/Tactic/ApplyRules.lean
+++ Mathlib/Tactic/ApplyRules.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison
 -/
 import Std.Util.TermUnsafe
-import Mathlib.Tactic.repeat
+import Mathlib.Tactic.Repeat
 
 /-!
 # The `apply_rules` tactic
