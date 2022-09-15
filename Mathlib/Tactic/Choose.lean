9488f1d51d3ca51d1f0780ad1dec2992a786a250
Mario Carneiro <di.gama@gmail.com>
2022-09-15 07:55:19 -0400

    choose1 in MetaM

--- Mathlib/Tactic/Choose.lean
+++ Mathlib/Tactic/Choose.lean
@@ -44,9 +44,8 @@ def ElimStatus.merge : ElimStatus → ElimStatus → ElimStatus
 | _, success => success
 | failure ts₁, failure ts₂ => failure (ts₁ ++ ts₂)
 
-def choose1 (nondep : Bool) (h : Option Expr) (data : Name) :
-  TacticM ElimStatus := do
-  let g ← getMainGoal
+def choose1 (g : MVarId) (nondep : Bool) (h : Option Expr) (data : Name) :
+  MetaM (ElimStatus × Expr × MVarId) := do
   let (g, h) ← match h with
   | some e => pure (g, e)
   | none   => do
@@ -55,53 +54,70 @@ def choose1 (nondep : Bool) (h : Option Expr) (data : Name) :
   g.withContext do
     let h ← instantiateMVars h
     let t ← inferType h
-    forallTelescopeReducing t $ fun ctxt t => do
-      let t ← withTransparency .all (whnf t)
-      match t.getAppFnArgs with
-      | (`Exists, #[α, p]) => do
-        let ne_fail : ElimStatus := .failure []
+    forallTelescopeReducing t fun ctxt t => do
+      (← withTransparency .all (whnf t)).withApp fun
+      | .const ``Exists [u], #[α, p] => do
+        let neFail : ElimStatus := .failure []
         let nonemp : Option Expr := none
         let ctxt' ← if nonemp.isSome then ctxt.filterM (fun e => not <$> isProof e) else pure ctxt
-        let data_ty ← mkForallFVars ctxt' α
-        let data_val ← mkAppM ``Classical.choose #[mkAppN h ctxt]
-        let (d, g) ← (← g.assert data data_ty data_val).intro1P
-        let spec_ty ← mkForallFVars ctxt (p.app (mkAppN (.fvar d) ctxt')).headBeta
-        let spec_val ← mkAppM ``Classical.choose_spec #[mkAppN h ctxt]
-        let mut g ← g.assert .anonymous spec_ty spec_val
-        match h with
-        | .fvar v => g ← g.clear v
-        | _ => pure ()
-        replaceMainGoal [g]
-        return ne_fail
-        | (`And, #[p, q]) => throwError "not implemented yet"
-        -- TODO: support Σ, × ?
-        | _ => throwError "expected a term of the shape `∀xs, ∃a, p xs a` or `∀xs, p xs ∧ q xs`"
+        let dataTy ← mkForallFVars ctxt' α
+        let dataVal ← mkLambdaFVars ctxt' <|
+          mkApp3 (.const ``Classical.choose [u]) α p (mkAppN h ctxt)
+        let specVal ← mkLambdaFVars ctxt <|
+          mkApp3 (.const ``Classical.choose_spec [u]) α p (mkAppN h ctxt)
+        let (fvar, g) ← withLocalDeclD data dataTy fun d => do
+          let specTy ← mkForallFVars ctxt (p.app (mkAppN d ctxt')).headBeta
+          g.withContext do
+          withLocalDeclD data dataTy fun d' => do
+          let mvarTy ← mkArrow (specTy.replaceFVar d d') (← g.getType)
+          let newMVar ← mkFreshExprSyntheticOpaqueMVar mvarTy (← g.getTag)
+          g.assign <| mkApp2 (← mkLambdaFVars #[d'] newMVar) dataVal specVal
+          pure (d', newMVar.mvarId!)
+        let g ← match h with
+        | .fvar v => g.clear v
+        | _ => pure g
+        return (neFail, fvar, g)
+      | .const ``And _, #[p, q] => throwError "not implemented yet"
+      -- TODO: support Σ, × ?
+      | _, _ => throwError "expected a term of the shape `∀xs, ∃a, p xs a` or `∀xs, p xs ∧ q xs`"
 
-def choose (nondep : Bool) (h : Option Expr) : List Name → ElimStatus → TacticM Unit
-| [], _ => throwError "expect list of variables"
-| [n], status => match nondep, status with
-  | true, .failure tys => do    -- We expected some elimination, but it didn't happen.
-    let gs ← tys.mapM (fun ty => Expr.mvarId! <$> (mkFreshExprMVar (some ty)))
-    setGoals gs
-    throwError "choose!: failed to synthesize any nonempty instances"
+def choose1WithInfo (g : MVarId) (nondep : Bool) (h : Option Expr) (data : Ident) :
+  TermElabM (ElimStatus × MVarId) := do
+  let (status, fvar, g) ← choose1 g nondep h data.getId
+  g.withContext do
+    Elab.Term.addLocalVarInfo data fvar
+  pure (status, g)
+
+def choose (nondep : Bool) (h : Option Expr) :
+  List Ident → ElimStatus → MVarId → TermElabM (Except (List Expr) MVarId)
+| [], _, _ => throwError "expect list of variables"
+| [n], status, g =>
+  match nondep, status with
+  | true, .failure tys => pure (.error tys)   -- We expected some elimination, but it didn't happen.
   | _, _ => do
-    let g ← getMainGoal
-    replaceMainGoal [(← g.intro n).2]
-| (n::ns), status => do
-  let status' ← choose1 nondep h n
-  choose nondep none ns (status.merge status')
+    let (fvar, g) ← g.intro n.getId
+    g.withContext do
+      Elab.Term.addLocalVarInfo n (mkFVar fvar)
+    return .ok g
+| (n::ns), status, g => do
+  let (status', g) ← choose1WithInfo g nondep h n
+  choose nondep none ns (status.merge status') g
 
 elab "choose1 " data:ident spec:ident " using " h:term : tactic =>
   withMainContext do
     let h ← Elab.Tactic.elabTerm h none
-    _ ← choose1 false h data.getId
+    replaceMainGoal [(← choose1WithInfo (← getMainGoal) false h data).2]
     evalTactic $ ← `(tactic|intro $spec:ident)
 
-elab "choose" first:ident rest:ident* " using " h:term : tactic =>
+elab "choose" ids:ident+ " using " h:term : tactic =>
   withMainContext do
     let h ← Elab.Tactic.elabTerm h none
-    _ ← choose false h ((first :: rest.toList).map (fun i => i.getId)) (.failure [])
-
+    match ← choose false h ids.toList (.failure []) (← getMainGoal) with
+    | .ok g => replaceMainGoal [g]
+    | .error tys =>
+      let gs ← tys.mapM (fun ty => Expr.mvarId! <$> (mkFreshExprMVar (some ty)))
+      setGoals gs
+      throwError "choose!: failed to synthesize any nonempty instances"
 
 example (h : ∀n m : Nat, n < m → ∃i j, m = n + i ∨ m + j = n) : True :=
 by
@@ -109,7 +125,7 @@ by
   -- choose1 i h' using h
   -- choose1 j ignoreme using h'
   -- clear ignoreme
-  trivial
+  sorry
   -- guard_hyp i : ∀n m : ℕ, n < m → ℕ,
   -- guard_hyp j : ∀n m : ℕ, n < m → ℕ,
   -- guard_hyp h : ∀ (n m : ℕ) (h : n < m), m = n + i n m h ∨ m + j n m h = n,
