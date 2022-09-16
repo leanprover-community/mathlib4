305f6507772bdb0856bbd652f1345f503006c55f
Scott Morrison <scott.morrison@gmail.com>
2022-09-16 11:57:10 +1000

    iteration bound, with a temporary workaround

--- Mathlib/Tactic/ApplyRules.lean
+++ Mathlib/Tactic/ApplyRules.lean
@@ -14,14 +14,49 @@ The `apply_rules` tactic calls `apply` (with a specified set of lemmas) and `ass
 repeatedly, until no more applications are possible.
 -/
 
+
+namespace Lean.Meta
+
+/--
+`repeat' f` runs `f` on all of the goals to produce a new list of goals,
+then runs `f` again on all of those goals, and repeats until `f` fails on all remaining goals,
+or until `maxIters` total calls to `f` have occurred.
+-/
+def repeat'' [Monad m] [MonadError m] [MonadMCtx m]
+    (f : MVarId → m (List MVarId)) (gs : List MVarId) (maxIters := 100000) : m (List MVarId) := do
+  let acc ← go maxIters gs [] #[]
+  pure (← acc.filterM fun g => not <$> g.isAssigned).toList
+where
+  /-- Auxiliary for `repeat'`. `repeat'.go f maxIters gs stk acc` evaluates to
+  essentially `acc.toList ++ repeat' f (gs::stk).join maxIters`: that is, `acc` are goals we will
+  not revisit, and `(gs::stk).join` is the accumulated todo list of subgoals. -/
+  go : Nat → List MVarId → List (List MVarId) → Array MVarId → m (Array MVarId)
+  | _, [], [], acc => pure acc
+  | n, [], gs::stk, acc => go n gs stk acc
+  | n, g::gs, stk, acc => do
+    if ← g.isAssigned then
+      go n gs stk acc
+    else
+      match n with
+      -- This line is different from the `repeat'` in `Std`; otherwise we drop goals.
+      | 0 => pure (acc ++ g::gs ++ stk.join)
+      | n+1 =>
+        match ← observing (f g) with
+        | .ok gs' => go n gs' (gs::stk) acc
+        | .error _ => go n gs stk (acc.push g)
+termination_by _ n gs stk _ => (n, stk, gs)
+
+end Lean.Meta
+
 namespace Mathlib.Tactic
 open Lean Meta Elab Tactic Term Parser.Tactic
 
 /--
 Implementation of the `apply_rules` tactic.
 -/
-def applyRules (cfg : ApplyConfig) (L : List Expr) : MVarId → MetaM (List MVarId) :=
-(repeat' (fun g => (do g.assumption; pure []) <|> L.firstM (g.apply · cfg)) [·])
+def applyRules (cfg : ApplyConfig) (maxIters : Nat) (L : List Expr) : MVarId → MetaM (List MVarId) :=
+fun h => repeat'' (maxIters := maxIters)
+  (fun g => (do g.assumption; pure []) <|> L.firstM (g.apply · cfg)) [h]
 
 -- This should be moved higher in the import hierarchy when others need it.
 declare_config_elab elabApplyConfig ApplyConfig
@@ -34,6 +69,9 @@ If `apply` generates new goals, `apply_rules` iteratively tries to solve those g
 You may include attributes amongst the lemmas:
 `apply_rules` will include all lemmas marked with these attributes.
 
+You can bound the iteration depth using the syntax `apply_rules lemmas n`.
+The default bound is 50.
+
 You can pass a further configuration `cfg : ApplyConfig` via the syntax `apply_rules cfg lemmas`.
 
 Unlike `solve_by_elim`, `apply_rules` does not perform backtracking, and greedily applies
@@ -43,8 +81,8 @@ TODO: add support for attributes
 TODO: copy the other tests/examples from Lean 3
 -/
 elab (name := applyRulesElab)
-  "apply_rules" cfg:(config ?) " [" lemmas:term,* "]" : tactic =>
+  "apply_rules" cfg:(config ?) " [" lemmas:term,* "]" n:(ppSpace num)? : tactic =>
 do
   let cfg ← elabApplyConfig cfg
   let lemmas ← lemmas.getElems.toList.mapM (elabTermForApply ·.raw)
-  liftMetaTactic $ applyRules cfg lemmas
+  liftMetaTactic $ applyRules cfg (n.map (·.getNat) |>.getD 50) lemmas
--- test/apply_rules.lean
+++ test/apply_rules.lean
@@ -11,6 +11,15 @@ example {a b c d e : Nat} (h1 : a ≤ b) (h2 : c ≤ d) (h3 : 0 ≤ e) :
   a + c * e + a + c + 0 ≤ b + d * e + b + d + e :=
 by apply_rules [add_le_add, Nat.mul_le_mul_of_nonneg_right]
 
+-- Check that when we supply an iteration bound,
+-- `apply_rules` works up to that bound and returns the remaining goals.
+example {a b c d e : Nat} (h1 : a ≤ b) (h2 : c ≤ d) (h3 : 0 ≤ e) :
+  a + c * e + a + c + 0 ≤ b + d * e + b + d + e :=
+by
+  apply_rules [add_le_add, Nat.mul_le_mul_of_nonneg_right] 9
+  guard_target = 0 ≤ e
+  assumption
+
 -- Test that metavariables created for implicit arguments don't get stuck
 -- This required extra work in Lean 3, but doesn't seem to be a problem in Lean 4.
 example (P : Nat → Type) (f : {n : Nat} → P n → P (n + 1)) (g : P 0) : P 2 :=
