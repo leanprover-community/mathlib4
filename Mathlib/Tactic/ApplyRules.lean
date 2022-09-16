0cf12c05038863f9a6a28fbcf579a45056fd3e12
Scott Morrison <scott.morrison@gmail.com>
2022-09-16 12:09:31 +1000

    update post repeat' fix

--- Mathlib/Tactic/ApplyRules.lean
+++ Mathlib/Tactic/ApplyRules.lean
@@ -14,40 +14,6 @@ The `apply_rules` tactic calls `apply` (with a specified set of lemmas) and `ass
 repeatedly, until no more applications are possible.
 -/
 
-
-namespace Lean.Meta
-
-/--
-`repeat' f` runs `f` on all of the goals to produce a new list of goals,
-then runs `f` again on all of those goals, and repeats until `f` fails on all remaining goals,
-or until `maxIters` total calls to `f` have occurred.
--/
-def repeat'' [Monad m] [MonadError m] [MonadMCtx m]
-    (f : MVarId → m (List MVarId)) (gs : List MVarId) (maxIters := 100000) : m (List MVarId) := do
-  let acc ← go maxIters gs [] #[]
-  pure (← acc.filterM fun g => not <$> g.isAssigned).toList
-where
-  /-- Auxiliary for `repeat'`. `repeat'.go f maxIters gs stk acc` evaluates to
-  essentially `acc.toList ++ repeat' f (gs::stk).join maxIters`: that is, `acc` are goals we will
-  not revisit, and `(gs::stk).join` is the accumulated todo list of subgoals. -/
-  go : Nat → List MVarId → List (List MVarId) → Array MVarId → m (Array MVarId)
-  | _, [], [], acc => pure acc
-  | n, [], gs::stk, acc => go n gs stk acc
-  | n, g::gs, stk, acc => do
-    if ← g.isAssigned then
-      go n gs stk acc
-    else
-      match n with
-      -- This line is different from the `repeat'` in `Std`; otherwise we drop goals.
-      | 0 => pure (acc ++ g::gs ++ stk.join)
-      | n+1 =>
-        match ← observing (f g) with
-        | .ok gs' => go n gs' (gs::stk) acc
-        | .error _ => go n gs stk (acc.push g)
-termination_by _ n gs stk _ => (n, stk, gs)
-
-end Lean.Meta
-
 namespace Mathlib.Tactic
 open Lean Meta Elab Tactic Term Parser.Tactic
 
@@ -56,7 +22,7 @@ Implementation of the `apply_rules` tactic.
 -/
 def applyRules (cfg : ApplyConfig) (maxIters : Nat) (L : List Expr) :
   MVarId → MetaM (List MVarId) :=
-fun h => repeat'' (maxIters := maxIters)
+fun h => repeat' (maxIters := maxIters)
   (fun g => (do g.assumption; pure []) <|> L.firstM (g.apply · cfg)) [h]
 
 -- This should be moved higher in the import hierarchy when others need it.
