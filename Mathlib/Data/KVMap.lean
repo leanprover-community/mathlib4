1b85a1a809c04f318f6e50014e745b51284876b5
Alex J Best <alex.j.best@gmail.com>
2022-09-19 11:13:54 +0200

    Update Mathlib/Data/KVMap.lean

--- Mathlib/Data/KVMap.lean
+++ Mathlib/Data/KVMap.lean
@@ -14,7 +14,7 @@ namespace Lean.KVMap
 /-- erase pairs whose names match the second argument from a list of
 `Name × DataValue` pairs-/
 def eraseCore : List (Name × DataValue) → Name → List (Name × DataValue)
-  | l, n => List.filter (fun a => a.1 ≠ n)
+  | l, n => List.filter (fun a => a.1 ≠ n) l
 
 /-- erase an entry from the map -/
 def erase : KVMap → Name → KVMap
