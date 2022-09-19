cd1290df3c56e9c437bf633e451ad6e5eebf3e85
newell <newell.jensen@gmail.com>
2022-09-18 18:51:41 -0700

    Add econstructor tactic.

--- test/Constructor.lean
+++ test/Constructor.lean
@@ -4,7 +4,13 @@ structure Foo where
   a : Type
   x : List a
 
+-- fconstructor
 example : Foo := by
   fconstructor
   exact Nat
+  exact [0,1,2]
+
+-- econstructor
+example : Foo := by
+  econstructor
   exact [0,1,2]
