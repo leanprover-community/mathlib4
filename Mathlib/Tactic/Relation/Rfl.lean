4384de4fc07d89bb1725edc018f9cf5a2d03a17f
newell <newell.jensen@gmail.com>
2022-09-22 16:28:12 -0700

    Add copyright string.

--- Mathlib/Tactic/Relation/Rfl.lean
+++ Mathlib/Tactic/Relation/Rfl.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2022 Newell Jensen. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Newell Jensen
+-/
+
 import Lean
 import Mathlib.Tactic.Relation.Basic
 
