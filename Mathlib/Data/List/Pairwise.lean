eb6fc5b1c9e364db5e7e8b394844d757e533d741
Scott Morrison <scott.morrison@gmail.com>
2022-10-08 20:23:40 +1100

    remove import ShowTerm

--- Mathlib/Data/List/Pairwise.lean
+++ Mathlib/Data/List/Pairwise.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro
 -/
 import Mathlib.Data.List.Basic
-import Mathlib.Tactic.ShowTerm
 
 /-!
 # Pairwise relations on a list
