1203930f75e86aa6260e8c1b9e5b5fbeed7656b7
Scott Morrison <scott@tqft.net>
2022-10-11 09:52:47 +1100

    chore: bump std (#464)
    * chore: bumping std
    
    * chore: remove unused theorem
    
    Co-authored-by: Mario Carneiro <di.gama@gmail.com>

--- Mathlib/Data/List/Pairwise.lean
+++ Mathlib/Data/List/Pairwise.lean
@@ -13,15 +13,6 @@ namespace List
 
 variable {α β : Type _} {R S T : α → α → Prop} {a : α} {l : List α}
 
-theorem pairwise_append {l₁ l₂ : List α} : Pairwise R (l₁ ++ l₂) ↔
-    Pairwise R l₁ ∧ Pairwise R l₂ ∧ (∀ x, x ∈ l₁ → ∀ y, y ∈ l₂ → R x y) := by
-  induction l₁ with
-  | nil => simp [Pairwise.nil]
-  | cons a l₁ ih =>
-    simp only [cons_append, Pairwise_cons, forall_mem_append, ih, forall_mem_cons,
-      forall_and_distrib, and_assoc, And.left_comm]
-    rfl
-
 theorem pairwise_append_comm (s : symmetric R) {l₁ l₂ : List α} :
     Pairwise R (l₁ ++ l₂) ↔ Pairwise R (l₂ ++ l₁) := by
   have : ∀ l₁ l₂ : List α, (∀ x : α, x ∈ l₁ → ∀ y : α, y ∈ l₂ → R x y) →
@@ -50,28 +41,13 @@ theorem Pairwise.imp_of_mem {S : α → α → Prop} {l : List α}
     · exact Ball.imp_right (fun x h => H (mem_cons_self _ _) (mem_cons_of_mem _ h)) r
     · exact ih fun {a b} m m' => H (mem_cons_of_mem _ m) (mem_cons_of_mem _ m')
 
-theorem Pairwise.imp (H : ∀ a b, R a b → S a b) : Pairwise R l → Pairwise S l :=
-  Pairwise.imp_of_mem fun {a b} _ _ => H a b
-
-theorem pairwise_map (f : β → α) :
-    ∀ {l : List β}, Pairwise R (map f l) ↔ Pairwise (fun a b : β => R (f a) (f b)) l
-  | [] => by
-    simp only [map, Pairwise.nil]
-  | b :: l => by
-    have : (∀ a b', b' ∈ l → a = f b' → R (f b) a) ↔ ∀ b' : β, b' ∈ l → R (f b) (f b') :=
-      forall_swap.trans <| forall_congr' fun a => forall_swap.trans <| by
-        simp only [forall_eq]
-        rfl
-    simp only [Pairwise_cons, map, pairwise_map, mem_map, exists_imp_distrib, and_imp, this]
-    rfl
-
 theorem Pairwise.of_map {S : β → β → Prop} (f : α → β) (H : ∀ a b : α, S (f a) (f b) → R a b)
     (p : Pairwise S (map f l)) : Pairwise R l :=
-  ((pairwise_map f).1 p).imp H
+  (pairwise_map.1 p).imp (H _ _)
 
 theorem Pairwise.map {S : β → β → Prop} (f : α → β) (H : ∀ a b : α, R a b → S (f a) (f b))
     (p : Pairwise R l) : Pairwise S (map f l) :=
-  (pairwise_map f).2 <| p.imp H
+  pairwise_map.2 <| p.imp (H _ _)
 
 theorem Pairwise.iff_of_mem {S : α → α → Prop} {l : List α}
     (H : ∀ {a b}, a ∈ l → b ∈ l → (R a b ↔ S a b)) : Pairwise R l ↔ Pairwise S l :=
