19972c8aaa41869959b89587e4c68b69d386b648
Scott Morrison <scott.morrison@gmail.com>
2022-10-17 18:00:16 +1100

    .

--- Mathlib.lean
+++ Mathlib.lean
@@ -65,8 +65,8 @@ import Mathlib.Mathport.Syntax
 import Mathlib.Order.Basic
 import Mathlib.Order.Monotone
 import Mathlib.Tactic.Alias
-import Mathlib.Tactic.ApplyWith
 import Mathlib.Tactic.ApplyFun
+import Mathlib.Tactic.ApplyWith
 import Mathlib.Tactic.Basic
 import Mathlib.Tactic.ByContra
 import Mathlib.Tactic.Cache
