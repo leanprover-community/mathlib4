24a95ca117a9647c94476ad01916d38a6cf08dff
Patrick Massot <patrickmassot@free.fr>
2022-10-18 17:13:58 +0200

    feat: port algebra.abs

--- Mathlib/Algebra/Abs.lean
+++ Mathlib/Algebra/Abs.lean
@@ -3,18 +3,18 @@ Copyright (c) 2021 Christopher Hoskin. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Christopher Hoskin
 -/
-
+import Mathlib.Mathport.Rename
 /-!
 # Absolute value
 
-This file defines a notational class `has_abs` which adds the unary operator `abs` and the notation
+This file defines a notational class `Abs` which adds the unary operator `abs` and the notation
 `|.|`. The concept of an absolute value occurs in lattice ordered groups and in GL and GM spaces.
 
 Mathematical structures possessing an absolute value often also possess a unique decomposition of
 elements into "positive" and "negative" parts which are in some sense "disjoint" (e.g. the Jordan
-decomposition of a measure). This file also defines `has_pos_part` and `has_neg_part` classes
+decomposition of a measure). This file also defines `PosPart` and `NegPart` classes
 which add unary operators `pos` and `neg`, representing the maps taking an element to its positive
-and negative part respectively along with the notation ⁺ and ⁻.
+and negative part respectively along with the notation `⁺` and `⁻`.
 
 ## Notations
 
@@ -30,25 +30,32 @@ absolute
 -/
 
 
-/-- Absolute value is a unary operator with properties similar to the absolute value of a real number.
+/--
+Absolute value is a unary operator with properties similar to the absolute value of a real number.
 -/
-class HasAbs (α : Type _) where
+class Abs (α : Type _) where
   abs : α → α
 
-export HasAbs (abs)
+#align has_abs Abs
+
+export Abs (abs)
 
 /-- The positive part of an element admiting a decomposition into positive and negative parts.
 -/
-class HasPosPart (α : Type _) where
+class PosPart (α : Type _) where
   Pos : α → α
+
+#align has_pos_part PosPart
 
 /-- The negative part of an element admiting a decomposition into positive and negative parts.
 -/
-class HasNegPart (α : Type _) where
+class NegPart (α : Type _) where
   neg : α → α
 
--- mathport name: «expr ⁺»
-postfix:1000 "⁺" => HasPosPart.pos
+#align has_neg_part NegPart
 
--- mathport name: «expr ⁻»
-postfix:1000 "⁻" => HasNegPart.neg
+macro atomic("|" noWs) a:term noWs "|" : term => `(abs $a)
+
+postfix:1000 "⁺" => PosPart.pos
+
+postfix:1000 "⁻" => NegPart.neg
