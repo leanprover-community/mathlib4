4b4a44455260a02207a0ae55d2316fc557336269
Scott Morrison <scott.morrison@gmail.com>
2022-10-19 15:26:17 +1100

    move comment to module-doc

--- Mathlib/Logic/Relator.lean
+++ Mathlib/Logic/Relator.lean
@@ -2,10 +2,13 @@
 Copyright (c) 2017 Johannes Hölzl. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl
-Relator for functions, pairs, sums, and lists.
 -/
 
 import Mathlib.Logic.Basic
+
+/-!
+# Relator for functions, pairs, sums, and lists.
+-/
 
 namespace Relator
 universe u₁ u₂ v₁ v₂
