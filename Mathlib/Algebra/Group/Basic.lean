6ec4c29ffedbbe2364025dedd6449872458af87e
Scott Morrison <scott.morrison@gmail.com>
2022-10-21 11:20:04 +1100

    fix downstream proofs

--- Mathlib/Algebra/Group/Basic.lean
+++ Mathlib/Algebra/Group/Basic.lean
@@ -104,7 +104,7 @@ variable {M : Type u} [CommMonoid M] {x y z : M}
 
 @[to_additive]
 theorem inv_unique (hy : x * y = 1) (hz : x * z = 1) : y = z :=
-  left_inv_eq_right_inv (trans (mul_comm _ _) hy) hz
+  left_inv_eq_right_inv (Trans.trans (mul_comm _ _) hy) hz
 
 end CommMonoid
 
