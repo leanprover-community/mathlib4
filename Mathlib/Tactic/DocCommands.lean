2298cbe351591ea952ed0b40b723a442d21d3b1a
Evgenia Karunus <lakesare@gmail.com>
2022-10-24 06:48:37 +0400

    Fix linting: add a module doc string

--- Mathlib/Tactic/DocCommands.lean
+++ Mathlib/Tactic/DocCommands.lean
@@ -4,6 +4,10 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Robert Y. Lewis
 -/
 
+/-!
+# Documentation commands
+-/
+
 import Mathlib.Lean.Expr.Basic
 open Lean Elab
 
