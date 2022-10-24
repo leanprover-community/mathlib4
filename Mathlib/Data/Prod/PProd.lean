bc4662a6beea10a1fdc0422270d7368204834b76
Yakov Pechersky <ypechersky@treeline.bio>
2022-10-24 19:34:04 -0400

    Rename congr_arg2

--- Mathlib/Data/Prod/PProd.lean
+++ Mathlib/Data/Prod/PProd.lean
@@ -40,4 +40,4 @@ theorem Function.Injective.pprod_map {f : α → β} {g : γ → δ} (hf : injec
     injective (fun x => ⟨f x.1, g x.2⟩ : PProd α γ → PProd β δ) := fun _ _ h =>
   have A := congr_arg PProd.fst h
   have B := congr_arg PProd.snd h
-  congr_arg2 PProd.mk (hf A) (hg B)
+  congr_arg₂ PProd.mk (hf A) (hg B)
