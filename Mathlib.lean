1771492739cfef3f26cbb7c3ef7d0510eeca3761
Yakov Pechersky <ypechersky@treeline.bio>
2022-10-23 23:45:48 -0400

    Fix import typo

--- Mathlib.lean
+++ Mathlib.lean
@@ -40,7 +40,7 @@ import Mathlib.Data.Nat.Basic
 import Mathlib.Data.Option.Basic
 import Mathlib.Data.Option.Defs
 import Mathlib.Data.Prod
-import Mathlib.Data.Prod.PProp
+import Mathlib.Data.Prod.PProd
 import Mathlib.Data.Sigma.Basic
 import Mathlib.Data.String.Defs
 import Mathlib.Data.String.Lemmas
