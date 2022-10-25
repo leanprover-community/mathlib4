e318ee1b121a40c2f1882638b0d8beef47bdb467
Scott Morrison <scott.morrison@gmail.com>
2022-10-25 15:46:10 +1100

    feat: library_search using

--- Mathlib/Tactic/LibrarySearch.lean
+++ Mathlib/Tactic/LibrarySearch.lean
@@ -50,8 +50,24 @@ initialize librarySearchLemmas : DeclCache (DiscrTree Name) ←
       let keys ← withReducible <| DiscrTree.mkPath type
       pure $ lemmas.insertCore keys name
 
-def librarySearch (goal : MVarId) (lemmas : DiscrTree Name) (solveByElimDepth := 6) :
-    MetaM <| Option (Array <| MetavarContext × List MVarId) := do
+/--
+Try to solve the goal either by:
+* calling `solveByElim`
+* or applying a library lemma then calling  `solveByElim` on the resulting goals.
+
+If it successfully closes the goal, returns `none`.
+Otherwise, it returns `some a`, where `a : Array (MetavarContext × List MVarId)`,
+with an entry for each library lemma which was successfully applied,
+containing the metavariable context after the application, and a list of the subsidiary goals.
+
+(Always succeeds, and the metavariable context stored in the monad is reverted,
+unless the goal was completely solved.)
+
+(Note that if `solveByElim` solves some but not all subsidiary goals,
+this is not currently tracked.)
+-/
+def librarySearch (goal : MVarId) (lemmas : DiscrTree Name) (required : List Expr)
+    (solveByElimDepth := 6) : MetaM <| Option (Array <| MetavarContext × List MVarId) := do
   profileitM Exception "librarySearch" (← getOptions) do
   let ty ← goal.getType
   withTraceNode `Tactic.librarySearch (return m!"{exceptOptionEmoji ·} {ty}") do
@@ -62,7 +78,10 @@ def librarySearch (goal : MVarId) (lemmas : DiscrTree Name) (solveByElimDepth :=
 
   try
     solveByElim solveByElimDepth goal
-    return none
+    if (← checkRequired) then
+      return none
+    else
+      set state0
   catch _ =>
     set state0
 
@@ -76,11 +95,19 @@ def librarySearch (goal : MVarId) (lemmas : DiscrTree Name) (solveByElimDepth :=
             newGoal.withContext do
               trace[Tactic.librarySearch] "proving {← addMessageContextFull (mkMVar newGoal)}"
               solveByElim solveByElimDepth newGoal
-          pure $ some $ Sum.inr ()
+          if (← checkRequired) then
+            pure $ some $ Sum.inr ()
+          else
+            set state0
+            pure none
         catch _ =>
           let res := some $ Sum.inl (← getMCtx, newGoals)
-          set state0
-          pure res)
+          if (← checkRequired) then
+            set state0
+            pure res
+          else
+            set state0
+            pure none)
     catch _ =>
       set state0
       pure none
@@ -90,6 +117,8 @@ def librarySearch (goal : MVarId) (lemmas : DiscrTree Name) (solveByElimDepth :=
     | some (Sum.inl suggestion) => suggestions := suggestions.push suggestion
 
   pure $ some suggestions
+    where
+  checkRequired : MetaM Bool := do pure <| required.all (·.occurs (← instantiateMVars (.mvar goal)))
 
 def lines (ls : List MessageData) :=
   MessageData.joinSep ls (MessageData.ofFormat Format.line)
@@ -98,22 +127,22 @@ open Lean.Parser.Tactic
 
 -- TODO: implement the additional options for `library_search` from Lean 3,
 -- in particular including additional lemmas
--- with `library_search [X, Y, Z]` or `library_search with attr`,
--- or requiring that a particular hypothesis is used in the solution, with `library_search using h`.
+-- with `library_search [X, Y, Z]` or `library_search with attr`.
 syntax (name := librarySearch') "library_search" (config)? (simpArgs)?
-  (" using " (colGt binderIdent)+)? : tactic
+  (" using " (colGt term),+)? : tactic
 syntax (name := librarySearch!) "library_search!" (config)? (simpArgs)?
-  (" using " (colGt binderIdent)+)? : tactic
+  (" using " (colGt term),+)? : tactic
 
 -- For now we only implement the basic functionality.
 -- The full syntax is recognized, but will produce a "Tactic has not been implemented" error.
 
 open Elab.Tactic Elab Tactic in
-elab_rules : tactic | `(tactic| library_search%$tk) => do
+elab_rules : tactic | `(tactic| library_search%$tk $[using $[$required:term],*]?) => do
   let mvar ← getMainGoal
   let (_, goal) ← (← getMainGoal).intros
   goal.withContext do
-    if let some suggestions ← librarySearch goal (← librarySearchLemmas.get) then
+    let required ← (required.getD {}).toList.mapM (elabTerm ·.raw none)
+    if let some suggestions ← librarySearch goal (← librarySearchLemmas.get) required then
       for suggestion in suggestions do
         withMCtx suggestion.1 do
           addExactSuggestion tk (← instantiateMVars (mkMVar mvar))
@@ -126,7 +155,7 @@ elab tk:"library_search%" : term <= expectedType => do
   let goal ← mkFreshExprMVar expectedType
   let (_, introdGoal) ← goal.mvarId!.intros
   introdGoal.withContext do
-    if let some suggestions ← librarySearch introdGoal (← librarySearchLemmas.get) then
+    if let some suggestions ← librarySearch introdGoal (← librarySearchLemmas.get) [] then
       for suggestion in suggestions do
         withMCtx suggestion.1 do
           addTermSuggestion tk (← instantiateMVars goal)
