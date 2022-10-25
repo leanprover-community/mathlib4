8ccb9c60a601298ae0966b5d43dcc7ffed922082
Jakob von Raumer <jakob@von-raumer.de>
2022-10-25 15:18:15 +0200

    lint and align

--- Mathlib/Data/Option/Defs.lean
+++ Mathlib/Data/Option/Defs.lean
@@ -26,7 +26,7 @@ def lift_or_get (f : α → α → α) : Option α → Option α → Option α
 | none, some b => some b
 | some a, some b => some (f a b)
 
-/-- Lifts a relation `α → β → Prop` to a relation `option α → option β → Prop` by just adding
+/-- Lifts a relation `α → β → Prop` to a relation `Option α → Option β → Prop` by just adding
 `none ~ none`. -/
 inductive rel (r : α → β → Prop) : Option α → Option β → Prop
 | /-- If `a ~ b`, then `some a ~ some b` -/
@@ -34,19 +34,20 @@ inductive rel (r : α → β → Prop) : Option α → Option β → Prop
 | /-- `none ~ none` -/
   none : rel r none none
 
+/-- Traverse an object of `Option α` with a function `f : α → F β` for an applicative `F`. -/
 protected def traverse.{u, v} {F : Type u → Type v} [Applicative F] {α β : Type _} (f : α → F β) :
   Option α → F (Option β)
 | none => pure none
 | some x => some <$> f x
 
-/-- If you maybe have a monadic computation in a `[monad m]` which produces a term of type `α`, then
-there is a naturally associated way to always perform a computation in `m` which maybe produces a
-result. -/
+/-- If you maybe have a monadic computation in a `[Monad m]` which produces a term of type `α`,
+then there is a naturally associated way to always perform a computation in `m` which maybe
+produces a result. -/
 def maybe.{u, v} {m : Type u → Type v} [Monad m] {α : Type u} : Option (m α) → m (Option α)
 | none => pure none
 | some fn => some <$> fn
 
-/-- Map a monadic function `f : α → m β` over an `o : option α`, maybe producing a result. -/
+/-- Map a monadic function `f : α → m β` over an `o : Option α`, maybe producing a result. -/
 def mmap.{u, v, w} {m : Type u → Type v} [Monad m] {α : Type w} {β : Type u} (f : α → m β)
     (o : Option α) : m (Option β) :=
   (o.map f).maybe
@@ -65,12 +66,13 @@ variable {α : Type _} {β : Type _}
 -- Porting note: Would need to add the attribute directly in `Init.Prelude`.
 -- attribute [inline] Option.isSome Option.isNone
 
--- Porting note: `Option.elim` already present with different argument order.
-/-- An elimination principle for `option`. It is a nondependent version of `option.rec`. -/
+/-- An elimination principle for `Option`. It is a nondependent version of `option.rec`. -/
 @[simp]
 protected def elim' (b : β) (f : α → β) : Option α → β
   | some a => f a
   | none => b
+
+#align option.elim Option.elim'
 
 theorem is_none_iff_eq_none {o : Option α} : o.isNone = true ↔ o = none :=
   ⟨Option.eq_none_of_isNone, fun e => e.symm ▸ rfl⟩
@@ -85,11 +87,15 @@ Try to use `o.isNone` or `o.isSome` instead.
 def decidableEqNone {o : Option α} : Decidable (o = none) :=
   decidable_of_decidable_of_iff is_none_iff_eq_none
 
-instance decidableForallMem {p : α → Prop} [DecidablePred p] : ∀ o : Option α, Decidable (∀ a ∈ o, p a)
+instance decidableForallMem {p : α → Prop} [DecidablePred p] :
+    ∀ o : Option α, Decidable (∀ a ∈ o, p a)
   | none => isTrue (by simp [false_imp_iff])
-  | some a => if h : p a then isTrue fun o e => some_inj.1 e ▸ h else isFalse <| mt (fun H => H _ rfl) h
+  | some a =>
+      if h : p a then isTrue fun o e => some_inj.1 e ▸ h
+      else isFalse <| mt (fun H => H _ rfl) h
 
-instance decidableExistsMem {p : α → Prop} [DecidablePred p] : ∀ o : Option α, Decidable (∃ a ∈ o, p a)
+instance decidableExistsMem {p : α → Prop} [DecidablePred p] :
+    ∀ o : Option α, Decidable (∃ a ∈ o, p a)
   | none => isFalse fun ⟨a, ⟨h, _⟩⟩ => by cases h
   | some a => if h : p a then isTrue <| ⟨_, rfl, h⟩ else isFalse fun ⟨_, ⟨rfl, hn⟩⟩ => h hn
 
@@ -109,13 +115,16 @@ theorem mem_to_list {a : α} {o : Option α} : a ∈ toList o ↔ a ∈ o := by
   cases o <;> simp [toList, eq_comm]
 
 -- lift f
-instance lift_or_get_comm (f : α → α → α) [h : IsCommutative α f] : IsCommutative (Option α) (liftOrGet f) :=
+instance lift_or_get_comm (f : α → α → α) [h : IsCommutative α f] :
+    IsCommutative (Option α) (liftOrGet f) :=
   ⟨fun a b => by cases a <;> cases b <;> simp [lift_or_get, IsCommutative.comm]⟩
 
-instance lift_or_get_assoc (f : α → α → α) [h : IsAssociative α f] : IsAssociative (Option α) (liftOrGet f) :=
+instance lift_or_get_assoc (f : α → α → α) [h : IsAssociative α f] :
+    IsAssociative (Option α) (liftOrGet f) :=
   ⟨fun a b c => by cases a <;> cases b <;> cases c <;> simp [lift_or_get, IsAssociative.assoc]⟩
 
-instance lift_or_get_idem (f : α → α → α) [h : IsIdempotent α f] : IsIdempotent (Option α) (liftOrGet f) :=
+instance lift_or_get_idem (f : α → α → α) [h : IsIdempotent α f] :
+    IsIdempotent (Option α) (liftOrGet f) :=
   ⟨fun a => by cases a <;> simp [lift_or_get, IsIdempotent.idempotent]⟩
 
 instance lift_or_get_is_left_id (f : α → α → α) : IsLeftId (Option α) (liftOrGet f) none :=
