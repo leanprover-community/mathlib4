68398f568f3b85d1adcee6d962d4c91b9f0a573f
Scott Morrison <scott.morrison@gmail.com>
2022-10-26 16:01:51 +1100

    remove redundant @[simp]

--- Mathlib/Data/Option/Basic.lean
+++ Mathlib/Data/Option/Basic.lean
@@ -20,10 +20,10 @@ theorem map_injective {f : α → β} (Hf : Function.Injective f) : Function.Inj
 -- theorem join_eq_none {o : Option (Option α)} : o.join = none ↔ o = none ∨ o = some none := by
 --   rcases o with _|_|_; simp
 
-@[simp] theorem lift_or_get_none_left {f} {b : Option α} : liftOrGet f none b = b := by
+theorem lift_or_get_none_left {f} {b : Option α} : liftOrGet f none b = b := by
   cases b <;> rfl
 
-@[simp] theorem lift_or_get_none_right {f} {a : Option α} : liftOrGet f a none = a := by
+theorem lift_or_get_none_right {f} {a : Option α} : liftOrGet f a none = a := by
   cases a <;> rfl
 
 @[simp] theorem lift_or_get_some_some {f} {a b : α} :
