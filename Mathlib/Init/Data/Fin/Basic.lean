2baa40ce5777e3a650675d946d1301cb262b7713
Scott Morrison <scott.morrison@gmail.com>
2022-10-30 11:09:41 +1100

    lint

--- Mathlib/Init/Data/Fin/Basic.lean
+++ Mathlib/Init/Data/Fin/Basic.lean
@@ -1,9 +1,13 @@
 /-
 Copyright (c) 2016 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Author: Leonardo de Moura
+Authors: Leonardo de Moura
 -/
 import Mathlib.Init.Data.Nat.Basic
+
+/-!
+# Theorems about equality in `Fin`.
+-/
 
 namespace Fin
 
