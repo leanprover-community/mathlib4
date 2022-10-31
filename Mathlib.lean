42d80ac1aefd80164550b31a4795c09ba24be853
Moritz Doll <moritz.doll@googlemail.com>
2022-10-31 23:16:16 +0000

    feat: port `zify` (#517)
    Initial port of `zify`. The tests are not really good, this is due to the fact that the relevant `norm_cast` lemmas do not exist yet.
    
    Co-authored-by: Scott Morrison <scott.morrison@gmail.com>
    Co-authored-by: Mario Carneiro <di.gama@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -158,6 +158,8 @@ import Mathlib.Tactic.Trace
 import Mathlib.Tactic.TypeCheck
 import Mathlib.Tactic.UnsetOption
 import Mathlib.Tactic.Use
+import Mathlib.Tactic.Zify
+import Mathlib.Tactic.Zify.Attr
 import Mathlib.Testing.SlimCheck.Gen
 import Mathlib.Testing.SlimCheck.Sampleable
 import Mathlib.Testing.SlimCheck.Testable
