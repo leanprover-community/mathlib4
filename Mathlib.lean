243f55c8ef1cf04343c699e47eaaf5dfc9443940
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2022-11-01 22:54:12 +0000

    chore(init/data/int): move lemmas (#525)
    The file `Init.Data.Int.Basic` contained three declarations:
    * `Int.LinearOrder`
    * `Int.coe_nat_le`
    * `Int.coe_nat_lt`
    
    In mathlib3 all three of these were in other files: the first in `Init.Data.Int.Order` and the two last in `Data.Int.Basic`.  I propose moving them to the corresponding mathlib4 files so that people know where to look for them.
    
    [Zulip](https://leanprover.zulipchat.com/#narrow/stream/287929-mathlib4/topic/Porting.20status.20for.20lean3.20core/near/306899172)

--- Mathlib.lean
+++ Mathlib.lean
@@ -59,8 +59,8 @@ import Mathlib.Init.Algebra.Order
 import Mathlib.Init.Align
 import Mathlib.Init.Classical
 import Mathlib.Init.Core
-import Mathlib.Init.Data.Int.Basic
 import Mathlib.Init.Data.Int.Notation
+import Mathlib.Init.Data.Int.Order
 import Mathlib.Init.Data.Nat.Basic
 import Mathlib.Init.Data.Nat.Lemmas
 import Mathlib.Init.Function
