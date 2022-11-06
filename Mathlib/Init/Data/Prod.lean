4754eab35e5b7c33381da86e2670dbd98a875838
Yakov Pechersky <ypechersky@treeline.bio>
2022-11-06 11:16:08 -0500

    Authors instead of Author

--- Mathlib/Init/Data/Prod.lean
+++ Mathlib/Init/Data/Prod.lean
@@ -1,10 +1,12 @@
 /-
 Copyright (c) 2014 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Author: Leonardo de Moura, Jeremy Avigad
+Authors: Leonardo de Moura, Jeremy Avigad
 -/
 prelude
 import Mathlib.Init.Logic
+
+/-! ### alignments from lean 3 `init.data.prod` -/
 
 universe u v
 
