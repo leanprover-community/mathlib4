1a1fe574825f005cf17652c7db1e5a4f0321955d
Yakov Pechersky <ypechersky@treeline.bio>
2022-11-06 11:44:51 -0500

    Bump imports

--- Mathlib/Order/Basic.lean
+++ Mathlib/Order/Basic.lean
@@ -3,7 +3,7 @@ Copyright (c) 2014 Jeremy Avigad. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jeremy Avigad, Mario Carneiro
 -/
-import Mathlib.Data.Prod
+import Mathlib.Data.Prod.Basic
 import Mathlib.Data.Subtype
 import Mathlib.Init.Algebra.Order
 import Mathlib.Init.ZeroOne
@@ -679,7 +679,7 @@ instance (α : Type u) (β : Type v) [PartialOrder α] [PartialOrder β] :
   PartialOrder (α × β) :=
 { toPreorder := instPreorderProd α β
   le_antisymm := λ _ _ ⟨hac, hbd⟩ ⟨hca, hdb⟩ =>
-    Prod.ext' (hac.antisymm hca) (hbd.antisymm hdb)
+    Prod.extₓ (hac.antisymm hca) (hbd.antisymm hdb)
 }
 
 end Prod
