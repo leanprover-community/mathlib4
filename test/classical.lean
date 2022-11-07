0596273755fc59557509dd612ec9d6bec832464c
Mario Carneiro <di.gama@gmail.com>
2022-11-06 20:18:24 -0800

    Apply suggestions from code review
    Co-authored-by: Scott Morrison <scott@tqft.net>

--- test/classical.lean
+++ test/classical.lean
@@ -1,4 +1,5 @@
 import Mathlib.Tactic.Classical
+import Mathlib.Tactic.PermuteGoals
 import Std.Tactic.GuardExpr
 
 noncomputable def foo : Bool := by
@@ -19,4 +20,13 @@ def bar : Bool := by
 -- double check no leakage
 def bar' : Bool := by
   fail_if_success have := ∀ p, decide p -- no classical in scope
+  exact decide (0 < 1) -- uses the decidable instance
+  
+-- check that classical respects tactic blocks
+def bar'' : Bool := by
+  fail_if_success have := ∀ p, decide p -- no classical in scope
+  on_goal 1 =>
+    classical
+    have := ∀ p, decide p -- uses the classical instance
+  fail_if_success have := ∀ p, decide p -- no classical in scope again
   exact decide (0 < 1) -- uses the decidable instance
