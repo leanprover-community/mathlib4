7b89140529807bfe66519440b87bb3c191ec174c
Yakov Pechersky <ypechersky@treeline.bio>
2022-11-08 22:03:24 -0500

    feat: port Order.Synonym

--- Mathlib.lean
+++ Mathlib.lean
@@ -102,6 +102,7 @@ import Mathlib.Mathport.Rename
 import Mathlib.Mathport.Syntax
 import Mathlib.Order.Basic
 import Mathlib.Order.Monotone
+import Mathlib.Order.Synonym
 import Mathlib.Tactic.Abel
 import Mathlib.Tactic.Alias
 import Mathlib.Tactic.ApplyFun
