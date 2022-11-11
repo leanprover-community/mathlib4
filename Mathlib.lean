715fee1e1433f2376eb8e734da70ac3bf55171a7
Yakov Pechersky <ypechersky@treeline.bio>
2022-11-10 19:51:35 -0500

    feat: port Order.Max

--- Mathlib.lean
+++ Mathlib.lean
@@ -103,6 +103,7 @@ import Mathlib.Mathport.Attributes
 import Mathlib.Mathport.Rename
 import Mathlib.Mathport.Syntax
 import Mathlib.Order.Basic
+import Mathlib.Order.Max
 import Mathlib.Order.Monotone
 import Mathlib.Order.Synonym
 import Mathlib.Tactic.Abel
