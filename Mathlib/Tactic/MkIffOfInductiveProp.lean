b4c5ef62aa88e21bd3888dca89d1b75cd497ed6d
David Renshaw <dwrenshaw@gmail.com>
2022-11-10 22:36:07 -0500

    remove unneeded import of Mathlib.Tactic.Existsi

--- Mathlib/Tactic/MkIffOfInductiveProp.lean
+++ Mathlib/Tactic/MkIffOfInductiveProp.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl, David Renshaw
 -/
 import Lean
-import Mathlib.Tactic.Existsi
 import Mathlib.Tactic.LeftRight
 
 /-!
