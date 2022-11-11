6fcbcacff4a7951690218ae90b1b4be572d651af
Yakov Pechersky <pechersky@users.noreply.github.com>
2022-11-10 20:23:29 -0500

    Prod is now as Prod.Basic

--- Mathlib/Logic/Nontrivial.lean
+++ Mathlib/Logic/Nontrivial.lean
@@ -3,7 +3,7 @@ Copyright (c) 2020 Sébastien Gouëzel. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Sébastien Gouëzel
 -/
-import Mathlib.Data.Prod
+import Mathlib.Data.Prod.Basic
 import Mathlib.Data.Subtype
 import Mathlib.Logic.Function.Basic
 import Mathlib.Logic.Unique
