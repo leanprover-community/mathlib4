5f8ae4c224f9a6196aa207e75c06c02022ebd70f
Scott Morrison <scott.morrison@gmail.com>
2022-11-14 20:47:31 +1100

    revert change that hides the simp failure

--- Mathlib/Data/Equiv/Functor.lean
+++ Mathlib/Data/Equiv/Functor.lean
@@ -31,8 +31,8 @@ theorem map_map (m : α → β) (g : β → γ) (x : f α) :
 def map_equiv (h : α ≃ β) : f α ≃ f β where
   toFun    := map h
   invFun   := map h.symm
-  left_inv x := by rw [map_map, h.symm_comp_self, id_map]
-  right_inv x := by rw [map_map, h.self_comp_symm, id_map]
+  left_inv x := by simp [map_map]
+  right_inv x := by simp [map_map]
 
 @[simp]
 lemma map_equiv_apply (h : α ≃ β) (x : f α) :
