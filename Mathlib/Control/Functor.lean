a8ae6df5bce3aae7a291e4c51dda8c0e0f699dde
Jireh Loreaux <loreaujy@gmail.com>
2022-11-16 01:42:34 -0600

    update names and docstrings

--- Mathlib/Control/Functor.lean
+++ Mathlib/Control/Functor.lean
@@ -10,13 +10,13 @@ import Std.Tactic.Lint.Misc
 /-!
 # Functors
 
-This module provides additional lemmas, definitions, and instances for `functor`s.
+This module provides additional lemmas, definitions, and instances for `Functor`s.
 
 ## Main definitions
 
-* `const α` is the functor that sends all types to `α`.
-* `add_const α` is `const α` but for when `α` has an additive structure.
-* `comp F G` for functors `F` and `G` is the functor composition of `F` and `G`.
+* `Functor.Const α` is the functor that sends all types to `α`.
+* `Functor.AddConst α` is `Functor.Const α` but for when `α` has an additive structure.
+* `Functor.Comp F G` for functors `F` and `G` is the functor composition of `F` and `G`.
 * `liftp` and `liftr` respectively lift predicates and relations on a type `α`
   to `F α`.  Terms of `F α` are considered to, in some sense, contain values of type `α`.
 
@@ -38,7 +38,8 @@ variable {α β γ : Type u}
 
 variable [Functor F] [LawfulFunctor F]
 
-theorem Functor.map_id : (· <$> ·) id = (id : F α → F α) := by apply funext <;> apply id_map
+theorem Functor.map_id : (· <$> ·) id = (id : F α → F α) := by
+  apply funext <;> apply id_map
 #align functor.map_id Functor.map_id
 
 theorem Functor.map_comp_map (f : α → β) (g : β → γ) :
@@ -62,35 +63,34 @@ theorem Functor.ext {F} :
 end Functor
 
 /-- Introduce the `id` functor. Incidentally, this is `pure` for
-`id` as a `monad` and as an `applicative` functor. -/
+`id` as a `Monad` and as an `Applicative` functor. -/
 def id.mk {α : Sort u} : α → id α :=
   id
-#align id.mk id.mk
 
 namespace Functor
 
-/-- `const α` is the constant functor, mapping every type to `α`. When
-`α` has a monoid structure, `const α` has an `applicative` instance.
-(If `α` has an additive monoid structure, see `functor.add_const`.) -/
+/-- `Const α` is the constant functor, mapping every type to `α`. When
+`α` has a monoid structure, `Const α` has an `Applicative` instance.
+(If `α` has an additive monoid structure, see `Functor.AddConst`.) -/
 @[nolint unusedArguments]
 def Const (α : Type _) (_β : Type _) :=
   α
 #align functor.const Functor.Const
 
-/-- `const.mk` is the canonical map `α → const α β` (the identity), and
+/-- `Const.mk` is the canonical map `α → Const α β` (the identity), and
 it can be used as a pattern to extract this value. -/
 @[match_pattern]
 def Const.mk {α β} (x : α) : Const α β :=
   x
 #align functor.const.mk Functor.Const.mk
 
-/-- `const.mk'` is `const.mk` but specialized to map `α` to
-`const α punit`, where `punit` is the terminal object in `Type*`. -/
+/-- `Const.mk'` is `Const.mk` but specialized to map `α` to
+`Const α PUnit`, where `PUnit` is the terminal object in `Type*`. -/
 def Const.mk' {α} (x : α) : Const α PUnit :=
   x
 #align functor.const.mk' Functor.Const.mk'
 
-/-- Extract the element of `α` from the `const` functor. -/
+/-- Extract the element of `α` from the `Const` functor. -/
 def Const.run {α β} (x : Const α β) : α :=
   x
 #align functor.const.run Functor.Const.run
@@ -101,31 +101,31 @@ protected theorem ext {α β} {x y : Const α β} (h : x.run = y.run) : x = y :=
   h
 #align functor.const.ext Functor.Const.ext
 
-/-- The map operation of the `const γ` functor. -/
+/-- The map operation of the `Const γ` functor. -/
 @[nolint unusedArguments]
 protected def map {γ α β} (_f : α → β) (x : Const γ β) : Const γ α :=
   x
 #align functor.const.map Functor.Const.map
 
-instance {γ} : Functor (Const γ) where map := @Const.map γ
+instance Functor {γ} : Functor (Const γ) where map := @Const.map γ
 
-instance {γ} : LawfulFunctor (Const γ) := by constructor <;> intros <;> rfl
+instance LawfulFunctor {γ} : LawfulFunctor (Const γ) := by constructor <;> intros <;> rfl
 
 instance {α β} [Inhabited α] : Inhabited (Const α β) :=
   ⟨(default : α)⟩
 
 end Const
 
-/-- `add_const α` is a synonym for constant functor `const α`, mapping
+/-- `AddConst α` is a synonym for constant functor `Const α`, mapping
 every type to `α`. When `α` has a additive monoid structure,
-`add_const α` has an `applicative` instance. (If `α` has a
-multiplicative monoid structure, see `functor.const`.) -/
+`AddConst α` has an `Applicative` instance. (If `α` has a
+multiplicative monoid structure, see `Functor.Const`.) -/
 def AddConst (α : Type _) :=
   Const α
 #align functor.add_const Functor.AddConst
 
-/-- `add_const.mk` is the canonical map `α → add_const α β`, which is the identity,
-where `add_const α β = const α β`. It can be used as a pattern to extract this value. -/
+/-- `AddConst.mk` is the canonical map `α → AddConst α β`, which is the identity,
+where `AddConst α β = const α β`. It can be used as a pattern to extract this value. -/
 @[match_pattern]
 def AddConst.mk {α β} (x : α) : AddConst α β :=
   x
@@ -136,32 +136,32 @@ def AddConst.run {α β} : AddConst α β → α :=
   id
 #align functor.add_const.run Functor.AddConst.run
 
-instance AddConst.functor {γ} : Functor (AddConst γ) :=
-  @Const.instFunctorConst γ
-#align functor.add_const.functor Functor.AddConst.functor
+instance AddConst.Functor {γ} : Functor (AddConst γ) :=
+  @Const.Functor γ
+#align functor.add_const.functor Functor.AddConst.Functor
 
-instance AddConst.is_lawful_functor {γ} : LawfulFunctor (AddConst γ) :=
-  @Const.instLawfulFunctorConstInstFunctorConst γ
-#align functor.add_const.is_lawful_functor Functor.AddConst.is_lawful_functor
+instance AddConst.LawfulFunctor {γ} : LawfulFunctor (AddConst γ) :=
+  @Const.LawfulFunctor γ
+#align functor.add_const.is_lawful_functor Functor.AddConst.LawfulFunctor
 
 instance {α β} [Inhabited α] : Inhabited (AddConst α β) :=
   ⟨(default : α)⟩
 
-/-- `functor.comp` is a wrapper around `function.comp` for types.
+/-- `Functor.Comp` is a wrapper around `Function.comp` for types.
     It prevents Lean's type class resolution mechanism from trying
-    a `functor (comp F id)` when `functor F` would do. -/
+    a `Functor (Comp F id)` when `functor F` would do. -/
 def Comp (F : Type u → Type w) (G : Type v → Type u) (α : Type v) : Type w :=
   F <| G α
 #align functor.comp Functor.Comp
 
-/-- Construct a term of `comp F G α` from a term of `F (G α)`, which is the same type.
+/-- Construct a term of `Comp F G α` from a term of `F (G α)`, which is the same type.
 Can be used as a pattern to extract a term of `F (G α)`. -/
 @[match_pattern]
 def Comp.mk {F : Type u → Type w} {G : Type v → Type u} {α : Type v} (x : F (G α)) : Comp F G α :=
   x
 #align functor.comp.mk Functor.Comp.mk
 
-/-- Extract a term of `F (G α)` from a term of `comp F G α`, which is the same type. -/
+/-- Extract a term of `F (G α)` from a term of `Comp F G α`, which is the same type. -/
 def Comp.run {F : Type u → Type w} {G : Type v → Type u} {α : Type v} (x : Comp F G α) : F (G α) :=
   x
 #align functor.comp.run Functor.Comp.run
@@ -179,12 +179,12 @@ instance {α} [Inhabited (F (G α))] : Inhabited (Comp F G α) :=
 
 variable [Functor F] [Functor G]
 
-/-- The map operation for the composition `comp F G` of functors `F` and `G`. -/
+/-- The map operation for the composition `Comp F G` of functors `F` and `G`. -/
 protected def map {α β : Type v} (h : α → β) : Comp F G α → Comp F G β
   | Comp.mk x => Comp.mk ((· <$> ·) h <$> x)
 #align functor.comp.map Functor.Comp.map
 
-instance : Functor (Comp F G) where map := @Comp.map F G _ _
+protected instance Functor : Functor (Comp F G) where map := @Comp.map F G _ _
 
 @[functor_norm]
 theorem map_mk {α β} (h : α → β) (x : F (G α)) : h <$> Comp.mk x = Comp.mk ((· <$> ·) h <$> x) :=
@@ -192,7 +192,8 @@ theorem map_mk {α β} (h : α → β) (x : F (G α)) : h <$> Comp.mk x = Comp.m
 #align functor.comp.map_mk Functor.Comp.map_mk
 
 @[simp]
-protected theorem run_map {α β} (h : α → β) (x : Comp F G α) : (h <$> x).run = (· <$> ·) h <$> x.run :=
+protected theorem run_map {α β} (h : α → β) (x : Comp F G α) :
+    (h <$> x).run = (· <$> ·) h <$> x.run :=
   rfl
 #align functor.comp.run_map Functor.Comp.run_map
 
@@ -206,22 +207,22 @@ protected theorem id_map : ∀ x : Comp F G α, Comp.map id x = x
 
 protected theorem comp_map (g' : α → β) (h : β → γ) :
     ∀ x : Comp F G α, Comp.map (h ∘ g') x = Comp.map h (Comp.map g' x)
-  | Comp.mk x => by simp [Comp.map, functor_norm, Comp.mk, Functor.map_comp_map]
+  | Comp.mk x => by simp [Comp.map, Comp.mk, Functor.map_comp_map, functor_norm]
 #align functor.comp.comp_map Functor.Comp.comp_map
 
-instance : LawfulFunctor (Comp F G) where
+protected instance LawfulFunctor : LawfulFunctor (Comp F G) where
   map_const := rfl
   id_map := @Comp.id_map F G _ _ _ _
   comp_map := @Comp.comp_map F G _ _ _ _
 
 theorem functor_comp_id {F} [AF : Functor F] [LawfulFunctor F] :
-  @Comp.instFunctorComp F Id _ _ = AF :=
-  @Functor.ext F _ AF (@Comp.instLawfulFunctorCompInstFunctorComp F Id _ _ _ _) _ fun _ _ _ _ => rfl
+  @Comp.Functor F Id _ _ = AF :=
+  @Functor.ext F _ AF (@Comp.LawfulFunctor F Id _ _ _ _) _ fun _ _ _ _ => rfl
 #align functor.comp.functor_comp_id Functor.Comp.functor_comp_id
 
 theorem functor_id_comp {F} [AF : Functor F] [LawfulFunctor F] :
-  @Comp.instFunctorComp Id F _ _ = AF :=
-  @Functor.ext F _ AF (@Comp.instLawfulFunctorCompInstFunctorComp Id F _ _ _ _) _ fun _ _ _ _ => rfl
+  @Comp.Functor Id F _ _ = AF :=
+  @Functor.ext F _ AF (@Comp.LawfulFunctor Id F _ _ _ _) _ fun _ _ _ _ => rfl
 #align functor.comp.functor_id_comp Functor.Comp.functor_id_comp
 
 end Comp
@@ -267,13 +268,13 @@ end Comp
 variable {F : Type u → Type u} [Functor F]
 
 /-- If we consider `x : F α` to, in some sense, contain values of type `α`,
-predicate `liftp p x` holds iff every value contained by `x` satisfies `p`. -/
+predicate `Liftp p x` holds iff every value contained by `x` satisfies `p`. -/
 def Liftp {α : Type u} (p : α → Prop) (x : F α) : Prop :=
   ∃ u : F (Subtype p), Subtype.val <$> u = x
 #align functor.liftp Functor.Liftp
 
 /-- If we consider `x : F α` to, in some sense, contain values of type `α`, then
-`liftr r x y` relates `x` and `y` iff (1) `x` and `y` have the same shape and
+`Liftr r x y` relates `x` and `y` iff (1) `x` and `y` have the same shape and
 (2) we can pair values `a` from `x` and `b` from `y` so that `r a b` holds. -/
 def Liftr {α : Type u} (r : α → α → Prop) (x y : F α) : Prop :=
   ∃ u : F { p : α × α // r p.fst p.snd },
