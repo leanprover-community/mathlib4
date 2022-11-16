8a648be75d4775dd4f4df46df1c11ccd2666039e
riccardobrasca <riccardo.brasca@gmail.com>
2022-11-16 15:40:30 +0100

    ops

--- Mathlib/Algebra/PemptyInstances.lean
+++ Mathlib/Algebra/PemptyInstances.lean
@@ -17,7 +17,7 @@ that it is a semigroup.
 universe u
 
 @[to_additive]
-instance SemigroupPempty : Semigroup PEmpty.{u + 1} where
+instance SemigroupPEmpty : Semigroup PEmpty.{u + 1} where
   mul x _ := by cases x
   mul_assoc x y z := by cases x
 
