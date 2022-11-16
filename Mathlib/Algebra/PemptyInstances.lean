213e76e7cde2e36d263703f7814d4648c3b65da9
riccardobrasca <riccardo.brasca@gmail.com>
2022-11-16 15:40:06 +0100

    fix align

--- Mathlib/Algebra/PemptyInstances.lean
+++ Mathlib/Algebra/PemptyInstances.lean
@@ -21,4 +21,4 @@ instance SemigroupPempty : Semigroup PEmpty.{u + 1} where
   mul x _ := by cases x
   mul_assoc x y z := by cases x
 
-#align semigroup_pempty SemigroupPempty
+#align semigroup_pempty SemigroupPEmpty
