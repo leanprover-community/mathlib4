c134c2a3341f359eb6a01b630b23e9ca5ef5a3c7
Scott Morrison <scott.morrison@gmail.com>
2022-11-18 10:01:12 +1100

    looks good

--- Mathlib.lean
+++ Mathlib.lean
@@ -23,6 +23,7 @@ import Mathlib.Control.Writer
 import Mathlib.Data.Array.Basic
 import Mathlib.Data.Array.Defs
 import Mathlib.Data.BinaryHeap
+import Mathlib.Data.Bool.Basic
 import Mathlib.Data.Bracket
 import Mathlib.Data.ByteArray
 import Mathlib.Data.Char
