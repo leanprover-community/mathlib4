330726ac3c80eb856dd7ee5a2375649e10b86390
Jireh Loreaux <loreaujy@gmail.com>
2022-11-17 11:52:38 -0600

    fix import order

--- Mathlib.lean
+++ Mathlib.lean
@@ -17,8 +17,8 @@ import Mathlib.Algebra.Order.Ring
 import Mathlib.Algebra.Ring.Basic
 import Mathlib.CategoryTheory.ConcreteCategory.Bundled
 import Mathlib.Control.Basic
-import Mathlib.Control.SimpSet
 import Mathlib.Control.Random
+import Mathlib.Control.SimpSet
 import Mathlib.Control.Writer
 import Mathlib.Data.Array.Basic
 import Mathlib.Data.Array.Defs
