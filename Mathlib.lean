c8ae183d1e316cd0096d6724f39996c4c2f9e967
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2022-11-18 10:39:06 +0100

    Update Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -115,6 +115,7 @@ import Mathlib.Mathport.Rename
 import Mathlib.Mathport.Syntax
 import Mathlib.Order.Basic
 import Mathlib.Order.Compare
+import Mathlib.Order.Lattice
 import Mathlib.Order.Max
 import Mathlib.Order.Monotone
 import Mathlib.Order.RelClasses
