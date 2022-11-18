5d4c1e496b12f57a385536e5dcde0436d9c048bc
Joël Riou <joel.riou@universite-paris-saclay.fr>
2022-11-18 09:51:33 +0100

    removed unnecessary align commands

--- Mathlib/Algebra/Homology/ComplexShape.lean
+++ Mathlib/Algebra/Homology/ComplexShape.lean
@@ -64,7 +64,6 @@ structure ComplexShape (ι : Type _) where
   next_eq : ∀ {i j j'}, Rel i j → Rel i j' → j = j'
   /-- There is at most one nonzero differential to `X j`. -/
   prev_eq : ∀ {i i' j}, Rel i j → Rel i' j → i = i'
-#align complex_shape ComplexShape
 
 namespace ComplexShape
 
@@ -79,7 +78,6 @@ def refl (ι : Type _) : ComplexShape ι where
   Rel i j := i = j
   next_eq w w' := w.symm.trans w'
   prev_eq w w' := w.trans w'.symm
-#align complex_shape.refl ComplexShape.refl
 
 /-- The reverse of a `ComplexShape`.
 -/
@@ -88,13 +86,11 @@ def symm (c : ComplexShape ι) : ComplexShape ι where
   Rel i j := c.Rel j i
   next_eq w w' := c.prev_eq w w'
   prev_eq w w' := c.next_eq w w'
-#align complex_shape.symm ComplexShape.symm
 
 @[simp]
 theorem symm_symm (c : ComplexShape ι) : c.symm.symm = c := by
   ext
   simp
-#align complex_shape.symm_symm ComplexShape.symm_symm
 
 /-- The "composition" of two `ComplexShape`s.
 
@@ -113,48 +109,41 @@ def trans (c₁ c₂ : ComplexShape ι) : ComplexShape ι where
     obtain ⟨k', w₁', w₂'⟩ := w'
     rw [c₂.prev_eq w₂ w₂'] at w₁
     exact c₁.prev_eq w₁ w₁'
-#align complex_shape.trans ComplexShape.trans
 
 instance subsingleton_next (c : ComplexShape ι) (i : ι) : Subsingleton { j // c.Rel i j } := by
   constructor
   rintro ⟨j, rij⟩ ⟨k, rik⟩
   congr
   exact c.next_eq rij rik
-#align complex_shape.subsingleton_next ComplexShape.subsingleton_next
 
 instance subsingleton_prev (c : ComplexShape ι) (j : ι) : Subsingleton { i // c.Rel i j } := by
   constructor
   rintro ⟨i, rik⟩ ⟨j, rjk⟩
   congr
   exact c.prev_eq rik rjk
-#align complex_shape.subsingleton_prev ComplexShape.subsingleton_prev
 
 /-- An arbitary choice of index `j` such that `Rel i j`, if such exists.
 Returns `i` otherwise.
 -/
 def next (c : ComplexShape ι) (i : ι) : ι :=
   if h : ∃ j, c.Rel i j then h.choose else i
-#align complex_shape.next ComplexShape.next
 
 /-- An arbitary choice of index `i` such that `Rel i j`, if such exists.
 Returns `j` otherwise.
 -/
 def prev (c : ComplexShape ι) (j : ι) : ι :=
   if h : ∃ i, c.Rel i j then h.choose else j
-#align complex_shape.prev ComplexShape.prev
 
 theorem next_eq' (c : ComplexShape ι) {i j : ι} (h : c.Rel i j) : c.next i = j := by
   apply c.next_eq _ h
   rw [next]
   rw [dif_pos]
   exact Exists.choose_spec ⟨j, h⟩
-#align complex_shape.next_eq' ComplexShape.next_eq'
 
 theorem prev_eq' (c : ComplexShape ι) {i j : ι} (h : c.Rel i j) : c.prev j = i := by
   apply c.prev_eq _ h
   rw [prev, dif_pos]
   exact Exists.choose_spec (⟨i, h⟩ : ∃ k, c.Rel k j)
-#align complex_shape.prev_eq' ComplexShape.prev_eq'
 
 /-- The `ComplexShape` allowing differentials from `X i` to `X (i+a)`.
 (For example when `a = 1`, a cohomology theory indexed by `ℕ` or `ℤ`)
@@ -164,7 +153,6 @@ def up' {α : Type _} [AddRightCancelSemigroup α] (a : α) : ComplexShape α wh
   Rel i j := i + a = j
   next_eq hi hj := hi.symm.trans hj
   prev_eq hi hj := add_right_cancel (hi.trans hj.symm)
-#align complex_shape.up' ComplexShape.up'
 
 /-- The `ComplexShape` allowing differentials from `X (j+a)` to `X j`.
 (For example when `a = 1`, a homology theory indexed by `ℕ` or `ℤ`)
@@ -174,29 +162,24 @@ def down' {α : Type _} [AddRightCancelSemigroup α] (a : α) : ComplexShape α 
   Rel i j := j + a = i
   next_eq hi hj := add_right_cancel (hi.trans hj.symm)
   prev_eq hi hj := hi.symm.trans hj
-#align complex_shape.down' ComplexShape.down'
 
 theorem down'_mk {α : Type _} [AddRightCancelSemigroup α] (a : α) (i j : α) (h : j + a = i) :
   (down' a).Rel i j := h
-#align complex_shape.down'_mk ComplexShape.down'_mk
 
 /-- The `ComplexShape` appropriate for cohomology, so `d : X i ⟶ X j` only when `j = i + 1`.
 -/
 @[simps]
 def up (α : Type _) [AddRightCancelSemigroup α] [One α] : ComplexShape α :=
   up' 1
-#align complex_shape.up ComplexShape.up
 
 /-- The `ComplexShape` appropriate for homology, so `d : X i ⟶ X j` only when `i = j + 1`.
 -/
 @[simps]
 def down (α : Type _) [AddRightCancelSemigroup α] [One α] : ComplexShape α :=
   down' 1
-#align complex_shape.down ComplexShape.down
 
 theorem down_mk {α : Type _} [AddRightCancelSemigroup α] [One α] (i j : α) (h : j + 1 = i) :
   (down α).Rel i j :=
   down'_mk (1 : α) i j h
-#align complex_shape.down_mk ComplexShape.down_mk
 
 end ComplexShape
