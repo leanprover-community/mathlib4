614d59974fac97fefee1b93c3ab3cb2da2c7d6b2
Arien Malec <arien.malec@gmail.com>
2022-11-19 21:15:58 -0800

    alphebetize reexports correctly

--- Mathlib.lean
+++ Mathlib.lean
@@ -19,8 +19,8 @@ import Mathlib.Algebra.PEmptyInstances
 import Mathlib.Algebra.Ring.Basic
 import Mathlib.CategoryTheory.ConcreteCategory.Bundled
 import Mathlib.Control.Random
-import Mathlib.Control.Writer
 import Mathlib.Control.ULift
+import Mathlib.Control.Writer
 import Mathlib.Data.Array.Basic
 import Mathlib.Data.Array.Defs
 import Mathlib.Data.BinaryHeap
