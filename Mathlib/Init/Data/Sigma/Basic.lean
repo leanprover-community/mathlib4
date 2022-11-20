0079d8372c5966f93ff5e2c0bb37b63b86c29d79
Scott Morrison <scott.morrison@gmail.com>
2022-11-21 08:38:41 +1100

    linter

--- Mathlib/Init/Data/Sigma/Basic.lean
+++ Mathlib/Init/Data/Sigma/Basic.lean
@@ -1,7 +1,8 @@
 /-
 Copyright (c) 2014 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Author: Leonardo de Moura, Jeremy Avigad, Floris van Doorn
+Authors: Leonardo de Moura, Jeremy Avigad, Floris van Doorn
+Ported by: Scott Morrison
 -/
 import Mathlib.Init.Logic
 
