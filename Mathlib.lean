f7dc75b7717662e1d91c798be86236a6a8edf038
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2022-11-20 16:07:09 +0000

    feat: port Algebra.Quotient (#643)
    Had to do very little. Please let me know if there is something I missed.
    
    Starting mathlib commit: c3019c79074b0619edb4b27553a91b2e82242395

--- Mathlib.lean
+++ Mathlib.lean
@@ -20,6 +20,7 @@ import Mathlib.Algebra.Order.Monoid
 import Mathlib.Algebra.Order.Monoid.Lemmas
 import Mathlib.Algebra.Order.Ring
 import Mathlib.Algebra.PEmptyInstances
+import Mathlib.Algebra.Quotient
 import Mathlib.Algebra.Ring.Basic
 import Mathlib.CategoryTheory.ConcreteCategory.Bundled
 import Mathlib.Control.Random
