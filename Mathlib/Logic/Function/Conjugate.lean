cd40c744ef750eb7fef2e0c0abb512d45ae16f1c
Mario Carneiro <di.gama@gmail.com>
2022-11-20 21:15:49 +0000

    feat: `#noalign` command; `align.precheck` option (#663)
    * The `#noalign` command is like `#align` but it doesn't take a lean 4 name. The semantics are that mathport will delete the theorem which would have been translated at this point, and future references will use `sorry` instead. I am unsure how much we will need this, but it is good for documenting intentional deletions.
    
    * The `#align` command now checks that the lean 4 declaration (after stripping `ₓ`) exists. It still requires that the lean 4 name be fully qualified, but it will tell you if you tried to reference something that was in scope. (I don't want it to do name resolution on its own because this will make it harder to compare with the lean 3 version.) You can disable the check using `set_option align.precheck false`.
    
    * The `mk_iff` command now does name resolution and attaches proper definition info to the input span.
    
    * As expected, the precheck caught a ton of naming bugs in ported files.

--- Mathlib/Logic/Function/Conjugate.lean
+++ Mathlib/Logic/Function/Conjugate.lean
@@ -130,22 +130,22 @@ theorem comp {f' : β → γ} {gc : γ → γ → γ} (hf' : Semiconj₂ f' gb g
 theorem isAssociative_right [IsAssociative α ga] (h : Semiconj₂ f ga gb) (h_surj : Surjective f) :
     IsAssociative β gb :=
   ⟨h_surj.forall₃.2 fun x₁ x₂ x₃ => by simp only [← h.eq, @IsAssociative.assoc _ ga]⟩
-#align semiconj₂.is_associative_right Semiconj₂.isAssociative_right
+#align function.semiconj₂.is_associative_right Function.Semiconj₂.isAssociative_right
 
 theorem isAssociative_left [IsAssociative β gb] (h : Semiconj₂ f ga gb) (h_inj : Injective f) :
     IsAssociative α ga :=
   ⟨fun x₁ x₂ x₃ => h_inj <| by simp only [h.eq, @IsAssociative.assoc _ gb]⟩
-#align semiconj₂.is_associative_left Semiconj₂.isAssociative_left
+#align function.semiconj₂.is_associative_left Function.Semiconj₂.isAssociative_left
 
 theorem isIdempotent_right [IsIdempotent α ga] (h : Semiconj₂ f ga gb) (h_surj : Surjective f) :
     IsIdempotent β gb :=
   ⟨h_surj.forall.2 fun x => by simp only [← h.eq, @IsIdempotent.idempotent _ ga]⟩
-#align semiconj₂.is_idempotent_right Semiconj₂.isIdempotent_right
+#align function.semiconj₂.is_idempotent_right Function.Semiconj₂.isIdempotent_right
 
 theorem isIdempotent_left [IsIdempotent β gb] (h : Semiconj₂ f ga gb) (h_inj : Injective f) :
     IsIdempotent α ga :=
   ⟨fun x => h_inj <| by rw [h.eq, @IsIdempotent.idempotent _ gb]⟩
-#align semiconj₂.is_idempotent_left Semiconj₂.isIdempotent_left
+#align function.semiconj₂.is_idempotent_left Function.Semiconj₂.isIdempotent_left
 
 end Semiconj₂
 
