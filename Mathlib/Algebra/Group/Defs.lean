c4824fa8eeedb45c62d53b8272f64a74a4739102
Scott Morrison <scott.morrison@gmail.com>
2022-11-23 14:39:01 +1100

    add a linink to a Lean 4 issue

--- Mathlib/Algebra/Group/Defs.lean
+++ Mathlib/Algebra/Group/Defs.lean
@@ -254,7 +254,7 @@ attribute [to_additive] MulOneClass
 @[ext, to_additive]
 theorem MulOneClass.ext {M : Type u} : ∀ ⦃m₁ m₂ : MulOneClass M⦄, m₁.mul = m₂.mul → m₁ = m₂ := by
   rintro @⟨⟨one₁⟩, ⟨mul₁⟩, one_mul₁, mul_one₁⟩ @⟨⟨one₂⟩, ⟨mul₂⟩, one_mul₂, mul_one₂⟩ ⟨rfl⟩
-  -- FIXME:
+  -- FIXME (See https://github.com/leanprover/lean4/issues/1711)
   -- congr
   suffices one₁ = one₂ by cases this; rfl
   exact (one_mul₂ one₁).symm.trans (mul_one₁ one₂)
