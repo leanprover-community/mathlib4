6e8e74eaf92c89a5b05d9fa8542bdaa80c9a36a1
Scott Morrison <scott@tqft.net>
2022-11-24 03:51:06 +1100

    Update Mathlib/Logic/Equiv/Basic.lean
    Co-authored-by: Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>

--- Mathlib/Logic/Equiv/Basic.lean
+++ Mathlib/Logic/Equiv/Basic.lean
@@ -387,8 +387,8 @@ theorem emptySum_apply_inr [IsEmpty α] (b : β) : emptySum α β (Sum.inr b) = 
 /-- `Option α` is equivalent to `α ⊕ punit` -/
 def optionEquivSumPUnit (α) : Option α ≃ Sum α PUnit :=
   ⟨fun o => o.elim (inr PUnit.unit) inl, fun s => s.elim some fun _ => none,
-    fun o => by cases o <;> rfl, fun s => by
-      rcases s with (_ | ⟨⟨⟩⟩) <;> rfl⟩
+    fun o => by cases o <;> rfl,
+    fun s => by rcases s with (_ | ⟨⟨⟩⟩) <;> rfl⟩
 #align equiv.option_equiv_sum_punit Equiv.optionEquivSumPUnit
 
 @[simp]
