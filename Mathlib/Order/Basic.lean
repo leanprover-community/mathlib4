fda1e64de0dded1b6a4d3139a94a7b3ed767e77f
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2022-11-25 19:13:32 +0100

    Revert spurious change

--- Mathlib/Order/Basic.lean
+++ Mathlib/Order/Basic.lean
@@ -882,8 +882,7 @@ end Preorder
 /-- The pointwise partial order on a product.
     (The lexicographic ordering is defined in `Order.Lexicographic`, and the instances are
     available via the type synonym `α ×ₗ β = α × β`.) -/
-instance partialOrder (α : Type u) (β : Type v) [PartialOrder α] [PartialOrder β] :
-  PartialOrder (α × β) where
+instance (α : Type u) (β : Type v) [PartialOrder α] [PartialOrder β] : PartialOrder (α × β) where
   __ := inferInstanceAs (Preorder (α × β))
   le_antisymm := fun _ _ ⟨hac, hbd⟩ ⟨hca, hdb⟩ ↦ Prod.ext' (hac.antisymm hca) (hbd.antisymm hdb)
 
