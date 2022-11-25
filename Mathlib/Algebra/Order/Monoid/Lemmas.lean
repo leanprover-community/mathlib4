06d355e676303936829688e8f398291a50825d28
Scott Morrison <scott.morrison@gmail.com>
2022-11-26 09:44:12 +1100

    fix

--- Mathlib/Algebra/Order/Monoid/Lemmas.lean
+++ Mathlib/Algebra/Order/Monoid/Lemmas.lean
@@ -256,40 +256,6 @@ section MulOneClass
 
 variable [MulOneClass α]
 
-<<<<<<< HEAD
-section Preorder
-variable [Preorder α]
-
-@[to_additive]
-theorem mul_lt_of_le_of_lt_one [CovariantClass α α (· * ·) (· < ·)] {a b c : α} (hbc : b ≤ c) (ha : a < 1) :
-    b * a < c :=
-  sorry
-
-@[to_additive]
-theorem mul_lt_of_lt_one_of_le [CovariantClass α α (swap (· * ·)) (· < ·)] {a b c : α} (ha : a < 1) (hbc : b ≤ c) :
-    a * b < c :=
-sorry
-
-/-- Assumes left covariance.
-The lemma assuming right covariance is `right.mul_lt_one`. -/
-@[to_additive "Assumes left covariance.\nThe lemma assuming right covariance is `right.add_neg`."]
-theorem Left.mul_lt_one [CovariantClass α α (· * ·) (· < ·)] {a b : α} (ha : a < 1) (hb : b < 1) : a * b < 1 :=
-  sorry
-
-/-- Assumes left covariance.
-The lemma assuming right covariance is `right.mul_le_one`. -/
-@[to_additive "Assumes left covariance.\nThe lemma assuming right covariance is `right.add_nonpos`."]
-theorem Left.mul_le_one [CovariantClass α α (· * ·) (· ≤ ·)] {a b : α} (ha : a ≤ 1) (hb : b ≤ 1) : a * b ≤ 1 :=
-  sorry
-
-alias Left.mul_le_one ← mul_le_one'
-
-attribute [to_additive add_nonpos "**Alias** of `left.add_nonpos`."] mul_le_one'
-
-end Preorder
-
-end MulOneClass
-=======
 section LE
 
 variable [LE α]
@@ -1505,4 +1471,3 @@ theorem bit0_strict_mono [CovariantClass α α (· + ·) (· < ·)]
 #align bit0_strict_mono bit0_strict_mono
 
 end Bit
->>>>>>> origin/master
