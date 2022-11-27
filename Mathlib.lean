e57c4dba598cf984ddacf581b73d88177bfb89f1
Jireh Loreaux <loreaujy@gmail.com>
2022-11-27 17:54:07 -0600

    add import

--- Mathlib.lean
+++ Mathlib.lean
@@ -15,6 +15,7 @@ import Mathlib.Algebra.GroupPower.Lemmas
 import Mathlib.Algebra.GroupWithZero.Basic
 import Mathlib.Algebra.GroupWithZero.Defs
 import Mathlib.Algebra.GroupWithZero.InjSurj
+import Mathlib.Algebra.GroupWithZero.Semiconj
 import Mathlib.Algebra.GroupWithZero.Units.Basic
 import Mathlib.Algebra.HierarchyDesign
 import Mathlib.Algebra.Homology.ComplexShape
