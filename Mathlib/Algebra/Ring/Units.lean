952a151cf1c1df0bd943c2730f7924f67a2b7077
Jireh Loreaux <loreaujy@gmail.com>
2022-11-26 20:39:21 -0600

    finish porting

--- Mathlib/Algebra/Ring/Units.lean
+++ Mathlib/Algebra/Ring/Units.lean
@@ -3,8 +3,8 @@ Copyright (c) 2014 Jeremy Avigad. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jeremy Avigad, Leonardo de Moura, Floris van Doorn, Yury Kudryashov, Neil Strickland
 -/
-import Mathbin.Algebra.Ring.InjSurj
-import Mathbin.Algebra.Group.Units
+import Mathlib.Algebra.Ring.InjSurj
+import Mathlib.Algebra.Group.Units
 
 /-!
 # Units in semirings and rings
@@ -31,9 +31,9 @@ instance : Neg αˣ :=
 /-- Representing an element of a ring's unit group as an element of the ring commutes with
     mapping this element to its additive inverse. -/
 @[simp, norm_cast]
-protected theorem coe_neg (u : αˣ) : (↑(-u) : α) = -u :=
+protected theorem val_neg (u : αˣ) : (↑(-u) : α) = -u :=
   rfl
-#align units.coe_neg Units.coe_neg
+#align units.coe_neg Units.val_neg
 
 @[simp, norm_cast]
 protected theorem coe_neg_one : ((-1 : αˣ) : α) = -1 :=
@@ -41,7 +41,7 @@ protected theorem coe_neg_one : ((-1 : αˣ) : α) = -1 :=
 #align units.coe_neg_one Units.coe_neg_one
 
 instance : HasDistribNeg αˣ :=
-  Units.ext.HasDistribNeg _ Units.coe_neg Units.coe_mul
+  Units.ext.hasDistribNeg _ Units.val_neg Units.val_mul
 
 @[field_simps]
 theorem neg_divp (a : α) (u : αˣ) : -(a /ₚ u) = -a /ₚ u := by simp only [divp, neg_mul]
@@ -54,7 +54,8 @@ section Ring
 variable [Ring α] {a b : α}
 
 @[field_simps]
-theorem divp_add_divp_same (a b : α) (u : αˣ) : a /ₚ u + b /ₚ u = (a + b) /ₚ u := by simp only [divp, add_mul]
+theorem divp_add_divp_same (a b : α) (u : αˣ) : a /ₚ u + b /ₚ u = (a + b) /ₚ u := by
+  simp only [divp, add_mul]
 #align units.divp_add_divp_same Units.divp_add_divp_same
 
 @[field_simps]
@@ -80,7 +81,7 @@ theorem divp_add (a b : α) (u : αˣ) : a /ₚ u + b = (a + b * u) /ₚ u := by
 @[field_simps]
 theorem divp_sub (a b : α) (u : αˣ) : a /ₚ u - b = (a - b * u) /ₚ u := by
   simp only [divp, sub_mul, sub_right_inj]
-  assoc_rw [Units.mul_inv, mul_one]
+  rw [mul_assoc, Units.mul_inv, mul_one]
 #align units.divp_sub Units.divp_sub
 
 end Ring
@@ -88,7 +89,7 @@ end Ring
 end Units
 
 theorem IsUnit.neg [Monoid α] [HasDistribNeg α] {a : α} : IsUnit a → IsUnit (-a)
-  | ⟨x, hx⟩ => hx ▸ (-x).IsUnit
+  | ⟨x, hx⟩ => hx ▸ (-x).isUnit
 #align is_unit.neg IsUnit.neg
 
 @[simp]
@@ -103,16 +104,19 @@ theorem IsUnit.sub_iff [Ring α] {x y : α} : IsUnit (x - y) ↔ IsUnit (y - x) 
 namespace Units
 
 @[field_simps]
-theorem divp_add_divp [CommRing α] (a b : α) (u₁ u₂ : αˣ) : a /ₚ u₁ + b /ₚ u₂ = (a * u₂ + u₁ * b) /ₚ (u₁ * u₂) := by
-  simp only [divp, add_mul, mul_inv_rev, coe_mul]
+theorem divp_add_divp [CommRing α] (a b : α) (u₁ u₂ : αˣ) :
+    a /ₚ u₁ + b /ₚ u₂ = (a * u₂ + u₁ * b) /ₚ (u₁ * u₂) := by
+  simp only [divp, add_mul, mul_inv_rev, val_mul]
   rw [mul_comm (↑u₁ * b), mul_comm b]
-  assoc_rw [mul_inv, mul_inv, mul_one, mul_one]
+  rw [←mul_assoc, ←mul_assoc, mul_assoc a, mul_assoc (↑u₂⁻¹ : α), mul_inv, inv_mul, mul_one,
+    mul_one]
+  -- porting note: `assoc_rw` not ported: `assoc_rw [mul_inv, mul_inv, mul_one, mul_one]`
 #align units.divp_add_divp Units.divp_add_divp
 
 @[field_simps]
-theorem divp_sub_divp [CommRing α] (a b : α) (u₁ u₂ : αˣ) : a /ₚ u₁ - b /ₚ u₂ = (a * u₂ - u₁ * b) /ₚ (u₁ * u₂) := by
-  simp_rw [sub_eq_add_neg, neg_divp, divp_add_divp, mul_neg]
+theorem divp_sub_divp [CommRing α] (a b : α) (u₁ u₂ : αˣ) :
+    a /ₚ u₁ - b /ₚ u₂ = (a * u₂ - u₁ * b) /ₚ (u₁ * u₂) := by
+  simp only [sub_eq_add_neg, neg_divp, divp_add_divp, mul_neg]
 #align units.divp_sub_divp Units.divp_sub_divp
 
 end Units
-
