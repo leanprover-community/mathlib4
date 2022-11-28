426373dc18374fbebb10366f93faea5e1ae46f07
Jireh Loreaux <loreaujy@gmail.com>
2022-11-27 21:28:54 -0600

    make it compile

--- Mathlib/Algebra/GroupWithZero/Commute.lean
+++ Mathlib/Algebra/GroupWithZero/Commute.lean
@@ -3,12 +3,12 @@ Copyright (c) 2020 Johan Commelin. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johan Commelin
 -/
-import Mathbin.Algebra.GroupWithZero.Semiconj
-import Mathbin.Algebra.Group.Commute
-import Mathbin.Tactic.Nontriviality
+import Mathlib.Algebra.GroupWithZero.Semiconj
+import Mathlib.Algebra.Group.Commute
+import Mathlib.Tactic.Nontriviality
 
 /-!
-# Lemmas about commuting elements in a `monoid_with_zero` or a `group_with_zero`.
+# Lemmas about commuting elements in a `MonoidWithZero` or a `GroupWithZero`.
 
 -/
 
@@ -21,26 +21,27 @@ namespace Ring
 
 open Classical
 
-theorem mul_inverse_rev' {a b : M₀} (h : Commute a b) : inverse (a * b) = inverse b * inverse a := by
+theorem mul_inverse_rev' {a b : M₀} (h : Commute a b) :
+    inverse (a * b) = inverse b * inverse a := by
   by_cases hab : IsUnit (a * b)
-  · obtain ⟨⟨a, rfl⟩, b, rfl⟩ := h.is_unit_mul_iff.mp hab
-    rw [← Units.coe_mul, inverse_unit, inverse_unit, inverse_unit, ← Units.coe_mul, mul_inv_rev]
-    
-  obtain ha | hb := not_and_distrib.mp (mt h.is_unit_mul_iff.mpr hab)
+  · obtain ⟨⟨a, rfl⟩, b, rfl⟩ := h.isUnit_mul_iff.mp hab
+    rw [← Units.val_mul, inverse_unit, inverse_unit, inverse_unit, ← Units.val_mul, mul_inv_rev]
+  obtain ha | hb := not_and_or.mp (mt h.isUnit_mul_iff.mpr hab)
   · rw [inverse_non_unit _ hab, inverse_non_unit _ ha, mul_zero]
-    
   · rw [inverse_non_unit _ hab, inverse_non_unit _ hb, zero_mul]
-    
 #align ring.mul_inverse_rev' Ring.mul_inverse_rev'
 
-theorem mul_inverse_rev {M₀} [CommMonoidWithZero M₀] (a b : M₀) : Ring.inverse (a * b) = inverse b * inverse a :=
+theorem mul_inverse_rev {M₀} [CommMonoidWithZero M₀] (a b : M₀) :
+    Ring.inverse (a * b) = inverse b * inverse a :=
   mul_inverse_rev' (Commute.all _ _)
 #align ring.mul_inverse_rev Ring.mul_inverse_rev
 
 end Ring
 
-theorem Commute.ring_inverse_ring_inverse {a b : M₀} (h : Commute a b) : Commute (Ring.inverse a) (Ring.inverse b) :=
-  (Ring.mul_inverse_rev' h.symm).symm.trans <| (congr_arg _ h.symm.Eq).trans <| Ring.mul_inverse_rev' h
+theorem Commute.ring_inverse_ring_inverse {a b : M₀} (h : Commute a b) :
+    Commute (Ring.inverse a) (Ring.inverse b) :=
+  (Ring.mul_inverse_rev' h.symm).symm.trans <| (congr_arg _ h.symm.eq).trans <|
+    Ring.mul_inverse_rev' h
 #align commute.ring_inverse_ring_inverse Commute.ring_inverse_ring_inverse
 
 namespace Commute
@@ -54,7 +55,8 @@ Case conversion may be inaccurate. Consider using '#align commute.zero_right Com
 @[simp]
 theorem zero_right [MulZeroClass G₀] (a : G₀) : Commute a 0 :=
   SemiconjBy.zero_right a
-#align commute.zero_right Commute.zero_right
+#align commute.zero_right Commute.zero_rightₓ
+-- mathport expects `Semiring` instead of `MulZeroClass`?
 
 /- warning: commute.zero_left -> Commute.zero_left is a dubious translation:
 lean 3 declaration is
@@ -65,7 +67,8 @@ Case conversion may be inaccurate. Consider using '#align commute.zero_left Comm
 @[simp]
 theorem zero_left [MulZeroClass G₀] (a : G₀) : Commute 0 a :=
   SemiconjBy.zero_left a a
-#align commute.zero_left Commute.zero_left
+#align commute.zero_left Commute.zero_leftₓ
+-- mathport expects `Semiring` instead of `MulZeroClass`?
 
 variable [GroupWithZero G₀] {a b c : G₀}
 
@@ -89,7 +92,7 @@ theorem inv_right₀ (h : Commute a b) : Commute a b⁻¹ :=
 
 @[simp]
 theorem div_right (hab : Commute a b) (hac : Commute a c) : Commute a (b / c) :=
-  hab.div_right hac
+  SemiconjBy.div_right hab hac
 #align commute.div_right Commute.div_right
 
 @[simp]
@@ -99,4 +102,3 @@ theorem div_left (hac : Commute a c) (hbc : Commute b c) : Commute (a / b) c := 
 #align commute.div_left Commute.div_left
 
 end Commute
-
