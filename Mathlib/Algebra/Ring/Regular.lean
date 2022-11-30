49f4ddf7781ef17250d51636952d756464d3f951
Richard Osborn <richardosborn@mac.com>
2022-11-29 21:49:05 -0600

    Update to mathlib4 naming convention

--- Mathlib/Algebra/Ring/Regular.lean
+++ Mathlib/Algebra/Ring/Regular.lean
@@ -13,50 +13,50 @@ import Mathlib.Algebra.Ring.Defs
 
 variable {α : Type _}
 
-/-- Left `mul` by a `k : α` over `[ring α]` is injective, if `k` is not a zero divisor.
-The typeclass that restricts all terms of `α` to have this property is `no_zero_divisors`. -/
-theorem is_left_regular_of_non_zero_divisor [NonUnitalNonAssocRing α] (k : α)
+/-- Left `Mul` by a `k : α` over `[Ring α]` is injective, if `k` is not a zero divisor.
+The typeclass that restricts all terms of `α` to have this property is `NoZeroDivisors`. -/
+theorem isLeftRegular_of_non_zero_divisor [NonUnitalNonAssocRing α] (k : α)
     (h : ∀ x : α, k * x = 0 → x = 0) : IsLeftRegular k := by
   refine' fun x y (h' : k * x = k * y) => sub_eq_zero.mp (h _ _)
   rw [mul_sub, sub_eq_zero, h']
-#align is_left_regular_of_non_zero_divisor is_left_regular_of_non_zero_divisor
+#align is_left_regular_of_non_zero_divisor isLeftRegular_of_non_zero_divisor
 
-/-- Right `mul` by a `k : α` over `[ring α]` is injective, if `k` is not a zero divisor.
-The typeclass that restricts all terms of `α` to have this property is `no_zero_divisors`. -/
-theorem is_right_regular_of_non_zero_divisor [NonUnitalNonAssocRing α] (k : α)
+/-- Right `Mul` by a `k : α` over `[Ring α]` is injective, if `k` is not a zero divisor.
+The typeclass that restricts all terms of `α` to have this property is `NoZeroDivisors`. -/
+theorem isRightRegular_of_non_zero_divisor [NonUnitalNonAssocRing α] (k : α)
     (h : ∀ x : α, x * k = 0 → x = 0) : IsRightRegular k := by
   refine' fun x y (h' : x * k = y * k) => sub_eq_zero.mp (h _ _)
   rw [sub_mul, sub_eq_zero, h']
-#align is_right_regular_of_non_zero_divisor is_right_regular_of_non_zero_divisor
+#align is_right_regular_of_non_zero_divisor isRightRegular_of_non_zero_divisor
 
-theorem is_regular_of_ne_zero' [NonUnitalNonAssocRing α] [NoZeroDivisors α] {k : α} (hk : k ≠ 0) :
+theorem isRegular_of_ne_zero' [NonUnitalNonAssocRing α] [NoZeroDivisors α] {k : α} (hk : k ≠ 0) :
     IsRegular k :=
-  ⟨is_left_regular_of_non_zero_divisor k fun _ h =>
+  ⟨isLeftRegular_of_non_zero_divisor k fun _ h =>
       (NoZeroDivisors.eq_zero_or_eq_zero_of_mul_eq_zero h).resolve_left hk,
-    is_right_regular_of_non_zero_divisor k fun _ h =>
+    isRightRegular_of_non_zero_divisor k fun _ h =>
       (NoZeroDivisors.eq_zero_or_eq_zero_of_mul_eq_zero h).resolve_right hk⟩
-#align is_regular_of_ne_zero' is_regular_of_ne_zero'
+#align is_regular_of_ne_zero' isRegular_of_ne_zero'
 
-theorem is_regular_iff_ne_zero' [Nontrivial α] [NonUnitalNonAssocRing α] [NoZeroDivisors α]
+theorem isRegular_iff_ne_zero' [Nontrivial α] [NonUnitalNonAssocRing α] [NoZeroDivisors α]
     {k : α} : IsRegular k ↔ k ≠ 0 :=
   ⟨fun h => by
     rintro rfl
-    exact not_not.mpr h.left not_isLeftRegular_zero, is_regular_of_ne_zero'⟩
-#align is_regular_iff_ne_zero' is_regular_iff_ne_zero'
+    exact not_not.mpr h.left not_isLeftRegular_zero, isRegular_of_ne_zero'⟩
+#align is_regular_iff_ne_zero' isRegular_iff_ne_zero'
 
-/-- A ring with no zero divisors is a `cancel_monoid_with_zero`.
+/-- A ring with no zero divisors is a `CancelMonoidWithZero`.
 
 Note this is not an instance as it forms a typeclass loop. -/
 @[reducible]
 def NoZeroDivisors.toCancelMonoidWithZero [Ring α] [NoZeroDivisors α] : CancelMonoidWithZero α :=
   { (by infer_instance : MonoidWithZero α) with
     mul_left_cancel_of_ne_zero := fun ha =>
-      @IsRegular.left _ _ _ (is_regular_of_ne_zero' ha) _ _,
+      @IsRegular.left _ _ _ (isRegular_of_ne_zero' ha) _ _,
     mul_right_cancel_of_ne_zero := fun hb =>
-      @IsRegular.right _ _ _ (is_regular_of_ne_zero' hb) _ _ }
+      @IsRegular.right _ _ _ (isRegular_of_ne_zero' hb) _ _ }
 #align no_zero_divisors.to_cancel_monoid_with_zero NoZeroDivisors.toCancelMonoidWithZero
 
-/-- A commutative ring with no zero divisors is a `cancel_comm_monoid_with_zero`.
+/-- A commutative ring with no zero divisors is a `CancelCommMonoidWithZero`.
 
 Note this is not an instance as it forms a typeclass loop. -/
 @[reducible]
