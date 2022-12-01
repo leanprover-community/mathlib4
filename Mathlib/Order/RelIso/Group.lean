c4f69a3734c15a644bf0d6e13f929981dafbdb28
thirdsgames <thirdsgames2018@gmail.com>
2022-12-01 10:45:17 +0000

    Copy from mathport
    Signed-off-by: thirdsgames <thirdsgames2018@gmail.com>

---
+++ Mathlib/Order/RelIso/Group.lean
@@ -0,0 +1,52 @@
+/-
+Copyright (c) 2017 Mario Carneiro. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Mario Carneiro
+-/
+import Mathbin.Algebra.Group.Defs
+import Mathbin.Order.RelIso.Basic
+
+/-!
+# Relation isomorphisms form a group
+-/
+
+
+variable {α : Type _} {r : α → α → Prop}
+
+namespace RelIso
+
+instance : Group (r ≃r r) where
+  one := RelIso.refl r
+  mul f₁ f₂ := f₂.trans f₁
+  inv := RelIso.symm
+  mul_assoc f₁ f₂ f₃ := rfl
+  one_mul f := ext fun _ => rfl
+  mul_one f := ext fun _ => rfl
+  mul_left_inv f := ext f.symm_apply_apply
+
+@[simp]
+theorem coe_one : ⇑(1 : r ≃r r) = id :=
+  rfl
+#align rel_iso.coe_one RelIso.coe_one
+
+@[simp]
+theorem coe_mul (e₁ e₂ : r ≃r r) : ⇑(e₁ * e₂) = e₁ ∘ e₂ :=
+  rfl
+#align rel_iso.coe_mul RelIso.coe_mul
+
+theorem mul_apply (e₁ e₂ : r ≃r r) (x : α) : (e₁ * e₂) x = e₁ (e₂ x) :=
+  rfl
+#align rel_iso.mul_apply RelIso.mul_apply
+
+@[simp]
+theorem inv_apply_self (e : r ≃r r) (x) : e⁻¹ (e x) = x :=
+  e.symm_apply_apply x
+#align rel_iso.inv_apply_self RelIso.inv_apply_self
+
+@[simp]
+theorem apply_inv_self (e : r ≃r r) (x) : e (e⁻¹ x) = x :=
+  e.apply_symm_apply x
+#align rel_iso.apply_inv_self RelIso.apply_inv_self
+
+end RelIso
+
