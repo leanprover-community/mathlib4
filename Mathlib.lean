3fc135870439cc9dfad464e65684884eb751319e
zeramorphic <thirdsgames2018@gmail.com>
2022-12-01 01:59:14 +0000

    feat: port Data.Int.Units (#807)
    mathlib3 SHA: 62a5626868683c104774de8d85b9855234ac807c
    
    I'm a bit confused about the choice of indenting that mathport uses in proofs - I've tried to copy it, but I can't find any documentation to explain a preferred style in Lean 4. I also added a missing `#align` in `Algebra.Group.Units`.
    
    Co-authored-by: Scott Morrison <scott.morrison@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -79,6 +79,7 @@ import Mathlib.Data.Int.Cast
 import Mathlib.Data.Int.Cast.Basic
 import Mathlib.Data.Int.Cast.Defs
 import Mathlib.Data.Int.Order.Basic
+import Mathlib.Data.Int.Units
 import Mathlib.Data.KVMap
 import Mathlib.Data.LazyList
 import Mathlib.Data.List.Basic
