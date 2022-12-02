db4c7d122b5898c282bcaa48d15920c21994543a
Arien Malec <arien.malec@gmail.com>
2022-12-02 14:36:44 -0800

    discovered another align & changed naming

--- Mathlib/Data/List/Defs.lean
+++ Mathlib/Data/List/Defs.lean
@@ -42,16 +42,14 @@ instance [DecidableEq α] : SDiff (List α) :=
 #align list.head' List.head?
 #align list.to_array List.toArray
 
-/-- "default" `nth` function: returns `d` instead of `none` in the case
-  that the index is out of bounds. -/
-@[nolint unusedArguments]
-def getD' (d : α) : ∀ (_ : List α) (_ : ℕ), α
-  | [], _ => d
-  | x :: _, 0 => x
-  | _ :: xs, n + 1 => nthd d xs n
-#align list.nthd List.nthd
+#align list.nthd List.getD
 
-#align list.inth List.getD
+/-- "inhabited" `nth` function: returns `default` instead of `none` in the case
+  that the index is out of bounds. -/
+def getI [Inhabited α] (l : List α) (n : Nat) : α :=
+  nthd default l n
+#align list.inth List.getI
+
 #align list.modify_nth_tail List.modifyNthTail
 #align list.modify_head List.modifyHead
 #align list.modify_nth List.modifyNth
