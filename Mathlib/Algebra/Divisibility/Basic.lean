29f3b39e88fd84a44d838e641f49c22d91c0c7e6
Matej Penciak <96667244+mpenciak@users.noreply.github.com>
2022-12-03 11:59:22 -0500

    Update Mathlib/Algebra/Divisibility/Basic.lean
    Co-authored-by: Scott Morrison <scott@tqft.net>

--- Mathlib/Algebra/Divisibility/Basic.lean
+++ Mathlib/Algebra/Divisibility/Basic.lean
@@ -3,6 +3,7 @@ Copyright (c) 2014 Jeremy Avigad. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jeremy Avigad, Leonardo de Moura, Floris van Doorn, Amelia Livingston, Yury Kudryashov,
 Neil Strickland, Aaron Anderson
+Ported by: Matej Penciak
 -/
 import Mathlib.Algebra.Hom.Group
 
