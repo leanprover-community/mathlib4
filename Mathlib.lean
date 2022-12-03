2816bf4b475f7ab7352c954059b368ed85790765
Frédéric Dupuis <dupuisf@iro.umontreal.ca>
2022-12-02 21:22:22 -0500

    update Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -20,6 +20,7 @@ import Mathlib.Algebra.GroupWithZero.InjSurj
 import Mathlib.Algebra.GroupWithZero.Semiconj
 import Mathlib.Algebra.GroupWithZero.Units.Basic
 import Mathlib.Algebra.HierarchyDesign
+import Mathlib.Algebra.Hom.Commute
 import Mathlib.Algebra.Hom.Embedding
 import Mathlib.Algebra.Hom.Group
 import Mathlib.Algebra.Homology.ComplexShape
