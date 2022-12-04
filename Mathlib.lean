e710a7beba524ff3ae7ed0880ed74e2048b5dbd0
Joël Riou <37772949+joelriou@users.noreply.github.com>
2022-12-04 02:55:00 +0100

    feat: port Combinatorics.Quiver.ConnectedComponent (#836)
    * feat: port Combinatorics.Quiver.ConnectedComponent
    
    * pleased lint

--- Mathlib.lean
+++ Mathlib.lean
@@ -61,6 +61,7 @@ import Mathlib.CategoryTheory.Iso
 import Mathlib.CategoryTheory.NatTrans
 import Mathlib.CategoryTheory.Thin
 import Mathlib.Combinatorics.Quiver.Basic
+import Mathlib.Combinatorics.Quiver.ConnectedComponent
 import Mathlib.Combinatorics.Quiver.Path
 import Mathlib.Combinatorics.Quiver.Subquiver
 import Mathlib.Control.Applicative
