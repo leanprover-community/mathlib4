9b4fff91d7875c93c8e817a9fd2b77501b973cd6
ChrisHughes24 <chrishughes24@gmail.com>
2022-12-06 18:21:11 +0000

    imports

--- Mathlib.lean
+++ Mathlib.lean
@@ -154,6 +154,7 @@ import Mathlib.Data.ULift
 import Mathlib.Data.UnionFind
 import Mathlib.GroupTheory.EckmannHilton
 import Mathlib.GroupTheory.GroupAction.Defs
+import Mathlib.GroupTheory.GroupAction.Sigma
 import Mathlib.Init.Algebra.Classes
 import Mathlib.Init.Algebra.Functions
 import Mathlib.Init.Algebra.Order
