263cad2888a2faecae86d27016778d3c48fdf144
ChrisHughes24 <chrishughes24@gmail.com>
2022-12-06 18:05:19 +0000

    fix build and lint

--- Mathlib/GroupTheory/GroupAction/Sum.lean
+++ Mathlib/GroupTheory/GroupAction/Sum.lean
@@ -3,19 +3,19 @@ Copyright (c) 2022 Yaël Dillies. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yaël Dillies
 -/
-import Mathbin.GroupTheory.GroupAction.Defs
+import Mathlib.GroupTheory.GroupAction.Defs
 
 /-!
 # Sum instances for additive and multiplicative actions
 
-This file defines instances for additive and multiplicative actions on the binary `sum` type.
+This file defines instances for additive and multiplicative actions on the binary `Sum` type.
 
 ## See also
 
-* `group_theory.group_action.option`
-* `group_theory.group_action.pi`
-* `group_theory.group_action.prod`
-* `group_theory.group_action.sigma`
+* `GroupTheory.GroupAction.Option`
+* `GroupTheory.GroupAction.Pi`
+* `GroupTheory.GroupAction.Prod`
+* `GroupTheory.GroupAction.Sigma`
 -/
 
 
@@ -23,13 +23,13 @@ variable {M N P α β γ : Type _}
 
 namespace Sum
 
-section HasSmul
+section SMul
 
-variable [HasSmul M α] [HasSmul M β] [HasSmul N α] [HasSmul N β] (a : M) (b : α) (c : β)
+variable [SMul M α] [SMul M β] [SMul N α] [SMul N β] (a : M) (b : α) (c : β)
   (x : Sum α β)
 
 @[to_additive Sum.hasVadd]
-instance : HasSmul M (Sum α β) :=
+instance : SMul M (Sum α β) :=
   ⟨fun a => Sum.map ((· • ·) a) ((· • ·) a)⟩
 
 @[to_additive]
@@ -51,35 +51,35 @@ theorem smul_inr : a • (inr c : Sum α β) = inr (a • c) :=
 theorem smul_swap : (a • x).swap = a • x.swap := by cases x <;> rfl
 #align sum.smul_swap Sum.smul_swap
 
-instance [HasSmul M N] [IsScalarTower M N α] [IsScalarTower M N β] : IsScalarTower M N (Sum α β) :=
+instance [SMul M N] [IsScalarTower M N α] [IsScalarTower M N β] : IsScalarTower M N (Sum α β) :=
   ⟨fun a b x => by
     cases x
     exacts[congr_arg inl (smul_assoc _ _ _), congr_arg inr (smul_assoc _ _ _)]⟩
 
 @[to_additive]
-instance [SmulCommClass M N α] [SmulCommClass M N β] : SmulCommClass M N (Sum α β) :=
+instance [SMulCommClass M N α] [SMulCommClass M N β] : SMulCommClass M N (Sum α β) :=
   ⟨fun a b x => by
     cases x
     exacts[congr_arg inl (smul_comm _ _ _), congr_arg inr (smul_comm _ _ _)]⟩
 
 @[to_additive]
-instance [HasSmul Mᵐᵒᵖ α] [HasSmul Mᵐᵒᵖ β] [IsCentralScalar M α] [IsCentralScalar M β] :
+instance [SMul Mᵐᵒᵖ α] [SMul Mᵐᵒᵖ β] [IsCentralScalar M α] [IsCentralScalar M β] :
     IsCentralScalar M (Sum α β) :=
   ⟨fun a x => by
     cases x
     exacts[congr_arg inl (op_smul_eq_smul _ _), congr_arg inr (op_smul_eq_smul _ _)]⟩
 
 @[to_additive]
-instance has_faithful_smul_left [HasFaithfulSmul M α] : HasFaithfulSmul M (Sum α β) :=
-  ⟨fun x y h => eq_of_smul_eq_smul fun a : α => by injection h (inl a)⟩
-#align sum.has_faithful_smul_left Sum.has_faithful_smul_left
+instance hasFaithfulSMulLeft [HasFaithfulSMul M α] : HasFaithfulSMul M (Sum α β) :=
+  ⟨fun h => eq_of_smul_eq_smul fun a : α => by injection h (inl a)⟩
+#align sum.has_faithful_smul_left Sum.hasFaithfulSMulLeft
 
 @[to_additive]
-instance has_faithful_smul_right [HasFaithfulSmul M β] : HasFaithfulSmul M (Sum α β) :=
-  ⟨fun x y h => eq_of_smul_eq_smul fun b : β => by injection h (inr b)⟩
-#align sum.has_faithful_smul_right Sum.has_faithful_smul_right
+instance hasFaithfulSMulRight [HasFaithfulSMul M β] : HasFaithfulSMul M (Sum α β) :=
+  ⟨fun h => eq_of_smul_eq_smul fun b : β => by injection h (inr b)⟩
+#align sum.has_faithful_smul_right Sum.hasFaithfulSMulRight
 
-end HasSmul
+end SMul
 
 @[to_additive]
 instance {m : Monoid M} [MulAction M α] [MulAction M β] :
