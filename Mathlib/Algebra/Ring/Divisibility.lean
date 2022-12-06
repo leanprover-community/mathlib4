653d5c3468cb46ebf070abd8b89713adda76e0be
Matej Penciak <96667244+mpenciak@users.noreply.github.com>
2022-12-06 09:19:30 -0500

    Update Mathlib/Algebra/Ring/Divisibility.lean
    Co-authored-by: Scott Morrison <scott@tqft.net>

--- Mathlib/Algebra/Ring/Divisibility.lean
+++ Mathlib/Algebra/Ring/Divisibility.lean
@@ -2,6 +2,7 @@
 Copyright (c) 2014 Jeremy Avigad. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jeremy Avigad, Leonardo de Moura, Floris van Doorn, Yury Kudryashov, Neil Strickland
+Ported by: Matej Penciak
 -/
 import Mathlib.Algebra.Divisibility.Basic
 import Mathlib.Algebra.Ring.Defs
