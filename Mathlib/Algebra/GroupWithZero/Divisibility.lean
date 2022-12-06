5dfc05b32ff52ad8c7f71d486883f8f93f7b3f67
zeramorphic <zeramorphic@proton.me>
2022-12-06 12:01:50 +0000

    Minor fixes
    Signed-off-by: zeramorphic <zeramorphic@proton.me>

--- Mathlib/Algebra/GroupWithZero/Divisibility.lean
+++ Mathlib/Algebra/GroupWithZero/Divisibility.lean
@@ -4,8 +4,8 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jeremy Avigad, Leonardo de Moura, Floris van Doorn, Amelia Livingston, Yury Kudryashov,
 Neil Strickland, Aaron Anderson
 -/
-import Mathbin.Algebra.GroupWithZero.Basic
-import Mathbin.Algebra.Divisibility.Units
+import Mathlib.Algebra.GroupWithZero.Basic
+import Mathlib.Algebra.Divisibility.Units
 
 /-!
 # Divisibility in groups with zero.
@@ -31,8 +31,7 @@ theorem eq_zero_of_zero_dvd (h : 0 ∣ a) : a = 0 :=
 theorem zero_dvd_iff : 0 ∣ a ↔ a = 0 :=
   ⟨eq_zero_of_zero_dvd, fun h => by
     rw [h]
-    use 0
-    simp⟩
+    exact ⟨0, by simp⟩⟩
 #align zero_dvd_iff zero_dvd_iff
 
 @[simp]
@@ -42,14 +41,14 @@ theorem dvd_zero (a : α) : a ∣ 0 :=
 
 end SemigroupWithZero
 
-/-- Given two elements `b`, `c` of a `cancel_monoid_with_zero` and a nonzero element `a`,
+/-- Given two elements `b`, `c` of a `CancelMonoidWithZero` and a nonzero element `a`,
  `a*b` divides `a*c` iff `b` divides `c`. -/
 theorem mul_dvd_mul_iff_left [CancelMonoidWithZero α] {a b c : α} (ha : a ≠ 0) :
     a * b ∣ a * c ↔ b ∣ c :=
   exists_congr fun d => by rw [mul_assoc, mul_right_inj' ha]
 #align mul_dvd_mul_iff_left mul_dvd_mul_iff_left
 
-/-- Given two elements `a`, `b` of a commutative `cancel_monoid_with_zero` and a nonzero
+/-- Given two elements `a`, `b` of a commutative `CancelMonoidWithZero` and a nonzero
   element `c`, `a*c` divides `b*c` iff `a` divides `b`. -/
 theorem mul_dvd_mul_iff_right [CancelCommMonoidWithZero α] {a b c : α} (hc : c ≠ 0) :
     a * c ∣ b * c ↔ a ∣ b :=
@@ -60,40 +59,38 @@ section CommMonoidWithZero
 
 variable [CommMonoidWithZero α]
 
-/-- `dvd_not_unit a b` expresses that `a` divides `b` "strictly", i.e. that `b` divided by `a`
+/-- `DvdNotUnit a b` expresses that `a` divides `b` "strictly", i.e. that `b` divided by `a`
 is not a unit. -/
 def DvdNotUnit (a b : α) : Prop :=
   a ≠ 0 ∧ ∃ x, ¬IsUnit x ∧ b = a * x
 #align dvd_not_unit DvdNotUnit
 
-theorem dvd_not_unit_of_dvd_of_not_dvd {a b : α} (hd : a ∣ b) (hnd : ¬b ∣ a) : DvdNotUnit a b := by
+theorem dvdNotUnit_of_dvd_of_not_dvd {a b : α} (hd : a ∣ b) (hnd : ¬b ∣ a) : DvdNotUnit a b := by
   constructor
   · rintro rfl
     exact hnd (dvd_zero _)
   · rcases hd with ⟨c, rfl⟩
     refine' ⟨c, _, rfl⟩
     rintro ⟨u, rfl⟩
-    simpa using hnd
-#align dvd_not_unit_of_dvd_of_not_dvd dvd_not_unit_of_dvd_of_not_dvd
+    simp at hnd
+#align dvd_not_unit_of_dvd_of_not_dvd dvdNotUnit_of_dvd_of_not_dvd
 
 end CommMonoidWithZero
 
 theorem dvd_and_not_dvd_iff [CancelCommMonoidWithZero α] {x y : α} :
     x ∣ y ∧ ¬y ∣ x ↔ DvdNotUnit x y :=
   ⟨fun ⟨⟨d, hd⟩, hyx⟩ =>
-    ⟨fun hx0 => by simpa [hx0] using hyx,
-      ⟨d, mt is_unit_iff_dvd_one.1 fun ⟨e, he⟩ => hyx ⟨e, by rw [hd, mul_assoc, ← he, mul_one]⟩,
+    ⟨fun hx0 => by simp [hx0] at hyx,
+      ⟨d, mt isUnit_iff_dvd_one.1 fun ⟨e, he⟩ => hyx ⟨e, by rw [hd, mul_assoc, ← he, mul_one]⟩,
         hd⟩⟩,
     fun ⟨hx0, d, hdu, hdx⟩ =>
     ⟨⟨d, hdx⟩, fun ⟨e, he⟩ =>
       hdu
-        (is_unit_of_dvd_one _
-          ⟨e,
-            mul_left_cancel₀ hx0 <| by
-              conv =>
-                  lhs
-                  rw [he, hdx] <;>
-                simp [mul_assoc]⟩)⟩⟩
+        (isUnit_of_dvd_one
+          ⟨e, mul_left_cancel₀ hx0 <| by conv =>
+            lhs
+            rw [he, hdx]
+            simp [mul_assoc]⟩)⟩⟩
 #align dvd_and_not_dvd_iff dvd_and_not_dvd_iff
 
 section MonoidWithZero
@@ -106,4 +103,3 @@ theorem ne_zero_of_dvd_ne_zero {p q : α} (h₁ : q ≠ 0) (h₂ : p ∣ q) : p 
 #align ne_zero_of_dvd_ne_zero ne_zero_of_dvd_ne_zero
 
 end MonoidWithZero
-
