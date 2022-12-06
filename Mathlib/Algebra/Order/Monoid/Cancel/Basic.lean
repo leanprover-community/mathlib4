4b173058f165b1d1dc3139ef59721c1efc8dd7a3
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2022-12-06 19:07:43 +0100

    easy fixes

--- Mathlib/Algebra/Order/Monoid/Cancel/Basic.lean
+++ Mathlib/Algebra/Order/Monoid/Cancel/Basic.lean
@@ -23,7 +23,7 @@ section OrderedCancelCommMonoid
 
 variable [OrderedCancelCommMonoid α]
 
-/-- Pullback an `ordered_cancel_comm_monoid` under an injective map.
+/-- Pullback an `OrderedCancelCommMonoid` under an injective map.
 See note [reducible non-instances]. -/
 @[reducible,
   to_additive Function.Injective.orderedCancelAddCommMonoid
@@ -31,10 +31,12 @@ See note [reducible non-instances]. -/
 def Function.Injective.orderedCancelCommMonoid {β : Type _} [One β] [Mul β] [Pow β ℕ] (f : β → α)
     (hf : Function.Injective f) (one : f 1 = 1) (mul : ∀ x y, f (x * y) = f x * f y)
     (npow : ∀ (x) (n : ℕ), f (x ^ n) = f x ^ n) : OrderedCancelCommMonoid β :=
-  { hf.OrderedCommMonoid f one mul npow with
-    le_of_mul_le_mul_left := fun a b c (bc : f (a * b) ≤ f (a * c)) =>
+  { hf.orderedCommMonoid f one mul npow with
+    le_of_mul_le_mul_left := fun a b c (bc : f (a * b) ≤ f (a * c)) ↦
       (mul_le_mul_iff_left (f a)).mp (by rwa [← mul, ← mul]) }
 #align function.injective.ordered_cancel_comm_monoid Function.Injective.orderedCancelCommMonoid
+#align function.injective.ordered_cancel_add_comm_monoid
+  Function.Injective.orderedCancelAddCommMonoid
 
 end OrderedCancelCommMonoid
 
@@ -42,7 +44,7 @@ section LinearOrderedCancelCommMonoid
 
 variable [LinearOrderedCancelCommMonoid α]
 
-/-- Pullback a `linear_ordered_cancel_comm_monoid` under an injective map.
+/-- Pullback a `LinearOrderedCancelCommMonoid` under an injective map.
 See note [reducible non-instances]. -/
 @[reducible,
   to_additive Function.Injective.linearOrderedCancelAddCommMonoid
@@ -52,9 +54,11 @@ def Function.Injective.linearOrderedCancelCommMonoid {β : Type _} [One β] [Mul
     (mul : ∀ x y, f (x * y) = f x * f y) (npow : ∀ (x) (n : ℕ), f (x ^ n) = f x ^ n)
     (hsup : ∀ x y, f (x ⊔ y) = max (f x) (f y)) (hinf : ∀ x y, f (x ⊓ y) = min (f x) (f y)) :
     LinearOrderedCancelCommMonoid β :=
-  { hf.LinearOrderedCommMonoid f one mul npow hsup hinf,
-    hf.OrderedCancelCommMonoid f one mul npow with }
-#align
-  function.injective.linear_ordered_cancel_comm_monoid Function.Injective.linearOrderedCancelCommMonoid
+  { hf.linearOrderedCommMonoid f one mul npow hsup hinf,
+    hf.orderedCancelCommMonoid f one mul npow with }
+#align function.injective.linear_ordered_cancel_comm_monoid
+  Function.Injective.linearOrderedCancelCommMonoid
+#align function.injective.linear_ordered_cancel_add_comm_monoid
+  Function.Injective.linearOrderedCancelAddCommMonoid
 
 end LinearOrderedCancelCommMonoid
