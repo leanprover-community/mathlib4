fa9629dfe528ec653b93a49d9e9c2940b55b79a0
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2022-12-07 16:35:28 +0000

    feat: port Algebra.Order.Group.Units (#898)
    mathlib3 a95b16cbade0f938fc24abd05412bde1e84bab9b
    
    Porting notes:
    - the module docstring title in mathlib3 was incorrect (probably a relic of a hasty split), I have changed it here without backporting
    - one elaboration issue, see [Zulip](https://leanprover.zulipchat.com/#narrow/stream/287929-mathlib4/topic/elaboration.20failure.20in.20algebra.2Eorder.2Egroup.2Eunits)
    - same issue with instance names as in #890, and I made the same choice about how to fix it

--- Mathlib.lean
+++ Mathlib.lean
@@ -38,6 +38,7 @@ import Mathlib.Algebra.Opposites
 import Mathlib.Algebra.Order.Group.Defs
 import Mathlib.Algebra.Order.Group.Instances
 import Mathlib.Algebra.Order.Group.OrderIso
+import Mathlib.Algebra.Order.Group.Units
 import Mathlib.Algebra.Order.Hom.Basic
 import Mathlib.Algebra.Order.Monoid.Basic
 import Mathlib.Algebra.Order.Monoid.Cancel.Basic
