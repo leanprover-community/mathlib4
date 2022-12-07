b4b59ba6ee072a395c4c9e2ed061a9b4c42fb7de
Chris Hughes <chrishughes24@gmail.com>
2022-12-07 23:03:33 +0000

    feat port: GroupTheory.GroupAction.Sigma (#885)
    mathlib3 SHA: f1a2caaf51ef593799107fe9a8d5e411599f3996
    
    Another easy one.
    
    - [x] depends on #886 
    
    Co-authored-by: Scott Morrison <scott.morrison@gmail.com>
    Co-authored-by: Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -171,6 +171,7 @@ import Mathlib.Data.UnionFind
 import Mathlib.GroupTheory.EckmannHilton
 import Mathlib.GroupTheory.GroupAction.Defs
 import Mathlib.GroupTheory.GroupAction.Option
+import Mathlib.GroupTheory.GroupAction.Sigma
 import Mathlib.GroupTheory.GroupAction.Sum
 import Mathlib.GroupTheory.GroupAction.Units
 import Mathlib.Init.Algebra.Classes
