751a528fb559bdd4aabb8f20978fc89ce83d708b
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2022-12-07 20:31:06 +0100

    fixes

--- Mathlib/Algebra/Hom/Equiv/Units/GroupWithZero.lean
+++ Mathlib/Algebra/Hom/Equiv/Units/GroupWithZero.lean
@@ -7,7 +7,7 @@ import Mathlib.Algebra.Hom.Equiv.Units.Basic
 import Mathlib.Algebra.GroupWithZero.Units.Basic
 
 /-!
-# Multiplication by a nonzero element in a `group_with_zero` is a permutation.
+# Multiplication by a nonzero element in a `GroupWithZero` is a permutation.
 -/
 
 
@@ -19,7 +19,7 @@ section GroupWithZero
 
 variable [GroupWithZero G]
 
-/-- Left multiplication by a nonzero element in a `group_with_zero` is a permutation of the
+/-- Left multiplication by a nonzero element in a `GroupWithZero` is a permutation of the
 underlying type. -/
 @[simps (config := { fullyApplied := false })]
 protected def mulLeft₀ (a : G) (ha : a ≠ 0) : Perm G :=
@@ -27,10 +27,10 @@ protected def mulLeft₀ (a : G) (ha : a ≠ 0) : Perm G :=
 #align equiv.mul_left₀ Equiv.mulLeft₀
 
 theorem mul_left_bijective₀ (a : G) (ha : a ≠ 0) : Function.Bijective ((· * ·) a : G → G) :=
-  (Equiv.mulLeft₀ a ha).Bijective
-#align mul_left_bijective₀ mul_left_bijective₀
+  (Equiv.mulLeft₀ a ha).bijective
+#align equiv.mul_left_bijective₀ Equiv.mul_left_bijective₀
 
-/-- Right multiplication by a nonzero element in a `group_with_zero` is a permutation of the
+/-- Right multiplication by a nonzero element in a `GroupWithZero` is a permutation of the
 underlying type. -/
 @[simps (config := { fullyApplied := false })]
 protected def mulRight₀ (a : G) (ha : a ≠ 0) : Perm G :=
@@ -38,8 +38,8 @@ protected def mulRight₀ (a : G) (ha : a ≠ 0) : Perm G :=
 #align equiv.mul_right₀ Equiv.mulRight₀
 
 theorem mul_right_bijective₀ (a : G) (ha : a ≠ 0) : Function.Bijective ((· * a) : G → G) :=
-  (Equiv.mulRight₀ a ha).Bijective
-#align mul_right_bijective₀ mul_right_bijective₀
+  (Equiv.mulRight₀ a ha).bijective
+#align equiv.mul_right_bijective₀ Equiv.mul_right_bijective₀
 
 end GroupWithZero
 
