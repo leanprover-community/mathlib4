6cf08d56e4613cf096232b81b019480c3a128d9c
Scott Morrison <scott.morrison@gmail.com>
2022-12-08 15:36:43 +1100

    proofs for linarith

--- Mathlib/Tactic/Linarith/Lemmas.lean
+++ Mathlib/Tactic/Linarith/Lemmas.lean
@@ -42,15 +42,13 @@ theorem lt_of_lt_of_eq {α} [OrderedSemiring α] {a b : α} (ha : a < 0) (hb : b
 
 @[nolint unusedArguments]
 theorem mul_neg {α} [StrictOrderedRing α] {a b : α} (ha : a < 0) (hb : 0 < b) : b * a < 0 :=
-sorry
--- have : (-b)*a > 0 := mul_pos_of_neg_of_neg (neg_neg_of_pos hb) ha
--- neg_of_neg_pos (by simpa)
+  have : (-b)*a > 0 := mul_pos_of_neg_of_neg (neg_neg_of_pos hb) ha
+  neg_of_neg_pos (by simpa)
 
 @[nolint unusedArguments]
 theorem mul_nonpos {α} [OrderedRing α] {a b : α} (ha : a ≤ 0) (hb : 0 < b) : b * a ≤ 0 :=
-sorry
--- have : (-b)*a ≥ 0 := mul_nonneg_of_nonpos_of_nonpos (le_of_lt (neg_neg_of_pos hb)) ha
--- by simpa
+  have : (-b)*a ≥ 0 := mul_nonneg_of_nonpos_of_nonpos (le_of_lt (neg_neg_of_pos hb)) ha
+  by simpa
 
 -- used alongside `mul_neg` and `mul_nonpos`, so has the same argument pattern for uniformity
 @[nolint unusedArguments]
