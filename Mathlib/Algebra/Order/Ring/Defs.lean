1b6bedb7893ddc2c2d4856ee5db3c8f5fb113dcc
Scott Morrison <scott.morrison@gmail.com>
2022-12-08 15:39:43 +1100

    add order/field/defs

--- Mathlib/Algebra/Order/Ring/Defs.lean
+++ Mathlib/Algebra/Order/Ring/Defs.lean
@@ -2,6 +2,7 @@
 Copyright (c) 2016 Jeremy Avigad. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jeremy Avigad, Leonardo de Moura, Mario Carneiro, Yaël Dillies
+Ported by: Scott Morrison
 -/
 import Mathlib.Algebra.Order.Group.Defs
 import Mathlib.Algebra.Order.Monoid.Cancel.Defs
