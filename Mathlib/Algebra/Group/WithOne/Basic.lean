c3e481c7762c5239178f1d13fb90ed4d835408db
Jireh Loreaux <loreaujy@gmail.com>
2022-12-08 15:52:54 -0600

    get it compiling

--- Mathlib/Algebra/Group/WithOne/Basic.lean
+++ Mathlib/Algebra/Group/WithOne/Basic.lean
@@ -3,8 +3,8 @@ Copyright (c) 2018 Mario Carneiro. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro, Johan Commelin
 -/
-import Mathbin.Algebra.Group.WithOne.Defs
-import Mathbin.Algebra.Hom.Equiv.Basic
+import Mathlib.Algebra.Group.WithOne.Defs
+import Mathlib.Algebra.Hom.Equiv.Basic
 
 /-!
 # More operations on `with_one` and `with_zero`
@@ -31,16 +31,17 @@ section
 -- will unfold it in the statement of the lemma it generates.
 /-- `coe` as a bundled morphism -/
 @[to_additive "`coe` as a bundled morphism", simps apply]
-def coeMulHom [Mul α] : α →ₙ* WithOne α where 
+def coeMulHom [Mul α] : α →ₙ* WithOne α where
   toFun := coe
-  map_mul' x y := rfl
+  map_mul' _ _ := rfl
 #align with_one.coe_mul_hom WithOne.coeMulHom
 
 end
 
 section lift
 
-attribute [local semireducible] WithOne WithZero
+-- porting note: these were never marked with `irreducible` when they were defined.
+-- attribute [local semireducible] WithOne WithZero
 
 variable [Mul α] [MulOneClass β]
 
@@ -49,24 +50,17 @@ variable [Mul α] [MulOneClass β]
 def lift :
     (α →ₙ* β) ≃
       (WithOne α →*
-        β) where 
+        β) where
   toFun f :=
     { toFun := fun x => Option.casesOn x 1 f, map_one' := rfl,
-      map_mul' := fun x y =>
-        (WithOne.cases_on x
-            (by 
-              rw [one_mul]
-              exact (one_mul _).symm))
-          fun x =>
-          (WithOne.cases_on y
-              (by 
-                rw [mul_one]
-                exact (mul_one _).symm))
-            fun y => f.map_mul x y }
+      map_mul' := fun x y => WithOne.cases_on x (by rw [one_mul]; exact (one_mul _).symm)
+        (fun x => WithOne.cases_on y (by rw [mul_one]; exact (mul_one _).symm)
+          (fun y => f.map_mul x y)) }
   invFun F := F.toMulHom.comp coeMulHom
   left_inv f := MulHom.ext fun x => rfl
-  right_inv F := MonoidHom.ext fun x => (WithOne.cases_on x F.map_one.symm) fun x => rfl
+  right_inv F := MonoidHom.ext fun x => WithOne.cases_on x F.map_one.symm (fun x => rfl)
 #align with_one.lift WithOne.lift
+-- porting note: the above proofs were broken because they were parenthesized wrong by mathport?
 
 variable (f : α →ₙ* β)
 
@@ -106,7 +100,7 @@ theorem map_coe (f : α →ₙ* β) (a : α) : map f (a : WithOne α) = f a :=
 
 @[simp, to_additive]
 theorem map_id : map (MulHom.id α) = MonoidHom.id (WithOne α) := by
-  ext
+  ext x
   induction x using WithOne.cases_on <;> rfl
 #align with_one.map_id WithOne.map_id
 
@@ -122,30 +116,42 @@ theorem map_comp (f : α →ₙ* β) (g : β →ₙ* γ) : map (g.comp f) = (map
 
 /-- A version of `equiv.option_congr` for `with_one`. -/
 @[to_additive "A version of `equiv.option_congr` for `with_zero`.", simps apply]
-def MulEquiv.withOneCongr (e : α ≃* β) : WithOne α ≃* WithOne β :=
-  { map e.toMulHom with toFun := map e.toMulHom, invFun := map e.symm.toMulHom,
-    left_inv := fun x => (map_map _ _ _).trans <| by induction x using WithOne.cases_on <;> · simp,
-    right_inv := fun x =>
-      (map_map _ _ _).trans <| by induction x using WithOne.cases_on <;> · simp }
+def _root_.MulEquiv.withOneCongr (e : α ≃* β) : WithOne α ≃* WithOne β :=
+  { map e.toMulHom with
+    toFun := map e.toMulHom, invFun := map e.symm.toMulHom,
+    left_inv := fun x => (map_map _ _ _).trans <| by
+      induction x using WithOne.cases_on;
+      · simp
+      · simp only [map_coe, MulHom.coe_mk, map_comp, MonoidHom.coe_comp, Function.comp_apply,
+          MulEquiv.toEquiv_symm, coe_inj]
+        apply Equiv.symm_apply_apply,
+      -- porting note: I think because of the way coercions are handled, this doesn't get changed
+      -- by `simp` into something where `Equiv.symm_apply_apply` automatically applies.
+    right_inv := fun x => (map_map _ _ _).trans <| by
+      induction x using WithOne.cases_on;
+      · simp
+      · simp only [map_coe, MulHom.coe_mk, MulEquiv.toEquiv_symm, map_comp, MonoidHom.coe_comp,
+          Function.comp_apply, coe_inj]
+        apply Equiv.apply_symm_apply }
 #align mul_equiv.with_one_congr MulEquiv.withOneCongr
 
 @[simp]
-theorem MulEquiv.with_one_congr_refl : (MulEquiv.refl α).withOneCongr = MulEquiv.refl _ :=
-  MulEquiv.to_monoid_hom_injective map_id
+theorem _root_.MulEquiv.with_one_congr_refl : (MulEquiv.refl α).withOneCongr = MulEquiv.refl _ :=
+  MulEquiv.to_monoidHom_injective map_id
 #align mul_equiv.with_one_congr_refl MulEquiv.with_one_congr_refl
 
 @[simp]
-theorem MulEquiv.with_one_congr_symm (e : α ≃* β) : e.withOneCongr.symm = e.symm.withOneCongr :=
+theorem _root_.MulEquiv.with_one_congr_symm (e : α ≃* β) :
+    e.withOneCongr.symm = e.symm.withOneCongr :=
   rfl
 #align mul_equiv.with_one_congr_symm MulEquiv.with_one_congr_symm
 
 @[simp]
-theorem MulEquiv.with_one_congr_trans (e₁ : α ≃* β) (e₂ : β ≃* γ) :
+theorem _root_.MulEquiv.with_one_congr_trans (e₁ : α ≃* β) (e₂ : β ≃* γ) :
     e₁.withOneCongr.trans e₂.withOneCongr = (e₁.trans e₂).withOneCongr :=
-  MulEquiv.to_monoid_hom_injective (map_comp _ _).symm
+  MulEquiv.to_monoidHom_injective (map_comp _ _).symm
 #align mul_equiv.with_one_congr_trans MulEquiv.with_one_congr_trans
 
 end Map
 
 end WithOne
-
