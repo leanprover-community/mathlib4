b3d29c96f789bfd0837302e62244ca8cd47b4bf0
ChrisHughes24 <chrishughes24@gmail.com>
2022-12-09 15:58:34 +0000

    fix build

--- Mathlib/Algebra/Ring/Defs.lean
+++ Mathlib/Algebra/Ring/Defs.lean
@@ -155,7 +155,7 @@ theorem mul_two (n : α) : n * 2 = n + n :=
   (congrArg₂ _ rfl one_add_one_eq_two.symm).trans <| (left_distrib n 1 1).trans (by rw [mul_one])
 #align mul_two mul_two
 
-end Semiring
+end NonAssocSemiring
 
 @[to_additive]
 theorem mul_ite {α} [Mul α] (P : Prop) [Decidable P] (a b c : α) :
