1dc9dacbc7051ccc9b9f7d7a6f802321a2158d3e
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2022-12-10 21:17:33 +0100

    Fix

--- Mathlib/Algebra/Order/Group/WithTop.lean
+++ Mathlib/Algebra/Order/Group/WithTop.lean
@@ -3,8 +3,8 @@ Copyright (c) 2016 Jeremy Avigad. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jeremy Avigad, Leonardo de Moura, Mario Carneiro, Johannes Hölzl
 -/
-import Mathbin.Algebra.Order.Group.Instances
-import Mathbin.Algebra.Order.Monoid.WithTop
+import Mathlib.Algebra.Order.Group.Instances
+import Mathlib.Algebra.Order.Monoid.WithTop
 
 /-!
 # Adjoining a top element to a `linear_ordered_add_comm_group_with_top`.
