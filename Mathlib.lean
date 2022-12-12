0fd65297a44550c0fcb775e3ecee58332176fbff
ChrisHughes24 <chrishughes24@gmail.com>
2022-12-12 17:42:39 +0000

    feat port: Data.Set.NAry

--- Mathlib.lean
+++ Mathlib.lean
@@ -186,6 +186,7 @@ import Mathlib.Data.Rat.Init
 import Mathlib.Data.Rat.Order
 import Mathlib.Data.Set.Basic
 import Mathlib.Data.Set.Image
+import Mathlib.Data.Set.NAry
 import Mathlib.Data.Sigma.Basic
 import Mathlib.Data.Sigma.Lex
 import Mathlib.Data.Sigma.Order
