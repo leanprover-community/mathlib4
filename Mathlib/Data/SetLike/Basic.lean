b0c05ca9993788f84f929fdd5161a82b676a8af7
Jireh Loreaux <loreaujy@gmail.com>
2022-12-13 16:48:01 -0600

    appease style linter

--- Mathlib/Data/SetLike/Basic.lean
+++ Mathlib/Data/SetLike/Basic.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Eric Wieser
 -/
 import Mathlib.Data.Set.Basic
---import Mathlib.Tactic.Monotonicity.Basic
 
 /-!
 # Typeclass for types with a set-like extensionality property
