51820a5326507ac68f762d592e52e9de25cfcc76
Rémy Degenne <Remydegenne@gmail.com>
2022-12-13 21:24:06 +0000

    feat: port Algebra.Order.Field.Canonical.Defs (#985)
    Mathlib SHA: fc2ed6f838ce7c9b7c7171e58d78eaf7b438fb0e
    
    Sorry, I forgot to push the mathport version. The only manual changes are:
    - breaking two align lines that were above 100 chars.
    - the deletion of a `@[protect_proj]` before line 20, which returned an error. I don't know whether that is the right thing to do or not. The wiki says I should protect all fields, but that class only extends two others and does not introduce new fields.
    - replace one `=>` by `↦`

--- Mathlib.lean
+++ Mathlib.lean
@@ -47,6 +47,7 @@ import Mathlib.Algebra.Homology.ComplexShape
 import Mathlib.Algebra.Invertible
 import Mathlib.Algebra.NeZero
 import Mathlib.Algebra.Opposites
+import Mathlib.Algebra.Order.Field.Canonical.Defs
 import Mathlib.Algebra.Order.Field.Defs
 import Mathlib.Algebra.Order.Group.Abs
 import Mathlib.Algebra.Order.Group.Defs
