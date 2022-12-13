8235498bc3c00ad67ff6698acafa98b06b027be3
Joël Riou <joel.riou@universite-paris-saclay.fr>
2022-12-13 17:13:35 +0100

    feat: port Data.TwoPointing

--- Mathlib/Data/TwoPointing.lean
+++ Mathlib/Data/TwoPointing.lean
@@ -2,17 +2,18 @@
 Copyright (c) 2022 Yaël Dillies. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yaël Dillies
+Ported by: Joël Riou
 -/
-import Mathbin.Data.Sum.Basic
-import Mathbin.Logic.Nontrivial
+import Mathlib.Data.Sum.Basic
+import Mathlib.Logic.Nontrivial
 
 /-!
 # Two-pointings
 
-This file defines `two_pointing α`, the type of two pointings of `α`. A two-pointing is the data of
+This file defines `TwoPointing α`, the type of two pointings of `α`. A two-pointing is the data of
 two distinct terms.
 
-This is morally a Type-valued `nontrivial`. Another type which is quite close in essence is `sym2`.
+This is morally a Type-valued `Nontrivial`. Another type which is quite close in essence is `Sym2`.
 Categorically speaking, `prod` is a cospan in the category of types. This forms the category of
 bipointed types. Two-pointed types form a full subcategory of those.
 
@@ -22,14 +23,14 @@ bipointed types. Two-pointed types form a full subcategory of those.
   (https://ncatlab.org/nlab/show/coalgebra+of+the+real+interval)
 -/
 
-
 open Function
 
 variable {α β : Type _}
 
-/-- Two-pointing of a type. This is a Type-valued termed `nontrivial`. -/
+/-- Two-pointing of a type. This is a Type-valued termed `Nontrivial`. -/
 @[ext]
 structure TwoPointing (α : Type _) extends α × α where
+  /-- `fst` and `snd` are distinct terms -/
   fst_ne_snd : fst ≠ snd
   deriving DecidableEq
 #align two_pointing TwoPointing
@@ -48,16 +49,14 @@ def swap : TwoPointing α :=
   ⟨(p.snd, p.fst), p.snd_ne_fst⟩
 #align two_pointing.swap TwoPointing.swap
 
-theorem swap_fst : p.swap.fst = p.snd :=
-  rfl
+theorem swap_fst : p.swap.fst = p.snd := rfl
 #align two_pointing.swap_fst TwoPointing.swap_fst
 
-theorem swap_snd : p.swap.snd = p.fst :=
-  rfl
+theorem swap_snd : p.swap.snd = p.fst := rfl
 #align two_pointing.swap_snd TwoPointing.swap_snd
 
 @[simp]
-theorem swap_swap : p.swap.swap = p := by ext <;> rfl
+theorem swap_swap : p.swap.swap = p := rfl
 #align two_pointing.swap_swap TwoPointing.swap_swap
 
 -- See note [reducible non instances]
@@ -72,7 +71,7 @@ instance [Nontrivial α] : Nonempty (TwoPointing α) :=
 
 @[simp]
 theorem nonempty_two_pointing_iff : Nonempty (TwoPointing α) ↔ Nontrivial α :=
-  ⟨fun ⟨p⟩ => p.to_nontrivial, @TwoPointing.nonempty _⟩
+  ⟨fun ⟨p⟩ ↦ p.to_nontrivial, fun _ => inferInstance⟩
 #align two_pointing.nonempty_two_pointing_iff TwoPointing.nonempty_two_pointing_iff
 
 section Pi
@@ -80,10 +79,10 @@ section Pi
 variable (α) [Nonempty α]
 
 /-- The two-pointing of constant functions. -/
-def pi : TwoPointing (α → β) where 
+def pi : TwoPointing (α → β) where
   fst _ := q.fst
   snd _ := q.snd
-  fst_ne_snd h := q.fst_ne_snd <| by convert congr_fun h (Classical.arbitrary α)
+  fst_ne_snd h := q.fst_ne_snd (congr_fun h (Classical.arbitrary α))
 #align two_pointing.pi TwoPointing.pi
 
 @[simp]
@@ -99,19 +98,19 @@ theorem pi_snd : (q.pi α).snd = const α q.snd :=
 end Pi
 
 /-- The product of two two-pointings. -/
-def prod : TwoPointing (α × β) where 
+def prod : TwoPointing (α × β) where
   fst := (p.fst, q.fst)
   snd := (p.snd, q.snd)
   fst_ne_snd h := p.fst_ne_snd (congr_arg Prod.fst h)
 #align two_pointing.prod TwoPointing.prod
 
 @[simp]
-theorem prod_fst : (p.Prod q).fst = (p.fst, q.fst) :=
+theorem prod_fst : (p.prod q).fst = (p.fst, q.fst) :=
   rfl
 #align two_pointing.prod_fst TwoPointing.prod_fst
 
 @[simp]
-theorem prod_snd : (p.Prod q).snd = (p.snd, q.snd) :=
+theorem prod_snd : (p.prod q).snd = (p.snd, q.snd) :=
   rfl
 #align two_pointing.prod_snd TwoPointing.prod_snd
 
@@ -122,28 +121,26 @@ protected def sum : TwoPointing (Sum α β) :=
 #align two_pointing.sum TwoPointing.sum
 
 @[simp]
-theorem sum_fst : (p.Sum q).fst = Sum.inl p.fst :=
+theorem sum_fst : (p.sum q).fst = Sum.inl p.fst :=
   rfl
 #align two_pointing.sum_fst TwoPointing.sum_fst
 
 @[simp]
-theorem sum_snd : (p.Sum q).snd = Sum.inr q.snd :=
+theorem sum_snd : (p.sum q).snd = Sum.inr q.snd :=
   rfl
 #align two_pointing.sum_snd TwoPointing.sum_snd
 
-/-- The `ff`, `tt` two-pointing of `bool`. -/
+/-- The `false`, `true` two-pointing of `Bool`. -/
 protected def bool : TwoPointing Bool :=
   ⟨(false, true), Bool.ff_ne_tt⟩
 #align two_pointing.bool TwoPointing.bool
 
 @[simp]
-theorem bool_fst : TwoPointing.bool.fst = ff :=
-  rfl
+theorem bool_fst : TwoPointing.bool.fst = false := rfl
 #align two_pointing.bool_fst TwoPointing.bool_fst
 
 @[simp]
-theorem bool_snd : TwoPointing.bool.snd = tt :=
-  rfl
+theorem bool_snd : TwoPointing.bool.snd = true := rfl
 #align two_pointing.bool_snd TwoPointing.bool_snd
 
 instance : Inhabited (TwoPointing Bool) :=
@@ -155,14 +152,13 @@ protected def prop : TwoPointing Prop :=
 #align two_pointing.Prop TwoPointing.prop
 
 @[simp]
-theorem Prop_fst : TwoPointing.prop.fst = False :=
+theorem prop_fst : TwoPointing.prop.fst = False :=
   rfl
-#align two_pointing.Prop_fst TwoPointing.Prop_fst
+#align two_pointing.Prop_fst TwoPointing.prop_fst
 
 @[simp]
-theorem Prop_snd : TwoPointing.prop.snd = True :=
+theorem prop_snd : TwoPointing.prop.snd = True :=
   rfl
-#align two_pointing.Prop_snd TwoPointing.Prop_snd
+#align two_pointing.Prop_snd TwoPointing.prop_snd
 
 end TwoPointing
-
