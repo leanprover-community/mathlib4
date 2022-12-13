25c7c93d620baaeecbb55118f6dadd23b420713f
Joël Riou <joel.riou@universite-paris-saclay.fr>
2022-12-13 16:27:46 +0100

    s/=>/\mapsto/

--- Mathlib/Algebra/GroupPower/Ring.lean
+++ Mathlib/Algebra/GroupPower/Ring.lean
@@ -173,8 +173,8 @@ variable (R)
 theorem neg_one_pow_eq_or : ∀ n : ℕ, (-1 : R) ^ n = 1 ∨ (-1 : R) ^ n = -1
   | 0 => Or.inl (pow_zero _)
   | n + 1 => (neg_one_pow_eq_or n).symm.imp
-    (fun h => by rw [pow_succ, h, neg_one_mul, neg_neg])
-    (fun h => by rw [pow_succ, h, mul_one])
+    (fun h ↦ by rw [pow_succ, h, neg_one_mul, neg_neg])
+    (fun h ↦ by rw [pow_succ, h, mul_one])
 
 variable {R}
 
