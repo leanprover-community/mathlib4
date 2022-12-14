47f7873262f8fc43527455de6f1bd358f4e342af
Scott Morrison <scott.morrison@gmail.com>
2022-12-14 11:55:15 +1100

    import

--- Mathlib.lean
+++ Mathlib.lean
@@ -154,6 +154,7 @@ import Mathlib.Data.Int.Basic
 import Mathlib.Data.Int.Cast
 import Mathlib.Data.Int.Cast.Basic
 import Mathlib.Data.Int.Cast.Defs
+import Mathlib.Data.Int.Cast.Lemmas
 import Mathlib.Data.Int.Order.Basic
 import Mathlib.Data.Int.Units
 import Mathlib.Data.KVMap
