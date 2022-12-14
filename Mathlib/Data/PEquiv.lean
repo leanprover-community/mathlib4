d4d7bc15918b3cee107e5bf26644e21f53eb5b3f
ChrisHughes24 <chrishughes24@gmail.com>
2022-12-14 09:54:03 +0000

    lline length

--- Mathlib/Data/PEquiv.lean
+++ Mathlib/Data/PEquiv.lean
@@ -9,7 +9,7 @@ import Mathlib.Data.Set.Basic
 
 # Partial Equivalences
 
-In this file, we define partial equivalences `PEquiv`, which are a bijection between a subset of `α`git
+In this file, we define partial equivalences `PEquiv`, which are a bijection between a subset of `α`
 and a subset of `β`. Notationally, a `PEquiv` is denoted by "`≃.`" (note that the full stop is part
 of the notation). The way we store these internally is with two functions `f : α → Option β` and
 the reverse function `g : β → Pption α`, with the condition that if `f a` is `some b`,
