31bbacbff567009bd75acba669002daf0507b7d3
ChrisHughes24 <chrishughes24@gmail.com>
2022-12-14 14:59:52 +0000

    fixes

--- Mathlib/Algebra/Order/Monoid/ToMulBot.lean
+++ Mathlib/Algebra/Order/Monoid/ToMulBot.lean
@@ -28,40 +28,40 @@ def toMulBot : WithZero (Multiplicative α) ≃* Multiplicative (WithBot α) :=
 #align with_zero.to_mul_bot WithZero.toMulBot
 
 @[simp]
-theorem to_mul_bot_zero : toMulBot (0 : WithZero (Multiplicative α)) = Multiplicative.ofAdd ⊥ :=
+theorem toMulBot_zero : toMulBot (0 : WithZero (Multiplicative α)) = Multiplicative.ofAdd ⊥ :=
   rfl
-#align with_zero.to_mul_bot_zero WithZero.to_mul_bot_zero
+#align with_zero.to_mul_bot_zero WithZero.toMulBot_zero
 
 @[simp]
-theorem to_mul_bot_coe (x : Multiplicative α) :
+theorem toMulBot_coe (x : Multiplicative α) :
     toMulBot ↑x = Multiplicative.ofAdd (Multiplicative.toAdd x : WithBot α) :=
   rfl
-#align with_zero.to_mul_bot_coe WithZero.to_mul_bot_coe
+#align with_zero.to_mul_bot_coe WithZero.toMulBot_coe
 
 @[simp]
-theorem to_mul_bot_symm_bot : toMulBot.symm (Multiplicative.ofAdd (⊥ : WithBot α)) = 0 :=
+theorem toMulBot_symm_bot : toMulBot.symm (Multiplicative.ofAdd (⊥ : WithBot α)) = 0 :=
   rfl
-#align with_zero.to_mul_bot_symm_bot WithZero.to_mul_bot_symm_bot
+#align with_zero.to_mul_bot_symm_bot WithZero.toMulBot_symm_bot
 
 @[simp]
-theorem to_mul_bot_coe_of_add (x : α) :
+theorem toMulBot_coe_of_add (x : α) :
     toMulBot.symm (Multiplicative.ofAdd (x : WithBot α)) = Multiplicative.ofAdd x :=
   rfl
-#align with_zero.to_mul_bot_coe_of_add WithZero.to_mul_bot_coe_of_add
+#align with_zero.to_mul_bot_coe_of_add WithZero.toMulBot_coe_of_add
 
 variable [Preorder α] (a b : WithZero (Multiplicative α))
 
-theorem to_mul_bot_strict_mono : StrictMono (@toMulBot α _) := fun _ _ => id
-#align with_zero.to_mul_bot_strict_mono WithZero.to_mul_bot_strict_mono
+theorem toMulBot_strictMono : StrictMono (@toMulBot α _) := fun _ _ => id
+#align with_zero.to_mul_bot_strict_mono WithZero.toMulBot_strictMono
 
 @[simp]
-theorem to_mul_bot_le : toMulBot a ≤ toMulBot b ↔ a ≤ b :=
+theorem toMulBot_le : toMulBot a ≤ toMulBot b ↔ a ≤ b :=
   Iff.rfl
-#align with_zero.to_mul_bot_le WithZero.to_mul_bot_le
+#align with_zero.to_mul_bot_le WithZero.toMulBot_le
 
 @[simp]
-theorem to_mul_bot_lt : toMulBot a < toMulBot b ↔ a < b :=
+theorem toMulBot_lt : toMulBot a < toMulBot b ↔ a < b :=
   Iff.rfl
-#align with_zero.to_mul_bot_lt WithZero.to_mul_bot_lt
+#align with_zero.to_mul_bot_lt WithZero.toMulBot_lt
 
 end WithZero
