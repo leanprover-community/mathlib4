922a88c4a36f9427268112628197c3c270f6c93a
Chris Hughes <chrishughes24@gmail.com>
2022-12-14 23:20:02 +0000

    feat port: Order.InitialSeg (#1012)
    ee0c179cd3c8a45aa5bffbf1b41d8dbede452865
    
    Co-authored-by: Scott Morrison <scott@tqft.net>

--- Mathlib.lean
+++ Mathlib.lean
@@ -310,6 +310,7 @@ import Mathlib.Order.GameAdd
 import Mathlib.Order.Heyting.Basic
 import Mathlib.Order.Heyting.Boundary
 import Mathlib.Order.Hom.Basic
+import Mathlib.Order.InitialSeg
 import Mathlib.Order.Iterate
 import Mathlib.Order.Lattice
 import Mathlib.Order.Max
