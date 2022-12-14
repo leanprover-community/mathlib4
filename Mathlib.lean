d49808fa3d25677a8f825eec9cf3e72bf9a4c337
Patrick Massot <patrickmassot@free.fr>
2022-12-14 03:08:37 +0000

    feat port: logic.embedding.set (#986)
    fc2ed6f838ce7c9b7c7171e58d78eaf7b438fb0e

--- Mathlib.lean
+++ Mathlib.lean
@@ -258,6 +258,7 @@ import Mathlib.Lean.Meta
 import Mathlib.Lean.Meta.Simp
 import Mathlib.Logic.Basic
 import Mathlib.Logic.Embedding.Basic
+import Mathlib.Logic.Embedding.Set
 import Mathlib.Logic.Equiv.Basic
 import Mathlib.Logic.Equiv.Defs
 import Mathlib.Logic.Equiv.LocalEquiv
