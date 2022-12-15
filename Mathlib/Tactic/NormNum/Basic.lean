7e6e81aa7f25cdf7c4d1666d2d396c9abc8ad11f
Riccardo Brasca <riccardo.brasca@gmail.com>
2022-12-15 17:59:08 +0100

    revert this

--- Mathlib/Tactic/NormNum/Basic.lean
+++ Mathlib/Tactic/NormNum/Basic.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro
 -/
 import Mathlib.Tactic.NormNum.Core
-import Mathlib.Data.Int.Cast.Lemmas
+import Mathlib.Algebra.GroupPower.Lemmas
 import Qq.Match
 
 /-!
