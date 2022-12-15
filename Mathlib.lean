862d5c14b138436e523cd7b67f4a03654202e790
Chris Hughes <chrishughes24@gmail.com>
2022-12-15 14:12:23 +0000

    feat port: Order.Monovary (#1022)
    ee0c179cd3c8a45aa5bffbf1b41d8dbede452865

--- Mathlib.lean
+++ Mathlib.lean
@@ -323,6 +323,7 @@ import Mathlib.Order.Lattice
 import Mathlib.Order.Max
 import Mathlib.Order.MinMax
 import Mathlib.Order.Monotone.Basic
+import Mathlib.Order.Monovary
 import Mathlib.Order.PropInstances
 import Mathlib.Order.RelClasses
 import Mathlib.Order.RelIso.Basic
