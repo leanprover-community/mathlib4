d2dbb0a805fffc8a04cffdad749348d3f8f08cc8
Yaël Dillies <yael.dillies@gmail.com>
2022-12-15 10:44:11 +0000

    forward port final changes

--- Mathlib/Order/Basic.lean
+++ Mathlib/Order/Basic.lean
@@ -431,12 +431,16 @@ theorem le_implies_le_of_le_of_le {a b c d : α} [Preorder α] (hca : c ≤ a) (
 section PartialOrder
 variable [PartialOrder α]
 
-lemma comm_of_le {f : β → β → α} (comm : ∀ a b, f a b ≤ f b a) : ∀ a b, f a b = f b a :=
+/-- To prove commutativity of a binary operation `○`, we only to check `a ○ b ≤ b ○ a` for all `a`,
+`b`. -/
+lemma commutative_of_le {f : β → β → α} (comm : ∀ a b, f a b ≤ f b a) : ∀ a b, f a b = f b a :=
 fun _ _ ↦ (comm _ _).antisymm $ comm _ _
 
-lemma assoc_of_comm_of_le {f : α → α → α} (comm : ∀ a b, f a b = f b a)
+/-- To prove associativity of a commutative binary operation `○`, we only to check
+`(a ○ b) ○ c ≤ a ○ (b ○ c)` for all `a`, `b`, `c`. -/
+lemma associative_of_commutative_of_le  {f : α → α → α} (comm : Commutative f)
   (assoc : ∀ a b c, f (f a b) c ≤ f a (f b c)) :
-  ∀ a b c, f (f a b) c = f a (f b c) :=
+  Associative f :=
 fun a b c ↦ le_antisymm (assoc _ _ _) $ by rw [comm, comm b, comm _ c, comm a]; exact assoc _ _ _
 
 end PartialOrder
