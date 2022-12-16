0c2274d8529875889536d4c08c271386a8b281fc
ChrisHughes24 <chrishughes24@gmail.com>
2022-12-16 16:42:33 +0000

    fixes

--- Mathlib/Data/Int/Order/Units.lean
+++ Mathlib/Data/Int/Order/Units.lean
@@ -8,9 +8,9 @@ Authors: Jeremy Avigad
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
-import Mathbin.Data.Int.Order.Basic
-import Mathbin.Data.Int.Units
-import Mathbin.Algebra.GroupPower.Order
+import Mathlib.Data.Int.Order.Basic
+import Mathlib.Data.Int.Units
+import Mathlib.Algebra.GroupPower.Order
 
 /-!
 # Lemmas about units in `ℤ`, which interact with the order structure.
@@ -19,16 +19,16 @@ import Mathbin.Algebra.GroupPower.Order
 
 namespace Int
 
-theorem is_unit_iff_abs_eq {x : ℤ} : IsUnit x ↔ abs x = 1 := by
-  rw [is_unit_iff_nat_abs_eq, abs_eq_nat_abs, ← Int.ofNat_one, coe_nat_inj']
-#align int.is_unit_iff_abs_eq Int.is_unit_iff_abs_eq
+theorem isUnit_iff_abs_eq {x : ℤ} : IsUnit x ↔ abs x = 1 := by
+  rw [isUnit_iff_natAbs_eq, abs_eq_natAbs, ← Int.ofNat_one, coe_nat_inj']
+#align int.is_unit_iff_abs_eq Int.isUnit_iff_abs_eq
 
-theorem is_unit_sq {a : ℤ} (ha : IsUnit a) : a ^ 2 = 1 := by rw [sq, is_unit_mul_self ha]
-#align int.is_unit_sq Int.is_unit_sq
+theorem isUnit_sq {a : ℤ} (ha : IsUnit a) : a ^ 2 = 1 := by rw [sq, isUnit_mul_self ha]
+#align int.is_unit_sq Int.isUnit_sq
 
 @[simp]
 theorem units_sq (u : ℤˣ) : u ^ 2 = 1 := by
-  rw [Units.ext_iff, Units.val_pow_eq_pow_val, Units.val_one, is_unit_sq u.is_unit]
+  rw [Units.ext_iff, Units.val_pow_eq_pow_val, Units.val_one, isUnit_sq u.isUnit]
 #align int.units_sq Int.units_sq
 
 alias units_sq ← units_pow_two
@@ -60,14 +60,14 @@ theorem sq_eq_one_of_sq_lt_four {x : ℤ} (h1 : x ^ 2 < 4) (h2 : x ≠ 0) : x ^ 
 #align int.sq_eq_one_of_sq_lt_four Int.sq_eq_one_of_sq_lt_four
 
 theorem sq_eq_one_of_sq_le_three {x : ℤ} (h1 : x ^ 2 ≤ 3) (h2 : x ≠ 0) : x ^ 2 = 1 :=
-  sq_eq_one_of_sq_lt_four (lt_of_le_of_lt h1 (lt_add_one 3)) h2
+  sq_eq_one_of_sq_lt_four (lt_of_le_of_lt h1 (lt_add_one (3 : ℤ))) h2
 #align int.sq_eq_one_of_sq_le_three Int.sq_eq_one_of_sq_le_three
 
 theorem units_pow_eq_pow_mod_two (u : ℤˣ) (n : ℕ) : u ^ n = u ^ (n % 2) := by
   conv =>
       lhs
-      rw [← Nat.mod_add_div n 2] <;>
-    rw [pow_add, pow_mul, units_sq, one_pow, mul_one]
+      rw [← Nat.mod_add_div n 2];
+      rw [pow_add, pow_mul, units_sq, one_pow, mul_one]
 #align int.units_pow_eq_pow_mod_two Int.units_pow_eq_pow_mod_two
 
 end Int
