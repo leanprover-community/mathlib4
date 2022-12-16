367529946d895c9f8f333f1642bd42d153b8eb33
Moritz Doll <moritz.doll@googlemail.com>
2022-12-16 12:00:21 +0000

    feat: port Order.LatticeIntervals (#1070)
    Lots of renames, but easy.
    
    I've added names for all instances
    
    Co-authored-by: Chris Hughes <33847686+ChrisHughes24@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -335,6 +335,7 @@ import Mathlib.Order.Hom.Set
 import Mathlib.Order.InitialSeg
 import Mathlib.Order.Iterate
 import Mathlib.Order.Lattice
+import Mathlib.Order.LatticeIntervals
 import Mathlib.Order.Max
 import Mathlib.Order.MinMax
 import Mathlib.Order.Monotone.Basic
