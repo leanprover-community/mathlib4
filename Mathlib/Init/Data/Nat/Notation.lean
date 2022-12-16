b581bcbba603b2211e9db298175a5d8693b33fdf
Scott Morrison <scott.morrison@gmail.com>
2022-12-16 20:43:41 +1100

    module doc

--- Mathlib/Init/Data/Nat/Notation.lean
+++ Mathlib/Init/Data/Nat/Notation.lean
@@ -4,4 +4,8 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Floris van Doorn, Leonardo de Moura
 -/
 
+/-!
+# Notation `ℕ` for the natural numbers.
+-/
+
 notation "ℕ" => Nat
