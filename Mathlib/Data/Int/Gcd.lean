f436d5d9d8c83c3336e508b38db96ba32c11eca7
Riccardo Brasca <riccardo.brasca@gmail.com>
2022-12-19 15:47:25 +0100

    add source header

--- Mathlib/Data/Int/Gcd.lean
+++ Mathlib/Data/Int/Gcd.lean
@@ -2,6 +2,11 @@
 Copyright (c) 2018 Guy Leroy. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Sangwoo Jo (aka Jason), Guy Leroy, Johannes Hölzl, Mario Carneiro
+
+! This file was ported from Lean 3 source module data.int.gcd
+! leanprover-community/mathlib commit d4f69d96f3532729da8ebb763f4bc26fcf640f06
+! Please do not edit these lines, except to modify the commit id
+! if you have ported upstream changes.
 -/
 import Mathlib.Data.Nat.Gcd.Basic
 import Mathlib.Algebra.Ring.Regular
