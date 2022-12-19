9d24a5e3bf320b79ab7356aba9cfc8515c727ab3
Jakob von Raumer <jakob@von-raumer.de>
2022-12-19 14:30:01 +0100

    add to main file

--- Mathlib.lean
+++ Mathlib.lean
@@ -222,6 +222,7 @@ import Mathlib.Data.PNat.Defs
 import Mathlib.Data.PSigma.Order
 import Mathlib.Data.Part
 import Mathlib.Data.Pi.Algebra
+import Mathlib.Data.Pi.Lex
 import Mathlib.Data.Prod.Basic
 import Mathlib.Data.Prod.Lex
 import Mathlib.Data.Prod.PProd
