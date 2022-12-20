leanprover-community/mathlib4

commit f4eb9061dd508dae028e86a0541b7541f8133122

feat: port Algebra.Group.WithOne.Basic (#922)
mathlib3 SHA: 4dc134b97a3de65ef2ed881f3513d56260971562

porting notes:

1. In `WithOne.lift` mathport's output was broken, but I think the only reason was that it was parenthesized wrong (or maybe it was a whitespace / formatting issue?)
2. In `MulEquiv.withOneCongr`, there were some `simp` calls that broke. I think this is a result of the way coercions are handled now. I think the net result is that `simp` doesn't get it into a form where it can apply `Equiv.symm_apply_apply`.
3. I added the `to_additive` attribute to `MulEquiv.withOneCongr_{refl, symm, trans}` because it just seemed to be missing from mathlib3.
4. `WithOne` already has semireducible transparency in mathlib4, so we are just ignoring the changing of transparency settings (this isn't even possible in Lean 4); see [Zulip](https://leanprover.zulipchat.com/#narrow/stream/287929-mathlib4/topic/local.20semireducible)

Mathlib/Algebra/Group/WithOne/Basic.lean
@@ -1,0 +1,175 @@
+/-
+Copyright (c) 2018 Mario Carneiro. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Mario Carneiro, Johan Commelin
+-/
+import Mathlib.Algebra.Group.WithOne.Defs
+import Mathlib.Algebra.Hom.Equiv.Basic
+
+/-!
+# More operations on `WithOne` and `WithZero`
+
+This file defines various bundled morphisms on `WithOne` and `WithZero`
+that were not available in `Algebra/Group/WithOne/Defs`.
+
+## Main definitions
+
+* `WithOne.lift`, `WithZero.lift`
+* `WithOne.map`, `WithZero.map`
+-/
+
+
+universe u v w
+
+variable {α : Type u} {β : Type v} {γ : Type w}
+
+namespace WithOne
+
+section
+
+-- porting note: the workaround described below doesn't seem to be a problem even with
+-- semireducible transparency
+-- workaround: we make `with_one`/`with_zero` irreducible for this definition, otherwise `simps`
+-- will unfold it in the statement of the lemma it generates.
+/-- `WithOne.coe` as a bundled morphism -/
+@[to_additive "`WithZero.coe` as a bundled morphism", simps apply]
+def coeMulHom [Mul α] : α →ₙ* WithOne α where
+  toFun := coe
+  map_mul' _ _ := rfl
+#align with_one.coe_mul_hom WithOne.coeMulHom
+#align with_zero.coe_add_hom WithZero.coeAddHom
+
+end
+
+section lift
+
+-- porting note: these were never marked with `irreducible` when they were defined.
+-- attribute [local semireducible] WithOne WithZero
+
+variable [Mul α] [MulOneClass β]
+
+/-- Lift a semigroup homomorphism `f` to a bundled monoid homorphism. -/
+@[to_additive "Lift an add semigroup homomorphism `f` to a bundled add monoid homorphism."]
+def lift :
+    (α →ₙ* β) ≃
+      (WithOne α →*
+        β) where
+  toFun f :=
+    { toFun := fun x => Option.casesOn x 1 f, map_one' := rfl,
+      map_mul' := fun x y => WithOne.cases_on x (by rw [one_mul]; exact (one_mul _).symm)
+        (fun x => WithOne.cases_on y (by rw [mul_one]; exact (mul_one _).symm)
+          (fun y => f.map_mul x y)) }
+  invFun F := F.toMulHom.comp coeMulHom
+  left_inv f := MulHom.ext fun x => rfl
+  right_inv F := MonoidHom.ext fun x => WithOne.cases_on x F.map_one.symm (fun x => rfl)
+-- porting note: the above proofs were broken because they were parenthesized wrong by mathport?
+#align with_one.lift WithOne.lift
+#align with_zero.lift WithZero.lift
+
+variable (f : α →ₙ* β)
+
+@[simp, to_additive]
+theorem lift_coe (x : α) : lift f x = f x :=
+  rfl
+#align with_one.lift_coe WithOne.lift_coe
+#align with_zero.lift_coe WithZero.lift_coe
+
+-- porting note: removed `simp` attribute to appease `simpNF` linter.
+@[to_additive]
+theorem lift_one : lift f 1 = 1 :=
+  rfl
+#align with_one.lift_one WithOne.lift_one
+#align with_zero.lift_zero WithZero.lift_zero
+
+@[to_additive]
+theorem lift_unique (f : WithOne α →* β) : f = lift (f.toMulHom.comp coeMulHom) :=
+  (lift.apply_symm_apply f).symm
+#align with_one.lift_unique WithOne.lift_unique
+#align with_zero.lift_unique WithZero.lift_unique
+
+end lift
+
+section Map
+
+variable [Mul α] [Mul β] [Mul γ]
+
+/-- Given a multiplicative map from `α → β` returns a monoid homomorphism
+  from `WithOne α` to `WithOne β` -/
+@[to_additive "Given an additive map from `α → β` returns an add monoid homomorphism\n  from
+`WithZero α` to `WithZero β`"]
+def map (f : α →ₙ* β) : WithOne α →* WithOne β :=
+  lift (coeMulHom.comp f)
+#align with_one.map WithOne.map
+#align with_zero.map WithZero.map
+
+@[simp, to_additive]
+theorem map_coe (f : α →ₙ* β) (a : α) : map f (a : WithOne α) = f a :=
+  lift_coe _ _
+#align with_one.map_coe WithOne.map_coe
+#align with_zero.map_coe WithZero.map_coe
+
+@[simp, to_additive]
+theorem map_id : map (MulHom.id α) = MonoidHom.id (WithOne α) := by
+  ext x
+  induction x using WithOne.cases_on <;> rfl
+#align with_one.map_id WithOne.map_id
+#align with_zero.map_id WithZero.map_id
+
+@[to_additive]
+theorem map_map (f : α →ₙ* β) (g : β →ₙ* γ) (x) : map g (map f x) = map (g.comp f) x := by
+  induction x using WithOne.cases_on <;> rfl
+#align with_one.map_map WithOne.map_map
+#align with_zero.map_map WithZero.map_map
+
+@[simp, to_additive]
+theorem map_comp (f : α →ₙ* β) (g : β →ₙ* γ) : map (g.comp f) = (map g).comp (map f) :=
+  MonoidHom.ext fun x => (map_map f g x).symm
+#align with_one.map_comp WithOne.map_comp
+#align with_zero.map_comp WithZero.map_comp
+
+/-- A version of `Equiv.optionCongr` for `WithOne`. -/
+@[to_additive "A version of `equiv.option_congr` for `with_zero`.", simps apply]
+def _root_.MulEquiv.withOneCongr (e : α ≃* β) : WithOne α ≃* WithOne β :=
+  { map e.toMulHom with
+    toFun := map e.toMulHom, invFun := map e.symm.toMulHom,
+    left_inv := fun x => (map_map _ _ _).trans <| by
+      -- porting note: in mathlib3 this worked as: `induction x using WithOne.cases_on <;> simp`
+      induction x using WithOne.cases_on
+      · simp
+      · simp only [map_coe, MulHom.coe_mk, map_comp, MonoidHom.coe_comp, Function.comp_apply,
+          MulEquiv.toEquiv_symm, coe_inj]
+        apply Equiv.symm_apply_apply,
+      -- porting note: I think because of the way coercions are handled, this doesn't get changed
+      -- by `simp` into something where `Equiv.symm_apply_apply` automatically applies.
+    right_inv := fun x => (map_map _ _ _).trans <| by
+      -- porting note: in mathlib3 this worked as: `induction x using WithOne.cases_on <;> simp`
+      induction x using WithOne.cases_on
+      · simp
+      · simp only [map_coe, MulHom.coe_mk, MulEquiv.toEquiv_symm, map_comp, MonoidHom.coe_comp,
+          Function.comp_apply, coe_inj]
+        apply Equiv.apply_symm_apply }
+#align mul_equiv.with_one_congr MulEquiv.withOneCongr
+#align add_equiv.with_zero_congr AddEquiv.withZeroCongr
+
+-- porting note: for this declaration and the two below I added the `to_additive` attribute because
+-- it seemed to be missing from mathlib3, hence the lack of additive `#align`s.
+@[simp, to_additive]
+theorem _root_.MulEquiv.withOneCongr_refl : (MulEquiv.refl α).withOneCongr = MulEquiv.refl _ :=
+  MulEquiv.to_monoidHom_injective map_id
+#align mul_equiv.with_one_congr_refl MulEquiv.withOneCongr_refl
+
+@[simp, to_additive]
+theorem _root_.MulEquiv.withOneCongr_symm (e : α ≃* β) :
+    e.withOneCongr.symm = e.symm.withOneCongr :=
+  rfl
+#align mul_equiv.with_one_congr_symm MulEquiv.withOneCongr_symm
+
+@[simp, to_additive]
+theorem _root_.MulEquiv.withOneCongr_trans (e₁ : α ≃* β) (e₂ : β ≃* γ) :
+    e₁.withOneCongr.trans e₂.withOneCongr = (e₁.trans e₂).withOneCongr :=
+  MulEquiv.to_monoidHom_injective (map_comp _ _).symm
+#align mul_equiv.with_one_congr_trans MulEquiv.withOneCongr_trans
+
+end Map
+
+end WithOne

Mathlib.lean
@@ -16,6 +16,7 @@ import Mathlib.Algebra.Group.Semiconj
 import Mathlib.Algebra.Group.TypeTags
 import Mathlib.Algebra.Group.ULift
 import Mathlib.Algebra.Group.Units
+import Mathlib.Algebra.Group.WithOne.Basic
 import Mathlib.Algebra.Group.WithOne.Defs
 import Mathlib.Algebra.GroupPower.Basic
 import Mathlib.Algebra.GroupPower.Identities

commit a2d69bdfa90c9d6136e403c8069143371af4e24b

fix: reintroduce to_fun_as_coe as simp lemma (#931)
This is a proposed fix for the broken simp calls in #922. See https://leanprover.zulipchat.com/#narrow/stream/287929-mathlib4/topic/.60simp.60.20calls.20broken.20in.20mathlib4.23922/near/314783451 for more details. Note that `to_fun_as_coe` was a syntactic tautology when it was ported, but this is no longer the case because we now have `FunLike`.

Mathlib/Algebra/Group/WithOne/Basic.lean
@@ -129,29 +129,32 @@ theorem map_comp (f : α →ₙ* β) (g : β →ₙ* γ) : map (g.comp f) = (map
 #align with_one.map_comp WithOne.map_comp
 #align with_zero.map_comp WithZero.map_comp
 
+-- porting note: this used to have `@[simps apply]` but it was generating lemmas which
+-- weren't in simp normal form.
 /-- A version of `Equiv.optionCongr` for `WithOne`. -/
-@[to_additive "A version of `equiv.option_congr` for `with_zero`.", simps apply]
+@[to_additive "A version of `equiv.option_congr` for `with_zero`."]
 def _root_.MulEquiv.withOneCongr (e : α ≃* β) : WithOne α ≃* WithOne β :=
   { map e.toMulHom with
     toFun := map e.toMulHom, invFun := map e.symm.toMulHom,
     left_inv := fun x => (map_map _ _ _).trans <| by
-      -- porting note: in mathlib3 this worked as: `induction x using WithOne.cases_on <;> simp`
-      induction x using WithOne.cases_on
-      · simp
-      · simp only [map_coe, MulHom.coe_mk, map_comp, MonoidHom.coe_comp, Function.comp_apply,
-          MulEquiv.toEquiv_symm, coe_inj]
-        apply Equiv.symm_apply_apply,
-      -- porting note: I think because of the way coercions are handled, this doesn't get changed
-      -- by `simp` into something where `Equiv.symm_apply_apply` automatically applies.
+      induction x using WithOne.cases_on <;> simp
     right_inv := fun x => (map_map _ _ _).trans <| by
-      -- porting note: in mathlib3 this worked as: `induction x using WithOne.cases_on <;> simp`
-      induction x using WithOne.cases_on
-      · simp
-      · simp only [map_coe, MulHom.coe_mk, MulEquiv.toEquiv_symm, map_comp, MonoidHom.coe_comp,
-          Function.comp_apply, coe_inj]
-        apply Equiv.apply_symm_apply }
+      induction x using WithOne.cases_on <;> simp }
 #align mul_equiv.with_one_congr MulEquiv.withOneCongr
 #align add_equiv.with_zero_congr AddEquiv.withZeroCongr
+
+-- porting note: an approximation to this was being generated by `@[simps apply]` but it
+-- mentioned Equiv.toFun so was not in simp normal form. This might be a bug in `@[simps].
+@[simp] theorem _root_.MulEquiv.withOneCongr_apply {α β : Type _} [Mul α] [Mul β] (e : α ≃* β)
+  (a : WithOne α) : (MulEquiv.withOneCongr e).toEquiv a = (map (MulEquiv.toMulHom e)) a := rfl
+#align mul_equiv.with_one_congr_apply MulEquiv.withOneCongr_apply
+
+-- porting note: `@[to_additive, simps apply]` was not generating this lemma at the
+-- time of writing this note. 
+@[simp] theorem _root_.AddEquiv.withZeroCongr_apply {α β : Type _} [Add α] [Add β] (e : α ≃+ β)
+  (a : WithZero α) :
+    (AddEquiv.withZeroCongr e).toEquiv a = (WithZero.map (AddEquiv.toAddHom e)) a := rfl
+#align add_equiv.with_zero_congr_apply AddEquiv.withZeroCongr_apply
 
 -- porting note: for this declaration and the two below I added the `to_additive` attribute because
 -- it seemed to be missing from mathlib3, hence the lack of additive `#align`s.

Mathlib/Algebra/Hom/Equiv/Basic.lean
@@ -529,8 +529,10 @@ theorem map_ne_one_iff {M N} [MulOneClass M] [MulOneClass N] (h : M ≃* N) {x :
 #align mul_equiv.map_ne_one_iff MulEquiv.map_ne_one_iff
 #align add_equiv.map_ne_zero_iff AddEquiv.map_ne_zero_iff
 
+-- porting note: Mathlib 3 had `@[simps apply]` but right now in Lean 4 it's generating
+-- simp lemmas which don't lint.
 /-- A bijective `Semigroup` homomorphism is an isomorphism -/
-@[to_additive "A bijective `AddSemigroup` homomorphism is an isomorphism", simps apply]
+@[to_additive "A bijective `AddSemigroup` homomorphism is an isomorphism"]
 noncomputable def ofBijective {M N F} [Mul M] [Mul N] [MulHomClass F M N]
 (f : F) (hf : Function.Bijective f) :
     M ≃* N :=
@@ -538,11 +540,20 @@ noncomputable def ofBijective {M N F} [Mul M] [Mul N] [MulHomClass F M N]
 #align mul_equiv.of_bijective MulEquiv.ofBijective
 #align add_equiv.of_bijective AddEquiv.ofBijective
 
-@[simp]
+-- porting note: `@[simps apply]` should be making this lemma but it actually makes
+-- a lemma with `toFun` which isn't in simp normal form.
+@[simp, to_additive] theorem ofBijective_apply {M N F} [Mul M] [Mul N] [MulHomClass F M N]
+    (f : F) (hf : Function.Bijective f) (a : M) : (MulEquiv.ofBijective f hf).toEquiv a = f a :=
+  rfl
+#align mul_equiv.of_bijective_apply MulEquiv.ofBijective_apply
+#align add_equiv.of_bijective_apply AddEquiv.ofBijective_apply
+
+@[simp, to_additive]
 theorem ofBijective_apply_symm_apply {M N} [MulOneClass M] [MulOneClass N] {n : N} (f : M →* N)
     (hf : Function.Bijective f) : f ((Equiv.ofBijective f hf).symm n) = n :=
   (MulEquiv.ofBijective f hf).apply_symm_apply n
 #align mul_equiv.of_bijective_apply_symm_apply MulEquiv.ofBijective_apply_symm_apply
+#align add_equiv.of_bijective_apply_symm_apply AddEquiv.ofBijective_apply_symm_apply
 
 /-- Extract the forward direction of a multiplicative equivalence
 as a multiplication-preserving function.
@@ -589,8 +600,9 @@ for multiplicative maps from a monoid to a commutative monoid.
 -/
 @[to_additive
   "An additive analogue of `Equiv.arrowCongr`,
-  for additive maps from an additive monoid to a commutative additive monoid.",
-  simps apply]
+  for additive maps from an additive monoid to a commutative additive monoid."]
+-- porting note: @[simps apply] removed because it was making a lemma which
+-- wasn't in simp normal form.
 def monoidHomCongr {M N P Q} [MulOneClass M] [MulOneClass N] [CommMonoid P] [CommMonoid Q]
   (f : M ≃* N) (g : P ≃* Q) :
   (M →* P) ≃* (N →* Q) where
@@ -601,6 +613,14 @@ def monoidHomCongr {M N P Q} [MulOneClass M] [MulOneClass N] [CommMonoid P] [Com
   map_mul' h k := by ext; simp
 #align mul_equiv.monoid_hom_congr MulEquiv.monoidHomCongr
 #align add_equiv.add_monoid_hom_congr AddEquiv.addMonoidHomCongr
+
+@[simp, to_additive] theorem monoidHomCongr_apply {M N P Q} [MulOneClass M] [MulOneClass N]
+    [CommMonoid P] [CommMonoid Q] (f : M ≃* N) (g : P ≃* Q) (h : M →* P) :
+    (MulEquiv.monoidHomCongr f g).toEquiv h = MonoidHom.comp (MulEquiv.toMonoidHom g)
+    (MonoidHom.comp h (MulEquiv.toMonoidHom (MulEquiv.symm f))) :=
+  rfl
+#align mul_equiv.monoid_hom_congr_apply MulEquiv.monoidHomCongr_apply
+#align add_equiv.add_monoid_hom_congr_apply AddEquiv.addMonoidHomCongr_apply
 
 /-- A family of multiplicative equivalences `Π j, (Ms j ≃* Ns j)` generates a
 multiplicative equivalence between `Π j, Ms j` and `Π j, Ns j`.
@@ -647,13 +667,28 @@ theorem piCongrRight_trans {η : Type _} {Ms Ns Ps : η → Type _} [∀ j, Mul 
 index. -/
 @[to_additive
   "A family indexed by a nonempty subsingleton type is
-  equivalent to the element at the single index.",
-  simps]
+  equivalent to the element at the single index."]
 def piSubsingleton {ι : Type _} (M : ι → Type _) [∀ j, Mul (M j)] [Subsingleton ι]
   (i : ι) : (∀ j, M j) ≃* M i :=
   { Equiv.piSubsingleton M i with map_mul' := fun _ _ => Pi.mul_apply _ _ _ }
 #align mul_equiv.Pi_subsingleton MulEquiv.piSubsingleton
 #align add_equiv.Pi_subsingleton AddEquiv.piSubsingleton
+
+-- porting note: the next two lemmas should be being generated by `@[to_additive, simps]`.
+-- They are added manually because `@[simps]` is currently generating lemmas with `toFun` in
+@[simp, to_additive] theorem piSubsingleton_apply {ι : Type _} (M : ι → Type _) [∀ j, Mul (M j)]
+    [Subsingleton ι] (i : ι) (f : (x : ι) → M x) : (MulEquiv.piSubsingleton M i).toEquiv f = f i :=
+  rfl
+#align mul_equiv.Pi_subsingleton_apply MulEquiv.piSubsingleton_apply
+#align add_equiv.Pi_subsingleton_apply AddEquiv.piSubsingleton_apply
+
+@[simp, to_additive] theorem piSubsingleton_symmApply {ι : Type _} (M : ι → Type _) [∀ j, Mul (M j)]
+    [Subsingleton ι] (i : ι) (x : M i) (b : ι) :
+    (MulEquiv.symm (MulEquiv.piSubsingleton M i)) x b =
+    cast (Subsingleton.elim i b ▸ rfl : M i = M b) x :=
+rfl
+#align mul_equiv.Pi_subsingleton_symmApply MulEquiv.piSubsingleton_symmApply
+#align add_equiv.Pi_subsingleton_symmApply AddEquiv.piSubsingleton_symmApply
 
 /-!
 # Groups
@@ -677,6 +712,10 @@ protected theorem map_div [Group G] [DivisionMonoid H] (h : G ≃* H) (x y : G) 
 
 end MulEquiv
 
+-- porting note: we want to add
+-- `@[simps (config := { fullyApplied := false })]`
+-- here, but it generates simp lemmas which aren't in simp normal form
+-- (they have `toFun` in)
 /-- Given a pair of multiplicative homomorphisms `f`, `g` such that `g.comp f = id` and
 `f.comp g = id`, returns an multiplicative equivalence with `toFun = f` and `invFun = g`. This
 constructor is useful if the underlying type(s) have specialized `ext` lemmas for multiplicative
@@ -685,8 +724,7 @@ homomorphisms. -/
   "Given a pair of additive homomorphisms `f`, `g` such that `g.comp f = id` and
   `f.comp g = id`, returns an additive equivalence with `toFun = f` and `invFun = g`. This
   constructor is useful if the underlying type(s) have specialized `ext` lemmas for additive
-  homomorphisms.",
-  simps (config := { fullyApplied := false })]
+  homomorphisms."]
 def MulHom.toMulEquiv [Mul M] [Mul N] (f : M →ₙ* N) (g : N →ₙ* M) (h₁ : g.comp f = MulHom.id _)
   (h₂ : f.comp g = MulHom.id _) : M ≃* N where
   toFun := f
@@ -697,6 +735,24 @@ def MulHom.toMulEquiv [Mul M] [Mul N] (f : M →ₙ* N) (g : N →ₙ* M) (h₁ 
 #align mul_hom.to_mul_equiv MulHom.toMulEquiv
 #align add_hom.to_add_equiv AddHom.toAddEquiv
 
+-- porting note: the next two lemmas were added manually because `@[simps]` is generating
+-- lemmas with `toFun` in
+@[simp, to_additive] theorem MulHom.toMulEquiv_apply [Mul M] [Mul N] (f : M →ₙ* N) (g : N →ₙ* M)
+    (h₁ : g.comp f = MulHom.id _) (h₂ : f.comp g = MulHom.id _) :
+    ((MulHom.toMulEquiv f g h₁ h₂).toEquiv : M → N) = f :=
+  rfl
+#align mul_hom.to_mul_equiv_apply MulHom.toMulEquiv_apply
+#align add_hom.to_add_equiv_apply AddHom.toAddEquiv_apply
+
+@[simp, to_additive] theorem MulHom.toMulEquiv_symmApply [Mul M] [Mul N] (f : M →ₙ* N) (g : N →ₙ* M)
+    (h₁ : g.comp f = MulHom.id _) (h₂ : f.comp g = MulHom.id _) :
+    (MulEquiv.symm (MulHom.toMulEquiv f g h₁ h₂) : N → M) = ↑g :=
+  rfl
+#align mul_hom.to_mul_equiv_symm_apply MulHom.toMulEquiv_symmApply
+#align add_hom.to_add_equiv_symm_apply AddHom.toAddEquiv_symmApply
+
+-- porting note: `@[simps (config := { fullyApplied := false })]` generates a simp lemma
+-- which is not in simp normal form, so we add them manually
 /-- Given a pair of monoid homomorphisms `f`, `g` such that `g.comp f = id` and `f.comp g = id`,
 returns an multiplicative equivalence with `toFun = f` and `invFun = g`.  This constructor is
 useful if the underlying type(s) have specialized `ext` lemmas for monoid homomorphisms. -/
@@ -704,8 +760,7 @@ useful if the underlying type(s) have specialized `ext` lemmas for monoid homomo
   "Given a pair of additive monoid homomorphisms `f`, `g` such that `g.comp f = id`
   and `f.comp g = id`, returns an additive equivalence with `toFun = f` and `invFun = g`.  This
   constructor is useful if the underlying type(s) have specialized `ext` lemmas for additive
-  monoid homomorphisms.",
-  simps (config := { fullyApplied := false })]
+  monoid homomorphisms."]
 def MonoidHom.toMulEquiv [MulOneClass M] [MulOneClass N] (f : M →* N) (g : N →* M)
   (h₁ : g.comp f = MonoidHom.id _) (h₂ : f.comp g = MonoidHom.id _) : M ≃* N where
   toFun := f
@@ -715,6 +770,23 @@ def MonoidHom.toMulEquiv [MulOneClass M] [MulOneClass N] (f : M →* N) (g : N �
   map_mul' := f.map_mul
 #align monoid_hom.to_mul_equiv MonoidHom.toMulEquiv
 #align add_monoid_hom.to_add_mul_equiv AddMonoidHom.toAddEquiv
+
+-- porting note: the next 2 lemmas should be being generated by
+-- `@[to_additive, simps (config := { fullyApplied := false })]`
+-- but right now it's generating `simp` lemmas which aren't in `simp` normal form.
+@[simp, to_additive] theorem MonoidHom.toMulEquiv_apply [MulOneClass M] [MulOneClass N] (f : M →* N)
+    (g : N →* M) (h₁ : g.comp f = MonoidHom.id _) (h₂ : f.comp g = MonoidHom.id _) :
+    ((MonoidHom.toMulEquiv f g h₁ h₂).toEquiv : M → N) = ↑f :=
+  rfl
+#align monoid_hom.to_mul_equiv_apply MonoidHom.toMulEquiv_apply
+#align add_monoid_hom.to_add_equiv_apply AddMonoidHom.toAddEquiv_apply
+
+@[simp, to_additive] theorem MonoidHom.toMulEquiv_symmApply [MulOneClass M] [MulOneClass N]
+    (f : M →* N) (g : N →* M) (h₁ : g.comp f = MonoidHom.id _) (h₂ : f.comp g = MonoidHom.id _) :
+    (MulEquiv.symm (MonoidHom.toMulEquiv f g h₁ h₂) : N → M) = g :=
+  rfl
+#align monoid_hom.to_mul_equiv_symm_apply MonoidHom.toMulEquiv_symmApply
+#align add_monoid_hom.to_add_equiv_symm_apply AddMonoidHom.toAddEquiv_symmApply
 
 namespace Equiv
 

Mathlib/Algebra/Hom/Equiv/Units/Basic.lean
@@ -220,13 +220,27 @@ end Equiv
 
 -- Porting note: we don't put `@[simp]` on the additive version;
 -- mysteriously simp can already prove that one (although not the multiplicative one)!
+-- porting note: `@[simps apply]` removed because right now it's generating lemmas which
+-- aren't in simp normal form (they contain a `toFun`)
 /-- In a `division_comm_monoid`, `equiv.inv` is a `mul_equiv`. There is a variant of this
 `mul_equiv.inv' G : G ≃* Gᵐᵒᵖ` for the non-commutative case. -/
-@[to_additive "When the `add_group` is commutative, `equiv.neg` is an `add_equiv`.", simps apply]
+@[to_additive "When the `add_group` is commutative, `equiv.neg` is an `add_equiv`."]
 def MulEquiv.inv (G : Type _) [DivisionCommMonoid G] : G ≃* G :=
   { Equiv.inv G with toFun := Inv.inv, invFun := Inv.inv, map_mul' := mul_inv }
 #align mul_equiv.inv MulEquiv.inv
 #align add_equiv.neg AddEquiv.neg
+
+-- porting note: this lemma and the next are added manually because `simps` was
+-- not quite generating the right thing
+@[simp] theorem MulEquiv.inv_apply (G : Type _) [DivisionCommMonoid G] (a : G) :
+    (MulEquiv.inv G).toEquiv a = a⁻¹ :=
+  rfl
+#align mul_equiv.inv_apply MulEquiv.inv_apply
+
+@[simp] theorem AddEquiv.neg_apply (G : Type _) [SubtractionCommMonoid G] (a : G) :
+    (AddEquiv.neg G).toEquiv a = -a :=
+  rfl
+#align add_equiv.neg_apply AddEquiv.neg_apply
 
 @[simp]
 theorem MulEquiv.inv_symm (G : Type _) [DivisionCommMonoid G] :

Mathlib/Logic/Equiv/Defs.lean
@@ -158,8 +158,15 @@ protected def trans (e₁ : α ≃ β) (e₂ : β ≃ γ) : α ≃ γ :=
 instance : Trans Equiv Equiv Equiv where
   trans := Equiv.trans
 
--- porting note: this lemma is now useless since coercions are eagerly unfolded
--- @[simp] theorem to_fun_as_coe (e : α ≃ β) : e.toFun = e := rfl
+-- porting note: this is not a syntactic tautology any more because
+-- the coercion from `e` to a function is now `FunLike.coe` not `e.toFun`
+@[simp] theorem to_fun_as_coe (e : α ≃ β) : e.toFun = e := rfl
+
+-- porting note: `simp` should prove this using `to_fun_as_coe`, but it doesn't.
+-- This might be a bug in `simp` -- see https://github.com/leanprover/lean4/issues/1937
+-- If this issue is fixed then the simp linter probably will start complaining, and
+-- this theorem can be deleted hopefully without breaking any `simp` proofs.
+@[simp] theorem to_fun_as_coe_apply (e : α ≃ β) (x : α) : e.toFun x = e x := rfl
 
 @[simp] theorem inv_fun_as_coe (e : α ≃ β) : e.invFun = e.symm := rfl
 

Mathlib/Order/RelIso/Group.lean
@@ -30,14 +30,14 @@ instance : Group (r ≃r r) where
   mul_left_inv f := ext f.symm_apply_apply
 
 @[simp]
-theorem toFun_one : (1 : r ≃r r).toFun = id :=
+theorem coe_one : ((1 : r ≃r r) : α → α) = id :=
   rfl
-#align rel_iso.coe_one RelIso.toFun_one
+#align rel_iso.coe_one RelIso.coe_one
 
 @[simp]
-theorem toFun_mul (e₁ e₂ : r ≃r r) : (e₁ * e₂).toFun = e₁ ∘ e₂ :=
+theorem coe_mul (e₁ e₂ : r ≃r r) : ((e₁ * e₂) : α → α) = e₁ ∘ e₂ :=
   rfl
-#align rel_iso.coe_mul RelIso.toFun_mul
+#align rel_iso.coe_mul RelIso.coe_mul
 
 theorem mul_apply (e₁ e₂ : r ≃r r) (x : α) : (e₁ * e₂) x = e₁ (e₂ x) :=
   rfl