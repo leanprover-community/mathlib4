9c40c1615be3d3de9190334fd03625646dd2f831
Moritz Doll <moritz.doll@googlemail.com>
2022-12-20 20:57:45 +1100

    add header

--- Mathlib/Algebra/Order/Ring/WithTop.lean
+++ Mathlib/Algebra/Order/Ring/WithTop.lean
@@ -2,6 +2,10 @@
 Copyright (c) 2016 Jeremy Avigad. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jeremy Avigad, Leonardo de Moura, Mario Carneiro
+! This file was ported from Lean 3 source module algebra.order.ring.with_top
+! leanprover-community/mathlib commit 550b58538991c8977703fdeb7c9d51a5aa27df11
+! Please do not edit these lines, except to modify the commit id
+! if you have ported upstream changes.
 -/
 import Mathlib.Algebra.Hom.Ring
 import Mathlib.Algebra.Order.Monoid.WithTop
