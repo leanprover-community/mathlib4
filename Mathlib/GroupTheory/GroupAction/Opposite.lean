5c9085e2d4b48b8cc2d6f50c3489c136aa6b14be
Johan Commelin <johan@commelin.net>
2022-12-20 12:18:48 +0000

    looks good to me

--- Mathlib/GroupTheory/GroupAction/Opposite.lean
+++ Mathlib/GroupTheory/GroupAction/Opposite.lean
@@ -164,5 +164,3 @@ instance CancelMonoidWithZero.to_has_faithful_opposite_scalar [CancelMonoidWithZ
   ⟨fun h => unop_injective <| mul_left_cancel₀ one_ne_zero (h 1)⟩
 #align
   cancel_monoid_with_zero.to_has_faithful_opposite_scalar CancelMonoidWithZero.to_has_faithful_opposite_scalar
-
-#lint
