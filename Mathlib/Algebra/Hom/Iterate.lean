97bc4cafc39bf4f3e68c1e6648bd017c1557caeb
Winston Yin <winstonyin@gmail.com>
2022-12-20 15:04:19 -0800

    linter happy

--- Mathlib/Algebra/Hom/Iterate.lean
+++ Mathlib/Algebra/Hom/Iterate.lean
@@ -16,9 +16,9 @@ import Mathlib.GroupTheory.GroupAction.Opposite
 # Iterates of monoid and ring homomorphisms
 
 Iterate of a monoid/ring homomorphism is a monoid/ring homomorphism but it has a wrong type, so Lean
-can't apply lemmas like `monoid_hom.map_one` to `f^[n] 1`. Though it is possible to define
+can't apply lemmas like `MonoidHom.map_one` to `f^[n] 1`. Though it is possible to define
 a monoid structure on the endomorphisms, quite often we do not want to convert from
-`M →* M` to `monoid.End M` and from `f^[n]` to `f^n` just to apply a simple lemma.
+`M →* M` to `Monoid.End M` and from `f^[n]` to `f^n` just to apply a simple lemma.
 
 So, we restate standard `*_hom.map_*` lemmas under names `*_hom.iterate_map_*`.
 
@@ -40,7 +40,7 @@ theorem hom_coe_pow {F : Type _} [Monoid F] (c : F → M → M) (h1 : c 1 = id)
   | 0 => by
     rw [pow_zero, h1]
     rfl
-  | n + 1 => by rw [pow_succ, iterate_succ', hmul, hom_coe_pow]
+  | n + 1 => by rw [pow_succ, iterate_succ', hmul, hom_coe_pow c h1 hmul f n]
 #align hom_coe_pow hom_coe_pow
 
 namespace MonoidHom
@@ -82,13 +82,13 @@ theorem iterate_map_zpow (f : G →* G) (n : ℕ) (a) (m : ℤ) : (f^[n]) (a ^ m
 #align monoid_hom.iterate_map_zpow MonoidHom.iterate_map_zpow
 
 theorem coe_pow {M} [CommMonoid M] (f : Monoid.End M) (n : ℕ) : ⇑(f ^ n) = f^[n] :=
-  hom_coe_pow _ rfl (fun f g => rfl) _ _
+  hom_coe_pow _ rfl (fun _ _ => rfl) _ _
 #align monoid_hom.coe_pow MonoidHom.coe_pow
 
 end MonoidHom
 
 theorem Monoid.End.coe_pow {M} [Monoid M] (f : Monoid.End M) (n : ℕ) : ⇑(f ^ n) = f^[n] :=
-  hom_coe_pow _ rfl (fun f g => rfl) _ _
+  hom_coe_pow _ rfl (fun _ _ => rfl) _ _
 #align monoid.End.coe_pow Monoid.End.coe_pow
 
 -- we define these manually so that we can pick a better argument order
@@ -100,18 +100,18 @@ theorem iterate_map_smul (f : M →+ M) (n m : ℕ) (x : M) : (f^[n]) (m • x) 
   f.toMultiplicative.iterate_map_pow n x m
 #align add_monoid_hom.iterate_map_smul AddMonoidHom.iterate_map_smul
 
-attribute [to_additive, to_additive_reorder 5] MonoidHom.iterate_map_pow
+attribute [to_additive (reorder := 5)] MonoidHom.iterate_map_pow
 
 theorem iterate_map_zsmul (f : G →+ G) (n : ℕ) (m : ℤ) (x : G) : (f^[n]) (m • x) = m • (f^[n]) x :=
   f.toMultiplicative.iterate_map_zpow n x m
 #align add_monoid_hom.iterate_map_zsmul AddMonoidHom.iterate_map_zsmul
 
-attribute [to_additive, to_additive_reorder 5] MonoidHom.iterate_map_zpow
+attribute [to_additive (reorder := 5)] MonoidHom.iterate_map_zpow
 
 end AddMonoidHom
 
 theorem AddMonoid.End.coe_pow {A} [AddMonoid A] (f : AddMonoid.End A) (n : ℕ) : ⇑(f ^ n) = f^[n] :=
-  hom_coe_pow _ rfl (fun f g => rfl) _ _
+  hom_coe_pow _ rfl (fun _ _ => rfl) _ _
 #align add_monoid.End.coe_pow AddMonoid.End.coe_pow
 
 namespace RingHom
@@ -121,7 +121,7 @@ section Semiring
 variable {R : Type _} [Semiring R] (f : R →+* R) (n : ℕ) (x y : R)
 
 theorem coe_pow (n : ℕ) : ⇑(f ^ n) = f^[n] :=
-  hom_coe_pow _ rfl (fun f g => rfl) f n
+  hom_coe_pow _ rfl (fun _ _ => rfl) f n
 #align ring_hom.coe_pow RingHom.coe_pow
 
 theorem iterate_map_one : (f^[n]) 1 = 1 :=
@@ -178,8 +178,8 @@ variable [Monoid G] (a : G) (n : ℕ)
 @[simp, to_additive]
 theorem smul_iterate [MulAction G H] : ((· • ·) a : H → H)^[n] = (· • ·) (a ^ n) :=
   funext fun b =>
-    Nat.recOn n (by rw [iterate_zero, id.def, pow_zero, one_smul]) fun n ih => by
-      rw [iterate_succ', comp_app, ih, pow_succ, mul_smul]
+    Nat.recOn n (by dsimp only; rw [iterate_zero, id.def, pow_zero, one_smul G b])
+    fun n ih => by dsimp only; rw [iterate_succ', comp_apply, ih, pow_succ, mul_smul]
 #align smul_iterate smul_iterate
 
 @[simp, to_additive]
@@ -197,7 +197,7 @@ theorem mul_right_iterate_apply_one : ((· * a)^[n]) 1 = a ^ n := by simp [mul_r
 #align mul_right_iterate_apply_one mul_right_iterate_apply_one
 
 @[simp, to_additive]
-theorem pow_iterate (n : ℕ) (j : ℕ) : (fun x : G => x ^ n)^[j] = fun x => x ^ n ^ j :=
+theorem pow_iterate (n : ℕ) (j : ℕ) : (fun x : G => x ^ n)^[j] = fun x : G => x ^ n ^ j :=
   letI : MulAction ℕ G :=
     { smul := fun n g => g ^ n
       one_smul := pow_one
@@ -229,11 +229,11 @@ variable [Semigroup G] {a b c : G}
 @[to_additive]
 theorem SemiconjBy.function_semiconj_mul_left (h : SemiconjBy a b c) :
     Function.Semiconj ((· * ·) a) ((· * ·) b) ((· * ·) c) := fun j => by
-  rw [← mul_assoc, h.eq, mul_assoc]
+  dsimp only; rw [← mul_assoc, h.eq, mul_assoc]
 #align semiconj_by.function_semiconj_mul_left SemiconjBy.function_semiconj_mul_left
 
 @[to_additive]
-theorem Commute.function_commute_mul_left (h : Commute a b) :
+theorem Commute.function_commute_mul_left (h : _root_.Commute a b) :
     Function.Commute ((· * ·) a) ((· * ·) b) :=
   SemiconjBy.function_semiconj_mul_left h
 #align commute.function_commute_mul_left Commute.function_commute_mul_left
@@ -244,7 +244,8 @@ theorem SemiconjBy.function_semiconj_mul_right_swap (h : SemiconjBy a b c) :
 #align semiconj_by.function_semiconj_mul_right_swap SemiconjBy.function_semiconj_mul_right_swap
 
 @[to_additive]
-theorem Commute.function_commute_mul_right (h : Commute a b) : Function.Commute (· * a) (· * b) :=
+theorem Commute.function_commute_mul_right (h : _root_.Commute a b) :
+  Function.Commute (· * a) (· * b) :=
   SemiconjBy.function_semiconj_mul_right_swap h
 #align commute.function_commute_mul_right Commute.function_commute_mul_right
 
