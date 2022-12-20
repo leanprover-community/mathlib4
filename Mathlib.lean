447419207f26183c80afbacc99cf22c42fddeed9
antoinelab01 <antoinelab01@gmail.com>
2022-12-20 13:42:52 +0000

    feat: port GroupTheory.GroupAction.Opposite (#1001)
    mathlib SHA : fc2ed6f838ce7c9b7c7171e58d78eaf7b438fb0e
    
    Co-authored-by: Scott Morrison <scott.morrison@gmail.com>
    Co-authored-by: Johan Commelin <johan@commelin.net>

--- Mathlib.lean
+++ Mathlib.lean
@@ -270,6 +270,7 @@ import Mathlib.Data.Vector
 import Mathlib.Dynamics.Basic
 import Mathlib.GroupTheory.EckmannHilton
 import Mathlib.GroupTheory.GroupAction.Defs
+import Mathlib.GroupTheory.GroupAction.Opposite
 import Mathlib.GroupTheory.GroupAction.Option
 import Mathlib.GroupTheory.GroupAction.Pi
 import Mathlib.GroupTheory.GroupAction.Prod
