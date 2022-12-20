db73cd404504c68a2b11225974cda028f5e6898e
Johan Commelin <johan@commelin.net>
2022-12-20 17:49:10 +0000

    chore: fix source header (#1127)

--- Mathlib/GroupTheory/GroupAction/Opposite.lean
+++ Mathlib/GroupTheory/GroupAction/Opposite.lean
@@ -3,7 +3,7 @@ Copyright (c) 2020 Eric Wieser. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Eric Wieser
 
-! This file was ported from Lean 3 source group_theory.group_action.opposite
+! This file was ported from Lean 3 source module group_theory.group_action.opposite
 ! leanprover-community/mathlib commit fc2ed6f838ce7c9b7c7171e58d78eaf7b438fb0e
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
