95df5d51c27941ccb5dd5aa001bcacce8f95fa01
Arien Malec <arien.malec@gmail.com>
2022-12-21 13:37:56 -0800

    reexport in Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -12,6 +12,7 @@ import Mathlib.Algebra.Field.Opposite
 import Mathlib.Algebra.Group.Basic
 import Mathlib.Algebra.Group.Commutator
 import Mathlib.Algebra.Group.Commute
+import Mathlib.Algebra.Group.Conj
 import Mathlib.Algebra.Group.Defs
 import Mathlib.Algebra.Group.Ext
 import Mathlib.Algebra.Group.InjSurj
