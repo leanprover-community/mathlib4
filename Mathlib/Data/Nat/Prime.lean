af1978022ea6370ca2f8e15e430fa2aa995c6c4d
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2022-12-22 14:01:21 +0100

    Port dvd_of_forall_prime_mul_dvd
    From leanprover-community/mathlib#17997

--- Mathlib/Data/Nat/Prime.lean
+++ Mathlib/Data/Nat/Prime.lean
@@ -472,6 +472,13 @@ theorem exists_prime_and_dvd {n : ℕ} (hn : n ≠ 1) : ∃ p, Prime p ∧ p ∣
   ⟨minFac n, minFac_prime hn, minFac_dvd _⟩
 #align nat.exists_prime_and_dvd Nat.exists_prime_and_dvd
 
+theorem dvd_of_forall_prime_mul_dvd {a b : ℕ}
+    (hdvd : ∀ p : ℕ, p.Prime → p ∣ a → p * a ∣ b) : a ∣ b := by
+  obtain rfl | ha := eq_or_ne a 1
+  · apply one_dvd
+  obtain ⟨p, hp⟩ := exists_prime_and_dvd ha
+  exact trans (dvd_mul_left a p) (hdvd p hp.1 hp.2)
+
 /-- Euclid's theorem on the **infinitude of primes**.
 Here given in the form: for every `n`, there exists a prime number `p ≥ n`. -/
 theorem exists_infinite_primes (n : ℕ) : ∃ p, n ≤ p ∧ Prime p :=
