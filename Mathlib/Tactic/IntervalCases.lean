caf0ea149e71ad1b7b83ca8feb8f5cc3d6d3af39
Mario Carneiro <di.gama@gmail.com>
2022-12-21 20:00:55 -0500

    fix header

--- Mathlib/Tactic/IntervalCases.lean
+++ Mathlib/Tactic/IntervalCases.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2022 Mario Carneiro. All rights reserved.
+Copyright (c) 2019 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Mario Carneiro, Heather Macbeth
+Authors: Scott Morrison, Mario Carneiro
 -/
 import Mathlib.Tactic.NormNum
 import Mathlib.Tactic.FinCases
