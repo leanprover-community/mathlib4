e487101e1f6050324806dd3f84ee471f706c28ea
Lukas Miaskiwskyi <lukas.mias@gmail.com>
2022-12-22 16:56:47 +0100

    porting adjustments

--- Mathlib/Data/Int/Sqrt.lean
+++ Mathlib/Data/Int/Sqrt.lean
@@ -8,13 +8,13 @@ Authors: Kenny Lau
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
-import Mathbin.Data.Nat.Sqrt
+import Mathlib.Data.Nat.Sqrt
 
 /-!
 # Square root of integers
 
-This file defines the square root function on integers. `int.sqrt z` is the greatest integer `r`
-such that `r * r ≤ z`. If `z ≤ 0`, then `int.sqrt z = 0`.
+This file defines the square root function on integers. `Int.sqrt z` is the greatest integer `r`
+such that `r * r ≤ z`. If `z ≤ 0`, then `Int.sqrt z = 0`.
 -/
 
 
@@ -23,17 +23,17 @@ namespace Int
 /-- `sqrt z` is the square root of an integer `z`. If `z` is positive, it returns the largest
 integer `r` such that `r * r ≤ n`. If it is negative, it returns `0`. For example, `sqrt (-1) = 0`,
 `sqrt 1 = 1`, `sqrt 2 = 1` -/
-@[pp_nodot]
+-- @[pp_nodot] porting note: unknown attribute
 def sqrt (z : ℤ) : ℤ :=
   Nat.sqrt <| Int.toNat z
 #align int.sqrt Int.sqrt
 
 theorem sqrt_eq (n : ℤ) : sqrt (n * n) = n.natAbs := by
-  rw [sqrt, ← nat_abs_mul_self, to_nat_coe_nat, Nat.sqrt_eq]
+  rw [sqrt, ← natAbs_mul_self, toNat_coe_nat, Nat.sqrt_eq]
 #align int.sqrt_eq Int.sqrt_eq
 
 theorem exists_mul_self (x : ℤ) : (∃ n, n * n = x) ↔ sqrt x * sqrt x = x :=
-  ⟨fun ⟨n, hn⟩ => by rw [← hn, sqrt_eq, ← Int.ofNat_mul, nat_abs_mul_self], fun h => ⟨sqrt x, h⟩⟩
+  ⟨fun ⟨n, hn⟩ => by rw [← hn, sqrt_eq, ← Int.ofNat_mul, natAbs_mul_self], fun h => ⟨sqrt x, h⟩⟩
 #align int.exists_mul_self Int.exists_mul_self
 
 theorem sqrt_nonneg (n : ℤ) : 0 ≤ sqrt n :=
@@ -41,4 +41,3 @@ theorem sqrt_nonneg (n : ℤ) : 0 ≤ sqrt n :=
 #align int.sqrt_nonneg Int.sqrt_nonneg
 
 end Int
-
