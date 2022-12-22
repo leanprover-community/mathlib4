bf4fd46a025b3343d60b3794eb8bceff829e8710
ART0 <18333981+0Art0@users.noreply.github.com>
2022-12-22 15:05:40 +0530

    Add original copyright message with `Ported by`

--- Mathlib/Data/Nat/ForSqrtNonEssential.lean
+++ Mathlib/Data/Nat/ForSqrtNonEssential.lean
@@ -1,3 +1,10 @@
+/-
+Copyright (c) 2015 Microsoft Corporation. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Leonardo de Moura, Johannes Hölzl, Mario Carneiro
+Ported by: Kevin Buzzard, Johan Commelin, Siddhartha Gadgil, Anand Rao
+-/
+
 import Mathlib.Data.Int.Order.Basic
 import Mathlib.Data.Nat.Size
 
