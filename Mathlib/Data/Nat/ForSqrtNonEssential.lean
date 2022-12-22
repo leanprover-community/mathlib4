bd5a88821dbd55be8391bc38cc9021ed5c328b98
ART0 <18333981+0Art0@users.noreply.github.com>
2022-12-22 14:58:24 +0530

    Add extra lemmas

--- Mathlib/Data/Nat/ForSqrtNonEssential.lean
+++ Mathlib/Data/Nat/ForSqrtNonEssential.lean
@@ -1,3 +1,4 @@
+import Mathlib.Data.Int.Order.Basic
 import Mathlib.Data.Nat.Size
 
 /-!
@@ -30,5 +31,22 @@ protected lemma div_mul_div_le (a b c d : ℕ) :
   transitivity ((a / b) * b) * ((c / d) * d)
   · apply le_of_eq; simp only [mul_assoc, mul_left_comm]
   · apply Nat.mul_le_mul <;> apply div_mul_le_self
+
+private abbrev iter_next (n guess : ℕ) : ℕ  :=
+    (guess + n / guess) / 2
+
+private lemma iter_fp_bound (n k : ℕ):
+    sqrt.iter n k ≤ iter_next n (sqrt.iter n k)  := by
+      unfold sqrt.iter
+      by_cases h : (k + n / k) / 2 < k
+      case pos => simp [if_pos h]; apply iter_fp_bound
+      case neg => simp [if_neg h, Nat.le_of_not_lt h]
+
+lemma am_gm_lemma (a b: ℤ) : 0 ≤ (a + b) * (a + b) - 4 * a * b := by
+  convert sq_nonneg (a - b)
+  simp only [sq, add_mul, mul_add, sub_mul, mul_sub, add_assoc, mul_comm b a, sub_eq_add_neg,
+    neg_mul, mul_neg, neg_neg, add_right_inj, neg_add_rev, neg_neg, add_left_comm _ (b*b)]
+  simp only [← two_mul, ← add_assoc, mul_neg, eq_neg_iff_add_eq_zero, mul_assoc]
+  simp only [← neg_mul, ← add_mul, _root_.mul_eq_zero, true_or]
 
 end Nat
