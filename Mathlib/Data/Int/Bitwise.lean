3e268d904b5f448b7d0a3e389660bb2ec4f7026b
Jon Eugster <eugster.jon@gmail.com>
2022-12-22 16:04:52 +0100

    some fixes

--- Mathlib/Data/Int/Bitwise.lean
+++ Mathlib/Data/Int/Bitwise.lean
@@ -27,10 +27,20 @@ namespace Int
 
 /-! ### bitwise ops -/
 
--- Porting note: This used to be in core.
+-- Porting note: These come from core `init.data.int.bitwise`
+-- and should probably go there again...
+def div2 : ℤ → ℤ
+  | ofNat n => n.div2
+  | negSucc n => negSucc n.div2
+
 def bodd : ℤ → Bool
   | ofNat n => n.bodd
   | negSucc n => not (n.bodd)
+
+@[deprecated]
+def bit (b : Bool) : ℤ → ℤ := cond b bit1 bit0
+
+-- Porting node: end of copied section
 
 @[simp]
 theorem bodd_zero : bodd 0 = false :=
@@ -63,16 +73,10 @@ theorem bodd_subNatNat (m n : ℕ) : bodd (subNatNat m n) = xor m.bodd n.bodd :=
     rw [Nat.bodd_succ, ←Bool.xor_assoc, Bool.xor_not_right, Bool.true_xor]
     rfl
 -- Porting note: Hevily refactored proof, used to work all with `simp`:
--- apply sub_nat_nat_elim m n fun m n i => bodd i = xor m.bodd n.bodd <;> intros <;> simp <;>
--- cases i.bodd <;>
+-- `apply sub_nat_nat_elim m n fun m n i => bodd i = xor m.bodd n.bodd <;> intros <;> simp <;>`
+-- `cases i.bodd <;> simp`
 
 #align int.bodd_sub_nat_nat Int.bodd_subNatNat
-
---lemma negOfNat_def (n : ℕ) : -ofNat n = negOfNat n := rfl
-#check ofNat_eq_coe
-#check negOfNat_eq
-example (n : ℕ) : bodd -[n+1] = !(bodd n) := rfl
-
 
 @[simp]
 theorem bodd_negOfNat (n : ℕ) : bodd (negOfNat n) = n.bodd := by
@@ -95,28 +99,45 @@ theorem bodd_neg (n : ℤ) : bodd (-n) = bodd n := by
 -- `cases n <;> simp [Neg.neg, Int.coe_nat_eq, Int.neg, bodd, -of_nat_eq_coe]`
 #align int.bodd_neg Int.bodd_neg
 
+@[simp] lemma h1 (n m : ℕ) : ↑m + -[n+1] = subNatNat m n.succ := rfl
+@[simp] lemma h2 (n m : ℕ) : -[m+1] + n = subNatNat n m.succ := rfl
+@[simp] lemma h3 (n m : ℕ) : -[m+1] + -[n+1] = negSucc ((m + n).succ) := rfl
+@[simp] lemma h4 (n m : ℕ) : ↑m * -[n+1] = negOfNat (m * n.succ) := rfl
+@[simp] lemma h5 (n m : ℕ) : -[m+1] * n = negOfNat (m.succ * n) := rfl
+@[simp] lemma h6 (n m : ℕ) : -[m+1] * -[n+1] = ↑(m.succ * n.succ) := rfl
+
 @[simp]
 theorem bodd_add (m n : ℤ) : bodd (m + n) = xor (bodd m) (bodd n) := by
-  cases' m with m m <;> cases' n with n n <;>
-  simp only [ofNat_eq_coe, bodd_coe, ←Nat.bodd_add, ←Nat.cast_add]
-
-
-
-  --simp [Int.add, Bool.xor_comm]
+  cases' m with m m <;>
+  cases' n with n n <;>
+  simp only [ofNat_eq_coe, ofNat_add_negSucc, negSucc_add_ofNat,
+             negSucc_add_negSucc, bodd_subNatNat] <;>
+  simp only [negSucc_coe, bodd_neg, bodd_coe, ←Nat.bodd_add, Bool.xor_comm, ←Nat.cast_add]
+  rw [←Nat.succ_add, add_assoc]
+-- Porting note: Hevily refactored proof, used to work all with `simp`:
+-- `by cases m with m m; cases n with n n; unfold has_add.add;`
+-- `simp [int.add, -of_nat_eq_coe, bool.bxor_comm]`
 #align int.bodd_add Int.bodd_add
+
+example (a b : ℕ) : (a : ℤ) * (b : ℤ) = ↑(a * b) := rfl
 
 @[simp]
 theorem bodd_mul (m n : ℤ) : bodd (m * n) = (bodd m && bodd n) := by
   cases' m with m m <;> cases' n with n n <;>
-    simp [← Int.mul_def, Int.mul, -of_nat_eq_coe, Bool.xor_comm]
+  simp only [ofNat_eq_coe, ofNat_mul_negSucc, negSucc_mul_ofNat, ofNat_mul_ofNat,
+             negSucc_mul_negSucc] <;>
+  simp only [negSucc_coe, bodd_neg, bodd_coe, ←Nat.bodd_mul]
+-- Porting note: Hevily refactored proof, used to be:
+-- `by cases m with m m; cases n with n n;`
+-- `simp [← int.mul_def, int.mul, -of_nat_eq_coe, bool.bxor_comm]`
 #align int.bodd_mul Int.bodd_mul
 
 theorem bodd_add_div2 : ∀ n, cond (bodd n) 1 0 + 2 * div2 n = n
   | (n : ℕ) => by
-    rw [show (cond (bodd n) 1 0 : ℤ) = (cond (bodd n) 1 0 : ℕ) by cases bodd n <;> rfl] <;>
-      exact congr_arg of_nat n.bodd_add_div2
+    rw [show (cond (bodd n) 1 0 : ℤ) = (cond (bodd n) 1 0 : ℕ) by cases bodd n <;> rfl]
+    exact congr_arg ofNat n.bodd_add_div2
   | -[n+1] => by
-    refine' Eq.trans _ (congr_arg neg_succ_of_nat n.bodd_add_div2)
+    refine' Eq.trans _ (congr_arg negSucc n.bodd_add_div2)
     dsimp [bodd]; cases Nat.bodd n <;> dsimp [cond, not, div2, Int.mul]
     · change -[2 * Nat.div2 n+1] = _
       rw [zero_add]
@@ -129,10 +150,14 @@ theorem div2_val : ∀ n, div2 n = n / 2
   | -[n+1] => congr_arg negSucc n.div2_val
 #align int.div2_val Int.div2_val
 
+section deprecated
+
+@[deprecated]
 theorem bit0_val (n : ℤ) : bit0 n = 2 * n :=
   (two_mul _).symm
 #align int.bit0_val Int.bit0_val
 
+@[deprecated]
 theorem bit1_val (n : ℤ) : bit1 n = 2 * n + 1 :=
   congr_arg (· + (1 : ℤ)) (bit0_val _)
 #align int.bit1_val Int.bit1_val
