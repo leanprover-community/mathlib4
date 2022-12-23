4876f6e654529dd59b8ff0c14e4a9456dac03792
Frédéric Dupuis <dupuisf@iro.umontreal.ca>
2022-12-22 21:08:34 -0500

    two sorries left

--- Mathlib/Data/Set/Functor.lean
+++ Mathlib/Data/Set/Functor.lean
@@ -9,13 +9,14 @@ Authors: Leonardo de Moura
 ! if you have ported upstream changes.
 -/
 import Mathlib.Data.Set.Lattice
+import Mathlib.Init.Set
+import Mathlib.Control.Basic
 
 /-!
-# Functoriality of `set`
+# Functoriality of `Set`
 
-This file defines the functor structure of `set`.
+This file defines the functor structure of `Set`.
 -/
-
 
 universe u
 
@@ -25,11 +26,11 @@ namespace Set
 
 variable {α β : Type u} {s : Set α} {f : α → Set β} {g : Set (α → β)}
 
-instance : Monad.{u} Set where 
-  pure α a := {a}
-  bind α β s f := ⋃ i ∈ s, f i
-  seq α β := Set.seq
-  map α β := Set.image
+instance monad : Monad.{u} Set where
+  pure a := {a}
+  bind s f := ⋃ i ∈ s, f i
+  seq s t := Set.seq s (t ())
+  map := Set.image
 
 @[simp]
 theorem bind_def : s >>= f = ⋃ i ∈ s, f i :=
@@ -54,26 +55,26 @@ theorem pure_def (a : α) : (pure a : Set α) = {a} :=
 /-- `set.image2` in terms of monadic operations. Note that this can't be taken as the definition
 because of the lack of universe polymorphism. -/
 theorem image2_def {α β γ : Type _} (f : α → β → γ) (s : Set α) (t : Set β) :
-    image2 f s t = f <$> s <*> t := by 
+    image2 f s t = f <$> s <*> t := by
   ext
   simp
 #align set.image2_def Set.image2_def
 
-instance : LawfulMonad Set where 
-  id_map α := image_id
-  comp_map α β γ f g s := image_comp _ _ _
-  pure_bind α β := bUnion_singleton
-  bind_assoc α β γ s f g := by simp only [bind_def, bUnion_Union]
-  bind_pure_comp_eq_map α β f s := (image_eq_Union _ _).symm
-  bind_map_eq_seq α β s t := seq_def.symm
+instance : LawfulMonad Set where
+  pure_bind := bunionᵢ_singleton
+  bind_assoc s f g := by simp only [bind_def, bunionᵢ_unionᵢ]
+  bind_pure_comp f s := (image_eq_unionᵢ _ _).symm
+  bind_map s t := seq_def.symm
+  pure_seq {α} {β} := by intro g s; ext s; simp
+  seqLeft_eq {α} {β} := sorry
+  seqRight_eq {α} {β} := sorry
 
 instance : CommApplicative (Set : Type u → Type u) :=
-  ⟨fun α β s t => prod_image_seq_comm s t⟩
+  ⟨fun s t => prod_image_seq_comm s t⟩
 
 instance : Alternative Set :=
-  { Set.monad with 
-    orelse := fun α => (· ∪ ·)
-    failure := fun α => ∅ }
+  { Set.monad with
+    orElse := fun s t => s ∪ (t ())
+    failure := ∅ }
 
 end Set
-
