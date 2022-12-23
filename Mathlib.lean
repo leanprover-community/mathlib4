a53dfd73f6f4cd4a262a5b60db6828d6367536fb
Frédéric Dupuis <dupuisf@iro.umontreal.ca>
2022-12-23 15:32:34 -0500

    Mathbin -> Mathlib; add import to Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -278,6 +278,7 @@ import Mathlib.Data.Set.NAry
 import Mathlib.Data.Set.Opposite
 import Mathlib.Data.Set.Prod
 import Mathlib.Data.Set.Sigma
+import Mathlib.Data.Set.UnionLift
 import Mathlib.Data.SetLike.Basic
 import Mathlib.Data.Setoid.Basic
 import Mathlib.Data.Sigma.Basic
--- Mathlib/Data/Set/UnionLift.lean
+++ Mathlib/Data/Set/UnionLift.lean
@@ -8,8 +8,8 @@ Authors: Chris Hughes
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
-import Mathbin.Data.Set.Lattice
-import Mathbin.Order.Directed
+import Mathlib.Data.Set.Lattice
+import Mathlib.Order.Directed
 
 /-!
 # Union lift
