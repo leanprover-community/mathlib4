ec12a9f720c60a90d2ecd7faa574977d102295a1
David Renshaw <dwrenshaw@gmail.com>
2022-12-23 20:27:08 -0500

    uncomment two more tests

--- test/positivity.lean
+++ test/positivity.lean
@@ -169,12 +169,12 @@ example {a b : ℤ} (ha : 3 < a) (hb : b ≥ 4) : 0 < 3 * a ^ 2 * b + b * 7 + 14
 
 example {a : ℤ} : 0 ≤ |a| := by positivity
 
--- example {a : ℤ} : 0 < |a| + 3 := by positivity
+example {a : ℤ} : 0 < |a| + 3 := by positivity
 
 -- example {n : ℤ} (hn : 0 < n) : 0 < n.nat_abs := by positivity
 -- example {n : ℤ} (hn : n ≠ 0) : 0 < n.nat_abs := by positivity
 
--- example {a : ℤ} (ha : 1 < a) : 0 < |(3:ℤ) + a| := by positivity
+example {a : ℤ} (ha : 1 < a) : 0 < |(3:ℤ) + a| := by positivity
 
 -- example {a : ℝ} (ha : 0 ≤ a) : 0 ≤ real.sqrt a := by positivity
 
