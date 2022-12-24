cd2e53a052b8e8a69f680aed0aed7b73adb6cde5
antoinelab01 <antoinelab01@gmail.com>
2022-12-24 07:34:36 +0000

    feat: port Data.Rat.Lemmas (#1138)
    Co-authored-by: Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -258,6 +258,7 @@ import Mathlib.Data.Quot
 import Mathlib.Data.Rat.Basic
 import Mathlib.Data.Rat.Defs
 import Mathlib.Data.Rat.Init
+import Mathlib.Data.Rat.Lemmas
 import Mathlib.Data.Rat.Order
 import Mathlib.Data.Rel
 import Mathlib.Data.Set.Basic
