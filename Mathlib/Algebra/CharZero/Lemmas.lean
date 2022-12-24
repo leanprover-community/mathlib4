9a50b6849b2639282c71ddb72b87d853f7094b5c
Reid Barton <rwbarton@gmail.com>
2022-12-24 10:40:20 +0100

    reflow

--- Mathlib/Algebra/CharZero/Lemmas.lean
+++ Mathlib/Algebra/CharZero/Lemmas.lean
@@ -177,10 +177,9 @@ end
 namespace WithTop
 
 instance {R : Type _} [AddMonoidWithOne R] [CharZero R] :
-    CharZero
-      (WithTop
-        R) where cast_injective m n h := by
-                  rwa [← coe_nat, ← coe_nat n, coe_eq_coe, Nat.cast_inj] at h
+    CharZero (WithTop R) where
+  cast_injective m n h := by
+    rwa [← coe_nat, ← coe_nat n, coe_eq_coe, Nat.cast_inj] at h
 
 end WithTop
 
