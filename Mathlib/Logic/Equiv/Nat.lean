fa20385ec33e390f3ff3eb52be47ec5abfd03fbb
ADedecker <anatolededecker@gmail.com>
2022-12-26 17:40:39 +0100

    Done except deprecated

--- Mathlib/Logic/Equiv/Nat.lean
+++ Mathlib/Logic/Equiv/Nat.lean
@@ -26,29 +26,28 @@ namespace Equiv
 variable {α : Type _}
 
 /-- An equivalence between `bool × ℕ` and `ℕ`, by mapping `(tt, x)` to `2 * x + 1` and `(ff, x)` to
-`2 * x`.
--/
+`2 * x`. -/
 @[simps]
 def boolProdNatEquivNat : Bool × ℕ ≃
       ℕ where
   toFun := uncurry bit
   invFun := boddDiv2
-  left_inv := fun ⟨b, n⟩ => by simp only [bodd_bit, div2_bit, uncurry_apply_pair, bodd_div2_eq]
-  right_inv n := by simp only [bit_decomp, bodd_div2_eq, uncurry_apply_pair]
+  left_inv := fun ⟨b, n⟩ => by simp only [bodd_bit, div2_bit, uncurry_apply_pair, boddDiv2_eq]
+  right_inv n := by simp only [bit_decomp, boddDiv2_eq, uncurry_apply_pair]
 #align equiv.bool_prod_nat_equiv_nat Equiv.boolProdNatEquivNat
 
 /-- An equivalence between `ℕ ⊕ ℕ` and `ℕ`, by mapping `(sum.inl x)` to `2 * x` and `(sum.inr x)` to
 `2 * x + 1`.
 -/
-@[simps symmApply]
+@[simps symm_apply]
 def natSumNatEquivNat : Sum ℕ ℕ ≃ ℕ :=
   (boolProdEquivSum ℕ).symm.trans boolProdNatEquivNat
 #align equiv.nat_sum_nat_equiv_nat Equiv.natSumNatEquivNat
 
 @[simp]
-theorem nat_sum_nat_equiv_nat_apply : ⇑nat_sum_nat_equiv_nat = Sum.elim bit0 bit1 := by
+theorem natSumNatEquivNat_apply : ⇑natSumNatEquivNat = Sum.elim bit0 bit1 := by
   ext (x | x) <;> rfl
-#align equiv.nat_sum_nat_equiv_nat_apply Equiv.nat_sum_nat_equiv_nat_apply
+#align equiv.nat_sum_nat_equiv_nat_apply Equiv.natSumNatEquivNat_apply
 
 /-- An equivalence between `ℤ` and `ℕ`, through `ℤ ≃ ℕ ⊕ ℕ` and `ℕ ⊕ ℕ ≃ ℕ`.
 -/
@@ -63,7 +62,6 @@ def prodEquivOfEquivNat (e : α ≃ ℕ) : α × α ≃ α :=
     α × α ≃ ℕ × ℕ := prodCongr e e
     _ ≃ ℕ := mkpairEquiv
     _ ≃ α := e.symm
-
 #align equiv.prod_equiv_of_equiv_nat Equiv.prodEquivOfEquivNat
 
 end Equiv
