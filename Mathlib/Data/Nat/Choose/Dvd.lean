8947622e0d6d76880123a42b5fa2d09136262f0e
Eric Rodriguez <ericrboidi@gmail.com>
2022-12-26 02:19:43 +0000

    Mathbin -> Mathlib; add import to Mathlib.lean

--- Mathlib/Data/Nat/Choose/Dvd.lean
+++ Mathlib/Data/Nat/Choose/Dvd.lean
@@ -8,8 +8,8 @@ Authors: Chris Hughes, Patrick Stevens
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
-import Mathbin.Data.Nat.Choose.Basic
-import Mathbin.Data.Nat.Prime
+import Mathlib.Data.Nat.Choose.Basic
+import Mathlib.Data.Nat.Prime
 
 /-!
 # Divisibility properties of binomial coefficients
