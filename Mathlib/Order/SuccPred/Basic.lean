5d28ad9b0d700c92b887e1323b350599407c99d7
Arien Malec <arien.malec@gmail.com>
2022-12-27 15:16:04 -0800

    fixes

--- Mathlib/Order/SuccPred/Basic.lean
+++ Mathlib/Order/SuccPred/Basic.lean
@@ -112,8 +112,8 @@ def SuccOrder.ofSuccLeIffOfLeLtSucc (succ : α → α) (hsucc_le_iff : ∀ {a b}
   { succ
     le_succ := fun a => (hsucc_le_iff.1 le_rfl).le
     max_of_succ_le := fun ha => (lt_irrefl _ <| hsucc_le_iff.1 ha).elim
-    succ_le_of_lt := fun a b => hsucc_le_iff.2
-    le_of_lt_succ := fun a b => hle_of_lt_succ }
+    succ_le_of_lt := fun h => hsucc_le_iff.2 h
+    le_of_lt_succ := fun h => hle_of_lt_succ h}
 #align succ_order.of_succ_le_iff_of_le_lt_succ SuccOrder.ofSuccLeIffOfLeLtSucc
 
 /-- A constructor for `PredOrder α` usable when `α` has no minimal element. -/
@@ -122,8 +122,8 @@ def PredOrder.ofLePredIffOfPredLePred (pred : α → α) (hle_pred_iff : ∀ {a 
   { pred
     pred_le := fun a => (hle_pred_iff.1 le_rfl).le
     min_of_le_pred := fun ha => (lt_irrefl _ <| hle_pred_iff.1 ha).elim
-    le_pred_of_lt := fun a b => hle_pred_iff.2
-    le_of_pred_lt := fun a b => hle_of_pred_lt }
+    le_pred_of_lt := fun h => hle_pred_iff.2 h
+    le_of_pred_lt := fun h => hle_of_pred_lt h }
 #align pred_order.of_le_pred_iff_of_pred_le_pred PredOrder.ofLePredIffOfPredLePred
 
 end Preorder
