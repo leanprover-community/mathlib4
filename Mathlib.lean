bc2fc2dc348542e60445a49227130349cac38c4f
Riccardo Brasca <riccardo.brasca@gmail.com>
2022-12-28 14:53:47 +0000

    feat: port Algebra.Ring.Ulift (#1240)
    Most of the porting consists of doing by hand what `pi_instance_derive_field` did.
    
    Co-authored-by: ChrisHughes24 <chrishughes24@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -131,6 +131,7 @@ import Mathlib.Algebra.Ring.Pi
 import Mathlib.Algebra.Ring.Prod
 import Mathlib.Algebra.Ring.Regular
 import Mathlib.Algebra.Ring.Semiconj
+import Mathlib.Algebra.Ring.ULift
 import Mathlib.Algebra.Ring.Units
 import Mathlib.Algebra.SMulWithZero
 import Mathlib.CategoryTheory.Category.Basic
