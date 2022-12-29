8eeb6edf95230aff7425ee141567d33deacc8b52
Chris Hughes <33847686+ChrisHughes24@users.noreply.github.com>
2022-12-29 16:12:31 +0000

    Update Mathlib/Data/Rat/Sqrt.lean

--- Mathlib/Data/Rat/Sqrt.lean
+++ Mathlib/Data/Rat/Sqrt.lean
@@ -31,7 +31,6 @@ def sqrt (q : ℚ) : ℚ := mkRat (Int.sqrt q.num) (Nat.sqrt q.den)
 
 theorem sqrt_eq (q : ℚ) : Rat.sqrt (q * q) = |q| := by
   rw [sqrt, mul_self_num, mul_self_den, Int.sqrt_eq, Nat.sqrt_eq, abs_def, divInt_ofNat]
-
 #align rat.sqrt_eq Rat.sqrt_eq
 
 theorem exists_mul_self (x : ℚ) : (∃ q, q * q = x) ↔ Rat.sqrt x * Rat.sqrt x = x :=
