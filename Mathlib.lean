43520f093b6fe9474b42592ecde3cfd5b63727c0
Jad Ghalayini <jad.ghalayini@hotmail.com>
2022-12-29 11:25:39 +0000

    feat: port CategoryTheory.FullSubcategory (#1126)
    Co-authored-by: Jad Ghalayini <jad.ghalayini@protonmail.com>
    Co-authored-by: Jad Ghalayini <jad.ghalayini@mail.utoronto.ca>
    Co-authored-by: Reid Barton <rwbarton@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -138,6 +138,7 @@ import Mathlib.CategoryTheory.Category.Basic
 import Mathlib.CategoryTheory.Category.KleisliCat
 import Mathlib.CategoryTheory.Category.RelCat
 import Mathlib.CategoryTheory.ConcreteCategory.Bundled
+import Mathlib.CategoryTheory.FullSubcategory
 import Mathlib.CategoryTheory.Functor.Basic
 import Mathlib.CategoryTheory.Functor.Category
 import Mathlib.CategoryTheory.Functor.Default
