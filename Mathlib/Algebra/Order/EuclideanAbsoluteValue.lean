cd12a6ed8b95becf21b13f4a8f85e1c12418b899
z <zbattleman@gmail.com>
2022-12-29 18:43:35 -0500

    initial

--- Mathlib/Algebra/Order/EuclideanAbsoluteValue.lean
+++ Mathlib/Algebra/Order/EuclideanAbsoluteValue.lean
@@ -71,11 +71,10 @@ open Int
 /-- `abs : ℤ → ℤ` is a Euclidean absolute value -/
 protected theorem abs_is_euclidean : IsEuclidean (AbsoluteValue.abs : AbsoluteValue ℤ ℤ) :=
   {
-    map_lt_map_iff' := fun x y =>
-      show abs x < abs y ↔ natAbs x < natAbs y by rw [abs_eq_nat_abs, abs_eq_nat_abs, coe_nat_lt] }
+    map_lt_map_iff' := @fun x y =>
+      show abs x < abs y ↔ natAbs x < natAbs y by rw [abs_eq_natAbs, abs_eq_natAbs, Int.ofNat_lt] }
 #align absolute_value.abs_is_euclidean AbsoluteValue.abs_is_euclidean
 
 end Int
 
 end AbsoluteValue
-
