fed36e42753b328817b9b1d7abf2b85fde911d49
Lukas Miaskiwskyi <lukas.mias@gmail.com>
2022-12-29 18:27:49 +0100

    feat: port Order.Monotone.Extension

--- Mathlib/Order/Monotone/Extension.lean
+++ Mathlib/Order/Monotone/Extension.lean
@@ -33,21 +33,21 @@ theorem MonotoneOn.exists_monotone_extension (h : MonotoneOn f s) (hl : BddBelow
     rcases hl with ⟨a, ha⟩
     have hu' : ∀ x, BddAbove (f '' (Iic x ∩ s)) := fun x =>
       hu.mono (image_subset _ (inter_subset_right _ _))
-    set g : α → β := fun x => if Disjoint (Iic x) s then a else Sup (f '' (Iic x ∩ s))
-    have hgs : eq_on f g s := by
+    let g : α → β := fun x => if Disjoint (Iic x) s then a else supₛ (f '' (Iic x ∩ s))
+    have hgs : EqOn f g s := by
       intro x hx
-      simp only [g]
+      simp only []
       have : IsGreatest (Iic x ∩ s) x := ⟨⟨right_mem_Iic, hx⟩, fun y hy => hy.1⟩
       rw [if_neg this.nonempty.not_disjoint,
-        ((h.mono <| inter_subset_right _ _).map_is_greatest this).cSup_eq]
+        ((h.mono <| inter_subset_right _ _).map_isGreatest this).csupₛ_eq]
     refine' ⟨g, fun x y hxy => _, hgs⟩
     by_cases hx : Disjoint (Iic x) s <;> by_cases hy : Disjoint (Iic y) s <;>
-      simp only [g, if_pos, if_neg, not_false_iff, *]
+      simp only [if_pos, if_neg, not_false_iff, *, refl]
     · rcases not_disjoint_iff_nonempty_inter.1 hy with ⟨z, hz⟩
-      exact le_cSup_of_le (hu' _) (mem_image_of_mem _ hz) (ha <| mem_image_of_mem _ hz.2)
+      exact le_csupₛ_of_le (hu' _) (mem_image_of_mem _ hz) (ha <| mem_image_of_mem _ hz.2)
     · exact (hx <| hy.mono_left <| Iic_subset_Iic.2 hxy).elim
     · rw [not_disjoint_iff_nonempty_inter] at hx hy
-      refine' cSup_le_cSup (hu' _) (hx.image _) (image_subset _ _)
+      refine' csupₛ_le_csupₛ (hu' _) (hx.image _) (image_subset _ _)
       exact inter_subset_inter_left _ (Iic_subset_Iic.2 hxy)
 #align monotone_on.exists_monotone_extension MonotoneOn.exists_monotone_extension
 
@@ -57,4 +57,3 @@ theorem AntitoneOn.exists_antitone_extension (h : AntitoneOn f s) (hl : BddBelow
     (hu : BddAbove (f '' s)) : ∃ g : α → β, Antitone g ∧ EqOn f g s :=
   h.dual_right.exists_monotone_extension hu hl
 #align antitone_on.exists_antitone_extension AntitoneOn.exists_antitone_extension
-
