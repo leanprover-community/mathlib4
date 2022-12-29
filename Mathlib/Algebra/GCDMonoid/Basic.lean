4cb6362b049e2bb1ec7e50b8938cbac0f498be18
Chris Hughes <33847686+ChrisHughes24@users.noreply.github.com>
2022-12-29 13:08:41 +0000

    Update Mathlib/Algebra/GCDMonoid/Basic.lean
    Co-authored-by: Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>

--- Mathlib/Algebra/GCDMonoid/Basic.lean
+++ Mathlib/Algebra/GCDMonoid/Basic.lean
@@ -702,8 +702,7 @@ section LCM
 
 theorem lcm_dvd_iff [GCDMonoid α] {a b c : α} : lcm a b ∣ c ↔ a ∣ c ∧ b ∣ c := by
   by_cases a = 0 ∨ b = 0
-  ·
-    rcases h with (rfl | rfl) <;>
+  · rcases h with (rfl | rfl) <;>
       simp (config := { contextual := true }) only [iff_def, lcm_zero_left, lcm_zero_right,
         zero_dvd_iff, dvd_zero, eq_self_iff_true, and_true_iff, imp_true_iff]
   · obtain ⟨h1, h2⟩ := not_or.1 h
