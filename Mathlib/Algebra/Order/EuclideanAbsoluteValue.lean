a2857fb3119575eb65659d45b416b7ea54ffc2a8
z <32079362+xoers@users.noreply.github.com>
2022-12-29 21:38:29 -0500

    Update Mathlib/Algebra/Order/EuclideanAbsoluteValue.lean
    Co-authored-by: Frédéric Dupuis <31101893+dupuisf@users.noreply.github.com>

--- Mathlib/Algebra/Order/EuclideanAbsoluteValue.lean
+++ Mathlib/Algebra/Order/EuclideanAbsoluteValue.lean
@@ -71,10 +71,9 @@ open Int
 
 -- TODO: generalize to `linear_ordered_euclidean_domain`s if we ever get a definition of those
 /-- `abs : ℤ → ℤ` is a Euclidean absolute value -/
-protected theorem abs_is_euclidean : IsEuclidean (AbsoluteValue.abs : AbsoluteValue ℤ ℤ) :=
-  {
-    map_lt_map_iff' := fun {x y} =>
-      show abs x < abs y ↔ natAbs x < natAbs y by rw [abs_eq_natAbs, abs_eq_natAbs, ofNat_lt] }
+protected theorem abs_isEuclidean : IsEuclidean (AbsoluteValue.abs : AbsoluteValue ℤ ℤ) :=
+  {  map_lt_map_iff' := fun {x y} =>
+       show abs x < abs y ↔ natAbs x < natAbs y by rw [abs_eq_natAbs, abs_eq_natAbs, ofNat_lt] }
 #align absolute_value.abs_is_euclidean AbsoluteValue.abs_is_euclidean
 
 end Int
