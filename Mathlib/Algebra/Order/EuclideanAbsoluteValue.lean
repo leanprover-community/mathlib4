c4c3fe677098d5a700537ea36a729babc8fd97da
z <zbattleman@gmail.com>
2022-12-29 21:44:23 -0500

    small change to name align

--- Mathlib/Algebra/Order/EuclideanAbsoluteValue.lean
+++ Mathlib/Algebra/Order/EuclideanAbsoluteValue.lean
@@ -74,7 +74,7 @@ open Int
 protected theorem abs_isEuclidean : IsEuclidean (AbsoluteValue.abs : AbsoluteValue ℤ ℤ) :=
   {  map_lt_map_iff' := fun {x y} =>
        show abs x < abs y ↔ natAbs x < natAbs y by rw [abs_eq_natAbs, abs_eq_natAbs, ofNat_lt] }
-#align absolute_value.abs_is_euclidean AbsoluteValue.abs_is_euclidean
+#align absolute_value.abs_is_euclidean AbsoluteValue.abs_isEuclidean
 
 end Int
 
