a0f424b70cc66a10b1d4fb929cf14f8f09c965a2
ChrisHughes24 <chrishughes24@gmail.com>
2022-12-30 20:14:08 +0000

    feat: port Order.Interval

--- Mathlib.lean
+++ Mathlib.lean
@@ -430,6 +430,7 @@ import Mathlib.Order.Hom.Basic
 import Mathlib.Order.Hom.Order
 import Mathlib.Order.Hom.Set
 import Mathlib.Order.InitialSeg
+import Mathlib.Order.Interval
 import Mathlib.Order.Iterate
 import Mathlib.Order.Lattice
 import Mathlib.Order.LatticeIntervals
