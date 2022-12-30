9e650d8dbaee460027186ce8791d643aed45c0ad
ChrisHughes24 <chrishughes24@gmail.com>
2022-12-30 17:50:59 +0000

    initial commit

--- Mathlib.lean
+++ Mathlib.lean
@@ -265,6 +265,7 @@ import Mathlib.Data.PEquiv
 import Mathlib.Data.PNat.Basic
 import Mathlib.Data.PNat.Defs
 import Mathlib.Data.PNat.Find
+import Mathlib.Data.PNat.Prime
 import Mathlib.Data.PSigma.Order
 import Mathlib.Data.Part
 import Mathlib.Data.Pi.Algebra
