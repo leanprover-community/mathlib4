5554696789c380d9eac9221e691233565790cdf7
Henrik Böving <hargonix@gmail.com>
2023-01-01 16:14:02 +0100

    Mathbin -> Mathlib; add import to Mathlib.lean

--- Mathlib/GroupTheory/Subsemigroup/Centralizer.lean
+++ Mathlib/GroupTheory/Subsemigroup/Centralizer.lean
@@ -8,8 +8,8 @@ Authors: Thomas Browning, Jireh Loreaux
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
-import Mathbin.GroupTheory.Subsemigroup.Center
-import Mathbin.Algebra.GroupWithZero.Units.Lemmas
+import Mathlib.GroupTheory.Subsemigroup.Center
+import Mathlib.Algebra.GroupWithZero.Units.Lemmas
 
 /-!
 # Centralizers of magmas and semigroups
