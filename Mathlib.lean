df4a954cc520b8d9b46146abcfc2cfc8b2cc08f5
casavaca <96765450+casavaca@users.noreply.github.com>
2023-01-02 12:57:43 -0800

    Mathbin -> Mathlib; add import to Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -582,3 +582,4 @@ import Mathlib.Util.Tactic
 import Mathlib.Util.Time
 import Mathlib.Util.WhatsNew
 import Mathlib.Util.WithWeakNamespace
+import Order.Concept
--- Order/Concept.lean
+++ Order/Concept.lean
@@ -8,7 +8,7 @@ Authors: Yaël Dillies
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
-import Mathbin.Data.Set.Lattice
+import Mathlib.Data.Set.Lattice
 
 /-!
 # Formal concept analysis
