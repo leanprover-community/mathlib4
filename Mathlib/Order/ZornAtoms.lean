911f469271717515f60419aa4689f0b287d5c43e
Frédéric Dupuis <dupuisf@iro.umontreal.ca>
2023-01-01 17:52:06 -0700

    fixed

--- Mathlib/Order/ZornAtoms.lean
+++ Mathlib/Order/ZornAtoms.lean
@@ -25,28 +25,25 @@ open Set
 /- ./././Mathport/Syntax/Translate/Basic.lean:632:2: warning: expanding binder collection (x «expr ≠ » «expr⊤»()) -/
 /-- **Zorn's lemma**: A partial order is coatomic if every nonempty chain `c`, `⊤ ∉ c`, has an upper
 bound not equal to `⊤`. -/
-theorem IsCoatomic.of_is_chain_bounded {α : Type _} [PartialOrder α] [OrderTop α]
+theorem IsCoatomic.of_isChain_bounded {α : Type _} [PartialOrder α] [OrderTop α]
     (h :
       ∀ c : Set α,
         IsChain (· ≤ ·) c → c.Nonempty → ⊤ ∉ c → ∃ (x : _)(_ : x ≠ ⊤), x ∈ upperBounds c) :
     IsCoatomic α := by
   refine' ⟨fun x => le_top.eq_or_lt.imp_right fun hx => _⟩
-  rcases zorn_nonempty_partial_order₀ (Ico x ⊤) (fun c hxc hc y hy => _) x (left_mem_Ico.2 hx) with
+  rcases zorn_nonempty_partialOrder₀ (Ico x ⊤) (fun c hxc hc y hy => _) x (left_mem_Ico.2 hx) with
     ⟨y, ⟨hxy, hy⟩, -, hy'⟩
   · refine' ⟨y, ⟨hy.ne, fun z hyz => le_top.eq_or_lt.resolve_right fun hz => _⟩, hxy⟩
     exact hyz.ne' (hy' z ⟨hxy.trans hyz.le, hz⟩ hyz.le)
-  · rcases h c hc ⟨y, hy⟩ fun h => (hxc h).2.Ne rfl with ⟨z, hz, hcz⟩
-    exact ⟨z, ⟨le_trans (hxc hy).1 (hcz hy), hz.lt_top⟩, hcz⟩
-#align is_coatomic.of_is_chain_bounded IsCoatomic.of_is_chain_bounded
+#align is_coatomic.of_is_chain_bounded IsCoatomic.of_isChain_bounded
 
 /- ./././Mathport/Syntax/Translate/Basic.lean:632:2: warning: expanding binder collection (x «expr ≠ » «expr⊥»()) -/
 /-- **Zorn's lemma**: A partial order is atomic if every nonempty chain `c`, `⊥ ∉ c`, has an lower
 bound not equal to `⊥`. -/
-theorem IsAtomic.of_is_chain_bounded {α : Type _} [PartialOrder α] [OrderBot α]
+theorem IsAtomic.of_isChain_bounded {α : Type _} [PartialOrder α] [OrderBot α]
     (h :
       ∀ c : Set α,
         IsChain (· ≤ ·) c → c.Nonempty → ⊥ ∉ c → ∃ (x : _)(_ : x ≠ ⊥), x ∈ lowerBounds c) :
     IsAtomic α :=
-  is_coatomic_dual_iff_is_atomic.mp <| IsCoatomic.of_is_chain_bounded fun c hc => h c hc.symm
-#align is_atomic.of_is_chain_bounded IsAtomic.of_is_chain_bounded
-
+  isCoatomic_dual_iff_isAtomic.mp <| IsCoatomic.of_isChain_bounded fun c hc => h c hc.symm
+#align is_atomic.of_is_chain_bounded IsAtomic.of_isChain_bounded
