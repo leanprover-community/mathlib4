41abdb584b7c0988ffce5461040f77a2d42bd693
riccardobrasca <riccardo.brasca@gmail.com>
2023-01-03 04:49:32 +0100

    chore: update SHA  in porting header

--- Mathlib/Combinatorics/Quiver/Push.lean
+++ Mathlib/Combinatorics/Quiver/Push.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Rémi Bottinelli
 
 ! This file was ported from Lean 3 source module combinatorics.quiver.push
-! leanprover-community/mathlib commit 9b2660e1b25419042c8da10bf411aa3c67f14383
+! leanprover-community/mathlib commit 2258b40dacd2942571c8ce136215350c702dc78f
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
