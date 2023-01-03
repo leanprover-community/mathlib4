dbd0006fb927f01a86c693911d73a59d2abf125a
riccardobrasca <riccardo.brasca@gmail.com>
2023-01-03 05:06:25 +0100

    these are also easy

--- Mathlib/Data/Rat/Defs.lean
+++ Mathlib/Data/Rat/Defs.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl, Mario Carneiro
 
 ! This file was ported from Lean 3 source module data.rat.defs
-! leanprover-community/mathlib commit fc2ed6f838ce7c9b7c7171e58d78eaf7b438fb0e
+! leanprover-community/mathlib commit 2258b40dacd2942571c8ce136215350c702dc78f
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
