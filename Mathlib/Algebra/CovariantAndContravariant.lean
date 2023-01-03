cac405f4bb86f998a4d71cb6836b2a05afb4bcea
Riccardo Brasca <riccardo.brasca@gmail.com>
2023-01-03 16:22:27 +0000

    chore: update SHA  in porting header (#1306)
    These files correspond to files flagged "The following files have been modified since the commit at which they were verified." by `port_status.py` but are in sync with mathlib3, so we can update the hash.
    
    I only updated the easy ones, the others need a closer inspection.
    
    Co-authored-by: Reid Barton <rwbarton@gmail.com>

--- Mathlib/Algebra/CovariantAndContravariant.lean
+++ Mathlib/Algebra/CovariantAndContravariant.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Damiano Testa
 
 ! This file was ported from Lean 3 source module algebra.covariant_and_contravariant
-! leanprover-community/mathlib commit c4658a649d216f57e99621708b09dcb3dcccbd23
+! leanprover-community/mathlib commit 2258b40dacd2942571c8ce136215350c702dc78f
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
