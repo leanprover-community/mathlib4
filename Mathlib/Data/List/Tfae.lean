db432cf6e36d07b807032528a1cba5d9abd5ba93
zeramorphic <zeramorphic@proton.me>
2023-01-05 18:32:10 +0000

    Tfae -> TFAE
    Signed-off-by: zeramorphic <zeramorphic@proton.me>

--- Mathlib/Data/List/Tfae.lean
+++ Mathlib/Data/List/Tfae.lean
@@ -15,29 +15,29 @@ import Mathlib.Init.Data.List.Basic
 # The Following Are Equivalent
 
 This file allows to state that all propositions in a list are equivalent. It is used by
-`tactic.tfae`.
-`tfae l` means `∀ x ∈ l, ∀ y ∈ l, x ↔ y`. This is equivalent to `pairwise (↔) l`.
+`Mathlib.Tactic.Tfae`.
+`TFAE l` means `∀ x ∈ l, ∀ y ∈ l, x ↔ y`. This is equivalent to `pairwise (↔) l`.
 -/
 
 
 namespace List
 
-/-- tfae: The Following (propositions) Are Equivalent.
+/-- TFAE: The Following (propositions) Are Equivalent.
 
-The `tfae_have` and `tfae_finish` tactics can be useful in proofs with `tfae` goals.
+The `tfae_have` and `tfae_finish` tactics can be useful in proofs with `TFAE` goals.
 -/
-def Tfae (l : List Prop) : Prop :=
+def TFAE (l : List Prop) : Prop :=
   ∀ x ∈ l, ∀ y ∈ l, x ↔ y
-#align list.tfae List.Tfae
+#align list.tfae List.TFAE
 
-theorem tfae_nil : Tfae [] :=
+theorem tfae_nil : TFAE [] :=
   forall_mem_nil _
 #align list.tfae_nil List.tfae_nil
 
-theorem tfae_singleton (p) : Tfae [p] := by simp [Tfae, -eq_iff_iff]
+theorem tfae_singleton (p) : TFAE [p] := by simp [TFAE, -eq_iff_iff]
 #align list.tfae_singleton List.tfae_singleton
 
-theorem tfae_cons_of_mem {a b} {l : List Prop} (h : b ∈ l) : Tfae (a :: l) ↔ (a ↔ b) ∧ Tfae l :=
+theorem tfae_cons_of_mem {a b} {l : List Prop} (h : b ∈ l) : TFAE (a :: l) ↔ (a ↔ b) ∧ TFAE l :=
   ⟨fun H => ⟨H a (by simp) b (Mem.tail a h),
     fun p hp q hq => H _ (Mem.tail a hp) _ (Mem.tail a hq)⟩,
     by
@@ -48,16 +48,16 @@ theorem tfae_cons_of_mem {a b} {l : List Prop} (h : b ∈ l) : Tfae (a :: l) ↔
     · exact H _ hp _ hq⟩
 #align list.tfae_cons_of_mem List.tfae_cons_of_mem
 
-theorem tfae_cons_cons {a b} {l : List Prop} : Tfae (a :: b :: l) ↔ (a ↔ b) ∧ Tfae (b :: l) :=
+theorem tfae_cons_cons {a b} {l : List Prop} : TFAE (a :: b :: l) ↔ (a ↔ b) ∧ TFAE (b :: l) :=
   tfae_cons_of_mem (Mem.head _)
 #align list.tfae_cons_cons List.tfae_cons_cons
 
-theorem tfae_of_forall (b : Prop) (l : List Prop) (h : ∀ a ∈ l, a ↔ b) : Tfae l :=
+theorem tfae_of_forall (b : Prop) (l : List Prop) (h : ∀ a ∈ l, a ↔ b) : TFAE l :=
   fun _a₁ h₁ _a₂ h₂ => (h _ h₁).trans (h _ h₂).symm
 #align list.tfae_of_forall List.tfae_of_forall
 
 theorem tfae_of_cycle {a b} {l : List Prop} :
-    List.Chain (· → ·) a (b :: l) → (ilast' b l → a) → Tfae (a :: b :: l) :=
+    List.Chain (· → ·) a (b :: l) → (ilast' b l → a) → TFAE (a :: b :: l) :=
   by
   induction' l with c l IH generalizing a b <;>
     simp only [tfae_cons_cons, tfae_singleton, and_true_iff, chain_cons, Chain.nil] at *
@@ -68,9 +68,9 @@ theorem tfae_of_cycle {a b} {l : List Prop} :
   exact ⟨⟨ab, la ∘ (this.2 c (Mem.head _) _ (ilast'_mem _ _)).1 ∘ bc⟩, this⟩
 #align list.tfae_of_cycle List.tfae_of_cycle
 
-theorem Tfae.out {l} (h : Tfae l) (n₁ n₂) {a b} (h₁ : List.get? l n₁ = some a := by rfl)
+theorem TFAE.out {l} (h : TFAE l) (n₁ n₂) {a b} (h₁ : List.get? l n₁ = some a := by rfl)
     (h₂ : List.get? l n₂ = some b := by rfl) : a ↔ b :=
   h _ (List.get?_mem h₁) _ (List.get?_mem h₂)
-#align list.tfae.out List.Tfae.out
+#align list.tfae.out List.TFAE.out
 
 end List
