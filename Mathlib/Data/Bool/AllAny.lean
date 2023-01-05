f441b4c67bd657ca006c9e7bdf3498476ddf65a4
Moritz Firsching <firsching@google.com>
2023-01-05 21:11:13 +0100

    first fixes

--- Mathlib/Data/Bool/AllAny.lean
+++ Mathlib/Data/Bool/AllAny.lean
@@ -22,7 +22,8 @@ variable {α : Type _} {p : α → Prop} [DecidablePred p] {l : List α} {a : α
 
 namespace List
 
-#print List.all_nil /-
+--#print List.all_nil
+/-
 @[simp]
 theorem all_nil (p : α → Bool) : all [] p = tt :=
   rfl
@@ -36,12 +37,11 @@ but is expected to have type
   forall {α : Type.{u1}} {p : α} {a : List.{u1} α} {l : α -> Bool}, Eq.{1} Bool (List.all.{u1} α (List.cons.{u1} α p a) l) (and (l p) (List.all.{u1} α a l))
 Case conversion may be inaccurate. Consider using '#align list.all_cons List.all_consₓ'. -/
 @[simp]
-theorem all_cons (p : α → Bool) (a : α) (l : List α) : all (a :: l) p = (p a && all l p) :=
+theorem all_consₓ (p : α → Bool) (a : α) (l : List α) : all (a :: l) p = (p a && all l p) :=
   rfl
-#align list.all_cons List.all_cons
+#align list.all_cons List.all_consₓ
 
-theorem all_iff_forall {p : α → Bool} : all l p ↔ ∀ a ∈ l, p a :=
-  by
+theorem all_iff_forall {p : α → Bool} : all l p ↔ ∀ a ∈ l, p a := by
   induction' l with a l ih
   · exact iff_of_true rfl (forall_mem_nil _)
   simp only [all_cons, Bool.and_coe_iff, ih, forall_mem_cons]
@@ -65,12 +65,11 @@ but is expected to have type
   forall {α : Type.{u1}} {p : α} {a : List.{u1} α} {l : α -> Bool}, Eq.{1} Bool (List.any.{u1} α (List.cons.{u1} α p a) l) (or (l p) (List.any.{u1} α a l))
 Case conversion may be inaccurate. Consider using '#align list.any_cons List.any_consₓ'. -/
 @[simp]
-theorem any_cons (p : α → Bool) (a : α) (l : List α) : any (a :: l) p = (p a || any l p) :=
+theorem any_consₓ (p : α → Bool) (a : α) (l : List α) : any (a :: l) p = (p a || any l p) :=
   rfl
-#align list.any_cons List.any_cons
+#align list.any_cons List.any_consₓ
 
-theorem any_iff_exists {p : α → Bool} : any l p ↔ ∃ a ∈ l, p a :=
-  by
+theorem any_iff_exists {p : α → Bool} : any l p ↔ ∃ a ∈ l, p a := by
   induction' l with a l ih
   · exact iff_of_false Bool.not_false' (not_exists_mem_nil _)
   simp only [any_cons, Bool.or_coe_iff, ih, exists_mem_cons_iff]
@@ -84,4 +83,3 @@ theorem any_of_mem {p : α → Bool} (h₁ : a ∈ l) (h₂ : p a) : any l p :=
 #align list.any_of_mem List.any_of_mem
 
 end List
-
