e18f5338fe086f2affea0d2aa0717d1f532ff0ce
Xavier-François Roblot <46200072+xroblot@users.noreply.github.com>
2023-01-05 17:20:15 +0100

    Fixes

--- Mathlib/Algebra/Tropical/Lattice.lean
+++ Mathlib/Algebra/Tropical/Lattice.lean
@@ -19,8 +19,8 @@ This file defines the orders induced on tropical algebraic structures by the und
 
 ## Main declarations
 
-* `conditionally_complete_lattice (tropical R)`
-* `conditionally_complete_linear_order (tropical R)`
+* `ConditionallyCompleteLattice (Tropical R)`
+* `ConditionallyCompleteLinearOrder (Tropical R)`
 
 ## Implementation notes
 
@@ -34,43 +34,46 @@ variable {R S : Type _}
 
 open Tropical
 
-instance [HasSup R] : HasSup (Tropical R) where sup x y := trop (untrop x ⊔ untrop y)
+instance [HasSup R] : HasSup (Tropical R) where
+  sup x y := trop (untrop x ⊔ untrop y)
 
-instance [HasInf R] : HasInf (Tropical R) where inf x y := trop (untrop x ⊓ untrop y)
+instance [HasInf R] : HasInf (Tropical R) where
+  inf x y := trop (untrop x ⊓ untrop y)
+
+#print instHasInfTropical
 
 instance [SemilatticeInf R] : SemilatticeInf (Tropical R) :=
-  { Tropical.hasInf,
-    Tropical.partialOrder with
-    le_inf := fun _ _ _ => le_inf
+  { instHasInfTropical,
+    Tropical.instPartialOrderTropical with
+    le_inf := sorry -- fun _ _ _ => le_inf
     inf_le_left := fun _ _ => inf_le_left
     inf_le_right := fun _ _ => inf_le_right }
 
 instance [SemilatticeSup R] : SemilatticeSup (Tropical R) :=
-  { Tropical.hasSup,
-    Tropical.partialOrder with
-    sup_le := fun _ _ _ => sup_le
+  { instHasSupTropical,
+    Tropical.instPartialOrderTropical with
+    sup_le := sorry -- fun _ _ _ => sup_le
     le_sup_left := fun _ _ => le_sup_left
     le_sup_right := fun _ _ => le_sup_right }
 
 instance [Lattice R] : Lattice (Tropical R) :=
-  { Tropical.semilatticeInf, Tropical.semilatticeSup with }
+  { instSemilatticeInfTropical, instSemilatticeSupTropical with }
 
-instance [SupSet R] : SupSet (Tropical R) where sup s := trop (supₛ (untrop '' s))
+instance [SupSet R] : SupSet (Tropical R) where supₛ s := trop (supₛ (untrop '' s))
 
-instance [InfSet R] : InfSet (Tropical R) where inf s := trop (infₛ (untrop '' s))
+instance [InfSet R] : InfSet (Tropical R) where infₛ s := trop (infₛ (untrop '' s))
 
 instance [ConditionallyCompleteLattice R] : ConditionallyCompleteLattice (Tropical R) :=
-  { Tropical.hasSup, Tropical.hasInf,
-    Tropical.lattice with
-    le_cSup := fun s x hs hx =>
-      le_csupₛ (untrop_monotone.map_bdd_above hs) (Set.mem_image_of_mem untrop hx)
-    cSup_le := fun s x hs hx =>
-      csupₛ_le (hs.image untrop) (untrop_monotone.mem_upper_bounds_image hx)
-    le_cInf := fun s x hs hx =>
-      le_cinfₛ (hs.image untrop) (untrop_monotone.mem_lower_bounds_image hx)
-    cInf_le := fun s x hs hx =>
-      cinfₛ_le (untrop_monotone.map_bdd_below hs) (Set.mem_image_of_mem untrop hx) }
+  { instHasSupTropical, instHasInfTropical,
+    instLatticeTropical with
+    le_csupₛ  := fun s x hs hx =>
+      le_csupₛ (untrop_monotone.map_bddAbove hs) (Set.mem_image_of_mem untrop hx)
+    csupₛ_le := fun s x hs hx =>
+      csupₛ_le (hs.image untrop) (untrop_monotone.mem_upperBounds_image hx)
+    le_cinfₛ := fun s x hs hx =>
+      le_cinfₛ (hs.image untrop) (untrop_monotone.mem_lowerBounds_image hx)
+    cinfₛ_le := fun s x hs hx =>
+      cinfₛ_le (untrop_monotone.map_bddBelow hs) (Set.mem_image_of_mem untrop hx) }
 
 instance [ConditionallyCompleteLinearOrder R] : ConditionallyCompleteLinearOrder (Tropical R) :=
-  { Tropical.conditionallyCompleteLattice, Tropical.linearOrder with }
-
+  { instConditionallyCompleteLatticeTropical, Tropical.instLinearOrderTropical with }
