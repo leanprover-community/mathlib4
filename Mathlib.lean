4ac4e46adb8dc2d2874a11765fe8eaa282f1fe7e
qawbecrdtey <qawbecrdtey@kaist.ac.kr>
2023-01-05 17:51:52 +0900

    Mathbin -> Mathlib; add import to Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -269,6 +269,7 @@ import Mathlib.Data.Nat.Prime
 import Mathlib.Data.Nat.Set
 import Mathlib.Data.Nat.Size
 import Mathlib.Data.Nat.Sqrt
+import Mathlib.Data.Nat.SqrtNormNum
 import Mathlib.Data.Nat.Units
 import Mathlib.Data.Nat.Upto
 import Mathlib.Data.Nat.WithBot
