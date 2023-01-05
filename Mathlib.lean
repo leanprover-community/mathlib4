f4cd7f1e6b59db0c63a1fc629f05a7a4e94bfa16
Chris Hughes <chrishughes24@gmail.com>
2023-01-05 15:50:29 +0000

    feat: Port Data.List.Lemmas (#1351)

--- Mathlib.lean
+++ Mathlib.lean
@@ -237,6 +237,7 @@ import Mathlib.Data.List.Card
 import Mathlib.Data.List.Chain
 import Mathlib.Data.List.Defs
 import Mathlib.Data.List.Func
+import Mathlib.Data.List.Lemmas
 import Mathlib.Data.List.Lex
 import Mathlib.Data.List.Nodup
 import Mathlib.Data.List.Pairwise
