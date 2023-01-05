8c3c57cbbb0d6156db055b39ec323249a0ff565d
zeramorphic <zeramorphic@proton.me>
2023-01-05 17:44:37 +0000

    File compiles
    Signed-off-by: zeramorphic <zeramorphic@proton.me>

--- Mathlib/Data/List/Tfae.lean
+++ Mathlib/Data/List/Tfae.lean
@@ -9,6 +9,7 @@ Authors: Johan Commelin, Simon Hudon
 ! if you have ported upstream changes.
 -/
 import Mathlib.Data.List.Basic
+import Mathlib.Init.Data.List.Basic
 
 /-!
 # The Following Are Equivalent
@@ -33,13 +34,13 @@ theorem tfae_nil : Tfae [] :=
   forall_mem_nil _
 #align list.tfae_nil List.tfae_nil
 
-theorem tfae_singleton (p) : Tfae [p] := by simp [tfae, -eq_iff_iff]
+theorem tfae_singleton (p) : Tfae [p] := by simp [Tfae, -eq_iff_iff]
 #align list.tfae_singleton List.tfae_singleton
 
 theorem tfae_cons_of_mem {a b} {l : List Prop} (h : b ∈ l) : Tfae (a :: l) ↔ (a ↔ b) ∧ Tfae l :=
-  ⟨fun H => ⟨H a (by simp) b (Or.inr h), fun p hp q hq => H _ (Or.inr hp) _ (Or.inr hq)⟩,
+  ⟨fun H => ⟨H a (by simp) b (Mem.tail a h), fun p hp q hq => H _ (Mem.tail a hp) _ (Mem.tail a hq)⟩,
     by
-    rintro ⟨ab, H⟩ p (rfl | hp) q (rfl | hq)
+    rintro ⟨ab, H⟩ p (_ | ⟨_, hp⟩) q (_ | ⟨_, hq⟩)
     · rfl
     · exact ab.trans (H _ h _ hq)
     · exact (ab.trans (H _ h _ hp)).symm
@@ -47,30 +48,28 @@ theorem tfae_cons_of_mem {a b} {l : List Prop} (h : b ∈ l) : Tfae (a :: l) ↔
 #align list.tfae_cons_of_mem List.tfae_cons_of_mem
 
 theorem tfae_cons_cons {a b} {l : List Prop} : Tfae (a :: b :: l) ↔ (a ↔ b) ∧ Tfae (b :: l) :=
-  tfae_cons_of_mem (Or.inl rfl)
+  tfae_cons_of_mem (Mem.head _)
 #align list.tfae_cons_cons List.tfae_cons_cons
 
 theorem tfae_of_forall (b : Prop) (l : List Prop) (h : ∀ a ∈ l, a ↔ b) : Tfae l :=
-  fun a₁ h₁ a₂ h₂ => (h _ h₁).trans (h _ h₂).symm
+  fun _a₁ h₁ _a₂ h₂ => (h _ h₁).trans (h _ h₂).symm
 #align list.tfae_of_forall List.tfae_of_forall
 
-/- ./././Mathport/Syntax/Translate/Expr.lean:219:4: warning: unsupported binary notation `«->» -/
 theorem tfae_of_cycle {a b} {l : List Prop} :
-    List.Chain («->» · ·) a (b :: l) → (ilast' b l → a) → Tfae (a :: b :: l) :=
+    List.Chain (· → ·) a (b :: l) → (ilast' b l → a) → Tfae (a :: b :: l) :=
   by
   induction' l with c l IH generalizing a b <;>
-    simp only [tfae_cons_cons, tfae_singleton, and_true_iff, chain_cons, chain.nil] at *
+    simp only [tfae_cons_cons, tfae_singleton, and_true_iff, chain_cons, Chain.nil] at *
   · intro a b
     exact Iff.intro a b
   rintro ⟨ab, ⟨bc, ch⟩⟩ la
   have := IH ⟨bc, ch⟩ (ab ∘ la)
-  exact ⟨⟨ab, la ∘ (this.2 c (Or.inl rfl) _ (ilast'_mem _ _)).1 ∘ bc⟩, this⟩
+  exact ⟨⟨ab, la ∘ (this.2 c (Mem.head _) _ (ilast'_mem _ _)).1 ∘ bc⟩, this⟩
 #align list.tfae_of_cycle List.tfae_of_cycle
 
-theorem Tfae.out {l} (h : Tfae l) (n₁ n₂) {a b} (h₁ : List.nth l n₁ = some a := by rfl)
-    (h₂ : List.nth l n₂ = some b := by rfl) : a ↔ b :=
-  h _ (List.nth_mem h₁) _ (List.nth_mem h₂)
+theorem Tfae.out {l} (h : Tfae l) (n₁ n₂) {a b} (h₁ : List.get? l n₁ = some a := by rfl)
+    (h₂ : List.get? l n₂ = some b := by rfl) : a ↔ b :=
+  h _ (List.get?_mem h₁) _ (List.get?_mem h₂)
 #align list.tfae.out List.Tfae.out
 
 end List
-
