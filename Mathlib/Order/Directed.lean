44bcd9370635098527cf1e15c3d76c88f0079cd8
Rémi Bottinelli <remi.bottinelli@bluewin.ch>
2023-01-06 11:24:27 +0100

    update mathlib3 sync SHAs

--- Mathlib/Order/Directed.lean
+++ Mathlib/Order/Directed.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl
 
 ! This file was ported from Lean 3 source module order.directed
-! leanprover-community/mathlib commit cf9386b56953fb40904843af98b7a80757bbe7f9
+! leanprover-community/mathlib commit 18a5306c091183ac90884daa9373fa3b178e8607
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
