1c42296f7e43c17e4b4dee9cd5f8517a18d60584
Johan Commelin <johan@commelin.net>
2023-01-06 11:54:59 +0000

    Mathbin -> Mathlib; add import to Mathlib.lean

--- Mathlib/Data/List/BigOperators/Basic.lean
+++ Mathlib/Data/List/BigOperators/Basic.lean
@@ -8,7 +8,7 @@ Authors: Johannes Hölzl, Floris van Doorn, Sébastien Gouëzel, Alex J. Best
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
-import Mathbin.Data.List.Forall2
+import Mathlib.Data.List.Forall2
 
 /-!
 # Sums and products from lists
