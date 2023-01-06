dbcf94d8b526458c5b1a0cb6f84c5fc942a842b3
YaelDillies <yael.dillies@gmail.com>
2023-01-06 21:42:29 +0100

    update SHA

--- Mathlib/Order/Lattice.lean
+++ Mathlib/Order/Lattice.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl
 
 ! This file was ported from Lean 3 source module order.lattice
-! leanprover-community/mathlib commit 2258b40dacd2942571c8ce136215350c702dc78f
+! leanprover-community/mathlib commit d6aad9528ddcac270ed35c6f7b5f1d8af25341d6
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
