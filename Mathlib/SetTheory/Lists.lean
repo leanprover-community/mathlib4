50d33336eadd118ef34fa6e09ad1df54d91370c0
qawbecrdtey <qawbecrdtey@kaist.ac.kr>
2023-01-06 10:29:21 +0900

    Mathbin -> Mathlib; add import to Mathlib.lean

--- Mathlib/SetTheory/Lists.lean
+++ Mathlib/SetTheory/Lists.lean
@@ -8,7 +8,7 @@ Authors: Mario Carneiro
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
-import Mathbin.Data.List.Basic
+import Mathlib.Data.List.Basic
 
 /-!
 # A computable model of ZFA without infinity
