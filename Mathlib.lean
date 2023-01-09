0e0255c18508e85f5415fdf8492f39ea48fe9eaa
Lukas Miaskiwskyi <lukas.mias@gmail.com>
2023-01-09 07:48:28 +0000

    feat: port Algebra.Module.Hom (#1417)

--- Mathlib.lean
+++ Mathlib.lean
@@ -62,6 +62,7 @@ import Mathlib.Algebra.Hom.Units
 import Mathlib.Algebra.Homology.ComplexShape
 import Mathlib.Algebra.Invertible
 import Mathlib.Algebra.Module.Basic
+import Mathlib.Algebra.Module.Hom
 import Mathlib.Algebra.Module.Pi
 import Mathlib.Algebra.Module.Prod
 import Mathlib.Algebra.NeZero
