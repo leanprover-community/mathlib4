70af83f4e27f72a38e2447b3fcd2828268881f78
ChrisHughes24 <chrishughes24@gmail.com>
2023-01-09 13:38:33 +0000

    fixes

--- Mathlib/Algebra/Star/Prod.lean
+++ Mathlib/Algebra/Star/Prod.lean
@@ -13,9 +13,9 @@ import Mathlib.Algebra.Ring.Prod
 import Mathlib.Algebra.Module.Prod
 
 /-!
-# `star` on product types
+# `Star` on product types
 
-We put a `has_star` structure on product types that operates elementwise.
+We put a `Star` structure on product types that operates elementwise.
 -/
 
 
@@ -25,23 +25,23 @@ variable {R : Type u} {S : Type v}
 
 namespace Prod
 
-instance [HasStar R] [HasStar S] : HasStar (R × S) where star x := (star x.1, star x.2)
+instance [Star R] [Star S] : Star (R × S) where star x := (star x.1, star x.2)
 
 @[simp]
-theorem fst_star [HasStar R] [HasStar S] (x : R × S) : (star x).1 = star x.1 :=
+theorem fst_star [Star R] [Star S] (x : R × S) : (star x).1 = star x.1 :=
   rfl
 #align prod.fst_star Prod.fst_star
 
 @[simp]
-theorem snd_star [HasStar R] [HasStar S] (x : R × S) : (star x).2 = star x.2 :=
+theorem snd_star [Star R] [Star S] (x : R × S) : (star x).2 = star x.2 :=
   rfl
 #align prod.snd_star Prod.snd_star
 
-theorem star_def [HasStar R] [HasStar S] (x : R × S) : star x = (star x.1, star x.2) :=
+theorem star_def [Star R] [Star S] (x : R × S) : star x = (star x.1, star x.2) :=
   rfl
 #align prod.star_def Prod.star_def
 
-instance [HasInvolutiveStar R] [HasInvolutiveStar S] : HasInvolutiveStar (R × S)
+instance [InvolutiveStar R] [InvolutiveStar S] : InvolutiveStar (R × S)
     where star_involutive _ := Prod.ext (star_star _) (star_star _)
 
 instance [Semigroup R] [Semigroup S] [StarSemigroup R] [StarSemigroup S] : StarSemigroup (R × S)
@@ -53,7 +53,7 @@ instance [AddMonoid R] [AddMonoid S] [StarAddMonoid R] [StarAddMonoid S] : StarA
 instance [NonUnitalSemiring R] [NonUnitalSemiring S] [StarRing R] [StarRing S] : StarRing (R × S) :=
   { Prod.starAddMonoid, (Prod.starSemigroup : StarSemigroup (R × S)) with }
 
-instance {α : Type w} [HasSmul α R] [HasSmul α S] [HasStar α] [HasStar R] [HasStar S]
+instance {α : Type w} [HasSmul α R] [HasSmul α S] [Star α] [Star R] [Star S]
     [StarModule α R] [StarModule α S] : StarModule α (R × S)
     where star_smul r x := Prod.ext (star_smul _ _) (star_smul _ _)
 
@@ -64,4 +64,3 @@ theorem Units.embed_product_star [Monoid R] [StarSemigroup R] (u : Rˣ) :
     Units.embedProduct R (star u) = star (Units.embedProduct R u) :=
   rfl
 #align units.embed_product_star Units.embed_product_star
-
