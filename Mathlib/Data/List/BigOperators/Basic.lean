5ef7512351d402fe61e269eeeec4e35b3e4a0963
Johan Commelin <johan@commelin.net>
2023-01-09 20:05:43 +0100

    minifixes

--- Mathlib/Data/List/BigOperators/Basic.lean
+++ Mathlib/Data/List/BigOperators/Basic.lean
@@ -27,7 +27,7 @@ section Monoid
 
 variable [Monoid M] [Monoid N] [Monoid P] {l l₁ l₂ : List M} {a : M}
 
-@[simp, to_additive]
+@[to_additive (attr := simp)]
 theorem prod_nil : ([] : List M).prod = 1 :=
   rfl
 #align list.prod_nil List.prod_nil
@@ -37,7 +37,7 @@ theorem prod_singleton : [a].prod = a :=
   one_mul a
 #align list.prod_singleton List.prod_singleton
 
-@[simp, to_additive]
+@[to_additive (attr := simp)]
 theorem prod_cons : (a :: l).prod = a * l.prod :=
   calc
     (a :: l).prod = foldl (· * ·) (a * 1) l :=
@@ -46,7 +46,7 @@ theorem prod_cons : (a :: l).prod = a * l.prod :=
 
 #align list.prod_cons List.prod_cons
 
-@[simp, to_additive]
+@[to_additive (attr := simp)]
 theorem prod_append : (l₁ ++ l₂).prod = l₁.prod * l₂.prod :=
   calc
     (l₁ ++ l₂).prod = foldl (· * ·) (foldl (· * ·) 1 l₁ * 1) l₂ := by simp [List.prod]
@@ -59,7 +59,7 @@ theorem prod_concat : (l.concat a).prod = l.prod * a := by
   rw [concat_eq_append, prod_append, prod_singleton]
 #align list.prod_concat List.prod_concat
 
-@[simp, to_additive]
+@[to_additive (attr := simp)]
 theorem prod_join {l : List (List M)} : l.join.prod = (l.map List.prod).prod := by
   induction l <;> [rfl, simp only [*, List.join, map, prod_append, prod_cons]]
 #align list.prod_join List.prod_join
@@ -70,7 +70,7 @@ theorem prod_eq_foldr : ∀ {l : List M}, l.prod = foldr (· * ·) 1 l
   | cons a l => by rw [prod_cons, foldr_cons, prod_eq_foldr]
 #align list.prod_eq_foldr List.prod_eq_foldr
 
-@[simp, to_additive]
+@[to_additive (attr := simp)]
 theorem prod_replicate (a : M) (n : ℕ) : (List.replicate n a).prod = a ^ n :=
   by
   induction' n with n ih
@@ -118,7 +118,7 @@ theorem prod_hom₂ (l : List ι) (f : M → N → P) (hf : ∀ a b c d, f (a * 
   exact hf _ _ _ _
 #align list.prod_hom₂ List.prod_hom₂
 
-@[simp, to_additive]
+@[to_additive (attr := simp)]
 theorem prod_map_mul {α : Type _} [CommMonoid α] {l : List ι} {f g : ι → α} :
     (l.map fun i => f i * g i).prod = (l.map f).prod * (l.map g).prod :=
   l.prod_hom₂ (· * ·) mul_mul_mul_comm (mul_one _) _ _
@@ -143,7 +143,7 @@ theorem prod_map_hom (L : List ι) (f : ι → M) {G : Type _} [MonoidHomClass G
 #align list.prod_map_hom List.prod_map_hom
 
 @[to_additive]
-theorem prod_isUnit : ∀ {L : List M} (u : ∀ m ∈ L, IsUnit m), IsUnit L.prod
+theorem prod_isUnit : ∀ {L : List M} (_ : ∀ m ∈ L, IsUnit m), IsUnit L.prod
   | [], _ => by simp
   | h :: t, u => by
     simp only [List.prod_cons]
@@ -161,7 +161,7 @@ theorem prod_isUnit_iff {α : Type _} [CommMonoid α] {L : List α} :
   exact fun m' h' => Or.elim (eq_or_mem_of_mem_cons h') (fun H => H.substr h.1) fun H => ih h.2 _ H
 #align list.prod_is_unit_iff List.prod_isUnit_iff
 
-@[simp, to_additive]
+@[to_additive (attr := simp)]
 theorem prod_take_mul_prod_drop : ∀ (L : List M) (i : ℕ), (L.take i).prod * (L.drop i).prod = L.prod
   | [], i => by simp [Nat.zero_le]
   | L, 0 => by simp
@@ -170,7 +170,7 @@ theorem prod_take_mul_prod_drop : ∀ (L : List M) (i : ℕ), (L.take i).prod * 
     rw [prod_cons, prod_cons, mul_assoc, prod_take_mul_prod_drop t]
 #align list.prod_take_mul_prod_drop List.prod_take_mul_prod_drop
 
-@[simp, to_additive]
+@[to_additive (attr := simp)]
 theorem prod_take_succ :
     ∀ (L : List M) (i : ℕ) (p), (L.take (i + 1)).prod = (L.take i).prod * L.nthLe i p
   | [], i, p => by cases p
@@ -249,7 +249,7 @@ theorem _root_.Commute.list_prod_right (l : List M) (y : M) (h : ∀ x ∈ l, Co
 @[to_additive]
 theorem _root_.Commute.list_prod_left (l : List M) (y : M) (h : ∀ x ∈ l, Commute x y) :
     Commute l.prod y :=
-  ((Commute.list_prod_right _ _) fun x hx => (h _ hx).symm).symm
+  ((Commute.list_prod_right _ _) fun _ hx => (h _ hx).symm).symm
 #align commute.list_prod_left Commute.list_prod_left
 
 @[to_additive sum_le_sum]
@@ -414,7 +414,7 @@ theorem prod_reverse_noncomm : ∀ L : List G, L.reverse.prod = (L.map fun x => 
 
 set_option linter.deprecated false in
 /-- Counterpart to `List.prod_take_succ` when we have an inverse operation -/
-@[simp, to_additive "Counterpart to `List.sum_take_succ` when we have an negation operation"]
+@[to_additive (attr := simp) "Counterpart to `List.sum_take_succ` when we have an negation operation"]
 theorem prod_drop_succ :
     ∀ (L : List G) (i : ℕ) (p), (L.drop (i + 1)).prod = (L.nthLe i p)⁻¹ * (L.drop i).prod
   | [], i, p => False.elim (Nat.not_lt_zero _ p)
@@ -531,7 +531,7 @@ theorem sum_le_foldr_max [AddMonoid M] [AddMonoid N] [LinearOrder N] (f : M → 
   exact (hadd _ _).trans (max_le_max le_rfl IH)
 #align list.sum_le_foldr_max List.sum_le_foldr_max
 
-@[simp, to_additive]
+@[to_additive (attr := simp)]
 theorem prod_erase [DecidableEq M] [CommMonoid M] {a} :
     ∀ {l : List M}, a ∈ l → a * (l.erase a).prod = l.prod
   | b :: l, h => by
@@ -540,7 +540,7 @@ theorem prod_erase [DecidableEq M] [CommMonoid M] {a} :
     · simp only [List.erase, beq_false_of_ne ne.symm, prod_cons, prod_erase h, mul_left_comm a b]
 #align list.prod_erase List.prod_erase
 
-@[simp, to_additive]
+@[to_additive (attr := simp)]
 theorem prod_map_erase [DecidableEq ι] [CommMonoid M] (f : ι → M) {a} :
     ∀ {l : List ι}, a ∈ l → f a * ((l.erase a).map f).prod = (l.map f).prod
   | b :: l, h => by
@@ -597,12 +597,12 @@ section
 
 variable [One α] [Mul α] [Inv α]
 
-@[simp, to_additive]
+@[to_additive (attr := simp)]
 theorem alternating_prod_nil : alternatingProd ([] : List α) = 1 :=
   rfl
 #align list.alternating_prod_nil List.alternating_prod_nil
 
-@[simp, to_additive]
+@[to_additive (attr := simp)]
 theorem alternating_prod_singleton (a : α) : alternatingProd [a] = a :=
   rfl
 #align list.alternating_prod_singleton List.alternating_prod_singleton
@@ -631,7 +631,7 @@ theorem alternating_prod_cons' :
     rw [alternating_prod_cons_cons', alternating_prod_cons' b l, mul_inv, inv_inv, mul_assoc]
 #align list.alternating_prod_cons' List.alternating_prod_cons'
 
-@[simp, to_additive]
+@[to_additive (attr := simp)]
 theorem alternating_prod_cons (a : α) (l : List α) :
     alternatingProd (a :: l) = a / alternatingProd l := by
   rw [div_eq_mul_inv, alternating_prod_cons']
