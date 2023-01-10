550e290706dc8bf8edcbe06b090a126168d0d39a
Moritz Doll <moritz.doll@googlemail.com>
2023-01-10 06:56:49 +0000

    feat: port Data.Real.CauSeq (#1124)
    - [x] depends on: #1398
    
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>
    Co-authored-by: ChrisHughes24 <chrishughes24@gmail.com>
    Co-authored-by: Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
    Co-authored-by: Chris Hughes <33847686+ChrisHughes24@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -316,6 +316,7 @@ import Mathlib.Data.Rat.Init
 import Mathlib.Data.Rat.Lemmas
 import Mathlib.Data.Rat.Order
 import Mathlib.Data.Rat.Sqrt
+import Mathlib.Data.Real.CauSeq
 import Mathlib.Data.Rel
 import Mathlib.Data.Semiquot
 import Mathlib.Data.Set.Accumulate
