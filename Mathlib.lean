557f77b32a77594a924ac8d889c4cf44cbb8e5b5
Moritz Firsching <moritz.firsching@gmail.com>
2023-01-10 15:05:45 +0000

    feat: port Data.List.Rdrop (#1382)
    Co-authored-by: Moritz Firsching <firsching@google.com>
    Co-authored-by: qawbecrdtey <40463813+qawbecrdtey@users.noreply.github.com>
    Co-authored-by: Lukas Miaskiwskyi <lukas.mias@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -263,6 +263,7 @@ import Mathlib.Data.List.Perm
 import Mathlib.Data.List.Permutation
 import Mathlib.Data.List.ProdSigma
 import Mathlib.Data.List.Range
+import Mathlib.Data.List.Rdrop
 import Mathlib.Data.List.Sections
 import Mathlib.Data.List.TFAE
 import Mathlib.Data.List.Zip
