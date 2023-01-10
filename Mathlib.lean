1d28dadf8536f004e18296eaeee436b4ffd16427
Heather Macbeth <anatolededecker@gmail.com>
2023-01-10 18:28:52 +0000

    feat: port Data.Rat.Floor (#1418)
    - [x] depends on: #1304
    - [x] depends on: #1422
    - [x] depends on: #1427
    
    Co-authored-by: Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
    Co-authored-by: Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -323,6 +323,7 @@ import Mathlib.Data.Quot
 import Mathlib.Data.Rat.Basic
 import Mathlib.Data.Rat.Cast
 import Mathlib.Data.Rat.Defs
+import Mathlib.Data.Rat.Floor
 import Mathlib.Data.Rat.Init
 import Mathlib.Data.Rat.Lemmas
 import Mathlib.Data.Rat.Order
