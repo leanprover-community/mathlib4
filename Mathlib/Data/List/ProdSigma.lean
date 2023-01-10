3752193887131f406174a16701cd57c3967bcc3f
qawbecrdtey <qawbecrdtey@kaist.ac.kr>
2023-01-10 17:07:18 +0900

    Mathbin -> Mathlib; add import to Mathlib.lean

--- Mathlib/Data/List/ProdSigma.lean
+++ Mathlib/Data/List/ProdSigma.lean
@@ -8,7 +8,7 @@ Authors: Leonardo de Moura, Mario Carneiro
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
-import Mathbin.Data.List.BigOperators.Basic
+import Mathlib.Data.List.BigOperators.Basic
 
 /-!
 # Lists in product and sigma types
