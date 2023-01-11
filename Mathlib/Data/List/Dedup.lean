b3119e9de87dae94b9eb682f4d50264874d41076
Chris Hughes <33847686+ChrisHughes24@users.noreply.github.com>
2023-01-11 11:13:59 +0000

    chore Data.List.Dedup : Update SHA (#1484)

--- Mathlib/Data/List/Dedup.lean
+++ Mathlib/Data/List/Dedup.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro
 
 ! This file was ported from Lean 3 source module data.list.dedup
-! leanprover-community/mathlib commit 7b78d1776212a91ecc94cf601f83bdcc46b04213
+! leanprover-community/mathlib commit 6133ae2da6ae6693248bb5451de703f1ef154cc8
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
