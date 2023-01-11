7ace52b6d9a00aa1db8cf347d2fa7caf738e9ea5
Xavier-François Roblot <46200072+xroblot@users.noreply.github.com>
2023-01-11 09:51:53 +0100

    Mathbin -> Mathlib; add import to Mathlib.lean

--- Mathlib/Algebra/FreeMonoid/Count.lean
+++ Mathlib/Algebra/FreeMonoid/Count.lean
@@ -8,8 +8,8 @@ Authors: Yury Kudryashov
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
-import Mathbin.Algebra.FreeMonoid.Basic
-import Mathbin.Data.List.Count
+import Mathlib.Algebra.FreeMonoid.Basic
+import Mathlib.Data.List.Count
 
 /-!
 # `list.count` as a bundled homomorphism
