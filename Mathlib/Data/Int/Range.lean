7354a19dacdfa5b04e10a899f585443e2e212684
qawbecrdtey <qawbecrdtey@kaist.ac.kr>
2023-01-11 12:06:43 +0900

    Initial commit.

--- Mathlib/Data/Int/Range.lean
+++ Mathlib/Data/Int/Range.lean
@@ -19,13 +19,13 @@ less than `n`.
 
 ## Note
 
-This could be unified with `data.list.intervals`. See the TODOs there.
+This could be unified with `Data.List.Intervals`. See the TODOs there.
 -/
 
 
 namespace Int
 
-/-- List enumerating `[m, n)`. This is the ℤ variant of `list.Ico`. -/
+/-- List enumerating `[m, n)`. This is the ℤ variant of `List.Ico`. -/
 def range (m n : ℤ) : List ℤ :=
   (List.range (toNat (n - m))).map fun r => m + r
 #align int.range Int.range
@@ -37,15 +37,15 @@ theorem mem_range_iff {m n r : ℤ} : r ∈ range m n ↔ m ≤ r ∧ r < n :=
       ⟨le_add_of_nonneg_right (ofNat_zero_le s),
         add_lt_of_lt_sub_left <|
           match n - m, h1 with
-          | (k : ℕ), h1 => by rwa [List.mem_range, to_nat_coe_nat, ← coe_nat_lt] at h1⟩,
+          | (k : ℕ), h1 => by rwa [List.range, toNat_coe_nat, ← ofNat_lt] at h1⟩,
     fun ⟨h1, h2⟩ =>
     List.mem_map'.2
       ⟨toNat (r - m),
         List.mem_range.2 <| by
-          rw [← coe_nat_lt, to_nat_of_nonneg (sub_nonneg_of_le h1),
-              to_nat_of_nonneg (sub_nonneg_of_le (le_of_lt (lt_of_le_of_lt h1 h2)))] <;>
+          rw [← ofNat_lt, toNat_of_nonneg (sub_nonneg_of_le h1),
+              toNat_of_nonneg (sub_nonneg_of_le (le_of_lt (lt_of_le_of_lt h1 h2)))] <;>
             exact sub_lt_sub_right h2 _,
-        show m + _ = _ by rw [to_nat_of_nonneg (sub_nonneg_of_le h1), add_sub_cancel'_right]⟩⟩
+        show m + _ = _ by rw [toNat_of_nonneg (sub_nonneg_of_le h1), add_sub_cancel'_right]⟩⟩
 #align int.mem_range_iff Int.mem_range_iff
 
 instance decidableLeLt (P : Int → Prop) [DecidablePred P] (m n : ℤ) :
@@ -70,4 +70,3 @@ instance decidableLtLe (P : Int → Prop) [DecidablePred P] (m n : ℤ) :
 #align int.decidable_lt_le Int.decidableLtLe
 
 end Int
-
