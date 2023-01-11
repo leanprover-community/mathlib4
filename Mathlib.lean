567e9bc0f2da9b19d583d79846fc3a7554fec97e
qawbecrdtey <qawbecrdtey@kaist.ac.kr>
2023-01-11 11:20:22 +0000

    feat: port Data.Int.Range (#1477)
    Co-authored-by: ChrisHughes24 <chrishughes24@gmail.com>
    Co-authored-by: Johan Commelin <johan@commelin.net>

--- Mathlib.lean
+++ Mathlib.lean
@@ -242,6 +242,7 @@ import Mathlib.Data.Int.NatPrime
 import Mathlib.Data.Int.Order.Basic
 import Mathlib.Data.Int.Order.Lemmas
 import Mathlib.Data.Int.Order.Units
+import Mathlib.Data.Int.Range
 import Mathlib.Data.Int.Sqrt
 import Mathlib.Data.Int.Units
 import Mathlib.Data.KVMap
