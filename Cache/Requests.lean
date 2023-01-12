leanprover-community/mathlib4

commit 1d47ea1b075356280e0fb120c8b200b9995817d9

feat: specific file names for `cache get` and `cache get!` (#1430)
This PR allows arguments for `cache get` and `cache get!` as explained in the help menu:

```text
'get' and 'get!' can process list of paths, allowing the user to be more
specific about what should be downloaded. For example, with automatic glob
expansion in shell, one can call:

$ lake exe cache get Mathlib/Algebra/Field/* Mathlib/Data/*

Which will download the cache for:
* Everything that starts with 'Mathlib/Algebra/Field/'
* Everything that starts with 'Mathlib/Data/'
* Everything that's needed for the above
```

Cache/Requests.lean
@@ -6,14 +6,6 @@ Authors: Arthur Paulino
 
 import Cache.Hashing
 
-def ByteArray.startsWith (a b : ByteArray) : Bool := Id.run do
-  let size := b.size
-  let a := a.copySlice 0 .empty 0 size
-  if size != a.size then return false
-  for i in [0 : size] do
-    if a.get! i != b.get! i then return false
-  return true
-
 namespace Cache.Requests
 
 /-- Azure blob URL -/
@@ -41,31 +33,18 @@ def mkGetConfigContent (hashMap : IO.HashMap) : IO String := do
     pure $ (s!"url = {← mkFileURL fileName none}\n-o {IO.CACHEDIR / fileName}") :: acc
   return "\n".intercalate l
 
-/--
-Calls `curl` to download files from the server.
-
-It downloads the files to CACHEDIR (.cache).
--/
-def downloadFiles (hashMap : IO.HashMap) : IO Unit := do
+/-- Calls `curl` to download files from the server to `CACHEDIR` (`.cache`) then unpacks them -/
+def getFiles (hashMap : IO.HashMap) (forceDownload : Bool) : IO Unit := do
+  let hashMap := if forceDownload then hashMap else hashMap.filter (← IO.getLocalCacheSet) false
   let size := hashMap.size
   if size > 0 then
     IO.mkDir IO.CACHEDIR
     IO.println s!"Attempting to download {size} file(s)"
     IO.FS.writeFile IO.CURLCFG (← mkGetConfigContent hashMap)
     discard $ IO.runCmd "curl"
-        #["-X", "GET", "--parallel", "-f", "-s", "-K", IO.CURLCFG.toString] false
+      #["-X", "GET", "--parallel", "-f", "-s", "-K", IO.CURLCFG.toString] false
     IO.FS.removeFile IO.CURLCFG
   else IO.println "No files to download"
-
-/--
-Downloads files from the server and unpacks them.
--/
-def getFiles (hashMap : IO.HashMap) (forceDownload : Bool) : IO Unit := do
-  downloadFiles
-    (← if forceDownload then
-      pure hashMap
-    else
-      pure (hashMap.filter (← IO.getLocalCacheSet) false))
   IO.unpackCache hashMap
 
 end Get
@@ -97,10 +76,10 @@ end Put
 
 section Commit
 
-def isStatusClean : IO Bool :=
+def isGitStatusClean : IO Bool :=
   return (← IO.runCmd "git" #["status", "--porcelain"]).isEmpty
 
-def getCommitHash : IO String := do
+def getGitCommitHash : IO String := do
   let ret := (← IO.runCmd "git" #["log", "-1"]).replace "\n" " "
   match ret.splitOn " " with
   | "commit" :: hash :: _ => return hash
@@ -112,7 +91,7 @@ Sends a commit file to the server, containing the hashes of the respective commi
 The file name is the current Git hash and the `c/` prefix means that it's a commit file.
 -/
 def commit (hashMap : IO.HashMap) (overwrite : Bool) (token : String) : IO Unit := do
-  let hash ← getCommitHash
+  let hash ← getGitCommitHash
   let path := IO.CACHEDIR / hash
   IO.mkDir IO.CACHEDIR
   IO.FS.writeFile path $ ("\n".intercalate $ hashMap.hashes.toList.map toString) ++ "\n"

Cache/Hashing.lean
@@ -11,8 +11,32 @@ namespace Cache.Hashing
 
 open System IO
 
-/-- We cache the hash of each file for faster lookup -/
-abbrev HashM := StateT IO.HashMap IO
+structure HashMemo where
+  depsMap : Lean.HashMap FilePath (Array FilePath)
+  hashMap : HashMap
+  deriving Inhabited
+
+partial def insertDeps (hashMap : HashMap) (path : FilePath) (hashMemo : HashMemo) : HashMap :=
+  if hashMap.contains path then hashMap else
+  match (hashMemo.depsMap.find? path, hashMemo.hashMap.find? path) with
+  | (some deps, some hash) => deps.foldl (insertDeps · · hashMemo) (hashMap.insert path hash)
+  | _ => hashMap
+
+/--
+Filters the `HashMap` of a `HashMemo` so that it only contains key/value pairs such that every key:
+* Belongs to the given list of file paths or
+* Corresponds to a file that's imported (transitively of not) by some file in the list of file paths
+-/
+def HashMemo.filterByFilePaths (hashMemo : HashMemo) (filePaths : List FilePath) : IO HashMap := do
+  let mut hashMap := default
+  for filePath in filePaths do
+    if hashMemo.hashMap.contains filePath then
+      hashMap := insertDeps hashMap filePath hashMemo
+    else throw $ IO.userError s!"No match for {filePath}"
+  return hashMap
+
+/-- We cache the hash of each file and their dependencies for later lookup -/
+abbrev HashM := StateT HashMemo IO
 
 /-- Gets the file paths to Mathlib files imported on a Lean source -/
 def getFileImports (source : String) (pkgDirs : PackageDirs) : Array FilePath :=
@@ -46,17 +70,21 @@ Computes the hash of a file, which mixes:
 * The hashes of the imported files that are part of `Mathlib`
 -/
 partial def getFileHash (filePath : FilePath) : HashM UInt64 := do
-  match (← get).find? filePath with
+  match (← get).hashMap.find? filePath with
   | some hash => pure hash
   | none =>
     let content ← IO.FS.readFile $ (← IO.getPackageDir filePath) / filePath
-    let importHashes ← (getFileImports content pkgDirs).mapM getFileHash
+    let fileImports := getFileImports content pkgDirs
+    let importHashes ← fileImports.mapM getFileHash
     let pathHash := hash filePath.components
     let fileHash := hash $ rootHash :: pathHash :: content.hash :: importHashes.toList
-    modifyGet (fileHash, ·.insert filePath fileHash)
+    modifyGet fun stt =>
+      (fileHash, { stt with
+        hashMap := stt.hashMap.insert filePath fileHash
+        depsMap := stt.depsMap.insert filePath fileImports })
 
 /-- Main API to retrieve the hashes of the Lean files -/
-def getHashes : IO IO.HashMap :=
+def getHashMemo : IO HashMemo :=
   return (← StateT.run (getFileHash ⟨"Mathlib.lean"⟩) default).2
 
 end Cache.Hashing

Cache/IO.lean
@@ -176,7 +176,7 @@ def unpackCache (hashMap : HashMap) : IO Unit := do
           "-C", mathlibDepPath.toString]
   else IO.println "No cache files to decompress"
 
-/-- Retrieves the azure token from the file system -/
+/-- Retrieves the azure token from the environment -/
 def getToken : IO String := do
   let some token ← IO.getEnv "MATHLIB_CACHE_SAS"
     | throw $ IO.userError "environment variable MATHLIB_CACHE_SAS must be set to upload caches"

Cache/Main.lean
@@ -11,31 +11,47 @@ Usage: cache [COMMAND]
 
 Commands:
   # No priviledge required
-  get       Download linked files missing on the local cache and decompress
-  get!      Download all linked files and decompress
-  mk        Compress non-compressed build files into the local cache
-  mk!       Compress build files into the local cache (no skipping)
-  unpack    Decompress linked already downloaded files
-  clean     Delete non-linked files
-  clean!    Delete everything on the local cache
+  get  [ARGS]  Download linked files missing on the local cache and decompress
+  get! [ARGS]  Download all linked files and decompress
+  mk           Compress non-compressed build files into the local cache
+  mk!          Compress build files into the local cache (no skipping)
+  unpack       Decompress linked already downloaded files
+  clean        Delete non-linked files
+  clean!       Delete everything on the local cache
 
   # Privilege required
-  put       Run 'mk' then upload linked files missing on the server
-  put!      Run 'mk' then upload all linked files
-  commit    Write a commit on the server
-  commit!   (Over)write a commit on the server
-  collect   TODO
+  put          Run 'mk' then upload linked files missing on the server
+  put!         Run 'mk' then upload all linked files
+  commit       Write a commit on the server
+  commit!      Overwrite a commit on the server
+  collect      TODO
 
 * Linked files refer to local cache files with corresponding Lean sources
-* Commands ending with '!' should be used manually, when hot-fixes are needed"
+* Commands ending with '!' should be used manually, when hot-fixes are needed
+
+# The arguments for 'get' and 'get!'
+
+'get' and 'get!' can process list of paths, allowing the user to be more
+specific about what should be downloaded. For example, with automatic glob
+expansion in shell, one can call:
+
+$ lake exe cache get Mathlib/Algebra/Field/* Mathlib/Data/*
+
+Which will download the cache for:
+* Everything that starts with 'Mathlib/Algebra/Field/'
+* Everything that starts with 'Mathlib/Data/'
+* Everything that's needed for the above"
 
 open Cache IO Hashing Requests in
 def main (args : List String) : IO Unit := do
   if !(← validateCurl) then return
-  let hashMap  ← getHashes
+  let hashMemo ← getHashMemo
+  let hashMap := hashMemo.hashMap
   match args with
   | ["get"] => getFiles hashMap false
   | ["get!"] => getFiles hashMap true
+  | "get"  :: args => getFiles (← hashMemo.filterByFilePaths (args.map .mk)) false
+  | "get!" :: args => getFiles (← hashMemo.filterByFilePaths (args.map .mk)) true
   | ["mk"] => discard $ mkCache hashMap false
   | ["mk!"] => discard $ mkCache hashMap true
   | ["unpack"] => unpackCache hashMap
@@ -45,10 +61,10 @@ def main (args : List String) : IO Unit := do
   | ["put"] => putFiles (← mkCache hashMap false) false (← getToken)
   | ["put!"] => putFiles (← mkCache hashMap false) true (← getToken)
   | ["commit"] =>
-    if !(← isStatusClean) then IO.println "Please commit your changes first" return else
+    if !(← isGitStatusClean) then IO.println "Please commit your changes first" return else
     commit hashMap false (← getToken)
   | ["commit!"] =>
-    if !(← isStatusClean) then IO.println "Please commit your changes first" return else
+    if !(← isGitStatusClean) then IO.println "Please commit your changes first" return else
     commit hashMap true (← getToken)
   | ["collect"] => IO.println "TODO"
   | _ => println help

commit 84db453d24a0fe039ec26f57e2ed642d2eb4a7c6

fix: unpack all files in lake cache get (#1510)
Redo of #1342.  This regression was introduced in #1430.

Cache/Requests.lean
@@ -33,8 +33,8 @@ def mkGetConfigContent (hashMap : IO.HashMap) : IO String := do
     pure $ (s!"url = {← mkFileURL fileName none}\n-o {IO.CACHEDIR / fileName}") :: acc
   return "\n".intercalate l
 
-/-- Calls `curl` to download files from the server to `CACHEDIR` (`.cache`) then unpacks them -/
-def getFiles (hashMap : IO.HashMap) (forceDownload : Bool) : IO Unit := do
+/-- Calls `curl` to download files from the server to `CACHEDIR` (`.cache`) -/
+def downloadFiles (hashMap : IO.HashMap) (forceDownload : Bool) : IO Unit := do
   let hashMap := if forceDownload then hashMap else hashMap.filter (← IO.getLocalCacheSet) false
   let size := hashMap.size
   if size > 0 then
@@ -45,6 +45,10 @@ def getFiles (hashMap : IO.HashMap) (forceDownload : Bool) : IO Unit := do
       #["-X", "GET", "--parallel", "-f", "-s", "-K", IO.CURLCFG.toString] false
     IO.FS.removeFile IO.CURLCFG
   else IO.println "No files to download"
+
+/-- Downloads missing files, and unpacks files. -/
+def getFiles (hashMap : IO.HashMap) (forceDownload : Bool) : IO Unit := do
+  downloadFiles hashMap forceDownload
   IO.unpackCache hashMap
 
 end Get