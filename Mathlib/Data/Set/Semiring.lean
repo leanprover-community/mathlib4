dfc11bbbdf209f03c34482f7fccfeb3aafe4d7a9
Chris Hughes <33847686+ChrisHughes24@users.noreply.github.com>
2023-01-13 14:52:09 +0000

    Update Mathlib/Data/Set/Semiring.lean

--- Mathlib/Data/Set/Semiring.lean
+++ Mathlib/Data/Set/Semiring.lean
@@ -26,7 +26,7 @@ open Pointwise
 variable {α β : Type _}
 
 -- Porting note: mathlib3 uses `deriving Inhabited, PartialOrder, OrderBot`
-/-- An alias for `set α`, which has a semiring structure given by `∪` as "addition" and pointwise
+/-- An alias for `Set α`, which has a semiring structure given by `∪` as "addition" and pointwise
   multiplication `*` as "multiplication". -/
 def SetSemiring (α : Type _) : Type _ :=
   Set α
