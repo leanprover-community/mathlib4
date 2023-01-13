e62012c2c6478d795c897fc877426174077bea6e
ChrisHughes24 <chrishughes24@gmail.com>
2023-01-13 17:39:09 +0000

    lemma naming

--- Mathlib/Data/Multiset/Sum.lean
+++ Mathlib/Data/Multiset/Sum.lean
@@ -34,82 +34,82 @@ def disjSum : Multiset (Sum α β) :=
 #align multiset.disj_sum Multiset.disjSum
 
 @[simp]
-theorem zero_disj_sum : (0 : Multiset α).disjSum t = t.map inr :=
+theorem zero_disjSum : (0 : Multiset α).disjSum t = t.map inr :=
   zero_add _
-#align multiset.zero_disj_sum Multiset.zero_disj_sum
+#align multiset.zero_disj_sum Multiset.zero_disjSum
 
 @[simp]
-theorem disj_sum_zero : s.disjSum (0 : Multiset β) = s.map inl :=
+theorem disjSum_zero : s.disjSum (0 : Multiset β) = s.map inl :=
   add_zero _
-#align multiset.disj_sum_zero Multiset.disj_sum_zero
+#align multiset.disj_sum_zero Multiset.disjSum_zero
 
 @[simp]
-theorem card_disj_sum : Multiset.card (s.disjSum t) = Multiset.card s + Multiset.card t := by
+theorem card_disjSum : Multiset.card (s.disjSum t) = Multiset.card s + Multiset.card t := by
   rw [disjSum, card_add, card_map, card_map]
-#align multiset.card_disj_sum Multiset.card_disj_sum
+#align multiset.card_disj_sum Multiset.card_disjSum
 
 variable {s t} {s₁ s₂ : Multiset α} {t₁ t₂ : Multiset β} {a : α} {b : β} {x : Sum α β}
 
-theorem mem_disj_sum : x ∈ s.disjSum t ↔ (∃ a, a ∈ s ∧ inl a = x) ∨ ∃ b, b ∈ t ∧ inr b = x := by
+theorem mem_disjSum : x ∈ s.disjSum t ↔ (∃ a, a ∈ s ∧ inl a = x) ∨ ∃ b, b ∈ t ∧ inr b = x := by
   simp_rw [disjSum, mem_add, mem_map]
-#align multiset.mem_disj_sum Multiset.mem_disj_sum
+#align multiset.mem_disj_sum Multiset.mem_disjSum
 
 @[simp]
-theorem inl_mem_disj_sum : inl a ∈ s.disjSum t ↔ a ∈ s := by
-  rw [mem_disj_sum, or_iff_left]
+theorem inl_mem_disjSum : inl a ∈ s.disjSum t ↔ a ∈ s := by
+  rw [mem_disjSum, or_iff_left]
   -- Porting note: Previous code for L62 was: simp only [exists_eq_right]
   simp only [inl.injEq, exists_eq_right]
   rintro ⟨b, _, hb⟩
   exact inr_ne_inl hb
-#align multiset.inl_mem_disj_sum Multiset.inl_mem_disj_sum
+#align multiset.inl_mem_disj_sum Multiset.inl_mem_disjSum
 
 @[simp]
-theorem inr_mem_disj_sum : inr b ∈ s.disjSum t ↔ b ∈ t := by
-  rw [mem_disj_sum, or_iff_right]
+theorem inr_mem_disjSum : inr b ∈ s.disjSum t ↔ b ∈ t := by
+  rw [mem_disjSum, or_iff_right]
   -- Porting note: Previous code for L72 was: simp only [exists_eq_right]
   simp only [inr.injEq, exists_eq_right]
   rintro ⟨a, _, ha⟩
   exact inl_ne_inr ha
-#align multiset.inr_mem_disj_sum Multiset.inr_mem_disj_sum
+#align multiset.inr_mem_disj_sum Multiset.inr_mem_disjSum
 
-theorem disj_sum_mono (hs : s₁ ≤ s₂) (ht : t₁ ≤ t₂) : s₁.disjSum t₁ ≤ s₂.disjSum t₂ :=
+theorem disjSum_mono (hs : s₁ ≤ s₂) (ht : t₁ ≤ t₂) : s₁.disjSum t₁ ≤ s₂.disjSum t₂ :=
   add_le_add (map_le_map hs) (map_le_map ht)
-#align multiset.disj_sum_mono Multiset.disj_sum_mono
+#align multiset.disj_sum_mono Multiset.disjSum_mono
 
-theorem disj_sum_mono_left (t : Multiset β) : Monotone fun s : Multiset α => s.disjSum t :=
+theorem disjSum_mono_left (t : Multiset β) : Monotone fun s : Multiset α => s.disjSum t :=
   fun _ _ hs => add_le_add_right (map_le_map hs) _
-#align multiset.disj_sum_mono_left Multiset.disj_sum_mono_left
+#align multiset.disj_sum_mono_left Multiset.disjSum_mono_left
 
-theorem disj_sum_mono_right (s : Multiset α) :
+theorem disjSum_mono_right (s : Multiset α) :
     Monotone (s.disjSum : Multiset β → Multiset (Sum α β)) := fun _ _ ht =>
   add_le_add_left (map_le_map ht) _
-#align multiset.disj_sum_mono_right Multiset.disj_sum_mono_right
+#align multiset.disj_sum_mono_right Multiset.disjSum_mono_right
 
-theorem disj_sum_lt_disj_sum_of_lt_of_le (hs : s₁ < s₂) (ht : t₁ ≤ t₂) :
+theorem disjSum_lt_disjSum_of_lt_of_le (hs : s₁ < s₂) (ht : t₁ ≤ t₂) :
     s₁.disjSum t₁ < s₂.disjSum t₂ :=
   add_lt_add_of_lt_of_le (map_lt_map hs) (map_le_map ht)
-#align multiset.disj_sum_lt_disj_sum_of_lt_of_le Multiset.disj_sum_lt_disj_sum_of_lt_of_le
+#align multiset.disj_sum_lt_disj_sum_of_lt_of_le Multiset.disjSum_lt_disjSum_of_lt_of_le
 
-theorem disj_sum_lt_disj_sum_of_le_of_lt (hs : s₁ ≤ s₂) (ht : t₁ < t₂) :
+theorem disjSum_lt_disjSum_of_le_of_lt (hs : s₁ ≤ s₂) (ht : t₁ < t₂) :
     s₁.disjSum t₁ < s₂.disjSum t₂ :=
   add_lt_add_of_le_of_lt (map_le_map hs) (map_lt_map ht)
-#align multiset.disj_sum_lt_disj_sum_of_le_of_lt Multiset.disj_sum_lt_disj_sum_of_le_of_lt
+#align multiset.disj_sum_lt_disj_sum_of_le_of_lt Multiset.disjSum_lt_disjSum_of_le_of_lt
 
-theorem disj_sum_strict_mono_left (t : Multiset β) : StrictMono fun s : Multiset α => s.disjSum t :=
-  fun _ _ hs => disj_sum_lt_disj_sum_of_lt_of_le hs le_rfl
-#align multiset.disj_sum_strict_mono_left Multiset.disj_sum_strict_mono_left
+theorem disjSum_strictMono_left (t : Multiset β) : StrictMono fun s : Multiset α => s.disjSum t :=
+  fun _ _ hs => disjSum_lt_disjSum_of_lt_of_le hs le_rfl
+#align multiset.disj_sum_strict_mono_left Multiset.disjSum_strictMono_left
 
-theorem disj_sum_strict_mono_right (s : Multiset α) :
+theorem disjSum_strictMono_right (s : Multiset α) :
     StrictMono (s.disjSum : Multiset β → Multiset (Sum α β)) := fun _ _ =>
-  disj_sum_lt_disj_sum_of_le_of_lt le_rfl
-#align multiset.disj_sum_strict_mono_right Multiset.disj_sum_strict_mono_right
+  disjSum_lt_disjSum_of_le_of_lt le_rfl
+#align multiset.disj_sum_strict_mono_right Multiset.disjSum_strictMono_right
 
-protected theorem Nodup.disj_sum (hs : s.Nodup) (ht : t.Nodup) : (s.disjSum t).Nodup := by
+protected theorem Nodup.disjSum (hs : s.Nodup) (ht : t.Nodup) : (s.disjSum t).Nodup := by
   refine' ((hs.map inl_injective).add_iff <| ht.map inr_injective).2 fun x hs ht => _
   rw [Multiset.mem_map] at hs ht
   obtain ⟨a, _, rfl⟩ := hs
   obtain ⟨b, _, h⟩ := ht
   exact inr_ne_inl h
-#align multiset.nodup.disj_sum Multiset.Nodup.disj_sum
+#align multiset.nodup.disj_sum Multiset.Nodup.disjSum
 
 end Multiset
