a840ab8ee8673495520383c1e4a42b7d68c955fc
qawbecrdtey <qawbecrdtey@kaist.ac.kr>
2023-01-14 01:38:36 +0900

    Initial commit.

--- Mathlib/Data/Multiset/Sum.lean
+++ Mathlib/Data/Multiset/Sum.lean
@@ -13,12 +13,12 @@ import Mathlib.Data.Multiset.Nodup
 /-!
 # Disjoint sum of multisets
 
-This file defines the disjoint sum of two multisets as `multiset (α ⊕ β)`. Beware not to confuse
-with the `multiset.sum` operation which computes the additive sum.
+This file defines the disjoint sum of two multisets as `Multiset (α ⊕ β)`. Beware not to confuse
+with the `Multiset.sum` operation which computes the additive sum.
 
 ## Main declarations
 
-* `multiset.disj_sum`: `s.disj_sum t` is the disjoint sum of `s` and `t`.
+* `Multiset.disjSum`: `s.disjSum t` is the disjoint sum of `s` and `t`.
 -/
 
 
@@ -44,14 +44,14 @@ theorem disj_sum_zero : s.disjSum (0 : Multiset β) = s.map inl :=
 #align multiset.disj_sum_zero Multiset.disj_sum_zero
 
 @[simp]
-theorem card_disj_sum : (s.disjSum t).card = s.card + t.card := by
-  rw [disj_sum, card_add, card_map, card_map]
+theorem card_disj_sum : Multiset.card (s.disjSum t) = Multiset.card s + Multiset.card t := by
+  rw [disjSum, card_add, card_map, card_map]
 #align multiset.card_disj_sum Multiset.card_disj_sum
 
 variable {s t} {s₁ s₂ : Multiset α} {t₁ t₂ : Multiset β} {a : α} {b : β} {x : Sum α β}
 
 theorem mem_disj_sum : x ∈ s.disjSum t ↔ (∃ a, a ∈ s ∧ inl a = x) ∨ ∃ b, b ∈ t ∧ inr b = x := by
-  simp_rw [disj_sum, mem_add, mem_map]
+  simp_rw [disjSum, mem_add, mem_map]
 #align multiset.mem_disj_sum Multiset.mem_disj_sum
 
 @[simp]
@@ -77,7 +77,7 @@ theorem disj_sum_mono (hs : s₁ ≤ s₂) (ht : t₁ ≤ t₂) : s₁.disjSum t
 #align multiset.disj_sum_mono Multiset.disj_sum_mono
 
 theorem disj_sum_mono_left (t : Multiset β) : Monotone fun s : Multiset α => s.disjSum t :=
-  fun s₁ s₂ hs => add_le_add_right (map_le_map hs) _
+  fun _ _ hs => add_le_add_right (map_le_map hs) _
 #align multiset.disj_sum_mono_left Multiset.disj_sum_mono_left
 
 theorem disj_sum_mono_right (s : Multiset α) :
@@ -96,7 +96,7 @@ theorem disj_sum_lt_disj_sum_of_le_of_lt (hs : s₁ ≤ s₂) (ht : t₁ < t₂)
 #align multiset.disj_sum_lt_disj_sum_of_le_of_lt Multiset.disj_sum_lt_disj_sum_of_le_of_lt
 
 theorem disj_sum_strict_mono_left (t : Multiset β) : StrictMono fun s : Multiset α => s.disjSum t :=
-  fun s₁ s₂ hs => disj_sum_lt_disj_sum_of_lt_of_le hs le_rfl
+  fun _ _ hs => disj_sum_lt_disj_sum_of_lt_of_le hs le_rfl
 #align multiset.disj_sum_strict_mono_left Multiset.disj_sum_strict_mono_left
 
 theorem disj_sum_strict_mono_right (s : Multiset α) :
@@ -114,4 +114,3 @@ protected theorem Nodup.disj_sum (hs : s.Nodup) (ht : t.Nodup) : (s.disjSum t).N
 #align multiset.nodup.disj_sum Multiset.Nodup.disj_sum
 
 end Multiset
-
