5d4d84df4cc2b0ff962ae71d966d5db261525ceb
qawbecrdtey <qawbecrdtey@naver.com>
2023-01-13 16:16:39 +0000

    Swapped place of two sections CompleteLattice and ConditionallyCompleteLattice.

--- Mathlib/Algebra/Order/Pointwise.lean
+++ Mathlib/Algebra/Order/Pointwise.lean
@@ -31,87 +31,8 @@ open Pointwise
 
 variable {α : Type _}
 
-section ConditionallyCompleteLattice
-
-variable [ConditionallyCompleteLattice α]
-
-section One
-
-variable [One α]
-
--- Porting note: Use something like `@[to_additive (attr := simp)]` instead.
--- However, this makes a simpNF error like:
---  `csupₛ_one` can be proved like `by simp only [csupₛ_zero]`
--- So the `simp` attribute is written below the definition.
-@[to_additive]
-theorem csupₛ_one : supₛ (1 : Set α) = 1 :=
-  csupₛ_singleton _
-#align cSup_zero csupₛ_zero
-#align cSup_one csupₛ_one
-
-@[to_additive]
-theorem cinfₛ_one : infₛ (1 : Set α) = 1 :=
-  cinfₛ_singleton _
-#align cInf_zero cinfₛ_zero
-#align cInf_one cinfₛ_one
-
-end One
-
-section Group
-
-variable [Group α] [CovariantClass α α (· * ·) (· ≤ ·)] [CovariantClass α α (swap (· * ·)) (· ≤ ·)]
-  {s t : Set α}
-
--- Porting note: Fix names such as
--- cSup → csupₛ
--- cInf → cinfₛ
-@[to_additive]
-theorem csupₛ_inv (hs₀ : s.Nonempty) (hs₁ : BddBelow s) : supₛ s⁻¹ = (infₛ s)⁻¹ := by
-  rw [← image_inv]
-  exact ((OrderIso.inv α).map_cinfₛ' hs₀ hs₁).symm
-#align cSup_inv csupₛ_inv
-#align cSup_neg csupₛ_neg
-
-@[to_additive]
-theorem cinfₛ_inv (hs₀ : s.Nonempty) (hs₁ : BddAbove s) : infₛ s⁻¹ = (supₛ s)⁻¹ := by
-  rw [← image_inv]
-  exact ((OrderIso.inv α).map_csupₛ' hs₀ hs₁).symm
-#align cInf_inv cinfₛ_inv
-#align cInf_neg cinfₛ_neg
-
-@[to_additive]
-theorem csupₛ_mul (hs₀ : s.Nonempty) (hs₁ : BddAbove s) (ht₀ : t.Nonempty) (ht₁ : BddAbove t) :
-    supₛ (s * t) = supₛ s * supₛ t :=
-  csupₛ_image2_eq_csupₛ_csupₛ (fun _ => (OrderIso.mulRight _).to_galoisConnection)
-    (fun _ => (OrderIso.mulLeft _).to_galoisConnection) hs₀ hs₁ ht₀ ht₁
-#align cSup_mul csupₛ_mul
-#align cSup_add csupₛ_add
-
-@[to_additive]
-theorem cinfₛ_mul (hs₀ : s.Nonempty) (hs₁ : BddBelow s) (ht₀ : t.Nonempty) (ht₁ : BddBelow t) :
-    infₛ (s * t) = infₛ s * infₛ t :=
-  cinfₛ_image2_eq_cinfₛ_cinfₛ (fun _ => (OrderIso.mulRight _).symm.to_galoisConnection)
-    (fun _ => (OrderIso.mulLeft _).symm.to_galoisConnection) hs₀ hs₁ ht₀ ht₁
-#align cInf_mul cinfₛ_mul
-#align cInf_add cinfₛ_add
-
-@[to_additive]
-theorem csupₛ_div (hs₀ : s.Nonempty) (hs₁ : BddAbove s) (ht₀ : t.Nonempty) (ht₁ : BddBelow t) :
-    supₛ (s / t) = supₛ s / infₛ t := by
-  rw [div_eq_mul_inv, csupₛ_mul hs₀ hs₁ ht₀.inv ht₁.inv, csupₛ_inv ht₀ ht₁, div_eq_mul_inv]
-#align cSup_div csupₛ_div
-#align cSup_sub csupₛ_sub
-
-@[to_additive]
-theorem cinfₛ_div (hs₀ : s.Nonempty) (hs₁ : BddBelow s) (ht₀ : t.Nonempty) (ht₁ : BddAbove t) :
-    infₛ (s / t) = infₛ s / supₛ t := by
-  rw [div_eq_mul_inv, cinfₛ_mul hs₀ hs₁ ht₀.inv ht₁.inv, cinfₛ_inv ht₀ ht₁, div_eq_mul_inv]
-#align cInf_div cinfₛ_div
-#align cInf_sub cinfₛ_sub
-
-end Group
-
-end ConditionallyCompleteLattice
+-- Porting note : Swapped the place of `CompleteLattice` and `ConditionallyCompleteLattice`
+-- due to simpNF problem between `supₛ_xx` `csupₛ_xx`.
 
 section CompleteLattice
 
@@ -120,31 +41,17 @@ variable [CompleteLattice α]
 section One
 
 variable [One α]
-
--- Porting note: Use something like `@[to_additive (attr := simp)]` instead.
--- However, this makes a simpNF error like:
---  `supₛ_one` can be proved like `by simp only [@supₛ_zero]`
--- So the `simp` attribute is written below the definition.
-@[to_additive]
+@[to_additive (attr := simp)]
 theorem supₛ_one : supₛ (1 : Set α) = 1 :=
   supₛ_singleton
 #align Sup_zero supₛ_zero
 #align Sup_one supₛ_one
 
-@[to_additive]
+@[to_additive (attr := simp)]
 theorem infₛ_one : infₛ (1 : Set α) = 1 :=
   infₛ_singleton
 #align Inf_zero infₛ_zero
 #align Inf_one infₛ_one
-
-attribute [simp] supₛ_one
-attribute [simp] infₛ_one
-attribute [simp] supₛ_zero
-attribute [simp] infₛ_zero
-attribute [simp] csupₛ_one
-attribute [simp] cinfₛ_one
-attribute [simp] csupₛ_zero
-attribute [simp] cinfₛ_zero
 
 end One
 
@@ -194,6 +101,81 @@ theorem infₛ_div : infₛ (s / t) = infₛ s / supₛ t := by simp_rw [div_eq_
 end Group
 
 end CompleteLattice
+
+section ConditionallyCompleteLattice
+
+variable [ConditionallyCompleteLattice α]
+
+section One
+
+variable [One α]
+
+@[to_additive (attr := simp)]
+theorem csupₛ_one : supₛ (1 : Set α) = 1 :=
+  csupₛ_singleton _
+#align cSup_zero csupₛ_zero
+#align cSup_one csupₛ_one
+
+@[to_additive (attr := simp)]
+theorem cinfₛ_one : infₛ (1 : Set α) = 1 :=
+  cinfₛ_singleton _
+#align cInf_zero cinfₛ_zero
+#align cInf_one cinfₛ_one
+
+end One
+
+section Group
+
+variable [Group α] [CovariantClass α α (· * ·) (· ≤ ·)] [CovariantClass α α (swap (· * ·)) (· ≤ ·)]
+  {s t : Set α}
+
+@[to_additive]
+theorem csupₛ_inv (hs₀ : s.Nonempty) (hs₁ : BddBelow s) : supₛ s⁻¹ = (infₛ s)⁻¹ := by
+  rw [← image_inv]
+  exact ((OrderIso.inv α).map_cinfₛ' hs₀ hs₁).symm
+#align cSup_inv csupₛ_inv
+#align cSup_neg csupₛ_neg
+
+@[to_additive]
+theorem cinfₛ_inv (hs₀ : s.Nonempty) (hs₁ : BddAbove s) : infₛ s⁻¹ = (supₛ s)⁻¹ := by
+  rw [← image_inv]
+  exact ((OrderIso.inv α).map_csupₛ' hs₀ hs₁).symm
+#align cInf_inv cinfₛ_inv
+#align cInf_neg cinfₛ_neg
+
+@[to_additive]
+theorem csupₛ_mul (hs₀ : s.Nonempty) (hs₁ : BddAbove s) (ht₀ : t.Nonempty) (ht₁ : BddAbove t) :
+    supₛ (s * t) = supₛ s * supₛ t :=
+  csupₛ_image2_eq_csupₛ_csupₛ (fun _ => (OrderIso.mulRight _).to_galoisConnection)
+    (fun _ => (OrderIso.mulLeft _).to_galoisConnection) hs₀ hs₁ ht₀ ht₁
+#align cSup_mul csupₛ_mul
+#align cSup_add csupₛ_add
+
+@[to_additive]
+theorem cinfₛ_mul (hs₀ : s.Nonempty) (hs₁ : BddBelow s) (ht₀ : t.Nonempty) (ht₁ : BddBelow t) :
+    infₛ (s * t) = infₛ s * infₛ t :=
+  cinfₛ_image2_eq_cinfₛ_cinfₛ (fun _ => (OrderIso.mulRight _).symm.to_galoisConnection)
+    (fun _ => (OrderIso.mulLeft _).symm.to_galoisConnection) hs₀ hs₁ ht₀ ht₁
+#align cInf_mul cinfₛ_mul
+#align cInf_add cinfₛ_add
+
+@[to_additive]
+theorem csupₛ_div (hs₀ : s.Nonempty) (hs₁ : BddAbove s) (ht₀ : t.Nonempty) (ht₁ : BddBelow t) :
+    supₛ (s / t) = supₛ s / infₛ t := by
+  rw [div_eq_mul_inv, csupₛ_mul hs₀ hs₁ ht₀.inv ht₁.inv, csupₛ_inv ht₀ ht₁, div_eq_mul_inv]
+#align cSup_div csupₛ_div
+#align cSup_sub csupₛ_sub
+
+@[to_additive]
+theorem cinfₛ_div (hs₀ : s.Nonempty) (hs₁ : BddBelow s) (ht₀ : t.Nonempty) (ht₁ : BddAbove t) :
+    infₛ (s / t) = infₛ s / supₛ t := by
+  rw [div_eq_mul_inv, cinfₛ_mul hs₀ hs₁ ht₀.inv ht₁.inv, cinfₛ_inv ht₀ ht₁, div_eq_mul_inv]
+#align cInf_div cinfₛ_div
+#align cInf_sub cinfₛ_sub
+
+end Group
+
+end ConditionallyCompleteLattice
 
 namespace LinearOrderedField
 
@@ -312,3 +294,4 @@ theorem smul_Iic : r • Iic a = Iic (r • a) := by
 #align linear_ordered_field.smul_Iic LinearOrderedField.smul_Iic
 
 end LinearOrderedField
+#lint
