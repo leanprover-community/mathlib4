a19f51dac5dfa88cdae64c9f13d80ed5bea8feb7
ChrisHughes24 <chrishughes24@gmail.com>
2023-01-13 11:11:03 +0000

    start fixing

--- Mathlib/Data/ENat/Basic.lean
+++ Mathlib/Data/ENat/Basic.lean
@@ -16,28 +16,35 @@ import Mathlib.Algebra.Order.Ring.WithTop
 /-!
 # Definition and basic properties of extended natural numbers
 
-In this file we define `enat` (notation: `ℕ∞`) to be `with_top ℕ` and prove some basic lemmas
+In this file we define `ENat` (notation: `ℕ∞`) to be `WithTop ℕ` and prove some basic lemmas
 about this type.
 -/
 
-
-/- ./././Mathport/Syntax/Translate/Command.lean:42:9: unsupported derive handler has_coe_t[has_coe_t] exprℕ() -/
-/-- Extended natural numbers `ℕ∞ = with_top ℕ`. -/
-def Enat : Type :=
+/-- Extended natural numbers `ℕ∞ = WithTop ℕ`. -/
+def ENat : Type :=
   WithTop ℕ deriving Zero, AddCommMonoidWithOne, CanonicallyOrderedCommSemiring, Nontrivial,
-  LinearOrder, OrderBot, OrderTop, Bot, Top, CanonicallyLinearOrderedAddMonoid, Sub, OrderedSub,
-  LinearOrderedAddCommMonoidWithTop, SuccOrder, WellFoundedLt, WellFoundedRelation, CharZero,
-  «./././Mathport/Syntax/Translate/Command.lean:42:9: unsupported derive handler has_coe_t[has_coe_t] exprℕ()»
-#align enat Enat
+  LinearOrder, Bot, Top,  CanonicallyLinearOrderedAddMonoid, Sub,
+  LinearOrderedAddCommMonoidWithTop,  WellFoundedRelation, Inhabited
+  -- OrderBot, OrderTop, OrderedSub,  SuccOrder --, WellFoundedLt, CharZero
+#align enat ENat
 
--- mathport name: «exprℕ∞»
-notation "ℕ∞" => Enat
 
-namespace Enat
+/-- Extended natural numbers `ℕ∞ = WithTop ℕ`. -/
+notation "ℕ∞" => ENat
 
-instance : Inhabited ℕ∞ :=
-  ⟨0⟩
+namespace ENat
 
+@[coe] def ofNat (n : ℕ) : ℕ∞ := WithTop.some n
+
+instance : Coe ℕ ℕ∞ := ⟨ofNat⟩
+
+--Porting note: instances that derive failed to find
+instance : OrderBot ℕ∞ := WithTop.orderBot
+instance : OrderTop ℕ∞ := WithTop.orderTop
+instance : OrderedSub ℕ∞ := by delta ENat; infer_instance
+instance : SuccOrder ℕ∞ := by delta ENat; infer_instance
+instance : WellFoundedLT ℕ∞ := by delta ENat; infer_instance
+instance : CharZero ℕ∞ := by delta ENat; infer_instance
 instance : IsWellOrder ℕ∞ (· < ·) where
 
 variable {m n : ℕ∞}
@@ -45,31 +52,31 @@ variable {m n : ℕ∞}
 @[simp, norm_cast]
 theorem coe_zero : ((0 : ℕ) : ℕ∞) = 0 :=
   rfl
-#align enat.coe_zero Enat.coe_zero
+#align enat.coe_zero ENat.coe_zero
 
 @[simp, norm_cast]
 theorem coe_one : ((1 : ℕ) : ℕ∞) = 1 :=
   rfl
-#align enat.coe_one Enat.coe_one
+#align enat.coe_one ENat.coe_one
 
 @[simp, norm_cast]
 theorem coe_add (m n : ℕ) : ↑(m + n) = (m + n : ℕ∞) :=
   rfl
-#align enat.coe_add Enat.coe_add
+#align enat.coe_add ENat.coe_add
 
 @[simp, norm_cast]
 theorem coe_sub (m n : ℕ) : ↑(m - n) = (m - n : ℕ∞) :=
   rfl
-#align enat.coe_sub Enat.coe_sub
+#align enat.coe_sub ENat.coe_sub
 
 @[simp, norm_cast]
 theorem coe_mul (m n : ℕ) : ↑(m * n) = (m * n : ℕ∞) :=
   WithTop.coe_mul
-#align enat.coe_mul Enat.coe_mul
+#align enat.coe_mul ENat.coe_mul
 
-instance canLift : CanLift ℕ∞ ℕ coe fun n => n ≠ ⊤ :=
+instance canLift : CanLift ℕ∞ ℕ (↑) fun n => n ≠ ⊤ :=
   WithTop.canLift
-#align enat.can_lift Enat.canLift
+#align enat.can_lift ENat.canLift
 
 /-- Conversion of `ℕ∞` to `ℕ` sending `∞` to `0`. -/
 def toNat : MonoidWithZeroHom ℕ∞ ℕ
@@ -78,36 +85,36 @@ def toNat : MonoidWithZeroHom ℕ∞ ℕ
   map_one' := rfl
   map_zero' := rfl
   map_mul' := WithTop.untop'_zero_mul
-#align enat.to_nat Enat.toNat
+#align enat.to_nat ENat.toNat
 
 @[simp]
-theorem to_nat_coe (n : ℕ) : toNat n = n :=
+theorem toNat_coe (n : ℕ) : toNat n = n :=
   rfl
-#align enat.to_nat_coe Enat.to_nat_coe
+#align enat.to_nat_coe ENat.toNat_coe
 
 @[simp]
-theorem to_nat_top : toNat ⊤ = 0 :=
+theorem toNat_top : toNat ⊤ = 0 :=
   rfl
-#align enat.to_nat_top Enat.to_nat_top
+#align enat.to_nat_top ENat.toNat_top
 
 @[simp]
-theorem coe_to_nat_eq_self : ↑n.toNat = n ↔ n ≠ ⊤ :=
-  WithTop.recTopCoe (by simp) (by simp) n
-#align enat.coe_to_nat_eq_self Enat.coe_to_nat_eq_self
+theorem coe_toNat_eq_self : ENat.toNat (n : ENat) = n ↔ n ≠ ⊤ :=
+  WithTop.recTopCoe (by simp) (fun _ => by rw [toNat_coe]) n
+#align enat.coe_to_nat_eq_self ENat.coe_toNat_eq_self
 
 alias coe_to_nat_eq_self ↔ _ coe_to_nat
-#align enat.coe_to_nat Enat.coe_to_nat
+#align enat.coe_to_nat ENat.coe_to_nat
 
 theorem coe_to_nat_le_self (n : ℕ∞) : ↑(toNat n) ≤ n :=
   WithTop.recTopCoe le_top (fun k => le_rfl) n
-#align enat.coe_to_nat_le_self Enat.coe_to_nat_le_self
+#align enat.coe_to_nat_le_self ENat.coe_to_nat_le_self
 
 theorem to_nat_add {m n : ℕ∞} (hm : m ≠ ⊤) (hn : n ≠ ⊤) : toNat (m + n) = toNat m + toNat n :=
   by
   lift m to ℕ using hm
   lift n to ℕ using hn
   rfl
-#align enat.to_nat_add Enat.to_nat_add
+#align enat.to_nat_add ENat.to_nat_add
 
 theorem to_nat_sub {n : ℕ∞} (hn : n ≠ ⊤) (m : ℕ∞) : toNat (m - n) = toNat m - toNat n :=
   by
@@ -115,35 +122,35 @@ theorem to_nat_sub {n : ℕ∞} (hn : n ≠ ⊤) (m : ℕ∞) : toNat (m - n) = 
   induction m using WithTop.recTopCoe
   · rw [WithTop.top_sub_coe, to_nat_top, zero_tsub]
   · rw [← coe_sub, to_nat_coe, to_nat_coe, to_nat_coe]
-#align enat.to_nat_sub Enat.to_nat_sub
+#align enat.to_nat_sub ENat.to_nat_sub
 
 theorem to_nat_eq_iff {m : ℕ∞} {n : ℕ} (hn : n ≠ 0) : m.toNat = n ↔ m = n := by
   induction m using WithTop.recTopCoe <;> simp [hn.symm]
-#align enat.to_nat_eq_iff Enat.to_nat_eq_iff
+#align enat.to_nat_eq_iff ENat.to_nat_eq_iff
 
 @[simp]
 theorem succ_def (m : ℕ∞) : Order.succ m = m + 1 := by cases m <;> rfl
-#align enat.succ_def Enat.succ_def
+#align enat.succ_def ENat.succ_def
 
 theorem add_one_le_of_lt (h : m < n) : m + 1 ≤ n :=
   m.succ_def ▸ Order.succ_le_of_lt h
-#align enat.add_one_le_of_lt Enat.add_one_le_of_lt
+#align enat.add_one_le_of_lt ENat.add_one_le_of_lt
 
 theorem add_one_le_iff (hm : m ≠ ⊤) : m + 1 ≤ n ↔ m < n :=
   m.succ_def ▸ (Order.succ_le_iff_of_not_isMax <| by rwa [isMax_iff_eq_top])
-#align enat.add_one_le_iff Enat.add_one_le_iff
+#align enat.add_one_le_iff ENat.add_one_le_iff
 
 theorem one_le_iff_pos : 1 ≤ n ↔ 0 < n :=
   add_one_le_iff WithTop.zero_ne_top
-#align enat.one_le_iff_pos Enat.one_le_iff_pos
+#align enat.one_le_iff_pos ENat.one_le_iff_pos
 
 theorem one_le_iff_ne_zero : 1 ≤ n ↔ n ≠ 0 :=
   one_le_iff_pos.trans pos_iff_ne_zero
-#align enat.one_le_iff_ne_zero Enat.one_le_iff_ne_zero
+#align enat.one_le_iff_ne_zero ENat.one_le_iff_ne_zero
 
 theorem le_of_lt_add_one (h : m < n + 1) : m ≤ n :=
   Order.le_of_lt_succ <| n.succ_def.symm ▸ h
-#align enat.le_of_lt_add_one Enat.le_of_lt_add_one
+#align enat.le_of_lt_add_one ENat.le_of_lt_add_one
 
 @[elab_as_elim]
 theorem nat_induction {P : ℕ∞ → Prop} (a : ℕ∞) (h0 : P 0) (hsuc : ∀ n : ℕ, P n → P n.succ)
@@ -152,7 +159,6 @@ theorem nat_induction {P : ℕ∞ → Prop} (a : ℕ∞) (h0 : P 0) (hsuc : ∀ 
   have A : ∀ n : ℕ, P n := fun n => Nat.recOn n h0 hsuc
   cases a
   exacts[htop A, A a]
-#align enat.nat_induction Enat.nat_induction
+#align enat.nat_induction ENat.nat_induction
 
-end Enat
-
+end ENat
