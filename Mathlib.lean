ae3c1ad1fd17bfdb6a91e9cd6b5ae513358dd594
casavaca <96765450+casavaca@users.noreply.github.com>
2023-01-13 09:36:07 +0000

    feat: Port/data.nat.succ_pred (#1480)

--- Mathlib.lean
+++ Mathlib.lean
@@ -310,6 +310,7 @@ import Mathlib.Data.Nat.Prime
 import Mathlib.Data.Nat.Set
 import Mathlib.Data.Nat.Size
 import Mathlib.Data.Nat.Sqrt
+import Mathlib.Data.Nat.SuccPred
 import Mathlib.Data.Nat.Units
 import Mathlib.Data.Nat.Upto
 import Mathlib.Data.Nat.WithBot
