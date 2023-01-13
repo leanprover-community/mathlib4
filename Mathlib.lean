7df37a9afeb7a18ae010118146d83f8e1594c3f8
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2023-01-13 14:55:14 +0000

    feat: port Data.Multiset.Dedup (#1541)
    Co-authored-by: ChrisHughes24 <chrishughes24@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -288,6 +288,7 @@ import Mathlib.Data.List.TFAE
 import Mathlib.Data.List.Zip
 import Mathlib.Data.Multiset.Basic
 import Mathlib.Data.Multiset.Bind
+import Mathlib.Data.Multiset.Dedup
 import Mathlib.Data.Multiset.Nodup
 import Mathlib.Data.Multiset.Range
 import Mathlib.Data.Nat.Basic
