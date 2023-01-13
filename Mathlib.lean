6d86a6ac7aa6eae79181b6bae1fca53be16d2379
maxwell-thum <maxwell.thum2718@gmail.com>
2023-01-13 09:15:55 +0000

    feat: port Data.Set.Pointwise.SMul (#1473)
    Co-authored-by: ChrisHughes24 <chrishughes24@gmail.com>
    Co-authored-by: Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -368,6 +368,7 @@ import Mathlib.Data.Set.NAry
 import Mathlib.Data.Set.Opposite
 import Mathlib.Data.Set.Pairwise
 import Mathlib.Data.Set.Pointwise.Basic
+import Mathlib.Data.Set.Pointwise.SMul
 import Mathlib.Data.Set.Prod
 import Mathlib.Data.Set.Sigma
 import Mathlib.Data.Set.UnionLift
