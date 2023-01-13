185c678a62d2720c02404865c296536e328b97c0
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2023-01-13 14:07:50 +0100

    Fix and tidy

--- Mathlib/Algebra/BigOperators/Multiset/Lemmas.lean
+++ Mathlib/Algebra/BigOperators/Multiset/Lemmas.lean
@@ -11,21 +11,21 @@ Authors: Chris Hughes, Bhavik Mehta, Eric Wieser
 import Mathlib.Data.List.BigOperators.Lemmas
 import Mathlib.Algebra.BigOperators.Multiset.Basic
 
-/-! # Lemmas about `multiset.sum` and `multiset.prod` requiring extra algebra imports -/
+/-! # Lemmas about `Multiset.sum` and `Multiset.prod` requiring extra algebra imports -/
 
 
-variable {ι α β γ : Type _}
+variable {α : Type _}
 
 namespace Multiset
 
-theorem dvd_prod [CommMonoid α] {s : Multiset α} {a : α} : a ∈ s → a ∣ s.Prod :=
-  Quotient.induction_on s (fun l a h => by simpa using List.dvd_prod h) a
+theorem dvd_prod [CommMonoid α] {s : Multiset α} {a : α} : a ∈ s → a ∣ s.prod :=
+  Quotient.inductionOn s (fun l a h => by simpa using List.dvd_prod h) a
 #align multiset.dvd_prod Multiset.dvd_prod
 
 @[to_additive]
 theorem prod_eq_one_iff [CanonicallyOrderedMonoid α] {m : Multiset α} :
-    m.Prod = 1 ↔ ∀ x ∈ m, x = (1 : α) :=
-  (Quotient.induction_on m) fun l => by simpa using List.prod_eq_one_iff l
+    m.prod = 1 ↔ ∀ x ∈ m, x = (1 : α) :=
+  Quotient.inductionOn m fun l => by simpa using List.prod_eq_one_iff l
 #align multiset.prod_eq_one_iff Multiset.prod_eq_one_iff
 
 end Multiset
@@ -34,18 +34,16 @@ open Multiset
 
 namespace Commute
 
-variable [NonUnitalNonAssocSemiring α] {a : α} {s : Multiset ι} {f : ι → α}
+variable [NonUnitalNonAssocSemiring α] (s : Multiset α)
 
-theorem multiset_sum_right (s : Multiset α) (a : α) (h : ∀ b ∈ s, Commute a b) : Commute a s.Sum :=
-  by
-  induction s using Quotient.induction_on
+theorem multiset_sum_right (a : α) (h : ∀ b ∈ s, Commute a b) : Commute a s.sum := by
+  induction s using Quotient.inductionOn
   rw [quot_mk_to_coe, coe_sum]
   exact Commute.list_sum_right _ _ h
 #align commute.multiset_sum_right Commute.multiset_sum_right
 
-theorem multiset_sum_left (s : Multiset α) (b : α) (h : ∀ a ∈ s, Commute a b) : Commute s.Sum b :=
-  ((Commute.multiset_sum_right _ _) fun a ha => (h _ ha).symm).symm
+theorem multiset_sum_left (b : α) (h : ∀ a ∈ s, Commute a b) : Commute s.sum b :=
+  ((Commute.multiset_sum_right _ _) fun _ ha => (h _ ha).symm).symm
 #align commute.multiset_sum_left Commute.multiset_sum_left
 
 end Commute
-
