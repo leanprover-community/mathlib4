b3b914b373e6433ff16b87f6bf117ba84beb96bf
maxwell-thum <119913396+maxwell-thum@users.noreply.github.com>
2023-01-13 21:24:25 -0700

    Update Mathlib/Data/Multiset/Antidiagonal.lean
    Co-authored-by: Johan Commelin <johan@commelin.net>

--- Mathlib/Data/Multiset/Antidiagonal.lean
+++ Mathlib/Data/Multiset/Antidiagonal.lean
@@ -103,7 +103,6 @@ theorem antidiagonal_eq_map_powerset [DecidableEq α] (s : Multiset α) :
     rw [cons_sub_of_le _ (mem_powerset.mp hx)]
 #align multiset.antidiagonal_eq_map_powerset Multiset.antidiagonal_eq_map_powerset
 
--- Porting note: Unnecessary `have` commented out to satisfy linter
 /- Porting note: The original file used unported tactic `cc` (congruence closure) in the last line,
 but it turns out that `add_comm` was all that was needed. -/
 @[simp]
