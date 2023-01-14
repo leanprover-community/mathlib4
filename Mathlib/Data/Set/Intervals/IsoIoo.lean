8ea0bd3890b9ffed327c659f4572f3c53a687073
Maxwell Thum <maxwell.thum2718@gmail.com>
2023-01-14 02:49:06 -0700

    Synthesizing instance `Abs ↑(Ioo (-1) 1)` might be a pain

--- Mathlib/Data/Set/Intervals/IsoIoo.lean
+++ Mathlib/Data/Set/Intervals/IsoIoo.lean
@@ -9,38 +9,41 @@ Authors: Yury Kudryashov
 ! if you have ported upstream changes.
 -/
 import Mathlib.Order.Monotone.Odd
-import Mathlib.Tactic.FieldSimp
+--import Mathlib.Tactic.FieldSimp
+import Mathlib.Algebra.Order.Field.Basic
+import Mathlib.Algebra.Abs
 
 /-!
 # Order isomorphism between a linear ordered field and `(-1, 1)`
 
-In this file we provide an order isomorphism `order_iso_Ioo_neg_one_one` between the open interval
+In this file we provide an order isomorphism `orderIsoIooNegOneOne` between the open interval
 `(-1, 1)` in a linear ordered field and the whole field.
 -/
 
+set_option autoImplicit false -- **TODO** delete this later
 
 open Set
 
 /-- In a linear ordered field, the whole field is order isomorphic to the open interval `(-1, 1)`.
 We consider the actual implementation to be a "black box", so it is irreducible.
 -/
-irreducible_def orderIsoIooNegOneOne (k : Type _) [LinearOrderedField k] : k ≃o Ioo (-1 : k) 1 :=
+@[irreducible]
+def orderIsoIooNegOneOne (k : Type _) [LinearOrderedField k] : k ≃o Ioo (-1 : k) 1 :=
   by
-  refine' StrictMono.orderIsoOfRightInverse _ _ (fun x => x / (1 - |x|)) _
-  · refine' cod_restrict (fun x => x / (1 + |x|)) _ fun x => abs_lt.1 _
+  refine' StrictMono.orderIsoOfRightInverse _ _ (fun x ↦ x / (1 - @abs _ _ x)) _
+  · refine' codRestrict (fun x ↦ x / (1 + |x|)) _ fun x ↦ abs_lt.1 _
     have H : 0 < 1 + |x| := (abs_nonneg x).trans_lt (lt_one_add _)
     calc
       |x / (1 + |x|)| = |x| / (1 + |x|) := by rw [abs_div, abs_of_pos H]
       _ < 1 := (div_lt_one H).2 (lt_one_add _)
-      
   · refine' (strictMono_of_odd_strictMono_on_nonneg _ _).codRestrict _
     · intro x
       simp only [abs_neg, neg_div]
     · rintro x (hx : 0 ≤ x) y (hy : 0 ≤ y) hxy
       simp [abs_of_nonneg, mul_add, mul_comm x y, div_lt_div_iff, hx.trans_lt (lt_one_add _),
         hy.trans_lt (lt_one_add _), *]
-  · refine' fun x => Subtype.ext _
+  · refine' fun x ↦ Subtype.ext _
     have : 0 < 1 - |(x : k)| := sub_pos.2 (abs_lt.2 x.2)
-    field_simp [abs_div, this.ne', abs_of_pos this]
+    simp [abs_div, this.ne', abs_of_pos this]
+    --field_simp [abs_div, this.ne', abs_of_pos this]
 #align order_iso_Ioo_neg_one_one orderIsoIooNegOneOne
-
