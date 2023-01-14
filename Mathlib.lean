f7d4c2c523a7dab79a3c63b78d13e8484c52c99f
qawbecrdtey <qawbecrdtey@kaist.ac.kr>
2023-01-14 05:54:48 +0000

    feat: port Data.Multiset.Sections (#1554)
    Co-authored-by: qawbecrdtey <40463813+qawbecrdtey@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -300,6 +300,7 @@ import Mathlib.Data.Multiset.Nodup
 import Mathlib.Data.Multiset.Pi
 import Mathlib.Data.Multiset.Powerset
 import Mathlib.Data.Multiset.Range
+import Mathlib.Data.Multiset.Sections
 import Mathlib.Data.Multiset.Sum
 import Mathlib.Data.Nat.Basic
 import Mathlib.Data.Nat.Bits
