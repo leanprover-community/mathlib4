07b3abc0dadaf6b31a2644f29c1fda7c6fe77a32
Henrik Böving <hargonix@gmail.com>
2023-01-16 10:37:56 +0000

    feat: port Algebra.Hom.GroupAction (#1424)
    Co-authored-by: ChrisHughes24 <chrishughes24@gmail.com>
    Co-authored-by: Lukas Miaskiwskyi <lukas.mias@gmail.com>
    Co-authored-by: Chris Hughes <33847686+ChrisHughes24@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -60,6 +60,7 @@ import Mathlib.Algebra.Hom.Equiv.TypeTags
 import Mathlib.Algebra.Hom.Equiv.Units.Basic
 import Mathlib.Algebra.Hom.Equiv.Units.GroupWithZero
 import Mathlib.Algebra.Hom.Group
+import Mathlib.Algebra.Hom.GroupAction
 import Mathlib.Algebra.Hom.GroupInstances
 import Mathlib.Algebra.Hom.Iterate
 import Mathlib.Algebra.Hom.Ring
