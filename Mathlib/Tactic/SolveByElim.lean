1f70f002d11a739d12531a7698b36aa89354bbc8
Scott Morrison <scott.morrison@gmail.com>
2023-01-16 15:51:19 +1100

    fix comment

--- Mathlib/Tactic/SolveByElim.lean
+++ Mathlib/Tactic/SolveByElim.lean
@@ -46,7 +46,7 @@ def applyFirst (cfg : ApplyConfig := {}) (trace : Name := .anonymous) (lemmas : 
       -- deals with closing new typeclass goals by calling
       -- `Lean.Elab.Term.synthesizeSyntheticMVarsNoPostponing`.
       -- It seems we can't reuse that machinery down here in `MetaM`,
-      -- so we just settle for trying to close each subgoal using `synthInstance`.
+      -- so we just settle for trying to close each subgoal using `inferInstance`.
       cont (← goals.filterM fun g => try g.inferInstance; pure false catch _ => pure true)
 
 end Lean.MVarId
