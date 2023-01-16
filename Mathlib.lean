452b52ee5c1d54df83846a0bdfe70fa1e5aa25b5
maxwell-thum <maxwell.thum2718@gmail.com>
2023-01-16 03:28:57 +0000

    feat: port Data.Set.Intervals.IsoIoo (#1321)
    Original imports `Mathlib.Tactic.FieldSimp` which is not yet ported. I'm going to try to do this without the tactic.
    
    Co-authored-by: maxwell-thum <119913396+maxwell-thum@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -375,6 +375,7 @@ import Mathlib.Data.Set.Image
 import Mathlib.Data.Set.Intervals.Basic
 import Mathlib.Data.Set.Intervals.Disjoint
 import Mathlib.Data.Set.Intervals.Group
+import Mathlib.Data.Set.Intervals.IsoIoo
 import Mathlib.Data.Set.Intervals.Monoid
 import Mathlib.Data.Set.Intervals.Monotone
 import Mathlib.Data.Set.Intervals.OrdConnected
