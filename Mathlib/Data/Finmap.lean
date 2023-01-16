d3c549da16a0f05c14b68df5015ca21ee6ed667b
Arien Malec <arien.malec@gmail.com>
2023-01-16 11:18:34 -0800

    fix mem_singleton

--- Mathlib/Data/Finmap.lean
+++ Mathlib/Data/Finmap.lean
@@ -248,7 +248,7 @@ theorem keys_singleton (a : α) (b : β a) : (singleton a b).keys = {a} :=
 
 @[simp]
 theorem mem_singleton (x y : α) (b : β y) : x ∈ singleton y b ↔ x = y := by
-  simp only [singleton] <;> erw [mem_cons_eq, mem_nil_iff, or_false_iff]
+  simp only [singleton] ; erw [mem_cons, mem_nil_iff, or_false_iff]
 #align finmap.mem_singleton Finmap.mem_singleton
 
 section
