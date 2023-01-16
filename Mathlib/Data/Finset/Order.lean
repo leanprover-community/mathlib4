ac9b77e7409f19ee7e1030ba22ef967a1ca31d69
Johan Commelin <johan@commelin.net>
2023-01-16 08:25:09 +0000

    Mathbin -> Mathlib; add import to Mathlib.lean

--- Mathlib/Data/Finset/Order.lean
+++ Mathlib/Data/Finset/Order.lean
@@ -8,7 +8,7 @@ Authors: Mario Carneiro, Kenny Lau
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
-import Mathbin.Data.Finset.Basic
+import Mathlib.Data.Finset.Basic
 
 /-!
 # Finsets of ordered types
