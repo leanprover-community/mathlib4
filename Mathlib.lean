98d34d74c28d47b13a2286e83934599c15fe8eb5
maxwell-thum <maxwell.thum2718@gmail.com>
2023-01-17 08:44:53 +0000

    feat: port Algebra.GCDMonoid.Finset (#1612)
    Co-authored-by: maxwell-thum <119913396+maxwell-thum@users.noreply.github.com>
    Co-authored-by: ChrisHughes24 <chrishughes24@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -18,6 +18,7 @@ import Mathlib.Algebra.Field.Power
 import Mathlib.Algebra.FreeMonoid.Basic
 import Mathlib.Algebra.FreeMonoid.Count
 import Mathlib.Algebra.GCDMonoid.Basic
+import Mathlib.Algebra.GCDMonoid.Finset
 import Mathlib.Algebra.GCDMonoid.Multiset
 import Mathlib.Algebra.Group.Basic
 import Mathlib.Algebra.Group.Commutator
