9903a3f7cae40f75080bc1ac72dfa8de93d285e8
maxwell-thum <119913396+maxwell-thum@users.noreply.github.com>
2023-01-16 21:37:15 -0800

    Update Mathlib/Algebra/GCDMonoid/Finset.lean
    Co-authored-by: Johan Commelin <johan@commelin.net>

--- Mathlib/Algebra/GCDMonoid/Finset.lean
+++ Mathlib/Algebra/GCDMonoid/Finset.lean
@@ -79,11 +79,9 @@ theorem dvd_lcm {b : β} (hb : b ∈ s) : f b ∣ s.lcm f :=
 
 @[simp]
 theorem lcm_insert [DecidableEq β] {b : β} :
-    (insert b s : Finset β).lcm f = GCDMonoid.lcm (f b) (s.lcm f) :=
-  by
+    (insert b s : Finset β).lcm f = GCDMonoid.lcm (f b) (s.lcm f) := by
   by_cases h : b ∈ s
-  ·
-    rw [insert_eq_of_mem h,
+  · rw [insert_eq_of_mem h,
       (lcm_eq_right_iff (f b) (s.lcm f) (Multiset.normalize_lcm (s.1.map f))).2 (dvd_lcm h)]
   apply fold_insert h
 #align finset.lcm_insert Finset.lcm_insert
