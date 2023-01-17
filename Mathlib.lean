76df73b58fba3b8b87626d405e331d4902a1da9a
Xavier-François Roblot <46200072+xroblot@users.noreply.github.com>
2023-01-17 11:37:12 +0000

    feat port : Data.Finset.NatAntidiagonal (#1598)
    Co-authored-by: ChrisHughes24 <chrishughes24@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -234,6 +234,7 @@ import Mathlib.Data.Finset.Fold
 import Mathlib.Data.Finset.Image
 import Mathlib.Data.Finset.Lattice
 import Mathlib.Data.Finset.NAry
+import Mathlib.Data.Finset.NatAntidiagonal
 import Mathlib.Data.Finset.Option
 import Mathlib.Data.Finset.Order
 import Mathlib.Data.Finset.Pi
