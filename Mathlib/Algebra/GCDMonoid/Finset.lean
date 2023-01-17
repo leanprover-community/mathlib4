bc1fbb6d9c42edafb8d27bc071a21f281436854d
maxwell-thum <119913396+maxwell-thum@users.noreply.github.com>
2023-01-16 21:37:21 -0800

    Update Mathlib/Algebra/GCDMonoid/Finset.lean
    Co-authored-by: Johan Commelin <johan@commelin.net>

--- Mathlib/Algebra/GCDMonoid/Finset.lean
+++ Mathlib/Algebra/GCDMonoid/Finset.lean
@@ -62,8 +62,7 @@ theorem lcm_empty : (∅ : Finset β).lcm f = 1 :=
 #align finset.lcm_empty Finset.lcm_empty
 
 @[simp]
-theorem lcm_dvd_iff {a : α} : s.lcm f ∣ a ↔ ∀ b ∈ s, f b ∣ a :=
-  by
+theorem lcm_dvd_iff {a : α} : s.lcm f ∣ a ↔ ∀ b ∈ s, f b ∣ a := by
   apply Iff.trans Multiset.lcm_dvd
   simp only [Multiset.mem_map, and_imp, exists_imp]
   exact ⟨fun k b hb ↦ k _ _ hb rfl, fun k a' b hb h ↦ h ▸ k _ hb⟩
