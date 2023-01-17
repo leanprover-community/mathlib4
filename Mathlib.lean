afa19001377971915954fe244828feb3d4eacc13
Chris Hughes <chrishughes24@gmail.com>
2023-01-17 09:34:20 +0000

    feat: port Order.Hom.Bounded (#888)
    mathlib3 SHA : f1a2caaf51ef593799107fe9a8d5e411599f3996
    
    Co-authored-by: Kevin Buzzard <k.buzzard@imperial.ac.uk>
    Co-authored-by: Scott Morrison <scott.morrison@gmail.com>
    Co-authored-by: Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
    Co-authored-by: Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
    Co-authored-by: Vierkantor <vierkantor@vierkantor.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -566,6 +566,7 @@ import Mathlib.Order.Heyting.Basic
 import Mathlib.Order.Heyting.Boundary
 import Mathlib.Order.Heyting.Regular
 import Mathlib.Order.Hom.Basic
+import Mathlib.Order.Hom.Bounded
 import Mathlib.Order.Hom.Order
 import Mathlib.Order.Hom.Set
 import Mathlib.Order.InitialSeg
