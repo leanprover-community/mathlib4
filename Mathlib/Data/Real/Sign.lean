fb532ccc9f0c4ba7a6dda6c7a0ad008faf196041
qawbecrdtey <qawbecrdtey@kaist.ac.kr>
2023-01-17 18:46:54 +0900

    Initial commit

--- Mathlib/Data/Real/Sign.lean
+++ Mathlib/Data/Real/Sign.lean
@@ -13,12 +13,12 @@ import Mathlib.Data.Real.Basic
 /-!
 # Real sign function
 
-This file introduces and contains some results about `real.sign` which maps negative
+This file introduces and contains some results about `Real.sign` which maps negative
 real numbers to -1, positive real numbers to 1, and 0 to 0.
 
 ## Main definitions
 
- * `real.sign r` is $\begin{cases} -1 & \text{if } r < 0, \\
+ * `Real.sign r` is $\begin{cases} -1 & \text{if } r < 0, \\
                                ~~\, 0 & \text{if } r = 0, \\
                                ~~\, 1 & \text{if } r > 0. \end{cases}$
 
@@ -51,8 +51,7 @@ theorem sign_one : sign 1 = 1 :=
   sign_of_pos <| by norm_num
 #align real.sign_one Real.sign_one
 
-theorem sign_apply_eq (r : ℝ) : sign r = -1 ∨ sign r = 0 ∨ sign r = 1 :=
-  by
+theorem sign_apply_eq (r : ℝ) : sign r = -1 ∨ sign r = 0 ∨ sign r = 1 := by
   obtain hn | rfl | hp := lt_trichotomy r (0 : ℝ)
   · exact Or.inl <| sign_of_neg hn
   · exact Or.inr <| Or.inl <| sign_zero
@@ -60,8 +59,7 @@ theorem sign_apply_eq (r : ℝ) : sign r = -1 ∨ sign r = 0 ∨ sign r = 1 :=
 #align real.sign_apply_eq Real.sign_apply_eq
 
 /-- This lemma is useful for working with `ℝˣ` -/
-theorem sign_apply_eq_of_ne_zero (r : ℝ) (h : r ≠ 0) : sign r = -1 ∨ sign r = 1 :=
-  by
+theorem sign_apply_eq_of_ne_zero (r : ℝ) (h : r ≠ 0) : sign r = -1 ∨ sign r = 1 := by
   obtain hn | rfl | hp := lt_trichotomy r (0 : ℝ)
   · exact Or.inl <| sign_of_neg hn
   · exact (h rfl).elim
@@ -80,26 +78,22 @@ theorem sign_eq_zero_iff {r : ℝ} : sign r = 0 ↔ r = 0 :=
     exact (one_ne_zero h).elim
 #align real.sign_eq_zero_iff Real.sign_eq_zero_iff
 
-theorem sign_int_cast (z : ℤ) : sign (z : ℝ) = ↑(Int.sign z) :=
-  by
+theorem sign_int_cast (z : ℤ) : sign (z : ℝ) = ↑(Int.sign z) := by
   obtain hn | rfl | hp := lt_trichotomy z (0 : ℤ)
-  ·
-    rw [sign_of_neg (int.cast_lt_zero.mpr hn), Int.sign_eq_neg_one_of_neg hn, Int.cast_neg,
+  · rw [sign_of_neg (int.cast_lt_zero.mpr hn), Int.sign_eq_neg_one_of_neg hn, Int.cast_neg,
       Int.cast_one]
   · rw [Int.cast_zero, sign_zero, Int.sign_zero, Int.cast_zero]
   · rw [sign_of_pos (int.cast_pos.mpr hp), Int.sign_eq_one_of_pos hp, Int.cast_one]
 #align real.sign_int_cast Real.sign_int_cast
 
-theorem sign_neg {r : ℝ} : sign (-r) = -sign r :=
-  by
+theorem sign_neg {r : ℝ} : sign (-r) = -sign r := by
   obtain hn | rfl | hp := lt_trichotomy r (0 : ℝ)
   · rw [sign_of_neg hn, sign_of_pos (neg_pos.mpr hn), neg_neg]
   · rw [sign_zero, neg_zero, sign_zero]
   · rw [sign_of_pos hp, sign_of_neg (neg_lt_zero.mpr hp)]
 #align real.sign_neg Real.sign_neg
 
-theorem sign_mul_nonneg (r : ℝ) : 0 ≤ sign r * r :=
-  by
+theorem sign_mul_nonneg (r : ℝ) : 0 ≤ sign r * r := by
   obtain hn | rfl | hp := lt_trichotomy r (0 : ℝ)
   · rw [sign_of_neg hn]
     exact mul_nonneg_of_nonpos_of_nonpos (by norm_num) hn.le
@@ -108,16 +102,14 @@ theorem sign_mul_nonneg (r : ℝ) : 0 ≤ sign r * r :=
     exact hp.le
 #align real.sign_mul_nonneg Real.sign_mul_nonneg
 
-theorem sign_mul_pos_of_ne_zero (r : ℝ) (hr : r ≠ 0) : 0 < sign r * r :=
-  by
+theorem sign_mul_pos_of_ne_zero (r : ℝ) (hr : r ≠ 0) : 0 < sign r * r := by
   refine' lt_of_le_of_ne (sign_mul_nonneg r) fun h => hr _
   have hs0 := (zero_eq_mul.mp h).resolve_right hr
   exact sign_eq_zero_iff.mp hs0
 #align real.sign_mul_pos_of_ne_zero Real.sign_mul_pos_of_ne_zero
 
 @[simp]
-theorem inv_sign (r : ℝ) : (sign r)⁻¹ = sign r :=
-  by
+theorem inv_sign (r : ℝ) : (sign r)⁻¹ = sign r := by
   obtain hn | hz | hp := sign_apply_eq r
   · rw [hn]
     norm_num
@@ -128,8 +120,7 @@ theorem inv_sign (r : ℝ) : (sign r)⁻¹ = sign r :=
 #align real.inv_sign Real.inv_sign
 
 @[simp]
-theorem sign_inv (r : ℝ) : sign r⁻¹ = sign r :=
-  by
+theorem sign_inv (r : ℝ) : sign r⁻¹ = sign r := by
   obtain hn | rfl | hp := lt_trichotomy r (0 : ℝ)
   · rw [sign_of_neg hn, sign_of_neg (inv_lt_zero.mpr hn)]
   · rw [sign_zero, inv_zero, sign_zero]
@@ -137,4 +128,3 @@ theorem sign_inv (r : ℝ) : sign r⁻¹ = sign r :=
 #align real.sign_inv Real.sign_inv
 
 end Real
-
