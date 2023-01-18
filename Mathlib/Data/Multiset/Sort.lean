1c22cf58df565b45aa66704a4bdb91761cb92b4e
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2023-01-18 11:30:26 +0100

    fix

--- Mathlib/Data/Multiset/Sort.lean
+++ Mathlib/Data/Multiset/Sort.lean
@@ -22,7 +22,7 @@ open List
 
 variable {α : Type _}
 
-section Sort
+section sort
 
 variable (r : α → α → Prop) [DecidableRel r] [IsTrans α r] [IsAntisymm α r] [IsTotal α r]
 
@@ -30,8 +30,8 @@ variable (r : α → α → Prop) [DecidableRel r] [IsTrans α r] [IsAntisymm α
   (Uses merge sort algorithm.) -/
 def sort (s : Multiset α) : List α :=
   Quot.liftOn s (mergeSort r) fun a b h =>
-    eq_of_perm_of_sorted ((perm_merge_sort _ _).trans <| h.trans (perm_merge_sort _ _).symm)
-      (sorted_merge_sort r _) (sorted_merge_sort r _)
+    eq_of_perm_of_sorted ((perm_mergeSort _ _).trans <| h.trans (perm_mergeSort _ _).symm)
+      (sorted_mergeSort r _) (sorted_mergeSort r _)
 #align multiset.sort Multiset.sort
 
 @[simp]
@@ -41,12 +41,12 @@ theorem coe_sort (l : List α) : sort r l = mergeSort r l :=
 
 @[simp]
 theorem sort_sorted (s : Multiset α) : Sorted r (sort r s) :=
-  Quot.inductionOn s fun l => sorted_merge_sort r _
+  Quot.inductionOn s fun l => sorted_mergeSort r _
 #align multiset.sort_sorted Multiset.sort_sorted
 
 @[simp]
 theorem sort_eq (s : Multiset α) : ↑(sort r s) = s :=
-  Quot.inductionOn s fun l => Quot.sound <| perm_merge_sort _ _
+  Quot.inductionOn s fun l => Quot.sound <| perm_mergeSort _ _
 #align multiset.sort_eq Multiset.sort_eq
 
 @[simp]
@@ -54,25 +54,24 @@ theorem mem_sort {s : Multiset α} {a : α} : a ∈ sort r s ↔ a ∈ s := by r
 #align multiset.mem_sort Multiset.mem_sort
 
 @[simp]
-theorem length_sort {s : Multiset α} : (sort r s).length = s.card :=
-  Quot.inductionOn s <| length_merge_sort _
+theorem length_sort {s : Multiset α} : (sort r s).length = card s :=
+  Quot.inductionOn s <| length_mergeSort _
 #align multiset.length_sort Multiset.length_sort
 
 @[simp]
 theorem sort_zero : sort r 0 = [] :=
-  List.merge_sort_nil r
+  List.mergeSort_nil r
 #align multiset.sort_zero Multiset.sort_zero
 
 @[simp]
 theorem sort_singleton (a : α) : sort r {a} = [a] :=
-  List.merge_sort_singleton r a
+  List.mergeSort_singleton r a
 #align multiset.sort_singleton Multiset.sort_singleton
 
-end Sort
+end sort
 
 -- TODO: use a sort order if available, gh-18166
 unsafe instance [Repr α] : Repr (Multiset α) :=
   ⟨fun s => "{" ++ String.intercalate ", " (s.unquot.map repr) ++ "}"⟩
 
 end Multiset
-
