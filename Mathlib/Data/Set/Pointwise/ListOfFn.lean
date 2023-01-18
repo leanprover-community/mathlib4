8ed6661bf23b09909b68d580c8c35ba2bebb9c28
Maxwell Thum <maxwell.thum2718@gmail.com>
2023-01-18 14:47:20 -0800

    Should be good

--- Mathlib/Data/Set/Pointwise/ListOfFn.lean
+++ Mathlib/Data/Set/Pointwise/ListOfFn.lean
@@ -17,8 +17,6 @@ import Mathlib.Data.List.OfFn
 This file proves some lemmas about pointwise algebraic operations with lists of sets.
 -/
 
-set_option autoImplicit false -- **TODO** delete this later
-
 namespace Set
 
 variable {F α β γ : Type _}
@@ -46,8 +44,8 @@ theorem mem_list_prod {l : List (Set α)} {a : α} :
         List.prod (l'.map fun x => (Sigma.snd x : α)) = a ∧ l'.map Sigma.fst = l :=
   by
   induction' l using List.ofFnRec with n f
-  simp_rw [List.exists_iff_exists_tuple, List.map_ofFn, List.ofFn_inj', and_left_comm,
-    exists_and_left, exists_eq_left, heq_iff_eq, Function.comp, mem_prod_list_ofFn]
+  simp only [mem_prod_list_ofFn, List.exists_iff_exists_tuple, List.map_ofFn, Function.comp,
+    List.ofFn_inj', Sigma.mk.inj_iff, and_left_comm, exists_and_left, exists_eq_left, heq_eq_eq]
   constructor
   · rintro ⟨fi, rfl⟩
     exact ⟨fun i => ⟨_, fi i⟩, rfl, rfl⟩
