8ae0d568cee00800329e2ebc46c53b300f60e1a0
Xavier-François Roblot <46200072+xroblot@users.noreply.github.com>
2023-01-18 15:19:52 +0100

    First pass

--- Mathlib/RingTheory/Prime.lean
+++ Mathlib/RingTheory/Prime.lean
@@ -23,7 +23,7 @@ variable {R : Type _} [CancelCommMonoidWithZero R]
 
 open Finset
 
-open BigOperators
+-- open BigOperators
 
 /-- If `x * y = a * ∏ i in s, p i` where `p i` is always prime, then
   `x` and `y` can both be written as a divisor of `a` multiplied by
@@ -31,25 +31,22 @@ open BigOperators
 theorem mul_eq_mul_prime_prod {α : Type _} [DecidableEq α] {x y a : R} {s : Finset α} {p : α → R}
     (hp : ∀ i ∈ s, Prime (p i)) (hx : x * y = a * ∏ i in s, p i) :
     ∃ (t u : Finset α)(b c : R),
-      t ∪ u = s ∧ Disjoint t u ∧ a = b * c ∧ (x = b * ∏ i in t, p i) ∧ y = c * ∏ i in u, p i :=
-  by
+      t ∪ u = s ∧ Disjoint t u ∧ a = b * c ∧ (x = b * ∏ i in t, p i) ∧ y = c * ∏ i in u, p i := by
   induction' s using Finset.induction with i s his ih generalizing x y a
   · exact ⟨∅, ∅, x, y, by simp [hx]⟩
   · rw [prod_insert his, ← mul_assoc] at hx
     have hpi : Prime (p i) := hp i (mem_insert_self _ _)
-    rcases ih (fun i hi => hp i (mem_insert_of_mem hi)) hx with
+    rcases ih (fun i hi ↦ hp i (mem_insert_of_mem hi)) hx with
       ⟨t, u, b, c, htus, htu, hbc, rfl, rfl⟩
-    have hit : i ∉ t := fun hit => his (htus ▸ mem_union_left _ hit)
-    have hiu : i ∉ u := fun hiu => his (htus ▸ mem_union_right _ hiu)
+    have hit : i ∉ t := fun hit ↦ his (htus ▸ mem_union_left _ hit)
+    have hiu : i ∉ u := fun hiu ↦ his (htus ▸ mem_union_right _ hiu)
     obtain ⟨d, rfl⟩ | ⟨d, rfl⟩ : p i ∣ b ∨ p i ∣ c
     exact hpi.dvd_or_dvd ⟨a, by rw [← hbc, mul_comm]⟩
     · rw [mul_assoc, mul_comm a, mul_right_inj' hpi.ne_zero] at hbc
-      exact
-        ⟨insert i t, u, d, c, by rw [insert_union, htus], disjoint_insert_left.2 ⟨hiu, htu⟩, by
+      exact ⟨insert i t, u, d, c, by rw [insert_union, htus], disjoint_insert_left.2 ⟨hiu, htu⟩, by
           simp [hbc, prod_insert hit, mul_assoc, mul_comm, mul_left_comm]⟩
     · rw [← mul_assoc, mul_right_comm b, mul_left_inj' hpi.ne_zero] at hbc
-      exact
-        ⟨t, insert i u, b, d, by rw [union_insert, htus], disjoint_insert_right.2 ⟨hit, htu⟩, by
+      exact ⟨t, insert i u, b, d, by rw [union_insert, htus], disjoint_insert_right.2 ⟨hit, htu⟩, by
           simp [← hbc, prod_insert hiu, mul_assoc, mul_comm, mul_left_comm]⟩
 #align mul_eq_mul_prime_prod mul_eq_mul_prime_prod
 
@@ -58,8 +55,8 @@ theorem mul_eq_mul_prime_prod {α : Type _} [DecidableEq α] {x y a : R} {s : Fi
 theorem mul_eq_mul_prime_pow {x y a p : R} {n : ℕ} (hp : Prime p) (hx : x * y = a * p ^ n) :
     ∃ (i j : ℕ)(b c : R), i + j = n ∧ a = b * c ∧ x = b * p ^ i ∧ y = c * p ^ j :=
   by
-  rcases mul_eq_mul_prime_prod (fun _ _ => hp)
-      (show x * y = a * (range n).Prod fun _ => p by simpa) with
+  rcases mul_eq_mul_prime_prod (fun _ _ ↦ hp)
+      (show x * y = a * (range n).prod fun _ ↦ p by simpa) with
     ⟨t, u, b, c, htus, htu, rfl, rfl, rfl⟩
   exact ⟨t.card, u.card, b, c, by rw [← card_disjoint_union htu, htus, card_range], by simp⟩
 #align mul_eq_mul_prime_pow mul_eq_mul_prime_pow
@@ -70,18 +67,15 @@ section CommRing
 
 variable {α : Type _} [CommRing α]
 
-theorem Prime.neg {p : α} (hp : Prime p) : Prime (-p) :=
-  by
+theorem Prime.neg {p : α} (hp : Prime p) : Prime (-p) := by
   obtain ⟨h1, h2, h3⟩ := hp
   exact ⟨neg_ne_zero.mpr h1, by rwa [IsUnit.neg_iff], by simpa [neg_dvd] using h3⟩
 #align prime.neg Prime.neg
 
-theorem Prime.abs [LinearOrder α] {p : α} (hp : Prime p) : Prime (abs p) :=
-  by
+theorem Prime.abs [LinearOrder α] {p : α} (hp : Prime p) : Prime (abs p) := by
   obtain h | h := abs_choice p <;> rw [h]
   · exact hp
   · exact hp.neg
 #align prime.abs Prime.abs
 
 end CommRing
-
