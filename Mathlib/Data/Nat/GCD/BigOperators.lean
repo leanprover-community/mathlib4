4d5c83b14840aff90d0e4a1ff1e65dc96af2b931
Xavier-François Roblot <46200072+xroblot@users.noreply.github.com>
2023-01-18 16:07:14 +0100

    First pass

--- Mathlib/Data/Nat/GCD/BigOperators.lean
+++ Mathlib/Data/Nat/GCD/BigOperators.lean
@@ -8,30 +8,30 @@ Authors: Jeremy Avigad, Leonardo de Moura
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
-import Mathbin.Data.Nat.Gcd.Basic
-import Mathbin.Algebra.BigOperators.Basic
+import Mathlib.Data.Nat.GCD.Basic
+import Mathlib.Algebra.BigOperators.Basic
 
 /-! # Lemmas about coprimality with big products.
 
-These lemmas are kept separate from `data.nat.gcd.basic` in order to minimize imports.
+These lemmas are kept separate from `Data.Nat.GCD.Basic` in order to minimize imports.
 -/
 
 
 namespace Nat
 
-open BigOperators
+-- Porting note: commented out the next line
+-- open BigOperators
 
 /-- See `is_coprime.prod_left` for the corresponding lemma about `is_coprime` -/
 theorem coprime_prod_left {ι : Type _} {x : ℕ} {s : ι → ℕ} {t : Finset ι} :
-    (∀ i : ι, i ∈ t → Coprime (s i) x) → Coprime (∏ i : ι in t, s i) x :=
-  Finset.prod_induction s (fun y => y.Coprime x) (fun a b => Coprime.mul) (by simp)
+    (∀ i : ι, i ∈ t → coprime (s i) x) → coprime (∏ i : ι in t, s i) x :=
+  Finset.prod_induction s (fun y ↦ y.coprime x) (fun a b ↦ coprime.mul) (by simp)
 #align nat.coprime_prod_left Nat.coprime_prod_left
 
 /-- See `is_coprime.prod_right` for the corresponding lemma about `is_coprime` -/
 theorem coprime_prod_right {ι : Type _} {x : ℕ} {s : ι → ℕ} {t : Finset ι} :
-    (∀ i : ι, i ∈ t → Coprime x (s i)) → Coprime x (∏ i : ι in t, s i) :=
-  Finset.prod_induction s (fun y => x.Coprime y) (fun a b => Coprime.mul_right) (by simp)
+    (∀ i : ι, i ∈ t → coprime x (s i)) → coprime x (∏ i : ι in t, s i) :=
+  Finset.prod_induction s (fun y ↦ x.coprime y) (fun a b ↦ coprime.mul_right) (by simp)
 #align nat.coprime_prod_right Nat.coprime_prod_right
 
 end Nat
-
