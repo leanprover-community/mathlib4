dfe36c4d3df74c223b37a8ecb7df0b7d91b3c859
Frédéric Dupuis <dupuisf@iro.umontreal.ca>
2023-01-18 20:56:49 -0500

    done

--- Mathlib/Data/Rat/BigOperators.lean
+++ Mathlib/Data/Rat/BigOperators.lean
@@ -14,8 +14,8 @@ import Mathlib.Algebra.BigOperators.Basic
 /-! # Casting lemmas for rational numbers involving sums and products
 -/
 
-
-open BigOperators
+-- Porting note: big operators are currently global
+--open BigOperators
 
 variable {ι α : Type _}
 
@@ -26,22 +26,22 @@ section WithDivRing
 variable [DivisionRing α] [CharZero α]
 
 @[simp, norm_cast]
-theorem cast_list_sum (s : List ℚ) : (↑s.Sum : α) = (s.map coe).Sum :=
+theorem cast_list_sum (s : List ℚ) : (↑s.sum : α) = (s.map (↑)).sum :=
   map_list_sum (Rat.castHom α) _
 #align rat.cast_list_sum Rat.cast_list_sum
 
 @[simp, norm_cast]
-theorem cast_multiset_sum (s : Multiset ℚ) : (↑s.Sum : α) = (s.map coe).Sum :=
+theorem cast_multiset_sum (s : Multiset ℚ) : (↑s.sum : α) = (s.map (↑)).sum :=
   map_multiset_sum (Rat.castHom α) _
 #align rat.cast_multiset_sum Rat.cast_multiset_sum
 
 @[simp, norm_cast]
-theorem cast_sum (s : Finset ι) (f : ι → ℚ) : (↑(∑ i in s, f i) : α) = ∑ i in s, f i :=
-  map_sum (Rat.castHom α) _ _
+theorem cast_sum (s : Finset ι) (f : ι → ℚ) : (∑ i in s, f i) = ∑ i in s, (f i : α) :=
+  (map_sum (Rat.castHom α) _ s)
 #align rat.cast_sum Rat.cast_sum
 
 @[simp, norm_cast]
-theorem cast_list_prod (s : List ℚ) : (↑s.Prod : α) = (s.map coe).Prod :=
+theorem cast_list_prod (s : List ℚ) : (↑s.prod : α) = (s.map (↑)).prod :=
   map_list_prod (Rat.castHom α) _
 #align rat.cast_list_prod Rat.cast_list_prod
 
@@ -52,16 +52,15 @@ section Field
 variable [Field α] [CharZero α]
 
 @[simp, norm_cast]
-theorem cast_multiset_prod (s : Multiset ℚ) : (↑s.Prod : α) = (s.map coe).Prod :=
+theorem cast_multiset_prod (s : Multiset ℚ) : (↑s.prod : α) = (s.map (↑)).prod :=
   map_multiset_prod (Rat.castHom α) _
 #align rat.cast_multiset_prod Rat.cast_multiset_prod
 
 @[simp, norm_cast]
-theorem cast_prod (s : Finset ι) (f : ι → ℚ) : (↑(∏ i in s, f i) : α) = ∏ i in s, f i :=
+theorem cast_prod (s : Finset ι) (f : ι → ℚ) : (∏ i in s, f i) = ∏ i in s, (f i : α) :=
   map_prod (Rat.castHom α) _ _
 #align rat.cast_prod Rat.cast_prod
 
 end Field
 
 end Rat
-
