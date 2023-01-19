a4a88be3891c0ca95e1c930c3b047899d707b613
Xavier-François Roblot <46200072+xroblot@users.noreply.github.com>
2023-01-19 18:39:32 +0100

    Fix to_additive name

--- Mathlib/Algebra/BigOperators/Pi.lean
+++ Mathlib/Algebra/BigOperators/Pi.lean
@@ -21,55 +21,56 @@ of monoids and groups
 -/
 
 
-open BigOperators
+-- Port note: commented out the next line
+-- open BigOperators
 
 namespace Pi
 
 @[to_additive]
 theorem list_prod_apply {α : Type _} {β : α → Type _} [∀ a, Monoid (β a)] (a : α)
-    (l : List (∀ a, β a)) : l.Prod a = (l.map fun f : ∀ a, β a => f a).Prod :=
+    (l : List (∀ a, β a)) : l.prod a = (l.map fun f : ∀ a, β a ↦ f a).prod :=
   (evalMonoidHom β a).map_list_prod _
 #align pi.list_prod_apply Pi.list_prod_apply
 #align pi.list_sum_apply Pi.list_sum_apply
 
 @[to_additive]
 theorem multiset_prod_apply {α : Type _} {β : α → Type _} [∀ a, CommMonoid (β a)] (a : α)
-    (s : Multiset (∀ a, β a)) : s.Prod a = (s.map fun f : ∀ a, β a => f a).Prod :=
+    (s : Multiset (∀ a, β a)) : s.prod a = (s.map fun f : ∀ a, β a ↦ f a).prod :=
   (evalMonoidHom β a).map_multiset_prod _
 #align pi.multiset_prod_apply Pi.multiset_prod_apply
 #align pi.multiset_sum_apply Pi.multiset_sum_apply
 
 end Pi
 
-@[simp, to_additive]
+@[to_additive (attr:=simp)]
 theorem Finset.prod_apply {α : Type _} {β : α → Type _} {γ} [∀ a, CommMonoid (β a)] (a : α)
     (s : Finset γ) (g : γ → ∀ a, β a) : (∏ c in s, g c) a = ∏ c in s, g c a :=
   (Pi.evalMonoidHom β a).map_prod _ _
 #align finset.prod_apply Finset.prod_apply
 #align finset.sum_apply Finset.sum_apply
 
-/-- An 'unapplied' analogue of `finset.prod_apply`. -/
-@[to_additive "An 'unapplied' analogue of `finset.sum_apply`."]
+/-- An 'unapplied' analogue of `Finset.prod_apply`. -/
+@[to_additive "An 'unapplied' analogue of `Finset.sum_apply`."]
 theorem Finset.prod_fn {α : Type _} {β : α → Type _} {γ} [∀ a, CommMonoid (β a)] (s : Finset γ)
-    (g : γ → ∀ a, β a) : (∏ c in s, g c) = fun a => ∏ c in s, g c a :=
-  funext fun a => Finset.prod_apply _ _ _
+    (g : γ → ∀ a, β a) : (∏ c in s, g c) = fun a ↦ ∏ c in s, g c a :=
+  funext fun _ ↦ Finset.prod_apply _ _ _
 #align finset.prod_fn Finset.prod_fn
 #align finset.sum_fn Finset.sum_fn
 
-@[simp, to_additive]
+@[to_additive (attr:=simp)]
 theorem Fintype.prod_apply {α : Type _} {β : α → Type _} {γ : Type _} [Fintype γ]
     [∀ a, CommMonoid (β a)] (a : α) (g : γ → ∀ a, β a) : (∏ c, g c) a = ∏ c, g c a :=
   Finset.prod_apply a Finset.univ g
 #align fintype.prod_apply Fintype.prod_apply
 #align fintype.sum_apply Fintype.sum_apply
 
-@[to_additive prod_mk_sum]
+@[to_additive]
 theorem prod_mk_prod {α β γ : Type _} [CommMonoid α] [CommMonoid β] (s : Finset γ) (f : γ → α)
     (g : γ → β) : (∏ x in s, f x, ∏ x in s, g x) = ∏ x in s, (f x, g x) :=
   haveI := Classical.decEq γ
   Finset.induction_on s rfl (by simp (config := { contextual := true }) [Prod.ext_iff])
 #align prod_mk_prod prod_mk_prod
-#align prod_mk_sum prod_mk_sum
+#align sum_mk_sum sum_mk_sum
 
 section Single
 
@@ -91,13 +92,13 @@ theorem AddMonoidHom.functions_ext [Finite I] (G : Type _) [AddCommMonoid G] (g 
   simp only [H]
 #align add_monoid_hom.functions_ext AddMonoidHom.functions_ext
 
-/-- This is used as the ext lemma instead of `add_monoid_hom.functions_ext` for reasons explained in
+/-- This is used as the ext lemma instead of `AddMonoidHom.functions_ext` for reasons explained in
 note [partially-applied ext lemmas]. -/
 @[ext]
 theorem AddMonoidHom.functions_ext' [Finite I] (M : Type _) [AddCommMonoid M]
     (g h : (∀ i, Z i) →+ M)
     (H : ∀ i, g.comp (AddMonoidHom.single Z i) = h.comp (AddMonoidHom.single Z i)) : g = h :=
-  have := fun i => AddMonoidHom.congr_fun (H i)
+  have := fun i ↦ AddMonoidHom.congr_fun (H i)
   -- elab without an expected type
       g.functions_ext
     M h this
@@ -122,21 +123,17 @@ theorem RingHom.functions_ext [Finite I] (G : Type _) [NonAssocSemiring G] (g h 
 
 end RingHom
 
-namespace Prod
 
 variable {α β γ : Type _} [CommMonoid α] [CommMonoid β] {s : Finset γ} {f : γ → α × β}
 
-@[to_additive]
-theorem fst_prod : (∏ c in s, f c).1 = ∏ c in s, (f c).1 :=
+@[to_additive Sum.fst_sum]
+theorem Prod.fst_prod : (∏ c in s, f c).1 = ∏ c in s, (f c).1 :=
   (MonoidHom.fst α β).map_prod f s
 #align prod.fst_prod Prod.fst_prod
-#align prod.fst_sum Prod.fst_sum
+#align prod.fst_sum Sum.fst_sum
 
-@[to_additive]
-theorem snd_prod : (∏ c in s, f c).2 = ∏ c in s, (f c).2 :=
+@[to_additive Sum.snd_sum]
+theorem Prod.snd_prod : (∏ c in s, f c).2 = ∏ c in s, (f c).2 :=
   (MonoidHom.snd α β).map_prod f s
 #align prod.snd_prod Prod.snd_prod
-#align prod.snd_sum Prod.snd_sum
-
-end Prod
-
+#align prod.snd_sum Sum.snd_sum
