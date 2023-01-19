cb14049979ce00f88cb34cb0ba7deaa66476c28d
qawbecrdtey <qawbecrdtey@kaist.ac.kr>
2023-01-20 00:56:12 +0900

    Initial commit, Fixed Zmod to ZMod

--- Mathlib/Data/ZMod/Defs.lean
+++ Mathlib/Data/ZMod/Defs.lean
@@ -8,26 +8,26 @@ Authors: Eric Rodriguez
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
-import Mathbin.Algebra.NeZero
-import Mathbin.Data.Nat.Modeq
-import Mathbin.Data.Fintype.Lattice
+import Mathlib.Algebra.NeZero
+import Mathlib.Data.Nat.ModEq
+import Mathlib.Data.Fintype.Lattice
 
 /-!
-# Definition of `zmod n` + basic results.
+# Definition of `ZMod n` + basic results.
 
-This file provides the basic details of `zmod n`, including its commutative ring structure.
+This file provides the basic details of `ZMod n`, including its commutative ring structure.
 
 ## Implementation details
 
-This used to be inlined into data/zmod/basic.lean. This file imports `char_p/basic`, which is an
-issue; all `char_p` instances create an `algebra (zmod p) R` instance; however, this instance may
-not be definitionally equal to other `algebra` instances (for example, `galois_field` also has an
-`algebra` instance as it is defined as a `splitting_field`). The way to fix this is to use the
-forgetful inheritance pattern, and make `char_p` carry the data of what the `smul` should be (so
-for example, the `smul` on the `galois_field` `char_p` instance should be equal to the `smul` from
-its `splitting_field` structure); there is only one possible `zmod p` algebra for any `p`, so this
-is not an issue mathematically. For this to be possible, however, we need `char_p/basic` to be
-able to import some part of `zmod`.
+This used to be inlined into Data/ZMod/Basic.lean. This file imports `CharP/Basic`, which is an
+issue; all `CharP` instances create an `Algebra (ZMod p) R` instance; however, this instance may
+not be definitionally equal to other `Algebra` instances (for example, `GaloisField` also has an
+`Algebra` instance as it is defined as a `SplittingField`). The way to fix this is to use the
+forgetful inheritance pattern, and make `CharP` carry the data of what the `smul` should be (so
+for example, the `smul` on the `GaloisField` `CharP` instance should be equal to the `smul` from
+its `SplittingField` structure); there is only one possible `ZMod p` Algebra for any `p`, so this
+is not an issue mathematically. For this to be possible, however, we need `CharP/Basic` to be
+able to import some part of `ZMod`.
 
 -/
 
@@ -35,20 +35,20 @@ able to import some part of `zmod`.
 namespace Fin
 
 /-!
-## Ring structure on `fin n`
+## Ring structure on `Fin n`
 
-We define a commutative ring structure on `fin n`, but we do not register it as instance.
-Afterwords, when we define `zmod n` in terms of `fin n`, we use these definitions
-to register the ring structure on `zmod n` as type class instance.
+We define a commutative ring structure on `Fin n`, but we do not register it as instance.
+Afterwords, when we define `ZMod n` in terms of `Fin n`, we use these definitions
+to register the ring structure on `ZMod n` as type class instance.
 -/
 
 
 open Nat.ModEq Int
 
-/-- Multiplicative commutative semigroup structure on `fin n`. -/
+/-- Multiplicative commutative semigroup structure on `Fin n`. -/
 instance (n : ℕ) : CommSemigroup (Fin n) :=
   {
-    Fin.hasMul with
+    Fin.mul with
     mul_assoc := fun ⟨a, ha⟩ ⟨b, hb⟩ ⟨c, hc⟩ =>
       Fin.eq_of_veq
         (calc
@@ -66,9 +66,9 @@ private theorem left_distrib_aux (n : ℕ) : ∀ a b c : Fin n, a * (b + c) = a 
       _ ≡ a * b + a * c [MOD n] := by rw [mul_add]
       _ ≡ a * b % n + a * c % n [MOD n] := (Nat.mod_modEq _ _).symm.add (Nat.mod_modEq _ _).symm
       )
-#align fin.left_distrib_aux fin.left_distrib_aux
+#align fin.left_distrib_aux Fin.left_distrib_aux
 
-/-- Commutative ring structure on `fin n`. -/
+/-- Commutative ring structure on `Fin n`. -/
 instance (n : ℕ) [NeZero n] : CommRing (Fin n) :=
   { Fin.addMonoidWithOne, Fin.addCommGroup n,
     Fin.commSemigroup n with
@@ -81,44 +81,43 @@ instance (n : ℕ) [NeZero n] : CommRing (Fin n) :=
 end Fin
 
 /-- The integers modulo `n : ℕ`. -/
-def Zmod : ℕ → Type
+def ZMod : ℕ → Type
   | 0 => ℤ
   | n + 1 => Fin (n + 1)
-#align zmod Zmod
+#align zmod ZMod
 
-instance Zmod.decidableEq : ∀ n : ℕ, DecidableEq (Zmod n)
+instance ZMod.decidableEq : ∀ n : ℕ, DecidableEq (ZMod n)
   | 0 => Int.decidableEq
   | n + 1 => Fin.decidableEq _
-#align zmod.decidable_eq Zmod.decidableEq
+#align zmod.decidable_eq ZMod.decidableEq
 
-instance Zmod.hasRepr : ∀ n : ℕ, Repr (Zmod n)
+instance ZMod.hasRepr : ∀ n : ℕ, Repr (ZMod n)
   | 0 => Int.hasRepr
   | n + 1 => Fin.hasRepr _
-#align zmod.has_repr Zmod.hasRepr
+#align zmod.has_repr ZMod.hasRepr
 
-namespace Zmod
+namespace ZMod
 
-instance fintype : ∀ (n : ℕ) [NeZero n], Fintype (Zmod n)
+instance fintype : ∀ (n : ℕ) [NeZero n], Fintype (ZMod n)
   | 0, h => (NeZero.ne 0 rfl).elim
   | n + 1, _ => Fin.fintype (n + 1)
-#align zmod.fintype Zmod.fintype
+#align zmod.fintype ZMod.fintype
 
-instance infinite : Infinite (Zmod 0) :=
+instance infinite : Infinite (ZMod 0) :=
   Int.infinite
-#align zmod.infinite Zmod.infinite
+#align zmod.infinite ZMod.infinite
 
 @[simp]
-theorem card (n : ℕ) [Fintype (Zmod n)] : Fintype.card (Zmod n) = n :=
-  by
+theorem card (n : ℕ) [Fintype (ZMod n)] : Fintype.card (ZMod n) = n := by
   cases n
-  · exact (not_finite (Zmod 0)).elim
-  · convert Fintype.card_fin (n + 1)
-#align zmod.card Zmod.card
+  · exact (not_finite (ZMod 0)).elim
+  · convert Fintype.card_fin (‹ℕ› + 1); simp
+#align zmod.card ZMod.card
 
-/- We define each field by cases, to ensure that the eta-expanded `zmod.comm_ring` is defeq to the
+/- We define each field by cases, to ensure that the eta-expanded `ZMod.comm_ring` is defeq to the
 original, this helps avoid diamonds with instances coming from classes extending `comm_ring` such as
 field. -/
-instance commRing (n : ℕ) : CommRing (Zmod n)
+instance commRing (n : ℕ) : CommRing (ZMod n)
     where
   add := Nat.casesOn n (@Add.add Int _) fun n => @Add.add (Fin n.succ) _
   add_assoc := Nat.casesOn n (@add_assoc Int _) fun n => @add_assoc (Fin n.succ) _
@@ -128,6 +127,8 @@ instance commRing (n : ℕ) : CommRing (Zmod n)
   neg := Nat.casesOn n (@Neg.neg Int _) fun n => @Neg.neg (Fin n.succ) _
   sub := Nat.casesOn n (@Sub.sub Int _) fun n => @Sub.sub (Fin n.succ) _
   sub_eq_add_neg := Nat.casesOn n (@sub_eq_add_neg Int _) fun n => @sub_eq_add_neg (Fin n.succ) _
+  zero_mul := sorry
+  mul_zero := sorry
   zsmul := Nat.casesOn n (@CommRing.zsmul Int _) fun n => @CommRing.zsmul (Fin n.succ) _
   zsmul_zero' :=
     Nat.casesOn n (@CommRing.zsmul_zero' Int _) fun n => @CommRing.zsmul_zero' (Fin n.succ) _
@@ -136,13 +137,13 @@ instance commRing (n : ℕ) : CommRing (Zmod n)
   zsmul_neg' :=
     Nat.casesOn n (@CommRing.zsmul_neg' Int _) fun n => @CommRing.zsmul_neg' (Fin n.succ) _
   nsmul := Nat.casesOn n (@CommRing.nsmul Int _) fun n => @CommRing.nsmul (Fin n.succ) _
-  nsmul_zero' :=
+  nsmul_zero :=
     Nat.casesOn n (@CommRing.nsmul_zero' Int _) fun n => @CommRing.nsmul_zero' (Fin n.succ) _
-  nsmul_succ' :=
+  nsmul_succ :=
     Nat.casesOn n (@CommRing.nsmul_succ' Int _) fun n => @CommRing.nsmul_succ' (Fin n.succ) _
   add_left_neg := by
     cases n
-    exacts[@add_left_neg Int _, @add_left_neg (Fin n.succ) _]
+    exacts [@add_left_neg Int _, @add_left_neg (Fin (Nat.succ _)) _]
   add_comm := Nat.casesOn n (@add_comm Int _) fun n => @add_comm (Fin n.succ) _
   mul := Nat.casesOn n (@Mul.mul Int _) fun n => @Mul.mul (Fin n.succ) _
   mul_assoc := Nat.casesOn n (@mul_assoc Int _) fun n => @mul_assoc (Fin n.succ) _
@@ -150,21 +151,20 @@ instance commRing (n : ℕ) : CommRing (Zmod n)
   one_mul := Nat.casesOn n (@one_mul Int _) fun n => @one_mul (Fin n.succ) _
   mul_one := Nat.casesOn n (@mul_one Int _) fun n => @mul_one (Fin n.succ) _
   natCast := Nat.casesOn n (coe : ℕ → ℤ) fun n => (coe : ℕ → Fin n.succ)
-  nat_cast_zero := Nat.casesOn n (@Nat.cast_zero Int _) fun n => @Nat.cast_zero (Fin n.succ) _
-  nat_cast_succ := Nat.casesOn n (@Nat.cast_succ Int _) fun n => @Nat.cast_succ (Fin n.succ) _
+  natCast_zero := Nat.casesOn n (@Nat.cast_zero Int _) fun n => @Nat.cast_zero (Fin n.succ) _
+  natCast_succ := Nat.casesOn n (@Nat.cast_succ Int _) fun n => @Nat.cast_succ (Fin n.succ) _
   intCast := Nat.casesOn n (coe : ℤ → ℤ) fun n => (coe : ℤ → Fin n.succ)
-  int_cast_of_nat := Nat.casesOn n (@Int.cast_of_nat Int _) fun n => @Int.cast_of_nat (Fin n.succ) _
-  int_cast_neg_succ_of_nat :=
+  intCast_ofNat := Nat.casesOn n (@Int.cast_of_nat Int _) fun n => @Int.cast_of_nat (Fin n.succ) _
+  intCast_negSucc :=
     Nat.casesOn n (@Int.cast_negSucc Int _) fun n => @Int.cast_negSucc (Fin n.succ) _
   left_distrib := Nat.casesOn n (@left_distrib Int _ _ _) fun n => @left_distrib (Fin n.succ) _ _ _
   right_distrib :=
     Nat.casesOn n (@right_distrib Int _ _ _) fun n => @right_distrib (Fin n.succ) _ _ _
   mul_comm := Nat.casesOn n (@mul_comm Int _) fun n => @mul_comm (Fin n.succ) _
-#align zmod.comm_ring Zmod.commRing
+#align zmod.comm_ring ZMod.commRing
 
-instance inhabited (n : ℕ) : Inhabited (Zmod n) :=
+instance inhabited (n : ℕ) : Inhabited (ZMod n) :=
   ⟨0⟩
-#align zmod.inhabited Zmod.inhabited
+#align zmod.inhabited ZMod.inhabited
 
-end Zmod
-
+end ZMod
