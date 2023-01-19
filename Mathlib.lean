67f19ab77969f0c63487d97c4b7ee44d12c6e493
Arien Malec <arien.malec@gmail.com>
2023-01-19 09:51:45 -0800

    export in Mathlib

--- Mathlib.lean
+++ Mathlib.lean
@@ -238,6 +238,7 @@ import Mathlib.Data.Fin.SuccPred
 import Mathlib.Data.Fin.Tuple.Basic
 import Mathlib.Data.FinEnum
 import Mathlib.Data.Finite.Defs
+import Mathlib.Data.Finite.Set
 import Mathlib.Data.Finset.Basic
 import Mathlib.Data.Finset.Card
 import Mathlib.Data.Finset.Fin
