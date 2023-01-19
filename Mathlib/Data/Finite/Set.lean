42fe4b744e4202371b8d4fafc8fb7d8f33dd8e49
Arien Malec <arien.malec@gmail.com>
2023-01-19 08:59:38 -0800

    merge + set_option

--- Mathlib/Data/Finite/Set.lean
+++ Mathlib/Data/Finite/Set.lean
@@ -9,7 +9,8 @@ Authors: Kyle Miller
 ! if you have ported upstream changes.
 -/
 import Mathlib.Data.Fintype.Card
- -- TODO: remove
+
+set_option autoImplicit false-- TODO: remove
 /-!
 # Lemmas about `Finite` and `Set`s
 
