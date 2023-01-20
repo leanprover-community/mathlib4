dbb8291856e9324fbe57ba3eba30b6ae16e5151a
z <zbattleman@gmail.com>
2023-01-20 12:08:40 -0500

    suggestions

--- Mathlib/Data/Vector/Zip.lean
+++ Mathlib/Data/Vector/Zip.lean
@@ -27,33 +27,33 @@ def zipWith : Vector α n → Vector β n → Vector γ n := fun x y => ⟨List.
 #align vector.zip_with Vector.zipWith
 
 @[simp]
-theorem zip_with_to_list (x : Vector α n) (y : Vector β n) :
+theorem zipWith_to_list (x : Vector α n) (y : Vector β n) :
     (Vector.zipWith f x y).toList = List.zipWith f x.toList y.toList :=
   rfl
-#align vector.zip_with_to_list Vector.zip_with_to_list
+#align vector.zip_with_to_list Vector.zipWith_to_list
 
 @[simp]
-theorem zip_with_nth (x : Vector α n) (y : Vector β n) (i) :
+theorem zipWith_get (x : Vector α n) (y : Vector β n) (i) :
     (Vector.zipWith f x y).get i = f (x.get i) (y.get i) := by
   dsimp only [Vector.zipWith, Vector.get]
   cases x; cases y
   simp only [List.nthLe_zipWith]
-#align vector.zip_with_nth Vector.zip_with_nth
+#align vector.zip_with_nth Vector.zipWith_get
 
 @[simp]
-theorem zip_with_tail (x : Vector α n) (y : Vector β n) :
+theorem zipWith_tail (x : Vector α n) (y : Vector β n) :
     (Vector.zipWith f x y).tail = Vector.zipWith f x.tail y.tail := by
   ext
   simp [get_tail]
-#align vector.zip_with_tail Vector.zip_with_tail
+#align vector.zip_with_tail Vector.zipWith_tail
 
 @[to_additive]
-theorem prod_mul_prod_eq_prod_zip_with [CommMonoid α] (x y : Vector α n) :
+theorem prod_mul_prod_eq_prod_zipWith [CommMonoid α] (x y : Vector α n) :
     x.toList.prod * y.toList.prod = (Vector.zipWith (· * ·) x y).toList.prod :=
   List.prod_mul_prod_eq_prod_zipWith_of_length_eq x.toList y.toList
     ((toList_length x).trans (toList_length y).symm)
-#align vector.prod_mul_prod_eq_prod_zip_with Vector.prod_mul_prod_eq_prod_zip_with
-#align vector.sum_add_sum_eq_sum_zip_with Vector.sum_add_sum_eq_sum_zip_with
+#align vector.prod_mul_prod_eq_prod_zip_with Vector.prod_mul_prod_eq_prod_zipWith
+#align vector.sum_add_sum_eq_sum_zip_with Vector.sum_add_sum_eq_sum_zipWith
 
 end ZipWith
 
