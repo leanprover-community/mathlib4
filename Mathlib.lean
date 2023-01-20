1a344abbcd1e8e9d9c6020f52dcd309a304b145b
Xavier-François Roblot <46200072+xroblot@users.noreply.github.com>
2023-01-20 10:47:07 +0000

    feat : port RingTheory.Fintype (#1712)

--- Mathlib.lean
+++ Mathlib.lean
@@ -660,6 +660,7 @@ import Mathlib.Order.ZornAtoms
 import Mathlib.RingTheory.Congruence
 import Mathlib.RingTheory.Coprime.Basic
 import Mathlib.RingTheory.Coprime.Lemmas
+import Mathlib.RingTheory.Fintype
 import Mathlib.RingTheory.OreLocalization.OreSet
 import Mathlib.RingTheory.Prime
 import Mathlib.SetTheory.Cardinal.SchroederBernstein
