a5db36af613ebc0e1d8f6b5fe8257cae69790ab1
arienmalec <arien.malec@gmail.com>
2023-01-20 17:11:39 +0000

    Feat: Port data.prod.tprod (#1464)
    Port of data.prod.tprod

--- Mathlib.lean
+++ Mathlib.lean
@@ -418,6 +418,7 @@ import Mathlib.Data.Pi.Lex
 import Mathlib.Data.Prod.Basic
 import Mathlib.Data.Prod.Lex
 import Mathlib.Data.Prod.PProd
+import Mathlib.Data.Prod.TProd
 import Mathlib.Data.Quot
 import Mathlib.Data.Rat.Basic
 import Mathlib.Data.Rat.BigOperators
