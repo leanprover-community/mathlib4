435c5475274867f2d65142285912a67323d4f424
Xavier-François Roblot <46200072+xroblot@users.noreply.github.com>
2023-01-20 11:06:34 +0100

    Initial file copy from mathport

---
+++ Mathlib/RingTheory/Fintype.lean
@@ -0,0 +1,24 @@
+/-
+Copyright (c) 2020 Scott Morrison. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Scott Morrison
+
+! This file was ported from Lean 3 source module ring_theory.fintype
+! leanprover-community/mathlib commit 1126441d6bccf98c81214a0780c73d499f6721fe
+! Please do not edit these lines, except to modify the commit id
+! if you have ported upstream changes.
+-/
+import Mathbin.Data.Fintype.Units
+
+/-!
+# Some facts about finite rings
+-/
+
+
+open Classical
+
+theorem card_units_lt (M₀ : Type _) [MonoidWithZero M₀] [Nontrivial M₀] [Fintype M₀] :
+    Fintype.card M₀ˣ < Fintype.card M₀ :=
+  Fintype.card_lt_of_injective_of_not_mem (coe : M₀ˣ → M₀) Units.ext not_isUnit_zero
+#align card_units_lt card_units_lt
+
