e5a64715483e198b99eedb134a2165d7e0f9d685
Chris Hughes <chrishughes24@gmail.com>
2023-01-21 16:25:52 +0000

    feat: Port Algebra.Support (#1747)

--- Mathlib.lean
+++ Mathlib.lean
@@ -181,6 +181,7 @@ import Mathlib.Algebra.Star.BigOperators
 import Mathlib.Algebra.Star.Pi
 import Mathlib.Algebra.Star.Prod
 import Mathlib.Algebra.Star.Unitary
+import Mathlib.Algebra.Support
 import Mathlib.Algebra.Tropical.Basic
 import Mathlib.Algebra.Tropical.Lattice
 import Mathlib.CategoryTheory.Category.Basic
