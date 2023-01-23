f7baace5adb7c14f1f87ed1b328b0492427a37ff
Moritz Firsching <moritz.firsching@gmail.com>
2023-01-23 07:56:55 +0000

    feat: port Combinatorics.Hall.Finite (#1763)
    Co-authored-by: Moritz Firsching <firsching@google.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -203,6 +203,7 @@ import Mathlib.CategoryTheory.Thin
 import Mathlib.CategoryTheory.Whiskering
 import Mathlib.Combinatorics.Additive.Energy
 import Mathlib.Combinatorics.DoubleCounting
+import Mathlib.Combinatorics.Hall.Finite
 import Mathlib.Combinatorics.Quiver.Arborescence
 import Mathlib.Combinatorics.Quiver.Basic
 import Mathlib.Combinatorics.Quiver.Cast
