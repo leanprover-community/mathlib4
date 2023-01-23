7aa98b6c70cd7b688eff8299b08eb74712da34cd
Xavier-François Roblot <46200072+xroblot@users.noreply.github.com>
2023-01-23 08:16:10 +0000

    feat : port Algebra.Star.Pointwise (#1749)
    Co-authored-by: Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -180,6 +180,7 @@ import Mathlib.Algebra.SMulWithZero
 import Mathlib.Algebra.Star.Basic
 import Mathlib.Algebra.Star.BigOperators
 import Mathlib.Algebra.Star.Pi
+import Mathlib.Algebra.Star.Pointwise
 import Mathlib.Algebra.Star.Prod
 import Mathlib.Algebra.Star.Unitary
 import Mathlib.Algebra.Support
