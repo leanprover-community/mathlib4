556bdf2ece314dab2395169e0b75ec9728c7f687
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2023-01-23 17:52:29 +0100

    Drop obsolete part of comment.

--- Mathlib/Data/ZMod/Defs.lean
+++ Mathlib/Data/ZMod/Defs.lean
@@ -37,7 +37,7 @@ namespace Fin
 /-!
 ## Ring structure on `Fin n`
 
-We define a commutative ring structure on `Fin n`, but we do not register it as instance.
+We define a commutative ring structure on `Fin n`.
 Afterwords, when we define `ZMod n` in terms of `Fin n`, we use these definitions
 to register the ring structure on `ZMod n` as type class instance.
 -/
