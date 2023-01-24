37d80190de25eb4c0815e5b4d5755165711a801f
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2023-01-24 01:25:17 +0100

    fix

--- Mathlib/Algebra/Ring/Fin.lean
+++ Mathlib/Algebra/Ring/Fin.lean
@@ -13,24 +13,23 @@ import Mathlib.Algebra.Ring.Equiv
 import Mathlib.Algebra.Group.Prod
 
 /-!
-# Rings and `fin`
+# Rings and `Fin`
 
-This file collects some basic results involving rings and the `fin` type
+This file collects some basic results involving rings and the `Fin` type
 
 ## Main results
 
- * `ring_equiv.fin_two`: The product over `fin 2` of some rings is the cartesian product
+ * `RingEquiv.piFinTwo`: The product over `Fin 2` of some rings is the cartesian product
 
 -/
 
 
-/-- The product over `fin 2` of some rings is just the cartesian product of these rings. -/
+/-- The product over `Fin 2` of some rings is just the cartesian product of these rings. -/
 @[simps]
 def RingEquiv.piFinTwo (R : Fin 2 → Type _) [∀ i, Semiring (R i)] :
     (∀ i : Fin 2, R i) ≃+* R 0 × R 1 :=
   { piFinTwoEquiv R with
     toFun := piFinTwoEquiv R
-    map_add' := fun a b => rfl
-    map_mul' := fun a b => rfl }
+    map_add' := fun _ _ => rfl
+    map_mul' := fun _ _ => rfl }
 #align ring_equiv.pi_fin_two RingEquiv.piFinTwo
-
