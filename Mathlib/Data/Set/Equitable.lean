0a64ff4bde908cbb49e9dd36227d1ba85774b424
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2023-01-24 09:14:16 +0100

    Fix

--- Mathlib/Data/Set/Equitable.lean
+++ Mathlib/Data/Set/Equitable.lean
@@ -21,12 +21,12 @@ useful when the codomain of `f` is `ℕ` or `ℤ` (or more generally a successor
 
 ## TODO
 
-`ℕ` can be replaced by any `succ_order` + `conditionally_complete_monoid`, but we don't have the
+`ℕ` can be replaced by any `SuccOrder` + `ConditionallyCompleteMonoid`, but we don't have the
 latter yet.
 -/
 
 
-open BigOperators
+-- Porting note: global for now: open BigOperators
 
 variable {α β : Type _}
 
@@ -39,7 +39,7 @@ def EquitableOn [LE β] [Add β] [One β] (s : Set α) (f : α → β) : Prop :=
 
 @[simp]
 theorem equitableOn_empty [LE β] [Add β] [One β] (f : α → β) : EquitableOn ∅ f := fun a _ ha =>
-  (Set.not_mem_empty _ ha).elim
+  (Set.not_mem_empty a ha).elim
 #align set.equitable_on_empty Set.equitableOn_empty
 
 theorem equitableOn_iff_exists_le_le_add_one {s : Set α} {f : α → ℕ} :
@@ -50,7 +50,7 @@ theorem equitableOn_iff_exists_le_le_add_one {s : Set α} {f : α → ℕ} :
   intro hs
   by_cases h : ∀ y ∈ s, f x ≤ f y
   · exact ⟨f x, fun y hy => ⟨h _ hy, hs hy hx⟩⟩
-  push_neg  at h
+  push_neg at h
   obtain ⟨w, hw, hwx⟩ := h
   refine' ⟨f w, fun y hy => ⟨Nat.le_of_succ_le_succ _, hs hy hw⟩⟩
   rw [(Nat.succ_le_of_lt hwx).antisymm (hs hx hw)]
@@ -59,12 +59,12 @@ theorem equitableOn_iff_exists_le_le_add_one {s : Set α} {f : α → ℕ} :
 
 theorem equitableOn_iff_exists_image_subset_icc {s : Set α} {f : α → ℕ} :
     s.EquitableOn f ↔ ∃ b, f '' s ⊆ Icc b (b + 1) := by
-  simpa only [image_subset_iff] using equitable_on_iff_exists_le_le_add_one
+  simpa only [image_subset_iff] using equitableOn_iff_exists_le_le_add_one
 #align set.equitable_on_iff_exists_image_subset_Icc Set.equitableOn_iff_exists_image_subset_icc
 
 theorem equitableOn_iff_exists_eq_eq_add_one {s : Set α} {f : α → ℕ} :
     s.EquitableOn f ↔ ∃ b, ∀ a ∈ s, f a = b ∨ f a = b + 1 := by
-  simp_rw [equitable_on_iff_exists_le_le_add_one, Nat.le_and_le_add_one_iff]
+  simp_rw [equitableOn_iff_exists_le_le_add_one, Nat.le_and_le_add_one_iff]
 #align set.equitable_on_iff_exists_eq_eq_add_one Set.equitableOn_iff_exists_eq_eq_add_one
 
 section OrderedSemiring
@@ -78,7 +78,7 @@ theorem Subsingleton.equitableOn {s : Set α} (hs : s.Subsingleton) (f : α → 
 #align set.subsingleton.equitable_on Set.Subsingleton.equitableOn
 
 theorem equitableOn_singleton (a : α) (f : α → β) : Set.EquitableOn {a} f :=
-  Set.subsingleton_singleton.EquitableOn f
+  Set.subsingleton_singleton.equitableOn f
 #align set.equitable_on_singleton Set.equitableOn_singleton
 
 end OrderedSemiring
@@ -101,7 +101,7 @@ theorem equitableOn_iff_le_le_add_one :
   · intro a ha
     rw [h _ ha, sum_const_nat h, Nat.mul_div_cancel_left _ (card_pos.2 ⟨a, ha⟩)]
     exact ⟨le_rfl, Nat.le_succ _⟩
-  push_neg  at h
+  push_neg at h
   obtain ⟨x, hx₁, hx₂⟩ := h
   suffices h : b = (∑ i in s, f i) / s.card
   · simp_rw [← h]
@@ -127,8 +127,7 @@ theorem EquitableOn.le_add_one (h : EquitableOn (s : Set α) f) (ha : a ∈ s) :
 theorem equitableOn_iff :
     EquitableOn (s : Set α) f ↔
       ∀ a ∈ s, f a = (∑ i in s, f i) / s.card ∨ f a = (∑ i in s, f i) / s.card + 1 :=
-  by simp_rw [equitable_on_iff_le_le_add_one, Nat.le_and_le_add_one_iff]
+  by simp_rw [equitableOn_iff_le_le_add_one, Nat.le_and_le_add_one_iff]
 #align finset.equitable_on_iff Finset.equitableOn_iff
 
 end Finset
-
