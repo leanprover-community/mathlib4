9742cd11988b8a5dc2e80094f40774a0103363c0
Yury G. Kudryashov <urkud@urkud.name>
2023-01-24 09:37:04 +0000

    feat: port Order.Filter.Lift (#1804)

--- Mathlib.lean
+++ Mathlib.lean
@@ -661,6 +661,7 @@ import Mathlib.Order.Filter.Basic
 import Mathlib.Order.Filter.CountableInter
 import Mathlib.Order.Filter.Curry
 import Mathlib.Order.Filter.Extr
+import Mathlib.Order.Filter.Lift
 import Mathlib.Order.Filter.NAry
 import Mathlib.Order.Filter.Pi
 import Mathlib.Order.Filter.Prod
