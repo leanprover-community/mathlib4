a923247dc087317c239d1e37fb5dc73c5fb9a2c4
Anatole Dedecker <anatolededecker@gmail.com>
2023-01-24 20:53:55 +0000

    feat: port Order.Filter.Ultrafilter (#1814)
    Co-authored-by: Yury G. Kudryashov <urkud@urkud.name>

--- Mathlib.lean
+++ Mathlib.lean
@@ -672,6 +672,7 @@ import Mathlib.Order.Filter.NAry
 import Mathlib.Order.Filter.Pi
 import Mathlib.Order.Filter.Prod
 import Mathlib.Order.Filter.SmallSets
+import Mathlib.Order.Filter.Ultrafilter
 import Mathlib.Order.FixedPoints
 import Mathlib.Order.GaloisConnection
 import Mathlib.Order.GameAdd
