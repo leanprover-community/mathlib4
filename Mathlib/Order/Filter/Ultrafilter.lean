6de82c83df7320ff323ac6e28fbb245d2490c5ed
Yury G. Kudryashov <urkud@urkud.name>
2023-01-24 12:10:34 -0600

    Fix compile

--- Mathlib/Order/Filter/Ultrafilter.lean
+++ Mathlib/Order/Filter/Ultrafilter.lean
@@ -454,7 +454,7 @@ theorem le_iff_ultrafilter {f₁ f₂ : Filter α} : f₁ ≤ f₂ ↔ ∀ g : U
 
 /-- A filter equals the intersection of all the ultrafilters which contain it. -/
 theorem supᵢ_ultrafilter_le_eq (f : Filter α) :
-    (⨆ (g : Ultrafilter α) (hg : g ≤ f), (g : Filter α)) = f :=
+    (⨆ (g : Ultrafilter α) (_hg : g ≤ f), (g : Filter α)) = f :=
   eq_of_forall_ge_iff fun f' => by simp only [supᵢ_le_iff, ← le_iff_ultrafilter]
 #align filter.supr_ultrafilter_le_eq Filter.supᵢ_ultrafilter_le_eq
 
