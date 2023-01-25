695f27a103db7bd66e7dc796eb99e3bbac8cbf10
Jireh Loreaux <loreaujy@gmail.com>
2023-01-25 16:04:06 -0600

    automated fixes
    Mathbin -> Mathlib
    
    fix certain import statements
    
    move "by" to end of line
    
    add import to Mathlib.lean

--- Mathlib/Topology/Bornology/Hom.lean
+++ Mathlib/Topology/Bornology/Hom.lean
@@ -8,7 +8,7 @@ Authors: Yaël Dillies
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
-import Mathbin.Topology.Bornology.Basic
+import Mathlib.Topology.Bornology.Basic
 
 /-!
 # Locally bounded maps
