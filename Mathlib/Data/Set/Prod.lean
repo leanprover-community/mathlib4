8b0ac1f6100d1f3b988945f4ce46c14820d84c26
Chris Hughes <33847686+ChrisHughes24@users.noreply.github.com>
2023-01-26 10:45:56 +0000

    update SHA

--- Mathlib/Data/Set/Prod.lean
+++ Mathlib/Data/Set/Prod.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro, Johannes Hölzl, Patrick Massot
 
 ! This file was ported from Lean 3 source module data.set.prod
-! leanprover-community/mathlib commit 2ed7e4aec72395b6a7c3ac4ac7873a7a43ead17c
+! leanprover-community/mathlib commit 27f315c5591c84687852f816d8ef31fe103d03de
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
