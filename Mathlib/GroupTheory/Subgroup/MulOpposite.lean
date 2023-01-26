41d9f94631576c59ce7924fc8fe1e84c08abd586
Jireh Loreaux <loreaujy@gmail.com>
2023-01-26 10:25:49 -0600

    fix all

--- Mathlib/GroupTheory/Subgroup/MulOpposite.lean
+++ Mathlib/GroupTheory/Subgroup/MulOpposite.lean
@@ -24,47 +24,45 @@ variable {G : Type _} [Group G]
 namespace Subgroup
 
 /-- A subgroup `H` of `G` determines a subgroup `H.opposite` of the opposite group `Gᵐᵒᵖ`. -/
-@[to_additive
-      "An additive subgroup `H` of `G` determines an additive subgroup `H.opposite` of the\n  opposite additive group `Gᵃᵒᵖ`."]
+@[to_additive "An additive subgroup `H` of `G` determines an additive subgroup `H.opposite` of the
+ opposite additive group `Gᵃᵒᵖ`."]
 def opposite : Subgroup G ≃ Subgroup Gᵐᵒᵖ
     where
   toFun H :=
     { carrier := MulOpposite.unop ⁻¹' (H : Set G)
       one_mem' := H.one_mem
-      mul_mem' := fun a b ha hb => H.mul_mem hb ha
-      inv_mem' := fun a => H.inv_mem }
+      mul_mem' := fun ha hb => H.mul_mem hb ha
+      inv_mem' := H.inv_mem }
   invFun H :=
     { carrier := MulOpposite.op ⁻¹' (H : Set Gᵐᵒᵖ)
       one_mem' := H.one_mem
-      mul_mem' := fun a b ha hb => H.mul_mem hb ha
-      inv_mem' := fun a => H.inv_mem }
-  left_inv H := SetLike.coe_injective rfl
-  right_inv H := SetLike.coe_injective rfl
+      mul_mem' := fun ha hb => H.mul_mem hb ha
+      inv_mem' := H.inv_mem }
+  left_inv _ := SetLike.coe_injective rfl
+  right_inv _ := SetLike.coe_injective rfl
 #align subgroup.opposite Subgroup.opposite
 #align add_subgroup.opposite AddSubgroup.opposite
 
 /-- Bijection between a subgroup `H` and its opposite. -/
 @[to_additive "Bijection between an additive subgroup `H` and its opposite.", simps]
-def oppositeEquiv (H : Subgroup G) : H ≃ H.opposite :=
+def oppositeEquiv (H : Subgroup G) : H ≃ opposite H :=
   MulOpposite.opEquiv.subtypeEquiv fun _ => Iff.rfl
 #align subgroup.opposite_equiv Subgroup.oppositeEquiv
 #align add_subgroup.opposite_equiv AddSubgroup.oppositeEquiv
 
 @[to_additive]
-instance (H : Subgroup G) [Encodable H] : Encodable H.opposite :=
+instance (H : Subgroup G) [Encodable H] : Encodable (opposite H) :=
   Encodable.ofEquiv H H.oppositeEquiv.symm
 
 @[to_additive]
-instance (H : Subgroup G) [Countable H] : Countable H.opposite :=
+instance (H : Subgroup G) [Countable H] : Countable (opposite H) :=
   Countable.of_equiv H H.oppositeEquiv
 
 @[to_additive]
-theorem smul_opposite_mul {H : Subgroup G} (x g : G) (h : H.opposite) : h • (g * x) = g * h • x :=
-  by
-  cases h
-  simp [(· • ·), mul_assoc]
+theorem smul_opposite_mul {H : Subgroup G} (x g : G) (h : opposite H) :
+    h • (g * x) = g * h • x :=
+  mul_assoc _ _ _
 #align subgroup.smul_opposite_mul Subgroup.smul_opposite_mul
 #align add_subgroup.vadd_opposite_add AddSubgroup.vadd_opposite_add
 
 end Subgroup
-
