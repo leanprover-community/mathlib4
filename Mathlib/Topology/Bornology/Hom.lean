28a124a43d26c32c0272083c67fd1cac428f14f4
Jireh Loreaux <loreaujy@gmail.com>
2023-01-26 09:33:36 -0600

    make all changes

--- Mathlib/Topology/Bornology/Hom.lean
+++ Mathlib/Topology/Bornology/Hom.lean
@@ -15,16 +15,16 @@ import Mathlib.Topology.Bornology.Basic
 
 This file defines locally bounded maps between bornologies.
 
-We use the `fun_like` design, so each type of morphisms has a companion typeclass which is meant to
+We use the `FunLike` design, so each type of morphisms has a companion typeclass which is meant to
 be satisfied by itself and all stricter types.
 
 ## Types of morphisms
 
-* `locally_bounded_map`: Locally bounded maps. Maps which preserve boundedness.
+* `LocallyBoundedMap`: Locally bounded maps. Maps which preserve boundedness.
 
 ## Typeclasses
 
-* `locally_bounded_map_class`
+* `LocallyBoundedMapClass`
 -/
 
 
@@ -34,17 +34,22 @@ variable {F α β γ δ : Type _}
 
 /-- The type of bounded maps from `α` to `β`, the maps which send a bounded set to a bounded set. -/
 structure LocallyBoundedMap (α β : Type _) [Bornology α] [Bornology β] where
+  /-- The function underlying a locally bounded map -/
   toFun : α → β
-  comap_cobounded_le' : (cobounded β).comap to_fun ≤ cobounded α
+  /-- The pullback of the `Bornology.cobounded` filter under the function is contained in the
+  cobounded filter. Equivalently, the function maps bounded sets to bounded sets. -/
+  comap_cobounded_le' : (cobounded β).comap toFun ≤ cobounded α
 #align locally_bounded_map LocallyBoundedMap
 
 section
 
-/-- `locally_bounded_map_class F α β` states that `F` is a type of bounded maps.
+/-- `LocallyBoundedMapClass F α β` states that `F` is a type of bounded maps.
 
-You should extend this class when you extend `locally_bounded_map`. -/
+You should extend this class when you extend `LocallyBoundedMap`. -/
 class LocallyBoundedMapClass (F : Type _) (α β : outParam <| Type _) [Bornology α]
-  [Bornology β] extends FunLike F α fun _ => β where
+    [Bornology β] extends FunLike F α fun _ => β where
+  /-- The pullback of the `Bornology.cobounded` filter under the function is contained in the
+  cobounded filter. Equivalently, the function maps bounded sets to bounded sets. -/
   comap_cobounded_le (f : F) : (cobounded β).comap f ≤ cobounded α
 #align locally_bounded_map_class LocallyBoundedMapClass
 
@@ -57,6 +62,15 @@ theorem IsBounded.image [Bornology α] [Bornology β] [LocallyBoundedMapClass F 
   comap_cobounded_le_iff.1 (comap_cobounded_le f) hs
 #align is_bounded.image IsBounded.image
 
+/-- Turn an element of a type `F` satisfying `LocallyBoundedMapClass F α β` into an actual
+`LocallyBoundedMap`. This is declared as the default coercion from `F` to
+`LocallyBoundedMap α β`. -/
+@[coe]
+def LocallyBoundedMapClass.toLocallyBoundedMap [Bornology α] [Bornology β]
+    [LocallyBoundedMapClass F α β] (f : F) : LocallyBoundedMap α β where
+  toFun := f
+  comap_cobounded_le' := comap_cobounded_le f
+
 instance [Bornology α] [Bornology β] [LocallyBoundedMapClass F α β] :
     CoeTC F (LocallyBoundedMap α β) :=
   ⟨fun f => ⟨f, comap_cobounded_le f⟩⟩
@@ -65,8 +79,7 @@ namespace LocallyBoundedMap
 
 variable [Bornology α] [Bornology β] [Bornology γ] [Bornology δ]
 
-instance : LocallyBoundedMapClass (LocallyBoundedMap α β) α β
-    where
+instance : LocallyBoundedMapClass (LocallyBoundedMap α β) α β where
   coe f := f.toFun
   coe_injective' f g h := by
     cases f
@@ -74,22 +87,21 @@ instance : LocallyBoundedMapClass (LocallyBoundedMap α β) α β
     congr
   comap_cobounded_le f := f.comap_cobounded_le'
 
-/-- Helper instance for when there's too many metavariables to apply `fun_like.has_coe_to_fun`
-directly. -/
-instance : CoeFun (LocallyBoundedMap α β) fun _ => α → β :=
-  FunLike.hasCoeToFun
+/- omitting helper instance because it is not needed in Lean 4.
+/-- Helper instance for when there's too many metavariables to apply the coercion via `FunLike`
+directly.
+instance : CoeFun (LocallyBoundedMap α β) fun _ => α → β where
+  coe := LocallyBoundedMap.toFun -/ -/
 
-@[simp]
-theorem toFun_eq_coe {f : LocallyBoundedMap α β} : f.toFun = (f : α → β) :=
-  rfl
-#align locally_bounded_map.to_fun_eq_coe LocallyBoundedMap.toFun_eq_coe
+-- porting note: syntactic tautology because of the way coercions work
+#noalign locally_bounded_map.to_fun_eq_coe
 
 @[ext]
 theorem ext {f g : LocallyBoundedMap α β} (h : ∀ a, f a = g a) : f = g :=
   FunLike.ext f g h
 #align locally_bounded_map.ext LocallyBoundedMap.ext
 
-/-- Copy of a `locally_bounded_map` with a new `to_fun` equal to the old one. Useful to fix
+/-- Copy of a `LocallyBoundedMap` with a new `toFun` equal to the old one. Useful to fix
 definitional equalities. -/
 protected def copy (f : LocallyBoundedMap α β) (f' : α → β) (h : f' = f) : LocallyBoundedMap α β :=
   ⟨f', h.symm ▸ f.comap_cobounded_le'⟩
@@ -104,10 +116,12 @@ theorem copy_eq (f : LocallyBoundedMap α β) (f' : α → β) (h : f' = f) : f.
   FunLike.ext' h
 #align locally_bounded_map.copy_eq LocallyBoundedMap.copy_eq
 
-/-- Construct a `locally_bounded_map` from the fact that the function maps bounded sets to bounded
+/-- Construct a `LocallyBoundedMap` from the fact that the function maps bounded sets to bounded
 sets. -/
-def ofMapBounded (f : α → β) (h) : LocallyBoundedMap α β :=
+def ofMapBounded (f : α → β) (h : ∀ ⦃s : Set α⦄, IsBounded s → IsBounded (f '' s)) :
+    LocallyBoundedMap α β :=
   ⟨f, comap_cobounded_le_iff.2 h⟩
+-- porting note: I had to provide the type of `h` explicitly.
 #align locally_bounded_map.of_map_bounded LocallyBoundedMap.ofMapBounded
 
 @[simp]
@@ -122,7 +136,7 @@ theorem ofMapBounded_apply (f : α → β) {h} (a : α) : ofMapBounded f h a = f
 
 variable (α)
 
-/-- `id` as a `locally_bounded_map`. -/
+/-- `id` as a `LocallyBoundedMap`. -/
 protected def id : LocallyBoundedMap α α :=
   ⟨id, comap_id.le⟩
 #align locally_bounded_map.id LocallyBoundedMap.id
@@ -142,7 +156,7 @@ theorem id_apply (a : α) : LocallyBoundedMap.id α a = a :=
   rfl
 #align locally_bounded_map.id_apply LocallyBoundedMap.id_apply
 
-/-- Composition of `locally_bounded_map`s as a `locally_bounded_map`. -/
+/-- Composition of `LocallyBoundedMap`s as a `LocallyBoundedMap`. -/
 def comp (f : LocallyBoundedMap β γ) (g : LocallyBoundedMap α β) : LocallyBoundedMap α γ
     where
   toFun := f ∘ g
@@ -169,17 +183,18 @@ theorem comp_assoc (f : LocallyBoundedMap γ δ) (g : LocallyBoundedMap β γ)
 
 @[simp]
 theorem comp_id (f : LocallyBoundedMap α β) : f.comp (LocallyBoundedMap.id α) = f :=
-  ext fun a => rfl
+  ext fun _ => rfl
 #align locally_bounded_map.comp_id LocallyBoundedMap.comp_id
 
 @[simp]
 theorem id_comp (f : LocallyBoundedMap α β) : (LocallyBoundedMap.id β).comp f = f :=
-  ext fun a => rfl
+  ext fun _ => rfl
 #align locally_bounded_map.id_comp LocallyBoundedMap.id_comp
 
 theorem cancel_right {g₁ g₂ : LocallyBoundedMap β γ} {f : LocallyBoundedMap α β}
     (hf : Surjective f) : g₁.comp f = g₂.comp f ↔ g₁ = g₂ :=
-  ⟨fun h => ext <| hf.forall.2 <| FunLike.ext_iff.1 h, congr_arg _⟩
+  ⟨fun h => ext <| hf.forall.2 <| FunLike.ext_iff.1 h, congrArg (fun x => comp x f)⟩
+-- porting note: unification was not strong enough to do `congrArg _`.
 #align locally_bounded_map.cancel_right LocallyBoundedMap.cancel_right
 
 theorem cancel_left {g : LocallyBoundedMap β γ} {f₁ f₂ : LocallyBoundedMap α β} (hg : Injective g) :
@@ -188,4 +203,3 @@ theorem cancel_left {g : LocallyBoundedMap β γ} {f₁ f₂ : LocallyBoundedMap
 #align locally_bounded_map.cancel_left LocallyBoundedMap.cancel_left
 
 end LocallyBoundedMap
-
