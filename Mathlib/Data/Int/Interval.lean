ae71b680f2504179f4467a7e183a695d65a08ce1
Jireh Loreaux <loreaujy@gmail.com>
2023-01-26 16:33:24 -0600

    get it compiling

--- Mathlib/Data/Int/Interval.lean
+++ Mathlib/Data/Int/Interval.lean
@@ -34,165 +34,166 @@ instance : LocallyFiniteOrder ℤ
   finset_mem_Icc a b x :=
     by
     simp_rw [mem_map, exists_prop, mem_range, Int.lt_toNat, Function.Embedding.trans_apply,
-      Nat.castEmbedding_apply, add_left_embedding_apply]
+      Nat.castEmbedding_apply, addLeftEmbedding_apply]
     constructor
     · rintro ⟨a, h, rfl⟩
       rw [lt_sub_iff_add_lt, Int.lt_add_one_iff, add_comm] at h
-      exact ⟨Int.le.intro rfl, h⟩
+      exact ⟨Int.le.intro a rfl, h⟩
     · rintro ⟨ha, hb⟩
       use (x - a).toNat
       rw [← lt_add_one_iff] at hb
-      rw [to_nat_sub_of_le ha]
+      rw [toNat_sub_of_le ha]
       exact ⟨sub_lt_sub_right hb _, add_sub_cancel'_right _ _⟩
   finset_mem_Ico a b x :=
     by
     simp_rw [mem_map, exists_prop, mem_range, Int.lt_toNat, Function.Embedding.trans_apply,
-      Nat.castEmbedding_apply, add_left_embedding_apply]
+      Nat.castEmbedding_apply, addLeftEmbedding_apply]
     constructor
     · rintro ⟨a, h, rfl⟩
-      exact ⟨Int.le.intro rfl, lt_sub_iff_add_lt'.mp h⟩
+      exact ⟨Int.le.intro a rfl, lt_sub_iff_add_lt'.mp h⟩
     · rintro ⟨ha, hb⟩
       use (x - a).toNat
-      rw [to_nat_sub_of_le ha]
+      rw [toNat_sub_of_le ha]
       exact ⟨sub_lt_sub_right hb _, add_sub_cancel'_right _ _⟩
   finset_mem_Ioc a b x :=
     by
     simp_rw [mem_map, exists_prop, mem_range, Int.lt_toNat, Function.Embedding.trans_apply,
-      Nat.castEmbedding_apply, add_left_embedding_apply]
+      Nat.castEmbedding_apply, addLeftEmbedding_apply]
     constructor
     · rintro ⟨a, h, rfl⟩
       rw [← add_one_le_iff, le_sub_iff_add_le', add_comm _ (1 : ℤ), ← add_assoc] at h
-      exact ⟨Int.le.intro rfl, h⟩
+      exact ⟨Int.le.intro a rfl, h⟩
     · rintro ⟨ha, hb⟩
       use (x - (a + 1)).toNat
-      rw [to_nat_sub_of_le ha, ← add_one_le_iff, sub_add, add_sub_cancel]
+      rw [toNat_sub_of_le ha, ← add_one_le_iff, sub_add, add_sub_cancel]
       exact ⟨sub_le_sub_right hb _, add_sub_cancel'_right _ _⟩
   finset_mem_Ioo a b x :=
     by
     simp_rw [mem_map, exists_prop, mem_range, Int.lt_toNat, Function.Embedding.trans_apply,
-      Nat.castEmbedding_apply, add_left_embedding_apply]
+      Nat.castEmbedding_apply, addLeftEmbedding_apply]
     constructor
     · rintro ⟨a, h, rfl⟩
       rw [sub_sub, lt_sub_iff_add_lt'] at h
-      exact ⟨Int.le.intro rfl, h⟩
+      exact ⟨Int.le.intro a rfl, h⟩
     · rintro ⟨ha, hb⟩
       use (x - (a + 1)).toNat
-      rw [to_nat_sub_of_le ha, sub_sub]
+      rw [toNat_sub_of_le ha, sub_sub]
       exact ⟨sub_lt_sub_right hb _, add_sub_cancel'_right _ _⟩
 
 namespace Int
 
 variable (a b : ℤ)
 
-theorem icc_eq_finset_map :
+theorem Icc_eq_finset_map :
     Icc a b =
       (Finset.range (b + 1 - a).toNat).map (Nat.castEmbedding.trans <| addLeftEmbedding a) :=
   rfl
-#align int.Icc_eq_finset_map Int.icc_eq_finset_map
+#align int.Icc_eq_finset_map Int.Icc_eq_finset_map
 
-theorem ico_eq_finset_map :
+theorem Ico_eq_finset_map :
     Ico a b = (Finset.range (b - a).toNat).map (Nat.castEmbedding.trans <| addLeftEmbedding a) :=
   rfl
-#align int.Ico_eq_finset_map Int.ico_eq_finset_map
+#align int.Ico_eq_finset_map Int.Ico_eq_finset_map
 
-theorem ioc_eq_finset_map :
+theorem Ioc_eq_finset_map :
     Ioc a b =
       (Finset.range (b - a).toNat).map (Nat.castEmbedding.trans <| addLeftEmbedding (a + 1)) :=
   rfl
-#align int.Ioc_eq_finset_map Int.ioc_eq_finset_map
+#align int.Ioc_eq_finset_map Int.Ioc_eq_finset_map
 
-theorem ioo_eq_finset_map :
+theorem Ioo_eq_finset_map :
     Ioo a b =
       (Finset.range (b - a - 1).toNat).map (Nat.castEmbedding.trans <| addLeftEmbedding (a + 1)) :=
   rfl
-#align int.Ioo_eq_finset_map Int.ioo_eq_finset_map
+#align int.Ioo_eq_finset_map Int.Ioo_eq_finset_map
 
 @[simp]
-theorem card_icc : (Icc a b).card = (b + 1 - a).toNat := by
+theorem card_Icc : (Icc a b).card = (b + 1 - a).toNat := by
   change (Finset.map _ _).card = _
   rw [Finset.card_map, Finset.card_range]
-#align int.card_Icc Int.card_icc
+#align int.card_Icc Int.card_Icc
 
 @[simp]
-theorem card_ico : (Ico a b).card = (b - a).toNat := by
+theorem card_Ico : (Ico a b).card = (b - a).toNat := by
   change (Finset.map _ _).card = _
   rw [Finset.card_map, Finset.card_range]
-#align int.card_Ico Int.card_ico
+#align int.card_Ico Int.card_Ico
 
 @[simp]
-theorem card_ioc : (Ioc a b).card = (b - a).toNat := by
+theorem card_Ioc : (Ioc a b).card = (b - a).toNat := by
   change (Finset.map _ _).card = _
   rw [Finset.card_map, Finset.card_range]
-#align int.card_Ioc Int.card_ioc
+#align int.card_Ioc Int.card_Ioc
 
 @[simp]
-theorem card_ioo : (Ioo a b).card = (b - a - 1).toNat := by
+theorem card_Ioo : (Ioo a b).card = (b - a - 1).toNat := by
   change (Finset.map _ _).card = _
   rw [Finset.card_map, Finset.card_range]
-#align int.card_Ioo Int.card_ioo
+#align int.card_Ioo Int.card_Ioo
 
-theorem card_icc_of_le (h : a ≤ b + 1) : ((Icc a b).card : ℤ) = b + 1 - a := by
-  rw [card_Icc, to_nat_sub_of_le h]
-#align int.card_Icc_of_le Int.card_icc_of_le
+theorem card_Icc_of_le (h : a ≤ b + 1) : ((Icc a b).card : ℤ) = b + 1 - a := by
+  rw [card_Icc, toNat_sub_of_le h]
+#align int.card_Icc_of_le Int.card_Icc_of_le
 
-theorem card_ico_of_le (h : a ≤ b) : ((Ico a b).card : ℤ) = b - a := by
-  rw [card_Ico, to_nat_sub_of_le h]
-#align int.card_Ico_of_le Int.card_ico_of_le
+theorem card_Ico_of_le (h : a ≤ b) : ((Ico a b).card : ℤ) = b - a := by
+  rw [card_Ico, toNat_sub_of_le h]
+#align int.card_Ico_of_le Int.card_Ico_of_le
 
-theorem card_ioc_of_le (h : a ≤ b) : ((Ioc a b).card : ℤ) = b - a := by
-  rw [card_Ioc, to_nat_sub_of_le h]
-#align int.card_Ioc_of_le Int.card_ioc_of_le
+theorem card_Ioc_of_le (h : a ≤ b) : ((Ioc a b).card : ℤ) = b - a := by
+  rw [card_Ioc, toNat_sub_of_le h]
+#align int.card_Ioc_of_le Int.card_Ioc_of_le
 
-theorem card_ioo_of_lt (h : a < b) : ((Ioo a b).card : ℤ) = b - a - 1 := by
-  rw [card_Ioo, sub_sub, to_nat_sub_of_le h]
-#align int.card_Ioo_of_lt Int.card_ioo_of_lt
+theorem card_Ioo_of_lt (h : a < b) : ((Ioo a b).card : ℤ) = b - a - 1 := by
+  rw [card_Ioo, sub_sub, toNat_sub_of_le h]
+#align int.card_Ioo_of_lt Int.card_Ioo_of_lt
 
 @[simp]
-theorem card_fintypeIcc : Fintype.card (Set.Icc a b) = (b + 1 - a).toNat := by
+theorem card_fintype_Icc : Fintype.card (Set.Icc a b) = (b + 1 - a).toNat := by
   rw [← card_Icc, Fintype.card_ofFinset]
-#align int.card_fintype_Icc Int.card_fintypeIcc
+#align int.card_fintype_Icc Int.card_fintype_Icc
 
 @[simp]
-theorem card_fintypeIco : Fintype.card (Set.Ico a b) = (b - a).toNat := by
+theorem card_fintype_Ico : Fintype.card (Set.Ico a b) = (b - a).toNat := by
   rw [← card_Ico, Fintype.card_ofFinset]
-#align int.card_fintype_Ico Int.card_fintypeIco
+#align int.card_fintype_Ico Int.card_fintype_Ico
 
 @[simp]
-theorem card_fintypeIoc : Fintype.card (Set.Ioc a b) = (b - a).toNat := by
+theorem card_fintype_Ioc : Fintype.card (Set.Ioc a b) = (b - a).toNat := by
   rw [← card_Ioc, Fintype.card_ofFinset]
-#align int.card_fintype_Ioc Int.card_fintypeIoc
+#align int.card_fintype_Ioc Int.card_fintype_Ioc
 
 @[simp]
-theorem card_fintypeIoo : Fintype.card (Set.Ioo a b) = (b - a - 1).toNat := by
+theorem card_fintype_Ioo : Fintype.card (Set.Ioo a b) = (b - a - 1).toNat := by
   rw [← card_Ioo, Fintype.card_ofFinset]
-#align int.card_fintype_Ioo Int.card_fintypeIoo
+#align int.card_fintype_Ioo Int.card_fintype_Ioo
 
-theorem card_fintypeIcc_of_le (h : a ≤ b + 1) : (Fintype.card (Set.Icc a b) : ℤ) = b + 1 - a := by
-  rw [card_fintype_Icc, to_nat_sub_of_le h]
-#align int.card_fintype_Icc_of_le Int.card_fintypeIcc_of_le
+theorem card_fintype_Icc_of_le (h : a ≤ b + 1) : (Fintype.card (Set.Icc a b) : ℤ) = b + 1 - a := by
+  rw [card_fintype_Icc, toNat_sub_of_le h]
+#align int.card_fintype_Icc_of_le Int.card_fintype_Icc_of_le
 
-theorem card_fintypeIco_of_le (h : a ≤ b) : (Fintype.card (Set.Ico a b) : ℤ) = b - a := by
-  rw [card_fintype_Ico, to_nat_sub_of_le h]
-#align int.card_fintype_Ico_of_le Int.card_fintypeIco_of_le
+theorem card_fintype_Ico_of_le (h : a ≤ b) : (Fintype.card (Set.Ico a b) : ℤ) = b - a := by
+  rw [card_fintype_Ico, toNat_sub_of_le h]
+#align int.card_fintype_Ico_of_le Int.card_fintype_Ico_of_le
 
-theorem card_fintypeIoc_of_le (h : a ≤ b) : (Fintype.card (Set.Ioc a b) : ℤ) = b - a := by
-  rw [card_fintype_Ioc, to_nat_sub_of_le h]
-#align int.card_fintype_Ioc_of_le Int.card_fintypeIoc_of_le
+theorem card_fintype_Ioc_of_le (h : a ≤ b) : (Fintype.card (Set.Ioc a b) : ℤ) = b - a := by
+  rw [card_fintype_Ioc, toNat_sub_of_le h]
+#align int.card_fintype_Ioc_of_le Int.card_fintype_Ioc_of_le
 
-theorem card_fintypeIoo_of_lt (h : a < b) : (Fintype.card (Set.Ioo a b) : ℤ) = b - a - 1 := by
-  rw [card_fintype_Ioo, sub_sub, to_nat_sub_of_le h]
-#align int.card_fintype_Ioo_of_lt Int.card_fintypeIoo_of_lt
+theorem card_fintype_Ioo_of_lt (h : a < b) : (Fintype.card (Set.Ioo a b) : ℤ) = b - a - 1 := by
+  rw [card_fintype_Ioo, sub_sub, toNat_sub_of_le h]
+#align int.card_fintype_Ioo_of_lt Int.card_fintype_Ioo_of_lt
 
-theorem image_ico_mod (n a : ℤ) (h : 0 ≤ a) : (Ico n (n + a)).image (· % a) = Ico 0 a := by
+theorem image_Ico_emod (n a : ℤ) (h : 0 ≤ a) : (Ico n (n + a)).image (· % a) = Ico 0 a := by
   obtain rfl | ha := eq_or_lt_of_le h
   · simp
   ext i
   simp only [mem_image, exists_prop, mem_range, mem_Ico]
   constructor
-  · rintro ⟨i, h, rfl⟩
-    exact ⟨mod_nonneg i (ne_of_gt ha), mod_lt_of_pos i ha⟩
+  · rintro ⟨i, _, rfl⟩
+    have := ha.ne'
+    exact ⟨emod_nonneg i ha.ne', emod_lt_of_pos i ha⟩
   intro hia
-  have hn := Int.mod_add_div n a
+  have hn := Int.emod_add_ediv n a
   obtain hi | hi := lt_or_le i (n % a)
   · refine' ⟨i + a * (n / a + 1), ⟨_, _⟩, _⟩
     · rw [add_comm (n / a), mul_add, mul_one, ← add_assoc]
@@ -208,7 +209,6 @@ theorem image_ico_mod (n a : ℤ) (h : 0 ≤ a) : (Ico n (n + a)).image (· % a)
       simp only [zero_le, le_add_iff_nonneg_left]
       exact Int.emod_nonneg n (ne_of_gt ha)
     · rw [Int.add_mul_emod_self_left, Int.emod_eq_of_lt hia.left hia.right]
-#align int.image_Ico_mod Int.image_ico_mod
+#align int.image_Ico_mod Int.image_Ico_emod
 
 end Int
-
