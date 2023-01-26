dd0b3ae28f7b9dfe6a20b827ef7f4d67f3df7299
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2023-01-26 16:39:52 +0000

    feat: port GroupTheory.GroupAction.Basic (#1845)

--- Mathlib.lean
+++ Mathlib.lean
@@ -546,6 +546,7 @@ import Mathlib.Dynamics.FixedPoints.Basic
 import Mathlib.GroupTheory.Archimedean
 import Mathlib.GroupTheory.Congruence
 import Mathlib.GroupTheory.EckmannHilton
+import Mathlib.GroupTheory.GroupAction.Basic
 import Mathlib.GroupTheory.GroupAction.BigOperators
 import Mathlib.GroupTheory.GroupAction.Defs
 import Mathlib.GroupTheory.GroupAction.Embedding
