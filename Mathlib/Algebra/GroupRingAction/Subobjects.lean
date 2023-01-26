3c4e56a4e563d7e26d513f1632f17b71a83745b4
ChrisHughes24 <chrishughes24@gmail.com>
2023-01-26 12:50:40 +0000

    automated fixes
    Mathbin -> Mathlib
    
    fix certain import statements
    
    move "by" to end of line
    
    add import to Mathlib.lean

--- Mathlib/Algebra/GroupRingAction/Subobjects.lean
+++ Mathlib/Algebra/GroupRingAction/Subobjects.lean
@@ -8,8 +8,8 @@ Authors: Eric Wieser
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
-import Mathbin.Algebra.GroupRingAction.Basic
-import Mathbin.GroupTheory.Subgroup.Basic
+import Mathlib.Algebra.GroupRingAction.Basic
+import Mathlib.GroupTheory.Subgroup.Basic
 
 /-!
 # Instances of `mul_semiring_action` for subobjects
