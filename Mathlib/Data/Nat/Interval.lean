f8cc07f8fd09584954fdf450237ca0a77e8052a5
Jon Eugster <eugster.jon@gmail.com>
2023-01-26 03:07:43 +0100

    script output

--- Mathlib/Data/Nat/Interval.lean
+++ Mathlib/Data/Nat/Interval.lean
@@ -8,7 +8,7 @@ Authors: Yaël Dillies
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
-import Mathbin.Data.Finset.LocallyFinite
+import Mathlib.Data.Finset.LocallyFinite
 
 /-!
 # Finite intervals of naturals
