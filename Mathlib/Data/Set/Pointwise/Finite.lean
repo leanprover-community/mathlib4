befeb2c41aa3dba247ebb53b365dbc6d82087314
int-y1 <jason_yuen2007@hotmail.com>
2023-01-26 03:13:44 -0800

    simple fixes 2

--- Mathlib/Data/Set/Pointwise/Finite.lean
+++ Mathlib/Data/Set/Pointwise/Finite.lean
@@ -57,7 +57,7 @@ variable [Monoid α] {s t : Set α}
 
 @[to_additive]
 instance decidableMemMul [Fintype α] [DecidableEq α] [DecidablePred (· ∈ s)]
-    [DecidablePred (· ∈ t)] : DecidablePred (· ∈ s * t) := fun _ => decidable_of_iff _ mem_mul.symm
+    [DecidablePred (· ∈ t)] : DecidablePred (· ∈ s * t) := fun _ ↦ decidable_of_iff _ mem_mul.symm
 #align set.decidable_mem_mul Set.decidableMemMul
 #align set.decidable_mem_add Set.decidableMemAdd
 
@@ -122,28 +122,28 @@ theorem card_pow_eq_card_pow_card_univ [∀ k : ℕ, DecidablePred (· ∈ S ^ k
     ∀ k, Fintype.card G ≤ k → Fintype.card (↥(S ^ k)) = Fintype.card (↥(S ^ Fintype.card G)) := by
   have hG : 0 < Fintype.card G := Fintype.card_pos_iff.mpr ⟨1⟩
   by_cases hS : S = ∅
-  · refine' fun k hk => Fintype.card_congr _
+  · refine' fun k hk ↦ Fintype.card_congr _
     rw [hS, empty_pow (ne_of_gt (lt_of_lt_of_le hG hk)), empty_pow (ne_of_gt hG)]
   obtain ⟨a, ha⟩ := Set.nonempty_iff_ne_empty.2 hS
   classical!
   have key : ∀ (a) (s t : Set G), (∀ b : G, b ∈ s → a * b ∈ t) → Fintype.card s ≤ Fintype.card t :=
     by
-    refine' fun a s t h => Fintype.card_le_of_injective (fun ⟨b, hb⟩ => ⟨a * b, h b hb⟩) _
+    refine' fun a s t h ↦ Fintype.card_le_of_injective (fun ⟨b, hb⟩ ↦ ⟨a * b, h b hb⟩) _
     rintro ⟨b, hb⟩ ⟨c, hc⟩ hbc
     exact Subtype.ext (mul_left_cancel (Subtype.ext_iff.mp hbc))
-  have mono : Monotone (fun n => Fintype.card (↥(S ^ n)) : ℕ → ℕ) :=
-    monotone_nat_of_le_succ fun n => key a _ _ fun b hb => Set.mul_mem_mul ha hb
+  have mono : Monotone (fun n ↦ Fintype.card (↥(S ^ n)) : ℕ → ℕ) :=
+    monotone_nat_of_le_succ fun n ↦ key a _ _ fun b hb ↦ Set.mul_mem_mul ha hb
   convert
-    card_pow_eq_card_pow_card_univ_aux mono (fun n => set_fintype_card_le_univ (S ^ n)) fun n h =>
+    card_pow_eq_card_pow_card_univ_aux mono (fun n ↦ set_fintype_card_le_univ (S ^ n)) fun n h ↦
       le_antisymm (mono (n + 1).le_succ) (key a⁻¹ _ _ _)
   · simp only [[anonymous], Fintype.card_ofFinset]
   replace h : {a} * S ^ n = S ^ (n + 1)
   · refine' Set.eq_of_subset_of_card_le _ (le_trans (ge_of_eq h) _)
-    · exact mul_subset_mul (set.singleton_subset_iff.mpr ha) Set.Subset.rfl
-    · convert key a (S ^ n) ({a} * S ^ n) fun b hb => Set.mul_mem_mul (Set.mem_singleton a) hb
+    · exact mul_subset_mul (Set.singleton_subset_iff.mpr ha) Set.Subset.rfl
+    · convert key a (S ^ n) ({a} * S ^ n) fun b hb ↦ Set.mul_mem_mul (Set.mem_singleton a) hb
   rw [pow_succ', ← h, mul_assoc, ← pow_succ', h]
   rintro _ ⟨b, c, hb, hc, rfl⟩
-  rwa [set.mem_singleton_iff.mp hb, inv_mul_cancel_left]
+  rwa [Set.mem_singleton_iff.mp hb, inv_mul_cancel_left]
 #align group.card_pow_eq_card_pow_card_univ Group.card_pow_eq_card_pow_card_univ
 #align add_group.card_nsmul_eq_card_nsmul_card_univ AddGroup.card_nsmul_eq_card_nsmul_card_univ
 
