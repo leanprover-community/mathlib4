9adc7dd7aca0749b2fb22e6cab7f1f781c9e07c7
Jireh Loreaux <loreaujy@gmail.com>
2023-01-27 16:56:08 -0600

    fix all

--- Mathlib/Data/PNat/Interval.lean
+++ Mathlib/Data/PNat/Interval.lean
@@ -14,7 +14,7 @@ import Mathlib.Data.PNat.Defs
 /-!
 # Finite intervals of positive naturals
 
-This file proves that `ℕ+` is a `locally_finite_order` and calculates the cardinality of its
+This file proves that `ℕ+` is a `LocallyFiniteOrder` and calculates the cardinality of its
 intervals as finsets and fintypes.
 -/
 
@@ -22,83 +22,94 @@ intervals as finsets and fintypes.
 open Finset PNat
 
 instance : LocallyFiniteOrder ℕ+ :=
-  Subtype.locallyFiniteOrder _
+  instLocallyFiniteOrderSubtypePreorder _
 
 namespace PNat
 
 variable (a b : ℕ+)
 
-theorem icc_eq_finset_subtype : Icc a b = (Icc (a : ℕ) b).Subtype fun n : ℕ => 0 < n :=
+theorem Icc_eq_finset_subtype : Icc a b = (Icc (a : ℕ) b).subtype fun n : ℕ => 0 < n :=
   rfl
-#align pnat.Icc_eq_finset_subtype PNat.icc_eq_finset_subtype
+#align pnat.Icc_eq_finset_subtype PNat.Icc_eq_finset_subtype
 
-theorem ico_eq_finset_subtype : Ico a b = (Ico (a : ℕ) b).Subtype fun n : ℕ => 0 < n :=
+theorem Ico_eq_finset_subtype : Ico a b = (Ico (a : ℕ) b).subtype fun n : ℕ => 0 < n :=
   rfl
-#align pnat.Ico_eq_finset_subtype PNat.ico_eq_finset_subtype
+#align pnat.Ico_eq_finset_subtype PNat.Ico_eq_finset_subtype
 
-theorem ioc_eq_finset_subtype : Ioc a b = (Ioc (a : ℕ) b).Subtype fun n : ℕ => 0 < n :=
+theorem Ioc_eq_finset_subtype : Ioc a b = (Ioc (a : ℕ) b).subtype fun n : ℕ => 0 < n :=
   rfl
-#align pnat.Ioc_eq_finset_subtype PNat.ioc_eq_finset_subtype
+#align pnat.Ioc_eq_finset_subtype PNat.Ioc_eq_finset_subtype
 
-theorem ioo_eq_finset_subtype : Ioo a b = (Ioo (a : ℕ) b).Subtype fun n : ℕ => 0 < n :=
+theorem Ioo_eq_finset_subtype : Ioo a b = (Ioo (a : ℕ) b).subtype fun n : ℕ => 0 < n :=
   rfl
-#align pnat.Ioo_eq_finset_subtype PNat.ioo_eq_finset_subtype
+#align pnat.Ioo_eq_finset_subtype PNat.Ioo_eq_finset_subtype
 
-theorem map_subtype_embedding_icc : (Icc a b).map (Function.Embedding.subtype _) = Icc (a : ℕ) b :=
-  map_subtype_embedding_Icc _ _ _ fun c _ x hx _ hc _ => hc.trans_le hx
-#align pnat.map_subtype_embedding_Icc PNat.map_subtype_embedding_icc
+theorem map_subtype_embedding_Icc : (Icc a b).map (Function.Embedding.subtype _) = Icc (a : ℕ) b :=
+  Finset.map_subtype_embedding_Icc _ _ _ fun _c _ _x hx _ hc _ => hc.trans_le hx
+#align pnat.map_subtype_embedding_Icc PNat.map_subtype_embedding_Icc
 
-theorem map_subtype_embedding_ico : (Ico a b).map (Function.Embedding.subtype _) = Ico (a : ℕ) b :=
-  map_subtype_embedding_Ico _ _ _ fun c _ x hx _ hc _ => hc.trans_le hx
-#align pnat.map_subtype_embedding_Ico PNat.map_subtype_embedding_ico
+theorem map_subtype_embedding_Ico : (Ico a b).map (Function.Embedding.subtype _) = Ico (a : ℕ) b :=
+  Finset.map_subtype_embedding_Ico _ _ _ fun _c _ _x hx _ hc _ => hc.trans_le hx
+#align pnat.map_subtype_embedding_Ico PNat.map_subtype_embedding_Ico
 
-theorem map_subtype_embedding_ioc : (Ioc a b).map (Function.Embedding.subtype _) = Ioc (a : ℕ) b :=
-  map_subtype_embedding_Ioc _ _ _ fun c _ x hx _ hc _ => hc.trans_le hx
-#align pnat.map_subtype_embedding_Ioc PNat.map_subtype_embedding_ioc
+theorem map_subtype_embedding_Ioc : (Ioc a b).map (Function.Embedding.subtype _) = Ioc (a : ℕ) b :=
+  Finset.map_subtype_embedding_Ioc _ _ _ fun _c _ _x hx _ hc _ => hc.trans_le hx
+#align pnat.map_subtype_embedding_Ioc PNat.map_subtype_embedding_Ioc
 
-theorem map_subtype_embedding_ioo : (Ioo a b).map (Function.Embedding.subtype _) = Ioo (a : ℕ) b :=
-  map_subtype_embedding_Ioo _ _ _ fun c _ x hx _ hc _ => hc.trans_le hx
-#align pnat.map_subtype_embedding_Ioo PNat.map_subtype_embedding_ioo
-
-@[simp]
-theorem card_icc : (Icc a b).card = b + 1 - a := by
-  rw [← Nat.card_Icc, ← map_subtype_embedding_Icc, card_map]
-#align pnat.card_Icc PNat.card_icc
+theorem map_subtype_embedding_Ioo : (Ioo a b).map (Function.Embedding.subtype _) = Ioo (a : ℕ) b :=
+  Finset.map_subtype_embedding_Ioo _ _ _ fun _c _ _x hx _ hc _ => hc.trans_le hx
+#align pnat.map_subtype_embedding_Ioo PNat.map_subtype_embedding_Ioo
 
 @[simp]
-theorem card_ico : (Ico a b).card = b - a := by
-  rw [← Nat.card_Ico, ← map_subtype_embedding_Ico, card_map]
-#align pnat.card_Ico PNat.card_ico
+theorem card_Icc : (Icc a b).card = b + 1 - a := by
+  rw [← Nat.card_Icc]
+  erw [← Finset.map_subtype_embedding_Icc _ a b (fun c x _ hx _ hc _ => hc.trans_le hx)]
+  -- porting note: I had to change this to `erw` *and* provide the proof, yuck.
+  rw [card_map]
+#align pnat.card_Icc PNat.card_Icc
 
 @[simp]
-theorem card_ioc : (Ioc a b).card = b - a := by
-  rw [← Nat.card_Ioc, ← map_subtype_embedding_Ioc, card_map]
-#align pnat.card_Ioc PNat.card_ioc
+theorem card_Ico : (Ico a b).card = b - a := by
+  rw [← Nat.card_Ico]
+  erw [← Finset.map_subtype_embedding_Ico _ a b (fun c x _ hx _ hc _ => hc.trans_le hx)]
+  -- porting note: I had to change this to `erw` *and* provide the proof, yuck.
+  rw  [card_map]
+#align pnat.card_Ico PNat.card_Ico
 
 @[simp]
-theorem card_ioo : (Ioo a b).card = b - a - 1 := by
-  rw [← Nat.card_Ioo, ← map_subtype_embedding_Ioo, card_map]
-#align pnat.card_Ioo PNat.card_ioo
+theorem card_Ioc : (Ioc a b).card = b - a := by
+  rw [← Nat.card_Ioc]
+  erw [← Finset.map_subtype_embedding_Ioc _ a b (fun c x _ hx _ hc _ => hc.trans_le hx)]
+  -- porting note: I had to change this to `erw` *and* provide the proof, yuck.
+  rw  [card_map]
+#align pnat.card_Ioc PNat.card_Ioc
 
 @[simp]
-theorem card_fintypeIcc : Fintype.card (Set.Icc a b) = b + 1 - a := by
+theorem card_Ioo : (Ioo a b).card = b - a - 1 := by
+  rw [← Nat.card_Ioo]
+  erw [← Finset.map_subtype_embedding_Ioo _ a b (fun c x _ hx _ hc _ => hc.trans_le hx)]
+  -- porting note: I had to change this to `erw` *and* provide the proof, yuck.
+  rw  [card_map]
+#align pnat.card_Ioo PNat.card_Ioo
+
+-- porting note: `simpNF` says `simp` can prove this
+theorem card_fintype_Icc : Fintype.card (Set.Icc a b) = b + 1 - a := by
   rw [← card_Icc, Fintype.card_ofFinset]
-#align pnat.card_fintype_Icc PNat.card_fintypeIcc
+#align pnat.card_fintype_Icc PNat.card_fintype_Icc
 
-@[simp]
-theorem card_fintypeIco : Fintype.card (Set.Ico a b) = b - a := by
+-- porting note: `simpNF` says `simp` can prove this
+theorem card_fintype_Ico : Fintype.card (Set.Ico a b) = b - a := by
   rw [← card_Ico, Fintype.card_ofFinset]
-#align pnat.card_fintype_Ico PNat.card_fintypeIco
+#align pnat.card_fintype_Ico PNat.card_fintype_Ico
 
-@[simp]
-theorem card_fintypeIoc : Fintype.card (Set.Ioc a b) = b - a := by
+-- porting note: `simpNF` says `simp` can prove this
+theorem card_fintype_Ioc : Fintype.card (Set.Ioc a b) = b - a := by
   rw [← card_Ioc, Fintype.card_ofFinset]
-#align pnat.card_fintype_Ioc PNat.card_fintypeIoc
+#align pnat.card_fintype_Ioc PNat.card_fintype_Ioc
 
-@[simp]
-theorem card_fintypeIoo : Fintype.card (Set.Ioo a b) = b - a - 1 := by
+-- porting note: `simpNF` says `simp` can prove this
+theorem card_fintype_Ioo : Fintype.card (Set.Ioo a b) = b - a - 1 := by
   rw [← card_Ioo, Fintype.card_ofFinset]
-#align pnat.card_fintype_Ioo PNat.card_fintypeIoo
+#align pnat.card_fintype_Ioo PNat.card_fintype_Ioo
 
 end PNat
-
