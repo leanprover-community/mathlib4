74407e25ac64cd0c43cdec3410bb9257f3b1595e
Arien Malec <arien.malec@gmail.com>
2023-01-27 09:11:08 -0800

    Initial file copy from mathport

---
+++ Mathlib/Data/ENat/Lattice.lean
@@ -0,0 +1,22 @@
+/-
+Copyright (c) 2022 Yury Kudryashov. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Yury Kudryashov
+
+! This file was ported from Lean 3 source module data.enat.lattice
+! leanprover-community/mathlib commit f7fc89d5d5ff1db2d1242c7bb0e9062ce47ef47c
+! Please do not edit these lines, except to modify the commit id
+! if you have ported upstream changes.
+-/
+import Mathbin.Data.Nat.Lattice
+import Mathbin.Data.Enat.Basic
+
+/-!
+# Extended natural numbers form a complete linear order
+
+This instance is not in `data.enat.basic` to avoid dependency on `finset`s.
+-/
+
+
+deriving instance CompleteLinearOrder for ENat
+
