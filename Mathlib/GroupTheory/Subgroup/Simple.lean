141375b9ed7720a4b1461c4f8603155c4ee9a88a
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2023-01-27 11:01:29 +0100

    docs

--- Mathlib/GroupTheory/Subgroup/Simple.lean
+++ Mathlib/GroupTheory/Subgroup/Simple.lean
@@ -36,11 +36,13 @@ variable (G) (A)
 
 /-- A `Group` is simple when it has exactly two normal `Subgroup`s. -/
 class IsSimpleGroup extends Nontrivial G : Prop where
+  /-- Any normal subgroup is either `⊥` or `⊤` -/
   eq_bot_or_eq_top_of_normal : ∀ H : Subgroup G, H.Normal → H = ⊥ ∨ H = ⊤
 #align is_simple_group IsSimpleGroup
 
 /-- An `AddGroup` is simple when it has exactly two normal `AddSubgroup`s. -/
 class IsSimpleAddGroup extends Nontrivial A : Prop where
+  /-- Any normal additive subgroup is either `⊥` or `⊤` -/
   eq_bot_or_eq_top_of_normal : ∀ H : AddSubgroup A, H.Normal → H = ⊥ ∨ H = ⊤
 #align is_simple_add_group IsSimpleAddGroup
 
