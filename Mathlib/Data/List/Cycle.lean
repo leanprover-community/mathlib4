5485f2ae022fa0d5158122cc4090e3c0e3ecd244
ChrisHughes24 <chrishughes24@gmail.com>
2023-01-27 10:59:25 +0000

    fix red

--- Mathlib/Data/List/Cycle.lean
+++ Mathlib/Data/List/Cycle.lean
@@ -418,7 +418,7 @@ theorem prev_reverse_eq_next (l : List α) (h : Nodup l) (x : α) (hx : x ∈ l)
     rw [← nthLe_reverse]
     · simp [tsub_tsub_cancel_of_le (Nat.le_pred_of_lt hk)]
     · simpa using (Nat.sub_le _ _).trans_lt (tsub_lt_self lpos Nat.succ_pos')
-  · simpa using (Nat.sub_le _ _).trans_lt (tsub_lt_self lpos Nat.succ_pos')
+    . simpa
 #align list.prev_reverse_eq_next List.prev_reverse_eq_next
 
 theorem next_reverse_eq_prev (l : List α) (h : Nodup l) (x : α) (hx : x ∈ l) :
