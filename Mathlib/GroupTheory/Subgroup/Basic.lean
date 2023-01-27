d02916a1952839bf83c5cac2b0e9fccf85e4445c
Jireh Loreaux <loreaujy@gmail.com>
2023-01-27 15:21:26 -0600

    fix two missed sorries

--- Mathlib/GroupTheory/Subgroup/Basic.lean
+++ Mathlib/GroupTheory/Subgroup/Basic.lean
@@ -161,7 +161,7 @@ theorem div_mem_comm_iff {a b : G} : a / b ∈ H ↔ b / a ∈ H := by
 #align div_mem_comm_iff div_mem_comm_iff
 #align sub_mem_comm_iff sub_mem_comm_iff
 
-@[to_additive /-(attr := simp)-/] -- porting note: `simp` cannot simplify LHS
+@[to_additive (attr := simp)] -- porting note: `simp` cannot simplify LHS
 theorem exists_inv_mem_iff_exists_mem {P : G → Prop} :
     (∃ x : G, x ∈ H ∧ P x⁻¹) ↔ ∃ x ∈ H, P x := by
   constructor <;>
@@ -169,8 +169,6 @@ theorem exists_inv_mem_iff_exists_mem {P : G → Prop} :
       exact ⟨x⁻¹, inv_mem x_in, by simp [hx]⟩
 #align exists_inv_mem_iff_exists_mem exists_inv_mem_iff_exists_mem
 #align exists_neg_mem_iff_exists_mem exists_neg_mem_iff_exists_mem
-
-
 
 @[to_additive]
 theorem mul_mem_cancel_right {x y : G} (h : x ∈ H) : y * x ∈ H ↔ y ∈ H :=
