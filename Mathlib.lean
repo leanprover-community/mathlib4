1190327da16cad8503740b2b8aecbdf710b57e6f
Moritz Firsching <moritz.firsching@gmail.com>
2023-01-27 17:43:59 +0000

    feat: port GroupTheory.IsFreeGroup (#1889)
    Co-authored-by: Moritz Firsching <firsching@google.com>
    Co-authored-by: adamtopaz <github@adamtopaz.com>
    Co-authored-by: Chris Hughes <33847686+ChrisHughes24@users.noreply.github.com>
    Co-authored-by: Adam Topaz <adamtopaz@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -575,6 +575,7 @@ import Mathlib.GroupTheory.GroupAction.SubMulAction
 import Mathlib.GroupTheory.GroupAction.Sum
 import Mathlib.GroupTheory.GroupAction.Support
 import Mathlib.GroupTheory.GroupAction.Units
+import Mathlib.GroupTheory.IsFreeGroup
 import Mathlib.GroupTheory.Perm.Basic
 import Mathlib.GroupTheory.Perm.List
 import Mathlib.GroupTheory.Perm.Support
