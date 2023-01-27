00ffd6aba0233a5b3c891a919c4c75f993d5d2a6
Moritz Firsching <firsching@google.com>
2023-01-27 21:40:06 +0100

    automated fixes
    Mathbin -> Mathlib
    
    fix certain import statements
    
    move "by" to end of line
    
    add import to Mathlib.lean

--- Mathlib/GroupTheory/GroupAction/SubMulAction/Pointwise.lean
+++ Mathlib/GroupTheory/GroupAction/SubMulAction/Pointwise.lean
@@ -8,7 +8,7 @@ Authors: Eric Wieser
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
-import Mathbin.GroupTheory.GroupAction.SubMulAction
+import Mathlib.GroupTheory.GroupAction.SubMulAction
 
 /-!
 # Pointwise monoid structures on sub_mul_action
