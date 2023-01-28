fcc90402a47ece0da18c32b29c0813aab3618eb1
Moritz Firsching <firsching@google.com>
2023-01-28 20:12:47 +0100

    builds now

--- Mathlib/Algebra/BigOperators/Intervals.lean
+++ Mathlib/Algebra/BigOperators/Intervals.lean
@@ -139,9 +139,9 @@ theorem sum_Ico_Ico_comm {M : Type _} [AddCommMonoid M] (a b : ℕ) (f : ℕ →
       ∑ j in Finset.Ico a b, ∑ i in Finset.Ico a (j + 1), f i j := by
   rw [Finset.sum_sigma', Finset.sum_sigma']
   refine'
-            Finset.sum_bij' (fun (x : Σi : ℕ, ℕ) _ => (⟨x.2, x.1⟩ : Σi : ℕ, ℕ)) _ (fun _ _ => rfl)
-              (fun (x : Σi : ℕ, ℕ) _ => (⟨x.2, x.1⟩ : Σi : ℕ, ℕ)) _ (by rintro ⟨⟩ _ <;> rfl)
-              (by rintro ⟨⟩ _ <;> rfl) <;>
+            Finset.sum_bij' (fun (x : Σ _ : ℕ, ℕ) _ => (⟨x.2, x.1⟩ : Σ _ : ℕ, ℕ)) _ (fun _ _ => rfl)
+              (fun (x : Σ _ : ℕ, ℕ) _ => (⟨x.2, x.1⟩ : Σ _ : ℕ, ℕ)) _ (by (rintro ⟨⟩ _; rfl))
+              (by (rintro ⟨⟩ _; rfl)) <;>
           simp only [Finset.mem_Ico, Sigma.forall, Finset.mem_sigma] <;>
         rintro a b ⟨⟨h₁, h₂⟩, ⟨h₃, h₄⟩⟩ <;>
       refine' ⟨⟨_, _⟩, ⟨_, _⟩⟩ <;>
@@ -167,7 +167,7 @@ theorem prod_Ico_reflect (f : ℕ → β) (k : ℕ) {m n : ℕ} (h : m ≤ n + 1
   · rw [← Nat.Ico_image_const_sub_eq_Ico (this _ hkm)]
     refine' (prod_image _).symm
     simp only [mem_Ico]
-    rintro i ⟨ki, im⟩ j ⟨kj, jm⟩ Hij
+    rintro i ⟨_, im⟩ j ⟨_, jm⟩ Hij
     rw [← tsub_tsub_cancel_of_le (this _ im), Hij, tsub_tsub_cancel_of_le (this _ jm)]
   · have : n + 1 - k ≤ n + 1 - m := by
       rw [tsub_le_tsub_iff_left h]
@@ -296,17 +296,23 @@ theorem sum_Ico_by_parts (hmn : m < n) :
       Nat.sub_add_cancel (pos_of_gt hmn), sub_add_cancel]
   rw [sum_eq_sum_Ico_succ_bot hmn]
   -- porting note: the following used to be done with `conv`
-  have h₃: (Finset.sum (Ico (m+1) n) fun i => f i • g i) =
-             (Finset.sum (Ico (m+1) n) fun i =>
+  have h₃: (Finset.sum (Ico (m + 1) n) fun i => f i • g i) =
+             (Finset.sum (Ico (m + 1) n) fun i =>
                 f i • ((Finset.sum (Finset.range (i + 1)) g) -
                         (Finset.sum (Finset.range i) g))) := by
     congr; funext; rw [← sum_range_succ_sub_sum g]
   rw [h₃]
   simp_rw [smul_sub, sum_sub_distrib, h₂, h₁]
-  conv_lhs =>
-    congr
-    skip
-    rw [← add_sub, add_comm, ← add_sub, ← sum_sub_distrib]
+  -- porting note: the following used to be done with `conv`
+  have h₄ : ((((Finset.sum (Ico m (n - 1)) fun i => f i • Finset.sum (range (i + 1)) fun i => g i) +
+      f (n - 1) • Finset.sum (range n) fun i => g i) -
+      f m • Finset.sum (range (m + 1)) fun i => g i) -
+      Finset.sum (Ico m (n - 1)) fun i => f (i + 1) • Finset.sum (range (i + 1)) fun i => g i) =
+      f (n - 1) • (range n).sum g - f m • (range (m + 1)).sum g +
+      Finset.sum (Ico m (n - 1)) (fun i => f i • (range (i + 1)).sum g -
+      f (i + 1) • (range (i + 1)).sum g) := by
+    rw [← add_sub, add_comm, ←add_sub, ← sum_sub_distrib]
+  rw [h₄]
   have : ∀ i, f i • G (i + 1) - f (i + 1) • G (i + 1) = -((f (i + 1) - f i) • G (i + 1)) := by
     intro i
     rw [sub_smul]
