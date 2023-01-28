2925f19566ef53547d62aa1dd7afe705bc02d0bc
Lukas Miaskiwskyi <lukas.mias@gmail.com>
2023-01-29 00:03:30 +0100

    fixed

--- Mathlib/Data/Finsupp/Indicator.lean
+++ Mathlib/Data/Finsupp/Indicator.lean
@@ -13,11 +13,11 @@ import Mathlib.Data.Finsupp.Defs
 /-!
 # Building finitely supported functions off finsets
 
-This file defines `finsupp.indicator` to help create finsupps from finsets.
+This file defines `Finsupp.indicator` to help create finsupps from finsets.
 
 ## Main declarations
 
-* `finsupp.indicator`: Turns a map from a `finset` into a `finsupp` from the entire type.
+* `Finsupp.indicator`: Turns a map from a `Finset` into a `Finsupp` from the entire type.
 -/
 
 
@@ -39,13 +39,9 @@ def indicator (s : Finset ι) (f : ∀ i ∈ s, α) : ι →₀ α
     if H : i ∈ s then f i H else 0
   support :=
     haveI := Classical.decEq α
-    (s.attach.filter fun i : s => f i.1 i.2 ≠ 0).map (embedding.subtype _)
-  mem_support_to_fun i := by
-    letI := Classical.decEq α
-    rw [mem_map, dite_ne_right_iff]
-    exact
-      ⟨fun ⟨⟨j, hj⟩, hf, rfl⟩ => ⟨hj, (mem_filter.1 hf).2⟩, fun ⟨hi, hf⟩ =>
-        ⟨⟨i, hi⟩, mem_filter.2 <| ⟨mem_attach _ _, hf⟩, rfl⟩⟩
+    (s.attach.filter fun i : s => f i.1 i.2 ≠ 0).map (Embedding.subtype _)
+  mem_support_toFun i := by
+    classical simp
 #align finsupp.indicator Finsupp.indicator
 
 theorem indicator_of_mem (hi : i ∈ s) (f : ∀ i ∈ s, α) : indicator s f i = f i hi :=
@@ -60,22 +56,22 @@ variable (s i)
 
 @[simp]
 theorem indicator_apply [DecidableEq ι] : indicator s f i = if hi : i ∈ s then f i hi else 0 := by
-  convert rfl
+  simp only [indicator, ne_eq, coe_mk]
+  congr
 #align finsupp.indicator_apply Finsupp.indicator_apply
 
 theorem indicator_injective : Injective fun f : ∀ i ∈ s, α => indicator s f := by
   intro a b h
   ext (i hi)
   rw [← indicator_of_mem hi a, ← indicator_of_mem hi b]
-  exact congr_fun h i
+  exact FunLike.congr_fun h i
 #align finsupp.indicator_injective Finsupp.indicator_injective
 
 theorem support_indicator_subset : ((indicator s f).support : Set ι) ⊆ s := by
   intro i hi
   rw [mem_coe, mem_support_iff] at hi
-  by_contra
+  by_contra h
   exact hi (indicator_of_not_mem h _)
 #align finsupp.support_indicator_subset Finsupp.support_indicator_subset
 
 end Finsupp
-
