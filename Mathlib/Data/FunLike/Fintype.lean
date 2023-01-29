d7f32b45e1eccb33fa2926be37bc5c52353439c2
Arien Malec <arien.malec@gmail.com>
2023-01-29 15:48:06 -0800

    auto: naming

--- Mathlib/Data/FunLike/Fintype.lean
+++ Mathlib/Data/FunLike/Fintype.lean
@@ -13,21 +13,21 @@ import Mathlib.Data.Fintype.Basic
 import Mathlib.Data.FunLike.Basic
 
 /-!
-# Finiteness of `fun_like` types
+# Finiteness of `FunLike` types
 
-We show a type `F` with a `fun_like F α β` is finite if both `α` and `β` are finite.
+We show a type `F` with a `FunLike F α β` is finite if both `α` and `β` are finite.
 This corresponds to the following two pairs of declarations:
 
- * `fun_like.fintype` is a definition stating all `fun_like`s are finite if their domain and
+ * `FunLike.fintype` is a definition stating all `FunLike`s are finite if their domain and
    codomain are.
- * `fun_like.finite` is a lemma stating all `fun_like`s are finite if their domain and
+ * `FunLike.finite` is a lemma stating all `FunLike`s are finite if their domain and
    codomain are.
- * `fun_like.fintype'` is a non-dependent version of `fun_like.fintype` and
- * `fun_like.finite` is a non-dependent version of `fun_like.finite`, because dependent instances
+ * `FunLike.fintype'` is a non-dependent version of `FunLike.fintype` and
+ * `FunLike.finite` is a non-dependent version of `FunLike.finite`, because dependent instances
    are harder to infer.
 
-You can use these to produce instances for specific `fun_like` types.
-(Although there might be options for `fintype` instances with better definitional behaviour.)
+You can use these to produce instances for specific `FunLike` types.
+(Although there might be options for `Fintype` instances with better definitional behaviour.)
 They can't be instances themselves since they can cause loops.
 -/
 
@@ -36,20 +36,20 @@ section Type'
 
 variable (F G : Type _) {α γ : Type _} {β : α → Type _} [FunLike F α β] [FunLike G α fun _ => γ]
 
-/-- All `fun_like`s are finite if their domain and codomain are.
+/-- All `FunLike`s are finite if their domain and codomain are.
 
-This is not an instance because specific `fun_like` types might have a better-suited definition.
+This is not an instance because specific `FunLike` types might have a better-suited definition.
 
-See also `fun_like.finite`.
+See also `FunLike.finite`.
 -/
 noncomputable def FunLike.fintype [DecidableEq α] [Fintype α] [∀ i, Fintype (β i)] : Fintype F :=
   Fintype.ofInjective _ FunLike.coe_injective
 #align fun_like.fintype FunLike.fintype
 
-/-- All `fun_like`s are finite if their domain and codomain are.
+/-- All `FunLike`s are finite if their domain and codomain are.
 
-Non-dependent version of `fun_like.fintype` that might be easier to infer.
-This is not an instance because specific `fun_like` types might have a better-suited definition.
+Non-dependent version of `FunLike.fintype` that might be easier to infer.
+This is not an instance because specific `FunLike` types might have a better-suited definition.
 -/
 noncomputable def FunLike.fintype' [DecidableEq α] [Fintype α] [Fintype γ] : Fintype G :=
   FunLike.fintype G
@@ -62,7 +62,7 @@ section Sort'
 
 variable (F G : Sort _) {α γ : Sort _} {β : α → Sort _} [FunLike F α β] [FunLike G α fun _ => γ]
 
-/-- All `fun_like`s are finite if their domain and codomain are.
+/-- All `FunLike`s are finite if their domain and codomain are.
 
 Can't be an instance because it can cause infinite loops.
 -/
@@ -70,9 +70,9 @@ theorem FunLike.finite [Finite α] [∀ i, Finite (β i)] : Finite F :=
   Finite.of_injective _ FunLike.coe_injective
 #align fun_like.finite FunLike.finite
 
-/-- All `fun_like`s are finite if their domain and codomain are.
+/-- All `FunLike`s are finite if their domain and codomain are.
 
-Non-dependent version of `fun_like.finite` that might be easier to infer.
+Non-dependent version of `FunLike.finite` that might be easier to infer.
 Can't be an instance because it can cause infinite loops.
 -/
 theorem FunLike.finite' [Finite α] [Finite γ] : Finite G :=
