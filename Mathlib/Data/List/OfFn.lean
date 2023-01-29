625948fba62c65c129d601097cad3aaaa4c8588e
YaelDillies <yael.dillies@gmail.com>
2023-01-29 11:00:03 +0000

    fix

--- Mathlib/Data/List/OfFn.lean
+++ Mathlib/Data/List/OfFn.lean
@@ -245,7 +245,7 @@ theorem ofFn_fin_repeat {m} (a : Fin m → α) (n : ℕ) :
 @[simp] lemma pairwise_ofFn {R : α → α → Prop} {n} {f : fin n → α} :
   (ofFn f).pairwise R ↔ ∀ ⦃i j⦄, i < j → R (f i) (f j) := by
   simp only [pairwise_iff_nthLe, Fin.forall_iff, length_ofFn, nthLe_ofFn', Fin.mk_lt_mk]
-  exact ⟨λ h i hi j hj hij, h _ _ hj hij, λ h i j hj hij, h _ (hij.trans hj) _ hj hij⟩
+  exact ⟨λ h i hi j hj hij ↦ h _ _ hj hij, λ h i j hj hij ↦ h _ (hij.trans hj) _ hj hij⟩
 
 /-- Lists are equivalent to the sigma type of tuples of a given length. -/
 @[simps]
