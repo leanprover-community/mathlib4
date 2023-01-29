47b64477d818a1136a5fa33f9b658692831e8291
int-y1 <jason_yuen2007@hotmail.com>
2023-01-28 20:33:34 -0800

    automated fixes
    Mathbin -> Mathlib
    
    fix certain import statements
    
    move "by" to end of line
    
    add import to Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -456,6 +456,7 @@ import Mathlib.Data.Nat.Units
 import Mathlib.Data.Nat.Upto
 import Mathlib.Data.Nat.WithBot
 import Mathlib.Data.Num.Basic
+import Mathlib.Data.Num.Bitwise
 import Mathlib.Data.Opposite
 import Mathlib.Data.Option.Basic
 import Mathlib.Data.Option.Defs
--- Mathlib/Data/Num/Bitwise.lean
+++ Mathlib/Data/Num/Bitwise.lean
@@ -8,8 +8,8 @@ Authors: Mario Carneiro
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
-import Mathbin.Data.Num.Basic
-import Mathbin.Data.Bitvec.Core
+import Mathlib.Data.Num.Basic
+import Mathlib.Data.Bitvec.Core
 
 /-!
 # Bitwise operations using binary representation of integers
