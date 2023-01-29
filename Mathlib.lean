59ded3f32295156dc8fbb8faa7b8f472717052ea
Xavier-François Roblot <46200072+xroblot@users.noreply.github.com>
2023-01-29 16:57:47 +0000

    feat port : Algebra.Free (#1353)
    Co-authored-by: Lukas Miaskiwskyi <lukas.mias@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -25,6 +25,7 @@ import Mathlib.Algebra.Field.Basic
 import Mathlib.Algebra.Field.Defs
 import Mathlib.Algebra.Field.Opposite
 import Mathlib.Algebra.Field.Power
+import Mathlib.Algebra.Free
 import Mathlib.Algebra.FreeMonoid.Basic
 import Mathlib.Algebra.FreeMonoid.Count
 import Mathlib.Algebra.GCDMonoid.Basic
