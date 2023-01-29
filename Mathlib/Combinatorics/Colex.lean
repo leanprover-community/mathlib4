7f0f77a6e52e44db12ea0f53ee8f7d826c8b603a
Moritz Firsching <firsching@google.com>
2023-01-29 21:40:01 +0100

    fix lt_trichotomy

--- Mathlib/Combinatorics/Colex.lean
+++ Mathlib/Combinatorics/Colex.lean
@@ -171,28 +171,29 @@ instance [LinearOrder α] : IsTrans (Finset.Colex α) (· < ·) :=
 theorem lt_trichotomy [LinearOrder α] (A B : Finset.Colex α) : A < B ∨ A = B ∨ B < A := by
   by_cases h₁ : A = B
   · tauto
-  rcases exists_max_image (A \ B ∪ B \ A) id _ with ⟨k, hk, z⟩
+  have h : Finset.Nonempty (A \ B ∪ B \ A) := by
+    rw [nonempty_iff_ne_empty]
+    intro a
+    simp only [union_eq_empty_iff, sdiff_eq_empty_iff_subset] at a
+    apply h₁ (Subset.antisymm a.1 a.2)
+  rcases exists_max_image (A \ B ∪ B \ A) id h with ⟨k, ⟨hk, z⟩⟩
   · simp only [mem_union, mem_sdiff] at hk
-    cases hk
+    cases' hk with hk hk
     · right
       right
-      refine' ⟨k, fun t th => _, hk.2, hk.1⟩
+      refine' ⟨k, @fun t th => _, hk.2, hk.1⟩
       specialize z t
       by_contra h₂
       simp only [mem_union, mem_sdiff, id.def] at z
-      rw [not_iff, iff_iff_and_or_not_and_not, not_not, and_comm'] at h₂
+      rw [not_iff, iff_iff_and_or_not_and_not, not_not, and_comm] at h₂
       apply not_le_of_lt th (z h₂)
     · left
-      refine' ⟨k, fun t th => _, hk.2, hk.1⟩
+      refine' ⟨k, @fun t th => _, hk.2, hk.1⟩
       specialize z t
       by_contra h₃
       simp only [mem_union, mem_sdiff, id.def] at z
-      rw [not_iff, iff_iff_and_or_not_and_not, not_not, and_comm', or_comm'] at h₃
+      rw [not_iff, iff_iff_and_or_not_and_not, not_not, and_comm, or_comm] at h₃
       apply not_le_of_lt th (z h₃)
-  rw [nonempty_iff_ne_empty]
-  intro a
-  simp only [union_eq_empty_iff, sdiff_eq_empty_iff_subset] at a
-  apply h₁ (subset.antisymm a.1 a.2)
 #align colex.lt_trichotomy Colex.lt_trichotomy
 
 instance [LinearOrder α] : IsTrichotomous (Finset.Colex α) (· < ·) :=
