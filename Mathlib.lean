29ffa06c6da7aa5b1b2f58fdc2a31f37a75e0846
Jason Yuen <jason_yuen2007@hotmail.com>
2023-01-31 19:04:25 +0000

    feat: port Data.Sum.Interval (#1962)
    Co-authored-by: Lukas Miaskiwskyi <lukas.mias@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -562,6 +562,7 @@ import Mathlib.Data.String.Defs
 import Mathlib.Data.String.Lemmas
 import Mathlib.Data.Subtype
 import Mathlib.Data.Sum.Basic
+import Mathlib.Data.Sum.Interval
 import Mathlib.Data.Sum.Order
 import Mathlib.Data.Sym.Basic
 import Mathlib.Data.Sym.Sym2
