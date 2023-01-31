5d06f18e5a444b74217b1b1aad5dafcd66908ebf
Yury G. Kudryashov <urkud@urkud.name>
2023-01-31 04:29:21 -0600

    Fix comments

--- Mathlib/Topology/Order.lean
+++ Mathlib/Topology/Order.lean
@@ -13,37 +13,39 @@ import Mathlib.Topology.Basic
 /-!
 # Ordering on topologies and (co)induced topologies
 
-Topologies on a fixed type `α` are ordered, by reverse inclusion.
-That is, for topologies `t₁` and `t₂` on `α`, we write `t₁ ≤ t₂`
-if every set open in `t₂` is also open in `t₁`.
-(One also calls `t₁` finer than `t₂`, and `t₂` coarser than `t₁`.)
+Topologies on a fixed type `α` are ordered, by reverse inclusion.  That is, for topologies `t₁` and
+`t₂` on `α`, we write `t₁ ≤ t₂` if every set open in `t₂` is also open in `t₁`.  (One also calls
+`t₁` *finer* than `t₂`, and `t₂` *coarser* than `t₁`.)
 
 Any function `f : α → β` induces
-       `induced f : topological_space β → topological_space α`
-and  `coinduced f : topological_space α → topological_space β`.
-Continuity, the ordering on topologies and (co)induced topologies are
-related as follows:
-* The identity map (α, t₁) → (α, t₂) is continuous iff t₁ ≤ t₂.
-* A map f : (α, t) → (β, u) is continuous
-    iff             t ≤ induced f u   (`continuous_iff_le_induced`)
-    iff coinduced f t ≤ u             (`continuous_iff_coinduced_le`).
 
-Topologies on α form a complete lattice, with ⊥ the discrete topology
-and ⊤ the indiscrete topology.
+* `TopologicalSpace.induced f : TopologicalSpace β → TopologicalSpace α`;
+* `TopologicalSpace.coinduced f : TopologicalSpace α → TopologicalSpace β`.
 
-For a function f : α → β, (coinduced f, induced f) is a Galois connection
-between topologies on α and topologies on β.
+Continuity, the ordering on topologies and (co)induced topologies are related as follows:
+
+* The identity map `(α, t₁) → (α, t₂)` is continuous iff `t₁ ≤ t₂`.
+* A map `f : (α, t) → (β, u)` is continuous
+  * iff `t ≤ TopologicalSpace.induced f u` (`continuous_iff_le_induced`)
+  * iff `TopologicalSpace.coinduced f t ≤ u` (`continuous_iff_coinduced_le`).
+
+Topologies on `α` form a complete lattice, with `⊥` the discrete topology and `⊤` the indiscrete
+topology.
+
+For a function `f : α → β`, `(TopologicalSpace.coinduced f, TopologicalSpace.induced f)` is a Galois
+connection between topologies on `α` and topologies on `β`.
 
 ## Implementation notes
 
-There is a Galois insertion between topologies on α (with the inclusion ordering)
-and all collections of sets in α. The complete lattice structure on topologies
-on α is defined as the reverse of the one obtained via this Galois insertion.
+There is a Galois insertion between topologies on `α` (with the inclusion ordering) and all
+collections of sets in `α`. The complete lattice structure on topologies on `α` is defined as the
+reverse of the one obtained via this Galois insertion. More precisely, we use the corresponding
+Galois coinsertion between topologies on `α` (with the reversed inclusion ordering) and collections
+of sets in `α` (with the reversed inclusion ordering).
 
 ## Tags
 
 finer, coarser, induced topology, coinduced topology
-
 -/
 
 
