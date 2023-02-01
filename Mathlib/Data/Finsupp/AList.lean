e93885b800e7de69a261b208e58fe541d13bd40e
Arien Malec <arien.malec@gmail.com>
2023-02-01 10:53:52 -0800

    Alist -> AList and to_foo -> toFoo

--- Mathlib/Data/Finsupp/AList.lean
+++ Mathlib/Data/Finsupp/AList.lean
@@ -9,7 +9,7 @@ Authors: Violeta Hernández
 ! if you have ported upstream changes.
 -/
 import Mathlib.Data.Finsupp.Basic
-import Mathlib.Data.List.Alist
+import Mathlib.Data.List.AList
 
 /-!
 # Connections between `finsupp` and `alist`
@@ -29,7 +29,7 @@ variable {α M : Type _} [Zero M]
 
 /-- Produce an association list for the finsupp over its support using choice. -/
 @[simps]
-noncomputable def toAlist (f : α →₀ M) : AList fun x : α => M :=
+noncomputable def toAList (f : α →₀ M) : AList fun x : α => M :=
   ⟨f.graph.toList.map Prod.toSigma,
     by
     rw [List.NodupKeys, List.keys, List.map_map, Prod.fst_comp_toSigma, List.nodup_map_iff_inj_on]
@@ -38,18 +38,18 @@ noncomputable def toAlist (f : α →₀ M) : AList fun x : α => M :=
       dsimp at hb hc
       rw [← hc.1, hb.1]
     · apply Finset.nodup_toList⟩
-#align finsupp.to_alist Finsupp.toAlist
+#align finsupp.to_alist Finsupp.toAList
 
 @[simp]
-theorem toAlist_keys_toFinset [DecidableEq α] (f : α →₀ M) : f.toAList.keys.toFinset = f.support :=
+theorem toAList_keys_toFinset [DecidableEq α] (f : α →₀ M) : f.toAList.keys.toFinset = f.support :=
   by
   ext
-  simp [to_alist, AList.mem_keys, AList.keys, List.keys]
-#align finsupp.to_alist_keys_to_finset Finsupp.toAlist_keys_toFinset
+  simp [toAList, AList.mem_keys, AList.keys, List.keys]
+#align finsupp.to_alist_keys_to_finset Finsupp.toAList_keys_toFinset
 
 @[simp]
 theorem mem_toAlist {f : α →₀ M} {x : α} : x ∈ f.toAList ↔ f x ≠ 0 := by
-  classical rw [AList.mem_keys, ← List.mem_toFinset, to_alist_keys_to_finset, mem_support_iff]
+  classical rw [AList.mem_keys, ← List.mem_toFinset, toAList_keys_toFinset, mem_support_iff]
 #align finsupp.mem_to_alist Finsupp.mem_toAlist
 
 end Finsupp
@@ -72,7 +72,7 @@ noncomputable def lookupFinsupp (l : AList fun x : α => M) : α →₀ M
     (l.lookup a).getD 0
   mem_support_toFun a := by
     classical
-      simp_rw [mem_to_finset, List.mem_keys, List.mem_filter, ← mem_lookup_iff]
+      simp_rw [mem_toFinset, List.mem_keys, List.mem_filter, ← mem_lookup_iff]
       cases lookup a l <;> simp
 #align alist.lookup_finsupp AList.lookupFinsupp
 
@@ -88,13 +88,13 @@ theorem lookupFinsupp_support [DecidableEq α] [DecidableEq M] (l : AList fun x 
 
 theorem lookupFinsupp_eq_iff_of_ne_zero [DecidableEq α] {l : AList fun x : α => M} {a : α} {x : M}
     (hx : x ≠ 0) : l.lookupFinsupp a = x ↔ x ∈ l.dlookup a := by
-  rw [lookup_finsupp_apply]
+  rw [lookupFinsupp_apply]
   cases' lookup a l with m <;> simp [hx.symm]
 #align alist.lookup_finsupp_eq_iff_of_ne_zero AList.lookupFinsupp_eq_iff_of_ne_zero
 
 theorem lookupFinsupp_eq_zero_iff [DecidableEq α] {l : AList fun x : α => M} {a : α} :
     l.lookupFinsupp a = 0 ↔ a ∉ l ∨ (0 : M) ∈ l.dlookup a := by
-  rw [lookup_finsupp_apply, ← lookup_eq_none]
+  rw [lookupFinsupp_apply, ← lookup_eq_none]
   cases' lookup a l with m <;> simp
 #align alist.lookup_finsupp_eq_zero_iff AList.lookupFinsupp_eq_zero_iff
 
@@ -118,20 +118,19 @@ theorem singleton_lookupFinsupp (a : α) (m : M) :
 #align alist.singleton_lookup_finsupp AList.singleton_lookupFinsupp
 
 @[simp]
-theorem Finsupp.toAlist_lookupFinsupp (f : α →₀ M) : f.toAList.lookupFinsupp = f := by
+theorem Finsupp.toAList_lookupFinsupp (f : α →₀ M) : f.toAList.lookupFinsupp = f := by
   ext
   classical
     by_cases h : f a = 0
-    · suffices f.to_alist.lookup a = none by simp [h, this]
+    · suffices f.toAlist.lookup a = none by simp [h, this]
       · simp [lookup_eq_none, h]
-    · suffices f.to_alist.lookup a = some (f a) by simp [h, this]
+    · suffices f.toAlist.lookup a = some (f a) by simp [h, this]
       · apply mem_lookup_iff.2
         simpa using h
-#align finsupp.to_alist_lookup_finsupp Finsupp.toAlist_lookupFinsupp
+#align finsupp.to_alist_lookup_finsupp Finsupp.toAList_lookupFinsupp
 
 theorem lookupFinsupp_surjective : Function.Surjective (@lookupFinsupp α M _) := fun f =>
-  ⟨_, Finsupp.toAlist_lookupFinsupp f⟩
+  ⟨_, Finsupp.toAList_lookupFinsupp f⟩
 #align alist.lookup_finsupp_surjective AList.lookupFinsupp_surjective
 
 end AList
-
