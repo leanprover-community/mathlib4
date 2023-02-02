2d0ccf760702f2d77e7ace6e35a30e7d3adc2189
Yury G. Kudryashov <urkud@urkud.name>
2023-02-02 08:33:43 -0600

    Partial fix

--- Mathlib/Topology/Algebra/Constructions.lean
+++ Mathlib/Topology/Algebra/Constructions.lean
@@ -25,9 +25,7 @@ topological space, opposite monoid, units
 
 variable {M X : Type _}
 
-open Filter
-
-open Topology
+open Filter Topology
 
 namespace MulOpposite
 
@@ -39,13 +37,13 @@ instance [TopologicalSpace M] : TopologicalSpace Mᵐᵒᵖ :=
 
 variable [TopologicalSpace M]
 
-@[continuity, to_additive]
+@[to_additive] -- porting note: todo: restore `continuity`
 theorem continuous_unop : Continuous (unop : Mᵐᵒᵖ → M) :=
   continuous_induced_dom
 #align mul_opposite.continuous_unop MulOpposite.continuous_unop
 #align add_opposite.continuous_unop AddOpposite.continuous_unop
 
-@[continuity, to_additive]
+@[to_additive] -- porting note: todo: restore `continuity`
 theorem continuous_op : Continuous (op : M → Mᵐᵒᵖ) :=
   continuous_induced_rng.2 continuous_id
 #align mul_opposite.continuous_op MulOpposite.continuous_op
@@ -62,27 +60,27 @@ def opHomeomorph : M ≃ₜ Mᵐᵒᵖ where
 
 @[to_additive]
 instance [T2Space M] : T2Space Mᵐᵒᵖ :=
-  opHomeomorph.symm.Embedding.T2Space
+  opHomeomorph.symm.embedding.t2Space
 
-@[simp, to_additive]
+@[to_additive (attr := simp)]
 theorem map_op_nhds (x : M) : map (op : M → Mᵐᵒᵖ) (𝓝 x) = 𝓝 (op x) :=
   opHomeomorph.map_nhds_eq x
 #align mul_opposite.map_op_nhds MulOpposite.map_op_nhds
 #align add_opposite.map_op_nhds AddOpposite.map_op_nhds
 
-@[simp, to_additive]
+@[to_additive (attr := simp)]
 theorem map_unop_nhds (x : Mᵐᵒᵖ) : map (unop : Mᵐᵒᵖ → M) (𝓝 x) = 𝓝 (unop x) :=
   opHomeomorph.symm.map_nhds_eq x
 #align mul_opposite.map_unop_nhds MulOpposite.map_unop_nhds
 #align add_opposite.map_unop_nhds AddOpposite.map_unop_nhds
 
-@[simp, to_additive]
+@[to_additive (attr := simp)]
 theorem comap_op_nhds (x : Mᵐᵒᵖ) : comap (op : M → Mᵐᵒᵖ) (𝓝 x) = 𝓝 (unop x) :=
   opHomeomorph.comap_nhds_eq x
 #align mul_opposite.comap_op_nhds MulOpposite.comap_op_nhds
 #align add_opposite.comap_op_nhds AddOpposite.comap_op_nhds
 
-@[simp, to_additive]
+@[to_additive (attr := simp)]
 theorem comap_unop_nhds (x : M) : comap (unop : Mᵐᵒᵖ → M) (𝓝 x) = 𝓝 (op x) :=
   opHomeomorph.symm.comap_nhds_eq x
 #align mul_opposite.comap_unop_nhds MulOpposite.comap_unop_nhds
@@ -100,37 +98,37 @@ variable [TopologicalSpace M] [Monoid M] [TopologicalSpace X]
 @[to_additive
       "The additive units of a monoid are equipped with a topology, via the embedding into\n`M × M`."]
 instance : TopologicalSpace Mˣ :=
-  Prod.topologicalSpace.induced (embedProduct M)
+  TopologicalSpace.induced (embedProduct M) inferInstance
 
 @[to_additive]
 theorem inducing_embedProduct : Inducing (embedProduct M) :=
   ⟨rfl⟩
 #align units.inducing_embed_product Units.inducing_embedProduct
-#align add_units.inducing_embed_product AddUnits.inducing_embed_product
+#align add_units.inducing_embed_product AddUnits.inducing_embedProduct
 
 @[to_additive]
 theorem embedding_embedProduct : Embedding (embedProduct M) :=
   ⟨inducing_embedProduct, embedProduct_injective M⟩
 #align units.embedding_embed_product Units.embedding_embedProduct
-#align add_units.embedding_embed_product AddUnits.embedding_embed_product
+#align add_units.embedding_embed_product AddUnits.embedding_embedProduct
 
 @[to_additive]
 theorem continuous_embedProduct : Continuous (embedProduct M) :=
   continuous_induced_dom
 #align units.continuous_embed_product Units.continuous_embedProduct
-#align add_units.continuous_embed_product AddUnits.continuous_embed_product
+#align add_units.continuous_embed_product AddUnits.continuous_embedProduct
 
 @[to_additive]
-theorem continuous_coe : Continuous (coe : Mˣ → M) :=
+theorem continuous_val : Continuous ((↑) : Mˣ → M) :=
   (@continuous_embedProduct M _ _).fst
-#align units.continuous_coe Units.continuous_coe
-#align add_units.continuous_coe AddUnits.continuous_coe
+#align units.continuous_coe Units.continuous_val
+#align add_units.continuous_coe AddUnits.continuous_val
 
 @[to_additive]
 protected theorem continuous_iff {f : X → Mˣ} :
-    Continuous f ↔ Continuous (coe ∘ f : X → M) ∧ Continuous (fun x => ↑(f x)⁻¹ : X → M) := by
-  simp only [inducing_embed_product.continuous_iff, embed_product_apply, (· ∘ ·),
-    continuous_prod_mk, op_homeomorph.symm.inducing.continuous_iff, op_homeomorph_symm_apply,
+    Continuous f ↔ Continuous (val ∘ f : X → M) ∧ Continuous (fun x => ↑(f x)⁻¹ : X → M) := by
+  simp only [inducing_embedProduct.continuous_iff, embedProduct_apply, (· ∘ ·),
+    continuous_prod_mk, opHomeomorph.symm.inducing.continuous_iff, opHomeomorph_symm_apply,
     unop_op]
 #align units.continuous_iff Units.continuous_iff
 #align add_units.continuous_iff AddUnits.continuous_iff
