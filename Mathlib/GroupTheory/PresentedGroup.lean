f42723eedea88aeb10846ae3636789020869ca6c
int-y1 <jason_yuen2007@hotmail.com>
2023-02-02 04:25:59 -0800

    first pass

--- Mathlib/GroupTheory/PresentedGroup.lean
+++ Mathlib/GroupTheory/PresentedGroup.lean
@@ -19,10 +19,10 @@ given by generators `x : α` and relations `r ∈ rels`.
 
 ## Main definitions
 
-* `presented_group rels`: the quotient group of the free group on a type `α` by a subset `rels` of
+* `PresentedGroup rels`: the quotient group of the free group on a type `α` by a subset `rels` of
   relations of the free group on `α`.
 * `of`: The canonical map from `α` to a presented group with generators `α`.
-* `to_group f`: the canonical group homomorphism `presented_group rels → G`, given a function
+* `toGroup f`: the canonical group homomorphism `PresentedGroup rels → G`, given a function
   `f : α → G` from a type `α` to a group `G` which satisfies the relations `rels`.
 
 ## Tags
@@ -33,8 +33,8 @@ generators, relations, group presentations
 
 variable {α : Type _}
 
-/-- Given a set of relations, rels, over a type `α`, presented_group constructs the group with
-generators `x : α` and relations `rels` as a quotient of free_group `α`.-/
+/-- Given a set of relations, rels, over a type `α`, `PresentedGroup` constructs the group with
+generators `x : α` and relations `rels` as a quotient of `FreeGroup α`. -/
 def PresentedGroup (rels : Set (FreeGroup α)) :=
   FreeGroup α ⧸ Subgroup.normalClosure rels
 #align presented_group PresentedGroup
@@ -45,7 +45,7 @@ instance (rels : Set (FreeGroup α)) : Group (PresentedGroup rels) :=
   QuotientGroup.Quotient.group _
 
 /-- `of` is the canonical map from `α` to a presented group with generators `x : α`. The term `x` is
-mapped to the equivalence class of the image of `x` in `free_group α`. -/
+mapped to the equivalence class of the image of `x` in `FreeGroup α`. -/
 def of {rels : Set (FreeGroup α)} (x : α) : PresentedGroup rels :=
   QuotientGroup.mk (FreeGroup.of x)
 #align presented_group.of PresentedGroup.of
@@ -55,25 +55,26 @@ section ToGroup
 /-
 Presented groups satisfy a universal property. If `G` is a group and `f : α → G` is a map such that
 the images of `f` satisfy all the given relations, then `f` extends uniquely to a group homomorphism
-from `presented_group rels` to `G`.
+from `PresentedGroup rels` to `G`.
 -/
 variable {G : Type _} [Group G] {f : α → G} {rels : Set (FreeGroup α)}
 
 -- mathport name: exprF
 local notation "F" => FreeGroup.lift f
 
-variable (h : ∀ r ∈ rels, F r = 1)
+-- Porting note: `F` has been expanded, because `F r = 1` produces a sorry.
+variable (h : ∀ r ∈ rels, FreeGroup.lift f r = 1)
 
 theorem closure_rels_subset_ker : Subgroup.normalClosure rels ≤ MonoidHom.ker F :=
-  Subgroup.normalClosure_le_normal fun x w => (MonoidHom.mem_ker _).2 (h x w)
+  Subgroup.normalClosure_le_normal fun x w ↦ (MonoidHom.mem_ker _).2 (h x w)
 #align presented_group.closure_rels_subset_ker PresentedGroup.closure_rels_subset_ker
 
-theorem to_group_eq_one_of_mem_closure : ∀ x ∈ Subgroup.normalClosure rels, F x = 1 := fun x w =>
-  (MonoidHom.mem_ker _).1 <| closure_rels_subset_ker h w
+theorem to_group_eq_one_of_mem_closure : ∀ x ∈ Subgroup.normalClosure rels, F x = 1 :=
+  fun _ w ↦ (MonoidHom.mem_ker _).1 <| closure_rels_subset_ker h w
 #align presented_group.to_group_eq_one_of_mem_closure PresentedGroup.to_group_eq_one_of_mem_closure
 
 /-- The extension of a map `f : α → G` that satisfies the given relations to a group homomorphism
-from `presented_group rels → G`. -/
+from `PresentedGroup rels → G`. -/
 def toGroup : PresentedGroup rels →* G :=
   QuotientGroup.lift (Subgroup.normalClosure rels) F (to_group_eq_one_of_mem_closure h)
 #align presented_group.to_group PresentedGroup.toGroup
@@ -83,9 +84,11 @@ theorem toGroup.of {x : α} : toGroup h (of x) = f x :=
   FreeGroup.lift.of
 #align presented_group.to_group.of PresentedGroup.toGroup.of
 
-theorem toGroup.unique (g : PresentedGroup rels →* G) (hg : ∀ x : α, g (of x) = f x) :
-    ∀ {x}, g x = toGroup h x := fun x =>
-  QuotientGroup.induction_on x fun _ => FreeGroup.lift.unique (g.comp (QuotientGroup.mk' _)) hg
+theorem toGroup.unique (g : PresentedGroup rels →* G)
+    (hg : ∀ x : α, g (PresentedGroup.of x) = f x) : ∀ {x}, g x = toGroup h x := by
+  intro x
+  refine' QuotientGroup.induction_on x _
+  exact fun _ ↦ FreeGroup.lift.unique (g.comp (QuotientGroup.mk' _)) hg
 #align presented_group.to_group.unique PresentedGroup.toGroup.unique
 
 end ToGroup
@@ -94,4 +97,3 @@ instance (rels : Set (FreeGroup α)) : Inhabited (PresentedGroup rels) :=
   ⟨1⟩
 
 end PresentedGroup
-
