a4cb396c6d1e543aa6f6bb6e75037648f06c6389
Jason Yuen <jason_yuen2007@hotmail.com>
2023-02-02 09:51:24 +0000

    feat: port Order.Ideal (#1985)
    Co-authored-by: Johan Commelin <johan@commelin.net>

--- Mathlib.lean
+++ Mathlib.lean
@@ -791,6 +791,7 @@ import Mathlib.Order.Hom.Basic
 import Mathlib.Order.Hom.Bounded
 import Mathlib.Order.Hom.Order
 import Mathlib.Order.Hom.Set
+import Mathlib.Order.Ideal
 import Mathlib.Order.InitialSeg
 import Mathlib.Order.Iterate
 import Mathlib.Order.Lattice
