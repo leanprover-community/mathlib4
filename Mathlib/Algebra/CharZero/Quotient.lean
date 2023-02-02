fb76056a8a32df9a5595418ae4293285060f0f74
Moritz Firsching <firsching@google.com>
2023-02-02 22:58:10 +0100

    what to put for coe?

--- Mathlib/Algebra/CharZero/Quotient.lean
+++ Mathlib/Algebra/CharZero/Quotient.lean
@@ -63,8 +63,10 @@ theorem zmultiples_zsmul_eq_zsmul_iff {ψ θ : R ⧸ AddSubgroup.zmultiples p} {
   induction θ using Quotient.inductionOn'
   have : (Quotient.mk'' : R → R ⧸ AddSubgroup.zmultiples p) = coe := rfl
   simp only [this]
-  simp_rw [← coe_zsmul, ← coe_nsmul, ← coe_add, QuotientAddGroup.eq_iff_sub_mem, ← smul_sub, ←
-    sub_sub, AddSubgroup.zsmul_mem_zmultiples_iff_exists_sub_div hz]
+  simp_rw [← QuotientAddGroup.mk_zsmul, ← QuotientAddGroup.mk_nsmul, ← QuotientAddGroup.mk_add,
+    QuotientAddGroup.eq_iff_sub_mem, ← smul_sub, ← sub_sub,
+    AddSubgroup.zsmul_mem_zmultiples_iff_exists_sub_div hz]
+
 #align quotient_add_group.zmultiples_zsmul_eq_zsmul_iff
     quotientAddGroup.zmultiples_zsmul_eq_zsmul_iff
 
