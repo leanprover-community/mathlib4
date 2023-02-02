b5c7d54d83d66a29220057045d40cfc68eda031c
arienmalec <arien.malec@gmail.com>
2023-02-02 11:02:08 +0000

    feat: port data.pfun (#1179)
    Port of data.pfun
    
    Co-authored-by: Antoine Labelle <antoinelab01@gmail.com>
    Co-authored-by: ChrisHughes24 <chrishughes24@gmail.com>
    Co-authored-by: Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
    Co-authored-by: Yury G. Kudryashov <urkud@urkud.name>

--- Mathlib.lean
+++ Mathlib.lean
@@ -486,6 +486,7 @@ import Mathlib.Data.Option.Basic
 import Mathlib.Data.Option.Defs
 import Mathlib.Data.Option.NAry
 import Mathlib.Data.PEquiv
+import Mathlib.Data.PFun
 import Mathlib.Data.PFunctor.Univariate.Basic
 import Mathlib.Data.PNat.Basic
 import Mathlib.Data.PNat.Defs
