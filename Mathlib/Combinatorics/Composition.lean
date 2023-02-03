2696db8dc9a4e18da183ff6b368b5b04e8d85e1f
Arien Malec <arien.malec@gmail.com>
2023-02-03 09:51:45 -0800

    naming fix

--- Mathlib/Combinatorics/Composition.lean
+++ Mathlib/Combinatorics/Composition.lean
@@ -367,7 +367,7 @@ theorem mem_range_embedding_iff {j : Fin n} {i : Fin c.length} :
   · intro h
     rcases Set.mem_range.2 h with ⟨k, hk⟩
     rw [Fin.ext_iff] at hk
-    change c.size_up_to i + k = (j : ℕ) at hk
+    change c.sizeUpTo i + k = (j : ℕ) at hk
     rw [← hk]
     simp [size_up_to_succ', k.is_lt]
   · intro h
