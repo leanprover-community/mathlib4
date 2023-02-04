efb1d825ddedf1fab8018e800bc75b65fa5710aa
Moritz Firsching <firsching@google.com>
2023-02-04 20:46:49 +0100

    first pass

--- Mathlib/Order/Partition/Equipartition.lean
+++ Mathlib/Order/Partition/Equipartition.lean
@@ -19,7 +19,7 @@ difference of `1`.
 
 ## Main declarations
 
-* `finpartition.is_equipartition`: Predicate for a `finpartition` to be an equipartition.
+* `Finpartition.IsEquipartition`: Predicate for a `Finpartition` to be an equipartition.
 -/
 
 
@@ -38,50 +38,53 @@ theorem isEquipartition_iff_card_parts_eq_average :
     P.IsEquipartition ↔
       ∀ a : Finset α,
         a ∈ P.parts → a.card = s.card / P.parts.card ∨ a.card = s.card / P.parts.card + 1 :=
-  by simp_rw [is_equipartition, Finset.equitableOn_iff, P.sum_card_parts]
-#align finpartition.is_equipartition_iff_card_parts_eq_average Finpartition.isEquipartition_iff_card_parts_eq_average
+  by simp_rw [IsEquipartition, Finset.equitableOn_iff, P.sum_card_parts]
+#align finpartition.is_equipartition_iff_card_parts_eq_average
+    Finpartition.isEquipartition_iff_card_parts_eq_average
 
 variable {P}
 
-theorem Set.Subsingleton.isEquipartition (h : (P.parts : Set (Finset α)).Subsingleton) :
+theorem Set.Subsingleton.IsEquipartition (h : (P.parts : Set (Finset α)).Subsingleton) :
     P.IsEquipartition :=
-  h.EquitableOn _
-#align set.subsingleton.is_equipartition Set.Subsingleton.isEquipartition
+  Set.Subsingleton.equitableOn h _
+#align finpartition.set.subsingleton.is_equipartition Finpartition.Set.Subsingleton.isEquipartition
 
 theorem IsEquipartition.card_parts_eq_average (hP : P.IsEquipartition) (ht : t ∈ P.parts) :
     t.card = s.card / P.parts.card ∨ t.card = s.card / P.parts.card + 1 :=
   P.isEquipartition_iff_card_parts_eq_average.1 hP _ ht
-#align finpartition.is_equipartition.card_parts_eq_average Finpartition.IsEquipartition.card_parts_eq_average
+#align finpartition.is_equipartition.card_parts_eq_average
+    Finpartition.IsEquipartition.card_parts_eq_average
 
 theorem IsEquipartition.average_le_card_part (hP : P.IsEquipartition) (ht : t ∈ P.parts) :
     s.card / P.parts.card ≤ t.card := by
   rw [← P.sum_card_parts]
-  exact equitable_on.le hP ht
-#align finpartition.is_equipartition.average_le_card_part Finpartition.IsEquipartition.average_le_card_part
+  exact Finset.EquitableOn.le hP ht
+#align finpartition.is_equipartition.average_le_card_part
+    Finpartition.IsEquipartition.average_le_card_part
 
 theorem IsEquipartition.card_part_le_average_add_one (hP : P.IsEquipartition) (ht : t ∈ P.parts) :
     t.card ≤ s.card / P.parts.card + 1 := by
   rw [← P.sum_card_parts]
-  exact equitable_on.le_add_one hP ht
-#align finpartition.is_equipartition.card_part_le_average_add_one Finpartition.IsEquipartition.card_part_le_average_add_one
+  exact Finset.EquitableOn.le_add_one hP ht
+#align finpartition.is_equipartition.card_part_le_average_add_one
+    Finpartition.IsEquipartition.card_part_le_average_add_one
 
 /-! ### Discrete and indiscrete finpartition -/
 
 
-variable (s)
+variable (s) -- [Decidable (a = ⊥)]
 
 theorem botIsEquipartition : (⊥ : Finpartition s).IsEquipartition :=
   Set.equitableOn_iff_exists_eq_eq_add_one.2 ⟨1, by simp⟩
 #align finpartition.bot_is_equipartition Finpartition.botIsEquipartition
 
-theorem topIsEquipartition : (⊤ : Finpartition s).IsEquipartition :=
-  (parts_top_subsingleton _).IsEquipartition
+theorem topIsEquipartition [Decidable (s = ⊥)] : (⊤ : Finpartition s).IsEquipartition :=
+  Set.Subsingleton.IsEquipartition (parts_top_subsingleton _)
 #align finpartition.top_is_equipartition Finpartition.topIsEquipartition
 
 theorem indiscreteIsEquipartition {hs : s ≠ ∅} : (indiscrete hs).IsEquipartition := by
-  rw [is_equipartition, indiscrete_parts, coe_singleton]
+  rw [IsEquipartition, indiscrete_parts, coe_singleton]
   exact Set.equitableOn_singleton s _
 #align finpartition.indiscrete_is_equipartition Finpartition.indiscreteIsEquipartition
 
 end Finpartition
-
