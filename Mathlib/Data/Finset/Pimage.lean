6966feef8e91bf7addc56b9ef641f5a5012ffd4b
Komyyy <pol_tta@outlook.jp>
2023-02-04 15:18:03 +0900

    fix

--- Mathlib/Data/Finset/Pimage.lean
+++ Mathlib/Data/Finset/Pimage.lean
@@ -28,7 +28,7 @@ variable {α β : Type _}
 
 namespace Part
 
-/-- Convert a `o : Part α` with decidable `Part.dom o` to `finset α`. -/
+/-- Convert a `o : Part α` with decidable `Part.dom o` to `Finset α`. -/
 def toFinset (o : Part α) [Decidable o.Dom] : Finset α :=
   o.toOption.toFinset
 #align part.to_finset Part.toFinset
@@ -57,23 +57,21 @@ end Part
 
 namespace Finset
 
-variable [DecidableEq β] {f g : α →. β} [∀ _, Decidable (f _).Dom] [∀ _, Decidable (g _).Dom]
+variable [DecidableEq β] {f g : α →. β} [∀ x, Decidable (f x).Dom] [∀ x, Decidable (g x).Dom]
   {s t : Finset α} {b : β}
 
 /-- Image of `s : Finset α` under a partially defined function `f : α →. β`. -/
-noncomputable def pimage (f : α →. β) (s : Finset α) : Finset β := by
-  let _ : ∀ x, Decidable (f x).Dom := fun _ => Classical.dec _
-  exact s.bunionᵢ fun x => (f x).toFinset
+noncomputable def pimage (f : α →. β) [∀ x, Decidable (f x).Dom] (s : Finset α) : Finset β :=
+  s.bunionᵢ fun x => (f x).toFinset
 #align finset.pimage Finset.pimage
 
 @[simp]
 theorem mem_pimage : b ∈ s.pimage f ↔ ∃ a ∈ s, b ∈ f a := by
-  let _ : ∀ x, Decidable (f x).Dom := fun _ => Classical.dec _
   simp [pimage]
 #align finset.mem_pimage Finset.mem_pimage
 
 @[simp, norm_cast]
-theorem coe_pimage [∀ z, Decidable (f z).Dom] : (s.pimage f : Set β) = f.image s :=
+theorem coe_pimage : (s.pimage f : Set β) = f.image s :=
   Set.ext fun _ => mem_pimage
 #align finset.coe_pimage Finset.coe_pimage
 
@@ -84,40 +82,26 @@ theorem pimage_some (s : Finset α) (f : α → β) [∀ x, Decidable (Part.some
   simp [eq_comm]
 #align finset.pimage_some Finset.pimage_some
 
-theorem pimage_congr (h₁ : s = t) [∀ z, Decidable (f z).Dom]  [∀ z, Decidable (g z).Dom]
-    (h₂ : ∀ x ∈ t, f x = g x) :
-s.pimage f = t.pimage g := by
+theorem pimage_congr (h₁ : s = t) (h₂ : ∀ x ∈ t, f x = g x) : s.pimage f = t.pimage g := by
   subst s
   ext y
-  --refine' exists
-  simp (config := { contextual := true }) [h₂]
-  -- porting note: TODO: golf a little
-  refine' ⟨_,_⟩
-  · intros h
-    cases' h with w h
-    use w
-    rw [h₂ w h.1] at h
-    exact h
-  · intros h
-    cases' h with w h
-    use w
-    rw [← h₂ w h.1] at h
-    exact h
+  -- Porting note: `←exists_prop` required because `∃ x ∈ s, p x` is defined differently
+  simp (config := { contextual := true }) only [mem_pimage, ←exists_prop, h₂]
 #align finset.pimage_congr Finset.pimage_congr
 
-/-- Rewrite `s.pimage f` in terms of `finset.filter`, `finset.attach`, and `finset.image`. -/
-theorem pimage_eq_image_filter :
-    s.pimage f =
-      (filter (fun x => (f x).Dom) s).attach.image fun z =>
-      (f z).get (mem_filter.mp (Subtype.coe_prop z)).2 :=
-  by
+/-- Rewrite `s.pimage f` in terms of `Finset.filter`, `Finset.attach`, and `Finset.image`. -/
+theorem pimage_eq_image_filter : s.pimage f =
+    (filter (fun x => (f x).Dom) s).attach.image
+      fun x : { x // x ∈ filter (fun x => (f x).Dom) s } =>
+        (f x).get (mem_filter.mp x.coe_prop).2 := by
   ext x
-  simp [Part.mem_eq, And.exists, -exists_prop]
+  simp [Part.mem_eq, And.exists]
+  -- Porting note: `←exists_prop` required because `∃ x ∈ s, p x` is defined differently
+  simp only [←exists_prop]
 #align finset.pimage_eq_image_filter Finset.pimage_eq_image_filter
 
 theorem pimage_union [DecidableEq α] : (s ∪ t).pimage f = s.pimage f ∪ t.pimage f :=
   coe_inj.1 <| by
-  let _ : ∀ x, Decidable (f x).Dom := fun _ => Classical.dec _
   simp only [coe_pimage, coe_union, ← PFun.image_union]
 #align finset.pimage_union Finset.pimage_union
 
@@ -133,11 +117,10 @@ theorem pimage_subset {t : Finset β} : s.pimage f ⊆ t ↔ ∀ x ∈ s, ∀ y 
 
 -- @[mono] Porting note: mono not implemented yet
 theorem pimage_mono (h : s ⊆ t) : s.pimage f ⊆ t.pimage f :=
-  pimage_subset.2 fun x hx y hy => mem_pimage.2 ⟨x, h hx, hy⟩
+  pimage_subset.2 fun x hx _ hy => mem_pimage.2 ⟨x, h hx, hy⟩
 #align finset.pimage_mono Finset.pimage_mono
 
 theorem pimage_inter [DecidableEq α] : (s ∩ t).pimage f ⊆ s.pimage f ∩ t.pimage f := by
-  let _ : ∀ x, Decidable (f x).Dom := fun _ => Classical.dec _
   simp only [← coe_subset, coe_pimage, coe_inter, PFun.image_inter]
 #align finset.pimage_inter Finset.pimage_inter
 
