64fabb81d667632e48e22969361679f8a25d3579
Yury G. Kudryashov <urkud@urkud.name>
2023-02-05 12:29:48 -0600

    automated fixes
    Mathbin -> Mathlib
    
    fix certain import statements
    
    move "by" to end of line
    
    add import to Mathlib.lean

--- Mathlib/Topology/Algebra/Order/Archimedean.lean
+++ Mathlib/Topology/Algebra/Order/Archimedean.lean
@@ -8,8 +8,8 @@ Authors: Yury G. Kudryashov
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
-import Mathbin.Topology.Order.Basic
-import Mathbin.Algebra.Order.Archimedean
+import Mathlib.Topology.Order.Basic
+import Mathlib.Algebra.Order.Archimedean
 
 /-!
 # Rational numbers are dense in a linear ordered archimedean field
