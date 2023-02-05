8971bb0fffa4b219680f24ac70fa116704b4965d
Jason Yuen <jason_yuen2007@hotmail.com>
2023-02-05 01:17:17 +0000

    feat: port Topology.Paracompact (#2036)
    Co-authored-by: Lukas Miaskiwskyi <lukas.mias@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -986,6 +986,7 @@ import Mathlib.Topology.Maps
 import Mathlib.Topology.NhdsSet
 import Mathlib.Topology.OmegaCompletePartialOrder
 import Mathlib.Topology.Order
+import Mathlib.Topology.Paracompact
 import Mathlib.Topology.Separation
 import Mathlib.Topology.SubsetProperties
 import Mathlib.Topology.Support
