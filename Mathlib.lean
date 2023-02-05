801069dfa9b65e207d896328a64e281cd7a8df01
arienmalec <arien.malec@gmail.com>
2023-02-05 17:34:23 +0000

    feat: Port Data.Fintype.Perm (#1691)
    Port of data.fintype.perm
    
    
    
    Co-authored-by: qawbecrdtey <qawbecrdtey@naver.com>
    Co-authored-by: Joachim Breitner <mail@joachim-breitner.de>

--- Mathlib.lean
+++ Mathlib.lean
@@ -347,6 +347,7 @@ import Mathlib.Data.Fintype.List
 import Mathlib.Data.Fintype.Option
 import Mathlib.Data.Fintype.Order
 import Mathlib.Data.Fintype.Parity
+import Mathlib.Data.Fintype.Perm
 import Mathlib.Data.Fintype.Pi
 import Mathlib.Data.Fintype.Powerset
 import Mathlib.Data.Fintype.Prod
