81507dea2b04db06a9998de9cb7e2391563a8034
Moritz Firsching <moritz.firsching@gmail.com>
2023-02-05 12:45:18 +0000

    feat: port Order.Partition.Equipartition (#2058)
    Co-authored-by: Moritz Firsching <firsching@google.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -832,6 +832,7 @@ import Mathlib.Order.OmegaCompletePartialOrder
 import Mathlib.Order.OrdContinuous
 import Mathlib.Order.OrderIsoNat
 import Mathlib.Order.PartialSups
+import Mathlib.Order.Partition.Equipartition
 import Mathlib.Order.Partition.Finpartition
 import Mathlib.Order.PropInstances
 import Mathlib.Order.RelClasses
