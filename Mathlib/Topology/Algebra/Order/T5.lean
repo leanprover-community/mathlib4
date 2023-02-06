cca7e6b8d10996c86d6341c4ce7503dde9ee7c23
Yury G. Kudryashov <urkud@urkud.name>
2023-02-06 08:45:12 -0600

    Fix compile

--- Mathlib/Topology/Algebra/Order/T5.lean
+++ Mathlib/Topology/Algebra/Order/T5.lean
@@ -19,9 +19,7 @@ topological space.
 -/
 
 
-open Filter Set Function OrderDual
-
-open Topology Filter Interval
+open Filter Set Function OrderDual Topology Interval
 
 variable {X : Type _} [LinearOrder X] [TopologicalSpace X] [OrderTopology X] {a b c : X}
   {s t : Set X}
@@ -30,40 +28,36 @@ namespace Set
 
 @[simp]
 theorem ordConnectedComponent_mem_nhds : ordConnectedComponent s a ∈ 𝓝 a ↔ s ∈ 𝓝 a := by
-  refine' ⟨fun h => mem_of_superset h ord_connected_component_subset, fun h => _⟩
+  refine' ⟨fun h => mem_of_superset h ordConnectedComponent_subset, fun h => _⟩
   rcases exists_Icc_mem_subset_of_mem_nhds h with ⟨b, c, ha, ha', hs⟩
-  exact mem_of_superset ha' (subset_ord_connected_component ha hs)
+  exact mem_of_superset ha' (subset_ordConnectedComponent ha hs)
 #align set.ord_connected_component_mem_nhds Set.ordConnectedComponent_mem_nhds
 
 theorem compl_section_ordSeparatingSet_mem_nhdsWithin_Ici (hd : Disjoint s (closure t))
-    (ha : a ∈ s) : (ordConnectedSection <| ordSeparatingSet s t)ᶜ ∈ 𝓝[≥] a := by
+    (ha : a ∈ s) : (ordConnectedSection (ordSeparatingSet s t))ᶜ ∈ 𝓝[≥] a := by
   have hmem : tᶜ ∈ 𝓝[≥] a := by
     refine' mem_nhdsWithin_of_mem_nhds _
     rw [← mem_interior_iff_mem_nhds, interior_compl]
     exact disjoint_left.1 hd ha
   rcases exists_Icc_mem_subset_of_mem_nhdsWithin_Ici hmem with ⟨b, hab, hmem', hsub⟩
-  by_cases H : Disjoint (Icc a b) (ord_connected_section <| ord_separating_set s t)
+  by_cases H : Disjoint (Icc a b) (ordConnectedSection <| ordSeparatingSet s t)
   · exact mem_of_superset hmem' (disjoint_left.1 H)
   · simp only [Set.disjoint_left, not_forall, Classical.not_not] at H
     rcases H with ⟨c, ⟨hac, hcb⟩, hc⟩
-    have hsub' : Icc a b ⊆ ord_connected_component (tᶜ) a :=
-      subset_ord_connected_component (left_mem_Icc.2 hab) hsub
-    replace hac : a < c :=
-      hac.lt_of_ne
-        (Ne.symm <|
-          ne_of_mem_of_not_mem hc <|
-            disjoint_left.1
-              (disjoint_left_ord_separating_set.mono_right ord_connected_section_subset) ha)
+    have hsub' : Icc a b ⊆ ordConnectedComponent (tᶜ) a :=
+      subset_ordConnectedComponent (left_mem_Icc.2 hab) hsub
+    have hd : Disjoint s (ordConnectedSection (ordSeparatingSet s t)) :=
+      disjoint_left_ordSeparatingSet.mono_right ordConnectedSection_subset
+    replace hac : a < c := hac.lt_of_ne <| Ne.symm <| ne_of_mem_of_not_mem hc <|
+      disjoint_left.1 hd ha
     refine' mem_of_superset (Ico_mem_nhdsWithin_Ici (left_mem_Ico.2 hac)) fun x hx hx' => _
-    refine' hx.2.Ne (eq_of_mem_ord_connected_section_of_uIcc_subset hx' hc _)
-    refine' subset_inter (subset_Union₂_of_subset a ha _) _
-    ·
-      exact
-        ord_connected.uIcc_subset inferInstance (hsub' ⟨hx.1, hx.2.le.trans hcb⟩)
-          (hsub' ⟨hac.le, hcb⟩)
-    · rcases mem_Union₂.1 (ord_connected_section_subset hx').2 with ⟨y, hyt, hxy⟩
-      refine' subset_Union₂_of_subset y hyt (ord_connected.uIcc_subset inferInstance hxy _)
-      refine' subset_ord_connected_component left_mem_uIcc hxy _
+    refine' hx.2.ne (eq_of_mem_ordConnectedSection_of_uIcc_subset hx' hc _)
+    refine' subset_inter (subset_unionᵢ₂_of_subset a ha _) _
+    · exact OrdConnected.uIcc_subset inferInstance (hsub' ⟨hx.1, hx.2.le.trans hcb⟩)
+        (hsub' ⟨hac.le, hcb⟩)
+    · rcases mem_unionᵢ₂.1 (ordConnectedSection_subset hx').2 with ⟨y, hyt, hxy⟩
+      refine' subset_unionᵢ₂_of_subset y hyt (OrdConnected.uIcc_subset inferInstance hxy _)
+      refine' subset_ordConnectedComponent left_mem_uIcc hxy _
       suffices c < y by
         rw [uIcc_of_ge (hx.2.trans this).le]
         exact ⟨hx.2.le, this.le⟩
@@ -76,26 +70,24 @@ theorem compl_section_ordSeparatingSet_mem_nhdsWithin_Iic (hd : Disjoint s (clos
     (ha : a ∈ s) : (ordConnectedSection <| ordSeparatingSet s t)ᶜ ∈ 𝓝[≤] a := by
   have hd' : Disjoint (ofDual ⁻¹' s) (closure <| ofDual ⁻¹' t) := hd
   have ha' : toDual a ∈ ofDual ⁻¹' s := ha
-  simpa only [dual_ord_separating_set, dual_ord_connected_section] using
-    compl_section_ord_separating_set_mem_nhds_within_Ici hd' ha'
+  simpa only [dual_ordSeparatingSet, dual_ordConnectedSection] using
+    compl_section_ordSeparatingSet_mem_nhdsWithin_Ici hd' ha'
 #align set.compl_section_ord_separating_set_mem_nhds_within_Iic Set.compl_section_ordSeparatingSet_mem_nhdsWithin_Iic
 
 theorem compl_section_ordSeparatingSet_mem_nhds (hd : Disjoint s (closure t)) (ha : a ∈ s) :
     (ordConnectedSection <| ordSeparatingSet s t)ᶜ ∈ 𝓝 a := by
   rw [← nhds_left_sup_nhds_right, mem_sup]
   exact
-    ⟨compl_section_ord_separating_set_mem_nhds_within_Iic hd ha,
-      compl_section_ord_separating_set_mem_nhds_within_Ici hd ha⟩
+    ⟨compl_section_ordSeparatingSet_mem_nhdsWithin_Iic hd ha,
+      compl_section_ordSeparatingSet_mem_nhdsWithin_Ici hd ha⟩
 #align set.compl_section_ord_separating_set_mem_nhds Set.compl_section_ordSeparatingSet_mem_nhds
 
 theorem ordT5Nhd_mem_nhdsSet (hd : Disjoint s (closure t)) : ordT5Nhd s t ∈ 𝓝ˢ s :=
-  bUnion_mem_nhdsSet fun x hx =>
-    ordConnectedComponent_mem_nhds.2 <|
-      inter_mem
-        (by
-          rw [← mem_interior_iff_mem_nhds, interior_compl]
-          exact disjoint_left.1 hd hx)
-        (compl_section_ordSeparatingSet_mem_nhds hd hx)
+  bUnion_mem_nhdsSet fun x hx => ordConnectedComponent_mem_nhds.2 <| inter_mem
+    (by
+      rw [← mem_interior_iff_mem_nhds, interior_compl]
+      exact disjoint_left.1 hd hx)
+    (compl_section_ordSeparatingSet_mem_nhds hd hx)
 #align set.ord_t5_nhd_mem_nhds_set Set.ordT5Nhd_mem_nhdsSet
 
 end Set
@@ -104,9 +96,7 @@ open Set
 
 /-- A linear order with order topology is a completely normal Hausdorff topological space. -/
 instance (priority := 100) OrderTopology.t5Space : T5Space X :=
-  ⟨fun s t h₁ h₂ =>
-    Filter.disjoint_iff.2
-      ⟨ordT5Nhd s t, ordT5Nhd_mem_nhdsSet h₂, ordT5Nhd t s, ordT5Nhd_mem_nhdsSet h₁.symm,
-        disjoint_ordT5Nhd⟩⟩
+  ⟨fun s t h₁ h₂ => Filter.disjoint_iff.2
+    ⟨ordT5Nhd s t, ordT5Nhd_mem_nhdsSet h₂, ordT5Nhd t s, ordT5Nhd_mem_nhdsSet h₁.symm,
+      disjoint_ordT5Nhd⟩⟩
 #align order_topology.t5_space OrderTopology.t5Space
-
