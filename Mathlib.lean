46f5661d5ac59e67ad88367c1b7b0c6c92034830
Jason Yuen <jason_yuen2007@hotmail.com>
2023-02-06 07:04:45 +0000

    feat: port Data.Dfinsupp.Order (#1942)
    Co-authored-by: Joachim Breitner <mail@joachim-breitner.de>

--- Mathlib.lean
+++ Mathlib.lean
@@ -283,6 +283,7 @@ import Mathlib.Data.DList.Basic
 import Mathlib.Data.DList.Instances
 import Mathlib.Data.Dfinsupp.Basic
 import Mathlib.Data.Dfinsupp.NeLocus
+import Mathlib.Data.Dfinsupp.Order
 import Mathlib.Data.ENat.Basic
 import Mathlib.Data.ENat.Lattice
 import Mathlib.Data.Equiv.Functor
