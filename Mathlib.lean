b5813cc15e5cc5b4b66f98d603e3e7ae8b87eccf
Jason Yuen <jason_yuen2007@hotmail.com>
2023-02-06 08:48:11 +0000

    feat: port Topology.Order.Priestley (#2070)
    Co-authored-by: Johan Commelin <johan@commelin.net>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1014,6 +1014,7 @@ import Mathlib.Topology.NhdsSet
 import Mathlib.Topology.OmegaCompletePartialOrder
 import Mathlib.Topology.Order
 import Mathlib.Topology.Order.Basic
+import Mathlib.Topology.Order.Priestley
 import Mathlib.Topology.Paracompact
 import Mathlib.Topology.Perfect
 import Mathlib.Topology.Separation
