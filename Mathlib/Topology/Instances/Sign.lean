0b1a794c742eb33e85a96f6bb332fb7e58855295
Arien Malec <arien.malec@gmail.com>
2023-02-05 20:02:48 -0800

    Initial file copy from mathport

---
+++ Mathlib/Topology/Instances/Sign.lean
@@ -0,0 +1,63 @@
+/-
+Copyright (c) 2022 Joseph Myers. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Joseph Myers
+
+! This file was ported from Lean 3 source module topology.instances.sign
+! leanprover-community/mathlib commit 4c19a16e4b705bf135cf9a80ac18fcc99c438514
+! Please do not edit these lines, except to modify the commit id
+! if you have ported upstream changes.
+-/
+import Mathbin.Data.Sign
+import Mathbin.Topology.Order.Basic
+
+/-!
+# Topology on `sign_type`
+
+This file gives `sign_type` the discrete topology, and proves continuity results for `sign` in
+an `order_topology`.
+
+-/
+
+
+instance : TopologicalSpace SignType :=
+  ⊥
+
+instance : DiscreteTopology SignType :=
+  ⟨rfl⟩
+
+variable {α : Type _} [Zero α] [TopologicalSpace α]
+
+section PartialOrder
+
+variable [PartialOrder α] [DecidableRel ((· < ·) : α → α → Prop)] [OrderTopology α]
+
+theorem continuousAt_sign_of_pos {a : α} (h : 0 < a) : ContinuousAt SignType.sign a :=
+  by
+  refine' (continuousAt_const : ContinuousAt (fun x => (1 : SignType)) a).congr _
+  rw [Filter.EventuallyEq, eventually_nhds_iff]
+  exact ⟨{ x | 0 < x }, fun x hx => (sign_pos hx).symm, isOpen_lt' 0, h⟩
+#align continuous_at_sign_of_pos continuousAt_sign_of_pos
+
+theorem continuousAt_sign_of_neg {a : α} (h : a < 0) : ContinuousAt SignType.sign a :=
+  by
+  refine' (continuousAt_const : ContinuousAt (fun x => (-1 : SignType)) a).congr _
+  rw [Filter.EventuallyEq, eventually_nhds_iff]
+  exact ⟨{ x | x < 0 }, fun x hx => (sign_neg hx).symm, isOpen_gt' 0, h⟩
+#align continuous_at_sign_of_neg continuousAt_sign_of_neg
+
+end PartialOrder
+
+section LinearOrder
+
+variable [LinearOrder α] [OrderTopology α]
+
+theorem continuousAt_sign_of_ne_zero {a : α} (h : a ≠ 0) : ContinuousAt SignType.sign a :=
+  by
+  rcases h.lt_or_lt with (h_neg | h_pos)
+  · exact continuousAt_sign_of_neg h_neg
+  · exact continuousAt_sign_of_pos h_pos
+#align continuous_at_sign_of_ne_zero continuousAt_sign_of_ne_zero
+
+end LinearOrder
+
