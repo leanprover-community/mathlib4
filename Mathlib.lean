23bcb17993e7c0933dd32b48d32764b79fcb25c9
arienmalec <arien.malec@gmail.com>
2023-02-06 08:48:14 +0000

    feat:  port/Topology.Algebra.Order.MonotoneConvergence (#2097)
    port of topology.algebra.order.monotone_convergence
    
    
    
    Co-authored-by: Moritz Doll <moritz.doll@googlemail.com>
    Co-authored-by: Johan Commelin <johan@commelin.net>

--- Mathlib.lean
+++ Mathlib.lean
@@ -991,6 +991,7 @@ import Mathlib.Topology.Algebra.Order.ExtrClosure
 import Mathlib.Topology.Algebra.Order.IntermediateValue
 import Mathlib.Topology.Algebra.Order.LeftRight
 import Mathlib.Topology.Algebra.Order.MonotoneContinuity
+import Mathlib.Topology.Algebra.Order.MonotoneConvergence
 import Mathlib.Topology.Algebra.Star
 import Mathlib.Topology.Bases
 import Mathlib.Topology.Basic
