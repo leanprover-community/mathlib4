2a9385b0ba1974cd48299767897de7406bea4c39
casavaca <96765450+casavaca@users.noreply.github.com>
2023-02-07 10:37:28 -0800

    Remove newly-added docstring.
    I had the wrong impression that the linter would complain.

--- Mathlib/Data/Nat/Choose/Central.lean
+++ Mathlib/Data/Nat/Choose/Central.lean
@@ -37,22 +37,18 @@ def centralBinom (n : ℕ) :=
   (2 * n).choose n
 #align nat.central_binom Nat.centralBinom
 
-/-- expand centralBinom to its definition. -/
 theorem centralBinom_eq_two_mul_choose (n : ℕ) : centralBinom n = (2 * n).choose n :=
   rfl
 #align nat.central_binom_eq_two_mul_choose Nat.centralBinom_eq_two_mul_choose
 
-/-- centralBinom n > 0. -/
 theorem centralBinom_pos (n : ℕ) : 0 < centralBinom n :=
   choose_pos (Nat.le_mul_of_pos_left zero_lt_two)
 #align nat.central_binom_pos Nat.centralBinom_pos
 
-/-- centralBinom n ≠ 0. -/
 theorem centralBinom_ne_zero (n : ℕ) : centralBinom n ≠ 0 :=
   (centralBinom_pos n).ne'
 #align nat.central_binom_ne_zero Nat.centralBinom_ne_zero
 
-/-- The central binomial coefficient of 0 is 1. -/
 @[simp]
 theorem centralBinom_zero : centralBinom 0 = 1 :=
   choose_zero_right _
@@ -125,14 +121,12 @@ theorem four_pow_le_two_mul_self_mul_centralBinom :
                                                refine' le_mul_of_pos_left zero_lt_two
 #align nat.four_pow_le_two_mul_self_mul_central_binom Nat.four_pow_le_two_mul_self_mul_centralBinom
 
-/-- centralBinom (n+1) is dividable by two. -/
 theorem two_dvd_centralBinom_succ (n : ℕ) : 2 ∣ centralBinom (n + 1) := by
   use (n + 1 + n).choose n
   rw [centralBinom_eq_two_mul_choose, two_mul, ← add_assoc,
       choose_succ_succ' (n + 1 + n) n, choose_symm_add, ← two_mul]
 #align nat.two_dvd_central_binom_succ Nat.two_dvd_centralBinom_succ
 
-/-- centralBinom n, where 0 < n, is dividable by two. -/
 theorem two_dvd_centralBinom_of_one_le {n : ℕ} (h : 0 < n) : 2 ∣ centralBinom n := by
   rw [← Nat.succ_pred_eq_of_pos h]
   exact two_dvd_centralBinom_succ n.pred
