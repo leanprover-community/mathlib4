fbd57420a181d9c1f2803615c5a9c9d556e2fd79
maxwell-thum <maxwell.thum2718@gmail.com>
2023-02-07 07:39:58 +0000

    feat: port Data/Nat/Choose/Sum (#2121)
    Co-authored-by: Johan Commelin <johan@commelin.net>

--- Mathlib.lean
+++ Mathlib.lean
@@ -474,6 +474,7 @@ import Mathlib.Data.Nat.Cast.WithTop
 import Mathlib.Data.Nat.Choose.Basic
 import Mathlib.Data.Nat.Choose.Bounds
 import Mathlib.Data.Nat.Choose.Dvd
+import Mathlib.Data.Nat.Choose.Sum
 import Mathlib.Data.Nat.Dist
 import Mathlib.Data.Nat.EvenOddRec
 import Mathlib.Data.Nat.Factorial.Basic
