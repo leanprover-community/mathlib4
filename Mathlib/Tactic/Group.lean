4c287e4f253ac45032853188e789899341a1ab70
Thomas Browning <tb65536@uw.edu>
2023-02-06 16:31:52 -0800

    working tactic

--- Mathlib/Tactic/Group.lean
+++ Mathlib/Tactic/Group.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2020. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Patrick Massot
+Authors: Thomas Browning, Patrick Massot
 
 ! This file was ported from Lean 3 source module tactic.group
 ! leanprover-community/mathlib commit 4c19a16e4b705bf135cf9a80ac18fcc99c438514
@@ -9,11 +9,10 @@ Authors: Patrick Massot
 ! if you have ported upstream changes.
 -/
 import Mathlib.Tactic.Ring
-import Mathlib.Tactic.DocCommands
 import Mathlib.Algebra.Group.Commutator
 
 /-!
-# `group`
+# `group` tactic
 
 Normalizes expressions in the language of groups. The basic idea is to use the simplifier
 to put everything into a product of group powers (`zpow` which takes a group element and an
@@ -27,68 +26,56 @@ some `ring` invocations.
 group_theory
 -/
 
+namespace Mathlib.Tactic.Group
+
+open Lean
+open Lean.Meta
+open Lean.Parser.Tactic
+open Lean.Elab.Tactic
 
 -- The next four lemmas are not general purpose lemmas, they are intended for use only by
 -- the `group` tactic.
 @[to_additive]
-theorem Tactic.Group.zpow_trick {G : Type _} [Group G] (a b : G) (n m : ℤ) :
+theorem zpow_trick {G : Type _} [Group G] (a b : G) (n m : ℤ) :
     a * b ^ n * b ^ m = a * b ^ (n + m) := by rw [mul_assoc, ← zpow_add]
-#align tactic.group.zpow_trick Tactic.Group.zpow_trick
-#align tactic.group.zsmul_trick Tactic.Group.zsmul_trick
+#align tactic.group.zpow_trick Mathlib.Tactic.Group.zpow_trick
+#align tactic.group.zsmul_trick Mathlib.Tactic.Group.zsmul_trick
 
 @[to_additive]
-theorem Tactic.Group.zpow_trick_one {G : Type _} [Group G] (a b : G) (m : ℤ) :
+theorem zpow_trick_one {G : Type _} [Group G] (a b : G) (m : ℤ) :
     a * b * b ^ m = a * b ^ (m + 1) := by rw [mul_assoc, mul_self_zpow]
-#align tactic.group.zpow_trick_one Tactic.Group.zpow_trick_one
-#align tactic.group.zsmul_trick_zero Tactic.Group.zsmul_trick_zero
+#align tactic.group.zpow_trick_one Mathlib.Tactic.Group.zpow_trick_one
+#align tactic.group.zsmul_trick_zero Mathlib.Tactic.Group.zsmul_trick_zero
 
 @[to_additive]
-theorem Tactic.Group.zpow_trick_one' {G : Type _} [Group G] (a b : G) (n : ℤ) :
+theorem zpow_trick_one' {G : Type _} [Group G] (a b : G) (n : ℤ) :
     a * b ^ n * b = a * b ^ (n + 1) := by rw [mul_assoc, mul_zpow_self]
-#align tactic.group.zpow_trick_one' Tactic.Group.zpow_trick_one'
-#align tactic.group.zsmul_trick_zero' Tactic.Group.zsmul_trick_zero'
-
-@[to_additive]
-theorem Tactic.Group.zpow_trick_sub {G : Type _} [Group G] (a b : G) (n m : ℤ) :
-    a * b ^ n * b ^ (-m) = a * b ^ (n - m) := by rw [mul_assoc, ← zpow_add] <;> rfl
-#align tactic.group.zpow_trick_sub Tactic.Group.zpow_trick_sub
-#align tactic.group.zsmul_trick_sub Tactic.Group.zsmul_trick_sub
-
-namespace Tactic
-
-/- ./././Mathport/Syntax/Translate/Tactic/Mathlib/Core.lean:38:34: unsupported: setup_tactic_parser -/
-open Tactic.SimpArgType Interactive Tactic.Group
+#align tactic.group.zpow_trick_one' Mathlib.Tactic.Group.zpow_trick_one'
+#align tactic.group.zsmul_trick_zero' Mathlib.Tactic.Group.zsmul_trick_zero'
 
 /-- Auxiliary tactic for the `group` tactic. Calls the simplifier only. -/
-unsafe def aux_group₁ (locat : Loc) : tactic Unit :=
-  simp_core { failIfUnchanged := false } skip true
-      [expr ``(commutatorElement_def), expr ``(mul_one), expr ``(one_mul), expr ``(one_pow),
-        expr ``(one_zpow), expr ``(sub_self), expr ``(add_neg_self), expr ``(neg_add_self),
-        expr ``(neg_neg), expr ``(tsub_self), expr ``(Int.ofNat_add), expr ``(Int.ofNat_mul),
-        expr ``(Int.ofNat_zero), expr ``(Int.ofNat_one), expr ``(Int.ofNat_bit0),
-        expr ``(Int.ofNat_bit1), expr ``(Int.mul_neg_eq_neg_mul_symm),
-        expr ``(Int.neg_mul_eq_neg_mul_symm), symm_expr ``(zpow_ofNat), symm_expr ``(zpow_neg_one),
-        symm_expr ``(zpow_mul), symm_expr ``(zpow_add_one), symm_expr ``(zpow_one_add),
-        symm_expr ``(zpow_add), expr ``(mul_zpow_neg_one), expr ``(zpow_zero), expr ``(mul_zpow),
-        symm_expr ``(mul_assoc), expr ``(zpow_trick), expr ``(zpow_trick_one),
-        expr ``(zpow_trick_one'), expr ``(zpow_trick_sub), expr ``(Tactic.Ring.horner)]
-      [] locat >>
-    skip
-#align tactic.aux_group₁ tactic.aux_group₁
+syntax (name := aux_group₁) "aux_group₁" (ppSpace location)? : tactic
+
+macro_rules
+| `(tactic| aux_group₁ $[at $location]?) =>
+  `(tactic| simp (config := {decide := false}) only
+    [commutatorElement_def, mul_one, one_mul,
+      ←zpow_neg_one, ←zpow_ofNat, ←zpow_mul,
+      Int.ofNat_add, Int.ofNat_mul,
+      Int.mul_neg_eq_neg_mul_symm, Int.neg_mul_eq_neg_mul_symm, neg_neg,
+      one_zpow, zpow_zero, zpow_one, mul_zpow_neg_one,
+      ←mul_assoc,
+      ←zpow_add, ←zpow_add_one, ←zpow_one_add, zpow_trick, zpow_trick_one, zpow_trick_one',
+      tsub_self, sub_self, add_neg_self, neg_add_self]
+  $[at $location]?)
 
 /-- Auxiliary tactic for the `group` tactic. Calls `ring_nf` to normalize exponents. -/
-unsafe def aux_group₂ (locat : Loc) : tactic Unit :=
-  ring_nf none Tactic.Ring.NormalizeMode.raw locat
-#align tactic.aux_group₂ tactic.aux_group₂
+syntax (name := aux_group₂) "aux_group₂" (ppSpace location)? : tactic
 
-end Tactic
+macro_rules
+| `(tactic| aux_group₂ $[at $location]?) =>
+  `(tactic| ring_nf $[at $location]?)
 
-namespace Tactic.Interactive
-
-/- ./././Mathport/Syntax/Translate/Tactic/Mathlib/Core.lean:38:34: unsupported: setup_tactic_parser -/
-open Tactic
-
-/- ./././Mathport/Syntax/Translate/Expr.lean:333:4: warning: unsupported (TODO): `[tacs] -/
 /-- Tactic for normalizing expressions in multiplicative groups, without assuming
 commutativity, using only the group axioms without any information about which group
 is manipulated.
@@ -105,17 +92,10 @@ begin
 end
 ```
 -/
-unsafe def group (locat : parse location) : tactic Unit := do
-  when locat sorry
-  aux_group₁ locat
-  repeat (andthen (aux_group₂ locat) (aux_group₁ locat))
-#align tactic.interactive.group tactic.interactive.group
+syntax (name := group) "group" (ppSpace location)? : tactic
 
-end Tactic.Interactive
-
-add_tactic_doc
-  { Name := "group"
-    category := DocCategory.tactic
-    declNames := [`tactic.interactive.group]
-    tags := ["decision procedure", "simplification"] }
-
+macro_rules
+| `(tactic| group $[at $location]?) =>
+  `(tactic| aux_group₁ $[at $location]? <;>
+            aux_group₂ $[at $location]? <;>
+            aux_group₁ $[at $location]?)
