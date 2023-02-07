7b91a301780c5a30af61f19ec630e1ec83e730d8
Xavier-François Roblot <46200072+xroblot@users.noreply.github.com>
2023-02-07 10:07:27 +0100

    1st pass

--- Mathlib/NumberTheory/Primorial.lean
+++ Mathlib/NumberTheory/Primorial.lean
@@ -43,19 +43,19 @@ def primorial (n : ℕ) : ℕ :=
 local notation x "#" => primorial x
 
 theorem primorial_pos (n : ℕ) : 0 < n# :=
-  prod_pos fun p hp => (mem_filter.1 hp).2.Pos
+  prod_pos fun p hp ↦ (mem_filter.1 hp).2.pos
 #align primorial_pos primorial_pos
 
 theorem primorial_succ {n : ℕ} (hn1 : n ≠ 1) (hn : Odd n) : (n + 1)# = n# := by
-  refine' prod_congr _ fun _ _ => rfl
-  rw [range_succ, filter_insert, if_neg fun h => odd_iff_not_even.mp hn _]
-  exact h.even_sub_one <| mt succ.inj hn1
+  refine prod_congr ?_ fun _ _ ↦ rfl
+  rw [range_succ, filter_insert, if_neg fun h ↦ odd_iff_not_even.mp hn _]
+  exact fun h ↦ h.even_sub_one <| mt succ.inj hn1
 #align primorial_succ primorial_succ
 
 theorem primorial_add (m n : ℕ) :
     (m + n)# = m# * ∏ p in filter Nat.Prime (Ico (m + 1) (m + n + 1)), p := by
   rw [primorial, primorial, ← Ico_zero_eq_range, ← prod_union, ← filter_union, Ico_union_Ico_eq_Ico]
-  exacts[zero_le _, add_le_add_right (Nat.le_add_right _ _) _,
+  exacts [Nat.zero_le _, add_le_add_right (Nat.le_add_right _ _) _,
     disjoint_filter_filter <| Ico_disjoint_Ico_consecutive _ _ _]
 #align primorial_add primorial_add
 
@@ -64,13 +64,13 @@ theorem primorial_add_dvd {m n : ℕ} (h : n ≤ m) : (m + n)# ∣ m# * choose (
     (m + n)# = m# * ∏ p in filter Nat.Prime (Ico (m + 1) (m + n + 1)), p := primorial_add _ _
     _ ∣ m# * choose (m + n) m :=
       mul_dvd_mul_left _ <|
-        prod_primes_dvd _ (fun k hk => (mem_filter.1 hk).2.Prime) fun p hp =>
+        prod_primes_dvd _ (fun k hk ↦ (mem_filter.1 hk).2.prime) fun p hp ↦
           by
           rw [mem_filter, mem_Ico] at hp
           exact
             hp.2.dvd_choose_add hp.1.1 (h.trans_lt (m.lt_succ_self.trans_le hp.1.1))
               (Nat.lt_succ_iff.1 hp.1.2)
-    
+
 #align primorial_add_dvd primorial_add_dvd
 
 theorem primorial_add_le {m n : ℕ} (h : n ≤ m) : (m + n)# ≤ m# * choose (m + n) m :=
@@ -79,7 +79,7 @@ theorem primorial_add_le {m n : ℕ} (h : n ≤ m) : (m + n)# ≤ m# * choose (m
 
 theorem primorial_le_4_pow (n : ℕ) : n# ≤ 4 ^ n := by
   induction' n using Nat.strong_induction_on with n ihn
-  cases n; · rfl
+  cases' n with n; · rfl
   rcases n.even_or_odd with (⟨m, rfl⟩ | ho)
   · rcases m.eq_zero_or_pos with (rfl | hm)
     · decide
@@ -90,7 +90,7 @@ theorem primorial_le_4_pow (n : ℕ) : n# ≤ 4 ^ n := by
       _ ≤ 4 ^ (m + 1) * 4 ^ m :=
         mul_le_mul' (ihn _ <| succ_lt_succ <| (lt_add_iff_pos_left _).2 hm) (choose_middle_le_pow _)
       _ ≤ 4 ^ (m + m + 1) := by rw [← pow_add, add_right_comm]
-      
+
   · rcases Decidable.eq_or_ne n 1 with (rfl | hn)
     · decide
     ·
@@ -98,6 +98,5 @@ theorem primorial_le_4_pow (n : ℕ) : n# ≤ 4 ^ n := by
         (n + 1)# = n# := primorial_succ hn ho
         _ ≤ 4 ^ n := ihn n n.lt_succ_self
         _ ≤ 4 ^ (n + 1) := pow_le_pow_of_le_right four_pos n.le_succ
-        
-#align primorial_le_4_pow primorial_le_4_pow
 
+#align primorial_le_4_pow primorial_le_4_pow
