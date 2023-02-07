b00ce534696a0857415d732425cc0823a766ba3a
Xavier-François Roblot <46200072+xroblot@users.noreply.github.com>
2023-02-07 13:59:40 +0000

    feat: port Data.Finsupp.Antidiagonal (#2142)

--- Mathlib.lean
+++ Mathlib.lean
@@ -336,6 +336,7 @@ import Mathlib.Data.Finset.Slice
 import Mathlib.Data.Finset.Sort
 import Mathlib.Data.Finset.Sum
 import Mathlib.Data.Finsupp.AList
+import Mathlib.Data.Finsupp.Antidiagonal
 import Mathlib.Data.Finsupp.Basic
 import Mathlib.Data.Finsupp.BigOperators
 import Mathlib.Data.Finsupp.Defs
