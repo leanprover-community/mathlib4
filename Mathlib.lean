f9869d8a03007fe8b1c75d32d433821962ec026e
thorimur <68410468+thorimur@users.noreply.github.com>
2023-02-07 15:41:02 -0500

    chore: update `Mathlib.lean`

--- Mathlib.lean
+++ Mathlib.lean
@@ -987,6 +987,7 @@ import Mathlib.Tactic.Trace
 import Mathlib.Tactic.TypeCheck
 import Mathlib.Tactic.UnsetOption
 import Mathlib.Tactic.Use
+import Mathlib.Tactic.WLOG
 import Mathlib.Tactic.Zify
 import Mathlib.Tactic.Zify.Attr
 import Mathlib.Testing.SlimCheck.Gen
