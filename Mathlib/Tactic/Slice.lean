2c56111eafac3bdc6298b3b4bfec8bc209fc8c92
thorimur <68410468+thorimur@users.noreply.github.com>
2023-02-08 14:57:53 -0500

    use `Mathport` syntax * use existing docs * fix docs typos

--- Mathlib/Tactic/Slice.lean
+++ Mathlib/Tactic/Slice.lean
@@ -10,11 +10,10 @@ import Mathlib.Tactic.Conv
 /-!
 # The `slice` tactic
 
-Applies a tactic to an interval of terms from a term obtained by repeated application 
-of `Category.comp`. 
+Applies a tactic to an interval of terms from a term obtained by repeated application
+of `Category.comp`.
 
 -/
-
 
 open CategoryTheory
 open Lean Parser.Tactic Elab Command Elab.Tactic Meta
@@ -27,13 +26,20 @@ open Tactic
 open Parser.Tactic.Conv
 
 /--
-`evalSlice` 
-- rewrites the target express using `Category.assoc`. 
+`slice` is a conv tactic; if the current focus is a composition of several morphisms,
+`slice a b` reassociates as needed, and zooms in on the `a`-th through `b`-th morphisms.
+Thus if the current focus is `(a ≫ b) ≫ ((c ≫ d) ≫ e)`, then `slice 2 3` zooms to `b ≫ c`.
+ -/
+syntax (name := slice) "slice " num num : conv
+
+/--
+`evalSlice`
+- rewrites the target expression using `Category.assoc`.
 - uses `congr` to split off the first `a-1` terms and rotates to `a`-th (last) term
-- it counts the number `k` of rewrites as it uses `←Category.assoc` to bring the target to 
+- counts the number `k` of rewrites as it uses `←Category.assoc` to bring the target to
   left associated form; from the first step this is the total number of remaining terms from `C`
-- it now splits off `b-a` terms from target using `congr` leaving the desired subterm 
-- finally, it rewrites it once more using `Category.assoc` to bring it right associated 
+- it now splits off `b-a` terms from target using `congr` leaving the desired subterm
+- finally, it rewrites it once more using `Category.assoc` to bring it to right-associated
   normal form
 -/
 def evalSlice (a b : Nat) : TacticM Unit := do
@@ -49,25 +55,25 @@ def evalSlice (a b : Nat) : TacticM Unit := do
   let _ ← iterateUntilFailureWithResults do
     evalTactic (← `(conv| rw [Category.assoc]))
 
-/-- `slice` is implemented by `evalSlice` -/
-elab "slice" a:num b:num : conv => evalSlice a.getNat b.getNat
+/-- `slice` is implemented by `evalSlice`. -/
+elab "slice " a:num b:num : conv => evalSlice a.getNat b.getNat
 
-/-- 
-`sliceLHS a b => tac` is a conv tactic which enters the LHS of a target, 
-uses `slice` to extract the `a` through `b` terms, and then applies 
-`tac` to the result. 
+/--
+`slice_lhs a b => tac` zooms to the left hand side, uses associativity for categorical
+composition as needed, zooms in on the `a`-th through `b`-th morphisms, and invokes `tac`.
 -/
-syntax (name := sliceLHS) "sliceLHS" num num " => " convSeq : tactic
+syntax (name := sliceLHS) "slice_lhs " num num " => " convSeq : tactic
 macro_rules
-  | `(tactic| sliceLHS $a $b => $seq) =>
+  | `(tactic| slice_lhs $a $b => $seq) =>
     `(tactic| conv => lhs; slice $a $b; ($seq:convSeq))
 
 /--
-`sliceRHS a b => tac` works as `sliceLHS` but on the RHS similarly
+`slice_rhs a b => tac` zooms to the right hand side, uses associativity for categorical
+composition as needed, zooms in on the `a`-th through `b`-th morphisms, and invokes `tac`.
 -/
-syntax (name := sliceRHS) "sliceRHS" num num " => " convSeq : tactic
+syntax (name := sliceRHS) "slice_rhs " num num " => " convSeq : tactic
 macro_rules
-  | `(tactic| sliceRHS $a $b => $seq) =>
+  | `(tactic| slice_rhs $a $b => $seq) =>
     `(tactic| conv => rhs; slice $a $b; ($seq:convSeq))
 
 /- Porting note: update when `add_tactic_doc` is supported` -/
