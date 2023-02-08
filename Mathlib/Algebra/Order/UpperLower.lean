a3c0862bdfac76f8944e21845dd9eebd3cce50d5
Evgenia Karunus <lakesare@gmail.com>
2023-02-08 18:22:02 +0400

    Linting: fix all `to_additive`

--- Mathlib/Algebra/Order/UpperLower.lean
+++ Mathlib/Algebra/Order/UpperLower.lean
@@ -151,31 +151,31 @@ instance : Div (UpperSet α) :=
 instance : SMul α (UpperSet α) :=
   ⟨fun a s ↦ ⟨(· • ·) a '' s, s.2.smul⟩⟩
 
-@[simp, norm_cast, to_additive]
+@[to_additive (attr := simp,norm_cast)]
 theorem coe_one : ((1 : UpperSet α) : Set α) = Set.Ici 1 :=
   rfl
 #align upper_set.coe_one UpperSet.coe_one
 #align upper_set.coe_zero UpperSet.coe_zero
 
-@[simp, norm_cast, to_additive]
+@[to_additive (attr := simp,norm_cast)]
 theorem coe_smul (a : α) (s : UpperSet α) : (↑(a • s) : Set α) = a • s :=
   rfl
 #align upper_set.coe_smul UpperSet.coe_smul
 #align upper_set.coe_vadd UpperSet.coe_vadd
 
-@[simp, norm_cast, to_additive]
+@[to_additive (attr := simp,norm_cast)]
 theorem coe_mul (s t : UpperSet α) : (↑(s * t) : Set α) = s * t :=
   rfl
 #align upper_set.coe_mul UpperSet.coe_mul
 #align upper_set.coe_add UpperSet.coe_add
 
-@[simp, norm_cast, to_additive]
+@[to_additive (attr := simp,norm_cast)]
 theorem coe_div (s t : UpperSet α) : (↑(s / t) : Set α) = s / t :=
   rfl
 #align upper_set.coe_div UpperSet.coe_div
 #align upper_set.coe_sub UpperSet.coe_sub
 
-@[simp, to_additive]
+@[to_additive (attr := simp)]
 theorem Ici_one : Ici (1 : α) = 1 :=
   rfl
 #align upper_set.Ici_one UpperSet.Ici_one
@@ -227,25 +227,25 @@ instance : Div (LowerSet α) :=
 instance : SMul α (LowerSet α) :=
   ⟨fun a s ↦ ⟨(· • ·) a '' s, s.2.smul⟩⟩
 
-@[simp, norm_cast, to_additive]
+@[to_additive (attr := simp,norm_cast)]
 theorem coe_smul (a : α) (s : LowerSet α) : (↑(a • s) : Set α) = a • s :=
   rfl
 #align lower_set.coe_smul LowerSet.coe_smul
 #align lower_set.coe_vadd LowerSet.coe_vadd
 
-@[simp, norm_cast, to_additive]
+@[to_additive (attr := simp,norm_cast)]
 theorem coe_mul (s t : LowerSet α) : (↑(s * t) : Set α) = s * t :=
   rfl
 #align lower_set.coe_mul LowerSet.coe_mul
 #align lower_set.coe_add LowerSet.coe_add
 
-@[simp, norm_cast, to_additive]
+@[to_additive (attr := simp,norm_cast)]
 theorem coe_div (s t : LowerSet α) : (↑(s / t) : Set α) = s / t :=
   rfl
 #align lower_set.coe_div LowerSet.coe_div
 #align lower_set.coe_sub LowerSet.coe_sub
 
-@[simp, to_additive]
+@[to_additive (attr := simp)]
 theorem Iic_one : Iic (1 : α) = 1 :=
   rfl
 #align lower_set.Iic_one LowerSet.Iic_one
@@ -281,25 +281,25 @@ end LowerSet
 
 variable (a s t)
 
-@[simp, to_additive]
+@[to_additive (attr := simp)]
 theorem upperClosure_one : upperClosure (1 : Set α) = 1 :=
   upperClosure_singleton _
 #align upper_closure_one upperClosure_one
 #align upper_closure_zero upperClosure_zero
 
-@[simp, to_additive]
+@[to_additive (attr := simp)]
 theorem lowerClosure_one : lowerClosure (1 : Set α) = 1 :=
   lowerClosure_singleton _
 #align lower_closure_one lowerClosure_one
 #align lower_closure_zero lowerClosure_zero
 
-@[simp, to_additive]
+@[to_additive (attr := simp)]
 theorem upperClosure_smul : upperClosure (a • s) = a • upperClosure s :=
   upperClosure_image <| OrderIso.mulLeft a
 #align upper_closure_smul upperClosure_smul
 #align upper_closure_vadd upperClosure_vadd
 
-@[simp, to_additive]
+@[to_additive (attr := simp)]
 theorem lowerClosure_smul : lowerClosure (a • s) = a • lowerClosure s :=
   lowerClosure_image <| OrderIso.mulLeft a
 #align lower_closure_smul lowerClosure_smul
@@ -337,14 +337,14 @@ theorem lowerClosure_mul : ↑(lowerClosure s) * t = lowerClosure (s * t) :=
 #align lower_closure_mul lowerClosure_mul
 #align lower_closure_add lowerClosure_add
 
-@[simp, to_additive]
+@[to_additive (attr := simp)]
 theorem upperClosure_mul_distrib : upperClosure (s * t) = upperClosure s * upperClosure t :=
   SetLike.coe_injective <| by
     rw [UpperSet.coe_mul, mul_upperClosure, upperClosure_mul, UpperSet.upperClosure]
 #align upper_closure_mul_distrib upperClosure_mul_distrib
 #align upper_closure_add_distrib upperClosure_add_distrib
 
-@[simp, to_additive]
+@[to_additive (attr := simp)]
 theorem lowerClosure_mul_distrib : lowerClosure (s * t) = lowerClosure s * lowerClosure t :=
   SetLike.coe_injective <| by
     rw [LowerSet.coe_mul, mul_lowerClosure, lowerClosure_mul, LowerSet.lowerClosure]
@@ -352,3 +352,5 @@ theorem lowerClosure_mul_distrib : lowerClosure (s * t) = lowerClosure s * lower
 #align lower_closure_add_distrib lowerClosure_add_distrib
 
 end OrderedCommGroup
+
+#lint
