8275ee7e27ce35bb68bacba9c615568f3ceac5b4
Komyyy <pol_tta@outlook.jp>
2023-02-08 15:42:21 +0900

    fix

--- Mathlib/Combinatorics/SimpleGraph/Regularity/Equitabilise.lean
+++ Mathlib/Combinatorics/SimpleGraph/Regularity/Equitabilise.lean
@@ -21,9 +21,9 @@ This file allows to blow partitions up into parts of controlled size. Given a pa
 
 ## Main declarations
 
-* `finpartition.equitabilise`: `P.equitabilise h` where `h : a * m + b * (m + 1)` is a partition
+* `Finpartition.equitabilise`: `P.equitabilise h` where `h : a * m + b * (m + 1)` is a partition
   with `a` parts of size `m` and `b` parts of size `m + 1` which almost refines `P`.
-* `finpartition.exists_equipartition_card_eq`: We can find equipartitions of arbitrary size.
+* `Finpartition.exists_equipartition_card_eq`: We can find equipartitions of arbitrary size.
 -/
 
 
@@ -51,44 +51,42 @@ theorem equitabilise_aux (hs : a * m + b * (m + 1) = s.card) :
     exact fun x hx a ha =>
       ⟨{a}, mem_map_of_mem _ (P.le hx ha), singleton_subset_iff.2 ha, mem_singleton_self _⟩
   -- Prove the case `m > 0` by strong induction on `s`
-  induction' s using Finset.strongInduction with s ih
+  induction' s using Finset.strongInduction with s ih generalizing a b
   -- If `a = b = 0`, then `s = ∅` and we can partition into zero parts
   by_cases hab : a = 0 ∧ b = 0
-  · simp only [hab.1, hab.2, add_zero, zero_mul, eq_comm, card_eq_zero] at hs
+  · simp only [hab.1, hab.2, add_zero, zero_mul, eq_comm, card_eq_zero, Finset.bot_eq_empty] at hs
     subst hs
-    have : P = ⊥ := by
-      sorry
+    -- Porting note: to synthesize `Finpartition ∅`, `have` is required
+    have : P = Finpartition.empty _ := Unique.eq_default (α := Finpartition ⊥) P
     exact ⟨Finpartition.empty _, by simp, by simp [this], by simp [hab.2]⟩
   simp_rw [not_and_or, ← Ne.def, ← pos_iff_ne_zero] at hab
   -- `n` will be the size of the smallest part
   set n := if 0 < a then m else m + 1 with hn
   -- Some easy facts about it
   obtain ⟨hn₀, hn₁, hn₂, hn₃⟩ :
-    0 < n ∧
-      n ≤ m + 1 ∧
-        n ≤ a * m + b * (m + 1) ∧
-          ite (0 < a) (a - 1) a * m + ite (0 < a) b (b - 1) * (m + 1) = s.card - n :=
+    0 < n ∧ n ≤ m + 1 ∧ n ≤ a * m + b * (m + 1) ∧
+      ite (0 < a) (a - 1) a * m + ite (0 < a) b (b - 1) * (m + 1) = s.card - n :=
     by
     rw [hn, ← hs]
-    split_ifs <;> rw [tsub_mul, one_mul]
+    split_ifs with h <;> rw [tsub_mul, one_mul]
     · refine' ⟨m_pos, le_succ _, le_add_right (le_mul_of_pos_left ‹0 < a›), _⟩
       rw [tsub_add_eq_add_tsub (le_mul_of_pos_left h)]
     · refine' ⟨succ_pos', le_rfl, le_add_left (le_mul_of_pos_left <| hab.resolve_left ‹¬0 < a›), _⟩
       rw [← add_tsub_assoc_of_le (le_mul_of_pos_left <| hab.resolve_left ‹¬0 < a›)]
   /- We will call the inductive hypothesis on a partition of `s \ t` for a carefully chosen `t ⊆ s`.
     To decide which, however, we must distinguish the case where all parts of `P` have size `m` (in
-    which case we take `t` to be an arbitrary subset of `s` of size `n`) from the case where at least
-    one part `u` of `P` has size `m + 1` (in which case we take `t` to be an arbitrary subset of `u`
-    of size `n`). The rest of each branch is just tedious calculations to satisfy the induction
-    hypothesis. -/
+    which case we take `t` to be an arbitrary subset of `s` of size `n`) from the case where at
+    least one part `u` of `P` has size `m + 1` (in which case we take `t` to be an arbitrary subset
+    of `u` of size `n`). The rest of each branch is just tedious calculations to satisfy the
+    induction hypothesis. -/
   by_cases ∀ u ∈ P.parts, card u < m + 1
   · obtain ⟨t, hts, htn⟩ := exists_smaller_set s n (hn₂.trans_eq hs)
     have ht : t.Nonempty := by rwa [← card_pos, htn]
     have hcard : ite (0 < a) (a - 1) a * m + ite (0 < a) b (b - 1) * (m + 1) = (s \ t).card := by
       rw [card_sdiff ‹t ⊆ s›, htn, hn₃]
-    obtain ⟨R, hR₁, hR₂, hR₃⟩ :=
-      @ih (s \ t) (sdiff_ssubset hts ‹t.Nonempty›) (P.avoid t) (if 0 < a then a - 1 else a)
-        (if 0 < a then b else b - 1) hcard
+    obtain ⟨R, hR₁, _, hR₃⟩ :=
+      @ih (s \ t) (sdiff_ssubset hts ‹t.Nonempty›) (if 0 < a then a - 1 else a)
+        (if 0 < a then b else b - 1) (P.avoid t) hcard
     refine' ⟨R.extend ht.ne_empty sdiff_disjoint (sdiff_sup_cancel hts), _, _, _⟩
     · simp only [extend_parts, mem_insert, forall_eq_or_imp, and_iff_left hR₁, htn, hn]
       exact ite_eq_or_eq _ _ _
@@ -96,9 +94,9 @@ theorem equitabilise_aux (hs : a * m + b * (m + 1) = s.card) :
     simp_rw [extend_parts, filter_insert, htn, hn, m.succ_ne_self.symm.ite_eq_right_iff]
     split_ifs with ha
     · rw [hR₃, if_pos ha]
-    rw [card_insert_of_not_mem fun H => _, hR₃, if_neg ha, tsub_add_cancel_of_le]
+    rw [card_insert_of_not_mem, hR₃, if_neg ha, tsub_add_cancel_of_le]
     · exact hab.resolve_left ha
-    · exact ht.ne_empty (le_sdiff_iff.1 <| R.le <| filter_subset _ _ H)
+    · intro H; exact ht.ne_empty (le_sdiff_iff.1 <| R.le <| filter_subset _ _ H)
   push_neg  at h
   obtain ⟨u, hu₁, hu₂⟩ := h
   obtain ⟨t, htu, htn⟩ := exists_smaller_set _ _ (hn₁.trans hu₂)
@@ -106,36 +104,38 @@ theorem equitabilise_aux (hs : a * m + b * (m + 1) = s.card) :
   have hcard : ite (0 < a) (a - 1) a * m + ite (0 < a) b (b - 1) * (m + 1) = (s \ t).card := by
     rw [card_sdiff (htu.trans <| P.le hu₁), htn, hn₃]
   obtain ⟨R, hR₁, hR₂, hR₃⟩ :=
-    @ih (s \ t) (sdiff_ssubset (htu.trans <| P.le hu₁) ht) (P.avoid t) (if 0 < a then a - 1 else a)
-      (if 0 < a then b else b - 1) hcard
+    @ih (s \ t) (sdiff_ssubset (htu.trans <| P.le hu₁) ht) (if 0 < a then a - 1 else a)
+      (if 0 < a then b else b - 1) (P.avoid t) hcard
   refine' ⟨R.extend ht.ne_empty sdiff_disjoint (sdiff_sup_cancel <| htu.trans <| P.le hu₁), _, _, _⟩
   · simp only [mem_insert, forall_eq_or_imp, extend_parts, and_iff_left hR₁, htn, hn]
     exact ite_eq_or_eq _ _ _
   · conv in _ ∈ _ => rw [← insert_erase hu₁]
     simp only [and_imp, mem_insert, forall_eq_or_imp, Ne.def, extend_parts]
     refine' ⟨_, fun x hx => (card_le_of_subset _).trans <| hR₂ x _⟩
-    · simp only [filter_insert, if_pos htu, bUnion_insert, mem_erase, id.def]
+    · simp only [filter_insert, if_pos htu, bunionᵢ_insert, mem_erase, id.def]
       obtain rfl | hut := eq_or_ne u t
       · rw [sdiff_eq_empty_iff_subset.2 (subset_union_left _ _)]
         exact bot_le
       refine'
         (card_le_of_subset fun i => _).trans
           (hR₂ (u \ t) <| P.mem_avoid.2 ⟨u, hu₁, fun i => hut <| i.antisymm htu, rfl⟩)
-      simp only [not_exists, mem_bUnion, and_imp, mem_union, mem_filter, mem_sdiff, id.def, not_or]
+      -- Porting note: `not_and` required because `∃ x ∈ s, p x` is defined diferently
+      simp only [not_exists, not_and, mem_bunionᵢ, and_imp, mem_union, mem_filter, mem_sdiff,
+        id.def, not_or]
       exact fun hi₁ hi₂ hi₃ =>
         ⟨⟨hi₁, hi₂⟩, fun x hx hx' => hi₃ _ hx <| hx'.trans <| sdiff_subset _ _⟩
-    · apply sdiff_subset_sdiff subset.rfl (bUnion_subset_bUnion_of_subset_left _ _)
+    · apply sdiff_subset_sdiff Subset.rfl (bunionᵢ_subset_bunionᵢ_of_subset_left _ _)
       exact filter_subset_filter _ (subset_insert _ _)
-    simp only [avoid, of_erase, mem_erase, mem_image, bot_eq_empty]
+    simp only [avoid, ofErase, mem_erase, mem_image, bot_eq_empty]
     exact
       ⟨(nonempty_of_mem_parts _ <| mem_of_mem_erase hx).ne_empty, _, mem_of_mem_erase hx,
         (disjoint_of_subset_right htu <|
             P.disjoint (mem_of_mem_erase hx) hu₁ <| ne_of_mem_erase hx).sdiff_eq_left⟩
   simp only [extend_parts, filter_insert, htn, hn, m.succ_ne_self.symm.ite_eq_right_iff]
-  split_ifs
+  split_ifs with h
   · rw [hR₃, if_pos h]
-  · rw [card_insert_of_not_mem fun H => _, hR₃, if_neg h, Nat.sub_add_cancel (hab.resolve_left h)]
-    exact ht.ne_empty (le_sdiff_iff.1 <| R.le <| filter_subset _ _ H)
+  · rw [card_insert_of_not_mem, hR₃, if_neg h, Nat.sub_add_cancel (hab.resolve_left h)]
+    intro H; exact ht.ne_empty (le_sdiff_iff.1 <| R.le <| filter_subset _ _ H)
 #align finpartition.equitabilise_aux Finpartition.equitabilise_aux
 
 variable (h : a * m + b * (m + 1) = s.card)
@@ -157,7 +157,7 @@ theorem card_eq_of_mem_parts_equitabilise :
 #align finpartition.card_eq_of_mem_parts_equitabilise Finpartition.card_eq_of_mem_parts_equitabilise
 
 theorem equitabilise_isEquipartition : (P.equitabilise h).IsEquipartition :=
-  Set.equitableOn_iff_exists_eq_eq_add_one.2 ⟨m, fun u => card_eq_of_mem_parts_equitabilise⟩
+  Set.equitableOn_iff_exists_eq_eq_add_one.2 ⟨m, fun _ => card_eq_of_mem_parts_equitabilise⟩
 #align finpartition.equitabilise_is_equipartition Finpartition.equitabilise_isEquipartition
 
 variable (P h)
