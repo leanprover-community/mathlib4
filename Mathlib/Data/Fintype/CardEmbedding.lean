4feaff521e651201c8e71aba20e8075177e68b1d
Alex Kassil <alexkassil@sbcglobal.net>
2023-02-08 23:25:03 -0800

    Step in the right direction, using rewrite' instead of induction'

--- Mathlib/Data/Fintype/CardEmbedding.lean
+++ Mathlib/Data/Fintype/CardEmbedding.lean
@@ -11,6 +11,9 @@ Authors: Eric Rodriguez
 import Mathlib.Data.Fintype.BigOperators
 import Mathlib.Logic.Equiv.Embedding
 
+set_option autoImplicit true -- **TODO** delete this later
+-- set_option pp.explicit true -- **TODO** delete this later
+
 /-!
 # Number of embeddings
 
@@ -35,21 +38,32 @@ theorem card_embedding_eq_of_unique {α β : Type _} [Unique α] [Fintype β] [F
   card_congr Equiv.uniqueEmbeddingEquivResult
 #align fintype.card_embedding_eq_of_unique Fintype.card_embedding_eq_of_unique
 
+
 -- Establishes the cardinality of the type of all injections between two finite types.
 @[simp]
-theorem card_embedding_eq {α β} [Fintype α] [Fintype β] [Fintype (α ↪ β)] :
+theorem card_embedding_eq {α β : Type _} [a : Fintype α] [b : Fintype β] [emb: Fintype (α ↪ β)] :
     ‖α ↪ β‖ = ‖β‖.descFactorial ‖α‖ := by
   classical
-    induction' ‹Fintype α› using Fintype.induction_empty_option with α₁ α₂ h₂ e ih α h ih
-    · letI := Fintype.ofEquiv _ e.symm
-      rw [← card_congr (Equiv.embeddingCongr e (Equiv.refl β)), ih, card_congr e]
+    -- letI := 1
+    refine' Fintype.induction_empty_option _ _ _ α
+    -- intro α₁ α₂ h₂ e ih
+
+    -- induction' ‹Fintype α› using Fintype.induction_empty_option with α₁ α₂ h₂ e ih α h ih
+    · intro α₁ α₂ h₂ e ih
+      letI := Fintype.ofEquiv α₂ e.symm
+      let v1 := (Equiv.refl β)
+      let v2 := (Equiv.embeddingCongr e v1)
+      let v3 := card_congr v2
+      erw [←v3, ih, card_congr e]
     · rw [card_pempty, Nat.descFactorial_zero, card_eq_one_iff]
       exact ⟨embedding.of_is_empty, fun x => FunLike.ext _ _ isEmptyElim⟩
-    · rw [card_option, Nat.descFactorial_succ, card_congr (embedding.option_embedding_equiv α β),
+    · intro α₁ h₂ ih
+      rw [card_option, Nat.descFactorial_succ, card_congr (embedding.option_embedding_equiv α β),
         card_sigma, ← ih]
       simp only [Fintype.card_compl_set, Fintype.card_range, Finset.sum_const, Finset.card_univ,
         smul_eq_mul, mul_comm]
-#align fintype.card_embedding_eq Fintype.card_embedding_eq
+
+-- #align fintype.card_embedding_eq Fintype.card_embedding_eq
 
 /- The cardinality of embeddings from an infinite type to a finite type is zero.
 This is a re-statement of the pigeonhole principle. -/
@@ -60,4 +74,3 @@ theorem card_embedding_eq_of_infinite {α β : Type _} [Infinite α] [Fintype β
 #align fintype.card_embedding_eq_of_infinite Fintype.card_embedding_eq_of_infinite
 
 end Fintype
-
