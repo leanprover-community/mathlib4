c85e7b861c65f5c131f68f3747633a8f49a44228
adamtopaz <github@adamtopaz.com>
2023-02-11 06:29:10 -0700

    automated fixes
    Mathbin -> Mathlib
    
    fix certain import statements
    
    move "by" to end of line
    
    add import to Mathlib.lean

--- Mathlib/CategoryTheory/Functor/Const.lean
+++ Mathlib/CategoryTheory/Functor/Const.lean
@@ -8,7 +8,7 @@ Authors: Scott Morrison, Bhavik Mehta
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
-import Mathbin.CategoryTheory.Opposites
+import Mathlib.CategoryTheory.Opposites
 
 /-!
 # The constant functor
