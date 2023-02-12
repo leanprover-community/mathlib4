7bcb7a5390ad805b82d71e1fc852ba6db80cd3e8
Moritz Firsching <firsching@google.com>
2023-02-12 13:17:03 +0100

    first pass

--- Mathlib/CategoryTheory/Functor/InvIsos.lean
+++ Mathlib/CategoryTheory/Functor/InvIsos.lean
@@ -14,7 +14,7 @@ import Mathlib.CategoryTheory.EqToHom
 # Natural isomorphisms with composition with inverses
 
 Definition of useful natural isomorphisms involving inverses of functors.
-These definitions cannot go in `category_theory/equivalence` because they require `eq_to_hom`.
+These definitions cannot go in `CategoryTheory/Equivalence` because they require `EqToHom`.
 -/
 
 
@@ -30,30 +30,29 @@ variable {A : Type u₁} [Category.{v₁} A] {B : Type u₂} [Category.{v₂} B]
 variable {F : A ⥤ C} {G : A ⥤ B} {H : B ⥤ C}
 
 /-- Construct an isomorphism `F ⋙ H.inv ≅ G` from an isomorphism `F ≅ G ⋙ H`. -/
-@[simps]
+@[simps!]
 def compInvIso [h : IsEquivalence H] (i : F ≅ G ⋙ H) : F ⋙ H.inv ≅ G :=
   isoWhiskerRight i H.inv ≪≫
     associator G H H.inv ≪≫ isoWhiskerLeft G h.unitIso.symm ≪≫ eqToIso (Functor.comp_id G)
 #align category_theory.comp_inv_iso CategoryTheory.compInvIso
 
 /-- Construct an isomorphism `G ≅ F ⋙ H.inv` from an isomorphism `G ⋙ H ≅ F`. -/
-@[simps]
-def isoCompInv [h : IsEquivalence H] (i : G ⋙ H ≅ F) : G ≅ F ⋙ H.inv :=
+@[simps!]
+def isoCompInv [_h : IsEquivalence H] (i : G ⋙ H ≅ F) : G ≅ F ⋙ H.inv :=
   (compInvIso i.symm).symm
 #align category_theory.iso_comp_inv CategoryTheory.isoCompInv
 
 /-- Construct an isomorphism `G.inv ⋙ F ≅ H` from an isomorphism `F ≅ G ⋙ H`. -/
-@[simps]
+@[simps!]
 def invCompIso [h : IsEquivalence G] (i : F ≅ G ⋙ H) : G.inv ⋙ F ≅ H :=
   isoWhiskerLeft G.inv i ≪≫
     (associator G.inv G H).symm ≪≫ isoWhiskerRight h.counitIso H ≪≫ eqToIso (Functor.id_comp H)
 #align category_theory.inv_comp_iso CategoryTheory.invCompIso
 
 /-- Construct an isomorphism `H ≅ G.inv ⋙ F` from an isomorphism `G ⋙ H ≅ F`. -/
-@[simps]
-def isoInvComp [h : IsEquivalence G] (i : G ⋙ H ≅ F) : H ≅ G.inv ⋙ F :=
+@[simps!]
+def isoInvComp [_h : IsEquivalence G] (i : G ⋙ H ≅ F) : H ≅ G.inv ⋙ F :=
   (invCompIso i.symm).symm
 #align category_theory.iso_inv_comp CategoryTheory.isoInvComp
 
 end CategoryTheory
-
