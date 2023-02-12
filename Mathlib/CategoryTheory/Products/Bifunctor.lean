4bf7dc60c9bcf8d15b57f3ecfbeff722b33b148d
Moritz Firsching <firsching@google.com>
2023-02-12 20:22:33 +0100

    first pass

--- Mathlib/CategoryTheory/Products/Bifunctor.lean
+++ Mathlib/CategoryTheory/Products/Bifunctor.lean
@@ -34,30 +34,29 @@ theorem map_id (F : C × D ⥤ E) (X : C) (Y : D) :
 @[simp]
 theorem map_id_comp (F : C × D ⥤ E) (W : C) {X Y Z : D} (f : X ⟶ Y) (g : Y ⟶ Z) :
     F.map ((𝟙 W, f ≫ g) : (W, X) ⟶ (W, Z)) =
-      F.map ((𝟙 W, f) : (W, X) ⟶ (W, Y)) ≫ F.map ((𝟙 W, g) : (W, Y) ⟶ (W, Z)) :=
-  by rw [← functor.map_comp, prod_comp, category.comp_id]
+      F.map ((𝟙 W, f) : (W, X) ⟶ (W, Y)) ≫ F.map ((𝟙 W, g) : (W, Y) ⟶ (W, Z)) := by
+  rw [← Functor.map_comp, prod_comp, Category.comp_id]
 #align category_theory.bifunctor.map_id_comp CategoryTheory.Bifunctor.map_id_comp
 
 @[simp]
 theorem map_comp_id (F : C × D ⥤ E) (X Y Z : C) (W : D) (f : X ⟶ Y) (g : Y ⟶ Z) :
     F.map ((f ≫ g, 𝟙 W) : (X, W) ⟶ (Z, W)) =
-      F.map ((f, 𝟙 W) : (X, W) ⟶ (Y, W)) ≫ F.map ((g, 𝟙 W) : (Y, W) ⟶ (Z, W)) :=
-  by rw [← functor.map_comp, prod_comp, category.comp_id]
+      F.map ((f, 𝟙 W) : (X, W) ⟶ (Y, W)) ≫ F.map ((g, 𝟙 W) : (Y, W) ⟶ (Z, W)) := by
+  rw [← Functor.map_comp, prod_comp, Category.comp_id]
 #align category_theory.bifunctor.map_comp_id CategoryTheory.Bifunctor.map_comp_id
 
 @[simp]
 theorem diagonal (F : C × D ⥤ E) (X X' : C) (f : X ⟶ X') (Y Y' : D) (g : Y ⟶ Y') :
     F.map ((𝟙 X, g) : (X, Y) ⟶ (X, Y')) ≫ F.map ((f, 𝟙 Y') : (X, Y') ⟶ (X', Y')) =
-      F.map ((f, g) : (X, Y) ⟶ (X', Y')) :=
-  by rw [← functor.map_comp, prod_comp, category.id_comp, category.comp_id]
+      F.map ((f, g) : (X, Y) ⟶ (X', Y')) := by
+  rw [← Functor.map_comp, prod_comp, Category.id_comp, Category.comp_id]
 #align category_theory.bifunctor.diagonal CategoryTheory.Bifunctor.diagonal
 
 @[simp]
 theorem diagonal' (F : C × D ⥤ E) (X X' : C) (f : X ⟶ X') (Y Y' : D) (g : Y ⟶ Y') :
     F.map ((f, 𝟙 Y) : (X, Y) ⟶ (X', Y)) ≫ F.map ((𝟙 X', g) : (X', Y) ⟶ (X', Y')) =
-      F.map ((f, g) : (X, Y) ⟶ (X', Y')) :=
-  by rw [← functor.map_comp, prod_comp, category.id_comp, category.comp_id]
+      F.map ((f, g) : (X, Y) ⟶ (X', Y')) := by
+  rw [← Functor.map_comp, prod_comp, Category.id_comp, Category.comp_id]
 #align category_theory.bifunctor.diagonal' CategoryTheory.Bifunctor.diagonal'
 
 end CategoryTheory.Bifunctor
-
