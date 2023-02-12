caedf9f1571e446984e7b9a96c4b3e9f6e8e1fc3
casavaca <96765450+casavaca@users.noreply.github.com>
2023-02-11 17:57:29 -0800

    Changes and Reflow. Σ doesn't work. Used _root_.Sigma instead

--- Mathlib/Data/QPF/Multivariate/Constructions/Sigma.lean
+++ Mathlib/Data/QPF/Multivariate/Constructions/Sigma.lean
@@ -9,7 +9,7 @@ Authors: Simon Hudon
 ! if you have ported upstream changes.
 -/
 import Mathlib.Data.PFunctor.Multivariate.Basic
-import Mathlib.Data.Qpf.Multivariate.Basic
+import Mathlib.Data.QPF.Multivariate.Basic
 
 /-!
 # Dependent product and sum of QPFs are QPFs
@@ -18,7 +18,7 @@ import Mathlib.Data.Qpf.Multivariate.Basic
 
 universe u
 
-namespace Mvqpf
+namespace MvQPF
 
 open MvFunctor
 
@@ -29,92 +29,92 @@ variable (F : A → TypeVec.{u} n → Type u)
 /-- Dependent sum of of an `n`-ary functor. The sum can range over
 data types like `ℕ` or over `Type.{u-1}` -/
 def Sigma (v : TypeVec.{u} n) : Type u :=
-  Σα : A, F α v
-#align mvqpf.sigma Mvqpf.Sigma
+  -- Porting note: should be Σα : A, F α v. but failed because somehow
+  -- Σ bind to MvQPF.Sigma, not _root_.Sigma
+  _root_.Sigma fun α : A => F α v
+#align mvqpf.sigma MvQPF.Sigma
 
 /-- Dependent product of of an `n`-ary functor. The sum can range over
 data types like `ℕ` or over `Type.{u-1}` -/
 def Pi (v : TypeVec.{u} n) : Type u :=
   ∀ α : A, F α v
-#align mvqpf.pi Mvqpf.Pi
+#align mvqpf.pi MvQPF.Pi
 
 instance Sigma.inhabited {α} [Inhabited A] [Inhabited (F default α)] : Inhabited (Sigma F α) :=
   ⟨⟨default, default⟩⟩
-#align mvqpf.sigma.inhabited Mvqpf.Sigma.inhabited
+#align mvqpf.sigma.inhabited MvQPF.Sigma.inhabited
 
 instance Pi.inhabited {α} [∀ a, Inhabited (F a α)] : Inhabited (Pi F α) :=
-  ⟨fun a => default⟩
-#align mvqpf.pi.inhabited Mvqpf.Pi.inhabited
+  ⟨fun _a => default⟩
+#align mvqpf.pi.inhabited MvQPF.Pi.inhabited
 
 variable [∀ α, MvFunctor <| F α]
 
 namespace Sigma
 
-instance : MvFunctor (Sigma F) where map := fun α β f ⟨a, x⟩ => ⟨a, f <$$> x⟩
+instance : MvFunctor (Sigma F) where map := fun f ⟨a, x⟩ => ⟨a, f <$$> x⟩
 
-variable [∀ α, Mvqpf <| F α]
+variable [∀ α, MvQPF <| F α]
 
 /-- polynomial functor representation of a dependent sum -/
-protected def p : Mvpfunctor n :=
-  ⟨Σa, (p (F a)).A, fun x => (p (F x.1)).B x.2⟩
-#align mvqpf.sigma.P Mvqpf.Sigma.p
+protected def p : MvPFunctor n :=
+  -- Porting note, Σ bind to Sigma, not _root_.Sigma
+  -- old code: ⟨Σa, (p (F a)).A, fun x => (p (F x.1)).B x.2⟩
+  ⟨_root_.Sigma fun a => (p (F a)).A, fun x => (p (F x.1)).B x.2⟩
+set_option linter.uppercaseLean3 false in
+#align mvqpf.sigma.P MvQPF.Sigma.p
 
 /-- abstraction function for dependent sums -/
 protected def abs ⦃α⦄ : (Sigma.p F).Obj α → Sigma F α
-  | ⟨a, f⟩ => ⟨a.1, Mvqpf.abs ⟨a.2, f⟩⟩
-#align mvqpf.sigma.abs Mvqpf.Sigma.abs
+  | ⟨a, f⟩ => ⟨a.1, MvQPF.abs ⟨a.2, f⟩⟩
+#align mvqpf.sigma.abs MvQPF.Sigma.abs
 
 /-- representation function for dependent sums -/
 protected def repr ⦃α⦄ : Sigma F α → (Sigma.p F).Obj α
   | ⟨a, f⟩ =>
-    let x := Mvqpf.repr f
+    let x := MvQPF.repr f
     ⟨⟨a, x.1⟩, x.2⟩
-#align mvqpf.sigma.repr Mvqpf.Sigma.repr
+#align mvqpf.sigma.repr MvQPF.Sigma.repr
 
-instance : Mvqpf (Sigma F) where
+instance : MvQPF (Sigma F) where
   p := Sigma.p F
-  abs := Sigma.abs F
-  repr := Sigma.repr F
-  abs_repr := by rintro α ⟨x, f⟩ <;> simp [sigma.repr, sigma.abs, abs_repr]
-  abs_map := by
-    rintro α β f ⟨x, g⟩ <;> simp [sigma.abs, Mvpfunctor.map_eq] <;>
-      simp [(· <$$> ·), mvfunctor._match_1, ← abs_map, ← Mvpfunctor.map_eq]
+  abs {α} := @Sigma.abs _ _ F _ _ α
+  repr {α} := @Sigma.repr _ _ F _ _ α
+  abs_repr := by rintro α ⟨x, f⟩ ; simp [Sigma.repr, Sigma.abs, abs_repr]
+  abs_map := by rintro α β f ⟨x, g⟩ ; simp [Sigma.abs, MvPFunctor.map_eq] ;
+                simp [(· <$$> ·), ← abs_map, ← MvPFunctor.map_eq]
 
 end Sigma
 
 namespace Pi
 
-instance : MvFunctor (Pi F) where map α β f x a := f <$$> x a
+instance : MvFunctor (Pi F) where map f x a := f <$$> x a
 
-variable [∀ α, Mvqpf <| F α]
+variable [∀ α, MvQPF <| F α]
 
 /-- polynomial functor representation of a dependent product -/
-protected def p : Mvpfunctor n :=
-  ⟨∀ a, (p (F a)).A, fun x i => Σa : A, (p (F a)).B (x a) i⟩
-#align mvqpf.pi.P Mvqpf.Pi.p
+protected def p : MvPFunctor n :=
+  ⟨∀ a, (p (F a)).A, fun x i => _root_.Sigma fun a : A => (p (F a)).B (x a) i⟩
+set_option linter.uppercaseLean3 false in
+#align mvqpf.pi.P MvQPF.Pi.p
 
 /-- abstraction function for dependent products -/
 protected def abs ⦃α⦄ : (Pi.p F).Obj α → Pi F α
-  | ⟨a, f⟩ => fun x => Mvqpf.abs ⟨a x, fun i y => f i ⟨_, y⟩⟩
-#align mvqpf.pi.abs Mvqpf.Pi.abs
+  | ⟨a, f⟩ => fun x => MvQPF.abs ⟨a x, fun i y => f i ⟨_, y⟩⟩
+#align mvqpf.pi.abs MvQPF.Pi.abs
 
 /-- representation function for dependent products -/
 protected def repr ⦃α⦄ : Pi F α → (Pi.p F).Obj α
-  | f => ⟨fun a => (Mvqpf.repr (f a)).1, fun i a => (Mvqpf.repr (f _)).2 _ a.2⟩
-#align mvqpf.pi.repr Mvqpf.Pi.repr
+  | f => ⟨fun a => (MvQPF.repr (f a)).1, fun _i a => (MvQPF.repr (f _)).2 _ a.2⟩
+#align mvqpf.pi.repr MvQPF.Pi.repr
 
-instance : Mvqpf (Pi F) where
+instance : MvQPF (Pi F) where
   p := Pi.p F
-  abs := Pi.abs F
-  repr := Pi.repr F
-  abs_repr := by rintro α f <;> ext <;> simp [pi.repr, pi.abs, abs_repr]
-  abs_map := by
-    rintro α β f ⟨x, g⟩ <;> simp only [pi.abs, Mvpfunctor.map_eq] <;> ext <;>
-          simp only [(· <$$> ·)] <;>
-        simp only [← abs_map, Mvpfunctor.map_eq] <;>
-      rfl
+  abs {α} := @Pi.abs _ _ F _ _ α
+  repr {α} := @Pi.repr _ _ F _ _ α
+  abs_repr := by rintro α f; simp only [Pi.abs, Pi.repr, Sigma.eta, abs_repr]
+  abs_map := by rintro α β f ⟨x, g⟩; simp only [Pi.abs, (· <$$> ·), ← abs_map]; rfl
 
 end Pi
 
-end Mvqpf
-
+end MvQPF
