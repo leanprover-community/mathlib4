5ef258d654342386b86703fa656c1b4a75b2a2e9
Eric Wieser <wieser.eric@gmail.com>
2023-02-13 01:25:31 +0000

    golf

--- Mathlib/Topology/Algebra/MulAction.lean
+++ Mathlib/Topology/Algebra/MulAction.lean
@@ -178,8 +178,9 @@ variable {ι : Sort _} {M X : Type _} [TopologicalSpace M] [SMul M X]
 @[to_additive]
 theorem continuousSMul_infₛ {ts : Set (TopologicalSpace X)}
     (h : ∀ t ∈ ts, @ContinuousSMul M X _ _ t) : @ContinuousSMul M X _ _ (infₛ ts) :=
-  -- porting note: {} doesn't work because `infₛ ts` isn't found by TC search
-  @ContinuousSMul.mk M X _ _ (infₛ ts) <| by
+  -- porting note: {} doesn't work because `infₛ ts` isn't found by TC search. `(_)` finds it by
+  -- unification instead.
+  @ContinuousSMul.mk M X _ _ (_) <| by
       rw [← @infₛ_singleton _ _ ‹TopologicalSpace M›]
       exact
         continuous_infₛ_rng.2 fun t ht =>
