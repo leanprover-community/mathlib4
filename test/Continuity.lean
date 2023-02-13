1dcc15ac8bf9173a93ba0d89083eaa30c406bb57
Moritz Doll <moritz.doll@googlemail.com>
2023-02-13 13:38:56 +1100

    Update test/Continuity.lean

--- test/Continuity.lean
+++ test/Continuity.lean
@@ -13,6 +13,13 @@ example {f : X → Y} {g : Y → X} (hf : Continuous f) (hg : Continuous g) :
 example {f : X → Y} {g : Y → X} (hf : Continuous f) (hg : Continuous g) :
   Continuous (f ∘ g) := by continuity
 
+example (y : Y) : Continuous (fun (x : X) ↦ y) := by continuity
+
+example {f : X → X} (y : Y) : Continuous (f ∘ (fun x ↦ y)) := by continuity
+
+example {g : Y → Y} (y : Y) : Continuous ((fun x ↦ y) ∘ g) := by continuity
+
+example {f : X → Y} (x : X) : Continuous (fun x' ↦ f x)) := by continuity
 -- Todo: more interesting examples when more algebra is ported
 
 -- Porting note: port the tests from mathlib3 once we have the necessary theory files
