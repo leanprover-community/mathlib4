a4371c915af0916ab8a8e8ef21f426f423e3cd8c
Moritz Doll <moritz.doll@googlemail.com>
2023-02-13 22:40:47 +0000

    feat: port Topology.Algebra.Order.LeftRightLim (#2131)
    Co-authored-by: thorimur <68410468+thorimur@users.noreply.github.com>
    Co-authored-by: Johan Commelin <johan@commelin.net>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1053,6 +1053,7 @@ import Mathlib.Topology.Algebra.Order.ExtrClosure
 import Mathlib.Topology.Algebra.Order.Filter
 import Mathlib.Topology.Algebra.Order.IntermediateValue
 import Mathlib.Topology.Algebra.Order.LeftRight
+import Mathlib.Topology.Algebra.Order.LeftRightLim
 import Mathlib.Topology.Algebra.Order.LiminfLimsup
 import Mathlib.Topology.Algebra.Order.MonotoneContinuity
 import Mathlib.Topology.Algebra.Order.MonotoneConvergence
