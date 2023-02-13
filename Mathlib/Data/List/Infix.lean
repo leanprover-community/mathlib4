7b40395f6b6bf96ef4e6ff04e28a17087d130854
Eric Wieser <wieser.eric@gmail.com>
2023-02-13 18:07:56 +0000

    Update Infix.lean

--- Mathlib/Data/List/Infix.lean
+++ Mathlib/Data/List/Infix.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro
 
 ! This file was ported from Lean 3 source module data.list.infix
-! leanprover-community/mathlib commit 6d0adfa76594f304b4650d098273d4366edeb61b
+! leanprover-community/mathlib commit 26f081a2fb920140ed5bc5cc5344e84bcc7cb2b2
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
