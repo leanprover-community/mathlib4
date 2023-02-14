2eacb8832ea69adfdf816153637c136ce3750a7c
Yakov Pechersky <pechersky@users.noreply.github.com>
2023-02-14 16:14:31 -0500

    Actual hash from https://github.com/leanprover-community/mathlib/commit/369525b73f229ccd76a6ec0e0e0bf2be57599768

--- Mathlib/Algebra/Group/Units.lean
+++ Mathlib/Algebra/Group/Units.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kenny Lau, Mario Carneiro, Johannes Hölzl, Chris Hughes, Jens Wagemaker, Jon Eugster
 
 ! This file was ported from Lean 3 source module algebra.group.units
-! leanprover-community/mathlib commit ecfe23cba85dcb543dc3500da22f5fdb0b36496e
+! leanprover-community/mathlib commit 369525b73f229ccd76a6ec0e0e0bf2be57599768
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
