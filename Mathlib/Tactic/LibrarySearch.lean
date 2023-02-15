56347f4c32712956bd3d55833f66e5f80c6c210b
David Renshaw <dwrenshaw@gmail.com>
2023-02-15 14:19:47 -0500

    fix: avoid sorryAx in library_search

--- Mathlib/Tactic/LibrarySearch.lean
+++ Mathlib/Tactic/LibrarySearch.lean
@@ -31,7 +31,7 @@ initialize registerTraceClass `Tactic.librarySearch
 
 -- from Lean.Server.Completion
 private def isBlackListed (declName : Name) : MetaM Bool := do
-  if declName == ``sorryAx then return false
+  if declName == ``sorryAx then return true
   if declName matches .str _ "inj" then return false
   if declName matches .str _ "noConfusionType" then return false
   let env ← getEnv
