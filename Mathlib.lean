87a53f83f500dca80b773e714708041d8fc6bdc7
qawbecrdtey <qawbecrdtey@kaist.ac.kr>
2023-02-15 14:53:54 +0000

    feat: port Computability.Language (#1453)
    Co-authored-by: qawbecrdtey <40463813+qawbecrdtey@users.noreply.github.com>
    Co-authored-by: Komyyy <pol_tta@outlook.jp>

--- Mathlib.lean
+++ Mathlib.lean
@@ -277,6 +277,7 @@ import Mathlib.Combinatorics.SetFamily.Shadow
 import Mathlib.Combinatorics.SimpleGraph.Regularity.Equitabilise
 import Mathlib.Combinatorics.Young.SemistandardTableau
 import Mathlib.Combinatorics.Young.YoungDiagram
+import Mathlib.Computability.Language
 import Mathlib.Computability.TuringMachine
 import Mathlib.Control.Applicative
 import Mathlib.Control.Basic
