leanprover-community/mathlib4

commit a445a5e1aadf4aa54f7f3607ed46b4be3ea49f99

feat: refactor of solve_by_elim (#856)
This is a thorough refactor of `solve_by_elim`.

* Bug fixes and additional tests.
* Support for removing local hypotheses using `solve_by_elim [-h]`.
* Use `symm` on hypotheses and `exfalso` on the goal, as needed.
* To support that, `MetaM` level tooling for the `symm` tactic. (`rfl` and `trans` deserve the same treatment at some point.)
* Additional hooks for flow control in `solve_by_elim` (suspending goals to return to the user, rejecting branches, running arbitrary procedures on the goals).
* Using those hooks, reimplement `apply_assumption` and `apply_rules` as thin wrappers around `solve_by_elim`, allowing access to new features (removing hypotheses, symm and exfalso) for free.
* Using those hooks, fix `library_search using` so 
```example (P Q : List ℕ) (h : ℕ) : List ℕ := by library_search using P, Q -- exact P ∩ Q```

Co-authored-by: Scott Morrison <scott.morrison@gmail.com>

Mathlib/Tactic/LibrarySearch.lean
@@ -49,13 +49,19 @@ initialize librarySearchLemmas : DeclCache (DiscrTree Name true) ←
       let keys ← withReducible <| DiscrTree.mkPath type
       pure $ lemmas.insertCore keys name
 
-/-- Shortcut for calling `solveByElimImpl`. -/
-def solveByElim (g : MVarId) (depth) := SolveByElim.solveByElimImpl false [] depth g
+/-- Shortcut for calling `solveByElim`. -/
+def solveByElim (goals : List MVarId) (required : List Expr) (depth) := do
+  -- There is only a marginal decrease in performance for using the `symm` and `exfalso`
+  -- options for `solveByElim`.
+  -- (measured via `lake build && time lake env lean test/librarySearch.lean`).
+  let cfg : SolveByElim.Config := { maxDepth := depth, exfalso := true, symm := true }
+  let cfg := if !required.isEmpty then cfg.requireUsingAll required else cfg
+  _ ← SolveByElim.solveByElim.processSyntax cfg false false [] [] goals
 
 /--
 Try to solve the goal either by:
 * calling `solveByElim`
-* or applying a library lemma then calling  `solveByElim` on the resulting goals.
+* or applying a library lemma then calling `solveByElim` on the resulting goals.
 
 If it successfully closes the goal, returns `none`.
 Otherwise, it returns `some a`, where `a : Array (MetavarContext × List MVarId)`,
@@ -79,11 +85,7 @@ def librarySearch (goal : MVarId) (lemmas : DiscrTree Name s) (required : List E
   let state0 ← get
 
   try
-    solveByElim goal solveByElimDepth
-    if (← checkRequired) then
-      return none
-    else
-      set state0
+    solveByElim [goal] required solveByElimDepth
   catch _ =>
     set state0
 
@@ -94,19 +96,13 @@ def librarySearch (goal : MVarId) (lemmas : DiscrTree Name s) (required : List E
         let newGoals ← goal.apply (← mkConstWithFreshMVarLevels lem)
         (try
           for newGoal in newGoals do
-            newGoal.withContext do
-              trace[Tactic.librarySearch] "proving {← addMessageContextFull (mkMVar newGoal)}"
-              solveByElim newGoal solveByElimDepth
-          if (← checkRequired) then
-            pure $ some $ Sum.inr ()
-          else
-            set state0
-            pure none
+            trace[Tactic.librarySearch] "proving {← addMessageContextFull (mkMVar newGoal)}"
+          solveByElim newGoals required solveByElimDepth
+          pure $ some $ Sum.inr ()
         catch _ =>
           let res := some $ Sum.inl (← getMCtx, newGoals)
-          let check ← checkRequired
           set state0
-          return if check then res else none)
+          return res)
     catch _ =>
       set state0
       pure none
@@ -116,10 +112,6 @@ def librarySearch (goal : MVarId) (lemmas : DiscrTree Name s) (required : List E
     | some (Sum.inl suggestion) => suggestions := suggestions.push suggestion
 
   pure $ some suggestions
-    where
-  /-- Verify that the instantiated goal contains each `Expr` in `required` as a sub-expression.
-  (Make sure to not reset the state before calling.) -/
-  checkRequired : MetaM Bool := return required.all (·.occurs (← instantiateMVars (.mvar goal)))
 
 def lines (ls : List MessageData) :=
   MessageData.joinSep ls (MessageData.ofFormat Format.line)

Mathlib.lean
@@ -518,7 +518,6 @@ import Mathlib.SetTheory.Cardinal.SchroederBernstein
 import Mathlib.Tactic.Abel
 import Mathlib.Tactic.Alias
 import Mathlib.Tactic.ApplyFun
-import Mathlib.Tactic.ApplyRules
 import Mathlib.Tactic.ApplyWith
 import Mathlib.Tactic.Basic
 import Mathlib.Tactic.ByContra

Mathlib/Algebra/Order/Ring/Canonical.lean
@@ -90,21 +90,21 @@ namespace CanonicallyOrderedCommSemiring
 variable [CanonicallyOrderedCommSemiring α] {a b : α}
 
 -- see Note [lower instance priority]
-instance (priority := 100) to_noZeroDivisors : NoZeroDivisors α :=
+instance (priority := 100) toNoZeroDivisors : NoZeroDivisors α :=
   ⟨CanonicallyOrderedCommSemiring.eq_zero_or_eq_zero_of_mul_eq_zero _ _⟩
 #align
   canonically_ordered_comm_semiring.to_no_zero_divisors
-  CanonicallyOrderedCommSemiring.to_noZeroDivisors
+  CanonicallyOrderedCommSemiring.toNoZeroDivisors
 
 -- see Note [lower instance priority]
-instance (priority := 100) to_covariant_mul_le : CovariantClass α α (· * ·) (· ≤ ·) := by
+instance (priority := 100) toCovariantClassMulLE : CovariantClass α α (· * ·) (· ≤ ·) := by
   refine' ⟨fun a b c h => _⟩
   rcases exists_add_of_le h with ⟨c, rfl⟩
   rw [mul_add]
   apply self_le_add_right
 #align
   canonically_ordered_comm_semiring.to_covariant_mul_le
-  CanonicallyOrderedCommSemiring.to_covariant_mul_le
+  CanonicallyOrderedCommSemiring.toCovariantClassMulLE
 
 -- see Note [lower instance priority]
 instance (priority := 100) toOrderedCommSemiring : OrderedCommSemiring α :=

Mathlib/Data/Nat/Choose/Basic.lean
@@ -145,7 +145,7 @@ theorem choose_mul_factorial_mul_factorial : ∀ {n k}, k ≤ n → choose n k *
 
 theorem choose_mul {n k s : ℕ} (hkn : k ≤ n) (hsk : s ≤ k) :
     n.choose k * k.choose s = n.choose s * (n - s).choose (k - s) :=
-  have h : (n - k)! * (k - s)! * s ! ≠ 0:= by apply_rules [factorial_ne_zero, mul_ne_zero]
+  have h : (n - k)! * (k - s)! * s ! ≠ 0 := by apply_rules [factorial_ne_zero, mul_ne_zero]
   mul_right_cancel₀ h <|
   calc
     n.choose k * k.choose s * ((n - k)! * (k - s)! * s !) =

Mathlib/Data/Set/Intervals/Basic.lean
@@ -11,7 +11,6 @@ Ported by: Winston Yin, Arien Malec
 -/
 import Mathlib.Order.MinMax
 import Mathlib.Data.Set.Prod
-import Mathlib.Tactic.ApplyRules
 
 /-!
 # Intervals

Mathlib/Data/Sum/Basic.lean
@@ -46,6 +46,7 @@ variable {α : Type u} {α' : Type w} {β : Type v} {β' : Type x} {γ δ : Type
 namespace Sum
 
 deriving instance DecidableEq for Sum
+deriving instance BEq for Sum
 
 @[simp]
 theorem «forall» {p : Sum α β → Prop} : (∀ x, p x) ↔ (∀ a, p (inl a)) ∧ ∀ b, p (inr b) :=

Mathlib/Init/Logic.lean
@@ -48,6 +48,8 @@ theorem not_of_eq_false {p : Prop} (h : p = False) : ¬p := fun hp ↦ h ▸ hp
 
 theorem cast_proof_irrel (h₁ h₂ : α = β) (a : α) : cast h₁ a = cast h₂ a := rfl
 
+attribute [symm] Eq.symm
+
 /- Ne -/
 
 theorem Ne.def {α : Sort u} (a b : α) : (a ≠ b) = ¬ (a = b) := rfl

Mathlib/Lean/Expr/Basic.lean
@@ -159,7 +159,7 @@ def natLit! : Expr → Nat
   | lit (Literal.natVal v) => v
   | _                      => panic! "nat literal expected"
 
-/-- If a `Expr` has form `.fvar n`, then returns `some n`, otherwise `none`. -/
+/-- If an `Expr` has form `.fvar n`, then returns `some n`, otherwise `none`. -/
 def fvarId? : Expr → Option FVarId
   | .fvar n => n
   | _ => none

commit 89005600cfe9837fadb280caccf24cade943bd30

fix: return early in library_search if initial solveByElim succeeds (#2302)
Fixes a bug introduced in #856.
If `solveByElim` succeeds, then we should return early. Otherwise, we look through all lemmas in the environment and find something like `namedPattern` or `Eq.mp` and unnecessarily wrap the result in that.

See https://github.com/leanprover-community/mathlib4/pull/2276 for an example bad output caused by this bug.

Another example is this test: https://github.com/leanprover-community/mathlib4/blob/4ed65899eca4909e9b8f23a113b52ff8cb3f5d41/test/librarySearch.lean#L15
On master, it emits `exact namedPattern p p rfl`, and after this PR it correctly emits `exact p`.

Mathlib/Tactic/LibrarySearch.lean
@@ -86,6 +86,7 @@ def librarySearch (goal : MVarId) (lemmas : DiscrTree Name s) (required : List E
 
   try
     solveByElim [goal] required solveByElimDepth
+    return none
   catch _ =>
     set state0
 