3a8b0135be238f52747407092a5dc1eda8dbd67e
Yury G. Kudryashov <urkud@urkud.name>
2023-02-15 01:06:36 +0000

    feat: port Topology.Algebra.GroupWithZero (#2288)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1058,6 +1058,7 @@ import Mathlib.Testing.SlimCheck.Testable
 import Mathlib.Topology.Alexandroff
 import Mathlib.Topology.Algebra.ConstMulAction
 import Mathlib.Topology.Algebra.Constructions
+import Mathlib.Topology.Algebra.GroupWithZero
 import Mathlib.Topology.Algebra.Monoid
 import Mathlib.Topology.Algebra.MulAction
 import Mathlib.Topology.Algebra.Order.Archimedean
