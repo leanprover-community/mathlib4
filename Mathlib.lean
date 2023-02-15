065e9956724008d1059577700fd676eaf25f35d5
Matthew Robert Ballard <matt@mrb.email>
2023-02-15 17:47:35 +0000

    feat: port CategoryTheory.Functor.Hom (#2295)

--- Mathlib.lean
+++ Mathlib.lean
@@ -238,6 +238,7 @@ import Mathlib.CategoryTheory.Functor.Currying
 import Mathlib.CategoryTheory.Functor.Default
 import Mathlib.CategoryTheory.Functor.FullyFaithful
 import Mathlib.CategoryTheory.Functor.Functorial
+import Mathlib.CategoryTheory.Functor.Hom
 import Mathlib.CategoryTheory.Functor.InvIsos
 import Mathlib.CategoryTheory.Groupoid
 import Mathlib.CategoryTheory.Iso
