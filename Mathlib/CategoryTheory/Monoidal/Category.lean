d6a89ce03062622736b026009ccaecbfea7aaec3
mpenciak <matej.penciak@gmail.com>
2023-02-16 10:37:24 -0500

    start of port

--- Mathlib/CategoryTheory/Monoidal/Category.lean
+++ Mathlib/CategoryTheory/Monoidal/Category.lean
@@ -8,7 +8,7 @@ Authors: Michael Jendrusch, Scott Morrison, Bhavik Mehta, Jakob von Raumer
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
-import Mathbin.CategoryTheory.Products.Basic
+import Mathlib.CategoryTheory.Products.Basic
 
 /-!
 # Monoidal categories
