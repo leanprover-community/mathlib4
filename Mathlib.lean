2c9195baabf3a360ade906a088ce44006b7b8517
Yury G. Kudryashov <urkud@urkud.name>
2023-02-17 16:48:34 -0600

    Sort

--- Mathlib.lean
+++ Mathlib.lean
@@ -922,10 +922,10 @@ import Mathlib.Order.Monotone.Union
 import Mathlib.Order.OmegaCompletePartialOrder
 import Mathlib.Order.OrdContinuous
 import Mathlib.Order.OrderIsoNat
+import Mathlib.Order.PFilter
 import Mathlib.Order.PartialSups
 import Mathlib.Order.Partition.Equipartition
 import Mathlib.Order.Partition.Finpartition
-import Mathlib.Order.PFilter
 import Mathlib.Order.PropInstances
 import Mathlib.Order.RelClasses
 import Mathlib.Order.RelIso.Basic
