5f3f3283e5922c2d5809df1e989f334970dd4e63
Yury G. Kudryashov <urkud@urkud.name>
2023-02-17 00:31:46 -0600

    Fix

--- Mathlib/Order/Extension/Well.lean
+++ Mathlib/Order/Extension/Well.lean
@@ -21,11 +21,21 @@ well-founded order.
 
 We can map our order into two well-orders:
 * the first map respects the order but isn't necessarily injective. Namely, this is the *rank*
-  function `rank : α → ordinal`.
+  function `WellFounded.rank : α → Ordinal`.
 * the second map is injective but doesn't necessarily respect the order. This is an arbitrary
-  well-order on `α`.
+  embedding into `Cardinal` given by `embeddingToCardinal`.
 
 Then their lexicographic product is a well-founded linear order which our original order injects in.
+
+## Porting notes
+
+The definition in `mathlib` 3 used an auxiliary well-founded order on `α` lifted from `Cardinal`
+instead of `Cardinal`. The new definition is definitionally equal to the `mathlib` 3 version but
+avoids non-standard instances.
+
+## Tags
+
+well founded relation, well order, extension
 -/
 
 
@@ -36,8 +46,6 @@ variable {α : Type u} {r : α → α → Prop}
 namespace WellFounded
 
 variable (hwf : WellFounded r)
-
-include hwf
 
 /-- An arbitrary well order on `α` that extends `r`.
 
@@ -50,29 +58,27 @@ get an well-order that extend our original order `r`. Another way to view this i
 arbitrary well-order to serve as a tiebreak between two elements of same rank.
 -/
 noncomputable def wellOrderExtension : LinearOrder α :=
-  let l : LinearOrder α := IsWellOrder.linearOrder WellOrderingRel
-  @LinearOrder.lift' α (Ordinal ×ₗ α) _ (fun a : α => (WellFounded.rank.{u} hwf a, a)) fun _ _ =>
-    congr_arg Prod.snd
+  @LinearOrder.lift' α (Ordinal ×ₗ Cardinal) _ (fun a : α => (hwf.rank a, embeddingToCardinal a))
+    fun _ _ h => embeddingToCardinal.injective <| congr_arg Prod.snd h
 #align well_founded.well_order_extension WellFounded.wellOrderExtension
 
 instance wellOrderExtension.isWellFounded_lt : IsWellFounded α hwf.wellOrderExtension.lt :=
-  ⟨InvImage.wf _ <| Prod.lex_wf Ordinal.wellFoundedLT.wf WellOrderingRel.isWellOrder.wf⟩
+  ⟨InvImage.wf (fun a : α => (hwf.rank a, embeddingToCardinal a)) <|
+    Ordinal.lt_wf.prod_lex Cardinal.lt_wf⟩
 #align well_founded.well_order_extension.is_well_founded_lt WellFounded.wellOrderExtension.isWellFounded_lt
 
 /-- Any well-founded relation can be extended to a well-ordering on that type. -/
 theorem exists_well_order_ge : ∃ s, r ≤ s ∧ IsWellOrder α s :=
-  ⟨hwf.wellOrderExtension.lt, fun a b h => Prod.Lex.left _ _ (hwf.rank_lt_of_rel h), by constructor⟩
+  ⟨hwf.wellOrderExtension.lt, fun _ _ h => Prod.Lex.left _ _ (hwf.rank_lt_of_rel h), ⟨⟩⟩
 #align well_founded.exists_well_order_ge WellFounded.exists_well_order_ge
 
 end WellFounded
 
 /-- A type alias for `α`, intended to extend a well-founded order on `α` to a well-order. -/
-def WellOrderExtension (α) : Type _ :=
-  α
+def WellOrderExtension (α : Type _) : Type _ := α
 #align well_order_extension WellOrderExtension
 
-instance [Inhabited α] : Inhabited (WellOrderExtension α) :=
-  ‹Inhabited (WellOrderExtension α)›
+instance [Inhabited α] : Inhabited (WellOrderExtension α) := ‹_›
 
 /-- "Identity" equivalence between a well-founded order and its well-order extension. -/
 def toWellOrderExtension : α ≃ WellOrderExtension α :=
@@ -88,7 +94,6 @@ instance WellOrderExtension.wellFoundedLT [LT α] [WellFoundedLT α] :
 #align well_order_extension.well_founded_lt WellOrderExtension.wellFoundedLT
 
 theorem toWellOrderExtension_strictMono [Preorder α] [WellFoundedLT α] :
-    StrictMono (toWellOrderExtension : α → WellOrderExtension α) := fun a b h =>
+    StrictMono (toWellOrderExtension : α → WellOrderExtension α) := fun _ _ h =>
   Prod.Lex.left _ _ <| WellFounded.rank_lt_of_rel _ h
 #align to_well_order_extension_strict_mono toWellOrderExtension_strictMono
-
