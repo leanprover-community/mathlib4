260f7b8786bf6a1c85d5ff7fd6857ba1d9925b65
thorimur <68410468+thorimur@users.noreply.github.com>
2023-02-17 02:12:22 -0500

    fix: use mathport tactic syntax, throw errors

--- Mathlib/Tactic/Monotonicity/Basic.lean
+++ Mathlib/Tactic/Monotonicity/Basic.lean
@@ -15,16 +15,19 @@ The tactic `mono` applies monotonicity rules (collected through the library by b
 The version of the tactic here is a cheap partial port of the `mono` tactic from Lean 3, which had
 many more options and features.  It is implemented as a wrapper on top of `solve_by_elim`.
 
-Syntax change: Lean 3 `mono` applied a single monotonicity rule, then applied local hypotheses and
-the `rfl` tactic as many times as it could.  This is hard to implement on top of `solve_by_elim`
-because the counting system used in the `maxDepth` field of its configuration would count these as
-separate steps, throwing off the count in the desired configuration `maxDepth := 1`.  So instead we
-just implement a version of `mono` in which monotonicity rules, local hypotheses and `rfl` are all
-applied repeatedly until nothing more is applicable.  The syntax for this in Lean 3 was `mono*`.
+Temporary syntax change: Lean 3 `mono` applied a single monotonicity rule, then applied local
+hypotheses and the `rfl` tactic as many times as it could.  This is hard to implement on top of
+`solve_by_elim` because the counting system used in the `maxDepth` field of its configuration would
+count these as separate steps, throwing off the count in the desired configuration
+`maxDepth := 1`.  So instead we just implement a version of `mono` in which monotonicity rules,
+local hypotheses and `rfl` are all applied repeatedly until nothing more is applicable.  The syntax
+for this in Lean 3 was `mono*`. Both `mono` and `mono*` implement this behavior for now.
 -/
 
-open Lean Elab Tactic
+open Lean Elab Tactic Parser Tactic
 open Mathlib Tactic SolveByElim
+
+namespace Mathlib.Tactic.Monoticity
 
 /--
 `mono` applies monotonicity rules and local hypotheses repetitively.  For example,
@@ -36,9 +39,15 @@ example (x y z k : ℤ)
   mono
 ```
 -/
-syntax (name := mono) "mono" : tactic
+syntax (name := mono) "mono" "*"? (ppSpace mono.side)?
+  (" with " (colGt term),+)? (" using " (colGt simpArg),+)? : tactic
 
-elab_rules : tactic | `(tactic| mono) => do
+elab_rules : tactic
+| `(tactic| mono $[*]? $[$h:mono.side]? $[ with%$w $a:term,*]? $[ using%$u $s,*]? ) => do
+  let msg (s : String) := s ++ " syntax is not yet supported in 'mono'"
+  if let some h := h then throwErrorAt h (msg "'left'/'right'/'both'")
+  if let some w := w then throwErrorAt w (msg "'with'")
+  if let some u := u then throwErrorAt u (msg "'using'")
   let cfg ← elabApplyRulesConfig <| mkNullNode #[]
   let cfg := { cfg.noBackTracking with
     transparency := .reducible
