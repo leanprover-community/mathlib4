c22f5070b04447318da611264f12a3de9c0d86f1
Alex Keizer <alex@keizer.dev>
2023-02-18 13:17:58 +0000

    feat: port Data.Pfunctor.Univariate.M (#1834)

--- Mathlib.lean
+++ Mathlib.lean
@@ -563,6 +563,7 @@ import Mathlib.Data.Option.NAry
 import Mathlib.Data.PEquiv
 import Mathlib.Data.PFun
 import Mathlib.Data.PFunctor.Univariate.Basic
+import Mathlib.Data.PFunctor.Univariate.M
 import Mathlib.Data.PNat.Basic
 import Mathlib.Data.PNat.Defs
 import Mathlib.Data.PNat.Factors
