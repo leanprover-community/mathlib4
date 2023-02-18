7ed98a03358db86982ddad82f1e0b8da3af9e5b6
int-y1 <jason_yuen2007@hotmail.com>
2023-02-18 02:47:24 -0800

    cleanup

--- Mathlib/Deprecated/Subring.lean
+++ Mathlib/Deprecated/Subring.lean
@@ -19,12 +19,12 @@ This file is deprecated, and is no longer imported by anything in mathlib other 
 deprecated files, and test files. You should not need to import it.
 
 This file defines predicates for unbundled subrings. Instead of using this file, please use
-`subring`, defined in `ring_theory.subring.basic`, for subrings of rings.
+`Subring`, defined in `RingTheory.Subring.Basic`, for subrings of rings.
 
 ## Main definitions
 
-`is_subring (S : set R) : Prop` : the predicate that `S` is the underlying set of a subring
-of the ring `R`. The bundled variant `subring R` should be used in preference to this.
+`IsSubring (S : Set R) : Prop` : the predicate that `S` is the underlying set of a subring
+of the ring `R`. The bundled variant `Subring R` should be used in preference to this.
 
 ## Tags
 
@@ -43,9 +43,8 @@ inverse. -/
 structure IsSubring (S : Set R) extends IsAddSubgroup S, IsSubmonoid S : Prop
 #align is_subring IsSubring
 
-/-- Construct a `subring` from a set satisfying `is_subring`. -/
-def IsSubring.subring {S : Set R} (hs : IsSubring S) : Subring R
-    where
+/-- Construct a `Subring` from a set satisfying `IsSubring`. -/
+def IsSubring.subring {S : Set R} (hs : IsSubring S) : Subring R where
   carrier := S
   one_mem' := hs.one_mem
   mul_mem' := hs.mul_mem
@@ -85,21 +84,21 @@ theorem IsSubring.inter {S₁ S₂ : Set R} (hS₁ : IsSubring S₁) (hS₂ : Is
 
 theorem IsSubring.interᵢ {ι : Sort _} {S : ι → Set R} (h : ∀ y : ι, IsSubring (S y)) :
     IsSubring (Set.interᵢ S) :=
-  { IsAddSubgroup.interᵢ fun i => (h i).toIsAddSubgroup,
-    IsSubmonoid.interᵢ fun i => (h i).toIsSubmonoid with }
+  { IsAddSubgroup.interᵢ fun i ↦ (h i).toIsAddSubgroup,
+    IsSubmonoid.interᵢ fun i ↦ (h i).toIsSubmonoid with }
 #align is_subring.Inter IsSubring.interᵢ
 
 theorem isSubring_unionᵢ_of_directed {ι : Type _} [Nonempty ι] {s : ι → Set R}
     (h : ∀ i, IsSubring (s i)) (directed : ∀ i j, ∃ k, s i ⊆ s k ∧ s j ⊆ s k) :
     IsSubring (⋃ i, s i) :=
-  { toIsAddSubgroup := isAddSubgroup_unionᵢ_of_directed (fun i => (h i).toIsAddSubgroup) directed
-    toIsSubmonoid := isSubmonoid_unionᵢ_of_directed (fun i => (h i).toIsSubmonoid) directed }
+  { toIsAddSubgroup := isAddSubgroup_unionᵢ_of_directed (fun i ↦ (h i).toIsAddSubgroup) directed
+    toIsSubmonoid := isSubmonoid_unionᵢ_of_directed (fun i ↦ (h i).toIsSubmonoid) directed }
 #align is_subring_Union_of_directed isSubring_unionᵢ_of_directed
 
 namespace Ring
 
 /-- The smallest subring containing a given subset of a ring, considered as a set. This function
-is deprecated; use `subring.closure`. -/
+is deprecated; use `Subring.closure`. -/
 def closure (s : Set R) :=
   AddGroup.closure (Monoid.Closure s)
 #align ring.closure Ring.closure
@@ -111,29 +110,26 @@ variable {s : Set R}
 theorem exists_list_of_mem_closure {a : R} (h : a ∈ closure s) :
     ∃ L : List (List R), (∀ l ∈ L, ∀ x ∈ l, x ∈ s ∨ x = (-1 : R)) ∧ (L.map List.prod).sum = a :=
   AddGroup.InClosure.recOn h
-    (fun {x} hx =>
+    (fun {x} hx ↦
       match x, Monoid.exists_list_of_mem_closure hx with
       | _, ⟨L, h1, rfl⟩ =>
-        ⟨[L], List.forall_mem_singleton.2 fun r hr => Or.inl (h1 r hr), zero_add _⟩)
+        ⟨[L], List.forall_mem_singleton.2 fun r hr ↦ Or.inl (h1 r hr), zero_add _⟩)
     ⟨[], List.forall_mem_nil _, rfl⟩
-    (fun {b} _ ih =>
+    (fun {b} _ ih ↦
       match b, ih with
       | _, ⟨L1, h1, rfl⟩ =>
-        ⟨L1.map (List.cons (-1)), fun L2 h2 =>
+        ⟨L1.map (List.cons (-1)), fun L2 h2 ↦
           match L2, List.mem_map'.1 h2 with
-          | _, ⟨L3, h3, rfl⟩ => List.forall_mem_cons.2 ⟨Or.inr rfl, h1 L3 h3⟩,
-          by
-          simp only [List.map_map, (· ∘ ·), List.prod_cons, neg_one_mul]
-          refine' List.recOn L1 neg_zero.symm fun hd tl ih => _
-          rw [List.map_cons, List.sum_cons, ih, List.map_cons, List.sum_cons, neg_add]⟩)
-    fun {r1 r2} _ _ ih1 ih2 =>
+          | _, ⟨L3, h3, rfl⟩ => List.forall_mem_cons.2 ⟨Or.inr rfl, h1 L3 h3⟩, by
+            simp only [List.map_map, (· ∘ ·), List.prod_cons, neg_one_mul]
+            refine' List.recOn L1 neg_zero.symm fun hd tl ih ↦ _
+            rw [List.map_cons, List.sum_cons, ih, List.map_cons, List.sum_cons, neg_add]⟩)
+    fun {r1 r2} _ _ ih1 ih2 ↦
     match r1, r2, ih1, ih2 with
     | _, _, ⟨L1, h1, rfl⟩, ⟨L2, h2, rfl⟩ =>
       ⟨L1 ++ L2, List.forall_mem_append.2 ⟨h1, h2⟩, by rw [List.map_append, List.sum_append]⟩
 #align ring.exists_list_of_mem_closure Ring.exists_list_of_mem_closure
 
-/- ./././Mathport/Syntax/Translate/Expr.lean:177:8: unsupported: ambiguous notation -/
-/- ./././Mathport/Syntax/Translate/Expr.lean:177:8: unsupported: ambiguous notation -/
 @[elab_as_elim]
 protected theorem InClosure.rec_on {C : R → Prop} {x : R} (hx : x ∈ closure s) (h1 : C 1)
     (hneg1 : C (-1)) (hs : ∀ z ∈ s, ∀ n, C n → C (z * n)) (ha : ∀ {x y}, C x → C y → C (x + y)) :
@@ -149,56 +145,50 @@ protected theorem InClosure.rec_on {C : R → Prop} {x : R} (hx : x ∈ closure 
     exact ha this (ih HL.2)
   replace HL := HL.1
   clear ih tl
-  rsuffices ⟨L, HL', HP | HP⟩ :
-    ∃ L : List R, (∀ x ∈ L, x ∈ s) ∧ (List.prod hd = List.prod L ∨ List.prod hd = -List.prod L)
-  · rw [HP]
-    clear HP HL hd
-    induction' L with hd tl ih
-    · exact h1
-    rw [List.forall_mem_cons] at HL'
-    rw [List.prod_cons]
-    exact hs _ HL'.1 _ (ih HL'.2)
-  · rw [HP]
-    clear HP HL hd
-    induction' L with hd tl ih
-    · exact hneg1
-    rw [List.prod_cons, neg_mul_eq_mul_neg]
-    rw [List.forall_mem_cons] at HL'
-    exact hs _ HL'.1 _ (ih HL'.2)
+  -- Porting note: Expanded `rsuffices`
+  suffices ∃ L : List R,
+      (∀ x ∈ L, x ∈ s) ∧ (List.prod hd = List.prod L ∨ List.prod hd = -List.prod L) by
+    rcases this with ⟨L, HL', HP | HP⟩ <;> rw [HP] <;> clear HP HL
+    · induction' L with hd tl ih
+      · exact h1
+      rw [List.forall_mem_cons] at HL'
+      rw [List.prod_cons]
+      exact hs _ HL'.1 _ (ih HL'.2)
+    · induction' L with hd tl ih
+      · exact hneg1
+      rw [List.prod_cons, neg_mul_eq_mul_neg]
+      rw [List.forall_mem_cons] at HL'
+      exact hs _ HL'.1 _ (ih HL'.2)
   induction' hd with hd tl ih
   · exact ⟨[], List.forall_mem_nil _, Or.inl rfl⟩
   rw [List.forall_mem_cons] at HL
   rcases ih HL.2 with ⟨L, HL', HP | HP⟩ <;> cases' HL.1 with hhd hhd
-  ·
-    exact
+  · exact
       ⟨hd::L, List.forall_mem_cons.2 ⟨hhd, HL'⟩,
         Or.inl <| by rw [List.prod_cons, List.prod_cons, HP]⟩
   · exact ⟨L, HL', Or.inr <| by rw [List.prod_cons, hhd, neg_one_mul, HP]⟩
-  ·
-    exact
+  · exact
       ⟨hd::L, List.forall_mem_cons.2 ⟨hhd, HL'⟩,
         Or.inr <| by rw [List.prod_cons, List.prod_cons, HP, neg_mul_eq_mul_neg]⟩
   · exact ⟨L, HL', Or.inl <| by rw [List.prod_cons, hhd, HP, neg_one_mul, neg_neg]⟩
 #align ring.in_closure.rec_on Ring.InClosure.rec_on
 
 theorem closure.isSubring : IsSubring (closure s) :=
-  {
-    AddGroup.closure.isAddSubgroup
-      _ with
+  { AddGroup.closure.isAddSubgroup _ with
     one_mem := AddGroup.mem_closure <| IsSubmonoid.one_mem <| Monoid.closure.isSubmonoid _
-    mul_mem := fun {a _} ha hb =>
+    mul_mem := fun {a _} ha hb ↦
       AddGroup.InClosure.recOn hb
-        (fun {c} hc =>
+        (fun {c} hc ↦
           AddGroup.InClosure.recOn ha
-            (fun hd => AddGroup.subset_closure ((Monoid.closure.isSubmonoid _).mul_mem hd hc))
+            (fun hd ↦ AddGroup.subset_closure ((Monoid.closure.isSubmonoid _).mul_mem hd hc))
             ((zero_mul c).symm ▸ (AddGroup.closure.isAddSubgroup _).zero_mem)
-            (fun {d} _ hdc =>
+            (fun {d} _ hdc ↦
               neg_mul_eq_neg_mul d c ▸ (AddGroup.closure.isAddSubgroup _).neg_mem hdc)
-            fun {d e} _ _ hdc hec =>
+            fun {d e} _ _ hdc hec ↦
             (add_mul d e c).symm ▸ (AddGroup.closure.isAddSubgroup _).add_mem hdc hec)
         ((mul_zero a).symm ▸ (AddGroup.closure.isAddSubgroup _).zero_mem)
-        (fun {c} _ hac => neg_mul_eq_mul_neg a c ▸ (AddGroup.closure.isAddSubgroup _).neg_mem hac)
-        fun {c d} _ _ hac had =>
+        (fun {c} _ hac ↦ neg_mul_eq_mul_neg a c ▸ (AddGroup.closure.isAddSubgroup _).neg_mem hac)
+        fun {c d} _ _ hac had ↦
         (mul_add a c d).symm ▸ (AddGroup.closure.isAddSubgroup _).add_mem hac had }
 #align ring.closure.is_subring Ring.closure.isSubring
 
@@ -206,7 +196,8 @@ theorem mem_closure {a : R} : a ∈ s → a ∈ closure s :=
   AddGroup.mem_closure ∘ @Monoid.subset_closure _ _ _ _
 #align ring.mem_closure Ring.mem_closure
 
-theorem subset_closure : s ⊆ closure s := fun _ => mem_closure
+theorem subset_closure : s ⊆ closure s :=
+  fun _ ↦ mem_closure
 #align ring.subset_closure Ring.subset_closure
 
 theorem closure_subset {t : Set R} (ht : IsSubring t) : s ⊆ t → closure s ⊆ t :=
@@ -227,7 +218,7 @@ theorem image_closure {S : Type _} [Ring S] (f : R →+* S) (s : Set R) :
   le_antisymm
     (by
       rintro _ ⟨x, hx, rfl⟩
-      apply AddGroup.InClosure.recOn (motive := fun {x} _ => f x ∈ closure (f '' s)) hx _ <;> intros
+      apply AddGroup.InClosure.recOn (motive := fun {x} _ ↦ f x ∈ closure (f '' s)) hx _ <;> intros
       · rw [f.map_zero]
         apply closure.isSubring.zero_mem
       · rw [f.map_neg]
