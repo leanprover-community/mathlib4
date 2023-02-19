963a07445f6f747808e15611be74841ecc201b8e
Jason Yuen <jason_yuen2007@hotmail.com>
2023-02-19 14:29:55 +0000

    feat: port Algebra.Module.Algebra (#2370)

--- Mathlib.lean
+++ Mathlib.lean
@@ -97,6 +97,7 @@ import Mathlib.Algebra.Homology.ComplexShape
 import Mathlib.Algebra.IndicatorFunction
 import Mathlib.Algebra.Invertible
 import Mathlib.Algebra.IsPrimePow
+import Mathlib.Algebra.Module.Algebra
 import Mathlib.Algebra.Module.Basic
 import Mathlib.Algebra.Module.BigOperators
 import Mathlib.Algebra.Module.Equiv
