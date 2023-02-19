778ec018dee9fdc2862f468b8b820b7ac2961da1
Moritz Firsching <firsching@google.com>
2023-02-19 10:26:09 +0100

    first pass

--- Mathlib/Data/Multiset/Interval.lean
+++ Mathlib/Data/Multiset/Interval.lean
@@ -16,14 +16,14 @@ import Mathlib.Data.Nat.Interval
 /-!
 # Finite intervals of multisets
 
-This file provides the `locally_finite_order` instance for `multiset α` and calculates the
+This file provides the `LocallyFiniteOrder` instance for `Multiset α` and calculates the
 cardinality of its finite intervals.
 
 ## Implementation notes
 
-We implement the intervals via the intervals on `dfinsupp`, rather than via filtering
-`multiset.powerset`; this is because `(multiset.replicate n x).powerset` has `2^n` entries not `n+1`
-entries as it contains duplicates. We do not go via `finsupp` as this would be noncomputable, and
+We implement the intervals via the intervals on `Dfinsupp`, rather than via filtering
+`Multiset.Powerset`; this is because `(Multiset.replicate n x).Powerset` has `2^n` entries not `n+1`
+entries as it contains duplicates. We do not go via `Finsupp` as this would be noncomputable, and
 multisets are typically used computationally.
 
 -/
@@ -42,39 +42,40 @@ variable [DecidableEq α] (f g : Multiset α)
 instance : LocallyFiniteOrder (Multiset α) :=
   LocallyFiniteOrder.ofIcc (Multiset α)
     (fun f g =>
-      (Finset.Icc f.toDfinsupp g.toDfinsupp).map Multiset.equivDfinsupp.toEquiv.symm.toEmbedding)
+      (Finset.Icc (Multiset.toDfinsupp f) (Multiset.toDfinsupp g)).map
+      Multiset.equivDfinsupp.toEquiv.symm.toEmbedding)
     fun f g x => by simp
 
 theorem Icc_eq :
     Finset.Icc f g =
-      (Finset.Icc f.toDfinsupp g.toDfinsupp).map Multiset.equivDfinsupp.toEquiv.symm.toEmbedding :=
+      (Finset.Icc (Multiset.toDfinsupp f) (Multiset.toDfinsupp g)).map
+      Multiset.equivDfinsupp.toEquiv.symm.toEmbedding :=
   rfl
 #align multiset.Icc_eq Multiset.Icc_eq
 
 theorem card_Icc :
-    (Finset.Icc f g).card = ∏ i in f.toFinset ∪ g.toFinset, g.count i + 1 - f.count i := by
+    (Finset.Icc f g).card = ∏ i in f.toFinset ∪ g.toFinset, (g.count i + 1 - f.count i) := by
   simp_rw [Icc_eq, Finset.card_map, Dfinsupp.card_Icc, Nat.card_Icc, Multiset.toDfinsupp_apply,
     toDfinsupp_support]
 #align multiset.card_Icc Multiset.card_Icc
 
 theorem card_Ico :
-    (Finset.Ico f g).card = (∏ i in f.toFinset ∪ g.toFinset, g.count i + 1 - f.count i) - 1 := by
+    (Finset.Ico f g).card = ∏ i in f.toFinset ∪ g.toFinset, (g.count i + 1 - f.count i) - 1 := by
   rw [card_Ico_eq_card_Icc_sub_one, card_Icc]
 #align multiset.card_Ico Multiset.card_Ico
 
 theorem card_Ioc :
-    (Finset.Ioc f g).card = (∏ i in f.toFinset ∪ g.toFinset, g.count i + 1 - f.count i) - 1 := by
+    (Finset.Ioc f g).card = ∏ i in f.toFinset ∪ g.toFinset, (g.count i + 1 - f.count i) - 1 := by
   rw [card_Ioc_eq_card_Icc_sub_one, card_Icc]
 #align multiset.card_Ioc Multiset.card_Ioc
 
 theorem card_Ioo :
-    (Finset.Ioo f g).card = (∏ i in f.toFinset ∪ g.toFinset, g.count i + 1 - f.count i) - 2 := by
+    (Finset.Ioo f g).card = ∏ i in f.toFinset ∪ g.toFinset, (g.count i + 1 - f.count i) - 2 := by
   rw [card_Ioo_eq_card_Icc_sub_two, card_Icc]
 #align multiset.card_Ioo Multiset.card_Ioo
 
-theorem card_Iic : (Finset.Iic f).card = ∏ i in f.toFinset, f.count i + 1 := by
-  simp_rw [Iic_eq_Icc, card_Icc, bot_eq_zero, to_finset_zero, empty_union, count_zero, tsub_zero]
+theorem card_Iic : (Finset.Iic f).card = ∏ i in f.toFinset, (f.count i + 1) := by
+  simp_rw [Iic_eq_Icc, card_Icc, bot_eq_zero, toFinset_zero, empty_union, count_zero, tsub_zero]
 #align multiset.card_Iic Multiset.card_Iic
 
 end Multiset
-
