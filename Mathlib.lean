843cf694ca60ac0583e9e1ca352f37b9358dee88
Matthew Ballard <matt@mrb.email>
2023-02-18 23:52:06 -0500

    fix import file

--- Mathlib.lean
+++ Mathlib.lean
@@ -258,7 +258,10 @@ import Mathlib.CategoryTheory.Iso
 import Mathlib.CategoryTheory.LiftingProperties.Adjunction
 import Mathlib.CategoryTheory.LiftingProperties.Basic
 import Mathlib.CategoryTheory.Limits.Cones
+import Mathlib.CategoryTheory.Limits.HasLimits
+import Mathlib.CategoryTheory.Limits.IsLimit
 import Mathlib.CategoryTheory.Limits.Shapes.StrongEpi
+import Mathlib.CategoryTheory.Monoidal.Category
 import Mathlib.CategoryTheory.NatIso
 import Mathlib.CategoryTheory.NatTrans
 import Mathlib.CategoryTheory.Opposites
