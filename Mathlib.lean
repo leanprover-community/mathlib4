be32c274e7a8fa80a48c93cefddfe8de70661c3c
Pol_tta <pol_tta@outlook.jp>
2023-02-20 12:02:52 +0000

    feat: port SetTheory.Ordinal.Exponential (#2330)

--- Mathlib.lean
+++ Mathlib.lean
@@ -984,6 +984,7 @@ import Mathlib.SetTheory.Cardinal.SchroederBernstein
 import Mathlib.SetTheory.Lists
 import Mathlib.SetTheory.Ordinal.Arithmetic
 import Mathlib.SetTheory.Ordinal.Basic
+import Mathlib.SetTheory.Ordinal.Exponential
 import Mathlib.SetTheory.Ordinal.NaturalOps
 import Mathlib.SetTheory.Ordinal.Topology
 import Mathlib.Tactic.Abel
