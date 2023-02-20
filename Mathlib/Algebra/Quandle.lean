2c953bfdf0d4d3ee9a80ce9f784ae244c86f460e
Kyle Miller <kmill31415@gmail.com>
2023-02-20 19:07:32 +0100

    update commit id

--- Mathlib/Algebra/Quandle.lean
+++ Mathlib/Algebra/Quandle.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kyle Miller
 
 ! This file was ported from Lean 3 source module algebra.quandle
-! leanprover-community/mathlib commit 8631e2d5ea77f6c13054d9151d82b83069680cb1
+! leanprover-community/mathlib commit 28aa996fc6fb4317f0083c4e6daf79878d81be33
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
