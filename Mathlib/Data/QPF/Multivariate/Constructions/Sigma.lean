20e7ff5a630a6d27496eb7ebe193f3fd46f4de30
casavaca <96765450+casavaca@users.noreply.github.com>
2023-02-21 11:50:35 -0800

    Fix rebase

--- Mathlib/Data/QPF/Multivariate/Constructions/Sigma.lean
+++ Mathlib/Data/QPF/Multivariate/Constructions/Sigma.lean
@@ -29,8 +29,7 @@ variable (F : A → TypeVec.{u} n → Type u)
 /-- Dependent sum of of an `n`-ary functor. The sum can range over
 data types like `ℕ` or over `Type.{u-1}` -/
 def Sigma (v : TypeVec.{u} n) : Type u :=
-  -- Porting note: should be Σα : A, F α v. but failed because somehow
-  -- Σ bind to MvQPF.Sigma, not _root_.Sigma
+  -- Porting note: replaced Σα : A, F α v with_root_.Sigma fun α : A => F α v
   _root_.Sigma fun α : A => F α v
 #align mvqpf.sigma MvQPF.Sigma
 
@@ -58,8 +57,7 @@ variable [∀ α, MvQPF <| F α]
 
 /-- polynomial functor representation of a dependent sum -/
 protected def P : MvPFunctor n :=
-  -- Porting note, Σ bind to Sigma, not _root_.Sigma
-  -- old code: ⟨Σa, (p (F a)).A, fun x => (p (F x.1)).B x.2⟩
+  -- Porting note, replaced Σ with _root_.Sigma
   ⟨_root_.Sigma fun a => (P (F a)).A, fun x => (P (F x.1)).B x.2⟩
 set_option linter.uppercaseLean3 false in
 #align mvqpf.sigma.P MvQPF.Sigma.P
@@ -93,24 +91,24 @@ instance : MvFunctor (Pi F) where map f x a := f <$$> x a
 variable [∀ α, MvQPF <| F α]
 
 /-- polynomial functor representation of a dependent product -/
-protected def p : MvPFunctor n :=
+protected def P : MvPFunctor n :=
   -- Porting note: _root_.Sigma
   ⟨∀ a, (P (F a)).A, fun x i => _root_.Sigma fun a : A => (P (F a)).B (x a) i⟩
 set_option linter.uppercaseLean3 false in
-#align mvqpf.pi.P MvQPF.Pi.p
+#align mvqpf.pi.P MvQPF.Pi.P
 
 /-- abstraction function for dependent products -/
-protected def abs ⦃α⦄ : (Pi.p F).Obj α → Pi F α
+protected def abs ⦃α⦄ : (Pi.P F).Obj α → Pi F α
   | ⟨a, f⟩ => fun x => MvQPF.abs ⟨a x, fun i y => f i ⟨_, y⟩⟩
 #align mvqpf.pi.abs MvQPF.Pi.abs
 
 /-- representation function for dependent products -/
-protected def repr ⦃α⦄ : Pi F α → (Pi.p F).Obj α
+protected def repr ⦃α⦄ : Pi F α → (Pi.P F).Obj α
   | f => ⟨fun a => (MvQPF.repr (f a)).1, fun _i a => (MvQPF.repr (f _)).2 _ a.2⟩
 #align mvqpf.pi.repr MvQPF.Pi.repr
 
 instance : MvQPF (Pi F) where
-  P := Pi.p F
+  P := Pi.P F
   abs := @Pi.abs _ _ F _ _
   repr := @Pi.repr _ _ F _ _
   abs_repr := by rintro α f; simp only [Pi.abs, Pi.repr, Sigma.eta, abs_repr]
