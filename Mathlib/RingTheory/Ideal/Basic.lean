9567c72d10b044c48a8a0ab84d4e50193c13029c
Johan Commelin <johan@commelin.net>
2023-02-21 09:56:43 +0100

    fix and lint

--- Mathlib/RingTheory/Ideal/Basic.lean
+++ Mathlib/RingTheory/Ideal/Basic.lean
@@ -589,7 +589,8 @@ theorem IsPrime.pow_mem_iff_mem {I : Ideal α} (hI : I.IsPrime) {r : α} (n : �
 #align ideal.is_prime.pow_mem_iff_mem Ideal.IsPrime.pow_mem_iff_mem
 
 theorem pow_multiset_sum_mem_span_pow (s : Multiset α) (n : ℕ) :
-    s.sum ^ (Multiset.card s * n + 1) ∈ span ((s.map fun (x : α) => x ^ (n + 1)).toFinset : Set α) := by
+    s.sum ^ (Multiset.card s * n + 1) ∈ span ((s.map fun (x:α) ↦ x ^ (n + 1)).toFinset : Set α) :=
+  by
   induction' s using Multiset.induction_on with a s hs
   · simp
   simp only [Finset.coe_insert, Multiset.map_cons, Multiset.toFinset_cons, Multiset.sum_cons,
@@ -598,9 +599,8 @@ theorem pow_multiset_sum_mem_span_pow (s : Multiset α) (n : ℕ) :
   intro c _hc
   rw [mem_span_insert]
   by_cases h : n + 1 ≤ c
-  · refine'
-      ⟨a ^ (c - (n + 1)) * s.sum ^ ((Multiset.card s + 1) * n + 1 - c) * ((Multiset.card s + 1) * n + 1).choose c, 0,
-        Submodule.zero_mem _, _⟩
+  · refine' ⟨a ^ (c - (n + 1)) * s.sum ^ ((Multiset.card s + 1) * n + 1 - c) *
+      ((Multiset.card s + 1) * n + 1).choose c, 0, Submodule.zero_mem _, _⟩
     rw [mul_comm _ (a ^ (n + 1))]
     simp_rw [← mul_assoc]
     rw [← pow_add, add_zero, add_tsub_cancel_of_le h]
@@ -621,25 +621,25 @@ theorem sum_pow_mem_span_pow {ι} (s : Finset ι) (f : ι → α) (n : ℕ) :
     Finset.coe_image, Finset.val_toFinset] using pow_multiset_sum_mem_span_pow (s.1.map f) n
 #align ideal.sum_pow_mem_span_pow Ideal.sum_pow_mem_span_pow
 
-theorem span_pow_eq_top (s : Set α) (hs : span s = ⊤) (n : ℕ) : span ((fun (x : α) => x ^ n) '' s) = ⊤ :=
+theorem span_pow_eq_top (s : Set α) (hs : span s = ⊤) (n : ℕ) :
+    span ((fun (x : α) => x ^ n) '' s) = ⊤ :=
   by
   rw [eq_top_iff_one]
-  cases n
+  cases' n with n
   · obtain rfl | ⟨x, hx⟩ := eq_empty_or_nonempty s
     · rw [Set.image_empty, hs]
       trivial
     · exact subset_span ⟨_, hx, pow_zero _⟩
   rw [eq_top_iff_one, span, Finsupp.mem_span_iff_total] at hs
   rcases hs with ⟨f, hf⟩
-  change (f.support.sum fun a => f a * a) = 1 at hf
+  have hf : (f.support.sum fun a => f a * a) = 1 := hf -- Porting note: was `change ... at hf`
   have := sum_pow_mem_span_pow f.support (fun a => f a * a) n
   rw [hf, one_pow] at this
   refine' span_le.mpr _ this
   rintro _ hx
   simp_rw [Finset.mem_coe, Set.mem_image] at hx
-  rcases hx with ⟨x, hx, rfl⟩
-  have : span ({x ^ (n + 1)} : Set α) ≤ span ((fun x : α => x ^ (n + 1)) '' s) :=
-    by
+  rcases hx with ⟨x, _, rfl⟩
+  have : span ({(x:α) ^ (n + 1)} : Set α) ≤ span ((fun x : α => x ^ (n + 1)) '' s) := by
     rw [span_le, Set.singleton_subset_iff]
     exact subset_span ⟨x, x.prop, rfl⟩
   refine' this _
@@ -751,7 +751,6 @@ namespace Ring
 
 variable {R : Type _} [CommSemiring R]
 
-/- ./././Mathport/Syntax/Translate/Basic.lean:628:2: warning: expanding binder collection (x «expr ≠ » (0 : R)) -/
 theorem exists_not_isUnit_of_not_isField [Nontrivial R] (hf : ¬IsField R) :
     ∃ (x : R) (_hx : x ≠ (0 : R)), ¬IsUnit x := by
   have : ¬_ := fun h => hf ⟨exists_pair_ne R, mul_comm, h⟩
