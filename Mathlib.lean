e94a78db0cbd821cd781781019cbc9c87186d81d
arienmalec <arien.malec@gmail.com>
2023-02-21 23:06:55 +0000

    feat: Port/Topology.Algebra.Group.Compact (#2420)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1130,6 +1130,7 @@ import Mathlib.Topology.Alexandroff
 import Mathlib.Topology.Algebra.ConstMulAction
 import Mathlib.Topology.Algebra.Constructions
 import Mathlib.Topology.Algebra.Group.Basic
+import Mathlib.Topology.Algebra.Group.Compact
 import Mathlib.Topology.Algebra.GroupWithZero
 import Mathlib.Topology.Algebra.Monoid
 import Mathlib.Topology.Algebra.MulAction
