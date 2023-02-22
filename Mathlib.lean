b2dca69e9597bbd8372b10c28c574b82846e431f
Matthew Robert Ballard <matt@mrb.email>
2023-02-22 04:10:59 +0000

    feat: port CategoryTheory.IsomorphismClasses (#2394)

--- Mathlib.lean
+++ Mathlib.lean
@@ -257,6 +257,7 @@ import Mathlib.CategoryTheory.Functor.InvIsos
 import Mathlib.CategoryTheory.Groupoid
 import Mathlib.CategoryTheory.Groupoid.Basic
 import Mathlib.CategoryTheory.Iso
+import Mathlib.CategoryTheory.IsomorphismClasses
 import Mathlib.CategoryTheory.LiftingProperties.Basic
 import Mathlib.CategoryTheory.Monoidal.Category
 import Mathlib.CategoryTheory.NatIso
