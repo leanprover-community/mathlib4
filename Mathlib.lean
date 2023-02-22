484f3e9fabd8905366ccc328b3bb749da14c6dc0
Joël Riou <joel.riou@universite-paris-saclay.fr>
2023-02-22 19:59:57 +0000

    feat: port CategoryTheory.Adjunction.FullyFaithful (#2418)

--- Mathlib.lean
+++ Mathlib.lean
@@ -223,6 +223,7 @@ import Mathlib.Algebra.Tropical.Basic
 import Mathlib.Algebra.Tropical.BigOperators
 import Mathlib.Algebra.Tropical.Lattice
 import Mathlib.CategoryTheory.Adjunction.Basic
+import Mathlib.CategoryTheory.Adjunction.FullyFaithful
 import Mathlib.CategoryTheory.Adjunction.Whiskering
 import Mathlib.CategoryTheory.Arrow
 import Mathlib.CategoryTheory.Balanced
