8f753e84d8ec3abd947eb733cb741d1e913552ba
casavaca <96765450+casavaca@users.noreply.github.com>
2023-02-22 03:15:30 +0000

    feat: Port Data.QPF.Multivariate.Constructions.Sigma (#2225)
    Co-authored-by: Alex Keizer <alex@keizer.dev>

--- Mathlib.lean
+++ Mathlib.lean
@@ -608,6 +608,7 @@ import Mathlib.Data.QPF.Multivariate.Constructions.Comp
 import Mathlib.Data.QPF.Multivariate.Constructions.Const
 import Mathlib.Data.QPF.Multivariate.Constructions.Prj
 import Mathlib.Data.QPF.Multivariate.Constructions.Quot
+import Mathlib.Data.QPF.Multivariate.Constructions.Sigma
 import Mathlib.Data.Quot
 import Mathlib.Data.Rat.Basic
 import Mathlib.Data.Rat.BigOperators
