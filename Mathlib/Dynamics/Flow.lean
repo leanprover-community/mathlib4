d9a3b4a9c0df9b4f7dd824a8d6fde11f3674a3c7
casavaca <96765450+casavaca@users.noreply.github.com>
2023-02-22 13:02:00 -0800

    Comments and reflow

--- Mathlib/Dynamics/Flow.lean
+++ Mathlib/Dynamics/Flow.lean
@@ -66,13 +66,13 @@ theorem IsInvariant.isFwInvariant [Preorder τ] [Zero τ] {ϕ : τ → α → α
 #align is_invariant.is_fw_invariant IsInvariant.isFwInvariant
 
 /-- If `τ` is a `canonically_ordered_add_monoid` (e.g., `ℕ` or `ℝ≥0`), then the notions
-`is_fw_invariant` and `is_invariant` are equivalent. -/
+`IsFwInvariant` and `IsInvariant` are equivalent. -/
 theorem IsFwInvariant.isInvariant [CanonicallyOrderedAddMonoid τ] {ϕ : τ → α → α} {s : Set α}
     (h : IsFwInvariant ϕ s) : IsInvariant ϕ s := fun t => h (zero_le t)
 #align is_fw_invariant.is_invariant IsFwInvariant.isInvariant
 
-/-- If `τ` is a `canonically_ordered_add_monoid` (e.g., `ℕ` or `ℝ≥0`), then the notions
-`is_fw_invariant` and `is_invariant` are equivalent. -/
+/-- If `τ` is a `CanonicallyOrderedAddMonoid` (e.g., `ℕ` or `ℝ≥0`), then the notions
+`IsFwInvariant` and `IsInvariant` are equivalent. -/
 theorem isFwInvariant_iff_isInvariant [CanonicallyOrderedAddMonoid τ] {ϕ : τ → α → α} {s : Set α} :
     IsFwInvariant ϕ s ↔ IsInvariant ϕ s :=
   ⟨IsFwInvariant.isInvariant, IsInvariant.isFwInvariant⟩
@@ -106,8 +106,7 @@ instance : Inhabited (Flow τ α) :=
       map_add' := fun _ _ _ => rfl
       map_zero' := fun _ => rfl }⟩
 
-instance : CoeFun (Flow τ α) fun _ => τ → α → α :=
-  ⟨Flow.toFun⟩
+instance : CoeFun (Flow τ α) fun _ => τ → α → α := ⟨Flow.toFun⟩
 
 @[ext]
 theorem ext : ∀ {ϕ₁ ϕ₂ : Flow τ α}, (∀ t x, ϕ₁ t x = ϕ₂ t x) → ϕ₁ = ϕ₂
@@ -126,23 +125,19 @@ protected theorem continuous {β : Type _} [TopologicalSpace β] {t : β → τ}
 alias Flow.continuous ← _root_.continuous.flow
 #align continuous.flow continuous.flow
 
-theorem map_add (t₁ t₂ : τ) (x : α) : ϕ (t₁ + t₂) x = ϕ t₁ (ϕ t₂ x) :=
-  ϕ.map_add' _ _ _
+theorem map_add (t₁ t₂ : τ) (x : α) : ϕ (t₁ + t₂) x = ϕ t₁ (ϕ t₂ x) := ϕ.map_add' _ _ _
 #align flow.map_add Flow.map_add
 
 @[simp]
-theorem map_zero : ϕ 0 = id :=
-  funext ϕ.map_zero'
+theorem map_zero : ϕ 0 = id := funext ϕ.map_zero'
 #align flow.map_zero Flow.map_zero
 
-theorem map_zero_apply (x : α) : ϕ 0 x = x :=
-  ϕ.map_zero' x
+theorem map_zero_apply (x : α) : ϕ 0 x = x := ϕ.map_zero' x
 #align flow.map_zero_apply Flow.map_zero_apply
 
 /-- Iterations of a continuous function from a topological space `α`
     to itself defines a semiflow by `ℕ` on `α`. -/
-def fromIter {g : α → α} (h : Continuous g) : Flow ℕ α
-    where
+def fromIter {g : α → α} (h : Continuous g) : Flow ℕ α where
   toFun n x := (g^[n]) x
   cont' := continuous_uncurry_of_discreteTopology_left (Continuous.iterate h)
   map_add' := iterate_add_apply _
@@ -150,8 +145,7 @@ def fromIter {g : α → α} (h : Continuous g) : Flow ℕ α
 #align flow.from_iter Flow.fromIter
 
 /-- Restriction of a flow onto an invariant set. -/
-def restrict {s : Set α} (h : IsInvariant ϕ s) : Flow τ (↥s)
-    where
+def restrict {s : Set α} (h : IsInvariant ϕ s) : Flow τ (↥s) where
   toFun t := (h t).restrict _ _ _
   cont' := (ϕ.continuous continuous_fst continuous_subtype_val.snd').subtype_mk _
   map_add' _ _ _ := Subtype.ext (map_add _ _ _ _)
@@ -181,10 +175,12 @@ def reverse : Flow τ α where
   map_zero' _ := by dsimp; rw [neg_zero, map_zero_apply]
 #align flow.reverse Flow.reverse
 
--- Porting note: add @continuity to Flow.toFun
+-- Porting note: add @continuity to Flow.toFun so that these works:
+-- Porting note: Homeomorphism.continuous_toFun  : Continuous toFun  := by continuity
+-- Porting note: Homeomorphism.continuous_invFun : Continuous invFun := by continuity
 @[continuity]
 theorem continuous_toFun (t : τ) : Continuous (ϕ.toFun t) := by
-  rw [←curry_uncurry ϕ.1]
+  rw [←curry_uncurry ϕ.toFun]
   apply continuous_curry
   exact ϕ.cont'
 
