7cc7261089d8f5f09bf2bd0158d62ed920e1f7fa
Arien Malec <arien.malec@gmail.com>
2023-02-23 08:46:40 -0800

    fix

--- Mathlib/GroupTheory/Perm/Sign.lean
+++ Mathlib/GroupTheory/Perm/Sign.lean
@@ -672,7 +672,7 @@ theorem sign_subtypePerm (f : Perm α) {p : α → Prop} [DecidablePred p] (h₁
     congr
     rw [← l.2.1]
     skip
-    rw [← hl'₂]
+  simp_rw [← hl'₂]
   rw [sign_prod_list_swap l.2.2, sign_prod_list_swap hl', List.length_map]
 #align equiv.perm.sign_subtype_perm Equiv.Perm.sign_subtypePerm
 
