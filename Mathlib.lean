dfbcc376ed088969941e290cd8cb497df2457236
casavaca <96765450+casavaca@users.noreply.github.com>
2023-02-24 08:05:22 +0000

    feat: Port SetTheory.Ordinal.CantorNormalForm (#2471)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1036,6 +1036,7 @@ import Mathlib.SetTheory.Cardinal.SchroederBernstein
 import Mathlib.SetTheory.Lists
 import Mathlib.SetTheory.Ordinal.Arithmetic
 import Mathlib.SetTheory.Ordinal.Basic
+import Mathlib.SetTheory.Ordinal.CantorNormalForm
 import Mathlib.SetTheory.Ordinal.Exponential
 import Mathlib.SetTheory.Ordinal.FixedPoint
 import Mathlib.SetTheory.Ordinal.NaturalOps
