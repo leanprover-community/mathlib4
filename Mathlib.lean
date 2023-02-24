43fabded487f1bca6259d4aa6eaa8c170c2f3025
casavaca <96765450+casavaca@users.noreply.github.com>
2023-02-24 08:05:23 +0000

    feat: Port SetTheory.Cardinal.Continuum (#2474)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1030,6 +1030,7 @@ import Mathlib.RingTheory.Subring.Basic
 import Mathlib.RingTheory.Subsemiring.Basic
 import Mathlib.RingTheory.Subsemiring.Pointwise
 import Mathlib.SetTheory.Cardinal.Basic
+import Mathlib.SetTheory.Cardinal.Continuum
 import Mathlib.SetTheory.Cardinal.Finite
 import Mathlib.SetTheory.Cardinal.Ordinal
 import Mathlib.SetTheory.Cardinal.SchroederBernstein
