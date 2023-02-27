219bbfb8085d2606b60ccf0e9eb23ecd5bbb133d
Moritz Firsching <moritz.firsching@gmail.com>
2023-02-27 10:46:07 +0000

    feat: port CategoryTheory.Monoidal.Functorial (#2504)
    Co-authored-by: Moritz Firsching <firsching@google.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -275,6 +275,7 @@ import Mathlib.CategoryTheory.Limits.IsLimit
 import Mathlib.CategoryTheory.Limits.Shapes.StrongEpi
 import Mathlib.CategoryTheory.Monoidal.Category
 import Mathlib.CategoryTheory.Monoidal.Functor
+import Mathlib.CategoryTheory.Monoidal.Functorial
 import Mathlib.CategoryTheory.NatIso
 import Mathlib.CategoryTheory.NatTrans
 import Mathlib.CategoryTheory.Opposites
