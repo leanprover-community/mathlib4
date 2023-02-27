085e6e544fcaa25e99e1a554b1a4e980ce4f2784
Yury G. Kudryashov <urkud@urkud.name>
2023-02-27 15:56:42 -0600

    Rename again

--- Mathlib.lean
+++ Mathlib.lean
@@ -657,7 +657,7 @@ import Mathlib.Data.Real.Basic
 import Mathlib.Data.Real.CauSeq
 import Mathlib.Data.Real.CauSeqCompletion
 import Mathlib.Data.Real.ENNReal
-import Mathlib.Data.Real.EnatENNReal
+import Mathlib.Data.Real.ENatENNReal
 import Mathlib.Data.Real.NNReal
 import Mathlib.Data.Real.Pointwise
 import Mathlib.Data.Real.Sign
