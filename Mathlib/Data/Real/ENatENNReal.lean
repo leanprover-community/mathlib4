38ad5d6e274e96667257a23b4f2d875df4996003
Yury G. Kudryashov <urkud@urkud.name>
2023-02-27 15:58:22 -0600

    use `toENNReal` instead of `coe_ennreal`

--- Mathlib/Data/Real/ENatENNReal.lean
+++ Mathlib/Data/Real/ENatENNReal.lean
@@ -50,76 +50,76 @@ def toENNRealRingHom : ℕ∞ →+* ℝ≥0∞ :=
 #align enat.to_ennreal_ring_hom ENat.toENNRealRingHom
 
 @[simp, norm_cast]
-theorem coe_ennreal_top : ((⊤ : ℕ∞) : ℝ≥0∞) = ⊤ :=
+theorem toENNReal_top : ((⊤ : ℕ∞) : ℝ≥0∞) = ⊤ :=
   rfl
-#align enat.coe_ennreal_top ENat.coe_ennreal_top
+#align enat.coe_ennreal_top ENat.toENNReal_top
 
 @[simp, norm_cast]
-theorem coe_ennreal_coe (n : ℕ) : ((n : ℕ∞) : ℝ≥0∞) = n :=
+theorem toENNReal_coe (n : ℕ) : ((n : ℕ∞) : ℝ≥0∞) = n :=
   rfl
-#align enat.coe_ennreal_coe ENat.coe_ennreal_coe
+#align enat.coe_ennreal_coe ENat.toENNReal_coe
 
 @[simp, norm_cast]
-theorem coe_ennreal_ofNat (n : ℕ) [n.AtLeastTwo] : ((OfNat.ofNat n : ℕ∞) : ℝ≥0∞) = OfNat.ofNat n :=
+theorem toENNReal_ofNat (n : ℕ) [n.AtLeastTwo] : ((OfNat.ofNat n : ℕ∞) : ℝ≥0∞) = OfNat.ofNat n :=
   rfl
 
 @[simp, norm_cast]
-theorem coe_ennreal_le : (m : ℝ≥0∞) ≤ n ↔ m ≤ n :=
+theorem toENNReal_le : (m : ℝ≥0∞) ≤ n ↔ m ≤ n :=
   toENNRealOrderEmbedding.le_iff_le
-#align enat.coe_ennreal_le ENat.coe_ennreal_le
+#align enat.coe_ennreal_le ENat.toENNReal_le
 
 @[simp, norm_cast]
-theorem coe_ennreal_lt : (m : ℝ≥0∞) < n ↔ m < n :=
+theorem toENNReal_lt : (m : ℝ≥0∞) < n ↔ m < n :=
   toENNRealOrderEmbedding.lt_iff_lt
-#align enat.coe_ennreal_lt ENat.coe_ennreal_lt
+#align enat.coe_ennreal_lt ENat.toENNReal_lt
 
 @[mono]
-theorem coe_ennreal_mono : Monotone ((↑) : ℕ∞ → ℝ≥0∞) :=
+theorem toENNReal_mono : Monotone ((↑) : ℕ∞ → ℝ≥0∞) :=
   toENNRealOrderEmbedding.monotone
-#align enat.coe_ennreal_mono ENat.coe_ennreal_mono
+#align enat.coe_ennreal_mono ENat.toENNReal_mono
 
 @[mono]
-theorem coe_ennreal_strictMono : StrictMono ((↑) : ℕ∞ → ℝ≥0∞) :=
+theorem toENNReal_strictMono : StrictMono ((↑) : ℕ∞ → ℝ≥0∞) :=
   toENNRealOrderEmbedding.strictMono
-#align enat.coe_ennreal_strict_mono ENat.coe_ennreal_strictMono
+#align enat.coe_ennreal_strict_mono ENat.toENNReal_strictMono
 
 @[simp, norm_cast]
-theorem coe_ennreal_zero : ((0 : ℕ∞) : ℝ≥0∞) = 0 :=
+theorem toENNReal_zero : ((0 : ℕ∞) : ℝ≥0∞) = 0 :=
   map_zero toENNRealRingHom
-#align enat.coe_ennreal_zero ENat.coe_ennreal_zero
+#align enat.coe_ennreal_zero ENat.toENNReal_zero
 
 @[simp]
-theorem coe_ennreal_add (m n : ℕ∞) : ↑(m + n) = (m + n : ℝ≥0∞) :=
+theorem toENNReal_add (m n : ℕ∞) : ↑(m + n) = (m + n : ℝ≥0∞) :=
   map_add toENNRealRingHom m n
-#align enat.coe_ennreal_add ENat.coe_ennreal_add
+#align enat.coe_ennreal_add ENat.toENNReal_add
 
 @[simp]
-theorem coe_ennreal_one : ((1 : ℕ∞) : ℝ≥0∞) = 1 :=
+theorem toENNReal_one : ((1 : ℕ∞) : ℝ≥0∞) = 1 :=
   map_one toENNRealRingHom
-#align enat.coe_ennreal_one ENat.coe_ennreal_one
+#align enat.coe_ennreal_one ENat.toENNReal_one
 
 #noalign enat.coe_ennreal_bit0
 #noalign enat.coe_ennreal_bit1
 
 @[simp]
-theorem coe_ennreal_mul (m n : ℕ∞) : ↑(m * n) = (m * n : ℝ≥0∞) :=
+theorem toENNReal_mul (m n : ℕ∞) : ↑(m * n) = (m * n : ℝ≥0∞) :=
   map_mul toENNRealRingHom m n
-#align enat.coe_ennreal_mul ENat.coe_ennreal_mul
+#align enat.coe_ennreal_mul ENat.toENNReal_mul
 
 @[simp]
-theorem coe_ennreal_min (m n : ℕ∞) : ↑(min m n) = (min m n : ℝ≥0∞) :=
-  coe_ennreal_mono.map_min
-#align enat.coe_ennreal_min ENat.coe_ennreal_min
+theorem toENNReal_min (m n : ℕ∞) : ↑(min m n) = (min m n : ℝ≥0∞) :=
+  toENNReal_mono.map_min
+#align enat.coe_ennreal_min ENat.toENNReal_min
 
 @[simp]
-theorem coe_ennreal_max (m n : ℕ∞) : ↑(max m n) = (max m n : ℝ≥0∞) :=
-  coe_ennreal_mono.map_max
-#align enat.coe_ennreal_max ENat.coe_ennreal_max
+theorem toENNReal_max (m n : ℕ∞) : ↑(max m n) = (max m n : ℝ≥0∞) :=
+  toENNReal_mono.map_max
+#align enat.coe_ennreal_max ENat.toENNReal_max
 
 @[simp]
-theorem coe_ennreal_sub (m n : ℕ∞) : ↑(m - n) = (m - n : ℝ≥0∞) :=
+theorem toENNReal_sub (m n : ℕ∞) : ↑(m - n) = (m - n : ℝ≥0∞) :=
   WithTop.map_sub Nat.cast_tsub Nat.cast_zero m n
-#align enat.coe_ennreal_sub ENat.coe_ennreal_sub
+#align enat.coe_ennreal_sub ENat.toENNReal_sub
 
 end ENat
 
