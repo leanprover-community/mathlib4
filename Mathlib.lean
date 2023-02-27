19b42ddab57f84032cb4cfeecb1cefa30db6a6e6
Lukas Miaskiwskyi <lukas.mias@gmail.com>
2023-02-27 13:48:25 +0000

    feat: port Algebra.Order.Hom.Ring (#1482)
    Worked around https://github.com/leanprover-community/mathlib4/issues/2505, but finally everything works.

--- Mathlib.lean
+++ Mathlib.lean
@@ -141,6 +141,7 @@ import Mathlib.Algebra.Order.Group.Units
 import Mathlib.Algebra.Order.Group.WithTop
 import Mathlib.Algebra.Order.Hom.Basic
 import Mathlib.Algebra.Order.Hom.Monoid
+import Mathlib.Algebra.Order.Hom.Ring
 import Mathlib.Algebra.Order.Invertible
 import Mathlib.Algebra.Order.Kleene
 import Mathlib.Algebra.Order.LatticeGroup
