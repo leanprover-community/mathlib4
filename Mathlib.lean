9777fdce84799c6b91eb1f4f2c0a131729002260
Lukas Miaskiwskyi <lukas.mias@gmail.com>
2023-02-27 13:07:01 +0000

    feat: port LinearAlgebra.Ray (#2468)
    Co-authored-by: Arien Malec <arien.malec@gmail.com>
    Co-authored-by: ChrisHughes24 <chrishughes24@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -876,6 +876,7 @@ import Mathlib.LinearAlgebra.Pi
 import Mathlib.LinearAlgebra.Prod
 import Mathlib.LinearAlgebra.Projection
 import Mathlib.LinearAlgebra.Quotient
+import Mathlib.LinearAlgebra.Ray
 import Mathlib.LinearAlgebra.Span
 import Mathlib.Logic.Basic
 import Mathlib.Logic.Denumerable
