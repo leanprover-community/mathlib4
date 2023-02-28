8dfdc5b36cf0845bcea1be8883713b17dcaa54c1
Eric Wieser <wieser.eric@gmail.com>
2023-02-28 11:21:54 +0000

    chore: update SHA
    This golf was already ported, see
    https://github.com/leanprover-community/mathlib4/blob/cd44151818ae76d9c83d765d40397adee1e9ec02/Mathlib/Data/Multiset/Basic.lean#L2445-L2447.

--- Mathlib/Data/Multiset/Basic.lean
+++ Mathlib/Data/Multiset/Basic.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro
 
 ! This file was ported from Lean 3 source module data.multiset.basic
-! leanprover-community/mathlib commit ccad6d5093bd2f5c6ca621fc74674cce51355af6
+! leanprover-community/mathlib commit 7c523cb78f4153682c2929e3006c863bfef463d0
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
