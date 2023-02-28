fdfb90aa706c177622b3007234ee19bb0ea5e4da
Matthew Ballard <matt@mrb.email>
2023-02-28 07:13:40 -0500

    fix import file again

--- Mathlib.lean
+++ Mathlib.lean
@@ -277,6 +277,7 @@ import Mathlib.CategoryTheory.Limits.Shapes.BinaryProducts
 import Mathlib.CategoryTheory.Limits.Shapes.Pullbacks
 import Mathlib.CategoryTheory.Limits.Shapes.StrongEpi
 import Mathlib.CategoryTheory.Limits.Shapes.Terminal
+import Mathlib.CategoryTheory.Limits.Shapes.WidePullbacks
 import Mathlib.CategoryTheory.Monoidal.Category
 import Mathlib.CategoryTheory.Monoidal.Functor
 import Mathlib.CategoryTheory.NatIso
