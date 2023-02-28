cd44151818ae76d9c83d765d40397adee1e9ec02
casavaca <96765450+casavaca@users.noreply.github.com>
2023-02-28 09:43:26 +0000

    feat: Port Algebra.Module.Submodule.Bilinear (#2531)

--- Mathlib.lean
+++ Mathlib.lean
@@ -111,6 +111,7 @@ import Mathlib.Algebra.Module.Pi
 import Mathlib.Algebra.Module.PointwisePi
 import Mathlib.Algebra.Module.Prod
 import Mathlib.Algebra.Module.Submodule.Basic
+import Mathlib.Algebra.Module.Submodule.Bilinear
 import Mathlib.Algebra.Module.Submodule.Lattice
 import Mathlib.Algebra.Module.Submodule.Pointwise
 import Mathlib.Algebra.Module.ULift
