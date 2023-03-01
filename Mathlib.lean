9312775318f9f4c5bc3f5e66f6623be085fd8d2b
Matthew Ballard <matt@mrb.email>
2023-03-01 14:23:01 -0500

    update import; clear merge remnants

--- Mathlib.lean
+++ Mathlib.lean
@@ -282,6 +282,7 @@ import Mathlib.CategoryTheory.LiftingProperties.Adjunction
 import Mathlib.CategoryTheory.LiftingProperties.Basic
 import Mathlib.CategoryTheory.Limits.Cones
 import Mathlib.CategoryTheory.Limits.Filtered
+import Mathlib.CategoryTheory.Limits.FunctorCategory
 import Mathlib.CategoryTheory.Limits.HasLimits
 import Mathlib.CategoryTheory.Limits.IsLimit
 import Mathlib.CategoryTheory.Limits.Preserves.Basic
