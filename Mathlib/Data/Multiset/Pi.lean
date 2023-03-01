358d77296eb532cb3873b93d8cc7f17f5fb8979b
Eric Wieser <wieser.eric@gmail.com>
2023-03-01 14:07:26 +0000

    shas

--- Mathlib/Data/Multiset/Pi.lean
+++ Mathlib/Data/Multiset/Pi.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl
 
 ! This file was ported from Lean 3 source module data.multiset.pi
-! leanprover-community/mathlib commit 9003f28797c0664a49e4179487267c494477d853
+! leanprover-community/mathlib commit 4c586d291f189eecb9d00581aeb3dd998ac34442
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
