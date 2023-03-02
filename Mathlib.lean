04511758fad40fda066926d3d667341aca81b54d
Matthew Robert Ballard <matt@mrb.email>
2023-03-02 07:55:37 +0000

    feat: port CategoryTheory.Limits.Shapes.Terminal (#2459)
    Co-authored-by: Matthew Robert Ballard <100034030+mattrobball@users.noreply.github.com>
    Co-authored-by: Johan Commelin <johan@commelin.net>

--- Mathlib.lean
+++ Mathlib.lean
@@ -289,6 +289,7 @@ import Mathlib.CategoryTheory.Limits.IsLimit
 import Mathlib.CategoryTheory.Limits.Preserves.Basic
 import Mathlib.CategoryTheory.Limits.Preserves.Limits
 import Mathlib.CategoryTheory.Limits.Shapes.StrongEpi
+import Mathlib.CategoryTheory.Limits.Shapes.Terminal
 import Mathlib.CategoryTheory.Limits.Shapes.WidePullbacks
 import Mathlib.CategoryTheory.Monoidal.Category
 import Mathlib.CategoryTheory.Monoidal.Functor
