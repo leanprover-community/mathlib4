a6e0a52467d52c4da97889a366288a8e9d129c0e
negiizhao <egresf@gmail.com>
2023-03-02 21:02:48 +0800

    SHAs

--- Mathlib/Data/Finsupp/Indicator.lean
+++ Mathlib/Data/Finsupp/Indicator.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yaël Dillies
 
 ! This file was ported from Lean 3 source module data.finsupp.indicator
-! leanprover-community/mathlib commit f7fc89d5d5ff1db2d1242c7bb0e9062ce47ef47c
+! leanprover-community/mathlib commit 842328d9df7e96fd90fc424e115679c15fb23a71
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
