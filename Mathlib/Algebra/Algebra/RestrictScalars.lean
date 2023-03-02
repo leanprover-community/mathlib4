944c5e9aa4d51c8fd87a8f1aa95aaf489c1013eb
Oliver Nash <github@olivernash.org>
2023-03-02 21:22:56 +0000

    Fix comments

--- Mathlib/Algebra/Algebra/RestrictScalars.lean
+++ Mathlib/Algebra/Algebra/RestrictScalars.lean
@@ -12,22 +12,22 @@ import Mathlib.Algebra.Algebra.Tower
 
 /-!
 
-# The `restrict_scalars` type alias
+# The `RestrictScalars` type alias
 
-See the documentation attached to the `restrict_scalars` definition for advice on how and when to
-use this type alias. As described there, it is often a better choice to use the `is_scalar_tower`
+See the documentation attached to the `RestrictScalars` definition for advice on how and when to
+use this type alias. As described there, it is often a better choice to use the `IsScalarTower`
 typeclass instead.
 
 ## Main definitions
 
-* `restrict_scalars R S M`: the `S`-module `M` viewed as an `R` module when `S` is an `R`-algebra.
-  Note that by default we do *not* have a `module S (restrict_scalars R S M)` instance
+* `RestrictScalars R S M`: the `S`-module `M` viewed as an `R` module when `S` is an `R`-algebra.
+  Note that by default we do *not* have a `Module S (RestrictScalars R S M)` instance
   for the original action.
-  This is available as a def `restrict_scalars.module_orig` if really needed.
-* `restrict_scalars.add_equiv : restrict_scalars R S M ≃+ M`: the additive equivalence
+  This is available as a def `RestrictScalars.moduleOrig` if really needed.
+* `RestrictScalars.addEquiv : RestrictScalars R S M ≃+ M`: the additive equivalence
   between the restricted and original space (in fact, they are definitionally equal,
   but sometimes it is helpful to avoid using this fact, to keep instances from leaking).
-* `restrict_scalars.ring_equiv : restrict_scalars R S A ≃+* A`: the ring equivalence
+* `RestrictScalars.ringEquiv : RestrictScalars R S A ≃+* A`: the ring equivalence
    between the restricted and original space when the module is an algebra.
 
 ## See also
@@ -35,12 +35,12 @@ typeclass instead.
 There are many similarly-named definitions elsewhere which do not refer to this type alias. These
 refer to restricting the scalar type in a bundled type, such as from `A →ₗ[R] B` to `A →ₗ[S] B`:
 
-* `linear_map.restrict_scalars`
-* `linear_equiv.restrict_scalars`
-* `alg_hom.restrict_scalars`
-* `alg_equiv.restrict_scalars`
-* `submodule.restrict_scalars`
-* `subalgebra.restrict_scalars`
+* `LinearMap.RestrictScalars`
+* `LinearEquiv.RestrictScalars`
+* `AlgHom.RestrictScalars`
+* `AlgEquiv.RestrictScalars`
+* `Submodule.RestrictScalars`
+* `Subalgebra.RestrictScalars`
 -/
 
 
@@ -48,34 +48,34 @@ variable (R S M A : Type _)
 
 /-- If we put an `R`-algebra structure on a semiring `S`, we get a natural equivalence from the
 category of `S`-modules to the category of representations of the algebra `S` (over `R`). The type
-synonym `restrict_scalars` is essentially this equivalence.
+synonym `RestrictScalars` is essentially this equivalence.
 
 Warning: use this type synonym judiciously! Consider an example where we want to construct an
 `R`-linear map from `M` to `S`, given:
 ```lean
-variables (R S M : Type*)
-variables [comm_semiring R] [semiring S] [algebra R S] [add_comm_monoid M] [module S M]
+variable (R S M : Type _)
+variable [CommSemiring R] [Semiring S] [Algebra R S] [AddCommMonoid M] [Module S M]
 ```
-With the assumptions above we can't directly state our map as we have no `module R M` structure, but
-`restrict_scalars` permits it to be written as:
+With the assumptions above we can't directly state our map as we have no `Module R M` structure, but
+`RestrictScalars` permits it to be written as:
 ```lean
--- an `R`-module structure on `M` is provided by `restrict_scalars` which is compatible
-example : restrict_scalars R S M →ₗ[R] S := sorry
+-- an `R`-module structure on `M` is provided by `RestrictScalars` which is compatible
+example : RestrictScalars R S M →ₗ[R] S := sorry
 ```
 However, it is usually better just to add this extra structure as an argument:
 ```lean
 -- an `R`-module structure on `M` and proof of its compatibility is provided by the user
-example [module R M] [is_scalar_tower R S M] : M →ₗ[R] S := sorry
+example [Module R M] [IsScalarTower R S M] : M →ₗ[R] S := sorry
 ```
 The advantage of the second approach is that it defers the duty of providing the missing typeclasses
-`[module R M] [is_scalar_tower R S M]`. If some concrete `M` naturally carries these (as is often
-the case) then we have avoided `restrict_scalars` entirely. If not, we can pass
-`restrict_scalars R S M` later on instead of `M`.
+`[Module R M] [IsScalarTower R S M]`. If some concrete `M` naturally carries these (as is often
+the case) then we have avoided `RestrictScalars` entirely. If not, we can pass
+`RestrictScalars R S M` later on instead of `M`.
 
 Note that this means we almost always want to state definitions and lemmas in the language of
-`is_scalar_tower` rather than `restrict_scalars`.
+`IsScalarTower` rather than `RestrictScalars`.
 
-An example of when one might want to use `restrict_scalars` would be if one has a vector space
+An example of when one might want to use `RestrictScalars` would be if one has a vector space
 over a field of characteristic zero and wishes to make use of the `ℚ`-algebra structure. -/
 @[nolint unusedArguments]
 def RestrictScalars (_R _S M : Type _) : Type _ := M
@@ -93,7 +93,7 @@ section
 
 variable [Semiring S] [AddCommMonoid M]
 
-/-- We temporarily install an action of the original ring on `restrict_sclars R S M`. -/
+/-- We temporarily install an action of the original ring on `RestrictScalars R S M`. -/
 def RestrictScalars.moduleOrig [I : Module S M] : Module S (RestrictScalars R S M) := I
 #align restrict_scalars.module_orig RestrictScalars.moduleOrig
 
@@ -106,12 +106,12 @@ attribute [local instance] RestrictScalars.moduleOrig
 /-- When `M` is a module over a ring `S`, and `S` is an algebra over `R`, then `M` inherits a
 module structure over `R`.
 
-The preferred way of setting this up is `[module R M] [module S M] [is_scalar_tower R S M]`.
+The preferred way of setting this up is `[Module R M] [Module S M] [IsScalarTower R S M]`.
 -/
 instance [Module S M] : Module R (RestrictScalars R S M) :=
   Module.compHom M (algebraMap R S)
 
-/-- This instance is only relevant when `restrict_scalars.module_orig` is available as an instance.
+/-- This instance is only relevant when `RestrictScalars.moduleOrig` is available as an instance.
 -/
 instance [Module S M] : IsScalarTower R S (RestrictScalars R S M) :=
   ⟨fun r S M ↦ by
@@ -123,7 +123,7 @@ end
 /-- When `M` is a right-module over a ring `S`, and `S` is an algebra over `R`, then `M` inherits a
 right-module structure over `R`.
 The preferred way of setting this up is
-`[module Rᵐᵒᵖ M] [module Sᵐᵒᵖ M] [is_scalar_tower Rᵐᵒᵖ Sᵐᵒᵖ M]`.
+`[Module Rᵐᵒᵖ M] [Module Sᵐᵒᵖ M] [IsScalarTower Rᵐᵒᵖ Sᵐᵒᵖ M]`.
 -/
 instance RestrictScalars.opModule [Module Sᵐᵒᵖ M] : Module Rᵐᵒᵖ (RestrictScalars R S M) :=
   letI : Module Sᵐᵒᵖ (RestrictScalars R S M) := ‹Module Sᵐᵒᵖ M›
@@ -136,10 +136,10 @@ instance RestrictScalars.isCentralScalar [Module S M] [Module Sᵐᵒᵖ M] [IsC
 #align restrict_scalars.is_central_scalar RestrictScalars.isCentralScalar
 
 /-- The `R`-algebra homomorphism from the original coefficient algebra `S` to endomorphisms
-of `restrict_scalars R S M`.
+of `RestrictScalars R S M`.
 -/
 def RestrictScalars.lsmul [Module S M] : S →ₐ[R] Module.End R (RestrictScalars R S M) :=
-  letI-- We use `restrict_scalars.module_orig` in the implementation,
+  letI-- We use `RestrictScalars.moduleOrig` in the implementation,
   -- but not in the type.
    : Module S (RestrictScalars R S M) := RestrictScalars.moduleOrig R S M
   Algebra.lsmul R (RestrictScalars R S M)
@@ -149,7 +149,7 @@ end
 
 variable [AddCommMonoid M]
 
-/-- `restrict_scalars.add_equiv` is the additive equivalence with the original module. -/
+/-- `RestrictScalars.add_equiv` is the additive equivalence with the original module. -/
 def RestrictScalars.addEquiv : RestrictScalars R S M ≃+ M :=
   AddEquiv.refl M
 #align restrict_scalars.add_equiv RestrictScalars.addEquiv
@@ -201,7 +201,7 @@ instance [I : CommRing A] : CommRing (RestrictScalars R S A) := I
 
 variable [Semiring A]
 
-/-- Tautological ring isomorphism `restrict_scalars R S A ≃+* A`. -/
+/-- Tautological ring isomorphism `RestrictScalars R S A ≃+* A`. -/
 def RestrictScalars.ringEquiv : RestrictScalars R S A ≃+* A :=
   RingEquiv.refl _
 #align restrict_scalars.ring_equiv RestrictScalars.ringEquiv
