8b9e28288429639b9228897c080d6335ee51f710
Rémi Bottinelli <remi.bottinelli@bluewin.ch>
2023-03-02 13:22:06 +0100

    auto: fix comments and line

--- Mathlib/CategoryTheory/PathCategory.lean
+++ Mathlib/CategoryTheory/PathCategory.lean
@@ -67,7 +67,7 @@ noncomputable def lift {C} [Category C] (φ : V ⥤q C) : Paths V ⥤ C
     where
   obj := φ.obj
   map {X} {Y} f :=
-    @Quiver.Path.rec V _ X (fun Y f => φ.obj X ⟶ φ.obj Y) (𝟙 <| φ.obj X)
+    @Quiver.Path.rec V _ X (fun Y _f => φ.obj X ⟶ φ.obj Y) (𝟙 <| φ.obj X)
       (fun p f ihp => ihp ≫ φ.map f) Y f
   map_id X := by rfl
   map_comp f g := by
@@ -147,7 +147,7 @@ end Paths
 
 variable (W : Type u₂) [Quiver.{v₂ + 1} W]
 
--- A restatement of `prefunctor.map_path_comp` using `f ≫ g` instead of `f.comp g`.
+-- A restatement of `Prefunctor.mapPath_comp` using `f ≫ g` instead of `f.comp g`.
 @[simp]
 theorem Prefunctor.mapPath_comp' (F : V ⥤q W) {X Y Z : Paths V} (f : X ⟶ Y) (g : Y ⟶ Z) :
     F.mapPath (f ≫ g) = (F.mapPath f).comp (F.mapPath g) :=
@@ -170,7 +170,7 @@ but is expected to have type
 Case conversion may be inaccurate. Consider using '#align category_theory.compose_path CategoryTheory.composePathₓ'. -/
 /-- A path in a category can be composed to a single morphism. -/
 @[simp]
-def composePath {X : C} : ∀ {Y : C} (p : Path X Y), X ⟶ Y
+def composePath {X : C} : ∀ {Y : C} (_p : Path X Y), X ⟶ Y
   | _, .nil => 𝟙 X
   | _, .cons p e => composePath p ≫ e
 #align category_theory.compose_path CategoryTheory.composePath
@@ -209,13 +209,13 @@ def pathComposition : Paths C ⥤ C where
 #align category_theory.path_composition CategoryTheory.pathComposition
 
 -- TODO: This, and what follows, should be generalized to
--- the `hom_rel` for the kernel of any functor.
+-- the `HomRel` for the kernel of any functor.
 -- Indeed, this should be part of an equivalence between congruence relations on a category `C`
 -- and full, essentially surjective functors out of `C`.
 /-- The canonical relation on the path category of a category:
 two paths are related if they compose to the same morphism. -/
 @[simp]
-def pathsHomRel : HomRel (Paths C) := fun X Y p q =>
+def pathsHomRel : HomRel (Paths C) := fun _X _Y p q =>
   (pathComposition C).map p = (pathComposition C).map q
 #align category_theory.paths_hom_rel CategoryTheory.pathsHomRel
 
