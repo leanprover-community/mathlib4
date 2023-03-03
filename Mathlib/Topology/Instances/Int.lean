bb5a3d201789bdc9269db0bd83a06bec7ce23742
Yury G. Kudryashov <urkud@urkud.name>
2023-03-03 16:34:54 -0600

    Fix compile

--- Mathlib/Topology/Instances/Int.lean
+++ Mathlib/Topology/Instances/Int.lean
@@ -28,9 +28,10 @@ namespace Int
 instance : Dist ℤ :=
   ⟨fun x y => dist (x : ℝ) y⟩
 
-theorem dist_eq (x y : ℤ) : dist x y = |x - y| :=
-  rfl
+theorem dist_eq (x y : ℤ) : dist x y = |(x : ℝ) - y| := rfl
 #align int.dist_eq Int.dist_eq
+
+theorem dist_eq' (m n : ℤ) : dist m n = |m - n| := by rw [dist_eq]; norm_cast
 
 @[norm_cast, simp]
 theorem dist_cast_real (x y : ℤ) : dist (x : ℝ) y = dist x y :=
@@ -42,23 +43,20 @@ theorem pairwise_one_le_dist : Pairwise fun m n : ℤ => 1 ≤ dist m n := by
   rw [dist_eq]; norm_cast; rwa [← zero_add (1 : ℤ), Int.add_one_le_iff, abs_pos, sub_ne_zero]
 #align int.pairwise_one_le_dist Int.pairwise_one_le_dist
 
-theorem uniformEmbedding_coe_real : UniformEmbedding (coe : ℤ → ℝ) :=
+theorem uniformEmbedding_coe_real : UniformEmbedding ((↑) : ℤ → ℝ) :=
   uniformEmbedding_bot_of_pairwise_le_dist zero_lt_one pairwise_one_le_dist
 #align int.uniform_embedding_coe_real Int.uniformEmbedding_coe_real
 
-theorem closedEmbedding_coe_real : ClosedEmbedding (coe : ℤ → ℝ) :=
+theorem closedEmbedding_coe_real : ClosedEmbedding ((↑) : ℤ → ℝ) :=
   closedEmbedding_of_pairwise_le_dist zero_lt_one pairwise_one_le_dist
 #align int.closed_embedding_coe_real Int.closedEmbedding_coe_real
 
-instance : MetricSpace ℤ :=
-  Int.uniformEmbedding_coe_real.comapMetricSpace _
+instance : MetricSpace ℤ := Int.uniformEmbedding_coe_real.comapMetricSpace _
 
-theorem preimage_ball (x : ℤ) (r : ℝ) : coe ⁻¹' ball (x : ℝ) r = ball x r :=
-  rfl
+theorem preimage_ball (x : ℤ) (r : ℝ) : (↑) ⁻¹' ball (x : ℝ) r = ball x r := rfl
 #align int.preimage_ball Int.preimage_ball
 
-theorem preimage_closedBall (x : ℤ) (r : ℝ) : coe ⁻¹' closedBall (x : ℝ) r = closedBall x r :=
-  rfl
+theorem preimage_closedBall (x : ℤ) (r : ℝ) : (↑) ⁻¹' closedBall (x : ℝ) r = closedBall x r := rfl
 #align int.preimage_closed_ball Int.preimage_closedBall
 
 theorem ball_eq_Ioo (x : ℤ) (r : ℝ) : ball x r = Ioo ⌊↑x - r⌋ ⌈↑x + r⌉ := by
@@ -66,19 +64,18 @@ theorem ball_eq_Ioo (x : ℤ) (r : ℝ) : ball x r = Ioo ⌊↑x - r⌋ ⌈↑x 
 #align int.ball_eq_Ioo Int.ball_eq_Ioo
 
 theorem closedBall_eq_Icc (x : ℤ) (r : ℝ) : closedBall x r = Icc ⌈↑x - r⌉ ⌊↑x + r⌋ := by
-  rw [← preimage_closed_ball, Real.closedBall_eq_Icc, preimage_Icc]
+  rw [← preimage_closedBall, Real.closedBall_eq_Icc, preimage_Icc]
 #align int.closed_ball_eq_Icc Int.closedBall_eq_Icc
 
 instance : ProperSpace ℤ :=
-  ⟨by
-    intro x r
-    rw [closed_ball_eq_Icc]
-    exact (Set.finite_Icc _ _).IsCompact⟩
+  ⟨fun x r => by
+    rw [closedBall_eq_Icc]
+    exact (Set.finite_Icc _ _).isCompact⟩
 
 @[simp]
 theorem cocompact_eq : cocompact ℤ = atBot ⊔ atTop := by
-  simp only [← comap_dist_right_atTop_eq_cocompact (0 : ℤ), dist_eq, sub_zero, cast_zero, ←
-    cast_abs, ← @comap_comap _ _ _ _ abs, Int.comap_cast_atTop, comap_abs_at_top]
+  simp_rw [← comap_dist_right_atTop_eq_cocompact (0 : ℤ), dist_eq', sub_zero,
+    ← comap_abs_atTop, ← @Int.comap_cast_atTop ℝ, comap_comap]; rfl
 #align int.cocompact_eq Int.cocompact_eq
 
 @[simp]
