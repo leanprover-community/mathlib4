e01a798a00b4dec99bcae160765a2008824b0f8b
Komyyy <pol_tta@outlook.jp>
2023-03-04 15:05:05 +0900

    automated fixes
    Mathbin -> Mathlib
    
    fix certain import statements
    
    move "by" to end of line
    
    add import to Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -668,6 +668,7 @@ import Mathlib.Data.Pi.Algebra
 import Mathlib.Data.Pi.Interval
 import Mathlib.Data.Pi.Lex
 import Mathlib.Data.Polynomial.Basic
+import Mathlib.Data.Polynomial.Monomial
 import Mathlib.Data.Prod.Basic
 import Mathlib.Data.Prod.Lex
 import Mathlib.Data.Prod.PProd
