e0609c5ae4ef46fd1f2b6d3bdfc3b569c2867b6f
Komyyy <pol_tta@outlook.jp>
2023-03-04 15:19:49 +0900

    2 red errors left

--- Mathlib/Data/Polynomial/Monomial.lean
+++ Mathlib/Data/Polynomial/Monomial.lean
@@ -31,12 +31,14 @@ variable [Semiring R] {p q r : R[X]}
 
 theorem monomial_one_eq_iff [Nontrivial R] {i j : ℕ} :
     (monomial i 1 : R[X]) = monomial j 1 ↔ i = j := by
-  simp_rw [← of_finsupp_single]
-  exact add_monoid_algebra.of_injective.eq_iff
+  -- Porting note: `ofFinsupp.injEq` is required.
+  simp_rw [← ofFinsupp_single, ofFinsupp.injEq]
+  exact AddMonoidAlgebra.of_injective.eq_iff
 #align polynomial.monomial_one_eq_iff Polynomial.monomial_one_eq_iff
 
-instance [Nontrivial R] : Infinite R[X] :=
+instance infinite [Nontrivial R] : Infinite R[X] :=
   Infinite.of_injective (fun i => monomial i 1) fun m n h => by simpa [monomial_one_eq_iff] using h
+#align polynomial.infinite Polynomial.infinite
 
 theorem card_support_le_one_iff_monomial {f : R[X]} :
     Finset.card f.support ≤ 1 ↔ ∃ n a, f = monomial n a := by
@@ -58,34 +60,31 @@ theorem card_support_le_one_iff_monomial {f : R[X]} :
     exact support_monomial' _ _
 #align polynomial.card_support_le_one_iff_monomial Polynomial.card_support_le_one_iff_monomial
 
-theorem ringHom_ext {S} [Semiring S] {f g : R[X] →+* S} (h₁ : ∀ a, f (c a) = g (c a))
+theorem ringHom_ext {S} [Semiring S] {f g : R[X] →+* S} (h₁ : ∀ a, f (C a) = g (C a))
     (h₂ : f x = g x) : f = g := by
-  set f' := f.comp (to_finsupp_iso R).symm.toRingHom with hf'
-  set g' := g.comp (to_finsupp_iso R).symm.toRingHom with hg'
+  set f' := f.comp (toFinsuppIso R).symm.toRingHom with hf'
+  set g' := g.comp (toFinsuppIso R).symm.toRingHom with hg'
   have A : f' = g' := by
     ext
     · simp [h₁, RingEquiv.toRingHom_eq_coe]
     · simpa [RingEquiv.toRingHom_eq_coe] using h₂
-  have B : f = f'.comp (to_finsupp_iso R) :=
-    by
+  have B : f = f'.comp (toFinsuppIso R) := by
     rw [hf', RingHom.comp_assoc]
     ext x
     simp only [RingEquiv.toRingHom_eq_coe, RingEquiv.symm_apply_apply, Function.comp_apply,
       RingHom.coe_comp, RingEquiv.coe_toRingHom]
-  have C : g = g'.comp (to_finsupp_iso R) :=
-    by
+  have C' : g = g'.comp (toFinsuppIso R) := by
     rw [hg', RingHom.comp_assoc]
     ext x
     simp only [RingEquiv.toRingHom_eq_coe, RingEquiv.symm_apply_apply, Function.comp_apply,
       RingHom.coe_comp, RingEquiv.coe_toRingHom]
-  rw [B, C, A]
+  rw [B, C', A]
 #align polynomial.ring_hom_ext Polynomial.ringHom_ext
 
 @[ext]
-theorem ringHom_ext' {S} [Semiring S] {f g : R[X] →+* S} (h₁ : f.comp c = g.comp c)
+theorem ringHom_ext' {S} [Semiring S] {f g : R[X] →+* S} (h₁ : f.comp C = g.comp C)
     (h₂ : f x = g x) : f = g :=
   ringHom_ext (RingHom.congr_fun h₁) h₂
 #align polynomial.ring_hom_ext' Polynomial.ringHom_ext'
 
 end Polynomial
-
