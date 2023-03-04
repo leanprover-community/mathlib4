866da777ae1fe573a60e3123d24fbd94aba1db22
casavaca <96765450+casavaca@users.noreply.github.com>
2023-03-04 01:40:12 -0800

    Mostly rename

--- Mathlib/Data/Polynomial/Induction.lean
+++ Mathlib/Data/Polynomial/Induction.lean
@@ -15,9 +15,9 @@ import Mathlib.Data.Polynomial.Basic
 # Induction on polynomials
 
 This file contains lemmas dealing with different flavours of induction on polynomials.
-See also `data/polynomial/inductions.lean` (with an `s`!).
+See also `Data/Polynomial/Inductions.lean` (with an `s`!).
 
-The main result is `polynomial.induction_on`.
+The main result is `Polynomial.inductionOn`.
 -/
 
 
@@ -39,16 +39,15 @@ section Semiring
 variable [Semiring R] {p q r : R[X]}
 
 @[elab_as_elim]
-protected theorem induction_on {M : R[X] → Prop} (p : R[X]) (h_C : ∀ a, M (c a))
+protected theorem inductionOn {M : R[X] → Prop} (p : R[X]) (h_C : ∀ a, M (C a))
     (h_add : ∀ p q, M p → M q → M (p + q))
-    (h_monomial : ∀ (n : ℕ) (a : R), M (c a * x ^ n) → M (c a * x ^ (n + 1))) : M p := by
+    (h_monomial : ∀ (n : ℕ) (a : R), M (C a * X ^ n) → M (C a * X ^ (n + 1))) : M p := by
   have A : ∀ {n : ℕ} {a}, M (C a * X ^ n) := by
     intro n a
     induction' n with n ih
-    · simp only [pow_zero, mul_one, h_C]
+    · rw [pow_zero, mul_one]; exact h_C a
     · exact h_monomial _ _ ih
-  have B : ∀ s : Finset ℕ, M (s.Sum fun n : ℕ => C (p.coeff n) * X ^ n) :=
-    by
+  have B : ∀ s : Finset ℕ, M (s.sum fun n : ℕ => C (p.coeff n) * X ^ n) := by
     apply Finset.induction
     · convert h_C 0
       exact C_0.symm
@@ -56,8 +55,8 @@ protected theorem induction_on {M : R[X] → Prop} (p : R[X]) (h_C : ∀ a, M (c
       rw [sum_insert ns]
       exact h_add _ _ A ih
   rw [← sum_C_mul_X_pow_eq p, Polynomial.sum]
-  exact B _
-#align polynomial.induction_on Polynomial.induction_on
+  exact B (support p)
+#align polynomial.induction_on Polynomial.inductionOn
 
 /-- To prove something about polynomials,
 it suffices to show the condition is closed under taking sums,
@@ -66,10 +65,8 @@ and it holds for monomials.
 @[elab_as_elim]
 protected theorem induction_on' {M : R[X] → Prop} (p : R[X]) (h_add : ∀ p q, M p → M q → M (p + q))
     (h_monomial : ∀ (n : ℕ) (a : R), M (monomial n a)) : M p :=
-  Polynomial.induction_on p (h_monomial 0) h_add fun n a h =>
-    by
-    rw [C_mul_X_pow_eq_monomial]
-    exact h_monomial _ _
+  Polynomial.inductionOn p (h_monomial 0) h_add fun n a _h =>
+    by rw [C_mul_X_pow_eq_monomial]; exact h_monomial _ _
 #align polynomial.induction_on' Polynomial.induction_on'
 
 open Submodule Polynomial Set
@@ -78,31 +75,34 @@ variable {f : R[X]} {I : Ideal R[X]}
 
 /-- If the coefficients of a polynomial belong to an ideal, then that ideal contains
 the ideal spanned by the coefficients of the polynomial. -/
-theorem span_le_of_c_coeff_mem (cf : ∀ i : ℕ, c (f.coeff i) ∈ I) :
-    Ideal.span { g | ∃ i, g = c (f.coeff i) } ≤ I := by
+theorem span_le_of_c_coeff_mem (cf : ∀ i : ℕ, C (f.coeff i) ∈ I) :
+    Ideal.span { g | ∃ i, g = C (f.coeff i) } ≤ I := by
   simp (config := { singlePass := true }) only [@eq_comm _ _ (C _)]
-  exact (ideal.span_le.trans range_subset_iff).mpr cf
+  exact (Ideal.span_le.trans range_subset_iff).mpr cf
+set_option linter.uppercaseLean3 false in
 #align polynomial.span_le_of_C_coeff_mem Polynomial.span_le_of_c_coeff_mem
 
-theorem mem_span_c_coeff : f ∈ Ideal.span { g : R[X] | ∃ i : ℕ, g = c (coeff f i) } := by
+theorem mem_span_c_coeff : f ∈ Ideal.span { g : R[X] | ∃ i : ℕ, g = C (coeff f i) } := by
   let p := Ideal.span { g : R[X] | ∃ i : ℕ, g = C (coeff f i) }
   nth_rw 1 [(sum_C_mul_X_pow_eq f).symm]
-  refine' Submodule.sum_mem _ fun n hn => _
+  refine' Submodule.sum_mem _ fun n _hn => _
   dsimp
   have : C (coeff f n) ∈ p := by
     apply subset_span
-    simp
+    rw [mem_setOf_eq]
+    use n
   have : monomial n (1 : R) • C (coeff f n) ∈ p := p.smul_mem _ this
   convert this using 1
   simp only [monomial_mul_C, one_mul, smul_eq_mul]
   rw [← C_mul_X_pow_eq_monomial]
+set_option linter.uppercaseLean3 false in
 #align polynomial.mem_span_C_coeff Polynomial.mem_span_c_coeff
 
-theorem exists_c_coeff_not_mem : f ∉ I → ∃ i : ℕ, c (coeff f i) ∉ I :=
+theorem exists_c_coeff_not_mem : f ∉ I → ∃ i : ℕ, C (coeff f i) ∉ I :=
   Not.imp_symm fun cf => span_le_of_c_coeff_mem (not_exists_not.mp cf) mem_span_c_coeff
+set_option linter.uppercaseLean3 false in
 #align polynomial.exists_C_coeff_not_mem Polynomial.exists_c_coeff_not_mem
 
 end Semiring
 
 end Polynomial
-
