70daecbe38423a2c8cd9d2fe0ba6a4a94e8b2aec
casavaca <96765450+casavaca@users.noreply.github.com>
2023-03-04 08:59:03 +0000

    feat: Port Topology.MetricSpace.Infsep (#2613)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1295,6 +1295,7 @@ import Mathlib.Topology.Maps
 import Mathlib.Topology.MetricSpace.Basic
 import Mathlib.Topology.MetricSpace.EMetricParacompact
 import Mathlib.Topology.MetricSpace.EMetricSpace
+import Mathlib.Topology.MetricSpace.Infsep
 import Mathlib.Topology.NhdsSet
 import Mathlib.Topology.NoetherianSpace
 import Mathlib.Topology.OmegaCompletePartialOrder
