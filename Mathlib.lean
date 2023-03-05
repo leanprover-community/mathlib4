41b3bbf5525a1b915adf018f8d0a8bd4141ec738
Moritz Firsching <moritz.firsching@gmail.com>
2023-03-05 07:28:36 +0000

    feat: port Topology.MetricSpace.ShrinkingLemma (#2629)
    Co-authored-by: Moritz Firsching <firsching@google.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1302,6 +1302,7 @@ import Mathlib.Topology.MetricSpace.EMetricParacompact
 import Mathlib.Topology.MetricSpace.EMetricSpace
 import Mathlib.Topology.MetricSpace.Infsep
 import Mathlib.Topology.MetricSpace.MetricSeparated
+import Mathlib.Topology.MetricSpace.ShrinkingLemma
 import Mathlib.Topology.NhdsSet
 import Mathlib.Topology.NoetherianSpace
 import Mathlib.Topology.OmegaCompletePartialOrder
