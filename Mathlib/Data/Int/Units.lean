bdacb529a186b41bd73679668620f64ebb7398b7
Thomas Browning <tb65536@uw.edu>
2023-03-05 13:34:04 -0800

    update SHA

--- Mathlib/Data/Int/Units.lean
+++ Mathlib/Data/Int/Units.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jeremy Avigad
 
 ! This file was ported from Lean 3 source module data.int.units
-! leanprover-community/mathlib commit 70d50ecfd4900dd6d328da39ab7ebd516abe4025
+! leanprover-community/mathlib commit 641b6a82006416ec431b2987b354af9311fed4f2
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
