2541faa637ee748b7868630544a5d1bdea563bfd
Jon Eugster <eugster.jon@gmail.com>
2023-03-05 23:52:38 +0100

    undo

--- Mathlib/Algebra/GroupPower/Lemmas.lean
+++ Mathlib/Algebra/GroupPower/Lemmas.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jeremy Avigad, Robert Y. Lewis
 
 ! This file was ported from Lean 3 source module algebra.group_power.lemmas
-! leanprover-community/mathlib commit 02c4026cbe3cd2122eb8ff196c80f24441037002
+! leanprover-community/mathlib commit f694c7dead66f5d4c80f446c796a5aad14707f0e
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
