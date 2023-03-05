2674d0e61cee876d485a3b8f8b60544617a45ecc
Jon Eugster <eugster.jon@gmail.com>
2023-03-05 23:50:49 +0100

    wrong SHA updated

--- Mathlib/Data/List/Lemmas.lean
+++ Mathlib/Data/List/Lemmas.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yakov Pechersky, Yury Kudryashov
 
 ! This file was ported from Lean 3 source module data.list.lemmas
-! leanprover-community/mathlib commit 6d0adfa76594f304b4650d098273d4366edeb61b
+! leanprover-community/mathlib commit f694c7dead66f5d4c80f446c796a5aad14707f0e
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
