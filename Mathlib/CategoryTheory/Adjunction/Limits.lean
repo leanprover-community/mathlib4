91df6cf2ccf982e85a224734f8ed42d49e055d6a
Joël Riou <joel.riou@universite-paris-saclay.fr>
2023-03-06 17:59:00 +0100

    automated fixes
    Mathbin -> Mathlib
    fix certain import statements
    move "by" to end of line
    add import to Mathlib.lean

--- Mathlib/CategoryTheory/Adjunction/Limits.lean
+++ Mathlib/CategoryTheory/Adjunction/Limits.lean
@@ -8,8 +8,8 @@ Authors: Reid Barton, Johan Commelin
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
-import Mathbin.CategoryTheory.Adjunction.Basic
-import Mathbin.CategoryTheory.Limits.Creates
+import Mathlib.CategoryTheory.Adjunction.Basic
+import Mathlib.CategoryTheory.Limits.Creates
 
 /-!
 # Adjunctions and limits
