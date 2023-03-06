934223d31a2dc6c8b38cbb88ea735382ffc90915
Yury G. Kudryashov <urkud@urkud.name>
2023-03-06 08:57:40 +0000

    feat: port Topology.Algebra.InfiniteSum.Order (#2644)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1246,6 +1246,7 @@ import Mathlib.Topology.Algebra.Group.Basic
 import Mathlib.Topology.Algebra.Group.Compact
 import Mathlib.Topology.Algebra.GroupWithZero
 import Mathlib.Topology.Algebra.InfiniteSum.Basic
+import Mathlib.Topology.Algebra.InfiniteSum.Order
 import Mathlib.Topology.Algebra.InfiniteSum.Ring
 import Mathlib.Topology.Algebra.Monoid
 import Mathlib.Topology.Algebra.MulAction
