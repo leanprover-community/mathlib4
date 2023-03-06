aeba075bcb830fe720f270b54514fc76ee012c5c
Yury G. Kudryashov <urkud@urkud.name>
2023-03-06 08:57:38 +0000

    feat: port Topology.MetricSpace.Lipschitz (#2634)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1310,6 +1310,7 @@ import Mathlib.Topology.MetricSpace.EMetricParacompact
 import Mathlib.Topology.MetricSpace.EMetricSpace
 import Mathlib.Topology.MetricSpace.Equicontinuity
 import Mathlib.Topology.MetricSpace.Infsep
+import Mathlib.Topology.MetricSpace.Lipschitz
 import Mathlib.Topology.MetricSpace.MetricSeparated
 import Mathlib.Topology.MetricSpace.ShrinkingLemma
 import Mathlib.Topology.NhdsSet
