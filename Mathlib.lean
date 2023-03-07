9222c02df753ee756d44c680ca1da8c0b1740373
Matthew Robert Ballard <matt@mrb.email>
2023-03-07 06:55:37 +0000

    feat: port CategoryTheory.Preserves.Shapes.Equalizers (#2687)

--- Mathlib.lean
+++ Mathlib.lean
@@ -299,6 +299,7 @@ import Mathlib.CategoryTheory.Limits.Preserves.Basic
 import Mathlib.CategoryTheory.Limits.Preserves.Finite
 import Mathlib.CategoryTheory.Limits.Preserves.Limits
 import Mathlib.CategoryTheory.Limits.Preserves.Shapes.BinaryProducts
+import Mathlib.CategoryTheory.Limits.Preserves.Shapes.Equalizers
 import Mathlib.CategoryTheory.Limits.Preserves.Shapes.Terminal
 import Mathlib.CategoryTheory.Limits.Shapes.BinaryProducts
 import Mathlib.CategoryTheory.Limits.Shapes.Equalizers
