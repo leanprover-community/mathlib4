c7635ebb876696fb646b0233a81e1918ddcb978d
Joël Riou <joel.riou@universite-paris-saclay.fr>
2023-03-07 16:30:51 +0100

    it compiles

--- Mathlib/CategoryTheory/Limits/ExactFunctor.lean
+++ Mathlib/CategoryTheory/Limits/ExactFunctor.lean
@@ -33,59 +33,106 @@ section
 
 variable (C) (D)
 
+-- porting note: removed @[nolint has_nonempty_instance]
 /-- Bundled left-exact functors. -/
-@[nolint has_nonempty_instance]
 def LeftExactFunctor :=
-  FullSubcategory fun F : C ⥤ D => Nonempty (PreservesFiniteLimits F)deriving Category
+  FullSubcategory fun F : C ⥤ D => Nonempty (PreservesFiniteLimits F)
+set_option linter.uppercaseLean3 false in
 #align category_theory.LeftExactFunctor CategoryTheory.LeftExactFunctor
 
--- mathport name: «expr ⥤ₗ »
+instance : Category (LeftExactFunctor C D) :=
+  FullSubcategory.category _
+
+/-- `C ⥤ₗ D` denotes left exact functors `C ⥤ D` -/
 infixr:26 " ⥤ₗ " => LeftExactFunctor
 
 /-- A left exact functor is in particular a functor. -/
 def LeftExactFunctor.forget : (C ⥤ₗ D) ⥤ C ⥤ D :=
-  fullSubcategoryInclusion _ deriving Full, Faithful
+  fullSubcategoryInclusion _
+set_option linter.uppercaseLean3 false in
 #align category_theory.LeftExactFunctor.forget CategoryTheory.LeftExactFunctor.forget
 
+instance : Full (LeftExactFunctor.forget C D) :=
+  FullSubcategory.full _
+
+instance : Faithful (LeftExactFunctor.forget C D) :=
+  FullSubcategory.faithful _
+
+-- porting note: removed @[nolint has_nonempty_instance]
 /-- Bundled right-exact functors. -/
-@[nolint has_nonempty_instance]
 def RightExactFunctor :=
-  FullSubcategory fun F : C ⥤ D => Nonempty (PreservesFiniteColimits F)deriving Category
+  FullSubcategory fun F : C ⥤ D => Nonempty (PreservesFiniteColimits F)
+set_option linter.uppercaseLean3 false in
 #align category_theory.RightExactFunctor CategoryTheory.RightExactFunctor
 
--- mathport name: «expr ⥤ᵣ »
+instance : Category (RightExactFunctor C D) :=
+  FullSubcategory.category _
+
+/-- `C ⥤ᵣ D` denotes right exact functors `C ⥤ D` -/
 infixr:26 " ⥤ᵣ " => RightExactFunctor
 
 /-- A right exact functor is in particular a functor. -/
 def RightExactFunctor.forget : (C ⥤ᵣ D) ⥤ C ⥤ D :=
-  fullSubcategoryInclusion _ deriving Full, Faithful
+  fullSubcategoryInclusion _
+set_option linter.uppercaseLean3 false in
 #align category_theory.RightExactFunctor.forget CategoryTheory.RightExactFunctor.forget
 
+instance : Full (RightExactFunctor.forget C D) :=
+  FullSubcategory.full _
+
+instance : Faithful (RightExactFunctor.forget C D) :=
+  FullSubcategory.faithful _
+
+-- porting note: removed @[nolint has_nonempty_instance]
 /-- Bundled exact functors. -/
-@[nolint has_nonempty_instance]
 def ExactFunctor :=
   FullSubcategory fun F : C ⥤ D =>
-    Nonempty (PreservesFiniteLimits F) ∧ Nonempty (PreservesFiniteColimits F)deriving
-  Category
+    Nonempty (PreservesFiniteLimits F) ∧ Nonempty (PreservesFiniteColimits F)
+set_option linter.uppercaseLean3 false in
 #align category_theory.ExactFunctor CategoryTheory.ExactFunctor
 
+instance : Category (ExactFunctor C D) :=
+  FullSubcategory.category _
+
 -- mathport name: «expr ⥤ₑ »
+/-- `C ⥤ₑ D` denotes exact functors `C ⥤ D` -/
 infixr:26 " ⥤ₑ " => ExactFunctor
 
 /-- An exact functor is in particular a functor. -/
 def ExactFunctor.forget : (C ⥤ₑ D) ⥤ C ⥤ D :=
-  fullSubcategoryInclusion _ deriving Full, Faithful
+  fullSubcategoryInclusion _
+set_option linter.uppercaseLean3 false in
 #align category_theory.ExactFunctor.forget CategoryTheory.ExactFunctor.forget
+
+instance : Full (ExactFunctor.forget C D) :=
+  FullSubcategory.full _
+
+instance : Faithful (ExactFunctor.forget C D) :=
+  FullSubcategory.faithful _
 
 /-- Turn an exact functor into a left exact functor. -/
 def LeftExactFunctor.ofExact : (C ⥤ₑ D) ⥤ C ⥤ₗ D :=
-  FullSubcategory.map fun X => And.left deriving Full, Faithful
+  FullSubcategory.map fun _ => And.left
+set_option linter.uppercaseLean3 false in
 #align category_theory.LeftExactFunctor.of_exact CategoryTheory.LeftExactFunctor.ofExact
+
+instance : Full (LeftExactFunctor.ofExact C D) :=
+  FullSubcategory.full_map _
+
+instance : Faithful (LeftExactFunctor.ofExact C D) :=
+  FullSubcategory.faithful_map _
 
 /-- Turn an exact functor into a left exact functor. -/
 def RightExactFunctor.ofExact : (C ⥤ₑ D) ⥤ C ⥤ᵣ D :=
-  FullSubcategory.map fun X => And.right deriving Full, Faithful
+  FullSubcategory.map fun _ => And.right
+set_option linter.uppercaseLean3 false in
 #align category_theory.RightExactFunctor.of_exact CategoryTheory.RightExactFunctor.ofExact
+
+instance : Full (RightExactFunctor.ofExact C D) :=
+  FullSubcategory.full_map _
+
+instance : Faithful (RightExactFunctor.ofExact C D) :=
+  FullSubcategory.faithful_map _
 
 variable {C D}
 
@@ -93,104 +140,123 @@ variable {C D}
 theorem LeftExactFunctor.ofExact_obj (F : C ⥤ₑ D) :
     (LeftExactFunctor.ofExact C D).obj F = ⟨F.1, F.2.1⟩ :=
   rfl
+set_option linter.uppercaseLean3 false in
 #align category_theory.LeftExactFunctor.of_exact_obj CategoryTheory.LeftExactFunctor.ofExact_obj
 
 @[simp]
 theorem RightExactFunctor.ofExact_obj (F : C ⥤ₑ D) :
     (RightExactFunctor.ofExact C D).obj F = ⟨F.1, F.2.2⟩ :=
   rfl
+set_option linter.uppercaseLean3 false in
 #align category_theory.RightExactFunctor.of_exact_obj CategoryTheory.RightExactFunctor.ofExact_obj
 
 @[simp]
 theorem LeftExactFunctor.ofExact_map {F G : C ⥤ₑ D} (α : F ⟶ G) :
     (LeftExactFunctor.ofExact C D).map α = α :=
   rfl
+set_option linter.uppercaseLean3 false in
 #align category_theory.LeftExactFunctor.of_exact_map CategoryTheory.LeftExactFunctor.ofExact_map
 
 @[simp]
 theorem RightExactFunctor.ofExact_map {F G : C ⥤ₑ D} (α : F ⟶ G) :
     (RightExactFunctor.ofExact C D).map α = α :=
   rfl
+set_option linter.uppercaseLean3 false in
 #align category_theory.RightExactFunctor.of_exact_map CategoryTheory.RightExactFunctor.ofExact_map
 
 @[simp]
 theorem LeftExactFunctor.forget_obj (F : C ⥤ₗ D) : (LeftExactFunctor.forget C D).obj F = F.1 :=
   rfl
+set_option linter.uppercaseLean3 false in
 #align category_theory.LeftExactFunctor.forget_obj CategoryTheory.LeftExactFunctor.forget_obj
 
 @[simp]
 theorem RightExactFunctor.forget_obj (F : C ⥤ᵣ D) : (RightExactFunctor.forget C D).obj F = F.1 :=
   rfl
+set_option linter.uppercaseLean3 false in
 #align category_theory.RightExactFunctor.forget_obj CategoryTheory.RightExactFunctor.forget_obj
 
 @[simp]
 theorem ExactFunctor.forget_obj (F : C ⥤ₑ D) : (ExactFunctor.forget C D).obj F = F.1 :=
   rfl
+set_option linter.uppercaseLean3 false in
 #align category_theory.ExactFunctor.forget_obj CategoryTheory.ExactFunctor.forget_obj
 
 @[simp]
 theorem LeftExactFunctor.forget_map {F G : C ⥤ₗ D} (α : F ⟶ G) :
     (LeftExactFunctor.forget C D).map α = α :=
   rfl
+set_option linter.uppercaseLean3 false in
 #align category_theory.LeftExactFunctor.forget_map CategoryTheory.LeftExactFunctor.forget_map
 
 @[simp]
 theorem RightExactFunctor.forget_map {F G : C ⥤ᵣ D} (α : F ⟶ G) :
     (RightExactFunctor.forget C D).map α = α :=
   rfl
+set_option linter.uppercaseLean3 false in
 #align category_theory.RightExactFunctor.forget_map CategoryTheory.RightExactFunctor.forget_map
 
 @[simp]
 theorem ExactFunctor.forget_map {F G : C ⥤ₑ D} (α : F ⟶ G) : (ExactFunctor.forget C D).map α = α :=
   rfl
+set_option linter.uppercaseLean3 false in
 #align category_theory.ExactFunctor.forget_map CategoryTheory.ExactFunctor.forget_map
 
 /-- Turn a left exact functor into an object of the category `LeftExactFunctor C D`. -/
 def LeftExactFunctor.of (F : C ⥤ D) [PreservesFiniteLimits F] : C ⥤ₗ D :=
   ⟨F, ⟨inferInstance⟩⟩
+set_option linter.uppercaseLean3 false in
 #align category_theory.LeftExactFunctor.of CategoryTheory.LeftExactFunctor.of
 
 /-- Turn a right exact functor into an object of the category `RightExactFunctor C D`. -/
 def RightExactFunctor.of (F : C ⥤ D) [PreservesFiniteColimits F] : C ⥤ᵣ D :=
   ⟨F, ⟨inferInstance⟩⟩
+set_option linter.uppercaseLean3 false in
 #align category_theory.RightExactFunctor.of CategoryTheory.RightExactFunctor.of
 
 /-- Turn an exact functor into an object of the category `ExactFunctor C D`. -/
 def ExactFunctor.of (F : C ⥤ D) [PreservesFiniteLimits F] [PreservesFiniteColimits F] : C ⥤ₑ D :=
   ⟨F, ⟨⟨inferInstance⟩, ⟨inferInstance⟩⟩⟩
+set_option linter.uppercaseLean3 false in
 #align category_theory.ExactFunctor.of CategoryTheory.ExactFunctor.of
 
 @[simp]
 theorem LeftExactFunctor.of_fst (F : C ⥤ D) [PreservesFiniteLimits F] :
     (LeftExactFunctor.of F).obj = F :=
   rfl
+set_option linter.uppercaseLean3 false in
 #align category_theory.LeftExactFunctor.of_fst CategoryTheory.LeftExactFunctor.of_fst
 
 @[simp]
 theorem RightExactFunctor.of_fst (F : C ⥤ D) [PreservesFiniteColimits F] :
     (RightExactFunctor.of F).obj = F :=
   rfl
+set_option linter.uppercaseLean3 false in
 #align category_theory.RightExactFunctor.of_fst CategoryTheory.RightExactFunctor.of_fst
 
 @[simp]
 theorem ExactFunctor.of_fst (F : C ⥤ D) [PreservesFiniteLimits F] [PreservesFiniteColimits F] :
     (ExactFunctor.of F).obj = F :=
   rfl
+set_option linter.uppercaseLean3 false in
 #align category_theory.ExactFunctor.of_fst CategoryTheory.ExactFunctor.of_fst
 
 theorem LeftExactFunctor.forget_obj_of (F : C ⥤ D) [PreservesFiniteLimits F] :
     (LeftExactFunctor.forget C D).obj (LeftExactFunctor.of F) = F :=
   rfl
+set_option linter.uppercaseLean3 false in
 #align category_theory.LeftExactFunctor.forget_obj_of CategoryTheory.LeftExactFunctor.forget_obj_of
 
 theorem RightExactFunctor.forget_obj_of (F : C ⥤ D) [PreservesFiniteColimits F] :
     (RightExactFunctor.forget C D).obj (RightExactFunctor.of F) = F :=
   rfl
+set_option linter.uppercaseLean3 false in
 #align category_theory.RightExactFunctor.forget_obj_of CategoryTheory.RightExactFunctor.forget_obj_of
 
 theorem ExactFunctor.forget_obj_of (F : C ⥤ D) [PreservesFiniteLimits F]
     [PreservesFiniteColimits F] : (ExactFunctor.forget C D).obj (ExactFunctor.of F) = F :=
   rfl
+set_option linter.uppercaseLean3 false in
 #align category_theory.ExactFunctor.forget_obj_of CategoryTheory.ExactFunctor.forget_obj_of
 
 noncomputable instance (F : C ⥤ₗ D) : PreservesFiniteLimits F.obj :=
@@ -208,4 +274,3 @@ noncomputable instance (F : C ⥤ₑ D) : PreservesFiniteColimits F.obj :=
 end
 
 end CategoryTheory
-
