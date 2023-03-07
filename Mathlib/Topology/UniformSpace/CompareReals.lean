72b76e6db6d37096de3e86a07b0894b28b40e483
Arien Malec <arien.malec@gmail.com>
2023-03-07 12:20:25 -0800

    manual naming

--- Mathlib/Topology/UniformSpace/CompareReals.lean
+++ Mathlib/Topology/UniformSpace/CompareReals.lean
@@ -16,7 +16,7 @@ import Mathlib.Topology.UniformSpace.Completion
 /-!
 # Comparison of Cauchy reals and Bourbaki reals
 
-In `data.real.basic` real numbers are defined using the so called Cauchy construction (although
+In `Data.Real.Basic` real numbers are defined using the so called Cauchy construction (although
 it is due to Georg Cantor). More precisely, this construction applies to commutative rings equipped
 with an absolute value with values in a linear ordered field.
 
@@ -28,7 +28,7 @@ with different level of generality. Comparing with Dedekind cuts or quasi-morphi
 completely different nature.
 
 Note that `MetricSpace/cau_seq_filter` also relates the notions of Cauchy sequences in metric
-spaces and Cauchy filters in general uniform spaces, and `MetricSpace/completion` makes sure
+spaces and Cauchy filters in general uniform spaces, and `MetricSpace/Completion` makes sure
 the completion (as a uniform space) of a metric space is a metric space.
 
 Historical note: mathlib used to define real numbers in an intermediate way, using completion
@@ -40,7 +40,7 @@ TODO:
 
 ## Implementation notes
 
-The heavy work is done in `topology/UniformSpace/AbstractCompletion` which provides an abstract
+The heavy work is done in `Topology/UniformSpace/AbstractCompletion` which provides an abstract
 caracterization of completions of uniform spaces, and isomorphisms between them. The only work left
 here is to prove the uniform space structure coming from the absolute value on ℚ (with values in ℚ,
 not referring to ℝ) coincides with the one coming from the metric space structure (which of course
