d0620ef77f13b16dd8ba0b52f6ef2301e8caff2c
Pol_tta <pol_tta@outlook.jp>
2023-03-08 08:56:20 +0000

    feat: port Data.Polynomial.Coeff (#2624)

--- Mathlib.lean
+++ Mathlib.lean
@@ -706,6 +706,7 @@ import Mathlib.Data.Pi.Algebra
 import Mathlib.Data.Pi.Interval
 import Mathlib.Data.Pi.Lex
 import Mathlib.Data.Polynomial.Basic
+import Mathlib.Data.Polynomial.Coeff
 import Mathlib.Data.Polynomial.Induction
 import Mathlib.Data.Polynomial.Monomial
 import Mathlib.Data.Prod.Basic
