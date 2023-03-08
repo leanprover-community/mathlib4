a66a47497b542808e52e42aea3951bec417bae6b
Lukas Miaskiwskyi <lukas.mias@gmail.com>
2023-03-08 20:58:32 +0100

    fixed as far as possible

--- Mathlib/Data/Polynomial/CancelLeads.lean
+++ Mathlib/Data/Polynomial/CancelLeads.lean
@@ -9,7 +9,6 @@ Authors: Aaron Anderson
 ! if you have ported upstream changes.
 -/
 import Mathlib.Data.Polynomial.Degree.Definitions
-import Mathlib.Tactic.ComputeDegree
 import Mathlib.Data.Polynomial.Degree.Lemmas
 
 /-!
@@ -17,11 +16,11 @@ import Mathlib.Data.Polynomial.Degree.Lemmas
 
 ## Definition
 
-* `cancel_leads p q`: the polynomial formed by multiplying `p` and `q` by monomials so that they
+* `cancelLeads p q`: the polynomial formed by multiplying `p` and `q` by monomials so that they
   have the same leading term, and then subtracting.
 
 ## Main Results
-The degree of `cancel_leads` is less than that of the larger of the two polynomials being cancelled.
+The degree of `cancelLeads` is less than that of the larger of the two polynomials being cancelled.
 Thus it is useful for induction or minimal-degree arguments.
 -/
 
@@ -38,7 +37,7 @@ section Ring
 
 variable [Ring R] (p q : R[X])
 
-/-- `cancel_leads p q` is formed by multiplying `p` and `q` by monomials so that they
+/-- `cancelLeads p q` is formed by multiplying `p` and `q` by monomials so that they
   have the same leading term, and then subtracting. -/
 def cancelLeads : R[X] :=
   C p.leadingCoeff * X ^ (p.natDegree - q.natDegree) * q -
@@ -58,19 +57,19 @@ theorem natDegree_cancelLeads_lt_of_natDegree_le_natDegree_of_comm
     (p.cancelLeads q).natDegree < q.natDegree := by
   by_cases hp : p = 0
   · convert hq
-    simp [hp, cancel_leads]
-  rw [cancel_leads, sub_eq_add_neg, tsub_eq_zero_iff_le.mpr h, pow_zero, mul_one]
+    simp [hp, cancelLeads]
+  rw [cancelLeads, sub_eq_add_neg, tsub_eq_zero_iff_le.mpr h, pow_zero, mul_one]
   by_cases h0 :
-    C p.leading_coeff * q + -(C q.leading_coeff * X ^ (q.nat_degree - p.nat_degree) * p) = 0
-  · exact (le_of_eq (by simp only [h0, nat_degree_zero])).trans_lt hq
+    C p.leadingCoeff * q + -(C q.leadingCoeff * X ^ (q.natDegree - p.natDegree) * p) = 0
+  · exact (le_of_eq (by simp only [h0, natDegree_zero])).trans_lt hq
   apply lt_of_le_of_ne
   · compute_degree_le
     repeat' rwa [Nat.sub_add_cancel]
   · contrapose! h0
-    rw [← leading_coeff_eq_zero, leading_coeff, h0, mul_assoc, X_pow_mul, ← tsub_add_cancel_of_le h,
-      add_comm _ p.nat_degree]
+    rw [← leadingCoeff_eq_zero, leadingCoeff, h0, mul_assoc, X_pow_mul, ← tsub_add_cancel_of_le h,
+      add_comm _ p.natDegree]
     simp only [coeff_mul_X_pow, coeff_neg, coeff_C_mul, add_tsub_cancel_left, coeff_add]
-    rw [add_comm p.nat_degree, tsub_add_cancel_of_le h, ← leading_coeff, ← leading_coeff, comm,
+    rw [add_comm p.natDegree, tsub_add_cancel_of_le h, ← leadingCoeff, ← leadingCoeff, comm,
       add_right_neg]
 #align polynomial.nat_degree_cancel_leads_lt_of_nat_degree_le_nat_degree_of_comm Polynomial.natDegree_cancelLeads_lt_of_natDegree_le_natDegree_of_comm
 
@@ -91,5 +90,4 @@ theorem natDegree_cancelLeads_lt_of_natDegree_le_natDegree (h : p.natDegree ≤ 
 
 end CommRing
 
-end Polynomial
-
+end
