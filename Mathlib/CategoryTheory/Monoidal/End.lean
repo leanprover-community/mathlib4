ad123598190856e8994d74fdefc982e316a0012f
Moritz Firsching <firsching@google.com>
2023-03-09 21:47:09 +0100

    silence linter when in fact simp cannot solve this

--- Mathlib/CategoryTheory/Monoidal/End.lean
+++ Mathlib/CategoryTheory/Monoidal/End.lean
@@ -192,7 +192,7 @@ theorem left_unitality_app (n : M) (X : C) :
   simpa using this.symm
 #align category_theory.left_unitality_app CategoryTheory.left_unitality_app
 
-@[reassoc (attr := simp)]
+@[reassoc (attr := simp, nolint simpNF)]
 theorem obj_ε_app (n : M) (X : C) :
     (F.obj n).map (F.ε.app X) = (F.map (λ_ n).inv).app X ≫ (F.μIso (𝟙_ M) n).inv.app X := by
   refine' Eq.trans _ (Category.id_comp _)
@@ -202,8 +202,7 @@ theorem obj_ε_app (n : M) (X : C) :
   · simp
 #align category_theory.obj_ε_app CategoryTheory.obj_ε_app
 
--- porting note: removed simp attribute
-@[reassoc]
+@[reassoc (attr := simp, nolint simpNF)]
 theorem obj_ε_inv_app (n : M) (X : C) :
     (F.obj n).map (F.εIso.inv.app X) = (F.μ (𝟙_ M) n).app X ≫ (F.map (λ_ n).hom).app X := by
   rw [← cancel_mono ((F.obj n).map (F.ε.app X)), ← Functor.map_comp]
@@ -247,7 +246,7 @@ theorem associativity_app (m₁ m₂ m₃ : M) (X : C) :
   simpa using this
 #align category_theory.associativity_app CategoryTheory.associativity_app
 
-@[reassoc (attr := simp)]
+@[reassoc (attr := simp, nolint simpNF)]
 theorem obj_μ_app (m₁ m₂ m₃ : M) (X : C) :
     (F.obj m₃).map ((F.μ m₁ m₂).app X) =
       (F.μ m₂ m₃).app ((F.obj m₁).obj X) ≫
@@ -260,7 +259,7 @@ theorem obj_μ_app (m₁ m₂ m₃ : M) (X : C) :
   simp
 #align category_theory.obj_μ_app CategoryTheory.obj_μ_app
 
-@[reassoc (attr := simp)]
+@[reassoc (attr := simp, nolint simpNF)]
 theorem obj_μ_inv_app (m₁ m₂ m₃ : M) (X : C) :
     (F.obj m₃).map ((F.μIso m₁ m₂).inv.app X) =
       (F.μ (m₁ ⊗ m₂) m₃).app X ≫
