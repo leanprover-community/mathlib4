f55caef8ee75a8804a3a070904a17140dac9bef9
Parcly Taxel <reddeloostw@gmail.com>
2023-03-10 15:52:39 +0800

    automated fixes
    Mathbin -> Mathlib
    fix certain import statements
    move "by" to end of line
    add import to Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -243,6 +243,7 @@ import Mathlib.Algebra.Tropical.Lattice
 import Mathlib.Analysis.Normed.Group.Basic
 import Mathlib.Analysis.Normed.Group.InfiniteSum
 import Mathlib.Analysis.Normed.Group.Seminorm
+import Mathlib.Analysis.NormedSpace.IndicatorFunction
 import Mathlib.CategoryTheory.Abelian.Images
 import Mathlib.CategoryTheory.Abelian.NonPreadditive
 import Mathlib.CategoryTheory.Adjunction.Basic
--- Mathlib/Analysis/NormedSpace/IndicatorFunction.lean
+++ Mathlib/Analysis/NormedSpace/IndicatorFunction.lean
@@ -8,8 +8,8 @@ Authors: Zhouhang Zhou, Yury Kudryashov
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
-import Mathbin.Analysis.Normed.Group.Basic
-import Mathbin.Algebra.IndicatorFunction
+import Mathlib.Analysis.Normed.Group.Basic
+import Mathlib.Algebra.IndicatorFunction
 
 /-!
 # Indicator function and norm
@@ -35,8 +35,7 @@ theorem nnnorm_indicator_eq_indicator_nnnorm :
 #align nnnorm_indicator_eq_indicator_nnnorm nnnorm_indicator_eq_indicator_nnnorm
 
 theorem norm_indicator_le_of_subset (h : s ⊆ t) (f : α → E) (a : α) :
-    ‖indicator s f a‖ ≤ ‖indicator t f a‖ :=
-  by
+    ‖indicator s f a‖ ≤ ‖indicator t f a‖ := by
   simp only [norm_indicator_eq_indicator_norm]
   exact indicator_le_indicator_of_subset ‹_› (fun _ => norm_nonneg _) _
 #align norm_indicator_le_of_subset norm_indicator_le_of_subset
@@ -45,8 +44,7 @@ theorem indicator_norm_le_norm_self : indicator s (fun a => ‖f a‖) a ≤ ‖
   indicator_le_self' (fun _ _ => norm_nonneg _) a
 #align indicator_norm_le_norm_self indicator_norm_le_norm_self
 
-theorem norm_indicator_le_norm_self : ‖indicator s f a‖ ≤ ‖f a‖ :=
-  by
+theorem norm_indicator_le_norm_self : ‖indicator s f a‖ ≤ ‖f a‖ := by
   rw [norm_indicator_eq_indicator_norm]
   apply indicator_norm_le_norm_self
 #align norm_indicator_le_norm_self norm_indicator_le_norm_self
