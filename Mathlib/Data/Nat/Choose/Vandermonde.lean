71cd2833ca47a0d3bd52b38886c8d8204f27bfb1
Parcly Taxel <reddeloostw@gmail.com>
2023-03-10 13:44:28 +0800

    OK, how did I miss this?

--- Mathlib/Data/Nat/Choose/Vandermonde.lean
+++ Mathlib/Data/Nat/Choose/Vandermonde.lean
@@ -37,6 +37,6 @@ theorem Nat.add_choose_eq (m n k : ℕ) :
     (m + n).choose k = ((X + 1) ^ (m + n)).coeff k := by rw [coeff_X_add_one_pow, Nat.cast_id]
     _ = ((X + 1) ^ m * (X + 1) ^ n).coeff k := by rw [pow_add]
     _ = ∑ ij : ℕ × ℕ in antidiagonal k, m.choose ij.1 * n.choose ij.2 := by
-      simp [coeff_mul, Finset.sum_congr rfl]
-    -- simp only [coeff_X_add_one_pow, Nat.cast_id, eq_self_iff_true, imp_true_iff]
+      rw [coeff_mul, Finset.sum_congr rfl]
+      simp only [coeff_X_add_one_pow, Nat.cast_id, eq_self_iff_true, imp_true_iff]
 #align nat.add_choose_eq Nat.add_choose_eq
