9d736b5786fd4cce4630a6d850d2b0eb43ef2afb
Moritz Doll <moritz.doll@googlemail.com>
2023-03-10 14:06:07 +1100

    one fix

--- Mathlib/LinearAlgebra/AffineSpace/AffineEquiv.lean
+++ Mathlib/LinearAlgebra/AffineSpace/AffineEquiv.lean
@@ -123,9 +123,12 @@ theorem coe_linear (e : P₁ ≃ᵃ[k] P₂) : (e : P₁ →ᵃ[k] P₂).linear 
   rfl
 #align affine_equiv.coe_linear AffineEquiv.coe_linear
 
+#check congr_fun
+
 theorem toAffineMap_injective : Injective (toAffineMap : (P₁ ≃ᵃ[k] P₂) → P₁ →ᵃ[k] P₂) := by
   rintro ⟨e, el, h⟩ ⟨e', el', h'⟩ H
-  simp only [toAffineMap_mk, Equiv.coe_inj, LinearEquiv.toLinearMap_inj] at H
+  rw [toAffineMap_mk, toAffineMap_mk, AffineMap.mk.injEq, FunLike.coe_fn_eq,
+    LinearEquiv.toLinearMap_inj] at H
   congr
   exacts[H.1, H.2]
 #align affine_equiv.to_affine_map_injective AffineEquiv.toAffineMap_injective
