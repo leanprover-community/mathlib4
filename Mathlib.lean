955518b4b1ea58602a5140c79f6499c45563da1e
Yury G. Kudryashov <urkud@urkud.name>
2023-03-10 22:18:54 +0000

    feat: port Analysis.Subadditive (#2772)

--- Mathlib.lean
+++ Mathlib.lean
@@ -248,6 +248,7 @@ import Mathlib.Analysis.Normed.Group.Hom
 import Mathlib.Analysis.Normed.Group.InfiniteSum
 import Mathlib.Analysis.Normed.Group.Seminorm
 import Mathlib.Analysis.NormedSpace.IndicatorFunction
+import Mathlib.Analysis.Subadditive
 import Mathlib.CategoryTheory.Abelian.Images
 import Mathlib.CategoryTheory.Abelian.NonPreadditive
 import Mathlib.CategoryTheory.Adjunction.Basic
