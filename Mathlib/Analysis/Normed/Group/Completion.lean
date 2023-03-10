0c202b4bafe7a30cc1e6f29503348e70d0adfb86
Yury G. Kudryashov <urkud@urkud.name>
2023-03-10 00:06:45 -0600

    Fix compile

--- Mathlib/Analysis/Normed/Group/Completion.lean
+++ Mathlib/Analysis/Normed/Group/Completion.lean
@@ -31,25 +31,21 @@ namespace Completion
 
 variable (E : Type _)
 
-instance [UniformSpace E] [Norm E] : Norm (Completion E)
-    where norm := Completion.extension Norm.norm
+instance [UniformSpace E] [Norm E] : Norm (Completion E) where
+  norm := Completion.extension Norm.norm
 
 @[simp]
 theorem norm_coe {E} [SeminormedAddCommGroup E] (x : E) : ‖(x : Completion E)‖ = ‖x‖ :=
   Completion.extension_coe uniformContinuous_norm x
 #align uniform_space.completion.norm_coe UniformSpace.Completion.norm_coe
 
-instance [SeminormedAddCommGroup E] : NormedAddCommGroup (Completion E) :=
-  { Completion.addCommGroup, Completion.metricSpace with
-    dist_eq := by
-      intro x y
-      apply completion.induction_on₂ x y <;> clear x y
-      · refine' isClosed_eq (completion.uniform_continuous_extension₂ _).Continuous _
-        exact Continuous.comp completion.continuous_extension continuous_sub
-      · intro x y
-        rw [← completion.coe_sub, norm_coe, completion.dist_eq, dist_eq_norm] }
+instance [SeminormedAddCommGroup E] : NormedAddCommGroup (Completion E) where
+  dist_eq x y := by
+    induction x, y using Completion.induction_on₂
+    · refine' isClosed_eq (Completion.uniformContinuous_extension₂ _).continuous _
+      exact Continuous.comp Completion.continuous_extension continuous_sub
+    · rw [← Completion.coe_sub, norm_coe, Completion.dist_eq, dist_eq_norm]
 
 end Completion
 
 end UniformSpace
-
