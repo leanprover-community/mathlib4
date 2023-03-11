2fb506f601ee7b015ce74eaff079904131c29d0c
Jeremy Tan Jie Rui <e0191785@u.nus.edu>
2023-03-11 15:48:45 +0000

    feat: port Data.Nat.Factorial.Cast (#2801)
    Co-authored-by: Parcly Taxel <reddeloostw@gmail.com>
    Co-authored-by: Johan Commelin <johan@commelin.net>

--- Mathlib.lean
+++ Mathlib.lean
@@ -699,6 +699,7 @@ import Mathlib.Data.Nat.Dist
 import Mathlib.Data.Nat.EvenOddRec
 import Mathlib.Data.Nat.Factorial.Basic
 import Mathlib.Data.Nat.Factorial.BigOperators
+import Mathlib.Data.Nat.Factorial.Cast
 import Mathlib.Data.Nat.Factors
 import Mathlib.Data.Nat.Fib
 import Mathlib.Data.Nat.ForSqrt
