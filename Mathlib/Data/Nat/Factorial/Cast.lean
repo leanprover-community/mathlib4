8cef2d4bb91ae4ddfab5c34afbaaeaffd721bc25
Parcly Taxel <reddeloostw@gmail.com>
2023-03-11 13:07:36 +0800

    Make the file compile

--- Mathlib/Data/Nat/Factorial/Cast.lean
+++ Mathlib/Data/Nat/Factorial/Cast.lean
@@ -16,9 +16,9 @@ import Mathlib.RingTheory.Polynomial.Pochhammer
 This file allows calculating factorials (including ascending and descending ones) as elements of a
 semiring.
 
-This is particularly crucial for `nat.desc_factorial` as subtraction on `ℕ` does **not** correspond
+This is particularly crucial for `Nat.descFactorial` as subtraction on `ℕ` does **not** correspond
 to subtraction on a general semiring. For example, we can't rely on existing cast lemmas to prove
-`↑(a.desc_factorial 2) = ↑a * (↑a - 1)`. We must use the fact that, whenever `↑(a - 1)` is not equal
+`↑(a.descFactorial 2) = ↑a * (↑a - 1)`. We must use the fact that, whenever `↑(a - 1)` is not equal
 to `↑a - 1`, the other factor is `0` anyway.
 -/
 
@@ -33,23 +33,25 @@ section Semiring
 
 variable [Semiring S] (a b : ℕ)
 
-theorem cast_ascFactorial : (a.ascFactorial b : S) = (pochhammer S b).eval (a + 1) := by
+-- Porting note: a + 1 not automatically lifted to S
+theorem cast_ascFactorial : (a.ascFactorial b : S) = (pochhammer S b).eval (a + 1 : S) := by
   rw [← pochhammer_nat_eq_ascFactorial, pochhammer_eval_cast, Nat.cast_add, Nat.cast_one]
 #align nat.cast_asc_factorial Nat.cast_ascFactorial
 
-theorem cast_descFactorial : (a.descFactorial b : S) = (pochhammer S b).eval (a - (b - 1) : ℕ) := by
+-- Porting note: a - (b - 1) not automatically lifted to S
+theorem cast_descFactorial : (a.descFactorial b : S) = (pochhammer S b).eval (a - (b - 1) : S) := by
   rw [← pochhammer_eval_cast, pochhammer_nat_eq_descFactorial]
-  cases b
-  · simp_rw [desc_factorial_zero]
-  simp_rw [add_succ, succ_sub_one]
-  obtain h | h := le_total a b
-  · rw [desc_factorial_of_lt (lt_succ_of_le h), desc_factorial_of_lt (lt_succ_of_le _)]
-    rw [tsub_eq_zero_iff_le.mpr h, zero_add]
-  · rw [tsub_add_cancel_of_le h]
+  induction' b with b
+  · simp
+  · simp_rw [add_succ, succ_sub_one]
+    obtain h | h := le_total a b
+    · rw [descFactorial_of_lt (lt_succ_of_le h), descFactorial_of_lt (lt_succ_of_le _)]
+      rw [tsub_eq_zero_iff_le.mpr h, zero_add]
+    · rw [tsub_add_cancel_of_le h]
 #align nat.cast_desc_factorial Nat.cast_descFactorial
 
 theorem cast_factorial : (a ! : S) = (pochhammer S a).eval 1 := by
-  rw [← zero_asc_factorial, cast_asc_factorial, cast_zero, zero_add]
+  rw [← zero_ascFactorial, cast_ascFactorial, cast_zero, zero_add]
 #align nat.cast_factorial Nat.cast_factorial
 
 end Semiring
@@ -59,13 +61,12 @@ section Ring
 variable [Ring S] (a b : ℕ)
 
 /-- Convenience lemma. The `a - 1` is not using truncated subtraction, as opposed to the definition
-of `nat.desc_factorial` as a natural. -/
+of `Nat.descFactorial` as a natural. -/
 theorem cast_descFactorial_two : (a.descFactorial 2 : S) = a * (a - 1) := by
-  rw [cast_desc_factorial]
+  rw [cast_descFactorial]
   cases a
-  · rw [zero_tsub, cast_zero, pochhammer_ne_zero_eval_zero _ two_ne_zero, zero_mul]
-  ·
-    rw [succ_sub_succ, tsub_zero, cast_succ, add_sub_cancel, pochhammer_succ_right, pochhammer_one,
+  · simp
+  · rw [succ_sub_succ, tsub_zero, cast_succ, add_sub_cancel, pochhammer_succ_right, pochhammer_one,
       Polynomial.X_mul, Polynomial.eval_mul_X, Polynomial.eval_add, Polynomial.eval_X, cast_one,
       Polynomial.eval_one]
 #align nat.cast_desc_factorial_two Nat.cast_descFactorial_two
@@ -73,4 +74,3 @@ theorem cast_descFactorial_two : (a.descFactorial 2 : S) = a * (a - 1) := by
 end Ring
 
 end Nat
-
