c24d32934343563c781c7dda707a446ef7416d2d
Jireh Loreaux <loreaujy@gmail.com>
2023-03-12 21:48:30 +0000

    feat: port Algebra.Star.Subalgebra (#2819)
    Note: Lean was continually crashing on me as I was porting this file, I believe sure to autoparams.
    
    - [x] depends on: #2817

--- Mathlib.lean
+++ Mathlib.lean
@@ -237,6 +237,7 @@ import Mathlib.Algebra.Star.Pointwise
 import Mathlib.Algebra.Star.Prod
 import Mathlib.Algebra.Star.SelfAdjoint
 import Mathlib.Algebra.Star.StarAlgHom
+import Mathlib.Algebra.Star.Subalgebra
 import Mathlib.Algebra.Star.Unitary
 import Mathlib.Algebra.Support
 import Mathlib.Algebra.Tropical.Basic
