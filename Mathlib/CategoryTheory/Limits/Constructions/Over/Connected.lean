51284f52ec2a7d1da9104757a78799ab87131858
Parcly Taxel <reddeloostw@gmail.com>
2023-03-13 04:24:53 +0800

    Fix errors but one

--- Mathlib/CategoryTheory/Limits/Constructions/Over/Connected.lean
+++ Mathlib/CategoryTheory/Limits/Constructions/Over/Connected.lean
@@ -15,14 +15,14 @@ import Mathlib.CategoryTheory.IsConnected
 /-!
 # Connected limits in the over category
 
-Shows that the forgetful functor `over B ⥤ C` creates connected limits, in particular `over B` has
+Shows that the forgetful functor `Over B ⥤ C` creates connected limits, in particular `Over B` has
 any connected limit which `C` has.
 -/
 
 
 universe v u
 
--- morphism levels before object levels. See note [category_theory universes].
+-- morphism levels before object levels. See note [CategoryTheory universes].
 noncomputable section
 
 open CategoryTheory CategoryTheory.Limits
@@ -40,33 +40,31 @@ namespace CreatesConnected
 /-- (Impl) Given a diagram in the over category, produce a natural transformation from the
 diagram legs to the specific object.
 -/
-def natTransInOver {B : C} (F : J ⥤ Over B) : F ⋙ forget B ⟶ (CategoryTheory.Functor.const J).obj B
-    where app j := (F.obj j).Hom
+def natTransInOver {B : C} (F : J ⥤ Over B) :
+    F ⋙ forget B ⟶ (CategoryTheory.Functor.const J).obj B where
+  app j := (F.obj j).hom
 #align category_theory.over.creates_connected.nat_trans_in_over CategoryTheory.Over.CreatesConnected.natTransInOver
-
-attribute [local tidy] tactic.case_bash
 
 /-- (Impl) Given a cone in the base category, raise it to a cone in the over category. Note this is
 where the connected assumption is used.
 -/
 @[simps]
-def raiseCone [IsConnected J] {B : C} {F : J ⥤ Over B} (c : Cone (F ⋙ forget B)) : Cone F
-    where
-  pt := Over.mk (c.π.app (Classical.arbitrary J) ≫ (F.obj (Classical.arbitrary J)).Hom)
+def raiseCone [IsConnected J] {B : C} {F : J ⥤ Over B} (c : Cone (F ⋙ forget B)) :
+    Cone F where
+  pt := Over.mk (c.π.app (Classical.arbitrary J) ≫ (F.obj (Classical.arbitrary J)).hom)
   π :=
-    {
-      app := fun j =>
+    { app := fun j =>
         Over.homMk (c.π.app j) (nat_trans_from_is_connected (c.π ≫ natTransInOver F) j _) }
 #align category_theory.over.creates_connected.raise_cone CategoryTheory.Over.CreatesConnected.raiseCone
 
 theorem raised_cone_lowers_to_original [IsConnected J] {B : C} {F : J ⥤ Over B}
-    (c : Cone (F ⋙ forget B)) (t : IsLimit c) : (forget B).mapCone (raiseCone c) = c := by tidy
+    (c : Cone (F ⋙ forget B)) (_ : IsLimit c) :
+  (forget B).mapCone (raiseCone c) = c := by aesop_cat
 #align category_theory.over.creates_connected.raised_cone_lowers_to_original CategoryTheory.Over.CreatesConnected.raised_cone_lowers_to_original
 
 /-- (Impl) Show that the raised cone is a limit. -/
 def raisedConeIsLimit [IsConnected J] {B : C} {F : J ⥤ Over B} {c : Cone (F ⋙ forget B)}
-    (t : IsLimit c) : IsLimit (raiseCone c)
-    where
+    (t : IsLimit c) : IsLimit (raiseCone c) where
   lift s :=
     Over.homMk (t.lift ((forget B).mapCone s))
       (by
@@ -82,8 +80,9 @@ def raisedConeIsLimit [IsConnected J] {B : C} {F : J ⥤ Over B} {c : Cone (F �
 end CreatesConnected
 
 /-- The forgetful functor from the over category creates any connected limit. -/
-instance forgetCreatesConnectedLimits [IsConnected J] {B : C} : CreatesLimitsOfShape J (forget B)
-    where CreatesLimit K :=
+instance forgetCreatesConnectedLimits [IsConnected J] {B : C} :
+    CreatesLimitsOfShape J (forget B) where
+  CreatesLimit :=
     createsLimitOfReflectsIso fun c t =>
       { liftedCone := CreatesConnected.raiseCone c
         validLift := eqToIso (CreatesConnected.raised_cone_lowers_to_original c t)
@@ -92,8 +91,7 @@ instance forgetCreatesConnectedLimits [IsConnected J] {B : C} : CreatesLimitsOfS
 
 /-- The over category has any connected limit which the original category has. -/
 instance has_connected_limits {B : C} [IsConnected J] [HasLimitsOfShape J C] :
-    HasLimitsOfShape J (Over B) where HasLimit F := hasLimit_of_created F (forget B)
+    HasLimitsOfShape J (Over B) where has_limit F := hasLimit_of_created F (forget B)
 #align category_theory.over.has_connected_limits CategoryTheory.Over.has_connected_limits
 
 end CategoryTheory.Over
-
