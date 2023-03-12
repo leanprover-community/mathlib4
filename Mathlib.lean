a742eb57448003ab1d4b39e5beaef25c077a3f71
Jireh Loreaux <loreaujy@gmail.com>
2023-03-12 01:27:24 -0600

    include `RingTheory.Adjoin.Basic`

--- Mathlib.lean
+++ Mathlib.lean
@@ -754,7 +754,6 @@ import Mathlib.Data.Part
 import Mathlib.Data.Pi.Algebra
 import Mathlib.Data.Pi.Interval
 import Mathlib.Data.Pi.Lex
-import Mathlib.Data.Polynomial.AlgebraMap
 import Mathlib.Data.Polynomial.Basic
 import Mathlib.Data.Polynomial.Cardinal
 import Mathlib.Data.Polynomial.Coeff
