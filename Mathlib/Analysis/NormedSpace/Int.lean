560fef7d202557dc801647158a2fe57ab6b0a705
Parcly Taxel <reddeloostw@gmail.com>
2023-03-13 23:10:45 +0800

    Fix errors

--- Mathlib/Analysis/NormedSpace/Int.lean
+++ Mathlib/Analysis/NormedSpace/Int.lean
@@ -17,7 +17,7 @@ This file contains basic facts about the integers as normed ring.
 
 Recall that `‖n‖` denotes the norm of `n` as real number.
 This norm is always nonnegative, so we can bundle the norm together with this fact,
-to obtain a term of type `nnreal` (the nonnegative real numbers).
+to obtain a term of type `NNReal` (the nonnegative real numbers).
 The resulting nonnegative real number is denoted by `‖n‖₊`.
 -/
 
@@ -27,12 +27,12 @@ open BigOperators
 namespace Int
 
 theorem nnnorm_coe_units (e : ℤˣ) : ‖(e : ℤ)‖₊ = 1 := by
-  obtain rfl | rfl := Int.units_eq_one_or e <;>
+  obtain rfl | rfl := units_eq_one_or e <;>
     simp only [Units.coe_neg_one, Units.val_one, nnnorm_neg, nnnorm_one]
 #align int.nnnorm_coe_units Int.nnnorm_coe_units
 
 theorem norm_coe_units (e : ℤˣ) : ‖(e : ℤ)‖ = 1 := by
-  rw [← coe_nnnorm, Int.nnnorm_coe_units, NNReal.coe_one]
+  rw [← coe_nnnorm, nnnorm_coe_units, NNReal.coe_one]
 #align int.norm_coe_units Int.norm_coe_units
 
 @[simp]
@@ -42,14 +42,13 @@ theorem nnnorm_coe_nat (n : ℕ) : ‖(n : ℤ)‖₊ = n :=
 
 @[simp]
 theorem toNat_add_toNat_neg_eq_nnnorm (n : ℤ) : ↑n.toNat + ↑(-n).toNat = ‖n‖₊ := by
-  rw [← Nat.cast_add, to_nat_add_to_nat_neg_eq_nat_abs, NNReal.coe_natAbs]
+  rw [← Nat.cast_add, toNat_add_toNat_neg_eq_natAbs, NNReal.coe_natAbs]
 #align int.to_nat_add_to_nat_neg_eq_nnnorm Int.toNat_add_toNat_neg_eq_nnnorm
 
 @[simp]
 theorem toNat_add_toNat_neg_eq_norm (n : ℤ) : ↑n.toNat + ↑(-n).toNat = ‖n‖ := by
   simpa only [NNReal.coe_nat_cast, NNReal.coe_add] using
-    congr_arg (coe : _ → ℝ) (to_nat_add_to_nat_neg_eq_nnnorm n)
+    congrArg NNReal.toReal (toNat_add_toNat_neg_eq_nnnorm n)
 #align int.to_nat_add_to_nat_neg_eq_norm Int.toNat_add_toNat_neg_eq_norm
 
 end Int
-
