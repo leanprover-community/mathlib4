bc4a1ea6116bf0f0100e53f31cdfbc352cc906c8
Joël Riou <joel.riou@universite-paris-saclay.fr>
2023-03-13 13:03:26 +0000

    feat: port CategoryTheory.Linear.LinearFunctor (#2814)

--- Mathlib.lean
+++ Mathlib.lean
@@ -377,6 +377,7 @@ import Mathlib.CategoryTheory.Limits.Unit
 import Mathlib.CategoryTheory.Limits.Yoneda
 import Mathlib.CategoryTheory.Linear.Basic
 import Mathlib.CategoryTheory.Linear.FunctorCategory
+import Mathlib.CategoryTheory.Linear.LinearFunctor
 import Mathlib.CategoryTheory.Monoidal.Category
 import Mathlib.CategoryTheory.Monoidal.End
 import Mathlib.CategoryTheory.Monoidal.Functor
