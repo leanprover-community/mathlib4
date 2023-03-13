8824002765ea31f34a74449f2a60f8e27ad6b97c
casavaca <96765450+casavaca@users.noreply.github.com>
2023-03-13 01:57:42 -0700

    lint!

--- Mathlib/RingTheory/Polynomial/ScaleRoots.lean
+++ Mathlib/RingTheory/Polynomial/ScaleRoots.lean
@@ -78,7 +78,6 @@ theorem degree_scaleRoots (p : R[X]) {s : R} : degree (scaleRoots p s) = degree 
   haveI := Classical.propDecidable
   by_cases hp : p = 0
   · rw [hp, zero_scaleRoots]
-  have := scaleRoots_ne_zero hp s
   refine' le_antisymm (Finset.sup_mono (support_scaleRoots_le p s)) (degree_le_degree _)
   rw [coeff_scaleRoots_natDegree]
   intro h
