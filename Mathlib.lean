c88ae7e90d2f505a7e6c6d129a3a1660dddf3559
Yury G. Kudryashov <urkud@urkud.name>
2023-03-14 07:53:53 +0000

    feat: port Analysis.Normed.Field.InfiniteSum (#2860)

--- Mathlib.lean
+++ Mathlib.lean
@@ -245,6 +245,7 @@ import Mathlib.Algebra.Tropical.Basic
 import Mathlib.Algebra.Tropical.BigOperators
 import Mathlib.Algebra.Tropical.Lattice
 import Mathlib.Analysis.Normed.Field.Basic
+import Mathlib.Analysis.Normed.Field.InfiniteSum
 import Mathlib.Analysis.Normed.Field.UnitBall
 import Mathlib.Analysis.Normed.Group.BallSphere
 import Mathlib.Analysis.Normed.Group.Basic
