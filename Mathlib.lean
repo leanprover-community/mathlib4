15bde78345bf3bb45e1183730fc69d18fb5a1b7e
Jeremy Tan Jie Rui <e0191785@u.nus.edu>
2023-03-14 15:28:47 +0000

    feat: port Data.Polynomial.Taylor (#2850)
    Co-authored-by: Parcly Taxel <reddeloostw@gmail.com>
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>
    Co-authored-by: Jeremy Tan Jie Rui <reddeloostw@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -804,6 +804,7 @@ import Mathlib.Data.Polynomial.Lifts
 import Mathlib.Data.Polynomial.Monic
 import Mathlib.Data.Polynomial.Monomial
 import Mathlib.Data.Polynomial.Reverse
+import Mathlib.Data.Polynomial.Taylor
 import Mathlib.Data.Prod.Basic
 import Mathlib.Data.Prod.Lex
 import Mathlib.Data.Prod.PProd
