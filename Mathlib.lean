1d68cc90bc3ec101033621cbcc7c1b806db5fb7c
Jireh Loreaux <loreaujy@gmail.com>
2023-03-14 03:45:17 +0000

    feat: port Analysis.Normed.Order.Lattice (#2851)

--- Mathlib.lean
+++ Mathlib.lean
@@ -252,6 +252,7 @@ import Mathlib.Analysis.Normed.Group.Hom
 import Mathlib.Analysis.Normed.Group.HomCompletion
 import Mathlib.Analysis.Normed.Group.InfiniteSum
 import Mathlib.Analysis.Normed.Group.Seminorm
+import Mathlib.Analysis.Normed.Order.Lattice
 import Mathlib.Analysis.NormedSpace.IndicatorFunction
 import Mathlib.Analysis.NormedSpace.Int
 import Mathlib.Analysis.Subadditive
