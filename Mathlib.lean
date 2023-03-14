4af8a2fcfa11f4bee6c6113ac96fd4cecb9dc132
thorimur <68410468+thorimur@users.noreply.github.com>
2023-03-14 04:30:38 -0400

    chore: update Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -508,6 +508,7 @@ import Mathlib.Data.Analysis.Filter
 import Mathlib.Data.Analysis.Topology
 import Mathlib.Data.Array.Basic
 import Mathlib.Data.Array.Defs
+import Mathlib.Data.Array.MinMax
 import Mathlib.Data.BinaryHeap
 import Mathlib.Data.Bitvec.Core
 import Mathlib.Data.Bool.AllAny
