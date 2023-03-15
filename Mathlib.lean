4661306a3db1a4e1663da93c66fb12ff06c055ab
Joël Riou <joel.riou@universite-paris-saclay.fr>
2023-03-15 10:38:12 +0000

    feat: port RingTheory.FreeRing (#2885)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1240,6 +1240,7 @@ import Mathlib.RingTheory.Congruence
 import Mathlib.RingTheory.Coprime.Basic
 import Mathlib.RingTheory.Coprime.Lemmas
 import Mathlib.RingTheory.Fintype
+import Mathlib.RingTheory.FreeRing
 import Mathlib.RingTheory.Ideal.Basic
 import Mathlib.RingTheory.Ideal.Operations
 import Mathlib.RingTheory.Ideal.Quotient
