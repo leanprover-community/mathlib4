af4339f7a46983a515b76e9610bcca880fb9f1d0
Kyle Miller <kmill31415@gmail.com>
2023-03-15 19:58:08 +0100

    another test

--- test/ClearValue.lean
+++ test/ClearValue.lean
@@ -22,3 +22,11 @@ example : let x := 22; let y : Fin x := 0; y.1 < x := by
   fail_if_success clear_value x
   fail_if_success clear_value y
   exact y.2
+
+example : let x := 22; let y : Fin x := 0; y.1 < x := by
+  intro x y
+  fail_if_success clear_value x
+  clear_value x y
+  fail_if_success clear_value x
+  fail_if_success clear_value y
+  exact y.2
