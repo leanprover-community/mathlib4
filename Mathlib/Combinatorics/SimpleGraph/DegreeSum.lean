acf39a3fb5052ac1f140b9a36bb5a14ff7045753
casavaca <96765450+casavaca@users.noreply.github.com>
2023-03-14 21:03:18 -0700

    automated fixes
    Mathbin -> Mathlib
    fix certain import statements
    move "by" to end of line
    add import to Mathlib.lean

--- Mathlib/Combinatorics/SimpleGraph/DegreeSum.lean
+++ Mathlib/Combinatorics/SimpleGraph/DegreeSum.lean
@@ -8,10 +8,10 @@ Authors: Kyle Miller
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
-import Mathbin.Combinatorics.SimpleGraph.Basic
-import Mathbin.Algebra.BigOperators.Basic
-import Mathbin.Data.Nat.Parity
-import Mathbin.Data.Zmod.Parity
+import Mathlib.Combinatorics.SimpleGraph.Basic
+import Mathlib.Algebra.BigOperators.Basic
+import Mathlib.Data.Nat.Parity
+import Mathlib.Data.ZMod.Parity
 
 /-!
 # Degree-sum formula and handshaking lemma
@@ -56,8 +56,7 @@ section DegreeSum
 variable [Fintype V] [DecidableRel G.Adj]
 
 theorem dart_fst_fiber [DecidableEq V] (v : V) :
-    (univ.filterₓ fun d : G.Dart => d.fst = v) = univ.image (G.dartOfNeighborSet v) :=
-  by
+    (univ.filterₓ fun d : G.Dart => d.fst = v) = univ.image (G.dartOfNeighborSet v) := by
   ext d
   simp only [mem_image, true_and_iff, mem_filter, SetCoe.exists, mem_univ, exists_prop_of_true]
   constructor
@@ -73,8 +72,7 @@ theorem dart_fst_fiber_card_eq_degree [DecidableEq V] (v : V) :
     card_image_of_injective univ (G.dart_of_neighbor_set_injective v)
 #align simple_graph.dart_fst_fiber_card_eq_degree SimpleGraph.dart_fst_fiber_card_eq_degree
 
-theorem dart_card_eq_sum_degrees : Fintype.card G.Dart = ∑ v, G.degree v :=
-  by
+theorem dart_card_eq_sum_degrees : Fintype.card G.Dart = ∑ v, G.degree v := by
   haveI := Classical.decEq V
   simp only [← card_univ, ← dart_fst_fiber_card_eq_degree]
   exact card_eq_sum_card_fiberwise (by simp)
@@ -90,8 +88,7 @@ theorem Dart.edge_fiber (d : G.Dart) :
 variable (G)
 
 theorem dart_edge_fiber_card (e : Sym2 V) (h : e ∈ G.edgeSetEmbedding) :
-    (univ.filterₓ fun d : G.Dart => d.edge = e).card = 2 :=
-  by
+    (univ.filterₓ fun d : G.Dart => d.edge = e).card = 2 := by
   refine' Sym2.ind (fun v w h => _) e h
   let d : G.dart := ⟨(v, w), h⟩
   convert congr_arg card d.edge_fiber
@@ -100,8 +97,7 @@ theorem dart_edge_fiber_card (e : Sym2 V) (h : e ∈ G.edgeSetEmbedding) :
   exact d.symm_ne.symm
 #align simple_graph.dart_edge_fiber_card SimpleGraph.dart_edge_fiber_card
 
-theorem dart_card_eq_twice_card_edges : Fintype.card G.Dart = 2 * G.edgeFinset.card :=
-  by
+theorem dart_card_eq_twice_card_edges : Fintype.card G.Dart = 2 * G.edgeFinset.card := by
   rw [← card_univ]
   rw [@card_eq_sum_card_fiberwise _ _ _ dart.edge _ G.edge_finset fun d h =>
       by
@@ -141,8 +137,7 @@ theorem even_card_odd_degree_vertices [Fintype V] [DecidableRel G.Adj] :
 #align simple_graph.even_card_odd_degree_vertices SimpleGraph.even_card_odd_degree_vertices
 
 theorem odd_card_odd_degree_vertices_ne [Fintype V] [DecidableEq V] [DecidableRel G.Adj] (v : V)
-    (h : Odd (G.degree v)) : Odd (univ.filterₓ fun w => w ≠ v ∧ Odd (G.degree w)).card :=
-  by
+    (h : Odd (G.degree v)) : Odd (univ.filterₓ fun w => w ≠ v ∧ Odd (G.degree w)).card := by
   rcases G.even_card_odd_degree_vertices with ⟨k, hg⟩
   have hk : 0 < k :=
     by
@@ -167,8 +162,7 @@ theorem odd_card_odd_degree_vertices_ne [Fintype V] [DecidableEq V] [DecidableRe
 #align simple_graph.odd_card_odd_degree_vertices_ne SimpleGraph.odd_card_odd_degree_vertices_ne
 
 theorem exists_ne_odd_degree_of_exists_odd_degree [Fintype V] [DecidableRel G.Adj] (v : V)
-    (h : Odd (G.degree v)) : ∃ w : V, w ≠ v ∧ Odd (G.degree w) :=
-  by
+    (h : Odd (G.degree v)) : ∃ w : V, w ≠ v ∧ Odd (G.degree w) := by
   haveI := Classical.decEq V
   rcases G.odd_card_odd_degree_vertices_ne v h with ⟨k, hg⟩
   have hg' : (Filter (fun w : V => w ≠ v ∧ Odd (G.degree w)) univ).card > 0 :=
