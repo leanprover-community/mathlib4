e30e508c1fac7e3ad18ec672bc1d011446447fc1
casavaca <96765450+casavaca@users.noreply.github.com>
2023-03-14 22:52:03 -0700

    Comments

--- Mathlib/Combinatorics/SimpleGraph/DegreeSum.lean
+++ Mathlib/Combinatorics/SimpleGraph/DegreeSum.lean
@@ -22,11 +22,11 @@ a corollary, is that the number of odd-degree vertices is even.
 
 ## Main definitions
 
-- `simple_graph.sum_degrees_eq_twice_card_edges` is the degree-sum formula.
-- `simple_graph.even_card_odd_degree_vertices` is the handshaking lemma.
-- `simple_graph.odd_card_odd_degree_vertices_ne` is that the number of odd-degree
+- `SimpleGraph.sum_degrees_eq_twice_card_edges` is the degree-sum formula.
+- `SimpleGraph.even_card_odd_degree_vertices` is the handshaking lemma.
+- `SimpleGraph.odd_card_odd_degree_vertices_ne` is that the number of odd-degree
   vertices different from a given odd-degree vertex is odd.
-- `simple_graph.exists_ne_odd_degree_of_exists_odd_degree` is that the existence of an
+- `SimpleGraph.exists_ne_odd_degree_of_exists_odd_degree` is that the existence of an
   odd-degree vertex implies the existence of another one.
 
 ## Implementation notes
@@ -95,8 +95,11 @@ theorem dart_edge_fiber_card (e : Sym2 V) (h : e ∈ G.edgeSet) :
   refine' Sym2.ind (fun v w h => _) e h
   let d : G.Dart := ⟨(v, w), h⟩
   -- Porting note: convert congr_arg card d.edge_fiber doesn't work?
+  -- convert congr_arg card d.edge_fiber
+  -- rw [card_insert_of_not_mem, card_singleton]
   have x := congr_arg card d.edge_fiber
-  rw [card_insert_of_not_mem, card_singleton] at x; exact x
+  rw [card_insert_of_not_mem, card_singleton] at x
+  convert x
   rw [mem_singleton]
   exact d.symm_ne.symm
 #align simple_graph.dart_edge_fiber_card SimpleGraph.dart_edge_fiber_card
@@ -112,14 +115,14 @@ theorem dart_card_eq_twice_card_edges : Fintype.card G.Dart = 2 * G.edgeFinset.c
 #align simple_graph.dart_card_eq_twice_card_edges SimpleGraph.dart_card_eq_twice_card_edges
 
 /-- The degree-sum formula.  This is also known as the handshaking lemma, which might
-more specifically refer to `simple_graph.even_card_odd_degree_vertices`. -/
+more specifically refer to `SimpleGraph.even_card_odd_degree_vertices`. -/
 theorem sum_degrees_eq_twice_card_edges : (∑ v, G.degree v) = 2 * G.edgeFinset.card :=
   G.dart_card_eq_sum_degrees.symm.trans G.dart_card_eq_twice_card_edges
 #align simple_graph.sum_degrees_eq_twice_card_edges SimpleGraph.sum_degrees_eq_twice_card_edges
 
 end DegreeSum
 
-/-- The handshaking lemma.  See also `simple_graph.sum_degrees_eq_twice_card_edges`. -/
+/-- The handshaking lemma.  See also `SimpleGraph.sum_degrees_eq_twice_card_edges`. -/
 theorem even_card_odd_degree_vertices [Fintype V] [DecidableRel G.Adj] :
     Even (univ.filter fun v => Odd (G.degree v)).card := by
   classical
@@ -129,9 +132,8 @@ theorem even_card_odd_degree_vertices [Fintype V] [DecidableRel G.Adj] :
     rw [@sum_congr _ _ _ _ (fun v => (G.degree v : ZMod 2)) (fun _v => (1 : ZMod 2)) _ rfl] at h
     · simp only [filter_congr, mul_one, nsmul_eq_mul, sum_const, Ne.def] at h
       rw [← ZMod.eq_zero_iff_even]
-      -- Porting note: convert h doesn't work?
-      rw [← h]
-      congr
+      -- Porting note: old code is `convert h` but doesn't work?
+      rw [← h]; congr
       ext v
       rw [← ZMod.ne_zero_iff_odd]
     · intro v
