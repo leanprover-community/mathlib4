b01b466b2d2aefdee02b2a99851cf27f4ce2264b
Eric Wieser <wieser.eric@gmail.com>
2023-03-16 16:14:13 +0000

    improve workaround

--- Mathlib/LinearAlgebra/AffineSpace/AffineMap.lean
+++ Mathlib/LinearAlgebra/AffineSpace/AffineMap.lean
@@ -259,13 +259,13 @@ instance : Zero (P1 →ᵃ[k] V2) where zero := ⟨0, 0, fun _ _ => (zero_vadd _
 
 instance : Add (P1 →ᵃ[k] V2)
     where add f g := ⟨f + g, f.linear + g.linear,
-      -- porting note: `simp` needs `map_vadd _` to use `map_vadd`
-      fun p v => by simp [add_add_add_comm, map_vadd _]⟩
+      -- porting note: `simp` needs lemmas to be expressions
+      fun p v => by simp [add_add_add_comm, (map_vadd)]⟩
 
 instance : Sub (P1 →ᵃ[k] V2)
     where sub f g := ⟨f - g, f.linear - g.linear,
-      -- porting note: `simp` needs `map_vadd _` to use `map_vadd`
-      fun p v => by simp [sub_add_sub_comm, map_vadd _]⟩
+      -- porting note: `simp` needs lemmas to be expressions
+      fun p v => by simp [sub_add_sub_comm, (map_vadd)]⟩
 
 instance : Neg (P1 →ᵃ[k] V2)
     where neg f := ⟨-f, -f.linear, fun p v => by simp [add_comm, map_vadd f]⟩
@@ -320,14 +320,14 @@ from `P1` to the vector space `V2` corresponding to `P2`. -/
 instance : AffineSpace (P1 →ᵃ[k] V2) (P1 →ᵃ[k] P2) where
   vadd f g :=
     ⟨fun p => f p +ᵥ g p, f.linear + g.linear,
-      -- porting note: `simp` needs `map_vadd _` to use `map_vadd`
-      fun p v => by simp [vadd_vadd, add_right_comm, map_vadd _]⟩
+      -- porting note: `simp` needs lemmas to be expressions
+      fun p v => by simp [vadd_vadd, add_right_comm, (map_vadd)]⟩
   zero_vadd f := ext fun p => zero_vadd _ (f p)
   add_vadd f₁ f₂ f₃ := ext fun p => add_vadd (f₁ p) (f₂ p) (f₃ p)
   vsub f g :=
     ⟨fun p => f p -ᵥ g p, f.linear - g.linear, fun p v => by
-      -- porting note: `simp` needs `_` to use these lemmas
-      simp [map_vadd _, vsub_vadd_eq_vsub_sub _, vadd_vsub_assoc _,
+      -- porting note: `simp` needs lemmas to be expressions
+      simp [(map_vadd), (vsub_vadd_eq_vsub_sub), (vadd_vsub_assoc),
         add_sub, sub_add_eq_add_sub]⟩
   vsub_vadd' f g := ext fun p => vsub_vadd (f p) (g p)
   vadd_vsub' f g := ext fun p => vadd_vsub (f p) (g p)
@@ -499,8 +499,8 @@ theorem linear_bijective_iff (f : P1 →ᵃ[k] P2) :
 theorem image_vsub_image {s t : Set P1} (f : P1 →ᵃ[k] P2) :
     f '' s -ᵥ f '' t = f.linear '' (s -ᵥ t) := by
   ext v
-  -- porting note: `simp` needs a placeholder for the `β` argument to `Set.mem_vsub`
-  simp only [Set.mem_vsub (β := _), Set.mem_image,
+  -- porting note: `simp` needs `Set.mem_vsub` to be an expression
+  simp only [(Set.mem_vsub), Set.mem_image,
     exists_exists_and_eq_and, exists_and_left, ← f.linearMap_vsub]
   constructor
   · rintro ⟨x, hx, y, hy, hv⟩
@@ -554,8 +554,8 @@ theorem lineMap_linear (p₀ p₁ : P1) :
 #align affine_map.line_map_linear AffineMap.lineMap_linear
 
 theorem lineMap_same_apply (p : P1) (c : k) : lineMap p p c = p := by
-  -- porting note: `simp` needs a `_` to use these lemmas
-  simp [lineMap_apply _, vsub_self _]
+  -- porting note: `simp` needs lemmas to be expressions
+  simp [(lineMap_apply), (vsub_self)]
 #align affine_map.line_map_same_apply AffineMap.lineMap_same_apply
 
 @[simp]
@@ -565,14 +565,14 @@ theorem lineMap_same (p : P1) : lineMap p p = const k k p :=
 
 @[simp]
 theorem lineMap_apply_zero (p₀ p₁ : P1) : lineMap p₀ p₁ (0 : k) = p₀ := by
-  -- porting note: `simp` needs a `_` to use these lemmas
-  simp [lineMap_apply _]
+  -- porting note: `simp` needs lemmas to be expressions
+  simp [(lineMap_apply)]
 #align affine_map.line_map_apply_zero AffineMap.lineMap_apply_zero
 
 @[simp]
 theorem lineMap_apply_one (p₀ p₁ : P1) : lineMap p₀ p₁ (1 : k) = p₁ := by
-  -- porting note: `simp` needs a `_` to use these lemmas
-  simp [lineMap_apply _, vsub_vadd _]
+  -- porting note: `simp` needs lemmas to be expressions
+  simp [(lineMap_apply), (vsub_vadd)]
 #align affine_map.line_map_apply_one AffineMap.lineMap_apply_one
 
 @[simp]
@@ -606,8 +606,8 @@ variable {k}
 @[simp]
 theorem apply_lineMap (f : P1 →ᵃ[k] P2) (p₀ p₁ : P1) (c : k) :
     f (lineMap p₀ p₁ c) = lineMap (f p₀) (f p₁) c := by
-  -- porting note: `simp` needs a `_` to use these lemmas
-  simp [lineMap_apply _, lineMap_apply _, map_vadd _, linearMap_vsub _]
+  -- porting note: `simp` needs lemmas to be expressions
+  simp [(lineMap_apply), (map_vadd), (linearMap_vsub)]
 #align affine_map.apply_line_map AffineMap.apply_lineMap
 
 @[simp]
@@ -782,7 +782,7 @@ def toConstProdLinearMap : (V1 →ᵃ[k] V2) ≃ₗ[R] V2 × (V1 →ₗ[k] V2) w
   left_inv f := by
     ext
     rw [f.decomp]
-    simp [const_apply _ _]  -- porting note: `simp` needs `_`s to use these lemmas
+    simp [const_apply _ _]  -- porting note: `simp` needs `_`s to use this lemma
   right_inv := by
     rintro ⟨v, f⟩
     ext <;> simp [const_apply _ _, const_linear _ _]  -- porting note: `simp` needs `_`s
