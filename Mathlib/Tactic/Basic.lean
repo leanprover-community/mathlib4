14e576f9f8bf0f2c7ba13d2e57c66a4ec2bd1cba
Kyle Miller <kmill31415@gmail.com>
2023-03-16 15:44:22 +0000

    feat: `change` tactic (#2836)
    This is an implementation of the `change` tactic, whose syntax is declared in Lean 4 core.

--- Mathlib/Tactic/Basic.lean
+++ Mathlib/Tactic/Basic.lean
@@ -31,10 +31,39 @@ syntax (name := lemma)
     stx.setKind ``Parser.Command.theorem
   pure <| stx.setKind ``Parser.Command.declaration
 
-/-- `change` is a synonym for `show`,
-and can be used to replace a goal with a definitionally equal one. -/
-macro_rules
-  | `(tactic| change $e:term) => `(tactic| show $e)
+/-- `change` can be used to replace the main goal or its local
+variables with definitionally equal ones.
+
+For example, if `n : ℕ` and the current goal is `⊢ n + 2 = 2`, then
+```lean
+change _ + 1 = _
+```
+changes the goal to `⊢ n + 1 + 1 = 2`. The tactic also applies to the local context.
+If `h : n + 2 = 2` and `h' : n + 3 = 4` are in the local context, then
+```lean
+change _ + 1 = _ at h h'
+```
+changes their types to be `h : n + 1 + 1 = 2` and `h' : n + 2 + 1 = 4`.
+
+Change is like `refine` in that every placeholder needs to be solved for by unification,
+but you can use named placeholders and `?_` where you want `change` to create new goals.
+
+The the tactic `show e` is interchangeable with `change e`, where the pattern `e` is applied to
+the main goal. -/
+elab_rules : tactic
+  | `(tactic| change $newType:term $[$loc:location]?) => do
+    withLocation (expandOptLocation (Lean.mkOptionalNode loc))
+      (atLocal := fun h ↦ do
+        let hTy ← h.getType
+        -- This is a hack to get the new type to elaborate in the same sort of way that
+        -- it would for a `show` expression for the goal.
+        let mvar ← mkFreshExprMVar none
+        let (_, mvars) ← elabTermWithHoles
+                          (← `(term | show $newType from $(← Term.exprToSyntax mvar))) hTy `change
+        liftMetaTactic fun mvarId ↦ do
+          return (← mvarId.changeLocalDecl h (← inferType mvar)) :: mvars)
+      (atTarget := evalTactic <| ← `(tactic| show $newType))
+      (failed := fun _ ↦ throwError "change tactic failed")
 
 /--
 `by_cases p` makes a case distinction on `p`,
