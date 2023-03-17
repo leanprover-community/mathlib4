8b32ac6a7fad15d465640770bccf2ec20b6c9f15
thorimur <68410468+thorimur@users.noreply.github.com>
2023-03-17 00:44:00 -0400

    chore: become a test and `NormNum.Basic` author

--- Mathlib/Tactic/NormNum/Basic.lean
+++ Mathlib/Tactic/NormNum/Basic.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2021 Mario Carneiro. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Mario Carneiro
+Authors: Mario Carneiro, Thomas Murrills
 -/
 import Mathlib.Tactic.NormNum.Core
 import Mathlib.Algebra.GroupPower.Lemmas
