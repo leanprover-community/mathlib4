21c773a16bc9b21880b71f8cb32b46badd65ed7f
arienmalec <arien.malec@gmail.com>
2023-03-18 07:48:03 +0000

    feat: Port/RingTheory.Valuation.Integers (#2966)
    mostly simple fixes: proofs to sub for `trans_rel_right` and moving instance declarations into namespace.

--- Mathlib.lean
+++ Mathlib.lean
@@ -1286,6 +1286,7 @@ import Mathlib.RingTheory.Subring.Pointwise
 import Mathlib.RingTheory.Subsemiring.Basic
 import Mathlib.RingTheory.Subsemiring.Pointwise
 import Mathlib.RingTheory.Valuation.Basic
+import Mathlib.RingTheory.Valuation.Integers
 import Mathlib.SetTheory.Cardinal.Basic
 import Mathlib.SetTheory.Cardinal.Cofinality
 import Mathlib.SetTheory.Cardinal.Continuum
