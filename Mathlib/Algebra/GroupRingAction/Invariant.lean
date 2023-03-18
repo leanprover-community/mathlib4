de8762e9fee9288a39f87175a6b47d8c6bc77e54
int-y1 <jason_yuen2007@hotmail.com>
2023-03-18 01:59:15 -0700

    fix

--- Mathlib/Algebra/GroupRingAction/Invariant.lean
+++ Mathlib/Algebra/GroupRingAction/Invariant.lean
@@ -24,20 +24,20 @@ open MulAction
 
 variable {R}
 
-/-- A typeclass for subrings invariant under a `mul_semiring_action`. -/
+/-- A typeclass for subrings invariant under a `MulSemiringAction`. -/
 class IsInvariantSubring : Prop where
   smul_mem : ∀ (m : M) {x : R}, x ∈ S → m • x ∈ S
 #align is_invariant_subring IsInvariantSubring
 
 instance IsInvariantSubring.toMulSemiringAction [IsInvariantSubring M S] : MulSemiringAction M S
     where
-  smul m x := ⟨m • x, IsInvariantSubring.smul_mem m x.2⟩
-  one_smul s := Subtype.eq <| one_smul M s
-  mul_smul m₁ m₂ s := Subtype.eq <| mul_smul m₁ m₂ s
-  smul_add m s₁ s₂ := Subtype.eq <| smul_add m s₁ s₂
+  smul m x := ⟨m • ↑x, IsInvariantSubring.smul_mem m x.2⟩
+  one_smul s := Subtype.eq <| one_smul M (s : R)
+  mul_smul m₁ m₂ s := Subtype.eq <| mul_smul m₁ m₂ (s : R)
+  smul_add m s₁ s₂ := Subtype.eq <| smul_add m (s₁ : R) (s₂ : R)
   smul_zero m := Subtype.eq <| smul_zero m
   smul_one m := Subtype.eq <| smul_one m
-  smul_mul m s₁ s₂ := Subtype.eq <| smul_mul' m s₁ s₂
+  smul_mul m s₁ s₂ := Subtype.eq <| smul_mul' m (s₁ : R) (s₂ : R)
 #align is_invariant_subring.to_mul_semiring_action IsInvariantSubring.toMulSemiringAction
 
 end Ring
@@ -52,19 +52,19 @@ variable (U : Subring R') [IsInvariantSubring M U]
 
 /-- The canonical inclusion from an invariant subring. -/
 def IsInvariantSubring.subtypeHom : U →+*[M] R' :=
-  { U.Subtype with map_smul' := fun m s => rfl }
+  { U.subtype with map_smul' := fun _ _ ↦ rfl }
 #align is_invariant_subring.subtype_hom IsInvariantSubring.subtypeHom
 
+-- Porting note: changed `coe` to `Subtype.val`
 @[simp]
-theorem IsInvariantSubring.coeSubtype_hom : (IsInvariantSubring.subtypeHom M U : U → R') = coe :=
-  rfl
+theorem IsInvariantSubring.coeSubtype_hom :
+    (IsInvariantSubring.subtypeHom M U : U → R') = Subtype.val := rfl
 #align is_invariant_subring.coe_subtype_hom IsInvariantSubring.coeSubtype_hom
 
+-- Porting note: added `toRingHom`
 @[simp]
 theorem IsInvariantSubring.coe_subtype_hom' :
-    (IsInvariantSubring.subtypeHom M U : U →+* R') = U.Subtype :=
-  rfl
+    ((IsInvariantSubring.subtypeHom M U).toRingHom : U →+* R') = U.subtype := rfl
 #align is_invariant_subring.coe_subtype_hom' IsInvariantSubring.coe_subtype_hom'
 
 end
-
