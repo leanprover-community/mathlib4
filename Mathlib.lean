c6b71c5573dec8a702ce86bdb6861b071cd3a566
Kyle Miller <kmill31415@gmail.com>
2023-03-19 21:17:29 +0000

    fix Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -1588,7 +1588,6 @@ import Mathlib.Util.AssertNoSorry
 import Mathlib.Util.AtomM
 import Mathlib.Util.Export
 import Mathlib.Util.IncludeStr
-import Mathlib.Util.Intro
 import Mathlib.Util.MemoFix
 import Mathlib.Util.Syntax
 import Mathlib.Util.SynthesizeUsing
