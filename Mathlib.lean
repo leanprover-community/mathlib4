a86f11bb6623b0ffa089a4225f73ec406521c216
casavaca <96765450+casavaca@users.noreply.github.com>
2023-03-19 15:06:32 +0000

    feat: Port Data.ZMod.Parity (#2893)
    Comments change only.
    
    
    
    Co-authored-by: Parcly Taxel <reddeloostw@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -955,6 +955,7 @@ import Mathlib.Data.W.Cardinal
 import Mathlib.Data.W.Constructions
 import Mathlib.Data.ZMod.Basic
 import Mathlib.Data.ZMod.Defs
+import Mathlib.Data.ZMod.Parity
 import Mathlib.Deprecated.Group
 import Mathlib.Deprecated.Ring
 import Mathlib.Deprecated.Subfield
