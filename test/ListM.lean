575eb7ce07cf23ed0d99cfaac912f1f3a8fe9605
Scott Morrison <scott.morrison@gmail.com>
2023-03-21 08:05:33 +1100

    long line

--- test/ListM.lean
+++ test/ListM.lean
@@ -22,7 +22,8 @@ open Lean
   guard $ x = some ([10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
 #check (rfl : ((ListM.fix F 10).take 4).run [] = some ([10, 9, 8, 7], [7, 8, 9, 10]))
-#check (rfl : (((ListM.fix F 10).map fun n => n*n).take 3).run [] = some ([100, 81, 64], [8, 9, 10]))
+#check
+  (rfl : (((ListM.fix F 10).map fun n => n*n).take 3).run [] = some ([100, 81, 64], [8, 9, 10]))
 
 unsafe def l1 : ListM S Nat := ListM.ofList [0,1,2]
 unsafe def l2 : ListM S Nat := ListM.ofList [3,4,5]
