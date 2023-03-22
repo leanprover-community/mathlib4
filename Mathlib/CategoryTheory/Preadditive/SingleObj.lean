dcf3c4c2fb388f0d1c3ac39872974cb6d02df94d
Parcly Taxel <reddeloostw@gmail.com>
2023-03-22 16:06:36 +0800

    (Almost) nothing to do here

--- Mathlib/CategoryTheory/Preadditive/SingleObj.lean
+++ Mathlib/CategoryTheory/Preadditive/SingleObj.lean
@@ -12,7 +12,7 @@ import Mathlib.CategoryTheory.Preadditive.Basic
 import Mathlib.CategoryTheory.SingleObj
 
 /-!
-# `single_obj α` is preadditive when `α` is a ring.
+# `SingleObj α` is preadditive when `α` is a ring.
 
 -/
 
@@ -21,11 +21,9 @@ namespace CategoryTheory
 
 variable {α : Type _} [Ring α]
 
-instance : Preadditive (SingleObj α)
-    where
+instance : Preadditive (SingleObj α) where
   add_comp _ _ _ f f' g := mul_add g f f'
   comp_add _ _ _ f g g' := add_mul g g' f
 
 -- TODO define `PreAddCat` (with additive functors as morphisms), and `Ring ⥤ PreAddCat`.
 end CategoryTheory
-
