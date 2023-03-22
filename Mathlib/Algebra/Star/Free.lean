a5a068e0d9e0ee4d3357c47021adad41df3e5040
Parcly Taxel <reddeloostw@gmail.com>
2023-03-22 23:13:13 +0800

    Restore original proof for the last definition

--- Mathlib/Algebra/Star/Free.lean
+++ Mathlib/Algebra/Star/Free.lean
@@ -77,7 +77,7 @@ theorem star_algebraMap (r : R) : star (algebraMap R (FreeAlgebra R X) r) = alge
 
 /-- `star` as an `AlgEquiv` -/
 noncomputable def starHom : FreeAlgebra R X ≃ₐ[R] (FreeAlgebra R X)ᵐᵒᵖ :=
-  { starRingEquiv with commutes' := fun r => by rfl }
+  { starRingEquiv with commutes' := fun r => by simp [star_algebraMap] }
 #align free_algebra.star_hom FreeAlgebra.starHom
 
 end FreeAlgebra
