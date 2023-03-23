7fc680c578dc04028f3366275f6d634c4c3b08df
Jason Yuen <jason_yuen2007@hotmail.com>
2023-03-23 14:22:02 +0000

    feat: port RingTheory.Localization.NumDen (#3044)
    Co-authored-by: Jeremy Tan Jie Rui <reddeloostw@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1310,6 +1310,7 @@ import Mathlib.RingTheory.Int.Basic
 import Mathlib.RingTheory.Localization.Basic
 import Mathlib.RingTheory.Localization.FractionRing
 import Mathlib.RingTheory.Localization.Integer
+import Mathlib.RingTheory.Localization.NumDen
 import Mathlib.RingTheory.Multiplicity
 import Mathlib.RingTheory.MvPolynomial.Symmetric
 import Mathlib.RingTheory.MvPolynomial.Tower
