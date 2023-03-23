ddbb81ccee4b03a4f67d7874bb058efeb42bc37a
Moritz Firsching <moritz.firsching@gmail.com>
2023-03-23 13:19:40 +0000

    feat: port CategoryTheory.Limits.Shapes.Reflexive (#3041)
    Co-authored-by: Moritz Firsching <firsching@google.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -408,6 +408,7 @@ import Mathlib.CategoryTheory.Limits.Shapes.NormalMono.Basic
 import Mathlib.CategoryTheory.Limits.Shapes.NormalMono.Equalizers
 import Mathlib.CategoryTheory.Limits.Shapes.Products
 import Mathlib.CategoryTheory.Limits.Shapes.Pullbacks
+import Mathlib.CategoryTheory.Limits.Shapes.Reflexive
 import Mathlib.CategoryTheory.Limits.Shapes.RegularMono
 import Mathlib.CategoryTheory.Limits.Shapes.SplitCoequalizer
 import Mathlib.CategoryTheory.Limits.Shapes.StrictInitial
