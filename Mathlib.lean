b8ffec924d91317cdcbc8c77bd514e28c22e4244
Adam Topaz <k.buzzard@imperial.ac.uk>
2023-03-23 16:06:56 +0000

    feat: Port CategoryTheory.Limits.Shapes.Multiequalizer (#2786)
    Co-authored-by: adamtopaz <github@adamtopaz.com>
    Co-authored-by: Parcly Taxel <reddeloostw@gmail.com>
    Co-authored-by: Adam Topaz <adamtopaz@users.noreply.github.com>
    Co-authored-by: Moritz Firsching <firsching@google.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -406,6 +406,7 @@ import Mathlib.CategoryTheory.Limits.Shapes.FunctorCategory
 import Mathlib.CategoryTheory.Limits.Shapes.Images
 import Mathlib.CategoryTheory.Limits.Shapes.KernelPair
 import Mathlib.CategoryTheory.Limits.Shapes.Kernels
+import Mathlib.CategoryTheory.Limits.Shapes.Multiequalizer
 import Mathlib.CategoryTheory.Limits.Shapes.NormalMono.Basic
 import Mathlib.CategoryTheory.Limits.Shapes.NormalMono.Equalizers
 import Mathlib.CategoryTheory.Limits.Shapes.Products
