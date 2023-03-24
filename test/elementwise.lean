3e2f7bbbf594916862dc972c8856adbe13e4cbcb
Scott Morrison <scott.morrison@gmail.com>
2023-03-24 13:31:17 +1100

    clean up test

--- test/elementwise.lean
+++ test/elementwise.lean
@@ -27,11 +27,6 @@ example : ∀ C [Category C] (X : C) (f g h : X ⟶ X) (_ : g ≫ h = h ≫ g) [
     (x : X), h (g (f x)) = g (h (f x)) := @ex2_apply
 
 -- Need nosimp on the following `elementwise` since the lemma can be proved by simp anyway.
-example : ∀ (C : Type u_1) [inst : Category C] (X Y : C) (f : X ≅ Y) [inst_1 : ConcreteCategory C]
-  (x : ConcreteCategory.Forget.obj X),
-  Prefunctor.map (forget C).toPrefunctor f.inv (Prefunctor.map (forget C).toPrefunctor f.hom x)
-    = x := by simp
-
 @[elementwise nosimp]
 theorem ex3 [Category C] {X Y : C} (f : X ≅ Y) : f.hom ≫ f.inv = 𝟙 X :=
   Iso.hom_inv_id _
