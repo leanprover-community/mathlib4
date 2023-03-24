0e24dac46d88b392bdc96291b657d624cf3f7bde
Moritz Doll <moritz.doll@googlemail.com>
2023-03-24 13:23:49 +0000

    feat: port LinearAlgebra.AffineSpace.AffineSubspace (#2923)
    Co-authored-by: Jireh Loreaux <loreaujy@gmail.com>
    Co-authored-by: Parcly Taxel <reddeloostw@gmail.com>
    Co-authored-by: ChrisHughes24 <chrishughes24@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1119,6 +1119,7 @@ import Mathlib.Lean.Meta
 import Mathlib.Lean.Meta.Simp
 import Mathlib.LinearAlgebra.AffineSpace.AffineEquiv
 import Mathlib.LinearAlgebra.AffineSpace.AffineMap
+import Mathlib.LinearAlgebra.AffineSpace.AffineSubspace
 import Mathlib.LinearAlgebra.AffineSpace.Basic
 import Mathlib.LinearAlgebra.AffineSpace.Slope
 import Mathlib.LinearAlgebra.Basic
