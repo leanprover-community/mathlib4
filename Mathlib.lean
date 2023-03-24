31b48701ec27fb9ea5d144976b207fd69d9f618b
Matthew Robert Ballard <matt@mrb.email>
2023-03-24 20:19:30 +0000

    feat: port RingTheory.Polynomial.Content (#3066)
    Co-authored-by: Parcly Taxel <reddeloostw@gmail.com>
    Co-authored-by: ChrisHughes24 <chrishughes24@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1350,6 +1350,7 @@ import Mathlib.RingTheory.NonZeroDivisors
 import Mathlib.RingTheory.OreLocalization.Basic
 import Mathlib.RingTheory.OreLocalization.OreSet
 import Mathlib.RingTheory.Polynomial.Chebyshev
+import Mathlib.RingTheory.Polynomial.Content
 import Mathlib.RingTheory.Polynomial.Opposites
 import Mathlib.RingTheory.Polynomial.Pochhammer
 import Mathlib.RingTheory.Polynomial.ScaleRoots
