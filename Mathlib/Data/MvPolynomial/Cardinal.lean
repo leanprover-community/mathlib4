fde6488d752c06193d3dd20536015066256e3da8
Parcly Taxel <reddeloostw@gmail.com>
2023-03-25 10:06:54 +0800

    Fix errors

--- Mathlib/Data/MvPolynomial/Cardinal.lean
+++ Mathlib/Data/MvPolynomial/Cardinal.lean
@@ -15,8 +15,8 @@ import Mathlib.SetTheory.Cardinal.Ordinal
 /-!
 # Cardinality of Multivariate Polynomial Ring
 
-The main result in this file is `mv_polynomial.cardinal_mk_le_max`, which says that
-the cardinality of `mv_polynomial σ R` is bounded above by the maximum of `#R`, `#σ`
+The main result in this file is `MvPolynomial.cardinal_mk_le_max`, which says that
+the cardinality of `MvPolynomial σ R` is bounded above by the maximum of `#R`, `#σ`
 and `ℵ₀`.
 -/
 
@@ -37,7 +37,7 @@ variable {σ : Type u} {R : Type v} [CommSemiring R]
 theorem cardinal_mk_eq_max_lift [Nonempty σ] [Nontrivial R] :
     (#MvPolynomial σ R) = max (max (Cardinal.lift.{u} <| (#R)) <| Cardinal.lift.{v} <| (#σ)) ℵ₀ :=
   (mk_finsupp_lift_of_infinite _ R).trans <| by
-    rw [mk_finsupp_nat, max_assoc, lift_max, lift_aleph_0, max_comm]
+    rw [mk_finsupp_nat, max_assoc, lift_max, lift_aleph0, max_comm]
 #align mv_polynomial.cardinal_mk_eq_max_lift MvPolynomial.cardinal_mk_eq_max_lift
 
 @[simp]
@@ -45,11 +45,10 @@ theorem cardinal_mk_eq_lift [IsEmpty σ] : (#MvPolynomial σ R) = Cardinal.lift.
   ((isEmptyRingEquiv R σ).toEquiv.trans Equiv.ulift.{u}.symm).cardinal_eq
 #align mv_polynomial.cardinal_mk_eq_lift MvPolynomial.cardinal_mk_eq_lift
 
-theorem cardinal_lift_mk_le_max {σ : Type u} {R : Type v} [CommSemiring R] :
-    (#MvPolynomial σ R) ≤ max (max (Cardinal.lift.{u} <| (#R)) <| Cardinal.lift.{v} <| (#σ)) ℵ₀ :=
-  by
+theorem cardinal_lift_mk_le_max {σ : Type u} {R : Type v} [CommSemiring R] : (#MvPolynomial σ R) ≤
+    max (max (Cardinal.lift.{u} <| (#R)) <| Cardinal.lift.{v} <| (#σ)) ℵ₀ := by
   cases subsingleton_or_nontrivial R
-  · exact (mk_eq_one _).trans_le (le_max_of_le_right one_le_aleph_0)
+  · exact (mk_eq_one _).trans_le (le_max_of_le_right one_le_aleph0)
   cases isEmpty_or_nonempty σ
   · exact cardinal_mk_eq_lift.trans_le (le_max_of_le_left <| le_max_left _ _)
   · exact cardinal_mk_eq_max_lift.le
@@ -63,11 +62,10 @@ theorem cardinal_mk_eq_max [Nonempty σ] [Nontrivial R] :
     (#MvPolynomial σ R) = max (max (#R) (#σ)) ℵ₀ := by simp
 #align mv_polynomial.cardinal_mk_eq_max MvPolynomial.cardinal_mk_eq_max
 
-/-- The cardinality of the multivariate polynomial ring, `mv_polynomial σ R` is at most the maximum
+/-- The cardinality of the multivariate polynomial ring, `MvPolynomial σ R` is at most the maximum
 of `#R`, `#σ` and `ℵ₀` -/
 theorem cardinal_mk_le_max : (#MvPolynomial σ R) ≤ max (max (#R) (#σ)) ℵ₀ :=
   cardinal_lift_mk_le_max.trans <| by rw [lift_id, lift_id]
 #align mv_polynomial.cardinal_mk_le_max MvPolynomial.cardinal_mk_le_max
 
 end MvPolynomial
-
