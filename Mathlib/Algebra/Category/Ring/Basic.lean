2b917b6b10df09fd904377984ce78239e897b749
adamtopaz <github@adamtopaz.com>
2023-03-25 22:14:30 -0600

    wip

--- Mathlib/Algebra/Category/Ring/Basic.lean
+++ Mathlib/Algebra/Category/Ring/Basic.lean
@@ -8,8 +8,8 @@ Authors: Scott Morrison, Johannes Hölzl, Yury Kudryashov
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
-import Mathlib.Algebra.Category.Group.Basic
-import Mathlib.CategoryTheory.ConcreteCategory.ReflectsIsomorphisms
+import Mathlib.Algebra.Category.GroupCat.Basic
+import Mathlib.CategoryTheory.ConcreteCategory.ReflectsIso
 import Mathlib.CategoryTheory.Elementwise
 import Mathlib.Algebra.Ring.Equiv
 
@@ -30,33 +30,68 @@ universe u v
 open CategoryTheory
 
 /-- The category of semirings. -/
-def SemiRing : Type (u + 1) :=
+def SemiRingCat : Type (u + 1) :=
   Bundled Semiring
-#align SemiRing SemiRing
+set_option linter.uppercaseLean3 false in
+#align SemiRing SemiRingCat
 
-namespace SemiRing
+namespace SemiRingCat
 
 /-- `ring_hom` doesn't actually assume associativity. This alias is needed to make the category
 theory machinery work. We use the same trick in `category_theory.Mon.assoc_monoid_hom`. -/
 abbrev AssocRingHom (M N : Type _) [Semiring M] [Semiring N] :=
   RingHom M N
-#align SemiRing.assoc_ring_hom SemiRing.AssocRingHom
+set_option linter.uppercaseLean3 false in
+#align SemiRing.assoc_ring_hom SemiRingCat.AssocRingHom
+/-
+  toFun : ∀ {α β : Type u} (Iα : c α) (Iβ : c β), hom Iα Iβ → α → β
+  /-- the identity as a bundled morphism -/
+  id : ∀ {α : Type u} (I : c α), hom I I
+  /-- composition of bundled morphisms -/
+  comp : ∀ {α β γ : Type u} (Iα : c α) (Iβ : c β) (Iγ : c γ), hom Iβ Iγ → hom Iα Iβ → hom Iα Iγ
+  /-- a bundled morphism is determined by the underlying map -/
+  hom_ext : ∀ {α β : Type u} (Iα : c α) (Iβ : c β), Function.Injective (toFun Iα Iβ) := by
+   aesop_cat
+  /-- compatibility with identities -/
+  id_toFun : ∀ {α : Type u} (I : c α), toFun I I (id I) = _root_.id := by aesop_cat
+  /-- compatibility with the composition -/
+  comp_toFun
+-/
 
-instance bundledHom : BundledHom AssocRingHom :=
+instance bundledHom : BundledHom AssocRingHom where
+  toFun := fun {M N} _ _ f => f
+  id := fun {A} _ => RingHom.id _
+  comp := fun {M N L} _ _ _ f g => f.comp g
+  hom_ext := fun {A B} _ _ f g h => by ext x ; exact congrFun h x
+  id_toFun := fun {A} _ => by ext x ; rfl
+  comp_toFun := fun {A B C} _ _ _ f g => rfl
+  /-
+  Porting note: The ported proof is this:
   ⟨fun M N [Semiring M] [Semiring N] => @RingHom.toFun M N _ _, fun M [Semiring M] =>
     @RingHom.id M _, fun M N P [Semiring M] [Semiring N] [Semiring P] => @RingHom.comp M N P _ _ _,
     fun M N [Semiring M] [Semiring N] => @RingHom.coe_inj M N _ _⟩
-#align SemiRing.bundled_hom SemiRing.bundledHom
+  -/
+set_option linter.uppercaseLean3 false in
+#align SemiRing.bundled_hom SemiRingCat.bundledHom
 
-deriving instance LargeCategory, ConcreteCategory for SemiRing
+--Porting note: deriving fails for ConcreteCategory, adding instance manually.
+--deriving instance LargeCategory, ConcreteCategory for SemiRingCat
 
-instance : CoeSort SemiRing (Type _) :=
-  Bundled.hasCoeToSort
+deriving instance LargeCategory for SemiRingCat
+
+instance : ConcreteCategory SemiRingCat := by
+  dsimp [SemiRingCat]
+  infer_instance
+
+instance : CoeSort SemiRingCat (Type _) := by
+  dsimp [SemiRingCat]
+  infer_instance
 
 /-- Construct a bundled SemiRing from the underlying type and typeclass. -/
-def of (R : Type u) [Semiring R] : SemiRing :=
+def of (R : Type u) [Semiring R] : SemiRingCat :=
   Bundled.of R
-#align SemiRing.of SemiRing.of
+set_option linter.uppercaseLean3 false in
+#align SemiRing.of SemiRingCat.of
 
 /-- Typecheck a `ring_hom` as a morphism in `SemiRing`. -/
 def ofHom {R S : Type u} [Semiring R] [Semiring S] (f : R →+* S) : of R ⟶ of S :=
@@ -375,4 +410,3 @@ theorem CommRingCat.ringHom_comp_eq_comp {R S T : Type _} [CommRing R] [CommRing
 attribute [local instance] reflects_isomorphisms_forget₂
 
 example : ReflectsIsomorphisms (forget₂ RingCat AddCommGroupCat) := by infer_instance
-
