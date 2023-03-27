49611fb991934e8bdc26e6dbd18f4b45e054707c
ChrisHughes24 <chrishughes24@gmail.com>
2023-03-27 18:15:13 +0100

    fix red

--- Mathlib/Data/Polynomial/PartialFractions.lean
+++ Mathlib/Data/Polynomial/PartialFractions.lean
@@ -56,6 +56,9 @@ variable (K : Type) [Field K] [Algebra R[X] K] [IsFractionRing R[X] K]
 
 section TwoDenominators
 
+--Porting note: added for scoped `Algebra.cast` instance
+open algebraMap
+
 /-- Let R be an integral domain and f, g₁, g₂ ∈ R[X]. Let g₁ and g₂ be monic and coprime.
 Then, ∃ q, r₁, r₂ ∈ R[X] such that f / g₁g₂ = q + r₁/g₁ + r₂/g₂ and deg(r₁) < deg(g₁) and
 deg(r₂) < deg(g₂).
@@ -64,19 +67,19 @@ theorem div_eq_quo_add_rem_div_add_rem_div (f : R[X]) {g₁ g₂ : R[X]} (hg₁ 
     (hg₂ : g₂.Monic) (hcoprime : IsCoprime g₁ g₂) :
     ∃ q r₁ r₂ : R[X],
       r₁.degree < g₁.degree ∧
-        r₂.degree < g₂.degree ∧ (↑f : K) / (↑g₁ * ↑g₂) = ↑q + ↑r₁ / ↑g₁ + ↑r₂ / ↑g₂ := by
+        r₂.degree < g₂.degree ∧ (f : K) / (↑g₁ * ↑g₂) = ↑q + ↑r₁ / ↑g₁ + ↑r₂ / ↑g₂ := by
   rcases hcoprime with ⟨c, d, hcd⟩
   refine'
-    ⟨f * d /ₘ g₁ + f * c /ₘ g₂, f * d %ₘ g₁, f * c %ₘ g₂, degree_mod_by_monic_lt _ hg₁,
-      degree_mod_by_monic_lt _ hg₂, _⟩
+    ⟨f * d /ₘ g₁ + f * c /ₘ g₂, f * d %ₘ g₁, f * c %ₘ g₂, degree_modByMonic_lt _ hg₁,
+      degree_modByMonic_lt _ hg₂, _⟩
   have hg₁' : (↑g₁ : K) ≠ 0 := by
     norm_cast
     exact hg₁.ne_zero_of_ne zero_ne_one
   have hg₂' : (↑g₂ : K) ≠ 0 := by
     norm_cast
     exact hg₂.ne_zero_of_ne zero_ne_one
-  have hfc := mod_by_monic_add_div (f * c) hg₂
-  have hfd := mod_by_monic_add_div (f * d) hg₁
+  have hfc := modByMonic_add_div (f * c) hg₂
+  have hfd := modByMonic_add_div (f * d) hg₁
   field_simp
   norm_cast
   linear_combination -1 * f * hcd + -1 * g₁ * hfc + -1 * g₂ * hfd
@@ -88,6 +91,9 @@ section NDenominators
 
 open BigOperators Classical
 
+--Porting note: added for scoped `Algebra.cast` instance
+open algebraMap
+
 /-- Let R be an integral domain and f ∈ R[X]. Let s be a finite index set.
 Then, a fraction of the form f / ∏ (g i) can be rewritten as q + ∑ (r i) / (g i), where
 deg(r i) < deg(g i), provided that the g i are monic and pairwise coprime.
@@ -96,17 +102,24 @@ theorem div_eq_quo_add_sum_rem_div (f : R[X]) {ι : Type _} {g : ι → R[X]} {s
     (hg : ∀ i ∈ s, (g i).Monic) (hcop : Set.Pairwise ↑s fun i j => IsCoprime (g i) (g j)) :
     ∃ (q : R[X])(r : ι → R[X]),
       (∀ i ∈ s, (r i).degree < (g i).degree) ∧
-        ((↑f : K) / ∏ i in s, ↑(g i)) = ↑q + ∑ i in s, ↑(r i) / ↑(g i) := by
+        ((↑f : K) / ∏ i in s, ↑(g i)) = ↑q + ∑ i in s, (r i : K) / (g i : K) := by
   induction' s using Finset.induction_on with a b hab Hind f generalizing f
-  · refine' ⟨f, fun i : ι => (0 : R[X]), fun i => _, by simp⟩
+  · refine' ⟨f, fun _ : ι => (0 : R[X]), fun i => _, by simp⟩
     rintro ⟨⟩
-  obtain ⟨q₀, r₁, r₂, hdeg₁, hdeg₂, hf : (↑f : K) / _ = _⟩ :=
-    div_eq_quo_add_rem_div_add_rem_div R K f (_ : monic (g a)) (_ : monic (∏ i : ι in b, g i)) _
+  obtain ⟨q₀, r₁, r₂, hdeg₁, _, hf : (↑f : K) / _ = _⟩ :=
+    div_eq_quo_add_rem_div_add_rem_div R K f
+      (hg a (b.mem_insert_self a) : Monic (g a))
+      (monic_prod_of_monic _ _ fun i hi => hg i (Finset.mem_insert_of_mem hi) :
+        Monic (∏ i : ι in b, g i))
+      (IsCoprime.prod_right fun i hi =>
+        hcop (Finset.mem_coe.2 (b.mem_insert_self a))
+          (Finset.mem_coe.2 (Finset.mem_insert_of_mem hi)) (by rintro rfl; exact hab hi))
   · obtain ⟨q, r, hrdeg, IH⟩ :=
-      Hind (fun i hi => hg i (Finset.mem_insert_of_mem hi))
-        (Set.Pairwise.mono (Finset.coe_subset.2 fun i hi => Finset.mem_insert_of_mem hi) hcop) r₂
-    refine' ⟨q₀ + q, fun i => if i = a then r₁ else r i, _, _⟩
+      Hind _ (fun i hi => hg i (Finset.mem_insert_of_mem hi))
+        (Set.Pairwise.mono (Finset.coe_subset.2 fun i hi => Finset.mem_insert_of_mem hi) hcop)
+    refine ⟨q₀ + q, fun i => if i = a then r₁ else r i, ?_, ?_⟩
     · intro i
+      dsimp only
       split_ifs with h1
       · cases h1
         intro
@@ -115,7 +128,7 @@ theorem div_eq_quo_add_sum_rem_div (f : R[X]) {ι : Type _} {g : ι → R[X]} {s
         exact hrdeg i (Finset.mem_of_mem_insert_of_ne hi h1)
     norm_cast  at hf IH⊢
     rw [Finset.prod_insert hab, hf, IH, Finset.sum_insert hab, if_pos rfl]
-    trans (↑(q₀ + q : R[X]) : K) + (↑r₁ / ↑(g a) + ∑ i : ι in b, ↑(r i) / ↑(g i))
+    trans (↑(q₀ + q : R[X]) : K) + (↑r₁ / ↑(g a) + ∑ i : ι in b, (r i : K) / (g i : K))
     · push_cast
       ring
     congr 2
@@ -123,15 +136,6 @@ theorem div_eq_quo_add_sum_rem_div (f : R[X]) {ι : Type _} {g : ι → R[X]} {s
     rw [if_neg]
     rintro rfl
     exact hab hxb
-  · exact hg a (b.mem_insert_self a)
-  · exact monic_prod_of_monic _ _ fun i hi => hg i (Finset.mem_insert_of_mem hi)
-  · refine'
-      IsCoprime.prod_right fun i hi =>
-        hcop (Finset.mem_coe.2 (b.mem_insert_self a))
-          (Finset.mem_coe.2 (Finset.mem_insert_of_mem hi)) _
-    rintro rfl
-    exact hab hi
 #align div_eq_quo_add_sum_rem_div div_eq_quo_add_sum_rem_div
 
 end NDenominators
-
