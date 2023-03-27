067ac0d9dfdc9880635559c5c49790e880573716
Parcly Taxel <reddeloostw@gmail.com>
2023-03-27 19:26:07 +0800

    chore: forward-port leanprover-community/mathlib#18636

--- Mathlib/Order/Antichain.lean
+++ Mathlib/Order/Antichain.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yaël Dillies
 
 ! This file was ported from Lean 3 source module order.antichain
-! leanprover-community/mathlib commit 207cfac9fcd06138865b5d04f7091e46d9320432
+! leanprover-community/mathlib commit c227d107bbada5d0d9d20287e3282c0a7f1651a0
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
@@ -30,7 +30,7 @@ open Function Set
 
 section General
 
-variable {α β : Type _} {r r₁ r₂ : α → α → Prop} {r' : β → β → Prop} {s t : Set α} {a : α}
+variable {α β : Type _} {r r₁ r₂ : α → α → Prop} {r' : β → β → Prop} {s t : Set α} {a b : α}
 
 protected theorem Symmetric.compl (h : Symmetric r) : Symmetric (rᶜ) := fun _ _ hr hr' =>
   hr <| h hr'
@@ -214,6 +214,10 @@ section Preorder
 
 variable [Preorder α]
 
+theorem IsAntichain.not_lt (hs : IsAntichain (· ≤ ·) s) (ha : a ∈ s) (hb : b ∈ s) : ¬a < b :=
+  fun h => hs ha hb h.ne h.le
+#align is_antichain.not_lt IsAntichain.not_lt
+
 theorem isAntichain_and_least_iff : IsAntichain (· ≤ ·) s ∧ IsLeast s a ↔ s = {a} :=
   ⟨fun h => eq_singleton_iff_unique_mem.2 ⟨h.2.1, fun b hb => h.1.eq' hb h.2.1 (h.2.2 hb)⟩, by
     rintro rfl
@@ -251,6 +255,17 @@ theorem IsAntichain.top_mem_iff [OrderTop α] (hs : IsAntichain (· ≤ ·) s) :
 #align is_antichain.top_mem_iff IsAntichain.top_mem_iff
 
 end Preorder
+
+section PartialOrder
+
+variable [PartialOrder α]
+
+theorem isAntichain_iff_forall_not_lt :
+    IsAntichain (· ≤ ·) s ↔ ∀ ⦃a⦄, a ∈ s → ∀ ⦃b⦄, b ∈ s → ¬a < b :=
+  ⟨fun hs _ ha _ => hs.not_lt ha, fun hs _ ha _ hb h h' => hs ha hb <| h'.lt_of_ne h⟩
+#align is_antichain_iff_forall_not_lt isAntichain_iff_forall_not_lt
+
+end PartialOrder
 
 /-! ### Strong antichains -/
 
