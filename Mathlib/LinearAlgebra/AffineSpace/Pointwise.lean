c4b66544e56e35337cff3398f5a43d34fe4a2269
ChrisHughes24 <chrishughes24@gmail.com>
2023-03-27 17:59:48 +0100

    fixes

--- Mathlib/LinearAlgebra/AffineSpace/Pointwise.lean
+++ Mathlib/LinearAlgebra/AffineSpace/Pointwise.lean
@@ -10,10 +10,10 @@ Authors: Hanting Zhang
 -/
 import Mathlib.LinearAlgebra.AffineSpace.AffineSubspace
 
-/-! # Pointwise instances on `affine_subspace`s
+/-! # Pointwise instances on `AffineSubspace`s
 
-This file provides the additive action `affine_subspace.pointwise_add_action` in the
-`pointwise` locale.
+This file provides the additive action `AffineSubspace.pointwiseAddAction` in the
+`Pointwise` locale.
 
 -/
 
@@ -28,13 +28,11 @@ variable {k : Type _} [Ring k]
 
 variable {V P V₁ P₁ V₂ P₂ : Type _}
 
-variable [AddCommGroup V] [Module k V] [affine_space V P]
+variable [AddCommGroup V] [Module k V] [AffineSpace V P]
 
 variable [AddCommGroup V₁] [Module k V₁] [AddTorsor V₁ P₁]
 
 variable [AddCommGroup V₂] [Module k V₂] [AddTorsor V₂ P₂]
-
-include V
 
 /-- The additive action on an affine subspace corresponding to applying the action to every element.
 
@@ -42,13 +40,18 @@ This is available as an instance in the `pointwise` locale. -/
 protected def pointwiseAddAction : AddAction V (AffineSubspace k P) where
   vadd x S := S.map (AffineEquiv.constVAdd k P x)
   zero_vadd p := ((congr_arg fun f => p.map f) <| AffineMap.ext <| zero_vadd _).trans p.map_id
-  add_vadd x y p :=
+  add_vadd _ _ p :=
     ((congr_arg fun f => p.map f) <| AffineMap.ext <| add_vadd _ _).trans (p.map_map _ _).symm
 #align affine_subspace.pointwise_add_action AffineSubspace.pointwiseAddAction
 
 scoped[Pointwise] attribute [instance] AffineSubspace.pointwiseAddAction
 
 open Pointwise
+
+--Porting note: new theorem
+theorem pointwise_vadd_eq_map (v : V) (s : AffineSubspace k P) :
+    v +ᵥ s = s.map (AffineEquiv.constVAdd k P v) :=
+  rfl
 
 @[simp]
 theorem coe_pointwise_vadd (v : V) (s : AffineSubspace k P) :
@@ -61,13 +64,13 @@ theorem vadd_mem_pointwise_vadd_iff {v : V} {s : AffineSubspace k P} {p : P} :
   vadd_mem_vadd_set_iff
 #align affine_subspace.vadd_mem_pointwise_vadd_iff AffineSubspace.vadd_mem_pointwise_vadd_iff
 
-theorem pointwise_vadd_bot (v : V) : v +ᵥ (⊥ : AffineSubspace k P) = ⊥ := by simp [SetLike.ext'_iff]
+theorem pointwise_vadd_bot (v : V) : v +ᵥ (⊥ : AffineSubspace k P) = ⊥ := by
+  ext; simp [pointwise_vadd_eq_map, map_bot]
 #align affine_subspace.pointwise_vadd_bot AffineSubspace.pointwise_vadd_bot
 
 theorem pointwise_vadd_direction (v : V) (s : AffineSubspace k P) :
     (v +ᵥ s).direction = s.direction := by
-  unfold VAdd.vadd
-  rw [map_direction]
+  rw [pointwise_vadd_eq_map, map_direction]
   exact Submodule.map_id _
 #align affine_subspace.pointwise_vadd_direction AffineSubspace.pointwise_vadd_direction
 
@@ -75,18 +78,12 @@ theorem pointwise_vadd_span (v : V) (s : Set P) : v +ᵥ affineSpan k s = affine
   map_span _ s
 #align affine_subspace.pointwise_vadd_span AffineSubspace.pointwise_vadd_span
 
-omit V
-
-include V₁ V₂
-
 theorem map_pointwise_vadd (f : P₁ →ᵃ[k] P₂) (v : V₁) (s : AffineSubspace k P₁) :
     (v +ᵥ s).map f = f.linear v +ᵥ s.map f := by
-  unfold VAdd.vadd
-  rw [map_map, map_map]
+  erw [pointwise_vadd_eq_map, pointwise_vadd_eq_map, map_map, map_map]
   congr 1
   ext
   exact f.map_vadd _ _
 #align affine_subspace.map_pointwise_vadd AffineSubspace.map_pointwise_vadd
 
 end AffineSubspace
-
