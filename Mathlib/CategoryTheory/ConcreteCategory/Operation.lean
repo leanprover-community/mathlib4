a287da0618d54f38eff30cdb2e2953e88d7b66ef
Joël Riou <joel.riou@universite-paris-saclay.fr>
2023-03-27 21:37:15 +0200

    some progress

--- Mathlib/CategoryTheory/ConcreteCategory/Operation.lean
+++ Mathlib/CategoryTheory/ConcreteCategory/Operation.lean
@@ -22,6 +22,7 @@ def functorPr₁ {F₁ F₂ : D ⥤ Type w} : functorConcat F₁ F₂ ⟶ F₁ w
 def functorPr₂ {F₁ F₂ : D ⥤ Type w} : functorConcat F₁ F₂ ⟶ F₂ where
   app X a := a.2
 
+@[simp]
 def natTransConcat {F F₁ F₂ : D ⥤ Type w} (τ₁ : F ⟶ F₁) (τ₂ : F ⟶ F₂) :
     F ⟶ functorConcat F₁ F₂ where
   app X a := ⟨τ₁.app X a, τ₂.app X a⟩
@@ -29,39 +30,127 @@ def natTransConcat {F F₁ F₂ : D ⥤ Type w} (τ₁ : F ⟶ F₁) (τ₂ : F 
     ext x
     exact Prod.ext (congr_fun (τ₁.naturality f) x) (congr_fun (τ₂.naturality f) x)
 
+@[simp]
+def functorConcat₃ (F₁ F₂ F₃ : D ⥤ Type w) :=
+  functorConcat F₁ (functorConcat F₂ F₃)
+
+@[simps!]
+def functorPr₃₁ {F₁ F₂ F₃ : D ⥤ Type w} : functorConcat₃ F₁ F₂ F₃ ⟶ F₁ := functorPr₁
+
+@[simps!]
+def functorPr₃₂ {F₁ F₂ F₃ : D ⥤ Type w} : functorConcat₃ F₁ F₂ F₃ ⟶ F₂ :=
+  functorPr₂ ≫ functorPr₁
+
+@[simps!]
+def functorPr₃₃ {F₁ F₂ F₃ : D ⥤ Type w} : functorConcat₃ F₁ F₂ F₃ ⟶ F₃ :=
+  functorPr₂ ≫ functorPr₂
+
+@[simp]
+def natTransConcat₃ {F F₁ F₂ F₃ : D ⥤ Type w} (τ₁ : F ⟶ F₁) (τ₂ : F ⟶ F₂) (τ₃ : F ⟶ F₃) :
+    F ⟶ functorConcat₃ F₁ F₂ F₃ :=
+  natTransConcat τ₁ (natTransConcat τ₂ τ₃)
+
+def functorOperation₀ (F : D ⥤ Type w) := (Functor.const D).obj PUnit ⟶ F
+def functorOperation₁ (F : D ⥤ Type w) := F ⟶ F
+def functorOperation₂ (F : D ⥤ Type w) := functorConcat F F ⟶ F
+def functorOperation₃ (F : D ⥤ Type w) := functorConcat₃ F F F ⟶ F
+
+@[simp]
+def functorOperation₀.of_iso {F₁ F₂ : D ⥤ Type w} (h : functorOperation₀ F₁)
+  (e : F₁ ≅ F₂) : functorOperation₀ F₂ := h ≫ e.hom
+
+def functorOperation₀.equiv_of_iso {F₁ F₂ : D ⥤ Type w} (e : F₁ ≅ F₂) :
+  functorOperation₀ F₁ ≃ functorOperation₀ F₂ where
+  toFun h := h.of_iso e
+  invFun h := h.of_iso e.symm
+  left_inv h := by simp
+  right_inv h := by simp
+
+@[simp]
+def functorOperation₁.of_iso {F₁ F₂ : D ⥤ Type w} (h : functorOperation₁ F₁)
+  (e : F₁ ≅ F₂) : functorOperation₁ F₂ := e.inv ≫ h ≫ e.hom
+
+def functorOperation₁.equiv_of_iso {F₁ F₂ : D ⥤ Type w} (e : F₁ ≅ F₂) :
+  functorOperation₁ F₁ ≃ functorOperation₁ F₂ where
+  toFun h := h.of_iso e
+  invFun h := h.of_iso e.symm
+  left_inv h := by simp
+  right_inv h := by simp
+
+@[simp]
+def functorOperation₂.of_iso {F₁ F₂ : D ⥤ Type w} (h : functorOperation₂ F₁)
+    (e : F₁ ≅ F₂) : functorOperation₂ F₂ :=
+  natTransConcat (functorPr₁ ≫ e.inv) (functorPr₂ ≫ e.inv) ≫ h ≫ e.hom
+
+def functorOperation₂.equiv_of_iso {F₁ F₂ : D ⥤ Type w} (e : F₁ ≅ F₂) :
+  functorOperation₂ F₁ ≃ functorOperation₂ F₂ where
+  toFun h := h.of_iso e
+  invFun h := h.of_iso e.symm
+  left_inv h := by
+    apply NatTrans.ext
+    ext1 X
+    funext
+    simp
+  right_inv h := by
+    apply NatTrans.ext
+    ext1 X
+    funext
+    simp
+
+@[simp]
+def functorOperation₃.of_iso {F₁ F₂ : D ⥤ Type w} (h : functorOperation₃ F₁)
+    (e : F₁ ≅ F₂) : functorOperation₃ F₂ :=
+  natTransConcat₃ (functorPr₃₁ ≫ e.inv) (functorPr₃₂ ≫ e.inv) (functorPr₃₃ ≫ e.inv) ≫
+    h ≫ e.hom
+
+def functorOperation₃.equiv_of_iso {F₁ F₂ : D ⥤ Type w} (e : F₁ ≅ F₂) :
+  functorOperation₃ F₁ ≃ functorOperation₃ F₂ where
+  toFun h := h.of_iso e
+  invFun h := h.of_iso e.symm
+  left_inv h := by
+    apply NatTrans.ext
+    ext1 X
+    funext
+    simp
+  right_inv h := by
+    apply NatTrans.ext
+    ext1 X
+    funext
+    simp
+
 end Types
 
 variable (A : Type u) [Category.{v} A] [ConcreteCategory.{w} A]
 
 namespace ConcreteCategory
 
-def operation₀ := (Functor.const A).obj PUnit ⟶ forget A
-def operation₁ := forget A ⟶ forget A
-def operation₂ := Types.functorConcat (forget A) (forget A) ⟶ forget A
-def operation₃ := Types.functorConcat (forget A) (Types.functorConcat (forget A) (forget A)) ⟶ forget A
+def Operation₀ := (Functor.const A).obj PUnit ⟶ forget A
+def Operation₁ := forget A ⟶ forget A
+def Operation₂ := Types.functorConcat (forget A) (forget A) ⟶ forget A
+def Operation₃ := Types.functorConcat (forget A) (Types.functorConcat (forget A) (forget A)) ⟶ forget A
 
 -- the naturality of these operations should be made automatic...
 
 @[simps]
-def AddCommGroup_zero : operation₀ AddCommGroupCat.{u} where
+def AddCommGroup_zero : Operation₀ AddCommGroupCat.{u} where
   app M _ := (0 : M)
-  naturality M N f:= by
+  naturality M N f := by
     ext
     exact (AddCommGroupCat.Hom.map_zero f).symm
 
 @[simps]
-def AddCommGroup_neg : operation₁ AddCommGroupCat.{u} where
+def AddCommGroup_neg : Operation₁ AddCommGroupCat.{u} where
   app M (x : M) := -x
   naturality M N f := by
     ext x
     exact (AddMonoidHom.map_neg (show AddMonoidHom M N from f) x).symm
 
 @[simps]
-def AddCommGroup_add : operation₂ AddCommGroupCat.{u} where
+def AddCommGroup_add : Operation₂ AddCommGroupCat.{u} where
   app M := fun ⟨(x : M), (y : M)⟩ => x + y
   naturality M N f := by
     ext
-    exact (AddMonoidHom.map_add (show AddMonoidHom M N from f) _ _).symm
+    exact (AddCommGroupCat.Hom.map_add _ _ _).symm
 
 end ConcreteCategory
 
--- Mathlib/CategoryTheory/Internal/Basic.lean
+++ Mathlib/CategoryTheory/Internal/Basic.lean
@@ -1,4 +1,4 @@
-import Mathlib.CategoryTheory.ConcreteCategory.Basic
+import Mathlib.CategoryTheory.ConcreteCategory.Operation
 
 universe v₁ v₂ u₁ u₂
 
@@ -13,5 +13,45 @@ structure Internal :=
 (iso : yoneda.obj obj ≅ presheaf ⋙ forget A)
 
 instance : Category (Internal A C) := InducedCategory.category (fun X => X.presheaf)
+
+def Internal.presheafFunctor : Internal A C ⥤ Cᵒᵖ ⥤ A := inducedFunctor _
+def Internal.typesPresheafFunctor : Internal A C ⥤ Cᵒᵖ ⥤ Type v₂ :=
+  Internal.presheafFunctor A C ⋙ (whiskeringRight Cᵒᵖ A (Type v₂)).obj (forget A)
+
+variable {A C}
+
+abbrev Internal.typesPresheaf (X : Internal A C) := (Internal.typesPresheafFunctor A C).obj X
+
+def ConcreteCategory.Operation₀.onTypesPresheaf (oper : Operation₀ A)
+    (X : Internal A C) : Types.functorOperation₀ X.typesPresheaf :=
+  whiskerLeft X.presheaf oper
+
+def ConcreteCategory.Operation₀.onInternal (oper : Operation₀ A)
+    (X : Internal A C) : Types.functorOperation₀ (yoneda.obj X.obj) :=
+  (oper.onTypesPresheaf X).of_iso X.iso.symm
+
+def ConcreteCategory.Operation₁.onTypesPresheaf (oper : Operation₁ A)
+    (X : Internal A C) : Types.functorOperation₁ X.typesPresheaf :=
+  whiskerLeft X.presheaf oper
+
+def ConcreteCategory.Operation₁.onInternal (oper : Operation₁ A)
+    (X : Internal A C) : Types.functorOperation₁ (yoneda.obj X.obj) :=
+  (oper.onTypesPresheaf X).of_iso X.iso.symm
+
+def ConcreteCategory.Operation₂.onTypesPresheaf (oper : Operation₂ A)
+  (X : Internal A C) : Types.functorOperation₂ X.typesPresheaf :=
+  whiskerLeft X.presheaf oper
+
+def ConcreteCategory.Operation₂.onInternal (oper : Operation₂ A)
+    (X : Internal A C) : Types.functorOperation₂ (yoneda.obj X.obj) :=
+  (oper.onTypesPresheaf X).of_iso X.iso.symm
+
+def ConcreteCategory.Operation₃.onTypesPresheaf (oper : Operation₃ A)
+  (X : Internal A C) : Types.functorOperation₃ X.typesPresheaf :=
+  whiskerLeft X.presheaf oper
+
+def ConcreteCategory.Operation₃.onInternal (oper : Operation₃ A)
+    (X : Internal A C) : Types.functorOperation₃ (yoneda.obj X.obj) :=
+  (oper.onTypesPresheaf X).of_iso X.iso.symm
 
 end CategoryTheory
