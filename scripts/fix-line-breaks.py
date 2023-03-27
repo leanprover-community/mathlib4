leanprover-community/mathlib4

commit 1dbd6d226c891b79f23f77bc6a5959b9cdb96962

fix: get rid of AWK and its implementation differences from `start_port.sh` (#2839)
This PR replaces the AWK script used in `start_port.sh` – introduced in #1523 – with a Python equivalent that is insensitive to any implementation difference.

By not having to worry about implementations I believe this is a superior solution to the problem that #2794 tackles.



Co-authored-by: Parcly Taxel <reddeloostw@gmail.com>
Co-authored-by: Jeremy Tan Jie Rui <reddeloostw@gmail.com>

scripts/fix-line-breaks.py
@@ -1,0 +1,18 @@
+#!/usr/bin/env python3
+import sys
+from collections import deque
+
+lns = deque([], 2)
+with (open(sys.argv[1], "r", encoding="utf-8", newline="\n") as f,
+      open(sys.argv[2], "w", encoding="utf-8", newline="\n") as g):
+    for ln_raw in f:
+        ln = ln_raw.strip("\n")
+        lns.append(ln)
+        if len(lns) > 1 and lns[1] == "  by" and len(lns[0]) < 98 and not lns[0].lstrip().startswith("--"):
+            lns.pop()
+            lns[0] += " by"
+        elif len(lns) > 1:
+            print(lns[0], file=g)
+    lns.popleft()
+    for ln in lns:
+        print(ln, file=g)

scripts/start_port.sh
@@ -14,6 +14,7 @@ if [ ! $1 ] ; then
 fi
 
 # arguments
+root_path="$(pwd)"
 mathlib4_path="$1"
 
 case $mathlib4_path in
@@ -67,8 +68,7 @@ echo "Applying automated fixes"
     sed -i 's/Mathbin\./Mathlib\./g' "$mathlib4_path"
     sed -i '/^import/{s/[.]Gcd/.GCD/g; s/[.]Modeq/.ModEq/g; s/[.]Nary/.NAry/g; s/[.]Peq/.PEq/g; s/[.]Pfun/.PFun/g; s/[.]Pnat/.PNat/g; s/[.]Smul/.SMul/g; s/[.]Zmod/.ZMod/g; s/[.]Nnreal/.NNReal/g; s/[.]Ennreal/ENNReal/g}' "$mathlib4_path"
 
-    # awk script taken from https://github.com/leanprover-community/mathlib4/pull/1523
-    awk '{do {{if (match($0, "^  by$") && length(p) < 98 && (!(match(p, "^[ \t]*--.*$")))) {p=p " by";} else {if (NR!=1) {print p}; p=$0}}} while (getline == 1) if (getline==0) print p}' "$mathlib4_path" > "$mathlib4_path.tmp"
+    python3 "$root_path/scripts/fix-line-breaks.py" "$mathlib4_path" "$mathlib4_path.tmp"
     mv "$mathlib4_path.tmp" "$mathlib4_path"
 
     (echo "import $mathlib4_mod" ; cat Mathlib.lean) | LC_ALL=C sort | uniq > Mathlib.lean.tmp

commit 48b7f84fa0a7741d5124316b095b40d480ae72a9

feat: extend `fix-line-breaks.py` to deeper `by`s and `where`s (#2996)
This PR extends the `fix-line-breaks.py` script introduced in #2839 to handle `by` and `where` keywords that are deeper in a theorem, definition or the like.



Co-authored-by: Parcly Taxel <reddeloostw@gmail.com>

scripts/fix-line-breaks.py
@@ -8,10 +8,15 @@ with open(sys.argv[1], "r", encoding="utf-8", newline="\n") as f, \
     for ln_raw in f:
         ln = ln_raw.strip("\n")
         lns.append(ln)
-        if len(lns) > 1 and lns[1] == "  by" and len(lns[0]) < 98 and not lns[0].lstrip().startswith("--"):
+        if len(lns) <= 1:
+            continue
+        if lns[1].lstrip() == "by" and len(lns[0]) < 98 and not lns[0].lstrip().startswith("--"):
             lns.pop()
             lns[0] += " by"
-        elif len(lns) > 1:
+        elif lns[1].lstrip() == "where" and len(lns[0]) < 95 and not lns[0].lstrip().startswith("--"):
+            lns.pop()
+            lns[0] += " where"
+        else:
             print(lns[0], file=g)
     lns.popleft()
     for ln in lns: