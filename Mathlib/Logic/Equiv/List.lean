a3e5a3bd95e2eaae1b38d184586f52da7dc1f6ba
Eric Wieser <wieser.eric@gmail.com>
2023-03-27 15:40:18 +0000

    chore: update SHAs

--- Mathlib/Logic/Equiv/List.lean
+++ Mathlib/Logic/Equiv/List.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro
 
 ! This file was ported from Lean 3 source module logic.equiv.list
-! leanprover-community/mathlib commit 1126441d6bccf98c81214a0780c73d499f6721fe
+! leanprover-community/mathlib commit d11893b411025250c8e61ff2f12ccbd7ee35ab15
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
