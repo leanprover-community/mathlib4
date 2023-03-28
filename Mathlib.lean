ea3d33c5be5db67229f8074e69beaaf60cad4717
Moritz Doll <moritz.doll@googlemail.com>
2023-03-28 12:06:28 +0000

    feat: port Analysis.Convex.Hull (#3147)

--- Mathlib.lean
+++ Mathlib.lean
@@ -274,6 +274,7 @@ import Mathlib.Algebra.Tropical.Basic
 import Mathlib.Algebra.Tropical.BigOperators
 import Mathlib.Algebra.Tropical.Lattice
 import Mathlib.Analysis.Convex.Basic
+import Mathlib.Analysis.Convex.Hull
 import Mathlib.Analysis.Convex.Segment
 import Mathlib.Analysis.Convex.Star
 import Mathlib.Analysis.Normed.Field.Basic
