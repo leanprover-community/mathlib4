1f088f32f6d56f429677e703f9e35035a73092a7
zhangir-azerbayev <zazerbayev@gmail.com>
2023-03-28 14:34:50 -0400

    fixed indentation

--- test/ChatGPT/amc12a_2019_p21.lean
+++ test/ChatGPT/amc12a_2019_p21.lean
@@ -5,8 +5,6 @@ import Mathlib.Data.Complex.Basic
 
 open BigOperators Complex
 
-theorem amc12a_2019_p21
-  (z : ℂ)
-  (h₀ : z = (1 + Complex.I) / Real.sqrt 2) :
+theorem amc12a_2019_p21 (z : ℂ) (h₀ : z = (1 + Complex.I) / Real.sqrt 2) :
   (∑ k in Finset.Icc 1 12, (z^(k^2))) * (∑ k in Finset.Icc 1 12, (1 / z^(k^2))) = 36 := by
   gpt
