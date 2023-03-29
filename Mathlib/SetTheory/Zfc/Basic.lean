6f1f269c36a677b2567b19e088dc1a81c33a59d7
Violeta Hernández <vi.hdz.p@gmail.com>
2023-03-29 10:37:34 -0600

    port complete!

--- Mathlib/SetTheory/Zfc/Basic.lean
+++ Mathlib/SetTheory/Zfc/Basic.lean
@@ -1332,8 +1332,12 @@ def funs (x y : ZFSet.{u}) : ZFSet.{u} :=
 theorem mem_funs {x y f : ZFSet.{u}} : f ∈ funs x y ↔ IsFunc x y f := by simp [funs, IsFunc]
 #align Set.mem_funs ZFSet.mem_funs
 
+/- Porting note: the `Definable` argument in `mapDefinableAux` is unused, though the TODO remark
+   suggests it shouldn't be. -/
+set_option linter.unusedVariables false
+
 -- TODO(Mario): Prove this computably
-noncomputable instance mapDefinableAux (f : ZFSet → ZFSet) [_H :Definable 1 f] :
+noncomputable instance mapDefinableAux (f : ZFSet → ZFSet) [Definable 1 f] :
     Definable 1 fun y => pair y (f y) :=
   @Classical.AllDefinable 1 _
 #align Set.map_definable_aux ZFSet.mapDefinableAux
@@ -1438,6 +1442,7 @@ theorem ext_iff {x y : Class.{u}} : x = y ↔ ∀ z, x z ↔ y z :=
 #align Class.ext_iff Class.ext_iff
 
 /-- Coerce a ZFC set into a class -/
+@[coe]
 def ofSet (x : ZFSet.{u}) : Class.{u} :=
   { y | y ∈ x }
 #align Class.of_Set Class.ofSet
@@ -1574,4 +1579,192 @@ theorem ofSet.inj {x y : ZFSet.{u}} (h : (x : Class.{u}) = y) : x = y :=
     rw [h]
 #align Class.of_Set.inj Class.ofSet.inj
 
-#lint
+@[simp]
+theorem toSet_of_setCat (A : Class.{u}) (x : ZFSet.{u}) : ToSet A x ↔ A x :=
+  ⟨fun ⟨y, yx, py⟩ => by rwa [ofSet.inj yx] at py, fun px => ⟨x, rfl, px⟩⟩
+#align Class.to_Set_of_Set Class.toSet_of_setCat
+
+@[simp, norm_cast]
+theorem coe_mem {x : ZFSet.{u}} {A : Class.{u}} : ↑x ∈ A ↔ A x :=
+  toSet_of_setCat _ _
+#align Class.coe_mem Class.coe_mem
+
+@[simp]
+theorem coe_apply {x y : ZFSet.{u}} : (y : Class.{u}) x ↔ x ∈ y :=
+  Iff.rfl
+#align Class.coe_apply Class.coe_apply
+
+@[simp, norm_cast]
+theorem coe_subset (x y : ZFSet.{u}) : (x : Class.{u}) ⊆ y ↔ x ⊆ y :=
+  Iff.rfl
+#align Class.coe_subset Class.coe_subset
+
+@[simp, norm_cast]
+theorem coe_sep (p : Class.{u}) (x : ZFSet.{u}) :
+    (ZFSet.sep p x : Class) = { y ∈ x | p y } :=
+  ext fun _ => ZFSet.mem_sep
+#align Class.coe_sep Class.coe_sep
+
+@[simp, norm_cast]
+theorem coe_empty : ↑(∅ : ZFSet.{u}) = (∅ : Class.{u}) :=
+  ext fun y => iff_false_iff.2 <| ZFSet.not_mem_empty y
+#align Class.coe_empty Class.coe_empty
+
+@[simp, norm_cast]
+theorem coe_insert (x y : ZFSet.{u}) : ↑(insert x y) = @insert ZFSet.{u} Class.{u} _ x y :=
+  ext fun _ => ZFSet.mem_insert_iff
+#align Class.coe_insert Class.coe_insert
+
+@[simp, norm_cast]
+theorem coe_union (x y : ZFSet.{u}) : ↑(x ∪ y) = (x : Class.{u}) ∪ y :=
+  ext fun _ => ZFSet.mem_union
+#align Class.coe_union Class.coe_union
+
+@[simp, norm_cast]
+theorem coe_inter (x y : ZFSet.{u}) : ↑(x ∩ y) = (x : Class.{u}) ∩ y :=
+  ext fun _ => ZFSet.mem_inter
+#align Class.coe_inter Class.coe_inter
+
+@[simp, norm_cast]
+theorem coe_diff (x y : ZFSet.{u}) : ↑(x \ y) = (x : Class.{u}) \ y :=
+  ext fun _ => ZFSet.mem_diff
+#align Class.coe_diff Class.coe_diff
+
+@[simp, norm_cast]
+theorem coe_powerset (x : ZFSet.{u}) : ↑x.powerset = powerset.{u} x :=
+  ext fun _ => ZFSet.mem_powerset
+#align Class.coe_powerset Class.coe_powerset
+
+@[simp]
+theorem powerset_apply {A : Class.{u}} {x : ZFSet.{u}} : powerset A x ↔ ↑x ⊆ A :=
+  Iff.rfl
+#align Class.powerset_apply Class.powerset_apply
+
+@[simp]
+theorem unionₛ_apply {x : Class} {y : ZFSet} : (⋃₀ x) y ↔ ∃ z : ZFSet, x z ∧ y ∈ z :=
+  by
+  constructor
+  · rintro ⟨-, ⟨z, rfl, hxz⟩, hyz⟩
+    exact ⟨z, hxz, hyz⟩
+  · exact fun ⟨z, hxz, hyz⟩ => ⟨_, coe_mem.2 hxz, hyz⟩
+#align Class.sUnion_apply Class.unionₛ_apply
+
+/- ./././Mathport/Syntax/Translate/Expr.lean:177:8: unsupported: ambiguous notation -/
+/- ./././Mathport/Syntax/Translate/Expr.lean:177:8: unsupported: ambiguous notation -/
+@[simp, norm_cast]
+theorem coe_unionₛ (x : ZFSet.{u}) : ↑(⋃₀ x : ZFSet) = ⋃₀ (x : Class.{u}) :=
+  ext fun y =>
+    ZFSet.mem_unionₛ.trans (unionₛ_apply.trans <| by rfl).symm
+#align Class.coe_sUnion Class.coe_unionₛ
+
+/- ./././Mathport/Syntax/Translate/Expr.lean:177:8: unsupported: ambiguous notation -/
+@[simp]
+theorem mem_sUnion {x y : Class.{u}} : y ∈ ⋃₀ x ↔ ∃ z, z ∈ x ∧ y ∈ z :=
+  by
+  constructor
+  · rintro ⟨w, rfl, z, hzx, hwz⟩
+    exact ⟨z, hzx, coe_mem.2 hwz⟩
+  · rintro ⟨w, hwx, z, rfl, hwz⟩
+    exact ⟨z, rfl, w, hwx, hwz⟩
+#align Class.mem_sUnion Class.mem_sUnion
+
+/- ./././Mathport/Syntax/Translate/Expr.lean:177:8: unsupported: ambiguous notation -/
+@[simp]
+theorem sUnion_empty : ⋃₀ (∅ : Class.{u}) = (∅ : Class.{u}) :=
+  by
+  ext
+  simp
+#align Class.sUnion_empty Class.sUnion_empty
+
+/-- An induction principle for sets. If every subset of a class is a member, then the class is
+  universal. -/
+theorem eq_univ_of_powerset_subset {A : Class} (hA : powerset A ⊆ A) : A = univ :=
+  eq_univ_of_forall
+    (by
+      by_contra' hnA
+      exact
+        WellFounded.min_mem ZFSet.mem_wf _ hnA
+          (hA fun x hx =>
+            Classical.not_not.1 fun hB =>
+              WellFounded.not_lt_min ZFSet.mem_wf _ hnA hB <| coe_apply.1 hx))
+#align Class.eq_univ_of_powerset_subset Class.eq_univ_of_powerset_subset
+
+/- ./././Mathport/Syntax/Translate/Expr.lean:177:8: unsupported: ambiguous notation -/
+/-- The definite description operator, which is `{x}` if `{y | A y} = {x}` and `∅` otherwise. -/
+def iota (A : Class) : Class :=
+  ⋃₀ { x | ∀ y, A y ↔ y = x }
+#align Class.iota Class.iota
+
+theorem iota_val (A : Class) (x : ZFSet) (H : ∀ y, A y ↔ y = x) : iota A = ↑x :=
+  ext fun y =>
+    ⟨fun ⟨_, ⟨x', rfl, h⟩, yx'⟩ => by rwa [← (H x').1 <| (h x').2 rfl], fun yx =>
+      ⟨_, ⟨x, rfl, H⟩, yx⟩⟩
+#align Class.iota_val Class.iota_val
+
+/-- Unlike the other set constructors, the `iota` definite descriptor
+  is a set for any set input, but not constructively so, so there is no
+  associated `Class → Set` function. -/
+theorem iota_ex (A) : iota.{u} A ∈ univ.{u} :=
+  mem_univ.2 <|
+    Or.elim (Classical.em <| ∃ x, ∀ y, A y ↔ y = x) (fun ⟨x, h⟩ => ⟨x, Eq.symm <| iota_val A x h⟩)
+      fun hn =>
+      ⟨∅, ext fun _ => coe_empty.symm ▸ ⟨False.rec, fun ⟨_, ⟨x, rfl, H⟩, _⟩ => hn ⟨x, H⟩⟩⟩
+#align Class.iota_ex Class.iota_ex
+
+/-- Function value -/
+def fval (F A : Class.{u}) : Class.{u} :=
+  iota fun y => ToSet (fun x => F (ZFSet.pair x y)) A
+#align Class.fval Class.fval
+
+-- mathport name: «expr ′ »
+infixl:100 " ′ " => fval
+
+theorem fval_ex (F A : Class.{u}) : F ′ A ∈ univ.{u} :=
+  iota_ex _
+#align Class.fval_ex Class.fval_ex
+
+end Class
+
+namespace ZFSet
+
+@[simp]
+theorem map_fval {f : ZFSet.{u} → ZFSet.{u}} [H : PSet.Definable 1 f] {x y : ZFSet.{u}}
+    (h : y ∈ x) : (ZFSet.map f x ′ y : Class.{u}) = f y :=
+  Class.iota_val _ _ fun z =>
+    by
+    rw [Class.toSet_of_setCat, Class.coe_apply, mem_map]
+    exact
+      ⟨fun ⟨w, _, pr⟩ => by
+        let ⟨wy, fw⟩ := ZFSet.pair_injective pr
+        rw [← fw, wy], fun e => by
+        subst e
+        exact ⟨_, h, rfl⟩⟩
+#align Set.map_fval ZFSet.map_fval
+
+variable (x : ZFSet.{u}) (h : ∅ ∉ x)
+
+/-- A choice function on the class of nonempty ZFC sets. -/
+noncomputable def choice : ZFSet :=
+  @map (fun y => Classical.epsilon fun z => z ∈ y) (Classical.AllDefinable _) x
+#align Set.choice ZFSet.choice
+
+theorem choice_mem_aux (y : ZFSet.{u}) (yx : y ∈ x) :
+    (Classical.epsilon fun z : ZFSet.{u} => z ∈ y) ∈ y :=
+  (@Classical.epsilon_spec _ fun z : ZFSet.{u} => z ∈ y) <|
+    by_contradiction fun n => h <| by rwa [← (eq_empty y).2 fun z zx => n ⟨z, zx⟩]
+#align Set.choice_mem_aux ZFSet.choice_mem_aux
+
+/- ./././Mathport/Syntax/Translate/Expr.lean:177:8: unsupported: ambiguous notation -/
+theorem choice_isFunc : IsFunc x (⋃₀ x) (choice x) :=
+  (@map_isFunc _ (Classical.AllDefinable _) _ _).2 fun y yx =>
+    mem_unionₛ.2 ⟨y, yx, choice_mem_aux x h y yx⟩
+#align Set.choice_is_func ZFSet.choice_isFunc
+
+theorem choice_mem (y : ZFSet.{u}) (yx : y ∈ x) : (choice x ′ y : Class.{u}) ∈ (y : Class.{u}) :=
+  by
+  delta choice
+  rw [@map_fval _ (Classical.AllDefinable _) x y yx, Class.coe_mem, Class.coe_apply]
+  exact choice_mem_aux x h y yx
+#align Set.choice_mem ZFSet.choice_mem
+
+end ZFSet
