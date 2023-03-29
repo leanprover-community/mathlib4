4a892278066da3d8655908958ef93fb2f2556230
Scott Morrison <scott.morrison@gmail.com>
2023-03-30 08:52:43 +1100

    copyright headers

--- Mathlib/Tactic/GPT/Sagredo/Dialog.lean
+++ Mathlib/Tactic/GPT/Sagredo/Dialog.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2023 Scott Morrison. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Scott Morrison, Zhangir Azerbayev
+-/
 import Mathlib.Tactic.GPT.Sagredo.Monad
 
 open Lean
--- Mathlib/Tactic/GPT/String.lean
+++ Mathlib/Tactic/GPT/String.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2023 Scott Morrison. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Scott Morrison
+-/
 -- TODO move all these
 
 def String.stripPrefix (s p : String) :=
