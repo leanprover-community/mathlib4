0a5f1759b67bc27b3f4e76535ac9cd3485cf43b3
Parcly Taxel <reddeloostw@gmail.com>
2023-03-29 18:10:42 +0800

    …and everything else

--- Mathlib.lean
+++ Mathlib.lean
@@ -326,7 +326,7 @@ import Mathlib.CategoryTheory.Category.Pointed
 import Mathlib.CategoryTheory.Category.Preorder
 import Mathlib.CategoryTheory.Category.QuivCat
 import Mathlib.CategoryTheory.Category.RelCat
-import Mathlib.CategoryTheory.Category.Twop
+import Mathlib.CategoryTheory.Category.TwoP
 import Mathlib.CategoryTheory.Category.ULift
 import Mathlib.CategoryTheory.Closed.Monoidal
 import Mathlib.CategoryTheory.CommSq
