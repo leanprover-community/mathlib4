a49681564cae61a94558bc0a7e0b4ff9b3b0c48d
Parcly Taxel <reddeloostw@gmail.com>
2023-03-30 11:32:26 +0800

    automated fixes
    Mathbin -> Mathlib
    fix certain import statements
    move "by" to end of line
    add import to Mathlib.lean

--- Mathlib/Order/UpperLower/LocallyFinite.lean
+++ Mathlib/Order/UpperLower/LocallyFinite.lean
@@ -8,8 +8,8 @@ Authors: Yaël Dillies
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
-import Mathbin.Order.LocallyFinite
-import Mathbin.Order.UpperLower.Basic
+import Mathlib.Order.LocallyFinite
+import Mathlib.Order.UpperLower.Basic
 
 /-!
 # Upper and lower sets in a locally finite order
