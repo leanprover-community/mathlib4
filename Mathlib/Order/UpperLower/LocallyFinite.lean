bdac464785ac2d7f839c5086ad37fbe760542cfb
Parcly Taxel <reddeloostw@gmail.com>
2023-03-30 11:42:51 +0800

    Fix errors

--- Mathlib/Order/UpperLower/LocallyFinite.lean
+++ Mathlib/Order/UpperLower/LocallyFinite.lean
@@ -14,7 +14,7 @@ import Mathlib.Order.UpperLower.Basic
 /-!
 # Upper and lower sets in a locally finite order
 
-In this file we characterise the interaction of `upper_set`/`lower_set` and `locally_finite_order`.
+In this file we characterise the interaction of `UpperSet`/`LowerSet` and `LocallyFiniteOrder`.
 -/
 
 
@@ -25,14 +25,13 @@ variable {α : Type _} [Preorder α] {s : Set α}
 protected theorem Finite.upperClosure [LocallyFiniteOrderTop α] (hs : s.Finite) :
     (upperClosure s : Set α).Finite := by
   rw [coe_upperClosure]
-  exact hs.bUnion fun _ _ => finite_Ici _
+  exact hs.bunionᵢ fun _ _ => finite_Ici _
 #align set.finite.upper_closure Set.Finite.upperClosure
 
 protected theorem Finite.lowerClosure [LocallyFiniteOrderBot α] (hs : s.Finite) :
     (lowerClosure s : Set α).Finite := by
   rw [coe_lowerClosure]
-  exact hs.bUnion fun _ _ => finite_Iic _
+  exact hs.bunionᵢ fun _ _ => finite_Iic _
 #align set.finite.lower_closure Set.Finite.lowerClosure
 
 end Set
-
