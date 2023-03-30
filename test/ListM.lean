dea0bef312c855069813e19756b04d3d3dc90403
Scott Morrison <scott.morrison@gmail.com>
2023-03-30 14:39:11 +1100

    fix import

--- test/ListM.lean
+++ test/ListM.lean
@@ -3,7 +3,7 @@ Copyright (c) 2019 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison
 -/
-import Mathlib.Data.ListM
+import Mathlib.Data.ListM.Basic
 
 @[reducible] def S (α : Type) := StateT (List Nat) Option α
 def append (x : Nat) : S Unit :=
