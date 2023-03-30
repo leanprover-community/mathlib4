ac1e75f550b4f6486f1aa7c4799216f4f43ed2fc
agjftucker <agjftucker@gmail.com>
2023-03-30 13:54:48 +0000

    refactor: hypothesis naming in custom recursors (#1658)
    I have renamed hypotheses in `WithBot.recBotCoe`, `WithTop.recTopCoe` and `Finset.cons_induction` on the basis that rather than
    ```lean
    cases f₁ using WithBot.recBotCoe with
    | h₁ => …
    | h₂ f₁ => …
    ```
    we would prefer to be able to write
    ```lean
    cases f₁ using WithBot.recBotCoe with
    | bot => …
    | coe f₁ => …
    ```
    and rather than
    ```lean
    induction s using cons_induction with
    | h₁ => …
    | @h₂ c s hc ih => …
    ```
    we would prefer
    ```lean
    induction s using cons_induction with
    | empty => …
    | @cons c t hc ih => …
    ````
    I also tidied up some of inf' stuff in Finset.Lattice by using named arguments to specify the dual type instead of `@`s and `_ _ _`.

--- Mathlib/Data/Multiset/Basic.lean
+++ Mathlib/Data/Multiset/Basic.lean
@@ -152,9 +152,9 @@ theorem cons_inj_right (a : α) : ∀ {s t : Multiset α}, a ::ₘ s = a ::ₘ t
 #align multiset.cons_inj_right Multiset.cons_inj_right
 
 @[recursor 5]
-protected theorem induction {p : Multiset α → Prop} (h₁ : p 0)
-    (h₂ : ∀ ⦃a : α⦄ {s : Multiset α}, p s → p (a ::ₘ s)) : ∀ s, p s := by
-  rintro ⟨l⟩; induction' l with _ _ ih <;> [exact h₁, exact h₂ ih]
+protected theorem induction {p : Multiset α → Prop} (empty : p 0)
+    (cons : ∀ ⦃a : α⦄ {s : Multiset α}, p s → p (a ::ₘ s)) : ∀ s, p s := by
+  rintro ⟨l⟩; induction' l with _ _ ih <;> [exact empty, exact cons ih]
 #align multiset.induction Multiset.induction
 
 @[elab_as_elim]
