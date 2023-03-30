b806e363f2272ae1646fe97f5933cac3bd168788
Scott Morrison <scott.morrison@gmail.com>
2023-03-30 21:26:53 +1100

    doc-strings

--- Mathlib/Util/REPL.lean
+++ Mathlib/Util/REPL.lean
@@ -7,20 +7,27 @@ namespace REPL
 
 section Json
 
+/-- Run Lean commands.
+If `env = none`, starts a new session (in which you can use `import`).
+If `env = some n`, builds on the existing environment `n`.
+-/
 structure Run where
   env : Option Nat
   body : String
 deriving ToJson, FromJson
 
+/-- Line and column information for error messages and sorries. -/
 structure Pos where
   line : Nat
   column : Nat
 deriving ToJson, FromJson
 
+/-- Severity of a message. -/
 inductive Severity
   | info | warning | error
 deriving ToJson, FromJson
 
+/-- A Lean message. -/
 structure Message where
   pos : Pos
   endPos : Option Pos
@@ -28,54 +35,64 @@ structure Message where
   data : String
 deriving ToJson, FromJson
 
+/-- Construct the JSON representation of a Lean message. -/
 def Message.of (m : Lean.Message) : IO Message := do pure <|
 { pos := ⟨m.pos.line, m.pos.column⟩,
   endPos := m.endPos.map fun p => ⟨p.line, p.column⟩,
   severity := match m.severity with
   | .information => .info
   | .warning => .warning
-  | .error => .warning,
+  | .error => .error,
   data := (← m.data.toString) }
 
+/-- A Lean `sorry`. -/
 structure Sorry where
   pos : Pos
   endPos : Pos
   goal : String
 deriving ToJson, FromJson
 
+/-- Construct the JSON representation of a Lean sorry. -/
 def Sorry.of (ctx : ContextInfo) (g : MVarId) (pos endPos : Lean.Position) :
     IO Sorry := do pure <|
 { pos := ⟨pos.line, pos.column⟩,
   endPos := ⟨endPos.line, endPos.column⟩,
   goal := s!"{(← ctx.ppGoals [g])}" }
 
+/--
+A response to a Lean command.
+`env` can be used in later calls, to build on the stored environment.
+-/
 structure Response where
-  id : Nat
+  env : Nat
   messages : List Message
   sorries : List Sorry
 deriving ToJson, FromJson
 
+/-- Json wrapper for an error. -/
 structure Error where
   message : String
 deriving ToJson, FromJson
 
 end Json
 
-
 open Mathlib.Tactic.GPT.Sagredo
 
+/-- The monadic state for the Lean REPL. -/
 structure State where
   environments : Array Environment
   lines : Array Nat
 
+/-- The Lean REPL monad. -/
 abbrev M (m : Type → Type) := StateT State m
 
-variable [Monad m]
-variable [MonadLiftT IO m] [MonadLiftT CoreM m]
+variable [Monad m] [MonadLiftT IO m]
 
+/-- Get the next available id for a new environment. -/
 def nextId : M m Nat := do pure (← get).environments.size
 
-def run (s : Run) : M IO Response := do
+/-- Run a command, returning the id of the new environment, and any messages and sorries. -/
+def run (s : Run) : M m Response := do
   let env? := s.env.bind ((← get).environments[·]?)
   let (env, messages, trees) ← processInput s.body env? {} ""
   let messages ← messages.mapM fun m => Message.of m
@@ -96,7 +113,7 @@ unsafe def getLines : IO String := do
   | "\n" => pure "\n"
   | line => pure <| line ++ (← getLines)
 
-/-- Read-eval-print loop for the GPT monad. -/
+/-- Read-eval-print loop for Lean. -/
 unsafe def repl : IO Unit :=
   StateT.run' loop ⟨#[], #[]⟩
 where loop : M IO Unit := do
