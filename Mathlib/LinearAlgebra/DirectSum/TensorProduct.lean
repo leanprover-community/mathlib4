b03327460b4fbd8cc1f2806c47c2a154be109588
Parcly Taxel <reddeloostw@gmail.com>
2023-03-31 05:30:51 +0800

    Linting and style

--- Mathlib/LinearAlgebra/DirectSum/TensorProduct.lean
+++ Mathlib/LinearAlgebra/DirectSum/TensorProduct.lean
@@ -51,8 +51,8 @@ variable [∀ i₂, AddCommGroup (M₂ i₂)] [AddCommGroup M₂']
 
 variable [∀ i₁, Module R (M₁ i₁)] [Module R M₁'] [∀ i₂, Module R (M₂ i₂)] [Module R M₂']
 
--- Porting note: usual problem of not syntheising RingHomInvPair id id
-set_option synthInstance.etaExperiment true in
+set_option synthInstance.etaExperiment true -- Porting note: gets around lean4#2074
+
 /-- The linear equivalence `(⨁ i₁, M₁ i₁) ⊗ (⨁ i₂, M₂ i₂) ≃ (⨁ i₁, ⨁ i₂, M₁ i₁ ⊗ M₂ i₂)`, i.e.
 "tensor product distributes over direct sum". -/
 protected def directSum :
@@ -71,7 +71,8 @@ protected def directSum :
     refine TensorProduct.ext ?_
     refine LinearMap.ext₂ fun m₁ m₂ => ?_
     -- porting note: seems much nicer than the `repeat` lean 3 proof.
-    simp only [compr₂_apply, comp_apply, id_apply, mk_apply, DirectSum.toModule_lof, map_tmul, lift.tmul, flip_apply, curry_apply]
+    simp only [compr₂_apply, comp_apply, id_apply, mk_apply, DirectSum.toModule_lof, map_tmul,
+        lift.tmul, flip_apply, curry_apply]
   · -- `(_)` prevents typeclass search timing out on problems that can be solved immediately by
     -- unification
     refine TensorProduct.ext ?_
@@ -80,7 +81,8 @@ protected def directSum :
     refine @DirectSum.linearMap_ext R _ _ _ _ _ _ _ _ (_) _ _ fun i₂ => ?_
     refine LinearMap.ext fun x₂ => ?_
     -- porting note: seems much nicer than the `repeat` lean 3 proof.
-    simp only [compr₂_apply, comp_apply, id_apply, mk_apply, DirectSum.toModule_lof, map_tmul, lift.tmul, flip_apply, curry_apply]
+    simp only [compr₂_apply, comp_apply, id_apply, mk_apply, DirectSum.toModule_lof, map_tmul,
+        lift.tmul, flip_apply, curry_apply]
   /- was:
 
     refine'
@@ -122,8 +124,6 @@ protected def directSum :
 -/
 #align tensor_product.direct_sum TensorProduct.directSum
 
--- Porting note: again cannot synthesize RingHomInvPair
-set_option synthInstance.etaExperiment true in
 /-- Tensor products distribute over a direct sum on the left . -/
 def directSumLeft : (⨁ i₁, M₁ i₁) ⊗[R] M₂' ≃ₗ[R] ⨁ i, M₁ i ⊗[R] M₂' :=
   LinearEquiv.ofLinear
@@ -133,22 +133,18 @@ def directSumLeft : (⨁ i₁, M₁ i₁) ⊗[R] M₂' ≃ₗ[R] ⨁ i, M₁ i �
     (DirectSum.toModule R _ _ fun i => rtensor _ (DirectSum.lof R ι₁ _ _))
     (DirectSum.linearMap_ext R fun i =>
       TensorProduct.ext <|
-        LinearMap.ext₂ fun m₁ m₂ =>
-          by
+        LinearMap.ext₂ fun m₁ m₂ => by
           dsimp only [comp_apply, compr₂_apply, id_apply, mk_apply]
           simp_rw [DirectSum.toModule_lof, rtensor_tmul, lift.tmul, DirectSum.toModule_lof,
             compr₂_apply, mk_apply])
     (TensorProduct.ext <|
       DirectSum.linearMap_ext R fun i =>
-        LinearMap.ext₂ fun m₁ m₂ =>
-          by
+        LinearMap.ext₂ fun m₁ m₂ => by
           dsimp only [comp_apply, compr₂_apply, id_apply, mk_apply]
           simp_rw [DirectSum.toModule_lof, lift.tmul, DirectSum.toModule_lof, compr₂_apply,
             mk_apply, DirectSum.toModule_lof, rtensor_tmul])
 #align tensor_product.direct_sum_left TensorProduct.directSumLeft
 
--- Porting note: cannot synthesize RingHomInvPair id id
-set_option synthInstance.etaExperiment true in
 /-- Tensor products distribute over a direct sum on the right. -/
 def directSumRight : (M₁' ⊗[R] ⨁ i, M₂ i) ≃ₗ[R] ⨁ i, M₁' ⊗[R] M₂ i :=
   TensorProduct.comm R _ _ ≪≫ₗ directSumLeft R M₂ M₁' ≪≫ₗ
@@ -157,53 +153,43 @@ def directSumRight : (M₁' ⊗[R] ⨁ i, M₂ i) ≃ₗ[R] ⨁ i, M₁' ⊗[R] 
 
 variable {M₁ M₁' M₂ M₂'}
 
--- Porting note: cannot find coercion to functions
-set_option synthInstance.etaExperiment true in
 @[simp]
 theorem directSum_lof_tmul_lof (i₁ : ι₁) (m₁ : M₁ i₁) (i₂ : ι₂) (m₂ : M₂ i₂) :
     TensorProduct.directSum R M₁ M₂ (DirectSum.lof R ι₁ M₁ i₁ m₁ ⊗ₜ DirectSum.lof R ι₂ M₂ i₂ m₂) =
-      DirectSum.lof R (ι₁ × ι₂) (fun i => M₁ i.1 ⊗[R] M₂ i.2) (i₁, i₂) (m₁ ⊗ₜ m₂) :=
-  by simp [TensorProduct.directSum]
+      DirectSum.lof R (ι₁ × ι₂) (fun i => M₁ i.1 ⊗[R] M₂ i.2) (i₁, i₂) (m₁ ⊗ₜ m₂) := by
+  simp [TensorProduct.directSum]
 #align tensor_product.direct_sum_lof_tmul_lof TensorProduct.directSum_lof_tmul_lof
 
--- Porting note: cannot find coercion to functions
-set_option synthInstance.etaExperiment true in
 @[simp]
 theorem directSumLeft_tmul_lof (i : ι₁) (x : M₁ i) (y : M₂') :
-    directSumLeft R M₁ M₂' (DirectSum.lof R _ _ i x ⊗ₜ[R] y) = DirectSum.lof R _ _ i (x ⊗ₜ[R] y) :=
-  by
+    directSumLeft R M₁ M₂' (DirectSum.lof R _ _ i x ⊗ₜ[R] y) =
+    DirectSum.lof R _ _ i (x ⊗ₜ[R] y) := by
   dsimp only [directSumLeft, LinearEquiv.ofLinear_apply, lift.tmul]
   rw [DirectSum.toModule_lof R i]
   rfl
 #align tensor_product.direct_sum_left_tmul_lof TensorProduct.directSumLeft_tmul_lof
 
--- Porting note: cannot find coercion to functions
-set_option synthInstance.etaExperiment true in
 @[simp]
 theorem directSumLeft_symm_lof_tmul (i : ι₁) (x : M₁ i) (y : M₂') :
     (directSumLeft R M₁ M₂').symm (DirectSum.lof R _ _ i (x ⊗ₜ[R] y)) =
-      DirectSum.lof R _ _ i x ⊗ₜ[R] y :=
-  by rw [LinearEquiv.symm_apply_eq, directSumLeft_tmul_lof]
+      DirectSum.lof R _ _ i x ⊗ₜ[R] y := by
+  rw [LinearEquiv.symm_apply_eq, directSumLeft_tmul_lof]
 #align tensor_product.direct_sum_left_symm_lof_tmul TensorProduct.directSumLeft_symm_lof_tmul
 
--- Porting note: cannot find coercion to functions
-set_option synthInstance.etaExperiment true in
 @[simp]
 theorem directSumRight_tmul_lof (x : M₁') (i : ι₂) (y : M₂ i) :
-    directSumRight R M₁' M₂ (x ⊗ₜ[R] DirectSum.lof R _ _ i y) = DirectSum.lof R _ _ i (x ⊗ₜ[R] y) :=
-  by
+    directSumRight R M₁' M₂ (x ⊗ₜ[R] DirectSum.lof R _ _ i y) =
+    DirectSum.lof R _ _ i (x ⊗ₜ[R] y) := by
   dsimp only [directSumRight, LinearEquiv.trans_apply, TensorProduct.comm_tmul]
   rw [directSumLeft_tmul_lof]
   exact Dfinsupp.mapRange_single (hf := fun _ => rfl)
 #align tensor_product.direct_sum_right_tmul_lof TensorProduct.directSumRight_tmul_lof
 
--- Porting note: cannot find coercion to functions
-set_option synthInstance.etaExperiment true in
 @[simp]
 theorem directSumRight_symm_lof_tmul (x : M₁') (i : ι₂) (y : M₂ i) :
     (directSumRight R M₁' M₂).symm (DirectSum.lof R _ _ i (x ⊗ₜ[R] y)) =
-      x ⊗ₜ[R] DirectSum.lof R _ _ i y :=
-  by rw [LinearEquiv.symm_apply_eq, directSumRight_tmul_lof]
+      x ⊗ₜ[R] DirectSum.lof R _ _ i y := by
+  rw [LinearEquiv.symm_apply_eq, directSumRight_tmul_lof]
 #align tensor_product.direct_sum_right_symm_lof_tmul TensorProduct.directSumRight_symm_lof_tmul
 
 end TensorProduct
