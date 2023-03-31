67ce1df31199feb74021e46634ca05d63c7ef291
Joël Riou <joel.riou@universite-paris-saclay.fr>
2023-03-31 12:42:14 +0200

    some progress

--- Mathlib/CategoryTheory/Internal/Preadditive.lean
+++ Mathlib/CategoryTheory/Internal/Preadditive.lean
@@ -12,7 +12,7 @@ namespace Preadditive
 variable {C : Type u} [Category.{v} C] (G : C ⥤ Internal AddCommGroupCat C)
   (iso : G ⋙ Internal.objFunctor _ _ ≅ 𝟭 C)
 
-def ofInternalAddCommGroup : Preadditive C := by
+def ofInternalAddCommGroupCat : Preadditive C := by
   letI : ∀ (P Q : C), AddCommGroup (P ⟶ Q) := fun P Q =>
     Internal.addCommGroup (Internal.ofIsoObj (G.obj Q) (iso.app Q)) (Opposite.op P)
   exact
@@ -28,9 +28,26 @@ def ofInternalAddCommGroup : Preadditive C := by
         (Internal.addCommGroup_addMonoidHom
           (Internal.ofIsoObj (G.obj Q) (iso.app Q)) f.op).map_add g g' }
 
-variable {X Y : C} (f : X ⟶ Y)
+variable (C)
 
-open Opposite
+def toInternalAddCommGroupCatFunctor [Preadditive C] : C ⥤ Internal AddCommGroupCat C where
+  obj X :=
+  { obj := X
+    presheaf :=
+    { obj := fun Y => AddCommGroupCat.of (Y.unop ⟶ X)
+      map := fun f => AddCommGroupCat.ofHom
+        (AddMonoidHom.mk' (fun g => f.unop ≫ g) (by simp)) }
+    iso := Iso.refl _ }
+  map f :=
+  { app := fun Z => by
+      dsimp
+      exact AddCommGroupCat.ofHom (AddMonoidHom.mk' (fun g => g ≫ f) (by simp)) }
+  map_comp := fun f g => by
+    apply NatTrans.ext
+    ext1 Z
+    ext1
+    exact (Category.assoc _ _ _).symm
+
 
 end Preadditive
 
