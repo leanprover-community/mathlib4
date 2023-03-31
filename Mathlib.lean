3ba9781d59ad03c29c91c231a37e26a36fc61cad
Jason Yuen <jason_yuen2007@hotmail.com>
2023-03-31 10:28:01 +0000

    feat: port RingTheory.Adjoin.Fg (#3199)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1367,6 +1367,7 @@ import Mathlib.Order.Zorn
 import Mathlib.Order.ZornAtoms
 import Mathlib.RepresentationTheory.Maschke
 import Mathlib.RingTheory.Adjoin.Basic
+import Mathlib.RingTheory.Adjoin.Fg
 import Mathlib.RingTheory.AlgebraTower
 import Mathlib.RingTheory.Congruence
 import Mathlib.RingTheory.Coprime.Basic
