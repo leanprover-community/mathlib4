045e145760354176d4f30a92fc9e605c59939f8f
Scott Morrison <scott.morrison@gmail.com>
2023-04-02 08:09:52 +1000

    import all files

--- Mathlib.lean
+++ Mathlib.lean
@@ -768,7 +768,9 @@ import Mathlib.Data.List.Sort
 import Mathlib.Data.List.Sublists
 import Mathlib.Data.List.TFAE
 import Mathlib.Data.List.Zip
-import Mathlib.Data.ListM
+import Mathlib.Data.ListM.Basic
+import Mathlib.Data.ListM.BestFirst
+import Mathlib.Data.ListM.DepthFirst
 import Mathlib.Data.Matrix.DMatrix
 import Mathlib.Data.Multiset.Antidiagonal
 import Mathlib.Data.Multiset.Basic
