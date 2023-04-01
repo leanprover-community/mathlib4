196e4a1e0784e8b92e8c931830448ee0d03b8927
Jeremy Tan Jie Rui <e0191785@u.nus.edu>
2023-04-01 22:20:51 +0000

    feat: port LinearAlgebra.DirectSum.Finsupp (#3205)
    Co-authored-by: Parcly Taxel <reddeloostw@gmail.com>
    Co-authored-by: Jeremy Tan Jie Rui <reddeloostw@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1184,6 +1184,7 @@ import Mathlib.LinearAlgebra.Basis
 import Mathlib.LinearAlgebra.Basis.Bilinear
 import Mathlib.LinearAlgebra.BilinearMap
 import Mathlib.LinearAlgebra.Dfinsupp
+import Mathlib.LinearAlgebra.DirectSum.Finsupp
 import Mathlib.LinearAlgebra.DirectSum.TensorProduct
 import Mathlib.LinearAlgebra.Finsupp
 import Mathlib.LinearAlgebra.GeneralLinearGroup
