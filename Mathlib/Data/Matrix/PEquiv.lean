d619d01bba0432505141e0e3333d9dea75878d08
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2023-04-03 18:02:55 +0200

    better?

--- Mathlib/Data/Matrix/PEquiv.lean
+++ Mathlib/Data/Matrix/PEquiv.lean
@@ -51,9 +51,9 @@ variable {α : Type v}
 open Matrix
 
 -- Porting note: added
-instance [DecidableEq n] (j : n) (o : Option n) : Decidable (j ∈ o) := by
-  rw [←Option.mem_toList]
-  infer_instance
+local instance [DecidableEq n] (j : n) (o : Option n) : Decidable (j ∈ o) :=
+  haveI : Decidable (o = some j) := inferInstance
+  this
 
 /-- `toMatrix` returns a matrix containing ones and zeros. `f.toMatrix i j` is `1` if
   `f i = some j` and `0` otherwise -/
@@ -123,11 +123,9 @@ theorem toMatrix_trans [Fintype m] [DecidableEq m] [DecidableEq n] [Semiring α]
   cases f i <;> simp
 #align pequiv.to_matrix_trans PEquiv.toMatrix_trans
 
--- Porting note: proof was `rfl`
 @[simp]
 theorem toMatrix_bot [DecidableEq n] [Zero α] [One α] :
-    ((⊥ : PEquiv m n).toMatrix : Matrix m n α) = 0 := by
-  simp only [toMatrix, bot_apply, Option.mem_def, ite_false]
+    ((⊥ : PEquiv m n).toMatrix : Matrix m n α) = 0 :=
   rfl
 #align pequiv.to_matrix_bot PEquiv.toMatrix_bot
 
