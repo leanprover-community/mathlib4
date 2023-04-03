9111999d4dee4e8bd38a6400b034c6996fae5d58
Kyle Miller <kmill31415@gmail.com>
2023-04-02 19:05:38 -0700

    add ToExpr Expr

--- Mathlib/Tactic/ToExpr.lean
+++ Mathlib/Tactic/ToExpr.lean
@@ -43,11 +43,39 @@ end override
 namespace Mathlib
 open Lean
 
+deriving instance ToExpr for Int
+
 deriving instance ToExpr for ULift
 
 /-- Hand-written instance since `PUnit` is a `Sort` rather than a `Type`. -/
 instance [ToLevel.{u}] : ToExpr PUnit.{u+1} where
   toExpr _ := mkConst ``PUnit.unit [toLevel.{u+1}]
   toTypeExpr := mkConst ``PUnit [toLevel.{u+1}]
+
+open DataValue in
+private def toExprMData (md : MData) : Expr := Id.run do
+  let mut e := mkConst ``MData.empty
+  for (k, v) in md do
+    let k := toExpr k
+    e := match v with
+          | ofString v => mkApp3 (mkConst ``KVMap.setString) e k (mkStrLit v)
+          | ofBool v   => mkApp3 (mkConst ``KVMap.setBool) e k (toExpr v)
+          | ofName v   => mkApp3 (mkConst ``KVMap.setName) e k (toExpr v)
+          | ofNat v    => mkApp3 (mkConst ``KVMap.setNat) e k (mkNatLit v)
+          | ofInt v    => mkApp3 (mkConst ``KVMap.setInt) e k (toExpr v)
+          | ofSyntax _ => panic! "toExpr for Syntax is not implemented in toExprMData"
+  return e
+
+instance : ToExpr MData where
+  toExpr := toExprMData
+  toTypeExpr := mkConst ``MData
+
+deriving instance ToExpr for FVarId
+deriving instance ToExpr for MVarId
+deriving instance ToExpr for LevelMVarId
+deriving instance ToExpr for Level
+deriving instance ToExpr for BinderInfo
+deriving instance ToExpr for Literal
+deriving instance ToExpr for Expr
 
 end Mathlib
