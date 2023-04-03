3af50c24f6d2512ee20f2725e17dee755012bd8c
Kyle Miller <kmill31415@gmail.com>
2023-04-02 17:43:14 -0700

    missing copyright

--- Mathlib/Tactic/ToExpr.lean
+++ Mathlib/Tactic/ToExpr.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2023 Kyle Miller. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Kyle Miller
+-/
 import Mathlib.Tactic.DeriveToExpr
 
 /-! # `ToExpr` instances for Mathlib
