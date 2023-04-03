f85368f49bea5a60679f40625f31177479d7f732
arienmalec <arien.malec@gmail.com>
2023-04-03 15:27:52 +0000

    feat Port/Computability.RegularExpressions (#2306)
    port of computability.regular_expressions
    
    The previous file defined `attribute [match_pattern] Mul.mul` but this is not allowed in Lean 4

--- Mathlib.lean
+++ Mathlib.lean
@@ -561,6 +561,7 @@ import Mathlib.Computability.EpsilonNFA
 import Mathlib.Computability.Language
 import Mathlib.Computability.NFA
 import Mathlib.Computability.Primrec
+import Mathlib.Computability.RegularExpressions
 import Mathlib.Computability.TuringMachine
 import Mathlib.Control.Applicative
 import Mathlib.Control.Basic
