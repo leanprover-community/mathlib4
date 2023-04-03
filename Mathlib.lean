d3c757d089c923d4912d463490a2b9e5c9ea1949
ChrisHughes24 <chrishughes24@gmail.com>
2023-04-03 13:19:40 +0100

    automated fixes
    Mathbin -> Mathlib
    fix certain import statements
    move "by" to end of line
    add import to Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -1390,6 +1390,7 @@ import Mathlib.RingTheory.Coprime.Basic
 import Mathlib.RingTheory.Coprime.Ideal
 import Mathlib.RingTheory.Coprime.Lemmas
 import Mathlib.RingTheory.EuclideanDomain
+import Mathlib.RingTheory.FiniteType
 import Mathlib.RingTheory.Finiteness
 import Mathlib.RingTheory.Fintype
 import Mathlib.RingTheory.Flat
