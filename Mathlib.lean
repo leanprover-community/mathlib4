331e8758e488178c55ae2d3f11b9d0bd38e6c2eb
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2023-04-04 17:04:35 +0530

    feat: add Mathlib.Tactic import

--- Mathlib.lean
+++ Mathlib.lean
@@ -1453,6 +1453,7 @@ import Mathlib.SetTheory.Ordinal.FixedPoint
 import Mathlib.SetTheory.Ordinal.NaturalOps
 import Mathlib.SetTheory.Ordinal.Principal
 import Mathlib.SetTheory.Ordinal.Topology
+import Mathlib.Tactic
 import Mathlib.Tactic.Abel
 import Mathlib.Tactic.Alias
 import Mathlib.Tactic.ApplyFun
