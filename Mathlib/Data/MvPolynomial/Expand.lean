9e6186ce780060b8c292fd392a0dc79b4ed1ef23
int-y1 <jason_yuen2007@hotmail.com>
2023-04-04 02:14:04 -0700

    fix

--- Mathlib/Data/MvPolynomial/Expand.lean
+++ Mathlib/Data/MvPolynomial/Expand.lean
@@ -15,11 +15,11 @@ import Mathlib.Data.MvPolynomial.Monad
 
 Given a multivariate polynomial `φ`, one may replace every occurence of `X i` by `X i ^ n`,
 for some natural number `n`.
-This operation is called `mv_polynomial.expand` and it is an algebra homomorphism.
+This operation is called `MvPolynomial.expand` and it is an algebra homomorphism.
 
 ### Main declaration
 
-* `mv_polynomial.expand`: expand a polynomial by a factor of p, so `∑ aₙ xⁿ` becomes `∑ aₙ xⁿᵖ`.
+* `MvPolynomial.expand`: expand a polynomial by a factor of p, so `∑ aₙ xⁿ` becomes `∑ aₙ xⁿᵖ`.
 -/
 
 
@@ -31,21 +31,23 @@ variable {σ τ R S : Type _} [CommSemiring R] [CommSemiring S]
 
 /-- Expand the polynomial by a factor of p, so `∑ aₙ xⁿ` becomes `∑ aₙ xⁿᵖ`.
 
-See also `polynomial.expand`. -/
+See also `Polynomial.expand`. -/
 noncomputable def expand (p : ℕ) : MvPolynomial σ R →ₐ[R] MvPolynomial σ R :=
-  { (eval₂Hom C fun i => X i ^ p : MvPolynomial σ R →+* MvPolynomial σ R) with
-    commutes' := fun r => eval₂Hom_C _ _ _ }
+  { (eval₂Hom C fun i ↦ X i ^ p : MvPolynomial σ R →+* MvPolynomial σ R) with
+    commutes' := fun _ ↦ eval₂Hom_C _ _ _ }
 #align mv_polynomial.expand MvPolynomial.expand
 
 @[simp]
-theorem expand_c (p : ℕ) (r : R) : expand p (C r : MvPolynomial σ R) = C r :=
+theorem expand_C (p : ℕ) (r : R) : expand p (C r : MvPolynomial σ R) = C r :=
   eval₂Hom_C _ _ _
-#align mv_polynomial.expand_C MvPolynomial.expand_c
+set_option linter.uppercaseLean3 false in
+#align mv_polynomial.expand_C MvPolynomial.expand_C
 
 @[simp]
-theorem expand_x (p : ℕ) (i : σ) : expand p (X i : MvPolynomial σ R) = X i ^ p :=
+theorem expand_X (p : ℕ) (i : σ) : expand p (X i : MvPolynomial σ R) = X i ^ p :=
   eval₂Hom_X' _ _ _
-#align mv_polynomial.expand_X MvPolynomial.expand_x
+set_option linter.uppercaseLean3 false in
+#align mv_polynomial.expand_X MvPolynomial.expand_X
 
 @[simp]
 theorem expand_monomial (p : ℕ) (d : σ →₀ ℕ) (r : R) :
@@ -54,8 +56,8 @@ theorem expand_monomial (p : ℕ) (d : σ →₀ ℕ) (r : R) :
 #align mv_polynomial.expand_monomial MvPolynomial.expand_monomial
 
 theorem expand_one_apply (f : MvPolynomial σ R) : expand 1 f = f := by
-  simp only [expand, bind₁_X_left, AlgHom.id_apply, RingHom.toFun_eq_coe, eval₂_hom_C_left,
-    AlgHom.coe_toRingHom, pow_one, AlgHom.coe_mks]
+  simp only [expand, pow_one, eval₂Hom_eq_bind₂, bind₂_C_left, RingHom.toMonoidHom_eq_coe,
+    RingHom.coe_monoidHom_id, AlgHom.coe_mk, RingHom.coe_mk, MonoidHom.id_apply]
 #align mv_polynomial.expand_one_apply MvPolynomial.expand_one_apply
 
 @[simp]
@@ -65,14 +67,14 @@ theorem expand_one : expand 1 = AlgHom.id R (MvPolynomial σ R) := by
 #align mv_polynomial.expand_one MvPolynomial.expand_one
 
 theorem expand_comp_bind₁ (p : ℕ) (f : σ → MvPolynomial τ R) :
-    (expand p).comp (bind₁ f) = bind₁ fun i => expand p (f i) := by
-  apply alg_hom_ext
+    (expand p).comp (bind₁ f) = bind₁ fun i ↦ expand p (f i) := by
+  apply algHom_ext
   intro i
   simp only [AlgHom.comp_apply, bind₁_X_right]
 #align mv_polynomial.expand_comp_bind₁ MvPolynomial.expand_comp_bind₁
 
 theorem expand_bind₁ (p : ℕ) (f : σ → MvPolynomial τ R) (φ : MvPolynomial σ R) :
-    expand p (bind₁ f φ) = bind₁ (fun i => expand p (f i)) φ := by
+    expand p (bind₁ f φ) = bind₁ (fun i ↦ expand p (f i)) φ := by
   rw [← AlgHom.comp_apply, expand_comp_bind₁]
 #align mv_polynomial.expand_bind₁ MvPolynomial.expand_bind₁
 
@@ -83,7 +85,8 @@ theorem map_expand (f : R →+* S) (p : ℕ) (φ : MvPolynomial σ R) :
 
 @[simp]
 theorem rename_expand (f : σ → τ) (p : ℕ) (φ : MvPolynomial σ R) :
-    rename f (expand p φ) = expand p (rename f φ) := by simp [expand, bind₁_rename, rename_bind₁]
+    rename f (expand p φ) = expand p (rename f φ) := by
+  simp [expand, bind₁_rename, rename_bind₁, Function.comp]
 #align mv_polynomial.rename_expand MvPolynomial.rename_expand
 
 @[simp]
@@ -95,4 +98,3 @@ theorem rename_comp_expand (f : σ → τ) (p : ℕ) :
 #align mv_polynomial.rename_comp_expand MvPolynomial.rename_comp_expand
 
 end MvPolynomial
-
