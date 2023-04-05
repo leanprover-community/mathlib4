bfe71f01db77eab975a564d4342721a944fcadb2
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2023-04-05 18:12:54 +0200

    Workaround

--- Mathlib/LinearAlgebra/StdBasis.lean
+++ Mathlib/LinearAlgebra/StdBasis.lean
@@ -299,19 +299,32 @@ variable (R : Type _) (m n : Type _) [Fintype m] [Fintype n] [Semiring R]
 
 /-- The standard basis of `matrix m n R`. -/
 noncomputable def stdBasis : Basis (m × n) R (Matrix m n R) :=
-  Basis.reindex (Pi.basis fun i : m => Pi.basisFun R n) (Equiv.sigmaEquivProd _ _)
+  Basis.reindex (Pi.basis fun _ : m => Pi.basisFun R n) (Equiv.sigmaEquivProd _ _)
 #align matrix.std_basis Matrix.stdBasis
 
 variable {n m}
 
 theorem stdBasis_eq_stdBasisMatrix (i : n) (j : m) [DecidableEq n] [DecidableEq m] :
     stdBasis R n m (i, j) = stdBasisMatrix i j (1 : R) := by
+  -- Porting note: `simp` fails to apply `Pi.basis_apply`
   ext (a b)
   by_cases hi : i = a <;> by_cases hj : j = b
-  · simp [stdBasis, hi, hj]
-  · simp [stdBasis, hi, hj, Ne.symm hj, LinearMap.stdBasis_ne]
-  · simp [stdBasis, hi, hj, Ne.symm hi, LinearMap.stdBasis_ne]
-  · simp [stdBasis, hi, hj, Ne.symm hj, Ne.symm hi, LinearMap.stdBasis_ne]
+  · simp [stdBasis, hi, hj, Basis.coe_reindex, comp_apply, Equiv.sigmaEquivProd_symm_apply,
+      StdBasisMatrix.apply_same]
+    erw [Pi.basis_apply]
+    simp
+  · simp only [stdBasis, hi, Basis.coe_reindex, comp_apply, Equiv.sigmaEquivProd_symm_apply, hj,
+      and_false, not_false_iff, StdBasisMatrix.apply_of_ne]
+    erw [Pi.basis_apply]
+    simp [hj]
+  · simp only [stdBasis, hj, Basis.coe_reindex, comp_apply, Equiv.sigmaEquivProd_symm_apply, hi, and_true,
+  not_false_iff, StdBasisMatrix.apply_of_ne]
+    erw [Pi.basis_apply]
+    simp [hi, hj, Ne.symm hi, LinearMap.stdBasis_ne]
+  · simp only [stdBasis, Basis.coe_reindex, comp_apply, Equiv.sigmaEquivProd_symm_apply, hi, hj, and_self,
+  not_false_iff, StdBasisMatrix.apply_of_ne]
+    erw [Pi.basis_apply]
+    simp [hi, hj, Ne.symm hj, Ne.symm hi, LinearMap.stdBasis_ne]
 #align matrix.std_basis_eq_std_basis_matrix Matrix.stdBasis_eq_stdBasisMatrix
 
 end Matrix
