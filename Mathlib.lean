c84577c585501070ca1d93c887e1ad32084878fc
Joël Riou <joel.riou@universite-paris-saclay.fr>
2023-04-05 11:30:42 +0000

    feat: port Order.Category.PartOrd (#3266)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1293,6 +1293,7 @@ import Mathlib.Order.Bounded
 import Mathlib.Order.BoundedOrder
 import Mathlib.Order.Bounds.Basic
 import Mathlib.Order.Bounds.OrderIso
+import Mathlib.Order.Category.PartOrdCat
 import Mathlib.Order.Category.PreordCat
 import Mathlib.Order.Chain
 import Mathlib.Order.Circular
