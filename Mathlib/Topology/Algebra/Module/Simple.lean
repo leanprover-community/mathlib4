e439bab0fd7e3b3212c04f495625444ec597027d
Parcly Taxel <reddeloostw@gmail.com>
2023-04-06 23:17:38 +0800

    automated fixes
    Mathbin -> Mathlib
    fix certain import statements
    move "by" to end of line
    add import to Mathlib.lean

--- Mathlib/Topology/Algebra/Module/Simple.lean
+++ Mathlib/Topology/Algebra/Module/Simple.lean
@@ -8,8 +8,8 @@ Authors: Anatole Dedecker
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
-import Mathbin.RingTheory.SimpleModule
-import Mathbin.Topology.Algebra.Module.Basic
+import Mathlib.RingTheory.SimpleModule
+import Mathlib.Topology.Algebra.Module.Basic
 
 /-!
 # The kernel of a linear function is closed or dense
@@ -29,8 +29,7 @@ variable {R : Type u} {M : Type v} {N : Type w} [Ring R] [TopologicalSpace R] [T
 /-- The kernel of a linear map taking values in a simple module over the base ring is closed or
 dense. Applies, e.g., to the case when `R = N` is a division ring. -/
 theorem LinearMap.isClosed_or_dense_ker (l : M →ₗ[R] N) :
-    IsClosed (l.ker : Set M) ∨ Dense (l.ker : Set M) :=
-  by
+    IsClosed (l.ker : Set M) ∨ Dense (l.ker : Set M) := by
   rcases l.surjective_or_eq_zero with (hl | rfl)
   · exact l.ker.is_closed_or_dense_of_is_coatom (LinearMap.isCoatom_ker_of_surjective hl)
   · rw [LinearMap.ker_zero]
