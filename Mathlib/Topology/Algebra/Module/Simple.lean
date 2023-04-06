f2e578ddeb2b4ad29c05905c80ec17d90c76f871
Parcly Taxel <reddeloostw@gmail.com>
2023-04-06 23:26:04 +0800

    Fix errors

--- Mathlib/Topology/Algebra/Module/Simple.lean
+++ Mathlib/Topology/Algebra/Module/Simple.lean
@@ -14,8 +14,8 @@ import Mathlib.Topology.Algebra.Module.Basic
 /-!
 # The kernel of a linear function is closed or dense
 
-In this file we prove (`linear_map.is_closed_or_dense_ker`) that the kernel of a linear function `f
-: M →ₗ[R] N` is either closed or dense in `M` provided that `N` is a simple module over `R`. This
+In this file we prove (`LinearMap.isClosed_or_dense_ker`) that the kernel of a linear function
+`f : M →ₗ[R] N` is either closed or dense in `M` provided that `N` is a simple module over `R`. This
 applies, e.g., to the case when `R = N` is a division ring.
 -/
 
@@ -26,14 +26,14 @@ variable {R : Type u} {M : Type v} {N : Type w} [Ring R] [TopologicalSpace R] [T
   [AddCommGroup M] [AddCommGroup N] [Module R M] [ContinuousSMul R M] [Module R N] [ContinuousAdd M]
   [IsSimpleModule R N]
 
+set_option synthInstance.etaExperiment true in -- Porting note: gets around lean4#2074
 /-- The kernel of a linear map taking values in a simple module over the base ring is closed or
 dense. Applies, e.g., to the case when `R = N` is a division ring. -/
 theorem LinearMap.isClosed_or_dense_ker (l : M →ₗ[R] N) :
-    IsClosed (l.ker : Set M) ∨ Dense (l.ker : Set M) := by
+    IsClosed (LinearMap.ker l : Set M) ∨ Dense (LinearMap.ker l : Set M) := by
   rcases l.surjective_or_eq_zero with (hl | rfl)
-  · exact l.ker.is_closed_or_dense_of_is_coatom (LinearMap.isCoatom_ker_of_surjective hl)
+  · exact l.ker.isClosed_or_dense_of_isCoatom (LinearMap.isCoatom_ker_of_surjective hl)
   · rw [LinearMap.ker_zero]
     left
     exact isClosed_univ
 #align linear_map.is_closed_or_dense_ker LinearMap.isClosed_or_dense_ker
-
