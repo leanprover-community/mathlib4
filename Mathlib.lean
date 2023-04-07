ed696b6e389341b5dbcbdb433578c5cb6ec7feb8
Yury G. Kudryashov <urkud@urkud.name>
2023-04-07 13:40:17 -0500

    Rename

--- Mathlib.lean
+++ Mathlib.lean
@@ -1068,7 +1068,7 @@ import Mathlib.Data.ZMod.Basic
 import Mathlib.Data.ZMod.Coprime
 import Mathlib.Data.ZMod.Defs
 import Mathlib.Data.ZMod.Parity
-import Mathlib.Data.Zmod.Quotient
+import Mathlib.Data.ZMod.Quotient
 import Mathlib.Deprecated.Group
 import Mathlib.Deprecated.Ring
 import Mathlib.Deprecated.Subfield
