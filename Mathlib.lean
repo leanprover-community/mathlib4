875c586ddeae8c4aadb3916e054aa9d34ec9c76a
Moritz Firsching <moritz.firsching@gmail.com>
2023-04-07 11:23:22 +0000

    feat: port Topology.Algebra.UniformField (#3049)
    Co-authored-by: Moritz Firsching <firsching@google.com>
    Co-authored-by: Moritz Doll <moritz.doll@googlemail.com>
    Co-authored-by: ChrisHughes24 <chrishughes24@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1660,6 +1660,7 @@ import Mathlib.Topology.Algebra.Ring.Ideal
 import Mathlib.Topology.Algebra.Semigroup
 import Mathlib.Topology.Algebra.Star
 import Mathlib.Topology.Algebra.StarSubalgebra
+import Mathlib.Topology.Algebra.UniformField
 import Mathlib.Topology.Algebra.UniformFilterBasis
 import Mathlib.Topology.Algebra.UniformGroup
 import Mathlib.Topology.Algebra.UniformMulAction
