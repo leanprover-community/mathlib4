8289b058f7ae53413d5dfa3666b7898c084d7eda
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2023-04-07 17:36:50 +0200

    Automated fixes

--- Mathlib/LinearAlgebra/Multilinear/Basis.lean
+++ Mathlib/LinearAlgebra/Multilinear/Basis.lean
@@ -19,7 +19,7 @@ This file proves lemmas about the action of multilinear maps on basis vectors.
 ## TODO
 
  * Refactor the proofs in terms of bases of tensor products, once there is an equivalent of
-   `basis.tensor_product` for `pi_tensor_product`.
+   `Basis.tensorProduct` for `pi_tensor_product`.
 
 -/
 
@@ -32,7 +32,7 @@ variable [CommSemiring R] [AddCommMonoid M₂] [AddCommMonoid M₃] [∀ i, AddC
 
 variable [∀ i, Module R (M i)] [Module R M₂] [Module R M₃]
 
-/-- Two multilinear maps indexed by `fin n` are equal if they are equal when all arguments are
+/-- Two multilinear maps indexed by `Fin n` are equal if they are equal when all arguments are
 basis vectors. -/
 theorem Basis.ext_multilinear_fin {f g : MultilinearMap R M M₂} {ι₁ : Fin n → Type _}
     (e : ∀ i, Basis (ι₁ i) R (M i))
@@ -57,8 +57,8 @@ theorem Basis.ext_multilinear_fin {f g : MultilinearMap R M M₂} {ι₁ : Fin n
       rw [Fin.cons_succ, Fin.cons_succ]
 #align basis.ext_multilinear_fin Basis.ext_multilinear_fin
 
-/-- Two multilinear maps indexed by a `fintype` are equal if they are equal when all arguments
-are basis vectors. Unlike `basis.ext_multilinear_fin`, this only uses a single basis; a
+/-- Two multilinear maps indexed by a `Fintype` are equal if they are equal when all arguments
+are basis vectors. Unlike `Basis.ext_multilinear_fin`, this only uses a single basis; a
 dependently-typed version would still be true, but the proof would need a dependently-typed
 version of `dom_dom_congr`. -/
 theorem Basis.ext_multilinear [Finite ι] {f g : MultilinearMap R (fun _ : ι => M₂) M₃} {ι₁ : Type _}
