0609a781300f52d0687b829cba977f236646d2c5
Yury G. Kudryashov <urkud@urkud.name>
2023-04-09 13:20:17 -0500

    Fix

--- Mathlib/Data/List/ToFinsupp.lean
+++ Mathlib/Data/List/ToFinsupp.lean
@@ -33,13 +33,11 @@ can be decidably determined to be not equal to zero or that one can decide one i
 bounds of a list. For concretely defined lists that are made up of elements of decidable terms,
 this holds. More work will be needed to support lists over non-dec-eq types like `ℝ`, where the
 elements are beyond the dec-eq terms of casted values from `ℕ, ℤ, ℚ`.
-
 -/
-
 
 namespace List
 
-variable {M : Type _} [Zero M] (l : List M) [DecidablePred fun i => getD l i 0 ≠ 0] (n : ℕ)
+variable {M : Type _} [Zero M] (l : List M) [DecidablePred (getD l · 0 ≠ 0)] (n : ℕ)
 
 /-- Indexing into a `l : list M`, as a finitely-supported function,
 where the support are all the indices within the length of the list
@@ -49,15 +47,15 @@ This is a computable version of the `finsupp.on_finset` construction.
 -/
 def toFinsupp : ℕ →₀ M where
   toFun i := getD l i 0
-  support := (Finset.range l.length).filterₓ fun i => getD l i 0 ≠ 0
+  support := (Finset.range l.length).filter fun i => getD l i 0 ≠ 0
   mem_support_toFun n := by
     simp only [Ne.def, Finset.mem_filter, Finset.mem_range, and_iff_right_iff_imp]
     contrapose!
-    exact nthd_eq_default _ _
+    exact getD_eq_default _ _
 #align list.to_finsupp List.toFinsupp
 
 @[norm_cast]
-theorem coe_toFinsupp : (l.toFinsupp : ℕ → M) = fun i => l.getD i 0 :=
+theorem coe_toFinsupp : (l.toFinsupp : ℕ → M) = (l.getD · 0) :=
   rfl
 #align list.coe_to_finsupp List.coe_toFinsupp
 
@@ -67,13 +65,21 @@ theorem toFinsupp_apply (i : ℕ) : (l.toFinsupp : ℕ → M) i = l.getD i 0 :=
 #align list.to_finsupp_apply List.toFinsupp_apply
 
 theorem toFinsupp_support :
-    l.toFinsupp.support = (Finset.range l.length).filterₓ fun i => getD l i 0 ≠ 0 :=
+    l.toFinsupp.support = (Finset.range l.length).filter (getD l · 0 ≠ 0) :=
   rfl
 #align list.to_finsupp_support List.toFinsupp_support
 
-theorem toFinsupp_apply_lt (hn : n < l.length) : l.toFinsupp n = l.nthLe n hn :=
-  getD_eq_nthLe _ _ _
-#align list.to_finsupp_apply_lt List.toFinsupp_apply_lt
+theorem toFinsupp_apply_lt (hn : n < l.length) : l.toFinsupp n = l.get ⟨n, hn⟩ :=
+  getD_eq_get _ _ _
+
+theorem toFinsupp_apply_fin (n : Fin l.length) : l.toFinsupp n = l.get n :=
+  getD_eq_get _ _ _
+
+set_option linter.deprecated false in
+@[deprecated]
+theorem toFinsupp_apply_lt' (hn : n < l.length) : l.toFinsupp n = l.nthLe n hn :=
+  getD_eq_get _ _ _
+#align list.to_finsupp_apply_lt List.toFinsupp_apply_lt'
 
 theorem toFinsupp_apply_le (hn : l.length ≤ n) : l.toFinsupp n = 0 :=
   getD_eq_default _ _ hn
@@ -86,72 +92,71 @@ theorem toFinsupp_nil [DecidablePred fun i => getD ([] : List M) i 0 ≠ 0] :
   simp
 #align list.to_finsupp_nil List.toFinsupp_nil
 
-theorem toFinsupp_singleton (x : M) [DecidablePred fun i => getD [x] i 0 ≠ 0] :
+theorem toFinsupp_singleton (x : M) [DecidablePred (getD [x] · 0 ≠ 0)] :
     toFinsupp [x] = Finsupp.single 0 x := by
-  ext ⟨_ | i⟩ <;> simp [Finsupp.single_apply, (Nat.zero_lt_succ _).Ne]
+  ext ⟨_ | i⟩ <;> simp [Finsupp.single_apply, (Nat.zero_lt_succ _).ne]
 #align list.to_finsupp_singleton List.toFinsupp_singleton
 
-/- ./././Mathport/Syntax/Translate/Expr.lean:177:8: unsupported: ambiguous notation -/
-/- ./././Mathport/Syntax/Translate/Expr.lean:177:8: unsupported: ambiguous notation -/
 @[simp]
 theorem toFinsupp_cons_apply_zero (x : M) (xs : List M)
-    [DecidablePred fun i => getD (x::xs) i 0 ≠ 0] : (x::xs).toFinsupp 0 = x :=
+    [DecidablePred (getD (x::xs) · 0 ≠ 0)] : (x::xs).toFinsupp 0 = x :=
   rfl
 #align list.to_finsupp_cons_apply_zero List.toFinsupp_cons_apply_zero
 
-/- ./././Mathport/Syntax/Translate/Expr.lean:177:8: unsupported: ambiguous notation -/
-/- ./././Mathport/Syntax/Translate/Expr.lean:177:8: unsupported: ambiguous notation -/
 @[simp]
 theorem toFinsupp_cons_apply_succ (x : M) (xs : List M) (n : ℕ)
-    [DecidablePred fun i => getD (x::xs) i 0 ≠ 0] [DecidablePred fun i => getD xs i 0 ≠ 0] :
+    [DecidablePred (getD (x::xs) · 0 ≠ 0)] [DecidablePred (getD xs · 0 ≠ 0)] :
     (x::xs).toFinsupp n.succ = xs.toFinsupp n :=
   rfl
 #align list.to_finsupp_cons_apply_succ List.toFinsupp_cons_apply_succ
 
-/- ./././Mathport/Syntax/Translate/Expr.lean:177:8: unsupported: ambiguous notation -/
-/- ./././Mathport/Syntax/Translate/Expr.lean:177:8: unsupported: ambiguous notation -/
+-- porting note: new theorem
+theorem toFinsupp_append {R : Type _} [AddZeroClass R] (l₁ l₂ : List R)
+    [DecidablePred (getD (l₁ ++ l₂) · 0 ≠ 0)] [DecidablePred (getD l₁ · 0 ≠ 0)]
+    [DecidablePred (getD l₂ · 0 ≠ 0)] :
+    toFinsupp (l₁ ++ l₂) =
+      toFinsupp l₁ + (toFinsupp l₂).embDomain (addLeftEmbedding l₁.length) := by
+  ext n
+  simp only [toFinsupp_apply, Finsupp.add_apply]
+  cases lt_or_le n l₁.length with
+  | inl h =>
+    rw [getD_append _ _ _ _ h, Finsupp.embDomain_notin_range, add_zero]
+    rintro ⟨k, rfl : length l₁ + k = n⟩
+    exact h.not_le (self_le_add_right _ _)
+  | inr h =>
+    rcases exists_add_of_le h with ⟨k, rfl⟩
+    rw [getD_append_right _ _ _ _ h, add_tsub_cancel_left, getD_eq_default _ _ h, zero_add]
+    exact Eq.symm (Finsupp.embDomain_apply _ _ _)
+
 theorem toFinsupp_cons_eq_single_add_embDomain {R : Type _} [AddZeroClass R] (x : R) (xs : List R)
-    [DecidablePred fun i => getD (x::xs) i 0 ≠ 0] [DecidablePred fun i => getD xs i 0 ≠ 0] :
+    [DecidablePred (getD (x::xs) · 0 ≠ 0)] [DecidablePred (getD xs · 0 ≠ 0)] :
     toFinsupp (x::xs) =
       Finsupp.single 0 x + (toFinsupp xs).embDomain ⟨Nat.succ, Nat.succ_injective⟩ := by
-  ext (_ | i)
-  · simp only [Nat.zero_eq, to_finsupp_cons_apply_zero, Finsupp.coe_add, Pi.add_apply,
-      Finsupp.single_eq_same]
-    rw [Finsupp.embDomain_notin_range]
-    · exact (add_zero _).symm
-    · simp
-  · simp only [to_finsupp_cons_apply_succ, Finsupp.coe_add, Pi.add_apply]
-    have hi : i.succ = (⟨Nat.succ, Nat.succ_injective⟩ : ℕ ↪ ℕ) i := rfl
-    rw [finsupp.single_apply_eq_zero.mpr, zero_add, hi, Finsupp.embDomain_apply]
-    simp
+  classical
+    convert toFinsupp_append [x] xs using 3
+    · exact (toFinsupp_singleton x).symm
+    · ext n
+      exact add_comm n 1
 #align list.to_finsupp_cons_eq_single_add_emb_domain List.toFinsupp_cons_eq_single_add_embDomain
 
 theorem toFinsupp_concat_eq_toFinsupp_add_single {R : Type _} [AddZeroClass R] (x : R) (xs : List R)
     [DecidablePred fun i => getD (xs ++ [x]) i 0 ≠ 0] [DecidablePred fun i => getD xs i 0 ≠ 0] :
     toFinsupp (xs ++ [x]) = toFinsupp xs + Finsupp.single xs.length x := by
-  ext i
-  simp only [Finsupp.coe_add, Pi.add_apply, Finsupp.single_apply]
-  rcases lt_trichotomy xs.length i with (hi | rfl | hi)
-  · rw [to_finsupp_apply_le _ _ hi.le, to_finsupp_apply_le, if_neg hi.ne, add_zero]
-    simpa using Nat.succ_le_of_lt hi
-  · rw [to_finsupp_apply_lt, to_finsupp_apply_le _ _ le_rfl, if_pos rfl, zero_add,
-      nth_le_append_right le_rfl]
-    · simp
-    · simp
-  · rw [to_finsupp_apply_lt _ _ hi, to_finsupp_apply_lt, if_neg hi.ne', add_zero, nth_le_append]
-    simpa using Nat.lt_succ_of_lt hi
+  classical rw [toFinsupp_append, toFinsupp_singleton, Finsupp.embDomain_single,
+    addLeftEmbedding_apply, add_zero]
 #align list.to_finsupp_concat_eq_to_finsupp_add_single List.toFinsupp_concat_eq_toFinsupp_add_single
 
+
 theorem toFinsupp_eq_sum_map_enum_single {R : Type _} [AddMonoid R] (l : List R)
-    [DecidablePred fun i => getD l i 0 ≠ 0] :
-    toFinsupp l = (l.enum.map fun nr : ℕ × R => Finsupp.single nr.1 nr.2).Sum := by
-  induction' l using List.reverseRecOn with xs x IH
-  · convert to_finsupp_nil
-  · simp only [enum_append, map, enum_from_singleton, map_append, sum_append, sum_cons, sum_nil,
-      add_zero]
-    classical
-      convert to_finsupp_concat_eq_to_finsupp_add_single _ _
-      exact IH.symm
+    [DecidablePred (getD l · 0 ≠ 0)] :
+    toFinsupp l = (l.enum.map fun nr : ℕ × R => Finsupp.single nr.1 nr.2).sum := by
+  /- porting note: todo: `induction` fails to substitute `l = []` in
+  `[DecidablePred (getD l · 0 ≠ 0)]`, so we manually do some `revert`/`intro` as a workaround -/
+  revert l; intro l
+  induction l using List.reverseRecOn with
+  | H0 => exact toFinsupp_nil
+  | H1 x xs ih =>
+    classical simp [toFinsupp_concat_eq_toFinsupp_add_single, enum_append, ih]
 #align list.to_finsupp_eq_sum_map_enum_single List.toFinsupp_eq_sum_map_enum_single
 
 end List
