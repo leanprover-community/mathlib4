c1ca156d4ae4f26423a7cfb07ebc455f76ab99a7
Parcly Taxel <reddeloostw@gmail.com>
2023-04-10 21:28:31 +0800

    Fix errors

--- Mathlib/FieldTheory/MvPolynomial.lean
+++ Mathlib/FieldTheory/MvPolynomial.lean
@@ -38,14 +38,15 @@ variable {σ : Type u} {K : Type v}
 
 variable (σ K) [Field K]
 
-theorem quotient_mk_comp_c_injective (I : Ideal (MvPolynomial σ K)) (hI : I ≠ ⊤) :
+theorem quotient_mk_comp_C_injective (I : Ideal (MvPolynomial σ K)) (hI : I ≠ ⊤) :
     Function.Injective ((Ideal.Quotient.mk I).comp MvPolynomial.C) := by
   refine' (injective_iff_map_eq_zero _).2 fun x hx => _
   rw [RingHom.comp_apply, Ideal.Quotient.eq_zero_iff_mem] at hx
-  refine' by_contradiction fun hx0 => absurd (I.eq_top_iff_one.2 _) hI
+  refine' _root_.by_contradiction fun hx0 => absurd (I.eq_top_iff_one.2 _) hI
   have := I.mul_mem_left (MvPolynomial.C x⁻¹) hx
-  rwa [← mv_polynomial.C.map_mul, inv_mul_cancel hx0, MvPolynomial.C_1] at this
-#align mv_polynomial.quotient_mk_comp_C_injective MvPolynomial.quotient_mk_comp_c_injective
+  rwa [← MvPolynomial.C.map_mul, inv_mul_cancel hx0, MvPolynomial.C_1] at this
+set_option linter.uppercaseLean3 false in
+#align mv_polynomial.quotient_mk_comp_C_injective MvPolynomial.quotient_mk_comp_C_injective
 
 end MvPolynomial
 
@@ -57,9 +58,9 @@ variable {σ : Type u} {K : Type u} [Field K]
 
 open Classical
 
+set_option synthInstance.etaExperiment true in
 theorem rank_mvPolynomial : Module.rank K (MvPolynomial σ K) = Cardinal.mk (σ →₀ ℕ) := by
-  rw [← Cardinal.lift_inj, ← (basis_monomials σ K).mk_eq_rank]
+  rw [← Cardinal.lift_inj, ← (basisMonomials σ K).mk_eq_rank]
 #align mv_polynomial.rank_mv_polynomial MvPolynomial.rank_mvPolynomial
 
 end MvPolynomial
-
