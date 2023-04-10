c5372bb1b31cc5e7e637aeb6bb5488fa3b7e8ca3
Joël Riou <vi.hdz.p@gmail.com>
2023-04-10 20:28:25 +0000

    feat: port CategoryTheory.Triangulated.Triangulated (#3072)
    Also actually corrects the SHA that was incorrectly corrected in #3346; the stupid example was in `CategoryTheory.Triangulated.Rotate` and not `CategoryTheory.Triangulated.Basic`.
    
    
    
    Co-authored-by: Joël Riou <joel.riou@universite-paris-saclay.fr>
    Co-authored-by: Jeremy Tan Jie Rui <reddeloostw@gmail.com>
    Co-authored-by: Parcly Taxel <reddeloostw@gmail.com>
    Co-authored-by: Scott Morrison <scott@tqft.net>
    Co-authored-by: Chris Hughes <chrishughes24@gmail.com>
    Co-authored-by: Jon Eugster <eugster.jon@gmail.com>
    Co-authored-by: Jason Yuen <jason_yuen2007@hotmail.com>
    Co-authored-by: Yury G. Kudryashov <urkud@urkud.name>
    Co-authored-by: qawbecrdtey <qawbecrdtey@kaist.ac.kr>
    Co-authored-by: Jeremy Tan Jie Rui <e0191785@u.nus.edu>
    Co-authored-by: Sebastian Zivota <loewenheim@mailbox.org>
    Co-authored-by: Kyle Miller <kmill31415@gmail.com>
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>
    Co-authored-by: Floris van Doorn <fpvdoorn@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -522,6 +522,7 @@ import Mathlib.CategoryTheory.Thin
 import Mathlib.CategoryTheory.Triangulated.Basic
 import Mathlib.CategoryTheory.Triangulated.Pretriangulated
 import Mathlib.CategoryTheory.Triangulated.Rotate
+import Mathlib.CategoryTheory.Triangulated.Triangulated
 import Mathlib.CategoryTheory.Types
 import Mathlib.CategoryTheory.Whiskering
 import Mathlib.CategoryTheory.Yoneda
