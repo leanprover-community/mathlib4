28c8a705258305443707b6a3dccec093b965e515
Moritz Doll <65514131+Ruben-VandeVelde@users.noreply.github.com>
2023-04-10 15:17:59 +0000

    feat: port Topology.LocalAtTarget (#2205)
    Co-authored-by: Moritz Doll <moritz.doll@googlemail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1725,6 +1725,7 @@ import Mathlib.Topology.Instances.Sign
 import Mathlib.Topology.Instances.TrivSqZeroExt
 import Mathlib.Topology.IsLocallyHomeomorph
 import Mathlib.Topology.List
+import Mathlib.Topology.LocalAtTarget
 import Mathlib.Topology.LocalExtr
 import Mathlib.Topology.LocalHomeomorph
 import Mathlib.Topology.LocallyConstant.Algebra
