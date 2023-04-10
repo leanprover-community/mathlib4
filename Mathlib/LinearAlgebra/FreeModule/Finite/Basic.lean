62197dd195b53d9b69dd92b3213237d0c043cd1b
Parcly Taxel <reddeloostw@gmail.com>
2023-04-10 17:15:11 +0800

    Fix errors

--- Mathlib/LinearAlgebra/FreeModule/Finite/Basic.lean
+++ Mathlib/LinearAlgebra/FreeModule/Finite/Basic.lean
@@ -19,11 +19,8 @@ We provide some instances for finite and free modules.
 
 ## Main results
 
-* `module.free.choose_basis_index.fintype` : If a free module is finite, then any basis is
-  finite.
-* `module.free.linear_map.free ` : if `M` and `N` are finite and free, then `M →ₗ[R] N` is free.
-* `module.free.linear_map.module.finite` : if `M` and `N` are finite and free, then `M →ₗ[R] N`
-  is finite.
+* `Module.Free.ChooseBasisIndex.fintype` : If a free module is finite, then any basis is finite.
+* `Module.Finite.of_basis` : A free module with a basis indexed by a `Fintype` is finite.
 -/
 
 
@@ -38,11 +35,12 @@ section Ring
 variable [Ring R] [AddCommGroup M] [Module R M] [Module.Free R M]
 
 /-- If a free module is finite, then any basis is finite. -/
-noncomputable instance [Nontrivial R] [Module.Finite R M] :
+noncomputable instance ChooseBasisIndex.fintype [Nontrivial R] [Module.Finite R M] :
     Fintype (Module.Free.ChooseBasisIndex R M) := by
   obtain ⟨h⟩ := id ‹Module.Finite R M›
   choose s hs using h
-  exact basisFintypeOfFiniteSpans (↑s) hs (choose_basis _ _)
+  exact basisFintypeOfFiniteSpans (↑s) hs (chooseBasis _ _)
+#align module.free.choose_basis_index.fintype Module.Free.ChooseBasisIndex.fintype
 
 end Ring
 
@@ -54,23 +52,22 @@ variable [AddCommGroup N] [Module R N] [Module.Free R N]
 
 variable {R}
 
-/-- A free module with a basis indexed by a `fintype` is finite. -/
-theorem Module.Finite.of_basis {R M ι : Type _} [CommRing R] [AddCommGroup M] [Module R M]
-    [Finite ι] (b : Basis ι R M) : Module.Finite R M := by
+/-- A free module with a basis indexed by a `Fintype` is finite. -/
+theorem _root_.Module.Finite.of_basis {R M ι : Type _} [CommRing R] [AddCommGroup M] [Module R M]
+    [_root_.Finite ι] (b : Basis ι R M) : Module.Finite R M := by
   cases nonempty_fintype ι
   classical
-    refine' ⟨⟨finset.univ.image b, _⟩⟩
+    refine' ⟨⟨Finset.univ.image b, _⟩⟩
     simp only [Set.image_univ, Finset.coe_univ, Finset.coe_image, Basis.span_eq]
 #align module.finite.of_basis Module.Finite.of_basis
 
-instance Module.Finite.matrix {ι₁ ι₂ : Type _} [Finite ι₁] [Finite ι₂] :
+instance _root_.Module.Finite.matrix {ι₁ ι₂ : Type _} [_root_.Finite ι₁] [_root_.Finite ι₂] :
     Module.Finite R (Matrix ι₁ ι₂ R) := by
   cases nonempty_fintype ι₁
   cases nonempty_fintype ι₂
-  exact Module.Finite.of_basis (Pi.basis fun i => Pi.basisFun R _)
+  exact Module.Finite.of_basis (Pi.basis fun _ => Pi.basisFun R _)
 #align module.finite.matrix Module.Finite.matrix
 
 end CommRing
 
 end Module.Free
-
