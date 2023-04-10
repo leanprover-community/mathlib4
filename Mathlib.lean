71bb33c5a4f900532b8f7ad0fe75899afcdc3837
Joël Riou <joel.riou@universite-paris-saclay.fr>
2023-04-10 19:55:17 +0000

    feat: port CategoryTheory.Fintype (#3365)

--- Mathlib.lean
+++ Mathlib.lean
@@ -363,6 +363,7 @@ import Mathlib.CategoryTheory.EssentialImage
 import Mathlib.CategoryTheory.EssentiallySmall
 import Mathlib.CategoryTheory.Filtered
 import Mathlib.CategoryTheory.FinCategory
+import Mathlib.CategoryTheory.FintypeCat
 import Mathlib.CategoryTheory.FullSubcategory
 import Mathlib.CategoryTheory.Functor.Basic
 import Mathlib.CategoryTheory.Functor.Category
