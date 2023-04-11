85b0cd3cc950157b1186b1b887cb2d9e0d8448c7
Jeremy Tan Jie Rui <e0191785@u.nus.edu>
2023-04-11 12:30:58 +0000

    feat: port Topology.Category.Top.Adjunctions (#3390)
    Co-authored-by: Parcly Taxel <reddeloostw@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1716,6 +1716,7 @@ import Mathlib.Topology.Bornology.Basic
 import Mathlib.Topology.Bornology.Constructions
 import Mathlib.Topology.Bornology.Hom
 import Mathlib.Topology.Category.Born
+import Mathlib.Topology.Category.Top.Adjunctions
 import Mathlib.Topology.Category.Top.Basic
 import Mathlib.Topology.CompactOpen
 import Mathlib.Topology.Connected
