d0b0bdf2c4128ad865667f2703b8ac4edb5132af
Parcly Taxel <reddeloostw@gmail.com>
2023-04-12 22:08:52 +0800

    Initial file copy from mathport

---
+++ Mathlib/CategoryTheory/Idempotents/SimplicialObject.lean
@@ -0,0 +1,40 @@
+/-
+Copyright (c) 2022 Joël Riou. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Joël Riou
+
+! This file was ported from Lean 3 source module category_theory.idempotents.simplicial_object
+! leanprover-community/mathlib commit 163d1a6d98caf9f0431704169027e49c5c6c6cc0
+! Please do not edit these lines, except to modify the commit id
+! if you have ported upstream changes.
+-/
+import Mathbin.AlgebraicTopology.SimplicialObject
+import Mathbin.CategoryTheory.Idempotents.FunctorCategories
+
+/-!
+
+# Idempotent completeness of categories of simplicial objects
+
+In this file, we provide an instance expressing that `simplicial_object C`
+and `cosimplicial_object C` are idempotent complete categories when the
+category `C` is.
+
+-/
+
+
+namespace CategoryTheory
+
+namespace Idempotents
+
+variable {C : Type _} [Category C] [IsIdempotentComplete C]
+
+instance : IsIdempotentComplete (SimplicialObject C) :=
+  Idempotents.functor_category_isIdempotentComplete _ _
+
+instance : IsIdempotentComplete (CosimplicialObject C) :=
+  Idempotents.functor_category_isIdempotentComplete _ _
+
+end Idempotents
+
+end CategoryTheory
+
