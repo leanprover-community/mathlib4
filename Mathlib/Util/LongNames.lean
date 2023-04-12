7a05aef1994e0df56e5c858ab46bb84aef792024
Scott Morrison <scott.morrison@gmail.com>
2023-04-12 17:19:33 +0930

    refactor

--- Mathlib/Util/LongNames.lean
+++ Mathlib/Util/LongNames.lean
@@ -3,7 +3,7 @@ Copyright (c) 2023 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison
 -/
-import Std.Data.HashMap
+import Mathlib.Lean.Name
 import Mathlib.Lean.Expr.Basic
 
 /-!
@@ -13,50 +13,6 @@ For finding declarations with excessively long names.
 -/
 
 open Lean Meta Elab
-
-private def isBlackListed (declName : Name) : CoreM Bool := do
-  if declName.toString.startsWith "Lean" then return true
-  let env ← getEnv
-  pure $ declName.isInternal'
-   || isAuxRecursor env declName
-   || isNoConfusion env declName
-  <||> isRec declName <||> isMatcher declName
-
-/--
-Retrieve all names in the environment satisfying a predicate.
--/
-def allNames (p : Name → Bool) : CoreM (Array Name) := do
-  let mut names := #[]
-  for (n, _) in (← getEnv).constants.map₁.toList ++ (← getEnv).constants.map₂.toList do
-    if p n && !(← isBlackListed n) then
-      names := names.push n
-  pure names
-
-def Lean.Name.getModule (n : Name) : CoreM (Option Name) := do
-  let env ← getEnv
-  let modules := env.header.moduleNames
-  match env.const2ModIdx[n] with
-  | none => pure none
-  | some (idx : Nat) => pure modules[idx]?
-
-/--
-Collect the values in an `Array` into a `HashMap`, according to their values under a function.
--/
-def gatherBy [BEq β] [Hashable β] (x : Array α) (f : α → β) : Std.HashMap β (Array α) := Id.run do
-  let mut h := Std.HashMap.empty
-  for a in x do
-    let b := f a
-    h := h.insert b (match h.find? b with
-    | none => #[a]
-    | some l => l.push a)
-  pure h
-
-/--
-Retrieve all names in the environment satisfying a predicate,
-gathered together into a `HashMap` according to the module they are defined in. -/
-def allNamesByModule (p : Name → Bool) : CoreM (Std.HashMap Name (Array Name)) := do
-  let names ← (← allNames p) |>.filterMapM fun n => do pure <| (n, ·) <$> (← n.getModule)
-  pure <| gatherBy names (·.2) |>.mapVal fun _ n => n.map (·.1)
 
 /-- Helper function for `#long_names` and `#long_instances`. -/
 def printNameHashMap (h : Std.HashMap Name (Array Name)) : IO Unit :=
