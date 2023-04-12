bbb99ccf5d1a1637c270c806eeb3418873367ca4
Floris van Doorn <fpvdoorn@gmail.com>
2023-04-12 12:11:35 +0000

    feat: port Analysis.NormedSpace.ContinuousLinearMap (#3381)
    Co-authored-by: Parcly Taxel <reddeloostw@gmail.com>
    Co-authored-by: qawbecrdtey <qawbecrdtey@naver.com>
    Co-authored-by: Matthew Ballard <matt@mrb.email>
    Co-authored-by: Moritz Doll <moritz.doll@googlemail.com>
    Co-authored-by: Jeremy Tan Jie Rui <reddeloostw@gmail.com>
    Co-authored-by: Chris Hughes <33847686+ChrisHughes24@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -311,6 +311,7 @@ import Mathlib.Analysis.Normed.Group.Seminorm
 import Mathlib.Analysis.Normed.Order.Basic
 import Mathlib.Analysis.Normed.Order.Lattice
 import Mathlib.Analysis.NormedSpace.Basic
+import Mathlib.Analysis.NormedSpace.ContinuousLinearMap
 import Mathlib.Analysis.NormedSpace.IndicatorFunction
 import Mathlib.Analysis.NormedSpace.Int
 import Mathlib.Analysis.NormedSpace.MStructure
