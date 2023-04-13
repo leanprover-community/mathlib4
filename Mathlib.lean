e3039685af30cd887448b5d44ff74230d872afff
Joël Riou <e0191785@u.nus.edu>
2023-04-13 11:38:17 +0000

    feat: port Algebra.Category.Module.EpiMono (#3413)
    Co-authored-by: Pol_tta <65514131+Ruben-VandeVelde@users.noreply.github.com>
    Co-authored-by: EmilieUthaiwat <emiliepathum@gmail.com>
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>
    Co-authored-by: Jireh Loreaux <loreaujy@gmail.com>
    Co-authored-by: Gabriel Ebner <gebner@gebner.org>
    Co-authored-by: Violeta Hernández <vi.hdz.p@gmail.com>
    Co-authored-by: Chris Hughes <chrishughes24@gmail.com>
    Co-authored-by: Floris van Doorn <fpvdoorn@gmail.com>
    Co-authored-by: Eric Rodriguez <ericrboidi@gmail.com>
    Co-authored-by: Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
    Co-authored-by: Joël Riou <joel.riou@universite-paris-saclay.fr>
    Co-authored-by: Jeremy Tan Jie Rui <reddeloostw@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -33,6 +33,7 @@ import Mathlib.Algebra.Category.GroupCat.Basic
 import Mathlib.Algebra.Category.GroupCat.Preadditive
 import Mathlib.Algebra.Category.GroupCat.Zero
 import Mathlib.Algebra.Category.ModuleCat.Basic
+import Mathlib.Algebra.Category.ModuleCat.EpiMono
 import Mathlib.Algebra.Category.MonCat.Basic
 import Mathlib.Algebra.CharP.Algebra
 import Mathlib.Algebra.CharP.Basic
