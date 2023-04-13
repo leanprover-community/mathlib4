8b3c1d39256d3fa6eee9305bc7b6d2363e6a0467
Parcly Taxel <reddeloostw@gmail.com>
2023-04-13 14:58:58 +0800

    Fix errors

--- Mathlib/Analysis/NormedSpace/Extr.lean
+++ Mathlib/Analysis/NormedSpace/Extr.lean
@@ -14,12 +14,12 @@ import Mathlib.Topology.LocalExtr
 /-!
 # (Local) maximums in a normed space
 
-In this file we prove the following lemma, see `is_max_filter.norm_add_same_ray`. If `f : α → E` is
+In this file we prove the following lemma, see `IsMaxFilter.norm_add_sameRay`. If `f : α → E` is
 a function such that `norm ∘ f` has a maximum along a filter `l` at a point `c` and `y` is a vector
-on the same ray as `f c`, then the function `λ x, ‖f x + y‖` has a maximul along `l` at `c`.
+on the same ray as `f c`, then the function `fun x => ‖f x + y‖` has a maximum along `l` at `c`.
 
-Then we specialize it to the case `y = f c` and to different special cases of `is_max_filter`:
-`is_max_on`, `is_local_max_on`, and `is_local_max`.
+Then we specialize it to the case `y = f c` and to different special cases of `IsMaxFilter`:
+`IsMaxOn`, `IsLocalMaxOn`, and `IsLocalMax`.
 
 ## Tags
 
@@ -34,8 +34,8 @@ section
 variable {f : α → E} {l : Filter α} {s : Set α} {c : α} {y : E}
 
 /-- If `f : α → E` is a function such that `norm ∘ f` has a maximum along a filter `l` at a point
-`c` and `y` is a vector on the same ray as `f c`, then the function `λ x, ‖f x + y‖` has a maximul
-along `l` at `c`. -/
+`c` and `y` is a vector on the same ray as `f c`, then the function `fun x => ‖f x + y‖` has
+a maximum along `l` at `c`. -/
 theorem IsMaxFilter.norm_add_sameRay (h : IsMaxFilter (norm ∘ f) l c) (hy : SameRay ℝ (f c) y) :
     IsMaxFilter (fun x => ‖f x + y‖) l c :=
   h.mono fun x hx =>
@@ -43,28 +43,28 @@ theorem IsMaxFilter.norm_add_sameRay (h : IsMaxFilter (norm ∘ f) l c) (hy : Sa
       ‖f x + y‖ ≤ ‖f x‖ + ‖y‖ := norm_add_le _ _
       _ ≤ ‖f c‖ + ‖y‖ := (add_le_add_right hx _)
       _ = ‖f c + y‖ := hy.norm_add.symm
-      
+
 #align is_max_filter.norm_add_same_ray IsMaxFilter.norm_add_sameRay
 
 /-- If `f : α → E` is a function such that `norm ∘ f` has a maximum along a filter `l` at a point
-`c`, then the function `λ x, ‖f x + f c‖` has a maximul along `l` at `c`. -/
+`c`, then the function `fun x => ‖f x + f c‖` has a maximum along `l` at `c`. -/
 theorem IsMaxFilter.norm_add_self (h : IsMaxFilter (norm ∘ f) l c) :
     IsMaxFilter (fun x => ‖f x + f c‖) l c :=
-  h.norm_add_sameRay SameRay.rfl
+  IsMaxFilter.norm_add_sameRay h SameRay.rfl
 #align is_max_filter.norm_add_self IsMaxFilter.norm_add_self
 
 /-- If `f : α → E` is a function such that `norm ∘ f` has a maximum on a set `s` at a point `c` and
-`y` is a vector on the same ray as `f c`, then the function `λ x, ‖f x + y‖` has a maximul on `s` at
-`c`. -/
+`y` is a vector on the same ray as `f c`, then the function `fun x => ‖f x + y‖` has a maximum
+on `s` at `c`. -/
 theorem IsMaxOn.norm_add_sameRay (h : IsMaxOn (norm ∘ f) s c) (hy : SameRay ℝ (f c) y) :
     IsMaxOn (fun x => ‖f x + y‖) s c :=
-  h.norm_add_sameRay hy
+  IsMaxFilter.norm_add_sameRay h hy
 #align is_max_on.norm_add_same_ray IsMaxOn.norm_add_sameRay
 
 /-- If `f : α → E` is a function such that `norm ∘ f` has a maximum on a set `s` at a point `c`,
-then the function `λ x, ‖f x + f c‖` has a maximul on `s` at `c`. -/
+then the function `fun x => ‖f x + f c‖` has a maximum on `s` at `c`. -/
 theorem IsMaxOn.norm_add_self (h : IsMaxOn (norm ∘ f) s c) : IsMaxOn (fun x => ‖f x + f c‖) s c :=
-  h.norm_add_self
+  IsMaxFilter.norm_add_self h
 #align is_max_on.norm_add_self IsMaxOn.norm_add_self
 
 end
@@ -72,31 +72,31 @@ end
 variable {f : X → E} {s : Set X} {c : X} {y : E}
 
 /-- If `f : α → E` is a function such that `norm ∘ f` has a local maximum on a set `s` at a point
-`c` and `y` is a vector on the same ray as `f c`, then the function `λ x, ‖f x + y‖` has a local
-maximul on `s` at `c`. -/
+`c` and `y` is a vector on the same ray as `f c`, then the function `fun x => ‖f x + y‖` has a local
+maximum on `s` at `c`. -/
 theorem IsLocalMaxOn.norm_add_sameRay (h : IsLocalMaxOn (norm ∘ f) s c) (hy : SameRay ℝ (f c) y) :
     IsLocalMaxOn (fun x => ‖f x + y‖) s c :=
-  h.norm_add_sameRay hy
+  IsMaxFilter.norm_add_sameRay h hy
 #align is_local_max_on.norm_add_same_ray IsLocalMaxOn.norm_add_sameRay
 
 /-- If `f : α → E` is a function such that `norm ∘ f` has a local maximum on a set `s` at a point
-`c`, then the function `λ x, ‖f x + f c‖` has a local maximul on `s` at `c`. -/
+`c`, then the function `fun x => ‖f x + f c‖` has a local maximum on `s` at `c`. -/
 theorem IsLocalMaxOn.norm_add_self (h : IsLocalMaxOn (norm ∘ f) s c) :
     IsLocalMaxOn (fun x => ‖f x + f c‖) s c :=
-  h.norm_add_self
+  IsMaxFilter.norm_add_self h
 #align is_local_max_on.norm_add_self IsLocalMaxOn.norm_add_self
 
 /-- If `f : α → E` is a function such that `norm ∘ f` has a local maximum at a point `c` and `y` is
-a vector on the same ray as `f c`, then the function `λ x, ‖f x + y‖` has a local maximul at `c`. -/
+a vector on the same ray as `f c`, then the function `fun x => ‖f x + y‖` has a local maximum
+at `c`. -/
 theorem IsLocalMax.norm_add_sameRay (h : IsLocalMax (norm ∘ f) c) (hy : SameRay ℝ (f c) y) :
     IsLocalMax (fun x => ‖f x + y‖) c :=
-  h.norm_add_sameRay hy
+  IsMaxFilter.norm_add_sameRay h hy
 #align is_local_max.norm_add_same_ray IsLocalMax.norm_add_sameRay
 
 /-- If `f : α → E` is a function such that `norm ∘ f` has a local maximum at a point `c`, then the
-function `λ x, ‖f x + f c‖` has a local maximul at `c`. -/
+function `fun x => ‖f x + f c‖` has a local maximum at `c`. -/
 theorem IsLocalMax.norm_add_self (h : IsLocalMax (norm ∘ f) c) :
     IsLocalMax (fun x => ‖f x + f c‖) c :=
-  h.norm_add_self
+  IsMaxFilter.norm_add_self h
 #align is_local_max.norm_add_self IsLocalMax.norm_add_self
-
