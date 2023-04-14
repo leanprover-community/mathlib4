81241a1a7e72413c161d02502b72d8528d7c30aa
Jason Yuen <65514131+Ruben-VandeVelde@users.noreply.github.com>
2023-04-14 15:26:42 +0000

    feat: port Analysis.BoxIntegral.Box.Basic (#2625)
    Co-authored-by: int-y1 <jason_yuen2007@hotmail.com>
    Co-authored-by: Moritz Firsching <firsching@google.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -292,6 +292,7 @@ import Mathlib.Analysis.Asymptotics.Asymptotics
 import Mathlib.Analysis.Asymptotics.SpecificAsymptotics
 import Mathlib.Analysis.Asymptotics.SuperpolynomialDecay
 import Mathlib.Analysis.Asymptotics.Theta
+import Mathlib.Analysis.BoxIntegral.Box.Basic
 import Mathlib.Analysis.Convex.Basic
 import Mathlib.Analysis.Convex.Extrema
 import Mathlib.Analysis.Convex.Extreme
