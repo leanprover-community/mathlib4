46662fece5c663b2e4b4472de287a8da76ae02ff
Yury G. Kudryashov <urkud@urkud.name>
2023-04-14 02:00:32 -0500

    Fix compile

--- Mathlib/Analysis/Convex/Body.lean
+++ Mathlib/Analysis/Convex/Body.lean
@@ -47,7 +47,7 @@ it is convex, compact, and nonempty.
 structure ConvexBody (V : Type _) [TopologicalSpace V] [AddCommMonoid V] [SMul ℝ V] where
   carrier : Set V
   convex' : Convex ℝ carrier
-  is_compact' : IsCompact carrier
+  isCompact' : IsCompact carrier
   nonempty' : carrier.Nonempty
 #align convex_body ConvexBody
 
@@ -69,8 +69,12 @@ protected theorem convex (K : ConvexBody V) : Convex ℝ (K : Set V) :=
 #align convex_body.convex ConvexBody.convex
 
 protected theorem isCompact (K : ConvexBody V) : IsCompact (K : Set V) :=
-  K.is_compact'
+  K.isCompact'
 #align convex_body.is_compact ConvexBody.isCompact
+
+-- porting note: new theorem
+protected theorem isClosed [T2Space V] (K : ConvexBody V) : IsClosed (K : Set V) :=
+  K.isCompact.isClosed
 
 protected theorem nonempty (K : ConvexBody V) : (K : Set V).Nonempty :=
   K.nonempty'
@@ -90,28 +94,33 @@ section ContinuousAdd
 
 variable [ContinuousAdd V]
 
-instance : AddMonoid (ConvexBody V) where
-  -- we cannot write K + L to avoid reducibility issues with the set.has_add instance
+-- we cannot write K + L to avoid reducibility issues with the set.has_add instance
+-- porting note: todo: is this^ still true?
+instance : Add (ConvexBody V) where
   add K L :=
-    ⟨Set.image2 (· + ·) K L, K.Convex.add L.Convex, K.IsCompact.add L.IsCompact,
-      K.Nonempty.add L.Nonempty⟩
-  add_assoc K L M := by
-    ext
-    simp only [coe_mk, Set.image2_add, add_assoc]
-  zero := ⟨0, convex_singleton 0, isCompact_singleton, Set.singleton_nonempty 0⟩
-  zero_add K := by
-    ext
-    simp only [coe_mk, Set.image2_add, zero_add]
-  add_zero K := by
-    ext
-    simp only [coe_mk, Set.image2_add, add_zero]
+    ⟨Set.image2 (· + ·) K L, K.convex.add L.convex, K.isCompact.add L.isCompact,
+      K.nonempty.add L.nonempty⟩
 
-@[simp]
+instance : Zero (ConvexBody V) where
+  zero := ⟨0, convex_singleton 0, isCompact_singleton, Set.singleton_nonempty 0⟩
+
+instance : SMul ℕ (ConvexBody V) where
+  smul := nsmulRec
+
+-- porting note: todo: add `@[simp, norm_cast]`
+theorem coe_nsmul : ∀ (n : ℕ) (K : ConvexBody V), ↑(n • K) = n • (K : Set V)
+  | 0, _ => rfl
+  | (n + 1), K => congr_arg₂ (Set.image2 (· + ·)) rfl (coe_nsmul n K)
+
+instance : AddMonoid (ConvexBody V) :=
+  SetLike.coe_injective.addMonoid (↑) rfl (fun _ _ ↦ rfl) fun _ _ ↦ coe_nsmul _ _
+
+@[simp] -- porting note: todo: add `norm_cast`
 theorem coe_add (K L : ConvexBody V) : (↑(K + L) : Set V) = (K : Set V) + L :=
   rfl
 #align convex_body.coe_add ConvexBody.coe_add
 
-@[simp]
+@[simp] -- porting note: todo: add `norm_cast`
 theorem coe_zero : (↑(0 : ConvexBody V) : Set V) = 0 :=
   rfl
 #align convex_body.coe_zero ConvexBody.coe_zero
@@ -120,41 +129,26 @@ instance : Inhabited (ConvexBody V) :=
   ⟨0⟩
 
 instance : AddCommMonoid (ConvexBody V) :=
-  { ConvexBody.addMonoid with
-    add_comm := fun K L => by
-      ext
-      simp only [coe_add, add_comm] }
+  SetLike.coe_injective.addCommMonoid (↑) rfl (fun _ _ ↦ rfl) fun _ _ ↦ coe_nsmul _ _
 
 end ContinuousAdd
 
 variable [ContinuousSMul ℝ V]
 
 instance : SMul ℝ (ConvexBody V)
-    where smul c K := ⟨c • (K : Set V), K.Convex.smul _, K.IsCompact.smul _, K.Nonempty.smul_set⟩
+    where smul c K := ⟨c • (K : Set V), K.convex.smul _, K.isCompact.smul _, K.nonempty.smul_set⟩
 
-@[simp]
+@[simp] -- porting note: todo: add `norm_cast`
 theorem coe_smul (c : ℝ) (K : ConvexBody V) : (↑(c • K) : Set V) = c • (K : Set V) :=
   rfl
 #align convex_body.coe_smul ConvexBody.coe_smul
 
 variable [ContinuousAdd V]
 
-instance : DistribMulAction ℝ (ConvexBody V) where
-  toSMul := ConvexBody.hasSmul
-  one_smul K := by
-    ext
-    simp only [coe_smul, one_smul]
-  mul_smul c d K := by
-    ext
-    simp only [coe_smul, mul_smul]
-  smul_add c K L := by
-    ext
-    simp only [coe_smul, coe_add, smul_add]
-  smul_zero c := by
-    ext
-    simp only [coe_smul, coe_zero, smul_zero]
+instance : DistribMulAction ℝ (ConvexBody V) :=
+  SetLike.coe_injective.distribMulAction ⟨⟨(↑), coe_zero⟩, coe_add⟩ coe_smul
 
-@[simp]
+@[simp] -- porting note: todo: add `norm_cast`
 theorem coe_smul' (c : ℝ≥0) (K : ConvexBody V) : (↑(c • K) : Set V) = c • (K : Set V) :=
   rfl
 #align convex_body.coe_smul' ConvexBody.coe_smul'
@@ -162,13 +156,8 @@ theorem coe_smul' (c : ℝ≥0) (K : ConvexBody V) : (↑(c • K) : Set V) = c 
 /-- The convex bodies in a fixed space $V$ form a module over the nonnegative reals.
 -/
 instance : Module ℝ≥0 (ConvexBody V) where
-  add_smul c d K := by
-    ext1
-    simp only [coe_smul, coe_add]
-    exact Convex.add_smul K.convex (NNReal.coe_nonneg _) (NNReal.coe_nonneg _)
-  zero_smul K := by
-    ext1
-    exact Set.zero_smul_set K.nonempty
+  add_smul c d K := SetLike.ext' <| Convex.add_smul K.convex c.coe_nonneg d.coe_nonneg
+  zero_smul K := SetLike.ext' <| Set.zero_smul_set K.nonempty
 
 end TVS
 
@@ -177,7 +166,7 @@ section SeminormedAddCommGroup
 variable [SeminormedAddCommGroup V] [NormedSpace ℝ V] (K L : ConvexBody V)
 
 protected theorem bounded : Metric.Bounded (K : Set V) :=
-  K.IsCompact.Bounded
+  K.isCompact.bounded
 #align convex_body.bounded ConvexBody.bounded
 
 theorem hausdorffEdist_ne_top {K L : ConvexBody V} : EMetric.hausdorffEdist (K : Set V) L ≠ ⊤ := by
@@ -192,6 +181,7 @@ noncomputable instance : PseudoMetricSpace (ConvexBody V) where
   dist_self _ := Metric.hausdorffDist_self_zero
   dist_comm _ _ := Metric.hausdorffDist_comm
   dist_triangle K L M := Metric.hausdorffDist_triangle hausdorffEdist_ne_top
+  edist_dist _ _ := by exact ENNReal.coe_nnreal_eq _
 
 @[simp, norm_cast]
 theorem hausdorffDist_coe : Metric.hausdorffDist (K : Set V) L = dist K L :=
@@ -201,7 +191,7 @@ theorem hausdorffDist_coe : Metric.hausdorffDist (K : Set V) L = dist K L :=
 @[simp, norm_cast]
 theorem hausdorffEdist_coe : EMetric.hausdorffEdist (K : Set V) L = edist K L := by
   rw [edist_dist]
-  exact (ENNReal.ofReal_toReal Hausdorff_edist_ne_top).symm
+  exact (ENNReal.ofReal_toReal hausdorffEdist_ne_top).symm
 #align convex_body.Hausdorff_edist_coe ConvexBody.hausdorffEdist_coe
 
 end SeminormedAddCommGroup
@@ -211,13 +201,10 @@ section NormedAddCommGroup
 variable [NormedAddCommGroup V] [NormedSpace ℝ V]
 
 /-- Convex bodies in a fixed normed space `V` form a metric space under the Hausdorff metric. -/
-noncomputable instance : MetricSpace (ConvexBody V)
-    where eq_of_dist_eq_zero K L hd :=
-    ConvexBody.ext <|
-      (K.IsCompact.IsClosed.hausdorffDist_zero_iff_eq L.IsCompact.IsClosed hausdorffEdist_ne_top).mp
-        hd
+noncomputable instance : MetricSpace (ConvexBody V) where
+  eq_of_dist_eq_zero {K L} hd := ConvexBody.ext <|
+    (K.isClosed.hausdorffDist_zero_iff_eq L.isClosed hausdorffEdist_ne_top).1 hd
 
 end NormedAddCommGroup
 
 end ConvexBody
-
