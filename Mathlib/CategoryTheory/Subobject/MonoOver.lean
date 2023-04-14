556c9662a0f398bcf7b4eef7ca665b5681e1b165
Joël Riou <joel.riou@universite-paris-saclay.fr>
2023-04-14 18:21:46 +0200

    fixed coercion

--- Mathlib/CategoryTheory/Subobject/MonoOver.lean
+++ Mathlib/CategoryTheory/Subobject/MonoOver.lean
@@ -75,8 +75,7 @@ def forget (X : C) : MonoOver X ⥤ Over X :=
   fullSubcategoryInclusion _
 #align category_theory.mono_over.forget CategoryTheory.MonoOver.forget
 
-@[nolint dangerousInstance]
-instance : Coe (MonoOver X) C where coe Y := Y.obj.left
+instance : CoeOut (MonoOver X) C where coe Y := Y.obj.left
 
 @[simp]
 theorem forget_obj_left {f} : ((forget X).obj f).left = (f : C) :=
