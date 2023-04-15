1dbb495df6b83033d2b7af1377247fe82f67aefd
Yury G. Kudryashov <65514131+Ruben-VandeVelde@users.noreply.github.com>
2023-04-15 00:49:34 +0000

    feat: port NumberTheory.LucasLehmer (#2988)
    This generalizes some results from `LeftCancelMonoid` to work around an issue in `LucasLehmer.order_ineq`.
    
    
    
    
    Co-authored-by: Yury G. Kudryashov <urkud@urkud.name>
    Co-authored-by: Scott Morrison <scott@tqft.net>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1360,6 +1360,7 @@ import Mathlib.NumberTheory.ClassNumber.AdmissibleAbsoluteValue
 import Mathlib.NumberTheory.Divisors
 import Mathlib.NumberTheory.Fermat4
 import Mathlib.NumberTheory.FrobeniusNumber
+import Mathlib.NumberTheory.LucasLehmer
 import Mathlib.NumberTheory.LucasPrimality
 import Mathlib.NumberTheory.Padics.PadicNorm
 import Mathlib.NumberTheory.Padics.PadicVal
