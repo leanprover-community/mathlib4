a2d435f6920c5dc7a4d5039c93152579461080e1
Scott Morrison <scott.morrison@gmail.com>
2023-04-15 12:10:15 +0930

    tidy

--- Extras/LibrarySearch.lean
+++ Extras/LibrarySearch.lean
@@ -3,9 +3,7 @@ Copyright (c) 2023 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison
 -/
--- FIXME change this back when I have internet
--- import Mathlib
-import Mathlib.Algebra.Hom.GroupInstances
+import Mathlib
 import Mathlib.Tactic.LibrarySearch
 import Mathlib.Control.Basic
 
