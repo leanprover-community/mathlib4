7dd809152cfc3b59bd1a4eb86c6fc1a1bd990faf
Parcly Taxel <reddeloostw@gmail.com>
2023-04-16 15:43:28 +0800

    Fix errors

--- Mathlib/Order/Category/DistLatCat.lean
+++ Mathlib/Order/Category/DistLatCat.lean
@@ -13,11 +13,11 @@ import Mathlib.Order.Category.LatCat
 /-!
 # The category of distributive lattices
 
-This file defines `DistLat`, the category of distributive lattices.
+This file defines `DistLatCat`, the category of distributive lattices.
 
-Note that [`DistLat`](https://ncatlab.org/nlab/show/DistLat) in the literature doesn't always
-correspond to `DistLat` as we don't require bottom or top elements. Instead, this `DistLat`
-corresponds to `BddDistLat`.
+Note that [`DistLatCat`](https://ncatlab.org/nlab/show/DistLat) in the literature doesn't always
+correspond to `DistLatCat` as we don't require bottom or top elements. Instead, this `DistLatCat`
+corresponds to `BddDistLatCat`.
 -/
 
 
@@ -25,73 +25,79 @@ universe u
 
 open CategoryTheory
 
+set_option linter.uppercaseLean3 false
+
 /-- The category of distributive lattices. -/
-def DistLat :=
+def DistLatCat :=
   Bundled DistribLattice
-#align DistLat DistLat
+#align DistLat DistLatCat
 
-namespace DistLat
+namespace DistLatCat
 
-instance : CoeSort DistLat (Type _) :=
-  Bundled.hasCoeToSort
+instance : CoeSort DistLatCat (Type _) :=
+  Bundled.coeSort
 
-instance (X : DistLat) : DistribLattice X :=
+instance (X : DistLatCat) : DistribLattice X :=
   X.str
 
-/-- Construct a bundled `DistLat` from a `distrib_lattice` underlying type and typeclass. -/
-def of (α : Type _) [DistribLattice α] : DistLat :=
+/-- Construct a bundled `DistLatCat` from a `DistribLattice` underlying type and typeclass. -/
+def of (α : Type _) [DistribLattice α] : DistLatCat :=
   Bundled.of α
-#align DistLat.of DistLat.of
+#align DistLat.of DistLatCat.of
 
 @[simp]
 theorem coe_of (α : Type _) [DistribLattice α] : ↥(of α) = α :=
   rfl
-#align DistLat.coe_of DistLat.coe_of
+#align DistLat.coe_of DistLatCat.coe_of
 
-instance : Inhabited DistLat :=
+instance : Inhabited DistLatCat :=
   ⟨of PUnit⟩
 
 instance : BundledHom.ParentProjection @DistribLattice.toLattice :=
   ⟨⟩
 
-deriving instance LargeCategory, ConcreteCategory for DistLat
+deriving instance LargeCategory for DistLatCat
 
-instance hasForgetToLat : HasForget₂ DistLat LatCat :=
+instance : ConcreteCategory DistLatCat :=
+  BundledHom.concreteCategory _
+
+instance hasForgetToLatCat : HasForget₂ DistLatCat LatCat :=
   BundledHom.forget₂ _ _
-#align DistLat.has_forget_to_Lat DistLat.hasForgetToLat
+#align DistLat.has_forget_to_Lat DistLatCat.hasForgetToLatCat
 
 /-- Constructs an equivalence between distributive lattices from an order isomorphism between them.
 -/
 @[simps]
-def Iso.mk {α β : DistLat.{u}} (e : α ≃o β) : α ≅ β where
-  Hom := e
-  inv := e.symm
-  hom_inv_id' := by
+def Iso.mk {α β : DistLatCat.{u}} (e : α ≃o β) : α ≅ β where
+  hom := (e : LatticeHom α β)
+  inv := (e.symm : LatticeHom β α)
+  hom_inv_id := by
     ext
     exact e.symm_apply_apply _
-  inv_hom_id' := by
+  inv_hom_id := by
     ext
     exact e.apply_symm_apply _
-#align DistLat.iso.mk DistLat.Iso.mk
+#align DistLat.iso.mk DistLatCat.Iso.mk
 
-/-- `order_dual` as a functor. -/
+/-- `OrderDual` as a functor. -/
 @[simps]
-def dual : DistLat ⥤ DistLat where
+def dual : DistLatCat ⥤ DistLatCat where
   obj X := of Xᵒᵈ
-  map X Y := LatticeHom.dual
-#align DistLat.dual DistLat.dual
+  map := LatticeHom.dual
+#align DistLat.dual DistLatCat.dual
 
-/-- The equivalence between `DistLat` and itself induced by `order_dual` both ways. -/
-@[simps Functor inverse]
-def dualEquiv : DistLat ≌ DistLat :=
-  Equivalence.mk dual dual
-    (NatIso.ofComponents (fun X => Iso.mk <| OrderIso.dualDual X) fun X Y f => rfl)
-    (NatIso.ofComponents (fun X => Iso.mk <| OrderIso.dualDual X) fun X Y f => rfl)
-#align DistLat.dual_equiv DistLat.dualEquiv
+/-- The equivalence between `DistLatCat` and itself induced by `OrderDual` both ways. -/
+@[simps functor inverse]
+def dualEquiv : DistLatCat ≌ DistLatCat where
+  functor := dual
+  inverse := dual
+  unitIso := NatIso.ofComponents (fun X => Iso.mk <| OrderIso.dualDual X) fun _ => rfl
+  counitIso := NatIso.ofComponents (fun X => Iso.mk <| OrderIso.dualDual X) fun _ => rfl
+#align DistLat.dual_equiv DistLatCat.dualEquiv
 
-end DistLat
+end DistLatCat
 
-theorem distLat_dual_comp_forget_to_latCat :
-    DistLat.dual ⋙ forget₂ DistLat LatCat = forget₂ DistLat LatCat ⋙ LatCat.dual :=
+theorem distLatCat_dual_comp_forget_to_latCat :
+    DistLatCat.dual ⋙ forget₂ DistLatCat LatCat = forget₂ DistLatCat LatCat ⋙ LatCat.dual :=
   rfl
-#align DistLat_dual_comp_forget_to_Lat distLat_dual_comp_forget_to_latCat
+#align DistLat_dual_comp_forget_to_Lat distLatCat_dual_comp_forget_to_latCat
