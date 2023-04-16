269dfc9a41fc1ed4238d57760e06d33263d7c476
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2023-04-16 18:57:56 +0100

    fix merge conflict issue

--- Mathlib/Analysis/NormedSpace/Basic.lean
+++ Mathlib/Analysis/NormedSpace/Basic.lean
@@ -97,10 +97,6 @@ theorem norm_smul [NormedSpace α β] (s : α) (x : β) : ‖s • x‖ = ‖s�
       ‖s‖ * ‖x‖ = ‖s‖ * ‖s⁻¹ • s • x‖ := by rw [inv_smul_smul₀ h]
       _ ≤ ‖s‖ * (‖s⁻¹‖ * ‖s • x‖) := (mul_le_mul_of_nonneg_left (norm_smul_le _ _) (norm_nonneg _))
       _ = ‖s • x‖ := by rw [norm_inv, ← mul_assoc, mul_inv_cancel (mt norm_eq_zero.1 h), one_mul]
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/master
 #align norm_smul norm_smul
 
 theorem norm_zsmul (α) [NormedField α] [NormedSpace α β] (n : ℤ) (x : β) :
