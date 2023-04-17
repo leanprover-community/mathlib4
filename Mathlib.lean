12e26e53ff84391488d3836ffad9c47ab4362ec2
Jeremy Tan Jie Rui <reddeloostw@gmail.com>
2023-04-17 08:52:27 +0000

    feat: port Order.Category.DistLatCat (#3458)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1390,6 +1390,7 @@ import Mathlib.Order.Bounded
 import Mathlib.Order.BoundedOrder
 import Mathlib.Order.Bounds.Basic
 import Mathlib.Order.Bounds.OrderIso
+import Mathlib.Order.Category.DistLatCat
 import Mathlib.Order.Category.LatCat
 import Mathlib.Order.Category.LinOrdCat
 import Mathlib.Order.Category.NonemptyFinLinOrdCat
