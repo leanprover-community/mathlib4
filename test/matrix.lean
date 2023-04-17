00539952005a6cab8eb1ceb8a4fd1d50998e63d7
Eric Wieser <wieser.eric@gmail.com>
2023-04-17 13:16:49 +0000

    fix remaining tests

--- test/matrix.lean
+++ test/matrix.lean
@@ -44,23 +44,23 @@ open Lean Elab in
 def run (x : TermElabM α) : Command.CommandElabM α := Command.liftTermElabM x
 
 -- we test equality of expressions here to ensure that we have `2` and not `1.succ` in the type
-run_cmd run do let d ← getDims ``(!![]);        guard $ d == (q(0), q(0))
-run_cmd run do let d ← getDims ``(!![;]);       guard $ d == (q(1), q(0))
-run_cmd run do let d ← getDims ``(!![;;]);      guard $ d == (q(2), q(0))
-run_cmd run do let d ← getDims ``(!![,]);       guard $ d == (q(0), q(1))
-run_cmd run do let d ← getDims ``(!![,,]);      guard $ d == (q(0), q(2))
-run_cmd run do let d ← getDims ``(!![1]);       guard $ d == (q(1), q(1))
-run_cmd run do let d ← getDims ``(!![1,]);      guard $ d == (q(1), q(1))
-run_cmd run do let d ← getDims ``(!![1;]);      guard $ d == (q(1), q(1))
-run_cmd run do let d ← getDims ``(!![1,2;3,4]); guard $ d == (q(2), q(2))
+run_cmd run do let d ← getDims `(!![]);        guard $ d == (q(0), q(0))
+run_cmd run do let d ← getDims `(!![;]);       guard $ d == (q(1), q(0))
+run_cmd run do let d ← getDims `(!![;;]);      guard $ d == (q(2), q(0))
+run_cmd run do let d ← getDims `(!![,]);       guard $ d == (q(0), q(1))
+run_cmd run do let d ← getDims `(!![,,]);      guard $ d == (q(0), q(2))
+run_cmd run do let d ← getDims `(!![1]);       guard $ d == (q(1), q(1))
+run_cmd run do let d ← getDims `(!![1,]);      guard $ d == (q(1), q(1))
+run_cmd run do let d ← getDims `(!![1;]);      guard $ d == (q(1), q(1))
+run_cmd run do let d ← getDims `(!![1,2;3,4]); guard $ d == (q(2), q(2))
 
 end dimensions
 
-run_cmd guard $ (!![1;2])       = of ![![1], ![2]]
-run_cmd guard $ (!![1,3])       = of ![![1,3]]
-run_cmd guard $ (!![1,2;3,4])   = of ![![1,2], ![3,4]]
-run_cmd guard $ (!![1,2;3,4;])  = of ![![1,2], ![3,4]]
-run_cmd guard $ (!![1,2,;3,4,]) = of ![![1,2], ![3,4]]
+run_cmd run do guard $ (!![1;2])       = of ![![1], ![2]]
+run_cmd run do guard $ (!![1,3])       = of ![![1,3]]
+run_cmd run do guard $ (!![1,2;3,4])   = of ![![1,2], ![3,4]]
+run_cmd run do guard $ (!![1,2;3,4;])  = of ![![1,2], ![3,4]]
+run_cmd run do guard $ (!![1,2,;3,4,]) = of ![![1,2], ![3,4]]
 
 example {a a' b b' c c' d d' : α} :
   !![a, b; c, d] + !![a', b'; c', d'] = !![a + a', b + b'; c + c', d + d'] :=
@@ -73,12 +73,17 @@ by simp
 example {a a' b b' c c' d d' : α} :
   !![a, b; c, d] ⬝ !![a', b'; c', d'] =
     !![a * a' + b * c', a * b' + b * d'; c * a' + d * c', c * b' + d * d'] :=
-by simp [-Equiv.Perm.coe_subsingleton]
+by simp
 
 example {a b c d x y : α} :
   mulVec !![a, b; c, d] ![x, y] = ![a * x + b * y, c * x + d * y] :=
 by simp
 
+/-!
+TODO: the below lemmas rely on simp lemmas assuming the indexing numerals are assembled from
+`bit0` and `bit1`, so no longer work in Lean 4
+-/
+/-
 example {a b c d : α} : submatrix !![a, b; c, d] ![1, 0] ![0] = !![c; a] :=
 by ext; simp
 
@@ -109,35 +114,32 @@ example {a b c d e f g h : α} : ![a, b, c, d, e, f, g, h] 5 = f := by simp
 example {a b c d e f g h : α} : ![a, b, c, d, e, f, g, h] 7 = h := by simp
 example {a b c d e f g h : α} : ![a, b, c, d, e, f, g, h] 37 = f := by simp
 example {a b c d e f g h : α} : ![a, b, c, d, e, f, g, h] 99 = d := by simp
+-/
 
--- todo: uncomment when porting `Matrix.det`
+-- TODO: uncomment snd update `try this` when porting `Matrix.det`
+/-
+example {α : Type _} [CommRing α] {a b c d : α} :
+    Matrix.det !![a, b; c, d] = a * d - b * c := by
+  simp [Matrix.det_succ_row_zero, Fin.sum_univ_succ]
+  /-
+  Try this: simp only [det_succ_row_zero, Fin.sum_univ_succ, neg_mul, mul_one,
+  Fin.default_eq_zero, Fin.coe_zero, one_mul, cons_val_one, Fin.coe_succ, univ_unique,
+  submatrix_apply, pow_one, Fin.zero_succ_above, Fin.succ_succ_above_zero,  finset.sum_singleton,
+  cons_val_zero, cons_val_succ, det_fin_zero, pow_zero]
+  -/
+  ring
 
--- example {α : Type*} [comm_ring α] {a b c d : α} :
---   Matrix.det !![a, b; c, d] = a * d - b * c :=
--- begin
---   simp [Matrix.det_succ_row_zero, Fin.sum_univ_succ],
---   /-
---   Try this: simp only [det_succ_row_zero, Fin.sum_univ_succ, neg_mul, mul_one,
---   Fin.default_eq_zero, Fin.coe_zero, one_mul, cons_val_one, Fin.coe_succ, univ_unique,
---   submatrix_apply, pow_one, Fin.zero_succ_above, Fin.succ_succ_above_zero,  finset.sum_singleton,
---   cons_val_zero, cons_val_succ, det_fin_zero, pow_zero]
---   -/
---   ring
--- end
-
--- example {α : Type*} [comm_ring α] {a b c d e f g h i : α} :
---   Matrix.det !![a, b, c; d, e, f; g, h, i] =
---     a * e * i - a * f * h - b * d * i + b * f * g + c * d * h - c * e * g :=
--- begin
---   simp [Matrix.det_succ_row_zero, Fin.sum_univ_succ],
---   /-
---   Try this: simp only [det_succ_row_zero, Fin.sum_univ_succ, neg_mul, cons_append,
---   mul_one, Fin.default_eq_zero, Fin.coe_zero, cons_vec_bit0_eq_alt0, one_mul, cons_val_one,
---   cons_vec_alt0, Fin.succ_succ_above_one, Fin.coe_succ, univ_unique, submatrix_apply, pow_one,
---   Fin.zero_succ_above, Fin.succ_zero_eq_one, Fin.succ_succ_above_zero, nat.neg_one_sq,
---   finset.sum_singleton, cons_val_zero, cons_val_succ, det_fin_zero, head_cons, pow_zero]
---    -/
---   ring
--- end
-
+example {α : Type _} [CommRing α] {a b c d e f g h i : α} :
+    Matrix.det !![a, b, c; d, e, f; g, h, i] =
+      a * e * i - a * f * h - b * d * i + b * f * g + c * d * h - c * e * g := by
+  simp [Matrix.det_succ_row_zero, Fin.sum_univ_succ]
+  /-
+  Try this: simp only [det_succ_row_zero, Fin.sum_univ_succ, neg_mul, cons_append,
+  mul_one, Fin.default_eq_zero, Fin.coe_zero, cons_vec_bit0_eq_alt0, one_mul, cons_val_one,
+  cons_vec_alt0, Fin.succ_succ_above_one, Fin.coe_succ, univ_unique, submatrix_apply, pow_one,
+  Fin.zero_succ_above, Fin.succ_zero_eq_one, Fin.succ_succ_above_zero, nat.neg_one_sq,
+  finset.sum_singleton, cons_val_zero, cons_val_succ, det_fin_zero, head_cons, pow_zero]
+   -/
+  ring
+-/
 end Matrix
