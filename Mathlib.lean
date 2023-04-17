0ec9f46182ff10de1a9209de0c1a5550cfb08322
Jason Yuen <jason_yuen2007@hotmail.com>
2023-04-17 07:01:43 +0000

    feat: port Geometry.Manifold.LocalInvariantProperties (#3401)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1135,6 +1135,7 @@ import Mathlib.FieldTheory.MvPolynomial
 import Mathlib.FieldTheory.PerfectClosure
 import Mathlib.FieldTheory.Subfield
 import Mathlib.Geometry.Manifold.ChartedSpace
+import Mathlib.Geometry.Manifold.LocalInvariantProperties
 import Mathlib.GroupTheory.Abelianization
 import Mathlib.GroupTheory.Archimedean
 import Mathlib.GroupTheory.Commensurable
