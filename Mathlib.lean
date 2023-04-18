6804474710e46b774eed24babb413da517e47ad6
Jason Yuen <jason_yuen2007@hotmail.com>
2023-04-18 03:37:00 +0000

    feat: port Control.Bitraversable.Lemmas (#3460)

--- Mathlib.lean
+++ Mathlib.lean
@@ -643,6 +643,7 @@ import Mathlib.Control.Applicative
 import Mathlib.Control.Basic
 import Mathlib.Control.Bifunctor
 import Mathlib.Control.Bitraversable.Basic
+import Mathlib.Control.Bitraversable.Lemmas
 import Mathlib.Control.EquivFunctor
 import Mathlib.Control.EquivFunctor.Instances
 import Mathlib.Control.Fix
