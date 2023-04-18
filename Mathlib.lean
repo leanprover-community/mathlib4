a372cbf84a66bd10fc26f660bb7fc901d60e8e84
Jason Yuen <jason_yuen2007@hotmail.com>
2023-04-18 11:12:26 +0000

    feat: port ModelTheory.Encoding (#3494)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1384,6 +1384,7 @@ import Mathlib.MeasureTheory.MeasurableSpace
 import Mathlib.MeasureTheory.MeasurableSpaceDef
 import Mathlib.MeasureTheory.PiSystem
 import Mathlib.ModelTheory.Basic
+import Mathlib.ModelTheory.Encoding
 import Mathlib.ModelTheory.LanguageMap
 import Mathlib.ModelTheory.Syntax
 import Mathlib.NumberTheory.ADEInequality
