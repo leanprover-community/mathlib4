c9aa46a30948143ea30b4fbb1fe6eb2af76f7ff1
Scott Morrison <scott@tqft.net>
2023-04-18 11:25:27 +0000

    feat: depth first and best first search using ListM (#3221)
    Implementations of depth first search, best first search, and beam search, for graphs described by a neighbours function `α → ListM m α`. There are also wrappers for using `α → List α`.
    
    This is only intended for use in meta code.
    
    
    
    Co-authored-by: Scott Morrison <scott.morrison@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -861,7 +861,9 @@ import Mathlib.Data.List.Sublists
 import Mathlib.Data.List.TFAE
 import Mathlib.Data.List.ToFinsupp
 import Mathlib.Data.List.Zip
-import Mathlib.Data.ListM
+import Mathlib.Data.ListM.Basic
+import Mathlib.Data.ListM.BestFirst
+import Mathlib.Data.ListM.DepthFirst
 import Mathlib.Data.ListM.Heartbeats
 import Mathlib.Data.Matrix.Basic
 import Mathlib.Data.Matrix.Basis
