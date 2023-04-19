0750c4879a739391144c061bf50bca7881e6e344
Alex J Best <alex.j.best@gmail.com>
2023-04-19 21:07:09 +0000

    add import

--- Mathlib/Tactic/Linarith/Frontend.lean
+++ Mathlib/Tactic/Linarith/Frontend.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Robert Y. Lewis
 Ported by: Scott Morrison
 -/
+import Mathlib.Control.Basic
 import Mathlib.Data.HashMap
 import Mathlib.Tactic.Linarith.Verification
 import Mathlib.Tactic.Linarith.Preprocessing
