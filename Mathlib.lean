542e28123d76e3231fb2a8735c895ea0a50ad2ba
Joël Riou <joel.riou@universite-paris-saclay.fr>
2023-04-19 11:37:16 +0000

    feat: port AlgebraicTopology.CechNerve (#3500)

--- Mathlib.lean
+++ Mathlib.lean
@@ -292,6 +292,7 @@ import Mathlib.Algebra.TrivSqZeroExt
 import Mathlib.Algebra.Tropical.Basic
 import Mathlib.Algebra.Tropical.BigOperators
 import Mathlib.Algebra.Tropical.Lattice
+import Mathlib.AlgebraicTopology.CechNerve
 import Mathlib.AlgebraicTopology.DoldKan.Compatibility
 import Mathlib.AlgebraicTopology.MooreComplex
 import Mathlib.AlgebraicTopology.SimplexCategory
