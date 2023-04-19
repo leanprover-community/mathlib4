c9a607f6035bfd472080a5568493eefd447c3e2c
Joël Riou <reddeloostw@gmail.com>
2023-04-19 15:37:15 +0000

    feat: port AlgebraicTopology.DoldKan.Notations (#3522)
    Co-authored-by: Joël Riou <joel.riou@universite-paris-saclay.fr>

--- Mathlib.lean
+++ Mathlib.lean
@@ -296,6 +296,7 @@ import Mathlib.Algebra.Tropical.Lattice
 import Mathlib.AlgebraicTopology.AlternatingFaceMapComplex
 import Mathlib.AlgebraicTopology.CechNerve
 import Mathlib.AlgebraicTopology.DoldKan.Compatibility
+import Mathlib.AlgebraicTopology.DoldKan.Notations
 import Mathlib.AlgebraicTopology.MooreComplex
 import Mathlib.AlgebraicTopology.SimplexCategory
 import Mathlib.AlgebraicTopology.SimplicialObject
