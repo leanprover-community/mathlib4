fe54b923eaddc77882fa3dc0bf532d59e0038d58
Adam Topaz <github@adamtopaz.com>
2023-04-20 15:22:27 +0000

    feat: Port CategoryTheory.Monad.Adjunction (#3528)
    ~~This is still WIP.~~ There may be a change to `reassoc` that would make this nicer, see [zulip](https://leanprover.zulipchat.com/#narrow/stream/287929-mathlib4/topic/Bug.20in.20reassoc.3F/near/351111877)

--- Mathlib.lean
+++ Mathlib.lean
@@ -530,6 +530,7 @@ import Mathlib.CategoryTheory.Linear.LinearFunctor
 import Mathlib.CategoryTheory.Localization.Construction
 import Mathlib.CategoryTheory.Localization.Opposite
 import Mathlib.CategoryTheory.Localization.Predicate
+import Mathlib.CategoryTheory.Monad.Adjunction
 import Mathlib.CategoryTheory.Monad.Algebra
 import Mathlib.CategoryTheory.Monad.Basic
 import Mathlib.CategoryTheory.Monad.Kleisli
