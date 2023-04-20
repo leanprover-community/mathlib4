d394799843a82bfe253bd296b973ae0c80cd2d96
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2023-04-20 15:22:29 +0000

    feat: port LinearAlgebra.Matrix.MvPolynomial (#3547)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1359,6 +1359,7 @@ import Mathlib.LinearAlgebra.LinearIndependent
 import Mathlib.LinearAlgebra.LinearPMap
 import Mathlib.LinearAlgebra.Matrix.Determinant
 import Mathlib.LinearAlgebra.Matrix.DotProduct
+import Mathlib.LinearAlgebra.Matrix.MvPolynomial
 import Mathlib.LinearAlgebra.Matrix.Orthogonal
 import Mathlib.LinearAlgebra.Matrix.Symmetric
 import Mathlib.LinearAlgebra.Matrix.Trace
