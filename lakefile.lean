068a885017eb13d6a2fdef14a7fad2109fcc7d37
Wojciech Nawrocki <wjnawrocki@protonmail.com>
2023-04-21 13:21:57 -0400

    chore: bump proofwidgets

--- lakefile.lean
+++ lakefile.lean
@@ -22,7 +22,7 @@ require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
 require std from git "https://github.com/leanprover/std4" @ "main"
 require Qq from git "https://github.com/gebner/quote4" @ "master"
 require aesop from git "https://github.com/JLimperg/aesop" @ "master"
-require ProofWidget from git "https://github.com/EdAyers/ProofWidgets4" @ "v0.0.3"
+require proofwidgets from git "https://github.com/EdAyers/ProofWidgets4" @ "v0.0.6"
 
 lean_lib Cache where
   roots := #[`Cache]
