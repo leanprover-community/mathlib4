4cdf635c7222f376bac4e5a1c3e05f04ce63c78e
Wojciech Nawrocki <wjnawrocki@protonmail.com>
2023-04-21 11:54:56 -0400

    doc: clarify widget instructions

--- Mathlib/Tactic/GPT/Sagredo/Widget.lean
+++ Mathlib/Tactic/GPT/Sagredo/Widget.lean
@@ -6,8 +6,7 @@ Authors: Wojciech Nawrocki, Scott Morrison
 import ProofWidgets.Component.Basic
 import Mathlib.Tactic.GPT.Sagredo.Dialog
 
--- This requires `npm install react-markdown`
--- Then in `./widget`, `npm run build`.
+-- Then in `./widget`, `npm install && npm run build`.
 
 namespace Mathlib.Tactic.GPT.Sagredo.Widget
 
