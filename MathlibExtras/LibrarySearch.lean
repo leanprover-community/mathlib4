2b3232b9159eb9dac89abaafffb5fad8b7158c41
Scott Morrison <scott.morrison@gmail.com>
2023-04-21 16:42:13 +1000

    fix

--- MathlibExtras/LibrarySearch.lean
+++ MathlibExtras/LibrarySearch.lean
@@ -18,6 +18,6 @@ open Lean.Elab.Command
 open Mathlib.Tactic.LibrarySearch
 
 run_cmd liftTermElabM do
-  let path ← cachePath
+  let path ← Option.get! <$> cachePath
   _ ← path.parent.mapM fun p => IO.FS.createDirAll p
   pickle path (← (← buildDiscrTree).get) `LibrarySearch
