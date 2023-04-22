154c94d41174377000e1b85638efc599b3a5d826
Joël Riou <joel.riou@universite-paris-saclay.fr>
2023-04-22 22:40:56 +0200

    it compiles

--- Mathlib/AlgebraicTopology/DoldKan/HomotopyEquivalence.lean
+++ Mathlib/AlgebraicTopology/DoldKan/HomotopyEquivalence.lean
@@ -15,15 +15,14 @@ import Mathlib.AlgebraicTopology.DoldKan.Normalized
 # The normalized Moore complex and the alternating face map complex are homotopy equivalent
 
 In this file, when the category `A` is abelian, we obtain the homotopy equivalence
-`homotopy_equiv_normalized_Moore_complex_alternating_face_map_complex` between the
+`homotopyEquivNormalizedMooreComplexAlternatingFaceMapComplex` between the
 normalized Moore complex and the alternating face map complex of a simplicial object in `A`.
 
 -/
 
 
-open CategoryTheory CategoryTheory.Category CategoryTheory.Limits CategoryTheory.Preadditive
-
-open Simplicial DoldKan
+open CategoryTheory CategoryTheory.Category CategoryTheory.Limits
+  CategoryTheory.Preadditive Simplicial DoldKan
 
 noncomputable section
 
@@ -40,46 +39,45 @@ noncomputable def homotopyPToId : ∀ q : ℕ, Homotopy (P q : K[X] ⟶ _) (𝟙
     refine'
       Homotopy.trans (Homotopy.ofEq _)
         (Homotopy.trans
-          (Homotopy.add (homotopy_P_to_id q) (Homotopy.compLeft (homotopy_Hσ_to_zero q) (P q)))
+          (Homotopy.add (homotopyPToId q) (Homotopy.compLeft (homotopyHσToZero q) (P q)))
           (Homotopy.ofEq _))
-    · unfold P
-      simp only [comp_add, comp_id]
+    · simp only [P_succ, comp_add, comp_id]
     · simp only [add_zero, comp_zero]
+set_option linter.uppercaseLean3 false in
 #align algebraic_topology.dold_kan.homotopy_P_to_id AlgebraicTopology.DoldKan.homotopyPToId
 
 /-- The complement projection `Q q` to `P q` is homotopic to zero. -/
 def homotopyQToZero (q : ℕ) : Homotopy (Q q : K[X] ⟶ _) 0 :=
   Homotopy.equivSubZero.toFun (homotopyPToId X q).symm
+set_option linter.uppercaseLean3 false in
 #align algebraic_topology.dold_kan.homotopy_Q_to_zero AlgebraicTopology.DoldKan.homotopyQToZero
 
 theorem homotopyPToId_eventually_constant {q n : ℕ} (hqn : n < q) :
-    ((homotopyPToId X (q + 1)).Hom n (n + 1) : X _[n] ⟶ X _[n + 1]) =
-      (homotopyPToId X q).Hom n (n + 1) := by
-  unfold homotopy_P_to_id
-  simp only [homotopy_Hσ_to_zero, hσ'_eq_zero hqn (c_mk (n + 1) n rfl), Homotopy.trans_hom,
-    Pi.add_apply, Homotopy.ofEq_hom, Pi.zero_apply, Homotopy.add_hom, Homotopy.compLeft_hom,
-    Homotopy.nullHomotopy'_hom, ComplexShape.down_Rel, eq_self_iff_true, dite_eq_ite, if_true,
-    comp_zero, add_zero, zero_add]
+    ((homotopyPToId X (q + 1)).hom n (n + 1) : X _[n] ⟶ X _[n + 1]) =
+      (homotopyPToId X q).hom n (n + 1) := by
+  simp only [homotopyHσToZero, AlternatingFaceMapComplex.obj_X, Nat.add_eq, Homotopy.trans_hom,
+    Homotopy.ofEq_hom, Pi.zero_apply, Homotopy.add_hom, Homotopy.compLeft_hom, add_zero,
+    Homotopy.nullHomotopy'_hom, ComplexShape.down_Rel, hσ'_eq_zero hqn (c_mk (n + 1) n rfl),
+    dite_eq_ite, ite_self, comp_zero, zero_add, homotopyPToId]
+  rfl
+set_option linter.uppercaseLean3 false in
 #align algebraic_topology.dold_kan.homotopy_P_to_id_eventually_constant AlgebraicTopology.DoldKan.homotopyPToId_eventually_constant
 
-variable (X)
-
-/-- Construction of the homotopy from `P_infty` to the identity using eventually
+/-- Construction of the homotopy from `PInfty` to the identity using eventually
 (termwise) constant homotopies from `P q` to the identity for all `q` -/
 @[simps]
 def homotopyPInftyToId : Homotopy (PInfty : K[X] ⟶ _) (𝟙 _) where
-  Hom i j := (homotopyPToId X (j + 1)).Hom i j
-  zero' i j hij := Homotopy.zero _ i j hij
+  hom i j := (homotopyPToId X (j + 1)).hom i j
+  zero i j hij := Homotopy.zero _ i j hij
   comm n := by
-    cases n
-    ·
-      simpa only [Homotopy.dNext_zero_chainComplex, Homotopy.prevD_chainComplex, P_f_0_eq, zero_add,
-        HomologicalComplex.id_f, P_infty_f] using (homotopy_P_to_id X 2).comm 0
-    ·
-      simpa only [Homotopy.dNext_succ_chainComplex, Homotopy.prevD_chainComplex,
-        HomologicalComplex.id_f, P_infty_f, ← P_is_eventually_constant (rfl.le : n + 1 ≤ n + 1),
-        homotopy_P_to_id_eventually_constant X (lt_add_one (n + 1))] using
-        (homotopy_P_to_id X (n + 2)).comm (n + 1)
+    rcases n with _|n
+    . simpa only [Homotopy.dNext_zero_chainComplex, Homotopy.prevD_chainComplex,
+        PInfty_f, Nat.zero_eq, P_f_0_eq, zero_add] using (homotopyPToId X 2).comm 0
+    · simpa only [Homotopy.dNext_succ_chainComplex, Homotopy.prevD_chainComplex,
+        HomologicalComplex.id_f, PInfty_f, ← P_is_eventually_constant (rfl.le : n + 1 ≤ n + 1),
+        homotopyPToId_eventually_constant X (lt_add_one (n + 1))] using
+        (homotopyPToId X (n + 2)).comm (n + 1)
+set_option linter.uppercaseLean3 false in
 #align algebraic_topology.dold_kan.homotopy_P_infty_to_id AlgebraicTopology.DoldKan.homotopyPInftyToId
 
 /-- The inclusion of the Moore complex in the alternating face map complex
@@ -88,16 +86,15 @@ is an homotopy equivalence -/
 def homotopyEquivNormalizedMooreComplexAlternatingFaceMapComplex {A : Type _} [Category A]
     [Abelian A] {Y : SimplicialObject A} :
     HomotopyEquiv ((normalizedMooreComplex A).obj Y) ((alternatingFaceMapComplex A).obj Y) where
-  Hom := inclusionOfMooreComplexMap Y
-  inv := pInftyToNormalizedMooreComplex Y
+  hom := inclusionOfMooreComplexMap Y
+  inv := PInftyToNormalizedMooreComplex Y
   homotopyHomInvId := Homotopy.ofEq (splitMonoInclusionOfMooreComplexMap Y).id
-  homotopyInvHomId :=
-    Homotopy.trans
-      (Homotopy.ofEq (pInftyToNormalizedMooreComplex_comp_inclusionOfMooreComplexMap Y))
+  homotopyInvHomId := Homotopy.trans
+      (Homotopy.ofEq (PInftyToNormalizedMooreComplex_comp_inclusionOfMooreComplexMap Y))
       (homotopyPInftyToId Y)
+set_option linter.uppercaseLean3 false in
 #align algebraic_topology.dold_kan.homotopy_equiv_normalized_Moore_complex_alternating_face_map_complex AlgebraicTopology.DoldKan.homotopyEquivNormalizedMooreComplexAlternatingFaceMapComplex
 
 end DoldKan
 
 end AlgebraicTopology
-
