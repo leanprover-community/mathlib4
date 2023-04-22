f9886d5057ea21303134262d2a770d9e25b4fbd0
Jeremy Tan Jie Rui <reddeloostw@gmail.com>
2023-04-22 09:23:40 +0000

    feat: port LinearAlgebra.AffineSpace.Basis (#3578)
    Co-authored-by: Scott Morrison <scott.morrison@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1342,6 +1342,7 @@ import Mathlib.LinearAlgebra.AffineSpace.AffineEquiv
 import Mathlib.LinearAlgebra.AffineSpace.AffineMap
 import Mathlib.LinearAlgebra.AffineSpace.AffineSubspace
 import Mathlib.LinearAlgebra.AffineSpace.Basic
+import Mathlib.LinearAlgebra.AffineSpace.Basis
 import Mathlib.LinearAlgebra.AffineSpace.Combination
 import Mathlib.LinearAlgebra.AffineSpace.Independent
 import Mathlib.LinearAlgebra.AffineSpace.Midpoint
