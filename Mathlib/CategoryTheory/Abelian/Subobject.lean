233ed8e485700534599bef14426a373f265aa444
Joël Riou <joel.riou@universite-paris-saclay.fr>
2023-04-23 18:13:15 +0200

    it compiles

--- Mathlib/CategoryTheory/Abelian/Subobject.lean
+++ Mathlib/CategoryTheory/Abelian/Subobject.lean
@@ -31,39 +31,35 @@ variable {C : Type u} [Category.{v} C]
     order-isomorphic via taking kernels and cokernels.
     Implemented here using subobjects in the opposite category,
     since mathlib does not have a notion of quotient objects at the time of writing. -/
-@[simps]
+@[simps!]
 def subobjectIsoSubobjectOp [Abelian C] (X : C) : Subobject X ≃o (Subobject (op X))ᵒᵈ := by
-  refine' OrderIso.ofHomInv (cokernel_order_hom X) (kernel_order_hom X) _ _
-  · change (cokernel_order_hom X).comp (kernel_order_hom X) = _
-    refine' OrderHom.ext _ _ (funext (subobject.ind _ _))
+  refine' OrderIso.ofHomInv (cokernelOrderHom X) (kernelOrderHom X) _ _
+  · change (cokernelOrderHom X).comp (kernelOrderHom X) = _
+    refine' OrderHom.ext _ _ (funext (Subobject.ind _ _))
     intro A f hf
-    dsimp only [OrderHom.comp_coe, Function.comp_apply, kernel_order_hom_coe, subobject.lift_mk,
-      cokernel_order_hom_coe, OrderHom.id_coe, id.def]
-    refine' subobject.mk_eq_mk_of_comm _ _ ⟨_, _, Quiver.Hom.unop_inj _, Quiver.Hom.unop_inj _⟩ _
-    · exact (abelian.epi_desc f.unop _ (cokernel.condition (kernel.ι f.unop))).op
+    dsimp only [OrderHom.comp_coe, Function.comp_apply, kernelOrderHom_coe, Subobject.lift_mk,
+      cokernelOrderHom_coe, OrderHom.id_coe, id.def]
+    refine' Subobject.mk_eq_mk_of_comm _ _ ⟨_, _, Quiver.Hom.unop_inj _, Quiver.Hom.unop_inj _⟩ _
+    · exact (Abelian.epiDesc f.unop _ (cokernel.condition (kernel.ι f.unop))).op
     · exact (cokernel.desc _ _ (kernel.condition f.unop)).op
-    ·
-      simp only [← cancel_epi (cokernel.π (kernel.ι f.unop)), unop_comp, Quiver.Hom.unop_op,
-        unop_id_op, cokernel.π_desc_assoc, comp_epi_desc, category.comp_id]
-    ·
-      simp only [← cancel_epi f.unop, unop_comp, Quiver.Hom.unop_op, unop_id, comp_epi_desc_assoc,
-        cokernel.π_desc, category.comp_id]
-    · exact Quiver.Hom.unop_inj (by simp only [unop_comp, Quiver.Hom.unop_op, comp_epi_desc])
-  · change (kernel_order_hom X).comp (cokernel_order_hom X) = _
-    refine' OrderHom.ext _ _ (funext (subobject.ind _ _))
+    · rw [← cancel_epi (cokernel.π (kernel.ι f.unop))]
+      simp only [unop_comp, Quiver.Hom.unop_op, unop_id_op, cokernel.π_desc_assoc,
+        comp_epiDesc, Category.comp_id]
+    · simp only [← cancel_epi f.unop, unop_comp, Quiver.Hom.unop_op, unop_id, comp_epiDesc_assoc,
+        cokernel.π_desc, Category.comp_id]
+    · exact Quiver.Hom.unop_inj (by simp only [unop_comp, Quiver.Hom.unop_op, comp_epiDesc])
+  · change (kernelOrderHom X).comp (cokernelOrderHom X) = _
+    refine' OrderHom.ext _ _ (funext (Subobject.ind _ _))
     intro A f hf
-    dsimp only [OrderHom.comp_coe, Function.comp_apply, cokernel_order_hom_coe, subobject.lift_mk,
-      kernel_order_hom_coe, OrderHom.id_coe, id.def, unop_op, Quiver.Hom.unop_op]
-    refine' subobject.mk_eq_mk_of_comm _ _ ⟨_, _, _, _⟩ _
-    · exact abelian.mono_lift f _ (kernel.condition (cokernel.π f))
+    dsimp only [OrderHom.comp_coe, Function.comp_apply, cokernelOrderHom_coe, Subobject.lift_mk,
+      kernelOrderHom_coe, OrderHom.id_coe, id.def, unop_op, Quiver.Hom.unop_op]
+    refine' Subobject.mk_eq_mk_of_comm _ _ ⟨_, _, _, _⟩ _
+    · exact Abelian.monoLift f _ (kernel.condition (cokernel.π f))
     · exact kernel.lift _ _ (cokernel.condition f)
-    ·
-      simp only [← cancel_mono (kernel.ι (cokernel.π f)), category.assoc, image.fac, mono_lift_comp,
-        category.id_comp, autoParam_eq]
-    ·
-      simp only [← cancel_mono f, category.assoc, mono_lift_comp, image.fac, category.id_comp,
-        autoParam_eq]
-    · simp only [mono_lift_comp]
+    · simp only [← cancel_mono (kernel.ι (cokernel.π f)), Category.assoc, image.fac, monoLift_comp,
+        Category.id_comp]
+    · simp only [← cancel_mono f, Category.assoc, monoLift_comp, image.fac, Category.id_comp]
+    · simp only [monoLift_comp]
 #align category_theory.abelian.subobject_iso_subobject_op CategoryTheory.Abelian.subobjectIsoSubobjectOp
 
 /-- A well-powered abelian category is also well-copowered. -/
@@ -73,4 +69,3 @@ instance wellPowered_opposite [Abelian C] [WellPowered C] : WellPowered Cᵒᵖ
 #align category_theory.abelian.well_powered_opposite CategoryTheory.Abelian.wellPowered_opposite
 
 end CategoryTheory.Abelian
-
