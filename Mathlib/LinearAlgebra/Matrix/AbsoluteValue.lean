07e7bb19799aefb03b8ca0126331c35927c30d14
int-y1 <jason_yuen2007@hotmail.com>
2023-04-22 17:04:12 -0700

    fix

--- Mathlib/LinearAlgebra/Matrix/AbsoluteValue.lean
+++ Mathlib/LinearAlgebra/Matrix/AbsoluteValue.lean
@@ -18,11 +18,11 @@ This file proves some bounds on matrices involving absolute values.
 
 ## Main results
 
- * `matrix.det_le`: if the entries of an `n × n` matrix are bounded by `x`,
+ * `Matrix.det_le`: if the entries of an `n × n` matrix are bounded by `x`,
    then the determinant is bounded by `n! x^n`
- * `matrix.det_sum_le`: if we have `s` `n × n` matrices and the entries of each
+ * `Matrix.det_sum_le`: if we have `s` `n × n` matrices and the entries of each
    matrix are bounded by `x`, then the determinant of their sum is bounded by `n! (s * x)^n`
- * `matrix.det_sum_smul_le`: if we have `s` `n × n` matrices each multiplied by
+ * `Matrix.det_sum_smul_le`: if we have `s` `n × n` matrices each multiplied by
    a constant bounded by `y`, and the entries of each matrix are bounded by `x`,
    then the determinant of the linear combination is bounded by `n! (s * y * x)^n`
 -/
@@ -43,17 +43,16 @@ variable {n : Type _} [Fintype n] [DecidableEq n]
 theorem det_le {A : Matrix n n R} {abv : AbsoluteValue R S} {x : S} (hx : ∀ i j, abv (A i j) ≤ x) :
     abv A.det ≤ Nat.factorial (Fintype.card n) • x ^ Fintype.card n :=
   calc
-    abv A.det = abv (∑ σ : Perm n, _) := congr_arg abv (det_apply _)
-    _ ≤ ∑ σ : Perm n, abv _ := (abv.sum_le _ _)
+    abv A.det = abv (∑ σ : Perm n, Perm.sign σ • ∏ i, A (σ i) i) := congr_arg abv (det_apply _)
+    _ ≤ ∑ σ : Perm n, abv (Perm.sign σ • ∏ i, A (σ i) i) := abv.sum_le _ _
     _ = ∑ σ : Perm n, ∏ i, abv (A (σ i) i) :=
-      (sum_congr rfl fun σ hσ => by rw [abv.map_units_int_smul, abv.map_prod])
-    _ ≤ ∑ σ : Perm n, ∏ i : n, x :=
-      (sum_le_sum fun _ _ => prod_le_prod (fun _ _ => abv.NonNeg _) fun _ _ => hx _ _)
-    _ = ∑ σ : Perm n, x ^ Fintype.card n :=
+      (sum_congr rfl fun σ _ => by rw [abv.map_units_int_smul, abv.map_prod])
+    _ ≤ ∑ σ : Perm n, ∏ _i : n, x :=
+      (sum_le_sum fun _ _ => prod_le_prod (fun _ _ => abv.nonneg _) fun _ _ => hx _ _)
+    _ = ∑ _σ : Perm n, x ^ Fintype.card n :=
       (sum_congr rfl fun _ _ => by rw [prod_const, Finset.card_univ])
     _ = Nat.factorial (Fintype.card n) • x ^ Fintype.card n := by
       rw [sum_const, Finset.card_univ, Fintype.card_perm]
-    
 #align matrix.det_le Matrix.det_le
 
 theorem det_sum_le {ι : Type _} (s : Finset ι) {A : ι → Matrix n n R} {abv : AbsoluteValue R S}
@@ -64,9 +63,8 @@ theorem det_sum_le {ι : Type _} (s : Finset ι) {A : ι → Matrix n n R} {abv 
     calc
       abv ((∑ k in s, A k) i j) = abv (∑ k in s, A k i j) := by simp only [sum_apply]
       _ ≤ ∑ k in s, abv (A k i j) := (abv.sum_le _ _)
-      _ ≤ ∑ k in s, x := (sum_le_sum fun k _ => hx k i j)
+      _ ≤ ∑ _k in s, x := (sum_le_sum fun k _ => hx k i j)
       _ = s.card • x := sum_const _
-      
 #align matrix.det_sum_le Matrix.det_sum_le
 
 theorem det_sum_smul_le {ι : Type _} (s : Finset ι) {c : ι → R} {A : ι → Matrix n n R}
@@ -79,8 +77,6 @@ theorem det_sum_smul_le {ι : Type _} (s : Finset ι) {c : ι → R} {A : ι →
       calc
         abv (c k * A k i j) = abv (c k) * abv (A k i j) := abv.map_mul _ _
         _ ≤ y * x := mul_le_mul (hy k) (hx k i j) (abv.nonneg _) ((abv.nonneg _).trans (hy k))
-        
 #align matrix.det_sum_smul_le Matrix.det_sum_smul_le
 
 end Matrix
-
