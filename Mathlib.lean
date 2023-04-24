63206757720f030484ecca910282d2a7c1755e58
Yury G. Kudryashov <urkud@urkud.name>
2023-04-24 22:52:30 +0000

    feat: port Data.Real.Hyperreal (#3586)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1104,6 +1104,7 @@ import Mathlib.Data.Real.ConjugateExponents
 import Mathlib.Data.Real.ENNReal
 import Mathlib.Data.Real.ENatENNReal
 import Mathlib.Data.Real.EReal
+import Mathlib.Data.Real.Hyperreal
 import Mathlib.Data.Real.NNReal
 import Mathlib.Data.Real.Pointwise
 import Mathlib.Data.Real.Sign
