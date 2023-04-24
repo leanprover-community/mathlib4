ea6ecb49e975c6e5ac768e852e67347c31562e04
Scott Morrison <scott.morrison@gmail.com>
2023-04-24 22:48:21 +1000

    import all

--- Mathlib.lean
+++ Mathlib.lean
@@ -1343,6 +1343,7 @@ import Mathlib.Lean.Meta.DiscrTree
 import Mathlib.Lean.Meta.Simp
 import Mathlib.Lean.Name
 import Mathlib.Lean.SMap
+import Mathlib.Lean.System.IO
 import Mathlib.LinearAlgebra.AffineSpace.AffineEquiv
 import Mathlib.LinearAlgebra.AffineSpace.AffineMap
 import Mathlib.LinearAlgebra.AffineSpace.AffineSubspace
