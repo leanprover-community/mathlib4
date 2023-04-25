66535631cfd589cff787a90bba0fbfa942459486
Jeremy Tan Jie Rui <kmill31415@gmail.com>
2023-04-25 11:42:55 +0000

    feat: port LinearAlgebra.FiniteDimensional (#3466)
    Co-authored-by: Parcly Taxel <reddeloostw@gmail.com>
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>
    Co-authored-by: Johan Commelin <johan@commelin.net>
    Co-authored-by: Scott Morrison <scott.morrison@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1385,6 +1385,7 @@ import Mathlib.LinearAlgebra.Dfinsupp
 import Mathlib.LinearAlgebra.Dimension
 import Mathlib.LinearAlgebra.DirectSum.Finsupp
 import Mathlib.LinearAlgebra.DirectSum.TensorProduct
+import Mathlib.LinearAlgebra.FiniteDimensional
 import Mathlib.LinearAlgebra.Finrank
 import Mathlib.LinearAlgebra.Finsupp
 import Mathlib.LinearAlgebra.FinsuppVectorSpace
