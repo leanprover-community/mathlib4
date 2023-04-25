d8af7b4fb87921ef569a90e7bde70ca08b6932fb
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2023-04-25 15:53:41 +0000

    feat: port LinearAlgebra.Matrix.NonsingularInverse (#3647)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1411,6 +1411,7 @@ import Mathlib.LinearAlgebra.Matrix.Determinant
 import Mathlib.LinearAlgebra.Matrix.DotProduct
 import Mathlib.LinearAlgebra.Matrix.MvPolynomial
 import Mathlib.LinearAlgebra.Matrix.Nondegenerate
+import Mathlib.LinearAlgebra.Matrix.NonsingularInverse
 import Mathlib.LinearAlgebra.Matrix.Orthogonal
 import Mathlib.LinearAlgebra.Matrix.Polynomial
 import Mathlib.LinearAlgebra.Matrix.Reindex
