d9a2280aebe448b1ed5c7240ac60acdc97436e38
Scott Morrison <scott.morrison@gmail.com>
2023-04-26 10:35:03 +1000

    nolint

--- Mathlib/Data/TypeMax.lean
+++ Mathlib/Data/TypeMax.lean
@@ -3,6 +3,7 @@ Copyright (c) 2023 Mario Carneiro. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro
 -/
+import Std.Tactic.Lint
 
 /-!
 # FIXME needs documentation
@@ -11,4 +12,5 @@ See also
 https://leanprover.zulipchat.com/#narrow/stream/287929-mathlib4/topic/!4.233463.20universe.20constraint.20issues
 -/
 
+@[nolint checkUnivs]
 abbrev TypeMax.{u, v} := Type max u v
