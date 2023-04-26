df86b00cd65a8d31c4ff45a88185a0d5ff32e2d3
Scott Morrison <scott.morrison@gmail.com>
2023-04-26 22:32:23 +1000

    correct source module

--- Mathlib/Topology/Category/Top/Limits/Basic.lean
+++ Mathlib/Topology/Category/Top/Limits/Basic.lean
@@ -3,7 +3,7 @@ Copyright (c) 2017 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Patrick Massot, Scott Morrison, Mario Carneiro, Andrew Yang
 
-! This file was ported from Lean 3 source module topology.category.Top.limits
+! This file was ported from Lean 3 source module topology.category.Top.limits.basic
 ! leanprover-community/mathlib commit 8195826f5c428fc283510bc67303dd4472d78498
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
