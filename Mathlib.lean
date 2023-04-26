39154da3296b0d0a0567e4d71ce15fcb372133b4
Parcly Taxel <reddeloostw@gmail.com>
2023-04-26 13:12:44 +0800

    Prepare for merge

--- Mathlib.lean
+++ Mathlib.lean
@@ -1506,7 +1506,7 @@ import Mathlib.SetTheory.Ordinal.FixedPoint
 import Mathlib.SetTheory.Ordinal.NaturalOps
 import Mathlib.SetTheory.Ordinal.Principal
 import Mathlib.SetTheory.Ordinal.Topology
-import Mathlib.SetTheory.Zfc.Basic
+import Mathlib.SetTheory.ZFC.Basic
 import Mathlib.Tactic
 import Mathlib.Tactic.Abel
 import Mathlib.Tactic.Alias
