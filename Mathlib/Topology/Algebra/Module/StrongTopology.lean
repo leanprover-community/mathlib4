79fb0c594908670cfef017fb0a3be199b42ccacc
Scott Morrison <scott.morrison@gmail.com>
2023-04-28 09:29:52 +1000

    fix

--- Mathlib/Topology/Algebra/Module/StrongTopology.lean
+++ Mathlib/Topology/Algebra/Module/StrongTopology.lean
@@ -130,7 +130,8 @@ theorem strongTopology.topologicalAddGroup [TopologicalSpace F] [TopologicalAddG
   haveI : UniformAddGroup F := comm_topologicalAddGroup_is_uniform
   letI : UniformSpace (E →SL[σ] F) := strongUniformity σ F 𝔖
   haveI : UniformAddGroup (E →SL[σ] F) := strongUniformity.uniformAddGroup σ F 𝔖
-  infer_instance
+  -- Porting note: this is not found by `inferInstance`, I'm not sure why.
+  exact UniformAddGroup.to_topologicalAddGroup
 #align continuous_linear_map.strong_topology.topological_add_group ContinuousLinearMap.strongTopology.topologicalAddGroup
 
 theorem strongTopology.t2Space [TopologicalSpace F] [TopologicalAddGroup F] [T2Space F]
