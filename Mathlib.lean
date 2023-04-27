1c05ad40e8f7a79f4cd8739c09abf85dd9d425a0
Matthew Robert Ballard <matt@mrb.email>
2023-04-27 14:06:43 +0000

    feat: port Topology.Category.Top.Limits.Basic (#3487)
    Co-authored-by: Scott Morrison <scott@tqft.net>
    Co-authored-by: Scott Morrison <scott.morrison@gmail.com>
    Co-authored-by: Jeremy Tan Jie Rui <reddeloostw@gmail.com>
    Co-authored-by: Parcly Taxel <reddeloostw@gmail.com>
    Co-authored-by: adamtopaz <github@adamtopaz.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1905,6 +1905,7 @@ import Mathlib.Topology.Category.Born
 import Mathlib.Topology.Category.Top.Adjunctions
 import Mathlib.Topology.Category.Top.Basic
 import Mathlib.Topology.Category.Top.EpiMono
+import Mathlib.Topology.Category.Top.Limits.Basic
 import Mathlib.Topology.Category.Top.Opens
 import Mathlib.Topology.CompactOpen
 import Mathlib.Topology.Connected
