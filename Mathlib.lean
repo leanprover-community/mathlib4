ea1dfb2dc1c13c459b796a395b7d967d893e8b28
Scott Morrison <scott@tqft.net>
2023-04-27 08:42:08 +0000

    chore: use TypeMax in CategoryTheory.Limits.Types (#3653)
    Per discussion at https://leanprover.zulipchat.com/#narrow/stream/287929-mathlib4/topic/!4.233463.20universe.20constraint.20issues
    
    
    
    Co-authored-by: Scott Morrison <scott.morrison@gmail.com>
    Co-authored-by: Matthew Ballard <matt@mrb.email>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1206,6 +1206,7 @@ import Mathlib.Data.Sym.Sym2
 import Mathlib.Data.Sym.Sym2.Init
 import Mathlib.Data.Tree
 import Mathlib.Data.TwoPointing
+import Mathlib.Data.TypeMax
 import Mathlib.Data.TypeVec
 import Mathlib.Data.TypeVec.Attr
 import Mathlib.Data.UInt
