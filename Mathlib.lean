d1a1c6b104967ffed8ca69ebb4f3ea9a4eb25424
Joël Riou <joel.riou@universite-paris-saclay.fr>
2023-04-27 21:43:33 +0000

    feat: port Analysis.Convex.StoneSeparation (#3686)

--- Mathlib.lean
+++ Mathlib.lean
@@ -350,6 +350,7 @@ import Mathlib.Analysis.Convex.Segment
 import Mathlib.Analysis.Convex.SimplicialComplex.Basic
 import Mathlib.Analysis.Convex.Slope
 import Mathlib.Analysis.Convex.Star
+import Mathlib.Analysis.Convex.StoneSeparation
 import Mathlib.Analysis.Convex.Strict
 import Mathlib.Analysis.Convex.Topology
 import Mathlib.Analysis.Hofer
