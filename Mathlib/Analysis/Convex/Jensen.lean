dacdc0791dd7625ecf65efb659cec6d16ab9d1e9
Scott Morrison <scott.morrison@gmail.com>
2023-04-28 15:58:09 +1000

    remove unnecessary universe variables

--- Mathlib/Analysis/Convex/Jensen.lean
+++ Mathlib/Analysis/Convex/Jensen.lean
@@ -129,7 +129,7 @@ theorem ConcaveOn.exists_le_of_centerMass (h : ConcaveOn 𝕜 s f) (hw₀ : ∀ 
 /-- Maximum principle for convex functions. If a function `f` is convex on the convex hull of `s`,
 then the eventual maximum of `f` on `convexHull 𝕜 s` lies in `s`. -/
 theorem ConvexOn.exists_ge_of_mem_convexHull (hf : ConvexOn 𝕜 (convexHull 𝕜 s) f) {x}
-    (hx : x ∈ convexHull.{u, v} 𝕜 s) : ∃ y ∈ s, f x ≤ f y := by
+    (hx : x ∈ convexHull 𝕜 s) : ∃ y ∈ s, f x ≤ f y := by
   rw [_root_.convexHull_eq] at hx
   obtain ⟨α, t, w, p, hw₀, hw₁, hp, rfl⟩ := hx
   rcases hf.exists_ge_of_centerMass hw₀ (hw₁.symm ▸ zero_lt_one) fun i hi =>
