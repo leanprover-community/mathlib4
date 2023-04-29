7f9ab3b9143e5e98970abcbb1ffb687c26ef8495
Joël Riou <joel.riou@universite-paris-saclay.fr>
2023-04-29 18:45:42 +0200

    it compiles

--- Mathlib/Algebra/Category/GroupCat/ZModuleEquivalence.lean
+++ Mathlib/Algebra/Category/GroupCat/ZModuleEquivalence.lean
@@ -8,7 +8,7 @@ Authors: Scott Morrison
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
-import Mathlib.Algebra.Category.Module.Basic
+import Mathlib.Algebra.Category.ModuleCat.Basic
 
 /-!
 The forgetful functor from ℤ-modules to additive commutative groups is
@@ -29,28 +29,33 @@ universe u
 namespace ModuleCat
 
 /-- The forgetful functor from `ℤ` modules to `AddCommGroup` is full. -/
-instance forget₂AddCommGroupFull : Full (forget₂ (ModuleCat ℤ) AddCommGroupCat.{u})
-    where preimage A B
-    f :=-- `add_monoid_hom.to_int_linear_map` doesn't work here because `A` and `B` are not definitionally
-    -- equal to the canonical `add_comm_group.int_module` module instances it expects.
-    { toFun := f
-      map_add' := AddMonoidHom.map_add f
-      map_smul' := fun n x => by
-        rw [int_smul_eq_zsmul, int_smul_eq_zsmul, map_zsmul, RingHom.id_apply] }
+instance forget₂AddCommGroupFull : Full (forget₂ (ModuleCat ℤ) AddCommGroupCat.{u}) where
+  preimage {A B}
+    -- `AddMonoidHom.toIntLinearMap` doesn't work here because `A` and `B` are not
+    -- definitionally equal to the canonical `AddCommGroup.intModule` module
+    -- instances it expects.
+    f := @LinearMap.mk _ _ _ _ _ _ _ _ _ A.isModule B.isModule
+        { toFun := f,
+          map_add' := AddMonoidHom.map_add (show AddMonoidHom A.carrier B.carrier from f) }
+        (fun n x => by
+          convert AddMonoidHom.map_zsmul (show A.carrier →+ B.carrier from f) x n <;>
+            ext <;> apply int_smul_eq_zsmul)
+set_option linter.uppercaseLean3 false in
 #align Module.forget₂_AddCommGroup_full ModuleCat.forget₂AddCommGroupFull
 
 /-- The forgetful functor from `ℤ` modules to `AddCommGroup` is essentially surjective. -/
 instance forget₂_addCommGroupCat_essSurj : EssSurj (forget₂ (ModuleCat ℤ) AddCommGroupCat.{u})
     where mem_essImage A :=
     ⟨ModuleCat.of ℤ A,
-      ⟨{  Hom := 𝟙 A
+      ⟨{  hom := 𝟙 A
           inv := 𝟙 A }⟩⟩
+set_option linter.uppercaseLean3 false in
 #align Module.forget₂_AddCommGroup_ess_surj ModuleCat.forget₂_addCommGroupCat_essSurj
 
 noncomputable instance forget₂AddCommGroupIsEquivalence :
     IsEquivalence (forget₂ (ModuleCat ℤ) AddCommGroupCat.{u}) :=
   Equivalence.ofFullyFaithfullyEssSurj (forget₂ (ModuleCat ℤ) AddCommGroupCat)
+set_option linter.uppercaseLean3 false in
 #align Module.forget₂_AddCommGroup_is_equivalence ModuleCat.forget₂AddCommGroupIsEquivalence
 
 end ModuleCat
-
