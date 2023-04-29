385ed76c7eeb31e4f8db084119054c883b4e1c19
Joël Riou <joel.riou@universite-paris-saclay.fr>
2023-04-29 23:07:53 +0000

    feat: port Algebra.Category.Group.ZModuleEquivalence (#3732)

--- Mathlib.lean
+++ Mathlib.lean
@@ -31,6 +31,7 @@ import Mathlib.Algebra.BigOperators.RingEquiv
 import Mathlib.Algebra.Bounds
 import Mathlib.Algebra.Category.GroupCat.Basic
 import Mathlib.Algebra.Category.GroupCat.Preadditive
+import Mathlib.Algebra.Category.GroupCat.ZModuleEquivalence
 import Mathlib.Algebra.Category.GroupCat.Zero
 import Mathlib.Algebra.Category.ModuleCat.Basic
 import Mathlib.Algebra.Category.ModuleCat.EpiMono
