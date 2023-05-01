c539f3e0737048ea10390c71701eaefc9eae9b98
Eric Wieser <wieser.eric@gmail.com>
2023-05-01 14:50:46 +0000

    remove mathport warnings

--- Mathlib/Topology/Category/Top/Limits/Basic.lean
+++ Mathlib/Topology/Category/Top/Limits/Basic.lean
@@ -41,12 +41,6 @@ variable {J : Type v} [SmallCategory J]
 -- mathport name: exprforget
 local notation "forget" => forget TopCat
 
-/- warning: Top.limit_cone -> TopCat.limitCone is a dubious translation:
-lean 3 declaration is
-  forall {J : Type.{u2}} [_inst_1 : CategoryTheory.SmallCategory.{u2} J] (F : CategoryTheory.Functor.{u2, max u2 u1, u2, succ (max u2 u1)} J _inst_1 TopCat.{max u2 u1} TopCat.largeCategory.{max u2 u1}), CategoryTheory.Limits.Cone.{u2, max u2 u1, u2, succ (max u2 u1)} J _inst_1 TopCat.{max u2 u1} TopCat.largeCategory.{max u2 u1} F
-but is expected to have type
-  forall {J : Type.{u1}} [_inst_1 : CategoryTheory.SmallCategory.{u1} J] (F : CategoryTheory.Functor.{u1, max u2 u1, u1, max (succ u2) (succ u1)} J _inst_1 TopCatMax.{u1, u2} instTopCatLargeCategory.{max u2 u1}), CategoryTheory.Limits.Cone.{u1, max u2 u1, u1, max (succ u2) (succ u1)} J _inst_1 TopCatMax.{u1, u2} instTopCatLargeCategory.{max u2 u1} F
-Case conversion may be inaccurate. Consider using '#align Top.limit_cone TopCat.limitConeₓ'. -/
 /-- A choice of limit cone for a functor `F : J ⥤ Top`.
 Generally you should just use `limit.cone F`, unless you need the actual definition
 (which is in terms of `types.limit_cone`).
@@ -61,12 +55,6 @@ def limitCone (F : J ⥤ TopCat.{max v u}) : Cone F where
             show Continuous ((fun u : ∀ j : J, F.obj j => u j) ∘ Subtype.val) by continuity } }
 #align Top.limit_cone TopCat.limitCone
 
-/- warning: Top.limit_cone_infi -> TopCat.limitConeInfi is a dubious translation:
-lean 3 declaration is
-  forall {J : Type.{u2}} [_inst_1 : CategoryTheory.SmallCategory.{u2} J] (F : CategoryTheory.Functor.{u2, max u2 u1, u2, succ (max u2 u1)} J _inst_1 TopCat.{max u2 u1} TopCat.largeCategory.{max u2 u1}), CategoryTheory.Limits.Cone.{u2, max u2 u1, u2, succ (max u2 u1)} J _inst_1 TopCat.{max u2 u1} TopCat.largeCategory.{max u2 u1} F
-but is expected to have type
-  forall {J : Type.{u1}} [_inst_1 : CategoryTheory.SmallCategory.{u1} J] (F : CategoryTheory.Functor.{u1, max u2 u1, u1, max (succ u2) (succ u1)} J _inst_1 TopCatMax.{u1, u2} instTopCatLargeCategory.{max u2 u1}), CategoryTheory.Limits.Cone.{u1, max u2 u1, u1, max (succ u2) (succ u1)} J _inst_1 TopCatMax.{u1, u2} instTopCatLargeCategory.{max u2 u1} F
-Case conversion may be inaccurate. Consider using '#align Top.limit_cone_infi TopCat.limitConeInfiₓ'. -/
 /-- A choice of limit cone for a functor `F : J ⥤ Top` whose topology is defined as an
 infimum of topologies infimum.
 Generally you should just use `limit.cone F`, unless you need the actual definition
@@ -83,12 +71,6 @@ def limitConeInfi (F : J ⥤ TopCat.{max v u}) : Cone F where
         ContinuousMap.coe_injective ((Types.limitCone (F ⋙ forget)).π.naturality f) }
 #align Top.limit_cone_infi TopCat.limitConeInfi
 
-/- warning: Top.limit_cone_is_limit -> TopCat.limitConeIsLimit is a dubious translation:
-lean 3 declaration is
-  forall {J : Type.{u2}} [_inst_1 : CategoryTheory.SmallCategory.{u2} J] (F : CategoryTheory.Functor.{u2, max u2 u1, u2, succ (max u2 u1)} J _inst_1 TopCat.{max u2 u1} TopCat.largeCategory.{max u2 u1}), CategoryTheory.Limits.IsLimit.{u2, max u2 u1, u2, succ (max u2 u1)} J _inst_1 TopCat.{max u2 u1} TopCat.largeCategory.{max u2 u1} F (TopCat.limitCone.{u1, u2} J _inst_1 F)
-but is expected to have type
-  forall {J : Type.{u1}} [_inst_1 : CategoryTheory.SmallCategory.{u1} J] (F : CategoryTheory.Functor.{u1, max u2 u1, u1, max (succ u2) (succ u1)} J _inst_1 TopCatMax.{u1, u2} instTopCatLargeCategory.{max u2 u1}), CategoryTheory.Limits.IsLimit.{u1, max u2 u1, u1, max (succ u2) (succ u1)} J _inst_1 TopCatMax.{u1, u2} instTopCatLargeCategory.{max u2 u1} F (TopCat.limitCone.{u1, u2} J _inst_1 F)
-Case conversion may be inaccurate. Consider using '#align Top.limit_cone_is_limit TopCat.limitConeIsLimitₓ'. -/
 /-- The chosen cone `Top.limit_cone F` for a functor `F : J ⥤ Top` is a limit cone.
 Generally you should just use `limit.is_limit F`, unless you need the actual definition
 (which is in terms of `types.limit_cone_is_limit`).
@@ -106,12 +88,6 @@ def limitConeIsLimit (F : J ⥤ TopCat.{max v u}) : IsLimit (limitCone F) where
     simpa [← h]
 #align Top.limit_cone_is_limit TopCat.limitConeIsLimit
 
-/- warning: Top.limit_cone_infi_is_limit -> TopCat.limitConeInfiIsLimit is a dubious translation:
-lean 3 declaration is
-  forall {J : Type.{u2}} [_inst_1 : CategoryTheory.SmallCategory.{u2} J] (F : CategoryTheory.Functor.{u2, max u2 u1, u2, succ (max u2 u1)} J _inst_1 TopCat.{max u2 u1} TopCat.largeCategory.{max u2 u1}), CategoryTheory.Limits.IsLimit.{u2, max u2 u1, u2, succ (max u2 u1)} J _inst_1 TopCat.{max u2 u1} TopCat.largeCategory.{max u2 u1} F (TopCat.limitConeInfi.{u1, u2} J _inst_1 F)
-but is expected to have type
-  forall {J : Type.{u1}} [_inst_1 : CategoryTheory.SmallCategory.{u1} J] (F : CategoryTheory.Functor.{u1, max u2 u1, u1, max (succ u2) (succ u1)} J _inst_1 TopCatMax.{u1, u2} instTopCatLargeCategory.{max u2 u1}), CategoryTheory.Limits.IsLimit.{u1, max u2 u1, u1, max (succ u2) (succ u1)} J _inst_1 TopCatMax.{u1, u2} instTopCatLargeCategory.{max u2 u1} F (TopCat.limitConeInfi.{u1, u2} J _inst_1 F)
-Case conversion may be inaccurate. Consider using '#align Top.limit_cone_infi_is_limit TopCat.limitConeInfiIsLimitₓ'. -/
 /-- The chosen cone `Top.limit_cone_infi F` for a functor `F : J ⥤ Top` is a limit cone.
 Generally you should just use `limit.is_limit F`, unless you need the actual definition
 (which is in terms of `types.limit_cone_is_limit`).
@@ -125,12 +101,6 @@ def limitConeInfiIsLimit (F : J ⥤ TopCat.{max v u}) : IsLimit (limitConeInfi F
           (continuous_iff_coinduced_le.mp (s.π.app j).Continuous : _))
 #align Top.limit_cone_infi_is_limit TopCat.limitConeInfiIsLimit
 
-/- warning: Top.Top_has_limits_of_size -> TopCat.topCat_hasLimitsOfSize is a dubious translation:
-lean 3 declaration is
-  CategoryTheory.Limits.HasLimitsOfSize.{u2, u2, max u2 u1, succ (max u2 u1)} TopCat.{max u2 u1} TopCat.largeCategory.{max u2 u1}
-but is expected to have type
-  CategoryTheory.Limits.HasLimitsOfSize.{u1, u1, max u2 u1, max (succ u2) (succ u1)} TopCatMax.{u1, u2} instTopCatLargeCategory.{max u2 u1}
-Case conversion may be inaccurate. Consider using '#align Top.Top_has_limits_of_size TopCat.topCat_hasLimitsOfSizeₓ'. -/
 instance topCat_hasLimitsOfSize : HasLimitsOfSize.{v} TopCat.{max v u}
     where HasLimitsOfShape J 𝒥 :=
     {
@@ -146,12 +116,6 @@ instance topCat_hasLimits : HasLimits TopCat.{u} :=
 #align Top.Top_has_limits TopCat.topCat_hasLimits
 -/
 
-/- warning: Top.forget_preserves_limits_of_size -> TopCat.forgetPreservesLimitsOfSize is a dubious translation:
-lean 3 declaration is
-  CategoryTheory.Limits.PreservesLimitsOfSize.{v, v, max v u, max v u, succ (max v u), succ (max v u)} TopCat.{max v u} TopCat.largeCategory.{max v u} Type.{max v u} CategoryTheory.types.{max v u} (CategoryTheory.forget.{succ (max v u), max v u, max v u} TopCat.{max v u} TopCat.largeCategory.{max v u} TopCat.concreteCategory.{max v u})
-but is expected to have type
-  CategoryTheory.Limits.PreservesLimitsOfSize.{v, v, v, v, succ v, succ v} TopCat.{v} instTopCatLargeCategory.{v} Type.{v} CategoryTheory.types.{v} (CategoryTheory.forget.{succ v, v, v} TopCat.{v} instTopCatLargeCategory.{v} TopCat.concreteCategory.{v})
-Case conversion may be inaccurate. Consider using '#align Top.forget_preserves_limits_of_size TopCat.forgetPreservesLimitsOfSizeₓ'. -/
 instance forgetPreservesLimitsOfSize :
     PreservesLimitsOfSize.{v, v} (forget : TopCat.{max v u} ⥤ Type max v u)
     where PreservesLimitsOfShape J 𝒥 :=
@@ -167,12 +131,6 @@ instance forgetPreservesLimits : PreservesLimits (forget : TopCat.{u} ⥤ Type u
 #align Top.forget_preserves_limits TopCat.forgetPreservesLimits
 -/
 
-/- warning: Top.colimit_cocone -> TopCat.colimitCocone is a dubious translation:
-lean 3 declaration is
-  forall {J : Type.{u2}} [_inst_1 : CategoryTheory.SmallCategory.{u2} J] (F : CategoryTheory.Functor.{u2, max u2 u1, u2, succ (max u2 u1)} J _inst_1 TopCat.{max u2 u1} TopCat.largeCategory.{max u2 u1}), CategoryTheory.Limits.Cocone.{u2, max u2 u1, u2, succ (max u2 u1)} J _inst_1 TopCat.{max u2 u1} TopCat.largeCategory.{max u2 u1} F
-but is expected to have type
-  forall {J : Type.{u1}} [_inst_1 : CategoryTheory.SmallCategory.{u1} J] (F : CategoryTheory.Functor.{u1, max u2 u1, u1, max (succ u2) (succ u1)} J _inst_1 TopCatMax.{u1, u2} instTopCatLargeCategory.{max u2 u1}), CategoryTheory.Limits.Cocone.{u1, max u2 u1, u1, max (succ u2) (succ u1)} J _inst_1 TopCatMax.{u1, u2} instTopCatLargeCategory.{max u2 u1} F
-Case conversion may be inaccurate. Consider using '#align Top.colimit_cocone TopCat.colimitCoconeₓ'. -/
 /-- A choice of colimit cocone for a functor `F : J ⥤ Top`.
 Generally you should just use `colimit.coone F`, unless you need the actual definition
 (which is in terms of `types.colimit_cocone`).
@@ -188,12 +146,6 @@ def colimitCocone (F : J ⥤ TopCat.{max v u}) : Cocone F where
         ContinuousMap.coe_injective ((Types.colimitCocone (F ⋙ forget)).ι.naturality f) }
 #align Top.colimit_cocone TopCat.colimitCocone
 
-/- warning: Top.colimit_cocone_is_colimit -> TopCat.colimitCoconeIsColimit is a dubious translation:
-lean 3 declaration is
-  forall {J : Type.{u2}} [_inst_1 : CategoryTheory.SmallCategory.{u2} J] (F : CategoryTheory.Functor.{u2, max u2 u1, u2, succ (max u2 u1)} J _inst_1 TopCat.{max u2 u1} TopCat.largeCategory.{max u2 u1}), CategoryTheory.Limits.IsColimit.{u2, max u2 u1, u2, succ (max u2 u1)} J _inst_1 TopCat.{max u2 u1} TopCat.largeCategory.{max u2 u1} F (TopCat.colimitCocone.{u1, u2} J _inst_1 F)
-but is expected to have type
-  forall {J : Type.{u1}} [_inst_1 : CategoryTheory.SmallCategory.{u1} J] (F : CategoryTheory.Functor.{u1, max u2 u1, u1, max (succ u2) (succ u1)} J _inst_1 TopCatMax.{u1, u2} instTopCatLargeCategory.{max u2 u1}), CategoryTheory.Limits.IsColimit.{u1, max u2 u1, u1, max (succ u2) (succ u1)} J _inst_1 TopCatMax.{u1, u2} instTopCatLargeCategory.{max u2 u1} F (TopCat.colimitCocone.{u1, u2} J _inst_1 F)
-Case conversion may be inaccurate. Consider using '#align Top.colimit_cocone_is_colimit TopCat.colimitCoconeIsColimitₓ'. -/
 /-- The chosen cocone `Top.colimit_cocone F` for a functor `F : J ⥤ Top` is a colimit cocone.
 Generally you should just use `colimit.is_colimit F`, unless you need the actual definition
 (which is in terms of `types.colimit_cocone_is_colimit`).
@@ -208,12 +160,6 @@ def colimitCoconeIsColimit (F : J ⥤ TopCat.{max v u}) : IsColimit (colimitCoco
           (continuous_iff_coinduced_le.mp (s.ι.app j).Continuous : _))
 #align Top.colimit_cocone_is_colimit TopCat.colimitCoconeIsColimit
 
-/- warning: Top.Top_has_colimits_of_size -> TopCat.topCat_hasColimitsOfSize is a dubious translation:
-lean 3 declaration is
-  CategoryTheory.Limits.HasColimitsOfSize.{u2, u2, max u2 u1, succ (max u2 u1)} TopCat.{max u2 u1} TopCat.largeCategory.{max u2 u1}
-but is expected to have type
-  CategoryTheory.Limits.HasColimitsOfSize.{u1, u1, max u2 u1, max (succ u2) (succ u1)} TopCatMax.{u1, u2} instTopCatLargeCategory.{max u2 u1}
-Case conversion may be inaccurate. Consider using '#align Top.Top_has_colimits_of_size TopCat.topCat_hasColimitsOfSizeₓ'. -/
 instance topCat_hasColimitsOfSize : HasColimitsOfSize.{v} TopCat.{max v u}
     where HasColimitsOfShape J 𝒥 :=
     {
@@ -229,12 +175,6 @@ instance topCat_hasColimits : HasColimits TopCat.{u} :=
 #align Top.Top_has_colimits TopCat.topCat_hasColimits
 -/
 
-/- warning: Top.forget_preserves_colimits_of_size -> TopCat.forgetPreservesColimitsOfSize is a dubious translation:
-lean 3 declaration is
-  CategoryTheory.Limits.PreservesColimitsOfSize.{u2, u2, max u2 u1, max u2 u1, succ (max u2 u1), succ (max u2 u1)} TopCat.{max u2 u1} TopCat.largeCategory.{max u2 u1} Type.{max u2 u1} CategoryTheory.types.{max u2 u1} (CategoryTheory.forget.{succ (max u2 u1), max u2 u1, max u2 u1} TopCat.{max u2 u1} TopCat.largeCategory.{max u2 u1} TopCat.concreteCategory.{max u2 u1})
-but is expected to have type
-  CategoryTheory.Limits.PreservesColimitsOfSize.{u1, u1, max u1 u2, max u1 u2, succ (max u1 u2), succ (max u1 u2)} TopCat.{max u1 u2} instTopCatLargeCategory.{max u1 u2} Type.{max u1 u2} CategoryTheory.types.{max u1 u2} (CategoryTheory.forget.{succ (max u1 u2), max u1 u2, max u1 u2} TopCat.{max u1 u2} instTopCatLargeCategory.{max u1 u2} TopCat.concreteCategory.{max u1 u2})
-Case conversion may be inaccurate. Consider using '#align Top.forget_preserves_colimits_of_size TopCat.forgetPreservesColimitsOfSizeₓ'. -/
 instance forgetPreservesColimitsOfSize :
     PreservesColimitsOfSize.{v, v} (forget : TopCat.{max v u} ⥤ Type max v u)
     where PreservesColimitsOfShape J 𝒥 :=
@@ -275,4 +215,3 @@ def initialIsoPempty : ⊥_ TopCat.{u} ≅ TopCat.of PEmpty :=
 #align Top.initial_iso_pempty TopCat.initialIsoPempty
 
 end TopCat
-
