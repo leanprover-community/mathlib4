dae6b1cf6d49e3bf0d6c1d23068f1e4f883ea0d8
Eric Wieser <wieser.eric@gmail.com>
2023-05-03 09:37:04 +0000

    fix

--- Mathlib/Data/Complex/Module.lean
+++ Mathlib/Data/Complex/Module.lean
@@ -352,7 +352,8 @@ def liftAux (I' : A) (hf : I' * I' = -1) : ℂ →ₐ[ℝ] A :=
       rw [add_mul, mul_add, mul_add, add_comm _ (y₁ • I' * y₂ • I'), add_add_add_comm]
       congr 1
       -- equate "real" and "imaginary" parts
-      · rw [smul_mul_smul, hf, smul_neg, ← Algebra.algebraMap_eq_smul_one, ← sub_eq_add_neg, ←
+      · let inst : SMulCommClass ℝ A A := by infer_instance  -- porting note: added
+        rw [smul_mul_smul, hf, smul_neg, ← Algebra.algebraMap_eq_smul_one, ← sub_eq_add_neg, ←
           RingHom.map_mul, ← RingHom.map_sub]
       · rw [Algebra.smul_def, Algebra.smul_def, Algebra.smul_def, ← Algebra.right_comm _ x₂, ←
           mul_assoc, ← add_mul, ← RingHom.map_mul, ← RingHom.map_mul, ← RingHom.map_add]
