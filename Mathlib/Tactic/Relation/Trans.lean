01e5f5705395d8748bc9c7ed33b5076dab0ec3cb
Floris van Doorn <fpvdoorn@gmail.com>
2023-05-03 18:16:34 +0200

    fix

--- Mathlib/Tactic/Relation/Trans.lean
+++ Mathlib/Tactic/Relation/Trans.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Siddhartha Gadgil, Mario Carneiro
 -/
 import Mathlib.Lean.Meta
-import Mathlib.Lean.Elab.Tactic.Basic
 import Lean.Elab.Tactic.Location
 
 /-!
