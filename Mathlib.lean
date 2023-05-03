8ab103915addb073d28433c15de0d8dfde02007c
Moritz Firsching <moritz.firsching@gmail.com>
2023-05-03 13:39:13 +0000

    feat: port CategoryTheory.Monoidal.Free.Basic (#2808)
    Co-authored-by: Moritz Firsching <firsching@google.com>
    Co-authored-by: Scott Morrison <scott.morrison@gmail.com>
    Co-authored-by: Joël Riou <joel.riou@universite-paris-saclay.fr>
    Co-authored-by: Johan Commelin <johan@commelin.net>

--- Mathlib.lean
+++ Mathlib.lean
@@ -595,6 +595,7 @@ import Mathlib.CategoryTheory.Monad.Types
 import Mathlib.CategoryTheory.Monoidal.Category
 import Mathlib.CategoryTheory.Monoidal.Discrete
 import Mathlib.CategoryTheory.Monoidal.End
+import Mathlib.CategoryTheory.Monoidal.Free.Basic
 import Mathlib.CategoryTheory.Monoidal.Functor
 import Mathlib.CategoryTheory.Monoidal.Functorial
 import Mathlib.CategoryTheory.Monoidal.Linear
