c643d1282c5c47f210c9497d1e19867dbd1bcb74
MonadMaverick <MonadMaverick@pm.me>
2023-05-04 08:44:21 +0000

    feat: port MeasureTheory.Measure.MeasureSpaceDef (#3325)
    Co-authored-by: Jeremy Tan Jie Rui <reddeloostw@gmail.com>
    Co-authored-by: Komyyy <pol_tta@outlook.jp>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1527,6 +1527,7 @@ import Mathlib.Mathport.Syntax
 import Mathlib.MeasureTheory.CardMeasurableSpace
 import Mathlib.MeasureTheory.MeasurableSpace
 import Mathlib.MeasureTheory.MeasurableSpaceDef
+import Mathlib.MeasureTheory.Measure.MeasureSpaceDef
 import Mathlib.MeasureTheory.Measure.OuterMeasure
 import Mathlib.MeasureTheory.PiSystem
 import Mathlib.ModelTheory.Basic
