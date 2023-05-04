65b0afafefd560152996de2f36f156c8dc327165
Scott Morrison <scott.morrison@gmail.com>
2023-05-04 10:38:57 +1000

    ask for Lean 4 syntax

--- test/ChatGPT.lean
+++ test/ChatGPT.lean
@@ -6,7 +6,7 @@ open GPT
 
 -- #eval OPENAI_API_KEY
 -- #eval send "Hello world" (cfg := { trace := true })
--- #eval send "Please write just the statement of the abc conjecture, in Lean 3 syntax."
+-- #eval send "Please write just the statement of the abc conjecture, in Lean 4 syntax."
 
 -- Check if "GPT-4" has been disabled because you don't have access yet:
 -- #eval show IO _  from do return (← unavailableModels.get).toList
