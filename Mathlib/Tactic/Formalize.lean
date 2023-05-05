d3eedcbf1859eea7cb0f2c596554758ccf0e96bd
Scott Morrison <scott.morrison@anu.edu.au>
2023-05-05 21:10:40 +1000

    move #formalize back to mathlib

--- Mathlib/Tactic/Formalize.lean
+++ Mathlib/Tactic/Formalize.lean
@@ -3,12 +3,68 @@ Copyright (c) 2023 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison
 -/
-import LLM.Formalize
+import LLM.FindChatBot
 
 /-!
-# `#formalize` command, requesting auto-formalization via an LLM.
+# `#formalize` command, requesting auto-formalization via a large language model.
 
-Running `#formalize` without an LLM setup will print a message with instructions.
+You must have a large language model available for this to work.
 
-The file is a no-op, it only serves to import `LLM.Formalize` into `Mathlib.Tactic`.
+Recommended is to use an OpenAI API key, with available quota,
+stored in the environment variables `OPENAI_API_KEY` for this to work.
+We start by trying the "gpt-4" model, but if this is not available we fall back automatically to
+"gpt-3.5-turbo".
+
+If you don't have access to GPT, or don't want to use it, it is also possible
+to use local LLMs. Running #formalize will fail, but print instructions for
+setting one up.
 -/
+
+open LLM GPT
+
+/-- The system message for `#formalize`. -/
+-- Please feel free to suggest changes, or to provide a customization hook.
+-- Not much thought has gone into this prompt!
+def systemMessage : String :=
+"You are an expert mathematician user of the interactive theorem prover Lean 4.
+You will be ask to formalize mathematical statements into Lean 4.
+
+Your answer should be in the form of a Lean 4 theorem statement
+equivalent to the query statement, with the proof given as `sorry`.
+You may include a short natural language explanation as a doc-string.
+
+Here are some examples:
+"
+
+/--
+A list of examples for few-shot prompting.
+-/
+def examples : List (String × String) :=
+[
+("There are infinitely many prime numbers.",
+"theorem infinitely_many_primes : ∀ N : Nat, ∃ p, N < p ∧ p.Prime := sorry"),
+("The length of the concatenation of two lists is the sum of the lengths of the lists.",
+"theorem List.append_length {L M : List α} : (L ++ M).length = L.length + M.length := sorry"),
+("The Lebesgue number lemma.",
+"/-- Let `c : ι → Set α` be an open cover of a compact set `s`. Then there exists an entourage
+`n` such that for each `x ∈ s` its `n`-neighborhood is contained in some `c i`. -/
+theorem lebesgue_number_lemma {α : Type u} [UniformSpace α] {s : Set α} {ι} {c : ι → Set α}
+    (hs : IsCompact s) (hc₁ : ∀ i, IsOpen (c i)) (hc₂ : s ⊆ ⋃ i, c i) :
+    ∃ n ∈ 𝓤 α, ∀ x ∈ s, ∃ i, { y | (x, y) ∈ n } ⊆ c i := sorry")]
+
+/--
+Construct the list of messages of the system message and the examples.
+-/
+def history : List Message :=
+⟨.system, systemMessage⟩ :: examples.bind fun (prompt, response) =>
+  [⟨.user, prompt⟩, ⟨.assistant, response⟩]
+
+open Lean Elab Command
+
+elab tk:"#formalize" t:term : command => liftTermElabM do
+  let .lit (.strVal s) ← Term.elabTerm t none
+    | throwError "#formalize must be followed by a string literal"
+  if s.trim.endsWith "." || s.trim.endsWith "?" then
+    logInfoAt tk (← (← findChatBot).sendMessages (history ++ [⟨.user, s⟩]))
+  else logInfoAt tk <|
+    "Please terminate your request with a '.' or '?', to avoid intermediate requests to the LLM."
--- test/formalize.lean
+++ test/formalize.lean
@@ -15,27 +15,74 @@ either GPT using an OpenAI API key in the environment variable `OPENAI_API_KEY` 
 or a local LLM. (Run `#formalize` without an OpenAI API key for instructions.)
 -/
 
--- #formalize "There are infinitely many primes numbers ending with a 7."
--- /-- There are infinitely many prime numbers ending with a 7. -/
--- theorem infinitely_many_primes_ending_with_7 : ∀ N : Nat, ∃ p, N < p ∧ p.Prime ∧ p % 10 = 7 :=
---   sorry
-
 -- #formalize "Concatenation of lists is associative."
+
+-- GPT4 suggests:
 -- /-- Concatenation of lists is associative. -/
 -- theorem List.append_assoc {α : Type u} : ∀ (L M N : List α), (L ++ M) ++ N = L ++ (M ++ N) :=
 --   sorry
 
+-- ggml-gpt4all-j-v1.3-groovy.bin suggests:
+-- {(L1 L2 L3 L4 : List α)} (L1 L2 L3 L4) L5 := L5 :: (L1 L2 L3 L4 L5)
+
+-- 7B/ggml-model-q4_0.bin suggests:
+-- ∀ {n m : List α} (o : List α → Set α), ∀ {k l : List α},
+--   k (m ++ l) = (k m) ++ (k l): sorry
+
+-- #formalize "There are infinitely many primes numbers ending with a 7."
+
+-- GPT4 suggests:
+-- /-- There are infinitely many prime numbers ending with a 7. -/
+-- theorem infinitely_many_primes_ending_with_7 : ∀ N : Nat, ∃ p, N < p ∧ p.Prime ∧ p % 10 = 7 :=
+--   sorry
+
+-- ggml-gpt4all-j-v1.3-groovy.bin suggests:
+-- There are infinitely many prime numbers that end with a 7.
+
+-- 7B/ggml-model-q4_0.bin suggests:
+-- ∀ p : nat, 7 < 7p + 1 := sorry
+-- \end{code}
+
 -- #formalize "The sum of the first n natural numbers."
+
+-- GPT4 suggests:
 -- /-- The sum of the first `n` natural numbers is equal to n * (n + 1) / 2. -/
 -- theorem sum_of_first_n_natural_numbers (n : ℕ) :
 --    (n * (n + 1)) / 2 = ∑ i in Finset.range (n + 1), i := sorry
 
+-- 7B/ggml-model-q4_0.bin suggests:
+-- 1 + ... + 2n = ∑ n (n+1)/2
+-- theorem nat_sum : ∀ n : Nat, 1 + ... + 2n = ∑ n (n+1)/2 := sorry
+
 -- #formalize "The only even prime number is 2."
+
+-- GPT4 suggests:
 -- -- /-- The only even prime number is 2. -/
 -- theorem only_even_prime_is_two : ∀ p : Nat, p.Prime ∧ p % 2 = 0 → p = 2 := sorry
 
--- #formalize "The abc conjecture"
+-- 7B/ggml-model-q4_0.bin suggests:
+-- 1 : Even ∧ prime := sorry
+-- \end{code}
+
+-- #formalize "The abc conjecture."
+
+-- GPT4 suggests:
 -- theorem abc_conjecture {a b c : ℤ} (h₁ : a * b * c ≠ 0) (h₂ : gcd a b = 1) (h₃ : gcd a c = 1)
 --     (h₄ : gcd b c = 1) (h₅ : a + b = c) :
 --     ∃ K : ℝ, ∀ ε > 0, abs (c : ℝ) ≤ K * (rad (a * b * c))^ε :=
 --   sorry
+
+-- 7B/ggml-model-q4_0.bin suggests:
+-- Ɐn  : ⇐ ∃ p ∈ ℋ, ∀ k ≥ 2, ⌈pⁿ⌉ < k (whenever p is odd). ⇐ ⇐
+-- theorem abc : ∀ k : Nat,
+-- ∀ n : ℕ, ∀ m : ℕ, ∃ p ∈ ℋ, ∀ q ∈ ℋ, ⌈pⁿ⌉ < k → ⌈n^m⌉ = ⌈q^m⌉ :=
+-- apply lebesgue_number_lemma with (hs := is_compact_s _ _ _ _ hc₁)
+--                                 (hc₂ := s ⊆ ⋃ i, c i); clear hc₁;
+--   refine (lebesgue_number_lemma _ _ _ _ hc₂);
+--
+-- ... and then pages more, much of which looks like pseudo-Isalbelle,
+--   finally ending with some Java ...
+--
+-- private MotechRole getRoleService() {
+--     return context.getBean(MotechRoleService.class);
+-- }
