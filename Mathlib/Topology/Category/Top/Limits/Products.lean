737618e9d9f637cc84633b0e7cad6db6c065a56b
Matthew Ballard <matt@mrb.email>
2023-05-04 21:03:12 -0400

    streamline coercion from X:TopCat to TopologicalSpace X

--- Mathlib/Topology/Category/Top/Limits/Products.lean
+++ Mathlib/Topology/Category/Top/Limits/Products.lean
@@ -274,19 +274,13 @@ theorem range_prod_map {W X Y Z : TopCat.{u}} (f : W ⟶ Y) (g : X ⟶ Z) :
       rw [this, hx₂]
 #align Top.range_prod_map TopCat.range_prod_map
 
--- Porting note: this is unpleasant because of topologicalSpace_forget had to be supplied
--- in Topology.Category.Top.Basic to get Lean to see through forget but now Lean cannot see
--- through topologicalSpace_forget
 theorem inducing_prod_map {W X Y Z : TopCat} {f : W ⟶ X} {g : Y ⟶ Z} (hf : Inducing f)
     (hg : Inducing g) : Inducing (Limits.prod.map f g) := by
   constructor
-  dsimp [topologicalSpace_forget, topologicalSpace_coe]
   simp only [prod_topology, induced_compose, ← coe_comp, Limits.prod.map_fst, Limits.prod.map_snd,
     induced_inf]
   simp only [coe_comp]
-  have : (Z : TopCat) → topologicalSpace_forget Z = Z.str := fun _ => rfl
-  rw [← this X, ← @induced_compose _ _ _ _ _ f, ←this Z, ← @induced_compose _ _ _ _ _ g, ← hf.induced, ← hg.induced]
-  simp only [this]
+  rw [← @induced_compose _ _ _ _ _ f, ← @induced_compose _ _ _ _ _ g, ← hf.induced, ← hg.induced]
 #align Top.inducing_prod_map TopCat.inducing_prod_map
 
 theorem embedding_prod_map {W X Y Z : TopCat} {f : W ⟶ X} {g : Y ⟶ Z} (hf : Embedding f)
