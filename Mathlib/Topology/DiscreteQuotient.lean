e0fd3f7dbf994599af312d5f630bf7fcedbb9bce
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2023-05-05 12:24:18 +0000

    chore: tidy various files (#3718)

--- Mathlib/Topology/DiscreteQuotient.lean
+++ Mathlib/Topology/DiscreteQuotient.lean
@@ -203,7 +203,7 @@ theorem comap_mono {A B : DiscreteQuotient Y} (h : A ≤ B) : A.comap f ≤ B.co
 
 end Comap
 
-section OfLe
+section OfLE
 
 variable {A B C : DiscreteQuotient X}
 
@@ -247,7 +247,7 @@ theorem ofLE_comp_proj (h : A ≤ B) : ofLE h ∘ A.proj = B.proj :=
   funext <| ofLE_proj _
 #align discrete_quotient.of_le_comp_proj DiscreteQuotient.ofLE_comp_proj
 
-end OfLe
+end OfLE
 
 /-- When `X` is a locally connected space, there is an `OrderBot` instance on
 `DiscreteQuotient X`. The bottom element is given by `connectedComponentSetoid X`
