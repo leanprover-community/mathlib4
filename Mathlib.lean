4a20b63f863073a5db279cf3e12f861df943157f
Xavier Roblot <46200072+xroblot@users.noreply.github.com>
2023-05-06 07:33:35 +0000

    feat port : RingTheory.Ideal.AssociatedPrime (#3810)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1693,6 +1693,7 @@ import Mathlib.RingTheory.Finiteness
 import Mathlib.RingTheory.Fintype
 import Mathlib.RingTheory.Flat
 import Mathlib.RingTheory.FreeRing
+import Mathlib.RingTheory.Ideal.AssociatedPrime
 import Mathlib.RingTheory.Ideal.Basic
 import Mathlib.RingTheory.Ideal.IdempotentFg
 import Mathlib.RingTheory.Ideal.Operations
