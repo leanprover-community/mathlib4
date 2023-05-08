28e49fca3bcedc90c9149a26c10c338ebc00d7a5
Jason Yuen <jason_yuen2007@hotmail.com>
2023-05-08 02:51:12 +0000

    feat: port Topology.Category.Top.OpenNhds (#3834)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1972,6 +1972,7 @@ import Mathlib.Topology.Category.Top.Adjunctions
 import Mathlib.Topology.Category.Top.Basic
 import Mathlib.Topology.Category.Top.EpiMono
 import Mathlib.Topology.Category.Top.Limits.Basic
+import Mathlib.Topology.Category.Top.OpenNhds
 import Mathlib.Topology.Category.Top.Opens
 import Mathlib.Topology.CompactOpen
 import Mathlib.Topology.Connected
