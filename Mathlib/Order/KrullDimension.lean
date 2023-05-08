a1819452289ee983b81de4777299c151d380a38b
Jujian Zhang <jujian.zhang1998@outlook.com>
2023-05-08 16:29:16 +0100

    add some constructions about strict series

--- Mathlib/Order/KrullDimension.lean
+++ Mathlib/Order/KrullDimension.lean
@@ -10,6 +10,8 @@ import Mathlib.Order.WithBot
 import Mathlib.Data.Fin.Basic
 import Mathlib.Data.Nat.Lattice
 import Mathlib.Logic.Equiv.Fin
+import Mathlib.Tactic.Linarith
+import Mathlib.Tactic.Conv
 
 /-!
 # Krull dimension of a preordered set
@@ -25,11 +27,11 @@ coheight is defined to be the krull dimension of `[a, +∞)`.
 ## Implementation notes
 Krull dimensions are defined to take value in `WithBot (WithTop ℕ)` so that `(-∞) + (+∞)` is
 also negative infinity. This is because we want Krull dimensions to be additive with respect
-to product of varieties so that `-∞` being the Krull dimension of empty variety is equal to 
+to product of varieties so that `-∞` being the Krull dimension of empty variety is equal to
 sum of `-∞` and the Krull dimension of any other varieties.
 -/
 
-variable (α : Type _) [Preorder α]
+variable (α β : Type _) [Preorder α] [Preorder β]
 
 /--
 For a preordered set `(α, <)`, a strict series of `α` of length `n` is a strictly monotonic function
@@ -82,6 +84,105 @@ le_antisymm (@le_top (StrictSeries α) _ _ _) (hp ⊤)
 
 lemma top_len_unique' (H1 H2 : OrderTop (StrictSeries α)) : H1.top.length = H2.top.length :=
 le_antisymm (H2.le_top H1.top) (H1.le_top H2.top)
+
+/--
+A strict series `a_0 < a_1 < ... < a_n` in `α` gives a strict series in `αᵒᵈ` by reversing the
+series `a_n < a_{n - 1} < ... < a_1 < a_0`.
+-/
+def reverse (p : StrictSeries α) : StrictSeries αᵒᵈ :=
+{ length := p.length
+  toFun := p ∘ (Sub.sub ⟨p.length, lt_add_one _⟩)
+  StrictMono := by
+    intros i j h
+    refine p.StrictMono (?_ : (_ % _ : ℕ) < _ % _)
+    dsimp only
+    rw [←Nat.add_sub_assoc, add_comm, Nat.add_sub_assoc, Nat.add_mod_left (p.length + 1),
+      ←Nat.add_sub_assoc, add_comm p.length (p.length + 1), Nat.add_sub_assoc,
+      Nat.add_mod_left (p.length + 1), Nat.mod_eq_of_lt, Nat.mod_eq_of_lt]
+    . exact Nat.sub_lt_sub_left (lt_of_lt_of_le h (Nat.lt_succ_iff.mp j.2)) h
+    . exact lt_of_le_of_lt (Nat.sub_le _ _) (lt_add_one _)
+    . exact lt_of_le_of_lt (Nat.sub_le _ _) (lt_add_one _)
+    . exact Nat.lt_succ_iff.mp i.2
+    . exact le_of_lt i.2
+    . exact Nat.lt_succ_iff.mp j.2
+    . exact le_of_lt j.2 }
+
+/--
+given a series `a_0 < a_1 < ... < a_n` and an `a` that is smaller than `a_0`, there is a series of
+length `n+1`: `a < a_0 < a_1 < ... < a_n`.
+-/
+@[simps]
+def cons (p : StrictSeries α) (a : α) (h : a < p 0) : StrictSeries α :=
+{ length := p.length + 1
+  toFun := Fin.cons a p
+  StrictMono := fun i ↦ Fin.cases (fun j ↦ Fin.cases (fun r ↦ (lt_irrefl _ r).elim)
+    (fun k _ ↦ by
+      rw [Fin.cons_zero, Fin.cons_succ]
+      exact lt_of_lt_of_le h (p.StrictMono.monotone $ by norm_num)) j)
+    (fun i' j ↦ Fin.cases (fun r ↦ (not_lt_of_lt r (Fin.succ_pos i')).elim)
+    (fun k hk ↦ by
+      rw [Fin.cons_succ, Fin.cons_succ]
+      exact p.StrictMono (Fin.succ_lt_succ_iff.mp hk)) j) i }
+
+lemma cons_zero (p : StrictSeries α) (a : α) (h : a < p 0) : p.cons a h 0 = a :=
+by simp only [cons_toFun, Fin.cons_zero]
+
+lemma cons_succ (p : StrictSeries α) (a : α) (h : a < p 0) (x) :
+  p.cons a h x.succ = p x :=
+by simp only [cons_toFun, Fin.cons_succ]
+
+/--
+given a series `a_0 < a_1 < ... < a_n` and an `a` that is greater than `a_n`, there is a series of
+length `n+1`: `a_0 < a_1 < ... < a_n < a`.
+-/
+@[simps]
+def snoc (p : StrictSeries α) (a : α) (h : p (Fin.last _) < a) : StrictSeries α :=
+{ length := p.length + 1
+  toFun := Fin.snoc p a
+  StrictMono := by
+    intros i j H
+    rw [Fin.snoc, Fin.snoc]
+    by_cases h1 : (j : ℕ) < p.length + 1
+    . rw [dif_pos h1, dif_pos]
+      . exact p.StrictMono H
+      . exact lt_trans H h1
+    . rw [dif_neg h1, cast_eq]
+      split_ifs with h2
+      . rw [cast_eq]
+        refine lt_of_le_of_lt (p.StrictMono.monotone (?_ : (i : ℕ) ≤ _)) h
+        linarith
+      . change i.1 < j.1 at H
+        push_neg at h2 h1
+        refine (ne_of_lt H ?_).elim
+        rw [show i.1 = p.length + 1 from _, show j.1 = p.length + 1 from _]
+        . linarith [j.2]
+        . linarith [i.2] }
+
+lemma snoc_last (p : StrictSeries α) (a : α) (h : p (Fin.last _) < a) :
+  p.snoc a h (Fin.last _) = a :=
+by simp
+
+/--
+If a series `a_0 < a_1 < ...` has positive length, then `a_1 < ...` is another series
+-/
+@[simps]
+def tail (p : StrictSeries α) (h : p.length ≠ 0) : StrictSeries α :=
+{ length := p.length.pred,
+  toFun := fun j ↦ p ⟨j + 1, Nat.succ_lt_succ (by
+    have hj := j.2
+    conv_rhs at hj =>
+      rw [← Nat.succ_eq_add_one, Nat.succ_pred_eq_of_pos (Nat.pos_of_ne_zero h)]
+    exact hj)⟩
+  StrictMono := fun _ _ H ↦ p.StrictMono (Nat.succ_lt_succ H) }
+
+lemma tail_zero (p : StrictSeries α) (h : p.length ≠ 0) : p.tail h 0 = p 1 :=
+by
+  rw [tail_toFun]
+  congr
+  change (0 : ℕ) % (p.length.pred + 1) + 1 = 1 % (p.length + 1)
+  rw [Nat.zero_mod, zero_add, Nat.mod_eq_of_lt]
+  rw [lt_add_iff_pos_left]
+  exact Nat.pos_of_ne_zero h
 
 end StrictSeries
 
