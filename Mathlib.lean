9462685b80684b04cec387d1413a1bbbf55ac04c
Matthew Ballard <matt@mrb.email>
2023-05-09 07:15:22 -0400

    update mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -29,8 +29,8 @@ import Mathlib.Algebra.BigOperators.Pi
 import Mathlib.Algebra.BigOperators.Ring
 import Mathlib.Algebra.BigOperators.RingEquiv
 import Mathlib.Algebra.Bounds
-import Mathlib.Algebra.Category.Group.EpiMono
 import Mathlib.Algebra.Category.GroupCat.Basic
+import Mathlib.Algebra.Category.GroupCat.EpiMono
 import Mathlib.Algebra.Category.GroupCat.Preadditive
 import Mathlib.Algebra.Category.GroupCat.ZModuleEquivalence
 import Mathlib.Algebra.Category.GroupCat.Zero
