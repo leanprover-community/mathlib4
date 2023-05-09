f27ce539cf26a6cd9c7f4fbc2bb2134a5d9238d7
Mario Carneiro <di.gama@gmail.com>
2023-05-08 21:24:08 -0400

    feat: add rel_congr implementation

--- Mathlib/Tactic/Rel/Basic.lean
+++ Mathlib/Tactic/Rel/Basic.lean
@@ -4,45 +4,220 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Heather Macbeth
 -/
 import Mathlib.Tactic.SolveByElim
+import Mathlib.Tactic.Positivity
 
-register_label_attr ineq_rules
+namespace Mathlib.Tactic.Rel
+open Lean Meta
 
-register_label_attr unsafe_ineq_rules
+structure RelCongrLemma where
+  declName : Name
+  mainSubgoals : Array Nat
+  varyingArgs : Array Bool
+  deriving Inhabited, Repr
 
-register_label_attr ineq_extra
+/-- Environment extensions for rel_congr lemmas -/
+initialize relCongrExt : SimpleScopedEnvExtension ((Name × Name) × RelCongrLemma)
+    (HashMap (Name × Name) (Array RelCongrLemma)) ←
+  registerSimpleScopedEnvExtension {
+    addEntry := fun m (n, lem) => m.insert n ((m.findD n #[]).push lem)
+    initial := {}
+  }
 
-register_label_attr mod_rules
+initialize registerBuiltinAttribute {
+  name := `rel_congr
+  descr := "relation congruence"
+  add := fun decl _ kind ↦ MetaM.run' do
+    let declTy := (← getConstInfo decl).type
+    withReducible <| forallTelescopeReducing declTy fun xs targetTy => do
+    let fail := throwError
+      "@[rel_congr] attribute only applies to lemmas proving {
+      ""}x₁ ~₁ x₁' → ... xₙ ~ₙ xₙ' → f x₁ ... xₙ ∼ f x₁' ... xₙ', got {declTy}"
+    let .app (.app rel lhs) rhs ← whnf targetTy | fail
+    let some relName := rel.getAppFn.constName? | fail
+    let (some head, lhsArgs) := lhs.withApp fun e a => (e.constName?, a) | fail
+    let (some head', rhsArgs) := rhs.withApp fun e a => (e.constName?, a) | fail
+    unless head == head' && lhsArgs.size == rhsArgs.size do fail
+    let mut varyingArgs := #[]
+    let mut pairs := #[]
+    for e1 in lhsArgs, e2 in rhsArgs do
+      let isEq ← isDefEq e1 e2
+      varyingArgs := varyingArgs.push !isEq
+      if !isEq then
+        unless e1.isFVar && e2.isFVar do fail
+        pairs := pairs.push (e1, e2)
+    let mut mainSubgoals := #[]
+    let mut i := 0
+    for hyp in xs do
+      mainSubgoals ← forallTelescopeReducing (← inferType hyp) fun _args hypTy => do
+        let mut mainSubgoals := mainSubgoals
+        if let .app (.app _ lhs₁) rhs₁ ← whnf hypTy then
+          let lhs₁ := lhs₁.getAppFn
+          let rhs₁ := rhs₁.getAppFn
+          if ← pairs.anyM fun (e1, e2) =>
+            isDefEq lhs₁ e1 <&&> isDefEq rhs₁ e2 <||>
+            isDefEq lhs₁ e2 <&&> isDefEq rhs₁ e1
+          then
+            mainSubgoals := mainSubgoals.push i
+        pure mainSubgoals
+      i := i + 1
+    relCongrExt.add ((relName, head), { declName := decl, mainSubgoals, varyingArgs }) kind
+}
 
-register_label_attr mod_extra
+syntax "rel_congr_discharger" : tactic
 
-register_label_attr iff_rules
+macro_rules | `(tactic| rel_congr_discharger) => `(tactic| positivity)
+macro_rules | `(tactic| rel_congr_discharger) => `(tactic| positivity)
 
-syntax (name := RelCongrSyntax) "rel_congr" : tactic
-syntax (name := RelSyntax) "rel" " [" term,* "] " : tactic
-syntax (name := ExtraSyntax) "extra" : tactic
+syntax "rel" " [" term,* "] " : tactic
+-- syntax (name := ExtraSyntax) "extra" : tactic
 
-open Lean Mathlib Tactic
+-- open Lean Mathlib Tactic
 
-def RelConfig : SolveByElim.Config :=
-{ transparency := .instances
-  -- On applying a lemma or hypothesis successfully, don't backtrack
-  failAtMaxDepth := false
-  maxDepth := 50 }
+-- def RelConfig : SolveByElim.Config :=
+-- { transparency := .instances
+--   -- On applying a lemma or hypothesis successfully, don't backtrack
+--   failAtMaxDepth := false
+--   maxDepth := 50 }
 
-def Lean.MVarId.RelCongr (attr : Name) --(add : List Term)
-    (disch : MVarId → MetaM (Option (List MVarId)) := fun _ => pure none)
-    (proc : List MVarId → List MVarId → MetaM (Option (List MVarId)) := fun _ _ => pure none)
-    (g : MVarId) :
-    MetaM (List MVarId) := do
-  let cfg : SolveByElim.Config := { RelConfig with discharge := disch, proc := proc }
-  SolveByElim.solveByElim.processSyntax cfg false false [] [] #[mkIdent attr] [g]
+-- -- def Lean.MVarId.RelCongr (attr : Name) --(add : List Term)
+-- --     (disch : MVarId → MetaM (Option (List MVarId)) := fun _ => pure none)
+-- --     (proc : List MVarId → List MVarId → MetaM (Option (List MVarId)) := fun _ _ => pure none)
+-- --     (g : MVarId) :
+-- --     MetaM (List MVarId) := do
+-- --   let cfg : SolveByElim.Config := { RelConfig with discharge := disch, proc := proc }
+-- --   SolveByElim.solveByElim.processSyntax cfg false false [] [] #[mkIdent attr] [g]
 
-def Lean.MVarId.Rel (attr : Array Name) (add : List Term) (m : MessageData)
-    (disch : MVarId → MetaM (Option (List MVarId)) := fun _ => pure none)
-    (proc : List MVarId → List MVarId → MetaM (Option (List MVarId)) := fun _ _ => pure none)
-    (g : MVarId) :
-    MetaM (List MVarId) := do
-  let cfg : SolveByElim.Config := { RelConfig with discharge := disch, proc := proc }
-  let [] ← SolveByElim.solveByElim.processSyntax cfg true false add [] (attr.map mkIdent) [g]
-    | throwError m
-  return []
+initialize registerTraceClass `Meta.rel
+
+partial def _root_.Lean.MVarId.relCongr
+    (g : MVarId) (template : Option Expr)
+    (discharger : MVarId → MetaM Unit)
+    (assumption : MVarId → MetaM Unit := fun g => g.assumption) :
+    MetaM (Array MVarId) := do
+  withTraceNode `Meta.rel (fun _ => return m!"rel_congr: ⊢ {← g.getType}") do
+  match template with
+  | none =>
+    try assumption g; return #[]
+    catch _ => pure ()
+  | some (.mvar mvarId) =>
+    if let .syntheticOpaque ← mvarId.getKind then
+      try assumption g; return #[]
+      catch _ => return #[g]
+  | some _ => pure ()
+  let .app (.app rel lhs) rhs ← withReducible g.getType'
+    | throwError "rel_congr failed, not a relation"
+  let some relName := rel.getAppFn.constName?
+    | throwError "rel_congr failed, relation head {rel} is not a constant"
+  let (some lhsHead, lhsArgs) := lhs.withApp fun e a => (e.constName?, a)
+    | if template.isNone then return #[g]
+      throwError "rel_congr failed, {lhs} is not a constant"
+  let (some rhsHead, rhsArgs) := rhs.withApp fun e a => (e.constName?, a)
+    | if template.isNone then return #[g]
+      throwError "rel_congr failed, {rhs} is not a constant"
+  let tplArgs ← if let some tpl := template then
+    let (some tplHead, tplArgs) := tpl.withApp fun e a => (e.constName?, a)
+      | throwError "rel_congr failed, {tpl} is not a constant"
+    unless tplHead == lhsHead && tplArgs.size == rhsArgs.size do
+      throwError "expected {tplHead}, got {lhsHead}\n{lhs}"
+    unless tplHead == rhsHead && tplArgs.size == rhsArgs.size do
+      throwError "expected {tplHead}, got {rhsHead}\n{rhs}"
+    tplArgs.mapM fun tpl => do
+      let mctx ← getMCtx
+      let hasMVar := tpl.findMVar? fun mvarId =>
+        if let some mdecl := mctx.findDecl? mvarId then
+          mdecl.kind matches .syntheticOpaque
+        else
+          false
+      pure (some tpl, hasMVar.isSome)
+  else
+    unless lhsHead == rhsHead && lhsArgs.size == rhsArgs.size do
+      return #[g]
+    (lhsArgs.zip rhsArgs).mapM fun (lhsArg, rhsArg) =>
+      return (none, !(← isDefEq lhsArg rhsArg))
+  let varyingArgs := tplArgs.map (·.2)
+  if varyingArgs.all not then
+    throwError "try refl"
+  let s ← saveState
+  let mut ex? := none
+  for lem in (relCongrExt.getState (← getEnv)).findD (relName, lhsHead) #[] do
+    if lem.varyingArgs == varyingArgs then
+      let gs ← try
+        Except.ok <$> g.apply (← mkConstWithFreshMVarLevels lem.declName)
+      catch e => pure (Except.error e)
+      match gs with
+      | .error e =>
+        ex? := ex? <|> (some (← saveState, e)) -- stash the first failure of `apply`
+        s.restore
+      | .ok gs =>
+        let some e ← getExprMVarAssignment? g | panic! "unassigned?"
+        let args := e.getAppArgs
+        let mut subgoals := #[]
+        for i in lem.mainSubgoals do
+          let .mvar mvarId := args[i]! | panic! "what kind of lemma is this?"
+          let (_vs, mvarId) ← mvarId.intros
+          let tpl ← tplArgs[i]!.1.mapM fun e => do
+            let (_vs, _, e) ← lambdaMetaTelescope e
+            pure e
+          subgoals := subgoals ++ (← mvarId.relCongr tpl discharger assumption)
+        let mut out := #[]
+        for g in gs do
+          if !(← g.isAssigned) then
+            try discharger g
+            catch _ => out := out.push g
+        return out ++ subgoals
+  if template.isNone then
+    return #[g]
+  let some (sErr, e) := ex? | throwError "rel_congr failed, no lemma with @[rel_congr] applies"
+  sErr.restore
+  throw e
+
+open Elab Tactic
+
+def myExact (g : MVarId) (e : Expr) : MetaM Unit := do
+  let .true ← isDefEq (← g.getType) (← inferType e) | failure
+  g.checkNotAssigned `myExact
+  g.assign e
+
+def relAssumption (hs : Array Expr) (g : MVarId) : MetaM Unit :=
+  withReducible do
+    let s ← saveState
+    withTraceNode `Meta.rel (fun _ => return m!"rel_assumption: ⊢ {← g.getType}") do
+    for h in hs do
+      try
+        try myExact g h catch _ =>
+          try myExact g (← mkAppM ``le_of_lt #[h]) catch _ =>
+            let m ← mkFreshExprMVar none
+            myExact g (← mkAppOptM ``Eq.subst #[h, m])
+            g.rfl
+        return
+      catch _ => s.restore
+    throwError "rel_assumption failed"
+
+elab "rel_congr" template:(colGt term)? : tactic => do
+  let g ← getMainGoal
+  g.withContext do
+  let .app (.app _rel lhs) _rhs ← withReducible g.getType'
+    | throwError "rel_congr failed, not a relation"
+  let template ← template.mapM fun e => do
+    Term.elabTerm e (← inferType lhs)
+  let disch g := Term.TermElabM.run' do
+    let [] ← Tactic.run g <| evalTactic (Unhygienic.run `(tactic| rel_congr_discharger))
+      | failure
+  let assum g := do
+    let mut hs := #[]
+    for h in ← getLCtx do
+      if !h.isImplementationDetail then
+        hs := hs.push (.fvar h.fvarId)
+    relAssumption hs g
+  replaceMainGoal (← g.relCongr template disch assum).toList
+
+
+-- def Lean.MVarId.rel (attr : Array Name) (add : List Term) (m : MessageData)
+--     (disch : MVarId → MetaM (Option (List MVarId)) := fun _ => pure none)
+--     (proc : List MVarId → List MVarId → MetaM (Option (List MVarId)) := fun _ _ => pure none)
+--     (g : MVarId) :
+--     MetaM (List MVarId) := do
+--   let cfg : SolveByElim.Config := { RelConfig with discharge := disch, proc := proc }
+--   let [] ← SolveByElim.solveByElim.processSyntax cfg true false add [] (attr.map mkIdent) [g]
+--     | throwError m
+--   return []
--- Mathlib/Tactic/RelCongr/Lemmas/Lemmas.lean
+++ Mathlib/Tactic/RelCongr/Lemmas/Lemmas.lean
@@ -1,52 +1,53 @@
 import Mathlib.Data.Int.ModEq
 import Mathlib.Data.Real.ENNReal
 import Mathlib.Tactic.LibrarySearch
+import Mathlib.Tactic.Rel.Basic
 
 /-! # ≤, + -/
 
-attribute [dummy_label_attr]
+attribute [rel_congr]
   add_le_add -- tt + tt
   add_le_add_left -- ff + tt
   add_le_add_right -- tt + ff
 
 /-! # <, + -/
 
-attribute [dummy_label_attr]
+attribute [rel_congr]
   add_lt_add ENNReal.add_lt_add -- tt + tt
   add_lt_add_left ENNReal.add_lt_add_left -- ff + tt
   add_lt_add_right ENNReal.add_lt_add_right -- tt + ff
 
 /-! # ≡ ZMOD, + -/
 
-attribute [dummy_label_attr]
+attribute [rel_congr]
   Int.ModEq.add -- tt + tt
   Int.ModEq.add_left -- ff + tt
   Int.ModEq.add_right -- tt + ff
 
 /-! # ≤, - -/
 
-attribute [dummy_label_attr]
+attribute [rel_congr]
   sub_le_sub -- tt - tt
   sub_le_sub_left -- ff - tt
   sub_le_sub_right -- tt - ff
 
 /-! # <, - -/
 
-attribute [dummy_label_attr]
+attribute [rel_congr]
   sub_lt_sub -- tt - tt
   sub_lt_sub_left -- ff - tt
   sub_lt_sub_right -- tt - ff
 
 /-! # ≡ ZMOD, - -/
 
-attribute [dummy_label_attr]
+attribute [rel_congr]
   Int.ModEq.sub -- tt - tt
   Int.ModEq.sub_left -- ff - tt
   Int.ModEq.sub_right -- tt - ff
 
 /-! # ≤, * -/
 
-attribute [dummy_label_attr]
+attribute [rel_congr]
   mul_le_mul' mul_le_mul -- tt * tt
   mul_le_mul_left' mul_le_mul_of_nonneg_left -- ff * tt
   mul_le_mul_right' mul_le_mul_of_nonneg_right -- tt * ff
@@ -61,14 +62,14 @@ theorem ENNReal.mul_lt_mul_right' {a b c : ENNReal} (h0 : a ≠ 0) (hinf : a ≠
     b * a < c * a :=
   mul_comm b a ▸ mul_comm c a ▸ ENNReal.mul_left_strictMono h0 hinf bc
 
-attribute [dummy_label_attr]
+attribute [rel_congr]
   mul_lt_mul_of_lt_of_lt mul_lt_mul'' ENNReal.mul_lt_mul -- tt * tt
   mul_lt_mul_left' mul_lt_mul_of_pos_left ENNReal.mul_lt_mul_left' -- ff * tt
   mul_lt_mul_right' mul_lt_mul_of_pos_right ENNReal.mul_lt_mul_right' -- tt * ff
 
 /-! # ≡ ZMOD, * -/
 
-attribute [dummy_label_attr]
+attribute [rel_congr]
   Int.ModEq.mul -- tt * tt
   Int.ModEq.mul_left -- ff * tt
   Int.ModEq.mul_right -- tt * ff
@@ -79,14 +80,14 @@ theorem Nat.div_le_div (a b c d : ℕ) (h1 : a ≤ b) (h2 : d ≤ c) (h3 : d ≠
   calc a / c ≤ b / c := Nat.div_le_div_right h1
     _ ≤ b / d := Nat.div_le_div_left h2 (Nat.pos_of_ne_zero h3)
 
-attribute [dummy_label_attr]
+attribute [rel_congr]
   div_le_div'' div_le_div Nat.div_le_div ENNReal.div_le_div -- tt / tt
   div_le_div_left' div_le_div_of_le_left Nat.div_le_div_left ENNReal.div_le_div_left -- ff / tt
   div_le_div_right' div_le_div_of_le Nat.div_le_div_right ENNReal.div_le_div_right -- tt / ff
 
 /-! # <, / -/
 
-attribute [dummy_label_attr]
+attribute [rel_congr]
   div_lt_div'' div_lt_div -- tt / tt
   div_lt_div_left' div_lt_div_of_lt_left -- ff / tt
   div_lt_div_right' div_lt_div_of_lt -- tt / ff
@@ -98,13 +99,13 @@ theorem ENNReal.inv_le_inv' {a b : ENNReal} (h : a ≤ b) : b⁻¹ ≤ a⁻¹ :=
 
 theorem ENNReal.inv_lt_inv' {a b : ENNReal} (h : a < b) : b⁻¹ < a⁻¹ := ENNReal.inv_strictAnti h
 
-attribute [dummy_label_attr]
+attribute [rel_congr]
   inv_le_inv_of_le inv_le_inv' ENNReal.inv_le_inv' -- tt
   inv_lt_inv_of_lt inv_lt_inv' ENNReal.inv_lt_inv' -- tt
 
 /-! # ≤, ^ -/
 
-attribute [dummy_label_attr]
+attribute [rel_congr]
   pow_le_pow pow_le_pow' zpow_le_zpow zpow_le_of_le ENNReal.zpow_le_of_le -- ff ^ tt
   pow_le_pow_of_le_left pow_le_pow_of_le_left' zpow_le_zpow' -- tt ^ ff
 
@@ -118,19 +119,19 @@ theorem ENNReal.pow_lt_pow_of_lt_left {x y : ENNReal} (h : x < y) {n : ℕ} (hn 
     x ^ n < y ^ n :=
   ENNReal.pow_strictMono hn h
 
-attribute [dummy_label_attr]
+attribute [rel_congr]
   pow_lt_pow pow_lt_pow' zpow_lt_zpow zpow_lt_of_lt -- ff ^ tt
   pow_lt_pow_of_lt_left ENNReal.pow_lt_pow_of_lt_left zpow_lt_zpow' -- tt ^ ff
 
 /-! # ≡ ZMOD, ^ -/
 
-attribute [dummy_label_attr]
+attribute [rel_congr]
   Int.ModEq.pow -- tt * ff
 
 /-! # ≤, ∑ -/
 
-attribute [dummy_label_attr]
-  Finset.sum_le_sum
+-- attribute [rel_congr]
+--   Finset.sum_le_sum
 
 -- a variant statement of `Finset.sum_le_sum`, if it doesn't match the attribute pattern in the
 -- library statement:
@@ -138,20 +139,23 @@ attribute [dummy_label_attr]
 def rel_on (s : Set α) (r : β → β → Prop) (f g : α → β) : Prop := ∀ x ∈ s, r (f x) (g x)
 
 theorem RelCongr.Finset.sum_le_sum [OrderedAddCommMonoid N] {f g : ι → N} {s : Finset ι}
-    (h : rel_on s (· ≤ ·) f g) : s.sum f ≤ s.sum g :=
+    (h : ∀ (i : ι), i ∈ s → f i ≤ g i) : s.sum f ≤ s.sum g :=
   _root_.Finset.sum_le_sum h
+
+attribute [rel_congr]
+  RelCongr.Finset.sum_le_sum
 
 /-! # <, ∑ -/
 
-attribute [dummy_label_attr]
-  Finset.sum_lt_sum_of_nonempty
+-- attribute [rel_congr]
+--   Finset.sum_lt_sum_of_nonempty
 
-/-! # ≤, ∏ -/
+-- /-! # ≤, ∏ -/
 
-attribute [dummy_label_attr]
-  Finset.prod_le_prod' Finset.prod_le_prod
+-- attribute [rel_congr]
+--   Finset.prod_le_prod' Finset.prod_le_prod
 
-/-! # <, ∏ -/
+-- /-! # <, ∏ -/
 
-attribute [dummy_label_attr]
-  Finset.prod_lt_prod_of_nonempty'
+-- attribute [rel_congr]
+--   Finset.prod_lt_prod_of_nonempty'
