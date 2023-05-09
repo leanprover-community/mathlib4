bff969888c63d5f768ac08b5771995eed99e29e2
thorimur <68410468+thorimur@users.noreply.github.com>
2023-05-09 00:34:40 -0400

    fix: clean up `applyMonosAlternatives`

--- Mathlib/Tactic/Monotonicity/Basic.lean
+++ Mathlib/Tactic/Monotonicity/Basic.lean
@@ -82,20 +82,19 @@ private def applyMonos (t : Expr) (side : Side) (failIfAmbiguousMatch : Bool) :
 
 private def applyMonosAlternatives (side : Side) (goal : MVarId) :
     MetaM (List (MetaM (List MVarId))) := withReducible do
-  trace[Tactic.mono] "running applyMonosAlts on {goal}"
+  trace[Tactic.mono] "running applyMonosAlternatives on {goal}"
   let goal? ← dsimpGoal goal Monos.SimpContext false
-  trace[Tactic.mono] "dsimped to get {goal?.1}"
   let goal ← match goal? with
   | (some goal, _) => pure goal
   | (none, _) => return []
   let (_, goal) ← goal.introsP!
   let t ← whnfR <|← instantiateMVars <|← goal.getType
-  trace[Tactic.mono] "looking at {t}"
   let monos ← getMatchingMonos t side
   trace[Tactic.mono] "matched:\n{monos.map (·.name) |>.toList}"
   if monos.isEmpty then throwError "no monos apply"
-  return monos.toList.map (fun m ↦ do let (e, gs) ← applyMono t m; goal.assign e; pure gs)
-/-- !! Apply the `mono` tactic to a goal. -/
+  return monos.toList.map fun m => goal.withContext do
+    let (e, gs) ← applyMono t m; goal.assign e; pure gs
+
 def _root_.Lean.MVarId.mono (goal : MVarId) (side : Side := .both)
     (simpUsing : Option Simp.Context := none) (recurse : Bool := false) :
     MetaM (List MVarId) := withReducible do
