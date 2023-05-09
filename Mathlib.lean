54d90c5520718ed3bb5fda696a489fd83794a651
Matthew Ballard <matt@mrb.email>
2023-05-08 21:59:04 -0400

    fix, lint, rename

--- Mathlib.lean
+++ Mathlib.lean
@@ -29,8 +29,8 @@ import Mathlib.Algebra.BigOperators.Pi
 import Mathlib.Algebra.BigOperators.Ring
 import Mathlib.Algebra.BigOperators.RingEquiv
 import Mathlib.Algebra.Bounds
-import Mathlib.Algebra.Category.Group.EquivalenceGroupAddGroup
 import Mathlib.Algebra.Category.GroupCat.Basic
+import Mathlib.Algebra.Category.GroupCat.EquivalenceGroupAddGroup
 import Mathlib.Algebra.Category.GroupCat.Preadditive
 import Mathlib.Algebra.Category.GroupCat.ZModuleEquivalence
 import Mathlib.Algebra.Category.GroupCat.Zero
--- Mathlib/Algebra/Category/GroupCat/EquivalenceGroupAddGroup.lean
+++ Mathlib/Algebra/Category/GroupCat/EquivalenceGroupAddGroup.lean
@@ -8,31 +8,39 @@ Authors: Jujian Zhang
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
-import Mathlib.Algebra.Category.Group.Basic
+import Mathlib.Algebra.Category.GroupCat.Basic
 import Mathlib.Algebra.Hom.Equiv.TypeTags
 
 /-!
 # Equivalence between `Group` and `AddGroup`
 
 This file contains two equivalences:
-* `Group_AddGroup_equivalence` : the equivalence between `Group` and `AddGroup` by sending
-  `X : Group` to `additive X` and `Y : AddGroup` to `multiplicative Y`.
-* `CommGroup_AddCommGroup_equivalence` : the equivalence between `CommGroup` and `AddCommGroup` by
-  sending `X : CommGroup` to `additive X` and `Y : AddCommGroup` to `multiplicative Y`.
+* `groupAddGroupEquivalence` : the equivalence between `GroupCat` and `AddGroupCat` by sending
+  `X : GroupCat` to `Additive X` and `Y : AddGroupCat` to `Multiplicative Y`.
+* `commGroupAddCommGroupEquivalence` : the equivalence between `CommGroupCat` and `AddCommGroupCat` by
+  sending `X : CommGroupCat` to `Additive X` and `Y : AddCommGroupCat` to `Multiplicative Y`.
 -/
 
+-- Porting note: everything is upper case
+set_option linter.uppercaseLean3 false
 
 open CategoryTheory
 
 namespace GroupCat
 
+-- Porting note: Lean cannot find these now
+private instance (X : GroupCat) : MulOneClass X.α := X.str.toMulOneClass
+private instance (X : CommGroupCat) : MulOneClass X.α := X.str.toMulOneClass
+private instance (X : AddGroupCat) : AddZeroClass X.α := X.str.toAddZeroClass
+private instance (X : AddCommGroupCat) : AddZeroClass X.α := X.str.toAddZeroClass
+
 /-- The functor `Group ⥤ AddGroup` by sending `X ↦ additive X` and `f ↦ f`.
 -/
 @[simps]
-def toAddGroup : GroupCat ⥤ AddGroupCat where
+def toAddGroupCat : GroupCat ⥤ AddGroupCat where
   obj X := AddGroupCat.of (Additive X)
-  map X Y := MonoidHom.toAdditive
-#align Group.to_AddGroup GroupCat.toAddGroup
+  map {X} {Y} := MonoidHom.toAdditive
+#align Group.to_AddGroup GroupCat.toAddGroupCat
 
 end GroupCat
 
@@ -41,10 +49,10 @@ namespace CommGroupCat
 /-- The functor `CommGroup ⥤ AddCommGroup` by sending `X ↦ additive X` and `f ↦ f`.
 -/
 @[simps]
-def toAddCommGroup : CommGroupCat ⥤ AddCommGroupCat where
+def toAddCommGroupCat : CommGroupCat ⥤ AddCommGroupCat where
   obj X := AddCommGroupCat.of (Additive X)
-  map X Y := MonoidHom.toAdditive
-#align CommGroup.to_AddCommGroup CommGroupCat.toAddCommGroup
+  map {X} {Y} := MonoidHom.toAdditive
+#align CommGroup.to_AddCommGroup CommGroupCat.toAddCommGroupCat
 
 end CommGroupCat
 
@@ -53,10 +61,10 @@ namespace AddGroupCat
 /-- The functor `AddGroup ⥤ Group` by sending `X ↦ multiplicative Y` and `f ↦ f`.
 -/
 @[simps]
-def toGroup : AddGroupCat ⥤ GroupCat where
+def toGroupCat : AddGroupCat ⥤ GroupCat where
   obj X := GroupCat.of (Multiplicative X)
-  map X Y := AddMonoidHom.toMultiplicative
-#align AddGroup.to_Group AddGroupCat.toGroup
+  map {X} {Y} := AddMonoidHom.toMultiplicative
+#align AddGroup.to_Group AddGroupCat.toGroupCat
 
 end AddGroupCat
 
@@ -65,32 +73,32 @@ namespace AddCommGroupCat
 /-- The functor `AddCommGroup ⥤ CommGroup` by sending `X ↦ multiplicative Y` and `f ↦ f`.
 -/
 @[simps]
-def toCommGroup : AddCommGroupCat ⥤ CommGroupCat where
+def toCommGroupCat : AddCommGroupCat ⥤ CommGroupCat where
   obj X := CommGroupCat.of (Multiplicative X)
-  map X Y := AddMonoidHom.toMultiplicative
-#align AddCommGroup.to_CommGroup AddCommGroupCat.toCommGroup
+  map {X} {Y} := AddMonoidHom.toMultiplicative
+#align AddCommGroup.to_CommGroup AddCommGroupCat.toCommGroupCat
 
 end AddCommGroupCat
 
 /-- The equivalence of categories between `Group` and `AddGroup`
 -/
-@[simps]
+@[simps!]
 def groupAddGroupEquivalence : GroupCat ≌ AddGroupCat :=
-  Equivalence.mk GroupCat.toAddGroup AddGroupCat.toGroup
+  CategoryTheory.Equivalence.mk GroupCat.toAddGroupCat AddGroupCat.toGroupCat
     (NatIso.ofComponents (fun X => MulEquiv.toGroupCatIso (MulEquiv.multiplicativeAdditive X))
-      fun X Y f => rfl)
+      fun _ => rfl)
     (NatIso.ofComponents (fun X => AddEquiv.toAddGroupCatIso (AddEquiv.additiveMultiplicative X))
-      fun X Y f => rfl)
+      fun _ => rfl)
 #align Group_AddGroup_equivalence groupAddGroupEquivalence
 
 /-- The equivalence of categories between `CommGroup` and `AddCommGroup`.
 -/
-@[simps]
+@[simps!]
 def commGroupAddCommGroupEquivalence : CommGroupCat ≌ AddCommGroupCat :=
-  Equivalence.mk CommGroupCat.toAddCommGroup AddCommGroupCat.toCommGroup
+  CategoryTheory.Equivalence.mk CommGroupCat.toAddCommGroupCat AddCommGroupCat.toCommGroupCat
     (NatIso.ofComponents (fun X => MulEquiv.toCommGroupCatIso (MulEquiv.multiplicativeAdditive X))
-      fun X Y f => rfl)
+      fun _ => rfl)
     (NatIso.ofComponents
-      (fun X => AddEquiv.toAddCommGroupCatIso (AddEquiv.additiveMultiplicative X)) fun X Y f => rfl)
+      (fun X => AddEquiv.toAddCommGroupCatIso (AddEquiv.additiveMultiplicative X)) fun _ => rfl)
 #align CommGroup_AddCommGroup_equivalence commGroupAddCommGroupEquivalence
 
