e37d90ba2a0353995375162baf694b21373b4037
Matthew Robert Ballard <100034030+mattrobball@users.noreply.github.com>
2023-05-09 05:38:05 -0400

    Update Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -1984,7 +1984,7 @@ import Mathlib.Topology.Category.Top.Adjunctions
 import Mathlib.Topology.Category.Top.Basic
 import Mathlib.Topology.Category.Top.EpiMono
 import Mathlib.Topology.Category.Top.Limits.Basic
-import Mathlib.Topology.Category.Top.Limits.Cofilteredimport
+import Mathlib.Topology.Category.Top.Limits.Cofiltered
 import Mathlib.Topology.Category.Top.Limits.Konig
 import Mathlib.Topology.Category.Top.OpenNhds
 import Mathlib.Topology.Category.Top.Opens
