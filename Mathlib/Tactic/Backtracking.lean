f41d9dcd6cd162491325f5993df01f0cc2a081e5
thorimur <68410468+thorimur@users.noreply.github.com>
2023-05-09 01:13:56 -0400

    docs: auth

--- Mathlib/Tactic/Backtracking.lean
+++ Mathlib/Tactic/Backtracking.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2023 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Scott Morrison
+Authors: Scott Morrison, Thomas Murrills
 -/
 import Lean.Meta.Basic
 import Std.Data.List.Basic
