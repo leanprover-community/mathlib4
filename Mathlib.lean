57b4fe5b503ce9cee9b7b5fec4a3ee00932602f2
Matthew Robert Ballard <matt@mrb.email>
2023-05-09 06:12:52 +0000

    feat: port Topology.Category.Top.Limits.Cofiltered  (#3855)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1982,6 +1982,7 @@ import Mathlib.Topology.Category.Top.Adjunctions
 import Mathlib.Topology.Category.Top.Basic
 import Mathlib.Topology.Category.Top.EpiMono
 import Mathlib.Topology.Category.Top.Limits.Basic
+import Mathlib.Topology.Category.Top.Limits.Cofiltered
 import Mathlib.Topology.Category.Top.OpenNhds
 import Mathlib.Topology.Category.Top.Opens
 import Mathlib.Topology.CompactOpen
