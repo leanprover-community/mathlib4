806ba450a6cc6db23ebd01eca2cedacf424d1ae5
Moritz Firsching <firsching@google.com>
2023-05-10 21:57:39 +0200

    line length lint

--- Mathlib/CategoryTheory/Preadditive/Yoneda/Projective.lean
+++ Mathlib/CategoryTheory/Preadditive/Yoneda/Projective.lean
@@ -35,7 +35,8 @@ namespace Projective
 theorem projective_iff_preservesEpimorphisms_preadditiveCoyoneda_obj (P : C) [ConcreteCategory C]:
     Projective P ↔ (preadditiveCoyoneda.obj (op P)).PreservesEpimorphisms := by
   rw [projective_iff_preservesEpimorphisms_coyoneda_obj]
-  refine' ⟨fun h : (preadditiveCoyoneda.obj (op P) ⋙ forget AddCommGroupCat).PreservesEpimorphisms => _, _⟩
+  refine' ⟨fun h : (preadditiveCoyoneda.obj (op P) ⋙
+      forget AddCommGroupCat).PreservesEpimorphisms => _, _⟩
   · exact Functor.preservesEpimorphisms_of_preserves_of_reflects (preadditiveCoyoneda.obj (op P))
         (forget _)
   · intro
@@ -45,7 +46,8 @@ theorem projective_iff_preservesEpimorphisms_preadditiveCoyoneda_obj (P : C) [Co
 theorem projective_iff_preservesEpimorphisms_preadditive_coyoneda_obj' (P : C) :
     Projective P ↔ (preadditiveCoyoneda.obj (op P)).PreservesEpimorphisms := by
   rw [projective_iff_preservesEpimorphisms_coyoneda_obj]
-  refine' ⟨fun h : (preadditiveCoyoneda.obj (op P) ⋙ forget AddCommGroupCat).PreservesEpimorphisms => _, _⟩
+  refine' ⟨fun h : (preadditiveCoyoneda.obj (op P) ⋙
+      forget AddCommGroupCat).PreservesEpimorphisms => _, _⟩
   · exact Functor.preservesEpimorphisms_of_preserves_of_reflects (preadditiveCoyoneda.obj (op P))
         (forget _)
   · intro
