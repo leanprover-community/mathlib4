68ec21983b07109bea75fc992f4856437db50ff6
Pol_tta <MonadMaverick@pm.me>
2023-05-10 01:01:22 +0000

    feat: port MeasureTheory.Measure.OpenPos (#3820)
    Co-authored-by: Komyyy <pol_tta@outlook.jp>
    Co-authored-by: Jeremy Tan Jie Rui <reddeloostw@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1551,6 +1551,7 @@ import Mathlib.MeasureTheory.Measure.MeasureSpace
 import Mathlib.MeasureTheory.Measure.MeasureSpaceDef
 import Mathlib.MeasureTheory.Measure.MutuallySingular
 import Mathlib.MeasureTheory.Measure.NullMeasurable
+import Mathlib.MeasureTheory.Measure.OpenPos
 import Mathlib.MeasureTheory.Measure.OuterMeasure
 import Mathlib.MeasureTheory.Measure.Sub
 import Mathlib.MeasureTheory.PiSystem
