773f87fc38bc6fe2d3bee89bd6d6a94291acbbaa
Matthew Ballard <matt@mrb.email>
2023-05-10 12:02:13 -0400

    fix, lint, rename

--- Mathlib/CategoryTheory/Preadditive/Yoneda/Injective.lean
+++ Mathlib/CategoryTheory/Preadditive/Yoneda/Injective.lean
@@ -10,8 +10,8 @@ Authors: Markus Himmel, Scott Morrison
 -/
 import Mathlib.CategoryTheory.Preadditive.Yoneda.Basic
 import Mathlib.CategoryTheory.Preadditive.Injective
-import Mathlib.Algebra.Category.Group.EpiMono
-import Mathlib.Algebra.Category.Module.EpiMono
+import Mathlib.Algebra.Category.GroupCat.EpiMono
+import Mathlib.Algebra.Category.ModuleCat.EpiMono
 
 /-!
 An object is injective iff the preadditive yoneda functor on it preserves epimorphisms.
@@ -34,24 +34,24 @@ namespace Injective
 
 theorem injective_iff_preservesEpimorphisms_preadditiveYoneda_obj (J : C) :
     Injective J ↔ (preadditiveYoneda.obj J).PreservesEpimorphisms := by
-  rw [injective_iff_preserves_epimorphisms_yoneda_obj]
-  refine' ⟨fun h : (preadditive_yoneda.obj J ⋙ forget _).PreservesEpimorphisms => _, _⟩
-  ·
-    exact
-      functor.preserves_epimorphisms_of_preserves_of_reflects (preadditive_yoneda.obj J) (forget _)
+  rw [injective_iff_preservesEpimorphisms_yoneda_obj]
+  refine'
+    ⟨fun h : (preadditiveYoneda.obj J ⋙  (forget AddCommGroupCat)).PreservesEpimorphisms => _, _⟩
+  · exact
+      Functor.preservesEpimorphisms_of_preserves_of_reflects (preadditiveYoneda.obj J) (forget _)
   · intro
-    exact (inferInstance : (preadditive_yoneda.obj J ⋙ forget _).PreservesEpimorphisms)
+    exact (inferInstance : (preadditiveYoneda.obj J ⋙ forget _).PreservesEpimorphisms)
 #align category_theory.injective.injective_iff_preserves_epimorphisms_preadditive_yoneda_obj CategoryTheory.Injective.injective_iff_preservesEpimorphisms_preadditiveYoneda_obj
 
 theorem injective_iff_preservesEpimorphisms_preadditive_yoneda_obj' (J : C) :
     Injective J ↔ (preadditiveYonedaObj J).PreservesEpimorphisms := by
-  rw [injective_iff_preserves_epimorphisms_yoneda_obj]
-  refine' ⟨fun h : (preadditive_yoneda_obj J ⋙ forget _).PreservesEpimorphisms => _, _⟩
-  ·
-    exact
-      functor.preserves_epimorphisms_of_preserves_of_reflects (preadditive_yoneda_obj J) (forget _)
+  rw [injective_iff_preservesEpimorphisms_yoneda_obj]
+  refine'
+    ⟨fun h : (preadditiveYonedaObj J ⋙ (forget <| ModuleCat (End J))).PreservesEpimorphisms => _, _⟩
+  · exact
+      Functor.preservesEpimorphisms_of_preserves_of_reflects (preadditiveYonedaObj J) (forget _)
   · intro
-    exact (inferInstance : (preadditive_yoneda_obj J ⋙ forget _).PreservesEpimorphisms)
+    exact (inferInstance : (preadditiveYonedaObj J ⋙ forget _).PreservesEpimorphisms)
 #align category_theory.injective.injective_iff_preserves_epimorphisms_preadditive_yoneda_obj' CategoryTheory.Injective.injective_iff_preservesEpimorphisms_preadditive_yoneda_obj'
 
 end Injective
