0f0b14bb4cb5564d503b8dcab867a11181266870
Yury G. Kudryashov <urkud@urkud.name>
2023-05-10 03:13:42 -0500

    Unused arg

--- Mathlib/Analysis/NormedSpace/Units.lean
+++ Mathlib/Analysis/NormedSpace/Units.lean
@@ -159,7 +159,7 @@ theorem inverse_add_nth_order (x : Rˣ) (n : ℕ) :
   rw [ht]
 #align normed_ring.inverse_add_nth_order NormedRing.inverse_add_nth_order
 
-theorem inverse_one_sub_norm : (fun t : R => inverse (1 - t)) =O[𝓝 0] (fun t => 1 : R → ℝ) := by
+theorem inverse_one_sub_norm : (fun t : R => inverse (1 - t)) =O[𝓝 0] (fun _t => 1 : R → ℝ) := by
   simp only [IsBigO, IsBigOWith, Metric.eventually_nhds_iff]
   refine ⟨‖(1 : R)‖ + 1, (2 : ℝ)⁻¹, by norm_num, fun t ht ↦ ?_⟩
   rw [dist_zero_right] at ht
