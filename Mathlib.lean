9912e05d51763f8ba2d7731703c2ff6bc40a7ae9
Moritz Firsching <moritz.firsching@gmail.com>
2023-05-10 13:30:59 +0000

    feat: port CategoryTheory.Preadditive.HomOrthogonal (#3734)
    Co-authored-by: Moritz Firsching <firsching@google.com>
    Co-authored-by: Jason Yuen <jason_yuen2007@hotmail.com>
    Co-authored-by: int-y1 <jason_yuen2007@hotmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -636,6 +636,7 @@ import Mathlib.CategoryTheory.Preadditive.Biproducts
 import Mathlib.CategoryTheory.Preadditive.EilenbergMoore
 import Mathlib.CategoryTheory.Preadditive.FunctorCategory
 import Mathlib.CategoryTheory.Preadditive.Generator
+import Mathlib.CategoryTheory.Preadditive.HomOrthogonal
 import Mathlib.CategoryTheory.Preadditive.Injective
 import Mathlib.CategoryTheory.Preadditive.InjectiveResolution
 import Mathlib.CategoryTheory.Preadditive.LeftExact
