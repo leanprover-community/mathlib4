75499222cc11ae5a47a99ba69c3066b962a0dc6f
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2023-05-10 21:33:18 -0400

    restore various uses

--- Mathlib/Analysis/Seminorm.lean
+++ Mathlib/Analysis/Seminorm.lean
@@ -464,8 +464,7 @@ variable [NormedField 𝕜] [AddCommGroup E] [Module 𝕜 E] {p q : Seminorm �
 theorem bddBelow_range_add : BddBelow (range fun u => p u + q (x - u)) :=
   ⟨0, by
     rintro _ ⟨x, rfl⟩
-    -- Porting note: the following was previously `dsimp; positivity`
-    exact add_nonneg (map_nonneg _ _) (map_nonneg _ _)⟩
+    dsimp ; positivity⟩
 #align seminorm.bdd_below_range_add Seminorm.bddBelow_range_add
 
 noncomputable instance instInf : Inf (Seminorm 𝕜 E) where
@@ -478,8 +477,7 @@ noncomputable instance instInf : Inf (Seminorm 𝕜 E) where
         · rw [norm_zero, MulZeroClass.zero_mul, zero_smul]
           refine'
             cinfᵢ_eq_of_forall_ge_of_forall_gt_exists_lt
-              -- Porting note: the following was previously `fun i => by positivity`
-              (fun i => add_nonneg (map_nonneg _ _) (map_nonneg _ _))
+              (fun i => by positivity)
               fun x hx => ⟨0, by rwa [map_zero, sub_zero, map_zero, add_zero]⟩
         simp_rw [Real.mul_infᵢ_of_nonneg (norm_nonneg a), mul_add, ← map_smul_eq_mul p, ←
           map_smul_eq_mul q, smul_sub]
