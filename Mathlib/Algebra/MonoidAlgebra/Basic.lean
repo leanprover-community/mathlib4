3baa1ac7ddccf7ea4fd5940b4cd9db0d6805a6ac
Eric Rodriguez <ericrboidi@gmail.com>
2023-05-11 22:58:14 +0100

    forgot sha update

--- Mathlib/Algebra/MonoidAlgebra/Basic.lean
+++ Mathlib/Algebra/MonoidAlgebra/Basic.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl, Yury G. Kudryashov, Scott Morrison
 
 ! This file was ported from Lean 3 source module algebra.monoid_algebra.basic
-! leanprover-community/mathlib commit f69db8cecc668e2d5894d7e9bfc491da60db3b9f
+! leanprover-community/mathlib commit 949dc57e616a621462062668c9f39e4e17b64b69
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
