0c30c2e6c84a596bf15ce94188bc0e9217559828
Kyle Miller <kmill31415@gmail.com>
2023-05-11 02:18:21 +0000

    change to extract_lets and make work more like intros

--- Mathlib/Tactic/IntroAt.lean
+++ Mathlib/Tactic/IntroAt.lean
@@ -6,75 +6,107 @@ Authors: Kyle Miller
 import Mathlib.Tactic.Basic
 
 /-!
-# The `intro at` tactic
+# The `extract_lets at` tactic
 
-This module defines a tactic `intro ... at h` that can be used to (in essence) do `cases`
+This module defines a tactic `extract_lets ... at h` that can be used to (in essence) do `cases`
 on a `let` expression.
 -/
 
 
 open Lean Elab Parser Meta Tactic
 
-/-- Given a local hypothesis that can in some sense be introed (like one with a type that is
-a `let`), then create a new local hypothesis with that introduced variable.
+/-- Given a local hypothesis whose type is a `let` expression, then lift the `let` bindings to be
+a new local definition. -/
+def Lean.MVarId.extractLetsAt (mvarId : MVarId) (fvarId : FVarId) (names : Array Name) :
+    MetaM (Array FVarId × MVarId) := do
+  mvarId.checkNotAssigned `extractLetAt
+  mvarId.withReverted #[fvarId] fun mvarId fvarIds => mvarId.withContext do
+    let mut mvarId := mvarId
+    let mut fvarIds' := #[]
+    for name in names do
+      let ty ← Lean.instantiateMVars (← mvarId.getType)
+      mvarId ← match ty with
+        | .letE n t v b ndep => process mvarId t (.letE n · v b ndep)
+        | .forallE n t v b   => process mvarId t (.forallE n · v b)
+        | _ => throwTacticEx `extractLetAt mvarId "unexpected auxiliary target"
+      let (fvarId', mvarId') ← mvarId.intro name
+      fvarIds' := fvarIds'.push fvarId'
+      mvarId := mvarId'
+    return (fvarIds', fvarIds.map .some, mvarId)
+where
+  /-- Check that `t` is a `let` and then do what's necessary to lift it over the binding
+  described by `mk`. -/
+  process (mvarId : MVarId) (t : Expr) (mk : Expr → Expr) : MetaM MVarId := do
+    let .letE n' t' v' b' _ := t.cleanupAnnotations
+      | throwTacticEx `extractLetAt mvarId "insufficient number of `let` expressions"
+    -- Lift the let
+    withLetDecl n' t' v' fun fvar => do
+      let b' := b'.instantiate1 fvar
+      let ty' ← mkLetFVars #[fvar] <| mk b'
+      mvarId.change ty'
 
-So far `let` is all `introAt` can handle, but it could be extended to handle constructs such as
-`Exists` and `Sigma`, which are one-constructor inductive types whose constructor has
-two arguments. -/
-def Lean.MVarId.introAt (mvarId : MVarId) (fvarId : FVarId) (name : Name) :
-    MetaM (FVarId × MVarId) := do
-  mvarId.checkNotAssigned `introAt
-  mvarId.withReverted #[fvarId] fun mvarId fvarids => mvarId.withContext do
-    let ty ← Lean.instantiateMVars (← mvarId.getType)
-    let mvarId ← match ty with
-      | .letE n (.letE n' t' v' b' _) v b ndep =>
-        -- Lift the let over the let
-        withLetDecl n' t' v' fun fvar => do
-          let b' := b'.instantiate1 fvar
-          let ty' ← mkLetFVars #[fvar] <| .letE n b' v b ndep
-          mvarId.change ty'
-      | .letE .. => throwTacticEx `introAt mvarId "local let binding must be a `let`"
-      | .forallE n (.letE n' t' v' b' _) v b =>
-        -- Lift the let over the forall
-        withLetDecl n' t' v' fun fvar => do
-          let b' := b'.instantiate1 fvar
-          let ty' ← mkLetFVars #[fvar] <| .forallE n b' v b
-          mvarId.change ty'
-      | .forallE .. => throwTacticEx `introAt mvarId "local variable must be a `let`"
-      | _ => throwTacticEx `introAt mvarId "unexpected auxiliary target"
-    let (fvarId, mvarId) ← mvarId.intro name
-    return (fvarId, fvarids.map .some, mvarId)
+/-- Counts the immediate depth of a nested `let` expression. -/
+def Lean.Expr.letDepth : Expr → Nat
+  | .letE _ _ _ b _ => b.letDepth + 1
+  | _ => 0
+
+/-- A more limited version of `Lean.MVarId.introN` that ensures the goal is a
+nested `let` expression. -/
+def Lean.MVarId.extractLets (mvarId : MVarId) (names : Array Name) :
+    MetaM (Array FVarId × MVarId) :=
+  mvarId.withContext do
+    let ty := (← Lean.instantiateMVars (← mvarId.getType)).cleanupAnnotations
+    if ty.letDepth < names.size then
+      throwTacticEx `extractLet mvarId "insufficient number of `let` expressions"
+    mvarId.introN names.size names.toList
 
 namespace Mathlib
 
-/-- The `intro ... at ...` tactic is for introducing additional local hypotheses associated
-to a given local hypothesis. The primary use is to lift `let`s into the local context, and
-it can be thought of as being a `cases` tactic for `let`s.
+/-- The `extract_lets at h` tactic takes a local hypothesis of the form `h : let x := v; b`
+and introduces a new local definition `x := v` while changing `h` to be `h : b`.  It can be thought
+of as being a `cases` tactic for `let` expressions. It can also be thought of as being like
+`intros at h` for `let` expressions.
 
-For example, if `h : let x := 1; x = x`, then `intro x at h` introduces `x : Nat := 1` and changes
-`h` to `h : x = x`.
+For example, if `h : let x := 1; x = x`, then `extract_lets x at h` introduces `x : Nat := 1` and
+changes `h` to `h : x = x`.
 
-The tactic `intro at ⊢` raises an error; one should instead use the `intro`. -/
-syntax (name := introAt) "intro " notFollowedBy("|") (colGt term:max)* (ppSpace location) : tactic
+Just like `intros`, the `extract_lets` tactic either takes a list of names, in which case
+that specifies the number of `let` bindings that must be extracted, or it takes no names, in which
+case all the `let` bindings are extracted.
 
-@[tactic Mathlib.introAt] def evalIntroAt : Tactic := fun stx => do
+The tactic `extract_let at ⊢` is a weaker form of `intros` that only introduces obvious `let`s. -/
+syntax (name := extractLets) "extract_lets " (colGt (ident <|> hole))* (ppSpace location) : tactic
+
+@[tactic Mathlib.extractLets] def evalExtractLet : Tactic := fun stx => do
   match stx with
-  | `(tactic| intro $loc:location)                   => introStep none `_ loc
-  | `(tactic| intro $h:ident $loc:location)          => introStep h h.getId loc
-  | `(tactic| intro _%$tk $loc:location)             => introStep tk `_ loc
-  | `(tactic| intro $h:term $hs:term* $loc:location) =>
-    evalTactic (← `(tactic| intro $h:term $loc:location; intro $hs:term* $loc:location))
+  | `(tactic| extract_lets $loc:location)         => doExtract none loc
+  | `(tactic| extract_lets $hs* $loc:location)    => doExtract hs loc
   | _ => throwUnsupportedSyntax
 where
-  introStep (ref : Option Syntax) (n : Name) (loc : TSyntax `Lean.Parser.Tactic.location) :
+  setupNames (ids? : Option (TSyntaxArray [`ident, `Lean.Parser.Term.hole])) (ty : Expr) :
+      MetaM (Array Name) := do
+    if let some ids := ids? then
+      return ids.map getNameOfIdent'
+    else
+      return Array.mkArray (← instantiateMVars ty).cleanupAnnotations.letDepth `_
+  doExtract (ids? : Option (TSyntaxArray [`ident, `Lean.Parser.Term.hole]))
+      (loc : TSyntax `Lean.Parser.Tactic.location) :
       TacticM Unit := do
+    let process (f : MVarId → Array Name → MetaM (Array FVarId × MVarId))
+        (ty : MVarId → MetaM Expr) : TacticM Unit := do
+      let fvarIds ← liftMetaTacticAux fun mvarId => do
+        let ids ← setupNames ids? (← ty mvarId)
+        let (fvarIds, mvarId) ← f mvarId ids
+        return (fvarIds, [mvarId])
+      if let some ids := ids? then
+        withMainContext do
+          for stx in ids, fvarId in fvarIds do
+            Term.addLocalVarInfo stx (.fvar fvarId)
     withLocation (expandOptLocation (mkOptionalNode loc))
       (atLocal := fun h ↦ do
-        let fvarId ← liftMetaTacticAux fun mvarId => do
-          let (fvarId, mvarId) ← mvarId.introAt h n
-          return (fvarId, [mvarId])
-        if let some stx := ref then Term.addLocalVarInfo stx (.fvar fvarId))
-      (atTarget := throwError "use the `intro` tactic rather than `intro at ⊢`")
-      (failed := fun _ ↦ throwError "intro at tactic failed")
+        process (fun mvarId ids => mvarId.extractLetsAt h ids) (fun _ => h.getType))
+      (atTarget := do
+        process (fun mvarId ids => mvarId.extractLets ids) (fun mvarId => mvarId.getType))
+      (failed := fun _ ↦ throwError "extract_let tactic failed")
 
 end Mathlib
--- test/IntroAt.lean
+++ test/IntroAt.lean
@@ -2,27 +2,45 @@ import Mathlib.Tactic.IntroAt
 import Std.Tactic.GuardExpr
 
 example (h : let x := 1; x = x) : True := by
-  intro y at h
+  extract_lets y at h
+  fail_if_success extract_lets a at h
+  extract_lets at h
   guard_hyp y : Nat := 1
   guard_hyp h :ₛ y = y
   trivial
 
 example : True := by
   let h : (let x := 1; x = x) := rfl
-  intro y at h
+  extract_lets y at h
   guard_hyp y : Nat := 1
   guard_hyp h :ₛ y = y := rfl
   trivial
 
 example (h : let x := 1; let y := 2; x + 1 = y) : True := by
-  intro x y at h
+  extract_lets x y at h
   guard_hyp x : Nat := 1
   guard_hyp y : Nat := 2
   guard_hyp h :ₛ x + 1 = y
   trivial
 
 example (h : let x := 1; let y := 2; x + 1 = y) : True := by
-  intro x at h
+  extract_lets at h
+  rename_i a b
+  guard_hyp a : Nat := 1
+  guard_hyp b : Nat := 2
+  guard_hyp h :ₛ a + 1 = b
+  trivial
+
+example (h : let x := 1; let y := 2; x + 1 = y) : True := by
+  extract_lets x at h
+  intros
   guard_hyp x : Nat := 1
   guard_hyp h :ₛ let y := 2; x + 1 = y
+  trivial
+
+example (h : let x := 1; let y := 2; x + 1 = y) : let _z := 3; True := by
+  extract_lets at *
+  guard_hyp h : _ + 1 = _
+  fail_if_success extract_lets x at h
+  guard_target =ₛ True
   trivial
