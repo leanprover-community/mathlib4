d3df16bb26bc9e477937c1cb8f107466bfb71e32
Matthew Ballard <matt@mrb.email>
2023-05-11 07:56:21 -0400

    move cofan results back to products.lean

--- Mathlib/Topology/Category/Top/Limits/Products.lean
+++ Mathlib/Topology/Category/Top/Limits/Products.lean
@@ -292,3 +292,107 @@ theorem embedding_prod_map {W X Y Z : TopCat.{u}} {f : W ⟶ X} {g : Y ⟶ Z} (h
 #align Top.embedding_prod_map TopCat.embedding_prod_map
 
 end Prod
+
+/-- The binary coproduct cofan in `TopCat`. -/
+protected def binaryCofan (X Y : TopCat.{u}) : BinaryCofan X Y :=
+  BinaryCofan.mk (⟨Sum.inl, by continuity⟩ : X ⟶ TopCat.of (Sum X Y)) ⟨Sum.inr, by continuity⟩
+#align Top.binary_cofan TopCat.binaryCofan
+
+/-- The constructed binary coproduct cofan in `TopCat` is the coproduct. -/
+def binaryCofanIsColimit (X Y : TopCat.{u}) : IsColimit (TopCat.binaryCofan X Y) := by
+  refine' Limits.BinaryCofan.isColimitMk (fun s =>
+    {toFun := Sum.elim s.inl s.inr, continuous_toFun := _ }) _ _ _
+  · apply
+      Continuous.sum_elim (BinaryCofan.inl s).continuous_toFun (BinaryCofan.inr s).continuous_toFun
+  · intro s
+    ext
+    rfl
+  · intro s
+    ext
+    rfl
+  · intro s m h₁ h₂
+    ext (x | x)
+    exacts[(ConcreteCategory.congr_hom h₁ x : _), (ConcreteCategory.congr_hom h₂ x : _)]
+#align Top.binary_cofan_is_colimit TopCat.binaryCofanIsColimit
+
+theorem binaryCofan_isColimit_iff {X Y : TopCat} (c : BinaryCofan X Y) :
+    Nonempty (IsColimit c) ↔
+      OpenEmbedding c.inl ∧ OpenEmbedding c.inr ∧ IsCompl (Set.range c.inl) (Set.range c.inr) := by
+  classical
+    constructor
+    · rintro ⟨h⟩
+      rw [← show _ = c.inl from
+          h.comp_coconePointUniqueUpToIso_inv (binaryCofanIsColimit X Y) ⟨WalkingPair.left⟩,
+        ← show _ = c.inr from
+          h.comp_coconePointUniqueUpToIso_inv (binaryCofanIsColimit X Y) ⟨WalkingPair.right⟩]
+      dsimp
+      refine' ⟨(homeoOfIso <| h.coconePointUniqueUpToIso
+        (binaryCofanIsColimit X Y)).symm.openEmbedding.comp openEmbedding_inl,
+          (homeoOfIso <| h.coconePointUniqueUpToIso
+            (binaryCofanIsColimit X Y)).symm.openEmbedding.comp openEmbedding_inr, _⟩
+      simp only [Functor.map_comp]
+      erw [Set.range_comp, ← eq_compl_iff_isCompl, Set.range_comp _ Sum.inr,
+        ← Set.image_compl_eq (homeoOfIso <| h.coconePointUniqueUpToIso
+          (binaryCofanIsColimit X Y)).symm.bijective, Set.compl_range_inr.symm]
+      congr 1
+    · rintro ⟨h₁, h₂, h₃⟩
+      have : ∀ x, x ∈ Set.range c.inl ∨ x ∈ Set.range c.inr := by
+        rw [eq_compl_iff_isCompl.mpr h₃.symm]
+        exact fun _ => or_not
+      refine' ⟨BinaryCofan.IsColimit.mk _ _ _ _ _⟩
+      · intro T f g
+        refine' ContinuousMap.mk _ _
+        · exact fun x =>
+            if h : x ∈ Set.range c.inl then f ((Equiv.ofInjective _ h₁.inj).symm ⟨x, h⟩)
+            else g ((Equiv.ofInjective _ h₂.inj).symm ⟨x, (this x).resolve_left h⟩)
+        rw [continuous_iff_continuousAt]
+        intro x
+        by_cases x ∈ Set.range c.inl
+        · revert h x
+          apply (IsOpen.continuousOn_iff _).mp
+          · rw [continuousOn_iff_continuous_restrict]
+            convert_to Continuous (f ∘ (Homeomorph.ofEmbedding _ h₁.toEmbedding).symm)
+            · ext ⟨x, hx⟩
+              exact dif_pos hx
+            apply Continuous.comp
+            · exact f.continuous_toFun
+            · continuity
+          · exact h₁.open_range
+        · revert h x
+          apply (IsOpen.continuousOn_iff _).mp
+          · rw [continuousOn_iff_continuous_restrict]
+            have : ∀ a, a ∉ Set.range c.inl → a ∈ Set.range c.inr := by
+              rintro a (h : a ∈ Set.range c.inlᶜ)
+              rwa [eq_compl_iff_isCompl.mpr h₃.symm]
+            convert_to Continuous
+                (g ∘ (Homeomorph.ofEmbedding _ h₂.toEmbedding).symm ∘ Subtype.map _ this)
+            · ext ⟨x, hx⟩
+              exact dif_neg hx
+            apply Continuous.comp
+            · exact g.continuous_toFun
+            · apply Continuous.comp
+              · continuity
+              · rw [embedding_subtype_val.toInducing.continuous_iff]
+                exact continuous_subtype_val
+          · change IsOpen (Set.range c.inlᶜ)
+            rw [← eq_compl_iff_isCompl.mpr h₃.symm]
+            exact h₂.open_range
+      · intro T f g
+        ext x
+        refine' (dif_pos _).trans _
+        · exact ⟨x, rfl⟩
+        · dsimp; conv_lhs => erw [Equiv.ofInjective_symm_apply]
+      · intro T f g
+        ext x
+        refine' (dif_neg _).trans _
+        · rintro ⟨y, e⟩
+          have : c.inr x ∈ Set.range c.inl ⊓ Set.range c.inr := ⟨⟨_, e⟩, ⟨_, rfl⟩⟩
+          rwa [disjoint_iff.mp h₃.1] at this
+        · exact congr_arg g (Equiv.ofInjective_symm_apply _ _)
+      · rintro T _ _ m rfl rfl
+        ext x
+        change m x = dite _ _ _
+        split_ifs <;> exact congr_arg _ (Equiv.apply_ofInjective_symm _ ⟨_, _⟩).symm
+#align Top.binary_cofan_is_colimit_iff TopCat.binaryCofan_isColimit_iff
+
+
