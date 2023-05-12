b29921cba3f014b819747d5a1df36591ed27fae4
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2023-05-12 14:43:08 +0000

    feat: port Algebra.Modeq (#3921)

--- Mathlib.lean
+++ Mathlib.lean
@@ -163,6 +163,7 @@ import Mathlib.Algebra.Lie.Basic
 import Mathlib.Algebra.Lie.NonUnitalNonAssocAlgebra
 import Mathlib.Algebra.Lie.Subalgebra
 import Mathlib.Algebra.LinearRecurrence
+import Mathlib.Algebra.ModEq
 import Mathlib.Algebra.Module.Algebra
 import Mathlib.Algebra.Module.Basic
 import Mathlib.Algebra.Module.BigOperators
