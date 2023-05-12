ed4042f6221b07f8bd3a67ecf8cf21e6c7235f7e
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2023-05-12 14:36:48 -0400

    fix test

--- test/positivity.lean
+++ test/positivity.lean
@@ -7,7 +7,7 @@ import Mathlib.Analysis.Normed.Group.Basic
 This tactic proves goals of the form `0 ≤ a` and `0 < a`.
 -/
 
-open Function NNReal ENNReal
+open Function Nat NNReal ENNReal
 
 variable {ι α β : Type _}
 
@@ -231,7 +231,7 @@ example : 0 ≤ max (-3 : ℤ) 5 := by positivity
 --   [OrderedSMul α β] {a : α} (ha : 0 < a) {b : β} (hb : 0 < b) : 0 ≤ a • b := by positivity
 
 example (n : ℕ) : 0 < n.succ := by positivity
-example (n : ℕ) : 0 < n! := by positivity
+example (n : ℕ) : 0 < n ! := by positivity
 -- example (n k : ℕ) : 0 < n.asc_factorial k := by positivity
 
 -- example {α : Type _} (s : Finset α) (hs : s.Nonempty) : 0 < s.card := by positivity
