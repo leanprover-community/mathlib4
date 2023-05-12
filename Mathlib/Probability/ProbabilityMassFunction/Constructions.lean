cc37412636c96f678d5b4a4e00286ad2174d76de
int-y1 <jason_yuen2007@hotmail.com>
2023-05-11 17:47:25 -0700

    fix

--- Mathlib/Probability/ProbabilityMassFunction/Constructions.lean
+++ Mathlib/Probability/ProbabilityMassFunction/Constructions.lean
@@ -13,18 +13,18 @@ import Mathlib.Probability.ProbabilityMassFunction.Monad
 /-!
 # Specific Constructions of Probability Mass Functions
 
-This file gives a number of different `pmf` constructions for common probability distributions.
+This file gives a number of different `Pmf` constructions for common probability distributions.
 
-`map` and `seq` allow pushing a `pmf α` along a function `f : α → β` (or distribution of
-functions `f : pmf (α → β)`) to get a `pmf β`
+`map` and `seq` allow pushing a `Pmf α` along a function `f : α → β` (or distribution of
+functions `f : Pmf (α → β)`) to get a `Pmf β`.
 
-`of_finset` and `of_fintype` simplify the construction of a `pmf α` from a function `f : α → ℝ≥0∞`,
-by allowing the "sum equals 1" constraint to be in terms of `finset.sum` instead of `tsum`.
+`ofFinset` and `ofFintype` simplify the construction of a `Pmf α` from a function `f : α → ℝ≥0∞`,
+by allowing the "sum equals 1" constraint to be in terms of `Finset.sum` instead of `tsum`.
 
-`normalize` constructs a `pmf α` by normalizing a function `f : α → ℝ≥0∞` by its sum,
-and `filter` uses this to filter the support of a `pmf` and re-normalize the new distribution.
+`normalize` constructs a `Pmf α` by normalizing a function `f : α → ℝ≥0∞` by its sum,
+and `filter` uses this to filter the support of a `Pmf` and re-normalize the new distribution.
 
-`bernoulli` represents the bernoulli distribution on `bool`
+`bernoulli` represents the bernoulli distribution on `Bool`.
 
 -/
 
@@ -39,15 +39,14 @@ open Classical BigOperators NNReal ENNReal
 
 section Map
 
-/-- The functorial action of a function on a `pmf`. -/
+/-- The functorial action of a function on a `Pmf`. -/
 def map (f : α → β) (p : Pmf α) : Pmf β :=
   bind p (pure ∘ f)
 #align pmf.map Pmf.map
 
 variable (f : α → β) (p : Pmf α) (b : β)
 
-theorem monad_map_eq_map {α β : Type _} (f : α → β) (p : Pmf α) : f <$> p = p.map f :=
-  rfl
+theorem monad_map_eq_map {α β : Type _} (f : α → β) (p : Pmf α) : f <$> p = p.map f := rfl
 #align pmf.monad_map_eq_map Pmf.monad_map_eq_map
 
 @[simp]
@@ -62,15 +61,14 @@ theorem support_map : (map f p).support = f '' p.support :=
 theorem mem_support_map_iff : b ∈ (map f p).support ↔ ∃ a ∈ p.support, f a = b := by simp
 #align pmf.mem_support_map_iff Pmf.mem_support_map_iff
 
-theorem bind_pure_comp : bind p (pure ∘ f) = map f p :=
-  rfl
+theorem bind_pure_comp : bind p (pure ∘ f) = map f p := rfl
 #align pmf.bind_pure_comp Pmf.bind_pure_comp
 
 theorem map_id : map id p = p :=
   bind_pure _
 #align pmf.map_id Pmf.map_id
 
-theorem map_comp (g : β → γ) : (p.map f).map g = p.map (g ∘ f) := by simp [map]
+theorem map_comp (g : β → γ) : (p.map f).map g = p.map (g ∘ f) := by simp [map, Function.comp]
 #align pmf.map_comp Pmf.map_comp
 
 theorem pure_map (a : α) : (pure a).map f = pure (f a) :=
@@ -83,12 +81,12 @@ theorem map_bind (q : α → Pmf β) (f : β → γ) : (p.bind q).map f = p.bind
 
 @[simp]
 theorem bind_map (p : Pmf α) (f : α → β) (q : β → Pmf γ) : (p.map f).bind q = p.bind (q ∘ f) :=
-  (bind_bind _ _ _).trans (congr_arg _ (funext fun a => pure_bind _ _))
+  (bind_bind _ _ _).trans (congr_arg _ (funext fun _ => pure_bind _ _))
 #align pmf.bind_map Pmf.bind_map
 
 @[simp]
 theorem map_const : p.map (Function.const α b) = pure b := by
-  simp only [map, bind_const, Function.comp_const]
+  simp only [map, Function.comp, bind_const, Function.const]
 #align pmf.map_const Pmf.map_const
 
 section Measure
@@ -97,15 +95,15 @@ variable (s : Set β)
 
 @[simp]
 theorem toOuterMeasure_map_apply : (p.map f).toOuterMeasure s = p.toOuterMeasure (f ⁻¹' s) := by
-  simp [map, Set.indicator, to_outer_measure_apply p (f ⁻¹' s)]
+  simp [map, Set.indicator, toOuterMeasure_apply p (f ⁻¹' s)]
 #align pmf.to_outer_measure_map_apply Pmf.toOuterMeasure_map_apply
 
 @[simp]
 theorem toMeasure_map_apply [MeasurableSpace α] [MeasurableSpace β] (hf : Measurable f)
     (hs : MeasurableSet s) : (p.map f).toMeasure s = p.toMeasure (f ⁻¹' s) := by
-  rw [to_measure_apply_eq_to_outer_measure_apply _ s hs,
-    to_measure_apply_eq_to_outer_measure_apply _ (f ⁻¹' s) (measurableSet_preimage hf hs)]
-  exact to_outer_measure_map_apply f p s
+  rw [toMeasure_apply_eq_toOuterMeasure_apply _ s hs,
+    toMeasure_apply_eq_toOuterMeasure_apply _ (f ⁻¹' s) (measurableSet_preimage hf hs)]
+  exact toOuterMeasure_map_apply f p s
 #align pmf.to_measure_map_apply Pmf.toMeasure_map_apply
 
 end Measure
@@ -114,15 +112,14 @@ end Map
 
 section Seq
 
-/-- The monadic sequencing operation for `pmf`. -/
+/-- The monadic sequencing operation for `Pmf`. -/
 def seq (q : Pmf (α → β)) (p : Pmf α) : Pmf β :=
   q.bind fun m => p.bind fun a => pure (m a)
 #align pmf.seq Pmf.seq
 
 variable (q : Pmf (α → β)) (p : Pmf α) (b : β)
 
-theorem monad_seq_eq_seq {α β : Type _} (q : Pmf (α → β)) (p : Pmf α) : q <*> p = q.seq p :=
-  rfl
+theorem monad_seq_eq_seq {α β : Type _} (q : Pmf (α → β)) (p : Pmf α) : q <*> p = q.seq p := rfl
 #align pmf.monad_seq_eq_seq Pmf.monad_seq_eq_seq
 
 @[simp]
@@ -143,36 +140,33 @@ theorem mem_support_seq_iff : b ∈ (seq q p).support ↔ ∃ f ∈ q.support, b
 end Seq
 
 instance : LawfulFunctor Pmf where
-  mapConst_eq α β := rfl
-  id_map α := bind_pure
-  comp_map α β γ g h x := (map_comp _ _ _).symm
+  map_const := rfl
+  id_map := bind_pure
+  comp_map _ _ _ := (map_comp _ _ _).symm
 
-instance : LawfulMonad Pmf where
-  bind_pure_comp_eq_map α β f x := rfl
-  bind_map_eq_seq α β f x := rfl
-  pure_bind α β := pure_bind
-  bind_assoc α β γ := bind_bind
+instance : LawfulMonad Pmf := LawfulMonad.mk'
+  (bind_pure_comp := fun f x => rfl)
+  (id_map := id_map)
+  (pure_bind := pure_bind)
+  (bind_assoc := bind_bind)
 
 section OfFinset
 
-/- ./././Mathport/Syntax/Translate/Basic.lean:635:2: warning: expanding binder collection (a «expr ∉ » s) -/
 /-- Given a finset `s` and a function `f : α → ℝ≥0∞` with sum `1` on `s`,
-  such that `f a = 0` for `a ∉ s`, we get a `pmf` -/
+  such that `f a = 0` for `a ∉ s`, we get a `Pmf`. -/
 def ofFinset (f : α → ℝ≥0∞) (s : Finset α) (h : (∑ a in s, f a) = 1)
     (h' : ∀ (a) (_ : a ∉ s), f a = 0) : Pmf α :=
   ⟨f, h ▸ hasSum_sum_of_ne_finset_zero h'⟩
 #align pmf.of_finset Pmf.ofFinset
 
-/- ./././Mathport/Syntax/Translate/Basic.lean:635:2: warning: expanding binder collection (a «expr ∉ » s) -/
 variable {f : α → ℝ≥0∞} {s : Finset α} (h : (∑ a in s, f a) = 1) (h' : ∀ (a) (_ : a ∉ s), f a = 0)
 
 @[simp]
-theorem ofFinset_apply (a : α) : ofFinset f s h h' a = f a :=
-  rfl
+theorem ofFinset_apply (a : α) : ofFinset f s h h' a = f a := rfl
 #align pmf.of_finset_apply Pmf.ofFinset_apply
 
 @[simp]
-theorem support_ofFinset : (ofFinset f s h h').support = s ∩ Function.support f :=
+theorem support_ofFinset : (ofFinset f s h h').support = ↑s ∩ Function.support f :=
   Set.ext fun a => by simpa [mem_support_iff] using mt (h' a)
 #align pmf.support_of_finset Pmf.support_ofFinset
 
@@ -206,7 +200,7 @@ end OfFinset
 
 section OfFintype
 
-/-- Given a finite type `α` and a function `f : α → ℝ≥0∞` with sum 1, we get a `pmf`. -/
+/-- Given a finite type `α` and a function `f : α → ℝ≥0∞` with sum 1, we get a `Pmf`. -/
 def ofFintype [Fintype α] (f : α → ℝ≥0∞) (h : (∑ a, f a) = 1) : Pmf α :=
   ofFinset f Finset.univ h fun a ha => absurd (Finset.mem_univ a) ha
 #align pmf.of_fintype Pmf.ofFintype
@@ -214,17 +208,14 @@ def ofFintype [Fintype α] (f : α → ℝ≥0∞) (h : (∑ a, f a) = 1) : Pmf 
 variable [Fintype α] {f : α → ℝ≥0∞} (h : (∑ a, f a) = 1)
 
 @[simp]
-theorem ofFintype_apply (a : α) : ofFintype f h a = f a :=
-  rfl
+theorem ofFintype_apply (a : α) : ofFintype f h a = f a := rfl
 #align pmf.of_fintype_apply Pmf.ofFintype_apply
 
 @[simp]
-theorem support_ofFintype : (ofFintype f h).support = Function.support f :=
-  rfl
+theorem support_ofFintype : (ofFintype f h).support = Function.support f := rfl
 #align pmf.support_of_fintype Pmf.support_ofFintype
 
-theorem mem_support_ofFintype_iff (a : α) : a ∈ (ofFintype f h).support ↔ f a ≠ 0 :=
-  Iff.rfl
+theorem mem_support_ofFintype_iff (a : α) : a ∈ (ofFintype f h).support ↔ f a ≠ 0 := Iff.rfl
 #align pmf.mem_support_of_fintype_iff Pmf.mem_support_ofFintype_iff
 
 section Measure
@@ -248,7 +239,7 @@ end OfFintype
 
 section normalize
 
-/-- Given a `f` with non-zero and non-infinite sum, get a `pmf` by normalizing `f` by its `tsum` -/
+/-- Given a `f` with non-zero and non-infinite sum, get a `Pmf` by normalizing `f` by its `tsum`. -/
 def normalize (f : α → ℝ≥0∞) (hf0 : tsum f ≠ 0) (hf : tsum f ≠ ∞) : Pmf α :=
   ⟨fun a => f a * (∑' x, f x)⁻¹,
     ENNReal.summable.hasSum_iff.2 (ENNReal.tsum_mul_right.trans (ENNReal.mul_inv_cancel hf0 hf))⟩
@@ -257,8 +248,7 @@ def normalize (f : α → ℝ≥0∞) (hf0 : tsum f ≠ 0) (hf : tsum f ≠ ∞)
 variable {f : α → ℝ≥0∞} (hf0 : tsum f ≠ 0) (hf : tsum f ≠ ∞)
 
 @[simp]
-theorem normalize_apply (a : α) : (normalize f hf0 hf) a = f a * (∑' x, f x)⁻¹ :=
-  rfl
+theorem normalize_apply (a : α) : (normalize f hf0 hf) a = f a * (∑' x, f x)⁻¹ := rfl
 #align pmf.normalize_apply Pmf.normalize_apply
 
 @[simp]
@@ -273,7 +263,7 @@ end normalize
 
 section Filter
 
-/-- Create new `pmf` by filtering on a set with non-zero measure and normalizing -/
+/-- Create new `Pmf` by filtering on a set with non-zero measure and normalizing. -/
 def filter (p : Pmf α) (s : Set α) (h : ∃ a ∈ s, a ∈ p.support) : Pmf α :=
   Pmf.normalize (s.indicator p) (by simpa using h) (p.tsum_coe_indicator_ne_top s)
 #align pmf.filter Pmf.filter
@@ -282,28 +272,28 @@ variable {p : Pmf α} {s : Set α} (h : ∃ a ∈ s, a ∈ p.support)
 
 @[simp]
 theorem filter_apply (a : α) :
-    (p.filterₓ s h) a = s.indicator p a * (∑' a', (s.indicator p) a')⁻¹ := by
-  rw [Filter, normalize_apply]
+    (p.filter s h) a = s.indicator p a * (∑' a', (s.indicator p) a')⁻¹ := by
+  rw [filter, normalize_apply]
 #align pmf.filter_apply Pmf.filter_apply
 
-theorem filter_apply_eq_zero_of_not_mem {a : α} (ha : a ∉ s) : (p.filterₓ s h) a = 0 := by
-  rw [filter_apply, set.indicator_apply_eq_zero.mpr fun ha' => absurd ha' ha, MulZeroClass.zero_mul]
+theorem filter_apply_eq_zero_of_not_mem {a : α} (ha : a ∉ s) : (p.filter s h) a = 0 := by
+  rw [filter_apply, Set.indicator_apply_eq_zero.mpr fun ha' => absurd ha' ha, MulZeroClass.zero_mul]
 #align pmf.filter_apply_eq_zero_of_not_mem Pmf.filter_apply_eq_zero_of_not_mem
 
-theorem mem_support_filter_iff {a : α} : a ∈ (p.filterₓ s h).support ↔ a ∈ s ∧ a ∈ p.support :=
+theorem mem_support_filter_iff {a : α} : a ∈ (p.filter s h).support ↔ a ∈ s ∧ a ∈ p.support :=
   (mem_support_normalize_iff _ _ _).trans Set.indicator_apply_ne_zero
 #align pmf.mem_support_filter_iff Pmf.mem_support_filter_iff
 
 @[simp]
-theorem support_filter : (p.filterₓ s h).support = s ∩ p.support :=
-  Set.ext fun x => mem_support_filter_iff _
+theorem support_filter : (p.filter s h).support = s ∩ p.support :=
+  Set.ext fun _ => mem_support_filter_iff _
 #align pmf.support_filter Pmf.support_filter
 
-theorem filter_apply_eq_zero_iff (a : α) : (p.filterₓ s h) a = 0 ↔ a ∉ s ∨ a ∉ p.support := by
+theorem filter_apply_eq_zero_iff (a : α) : (p.filter s h) a = 0 ↔ a ∉ s ∨ a ∉ p.support := by
   erw [apply_eq_zero_iff, support_filter, Set.mem_inter_iff, not_and_or]
 #align pmf.filter_apply_eq_zero_iff Pmf.filter_apply_eq_zero_iff
 
-theorem filter_apply_ne_zero_iff (a : α) : (p.filterₓ s h) a ≠ 0 ↔ a ∈ s ∧ a ∈ p.support := by
+theorem filter_apply_ne_zero_iff (a : α) : (p.filter s h) a ≠ 0 ↔ a ∈ s ∧ a ∈ p.support := by
   rw [Ne.def, filter_apply_eq_zero_iff, not_or, Classical.not_not, Classical.not_not]
 #align pmf.filter_apply_ne_zero_iff Pmf.filter_apply_ne_zero_iff
 
@@ -311,7 +301,7 @@ end Filter
 
 section bernoulli
 
-/-- A `pmf` which assigns probability `p` to `tt` and `1 - p` to `ff`. -/
+/-- A `Pmf` which assigns probability `p` to `true` and `1 - p` to `false`. -/
 def bernoulli (p : ℝ≥0∞) (h : p ≤ 1) : Pmf Bool :=
   ofFintype (fun b => cond b p (1 - p)) (by simp [h])
 #align pmf.bernoulli Pmf.bernoulli
@@ -319,8 +309,7 @@ def bernoulli (p : ℝ≥0∞) (h : p ≤ 1) : Pmf Bool :=
 variable {p : ℝ≥0∞} (h : p ≤ 1) (b : Bool)
 
 @[simp]
-theorem bernoulli_apply : bernoulli p h b = cond b p (1 - p) :=
-  rfl
+theorem bernoulli_apply : bernoulli p h b = cond b p (1 - p) := rfl
 #align pmf.bernoulli_apply Pmf.bernoulli_apply
 
 @[simp]
@@ -337,5 +326,6 @@ theorem mem_support_bernoulli_iff : b ∈ (bernoulli p h).support ↔ cond b (p 
 
 end bernoulli
 
-end Pmf
+end
 
+end Pmf
