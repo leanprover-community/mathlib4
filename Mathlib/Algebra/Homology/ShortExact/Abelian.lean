9464690168a71c87873347f60ce0a9db2decd6c9
Joël Riou <joel.riou@universite-paris-saclay.fr>
2023-05-12 14:25:42 +0200

    it compiles

--- Mathlib/Algebra/Homology/ShortExact/Abelian.lean
+++ Mathlib/Algebra/Homology/ShortExact/Abelian.lean
@@ -33,18 +33,16 @@ variable [Abelian 𝒜]
 open ZeroObject
 
 theorem isIso_of_shortExact_of_isIso_of_isIso (h : ShortExact f g) (h' : ShortExact f' g')
-    (i₁ : A ⟶ A') (i₂ : B ⟶ B') (i₃ : C ⟶ C') (comm₁ : i₁ ≫ f' = f ≫ i₂) (comm₂ : i₂ ≫ g' = g ≫ i₃)
-    [IsIso i₁] [IsIso i₃] : IsIso i₂ := by
+    (i₁ : A ⟶ A') (i₂ : B ⟶ B') (i₃ : C ⟶ C') (comm₁ : i₁ ≫ f' = f ≫ i₂)
+    (comm₂ : i₂ ≫ g' = g ≫ i₃) [IsIso i₁] [IsIso i₃] : IsIso i₂ := by
   obtain ⟨_⟩ := h
   obtain ⟨_⟩ := h'
-  skip
-  refine'
-            @abelian.is_iso_of_is_iso_of_is_iso_of_is_iso_of_is_iso 𝒜 _ _ 0 _ _ _ 0 _ _ _ 0 f g 0 f'
-              g' 0 i₁ i₂ i₃ _ comm₁ comm₂ 0 0 0 0 0 _ _ _ _ _ _ _ _ _ _ _ <;>
-          try simp <;>
-        try apply exact_zero_left_of_mono <;>
-      try assumption <;>
-    rwa [← epi_iff_exact_zero_right]
+  refine @Abelian.isIso_of_isIso_of_isIso_of_isIso_of_isIso 𝒜 _ _ 0 _ _ _ 0 _ _ _ 0 f g 0 f' g'
+      0 i₁ i₂ i₃ ?_ comm₁ comm₂ 0 0 0 0 0 ?_ ?_ ?_ ?_ ?_ ?_ ?_ _ _ _ _
+  all_goals try simp
+  all_goals try assumption
+  all_goals try apply exact_zero_left_of_mono
+  all_goals rwa [← epi_iff_exact_zero_right ]
 #align category_theory.is_iso_of_short_exact_of_is_iso_of_is_iso CategoryTheory.isIso_of_shortExact_of_isIso_of_isIso
 
 /-- To construct a splitting of `A -f⟶ B -g⟶ C` it suffices to supply
@@ -54,16 +52,12 @@ together with proofs that `f` is mono and `g` is epi.
 
 The morphism `i` is then automatically an isomorphism. -/
 def Splitting.mk' (h : ShortExact f g) (i : B ⟶ A ⊞ C) (h1 : f ≫ i = biprod.inl)
-    (h2 : i ≫ biprod.snd = g) : Splitting f g where
-  Iso := by
-    refine' @as_iso _ _ _ _ i (id _)
-    refine'
-      is_iso_of_short_exact_of_is_iso_of_is_iso h _ _ _ _ (h1.trans (category.id_comp _).symm).symm
-        (h2.trans (category.comp_id _).symm)
-    constructor
-    apply exact_inl_snd
-  comp_iso_eq_inl := by rwa [as_iso_hom]
-  iso_comp_snd_eq := h2
+    (h2 : i ≫ biprod.snd = g) : Splitting f g :=
+  have : IsIso i := isIso_of_shortExact_of_isIso_of_isIso h ⟨exact_inl_snd A C⟩  (𝟙 _) i (𝟙 _)
+    (by aesop_cat) (by aesop_cat)
+  { iso := asIso i
+    comp_iso_eq_inl := h1
+    iso_comp_snd_eq := h2 }
 #align category_theory.splitting.mk' CategoryTheory.Splitting.mk'
 
 /-- To construct a splitting of `A -f⟶ B -g⟶ C` it suffices to supply
@@ -73,36 +67,31 @@ together with proofs that `f` is mono and `g` is epi.
 
 The morphism `i` is then automatically an isomorphism. -/
 def Splitting.mk'' (h : ShortExact f g) (i : A ⊞ C ⟶ B) (h1 : biprod.inl ≫ i = f)
-    (h2 : i ≫ g = biprod.snd) : Splitting f g where
-  Iso := by
-    refine' (@as_iso _ _ _ _ i (id _)).symm
-    refine'
-      is_iso_of_short_exact_of_is_iso_of_is_iso _ h _ _ _ (h1.trans (category.id_comp _).symm).symm
-        (h2.trans (category.comp_id _).symm)
-    constructor
-    apply exact_inl_snd
-  comp_iso_eq_inl := by rw [iso.symm_hom, as_iso_inv, is_iso.comp_inv_eq, h1]
-  iso_comp_snd_eq := by rw [iso.symm_hom, as_iso_inv, is_iso.inv_comp_eq, h2]
+    (h2 : i ≫ g = biprod.snd) : Splitting f g :=
+  have : IsIso i := isIso_of_shortExact_of_isIso_of_isIso ⟨exact_inl_snd A C⟩ h (𝟙 _) i (𝟙 _)
+    (by aesop_cat) (by aesop_cat)
+  { iso := (asIso i).symm
+    comp_iso_eq_inl := by rw [Iso.symm_hom, asIso_inv, IsIso.comp_inv_eq, h1]
+    iso_comp_snd_eq := by rw [Iso.symm_hom, asIso_inv, IsIso.inv_comp_eq, h2] }
 #align category_theory.splitting.mk'' CategoryTheory.Splitting.mk''
 
 /-- A short exact sequence that is left split admits a splitting. -/
 def LeftSplit.splitting {f : A ⟶ B} {g : B ⟶ C} (h : LeftSplit f g) : Splitting f g :=
-  Splitting.mk' h.ShortExact (biprod.lift h.LeftSplit.some g)
+  Splitting.mk' h.shortExact (biprod.lift h.left_split.choose g)
     (by
       ext
-      · simpa only [biprod.inl_fst, biprod.lift_fst, category.assoc] using h.left_split.some_spec
-      · simp only [biprod.inl_snd, biprod.lift_snd, category.assoc, h.exact.w])
+      · simpa only [biprod.inl_fst, biprod.lift_fst, Category.assoc] using h.left_split.choose_spec
+      · simp only [biprod.inl_snd, biprod.lift_snd, Category.assoc, h.exact.w])
     (by simp only [biprod.lift_snd])
 #align category_theory.left_split.splitting CategoryTheory.LeftSplit.splitting
 
 /-- A short exact sequence that is right split admits a splitting. -/
 def RightSplit.splitting {f : A ⟶ B} {g : B ⟶ C} (h : RightSplit f g) : Splitting f g :=
-  Splitting.mk'' h.ShortExact (biprod.desc f h.RightSplit.some) (biprod.inl_desc _ _)
+  Splitting.mk'' h.shortExact (biprod.desc f h.right_split.choose) (biprod.inl_desc _ _)
     (by
       ext
-      · rw [biprod.inl_snd, ← category.assoc, biprod.inl_desc, h.exact.w]
-      · rw [biprod.inr_snd, ← category.assoc, biprod.inr_desc, h.right_split.some_spec])
+      · rw [biprod.inl_snd, ← Category.assoc, biprod.inl_desc, h.exact.w]
+      · rw [biprod.inr_snd, ← Category.assoc, biprod.inr_desc, h.right_split.choose_spec])
 #align category_theory.right_split.splitting CategoryTheory.RightSplit.splitting
 
 end CategoryTheory
-
