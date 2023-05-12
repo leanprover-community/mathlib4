007a9043baa8123a88ecc65af9b2ec138a8c5bb3
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2023-05-11 20:24:18 -0400

    move out mod lemmas

--- test/RelCongr/mod.lean
+++ test/RelCongr/mod.lean
@@ -3,7 +3,7 @@ Copyright (c) 2022 Heather Macbeth. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Heather Macbeth
 -/
-import Mathlib.Tactic.RelCongr.ModEq
+import Mathlib.Data.Int.ModEq
 
 /-! # Modular arithmetic tests for the `rel_congr` tactic -/
 
