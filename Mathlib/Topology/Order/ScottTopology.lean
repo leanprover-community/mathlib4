ff2706d1875f444ff66a7008cc2b369a9edd350e
Christopher Hoskin <christopher.hoskin@gmail.com>
2023-05-12 20:38:41 +0100

    Use the name Scott-Hausdorff (after Karner)

--- Mathlib/Topology/Order/ScottTopology.lean
+++ Mathlib/Topology/Order/ScottTopology.lean
@@ -81,42 +81,43 @@ def upperSetTopology : TopologicalSpace α :=
   isOpen_inter := fun _ _ => IsUpperSet.inter,
   isOpen_unionₛ := fun _ h => isUpperSet_unionₛ h, }
 
+def ScottHausdorffTopology : TopologicalSpace α :=
+{ IsOpen := fun u => ∀ (d : Set α) (a : α), d.Nonempty → DirectedOn (· ≤ ·) d → IsLUB d a →
+    a ∈ u → ∃ b ∈ d, Ici b ∩ d ⊆ u,
+  isOpen_univ := by
+    intros d _ hd₁ _ _ _
+    cases' hd₁ with b hb
+    use b
+    constructor
+    . exact hb
+    . exact (Ici b ∩ d).subset_univ,
+  isOpen_inter := by
+    intros s t hs ht d a hd₁ hd₂ hd₃ ha
+    obtain ⟨b₁, hb₁_w, hb₁_h⟩ := hs d a hd₁ hd₂ hd₃ ha.1
+    obtain ⟨b₂, hb₂_w, hb₂_h⟩ := ht d a hd₁ hd₂ hd₃ ha.2
+    obtain ⟨c, hc_w, hc_h⟩ := hd₂ b₁ hb₁_w b₂ hb₂_w
+    refine ⟨c, hc_w, ?_⟩
+    . calc
+        Ici c ∩ d ⊆ (Ici b₁ ∩ Ici b₂) ∩ d := by
+        { apply inter_subset_inter_left d
+          apply subset_inter (Ici_subset_Ici.mpr hc_h.1) (Ici_subset_Ici.mpr hc_h.2) }
+        _ = (Ici b₁ ∩ d) ∩ (Ici b₂ ∩ d) := by rw [inter_inter_distrib_right]
+        _ ⊆ s ∩ t := inter_subset_inter hb₁_h hb₂_h
+  isOpen_unionₛ := by
+    intros s h d a hd₁ hd₂ hd₃ ha
+    obtain ⟨s₀, hs₀_w, hs₀_h⟩ := ha
+    obtain ⟨b, hb_w, hb_h⟩ := h s₀ hs₀_w d a hd₁ hd₂ hd₃ hs₀_h
+    use b
+    constructor
+    . exact hb_w
+    . exact Set.subset_unionₛ_of_subset s s₀ hb_h hs₀_w }
+
 /--
 The Scott topology is defined as the join of the topology of upper sets and the topological space
 where a set `u` is open if, when the least upper bound of a directed set `d` lies in `u` then there
 is a tail of `d` which is a subset of `u`.
 -/
-def ScottTopology' : TopologicalSpace α := upperSetTopology ⊔
-    { IsOpen := fun u => ∀ (d : Set α) (a : α), d.Nonempty → DirectedOn (· ≤ ·) d → IsLUB d a →
-      a ∈ u → ∃ b ∈ d, Ici b ∩ d ⊆ u,
-      isOpen_univ := by
-        intros d _ hd₁ _ _ _
-        cases' hd₁ with b hb
-        use b
-        constructor
-        . exact hb
-        . exact (Ici b ∩ d).subset_univ,
-      isOpen_inter := by
-        intros s t hs ht d a hd₁ hd₂ hd₃ ha
-        obtain ⟨b₁, hb₁_w, hb₁_h⟩ := hs d a hd₁ hd₂ hd₃ ha.1
-        obtain ⟨b₂, hb₂_w, hb₂_h⟩ := ht d a hd₁ hd₂ hd₃ ha.2
-        obtain ⟨c, hc_w, hc_h⟩ := hd₂ b₁ hb₁_w b₂ hb₂_w
-        refine ⟨c, hc_w, ?_⟩
-        . calc
-            Ici c ∩ d ⊆ (Ici b₁ ∩ Ici b₂) ∩ d := by
-            { apply inter_subset_inter_left d
-              apply subset_inter (Ici_subset_Ici.mpr hc_h.1) (Ici_subset_Ici.mpr hc_h.2) }
-            _ = (Ici b₁ ∩ d) ∩ (Ici b₂ ∩ d) := by rw [inter_inter_distrib_right]
-            _ ⊆ s ∩ t := inter_subset_inter hb₁_h hb₂_h
-      isOpen_unionₛ := by
-        intros s h d a hd₁ hd₂ hd₃ ha
-        obtain ⟨s₀, hs₀_w, hs₀_h⟩ := ha
-        obtain ⟨b, hb_w, hb_h⟩ := h s₀ hs₀_w d a hd₁ hd₂ hd₃ hs₀_h
-        use b
-        constructor
-        . exact hb_w
-        . exact Set.subset_unionₛ_of_subset s s₀ hb_h hs₀_w
-      }
+def ScottTopology' : TopologicalSpace α := upperSetTopology ⊔ ScottHausdorffTopology
 
 end preorder
 
