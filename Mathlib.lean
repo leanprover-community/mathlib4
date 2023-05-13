db47e7ed599c353ef8b6cab143297572f2350cba
Joël Riou <joel.riou@universite-paris-saclay.fr>
2023-05-13 13:53:57 +0000

    feat: port Topology.Sheaves.Abelian (#3949)

--- Mathlib.lean
+++ Mathlib.lean
@@ -2144,6 +2144,7 @@ import Mathlib.Topology.Sets.Closeds
 import Mathlib.Topology.Sets.Compacts
 import Mathlib.Topology.Sets.Opens
 import Mathlib.Topology.Sets.Order
+import Mathlib.Topology.Sheaves.Abelian
 import Mathlib.Topology.ShrinkingLemma
 import Mathlib.Topology.Sober
 import Mathlib.Topology.Spectral.Hom
