82298d98b4a04a04c7636ea14dba5c1f6dcaf533
Yuma Mizuno <mizuno.y.aj@gmail.com>
2023-05-15 14:17:59 +0900

    fix comments

--- Mathlib/GroupTheory/NielsenSchreier.lean
+++ Mathlib/GroupTheory/NielsenSchreier.lean
@@ -59,8 +59,8 @@ open Classical
 
 universe v u
 
-/- ./././Mathport/Syntax/Translate/Command.lean:229:11:
-unsupported: unusual advanced open style -/
+/- Porting note: ./././Mathport/Syntax/Translate/Command.lean:229:11:unsupported:
+unusual advanced open style -/
 open CategoryTheory CategoryTheory.ActionCategory CategoryTheory.SingleObj Quiver FreeGroup
 
 /-- `IsFreeGroupoid.Generators G` is a type synonym for `G`. We think of this as
@@ -194,8 +194,6 @@ def loopOfHom {a b : G} (p : a ⟶ b) : End (root' T) :=
   treeHom T a ≫ p ≫ inv (treeHom T b)
 #align is_free_groupoid.spanning_tree.loop_of_hom IsFreeGroupoid.SpanningTree.loopOfHom
 
-/- ./././Mathport/Syntax/Translate/Basic.lean:635:2: warning: expanding binder collection
-(e «expr ∈ » wide_subquiver_symmetrify[quiver.wide_subquiver_symmetrify] T a b) -/
 /-- Turning an edge in the spanning tree into a loop gives the indentity loop. -/
 theorem loopOfHom_eq_id {a b : Generators G} (e) (H : e ∈ wideSubquiverSymmetrify T a b) :
     loopOfHom T (of e) = 𝟙 (root' T) := by
