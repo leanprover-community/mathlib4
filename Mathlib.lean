c0f7bfdefbf97107687d4dc99db525fa09c04bba
Kyle Miller <kmill31415@gmail.com>
2023-05-16 00:56:21 +0000

    feat: porting norm_num extensions for `Nat.gcd`, `Nat.lcm`, `Int.gcd`, `Int.lcm`, and `IsCoprime` (#3923)
    This completes the porting of these norm_num extensions while adding a new extension for `IsCoprime` over `Int`. It also adds the norm_num extension testing file from mathlib3. Closes #3246.
    
    
    
    Co-authored-by: Mario Carneiro <di.gama@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1923,6 +1923,8 @@ import Mathlib.Tactic.NormCast.Tactic
 import Mathlib.Tactic.NormNum
 import Mathlib.Tactic.NormNum.Basic
 import Mathlib.Tactic.NormNum.Core
+import Mathlib.Tactic.NormNum.GCD
+import Mathlib.Tactic.NormNum.IsCoprime
 import Mathlib.Tactic.NthRewrite
 import Mathlib.Tactic.PermuteGoals
 import Mathlib.Tactic.Polyrith
