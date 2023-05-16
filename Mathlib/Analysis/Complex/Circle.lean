82d7ad02d7c54ffa3dedf992c2bb90daae6b231a
Parcly Taxel <reddeloostw@gmail.com>
2023-05-16 09:28:43 +0800

    Fix errors

--- Mathlib/Analysis/Complex/Circle.lean
+++ Mathlib/Analysis/Complex/Circle.lean
@@ -15,24 +15,24 @@ import Mathlib.Analysis.Normed.Field.UnitBall
 /-!
 # The circle
 
-This file defines `circle` to be the metric sphere (`metric.sphere`) in `ℂ` centred at `0` of
+This file defines `circle` to be the metric sphere (`Metric.sphere`) in `ℂ` centred at `0` of
 radius `1`.  We equip it with the following structure:
 
 * a submonoid of `ℂ`
 * a group
 * a topological group
 
-We furthermore define `exp_map_circle` to be the natural map `λ t, exp (t * I)` from `ℝ` to
+We furthermore define `expMapCircle` to be the natural map `λ t, exp (t * I)` from `ℝ` to
 `circle`, and show that this map is a group homomorphism.
 
 ## Implementation notes
 
-Because later (in `geometry.manifold.instances.sphere`) one wants to equip the circle with a smooth
-manifold structure borrowed from `metric.sphere`, the underlying set is
+Because later (in `Geometry.Manifold.Instances.Sphere`) one wants to equip the circle with a smooth
+manifold structure borrowed from `Metric.sphere`, the underlying set is
 `{z : ℂ | abs (z - 0) = 1}`.  This prevents certain algebraic facts from working definitionally --
-for example, the circle is not defeq to `{z : ℂ | abs z = 1}`, which is the kernel of `complex.abs`
-considered as a homomorphism from `ℂ` to `ℝ`, nor is it defeq to `{z : ℂ | norm_sq z = 1}`, which
-is the kernel of the homomorphism `complex.norm_sq` from `ℂ` to `ℝ`.
+for example, the circle is not defeq to `{z : ℂ | abs z = 1}`, which is the kernel of `Complex.abs`
+considered as a homomorphism from `ℂ` to `ℝ`, nor is it defeq to `{z : ℂ | normSq z = 1}`, which
+is the kernel of the homomorphism `Complex.normSq` from `ℂ` to `ℝ`.
 
 -/
 
@@ -54,7 +54,7 @@ theorem mem_circle_iff_abs {z : ℂ} : z ∈ circle ↔ abs z = 1 :=
 #align mem_circle_iff_abs mem_circle_iff_abs
 
 theorem circle_def : ↑circle = { z : ℂ | abs z = 1 } :=
-  Set.ext fun z => mem_circle_iff_abs
+  Set.ext fun _ => mem_circle_iff_abs
 #align circle_def circle_def
 
 @[simp]
@@ -66,14 +66,14 @@ theorem mem_circle_iff_normSq {z : ℂ} : z ∈ circle ↔ normSq z = 1 := by si
 #align mem_circle_iff_norm_sq mem_circle_iff_normSq
 
 @[simp]
-theorem normSq_eq_of_mem_circle (z : circle) : normSq z = 1 := by simp [norm_sq_eq_abs]
+theorem normSq_eq_of_mem_circle (z : circle) : normSq z = 1 := by simp [normSq_eq_abs]
 #align norm_sq_eq_of_mem_circle normSq_eq_of_mem_circle
 
 theorem ne_zero_of_mem_circle (z : circle) : (z : ℂ) ≠ 0 :=
   ne_zero_of_mem_unit_sphere z
 #align ne_zero_of_mem_circle ne_zero_of_mem_circle
 
-instance : CommGroup circle :=
+instance commGroup : CommGroup circle :=
   Metric.sphere.commGroup
 
 @[simp]
@@ -82,12 +82,12 @@ theorem coe_inv_circle (z : circle) : ↑z⁻¹ = (z : ℂ)⁻¹ :=
 #align coe_inv_circle coe_inv_circle
 
 theorem coe_inv_circle_eq_conj (z : circle) : ↑z⁻¹ = conj (z : ℂ) := by
-  rw [coe_inv_circle, inv_def, normSq_eq_of_mem_circle, inv_one, of_real_one, mul_one]
+  rw [coe_inv_circle, inv_def, normSq_eq_of_mem_circle, inv_one, ofReal_one, mul_one]
 #align coe_inv_circle_eq_conj coe_inv_circle_eq_conj
 
 @[simp]
 theorem coe_div_circle (z w : circle) : ↑(z / w) = (z : ℂ) / w :=
-  circle.Subtype.map_div z w
+  circle.subtype.map_div z w
 #align coe_div_circle coe_div_circle
 
 /-- The elements of the circle embed into the units. -/
@@ -98,7 +98,7 @@ def circle.toUnits : circle →* Units ℂ :=
 -- written manually because `@[simps]` was slow and generated the wrong lemma
 @[simp]
 theorem circle.toUnits_apply (z : circle) :
-    circle.toUnits z = Units.mk0 z (ne_zero_of_mem_circle z) :=
+    circle.toUnits z = Units.mk0 ↑z (ne_zero_of_mem_circle z) :=
   rfl
 #align circle.to_units_apply circle.toUnits_apply
 
@@ -112,10 +112,10 @@ instance : TopologicalGroup circle :=
 @[simps]
 def circle.ofConjDivSelf (z : ℂ) (hz : z ≠ 0) : circle :=
   ⟨conj z / z,
-    mem_circle_iff_abs.2 <| by rw [map_div₀, abs_conj, div_self (complex.abs.ne_zero hz)]⟩
+    mem_circle_iff_abs.2 <| by rw [map_div₀, abs_conj, div_self]; exact Complex.abs.ne_zero hz⟩
 #align circle.of_conj_div_self circle.ofConjDivSelf
 
-/-- The map `λ t, exp (t * I)` from `ℝ` to the unit circle in `ℂ`. -/
+/-- The map `fun t => exp (t * I)` from `ℝ` to the unit circle in `ℂ`. -/
 def expMapCircle : C(ℝ, circle)
     where toFun t := ⟨exp (t * I), by simp [exp_mul_I, abs_cos_add_sin_mul_I]⟩
 #align exp_map_circle expMapCircle
@@ -128,17 +128,17 @@ theorem expMapCircle_apply (t : ℝ) : ↑(expMapCircle t) = Complex.exp (t * Co
 @[simp]
 theorem expMapCircle_zero : expMapCircle 0 = 1 :=
   Subtype.ext <| by
-    rw [expMapCircle_apply, of_real_zero, MulZeroClass.zero_mul, exp_zero, Submonoid.coe_one]
+    rw [expMapCircle_apply, ofReal_zero, MulZeroClass.zero_mul, exp_zero, Submonoid.coe_one]
 #align exp_map_circle_zero expMapCircle_zero
 
 @[simp]
 theorem expMapCircle_add (x y : ℝ) : expMapCircle (x + y) = expMapCircle x * expMapCircle y :=
   Subtype.ext <| by
-    simp only [expMapCircle_apply, Submonoid.coe_mul, of_real_add, add_mul, Complex.exp_add]
+    simp only [expMapCircle_apply, Submonoid.coe_mul, ofReal_add, add_mul, Complex.exp_add]
 #align exp_map_circle_add expMapCircle_add
 
-/-- The map `λ t, exp (t * I)` from `ℝ` to the unit circle in `ℂ`, considered as a homomorphism of
-groups. -/
+/-- The map `fun t => exp (t * I)` from `ℝ` to the unit circle in `ℂ`,
+considered as a homomorphism of groups. -/
 @[simps]
 def expMapCircleHom : ℝ →+ Additive circle where
   toFun := Additive.ofMul ∘ expMapCircle
@@ -155,4 +155,3 @@ theorem expMapCircle_sub (x y : ℝ) : expMapCircle (x - y) = expMapCircle x / e
 theorem expMapCircle_neg (x : ℝ) : expMapCircle (-x) = (expMapCircle x)⁻¹ :=
   expMapCircleHom.map_neg x
 #align exp_map_circle_neg expMapCircle_neg
-
