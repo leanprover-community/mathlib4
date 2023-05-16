06c43c544e9b7753659346e7d3e48cd301efb024
Jeremy Tan Jie Rui <reddeloostw@gmail.com>
2023-05-16 15:19:48 +0000

    feat: port Topology.Category.Locale (#4011)

--- Mathlib.lean
+++ Mathlib.lean
@@ -2059,6 +2059,7 @@ import Mathlib.Topology.Bornology.Hom
 import Mathlib.Topology.Category.Born
 import Mathlib.Topology.Category.CompHaus.Basic
 import Mathlib.Topology.Category.CompHaus.Projective
+import Mathlib.Topology.Category.Locale
 import Mathlib.Topology.Category.Profinite.AsLimit
 import Mathlib.Topology.Category.Profinite.Basic
 import Mathlib.Topology.Category.Profinite.Projective
