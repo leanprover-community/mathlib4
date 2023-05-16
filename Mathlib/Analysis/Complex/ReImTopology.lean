9bd4a0bda39c340a93cfc16664756f5f28a3ee72
Parcly Taxel <reddeloostw@gmail.com>
2023-05-16 08:41:34 +0800

    Fix errors

--- Mathlib/Analysis/Complex/ReImTopology.lean
+++ Mathlib/Analysis/Complex/ReImTopology.lean
@@ -15,20 +15,20 @@ import Mathlib.Topology.FiberBundle.IsHomeomorphicTrivialBundle
 # Closure, interior, and frontier of preimages under `re` and `im`
 
 In this fact we use the fact that `ℂ` is naturally homeomorphic to `ℝ × ℝ` to deduce some
-topological properties of `complex.re` and `complex.im`.
+topological properties of `Complex.re` and `Complex.im`.
 
 ## Main statements
 
-Each statement about `complex.re` listed below has a counterpart about `complex.im`.
+Each statement about `Complex.re` listed below has a counterpart about `Complex.im`.
 
-* `complex.is_homeomorphic_trivial_fiber_bundle_re`: `complex.re` turns `ℂ` into a trivial
+* `Complex.isHomeomorphicTrivialFiberBundle_re`: `Complex.re` turns `ℂ` into a trivial
   topological fiber bundle over `ℝ`;
-* `complex.is_open_map_re`, `complex.quotient_map_re`: in particular, `complex.re` is an open map
+* `Complex.isOpenMap_re`, `Complex.quotientMap_re`: in particular, `Complex.re` is an open map
   and is a quotient map;
-* `complex.interior_preimage_re`, `complex.closure_preimage_re`, `complex.frontier_preimage_re`:
-  formulas for `interior (complex.re ⁻¹' s)` etc;
-* `complex.interior_set_of_re_le` etc: particular cases of the above formulas in the cases when `s`
-  is one of the infinite intervals `set.Ioi a`, `set.Ici a`, `set.Iio a`, and `set.Iic a`,
+* `Complex.interior_preimage_re`, `Complex.closure_preimage_re`, `Complex.frontier_preimage_re`:
+  formulas for `interior (Complex.re ⁻¹' s)` etc;
+* `Complex.interior_setOf_re_le` etc: particular cases of the above formulas in the cases when `s`
+  is one of the infinite intervals `Set.Ioi a`, `Set.Ici a`, `Set.Iio a`, and `Set.Iic a`,
   formulated as `interior {z : ℂ | z.re ≤ a} = {z | z.re < a}` etc.
 
 ## Tags
@@ -43,14 +43,14 @@ noncomputable section
 
 namespace Complex
 
-/-- `complex.re` turns `ℂ` into a trivial topological fiber bundle over `ℝ`. -/
+/-- `Complex.re` turns `ℂ` into a trivial topological fiber bundle over `ℝ`. -/
 theorem isHomeomorphicTrivialFiberBundle_re : IsHomeomorphicTrivialFiberBundle ℝ re :=
-  ⟨equivRealProdClm.toHomeomorph, fun z => rfl⟩
+  ⟨equivRealProdClm.toHomeomorph, fun _ => rfl⟩
 #align complex.is_homeomorphic_trivial_fiber_bundle_re Complex.isHomeomorphicTrivialFiberBundle_re
 
-/-- `complex.im` turns `ℂ` into a trivial topological fiber bundle over `ℝ`. -/
+/-- `Complex.im` turns `ℂ` into a trivial topological fiber bundle over `ℝ`. -/
 theorem isHomeomorphicTrivialFiberBundle_im : IsHomeomorphicTrivialFiberBundle ℝ im :=
-  ⟨equivRealProdClm.toHomeomorph.trans (Homeomorph.prodComm ℝ ℝ), fun z => rfl⟩
+  ⟨equivRealProdClm.toHomeomorph.trans (Homeomorph.prodComm ℝ ℝ), fun _ => rfl⟩
 #align complex.is_homeomorphic_trivial_fiber_bundle_im Complex.isHomeomorphicTrivialFiberBundle_im
 
 theorem isOpenMap_re : IsOpenMap re :=
@@ -174,29 +174,29 @@ theorem frontier_setOf_lt_im (a : ℝ) : frontier { z : ℂ | a < z.im } = { z |
 #align complex.frontier_set_of_lt_im Complex.frontier_setOf_lt_im
 
 theorem closure_reProdIm (s t : Set ℝ) : closure (s ×ℂ t) = closure s ×ℂ closure t := by
-  simpa only [← preimage_eq_preimage equiv_real_prod_clm.symm.to_homeomorph.surjective,
-    equiv_real_prod_clm.symm.to_homeomorph.preimage_closure] using @closure_prod_eq _ _ _ _ s t
+  simpa only [← preimage_eq_preimage equivRealProdClm.symm.toHomeomorph.surjective,
+    equivRealProdClm.symm.toHomeomorph.preimage_closure] using @closure_prod_eq _ _ _ _ s t
 #align complex.closure_re_prod_im Complex.closure_reProdIm
 
 theorem interior_reProdIm (s t : Set ℝ) : interior (s ×ℂ t) = interior s ×ℂ interior t := by
-  rw [re_prod_im, re_prod_im, interior_inter, interior_preimage_re, interior_preimage_im]
+  rw [Set.reProdIm, Set.reProdIm, interior_inter, interior_preimage_re, interior_preimage_im]
 #align complex.interior_re_prod_im Complex.interior_reProdIm
 
 theorem frontier_reProdIm (s t : Set ℝ) :
     frontier (s ×ℂ t) = closure s ×ℂ frontier t ∪ frontier s ×ℂ closure t := by
-  simpa only [← preimage_eq_preimage equiv_real_prod_clm.symm.to_homeomorph.surjective,
-    equiv_real_prod_clm.symm.to_homeomorph.preimage_frontier] using frontier_prod_eq s t
+  simpa only [← preimage_eq_preimage equivRealProdClm.symm.toHomeomorph.surjective,
+    equivRealProdClm.symm.toHomeomorph.preimage_frontier] using frontier_prod_eq s t
 #align complex.frontier_re_prod_im Complex.frontier_reProdIm
 
 theorem frontier_setOf_le_re_and_le_im (a b : ℝ) :
     frontier { z | a ≤ re z ∧ b ≤ im z } = { z | a ≤ re z ∧ im z = b ∨ re z = a ∧ b ≤ im z } := by
-  simpa only [closure_Ici, frontier_Ici] using frontier_re_prod_im (Ici a) (Ici b)
+  simpa only [closure_Ici, frontier_Ici] using frontier_reProdIm (Ici a) (Ici b)
 #align complex.frontier_set_of_le_re_and_le_im Complex.frontier_setOf_le_re_and_le_im
 
 theorem frontier_setOf_le_re_and_im_le (a b : ℝ) :
     frontier { z | a ≤ re z ∧ im z ≤ b } = { z | a ≤ re z ∧ im z = b ∨ re z = a ∧ im z ≤ b } := by
   simpa only [closure_Ici, closure_Iic, frontier_Ici, frontier_Iic] using
-    frontier_re_prod_im (Ici a) (Iic b)
+    frontier_reProdIm (Ici a) (Iic b)
 #align complex.frontier_set_of_le_re_and_im_le Complex.frontier_setOf_le_re_and_im_le
 
 end Complex
@@ -206,14 +206,13 @@ open Complex Metric
 variable {s t : Set ℝ}
 
 theorem IsOpen.reProdIm (hs : IsOpen s) (ht : IsOpen t) : IsOpen (s ×ℂ t) :=
-  (hs.Preimage continuous_re).inter (ht.Preimage continuous_im)
+  (hs.preimage continuous_re).inter (ht.preimage continuous_im)
 #align is_open.re_prod_im IsOpen.reProdIm
 
 theorem IsClosed.reProdIm (hs : IsClosed s) (ht : IsClosed t) : IsClosed (s ×ℂ t) :=
-  (hs.Preimage continuous_re).inter (ht.Preimage continuous_im)
+  (hs.preimage continuous_re).inter (ht.preimage continuous_im)
 #align is_closed.re_prod_im IsClosed.reProdIm
 
 theorem Metric.Bounded.reProdIm (hs : Bounded s) (ht : Bounded t) : Bounded (s ×ℂ t) :=
-  antilipschitz_equivRealProd.bounded_preimage (hs.Prod ht)
+  antilipschitz_equivRealProd.bounded_preimage (hs.prod ht)
 #align metric.bounded.re_prod_im Metric.Bounded.reProdIm
-
