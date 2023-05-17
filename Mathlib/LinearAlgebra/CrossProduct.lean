fc758524e0b6caaeae93c4c170d5b0ff3d0716e7
int-y1 <jason_yuen2007@hotmail.com>
2023-05-16 23:33:22 -0700

    remove eta

--- Mathlib/LinearAlgebra/CrossProduct.lean
+++ Mathlib/LinearAlgebra/CrossProduct.lean
@@ -47,7 +47,6 @@ open Matrix
 
 variable {R : Type _} [CommRing R]
 
-set_option synthInstance.etaExperiment true in
 /-- The cross product of two vectors in $R^3$ for $R$ a commutative ring. -/
 def crossProduct : (Fin 3 → R) →ₗ[R] (Fin 3 → R) →ₗ[R] Fin 3 → R := by
   apply LinearMap.mk₂ R fun a b : Fin 3 → R =>
@@ -64,17 +63,14 @@ def crossProduct : (Fin 3 → R) →ₗ[R] (Fin 3 → R) →ₗ[R] Fin 3 → R :
     simp_rw [smul_vec3, Pi.smul_apply, smul_sub, mul_smul_comm]
 #align cross_product crossProduct
 
--- Porting note: Every use of `×₃` requires etaExperiment
 scoped[Matrix] infixl:74 " ×₃ " => crossProduct
 
-set_option synthInstance.etaExperiment true in
 theorem cross_apply (a b : Fin 3 → R) :
     a ×₃ b = ![a 1 * b 2 - a 2 * b 1, a 2 * b 0 - a 0 * b 2, a 0 * b 1 - a 1 * b 0] := rfl
 #align cross_apply cross_apply
 
 section ProductsProperties
 
-set_option synthInstance.etaExperiment true in
 @[simp]
 theorem cross_anticomm (v w : Fin 3 → R) : -(v ×₃ w) = w ×₃ v := by
   simp [cross_apply, mul_comm]
@@ -83,13 +79,11 @@ theorem cross_anticomm (v w : Fin 3 → R) : -(v ×₃ w) = w ×₃ v := by
 alias cross_anticomm ← neg_cross
 #align neg_cross neg_cross
 
-set_option synthInstance.etaExperiment true in
 @[simp]
 theorem cross_anticomm' (v w : Fin 3 → R) : v ×₃ w + w ×₃ v = 0 := by
   rw [add_eq_zero_iff_eq_neg, cross_anticomm]
 #align cross_anticomm' cross_anticomm'
 
-set_option synthInstance.etaExperiment true in
 @[simp]
 theorem cross_self (v : Fin 3 → R) : v ×₃ v = 0 := by
   -- Porting note: Original proof was `simp [cross_apply, mul_comm]`
@@ -101,7 +95,6 @@ theorem cross_self (v : Fin 3 → R) : v ×₃ v = 0 := by
 @[simp]
 private theorem vecCons2 (a : R) (v : Fin 2 → R) : vecCons a v 2 = v 1 := rfl
 
-set_option synthInstance.etaExperiment true in
 /-- The cross product of two vectors is perpendicular to the first vector. -/
 @[simp]
 theorem dot_self_cross (v w : Fin 3 → R) : v ⬝ᵥ v ×₃ w = 0 := by
@@ -110,14 +103,12 @@ theorem dot_self_cross (v w : Fin 3 → R) : v ⬝ᵥ v ×₃ w = 0 := by
   ring
 #align dot_self_cross dot_self_cross
 
-set_option synthInstance.etaExperiment true in
 /-- The cross product of two vectors is perpendicular to the second vector. -/
 @[simp]
 theorem dot_cross_self (v w : Fin 3 → R) : w ⬝ᵥ v ×₃ w = 0 := by
   rw [← cross_anticomm, Matrix.dotProduct_neg, dot_self_cross, neg_zero]
 #align dot_cross_self dot_cross_self
 
-set_option synthInstance.etaExperiment true in
 /-- Cyclic permutations preserve the triple product. See also `triple_product_eq_det`. -/
 theorem triple_product_permutation (u v w : Fin 3 → R) : u ⬝ᵥ v ×₃ w = v ⬝ᵥ w ×₃ u := by
   simp_rw [cross_apply, vec3_dotProduct]
@@ -125,7 +116,6 @@ theorem triple_product_permutation (u v w : Fin 3 → R) : u ⬝ᵥ v ×₃ w = 
   ring
 #align triple_product_permutation triple_product_permutation
 
-set_option synthInstance.etaExperiment true in
 /-- The triple product of `u`, `v`, and `w` is equal to the determinant of the matrix
     with those vectors as its rows. -/
 theorem triple_product_eq_det (u v w : Fin 3 → R) : u ⬝ᵥ v ×₃ w = Matrix.det ![u, v, w] := by
@@ -134,7 +124,6 @@ theorem triple_product_eq_det (u v w : Fin 3 → R) : u ⬝ᵥ v ×₃ w = Matri
   ring
 #align triple_product_eq_det triple_product_eq_det
 
-set_option synthInstance.etaExperiment true in
 /-- The scalar quadruple product identity, related to the Binet-Cauchy identity. -/
 theorem cross_dot_cross (u v w x : Fin 3 → R) :
     u ×₃ v ⬝ᵥ w ×₃ x = u ⬝ᵥ w * v ⬝ᵥ x - u ⬝ᵥ x * v ⬝ᵥ w := by
@@ -147,14 +136,12 @@ end ProductsProperties
 
 section LeibnizProperties
 
-set_option synthInstance.etaExperiment true in
 /-- The cross product satisfies the Leibniz lie property. -/
 theorem leibniz_cross (u v w : Fin 3 → R) : u ×₃ (v ×₃ w) = u ×₃ v ×₃ w + v ×₃ (u ×₃ w) := by
   simp_rw [cross_apply, vec3_add]
   apply vec3_eq <;> norm_num <;> ring
 #align leibniz_cross leibniz_cross
 
-set_option synthInstance.etaExperiment true in
 /-- The three-dimensional vectors together with the operations + and ×₃ form a Lie ring.
     Note we do not make this an instance as a conflicting one already exists
     via `LieRing.ofAssociativeRing`. -/
@@ -169,12 +156,10 @@ def Cross.lieRing : LieRing (Fin 3 → R) :=
 
 attribute [local instance] Cross.lieRing
 
-set_option synthInstance.etaExperiment true in
 theorem cross_cross (u v w : Fin 3 → R) : u ×₃ v ×₃ w = u ×₃ (v ×₃ w) - v ×₃ (u ×₃ w) :=
   lie_lie u v w
 #align cross_cross cross_cross
 
-set_option synthInstance.etaExperiment true in
 /-- Jacobi identity: For a cross product of three vectors,
     their sum over the three even permutations is equal to the zero vector. -/
 theorem jacobi_cross (u v w : Fin 3 → R) : u ×₃ (v ×₃ w) + v ×₃ (w ×₃ u) + w ×₃ (u ×₃ v) = 0 :=
