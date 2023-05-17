4cb84b72c51aaee640b583aea80093c09ac447d6
Jason Yuen <jason_yuen2007@hotmail.com>
2023-05-17 06:12:38 +0000

    feat: port Algebra.Homology.Opposite (#3998)
    Co-authored-by: Moritz Firsching <firsching@google.com>
    Co-authored-by: Matthew Ballard <matt@mrb.email>

--- Mathlib.lean
+++ Mathlib.lean
@@ -156,6 +156,7 @@ import Mathlib.Algebra.Homology.Homology
 import Mathlib.Algebra.Homology.Homotopy
 import Mathlib.Algebra.Homology.HomotopyCategory
 import Mathlib.Algebra.Homology.ImageToKernel
+import Mathlib.Algebra.Homology.Opposite
 import Mathlib.Algebra.Homology.ShortExact.Abelian
 import Mathlib.Algebra.Homology.ShortExact.Preadditive
 import Mathlib.Algebra.Homology.Single
