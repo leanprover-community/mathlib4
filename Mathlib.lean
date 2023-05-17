2751e19181a46a25a1e801acc5eaeb2b3fab634a
Yakov Pechersky <ypechersky@treeline.bio>
2023-05-16 22:36:03 -0400

    Squashed commit of the following:
    commit 56a6daebfa10fab2f806dd0039644cbd7bcc0031
    Author: Yakov Pechersky <ypechersky@treeline.bio>
    Date:   Tue May 16 22:13:23 2023 -0400
    
        fixes, a new lemma, and a new helper def
    
    commit 8c9e9c9c38f8ae5f0dbc42a26bae8eaa92385d66
    Author: Yakov Pechersky <ypechersky@treeline.bio>
    Date:   Tue May 16 20:09:59 2023 -0400
    
        Squashed commit of the following:
    
        commit 8bd389f0237c8e6ee79629831e815d957f2623cb
        Author: Yury G. Kudryashov <urkud@urkud.name>
        Date:   Tue May 16 11:46:49 2023 -0500
    
            Fix lint
    
        commit 0d6be882d768a8e0312d4d47601308ecb3e54de3
        Author: Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
        Date:   Tue May 16 14:58:25 2023 +0200
    
            build
    
        commit b97ce754b2ff164cb07819cefa891262c92ea14a
        Author: Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
        Date:   Tue May 16 14:26:15 2023 +0200
    
            Build
    
        commit df57fa2bb8c390276f4547f6e25e0ce17012c6be
        Author: Jeremy Tan Jie Rui <reddeloostw@gmail.com>
        Date:   Tue May 16 13:15:02 2023 +0800
    
            Update Basic.lean
    
        commit 67a4fc91c525893ba847bd769afd3df272564151
        Author: Yakov Pechersky <ypechersky@treeline.bio>
        Date:   Mon May 15 21:42:15 2023 -0400
    
            automated fixes
    
            Mathbin -> Mathlib
            fix certain import statements
            move "by" to end of line
            add import to Mathlib.lean
    
        commit 5cbeaf74c732c7462a569f47cbede85f43b90b7c
        Author: Yakov Pechersky <ypechersky@treeline.bio>
        Date:   Mon May 15 21:42:15 2023 -0400
    
            Initial file copy from mathport
    
        commit 6ef95d40bef045270f9abd6c7fb2f596c6d3476a
        Author: Yakov Pechersky <ypechersky@treeline.bio>
        Date:   Mon May 15 21:42:15 2023 -0400
    
            feat: port Analysis.SpecialFunctions.Trigonometric.Basic
    
    commit c2563ee32d9b47227bf385707478371f3aafa462
    Author: Yakov Pechersky <ypechersky@treeline.bio>
    Date:   Tue May 16 20:08:53 2023 -0400
    
        automated fixes
    
        Mathbin -> Mathlib
        fix certain import statements
        move "by" to end of line
        add import to Mathlib.lean
    
    commit 3dc30c627319549028816ba8fe2db3acd24cfd52
    Author: Yakov Pechersky <ypechersky@treeline.bio>
    Date:   Tue May 16 20:08:53 2023 -0400
    
        Initial file copy from mathport
    
    commit 9b6d6679b98d9d1a36daa9cb3a6b6f0b2a485e5d
    Author: Yakov Pechersky <ypechersky@treeline.bio>
    Date:   Tue May 16 20:08:53 2023 -0400
    
        feat: port Analysis.SpecialFunctions.Trigonometric.Angle

--- Mathlib.lean
+++ Mathlib.lean
@@ -429,6 +429,7 @@ import Mathlib.Analysis.SpecialFunctions.Complex.Arg
 import Mathlib.Analysis.SpecialFunctions.Exp
 import Mathlib.Analysis.SpecialFunctions.Log.Basic
 import Mathlib.Analysis.SpecialFunctions.Polynomials
+import Mathlib.Analysis.SpecialFunctions.Trigonometric.Angle
 import Mathlib.Analysis.SpecialFunctions.Trigonometric.Basic
 import Mathlib.Analysis.SpecialFunctions.Trigonometric.Chebyshev
 import Mathlib.Analysis.SpecialFunctions.Trigonometric.Inverse
