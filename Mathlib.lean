ee5111f8b25f7a482bc26be92ceddbf7fba38d2e
Moritz Firsching <moritz.firsching@gmail.com>
2023-05-18 15:44:49 +0000

    feat: port Algebra.CharP.LocalRing (#4082)
    Co-authored-by: Moritz Firsching <firsching@google.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -47,6 +47,7 @@ import Mathlib.Algebra.CharP.Basic
 import Mathlib.Algebra.CharP.CharAndCard
 import Mathlib.Algebra.CharP.ExpChar
 import Mathlib.Algebra.CharP.Invertible
+import Mathlib.Algebra.CharP.LocalRing
 import Mathlib.Algebra.CharP.Pi
 import Mathlib.Algebra.CharP.Quotient
 import Mathlib.Algebra.CharP.Subring
