c0be162f677ea69cf2c73546cfe608cdd16a84dd
Yury G. Kudryashov <urkud@urkud.name>
2023-05-18 16:02:27 +0000

    feat: port Topology.Sheaves.Presheaf (#3828)
    Co-authored-by: Moritz Firsching <firsching@google.com>
    Co-authored-by: int-y1 <jason_yuen2007@hotmail.com>
    Co-authored-by: Jujian Zhang <jujian.zhang1998@outlook.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -2218,6 +2218,8 @@ import Mathlib.Topology.Sets.Compacts
 import Mathlib.Topology.Sets.Opens
 import Mathlib.Topology.Sets.Order
 import Mathlib.Topology.Sheaves.Abelian
+import Mathlib.Topology.Sheaves.Init
+import Mathlib.Topology.Sheaves.Presheaf
 import Mathlib.Topology.ShrinkingLemma
 import Mathlib.Topology.Sober
 import Mathlib.Topology.Spectral.Hom
