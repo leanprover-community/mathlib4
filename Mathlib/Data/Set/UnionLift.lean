d21e25d83e2588df6b41a5d68f7070ee1320d792
ChrisHughes24 <chrishughes24@gmail.com>
2023-05-18 09:59:12 +0100

    match mathlib3 style. Set is Type _ -> Type _ so α is Type _ anyway

--- Mathlib/Data/Set/UnionLift.lean
+++ Mathlib/Data/Set/UnionLift.lean
@@ -41,7 +41,7 @@ constants, unary functions, or binary functions are preserved. These lemmas are:
 directed union, directed supremum, glue, gluing
 -/
 
-variable {α ι β : Sort _}
+variable {α : Type _} {ι β : Sort _}
 
 namespace Set
 
