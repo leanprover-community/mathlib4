6420b0ad13c3f6965a54ac25edbe5befc90650a6
Parcly Taxel <reddeloostw@gmail.com>
2023-05-20 00:02:46 +0800

    Fix errors

--- Mathlib/Data/Nat/Factorial/DoubleFactorial.lean
+++ Mathlib/Data/Nat/Factorial/DoubleFactorial.lean
@@ -20,7 +20,7 @@ This file defines the double factorial,
 
 ## Main declarations
 
-* `nat.double_factorial`: The double factorial.
+* `Nat.doubleFactorial`: The double factorial.
 -/
 
 
@@ -28,15 +28,14 @@ open Nat
 
 namespace Nat
 
-/-- `nat.double_factorial n` is the double factorial of `n`. -/
+/-- `Nat.doubleFactorial n` is the double factorial of `n`. -/
 @[simp]
 def doubleFactorial : ℕ → ℕ
   | 0 => 1
   | 1 => 1
-  | k + 2 => (k + 2) * double_factorial k
+  | k + 2 => (k + 2) * doubleFactorial k
 #align nat.double_factorial Nat.doubleFactorial
 
--- mathport name: nat.double_factorial
 -- This notation is `\!!` not two !'s
 scoped notation:10000 n "‼" => Nat.doubleFactorial n
 
@@ -50,14 +49,14 @@ theorem doubleFactorial_add_one (n : ℕ) : (n + 1)‼ = (n + 1) * (n - 1)‼ :=
 theorem factorial_eq_mul_doubleFactorial : ∀ n : ℕ, (n + 1)! = (n + 1)‼ * n‼
   | 0 => rfl
   | k + 1 => by
-    rw [double_factorial_add_two, factorial, factorial_eq_mul_double_factorial, mul_comm _ k‼,
+    rw [doubleFactorial_add_two, factorial, factorial_eq_mul_doubleFactorial _, mul_comm _ k‼,
       mul_assoc]
 #align nat.factorial_eq_mul_double_factorial Nat.factorial_eq_mul_doubleFactorial
 
 theorem doubleFactorial_two_mul : ∀ n : ℕ, (2 * n)‼ = 2 ^ n * n !
   | 0 => rfl
   | n + 1 => by
-    rw [mul_add, mul_one, double_factorial_add_two, factorial, pow_succ, double_factorial_two_mul,
+    rw [mul_add, mul_one, doubleFactorial_add_two, factorial, pow_succ, doubleFactorial_two_mul _,
       succ_eq_add_one]
     ring
 #align nat.double_factorial_two_mul Nat.doubleFactorial_two_mul
@@ -67,18 +66,18 @@ open BigOperators
 theorem doubleFactorial_eq_prod_even : ∀ n : ℕ, (2 * n)‼ = ∏ i in Finset.range n, 2 * (i + 1)
   | 0 => rfl
   | n + 1 => by
-    rw [Finset.prod_range_succ, ← double_factorial_eq_prod_even, mul_comm (2 * n)‼,
+    rw [Finset.prod_range_succ, ← doubleFactorial_eq_prod_even _, mul_comm (2 * n)‼,
       (by ring : 2 * (n + 1) = 2 * n + 2)]
     rfl
 #align nat.double_factorial_eq_prod_even Nat.doubleFactorial_eq_prod_even
 
-theorem doubleFactorial_eq_prod_odd : ∀ n : ℕ, (2 * n + 1)‼ = ∏ i in Finset.range n, 2 * (i + 1) + 1
+theorem doubleFactorial_eq_prod_odd :
+    ∀ n : ℕ, (2 * n + 1)‼ = ∏ i in Finset.range n, (2 * (i + 1) + 1)
   | 0 => rfl
   | n + 1 => by
-    rw [Finset.prod_range_succ, ← double_factorial_eq_prod_odd, mul_comm (2 * n + 1)‼,
+    rw [Finset.prod_range_succ, ← doubleFactorial_eq_prod_odd _, mul_comm (2 * n + 1)‼,
       (by ring : 2 * (n + 1) + 1 = 2 * n + 1 + 2)]
     rfl
 #align nat.double_factorial_eq_prod_odd Nat.doubleFactorial_eq_prod_odd
 
 end Nat
-
