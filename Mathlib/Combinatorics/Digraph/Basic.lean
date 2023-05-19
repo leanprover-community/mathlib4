ae4221011579c7058c6b134ff8a1ca4047eeaddb
Kyle Miller <kmill31415@gmail.com>
2023-05-19 23:06:18 +0000

    move

--- Mathlib/Combinatorics/Digraph/Basic.lean
+++ Mathlib/Combinatorics/Digraph/Basic.lean
@@ -3,7 +3,7 @@ Copyright (c) 2023 Kyle Miller. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kyle Miller
 -/
-import Mathlib.Combinatorics.GraphDefs
+import Mathlib.Combinatorics.Graph.Classes
 import Mathlib.Data.Rel
 import Mathlib.Data.Set.Finite
 import Mathlib.Tactic.ProjectionNotation
