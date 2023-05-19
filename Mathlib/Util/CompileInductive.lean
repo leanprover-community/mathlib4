afc3b05205709578b5b1e8a5e423102b28367aac
Mario Carneiro <di.gama@gmail.com>
2023-05-19 05:50:09 -0400

    add authors

--- Mathlib/Util/CompileInductive.lean
+++ Mathlib/Util/CompileInductive.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2023 Parth Shastri. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Parth Shastri
+Authors: Parth Shastri, Gabriel Ebner, Mario Carneiro
 -/
 import Lean.Compiler.CSimpAttr
 import Lean.Elab.PreDefinition
