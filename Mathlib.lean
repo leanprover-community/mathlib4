b58077e6cb289534afe5ed4727bd224a6511daed
Apurva Nakade <apurvnakade@gmail.com>
2023-05-20 10:55:32 +0000

    feat: port Algebra.Category.ModuleCat.Projective (#3964)
    This is stuck at a universe issue. Lean is having trouble merging `Type (max (max v u) u v)` with `Type (max u u v)`
    
    See comment below. 
    
    
    
    Co-authored-by: Matthew Ballard <matt@mrb.email>

--- Mathlib.lean
+++ Mathlib.lean
@@ -39,6 +39,7 @@ import Mathlib.Algebra.Category.GroupCat.Zero
 import Mathlib.Algebra.Category.ModuleCat.Basic
 import Mathlib.Algebra.Category.ModuleCat.EpiMono
 import Mathlib.Algebra.Category.ModuleCat.Products
+import Mathlib.Algebra.Category.ModuleCat.Projective
 import Mathlib.Algebra.Category.ModuleCat.Tannaka
 import Mathlib.Algebra.Category.MonCat.Basic
 import Mathlib.Algebra.Category.MonCat.Limits
