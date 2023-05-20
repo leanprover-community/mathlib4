6de3b498beb4eb0c6a244f034e7bcbf276dc8821
Jeremy Tan Jie Rui <reddeloostw@gmail.com>
2023-05-20 10:23:45 +0000

    feat: port Topology.Algebra.Module.Star (#4135)

--- Mathlib.lean
+++ Mathlib.lean
@@ -2079,6 +2079,7 @@ import Mathlib.Topology.Algebra.Module.FiniteDimension
 import Mathlib.Topology.Algebra.Module.LinearPMap
 import Mathlib.Topology.Algebra.Module.Multilinear
 import Mathlib.Topology.Algebra.Module.Simple
+import Mathlib.Topology.Algebra.Module.Star
 import Mathlib.Topology.Algebra.Module.StrongTopology
 import Mathlib.Topology.Algebra.Module.WeakDual
 import Mathlib.Topology.Algebra.Monoid
