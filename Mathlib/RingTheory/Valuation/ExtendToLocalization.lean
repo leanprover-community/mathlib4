b7768c38fca21d67dbef26cdf0e44bbbd7029cd2
int-y1 <jason_yuen2007@hotmail.com>
2023-05-19 20:36:49 -0700

    fix

--- Mathlib/RingTheory/Valuation/ExtendToLocalization.lean
+++ Mathlib/RingTheory/Valuation/ExtendToLocalization.lean
@@ -16,7 +16,7 @@ import Mathlib.RingTheory.Valuation.Basic
 # Extending valuations to a localization
 
 We show that, given a valuation `v` taking values in a linearly ordered commutative *group*
-with zero `Γ`, and a submonoid `S` of `v.supp.prime_compl`, the valuation `v` can be naturally
+with zero `Γ`, and a submonoid `S` of `v.supp.primeCompl`, the valuation `v` can be naturally
 extended to the localization `S⁻¹A`.
 
 -/
@@ -32,7 +32,7 @@ noncomputable def Valuation.extendToLocalization : Valuation B Γ :=
   let f := IsLocalization.toLocalizationMap S B
   let h : ∀ s : S, IsUnit (v.1.toMonoidHom s) := fun s => isUnit_iff_ne_zero.2 (hS s.2)
   { f.lift h with
-    map_zero' := by convert f.lift_eq _ 0 <;> simp
+    map_zero' := by convert f.lift_eq (P := Γ) _ 0 <;> simp
     map_add_le_max' := fun x y => by
       obtain ⟨a, b, s, rfl, rfl⟩ : ∃ (a b : A)(s : S), f.mk' a s = x ∧ f.mk' b s = y := by
         obtain ⟨a, s, rfl⟩ := f.mk'_surjective x
@@ -43,7 +43,7 @@ noncomputable def Valuation.extendToLocalization : Valuation B Γ :=
             ring_nf
       convert_to f.lift h (f.mk' (a + b) s) ≤ max (f.lift h _) (f.lift h _)
       · refine' congr_arg (f.lift h) (IsLocalization.eq_mk'_iff_mul_eq.2 _)
-        rw [add_mul, map_add]
+        rw [add_mul, _root_.map_add]
         iterate 2 erw [IsLocalization.mk'_spec]
       iterate 3 rw [f.lift_mk']
       rw [max_mul_mul_right]
@@ -55,4 +55,3 @@ theorem Valuation.extendToLocalization_apply_map_apply (a : A) :
     v.extendToLocalization hS B (algebraMap A B a) = v a :=
   Submonoid.LocalizationMap.lift_eq _ _ a
 #align valuation.extend_to_localization_apply_map_apply Valuation.extendToLocalization_apply_map_apply
-
