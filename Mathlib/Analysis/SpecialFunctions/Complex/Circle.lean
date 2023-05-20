66282bddf72f58515b69aefe3bc4ececccffddc0
Moritz Doll <moritz.doll@googlemail.com>
2023-05-20 17:16:20 -0600

    easy

--- Mathlib/Analysis/SpecialFunctions/Complex/Circle.lean
+++ Mathlib/Analysis/SpecialFunctions/Complex/Circle.lean
@@ -14,9 +14,9 @@ import Mathlib.Analysis.SpecialFunctions.Complex.Log
 /-!
 # Maps on the unit circle
 
-In this file we prove some basic lemmas about `exp_map_circle` and the restriction of `complex.arg`
+In this file we prove some basic lemmas about `exp_map_circle` and the restriction of `Complex.arg`
 to the unit circle. These two maps define a local equivalence between `circle` and `ℝ`, see
-`circle.arg_local_equiv` and `circle.arg_equiv`, that sends the whole circle to `(-π, π]`.
+`circle.argLocalEquiv` and `circle.argEquiv`, that sends the whole circle to `(-π, π]`.
 -/
 
 
@@ -48,49 +48,49 @@ theorem expMapCircle_arg (z : circle) : expMapCircle (arg z) = z :=
 
 namespace circle
 
-/-- `complex.arg ∘ coe` and `exp_map_circle` define a local equivalence between `circle and `ℝ` with
-`source = set.univ` and `target = set.Ioc (-π) π`. -/
+/-- `complex.arg ∘ (↑)` and `exp_map_circle` define a local equivalence between `circle` and `ℝ`
+with `source = set.univ` and `target = set.Ioc (-π) π`. -/
 @[simps (config := { fullyApplied := false })]
 noncomputable def argLocalEquiv : LocalEquiv circle ℝ where
-  toFun := arg ∘ coe
+  toFun := arg ∘ (↑)
   invFun := expMapCircle
   source := univ
   target := Ioc (-π) π
-  map_source' z _ := ⟨neg_pi_lt_arg _, arg_le_pi _⟩
+  map_source' _ _ := ⟨neg_pi_lt_arg _, arg_le_pi _⟩
   map_target' := mapsTo_univ _ _
   left_inv' z _ := expMapCircle_arg z
-  right_inv' x hx := arg_expMapCircle hx.1 hx.2
+  right_inv' _ hx := arg_expMapCircle hx.1 hx.2
 #align circle.arg_local_equiv circle.argLocalEquiv
 
-/-- `complex.arg` and `exp_map_circle` define an equivalence between `circle and `(-π, π]`. -/
+/-- `complex.arg` and `exp_map_circle` define an equivalence between `circle` and `(-π, π]`. -/
 @[simps (config := { fullyApplied := false })]
 noncomputable def argEquiv : circle ≃ Ioc (-π) π where
   toFun z := ⟨arg z, neg_pi_lt_arg _, arg_le_pi _⟩
-  invFun := expMapCircle ∘ coe
-  left_inv z := argLocalEquiv.left_inv trivial
+  invFun := expMapCircle ∘ (↑)
+  left_inv _ := argLocalEquiv.left_inv trivial
   right_inv x := Subtype.ext <| argLocalEquiv.right_inv x.2
 #align circle.arg_equiv circle.argEquiv
 
 end circle
 
-theorem leftInverse_expMapCircle_arg : LeftInverse expMapCircle (arg ∘ coe) :=
+theorem leftInverse_expMapCircle_arg : LeftInverse expMapCircle (arg ∘ (↑)) :=
   expMapCircle_arg
 #align left_inverse_exp_map_circle_arg leftInverse_expMapCircle_arg
 
-theorem invOn_arg_expMapCircle : InvOn (arg ∘ coe) expMapCircle (Ioc (-π) π) univ :=
-  circle.argLocalEquiv.symm.InvOn
+theorem invOn_arg_expMapCircle : InvOn (arg ∘ (↑)) expMapCircle (Ioc (-π) π) univ :=
+  circle.argLocalEquiv.symm.invOn
 #align inv_on_arg_exp_map_circle invOn_arg_expMapCircle
 
 theorem surjOn_expMapCircle_neg_pi_pi : SurjOn expMapCircle (Ioc (-π) π) univ :=
-  circle.argLocalEquiv.symm.SurjOn
+  circle.argLocalEquiv.symm.surjOn
 #align surj_on_exp_map_circle_neg_pi_pi surjOn_expMapCircle_neg_pi_pi
 
 theorem expMapCircle_eq_expMapCircle {x y : ℝ} :
     expMapCircle x = expMapCircle y ↔ ∃ m : ℤ, x = y + m * (2 * π) := by
   rw [Subtype.ext_iff, expMapCircle_apply, expMapCircle_apply, exp_eq_exp_iff_exists_int]
   refine' exists_congr fun n => _
-  rw [← mul_assoc, ← add_mul, mul_left_inj' I_ne_zero, ← of_real_one, ← of_real_bit0, ← of_real_mul,
-    ← of_real_int_cast, ← of_real_mul, ← of_real_add, of_real_inj]
+  rw [← mul_assoc, ← add_mul, mul_left_inj' I_ne_zero]
+  norm_cast
 #align exp_map_circle_eq_exp_map_circle expMapCircle_eq_expMapCircle
 
 theorem periodic_expMapCircle : Periodic expMapCircle (2 * π) := fun z =>
@@ -99,7 +99,7 @@ theorem periodic_expMapCircle : Periodic expMapCircle (2 * π) := fun z =>
 
 @[simp]
 theorem expMapCircle_two_pi : expMapCircle (2 * π) = 1 :=
-  periodic_expMapCircle.Eq.trans expMapCircle_zero
+  periodic_expMapCircle.eq.trans expMapCircle_zero
 #align exp_map_circle_two_pi expMapCircle_two_pi
 
 theorem expMapCircle_sub_two_pi (x : ℝ) : expMapCircle (x - 2 * π) = expMapCircle x :=
@@ -110,13 +110,13 @@ theorem expMapCircle_add_two_pi (x : ℝ) : expMapCircle (x + 2 * π) = expMapCi
   periodic_expMapCircle x
 #align exp_map_circle_add_two_pi expMapCircle_add_two_pi
 
-/-- `exp_map_circle`, applied to a `real.angle`. -/
+/-- `expMapCircle`, applied to a `Real.Angle`. -/
 noncomputable def Real.Angle.expMapCircle (θ : Real.Angle) : circle :=
   periodic_expMapCircle.lift θ
 #align real.angle.exp_map_circle Real.Angle.expMapCircle
 
 @[simp]
-theorem Real.Angle.expMapCircle_coe (x : ℝ) : Real.Angle.expMapCircle x = expMapCircle x :=
+theorem Real.Angle.expMapCircle_coe (x : ℝ) : Real.Angle.expMapCircle x = _root_.expMapCircle x :=
   rfl
 #align real.angle.exp_map_circle_coe Real.Angle.expMapCircle_coe
 
@@ -128,14 +128,14 @@ theorem Real.Angle.coe_expMapCircle (θ : Real.Angle) : (θ.expMapCircle : ℂ) 
 
 @[simp]
 theorem Real.Angle.expMapCircle_zero : Real.Angle.expMapCircle 0 = 1 := by
-  rw [← Real.Angle.coe_zero, Real.Angle.expMapCircle_coe, expMapCircle_zero]
+  rw [← Real.Angle.coe_zero, Real.Angle.expMapCircle_coe, _root_.expMapCircle_zero]
 #align real.angle.exp_map_circle_zero Real.Angle.expMapCircle_zero
 
 @[simp]
 theorem Real.Angle.expMapCircle_neg (θ : Real.Angle) :
     Real.Angle.expMapCircle (-θ) = (Real.Angle.expMapCircle θ)⁻¹ := by
   induction θ using Real.Angle.induction_on
-  simp_rw [← Real.Angle.coe_neg, Real.Angle.expMapCircle_coe, expMapCircle_neg]
+  simp_rw [← Real.Angle.coe_neg, Real.Angle.expMapCircle_coe, _root_.expMapCircle_neg]
 #align real.angle.exp_map_circle_neg Real.Angle.expMapCircle_neg
 
 @[simp]
@@ -144,14 +144,13 @@ theorem Real.Angle.expMapCircle_add (θ₁ θ₂ : Real.Angle) :
   by
   induction θ₁ using Real.Angle.induction_on
   induction θ₂ using Real.Angle.induction_on
-  exact expMapCircle_add θ₁ θ₂
+  exact _root_.expMapCircle_add _ _
 #align real.angle.exp_map_circle_add Real.Angle.expMapCircle_add
 
 @[simp]
 theorem Real.Angle.arg_expMapCircle (θ : Real.Angle) :
     (arg (Real.Angle.expMapCircle θ) : Real.Angle) = θ := by
   induction θ using Real.Angle.induction_on
-  rw [Real.Angle.expMapCircle_coe, expMapCircle_apply, exp_mul_I, ← of_real_cos, ← of_real_sin, ←
+  rw [Real.Angle.expMapCircle_coe, expMapCircle_apply, exp_mul_I, ← ofReal_cos, ← ofReal_sin, ←
     Real.Angle.cos_coe, ← Real.Angle.sin_coe, arg_cos_add_sin_mul_I_coe_angle]
 #align real.angle.arg_exp_map_circle Real.Angle.arg_expMapCircle
-
