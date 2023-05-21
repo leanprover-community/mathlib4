6d77e3b424faddf989faaf53e0a58f328f7d13ff
Joël Riou <joel.riou@universite-paris-saclay.fr>
2023-05-21 15:13:25 +0000

    feat: port AlgebraicGeometry.PresheafedSpace (#4093)

--- Mathlib.lean
+++ Mathlib.lean
@@ -331,6 +331,7 @@ import Mathlib.Algebra.TrivSqZeroExt
 import Mathlib.Algebra.Tropical.Basic
 import Mathlib.Algebra.Tropical.BigOperators
 import Mathlib.Algebra.Tropical.Lattice
+import Mathlib.AlgebraicGeometry.PresheafedSpace
 import Mathlib.AlgebraicTopology.AlternatingFaceMapComplex
 import Mathlib.AlgebraicTopology.CechNerve
 import Mathlib.AlgebraicTopology.DoldKan.Compatibility
