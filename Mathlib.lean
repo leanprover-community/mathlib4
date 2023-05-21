3c517ae27741ae4e85a272d0a8f7e7b39f5b7727
damiano <adomani@gmail.com>
2023-05-21 08:56:04 +0000

    feat: port RingTheory.PolynomialAlgebra (#4106)
    This is my first attempt at porting a file.  I fixed a few of the errors, but there are still two issues left.
    
    
    
    Co-authored-by: ChrisHughes24 <chrishughes24@gmail.com>
    Co-authored-by: Kevin Buzzard <k.buzzard@imperial.ac.uk>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1898,6 +1898,7 @@ import Mathlib.RingTheory.Polynomial.Quotient
 import Mathlib.RingTheory.Polynomial.ScaleRoots
 import Mathlib.RingTheory.Polynomial.Tower
 import Mathlib.RingTheory.Polynomial.Vieta
+import Mathlib.RingTheory.PolynomialAlgebra
 import Mathlib.RingTheory.Prime
 import Mathlib.RingTheory.PrincipalIdealDomain
 import Mathlib.RingTheory.QuotientNilpotent
