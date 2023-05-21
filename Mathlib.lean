39893cc2c7c4fdd84b78a723959c9723c7d448f0
Pol_tta <pol_tta@outlook.jp>
2023-05-21 16:04:44 +0000

    feat: port MeasureTheory.Function.SimpleFunc (#4043)
    Co-authored-by: Parcly Taxel <reddeloostw@gmail.com>
    Co-authored-by: Chris Hughes <chrishughes24@gmail.com>
    Co-authored-by: Yury G. Kudryashov <urkud@urkud.name>
    Co-authored-by: ChrisHughes24 <chrishughes24@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1671,6 +1671,7 @@ import Mathlib.MeasureTheory.Decomposition.UnsignedHahn
 import Mathlib.MeasureTheory.Function.AEMeasurableOrder
 import Mathlib.MeasureTheory.Function.AEMeasurableSequence
 import Mathlib.MeasureTheory.Function.EssSup
+import Mathlib.MeasureTheory.Function.SimpleFunc
 import Mathlib.MeasureTheory.Group.Arithmetic
 import Mathlib.MeasureTheory.Group.MeasurableEquiv
 import Mathlib.MeasureTheory.Group.Pointwise
