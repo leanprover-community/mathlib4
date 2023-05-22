eec62fe5908745e2c2b9d2c1bcfa77621bd053fe
Xavier Roblot <46200072+xroblot@users.noreply.github.com>
2023-05-22 03:13:16 +0000

    feat: port RingTheory.Localization.LocalizationLocalization (#4137)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1908,6 +1908,7 @@ import Mathlib.RingTheory.Localization.FractionRing
 import Mathlib.RingTheory.Localization.Ideal
 import Mathlib.RingTheory.Localization.Integer
 import Mathlib.RingTheory.Localization.InvSubmonoid
+import Mathlib.RingTheory.Localization.LocalizationLocalization
 import Mathlib.RingTheory.Localization.Module
 import Mathlib.RingTheory.Localization.NumDen
 import Mathlib.RingTheory.Localization.Submodule
