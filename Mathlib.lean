d4dbf92899cbcdbc8861b33a09fe9c4892559115
Jeremy Tan Jie Rui <reddeloostw@gmail.com>
2023-05-22 19:15:02 +0000

    feat: port Dynamics.Ergodic.Conservative (#4230)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1401,6 +1401,7 @@ import Mathlib.Deprecated.Subgroup
 import Mathlib.Deprecated.Submonoid
 import Mathlib.Deprecated.Subring
 import Mathlib.Dynamics.Circle.RotationNumber.TranslationNumber
+import Mathlib.Dynamics.Ergodic.Conservative
 import Mathlib.Dynamics.Ergodic.Ergodic
 import Mathlib.Dynamics.Ergodic.MeasurePreserving
 import Mathlib.Dynamics.FixedPoints.Basic
