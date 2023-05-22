5afb4f38dea6037f78a240905649136f4a706a3f
Jeremy Tan Jie Rui <reddeloostw@gmail.com>
2023-05-22 22:00:44 +0000

    feat: port Algebra.AlgebraicCard (#4236)

--- Mathlib.lean
+++ Mathlib.lean
@@ -13,6 +13,7 @@ import Mathlib.Algebra.Algebra.Subalgebra.Pointwise
 import Mathlib.Algebra.Algebra.Subalgebra.Tower
 import Mathlib.Algebra.Algebra.Tower
 import Mathlib.Algebra.Algebra.Unitization
+import Mathlib.Algebra.AlgebraicCard
 import Mathlib.Algebra.Associated
 import Mathlib.Algebra.BigOperators.Associated
 import Mathlib.Algebra.BigOperators.Basic
