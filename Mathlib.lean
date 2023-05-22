76a0306659b4756541b50aafe890e3aa55da09fe
Pim Otte <potte@quintor.nl>
2023-05-22 19:14:58 +0000

    feat: port Algebra.Category.GroupWithZero (#3962)

--- Mathlib.lean
+++ Mathlib.lean
@@ -37,6 +37,7 @@ import Mathlib.Algebra.Category.GroupCat.Limits
 import Mathlib.Algebra.Category.GroupCat.Preadditive
 import Mathlib.Algebra.Category.GroupCat.ZModuleEquivalence
 import Mathlib.Algebra.Category.GroupCat.Zero
+import Mathlib.Algebra.Category.GroupWithZeroCat
 import Mathlib.Algebra.Category.ModuleCat.Basic
 import Mathlib.Algebra.Category.ModuleCat.EpiMono
 import Mathlib.Algebra.Category.ModuleCat.Kernels
