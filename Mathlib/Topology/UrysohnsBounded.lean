ddc210c348de4a786963a69b0287dcae4e2b2264
Parcly Taxel <reddeloostw@gmail.com>
2023-05-22 11:54:35 +0800

    Fix errors

--- Mathlib/Topology/UrysohnsBounded.lean
+++ Mathlib/Topology/UrysohnsBounded.lean
@@ -16,7 +16,7 @@ import Mathlib.Topology.ContinuousFunction.Bounded
 
 In this file we reformulate Urysohn's lemma `exists_continuous_zero_one_of_closed` in terms of
 bounded continuous functions `X →ᵇ ℝ`. These lemmas live in a separate file because
-`topology.continuous_function.bounded` imports too many other files.
+`Topology.ContinuousFunction.Bounded` imports too many other files.
 
 ## Tags
 
@@ -28,7 +28,7 @@ open BoundedContinuousFunction
 
 open Set Function
 
-/-- Urysohns lemma: if `s` and `t` are two disjoint closed sets in a normal topological space `X`,
+/-- Urysohn's lemma: if `s` and `t` are two disjoint closed sets in a normal topological space `X`,
 then there exists a continuous function `f : X → ℝ` such that
 
 * `f` equals zero on `s`;
@@ -39,10 +39,10 @@ theorem exists_bounded_zero_one_of_closed {X : Type _} [TopologicalSpace X] [Nor
     {s t : Set X} (hs : IsClosed s) (ht : IsClosed t) (hd : Disjoint s t) :
     ∃ f : X →ᵇ ℝ, EqOn f 0 s ∧ EqOn f 1 t ∧ ∀ x, f x ∈ Icc (0 : ℝ) 1 :=
   let ⟨f, hfs, hft, hf⟩ := exists_continuous_zero_one_of_closed hs ht hd
-  ⟨⟨f, 1, fun x y => Real.dist_le_of_mem_Icc_01 (hf _) (hf _)⟩, hfs, hft, hf⟩
+  ⟨⟨f, 1, fun _ _ => Real.dist_le_of_mem_Icc_01 (hf _) (hf _)⟩, hfs, hft, hf⟩
 #align exists_bounded_zero_one_of_closed exists_bounded_zero_one_of_closed
 
-/-- Urysohns lemma: if `s` and `t` are two disjoint closed sets in a normal topological space `X`,
+/-- Urysohn's lemma: if `s` and `t` are two disjoint closed sets in a normal topological space `X`,
 and `a ≤ b` are two real numbers, then there exists a continuous function `f : X → ℝ` such that
 
 * `f` equals `a` on `s`;
@@ -51,10 +51,10 @@ and `a ≤ b` are two real numbers, then there exists a continuous function `f :
 -/
 theorem exists_bounded_mem_Icc_of_closed_of_le {X : Type _} [TopologicalSpace X] [NormalSpace X]
     {s t : Set X} (hs : IsClosed s) (ht : IsClosed t) (hd : Disjoint s t) {a b : ℝ} (hle : a ≤ b) :
-    ∃ f : X →ᵇ ℝ, EqOn f (const X a) s ∧ EqOn f (const X b) t ∧ ∀ x, f x ∈ Icc a b :=
+    ∃ f : X →ᵇ ℝ, EqOn f (Function.const X a) s ∧ EqOn f (Function.const X b) t ∧
+    ∀ x, f x ∈ Icc a b :=
   let ⟨f, hfs, hft, hf01⟩ := exists_bounded_zero_one_of_closed hs ht hd
   ⟨BoundedContinuousFunction.const X a + (b - a) • f, fun x hx => by simp [hfs hx], fun x hx => by
     simp [hft hx], fun x =>
-    ⟨by dsimp <;> nlinarith [(hf01 x).1], by dsimp <;> nlinarith [(hf01 x).2]⟩⟩
+    ⟨by dsimp; nlinarith [(hf01 x).1], by dsimp; nlinarith [(hf01 x).2]⟩⟩
 #align exists_bounded_mem_Icc_of_closed_of_le exists_bounded_mem_Icc_of_closed_of_le
-
