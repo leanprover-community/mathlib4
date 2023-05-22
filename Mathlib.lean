314e4d60e4530dc464b5bdb62f081195666d760f
Yury G. Kudryashov <urkud@urkud.name>
2023-05-22 18:33:49 +0000

    feat: port Topology.VectorBundle.Constructions (#4207)

--- Mathlib.lean
+++ Mathlib.lean
@@ -2360,6 +2360,7 @@ import Mathlib.Topology.UnitInterval
 import Mathlib.Topology.UrysohnsBounded
 import Mathlib.Topology.UrysohnsLemma
 import Mathlib.Topology.VectorBundle.Basic
+import Mathlib.Topology.VectorBundle.Constructions
 import Mathlib.Util.AddRelatedDecl
 import Mathlib.Util.AssertNoSorry
 import Mathlib.Util.AtomM
