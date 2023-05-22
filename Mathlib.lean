d911e507fd549fc4c3f2532882e618e9f22cfc7b
Yury G. Kudryashov <urkud@urkud.name>
2023-05-22 19:15:01 +0000

    feat: port Analysis.Calculus.Fderiv.Equiv (#4210)
    Co-authored-by: Johan Commelin <johan@commelin.net>
    Co-authored-by: Chris Hughes <33847686+ChrisHughes24@users.noreply.github.com>
    Co-authored-by: Chris Hughes <chrishughes24@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -383,6 +383,7 @@ import Mathlib.Analysis.BoxIntegral.Partition.Tagged
 import Mathlib.Analysis.Calculus.FDeriv.Add
 import Mathlib.Analysis.Calculus.FDeriv.Basic
 import Mathlib.Analysis.Calculus.FDeriv.Comp
+import Mathlib.Analysis.Calculus.FDeriv.Equiv
 import Mathlib.Analysis.Calculus.FDeriv.Linear
 import Mathlib.Analysis.Calculus.FDeriv.Prod
 import Mathlib.Analysis.Calculus.FDeriv.RestrictScalars
