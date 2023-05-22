e9ea47c05d9f6321f8908bf931614ff70b177797
Parcly Taxel <reddeloostw@gmail.com>
2023-05-23 05:29:51 +0800

    Fix errors

--- Mathlib/Algebra/AlgebraicCard.lean
+++ Mathlib/Algebra/AlgebraicCard.lean
@@ -18,7 +18,7 @@ In this file, we prove variants of the following result: the cardinality of alge
 an R-algebra is at most `# R[X] * ℵ₀`.
 
 Although this can be used to prove that real or complex transcendental numbers exist, a more direct
-proof is given by `liouville.is_transcendental`.
+proof is given by `Liouville.is_transcendental`.
 -/
 
 
@@ -47,14 +47,14 @@ variable (R : Type u) (A : Type v) [CommRing R] [CommRing A] [IsDomain A] [Algeb
 
 theorem cardinal_mk_lift_le_mul :
     Cardinal.lift.{u} (#{ x : A // IsAlgebraic R x }) ≤ Cardinal.lift.{v} (#R[X]) * ℵ₀ := by
-  rw [← mk_ulift, ← mk_ulift]
+  rw [← mk_uLift, ← mk_uLift]
   choose g hg₁ hg₂ using fun x : { x : A | IsAlgebraic R x } => x.coe_prop
   refine' lift_mk_le_lift_mk_mul_of_lift_mk_preimage_le g fun f => _
-  rw [lift_le_aleph_0, le_aleph_0_iff_set_countable]
-  suffices : maps_to coe (g ⁻¹' {f}) (f.root_set A)
-  exact this.countable_of_inj_on (subtype.coe_injective.inj_on _) (f.root_set_finite A).Countable
+  rw [lift_le_aleph0, le_aleph0_iff_set_countable]
+  suffices : MapsTo (↑) (g ⁻¹' {f}) (f.rootSet A)
+  exact this.countable_of_injOn (Subtype.coe_injective.injOn _) (f.rootSet_finite A).countable
   rintro x (rfl : g x = f)
-  exact mem_root_set.2 ⟨hg₁ x, hg₂ x⟩
+  exact mem_rootSet.2 ⟨hg₁ x, hg₂ x⟩
 #align algebraic.cardinal_mk_lift_le_mul Algebraic.cardinal_mk_lift_le_mul
 
 theorem cardinal_mk_lift_le_max :
@@ -67,16 +67,15 @@ theorem cardinal_mk_lift_le_max :
 theorem cardinal_mk_lift_of_infinite [Infinite R] :
     Cardinal.lift.{u} (#{ x : A // IsAlgebraic R x }) = Cardinal.lift.{v} (#R) :=
   ((cardinal_mk_lift_le_max R A).trans_eq (max_eq_left <| aleph0_le_mk _)).antisymm <|
-    lift_mk_le'.2
-      ⟨⟨fun x => ⟨algebraMap R A x, isAlgebraic_algebraMap _⟩, fun x y h =>
-          NoZeroSMulDivisors.algebraMap_injective R A (Subtype.ext_iff.1 h)⟩⟩
+    lift_mk_le'.2 ⟨⟨fun x => ⟨algebraMap R A x, isAlgebraic_algebraMap _⟩, fun _ _ h =>
+      NoZeroSMulDivisors.algebraMap_injective R A (Subtype.ext_iff.1 h)⟩⟩
 #align algebraic.cardinal_mk_lift_of_infinite Algebraic.cardinal_mk_lift_of_infinite
 
 variable [Countable R]
 
 @[simp]
 protected theorem countable : Set.Countable { x : A | IsAlgebraic R x } := by
-  rw [← le_aleph_0_iff_set_countable, ← lift_le]
+  rw [← le_aleph0_iff_set_countable, ← lift_le]
   apply (cardinal_mk_lift_le_max R A).trans
   simp
 #align algebraic.countable Algebraic.countable
@@ -112,4 +111,3 @@ theorem cardinal_mk_of_infinite [Infinite R] : (#{ x : A // IsAlgebraic R x }) =
 end NonLift
 
 end Algebraic
-
