0bc4563d29f7dcbd61bb5d42896281d6efebe294
Parcly Taxel <reddeloostw@gmail.com>
2023-05-23 03:21:29 +0800

    Fix errors

--- Mathlib/LinearAlgebra/BilinearForm/TensorProduct.lean
+++ Mathlib/LinearAlgebra/BilinearForm/TensorProduct.lean
@@ -16,9 +16,9 @@ import Mathlib.LinearAlgebra.TensorProduct
 
 ## Main definitions
 
-* `bilin_form.tensor_distrib (B₁ ⊗ₜ B₂)`: the bilinear form on `M₁ ⊗ M₂` constructed by applying
+* `BilinForm.tensorDistrib (B₁ ⊗ₜ B₂)`: the bilinear form on `M₁ ⊗ M₂` constructed by applying
   `B₁` on `M₁` and `B₂` on `M₂`.
-* `bilin_form.tensor_distrib_equiv`: `bilin_form.tensor_distrib` as an equivalence on finite free
+* `BilinForm.tensorDistribEquiv`: `BilinForm.tensorDistrib` as an equivalence on finite free
   modules.
 
 -/
@@ -43,24 +43,23 @@ variable [Module R M₁] [Module R M₂]
 /-- The tensor product of two bilinear forms injects into bilinear forms on tensor products. -/
 def tensorDistrib : BilinForm R M₁ ⊗[R] BilinForm R M₂ →ₗ[R] BilinForm R (M₁ ⊗[R] M₂) :=
   ((TensorProduct.tensorTensorTensorComm R _ _ _ _).dualMap ≪≫ₗ
-          (TensorProduct.lift.equiv R _ _ _).symm ≪≫ₗ
-        LinearMap.toBilin).toLinearMap ∘ₗ
-    TensorProduct.dualDistrib R _ _ ∘ₗ
-      (TensorProduct.congr (BilinForm.toLin ≪≫ₗ TensorProduct.lift.equiv R _ _ _)
-          (BilinForm.toLin ≪≫ₗ TensorProduct.lift.equiv R _ _ _)).toLinearMap
+    (TensorProduct.lift.equiv R _ _ _).symm ≪≫ₗ LinearMap.toBilin).toLinearMap ∘ₗ
+  TensorProduct.dualDistrib R _ _ ∘ₗ
+  (TensorProduct.congr (BilinForm.toLin ≪≫ₗ TensorProduct.lift.equiv R _ _ _)
+    (BilinForm.toLin ≪≫ₗ TensorProduct.lift.equiv R _ _ _)).toLinearMap
 #align bilin_form.tensor_distrib BilinForm.tensorDistrib
 
 @[simp]
 theorem tensorDistrib_tmul (B₁ : BilinForm R M₁) (B₂ : BilinForm R M₂) (m₁ : M₁) (m₂ : M₂)
     (m₁' : M₁) (m₂' : M₂) :
-    tensorDistrib (B₁ ⊗ₜ B₂) (m₁ ⊗ₜ m₂) (m₁' ⊗ₜ m₂') = B₁ m₁ m₁' * B₂ m₂ m₂' :=
+    tensorDistrib (R := R) (B₁ ⊗ₜ B₂) (m₁ ⊗ₜ m₂) (m₁' ⊗ₜ m₂') = B₁ m₁ m₁' * B₂ m₂ m₂' :=
   rfl
 #align bilin_form.tensor_distrib_tmul BilinForm.tensorDistrib_tmul
 
 /-- The tensor product of two bilinear forms, a shorthand for dot notation. -/
 @[reducible]
 protected def tmul (B₁ : BilinForm R M₁) (B₂ : BilinForm R M₂) : BilinForm R (M₁ ⊗[R] M₂) :=
-  tensorDistrib (B₁ ⊗ₜ[R] B₂)
+  tensorDistrib (R := R) (B₁ ⊗ₜ[R] B₂)
 #align bilin_form.tmul BilinForm.tmul
 
 end CommSemiring
@@ -79,27 +78,24 @@ variable [Module.Free R M₂] [Module.Finite R M₂]
 
 variable [Nontrivial R]
 
-/-- `tensor_distrib` as an equivalence. -/
+/-- `tensorDistrib` as an equivalence. -/
 noncomputable def tensorDistribEquiv :
     BilinForm R M₁ ⊗[R] BilinForm R M₂ ≃ₗ[R] BilinForm R (M₁ ⊗[R] M₂) :=
-  -- the same `linear_equiv`s as from `tensor_distrib`, but with the inner linear map also as an
-            -- equiv
-            TensorProduct.congr
-            (BilinForm.toLin ≪≫ₗ TensorProduct.lift.equiv R _ _ _)
-            (BilinForm.toLin ≪≫ₗ TensorProduct.lift.equiv R _ _ _) ≪≫ₗ
-          TensorProduct.dualDistribEquiv R (M₁ ⊗ M₁) (M₂ ⊗ M₂) ≪≫ₗ
-        (TensorProduct.tensorTensorTensorComm R _ _ _ _).dualMap ≪≫ₗ
-      (TensorProduct.lift.equiv R _ _ _).symm ≪≫ₗ
-    LinearMap.toBilin
+  -- the same `LinearEquiv`s as from `tensorDistrib`,
+  -- but with the inner linear map also as an equiv
+  TensorProduct.congr (BilinForm.toLin ≪≫ₗ TensorProduct.lift.equiv R _ _ _)
+    (BilinForm.toLin ≪≫ₗ TensorProduct.lift.equiv R _ _ _) ≪≫ₗ
+  TensorProduct.dualDistribEquiv R (M₁ ⊗ M₁) (M₂ ⊗ M₂) ≪≫ₗ
+  (TensorProduct.tensorTensorTensorComm R _ _ _ _).dualMap ≪≫ₗ
+  (TensorProduct.lift.equiv R _ _ _).symm ≪≫ₗ LinearMap.toBilin
 #align bilin_form.tensor_distrib_equiv BilinForm.tensorDistribEquiv
 
 @[simp]
 theorem tensorDistribEquiv_apply (B : BilinForm R M₁ ⊗ BilinForm R M₂) :
-    tensorDistribEquiv B = tensorDistrib B :=
+    tensorDistribEquiv (R := R) (M₁ := M₁) (M₂ := M₂) B = tensorDistrib (R := R) B :=
   rfl
 #align bilin_form.tensor_distrib_equiv_apply BilinForm.tensorDistribEquiv_apply
 
 end CommRing
 
 end BilinForm
-
