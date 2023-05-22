05ab6b9b99ee3de399e060fd872bf281c2bcf46d
Joël Riou <joel.riou@universite-paris-saclay.fr>
2023-05-22 19:04:43 +0200

    fixed a typo

--- Mathlib/Algebra/Homology/ShortComplex/LeftHomology.lean
+++ Mathlib/Algebra/Homology/ShortComplex/LeftHomology.lean
@@ -56,7 +56,7 @@ structure LeftHomologyData where
   hi : IsLimit (KernelFork.ofι i wi)
   /-- the cokernel condition for `π` -/
   wπ : hi.lift (KernelFork.ofι _ S.zero) ≫ π = 0
-  /-- `π : K ⟶ H ` is a cokernel of the induced morphism `f' : S.X₁ ⟶ K` -/
+  /-- `π : K ⟶ H ` is a cokernel of the induced morphism `S.f' : S.X₁ ⟶ K` -/
   hπ : IsColimit (CokernelCofork.ofπ π wπ)
 
 initialize_simps_projections LeftHomologyData (-hi, -hπ)
