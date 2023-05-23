f1455f1d3020ace0dab8becec2833e0c52a7c855
Jujian Zhang <jujian.zhang1998@outlook.com>
2023-05-23 13:58:59 +0000

    feat: port RingTheory.Ideal.Over (#4270)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1945,6 +1945,7 @@ import Mathlib.RingTheory.Ideal.IdempotentFG
 import Mathlib.RingTheory.Ideal.LocalRing
 import Mathlib.RingTheory.Ideal.MinimalPrime
 import Mathlib.RingTheory.Ideal.Operations
+import Mathlib.RingTheory.Ideal.Over
 import Mathlib.RingTheory.Ideal.Prod
 import Mathlib.RingTheory.Ideal.Quotient
 import Mathlib.RingTheory.Ideal.QuotientOperations
