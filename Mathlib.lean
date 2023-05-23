5a3910475d832ead3e4353d488ab2e4857391832
Jujian Zhang <jujian.zhang1998@outlook.com>
2023-05-23 15:02:36 +0000

    feat: port Topology.Sheaves.SheafCondition.OpensLeCover (#4273)

--- Mathlib.lean
+++ Mathlib.lean
@@ -2366,6 +2366,7 @@ import Mathlib.Topology.Sheaves.PUnit
 import Mathlib.Topology.Sheaves.Presheaf
 import Mathlib.Topology.Sheaves.PresheafOfFunctions
 import Mathlib.Topology.Sheaves.Sheaf
+import Mathlib.Topology.Sheaves.SheafCondition.OpensLeCover
 import Mathlib.Topology.Sheaves.SheafCondition.Sites
 import Mathlib.Topology.ShrinkingLemma
 import Mathlib.Topology.Sober
