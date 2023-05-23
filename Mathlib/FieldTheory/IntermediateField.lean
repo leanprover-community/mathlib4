c6ddcf3493ac319964307dc8fe116e21fc027dbd
Chris Hughes <chrishughes24@gmail.com>
2023-05-23 08:56:41 +0100

    increase heartbeats

--- Mathlib/FieldTheory/IntermediateField.lean
+++ Mathlib/FieldTheory/IntermediateField.lean
@@ -677,11 +677,13 @@ instance finiteDimensional_right [FiniteDimensional K L] : FiniteDimensional F L
   right K F L
 #align intermediate_field.finite_dimensional_right IntermediateField.finiteDimensional_right
 
+set_option synthInstance.maxHeartbeats 25000 in
 @[simp]
 theorem rank_eq_rank_subalgebra : Module.rank K F.toSubalgebra = Module.rank K F :=
   rfl
 #align intermediate_field.rank_eq_rank_subalgebra IntermediateField.rank_eq_rank_subalgebra
 
+set_option synthInstance.maxHeartbeats 25000 in
 @[simp]
 theorem finrank_eq_finrank_subalgebra : finrank K F.toSubalgebra = finrank K F :=
   rfl
