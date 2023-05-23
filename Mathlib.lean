22a8c45f2141c2d8bca05da514bf733f388007b4
Moritz Doll <moritz.doll@googlemail.com>
2023-05-23 07:30:08 +0000

    feat: port Analysis.Convex.Cone.Basic (#4152)
    Co-authored-by: Yury G. Kudryashov <urkud@urkud.name>

--- Mathlib.lean
+++ Mathlib.lean
@@ -400,6 +400,7 @@ import Mathlib.Analysis.Convex.Body
 import Mathlib.Analysis.Convex.Caratheodory
 import Mathlib.Analysis.Convex.Combination
 import Mathlib.Analysis.Convex.Complex
+import Mathlib.Analysis.Convex.Cone.Basic
 import Mathlib.Analysis.Convex.Contractible
 import Mathlib.Analysis.Convex.Exposed
 import Mathlib.Analysis.Convex.Extrema
