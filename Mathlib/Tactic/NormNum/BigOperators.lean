0cab37bed582eeb41a36f29d51c63dfac12ebfac
Vierkantor <vierkantor@vierkantor.com>
2023-05-23 23:35:44 +0000

    Support for `Finset.univ`

--- Mathlib/Tactic/NormNum/BigOperators.lean
+++ Mathlib/Tactic/NormNum/BigOperators.lean
@@ -18,11 +18,18 @@ open Lean hiding Rat mkRat
 open Meta
 open Qq
 
--- FIXME: docs
+/-- This represents the result of trying to determine whether the given expression `n : Q(ℕ)`
+is either `zero` or `succ`. -/
 inductive Nat.UnifyZeroOrSuccResult (n : Q(ℕ))
+/-- `n` unifies with `0` -/
 | zero (pf : $n =Q 0)
+/-- `n` unifies with `succ n'` for this specific `n'` -/
 | succ (n' : Q(ℕ)) (pf : $n =Q Nat.succ $n')
 
+/-- Determine whether the expression `n : Q(ℕ)` unifies with `0` or `Nat.succ n'`.
+
+Fails if neither of the options succeed.
+-/
 def Nat.unifyZeroOrSucc (n : Q(ℕ)) : MetaM (Nat.UnifyZeroOrSuccResult n) := do
 match ← isDefEqQ n q(0) with
 | .defEq pf => return .zero pf
@@ -33,16 +40,31 @@ match ← isDefEqQ n q(0) with
     throwError "could not figure out value of `?n` from `{n} =?= nat.succ ?n`"
   pure (.succ n'_val ⟨⟩)
 
--- FIXME: docs
+/-- This represents the result of trying to determine whether the given expression
+`s : Q(Finset $α)` is either empty or consists of an element inserted into a strict subset. -/
 inductive Finset.ProveEmptyOrConsResult {α : Q(Type u)} (s : Q(Finset $α))
+/-- The set is empty. -/
 | empty (pf : Q($s = ∅))
+/-- The set equals `a` inserted into the strict subset `s'`. -/
 | cons (a : Q($α)) (s' : Q(Finset $α)) (h : Q($a ∉ $s')) (pf : Q($s = Finset.cons $a $s' $h))
 
+/-- If `s` unifies with `t`, convert a result for `s` to a result for `t`.
+
+If `s` does not unify with `t`, this results in a type-incorrect proof.
+-/
 def Finset.ProveEmptyOrConsResult.uncheckedCast {α : Q(Type u)} {β : Q(Type v)}
     (s : Q(Finset $α)) (t : Q(Finset $β)) :
     Finset.ProveEmptyOrConsResult s → Finset.ProveEmptyOrConsResult t
 | .empty pf => .empty pf
 | .cons a s' h pf => .cons a s' h pf
+
+/-- If `s = t` and we can get the result for `t`, then we can get the result for `s`.
+-/
+def Finset.ProveEmptyOrConsResult.eq_trans {α : Q(Type u)} {s t : Q(Finset $α)}
+    (eq : Q($s = $t)) :
+    Finset.ProveEmptyOrConsResult t → Finset.ProveEmptyOrConsResult s
+| .empty pf => .empty q(Eq.trans $eq $pf)
+| .cons a s' h pf => .cons a s' h q(Eq.trans $eq $pf)
 
 lemma Finset.insert_eq_cons {α : Type _} [DecidableEq α] (a : α) (s : Finset α) (h : a ∉ s) :
   insert a s = Finset.cons a s h :=
@@ -53,22 +75,38 @@ lemma Finset.range_succ_eq_cons (n' : ℕ) :
     Finset.cons n' (Finset.range n') Finset.not_mem_range_self :=
 by rw [Finset.range_succ, Finset.insert_eq_cons]
 
-def Finset.proveEmptyOrCons {α : Q(Type u)} :
+lemma Finset.univ_eq_elems {α : Type _} [Fintype α] (elems : Finset α)
+    (complete : ∀ x : α, x ∈ elems) :
+    Finset.univ = elems := by
+  ext x; simpa using complete x
+
+/-- Either show the expression `s : Q(Finset α)` is empty, or remove one element from it.
+
+Fails if neither of the options succeed.
+-/
+partial def Finset.proveEmptyOrCons {α : Q(Type u)} :
   (s : Q(Finset $α)) → MetaM (Finset.ProveEmptyOrConsResult s) :=
 fun s ↦
 match Expr.getAppFnArgs s with
 | (`EmptyCollection.emptyCollection, _) => pure (.empty (q(rfl) : Q($s = $s)))
-| (`Finset.cons, #[α, a, s', h]) => pure (.cons a s' h (q(rfl) : Q($s = $s)))
+| (`Finset.cons, #[_, a, s', h]) => pure (.cons a s' h (q(rfl) : Q($s = $s)))
 | (`Finset.range, #[(n : Q(ℕ))]) => do
   match ← Nat.unifyZeroOrSucc n with
-  | .zero pf => do
+  | .zero _pf => do
     pure (.empty (q(Finset.range_zero) : Q(Finset.range 0 = ∅)))
-  | .succ n' pf => pure <| (Finset.ProveEmptyOrConsResult.cons
+  | .succ n' _pf => pure <| (Finset.ProveEmptyOrConsResult.cons
       n'
       (q(Finset.range $n'))
       (q(@Finset.not_mem_range_self $n'))
       (q(Finset.range_succ_eq_cons $n'))).uncheckedCast (q(Finset.range (Nat.succ $n')) : Q(Finset ℕ)) s
-| _ => throwError "could not match {s}"
+| (`Finset.univ, #[_, instFT]) => do
+  match Expr.getAppFnArgs (← whnfI instFT) with
+  | (`Fintype.mk, #[_, (elems : Q(Finset $α)), (complete : Q(∀ x : $α, x ∈ $elems))]) => do
+    have _instFT : Q(Fintype $α) := instFT
+    let res ← Finset.proveEmptyOrCons elems
+    pure <| res.eq_trans (q(Finset.univ_eq_elems $elems $complete) : Q(Finset.univ = $elems))
+  | e => throwError "Finset.proveEmptyOrCons: could not determine elements of Fintype instance {e}"
+| _ => throwError "Finset.proveEmptyOrCons: unsupported finset expression {s}"
 
 namespace NormNum
 
@@ -98,6 +136,8 @@ protected lemma Finset.prod_empty {β α : Type _} [CommSemiring β] (f : α →
     IsNat (Finset.prod ∅ f) 1 :=
   ⟨by simp⟩
 
+/-- Evaluate a big operator applied to a finset by repeating `proveEmptyOrCons` until
+we exhaust all elements of the set. -/
 partial def evalFinsetBigop {α : Q(Type u)} {β : Q(Type v)}
     (op : Q(Finset $α → ($α → $β) → $β))
     (f : Q($α → $β))
@@ -118,6 +158,10 @@ partial def evalFinsetBigop {α : Q(Type u)} {β : Q(Type v)}
     dbg_trace "cons. fa: {fa} res_fa: {res_fa.toRat} res_op_s': {res_op_s'.toRat}"
     pure (res.eq_trans eq)
 
+/-- `norm_num` plugin for evaluating products of finsets.
+
+If your finset is not supported, you can add it to the match in `Finset.proveEmptyOrCons`.
+-/
 @[norm_num @Finset.prod _ _ _ _ _]
 partial def evalFinsetProd : NormNumExt where eval {u β} e := do
   dbg_trace "evalFinsetProd"
@@ -144,6 +188,10 @@ partial def evalFinsetProd : NormNumExt where eval {u β} e := do
       pure <| res.eq_trans eq)
     s
 
+/-- `norm_num` plugin for evaluating sums of finsets.
+
+If your finset is not supported, you can add it to the match in `Finset.proveEmptyOrCons`.
+-/
 @[norm_num @Finset.sum _ _ _ _ _]
 partial def evalFinsetSum : NormNumExt where eval {u β} e := do
   dbg_trace "evalFinsetSum"
@@ -172,7 +220,7 @@ partial def evalFinsetSum : NormNumExt where eval {u β} e := do
 set_option trace.Tactic.norm_num true
 
 example : Finset.prod (Finset.cons 2 ∅ (Finset.not_mem_empty _)) (λ x => x) = 2 := by norm_num1
-example : Finset.prod (Finset.cons 6 (Finset.cons 2 ∅ (Finset.not_mem_empty _)) sorry) (λ x => x) = 12 := by norm_num1
+example : Finset.prod (Finset.cons 6 (Finset.cons 2 ∅ (Finset.not_mem_empty _)) (by norm_num)) (λ x => x) = 12 := by norm_num1
 
 section big_operators
 
@@ -199,6 +247,7 @@ example (f : ℕ → α) : ∏ i in Finset.range 0, f i = 1 := by norm_num1
 example (f : Fin 0 → α) : ∏ i : Fin 0, f i = 1 := by norm_num1
 example (f : Fin 0 → α) : ∑ i : Fin 0, f i = 0 := by norm_num1
 example (f : ℕ → α) : ∑ i in (∅ : Finset ℕ), f i = 0 := by norm_num1
+example (f : Fin 3 → α) : ∑ i : Fin 3, (i : ℕ) = 3 := by norm_num1
 /-
 example (f : Fin 3 → α) : ∑ i : Fin 3, f i = f 0 + f 1 + f 2 := by norm_num <;> ring
 example (f : Fin 4 → α) : ∑ i : Fin 4, f i = f 0 + f 1 + f 2 + f 3 := by norm_num <;> ring
