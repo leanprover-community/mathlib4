d29d089af00b64a556e4138d7eab3291ead4f60f
Parcly Taxel <reddeloostw@gmail.com>
2023-05-23 16:08:14 +0800

    Fix errors

--- Mathlib/LinearAlgebra/FreeModule/StrongRankCondition.lean
+++ Mathlib/LinearAlgebra/FreeModule/StrongRankCondition.lean
@@ -15,21 +15,21 @@ import Mathlib.LinearAlgebra.InvariantBasisNumber
 
 # Strong rank condition for commutative rings
 
-We prove that any nontrivial commutative ring satisfies `strong_rank_condition`, meaning that
-if there is an injective linear map `(fin n → R) →ₗ[R] fin m → R`, then `n ≤ m`. This implies that
-any commutative ring satisfies `invariant_basis_number`: the rank of a finitely generated free
+We prove that any nontrivial commutative ring satisfies `StrongRankCondition`, meaning that
+if there is an injective linear map `(Fin n → R) →ₗ[R] Fin m → R`, then `n ≤ m`. This implies that
+any commutative ring satisfies `InvariantBasisNumber`: the rank of a finitely generated free
 module is well defined.
 
 ## Main result
 
-* `comm_ring_strong_rank_condition R` : `R` has the `strong_rank_condition`.
+* `commRing_strongRankCondition R` : `R` has the `StrongRankCondition`.
 
 ## References
 
 We follow the proof given in https://mathoverflow.net/a/47846/7845.
 The argument is the following: it is enough to prove that for all `n`, there is no injective linear
-map `(fin (n + 1) → R) →ₗ[R] fin n → R`. Given such an `f`, we get by extension an injective
-linear map `g : (fin (n + 1) → R) →ₗ[R] fin (n + 1) → R`. Injectivity implies that `P`, the
+map `(Fin (n + 1) → R) →ₗ[R] Fin n → R`. Given such an `f`, we get by extension an injective
+linear map `g : (Fin (n + 1) → R) →ₗ[R] Fin (n + 1) → R`. Injectivity implies that `P`, the
 minimal polynomial of `g`, has non-zero constant term `a₀ ≠ 0`. But evaluating `0 = P(g)` at the
 vector `(0,...,0,1)` gives `a₀`, contradiction.
 
@@ -40,19 +40,20 @@ variable (R : Type _) [CommRing R] [Nontrivial R]
 
 open Polynomial Function Fin LinearMap
 
-/-- Any commutative ring satisfies the `strong_rank_condition`. -/
+set_option maxHeartbeats 250000 in
+set_option synthInstance.maxHeartbeats 25000 in
+/-- Any commutative ring satisfies the `StrongRankCondition`. -/
 instance (priority := 100) commRing_strongRankCondition : StrongRankCondition R := by
-  suffices ∀ n, ∀ f : (Fin (n + 1) → R) →ₗ[R] Fin n → R, ¬injective f by
+  suffices ∀ n, ∀ f : (Fin (n + 1) → R) →ₗ[R] Fin n → R, ¬Injective f by
     rwa [strongRankCondition_iff_succ R]
   intro n f
   by_contra hf
-  -- Lean is unable to find these instances without help, either via this `letI`, or via duplicate
-  -- instances with unecessarily strong typeclasses on `R` and `M`.
-  letI : Module.Finite R (Fin n.succ → R) := Module.Finite.pi
-  letI : Module.Free R (Fin n.succ → R) := Module.Free.pi _ _
-  let g : (Fin (n + 1) → R) →ₗ[R] Fin (n + 1) → R := (extend_by_zero.linear_map R cast_succ).comp f
-  have hg : injective g := (extend_injective (RelEmbedding.injective cast_succ) 0).comp hf
-  have hnex : ¬∃ i : Fin n, cast_succ i = last n := fun ⟨i, hi⟩ => ne_of_lt (cast_succ_lt_last i) hi
+  -- Porting note: Lean can now find these instances without help...
+  -- letI : Module.Finite R (Fin n.succ → R) := Module.Finite.pi
+  -- letI : Module.Free R (Fin n.succ → R) := Module.Free.pi _ _
+  let g : (Fin (n + 1) → R) →ₗ[R] Fin (n + 1) → R := (ExtendByZero.linearMap R castSucc).comp f
+  have hg : Injective g := (extend_injective (RelEmbedding.injective castSucc) _).comp hf
+  have hnex : ¬∃ i : Fin n, castSucc i = last n := fun ⟨i, hi⟩ => ne_of_lt (castSucc_lt_last i) hi
   let a₀ := (minpoly R g).coeff 0
   have : a₀ ≠ 0 := minpoly_coeff_zero_of_injective hg
   have : a₀ = 0 := by
@@ -61,7 +62,7 @@ instance (priority := 100) commRing_strongRankCondition : StrongRankCondition R 
     obtain ⟨P, hP⟩ := X_dvd_iff.2 (erase_same (minpoly R g) 0)
     rw [← monomial_add_erase (minpoly R g) 0, hP] at heval
     replace heval := congr_fun heval (Fin.last n)
+    -- Porting note: ...it's just that this line gives a timeout without slightly raising heartbeats
     simpa [hnex] using heval
   contradiction
 #align comm_ring_strong_rank_condition commRing_strongRankCondition
-
