f31658642237a5699af0342976b95c71691a56fa
Joël Riou <joel.riou@universite-paris-saclay.fr>
2023-05-24 00:50:56 +0200

    fixed docstrings

--- Mathlib/Algebra/DirectLimit.lean
+++ Mathlib/Algebra/DirectLimit.lean
@@ -27,10 +27,10 @@ so as to make the operations (addition etc.) "computable".
 
 ## Main definitions
 
-* `directed_system f`
-* `module.direct_limit G f`
-* `add_comm_group.direct_limit G f`
-* `ring.direct_limit G f`
+* `DirectedSystem f`
+* `Module.DirectLimit G f`
+* `AddCommGroup.DirectLimit G f`
+* `Ring.DirectLimit G f`
 
 -/
 
@@ -70,14 +70,14 @@ variable [∀ i, AddCommGroup (G i)] [∀ i, Module R (G i)]
 
 variable {G} (f : ∀ i j, i ≤ j → G i →ₗ[R] G j)
 
-/-- A copy of `directed_system.map_self` specialized to linear maps, as otherwise the
+/-- A copy of `DirectedSystem.map_self` specialized to linear maps, as otherwise the
 `λ i j h, f i j h` can confuse the simplifier. -/
 nonrec theorem DirectedSystem.map_self [DirectedSystem G fun i j h => f i j h] (i x h) :
     f i i h x = x :=
   DirectedSystem.map_self (fun i j h => f i j h) i x h
 #align module.directed_system.map_self Module.DirectedSystem.map_self
 
-/-- A copy of `directed_system.map_map` specialized to linear maps, as otherwise the
+/-- A copy of `DirectedSystem.map_map` specialized to linear maps, as otherwise the
 `λ i j h, f i j h` can confuse the simplifier. -/
 nonrec theorem DirectedSystem.map_map [DirectedSystem G fun i j h => f i j h] {i j k} (hij hjk x) :
     f j k hjk (f i j hij x) = f i k (le_trans hij hjk) x :=
