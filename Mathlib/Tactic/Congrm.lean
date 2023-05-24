43e1fa08a7cbcd4dbf2be91e956eed71f7032e37
Moritz Doll <moritz.doll@googlemail.com>
2023-05-24 11:43:50 -0600

    cleanup

--- Mathlib/Tactic/Congrm.lean
+++ Mathlib/Tactic/Congrm.lean
@@ -17,10 +17,12 @@ initialize registerTraceClass `trace.congrm
 
 syntax (name := congrM) "congrm " term : tactic
 
+section util
+
 /-- Given a function, extract the explicit arguments. -/
 def _root_.Lean.Expr.getExplicitArgs (f : Expr) : MetaM (Array Expr) := do
   let args := f.getAppArgs
-  let pinfo := (← Lean.Meta.getFunInfoNArgs f.getAppFn args.toList.length).paramInfo
+  let pinfo := (← Lean.Meta.getFunInfo f.getAppFn').paramInfo
   logInfo s!"all arguments: {← args.mapM ppExpr} and patternInfos: {pinfo.toList.length}"
   return (pinfo.zip args).filterMap (λ arg => if arg.1.isExplicit then some arg.2 else none)
 
@@ -29,72 +31,70 @@ def try_refl (goal : MVarId) : MetaM (Option MVarId) := do
     goal.refl
   if res == none then return some goal else return none
 
-private def applyCongrThm? (mvarId : MVarId) (congrThm : CongrTheorem) : MetaM (List MVarId) := do
-  let mvarId ← mvarId.assert (← mkFreshUserName `h_congr_thm) congrThm.type congrThm.proof
-  let (fvarId, mvarId) ← mvarId.intro1P
-  let mvarIds ← mvarId.apply (mkFVar fvarId) { synthAssignedInstances := false }
-  mvarIds.mapM fun mvarId => mvarId.tryClear fvarId
+def _root_.Lean.MVarId.applyWithFreshMVarLevels (goal : MVarId) (lem : Name) :
+    MetaM (List MVarId) := do
+  goal.apply (← mkConstWithFreshMVarLevels lem)
 
-partial def foo (lem : Name) (goal : MVarId) (x : Expr) : MetaM MVarId := do
-  let localDecl ← x.fvarId!.getDecl
-  let newGoals ← goal.apply (← mkConstWithFreshMVarLevels lem)
+def modifyMainGoal (f : MVarId → MetaM (List MVarId)) : TacticM Unit := withMainContext do
+  setGoals <| ← f <| ← getMainGoal
+
+end util
+
+private partial def telescopingFn (lem : Name) (goal : MVarId) (x : Expr) : MetaM MVarId := do
+  let userName ← x.fvarId!.getUserName
+  let newGoals ← goal.applyWithFreshMVarLevels lem
   if newGoals.length == 1 then
     let newGoal := newGoals[0]!
-    return (← newGoal.intro localDecl.userName).2
-  return goal
+    return (← newGoal.intro userName).2
+  failure
 
-partial def congrm_loop (goal : MVarId) (pat : Expr) : MetaM (List MVarId) := do
+open private applyCongrThm? from Lean.Meta.Tactic.Congr in
+
+partial def congrm_loop (pat : Expr) (goal : MVarId) : MetaM (List MVarId) := goal.withContext do
   -- Helper function (stolen from somewhere) that creates the correct FVars in `λ` and `∀`
   -- and does the recursion
   let binders (mvarId : MVarId) (lem : Name) (xs : Array Expr) (k : Expr) : MetaM (List MVarId) := do
-    congrm_loop (← xs.foldlM (foo lem) mvarId) k
+    congrm_loop k (← xs.foldlM (telescopingFn lem) mvarId)
   logInfo "Pattern {← ppExpr pat}"
-  if pat.isForall then
+  if pat.isMVar then
+    return [goal]
+  else if pat.isForall then
     logInfo "Forall pattern"
     forallTelescope pat (binders goal ``pi_congr)
   else if pat.isLambda then
     logInfo "Lambda pattern"
     lambdaTelescope pat (binders goal ``funext)
   else if pat.isApp then
-    let args := (← pat.getExplicitArgs).toList
+    let patternArgs := (← pat.getExplicitArgs).toList
     let some congrThm ← mkCongrSimp? pat.getAppFn' (subsingletonInstImplicitRhs := false) | return []
-    if congrThm.type.isMVar then
-      logInfo "Invalid congr lemma"
-      return [goal]
-
     if pat.getAppFn.isMVar then
       logInfo "Fun is metavar"
       return [goal]
-    -- Should check whether the congr_lem is valid
 
-    let argumentList ← applyCongrThm? goal congrThm
+    let goalArgs ← applyCongrThm? goal congrThm
     logInfo s!"Apply pattern, fun: {← ppExpr pat.getAppFn}"
     -- If the pattern has a different number of arguments, then we just fail:
-    if argumentList.length != args.length then
+    if goalArgs.length != patternArgs.length then
       logInfo s!"Number of patterns and arguments are different:
-        args: {argumentList.map MVarId.name}
+        args: {goalArgs.map MVarId.name}
         pat: {← pat.getAppArgs.mapM ppExpr}
-         {← args.mapM ppExpr}"
+         {← patternArgs.mapM ppExpr}"
       throwTacticEx `congrm goal m!"Number of function arguments does not match"
-      --failure
-      -- There is some problem with `Eq` `_ = _` has 2 arguments, but there appears only one argument
-    -- We should not set the goals, but see from which part each goal came and recursively apply
-    -- patterns
 
-    let blubb := argumentList.zip args
-    return (← blubb.mapM (λ (m,e) => congrm_loop m e)).join
-  else return [goal]
+    -- Apply `congrm_loop` to all arguments with the corresponding pattern and concat the resulting
+    -- list.
+    return (← (patternArgs.zip goalArgs).mapM (λ (e,m) => congrm_loop e m)).join
+  else
+    return [goal]
+    --throwTacticEx `congrm goal m!"Invalid pattern"
 
-partial def congrm_core (goal : MVarId) (pat : Expr) : MetaM (List MVarId) := do
+partial def congrm_core (pat : Expr) (goal : MVarId) : MetaM (List MVarId) := do
   -- First change `iff` to `=` and then run the loop:
-  let mvars ← congrm_loop (← goal.iffOfEq) pat
+  let mvars ← congrm_loop pat (← goal.iffOfEq)
   mvars.filterMapM try_refl
 
-def evalTactic (f : MVarId → MetaM (List MVarId)) : TacticM Unit := withMainContext do
-  setGoals (← f (← getMainGoal))
-
 elab_rules : tactic | `(tactic| congrm $expr:term) => withMainContext do
-  setGoals (← congrm_core (← getMainGoal) (← elabTerm expr none))
+  modifyMainGoal <| congrm_core <| ← elabTerm expr none
 
 -- Testing that the trivial `forall` rule works:
 example (f : α → Prop) (h : ∀ a, f a = True) : (∀ a : α, f a) ↔ (∀ _ : α, True) := by
@@ -132,11 +132,19 @@ example (a b c : ℕ) (h : b = c) : a = b ↔ a = c := by
   congrm (_ = _)
   exact h
 
--- Testing for implicit arguments in function application
+example {a b : ℕ} (h : a = b) : (fun _ : ℕ => ∀ z, a + a = z) = (fun _ => ∀ z, b + a = z) := by
+  congrm λ x => ∀ w, (_ + a = w)
+  simp only [h]
+
+-- Tests that should fail:
+
+
+-- Testing for too many arguments
 example (a b c : ℕ) (h : b = c) : a = b ↔ a = c := by
   congrm (Eq _ _ _) -- Todo: good error message
   exact h
 
-example {a b : ℕ} (h : a = b) : (fun _ : ℕ => ∀ z, a + a = z) = (fun _ => ∀ z, b + a = z) := by
-  congrm λ x => ∀ w, (_ + a = w)
-  simp only [h]
+-- Testing for wrong pattern
+example (f : α → Prop) (h : ∀ a, f a = True) : (∀ a : α, f a) ↔ (∀ _ : α, True) := by
+  congrm (f _)
+  exact h x
