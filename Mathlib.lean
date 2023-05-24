3d4bf22a1b7a0ce6b7c81e09c2476bf77364356a
Pol_tta <pol_tta@outlook.jp>
2023-05-24 10:36:46 +0000

    feat: port Data.Fintype.Quotient (#3971)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1017,6 +1017,7 @@ import Mathlib.Data.Fintype.Perm
 import Mathlib.Data.Fintype.Pi
 import Mathlib.Data.Fintype.Powerset
 import Mathlib.Data.Fintype.Prod
+import Mathlib.Data.Fintype.Quotient
 import Mathlib.Data.Fintype.Sigma
 import Mathlib.Data.Fintype.Small
 import Mathlib.Data.Fintype.Sort
