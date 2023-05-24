88a01cc399e192ab1917cf239d8eae5cfcde0c7b
Pol_tta <pol_tta@outlook.jp>
2023-05-24 11:42:22 +0000

    feat: port ModelTheory.Quotients (#3972)
    Co-authored-by: Chris Hughes <chrishughes24@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1775,6 +1775,7 @@ import Mathlib.ModelTheory.Encoding
 import Mathlib.ModelTheory.FinitelyGenerated
 import Mathlib.ModelTheory.LanguageMap
 import Mathlib.ModelTheory.Order
+import Mathlib.ModelTheory.Quotients
 import Mathlib.ModelTheory.Semantics
 import Mathlib.ModelTheory.Skolem
 import Mathlib.ModelTheory.Substructures
