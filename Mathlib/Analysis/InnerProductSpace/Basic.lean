f91f75ca5cd194ceeefc5dedfb39dcccb47ef24b
Jireh Loreaux <loreaujy@gmail.com>
2023-05-24 10:14:17 -0500

    heartbeats instead of errors

--- Mathlib/Analysis/InnerProductSpace/Basic.lean
+++ Mathlib/Analysis/InnerProductSpace/Basic.lean
@@ -1791,7 +1791,7 @@ theorem innerSL_apply_norm (x : E) : ‖innerSL 𝕜 x‖ = ‖x‖ := by
 set_option linter.uppercaseLean3 false in
 #align innerSL_apply_norm innerSL_apply_norm
 
-set_option synthInstance.maxHeartbeats 80000
+set_option synthInstance.maxHeartbeats 80000 in
 -- porting note: I'm not sure why the maxHeartbeats needed to be so high here
 /-- The inner product as a continuous sesquilinear map, with the two arguments flipped. -/
 def innerSLFlip : E →L[𝕜] E →L⋆[𝕜] 𝕜 :=
@@ -1812,10 +1812,12 @@ namespace ContinuousLinearMap
 
 variable {E' : Type _} [NormedAddCommGroup E'] [InnerProductSpace 𝕜 E']
 
+set_option maxHeartbeats 500000 in
+set_option synthInstance.maxHeartbeats 100000 in
 /-- Given `f : E →L[𝕜] E'`, construct the continuous sesquilinear form `λ x y, ⟪x, A y⟫`, given
 as a continuous linear map. -/
 def toSesqForm : (E →L[𝕜] E') →L[𝕜] E' →L⋆[𝕜] E →L[𝕜] 𝕜 :=
-  (↑(ContinuousLinearMap.flipₗᵢ' E E' 𝕜 (starRingEnd 𝕜) (RingHom.id 𝕜)).toContinuousLinearEquiv : _ →L[𝕜] _) ∘L
+  (ContinuousLinearMap.flipₗᵢ' E E' 𝕜 (starRingEnd 𝕜) (RingHom.id 𝕜)).toContinuousLinearEquiv ∘L
     ContinuousLinearMap.compSL E E' (E' →L⋆[𝕜] 𝕜) (RingHom.id 𝕜) (RingHom.id 𝕜) (innerSLFlip 𝕜)
 #align continuous_linear_map.to_sesq_form ContinuousLinearMap.toSesqForm
 
