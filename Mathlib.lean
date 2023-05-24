5719f012578a2648d43c3ce1adb8b82527a3cd82
Jujian Zhang <jujian.zhang1998@outlook.com>
2023-05-24 09:36:33 +0000

    feat: port AlgebraicGeometry.PrimeSpectrum.Basic (#4276)

--- Mathlib.lean
+++ Mathlib.lean
@@ -342,6 +342,7 @@ import Mathlib.Algebra.Tropical.Basic
 import Mathlib.Algebra.Tropical.BigOperators
 import Mathlib.Algebra.Tropical.Lattice
 import Mathlib.AlgebraicGeometry.PresheafedSpace
+import Mathlib.AlgebraicGeometry.PrimeSpectrum.Basic
 import Mathlib.AlgebraicGeometry.ProjectiveSpectrum.Topology
 import Mathlib.AlgebraicTopology.AlternatingFaceMapComplex
 import Mathlib.AlgebraicTopology.CechNerve
