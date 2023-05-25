3dde1ee7ebf54afe7d00971284f6c784d2a9d41b
Moritz Doll <moritz.doll@googlemail.com>
2023-05-25 19:41:35 +0000

    feat: port Analysis.PSeries (#4182)
    Removed the `simp` attribute on one lemma because `simpNF` was timing out trying to reduce the LHS. See [Zulip](https://leanprover.zulipchat.com/#narrow/stream/287929-mathlib4/topic/looping.20in.20.60simp.60.20set) for the problem. Once this error is corrected with `simp`, we should be able to add the attribute again.
    
    
    
    Co-authored-by: Jireh Loreaux <loreaujy@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -519,6 +519,7 @@ import Mathlib.Analysis.NormedSpace.Star.Basic
 import Mathlib.Analysis.NormedSpace.Star.Mul
 import Mathlib.Analysis.NormedSpace.Star.Multiplier
 import Mathlib.Analysis.NormedSpace.Units
+import Mathlib.Analysis.PSeries
 import Mathlib.Analysis.Seminorm
 import Mathlib.Analysis.SpecialFunctions.Complex.Arg
 import Mathlib.Analysis.SpecialFunctions.Complex.Circle
