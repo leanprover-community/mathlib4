a733227eef6951463120196003bc8d7eceaa6073
leopoldmayer <leomayer@uw.edu>
2023-05-25 12:43:34 -0700

    added more signatures for necessary functors

--- Mathlib/Topology/Order/FrameAdjunction.lean
+++ Mathlib/Topology/Order/FrameAdjunction.lean
@@ -2,10 +2,11 @@ import Mathlib.Order.CompleteBooleanAlgebra
 import Mathlib.Topology.Basic
 import Mathlib.Topology.Sets.Opens
 import Mathlib.CategoryTheory.Adjunction.Basic
+import Mathlib.CategoryTheory.Opposites
 import Mathlib.Topology.Category.TopCat.Basic
 import Mathlib.Order.Category.FrmCat
 
-open CategoryTheory
+open CategoryTheory Topology TopologicalSpace
 universe u
 variable (X : Type u)
 
@@ -24,13 +25,25 @@ def open_of_element_hom (L : Type _) [Order.Frame L] : FrameHom L (Set (pt_obj L
 -- pt L is a topological space
 instance ptTop (L : Type _) [Order.Frame L] : TopologicalSpace (pt_obj L) where
   IsOpen := sorry
+  isOpen_univ := sorry
+  isOpen_inter := sorry
+  isOpen_sUnion := sorry
 
--- this should be FrmCat.op but don't know syntax
-def pt : FrmCat ⥤ TopCat where
-  obj L := ⟨ FrameHom L Prop, by infer_instance⟩
+def pt : FrmCatᵒᵖ ⥤ TopCat where
+  obj L := ⟨ FrameHom L.unop Prop, by infer_instance⟩
   map    := sorry
+  map_id := sorry
+  map_comp := sorry
+
+
+def 𝒪 : TopCat ⥤ FrmCatᵒᵖ where
+  obj X := ⟨Opens X, by infer_instance⟩
+  map := sorry
   map_id := sorry
   map_comp := sorry
 
 -- the final goal
 theorem frame_top_adjunction : pt ⊣ 𝒪 := sorry
+
+
+#check Adjunction.mkOfUnitCounit
