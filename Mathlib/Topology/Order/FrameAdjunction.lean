1ad1f516f6c6cbe0b8e2e0321ab9ffb75979d8e1
Brendan Murphy <shamrockfrost@gmail.com>
2023-05-25 15:13:32 -0600

    Filled out FrameAdjunction.lean stub

--- Mathlib/Topology/Order/FrameAdjunction.lean
+++ Mathlib/Topology/Order/FrameAdjunction.lean
@@ -1,5 +1,6 @@
 import Mathlib.Order.CompleteBooleanAlgebra
 import Mathlib.Topology.Basic
+import Mathlib.Topology.Bases
 import Mathlib.Topology.Sets.Opens
 import Mathlib.CategoryTheory.Adjunction.Basic
 import Mathlib.CategoryTheory.Opposites
@@ -19,28 +20,72 @@ def open_of_element_hom (L : Type _) [Order.Frame L] : FrameHom L (Set (pt_obj L
   toFun u :=  {x | x u}
   map_inf' a b := by simp; rfl
   map_top'     := by simp; rfl
-  map_sSup' S  := by
-    simp; sorry
+  map_sSup' S  := by {
+    simp
+    ext Z
+    constructor
+    . rintro ⟨p, ⟨x, hx, hp⟩, h⟩
+      aesop_subst hp
+      simp only [Set.mem_iUnion, Set.mem_setOf_eq, exists_prop]
+      exact ⟨x, hx, h⟩
+    . rintro ⟨S', ⟨x, h⟩, hxZ⟩
+      subst h
+      use true
+      simp only [iff_true, and_true]
+      obtain ⟨S', h⟩ := hxZ
+      simp only [Set.mem_range, exists_prop] at h
+      obtain ⟨⟨hx, hS'⟩, hxZ⟩ := h
+      subst hS'
+      exact ⟨x, hx, hxZ⟩
+  }
 
 -- pt L is a topological space
 instance ptTop (L : Type _) [Order.Frame L] : TopologicalSpace (pt_obj L) where
-  IsOpen := sorry
-  isOpen_univ := sorry
-  isOpen_inter := sorry
-  isOpen_sUnion := sorry
+  IsOpen := Set.range fun u ↦ { x : pt_obj L | x u }
+  isOpen_univ := ⟨⊤, by simp only [map_top]; exact rfl⟩
+  isOpen_inter := by
+    rintro s t ⟨u, hs⟩ ⟨v, ht⟩
+    subst hs ht
+    use u ⊓ v
+    ext p
+    simp only [ge_iff_le, map_inf, le_Prop_eq, inf_Prop_eq,
+               Set.mem_setOf_eq, Set.mem_inter_iff]
+  isOpen_sUnion := by
+    intro U hU
+    use (sSup { u | { x | x u } ∈ U })
+    ext p
+    simp only [map_sSup, sSup_Prop_eq, Set.mem_image, Set.mem_setOf_eq,
+               eq_iff_iff, Set.mem_sUnion]
+    constructor
+    . rintro ⟨P, ⟨u, hu, hP⟩, h⟩
+      aesop_subst hP
+      exact ⟨{ x | x u }, hu, h⟩
+    . rintro ⟨t, ht, hp⟩
+      use true
+      simp only [iff_true, and_true]
+      obtain ⟨u, h⟩ := hU t ht
+      subst h
+      exact ⟨u, ht, hp⟩
+
+@[reducible]
+def pt_map {L L' : Type _} [Order.Frame L] [Order.Frame L']
+  (f : FrameHom L' L) : C(pt_obj L, pt_obj L') where
+  toFun := fun p ↦ p.comp f
+  continuous_toFun := ⟨by
+    rintro s ⟨u, hu⟩
+    subst hu
+    use f u
+    ext p
+    simp only [Set.mem_setOf_eq, Set.preimage_setOf_eq, FrameHom.comp_apply]⟩
+
 
 def pt : FrmCatᵒᵖ ⥤ TopCat where
-  obj L := ⟨ FrameHom L.unop Prop, by infer_instance⟩
-  map    := sorry
-  map_id := sorry
-  map_comp := sorry
-
+  obj L    := ⟨FrameHom L.unop Prop, by infer_instance⟩
+  map f    := pt_map f.unop
 
 def 𝒪 : TopCat ⥤ FrmCatᵒᵖ where
-  obj X := ⟨Opens X, by infer_instance⟩
-  map := sorry
-  map_id := sorry
-  map_comp := sorry
+  obj X := ⟨Opens X.α, by infer_instance⟩
+  map {X Y} f := by apply Opposite.op; exact Opens.comap f
 
 -- the final goal
 theorem frame_top_adjunction : pt ⊣ 𝒪 := sorry
