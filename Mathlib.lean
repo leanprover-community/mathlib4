63d78da6a358d613bff5f34c62ed9481dbbc52c2
Xavier Roblot <46200072+xroblot@users.noreply.github.com>
2023-05-25 12:49:14 +0000

    feat: port NumberTheory.Padics.PadicIntegers (#4269)
    Co-authored-by: Chris Hughes <chrishughes24@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1824,6 +1824,7 @@ import Mathlib.NumberTheory.LucasLehmer
 import Mathlib.NumberTheory.LucasPrimality
 import Mathlib.NumberTheory.ModularForms.CongruenceSubgroups
 import Mathlib.NumberTheory.Multiplicity
+import Mathlib.NumberTheory.Padics.PadicIntegers
 import Mathlib.NumberTheory.Padics.PadicNorm
 import Mathlib.NumberTheory.Padics.PadicNumbers
 import Mathlib.NumberTheory.Padics.PadicVal
