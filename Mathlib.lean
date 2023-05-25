83f97af5b74329547d5445dab85fc9dcce1aeec0
damiano <adomani@gmail.com>
2023-05-25 21:39:52 +0000

    feat: port FieldTheory.Ratfunc (#4293)
    I am going to try to work on this for a bit, but it looks hard.
    
    
    
    Co-authored-by: Chris Hughes <chrishughes24@gmail.com>
    Co-authored-by: Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
    Co-authored-by: Vierkantor <vierkantor@vierkantor.com>
    Co-authored-by: Anne Baanen <Vierkantor@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1472,6 +1472,7 @@ import Mathlib.FieldTheory.Minpoly.Basic
 import Mathlib.FieldTheory.Minpoly.Field
 import Mathlib.FieldTheory.MvPolynomial
 import Mathlib.FieldTheory.PerfectClosure
+import Mathlib.FieldTheory.RatFunc
 import Mathlib.FieldTheory.Separable
 import Mathlib.FieldTheory.SeparableDegree
 import Mathlib.FieldTheory.Subfield
