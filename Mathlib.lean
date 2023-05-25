56573ef9a17d75b0f7b5334910a56a72d66c5000
Pol_tta <pol_tta@outlook.jp>
2023-05-25 11:24:51 +0000

    feat: port ModelTheory.Graph (#3989)
    Co-authored-by: Chris Hughes <chrishughes24@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1794,6 +1794,7 @@ import Mathlib.ModelTheory.Definability
 import Mathlib.ModelTheory.ElementaryMaps
 import Mathlib.ModelTheory.Encoding
 import Mathlib.ModelTheory.FinitelyGenerated
+import Mathlib.ModelTheory.Graph
 import Mathlib.ModelTheory.LanguageMap
 import Mathlib.ModelTheory.Order
 import Mathlib.ModelTheory.Quotients
