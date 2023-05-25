e7890e23a7ad6c2374a2a34ba4bbde509b55b866
Jireh Loreaux <loreaujy@gmail.com>
2023-05-25 08:46:17 +0000

    feat: port LinearAlgebra.Matrix.SesquilinearForm (#4315)
    Dot notation is broken here, but I don't think there is anything we can do about it.

--- Mathlib.lean
+++ Mathlib.lean
@@ -1670,6 +1670,7 @@ import Mathlib.LinearAlgebra.Matrix.NonsingularInverse
 import Mathlib.LinearAlgebra.Matrix.Orthogonal
 import Mathlib.LinearAlgebra.Matrix.Polynomial
 import Mathlib.LinearAlgebra.Matrix.Reindex
+import Mathlib.LinearAlgebra.Matrix.SesquilinearForm
 import Mathlib.LinearAlgebra.Matrix.SpecialLinearGroup
 import Mathlib.LinearAlgebra.Matrix.Symmetric
 import Mathlib.LinearAlgebra.Matrix.ToLin
