52dc404d1cbb23eca3bcf45f77e2fcf2c980cd69
thorimur <68410468+thorimur@users.noreply.github.com>
2023-05-25 01:43:17 -0400

    feat: `mkFreshMVarsArray`

--- Mathlib/Lean/Meta/Basic.lean
+++ Mathlib/Lean/Meta/Basic.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2023 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Scott Morrison
+Authors: Scott Morrison, Thomas Murrills
 -/
 import Lean.Meta.Basic
 
@@ -17,3 +17,7 @@ Restore the metavariable context after execution.
 def Lean.Meta.preservingMCtx (x : MetaM α) : MetaM α := do
   let mctx ← getMCtx
   try x finally setMCtx mctx
+
+/-- Create an array of fresh level mvars. -/
+def Lean.Meta.mkFreshLevelMVarsArray (n : Nat) : MetaM (Array Level) :=
+  n.foldM (init := Array.mkEmpty (α := Level) n) fun _ acc => return acc.push (← mkFreshLevelMVar)
