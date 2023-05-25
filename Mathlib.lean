c111a30b3cc11a2231f9433c50bcdb1024e33bfb
Jason Yuen <jason_yuen2007@hotmail.com>
2023-05-25 17:58:55 +0000

    feat: port Analysis.Complex.UnitDisc.Basic (#4180)
    Co-authored-by: Jireh Loreaux <loreaujy@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -417,6 +417,7 @@ import Mathlib.Analysis.Complex.Conformal
 import Mathlib.Analysis.Complex.Isometry
 import Mathlib.Analysis.Complex.OperatorNorm
 import Mathlib.Analysis.Complex.ReImTopology
+import Mathlib.Analysis.Complex.UnitDisc.Basic
 import Mathlib.Analysis.Convex.Basic
 import Mathlib.Analysis.Convex.Between
 import Mathlib.Analysis.Convex.Body
