141c9c36760495c13960e038fd5bdf4dfeb7a08b
Pol_tta <pol_tta@outlook.jp>
2023-05-25 03:53:12 +0000

    feat: port ModelTheory.Ultraproducts (#3976)
    Co-authored-by: Chris Hughes <chrishughes24@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1788,6 +1788,7 @@ import Mathlib.ModelTheory.Semantics
 import Mathlib.ModelTheory.Skolem
 import Mathlib.ModelTheory.Substructures
 import Mathlib.ModelTheory.Syntax
+import Mathlib.ModelTheory.Ultraproducts
 import Mathlib.NumberTheory.ADEInequality
 import Mathlib.NumberTheory.Basic
 import Mathlib.NumberTheory.ClassNumber.AdmissibleAbs
