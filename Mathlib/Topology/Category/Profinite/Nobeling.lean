3550728e93fc370249e7fe9221c1ce0dc377780d
dagurtomas <dagurtomas@gmail.com>
2023-05-25 19:17:38 -0600

    set up induction

---
+++ Mathlib/Topology/Category/Profinite/Nobeling.lean
@@ -0,0 +1,258 @@
+import Mathlib.Topology.Category.Profinite.Basic
+import Mathlib.Topology.LocallyConstant.Algebra
+import Mathlib.LinearAlgebra.FreeModule.Basic
+import Mathlib.SetTheory.Ordinal.Basic
+import Mathlib.Tactic.LibrarySearch
+import Mathlib.Topology.Category.Profinite.InjectiveMap
+
+namespace LocallyConstant
+
+variable {X Y : Type _} [TopologicalSpace X] [TopologicalSpace Y] {Z : Type _}
+
+noncomputable
+def equiv (e : X ≃ₜ Y) : LocallyConstant X Z ≃ LocallyConstant Y Z :=
+{ toFun := comap e.invFun
+  invFun := comap e.toFun
+  left_inv := by
+    intro x
+    rw [comap_comp_apply _ _ e.continuous_toFun e.continuous_invFun]
+    simp
+  right_inv := by
+    intro x
+    rw [comap_comp_apply _ _ e.continuous_invFun e.continuous_toFun]
+    simp }
+
+@[simp]
+theorem coe_comap_apply (f : X → Y) (g : LocallyConstant Y Z) (hf : Continuous f) :
+    ∀ x, comap f g x = g (f x) := by
+  intro x
+  rw [← @Function.comp_apply _ _ _ g f x]
+  rw [← coe_comap _ _ hf]
+
+
+variable {R : Type _} [Ring R] [AddCommMonoid Z] [Module R Z]
+
+noncomputable
+def comapLinear (f : X → Y) (hf : Continuous f) : LocallyConstant Y Z →ₗ[R] LocallyConstant X Z :=
+{ toFun := comap f
+  map_add' := by
+    intro r s
+    ext x
+    simp
+    rw [coe_comap_apply _ _ hf]
+    rw [coe_comap_apply _ _ hf]
+    rw [coe_comap_apply _ _ hf]
+    rfl
+  map_smul' := by
+    intro r s
+    dsimp
+    ext x
+    simp
+    rw [coe_comap_apply _ _ hf]
+    rw [coe_comap_apply _ _ hf]
+    rfl }
+
+noncomputable
+def equivLinear (e : X ≃ₜ Y) : LocallyConstant X Z ≃ₗ[R] LocallyConstant Y Z :=
+{ toFun := (equiv e).toFun
+  map_smul' := (comapLinear _ e.continuous_invFun).map_smul'
+  map_add' := by -- why doesn't (comapLinear _ e.continuous_invFun).map_add' work?
+    intro r s
+    ext x
+    dsimp [equiv]
+    have hf : Continuous ↑(e.symm) := e.continuous_invFun
+    rw [coe_comap_apply _ _ hf]
+    rw [coe_comap_apply _ _ hf]
+    rw [coe_comap_apply _ _ hf]
+    rfl
+  invFun := (equiv e).invFun
+  left_inv := (equiv e).left_inv
+  right_inv := (equiv e).right_inv }
+
+end LocallyConstant
+
+namespace NobelingProof
+
+variable {I : Type _} [LinearOrder I] [IsWellOrder I (·<·)] (C : Set ((WithTop I) → Bool))
+
+def BoolToZ : Bool → ℤ := (if · then 1 else 0)
+
+variable (I)
+
+def r : (I → Bool) → (WithTop I) → Bool := fun f i ↦ if let some a := i then f a else false
+
+lemma r.embedding : ClosedEmbedding (r I) := by
+  apply Continuous.closedEmbedding
+  · apply continuous_pi
+    intro i
+    dsimp [r]
+    cases i
+    · exact continuous_const
+    · exact continuous_apply _
+  · intros f g hfg
+    ext i
+    exact congr_fun hfg i
+
+variable {I}
+
+def e (μ : WithTop I) : LocallyConstant {i // i ∈ C} ℤ :=
+{ toFun := fun f ↦ BoolToZ (f.1 μ)
+  isLocallyConstant := by
+    rw [IsLocallyConstant.iff_continuous]
+    refine' @Continuous.comp _ _ _ _ _ _ BoolToZ _ continuous_of_discreteTopology _
+    refine' Continuous.comp (continuous_apply μ) _
+    continuity }
+
+def Products (I : Type _) [LinearOrder I] := {l : List I // l.Chain' (·>·)}
+
+noncomputable
+instance : LinearOrder (Products (WithTop I)) :=
+  inferInstanceAs (LinearOrder {l : List (WithTop I) // l.Chain' (·>·)})
+
+def Products.eval (l : Products (WithTop I)) := (l.1.map (e C)).prod
+
+def Products.isGood (l : Products (WithTop I)) : Prop :=
+  l.eval C ∉ Submodule.span ℤ ((Products.eval C) '' {m | m < l})
+
+def GoodProducts := {l : Products (WithTop I) // l.isGood C}
+
+def GoodProducts.eval (l : {l : Products (WithTop I) // l.isGood C}) :
+  LocallyConstant {i // i ∈ C} ℤ := Products.eval C l.1
+
+def Support : Set (WithTop I) := {i : WithTop I | ∃ f ∈ C, f i}
+
+def P (i : WithTop I) : Prop :=
+∀ C, IsClosed C → Support C ⊆ {j | j < i} → LinearIndependent ℤ (GoodProducts.eval C)
+
+def Q (i : WithTop I) : Prop :=
+∀ C, IsClosed C → Support C ⊆ {j | j < i} → ⊤ ≤ Submodule.span ℤ (Set.range (GoodProducts.eval C))
+
+instance : IsWellFounded (WithTop I) (·<·) := inferInstance
+
+lemma GoodProducts.linearIndependentAux (i : WithTop I) : P i := by
+  apply IsWellFounded.induction (·<·)
+  intro μ h
+  sorry
+
+lemma GoodProducts.spanAux (i : WithTop I) : Q i := sorry
+
+variable {C₁ : Set (I → Bool)}
+
+lemma isClosedInWithTop (hC₁ : IsClosed C₁) : IsClosed ((r I) '' C₁) :=
+(r.embedding I).toEmbedding.toInducing.isClosedMap (r.embedding I).closed_range C₁ hC₁
+
+lemma supportTop (C₁ : Set (I → Bool)) : Support ((r I) '' C₁) ⊆ {j | j < ⊤} := by
+  dsimp [Support]
+  intro i hi
+  obtain ⟨f, hf⟩ := hi
+  obtain ⟨c, hc⟩ := hf.1
+  rw [← hc.2] at hf
+  dsimp [r] at hf
+  cases i
+  · dsimp at hf
+    exfalso
+    exact Bool.not_false' hf.2
+  · dsimp at hf
+    dsimp
+    rw [← WithTop.none_eq_top]
+    exact WithTop.some_lt_none _
+
+lemma GoodProducts.linearIndependent (hC₁ : IsClosed C₁) :
+  LinearIndependent ℤ (GoodProducts.eval ((r I) '' C₁)) :=
+GoodProducts.linearIndependentAux ⊤ ((r I) '' C₁) (isClosedInWithTop hC₁) (supportTop C₁)
+
+lemma GoodProducts.span (hC₁ : IsClosed C₁) :
+  ⊤ ≤ Submodule.span ℤ (Set.range (GoodProducts.eval ((r I) '' C₁))) :=
+GoodProducts.spanAux ⊤ ((r I) '' C₁) (isClosedInWithTop hC₁) (supportTop C₁)
+
+noncomputable
+def GoodProducts.Basis (hC₁ : IsClosed C₁) : Basis (GoodProducts ((r I) '' C₁)) ℤ
+  (LocallyConstant {i // i ∈ ((r I) '' C₁)} ℤ) :=
+Basis.mk (GoodProducts.linearIndependent hC₁) (GoodProducts.span hC₁)
+
+lemma closedFree (hC₁ : IsClosed C₁) : Module.Free ℤ (LocallyConstant {i // i ∈ ((r I) '' C₁)} ℤ) :=
+Module.Free.of_basis $ GoodProducts.Basis hC₁
+
+variable {S : Profinite} {ι : S → I → Bool} (hι : ClosedEmbedding ι)
+
+noncomputable
+def homeoClosed₁ : S ≃ₜ Set.range ι := Homeomorph.ofEmbedding ι hι.toEmbedding
+
+def rι.embedding : ClosedEmbedding (r I ∘ ι) := ClosedEmbedding.comp (r.embedding I) hι
+
+noncomputable
+def homeoClosed₂ : S ≃ₜ Set.range (r I ∘ ι) :=
+Homeomorph.ofEmbedding (r I ∘ ι) (rι.embedding hι).toEmbedding
+
+def homeoRange : Set.range (r I ∘ ι) ≃ₜ r I '' Set.range ι := by
+  rw [Set.range_comp]
+  exact Homeomorph.refl _
+
+def setHomeoSubtype {X : Type _} [TopologicalSpace X] (s : Set X) : s ≃ₜ {x // x ∈ s} :=
+{ toFun := fun x ↦ ⟨x.val, x.prop⟩
+  invFun := fun x ↦ x
+  left_inv := by
+    intro x
+    dsimp
+  right_inv := by
+    intro x
+    dsimp }
+
+noncomputable
+def homeoClosed : S ≃ₜ { i // i ∈ r I '' Set.range ι } :=
+(homeoClosed₂ hι).trans (homeoRange.trans (setHomeoSubtype (r I '' Set.range ι)))
+
+noncomputable
+def locConstIso (hι : ClosedEmbedding ι) :
+  (LocallyConstant S ℤ) ≃ₗ[ℤ] (LocallyConstant { i // i ∈ r I '' Set.range ι } ℤ) :=
+LocallyConstant.equivLinear (homeoClosed hι)
+
+lemma Nobeling : Module.Free ℤ (LocallyConstant S ℤ) := Module.Free.of_equiv'
+  (closedFree hι.closed_range) (locConstIso hι).symm
+
+end NobelingProof
+
+variable (S : Profinite)
+
+open Classical
+
+noncomputable
+def Nobeling.ι : S → ({C : Set S // IsClopen C} → Bool) := fun s C => decide (s ∈ C.1)
+
+lemma Nobeling.embedding : ClosedEmbedding (Nobeling.ι S) := by
+  apply Continuous.closedEmbedding
+  · dsimp [ι]
+    refine' continuous_pi _
+    intro C
+    rw [← IsLocallyConstant.iff_continuous]
+    refine' ((IsLocallyConstant.tfae _).out 0 3).mpr _
+    rintro ⟨⟩
+    · have : (fun a ↦ decide (a ∈ C.1)) ⁻¹' {false} = C.1ᶜ
+      · ext x
+        simp only [Set.mem_preimage, Set.mem_singleton_iff,
+          decide_eq_false_iff_not, Set.mem_compl_iff]
+      · rw [this]
+        refine' IsClopen.isOpen _
+        simp only [isClopen_compl_iff]
+        exact C.2
+    · have : (fun a ↦ decide (a ∈ C.1)) ⁻¹' {true} = C.1
+      · ext x
+        simp only [Set.mem_preimage, Set.mem_singleton_iff, decide_eq_true_eq]
+      · rw [this]
+        refine' IsClopen.isOpen _
+        exact C.2
+  · intro a b hab
+    by_contra hnab
+    have h' := exists_clopen_of_totally_separated hnab
+    obtain ⟨C, hC, h₁⟩ := h'
+    apply h₁.2
+    have ha : ι S a ⟨C, hC⟩ = decide (a ∈ C) := rfl
+    have hb : ι S b ⟨C, hC⟩ = decide (b ∈ C) := rfl
+    apply of_decide_eq_true
+    rw [← hb, ← hab, ha]
+    apply decide_eq_true
+    exact h₁.1
+
+theorem Nobeling : Module.Free ℤ (LocallyConstant S ℤ) :=
+@NobelingProof.Nobeling {C : Set S // IsClopen C} (IsWellOrder.linearOrder WellOrderingRel)
+  WellOrderingRel.isWellOrder S (Nobeling.ι S) (Nobeling.embedding S)
