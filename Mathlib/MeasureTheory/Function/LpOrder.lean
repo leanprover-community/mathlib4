aee9460b2e4aaebdace2f1dc3afb70a9b23b4f05
Komyyy <pol_tta@outlook.jp>
2023-05-26 14:05:53 +0900

    fix

--- Mathlib/MeasureTheory/Function/LpOrder.lean
+++ Mathlib/MeasureTheory/Function/LpOrder.lean
@@ -16,15 +16,17 @@ import Mathlib.MeasureTheory.Function.LpSpace
 
 ### Results
 
-- `Lp E p μ` is an `ordered_add_comm_group` when `E` is a `normed_lattice_add_comm_group`.
+- `Lp E p μ` is an `OrderedAddCommGroup` when `E` is a `NormedLatticeAddCommGroup`.
 
 ### TODO
 
-- move definitions of `Lp.pos_part` and `Lp.neg_part` to this file, and define them as
-  `has_pos_part.pos` and `has_pos_part.neg` given by the lattice structure.
+- move definitions of `Lp.posPart` and `Lp.negPart` to this file, and define them as
+  `PosPart.pos` and `NegPart.neg` given by the lattice structure.
 
 -/
 
+
+set_option linter.uppercaseLean3 false
 
 open TopologicalSpace MeasureTheory LatticeOrderedCommGroup
 
@@ -40,81 +42,84 @@ section Order
 
 variable [NormedLatticeAddCommGroup E]
 
-theorem coeFn_le (f g : lp E p μ) : f ≤ᵐ[μ] g ↔ f ≤ g := by
-  rw [← Subtype.coe_le_coe, ← ae_eq_fun.coe_fn_le, ← coeFn_coeBase, ← coeFn_coeBase]
-#align measure_theory.Lp.coe_fn_le MeasureTheory.lp.coeFn_le
+theorem coeFn_le (f g : Lp E p μ) : f ≤ᵐ[μ] g ↔ f ≤ g := by
+  rw [← Subtype.coe_le_coe, ← AEEqFun.coeFn_le, Lp.cast, Lp.cast]
+#align measure_theory.Lp.coe_fn_le MeasureTheory.Lp.coeFn_le
 
-theorem coeFn_nonneg (f : lp E p μ) : 0 ≤ᵐ[μ] f ↔ 0 ≤ f := by
-  rw [← coe_fn_le]
-  have h0 := Lp.coe_fn_zero E p μ
-  constructor <;> intro h <;> filter_upwards [h, h0]with _ _ h2
+theorem coeFn_nonneg (f : Lp E p μ) : 0 ≤ᵐ[μ] f ↔ 0 ≤ f := by
+  rw [← coeFn_le]
+  have h0 := Lp.coeFn_zero E p μ
+  constructor <;> intro h <;> filter_upwards [h, h0] with _ _ h2
   · rwa [h2]
   · rwa [← h2]
-#align measure_theory.Lp.coe_fn_nonneg MeasureTheory.lp.coeFn_nonneg
+#align measure_theory.Lp.coe_fn_nonneg MeasureTheory.Lp.coeFn_nonneg
 
-instance : CovariantClass (lp E p μ) (lp E p μ) (· + ·) (· ≤ ·) := by
+instance instCovariantClassLE : CovariantClass (Lp E p μ) (Lp E p μ) (· + ·) (· ≤ ·) := by
   refine' ⟨fun f g₁ g₂ hg₁₂ => _⟩
-  rw [← coe_fn_le] at hg₁₂⊢
-  filter_upwards [coe_fn_add f g₁, coe_fn_add f g₂, hg₁₂]with _ h1 h2 h3
+  rw [← coeFn_le] at hg₁₂ ⊢
+  filter_upwards [coeFn_add f g₁, coeFn_add f g₂, hg₁₂] with _ h1 h2 h3
   rw [h1, h2, Pi.add_apply, Pi.add_apply]
   exact add_le_add le_rfl h3
+#align measure_theory.Lp.has_le.le.covariant_class MeasureTheory.Lp.instCovariantClassLE
 
-instance : OrderedAddCommGroup (lp E p μ) :=
+instance instOrderedAddCommGroup : OrderedAddCommGroup (Lp E p μ) :=
   { Subtype.partialOrder _, AddSubgroup.toAddCommGroup _ with
-    add_le_add_left := fun f g => add_le_add_left }
+    add_le_add_left := fun _ _ => add_le_add_left }
+#align measure_theory.Lp.ordered_add_comm_group MeasureTheory.Lp.instOrderedAddCommGroup
 
-theorem MeasureTheory.Memℒp.sup {f g : α → E} (hf : Memℒp f p μ) (hg : Memℒp g p μ) :
+theorem _root_.MeasureTheory.Memℒp.sup {f g : α → E} (hf : Memℒp f p μ) (hg : Memℒp g p μ) :
     Memℒp (f ⊔ g) p μ :=
   Memℒp.mono' (hf.norm.add hg.norm) (hf.1.sup hg.1)
     (Filter.eventually_of_forall fun x => norm_sup_le_add (f x) (g x))
 #align measure_theory.mem_ℒp.sup MeasureTheory.Memℒp.sup
 
-theorem MeasureTheory.Memℒp.inf {f g : α → E} (hf : Memℒp f p μ) (hg : Memℒp g p μ) :
+theorem _root_.MeasureTheory.Memℒp.inf {f g : α → E} (hf : Memℒp f p μ) (hg : Memℒp g p μ) :
     Memℒp (f ⊓ g) p μ :=
   Memℒp.mono' (hf.norm.add hg.norm) (hf.1.inf hg.1)
     (Filter.eventually_of_forall fun x => norm_inf_le_add (f x) (g x))
 #align measure_theory.mem_ℒp.inf MeasureTheory.Memℒp.inf
 
-theorem MeasureTheory.Memℒp.abs {f : α → E} (hf : Memℒp f p μ) : Memℒp (|f|) p μ :=
+theorem _root_.MeasureTheory.Memℒp.abs {f : α → E} (hf : Memℒp f p μ) : Memℒp (|f|) p μ :=
   hf.sup hf.neg
 #align measure_theory.mem_ℒp.abs MeasureTheory.Memℒp.abs
 
-instance : Lattice (lp E p μ) :=
+instance instLattice : Lattice (Lp E p μ) :=
   Subtype.lattice
     (fun f g hf hg => by
-      rw [mem_Lp_iff_mem_ℒp] at *
-      exact (mem_ℒp_congr_ae (ae_eq_fun.coe_fn_sup _ _)).mpr (hf.sup hg))
+      rw [mem_Lp_iff_memℒp] at *
+      exact (memℒp_congr_ae (AEEqFun.coeFn_sup _ _)).mpr (hf.sup hg))
     fun f g hf hg => by
-    rw [mem_Lp_iff_mem_ℒp] at *
-    exact (mem_ℒp_congr_ae (ae_eq_fun.coe_fn_inf _ _)).mpr (hf.inf hg)
+    rw [mem_Lp_iff_memℒp] at *
+    exact (memℒp_congr_ae (AEEqFun.coeFn_inf _ _)).mpr (hf.inf hg)
+#align measure_theory.Lp.lattice MeasureTheory.Lp.instLattice
 
-theorem coeFn_sup (f g : lp E p μ) : ⇑(f ⊔ g) =ᵐ[μ] ⇑f ⊔ ⇑g :=
+theorem coeFn_sup (f g : Lp E p μ) : ⇑(f ⊔ g) =ᵐ[μ] ⇑f ⊔ ⇑g :=
   AEEqFun.coeFn_sup _ _
-#align measure_theory.Lp.coe_fn_sup MeasureTheory.lp.coeFn_sup
+#align measure_theory.Lp.coe_fn_sup MeasureTheory.Lp.coeFn_sup
 
-theorem coeFn_inf (f g : lp E p μ) : ⇑(f ⊓ g) =ᵐ[μ] ⇑f ⊓ ⇑g :=
+theorem coeFn_inf (f g : Lp E p μ) : ⇑(f ⊓ g) =ᵐ[μ] ⇑f ⊓ ⇑g :=
   AEEqFun.coeFn_inf _ _
-#align measure_theory.Lp.coe_fn_inf MeasureTheory.lp.coeFn_inf
+#align measure_theory.Lp.coe_fn_inf MeasureTheory.Lp.coeFn_inf
 
-theorem coeFn_abs (f : lp E p μ) : ⇑(|f|) =ᵐ[μ] fun x => |f x| :=
+theorem coeFn_abs (f : Lp E p μ) : ⇑(|f|) =ᵐ[μ] fun x => |f x| :=
   AEEqFun.coeFn_abs _
-#align measure_theory.Lp.coe_fn_abs MeasureTheory.lp.coeFn_abs
+#align measure_theory.Lp.coe_fn_abs MeasureTheory.Lp.coeFn_abs
 
-noncomputable instance [Fact (1 ≤ p)] : NormedLatticeAddCommGroup (lp E p μ) :=
-  { lp.lattice,
-    lp.normedAddCommGroup with
+noncomputable instance instNormedLatticeAddCommGroup [Fact (1 ≤ p)] :
+    NormedLatticeAddCommGroup (Lp E p μ) :=
+  { Lp.instLattice, Lp.instNormedAddCommGroup with
     add_le_add_left := fun f g => add_le_add_left
     solid := fun f g hfg => by
-      rw [← coe_fn_le] at hfg
+      rw [← coeFn_le] at hfg
       simp_rw [Lp.norm_def, ENNReal.toReal_le_toReal (Lp.snorm_ne_top f) (Lp.snorm_ne_top g)]
       refine' snorm_mono_ae _
-      filter_upwards [hfg, Lp.coe_fn_abs f, Lp.coe_fn_abs g]with x hx hxf hxg
+      filter_upwards [hfg, Lp.coeFn_abs f, Lp.coeFn_abs g] with x hx hxf hxg
       rw [hxf, hxg] at hx
       exact HasSolidNorm.solid hx }
+#align measure_theory.Lp.normed_lattice_add_comm_group MeasureTheory.Lp.instNormedLatticeAddCommGroup
 
 end Order
 
 end Lp
 
 end MeasureTheory
-
