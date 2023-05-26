416ff470a4ee6e83802ff29898e9f0b66a0fa985
Jason Yuen <jason_yuen2007@hotmail.com>
2023-05-26 09:48:18 +0000

    feat: port Geometry.Euclidean.Inversion (#4375)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1488,6 +1488,7 @@ import Mathlib.FieldTheory.Separable
 import Mathlib.FieldTheory.SeparableDegree
 import Mathlib.FieldTheory.Subfield
 import Mathlib.FieldTheory.Tower
+import Mathlib.Geometry.Euclidean.Inversion
 import Mathlib.Geometry.Manifold.ChartedSpace
 import Mathlib.Geometry.Manifold.ConformalGroupoid
 import Mathlib.Geometry.Manifold.LocalInvariantProperties
