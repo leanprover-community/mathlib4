e18a7afa3ba315f91afe49a676a5b6a70cdffbf0
damiano <adomani@gmail.com>
2023-05-26 05:11:11 +0000

    feat: port RingTheory.IntegralDomain (#4362)
    Co-authored-by: Vierkantor <vierkantor@vierkantor.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -2032,6 +2032,7 @@ import Mathlib.RingTheory.Ideal.Quotient
 import Mathlib.RingTheory.Ideal.QuotientOperations
 import Mathlib.RingTheory.Int.Basic
 import Mathlib.RingTheory.IntegralClosure
+import Mathlib.RingTheory.IntegralDomain
 import Mathlib.RingTheory.IsTensorProduct
 import Mathlib.RingTheory.JacobsonIdeal
 import Mathlib.RingTheory.LaurentSeries
