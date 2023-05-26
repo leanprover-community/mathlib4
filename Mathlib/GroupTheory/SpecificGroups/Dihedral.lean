6eb8d4ad227c4036bf6283c8e320f54bf54e7724
int-y1 <jason_yuen2007@hotmail.com>
2023-05-25 20:03:10 -0700

    fix

--- Mathlib/GroupTheory/SpecificGroups/Dihedral.lean
+++ Mathlib/GroupTheory/SpecificGroups/Dihedral.lean
@@ -14,21 +14,21 @@ import Mathlib.GroupTheory.Exponent
 /-!
 # Dihedral Groups
 
-We define the dihedral groups `dihedral_group n`, with elements `r i` and `sr i` for `i : zmod n`.
+We define the dihedral groups `DihedralGroup n`, with elements `r i` and `sr i` for `i : ZMod n`.
 
-For `n ≠ 0`, `dihedral_group n` represents the symmetry group of the regular `n`-gon. `r i`
+For `n ≠ 0`, `DihedralGroup n` represents the symmetry group of the regular `n`-gon. `r i`
 represents the rotations of the `n`-gon by `2πi/n`, and `sr i` represents the reflections of the
-`n`-gon. `dihedral_group 0` corresponds to the infinite dihedral group.
+`n`-gon. `DihedralGroup 0` corresponds to the infinite dihedral group.
 -/
 
 
-/-- For `n ≠ 0`, `dihedral_group n` represents the symmetry group of the regular `n`-gon.
+/-- For `n ≠ 0`, `DihedralGroup n` represents the symmetry group of the regular `n`-gon.
 `r i` represents the rotations of the `n`-gon by `2πi/n`, and `sr i` represents the reflections of
-the `n`-gon. `dihedral_group 0` corresponds to the infinite dihedral group.
+the `n`-gon. `DihedralGroup 0` corresponds to the infinite dihedral group.
 -/
 inductive DihedralGroup (n : ℕ) : Type
-  | r : ZMod n → DihedralGroup
-  | sr : ZMod n → DihedralGroup
+  | r : ZMod n → DihedralGroup n
+  | sr : ZMod n → DihedralGroup n
   deriving DecidableEq
 #align dihedral_group DihedralGroup
 
@@ -43,13 +43,11 @@ private def mul : DihedralGroup n → DihedralGroup n → DihedralGroup n
   | r i, sr j => sr (j - i)
   | sr i, r j => sr (i + j)
   | sr i, sr j => r (j - i)
-#align dihedral_group.mul dihedral_group.mul
 
 /-- The identity `1` is the rotation by `0`.
 -/
 private def one : DihedralGroup n :=
   r 0
-#align dihedral_group.one dihedral_group.one
 
 instance : Inhabited (DihedralGroup n) :=
   ⟨one⟩
@@ -59,13 +57,12 @@ instance : Inhabited (DihedralGroup n) :=
 private def inv : DihedralGroup n → DihedralGroup n
   | r i => r (-i)
   | sr i => sr i
-#align dihedral_group.inv dihedral_group.inv
 
-/-- The group structure on `dihedral_group n`.
+/-- The group structure on `DihedralGroup n`.
 -/
 instance : Group (DihedralGroup n) where
   mul := mul
-  mul_assoc := by rintro (a | a) (b | b) (c | c) <;> simp only [mul] <;> ring
+  mul_assoc := by rintro (a | a) (b | b) (c | c) <;> simp only [(· * ·), mul] <;> ring_nf
   one := one
   one_mul := by
     rintro (a | a)
@@ -105,31 +102,28 @@ theorem one_def : (1 : DihedralGroup n) = r 0 :=
   rfl
 #align dihedral_group.one_def DihedralGroup.one_def
 
-private def fintype_helper : Sum (ZMod n) (ZMod n) ≃ DihedralGroup n where
-  invFun i :=
-    match i with
+private def fintypeHelper : Sum (ZMod n) (ZMod n) ≃ DihedralGroup n where
+  invFun i := match i with
     | r j => Sum.inl j
     | sr j => Sum.inr j
-  toFun i :=
-    match i with
+  toFun i := match i with
     | Sum.inl j => r j
     | Sum.inr j => sr j
   left_inv := by rintro (x | x) <;> rfl
   right_inv := by rintro (x | x) <;> rfl
-#align dihedral_group.fintype_helper dihedral_group.fintype_helper
 
-/-- If `0 < n`, then `dihedral_group n` is a finite group.
+/-- If `0 < n`, then `DihedralGroup n` is a finite group.
 -/
 instance [NeZero n] : Fintype (DihedralGroup n) :=
   Fintype.ofEquiv _ fintypeHelper
 
 instance : Nontrivial (DihedralGroup n) :=
-  ⟨⟨r 0, sr 0, by decide⟩⟩
+  ⟨⟨r 0, sr 0, by simp_rw [ne_eq]⟩⟩
 
-/-- If `0 < n`, then `dihedral_group n` has `2n` elements.
+/-- If `0 < n`, then `DihedralGroup n` has `2n` elements.
 -/
 theorem card [NeZero n] : Fintype.card (DihedralGroup n) = 2 * n := by
-  rw [← fintype.card_eq.mpr ⟨fintype_helper⟩, Fintype.card_sum, ZMod.card, two_mul]
+  rw [← Fintype.card_eq.mpr ⟨fintypeHelper⟩, Fintype.card_sum, ZMod.card, two_mul]
 #align dihedral_group.card DihedralGroup.card
 
 @[simp]
@@ -158,10 +152,9 @@ theorem sr_mul_self (i : ZMod n) : sr i * sr i = 1 := by rw [sr_mul_sr, sub_self
 -/
 @[simp]
 theorem orderOf_sr (i : ZMod n) : orderOf (sr i) = 2 := by
-  rw [orderOf_eq_prime _ _]
-  · exact ⟨Nat.prime_two⟩
-  rw [sq, sr_mul_self]
-  decide
+  apply orderOf_eq_prime
+  · rw [sq, sr_mul_self]
+  · decide
 #align dihedral_group.order_of_sr DihedralGroup.orderOf_sr
 
 /-- If `0 < n`, then `r 1` has order `n`.
@@ -174,43 +167,39 @@ theorem orderOf_r_one : orderOf (r 1 : DihedralGroup n) = n := by
     rw [r_one_pow, one_def]
     apply mt r.inj
     simpa using hn.ne'
-  · skip
-    apply
-      (Nat.le_of_dvd (NeZero.pos n) <|
-            orderOf_dvd_of_pow_eq_one <| @r_one_pow_n n).lt_or_eq.resolve_left
+  · apply (Nat.le_of_dvd (NeZero.pos n) <|
+      orderOf_dvd_of_pow_eq_one <| @r_one_pow_n n).lt_or_eq.resolve_left
     intro h
     have h1 : (r 1 : DihedralGroup n) ^ orderOf (r 1) = 1 := pow_orderOf_eq_one _
     rw [r_one_pow] at h1
     injection h1 with h2
     rw [← ZMod.val_eq_zero, ZMod.val_nat_cast, Nat.mod_eq_of_lt h] at h2
-    exact absurd h2.symm (orderOf_pos _).Ne
+    exact absurd h2.symm (orderOf_pos _).ne
 #align dihedral_group.order_of_r_one DihedralGroup.orderOf_r_one
 
-/-- If `0 < n`, then `i : zmod n` has order `n / gcd n i`.
+/-- If `0 < n`, then `i : ZMod n` has order `n / gcd n i`.
 -/
 theorem orderOf_r [NeZero n] (i : ZMod n) : orderOf (r i) = n / Nat.gcd n i.val := by
   conv_lhs => rw [← ZMod.nat_cast_zmod_val i]
-  rw [← r_one_pow, orderOf_pow, order_of_r_one]
+  rw [← r_one_pow, orderOf_pow, orderOf_r_one]
 #align dihedral_group.order_of_r DihedralGroup.orderOf_r
 
 theorem exponent : Monoid.exponent (DihedralGroup n) = lcm n 2 := by
   rcases eq_zero_or_neZero n with (rfl | hn)
-  · exact Monoid.exponent_eq_zero_of_order_zero order_of_r_one
-  skip
+  · exact Monoid.exponent_eq_zero_of_order_zero orderOf_r_one
   apply Nat.dvd_antisymm
   · apply Monoid.exponent_dvd_of_forall_pow_eq_one
     rintro (m | m)
-    · rw [← orderOf_dvd_iff_pow_eq_one, order_of_r]
+    · rw [← orderOf_dvd_iff_pow_eq_one, orderOf_r]
       refine' Nat.dvd_trans ⟨gcd n m.val, _⟩ (dvd_lcm_left n 2)
       · exact (Nat.div_mul_cancel (Nat.gcd_dvd_left n m.val)).symm
-    · rw [← orderOf_dvd_iff_pow_eq_one, order_of_sr]
+    · rw [← orderOf_dvd_iff_pow_eq_one, orderOf_sr]
       exact dvd_lcm_right n 2
   · apply lcm_dvd
-    · convert Monoid.order_dvd_exponent (r 1)
-      exact order_of_r_one.symm
-    · convert Monoid.order_dvd_exponent (sr 0)
-      exact (order_of_sr 0).symm
+    · convert Monoid.order_dvd_exponent (r (1 : ZMod n))
+      exact orderOf_r_one.symm
+    · convert Monoid.order_dvd_exponent (sr (0 : ZMod n))
+      exact (orderOf_sr 0).symm
 #align dihedral_group.exponent DihedralGroup.exponent
 
 end DihedralGroup
-
