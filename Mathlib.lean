6cba052f0d22ed5659134eb7648396d8f50ba509
Jeremy Tan Jie Rui <reddeloostw@gmail.com>
2023-05-26 17:27:59 +0000

    feat: port Geometry.Euclidean.Angle.Unoriented.Conformal (#4397)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1493,6 +1493,7 @@ import Mathlib.FieldTheory.SeparableDegree
 import Mathlib.FieldTheory.Subfield
 import Mathlib.FieldTheory.Tower
 import Mathlib.Geometry.Euclidean.Angle.Unoriented.Basic
+import Mathlib.Geometry.Euclidean.Angle.Unoriented.Conformal
 import Mathlib.Geometry.Euclidean.Inversion
 import Mathlib.Geometry.Manifold.ChartedSpace
 import Mathlib.Geometry.Manifold.ConformalGroupoid
