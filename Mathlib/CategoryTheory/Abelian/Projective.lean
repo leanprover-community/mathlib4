a392307ead195abd2dd870092778145e5f80da5b
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2023-05-25 23:02:55 -0600

    nearly there but ran into timeout

--- Mathlib/CategoryTheory/Abelian/Projective.lean
+++ Mathlib/CategoryTheory/Abelian/Projective.lean
@@ -45,17 +45,22 @@ theorem exact_d_f [EnoughProjectives C] {X Y : C} (f : X ⟶ Y) : Exact (d f) f 
     ⟨by simp, zero_of_epi_comp (π _) <| by rw [← Category.assoc, cokernel.condition]⟩
 #align category_theory.exact_d_f CategoryTheory.exact_d_f
 
+-- TODO -- mention linter slowness on Zulip have (slow)/haveI (quick)
 /-- The preadditive Co-Yoneda functor on `P` preserves colimits if `P` is projective. -/
 def preservesFiniteColimitsPreadditiveCoyonedaObjOfProjective (P : C) [hP : Projective P] :
     PreservesFiniteColimits (preadditiveCoyonedaObj (op P)) := by
-  letI := (projective_iff_preservesEpimorphisms_preadditiveCoyoneda_obj' P).mp hP
-  apply functor.preserves_finite_colimits_of_preserves_epis_and_kernels
+  haveI := (projective_iff_preservesEpimorphisms_preadditiveCoyoneda_obj' P).mp hP
+  -- porting note: this next instance wasn't necessary in Lean 3
+  haveI := @Functor.preservesEpimorphisms_of_preserves_of_reflects _ _ _ _ _ _ _ _ this _
+  apply Functor.preservesFiniteColimitsOfPreservesEpisAndKernels
 #align category_theory.preserves_finite_colimits_preadditive_coyoneda_obj_of_projective CategoryTheory.preservesFiniteColimitsPreadditiveCoyonedaObjOfProjective
 
 /-- An object is projective if its preadditive Co-Yoneda functor preserves finite colimits. -/
 theorem projective_of_preservesFiniteColimits_preadditiveCoyonedaObj (P : C)
     [hP : PreservesFiniteColimits (preadditiveCoyonedaObj (op P))] : Projective P := by
   rw [projective_iff_preservesEpimorphisms_preadditiveCoyoneda_obj']
+  -- porting note: this next line wasn't necessary in Lean 3
+  dsimp only [preadditiveCoyoneda]
   infer_instance
 #align category_theory.projective_of_preserves_finite_colimits_preadditive_coyoneda_obj CategoryTheory.projective_of_preservesFiniteColimits_preadditiveCoyonedaObj
 
@@ -73,34 +78,42 @@ and the map to the `n`-th object as `projective.d`.
 
 variable [EnoughProjectives C]
 
+/-
+Porting note:
+compilation of CategoryTheory.ProjectiveResolution.ofComplex took 4.95s
+-/
 /-- Auxiliary definition for `ProjectiveResolution.of`. -/
-@[simps]
+--@[simps!]
 def ofComplex (Z : C) : ChainComplex C ℕ :=
   ChainComplex.mk' (Projective.over Z) (Projective.syzygies (Projective.π Z))
-    (Projective.d (Projective.π Z)) fun ⟨X, Y, f⟩ =>
+    (Projective.d (Projective.π Z)) fun ⟨_, _, f⟩ =>
     ⟨Projective.syzygies f, Projective.d f, (exact_d_f f).w⟩
+set_option linter.uppercaseLean3 false in
 #align category_theory.ProjectiveResolution.of_complex CategoryTheory.ProjectiveResolution.ofComplex
 
 /-- In any abelian category with enough projectives,
 `ProjectiveResolution.of Z` constructs a projective resolution of the object `Z`.
 -/
-irreducible_def of (Z : C) : ProjectiveResolution Z :=
+irreducible_def of (Z : C) : ProjectiveResolution Z := sorry #exit
   { complex := ofComplex Z
-    π :=
-      ChainComplex.mkHom _ _ (Projective.π Z) 0
-        (by
-          simp
-          exact (exact_d_f (projective.π Z)).w.symm)
-        fun n _ => ⟨0, by ext⟩
-    Projective := by rintro (_ | _ | _ | n) <;> apply projective.projective_over
-    exact₀ := by simpa using exact_d_f (projective.π Z)
-    exact := by
-      rintro (_ | n) <;>
-        · simp
-          apply exact_d_f
-    Epi := Projective.π_epi Z }
+    π := sorry
+--      ChainComplex.mkHom _ _ (Projective.π Z) 0
+--        (by
+--          simp
+--          exact (exact_d_f (projective.π Z)).w.symm)
+--        fun n _ => ⟨0, by ext⟩
+    projective := sorry--by rintro (_ | _ | _ | n) <;> apply projective.projective_over
+    exact₀ := sorry--by simpa using exact_d_f (projective.π Z)
+    exact := sorry
+    -- by
+    --   rintro (_ | n) <;>
+    --     · simp
+    --       apply exact_d_f
+    epi := sorry }
+    --Projective.π_epi Z }
 #align category_theory.ProjectiveResolution.of CategoryTheory.ProjectiveResolution.of
 
+#exit
 instance (priority := 100) (Z : C) : HasProjectiveResolution Z where out := ⟨of Z⟩
 
 instance (priority := 100) : HasProjectiveResolutions C where out Z := by infer_instance
