b0f396b2dfd29bc9fcb64c2727897c76691efab7
Scott Morrison <scott.morrison@gmail.com>
2023-05-26 11:37:04 -0600

    fix proofs

--- Mathlib/CategoryTheory/Abelian/Ext.lean
+++ Mathlib/CategoryTheory/Abelian/Ext.lean
@@ -8,7 +8,7 @@ Authors: Scott Morrison, Adam Topaz
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
-import Mathlib.Algebra.Category.Module.Abelian
+import Mathlib.Algebra.Category.ModuleCat.Abelian
 import Mathlib.CategoryTheory.Functor.LeftDerived
 import Mathlib.CategoryTheory.Linear.Yoneda
 import Mathlib.CategoryTheory.Abelian.Opposite
@@ -38,47 +38,33 @@ open CategoryTheory
 variable (R : Type _) [Ring R] (C : Type _) [Category C] [Abelian C] [Linear R C]
   [EnoughProjectives C]
 
-/- warning: Ext -> ext is a dubious translation:
-lean 3 declaration is
-  forall (R : Type.{u_1}) [_inst_1 : Ring.{u_1} R] (C : Type.{u_2}) [_inst_2 : CategoryTheory.Category.{u_3, u_2} C] [_inst_3 : CategoryTheory.Abelian.{u_3, u_2} C _inst_2] [_inst_4 : CategoryTheory.Linear.{u_1, u_3, u_2} R (Ring.toSemiring.{u_1} R _inst_1) C _inst_2 (CategoryTheory.Abelian.toPreadditive.{u_3, u_2} C _inst_2 _inst_3)] [_inst_5 : CategoryTheory.EnoughProjectives.{u_3, u_2} C _inst_2], Nat -> (CategoryTheory.Functor.{u_3, max u_2 u_3, u_2, max u_3 u_2 u_1 (succ u_3)} (Opposite.{succ u_2} C) (CategoryTheory.Category.opposite.{u_3, u_2} C _inst_2) (CategoryTheory.Functor.{u_3, u_3, u_2, max u_1 (succ u_3)} C _inst_2 (ModuleCat.{u_3, u_1} R _inst_1) (ModuleCat.moduleCategory.{u_3, u_1} R _inst_1)) (CategoryTheory.Functor.category.{u_3, u_3, u_2, max u_1 (succ u_3)} C _inst_2 (ModuleCat.{u_3, u_1} R _inst_1) (ModuleCat.moduleCategory.{u_3, u_1} R _inst_1)))
-but is expected to have type
-  forall {R : Type.{u_1}} [_inst_1 : Semiring.{u_1} R] {C : LaurentPolynomial.{u_1} R _inst_1} {_inst_2 : LaurentPolynomial.{u_1} R _inst_1}, (forall (a : Int), Eq.{succ u_1} ((fun (x._@.Mathlib.Data.Finsupp.Defs._hyg.779 : Int) => R) a) (FunLike.coe.{succ u_1, 1, succ u_1} (Finsupp.{0, u_1} Int R (MonoidWithZero.toZero.{u_1} R (Semiring.toMonoidWithZero.{u_1} R _inst_1))) Int (fun (a : Int) => (fun (x._@.Mathlib.Data.Finsupp.Defs._hyg.779 : Int) => R) a) (Finsupp.funLike.{0, u_1} Int R (MonoidWithZero.toZero.{u_1} R (Semiring.toMonoidWithZero.{u_1} R _inst_1))) C a) (FunLike.coe.{succ u_1, 1, succ u_1} (Finsupp.{0, u_1} Int R (MonoidWithZero.toZero.{u_1} R (Semiring.toMonoidWithZero.{u_1} R _inst_1))) Int (fun (a : Int) => (fun (x._@.Mathlib.Data.Finsupp.Defs._hyg.779 : Int) => R) a) (Finsupp.funLike.{0, u_1} Int R (MonoidWithZero.toZero.{u_1} R (Semiring.toMonoidWithZero.{u_1} R _inst_1))) _inst_2 a)) -> (Eq.{succ u_1} (LaurentPolynomial.{u_1} R _inst_1) C _inst_2)
-Case conversion may be inaccurate. Consider using '#align Ext extₓ'. -/
 /-- `Ext R C n` is defined by deriving in the first argument of `(X, Y) ↦ Module.of R (unop X ⟶ Y)`
 (which is the second argument of `linear_yoneda`).
 -/
-@[simps obj map]
-def ext (n : ℕ) : Cᵒᵖ ⥤ C ⥤ ModuleCat R :=
+-- Porting note: the mathlib4 proofs of `map_id` and `map_comp` were timing out,
+-- but `aesop_cat` is fast if we leave them out.
+@[simps! obj map]
+def Ext (n : ℕ) : Cᵒᵖ ⥤ C ⥤ ModuleCat R :=
   Functor.flip
     { obj := fun Y => (((linearYoneda R C).obj Y).rightOp.leftDerived n).leftOp
-      map := fun Y Y' f => (NatTrans.leftDerived ((linearYoneda R C).map f).rightOp n).leftOp
-      map_id' := by
-        intro X
-        ext Y : 2
-        dsimp only [nat_trans.id_app, nat_trans.left_op_app, nat_trans.right_op_app,
-          functor.left_op_obj, functor.right_op_obj]
-        rw [(linear_yoneda R C).map_id, ← unop_id, nat_trans.right_op_id, nat_trans.left_derived_id]
-        rfl
-      map_comp' := by
-        intro X Y Z f g
-        rw [(linear_yoneda R C).map_comp, nat_trans.right_op_comp, nat_trans.left_derived_comp]
-        rfl }
-#align Ext ext
+      -- Porting note: dot notation is not working for `NatTrans.leftOp` / `NatTrans.rightOp`
+      map := fun f =>
+        NatTrans.leftOp (NatTrans.leftDerived (NatTrans.rightOp ((linearYoneda R C).map f)) n) }
+set_option linter.uppercaseLean3 false in
+#align Ext Ext
 
 open ZeroObject
 
 /-- If `X : C` is projective and `n : ℕ`, then `Ext^(n + 1) X Y ≅ 0` for any `Y`. -/
 def extSuccOfProjective (X Y : C) [Projective X] (n : ℕ) :
-    ((ext R C (n + 1)).obj (Opposite.op X)).obj Y ≅ 0 :=
+    ((Ext R C (n + 1)).obj (Opposite.op X)).obj Y ≅ 0 :=
   let E := (((linearYoneda R C).obj Y).rightOp.leftDerivedObjProjectiveSucc n X).unop.symm
   E ≪≫
     { hom := 0
       inv := 0
-      hom_inv_id' := by
+      hom_inv_id := by
         let Z : (ModuleCat R)ᵒᵖ := 0
         rw [← (0 : 0 ⟶ Z.unop).unop_op, ← (0 : Z.unop ⟶ 0).unop_op, ← unop_id, ← unop_comp]
-        congr 1
-        dsimp
-        decide }
+        aesop }
+set_option linter.uppercaseLean3 false in
 #align Ext_succ_of_projective extSuccOfProjective
-
