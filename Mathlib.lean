314bfe2970cbd6020d5c871759caa9fbecb2c587
Apurva <apurvnakade@gmail.com>
2023-05-26 09:28:19 +0000

    feat: port Analysis.NormedSpace.WeakDual (#4351)

--- Mathlib.lean
+++ Mathlib.lean
@@ -529,6 +529,7 @@ import Mathlib.Analysis.NormedSpace.Star.Basic
 import Mathlib.Analysis.NormedSpace.Star.Mul
 import Mathlib.Analysis.NormedSpace.Star.Multiplier
 import Mathlib.Analysis.NormedSpace.Units
+import Mathlib.Analysis.NormedSpace.WeakDual
 import Mathlib.Analysis.PSeries
 import Mathlib.Analysis.Seminorm
 import Mathlib.Analysis.SpecialFunctions.Complex.Arg
