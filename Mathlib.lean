d3d280eb1e480f4b296716bc7b9ce9fc7b63d690
Jireh Loreaux <loreaujy@gmail.com>
2023-05-26 04:47:52 +0000

    feat: port Analysis.InnerProductSpace.Orthogonal (#4363)

--- Mathlib.lean
+++ Mathlib.lean
@@ -457,6 +457,7 @@ import Mathlib.Analysis.Convex.Topology
 import Mathlib.Analysis.Convex.Uniform
 import Mathlib.Analysis.Hofer
 import Mathlib.Analysis.InnerProductSpace.Basic
+import Mathlib.Analysis.InnerProductSpace.Orthogonal
 import Mathlib.Analysis.LocallyConvex.AbsConvex
 import Mathlib.Analysis.LocallyConvex.BalancedCoreHull
 import Mathlib.Analysis.LocallyConvex.Basic
