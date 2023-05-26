f7eb52d98f89362f463beeaaecdda17fce3dec34
Jeremy Tan Jie Rui <reddeloostw@gmail.com>
2023-05-26 09:02:04 +0000

    feat: port Algebra.Category.GroupCat.Abelian (#4380)

--- Mathlib.lean
+++ Mathlib.lean
@@ -30,6 +30,7 @@ import Mathlib.Algebra.BigOperators.Pi
 import Mathlib.Algebra.BigOperators.Ring
 import Mathlib.Algebra.BigOperators.RingEquiv
 import Mathlib.Algebra.Bounds
+import Mathlib.Algebra.Category.GroupCat.Abelian
 import Mathlib.Algebra.Category.GroupCat.Basic
 import Mathlib.Algebra.Category.GroupCat.Colimits
 import Mathlib.Algebra.Category.GroupCat.EpiMono
