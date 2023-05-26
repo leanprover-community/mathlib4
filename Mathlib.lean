598f6a6493bbbe896086e6c78bcf746473aeef9b
Jeremy Tan Jie Rui <reddeloostw@gmail.com>
2023-05-26 09:02:03 +0000

    feat: port Analysis.InnerProductSpace.ConformalLinearMap (#4378)

--- Mathlib.lean
+++ Mathlib.lean
@@ -460,6 +460,7 @@ import Mathlib.Analysis.Convex.Topology
 import Mathlib.Analysis.Convex.Uniform
 import Mathlib.Analysis.Hofer
 import Mathlib.Analysis.InnerProductSpace.Basic
+import Mathlib.Analysis.InnerProductSpace.ConformalLinearMap
 import Mathlib.Analysis.InnerProductSpace.Orthogonal
 import Mathlib.Analysis.InnerProductSpace.Symmetric
 import Mathlib.Analysis.LocallyConvex.AbsConvex
