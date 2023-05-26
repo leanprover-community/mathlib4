6ef633be46cedd4b7c246fc8e493fd15af81dd7d
Parcly Taxel <reddeloostw@gmail.com>
2023-05-26 14:12:35 +0800

    Fix errors

--- Mathlib/Analysis/InnerProductSpace/ConformalLinearMap.lean
+++ Mathlib/Analysis/InnerProductSpace/ConformalLinearMap.lean
@@ -37,15 +37,13 @@ theorem isConformalMap_iff (f : E →L[ℝ] F) :
   · rintro ⟨c₁, hc₁, li, rfl⟩
     refine' ⟨c₁ * c₁, mul_self_pos.2 hc₁, fun u v => _⟩
     simp only [real_inner_smul_left, real_inner_smul_right, mul_assoc, coe_smul',
-      coe_to_continuous_linear_map, Pi.smul_apply, inner_map_map]
+      coe_toContinuousLinearMap, Pi.smul_apply, inner_map_map]
   · rintro ⟨c₁, hc₁, huv⟩
     obtain ⟨c, hc, rfl⟩ : ∃ c : ℝ, 0 < c ∧ c₁ = c * c
     exact ⟨Real.sqrt c₁, Real.sqrt_pos.2 hc₁, (Real.mul_self_sqrt hc₁.le).symm⟩
     refine' ⟨c, hc.ne', (c⁻¹ • f : E →ₗ[ℝ] F).isometryOfInner fun u v => _, _⟩
-    ·
-      simp only [real_inner_smul_left, real_inner_smul_right, huv, mul_assoc, coe_smul,
+    · simp only [real_inner_smul_left, real_inner_smul_right, huv, mul_assoc, coe_smul,
         inv_mul_cancel_left₀ hc.ne', LinearMap.smul_apply, ContinuousLinearMap.coe_coe]
     · ext1 x
       exact (smul_inv_smul₀ hc.ne' (f x)).symm
 #align is_conformal_map_iff isConformalMap_iff
-
