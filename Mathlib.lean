d63fec9d48b0b6b9cf0aab9a697b5d94b533c068
Parcly Taxel <reddeloostw@gmail.com>
2023-05-27 02:22:47 +0800

    Fix errors

--- Mathlib.lean
+++ Mathlib.lean
@@ -30,13 +30,13 @@ import Mathlib.Algebra.BigOperators.Pi
 import Mathlib.Algebra.BigOperators.Ring
 import Mathlib.Algebra.BigOperators.RingEquiv
 import Mathlib.Algebra.Bounds
-import Mathlib.Algebra.Category.Group.Images
 import Mathlib.Algebra.Category.GroupCat.Abelian
 import Mathlib.Algebra.Category.GroupCat.Basic
 import Mathlib.Algebra.Category.GroupCat.Colimits
 import Mathlib.Algebra.Category.GroupCat.EpiMono
 import Mathlib.Algebra.Category.GroupCat.EquivalenceGroupAddGroup
 import Mathlib.Algebra.Category.GroupCat.FilteredColimits
+import Mathlib.Algebra.Category.GroupCat.Images
 import Mathlib.Algebra.Category.GroupCat.Injective
 import Mathlib.Algebra.Category.GroupCat.Limits
 import Mathlib.Algebra.Category.GroupCat.Preadditive
--- Mathlib/Algebra/Category/GroupCat/Images.lean
+++ Mathlib/Algebra/Category/GroupCat/Images.lean
@@ -8,14 +8,14 @@ Authors: Scott Morrison
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
-import Mathlib.Algebra.Category.Group.Abelian
+import Mathlib.Algebra.Category.GroupCat.Abelian
 import Mathlib.CategoryTheory.Limits.Shapes.Images
 
 /-!
 # The category of commutative additive groups has images.
 
 Note that we don't need to register any of the constructions here as instances, because we get them
-from the fact that `AddCommGroup` is an abelian category.
+from the fact that `AddCommGroupCat` is an abelian category.
 -/
 
 
@@ -27,6 +27,8 @@ universe u
 
 namespace AddCommGroupCat
 
+set_option linter.uppercaseLean3 false
+
 -- Note that because `injective_of_mono` is currently only proved in `Type 0`,
 -- we restrict to the lowest universe here for now.
 variable {G H : AddCommGroupCat.{0}} (f : G ⟶ H)
@@ -35,15 +37,15 @@ attribute [local ext] Subtype.ext_val
 
 section
 
--- implementation details of `has_image` for AddCommGroup; use the API, not these
-/-- the image of a morphism in AddCommGroup is just the bundling of `add_monoid_hom.range f` -/
+-- implementation details of `has_image` for `AddCommGroupCat`; use the API, not these
+/-- the image of a morphism in `AddCommGroupCat` is just the bundling of `AddMonoidHom.range f` -/
 def image : AddCommGroupCat :=
   AddCommGroupCat.of (AddMonoidHom.range f)
 #align AddCommGroup.image AddCommGroupCat.image
 
 /-- the inclusion of `image f` into the target -/
 def image.ι : image f ⟶ H :=
-  f.range.Subtype
+  f.range.subtype
 #align AddCommGroup.image.ι AddCommGroupCat.image.ι
 
 instance : Mono (image.ι f) :=
@@ -51,7 +53,7 @@ instance : Mono (image.ι f) :=
 
 /-- the corestriction map to the image -/
 def factorThruImage : G ⟶ image f :=
-  f.range_restrict
+  f.rangeRestrict
 #align AddCommGroup.factor_thru_image AddCommGroupCat.factorThruImage
 
 theorem image.fac : factorThruImage f ≫ image.ι f = f := by
@@ -64,8 +66,8 @@ attribute [local simp] image.fac
 variable {f}
 
 /-- the universal property for the image factorisation -/
-noncomputable def image.lift (F' : MonoFactorisation f) : image f ⟶ F'.i where
-  toFun := (fun x => F'.e (Classical.indefiniteDescription _ x.2).1 : image f → F'.i)
+noncomputable def image.lift (F' : MonoFactorisation f) : image f ⟶ F'.I where
+  toFun := (fun x => F'.e (Classical.indefiniteDescription _ x.2).1 : image f → F'.I)
   map_zero' := by
     haveI := F'.m_mono
     apply injective_of_mono F'.m
@@ -94,21 +96,21 @@ theorem image.lift_fac (F' : MonoFactorisation f) : image.lift F' ≫ F'.m = ima
 
 end
 
-/-- the factorisation of any morphism in AddCommGroup through a mono. -/
+/-- the factorisation of any morphism in `AddCommGroupCat` through a mono. -/
 def monoFactorisation : MonoFactorisation f where
-  i := image f
+  I := image f
   m := image.ι f
   e := factorThruImage f
 #align AddCommGroup.mono_factorisation AddCommGroupCat.monoFactorisation
 
-/-- the factorisation of any morphism in AddCommGroup through a mono has the universal property of
-the image. -/
+/-- the factorisation of any morphism in `AddCommGroupCat` through a mono has
+the universal property of the image. -/
 noncomputable def isImage : IsImage (monoFactorisation f) where
   lift := image.lift
   lift_fac := image.lift_fac
 #align AddCommGroup.is_image AddCommGroupCat.isImage
 
-/-- The categorical image of a morphism in `AddCommGroup`
+/-- The categorical image of a morphism in `AddCommGroupCat`
 agrees with the usual group-theoretical range.
 -/
 noncomputable def imageIsoRange {G H : AddCommGroupCat.{0}} (f : G ⟶ H) :
@@ -117,4 +119,3 @@ noncomputable def imageIsoRange {G H : AddCommGroupCat.{0}} (f : G ⟶ H) :
 #align AddCommGroup.image_iso_range AddCommGroupCat.imageIsoRange
 
 end AddCommGroupCat
-
