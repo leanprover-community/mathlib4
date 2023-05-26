4e6f57457c50c0139bd663438c2aca9ef63888ea
Jason Yuen <jason_yuen2007@hotmail.com>
2023-05-26 09:02:06 +0000

    feat: port Algebra.Category.Module.Images (#4385)

--- Mathlib.lean
+++ Mathlib.lean
@@ -45,6 +45,7 @@ import Mathlib.Algebra.Category.GroupWithZeroCat
 import Mathlib.Algebra.Category.ModuleCat.Abelian
 import Mathlib.Algebra.Category.ModuleCat.Basic
 import Mathlib.Algebra.Category.ModuleCat.EpiMono
+import Mathlib.Algebra.Category.ModuleCat.Images
 import Mathlib.Algebra.Category.ModuleCat.Kernels
 import Mathlib.Algebra.Category.ModuleCat.Limits
 import Mathlib.Algebra.Category.ModuleCat.Products
