03406ab8ad29a94fa5b58c5b3a1dbca61227278b
Apurva <apurvnakade@gmail.com>
2023-05-27 13:11:52 +0000

    feat: port Analysis.Convex.Cone.Dual (#4410)

--- Mathlib.lean
+++ Mathlib.lean
@@ -439,6 +439,7 @@ import Mathlib.Analysis.Convex.Caratheodory
 import Mathlib.Analysis.Convex.Combination
 import Mathlib.Analysis.Convex.Complex
 import Mathlib.Analysis.Convex.Cone.Basic
+import Mathlib.Analysis.Convex.Cone.Dual
 import Mathlib.Analysis.Convex.Contractible
 import Mathlib.Analysis.Convex.Exposed
 import Mathlib.Analysis.Convex.Extrema
