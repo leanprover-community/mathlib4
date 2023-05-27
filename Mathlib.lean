9b8711f09e8b3ffe3f9cf94fa8d0aef08294c620
Matthew Robert Ballard <matt@mrb.email>
2023-05-27 20:20:49 +0000

    feat: port Algebra.Category.ModuleCat.Monoidal.Basic (#4368)

--- Mathlib.lean
+++ Mathlib.lean
@@ -51,6 +51,7 @@ import Mathlib.Algebra.Category.ModuleCat.EpiMono
 import Mathlib.Algebra.Category.ModuleCat.Images
 import Mathlib.Algebra.Category.ModuleCat.Kernels
 import Mathlib.Algebra.Category.ModuleCat.Limits
+import Mathlib.Algebra.Category.ModuleCat.Monoidal.Basic
 import Mathlib.Algebra.Category.ModuleCat.Products
 import Mathlib.Algebra.Category.ModuleCat.Projective
 import Mathlib.Algebra.Category.ModuleCat.Tannaka
