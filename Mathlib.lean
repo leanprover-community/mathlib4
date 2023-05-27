5c0e233a83ef0df91f29b1b1379aedfad7531b57
Jujian Zhang <jujian.zhang1998@outlook.com>
2023-05-27 13:11:50 +0000

    feat: port Topology.Sheaves.Forget (#4407)

--- Mathlib.lean
+++ Mathlib.lean
@@ -2485,6 +2485,7 @@ import Mathlib.Topology.Sets.Compacts
 import Mathlib.Topology.Sets.Opens
 import Mathlib.Topology.Sets.Order
 import Mathlib.Topology.Sheaves.Abelian
+import Mathlib.Topology.Sheaves.Forget
 import Mathlib.Topology.Sheaves.Functors
 import Mathlib.Topology.Sheaves.Init
 import Mathlib.Topology.Sheaves.Limits
