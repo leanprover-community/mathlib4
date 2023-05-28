c684afcb8c2928973782284052d11a6c18e368dc
yuma-mizuno <mizuno.y.aj@gmail.com>
2023-05-28 21:30:20 +0900

    fix comments

--- Mathlib/Topology/Sheaves/Stalks.lean
+++ Mathlib/Topology/Sheaves/Stalks.lean
@@ -24,21 +24,21 @@ import Mathlib.CategoryTheory.Sites.Pushforward
 
 For a presheaf `F` on a topological space `X`, valued in some category `C`, the *stalk* of `F`
 at the point `x : X` is defined as the colimit of the composition of the inclusion of categories
-`(nhds x)ᵒᵖ ⥤ (opens X)ᵒᵖ` and the functor `F : (opens X)ᵒᵖ ⥤ C`.
+`(OpenNhds x)ᵒᵖ ⥤ (Opens X)ᵒᵖ` and the functor `F : (Opens X)ᵒᵖ ⥤ C`.
 For an open neighborhood `U` of `x`, we define the map `F.germ x : F.obj (op U) ⟶ F.stalk x` as the
 canonical morphism into this colimit.
 
-Taking stalks is functorial: For every point `x : X` we define a functor `stalk_functor C x`,
+Taking stalks is functorial: For every point `x : X` we define a functor `stalkFunctor C x`,
 sending presheaves on `X` to objects of `C`. Furthermore, for a map `f : X ⟶ Y` between
 topological spaces, we define `stalkPushforward` as the induced map on the stalks
 `(f _* ℱ).stalk (f x) ⟶ ℱ.stalk x`.
 
 Some lemmas about stalks and germs only hold for certain classes of concrete categories. A basic
-property of forgetful functors of categories of algebraic structures (like `Mon`, `CommRing`,...)
-is that they preserve filtered colimits. Since stalks are filtered colimits, this ensures that
-the stalks of presheaves valued in these categories behave exactly as for `Type`-valued presheaves.
-For example, in `germ_exist` we prove that in such a category, every element of the stalk is the
-germ of a section.
+property of forgetful functors of categories of algebraic structures (like `MonCat`,
+`CommRingCat`,...) is that they preserve filtered colimits. Since stalks are filtered colimits,
+this ensures that the stalks of presheaves valued in these categories behave exactly as for
+`Type`-valued presheaves. For example, in `germ_exist` we prove that in such a category, every
+element of the stalk is the germ of a section.
 
 Furthermore, if we require the forgetful functor to reflect isomorphisms and preserve limits (as
 is the case for most algebraic structures), we have access to the unique gluing API and can prove
@@ -617,7 +617,7 @@ theorem app_isIso_of_stalkFunctor_map_iso {F G : Sheaf C X} (f : F ⟶ G) (U : O
 set_option linter.uppercaseLean3 false in
 #align Top.presheaf.app_is_iso_of_stalk_functor_map_iso TopCat.Presheaf.app_isIso_of_stalkFunctor_map_iso
 
--- Making this an instance would cause a loop in typeclass resolution with `functor.map_is_iso`
+-- Making this an instance would cause a loop in typeclass resolution with `Functor.map_isIso`
 /-- Let `F` and `G` be sheaves valued in a concrete category, whose forgetful functor reflects
 isomorphisms, preserves limits and filtered colimits. Then if the stalk maps of a morphism
 `f : F ⟶ G` are all isomorphisms, `f` must be an isomorphism.
