4052cafda786719b6a362525d89e612b6f7b9950
Yury G. Kudryashov <urkud@urkud.name>
2023-05-28 23:39:46 +0000

    feat: port Analysis.Calculus.Deriv.Inverse (#4438)

--- Mathlib.lean
+++ Mathlib.lean
@@ -418,6 +418,7 @@ import Mathlib.Analysis.Calculus.Conformal.NormedSpace
 import Mathlib.Analysis.Calculus.Deriv.Add
 import Mathlib.Analysis.Calculus.Deriv.Basic
 import Mathlib.Analysis.Calculus.Deriv.Comp
+import Mathlib.Analysis.Calculus.Deriv.Inverse
 import Mathlib.Analysis.Calculus.Deriv.Linear
 import Mathlib.Analysis.Calculus.Deriv.Mul
 import Mathlib.Analysis.Calculus.Deriv.Prod
