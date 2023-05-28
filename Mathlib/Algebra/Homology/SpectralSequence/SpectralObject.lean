db9ed601fed333c68749d344931dfcd9322e0726
Joël Riou <joel.riou@universite-paris-saclay.fr>
2023-05-28 11:59:39 +0200

    some progress

--- Mathlib/Algebra/Homology/SpectralSequence/SpectralObject.lean
+++ Mathlib/Algebra/Homology/SpectralSequence/SpectralObject.lean
@@ -1,4 +1,6 @@
+import Mathlib.CategoryTheory.Limits.FunctorCategory
 import Mathlib.Algebra.Homology.ShortComplex.Abelian
+import Mathlib.Algebra.Homology.ShortComplex.PreservesHomology
 import Mathlib.Algebra.Homology.ShortComplex.Exact
 import Mathlib.CategoryTheory.ArrowThree
 import Mathlib.Tactic.Linarith
@@ -32,27 +34,47 @@ namespace SpectralObject
 variable {C ι}
 variable (X : SpectralObject C ι) (n₀ n₁ n₂ : ℤ) (hn₁ : n₀ + 1 = n₁) (hn₂ : n₁ + 1 = n₂)
 
--- the homology of these short complexes gives the terms in all the pages of the spectral sequence
-@[simps]
-def shortComplexE' : Arrow₃ ι ⥤ ShortComplex C where
-  obj D := ShortComplex.mk ((X.δ n₀ n₁ hn₁).app (Arrow₃.δ₀.obj D))
-    ((X.δ n₁ n₂ hn₂).app (Arrow₃.δ₃.obj D)) (by
-      have eq := (X.δ n₁ n₂ hn₂).naturality (Arrow₃.δ₃Toδ₂.app D)
-      dsimp at eq
-      simp only [Arrow₃.δ₂_map_δ₃Toδ₂_app, Arrow₂.δ₂_obj, Arrow₃.δ₃_obj_f,
-        Functor.map_id, comp_id] at eq
-      rw [← eq, Arrow₃.δ₀_map_δ₃Toδ₂_app_eq_δ₂Toδ₁_app_δ₀_obj,
-        reassoc_of% (X.zero₁ n₀ n₁ hn₁ (Arrow₃.δ₀.obj D)), zero_comp])
-  map φ :=
-    { τ₁ := (X.H n₀).map (Arrow₃.hMor.map φ)
-      τ₂ := (X.H n₁).map (Arrow₃.gMor.map φ)
-      τ₃ := (X.H n₂).map (Arrow₃.fMor.map φ)
-      comm₁₂ := (X.δ n₀ n₁ hn₁).naturality (Arrow₃.δ₀.map φ)
-      comm₂₃ := (X.δ n₁ n₂ hn₂).naturality (Arrow₃.δ₃.map φ) }
-  map_id _ := by ext <;> dsimp <;> simp
-  map_comp _ _ := by ext <;> dsimp <;> simp
+-- the homology of this short complex gives the terms in all the pages of the spectral sequence
+def shortComplexE' : ShortComplex (Arrow₃ ι ⥤ C) where
+  X₁ := Arrow₃.hMor ⋙ X.H n₀
+  X₂ := Arrow₃.gMor ⋙ X.H n₁
+  X₃ := Arrow₃.fMor ⋙ X.H n₂
+  f := whiskerLeft (Arrow₃.δ₀) (X.δ n₀ n₁ hn₁)
+  g := whiskerLeft (Arrow₃.δ₃) (X.δ n₁ n₂ hn₂)
+  zero := by
+    ext D
+    have eq := (X.δ n₁ n₂ hn₂).naturality (Arrow₃.δ₃Toδ₂.app D)
+    dsimp at eq ⊢
+    simp only [Arrow₃.δ₂_map_δ₃Toδ₂_app, Arrow₂.δ₂_obj, Arrow₃.δ₃_obj_f,
+      Functor.map_id, comp_id] at eq
+    rw [← eq, Arrow₃.δ₀_map_δ₃Toδ₂_app_eq_δ₂Toδ₁_app_δ₀_obj,
+      reassoc_of% (X.zero₁ n₀ n₁ hn₁ (Arrow₃.δ₀.obj D)), zero_comp]
 
-noncomputable def E := X.shortComplexE' n₀ n₁ n₂ hn₁ hn₂ ⋙ ShortComplex.homologyFunctor C
+section
+
+variable (n₀ n₁ n₂ : ℤ) (h : n₀ + 1 = n₁) (h' : n₁ + 1 = n₂) (D : Arrow₂ ι)
+
+noncomputable def cycles : Arrow₂ ι ⥤ C := kernel (X.δ n₀ n₁ h)
+noncomputable def cyclesCo : Arrow₂ ι ⥤ C := cokernel (X.δ n₀ n₁ h)
+
+noncomputable def iCycles : X.cycles n₀ n₁ h ⟶ Arrow₂.δ₀ ⋙ X.H n₀ := kernel.ι _
+noncomputable def pCyclesCo : Arrow₂.δ₂ ⋙ X.H n₁ ⟶ X.cyclesCo n₀ n₁ h := cokernel.π _
+
+lemma iCycles_δ_app : (X.iCycles n₀ n₁ h).app D ≫ (X.δ n₀ n₁ h).app D = 0 := by
+  simp only [← NatTrans.comp_app, iCycles, kernel.condition, zero_app]
+
+lemma δ_pCyclesCo_app : (X.δ n₀ n₁ h).app D ≫ (X.pCyclesCo n₀ n₁ h).app D  = 0 := by
+  simp only [← NatTrans.comp_app, pCyclesCo, cokernel.condition, zero_app]
+
+noncomputable def isLimitCycles (D : Arrow₂ ι) :
+    IsLimit (KernelFork.ofι _ (X.iCycles_δ_app n₀ n₁ h D)) :=
+  KernelFork.mapIsLimit _ (kernelIsKernel (X.δ n₀ n₁ h)) ((evaluation _ _).obj D)
+
+noncomputable def isColimitCyclesCo (D : Arrow₂ ι) :
+    IsColimit (CokernelCofork.ofπ _ (X.δ_pCyclesCo_app n₀ n₁ h D)) :=
+  CokernelCofork.mapIsColimit _ (cokernelIsCokernel (X.δ n₀ n₁ h)) ((evaluation _ _).obj D)
+
+end
 
 end SpectralObject
 
