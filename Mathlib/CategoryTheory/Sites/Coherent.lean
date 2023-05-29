0aa525050c6f0d757e961eacd4b00115c2486c2b
adamtopaz <github@adamtopaz.com>
2023-05-29 11:26:01 -0600

    rename

--- Mathlib/CategoryTheory/Sites/Coherent.lean
+++ Mathlib/CategoryTheory/Sites/Coherent.lean
@@ -17,8 +17,8 @@ def Sieve.EffectiveEpimorphic {X : C} (S : Sieve X) : Prop :=
 
 variable (C)
 
-class HasCoherentCoverage : Prop where
-  condition {B₁ B₂ : C} (f : B₂ ⟶ B₁) :
+class Precoherent : Prop where
+  cond {B₁ B₂ : C} (f : B₂ ⟶ B₁) :
     ∀ (α : Type) [Fintype α] (X₁ : α → C) (π₁ : (a : α) → (X₁ a ⟶ B₁)),
       (Sieve.generate (Presieve.ofArrows X₁ π₁)).EffectiveEpimorphic →
     ∃ (β : Type) (_ : Fintype β) (X₂ : β → C) (π₂ : (b : β) → (X₂ b ⟶ B₂)),
@@ -26,20 +26,20 @@ class HasCoherentCoverage : Prop where
       ∃ (i : β → α) (ι : (b :  β) → (X₂ b ⟶ X₁ (i b))),
       ∀ (b : β), ι b ≫ π₁ _ = π₂ _ ≫ f
 
-def CoherentCoverage [HasCoherentCoverage C] : Coverage C where
+def CoherentCoverage [Precoherent C] : Coverage C where
   covering B := { S | ∃ (α : Type) (_ : Fintype α) (X : α → C) (π : (a : α) → (X a ⟶ B)),
     S = Presieve.ofArrows X π ∧ (Sieve.generate S).EffectiveEpimorphic }
   pullback := by
     rintro B₁ B₂ f S ⟨α, _, X₁, π₁, rfl, hS⟩
-    obtain ⟨β,_,X₂,π₂,h,i,ι,hh⟩ := HasCoherentCoverage.condition f α X₁ π₁ hS
+    obtain ⟨β,_,X₂,π₂,h,i,ι,hh⟩ := Precoherent.cond f α X₁ π₁ hS
     refine ⟨Presieve.ofArrows X₂ π₂, ⟨β, inferInstance, X₂, π₂, rfl, h⟩, ?_⟩
     rintro _ _ ⟨b⟩
     refine ⟨(X₁ (i b)), ι _, π₁ _, ⟨_⟩, hh _⟩
 
-def CoherentTopology [HasCoherentCoverage C] : GrothendieckTopology C :=
+def CoherentTopology [Precoherent C] : GrothendieckTopology C :=
   Coverage.toGrothendieck _ <| CoherentCoverage C
 
-lemma isSheaf_coherent [HasCoherentCoverage C] (P : Cᵒᵖ ⥤ Type w) :
+lemma isSheaf_coherent [Precoherent C] (P : Cᵒᵖ ⥤ Type w) :
     Presieve.IsSheaf (CoherentTopology C) P ↔
     (∀ (B : C) (α : Type) [Fintype α] (X : α → C) (π : (a : α) → (X a ⟶ B)),
       (Sieve.generate (Presieve.ofArrows X π)).EffectiveEpimorphic →
