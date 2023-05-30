6e4598a6fb8f8cdb7e3da5afdc0d7e1c68873793
Jeremy Tan Jie Rui <reddeloostw@gmail.com>
2023-05-30 08:23:50 +0000

    feat: port Analysis.Calculus.Dslope (#4491)

--- Mathlib.lean
+++ Mathlib.lean
@@ -430,6 +430,7 @@ import Mathlib.Analysis.Calculus.Deriv.Prod
 import Mathlib.Analysis.Calculus.Deriv.Slope
 import Mathlib.Analysis.Calculus.Deriv.Star
 import Mathlib.Analysis.Calculus.Deriv.Support
+import Mathlib.Analysis.Calculus.Dslope
 import Mathlib.Analysis.Calculus.FDeriv.Add
 import Mathlib.Analysis.Calculus.FDeriv.Basic
 import Mathlib.Analysis.Calculus.FDeriv.Bilinear
