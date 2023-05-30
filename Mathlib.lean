541bcf2bd61a2f116346fac9404403fa8f678350
Jeremy Tan Jie Rui <reddeloostw@gmail.com>
2023-05-30 09:32:47 +0000

    feat: port NumberTheory.VonMangoldt (#4494)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1934,6 +1934,7 @@ import Mathlib.NumberTheory.Padics.RingHoms
 import Mathlib.NumberTheory.PellMatiyasevic
 import Mathlib.NumberTheory.Primorial
 import Mathlib.NumberTheory.PythagoreanTriples
+import Mathlib.NumberTheory.VonMangoldt
 import Mathlib.NumberTheory.Zsqrtd.Basic
 import Mathlib.NumberTheory.Zsqrtd.ToReal
 import Mathlib.Order.Antichain
