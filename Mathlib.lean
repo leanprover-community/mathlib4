7e873bba18670cd780024ffcfbd043e230f5f89e
Pim Otte <potte@quintor.nl>
2023-05-30 08:46:40 +0000

    feat: port Topology.Algebra.ContinuousMonoidHom (#4162)

--- Mathlib.lean
+++ Mathlib.lean
@@ -2346,6 +2346,7 @@ import Mathlib.Topology.Algebra.Affine
 import Mathlib.Topology.Algebra.Algebra
 import Mathlib.Topology.Algebra.ConstMulAction
 import Mathlib.Topology.Algebra.Constructions
+import Mathlib.Topology.Algebra.ContinuousMonoidHom
 import Mathlib.Topology.Algebra.Equicontinuity
 import Mathlib.Topology.Algebra.Field
 import Mathlib.Topology.Algebra.FilterBasis
