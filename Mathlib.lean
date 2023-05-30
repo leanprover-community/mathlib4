4acdb595412cd0103ffd353a4e1854d8b47a0392
Jason Yuen <jason_yuen2007@hotmail.com>
2023-05-30 06:01:00 +0000

    feat: port Data.Real.GoldenRatio (#4332)
    Co-authored-by: Komyyy <pol_tta@outlook.jp>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1392,6 +1392,7 @@ import Mathlib.Data.Real.ConjugateExponents
 import Mathlib.Data.Real.ENNReal
 import Mathlib.Data.Real.ENatENNReal
 import Mathlib.Data.Real.EReal
+import Mathlib.Data.Real.GoldenRatio
 import Mathlib.Data.Real.Hyperreal
 import Mathlib.Data.Real.Irrational
 import Mathlib.Data.Real.NNReal
