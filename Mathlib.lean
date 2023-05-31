ec79b6b1b3b8c57957dcf5682dca21d8a7b69783
Pol_tta <pol_tta@outlook.jp>
2023-05-31 01:08:09 +0000

    feat: port MeasureTheory.Group.Prod (#4502)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1871,6 +1871,7 @@ import Mathlib.MeasureTheory.Group.Arithmetic
 import Mathlib.MeasureTheory.Group.MeasurableEquiv
 import Mathlib.MeasureTheory.Group.Measure
 import Mathlib.MeasureTheory.Group.Pointwise
+import Mathlib.MeasureTheory.Group.Prod
 import Mathlib.MeasureTheory.Integral.Lebesgue
 import Mathlib.MeasureTheory.Integral.LebesgueNormedSpace
 import Mathlib.MeasureTheory.Integral.MeanInequalities
