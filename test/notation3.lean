c153c030d2de9e34d93f83f1ab5aa1a0bbca6028
Kyle Miller <kmill31415@gmail.com>
2023-05-31 23:52:28 +0000

    feat: `notation3` delaborator generation

--- test/notation3.lean
+++ test/notation3.lean
@@ -3,6 +3,8 @@ import Mathlib.Init.Data.Nat.Lemmas
 
 namespace Test
 
+set_option trace.notation3 true
+
 def Filter (α : Type) : Type := (α → Prop) → Prop
 def Filter.atTop [Preorder α] : Filter α := fun _ => True
 def Filter.eventually (p : α → Prop) (f : Filter α) := f p
@@ -17,9 +19,9 @@ notation3 "∃' " (...) ", " r:(scoped p => Exists p) => r
 #check ∃' x < 3, x < 3
 
 
-notation3 "~{" (x";"* => foldl (a b => (a, b)) ()) "}~" => x
+notation3 "~{" (x"; "* => foldl (a b => Prod.mk a b) Unit) "}~" => x
 #check ~{1; true; ~{2}~}~
 #check ~{}~
 
-notation3 "%[" (x","* => foldr (a b => a :: b) []) "]" => x
+notation3 "%[" (x", "* => foldr (a b => List.cons a b) List.nil) "]" => x
 #check %[1, 2, 3]
