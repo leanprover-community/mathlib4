34e8b42c77800853ec0d3c97d8425704cdbb677b
Jason Yuen <jason_yuen2007@hotmail.com>
2023-06-01 13:13:04 +0000

    feat: port Analysis.InnerProductSpace.Adjoint (#4476)
    Co-authored-by: Jireh Loreaux <loreaujy@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -493,6 +493,7 @@ import Mathlib.Analysis.Convex.StrictConvexSpace
 import Mathlib.Analysis.Convex.Topology
 import Mathlib.Analysis.Convex.Uniform
 import Mathlib.Analysis.Hofer
+import Mathlib.Analysis.InnerProductSpace.Adjoint
 import Mathlib.Analysis.InnerProductSpace.Basic
 import Mathlib.Analysis.InnerProductSpace.ConformalLinearMap
 import Mathlib.Analysis.InnerProductSpace.Dual
