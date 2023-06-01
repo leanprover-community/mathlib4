472a1a8e06b2d05146c609f9d67210386183aede
adamtopaz <github@adamtopaz.com>
2023-06-01 11:38:30 -0600

    docs

--- Mathlib/Topology/Category/CompHaus/ExplicitLimits.lean
+++ Mathlib/Topology/Category/CompHaus/ExplicitLimits.lean
@@ -30,6 +30,10 @@ section Pullbacks
 
 variable {X Y B : CompHaus.{u}} (f : X ⟶ B) (g : Y ⟶ B)
 
+/--
+The pullback of two morphisms `f,g` in `CompHaus`, constructed explicitly as the set of
+pairs `(x,y)` such that `f x = g y`, with the topology induced by the product.
+-/
 def Pullback : CompHaus.{u} :=
   letI set := { xy : X × Y | f xy.fst = g xy.snd }
   haveI : CompactSpace set := by
@@ -38,10 +42,16 @@ def Pullback : CompHaus.{u} :=
     exact isClosed_eq (f.continuous.comp continuous_fst) (g.continuous.comp continuous_snd)
   CompHaus.of set
 
+/--
+The projection from the pullback to the first component.
+-/
 def Pullback.fst : Pullback f g ⟶ X where
   toFun := fun ⟨⟨x,_⟩,_⟩ => x
   continuous_toFun := Continuous.comp continuous_fst continuous_subtype_val
 
+/--
+The projection from the pullback to the second component.
+-/
 def Pullback.snd : Pullback f g ⟶ Y where
   toFun := fun ⟨⟨_,y⟩,_⟩ => y
   continuous_toFun := Continuous.comp continuous_snd continuous_subtype_val
@@ -50,6 +60,11 @@ def Pullback.snd : Pullback f g ⟶ Y where
 lemma Pullback.condition : Pullback.fst f g ≫ f = Pullback.snd f g ≫ g := by
   ext ⟨_,h⟩ ; exact h
 
+/--
+Construct a morphism to the explicit pullback given morphisms to the factors
+which are compatible with the maps to the base.
+This is essentially the universal property of the pullback.
+-/
 def Pullback.lift {Z : CompHaus.{u}} (a : Z ⟶ X) (b : Z ⟶ Y) (w : a ≫ f = b ≫ g) :
     Z ⟶ Pullback f g where
   toFun := fun z => ⟨⟨a z, b z⟩, by apply_fun (fun q => q z) at w ; exact w⟩
@@ -76,10 +91,16 @@ lemma Pullback.hom_ext {Z : CompHaus.{u}} (a b : Z ⟶ Pullback f g)
   · exact hfst
   · exact hsnd
 
+/--
+The pullback cone whose cone point is the explicit pullback.
+-/
 @[simps! pt π]
 def Pullback.cone : Limits.PullbackCone f g:=
   Limits.PullbackCone.mk (Pullback.fst f g) (Pullback.snd f g) (Pullback.condition f g)
 
+/--
+The explicit pullback cone is a limit cone.
+-/
 @[simps! lift]
 def Pullback.isLimit : Limits.IsLimit (Pullback.cone f g) :=
   Limits.PullbackCone.isLimitAux _
@@ -94,12 +115,24 @@ section FiniteCoproducts
 
 variable {α : Type} [Fintype α] (X : α → CompHaus.{u})
 
+/--
+The coproduct of a finite family of objects in `CompHaus`, constructed as the disjoint
+union with its usual topology.
+-/
 def FiniteCoproduct : CompHaus := CompHaus.of <| Σ (a : α), X a
 
+/--
+The inclusion of one of the factors into the explicit finite coproduct.
+-/
 def FiniteCoproduct.incl (a : α) : X a ⟶ FiniteCoproduct X where
   toFun := fun x => ⟨a,x⟩
   continuous_toFun := continuous_sigmaMk (σ := fun a => X a)
 
+/--
+To construct a morphism from the explicit finite coproduct, it suffices to
+specify a morphism from each of its factors.
+This is essentially the universal property of the coproduct.
+-/
 def FiniteCoproduct.desc {B : CompHaus.{u}} (e : (a : α) → (X a ⟶ B)) :
     FiniteCoproduct X ⟶ B where
   toFun := fun ⟨a,x⟩ => e a x
@@ -118,11 +151,17 @@ lemma FiniteCoproduct.hom_ext {B : CompHaus.{u}} (f g : FiniteCoproduct X ⟶ B)
   apply_fun (fun q => q x) at h
   exact h
 
+/--
+The coproduct cocone associated to the explicit finite coproduct.
+-/
 @[simps]
 def FiniteCoproduct.cocone : Limits.Cocone (Discrete.functor X) where
   pt := FiniteCoproduct X
   ι := Discrete.natTrans fun ⟨a⟩ => FiniteCoproduct.incl X a
 
+/--
+The explicit finite coproduct cocone is a colimit cocone.
+-/
 @[simps]
 def FiniteCoproduct.isColimit : Limits.IsColimit (FiniteCoproduct.cocone X) where
   desc := fun s => FiniteCoproduct.desc _ fun a => s.ι.app ⟨a⟩
