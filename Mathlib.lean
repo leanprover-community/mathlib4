49ae3c6bd0eda957f52d03b09320f1308a4891a6
Jeremy Tan Jie Rui <reddeloostw@gmail.com>
2023-06-02 16:56:03 +0000

    feat: port MeasureTheory.Measure.Lebesgue.Complex (#4598)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1939,6 +1939,7 @@ import Mathlib.MeasureTheory.Measure.GiryMonad
 import Mathlib.MeasureTheory.Measure.Haar.Basic
 import Mathlib.MeasureTheory.Measure.Haar.OfBasis
 import Mathlib.MeasureTheory.Measure.Lebesgue.Basic
+import Mathlib.MeasureTheory.Measure.Lebesgue.Complex
 import Mathlib.MeasureTheory.Measure.MeasureSpace
 import Mathlib.MeasureTheory.Measure.MeasureSpaceDef
 import Mathlib.MeasureTheory.Measure.MutuallySingular
