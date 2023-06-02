c0a6eca993635408986e14c4cf73b6eef41d630d
Yury G. Kudryashov <urkud@urkud.name>
2023-06-02 11:07:20 +0000

    feat: port RingTheory.Derivation.Basic (#4575)

--- Mathlib.lean
+++ Mathlib.lean
@@ -2131,6 +2131,7 @@ import Mathlib.RingTheory.Congruence
 import Mathlib.RingTheory.Coprime.Basic
 import Mathlib.RingTheory.Coprime.Ideal
 import Mathlib.RingTheory.Coprime.Lemmas
+import Mathlib.RingTheory.Derivation.Basic
 import Mathlib.RingTheory.DiscreteValuationRing.Basic
 import Mathlib.RingTheory.EisensteinCriterion
 import Mathlib.RingTheory.EuclideanDomain
