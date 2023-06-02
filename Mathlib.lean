2afe8cb1948417c245ecbb04c3232f58a3697bc9
Pol_tta <pol_tta@outlook.jp>
2023-06-02 11:07:17 +0000

    feat: port Data.Ordmap.Ordset (#4541)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1344,6 +1344,7 @@ import Mathlib.Data.Option.Basic
 import Mathlib.Data.Option.Defs
 import Mathlib.Data.Option.NAry
 import Mathlib.Data.Ordmap.Ordnode
+import Mathlib.Data.Ordmap.Ordset
 import Mathlib.Data.PEquiv
 import Mathlib.Data.PFun
 import Mathlib.Data.PFunctor.Multivariate.Basic
