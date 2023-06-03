5f2fbffd4efe997029ff75a135033748b8f91746
adomani <adomani@gmail.com>
2023-06-03 05:16:26 +0200

    Add a test with autoimplicit Sort and variable

--- test/ExtractGoal.lean
+++ test/ExtractGoal.lean
@@ -7,14 +7,19 @@ example (i j k : ℕ) (h₀ : i ≤ j) (h₁ : j ≤ k) : i ≤ k := by
   exact h₀.trans h₁
 
 -- an example with all binder types
-example {α : Type u} {β : Type v} [Add α] [_h : Sub β] (f : α → β) ⦃_g : ℤ⦄ (a : α) {b : β} :
-    (f a = b) ∨ True := by
+example {α : Type u} {β : Type v} [Add α] [h : Sub β] (f : α → β) ⦃_g : ℤ⦄ (a : α) {b : β} :
+    f a - b = f a - b := by
   extract_goal
-  exact Or.inr trivial
+  rfl
 
 -- an example with a hygienic variable
 example (n : ℕ) : n = n := by
   cases n
   rfl
+  extract_goal
+  rfl
+
+-- an example with auto-implicit `Sort` and variable
+example : n = n := by
   extract_goal
   rfl
