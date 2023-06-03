222faaa06969b6a47bd8ae9938eff0ab7dba809e
int-y1 <jason_yuen2007@hotmail.com>
2023-06-03 02:34:11 -0700

    fix

--- Mathlib/Topology/Sheaves/Sheafify.lean
+++ Mathlib/Topology/Sheaves/Sheafify.lean
@@ -19,13 +19,13 @@ as the subsheaf of dependent functions into the stalks
 consisting of functions which are locally germs.
 
 We show that the stalks of the sheafification are isomorphic to the original stalks,
-via `stalk_to_fiber` which evaluates a germ of a dependent function at a point.
+via `stalkToFiber` which evaluates a germ of a dependent function at a point.
 
-We construct a morphism `to_sheafify` from a presheaf to (the underlying presheaf of)
+We construct a morphism `toSheafify` from a presheaf to (the underlying presheaf of)
 its sheafification, given by sending a section to its collection of germs.
 
 ## Future work
-Show that the map induced on stalks by `to_sheafify` is the inverse of `stalk_to_fiber`.
+Show that the map induced on stalks by `toSheafify` is the inverse of `stalkToFiber`.
 
 Show sheafification is a functor from presheaves to sheaves,
 and that it is the left adjoint of the forgetful functor,
@@ -46,6 +46,7 @@ open TopologicalSpace
 variable {X : TopCat.{v}} (F : Presheaf (Type v) X)
 
 namespace TopCat.Presheaf
+set_option linter.uppercaseLean3 false -- `Top`
 
 namespace Sheafify
 
@@ -53,9 +54,8 @@ namespace Sheafify
 The prelocal predicate on functions into the stalks, asserting that the function is equal to a germ.
 -/
 def isGerm : PrelocalPredicate fun x => F.stalk x where
-  pred U f := ∃ g : F.obj (op U), ∀ x : U, f x = F.germ x g
-  res := fun V U i f ⟨g, p⟩ =>
-    ⟨F.map i.op g, fun x => (p (i x)).trans (F.germ_res_apply _ _ _).symm⟩
+  pred {U} f := ∃ g : F.obj (op U), ∀ x : U, f x = F.germ x g
+  res := fun i _ ⟨g, p⟩ => ⟨F.map i.op g, fun x => (p (i x)).trans (F.germ_res_apply i x g).symm⟩
 #align Top.presheaf.sheafify.is_germ TopCat.Presheaf.Sheafify.isGerm
 
 /-- The local predicate on functions into the stalks,
@@ -80,29 +80,33 @@ sending each section to its germs.
 -/
 def toSheafify : F ⟶ F.sheafify.1 where
   app U f := ⟨fun x => F.germ x f, PrelocalPredicate.sheafifyOf ⟨f, fun x => rfl⟩⟩
-  naturality' U U' f := by ext (x⟨u, m⟩); exact germ_res_apply F f.unop ⟨u, m⟩ x
+  naturality U U' f := by
+    ext x
+    apply Subtype.ext -- Porting note: Added `apply`
+    ext ⟨u, m⟩
+    exact germ_res_apply F f.unop ⟨u, m⟩ x
 #align Top.presheaf.to_sheafify TopCat.Presheaf.toSheafify
 
 /-- The natural morphism from the stalk of the sheafification to the original stalk.
-In `sheafify_stalk_iso` we show this is an isomorphism.
+In `sheafifyStalkIso` we show this is an isomorphism.
 -/
-def stalkToFiber (x : X) : F.sheafify.Presheaf.stalk x ⟶ F.stalk x :=
-  stalkToFiber (Sheafify.isLocallyGerm F) x
+def stalkToFiber (x : X) : F.sheafify.presheaf.stalk x ⟶ F.stalk x :=
+  TopCat.stalkToFiber (Sheafify.isLocallyGerm F) x
 #align Top.presheaf.stalk_to_fiber TopCat.Presheaf.stalkToFiber
 
 theorem stalkToFiber_surjective (x : X) : Function.Surjective (F.stalkToFiber x) := by
-  apply stalk_to_fiber_surjective
+  apply TopCat.stalkToFiber_surjective
   intro t
   obtain ⟨U, m, s, rfl⟩ := F.germ_exist _ t
   · use ⟨U, m⟩
     fconstructor
     · exact fun y => F.germ y s
-    · exact ⟨prelocal_predicate.sheafify_of ⟨s, fun _ => rfl⟩, rfl⟩
+    · exact ⟨PrelocalPredicate.sheafifyOf ⟨s, fun _ => rfl⟩, rfl⟩
 #align Top.presheaf.stalk_to_fiber_surjective TopCat.Presheaf.stalkToFiber_surjective
 
 theorem stalkToFiber_injective (x : X) : Function.Injective (F.stalkToFiber x) := by
-  apply stalk_to_fiber_injective
-  intros
+  apply TopCat.stalkToFiber_injective
+  intro U V fU hU fV hV e
   rcases hU ⟨x, U.2⟩ with ⟨U', mU, iU, gU, wU⟩
   rcases hV ⟨x, V.2⟩ with ⟨V', mV, iV, gV, wV⟩
   have wUx := wU ⟨x, mU⟩
@@ -110,29 +114,26 @@ theorem stalkToFiber_injective (x : X) : Function.Injective (F.stalkToFiber x) :
   have wVx := wV ⟨x, mV⟩
   dsimp at wVx ; erw [wVx] at e ; clear wVx
   rcases F.germ_eq x mU mV gU gV e with ⟨W, mW, iU', iV', e'⟩
-  dsimp at e' 
+  dsimp at e'
   use ⟨W ⊓ (U' ⊓ V'), ⟨mW, mU, mV⟩⟩
   refine' ⟨_, _, _⟩
   · change W ⊓ (U' ⊓ V') ⟶ U.obj
-    exact opens.inf_le_right _ _ ≫ opens.inf_le_left _ _ ≫ iU
+    exact Opens.infLERight _ _ ≫ Opens.infLELeft _ _ ≫ iU
   · change W ⊓ (U' ⊓ V') ⟶ V.obj
-    exact opens.inf_le_right _ _ ≫ opens.inf_le_right _ _ ≫ iV
+    exact Opens.infLERight _ _ ≫ Opens.infLERight _ _ ≫ iV
   · intro w
-    dsimp
     specialize wU ⟨w.1, w.2.2.1⟩
-    dsimp at wU 
     specialize wV ⟨w.1, w.2.2.2⟩
-    dsimp at wV 
+    dsimp at wU wV ⊢
     erw [wU, ← F.germ_res iU' ⟨w, w.2.1⟩, wV, ← F.germ_res iV' ⟨w, w.2.1⟩,
-      CategoryTheory.types_comp_apply, CategoryTheory.types_comp_apply, e']
+      CategoryTheory.types_comp_apply, CategoryTheory.types_comp_apply, e']; rfl
 #align Top.presheaf.stalk_to_fiber_injective TopCat.Presheaf.stalkToFiber_injective
 
 /-- The isomorphism betweeen a stalk of the sheafification and the original stalk.
 -/
-def sheafifyStalkIso (x : X) : F.sheafify.Presheaf.stalk x ≅ F.stalk x :=
+def sheafifyStalkIso (x : X) : F.sheafify.presheaf.stalk x ≅ F.stalk x :=
   (Equiv.ofBijective _ ⟨stalkToFiber_injective _ _, stalkToFiber_surjective _ _⟩).toIso
 #align Top.presheaf.sheafify_stalk_iso TopCat.Presheaf.sheafifyStalkIso
 
 -- PROJECT functoriality, and that sheafification is the left adjoint of the forgetful functor.
 end TopCat.Presheaf
-
