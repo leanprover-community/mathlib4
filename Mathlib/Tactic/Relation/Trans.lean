66f38f8ae9e245d15675ee4ec533b90921944d13
thorimur <68410468+thorimur@users.noreply.github.com>
2023-06-04 19:06:17 -0400

    . (trans: fixup with attr)

--- Mathlib/Tactic/Relation/Trans.lean
+++ Mathlib/Tactic/Relation/Trans.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Siddhartha Gadgil, Mario Carneiro
 -/
 import Mathlib.Lean.Meta
+import Mathlib.Lean.Expr.Basic
 import Lean.Elab.Tactic.Location
 
 /-!
