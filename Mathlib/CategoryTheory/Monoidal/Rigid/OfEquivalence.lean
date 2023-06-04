05b6c6cb889ecbacdc428a8b266bba329ec9a3ab
Joël Riou <joel.riou@universite-paris-saclay.fr>
2023-06-04 22:17:45 +0200

    it compiles

--- Mathlib/CategoryTheory/Monoidal/Rigid/OfEquivalence.lean
+++ Mathlib/CategoryTheory/Monoidal/Rigid/OfEquivalence.lean
@@ -23,8 +23,6 @@ variable {C D : Type _} [Category C] [Category D] [MonoidalCategory C] [Monoidal
 
 variable (F : MonoidalFunctor C D)
 
-/- ./././Mathport/Syntax/Translate/Expr.lean:177:8: unsupported: ambiguous notation -/
-/- ./././Mathport/Syntax/Translate/Expr.lean:177:8: unsupported: ambiguous notation -/
 /-- Given candidate data for an exact pairing,
 which is sent by a faithful monoidal functor to an exact pairing,
 the equations holds automatically. -/
@@ -32,12 +30,12 @@ def exactPairingOfFaithful [Faithful F.toFunctor] {X Y : C} (eval : Y ⊗ X ⟶ 
     (coeval : 𝟙_ C ⟶ X ⊗ Y) [ExactPairing (F.obj X) (F.obj Y)]
     (map_eval : F.map eval = inv (F.μ _ _) ≫ ε_ _ _ ≫ F.ε)
     (map_coeval : F.map coeval = inv F.ε ≫ η_ _ _ ≫ F.μ _ _) : ExactPairing X Y where
-  evaluation := eval
-  coevaluation := coeval
+  evaluation' := eval
+  coevaluation' := coeval
   evaluation_coevaluation' :=
-    F.toFunctor.map_injective (by simp [map_eval, map_coeval, monoidal_functor.map_tensor])
+    F.toFunctor.map_injective (by simp [map_eval, map_coeval, MonoidalFunctor.map_tensor])
   coevaluation_evaluation' :=
-    F.toFunctor.map_injective (by simp [map_eval, map_coeval, monoidal_functor.map_tensor])
+    F.toFunctor.map_injective (by simp [map_eval, map_coeval, MonoidalFunctor.map_tensor])
 #align category_theory.exact_pairing_of_faithful CategoryTheory.exactPairingOfFaithful
 
 /-- Given a pair of objects which are sent by a fully faithful functor to a pair of objects
@@ -49,26 +47,26 @@ def exactPairingOfFullyFaithful [Full F.toFunctor] [Faithful F.toFunctor] (X Y :
     (F.toFunctor.preimage (inv F.ε ≫ η_ _ _ ≫ F.μ _ _)) (by simp) (by simp)
 #align category_theory.exact_pairing_of_fully_faithful CategoryTheory.exactPairingOfFullyFaithful
 
-/- ./././Mathport/Syntax/Translate/Expr.lean:177:8: unsupported: ambiguous notation -/
 /-- Pull back a left dual along an equivalence. -/
 def hasLeftDualOfEquivalence [IsEquivalence F.toFunctor] (X : C) [HasLeftDual (F.obj X)] :
     HasLeftDual X where
-  leftDual := F.toFunctor.inv.obj (ᘁF.obj X)
+  leftDual := F.toFunctor.inv.obj (ᘁ(F.obj X))
   exact := by
-    apply exact_pairing_of_fully_faithful F _ _
-    apply exact_pairing_congr_left (F.to_functor.as_equivalence.counit_iso.app _)
+    -- porting note: in Lean3, `apply exactPairingOfFullyFaithful F _ _` automatically
+    -- created the goals for `ExactPairing` type class
+    refine @exactPairingOfFullyFaithful _ _ _ _ _ _ F _ _ _ _ ?_
+    refine @exactPairingCongrLeft _ _ _ _ _ _ ?_ (F.toFunctor.asEquivalence.counitIso.app _)
     dsimp
     infer_instance
 #align category_theory.has_left_dual_of_equivalence CategoryTheory.hasLeftDualOfEquivalence
 
-/- ./././Mathport/Syntax/Translate/Expr.lean:177:8: unsupported: ambiguous notation -/
 /-- Pull back a right dual along an equivalence. -/
 def hasRightDualOfEquivalence [IsEquivalence F.toFunctor] (X : C) [HasRightDual (F.obj X)] :
     HasRightDual X where
-  rightDual := F.toFunctor.inv.obj (F.obj Xᘁ)
+  rightDual := F.toFunctor.inv.obj ((F.obj X)ᘁ)
   exact := by
-    apply exact_pairing_of_fully_faithful F _ _
-    apply exact_pairing_congr_right (F.to_functor.as_equivalence.counit_iso.app _)
+    refine @exactPairingOfFullyFaithful _ _ _ _ _ _ F _ _ _ _ ?_
+    refine @exactPairingCongrRight _ _ _ _ _ _ ?_ (F.toFunctor.asEquivalence.counitIso.app _)
     dsimp
     infer_instance
 #align category_theory.has_right_dual_of_equivalence CategoryTheory.hasRightDualOfEquivalence
@@ -90,4 +88,3 @@ def rigidCategoryOfEquivalence [IsEquivalence F.toFunctor] [RigidCategory D] : R
 #align category_theory.rigid_category_of_equivalence CategoryTheory.rigidCategoryOfEquivalence
 
 end CategoryTheory
-
