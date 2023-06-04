e8a65ab46904c0085466e541cf7b4dbee924890f
Joël Riou <joel.riou@universite-paris-saclay.fr>
2023-06-04 09:05:56 +0200

    it compiles

--- Mathlib/CategoryTheory/Monoidal/Rigid/FunctorCategory.lean
+++ Mathlib/CategoryTheory/Monoidal/Rigid/FunctorCategory.lean
@@ -28,55 +28,51 @@ namespace CategoryTheory.Monoidal
 
 variable {C D : Type _} [Groupoid C] [Category D] [MonoidalCategory D]
 
-/- ./././Mathport/Syntax/Translate/Expr.lean:177:8: unsupported: ambiguous notation -/
-/- ./././Mathport/Syntax/Translate/Expr.lean:177:8: unsupported: ambiguous notation -/
 instance functorHasRightDual [RightRigidCategory D] (F : C ⥤ D) : HasRightDual F where
   rightDual :=
-    { obj := fun X => F.obj Xᘁ
-      map := fun X Y f => F.map (inv f)ᘁ
-      map_comp' := fun X Y Z f g => by simp [comp_right_adjoint_mate] }
+    { obj := fun X => (F.obj X)ᘁ
+      map := fun f => (F.map (inv f))ᘁ
+      map_comp := fun f g => by simp [comp_rightAdjointMate] }
   exact :=
-    { evaluation :=
+    { evaluation' :=
         { app := fun X => ε_ _ _
-          naturality' := fun X Y f => by
+          naturality := fun X Y f => by
             dsimp
-            rw [category.comp_id, functor.map_inv, ← id_tensor_comp_tensor_id, category.assoc,
-              right_adjoint_mate_comp_evaluation, ← category.assoc, ← id_tensor_comp,
-              is_iso.hom_inv_id, tensor_id, category.id_comp] }
-      coevaluation :=
+            rw [Category.comp_id, Functor.map_inv, ← id_tensor_comp_tensor_id, Category.assoc,
+              rightAdjointMate_comp_evaluation, ← Category.assoc, ← id_tensor_comp,
+              IsIso.hom_inv_id, tensor_id, Category.id_comp] }
+      coevaluation' :=
         { app := fun X => η_ _ _
-          naturality' := fun X Y f => by
+          naturality := fun X Y f => by
             dsimp
-            rw [functor.map_inv, category.id_comp, ← id_tensor_comp_tensor_id, ← category.assoc,
-              coevaluation_comp_right_adjoint_mate, category.assoc, ← comp_tensor_id,
-              is_iso.inv_hom_id, tensor_id, category.comp_id] } }
+            rw [Functor.map_inv, Category.id_comp, ← id_tensor_comp_tensor_id, ← Category.assoc,
+              coevaluation_comp_rightAdjointMate, Category.assoc, ← comp_tensor_id,
+              IsIso.inv_hom_id, tensor_id, Category.comp_id] } }
 #align category_theory.monoidal.functor_has_right_dual CategoryTheory.Monoidal.functorHasRightDual
 
 instance rightRigidFunctorCategory [RightRigidCategory D] : RightRigidCategory (C ⥤ D) where
 #align category_theory.monoidal.right_rigid_functor_category CategoryTheory.Monoidal.rightRigidFunctorCategory
 
-/- ./././Mathport/Syntax/Translate/Expr.lean:177:8: unsupported: ambiguous notation -/
-/- ./././Mathport/Syntax/Translate/Expr.lean:177:8: unsupported: ambiguous notation -/
 instance functorHasLeftDual [LeftRigidCategory D] (F : C ⥤ D) : HasLeftDual F where
   leftDual :=
-    { obj := fun X => ᘁF.obj X
-      map := fun X Y f => ᘁF.map (inv f)
-      map_comp' := fun X Y Z f g => by simp [comp_left_adjoint_mate] }
+    { obj := fun X => ᘁ(F.obj X)
+      map := fun f => ᘁ(F.map (inv f))
+      map_comp := fun f g => by simp [comp_leftAdjointMate] }
   exact :=
-    { evaluation :=
+    { evaluation' :=
         { app := fun X => ε_ _ _
-          naturality' := fun X Y f => by
+          naturality := fun X Y f => by
             dsimp
-            rw [category.comp_id, functor.map_inv, ← tensor_id_comp_id_tensor, category.assoc,
-              left_adjoint_mate_comp_evaluation, ← category.assoc, ← comp_tensor_id,
-              is_iso.hom_inv_id, tensor_id, category.id_comp] }
-      coevaluation :=
+            rw [Category.comp_id, Functor.map_inv, ← tensor_id_comp_id_tensor, Category.assoc,
+              leftAdjointMate_comp_evaluation, ← Category.assoc, ← comp_tensor_id,
+              IsIso.hom_inv_id, tensor_id, Category.id_comp] }
+      coevaluation' :=
         { app := fun X => η_ _ _
-          naturality' := fun X Y f => by
+          naturality := fun X Y f => by
             dsimp
-            rw [functor.map_inv, category.id_comp, ← tensor_id_comp_id_tensor, ← category.assoc,
-              coevaluation_comp_left_adjoint_mate, category.assoc, ← id_tensor_comp,
-              is_iso.inv_hom_id, tensor_id, category.comp_id] } }
+            rw [Functor.map_inv, Category.id_comp, ← tensor_id_comp_id_tensor, ← Category.assoc,
+              coevaluation_comp_leftAdjointMate, Category.assoc, ← id_tensor_comp,
+              IsIso.inv_hom_id, tensor_id, Category.comp_id] } }
 #align category_theory.monoidal.functor_has_left_dual CategoryTheory.Monoidal.functorHasLeftDual
 
 instance leftRigidFunctorCategory [LeftRigidCategory D] : LeftRigidCategory (C ⥤ D) where
@@ -86,4 +82,3 @@ instance rigidFunctorCategory [RigidCategory D] : RigidCategory (C ⥤ D) where
 #align category_theory.monoidal.rigid_functor_category CategoryTheory.Monoidal.rigidFunctorCategory
 
 end CategoryTheory.Monoidal
-
