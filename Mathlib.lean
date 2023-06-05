78d929848dfaa9dcc2200f72846ac91db0c15327
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2023-06-05 22:03:30 +0200

    Move

--- Mathlib.lean
+++ Mathlib.lean
@@ -265,8 +265,8 @@ import Mathlib.Algebra.Module.Injective
 import Mathlib.Algebra.Module.LinearMap
 import Mathlib.Algebra.Module.LocalizedModule
 import Mathlib.Algebra.Module.Opposites
+import Mathlib.Algebra.Module.PID
 import Mathlib.Algebra.Module.Pi
-import Mathlib.Algebra.Module.Pid
 import Mathlib.Algebra.Module.PointwisePi
 import Mathlib.Algebra.Module.Prod
 import Mathlib.Algebra.Module.Projective
