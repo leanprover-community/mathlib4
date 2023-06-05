716d796e655b90a8d4def1b6be5b05a1265b69dd
Scott Morrison <scott@tqft.net>
2023-06-05 00:03:12 +0000

    feat: port Analysis.SpecialFunctions.Trigonometric.Deriv (#4653)
    - [x] depends on: #4651
    
    [![Open in Gitpod](https://gitpod.io/button/open-in-gitpod.svg)](https://gitpod.io/from-referrer/)
    
    
    Co-authored-by: Scott Morrison <scott.morrison@gmail.com>
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>
    Co-authored-by: Yury G. Kudryashov <urkud@urkud.name>
    Co-authored-by: Floris van Doorn <fpvdoorn@gmail.com>
    Co-authored-by: Patrick Massot <patrickmassot@free.fr>
    Co-authored-by: Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
    Co-authored-by: Parcly Taxel <reddeloostw@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -624,6 +624,7 @@ import Mathlib.Analysis.SpecialFunctions.Sqrt
 import Mathlib.Analysis.SpecialFunctions.Trigonometric.Angle
 import Mathlib.Analysis.SpecialFunctions.Trigonometric.Basic
 import Mathlib.Analysis.SpecialFunctions.Trigonometric.Chebyshev
+import Mathlib.Analysis.SpecialFunctions.Trigonometric.Deriv
 import Mathlib.Analysis.SpecialFunctions.Trigonometric.Inverse
 import Mathlib.Analysis.SpecificLimits.Basic
 import Mathlib.Analysis.SpecificLimits.FloorPow
