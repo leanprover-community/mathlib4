5057f96775eae9f6f09a452b828f876710cd365d
Parcly Taxel <reddeloostw@gmail.com>
2023-06-05 14:26:27 +0800

    Fix docs

--- Mathlib/Geometry/Manifold/Instances/UnitsOfNormedAlgebra.lean
+++ Mathlib/Geometry/Manifold/Instances/UnitsOfNormedAlgebra.lean
@@ -22,28 +22,28 @@ This manifold is actually a Lie group, which eventually should be the main resul
 
 An important special case of this construction is the general linear group.  For a normed space `V`
 over a field `𝕜`, the `𝕜`-linear endomorphisms of `V` are a normed `𝕜`-algebra (see
-`continuous_linear_map.to_normed_algebra`), so this construction provides a Lie group structure on
+`ContinuousLinearMap.toNormedAlgebra`), so this construction provides a Lie group structure on
 its group of units, the general linear group GL(`𝕜`, `V`).
 
 ## TODO
 
 The Lie group instance requires the following fields:
 ```
-instance : lie_group 𝓘(𝕜, R) Rˣ :=
-{ smooth_mul := sorry,
-  smooth_inv := sorry,
-  ..units.smooth_manifold_with_corners }
+instance : LieGroup 𝓘(𝕜, R) Rˣ :=
+{ Units.smoothManifoldWithCorners with
+  smooth_mul := sorry,
+  smooth_inv := sorry }
 ```
 
 The ingredients needed for the construction are
 * smoothness of multiplication and inversion in the charts, i.e. as functions on the normed
-  `𝕜`-space `R`:  see `cont_diff_at_ring_inverse` for the inversion result, and
-  `cont_diff_mul` (needs to be generalized from field to algebra) for the multiplication
+  `𝕜`-space `R`:  see `contDiffAt_ring_inverse` for the inversion result, and
+  `contDiff_mul` (needs to be generalized from field to algebra) for the multiplication
   result
 * for an open embedding `f`, whose domain is equipped with the induced manifold structure
-  `f.singleton_smooth_manifold_with_corners`, characterization of smoothness of functions to/from
+  `f.singleton_smoothManifoldWithCorners`, characterization of smoothness of functions to/from
   this manifold in terms of smoothness in the target space.  See the pair of lemmas
-  `cont_mdiff_coe_sphere` and `cont_mdiff.cod_restrict_sphere` for a model.
+  `ContMDiff_coe_sphere` and `ContMDiff.codRestrict_sphere` for a model.
 None of this should be particularly difficult.
 
 -/
@@ -74,4 +74,3 @@ instance : SmoothManifoldWithCorners 𝓘(𝕜, R) Rˣ :=
   openEmbedding_val.singleton_smoothManifoldWithCorners 𝓘(𝕜, R)
 
 end Units
-
