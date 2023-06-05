6af7bec919461a3f3e9aafa0d21e5c5342af7a25
Ruben Van de Velde <pol_tta@outlook.jp>
2023-06-05 08:37:48 +0000

    feat: port MeasureTheory.Integral.Bochner (#4590)
    Co-authored-by: Yury G. Kudryashov <urkud@urkud.name>
    Co-authored-by: Scott Morrison <scott.morrison@gmail.com>
    Co-authored-by: Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
    Co-authored-by: int-y1 <jason_yuen2007@hotmail.com>
    Co-authored-by: Moritz Doll <moritz.doll@googlemail.com>
    Co-authored-by: Johan Commelin <johan@commelin.net>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1964,7 +1964,9 @@ import Mathlib.MeasureTheory.Group.MeasurableEquiv
 import Mathlib.MeasureTheory.Group.Measure
 import Mathlib.MeasureTheory.Group.Pointwise
 import Mathlib.MeasureTheory.Group.Prod
+import Mathlib.MeasureTheory.Integral.Bochner
 import Mathlib.MeasureTheory.Integral.IntegrableOn
+import Mathlib.MeasureTheory.Integral.IntegralSimps
 import Mathlib.MeasureTheory.Integral.Lebesgue
 import Mathlib.MeasureTheory.Integral.LebesgueNormedSpace
 import Mathlib.MeasureTheory.Integral.MeanInequalities
