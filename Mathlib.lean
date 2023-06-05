d75993443babf361446cfe3d2ed8b4a7477c5dea
Jason Yuen <jason_yuen2007@hotmail.com>
2023-06-05 19:04:19 +0000

    feat: port Algebra.Lie.DirectSum (#4684)

--- Mathlib.lean
+++ Mathlib.lean
@@ -207,6 +207,7 @@ import Mathlib.Algebra.Lie.Abelian
 import Mathlib.Algebra.Lie.BaseChange
 import Mathlib.Algebra.Lie.Basic
 import Mathlib.Algebra.Lie.Character
+import Mathlib.Algebra.Lie.DirectSum
 import Mathlib.Algebra.Lie.IdealOperations
 import Mathlib.Algebra.Lie.Matrix
 import Mathlib.Algebra.Lie.NonUnitalNonAssocAlgebra
