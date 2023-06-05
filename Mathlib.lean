ba4be16ef592ed61b93200a0ed404fdd2f724d53
Jason Yuen <jason_yuen2007@hotmail.com>
2023-06-05 07:16:42 +0000

    feat: port Algebra.Lie.SkewAdjoint (#4649)
    Co-authored-by: Jeremy Tan Jie Rui <reddeloostw@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -209,6 +209,7 @@ import Mathlib.Algebra.Lie.IdealOperations
 import Mathlib.Algebra.Lie.Matrix
 import Mathlib.Algebra.Lie.NonUnitalNonAssocAlgebra
 import Mathlib.Algebra.Lie.OfAssociative
+import Mathlib.Algebra.Lie.SkewAdjoint
 import Mathlib.Algebra.Lie.Solvable
 import Mathlib.Algebra.Lie.Subalgebra
 import Mathlib.Algebra.Lie.Submodule
