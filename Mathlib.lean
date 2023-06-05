58aa2857672117169839b15480fd1feed8efcb96
Jeremy Tan Jie Rui <urkud@urkud.name>
2023-06-05 10:31:30 +0000

    feat: port Data.Complex.ExponentialBounds (#4682)
    Co-authored-by: Parcly Taxel <reddeloostw@gmail.com>
    Co-authored-by: Chris Hughes <chrishughes24@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1085,6 +1085,7 @@ import Mathlib.Data.Complex.Basic
 import Mathlib.Data.Complex.Cardinality
 import Mathlib.Data.Complex.Determinant
 import Mathlib.Data.Complex.Exponential
+import Mathlib.Data.Complex.ExponentialBounds
 import Mathlib.Data.Complex.Module
 import Mathlib.Data.Complex.Orientation
 import Mathlib.Data.Countable.Basic
