8951bb6425b44f78e5cee4936e9c6558317a4810
Joël Riou <joel.riou@universite-paris-saclay.fr>
2023-06-05 01:30:22 +0000

    feat: port CategoryTheory.Monoidal.Rigid.OfEquivalence (#4660)

--- Mathlib.lean
+++ Mathlib.lean
@@ -865,6 +865,7 @@ import Mathlib.CategoryTheory.Monoidal.OfChosenFiniteProducts.Basic
 import Mathlib.CategoryTheory.Monoidal.OfChosenFiniteProducts.Symmetric
 import Mathlib.CategoryTheory.Monoidal.Preadditive
 import Mathlib.CategoryTheory.Monoidal.Rigid.Basic
+import Mathlib.CategoryTheory.Monoidal.Rigid.OfEquivalence
 import Mathlib.CategoryTheory.Monoidal.Tor
 import Mathlib.CategoryTheory.Monoidal.Transport
 import Mathlib.CategoryTheory.Monoidal.Types.Basic
