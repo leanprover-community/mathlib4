9aaa5995bf00a71cc3bd09fae1daf84ea0fe657d
Moritz Firsching <moritz.firsching@gmail.com>
2023-06-06 03:55:35 +0000

    feat: port CategoryTheory.WithTerminal (#2630)
    Co-authored-by: Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
    Co-authored-by: Moritz Firsching <firsching@google.com>
    Co-authored-by: Jujian Zhang <jujian.zhang1998@outlook.com>
    Co-authored-by: Scott Morrison <scott.morrison@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -975,6 +975,7 @@ import Mathlib.CategoryTheory.Triangulated.Rotate
 import Mathlib.CategoryTheory.Triangulated.Triangulated
 import Mathlib.CategoryTheory.Types
 import Mathlib.CategoryTheory.Whiskering
+import Mathlib.CategoryTheory.WithTerminal
 import Mathlib.CategoryTheory.Yoneda
 import Mathlib.Combinatorics.Additive.Energy
 import Mathlib.Combinatorics.Additive.Etransform
