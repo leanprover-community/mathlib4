19aa42461b3d15da945902f4aa02c627937a1a8f
Parcly Taxel <reddeloostw@gmail.com>
2023-06-06 10:18:05 +0800

    Fix errors

--- Mathlib/MeasureTheory/Measure/Haar/InnerProductSpace.lean
+++ Mathlib/MeasureTheory/Measure/Haar/InnerProductSpace.lean
@@ -18,7 +18,7 @@ A volume form induces a Lebesgue measure on general finite-dimensional real vect
 file, we discuss the specific situation of inner product spaces, where an orientation gives
 rise to a canonical volume form. We show that the measure coming from this volume form gives
 measure `1` to the parallelepiped spanned by any orthonormal basis, and that it coincides with
-the canonical `volume` from the `measure_space` instance.
+the canonical `volume` from the `MeasureSpace` instance.
 -/
 
 
@@ -33,32 +33,29 @@ section
 
 variable {m n : ℕ} [_i : Fact (finrank ℝ F = n)]
 
-include _i
-
 /-- The volume form coming from an orientation in an inner product space gives measure `1` to the
 parallelepiped associated to any orthonormal basis. This is a rephrasing of
-`abs_volume_form_apply_of_orthonormal` in terms of measures. -/
+`abs_volumeForm_apply_of_orthonormal` in terms of measures. -/
 theorem Orientation.measure_orthonormalBasis (o : Orientation ℝ F (Fin n))
-    (b : OrthonormalBasis ι ℝ F) : o.volumeForm.Measure (parallelepiped b) = 1 := by
+    (b : OrthonormalBasis ι ℝ F) : o.volumeForm.measure (parallelepiped b) = 1 := by
   have e : ι ≃ Fin n := by
     refine' Fintype.equivFinOfCardEq _
-    rw [← _i.out, finrank_eq_card_basis b.to_basis]
+    rw [← _i.out, finrank_eq_card_basis b.toBasis]
   have A : ⇑b = b.reindex e ∘ e := by
     ext x
     simp only [OrthonormalBasis.coe_reindex, Function.comp_apply, Equiv.symm_apply_apply]
   rw [A, parallelepiped_comp_equiv, AlternatingMap.measure_parallelepiped,
-    o.abs_volume_form_apply_of_orthonormal, ENNReal.ofReal_one]
+    o.abs_volumeForm_apply_of_orthonormal, ENNReal.ofReal_one]
 #align orientation.measure_orthonormal_basis Orientation.measure_orthonormalBasis
 
 /-- In an oriented inner product space, the measure coming from the canonical volume form
 associated to an orientation coincides with the volume. -/
 theorem Orientation.measure_eq_volume (o : Orientation ℝ F (Fin n)) :
-    o.volumeForm.Measure = volume := by
-  have A : o.volume_form.measure (stdOrthonormalBasis ℝ F).toBasis.parallelepiped = 1 :=
+    o.volumeForm.measure = volume := by
+  have A : o.volumeForm.measure (stdOrthonormalBasis ℝ F).toBasis.parallelepiped = 1 :=
     Orientation.measure_orthonormalBasis o (stdOrthonormalBasis ℝ F)
-  rw [add_haar_measure_unique o.volume_form.measure
-      (stdOrthonormalBasis ℝ F).toBasis.parallelepiped,
-    A, one_smul]
+  rw [addHaarMeasure_unique o.volumeForm.measure
+    (stdOrthonormalBasis ℝ F).toBasis.parallelepiped, A, one_smul]
   simp only [volume, Basis.addHaar]
 #align orientation.measure_eq_volume Orientation.measure_eq_volume
 
@@ -69,8 +66,7 @@ parallelepiped spanned by any orthonormal basis. -/
 theorem OrthonormalBasis.volume_parallelepiped (b : OrthonormalBasis ι ℝ F) :
     volume (parallelepiped b) = 1 := by
   haveI : Fact (finrank ℝ F = finrank ℝ F) := ⟨rfl⟩
-  let o := (stdOrthonormalBasis ℝ F).toBasis.Orientation
+  let o := (stdOrthonormalBasis ℝ F).toBasis.orientation
   rw [← o.measure_eq_volume]
-  exact o.measure_orthonormal_basis b
+  exact o.measure_orthonormalBasis b
 #align orthonormal_basis.volume_parallelepiped OrthonormalBasis.volume_parallelepiped
-
