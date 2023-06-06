01b8d276895cf30aa59c328ebf5505610d01131a
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2023-06-06 15:36:57 +0200

    Fix

--- Mathlib/RingTheory/RootsOfUnity/Basic.lean
+++ Mathlib/RingTheory/RootsOfUnity/Basic.lean
@@ -255,11 +255,11 @@ theorem card_rootsOfUnity : Fintype.card (rootsOfUnity k R) ≤ k :=
 variable {k R}
 
 theorem map_root_of_unity_eq_pow_self [RingHomClass F R R] (σ : F) (ζ : rootsOfUnity k R) :
-    ∃ m : ℕ, σ ζ = ζ ^ m := by
+    ∃ m : ℕ, σ (ζ : Rˣ) = ((ζ : Rˣ) : R) ^ m := by
   obtain ⟨m, hm⟩ := MonoidHom.map_cyclic (restrictRootsOfUnity σ k)
   rw [← restrictRootsOfUnity_coe_apply, hm, zpow_eq_mod_orderOf, ←
     Int.toNat_of_nonneg
-      (m.mod_nonneg (int.coe_nat_ne_zero.mpr (pos_iff_ne_zero.mp (orderOf_pos ζ)))),
+      (m.emod_nonneg (Int.coe_nat_ne_zero.mpr (pos_iff_ne_zero.mp (orderOf_pos ζ)))),
     zpow_ofNat, rootsOfUnity.coe_pow]
   exact ⟨(m % orderOf ζ).toNat, rfl⟩
 #align map_root_of_unity_eq_pow_self map_root_of_unity_eq_pow_self
