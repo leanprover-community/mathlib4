2076bf0faf2bbc5c3455c6f7c68429e336815f02
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2023-06-06 22:12:59 +0000

    feat: port LinearAlgebra.QuadraticForm.Real (#4758)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1912,6 +1912,7 @@ import Mathlib.LinearAlgebra.QuadraticForm.Basic
 import Mathlib.LinearAlgebra.QuadraticForm.Complex
 import Mathlib.LinearAlgebra.QuadraticForm.Isometry
 import Mathlib.LinearAlgebra.QuadraticForm.Prod
+import Mathlib.LinearAlgebra.QuadraticForm.Real
 import Mathlib.LinearAlgebra.Quotient
 import Mathlib.LinearAlgebra.QuotientPi
 import Mathlib.LinearAlgebra.Ray
