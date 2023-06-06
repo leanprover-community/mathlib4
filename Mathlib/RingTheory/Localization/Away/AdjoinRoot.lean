e401209a20efc20742562c7a124a949531898509
Jon Eugster <eugster.jon@gmail.com>
2023-06-06 11:16:31 +0200

    it compiles

--- Mathlib/RingTheory/Localization/Away/AdjoinRoot.lean
+++ Mathlib/RingTheory/Localization/Away/AdjoinRoot.lean
@@ -12,7 +12,7 @@ import Mathlib.RingTheory.AdjoinRoot
 import Mathlib.RingTheory.Localization.Away.Basic
 
 /-!
-The `R`-`alg_equiv` between the localization of `R` away from `r` and
+The `R`-`AlgEquiv` between the localization of `R` away from `r` and
 `R` with an inverse of `r` adjoined.
 -/
 
@@ -21,19 +21,30 @@ open Polynomial AdjoinRoot Localization
 
 variable {R : Type _} [CommRing R]
 
+-- Porting note: `cannot find synthesization order for instance ` but this is probably a bad hack.
+set_option synthInstance.checkSynthOrder false
+
 attribute [local instance] IsLocalization.algHom_subsingleton AdjoinRoot.algHom_subsingleton
 
-/-- The `R`-`alg_equiv` between the localization of `R` away from `r` and
+/-- The `R`-`AlgEquiv` between the localization of `R` away from `r` and
     `R` with an inverse of `r` adjoined. -/
-noncomputable def Localization.awayEquivAdjoin (r : R) : Away r ≃ₐ[R] AdjoinRoot (C r * X - 1) :=
-  AlgEquiv.ofAlgHom
-    { awayLift _ r _ with
+noncomputable def Localization.awayEquivAdjoin (r : R) : Away r ≃ₐ[R] AdjoinRoot (C r * X - 1) := by
+  have : Subsingleton (AdjoinRoot (C r * X - 1) →ₐ[R] AdjoinRoot (C r * X - 1))
+  · infer_instance
+  have : Subsingleton (Away r →ₐ[R] Away r)
+  · infer_instance
+  exact AlgEquiv.ofAlgHom
+    { awayLift _ r
+      -- Porting note: This argument used to be found automatically, i.e. `_`
+      (isUnit_of_mul_eq_one ((algebraMap R (AdjoinRoot (C r * X - 1))) r) (root (C r * X - 1))
+        (root_isInv r)) with
       commutes' :=
-        IsLocalization.Away.AwayMap.lift_eq r (isUnit_of_mul_eq_one _ _ <| root_is_inv r) }
+        IsLocalization.Away.AwayMap.lift_eq r (isUnit_of_mul_eq_one _ _ <| root_isInv r) }
     (liftHom _ (IsLocalization.Away.invSelf r) <| by
       simp only [map_sub, map_mul, aeval_C, aeval_X, IsLocalization.Away.mul_invSelf, aeval_one,
         sub_self])
-    (Subsingleton.elim _ _) (Subsingleton.elim _ _)
+    (Subsingleton.elim _ _)
+    (Subsingleton.elim _ _)
 #align localization.away_equiv_adjoin Localization.awayEquivAdjoin
 
 theorem IsLocalization.adjoin_inv (r : R) : IsLocalization.Away r (AdjoinRoot <| C r * X - 1) :=
@@ -42,7 +53,6 @@ theorem IsLocalization.adjoin_inv (r : R) : IsLocalization.Away r (AdjoinRoot <|
 
 theorem IsLocalization.Away.finitePresentation (r : R) {S} [CommRing S] [Algebra R S]
     [IsLocalization.Away r S] : Algebra.FinitePresentation R S :=
-  (AdjoinRoot.finitePresentation _).Equiv <|
+  (AdjoinRoot.finitePresentation _).equiv <|
     (Localization.awayEquivAdjoin r).symm.trans <| IsLocalization.algEquiv (Submonoid.powers r) _ _
 #align is_localization.away.finite_presentation IsLocalization.Away.finitePresentation
-
