1eef42593ff29ee85606c2ba027106e0b926b5c0
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2023-06-06 15:31:52 +0200

    Fix

--- Mathlib/RingTheory/RootsOfUnity/Basic.lean
+++ Mathlib/RingTheory/RootsOfUnity/Basic.lean
@@ -246,8 +246,8 @@ theorem card_rootsOfUnity : Fintype.card (rootsOfUnity k R) ≤ k :=
   calc
     Fintype.card (rootsOfUnity k R) = Fintype.card { x // x ∈ nthRoots k (1 : R) } :=
       Fintype.card_congr (rootsOfUnityEquivNthRoots R k)
-    _ ≤ (nthRoots k (1 : R)).attach.card := (Multiset.card_le_of_le (Multiset.dedup_le _))
-    _ = (nthRoots k (1 : R)).card := Multiset.card_attach
+    _ ≤ Multiset.card (nthRoots k (1 : R)).attach := (Multiset.card_le_of_le (Multiset.dedup_le _))
+    _ = Multiset.card (nthRoots k (1 : R)) := Multiset.card_attach
     _ ≤ k := card_nthRoots k 1
 
 #align card_roots_of_unity card_rootsOfUnity
