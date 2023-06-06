aecdd1618511a0168f71d1acca001f54003fce2e
Pol_tta <pol_tta@outlook.jp>
2023-06-06 04:38:40 +0000

    feat: port MeasureTheory.Function.AEEqOfIntegral (#4711)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1957,6 +1957,7 @@ import Mathlib.MeasureTheory.Decomposition.Jordan
 import Mathlib.MeasureTheory.Decomposition.SignedHahn
 import Mathlib.MeasureTheory.Decomposition.UnsignedHahn
 import Mathlib.MeasureTheory.Function.AEEqFun
+import Mathlib.MeasureTheory.Function.AEEqOfIntegral
 import Mathlib.MeasureTheory.Function.AEMeasurableOrder
 import Mathlib.MeasureTheory.Function.AEMeasurableSequence
 import Mathlib.MeasureTheory.Function.ConvergenceInMeasure
