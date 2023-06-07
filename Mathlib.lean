d2f2b0e5c6a50c788a8534a050313dd226c109e4
Jon Eugster <eugster.jon@gmail.com>
2023-06-07 16:01:21 +0000

    feat: port Algebra.Category.ModuleCat.Algebra (#4801)

--- Mathlib.lean
+++ Mathlib.lean
@@ -46,6 +46,7 @@ import Mathlib.Algebra.Category.GroupCat.Zero
 import Mathlib.Algebra.Category.GroupWithZeroCat
 import Mathlib.Algebra.Category.ModuleCat.Abelian
 import Mathlib.Algebra.Category.ModuleCat.Adjunctions
+import Mathlib.Algebra.Category.ModuleCat.Algebra
 import Mathlib.Algebra.Category.ModuleCat.Basic
 import Mathlib.Algebra.Category.ModuleCat.Biproducts
 import Mathlib.Algebra.Category.ModuleCat.Colimits
