e273ca6bc8d521248207feb0b72e37c3411d2621
Jon Eugster <eugster.jon@gmail.com>
2023-06-07 16:45:44 +0200

    fixes

--- Mathlib/Algebra/Category/ModuleCat/Algebra.lean
+++ Mathlib/Algebra/Category/ModuleCat/Algebra.lean
@@ -15,25 +15,26 @@ import Mathlib.Algebra.Category.ModuleCat.Basic
 /-!
 # Additional typeclass for modules over an algebra
 
-For an object in `M : Module A`, where `A` is a `k`-algebra,
+For an object in `M : ModuleCat A`, where `A` is a `k`-algebra,
 we provide additional typeclasses on the underlying type `M`,
-namely `module k M` and `is_scalar_tower k A M`.
+namely `Module k M` and `IsScalarTower k A M`.
 These are not made into instances by default.
 
-We provide the `linear k (Module A)` instance.
+We provide the `Linear k (ModuleCat A)` instance.
 
 ## Note
 
-If you begin with a `[module k M] [module A M] [is_scalar_tower k A M]`,
-and build a bundled module via `Module.of A M`,
+If you begin with a `[Module k M] [Module A M] [IsScalarTower k A M]`,
+and build a bundled module via `ModuleCat.of A M`,
 these instances will not necessarily agree with the original ones.
 
-It seems without making a parallel version `Module' k A`, for modules over a `k`-algebra `A`,
+It seems without making a parallel version `ModuleCat' k A`, for modules over a `k`-algebra `A`,
 that carries these typeclasses, this seems hard to achieve.
-(An alternative would be to always require these typeclasses, and remove the original `Module`,
-requiring users to write `Module' ℤ A` when `A` is merely a ring.)
+(An alternative would be to always require these typeclasses, and remove the original `ModuleCat`,
+requiring users to write `ModuleCat' ℤ A` when `A` is merely a ring.)
 -/
 
+set_option linter.uppercaseLean3 false
 
 universe v u w
 
@@ -45,8 +46,7 @@ variable {k : Type u} [Field k]
 
 variable {A : Type w} [Ring A] [Algebra k A]
 
-/-- Type synonym for considering a module over a `k`-algebra as a `k`-module.
--/
+/-- Type synonym for considering a module over a `k`-algebra as a `k`-module. -/
 def moduleOfAlgebraModule (M : ModuleCat.{v} A) : Module k M :=
   RestrictScalars.module k A M
 #align Module.module_of_algebra_Module ModuleCat.moduleOfAlgebraModule
@@ -60,9 +60,17 @@ theorem isScalarTower_of_algebra_moduleCat (M : ModuleCat.{v} A) : IsScalarTower
 attribute [scoped instance] ModuleCat.isScalarTower_of_algebra_moduleCat
 
 -- We verify that the morphism spaces become `k`-modules.
-example (M N : ModuleCat.{v} A) : Module k (M ⟶ N) := by infer_instance
+example (M N : ModuleCat.{v} A) : Module k (M ⟶ N) := LinearMap.module
+-- Porting note: used to be `by infer_instance` instead of `LinearMap.module`
 
-instance linearOverField : Linear k (ModuleCat.{v} A) where homModule M N := by infer_instance
+instance linearOverField : Linear k (ModuleCat.{v} A) where
+  --Porting note: used to be `by infer_instance` instead of `LinearMap.module`
+  homModule M N := LinearMap.module
+  smul_comp := by
+    -- Porting note: this was automatic by `aesop_cat`
+    aesop_cat_nonterminal
+    rw [LinearMap.smul_apply, LinearMap.smul_apply, LinearMap.map_smul_of_tower]
+    rfl
 #align Module.linear_over_field ModuleCat.linearOverField
 
 end ModuleCat
