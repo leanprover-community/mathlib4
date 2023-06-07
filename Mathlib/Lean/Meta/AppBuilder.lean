a5e11a73cd4af50c110aad56b7ea2fed7dfb55c0
thorimur <68410468+thorimur@users.noreply.github.com>
2023-06-07 02:38:46 -0400

    feat: open private defs, adapt withAppBuilderTrace

--- Mathlib/Lean/Meta/AppBuilder.lean
+++ Mathlib/Lean/Meta/AppBuilder.lean
@@ -22,6 +22,23 @@ if newly-created implicit argument mvars are unassigned, instead returning them 
 
 -/
 
+open Lean Meta
+
+open private mkFun throwAppBuilderException withAppBuilderTrace from Lean.Meta.AppBuilder
+
+/-- Like `withAppBuilderTrace`, but generalized to arbitrary return types. -/
+private def withAppBuilderTrace' [ToMessageData α] [ToMessageData β] [ToMessageData γ]
+    (f : α) (xs : β) (k : MetaM γ) : MetaM γ :=
+  let emoji | .ok .. => checkEmoji | .error .. => crossEmoji
+  withTraceNode `Meta.appBuilder (return m!"{emoji ·} f: {f}, xs: {xs}") do
+    try
+      let res ← k
+      trace[Meta.appBuilder.result] "{res}"
+      pure res
+    catch ex =>
+      trace[Meta.appBuilder.error] ex.toMessageData
+      throw ex
+
 
 -/
 
