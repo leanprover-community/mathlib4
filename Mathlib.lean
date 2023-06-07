4eebfa2e3c86287fd46618b928c2e3caad946ed5
Jason Yuen <jason_yuen2007@hotmail.com>
2023-06-07 16:52:48 +0000

    feat: port Algebra.Order.Interval (#4464)
    Co-authored-by: Xavier-François Roblot <46200072+xroblot@users.noreply.github.com>
    Co-authored-by: Komyyy <pol_tta@outlook.jp>

--- Mathlib.lean
+++ Mathlib.lean
@@ -285,6 +285,7 @@ import Mathlib.Algebra.Order.Group.WithTop
 import Mathlib.Algebra.Order.Hom.Basic
 import Mathlib.Algebra.Order.Hom.Monoid
 import Mathlib.Algebra.Order.Hom.Ring
+import Mathlib.Algebra.Order.Interval
 import Mathlib.Algebra.Order.Invertible
 import Mathlib.Algebra.Order.Kleene
 import Mathlib.Algebra.Order.LatticeGroup
