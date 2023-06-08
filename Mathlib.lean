832c78a693b5f316c20828cd11d5a701e84ba25d
Riccardo Brasca <riccardo.brasca@gmail.com>
2023-06-08 11:30:14 +0000

    feat: port FieldTheory.Adjoin (#4723)
    Co-authored-by: Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
    Co-authored-by: adomani <adomani@gmail.com>
    Co-authored-by: Chris Hughes <chrishughes24@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1653,6 +1653,7 @@ import Mathlib.Dynamics.Flow
 import Mathlib.Dynamics.Minimal
 import Mathlib.Dynamics.OmegaLimit
 import Mathlib.Dynamics.PeriodicPts
+import Mathlib.FieldTheory.Adjoin
 import Mathlib.FieldTheory.AxGrothendieck
 import Mathlib.FieldTheory.ChevalleyWarning
 import Mathlib.FieldTheory.Finite.Basic
