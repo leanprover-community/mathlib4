0674e58f47f60892268bd9c3deec862286b6f5c5
Pol'tta / Miyahara Kō <pol_tta@outlook.jp>
2023-06-08 04:46:14 +0000

    feat: port MeasureTheory.Function.Jacobian (#4839)

--- Mathlib.lean
+++ Mathlib.lean
@@ -2020,6 +2020,7 @@ import Mathlib.MeasureTheory.Function.ConvergenceInMeasure
 import Mathlib.MeasureTheory.Function.Egorov
 import Mathlib.MeasureTheory.Function.EssSup
 import Mathlib.MeasureTheory.Function.Floor
+import Mathlib.MeasureTheory.Function.Jacobian
 import Mathlib.MeasureTheory.Function.L1Space
 import Mathlib.MeasureTheory.Function.L2Space
 import Mathlib.MeasureTheory.Function.LocallyIntegrable
