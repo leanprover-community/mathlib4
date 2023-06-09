9f017855e9815fd19ac264e083f774c886caf3b6
Dagur Tómas Ásgeirsson <dagurtomas@gmail.com>
2023-06-09 14:11:26 +0000

    feat: port Topology.Category.Profinite.CofilteredLimit (#4889)

--- Mathlib.lean
+++ Mathlib.lean
@@ -2706,6 +2706,7 @@ import Mathlib.Topology.Category.Compactum
 import Mathlib.Topology.Category.Locale
 import Mathlib.Topology.Category.Profinite.AsLimit
 import Mathlib.Topology.Category.Profinite.Basic
+import Mathlib.Topology.Category.Profinite.CofilteredLimit
 import Mathlib.Topology.Category.Profinite.Projective
 import Mathlib.Topology.Category.TopCat.Adjunctions
 import Mathlib.Topology.Category.TopCat.Basic
