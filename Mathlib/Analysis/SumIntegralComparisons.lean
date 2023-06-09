9975a40ce7bcb959fd4a97d843db1c400e6f445e
Parcly Taxel <reddeloostw@gmail.com>
2023-06-09 20:30:47 +0800

    Fix errors

--- Mathlib/Analysis/SumIntegralComparisons.lean
+++ Mathlib/Analysis/SumIntegralComparisons.lean
@@ -21,21 +21,21 @@ It is often the case that error terms in analysis can be computed by comparing
 an infinite sum to the improper integral of an antitone function. This file will eventually enable
 that.
 
-At the moment it contains four lemmas in this direction: `antitone_on.integral_le_sum`,
-`antitone_on.sum_le_integral` and versions for monotone functions, which can all be paired
-with a `filter.tendsto` to estimate some errors.
+At the moment it contains four lemmas in this direction: `AntitoneOn.integral_le_sum`,
+`AntitoneOn.sum_le_integral` and versions for monotone functions, which can all be paired
+with a `Filter.Tendsto` to estimate some errors.
 
 `TODO`: Add more lemmas to the API to directly address limiting issues
 
 ## Main Results
 
-* `antitone_on.integral_le_sum`: The integral of an antitone function is at most the sum of its
+* `AntitoneOn.integral_le_sum`: The integral of an antitone function is at most the sum of its
   values at integer steps aligning with the left-hand side of the interval
-* `antitone_on.sum_le_integral`: The sum of an antitone function along integer steps aligning with
+* `AntitoneOn.sum_le_integral`: The sum of an antitone function along integer steps aligning with
   the right-hand side of the interval is at most the integral of the function along that interval
-* `monotone_on.integral_le_sum`: The integral of a monotone function is at most the sum of its
+* `MonotoneOn.integral_le_sum`: The integral of a monotone function is at most the sum of its
   values at integer steps aligning with the right-hand side of the interval
-* `monotone_on.sum_le_integral`: The sum of a monotone function along integer steps aligning with
+* `MonotoneOn.sum_le_integral`: The sum of a monotone function along integer steps aligning with
   the left-hand side of the interval is at most the integral of the function along that interval
 
 ## Tags
@@ -54,7 +54,7 @@ theorem AntitoneOn.integral_le_sum (hf : AntitoneOn f (Icc x₀ (x₀ + a))) :
     (∫ x in x₀..x₀ + a, f x) ≤ ∑ i in Finset.range a, f (x₀ + i) := by
   have hint : ∀ k : ℕ, k < a → IntervalIntegrable f volume (x₀ + k) (x₀ + (k + 1 : ℕ)) := by
     intro k hk
-    refine' (hf.mono _).IntervalIntegrable
+    refine' (hf.mono _).intervalIntegrable
     rw [uIcc_of_le]
     · apply Icc_subset_Icc
       · simp only [le_add_iff_nonneg_right, Nat.cast_nonneg]
@@ -64,13 +64,12 @@ theorem AntitoneOn.integral_le_sum (hf : AntitoneOn f (Icc x₀ (x₀ + a))) :
     (∫ x in x₀..x₀ + a, f x) = ∑ i in Finset.range a, ∫ x in x₀ + i..x₀ + (i + 1 : ℕ), f x := by
       convert (intervalIntegral.sum_integral_adjacent_intervals hint).symm
       simp only [Nat.cast_zero, add_zero]
-    _ ≤ ∑ i in Finset.range a, ∫ x in x₀ + i..x₀ + (i + 1 : ℕ), f (x₀ + i) := by
-      apply Finset.sum_le_sum fun i hi => _
+    _ ≤ ∑ i in Finset.range a, ∫ _ in x₀ + i..x₀ + (i + 1 : ℕ), f (x₀ + i) := by
+      apply Finset.sum_le_sum fun i hi => ?_
       have ia : i < a := Finset.mem_range.1 hi
       refine' intervalIntegral.integral_mono_on (by simp) (hint _ ia) (by simp) fun x hx => _
       apply hf _ _ hx.1
-      ·
-        simp only [ia.le, mem_Icc, le_add_iff_nonneg_right, Nat.cast_nonneg, add_le_add_iff_left,
+      · simp only [ia.le, mem_Icc, le_add_iff_nonneg_right, Nat.cast_nonneg, add_le_add_iff_left,
           Nat.cast_le, and_self_iff]
       · refine' mem_Icc.2 ⟨le_trans (by simp) hx.1, le_trans hx.2 _⟩
         simp only [add_le_add_iff_left, Nat.cast_le, Nat.succ_le_of_lt ia]
@@ -83,11 +82,11 @@ theorem AntitoneOn.integral_le_sum_Ico (hab : a ≤ b) (hf : AntitoneOn f (Set.I
   conv =>
     congr
     congr
-    skip
-    skip
+    · skip
+    · skip
     rw [add_comm]
-    skip
-    skip
+    · skip
+    · skip
     congr
     congr
     rw [← zero_add a]
@@ -95,7 +94,7 @@ theorem AntitoneOn.integral_le_sum_Ico (hab : a ≤ b) (hf : AntitoneOn f (Set.I
   conv =>
     rhs
     congr
-    skip
+    · skip
     ext
     rw [Nat.cast_add]
   apply AntitoneOn.integral_le_sum
@@ -106,7 +105,7 @@ theorem AntitoneOn.sum_le_integral (hf : AntitoneOn f (Icc x₀ (x₀ + a))) :
     (∑ i in Finset.range a, f (x₀ + (i + 1 : ℕ))) ≤ ∫ x in x₀..x₀ + a, f x := by
   have hint : ∀ k : ℕ, k < a → IntervalIntegrable f volume (x₀ + k) (x₀ + (k + 1 : ℕ)) := by
     intro k hk
-    refine' (hf.mono _).IntervalIntegrable
+    refine' (hf.mono _).intervalIntegrable
     rw [uIcc_of_le]
     · apply Icc_subset_Icc
       · simp only [le_add_iff_nonneg_right, Nat.cast_nonneg]
@@ -114,16 +113,14 @@ theorem AntitoneOn.sum_le_integral (hf : AntitoneOn f (Icc x₀ (x₀ + a))) :
     · simp only [add_le_add_iff_left, Nat.cast_le, Nat.le_succ]
   calc
     (∑ i in Finset.range a, f (x₀ + (i + 1 : ℕ))) =
-        ∑ i in Finset.range a, ∫ x in x₀ + i..x₀ + (i + 1 : ℕ), f (x₀ + (i + 1 : ℕ)) :=
-      by simp
+        ∑ i in Finset.range a, ∫ _ in x₀ + i..x₀ + (i + 1 : ℕ), f (x₀ + (i + 1 : ℕ)) := by simp
     _ ≤ ∑ i in Finset.range a, ∫ x in x₀ + i..x₀ + (i + 1 : ℕ), f x := by
-      apply Finset.sum_le_sum fun i hi => _
+      apply Finset.sum_le_sum fun i hi => ?_
       have ia : i + 1 ≤ a := Finset.mem_range.1 hi
       refine' intervalIntegral.integral_mono_on (by simp) (by simp) (hint _ ia) fun x hx => _
       apply hf _ _ hx.2
-      · refine'
-          mem_Icc.2
-            ⟨le_trans ((le_add_iff_nonneg_right _).2 (Nat.cast_nonneg _)) hx.1, le_trans hx.2 _⟩
+      · refine' mem_Icc.2 ⟨le_trans ((le_add_iff_nonneg_right _).2 (Nat.cast_nonneg _)) hx.1,
+          le_trans hx.2 _⟩
         simp only [Nat.cast_le, add_le_add_iff_left, ia]
       · refine' mem_Icc.2 ⟨(le_add_iff_nonneg_right _).2 (Nat.cast_nonneg _), _⟩
         simp only [add_le_add_iff_left, Nat.cast_le, ia]
@@ -140,16 +137,16 @@ theorem AntitoneOn.sum_le_integral_Ico (hab : a ≤ b) (hf : AntitoneOn f (Set.I
     congr
     congr
     rw [← zero_add a]
-    skip
-    skip
-    skip
+    · skip
+    · skip
+    · skip
     rw [add_comm]
   rw [← Finset.sum_Ico_add, Nat.Ico_zero_eq_range]
   conv =>
     lhs
     congr
     congr
-    skip
+    · skip
     ext
     rw [add_assoc, Nat.cast_add]
   apply AntitoneOn.sum_le_integral
@@ -179,4 +176,3 @@ theorem MonotoneOn.integral_le_sum_Ico (hab : a ≤ b) (hf : MonotoneOn f (Set.I
   rw [← neg_le_neg_iff, ← Finset.sum_neg_distrib, ← intervalIntegral.integral_neg]
   exact hf.neg.sum_le_integral_Ico hab
 #align monotone_on.integral_le_sum_Ico MonotoneOn.integral_le_sum_Ico
-
