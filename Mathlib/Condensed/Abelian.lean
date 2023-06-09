122899fc1b4e33cd56c69ebe9a60f29250ad8e1a
adamtopaz <github@adamtopaz.com>
2023-06-09 08:39:27 -0600

    add files

---
+++ Mathlib/Condensed/Abelian.lean
@@ -0,0 +1,24 @@
+/-
+Copyright (c) 2021 Adam Topaz. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Adam Topaz
+-/
+import Mathlib.CategoryTheory.Sites.AbelianSheaf
+import Mathlib.Condensed.Basic
+
+/-!
+
+Condensed abelian groups form an abelian category.
+
+-/
+
+universe u
+
+/--
+The category of condensed abelian groups, defined as sheaves of abelian groups over
+`CompHaus` with respect to the coherent Grothendieck topology.
+-/
+abbrev CondensedAb := Condensed.{u} AddCommGroupCat.{u+1}
+
+noncomputable instance CondensedAb.abelian : CategoryTheory.Abelian CondensedAb :=
+  CategoryTheory.AbelianSheaf.abelian _
