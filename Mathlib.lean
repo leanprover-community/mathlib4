ef9a563baafacb72eccdc141b84cabe7cc2afcc5
Johan Commelin <johan@commelin.net>
2023-06-09 23:11:05 +0000

    feat: port FieldTheory.IsAlgClosed.Basic (#4888)
    Co-authored-by: Moritz Firsching <firsching@google.com>
    Co-authored-by: Chris Hughes <chrishughes24@gmail.com>
    Co-authored-by: Vierkantor <vierkantor@vierkantor.com>
    Co-authored-by: Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
    Co-authored-by: Scott Morrison <scott.morrison@anu.edu.au>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1692,6 +1692,7 @@ import Mathlib.FieldTheory.Finite.Polynomial
 import Mathlib.FieldTheory.Finiteness
 import Mathlib.FieldTheory.Fixed
 import Mathlib.FieldTheory.IntermediateField
+import Mathlib.FieldTheory.IsAlgClosed.Basic
 import Mathlib.FieldTheory.Laurent
 import Mathlib.FieldTheory.Minpoly.Basic
 import Mathlib.FieldTheory.Minpoly.Field
