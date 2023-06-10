23dc64a4f02b3ac138194c935bbef3d1ddfe9a7c
Yury G. Kudryashov <urkud@urkud.name>
2023-06-10 18:29:54 -0500

    automated fixes
    Mathbin -> Mathlib
    fix certain import statements
    move "by" to end of line
    add import to Mathlib.lean

--- Mathlib/NumberTheory/Wilson.lean
+++ Mathlib/NumberTheory/Wilson.lean
@@ -8,7 +8,7 @@ Authors: John Nicol
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
-import Mathbin.NumberTheory.LegendreSymbol.GaussEisensteinLemmas
+import Mathlib.NumberTheory.LegendreSymbol.GaussEisensteinLemmas
 
 /-!
 # Wilson's theorem.
@@ -37,8 +37,7 @@ namespace Nat
 variable {n : ℕ}
 
 /-- For `n ≠ 1`, `(n-1)!` is congruent to `-1` modulo `n` only if n is prime. -/
-theorem prime_of_fac_equiv_neg_one (h : ((n - 1)! : ZMod n) = -1) (h1 : n ≠ 1) : Prime n :=
-  by
+theorem prime_of_fac_equiv_neg_one (h : ((n - 1)! : ZMod n) = -1) (h1 : n ≠ 1) : Prime n := by
   rcases eq_or_ne n 0 with (rfl | h0)
   · norm_num at h 
   replace h1 : 1 < n := n.two_le_iff.mpr ⟨h0, h1⟩
@@ -50,8 +49,7 @@ theorem prime_of_fac_equiv_neg_one (h : ((n - 1)! : ZMod n) = -1) (h1 : n ≠ 1)
 #align nat.prime_of_fac_equiv_neg_one Nat.prime_of_fac_equiv_neg_one
 
 /-- **Wilson's Theorem**: For `n ≠ 1`, `(n-1)!` is congruent to `-1` modulo `n` iff n is prime. -/
-theorem prime_iff_fac_equiv_neg_one (h : n ≠ 1) : Prime n ↔ ((n - 1)! : ZMod n) = -1 :=
-  by
+theorem prime_iff_fac_equiv_neg_one (h : n ≠ 1) : Prime n ↔ ((n - 1)! : ZMod n) = -1 := by
   refine' ⟨fun h1 => _, fun h2 => prime_of_fac_equiv_neg_one h2 h⟩
   haveI := Fact.mk h1
   exact ZMod.wilsons_lemma n
