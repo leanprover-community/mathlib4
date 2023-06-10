359420003e021b90277df0dfceffbc6b3e8f1235
Yury G. Kudryashov <urkud@urkud.name>
2023-06-09 19:21:40 -0500

    Fix compile

--- Mathlib/Analysis/Complex/Polynomial.lean
+++ Mathlib/Analysis/Complex/Polynomial.lean
@@ -19,7 +19,6 @@ This file proves that every nonconstant complex polynomial has a root using Liou
 As a consequence, the complex numbers are algebraically closed.
 -/
 
-
 open Polynomial
 
 open scoped Polynomial
@@ -30,17 +29,17 @@ namespace Complex
   has a root -/
 theorem exists_root {f : ℂ[X]} (hf : 0 < degree f) : ∃ z : ℂ, IsRoot f z := by
   contrapose! hf
-  obtain ⟨c, hc⟩ := (f.differentiable.inv hf).exists_const_forall_eq_of_bounded _
+  have : Metric.Bounded (Set.range (eval · f)⁻¹)
+  · obtain ⟨z₀, h₀⟩ := f.exists_forall_norm_le
+    simp only [Pi.inv_apply, bounded_iff_forall_norm_le, Set.forall_range_iff, norm_inv]
+    exact ⟨‖eval z₀ f‖⁻¹, fun z => inv_le_inv_of_le (norm_pos_iff.2 <| hf z₀) (h₀ z)⟩
+  obtain ⟨c, hc⟩ := (f.differentiable.inv hf).exists_const_forall_eq_of_bounded this
   · obtain rfl : f = C c⁻¹ := Polynomial.funext fun z => by rw [eval_C, ← hc z, inv_inv]
     exact degree_C_le
-  · obtain ⟨z₀, h₀⟩ := f.exists_forall_norm_le
-    simp only [bounded_iff_forall_norm_le, Set.forall_range_iff, norm_inv]
-    exact ⟨‖eval z₀ f‖⁻¹, fun z => inv_le_inv_of_le (norm_pos_iff.2 <| hf z₀) (h₀ z)⟩
 #align complex.exists_root Complex.exists_root
 
 instance isAlgClosed : IsAlgClosed ℂ :=
-  IsAlgClosed.of_exists_root _ fun p _ hp => Complex.exists_root <| degree_pos_of_irreducible hp
+  IsAlgClosed.of_exists_root _ fun _p _ hp => Complex.exists_root <| degree_pos_of_irreducible hp
 #align complex.is_alg_closed Complex.isAlgClosed
 
 end Complex
-
