14bfe91764d258158ec47d5de74becd9e45303d8
Eric Wieser <wieser.eric@gmail.com>
2023-06-11 16:26:46 +0000

    fix

--- Mathlib/Order/Filter/Basic.lean
+++ Mathlib/Order/Filter/Basic.lean
@@ -82,7 +82,7 @@ we do *not* require. This gives `Filter X` better formal properties, in particul
 
 
 open Function Set Order
-open scoped Classical hiding by_cases not_not
+open scoped Classical
 
 universe u v w x y
 
