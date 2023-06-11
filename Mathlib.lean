45f44831651beff2b0b04652a88a522a688c0552
Riccardo Brasca <riccardo.brasca@gmail.com>
2023-06-11 13:19:38 +0000

    Port/Algebra.Category.Module.FilteredColimits (#4949)
    Co-authored-by: Scott Morrison <scott.morrison@anu.edu.au>

--- Mathlib.lean
+++ Mathlib.lean
@@ -52,6 +52,7 @@ import Mathlib.Algebra.Category.ModuleCat.Basic
 import Mathlib.Algebra.Category.ModuleCat.Biproducts
 import Mathlib.Algebra.Category.ModuleCat.Colimits
 import Mathlib.Algebra.Category.ModuleCat.EpiMono
+import Mathlib.Algebra.Category.ModuleCat.FilteredColimits
 import Mathlib.Algebra.Category.ModuleCat.Images
 import Mathlib.Algebra.Category.ModuleCat.Kernels
 import Mathlib.Algebra.Category.ModuleCat.Limits
