a7a494d678bc1643c85bac3eb9c79ec8c7e6aca5
Christopher Hoskin <christopher.hoskin@gmail.com>
2023-06-11 21:39:22 +0100

    Re-interp orderings in terms of inclusions

--- Mathlib/Topology/Order/ScottTopology.lean
+++ Mathlib/Topology/Order/ScottTopology.lean
@@ -384,16 +384,29 @@ lemma isClosed_iff_lower_and_closed_under_Directed_Sup {s : Set α} : IsClosed s
 
 end complete_lattice
 
-lemma UpperSet_le_Scott [Preorder α] [TopologicalSpace α] [ScottTopology α] :
+variable [Preorder α]
+
+lemma UpperSet_le_Scott [TopologicalSpace α] [ScottTopology α] :
 upperSetTopology ≤ ‹TopologicalSpace α› := by
   rw [ScottTopology.topology_eq α, ScottTopology']
   apply le_sup_left
 
-lemma Scott_Hausdorff_le_Scott [Preorder α] [TopologicalSpace α] [ScottTopology α] :
+lemma ScottOpen_implies_UpperSetOpen {s : Set α} :
+  IsOpen (WithScottTopology.ofScott ⁻¹' s) → upperSetTopology.IsOpen s := UpperSet_le_Scott _
+
+lemma Scott_Hausdorff_le_Scott [TopologicalSpace α] [ScottTopology α] :
 ScottHausdorffTopology ≤ ‹TopologicalSpace α› := by
   rw [ScottTopology.topology_eq α, ScottTopology']
   apply le_sup_right
 
-lemma Scott_Hausdorff_le_Lower [Preorder α] [TopologicalSpace α] [LowerTopology α] :
+lemma ScottOpen_implies_ScottHausdorffOpen {s : Set α} :
+  IsOpen (WithScottTopology.ofScott ⁻¹' s) → ScottHausdorffTopology.IsOpen s :=
+  Scott_Hausdorff_le_Scott _
+
+lemma Scott_Hausdorff_le_Lower [TopologicalSpace α] [LowerTopology α] :
 ScottHausdorffTopology ≤  ‹TopologicalSpace α› :=
   fun _ h => ScottHausdorffTopology.Lower_IsOpen (LowerTopology.isLowerSet_of_isOpen h)
+
+lemma ScottHausdorffOpen_implies_LowerOpen {s : Set α} :
+  IsOpen (WithLowerTopology.ofLower ⁻¹' s) → ScottHausdorffTopology.IsOpen s :=
+  Scott_Hausdorff_le_Lower _
