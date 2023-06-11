bd0681b8549f07cbe5d920d58cc28dda44cb9abe
Jason Yuen <jason_yuen2007@hotmail.com>
2023-06-11 14:32:27 +0000

    feat: port MeasureTheory.Measure.Hausdorff (#4886)
    Co-authored-by: Moritz Firsching <firsching@google.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -2143,6 +2143,7 @@ import Mathlib.MeasureTheory.Measure.Haar.InnerProductSpace
 import Mathlib.MeasureTheory.Measure.Haar.NormedSpace
 import Mathlib.MeasureTheory.Measure.Haar.OfBasis
 import Mathlib.MeasureTheory.Measure.Haar.Quotient
+import Mathlib.MeasureTheory.Measure.Hausdorff
 import Mathlib.MeasureTheory.Measure.Lebesgue.Basic
 import Mathlib.MeasureTheory.Measure.Lebesgue.Complex
 import Mathlib.MeasureTheory.Measure.Lebesgue.EqHaar
