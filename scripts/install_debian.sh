79714ad77350c2b26ca09d4198a66714f29321e8
Scott Morrison <scott.morrison@anu.edu.au>
2023-06-11 19:12:12 +1000

    chore: adapt install scripts from mathlib-tools

---
+++ scripts/install_debian.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+sudo apt install -y git curl
+# The following test is needed in case VScode or VSCodium was installed by other
+# means (e.g. using Ubuntu snap)
+vsc="$(which code || which codium)"
+if [ -z "$vsc" ]; then
+  wget -O code.deb https://go.microsoft.com/fwlink/?LinkID=760868
+  sudo apt install -y ./code.deb
+  rm code.deb
+  vsc=code
+fi
+"$vsc" --install-extension leanprover.lean4
+wget https://raw.githubusercontent.com/leanprover/elan/master/elan-init.sh
+bash elan-init.sh -y
+rm elan-init.sh
+. ~/.profile
---
+++ scripts/install_macos.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Install Homebrew
+set -e
+
+if ! which brew > /dev/null; then
+    # Install Homebrew
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+else
+    # Update it, in case it has been ages since it's been updated
+    brew update
+fi
+
+brew install elan
+elan toolchain install stable
+elan default stable
+
+# Install and configure VS Code
+if ! which code > /dev/null; then
+    brew install --cask visual-studio-code
+fi
+code --install-extension leanprover.lean4
