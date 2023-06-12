269ae93f5e48c9a457db15850785bb143cef092f
Evgenia Karunus <lakesare@gmail.com>
2023-06-12 02:21:54 +0000

    feat: port `#explode` (#1218)
    Ported from [/explode.lean](https://github.com/leanprover-community/mathlib/blob/master/src/tactic/explode.lean).
    
    ### Example output
    ```
    lemma_5 : ∀ (p q : Prop), (¬q → ¬p) → p → q
    
    0 │          │ p            ├ Prop
    1 │          │ q            ├ Prop
    2 │          │ hNQNP        ├ ¬q → ¬p
    3 │          │ hP           ├ p
    4 │          │ Classical.em │ q ∨ ¬q
    5 │          │ hQ           │ ┌ q
    6 │5,5       │ ∀I           │ q → q
    7 │          │ hNQ          │ ┌ ¬q
    8 │2,7       │ ∀E           │ │ ¬p
    10│8,3       │ ∀E           │ │ False
    11│10        │ False.elim   │ │ q
    12│7,11      │ ∀I           │ ¬q → q
    13│4,6,12    │ Or.elim      │ q
    14│0,1,2,3,13│ ∀I           │ ∀ (p q : Prop), (¬q → ¬p) → p → q
    ```
    
    
    
    Co-authored-by: Kyle Miller <kmill31415@gmail.com>
    Co-authored-by: Mario Carneiro <di.gama@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -2558,6 +2558,9 @@ import Mathlib.Tactic.DeriveFintype
 import Mathlib.Tactic.DeriveToExpr
 import Mathlib.Tactic.Eqns
 import Mathlib.Tactic.Existsi
+import Mathlib.Tactic.Explode
+import Mathlib.Tactic.Explode.Datatypes
+import Mathlib.Tactic.Explode.Pretty
 import Mathlib.Tactic.FBinop
 import Mathlib.Tactic.FailIfNoProgress
 import Mathlib.Tactic.FieldSimp
