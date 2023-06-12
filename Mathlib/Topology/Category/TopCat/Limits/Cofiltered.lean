1ede842c7a8c727220e7a151e48a219a841f6792
Eric Wieser <wieser.eric@gmail.com>
2023-06-12 10:46:42 +0000

    fix build

--- Mathlib/Topology/Category/TopCat/Limits/Cofiltered.lean
+++ Mathlib/Topology/Category/TopCat/Limits/Cofiltered.lean
@@ -8,7 +8,7 @@ Authors: Patrick Massot, Scott Morrison, Mario Carneiro, Andrew Yang
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
-import Mathlib.Topology.Category.Top.Limits.Basic
+import Mathlib.Topology.Category.TopCat.Limits.Basic
 
 /-!
 # Cofiltered limits in Top.
@@ -18,6 +18,8 @@ which contain `Set.univ` and are closed under intersections, the induced *naive*
 of sets in the limit is, in fact, a topological basis.
 -/
 
+-- Porting note: every ML3 decl has an uppercase letter
+set_option linter.uppercaseLean3 false
 
 open TopologicalSpace
 
@@ -33,10 +35,8 @@ namespace TopCat
 
 section CofilteredLimit
 
-variable {J : Type v} [SmallCategory J] [IsCofiltered J] (F : J ⥤ TopCat.{max v u}) (C : Cone F)
+variable {J : Type v} [SmallCategory J] [IsCofiltered J] (F : J ⥤ TopCatMax.{v, u}) (C : Cone F)
   (hC : IsLimit C)
-
-include hC
 
 /-- Given a *compatible* collection of topological bases for the factors in a cofiltered limit
 which contain `Set.univ` and are closed under intersections, the induced *naive* collection
@@ -45,19 +45,19 @@ of sets in the limit is, in fact, a topological basis.
 theorem isTopologicalBasis_cofiltered_limit (T : ∀ j, Set (Set (F.obj j)))
     (hT : ∀ j, IsTopologicalBasis (T j)) (univ : ∀ i : J, Set.univ ∈ T i)
     (inter : ∀ (i) (U1 U2 : Set (F.obj i)), U1 ∈ T i → U2 ∈ T i → U1 ∩ U2 ∈ T i)
-    (compat : ∀ (i j : J) (f : i ⟶ j) (V : Set (F.obj j)) (hV : V ∈ T j), F.map f ⁻¹' V ∈ T i) :
+    (compat : ∀ (i j : J) (f : i ⟶ j) (V : Set (F.obj j)) (_hV : V ∈ T j), F.map f ⁻¹' V ∈ T i) :
     IsTopologicalBasis
       {U : Set C.pt | ∃ (j : _) (V : Set (F.obj j)), V ∈ T j ∧ U = C.π.app j ⁻¹' V} := by
   classical
   -- The limit cone for `F` whose topology is defined as an infimum.
-  let D := limit_cone_infi F
+  let D := limitConeInfi F
   -- The isomorphism between the cone point of `C` and the cone point of `D`.
-  let E : C.X ≅ D.X := hC.cone_point_unique_up_to_iso (limit_cone_infi_is_limit _)
-  have hE : Inducing E.hom := (TopCat.homeoOfIso E).Inducing
+  let E : C.pt ≅ D.pt := hC.conePointUniqueUpToIso (limitConeInfiIsLimit _)
+  have hE : Inducing E.hom := (TopCat.homeoOfIso E).inducing
   -- Reduce to the assertion of the theorem with `D` instead of `C`.
   suffices
-    is_topological_basis
-      {U : Set D.X | ∃ (j : _) (V : Set (F.obj j)), V ∈ T j ∧ U = D.π.app j ⁻¹' V} by
+    IsTopologicalBasis
+      {U : Set D.pt | ∃ (j : _) (V : Set (F.obj j)), V ∈ T j ∧ U = D.π.app j ⁻¹' V} by
     convert this.inducing hE
     ext U0
     constructor
@@ -67,7 +67,7 @@ theorem isTopologicalBasis_cofiltered_limit (T : ∀ j, Set (Set (F.obj j)))
       refine' ⟨j, V, hV, rfl⟩
   -- Using `D`, we can apply the characterization of the topological basis of a
   -- topology defined as an infimum...
-  convert isTopologicalBasis_iInf hT fun j (x : D.X) => D.π.app j x
+  convert isTopologicalBasis_iInf hT fun j (x : D.pt) => D.π.app j x using 1
   ext U0
   constructor
   · rintro ⟨j, V, hV, rfl⟩
@@ -75,28 +75,30 @@ theorem isTopologicalBasis_cofiltered_limit (T : ∀ j, Set (Set (F.obj j)))
     refine' ⟨U, {j}, _, _⟩
     · rintro i h
       rw [Finset.mem_singleton] at h
-      dsimp [U]
+      dsimp
       rw [dif_pos h]
       subst h
       exact hV
-    · dsimp [U]
+    · dsimp
       simp
   · rintro ⟨U, G, h1, h2⟩
-    obtain ⟨j, hj⟩ := is_cofiltered.inf_objs_exists G
-    let g : ∀ (e) (he : e ∈ G), j ⟶ e := fun _ he => (hj he).some
+    obtain ⟨j, hj⟩ := IsCofiltered.inf_objs_exists G
+    let g : ∀ (e) (_he : e ∈ G), j ⟶ e := fun _ he => (hj he).some
     let Vs : J → Set (F.obj j) := fun e => if h : e ∈ G then F.map (g e h) ⁻¹' U e else Set.univ
-    let V : Set (F.obj j) := ⋂ (e : J) (he : e ∈ G), Vs e
+    let V : Set (F.obj j) := ⋂ (e : J) (_he : e ∈ G), Vs e
     refine' ⟨j, V, _, _⟩
     · -- An intermediate claim used to apply induction along `G : Finset J` later on.
       have :
-        ∀ (S : Set (Set (F.obj j))) (E : Finset J) (P : J → Set (F.obj j)) (univ : Set.univ ∈ S)
-          (inter : ∀ A B : Set (F.obj j), A ∈ S → B ∈ S → A ∩ B ∈ S)
-          (cond : ∀ (e : J) (he : e ∈ E), P e ∈ S), (⋂ (e) (he : e ∈ E), P e) ∈ S := by
+        ∀ (S : Set (Set (F.obj j))) (E : Finset J) (P : J → Set (F.obj j)) (_univ : Set.univ ∈ S)
+          (_inter : ∀ A B : Set (F.obj j), A ∈ S → B ∈ S → A ∩ B ∈ S)
+          (_cond : ∀ (e : J) (_he : e ∈ E), P e ∈ S), (⋂ (e) (_he : e ∈ E), P e) ∈ S := by
         intro S E
-        apply E.induction_on
-        · intro P he hh
+        induction E using Finset.induction_on with
+        | empty =>
+          intro P he _hh
           simpa
-        · intro a E ha hh1 hh2 hh3 hh4 hh5
+        | @insert a E _ha hh1 =>
+          intro hh2 hh3 hh4 hh5
           rw [Finset.set_biInter_insert]
           refine' hh4 _ _ (hh5 _ (Finset.mem_insert_self _ _)) (hh1 _ hh3 hh4 _)
           intro e he
@@ -104,23 +106,25 @@ theorem isTopologicalBasis_cofiltered_limit (T : ∀ j, Set (Set (F.obj j)))
       -- use the intermediate claim to finish off the goal using `univ` and `inter`.
       refine' this _ _ _ (univ _) (inter _) _
       intro e he
-      dsimp [Vs]
+      dsimp
       rw [dif_pos he]
       exact compat j e (g e he) (U e) (h1 e he)
     · -- conclude...
       rw [h2]
-      dsimp [V]
+      change _ = (D.π.app j)⁻¹' ⋂ (e : J) (_ : e ∈ G), Vs e
       rw [Set.preimage_iInter]
-      congr 1
+      apply congrArg
       ext1 e
-      rw [Set.preimage_iInter]
-      congr 1
+      erw [Set.preimage_iInter]
+      apply congrArg
       ext1 he
-      dsimp [Vs]
+      -- Porting note: needed more hand holding here
+      change (forget TopCat).map (D.π.app e)⁻¹' U e =
+        (forget TopCat).map (D.π.app j) ⁻¹' if h : e ∈ G then F.map (g e h) ⁻¹' U e else Set.univ
       rw [dif_pos he, ← Set.preimage_comp]
-      congr 1
-      change _ = ⇑(D.π.app j ≫ F.map (g e he))
-      rw [D.w]
+      apply congrFun
+      apply congrArg
+      rw [←coe_comp, D.w]
 #align Top.is_topological_basis_cofiltered_limit TopCat.isTopologicalBasis_cofiltered_limit
 
 end CofilteredLimit
