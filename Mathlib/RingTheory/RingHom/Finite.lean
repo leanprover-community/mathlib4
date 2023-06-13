f8a81da3442582cfbda40ba893de09ad165509dc
Xavier-François Roblot <46200072+xroblot@users.noreply.github.com>
2023-06-13 21:32:35 +0200

    WIP

--- Mathlib/RingTheory/RingHom/Finite.lean
+++ Mathlib/RingTheory/RingHom/Finite.lean
@@ -23,26 +23,29 @@ open scoped TensorProduct
 
 open TensorProduct Algebra.TensorProduct
 
-theorem finite_stableUnderComposition : StableUnderComposition @Finite := by introv R hf hg;
+theorem finite_stableUnderComposition : StableUnderComposition @Finite := by
+  introv R hf hg
   exact hg.comp hf
 #align ring_hom.finite_stable_under_composition RingHom.finite_stableUnderComposition
 
 theorem finite_respectsIso : RespectsIso @Finite := by
-  apply finite_stable_under_composition.respects_iso
+  apply finite_stableUnderComposition.respectsIso
   intros
-  exact Finite.of_surjective _ e.to_equiv.surjective
+  exact Finite.of_surjective _ (RingEquiv.toEquiv _).surjective
 #align ring_hom.finite_respects_iso RingHom.finite_respectsIso
 
 theorem finite_stableUnderBaseChange : StableUnderBaseChange @Finite := by
-  refine' stable_under_base_change.mk _ finite_respects_iso _
+  refine StableUnderBaseChange.mk _ finite_respectsIso ?_
   classical
   introv h
-  skip
-  replace h : Module.Finite R T := by convert h; ext; rw [Algebra.smul_def]; rfl
-  suffices Module.Finite S (S ⊗[R] T) by change Module.Finite _ _; convert this; ext;
-    rw [Algebra.smul_def]; rfl
+  replace h : Module.Finite R T := by
+    rw [RingHom.Finite] at h; convert h; ext; simp_rw [Algebra.smul_def]; rfl
+  suffices Module.Finite S (S ⊗[R] T) by
+    rw [RingHom.Finite]
+  --  change Module.Finite _ _
+    convert this; ext;
+    simp_rw [Algebra.smul_def]; rfl
   exact inferInstance
 #align ring_hom.finite_stable_under_base_change RingHom.finite_stableUnderBaseChange
 
 end RingHom
-
