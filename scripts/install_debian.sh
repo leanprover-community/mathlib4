686b234bc30fd5cc4b90a349aeb4d94e288f9263
Scott Morrison <scott.morrison@anu.edu.au>
2023-06-13 15:57:11 +1000

    better initial incantations

--- scripts/install_debian.sh
+++ scripts/install_debian.sh
@@ -1,4 +1,6 @@
-#! /bin/bash
+#!/usr/bin/env bash
+
+set -exo pipefail
 
 sudo apt install -y git curl
 # The following test is needed in case VScode or VSCodium was installed by other
--- scripts/install_macos.sh
+++ scripts/install_macos.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -exo pipefail
+
 # Install Homebrew
 set -e
 
