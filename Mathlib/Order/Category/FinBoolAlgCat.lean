19aacea4ddcd5f7ffc8aacc3ac9504b952f74510
Jon Eugster <eugster.jon@gmail.com>
2023-06-14 16:04:43 +0200

    partial fixes

--- Mathlib/Order/Category/FinBoolAlgCat.lean
+++ Mathlib/Order/Category/FinBoolAlgCat.lean
@@ -27,6 +27,7 @@ Birkhoff's representation for finite Boolean algebras.
 `FinBoolAlgCat` is essentially small.
 -/
 
+set_option linter.uppercaseLean3 false
 
 universe u
 
@@ -48,14 +49,16 @@ instance (X : FinBoolAlgCat) : BooleanAlgebra X :=
 
 attribute [instance] FinBoolAlgCat.isFintype
 
-@[simp]
-theorem coe_toBoolAlgCat (X : FinBoolAlgCat) : ↥X.toBoolAlgCat = ↥X :=
-  rfl
-#align FinBoolAlg.coe_to_BoolAlg FinBoolAlgCat.coe_toBoolAlgCat
+-- Porting note: linter says this is a syntactic tautology now
+-- @[simp]
+-- theorem coe_toBoolAlgCat (X : FinBoolAlgCat) : ↥X.toBoolAlgCat = ↥X :=
+--   rfl
+-- #align FinBoolAlg.coe_to_BoolAlg FinBoolAlgCat.coe_toBoolAlgCat
+#noalign FinBoolAlg.coe_to_BoolAlg
 
-/-- Construct a bundled `FinBoolAlgCat` from `boolean_algebra` + `fintype`. -/
+/-- Construct a bundled `FinBoolAlgCat` from `BooleanAlgebra` + `Fintype`. -/
 def of (α : Type _) [BooleanAlgebra α] [Fintype α] : FinBoolAlgCat :=
-  ⟨⟨α⟩⟩
+  ⟨{α := α}⟩
 #align FinBoolAlg.of FinBoolAlgCat.of
 
 @[simp]
@@ -78,10 +81,10 @@ instance hasForgetToBoolAlg : HasForget₂ FinBoolAlgCat BoolAlgCat :=
   InducedCategory.hasForget₂ FinBoolAlgCat.toBoolAlgCat
 #align FinBoolAlg.has_forget_to_BoolAlg FinBoolAlgCat.hasForgetToBoolAlg
 
-instance hasForgetToFinBddDistLat : HasForget₂ FinBoolAlgCat FinBddDistLat where
+instance hasForgetToFinBddDistLat : HasForget₂ FinBoolAlgCat FinBddDistLatCat where
   forget₂ :=
-    { obj := fun X => FinBddDistLat.of X
-      map := fun X Y f => f }
+    { obj := fun X => FinBddDistLatCat.of X
+      map := fun {X Y} f => f }
   forget_comp := rfl
 #align FinBoolAlg.has_forget_to_FinBddDistLat FinBoolAlgCat.hasForgetToFinBddDistLat
 
@@ -97,13 +100,14 @@ instance forget_to_boolAlg_faithful : Faithful (forget₂ FinBoolAlgCat BoolAlgC
 instance hasForgetToFinPartOrd : HasForget₂ FinBoolAlgCat FinPartOrd
     where forget₂ :=
     { obj := fun X => FinPartOrd.of X
-      map := fun X Y f => show OrderHom X Y from ↑(show BoundedLatticeHom X Y from f) }
+      map := fun {X Y} f => show OrderHom X Y from ↑(show BoundedLatticeHom X Y from f) }
 #align FinBoolAlg.has_forget_to_FinPartOrd FinBoolAlgCat.hasForgetToFinPartOrd
 
 instance forget_to_finPartOrd_faithful : Faithful (forget₂ FinBoolAlgCat FinPartOrd) :=
-  ⟨fun X Y f g h =>
-    haveI := congr_arg (coeFn : _ → X → Y) h
-    FunLike.coe_injective this⟩
+  ⟨sorry⟩
+  -- ⟨fun {X Y} f g h =>
+  --   haveI := congr_arg (coeFn : _ → X → Y) h
+  --   FunLike.coe_injective this⟩
 #align FinBoolAlg.forget_to_FinPartOrd_faithful FinBoolAlgCat.forget_to_finPartOrd_faithful
 
 /-- Constructs an equivalence between finite Boolean algebras from an order isomorphism between
@@ -112,26 +116,27 @@ them. -/
 def Iso.mk {α β : FinBoolAlgCat.{u}} (e : α ≃o β) : α ≅ β where
   hom := (e : BoundedLatticeHom α β)
   inv := (e.symm : BoundedLatticeHom β α)
-  hom_inv_id' := by ext; exact e.symm_apply_apply _
-  inv_hom_id' := by ext; exact e.apply_symm_apply _
+  hom_inv_id := by ext; exact e.symm_apply_apply _
+  inv_hom_id := by ext; exact e.apply_symm_apply _
 #align FinBoolAlg.iso.mk FinBoolAlgCat.Iso.mk
 
 /-- `order_dual` as a functor. -/
 @[simps]
 def dual : FinBoolAlgCat ⥤ FinBoolAlgCat where
   obj X := of Xᵒᵈ
-  map X Y := BoundedLatticeHom.dual
+  map {X Y} := BoundedLatticeHom.dual
 #align FinBoolAlg.dual FinBoolAlgCat.dual
 
 /-- The equivalence between `FinBoolAlgCat` and itself induced by `order_dual` both ways. -/
-@[simps Functor inverse]
-def dualEquiv : FinBoolAlgCat ≌ FinBoolAlgCat :=
-  Equivalence.mk dual dual
-    (NatIso.ofComponents (fun X => Iso.mk <| OrderIso.dualDual X) fun X Y f => rfl)
-    (NatIso.ofComponents (fun X => Iso.mk <| OrderIso.dualDual X) fun X Y f => rfl)
+@[simps functor inverse]
+def dualEquiv : FinBoolAlgCat ≌ FinBoolAlgCat where
+  functor := dual
+  inverse := dual
+  unitIso := NatIso.ofComponents fun X => Iso.mk <| OrderIso.dualDual X
+  counitIso := NatIso.ofComponents fun X => Iso.mk <| OrderIso.dualDual X
 #align FinBoolAlg.dual_equiv FinBoolAlgCat.dualEquiv
 
-end FinBoolAlg
+end FinBoolAlgCat
 
 theorem finBoolAlgCat_dual_comp_forget_to_finBddDistLatCat :
     FinBoolAlgCat.dual ⋙ forget₂ FinBoolAlgCat FinBddDistLatCat =
@@ -143,6 +148,6 @@ theorem finBoolAlgCat_dual_comp_forget_to_finBddDistLatCat :
 @[simps]
 def fintypeToFinBoolAlgCatOp : FintypeCat ⥤ FinBoolAlgCatᵒᵖ where
   obj X := op <| FinBoolAlgCat.of (Set X)
-  map X Y f :=
+  map {X Y} f :=
     Quiver.Hom.op <| (CompleteLatticeHom.setPreimage f : BoundedLatticeHom (Set Y) (Set X))
 #align Fintype_to_FinBoolAlg_op fintypeToFinBoolAlgCatOp
