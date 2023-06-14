db3485e99ba1567860ab0f865ef1b80fecdd5c3d
Jon Eugster <eugster.jon@gmail.com>
2023-06-14 14:37:08 +0200

    add Cat

--- Mathlib/Order/Category/FinBddDistLatCat.lean
+++ Mathlib/Order/Category/FinBddDistLatCat.lean
@@ -15,7 +15,7 @@ import Mathlib.Order.Category.FinPartOrd
 /-!
 # The category of finite bounded distributive lattices
 
-This file defines `FinBddDistLat`, the category of finite distributive lattices with
+This file defines `FinBddDistLatCat`, the category of finite distributive lattices with
 bounded lattice homomorphisms.
 -/
 
@@ -25,88 +25,88 @@ universe u
 open CategoryTheory
 
 /-- The category of finite distributive lattices with bounded lattice morphisms. -/
-structure FinBddDistLat where
-  toBddDistLat : BddDistLat
+structure FinBddDistLatCat where
+  toBddDistLatCat : BddDistLatCat
   [isFintype : Fintype to_BddDistLat]
-#align FinBddDistLat FinBddDistLat
+#align FinBddDistLat FinBddDistLatCat
 
-namespace FinBddDistLat
+namespace FinBddDistLatCat
 
-instance : CoeSort FinBddDistLat (Type _) :=
-  ⟨fun X => X.toBddDistLat⟩
+instance : CoeSort FinBddDistLatCat (Type _) :=
+  ⟨fun X => X.toBddDistLatCat⟩
 
-instance (X : FinBddDistLat) : DistribLattice X :=
-  X.toBddDistLat.toDistLat.str
+instance (X : FinBddDistLatCat) : DistribLattice X :=
+  X.toBddDistLatCat.toDistLatCat.str
 
-instance (X : FinBddDistLat) : BoundedOrder X :=
-  X.toBddDistLat.isBoundedOrder
+instance (X : FinBddDistLatCat) : BoundedOrder X :=
+  X.toBddDistLatCat.isBoundedOrder
 
-attribute [instance] FinBddDistLat.isFintype
+attribute [instance] FinBddDistLatCat.isFintype
 
-/-- Construct a bundled `FinBddDistLat` from a `nonempty` `bounded_order` `distrib_lattice`. -/
-def of (α : Type _) [DistribLattice α] [BoundedOrder α] [Fintype α] : FinBddDistLat :=
+/-- Construct a bundled `FinBddDistLatCat` from a `nonempty` `bounded_order` `distrib_lattice`. -/
+def of (α : Type _) [DistribLattice α] [BoundedOrder α] [Fintype α] : FinBddDistLatCat :=
   ⟨⟨⟨α⟩⟩⟩
-#align FinBddDistLat.of FinBddDistLat.of
+#align FinBddDistLat.of FinBddDistLatCat.of
 
-/-- Construct a bundled `FinBddDistLat` from a `nonempty` `bounded_order` `distrib_lattice`. -/
-def of' (α : Type _) [DistribLattice α] [Fintype α] [Nonempty α] : FinBddDistLat :=
+/-- Construct a bundled `FinBddDistLatCat` from a `nonempty` `bounded_order` `distrib_lattice`. -/
+def of' (α : Type _) [DistribLattice α] [Fintype α] [Nonempty α] : FinBddDistLatCat :=
   haveI := Fintype.toBoundedOrder α
   ⟨⟨⟨α⟩⟩⟩
-#align FinBddDistLat.of' FinBddDistLat.of'
+#align FinBddDistLat.of' FinBddDistLatCat.of'
 
-instance : Inhabited FinBddDistLat :=
+instance : Inhabited FinBddDistLatCat :=
   ⟨of PUnit⟩
 
-instance largeCategory : LargeCategory FinBddDistLat :=
-  InducedCategory.category toBddDistLat
-#align FinBddDistLat.large_category FinBddDistLat.largeCategory
+instance largeCategory : LargeCategory FinBddDistLatCat :=
+  InducedCategory.category toBddDistLatCat
+#align FinBddDistLat.large_category FinBddDistLatCat.largeCategory
 
-instance concreteCategory : ConcreteCategory FinBddDistLat :=
-  InducedCategory.concreteCategory toBddDistLat
-#align FinBddDistLat.concrete_category FinBddDistLat.concreteCategory
+instance concreteCategory : ConcreteCategory FinBddDistLatCat :=
+  InducedCategory.concreteCategory toBddDistLatCat
+#align FinBddDistLat.concrete_category FinBddDistLatCat.concreteCategory
 
-instance hasForgetToBddDistLat : HasForget₂ FinBddDistLat BddDistLat :=
-  InducedCategory.hasForget₂ FinBddDistLat.toBddDistLat
-#align FinBddDistLat.has_forget_to_BddDistLat FinBddDistLat.hasForgetToBddDistLat
+instance hasForgetToBddDistLatCat : HasForget₂ FinBddDistLatCat BddDistLatCat :=
+  InducedCategory.hasForget₂ FinBddDistLatCat.toBddDistLatCat
+#align FinBddDistLat.has_forget_to_BddDistLat FinBddDistLatCat.hasForgetToBddDistLatCat
 
-instance hasForgetToFinPartOrd : HasForget₂ FinBddDistLat FinPartOrd
+instance hasForgetToFinPartOrd : HasForget₂ FinBddDistLatCat FinPartOrd
     where forget₂ :=
     { obj := fun X => FinPartOrd.of X
       map := fun X Y f => (show BoundedLatticeHom X Y from f : X →o Y) }
-#align FinBddDistLat.has_forget_to_FinPartOrd FinBddDistLat.hasForgetToFinPartOrd
+#align FinBddDistLat.has_forget_to_FinPartOrd FinBddDistLatCat.hasForgetToFinPartOrd
 
 /-- Constructs an equivalence between finite distributive lattices from an order isomorphism
 between them. -/
 @[simps]
-def Iso.mk {α β : FinBddDistLat.{u}} (e : α ≃o β) : α ≅ β where
+def Iso.mk {α β : FinBddDistLatCat.{u}} (e : α ≃o β) : α ≅ β where
   hom := (e : BoundedLatticeHom α β)
   inv := (e.symm : BoundedLatticeHom β α)
   hom_inv_id' := by ext; exact e.symm_apply_apply _
   inv_hom_id' := by ext; exact e.apply_symm_apply _
-#align FinBddDistLat.iso.mk FinBddDistLat.Iso.mk
+#align FinBddDistLat.iso.mk FinBddDistLatCat.Iso.mk
 
-example {X Y : FinBddDistLat} : (X ⟶ Y) = BoundedLatticeHom X Y :=
+example {X Y : FinBddDistLatCat} : (X ⟶ Y) = BoundedLatticeHom X Y :=
   rfl
 
 /-- `order_dual` as a functor. -/
 @[simps]
-def dual : FinBddDistLat ⥤ FinBddDistLat where
+def dual : FinBddDistLatCat ⥤ FinBddDistLatCat where
   obj X := of Xᵒᵈ
   map X Y := BoundedLatticeHom.dual
-#align FinBddDistLat.dual FinBddDistLat.dual
+#align FinBddDistLat.dual FinBddDistLatCat.dual
 
-/-- The equivalence between `FinBddDistLat` and itself induced by `order_dual` both ways. -/
+/-- The equivalence between `FinBddDistLatCat` and itself induced by `order_dual` both ways. -/
 @[simps Functor inverse]
-def dualEquiv : FinBddDistLat ≌ FinBddDistLat :=
+def dualEquiv : FinBddDistLatCat ≌ FinBddDistLatCat :=
   Equivalence.mk dual dual
     (NatIso.ofComponents (fun X => Iso.mk <| OrderIso.dualDual X) fun X Y f => rfl)
     (NatIso.ofComponents (fun X => Iso.mk <| OrderIso.dualDual X) fun X Y f => rfl)
-#align FinBddDistLat.dual_equiv FinBddDistLat.dualEquiv
+#align FinBddDistLat.dual_equiv FinBddDistLatCat.dualEquiv
 
 end FinBddDistLat
 
-theorem finBddDistLat_dual_comp_forget_to_bddDistLat :
-    FinBddDistLat.dual ⋙ forget₂ FinBddDistLat BddDistLat =
-      forget₂ FinBddDistLat BddDistLat ⋙ BddDistLat.dual :=
+theorem finBddDistLatCat_dual_comp_forget_to_bddDistLatCat :
+    FinBddDistLatCat.dual ⋙ forget₂ FinBddDistLatCat BddDistLatCat =
+      forget₂ FinBddDistLatCat BddDistLatCat ⋙ BddDistLatCat.dual :=
   rfl
-#align FinBddDistLat_dual_comp_forget_to_BddDistLat finBddDistLat_dual_comp_forget_to_bddDistLat
+#align FinBddDistLat_dual_comp_forget_to_BddDistLat finBddDistLatCat_dual_comp_forget_to_bddDistLatCat
