9b9eb6ac745ef1a43740a6b78993c896ab173ba7
Jon Eugster <eugster.jon@gmail.com>
2023-06-14 16:16:33 +0200

    add Cat

--- Mathlib/Algebra/Category/BoolRingCat.lean
+++ Mathlib/Algebra/Category/BoolRingCat.lean
@@ -15,11 +15,11 @@ import Mathlib.Order.Category.BoolAlgCat
 /-!
 # The category of Boolean rings
 
-This file defines `BoolRing`, the category of Boolean rings.
+This file defines `BoolRingCat`, the category of Boolean rings.
 
 ## TODO
 
-Finish the equivalence with `BoolAlg`.
+Finish the equivalence with `BoolAlgCat`.
 -/
 
 
@@ -28,75 +28,75 @@ universe u
 open CategoryTheory Order
 
 /-- The category of Boolean rings. -/
-def BoolRing :=
+def BoolRingCat :=
   Bundled BooleanRing
-#align BoolRing BoolRing
+#align BoolRing BoolRingCat
 
 namespace BoolRing
 
-instance : CoeSort BoolRing (Type _) :=
+instance : CoeSort BoolRingCat (Type _) :=
   Bundled.hasCoeToSort
 
-instance (X : BoolRing) : BooleanRing X :=
+instance (X : BoolRingCat) : BooleanRing X :=
   X.str
 
-/-- Construct a bundled `BoolRing` from a `boolean_ring`. -/
-def of (α : Type _) [BooleanRing α] : BoolRing :=
+/-- Construct a bundled `BoolRingCat` from a `boolean_ring`. -/
+def of (α : Type _) [BooleanRing α] : BoolRingCat :=
   Bundled.of α
-#align BoolRing.of BoolRing.of
+#align BoolRing.of BoolRingCat.of
 
 @[simp]
 theorem coe_of (α : Type _) [BooleanRing α] : ↥(of α) = α :=
   rfl
-#align BoolRing.coe_of BoolRing.coe_of
+#align BoolRing.coe_of BoolRingCat.coe_of
 
-instance : Inhabited BoolRing :=
+instance : Inhabited BoolRingCat :=
   ⟨of PUnit⟩
 
 instance : BundledHom.ParentProjection @BooleanRing.toCommRing :=
   ⟨⟩
 
-deriving instance LargeCategory, ConcreteCategory for BoolRing
+deriving instance LargeCategory, ConcreteCategory for BoolRingCat
 
 @[simps]
-instance hasForgetToCommRing : HasForget₂ BoolRing CommRingCat :=
+instance hasForgetToCommRing : HasForget₂ BoolRingCat CommRingCat :=
   BundledHom.forget₂ _ _
-#align BoolRing.has_forget_to_CommRing BoolRing.hasForgetToCommRing
+#align BoolRing.has_forget_to_CommRing BoolRingCat.hasForgetToCommRing
 
 /-- Constructs an isomorphism of Boolean rings from a ring isomorphism between them. -/
 @[simps]
-def Iso.mk {α β : BoolRing.{u}} (e : α ≃+* β) : α ≅ β where
+def Iso.mk {α β : BoolRingCat.{u}} (e : α ≃+* β) : α ≅ β where
   Hom := e
   inv := e.symm
   hom_inv_id' := by ext; exact e.symm_apply_apply _
   inv_hom_id' := by ext; exact e.apply_symm_apply _
-#align BoolRing.iso.mk BoolRing.Iso.mk
+#align BoolRing.iso.mk BoolRingCat.Iso.mk
 
 end BoolRing
 
-/-! ### Equivalence between `BoolAlg` and `BoolRing` -/
+/-! ### Equivalence between `BoolAlgCat` and `BoolRingCat` -/
 
 
 @[simps]
-instance BoolRing.hasForgetToBoolAlg : HasForget₂ BoolRing BoolAlg
+instance BoolRingCat.hasForgetToBoolAlgCat : HasForget₂ BoolRingCat BoolAlgCat
     where forget₂ :=
-    { obj := fun X => BoolAlg.of (AsBoolAlg X)
-      map := fun X Y => RingHom.asBoolAlg }
-#align BoolRing.has_forget_to_BoolAlg BoolRing.hasForgetToBoolAlg
+    { obj := fun X => BoolAlgCat.of (AsBoolAlgCat X)
+      map := fun X Y => RingHom.asBoolAlgCat }
+#align BoolRing.has_forget_to_BoolAlg BoolRingCat.hasForgetToBoolAlgCat
 
 @[simps]
-instance BoolAlg.hasForgetToBoolRing : HasForget₂ BoolAlg BoolRing
+instance BoolAlgCat.hasForgetToBoolRingCat : HasForget₂ BoolAlgCat BoolRingCat
     where forget₂ :=
-    { obj := fun X => BoolRing.of (AsBoolRing X)
-      map := fun X Y => BoundedLatticeHom.asBoolRing }
-#align BoolAlg.has_forget_to_BoolRing BoolAlg.hasForgetToBoolRing
+    { obj := fun X => BoolRingCat.of (AsBoolRingCat X)
+      map := fun X Y => BoundedLatticeHom.asBoolRingCat }
+#align BoolAlg.has_forget_to_BoolRingCat BoolAlgCat.hasForgetToBoolRingCat
 
 /-- The equivalence between Boolean rings and Boolean algebras. This is actually an isomorphism. -/
 @[simps Functor inverse]
-def boolRingEquivBoolAlg : BoolRing ≌ BoolAlg :=
-  Equivalence.mk (forget₂ BoolRing BoolAlg) (forget₂ BoolAlg BoolRing)
-    (NatIso.ofComponents (fun X => BoolRing.Iso.mk <| (RingEquiv.asBoolRingAsBoolAlg X).symm)
+def boolRingCatEquivBoolAlgCat : BoolRingCat ≌ BoolAlgCat :=
+  Equivalence.mk (forget₂ BoolRingCat BoolAlgCat) (forget₂ BoolAlgCat BoolRingCat)
+    (NatIso.ofComponents (fun X => BoolRingCat.Iso.mk <| (RingEquiv.asBoolRingCatAsBoolAlCatg X).symm)
       fun X Y f => rfl)
-    (NatIso.ofComponents (fun X => BoolAlg.Iso.mk <| OrderIso.asBoolAlgAsBoolRing X) fun X Y f =>
+    (NatIso.ofComponents (fun X => BoolAlgCat.Iso.mk <| OrderIso.asBoolAlgCatAsBoolRingCat X) fun X Y f =>
       rfl)
-#align BoolRing_equiv_BoolAlg boolRingEquivBoolAlg
+#align BoolRing_equiv_BoolAlg boolRingCatEquivBoolAlgCat
