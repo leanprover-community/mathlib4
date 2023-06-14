159726b679aca84a593636dc681526d787dcc355
Komyyy <pol_tta@outlook.jp>
2023-06-14 11:51:27 +0900

    authors

--- Mathlib/Data/DList/Defs.lean
+++ Mathlib/Data/DList/Defs.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2017 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Author: Leonardo de Moura
+Authors: Leonardo de Moura
 
 ! This file was ported from Lean 3 source module lean_core.data.dlist
 ! leanprover-community/lean commit 855e5b74e3a52a40552e8f067169d747d48743fd
