b75e00924a485cb1003a0f1b62143eca7a22e727
Jon Eugster <eugster.jon@gmail.com>
2023-06-14 20:11:50 +0000

    feat: port Order.Category.BddDistLatCat (#5015)
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -2270,6 +2270,7 @@ import Mathlib.Order.Bounded
 import Mathlib.Order.BoundedOrder
 import Mathlib.Order.Bounds.Basic
 import Mathlib.Order.Bounds.OrderIso
+import Mathlib.Order.Category.BddDistLatCat
 import Mathlib.Order.Category.BddLatCat
 import Mathlib.Order.Category.BddOrdCat
 import Mathlib.Order.Category.DistLatCat
