9c20de924c767d44dcd380dcac454f7c2eaf09ff
Moritz Firsching <moritz.firsching@gmail.com>
2023-06-14 00:07:33 +0000

    feat: port Topology.ContinuousFunction.Ideals (#4852)
    Co-authored-by: Moritz Firsching <firsching@google.com>
    Co-authored-by: Jireh Loreaux <loreaujy@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -2800,6 +2800,7 @@ import Mathlib.Topology.ContinuousFunction.Basic
 import Mathlib.Topology.ContinuousFunction.Bounded
 import Mathlib.Topology.ContinuousFunction.CocompactMap
 import Mathlib.Topology.ContinuousFunction.Compact
+import Mathlib.Topology.ContinuousFunction.Ideals
 import Mathlib.Topology.ContinuousFunction.LocallyConstant
 import Mathlib.Topology.ContinuousFunction.Ordered
 import Mathlib.Topology.ContinuousFunction.Polynomial
