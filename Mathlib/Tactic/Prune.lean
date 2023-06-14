35c9b1d2467a2645d24eabc1e4dfa91c3f3acfa9
adomani <adomani@gmail.com>
2023-06-14 22:07:27 +0100

    With several iterations

--- Mathlib/Tactic/Prune.lean
+++ Mathlib/Tactic/Prune.lean
@@ -20,9 +20,49 @@ def revertLoop : TacticM (List LocalDecl × MVarId) := focus do
   let (fvars, ctx, newGoal) := ← revertVarsOnce
   if fvars.size == 0 then pure (ctx, newGoal) else revertLoop
 
+def revertNMLoop (n m : Nat) : TacticM (List LocalDecl × MVarId) := do
+  match n with
+  | 0     => revertLoop
+  | 1     => do let goal := ← getMainGoal; pure ([], goal)
+  | n + 1 => focus do
+    let (fvars, ctx, newGoal) := ← revertVarsOnce
+--    logInfo m!"fvs: {fvars.size}"
+--    let decl := ← fvars.mapM (Lean.FVarId.getDecl ·)
+--    dbg_trace Format.pretty f!"{decl.map LocalDecl.userName}"
+    if fvars.size == 0 then
+      logInfo m!"{m} iterations would have sufficed.\nTry this: prunne {m}"
+      pure (ctx, newGoal) else revertNMLoop n (m + 1)
+
+/-
+def revertNLoop (n : Nat) : TacticM (List LocalDecl × MVarId) := do
+  let mut m := 0; match n with
+  | 0     => revertLoop
+  | 1     => do let goal := ← getMainGoal; pure ([], goal)
+  | n + 1 => dbg_trace m; focus do
+    let m := n;
+    let (fvars, ctx, newGoal) := ← revertVarsOnce
+    if fvars.size == 0 then
+      logInfo m!"{m} iterations would have sufficed -- "
+      pure (ctx, newGoal) else revertNLoop n
+-/
+/-
+  if n == 0 then revertLoop else
+  if n == 1 then do let goal := ← getMainGoal; pure ([], goal) else
+
+  focus do
+    let m := n;
+    let (fvars, ctx, newGoal) := ← revertVarsOnce
+    if fvars.size == 0 then
+      logInfo m!"{m} iterations would have sufficed -- "
+      pure (ctx, newGoal) else revertNLoop n
+--  for i in [:n] do
+
+#exit
+-/
+
+
 def pruneTac : TacticM Unit := focus do
   let dcls := (← getLCtx).decls.toList.reduceOption
-  dbg_trace dcls.length
   let goal := ← getMainGoal
   let newGoal ← goal.tryClearMany (dcls.map LocalDecl.fvarId).toArray
   setGoals [newGoal]
@@ -34,13 +74,18 @@ elab "prune" : tactic => do
   let _ := ← revertLoop
   pruneTac
 
+elab "prunne" n:num : tactic => do
+  let _ := ← revertNMLoop n.getNat 0
+  pruneTac
+
 end Lean.Elab.Tactic
 
 universe u
-variable {α : Type u} [Add α] [Add α] {e f : α} {a b _d : Nat} {_h : e ≠ f} (h₁ : a = b) {c : Int}
+variable {α : Type u} [Add α] [Add α] {e f : α} {a b _d : Nat} {_h : e ≠ f} (h₁ : a = b)
+  (h₂ : ff = b) {c : Int}
 
 example : a + 5 = c ∨ True := by
-  prune
+  prunne 5
   /- goal state:
   b a: Nat
   h₁: a = b
