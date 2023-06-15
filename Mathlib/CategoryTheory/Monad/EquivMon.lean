1f9b61df07fa358e470df94cffabeadc764c7ad5
Jon Eugster <eugster.jon@gmail.com>
2023-06-15 19:20:45 +0200

    partial fixes

--- Mathlib/CategoryTheory/Monad/EquivMon.lean
+++ Mathlib/CategoryTheory/Monad/EquivMon.lean
@@ -20,13 +20,12 @@ A monad "is just" a monoid in the category of endofunctors.
 
 # Definitions/Theorems
 
-1. `to_Mon` associates a monoid object in `C ⥤ C` to any monad on `C`.
-2. `Monad_to_Mon` is the functorial version of `to_Mon`.
-3. `of_Mon` associates a monad on `C` to any monoid object in `C ⥤ C`.
-4. `Monad_Mon_equiv` is the equivalence between `Monad C` and `Mon_ (C ⥤ C)`.
+1. `toMon` associates a monoid object in `C ⥤ C` to any monad on `C`.
+2. `monadToMon` is the functorial version of `to_Mon`.
+3. `ofMon` associates a monad on `C` to any monoid object in `C ⥤ C`.
+4. `monadMonEquiv` is the equivalence between `Monad C` and `Mon_ (C ⥤ C)`.
 
 -/
-
 
 namespace CategoryTheory
 
@@ -39,19 +38,19 @@ variable {C : Type u} [Category.{v} C]
 
 namespace Monad
 
-attribute [local instance, local reducible] endofunctor_monoidal_category
+-- porting note: was `local reducible` but "reducible must be global"
+attribute [local instance] endofunctorMonoidalCategory
 
 /-- To every `Monad C` we associated a monoid object in `C ⥤ C`.-/
 @[simps]
 def toMon : Monad C → Mon_ (C ⥤ C) := fun M =>
-  { pt := (M : C ⥤ C)
+  { X := (M : C ⥤ C)
     one := M.η
     mul := M.μ
-    one_mul' := by ext; simp
-    -- `obviously` provides this, but slowly
-    mul_one' := by ext; simp
-    -- `obviously` provides this, but slowly
-    mul_assoc' := by ext; dsimp; simp [M.assoc] }
+    one_mul := by ext; simp [endofunctorMonoidalCategory]
+    mul_one := by ext; simp [endofunctorMonoidalCategory]
+    mul_assoc := by ext; simp [M.assoc, endofunctorMonoidalCategory]
+    }
 #align category_theory.Monad.to_Mon CategoryTheory.Monad.toMon
 
 variable (C)
@@ -60,10 +59,17 @@ variable (C)
 @[simps]
 def monadToMon : Monad C ⥤ Mon_ (C ⥤ C) where
   obj := toMon
-  map _ _ f := { Hom := f.toNatTrans }
-  map_id' := by intro X; rfl
-  -- `obviously` provides this, but slowly
-  map_comp' := by intro X Y Z f g; rfl
+  map f := {
+    hom := f.toNatTrans
+    -- Porting note: added `mul_hom :=` since `local reducible` above does not exist
+    mul_hom := by
+      dsimp [endofunctorMonoidalCategory]
+      aesop_cat
+  }
+  -- Porting note: seems that `aesop_cat` is faster than `obviously` was!
+  -- map_id := by intro X; rfl
+  -- -- `obviously` provides this, but slowly
+  -- map_comp := by intro X Y Z f g; rfl
 #align category_theory.Monad.Monad_to_Mon CategoryTheory.Monad.monadToMon
 
 variable {C}
@@ -71,12 +77,19 @@ variable {C}
 /-- To every monoid object in `C ⥤ C` we associate a `Monad C`. -/
 @[simps]
 def ofMon : Mon_ (C ⥤ C) → Monad C := fun M =>
-  { toFunctor := M.pt
+  { toFunctor := M.X
     η' := M.one
     μ' := M.mul
-    left_unit' := fun X => by rw [← M.one.id_hcomp_app, ← nat_trans.comp_app, M.mul_one]; rfl
-    right_unit' := fun X => by rw [← M.one.hcomp_id_app, ← nat_trans.comp_app, M.one_mul]; rfl
-    assoc' := fun X => by rw [← nat_trans.hcomp_id_app, ← nat_trans.comp_app]; simp }
+    left_unit' := fun X => by
+      rw [← NatTrans.id_hcomp_app M.one, ← NatTrans.comp_app, M.mul_one]
+      rfl
+    right_unit' := fun X => by
+      rw [← NatTrans.hcomp_id_app M.one, ← NatTrans.comp_app, M.one_mul]
+      rfl
+    assoc' := fun X => by
+      rw [← NatTrans.hcomp_id_app, ← NatTrans.comp_app]
+      simp
+      }
 #align category_theory.Monad.of_Mon CategoryTheory.Monad.ofMon
 
 variable (C)
@@ -85,18 +98,18 @@ variable (C)
 @[simps]
 def monToMonad : Mon_ (C ⥤ C) ⥤ Monad C where
   obj := ofMon
-  map _ _ f :=
+  map f :=
     {-- `finish` closes this goal
-        f.Hom with
-      app_η' := by
+        f.hom with
+      app_η := by
         intro X
-        erw [← nat_trans.comp_app, f.one_hom]
+        erw [← NatTrans.comp_app, f.one_hom]
         rfl
-      app_μ' := by
+      app_μ := by
         intro X
-        erw [← nat_trans.comp_app, f.mul_hom]
-        simpa only [nat_trans.naturality, nat_trans.hcomp_app, assoc, nat_trans.comp_app,
-          of_Mon_μ] }
+        erw [← NatTrans.comp_app, f.mul_hom]
+        simpa only [NatTrans.naturality, NatTrans.hcomp_app, assoc, NatTrans.comp_app,
+          ofMon_μ] }
 #align category_theory.Monad.Mon_to_Monad CategoryTheory.Monad.monToMonad
 
 namespace MonadMonEquiv
@@ -106,11 +119,11 @@ variable {C}
 /-- Isomorphism of functors used in `Monad_Mon_equiv` -/
 @[simps (config := { rhsMd := semireducible })]
 def counitIso : monToMonad C ⋙ monadToMon C ≅ 𝟭 _ where
-  Hom := { app := fun _ => { Hom := 𝟙 _ } }
-  inv := { app := fun _ => { Hom := 𝟙 _ } }
-  hom_inv_id' := by ext; simp
+  hom := { app := fun _ => { hom := 𝟙 _ } }
+  inv := { app := fun _ => { hom := 𝟙 _ } }
+  hom_inv_id := by ext; simp
   -- `obviously` provides these, but slowly
-  inv_hom_id' := by ext; simp
+  inv_hom_id := by ext; simp
 #align category_theory.Monad.Monad_Mon_equiv.counit_iso CategoryTheory.Monad.MonadMonEquiv.counitIso
 
 /-- Auxiliary definition for `Monad_Mon_equiv` -/
@@ -126,11 +139,11 @@ def unitIsoInv : monadToMon C ⋙ monToMonad C ⟶ 𝟭 _ where app _ := { app :
 /-- Isomorphism of functors used in `Monad_Mon_equiv` -/
 @[simps]
 def unitIso : 𝟭 _ ≅ monadToMon C ⋙ monToMonad C where
-  Hom := unitIsoHom
+  hom := unitIsoHom
   inv := unitIsoInv
-  hom_inv_id' := by ext; simp
+  hom_inv_id := by ext; simp
   -- `obviously` provides these, but slowly
-  inv_hom_id' := by ext; simp
+  inv_hom_id := by ext; simp
 #align category_theory.Monad.Monad_Mon_equiv.unit_iso CategoryTheory.Monad.MonadMonEquiv.unitIso
 
 end MonadMonEquiv
@@ -140,19 +153,22 @@ open MonadMonEquiv
 /-- Oh, monads are just monoids in the category of endofunctors (equivalence of categories). -/
 @[simps]
 def monadMonEquiv : Monad C ≌ Mon_ (C ⥤ C) where
-  Functor := monadToMon _
+  functor := monadToMon _
   inverse := monToMonad _
   unitIso := unitIso
   counitIso := counitIso
-  functor_unitIso_comp' := by intro X; ext; dsimp; simp
+  functor_unitIso_comp := by
+    intro X
+    ext
+    dsimp
+    simp
 #align category_theory.Monad.Monad_Mon_equiv CategoryTheory.Monad.monadMonEquiv
 
 -- `obviously`, slowly
 -- Sanity check
-example (A : Monad C) {X : C} : ((monadMonEquiv C).unitIso.app A).Hom.app X = 𝟙 _ :=
+example (A : Monad C) {X : C} : ((monadMonEquiv C).unitIso.app A).hom.app X = 𝟙 _ :=
   rfl
 
 end Monad
 
 end CategoryTheory
-
