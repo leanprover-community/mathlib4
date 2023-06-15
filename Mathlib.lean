0fc56964ed178eff6eecaaa3565b5b8ec347b2c7
nick-kuhn <65514131+Ruben-VandeVelde@users.noreply.github.com>
2023-06-15 13:18:13 +0000

    feat: port Analysis.Normed.Group.SemiNormedGroupCat (#4047)
    Co-authored-by: Scott Morrison <scott.morrison@anu.edu.au>
    Co-authored-by: Jeremy Tan Jie Rui <reddeloostw@gmail.com>
    Co-authored-by: nick-kuhn <nikolaskuhn@gmx.de>
    Co-authored-by: Jujian Zhang <jujian.zhang1998@outlook.com>
    Co-authored-by: nick-kuhn <46423253+nick-kuhn@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -625,6 +625,7 @@ import Mathlib.Analysis.Normed.Group.HomCompletion
 import Mathlib.Analysis.Normed.Group.InfiniteSum
 import Mathlib.Analysis.Normed.Group.Pointwise
 import Mathlib.Analysis.Normed.Group.Quotient
+import Mathlib.Analysis.Normed.Group.SemiNormedGroupCat
 import Mathlib.Analysis.Normed.Group.Seminorm
 import Mathlib.Analysis.Normed.MulAction
 import Mathlib.Analysis.Normed.Order.Basic
