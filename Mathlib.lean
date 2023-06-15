bb23b07e69611d9b90864ab47a9211f588dc4a2b
Scott Morrison <scott@tqft.net>
2023-06-15 11:52:41 +0000

    feat: further functions on ListM (#3618)
    Co-authored-by: Scott Morrison <scott.morrison@gmail.com>
    Co-authored-by: Scott Morrison <scott.morrison@anu.edu.au>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1398,6 +1398,7 @@ import Mathlib.Data.ListM.BestFirst
 import Mathlib.Data.ListM.DepthFirst
 import Mathlib.Data.ListM.Heartbeats
 import Mathlib.Data.ListM.IO
+import Mathlib.Data.ListM.Split
 import Mathlib.Data.Matrix.Basic
 import Mathlib.Data.Matrix.Basis
 import Mathlib.Data.Matrix.Block
