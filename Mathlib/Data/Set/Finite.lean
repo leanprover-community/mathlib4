3c968ab22bb01474b04d4e380a8c890fc08f9d18
Eric Wieser <wieser.eric@gmail.com>
2023-06-15 09:22:10 +0000

    minimize diff

--- Mathlib/Data/Set/Finite.lean
+++ Mathlib/Data/Set/Finite.lean
@@ -242,7 +242,8 @@ alias Finite.toFinset_ssubset_toFinset ↔ _ toFinset_strictMono
 protected theorem toFinset_setOf [Fintype α] (p : α → Prop) [DecidablePred p]
     (h : { x | p x }.Finite) : h.toFinset = Finset.univ.filter p := by
   -- porting note: `simp` doesn't use the `simp` lemma `Set.toFinset_setOf` without the `_`
-  ext; simp [Set.toFinset_setOf _]
+  ext
+  simp [Set.toFinset_setOf _]
 #align set.finite.to_finset_set_of Set.Finite.toFinset_setOf
 
 @[simp]
