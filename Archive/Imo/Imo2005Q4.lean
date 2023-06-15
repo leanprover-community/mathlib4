769175a7e21ddb669e03f4810fbc9226fbb525db
Yury G. Kudryashov <urkud@urkud.name>
2023-06-15 13:30:13 -0500

    automated fixes
    Mathbin -> Mathlib
    fix certain import statements
    move "by" to end of line
    add import to Mathlib.lean

--- Archive/Imo/Imo2005Q4.lean
+++ Archive/Imo/Imo2005Q4.lean
@@ -8,7 +8,7 @@ Authors: Heather Macbeth
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
-import Mathbin.FieldTheory.Finite.Basic
+import Mathlib.FieldTheory.Finite.Basic
 
 /-!
 # IMO 2005 Q4
@@ -34,16 +34,14 @@ theorem find_specified_factor {p : ℕ} (hp : Nat.Prime p) (hp' : IsCoprime (6 :
     ↑p ∣ a (p - 2) := by
   rw [← Int.modEq_zero_iff_dvd]
   -- Since `p` and `6` are coprime, `6` has an inverse mod `p`
-  obtain ⟨b, hb⟩ : ∃ b : ℤ, 6 * b ≡ 1 [ZMOD p] :=
-    by
+  obtain ⟨b, hb⟩ : ∃ b : ℤ, 6 * b ≡ 1 [ZMOD p] := by
     refine' Int.mod_coprime _
     exact nat.is_coprime_iff_coprime.mp hp'
   -- Also since `p` is coprime to `6`, it's coprime to `2` and `3`
   have hp₂ : IsCoprime (2 : ℤ) p := (id hp' : IsCoprime (3 * 2 : ℤ) p).of_mul_left_right
   have hp₃ : IsCoprime (3 : ℤ) p := (id hp' : IsCoprime (2 * 3 : ℤ) p).of_mul_left_right
   -- Slightly painful nat-subtraction calculation
-  have hp_sub_one : p - 1 = p - 2 + 1 :=
-    by
+  have hp_sub_one : p - 1 = p - 2 + 1 := by
     have : 1 ≤ p - 1 := le_tsub_of_add_le_right hp.two_le
     conv_lhs => rw [← Nat.sub_add_cancel this]
     rfl
@@ -52,8 +50,7 @@ theorem find_specified_factor {p : ℕ} (hp : Nat.Prime p) (hp' : IsCoprime (6 :
   calc
     (6 : ℤ) * a (p - 2) = 3 * 2 ^ (p - 1) + 2 * 3 ^ (p - 1) + 6 ^ (p - 1) - 6 := by
       simp only [a, mul_add, mul_sub, hp_sub_one, pow_succ]; ring
-    _ ≡ 3 * 1 + 2 * 1 + 1 - 6 [ZMOD p] :=-- At this step we use Fermat's little theorem
-    by
+    _ ≡ 3 * 1 + 2 * 1 + 1 - 6 [ZMOD p] :=-- At this step we use Fermat's little theorem by
       apply_rules [Int.ModEq.sub_right, Int.ModEq.add, Int.ModEq.mul_left,
         Int.ModEq.pow_card_sub_one_eq_one hp]
     _ = 0 := by norm_num
@@ -71,8 +68,7 @@ end imo2005_q4
 open imo2005_q4
 
 /-- Main statement:  The only positive integer coprime to all terms of the sequence `a` is `1`. -/
-theorem imo2005_q4 {k : ℕ} (hk : 0 < k) : (∀ n : ℕ, 1 ≤ n → IsCoprime (a n) k) ↔ k = 1 :=
-  by
+theorem imo2005_q4 {k : ℕ} (hk : 0 < k) : (∀ n : ℕ, 1 ≤ n → IsCoprime (a n) k) ↔ k = 1 := by
   constructor; rotate_left
   · -- The property is clearly true for `k = 1`
     rintro rfl n hn
@@ -83,8 +79,7 @@ theorem imo2005_q4 {k : ℕ} (hk : 0 < k) : (∀ n : ℕ, 1 ≤ n → IsCoprime 
   let p := k.min_fac
   -- Testing the special property of `k` for `48`, the second term of the sequence, we see that `p`
   -- is coprime to `6`.
-  have hp₆ : IsCoprime (6 : ℤ) p :=
-    by
+  have hp₆ : IsCoprime (6 : ℤ) p := by
     refine' IsCoprime.of_isCoprime_of_dvd_right _ (int.coe_nat_dvd.mpr k.min_fac_dvd)
     exact (id (h 2 one_le_two) : IsCoprime (8 * 6 : ℤ) k).of_mul_left_right
   -- In particular `p` is coprime to `2` (we record the `nat.coprime` version since that's what's
@@ -97,14 +92,12 @@ theorem imo2005_q4 {k : ℕ} (hk : 0 < k) : (∀ n : ℕ, 1 ≤ n → IsCoprime 
   by_contra hk'
   have hp : Nat.Prime p := Nat.minFac_prime hk'
   -- So `3 ≤ p`
-  have hp₃ : 3 ≤ p :=
-    by
+  have hp₃ : 3 ≤ p := by
     have : 2 ≠ p := by rwa [Nat.coprime_primes Nat.prime_two hp] at hp₂ 
     apply Nat.lt_of_le_and_ne hp.two_le this
   -- Testing the special property of `k` for the `p - 2`th term of the sequence, we see that `p` is
   -- coprime to `a (p - 2)`.
-  have : IsCoprime (↑p) (a (p - 2)) :=
-    by
+  have : IsCoprime (↑p) (a (p - 2)) := by
     refine' ((h (p - 2) _).of_isCoprime_of_dvd_right (int.coe_nat_dvd.mpr k.min_fac_dvd)).symm
     exact le_tsub_of_add_le_right hp₃
   rw [(nat.prime_iff_prime_int.mp hp).coprime_iff_not_dvd] at this 
