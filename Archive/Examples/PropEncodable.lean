cb149b99d09435ccd177cd1c3f6ec8fca8f963ed
Yury G. Kudryashov <urkud@urkud.name>
2023-06-15 14:20:13 -0500

    automated fixes
    Mathbin -> Mathlib
    fix certain import statements
    move "by" to end of line
    add import to Mathlib.lean

--- Archive/Examples/PropEncodable.lean
+++ Archive/Examples/PropEncodable.lean
@@ -8,7 +8,7 @@ Authors: Jeremy Avigad
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
-import Mathbin.Data.W.Basic
+import Mathlib.Data.W.Basic
 
 /-!
 # W types
