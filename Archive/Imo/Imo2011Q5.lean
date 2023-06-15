9fa361ad49fe745725aac89204e37008f4f1d89f
Yury G. Kudryashov <urkud@urkud.name>
2023-06-15 13:31:29 -0500

    automated fixes
    Mathbin -> Mathlib
    fix certain import statements
    move "by" to end of line
    add import to Mathlib.lean

--- Archive/Imo/Imo2011Q5.lean
+++ Archive/Imo/Imo2011Q5.lean
@@ -8,7 +8,7 @@ Authors: Alain Verberkmoes
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
-import Mathbin.Data.Int.Dvd.Basic
+import Mathlib.Data.Int.Dvd.Basic
 
 /-!
 # IMO 2011 Q5
@@ -24,8 +24,7 @@ of positive integers.  Suppose that, for any two integers
 open Int
 
 theorem imo2011_q5 (f : ℤ → ℤ) (hpos : ∀ n : ℤ, 0 < f n) (hdvd : ∀ m n : ℤ, f (m - n) ∣ f m - f n) :
-    ∀ m n : ℤ, f m ≤ f n → f m ∣ f n :=
-  by
+    ∀ m n : ℤ, f m ≤ f n → f m ∣ f n := by
   intro m n h_fm_le_fn
   cases' lt_or_eq_of_le h_fm_le_fn with h_fm_lt_fn h_fm_eq_fn
   · -- m < n
@@ -37,8 +36,7 @@ theorem imo2011_q5 (f : ℤ → ℤ) (hpos : ∀ n : ℤ, 0 < f n) (hdvd : ∀ m
       calc
         d < f m := sub_lt_self _ (hpos (m - n))
         _ < f n := h_fm_lt_fn
-    have h_neg_d_lt_fn : -d < f n :=
-      by
+    have h_neg_d_lt_fn : -d < f n := by
       calc
         -d = f (m - n) - f m := neg_sub _ _
         _ < f (m - n) := (sub_lt_self _ (hpos m))
@@ -47,8 +45,7 @@ theorem imo2011_q5 (f : ℤ → ℤ) (hpos : ∀ n : ℤ, 0 < f n) (hdvd : ∀ m
       -- ⊢ f (m - n) ∣ f n - f m
       rw [← dvd_neg, neg_sub];
       exact hdvd m n
-    have h_d_eq_zero : d = 0 :=
-      by
+    have h_d_eq_zero : d = 0 := by
       obtain hd | hd | hd : d > 0 ∨ d = 0 ∨ d < 0 := trichotomous d 0
       · -- d > 0
         have h₁ : f n ≤ d := le_of_dvd hd h_fn_dvd_d
