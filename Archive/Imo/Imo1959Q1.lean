b125b31c48a1b2acac1d05cfa70f7d9e41e11679
Yury G. Kudryashov <urkud@urkud.name>
2023-06-15 12:44:44 -0500

    Fix

--- Archive/Imo/Imo1959Q1.lean
+++ Archive/Imo/Imo1959Q1.lean
@@ -23,20 +23,19 @@ as saying the numerator and denominator are relatively prime.
 
 open Nat
 
-namespace imo1959_q1
+namespace IMO1959Q1
 
 theorem calculation (n k : ℕ) (h1 : k ∣ 21 * n + 4) (h2 : k ∣ 14 * n + 3) : k ∣ 1 :=
   have h3 : k ∣ 2 * (21 * n + 4) := h1.mul_left 2
   have h4 : k ∣ 3 * (14 * n + 3) := h2.mul_left 3
   have h5 : 3 * (14 * n + 3) = 2 * (21 * n + 4) + 1 := by ring
   (Nat.dvd_add_right h3).mp (h5 ▸ h4)
-#align imo1959_q1.calculation Imo1959Q1.calculation
+#align imo1959_q1.calculation IMO1959Q1.calculation
 
-end imo1959_q1
+end IMO1959Q1
 
-open imo1959_q1
+open IMO1959Q1
 
 theorem imo1959_q1 : ∀ n : ℕ, coprime (21 * n + 4) (14 * n + 3) := fun n =>
-  coprime_of_dvd' fun k hp h1 h2 => calculation n k h1 h2
+  coprime_of_dvd' fun k _ h1 h2 => calculation n k h1 h2
 #align imo1959_q1 imo1959_q1
-
