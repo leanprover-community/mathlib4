9ada2df96656b66edfb114b8a7f72b2342f62c4d
int-y1 <jason_yuen2007@hotmail.com>
2023-06-15 19:52:56 -0700

    code style

--- Archive/100-theorems-list/42InverseTriangleSum.lean
+++ Archive/100-theorems-list/42InverseTriangleSum.lean
@@ -33,11 +33,11 @@ open Finset
 theorem Theorem100.inverse_triangle_sum :
     ∀ n, ∑ k in range n, (2 : ℚ) / (k * (k + 1)) = if n = 0 then 0 else 2 - (2 : ℚ) / n := by
   refine' sum_range_induction _ _ (if_pos rfl) _
-  rintro (_ | n); · rw [if_neg, if_pos] <;> norm_num
+  rintro (_ | n)
+  · rw [if_neg, if_pos] <;> norm_num
   simp_rw [if_neg (Nat.succ_ne_zero _), Nat.succ_eq_add_one]
   have A : (n + 1 + 1 : ℚ) ≠ 0 := by norm_cast; norm_num
   push_cast
   field_simp [Nat.cast_add_one_ne_zero]
   ring
 #align theorem_100.inverse_triangle_sum Theorem100.inverse_triangle_sum
-
