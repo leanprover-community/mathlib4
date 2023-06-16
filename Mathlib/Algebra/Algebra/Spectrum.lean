e40aa4e20994c429284f9b4867612638eb580af4
Jireh Loreaux <loreaujy@gmail.com>
2023-06-16 14:53:01 -0500

    feat: add `AlgEquiv.spectrum_eq`

--- Mathlib/Algebra/Algebra/Spectrum.lean
+++ Mathlib/Algebra/Algebra/Spectrum.lean
@@ -431,3 +431,10 @@ theorem apply_mem_spectrum [Nontrivial R] (φ : F) (a : A) : φ a ∈ σ a := by
 end CommRing
 
 end AlgHom
+
+theorem AlgEquiv.spectrum_eq {F R A B : Type _} [CommSemiring R] [Ring A] [Ring B] [Algebra R A]
+    [Algebra R B] [AlgEquivClass F R A B] (f : F) (a : A) :
+    spectrum R (f a) = spectrum R a :=
+  Set.Subset.antisymm (AlgHom.spectrum_apply_subset _ _) <| by
+    simpa only [AlgEquiv.coe_algHom, AlgEquiv.coe_coe_symm_apply_coe_apply] using
+      AlgHom.spectrum_apply_subset (f : A ≃ₐ[R] B).symm (f a)
