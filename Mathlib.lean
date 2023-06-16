3ace58e911df7256075e339f5ddc4bfb0cf7d6fd
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2023-06-16 10:41:37 +0000

    feat: port NumberTheory.DiophantineApproximation (#5126)

--- Mathlib.lean
+++ Mathlib.lean
@@ -2245,6 +2245,7 @@ import Mathlib.NumberTheory.ClassNumber.AdmissibleAbs
 import Mathlib.NumberTheory.ClassNumber.AdmissibleAbsoluteValue
 import Mathlib.NumberTheory.ClassNumber.AdmissibleCardPowDegree
 import Mathlib.NumberTheory.Dioph
+import Mathlib.NumberTheory.DiophantineApproximation
 import Mathlib.NumberTheory.Divisors
 import Mathlib.NumberTheory.Fermat4
 import Mathlib.NumberTheory.FermatPsp
