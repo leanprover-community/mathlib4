7940a66f5e889ee05146630bacd75462600bd8fd
Parcly Taxel <reddeloostw@gmail.com>
2023-06-16 15:06:23 +0800

    Fix errors

--- Counterexamples/LinearOrderWithPosMulPosEqZero.lean
+++ Counterexamples/LinearOrderWithPosMulPosEqZero.lean
@@ -13,7 +13,7 @@ import Mathlib.Algebra.Order.Monoid.Defs
 import Mathlib.Algebra.Order.Monoid.WithZero.Defs
 
 /-!
-An example of a `linear_ordered_comm_monoid_with_zero` in which the product of two positive
+An example of a `LinearOrderedCommMonoidWithZero` in which the product of two positive
 elements vanishes.
 
 This is the monoid with 3 elements `0, ε, 1` where `ε ^ 2 = 0` and everything else is forced.
@@ -48,98 +48,55 @@ instance : One Foo :=
 
 local notation "ε" => eps
 
-/-- The order on `foo` is the one induced by the natural order on the image of `aux1`. -/
+/-- The order on `Foo` is the one induced by the natural order on the image of `aux1`. -/
 def aux1 : Foo → ℕ
   | 0 => 0
   | ε => 1
   | 1 => 2
 #align counterexample.foo.aux1 Counterexample.Foo.aux1
 
-/- ./././Mathport/Syntax/Translate/Expr.lean:336:4: warning: unsupported (TODO): `[tacs] -/
 /-- A tactic to prove facts by cases. -/
-unsafe def boom : tactic Unit :=
-  sorry
-#align counterexample.foo.boom counterexample.foo.boom
+macro (name := boom) "boom" : tactic => `(tactic| (repeat' rintro ⟨⟩) <;> decide)
+#align counterexample.foo.boom Counterexample.Foo.boom
 
-/- ./././Mathport/Syntax/Translate/Tactic/Builtin.lean:69:18: unsupported non-interactive tactic counterexample.foo.boom -/
-theorem aux1_inj : Function.Injective aux1 := by
-  run_tac
-    boom
+theorem aux1_inj : Function.Injective aux1 := by boom
 #align counterexample.foo.aux1_inj Counterexample.Foo.aux1_inj
 
-instance : LinearOrder Foo :=
+instance linearOrder : LinearOrder Foo :=
   LinearOrder.lift' aux1 aux1_inj
 
-/-- Multiplication on `foo`: the only external input is that `ε ^ 2 = 0`. -/
+/-- Multiplication on `Foo`: the only external input is that `ε ^ 2 = 0`. -/
 def mul : Foo → Foo → Foo
   | 1, x => x
   | x, 1 => x
   | _, _ => 0
 #align counterexample.foo.mul Counterexample.Foo.mul
 
-/- ./././Mathport/Syntax/Translate/Tactic/Builtin.lean:69:18: unsupported non-interactive tactic counterexample.foo.boom -/
-/- ./././Mathport/Syntax/Translate/Tactic/Builtin.lean:69:18: unsupported non-interactive tactic counterexample.foo.boom -/
-/- ./././Mathport/Syntax/Translate/Tactic/Builtin.lean:69:18: unsupported non-interactive tactic counterexample.foo.boom -/
-/- ./././Mathport/Syntax/Translate/Tactic/Builtin.lean:69:18: unsupported non-interactive tactic counterexample.foo.boom -/
-instance : CommMonoid Foo where
+instance commMonoid : CommMonoid Foo where
   mul := mul
   one := 1
-  one_mul := by
-    run_tac
-      boom
-  mul_one := by
-    run_tac
-      boom
-  mul_comm := by
-    run_tac
-      boom
-  mul_assoc := by
-    run_tac
-      boom
+  one_mul := by boom
+  mul_one := by boom
+  mul_comm := by boom
+  mul_assoc := by boom
 
-/- ./././Mathport/Syntax/Translate/Tactic/Builtin.lean:69:18: unsupported non-interactive tactic counterexample.foo.boom -/
-/- ./././Mathport/Syntax/Translate/Tactic/Builtin.lean:69:18: unsupported non-interactive tactic counterexample.foo.boom -/
 instance : LinearOrderedCommMonoidWithZero Foo :=
   { Foo.linearOrder, Foo.commMonoid with
     zero := 0
-    zero_mul := by
-      run_tac
-        boom
-    mul_zero := by
-      run_tac
-        boom
+    zero_mul := by boom
+    mul_zero := by boom
     mul_le_mul_left := by rintro ⟨⟩ ⟨⟩ h ⟨⟩ <;> revert h <;> decide
     zero_le_one := by decide }
 
-/- ./././Mathport/Syntax/Translate/Tactic/Builtin.lean:69:18: unsupported non-interactive tactic counterexample.foo.boom -/
-/- ./././Mathport/Syntax/Translate/Tactic/Builtin.lean:69:18: unsupported non-interactive tactic counterexample.foo.boom -/
-/- ./././Mathport/Syntax/Translate/Tactic/Builtin.lean:69:18: unsupported non-interactive tactic counterexample.foo.boom -/
-theorem not_mul_pos :
-    ¬∀ {M : Type} [LinearOrderedCommMonoidWithZero M],
-        ∀ (a b : M) (ha : 0 < a) (hb : 0 < b), 0 < a * b := by
+theorem not_mul_pos : ¬∀ {M : Type} [LinearOrderedCommMonoidWithZero M],
+    ∀ (a b : M) (_ : 0 < a) (_ : 0 < b), 0 < a * b := by
   intro h
-  specialize
-    h ε ε
-      (by
-        run_tac
-          boom)
-      (by
-        run_tac
-          boom)
-  exact
-    (lt_irrefl 0
-        (h.trans_le
-          (by
-            run_tac
-              boom))).elim
+  specialize h ε ε (by boom) (by boom)
+  exact (lt_irrefl 0 (h.trans_le (by boom))).elim
 #align counterexample.foo.not_mul_pos Counterexample.Foo.not_mul_pos
 
-/- ./././Mathport/Syntax/Translate/Tactic/Builtin.lean:69:18: unsupported non-interactive tactic counterexample.foo.boom -/
-example : 0 < ε ∧ ε * ε = 0 := by
-  run_tac
-    boom
+example : 0 < ε ∧ ε * ε = 0 := by boom
 
 end Foo
 
 end Counterexample
-
