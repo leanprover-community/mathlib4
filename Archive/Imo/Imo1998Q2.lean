d287250fb6308271966ee2ed7777651f7becfcab
Moritz Firsching <firsching@google.com>
2023-06-16 22:10:02 +0200

    starting

--- Archive/Imo/Imo1998Q2.lean
+++ Archive/Imo/Imo1998Q2.lean
@@ -43,11 +43,14 @@ Rearranging gives the result.
 -/
 
 
+-- porting note: `A` already upper case
+set_option linter.uppercaseLean3 false
+
 open scoped Classical
 
 variable {C J : Type _} (r : C → J → Prop)
 
-namespace imo1998_q2
+namespace Imo1998Q2
 
 noncomputable section
 
@@ -100,64 +103,67 @@ theorem JudgePair.agree_iff_same_rating (p : JudgePair J) (c : C) :
 
 /-- The set of contestants on which two judges agree. -/
 def agreedContestants [Fintype C] (p : JudgePair J) : Finset C :=
-  Finset.univ.filterₓ fun c => p.Agree r c
+  Finset.univ.filter fun c => p.Agree r c
 #align imo1998_q2.agreed_contestants Imo1998Q2.agreedContestants
-
 section
 
 variable [Fintype J] [Fintype C]
 
 /-- All incidences of agreement. -/
-def a : Finset (AgreedTriple C J) :=
-  Finset.univ.filterₓ fun a : AgreedTriple C J =>
-    a.JudgePair.Agree r a.contestant ∧ a.JudgePair.distinct
-#align imo1998_q2.A Imo1998Q2.a
+def A : Finset (AgreedTriple C J) :=
+  Finset.univ.filter @fun (a : AgreedTriple C J) =>
+    (a.judgePair.Agree r a.contestant ∧ a.judgePair.Distinct)
+#align imo1998_q2.A Imo1998Q2.A
 
-theorem a_maps_to_offDiag_judgePair (a : AgreedTriple C J) :
-    a ∈ a r → a.JudgePair ∈ Finset.offDiag (@Finset.univ J _) := by simp [A, Finset.mem_offDiag]
-#align imo1998_q2.A_maps_to_off_diag_judge_pair Imo1998Q2.a_maps_to_offDiag_judgePair
+theorem A_maps_to_offDiag_judgePair (a : AgreedTriple C J) :
+    a ∈ A r → a.judgePair ∈ Finset.offDiag (@Finset.univ J _) := by simp [A, Finset.mem_offDiag]
+#align imo1998_q2.A_maps_to_off_diag_judge_pair Imo1998Q2.A_maps_to_offDiag_judgePair
 
-theorem a_fibre_over_contestant (c : C) :
-    (Finset.univ.filterₓ fun p : JudgePair J => p.Agree r c ∧ p.distinct) =
-      ((a r).filterₓ fun a : AgreedTriple C J => a.contestant = c).image Prod.snd := by
+theorem A_fibre_over_contestant (c : C) :
+    (Finset.univ.filter fun p : JudgePair J => p.Agree r c ∧ p.Distinct) =
+      ((A r).filter fun a : AgreedTriple C J => a.contestant = c).image Prod.snd := by
   ext p
   simp only [A, Finset.mem_univ, Finset.mem_filter, Finset.mem_image, true_and_iff, exists_prop]
   constructor
-  · rintro ⟨h₁, h₂⟩; refine' ⟨(c, p), _⟩; finish
-  · intro h; finish
-#align imo1998_q2.A_fibre_over_contestant Imo1998Q2.a_fibre_over_contestant
+  · rintro ⟨h₁, h₂⟩; refine' ⟨(c, p), _⟩; tauto
+  · intro h; aesop
+#align imo1998_q2.A_fibre_over_contestant Imo1998Q2.A_fibre_over_contestant
 
-theorem a_fibre_over_contestant_card (c : C) :
-    (Finset.univ.filterₓ fun p : JudgePair J => p.Agree r c ∧ p.distinct).card =
-      ((a r).filterₓ fun a : AgreedTriple C J => a.contestant = c).card :=
-  by rw [A_fibre_over_contestant r]; apply Finset.card_image_of_injOn; tidy
-#align imo1998_q2.A_fibre_over_contestant_card Imo1998Q2.a_fibre_over_contestant_card
+theorem A_fibre_over_contestant_card (c : C) :
+    (Finset.univ.filter fun p : JudgePair J => p.Agree r c ∧ p.Distinct).card =
+      ((A r).filter fun a : AgreedTriple C J => a.contestant = c).card := by
+  rw [A_fibre_over_contestant r]
+  apply Finset.card_image_of_injOn
+  sorry
+#align imo1998_q2.A_fibre_over_contestant_card Imo1998Q2.A_fibre_over_contestant_card
 
-theorem a_fibre_over_judgePair {p : JudgePair J} (h : p.distinct) :
+theorem A_fibre_over_judgePair {p : JudgePair J} (h : p.Distinct) :
     agreedContestants r p =
-      ((a r).filterₓ fun a : AgreedTriple C J => a.JudgePair = p).image AgreedTriple.contestant :=
+      ((A r).filter fun a : AgreedTriple C J => a.judgePair = p).image AgreedTriple.contestant :=
   by
-  dsimp only [A, agreed_contestants]; ext c; constructor <;> intro h
-  · rw [Finset.mem_image]; refine' ⟨⟨c, p⟩, _⟩; finish
-  · finish
-#align imo1998_q2.A_fibre_over_judge_pair Imo1998Q2.a_fibre_over_judgePair
+  dsimp only [A, agreedContestants]; ext c; constructor <;> intro h
+  · rw [Finset.mem_image]; refine' ⟨⟨c, p⟩, _⟩; aesop
+  · sorry
+#align imo1998_q2.A_fibre_over_judge_pair Imo1998Q2.A_fibre_over_judgePair
 
-theorem a_fibre_over_judgePair_card {p : JudgePair J} (h : p.distinct) :
+theorem A_fibre_over_judgePair_card {p : JudgePair J} (h : p.Distinct) :
     (agreedContestants r p).card =
-      ((a r).filterₓ fun a : AgreedTriple C J => a.JudgePair = p).card :=
-  by rw [A_fibre_over_judge_pair r h]; apply Finset.card_image_of_injOn; tidy
-#align imo1998_q2.A_fibre_over_judge_pair_card Imo1998Q2.a_fibre_over_judgePair_card
+      ((A r).filter fun a : AgreedTriple C J => a.judgePair = p).card := by
+  rw [A_fibre_over_judgePair r h]
+  apply Finset.card_image_of_injOn
+  sorry
+#align imo1998_q2.A_fibre_over_judge_pair_card Imo1998Q2.A_fibre_over_judgePair_card
 
-theorem a_card_upper_bound {k : ℕ}
-    (hk : ∀ p : JudgePair J, p.distinct → (agreedContestants r p).card ≤ k) :
-    (a r).card ≤ k * (Fintype.card J * Fintype.card J - Fintype.card J) := by
+theorem A_card_upper_bound {k : ℕ}
+    (hk : ∀ p : JudgePair J, p.Distinct → (agreedContestants r p).card ≤ k) :
+    (A r).card ≤ k * (Fintype.card J * Fintype.card J - Fintype.card J) := by
   change _ ≤ k * (Finset.card _ * Finset.card _ - Finset.card _)
   rw [← Finset.offDiag_card]
-  apply Finset.card_le_mul_card_image_of_maps_to (A_maps_to_off_diag_judge_pair r)
+  apply Finset.card_le_mul_card_image_of_maps_to (A_maps_to_offDiag_judgePair r)
   intro p hp
-  have hp' : p.distinct := by simp [Finset.mem_offDiag] at hp ; exact hp
-  rw [← A_fibre_over_judge_pair_card r hp']; apply hk; exact hp'
-#align imo1998_q2.A_card_upper_bound Imo1998Q2.a_card_upper_bound
+  have hp' : p.Distinct := by simp [Finset.mem_offDiag] at hp ; exact hp
+  rw [← A_fibre_over_judgePair_card r hp']; apply hk; exact hp'
+#align imo1998_q2.A_card_upper_bound Imo1998Q2.A_card_upper_bound
 
 end
 
@@ -180,10 +186,10 @@ section
 variable [Fintype J]
 
 theorem judge_pairs_card_lower_bound {z : ℕ} (hJ : Fintype.card J = 2 * z + 1) (c : C) :
-    2 * z * z + 2 * z + 1 ≤ (Finset.univ.filterₓ fun p : JudgePair J => p.Agree r c).card := by
-  let x := (finset.univ.filter fun j => r c j).card
-  let y := (finset.univ.filter fun j => ¬r c j).card
-  have h : (finset.univ.filter fun p : judge_pair J => p.Agree r c).card = x * x + y * y := by
+    2 * z * z + 2 * z + 1 ≤ (Finset.univ.filter fun p : JudgePair J => p.Agree r c).card := by
+  let x := (Finset.univ.filter fun j => r c j).card
+  let y := (Finset.univ.filter fun j => ¬r c j).card
+  have h : (Finset.univ.filter fun p : JudgePair J => p.Agree r c).card = x * x + y * y := by
     simp [← Finset.filter_product_card]
   rw [h]; apply Int.le_of_ofNat_le_ofNat; simp only [Int.ofNat_add, Int.ofNat_mul]
   apply norm_bound_of_odd_sum
@@ -192,28 +198,28 @@ theorem judge_pairs_card_lower_bound {z : ℕ} (hJ : Fintype.card J = 2 * z + 1)
 #align imo1998_q2.judge_pairs_card_lower_bound Imo1998Q2.judge_pairs_card_lower_bound
 
 theorem distinct_judge_pairs_card_lower_bound {z : ℕ} (hJ : Fintype.card J = 2 * z + 1) (c : C) :
-    2 * z * z ≤ (Finset.univ.filterₓ fun p : JudgePair J => p.Agree r c ∧ p.distinct).card := by
-  let s := finset.univ.filter fun p : judge_pair J => p.Agree r c
-  let t := finset.univ.filter fun p : judge_pair J => p.distinct
+    2 * z * z ≤ (Finset.univ.filter fun p : JudgePair J => p.Agree r c ∧ p.Distinct).card := by
+  let s := Finset.univ.filter fun p : JudgePair J => p.Agree r c
+  let t := Finset.univ.filter fun p : JudgePair J => p.Distinct
   have hs : 2 * z * z + 2 * z + 1 ≤ s.card := judge_pairs_card_lower_bound r hJ c
-  have hst : s \ t = finset.univ.diag := by
+  have hst : s \ t = Finset.univ.diag := by
     ext p; constructor <;> intros
-    · finish
-    · suffices p.judge₁ = p.judge₂ by simp [this]; finish
+    · aesop
+    · suffices p.judge₁ = p.judge₂ by simp [this];
   have hst' : (s \ t).card = 2 * z + 1 := by rw [hst, Finset.diag_card, ← hJ]; rfl
   rw [Finset.filter_and, ← Finset.sdiff_sdiff_self_left s t, Finset.card_sdiff]
   · rw [hst']; rw [add_assoc] at hs ; apply le_tsub_of_add_le_right hs
   · apply Finset.sdiff_subset
 #align imo1998_q2.distinct_judge_pairs_card_lower_bound Imo1998Q2.distinct_judge_pairs_card_lower_bound
 
-theorem a_card_lower_bound [Fintype C] {z : ℕ} (hJ : Fintype.card J = 2 * z + 1) :
-    2 * z * z * Fintype.card C ≤ (a r).card := by
+theorem A_card_lower_bound [Fintype C] {z : ℕ} (hJ : Fintype.card J = 2 * z + 1) :
+    2 * z * z * Fintype.card C ≤ (A r).card := by
   have h : ∀ a, a ∈ A r → Prod.fst a ∈ @Finset.univ C _ := by intros; apply Finset.mem_univ
   apply Finset.mul_card_image_le_card_of_maps_to h
-  intro c hc
+  intro c _
   rw [← A_fibre_over_contestant_card]
   apply distinct_judge_pairs_card_lower_bound r hJ
-#align imo1998_q2.A_card_lower_bound Imo1998Q2.a_card_lower_bound
+#align imo1998_q2.A_card_lower_bound Imo1998Q2.A_card_lower_bound
 
 end
 
@@ -222,25 +228,24 @@ theorem clear_denominators {a b k : ℕ} (ha : 0 < a) (hb : 0 < b) :
   rw [div_le_div_iff] <;> norm_cast <;> simp [ha, hb]
 #align imo1998_q2.clear_denominators Imo1998Q2.clear_denominators
 
-end imo1998_q2
+end Imo1998Q2
 
-open imo1998_q2
+open Imo1998Q2
 
 theorem imo1998_q2 [Fintype J] [Fintype C] (a b k : ℕ) (hC : Fintype.card C = a)
     (hJ : Fintype.card J = b) (ha : 0 < a) (hb : Odd b)
-    (hk : ∀ p : JudgePair J, p.distinct → (agreedContestants r p).card ≤ k) :
+    (hk : ∀ p : JudgePair J, p.Distinct → (agreedContestants r p).card ≤ k) :
     (b - 1 : ℚ) / (2 * b) ≤ k / a := by
   rw [clear_denominators ha hb.pos]
   obtain ⟨z, hz⟩ := hb; rw [hz] at hJ ; rw [hz]
   have h := le_trans (A_card_lower_bound r hJ) (A_card_upper_bound r hk)
-  rw [hC, hJ] at h 
+  rw [hC, hJ] at h
   -- We are now essentially done; we just need to bash `h` into exactly the right shape.
   have hl : k * ((2 * z + 1) * (2 * z + 1) - (2 * z + 1)) = k * (2 * (2 * z + 1)) * z := by
-    simp only [add_mul, two_mul, mul_comm, mul_assoc]; finish
+    simp only [add_mul, two_mul, mul_comm, mul_assoc]; sorry
   have hr : 2 * z * z * a = 2 * z * a * z := by ring
-  rw [hl, hr] at h 
-  cases z
+  rw [hl, hr] at h
+  cases' z with z
   · simp
   · exact le_of_mul_le_mul_right h z.succ_pos
 #align imo1998_q2 imo1998_q2
-
