e9426d48c849c7d32eb18dd79a4ad63a38039cb3
Komyyy <pol_tta@outlook.jp>
2023-06-16 14:14:01 +0900

    fix

--- Counterexamples/Cyclotomic105.lean
+++ Counterexamples/Cyclotomic105.lean
@@ -47,6 +47,7 @@ theorem properDivisors_35 : Nat.properDivisors 35 = {1, 5, 7} :=
   rfl
 #align counterexample.proper_divisors_35 Counterexample.properDivisors_35
 
+set_option maxRecDepth 1024 in
 theorem properDivisors_105 : Nat.properDivisors 105 = {1, 3, 5, 7, 15, 21, 35} :=
   rfl
 #align counterexample.proper_divisors_105 Counterexample.properDivisors_105
@@ -69,7 +70,7 @@ theorem cyclotomic_7 : cyclotomic 7 ℤ = 1 + X + X ^ 2 + X ^ 3 + X ^ 4 + X ^ 5 
 
 theorem cyclotomic_15 : cyclotomic 15 ℤ = 1 - X + X ^ 3 - X ^ 4 + X ^ 5 - X ^ 7 + X ^ 8 := by
   refine' ((eq_cyclotomic_iff (by norm_num) _).2 _).symm
-  rw [proper_divisors_15, Finset.prod_insert _, Finset.prod_insert _, Finset.prod_singleton,
+  rw [properDivisors_15, Finset.prod_insert _, Finset.prod_insert _, Finset.prod_singleton,
     cyclotomic_one, cyclotomic_3, cyclotomic_5]
   ring
   repeat' norm_num
@@ -78,7 +79,7 @@ theorem cyclotomic_15 : cyclotomic 15 ℤ = 1 - X + X ^ 3 - X ^ 4 + X ^ 5 - X ^ 
 theorem cyclotomic_21 :
     cyclotomic 21 ℤ = 1 - X + X ^ 3 - X ^ 4 + X ^ 6 - X ^ 8 + X ^ 9 - X ^ 11 + X ^ 12 := by
   refine' ((eq_cyclotomic_iff (by norm_num) _).2 _).symm
-  rw [proper_divisors_21, Finset.prod_insert _, Finset.prod_insert _, Finset.prod_singleton,
+  rw [properDivisors_21, Finset.prod_insert _, Finset.prod_insert _, Finset.prod_singleton,
     cyclotomic_one, cyclotomic_3, cyclotomic_7]
   ring
   repeat' norm_num
@@ -87,13 +88,9 @@ theorem cyclotomic_21 :
 theorem cyclotomic_35 :
     cyclotomic 35 ℤ =
       1 - X + X ^ 5 - X ^ 6 + X ^ 7 - X ^ 8 + X ^ 10 - X ^ 11 + X ^ 12 - X ^ 13 + X ^ 14 - X ^ 16 +
-                X ^ 17 -
-              X ^ 18 +
-            X ^ 19 -
-          X ^ 23 +
-        X ^ 24 := by
+        X ^ 17 - X ^ 18 + X ^ 19 - X ^ 23 + X ^ 24 := by
   refine' ((eq_cyclotomic_iff (by norm_num) _).2 _).symm
-  rw [proper_divisors_35, Finset.prod_insert _, Finset.prod_insert _, Finset.prod_singleton,
+  rw [properDivisors_35, Finset.prod_insert _, Finset.prod_insert _, Finset.prod_singleton,
     cyclotomic_one, cyclotomic_5, cyclotomic_7]
   ring
   repeat' norm_num
@@ -102,31 +99,12 @@ theorem cyclotomic_35 :
 theorem cyclotomic_105 :
     cyclotomic 105 ℤ =
       1 + X + X ^ 2 - X ^ 5 - X ^ 6 - 2 * X ^ 7 - X ^ 8 - X ^ 9 + X ^ 12 + X ^ 13 + X ^ 14 +
-                                                  X ^ 15 +
-                                                X ^ 16 +
-                                              X ^ 17 -
-                                            X ^ 20 -
-                                          X ^ 22 -
-                                        X ^ 24 -
-                                      X ^ 26 -
-                                    X ^ 28 +
-                                  X ^ 31 +
-                                X ^ 32 +
-                              X ^ 33 +
-                            X ^ 34 +
-                          X ^ 35 +
-                        X ^ 36 -
-                      X ^ 39 -
-                    X ^ 40 -
-                  2 * X ^ 41 -
-                X ^ 42 -
-              X ^ 43 +
-            X ^ 46 +
-          X ^ 47 +
-        X ^ 48 := by
+        X ^ 15 + X ^ 16 + X ^ 17 - X ^ 20 - X ^ 22 - X ^ 24 - X ^ 26 - X ^ 28 + X ^ 31 + X ^ 32 +
+        X ^ 33 + X ^ 34 + X ^ 35 + X ^ 36 - X ^ 39 - X ^ 40 - 2 * X ^ 41 - X ^ 42 - X ^ 43 +
+        X ^ 46 + X ^ 47 + X ^ 48 := by
   refine' ((eq_cyclotomic_iff (by norm_num) _).2 _).symm
-  rw [proper_divisors_105]
-  repeat' rw [Finset.prod_insert _]
+  rw [properDivisors_105]
+  repeat' rw [Finset.prod_insert (α := ℕ) (β := ℤ[X])]
   rw [Finset.prod_singleton, cyclotomic_one, cyclotomic_3, cyclotomic_5, cyclotomic_7,
     cyclotomic_15, cyclotomic_21, cyclotomic_35]
   ring
@@ -134,16 +112,15 @@ theorem cyclotomic_105 :
 #align counterexample.cyclotomic_105 Counterexample.cyclotomic_105
 
 theorem coeff_cyclotomic_105 : coeff (cyclotomic 105 ℤ) 7 = -2 := by
-  simp [cyclotomic_105, coeff_X_pow, coeff_one, coeff_X_of_ne_one, coeff_bit0_mul, coeff_bit1_mul]
+  simp [cyclotomic_105, coeff_X_pow, coeff_one, coeff_X_of_ne_one, coeff_bit0_mul, two_mul]
 #align counterexample.coeff_cyclotomic_105 Counterexample.coeff_cyclotomic_105
 
 theorem not_forall_coeff_cyclotomic_neg_one_zero_one :
     ¬∀ n i, coeff (cyclotomic n ℤ) i ∈ ({-1, 0, 1} : Set ℤ) := by
   intro h
   specialize h 105 7
-  rw [coeff_cyclotomic_105] at h 
-  norm_num at h 
+  rw [coeff_cyclotomic_105] at h
+  norm_num at h
 #align counterexample.not_forall_coeff_cyclotomic_neg_one_zero_one Counterexample.not_forall_coeff_cyclotomic_neg_one_zero_one
 
 end Counterexample
-
