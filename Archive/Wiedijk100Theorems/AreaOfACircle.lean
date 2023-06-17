02bdf97da408fc0ac10237db1b0418d6a1c51164
Komyyy <pol_tta@outlook.jp>
2023-06-17 14:46:55 +0900

    fix

--- Archive/Wiedijk100Theorems/AreaOfACircle.lean
+++ Archive/Wiedijk100Theorems/AreaOfACircle.lean
@@ -3,8 +3,8 @@ Copyright (c) 2021 James Arthur, Benjamin Davidson, Andrew Souther. All rights r
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: James Arthur, Benjamin Davidson, Andrew Souther
 
-! This file was ported from Lean 3 source module «100-theorems-list».«9_area_of_a_circle»
-! leanprover-community/mathlib commit 328375597f2c0dd00522d9c2e5a33b6a6128feeb
+! This file was ported from Lean 3 source module wiedijk_100_theorems.area_of_a_circle
+! leanprover-community/mathlib commit 5563b1b49e86e135e8c7b556da5ad2f5ff881cad
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
