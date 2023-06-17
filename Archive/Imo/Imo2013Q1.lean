a52951de4fdddcbdd31484521f116459f7a93659
Moritz Firsching <firsching@google.com>
2023-06-17 14:47:50 +0200

    starting

--- Archive/Imo/Imo2013Q1.lean
+++ Archive/Imo/Imo2013Q1.lean
@@ -32,11 +32,11 @@ We prove a slightly more general version where k does not need to be strictly po
 
 open scoped BigOperators
 
-namespace imo2013_q1
+namespace Imo2013Q1
 
 theorem arith_lemma (k n : ℕ) : 0 < 2 * n + 2 ^ k.succ :=
   calc
-    0 < 2 := zero_lt_two
+    0 < 2 := by decide
     _ = 2 ^ 1 := (pow_one 2).symm
     _ ≤ 2 ^ k.succ := (Nat.pow_le_pow_of_le_right zero_lt_two (Nat.le_add_left 1 k))
     _ ≤ 2 * n + 2 ^ k.succ := Nat.le_add_left _ _
@@ -44,30 +44,29 @@ theorem arith_lemma (k n : ℕ) : 0 < 2 * n + 2 ^ k.succ :=
 
 theorem prod_lemma (m : ℕ → ℕ+) (k : ℕ) (nm : ℕ+) :
     ∏ i : ℕ in Finset.range k, ((1 : ℚ) + 1 / ↑(if i < k then m i else nm)) =
-      ∏ i : ℕ in Finset.range k, (1 + 1 / m i) := by
+      ∏ i : ℕ in Finset.range k, (1 + 1 / (m i : ℚ)) := by
   suffices : ∀ i, i ∈ Finset.range k → (1 : ℚ) + 1 / ↑(if i < k then m i else nm) = 1 + 1 / m i
   exact Finset.prod_congr rfl this
   intro i hi
-  simp [finset.mem_range.mp hi]
+  simp [Finset.mem_range.mp hi]
 #align imo2013_q1.prod_lemma Imo2013Q1.prod_lemma
 
-end imo2013_q1
+end Imo2013Q1
 
-open imo2013_q1
-
+open Imo2013Q1
 theorem imo2013_q1 (n : ℕ+) (k : ℕ) :
-    ∃ m : ℕ → ℕ+, (1 : ℚ) + (2 ^ k - 1) / n = ∏ i in Finset.range k, (1 + 1 / m i) := by
+    ∃ m : ℕ → ℕ+, (1 : ℚ) + (2 ^ k - 1) / n = ∏ i in Finset.range k, (1 + 1 / (m i : ℚ)) := by
   revert n
   induction' k with pk hpk
-  · intro n; use fun _ => 1; simp
+  · intro n; use fun (_ : ℕ) => (1 : ℕ+); simp
   -- For the base case, any m works.
   intro n
   obtain ⟨t, ht : ↑n = t + t⟩ | ⟨t, ht : ↑n = 2 * t + 1⟩ := (n : ℕ).even_or_odd
   · -- even case
-    rw [← two_mul] at ht 
-    cases t
+    rw [← two_mul] at ht
+    cases' t with t
     -- Eliminate the zero case to simplify later calculations.
-    · exfalso; rw [MulZeroClass.mul_zero] at ht ; exact PNat.ne_zero n ht
+    · exfalso; rw [Nat.mul_zero] at ht ; exact PNat.ne_zero n ht
     -- Now we have ht : ↑n = 2 * (t + 1).
     let t_succ : ℕ+ := ⟨t + 1, t.succ_pos⟩
     obtain ⟨pm, hpm⟩ := hpk t_succ
@@ -75,35 +74,42 @@ theorem imo2013_q1 (n : ℕ+) (k : ℕ) :
     use m
     have hmpk : (m pk : ℚ) = 2 * t + 2 ^ pk.succ := by
       have : m pk = ⟨2 * t + 2 ^ pk.succ, _⟩ := if_neg (irrefl pk); simp [this]
-    have denom_ne_zero : 2 * (t : ℚ) + 2 ^ pk.succ ≠ 0 := by norm_cast;
+    have denom_ne_zero : 2 * (t : ℚ) + 2 ^ pk.succ ≠ 0 := by
+      norm_cast
       exact ne_of_gt <| arith_lemma pk t
     calc
-      (1 : ℚ) + (2 ^ pk.succ - 1) / ↑n = 1 + (2 * 2 ^ pk - 1) / (2 * (t + 1) : ℕ) := by
-        rw [coe_coe n, ht, pow_succ]
+      ((1 : ℚ) + (2 ^ pk.succ - 1) / (n : ℚ) : ℚ)= 1 + (2 * 2 ^ pk - 1) / (2 * (t + 1) : ℕ) := by
+        rw [ht, pow_succ]
       _ = (1 + 1 / (2 * t + 2 * 2 ^ pk)) * (1 + (2 ^ pk - 1) / (↑t + 1)) := by
+        have : (2 * (t : ℚ) + 2 * 2 ^ pk) ≠ 0 := by
+          sorry
         field_simp [t.cast_add_one_ne_zero]
         ring
-      _ = (1 + 1 / (2 * t + 2 ^ pk.succ)) * (1 + (2 ^ pk - 1) / t_succ) := by norm_cast
-      _ = (∏ i in Finset.range pk, (1 + 1 / m i)) * (1 + 1 / m pk) := by
+      _ = (1 + 1 / (2 * t + 2 ^ pk.succ)) * (1 + (2 ^ pk - 1) / t_succ) := by
+        -- porting note: used to work with `norm_cast`
+        simp only [PNat.mk_coe, Nat.cast_add, Nat.cast_one, mul_eq_mul_right_iff]
+        left
+        rfl
+      _ = (∏ i in Finset.range pk, (1 + 1 / (m i : ℚ))) * (1 + 1 / m pk) := by
         rw [prod_lemma, hpm, ← hmpk, mul_comm]
-      _ = ∏ i in Finset.range pk.succ, (1 + 1 / m i) := by rw [← Finset.prod_range_succ _ pk]
+      _ = ∏ i in Finset.range pk.succ, (1 + 1 / (m i : ℚ)) := by rw [← Finset.prod_range_succ _ pk]
   · -- odd case
     let t_succ : ℕ+ := ⟨t + 1, t.succ_pos⟩
     obtain ⟨pm, hpm⟩ := hpk t_succ
     let m i := if i < pk then pm i else ⟨2 * t + 1, Nat.succ_pos _⟩
     use m
-    have hmpk : (m pk : ℚ) = 2 * t + 1 := by have : m pk = ⟨2 * t + 1, _⟩ := if_neg (irrefl pk);
+    have hmpk : (m pk : ℚ) = 2 * t + 1 := by
+      have : m pk = ⟨2 * t + 1, _⟩ := if_neg (irrefl pk)
       simp [this]
     have denom_ne_zero : 2 * (t : ℚ) + 1 ≠ 0 := by norm_cast; apply (2 * t).succ_ne_zero
     calc
-      (1 : ℚ) + (2 ^ pk.succ - 1) / ↑n = 1 + (2 * 2 ^ pk - 1) / (2 * t + 1 : ℕ) := by
-        rw [coe_coe n, ht, pow_succ]
+      ((1 : ℚ) + (2 ^ pk.succ - 1) / ↑n : ℚ) = 1 + (2 * 2 ^ pk - 1) / (2 * t + 1 : ℕ) := by
+        rw [ht, pow_succ]
       _ = (1 + 1 / (2 * t + 1)) * (1 + (2 ^ pk - 1) / (t + 1)) := by
         field_simp [t.cast_add_one_ne_zero]
         ring
       _ = (1 + 1 / (2 * t + 1)) * (1 + (2 ^ pk - 1) / t_succ) := by norm_cast
-      _ = (∏ i in Finset.range pk, (1 + 1 / m i)) * (1 + 1 / ↑(m pk)) := by
+      _ = (∏ i in Finset.range pk, (1 + 1 / (m i : ℚ))) * (1 + 1 / ↑(m pk)) := by
         rw [prod_lemma, hpm, ← hmpk, mul_comm]
-      _ = ∏ i in Finset.range pk.succ, (1 + 1 / m i) := by rw [← Finset.prod_range_succ _ pk]
+      _ = ∏ i in Finset.range pk.succ, (1 + 1 / (m i : ℚ)) := by rw [← Finset.prod_range_succ _ pk]
 #align imo2013_q1 imo2013_q1
-
