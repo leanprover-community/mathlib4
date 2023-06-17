92e17130e7c57b1673d3831ab0b872c2bb28c9cf
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2023-06-17 11:16:43 +0000

    feat: port NumberTheory.LegendreSymbol.QuadraticChar.Basic (#5175)
    Co-authored-by: Parcly Taxel <reddeloostw@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -2259,6 +2259,7 @@ import Mathlib.NumberTheory.FermatPsp
 import Mathlib.NumberTheory.FrobeniusNumber
 import Mathlib.NumberTheory.LSeries
 import Mathlib.NumberTheory.LegendreSymbol.MulCharacter
+import Mathlib.NumberTheory.LegendreSymbol.QuadraticChar.Basic
 import Mathlib.NumberTheory.LegendreSymbol.ZModChar
 import Mathlib.NumberTheory.Liouville.Basic
 import Mathlib.NumberTheory.Liouville.LiouvilleNumber
