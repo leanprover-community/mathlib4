92afb1976cbd8c6fd61551ede2bacee9f8f38293
Jeremy Tan Jie Rui <reddeloostw@gmail.com>
2023-06-17 11:54:53 +0000

    feat: port Archive.Imo.Imo1975Q1 (#5179)

--- Archive.lean
+++ Archive.lean
@@ -3,6 +3,7 @@ import Archive.Imo.Imo1962Q4
 import Archive.Imo.Imo1964Q1
 import Archive.Imo.Imo1969Q1
 import Archive.Imo.Imo1972Q5
+import Archive.Imo.Imo1975Q1
 import Archive.Imo.Imo1977Q6
 import Archive.Imo.Imo1994Q1
 import Archive.Imo.Imo2001Q6
