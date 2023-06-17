cf7c085de6b54a1eb73ee47fd8ab216b8d75d29f
Scott Morrison <scott@tqft.net>
2023-06-17 08:04:49 +0000

    chore: add links to porting notes (#5111)
    Co-authored-by: Scott Morrison <scott.morrison@anu.edu.au>

--- Mathlib/Topology/Sheaves/Operations.lean
+++ Mathlib/Topology/Sheaves/Operations.lean
@@ -118,7 +118,8 @@ noncomputable def toTotalQuotientPresheaf : F ⟶ F.totalQuotientPresheaf :=
 instance : Epi (toTotalQuotientPresheaf F) := epi_toLocalizationPresheaf _
 
 instance (F : X.Sheaf CommRingCat.{w}) : Mono F.presheaf.toTotalQuotientPresheaf := by
-  -- Porting note : was an `apply (config := { synthAssignedInstances := false })`
+  -- Porting note : was an `apply (config := { instances := false })`
+  -- See https://github.com/leanprover/lean4/issues/2273
   suffices : ∀ (U : (Opens ↑X)ᵒᵖ), Mono (F.presheaf.toTotalQuotientPresheaf.app U)
   . apply NatTrans.mono_of_mono_app
   intro U
