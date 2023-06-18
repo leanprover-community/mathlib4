90ab5b137cd1cbec600817f21ec5ce5b1f1a7e45
int-y1 <jason_yuen2007@hotmail.com>
2023-06-17 22:13:31 -0700

    fix

--- Counterexamples/Pseudoelement.lean
+++ Counterexamples/Pseudoelement.lean
@@ -9,23 +9,23 @@ Authors: Riccardo Brasca
 ! if you have ported upstream changes.
 -/
 import Mathlib.CategoryTheory.Abelian.Pseudoelements
-import Mathlib.Algebra.Category.Module.Biproducts
+import Mathlib.Algebra.Category.ModuleCat.Biproducts
 
 /-!
 # Pseudoelements and pullbacks
 Borceux claims in Proposition 1.9.5 that the pseudoelement constructed in
-`category_theory.abelian.pseudoelement.pseudo_pullback` is unique. We show here that this claim is
+`CategoryTheory.Abelian.Pseudoelement.pseudo_pullback` is unique. We show here that this claim is
 false. This means in particular that we cannot have an extensionality principle for pullbacks in
 terms of pseudoelements.
 
 ## Implementation details
 The construction, suggested in https://mathoverflow.net/a/419951/7845, is the following.
-We work in the category of `Module ℤ` and we consider the special case of `ℚ ⊞ ℚ` (that is the
+We work in the category of `ModuleCat ℤ` and we consider the special case of `ℚ ⊞ ℚ` (that is the
 pullback over the terminal object). We consider the pseudoelements associated to `x : ℚ ⟶ ℚ ⊞ ℚ`
 given by `t ↦ (t, 2 * t)` and `y : ℚ ⟶ ℚ ⊞ ℚ` given by `t ↦ (t, t)`.
 
 ## Main results
-* `category_theory.abelian.pseudoelement.exist_ne_and_fst_eq_fst_and_snd_eq_snd` : there are two
+* `Counterexample.exist_ne_and_fst_eq_fst_and_snd_eq_snd` : there are two
   pseudoelements `x y : ℚ ⊞ ℚ` such that `x ≠ y`, `biprod.fst x = biprod.fst y` and
   `biprod.snd x = biprod.snd y`.
 
@@ -44,7 +44,7 @@ open CategoryTheory.Abelian.Pseudoelement
 
 /-- `x` is given by `t ↦ (t, 2 * t)`. -/
 def x : Over (of ℤ ℚ ⊞ of ℤ ℚ) :=
-  Over.mk (biprod.lift (ofHom id) (ofHom (2 * id)))
+  Over.mk (biprod.lift (ofHom id) (ofHom (2 * LinearMap.id)))
 #align counterexample.x Counterexample.x
 
 /-- `y` is given by `t ↦ (t, t)`. -/
@@ -54,62 +54,63 @@ def y : Over (of ℤ ℚ ⊞ of ℤ ℚ) :=
 
 /-- `biprod.fst ≫ x` is pseudoequal to `biprod.fst y`. -/
 theorem fst_x_pseudo_eq_fst_y : PseudoEqual _ (app biprod.fst x) (app biprod.fst y) := by
-  refine' ⟨of ℤ ℚ, of_hom id, of_hom id, category_struct.id.epi (of ℤ ℚ), _, _⟩
-  · exact (ModuleCat.epi_iff_surjective _).2 fun a => ⟨(a : ℚ), by simp⟩
+  refine' ⟨of ℤ ℚ, ofHom id, ofHom id, instEpiIdToCategoryStruct (of ℤ ℚ), _, _⟩
+  · exact (ModuleCat.epi_iff_surjective _).2 fun a => ⟨(a : ℚ), rfl⟩
   · dsimp [x, y]
     simp
 #align counterexample.fst_x_pseudo_eq_fst_y Counterexample.fst_x_pseudo_eq_fst_y
 
 /-- `biprod.snd ≫ x` is pseudoequal to `biprod.snd y`. -/
 theorem snd_x_pseudo_eq_snd_y : PseudoEqual _ (app biprod.snd x) (app biprod.snd y) := by
-  refine' ⟨of ℤ ℚ, of_hom id, 2 • of_hom id, category_struct.id.epi (of ℤ ℚ), _, _⟩
-  · refine' (ModuleCat.epi_iff_surjective _).2 fun a => ⟨(a / 2 : ℚ), _⟩
-    simp only [two_smul, add_apply, of_hom_apply, id_coe, id.def]
+  refine' ⟨of ℤ ℚ, ofHom id, 2 • ofHom id, instEpiIdToCategoryStruct (of ℤ ℚ), _, _⟩
+  · refine' (ModuleCat.epi_iff_surjective _).2 fun a => ⟨(show ℚ from a) / 2, _⟩
+    simp only [two_smul, add_apply, ofHom_apply, id_coe, id.def]
     exact add_halves' (show ℚ from a)
   · dsimp [x, y]
-    exact concrete_category.hom_ext _ _ fun a => by simpa
+    refine' ConcreteCategory.hom_ext _ _ fun a => _
+    simp_rw [biprod.lift_snd]; rfl
 #align counterexample.snd_x_pseudo_eq_snd_y Counterexample.snd_x_pseudo_eq_snd_y
 
 /-- `x` is not pseudoequal to `y`. -/
 theorem x_not_pseudo_eq : ¬PseudoEqual _ x y := by
   intro h
-  replace h := Module.eq_range_of_pseudoequal h
-  dsimp [x, y] at h 
-  let φ := biprod.lift (of_hom (id : ℚ →ₗ[ℤ] ℚ)) (of_hom (2 * id))
+  replace h := ModuleCat.eq_range_of_pseudoequal h
+  dsimp [x, y] at h
+  let φ := biprod.lift (ofHom (id : ℚ →ₗ[ℤ] ℚ)) (ofHom (2 * LinearMap.id))
   have mem_range := mem_range_self φ (1 : ℚ)
-  rw [h] at mem_range 
+  rw [h] at mem_range
   obtain ⟨a, ha⟩ := mem_range
   rw [← ModuleCat.id_apply (φ (1 : ℚ)), ← biprod.total, ← LinearMap.comp_apply, ← comp_def,
-    preadditive.comp_add] at ha 
+    Preadditive.comp_add] at ha
   let π₁ := (biprod.fst : of ℤ ℚ ⊞ of ℤ ℚ ⟶ _)
   have ha₁ := congr_arg π₁ ha
-  simp only [← LinearMap.comp_apply, ← comp_def] at ha₁ 
-  simp only [biprod.lift_fst, of_hom_apply, id_coe, id.def, preadditive.add_comp, category.assoc,
-    biprod.inl_fst, category.comp_id, biprod.inr_fst, limits.comp_zero, add_zero] at ha₁ 
+  simp only [← LinearMap.comp_apply, ← comp_def] at ha₁
+  simp only [biprod.lift_fst, ofHom_apply, id_coe, id.def, Preadditive.add_comp, Category.assoc,
+    biprod.inl_fst, Category.comp_id, biprod.inr_fst, Limits.comp_zero, add_zero] at ha₁
   let π₂ := (biprod.snd : of ℤ ℚ ⊞ of ℤ ℚ ⟶ _)
   have ha₂ := congr_arg π₂ ha
-  simp only [← LinearMap.comp_apply, ← comp_def] at ha₂ 
+  simp only [← LinearMap.comp_apply, ← comp_def] at ha₂
   have : (2 : ℚ →ₗ[ℤ] ℚ) 1 = 1 + 1 := rfl
-  simp only [ha₁, this, biprod.lift_snd, of_hom_apply, id_coe, id.def, preadditive.add_comp,
-    category.assoc, biprod.inl_snd, limits.comp_zero, biprod.inr_snd, category.comp_id, zero_add,
-    mul_apply, self_eq_add_left] at ha₂ 
+  simp only [ha₁, this, biprod.lift_snd, ofHom_apply, id_coe, id.def, Preadditive.add_comp,
+    Category.assoc, biprod.inl_snd, Limits.comp_zero, biprod.inr_snd, Category.comp_id, zero_add,
+    mul_apply, self_eq_add_left] at ha₂
   exact one_ne_zero' ℚ ha₂
 #align counterexample.x_not_pseudo_eq Counterexample.x_not_pseudo_eq
 
-attribute [local instance] pseudoelement.setoid
+attribute [local instance] Pseudoelement.setoid
 
 open scoped Pseudoelement
 
 /-- `biprod.fst ⟦x⟧ = biprod.fst ⟦y⟧`. -/
 theorem fst_mk'_x_eq_fst_mk'_y :
-    (biprod.fst : of ℤ ℚ ⊞ of ℤ ℚ ⟶ _) ⟦x⟧ = (biprod.fst : of ℤ ℚ ⊞ of ℤ ℚ ⟶ _) ⟦y⟧ := by
-  simpa only [abelian.pseudoelement.pseudo_apply_mk, Quotient.eq'] using fst_x_pseudo_eq_fst_y
+    (biprod.fst : of ℤ ℚ ⊞ of ℤ ℚ ⟶ _) ⟦x⟧ = (biprod.fst : of ℤ ℚ ⊞ of ℤ ℚ ⟶ _) ⟦y⟧ :=
+  Quotient.eq.2 fst_x_pseudo_eq_fst_y
 #align counterexample.fst_mk_x_eq_fst_mk_y Counterexample.fst_mk'_x_eq_fst_mk'_y
 
 /-- `biprod.snd ⟦x⟧ = biprod.snd ⟦y⟧`. -/
 theorem snd_mk'_x_eq_snd_mk'_y :
-    (biprod.snd : of ℤ ℚ ⊞ of ℤ ℚ ⟶ _) ⟦x⟧ = (biprod.snd : of ℤ ℚ ⊞ of ℤ ℚ ⟶ _) ⟦y⟧ := by
-  simpa only [abelian.pseudoelement.pseudo_apply_mk, Quotient.eq'] using snd_x_pseudo_eq_snd_y
+    (biprod.snd : of ℤ ℚ ⊞ of ℤ ℚ ⟶ _) ⟦x⟧ = (biprod.snd : of ℤ ℚ ⊞ of ℤ ℚ ⟶ _) ⟦y⟧ :=
+  Quotient.eq.2 snd_x_pseudo_eq_snd_y
 #align counterexample.snd_mk_x_eq_snd_mk_y Counterexample.snd_mk'_x_eq_snd_mk'_y
 
 /-- `⟦x⟧ ≠ ⟦y⟧`. -/
@@ -126,5 +127,6 @@ theorem exist_ne_and_fst_eq_fst_and_snd_eq_snd :
   ⟨⟦x⟧, ⟦y⟧, mk'_x_ne_mk'_y, fst_mk'_x_eq_fst_mk'_y, snd_mk'_x_eq_snd_mk'_y⟩
 #align counterexample.exist_ne_and_fst_eq_fst_and_snd_eq_snd Counterexample.exist_ne_and_fst_eq_fst_and_snd_eq_snd
 
-end Counterexample
+end
 
+end Counterexample
