191c648a97550d50277bc1d8c2f29ed8b140b343
int-y1 <jason_yuen2007@hotmail.com>
2023-06-17 19:40:59 -0700

    fix

--- Archive/Imo/Imo2008Q2.lean
+++ Archive/Imo/Imo2008Q2.lean
@@ -37,10 +37,11 @@ namespace Imo2008Q2
 theorem subst_abc {x y z : ℝ} (h : x * y * z = 1) :
     ∃ a b c : ℝ, a ≠ 0 ∧ b ≠ 0 ∧ c ≠ 0 ∧ x = a / b ∧ y = b / c ∧ z = c / a := by
   use x, 1, 1 / y
-  obtain ⟨⟨hx, hy⟩, hz⟩ : (x ≠ 0 ∧ y ≠ 0) ∧ z ≠ 0 := by
-    simpa [not_or] using trans_rel_right (· ≠ ·) h one_ne_zero
+  obtain ⟨⟨hx, hy⟩, _⟩ : (x ≠ 0 ∧ y ≠ 0) ∧ z ≠ 0 := by
+    have := h.symm ▸ one_ne_zero
+    simpa [not_or] using this
   have : z * (y * x) = 1 := by rw [← h]; ac_rfl
-  field_simp [*]
+  field_simp [hx, this]
 #align imo2008_q2.subst_abc Imo2008Q2.subst_abc
 
 theorem imo2008_q2a (x y z : ℝ) (h : x * y * z = 1) (hx : x ≠ 1) (hy : y ≠ 1) (hz : z ≠ 1) :
@@ -53,28 +54,20 @@ theorem imo2008_q2a (x y z : ℝ) (h : x * y * z = 1) (hx : x ≠ 1) (hy : y ≠
   have hc_sub_sub : c - (c - m - n) = m + n := by abel
   rw [ge_iff_le, ← sub_nonneg]
   convert sq_nonneg ((c * (m ^ 2 + n ^ 2 + m * n) - m * (m + n) ^ 2) / (m * n * (m + n)))
-  field_simp [*]; ring
+  field_simp [hc_sub_sub]; ring
 #align imo2008_q2.imo2008_q2a Imo2008Q2.imo2008_q2a
 
 def rationalSolutions :=
-  {s : ℚ × ℚ × ℚ |
-    ∃ x y z : ℚ,
-      s = (x, y, z) ∧
-        x ≠ 1 ∧
-          y ≠ 1 ∧
-            z ≠ 1 ∧
-              x * y * z = 1 ∧ x ^ 2 / (x - 1) ^ 2 + y ^ 2 / (y - 1) ^ 2 + z ^ 2 / (z - 1) ^ 2 = 1}
+  {s : ℚ × ℚ × ℚ | ∃ x y z : ℚ, s = (x, y, z) ∧ x ≠ 1 ∧ y ≠ 1 ∧ z ≠ 1 ∧ x * y * z = 1 ∧
+    x ^ 2 / (x - 1) ^ 2 + y ^ 2 / (y - 1) ^ 2 + z ^ 2 / (z - 1) ^ 2 = 1}
 #align imo2008_q2.rational_solutions Imo2008Q2.rationalSolutions
 
 theorem imo2008_q2b : Set.Infinite rationalSolutions := by
-  let W :=
-    {s : ℚ × ℚ × ℚ |
-      ∃ x y z : ℚ,
-        s = (x, y, z) ∧
-          ∃ t : ℚ, t > 0 ∧ x = -(t + 1) / t ^ 2 ∧ y = t / (t + 1) ^ 2 ∧ z = -t * (t + 1)}
-  have hW_sub_S : W ⊆ rational_solutions := by
+  let W := {s : ℚ × ℚ × ℚ | ∃ x y z : ℚ, s = (x, y, z) ∧
+    ∃ t : ℚ, t > 0 ∧ x = -(t + 1) / t ^ 2 ∧ y = t / (t + 1) ^ 2 ∧ z = -t * (t + 1)}
+  have hW_sub_S : W ⊆ rationalSolutions := by
     intro s hs_in_W
-    rw [rational_solutions]
+    rw [rationalSolutions]
     simp only [Set.mem_setOf_eq] at hs_in_W ⊢
     rcases hs_in_W with ⟨x, y, z, h₁, t, ht_gt_zero, hx_t, hy_t, hz_t⟩
     use x, y, z
@@ -87,15 +80,15 @@ theorem imo2008_q2b : Set.Infinite rationalSolutions := by
     have h₄ : z ≠ 1 := by rw [hz_t]; linarith [key_gt_zero]
     have h₅ : x * y * z = 1 := by rw [hx_t, hy_t, hz_t]; field_simp; ring
     have h₆ : x ^ 2 / (x - 1) ^ 2 + y ^ 2 / (y - 1) ^ 2 + z ^ 2 / (z - 1) ^ 2 = 1 := by
-      have hx1 : (x - 1) ^ 2 = (t ^ 2 + t + 1) ^ 2 / t ^ 4 := by field_simp; rw [hx_t]; field_simp;
-        ring
-      have hy1 : (y - 1) ^ 2 = (t ^ 2 + t + 1) ^ 2 / (t + 1) ^ 4 := by field_simp; rw [hy_t];
-        field_simp; ring
+      have hx1 : (x - 1) ^ 2 = (t ^ 2 + t + 1) ^ 2 / t ^ 4 := by
+        field_simp; rw [hx_t]; field_simp; ring
+      have hy1 : (y - 1) ^ 2 = (t ^ 2 + t + 1) ^ 2 / (t + 1) ^ 4 := by
+        field_simp; rw [hy_t]; field_simp; ring
       have hz1 : (z - 1) ^ 2 = (t ^ 2 + t + 1) ^ 2 := by rw [hz_t]; ring
       calc
         x ^ 2 / (x - 1) ^ 2 + y ^ 2 / (y - 1) ^ 2 + z ^ 2 / (z - 1) ^ 2 =
-            (x ^ 2 * t ^ 4 + y ^ 2 * (t + 1) ^ 4 + z ^ 2) / (t ^ 2 + t + 1) ^ 2 :=
-          by rw [hx1, hy1, hz1]; field_simp
+            (x ^ 2 * t ^ 4 + y ^ 2 * (t + 1) ^ 4 + z ^ 2) / (t ^ 2 + t + 1) ^ 2 := by
+          rw [hx1, hy1, hz1]; field_simp
         _ = 1 := by rw [hx_t, hy_t, hz_t]; field_simp; ring
     exact ⟨h₁, h₂, h₃, h₄, h₅, h₆⟩
   have hW_inf : Set.Infinite W := by
@@ -116,9 +109,9 @@ theorem imo2008_q2b : Set.Infinite rationalSolutions := by
         · use x, y, z; constructor
           rfl
           · use t; constructor
-            · simp only [gt_iff_lt, lt_max_iff]; right; exact zero_lt_one
+            · simp only [gt_iff_lt, lt_max_iff]; right; trivial
             exact ⟨rfl, rfl, rfl⟩
-        · have hg : g (x, y, z) = -z := rfl
+        · have hg : -z = g (x, y, z) := rfl
           rw [hg, hz_def]; ring
       have h₂ : q < t * (t + 1) := by
         calc
@@ -133,4 +126,3 @@ theorem imo2008_q2b : Set.Infinite rationalSolutions := by
 #align imo2008_q2.imo2008_q2b Imo2008Q2.imo2008_q2b
 
 end Imo2008Q2
-
