f3d1d70ff291a7f4da9ce20049f498b8112cd72d
Yury G. Kudryashov <urkud@urkud.name>
2023-06-18 22:29:27 -0500

    Update `Mathlib.lean`

--- Mathlib.lean
+++ Mathlib.lean
@@ -675,6 +675,7 @@ import Mathlib.Analysis.NormedSpace.Pointwise
 import Mathlib.Analysis.NormedSpace.QuaternionExponential
 import Mathlib.Analysis.NormedSpace.Ray
 import Mathlib.Analysis.NormedSpace.RieszLemma
+import Mathlib.Analysis.NormedSpace.ScaleToNorm
 import Mathlib.Analysis.NormedSpace.Spectrum
 import Mathlib.Analysis.NormedSpace.Star.Basic
 import Mathlib.Analysis.NormedSpace.Star.ContinuousFunctionalCalculus
