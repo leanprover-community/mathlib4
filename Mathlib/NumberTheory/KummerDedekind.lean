5918bc1c201d7c69a069939bea84bc5467a3d6d4
Xavier-François Roblot <46200072+xroblot@users.noreply.github.com>
2023-06-19 09:55:00 +0200

    Fix notation

--- Mathlib/NumberTheory/KummerDedekind.lean
+++ Mathlib/NumberTheory/KummerDedekind.lean
@@ -60,7 +60,7 @@ variable (R : Type _) {S : Type _} [CommRing R] [CommRing S] [Algebra R S]
 
 open Ideal Polynomial DoubleQuot UniqueFactorizationMonoid Algebra RingHom
 
-local notation:max R "<" x ">" => adjoin R ({x} : Set S)
+local notation:max R "<" x:max ">" => adjoin R ({x} : Set S)
 
 /-- Let `S / R` be a ring extension and `x : S`, then the conductor of `R<x>` is the
     biggest ideal of `S` contained in `R<x>`. -/
@@ -100,9 +100,9 @@ variable {I : Ideal R}
 theorem prod_mem_ideal_map_of_mem_conductor {p : R} {z : S}
     (hp : p ∈ Ideal.comap (algebraMap R S) (conductor R x)) (hz' : z ∈ I.map (algebraMap R S)) :
     algebraMap R S p * z ∈ algebraMap R<x> S '' ↑(I.map (algebraMap R R<x>)) := by
-  rw [Ideal.map, Ideal.span, Finsupp.mem_span_image_iff_total] at hz' 
+  rw [Ideal.map, Ideal.span, Finsupp.mem_span_image_iff_total] at hz'
   obtain ⟨l, H, H'⟩ := hz'
-  rw [Finsupp.total_apply] at H' 
+  rw [Finsupp.total_apply] at H'
   rw [← H', mul_comm, Finsupp.sum_mul]
   have lem :
     ∀ {a : R},
@@ -189,7 +189,7 @@ noncomputable def quotAdjoinEquivQuotMap (hx : (conductor R x).comap (algebraMap
         --this is contained in `I * R<x>`, which is the content of the previous lemma.
         refine' RingHom.lift_injective_of_ker_le_ideal _ _ fun u hu => _
         rwa [RingHom.mem_ker, RingHom.comp_apply, Ideal.Quotient.eq_zero_iff_mem, ← Ideal.mem_comap,
-          comap_map_eq_map_adjoin_of_coprime_conductor hx h_alg] at hu 
+          comap_map_eq_map_adjoin_of_coprime_conductor hx h_alg] at hu
       · -- Surjectivity follows from the surjectivity of the canonical map `R<x> → S ⧸ (I * S)`,
         -- which in turn follows from the fact that `I * S + (conductor R x) = S`.
         refine' Ideal.Quotient.lift_surjective_of_surjective _ _ fun y => _
@@ -198,14 +198,14 @@ noncomputable def quotAdjoinEquivQuotMap (hx : (conductor R x).comap (algebraMap
           suffices conductor R x ⊔ I.map (algebraMap R S) = ⊤ by simp only [this]
           rw [Ideal.eq_top_iff_one] at hx ⊢
           replace hx := Ideal.mem_map_of_mem (algebraMap R S) hx
-          rw [Ideal.map_sup, RingHom.map_one] at hx 
+          rw [Ideal.map_sup, RingHom.map_one] at hx
           exact
             (sup_le_sup
                 (show ((conductor R x).comap (algebraMap R S)).map (algebraMap R S) ≤ conductor R x
                   from Ideal.map_comap_le)
                 (le_refl (I.map (algebraMap R S))))
               hx
-        rw [← Ideal.mem_quotient_iff_mem_sup, hz, Ideal.mem_map_iff_of_surjective] at this 
+        rw [← Ideal.mem_quotient_iff_mem_sup, hz, Ideal.mem_map_iff_of_surjective] at this
         obtain ⟨u, hu, hu'⟩ := this
         use ⟨u, conductor_subset_adjoin hu⟩
         simpa only [← hu']
@@ -302,7 +302,7 @@ theorem normalizedFactors_ideal_map_eq_normalizedFactors_min_poly_mk_map (hI : I
   have := multiplicity_factors_map_eq_multiplicity hI hI' hx hx' hJ
   rw [multiplicity_eq_count_normalized_factors, multiplicity_eq_count_normalized_factors,
     UniqueFactorizationMonoid.normalize_normalized_factor _ hJ,
-    UniqueFactorizationMonoid.normalize_normalized_factor, PartENat.natCast_inj] at this 
+    UniqueFactorizationMonoid.normalize_normalized_factor, PartENat.natCast_inj] at this
   refine' this.trans _
   -- Get rid of the `map` by applying the equiv to both sides.
   generalize hJ' :
@@ -346,7 +346,7 @@ theorem Ideal.irreducible_map_of_irreducible_minpoly (hI : IsMaximal I) (hI' : I
         (show I.map (algebraMap R S) ≠ 0 by
           rwa [← bot_eq_zero, Ne.def,
             map_eq_bot_iff_of_injective (NoZeroSMulDivisors.algebraMap_injective R S)])
-    rw [associated_iff_eq, hy, Multiset.prod_singleton] at h 
+    rw [associated_iff_eq, hy, Multiset.prod_singleton] at h
     rw [← h]
     exact
       irreducible_of_normalized_factor y
@@ -365,4 +365,3 @@ theorem Ideal.irreducible_map_of_irreducible_minpoly (hI : IsMaximal I) (hI' : I
 #align kummer_dedekind.ideal.irreducible_map_of_irreducible_minpoly KummerDedekind.Ideal.irreducible_map_of_irreducible_minpoly
 
 end KummerDedekind
-
