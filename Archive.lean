3eb0bc28fcc970199f8a77be3a7c69575447b44b
Yury G. Kudryashov <urkud@urkud.name>
2023-06-19 07:36:22 +0000

    feat: port Archive.Imo.Imo1987Q1 (#5091)
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- Archive.lean
+++ Archive.lean
@@ -9,6 +9,7 @@ import Archive.Imo.Imo1972Q5
 import Archive.Imo.Imo1975Q1
 import Archive.Imo.Imo1977Q6
 import Archive.Imo.Imo1981Q3
+import Archive.Imo.Imo1987Q1
 import Archive.Imo.Imo1994Q1
 import Archive.Imo.Imo2001Q2
 import Archive.Imo.Imo2001Q6
