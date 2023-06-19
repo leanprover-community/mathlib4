2a3341cff5dc29557c89a3ebe11ed628d9d88c6d
Joël Riou <joel.riou@universite-paris-saclay.fr>
2023-06-19 13:22:53 +0200

    some progress

--- Mathlib/CategoryTheory/Abelian/YonedaExt.lean
+++ Mathlib/CategoryTheory/Abelian/YonedaExt.lean
@@ -204,15 +204,89 @@ lemma isZero_homology_ZToX₁ (d : ℤ) (hd : d ≠ 0) :
     . simp [E.Z_d_eq (d-1) d (d+n) (d+n+1) (by linarith) (by linarith) (by linarith) (by linarith)]
     . simp [E.Z_d_eq d (d+1) (d+n+1) (d+n+2) (by linarith) (by linarith) (by linarith) hd']
 
+@[simps]
+noncomputable def shortComplex : ShortComplex C where
+  X₁ := E.Z.X (-1)
+  X₂ := E.Z.X 0
+  X₃ := X₁
+  f := E.Z.d (-1) 0
+  g := (Z.XIso E 0 (n+1) (by linarith) (by linarith)).hom ≫ E.K.d (n+1) (n+2) ≫ E.iso₁.hom
+  zero := by
+    simp [E.Z_d_eq (-1) 0 n (n+1) (by linarith) (by linarith) (by linarith) (by linarith)]
+
+lemma shortComplex_exact : E.shortComplex.Exact := by
+  rw [ShortComplex.exact_iff_isZero_homology]
+  refine' IsZero.of_iso (E.hK (n+1)) (ShortComplex.homologyMapIso
+    (_ ≪≫ (HomologicalComplex.natIsoSc' C (ComplexShape.up ℤ) n (n+1) (n+2)
+      (by simp) (by simp ; linarith)).symm.app _))
+  refine' ShortComplex.isoMk (by exact Z.XIso E (-1) n (by linarith) (by linarith))
+    (Z.XIso E 0 (n+1) (by linarith) (by linarith)) E.iso₁.symm _ _
+  . simp [E.Z_d_eq (-1) 0 n (n+1) (by linarith) (by linarith) (by linarith) (by linarith)]
+  . simp
+
+attribute [local instance] epi_comp
+
+instance : Epi E.shortComplex.g := by
+  suffices Epi (E.K.d (n+1) (n+2)) by
+    dsimp
+    infer_instance
+  have h : (E.K.sc' (n+1) (n+2) (n+3)).Exact := by
+    refine' ShortComplex.exact_of_iso
+      ((HomologicalComplex.natIsoSc' C (ComplexShape.up ℤ) (n+1) (n+2) (n+3)
+        (by simp only [CochainComplex.prev] ; linarith)
+        (by simp only [CochainComplex.next] ; linarith)).app E.K) _
+    rw [ShortComplex.exact_iff_isZero_homology]
+    apply E.hK
+  rw [ShortComplex.exact_iff_epi _
+    (IsZero.eq_of_tgt (E.K.isZero_of_isStrictlyLE  (n+2) _ (by linarith)) _ _)] at h
+  exact h
+
+@[simps!]
+noncomputable def leftHomologyData : (E.Z.sc' (-1) 0 1).LeftHomologyData :=
+  ShortComplex.LeftHomologyData.ofIsColimitCokernelCofork (E.Z.sc' (-1) 0 1)
+    (IsZero.eq_of_tgt (E.isZero_Z_X_of_ge 1 (by rfl)) _ _) _ E.shortComplex_exact.gIsCokernel
+
+@[simp]
+lemma leftHomologyData_f' : E.leftHomologyData.f' = E.Z.d (-1) 0 := by
+  rw [← cancel_mono (E.leftHomologyData.i), ShortComplex.LeftHomologyData.f'_i]
+  dsimp
+  rw [comp_id]
+
+@[simps]
+noncomputable def leftHomologyMapData : ShortComplex.LeftHomologyMapData
+    ((HomologicalComplex.shortComplexFunctor' _ _ (-1) 0 1).map E.ZToX₁) E.leftHomologyData
+    (ShortComplex.LeftHomologyData.ofZeros _ (by simp) (by simp)) where
+  φK := (Z.XIso E 0 (n+1) (by linarith) (by linarith)).hom ≫ E.K.d (n+1) (n+2) ≫ E.iso₁.hom ≫
+      (HomologicalComplex.singleObjXSelf C (ComplexShape.up ℤ) 0 X₁).hom
+  φH := (HomologicalComplex.singleObjXSelf C (ComplexShape.up ℤ) 0 X₁).hom
+  commf' := by
+    dsimp
+    simp only [leftHomologyData_f', ShortComplex.LeftHomologyData.ofZeros_f', comp_zero,
+      E.Z_d_eq (-1) 0 n (n+1) (by linarith) (by linarith) (by linarith) (by linarith), assoc,
+      Iso.inv_hom_id_assoc, E.K.d_comp_d_assoc, zero_comp]
+  commi := by
+    dsimp [ZToX₁, HomologicalComplex.toSingleEquiv]
+    simp only [comp_id, id_comp, ite_true, Iso.cancel_iso_hom_left]
+    erw [comp_id]
+
 lemma ZToX₁_quasi_iso (n : ℤ) :
     IsIso ((HomologicalComplex.newHomologyFunctor _ _ n).map E.ZToX₁) := by
-  by_cases n ≠ 0
+  by_cases n = 0
+  . subst h
+    suffices IsIso (ShortComplex.homologyMap
+        ((HomologicalComplex.shortComplexFunctor' _ _ (-1) 0 1).map E.ZToX₁)) from
+      (NatIso.isIso_map_iff (isoWhiskerRight
+        (HomologicalComplex.natIsoSc' C (ComplexShape.up ℤ) (-1) 0 1 (by simp) (by simp))
+        (ShortComplex.homologyFunctor _ )) E.ZToX₁).2 this
+    rw [E.leftHomologyMapData.homologyMap_eq]
+    dsimp
+    erw [id_comp]
+    infer_instance
   . refine' ⟨0, IsZero.eq_of_src (E.isZero_homology_ZToX₁ n h) _ _,
       IsZero.eq_of_src (ShortComplex.isZero_homology_of_isZero_X₂ _ _) _ _⟩
     dsimp
     rw [if_neg h]
     exact isZero_zero _
-  . sorry
 
 end IteratedExtCategory
 
