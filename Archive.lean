b90ede3a946db2dc7ea653edae48a4588cbf396b
Jason Yuen <jason_yuen2007@hotmail.com>
2023-06-19 08:58:32 +0000

    feat: port Archive.Wiedijk100Theorems.CubingACube (#5201)
    Co-authored-by: Jeremy Tan Jie Rui <reddeloostw@gmail.com>

--- Archive.lean
+++ Archive.lean
@@ -27,6 +27,7 @@ import Archive.Imo.Imo2020Q2
 import Archive.Imo.Imo2021Q1
 import Archive.Wiedijk100Theorems.AreaOfACircle
 import Archive.Wiedijk100Theorems.AscendingDescendingSequences
+import Archive.Wiedijk100Theorems.CubingACube
 import Archive.Wiedijk100Theorems.HeronsFormula
 import Archive.Wiedijk100Theorems.InverseTriangleSum
 import Archive.Wiedijk100Theorems.Konigsberg
