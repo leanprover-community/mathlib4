c10de4a09625ad98d319e82a55fe1e196cbb097a
Moritz Doll <moritz.doll@googlemail.com>
2023-06-19 04:18:46 +0000

    feat: port Analysis.SchwartzSpace (#4762)
    Co-authored-by: Moritz Firsching <firsching@google.com>
    Co-authored-by: int-y1 <jason_yuen2007@hotmail.com>
    Co-authored-by: Xavier-François Roblot <46200072+xroblot@users.noreply.github.com>
    Co-authored-by: Xavier Roblot <46200072+xroblot@users.noreply.github.com>
    Co-authored-by: Yury G. Kudryashov <urkud@urkud.name>
    Co-authored-by: Komyyy <pol_tta@outlook.jp>
    Co-authored-by: Jeremy Tan Jie Rui <reddeloostw@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -691,6 +691,7 @@ import Mathlib.Analysis.ODE.Gronwall
 import Mathlib.Analysis.ODE.PicardLindelof
 import Mathlib.Analysis.PSeries
 import Mathlib.Analysis.Quaternion
+import Mathlib.Analysis.SchwartzSpace
 import Mathlib.Analysis.Seminorm
 import Mathlib.Analysis.SpecialFunctions.Arsinh
 import Mathlib.Analysis.SpecialFunctions.Bernstein
