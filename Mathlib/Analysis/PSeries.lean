f3d37b73dc2de0011419448ffb91d5b14ebe1cb3
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2023-06-20 21:15:10 +0200

    fixup! Fix PSeries

--- Mathlib/Analysis/PSeries.lean
+++ Mathlib/Analysis/PSeries.lean
@@ -219,7 +219,8 @@ theorem Real.summable_nat_pow_inv {p : ℕ} :
 
 /-- Test for convergence of the `p`-series: the real-valued series `∑' n : ℕ, 1 / n ^ p` converges
 if and only if `1 < p`. -/
-theorem Real.summable_one_div_nat_pow {p : ℕ} : Summable (fun n => 1 / (n : ℝ) ^ p : ℕ → ℝ) ↔ 1 < p := by
+theorem Real.summable_one_div_nat_pow {p : ℕ} :
+    Summable (fun n => 1 / (n : ℝ) ^ p : ℕ → ℝ) ↔ 1 < p := by
   -- Porting note: was `simp`
   simp only [one_div, Real.summable_nat_pow_inv]
 #align real.summable_one_div_nat_pow Real.summable_one_div_nat_pow
