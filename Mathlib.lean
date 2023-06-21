1e2e427ce735599518654001565269896aef3da4
Pol'tta / Miyahara Kō <pol_tta@outlook.jp>
2023-06-21 05:19:20 +0000

    feat: port Control.ULiftable (#5223)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1203,6 +1203,7 @@ import Mathlib.Control.Traversable.Equiv
 import Mathlib.Control.Traversable.Instances
 import Mathlib.Control.Traversable.Lemmas
 import Mathlib.Control.ULift
+import Mathlib.Control.ULiftable
 import Mathlib.Data.Analysis.Filter
 import Mathlib.Data.Analysis.Topology
 import Mathlib.Data.Array.Basic
