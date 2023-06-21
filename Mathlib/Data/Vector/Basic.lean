edda110968eb7eeb8c7f9a0beedd15d373115a91
Alex Keizer <alex@keizer.dev>
2023-06-21 13:05:43 +0200

    Fix simp normal forms of simp lemmas

--- Mathlib/Data/Vector/Basic.lean
+++ Mathlib/Data/Vector/Basic.lean
@@ -166,7 +166,7 @@ def _root_.Equiv.vectorEquivFin (α : Type _) (n : ℕ) : Vector α n ≃ (Fin n
 #align equiv.vector_equiv_fin Equiv.vectorEquivFin
 
 theorem get_tail (x : Vector α n) (i : Fin (n-1)) :
-    x.tail[i.1] = x[i.succ.1]'(lt_tsub_iff_right.mp i.2)  := by
+    x.tail[i.1] = x[i.1+1]'(lt_tsub_iff_right.mp i.2)  := by
   cases' i with i ih ; dsimp
   rcases x with ⟨_ | _, h⟩ <;> try rfl
   rw [List.length] at h
@@ -176,9 +176,10 @@ theorem get_tail (x : Vector α n) (i : Fin (n-1)) :
 
 @[simp]
 theorem get_tail_succ_nat : ∀ (v : Vector α n.succ) (i : Nat) {h},
-    (tail v)[i]'h = v[i.succ]'(Nat.lt_succ.mpr h)
+    (tail v)[i]'h = v[i+1]'(Nat.lt_succ.mpr h)
   | ⟨a :: l, e⟩, i => by simp [get_eq_get] ; rfl
 
+@[simp]
 theorem get_tail_succ : ∀ (v : Vector α n.succ) (i : Fin n), (tail v)[i.1] = v[i.succ.1] :=
   fun _ _ => get_tail_succ_nat ..
 #align vector.nth_tail_succ Vector.get_tail_succ
@@ -205,7 +206,7 @@ theorem tail_ofFn {n : ℕ} (f : Fin n.succ → α) : tail (ofFn f) = ofFn fun i
   (ofFn_get _).symm.trans <| by
     congr
     funext i
-    rw [get, get_tail, get_ofFn]
+    rw[get, get_tail_succ, get_ofFn]
 #align vector.tail_of_fn Vector.tail_ofFn
 
 @[simp]
@@ -268,6 +269,7 @@ theorem reverse_reverse {v : Vector α n} : v.reverse.reverse = v := by
 theorem get_zero_nat : ∀ v : Vector α n.succ, v[0] = head v
   | ⟨_ :: _, _⟩ => rfl
 
+@[simp]
 theorem get_zero : ∀ v : Vector α n.succ, v[(0 : Fin n.succ).1] = head v :=
   get_zero_nat
 #align vector.nth_zero Vector.get_zero
@@ -295,7 +297,7 @@ theorem get_cons_nil : ∀ {ix : Fin 1} (x : α), (x ::ᵥ nil)[ix.1] = x
 #align vector.nth_cons_nil Vector.get_cons_nil
 
 @[simp]
-theorem get_cons_succ_nat (a : α) (v : Vector α n) (i : Nat) {h} : (a ::ᵥ v)[i.succ]'h = v[i]'(Nat.lt_succ.mp h) := by
+theorem get_cons_succ_nat (a : α) (v : Vector α n) (i : Nat) {h} : (a ::ᵥ v)[i + 1]'h = v[i]'(Nat.lt_succ.mp h) := by
   rw [← get_tail_succ_nat, tail_cons]; rfl
 
 @[simp]
@@ -396,31 +398,34 @@ theorem scanl_head : (scanl f b v).head = b := by
     simp only [←get_zero, get_eq_get, toList_scanl, toList_cons, List.scanl, List.get]
 #align vector.scanl_head Vector.scanl_head
 
-/-- For an index `i : Fin n`, the nth element of `scanl` of a
-vector `v : Vector α n` at `i.succ`, is equal to the application
-function `f : β → α → β` of the `castSucc i` element of
-`scanl f b v` and `get v i`.
+-- set_option pp.analyze true in
+-- /- For an index `i : Fin n`, the nth element of `scanl` of a
+-- vector `v : Vector α n` at `i.succ`, is equal to the application
+-- function `f : β → α → β` of the `castSucc i` element of
+-- `scanl f b v` and `get v i`.
 
-This lemma is the `get` version of `scanl_cons`.
--/
-@[simp]
-theorem scanl_get (i : Fin n) :
-    (scanl f b v)[i.succ.1] = f ((scanl f b v)[(Fin.castSucc i).1]) v[i.1] := by
-  cases' n with n
-  · exact i.elim0
-  induction' n with n hn generalizing b
-  · have i0 : i = 0 := Fin.eq_zero _
-    simp [scanl_singleton, get_zero, get_eq_get]
-  · rw [← cons_head_tail v, scanl_cons, get_cons_succ _ _ i]
-    refine' Fin.cases _ _ i
-    · simp [get_cons_zero_nat, scanl_head, Fin.castSucc_zero, head_cons]
-      -- Why does the following not work?
-      rw[get_zero_nat (@cons β (Nat.succ (n + 1) - 1 + 1) b (scanl f (f b (head v)) (tail v)))]
-      sorry
-    · intro i'
-      simp only [hn, Fin.castSucc_fin_succ, get_cons_succ]
-      sorry
-#align vector.scanl_nth Vector.scanl_get
+-- This lemma is the `get` version of `scanl_cons`.
+-- -/
+-- @[simp]
+-- theorem scanl_get (i : Fin n) :
+--     (scanl f b v)[i.succ.1] = f ((scanl f b v)[i.1]) v[i.1] := by
+--   cases' n with n
+--   · exact i.elim0
+--   simp
+--   induction' n with n hn generalizing b
+--   · have i0 : i = 0 := Fin.eq_zero _
+--     simp [scanl_singleton, get_zero, get_eq_get]
+--   · rw [← cons_head_tail v, scanl_cons, get_cons_succ _ _ i]
+--     refine' Fin.cases _ _ i
+--     · simp [get_zero_nat (n := n + 1), get_zero_nat (n := n + 2),
+--             scanl_head, head_cons]
+--     · intro i'
+--       simp
+--       rw[hn, get_tail_succ_nat (n:=n) _ i'.1]
+--       simp [get_cons_succ_nat (n := n + 2)]
+--       simp
+--       sorry
+-- #align vector.scanl_nth Vector.scanl_get
 
 end Scan
 
