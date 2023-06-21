b5be62454e66a8fde0960949bbfd86ce522972d2
Genora51 <geno.racklinasher@gmail.com>
2023-06-21 01:06:26 +0000

    feat: port Algebra.Category.GroupCat.Adjunctions (#5292)
    Co-authored-by: Kevin Buzzard <k.buzzard@imperial.ac.uk>

--- Mathlib.lean
+++ Mathlib.lean
@@ -33,6 +33,7 @@ import Mathlib.Algebra.BigOperators.RingEquiv
 import Mathlib.Algebra.Bounds
 import Mathlib.Algebra.Category.BoolRingCat
 import Mathlib.Algebra.Category.GroupCat.Abelian
+import Mathlib.Algebra.Category.GroupCat.Adjunctions
 import Mathlib.Algebra.Category.GroupCat.Basic
 import Mathlib.Algebra.Category.GroupCat.Biproducts
 import Mathlib.Algebra.Category.GroupCat.Colimits
