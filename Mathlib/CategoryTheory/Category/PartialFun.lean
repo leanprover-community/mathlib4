c3926c8b5e48891ac290cf8f3f54ea61a39afa18
Parcly Taxel <reddeloostw@gmail.com>
2023-06-22 00:07:52 +0800

    That ain't working

--- Mathlib/CategoryTheory/Category/PartialFun.lean
+++ Mathlib/CategoryTheory/Category/PartialFun.lean
@@ -56,7 +56,7 @@ def of : Type _ → PartialFun :=
 #align PartialFun.of PartialFun.of
 
 -- porting note: removed this lemma which is useless because of the expansion of coercions
-#noalign PartialFun.coe_of PartialFun.coe_of
+#noalign PartialFun.coe_of
 
 instance : Inhabited PartialFun :=
   ⟨Type _⟩
