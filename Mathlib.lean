7eebe3d2d39a7038372fa1e3bdbbe84781c6f58d
Moritz Firsching <moritz.firsching@gmail.com>
2023-06-21 05:19:19 +0000

    feat: port SetTheory.Ordinal.Notation (#2470)
    Co-authored-by: Arien Malec <arien.malec@gmail.com>
    Co-authored-by: Moritz Firsching <firsching@google.com>
    Co-authored-by: Jon Eugster <eugster.jon@gmail.com>
    Co-authored-by: Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
    Co-authored-by: Apurva Nakade <apurvnakade@gmail.com>
    Co-authored-by: Komyyy <pol_tta@outlook.jp>

--- Mathlib.lean
+++ Mathlib.lean
@@ -2660,6 +2660,7 @@ import Mathlib.SetTheory.Ordinal.CantorNormalForm
 import Mathlib.SetTheory.Ordinal.Exponential
 import Mathlib.SetTheory.Ordinal.FixedPoint
 import Mathlib.SetTheory.Ordinal.NaturalOps
+import Mathlib.SetTheory.Ordinal.Notation
 import Mathlib.SetTheory.Ordinal.Principal
 import Mathlib.SetTheory.Ordinal.Topology
 import Mathlib.SetTheory.ZFC.Basic
