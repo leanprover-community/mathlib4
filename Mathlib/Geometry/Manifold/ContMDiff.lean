5230fca73b62ec52f6371c62a94dbd5d97587a76
Yury G. Kudryashov <urkud@urkud.name>
2023-06-22 18:12:02 -0500

    Merge 2 lines

--- Mathlib/Geometry/Manifold/ContMDiff.lean
+++ Mathlib/Geometry/Manifold/ContMDiff.lean
@@ -137,10 +137,9 @@ theorem contDiffWithinAt_localInvariantProp (n : ℕ∞) :
     have := (mem_groupoid_of_pregroupoid.2 he).2.contDiffWithinAt this
     convert (h.comp' _ (this.of_le le_top)).mono_of_mem _ using 1
     · ext y; simp only [mfld_simps]
-    refine'
-      mem_nhdsWithin.mpr
-        ⟨I.symm ⁻¹' e.target, e.open_target.preimage I.continuous_symm, by
-          simp_rw [mem_preimage, I.left_inv, e.mapsTo hx], _⟩
+    refine' mem_nhdsWithin.mpr
+      ⟨I.symm ⁻¹' e.target, e.open_target.preimage I.continuous_symm, by
+        simp_rw [mem_preimage, I.left_inv, e.mapsTo hx], _⟩
     mfld_set_tac
   congr_of_forall := by
     intro s x f g h hx hf
