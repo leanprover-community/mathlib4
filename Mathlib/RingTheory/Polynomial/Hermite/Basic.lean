ff25b7226848109dffb709cc9c6475c68c1b96ea
Alex J Best <alex.j.best@gmail.com>
2023-06-22 19:02:19 +0000

    and fix

--- Mathlib/RingTheory/Polynomial/Hermite/Basic.lean
+++ Mathlib/RingTheory/Polynomial/Hermite/Basic.lean
@@ -190,9 +190,9 @@ theorem coeff_hermite_explicit :
         (by ring : 2 * (n + 1) + k = 2 * n + 1 + (k + 1)),
         (by ring : 2 * n + (k + 2) = 2 * n + 1 + (k + 1))]
       rw [Nat.choose, Nat.choose_succ_right_eq (2 * n + 1 + (k + 1)) (k + 1), Nat.add_sub_cancel,
-      Int.negSucc_eq]
+        Int.negSucc_eq]
       -- porting note: ring could not solve the goal so the lines 195, 198-200 were added.
-      ring
+      ring_nf
       simp only [sub_eq_add_neg, ← neg_mul, ← right_distrib _ _ ((-(1 : ℤ)) ^ n), ← neg_add]
       norm_cast
       simp only [← add_assoc, add_comm]
