5e0a448978240565df9b7be5a049c450b8e104eb
Xavier-François Roblot <46200072+xroblot@users.noreply.github.com>
2023-06-22 09:37:22 +0200

    Clean up

--- Mathlib/Data/Matrix/Invertible.lean
+++ Mathlib/Data/Matrix/Invertible.lean
@@ -9,17 +9,14 @@ Authors: Eric Wieser
 ! if you have ported upstream changes.
 -/
 import Mathlib.Algebra.Invertible
-import Mathlib.Data.Matrix.Basic
 import Mathlib.LinearAlgebra.Matrix.NonsingularInverse
-
--- Porting note: TODO -- check the imports
 
 /-! # Extra lemmas about invertible matrices
 
-Many of the `invertible` lemmas are about `*`; this restates them to be about `⬝`.
+Many of the `Invertible` lemmas are about `*`; this restates them to be about `⬝`.
 
-For lemmas about the matrix inverse in terms of the determinant and adjugate, see `matrix.has_inv`
-in `linear_algebra/matrix/nonsingular_inverse.lean`.
+For lemmas about the matrix inverse in terms of the determinant and adjugate, see `Matrix.inv`
+in `LinearAlgebra/Matrix/NonSingularInverse.lean`.
 -/
 
 
@@ -32,48 +29,48 @@ variable [Fintype n] [DecidableEq n] [Semiring α]
 namespace Matrix
 
 #print Matrix.invOf_mul_self /-
-/-- A copy of `inv_of_mul_self` using `⬝` not `*`. -/
+/-- A copy of `invOf_mul_self` using `⬝` not `*`. -/
 protected theorem invOf_mul_self (A : Matrix n n α) [Invertible A] : ⅟ A ⬝ A = 1 :=
   invOf_mul_self A
 #align matrix.inv_of_mul_self Matrix.invOf_mul_self
 -/
 
 #print Matrix.mul_invOf_self /-
-/-- A copy of `mul_inv_of_self` using `⬝` not `*`. -/
+/-- A copy of `mul_invOf_self` using `⬝` not `*`. -/
 protected theorem mul_invOf_self (A : Matrix n n α) [Invertible A] : A ⬝ ⅟ A = 1 :=
   mul_invOf_self A
 #align matrix.mul_inv_of_self Matrix.mul_invOf_self
 -/
 
 #print Matrix.invOf_mul_self_assoc /-
-/-- A copy of `inv_of_mul_self_assoc` using `⬝` not `*`. -/
+/-- A copy of `invOf_mul_self_assoc` using `⬝` not `*`. -/
 protected theorem invOf_mul_self_assoc (A : Matrix n n α) (B : Matrix n m α) [Invertible A] :
     ⅟ A ⬝ (A ⬝ B) = B := by rw [← Matrix.mul_assoc, Matrix.invOf_mul_self, Matrix.one_mul]
 #align matrix.inv_of_mul_self_assoc Matrix.invOf_mul_self_assoc
 -/
 
 #print Matrix.mul_invOf_self_assoc /-
-/-- A copy of `mul_inv_of_self_assoc` using `⬝` not `*`. -/
+/-- A copy of `mul_invOf_self_assoc` using `⬝` not `*`. -/
 protected theorem mul_invOf_self_assoc (A : Matrix n n α) (B : Matrix n m α) [Invertible A] :
     A ⬝ (⅟ A ⬝ B) = B := by rw [← Matrix.mul_assoc, Matrix.mul_invOf_self, Matrix.one_mul]
 #align matrix.mul_inv_of_self_assoc Matrix.mul_invOf_self_assoc
 -/
 
 #print Matrix.mul_invOf_mul_self_cancel /-
-/-- A copy of `mul_inv_of_mul_self_cancel` using `⬝` not `*`. -/
+/-- A copy of `mul_invOf_mul_self_cancel` using `⬝` not `*`. -/
 protected theorem mul_invOf_mul_self_cancel (A : Matrix m n α) (B : Matrix n n α) [Invertible B] :
     A ⬝ ⅟ B ⬝ B = A := by rw [Matrix.mul_assoc, Matrix.invOf_mul_self, Matrix.mul_one]
 #align matrix.mul_inv_of_mul_self_cancel Matrix.mul_invOf_mul_self_cancel
 -/
 
 #print Matrix.mul_mul_invOf_self_cancel /-
-/-- A copy of `mul_mul_inv_of_self_cancel` using `⬝` not `*`. -/
+/-- A copy of `mul_mul_invOf_self_cancel` using `⬝` not `*`. -/
 protected theorem mul_mul_invOf_self_cancel (A : Matrix m n α) (B : Matrix n n α) [Invertible B] :
     A ⬝ B ⬝ ⅟ B = A := by rw [Matrix.mul_assoc, Matrix.mul_invOf_self, Matrix.mul_one]
 #align matrix.mul_mul_inv_of_self_cancel Matrix.mul_mul_invOf_self_cancel
 -/
 
-/-- A copy of `invertible_mul` using `⬝` not `*`. -/
+/-- A copy of `invertibleMul` using `⬝` not `*`. -/
 @[reducible]
 protected def invertibleMul (A B : Matrix n n α) [Invertible A] [Invertible B] :
     Invertible (A ⬝ B) :=
@@ -92,14 +89,14 @@ protected theorem invOf_mul {A B : Matrix n n α} [Invertible A] [Invertible B]
   invOf_mul _ _
 #align matrix.inv_of_mul Matrix.invOf_mul
 
-/-- A copy of `invertible_of_invertible_mul` using `⬝` not `*`. -/
+/-- A copy of `invertibleOfInvertibleMul` using `⬝` not `*`. -/
 @[reducible]
 protected def invertibleOfInvertibleMul (a b : Matrix n n α) [Invertible a] [Invertible (a ⬝ b)] :
     Invertible b :=
   { invertibleOfInvertibleMul a b with invOf := ⅟ (a ⬝ b) ⬝ a }
 #align matrix.invertible_of_invertible_mul Matrix.invertibleOfInvertibleMul
 
-/-- A copy of `invertible_of_mul_invertible` using `⬝` not `*`. -/
+/-- A copy of `invertibleOfMulInvertible` using `⬝` not `*`. -/
 @[reducible]
 protected def invertibleOfMulInvertible (a b : Matrix n n α) [Invertible (a ⬝ b)] [Invertible b] :
     Invertible a :=
@@ -108,22 +105,22 @@ protected def invertibleOfMulInvertible (a b : Matrix n n α) [Invertible (a ⬝
 
 end Matrix
 
-/-- A copy of `invertible.mul_left` using `⬝` not `*`. -/
+/-- A copy of `Invertible.mulLeft` using `⬝` not `*`. -/
 @[reducible]
-def Invertible.matrixMulLeft {a : Matrix n n α} (ha : Invertible a) (b : Matrix n n α) :
+def Invertible.matrixMulLeft {a : Matrix n n α} (_ : Invertible a) (b : Matrix n n α) :
     Invertible b ≃ Invertible (a ⬝ b) where
-  toFun hb := Matrix.invertibleMul a b
-  invFun hab := Matrix.invertibleOfInvertibleMul a _
-  left_inv hb := Subsingleton.elim _ _
-  right_inv hab := Subsingleton.elim _ _
+  toFun _ := Matrix.invertibleMul a b
+  invFun _ := Matrix.invertibleOfInvertibleMul a _
+  left_inv _ := Subsingleton.elim _ _
+  right_inv _ := Subsingleton.elim _ _
 #align invertible.matrix_mul_left Invertible.matrixMulLeft
 
-/-- A copy of `invertible.mul_right` using `⬝` not `*`. -/
+/-- A copy of `Invertible.mulRight` using `⬝` not `*`. -/
 @[reducible]
-def Invertible.matrixMulRight (a : Matrix n n α) {b : Matrix n n α} (ha : Invertible b) :
+def Invertible.matrixMulRight (a : Matrix n n α) {b : Matrix n n α} (_ : Invertible b) :
     Invertible a ≃ Invertible (a ⬝ b) where
-  toFun hb := Matrix.invertibleMul a b
-  invFun hab := Matrix.invertibleOfMulInvertible _ b
-  left_inv hb := Subsingleton.elim _ _
-  right_inv hab := Subsingleton.elim _ _
+  toFun _ := Matrix.invertibleMul a b
+  invFun _ := Matrix.invertibleOfMulInvertible _ b
+  left_inv _ := Subsingleton.elim _ _
+  right_inv _ := Subsingleton.elim _ _
 #align invertible.matrix_mul_right Invertible.matrixMulRight
