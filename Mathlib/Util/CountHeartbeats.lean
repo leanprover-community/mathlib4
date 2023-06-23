0e2a0889a40a62b82e04d83355ebcca35b9b54f1
Scott Morrison <scott.morrison@gmail.com>
2023-06-23 10:01:08 +1000

    clean up imports

--- Mathlib/Util/CountHeartbeats.lean
+++ Mathlib/Util/CountHeartbeats.lean
@@ -4,9 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison
 -/
 import Mathlib.Lean.CoreM
-import Mathlib.Data.KVMap
 import Std.Tactic.TryThis
-import Mathlib.Tactic.LibrarySearch
 
 /-!
 Defines a command wrapper that prints the number of heartbeats used in the enclosed command.
