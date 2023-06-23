e12cc517f5084aaae2e1ec9cf1523d24d2bf80ac
Xavier Roblot <46200072+xroblot@users.noreply.github.com>
2023-06-23 09:23:57 +0000

    feat: port LinearAlgebra.FreeModule.Norm (#5399)

--- Mathlib.lean
+++ Mathlib.lean
@@ -2025,6 +2025,7 @@ import Mathlib.LinearAlgebra.FreeModule.Finite.Basic
 import Mathlib.LinearAlgebra.FreeModule.Finite.Matrix
 import Mathlib.LinearAlgebra.FreeModule.Finite.Rank
 import Mathlib.LinearAlgebra.FreeModule.IdealQuotient
+import Mathlib.LinearAlgebra.FreeModule.Norm
 import Mathlib.LinearAlgebra.FreeModule.PID
 import Mathlib.LinearAlgebra.FreeModule.Rank
 import Mathlib.LinearAlgebra.FreeModule.StrongRankCondition
