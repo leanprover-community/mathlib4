dd3517e519402b3ba715eb46ea4b5eca790fdf42
Yury G. Kudryashov <urkud@urkud.name>
2023-06-24 05:23:12 +0000

    feat: port Geometry.Manifold.BumpFunction (#5437)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1812,6 +1812,7 @@ import Mathlib.Geometry.Euclidean.Sphere.Power
 import Mathlib.Geometry.Euclidean.Sphere.Ptolemy
 import Mathlib.Geometry.Euclidean.Sphere.SecondInter
 import Mathlib.Geometry.Euclidean.Triangle
+import Mathlib.Geometry.Manifold.BumpFunction
 import Mathlib.Geometry.Manifold.ChartedSpace
 import Mathlib.Geometry.Manifold.ConformalGroupoid
 import Mathlib.Geometry.Manifold.ContMDiff
