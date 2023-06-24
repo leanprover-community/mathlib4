d5d5cd9735520cdc5d7fc52d2bac1ab6ff342882
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2023-06-24 15:03:01 +0000

    feat: port FieldTheory.AbelRuffini (#5424)
    Co-authored-by: Jeremy Tan Jie Rui <reddeloostw@gmail.com>
    Co-authored-by: Riccardo Brasca <riccardo.brasca@gmail.com>
    Co-authored-by: Parcly Taxel <reddeloostw@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1762,6 +1762,7 @@ import Mathlib.Dynamics.Flow
 import Mathlib.Dynamics.Minimal
 import Mathlib.Dynamics.OmegaLimit
 import Mathlib.Dynamics.PeriodicPts
+import Mathlib.FieldTheory.AbelRuffini
 import Mathlib.FieldTheory.Adjoin
 import Mathlib.FieldTheory.AxGrothendieck
 import Mathlib.FieldTheory.Cardinality
