ab06071b3afa9e158dba77f112b0d69120086711
Alex Keizer <alex@keizer.dev>
2023-06-24 14:48:42 +0200

    Refactor: inline redundant `variable` commands

--- Mathlib/Data/QPF/Multivariate/Constructions/Quot.lean
+++ Mathlib/Data/QPF/Multivariate/Constructions/Quot.lean
@@ -31,20 +31,16 @@ variable {F : TypeVec.{u} n → Type u}
 
 section repr
 
-variable [q : MvQPF F]
-
-variable {G : TypeVec.{u} n → Type u} [MvFunctor G]
-
-variable {FG_abs : ∀ {α}, F α → G α}
-
-variable {FG_repr : ∀ {α}, G α → F α}
-
 /-- If `F` is a QPF then `G` is a QPF as well. Can be used to
 construct `MvQPF` instances by transporting them across
 surjective functions -/
-def quotientQPF (FG_abs_repr : ∀ {α} (x : G α), FG_abs (FG_repr x) = x)
-    (FG_abs_map : ∀ {α β} (f : α ⟹ β) (x : F α), FG_abs (f <$$> x) = f <$$> FG_abs x) : MvQPF G
-    where
+def quotientQPF (F) [q : MvQPF F]
+                {G : TypeVec.{u} n → Type u} [MvFunctor G]
+                {FG_abs : ∀ {α}, F α → G α}
+                {FG_repr : ∀ {α}, G α → F α}
+                (FG_abs_repr : ∀ {α} (x : G α), FG_abs (FG_repr x) = x)
+                (FG_abs_map : ∀ {α β} (f : α ⟹ β) (x : F α), FG_abs (f <$$> x) = f <$$> FG_abs x) :
+      MvQPF G where
   P := q.P
   abs p := FG_abs (abs p)
   repr x := repr (FG_repr x)
