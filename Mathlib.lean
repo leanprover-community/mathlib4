2027606afed84bc41be14ba276de8316f782a5b1
Jeremy Tan Jie Rui <reddeloostw@gmail.com>
2023-06-26 08:41:43 +0000

    feat: port Combinatorics.SimpleGraph.Regularity.Chunk (#5474)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1169,6 +1169,7 @@ import Mathlib.Combinatorics.SimpleGraph.Metric
 import Mathlib.Combinatorics.SimpleGraph.Partition
 import Mathlib.Combinatorics.SimpleGraph.Prod
 import Mathlib.Combinatorics.SimpleGraph.Regularity.Bound
+import Mathlib.Combinatorics.SimpleGraph.Regularity.Chunk
 import Mathlib.Combinatorics.SimpleGraph.Regularity.Energy
 import Mathlib.Combinatorics.SimpleGraph.Regularity.Equitabilise
 import Mathlib.Combinatorics.SimpleGraph.Regularity.Uniform
