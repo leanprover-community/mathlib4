6f0a97ee1a047eb9b6ef03d5c39ea6ea3a3dd162
Jeremy Tan Jie Rui <reddeloostw@gmail.com>
2023-06-26 09:40:00 +0000

    feat: port Combinatorics.SimpleGraph.Regularity.Increment (#5476)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1172,6 +1172,7 @@ import Mathlib.Combinatorics.SimpleGraph.Regularity.Bound
 import Mathlib.Combinatorics.SimpleGraph.Regularity.Chunk
 import Mathlib.Combinatorics.SimpleGraph.Regularity.Energy
 import Mathlib.Combinatorics.SimpleGraph.Regularity.Equitabilise
+import Mathlib.Combinatorics.SimpleGraph.Regularity.Increment
 import Mathlib.Combinatorics.SimpleGraph.Regularity.Uniform
 import Mathlib.Combinatorics.SimpleGraph.StronglyRegular
 import Mathlib.Combinatorics.SimpleGraph.Subgraph
