0a6b2b6ae70488b7d4c045694514a79ec4a843d8
Parcly Taxel <reddeloostw@gmail.com>
2023-06-26 16:26:08 +0800

    automated fixes
    Mathbin -> Mathlib
    fix certain import statements
    move "by" to end of line
    add import to Mathlib.lean

--- Mathlib/Combinatorics/SimpleGraph/Regularity/Increment.lean
+++ Mathlib/Combinatorics/SimpleGraph/Regularity/Increment.lean
@@ -8,8 +8,8 @@ Authors: Yaël Dillies, Bhavik Mehta
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
-import Mathbin.Combinatorics.SimpleGraph.Regularity.Chunk
-import Mathbin.Combinatorics.SimpleGraph.Regularity.Energy
+import Mathlib.Combinatorics.SimpleGraph.Regularity.Chunk
+import Mathlib.Combinatorics.SimpleGraph.Regularity.Energy
 
 /-!
 # Increment partition for Szemerédi Regularity Lemma
@@ -68,8 +68,7 @@ variable {hP G ε}
 
 /-- The increment partition has a prescribed (very big) size in terms of the original partition. -/
 theorem card_increment (hPα : P.parts.card * 16 ^ P.parts.card ≤ card α) (hPG : ¬P.IsUniform G ε) :
-    (increment hP G ε).parts.card = stepBound P.parts.card :=
-  by
+    (increment hP G ε).parts.card = stepBound P.parts.card := by
   have hPα' : step_bound P.parts.card ≤ card α :=
     (mul_le_mul_left' (pow_le_pow_of_le_left' (by norm_num) _) _).trans hPα
   have hPpos : 0 < step_bound P.parts.card := step_bound_pos (nonempty_of_not_uniform hPG).card_pos
@@ -84,8 +83,7 @@ theorem card_increment (hPα : P.parts.card * 16 ^ P.parts.card ≤ card α) (hP
 #align szemeredi_regularity.card_increment SzemerediRegularity.card_increment
 
 theorem increment_isEquipartition (hP : P.IsEquipartition) (G : SimpleGraph α) (ε : ℝ) :
-    (increment hP G ε).IsEquipartition :=
-  by
+    (increment hP G ε).IsEquipartition := by
   simp_rw [is_equipartition, Set.equitableOn_iff_exists_eq_eq_add_one]
   refine' ⟨m, fun A hA => _⟩
   rw [mem_coe, increment, mem_bind] at hA 
@@ -98,8 +96,7 @@ private theorem distinct_pairs_increment :
     (P.parts.offDiag.attach.biUnion fun UV =>
         (chunk hP G ε (mem_offDiag.1 UV.2).1).parts ×ˢ
           (chunk hP G ε (mem_offDiag.1 UV.2).2.1).parts) ⊆
-      (increment hP G ε).parts.offDiag :=
-  by
+      (increment hP G ε).parts.offDiag := by
   rintro ⟨Ui, Vj⟩
   simp only [increment, mem_off_diag, bind_parts, mem_bUnion, Prod.exists, exists_and_left,
     exists_prop, mem_product, mem_attach, true_and_iff, Subtype.exists, and_imp, mem_off_diag,
@@ -121,8 +118,7 @@ private noncomputable def pair_contrib (G : SimpleGraph α) (ε : ℝ) (hP : P.I
 
 theorem offDiag_pairs_le_increment_energy :
     ∑ x in P.parts.offDiag.attach, pairContrib G ε hP x / (increment hP G ε).parts.card ^ 2 ≤
-      (increment hP G ε).energy G :=
-  by
+      (increment hP G ε).energy G := by
   simp_rw [pair_contrib, ← sum_div]
   refine' div_le_div_of_le_of_nonneg _ (sq_nonneg _)
   rw [← sum_bUnion]
@@ -147,8 +143,7 @@ theorem pairContrib_lower_bound [Nonempty α] (x : { i // i ∈ P.parts.offDiag 
     (hPα : P.parts.card * 16 ^ P.parts.card ≤ card α) (hPε : 100 ≤ 4 ^ P.parts.card * ε ^ 5) :
     (↑(G.edgeDensity x.1.1 x.1.2) ^ 2 - ε ^ 5 / 25 +
         if G.IsUniform ε x.1.1 x.1.2 then 0 else ε ^ 4 / 3) ≤
-      pairContrib G ε hP x / 16 ^ P.parts.card :=
-  by
+      pairContrib G ε hP x / 16 ^ P.parts.card := by
   rw [pair_contrib]
   push_cast
   split_ifs
@@ -162,8 +157,7 @@ theorem uniform_add_nonuniform_eq_offDiag_pairs [Nonempty α] (hε₁ : ε ≤ 1
     (hPG : ¬P.IsUniform G ε) :
     (∑ x in P.parts.offDiag, G.edgeDensity x.1 x.2 ^ 2 + P.parts.card ^ 2 * (ε ^ 5 / 4) : ℝ) /
         P.parts.card ^ 2 ≤
-      ∑ x in P.parts.offDiag.attach, pairContrib G ε hP x / (increment hP G ε).parts.card ^ 2 :=
-  by
+      ∑ x in P.parts.offDiag.attach, pairContrib G ε hP x / (increment hP G ε).parts.card ^ 2 := by
   conv_rhs =>
     rw [← sum_div, card_increment hPα hPG, step_bound, ← Nat.cast_pow, mul_pow, pow_right_comm,
       Nat.cast_mul, mul_comm, ← div_div, show 4 ^ 2 = 16 by norm_num, sum_div]
@@ -216,8 +210,7 @@ theorem uniform_add_nonuniform_eq_offDiag_pairs [Nonempty α] (hε₁ : ε ≤ 1
 theorem energy_increment [Nonempty α] (hP : P.IsEquipartition) (hP₇ : 7 ≤ P.parts.card)
     (hε : 100 < 4 ^ P.parts.card * ε ^ 5) (hPα : P.parts.card * 16 ^ P.parts.card ≤ card α)
     (hPG : ¬P.IsUniform G ε) (hε₁ : ε ≤ 1) :
-    ↑(P.energy G) + ε ^ 5 / 4 ≤ (increment hP G ε).energy G :=
-  by
+    ↑(P.energy G) + ε ^ 5 / 4 ≤ (increment hP G ε).energy G := by
   rw [coe_energy]
   have h := uniform_add_nonuniform_eq_off_diag_pairs hε₁ hP₇ hPα hε.le hPG
   rw [add_div, mul_div_cancel_left] at h 
