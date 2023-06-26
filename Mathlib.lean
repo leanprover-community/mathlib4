e51fc02168d822d5c1a29e0e2770d1545f2d2df6
Yury G. Kudryashov <urkud@urkud.name>
2023-06-26 19:57:07 +0000

    feat: port Geometry.Manifold.Mfderiv (#5454)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1833,6 +1833,7 @@ import Mathlib.Geometry.Manifold.ContMDiffMap
 import Mathlib.Geometry.Manifold.Instances.Real
 import Mathlib.Geometry.Manifold.Instances.UnitsOfNormedAlgebra
 import Mathlib.Geometry.Manifold.LocalInvariantProperties
+import Mathlib.Geometry.Manifold.MFDeriv
 import Mathlib.Geometry.Manifold.Metrizable
 import Mathlib.Geometry.Manifold.Sheaf.Basic
 import Mathlib.Geometry.Manifold.SmoothManifoldWithCorners
