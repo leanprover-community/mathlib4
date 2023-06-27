bbbec1acbd5de93ea4567e45104b7da77dd8b96f
thorimur <68410468+thorimur@users.noreply.github.com>
2023-06-27 16:36:35 +0000

    feat: port RingTheory.WittVector.InitTail (#5520)
    Co-authored-by: Johan Commelin <johan@commelin.net>
    Co-authored-by: Jeremy Tan Jie Rui <reddeloostw@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -2742,6 +2742,7 @@ import Mathlib.RingTheory.Valuation.ValuationSubring
 import Mathlib.RingTheory.WittVector.Basic
 import Mathlib.RingTheory.WittVector.Defs
 import Mathlib.RingTheory.WittVector.Frobenius
+import Mathlib.RingTheory.WittVector.InitTail
 import Mathlib.RingTheory.WittVector.IsPoly
 import Mathlib.RingTheory.WittVector.MulP
 import Mathlib.RingTheory.WittVector.StructurePolynomial
