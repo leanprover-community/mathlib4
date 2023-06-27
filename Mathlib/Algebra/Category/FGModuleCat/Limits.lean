448892d16378f9604385b7beaaa43c6ad275746e
int-y1 <jason_yuen2007@hotmail.com>
2023-06-27 01:03:57 -0700

    fix most

--- Mathlib/Algebra/Category/FGModuleCat/Limits.lean
+++ Mathlib/Algebra/Category/FGModuleCat/Limits.lean
@@ -17,12 +17,12 @@ import Mathlib.CategoryTheory.Limits.Shapes.FiniteLimits
 import Mathlib.CategoryTheory.Limits.Constructions.LimitsOfProductsAndEqualizers
 
 /-!
-# `forget₂ (fgModule K) (Module K)` creates all finite limits.
+# `forget₂ (FGModuleCat K) (ModuleCat K)` creates all finite limits.
 
-And hence `fgModule K` has all finite limits.
+And hence `FGModuleCat K` has all finite limits.
 
 ## Future work
-After generalising `fgModule` to allow the ring and the module to live in different universes,
+After generalising `FGModuleCat` to allow the ring and the module to live in different universes,
 generalize this construction so we can take limits over smaller diagrams,
 as is done for the other algebraic categories.
 
@@ -46,8 +46,8 @@ variable {k : Type v} [Field k]
 
 instance {J : Type} [Fintype J] (Z : J → ModuleCat.{v} k) [∀ j, FiniteDimensional k (Z j)] :
     FiniteDimensional k (∏ fun j => Z j : ModuleCat.{v} k) :=
-  haveI : FiniteDimensional k (ModuleCat.of k (∀ j, Z j)) := by dsimp; infer_instance
-  FiniteDimensional.of_injective (ModuleCat.piIsoPi _).Hom
+  haveI : FiniteDimensional k (ModuleCat.of k (∀ j, Z j)) := by unfold ModuleCat.of; infer_instance
+  FiniteDimensional.of_injective (ModuleCat.piIsoPi _).hom
     ((ModuleCat.mono_iff_injective _).1 (by infer_instance))
 
 /-- Finite limits of finite dimensional vectors spaces are finite dimensional,
@@ -55,28 +55,28 @@ because we can realise them as subobjects of a finite product. -/
 instance (F : J ⥤ FGModuleCat k) :
     FiniteDimensional k (limit (F ⋙ forget₂ (FGModuleCat k) (ModuleCat.{v} k)) : ModuleCat.{v} k) :=
   haveI : ∀ j, FiniteDimensional k ((F ⋙ forget₂ (FGModuleCat k) (ModuleCat.{v} k)).obj j) := by
-    intro j; change FiniteDimensional k (F.obj j).obj; infer_instance
+    intro j; change FiniteDimensional k (F.obj j); infer_instance
   FiniteDimensional.of_injective
-    (limit_subobject_product (F ⋙ forget₂ (FGModuleCat k) (ModuleCat.{v} k)))
-    ((ModuleCat.mono_iff_injective _).1 (by infer_instance))
+    (limitSubobjectProduct (F ⋙ forget₂ (FGModuleCat k) (ModuleCat.{v} k)))
+    ((ModuleCat.mono_iff_injective _).1 inferInstance)
 
-/-- The forgetful functor from `fgModule k` to `Module k` creates all finite limits. -/
+/-- The forgetful functor from `FGModuleCat k` to `ModuleCat k` creates all finite limits. -/
 def forget₂CreatesLimit (F : J ⥤ FGModuleCat k) :
     CreatesLimit F (forget₂ (FGModuleCat k) (ModuleCat.{v} k)) :=
   createsLimitOfFullyFaithfulOfIso
-    ⟨(limit (F ⋙ forget₂ (FGModuleCat k) (ModuleCat.{v} k)) : ModuleCat.{v} k), by infer_instance⟩
+    ⟨(limit (F ⋙ forget₂ (FGModuleCat k) (ModuleCat.{v} k)) : ModuleCat.{v} k), inferInstance⟩
     (Iso.refl _)
+set_option linter.uppercaseLean3 false in
 #align fgModule.forget₂_creates_limit FGModuleCat.forget₂CreatesLimit
 
-instance : CreatesLimitsOfShape J (forget₂ (FGModuleCat k) (ModuleCat.{v} k))
-    where CreatesLimit F := forget₂CreatesLimit F
+instance : CreatesLimitsOfShape J (forget₂ (FGModuleCat k) (ModuleCat.{v} k)) where
+  CreatesLimit {F} := forget₂CreatesLimit F
 
-instance : HasFiniteLimits (FGModuleCat k)
-    where out J _ _ :=
-    has_limits_of_shape_of_has_limits_of_shape_creates_limits_of_shape
-      (forget₂ (FGModuleCat k) (ModuleCat.{v} k))
+instance : HasFiniteLimits (FGModuleCat k) where
+  out _ _ _ := hasLimitsOfShape_of_hasLimitsOfShape_createsLimitsOfShape
+    (forget₂ (FGModuleCat k) (ModuleCat.{v} k))
 
-instance : PreservesFiniteLimits (forget₂ (FGModuleCat k) (ModuleCat.{v} k))
-    where PreservesFiniteLimits J _ _ := inferInstance
+instance : PreservesFiniteLimits (forget₂ (FGModuleCat k) (ModuleCat.{v} k)) where
+  preservesFiniteLimits _ _ _ := inferInstance
 
 end FGModuleCat
