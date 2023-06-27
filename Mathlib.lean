75cb8716bda42ef9523ab7ff2bf2a495bf8c8ed4
maxwell-thum <maxwell.thum2718@gmail.com>
2023-06-27 15:37:12 +0000

    feat: port Logic.Equiv.Array (#1733)
    Co-authored-by: Parcly Taxel <reddeloostw@gmail.com>
    Co-authored-by: thorimur <68410468+thorimur@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -2153,6 +2153,7 @@ import Mathlib.Logic.Embedding.Basic
 import Mathlib.Logic.Embedding.Set
 import Mathlib.Logic.Encodable.Basic
 import Mathlib.Logic.Encodable.Lattice
+import Mathlib.Logic.Equiv.Array
 import Mathlib.Logic.Equiv.Basic
 import Mathlib.Logic.Equiv.Defs
 import Mathlib.Logic.Equiv.Embedding
