095e321ce7f6e64586d811332003dba145e381d7
thorimur <68410468+thorimur@users.noreply.github.com>
2023-06-27 10:35:07 -0400

    `have` `IsPoly` instance for`ghost_calc`

--- Mathlib/RingTheory/WittVector/InitTail.lean
+++ Mathlib/RingTheory/WittVector/InitTail.lean
@@ -89,6 +89,10 @@ instance select_isPoly {P : ℕ → Prop} : IsPoly p fun _ _ x => select P x := 
 #align witt_vector.select_is_poly WittVector.select_isPoly
 
 theorem select_add_select_not : ∀ x : 𝕎 R, select P x + select (fun i => ¬P i) x = x := by
+  -- Porting note: TC search was insufficient to find this instance, even though all required
+  -- instances exist. See zulip: [https://leanprover.zulipchat.com/#narrow/stream/287929-mathlib4/topic/WittVector.20saga/near/370073526]
+  have : IsPoly p fun {R} [CommRing R] x ↦ select P x + select (fun i ↦ ¬P i) x :=
+    IsPoly₂.diag (hf := IsPoly₂.comp)
   ghost_calc x
   intro n
   simp only [RingHom.map_add]
