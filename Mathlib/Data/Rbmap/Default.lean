6d19fc05764525506dc617ff62d10dcedb1a595c
Jeremy Tan Jie Rui <reddeloostw@gmail.com>
2023-06-27 01:24:56 +0000

    feat: "port" `Rbmap` and `Rbtree` files (#5504)
    Cf. [here](https://leanprover.zulipchat.com/#narrow/stream/287929-mathlib4/topic/Mathlib4.20porting.20meeting.20series/near/369848971). Incorporates #5479.
    
    
    
    Co-authored-by: Johan Commelin <johan@commelin.net>

---
+++ Mathlib/Data/Rbmap/Default.lean
@@ -0,0 +1,17 @@
+/-
+Copyright (c) 2017 Microsoft Corporation. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Leonardo de Moura
+
+! This file was ported from Lean 3 source module data.rbmap.default
+! leanprover-community/mathlib commit 70fd9563a21e7b963887c9360bd29b2393e6225a
+! Please do not edit these lines, except to modify the commit id
+! if you have ported upstream changes.
+-/
+
+/-!
+# Porting note: essentially already ported to std4
+
+https://leanprover.zulipchat.com/#narrow/stream/287929-mathlib4/topic/Mathlib4.20porting.20meeting.20series/near/369848971
+
+-/
