3a33b2be95f4e443bd087549f7bf3fe81ecade07
Jeremy Tan Jie Rui <reddeloostw@gmail.com>
2023-06-28 05:20:20 +0000

    feat: port NumberTheory.ModularForms.SlashActions (#5532)

--- Mathlib.lean
+++ Mathlib.lean
@@ -2374,6 +2374,7 @@ import Mathlib.NumberTheory.Liouville.Residual
 import Mathlib.NumberTheory.LucasLehmer
 import Mathlib.NumberTheory.LucasPrimality
 import Mathlib.NumberTheory.ModularForms.CongruenceSubgroups
+import Mathlib.NumberTheory.ModularForms.SlashActions
 import Mathlib.NumberTheory.Multiplicity
 import Mathlib.NumberTheory.NumberField.Basic
 import Mathlib.NumberTheory.NumberField.CanonicalEmbedding
