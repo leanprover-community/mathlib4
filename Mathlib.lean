f90c0b2938f970c904463bec9bd7b04f2cb3b2d8
María Inés de Frutos-Fernández <mariaines.dff@gmail.com>
2023-06-28 18:09:27 +0000

    feat: port ring theory.witt_vector.compare (#5555)

--- Mathlib.lean
+++ Mathlib.lean
@@ -2754,6 +2754,7 @@ import Mathlib.RingTheory.Valuation.RamificationGroup
 import Mathlib.RingTheory.Valuation.ValuationRing
 import Mathlib.RingTheory.Valuation.ValuationSubring
 import Mathlib.RingTheory.WittVector.Basic
+import Mathlib.RingTheory.WittVector.Compare
 import Mathlib.RingTheory.WittVector.Defs
 import Mathlib.RingTheory.WittVector.DiscreteValuationRing
 import Mathlib.RingTheory.WittVector.Domain
