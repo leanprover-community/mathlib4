ad5283005fe578a04a7a84b900b73d03b7ed8231
Scott Morrison <scott@tqft.net>
2023-06-29 19:22:32 +0000

    fix: bug fixes to rw? (#5563)
    As reported on [zulip](https://leanprover.zulipchat.com/#narrow/stream/287929-mathlib4/topic/panic.20and.20error.20with.20rw.3F/near/370495531)
    
    
    
    Co-authored-by: Scott Morrison <scott.morrison@gmail.com>

--- Mathlib/Lean/Meta/DiscrTree.lean
+++ Mathlib/Lean/Meta/DiscrTree.lean
@@ -26,12 +26,28 @@ def insertIfSpecific [BEq α] (d : DiscrTree α s)
 /--
 Find keys which match the expression, or some subexpression.
 
+Note that repeated subexpressions will be visited each time they appear,
+making this operation potentially very expensive.
+It would be good to solve this problem!
+
 Implementation: we reverse the results from `getMatch`,
 so that we return lemmas matching larger subexpressions first,
 and amongst those we return more specific lemmas first.
 -/
 partial def getSubexpressionMatches (d : DiscrTree α s) (e : Expr) : MetaM (Array α) := do
-  e.foldlM (fun a f => do pure <| a ++ (← d.getSubexpressionMatches f)) (← d.getMatch e).reverse
+  match e with
+  | .bvar _ => return #[]
+  | .forallE _ _ _ _ => forallTelescope e (fun args body => do
+      args.foldlM (fun acc arg => do
+          pure <| acc ++ (← d.getSubexpressionMatches (← inferType arg)))
+        (← d.getSubexpressionMatches body).reverse)
+  | .lam _ _ _ _ => lambdaTelescope e (fun args body => do
+      args.foldlM (fun acc arg => do
+          pure <| acc ++ (← d.getSubexpressionMatches (← inferType arg)))
+        (← d.getSubexpressionMatches body).reverse)
+  | _ =>
+    e.foldlM (fun a f => do pure <| a ++ (← d.getSubexpressionMatches f)) (← d.getMatch e).reverse
+
 variable {m : Type → Type} [Monad m]
 
 /-- Apply a monadic function to the array of values at each node in a `DiscrTree`. -/
