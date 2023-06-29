9591c2b7780254d56ce3fbe609481100963c97c2
Jeremy Tan Jie Rui <reddeloostw@gmail.com>
2023-06-29 14:20:05 +0000

    feat: port AlgebraicGeometry.Properties (#5585)

--- Mathlib.lean
+++ Mathlib.lean
@@ -429,6 +429,7 @@ import Mathlib.AlgebraicGeometry.PrimeSpectrum.IsOpenComapC
 import Mathlib.AlgebraicGeometry.PrimeSpectrum.Maximal
 import Mathlib.AlgebraicGeometry.PrimeSpectrum.Noetherian
 import Mathlib.AlgebraicGeometry.ProjectiveSpectrum.Topology
+import Mathlib.AlgebraicGeometry.Properties
 import Mathlib.AlgebraicGeometry.RingedSpace
 import Mathlib.AlgebraicGeometry.Scheme
 import Mathlib.AlgebraicGeometry.SheafedSpace
