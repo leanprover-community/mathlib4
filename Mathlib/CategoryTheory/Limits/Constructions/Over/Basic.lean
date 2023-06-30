cd33223e7641cde6dced2905d1f22e48300a943b
int-y1 <jason_yuen2007@hotmail.com>
2023-06-30 01:01:27 -0700

    fix

--- Mathlib/CategoryTheory/Limits/Constructions/Over/Basic.lean
+++ Mathlib/CategoryTheory/Limits/Constructions/Over/Basic.lean
@@ -17,8 +17,8 @@ import Mathlib.CategoryTheory.Limits.Constructions.Equalizers
 /-!
 # Limits in the over category
 
-Declare instances for limits in the over category: If `C` has finite wide pullbacks, `over B` has
-finite limits, and if `C` has arbitrary wide pullbacks then `over B` has limits.
+Declare instances for limits in the over category: If `C` has finite wide pullbacks, `Over B` has
+finite limits, and if `C` has arbitrary wide pullbacks then `Over B` has limits.
 -/
 
 
@@ -33,37 +33,34 @@ variable {X : C}
 
 namespace CategoryTheory.Over
 
-/-- Make sure we can derive pullbacks in `over B`. -/
+/-- Make sure we can derive pullbacks in `Over B`. -/
 instance {B : C} [HasPullbacks C] : HasPullbacks (Over B) := by
-  letI : has_limits_of_shape (ULiftHom.{v} (ULift.{v} walking_cospan)) C :=
-    has_limits_of_shape_of_equivalence (ULiftHomULiftCategory.equiv.{v} _)
-  letI : category (ULiftHom.{v} (ULift.{v} walking_cospan)) := inferInstance
-  exact has_limits_of_shape_of_equivalence (ULiftHomULiftCategory.equiv.{v, v} _).symm
+  letI : HasLimitsOfShape (ULiftHom.{v} (ULift.{v} WalkingCospan)) C :=
+    hasLimitsOfShape_of_equivalence (ULiftHomULiftCategory.equiv.{v} _)
+  letI : Category (ULiftHom.{v} (ULift.{v} WalkingCospan)) := inferInstance
+  exact hasLimitsOfShape_of_equivalence (ULiftHomULiftCategory.equiv.{v, v} _).symm
 
-/-- Make sure we can derive equalizers in `over B`. -/
+/-- Make sure we can derive equalizers in `Over B`. -/
 instance {B : C} [HasEqualizers C] : HasEqualizers (Over B) := by
-  letI : has_limits_of_shape (ULiftHom.{v} (ULift.{v} walking_parallel_pair)) C :=
-    has_limits_of_shape_of_equivalence (ULiftHomULiftCategory.equiv.{v} _)
-  letI : category (ULiftHom.{v} (ULift.{v} walking_parallel_pair)) := inferInstance
-  exact has_limits_of_shape_of_equivalence (ULiftHomULiftCategory.equiv.{v, v} _).symm
+  letI : HasLimitsOfShape (ULiftHom.{v} (ULift.{v} WalkingParallelPair)) C :=
+    hasLimitsOfShape_of_equivalence (ULiftHomULiftCategory.equiv.{v} _)
+  letI : Category (ULiftHom.{v} (ULift.{v} WalkingParallelPair)) := inferInstance
+  exact hasLimitsOfShape_of_equivalence (ULiftHomULiftCategory.equiv.{v, v} _).symm
 
 instance hasFiniteLimits {B : C} [HasFiniteWidePullbacks C] : HasFiniteLimits (Over B) := by
-  apply @has_finite_limits_of_has_equalizers_and_finite_products _ _ _ _
-  · exact construct_products.over_finite_products_of_finite_wide_pullbacks
-  · apply @has_equalizers_of_has_pullbacks_and_binary_products _ _ _ _
-    · haveI : has_pullbacks C := ⟨by infer_instance⟩
-      exact construct_products.over_binary_product_of_pullback
-    · infer_instance
+  apply @hasFiniteLimits_of_hasEqualizers_and_finite_products _ _ ?_ ?_
+  · exact ConstructProducts.over_finiteProducts_of_finiteWidePullbacks
+  · apply @hasEqualizers_of_hasPullbacks_and_binary_products _ _ ?_ _
+    · haveI : HasPullbacks C := ⟨inferInstance⟩
+      exact ConstructProducts.over_binaryProduct_of_pullback
 #align category_theory.over.has_finite_limits CategoryTheory.Over.hasFiniteLimits
 
-instance has_limits {B : C} [HasWidePullbacks.{w} C] : HasLimitsOfSize.{w} (Over B) := by
-  apply @has_limits_of_has_equalizers_and_products _ _ _ _
-  · exact construct_products.over_products_of_wide_pullbacks
-  · apply @has_equalizers_of_has_pullbacks_and_binary_products _ _ _ _
-    · haveI : has_pullbacks C := ⟨inferInstance⟩
-      exact construct_products.over_binary_product_of_pullback
-    · infer_instance
-#align category_theory.over.has_limits CategoryTheory.Over.has_limits
+instance hasLimits {B : C} [HasWidePullbacks.{w} C] : HasLimitsOfSize.{w} (Over B) := by
+  apply @has_limits_of_hasEqualizers_and_products _ _ ?_ ?_
+  · exact ConstructProducts.over_products_of_widePullbacks
+  · apply @hasEqualizers_of_hasPullbacks_and_binary_products _ _ ?_ _
+    · haveI : HasPullbacks C := ⟨inferInstance⟩
+      exact ConstructProducts.over_binaryProduct_of_pullback
+#align category_theory.over.has_limits CategoryTheory.Over.hasLimits
 
 end CategoryTheory.Over
-
