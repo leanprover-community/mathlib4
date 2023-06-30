3de980d9046cdbac4478f84af7526aadb9d40b97
Floris van Doorn <fpvdoorn@gmail.com>
2023-06-30 15:43:14 +0200

    also fix abs precedence

--- Mathlib/Algebra/Abs.lean
+++ Mathlib/Algebra/Abs.lean
@@ -63,7 +63,7 @@ class NegPart (α : Type _) where
 #align has_neg_part NegPart
 
 @[inherit_doc Abs.abs]
-macro atomic("|" noWs) a:term noWs "|" : term => `(abs $a)
+macro:max atomic("|" noWs) a:term noWs "|" : term => `(abs $a)
 
 /-- Unexpander for the notation `|a|` for `abs a`.
 Tries to add discretionary parentheses in unparseable cases. -/
