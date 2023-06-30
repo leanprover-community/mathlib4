e71da81e8a9278f1dd851419dcd766b30feec966
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2023-06-30 13:52:45 +0000

    feat: port RingTheory.WittVector.Isocrystal (#5579)
    Co-authored-by: Chris Hughes <chrishughes24@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -2776,6 +2776,7 @@ import Mathlib.RingTheory.WittVector.FrobeniusFractionField
 import Mathlib.RingTheory.WittVector.Identities
 import Mathlib.RingTheory.WittVector.InitTail
 import Mathlib.RingTheory.WittVector.IsPoly
+import Mathlib.RingTheory.WittVector.Isocrystal
 import Mathlib.RingTheory.WittVector.MulCoeff
 import Mathlib.RingTheory.WittVector.MulP
 import Mathlib.RingTheory.WittVector.StructurePolynomial
