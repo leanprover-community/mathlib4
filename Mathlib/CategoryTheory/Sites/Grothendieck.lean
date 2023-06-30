4556475f9d1f03b9ae37c0043495a903b6ab75c2
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2023-06-30 14:50:32 +0000

    fix: better universe for Cover (#5625)
    It had type `Type (max 0 u v)` before.

--- Mathlib/CategoryTheory/Sites/Grothendieck.lean
+++ Mathlib/CategoryTheory/Sites/Grothendieck.lean
@@ -403,7 +403,8 @@ def atomic (hro : RightOreCondition C) : GrothendieckTopology C
 
 /-- `J.Cover X` denotes the poset of covers of `X` with respect to the
 Grothendieck topology `J`. -/
-def Cover (X : C) :=
+-- porting note : Lean 3 inferred `Type max u v`, Lean 4 by default gives `Type (max 0 u v)`
+def Cover (X : C) : Type max u v :=
   { S : Sieve X // S ∈ J X } -- deriving Preorder
 #align category_theory.grothendieck_topology.cover CategoryTheory.GrothendieckTopology.Cover
 
