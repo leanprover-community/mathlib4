f66c619e32440ce97ee507866c3dfb4bb56101ef
nick-kuhn <nikolaskuhn@gmx.de>
2023-07-01 12:48:49 +0200

    Small fix

--- Mathlib/CategoryTheory/Sites/SheafOfTypes.lean
+++ Mathlib/CategoryTheory/Sites/SheafOfTypes.lean
@@ -823,7 +823,7 @@ theorem yonedaFamily_fromCocone_compatible (S : Sieve X) (s : Cocone (diagram S.
   have Hs := s.ι.naturality
   simp only [Functor.id_obj, yoneda_obj_obj, Opposite.unop_op, yoneda_obj_map, Quiver.Hom.unop_op]
   dsimp [yonedaFamilyOfElements_fromCocone]
-  have hgf₁ : S.arrows (g₁ ≫ f₁) := by exact Sieve.downward_closed S (by exact?) g₁
+  have hgf₁ : S.arrows (g₁ ≫ f₁) := by exact Sieve.downward_closed S hf₁ g₁
   have hgf₂ : S.arrows (g₂ ≫ f₂) := by exact Sieve.downward_closed S hf₂ g₂
   let F : (Over.mk (g₁ ≫ f₁) : Over X) ⟶ (Over.mk (g₂ ≫ f₂) : Over X) := (Over.homMk (𝟙 Z) )
   let F₁ : (Over.mk (g₁ ≫ f₁) : Over X) ⟶ (Over.mk f₁ : Over X) := (Over.homMk g₁)
