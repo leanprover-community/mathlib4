7b03e41e09ebe63848f091880549f8e1cd27da28
timotree3 <timorcb@gmail.com>
2023-07-01 04:17:37 -0400

    get Short.lean compiling

--- Mathlib/SetTheory/Game/Short.lean
+++ Mathlib/SetTheory/Game/Short.lean
@@ -37,24 +37,50 @@ inductive Short : PGame.{u} → Type (u + 1)
       (_ : ∀ j : β, Short (R j)) [Fintype α] [Fintype β], Short ⟨α, β, L, R⟩
 #align pgame.short PGame.Short
 
-instance subsingleton_short : ∀ x : PGame, Subsingleton (Short x)
+instance subsingleton_short (x : PGame) : Subsingleton (Short x) := by
+  induction x with
+  | mk xl xr xL xR =>
+    constructor
+    intro a b
+    cases a; cases b
+    congr!
+
+-- Porting note: We use `induction` to prove `subsingleton_short` instead of recursion.
+-- A proof using recursion generates a harder `decreasing_by` goal than in Lean 3 for some reason:
+attribute [-instance] subsingleton_short in
+def subsingleton_short_example : ∀ x : PGame, Subsingleton (Short x)
   | mk xl xr xL xR =>
     ⟨fun a b => by
       cases a; cases b
       congr!
       · funext x
-        apply @Subsingleton.elim _ (subsingleton_short (xL x))
+        apply @Subsingleton.elim _ (subsingleton_short_example (xL x))
+        -- Decreasing goal in Lean 4 is `Subsequent (xL x) (mk α β L R)`
+        -- where `α`, `β`, `L`, and `R` are fresh hypotheses only propositionally
+        -- equal to `xl`, `xr`, `xL`, and `xR`.
+        -- (In Lean 3 it was `(mk xl xr xL xR)` instead.)
       · funext x
-        apply @Subsingleton.elim _ (subsingleton_short (xR x))⟩
-termination_by subsingleton_short x => x
-decreasing_by pgame_wf_tac
+        apply @Subsingleton.elim _ (subsingleton_short_example (xR x))⟩
+termination_by subsingleton_short_example x => x
+-- We need to unify a bunch of hypotheses before `pgame_wf_tac` can work.
+decreasing_by {
+  subst_vars
+  simp only [mk.injEq, heq_eq_eq, true_and] at *
+  casesm* _ ∧ _
+  subst_vars
+  pgame_wf_tac
+}
+
 #align pgame.subsingleton_short PGame.subsingleton_short
 
 /-- A synonym for `short.mk` that specifies the pgame in an implicit argument. -/
 def Short.mk' {x : PGame} [Fintype x.LeftMoves] [Fintype x.RightMoves]
     (sL : ∀ i : x.LeftMoves, Short (x.moveLeft i))
     (sR : ∀ j : x.RightMoves, Short (x.moveRight j)) : Short x := by
-  (cases x; dsimp at *) <;> exact Short.mk sL sR
+  -- Porting note: Old proof relied on `unfreezingI`, which doesn't exist in Lean 4.
+  convert Short.mk sL sR
+  cases x
+  dsimp
 #align pgame.short.mk' PGame.Short.mk'
 
 attribute [class] Short
@@ -66,10 +92,11 @@ def fintypeLeft {α β : Type u} {L : α → PGame.{u}} {R : β → PGame.{u}} [
     Fintype α := by cases' S with _ _ _ _ _ _ F _; exact F
 #align pgame.fintype_left PGame.fintypeLeft
 
-attribute [local instance] fintypeLeft
+-- Porting note: In Lean 4, it seems it can't be made an instance at all.
+-- attribute [local instance] fintypeLeft
 
 instance fintypeLeftMoves (x : PGame) [S : Short x] : Fintype x.LeftMoves := by
-  cases x; dsimp; infer_instance
+  cases S; assumption
 #align pgame.fintype_left_moves PGame.fintypeLeftMoves
 
 /-- Extracting the `fintype` instance for the indexing type for Right's moves in a short game.
@@ -79,10 +106,11 @@ def fintypeRight {α β : Type u} {L : α → PGame.{u}} {R : β → PGame.{u}} 
     Fintype β := by cases' S with _ _ _ _ _ _ _ F; exact F
 #align pgame.fintype_right PGame.fintypeRight
 
-attribute [local instance] fintypeRight
+-- Porting note: In Lean 4, it seems it can't be made an instance at all.
+-- attribute [local instance] fintypeRight
 
 instance fintypeRightMoves (x : PGame) [S : Short x] : Fintype x.RightMoves := by
-  cases x; dsimp; infer_instance
+  cases S; assumption
 #align pgame.fintype_right_moves PGame.fintypeRightMoves
 
 instance moveLeftShort (x : PGame) [S : Short x] (i : x.LeftMoves) : Short (x.moveLeft i) := by
@@ -97,7 +125,8 @@ def moveLeftShort' {xl xr} (xL xR) [S : Short (mk xl xr xL xR)] (i : xl) : Short
   cases' S with _ _ _ _ L _ _ _; apply L
 #align pgame.move_left_short' PGame.moveLeftShort'
 
-attribute [local instance] moveLeftShort'
+-- Porting note: In Lean 4, it seems it can't be made an instance at all.
+-- attribute [local instance] moveLeftShort'
 
 instance moveRightShort (x : PGame) [S : Short x] (j : x.RightMoves) : Short (x.moveRight j) := by
   cases' S with _ _ _ _ _ R _ _; apply R
@@ -111,11 +140,14 @@ def moveRightShort' {xl xr} (xL xR) [S : Short (mk xl xr xL xR)] (j : xr) : Shor
   cases' S with _ _ _ _ _ R _ _; apply R
 #align pgame.move_right_short' PGame.moveRightShort'
 
-attribute [local instance] moveRightShort'
+-- Porting note: In Lean 4, it seems it can't be made an instance at all.
+-- attribute [local instance] moveRightShort'
 
-theorem short_birthday : ∀ (x : PGame.{u}) [Short x], x.birthday < Ordinal.omega
-  | ⟨xl, xr, xL, xR⟩, hs => by
-    haveI := hs
+theorem short_birthday (x : PGame.{u}) : [Short x] → x.birthday < Ordinal.omega := by
+  -- Porting note: Again `induction` is used instead of `pgame_wf_tac`
+  induction x with
+  | mk xl xr xL xR ihl ihr =>
+    intro hs
     rcases hs with ⟨sL, sR⟩
     rw [birthday, max_lt_iff]
     constructor;
@@ -125,14 +157,12 @@ theorem short_birthday : ∀ (x : PGame.{u}) [Short x], x.birthday < Ordinal.ome
         Cardinal.lsub_lt_ord_of_isRegular.{u, u} Cardinal.isRegular_aleph0
           (Cardinal.lt_aleph0_of_finite _) fun i => _
       rw [Cardinal.ord_aleph0]
-      apply short_birthday _
-    · exact move_left_short' xL xR i
-    · exact move_right_short' xL xR i
-termination_by short_birthday x _ => x
+    · apply ihl
+    · apply ihr
 #align pgame.short_birthday PGame.short_birthday
 
 /-- This leads to infinite loops if made into an instance. -/
-def Short.ofIsEmpty {l r xL xR} [IsEmpty l] [IsEmpty r] : Short (mk l r xL xR) :=
+def Short.ofIsEmpty {l r xL xR} [IsEmpty l] [IsEmpty r] : Short (PGame.mk l r xL xR) :=
   Short.mk isEmptyElim isEmptyElim
 #align pgame.short.of_is_empty PGame.Short.ofIsEmpty
 
@@ -144,71 +174,104 @@ instance short1 : Short 1 :=
   Short.mk (fun i => by cases i; infer_instance) fun j => by cases j
 #align pgame.short_1 PGame.short1
 
-/- ./././Mathport/Syntax/Translate/Expr.lean:177:8: unsupported: ambiguous notation -/
 /-- Evidence that every `pgame` in a list is `short`. -/
-inductive ListShort : List PGame.{u} → Type (u + 1)
-  | nil : list_short []
-  | cons : ∀ (hd : PGame.{u}) [Short hd] (tl : List PGame.{u}) [list_short tl], list_short (hd::tl)
+class inductive ListShort : List PGame.{u} → Type (u + 1)
+  | nil : ListShort []
+  -- Porting note: We introduce `cons` as a separate instance because attempting to use
+  -- `[ListShort tl]` as a constructor argument errors saying that `ListShort tl` is not a class.
+  -- Is this a bug in `class inductive`?
+  | cons' (hd : PGame.{u}) [Short hd] (tl : List PGame.{u}) : ListShort tl → ListShort (hd::tl)
 #align pgame.list_short PGame.ListShort
 
-attribute [class] list_short
+instance ListShort.cons (hd : PGame.{u}) [Short hd] (tl : List PGame.{u}) [h : ListShort tl] : ListShort (hd::tl) :=
+  cons' hd tl h
+#align pgame.list_short.cons PGame.ListShort.cons
 
-attribute [instance] list_short.nil list_short.cons
+attribute [instance] ListShort.nil
 
-/- ./././Mathport/Syntax/Translate/Expr.lean:177:8: unsupported: ambiguous notation -/
-/- ./././Mathport/Syntax/Translate/Expr.lean:177:8: unsupported: ambiguous notation -/
-instance listShortNthLe :
-    ∀ (L : List PGame.{u}) [ListShort L] (i : Fin (List.length L)), Short (List.nthLe L i i.is_lt)
-  | [], _, n => by exfalso; rcases n with ⟨_, ⟨⟩⟩
-  | hd::tl, @list_short.cons _ S _ _, ⟨0, _⟩ => S
-  | hd::tl, @list_short.cons _ _ _ S, ⟨n + 1, h⟩ =>
-    @list_short_nth_le tl S ⟨n, (add_lt_add_iff_right 1).mp h⟩
+-- Porting note: use `List.get` instead of `List.nthLe` because it has been deprecated
+instance listShortGet :
+    ∀ (L : List PGame.{u}) [ListShort L] (i : Fin (List.length L)), Short (List.get L i)
+  | [], _, n => by
+    exfalso
+    rcases n with ⟨_, ⟨⟩⟩
+    -- Porting note: The proof errors unless `done` or a `;` is added after `rcases`
+    done
+  | _::_, @ListShort.cons' _ S _ _, ⟨0, _⟩ => S
+  | hd::tl, ListShort.cons' _ _ S, ⟨n + 1, h⟩ =>
+    @listShortGet tl S ⟨n, (add_lt_add_iff_right 1).mp h⟩
+
+set_option linter.deprecated false in
+@[deprecated listShortGet]
+instance listShortNthLe (L : List PGame.{u}) [ListShort L] (i : Fin (List.length L)) :
+    Short (List.nthLe L i i.is_lt) := by
+  rw [List.nthLe_eq]
+  apply listShortGet
 #align pgame.list_short_nth_le PGame.listShortNthLe
 
 instance shortOfLists : ∀ (L R : List PGame) [ListShort L] [ListShort R], Short (PGame.ofLists L R)
   | L, R, _, _ => by
-    skip; apply short.mk
+    apply Short.mk
     · intros; infer_instance
-    · intros; apply PGame.listShortNthLe
+    · intros; apply PGame.listShortGet
 #align pgame.short_of_lists PGame.shortOfLists
 
--- where does the subtype.val come from?
 /-- If `x` is a short game, and `y` is a relabelling of `x`, then `y` is also short. -/
-def shortOfRelabelling : ∀ {x y : PGame.{u}} (R : Relabelling x y) (S : Short x), Short y
+def shortOfRelabelling : ∀ {x y : PGame.{u}}, Relabelling x y → Short x → Short y
   | x, y, ⟨L, R, rL, rR⟩, S => by
-    skip
     haveI := Fintype.ofEquiv _ L
     haveI := Fintype.ofEquiv _ R
     exact
-      short.mk'
-        (fun i => by rw [← L.right_inv i]; apply short_of_relabelling (rL (L.symm i)) inferInstance)
-        fun j => by simpa using short_of_relabelling (rR (R.symm j)) inferInstance
+      Short.mk'
+        (fun i => by rw [← L.right_inv i]; apply shortOfRelabelling (rL (L.symm i)) inferInstance)
+        fun j => by simpa using shortOfRelabelling (rR (R.symm j)) inferInstance
 #align pgame.short_of_relabelling PGame.shortOfRelabelling
 
 instance shortNeg : ∀ (x : PGame.{u}) [Short x], Short (-x)
-  | mk xl xr xL xR, _ => by skip; exact short.mk (fun i => short_neg _) fun i => short_neg _
-decreasing_by pgame_wf_tac
+  | mk xl xr xL xR, _ => by
+    -- Porting note: These are needed because the local instances above were rejected by Lean 4
+    haveI := moveLeftShort' xL xR
+    haveI := moveRightShort' xL xR
+    haveI := @fintypeLeft xl xr xL xR _
+    haveI := @fintypeRight xl xr xL xR _
+    exact Short.mk (fun i => shortNeg _) fun i => shortNeg _
+-- Porting note: `decreasing_by pgame_wf_tac` is no longer needed.
 #align pgame.short_neg PGame.shortNeg
 
 instance shortAdd : ∀ (x y : PGame.{u}) [Short x] [Short y], Short (x + y)
   | mk xl xr xL xR, mk yl yr yL yR, _, _ => by
-    skip
-    apply short.mk;
+    -- Porting note: These are needed because the local instances above were rejected by Lean 4
+    haveI := moveLeftShort' xL xR
+    haveI := moveRightShort' xL xR
+    haveI := @fintypeLeft xl xr xL xR _
+    haveI := @fintypeRight xl xr xL xR _
+    haveI := moveLeftShort' yL yR
+    haveI := moveRightShort' yL yR
+    haveI := @fintypeLeft yl yr yL yR _
+    haveI := @fintypeRight yl yr yL yR _
+    apply Short.mk;
     all_goals
       rintro ⟨i⟩
-      · apply short_add
-      · change short (mk xl xr xL xR + _); apply short_add
-decreasing_by pgame_wf_tac
+      · apply shortAdd
+      · change Short (mk xl xr xL xR + _); apply shortAdd
+-- Porting note: In Lean 3 `using_well_founded` didn't need this to be explicit.
+termination_by shortAdd x y _ _ => Prod.mk x y
+-- Porting note: `decreasing_by pgame_wf_tac` is no longer needed.
 #align pgame.short_add PGame.shortAdd
 
 instance shortNat : ∀ n : ℕ, Short n
   | 0 => PGame.short0
-  | n + 1 => @PGame.shortAdd _ _ (short_nat n) PGame.short1
+  | n + 1 => @PGame.shortAdd _ _ (shortNat n) PGame.short1
 #align pgame.short_nat PGame.shortNat
 
+instance shortOfNat (n : ℕ) [Nat.AtLeastTwo n] : Short (OfNat.ofNat n) := shortNat n
+
+-- Porting note: `bit0` and `bit1` are deprecated so these instances can probably be removed.
+set_option linter.deprecated false in
 instance shortBit0 (x : PGame.{u}) [Short x] : Short (bit0 x) := by dsimp [bit0]; infer_instance
 #align pgame.short_bit0 PGame.shortBit0
 
+set_option linter.deprecated false in
 instance shortBit1 (x : PGame.{u}) [Short x] : Short (bit1 x) := by dsimp [bit1]; infer_instance
 #align pgame.short_bit1 PGame.shortBit1
 
@@ -218,25 +281,35 @@ Instances for the two projections separately are provided below.
 -/
 def leLfDecidable : ∀ (x y : PGame.{u}) [Short x] [Short y], Decidable (x ≤ y) × Decidable (x ⧏ y)
   | mk xl xr xL xR, mk yl yr yL yR, shortx, shorty => by
-    skip
+    -- Porting note: These are needed because the local instances above were rejected by Lean 4
+    haveI := moveLeftShort' xL xR
+    haveI := moveRightShort' xL xR
+    haveI := @fintypeLeft xl xr xL xR _
+    haveI := @fintypeRight xl xr xL xR _
+    haveI := moveLeftShort' yL yR
+    haveI := moveRightShort' yL yR
+    haveI := @fintypeLeft yl yr yL yR _
+    haveI := @fintypeRight yl yr yL yR _
     constructor
     · refine' @decidable_of_iff' _ _ mk_le_mk (id _)
-      apply @And.decidable _ _ _ _
-      · apply @Fintype.decidableForallFintype xl _ _ (by infer_instance)
+      apply @And.decidable _ _ ?_ ?_
+      · apply @Fintype.decidableForallFintype xl _ ?_ _
         intro i
-        apply (@le_lf_decidable _ _ _ _).2 <;> infer_instance
-      · apply @Fintype.decidableForallFintype yr _ _ (by infer_instance)
+        apply (leLfDecidable _ _).2
+      · apply @Fintype.decidableForallFintype yr _ ?_ _
         intro i
-        apply (@le_lf_decidable _ _ _ _).2 <;> infer_instance
+        apply (leLfDecidable _ _).2
     · refine' @decidable_of_iff' _ _ mk_lf_mk (id _)
-      apply @Or.decidable _ _ _ _
-      · apply @Fintype.decidableExistsFintype yl _ _ (by infer_instance)
+      apply @Or.decidable _ _ ?_ ?_
+      · apply @Fintype.decidableExistsFintype yl _ ?_ _
         intro i
-        apply (@le_lf_decidable _ _ _ _).1 <;> infer_instance
-      · apply @Fintype.decidableExistsFintype xr _ _ (by infer_instance)
+        apply (leLfDecidable _ _).1
+      · apply @Fintype.decidableExistsFintype xr _ ?_ _
         intro i
-        apply (@le_lf_decidable _ _ _ _).1 <;> infer_instance
-decreasing_by pgame_wf_tac
+        apply (leLfDecidable _ _).1
+-- Porting note: In Lean 3 `using_well_founded` didn't need this to be explicit.
+termination_by leLfDecidable x y _ _ => Prod.mk x y
+-- Porting note: `decreasing_by pgame_wf_tac` is no longer needed.
 #align pgame.le_lf_decidable PGame.leLfDecidable
 
 instance leDecidable (x y : PGame.{u}) [Short x] [Short y] : Decidable (x ≤ y) :=
