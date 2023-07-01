71344bdeca332108dcb5ceed8173cb141716ff29
Yury G. Kudryashov <urkud@urkud.name>
2023-07-01 11:19:42 -0500

    Snapshot

--- Mathlib/Topology/ContinuousFunction/Sigma.lean
+++ Mathlib/Topology/ContinuousFunction/Sigma.lean
@@ -35,11 +35,20 @@ continuous map, sigma type, disjoint union
 
 noncomputable section
 
-variable {X ι : Type _} {Y : ι → Type _} [TopologicalSpace X] [∀ i, TopologicalSpace (Y i)]
-  [ConnectedSpace X]
+open scoped Topology
+open Filter
 
-theorem ContinuousMap.exists_lift_sigma (f : C(X, Σ i, Y i)) :
-    ∃ (i : ι) (g : C(X, Y i)), f = (sigmaMk i).comp g :=
+variable {X ι : Type _} {Y : ι → Type _} [TopologicalSpace X] [∀ i, TopologicalSpace (Y i)]
+
+namespace ContinuousMap
+
+theorem embedding_sigmaMk_comp : Embedding (fun g : Σ i, C(X, Y i) ↦ (sigmaMk g.1).comp g.2) := _
+
+section ConnectedSpace
+
+variable [ConnectedSpace X]
+
+theorem exists_lift_sigma (f : C(X, Σ i, Y i)) : ∃ i g, f = (sigmaMk i).comp g :=
   let ⟨i, g, hg, hfg⟩ := f.continuous.exists_lift_sigma
   ⟨i, ⟨g, hg⟩, FunLike.ext' hfg⟩
 
@@ -62,22 +71,7 @@ def ContinuousMap.sigmaCodEquiv : C(X, Σ i, Y i) ≃ Σ i, C(X, Y i) :=
       exact ⟨⟨i, g⟩, rfl⟩
 
 
-
-/-- A continuous map from a connected space to a `Σ`-type can be lifted to one of the
-components `π i`. -/
-theorem continuous_map.exists_lift_sigma {X ι : Type*} {π : ι → Type*} [topological_space X]
-  [connected_space X] [∀ i, topological_space (π i)] (f : C(X, Σ i, π i)) :
-  ∃ (i : ι) (g : C(X, π i)), f = continuous_map.comp ⟨sigma.mk i⟩ g :=
-let ⟨i, g, hgc, hfg⟩ := f.continuous.exists_lift_sigma in
-⟨i, ⟨g, hgc⟩, fun_like.ext' hfg⟩
-
-noncomputable def equiv.continuous_map_sigma_equiv {X ι : Type*} {π : ι → Type*}
-  [topological_space X] [connected_space X] [∀ i, topological_space (π i)] :
-  C(X, Σ i, π i) ≃ Σ i, C(X, π i) :=
-equiv.symm $ equiv.of_bijective (λ g, continuous_map.comp ⟨sigma.mk g.1⟩ g.2) $
-  begin
-    refine ⟨_, λ f, let ⟨i, g, h⟩ := f.exists_lift_sigma in ⟨⟨i, g⟩, h.symm⟩⟩,
-    rintro ⟨i, g⟩ ⟨j, g'⟩ h,
-    obtain ⟨rfl : i = j, h⟩ := function.eq_of_sigma_mk_comp (fun_like.ext'_iff.1 h),
-    simpa using h
-  end
+def ContinuousMap.sigmaCodHomeomorph : C(X, Σ i, Y i) ≃ₜ Σ i, C(X, Y i) :=
+  .symm <| (sigmaCodEquiv X Y).symm.toHomeomorphOfInducing <| by
+    change Inducing fun g : Σ i, C(X, Y i) ↦ (sigmaMk g.1).comp g.2
+    rw [inducing_iff_nhds]
