7263ba4f649c31a4df92ebbe9fb2f22b39b05a38
Johan Commelin <johan@commelin.net>
2023-07-01 21:37:35 +0200

    fixes

--- Mathlib/Algebra/Category/AlgebraCat/Basic.lean
+++ Mathlib/Algebra/Category/AlgebraCat/Basic.lean
@@ -136,7 +136,7 @@ theorem coe_comp (f : M ⟶ N) (g : N ⟶ U) : (f ≫ g : M → U) = g ∘ f :=
 variable (R)
 
 /-- The "free algebra" functor, sending a type `S` to the free algebra on `S`. -/
-@[simps]
+@[simps!]
 def free : Type u ⥤ AlgebraCat.{u} R where
   obj S :=
     { carrier := FreeAlgebra R S
@@ -145,10 +145,14 @@ def free : Type u ⥤ AlgebraCat.{u} R where
   -- porting note: `apply FreeAlgebra.hom_ext` was `ext1`.
   map_id := by intro X; apply FreeAlgebra.hom_ext; simp only [FreeAlgebra.ι_comp_lift]; rfl
   map_comp := by
-    -- porting note: ditto
+  -- porting note: `apply FreeAlgebra.hom_ext` was `ext1`.
     intros; apply FreeAlgebra.hom_ext; simp only [FreeAlgebra.ι_comp_lift]; ext1
-    simp only [FreeAlgebra.lift_ι_apply, CategoryTheory.coe_comp, Function.comp_apply,
-      types_comp_apply]
+    -- Porting node: this ↓ `erw` used to be handled by the `simp` below it
+    erw [CategoryTheory.coe_comp]
+    simp only [CategoryTheory.coe_comp, Function.comp_apply, types_comp_apply]
+    -- Porting node: this ↓ `erw` and `rfl` used to be handled by the `simp` above
+    erw [FreeAlgebra.lift_ι_apply, FreeAlgebra.lift_ι_apply]
+    rfl
 #align Algebra.free AlgebraCat.free
 
 /-- The free/forget adjunction for `R`-algebras. -/
@@ -157,13 +161,21 @@ def adj : free.{u} R ⊣ forget (AlgebraCat.{u} R) :=
     { homEquiv := fun X A => (FreeAlgebra.lift _).symm
       -- Relying on `obviously` to fill out these proofs is very slow :(
       homEquiv_naturality_left_symm := by
-        intros; ext
+        -- porting note: `apply FreeAlgebra.hom_ext` was `ext1`.
+        intros; apply FreeAlgebra.hom_ext; simp only [FreeAlgebra.ι_comp_lift]; ext1
         simp only [free_map, Equiv.symm_symm, FreeAlgebra.lift_ι_apply, CategoryTheory.coe_comp,
           Function.comp_apply, types_comp_apply]
+        -- Porting node: this ↓ `erw` and `rfl` used to be handled by the `simp` above
+        erw [FreeAlgebra.lift_ι_apply, CategoryTheory.comp_apply, FreeAlgebra.lift_ι_apply,
+          Function.comp_apply, FreeAlgebra.lift_ι_apply]
+        rfl
       homEquiv_naturality_right := by
         intros; ext
         simp only [CategoryTheory.coe_comp, Function.comp_apply,
-          FreeAlgebra.lift_symm_apply, types_comp_apply] }
+          FreeAlgebra.lift_symm_apply, types_comp_apply]
+        -- Porting note: proof used to be done after this ↑ `simp`; added ↓ two lines
+        erw [FreeAlgebra.lift_symm_apply, FreeAlgebra.lift_symm_apply]
+        rfl }
 #align Algebra.adj AlgebraCat.adj
 
 instance : IsRightAdjoint (forget (AlgebraCat.{u} R)) :=
@@ -202,9 +214,9 @@ def toAlgEquiv {X Y : AlgebraCat R} (i : X ≅ Y) : X ≃ₐ[R] Y where
     change (i.inv ≫ i.hom) x = x
     simp only [inv_hom_id]
     rw [id_apply]
-  map_add' := by simp
-  map_mul' := by simp
-  commutes' := by simp
+  map_add' := i.hom.map_add -- Porting note: was `by simp`
+  map_mul' := i.hom.map_mul -- Porting note: was `by simp`
+  commutes' := i.hom.commutes -- Porting note: was `by simp`
 #align category_theory.iso.to_alg_equiv CategoryTheory.Iso.toAlgEquiv
 
 end CategoryTheory.Iso
