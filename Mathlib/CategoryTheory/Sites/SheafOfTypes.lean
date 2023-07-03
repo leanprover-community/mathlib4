1011b11144ee42099d2c0c4556ba487f7bc10abc
nick-kuhn <nikolaskuhn@gmx.de>
2023-07-03 11:03:23 +0000

    feat: cocones over Sieves and FamilyOfElements for yoneda (#5647)
    Adding some lemmas that relate a `FamilyOfElements` for yoneda presheaves with cocones over a sieve.
    
    
    
    Co-authored-by: nick-kuhn <46423253+nick-kuhn@users.noreply.github.com>

--- Mathlib/CategoryTheory/Sites/SheafOfTypes.lean
+++ Mathlib/CategoryTheory/Sites/SheafOfTypes.lean
@@ -783,7 +783,92 @@ theorem isSheaf_bot : IsSheaf (⊥ : GrothendieckTopology C) P := fun X => by
   simp [isSheafFor_top_sieve]
 #align category_theory.presieve.is_sheaf_bot CategoryTheory.Presieve.isSheaf_bot
 
+/--
+For a presheaf of the form `yoneda.obj W`, a compatible family of elements on a sieve
+is the same as a co-cone over the sieve. Constructing a co-cone from a compatible family works for
+any presieve, as does constructing a family of elements from a co-cone. Showing compatibility of the
+family needs the sieve condition.
+Note: This is related to `CategoryTheory.Presheaf.conesEquivSieveCompatibleFamily`
+ -/
+
+def compatibleYonedaFamily_toCocone (R : Presieve X) (W : C) (x : FamilyOfElements (yoneda.obj W) R)
+  (hx : FamilyOfElements.Compatible x) :
+     Cocone (R.diagram)  where
+  pt := W
+  ι  := {
+    app := fun f => x f.obj.hom f.property
+    naturality := by
+      intro g₁ g₂ F
+      simp only [Functor.id_obj, Functor.comp_obj, fullSubcategoryInclusion.obj, Over.forget_obj,
+          Functor.const_obj_obj, Functor.comp_map, fullSubcategoryInclusion.map, Over.forget_map,
+          Functor.const_obj_map, Category.comp_id]
+      rw [← Category.id_comp (x g₁.obj.hom g₁.property)]
+      apply hx
+      simp only [Functor.id_obj, Over.w, Opposite.unop_op, Category.id_comp]
+  }
+
+def yonedaFamilyOfElements_fromCocone (R : Presieve X) (s : Cocone (diagram R)) :
+      FamilyOfElements (yoneda.obj s.pt) R := fun _ f hf => s.ι.app ⟨Over.mk f, hf⟩
+
 end Presieve
+
+namespace Sieve
+open Presieve
+
+variable {C : Type u₁} [Category.{v₁} C]
+
+variable {X : C}
+
+theorem yonedaFamily_fromCocone_compatible (S : Sieve X) (s : Cocone (diagram S.arrows)) :
+    FamilyOfElements.Compatible <| yonedaFamilyOfElements_fromCocone S.arrows s := by
+  intro Y₁ Y₂ Z g₁ g₂ f₁ f₂ hf₁ hf₂ hgf
+  have Hs := s.ι.naturality
+  simp only [Functor.id_obj, yoneda_obj_obj, Opposite.unop_op, yoneda_obj_map, Quiver.Hom.unop_op]
+  dsimp [yonedaFamilyOfElements_fromCocone]
+  have hgf₁ : S.arrows (g₁ ≫ f₁) := by exact Sieve.downward_closed S hf₁ g₁
+  have hgf₂ : S.arrows (g₂ ≫ f₂) := by exact Sieve.downward_closed S hf₂ g₂
+  let F : (Over.mk (g₁ ≫ f₁) : Over X) ⟶ (Over.mk (g₂ ≫ f₂) : Over X) := (Over.homMk (𝟙 Z) )
+  let F₁ : (Over.mk (g₁ ≫ f₁) : Over X) ⟶ (Over.mk f₁ : Over X) := (Over.homMk g₁)
+  let F₂ : (Over.mk (g₂ ≫ f₂) : Over X) ⟶ (Over.mk f₂ : Over X) := (Over.homMk g₂)
+  have hF := @Hs ⟨Over.mk (g₁ ≫ f₁), hgf₁⟩ ⟨Over.mk (g₂ ≫ f₂), hgf₂⟩ F
+  have hF₁ := @Hs ⟨Over.mk (g₁ ≫ f₁), hgf₁⟩ ⟨Over.mk f₁, hf₁⟩ F₁
+  have hF₂ := @Hs ⟨Over.mk (g₂ ≫ f₂), hgf₂⟩ ⟨Over.mk f₂, hf₂⟩ F₂
+  aesop_cat
+
+/--
+The base of a sieve `S` is a colimit of `S` iff all Yoneda-presheaves satisfy
+the sheaf condition for `S`.
+-/
+theorem forallYonedaIsSheaf_iff_colimit (S : Sieve X) :
+    (∀ W : C, Presieve.IsSheafFor (yoneda.obj W) (S : Presieve X)) ↔
+      Nonempty (IsColimit S.arrows.cocone) := by
+  constructor
+  · intro H
+    refine Nonempty.intro ?_
+    exact {
+    desc := fun s => H s.pt (yonedaFamilyOfElements_fromCocone S.arrows s)
+        (yonedaFamily_fromCocone_compatible S s) |>.choose
+    fac := by
+      intro s f
+      replace H := H s.pt (yonedaFamilyOfElements_fromCocone S.arrows s)
+         (yonedaFamily_fromCocone_compatible S s)
+      have ht := H.choose_spec.1 f.obj.hom f.property
+      aesop_cat
+    uniq := by
+      intro s Fs HFs
+      replace H := H s.pt (yonedaFamilyOfElements_fromCocone S.arrows s)
+          (yonedaFamily_fromCocone_compatible S s)
+      apply H.choose_spec.2 Fs
+      exact fun _ f hf => HFs ⟨Over.mk f, hf⟩  }
+  · intro H W x hx
+    replace H := Classical.choice H
+    let s := compatibleYonedaFamily_toCocone S W x hx
+    use H.desc s
+    constructor
+    · exact fun _ f hf => (H.fac s) ⟨Over.mk f, hf⟩
+    · exact fun g hg => H.uniq s g (fun ⟨⟨f, _, hom⟩, hf⟩ => hg hom hf)
+
+end Sieve
 
 namespace Equalizer
 
