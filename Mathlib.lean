ff0764a844925427b6cb86cf0a8e685ec737dc9c
Joël Riou <joel.riou@universite-paris-saclay.fr>
2023-07-03 16:14:58 +0000

    feat: port AlgebraicGeometry.Morphisms.QuasiCompact (#5642)
    Co-authored-by: Jujian Zhang <jujian.zhang1998@outlook.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -427,6 +427,7 @@ import Mathlib.AlgebraicGeometry.LocallyRingedSpace
 import Mathlib.AlgebraicGeometry.LocallyRingedSpace.HasColimits
 import Mathlib.AlgebraicGeometry.Morphisms.Basic
 import Mathlib.AlgebraicGeometry.Morphisms.OpenImmersion
+import Mathlib.AlgebraicGeometry.Morphisms.QuasiCompact
 import Mathlib.AlgebraicGeometry.Morphisms.UniversallyClosed
 import Mathlib.AlgebraicGeometry.OpenImmersion.Basic
 import Mathlib.AlgebraicGeometry.OpenImmersion.Scheme
