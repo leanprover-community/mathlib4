9c8d5b1c1e21a6c81165f33e9d6195733bf5b08e
Jason Yuen <jason_yuen2007@hotmail.com>
2023-07-03 11:03:24 +0000

    feat: port RepresentationTheory.Character (#5679)
    Co-authored-by: Johan Commelin <johan@commelin.net>

--- Mathlib.lean
+++ Mathlib.lean
@@ -2621,6 +2621,7 @@ import Mathlib.Probability.StrongLaw
 import Mathlib.Probability.Variance
 import Mathlib.RepresentationTheory.Action
 import Mathlib.RepresentationTheory.Basic
+import Mathlib.RepresentationTheory.Character
 import Mathlib.RepresentationTheory.FdRep
 import Mathlib.RepresentationTheory.GroupCohomology.Resolution
 import Mathlib.RepresentationTheory.Invariants
