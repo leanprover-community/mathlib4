d4ec66d43a1794710c5832cab3fc7f5e9b9cd9a8
Moritz Firsching <firsching@google.com>
2023-07-03 19:58:28 +0200

    Update Archive/Imo/Imo1998Q2.lean
    Co-authored-by: Johan Commelin <johan@commelin.net>

--- Archive/Imo/Imo1998Q2.lean
+++ Archive/Imo/Imo1998Q2.lean
@@ -231,7 +231,7 @@ end
 theorem clear_denominators {a b k : ℕ} (ha : 0 < a) (hb : 0 < b) :
     (b - 1 : ℚ) / (2 * b) ≤ k / a ↔ ((b : ℕ) - 1) * a ≤ k * (2 * b) := by
   rw [div_le_div_iff]
-  -- porting note: norm_cast alone was able to to that before
+  -- porting note: proof used to finish with `<;> norm_cast <;> simp [ha, hb]`
   · convert @Nat.cast_le ℚ _ _ _ _
     · aesop
     · norm_cast
