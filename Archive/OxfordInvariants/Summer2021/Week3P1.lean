bc4f2cc563bbfc90c0ccc1acd9d1aa12b167cda9
Jeremy Tan Jie Rui <reddeloostw@gmail.com>
2023-07-04 19:07:35 +0800

    enaure port dashboard will notice this

--- Archive/OxfordInvariants/Summer2021/Week3P1.lean
+++ Archive/OxfordInvariants/Summer2021/Week3P1.lean
@@ -3,7 +3,7 @@ Copyright (c) 2021 Yaël Dillies, Bhavik Mehta. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yaël Dillies, Bhavik Mehta
 
-! This file was ported from Lean 3 source module oxford_invariants.«2021summer».week3_p1
+! This file was ported from Lean 3 source module oxford_invariants.2021summer.week3_p1
 ! leanprover-community/mathlib commit 328375597f2c0dd00522d9c2e5a33b6a6128feeb
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
