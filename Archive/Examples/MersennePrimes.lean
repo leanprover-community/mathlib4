67d73393bb5bb1a9887cb33fc5444359282a1d1c
Johan Commelin <johan@commelin.net>
2023-07-04 11:55:11 +0000

    more nearby fixes

--- Archive/Examples/MersennePrimes.lean
+++ Archive/Examples/MersennePrimes.lean
@@ -41,21 +41,21 @@ but work fine on the command line.
 
 -- /-- 2305843009213693951.Prime, Pervouchine (1883), Seelhoff (1886) -/
 -- example : (mersenne 61).Prime :=
---   lucas_lehmer_sufficiency _ (by norm_num) (by LucasLehmer.run_test).
+--   lucas_lehmer_sufficiency _ (by norm_num) (by lucas_lehmer_test).
 -- /-- 618970019642690137449562111.Prime, Powers (1911) -/
 -- -- takes ~100s
 -- example : (mersenne 89).Prime :=
---   lucas_lehmer_sufficiency _ (by norm_num) (by LucasLehmer.run_test).
+--   lucas_lehmer_sufficiency _ (by norm_num) (by lucas_lehmer_test).
 -- /-- 162259276829213363391578010288127.Prime, Power (1914) -/
 -- -- takes ~190s
 -- example : (mersenne 107).Prime :=
---   lucas_lehmer_sufficiency _ (by norm_num) (by LucasLehmer.run_test).
+--   lucas_lehmer_sufficiency _ (by norm_num) (by lucas_lehmer_test).
 -- /-- 170141183460469231731687303715884105727.Prime, Lucas (1876) -/
 -- -- takes ~370s
 -- example : (mersenne 127).Prime :=
---   lucas_lehmer_sufficiency _ (by norm_num) (by LucasLehmer.run_test).
+--   lucas_lehmer_sufficiency _ (by norm_num) (by lucas_lehmer_test).
 -- This still doesn't get us over the big gap and into the computer era, unfortunately.
 -- /-- (2^521 - 1).Prime, Robinson (1954) -/
 -- -- This has not been run successfully!
 -- example : (mersenne 521).Prime :=
---   lucas_lehmer_sufficiency _ (by norm_num) (by LucasLehmer.run_test).
+--   lucas_lehmer_sufficiency _ (by norm_num) (by lucas_lehmer_test).
