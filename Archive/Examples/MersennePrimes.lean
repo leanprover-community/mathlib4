342ce99fe36055d58b7b002e694bcb2beee75049
Johan Commelin <johan@commelin.net>
2023-07-04 07:00:22 +0000

    automated fixes
    Mathbin -> Mathlib
    fix certain import statements
    move "by" to end of line
    add import to Archive.lean

--- Archive/Examples/MersennePrimes.lean
+++ Archive/Examples/MersennePrimes.lean
@@ -8,7 +8,7 @@ Authors: Scott Morrison
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
-import Mathbin.NumberTheory.LucasLehmer
+import Mathlib.NumberTheory.LucasLehmer
 
 /-!
 # Explicit Mersenne primes
