7b3852c044639cfded79b94fd26d74c13563ae70
thorimur <68410468+thorimur@users.noreply.github.com>
2023-07-06 17:42:52 -0400

    docs: fix copyright and authors field? (unsure)

--- Mathlib/Lean/Meta/AppBuilder.lean
+++ Mathlib/Lean/Meta/AppBuilder.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2019 Microsoft Corporation. All rights reserved.
+Copyright (c) 2023 Thomas Murrills. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Leonardo de Moura, Thomas Murrills
+Authors: Thomas Murrills
 -/
 import Lean
 import Std.Tactic.OpenPrivate
