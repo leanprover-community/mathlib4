fa5763179a5926cb475f42ced2a4c2978833a573
Alex J Best <alex.j.best@gmail.com>
2023-07-06 16:15:07 +0100

    bomp

--- Mathlib.lean
+++ Mathlib.lean
@@ -2882,6 +2882,7 @@ import Mathlib.Tactic.Coe
 import Mathlib.Tactic.Common
 import Mathlib.Tactic.Congr!
 import Mathlib.Tactic.Constructor
+import Mathlib.Tactic.Consume
 import Mathlib.Tactic.Continuity
 import Mathlib.Tactic.Continuity.Init
 import Mathlib.Tactic.Contrapose
