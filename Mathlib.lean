28d08e7fa0e799c3467abacc07e10ef398ee82e7
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2023-07-06 13:41:34 +0000

    feat: port Analysis.Normed.Group.SemiNormedGroup.Kernels (#5085)
    Co-authored-by: Kevin Buzzard <k.buzzard@imperial.ac.uk>
    Co-authored-by: Johan Commelin <johan@commelin.net>

--- Mathlib.lean
+++ Mathlib.lean
@@ -676,6 +676,7 @@ import Mathlib.Analysis.Normed.Group.InfiniteSum
 import Mathlib.Analysis.Normed.Group.Pointwise
 import Mathlib.Analysis.Normed.Group.Quotient
 import Mathlib.Analysis.Normed.Group.SemiNormedGroupCat
+import Mathlib.Analysis.Normed.Group.SemiNormedGroupCat.Kernels
 import Mathlib.Analysis.Normed.Group.Seminorm
 import Mathlib.Analysis.Normed.MulAction
 import Mathlib.Analysis.Normed.Order.Basic
