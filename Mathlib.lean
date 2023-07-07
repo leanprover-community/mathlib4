75c9ab8bc145359dca38f2ae172f5cabfcbc8084
dagurtomas <dagurtomas@gmail.com>
2023-07-07 15:48:46 +0000

    update Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -3103,6 +3103,7 @@ import Mathlib.Topology.Category.Locale
 import Mathlib.Topology.Category.Profinite.AsLimit
 import Mathlib.Topology.Category.Profinite.Basic
 import Mathlib.Topology.Category.Profinite.CofilteredLimit
+import Mathlib.Topology.Category.Profinite.ExplicitLimits
 import Mathlib.Topology.Category.Profinite.Projective
 import Mathlib.Topology.Category.TopCat.Adjunctions
 import Mathlib.Topology.Category.TopCat.Basic
