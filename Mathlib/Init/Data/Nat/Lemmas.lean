c5978139be2aa7824ee3e55a8a8ab904b7427254
Eric Wieser <wieser.eric@gmail.com>
2023-07-09 15:59:01 +0000

    restore unrelated lemmas

--- Mathlib/Init/Data/Nat/Lemmas.lean
+++ Mathlib/Init/Data/Nat/Lemmas.lean
@@ -762,6 +762,82 @@ protected theorem find_min' {m : ℕ} (h : p m) : Nat.find H ≤ m :=
   le_of_not_lt fun l => Nat.find_min H l h
 #align nat.find_min' Nat.find_min'
 
+lemma to_digits_core_lens_eq_aux (b f : Nat) :
+  ∀ (n : Nat) (l1 l2 : List Char), l1.length = l2.length →
+    (Nat.toDigitsCore b f n l1).length = (Nat.toDigitsCore b f n l2).length := by
+  induction f with (simp only [Nat.toDigitsCore, List.length]; intro n l1 l2 hlen)
+  | zero => assumption
+  | succ f ih =>
+    by_cases hx : n / b = 0
+    case pos => simp only [hx, if_true, List.length, congrArg (fun l ↦ l + 1) hlen]
+    case neg =>
+      simp only [hx, if_false]
+      specialize ih (n / b) (Nat.digitChar (n % b) :: l1) (Nat.digitChar (n % b) :: l2)
+      simp only [List.length, congrArg (fun l ↦ l + 1) hlen] at ih
+      exact ih trivial
+
+lemma to_digits_core_lens_eq (b f : Nat) : ∀ (n : Nat) (c : Char) (tl : List Char),
+    (Nat.toDigitsCore b f n (c :: tl)).length = (Nat.toDigitsCore b f n tl).length + 1 := by
+  induction f with (intro n c tl; simp only [Nat.toDigitsCore, List.length])
+  | succ f ih =>
+    by_cases hnb : (n / b) = 0
+    case pos => simp only [hnb, if_true, List.length]
+    case neg =>
+      generalize hx: Nat.digitChar (n % b) = x
+      simp only [hx, hnb, if_false] at ih
+      simp only [hnb, if_false]
+      specialize ih (n / b) c (x :: tl)
+      rw [← ih]
+      have lens_eq : (x :: (c :: tl)).length = (c :: x :: tl).length := by simp
+      apply to_digits_core_lens_eq_aux
+      exact lens_eq
+
+lemma nat_repr_len_aux (n b e : Nat) (h_b_pos : 0 < b) :  n < b ^ e.succ → n / b < b ^ e := by
+  simp only [Nat.pow_succ]
+  exact (@Nat.div_lt_iff_lt_mul b n (b ^ e) h_b_pos).mpr
+
+/-- The String representation produced by toDigitsCore has the proper length relative to
+the number of digits in `n < e` for some base `b`. Since this works with any base greater
+than one, it can be used for binary, decimal, and hex. -/
+lemma to_digits_core_length (b : Nat) (h : 2 <= b) (f n e : Nat)
+    (hlt : n < b ^ e) (h_e_pos: 0 < e) : (Nat.toDigitsCore b f n []).length <= e := by
+  induction f generalizing n e hlt h_e_pos with
+    simp only [Nat.toDigitsCore, List.length, Nat.zero_le]
+  | succ f ih =>
+    cases e with
+    | zero => exact False.elim (Nat.lt_irrefl 0 h_e_pos)
+    | succ e =>
+      by_cases h_pred_pos : 0 < e
+      case pos =>
+        have _ : 0 < b := Nat.lt_trans (by decide) h
+        specialize ih (n / b) e (nat_repr_len_aux n b e ‹0 < b› hlt) h_pred_pos
+        by_cases hdiv_ten : n / b = 0
+        case pos => simp only [hdiv_ten]; exact Nat.le.step h_pred_pos
+        case neg =>
+          simp only [hdiv_ten,
+            to_digits_core_lens_eq b f (n / b) (Nat.digitChar $ n % b), if_false]
+          exact Nat.succ_le_succ ih
+      case neg =>
+        have _ : e = 0 := Nat.eq_zero_of_nonpos e h_pred_pos
+        rw [‹e = 0›]
+        have _ : b ^ 1 = b := by simp only [pow_succ, pow_zero, Nat.one_mul]
+        have _ : n < b := ‹b ^ 1 = b› ▸ (‹e = 0› ▸ hlt : n < b ^ Nat.succ 0)
+        simp only [(@Nat.div_eq_of_lt n b ‹n < b› : n / b = 0), if_true, List.length]
+
+/-- The core implementation of `Nat.repr` returns a String with length less than or equal to the
+number of digits in the decimal number (represented by `e`). For example, the decimal string
+representation of any number less than 1000 (10 ^ 3) has a length less than or equal to 3. -/
+lemma repr_length (n e : Nat) : 0 < e → n < 10 ^ e → (Nat.repr n).length <= e := by
+  cases n with
+    (intro e0 he; simp only [Nat.repr, Nat.toDigits, String.length, List.asString])
+  | zero => assumption
+  | succ n =>
+    by_cases hterm : n.succ / 10 = 0
+    case pos => simp only [hterm, Nat.toDigitsCore]; assumption
+    case neg =>
+      simp only [hterm]
+      exact to_digits_core_length 10 (by decide) (Nat.succ n + 1) (Nat.succ n) e he e0
+
 end Find
 
 end Nat
