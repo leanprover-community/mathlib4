a0238a7a5ab8ec2601306e6d3beb78fe2d95c1da
dagurtomas <dagurtomas@gmail.com>
2023-07-09 21:58:54 +0000

    move totally separated instance to ExtremallyDisconnected file

--- Mathlib/Topology/Category/ExtrDisc/Basic.lean
+++ Mathlib/Topology/Category/ExtrDisc/Basic.lean
@@ -131,28 +131,6 @@ instance (X : ExtrDisc.{u}) : T2Space X :=
 instance (X : ExtrDisc.{u}) : ExtremallyDisconnected X :=
   X.2
 
-/-- Extremally disconnected spaces are totally separated. -/
-instance (X : ExtrDisc.{u}) : TotallySeparatedSpace X :=
-{ isTotallySeparated_univ := by
-    intro x _ y _ hxy
-    obtain ⟨U, V, hUV⟩ := T2Space.t2 x y hxy
-    use closure U
-    use (closure U)ᶜ
-    refine ⟨ExtremallyDisconnected.open_closure U hUV.1,
-      by simp only [isOpen_compl_iff, isClosed_closure], subset_closure hUV.2.2.1, ?_,
-      by simp only [Set.union_compl_self, Set.subset_univ], disjoint_compl_right⟩
-    simp only [Set.mem_compl_iff]
-    rw [mem_closure_iff]
-    push_neg
-    refine' ⟨V, ⟨hUV.2.1, hUV.2.2.2.1, _⟩⟩
-    rw [Set.nonempty_iff_ne_empty]
-    simp only [not_not]
-    rw [← Set.disjoint_iff_inter_eq_empty, disjoint_comm]
-    exact hUV.2.2.2.2 }
-
-/-- Extremally disconnected spaces are totally disconnected. -/
-instance (X : ExtrDisc.{u}) : TotallyDisconnectedSpace X := inferInstance
-
 /-- The functor from extremally disconnected spaces to profinite spaces. -/
 @[simps]
 def toProfinite : ExtrDisc.{u} ⥤ Profinite.{u} where
