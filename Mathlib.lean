f4657c81a5f02c646ebe1ab981804e5081f634f5
Christopher Hoskin <christopher.hoskin@gmail.com>
2023-07-10 22:02:50 +0100

    Fix imports

--- Mathlib.lean
+++ Mathlib.lean
@@ -2493,6 +2493,7 @@ import Mathlib.Order.Compare
 import Mathlib.Order.CompleteBooleanAlgebra
 import Mathlib.Order.CompleteLattice
 import Mathlib.Order.CompleteLatticeIntervals
+import Mathlib.Order.CompletePartialOrder
 import Mathlib.Order.Concept
 import Mathlib.Order.ConditionallyCompleteLattice.Basic
 import Mathlib.Order.ConditionallyCompleteLattice.Finset
