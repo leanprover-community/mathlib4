5a28a0cbf00a9a3938805781a4035a798f552ce6
Eric Wieser <wieser.eric@gmail.com>
2023-07-10 12:17:35 +0000

    chore: remove lean_core prefix (#5794)
    This prefix isn't present in the lean3port repository, nor is it present when actually writing `import` in Lean 3.

--- Mathlib/Data/Vector.lean
+++ Mathlib/Data/Vector.lean
@@ -3,7 +3,7 @@ Copyright (c) 2016 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Leonardo de Moura
 
-! This file was ported from Lean 3 source module lean_core.data.vector
+! This file was ported from Lean 3 source module data.vector
 ! leanprover-community/lean commit 855e5b74e3a52a40552e8f067169d747d48743fd
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
