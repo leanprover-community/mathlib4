10e6ab41739ccb3dfc9c7e0ca1ec5add98cf2aef
dagurtomas <dagurtomas@gmail.com>
2023-07-11 08:57:29 +0000

    Add projecrtivity to ExtrDisc.Basic

--- Mathlib/Topology/Category/ExtrDisc/Basic.lean
+++ Mathlib/Topology/Category/ExtrDisc/Basic.lean
@@ -152,4 +152,81 @@ instance : Faithful toProfinite := by
 instance : toProfinite ⋙ profiniteToCompHaus = toCompHaus :=
   rfl
 
+-- TODO: Gleason's theorem.
+instance (X : ExtrDisc) : Projective X.compHaus where
+  factors := by
+    intro B C φ f _
+    haveI : ExtremallyDisconnected X.compHaus.toTop := X.extrDisc
+    have hf : f.1.Surjective
+    · rwa [CompHaus.epi_iff_surjective] at *
+    obtain ⟨f', h⟩ := CompactT2.ExtremallyDisconnected.projective φ.continuous f.continuous hf
+    use ⟨f', h.left⟩
+    ext
+    exact congr_fun h.right _
+
 end ExtrDisc
+
+namespace CompHaus
+
+/-- If `X` is compact Hausdorff, `presentation X` is an extremally disconnected space
+  equipped with an epimorphism down to `X`. It is a "constructive" witness to the
+  fact that `CompHaus` has enough projectives.  -/
+noncomputable
+def presentation (X : CompHaus) : ExtrDisc where
+  compHaus := (projectivePresentation X).p
+  extrDisc := by
+    refine' CompactT2.Projective.extremallyDisconnected
+      (@fun Y Z _ _ _ _ _ _ f g hfcont hgcont hgsurj => _)
+    let g₁ : (CompHaus.of Y) ⟶ (CompHaus.of Z) := ⟨g, hgcont⟩
+    let f₁ : (projectivePresentation X).p ⟶ (CompHaus.of Z) := ⟨f, hfcont⟩
+    have hg₁ : Epi g₁ := (epi_iff_surjective _).2 hgsurj
+    refine' ⟨Projective.factorThru f₁ g₁, (Projective.factorThru f₁ g₁).2, funext (fun _ => _)⟩
+    change (Projective.factorThru f₁ g₁ ≫ g₁) _ = f _
+    rw [Projective.factorThru_comp]
+    rfl
+
+/-- The morphism from `presentation X` to `X`. -/
+noncomputable
+def presentationπ (X : CompHaus) : X.presentation.compHaus ⟶ X :=
+  (projectivePresentation X).f
+
+/-- The morphism from `presentation X` to `X` is an epimorphism. -/
+noncomputable
+instance epiPresentπ (X : CompHaus) : Epi X.presentationπ :=
+  (projectivePresentation X).epi
+
+/--
+
+               X
+               |
+              (f)
+               |
+               \/
+  Z ---(e)---> Y
+
+If `Z` is extremally disconnected, X, Y are compact Hausdorff, if `f : X ⟶ Y` is an epi and `e : Z ⟶ Y` is
+arbitrary, then `lift e f` is a fixed (but arbitrary) lift of `e` to a morphism `Z ⟶ X`. It exists because
+`Z` is a projective object in `CompHaus`.
+-/
+noncomputable
+def lift {X Y : CompHaus} {Z : ExtrDisc} (e : Z.compHaus ⟶ Y) (f : X ⟶ Y) [Epi f] : Z.compHaus ⟶ X :=
+  Projective.factorThru e f
+
+@[simp, reassoc]
+lemma lift_lifts {X Y : CompHaus} {Z : ExtrDisc} (e : Z.compHaus ⟶ Y) (f : X ⟶ Y) [Epi f] :
+    lift e f ≫ f = e := by simp [lift]
+
+namespace CompHaus
+
+lemma Gleason (X : CompHaus.{u}) :
+    Projective X ↔ ExtremallyDisconnected X := by
+  constructor
+  · intro h
+    show ExtremallyDisconnected X.toExtrDisc
+    infer_instance
+  · intro h
+    let X' : ExtrDisc := ⟨X⟩
+    show Projective X'.compHaus
+    apply ExtrDisc.instProjectiveCompHausCategoryCompHaus
+
+end CompHaus
