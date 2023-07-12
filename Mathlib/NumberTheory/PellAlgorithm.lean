76a157ecc9c4ee29380e74a1be1f9c1810c970ff
Floris van Doorn <fpvdoorn@gmail.com>
2023-07-12 17:19:40 +0200

    WIP file

---
+++ Mathlib/NumberTheory/PellAlgorithm.lean
@@ -0,0 +1,78 @@
+
+import Mathlib
+
+
+/-
+fun (a,b,c,D) ↦
+conditions: sign(a) = - sign(c), a ≠ 0, D = b^2 - 4ac
+
+m = ⌊ (-b + √D) / (2a) ⌋
+
+
+return (-(am^2+bm+c),-(2am+b),-a,m)
+Pell:
+x^2 - Ny^2 = 1
+* equivalently, elt in ℤ√N with norm 1
+* find fundamental solution η
+* really fundamental solution: also generated everything with norm = ±1
+* all solutions are in the form ±η^k
+
+example: N = 19 -> 170^2 - 19*39^2 = 1
+
+want to find x^2-19y^2=1
+x>y
+suppose we know `m = ⌊x / y⌋`
+Let (x', y') := (y, x - my) (cf Euclidean algorithm)
+(x,y) := (y'+mx',x')
+If ax^2+bxy+cy^2=1
+then
+
+all steps but the first one are reversible
+Book :
+* A. Weil: number theory an approach through history from hammurapi to legendre
+
+-/
+
+
+
+def brouckner (a b c D : ℤ) : ℤ × ℤ × ℤ × ℤ :=
+  let m := ⌊ (-b + (D : ℚ).sqrt) / (2 * a) ⌋
+  (-(a*m^2+b*m+c),-(2*a*m+b),-a,m)
+
+def iterate_brouckner (a b c D : ℤ) (l : List (ℤ × ℤ × ℤ × ℤ)) :
+    (n : ℕ) → List (ℤ × ℤ × ℤ × ℤ)
+  | 0     => l
+  | n + 1 =>
+    let (a, b, c, m) := brouckner a b c D
+    if a = 1 then
+      (a,b,c,m)::l
+    else
+      iterate_brouckner a b c D ((a,b,c,m)::l) n
+
+#eval iterate_brouckner 1 0 (-19) (4 * 19) [] 20
+
+-- example : let (a',b',c',m) := brouckner a b c D;
+--   brouckner (-c') (-b') (-a') D = (-c,-b,-a,sorry) := by
+--   simp [brouckner]
+--   constructor
+--   field_simp
+
+
+/-
+1 compute x and y and that they are a solution
+2 show that a > 0 and c < 0 in intermdiate steps
+3-ε: claim: if brouckner a b c D = (a',b',c',m) then
+    brouckner (-c) (-b) (-a) D = (a,b,c,?)
+3 from 3-ε show reversibility of all steps but first one
+4 show that there are finitely many options for a,b,c
+  with b^2-4ac=D and a>0,c<0 (at most √D * (√D)ᵉ for some small `e>0`)
+5 from 2,3,4 show termination
+6 the x,y we get in step 1 are a *fundamental* solution
+  to do: show that brouckner is monotone w.r.t. some order
+-/
+
+-- #reduce (54 : ℤ).sqrt
+example : (50 : ℕ).sqrt = 7 := by norm_num
+example : (123456789 : ℕ).sqrt = 11111 := by norm_num
+example : (2340276615670 ^ 2 : ℕ).sqrt = 2340276615670 := by
+  norm_num
