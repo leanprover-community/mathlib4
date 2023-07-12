e521ca98e7731d26f34ed2bae9e7cc5a69ad170c
thorimur <68410468+thorimur@users.noreply.github.com>
2023-07-12 04:08:24 -0400

    style, refactor fixes: * namespacing to use `.compare` notation * take length check out of `compareListM`, put in usages * add `resultMsg` to take mode into account * add args to let decl's be accessed in the correct state then passed in

--- Mathlib/Tactic/FailIfNoProgress.lean
+++ Mathlib/Tactic/FailIfNoProgress.lean
@@ -36,9 +36,9 @@ having any  effect, e.g. `repeat (fail_if_no_progress simp <;> ring_nf)`.
 
 
 
-namespace Mathlib.Tactic
+namespace Mathlib.Meta
 
-open Lean Meta Elab Tactic Parser.Tactic
+open Lean Meta Elab
 
 /-- Specifies which type of equality check to use. Either `.beq` or `.defEq`. -/
 inductive EqKind where | beq | defEq
@@ -111,151 +111,183 @@ structure MVarIdComparisonConfig where
   compareMetavarDecls? : Option MetavarDeclComparisonConfig := some {}
 deriving Repr, Inhabited
 -- !! Generalize with filtering and mustNotLose,mustNotGain list/array config stuff? Generalize into a goal list config?
-inductive FailIfNoProgress.Mode where
-/-- Compares the goal lists before and after the tactic. -/
-| normal
-/-- Only checks if the initial metavariables have been assigned or not. -/
-| quick
-deriving Repr, Inhabited
 
 
 
 
 -- allow custom functions for comparing `Expr`s etc.? It'd be cool if every comparison function could be overridden, and really was stored in a structure. It'd be neat to do the custom elaborator thing.
-/--
---!! Needs updating
 
-Config data for `failIfNoProgress`, `fail_if_no_progress`, and `runAndFailIfNoProgress`.
 
-This determines which aspects of the goals are checked for "progress" and the nature of the
-equality checks made.
 
-* `eqKind : EqKind := .defEq` – either `.beq` or `.defEq`; `.beq` uses the instance of `BEq Expr` to
-  compare expressions, and `.defEq` uses `isDefEq`
-* `transparency : TransparencyMode := .reducible` – the `TransparencyMode` to compare expressions
-at.
-* `checkTarget : Bool := true` – compare the targets of goals when comparing goals.
-* `onLocalInstances : Option LocalInstanceComparisonConfig` – The configuration for comparing local
-instances of goals. If `none`, local instances are not compared.
-  * `eqKind : EqKind := eqKind`
-  * `transparency : TransparencyMode := transparency`
-* `onCtx : Option CtxComparisonConfig` – The configuration for comparing local contexts. By default,
-  this inherits the given values of `eqKind` and `transparency`, and only checks indices and
-  FVarIds if `eqKind := .beq`. If `none`, local contexts are not compared.
-  * `eqKind : EqKind := eqKind`
-  * `transparency : TransparencyMode := transparency`
-  * `checkIndex : Bool := eqKind.isBEq` – checks if the indices (positions) of fvars have changed;
-    amounts to checking if some fvars have been made unavialable
-  * `checkFVarIds : Bool := eqKind.isBEq` – compares `FVarId`s directly
-  * `checkUserNames : Bool := true` – compares the names of fvars in the local context
-  * `checkLetValue  : Bool := true` – compares the values of `ldecls`
--/
-structure FailIfNoProgress.Config extends ExprComparisonConfig, MetavarDeclComparisonConfig where
-  mode : FailIfNoProgress.Mode := .normal
-deriving Repr, Inhabited
+end Mathlib.Meta
 
---!! Does transparency affect BEq or is it only a DefEq thing? I think the latter, but want to be
--- sure.
-/-- Compares two expressions according to the given `ComparisonConfig`. -/
-def compareExpr (config : ExprComparisonConfig := {}) : Expr → Expr → MetaM Bool :=
+namespace Lean
+
+open Lean Meta Mathlib.Meta
+
+/-- Compares two expressions according to the given `ExprComparisonConfig`. See the documentation for `ExprComparisonConfig` for more information. -/
+def Expr.compare (e₁ e₂ : Expr) (config : ExprComparisonConfig := {}) : MetaM Bool :=
   match config with
-  | ⟨.beq, _⟩ => fun e₁ e₂ => pure (e₁ == e₂)
-  | ⟨.defEq, t⟩ => fun e₁ e₂ => withTransparency t <| withNewMCtxDepth <| isDefEq e₁ e₂
+  | ⟨.beq, _⟩ => pure (e₁ == e₂)
+  | ⟨.defEq, t⟩ => withTransparency t <| withNewMCtxDepth <| isDefEq e₁ e₂
 
-/-- Compares two lists monadically. -/
-private def compareListM [Monad m] (c : α → α → m Bool) (l₁ l₂ : List α) : m Bool := do
-  unless l₁.length == l₂.length do return false
-  l₁.zip l₂ |>.allM fun (a, b) => c a b
+/-- Compares two lists monadically. Does not check length first. Unrelated to `ListM`. -/
+private def compareListM (eq : α → α → MetaM Bool) (l₁ l₂ : List α) : MetaM Bool :=
+  l₁.zip l₂ |>.allM fun (a, b) => eq a b
 
-/-- Compare two `LocalDecl`s according to `config`. -/
-def compareLocalDecl (l₁ l₂ : LocalDecl) (cfg : LocalDeclComparisonConfig := {})
+/-- Compare two `LocalDecl`s according to the configs. The settings in `ExprComparisonConfig` will
+be used when comparing expressions. -/
+def LocalDecl.compare (l₁ l₂ : LocalDecl) (cfg : LocalDeclComparisonConfig := {})
     (ecfg : ExprComparisonConfig) : MetaM Bool :=
+  -- withTraceNode `Meta.compare (fun _ => pure "comparing LocalDecls:") <|
   (pure <| (! cfg.checkFVarId   || l₁.fvarId   == l₂.fvarId)
     && (! cfg.checkIndex    || l₁.index    == l₂.index)
     && (! cfg.checkUserName || l₁.userName == l₂.userName)
     && (! cfg.checkLocalDeclKind || l₁.kind == l₂.kind)
     && (! cfg.checkBinderInfo || l₁.binderInfo == l₂.binderInfo))
-  <&&> (pure ! (cfg.checkLetValue && l₁.isLet && l₂.isLet) <||> compareExpr ecfg l₁.value l₂.value)
-  <&&> (pure ! cfg.checkType <||> compareExpr ecfg l₁.type l₂.type)
+  <&&> (pure ! (cfg.checkLetValue && l₁.isLet && l₂.isLet) <||> l₁.value.compare l₂.value)
+  <&&> (pure ! cfg.checkType <||> l₁.type.compare l₂.type ecfg)
 
-/-- Compare two `LocalContext`s according to the specifications in `config`. -/
-def compareLCtx (lctx₁ lctx₂ : LocalContext) (cfg : LocalContextComparisonConfig) (ecfg : ExprComparisonConfig) : MetaM Bool := do
-  -- Would be slightly better if we could normalize this function given cfg in advance. Likewise elsewhere. And also not filter at all if they're all true.
+/-- Compare two `LocalDecl`s according to the configs. The settings in `ExprComparisonConfig` will
+be used when comparing expressions. -/
+def LocalContext.compare (lctx₁ lctx₂ : LocalContext)
+    (cfg : LocalContextComparisonConfig) (ecfg : ExprComparisonConfig) : MetaM Bool := do
+  -- !! Would be slightly better if we could normalize this function given cfg in advance somehow.
+  -- Likewise elsewhere. And also not filter at all if they're all true.
   let f (d : LocalDecl) :=
-    (cfg.includeImplDetails || ! d.isImplementationDetail)
+    (cfg.includeImplDetails || ! d.kind == .implDetail)
     && (cfg.includeAuxDecls || ! d.isAuxDecl)
     && (cfg.includeCDecls || d.isLet)
     && (cfg.includeLDecls || ! d.isLet)
     && (cfg.includeDefaultDecls || ! (d.kind == .default))
   let l₁ := lctx₁.decls.toList.reduceOption.filter f
   let l₂ := lctx₂.decls.toList.reduceOption.filter f
-  compareListM (compareLocalDecl (cfg := cfg.toLocalDeclComparisonConfig) (ecfg := ecfg)) l₁ l₂
+  if l₁.length ≠ l₂.length then return false
+  compareListM (·.compare · cfg.toLocalDeclComparisonConfig ecfg) l₁ l₂
 
-instance : BEq MetavarKind where
-  beq
+-- !! move?
+instance : BEq MetavarKind where beq
   | .natural, .natural | .synthetic, .synthetic | .syntheticOpaque, .syntheticOpaque => true
   | _, _ => false
 
 /-- Compare two `MVarId`s by comparing their types and their local contexts, according to
 `config`. -/
-def compareMVarId (goal₁ goal₂ : MVarId) (cfg : MetavarDeclComparisonConfig := {})
-    (ecfg : ExprComparisonConfig := {}) : MetaM Bool := do
-  let decl₁ ← goal₁.getDecl
-  let decl₂ ← goal₂.getDecl
-  (pure (
-    (! cfg.checkIndex || decl₁.index == decl₂.index)
+def MetavarDecl.compare (decl₁ decl₂ : MetavarDecl)
+    (cfg : MetavarDeclComparisonConfig := {}) (ecfg : ExprComparisonConfig := {}) : MetaM Bool := do
+  (pure <| (! cfg.checkIndex || decl₁.index == decl₂.index)
     && (! cfg.checkUserName || decl₁.userName == decl₂.userName)
     && (! cfg.checkMetavarKind || decl₁.kind == decl₂.kind)
-    && (! cfg.checkLocalInstances || decl₁.localInstances == decl₂.localInstances)
-  ))
-  <&&> (pure ! cfg.checkTarget <||> compareExpr ecfg decl₁.type decl₂.type)
-  <&&>
-    if let some lcfg := cfg.compareLocalContexts? then
-      compareLCtx decl₁.lctx decl₂.lctx lcfg ecfg
-    else
-      pure true
+    && (! cfg.checkLocalInstances || decl₁.localInstances == decl₂.localInstances))
+  <&&> (pure ! cfg.checkTarget <||> decl₁.type.compare decl₂.type ecfg)
+  <&&> do
+    let some lcfg := cfg.compareLocalContexts? | pure true
+    decl₁.lctx.compare decl₂.lctx lcfg ecfg
+
+/-- Compare a metavariable to another metavariable according to the configs. The settings in
+`ExprComparisonConfig` will be used when comparing expressions.
+
+A decl can be provided for each goal as an override for `goal.getDecl`. This is useful when the
+goals ought to be compared across two different monad states: then the decl's can be obtained at
+the right time and passed in later.
+-/
+def MVarId.compare (goal₁ goal₂ : MVarId) (cfg : MVarIdComparisonConfig := {})
+    (ecfg : ExprComparisonConfig := {}) (decl₁ decl₂ : Option MetavarDecl := none)
+    : MetaM Bool := do
+  pure (! cfg.checkMVarId || goal₁ == goal₂)
+  <&&> do
+    let some mdcfg := cfg.compareMetavarDecls? | pure true
+    let decl₁ ← decl₁.getDM goal₁.getDecl
+    let decl₂ ← decl₂.getDM goal₂.getDecl
+    decl₁.compare decl₂ mdcfg ecfg
+
+end Lean
+
+namespace Mathlib
+
+open Lean Meta Elab Tactic Parser.Tactic
+
+namespace Meta
 
 /-- Compare a list of goals to another list of goals. Return false if the number of goals differs,
-and otherwise compare the goals pairwise in order according to `config`. -/
-def compareGoalList (goals₁ goals₂ : List MVarId) (cfg : MetavarDeclComparisonConfig := {})
-    (ecfg : ExprComparisonConfig := {}) : MetaM Bool :=
-  compareListM (compareMVarId · · cfg ecfg) goals₁ goals₂
+and otherwise compare the goals pairwise in order according to the configs.
+
+A list of decls can be provided for each list of goals as an override for `goals.mapM (·.getDecl)`.
+This is useful when the goals ought to be compared across two different monad states: then the
+decl's can be obtained at the right time and passed in later. This does not check that
+`goalsᵢ.length == declsᵢ.length`.
+-/
+def compareGoalList (goals₁ goals₂ : List MVarId) (cfg : MVarIdComparisonConfig := {})
+    (ecfg : ExprComparisonConfig := {}) (decls₁ decls₂ : Option (List MetavarDecl) := none)
+    : MetaM Bool :=
+  (pure <| (goals₁.length == goals₂.length)
+    && (! cfg.checkMVarId || goals₁ == goals₂))
+  <&&> do
+    let some mdcfg := cfg.compareMetavarDecls? | pure true
+    let decls₁ ← decls₁.getDM <| goals₁.mapM (·.getDecl)
+    let decls₂ ← decls₂.getDM <| goals₂.mapM (·.getDecl)
+    compareListM (·.compare · mdcfg ecfg) decls₁ decls₂
+
+end Meta
+
+namespace Tactic.FailIfNoProgress
+
+/-- The overall mode for a `FailIfNoProgress` config. If `mode := .normal`, the rest of the config
+is used to determined equality as described. If `mode := .quick`, the rest of the config is
+ignored, and `fail_if_no_progress` only checks if any of the original goals have been assigned. If
+not, no progress is considered to have been made. -/
+inductive Mode where
+/-- Compares the goal lists before and after the tactic using the given config settings. -/
+| normal
+/-- Only checks if the initial metavariables have been assigned or not. If this mode is used, no
+other checks are performed. -/
+| quick
+deriving Repr, Inhabited
+
+structure Config extends ExprComparisonConfig, MVarIdComparisonConfig where
+  mode : FailIfNoProgress.Mode := .normal
+deriving Repr, Inhabited
 
 /-- Run `tacs : TacticM Unit` on `goal`, and fail if no progress is made. Return the resulting list of goals otherwise. -/
 def runAndFailIfNoProgress (goal : MVarId) (tacs : TacticM Unit)
     (cfg : FailIfNoProgress.Config := {}) : TacticM (List MVarId) := do
-  let s ← saveState
-  let l ← run goal tacs -- !! correct `run`?
+  let decl ← goal.getDecl
+  let l ← run goal tacs
   try -- failing this `try` means we return `l`, and occurs iff progress has been made
     if let .quick := cfg.mode then
-      if ← goal.isAssigned then failure -- progress made
+      if ← goal.isAssigned then failure -- progress
     else
-      let [newGoal] := l | failure -- progress if number of goals has changed
-      -- progress if they are not equivalent goals
-      guard <|← compareMVarId goal newGoal cfg.toMetavarDeclComparisonConfig
-        cfg.toExprComparisonConfig
+      let [newGoal] := l | failure -- progress
+      guard <|← goal.compare newGoal cfg.toMVarIdComparisonConfig
+        cfg.toExprComparisonConfig decl
   catch _ =>
     return l
-  s.restore
-  throwError "no progress made on goal:\n{goal}"
+  let resultMsg ← match cfg.mode with
+  | .quick => pure m!"no goals were assigned"
+  | .normal => do pure m!"obtained\n{← getGoals}"
+  throwError "no progress made on goal; {resultMsg}"
 
 /-- Run `tacs`, and fail if no progress is made. Return the result otherwise. -/
 def failIfNoProgress (tacs : TacticM α) (cfg : FailIfNoProgress.Config := {}) :
     TacticM α := do
-  let s ← saveState
   let goals₁ ← getGoals
+  let decls₁ ← goals₁.mapM (·.getDecl)
   let result ← tacs
-  let noProgress ← match cfg.mode with
-    | .quick => notM <| goals₁.anyM (·.isAssigned)
+  let progress ← match cfg.mode with
+    | .quick => goals₁.anyM (·.isAssigned)
     | .normal => do
       let goals₂ ← getGoals
-      compareGoalList goals₁ goals₂ cfg.toMetavarDeclComparisonConfig cfg.toExprComparisonConfig
-  unless noProgress do return result
-  s.restore
-  let pluralization := if goals₁.length = 1 then "" else "s"
-  throwError "no progress made on goal{pluralization}"
+      notM <|
+        compareGoalList goals₁ goals₂ cfg.toMVarIdComparisonConfig cfg.toExprComparisonConfig decls₁
+  if progress then
+    return result
+  else
+    let resultMsg ← match cfg.mode with
+      | .quick => pure m!"no goals were assigned"
+      | .normal => do pure m!"obtained\n{← getGoals}"
+    let pluralization := if goals₁.length = 1 then "" else "s"
+    throwError "no progress made on goal{pluralization}; {resultMsg}"
 
-namespace FailIfNoProgress
+
 
 /-- `fail_if_no_progress tacs` evaluates `tacs`, and fails if no progress is made on the list of
 goals. This compares each of their types and local contexts before and after `tacs` is run.
@@ -269,7 +301,7 @@ be used instead of `isDefEq` by specifying `eqKind := .beq`. `BEq` is weaker tha
 will therefore cause more minor changes to count as "progress". For more configuration options see
 `FailIfNoProgress.Config`.
  -/
-syntax (name := failIfNoProgress) "fail_if_no_progress " (config)? tacticSeq : tactic
+syntax (name := failIfNoProgressSyntax) "fail_if_no_progress " (config)? tacticSeq : tactic
 
 /-- Elaborates `FailIfNoProgress.Config`. -/
 declare_config_elab elabConfig Config
@@ -277,4 +309,4 @@ declare_config_elab elabConfig Config
 elab_rules : tactic
 | `(tactic| fail_if_no_progress $[$cfg:config]? $tacs:tacticSeq) => do
   let cfg ← elabConfig (mkOptionalNode cfg)
-  Tactic.failIfNoProgress (evalTactic tacs) cfg
+  failIfNoProgress (evalTactic tacs) cfg
