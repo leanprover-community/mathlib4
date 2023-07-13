932a9d5ddf5b47fb3bcb541c6e4edba22be71027
Scott Morrison <scott@tqft.net>
2023-07-13 16:55:40 +1000

    Apply suggestions from code review

--- Mathlib/Data/HashMap.lean
+++ Mathlib/Data/HashMap.lean
@@ -3,8 +3,8 @@ Copyright (c) 2022 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison
 
-! This file was ported from Lean 3 source module data.set.pairwise.lattice
-! leanprover-community/mathlib commit c4c2ed622f43768eff32608d4a0f8a6cec1c047d
+! This file was ported from Lean 3 source module data.hash_map
+! leanprover-community/mathlib commit 2220b0cbab795e73674b8191170b0cc68c6b54a8
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 
