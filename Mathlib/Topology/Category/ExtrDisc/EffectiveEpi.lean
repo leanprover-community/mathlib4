9c8c255b85237f030a91990c01aeee233728af50
dagurtomas <dagurtomas@gmail.com>
2023-07-13 11:37:19 +0000

    forgot to add proof of precoherent ExtrDisc

--- Mathlib/Topology/Category/ExtrDisc/EffectiveEpi.lean
+++ Mathlib/Topology/Category/ExtrDisc/EffectiveEpi.lean
@@ -3,8 +3,24 @@ Copyright (c) 2023 Jon Eugster. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Dagur Asgeirsson, Boris Bolvig Kjær, Jon Eugster, Sina Hazratpour, Nima Rasekh
 -/
+
 import Mathlib.Topology.Category.ExtrDisc.ExplicitLimits
 import Mathlib.Topology.Category.CompHaus.EffectiveEpi
+
+/-!
+
+# Effective epimorphic families in `ExtrDisc`
+
+Let `π a : X a ⟶ B` be a family of morphisms in `ExtrDisc` indexed by a finite type `α`.
+In this file, we show that the following are all equivalent:
+- The family `π` is effective epimorphic.
+- The induced map `∐ X ⟶ B` is epimorphic.
+- The family `π` is jointly surjective.
+This is the main result of this file, which can be found in `ExtrDisc.effectiveEpiFamily_tfae`
+
+As a consequence, we also show that `ExtrDisc` is precoherent.
+
+-/
 
 open CategoryTheory Limits
 
@@ -144,6 +160,8 @@ def struct : EffectiveEpiFamilyStruct X π where
 
 end EffectiveEpiFamily
 
+section JointlySurjective
+
 /-- On direction of `effectiveEpiFamily_tfae` -/
 theorem effectiveEpiFamily_of_jointly_surjective
     {α : Type} [Fintype α] {B : ExtrDisc}
@@ -192,5 +210,40 @@ theorem effectiveEpiFamily_tfae {α : Type} [Fintype α] {B : ExtrDisc}
   tfae_have 3 → 1
   · apply effectiveEpiFamily_of_jointly_surjective
   tfae_finish
+
+end JointlySurjective
+
+section Coherent
+
+open CompHaus Functor
+
+theorem _root_.CategoryTheory.EffectiveEpiFamily.toCompHaus {α : Type} [Fintype α] {B : ExtrDisc.{u}}
+    {X : α → ExtrDisc.{u}} {π : (a : α) → (X a ⟶ B)} (H : EffectiveEpiFamily X π) :
+    EffectiveEpiFamily (fun a => toCompHaus.obj (X a) ) (fun a => toCompHaus.map (π a)) := by
+  refine' ((CompHaus.effectiveEpiFamily_tfae _ _).out 0 2).2 (fun b => _)
+  exact (((effectiveEpiFamily_tfae _ _).out 0 2).1 H : ∀ _, ∃ _, _) _
+
+instance : Precoherent ExtrDisc.{u} := by
+  constructor
+  intro B₁ B₂ f α _ X₁ π₁ h₁
+  refine' ⟨α, inferInstance, fun a => (pullback f (π₁ a)).presentation, fun a =>
+    toCompHaus.preimage (presentationπ _ ≫ (pullback.fst _ _)), _, id, fun a =>
+    toCompHaus.preimage (presentationπ _ ≫ (pullback.snd _ _ )), fun a => _⟩
+  · refine' ((effectiveEpiFamily_tfae _ _).out 0 2).2 (fun b => _)
+    have h₁' := ((CompHaus.effectiveEpiFamily_tfae _ _).out 0 2).1 h₁.toCompHaus
+    obtain ⟨a, x, h⟩ := h₁' (f b)
+    obtain ⟨c, hc⟩ := (CompHaus.epi_iff_surjective _).1
+      (epiPresentπ (CompHaus.pullback f (π₁ a))) ⟨⟨b, x⟩, h.symm⟩
+    refine' ⟨a, c, _⟩
+    change toCompHaus.map (toCompHaus.preimage _) _ = _
+    simp only [image_preimage, toCompHaus_obj, comp_apply, hc]
+    rfl
+  · apply map_injective toCompHaus
+    simp only [map_comp, image_preimage, Category.assoc]
+    congr 1
+    ext ⟨⟨_, _⟩, h⟩
+    exact h.symm
+
+end Coherent
 
 end ExtrDisc
