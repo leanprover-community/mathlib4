8200fa49311950717280153d9aa5b4572309eb55
Pol'tta / Miyahara Kō <pol_tta@outlook.jp>
2023-07-13 08:35:51 +0000

    chore: forward port leanprover-community/mathlib#19228 (#5843)
    The implementation in Mathlib 4 doesn't use `mapAccumLM` so the thing to do is only updating the header.

--- Mathlib/Tactic/DeriveTraversable.lean
+++ Mathlib/Tactic/DeriveTraversable.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Simon Hudon
 
 ! This file was ported from Lean 3 source module control.traversable.derive
-! leanprover-community/mathlib commit 70fd9563a21e7b963887c9360bd29b2393e6225a
+! leanprover-community/mathlib commit b01d6eb9d0a308807af54319b264d0994b91774b
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
