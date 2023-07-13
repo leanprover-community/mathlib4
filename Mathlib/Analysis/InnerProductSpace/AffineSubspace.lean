636d1c3b3afce5b8fbf0b04ceee3d2f1e4c01741
MithicSpirit <rpc01234@gmail.com>
2023-07-13 18:16:18 -0400

    chore: add copyright header

--- Mathlib/Analysis/InnerProductSpace/AffineSubspace.lean
+++ Mathlib/Analysis/InnerProductSpace/AffineSubspace.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2023 Ricardo Prado Cunha. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Ricardo Prado Cunha
+-/
 import Mathlib.LinearAlgebra.AffineSpace.AffineSubspace
 import Mathlib.Analysis.InnerProductSpace.Orthogonal
 import Mathlib.Analysis.InnerProductSpace.Projection
