625a3a6252676bc9d60a18fce8cad8e78a7604bd
Yury G. Kudryashov <urkud@urkud.name>
2023-07-13 04:03:37 +0000

    feat: port Geometry.Manifold.ContMdiffMfderiv (#5496)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1891,6 +1891,7 @@ import Mathlib.Geometry.Manifold.ChartedSpace
 import Mathlib.Geometry.Manifold.Complex
 import Mathlib.Geometry.Manifold.ConformalGroupoid
 import Mathlib.Geometry.Manifold.ContMDiff
+import Mathlib.Geometry.Manifold.ContMDiffMFDeriv
 import Mathlib.Geometry.Manifold.ContMDiffMap
 import Mathlib.Geometry.Manifold.DerivationBundle
 import Mathlib.Geometry.Manifold.Diffeomorph
