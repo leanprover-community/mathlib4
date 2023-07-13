9570d7fba9a6966aaa0e2d1e976d5adedebb4166
Scott Morrison <scott@tqft.net>
2023-07-13 08:07:27 +0000

    chore: empty ports of the Data/Buffer/Parser files (#5848)
    Co-authored-by: Scott Morrison <scott.morrison@gmail.com>

---
+++ Mathlib/Data/Buffer/Basic.lean
@@ -0,0 +1,15 @@
+/-
+Copyright (c) 2020 Yakov Pechersky. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Yakov Pechersky
+
+! This file was ported from Lean 3 source module data.buffer.basic
+! leanprover-community/mathlib commit 2220b0cbab795e73674b8191170b0cc68c6b54a8
+! Please do not edit these lines, except to modify the commit id
+! if you have ported upstream changes.
+
+Porting note:
+As the parsing framework has completely changed in Lean 4
+there is no point porting these files directly.
+They can be rewritten from scratch as needed, just as for tactics.
+-/
