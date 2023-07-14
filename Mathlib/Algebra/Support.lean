a520d1b42b03d2e08fe3d28334c7c281a39b3425
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2023-07-14 13:57:26 +0200

    Update Mathlib/Algebra/Support.lean
    Co-authored-by: Anne Baanen <Vierkantor@users.noreply.github.com>

--- Mathlib/Algebra/Support.lean
+++ Mathlib/Algebra/Support.lean
@@ -218,10 +218,10 @@ theorem mulSupport_comp_eq (g : M → N) (hg : ∀ {x}, g x = 1 ↔ x = 1) (f : 
 #align function.support_comp_eq Function.support_comp_eq
 
 @[to_additive]
-theorem mulSupport_comp_eq_of_range_subset {g : M → N} {s : Set M}
-    (hg : ∀ {x}, x ∈ s → (g x = 1 ↔ x = 1)) {f : α → M} (hf : range f ⊆ s) :
+theorem mulSupport_comp_eq_of_range_subset {g : M → N} {f : α → M}
+    (hg : ∀ {x}, x ∈ range f → (g x = 1 ↔ x = 1)) :
     mulSupport (g ∘ f) = mulSupport f :=
-  Set.ext fun x ↦ not_congr <| by rw [Function.comp, hg (hf (mem_range_self x))]
+  Set.ext fun x ↦ not_congr <| by rw [Function.comp, hg (mem_range_self x)]
 
 @[to_additive]
 theorem mulSupport_comp_eq_preimage (g : β → M) (f : α → β) :
