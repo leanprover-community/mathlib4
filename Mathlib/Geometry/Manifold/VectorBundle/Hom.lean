bcd7faa57fba6051b652ee07a16b7490a132cd32
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2023-07-14 16:34:55 +0000

    chore: forward-port leanprover-community/mathlib#19128 (#5905)
    The changed proof was rewritten completely.
    
    There appears to have been a race between #5791 and #5727 that caused the commit hash to be out of date.

--- Mathlib/Geometry/Manifold/VectorBundle/Hom.lean
+++ Mathlib/Geometry/Manifold/VectorBundle/Hom.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Floris van Doorn
 
 ! This file was ported from Lean 3 source module geometry.manifold.vector_bundle.hom
-! leanprover-community/mathlib commit e473c3198bb41f68560cab68a0529c854b618833
+! leanprover-community/mathlib commit 8905e5ed90859939681a725b00f6063e65096d95
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
