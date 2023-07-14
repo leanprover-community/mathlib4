94a16e1b87ef2e8433b3a26cbb50d36cac3da7f1
Eric Wieser <wieser.eric@gmail.com>
2023-07-14 09:48:29 +0000

    fix

--- Mathlib/Data/List/Dedup.lean
+++ Mathlib/Data/List/Dedup.lean
@@ -101,9 +101,9 @@ theorem dedup_eq_cons (l : List α) (a : α) (l' : List α) :
     have : count a l.dedup ≤ 1 := nodup_iff_count_le_one.1 (nodup_dedup l) a
     rw [h, count_cons_self, add_le_iff_nonpos_left] at this
     exact not_le_of_lt (count_pos.2 ha) this
-  · have := @cons_head_tail α ⟨a⟩ _ (ne_nil_of_mem (mem_dedup.2 h.1))
+  · have := @List.cons_head!_tail α ⟨a⟩ _ (ne_nil_of_mem (mem_dedup.2 h.1))
     have hal : a ∈ l.dedup := mem_dedup.2 h.1
-    rw [← this, mem_cons_iff, or_iff_not_imp_right] at hal
+    rw [← this, mem_cons, or_iff_not_imp_right] at hal
     exact this ▸ h.2.2.symm ▸ cons_eq_cons.2 ⟨(hal (h.2.2.symm ▸ h.2.1)).symm, rfl⟩
 #align list.dedup_eq_cons List.dedup_eq_cons
 
