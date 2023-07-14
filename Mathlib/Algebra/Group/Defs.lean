bd831e060735ecd2986fdb9e3c0e16094a10e186
Floris van Doorn <fpvdoorn@gmail.com>
2023-07-14 09:35:20 +0200

    first custom instance priority

--- Mathlib/Algebra/Group/Defs.lean
+++ Mathlib/Algebra/Group/Defs.lean
@@ -579,6 +579,18 @@ class AddMonoid (M : Type u) extends AddSemigroup M, AddZeroClass M where
   nsmul_succ : ∀ (n : ℕ) (x), nsmul (n + 1) x = x + nsmul n x := by intros; rfl
 #align add_monoid AddMonoid
 
+/-
+If
+structure AB extends A
+structure AC extends A
+structure ABC extends AB, AC
+then AB.toA should have higher priority than AC.toA
+Todo: write linter.
+-/
+
+attribute [instance 150] AddSemigroup.toAdd
+attribute [instance 50] AddZeroClass.toAdd
+
 #align add_monoid.nsmul_zero' AddMonoid.nsmul_zero
 #align add_monoid.nsmul_succ' AddMonoid.nsmul_succ
 
