3ef7b4acb7a235910f1a4d9830ef509e311cd083
Sam <59202064+samvang@users.noreply.github.com>
2023-07-14 12:13:22 +0200

    getting to the point

--- Mathlib/NumberTheory/PellAlgorithm.lean
+++ Mathlib/NumberTheory/PellAlgorithm.lean
@@ -10,7 +10,7 @@ structure Triple : Type where
 deriving Repr
 
 /-- The floor of the positive root of the triple. -/
-def fpr (t : Triple) : ℤ := (-t.b + (discrim t.a t.b t.c).toNat.sqrt) / (2 * t.a)
+def fpr (t : Triple) : ℤ := (-t.b + (discrim t.a t.b t.c).sqrt) / (2 * t.a)
 
 /-- The quadratic form defined by the triple, and its fpr-translation. -/
 def qf (t : Triple) (x : ℝ) := t.a * x * x + t.b * x + t.c
@@ -37,7 +37,7 @@ def iterate_brouckner (t : Triple) (l : List (Triple × ℤ)) : (n : ℕ) → Li
       iterate_brouckner t ((t, fpr t) :: l) n
 
 -- Example
--- #eval iterate_brouckner ⟨1,0, (-19)⟩ [] 20
+#eval iterate_brouckner ⟨1,0, (-19)⟩ [] 20
 
 /-- Swap a and c in a triple. -/
 def swapac (t : Triple) : Triple := {a := t.c, b := t.b, c := t.a}
@@ -68,6 +68,106 @@ theorem brouckner_root (t: Triple) (z : ℝ) (hz : z ≠ fpr t) :
   qf t z = 0 ↔ qf (brouckner t) (1 / (z - fpr t)) = 0 := by
   rw [swapac_twice (brouckner t), ← swapac_root _ _ (sub_ne_zero.mpr hz),
   ←neg_qf_fpr_translate_eq_brouckner, neg_eq_zero,←root_iff_translate_root]
+
+/- First, I will show: if a z² + b z + c = 0 has
+a negative root z_- and
+a positive root z_+>1,
+and a>0, c<0, then
+a' z² + b' z + c' has a root in (-1,0) and a root>1, and a'>0, and c'<0.
+That c'<0 is trivial (because c' = - a). Since a>0, we see that a z² + b z + c is <0 for z in (z_-,z_+); since m is both < z_+ and >= 1 > z_-, we see that a m² + b m + c < 0, and so a'>0.-/
+
+structure Roots (t : Triple) : Type where
+  x1 : ℝ
+  x2 : ℝ
+  h1 : qf t x1 = 0
+  h2 : qf t x2 = 0
+
+/-- Computes the roots of `brouckner t` given the roots of `t`, provided that `t` has no integer
+roots. -/
+
+noncomputable def brouckner_roots (t : Triple) (r : Roots t)
+(hne1 : r.x1 ≠ fpr t) (hne2 : r.x2 ≠ fpr t) : Roots (brouckner t) :=
+  {x1 := 1 / (r.x2 - fpr t),
+   x2 := 1 / (r.x1 - fpr t),
+   h1 := ((brouckner_root t r.x2) hne2).mp r.h2,
+   h2 := ((brouckner_root t r.x1) hne1).mp r.h1}
+
+#check quadratic_eq_zero_iff
+
+lemma lem1 (T : Triple) (z : ℝ) (hz1 : qf T z = 0) (hz2 : 1 < z) (ha : T.a > 0) (hc : T.c < 0) :
+  0 < T.a * z + T.b := by sorry
+
+lemma lem2 (T : Triple) (z : ℝ) (hz1 : qf T z = 0) (hz2 : 1 < z) (ha : T.a > 0) (hc : T.c < 0)
+  (t : ℝ) (ht1 : t < z) (ht2 : 1 ≤ t) : (qf T t < 0) := by
+  rw [←hz1, qf, qf]
+  simp only [add_lt_add_iff_right]
+  rw [← add_mul, ← add_mul]
+  apply mul_lt_mul
+  simp only [add_lt_add_iff_right]
+  apply mul_lt_mul'
+  trivial
+  assumption
+  exact le_trans zero_le_one ht2
+  norm_cast
+  exact ht1.le
+  exact lt_of_lt_of_le zero_lt_one ht2
+  apply le_of_lt
+  apply lem1 T z hz1 hz2 ha hc
+
+lemma discrim_bound (T: Triple) (z : ℝ) (hz : qf T z = 0) :
+  (discrim T.a T.b T.c ≤ (2 * T.a * z + T.b) ^ 2) := by
+  rw [Real.rpow_two, add_sq, discrim]
+  ring_nf
+  simp only [Int.cast_sub, Int.cast_pow, Int.cast_mul, Int.int_cast_ofNat, tsub_le_iff_right]
+  rw [add_assoc,add_comm, add_assoc]
+  simp only [le_add_iff_nonneg_right]
+  apply le_of_eq
+  calc
+    0 = 4 * T.a * qf T z := by rw [hz, mul_zero]
+    _ = 4 * T.a * (T.a * z * z + T.b * z + T.c) := by rw [qf]
+    _ = ↑T.a * ↑T.c * 4 + (↑T.a * z * ↑T.b * 4 + ↑T.a ^ 2 * z ^ 2 * 4) := by
+      rw[Real.rpow_two, Real.rpow_two]; ring_nf
+  simp only [Real.rpow_two]
+
+lemma le_of_le_sq (x y : ℝ) (hx : 0 ≤ x) (hy : 0 ≤ y) (H : x * x ≤ y * y) : x ≤ y :=
+  (mul_self_le_mul_self_iff hx hy).mpr H
+
+lemma square_sqrt_le_self_of_nonneg (x : ℤ) (hx : 0 ≤ x) : x.sqrt * x.sqrt ≤ x := by
+  rw [Int.sqrt]
+  obtain ⟨n, hn⟩ := Int.NonNeg.elim hx
+  simp only [sub_zero] at hn
+  rw [hn]
+  simp only [Int.toNat_ofNat]
+  norm_cast
+  exact Nat.sqrt_le n
+
+lemma fpr_le_pos_root (T : Triple) (r : Roots T) (h : r.x1 ≤ r.x2) (ha : 0 < T.a) :
+  (fpr T) ≤ r.x2 := by
+  have key : (discrim T.a T.b T.c).sqrt ≤ 2 * T.a * r.x2 + T.b
+  apply le_of_le_sq
+  norm_cast; apply Int.sqrt_nonneg
+  sorry
+  norm_num
+  rw [←Int.cast_mul]
+  have := Int.cast_le.mpr (square_sqrt_le_self_of_nonneg ?_ ?_)
+  apply le_trans  (square_sqrt_le_self_of_nonneg)
+  rw [←Int.cast_le]
+
+
+  --   let s := (discrim t.a t.b t.c : ℝ).sqrt
+  --   have : s * s = discrim t.a t.b t.c :=
+  --     by apply (sqrt_eq_iff_mul_self_eq)
+  --   x = (-b + s) / (2 * a) ∨ x = (-b - s) / (2 * a)
+  -- sorry
+
+lemma brouckner_root_signs (t : Triple) (r : Roots t) (h1 : r.x1 < 0) (h2 : r.x2 > 1)
+  (h3 : 0 < t.a) (h4 : t.c < 0)  : 0 < (brouckner t).a := by
+  have ne1 : r.x1 ≠ fpr t := by sorry
+  have ne2 : r.x2 ≠ fpr t := by sorry
+  have br : Roots (brouckner t) := brouckner_roots t r ne1 ne2;
+
+
+    done
 
 end PellAlg
 
