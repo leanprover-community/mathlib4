694220dee81bd4d7357f9498dc4c46f97e143b33
Yury G. Kudryashov <urkud@urkud.name>
2023-07-15 22:04:07 -0500

    Update imports

--- Mathlib.lean
+++ Mathlib.lean
@@ -1884,6 +1884,7 @@ import Mathlib.Geometry.Euclidean.Angle.Unoriented.RightAngle
 import Mathlib.Geometry.Euclidean.Basic
 import Mathlib.Geometry.Euclidean.Circumcenter
 import Mathlib.Geometry.Euclidean.Inversion.Basic
+import Mathlib.Geometry.Euclidean.Inversion.Calculus
 import Mathlib.Geometry.Euclidean.Inversion.ImageHyperplane
 import Mathlib.Geometry.Euclidean.MongePoint
 import Mathlib.Geometry.Euclidean.PerpBisector
