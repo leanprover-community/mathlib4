75a6547ac35c50ec07b15b9bbe6d646067fa0124
Scott Morrison <scott.morrison@gmail.com>
2023-07-17 08:25:42 +1000

    lint

--- Mathlib/Init/Data/Sigma/Lex.lean
+++ Mathlib/Init/Data/Sigma/Lex.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2016 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Author: Leonardo de Moura
+Authors: Leonardo de Moura
 
 ! This file was ported from Lean 3 source module init.data.sigma.lex
 ! leanprover-community/lean commit 9af482290ef68e8aaa5ead01aa7b09b7be7019fd
@@ -9,6 +9,10 @@ Author: Leonardo de Moura
 ! if you have ported upstream changes.
 -/
 import Mathlib.Init.Data.Sigma.Basic
+
+/-!
+# Facts about `PSigma.Lex` from Lean 3 core.
+-/
 
 universe u v
 
