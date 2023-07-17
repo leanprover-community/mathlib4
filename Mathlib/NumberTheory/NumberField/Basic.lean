07325e1ad53fe8dd3cbf33102496e3356a969e06
Xavier-François Roblot <46200072+xroblot@users.noreply.github.com>
2023-07-18 08:06:03 +0900

    Update

--- Mathlib/NumberTheory/NumberField/Basic.lean
+++ Mathlib/NumberTheory/NumberField/Basic.lean
@@ -171,6 +171,21 @@ theorem integralBasis_apply (i : Free.ChooseBasisIndex ℤ (𝓞 K)) :
   Basis.localizationLocalization_apply ℚ (nonZeroDivisors ℤ) K (RingOfIntegers.basis K) i
 #align number_field.integral_basis_apply NumberField.integralBasis_apply
 
+theorem mem_span_integralBasis {x : K} :
+    x ∈ Submodule.span ℤ (Set.range (integralBasis K)) ↔ x ∈ 𝓞 K := by
+  suffices Submodule.span ℤ (Set.range (integralBasis K)) =
+      AddSubgroup.toIntSubmodule (𝓞 K).toSubring.toAddSubgroup by rw [this]; rfl
+  convert congrArg
+    (Submodule.map (Submodule.subtype (AddSubgroup.toIntSubmodule (𝓞 K).toSubring.toAddSubgroup)))
+    (Basis.span_eq (RingOfIntegers.basis K))
+  · rw [Submodule.map_span]
+    congr
+    ext
+    simp only [Set.mem_range, integralBasis_apply, Submodule.coeSubtype, Set.mem_image,
+      exists_exists_eq_and]
+    rfl
+  · simp only [Submodule.map_top, Submodule.range_subtype]
+
 theorem RingOfIntegers.rank : FiniteDimensional.finrank ℤ (𝓞 K) = FiniteDimensional.finrank ℚ K :=
   IsIntegralClosure.rank ℤ ℚ K (𝓞 K)
 #align number_field.ring_of_integers.rank NumberField.RingOfIntegers.rank
