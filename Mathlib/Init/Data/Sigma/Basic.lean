0fc15a2cfca6dee302b10cf0de141bc4b8c09a5d
Parcly Taxel <reddeloostw@gmail.com>
2023-07-17 09:37:11 +0800

    restore

--- Mathlib/Init/Data/Sigma/Basic.lean
+++ Mathlib/Init/Data/Sigma/Basic.lean
@@ -1,48 +1,26 @@
 /-
 Copyright (c) 2014 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Author: Leonardo de Moura, Jeremy Avigad, Floris van Doorn
+Authors: Leonardo de Moura, Jeremy Avigad, Floris van Doorn
 
 ! This file was ported from Lean 3 source module init.data.sigma.basic
 ! leanprover-community/lean commit 87932f1c56b7c05aefd2810d05e69da9500a0064
 ! Please do not edit these lines, except to modify the commit id
 ! if you have ported upstream changes.
 -/
-prelude
 import Mathlib.Init.Logic
-import Mathlib.Init.Wf
 
-universe u v
+/-!
+# Lemmas about `Sigma` from Lean 3 core.
+-/
 
-#print ex_of_psig /-
-theorem ex_of_psig {α : Type u} {p : α → Prop} : (Σ' x, p x) → ∃ x, p x
+theorem ex_of_psig {p : α → Prop} : (Σ' x, p x) → ∃ x, p x
   | ⟨x, hx⟩ => ⟨x, hx⟩
-#align ex_of_psig ex_of_psig
--/
 
-section
+protected theorem Sigma.eq {β : α → Type v} : ∀ {p₁ p₂ : Σ a, β a} (h₁ : p₁.1 = p₂.1),
+    (Eq.recOn h₁ p₁.2 : β p₂.1) = p₂.2 → p₁ = p₂
+  | ⟨_, _⟩, _, rfl, rfl => rfl
 
-variable {α : Type u} {β : α → Type v}
-
-#print Sigma.eq /-
-protected theorem Sigma.eq :
-    ∀ {p₁ p₂ : Σ a : α, β a} (h₁ : p₁.1 = p₂.1), (Eq.recOn h₁ p₁.2 : β p₂.1) = p₂.2 → p₁ = p₂
-  | ⟨a, b⟩, ⟨a, b⟩, rfl, rfl => rfl
-#align sigma.eq Sigma.eq
--/
-
-end
-
-section
-
-variable {α : Sort u} {β : α → Sort v}
-
-#print PSigma.eq /-
-protected theorem PSigma.eq :
-    ∀ {p₁ p₂ : PSigma β} (h₁ : p₁.1 = p₂.1), (Eq.recOn h₁ p₁.2 : β p₂.1) = p₂.2 → p₁ = p₂
-  | ⟨a, b⟩, ⟨a, b⟩, rfl, rfl => rfl
-#align psigma.eq PSigma.eq
--/
-
-end
-
+protected theorem PSigma.eq {β : α → Sort v} : ∀ {p₁ p₂ : Σ' a, β a} (h₁ : p₁.1 = p₂.1),
+    (Eq.recOn h₁ p₁.2 : β p₂.1) = p₂.2 → p₁ = p₂
+  | ⟨_, _⟩, _, rfl, rfl => rfl
