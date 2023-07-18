5b5c7f09714ef09a7d009b7928e538a42ef67199
dagurtomas <dagurtomas@gmail.com>
2023-07-18 10:31:17 +0000

    add line to Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -2953,6 +2953,7 @@ import Mathlib.Topology.Category.Locale
 import Mathlib.Topology.Category.Profinite.AsLimit
 import Mathlib.Topology.Category.Profinite.Basic
 import Mathlib.Topology.Category.Profinite.CofilteredLimit
+import Mathlib.Topology.Category.Profinite.Nobeling
 import Mathlib.Topology.Category.Profinite.Projective
 import Mathlib.Topology.Category.TopCat.Adjunctions
 import Mathlib.Topology.Category.TopCat.Basic
