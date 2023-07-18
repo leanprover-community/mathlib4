732fbd5fc1459b446b2c52922f4a1e265faaede1
adomani <adomani@gmail.com>
2023-07-18 21:57:07 +0200

    More tests with Ring

--- test/ComputeDegree.lean
+++ test/ComputeDegree.lean
@@ -11,6 +11,9 @@ example : natDegree (- C z * X ^ 5 + (monomial 2 5) ^ 2 - 0 + 1 + IntCast.intCas
     NatCast.natCast 1 + (z : Int[X]) + (n : Int[X]) + f) ≤ 5 := by
   compute_degree_le!
 
+example [Semiring R] : natDegree (OfNat.ofNat 0 : R[X]) ≤ 0 := by
+  compute_degree_le
+
 /--  This example flows through all the matches in `direct` with a `degree` goal. -/
 example : degree (- C z * X ^ 5 + (monomial 2 5) ^ 2 - 0 + 1 + IntCast.intCast 1 +
     NatCast.natCast 1 + (z : Int[X]) + (n : Int[X]) + f) ≤ 5 := by
@@ -60,6 +63,48 @@ example (h : n ≤ 5) : natDegree (monomial n (5 + n)) ≤ 5 := by
 
 --  Expr.fvar
 example {f : Nat[X]} : natDegree f ≤ natDegree f := by
+  compute_degree_le
+
+variable [Ring R]
+
+--  OfNat.ofNat 0
+example : natDegree (0 : R[X]) ≤ 5 := by
+  compute_degree_le!
+
+--  OfNat.ofNat (non-zero)
+example : natDegree (1 : R[X]) ≤ 5 := by
+  compute_degree_le!
+
+--  NatCast.natCast
+example : natDegree (NatCast.natCast 4 : R[X]) ≤ 5 := by
+  compute_degree_le!
+
+--  Nat.cast
+example : natDegree (n : R[X]) ≤ 5 := by
+  compute_degree_le!
+
+--  IntCast.intCast
+example : natDegree (IntCast.intCast 4 : R[X]) ≤ 5 := by
+  compute_degree_le!
+
+--  Int.cast
+example : natDegree (z : R[X]) ≤ 5 := by
+  compute_degree_le!
+
+--  Polynomial.X
+example : natDegree (X : R[X]) ≤ 5 := by
+  compute_degree_le!
+
+--  Polynomial.C
+example : natDegree (C n) ≤ 5 := by
+  compute_degree_le!
+
+--  Polynomial.monomial
+example (h : n ≤ 5) : natDegree (monomial n (5 + n : R)) ≤ 5 := by
+  compute_degree_le!
+
+--  Expr.fvar
+example {f : R[X]} : natDegree f ≤ natDegree f := by
   compute_degree_le
 
 end native_mathlib4_tests
