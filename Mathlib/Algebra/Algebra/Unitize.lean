f3f065ab009d09cfe6115444503d2c6d4c885712
Jireh Loreaux <loreaujy@gmail.com>
2023-07-19 15:39:24 -0500

    fix import

--- Mathlib/Algebra/Algebra/Unitize.lean
+++ Mathlib/Algebra/Algebra/Unitize.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jireh Loreaux
 -/
 
-import Mathlib.Algebra.Algebra.NonUnitalSubalgebra.Basic
+import Mathlib.Algebra.Algebra.NonUnitalSubalgebra
 import Mathlib.Algebra.Star.Subalgebra
 import Mathlib.Algebra.Algebra.Unitization
 import Mathlib.Algebra.Star.NonUnitalSubalgebra
