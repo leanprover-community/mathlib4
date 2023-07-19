d032a14d0b6d02905536854fae548963c2fdfa75
Mohanad Ahmed <m.a.m.elhassan@gmail.com>
2023-07-19 10:29:45 +0300

    Fit the 100 char line limit

--- Mathlib/LinearAlgebra/Matrix/SVD/ColumnRowBlocks.lean
+++ Mathlib/LinearAlgebra/Matrix/SVD/ColumnRowBlocks.lean
@@ -46,7 +46,8 @@ def toColumns₂ (A: Matrix M (N₁ ⊕ N₂) R): Matrix M N₂ R :=  of fun i j
 def toRows₁ (A: Matrix (M₁ ⊕ M₂) N R): Matrix M₁ N R :=  of fun i j => (A (Sum.inl i) j)
 def toRows₂ (A: Matrix (M₁ ⊕ M₂) N R): Matrix M₂ N R :=  of fun i j => (A (Sum.inr i) j)
 
-lemma fromRows_mul_fromColumns (A₁: Matrix M₁ N R)(A₂: Matrix M₂ N R) (B₁: Matrix N N₁ R)(B₂: Matrix N N₂ R):
+lemma fromRows_mul_fromColumns (A₁: Matrix M₁ N R) (A₂: Matrix M₂ N R)
+  (B₁: Matrix N N₁ R) (B₂: Matrix N N₂ R) :
   (fromRows A₁ A₂) ⬝ (fromColumns B₁ B₂) = fromBlocks (A₁⬝B₁) (A₁⬝B₂) (A₂⬝B₁) (A₂⬝B₂) := by
   funext i j
   rw [fromRows, fromColumns]
@@ -65,11 +66,13 @@ lemma fromColumns_mul_fromRows (A₁: Matrix M N₁ R)(A₂: Matrix M N₂ R)
 
 lemma fromColumns_mul_fromBlocks (A₁: Matrix M M₁ R) (A₂: Matrix M M₂ R)
   (B₁₁: Matrix M₁ N₁ R)(B₁₂: Matrix M₁ N₂ R)(B₂₁: Matrix M₂ N₁ R)(B₂₂: Matrix M₂ N₂ R):
-  (fromColumns A₁ A₂) ⬝ fromBlocks B₁₁ B₁₂ B₂₁ B₂₂ = fromColumns (A₁⬝B₁₁ + A₂⬝B₂₁) (A₁⬝B₁₂ + A₂⬝B₂₂) := by
+  (fromColumns A₁ A₂) ⬝ fromBlocks B₁₁ B₁₂ B₂₁ B₂₂ =
+    fromColumns (A₁⬝B₁₁ + A₂⬝B₂₁) (A₁⬝B₁₂ + A₂⬝B₂₂) := by
   funext i j
   rw [fromColumns, fromColumns, fromBlocks]
   cases j
-  all_goals simp only [of_apply, add_apply, Fintype.sum_sum_type, Sum.elim_inl, Sum.elim_inr, mul_apply]
+  all_goals simp only [of_apply, add_apply, Fintype.sum_sum_type, Sum.elim_inl,
+    Sum.elim_inr, mul_apply]
 
 lemma fromBlocks_mul_fromRows (A₁: Matrix N₁ N R) (A₂: Matrix N₂ N R)
   (B₁₁: Matrix M₁ N₁ R)(B₁₂: Matrix M₁ N₂ R)(B₂₁: Matrix M₂ N₁ R)(B₂₂: Matrix M₂ N₂ R):
@@ -77,7 +80,8 @@ lemma fromBlocks_mul_fromRows (A₁: Matrix N₁ N R) (A₂: Matrix N₂ N R)
   funext i j
   rw [fromRows, fromRows, fromBlocks]
   cases i
-  all_goals simp only [of_apply, add_apply, Fintype.sum_sum_type, Sum.elim_inl, Sum.elim_inr, mul_apply]
+  all_goals simp only [of_apply, add_apply, Fintype.sum_sum_type, Sum.elim_inl, Sum.elim_inr,
+    mul_apply]
 
 lemma fromColumns_mul_fromRows_eq_one_comm (e: N ≃ N₁ ⊕ N₂)
   (A₁: Matrix N N₁ R)(A₂: Matrix N N₂ R)(B₁: Matrix N₁ N R)(B₂: Matrix N₂ N R):
@@ -95,7 +99,8 @@ lemma fromColumns_mul_fromRows_eq_one_comm (e: N ≃ N₁ ⊕ N₂)
     exact Function.bijective_id )
 
 lemma equiv_compl_fromColumns_mul_fromRows_eq_one_comm (p: N → Prop)[DecidablePred p]
-  (A₁: Matrix N {i // p i} R)(A₂: Matrix N {i // ¬p i} R)(B₁: Matrix {i // p i} N R)(B₂: Matrix {i // ¬p i} N R):
+  (A₁: Matrix N {i // p i} R) (A₂: Matrix N {i // ¬p i} R)
+    (B₁: Matrix {i // p i} N R) (B₂: Matrix {i // ¬p i} N R):
   fromColumns A₁ A₂ ⬝ fromRows B₁ B₂ = 1 ↔ fromRows B₁ B₂ ⬝ fromColumns A₁ A₂ = 1 := by
   let e := Equiv.sumCompl p
   exact fromColumns_mul_fromRows_eq_one_comm (id e.symm) A₁ A₂ B₁ B₂
