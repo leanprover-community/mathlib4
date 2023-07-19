7458516147975424cf2e2134bfa4a408591fa18f
Scott Morrison <scott.morrison@gmail.com>
2023-07-19 11:57:18 +1000

    reduce import

--- Mathlib/Algebra/Order/Sub/Prod.lean
+++ Mathlib/Algebra/Order/Sub/Prod.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison
 -/
 import Mathlib.Algebra.Group.Prod
-import Mathlib.Algebra.Order.Sub.Canonical
+import Mathlib.Algebra.Order.Sub.Basic
 
 /-!
 # Products of `OrderedSub` types.
