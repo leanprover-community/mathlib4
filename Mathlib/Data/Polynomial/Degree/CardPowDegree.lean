af0439deb2a285e585d506932939b6151024e9e1
Antoine Chambert-Loir <antoine.chambert-loir@math.univ-paris-diderot.fr>
2023-07-22 23:15:26 +0200

    restore proof of cardPowDegree_zero by rfl

--- Mathlib/Data/Polynomial/Degree/CardPowDegree.lean
+++ Mathlib/Data/Polynomial/Degree/CardPowDegree.lean
@@ -81,9 +81,9 @@ theorem cardPowDegree_apply (p : Fq[X]) :
   rfl
 #align polynomial.card_pow_degree_apply Polynomial.cardPowDegree_apply
 
-@[simp 1001]
-theorem cardPowDegree_zero : cardPowDegree (0 : Fq[X]) = 0 := by
-  simp only [map_zero]
+-- @[simp] -- Porting note: simp can prove this
+theorem cardPowDegree_zero : cardPowDegree (0 : Fq[X]) = 0 := rfl
+-- was: if_pos rfl
 #align polynomial.card_pow_degree_zero Polynomial.cardPowDegree_zero
 
 @[simp]
