a72aef558d8ebf69f9bd3e36c04b9ec1e64ab8fb
Mohanad Ahmed <m.a.m.elhassan@gmail.com>
2023-07-22 03:38:33 +0300

    @kbuzzard and @alexjbest style review comments

--- Mathlib/Data/Matrix/Rank.lean
+++ Mathlib/Data/Matrix/Rank.lean
@@ -280,22 +280,19 @@ section MulNonsingInv
 /-! ### Lemmas about pre or post multiplying by an invertible matrix. -/
 
 /-- Pre (Left) multiplying by an invertible matrix does not change the rank -/
-lemma rank_mul_IsUnit {m n R: Type}
-  [Fintype n][DecidableEq n][CommRing R]
-  (A: Matrix n n R)(B: Matrix m n R)(hA: IsUnit A.det):
-  (B⬝A).rank = B.rank := by
+lemma rank_mul_IsUnit {m n R: Type} [Fintype n] [DecidableEq n] [CommRing R]
+    (A: Matrix n n R) (B: Matrix m n R) (hA: IsUnit A.det) :
+    (B ⬝ A).rank = B.rank := by
   rw [Matrix.rank, mulVecLin_mul, LinearMap.range_comp_of_range_eq_top, ←Matrix.rank ]
-  rw [LinearMap.range_eq_top]
-  intros x
+  rw [LinearMap.range_eq_top] -- On separate line since implicit rfl works in prev line
+  intro x
   use ((A⁻¹).mulVecLin x)
-  rw [mulVecLin_apply, mulVecLin_apply, mulVec_mulVec, mul_nonsing_inv, one_mulVec]
-  exact hA
+  rwa [mulVecLin_apply, mulVecLin_apply, mulVec_mulVec, mul_nonsing_inv, one_mulVec]
 
 /-- Post (right) multiplying by an invertible matrix does not change the rank -/
-lemma rank_IsUnit_mul {m n R: Type}
-  [Fintype m][Fintype n][DecidableEq m][Field R]
-  (A: Matrix m m R)(B: Matrix m n R)(hA: IsUnit A.det):
-  (A⬝B).rank = B.rank := by
+lemma rank_IsUnit_mul {m n R: Type} [Fintype m] [Fintype n] [DecidableEq m] [Field R]
+    (A: Matrix m m R) (B: Matrix m n R) (hA: IsUnit A.det):
+    (A ⬝ B).rank = B.rank := by
   suffices h: LinearMap.ker ((A⬝B).mulVecLin) = LinearMap.ker (B.mulVecLin)
   have hB := LinearMap.finrank_range_add_finrank_ker (B.mulVecLin)
   rw [← LinearMap.finrank_range_add_finrank_ker ((A⬝B).mulVecLin), h, add_left_inj] at hB
@@ -304,9 +301,8 @@ lemma rank_IsUnit_mul {m n R: Type}
   rw [LinearMap.ker_eq_bot]
   simp_rw [Function.Injective, mulVecLin_apply]
   intros x y h
-  replace h := congr_arg (fun x => Matrix.mulVec (A⁻¹) x)  h
-  simp_rw [mulVec_mulVec, nonsing_inv_mul A hA, one_mulVec] at h
-  exact h
+  apply_fun Matrix.mulVec (A⁻¹) at h
+  simpa [mulVec_mulVec, nonsing_inv_mul A hA, one_mulVec] using h
 
 end MulNonsingInv
 
