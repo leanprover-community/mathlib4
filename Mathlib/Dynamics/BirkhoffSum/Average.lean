72e648dc4b026124761da25e6f8e8fd6e106010d
Yury G. Kudryashov <urkud@urkud.name>
2023-07-22 18:45:43 -0500

    Snapshot

---
+++ Mathlib/Dynamics/BirkhoffSum/Average.lean
@@ -0,0 +1,48 @@
+import Mathlib.Dynamics.BirkhoffSum.Basic
+import Mathlib.Algebra.Module.Basic
+
+section birkhoffAverage
+
+variable (R : Type _) {α M : Type _} [DivisionSemiring R] [AddCommMonoid M] [Module R M]
+
+/-- The average value of `g` on the first `n` points of the orbit of `x` under `f`,
+i.e. the Birkhoff sum `∑ k in Finset.range n, g (f^[k] x)` divided by `n`.
+
+This average appears in many ergodic theorems
+which say that `(birkhoffAverage R f g · x)`
+converges to the "space average" `⨍ x, g x ∂μ` as `n → ∞`.
+
+We use an auxiliary `[DivisionSemiring R]` to define division by `n`.
+However, the definition does not depend on the choice of `R`,
+see `birkhoffAverage_congr_ring`. -/
+def birkhoffAverage (f : α → α) (g : α → M) (n : ℕ) (x : α) : M := (n : R)⁻¹ • birkhoffSum f g n x
+
+theorem birkhoffAverage_zero (f : α → α) (g : α → M) (x : α) :
+    birkhoffAverage R f g 0 x = 0 := by simp [birkhoffAverage]
+
+@[simp] theorem birkhoffAverage_zero' (f : α → α) (g : α → M) : birkhoffAverage R f g 0 = 0 :=
+  funext <| birkhoffAverage_zero _ _ _
+
+theorem birkhoffAverage_one (f : α → α) (g : α → M) (x : α) :
+    birkhoffAverage R f g 1 x = g x := by simp [birkhoffAverage]
+
+@[simp]
+theorem birkhoffAverage_one' (f : α → α) (g : α → M) : birkhoffAverage R f g 1 = g :=
+  funext <| birkhoffAverage_one R f g
+
+theorem map_birkhoffAverage (S : Type _) {F N : Type _}
+    [DivisionSemiring S] [AddCommMonoid N] [Module S N]
+    [AddMonoidHomClass F M N] (g' : F) (f : α → α) (g : α → M) (n : ℕ) (x : α) :
+    g' (birkhoffAverage R f g n x) = birkhoffAverage S f (g' ∘ g) n x := by
+  simp only [birkhoffAverage, map_inv_nat_cast_smul g' R S, map_birkhoffSum]
+
+theorem birkhoffAverage_congr_ring (S : Type _) [DivisionSemiring S] [Module S M]
+    (f : α → α) (g : α → M) (n : ℕ) (x : α) :
+    birkhoffAverage R f g n x = birkhoffAverage S f g n x :=
+  map_birkhoffAverage R S (AddMonoidHom.id M) f g n x
+
+theorem birkhoffAverage_congr_ring' (S : Type _) [DivisionSemiring S] [Module S M] :
+    birkhoffAverage (α := α) (M := M) R = birkhoffAverage S := by
+  ext; apply birkhoffAverage_congr_ring
+
+end birkhoffAverage
