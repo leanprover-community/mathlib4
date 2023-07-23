669310d6e97816c2b43d1318eb4f414c2d797428
ADedecker <anatolededecker@gmail.com>
2023-07-23 18:08:21 +0300

    Tmp

--- Mathlib/Topology/ProperMap.lean
+++ Mathlib/Topology/ProperMap.lean
@@ -38,16 +38,24 @@ theorem IsClosedMap.mapClusterPt_iff_lift'_closure [TopologicalSpace X] [Topolog
 variable [TopologicalSpace X] [TopologicalSpace Y] [TopologicalSpace Z] [TopologicalSpace W]
   {f : X → Y}
 
+/-- A map `f : X → Y` between two topological spaces is said to be **proper** if it is continuous
+and, for all `ℱ : Filter X`, any cluster point of `map f ℱ` is the image by `f` of a cluster point
+of `f`. -/
 structure IsProperMap (f : X → Y) extends Continuous f : Prop where
+  /-- By definition, if `f` is a proper map and `ℱ` is any filter on `X`, then any cluster point of
+  `map f ℱ` is the image by `f` of some cluster point of `ℱ`. -/
   clusterPt_of_mapClusterPt :
     ∀ ⦃ℱ : Filter X⦄, ∀ ⦃y : Y⦄, MapClusterPt y ℱ f → ∃ x, f x = y ∧ ClusterPt x ℱ
 
+/-- By definition, a proper map is continuous. -/
 lemma IsProperMap.continuous (h : IsProperMap f) : Continuous f := h.toContinuous
 
+/-- Definition of proper maps. -/
 lemma isProperMap_iff_clusterPt : IsProperMap f ↔ Continuous f ∧
     ∀ ⦃ℱ : Filter X⦄, ∀ ⦃y : Y⦄, MapClusterPt y ℱ f → ∃ x, f x = y ∧ ClusterPt x ℱ :=
   ⟨fun h' ↦ ⟨h'.continuous, h'.clusterPt_of_mapClusterPt⟩, fun ⟨h, h'⟩ ↦ ⟨h, h'⟩⟩
 
+/-- An homeomorphism is proper. -/
 lemma Homeomorph.isProperMap (e : X ≃ₜ Y) : IsProperMap e := by
   rw [isProperMap_iff_clusterPt]
   refine ⟨e.continuous, fun ℱ y ↦ ?_⟩
@@ -55,9 +63,12 @@ lemma Homeomorph.isProperMap (e : X ≃ₜ Y) : IsProperMap e := by
     ← e.coe_toEquiv, ← e.eq_symm_apply, exists_eq_left]
   exact id
 
+/-- The identity is proper. -/
 lemma isProperMap_id : IsProperMap (id : X → X) := (Homeomorph.refl X).isProperMap
 
+/-- A proper map is closed. -/
 lemma IsProperMap.isClosedMap (h : IsProperMap f) : IsClosedMap f := by
+  rw [IsClosedMap_iff_clusterPt]
   intro A hA
   rw [isClosed_iff_clusterPt] at hA ⊢
   intro y hy
@@ -111,18 +122,22 @@ lemma IsProperMap.pi_map {X Y : ι → Type _} [∀ i, TopologicalSpace (X i)]
     refine ⟨x, funext hxy, ?_⟩
     rwa [nhds_pi, le_pi]
 
+lemma IsProperMap.isCompact_preimage (h : IsProperMap f) {K : Set Y} (hK : IsCompact K) :
+    IsCompact (f ⁻¹' K) := by
+  rw [isCompact_iff_ultrafilter_le_nhds]
+  intro 𝒰 h𝒰
+  rw [← comap_principal, ← map_le_iff_le_comap, ← Ultrafilter.coe_map] at h𝒰
+  rcases hK.ultrafilter_le_nhds _ h𝒰 with ⟨y, hyK, hy⟩
+  rcases h.ultrafilter_le_nhds_of_tendsto hy with ⟨x, rfl, hx⟩
+  exact ⟨x, hyK, hx⟩
+
 theorem isProperMap_iff_isClosedMap_and_compact_fibers :
     IsProperMap f ↔ Continuous f ∧ IsClosedMap f ∧ ∀ y, IsCompact (f ⁻¹' {y}) := by
   constructor <;> intro H
   -- Note: In Bourbaki, the direct implication is proved by going through universally closed maps.
-  -- We could do the same (using a `TFAE` cycle) but the direct proof is nice enough already
-  -- so we don't bother with that.
-  · refine ⟨H.continuous, H.isClosedMap, fun y ↦ ?_⟩
-    rw [isCompact_iff_ultrafilter_le_nhds]
-    intro 𝒰 h𝒰
-    rw [← comap_principal, ← map_le_iff_le_comap, ← Ultrafilter.coe_map] at h𝒰
-    rcases isCompact_singleton.ultrafilter_le_nhds _ h𝒰 with ⟨y, rfl, hy⟩
-    exact H.ultrafilter_le_nhds_of_tendsto hy
+  -- We could do the same (using a `TFAE` cycle) but proving it directly from
+  -- `IsProperMap.isCompact_preimage` is nice enough already so we don't bother with that.
+  · exact ⟨H.continuous, H.isClosedMap, fun y ↦ H.isCompact_preimage isCompact_singleton⟩
   · rw [isProperMap_iff_clusterPt]
     refine ⟨H.1, fun 𝓕 y hy ↦ ?_⟩
     rw [H.2.1.mapClusterPt_iff_lift'_closure H.1] at hy
@@ -130,6 +145,40 @@ theorem isProperMap_iff_isClosedMap_and_compact_fibers :
     refine ⟨x, hxy, ?_⟩
     rw [← clusterPt_lift'_closure_iff]
     exact hx.mono inf_le_left
+
+theorem isProperMap_iff_isClosedMap_and_tendsto_cofinite [T1Space Y] :
+    IsProperMap f ↔ Continuous f ∧ IsClosedMap f ∧ Tendsto f (cocompact X) cofinite := by
+  simp_rw [isProperMap_iff_isClosedMap_and_compact_fibers, Tendsto,
+    le_cofinite_iff_compl_singleton_mem, mem_map, preimage_compl]
+  refine and_congr_right fun f_cont ↦ and_congr_right fun _ ↦
+    ⟨fun H y ↦ (H y).compl_mem_cocompact, fun H y ↦ ?_⟩
+  rcases mem_cocompact.mp (H y) with ⟨K, hK, hKy⟩
+  exact isCompact_of_isClosed_subset hK (isClosed_singleton.preimage f_cont)
+    (compl_le_compl_iff_le.mp hKy)
+
+theorem isProperMap_iff_isCompact_preimage [T2Space Y] [LocallyCompactSpace Y] :
+    IsProperMap f ↔ Continuous f ∧ ∀ ⦃K⦄, IsCompact K → IsCompact (f ⁻¹' K) := by
+  constructor <;> intro H
+  · exact ⟨H.continuous, fun K hK ↦ H.isCompact_preimage hK⟩
+  · rw [isProperMap_iff_ultrafilter]
+    refine ⟨H.1, fun 𝒰 y hy ↦ ?_⟩
+    rcases exists_compact_mem_nhds y with ⟨K, hK, hKy⟩
+    have : 𝒰 ≤ 𝓟 (f ⁻¹' K) := by
+      simpa only [← comap_principal, ← tendsto_iff_comap] using
+        hy.mono_right (le_principal_iff.mpr hKy)
+    rcases (H.2 hK).ultrafilter_le_nhds _ this with ⟨x, -, hx⟩
+    rw [← tendsto_id'] at hx
+    refine ⟨x, tendsto_nhds_unique ((H.1.tendsto _).comp hx) hy, hx⟩
+
+theorem isProperMap_iff_tendsto_cocompact [T2Space Y] [LocallyCompactSpace Y] :
+    IsProperMap f ↔ Continuous f ∧ Tendsto f (cocompact X) (cocompact Y) := by
+  simp_rw [isProperMap_iff_isCompact_preimage, hasBasis_cocompact.tendsto_right_iff,
+    ← mem_preimage, eventually_mem_set, preimage_compl]
+  refine and_congr_right fun f_cont ↦
+    ⟨fun H K hK ↦ (H hK).compl_mem_cocompact, fun H K hK ↦ ?_⟩
+  rcases mem_cocompact.mp (H K hK) with ⟨K', hK', hK'y⟩
+  exact isCompact_of_isClosed_subset hK' (hK.isClosed.preimage f_cont)
+    (compl_le_compl_iff_le.mp hK'y)
 
 theorem IsProperMap.universally_closed (Z) [TopologicalSpace Z] (h : IsProperMap f) :
     IsClosedMap (Prod.map f id : X × Z → Y × Z) :=
@@ -148,7 +197,7 @@ theorem isProperMap_iff_isClosedMap_filter {X : Type u} {Y : Type v} [Topologica
     have : (y, ↑𝒰) ∈ Prod.map f id '' F :=
       this.mem_of_tendsto (hy.prod_mk_nhds (Filter.tendsto_pure_self (𝒰 : Filter X)))
         (eventually_of_forall fun x ↦ ⟨⟨x, pure x⟩, subset_closure rfl, rfl⟩)
-    rcases this with ⟨⟨x, _⟩, hx, ⟨_, _⟩⟩ -- I don't even understand how this works!
+    rcases this with ⟨⟨x, _⟩, hx, ⟨_, _⟩⟩
     refine ⟨x, rfl, fun U hU ↦ Ultrafilter.compl_not_mem_iff.mp fun hUc ↦ ?_⟩
     rw [mem_closure_iff_nhds] at hx
     rcases hx (U ×ˢ {𝒢 | Uᶜ ∈ 𝒢}) (prod_mem_nhds hU (isOpen_setOf_mem.mem_nhds hUc)) with
@@ -168,18 +217,12 @@ theorem isProperMap_iff_isClosedMap_ultrafilter {X : Type u} {Y : Type v} [Topol
     have : (y, 𝒰) ∈ Prod.map f id '' F :=
       this.mem_of_tendsto (hy.prod_mk_nhds (Ultrafilter.tendsto_pure_self 𝒰))
         (eventually_of_forall fun x ↦ ⟨⟨x, pure x⟩, subset_closure rfl, rfl⟩)
-    rcases this with ⟨⟨x, _⟩, hx, ⟨_, _⟩⟩ -- I don't even understand how this works!
+    rcases this with ⟨⟨x, _⟩, hx, ⟨_, _⟩⟩
     refine ⟨x, rfl, fun U hU ↦ Ultrafilter.compl_not_mem_iff.mp fun hUc ↦ ?_⟩
     rw [mem_closure_iff_nhds] at hx
     rcases hx (U ×ˢ {𝒢 | Uᶜ ∈ 𝒢}) (prod_mem_nhds hU ((ultrafilter_isOpen_basic _).mem_nhds hUc))
       with ⟨⟨y, 𝒢⟩, ⟨⟨hy : y ∈ U, hy' : Uᶜ ∈ 𝒢⟩, rfl : 𝒢 = pure y⟩⟩
     exact hy' hy
-
-theorem isProperMap_iff_isClosedMap_stoneCech {X : Type u} {Y : Type v} [TopologicalSpace X]
-    [TopologicalSpace Y] {f : X → Y} :
-    IsProperMap f ↔ Continuous f ∧ IsClosedMap
-      (Prod.map f id : X × StoneCech X → Y × StoneCech X) := by
-  sorry
 
 theorem isProperMap_iff_universally_closed {X : Type u} {Y : Type v} [TopologicalSpace X]
     [TopologicalSpace Y] {f : X → Y} :
