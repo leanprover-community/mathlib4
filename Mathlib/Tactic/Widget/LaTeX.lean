80b5b430748060b69ce5e2ee204b796e3ad9c0fb
thorimur <68410468+thorimur@users.noreply.github.com>
2023-07-23 01:24:20 -0400

    imports and module docs

---
+++ Mathlib/Tactic/Widget/LaTeX.lean
@@ -0,0 +1,18 @@
+import ProofWidgets
+import ProofWidgets.Component.HtmlDisplay
+import ProofWidgets.Component.SelectionPanel
+import ProofWidgets.Component.GoalTypePanel
+
+/-!
+
+# In-progress experimentation for rendering LaTeX
+
+This branch is for experimentation only.
+
+We've got two ways to render math available to us:
+- MathJax
+- KaTeX
+
+We put the local copies of MathJax and KaTeX at the top level. (This is not how we'd want to actually do things! Quick access for demo purposes only.)
+-/
+
