9d44b44f0026136182e643c88c14793213b70a0a
thorimur <68410468+thorimur@users.noreply.github.com>
2023-07-23 01:29:17 -0400

    mathjax docs

--- Mathlib/Tactic/Widget/LaTeX.lean
+++ Mathlib/Tactic/Widget/LaTeX.lean
@@ -31,3 +31,25 @@ structure TeXProps extends TextProps where
   display : Bool := false
 #mkrpcenc TeXProps
 
+
+/- # MathJax
+
+We switch between MathJax for SVG and MathJax for CHTML for demo purposes. That means you may need to "click to reload" at certain points—a misclick can get the webview stuck in an errored state if it tries to e.g. find a function that doesn't exist on the current MathJax object.
+
+We use the whole MathJax *source* directory, in case we want to try to modify it to get fonts to work.
+
+The `mathjax/` directory here was obtained with
+```
+# clone source; would otherwise use https://github.com/mathjax/MathJax.git
+git clone https://github.com/mathjax/MathJax-src.git mathjax
+
+# build
+cd mathjax
+npm install
+npm run compile
+npm run make-components
+cd ..
+```
+See [https://docs.mathjax.org/en/latest/web/hosting.html].
+
+-/
