378d150351323a8943b7827ca9b3c3a2fa3d858b
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2023-07-23 11:06:59 +0200

    fix import

--- Mathlib/Analysis/Distribution/AEEqOfIntegralContDiff.lean
+++ Mathlib/Analysis/Distribution/AEEqOfIntegralContDiff.lean
@@ -3,7 +3,7 @@ Copyright (c) 2023 Sébastien Gouëzel. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Sébastien Gouëzel
 -/
-import Mathlib.Analysis.Calculus.BumpFunctionFindim
+import Geometry.Manifold.PartitionOfUnity
 
 /-!
 # Functions which vanish as distributions vanish as functions
