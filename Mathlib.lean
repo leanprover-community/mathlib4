f5a6f12bd1d14531d704b093f6a293ad0e3cc1d0
Michael Lee <michael_lee1@brown.edu>
2023-07-24 14:23:45 -0400

    add new module

--- Mathlib.lean
+++ Mathlib.lean
@@ -2780,6 +2780,7 @@ import Mathlib.RingTheory.Multiplicity
 import Mathlib.RingTheory.MvPolynomial.Basic
 import Mathlib.RingTheory.MvPolynomial.Homogeneous
 import Mathlib.RingTheory.MvPolynomial.Ideal
+import Mathlib.RingTheory.MvPolynomial.NewtonIdentities
 import Mathlib.RingTheory.MvPolynomial.Symmetric
 import Mathlib.RingTheory.MvPolynomial.Tower
 import Mathlib.RingTheory.MvPolynomial.WeightedHomogeneous
