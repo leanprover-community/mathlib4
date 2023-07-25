7cad8bc5bb0122ad4dca9f8ea79b91b9e7b8aa77
int-y1 <jason_yuen2007@hotmail.com>
2023-07-24 20:27:07 -0700

    chore: fix grammar mistakes

--- Mathlib/Algebra/Order/Nonneg/Ring.lean
+++ Mathlib/Algebra/Order/Nonneg/Ring.lean
@@ -29,7 +29,7 @@ When `α` is `ℝ`, this will give us some properties about `ℝ≥0`.
 ## Implementation Notes
 
 Instead of `{x : α // 0 ≤ x}` we could also use `Set.Ici (0 : α)`, which is definitionally equal.
-However, using the explicit subtype has a big advantage: when writing and element explicitly
+However, using the explicit subtype has a big advantage: when writing an element explicitly
 with a proof of nonnegativity as `⟨x, hx⟩`, the `hx` is expected to have type `0 ≤ x`. If we would
 use `Ici 0`, then the type is expected to be `x ∈ Ici 0`. Although these types are definitionally
 equal, this often confuses the elaborator. Similar problems arise when doing cases on an element.
