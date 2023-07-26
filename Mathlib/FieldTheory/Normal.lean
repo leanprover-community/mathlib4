5724e2218ddf3d52b785d186a5973c58bc082b13
Thomas Browning <tb65536@uw.edu>
2023-07-26 15:48:21 -0700

    revert

--- Mathlib/FieldTheory/Normal.lean
+++ Mathlib/FieldTheory/Normal.lean
@@ -474,3 +474,89 @@ theorem isSolvable_of_isScalarTower [Normal F K₁] [h1 : IsSolvable (K₁ ≃�
 #align is_solvable_of_is_scalar_tower isSolvable_of_isScalarTower
 
 end lift
+
+section normalClosure
+
+open IntermediateField
+
+variable (F K)
+variable [Algebra F K]
+variable (L : Type _) [Field L] [Algebra F L] [Algebra K L] [IsScalarTower F K L]
+
+/-- The normal closure of `K` in `L`. -/
+noncomputable def normalClosure : IntermediateField K L :=
+  { (⨆ f : K →ₐ[F] L, f.fieldRange).toSubfield with
+    -- Porting note: could not inherit neg_mem
+    neg_mem' := fun _ hx => Subfield.neg_mem (⨆ f : K →ₐ[F] L, f.fieldRange).toSubfield hx
+    algebraMap_mem' := fun r =>
+      le_iSup (fun f : K →ₐ[F] L => f.fieldRange) (IsScalarTower.toAlgHom F K L) ⟨r, rfl⟩ }
+#align normal_closure normalClosure
+
+namespace normalClosure
+
+theorem restrictScalars_eq_iSup_adjoin [h : Normal F L] :
+    (normalClosure F K L).restrictScalars F = ⨆ x : K, adjoin F ((minpoly F x).rootSet L) := by
+  refine' le_antisymm (iSup_le _) (iSup_le fun x => adjoin_le_iff.mpr fun y hy => _)
+  · rintro f _ ⟨x, rfl⟩
+    refine'
+      le_iSup (fun x => adjoin F ((minpoly F x).rootSet L)) x
+        (subset_adjoin F ((minpoly F x).rootSet L) _)
+    rw [mem_rootSet_of_ne, AlgHom.toRingHom_eq_coe, AlgHom.coe_toRingHom,
+      Polynomial.aeval_algHom_apply, minpoly.aeval, map_zero]
+    exact
+      minpoly.ne_zero
+        ((isIntegral_algebraMap_iff (algebraMap K L).injective).mp
+          (h.isIntegral (algebraMap K L x)))
+  · rw [Polynomial.rootSet, Finset.mem_coe, Multiset.mem_toFinset] at hy
+    let g :=
+      (algHomAdjoinIntegralEquiv F
+            ((isIntegral_algebraMap_iff (algebraMap K L).injective).mp
+              (h.isIntegral (algebraMap K L x)))).symm
+        ⟨y, hy⟩
+    refine'
+      le_iSup (fun f : K →ₐ[F] L => f.fieldRange)
+        ((g.liftNormal L).comp (IsScalarTower.toAlgHom F K L))
+        ⟨x, (g.liftNormal_commutes L (AdjoinSimple.gen F x)).trans _⟩
+    rw [Algebra.id.map_eq_id, RingHom.id_apply]
+    -- Porting note: in mathlib3 this next `apply` closed the goal.
+    -- Now it can't find a proof by unification, so we have to do it ourselves.
+    apply PowerBasis.lift_gen
+    change aeval y (minpoly F (AdjoinSimple.gen F x)) = 0
+    suffices : minpoly F (AdjoinSimple.gen F x) = minpoly F x
+    · exact this ▸ aeval_eq_zero_of_mem_rootSet (Multiset.mem_toFinset.mpr hy)
+    exact minpoly_gen ((isIntegral_algebraMap_iff (algebraMap K L).injective).mp
+      (h.isIntegral (algebraMap K L x)))
+
+#align normal_closure.restrict_scalars_eq_supr_adjoin normalClosure.restrictScalars_eq_iSup_adjoin
+
+instance normal [h : Normal F L] : Normal F (normalClosure F K L) := by
+  let ϕ := algebraMap K L
+  rw [← IntermediateField.restrictScalars_normal, restrictScalars_eq_iSup_adjoin]
+  -- Porting note: use the `(_)` trick to obtain an instance by unification.
+  apply @IntermediateField.normal_iSup F L _ _ _ _ _ (_)
+  intro x
+  -- Porting note: use the `(_)` trick to obtain an instance by unification.
+  apply @Normal.of_isSplittingField _ _ _ _ _ (minpoly F x) (_)
+  exact
+    adjoin_rootSet_isSplittingField
+      ((minpoly.eq_of_algebraMap_eq ϕ.injective
+            ((isIntegral_algebraMap_iff ϕ.injective).mp (h.isIntegral (ϕ x))) rfl).symm ▸
+        h.splits _)
+#align normal_closure.normal normalClosure.normal
+
+instance is_finiteDimensional [FiniteDimensional F K] :
+    FiniteDimensional F (normalClosure F K L) := by
+  haveI : ∀ f : K →ₐ[F] L, FiniteDimensional F f.fieldRange := fun f =>
+    f.toLinearMap.finiteDimensional_range
+  apply IntermediateField.finiteDimensional_iSup_of_finite
+#align normal_closure.is_finite_dimensional normalClosure.is_finiteDimensional
+
+instance isScalarTower : IsScalarTower F (normalClosure F K L) L :=
+  -- Porting note: the last argument here `(⨆ (f : K →ₐ[F] L), f.fieldRange).toSubalgebra`
+  -- was just written as `_` in mathlib3.
+  IsScalarTower.subalgebra' F L L (⨆ (f : K →ₐ[F] L), f.fieldRange).toSubalgebra
+#align normal_closure.is_scalar_tower normalClosure.isScalarTower
+
+end normalClosure
+
+end normalClosure
