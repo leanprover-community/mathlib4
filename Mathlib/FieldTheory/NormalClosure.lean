2dea73e7e8812633941343b99c0983e2786c8704
Thomas Browning <tb65536@uw.edu>
2023-07-26 16:15:48 -0700

    fix

--- Mathlib/FieldTheory/NormalClosure.lean
+++ Mathlib/FieldTheory/NormalClosure.lean
@@ -19,56 +19,27 @@ that contains the image of every `F`-algebra embedding `K →ₐ[F] L`.
 - `IntermediateField.normalClosure K` for `K : IntermediateField F L`.
 -/
 
-
-
 open BigOperators Polynomial
 
 variable {F L : Type _} [Field F] [Field L] [Algebra F L] (K : IntermediateField F L)
 
-/-- The normal closure of `K` in `L`. -/
-def IntermediateField.normalClosure' : IntermediateField F L :=
-  (normalClosure F K L).restrictScalars F
-
 namespace IntermediateField
+
+/-- The normal closure of `K` in `L`. -/
+noncomputable def normalClosure : IntermediateField F L :=
+  (_root_.normalClosure F K L).restrictScalars F
 
 lemma normalClosure_def : K.normalClosure = ⨆ f : K →ₐ[F] L, f.fieldRange :=
   rfl
 
 namespace normalClosure
 
-theorem eq_iSup_adjoin [h : Normal F L] :
-    K.normalClosure = ⨆ x : K, adjoin F ((minpoly F x).rootSet L) := by
-  classical
-  have hi : ∀ x : K, IsIntegral F x := fun x ↦ isIntegral_iff.mpr (h.isIntegral (algebraMap K L x))
-  refine' le_antisymm (iSup_le _) (iSup_le fun x ↦ adjoin_le_iff.mpr fun y hy ↦ _)
-  · rintro f _ ⟨x, rfl⟩
-    refine' le_iSup (fun x ↦ adjoin F ((minpoly F x).rootSet L)) x
-      (subset_adjoin F ((minpoly F x).rootSet L) _)
-    rw [mem_rootSet_of_ne (minpoly.ne_zero (hi x)), AlgHom.toRingHom_eq_coe, RingHom.coe_coe,
-      Polynomial.aeval_algHom_apply, minpoly.aeval, map_zero]
-  · rw [Polynomial.rootSet, Finset.mem_coe, Multiset.mem_toFinset] at hy
-    let g := (algHomAdjoinIntegralEquiv F (hi x)).symm ⟨y, hy⟩
-    refine' le_iSup AlgHom.fieldRange ((g.liftNormal L).comp (IsScalarTower.toAlgHom F K L))
-      ⟨x, (g.liftNormal_commutes L (AdjoinSimple.gen F x)).trans _⟩
-    -- Porting note: in mathlib3 this next `apply` closed the goal.
-    -- Now it can't find a proof by unification, so we have to do it ourselves.
-    apply PowerBasis.lift_gen
-    change aeval y (minpoly F (AdjoinSimple.gen F x)) = 0
-    exact minpoly_gen (hi x) ▸ aeval_eq_zero_of_mem_rootSet (Multiset.mem_toFinset.mpr hy)
-
-instance normal [h : Normal F L] : Normal F K.normalClosure := by
-  rw [eq_iSup_adjoin]
-  -- Porting note: use the `(_)` trick to obtain an instance by unification.
-  apply IntermediateField.normal_iSup (h := _)
-  intro x
-  have := adjoin_rootSet_isSplittingField ((minpoly_eq x).symm ▸ h.splits x)
-  exact Normal.of_isSplittingField (minpoly F x)
+instance normal [Normal F L] : Normal F K.normalClosure :=
+  _root_.normalClosure.normal F K L
 
 instance finiteDimensional [FiniteDimensional F K] :
-    FiniteDimensional F K.normalClosure := by
-  have : ∀ f : K →ₐ[F] L, FiniteDimensional F f.fieldRange :=
-    fun f ↦ f.toLinearMap.finiteDimensional_range
-  exact IntermediateField.finiteDimensional_iSup_of_finite
+    FiniteDimensional F K.normalClosure :=
+  normalClosure.is_finiteDimensional F K L
 
 end normalClosure
 
