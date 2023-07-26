e0e751ec5677c417012413c011a5cad08e9b432d
Jon Eugster <eugster.jon@gmail.com>
2023-07-26 15:17:20 +0200

    update Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -2071,6 +2071,7 @@ import Mathlib.Init.Quot
 import Mathlib.Init.Set
 import Mathlib.Init.ZeroOne
 import Mathlib.Lean.CoreM
+import Mathlib.Lean.Data.NameMap
 import Mathlib.Lean.Elab.Tactic.Basic
 import Mathlib.Lean.EnvExtension
 import Mathlib.Lean.Exception
