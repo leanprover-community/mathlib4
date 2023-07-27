630ee8ccdf5aea1d48844fe637cd0c014e0d5063
qawbecrdtey <qawbecrdtey@kaist.ac.kr>
2023-07-27 23:04:55 +0900

    Working on 'greedoidRankAxioms_unique_greedoid' and 'rank.toGreedoid'

--- Mathlib/Data/Greedoid/Basic.lean
+++ Mathlib/Data/Greedoid/Basic.lean
@@ -593,7 +593,34 @@ protected def rank.toGreedoid (r : Finset α → ℕ) (hr : greedoidRankAxioms r
 
 theorem greedoidRankAxioms_unique_greedoid {r : Finset α → ℕ} (hr : greedoidRankAxioms r) :
     ∃! G : Greedoid α, G.rank = r := by
-  sorry
+  exists rank.toGreedoid r hr
+  simp only [rank.toGreedoid, mem_univ, forall_true_left]
+  constructor
+  . ext s
+    simp only [rank]
+    apply Nat.le_antisymm
+    . simp only [mem_univ, forall_true_left, Finset.mem_filter, true_and, WithBot.unbot_le_iff]
+      apply Finset.max_le
+      intro a ha
+      rw [WithBot.coe_le_coe]
+      simp only [mem_univ, forall_true_left, Finset.mem_filter, true_and, mem_image] at ha
+      let ⟨b, ⟨hb₁, hb₂⟩, hb₃⟩ := ha
+      rw [← hb₃, ← hb₁]
+      exact hr.2.2.1 hb₂
+    . simp only [mem_univ, forall_true_left, Finset.mem_filter, true_and, WithBot.le_unbot_iff]
+      apply Finset.le_max
+      simp only [mem_univ, forall_true_left, Finset.mem_filter, true_and, mem_image]
+      by_contra' h'
+      simp only [ne_eq, and_imp] at h'
+      sorry
+  . intro G' hG'
+    apply Greedoid.eq_of_veq
+    simp only [mem_univ, forall_true_left]
+    ext x; constructor <;> intro h
+    . simp [← hG', system_feasible_set_mem_mem.mp h]
+    . simp only [system_feasible_set_mem_mem, Finset.mem_filter, mem_univ, true_and] at *
+      rw [← hG', rank_eq_card_iff_feasible] at h
+      exact h
 
 -- /-- Rank function satisfying `greedoidRankAxioms` generates a unique greedoid. -/
 -- protected def rank.toGreedoid (r : Finset α → ℕ) (hr : greedoidRankAxioms r) : Greedoid α :=
