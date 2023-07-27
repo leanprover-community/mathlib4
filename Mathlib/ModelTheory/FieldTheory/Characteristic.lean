a645f7647c2cd9e3532ddb5d47a7509e47084c6a
Chris Hughes <chrishughes24@gmail.com>
2023-07-27 13:34:06 +0100

    define Theory.fieldOfChar

--- Mathlib/ModelTheory/FieldTheory/Characteristic.lean
+++ Mathlib/ModelTheory/FieldTheory/Characteristic.lean
@@ -10,7 +10,11 @@ def ofNat {α : Type} : ℕ → Language.field.Term α
   | 0 => 0
   | n+1 => ofNat n + 1
 
+def eqZero (n : ℕ) : Language.field.Sentence :=
+  Term.equal (ofNat n) 0
 
+def Theory.fieldOfChar (n : ℕ) : Language.field.Theory :=
+  Theory.field ∪ {eqZero n} ∪ ⋃ (i : ℕ) (_ : n ≠ i), {∼ (eqZero i)}
 
 end field
 
