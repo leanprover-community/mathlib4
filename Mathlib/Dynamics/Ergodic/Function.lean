640ad00415f4cf5e84992a3bdd6e8dbd352642af
Yury G. Kudryashov <urkud@urkud.name>
2023-07-26 22:32:38 -0500

    Update docstrings

--- Mathlib/Dynamics/Ergodic/Function.lean
+++ Mathlib/Dynamics/Ergodic/Function.lean
@@ -9,20 +9,21 @@ import Mathlib.MeasureTheory.Function.AEEqFun
 /-!
 # Functions invariant under (quasi)ergodic map
 
-In this file we prove that an a.e. strongly measurable function `g : α → X` that is a.e. invariant
-under a (quasi)ergodic map is a.e. equal to a constant. We prove several versions of this statement with
-slightly different measurability assumptions. We also formulate a version for
-`MeasureTheory.AEEqFun` functions with all a.e. equalities replaced with equalities in the quotient
-space.
+In this file we prove that an a.e. strongly measurable function `g : α → X`
+that is a.e. invariant under a (quasi)ergodic map is a.e. equal to a constant.
+We prove several versions of this statement with slightly different measurability assumptions.
+We also formulate a version for `MeasureTheory.AEEqFun` functions
+with all a.e. equalities replaced with equalities in the quotient space.
 -/
 
 open Function Set Filter MeasureTheory Topology TopologicalSpace
 
 variable {α X : Type _} [MeasurableSpace α] {μ : MeasureTheory.Measure α}
 
-/-- Let `f : α → α` be a (quasi)ergodic map. Let `g : α → X` is a null-measurable function from `α`
-to a nonempty space with a countable family of measurable sets separating points of a set `s` such
-that `f x ∈ s` for a.e. `x`. If `g` that is a.e.-invariant under `f`, then `g` is a.e. constant. -/
+/-- Let `f : α → α` be a (quasi)ergodic map. Let `g : α → X` is a null-measurable function
+from `α` to a nonempty space with a countable family of measurable sets
+separating points of a set `s` such that `f x ∈ s` for a.e. `x`.
+If `g` that is a.e.-invariant under `f`, then `g` is a.e. constant. -/
 theorem QuasiErgodic.ae_eq_const_of_ae_eq_comp_of_ae_range₀ [Nonempty X] [MeasurableSpace X]
     {s : Set X} [HasCountableSeparatingOn X MeasurableSet s] (h : QuasiErgodic f μ)
     (hs : ∀ᵐ x ∂μ, g x ∈ s) (hgm : NullMeasurable g μ) (hg_eq : g ∘ f =ᵐ[μ] g) :
@@ -37,24 +38,27 @@ section CountableSeparatingOnUniv
 variable [Nonempty X] [MeasurableSpace X] [HasCountableSeparatingOn X MeasurableSet univ]
   {f : α → α} {g : α → X}
 
-/-- If `f : α → α` is a (pre)ergodic map and `g : α → X` is a measurable function from `α` to a
-nonempty topological space with second countable topology that is invariant under `f`, then `g` is
-a.e. constant. -/
+/-- Let `f : α → α` be a (pre)ergodic map.
+Let `g : α → X` be a measurable function from `α` to a nonempty measurable space
+with a countable family of measurable sets separating the points of `X`.
+If `g` is invariant under `f`, then `g` is a.e. constant. -/
 theorem PreErgodic.ae_eq_const_of_ae_eq_comp (h : PreErgodic f μ) (hgm : Measurable g)
     (hg_eq : g ∘ f = g) : ∃ c, g =ᵐ[μ] const α c :=
   exists_eventuallyEq_const_of_forall_separating MeasurableSet fun U hU ↦
     h.ae_mem_or_ae_nmem (s := g ⁻¹' U) (hgm hU) <| by rw [← preimage_comp, hg_eq]
 
-/-- If `f : α → α` is a (quasi)ergodic map and `g : α → X` is a null-measurable function from `α` to
-a nonempty topological space with second countable topology that is a.e.-invariant under `f`, then
-`g` is a.e. constant. -/
+/-- Let `f : α → α` be a quasi ergodic map.
+Let `g : α → X` be a null-measurable function from `α` to a nonempty measurable space
+with a countable family of measurable sets separating the points of `X`.
+If `g` is a.e.-invariant under `f`, then `g` is a.e. constant. -/
 theorem QuasiErgodic.ae_eq_const_of_ae_eq_comp₀ (h : QuasiErgodic f μ) (hgm : NullMeasurable g μ)
     (hg_eq : g ∘ f =ᵐ[μ] g) : ∃ c, g =ᵐ[μ] const α c :=
   h.ae_eq_const_of_ae_eq_comp_of_ae_range₀ (s := univ) univ_mem hgm hg_eq
 
-/-- If `f : α → α` is an ergodic map and `g : α → X` is a null-measurable function from `α` to
-a nonempty topological space with second countable topology that is a.e.-invariant under `f`, then
-`g` is a.e. constant. -/
+/-- Let `f : α → α` be an ergodic map.
+Let `g : α → X` be a null-measurable function from `α` to a nonempty measurable space
+with a countable family of measurable sets separating the points of `X`.
+If `g` is a.e.-invariant under `f`, then `g` is a.e. constant. -/
 theorem Ergodic.ae_eq_const_of_ae_eq_comp₀ (h : Ergodic f μ) (hgm : NullMeasurable g μ)
     (hg_eq : g ∘ f =ᵐ[μ] g) : ∃ c, g =ᵐ[μ] const α c :=
   h.quasiErgodic.ae_eq_const_of_ae_eq_comp₀ hgm hg_eq
@@ -65,9 +69,10 @@ variable [TopologicalSpace X] [MetrizableSpace X] [Nonempty X] {f : α → α}
 
 namespace QuasiErgodic
 
-/-- If `f : α → α` is a (quasi)ergodic map and `g : α → X` is an a.e. strongly measurable function
-from `α` to a nonempty topological space that is a.e.-invariant under `f`, then `g` is
-a.e. constant. -/
+/-- Let `f : α → α` be a quasi ergodic map.
+Let `g : α → X` be an a.e. strongly measurable function
+from `α` to a nonempty metrizable topological space.
+If `g` is a.e.-invariant under `f`, then `g` is a.e. constant. -/
 theorem ae_eq_const_of_ae_eq_comp_ae {g : α → X} (h : QuasiErgodic f μ)
     (hgm : AEStronglyMeasurable g μ) (hg_eq : g ∘ f =ᵐ[μ] g) : ∃ c, g =ᵐ[μ] const α c := by
   borelize X
@@ -86,9 +91,10 @@ end QuasiErgodic
 
 namespace Ergodic
 
-/-- If `f : α → α` is a (quasi)ergodic map and `g : α → X` is an a.e. strongly measurable function
-from `α` to a nonempty topological space that is a.e.-invariant under `f`, then `g` is
-a.e. constant. -/
+/-- Let `f : α → α` be an ergodic map.
+Let `g : α → X` be an a.e. strongly measurable function
+from `α` to a nonempty metrizable topological space.
+If `g` is a.e.-invariant under `f`, then `g` is a.e. constant. -/
 theorem ae_eq_const_of_ae_eq_comp_ae {g : α → X} (h : Ergodic f μ) (hgm : AEStronglyMeasurable g μ)
     (hg_eq : g ∘ f =ᵐ[μ] g) : ∃ c, g =ᵐ[μ] const α c :=
   h.quasiErgodic.ae_eq_const_of_ae_eq_comp_ae hgm hg_eq
