57e027d995aa6327f797d70d8594d264aad361d9
negiizhao <egresf@gmail.com>
2023-07-28 01:13:03 +0800

    fix

--- Mathlib/Logic/Nontrivial.lean
+++ Mathlib/Logic/Nontrivial.lean
@@ -204,8 +204,8 @@ protected theorem Subsingleton.le [Preorder α] [Subsingleton α] (x y : α) : x
   le_of_eq (Subsingleton.elim x y)
 #align subsingleton.le Subsingleton.le
 
-@[nontriviality]
-theorem Subsingleton.eq_zero [Zero α] [Subsingleton α] (a : α) : a = 0 :=
+@[to_additive]
+theorem Subsingleton.eq_one [One α] [Subsingleton α] (a : α) : a = 1 :=
   Subsingleton.elim _ _
 
 namespace Bool
