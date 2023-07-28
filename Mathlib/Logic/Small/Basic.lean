8c774aebdc060524869cb52b3ba4a9ec39219901
Markus Himmel <markus@himmel-villmar.de>
2023-07-28 07:24:32 +0200

    Rename hypothesis

--- Mathlib/Logic/Small/Basic.lean
+++ Mathlib/Logic/Small/Basic.lean
@@ -128,11 +128,11 @@ instance (priority := 100) small_subsingleton (α : Type v) [Subsingleton α] : 
 theorem small_of_injective_of_exists {α : Type v} {β : Type w} {γ : Type v'} [Small.{u} α]
     (f : α → γ) {g : β → γ} (hg : Function.Injective g) (h : ∀ b : β, ∃ a : α, f a = g b) :
     Small.{u} β := by
-  by_cases hγ : Nonempty β
+  by_cases hβ : Nonempty β
   · refine' small_of_surjective (f := Function.invFun g ∘ f) (fun b => _)
     obtain ⟨a, ha⟩ := h b
     exact ⟨a, by rw [Function.comp_apply, ha, Function.leftInverse_invFun hg]⟩
-  · simp only [not_nonempty_iff] at hγ
+  · simp only [not_nonempty_iff] at hβ
     infer_instance
 
 /-!
