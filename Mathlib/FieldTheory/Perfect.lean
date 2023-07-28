fd6bf42d887ca7d5982b01ea543a007e8bb9d74f
Oliver Nash <github@olivernash.org>
2023-07-28 15:23:45 +0100

    Minor tweakz

--- Mathlib/FieldTheory/Perfect.lean
+++ Mathlib/FieldTheory/Perfect.lean
@@ -35,10 +35,10 @@ variable (R : Type _) (p : ℕ) [CommRing R] [Fact p.Prime] [CharP R p]
 
 /-- For a reduced ring, surjectivity of the Frobenius map is a sufficient condition for perfection.
 -/
-lemma mkOfReduced [IsReduced R] (h : Surjective $ frobenius R p) : PerfectRing R p :=
+lemma PerfectRing.mkOfReduced [IsReduced R] (h : Surjective $ frobenius R p) : PerfectRing R p :=
   ⟨frobenius_inj R p, h⟩
 
-instance ofFiniteOfIsReduced [Finite R] [IsReduced R] : PerfectRing R p :=
+instance PerfectRing.ofFiniteOfIsReduced [Finite R] [IsReduced R] : PerfectRing R p :=
   mkOfReduced _ _ $ Finite.surjective_of_injective (frobenius_inj R p)
 
 variable [PerfectRing R p]
@@ -78,7 +78,7 @@ class PerfectField (K : Type _) [Field K] : Prop where
 
 lemma PerfectRing.toPerfectField (K : Type _) (p : ℕ)
     [Field K] [hp : Fact p.Prime] [CharP K p] [PerfectRing K p] : PerfectField K := by
-  refine' PerfectField.mk $ fun {f} hf ↦ _
+  refine' PerfectField.mk $ fun hf ↦ _
   rcases Polynomial.separable_or p hf with h | ⟨-, g, -, rfl⟩
   · assumption
   · exfalso; revert hf; simp
@@ -96,8 +96,8 @@ instance ofFinite [Finite K] : PerfectField K := by
 
 variable [PerfectField K]
 
-instance PerfectField.toPerfectRing (p : ℕ) [Fact p.Prime] [CharP K p] : PerfectRing K p := by
-  refine' mkOfReduced _ _ $ fun y ↦ _
+instance PerfectField.toPerfectRing (p : ℕ) [hp : Fact p.Prime] [CharP K p] : PerfectRing K p := by
+  refine' PerfectRing.mkOfReduced _ _ $ fun y ↦ _
   let f : Polynomial K := Polynomial.X ^ p - Polynomial.C y
   sorry
 
