aa8df15df57e94147867f01ab268f7e2a908195f
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2023-07-28 09:19:16 +0200

    start on cardinality

--- Mathlib/test.lean
+++ Mathlib/test.lean
@@ -1,10 +1,36 @@
 import Mathlib.Analysis.Convex.Segment
 import Mathlib.Topology.Instances.Real
 import Mathlib.LinearAlgebra.Finrank
+import Mathlib.Analysis.Convolution
+import Mathlib.SetTheory.Cardinal.Finite
 
 open LinearMap Set
 
 open BigOperators Classical Convex Pointwise
+
+open scoped Cardinal
+
+universe u v
+
+open Cardinal
+
+
+lemma card_iUnion__of_mono_countable {α ι : Type u}
+    [LinearOrder ι] [Countable ι] [hι : Nonempty ι] {f : ι → Set α} (hf : Monotone f) {a : Cardinal}
+    (h'f : ∀ i, #(f i) = a) : #(⋃ i, f i) = a := by
+  rcases hι with ⟨i⟩
+  apply le_antisymm; swap
+  · rw [← h'f i]
+    exact Cardinal.mk_le_mk_of_subset (subset_iUnion f i)
+  rcases lt_or_le a ℵ₀ with ha|ha
+  -- case `a` finite
+  · sorry
+  -- case `a` infinite
+  · refine (Cardinal.mk_iUnion_le_sum_mk (f := f)).trans ?_
+    simp [h'f]
+
+
+#exit
 
 lemma foo {E : Type _} [AddCommGroup E] [Module ℝ E] (x y : E) (h : LinearIndependent ℝ ![x, y])
     (s t : ℝ) (hs : s ≠ t) : [x -[ℝ]- t • y] ∩ [x -[ℝ]- s • y] ⊆ {x} := by
@@ -19,7 +45,13 @@ lemma foo {E : Type _} [AddCommGroup E] [Module ℝ E] (x y : E) (h : LinearInde
   · have A : s = t := by simpa [mul_eq_mul_left_iff, hq0'.ne'] using (h.eq_of_pair H).2
     exact (hs A).elim
 
+theorem glouglou1 {E : Type _} [TopologicalSpace E] [NormedAddCommGroup E] [NormedSpace ℝ E]
+    [NonTrivial E] (s : Set E) (hs : s.Countable) : Dense sᶜ := by
+  exact?
+
+
+
 theorem glouglou {E : Type _} [TopologicalSpace E] [AddCommGroup E] [Module ℝ E]
-    (h : 2 ≤ Module.rank ℝ E) (s : Set E) (hs : s.countable) :
+    (h : 2 ≤ Module.rank ℝ E) (s : Set E) (hs : s.Countable) :
     IsConnected sᶜ := by
   sorry
