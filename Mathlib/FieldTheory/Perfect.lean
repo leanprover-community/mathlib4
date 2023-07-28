4b33ee434f8077f276a7044b570dee1a148ae0ac
Oliver Nash <github@olivernash.org>
2023-07-28 15:24:07 +0100

    Add proof of `PerfectField.toPerfectRing`
    Just bashed through till sorry-free: needs much tidying!

--- Mathlib/FieldTheory/Perfect.lean
+++ Mathlib/FieldTheory/Perfect.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Oliver Nash
 -/
 import Mathlib.FieldTheory.Separable
+import Mathlib.FieldTheory.SplittingField.Construction
 
 /-!
 
@@ -99,6 +100,40 @@ variable [PerfectField K]
 instance PerfectField.toPerfectRing (p : ℕ) [hp : Fact p.Prime] [CharP K p] : PerfectRing K p := by
   refine' PerfectRing.mkOfReduced _ _ $ fun y ↦ _
   let f : Polynomial K := Polynomial.X ^ p - Polynomial.C y
-  sorry
+  let L := f.SplittingField
+  let ι := algebraMap K L
+  have : CharP L p := -- TODO Make this a global `instance`
+    charP_of_injective_algebraMap (NoZeroSMulDivisors.algebraMap_injective K L) p
+  have hf : f.degree ≠ 0 := by
+    rw [Polynomial.degree_X_pow_sub_C hp.out.pos y, p.cast_ne_zero]; exact hp.out.ne_zero
+  let a : L := f.rootOfSplits ι (Polynomial.SplittingField.splits f) hf
+  have hfa : Polynomial.aeval a f = 0 := by
+    rw [Polynomial.aeval_def, Polynomial.map_rootOfSplits _ (Polynomial.SplittingField.splits f) hf]
+  have hfa' : a^p = ι y := by
+    rwa [AlgHom.map_sub, Polynomial.aeval_X_pow, Polynomial.aeval_C, sub_eq_zero] at hfa
+  let g : Polynomial K := minpoly K a
+  have hgd : 0 < g.degree := minpoly.degree_pos (isIntegral_of_finite K a)
+  suffices : g.degree = 1
+  · obtain ⟨a' : K, ha' : ι a' = a⟩ := minpoly.mem_range_of_degree_eq_one K a this
+    refine' ⟨a', NoZeroSMulDivisors.algebraMap_injective K L _⟩
+    rw [RingHom.map_frobenius, ha', frobenius_def, hfa']
+  have hg : g.Separable := separable_of_irreducible $ minpoly.irreducible $ isIntegral_of_finite K a
+  have hgf : g ∣ f := minpoly.dvd K a hfa
+  have hgf' := Polynomial.map_dvd ι hgf
+  have hg' : (g.map ι).Separable := hg.map
+  have hf' : f.map ι = (Polynomial.X - Polynomial.C a)^p := by
+    rw [sub_pow_char, Polynomial.map_sub, Polynomial.map_pow, Polynomial.map_X, Polynomial.map_C,
+      ← hfa', map_pow]
+  rw [hf'] at hgf'
+  have hgg : g.map ι = (Polynomial.X - Polynomial.C a)^(g.map ι).natDegree := by
+    have hgm : (g.map ι).Monic := (minpoly.monic $ isIntegral_of_finite K a).map ι
+    obtain ⟨q, -, hq⟩ := (dvd_prime_pow (Polynomial.prime_X_sub_C a) p).mp hgf'
+    rw [Polynomial.eq_of_monic_of_associated hgm ((Polynomial.monic_X_sub_C a).pow q) hq,
+      Polynomial.natDegree_pow, Polynomial.natDegree_X_sub_C, mul_one]
+  suffices : (g.map ι).natDegree = 1
+  · rwa [g.natDegree_map, ← Polynomial.degree_eq_iff_natDegree_eq_of_pos Nat.one_pos] at this
+  rw [hgg] at hg'
+  refine' (Polynomial.Separable.of_pow (Polynomial.not_isUnit_X_sub_C a) _ hg').2
+  rwa [g.natDegree_map ι, ← Nat.pos_iff_ne_zero, Polynomial.natDegree_pos_iff_degree_pos]
 
 end PerfectField
