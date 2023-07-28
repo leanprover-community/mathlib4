cd6ebed6f83314a0a6e8d3cd3bbe5f7d673207a6
Harun Khan <mhk119@ic.ac.uk>
2023-07-28 06:33:53 -0700

    moved lemmas to new file

--- Mathlib/Data/Bitvec/Defs.lean
+++ Mathlib/Data/Bitvec/Defs.lean
@@ -47,10 +47,7 @@ instance : OfNat (BitVec w) (nat_lit 0) :=
 We inherit `Fin` implementations when fast but write mod/div
 ourselves to avoid the extra modulo operation.
 -/
-protected def add (x y : BitVec w) : BitVec w := Fin.add x y
-protected def sub (x y : BitVec w) : BitVec w := Fin.sub x y
-protected def mul (x y : BitVec w) : BitVec w := Fin.mul x y
-protected def neg (x : BitVec w) : BitVec w := (Fin.neg (2^w)).neg x
+
 protected def mod (x y : BitVec w) : BitVec w :=
   ⟨x.val % y.val, Nat.lt_of_le_of_lt (Nat.mod_le _ _) x.isLt⟩
 protected def div (x y : BitVec w) : BitVec w :=
@@ -60,14 +57,14 @@ protected def lt (x y : BitVec w) : Bool :=
 protected def le (x y : BitVec w) : Bool :=
   x.val ≤ y.val
 
-instance : Add (BitVec w) := ⟨BitVec.add⟩
-instance : Sub (BitVec w) := ⟨BitVec.sub⟩
-instance : Mul (BitVec w) := ⟨BitVec.mul⟩
+instance : Add (BitVec w) := inferInstanceAs (Add (Fin _))
+instance : Sub (BitVec w) := inferInstanceAs (Sub (Fin _))
+instance : Mul (BitVec w) := inferInstanceAs (Mul (Fin _))
+instance : Neg (BitVec w) := inferInstanceAs (Neg (Fin _))
 instance : Mod (BitVec w) := ⟨BitVec.mod⟩
 instance : Div (BitVec w) := ⟨BitVec.div⟩
 instance : LT (BitVec w)  := ⟨fun x y => BitVec.lt x y⟩
 instance : LE (BitVec w)  := ⟨fun x y => BitVec.le x y⟩
-instance : Neg (BitVec w) := ⟨BitVec.neg⟩
 
 /-!
 ## Bitwise operations
@@ -169,6 +166,9 @@ def shrink (v : Nat) (x : BitVec w) : BitVec v :=
 @[simp] def lsbGet (x : BitVec w) (i : Nat) : Bool :=
   x.extract i i != 0
 
+def bbT (bs : List Bool) : BitVec bs.length :=
+  ⟨Nat.ofBits (λ i => bs[i]!) 0 bs.length, @Nat.ofBits_lt _ (bs.length)⟩
+
 theorem lsbGet_eq_testBit {x : BitVec w} : x.lsbGet i = x.val.testBit i := by
   cases' h: Nat.bodd (Nat.shiftr (x.val) i)
   <;> simp [Nat.testBit, BitVec.ofNat, Fin.ofNat',
@@ -178,69 +178,4 @@ theorem lsbGet_eq_testBit {x : BitVec w} : x.lsbGet i = x.val.testBit i := by
 instance : GetElem (BitVec w) Nat Bool (fun _ i => i < w) where
   getElem x i _ := Nat.testBit x.val i
 
-open Nat
-
-lemma testBit_eq_ofNat {x: BitVec w} :
-  Bool.toNat (testBit (x.val) k) = (BitVec.ofNat 1 (x.val >>> k)).val:= by
-  simp only [BitVec.ofNat, Fin.ofNat', testBit, shiftRight_eq_shiftr, mod_two_of_bodd, pow_one]
-  aesop
-
-lemma val_to_ofNat (h: m < 2^w) : (BitVec.ofNat w m).val = m := by
-  simp [BitVec.ofNat, Fin.ofNat', mod_eq_of_lt h]
-
-lemma ofNat_to_val (x : BitVec w) : BitVec.ofNat w x.val = x := by
-  simp [BitVec.ofNat, Fin.ofNat', mod_eq_of_lt x.isLt]
-
-lemma ofNat_to_val' (x : BitVec w) (h : v = w):
-  HEq x (BitVec.ofNat v x.val) := h ▸ heq_of_eq (ofNat_to_val x).symm
-
-theorem concat_ext {x : BitVec a} {y : BitVec b} :
-  (x ++ y).val = x.val <<< b ||| y.val := rfl
-
-theorem extract_ext : (extract i j x).val = x.val/2^j % (2^(i - j + 1)) := by
-  simp [extract, BitVec.ofNat, Fin.ofNat', shiftRight_eq_shiftr, shiftr_eq_div_pow]
-
-theorem testBit_eq_rep {x: BitVec w} (i : Nat) (h: i< w): x[i] = testBit x.val i := by rfl
-
-theorem testBit_eq_rep' {x: Nat} (i : Nat) (h: i< w) (h2: x< 2^w):
-  (BitVec.ofNat w x)[i] = testBit x i := by
-  simp [BitVec.ofNat, GetElem.getElem, lsbGet, extract, Fin.ofNat', mod_eq_of_lt, h2]
-
-def bbT (bs : List Bool) : BitVec bs.length :=
-  ⟨ofBits (λ i => bs[i]!) 0 bs.length, @ofBits_lt _ (bs.length)⟩
-
-
-/-! ### Equivalence between bitwise and BitVec operations -/
-
-theorem BV_add {x y : BitVec w}: bitadd x.val y.val w = (x + y).val := by
-  rw [bitadd_eq_add]
-  norm_cast
-
--- theorem BV_neg {x : BitVec w}: bitwise_neg x.val w = x.neg.val := by
---   simp only [bitwise_neg_eq_neg, x.isLt]
---   norm_cast
-
--- theorem BV_mul {x y : BitVec w} (h : 0 < w): bitwise_mul x.val y.val w = (x * y).val := by
---   rw [bitwise_mul_eq_mul h]
---   norm_cast
-
--- theorem BV_extract {x : BitVec w} : bitwise_extract x.val i j = (extract i j x).val := by
---   rw [bitwise_extract_eq_extract]
---   norm_cast
-
--- theorem BV_concat {x : BitVec w} {y : BitVec v} :
---  bitwise_concat y.val x.val v w  = (x ++ y).val := by
---   rw [bitwise_concat_eq_concat y.isLt x.isLt]
---   norm_cast
-
--- theorem BV_eq {x y : BitVec w} (h: 0 < w): bitwise_eq x.val y.val w = (x = y) := by
---   simp [← bitwise_eq_eq h x.isLt y.isLt]
-
-theorem BV_ult {x y : BitVec w} (h1: x < y) :
-  bitult x.val y.val w:= bitult_of_ult y.isLt (val_bitvec_lt.mpr h1)
-
--- theorem BV_slt {x y : BitVec w} (h1: x < y) : bitwise_slt x.val y.val w:= sorry
-
--- theorem BV_signExtend {x : BitVec w} (h: 0 < w) :
--- (signExtend i x).val = bitwise_ext x.val w i := sorry
 end BitVec
---
+++ Mathlib/Data/Bitvec/Lemmas.lean
@@ -0,0 +1,71 @@
+/-
+Copyright (c) 2022 by the authors listed in the file AUTHORS and their
+institutional affiliations. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Harun Khan
+-/
+
+import Mathlib.Data.Bitvec.Defs
+
+open BitVec Nat
+
+lemma testBit_eq_ofNat {x: BitVec w} :
+  Bool.toNat (testBit (x.val) k) = (BitVec.ofNat 1 (x.val >>> k)).val:= by
+  simp only [BitVec.ofNat, Fin.ofNat', testBit, shiftRight_eq_shiftr, mod_two_of_bodd, pow_one]
+  aesop
+
+lemma val_to_ofNat (h: m < 2^w) : (BitVec.ofNat w m).val = m := by
+  simp [BitVec.ofNat, Fin.ofNat', mod_eq_of_lt h]
+
+lemma ofNat_to_val (x : BitVec w) : BitVec.ofNat w x.val = x := by
+  simp [BitVec.ofNat, Fin.ofNat', mod_eq_of_lt x.isLt]
+
+lemma ofNat_to_val' (x : BitVec w) (h : v = w):
+  HEq x (BitVec.ofNat v x.val) := h ▸ heq_of_eq (ofNat_to_val x).symm
+
+theorem concat_ext {x : BitVec a} {y : BitVec b} :
+  (x ++ y).val = x.val <<< b ||| y.val := rfl
+
+theorem extract_ext : (extract i j x).val = x.val/2^j % (2^(i - j + 1)) := by
+  simp [extract, BitVec.ofNat, Fin.ofNat', shiftRight_eq_shiftr, shiftr_eq_div_pow]
+
+theorem testBit_eq_rep {x: BitVec w} (i : Nat) (h: i< w): x[i] = testBit x.val i := by rfl
+
+theorem testBit_eq_rep' {x: Nat} (i : Nat) (h: i< w) (h2: x< 2^w):
+  (BitVec.ofNat w x)[i] = testBit x i := by
+  simp [BitVec.ofNat, GetElem.getElem, lsbGet, extract, Fin.ofNat', mod_eq_of_lt, h2]
+
+
+/-! ### Equivalence between bitwise and BitVec operations -/
+
+theorem BV_add {x y : BitVec w}: bitadd x.val y.val w = (x + y).val := by
+  rw [bitadd_eq_add]
+  norm_cast
+
+-- theorem BV_neg {x : BitVec w}: bitwise_neg x.val w = x.neg.val := by
+--   simp only [bitwise_neg_eq_neg, x.isLt]
+--   norm_cast
+
+-- theorem BV_mul {x y : BitVec w} (h : 0 < w): bitwise_mul x.val y.val w = (x * y).val := by
+--   rw [bitwise_mul_eq_mul h]
+--   norm_cast
+
+-- theorem BV_extract {x : BitVec w} : bitwise_extract x.val i j = (extract i j x).val := by
+--   rw [bitwise_extract_eq_extract]
+--   norm_cast
+
+-- theorem BV_concat {x : BitVec w} {y : BitVec v} :
+--  bitwise_concat y.val x.val v w  = (x ++ y).val := by
+--   rw [bitwise_concat_eq_concat y.isLt x.isLt]
+--   norm_cast
+
+-- theorem BV_eq {x y : BitVec w} (h: 0 < w): bitwise_eq x.val y.val w = (x = y) := by
+--   simp [← bitwise_eq_eq h x.isLt y.isLt]
+
+theorem BV_ult {x y : BitVec w} (h1: x < y) :
+  bitult x.val y.val w:= bitult_of_ult y.isLt (val_bitvec_lt.mpr h1)
+
+-- theorem BV_slt {x y : BitVec w} (h1: x < y) : bitwise_slt x.val y.val w:= sorry
+
+-- theorem BV_signExtend {x : BitVec w} (h: 0 < w) :
+-- (signExtend i x).val = bitwise_ext x.val w i := sorry
