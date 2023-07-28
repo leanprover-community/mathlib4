0d0c6b1c104e8a6b9de38d9b234672ddca123cf9
Christopher Hoskin <mans0954@users.noreply.github.com>
2023-07-28 18:10:50 +0100

    Update Mathlib/Order/Monotone/Basic.lean
    Co-authored-by: Anatole Dedecker <anatolededecker@gmail.com>

--- Mathlib/Order/Monotone/Basic.lean
+++ Mathlib/Order/Monotone/Basic.lean
@@ -246,7 +246,7 @@ protected theorem Antitone.dual (hf : Antitone f) : Antitone (toDual ∘ f ∘ o
   swap hf
 #align antitone.dual Antitone.dual
 
-protected theorem Monotone.dual_iff : Monotone f ↔ Monotone (toDual ∘ f ∘ ofDual : αᵒᵈ → βᵒᵈ) := by
+theorem monotone_dual_iff : Monotone f ↔ Monotone (toDual ∘ f ∘ ofDual : αᵒᵈ → βᵒᵈ) := by
   rw [monotone_toDual_comp_iff, antitone_comp_ofDual_iff]
 
 protected theorem Antitone.dual_iff : Antitone f ↔ Antitone (toDual ∘ f ∘ ofDual : αᵒᵈ → βᵒᵈ) := by
