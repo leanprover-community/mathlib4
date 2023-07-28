9fc71324b19113324723bff6af5a5cadea06c440
Christopher Hoskin <christopher.hoskin@gmail.com>
2023-07-28 07:42:55 +0100

    Port changes to StoneWeierstrass from mathlib PR

--- Mathlib/Topology/ContinuousFunction/StoneWeierstrass.lean
+++ Mathlib/Topology/ContinuousFunction/StoneWeierstrass.lean
@@ -118,7 +118,7 @@ theorem abs_mem_subalgebra_closure (A : Subalgebra ℝ C(X, ℝ)) (f : A) :
 
 theorem inf_mem_subalgebra_closure (A : Subalgebra ℝ C(X, ℝ)) (f g : A) :
     (f : C(X, ℝ)) ⊓ (g : C(X, ℝ)) ∈ A.topologicalClosure := by
-  rw [inf_eq]
+  rw [(inf_eq_half_smul_add_sub_abs_sub' ℝ)]
   refine'
     A.topologicalClosure.smul_mem
       (A.topologicalClosure.sub_mem
@@ -140,7 +140,7 @@ theorem inf_mem_closed_subalgebra (A : Subalgebra ℝ C(X, ℝ)) (h : IsClosed (
 
 theorem sup_mem_subalgebra_closure (A : Subalgebra ℝ C(X, ℝ)) (f g : A) :
     (f : C(X, ℝ)) ⊔ (g : C(X, ℝ)) ∈ A.topologicalClosure := by
-  rw [sup_eq]
+  rw [(sup_eq_half_smul_add_add_abs_sub' ℝ)]
   refine'
     A.topologicalClosure.smul_mem
       (A.topologicalClosure.add_mem
