9119cbf5acc6799e75e253848313580b757866d9
Joël Riou <37772949+joelriou@users.noreply.github.com>
2023-07-29 13:51:25 +0530

    Update Mathlib/CategoryTheory/CatCommSq.lean
    Co-authored-by: Markus Himmel <markus@himmel-villmar.de>

--- Mathlib/CategoryTheory/CatCommSq.lean
+++ Mathlib/CategoryTheory/CatCommSq.lean
@@ -3,7 +3,6 @@ Copyright (c) 2023 Joël Riou. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joël Riou
 -/
-
 import Mathlib.CategoryTheory.Equivalence
 
 /-! 2-commutative squares of functors
