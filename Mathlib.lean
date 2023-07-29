b0416072d42eab4b77a9dd84e2649f6bebb57a7f
Dagur Tómas Ásgeirsson <dagurtomas@gmail.com>
2023-07-29 10:38:01 +0000

    feat: explicit limits in Profinite (#5763)
    Co-authored by:
    
    - Riccardo Brasca [riccardo.brasca@gmail.com](mailto:riccardo.brasca@gmail.com) @riccardobrasca
    - Filippo A. E. Nuccio [filippo.nuccio@univ-st-etienne.fr](mailto:filippo.nuccio@univ-st-etienne.fr) @faenuccio
    
    Analogue of CompHaus/ExplicitLimits for Profinite.
    This work was done during the 2023 Copenhagen masterclass on formalisation of condensed mathematics. Numerous participants contributed.
    
    
    
    Co-authored-by: Jon Eugster <eugster.jon@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -3160,6 +3160,7 @@ import Mathlib.Topology.Category.Locale
 import Mathlib.Topology.Category.Profinite.AsLimit
 import Mathlib.Topology.Category.Profinite.Basic
 import Mathlib.Topology.Category.Profinite.CofilteredLimit
+import Mathlib.Topology.Category.Profinite.Limits
 import Mathlib.Topology.Category.Profinite.Projective
 import Mathlib.Topology.Category.Stonean.Basic
 import Mathlib.Topology.Category.Stonean.Limits
