61b9a4c7634cc3302c8f0c250e11033b43c9f374
Dagur Tómas Ásgeirsson <dagurtomas@gmail.com>
2023-07-29 10:38:00 +0000

    feat: some API for explicit limits in CompHaus (#5762)
    Co-authored by: 
    - Riccardo Brasca [riccardo.brasca@gmail.com](mailto:riccardo.brasca@gmail.com) @riccardobrasca 
    - Filippo A. E. Nuccio [filippo.nuccio@univ-st-etienne.fr](mailto:filippo.nuccio@univ-st-etienne.fr) @faenuccio 
    
    We add some more API for the compatibility of explicit and abstract limits in CompHaus
    This work was done during the 2023 Copenhagen masterclass on formalisation of condensed mathematics. Numerous participants contributed.

--- Mathlib.lean
+++ Mathlib.lean
@@ -3152,7 +3152,7 @@ import Mathlib.Topology.Bornology.Hom
 import Mathlib.Topology.Category.Born
 import Mathlib.Topology.Category.CompHaus.Basic
 import Mathlib.Topology.Category.CompHaus.EffectiveEpi
-import Mathlib.Topology.Category.CompHaus.ExplicitLimits
+import Mathlib.Topology.Category.CompHaus.Limits
 import Mathlib.Topology.Category.CompHaus.Projective
 import Mathlib.Topology.Category.Compactum
 import Mathlib.Topology.Category.Locale
