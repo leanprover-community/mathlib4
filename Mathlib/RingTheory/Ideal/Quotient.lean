33cd8b1f1395d92299e183304a59a8365cb15485
Eric Wieser <wieser.eric@gmail.com>
2023-07-29 10:58:27 +0000

    fix

--- Mathlib/RingTheory/Ideal/Quotient.lean
+++ Mathlib/RingTheory/Ideal/Quotient.lean
@@ -189,7 +189,7 @@ instance isDomain (I : Ideal R) [hI : I.IsPrime] : IsDomain (R ⧸ I) :=
 
 theorem isDomain_iff_prime (I : Ideal R) : IsDomain (R ⧸ I) ↔ I.IsPrime := by
   refine' ⟨fun H => ⟨zero_ne_one_iff.1 _, fun {x y} h => _⟩, fun h => inferInstance⟩
-  · haveI : Nontrivial (R ⧸ I) := ⟨H.2.1⟩
+  · haveI : Nontrivial (R ⧸ I) := ⟨H.exists_pair_ne⟩
     exact zero_ne_one
   · simp only [← eq_zero_iff_mem, (mk I).map_mul] at h ⊢
     haveI := @IsDomain.to_noZeroDivisors (R ⧸ I) _ H
