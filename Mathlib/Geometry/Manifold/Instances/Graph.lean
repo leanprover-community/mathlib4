219b456958ce22d5875013b3a0a82b949b76dbd7
Michael Lee <michael_lee1@brown.edu>
2023-07-30 16:16:23 -0400

    remove unused import

--- Mathlib/Geometry/Manifold/Instances/Graph.lean
+++ Mathlib/Geometry/Manifold/Instances/Graph.lean
@@ -3,7 +3,6 @@ Copyright (c) 2023 Michael Lee. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Michael Lee
 -/
-import Mathlib.Geometry.Manifold.ContMDiff
 import Mathlib.Geometry.Manifold.SmoothManifoldWithCorners
 
 /-!
