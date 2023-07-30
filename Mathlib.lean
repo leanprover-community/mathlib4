bcbf5be73f50008c2ace0b055b3f93072cffc0e1
Michael Lee <michael_lee1@brown.edu>
2023-07-30 15:48:31 -0400

    import file

--- Mathlib.lean
+++ Mathlib.lean
@@ -1919,6 +1919,7 @@ import Mathlib.Geometry.Manifold.ContMDiffMFDeriv
 import Mathlib.Geometry.Manifold.ContMDiffMap
 import Mathlib.Geometry.Manifold.DerivationBundle
 import Mathlib.Geometry.Manifold.Diffeomorph
+import Mathlib.Geometry.Manifold.Instances.Graph
 import Mathlib.Geometry.Manifold.Instances.Real
 import Mathlib.Geometry.Manifold.Instances.Sphere
 import Mathlib.Geometry.Manifold.Instances.UnitsOfNormedAlgebra
