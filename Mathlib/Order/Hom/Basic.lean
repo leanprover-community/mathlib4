1f7428a52ffa19853844ea54d7531db49ab53fd9
Scott Morrison <scott@tqft.net>
2023-07-31 14:06:21 +0000

    feat: Subtype.orderEmbedding (#6097)
    Co-authored-by: Scott Morrison <scott.morrison@gmail.com>

--- Mathlib/Order/Hom/Basic.lean
+++ Mathlib/Order/Hom/Basic.lean
@@ -545,6 +545,13 @@ def Subtype.val (p : α → Prop) : Subtype p →o α :=
 #align order_hom.subtype.val OrderHom.Subtype.val
 #align order_hom.subtype.val_coe OrderHom.Subtype.val_coe
 
+/-- `Subtype.impEmbedding` as an order embedding. -/
+@[simps!]
+def _root_.Subtype.orderEmbedding {p q : α → Prop} (h : ∀ a, p a → q a) :
+    {x // p x} ↪o {x // q x} :=
+  { Subtype.impEmbedding _ _ h with
+    map_rel_iff' := by aesop }
+
 /-- There is a unique monotone map from a subsingleton to itself. -/
 instance unique [Subsingleton α] : Unique (α →o α) where
   default := OrderHom.id
