b2f734073c7fe455e62e92edccd55ea9dc91ca23
Eric Wieser <wieser.eric@gmail.com>
2023-07-31 12:25:26 +0100

    fix copyright

--- Mathlib/Order/ULift.lean
+++ Mathlib/Order/ULift.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2023 Jeremy Avigad. All rights reserved.
+Copyright (c) 2023 Eric Wieser. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Eric Wieser
 -/
