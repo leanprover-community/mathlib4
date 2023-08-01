b64687705f456727e7fac75fc45e12c9c22773c1
Chris Hughes <chrishughes24@gmail.com>
2023-08-01 13:23:20 +0100

    realize_pow

--- Mathlib/ModelTheory/FieldTheory/AlgClosed.lean
+++ Mathlib/ModelTheory/FieldTheory/AlgClosed.lean
@@ -6,9 +6,19 @@ namespace FirstOrder
 namespace Language
 
 def pow (a : Language.field.Term α) (n : ℕ) : Language.field.Term α :=
-  Nat.recOn n a (λ _ x => a * x)
+  Nat.recOn n 1 (λ _ x => a * x)
 
 instance : Pow (Language.field.Term α) ℕ := ⟨pow⟩
+
+theorem pow_def (a : Language.field.Term α) (n : ℕ) :
+    a ^ n = pow a n := rfl
+
+@[simp]
+theorem realize_pow {K : Type _} [Field K]
+    (n : ℕ) (a : Language.field.Term α) (v : α → K) :
+    (a ^ n).realize v = (a.realize v) ^ n := by
+  induction n <;> simp_all [pow_def, pow, field.one_def, constantMap,
+    Term.realize, pow_succ]
 
 def polyOfDegree : ∀ (n : ℕ),
     Language.field.Term (Empty ⊕ Fin (n+2))
