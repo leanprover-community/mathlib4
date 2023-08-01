54d019e432eb68d86188f7e9b38f5b7d425c5987
Chris Hughes <chrishughes24@gmail.com>
2023-08-01 16:35:34 +0100

    think a bit more

--- Mathlib/ModelTheory/Algebra/Field/AlgClosed.lean
+++ Mathlib/ModelTheory/Algebra/Field/AlgClosed.lean
@@ -7,12 +7,42 @@ namespace FirstOrder
 
 namespace Language
 
-open field FreeCommRing BigOperators
+open field FreeCommRing BigOperators Polynomial
 
 variable {K : Type _} [Field K]
 
-def genMonicPoly (n : ℕ) : FreeCommRing (Fin (n + 1)) :=
-  of 0 ^ n + ∑ i : Fin n, of i.castSucc * of 0 ^ (i : ℕ)
+def genericPoly (supp : Finset ℕ) : FreeCommRing (Fin (n + 1)) :=
+  ∑ i ∈ supp, of
+
+theorem lift_genericMonicPoly (p : Polynomial K)
+    (hm : p.Monic) (x : K) : FreeCommRing.lift
+    (fun i => Fin.cases x (fun i => p.coeff i) i)
+    (genericMonicPoly p.natDegree) = p.eval x := by
+  simp only [genericMonicPoly, map_add, map_pow, lift_of,
+    Fin.cases_zero]
+  rw [map_sum]
+  simp
+
+
+
+
+noncomputable def genMonicPolyHasRoot (n : ℕ) : Language.field.Sentence :=
+  (∃' ((termOfFreeCommRing (genericMonicPoly n)).relabel Sum.inr =' 0)).alls
+
+theorem realize_genMonicPolyHasRoot (n : ℕ) :
+    K ⊨ genMonicPolyHasRoot n ↔ ∀ p : K[X],
+      p.natDegree = n → p.Monic → ∃ x, p.eval x = 0 := by
+  simp only [Sentence.Realize, genMonicPolyHasRoot,
+    BoundedFormula.realize_alls, BoundedFormula.realize_ex,
+    BoundedFormula.realize_bdEqual, Term.realize_relabel,
+    Sum.elim_comp_inr, realize_termOfFreeCommRing]
+  constructor
+  . rintro h p rfl hpm
+    rcases h (fun i => p.coeff i) with ⟨x, hx⟩
+    use x
+
+
+
 
 end Language
 
