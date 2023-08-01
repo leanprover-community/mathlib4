564672801e21df4b4d8c5ed4ce4d7accc16980ab
Chris Hughes <chrishughes24@gmail.com>
2023-08-01 15:39:59 +0100

    don't know what happened there

--- Mathlib/ModelTheory/Algebra/Field/FreeCommRing.lean
+++ Mathlib/ModelTheory/Algebra/Field/FreeCommRing.lean
@@ -1,34 +1,27 @@
 import Mathlib.ModelTheory.Algebra.Ring.FreeCommRing
-import Mathlib.RingTheory.FreeCommRing
+import Mathlib.ModelTheory.Algebra.Field.Basic
 
 namespace FirstOrder
 
 namespace Language
 
-theorem exists_term_realize_eq_freeCommRing (p : FreeCommRing α) :
-    ∃ t : Language.ring.Term α,
-      (t.realize FreeCommRing.of : FreeCommRing α) = p :=
-  FreeCommRing.induction_on p
-    ⟨-1, by simp [Term.realize]⟩
-    (fun a => ⟨Term.var a, by simp [Term.realize]⟩)
-    (fun x y ⟨t₁, ht₁⟩ ⟨t₂, ht₂⟩ =>
-      ⟨t₁ + t₂, by simp_all [Term.realize]⟩)
-    (fun x y ⟨t₁, ht₁⟩ ⟨t₂, ht₂⟩ =>
-      ⟨t₁ * t₂, by simp_all [Term.realize]⟩)
+namespace field
 
-noncomputable def termOfFreeCommRing (p : FreeCommRing α) : Language.ring.Term α :=
-  Classical.choose (exists_term_realize_eq_freeCommRing p)
+noncomputable def termOfFreeCommRing (p : FreeCommRing α) : Language.field.Term α :=
+  (ring.termOfFreeCommRing p).realize Term.var
 
-variable {R : Type _} [CommRing R]
+variable {K : Type _} [Field K]
 
-theorem realize_termOfFreeCommRing (p : FreeCommRing α) (v : α → R) :
+@[simp]
+theorem realize_termOfFreeCommRing (p : FreeCommRing α) (v : α → K) :
     (termOfFreeCommRing p).realize v = FreeCommRing.lift v p := by
-  have : (termOfFreeCommRing p).realize FreeCommRing.of = p :=
-    Classical.choose_spec (exists_term_realize_eq_freeCommRing p)
-  conv_rhs => rw [← this]
-  induction termOfFreeCommRing p with
+  conv_rhs => rw [← ring.realize_termOfFreeCommRing]
+  rw [termOfFreeCommRing]
+  induction ring.termOfFreeCommRing p with
   | var _ => simp
-  | func f _ ih => cases f <;> simp_all
+  | func f _ ih => cases f <;> simp_all [Term.realize]
+
+end field
 
 end Language
 
