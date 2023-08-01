79527cb586c66befb3d62a5b031424a47b6bef2d
Christopher Hoskin <christopher.hoskin@gmail.com>
2023-08-01 18:11:40 +0000

    feat(Topology/Order/UpperLowerSetTopology): Introduce the Upper Set (or Alexandrov) topology and the Lower Set topology (#5672)
    Introduces the Upper Set topology, which is the canonical example of an Alexandrov topology and its dual, the Lower Set topology.
    
    
    
    Co-authored-by: Christopher Hoskin <christopher.hoskin@overleaf.com>
    Co-authored-by: Christopher Hoskin <mans0954@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -3296,6 +3296,7 @@ import Mathlib.Topology.Order.Lattice
 import Mathlib.Topology.Order.LowerUpperTopology
 import Mathlib.Topology.Order.NhdsSet
 import Mathlib.Topology.Order.Priestley
+import Mathlib.Topology.Order.UpperLowerSetTopology
 import Mathlib.Topology.Paracompact
 import Mathlib.Topology.Partial
 import Mathlib.Topology.PartitionOfUnity
