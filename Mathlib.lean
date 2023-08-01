6e47ba90f9f8f78cf384d5504b28e072ad0a954c
Thomas Browning <tb65536@uw.edu>
2023-08-01 16:05:19 +0000

    refactor(FieldTheory/NormalClosure): move to new file (#6273)
    This PR moves `normalClosure` to a separate file, in preparation for #6163.
    
    
    
    Co-authored-by: Thomas Browning <tb65536@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1879,6 +1879,7 @@ import Mathlib.FieldTheory.Minpoly.Field
 import Mathlib.FieldTheory.Minpoly.IsIntegrallyClosed
 import Mathlib.FieldTheory.MvPolynomial
 import Mathlib.FieldTheory.Normal
+import Mathlib.FieldTheory.NormalClosure
 import Mathlib.FieldTheory.PerfectClosure
 import Mathlib.FieldTheory.PolynomialGaloisGroup
 import Mathlib.FieldTheory.PrimitiveElement
