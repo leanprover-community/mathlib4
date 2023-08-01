4642a0e47e540e7d1bd79cf6be448040cf6478b1
dagurtomas <dagurtomas@gmail.com>
2023-08-01 12:32:40 +0000

    remove some files

--- Mathlib/Topology/Category/Profinite/wf_mwe.lean
+++
@@ -1,7 +0,0 @@
-import Mathlib.Data.List.Lex
-
-variable (α : Type _) [LinearOrder α] [IsWellFounded α (·<·)]
-
-instance : IsWellFounded {l : List α // l.Chain' (·>·)}
-    (fun l m ↦ List.Lex (·<·) l.val m.val) :=
-  sorry
