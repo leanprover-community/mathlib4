416ee1d2a690fcdc3ca922da0d43d274c2211fb0
Eric Wieser <wieser.eric@gmail.com>
2023-08-02 14:17:49 +0000

    feat(LinearAlgebra/BilinearForm/TensorProduct): base change of bilinear forms

--- Mathlib/LinearAlgebra/BilinearForm/TensorProduct.lean
+++ Mathlib/LinearAlgebra/BilinearForm/TensorProduct.lean
@@ -5,6 +5,7 @@ Authors: Eric Wieser
 -/
 import Mathlib.LinearAlgebra.BilinearForm
 import Mathlib.LinearAlgebra.TensorProduct
+import Mathlib.LinearAlgebra.TensorProduct.Tower
 
 #align_import linear_algebra.bilinear_form.tensor_product from "leanprover-community/mathlib"@"f0c8bf9245297a541f468be517f1bde6195105e9"
 
@@ -23,31 +24,32 @@ import Mathlib.LinearAlgebra.TensorProduct
 
 universe u v w
 
-variable {ι : Type _} {R : Type _} {M₁ M₂ : Type _}
+variable {ι : Type _} {R A : Type _} {M₁ M₂ : Type _}
 
 open TensorProduct
 
 namespace BilinForm
 
 section CommSemiring
-
-variable [CommSemiring R]
-
-variable [AddCommMonoid M₁] [AddCommMonoid M₂]
-
-variable [Module R M₁] [Module R M₂]
+variables [CommSemiring R] [CommSemiring A]
+variables [AddCommMonoid M₁] [AddCommMonoid M₂]
+variables [Algebra R A] [Module R M₁] [Module A M₁]
+variables [SMulCommClass R A M₁] [SMulCommClass A R M₁] [SMulCommClass R A A] [IsScalarTower R A M₁]
+variables [Module R M₂]
 
 /-- The tensor product of two bilinear forms injects into bilinear forms on tensor products. -/
-def tensorDistrib : BilinForm R M₁ ⊗[R] BilinForm R M₂ →ₗ[R] BilinForm R (M₁ ⊗[R] M₂) :=
-  ((TensorProduct.tensorTensorTensorComm R _ _ _ _).dualMap ≪≫ₗ
-    (TensorProduct.lift.equiv R _ _ _).symm ≪≫ₗ LinearMap.toBilin).toLinearMap ∘ₗ
-  TensorProduct.dualDistrib R _ _ ∘ₗ
-  (TensorProduct.congr (BilinForm.toLin ≪≫ₗ TensorProduct.lift.equiv R _ _ _)
+def tensorDistrib : BilinForm A M₁ ⊗[R] BilinForm R M₂ →ₗ[R] BilinForm A (M₁ ⊗[R] M₂) :=
+  ((TensorProduct.AlgebraTensorModule.tensorTensorTensorComm R A M₁ M₂ M₁ M₂).dualMap
+    ≪≫ₗ (TensorProduct.lift.equiv A (M₁ ⊗[R] M₂) (M₁ ⊗[R] M₂) A).symm
+    ≪≫ₗ LinearMap.toBilin).toLinearMap
+  ∘ₗ TensorProduct.AlgebraTensorModule.dualDistrib R _ _
+  ∘ₗ (TensorProduct.AlgebraTensorModule.congr
+    (BilinForm.toLin ≪≫ₗ TensorProduct.lift.equiv R _ _ _)
     (BilinForm.toLin ≪≫ₗ TensorProduct.lift.equiv R _ _ _)).toLinearMap
 #align bilin_form.tensor_distrib BilinForm.tensorDistrib
 
 @[simp]
-theorem tensorDistrib_tmul (B₁ : BilinForm R M₁) (B₂ : BilinForm R M₂) (m₁ : M₁) (m₂ : M₂)
+theorem tensorDistrib_tmul (B₁ : BilinForm A M₁) (B₂ : BilinForm R M₂) (m₁ : M₁) (m₂ : M₂)
     (m₁' : M₁) (m₂' : M₂) :
     tensorDistrib (R := R) (B₁ ⊗ₜ B₂) (m₁ ⊗ₜ m₂) (m₁' ⊗ₜ m₂') = B₁ m₁ m₁' * B₂ m₂ m₂' :=
   rfl
@@ -55,7 +57,7 @@ theorem tensorDistrib_tmul (B₁ : BilinForm R M₁) (B₂ : BilinForm R M₂) (
 
 /-- The tensor product of two bilinear forms, a shorthand for dot notation. -/
 @[reducible]
-protected def tmul (B₁ : BilinForm R M₁) (B₂ : BilinForm R M₂) : BilinForm R (M₁ ⊗[R] M₂) :=
+protected def tmul (B₁ : BilinForm A M₁) (B₂ : BilinForm R M₂) : BilinForm A (M₁ ⊗[R] M₂) :=
   tensorDistrib (R := R) (B₁ ⊗ₜ[R] B₂)
 #align bilin_form.tmul BilinForm.tmul
 
