0c8f33523fc204419eb366de3599edbabce1f77f
Matthew Robert Ballard <100034030+mattrobball@users.noreply.github.com>
2023-08-02 10:36:59 -0400

    Update Basic.lean

--- Mathlib/Data/Rat/Basic.lean
+++ Mathlib/Data/Rat/Basic.lean
@@ -42,20 +42,6 @@ instance field : Field ℚ :=
     ratCast_mk := fun a b h1 h2 => (num_div_den _).symm
     qsmul := (· * ·) }
 
--- instance og : Field ℚ :=
---   { Rat.commRing, Rat.commGroupWithZero with
---     zero := 0
---     add := (· + ·)
---     neg := Neg.neg
---     one := 1
---     mul := (· * ·)
---     inv := Inv.inv
---     ratCast := Rat.cast
---     ratCast_mk := fun a b h1 h2 => (num_div_den _).symm
---     qsmul := (· * ·) }
---
--- example : field = og := rfl
-
 -- Extra instances to short-circuit type class resolution
 instance divisionRing : DivisionRing ℚ := by infer_instance
 
