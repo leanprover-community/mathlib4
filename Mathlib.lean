100debf7e8c93f7fb42d8841ed15a266aa75e301
damiano <adomani@gmail.com>
2023-08-02 15:54:04 +0000

    feat(Tactic/ExtractGoal + test/ExtractGoal): port `extract_goal` tactic (#4595)
    This is a first PR porting some of the functionality of the `extract_goal` tactic from mathlib3.
    
    
    
    Co-authored-by: Alex J Best <alex.j.best@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -2965,6 +2965,7 @@ import Mathlib.Tactic.Existsi
 import Mathlib.Tactic.Explode
 import Mathlib.Tactic.Explode.Datatypes
 import Mathlib.Tactic.Explode.Pretty
+import Mathlib.Tactic.ExtractGoal
 import Mathlib.Tactic.ExtractLets
 import Mathlib.Tactic.FBinop
 import Mathlib.Tactic.FailIfNoProgress
