113338b114ecd923676518eb9286c6e865dde118
Scott Morrison <scott.morrison@gmail.com>
2023-08-02 19:20:14 +1000

    max heartbeats

--- Mathlib/LinearAlgebra/Charpoly/ToMatrix.lean
+++ Mathlib/LinearAlgebra/Charpoly/ToMatrix.lean
@@ -36,6 +36,7 @@ namespace LinearMap
 
 section Basic
 
+set_option maxHeartbeats 250000 in
 /-- `charpoly f` is the characteristic polynomial of the matrix of `f` in any basis. -/
 @[simp]
 theorem charpoly_toMatrix {ι : Type w} [Fintype ι] (b : Basis ι R M) :
