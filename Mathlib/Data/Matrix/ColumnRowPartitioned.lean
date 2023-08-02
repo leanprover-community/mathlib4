51275ea27ccc42a2895510d3bbd8304715f321b0
MohanadAhmed <m.a.m.elhassan@gmail.com>
2023-08-02 15:59:18 +0300

    @Vierkantor golfs some proofs into oblivion
    - Rfl proofs of simp lemmas.
    - ext combined with simp for partitioned matrix equalities
    - pass arguments into simp when they are not simp lemmas i.e. mul_apply
    
    Co-authored-by: Anne Baanen <Vierkantor@users.noreply.github.com>

--- Mathlib/Data/Matrix/ColumnRowPartitioned.lean
+++ Mathlib/Data/Matrix/ColumnRowPartitioned.lean
@@ -54,33 +54,71 @@ def toRows₁ (A : Matrix (m₁ ⊕ m₂) n R) : Matrix m₁ n R := of fun i j =
 def toRows₂ (A : Matrix (m₁ ⊕ m₂) n R) : Matrix m₂ n R := of fun i j => (A (Sum.inr i) j)
 
 @[simp]
+lemma fromRows_apply_inl (A₁ : Matrix m₁ n R) (A₂ : Matrix m₂ n R) (i : m₁) (j : n) :
+    (fromRows A₁ A₂) (Sum.inl i) j = A₁ i j := rfl
+
+@[simp]
+lemma fromRows_apply_inr (A₁ : Matrix m₁ n R) (A₂ : Matrix m₂ n R) (i : m₂) (j : n) :
+    (fromRows A₁ A₂) (Sum.inr i) j = A₂ i j := rfl
+
+@[simp]
+lemma fromColumns_apply_inl (A₁ : Matrix m n₁ R) (A₂ : Matrix m n₂ R) (i : m) (j : n₁) :
+    (fromColumns A₁ A₂) i (Sum.inl j) = A₁ i j := rfl
+
+@[simp]
+lemma fromColumns_apply_inr (A₁ : Matrix m n₁ R) (A₂ : Matrix m n₂ R) (i : m) (j : n₂) :
+    (fromColumns A₁ A₂) i (Sum.inr j) = A₂ i j := rfl
+
+@[simp]
+lemma toRows₁_apply (A : Matrix (m₁ ⊕ m₂) n R) (i : m₁) (j : n) :
+    (toRows₁ A) i j = A (Sum.inl i) j := rfl
+
+@[simp]
+lemma toRows₂_apply (A : Matrix (m₁ ⊕ m₂) n R) (i : m₂) (j : n) :
+    (toRows₂ A) i j = A (Sum.inr i) j := rfl
+
+@[simp]
+lemma toRows₁_fromRows  (A₁ : Matrix m₁ n R) (A₂ : Matrix m₂ n R) :
+    toRows₁ (fromRows A₁ A₂) = A₁ := rfl
+
+@[simp]
+lemma toRows₂_fromRows  (A₁ : Matrix m₁ n R) (A₂ : Matrix m₂ n R) :
+    toRows₂ (fromRows A₁ A₂) = A₂ := rfl
+
+@[simp]
+lemma toColumns₁_apply (A : Matrix m (n₁ ⊕ n₂) R) (i : m) (j : n₁) :
+    (toColumns₁ A) i j = A i (Sum.inl j) := rfl
+
+@[simp]
+lemma toColumns₂_apply (A : Matrix m (n₁ ⊕ n₂) R) (i : m) (j : n₂) :
+    (toColumns₂ A) i j = A i (Sum.inr j) := rfl
+
+@[simp]
+lemma toColumns₁_fromColumns  (A₁ : Matrix m n₁ R) (A₂ : Matrix m n₂ R) :
+    toColumns₁ (fromColumns A₁ A₂) = A₁ := rfl
+
+@[simp]
+lemma toColumns₂_fromColumns  (A₁ : Matrix m n₁ R) (A₂ : Matrix m n₂ R) :
+    toColumns₂ (fromColumns A₁ A₂) = A₂ := rfl
+
+@[simp]
 lemma fromColumns_toColumns (A : Matrix m (n₁ ⊕ n₂) R) :
     fromColumns A.toColumns₁ A.toColumns₂ = A := by
-  unfold fromColumns toColumns₁ toColumns₂
-  funext i j
-  cases' j
-  all_goals (simp only [of_apply, Sum.elim_inl, Sum.elim_inr])
+  ext i (j | j) <;> simp
 
 @[simp]
 lemma fromRows_toRows (A : Matrix (m₁ ⊕ m₂) n R) : fromRows A.toRows₁ A.toRows₂ = A := by
-  unfold fromRows toRows₁ toRows₂
-  funext i j
-  cases' i
-  all_goals (simp only [of_apply, Sum.elim_inl, Sum.elim_inr])
+  ext (i | i) j <;> simp
 
 lemma fromRows_inj : Function.Injective2 (@fromRows R m₁ m₂ n) := by
   intros x1 x2 y1 y2
-  unfold fromRows
-  simp_rw [Function.funext_iff, ← Matrix.ext_iff, EmbeddingLike.apply_eq_iff_eq,
-    of_apply, Sum.forall, Sum.elim_inl, Sum.elim_inr]
-  exact (fun h => ⟨ fun _ _ => h.1 _ _ , fun _ _ => h.2 _ _ ⟩)
+  simp only [Function.funext_iff, ← Matrix.ext_iff]
+  aesop
 
 lemma fromColumns_inj : Function.Injective2 (@fromColumns R m n₁ n₂) := by
   intros x1 x2 y1 y2
-  unfold fromColumns
-  simp_rw [Function.funext_iff, ← Matrix.ext_iff, EmbeddingLike.apply_eq_iff_eq,
-    of_apply, Sum.forall, Sum.elim_inl, Sum.elim_inr]
-  exact (fun h => ⟨fun _ _ => (h _).1 _, fun _ _ => (h _).2 _⟩)
+  simp only [Function.funext_iff, ← Matrix.ext_iff]
+  aesop
 
 lemma fromColumns_ext_iff (A₁ : Matrix m n₁ R) (A₂ : Matrix m n₂ R) (B₁ : Matrix m n₁ R)
     (B₂ : Matrix m n₂ R) :
@@ -90,87 +128,17 @@ lemma fromRows_ext_iff (A₁ : Matrix m₁ n R) (A₂ : Matrix m₂ n R) (B₁ :
     (B₂ : Matrix m₂ n R) :
     fromRows A₁ A₂ = fromRows B₁ B₂ ↔ A₁ = B₁ ∧ A₂ = B₂ := fromRows_inj.eq_iff
 
-@[simp]
-lemma fromRows_apply_inl (A₁ : Matrix m₁ n R) (A₂ : Matrix m₂ n R) (i : m₁) (j : n) :
-    (fromRows A₁ A₂) (Sum.inl i) j = A₁ i j := by
-  unfold fromRows
-  simp only [of_apply, Sum.elim_inl]
-
-@[simp]
-lemma fromRows_apply_inr (A₁ : Matrix m₁ n R) (A₂ : Matrix m₂ n R) (i : m₂) (j : n) :
-    (fromRows A₁ A₂) (Sum.inr i) j = A₂ i j := by
-  unfold fromRows
-  simp only [of_apply, Sum.elim_inr]
-
-@[simp]
-lemma fromColumns_apply_inl (A₁ : Matrix m n₁ R) (A₂ : Matrix m n₂ R) (i : m) (j : n₁) :
-    (fromColumns A₁ A₂) i (Sum.inl j) = A₁ i j := by
-  unfold fromColumns
-  simp only [of_apply, Sum.elim_inl]
-
-@[simp]
-lemma fromColumns_apply_inr (A₁ : Matrix m n₁ R) (A₂ : Matrix m n₂ R) (i : m) (j : n₂) :
-    (fromColumns A₁ A₂) i (Sum.inr j) = A₂ i j := by
-  unfold fromColumns
-  simp only [of_apply, Sum.elim_inr]
-
-@[simp]
-lemma toRows₁_apply (A : Matrix (m₁ ⊕ m₂) n R) (i : m₁) (j : n) :
-    (toRows₁ A) i j = A (Sum.inl i) j := by rw [toRows₁, of_apply]
-
-@[simp]
-lemma toRows₂_apply (A : Matrix (m₁ ⊕ m₂) n R) (i : m₂) (j : n) :
-    (toRows₂ A) i j = A (Sum.inr i) j := by rw [toRows₂, of_apply]
-
-@[simp]
-lemma toRows₁_fromRows  (A₁ : Matrix m₁ n R) (A₂ : Matrix m₂ n R) :
-    toRows₁ (fromRows A₁ A₂) = A₁ := by
-  funext i j
-  simp only [toRows₁_apply, fromRows_apply_inl]
-
-@[simp]
-lemma toRows₂_fromRows  (A₁ : Matrix m₁ n R) (A₂ : Matrix m₂ n R) :
-    toRows₂ (fromRows A₁ A₂) = A₂ := by
-  funext i j
-  simp only [toRows₂_apply, fromRows_apply_inr]
-
-@[simp]
-lemma toColumns₁_apply (A : Matrix m (n₁ ⊕ n₂) R) (i : m) (j : n₁) :
-    (toColumns₁ A) i j = A i (Sum.inl j) := by rw [toColumns₁, of_apply]
-
-@[simp]
-lemma toColumns₂_apply (A : Matrix m (n₁ ⊕ n₂) R) (i : m) (j : n₂) :
-    (toColumns₂ A) i j = A i (Sum.inr j) := by rw [toColumns₂, of_apply]
-
-@[simp]
-lemma toColumns₁_fromColumns  (A₁ : Matrix m n₁ R) (A₂ : Matrix m n₂ R) :
-    toColumns₁ (fromColumns A₁ A₂) = A₁ := by
-    funext i j
-    simp only [toColumns₁_apply, fromColumns_apply_inl]
-
-@[simp]
-lemma toColumns₂_fromColumns  (A₁ : Matrix m n₁ R) (A₂ : Matrix m n₂ R) :
-    toColumns₂ (fromColumns A₁ A₂) = A₂ := by
-    ext i j
-    simp only [toColumns₂_apply, fromColumns_apply_inr]
-
 /- A column partioned matrix when transposed gives a row partioned matrix with columns of the
 initial matrix tranposed to become rows. -/
 lemma transpose_fromColumns (A₁ : Matrix m n₁ R) (A₂ : Matrix m n₂ R) :
     transpose (fromColumns A₁ A₂) = fromRows (transpose A₁) (transpose A₂) := by
-  funext i j
-  cases' i with i i
-  all_goals (simp only [transpose_apply, fromColumns_apply_inl, fromRows_apply_inl,
-    fromColumns_apply_inr, fromRows_apply_inr])
+  ext (i | i) j <;> simp
 
 /- A row partioned matrix when transposed gives a column partioned matrix with rows of the initial
 matrix tranposed to become columns. -/
 lemma transpose_fromRows (A₁ : Matrix m₁ n R) (A₂ : Matrix m₂ n R) :
     transpose (fromRows A₁ A₂) = fromColumns (transpose A₁) (transpose A₂) := by
-  funext i j
-  cases' j with j j
-  all_goals (simp only [transpose_apply, fromRows_apply_inl, fromColumns_apply_inl,
-    fromColumns_apply_inr, fromRows_apply_inr])
+  ext i (j | j) <;> simp
 
 section Semiring
 
@@ -179,94 +147,61 @@ variable [Semiring R]
 @[simp]
 lemma fromRows_mul (A₁ : Matrix m₁ n R) (A₂ : Matrix m₂ n R) (B : Matrix n m R) :
     (fromRows A₁ A₂) ⬝ B = fromRows (A₁ ⬝ B) (A₂ ⬝ B) := by
-  funext i j
-  cases' i with i i
-  all_goals (simp only [mul_apply, fromRows_apply_inl, fromRows_apply_inr])
+  ext (_ | _) _ <;> simp [mul_apply]
 
 @[simp]
 lemma mul_fromColumns (A : Matrix m n R) (B₁ : Matrix n n₁ R) (B₂ : Matrix n n₂ R) :
     A ⬝ (fromColumns B₁ B₂) = fromColumns (A ⬝ B₁) (A ⬝ B₂) := by
-  funext i j
-  cases' j with j j
-  all_goals (simp only [mul_apply, fromColumns_apply_inl, fromColumns_apply_inr] )
+  ext _ (_ | _) <;> simp [mul_apply]
 
 @[simp]
 lemma fromRows_zero : fromRows (0 : Matrix m₁ n R) (0 : Matrix m₂ n R) = 0 := by
-  funext i j
-  unfold fromRows
-  simp only [Sum.elim_zero_zero, of_apply, Pi.zero_apply, zero_apply]
+  ext (_ | _) _ <;> simp
 
 @[simp]
 lemma fromColumns_zero : fromColumns (0 : Matrix m n₁ R) (0 : Matrix m n₂ R) = 0 := by
-  funext i j
-  cases' j with j j
-  all_goals (simp only [fromColumns_apply_inl, fromColumns_apply_inr, zero_apply])
+  ext _ (_ | _) <;> simp
 
 @[simp]
 lemma fromColumns_fromRows_eq_fromBlocks (B₁₁ : Matrix m₁ n₁ R) (B₁₂ : Matrix m₁ n₂ R)
     (B₂₁ : Matrix m₂ n₁ R) (B₂₂ : Matrix m₂ n₂ R) :
     fromColumns (fromRows B₁₁ B₂₁) (fromRows B₁₂ B₂₂) = fromBlocks B₁₁ B₁₂ B₂₁ B₂₂ := by
-  funext i j
-  cases' i with i i
-  all_goals (
-    cases' j with j j
-    all_goals (simp only [fromColumns_apply_inl, fromColumns_apply_inr,
-      fromRows_apply_inl, fromRows_apply_inr, fromBlocks_apply₁₁, fromBlocks_apply₁₂,
-      fromBlocks_apply₂₁,  fromBlocks_apply₂₂] ) )
+  ext (_ | _) (_ | _) <;> simp
 
 @[simp]
 lemma fromRows_fromColumn_eq_fromBlocks (B₁₁ : Matrix m₁ n₁ R) (B₁₂ : Matrix m₁ n₂ R)
     (B₂₁ : Matrix m₂ n₁ R) (B₂₂ : Matrix m₂ n₂ R) :
     fromRows (fromColumns B₁₁ B₁₂) (fromColumns B₂₁ B₂₂) = fromBlocks B₁₁ B₁₂ B₂₁ B₂₂ := by
-  funext i j
-  cases' i with i i
-  all_goals (cases' j with j j)
-  all_goals (simp only [fromColumns_apply_inl, fromColumns_apply_inr,
-    fromRows_apply_inl, fromRows_apply_inr, fromBlocks_apply₁₁, fromBlocks_apply₁₂,
-    fromBlocks_apply₂₁,  fromBlocks_apply₂₂] )
+  ext (_ | _) (_ | _) <;> simp
 
 /-- A row partitioned matrix multiplied by a column partioned matrix gives a 2 by 2 block matrix -/
 lemma fromRows_mul_fromColumns (A₁ : Matrix m₁ n R) (A₂ : Matrix m₂ n R)
     (B₁ : Matrix n n₁ R) (B₂ : Matrix n n₂ R) :
     (fromRows A₁ A₂) ⬝ (fromColumns B₁ B₂) =
       fromBlocks (A₁ ⬝ B₁) (A₁ ⬝ B₂) (A₂ ⬝ B₁) (A₂ ⬝ B₂) := by
-  funext i j
-  cases' i with i i;
-  all_goals (cases' j with j j)
-  all_goals ( simp only [mul_fromColumns, fromRows_mul, fromColumns_apply_inl,
-    fromColumns_apply_inr, fromRows_apply_inl, fromRows_apply_inr, fromBlocks_apply₁₁,
-    fromBlocks_apply₁₂, fromBlocks_apply₂₁, fromBlocks_apply₂₂] )
+  ext (_ | _) (_ | _) <;> simp
 
 /-- A column partitioned matrix mulitplied by a row partitioned matrix gives the sum of the "outer"
 products of the block matrices -/
 lemma fromColumns_mul_fromRows (A₁ : Matrix m n₁ R) (A₂ : Matrix m n₂ R)
     (B₁ : Matrix n₁ n R) (B₂ : Matrix n₂ n R) :
     fromColumns A₁ A₂ ⬝ fromRows B₁ B₂ = (A₁ ⬝ B₁ + A₂ ⬝ B₂) := by
-  funext i j
-  rw [fromRows, fromColumns]
-  simp only [add_apply, mul_apply, of_apply, Fintype.sum_sum_type, Sum.elim_inl, Sum.elim_inr]
+  ext
+  simp [mul_apply]
 
 /-- A column partitioned matrix multipiled by a block matrix results in a column partioned matrix -/
 lemma fromColumns_mul_fromBlocks (A₁ : Matrix m m₁ R) (A₂ : Matrix m m₂ R)
     (B₁₁ : Matrix m₁ n₁ R) (B₁₂ : Matrix m₁ n₂ R) (B₂₁ : Matrix m₂ n₁ R) (B₂₂ : Matrix m₂ n₂ R) :
     (fromColumns A₁ A₂) ⬝ fromBlocks B₁₁ B₁₂ B₂₁ B₂₂ =
       fromColumns (A₁ ⬝ B₁₁ + A₂ ⬝ B₂₁) (A₁ ⬝ B₁₂ + A₂ ⬝ B₂₂) := by
-  funext i j
-  rw [fromColumns, fromColumns, fromBlocks]
-  cases j
-  all_goals simp only [of_apply, add_apply, Fintype.sum_sum_type, Sum.elim_inl,
-    Sum.elim_inr, mul_apply]
+  ext _ (_ | _) <;> simp [mul_apply]
 
 /-- A block matrix mulitplied by a row partitioned matrix gives a row partitioned matrix -/
 lemma fromBlocks_mul_fromRows (A₁ : Matrix n₁ n R) (A₂ : Matrix n₂ n R)
     (B₁₁ : Matrix m₁ n₁ R) (B₁₂ : Matrix m₁ n₂ R) (B₂₁ : Matrix m₂ n₁ R) (B₂₂ : Matrix m₂ n₂ R) :
     fromBlocks B₁₁ B₁₂ B₂₁ B₂₂ ⬝ (fromRows A₁ A₂) =
       fromRows (B₁₁ ⬝ A₁ + B₁₂ ⬝ A₂) (B₂₁ ⬝ A₁ + B₂₂ ⬝ A₂) := by
-  funext i j
-  rw [fromRows, fromRows, fromBlocks]
-  cases i
-  all_goals simp only [of_apply, add_apply, Fintype.sum_sum_type, Sum.elim_inl, Sum.elim_inr,
-    mul_apply]
+  ext (_ | _) _ <;> simp [mul_apply]
 
 end Semiring
 
@@ -282,17 +217,16 @@ The condition `e : n ≃ n₁ ⊕ n₂` states that `fromColumns A₁ A₂` and 
 lemma fromColumns_mul_fromRows_eq_one_comm (e : n ≃ n₁ ⊕ n₂)
     (A₁ : Matrix n n₁ R) (A₂ : Matrix n n₂ R) (B₁ : Matrix n₁ n R) (B₂ : Matrix n₂ n R) :
     fromColumns A₁ A₂ ⬝ fromRows B₁ B₂ = 1 ↔ fromRows B₁ B₂ ⬝ fromColumns A₁ A₂ = 1 := by
-  constructor
-  intro h
-  rw [← Matrix.submatrix_id_id (fromRows B₁ B₂ ⬝ fromColumns A₁ A₂),
-    ← Matrix.submatrix_mul_equiv (fromRows B₁ B₂) (fromColumns A₁ A₂) _ e.symm _]
-  swap
-  intro h
-  rw [← Matrix.submatrix_id_id (fromColumns A₁ A₂ ⬝ fromRows B₁ B₂),
-    ← Matrix.submatrix_mul_equiv (fromColumns A₁ A₂) (fromRows B₁ B₂) _ e _]
-  all_goals
-  ( rw [mul_eq_one_comm, ← Matrix.submatrix_mul, h, submatrix_one_equiv]
-    exact Function.bijective_id )
+  calc fromColumns A₁ A₂ ⬝ fromRows B₁ B₂ = 1
+  _ ↔ submatrix (fromColumns A₁ A₂) id e ⬝ submatrix (fromRows B₁ B₂) e id = 1 := by
+    simp
+  _ ↔ submatrix (fromRows B₁ B₂) e id ⬝ submatrix (fromColumns A₁ A₂) id e = 1 :=
+    mul_eq_one_comm
+  _ ↔ reindex e.symm e.symm (fromRows B₁ B₂ ⬝ fromColumns A₁ A₂) = reindex e.symm e.symm 1 := by
+    simp only [reindex_apply, Equiv.symm_symm, submatrix_one_equiv,
+        submatrix_mul (he₂ := Function.bijective_id)]
+  _ ↔ fromRows B₁ B₂ ⬝ fromColumns A₁ A₂ = 1 :=
+    (reindex _ _).injective.eq_iff
 
 /- The lemma `fromColumns_mul_fromRows_eq_one_comm` specialized to the case where the index sets n₁
 and n₂, are the result of subtyping by a predicate and its complement. -/
@@ -306,26 +240,20 @@ end CommRing
 
 section Star
 variable [Star R]
+
 /- A column partioned matrix in a Star ring when conjugate transposed gives a row partitioned matrix
 with the columns of the initial matrix conjugate transposed to become rows. -/
 lemma conjTranspose_fromColumns_eq_fromRows_conjTranspose (A₁ : Matrix m n₁ R)
     (A₂ : Matrix m n₂ R) :
     conjTranspose (fromColumns A₁ A₂) = fromRows (conjTranspose A₁) (conjTranspose A₂) := by
-  funext i j
-  cases' i with i i
-  all_goals (simp only [conjTranspose_apply, fromColumns_apply_inl, fromRows_apply_inl,
-    fromColumns_apply_inr, fromRows_apply_inr] )
+  ext (_ | _) _ <;> simp
 
 /- A row partioned matrix in a Star ring when conjugate transposed gives a column partitioned matrix
 with the rows of the initial matrix conjugate transposed to become columns. -/
 lemma conjTranspose_fromRows_eq_fromColumns_conjTranspose (A₁ : Matrix m₁ n R)
     (A₂ : Matrix m₂ n R) : conjTranspose (fromRows A₁ A₂) =
       fromColumns (conjTranspose A₁) (conjTranspose A₂) := by
-  /- I left this one for comparison. I think this proof looks more compact (nice)! -/
-  rw [fromColumns, fromRows]
-  funext j i
-  cases' i with i i
-  all_goals (simp only [conjTranspose_apply, of_apply, Sum.elim_inl, Sum.elim_inr])
+  ext _ (_ | _) <;> simp
 
 end Star
 
