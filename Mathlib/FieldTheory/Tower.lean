f01422027d9271188933da82fc73f8304a897576
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2023-08-02 15:31:19 +0200

    fixes

--- Mathlib/FieldTheory/Tower.lean
+++ Mathlib/FieldTheory/Tower.lean
@@ -35,7 +35,7 @@ tower law
 
 universe u v w u₁ v₁ w₁
 
-open Classical BigOperators FiniteDimensional Cardinal
+open BigOperators Cardinal Submodule
 
 variable (F : Type u) (K : Type v) (A : Type w)
 
