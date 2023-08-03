3306cc24a84584732a4d06dd67b68f2d6bbec21f
Arend Mellendijk <arend.mellendijk@gmail.com>
2023-08-03 18:38:08 +0200

    naming

--- Mathlib/Data/Nat/Squarefree.lean
+++ Mathlib/Data/Nat/Squarefree.lean
@@ -391,16 +391,16 @@ theorem squarefree_mul_iff {m n : ℕ} :
   ⟨fun h => ⟨coprime_of_squarefree_mul h, (squarefree_mul $ coprime_of_squarefree_mul h).mp h⟩,
     fun h => (squarefree_mul h.1).mpr h.2⟩
 
-theorem prod_toFinset_factors_of_squarefree {n : ℕ} (hn : Squarefree n) :
+theorem prod_factors_toFinset_of_squarefree {n : ℕ} (hn : Squarefree n) :
     ∏ p in n.factors.toFinset, p = n := by
   erw [List.prod_toFinset _ hn.nodup_factors, List.map_id, Nat.prod_factors hn.ne_zero]
 
-theorem prod_factors_sdiff_of_squarefree {n : ℕ} (hn : Squarefree n) {t : Finset ℕ}
+theorem prod_factors_toFinset_sdiff_of_squarefree {n : ℕ} (hn : Squarefree n) {t : Finset ℕ}
     (ht : t ⊆ n.factors.toFinset) :
     ∏ a in (n.factors.toFinset \ t), a = n / ∏ a in t, a := by
   refine symm $ Nat.div_eq_of_eq_mul_left (Finset.prod_pos
     fun p hp => (prime_of_mem_factors (List.mem_toFinset.mp (ht hp))).pos) ?_
-  rw [Finset.prod_sdiff ht, prod_toFinset_factors_of_squarefree hn]
+  rw [Finset.prod_sdiff ht, prod_factors_toFinset_of_squarefree hn]
 
 end Nat
 
