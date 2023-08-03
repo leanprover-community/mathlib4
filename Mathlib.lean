d3112bf0891834a1b1919b5357a81922dc73f9c8
Jireh Loreaux <loreaujy@gmail.com>
2023-08-03 14:14:07 -0500

    rename file

--- Mathlib.lean
+++ Mathlib.lean
@@ -13,9 +13,9 @@ import Mathlib.Algebra.Algebra.Spectrum
 import Mathlib.Algebra.Algebra.Subalgebra.Basic
 import Mathlib.Algebra.Algebra.Subalgebra.Pointwise
 import Mathlib.Algebra.Algebra.Subalgebra.Tower
+import Mathlib.Algebra.Algebra.Subalgebra.Unitization
 import Mathlib.Algebra.Algebra.Tower
 import Mathlib.Algebra.Algebra.Unitization
-import Mathlib.Algebra.Algebra.Unitize
 import Mathlib.Algebra.AlgebraicCard
 import Mathlib.Algebra.Associated
 import Mathlib.Algebra.BigOperators.Associated
