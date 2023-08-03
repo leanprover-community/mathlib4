4b508821543cc708722d299070e934ef3f7a8f3f
Scott Morrison <scott.morrison@gmail.com>
2023-08-03 13:29:28 +1000

    fix

--- Mathlib/Tactic/Rewrites.lean
+++ Mathlib/Tactic/Rewrites.lean
@@ -216,9 +216,9 @@ elab_rules : tactic |
       if lucky.isSome then
         match results[0]? with
         | some r => do
-           withMCtx r.mctx do
-              let replaceResult ← goal.replaceLocalDecl f r.result.eNew r.result.eqProof
-              replaceMainGoal (replaceResult.mvarId :: r.result.mvarIds)
+            setMCtx r.mctx
+            let replaceResult ← goal.replaceLocalDecl f r.result.eNew r.result.eqProof
+            replaceMainGoal (replaceResult.mvarId :: r.result.mvarIds)
         | _ => failure
     -- See https://github.com/leanprover/lean4/issues/2150
     do withMainContext do
