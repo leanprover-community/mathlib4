94e88c9eca122cc698ef15da7dc46844b63162ed
Peter Nelson <71660771+apnelson1@users.noreply.github.com>
2023-08-03 08:41:17 -0400

    mathlib.lean fix

--- Mathlib.lean
+++ Mathlib.lean
@@ -1776,7 +1776,7 @@ import Mathlib.Data.Set.Lattice
 import Mathlib.Data.Set.List
 import Mathlib.Data.Set.MulAntidiagonal
 import Mathlib.Data.Set.NAry
-import Mathlib.Data.Set.Ncard
+import Mathlib.Data.Set.Card
 import Mathlib.Data.Set.Opposite
 import Mathlib.Data.Set.Pairwise.Basic
 import Mathlib.Data.Set.Pairwise.Lattice
