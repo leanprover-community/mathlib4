f143e19aff58badf88119a8b2f7b7f33b386fc97
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2023-08-05 12:21:28 +0200

    Eric's comments, cleanup

--- Mathlib/Analysis/NormedSpace/HahnBanach/SeparatingDual.lean
+++ Mathlib/Analysis/NormedSpace/HahnBanach/SeparatingDual.lean
@@ -23,7 +23,7 @@ equivalences acts transitively on the set of nonzero vectors.
 /-- When `E` is a topological module over a topological ring `R`, the class `SeparatingDual R E`
 registers that continuous linear forms on `E` separate points of `E`. -/
 class SeparatingDual (R : Type _) (V : Type _) [Ring R] [AddCommGroup V] [TopologicalSpace V]
-  [TopologicalSpace R] [Module R V] : Prop :=
+    [TopologicalSpace R] [Module R V] : Prop :=
   /-- Any nonzero vector can be mapped by a continuous linear map to a nonzero scalar. -/
   exists_ne_zero' : ∀ (x : V), x ≠ 0 → ∃ f : V →L[R] R, f x ≠ 0
 
@@ -42,12 +42,12 @@ instance {E 𝕜 : Type _} [IsROrC 𝕜] [NormedAddCommGroup E] [NormedSpace �
 
 namespace SeparatingDual
 
-section CommRing
+section Ring
 
-variable {R V : Type _} [CommRing R] [AddCommGroup V] [TopologicalSpace V]
+variable {R V : Type _} [Ring R] [AddCommGroup V] [TopologicalSpace V]
   [TopologicalSpace R] [Module R V] [SeparatingDual R V]
 
-lemma exists_ne_zero  {x : V} (hx : x ≠ 0) :
+lemma exists_ne_zero {x : V} (hx : x ≠ 0) :
     ∃ f : V →L[R] R, f x ≠ 0 :=
   exists_ne_zero' x hx
 
@@ -59,36 +59,34 @@ theorem exists_separating_of_ne {x y : V} (h : x ≠ y) :
 protected theorem T1Space [T1Space R] : T1Space V := by
   apply t1Space_iff_exists_open.2 (fun x y hxy ↦ ?_)
   rcases exists_separating_of_ne (R := R) hxy with ⟨f, hf⟩
-  exact ⟨f ⁻¹' ({f y}ᶜ), isOpen_compl_singleton.preimage f.continuous, hf, by simp⟩
+  exact ⟨f ⁻¹' {f y}ᶜ, isOpen_compl_singleton.preimage f.continuous, hf, by simp⟩
 
-end CommRing
+end Ring
 
 section Field
 
-variable {R V : Type _} [NormedField R] [TopologicalSpace V] [AddCommGroup V]
-  [TopologicalAddGroup V] [Module R V] [SeparatingDual R V]
+variable [Field R] [AddCommGroup V] [TopologicalSpace R] [TopologicalSpace V]
+  [TopologicalRing R] [TopologicalAddGroup V] [Module R V] [SeparatingDual R V]
 
 lemma exists_eq_one {x : V} (hx : x ≠ 0) :
     ∃ f : V →L[R] R, f x = 1 := by
   rcases exists_ne_zero (R := R) hx with ⟨f, hf⟩
-  refine ⟨(f x)⁻¹ • f, ?_⟩
-  simp only [ContinuousLinearMap.coe_smul', Pi.smul_apply, smul_eq_mul, ne_eq, inv_mul_cancel hf]
+  exact ⟨(f x)⁻¹ • f, inv_mul_cancel hf⟩
 
-theorem exists_eq_one_ne_zero_of_ne_zero_pair
-    {x y : V} (hx : x ≠ 0) (hy : y ≠ 0) :
+theorem exists_eq_one_ne_zero_of_ne_zero_pair {x y : V} (hx : x ≠ 0) (hy : y ≠ 0) :
     ∃ f : V →L[R] R, f x = 1 ∧ f y ≠ 0 := by
   obtain ⟨u, ux⟩ : ∃ u : V →L[R] R, u x = 1 := exists_eq_one hx
   rcases ne_or_eq (u y) 0 with uy|uy
   · exact ⟨u, ux, uy⟩
   obtain ⟨v, vy⟩ : ∃ v : V →L[R] R, v y = 1 := exists_eq_one hy
   rcases ne_or_eq (v x) 0 with vx|vx
-  · exact ⟨(v x)⁻¹ • v, by simp [inv_mul_cancel vx], by simp [vx, vy]⟩
+  · exact ⟨(v x)⁻¹ • v, inv_mul_cancel vx, show (v x)⁻¹ * v y ≠ 0 by simp [vx, vy]⟩
   · exact ⟨u + v, by simp [ux, vx], by simp [uy, vy]⟩
 
 /-- In a topological vector space with separating dual, the group of continuous linear equivalences
 acts transitively on the set of nonzero vectors: given two nonzero vectors `x` and `y`, there
 exists `A : V ≃L[R] V` mapping `x` to `y`. -/
-theorem exists_continuousLinearMap_apply_eq [ContinuousSMul R V]
+theorem exists_continuousLinearEquiv_apply_eq [ContinuousSMul R V]
     {x y : V} (hx : x ≠ 0) (hy : y ≠ 0) :
     ∃ A : V ≃L[R] V, A x = y := by
   obtain ⟨G, Gx, Gy⟩ : ∃ G : V →L[R] R, G x = 1 ∧ G y ≠ 0 :=
@@ -99,22 +97,21 @@ theorem exists_continuousLinearMap_apply_eq [ContinuousSMul R V]
     map_add' := fun a b ↦ by simp [add_smul]; abel
     map_smul' := by simp [smul_smul]
     left_inv := fun z ↦ by
-      simp only [id_eq, eq_mpr_eq_cast, smul_eq_mul, AddHom.toFun_eq_coe, AddHom.coe_mk, map_add,
-        SMulHomClass.map_smul, map_sub, Gx, mul_sub, mul_one, add_sub_cancel'_right]
+      simp only [id_eq, eq_mpr_eq_cast, RingHom.id_apply, smul_eq_mul, AddHom.toFun_eq_coe,
+        AddHom.coe_mk, map_add, map_smulₛₗ, map_sub, Gx, mul_sub, mul_one, add_sub_cancel'_right]
       rw [mul_comm (G z), ← mul_assoc, inv_mul_cancel Gy]
       simp only [smul_sub, one_mul]
       abel
     right_inv := fun z ↦ by
-      simp only [map_add, SMulHomClass.map_smul, map_sub, Gx, smul_eq_mul, mul_sub, mul_one]
+      simp only [map_add, map_smulₛₗ, map_mul, map_inv₀, RingHom.id_apply, map_sub, Gx,
+        smul_eq_mul, mul_sub, mul_one]
       rw [mul_comm _ (G y), ← mul_assoc, mul_inv_cancel Gy]
       simp only [smul_sub, one_mul, add_sub_cancel'_right]
       abel
     continuous_toFun := continuous_id.add (G.continuous.smul continuous_const)
     continuous_invFun :=
       continuous_id.add ((continuous_const.mul G.continuous).smul continuous_const) }
-  refine ⟨A, ?_⟩
-  show x + G x • (y - x) = y
-  simp [Gx]
+  exact ⟨A, show x + G x • (y - x) = y by simp [Gx]⟩
 
 end Field
 
