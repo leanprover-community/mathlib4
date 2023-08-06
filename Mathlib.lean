5b1be14f0ecbd9851ff763c8202714c9830c4c2c
Eric Rodriguez <ericrboidi@gmail.com>
2023-08-06 17:52:38 +0100

    add to mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -1947,6 +1947,7 @@ import Mathlib.Geometry.Manifold.VectorBundle.Tangent
 import Mathlib.Geometry.Manifold.WhitneyEmbedding
 import Mathlib.GroupTheory.Abelianization
 import Mathlib.GroupTheory.Archimedean
+import Mathlib.GroupTheory.ClassEquation
 import Mathlib.GroupTheory.Commensurable
 import Mathlib.GroupTheory.Commutator
 import Mathlib.GroupTheory.CommutingProbability
