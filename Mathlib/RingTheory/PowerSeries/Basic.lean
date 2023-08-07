74ee7feb4e44b1f8b131aaf8b36360f4e1c120a2
Eric Wieser <wieser.eric@gmail.com>
2023-08-07 11:09:37 +0000

    use refine instead

--- Mathlib/RingTheory/PowerSeries/Basic.lean
+++ Mathlib/RingTheory/PowerSeries/Basic.lean
@@ -1473,8 +1473,9 @@ theorem coeff_zero_one : coeff R 0 (1 : PowerSeries R) = 1 :=
 
 theorem coeff_mul (n : ℕ) (φ ψ : PowerSeries R) :
     coeff R n (φ * ψ) = ∑ p in Finset.Nat.antidiagonal n, coeff R p.1 φ * coeff R p.2 ψ := by
+  -- `rw` can't see that `PowerSeries = MvPowerSeries Unit`, so use `.trans`
+  refine (MvPowerSeries.coeff_mul _ φ ψ).trans ?_
   symm
-  erw [MvPowerSeries.coeff_mul] -- `rw` can't see that `PowerSeries = MvPowerSeries Unit`
   apply Finset.sum_bij fun (p : ℕ × ℕ) _h => (single () p.1, single () p.2)
   · rintro ⟨i, j⟩ hij
     rw [Finset.Nat.mem_antidiagonal] at hij
