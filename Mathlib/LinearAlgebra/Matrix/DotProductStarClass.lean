8fd82661886494919c6e4191922704ce04532455
Mohanad Ahmed <m.a.m.elhassan@gmail.com>
2023-08-07 14:16:34 +0300

    Infer instances rather than proving them

--- Mathlib/LinearAlgebra/Matrix/DotProductStarClass.lean
+++ Mathlib/LinearAlgebra/Matrix/DotProductStarClass.lean
@@ -11,6 +11,7 @@ import Mathlib.Data.Rat.Basic
 import Mathlib.Data.Rat.Star
 import Mathlib.Data.IsROrC.Basic
 import Mathlib.LinearAlgebra.Matrix.ToLin
+import Mathlib.Analysis.Complex.Basic
 
 /-! # Types with Star Inner Product such that (star v) ⬝ᵥ v = 0 ↔ v = 0
 
@@ -42,32 +43,6 @@ instance IsROrC.toStarDotProduct [IsROrC K] : DotProductInnerProductSpace n K wh
     rw [Finset.sum_eq_zero_iff_of_nonneg] <;>
     simp only [Finset.mem_univ, forall_true_left, map_eq_zero, implies_true, IsROrC.normSq_nonneg]
 
-instance Real.toStarDotProduct : DotProductInnerProductSpace n ℝ where
-  dotProduct_star_self_eq_zero_iff := by
-    intro v
-    rw [star_trivial, dotProduct, Finset.sum_eq_zero_iff_of_nonneg, Function.funext_iff] <;>
-    simp only [Finset.mem_univ, mul_eq_zero, or_self, forall_true_left, Pi.zero_apply,
-      mul_self_nonneg, implies_true]
-
-instance Complex.toStarDotProduct : DotProductInnerProductSpace n ℂ where
-  dotProduct_star_self_eq_zero_iff := by
-    intro v
-    simp_rw [dotProduct, Pi.star_apply, Complex.star_def, ← Complex.normSq_eq_conj_mul_self,
-      Complex.ext_iff, Complex.im_sum, Complex.zero_im, Complex.ofReal_im, Finset.sum_const_zero,
-      and_true, Complex.zero_re, Complex.re_sum, Complex.ofReal_re, Function.funext_iff,
-      Pi.zero_apply]
-    rw [Finset.sum_eq_zero_iff_of_nonneg] <;>
-    simp only [Finset.mem_univ, map_eq_zero, forall_true_left, implies_true,
-      (fun _ => Complex.normSq_nonneg _)]
-
-
-instance Rat.toStarDotProduct : DotProductInnerProductSpace n ℚ where
-  dotProduct_star_self_eq_zero_iff := by
-    intro v
-    rw [star_trivial, dotProduct, Finset.sum_eq_zero_iff_of_nonneg, Function.funext_iff] <;>
-    simp only [Finset.mem_univ, mul_eq_zero, or_self, forall_true_left, Pi.zero_apply,
-      mul_self_nonneg, implies_true]
-
 instance StarOrderedRing.toStarDotProduct [Field K] [PartialOrder K] [StarOrderedRing K] :
     DotProductInnerProductSpace n K where
   dotProduct_star_self_eq_zero_iff := by
@@ -75,3 +50,9 @@ instance StarOrderedRing.toStarDotProduct [Field K] [PartialOrder K] [StarOrdere
     rw [dotProduct, Finset.sum_eq_zero_iff_of_nonneg, Function.funext_iff] <;>
     simp only [Finset.mem_univ, Pi.star_apply, forall_true_left, star_mul_self_nonneg, implies_true,
       mul_eq_zero, Pi.zero_apply, star_eq_zero, or_self]
+
+instance Real.toStarDotProduct : DotProductInnerProductSpace n ℝ := by infer_instance
+
+instance Complex.toStarDotProduct : DotProductInnerProductSpace n ℂ := by infer_instance
+
+instance Rat.toStarDotProduct : DotProductInnerProductSpace n ℚ := by infer_instance
