ee40179a8015c075935b04dc5a2ae19e0897e9a7
Scott Morrison <scott.morrison@gmail.com>
2023-08-08 11:37:46 +1000

    reduce import

--- Mathlib/Tactic/TryThis.lean
+++ Mathlib/Tactic/TryThis.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison
 -/
 import Std.Tactic.TryThis
-import Lean.Meta.Tactic.Util
 import Mathlib.Util.Syntax
 
 /-!
