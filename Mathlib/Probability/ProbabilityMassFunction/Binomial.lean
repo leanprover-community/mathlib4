1ecccfebb4699701fc638cc7f77a67493501b714
Joachim Breitner <mail@joachim-breitner.de>
2023-08-08 13:46:44 +0200

    s/binominal/binomial/

--- Mathlib/Probability/ProbabilityMassFunction/Binomial.lean
+++ Mathlib/Probability/ProbabilityMassFunction/Binomial.lean
@@ -8,9 +8,9 @@ import Mathlib.Data.Nat.Choose.Multinomial
 import Mathlib.Tactic.FinCases
 
 /-!
-# The binominal distribution
+# The binomial distribution
 
-This file defines the probability mass function of the binominal distribution, and proves
+This file defines the probability mass function of the binomial distribution, and proves
 it to be equal to `Pmf.bernoulli` for `n = 1`.
 -/
 
@@ -20,7 +20,7 @@ namespace Pmf
 
 /-- The binomial `Pmf`: The probability of that `i` out of `n` coins come up heads if the
 probability of heads is `p`. -/
-def binominal (p : ENNReal) (h : p ≤ 1) (n : ℕ) : Pmf (Fin (n + 1)) :=
+def binomial (p : ENNReal) (h : p ≤ 1) (n : ℕ) : Pmf (Fin (n + 1)) :=
   .ofFintype (fun i => p^(i : ℕ) * (1-p)^(n - (i : ℕ)) * (n.choose i : ℕ)) (by
     convert (add_pow p (1-p) n).symm
     · rw [Finset.sum_fin_eq_sum_range]
@@ -30,18 +30,18 @@ def binominal (p : ENNReal) (h : p ≤ 1) (n : ℕ) : Pmf (Fin (n + 1)) :=
       rw [dif_pos hi]
     · simp [h])
 
-theorem binominal_apply : binominal p h n i =
+theorem binomial_apply : binomial p h n i =
   p^(i : ℕ) * (1-p)^(n - (i : ℕ)) * (n.choose i : ℕ) := rfl
 
 @[simp]
-theorem binominal_apply_0 : binominal p h n 0 = (1-p)^n :=
-  by simp [binominal_apply]
+theorem binomial_apply_0 : binomial p h n 0 = (1-p)^n :=
+  by simp [binomial_apply]
 
 @[simp]
-theorem binominal_apply_n : binominal p h n n = p^n :=
-  by simp [binominal_apply, Nat.mod_eq_of_lt]
+theorem binomial_apply_n : binomial p h n n = p^n :=
+  by simp [binomial_apply, Nat.mod_eq_of_lt]
 
-/-- The binominal distribution on one coin is the bernoully distribution. -/
-theorem binominal_one_eq_bernoulli :
-  binominal p h 1 = (bernoulli p h).map (cond · 1 0) := by
-    ext i; fin_cases i <;> simp [tsum_bool, binominal_apply]
+/-- The binomial distribution on one coin is the bernoully distribution. -/
+theorem binomial_one_eq_bernoulli :
+  binomial p h 1 = (bernoulli p h).map (cond · 1 0) := by
+    ext i; fin_cases i <;> simp [tsum_bool, binomial_apply]
