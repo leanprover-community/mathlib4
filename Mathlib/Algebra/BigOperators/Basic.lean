58da14d261b92f5b3bb179ef2cdd9f4a9b589e50
Eric Rodriguez <37984851+ericrbg@users.noreply.github.com>
2023-08-08 12:57:14 -0400

    namespacing

--- Mathlib/Algebra/BigOperators/Basic.lean
+++ Mathlib/Algebra/BigOperators/Basic.lean
@@ -926,7 +926,7 @@ theorem prod_subtype {p : α → Prop} {F : Fintype (Subtype p)} (s : Finset α)
 #align finset.sum_subtype Finset.sum_subtype
 
 @[to_additive]
-theorem Finset.prod_set_coe (s : Set α) [Fintype s] : (∏ i : s, f i) = ∏ i in s.toFinset, f i :=
+theorem prod_set_coe (s : Set α) [Fintype s] : (∏ i : s, f i) = ∏ i in s.toFinset, f i :=
 (Finset.prod_subtype s.toFinset (fun _ ↦ Set.mem_toFinset) f).symm
 
 /-- The product of a function `g` defined only on a set `s` is equal to
