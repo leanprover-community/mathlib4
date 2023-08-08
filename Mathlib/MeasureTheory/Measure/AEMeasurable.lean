79cd8f1c9a704c7df047a6cf42a787dceb6e6bc4
ADedecker <anatolededecker@gmail.com>
2023-08-08 17:44:53 +0300

    Import not needed

--- Mathlib/MeasureTheory/Measure/AEMeasurable.lean
+++ Mathlib/MeasureTheory/Measure/AEMeasurable.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Sébastien Gouëzel
 -/
 import Mathlib.MeasureTheory.Measure.MeasureSpace
-import Mathlib.Order.Filter.IndicatorFunction
 
 #align_import measure_theory.measure.ae_measurable from "leanprover-community/mathlib"@"3310acfa9787aa171db6d4cba3945f6f275fe9f2"
 
