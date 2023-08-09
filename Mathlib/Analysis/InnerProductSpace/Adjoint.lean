976c85061f392750aa963d995122d884852a5737
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2023-08-09 11:47:22 +0200

    Update Mathlib/Analysis/InnerProductSpace/Adjoint.lean
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- Mathlib/Analysis/InnerProductSpace/Adjoint.lean
+++ Mathlib/Analysis/InnerProductSpace/Adjoint.lean
@@ -40,7 +40,7 @@ adjoint
 
 -/
 
-local macro_rules | `($x ^ $y)   => `(HPow.hPow $x $y) -- Porting note: See issue #2220
+local macro_rules | `($x ^ $y)   => `(HPow.hPow $x $y) -- Porting note: See issue lean4#2220
 
 noncomputable section
 
