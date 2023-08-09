2cfbc188bee3cd7a6914a06666a074060115693d
Joachim Breitner <mail@joachim-breitner.de>
2023-08-09 15:41:53 +0200

    feat: Summable.countable_support

--- Mathlib/Topology/Instances/ENNReal.lean
+++ Mathlib/Topology/Instances/ENNReal.lean
@@ -1117,6 +1117,28 @@ theorem summable_of_le {f g : β → ℝ≥0} (hgf : ∀ b, g b ≤ f b) : Summa
     hp.summable
 #align nnreal.summable_of_le NNReal.summable_of_le
 
+/-- Summable non-negative functions have countable support -/
+theorem _root_.Summable.countable_support (f : α → ℝ≥0) (h : Summable f) :
+  f.support.Countable := by
+  let eps := fun (n : ℕ) => (1/(n+1) : ℝ≥0)
+  let s := fun (n : ℕ) => { x : α | eps n ≤ f x }
+  have hf : f.support ⊆ ⋃ (n : ℕ), s n := by
+    intro a ha
+    change f a ≠ 0 at ha
+    have ⟨N, hN⟩ := exists_nat_one_div_lt (by positivity : 0 < (f a : ℝ))
+    exact Set.mem_iUnion_of_mem N hN.le
+  apply Set.Countable.mono hf
+  rw [Set.countable_iUnion_iff]
+  intro n
+  apply Set.Finite.countable
+  rw [← Set.finite_coe_iff]
+  apply Finite.of_summable_const (ι := s n) (b := (eps n : ℝ))
+  . simp; positivity
+  . have hs : Summable ((s n).indicator (fun _ => (eps n : ℝ≥0))) :=
+      NNReal.summable_of_le (fun a ↦ Set.indicator_le (fun b hb ↦ hb) a) h
+    rw [← summable_subtype_iff_indicator] at hs
+    rwa [NNReal.summable_coe]
+
 /-- A series of non-negative real numbers converges to `r` in the sense of `HasSum` if and only if
 the sequence of partial sum converges to `r`. -/
 theorem hasSum_iff_tendsto_nat {f : ℕ → ℝ≥0} {r : ℝ≥0} :
