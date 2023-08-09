44e37d0969b8ed1b24b9b644f7b566edf8e1d740
Scott Morrison <scott.morrison@gmail.com>
2023-08-09 11:57:56 +1000

    linting

--- Mathlib/Data/Option/Basic.lean
+++ Mathlib/Data/Option/Basic.lean
@@ -43,7 +43,10 @@ theorem coe_def : (fun a ↦ ↑a : α → Option α) = some :=
 theorem mem_map {f : α → β} {y : β} {o : Option α} : y ∈ o.map f ↔ ∃ x ∈ o, f x = y := by simp
 #align option.mem_map Option.mem_map
 
-@[simp 1100] -- This needs to be higher priority than `Option.mem_def`.
+-- The simpNF linter says that the LHS can be simplified via `Option.mem_def`.
+-- However this is a higher priority lemma.
+-- https://github.com/leanprover/std4/issues/207
+@[simp 1100, nolint simpNF]
 theorem mem_map_of_injective {f : α → β} (H : Function.Injective f) {a : α} {o : Option α} :
     f a ∈ o.map f ↔ a ∈ o := by
   aesop
