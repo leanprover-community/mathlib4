38b1f8e8c028612faa77c9e79b0941a36fab66fb
Oliver Nash <github@olivernash.org>
2023-08-10 19:10:34 +0100

    Golf `MonoidAlgebra.instStarRing`

--- Mathlib/Algebra/MonoidAlgebra/Star.lean
+++ Mathlib/Algebra/MonoidAlgebra/Star.lean
@@ -33,19 +33,15 @@ namespace MonoidAlgebra
 variable {k G : Type _} [Semiring k] [StarRing k] [DecidableEq G] [Group G]
 
 instance instStarRing : StarRing (MonoidAlgebra k G) where
-  star := fun f ↦ ⟨f.support.image Inv.inv, star ∘ f ∘ Inv.inv, fun x ↦ by
-    simp only [Finset.mem_image, Finsupp.mem_support_iff, Function.comp_apply]
-    refine' ⟨_, fun h ↦ ⟨x⁻¹, star_ne_zero.mp h, inv_inv x⟩⟩
-    rintro ⟨y, h, rfl⟩
-    rw [inv_inv]
-    exact star_ne_zero.mpr h⟩
+  star f := ⟨f.support.map (Equiv.inv G), star ∘ f ∘ Inv.inv, fun x ↦ by
+    simp only [Finset.mem_map_equiv, Finsupp.mem_support_iff, Function.comp_apply, star_ne_zero,
+      Equiv.inv_symm, Equiv.inv_apply]⟩
   star_involutive := fun f ↦ Finsupp.ext fun x ↦ by simp
   star_mul := fun f g ↦ Finsupp.ext fun x ↦ by
     simp only [Finsupp.coe_mk, Function.comp_apply]
     rw [MonoidAlgebra.mul_apply, MonoidAlgebra.mul_apply]
-    simp only [Finsupp.sum, Finsupp.coe_mk, Function.comp_apply, Finsupp.mem_support_iff, ne_eq,
-      inv_inj, imp_self, implies_true, forall_const, Finset.sum_image, inv_inv, star_sum, apply_ite,
-      star_zero, star_mul]
+    simp only [Finsupp.sum, star_sum, apply_ite, star_mul, star_zero, Finsupp.coe_mk,
+      Function.comp_apply, Finset.sum_map, Equiv.coe_toEmbedding, Equiv.inv_apply, inv_inv]
     rw [Finset.sum_comm]
     simp_rw [← mul_inv_rev, inv_eq_iff_eq_inv]
   star_add := fun f g ↦ Finsupp.ext fun x ↦ by
@@ -67,19 +63,15 @@ namespace AddMonoidAlgebra
 variable {k G : Type _} [Semiring k] [StarRing k] [DecidableEq G] [AddGroup G]
 
 instance instStarRing : StarRing (AddMonoidAlgebra k G) where
-  star := fun f ↦ ⟨f.support.image Neg.neg, star ∘ f ∘ Neg.neg, fun x ↦ by
-    simp only [Finset.mem_image, Finsupp.mem_support_iff, Function.comp_apply]
-    refine' ⟨_, fun h ↦ ⟨-x, star_ne_zero.mp h, neg_neg x⟩⟩
-    rintro ⟨y, h, rfl⟩
-    rw [neg_neg]
-    exact star_ne_zero.mpr h⟩
+  star f := ⟨f.support.map (Equiv.neg G), star ∘ f ∘ Neg.neg, fun x ↦ by
+    simp only [Finset.mem_map_equiv, Finsupp.mem_support_iff, Function.comp_apply, star_ne_zero,
+      Equiv.neg_symm, Equiv.neg_apply]⟩
   star_involutive := fun f ↦ Finsupp.ext fun x ↦ by simp
   star_mul := fun f g ↦ Finsupp.ext fun x ↦ by
     simp only [Finsupp.coe_mk, Function.comp_apply]
     rw [AddMonoidAlgebra.mul_apply, AddMonoidAlgebra.mul_apply]
-    simp only [Finsupp.sum, Finsupp.coe_mk, Function.comp_apply, Finsupp.mem_support_iff, ne_eq,
-      neg_inj, imp_self, implies_true, forall_const, Finset.sum_image, neg_neg, star_sum, apply_ite,
-      star_zero, star_mul]
+    simp only [Finsupp.sum, star_sum, apply_ite, star_mul, star_zero, Finsupp.coe_mk,
+      Function.comp_apply, Finset.sum_map, Equiv.coe_toEmbedding, Equiv.neg_apply, neg_neg]
     rw [Finset.sum_comm]
     simp_rw [← neg_add_rev, neg_eq_iff_eq_neg]
   star_add := fun f g ↦ Finsupp.ext fun x ↦ by
