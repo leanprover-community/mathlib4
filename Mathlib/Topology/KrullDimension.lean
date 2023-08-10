a0077ab77bd98a3cdc945132846b474b11e29afc
FMLJohn <417963616@qq.com>
2023-08-10 16:28:34 +0100

    added copyright information to Topology.KrullDim

--- Mathlib/Topology/KrullDimension.lean
+++ Mathlib/Topology/KrullDimension.lean
@@ -1,9 +1,23 @@
+/-
+Copyright (c) 2023 Jujian Zhang. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Jujian Zhang, Fangming Li
+-/
+
 import Mathlib.Order.KrullDimension
 import Mathlib.Topology.SubsetProperties
 
-/--
-Krull dimension of a topological space is the supremum of length of chains of closed irreducible
-sets.
+/-!
+# Krull dimension of a topological space
+
+The Krull dimension of a topological space is the order theoretic Krull dimension applied to the
+collection of all its subsets that are closed and irreducible. Unfolding this definition, it is
+the length of longest series of closed irreducible subsets ordered by inclusion.
+-/
+
+/-
+The Krull dimension of a topological space is the supremum of length of chains of
+closed irreducible sets.
 -/
 noncomputable def topologicalKrullDim (T : Type _) [TopologicalSpace T] :
   WithBot (WithTop ℕ) :=
