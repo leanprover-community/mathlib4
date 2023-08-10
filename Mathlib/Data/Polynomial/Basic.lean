8c698a524a8dbde0edac827a4ea68a59f423d5b6
damiano <adomani@gmail.com>
2023-08-10 12:07:45 +0000

    feat: simple lemmas about polynomials and their degrees (#6220)
    This PR extracts some lemmas about polynomials that are helpful for the tactic `compute_degree` (#6221).
    
    The signature of a theorem changed:
    ```lean
    theorem coeff_pow_of_natDegree_le (pn : p.natDegree ≤ n) :
        (p ^ m).coeff (n * m) = p.coeff n ^ m  -- <-- the order of the product was `n * m`
        (p ^ m).coeff (m * n) = p.coeff n ^ m  -- <-- and it became `m * n`
    ```
    Modified files:
    ```
    Data/Polynomial/Basic.lean
    Data/Polynomial/Degree/Lemmas.lean
    Data/Polynomial/Degree/Definitions.lean
    Data/Polynomial/Coeff.lean  -- for a "`simp` can prove this" golf
    ```

--- Mathlib/Data/Polynomial/Basic.lean
+++ Mathlib/Data/Polynomial/Basic.lean
@@ -729,6 +729,10 @@ theorem coeff_C_zero : coeff (C a) 0 = a :=
 theorem coeff_C_ne_zero (h : n ≠ 0) : (C a).coeff n = 0 := by rw [coeff_C, if_neg h]
 #align polynomial.coeff_C_ne_zero Polynomial.coeff_C_ne_zero
 
+@[simp]
+theorem coeff_nat_cast_ite : (Nat.cast m : R[X]).coeff n = ite (n = 0) m 0 := by
+  simp only [← C_eq_nat_cast, coeff_C, Nat.cast_ite, Nat.cast_zero]
+
 theorem C_mul_X_pow_eq_monomial : ∀ {n : ℕ}, C a * X ^ n = monomial n a
   | 0 => mul_one _
   | n + 1 => by
