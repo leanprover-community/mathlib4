feb5087e7d8fb956cf9008c3db60c25645455f0d
Thomas Browning <tb65536@uw.edu>
2023-08-10 10:46:12 -0700

    fix

--- Mathlib/GroupTheory/CommutingProbability.lean
+++ Mathlib/GroupTheory/CommutingProbability.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Thomas Browning
 -/
 import Mathlib.Algebra.Group.ConjFinite
-import Mathlib.Data.Set.Ncard
+import Mathlib.Data.Set.Card
 import Mathlib.GroupTheory.Abelianization
 import Mathlib.GroupTheory.GroupAction.ConjAct
 import Mathlib.GroupTheory.GroupAction.Quotient
