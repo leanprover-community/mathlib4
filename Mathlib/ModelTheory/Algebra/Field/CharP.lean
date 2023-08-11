149b259475db3375386bec7d2519813a01ea2350
Chris Hughes <chrishughes24@gmail.com>
2023-08-11 13:52:49 +0200

    slight improvement

--- Mathlib/ModelTheory/Algebra/Field/CharP.lean
+++ Mathlib/ModelTheory/Algebra/Field/CharP.lean
@@ -21,6 +21,10 @@ end field
 
 open field
 
+@[simp] theorem realize_eqZero {K : Type _} [CompatibleField K] (n : ℕ)
+    (v : Empty → K) : (Formula.Realize (eqZero n) v) ↔ ((n : K) = 0) := by
+    simp [eqZero, Term.realize]
+
 def Theory.hasChar (p : ℕ) : Language.field.Theory :=
   if p = 0
   then (fun q => ∼(eqZero q)) '' {q : ℕ | q.Prime}
@@ -32,13 +36,12 @@ theorem model_hasChar_of_charP {K : Type _} [CompatibleField K] {p : ℕ} [CharP
   rw [Theory.hasChar]
   cases CharP.char_is_prime_or_zero K p with
   | inl hp =>
-    simp [hp.ne_zero, hp, Sentence.Realize, eqZero, Term.realize]
+    simp [hp.ne_zero, hp, Sentence.Realize]
   | inr hp =>
     subst hp
-    simp only [eqZero, Nat.cast_zero, ite_false, ite_true, Theory.model_iff, Set.mem_image, Set.mem_setOf_eq,
-      Sentence.Realize, forall_exists_index, and_imp, forall_apply_eq_imp_iff₂, Formula.realize_not,
-      Formula.realize_equal, realize_termOfFreeCommRing, map_natCast, Term.realize, CompatibleField.funMap_zero,
-      ← CharZero.charZero_iff_forall_prime_ne_zero]
+    simp only [ite_false, ite_true, Theory.model_iff, Set.mem_image, Set.mem_setOf_eq,
+      Sentence.Realize, forall_exists_index, and_imp, forall_apply_eq_imp_iff₂,
+      Formula.realize_not, realize_eqZero, ← CharZero.charZero_iff_forall_prime_ne_zero]
     exact CharP.charP_to_charZero K
 
 
@@ -47,14 +50,12 @@ theorem charP_of_model_hasChar {K : Type _} [CompatibleField K]
   rw [Theory.hasChar] at h
   split_ifs at h with hp0 hp
   · subst hp0
-    simp only [eqZero, Theory.model_iff, Set.mem_image, Set.mem_setOf_eq, Sentence.Realize,
+    simp only [Theory.model_iff, Set.mem_image, Set.mem_setOf_eq, Sentence.Realize,
       forall_exists_index, and_imp, forall_apply_eq_imp_iff₂, Formula.realize_not,
-      Formula.realize_equal, realize_termOfFreeCommRing, map_natCast, Term.realize,
-      CompatibleField.funMap_zero, ← CharZero.charZero_iff_forall_prime_ne_zero K] at h
+      realize_eqZero, ← CharZero.charZero_iff_forall_prime_ne_zero] at h
     refine CharP.ofCharZero K
-  · simp only [eqZero, Theory.model_iff, Set.mem_singleton_iff, Sentence.Realize,
-      forall_eq, Formula.realize_equal, realize_termOfFreeCommRing, map_natCast,
-      Term.realize, CompatibleField.funMap_zero, ← CharP.charP_iff_prime_eq_zero hp] at h
+  · simp only [Theory.model_iff, Set.mem_singleton_iff, Sentence.Realize, forall_eq,
+      realize_eqZero, ← CharP.charP_iff_prime_eq_zero hp] at h
     exact h
   · simp [Sentence.Realize] at h
 
