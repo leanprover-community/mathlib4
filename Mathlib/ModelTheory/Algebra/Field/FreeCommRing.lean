e8f8b81c46d2482556e7cd71f4f4ee213573913c
Chris Hughes <chrishughes24@gmail.com>
2023-08-11 04:37:31 +0200

    more tidying

--- Mathlib/ModelTheory/Algebra/Field/FreeCommRing.lean
+++ Mathlib/ModelTheory/Algebra/Field/FreeCommRing.lean
@@ -13,22 +13,51 @@ namespace Language
 
 namespace field
 
+@[simp]
+def freeCommRingRingStructure (α : Type _) :
+    Language.ring.Structure (FreeCommRing α) :=
+  { funMap := fun {n} f =>
+      match n, f with
+      | _, .add => fun x => x 0 + x 1
+      | _, .mul => fun x => x 0 * x 1
+      | _, .neg => fun x => - x 0
+      | _, .zero => fun _ => 0
+      | _, .one => fun _ => 1,
+    RelMap := fun f => by cases f }
+
+section
+
+attribute [local instance] freeCommRingRingStructure
+
+theorem exists_term_realize_eq_freeCommRing (p : FreeCommRing α) :
+    ∃ t : Language.ring.Term α,
+      (t.realize FreeCommRing.of : FreeCommRing α) = p :=
+  FreeCommRing.induction_on p
+    ⟨-1, by simp [Term.realize]⟩
+    (fun a => ⟨Term.var a, by simp [Term.realize]⟩)
+    (fun x y ⟨t₁, ht₁⟩ ⟨t₂, ht₂⟩ =>
+      ⟨t₁ + t₂, by simp_all [Term.realize]⟩)
+    (fun x y ⟨t₁, ht₁⟩ ⟨t₂, ht₂⟩ =>
+      ⟨t₁ * t₂, by simp_all [Term.realize]⟩)
+
+end
+
 noncomputable def termOfFreeCommRing (p : FreeCommRing α) : Language.field.Term α :=
-  field.ofRing.onTerm (ring.termOfFreeCommRing p)
+  field.ofRing.onTerm <| Classical.choose (exists_term_realize_eq_freeCommRing p)
 
 variable {K : Type _} [CompatibleField K]
 
 @[simp]
 theorem realize_termOfFreeCommRing (p : FreeCommRing α) (v : α → K) :
     (termOfFreeCommRing p).realize v = FreeCommRing.lift v p := by
-  letI := Language.ring.freeCommRingRingStructure α
-  have : (ring.termOfFreeCommRing p).realize FreeCommRing.of = p :=
-    Classical.choose_spec (ring.exists_term_realize_eq_freeCommRing p)
-  conv_rhs => rw [← this]
-  simp only [termOfFreeCommRing]
-  induction ring.termOfFreeCommRing p with
-  | var _ => simp [termOfFreeCommRing, Term.realize]
-  | func f _ ih => cases f <;> simp_all [Term.realize, field.ofRing]
+  letI := Language.field.freeCommRingRingStructure α
+  rw [termOfFreeCommRing, ofRing]
+  conv_rhs => rw [← Classical.choose_spec (field.exists_term_realize_eq_freeCommRing p)]
+  induction Classical.choose (field.exists_term_realize_eq_freeCommRing p) with
+  | var _ => simp [Term.realize]
+  | func f a ih =>
+    cases f <;>
+    simp [Term.realize, ih, ofRing, LHom.onTerm]
 
 end field
 
