61703e6466dcdbdd4ac5421ec72f16374dd5b8eb
Scott Morrison <scott@tqft.net>
2023-08-11 07:19:05 +0000

    chore: fix lakefile (#6510)
    My PR https://github.com/leanprover-community/mathlib4/pull/6423 modified the lakefile to point to a branch of the `Qq` library, but this had only been necessary during my local testing against a patched version of Lean. I should not have committed this change, and we should not have merged it to `master`.
    
    It's harmless, I think, but should be reverted asap.
    
    
    
    Co-authored-by: Scott Morrison <scott.morrison@gmail.com>

--- lakefile.lean
+++ lakefile.lean
@@ -42,7 +42,7 @@ meta if get_config? doc = some "on" then -- do not download and build doc-gen4 b
 require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
 
 require std from git "https://github.com/leanprover/std4" @ "main"
-require Qq from git "https://github.com/semorrison/quote4" @ "instance_names"
+require Qq from git "https://github.com/gebner/quote4" @ "master"
 require aesop from git "https://github.com/JLimperg/aesop" @ "master"
 require Cli from git "https://github.com/mhuisi/lean4-cli.git" @ "nightly"
 require proofwidgets from git "https://github.com/EdAyers/ProofWidgets4" @ "v0.0.13"
