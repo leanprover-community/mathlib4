0b61185ec6163575db9eb70b91f7e788e32520de
thorimur <68410468+thorimur@users.noreply.github.com>
2023-08-13 00:13:56 -0400

    lint: long lines

--- Mathlib/Lean/Meta/Compare.lean
+++ Mathlib/Lean/Meta/Compare.lean
@@ -34,7 +34,8 @@ every aspect of the objects and only the expressions appearing in the object, re
 
 ## Implementation notes
 
-We use the following pattern for specifying e.g. the `LocalContextComparisonConfig` in the `MetavarDeclComparisonConfig`:
+We use the following pattern for specifying e.g. the `LocalContextComparisonConfig` in the 
+`MetavarDeclComparisonConfig`:
 ```
 structure MetavarDeclComparisonConfig where
   ...
@@ -42,7 +43,13 @@ structure MetavarDeclComparisonConfig where
 ```
 If `compareLocalContexts?` is `none`, we don't compare local contexts at all.
 
-However, this pattern means that the config for comparing `Expr`s, which is needed both when comparing `LocalContext`s and `MetavarDecl`s, can't simply be extended by `LocalContextComparisonConfig`, as that's not always present in `MetavarDeclComparisonConfig`. It also can't simply be extended by *both* `LocalContextComparisonConfig` and `MetavarDeclComparisonConfig` (using this pattern), as then changing the setting in `MetavarDeclComparisonConfig` wouldn't propagate that setting down into `LocalContextComparisonConfig`.
+However, this pattern means that the config for comparing `Expr`s, which is needed both when 
+comparing `LocalContext`s and `MetavarDecl`s, can't simply be extended by 
+`LocalContextComparisonConfig`, as that's not always present in `MetavarDeclComparisonConfig`. It 
+also can't simply be extended by *both* `LocalContextComparisonConfig` and 
+`MetavarDeclComparisonConfig` (using this pattern), as then changing the setting in 
+`MetavarDeclComparisonConfig` wouldn't propagate that setting down into 
+`LocalContextComparisonConfig`.
 
 As such, we also pass `ExprComparisonConfig` as an argument to each `compare` function.
 
