ad23a18607f23888c70e8a455600b83ef880842f
Komyyy <pol_tta@outlook.jp>
2023-08-14 06:40:32 +0900

    align_import

--- Mathlib/Tactic/Hint.lean
+++ Mathlib/Tactic/Hint.lean
@@ -2,10 +2,7 @@
 Copyright (c) 2020 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison
-
-! This file was ported from Lean 3 source module tactic.hint
-! leanprover-community/mathlib commit 8f6fd1b69096c6a587f745d354306c0d46396915
-! Please do not edit these lines, except to modify the commit id
-! if you have ported upstream changes.
 -/
 import Mathlib.Tactic.Hint.Basic
+
+#align_import tactic.hint from "leanprover-community/mathlib"@"8f6fd1b69096c6a587f745d354306c0d46396915"
