0843eb01a49d808ed87ecb31cd8e0d49974697f6
Antoine Chambert-Loir <antoine.chambert-loir@math.univ-paris-diderot.fr>
2023-08-14 17:20:13 +0200

    add docstring and make linter 100 happy

--- Mathlib/LinearAlgebra/TensorProduct/RightExactness.lean
+++ Mathlib/LinearAlgebra/TensorProduct/RightExactness.lean
@@ -16,9 +16,11 @@ import Mathlib.Algebra.Algebra.Subalgebra.Basic
 
 * For linear maps `f : M →ₗ[R] N`  and `g : N →ₗ[R] P`, `Exact f g` says that `range f = ker g``
 
-* `TensorProduct.rTensor_exact` says that one can tensor a short exact sequence on the right, and `rTensor.equiv` gives a LinearEquiv.
+* `TensorProduct.rTensor_exact` says that one can tensor a short exact sequence on the right,
+  and `rTensor.equiv` gives a LinearEquiv.
 
-* `TensorProduct.lTensor_exact` says that one can tensor a short exact sequence on the left, and `lTensor.equiv` gives a LinearEquiv.
+* `TensorProduct.lTensor_exact` says that one can tensor a short exact sequence on the left,
+  and `lTensor.equiv` gives a LinearEquiv.
 
 * For `N : Submodule R M`, `LinearMap.exact_subtype_mkQ N` says that
   the inclusion of the submodule and the quotient map form an exact pair,
@@ -29,7 +31,8 @@ import Mathlib.Algebra.Algebra.Subalgebra.Basic
 
 The proofs are those of [bourbaki1989] (chap. 2, §3, n°6)
 
-* Analogue for morphisms of algebras :  `Algebra.TensorProduct.ker_map` computes the kernel of `Algebra.TensorProduct.map f g`
+* Analogue for morphisms of algebras :
+  `Algebra.TensorProduct.ker_map` computes the kernel of `Algebra.TensorProduct.map f g`
 
 * The two more specific lemmas `Algebra.TensorProduct.lTensor_ker`
   and `Algebra.TensorProduct.rTensor_ker` compute the kernels
@@ -51,6 +54,8 @@ section Exact
 
 variable {M N P : Type*} (f : M → N) (g : N → P)
 
+/-- The maps `f` and `g` form an exact pair :
+  `g y = 0` iff `y` belongs to the image of `f` -/
 def Exact [Zero P] : Prop := ∀ y, g y = 0 ↔ y ∈ Set.range f
 
 lemma Exact.comp_eq_zero [Zero P] (h : Exact f g) : g.comp f = 0 :=
@@ -159,12 +164,15 @@ variable (Q : Type*) [AddCommGroup Q] [Module R Q]
 
 variable (hfg : Exact f g) (hg : Function.Surjective g)
 
+/-- The direct map in `rTensor.equiv` -/
 def rTensor.toFun :
   N ⊗[R] Q ⧸ LinearMap.range (rTensor Q f) →ₗ[R] P ⊗[R] Q := by
   apply Submodule.liftQ _ (rTensor Q g)
   rw [LinearMap.range_le_iff_comap, ← LinearMap.ker_comp, ← rTensor_comp,
     hfg.linearMap_comp_eq_zero, rTensor_zero, ker_zero]
 
+/-- The inverse map in `rTensor.equiv_ofRightInverse`
+  (computably, given a right inverse) -/
 def rTensor.inverse_ofRightInverse {h : P → N} (hgh : Function.RightInverse h g) :
     P ⊗[R] Q →ₗ[R] N ⊗[R] Q ⧸ LinearMap.range (rTensor Q f) := by
   rw [exact_iff] at hfg
@@ -202,6 +210,7 @@ lemma rTensor.inverse_ofRightInverse_apply
   apply le_comap_range_rTensor f
   rw [← hfg, mem_ker, map_sub, sub_eq_zero, hgh]
 
+/-- The inverse map in `rTensor.equiv` -/
 noncomputable
 def rTensor.inverse :
     P ⊗[R] Q →ₗ[R] N ⊗[R] Q ⧸ LinearMap.range (rTensor Q f) :=
@@ -260,12 +269,14 @@ lemma rTensor_mkQ (N : Submodule R M) :
   rw [← LinearMap.exact_iff]
   exact rTensor_exact Q (LinearMap.exact_subtype_mkQ N) (Submodule.mkQ_surjective N)
 
+/-- The direct map in `lTensor.equiv` -/
 def lTensor.toFun :
   Q ⊗[R] N ⧸ LinearMap.range (lTensor Q f) →ₗ[R] Q ⊗[R] P := by
   apply Submodule.liftQ _ (lTensor Q g)
   rw [LinearMap.range_le_iff_comap, ← LinearMap.ker_comp, ← lTensor_comp,
     hfg.linearMap_comp_eq_zero, lTensor_zero, ker_zero]
 
+/-- The inverse map in `lTensor.equiv_ofRightInverse` (computably, given a right inverse)-/
 def lTensor.inverse_ofRightInverse {h : P → N} (hgh : Function.RightInverse h g) :
     Q ⊗[R] P →ₗ[R] Q ⊗[R] N ⧸ LinearMap.range (lTensor Q f) := by
   rw [exact_iff] at hfg
@@ -298,7 +309,8 @@ lemma lTensor.inverse_ofRightInverse_apply
   simp only [coe_comp, Function.comp_apply, lTensor_tmul, Submodule.mkQ_apply]
   simp only [lTensor.inverse_ofRightInverse]
   simp only [TensorProduct.lift.tmul, coe_mk, AddHom.coe_mk, mk₂_apply]
-  simp only [flip_apply, coe_mk, AddHom.coe_mk, coe_comp, Function.comp_apply, mk_apply, Submodule.mkQ_apply]
+  simp only [flip_apply, coe_mk, AddHom.coe_mk, coe_comp, Function.comp_apply, mk_apply,
+    Submodule.mkQ_apply]
   rw [Submodule.Quotient.eq]
   rw [← TensorProduct.tmul_sub]
   apply le_comap_range_lTensor f n
@@ -310,8 +322,10 @@ lemma lTensor.inverse_ofRightInverse_comp_rTensor
       Submodule.mkQ (p := LinearMap.range (lTensor Q f)) := by
   rw [LinearMap.ext_iff]
   intro y
-  simp only [coe_comp, Function.comp_apply, Submodule.mkQ_apply, lTensor.inverse_ofRightInverse_apply]
+  simp only [coe_comp, Function.comp_apply, Submodule.mkQ_apply,
+    lTensor.inverse_ofRightInverse_apply]
 
+/-- The inverse map in `lTensor.equiv` -/
 noncomputable
 def lTensor.inverse :
     Q ⊗[R] P →ₗ[R] Q ⊗[R] N ⧸ LinearMap.range (lTensor Q f) :=
@@ -437,7 +451,8 @@ def Ideal.map_includeLeft (I : Ideal A) : Ideal (A ⊗[R] B) := {
             rw [map_zero]
             apply zero_mem
         | tmul x y =>
-            simp only [LinearMap.lTensor_tmul, Submodule.coeSubtype, smul_eq_mul, Algebra.TensorProduct.tmul_mul_tmul]
+            simp only [LinearMap.lTensor_tmul, Submodule.coeSubtype, smul_eq_mul,
+              Algebra.TensorProduct.tmul_mul_tmul]
             suffices h : a * ↑x ∈ Submodule.restrictScalars R I
             · use ⟨a * ↑x, h⟩ ⊗ₜ[R] (b * y)
               rfl
@@ -468,14 +483,15 @@ lemma Ideal.map_includeLeft_eq (I : Ideal A) :
           apply Ideal.mul_mem_left
           apply Ideal.mem_map_of_mem
           exact Submodule.coe_mem a
-        simp only [Submodule.coe_restrictScalars, Algebra.TensorProduct.tmul_mul_tmul, mul_one, one_mul]
+        simp only [Submodule.coe_restrictScalars, Algebra.TensorProduct.tmul_mul_tmul,
+          mul_one, one_mul]
     | add x y hx hy =>
         rw [map_add]
         apply Ideal.add_mem _ hx hy
   · rw [Ideal.map, Ideal.span_le]
     rintro x ⟨a, ha, rfl⟩
-    simp only [Algebra.TensorProduct.includeRight_apply, Submodule.coe_set_mk, AddSubmonoid.coe_set_mk, AddSubsemigroup.coe_set_mk,
-      SetLike.mem_coe, LinearMap.mem_range]
+    simp only [Algebra.TensorProduct.includeRight_apply, Submodule.coe_set_mk,
+      AddSubmonoid.coe_set_mk, AddSubsemigroup.coe_set_mk, SetLike.mem_coe, LinearMap.mem_range]
     use ⟨a, ha⟩ ⊗ₜ 1
     rfl
 
@@ -509,7 +525,8 @@ def Ideal.map_includeRight (I : Ideal B) : Ideal (A ⊗[R] B) := {
             rw [map_zero]
             apply zero_mem
         | tmul x y =>
-            simp only [LinearMap.lTensor_tmul, Submodule.coeSubtype, smul_eq_mul, Algebra.TensorProduct.tmul_mul_tmul]
+            simp only [LinearMap.lTensor_tmul, Submodule.coeSubtype, smul_eq_mul,
+              Algebra.TensorProduct.tmul_mul_tmul]
             suffices h : b * ↑y ∈ Submodule.restrictScalars R I
             use (a * x) ⊗ₜ[R] ⟨b * ↑y, h⟩
             rfl
@@ -540,14 +557,15 @@ lemma Ideal.map_includeRight_eq (I : Ideal B) :
         apply Ideal.mul_mem_left
         apply Ideal.mem_map_of_mem
         exact Submodule.coe_mem b
-        simp only [Submodule.coe_restrictScalars, Algebra.TensorProduct.tmul_mul_tmul, mul_one, one_mul]
+        simp only [Submodule.coe_restrictScalars, Algebra.TensorProduct.tmul_mul_tmul,
+          mul_one, one_mul]
     | add x y hx hy =>
         rw [map_add]
         apply Ideal.add_mem _ hx hy
   · rw [Ideal.map, Ideal.span_le]
     rintro x ⟨b, hb, rfl⟩
-    simp only [Algebra.TensorProduct.includeRight_apply, Submodule.coe_set_mk, AddSubmonoid.coe_set_mk, AddSubsemigroup.coe_set_mk,
-      SetLike.mem_coe, LinearMap.mem_range]
+    simp only [Algebra.TensorProduct.includeRight_apply, Submodule.coe_set_mk,
+      AddSubmonoid.coe_set_mk, AddSubsemigroup.coe_set_mk, SetLike.mem_coe, LinearMap.mem_range]
     use 1 ⊗ₜ[R] ⟨b, hb⟩
     rfl
 
@@ -585,7 +603,7 @@ lemma Algebra.TensorProduct.lTensor_ker (hg : Function.Surjective g) :
 /-- If f is surjective, then the kernel of f ⊗ (id B)
   is generated by the kernel of f -/
 lemma Algebra.TensorProduct.rTensor_ker (hf : Function.Surjective f) :
-  RingHom.ker (map f (AlgHom.id R C)) =
+    RingHom.ker (map f (AlgHom.id R C)) =
     (RingHom.ker f).map (Algebra.TensorProduct.includeLeft : A →ₐ[R] A ⊗[R] C) := by
   rw [← Ideal.map_includeLeft_eq]
   apply Submodule.restrictScalars_injective R
@@ -595,18 +613,20 @@ lemma Algebra.TensorProduct.rTensor_ker (hf : Function.Surjective f) :
   rw [(rTensor_exact C f.toLinearMap.exact_subtype_ker_map hf).linearMap_ker_eq]
   rfl
 
-lemma AlgHom.coe_ker {R A B : Type*} [CommSemiring R] [Semiring A] [Semiring B] [Algebra R A] [Algebra R B] (f : A →ₐ[R] B) :
-  RingHom.ker f = RingHom.ker (f : A →+* B) := rfl
+lemma AlgHom.coe_ker {R A B : Type*} [CommSemiring R] [Semiring A] [Semiring B]
+    [Algebra R A] [Algebra R B] (f : A →ₐ[R] B) :
+    RingHom.ker f = RingHom.ker (f : A →+* B) := rfl
 
-lemma AlgHom.coe_ideal_map {R A B : Type*} [CommSemiring R] [Semiring A] [Semiring B] [Algebra R A] [Algebra R B] (f : A →ₐ[R] B) (I : Ideal A) :
-  Ideal.map f I = Ideal.map (f : A →+* B) I := rfl
+lemma AlgHom.coe_ideal_map {R A B : Type*} [CommSemiring R] [Semiring A] [Semiring B]
+    [Algebra R A] [Algebra R B] (f : A →ₐ[R] B) (I : Ideal A) :
+    Ideal.map f I = Ideal.map (f : A →+* B) I := rfl
 
 /-- If f and g are surjective morphisms of algebras,
   then the kernel of f ⊗ g is generated by the kernels of f and g -/
 theorem Algebra.TensorProduct.map_ker (hf : Function.Surjective f) (hg : Function.Surjective g) :
-  RingHom.ker (map f g) =
-    (RingHom.ker f).map (Algebra.TensorProduct.includeLeft : A →ₐ[R] A ⊗[R] C) ⊔
-      (RingHom.ker g).map (Algebra.TensorProduct.includeRight : C →ₐ[R] A ⊗[R] C) := by
+    RingHom.ker (map f g) =
+      (RingHom.ker f).map (Algebra.TensorProduct.includeLeft : A →ₐ[R] A ⊗[R] C) ⊔
+        (RingHom.ker g).map (Algebra.TensorProduct.includeRight : C →ₐ[R] A ⊗[R] C) := by
   -- rewrite map f g as the composition of two maps
   have : map f g = (map f (AlgHom.id R D)).comp (map (AlgHom.id R A) g)  := by
     exact ext rfl rfl
