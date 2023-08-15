f3fdf9b7443e9b83d4f28e5f3ef1629be3e1f736
Frédéric Dupuis <dupuisf@iro.umontreal.ca>
2023-08-14 20:31:33 -0400

    Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -1267,6 +1267,7 @@ import Mathlib.Combinatorics.SimpleGraph.Triangle.Basic
 import Mathlib.Combinatorics.Young.SemistandardTableau
 import Mathlib.Combinatorics.Young.YoungDiagram
 import Mathlib.Computability.Ackermann
+import Mathlib.Computability.AkraBazzi
 import Mathlib.Computability.DFA
 import Mathlib.Computability.Encoding
 import Mathlib.Computability.EpsilonNFA
