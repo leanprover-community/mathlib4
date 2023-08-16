aca634f4b0c3293dcab8e77fbb950b8f723adfbc
Antoine Chambert-Loir <antoine.chambert-loir@math.univ-paris-diderot.fr>
2023-08-16 13:12:59 +0200

    simplify using EW's comments

--- Mathlib/LinearAlgebra/TensorProduct/BiModule.lean
+++ Mathlib/LinearAlgebra/TensorProduct/BiModule.lean
@@ -15,28 +15,6 @@ section General
 variable {R A : Type*} [CommSemiring R] [Semiring A] [Algebra R A]
   {M : Type*} [AddCommMonoid M] [Module R M]
 
-def moduleOfRingHom (f : A →+* (M →ₗ[R] M)) : Module A M := {
-  smul := fun a x ↦ f a x
-  one_smul := fun x ↦ by
-    change f 1 x = x
-    simp only [map_one, LinearMap.one_apply]
-  mul_smul := fun a a' x ↦ by
-    change f (a * a') x = f a (f a' x)
-    simp only [map_mul, LinearMap.mul_apply]
-  smul_zero := fun a ↦ by
-    change f a 0 = 0
-    simp only [LinearMap.map_zero]
-  smul_add := fun a x y ↦ by
-    change f a (x + y) = f a x + f a y
-    simp only [map_add]
-  add_smul := fun a a' x ↦ by
-    change f (a + a') x = f a x + f a' x
-    simp only [map_add, LinearMap.add_apply]
-  zero_smul := fun x ↦ by
-    change f 0 x = 0
-    simp only [map_zero, LinearMap.zero_apply] }
-
-
 section TensorProduct
 variable {R A B : Type*} [CommSemiring R] [Ring A] [Ring B]
   [Algebra R A] [Algebra R B]
@@ -46,41 +24,32 @@ variable {R A B : Type*} [CommSemiring R] [Ring A] [Ring B]
 
 open scoped TensorProduct
 
-def toFun_aux (a : A) (b : B) : M ⊗[R] N →ₗ[R] M ⊗[R] N := by
-  apply TensorProduct.lift
-  apply LinearMap.mk₂ R (fun m n ↦ (a • m) ⊗ₜ[R] (b • n))
-  · intro m m' n
-    simp only [smul_add, TensorProduct.add_tmul]
-  · intro r m n
-    rw [TensorProduct.smul_tmul', smul_algebra_smul_comm]
-  · intro m n n'
-    simp only [smul_add, TensorProduct.tmul_add]
-  · intro r m n
-    rw [← TensorProduct.tmul_smul r, smul_algebra_smul_comm]
-
-@[simp]
-def toFun_aux_apply (a : A) (b: B) (m : M) (n : N) :
-    toFun_aux a b (m ⊗ₜ[R] n) = (a • m) ⊗ₜ[R] (b • n) := rfl
-
 def TensorProduct.moduleMap' : (A ⊗[R] B) →ₗ[R] (M ⊗[R] N) →ₗ[R] (M ⊗[R] N) := by
   apply TensorProduct.lift
-  apply LinearMap.mk₂ R toFun_aux
+  apply LinearMap.mk₂ R
+    (fun a b ↦ TensorProduct.map
+      (DistribMulAction.toModuleEnd R M a) (DistribMulAction.toModuleEnd R N b))
   · intro a a' b
     apply TensorProduct.ext'
     intro m n
-    simp only [toFun_aux_apply, LinearMap.add_apply, add_smul, TensorProduct.add_tmul]
+    simp only [DistribMulAction.toModuleEnd_apply, map_tmul, DistribMulAction.toLinearMap_apply, LinearMap.add_apply]
+    simp only [add_smul, TensorProduct.add_tmul]
   · intro r a b
     apply TensorProduct.ext'
     intro m n
-    simp only [toFun_aux_apply, smul_assoc, LinearMap.smul_apply, TensorProduct.smul_tmul']
+    simp only [DistribMulAction.toModuleEnd_apply, map_tmul, DistribMulAction.toLinearMap_apply, smul_assoc,
+      LinearMap.smul_apply]
+    simp only [TensorProduct.smul_tmul']
   · intro a b b'
     apply TensorProduct.ext'
     intro m n
-    simp only [toFun_aux_apply, LinearMap.add_apply, add_smul, TensorProduct.tmul_add]
+    simp only [DistribMulAction.toModuleEnd_apply, map_tmul, DistribMulAction.toLinearMap_apply, LinearMap.add_apply]
+    simp only [add_smul, TensorProduct.tmul_add]
   · intro r a b
     apply TensorProduct.ext'
     intro m n
-    simp only [toFun_aux_apply, smul_assoc, TensorProduct.tmul_smul, LinearMap.smul_apply]
+    simp only [DistribMulAction.toModuleEnd_apply, map_tmul, DistribMulAction.toLinearMap_apply, smul_assoc, tmul_smul,
+      LinearMap.smul_apply]
 
 def Algebra.TensorProduct.moduleMap : (A ⊗[R] B) →ₐ[R] (M ⊗[R] N) →ₗ[R] (M ⊗[R] N) := by
   apply Algebra.TensorProduct.algHomOfLinearMapTensorProduct
@@ -89,16 +58,19 @@ def Algebra.TensorProduct.moduleMap : (A ⊗[R] B) →ₐ[R] (M ⊗[R] N) →ₗ
     simp only [TensorProduct.moduleMap', TensorProduct.lift.tmul, LinearMap.mk₂_apply]
     apply _root_.TensorProduct.ext'
     intro m n
-    simp only [toFun_aux_apply, LinearMap.mul_apply, mul_smul]
+    simp only [map_mul, DistribMulAction.toModuleEnd_apply, _root_.TensorProduct.map_tmul, LinearMap.mul_apply,
+      DistribMulAction.toLinearMap_apply]
   · intro r
     simp only [TensorProduct.moduleMap', TensorProduct.lift.tmul, LinearMap.mk₂_apply]
     apply _root_.TensorProduct.ext'
     intro m n
-    simp only [toFun_aux_apply, algebraMap_smul, one_smul, Module.algebraMap_end_apply]
-    exact rfl
+    simp only [DistribMulAction.toModuleEnd_apply, map_one, _root_.TensorProduct.map_tmul,
+      DistribMulAction.toLinearMap_apply, algebraMap_smul, LinearMap.one_apply, Module.algebraMap_end_apply]
+    rfl
 
 def Algebra.TensorProduct.module : Module (A ⊗[R] B) (M ⊗[R] N) :=
-  moduleOfRingHom (Algebra.TensorProduct.moduleMap.toRingHom)
+  Module.compHom (M ⊗[R] N)
+    (Algebra.TensorProduct.moduleMap (R := R) (A := A) (B := B) (M := M) (N := N)).toRingHom
 
 lemma TensorProduct.lift_mk₂ (R : Type*) [CommSemiring R]
     {M Nₗ Pₗ : Type*} [AddCommMonoid M] [AddCommMonoid Nₗ] [AddCommMonoid Pₗ]
