59b3a80cf23c417beaa14ba67fa33ce5403d8166
Chris Hughes <chrishughes24@gmail.com>
2023-08-16 18:24:00 +0200

    small refactor

--- Mathlib/FieldTheory/AxGrothendieck.lean
+++ Mathlib/FieldTheory/AxGrothendieck.lean
@@ -3,10 +3,11 @@ Copyright (c) 2023 Chris Hughes. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Chris Hughes
 -/
-import Mathlib.Data.MvPolynomial.Basic
+
 import Mathlib.RingTheory.Algebraic
 import Mathlib.Data.Fintype.Card
 import Mathlib.ModelTheory.Algebra.Field.AlgClosed
+import Mathlib.ModelTheory.Algebra.Ring.MvPolynomial
 
 #align_import field_theory.ax_grothendieck from "leanprover-community/mathlib"@"4e529b03dd62b7b7d13806c3fb974d9d4848910e"
 
@@ -78,49 +79,7 @@ namespace FirstOrder
 
 open MvPolynomial FreeCommRing Language Field Ring
 
-def genericPolyMap {ι : Type*} (monoms : ι → Finset (ι →₀ ℕ)) :
-    ι → FreeCommRing ((Σ i : ι, monoms i) ⊕ ι) :=
-  fun i => (monoms i).attach.sum
-    (fun m => FreeCommRing.of (Sum.inl ⟨i, m⟩) *
-      Finsupp.prod m.1 (fun j n => FreeCommRing.of (Sum.inr j)^ n))
-
-noncomputable def mvPolynomialSupportLEEquiv (ι : Type*)
-    [DecidableEq ι] (R : Type _) [CommRing R] [DecidableEq R]
-    (monoms : ι → Finset (ι →₀ ℕ)) :
-    { p : ι → MvPolynomial ι R // ∀ i, (p i).support ⊆ monoms i } ≃
-      ((Σ i, monoms i) → R) :=
-  { toFun := fun p i => (p.1 i.1).coeff i.2,
-    invFun := fun p => ⟨fun i =>
-      { toFun := fun m => if hm : m ∈ monoms i then p ⟨i, ⟨m, hm⟩⟩ else 0
-        support := (monoms i).filter (fun m => ∃ hm : m ∈ monoms i, p ⟨i, ⟨m, hm⟩⟩ ≠ 0),
-        mem_support_toFun := by simp (config := {contextual := true}) },
-      fun i => Finset.filter_subset _ _⟩,
-    left_inv := fun p => by
-      ext i m
-      simp only [coeff, ne_eq, exists_prop, dite_eq_ite, Finsupp.coe_mk, ite_eq_left_iff]
-      intro hm
-      have : m ∉ (p.1 i).support := fun h => hm (p.2 i h)
-      simpa [coeff, eq_comm, MvPolynomial.mem_support_iff] using this
-    right_inv := fun p => by ext; simp [coeff] }
-
-@[simp]
-theorem lift_genericPolyMap {ι R : Type*} [CommRing R]
-    [DecidableEq ι] [DecidableEq R] (mons : ι → Finset (ι →₀ ℕ))
-    (f :  (i : ι) × { x // x ∈ mons i } ⊕ ι → R) (i : ι) :
-    FreeCommRing.lift f (genericPolyMap mons i) =
-      MvPolynomial.eval (f ∘ Sum.inr)
-        (((mvPolynomialSupportLEEquiv ι R mons).symm
-          (f ∘ Sum.inl)).1 i) := by
-  simp only [genericPolyMap, Finsupp.prod_pow, map_sum, map_mul, lift_of, support,
-    mvPolynomialSupportLEEquiv, coeff, map_prod, Finset.sum_filter, MvPolynomial.eval_eq,
-    ne_eq, Function.comp, Equiv.coe_fn_symm_mk, Finsupp.coe_mk]
-  conv_rhs => rw [← Finset.sum_attach]
-  refine Finset.sum_congr rfl ?_
-  intros m _
-  simp only [Finsupp.prod, map_prod, map_pow, lift_of, Subtype.coe_eta, Finset.coe_mem,
-    exists_prop, true_and, dite_eq_ite, ite_true, ite_not]
-  split_ifs with h0 <;> simp_all
-
+/-- The collection of first order formulas corresponding to the Ax-Grothendieck theorem. -/
 noncomputable def genericPolyMapSurjectiveOfInjective {ι : Type*} [Fintype ι]
     (mons : ι → Finset (ι →₀ ℕ)) : Language.ring.Sentence :=
   let l1 : List (Language.ring.Formula ((Σ i : ι, mons i) ⊕ (Fin 2 × ι))) :=
@@ -163,7 +122,7 @@ theorem realize_genericPolyMapSurjectiveOfInjective
         Function.Surjective (fun v i => MvPolynomial.eval v (p.1 i)) := by
   letI := Classical.decEq K
   letI := Classical.decEq ι
-  rw [Equiv.forall_congr_left' (mvPolynomialSupportLEEquiv ι K mons)]
+  rw [Equiv.forall_congr_left' (mvPolynomialSupportLEEquiv mons)]
   simp only [Sentence.Realize, Formula.Realize, genericPolyMapSurjectiveOfInjective, Function.comp,
     Sum.map, id_eq, BoundedFormula.realize_iAlls, Sum.elim_inr, BoundedFormula.realize_imp,
     BoundedFormula.realize_foldr_inf, List.mem_map, Finset.mem_toList, Finset.mem_univ, true_and,
