4ac441d9bee95825383f2cbcfcb6e051bac3e9b4
Chris Hughes <chrishughes24@gmail.com>
2023-08-17 10:46:33 +0200

    hopefully finish linting

--- Mathlib/ModelTheory/Algebra/Field/CharP.lean
+++ Mathlib/ModelTheory/Algebra/Field/CharP.lean
@@ -81,6 +81,7 @@ instance model_fieldOfChar_of_charP [Field K] [CompatibleRing K]
     [CharP K p] : (Theory.fieldOfChar p).Model K :=
   charP_iff_model_fieldOfChar.2 inferInstance
 
+/- Not an instance because it caused performance problems in a different file. -/
 theorem charP_of_model_fieldOfChar [Field K] [CompatibleRing K]
     [h : (Theory.fieldOfChar p).Model K] : CharP K p :=
   charP_iff_model_fieldOfChar.1 h
