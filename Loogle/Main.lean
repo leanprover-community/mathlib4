1c11004353b0bfdec06f1f2edbcf97619b3d585e
Joachim Breitner <mail@joachim-breitner.de>
2023-08-17 21:55:51 +0200

    Take module name on the command line

--- Loogle/Main.lean
+++ Loogle/Main.lean
@@ -8,24 +8,25 @@ open Lean Core Meta Elab Term Command
 elab "compileTimeSearchPath" : term =>
   return toExpr (← searchPathRef.get)
 
-unsafe def main (args : List String) : IO Unit := do
-  match args with
-  | [query] => do
-    searchPathRef.set compileTimeSearchPath
-    withImportModules [{module := `Mathlib.Tactic.Find}] {} 0 fun env => do
-      let ctx := {fileName := "", fileMap := default}
-      let state := {env}
-      Prod.fst <$> (CoreM.toIO · ctx state) do
-        match Parser.runParserCategory (← getEnv) `find_patterns query with
-        | .error err => IO.println err
-        | .ok s => do
-          MetaM.run' do
-            let args ← TermElabM.run' $ Mathlib.Tactic.Find.parseFindPatterns (.mk s)
-            match ← Mathlib.Tactic.Find.find args with
-            | .ok (header, names) => do
-                IO.println (← header.toString)
-                names.forM (fun name => IO.println name)
-            | .error err => IO.println (← err.toString)
+unsafe def work (mod : String) (query : String) : IO Unit := do
+  searchPathRef.set compileTimeSearchPath
+  withImportModules [{module := mod.toName}] {} 0 fun env => do
+    let ctx := {fileName := "", fileMap := default}
+    let state := {env}
+    Prod.fst <$> (CoreM.toIO · ctx state) do
+      match Parser.runParserCategory (← getEnv) `find_patterns query with
+      | .error err => IO.println err
+      | .ok s => do
+        MetaM.run' do
+          let args ← TermElabM.run' $ Mathlib.Tactic.Find.parseFindPatterns (.mk s)
+          match ← Mathlib.Tactic.Find.find args with
+          | .ok (header, names) => do
+              IO.println (← header.toString)
+              names.forM (fun name => IO.println name)
+          | .error err => IO.println (← err.toString)
 
-  | _ => do
-      IO.println "Usage: loogle 'query' (one argument!)"
+unsafe def main (args : List String) : IO Unit := do
+   match args with
+  | [query] => work "Mathlib" query
+  | [mod, query] => work mod query
+  | _ => IO.println "Usage: loogle [module] query"
