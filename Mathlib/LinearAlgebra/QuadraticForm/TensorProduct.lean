4ad432b9abacf2804e0a90bd766bcbfa8fcd004c
Eric Wieser <wieser.eric@gmail.com>
2023-08-17 15:36:52 +0000

    update

--- Mathlib/LinearAlgebra/QuadraticForm/TensorProduct.lean
+++ Mathlib/LinearAlgebra/QuadraticForm/TensorProduct.lean
@@ -11,10 +11,8 @@ import Mathlib.LinearAlgebra.QuadraticForm.Basic
 
 ## Main definitions
 
-* `QuadraticForm.tensorDistrib (B₁ ⊗ₜ B₂)`: the quadratic form on `M₁ ⊗ M₂` constructed by applying
-  `B₁` on `M₁` and `B₂` on `M₂`. This construction is not available in characteristic two.
-* `QuadraticForm.tensorDistribEquiv`: `QuadraticForm.tensorDistrib` as an equivalence on finite free
-  modules.
+* `QuadraticForm.tensorDistrib (Q₁ ⊗ₜ Q₂)`: the quadratic form on `M₁ ⊗ M₂` constructed by applying
+  `Q₁` on `M₁` and `Q₂` on `M₂`. This construction is not available in characteristic two.
 
 -/
 
@@ -34,9 +32,6 @@ variable [Algebra R A] [Module R M₁] [Module A M₁]
 variable [SMulCommClass R A M₁] [SMulCommClass A R M₁] [IsScalarTower R A M₁]
 variable [Module R M₂] [Invertible (2 : R)]
 
-instance : Module A (QuadraticForm A M₁ ⊗[R] QuadraticForm R M₂) :=
-  TensorProduct.leftModule
-
 
 variable (R A) in
 /-- The tensor product of two quadratic forms injects into quadratic forms on tensor products.
@@ -44,37 +39,37 @@ variable (R A) in
 Note this is heterobasic; the quadratic form on the left can take values in a larger ring than
 the one on the right. -/
 def tensorDistrib : QuadraticForm A M₁ ⊗[R] QuadraticForm R M₂ →ₗ[A] QuadraticForm A (M₁ ⊗[R] M₂) :=
-  BilinForm.toQuadraticFormLinearMap A A (M₁ ⊗[R] M₂)
-    ∘ₗ BilinForm.tensorDistrib R A (M₁ := M₁) (M₂ := M₂)
-    ∘ₗ AlgebraTensorModule.map
+  letI : Invertible (2 : A) := (Invertible.map (algebraMap R A) 2).copy 2 (map_ofNat _ _).symm
+  let toQ := BilinForm.toQuadraticFormLinearMap A A (M₁ ⊗[R] M₂)
+  let tmulB := BilinForm.tensorDistrib R A (M₁ := M₁) (M₂ := M₂)
+  let toB := AlgebraTensorModule.map
       (QuadraticForm.associatedHom A : QuadraticForm A M₁ →ₗ[A] BilinForm A M₁)
-      (QuadraticForm.associatedHom R : QuadraticForm R M₁ →ₗ[R] BilinForm R M₁)
-
+      (QuadraticForm.associatedHom R : QuadraticForm R M₂ →ₗ[R] BilinForm R M₂)
+  toQ ∘ₗ tmulB ∘ₗ toB
 
 -- TODO: make the RHS `MulOpposite.op (Q₂ m₂) • Q₁ m₁` so that this has a nicer defeq for
 -- `R = A` of `Q₁ m₁ * Q₂ m₂`.
 @[simp]
-theorem tensorDistrib_tmul (Q₁ : QuadraticForm A M₁) (Q₂ : QuadraticForm R M₂) (m₁ : M₁) (m₂ : M₂)
-    (m₁' : M₁) (m₂' : M₂) :
-    tensorDistrib (A := A) (Q₁ ⊗ₜ B₂) (m₁ ⊗ₜ m₂) (m₁' ⊗ₜ m₂')
-      = Q₂ m₂ • Q₁ m₁ :=
-  rfl
-#align bilin_form.tensor_distrib_tmul QuadraticForm.tensorDistrib_tmulₓ
+theorem tensorDistrib_tmul (Q₁ : QuadraticForm A M₁) (Q₂ : QuadraticForm R M₂) (m₁ : M₁) (m₂ : M₂) :
+    tensorDistrib R A (Q₁ ⊗ₜ Q₂) (m₁ ⊗ₜ m₂) = Q₂ m₂ • Q₁ m₁ :=
+  letI : Invertible (2 : A) := (Invertible.map (algebraMap R A) 2).copy 2 (map_ofNat _ _).symm
+  (BilinForm.tensorDistrib_tmul _ _ _ _ _ _).trans <| congr_arg₂ _
+    (associated_eq_self_apply _ _ _) (associated_eq_self_apply _ _ _)
 
-/-- The tensor product of two bilinear forms, a shorthand for dot notation. -/
+/-- The tensor product of two quadratic forms, a shorthand for dot notation. -/
 @[reducible]
-protected def tmul (B₁ : QuadraticForm A M₁) (B₂ : QuadraticForm R M₂) : QuadraticForm A (M₁ ⊗[R] M₂) :=
-  tensorDistrib (A := A) (B₁ ⊗ₜ[R] B₂)
-#align bilin_form.tmul QuadraticForm.tmul
+protected def tmul (Q₁ : QuadraticForm A M₁) (Q₂ : QuadraticForm R M₂) :
+    QuadraticForm A (M₁ ⊗[R] M₂) :=
+  tensorDistrib (A := A) (Q₁ ⊗ₜ[R] Q₂)
 
-/-- The base change of a bilinear form. -/
+/-- The base change of a quadratic form. -/
 protected def baseChange (Q : QuadraticForm R M₂) : QuadraticForm A (A ⊗[R] M₂) :=
   QuadraticForm.tmul (R := R) (A := A) (M₁ := A) (M₂ := M₂) (QuadraticForm.sq) Q
 
 @[simp]
 theorem baseChange_tmul (Q : QuadraticForm R M₂) (a : A) (m₂ : M₂) :
     Q.baseChange (a ⊗ₜ m₂) = Q m₂ • a :=
-  rfl
+  tensorDistrib_tmul _ _
 
 end CommSemiring
 
