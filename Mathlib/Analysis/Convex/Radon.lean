e54a3d646fdf2290d7d5c99af05e3b3e7cb250ca
Vasily Nesterov <vas.nesterov63@gmail.com>
2023-08-18 11:24:13 +0300

    family instead of set

--- Mathlib/Analysis/Convex/Radon.lean
+++ Mathlib/Analysis/Convex/Radon.lean
@@ -30,31 +30,29 @@ universe u
 
 variable {𝕜 : Type*} {E : Type u} [LinearOrderedField 𝕜] [AddCommGroup E] [Module 𝕜 E]
 
-/-- If `s` is a set of affine dependent vectors, it has subset `I` with the property that
-convex hulls of `I` and `s \ I` intersect. -/
-theorem Radon_partition (s : Set E)
-(h : ¬AffineIndependent 𝕜 ((↑) : s → E)) : ∃ (I : Set E), (I ⊆ s) ∧
-(Set.Nonempty ((convexHull 𝕜 I) ∩ (convexHull 𝕜 (s \ I)))) := by
+/-- Any family `f` of affine dependent vectors contains a set `I` with the property that
+convex hulls of `I` and `Iᶜ` intersect. -/
+theorem radon_partition {ι : Type*} {f : ι → E}
+    (h : ¬AffineIndependent 𝕜 f) : ∃ (I : Set ι),
+    (Set.Nonempty ((convexHull 𝕜 (f '' I)) ∩ (convexHull 𝕜 (f '' Iᶜ)))) := by  
   unfold AffineIndependent at h; push_neg at h
   rcases h with ⟨s1, w, h_wsum, h_vsum, nonzero_w_index, h1, h2⟩
-  let I : Finset { x // x ∈ s } := s1.filter (fun x => w x ≥ 0)
-  let J : Finset { x // x ∈ s } := s1.filter (fun x => w x < 0)
-  use (I : Set {x // x ∈ s})
-  constructor
-  exact Set.coe_subset
+  let I : Finset ι := s1.filter (fun i => w i ≥ 0)
+  let J : Finset ι := s1.filter (fun i => w i < 0)
+  use (I : Set ι)
 
   let weights_sum_I := ∑ i in I, w i
   let weights_sum_J := ∑ i in J, w i
   have h3 : weights_sum_I + weights_sum_J = 0 := by
     rw [←h_wsum]; simp
-    have h3_aux := Finset.sum_filter_add_sum_filter_not s1 (fun x => 0 ≤ w x) w
+    have h3_aux := Finset.sum_filter_add_sum_filter_not s1 (fun i => 0 ≤ w i) w
     simp at h3_aux; exact h3_aux
 
   have h4 : (weights_sum_I > 0) ∧ (weights_sum_J < 0) := by
     rcases (lt_or_gt_of_ne h2) with h_neg | h_pos
     have h_J_neg : weights_sum_J < 0 := by
       rw [←neg_pos, ←Finset.sum_neg_distrib]
-      let neg_w : {x // x ∈ s} → 𝕜 := fun v => -w v
+      let neg_w : ι → 𝕜 := fun i => -w i
       have h4_aux : neg_w nonzero_w_index ≤ (Finset.sum J neg_w) := by
         apply Finset.single_le_sum
         intros i hi
@@ -77,14 +75,14 @@ theorem Radon_partition (s : Set E)
     linarith
   rcases h4 with ⟨h_I_pos, h_J_neg⟩
 
-  let w' : { x // x ∈ s} → 𝕜 := fun i => if w i ≥ 0
+  let w' : ι → 𝕜 := fun i => if w i ≥ 0
     then (w i) / weights_sum_I
     else -(w i) / weights_sum_I
-  let p : E := ∑ v in I, w' v • (v : E) -- point of intersection
+  let p : E := ∑ i in I, (w' i) • (f i) -- point of intersection
 
-  have h5_I : ∑ v in I, w' v = 1 := by
-    let w'' : { x // x ∈ s} → 𝕜 := fun i => (w i) / weights_sum_I
-    have h5_aux : ∑ v in I, w' v = ∑ v in I, w'' v := by
+  have h5_I : ∑ i in I, w' i = 1 := by
+    let w'' : ι → 𝕜 := fun i => (w i) / weights_sum_I
+    have h5_aux : ∑ i in I, w' i = ∑ i in I, w'' i := by
       apply Finset.sum_congr; rfl
       intros i hi
       rw [Finset.mem_filter] at hi
@@ -98,11 +96,11 @@ theorem Radon_partition (s : Set E)
     apply div_self
     exact ne_of_gt h_I_pos
 
-  have h5_J : ∑ v in J, w' v = 1 := by
-    let w'' : { x // x ∈ s} → 𝕜 := fun i => (w i) / weights_sum_J
+  have h5_J : ∑ i in J, w' i = 1 := by
+    let w'' : ι → 𝕜 := fun i => (w i) / weights_sum_J
     have h5_aux1 : weights_sum_I = -weights_sum_J := by
       exact Iff.mp add_eq_zero_iff_eq_neg h3
-    have h5_aux2 : ∑ v in J, w' v = ∑ v in J, w'' v := by
+    have h5_aux2 : ∑ i in J, w' i = ∑ i in J, w'' i := by
       apply Finset.sum_congr; rfl
       intros i hi
       rw [Finset.mem_filter] at hi
@@ -121,17 +119,17 @@ theorem Radon_partition (s : Set E)
     intro; simp; split_ifs
     repeat {apply div_nonneg; linarith; linarith}
 
-  let p' : E := (Finset.sum J fun v => w' v • (v : E))
+  let p' : E := (Finset.sum J fun i => (w' i) • (f i))
   have h7 : p = p' := by
     suffices h7_suf1 : p -ᵥ p' = 0 by
       exact eq_of_vsub_eq_zero h7_suf1
 
-    rw [Finset.weightedVSub_eq_weightedVSubOfPoint_of_sum_eq_zero s1 w Subtype.val h_wsum 0]
+    rw [Finset.weightedVSub_eq_weightedVSubOfPoint_of_sum_eq_zero s1 w f h_wsum 0]
       at h_vsum
     simp at h_vsum; simp
 
-    let g : { x // x ∈ s} → E := fun v =>  weights_sum_I⁻¹ • ((w v) • (v : E))
-    have h7_aux2 : ∑ v in I, (w' v) • (v : E) = ∑ v in I, g v := by
+    let g : ι → E := fun i =>  weights_sum_I⁻¹ • ((w i) • (f i))
+    have h7_aux2 : ∑ i in I, (w' i) • (f i) = ∑ i in I, g i := by
       apply Finset.sum_congr; rfl
       intros i hi
       rw [Finset.mem_filter] at hi
@@ -141,7 +139,7 @@ theorem Radon_partition (s : Set E)
       rw [mul_comm, mul_smul]
       exfalso; linarith
 
-    have h7_aux3 : ∑ v in J, (w' v) • (v : E) = -∑ v in J, g v := by
+    have h7_aux3 : ∑ i in J, (w' i) • (f i) = -∑ i in J, g i := by
       rw [←Finset.sum_neg_distrib]
       apply Finset.sum_congr; rfl
       intros i hi
@@ -154,7 +152,7 @@ theorem Radon_partition (s : Set E)
 
     rw [h7_aux2, h7_aux3]
     simp
-    have h7_aux4 := Finset.sum_filter_add_sum_filter_not s1 (fun x => 0 ≤ w x) g
+    have h7_aux4 := Finset.sum_filter_add_sum_filter_not s1 (fun i => 0 ≤ w i) g
     simp at h7_aux4; rw [h7_aux4]
     rw [←Finset.smul_sum, h_vsum, smul_zero]
 
@@ -166,9 +164,8 @@ theorem Radon_partition (s : Set E)
   intros i _; exact h6 i
   exact h5_I
   intros i hi
-  have h8 : (i : E) ∈ (Lean.Internal.coeM (I : Set {x // x ∈ s})) := by
-    apply Set.mem_coe_of_mem (Subtype.mem i)
-    exact hi
+  have h8 : f i ∈ (f '' I) := by
+    exact Set.mem_image_of_mem f hi
   apply Set.mem_of_mem_of_subset h8
   apply subset_convexHull
 
@@ -178,13 +175,58 @@ theorem Radon_partition (s : Set E)
   intros i _; exact h6 i
   exact h5_J
   intros i hi
-  have h9 : (i : E) ∈ (s \ Lean.Internal.coeM (I : Set {x // x ∈ s})) := by
-    apply Set.mem_diff_of_mem
-    exact Subtype.mem i
+  have h9 : f i ∈ (f '' Iᶜ) := by
+    apply Set.mem_image_of_mem
+    apply Set.mem_compl
     intro h9_neg
-    have h9_aux1 : i ∈ I := Iff.mpr Finset.mem_coe (Set.mem_of_mem_coe h9_neg)
     have := (Iff.mp Finset.mem_filter hi).right
-    have := (Iff.mp Finset.mem_filter h9_aux1).right
+    have := (Iff.mp Finset.mem_filter h9_neg).right
     linarith
   apply Set.mem_of_mem_of_subset h9
   apply subset_convexHull
+
+/-- If `s` is a set of affine dependent vectors, it has subset `I` with the property that
+convex hulls of `I` and `s \ I` intersect. -/
+theorem radon_set_partition (s : Set E)
+    (h : ¬AffineIndependent 𝕜 ((↑) : s → E)) : ∃ (I : Set E), (I ⊆ s) ∧
+    (Set.Nonempty ((convexHull 𝕜 I) ∩ (convexHull 𝕜 (s \ I)))) := by
+  have h1 := radon_partition h
+  rcases h1 with ⟨I, h1⟩
+  use I; constructor
+  exact Set.coe_subset
+
+  have h2 : Subtype.val '' I = Lean.Internal.coeM I := by
+    apply Set.ext
+    intro x; constructor
+    intro hx
+    rw [Set.mem_image] at hx; rcases hx with ⟨x1, hx1, hx2⟩
+    rw [←hx2]
+    apply Set.mem_coe_of_mem; exact hx1
+    intro hx
+    have hx := Set.mem_of_mem_coe hx
+    rw [Set.mem_image]
+    constructor; constructor
+    exact hx; rfl
+  
+  have h3 : Subtype.val '' Iᶜ = s \ Lean.Internal.coeM I := by
+    apply Set.ext
+    intro x; constructor
+    intro hx
+    rw [Set.mem_image] at hx; rcases hx with ⟨x1, hx1, hx2⟩
+    rw [←hx2, Set.mem_diff]; constructor
+    exact Subtype.mem x1
+    intro hx3
+    have hx3 := Set.mem_of_mem_coe hx3
+    simp at hx3
+    exact hx1 hx3
+    intro hx
+    rw [Set.mem_diff] at hx; rcases hx with ⟨hx1, hx2⟩
+    rw [Set.mem_image]
+    use {val := x, property := hx1}; constructor
+    by_contra hx3; simp at hx3
+    simp at hx2
+    contrapose hx2; simp
+    apply Set.mem_coe_of_mem
+    exact hx3; rfl
+  
+  rw [←h3, ←h2]; exact h1
