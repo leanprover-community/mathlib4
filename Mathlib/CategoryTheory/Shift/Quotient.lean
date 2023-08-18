cc2cbc0961a75bf5e977cc3d019301a4a0f865f1
Joël Riou <joel.riou@universite-paris-saclay.fr>
2023-08-18 16:37:02 +0530

    better Shift.Quotient

--- Mathlib/CategoryTheory/Shift/Quotient.lean
+++ Mathlib/CategoryTheory/Shift/Quotient.lean
@@ -1,65 +1,59 @@
+/-
+Copyright (c) 2023 Joël Riou. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Joël Riou
+-/
 import Mathlib.CategoryTheory.Shift.Induced
 import Mathlib.CategoryTheory.Quotient
 
-open CategoryTheory Category
+/-!
+# Shift on a quotient category
 
-variable {C : Type _} [Category C]
-  (r : HomRel C) (A : Type _) [AddMonoid A] [HasShift C A]
+Let `C` be a category equipped a shift by a monoid `A`. If we have a relation
+on morphisms `r : HomRel C` that is compatible with the shift (i.e. if two
+morphisms `f` and `g` are related, then `f⟦a⟧'` and `g⟦a⟧'` are also related
+for all `a : A`), then the quotient category `Quotient r` is equipped with
+a shift.
+
+The condition `r.IsCompatibleWithShift A` on the relation `r` is a class so that
+the shift can be automatically infered on the quotient category.
+
+-/
+
+universe v u w
+
+open CategoryTheory
+
+variable {C : Type u} [Category.{v} C]
+  (r : HomRel C) (A : Type w) [AddMonoid A] [HasShift C A]
 
 namespace HomRel
 
+/-- A relation on morphisms is compatible with the shift by a monoid `A` when the
+relation if preserved by the shift. -/
 class IsCompatibleWithShift : Prop :=
-  translate : ∀ (a : A) ⦃X Y : C⦄ (f g : X ⟶ Y) (_ : r f g), r (f⟦a⟧') (g⟦a⟧')
+  /-- the condition that the relation is preserved by the shift -/
+  condition : ∀ (a : A) ⦃X Y : C⦄ (f g : X ⟶ Y), r f g → r (f⟦a⟧') (g⟦a⟧')
 
 end HomRel
 
 namespace CategoryTheory
 
-variable (s : A → Quotient r ⥤ Quotient r)
-  (i : ∀ a, Quotient.functor r ⋙ s a ≅ shiftFunctor C a ⋙ Quotient.functor r)
-
-lemma HasShift.quotient'_aux :
-    Nonempty (Full ((whiskeringLeft C _ (Quotient r)).obj (Quotient.functor r))) ∧
-        Faithful ((whiskeringLeft C _ (Quotient r)).obj (Quotient.functor r)) :=
-  ⟨⟨inferInstance⟩, inferInstance⟩
-
-noncomputable def HasShift.quotient' :
+/-- The shift by a monoid `A` induced on a quotient category `Quotient r` when the
+relation `r` is compatible with the shift. -/
+noncomputable instance HasShift.quotient [r.IsCompatibleWithShift A] :
     HasShift (Quotient r) A :=
-  HasShift.induced (Quotient.functor r) A s i (HasShift.quotient'_aux r)
+  HasShift.induced (Quotient.functor r) A
+    (fun a => Quotient.lift r (shiftFunctor C a ⋙ Quotient.functor r)
+      (fun _ _ _ _ hfg => Quotient.sound r (HomRel.IsCompatibleWithShift.condition _ _ _ hfg)))
+    (fun _ => Quotient.lift.isLift _ _ _) ⟨⟨inferInstance⟩, inferInstance⟩
 
-noncomputable def Functor.CommShift.quotient' :
-    letI : HasShift (Quotient r) A := HasShift.quotient' r A s i
+/-- The functor `Quotient.functor r : C ⥤ Quotient r` commutes with the shift. -/
+noncomputable instance Quotient.functor_commShift [r.IsCompatibleWithShift A] :
     (Quotient.functor r).CommShift A :=
   Functor.CommShift.ofInduced _ _ _ _ _
 
-variable {A}
-
-def Quotient.shiftFunctor' [r.IsCompatibleWithShift A] (a : A) : Quotient r ⥤ Quotient r :=
-  Quotient.lift r (CategoryTheory.shiftFunctor C a ⋙ Quotient.functor r)
-    (fun _ _ _ _ hfg => Quotient.sound r (HomRel.IsCompatibleWithShift.translate _ _ _ hfg))
-
-def Quotient.shiftFunctor'Factors [r.IsCompatibleWithShift A] (a : A) :
-    Quotient.functor r ⋙ Quotient.shiftFunctor' r a ≅
-      CategoryTheory.shiftFunctor C a ⋙ Quotient.functor r :=
-  Quotient.lift.isLift _ _ _
-
-variable (A)
-
-noncomputable instance HasShift.quotient [r.IsCompatibleWithShift A] :
-    HasShift (Quotient r) A :=
-  HasShift.quotient' r A (Quotient.shiftFunctor' r) (Quotient.shiftFunctor'Factors r)
-
-noncomputable instance Quotient.functor_commShift [r.IsCompatibleWithShift A] :
-    (Quotient.functor r).CommShift A :=
-  Functor.CommShift.quotient' _ _ _ _
-
--- the construction is made irreducible in order to prevent timeouts
+-- the construction is made irreducible in order to prevent timeouts and abuse of defeq
 attribute [irreducible] HasShift.quotient Quotient.functor_commShift
-
-noncomputable example [r.IsCompatibleWithShift A] :
-    HasShift (Quotient r) A := inferInstance
-
-noncomputable example [r.IsCompatibleWithShift A] :
-    Functor.CommShift (Quotient.functor r) A := inferInstance
 
 end CategoryTheory
