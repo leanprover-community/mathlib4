86a7df2bfb7fcaf21c443f40001f4cd51a396a5a
Oliver Nash <github@olivernash.org>
2023-08-18 17:17:30 +0100

    Appease linter

--- Mathlib/LinearAlgebra/Trace.lean
+++ Mathlib/LinearAlgebra/Trace.lean
@@ -319,12 +319,11 @@ lemma trace_restrict_eq_of_forall_mem [IsDomain R] [IsPrincipalIdealRing R]
   rw [trace_eq_matrix_trace R snf.bM, trace_eq_matrix_trace R snf.bN]
   set A : Matrix (Fin n) (Fin n) R := toMatrix snf.bN snf.bN (f.restrict hf')
   set B : Matrix ι ι R := toMatrix snf.bM snf.bM f
-  have h₁ : ∀ i, A i i = B (snf.f i) (snf.f i) := by simp
-  have h₂ : ∀ i, B i i ≠ 0 → i ∈ Set.range snf.f := fun i hi ↦ by
+  have aux : ∀ i, B i i ≠ 0 → i ∈ Set.range snf.f := fun i hi ↦ by
     contrapose! hi; exact snf.repr_eq_zero_of_nmem_range ⟨_, (hf _)⟩ hi
   change ∑ i, A i i = ∑ i, B i i
-  rw [← Finset.sum_filter_of_ne (p := fun j ↦ j ∈ Set.range snf.f) (by simpa using h₂)]
-  simp [h₁]
+  rw [← Finset.sum_filter_of_ne (p := fun j ↦ j ∈ Set.range snf.f) (by simpa using aux)]
+  simp
 
 end
 
