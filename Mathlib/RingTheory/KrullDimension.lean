9dc05886ff292db5e4e54c027e8e31e56c29f4c5
FMLJohn <417963616@qq.com>
2023-08-19 11:55:35 +0100

    proved Ideal.IsPrime.isMaximal_of_isArtinianRing

--- Mathlib/RingTheory/KrullDimension.lean
+++ Mathlib/RingTheory/KrullDimension.lean
@@ -99,7 +99,7 @@ instance (F : Type _) [Field F] : OrderTop (LTSeries (PrimeSpectrum F)) where
 lemma eq_zero_of_Field (F : Type _) [Field F] : ringKrullDim F = 0 :=
   krullDim.eq_len_of_orderTop
 
-lemma eq_zero_of_IsArtinianRing (R : Type _) [CommRing R] [Nontrivial R] [IsArtinianRing R] :
+lemma eq_zero_of_isArtinianRing (R : Type _) [CommRing R] [Nontrivial R] [IsArtinianRing R] :
   ringKrullDim R = 0 := by
 { haveI : Inhabited (PrimeSpectrum R) := Classical.inhabited_of_nonempty
     PrimeSpectrum.instNonemptyPrimeSpectrum
@@ -176,7 +176,7 @@ noncomputable def PID_finiteDimensional (R : Type _) [CommRing R] [IsPrincipalId
           rw [←mul_left_cancel₀ h hr2] at hr1
           exact (hr1 x.isUnit).elim
 
-lemma PID_eq_one_of_not_IsField (R : Type _) [CommRing R] [IsPrincipalIdealRing R] [IsDomain R]
+lemma PID_eq_one_of_not_isField (R : Type _) [CommRing R] [IsPrincipalIdealRing R] [IsDomain R]
   (hR : ¬ IsField R) : ringKrullDim R = 1 := by
 { rw [ringKrullDim, @krullDim.eq_len_of_orderTop _ _ (PID_finiteDimensional _ hR)]; rfl }
 
@@ -447,3 +447,36 @@ krullDim.eq_of_OrderIso (PrimeSpectrum.IicToLocalizationAtPrime_OrderIso I)
 end aboutHeightAndLocalization
 
 end ringKrullDim
+
+
+
+section IsArtinianRing
+
+variable {R : Type _} [CommRing R] [IsArtinianRing R]
+variable (p : Ideal R) [Ideal.IsPrime p]
+
+instance Ideal.IsPrime.isMaximal_of_isArtinianRing : p.IsMaximal := by
+{ refine' Ideal.Quotient.maximal_of_isField p {
+    exists_pair_ne := ⟨0, 1, zero_ne_one⟩
+    mul_comm := mul_comm
+    mul_inv_cancel := by
+      intro x hx
+      haveI ins1 : IsArtinianRing (R ⧸ p) := (@Ideal.Quotient.mk_surjective R _ p).isArtinianRing
+      obtain ⟨n, hn⟩ : ∃ (n : ℕ), Ideal.span {x^n} = Ideal.span {x^(n+1)}
+      { obtain ⟨n, h⟩ := @IsArtinian.monotone_stabilizes _ _ _ _ _ ins1
+          ⟨λ m ↦ OrderDual.toDual (Ideal.span {x^m}), λ m n h y hy ↦ by
+            dsimp [OrderDual.toDual] at *
+            rw [Ideal.mem_span_singleton] at hy ⊢
+            obtain ⟨z, rfl⟩ := hy
+            exact dvd_mul_of_dvd_left (pow_dvd_pow _ h) _⟩
+        specialize h (n + 1) (by norm_num)
+        exact ⟨n, h⟩ }
+      have H : x^n ∈ Ideal.span {x^(n+1)} := by
+      { rw [←hn]; exact Submodule.subset_span (Set.mem_singleton _) }
+      rw [Ideal.mem_span_singleton] at H
+      obtain ⟨y, hy⟩ := H
+      rw [pow_add, mul_assoc, pow_one] at hy
+      conv_lhs at hy => rw [←mul_one (x^n)]
+      exact ⟨y, mul_left_cancel₀ (pow_ne_zero _ hx) hy.symm⟩ } }
+
+end IsArtinianRing
