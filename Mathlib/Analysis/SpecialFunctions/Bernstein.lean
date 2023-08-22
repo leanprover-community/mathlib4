b6e8f14013150e93945edf7dfe7a1cf727793b7b
Alex J Best <alex.j.best@gmail.com>
2023-08-22 16:50:48 +0000

    fix

--- Mathlib/Analysis/SpecialFunctions/Bernstein.lean
+++ Mathlib/Analysis/SpecialFunctions/Bernstein.lean
@@ -116,6 +116,7 @@ theorem variance {n : ℕ} (h : 0 < (n : ℝ)) (x : I) :
     (∑ k : Fin (n + 1), (x - k/ₙ : ℝ) ^ 2 * bernstein n k x) = (x : ℝ) * (1 - x) / n := by
   have h' : (n : ℝ) ≠ 0 := ne_of_gt h
   apply_fun fun x : ℝ => x * n using GroupWithZero.mul_right_injective h'
+  apply_fun fun x : ℝ => x * n using GroupWithZero.mul_right_injective h'
   dsimp
   conv_lhs => simp only [Finset.sum_mul, z]
   conv_rhs => rw [div_mul_cancel _ h']
