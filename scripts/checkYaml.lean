416628b79a64d1853924f0f8072030c09d43993f
Mario Carneiro <di.gama@gmail.com>
2023-08-21 20:03:10 -0400

    fix: reduce imports for scripts

--- scripts/checkYaml.lean
+++ scripts/checkYaml.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Eric Wieser
 -/
 
-import Mathlib.Tactic.Basic
+import Std.Tactic.Lint
 import Mathlib.Tactic.ToExpr
 
 /-! # Script to check `undergrad.yaml`, `overview.yaml`, and `100.yaml`
