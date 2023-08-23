adf505fa0b8f4e001aca4ff558c138e26404a71f
Jujian Zhang <jujian.zhang1998@outlook.com>
2023-08-23 17:18:48 +0100

    got `interList_qf_equiv` at 1600000 heartbeats

--- Mathlib/Algebra/Module/Submodule/JordanHolder.lean
+++ Mathlib/Algebra/Module/Submodule/JordanHolder.lean
@@ -26,106 +26,121 @@ namespace CompositionSeries
 
 /-- if `x ≤ y` are both `R`-submodule of `M`, we can mathematically form their quotient but type
 theoretically more complicated, so introduce a definition to use a notation. -/
-@[reducible]
-private def quot (x y : Submodule R M) : Type _ := x ⧸ (Submodule.comap x.subtype y)
+private def quot {M : Type _} [AddCommGroup M] [Module R M] (x y : Submodule R M) : Type _ :=
+  x ⧸ (Submodule.comap x.subtype y)
 local infix:80 "⧸ₛ" => quot
 
-instance addCommGroup_quot (x y : Submodule R M)  : AddCommGroup (x ⧸ₛ y) := inferInstance
-
-instance module_quot (x y : Submodule R M)  : Module R (x ⧸ₛ y) := inferInstance
-
 /-- quotient factor of a composition series -/
-@[reducible]
-def qf (i : ℕ) (hi : i < s.length) : Type _ :=
-s ⟨i + 1, (add_lt_add_iff_right _).mpr hi⟩ ⧸ₛ s ⟨i, hi.trans $ lt_add_one _⟩
+def qf (i : Fin s.length) : Type _ :=
+s i.succ ⧸ₛ s i.castSucc
 
-instance qf_isSimpleModule (i : ℕ) (hi : i < s.length) : IsSimpleModule R (s.qf i hi) := by
+instance (i : Fin s.length) : AddCommGroup (s.qf i) := by
+  delta qf quot; infer_instance
+
+instance (i : Fin s.length) : Module R (s.qf i) := by
+  delta qf quot; infer_instance
+
+lemma qf_def (i : Fin s.length) : s.qf i = s i.succ ⧸ₛ s i.castSucc := rfl
+
+instance qf_isSimpleModule (i : Fin s.length) : IsSimpleModule R $ s.qf i := by
+  delta qf quot
   rw [←covby_iff_quot_is_simple (s.strictMono.monotone _)]
-  pick_goal 2
-  · norm_num
-  exact s.step ⟨i, hi⟩
+  · exact s.step i
+  · change i.1 ≤ i.1 + 1
+    linarith
 
 /-- Given an `R`-submodule `N`, we can form a list of submodule of `N` by taking intersections with
 a given composition series-/
-@[reducible]
 def interList : List (Submodule R N) :=
   s.toList.map $ fun si => Submodule.comap N.subtype (N ⊓ si)
 
 lemma interList_length : (s.interList N).length = s.length + 1 :=
 by rw [interList, List.length_map, CompositionSeries.toList, List.length_ofFn]
 
-lemma interList_get (i : ℕ) (hi : i < s.length + 1) :
-    (s.interList N).get ⟨i, ((s.interList_length N).symm ▸ hi)⟩ =
-    Submodule.comap N.subtype (N ⊓ s ⟨i, hi⟩) := by
+private def interList_get (i : Fin s.length) : Submodule R N :=
+  (s.interList N).get (i.castLE <| by rw [interList_length]; linarith)
+
+private def interList_get_succ (i : Fin s.length) : Submodule R N :=
+  (s.interList N).get (i.succ.castLE <| by rw [interList_length])
+
+lemma interList_get_eq (i : Fin s.length) :
+    s.interList_get N i =
+    Submodule.comap N.subtype (N ⊓ s i.castSucc) := by
+  delta interList_get interList
   rw [List.get_map]
   congr 2
-  exact List.get_ofFn _ ⟨i, _⟩
+  exact List.get_ofFn _ _
 
-lemma interList_get_le_get_succ (i : ℕ) (hi : i < s.length) :
-    (s.interList N).get ⟨i, by rw [s.interList_length N]; exact hi.trans (lt_add_one _)⟩ ≤
-    (s.interList N).get ⟨i + 1, (s.interList_length N).symm ▸ (add_lt_add_iff_right 1).mpr hi⟩ := by
-  generalize_proofs h1 h2
-  rw [interList_length] at h1 h2
-  rw [interList_get _ _ _ h1, interList_get _ _ _ h2]
+lemma interList_get_succ_eq (i : Fin s.length) :
+    s.interList_get_succ N i =
+    Submodule.comap N.subtype (N ⊓ s i.succ) := by
+  delta interList_get_succ interList
+  rw [List.get_map]
+  congr 2
+  exact List.get_ofFn _ _
+
+lemma interList_get_le_get_succ (i : Fin s.length) :
+    s.interList_get N i ≤ s.interList_get_succ N i := by
+  rw [interList_get_eq _ _ _, interList_get_succ_eq _ _ _]
   refine Submodule.comap_mono (le_inf inf_le_left (inf_le_right.trans $ s.strictMono.monotone ?_))
-  norm_num
-
-/-- quotient factor of intersection between a submodule and a composition series. -/
-@[reducible]
-def interList_qf (i : ℕ) (hi : i < s.length) : Type _ :=
-    (s.interList N).get ⟨i + 1, (s.interList_length N).symm ▸ (add_lt_add_iff_right 1).mpr hi⟩ ⧸ₛ
-    (s.interList N).get ⟨i, by rw [s.interList_length N]; exact hi.trans (lt_add_one _)⟩
-
-instance interList_qf_addCommGroup (i : ℕ) (hi : i < s.length) :
-    AddCommGroup (s.interList_qf N i hi) := by
-  delta interList_qf; infer_instance
-
-instance interList_qf_module (i : ℕ) (hi : i < s.length) :
-    Module R (s.interList_qf N i hi) := by
-  delta interList_qf; infer_instance
+  change i.1 ≤ i.1 + 1
+  linarith
 
 /-- Given composition series `s`, the canonical map `s_{i + 1} ⊓ N` to `i`-th quotient factor of
   `s`-/
-@[reducible]
-def interList_get_to_qf (i : ℕ) (hi : i < s.length) :
-  (s.interList N).get ⟨i+1, (s.interList_length N).symm ▸ (add_lt_add_iff_right 1).mpr hi⟩ →ₗ[R]
-  s.qf i hi :=
-(Submodule.mkQ _).comp $ N.subtype.restrict $ λ ⟨x, hx1⟩ hx2 ↦ by
-  rw [interList_get, Submodule.mem_comap] at hx2
-  exact hx2.2
+@[simps! apply]
+def interList_get_succ_to_qf (i : Fin s.length) :
+  s.interList_get_succ N i →ₗ[R] s.qf i :=
+(Submodule.mkQ _).comp $ N.subtype.restrict $ λ _ h ↦ by
+  rw [interList_get_succ_eq, Submodule.mem_comap] at h
+  exact h.2
 
-lemma interList_get_to_qf_ker (i : ℕ) (hi : i < s.length) :
-  LinearMap.ker (s.interList_get_to_qf N i hi) =
-  (Submodule.comap
-    ((s.interList N).get
-      ⟨i + 1, (s.interList_length N).symm ▸ (add_lt_add_iff_right 1).mpr hi⟩).subtype
-    ((s.interList N).get
-      ⟨i, lt_of_lt_of_le hi $ (s.interList_length N).symm ▸ by norm_num⟩)) := by
+lemma interList_get_succ_to_qf_ker (i : Fin s.length) :
+    LinearMap.ker (s.interList_get_succ_to_qf N i) =
+    Submodule.comap (s.interList_get_succ N i).subtype (s.interList_get N i) := by
   ext ⟨x, hx⟩
-  rw [LinearMap.mem_ker, Submodule.mem_comap, Submodule.subtype_apply, LinearMap.comp_apply,
-    LinearMap.restrict_apply, Submodule.mkQ_apply, Submodule.Quotient.mk_eq_zero,
-    Submodule.mem_comap]
-  simp_rw [Submodule.subtype_apply]
-  rw [interList_get s N i (hi.trans (lt_add_one _)), Submodule.mem_comap,
-    Submodule.subtype_apply, Submodule.mem_inf, iff_and_self]
-  intros hx'
-  rw [interList_get s N _ _] at hx
-  pick_goal 2
-  · linarith
-  exact hx.1
+  rw [LinearMap.mem_ker, Submodule.mem_comap, Submodule.subtype_apply, interList_get_succ_to_qf_apply,
+    Submodule.Quotient.mk_eq_zero, LinearMap.restrict_apply, Submodule.mem_comap]
+  change x.1 ∈ _ ↔ _
+  rw [interList_get_eq, Submodule.mem_comap, Submodule.subtype_apply, Submodule.mem_inf,
+    iff_and_self]
+  rintro -
+  exact x.2
 
-/--
-the quotient factor of a composition series intersecting with a submodule
--/
-@[reducible]
-def inter_qf (i : ℕ) (hi : i < s.length) : Type _ :=
-  (s.interList N).get ⟨i + 1, (s.interList_length N).symm ▸ (add_lt_add_iff_right 1).mpr hi⟩ ⧸ₛ
-  ((s.interList N).get ⟨i, (s.interList_length N).symm ▸ hi.trans (by linarith)⟩)
+/-- quotient factor of intersection between a submodule and a composition series. -/
+def interList_qf (i : Fin s.length) : Type _ :=
+    s.interList_get_succ N i ⧸ₛ s.interList_get N i
 
-instance inter_qf_addCommGroup (i : ℕ) (hi : i < s.length) : AddCommGroup <| s.inter_qf N i hi :=
-  inferInstance
+instance {M : Type _} [AddCommGroup M] [Module R M] (x y : Submodule R M) :
+  AddCommGroup (x ⧸ₛ y) := by delta quot; exact Submodule.Quotient.addCommGroup _
 
-instance inter_qf_module (i : ℕ) (hi : i < s.length) : Module R <| s.inter_qf N i hi :=
-  inferInstance
+instance {M : Type _} [AddCommGroup M] [Module R M] (x y : Submodule R M) :
+  Module R (x ⧸ₛ y) := by delta quot; exact Submodule.Quotient.module _
+
+instance (i : Fin s.length) : AddCommGroup (s.interList_qf N i) := by
+  delta interList_qf
+  infer_instance
+
+instance (i : Fin s.length) : Module R (s.interList_qf N i) := by
+  delta interList_qf
+  infer_instance
+
+private noncomputable def aux1
+    {x : Submodule R N} {k : Submodule R x} {y : Type _} [AddCommGroup y] [Module R y]
+    (l : x →ₗ[R] y) (eq1 : LinearMap.ker l = k) : (x ⧸ k) ≃ₗ[R] LinearMap.range l :=
+  LinearEquiv.trans
+    (Submodule.Quotient.equiv _ _ (LinearEquiv.refl _ _) $ by
+      rw [eq1]
+      exact Submodule.map_id _ : (x ⧸ k) ≃ₗ[R] (x ⧸ LinearMap.ker l))
+    (LinearMap.quotKerEquivRange l)
+
+set_option maxHeartbeats 1600000 in
+/-- the `i`-th quotient factor of `s ⊓ N` is equivalent to the range of
+  `(s.interList_get_succ_to_qf N i)`-/
+noncomputable def interList_qf_equiv (i : Fin s.length) :
+    (s.interList_qf N i) ≃ₗ[R] LinearMap.range (s.interList_get_succ_to_qf N i) :=
+  aux1 N (s.interList_get_succ_to_qf N i) (s.interList_get_succ_to_qf_ker N i)
+
+example : true := rfl
 
 end CompositionSeries
