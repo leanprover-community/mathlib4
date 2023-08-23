2fca8dcece8e293eba9141030a1d0f9ed344f700
adomani <adomani@gmail.com>
2023-08-23 06:02:21 +0200

    Add support for `MonoidAlgebra`

--- Mathlib/Algebra/MonoidAlgebra/NoZeroDivisors.lean
+++ Mathlib/Algebra/MonoidAlgebra/NoZeroDivisors.lean
@@ -45,10 +45,67 @@ These conditions are sufficient, but not necessary.  As mentioned above, *Kaplan
 asserts that `A` being torsion-free may be enough.
 -/
 
+open Finsupp
+
+variable {R A : Type*} [Semiring R]
+
+namespace MonoidAlgebra
+
+/-- The coefficient of a monomial in a product `f * g` that can be reached in at most one way
+as a product of monomials in the supports of `f` and `g` is a product. -/
+theorem mul_apply_mul_eq_mul_of_uniqueAdd [Mul A] {f g : MonoidAlgebra R A} {a0 b0 : A}
+    (h : UniqueMul f.support g.support a0 b0) :
+    (f * g) (a0 * b0) = f a0 * g b0 := by
+  classical
+  simp_rw [mul_apply, sum, ← Finset.sum_product']
+  refine' (Finset.sum_eq_single (a0, b0) _ _).trans (if_pos rfl) <;> simp_rw [Finset.mem_product]
+  · refine fun ab hab hne => if_neg (fun he => hne <| Prod.ext ?_ ?_)
+    exacts [(h hab.1 hab.2 he).1, (h hab.1 hab.2 he).2]
+  · refine fun hnmem => ite_eq_right_iff.mpr (fun _ => ?_)
+    rcases not_and_or.mp hnmem with af | bg
+    · rw [not_mem_support_iff.mp af, zero_mul]
+    · rw [not_mem_support_iff.mp bg, mul_zero]
+
+instance [NoZeroDivisors R] [Mul A] [UniqueProds A] :
+    NoZeroDivisors (MonoidAlgebra R A) where
+  eq_zero_or_eq_zero_of_mul_eq_zero := fun {a b} ab => by
+    contrapose! ab
+    obtain ⟨da, a0, db, b0, h⟩ := UniqueProds.uniqueMul_of_nonempty
+      (support_nonempty_iff.mpr ab.1) (support_nonempty_iff.mpr ab.2)
+    refine support_nonempty_iff.mp ⟨da * db, ?_⟩
+    rw [mem_support_iff] at a0 b0 ⊢
+    exact mul_apply_mul_eq_mul_of_uniqueAdd h ▸ mul_ne_zero a0 b0
+
+end MonoidAlgebra
 
 namespace AddMonoidAlgebra
 
-open Finsupp
+/-- The coefficient of a monomial in a product `f * g` that can be reached in at most one way
+as a product of monomials in the supports of `f` and `g` is a product. -/
+theorem mul_apply_add_eq_mul_of_uniqueAdd [Add A] {f g : AddMonoidAlgebra R A} {a0 b0 : A}
+    (h : UniqueAdd f.support g.support a0 b0) :
+    (f * g) (a0 + b0) = f a0 * g b0 := by
+  classical
+  simp_rw [mul_apply, sum, ← Finset.sum_product']
+  refine' (Finset.sum_eq_single (a0, b0) _ _).trans (if_pos rfl) <;> simp_rw [Finset.mem_product]
+  · refine fun ab hab hne => if_neg (fun he => hne <| Prod.ext ?_ ?_)
+    exacts [(h hab.1 hab.2 he).1, (h hab.1 hab.2 he).2]
+  · refine fun hnmem => ite_eq_right_iff.mpr (fun _ => ?_)
+    rcases not_and_or.mp hnmem with af | bg
+    · rw [not_mem_support_iff.mp af, zero_mul]
+    · rw [not_mem_support_iff.mp bg, mul_zero]
+
+instance [NoZeroDivisors R] [Add A] [UniqueSums A] :
+    NoZeroDivisors (AddMonoidAlgebra R A) where
+  eq_zero_or_eq_zero_of_mul_eq_zero := fun {a b} ab => by
+    contrapose! ab
+    obtain ⟨da, a0, db, b0, h⟩ := UniqueSums.uniqueAdd_of_nonempty
+      (support_nonempty_iff.mpr ab.1) (support_nonempty_iff.mpr ab.2)
+    refine support_nonempty_iff.mp ⟨da + db, ?_⟩
+    rw [mem_support_iff] at a0 b0 ⊢
+    exact mul_apply_add_eq_mul_of_uniqueAdd h ▸ mul_ne_zero a0 b0
+
+end AddMonoidAlgebra
 
 instance {L σ : Type*} [LinearOrder L] [AddGroup L]
     [ContravariantClass L L (· + ·) (· ≤ ·)]
@@ -66,40 +123,11 @@ instance {L σ : Type*} [LinearOrder L] [AddGroup L]
   ⟨_, A.max'_mem A0, _, B.max'_mem B0, fun a b aA bB =>
     (add_eq_add_iff_eq_and_eq (A.le_max' a aA) (B.le_max' b bB)).mp⟩ }
 
-variable {R A : Type*} [Semiring R]
-
-/-- The coefficient of a monomial in a product `f * g` that can be reached in at most one way
-as a product of monomials in the supports of `f` and `g` is a product. -/
-theorem mul_apply_add_eq_mul_of_uniqueAdd [Add A] {f g : AddMonoidAlgebra R A} {a0 b0 : A}
-    (h : UniqueAdd f.support g.support a0 b0) :
-    (f * g) (a0 + b0) = f a0 * g b0 := by
-  classical
-  simp_rw [mul_apply, sum, ← Finset.sum_product']
-  refine' (Finset.sum_eq_single (a0, b0) _ _).trans (if_pos rfl) <;> simp_rw [Finset.mem_product]
-  · refine fun ab hab hne => if_neg (fun he => hne <| Prod.ext ?_ ?_)
-    exacts [(h hab.1 hab.2 he).1, (h hab.1 hab.2 he).2]
-  · refine fun hnmem => ite_eq_right_iff.mpr (fun _ => ?_)
-    rcases not_and_or.mp hnmem with af | bg
-    · rw [not_mem_support_iff.mp af, zero_mul]
-    · rw [not_mem_support_iff.mp bg, mul_zero]
-
-instance {A : Type*} [NoZeroDivisors R] [Add A] [UniqueSums A] :
-    NoZeroDivisors (AddMonoidAlgebra R A) where
-  eq_zero_or_eq_zero_of_mul_eq_zero := fun {a b} ab => by
-    contrapose! ab
-    obtain ⟨da, a0, db, b0, h⟩ := UniqueSums.uniqueAdd_of_nonempty
-      (support_nonempty_iff.mpr ab.1) (support_nonempty_iff.mpr ab.2)
-    refine support_nonempty_iff.mp ⟨da + db, ?_⟩
-    rw [mem_support_iff] at a0 b0 ⊢
-    exact mul_apply_add_eq_mul_of_uniqueAdd h ▸ mul_ne_zero a0 b0
-
-/-- The proof goes via the equivalence `R ≃ₗ[ℚ] (Basis.ofVectorSpaceIndex ℚ R) →₀ ℚ`,
+/-- The proof goes via the equivalence `A ≃ₗ[ℚ] (Basis.ofVectorSpaceIndex ℚ A) →₀ ℚ`,
 i.e. choosing a basis.
 Once we have a basis, we use the embedding into sequences of coordinates and all the instances
 that `ℚ` already has.
 -/
-instance {R : Type*} [AddCommGroup R] [Module ℚ R] : UniqueSums R :=
-let r := (Basis.ofVectorSpace ℚ R).repr
+instance [AddCommGroup A] [Module ℚ A] : UniqueSums A :=
+let r := (Basis.ofVectorSpace ℚ A).repr
 UniqueSums.addHom_image_of_injective r r.injective inferInstance
-
-end AddMonoidAlgebra
