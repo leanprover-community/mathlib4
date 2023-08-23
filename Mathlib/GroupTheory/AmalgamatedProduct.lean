b8277dfd5d765a6c3db3c53bfadf80645e963cce
Chris Hughes <chrishughes24@gmail.com>
2023-08-23 17:06:36 +0200

    define smulWord

--- Mathlib/GroupTheory/AmalgamatedProduct.lean
+++ Mathlib/GroupTheory/AmalgamatedProduct.lean
@@ -50,4 +50,45 @@ theorem hom_ext {f g : AmalgamatedProduct G H φ →* K}
   simp only [FunLike.ext_iff] at h
   exact h _ _
 
+section Word
+
+variable (G H φ) (n : ι)
+
+structure Word extends CoprodI.Word G where
+  normalized : ∀ x ∈ toList, x.2 ∈ (φ x.1).range → x.1 = n
+
+variable {G H φ}
+
+noncomputable def smulWord {i : ι} (g : G i) (w : Word G H φ n) : Word G H φ n :=
+  letI := Classical.propDecidable
+  if hg1 : g = 1 then w
+  else if hg : (∃ x : H, φ i x = g) ∧ i ≠ n then
+    have _ : 0 < if i = n then 0 else 1 := by simp [hg.2]
+    smulWord (φ n (Classical.choose hg.1)) w
+  else
+    match w with
+    | ⟨⟨[], _, _⟩,_⟩ =>
+      ⟨⟨[⟨i, g⟩], by simp_all, by simp_all⟩, by
+        simpa using hg⟩
+    | ⟨⟨⟨j, h⟩::l, ne_one, chain_ne⟩, normalized⟩ =>
+      if hij : i = j
+      then smulWord (g * hij ▸ h)
+        ⟨⟨l,
+          fun x hx => ne_one x (List.mem_cons_of_mem _ hx),
+          (List.chain'_cons'.1 chain_ne).2⟩,
+          by simp_all; tauto⟩
+      else ⟨⟨⟨i, g⟩ :: ⟨j, h⟩ :: l,
+        by simp; simp at ne_one; tauto,
+        by simp only [ne_eq, List.chain'_cons, hij,
+            not_false_eq_true, chain_ne, and_self]⟩,
+        by
+          simp only [ne_eq, not_and, not_not, forall_exists_index] at hg
+          simp only [List.find?, List.mem_cons, MonoidHom.mem_range,
+            forall_exists_index, forall_eq_or_imp] at normalized ⊢
+          exact ⟨hg, normalized⟩⟩
+termination_by smulWord i g w => (w.toList.length,
+  letI := Classical.decEq ι; if i = n then 0 else 1)
+
+end Word
+
 end AmalgamatedProduct
