29e7c4c36cb8525be16e012c6b89df1eb3ea90e2
Chris Hughes <chrishughes24@gmail.com>
2023-08-23 14:53:06 +0200

    just the universal property

---
+++ Mathlib/GroupTheory/Subgroup/AmalgamatedProduct.lean
@@ -0,0 +1,53 @@
+/-
+Copyright (c) 2023 Chris Hughes. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Chris Hughes
+-/
+
+import Mathlib.GroupTheory.CoprodI
+import Mathlib.GroupTheory.QuotientGroup
+
+
+variable {ι : Type*} (G : ι → Type*) [∀ i, Group (G i)] (H : Type*) [Group H]
+  (φ : ∀ i, H →* G i) {K : Type*} [Group K]
+
+open Monoid CoprodI Subgroup
+
+def AmalgamatedProduct : Type _ :=
+  CoprodI G ⧸ normalClosure
+    (⋃ (i : ι) (j : ι), Set.range (fun g => of (φ i g) * (of (φ j g))⁻¹))
+
+namespace AmalgamatedProduct
+
+variable {G H φ}
+
+instance : Group (AmalgamatedProduct G H φ) :=
+  QuotientGroup.Quotient.group _
+
+def of {i : ι} : G i →* AmalgamatedProduct G H φ :=
+  (QuotientGroup.mk' _).comp CoprodI.of
+
+def lift (f : ∀ i, G i →* K) (hf : ∀ i, (f i).comp (φ i) = 1) :
+    AmalgamatedProduct G H φ →* K :=
+  QuotientGroup.lift _ (CoprodI.lift f)
+    (show normalClosure _ ≤ (CoprodI.lift f).ker
+      from normalClosure_le_normal <| by
+        simp only [FunLike.ext_iff, MonoidHom.coe_comp, Function.comp_apply,
+          MonoidHom.one_apply] at hf
+        simp [Set.iUnion_subset_iff, Set.range_subset_iff, SetLike.mem_coe, MonoidHom.mem_ker, hf])
+
+@[simp]
+theorem lift_of (f : ∀ i, G i →* K) (hf : ∀ i, (f i).comp (φ i) = 1) {i : ι} (g : G i) :
+    (lift f hf) (of g : AmalgamatedProduct G H φ) = f i g := by
+  delta AmalgamatedProduct
+  simp [lift, of]
+
+@[ext high]
+theorem hom_ext {f g : AmalgamatedProduct G H φ →* K}
+    (h : ∀ i, f.comp (of : G i →* _) = g.comp of) : f = g := by
+  delta AmalgamatedProduct
+  ext i x
+  simp only [FunLike.ext_iff] at h
+  exact h _ _
+
+end AmalgamatedProduct
