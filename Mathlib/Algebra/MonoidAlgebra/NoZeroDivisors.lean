69e79ebdad36dbcb15af693faf0acc7977ff4f61
adomani <adomani@gmail.com>
2023-08-24 17:45:14 +0200

    chore(Algebra/MonoidAlgebra/NoZeroDivisors): AddMul golf

--- Mathlib/Algebra/MonoidAlgebra/NoZeroDivisors.lean
+++ Mathlib/Algebra/MonoidAlgebra/NoZeroDivisors.lean
@@ -79,7 +79,7 @@ theorem mul_apply_mul_eq_mul_of_uniqueMul [Mul A] {f g : MonoidAlgebra R A} {a0 
     · rw [not_mem_support_iff.mp af, zero_mul]
     · rw [not_mem_support_iff.mp bg, mul_zero]
 
-instance [NoZeroDivisors R] [Mul A] [UniqueProds A] :
+instance instNoZeroDivisorsOfUniqueProds [NoZeroDivisors R] [Mul A] [UniqueProds A] :
     NoZeroDivisors (MonoidAlgebra R A) where
   eq_zero_or_eq_zero_of_mul_eq_zero {a b} ab := by
     contrapose! ab
@@ -97,26 +97,11 @@ namespace AddMonoidAlgebra
 as a product of monomials in the supports of `f` and `g` is a product. -/
 theorem mul_apply_add_eq_mul_of_uniqueAdd [Add A] {f g : AddMonoidAlgebra R A} {a0 b0 : A}
     (h : UniqueAdd f.support g.support a0 b0) :
-    (f * g) (a0 + b0) = f a0 * g b0 := by
-  classical
-  simp_rw [mul_apply, sum, ← Finset.sum_product']
-  refine' (Finset.sum_eq_single (a0, b0) _ _).trans (if_pos rfl) <;> simp_rw [Finset.mem_product]
-  · refine fun ab hab hne => if_neg (fun he => hne <| Prod.ext ?_ ?_)
-    exacts [(h hab.1 hab.2 he).1, (h hab.1 hab.2 he).2]
-  · refine fun hnmem => ite_eq_right_iff.mpr (fun _ => ?_)
-    rcases not_and_or.mp hnmem with af | bg
-    · rw [not_mem_support_iff.mp af, zero_mul]
-    · rw [not_mem_support_iff.mp bg, mul_zero]
+    (f * g) (a0 + b0) = f a0 * g b0 :=
+MonoidAlgebra.mul_apply_mul_eq_mul_of_uniqueMul (A := Multiplicative A) h
 
-instance [NoZeroDivisors R] [Add A] [UniqueSums A] :
-    NoZeroDivisors (AddMonoidAlgebra R A) where
-  eq_zero_or_eq_zero_of_mul_eq_zero {a b} ab := by
-    contrapose! ab
-    obtain ⟨da, a0, db, b0, h⟩ := UniqueSums.uniqueAdd_of_nonempty
-      (support_nonempty_iff.mpr ab.1) (support_nonempty_iff.mpr ab.2)
-    refine support_nonempty_iff.mp ⟨da + db, ?_⟩
-    rw [mem_support_iff] at a0 b0 ⊢
-    exact mul_apply_add_eq_mul_of_uniqueAdd h ▸ mul_ne_zero a0 b0
+instance [NoZeroDivisors R] [Add A] [UniqueSums A] : NoZeroDivisors (AddMonoidAlgebra R A) :=
+MonoidAlgebra.instNoZeroDivisorsOfUniqueProds (A := Multiplicative A)
 
 end AddMonoidAlgebra
 
