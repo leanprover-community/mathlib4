465dada7e266c30301c51484fa16980e86f8a1d5
Eric Rodriguez <ericrboidi@gmail.com>
2023-08-25 20:01:08 +0000

    feat: unit equivalences (#6526)
    Useful for Wedderburn's little theorem.
    
    
    
    Co-authored-by: Eric Rodriguez <37984851+ericrbg@users.noreply.github.com>

--- Mathlib/GroupTheory/Submonoid/Center.lean
+++ Mathlib/GroupTheory/Submonoid/Center.lean
@@ -97,5 +97,20 @@ end
 
 end Submonoid
 
+variable (M)
+
+/-- For a monoid, the units of the center inject into the center of the units. This is not an
+equivalence in general; one case when it is is for groups with zero, which is covered in
+`centerUnitsEquivUnitsCenter`. -/
+@[to_additive (attr := simps! apply_coe_val)
+  "For an additive monoid, the units of the center inject into the center of the units."]
+def unitsCenterToCenterUnits [Monoid M] : (Submonoid.center M)ˣ →* Submonoid.center (Mˣ) :=
+  (Units.map (Submonoid.center M).subtype).codRestrict _ <| fun u r ↦ Units.ext <| u.1.prop r
+
+@[to_additive]
+theorem unitsCenterToCenterUnits_injective [Monoid M] :
+    Function.Injective (unitsCenterToCenterUnits M) :=
+  fun _a _b h => Units.ext <| Subtype.ext <| congr_arg (Units.val ∘ Subtype.val) h
+
 -- Guard against import creep
 assert_not_exists Finset
