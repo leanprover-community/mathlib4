c4d9ecc6a33cdad34df7bbe728c15468bee72796
Kyle Miller <kmill31415@gmail.com>
2023-08-25 15:29:00 +0000

    a little review

--- test/Clean.lean
+++ test/Clean.lean
@@ -1,20 +1,29 @@
 import Mathlib.Tactic.Clean
 import Std
 
-def this : 2 + 2 = 4 := by clean id rfl
+namespace Tests
 
-def this1 : 2 + 2 = 4 := by exact id rfl
+def withClean : 2 + 2 = 4 := by clean id rfl
 
-/--
-info: theorem this1.proof_1 : 2 + 2 = 4 :=
-id rfl
--/
-#guard_msgs in
-#print this1.proof_1
+def withExact : 2 + 2 = 4 := by exact id rfl
 
 /--
-info: theorem this.proof_1 : 2 + 2 = 2 + 2 :=
+info: theorem Tests.withClean.proof_1 : 2 + 2 = 2 + 2 :=
 rfl
 -/
 #guard_msgs in
-#print this.proof_1
+#print Tests.withClean.proof_1
+
+/--
+info: theorem Tests.withExact.proof_1 : 2 + 2 = 4 :=
+id rfl
+-/
+#guard_msgs in
+#print Tests.withExact.proof_1
+
+example : True := by
+  let x : id Nat := by dsimp; exact 1
+  guard_hyp x :ₛ id Nat := id (1 : Nat)
+  let y : id Nat := by clean by dsimp; exact 1
+  guard_hyp y :ₛ id Nat := (1 : Nat)
+  trivial
