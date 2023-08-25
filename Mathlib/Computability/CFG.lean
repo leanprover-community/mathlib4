bc56f3947f76ab38fd3886fa6f3430b391058ad7
madvorak <martin.dvorak@matfyz.cz>
2023-08-25 13:51:28 +0200

    NT capitalized

--- Mathlib/Computability/CFG.lean
+++ Mathlib/Computability/CFG.lean
@@ -24,23 +24,23 @@ inductive Symbol (T : Type u) (N : Type u)
 
 /-- Context-free grammar that generates words over the alphabet `T` (a type of terminals). -/
 structure CFG (T : Type u) where
-  nt : Type u                            -- type of nonterminals
-  initial : nt                           -- initial nonterminal
-  rules : List (nt × List (Symbol T nt)) -- rewrite rules
+  NT : Type u                            -- type of nonterminals
+  initial : NT                           -- initial nonterminal
+  rules : List (NT × List (Symbol T NT)) -- rewrite rules
 
 namespace CFG
 
 variable {T : Type u}
 
 /-- One step of context-free transformation. -/
-def Transforms (g : CFG T) (w₁ w₂ : List (Symbol T g.nt)) : Prop :=
-  ∃ r : g.nt × List (Symbol T g.nt),
+def Transforms (g : CFG T) (w₁ w₂ : List (Symbol T g.NT)) : Prop :=
+  ∃ r : g.NT × List (Symbol T g.NT),
     r ∈ g.rules ∧
-    ∃ u v : List (Symbol T g.nt),
+    ∃ u v : List (Symbol T g.NT),
       w₁ = u ++ [Symbol.nonterminal r.fst] ++ v ∧ w₂ = u ++ r.snd ++ v
 
 /-- Any number of steps of context-free transformation. -/
-def Derives (g : CFG T) : List (Symbol T g.nt) → List (Symbol T g.nt) → Prop :=
+def Derives (g : CFG T) : List (Symbol T g.NT) → List (Symbol T g.NT) → Prop :=
   Relation.ReflTransGen g.Transforms
 
 /-- Accepts a word (a list of terminals) iff it can be derived from the initial nonterminal. -/
@@ -57,35 +57,35 @@ def _root_.Language.IsCF (L : Language T) : Prop :=
 
 variable {g : CFG T}
 
-lemma Transforms.single {v w : List (Symbol T g.nt)} (hvw : g.Transforms v w) :
+lemma Transforms.single {v w : List (Symbol T g.NT)} (hvw : g.Transforms v w) :
     g.Derives v w :=
   Relation.ReflTransGen.single hvw
 
 @[refl]
-lemma Derives.refl {w : List (Symbol T g.nt)} :
+lemma Derives.refl {w : List (Symbol T g.NT)} :
     g.Derives w w :=
   Relation.ReflTransGen.refl
 
 @[trans]
-lemma Derives.derives {u v w : List (Symbol T g.nt)}
+lemma Derives.derives {u v w : List (Symbol T g.NT)}
     (huv : g.Derives u v) (hvw : g.Derives v w) :
     g.Derives u w :=
   Relation.ReflTransGen.trans huv hvw
 
-lemma Derives.transforms {u v w : List (Symbol T g.nt)}
+lemma Derives.transforms {u v w : List (Symbol T g.NT)}
     (huv : g.Derives u v) (hvw : g.Transforms v w) :
     g.Derives u w :=
   huv.derives hvw.single
 
-lemma Transforms.derives {u v w : List (Symbol T g.nt)}
+lemma Transforms.derives {u v w : List (Symbol T g.NT)}
     (huv : g.Transforms u v) (hvw : g.Derives v w) :
     g.Derives u w :=
   huv.single.derives hvw
 
-lemma Derives.eq_or_head {u w : List (Symbol T g.nt)} (huw : g.Derives u w) :
-    u = w ∨ ∃ v : List (Symbol T g.nt), g.Transforms u v ∧ g.Derives v w :=
+lemma Derives.eq_or_head {u w : List (Symbol T g.NT)} (huw : g.Derives u w) :
+    u = w ∨ ∃ v : List (Symbol T g.NT), g.Transforms u v ∧ g.Derives v w :=
   Relation.ReflTransGen.cases_head huw
 
-lemma Derives.eq_or_tail {u w : List (Symbol T g.nt)} (huw : g.Derives u w) :
-    u = w ∨ ∃ v : List (Symbol T g.nt), g.Derives u v ∧ g.Transforms v w :=
+lemma Derives.eq_or_tail {u w : List (Symbol T g.NT)} (huw : g.Derives u w) :
+    u = w ∨ ∃ v : List (Symbol T g.NT), g.Derives u v ∧ g.Transforms v w :=
   (Relation.ReflTransGen.cases_tail huw).casesOn (Or.inl ∘ Eq.symm) Or.inr
