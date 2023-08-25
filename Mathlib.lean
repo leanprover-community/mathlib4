61d1093eacb1725b745086ff6ba1f2a5712e23a6
Jujian Zhang <jujian.zhang1998@outlook.com>
2023-08-25 12:56:30 +0100

    update `Mathlib.lean`

--- Mathlib.lean
+++ Mathlib.lean
@@ -1514,6 +1514,7 @@ import Mathlib.Data.List.NodupEquivFin
 import Mathlib.Data.List.OfFn
 import Mathlib.Data.List.Pairwise
 import Mathlib.Data.List.Palindrome
+import Mathlib.Data.List.PartialOrderType
 import Mathlib.Data.List.Perm
 import Mathlib.Data.List.Permutation
 import Mathlib.Data.List.Prime
