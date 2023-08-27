70851c424c7a38e314008eb0c017f65b989d597d
adomani <adomani@gmail.com>
2023-08-27 06:55:50 +0200

    Move commented imports next to commented example

--- test/Monotonicity.lean
+++ test/Monotonicity.lean
@@ -7,8 +7,6 @@ import Mathlib.Algebra.Order.Ring.Defs
 import Mathlib.Data.List.Defs
 import Mathlib.Tactic.Monotonicity
 import Mathlib.Tactic.NormNum
--- import Mathlib.MeasureTheory.Function.LocallyIntegrable
--- import Mathlib.MeasureTheory.Measure.Lebesgue.Integral
 
 open List Set
 
@@ -415,6 +413,8 @@ example {x y z w : ℕ} : true := by
 --   mono
 --   mono
 
+-- import Mathlib.MeasureTheory.Function.LocallyIntegrable
+-- import Mathlib.MeasureTheory.Measure.Lebesgue.Integral
 -- example : ∫ x in Icc 0 1, real.exp x ≤ ∫ x in Icc 0 1, real.exp (x+1) := by
 --   mono
 --   · exact real.continuous_exp.locally_integrable.integrable_on_is_compact is_compact_Icc
