1410a31e9d5007099d4b02b3eaecbb95a36937f2
Kyle Miller <kmill31415@gmail.com>
2023-08-27 13:15:09 +0000

    term elaborator, handle `show .. from ..`

--- test/Clean.lean
+++ test/Clean.lean
@@ -3,9 +3,24 @@ import Std
 
 namespace Tests
 
-def withClean : 2 + 2 = 4 := by clean id rfl
+def x : Id Nat := by dsimp [Id]; exact 1
+def x' : Id Nat := clean% by dsimp [Id]; exact 1
 
-def withExact : 2 + 2 = 4 := by exact id rfl
+/--
+info: def Tests.x : Id Nat :=
+id 1
+-/
+#guard_msgs in #print x
+
+/--
+info: def Tests.x' : Id Nat :=
+1
+-/
+#guard_msgs in #print x'
+-- def x : Id Nat := 1
+
+def withClean : 2 + 2 = 4 := clean% by exact id rfl
+def withoutClean : 2 + 2 = 4 := by exact id rfl
 
 /--
 info: theorem Tests.withClean.proof_1 : 2 + 2 = 2 + 2 :=
@@ -15,15 +30,25 @@ rfl
 #print Tests.withClean.proof_1
 
 /--
-info: theorem Tests.withExact.proof_1 : 2 + 2 = 4 :=
+info: theorem Tests.withoutClean.proof_1 : 2 + 2 = 4 :=
 id rfl
 -/
 #guard_msgs in
-#print Tests.withExact.proof_1
+#print Tests.withoutClean.proof_1
 
 example : True := by
   let x : id Nat := by dsimp; exact 1
   guard_hyp x :ₛ id Nat := id (1 : Nat)
-  let y : id Nat := by clean by dsimp; exact 1
-  guard_hyp y :ₛ id Nat := (1 : Nat)
+  let x' : id Nat := clean% by dsimp; exact 1
+  guard_hyp x' :ₛ id Nat := (1 : Nat)
+
+  let y := show Nat from 1
+  guard_hyp y :ₛ Nat := let_fun this := 1; this
+  let y' := clean% show Nat from 1
+  guard_hyp y' :ₛ Nat := 1
+
+  -- Not a tautological let_fun:
+  let z := clean% let_fun x := 1; x + x
+  guard_hyp z :ₛ Nat := let_fun x := 1; x + x
+
   trivial
