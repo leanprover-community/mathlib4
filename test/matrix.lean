80b0f6db3601453670bee7d98f799d91e2848f29
adomani <adomani@gmail.com>
2023-08-27 06:58:31 +0200

    Sort imports

--- test/matrix.lean
+++ test/matrix.lean
@@ -4,9 +4,9 @@ https://github.com/leanprover-community/mathlib/blob/4f4a1c875d0baa92ab5d92f3fb1
 -/
 import Mathlib.Data.Matrix.Notation
 import Mathlib.GroupTheory.Perm.Fin
+import Mathlib.LinearAlgebra.Matrix.Determinant
 import Qq
 import Std.Tactic.GuardExpr
-import Mathlib.LinearAlgebra.Matrix.Determinant
 
 open Qq
 
