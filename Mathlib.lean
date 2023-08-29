46f6f34063bce6359ec55fc1eb5702b9092857c1
Scott Morrison <scott@tqft.net>
2023-08-29 12:49:38 +0000

    feat: Estimator typeclass and implementation for Levenshtein distance (#6119)
    Co-authored-by: Scott Morrison <scott.morrison@gmail.com>
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>
    Co-authored-by: Thomas Browning <tb65536@uw.edu>
    Co-authored-by: Chris Hughes <chrishughes24@gmail.com>
    Co-authored-by: Oliver Nash <github@olivernash.org>
    Co-authored-by: sgouezel <sebastien.gouezel@univ-rennes1.fr>
    Co-authored-by: Alex J Best <alex.j.best@gmail.com>
    Co-authored-by: Xavier Roblot <46200072+xroblot@users.noreply.github.com>
    Co-authored-by: michaellee94 <michael_lee1@brown.edu>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1500,6 +1500,7 @@ import Mathlib.Data.List.Destutter
 import Mathlib.Data.List.Duplicate
 import Mathlib.Data.List.EditDistance.Bounds
 import Mathlib.Data.List.EditDistance.Defs
+import Mathlib.Data.List.EditDistance.Estimator
 import Mathlib.Data.List.FinRange
 import Mathlib.Data.List.Forall2
 import Mathlib.Data.List.Func
@@ -2604,6 +2605,7 @@ import Mathlib.Order.Cover
 import Mathlib.Order.Directed
 import Mathlib.Order.Disjoint
 import Mathlib.Order.Disjointed
+import Mathlib.Order.Estimator
 import Mathlib.Order.Extension.Linear
 import Mathlib.Order.Extension.Well
 import Mathlib.Order.Filter.Archimedean
