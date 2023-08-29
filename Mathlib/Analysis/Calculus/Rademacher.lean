545534cd18db57df16e5d56c5e88f3633499acad
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2023-08-29 13:53:48 +0200

    fix last file

--- Mathlib/Analysis/Calculus/Rademacher.lean
+++ Mathlib/Analysis/Calculus/Rademacher.lean
@@ -1,17 +1,11 @@
 /-
-Copyright (c) 2018 Mario Carneiro. All rights reserved.
+Copyright (c) 2023 Sébastien Gouëzel. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Mario Carneiro, Johannes Hölzl, Sander Dahmen, Scott Morrison
+Authors: Sébastien Gouëzel
 -/
-import Mathlib.Algebra.Module.BigOperators
-import Mathlib.LinearAlgebra.Basis.VectorSpace
-import Mathlib.LinearAlgebra.DFinsupp
-import Mathlib.LinearAlgebra.FreeModule.Basic
-import Mathlib.LinearAlgebra.InvariantBasisNumber
-import Mathlib.LinearAlgebra.Isomorphisms
-import Mathlib.LinearAlgebra.StdBasis
-import Mathlib.SetTheory.Cardinal.Cofinality
-import Mathlib.Analysis.Convolution
+import Mathlib.Analysis.Calculus.LineDeriv.Basic
+import Mathlib.Analysis.Calculus.FDeriv.Measurable
+import Mathlib.Analysis.NormedSpace.FiniteDimension
 
 /-!
 # Rademacher theorem: a Lipschitz function is differentiable almost everywhere
@@ -23,6 +17,7 @@ variable {E : Type _} [NormedAddCommGroup E] [NormedSpace ℝ E] [FiniteDimensio
 
 open Filter
 
+/-
 noncomputable def directionalDeriv (f : E → ℝ) (v : E) (x : E) : ℝ :=
   limsup (fun (n : ℕ) ↦ n • (f (x + ((1 : ℝ)/n) • v) - f x)) (atTop : Filter ℕ)
 
@@ -34,3 +29,4 @@ lemma measurable_directionalDeriv {f : E → ℝ} (hf : Continuous f) (v : E) :
 #exit
 
 lemma zoug : trivial := sorry
+-/
