9c1db788fed1a8d7a3607d25f4344a87898e40cf
Mohanad Ahmed <m.a.m.elhassan@gmail.com>
2023-08-31 00:52:08 +0300

    some changes

--- Mathlib/LinearAlgebra/Matrix/DFT.lean
+++ Mathlib/LinearAlgebra/Matrix/DFT.lean
@@ -16,21 +16,21 @@ import Mathlib.LinearAlgebra.Vandermonde
 /-!
 # Discrete Fourier Transform (DFT) Matrix and DFT of a (finite) sequence
 
-This file defines the `dft` opertaion on a sequence (also a vector) and the DFT operation matrix
+This file defines the `dft` operation on a sequence (also a vector) and the DFT operation matrix
 
 ## Main definitions
 
  - `dft v`: given a sequence (v : (Fin n) → ℂ) we can transform it into a sequence (V : (Fin n) →ℂ)
  such that
- $$ V [p] = ∑_{k = 0}^{N - 1} e^{-i*2πkp/n} v [k] $$
- - `idft V` : given a sequence (V : (Fin n) → ℂ) we can transform it into a sequence
- (v : (Fin n) → ℂ)
+ $$ V [p] = ∑_{k = 0}^{N - 1} e^{-i 2 π k p / n} v (k) $$
+ - `idft V` : given a sequence (V : Fin n → ℂ) we can transform it into a sequence
+ (v : Fin n → ℂ)
 such that
-$$ v [k] = ¼{1}{N}∑_{p = 0}^{n - 1} e^{j*2πkp/N} v [p] $$
+$$ v [k] = \frac{1}{N}∑_{p = 0}^{n - 1} e^{i 2 π k p / N} v [p] $$
 - `Wₙ` : the dft matrix with the `k, p` entry equal to
-$$ Wₙ[k, p] = e^{-j2πkp/n} $$
+$$ Wₙ[k, p] = e^{-i 2 π k p/n} $$
 - `Wₙ⁻¹` : the idft matrix with the `k, p` entry equal to
-$$ Wₙ[k, p] = (1/N) * e^{j2πkp/n} $$
+$$ Wₙ[k, p] = (1/N) * e^{i 2 π k p / n} $$
 
 
 ## Main results
@@ -52,33 +52,36 @@ namespace DFT
 
 open Complex Matrix BigOperators Finset Real
 
-variable (n : ℕ) [NeZero n]
+variable (n : ℕ)
 
 /-- The DFT operation defined via a Sum -/
-noncomputable def dft (v : (Fin n) → ℂ) : Fin n → ℂ :=
-fun k : Fin n =>  ∑ p : (Fin n),  (Complex.exp (2 * π * I * k * p / n)) * (v p)
+noncomputable def dft (v : Fin n → ℂ) : Fin n → ℂ :=
+fun k : Fin n =>  ∑ p : Fin n,  (Complex.exp (2 * π * I * k * p / n)) * (v p)
 
 /-- The IDFT operation defined via a Sum -/
-noncomputable def idft  (V : (Fin n) → ℂ) : (Fin n) → ℂ :=
-fun p : Fin n =>  ∑ k : (Fin n),  ((Complex.exp (-2 * π * I * p * k / n))/ n) * (V k)
+noncomputable def idft  (V : Fin n → ℂ) : (Fin n) → ℂ :=
+fun p : Fin n =>  ∑ k : Fin n,  ((Complex.exp (-2 * π * I * p * k / n))/ n) * (V k)
 
 /-- The DFT Matrix -/
 noncomputable def Wₙ  : Matrix (Fin n) (Fin n) ℂ :=
 Matrix.of (fun (k p : Fin n) => Complex.exp (2 * π * I * k * p / n))
 
 -- @[simp]
-lemma Wₙ_apply (k p: Fin n) : Wₙ n k p = exp (2 * π * I * k * p / n) := rfl
+lemma Wₙ_apply (k p : Fin n) : Wₙ n k p = exp (2 * π * I * k * p / n) := rfl
 
 lemma mod_eq_mod_neg (m a : ℤ) : Int.mod (-a) m = -Int.mod (a) m := by
   rw [Int.mod_def, Int.mod_def, Int.neg_div, neg_sub', mul_neg, sub_neg_eq_add]
 
-lemma cexp_sub_ne_one {m : ℕ} [NeZero m](k p : Fin m) (h : ¬(k = p)) :
+lemma cexp_sub_ne_one {m : ℕ} (k p : Fin m) (h : (k ≠ p)) :
     Complex.exp (2 * π * I * (k - p) / m) ≠ 1 := by
+  by_cases hm : m = 0
+  exfalso
+  apply Fin.elim0 (by convert k; exact hm.symm)
   by_contra hg
   rw [Complex.exp_eq_one_iff] at hg
   obtain ⟨z, hz⟩ := hg
   rw [mul_div_assoc, mul_comm (z:ℂ) _, (mul_right_inj' two_pi_I_ne_zero),
-      (div_eq_iff_mul_eq (Nat.cast_ne_zero.2 (NeZero.ne _)))] at hz
+      (div_eq_iff_mul_eq (Nat.cast_ne_zero.2 hm))] at hz
   norm_cast at hz
   apply_fun ( Int.mod · m) at hz
   simp only [Int.mul_mod_left, Int.subNatNat_eq_coe] at hz
@@ -114,13 +117,14 @@ lemma cexp_sub_ne_one {m : ℕ} [NeZero m](k p : Fin m) (h : ¬(k = p)) :
 noncomputable def invWₙ : Invertible (Wₙ n) := by
   apply invertibleOfRightInverse  _ (of (fun (k p : Fin n) => exp (-2 * π * I * k * p / n) / n)) _
   funext k p
-  -- rw [mul_apply]
+  by_cases hn: n = 0
+  apply False.elim $ Fin.elim0 (by convert k; exact hn.symm)
   simp_rw [mul_apply, Wₙ_apply, of_apply, mul_div_assoc', ← Complex.exp_add]
   by_cases h : k = p
   · rw [h, one_apply_eq]
     ring_nf
     simp_rw [Complex.exp_zero, Finset.sum_const, card_fin, nsmul_eq_mul]
-    apply mul_inv_cancel_left₀ (Nat.cast_ne_zero.2 (NeZero.ne _))
+    apply mul_inv_cancel_left₀ (Nat.cast_ne_zero.2 hn)
   · rw [one_apply_ne h]
     have h1 : ∀ (x : Fin n), 2 * π * I * k * x / n +  (-2) * π * I * x * p / n  =
         (x : ℕ) * ((2*π * I * (k - p)) / n) := by intro x; ring
@@ -133,7 +137,7 @@ noncomputable def invWₙ : Invertible (Wₙ n) := by
     rw [mul_div_cancel', mul_comm, mul_eq_mul_right_iff]
     left
     simp_rw [Int.cast_sub, Int.cast_ofNat]
-    exact (Nat.cast_ne_zero.2 (NeZero.ne _))
+    exact (Nat.cast_ne_zero.2 hn)
     apply cexp_sub_ne_one _ _ h
 
 /-- The IDFT Matrix -/
@@ -142,7 +146,7 @@ theorem dftMatrix_inv  [Invertible (Wₙ n)] :
   letI := (invWₙ n)
   convert (rfl : ⅟(Wₙ n) = _)
 
-lemma iWₙ_apply (k p : Fin n) : (Wₙ n)⁻¹ k p = exp (-2 * π * I * k * p / n)/n := by
+lemma iWₙ_apply (k p : Fin n) : (Wₙ n)⁻¹ k p = exp (-2 * π * I * k * p / n) / n := by
   letI := invWₙ n
   rw [← Matrix.invOf_eq_nonsing_inv (Wₙ n), dftMatrix_inv, of_apply]
 
@@ -166,14 +170,16 @@ theorem dft_idft  (V : Fin n → ℂ) : dft  n (idft n V) = V := by
   rw [dft_eq_Wₙ_mul, idft_eq_iWₙ_mul, mulVec_mulVec, mul_inv_of_invertible, one_mulVec]
 
 /-- The IDFT matrix is the scaled conjugate transpose of the DFT matrix-/
-theorem Wₙ_conjTranspose_eq_iWₙ :  (Wₙ n)⁻¹ = ((1:ℂ)/n) • (Wₙ n)ᴴ := by
+theorem Wₙ_conjTranspose_eq_iWₙ :  (Wₙ n)⁻¹ = ((1:ℂ) / n) • (Wₙ n)ᴴ := by
   funext x y
+  by_cases hn: n = 0
+  apply False.elim $ Fin.elim0 (by convert x; exact hn.symm)
   simp only [iWₙ_apply, smul_apply, conjTranspose_apply, Wₙ_apply, smul_eq_mul]
   rw [star_def, ← Complex.exp_conj, ← div_mul_comm, mul_one, div_left_inj', ← star_def]
   simp only [star_div', star_natCast, star_mul', conj_I, star_def, conj_ofReal, map_ofNat, neg_mul,
     mul_neg]
   ring_nf
-  exact (Nat.cast_ne_zero.2 (NeZero.ne _))
+  exact (Nat.cast_ne_zero.2 hn)
 
 /-- The DFT matrix is symmetric -/
 lemma Wₙ_transpose_eq_Wₙ : (Wₙ n)ᵀ = Wₙ n := by
@@ -183,13 +189,13 @@ lemma Wₙ_transpose_eq_Wₙ : (Wₙ n)ᵀ = Wₙ n := by
 
 def shiftk (N : ℕ) (k : Fin N) : (Fin N → Fin N) := fun n : (Fin N) => (n - k)
 
-def shiftk_equiv {N: ℕ} [hN : NeZero N] (k : Fin N) : (Fin N) ≃ (Fin N) where
+def shiftk_equiv {N: ℕ} [NeZero N] (k : Fin N) : (Fin N) ≃ (Fin N) where
   toFun := shiftk N k
   invFun := shiftk N (-k)
   left_inv := by intro x;  simp only [shiftk, sub_neg_eq_add, sub_add_cancel]
   right_inv := by intro x; simp only [shiftk, sub_neg_eq_add, add_sub_cancel]
 
-lemma cexp_shiftk_invariant (x a b : Fin n) :
+lemma cexp_shiftk_invariant [NeZero n] (x a b : Fin n) :
     exp (2 * ↑π * I * (↑↑a * ((x:ℂ) - (b:ℂ))) / ↑n) =
     Complex.exp ((2*π*I)*(a*(shiftk_equiv (b) x))/n) := by
   rw [Complex.exp_eq_exp_iff_exists_int]
@@ -236,21 +242,23 @@ theorem circulant_dft  (t : Fin n → ℂ) :
   dsimp
   rw [Matrix.mul_assoc, Matrix.mul_inv_cancel_left_of_invertible]
   funext a b
+  by_cases hn : n = 0
+  apply False.elim $ Fin.elim0 (by convert a; exact hn.symm)
+  letI := neZero_iff.2 hn
   simp only [diagonal_mul, mul_apply, circulant_apply, Wₙ_apply, diagonal_apply, dft]
-  -- by_cases h : a = b
   simp_rw [ite_mul, zero_mul, sum_ite_eq, mem_univ, ite_true,
     ← mul_inv_eq_iff_eq_mul₀ (Complex.exp_ne_zero _), ← Complex.exp_neg]
   rw [mul_comm]
   simp_rw [mul_sum, ← mul_assoc, ← Complex.exp_add, neg_add_eq_sub, ← sub_div, mul_assoc (2*π*I),
     ← mul_sub]
   let f := fun x : (Fin n) => (Complex.exp ( (2* π * I) * (a * (x))/n)) * (t (x))
-  have h1 : ∀ (x : Fin n), (shiftk_equiv (b)) x = x - b := by
+  have h1 : ∀ (x : Fin n), (shiftk_equiv b) x = x - b := by
     intro x; unfold shiftk_equiv shiftk; simp only [sub_neg_eq_add, Equiv.coe_fn_mk]
   have h2 : ∀ (x : Fin n), Complex.exp (2 * ↑π * I * (↑↑a * ((x:ℂ) - (b:ℂ))) / ↑n) =
-    Complex.exp ((2 * π * I) * (a * (shiftk_equiv b x) / n) := by
+    Complex.exp ((2 * π * I) * (a * (shiftk_equiv b x) ) / n) := by
     intro x; apply cexp_shiftk_invariant n x a b
   simp_rw [← h1, h2]
-  rw [Equiv.sum_comp (shiftk_equiv (b)) f]
+  rw [Equiv.sum_comp (shiftk_equiv b) f]
   apply Matrix.mul_right_injective_of_invertible (Wₙ n)
 
 /-- The DFT matrix is a vandermonde matrix -/
