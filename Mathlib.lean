7f33b5f167d85d40bf525daf676ca3ba227308a9
Yaël Dillies <yael.dillies@gmail.com>
2023-08-30 16:45:12 +0000

    feat: Zeckendorf's theorem Prove Zeckendorf's theorem: Every natural number can be written uniquely as a sum of distinct non-consecutive Fibonacci numbers.
    Instead of proving an `ExistsUnique` statement, I define an equivalence between natural numbers and Zeckendorf representations.

--- Mathlib.lean
+++ Mathlib.lean
@@ -1616,7 +1616,7 @@ import Mathlib.Data.Nat.Factorial.DoubleFactorial
 import Mathlib.Data.Nat.Factorization.Basic
 import Mathlib.Data.Nat.Factorization.PrimePow
 import Mathlib.Data.Nat.Factors
-import Mathlib.Data.Nat.Fib
+import Mathlib.Data.Nat.Fib.Basic
 import Mathlib.Data.Nat.ForSqrt
 import Mathlib.Data.Nat.GCD.Basic
 import Mathlib.Data.Nat.GCD.BigOperators
