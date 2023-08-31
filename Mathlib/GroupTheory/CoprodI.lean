9b0aea0901681ecd622b1d7181719261d106d9e3
Chris Hughes <chrishughes24@gmail.com>
2023-08-31 15:51:13 +0100

    undo one change

--- Mathlib/GroupTheory/CoprodI.lean
+++ Mathlib/GroupTheory/CoprodI.lean
@@ -234,8 +234,7 @@ theorem inv_def (x : CoprodI G) :
 #align free_product.inv_def Monoid.CoprodI.inv_def
 
 instance : Group (CoprodI G) :=
-  { toInv := inferInstanceAs (Inv (CoprodI G)),
-    toMonoid := inferInstanceAs (Monoid (CoprodI G)),
+  { inferInstanceAs (Inv (CoprodI G)), inferInstanceAs (Monoid (CoprodI G)) with
     mul_left_inv := by
       intro m
       rw [inv_def]
