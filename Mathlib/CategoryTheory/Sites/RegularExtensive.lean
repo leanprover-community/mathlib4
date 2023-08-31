3d6198ef2b6a86e715f0d69a68204d4381fcd110
dagurtomas <dagurtomas@gmail.com>
2023-08-31 12:27:20 +0000

    explicit universes

--- Mathlib/CategoryTheory/Sites/RegularExtensive.lean
+++ Mathlib/CategoryTheory/Sites/RegularExtensive.lean
@@ -29,11 +29,13 @@ vice versa.
 
 -/
 
+universe v u w
+
 namespace CategoryTheory
 
 open Limits
 
-variable (C : Type*) [Category C]
+variable (C : Type u) [Category.{v} C]
 
 /--
 The condition `Regular C` is property that effective epis can be "pulled back" along any
@@ -64,11 +66,11 @@ of the morphisms is an inclusion map into the coproduct (up to isomorphism).
 class HasPullbacksOfInclusions : Prop where
     /-- For any morphism `f : X ⟶ Z`, where `Z` is the coproduct of `i : (a : α) → Y a ⟶ Z` with
     `α` finite, the pullback of `f` and `i a` exists for every `a : α`. -/
-    has_pullback : ∀ {X Z : C} {α : Type _} (f : X ⟶ Z) {Y : (a : α) → C}
+    has_pullback : ∀ {X Z : C} {α : Type w} (f : X ⟶ Z) {Y : (a : α) → C}
     (i : (a : α) → Y a ⟶ Z) [Fintype α] [HasCoproduct Y] [IsIso (Sigma.desc i)] (a : α),
     HasPullback f (i a)
 
-instance [HasPullbacksOfInclusions C] {X Z : C} {α : Type _} (f : X ⟶ Z) {Y : (a : α) → C}
+instance [HasPullbacksOfInclusions C] {X Z : C} {α : Type w} (f : X ⟶ Z) {Y : (a : α) → C}
     (i : (a : α) → Y a ⟶ Z) [Fintype α] [HasCoproduct Y] [IsIso (Sigma.desc i)] (a : α) :
     HasPullback f (i a) := HasPullbacksOfInclusions.has_pullback f i a
 
