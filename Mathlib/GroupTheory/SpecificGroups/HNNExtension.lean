dec4a1e1514ca02684a12b11d8e3bf20a4437c39
Chris Hughes <chrishughes24@gmail.com>
2023-09-01 11:59:39 +0100

    a bit of progress

--- Mathlib/GroupTheory/SpecificGroups/HNNExtension.lean
+++ Mathlib/GroupTheory/SpecificGroups/HNNExtension.lean
@@ -5,26 +5,13 @@ Authors: Chris Hughes
 -/
 
 import Mathlib.GroupTheory.SpecificGroups.Coprod
+import Mathlib.GroupTheory.CoprodI
 import Mathlib.GroupTheory.FreeGroup
+import Mathlib.GroupTheory.Complement
 
-open Monoid Coprod Multiplicative
+open Monoid Coprod Multiplicative Subgroup
 
-notation "C∞ " => Multiplicative ℤ
-
--- @[reducible]
--- def HNNExtension.TypeFamilyAux (G : Type u) [Group G] :
---     Bool → Σ G : Type u, Group G
---   | true => ⟨FreeGroup PUnit, inferInstance⟩
---   | false => ⟨G, inferInstance⟩
-
--- variable {G : Type u} [Group G]
-
--- @[reducible]
--- def HNNExtension.TypeFamily (G : Type u) [Group G] (b : Bool) : Type u :=
---   (HNNExtension.TypeFamilyAux G b).1
-
--- instance (b : Bool) : Group (HNNExtension.TypeFamily G b) :=
---   (HNNExtension.TypeFamilyAux G b).2
+local notation "C∞ " => Multiplicative ℤ
 
 def HNNExtension.con (G : Type*) [Group G] (A B : Subgroup G) (φ : A ≃* B) : Con (G ∗ C∞) :=
   conGen (fun x y => ∃ (a : A),
@@ -47,6 +34,30 @@ def of : G →* HNNExtension G A B φ :=
 def t : HNNExtension G A B φ :=
   (HNNExtension.con G A B φ).mk'.comp inr (ofAdd 1)
 
+theorem t_mul_of (a : A) :
+    t * (of (a : G) : HNNExtension G A B φ) = of (φ a : G) * t :=
+  (Con.eq _).2 <| ConGen.Rel.of _ _ <| ⟨a, by simp⟩
+
+theorem of_mul_t (b : B) :
+    (of (b : G) : HNNExtension G A B φ) * t = t * of (φ.symm b : G) := by
+  rw [t_mul_of]; simp
+
+theorem equiv_eq_conj (a : A) :
+    (of (φ a : G) : HNNExtension G A B φ) = t * of (a : G) * t⁻¹ := by
+  rw [t_mul_of]; simp
+
+theorem equiv_symm_eq_conj (b : B) :
+    (of (φ.symm b : G) : HNNExtension G A B φ) = t⁻¹ * of (b : G) * t := by
+  rw [mul_assoc, of_mul_t]; simp
+
+theorem inv_t_mul_of (b : B) :
+    t⁻¹ * (of (b : G) : HNNExtension G A B φ) = of (φ.symm b : G) * t⁻¹ := by
+  rw [equiv_symm_eq_conj]; simp
+
+theorem of_mul_inv_t (a : A) :
+    (of (a : G) : HNNExtension G A B φ) * t⁻¹ = t⁻¹ * of (φ a : G) := by
+  rw [equiv_eq_conj]; simp [mul_assoc]
+
 def lift (f : G →* H) (x : H) (hx : ∀ a : A, x * f ↑a = f (φ a : G) * x) :
     HNNExtension G A B φ →* H :=
   Con.lift _ (Coprod.lift f (zpowersHom H x)) (Con.conGen_le <| by
@@ -68,5 +79,81 @@ theorem hom_ext {f g : HNNExtension G A B φ →* H}
     (hg : f.comp of  = g.comp of) (ht : f t = g t) : f = g :=
   (MonoidHom.cancel_right Con.mk'_surjective).mp <|
     Coprod.ext_hom _ _ hg (MonoidHom.ext_mint ht)
+
+namespace NormalWord
+
+@[reducible]
+def BoolFamily (G : Type*) [Group G] : Bool → Type _
+  | true => ULift C∞
+  | false => G
+
+instance (b : Bool) : Group (BoolFamily G b) := by
+  cases b <;> dsimp [BoolFamily] <;> infer_instance
+
+variable (G)
+
+abbrev SumInt : Type _ := Sigma (BoolFamily G)
+
+variable {G}
+
+def SumInt.ofInt (n : ℤ) : SumInt G := ⟨true, ULift.up (ofAdd n)⟩
+
+def SumInt.ofGroup (g : G) : SumInt G := ⟨false, g⟩
+
+@[elab_as_elim]
+def SumInt.casesOn {motive : SumInt G → Sort _} (n : SumInt G)
+    (h₁ : ∀ n : ℤ, motive (SumInt.ofInt n))
+    (h₂ : ∀ g : G, motive (SumInt.ofGroup g)) : motive n :=
+  match n with
+  | ⟨true, ULift.up n⟩ => h₁ n
+  | ⟨false, g⟩ => h₂ g
+
+@[simp]
+theorem SumInt.casesOn_ofInt {motive : SumInt G → Sort _} (n : ℤ)
+    (h₁ : ∀ n : ℤ, motive (SumInt.ofInt n))
+    (h₂ : ∀ g : G, motive (SumInt.ofGroup g)) :
+    SumInt.casesOn (SumInt.ofInt n) h₁ h₂ = h₁ n :=
+  rfl
+
+@[simp]
+theorem SumInt.casesOn_ofGroup {motive : SumInt G → Sort _} (g : G)
+    (h₁ : ∀ n : ℤ, motive (SumInt.ofInt n))
+    (h₂ : ∀ g : G, motive (SumInt.ofGroup g)) :
+    SumInt.casesOn (SumInt.ofGroup g) h₁ h₂ = h₂ g :=
+  rfl
+
+variable (G A B φ)
+
+structure TransversalPair : Type _ :=
+  /-- The transversal of the first subgroup -/
+  ( setA : Set G )
+  /-- The chosen element of the subgroup itself is the identity -/
+  ( one_mem_A : 1 ∈ setA )
+  /-- We have exactly one element of each coset of the subgroup -/
+  ( compl_A : IsComplement A setA )
+  /-- The transversal of the second subgroup -/
+  ( setB : Set G )
+  /-- The chosen element of the subgroup itself is the identity -/
+  ( one_mem_B : 1 ∈ setB )
+  /-- We have exactly one element of each coset of the subgroup -/
+  ( compl_B : IsComplement B setB )
+
+instance transversalPair_nonempty  : Nonempty (TransversalPair G A B) := by
+  rcases exists_right_transversal (H := A) 1 with ⟨setA, compl_A, one_mem_A⟩
+  rcases exists_right_transversal (H := B) 1 with ⟨setB, compl_B, one_mem_B⟩
+  exact
+    ⟨{ setA := setA, one_mem_A := one_mem_A, compl_A := compl_A,
+       setB := setB, one_mem_B := one_mem_B, compl_B := compl_B }⟩
+
+variable {G A B}
+
+structure NormalWord (d : TransversalPair G A B) extends Monoid.CoprodI.Word (BoolFamily G) : Type _ :=
+  ( left : ℤ )
+  ( chain : toList.Chain' (fun a b : SumInt G => ∀ (t : ℤ) (g : G),
+      a = SumInt.ofGroup g → b = SumInt.ofInt t →
+      (0 < t → g ∈ d.setB) ∧ (t < 0 → g ∈ d.setA) ) )
+
+
+end NormalWord
 
 end HNNExtension
