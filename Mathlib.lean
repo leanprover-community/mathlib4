bc95c04e6a7f45173c8e15d0f7c366f34076bb60
Antoine Chambert-Loir <antoine.chambert-loir@math.univ-paris-diderot.fr>
2023-09-01 19:18:14 +0200

    add line in Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -2256,6 +2256,7 @@ import Mathlib.LinearAlgebra.TensorAlgebra.ToTensorPower
 import Mathlib.LinearAlgebra.TensorPower
 import Mathlib.LinearAlgebra.TensorProduct
 import Mathlib.LinearAlgebra.TensorProduct.Matrix
+import Mathlib.LinearAlgebra.TensorProduct.RightExactness
 import Mathlib.LinearAlgebra.TensorProduct.Tower
 import Mathlib.LinearAlgebra.TensorProductBasis
 import Mathlib.LinearAlgebra.Trace
