2188a8aec0467c66de468a44f584551c20c9ae32
Joël Riou <joel.riou@universite-paris-saclay.fr>
2023-09-01 22:33:57 +0200

    mostly done the colimits in the category of presheaves of modules

--- Mathlib/Algebra/Category/ModuleCat/Presheaf/Limits.lean
+++ Mathlib/Algebra/Category/ModuleCat/Presheaf/Limits.lean
@@ -1,5 +1,6 @@
 import Mathlib.Algebra.Category.ModuleCat.Presheaf
 import Mathlib.Algebra.Category.ModuleCat.Limits
+import Mathlib.Algebra.Category.ModuleCat.Colimits
 import Mathlib.CategoryTheory.Limits.Preserves.Limits
 
 universe v u₂ v₁ v₂ v₃ u₁ u₃ u
@@ -91,15 +92,28 @@ noncomputable def restrictScalarsPreservesLimitsOfShape (J : Type u₂) [SmallCa
     refine' @isLimitOfReflects _ _ _ _ _ _ _ (forget₂ (ModuleCat.{max u₂ v} R) AddCommGroupCat) ((restrictScalars f).mapCone c) _ _
     exact isLimitOfPreserves (forget₂ (ModuleCat.{max u₂ v} S) AddCommGroupCat) hc⟩
 
+noncomputable instance forget₂PreservesColimitsOfShape (J : Type u) [SmallCategory J] (R : Type u) [Ring R] :
+    PreservesColimitsOfShape J (forget₂ (ModuleCat.{u} R) AddCommGroupCat) := by
+  sorry
+
+noncomputable instance forget₂ReflectsColimitsOfShape (J : Type u) [SmallCategory J] (R : Type u) [Ring R] :
+    ReflectsColimitsOfShape J (forget₂ (ModuleCat.{u} R) AddCommGroupCat) :=
+  reflectsColimitsOfShapeOfReflectsIsomorphisms
+
+noncomputable instance restrictScalarsPreservesColimitsOfShape (J : Type u) [SmallCategory J] {R S : Type u}
+    [Ring R] [Ring S] (f : R →+* S) :
+    PreservesColimitsOfShape J (ModuleCat.restrictScalars.{u} f) where
+  preservesColimit {K} := ⟨fun {c} hc => by
+    refine' @isColimitOfReflects _ _ _ _ _ _ _ (forget₂ (ModuleCat.{u} R) AddCommGroupCat) ((restrictScalars f).mapCocone c) _ _
+    exact isColimitOfPreserves (forget₂ (ModuleCat S) AddCommGroupCat) hc⟩
+
 end ModuleCat
 
 namespace PresheafOfModules
 
 open CategoryTheory Category Limits
 
-noncomputable example (R : Type u) [Ring R] :
-  PreservesLimits (forget₂ (ModuleCat.{v} R) AddCommGroupCat.{v}) :=
-    @ModuleCat.forget₂AddCommGroupPreservesLimits R _
+section
 
 variable {C : Type u₁} [Category.{v₁} C] {R : Cᵒᵖ ⥤ RingCat.{u}}
   {J : Type u₂} [SmallCategory J] (F : J ⥤ PresheafOfModules.{max u₂ v} R)
@@ -213,5 +227,107 @@ instance (J : Type v) [SmallCategory J] : HasLimitsOfShape J (PresheafOfModules.
 noncomputable instance (J : Type v) [SmallCategory J] (X : Cᵒᵖ) :
     PreservesLimitsOfShape J (evaluation.{v} R X) :=
   evaluationPreservesLimitsOfShape.{v, v} _ _ _
+
+end
+
+section
+
+-- `J : Type u` for now
+variable {C : Type u₁} [Category.{v₁} C] {R : Cᵒᵖ ⥤ RingCat.{u}}
+  {J : Type u} [SmallCategory J] (F : J ⥤ PresheafOfModules.{u} R)
+
+def evaluationJointlyReflectsColimits (c : Cocone F)
+    (hc : ∀ (X : Cᵒᵖ), IsColimit ((evaluation R X).mapCocone c)) : IsColimit c := by
+  exact
+  { desc := fun s => Hom.mk'' (fun X => (hc X).desc ((evaluation R X).mapCocone s)) (fun X Y f => by
+      apply (hc X).hom_ext
+      intro j
+      erw [(hc X).fac_assoc, (restriction R f).naturality_assoc]
+      dsimp
+      rw [← Functor.map_comp]
+      erw [(ModuleCat.restrictScalars (R.map f)).congr_map ((hc Y).fac ((evaluation R Y).mapCocone s) j),
+        (restriction R f).naturality]
+      rfl )
+    fac := fun s j => by
+      ext1 X
+      exact (hc X).fac ((evaluation R X).mapCocone s) j
+    uniq := fun s m hm => by
+      ext1 X
+      exact (hc X).uniq ((evaluation R X).mapCocone s) ((evaluation R X).map m) (fun j => by
+        dsimp
+        rw [← hm]
+        rfl) }
+
+@[simps]
+noncomputable def colimitBundledMkStruct : BundledMkStruct.{u} R := by
+  exact
+  { obj := fun X => colimit (F ⋙ evaluation R X)
+    map := fun {X Y} f => colimMap (whiskerLeft F (restriction R f)) ≫
+        (preservesColimitIso (ModuleCat.restrictScalars (R.map f)) (F ⋙ evaluation R Y)).inv
+    map_id := fun X => by
+      apply colimit.hom_ext
+      intro j
+      dsimp
+      simp only [Functor.comp_obj, evaluation_obj, ι_colimMap_assoc, whiskerLeft_app,
+        restriction_app_id]
+      erw [ι_preservesColimitsIso_inv (ModuleCat.restrictScalars (R.map (𝟙 X))) (F ⋙ evaluation R X),
+        ← NatTrans.naturality]
+      rfl
+    map_comp := fun {X Y Z} f g => by
+      apply colimit.hom_ext
+      intro j
+      dsimp
+      simp only [ι_colimMap_assoc, Functor.comp_obj, evaluation_obj, whiskerLeft_app,
+        Functor.map_comp, assoc, restriction_app_comp]
+      erw [ι_preservesColimitsIso_inv_assoc (ModuleCat.restrictScalars (R.map f)) (F ⋙ evaluation R Y) j]
+      simp only [← Functor.map_comp_assoc, ι_colimMap_assoc]
+      congr 1
+      erw [ι_preservesColimitsIso_inv (ModuleCat.restrictScalars (R.map g)) (F ⋙ evaluation R Z) j]
+      erw [ι_preservesColimitsIso_inv (ModuleCat.restrictScalars (R.map (f ≫ g))) (F ⋙ evaluation R Z) j]
+      simp only [Functor.comp_obj, evaluation_obj, Functor.map_comp, whiskerLeft_app, assoc]
+      erw [← NatTrans.naturality]
+      rfl }
+
+noncomputable def colimitCocone : Cocone F := by
+  exact
+  { pt := mk'' (colimitBundledMkStruct F)
+    ι :=
+      { app := fun j => Hom.mk'' (fun X => colimit.ι (F ⋙ evaluation R X) j) (fun X Y f => by
+          dsimp
+          erw [ι_colimMap_assoc]
+          simp
+          congr 1
+          dsimp
+          erw [ι_preservesColimitsIso_inv (ModuleCat.restrictScalars (R.map f))])
+        naturality := fun i j φ => by
+          dsimp
+          rw [comp_id]
+          ext1 X
+          simp only [mk''_obj, colimitBundledMkStruct_obj, Hom.comp_app, Hom.mk'_app]
+          exact (colimit.w (F ⋙ evaluation R X) φ) } }
+
+noncomputable def isColimitColimitCocone : IsColimit (colimitCocone F) :=
+  evaluationJointlyReflectsColimits _ _ (fun _ => colimit.isColimit _)
+
+variable (R J)
+
+instance : HasColimitsOfShape J (PresheafOfModules.{u} R) where
+  has_colimit F := ⟨_, isColimitColimitCocone F⟩
+
+instance : HasColimitsOfSize.{u, u} (PresheafOfModules.{u} R) where
+
+noncomputable instance (X : Cᵒᵖ) : PreservesColimitsOfShape J (evaluation R X) where
+  preservesColimit := fun {K} => by
+    exact preservesColimitOfPreservesColimitCocone (isColimitColimitCocone K) (colimit.isColimit _)
+
+noncomputable instance (X : Cᵒᵖ) : PreservesColimitsOfSize.{u, u} (evaluation R X) where
+
+instance : HasFiniteColimits (PresheafOfModules.{u} R) :=
+  hasFiniteColimits_of_hasColimitsOfSize.{u, u} (PresheafOfModules.{u} R)
+
+noncomputable instance (X : Cᵒᵖ) : PreservesFiniteColimits (evaluation.{u} R X) :=
+  PreservesColimitsOfSize.preservesFiniteColimits.{u, u} _
+
+end
 
 end PresheafOfModules
