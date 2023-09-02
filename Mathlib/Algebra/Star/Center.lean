cf1aa9d12c3930ccd358c6c988c0993b4e02e077
Christopher Hoskin <christopher.hoskin@gmail.com>
2023-09-02 08:25:21 +0100

    centre star

--- Mathlib/Algebra/Star/Center.lean
+++ Mathlib/Algebra/Star/Center.lean
@@ -15,13 +15,24 @@ variable {R : Type*} [Mul R] [StarMul R] {a : R} {s : Set R}
 theorem Set.star_mem_center (ha : a ∈ Set.center R) : star a ∈ Set.center R where
   comm := by simpa only [star_mul, star_star] using fun g =>
     congr_arg star (((Set.mem_center_iff R).mp ha).comm <| star g).symm
-  left_assoc := by
-    intros b c
-    calc
-      star a * (b * c) = star a * (b * c)
-
-  mid_assoc := sorry
-  right_assoc := sorry
+  left_assoc := fun b c => calc
+    star a * (b * c) = star a * (star (star b) * star (star c)) := by rw [star_star, star_star]
+    _ = star a * star (star c * star b) := by rw [star_mul]
+    _ = star ((star c * star b) * a) := by rw [← star_mul]
+    _ = star (star c * (star b * a)) := by rw [ha.right_assoc]
+    _ = star (star b * a) * c := by rw [star_mul, star_star]
+    _ = (star a * b) * c := by rw [star_mul, star_star]
+  mid_assoc := fun b c => calc
+    b * star a * c = star (star c * star (b * star a)) := by rw [← star_mul, star_star]
+    _ = star (star c * (a * star b)) := by rw [star_mul b, star_star]
+    _ = star ((star c * a) * star b) := by rw [ha.mid_assoc]
+    _ = b * (star a * c) := by rw [star_mul, star_star, star_mul (star c), star_star]
+  right_assoc := fun b c => calc
+    b * c * star a = star (a * star (b * c)) := by rw [star_mul, star_star]
+    _ = star (a * (star c * star b)) := by rw [star_mul b]
+    _ = star ((a * star c) * star b) := by rw [ha.left_assoc]
+    _ = b * star (a * star c) := by rw [star_mul, star_star]
+    _ = b * (c * star a) := by rw [star_mul, star_star]
 /-
   simpa only [star_mul, star_star] using fun g =>
     congr_arg star ((Set.mem_center_iff R).mp ha <| star g).symm
