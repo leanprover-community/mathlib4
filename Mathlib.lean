faba1c3eb8838b67c3eb08b266dec300f7c83889
Scott Morrison <scott@tqft.net>
2023-09-04 13:19:01 +0000

    chore: delete restate_axiom (#6939)
    This isn't used in Mathlib4, and wasn't useful during the port, so I think we can just drop it now.
    
    
    
    Co-authored-by: Scott Morrison <scott.morrison@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -3104,7 +3104,6 @@ import Mathlib.Tactic.Relation.Trans
 import Mathlib.Tactic.Rename
 import Mathlib.Tactic.RenameBVar
 import Mathlib.Tactic.Replace
-import Mathlib.Tactic.RestateAxiom
 import Mathlib.Tactic.Rewrites
 import Mathlib.Tactic.Ring
 import Mathlib.Tactic.Ring.Basic
