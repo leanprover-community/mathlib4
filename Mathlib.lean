57559c59a501e14bcdd3ddef0fca4834decaa271
Yaël Dillies <yael.dillies@gmail.com>
2023-09-04 13:18:58 +0000

    feat: Sup-closed sets (#6901)
    This defines sets closed under supremum/infimum, shows that every set has a sup-closure/inf-closure and prove that if every sup-closed/inf-closed set in a sup-semilattice/inf-semilattice has a least upper bound/greatest lower, then the lattice is in fact complete.
    
    As a bonus, we use our new predicate in `Order.CompactlyGenerated`.
    
    Co-authored-by: Christopher Hoskin <christopher.hoskin@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -2693,6 +2693,7 @@ import Mathlib.Order.SuccPred.IntervalSucc
 import Mathlib.Order.SuccPred.Limit
 import Mathlib.Order.SuccPred.LinearLocallyFinite
 import Mathlib.Order.SuccPred.Relation
+import Mathlib.Order.SupClosed
 import Mathlib.Order.SupIndep
 import Mathlib.Order.SymmDiff
 import Mathlib.Order.Synonym
