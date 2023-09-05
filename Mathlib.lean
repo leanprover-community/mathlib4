5dac7e8d551ccbb757855db8acec18bea2932fdf
Joël Riou <joel.riou@universite-paris-saclay.fr>
2023-09-05 08:30:49 +0000

    feat: the shift on a quotient category (#6653)
    This PR constructs the shift on a quotient category.
    
    
    
    Co-authored-by: Joël Riou <37772949+joelriou@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1154,6 +1154,7 @@ import Mathlib.CategoryTheory.Quotient
 import Mathlib.CategoryTheory.Shift.Basic
 import Mathlib.CategoryTheory.Shift.CommShift
 import Mathlib.CategoryTheory.Shift.Induced
+import Mathlib.CategoryTheory.Shift.Quotient
 import Mathlib.CategoryTheory.Sigma.Basic
 import Mathlib.CategoryTheory.Simple
 import Mathlib.CategoryTheory.SingleObj
