99477edcfe60ed629d4ae8116196e060666ee659
Linus <95619282+linus-md@users.noreply.github.com>
2023-09-05 10:01:36 +0200

    Add pseudo code definitions

--- Mathlib/Combinatorics/SimpleGraph/Dirac.lean
+++ Mathlib/Combinatorics/SimpleGraph/Dirac.lean
@@ -7,12 +7,17 @@ variable {V : Type} [Fintype V] {G : SimpleGraph V} {u v : V}
 
 def SimpleGraph.Walk.IsHamiltonian (p : G.Walk u v) : Prop :=
   ∀ v : V, p.support.count v = 1
-
 -- See
 #check SimpleGraph.Walk.IsEulerian
 
-def SimpleGraph.Walk.IsHamiltonianCycle (p : G.Walk v v) : Prop := sorry
+def SimpleGraph.Walk.IsHamiltonianCycle (p : G.Walk v v) : Prop :=
+  -- ∃ W : SimpleGraph.Walk.IsHamiltonian V ∧ Walk is from v to v.
+  sorry
 
-def SimpleGraph.IsHamiltonian (G : SimpleGraph V) : Prop := sorry
+def SimpleGraph.IsHamiltonian (G : SimpleGraph V) : Prop :=
+  -- ∃ W : SimpleGraph.Walk.IsHamiltonianCycle ∧ |W| = |V|
+  sorry
 
-theorem Dirac {G : SimpleGraph V} (degree_condition : sorry) : G.IsHamiltonian := sorry
+theorem Dirac {G : SimpleGraph V} (degree_condition : sorry) : G.IsHamiltonian :=
+  -- |G| = n ≥ 3 ∧ δ(G) ≥ n/2 → G is hamiltonian
+  sorry
