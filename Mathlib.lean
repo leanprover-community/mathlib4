9a51c25f4b4d7fb971da5413a6e8092b21af94f2
Scott Morrison <scott@tqft.net>
2023-09-05 09:13:07 +0000

    chore: separate NeZero dependency from Nat/Cast/Defs (#6955)
    I'm trying to remove any extraneous material from the core definitions in the algebraic hierarchy that are used in tactics.
    
    
    
    Co-authored-by: Scott Morrison <scott.morrison@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1601,6 +1601,7 @@ import Mathlib.Data.Nat.Bitwise
 import Mathlib.Data.Nat.Cast.Basic
 import Mathlib.Data.Nat.Cast.Defs
 import Mathlib.Data.Nat.Cast.Field
+import Mathlib.Data.Nat.Cast.NeZero
 import Mathlib.Data.Nat.Cast.Prod
 import Mathlib.Data.Nat.Cast.WithTop
 import Mathlib.Data.Nat.Choose.Basic
