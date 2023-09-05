e5deea2ff710187f86a1f06cf4ee0f637108894e
Bhavik Mehta <bhavikmehta8@gmail.com>
2023-09-05 15:15:25 +0200

    contains_vertex

--- Mathlib/Combinatorics/SimpleGraph/Dirac.lean
+++ Mathlib/Combinatorics/SimpleGraph/Dirac.lean
@@ -1,9 +1,7 @@
 import Mathlib.Combinatorics.SimpleGraph.Connectivity
 import Mathlib.Combinatorics.SimpleGraph.Trails
 
-open Classical
-
-variable {V : Type} [Fintype V] {G : SimpleGraph V} {u v : V}
+variable {V : Type} [Fintype V] [DecidableEq V] {G : SimpleGraph V} [DecidableRel G.Adj] {u v : V}
 
 /-- A *Hamiltonian path* is a walk `p` that visits every vertex exactly once. -/
 def SimpleGraph.Walk.IsHamiltonian (p : G.Walk u v) : Prop :=
@@ -11,12 +9,14 @@ def SimpleGraph.Walk.IsHamiltonian (p : G.Walk u v) : Prop :=
 
 /-- If `p` is a Hamiltonian path and `w` is a member of the vertex set, `w` is in the vertex set of `p`. -/
 lemma SimpleGraph.Walk.IsHamiltonian.contains_vertex (p : G.Walk u v) (hp : p.IsHamiltonian)
-  (w : V) : w ∈ p.support := by
-  sorry
+    (w : V) : w ∈ p.support := by
+  specialize hp w
+  rw [←List.count_pos_iff_mem, hp]
+  norm_num1
 
 /-- If `p` is a Hamiltonian path -/
 lemma SimpleGraph.Walk.IsHamiltonian.length (p : G.Walk u v) (hp : p.IsHamiltonian) :
-  p.length = Fintype.card V := by
+    p.length = Fintype.card V := by
   sorry
 
 /-- A *Hamiltonian cycle* is a Walk that visits every vertex once, except the initial
