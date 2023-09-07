c132867c0e2b5fc562de1e77a5ecdc7e70363205
Alex J. Best <alex.j.best@gmail.com>
2023-09-07 23:30:14 +0100

    ok

--- test/eqns.lean
+++ test/eqns.lean
@@ -24,7 +24,7 @@ theorem t_def' : t = 1 := by rw [t]
 
 /--
 error: There already exist stored eqns for 't' registering new equations
-will not have the desired effect supported
+will not have the desired effect.
 -/
 #guard_msgs(error) in
 attribute [eqns t_def] t
