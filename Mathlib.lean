a384f1b247ecdc81dd85dd7a223cbc094bbe35c4
Markus Himmel <markus@himmel-villmar.de>
2023-09-08 11:01:27 +0000

    feat: a functor from a small category to a filtered category factors through a small filtered category (#6212)

--- Mathlib.lean
+++ Mathlib.lean
@@ -940,7 +940,8 @@ import Mathlib.CategoryTheory.Equivalence
 import Mathlib.CategoryTheory.EssentialImage
 import Mathlib.CategoryTheory.EssentiallySmall
 import Mathlib.CategoryTheory.Extensive
-import Mathlib.CategoryTheory.Filtered
+import Mathlib.CategoryTheory.Filtered.Basic
+import Mathlib.CategoryTheory.Filtered.Small
 import Mathlib.CategoryTheory.FinCategory
 import Mathlib.CategoryTheory.FintypeCat
 import Mathlib.CategoryTheory.FullSubcategory
