886117ab226aef116bb6b1219db9ed266073b47d
Koundinya Vajjha <koundinya.vajjha@gmail.com>
2023-09-08 15:55:26 -0700

    feat: prove not_int_of_not_padic_int

--- Counterexamples/HarmonicNotInteger.lean
+++ Counterexamples/HarmonicNotInteger.lean
@@ -26,21 +26,34 @@ def harmonic : ℕ  → ℚ
 | 0 => 0
 | (k+1) => 1 / (k+1) + harmonic k
 
-theorem not_int_of_not_padic_int (a : ℚ) : padicNorm 2 a >1 → a.isInt = False := by {
+theorem not_int_of_not_padic_int (a : ℚ) : padicNorm 2 a > 1 → ¬ a.isInt := by {
   intro H
+  suffices : a.den ≠ 1; simpa [Rat.isInt]
   by_cases (a = 0)
   {
     rw [h] at H h
     dsimp [padicNorm] at *
-    simp at H
+    simp only at H
   }
   {
     dsimp [padicNorm] at *
-    sorry
+    split_ifs at H; try contradiction
+    suffices : padicValRat 2 a < 0
+    unfold padicValRat at this
+    intro Hden
+    rw [Hden] at this
+    simp only [padicValNat.one, CharP.cast_eq_zero, sub_zero] at this
+    norm_cast
+    have Hz : 0 = -0 := by {norm_num}
+    rw [Hz]
+    apply lt_neg_of_lt_neg
+    have hx : (1 : ℚ) < 2 := by {norm_num}
+    rw [← zpow_lt_iff_lt hx]
+    simpa only [zpow_zero]
   }
 }
 
-theorem harmonic_not_int : ∀ n, n ≥ 2 -> (harmonic n).isInt = False := by {
+theorem harmonic_not_int : ∀ n, n ≥ 2 -> ¬ (harmonic n).isInt := by {
   intro n Hn
   apply not_int_of_not_padic_int
 
