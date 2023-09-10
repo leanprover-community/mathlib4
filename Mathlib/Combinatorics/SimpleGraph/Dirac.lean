7289b77ad0915a4d0e5e1c1d2b0c6e9d6b0470f7
Linus <95619282+linus-md@users.noreply.github.com>
2023-09-10 17:49:13 +0200

    Add copyright, split files

---
+++ Mathlib/Combinatorics/SimpleGraph/Dirac.lean
@@ -0,0 +1,15 @@
+/-
+Copyright (c) 2023 Abhiruk Lahiri, Bhavik Mehta, Rishi Mehta, Linus Sommer. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Abhirkuk Lahiri, Bhavik Mehta, Rishi Mehta, Linus Sommer
+-/
+
+import Mathlib.Combinatorics.SimpleGraph.Hamiltonian
+import Mathlib.Tactic.Linarith
+open BigOperators
+
+variable {V : Type} [Fintype V] [DecidableEq V] {G : SimpleGraph V} [DecidableRel G.Adj] {u v : V}
+
+/-- Dirac's theorem (1952): Let |G| = n ≥ 3 ∧ δ(G) ≥ n/2 → G is *hamiltonian*. -/
+theorem Dirac {G : SimpleGraph V} [DecidableRel G.Adj] (CardinalityCondition: Fintype.card V ≥ 3) (MinDegreeCondition: G.minDegree ≥ Fintype.card V/2) : G.IsHamiltonian := by
+  sorry
--- Mathlib/Combinatorics/SimpleGraph/Hamiltonian.lean
+++ Mathlib/Combinatorics/SimpleGraph/Hamiltonian.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2023 Abhiruk Lahiri, Bhavik Mehta, Rishi Mehta, Linus Sommer. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Abhirkuk Lahiri, Bhavik Mehta, Rishi Mehta, Linus Sommer
+-/
+
 import Mathlib.Combinatorics.SimpleGraph.Connectivity
 import Mathlib.Combinatorics.SimpleGraph.Trails
 import Mathlib.Algebra.BigOperators.Basic
