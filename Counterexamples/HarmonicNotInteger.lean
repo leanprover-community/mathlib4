c7c345cf1ec1a2da67b9edc0be23b9794a8e58f6
Koundinya Vajjha <koundinya.vajjha@gmail.com>
2023-09-09 22:44:05 -0700

    add finset defn of harmonic numbers

--- Counterexamples/HarmonicNotInteger.lean
+++ Counterexamples/HarmonicNotInteger.lean
@@ -7,6 +7,7 @@ Authors: Koundinya Vajjha
 import Mathlib.NumberTheory.Padics.PadicNorm
 import Mathlib.NumberTheory.Padics.PadicNumbers
 import Mathlib.NumberTheory.Padics.PadicIntegers
+import Mathlib.Data.Int.Log
 /-!
 
 The nth Harmonic number is not an integer. We formalize the proof using
@@ -20,13 +21,21 @@ https://kconrad.math.uconn.edu/blurbs/gradnumthy/padicharmonicsum.pdf
 namespace Counterexample
 
 open Rat
-
-variable (p : ℕ) (x : ℤ) [Fact p.Prime]
-
+open BigOperators
 
 def harmonic : ℕ  → ℚ
 | 0 => 0
 | (k+1) => 1 / (k+1) + harmonic k
+
+def harmonic' : ℕ → ℚ := fun n => ∑ i in Finset.range n, 1 / (i + 1)
+
+lemma harmonic_harmonic' n : harmonic n = harmonic' n := by {
+  induction' n with k ih ; try simp
+  dsimp [harmonic, harmonic']
+  rw [Finset.sum_range_succ, ih]
+  dsimp [harmonic']
+  rw [add_comm]
+}
 
 
 lemma harmonic_ne_zero : ∀ n, n ≠ 0 → harmonic n > 0 := by {
@@ -77,6 +86,7 @@ theorem not_int_of_not_padic_int (a : ℚ) :
   }
 }
 
+
 theorem harmonic_not_int : ∀ n, n ≥ 2 -> ¬ (harmonic n).isInt := by {
   intro n Hn
   apply not_int_of_not_padic_int
@@ -92,10 +102,11 @@ theorem harmonic_not_int : ∀ n, n ≥ 2 -> ¬ (harmonic n).isInt := by {
     apply one_lt_zpow; try simp
     apply lt_neg.mp
     rw [neg_zero]
-
-
-
-
+    suffices Hlog : padicValRat 2 (harmonic n) = -Nat.log 2 n
+    rw [Hlog]
+    simpa only [Left.neg_neg_iff, Nat.cast_pos, Nat.log_pos_iff, and_true]
+    norm_cast
+    sorry
   }
 
 }
