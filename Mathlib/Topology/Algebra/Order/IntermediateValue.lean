059e16ec5e9e1afb225b46d0c7ba1dacee6ca7ce
Wen Yang <yang-wen@139.com>
2023-09-10 09:38:40 +0800

    generalize the theorem to `f: (a, b) → δ`
    Every continuous injective `f: (a, b) → δ` is strictly monotone.

--- Mathlib/Topology/Algebra/Order/IntermediateValue.lean
+++ Mathlib/Topology/Algebra/Order/IntermediateValue.lean
@@ -648,8 +648,8 @@ theorem ContinuousOn.surjOn_of_tendsto' {f : α → δ} {s : Set α} [OrdConnect
 /-- Suppose f : [a, b] → δ is continuous and injective. Then f is strictly monotone.
 Here is the case where f is strictly increasing. -/
 theorem continuous_inj_is_monotone_Icc {a b : α} {f : α → δ} (h1 : a < b) (h2 : f a < f b)
-    (p1 : ContinuousOn f (Set.Icc a b)) (p2 : Set.InjOn f (Set.Icc a b)) :
-    StrictMonoOn f (Set.Icc a b) := by
+    (p1 : ContinuousOn f (Icc a b)) (p2 : InjOn f (Icc a b)) :
+    StrictMonoOn f (Icc a b) := by
 -- I learned this proof from
 -- <https://www.math.cuhk.edu.hk/course_builder/2021/math1050b/1050b-l14h-3.pdf>
   unfold StrictMonoOn
@@ -662,53 +662,132 @@ theorem continuous_inj_is_monotone_Icc {a b : α} {f : α → δ} (h1 : a < b) (
   by_contra' h5
   have h6 : f t < f s := Ne.lt_of_le (id (Ne.symm h4)) h5
   by_cases h7 : f s ≤ f a
-  · have h9 : Set.Icc t b ⊆ Set.Icc a b := Set.Icc_subset_Icc_left ht.1
-    have p3 : ContinuousOn f (Set.Icc t b) := ContinuousOn.mono p1 h9
+  · have h9 : Icc t b ⊆ Icc a b := Icc_subset_Icc_left ht.1
+    have p3 : ContinuousOn f (Icc t b) := ContinuousOn.mono p1 h9
     have h10 := intermediate_value_Ioo ht.2 p3
-    have h11 : f a ∈ Set.Ioo (f t) (f b) := ⟨(LT.lt.trans_le h6 h7), h2⟩
-    have h12 : f a ∈ f '' Set.Ioo t b := h10 h11
+    have h11 : f a ∈ Ioo (f t) (f b) := ⟨(LT.lt.trans_le h6 h7), h2⟩
+    have h12 : f a ∈ f '' Ioo t b := h10 h11
     choose u hu using h12
     have h13 : u = a :=
-      p2 (h9 (Set.Ioo_subset_Icc_self hu.1)) (Set.left_mem_Icc.mpr (le_of_lt h1)) hu.2
+      p2 (h9 (Ioo_subset_Icc_self hu.1)) (left_mem_Icc.mpr (le_of_lt h1)) hu.2
     have h14 : a < u := lt_of_lt_of_le' hu.1.1 ht.1
     rw [h13] at h14
     exact LT.lt.false h14
   · have h8 : a < t := lt_of_lt_of_le' h3 hs.1
     have h9 : f a ≠ f t := by
       by_contra' this
-      have : a = t := p2 (Set.left_mem_Icc.mpr (le_of_lt h1)) ht this
+      have : a = t := p2 (left_mem_Icc.mpr (le_of_lt h1)) ht this
       rw [this] at h8
       exact LT.lt.false h8
     by_cases h10 : f a < f t
-    · have h11 : Set.Icc a s ⊆ Set.Icc a b := Set.Icc_subset_Icc_right hs.2
-      have p3 : ContinuousOn f (Set.Icc a s) := ContinuousOn.mono p1 h11
+    · have h11 : Icc a s ⊆ Icc a b := Icc_subset_Icc_right hs.2
+      have p3 : ContinuousOn f (Icc a s) := ContinuousOn.mono p1 h11
       have h12 := intermediate_value_Ioo hs.1 p3
-      have h13 : f t ∈ Set.Ioo (f a) (f s) := ⟨h10, h6⟩
+      have h13 : f t ∈ Ioo (f a) (f s) := ⟨h10, h6⟩
       choose u hu using h12 h13
-      have h14 : u = t := p2 (h11 (Set.Ioo_subset_Icc_self hu.1)) ht hu.2
+      have h14 : u = t := p2 (h11 (Ioo_subset_Icc_self hu.1)) ht hu.2
       have h15 : u < t := lt_trans hu.1.2 h3
       rw [h14] at h15
       exact LT.lt.false h15
     · push_neg at h10
       have h11 : f t < f a := Ne.lt_of_le (id (Ne.symm h9)) h10
-      have h12 : Set.Icc s t ⊆ Set.Icc a b := Set.Icc_subset_Icc hs.1 ht.2
-      have p3 : ContinuousOn f (Set.Icc s t) := ContinuousOn.mono p1 h12
+      have h12 : Icc s t ⊆ Icc a b := Icc_subset_Icc hs.1 ht.2
+      have p3 : ContinuousOn f (Icc s t) := ContinuousOn.mono p1 h12
       push_neg at h7
       have h13 := intermediate_value_Ioo' (le_of_lt h3) p3
-      have h14 : f a ∈ Set.Ioo (f t) (f s) := ⟨h11, h7⟩
+      have h14 : f a ∈ Ioo (f t) (f s) := ⟨h11, h7⟩
       choose u hu using h13 h14
       have h15 : u = a :=
-        p2 (h12 (Set.Ioo_subset_Icc_self hu.1)) (Set.left_mem_Icc.mpr (le_of_lt h1)) hu.2
+        p2 (h12 (Ioo_subset_Icc_self hu.1)) (left_mem_Icc.mpr (le_of_lt h1)) hu.2
       have h16 : a < u := lt_of_lt_of_le' hu.1.1 hs.1
       rw [h15] at h16
       exact LT.lt.false h16
 /-- Suppose f : [a, b] → δ is continuous and injective. Then f is strictly monotone.
 Here is the case where f is strictly decreasing. -/
 theorem continuous_inj_is_monotone_Icc' {a b : α} {f : α → δ} (h1 : a < b) (h2 : f b < f a)
-    (p1 : ContinuousOn f (Set.Icc a b)) (p2 : Set.InjOn f (Set.Icc a b)) :
-    StrictAntiOn f (Set.Icc a b) := by
+    (p1 : ContinuousOn f (Icc a b)) (p2 : InjOn f (Icc a b)) :
+    StrictAntiOn f (Icc a b) := by
     let g (x : α) : OrderDual δ := f x
     have h2' : g a < g b := h2
-    have p1' : ContinuousOn g (Set.Icc a b) := p1
-    have p2' : Set.InjOn g (Set.Icc a b) := p2
+    have p1' : ContinuousOn g (Icc a b) := p1
+    have p2' : InjOn g (Icc a b) := p2
     exact continuous_inj_is_monotone_Icc h1 h2' p1' p2'
+/-- Suppose f : [a, b] → δ is continuous and injective. Then f is strictly monotone.-/
+theorem continuous_inj_is_monotone_Icc'' {a b : α} {f : α → δ} (h1 : a < b)
+    (p1 : ContinuousOn f (Icc a b)) (p2 : InjOn f (Icc a b)) :
+    StrictMonoOn f (Icc a b) ∨ StrictAntiOn f (Icc a b) := by
+  by_cases h2 : f a < f b
+  · exact Or.inl (continuous_inj_is_monotone_Icc h1 h2 p1 p2)
+  · have ha : a ∈ Icc a b := left_mem_Icc.mpr (le_of_lt h1)
+    have hb : b ∈ Icc a b := right_mem_Icc.mpr (le_of_lt h1)
+    have h3 : f a ≠ f b := (InjOn.ne_iff p2 ha hb).mpr (ne_of_lt h1)
+    push_neg at h2
+    have h4 : f b < f a := Ne.lt_of_le (id (Ne.symm h3)) h2
+    exact Or.inr (continuous_inj_is_monotone_Icc' h1 h4 p1 p2)
+
+/-- This lemma is only used to
+prove and will be immediately superseded by
+`continuous_inj_is_monotone_Ioo` below.
+It asserts that a continuous injective `f: (a, b) → δ` must be strictly monotone on
+every closed interval `[c, d] ⊆ (a, b)`.-/
+private lemma continuous_inj_is_monotone_Ioo_H1 {a b : α} {f : α → δ}
+    (p1 : ContinuousOn f (Ioo a b)) (p2 : InjOn f (Ioo a b))
+    {c d : α} (h2 : c ∈ Ioo a b) (h3 : d ∈ Ioo a b) (h4 : c < d) :
+    StrictMonoOn f (Icc c d) ∨ StrictAntiOn f (Icc c d) := by
+  have h5 : Icc c d ⊆ Ioo a b := Set.Icc_subset (Ioo a b) h2 h3
+  have h6 : ContinuousOn f (Icc c d) := ContinuousOn.mono p1 h5
+  have h7 : InjOn f (Icc c d) := InjOn.mono h5 p2
+  exact continuous_inj_is_monotone_Icc'' h4 h6 h7
+/-- This lemma is only used to
+prove and will be immediately superseded by
+`continuous_inj_is_monotone_Ioo` below.
+It asserts that a continuous injective `f: (a, b) → δ` is strictly increasing if
+it is strictly increasing on some closed interval `[c, d] ⊆ (a, b)`.-/
+private lemma continuous_inj_is_monotone_Ioo_H2 {a b : α} {f : α → δ}
+    {c d : α} (hc : c ∈ Ioo a b) (hd : d ∈ Ioo a b) (hcd : c < d)
+    (p1 : ContinuousOn f (Ioo a b)) (p2 : InjOn f (Ioo a b))
+    (h5 : StrictMonoOn f (Icc c d)) : StrictMonoOn f (Ioo a b) := by
+  unfold StrictMonoOn
+  intro x hx y hy hxy
+  let s := min c x
+  let t := max d y
+  have hsc : s ≤ c := min_le_left c x
+  have hdt : d ≤ t := le_max_left d y
+  have hs : s ∈ Ioo a b := ⟨lt_min hc.1 hx.1, min_lt_of_left_lt hc.2⟩
+  have ht : t ∈ Ioo a b := ⟨lt_max_of_lt_right hy.1, max_lt hd.2 hy.2⟩
+  have hst : s < t := lt_of_le_of_lt hsc (lt_of_lt_of_le hcd hdt)
+  have h6 : Icc c d ⊆ Icc s t := Icc_subset_Icc hsc hdt
+  have h7 : StrictMonoOn f (Icc s t) ∨ StrictAntiOn f (Icc s t) := continuous_inj_is_monotone_Ioo_H1 p1 p2 hs ht hst
+  have h8' : StrictAntiOn f (Icc s t) → False := by
+    intro h7
+    have h9 : StrictAntiOn f (Icc c d) := StrictAntiOn.mono h7 h6
+    have hcd' : c ≤ d := LT.lt.le hcd
+    have hc : c ∈ Icc c d := Iff.mpr left_mem_Icc hcd'
+    have hd : d ∈ Icc c d := Iff.mpr right_mem_Icc hcd'
+    have hcd_1 : f c < f d := h5 hc hd hcd
+    have hcd_2 : f d < f c := h9 hc hd hcd
+    have : f c < f c := lt_trans hcd_1 hcd_2
+    exact LT.lt.false this
+  have h8 : StrictMonoOn f (Icc s t) := by tauto
+  have hsx : s ≤ x := min_le_right c x
+  have hyt : y ≤ t := le_max_right d y
+  have h9 : Icc x y ⊆ Icc s t := Icc_subset_Icc hsx hyt
+  have h10 : StrictMonoOn f (Icc x y) := StrictMonoOn.mono h8 h9
+  exact h10 (left_mem_Icc.mpr (le_of_lt hxy)) (right_mem_Icc.mpr (le_of_lt hxy)) hxy
+/-- Every continuous injective `f: (a, b) → δ` is strictly monotone.-/
+theorem continuous_inj_is_monotone_Ioo {a b : α} {f : α → δ} (h1 : a < b)
+    (p1 : ContinuousOn f (Ioo a b)) (p2 : InjOn f (Ioo a b)) :
+    StrictMonoOn f (Ioo a b) ∨ StrictAntiOn f (Ioo a b) := by
+  have h3 : (Ioo a b).Nonempty := Iff.mpr nonempty_Ioo h1
+  choose c hc using h3
+  have h4 : (Ioo c b).Nonempty := Iff.mpr nonempty_Ioo hc.2
+  choose d hd' using h4
+  have hcd : c < d := hd'.1
+  have hd : d ∈ Ioo a b := ⟨lt_trans hc.1 hcd, hd'.2⟩
+  have h5 : StrictMonoOn f (Icc c d) ∨ StrictAntiOn f (Icc c d) := continuous_inj_is_monotone_Ioo_H1 p1 p2 hc hd hcd
+  have h5_1 := continuous_inj_is_monotone_Ioo_H2 hc hd hcd p1 p2
+  have h5_2 (h5 : StrictAntiOn f (Icc c d)) : StrictAntiOn f (Ioo a b) := by
+    let g (x : α) : OrderDual δ := f x
+    have hg' : StrictMonoOn g (Ioo a b) := continuous_inj_is_monotone_Ioo_H2 hc hd hcd p1 p2 h5
+    exact hg'
+  tauto
