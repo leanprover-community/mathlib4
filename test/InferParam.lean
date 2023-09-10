8f71af04e0f9f41b3b80bcdb25be06e569abb322
adomani <adomani@gmail.com>
2023-09-10 15:50:27 +0100

    test InferParam

--- test/InferParam.lean
+++ test/InferParam.lean
@@ -2,11 +2,11 @@ import Mathlib.Tactic.InferParam
 
 namespace InferParamTest
 
-lemma zero_le_add (a : Nat) (ha : 0 ≤ a := Nat.zero_le a) : 0 ≤ a + a := calc
+theorem zero_le_add (a : Nat) (ha : 0 ≤ a := Nat.zero_le a) : 0 ≤ a + a := calc
   0 ≤ a := ha
   _ ≤ a + a := Nat.le_add_left _ _
 
-lemma zero_le_add' (a : Nat) (ha : 0 ≤ a := by decide) : 0 ≤ a + a := zero_le_add a ha
+theorem zero_le_add' (a : Nat) (ha : 0 ≤ a := by decide) : 0 ≤ a + a := zero_le_add a ha
 
 example : 0 ≤ 2 + 2 := by
   fail_if_success infer_param
