1263ce4ed8df951469ceee54dbb8268345d119db
Rishi Mehta <rishicomplex@gmail.com>
2023-09-10 15:17:25 +0000

    Finish proof of IsHamiltonian.length.

--- Mathlib/Combinatorics/SimpleGraph/Dirac.lean
+++ Mathlib/Combinatorics/SimpleGraph/Dirac.lean
@@ -20,7 +20,7 @@ lemma SimpleGraph.Walk.IsHamiltonian.contains_vertex (p : G.Walk u v) (hp : p.Is
 /-- If `p` is a Hamiltonian path of a graph `G` its length is one less than the number of vertices of `G`.-/
 lemma SimpleGraph.Walk.IsHamiltonian.length (p : G.Walk u v) (hp : p.IsHamiltonian) :
     p.length = Fintype.card V - 1 := by
-    dsimp only [IsHamiltonian] at hp
+    unfold IsHamiltonian at hp
     have length_relation : p.length = p.support.length - 1
     · cases p
       case nil =>
@@ -28,12 +28,37 @@ lemma SimpleGraph.Walk.IsHamiltonian.length (p : G.Walk u v) (hp : p.IsHamiltoni
       case cons h' p' =>
       simp
     · have : p.support.length = Fintype.card V
-      · have : ∑ u : V, p.support.count u = Fintype.card V - 1
-        · sorry
-        . sorry
+      · have : ∑ u : V, p.support.count u = Fintype.card V
+        · have : ∑ u : V, p.support.count u = ∑ u : V, 1
+          · rw [Finset.sum_congr]
+            · rfl
+            · intro x _
+              specialize hp x
+              assumption
+          · have sum_ones_eq_card : ∑ u : V, 1 = Fintype.card V
+            · rw [← @Finset.card_eq_sum_ones]
+              rfl
+            · rw [sum_ones_eq_card] at this
+              rw [this]
+        · rw [←this]
+          have h₁ := Multiset.toFinset_sum_count_eq (support p : Multiset V)
+          simp only [List.toFinset_coe, Multiset.mem_coe, Multiset.coe_nodup, Multiset.coe_count, Multiset.coe_card,
+            length_support] at h₁
+          have h₂ : p.support.toFinset = Finset.univ
+          · ext a
+            constructor
+            · exact fun _ ↦ Finset.mem_univ a
+            · intro _
+              rw [List.mem_toFinset]
+              exact contains_vertex p hp a
+          · rw [←h₂, h₁, length_relation]
+            rw [tsub_add_eq_add_tsub]
+            · simp
+            · rw [@length_support]
+              simp
       · rw [this] at length_relation
-        sorry
-        -- exact Iff.mp Nat.succ_inj' length_relation
+        assumption
+
 
 lemma Nil_iff_eq_nil {v : V} : ∀ p : G.Walk v v, p.Nil ↔ p = SimpleGraph.Walk.nil
 | .nil | .cons _ _ => by simp
@@ -45,6 +70,7 @@ structure SimpleGraph.Walk.IsHamiltonianCycle (p : G.Walk v v) extends p.IsCycle
     rw [Nil_iff_eq_nil p] at np
     contradiction
   )).IsHamiltonian
+
 
 lemma SimpleGraph.Walk.support_of_tail_eq_tail_of_support (p : G.Walk v v) (hnil : ¬p.Nil) : (p.tail hnil).support = p.support.tail := by
   rw [←SimpleGraph.Walk.cons_support_tail p hnil]
