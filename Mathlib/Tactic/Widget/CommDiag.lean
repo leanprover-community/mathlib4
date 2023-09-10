leanprover-community/mathlib4

commit d00a69d57e2232764c9e2d1c0306276b9e074025

chore: bump ProofWidgets (#7044)

lakefile.lean
@@ -44,7 +44,7 @@ require std from git "https://github.com/leanprover/std4" @ "main"
 require Qq from git "https://github.com/gebner/quote4" @ "master"
 require aesop from git "https://github.com/JLimperg/aesop" @ "master"
 require Cli from git "https://github.com/mhuisi/lean4-cli.git" @ "nightly"
-require proofwidgets from git "https://github.com/EdAyers/ProofWidgets4" @ "v0.0.13"
+require proofwidgets from git "https://github.com/EdAyers/ProofWidgets4" @ "v0.0.15"
 
 lean_lib Cache where
   moreLeanArgs := moreLeanArgs

commit f93f41c18cdb522a8feca6f639ca1d8bfb37ab3b

chore: revert ProofWidgets bump in #7044 and #7056 (#7069)
The cache is broken after #7044 and #7056, so I am reverting them. (And merging directly, rather than via bors.)

Mathlib/Tactic/Widget/CommDiag.lean
@@ -8,7 +8,7 @@ import Mathlib.CategoryTheory.Category.Basic
 
 import ProofWidgets.Component.PenroseDiagram
 import ProofWidgets.Presentation.Expr
-import ProofWidgets.Component.Panel.SelectionPanel
+import ProofWidgets.Component.SelectionPanel
 
 /-! This module defines tactic/meta infrastructure for displaying commutative diagrams in the
 infoview. -/
@@ -58,13 +58,13 @@ open scoped Jsx in
 display as labels in the diagram. -/
 def mkCommDiag (sub : String) (embeds : ExprEmbeds) : MetaM Html := do
   let embeds ← embeds.mapM fun (s, h) =>
-      return (s, <InteractiveCode fmt={← Widget.ppExprTagged h} />)
-  return (
+      return (s, Html.ofTHtml <InteractiveCode fmt={← Widget.ppExprTagged h} />)
+  return Html.ofTHtml
     <PenroseDiagram
       embeds={embeds}
       dsl={include_str ".."/".."/".."/"widget"/"src"/"penrose"/"commutative.dsl"}
       sty={include_str ".."/".."/".."/"widget"/"src"/"penrose"/"commutative.sty"}
-      sub={sub} />)
+      sub={sub} />
 
 /-! ## Commutative triangles -/
 

lakefile.lean
@@ -44,7 +44,7 @@ require std from git "https://github.com/leanprover/std4" @ "main"
 require Qq from git "https://github.com/gebner/quote4" @ "master"
 require aesop from git "https://github.com/JLimperg/aesop" @ "master"
 require Cli from git "https://github.com/mhuisi/lean4-cli.git" @ "nightly"
-require proofwidgets from git "https://github.com/EdAyers/ProofWidgets4" @ "v0.0.15"
+require proofwidgets from git "https://github.com/EdAyers/ProofWidgets4" @ "v0.0.13"
 
 lean_lib Cache where
   moreLeanArgs := moreLeanArgs

test/CommDiag.lean
@@ -1,5 +1,5 @@
 import Mathlib.Tactic.Widget.CommDiag
-import ProofWidgets.Component.Panel.GoalTypePanel
+import ProofWidgets.Component.GoalTypePanel
 
 /-! ## Example use of commutative diagram widgets -/
 