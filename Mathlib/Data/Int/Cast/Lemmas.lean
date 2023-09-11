7451a31677d1587a6b23a9d42db4cb26b926d434
Scott Morrison <scott.morrison@gmail.com>
2023-09-11 14:54:55 +1000

    .

--- Mathlib/Data/Int/Cast/Lemmas.lean
+++ Mathlib/Data/Int/Cast/Lemmas.lean
@@ -6,6 +6,7 @@ Authors: Mario Carneiro
 import Mathlib.Data.Int.Order.Basic
 import Mathlib.Data.Nat.Cast.Commute
 import Mathlib.Data.Nat.Cast.Order
+import Mathlib.Algebra.Hom.Ring.Basic
 
 #align_import data.int.cast.lemmas from "leanprover-community/mathlib"@"acebd8d49928f6ed8920e502a6c90674e75bd441"
 
