17cf7e38788779b9ae0a296952f8ff3cf9768cd6
cyl <2158864003@qq.com>
2023-09-11 10:42:19 +0800

    Update StronglyConvex.lean

--- Mathlib/Analysis/Convex/StronglyConvex.lean
+++ Mathlib/Analysis/Convex/StronglyConvex.lean
@@ -6,7 +6,7 @@ Authors: Chenyi Li, Ziyu Wang
 import Mathlib.Analysis.InnerProductSpace.PiL2
 
 /-!
-# Stongly convex functions from ℝⁿ to ℝ
+# Stongly convex functions from InnerProductSpace E to ℝ
 
 In this file, we define the strongly convex functions and prove the basic properties
 of a strongly convex function.
@@ -33,16 +33,17 @@ variable (s : Set E) {f : E → ℝ}
 lemma equiv_lemma {x y : E} {o m: ℝ} (h₁ : o > 0) (h₂ : o < 1):
 o * (f x - m / 2 * ‖x‖ ^ 2) + (1 - o) * (f y - m / 2 * ‖y‖ ^ 2)
   + m / 2 * ‖o • x + (1 - o) • y‖ ^ 2 =
-o * f x + (1 - o) * f y - m / 2 * o * (1 - o) * ‖x - y‖ ^ 2 :=
-  calc o * (f x - m / 2 * ‖x‖ ^ 2) + (1 - o) * (f y - m / 2 * ‖y‖ ^ 2)
-           + m / 2 * ‖o • x + (1 - o) • y‖ ^ 2
-    _ = o * f x + (1 - o) * f y - m / 2 * o * (1 - o) * (‖x‖ ^ 2 - 2 * inner x y + ‖y‖ ^ 2) := by
-      simp only [Real.rpow_two]
-      rw [norm_add_sq_real (o • x), norm_smul, norm_smul, real_inner_smul_left, inner_smul_right]
-      simp only [Real.norm_eq_abs]
-      rw [abs_of_pos (by linarith), abs_of_pos (by linarith), mul_pow, mul_pow]; ring_nf
-    _ = o * f x + (1 - o) * f y - m / 2 * o * (1 - o) * ‖x - y‖ ^ 2 := by
-          simp only [Real.rpow_two]; rw [← norm_sub_sq_real]
+    o * f x + (1 - o) * f y - m / 2 * o * (1 - o) * ‖x - y‖ ^ 2 :=
+  calc o * (f x - m / 2 * ‖x‖ ^ 2) + (1 - o) * (f y - m / 2 * ‖y‖ ^ 2) 
+        + m / 2 * ‖o • x + (1 - o) • y‖ ^ 2
+          = o * f x + (1 - o) * f y - m / 2 * o 
+            * (1 - o) * (‖x‖ ^ 2 - 2 * inner x y + ‖y‖ ^ 2) := by
+              simp only [Real.rpow_two]
+              rw [norm_add_sq_real, norm_smul, norm_smul, real_inner_smul_left, inner_smul_right]
+              simp only [Real.norm_eq_abs]
+              rw [abs_of_pos (by linarith), abs_of_pos (by linarith), mul_pow, mul_pow]; ring_nf
+        _ = o * f x + (1 - o) * f y - m / 2 * o * (1 - o) * ‖x - y‖ ^ 2 := by
+            simp only [Real.rpow_two]; rw [← norm_sub_sq_real]
 
 theorem Strongly_Convex_Bound (m : ℝ) (strongly_convex: StronglyConvexOn s m f):
 m > 0 ∧ ∀ (x : E), x ∈ s → ∀ (y : E) , y ∈ s
