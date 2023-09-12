4345a5523fe405c6a897229943fbbb851644b441
Jireh Loreaux <loreaujy@gmail.com>
2023-09-12 12:43:48 -0500

    add penalty option

--- Mathlib/Tactic/SetLike/Example.lean
+++ Mathlib/Tactic/SetLike/Example.lean
@@ -1,7 +1,11 @@
+import Mathlib.Algebra.Star.SelfAdjoint
 import Mathlib.Algebra.Star.Subalgebra
 import Mathlib.Algebra.Star.NonUnitalSubalgebra
+import Mathlib.Analysis.Convex.Cone.Basic
 import Mathlib.FieldTheory.Subfield
 import Mathlib.Tactic.SetLike.Macro
+import Mathlib.Topology.Algebra.Algebra
+import Mathlib.Topology.Algebra.StarSubalgebra
 
 set_option autoImplicit true
 
@@ -21,89 +25,113 @@ lemma ofScientific_mem [Field F] [SetLike S F] [SubfieldClass S F] (s : S) (b : 
     (OfScientific.ofScientific n b m : F) ∈ s :=
   SubfieldClass.coe_rat_mem ..
 
+-- general lemmas for `SetLike` classes
 attribute [set_like]
-  zero_mem
+  -- Mathlib.GroupTheory.Submonoid.Basic
   one_mem
+  zero_mem
+  pow_mem
+  nsmul_mem
+  -- Mathlib.GroupTheory.Subsemigroup.Basic
+  mul_mem
   add_mem
+  -- Mathlib.GroupTheory.Subgroup.Basic
   neg_mem
   sub_mem
-  mul_mem
   zpow_mem
-  pow_mem
   div_mem
   inv_mem
-  star_mem
-  algebraMap_mem
-  nsmul_mem
   zsmul_mem
+  -- Mathlib.Algebra.Star.Basic
+  star_mem
+  -- Mathlib.Algebra.Algebra.Subalgebra.Basic
+  algebraMap_mem
+  -- Mathlib.RingTheory.Subsemiring.Basic
   natCast_mem
-  coe_int_mem
   ofNat_mem
+  -- Mathlib.RingTheory.Subring.Basic
+  coe_int_mem
+  -- Mathlib.FieldTheory.Subfield
   ofScientific_mem
   SubfieldClass.coe_rat_mem
   SubfieldClass.rat_smul_mem
 
+-- specific lemmas for `SetLike` classes
+attribute [set_like]
+  ConvexCone.smul_mem
+  -- Mathlib.Analysis.Convex.Cone.Basic
+  skewAdjoint.smul_mem
+  -- Mathlib.Algebra.Star.SelfAdjoint
+
+
 -- we want `nsmul_mem` and `zsmul_mem` to trigger before this
-attribute [aesop safe 2 apply (rule_sets [SetLike])] SMulMemClass.smul_mem
+attribute [set_like 10] SMulMemClass.smul_mem
 
 example [Ring R] (S : Subring R) (hx : x ∈ S) (hy : y ∈ S) (hz : z ∈ S) (n m : ℕ) :
     n • x ^ 3 - 2 • y + z ^ m ∈ S := by
   set_like
 
 -- These lemmas need to exist so that the `set_like` tactic can apply them
+-- Mathlib.GroupTheory.Subsemigroup.Basic
+@[to_additive]
 lemma Subsemigroup.mem_closure_of_mem {M : Type*} [Mul M] {s : Set M} {x : M} (hx : x ∈ s) :
     x ∈ Subsemigroup.closure s :=
   Subsemigroup.subset_closure hx
+-- Mathlib.GroupTheory.Submonoid.Basic
+@[to_additive]
 lemma Submonoid.mem_closure_of_mem {M : Type*} [MulOneClass M] {s : Set M} {x : M} (hx : x ∈ s) :
     x ∈ Submonoid.closure s :=
   Submonoid.subset_closure hx
+-- Mathlib.GroupTheory.Subgroup.Basic
+@[to_additive]
 lemma Subgroup.mem_closure_of_mem {G : Type*} [Group G] {k : Set G} {x : G} (hx : x ∈ k) :
     x ∈ Subgroup.closure k :=
   Subgroup.subset_closure hx
-lemma AddSubsemigroup.mem_closure_of_mem {M : Type*} [Add M] {s : Set M} {x : M} (hx : x ∈ s) :
-    x ∈ AddSubsemigroup.closure s :=
-  AddSubsemigroup.subset_closure hx
-lemma AddSubmonoid.mem_closure_of_mem {M : Type*} [AddZeroClass M] {s : Set M}
-    {x : M} (hx : x ∈ s) : x ∈ AddSubmonoid.closure s :=
-  AddSubmonoid.subset_closure hx
-lemma AddSubgroup.mem_closure_of_mem {G : Type*} [AddGroup G] {k : Set G} {x : G} (hx : x ∈ k) :
-    x ∈ AddSubgroup.closure k :=
-  AddSubgroup.subset_closure hx
+-- Mathlib.LinearAlgebra.Span
 lemma Submodule.mem_span_of_mem {R M : Type*} [Semiring R] [AddCommMonoid M] [Module R M]
     {s : Set M} {x : M} (hx : x ∈ s) : x ∈ Submodule.span R s :=
   Submodule.subset_span hx
+-- Mathlib.RingTheory.Subring.Basic
 lemma Subring.mem_closure_of_mem {R : Type*} [Ring R] {s : Set R} {x : R} (hx : x ∈ s) :
     x ∈ Subring.closure s :=
   Subring.subset_closure hx
+-- Mathlib.RingTheory.Subsemiring.Basic
 lemma Subsemiring.mem_closure_of_mem {R : Type*} [NonAssocSemiring R] {s : Set R} {x : R}
     (hx : x ∈ s) : x ∈ Subsemiring.closure s :=
   Subsemiring.subset_closure hx
+-- Mathlib.RingTheory.Adjoin.Basic
 lemma Algebra.mem_adjoin_of_mem {R A : Type*} [CommSemiring R] [Semiring A] [Algebra R A]
     {s : Set A} {x : A} (hx : x ∈ s) : x ∈ Algebra.adjoin R s :=
   Algebra.subset_adjoin hx
+-- Mathlib.Algebra.Star.Subalgebra
 lemma StarSubalgebra.mem_adjoin_of_mem (R : Type*) {A : Type*} [CommSemiring R] [StarRing R]
     [Semiring A] [Algebra R A] [StarRing A] [StarModule R A] (s : Set A) {x : A} (hx : x ∈ s) :
     x ∈ StarSubalgebra.adjoin R s :=
   StarSubalgebra.subset_adjoin R s hx
+-- Mathlib.RingTheory.NonUnitalSubsemiring.Basic
 lemma NonUnitalSubsemiring.mem_closure_of_mem {R : Type*} [NonUnitalNonAssocSemiring R] {s : Set R}
     {x : R} (hx : x ∈ s) : x ∈ NonUnitalSubsemiring.closure s :=
   NonUnitalSubsemiring.subset_closure hx
+-- Mathlib.RingTheory.NonUnitalSubring.Basic
 lemma NonUnitalSubring.mem_closure_of_mem {R : Type*} [NonUnitalNonAssocRing R] {s : Set R}
     {x : R} (hx : x ∈ s) : x ∈ NonUnitalSubring.closure s :=
   NonUnitalSubring.subset_closure hx
+-- Mathlib.Algebra.Algebra.Subalgebra
 lemma NonUnitalAlgebra.mem_adjoin_of_mem (R : Type*) {A : Type*} [CommSemiring R]
     [NonUnitalNonAssocSemiring A] [Module R A] [IsScalarTower R A A] [SMulCommClass R A A]
     {s : Set A} {x : A} (hx : x ∈ s) : x ∈ NonUnitalAlgebra.adjoin R s :=
   NonUnitalAlgebra.subset_adjoin R hx
+-- Mathlib.Algebra.Star.NonUnitalSubalgebra
 lemma NonUnitalStarAlgebra.mem_adjoin_of_mem (R : Type*) {A : Type*} [CommSemiring R] [StarRing R]
     [NonUnitalSemiring A] [StarRing A] [Module R A] [IsScalarTower R A A] [SMulCommClass R A A]
     [StarModule R A] (s : Set A) {x : A} (hx : x ∈ s) : x ∈ NonUnitalStarAlgebra.adjoin R s :=
   NonUnitalStarAlgebra.subset_adjoin R s hx
+-- Mathlib.FieldTheory.Subfield
 theorem Subfield.mem_closure_of_mem {K : Type*} [Field K] {s : Set K} {x : K} (hx : x ∈ s) :
     x ∈ Subfield.closure s :=
   Subfield.subset_closure hx
 
-attribute [aesop safe apply 3 (rule_sets [SetLike])]
+attribute [set_like 20]
   Subsemigroup.mem_closure_of_mem
   Submonoid.mem_closure_of_mem
   Subgroup.mem_closure_of_mem
@@ -119,6 +147,12 @@ attribute [aesop safe apply 3 (rule_sets [SetLike])]
   NonUnitalSubring.mem_closure_of_mem
   NonUnitalAlgebra.mem_adjoin_of_mem
   NonUnitalStarAlgebra.mem_adjoin_of_mem
+  Subfield.mem_closure_of_mem
+  -- specific lemmas
+  -- Mathlib.Topology.Algebra.Algebra
+  Algebra.self_mem_elementalAlgebra
+  -- Mathlib.Topology.Algebra.StarSubalgebra
+  elementalStarAlgebra.self_mem
 
 example [Ring R] (S : Set R) (hx : x ∈ S) (hy : y ∈ S) (hz : z ∈ S) (n m : ℕ) :
     n • x ^ 3 - y + z ^ m ∈ Subring.closure S := by
@@ -131,9 +165,9 @@ instance StarSubalgebra.smulMemClass [CommSemiring R] [Semiring A] [Algebra R A]
     (SMulMemClass.smul_mem r ha : r • a ∈ s.toSubalgebra)
 
 example [CommRing R] [Ring A] [Algebra R A] [StarRing R] [StarRing A] [StarModule R A]
-    (r : R) (a b c : A) (s : Set A) (ha : a ∈ s) (hb : b ∈ s) (hc : c ∈ s) (n : ℕ) :
-    -b + star (algebraMap R A r) + a ^ n * c ∈ StarSubalgebra.adjoin R s := by
-  set_like
+    (r : R) (a b c : A) (n : ℕ) :
+    -b + star (algebraMap R A r) + a ^ n * c ∈ StarSubalgebra.adjoin R {a, b, c} := by
+  set_like?
 
 example [Monoid M] (x : M) (n : ℕ) : x ^ n ∈ Submonoid.closure {x} := by
   set_like
@@ -153,7 +187,6 @@ example [Monoid M] (x y z : M) (S₁ S₂ : Submonoid M) (h : S₁ ≤ S₂) (hx
     (hy : y ∈ S₁) (hz : z ∈ S₂) :
     x * y * z ∈ S₁ ⊔ S₂ := by
   set_like
-
 
 example [Monoid M] (x y z : M) (S : Submonoid M) (hxy : x * y ∈ S)  (hz : z ∈ S) :
     z * (x * y) ∈ S := by
