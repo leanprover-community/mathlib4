1ded75cd4ae60e010739d0e394f10dc59b8b47ec
Yaël Dillies <yael.dillies@gmail.com>
2023-09-14 09:26:58 +0000

    reduce diff

--- Mathlib/Data/Set/Intervals/Image.lean
+++ Mathlib/Data/Set/Intervals/Image.lean
@@ -19,7 +19,7 @@ section Preorder
 variable [Preorder α] [Preorder β] {a b : α}
 
 lemma MonotoneOn.mapsTo_Ici (h : MonotoneOn f (Ici a)) : MapsTo f (Ici a) (Ici (f a)) :=
-λ _c hc ↦ by aesop
+  fun _c hc => by aesop
 
 lemma MonotoneOn.mapsTo_Iic (h : MonotoneOn f (Iic b)) : MapsTo f (Iic b) (Iic (f b)) :=
 λ _c hc ↦ by aesop
