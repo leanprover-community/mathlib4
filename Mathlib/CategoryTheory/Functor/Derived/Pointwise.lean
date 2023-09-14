43e59654b795ea26cf6046a5adb4cd1554b8aafd
Joël Riou <joel.riou@universite-paris-saclay.fr>
2023-09-14 22:35:34 +0200

    wip

--- Mathlib/CategoryTheory/Functor/Derived/Pointwise.lean
+++ Mathlib/CategoryTheory/Functor/Derived/Pointwise.lean
@@ -1,5 +1,6 @@
 import Mathlib.CategoryTheory.Functor.Derived.RightDerived
 import Mathlib.CategoryTheory.Functor.KanExtension.Pointwise
+import Mathlib.CategoryTheory.RespectsIso
 
 namespace CategoryTheory
 
@@ -56,6 +57,77 @@ noncomputable def isPointwiseLeftKanExtensionOfHasPointwiseRightDerivedFunctor
   exact isPointwiseLeftKanExtensionOfIsLeftKanExtension F' α
 
 end
+
+/-section
+
+variable {Y : C} (S : Set (CostructuredArrow L (L.obj Y)))
+  [L.IsLocalization W]
+  (hS₀ : CostructuredArrow.mk (𝟙 (L.obj Y)) ∈ S)
+  (hS₁ : ∀ ⦃X₁ X₂ : C⦄ (f : X₁ ⟶ X₂) (φ : L.obj X₂ ⟶ L.obj Y),
+    CostructuredArrow.mk φ ∈ S → CostructuredArrow.mk (L.map f ≫ φ) ∈ S)
+  (hS₂ : ∀ ⦃X₁ X₂ : C⦄ (w : X₁ ⟶ X₂) (hw : W w) (φ : L.obj X₁ ⟶ L.obj Y),
+    CostructuredArrow.mk φ ∈ S → CostructuredArrow.mk ((Localization.isoOfHom L W w hw).inv  ≫ φ) ∈ S)
+
+lemma Localization.induction_costructuredArrow [L.IsLocalization W] : S = ⊤ := by
+  have := hS₀
+  have := hS₁
+  have := hS₂
+  sorry
+
+end
+
+section
+
+variable {F L}
+
+def isPointwiseLeftKanExtensionAtOfInverts {G : D ⥤ H} (e : F ≅ L ⋙ G)
+    [L.IsLocalization W] (Y : C) :
+    (LeftExtension.mk _ e.hom).IsPointwiseLeftKanExtensionAt (L.obj Y) where
+  desc s := e.inv.app Y ≫ s.ι.app (CostructuredArrow.mk (𝟙 (L.obj Y)))
+  fac s := by
+    let S : Set (CostructuredArrow L (L.obj Y)) := fun j =>
+      e.hom.app j.left ≫ G.map j.hom ≫ e.inv.app Y ≫
+        NatTrans.app s.ι (CostructuredArrow.mk (𝟙 (L.obj Y))) = s.ι.app j
+    suffices S = ⊤ by
+      intro j
+      have h : S j := by
+        rw [this]
+        tauto
+      dsimp
+      rw [assoc, h]
+    apply Localization.induction_costructuredArrow L W
+    · change _ = _
+      simp
+    · intro X₁ X₂ f φ hφ
+      change _ = _ at hφ ⊢
+      simp at hφ ⊢
+      have eq := s.ι.naturality (CostructuredArrow.homMk f : CostructuredArrow.mk (L.map f ≫ φ) ⟶ CostructuredArrow.mk φ)
+      dsimp at eq
+      rw [comp_id] at eq
+      rw [← eq, ← hφ]
+      simp
+    · intro X₁ X₂ w hw φ hφ
+      change _ = _ at hφ ⊢
+      simp at hφ ⊢
+      have eq := s.ι.naturality (CostructuredArrow.homMk w : CostructuredArrow.mk φ ⟶ CostructuredArrow.mk ((Localization.isoOfHom L W w hw).inv ≫ φ))
+      dsimp at eq
+      rw [comp_id] at eq
+      have : IsIso (F.map w) := by
+        have := Localization.inverts L W w hw
+        rw [← NatIso.naturality_2 e w]
+        dsimp
+        infer_instance
+      rw [← cancel_epi (F.map w), eq, ← hφ]
+      simp only [NatTrans.naturality_assoc, comp_obj, comp_map,
+        NatIso.cancel_natIso_hom_left, ← G.map_comp_assoc,
+        Localization.isoOfHom_hom_inv_id_assoc]
+  uniq s m hm := by
+    dsimp at m hm ⊢
+    have eq := hm (CostructuredArrow.mk (𝟙 (L.obj Y)))
+    dsimp at eq
+    simp only [← eq, map_id, comp_id, Iso.inv_hom_id_app_assoc]
+
+end-/
 
 end Functor
 
