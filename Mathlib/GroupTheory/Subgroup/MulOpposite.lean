3e255f2608821ead639fa56e23a07df1bd73de27
Jujian Zhang <jujian.zhang1998@outlook.com>
2023-09-15 16:14:01 +0100

    apply suggestions from @eric-wieser

--- Mathlib/GroupTheory/Subgroup/MulOpposite.lean
+++ Mathlib/GroupTheory/Subgroup/MulOpposite.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Alex Kontorovich
 -/
 import Mathlib.GroupTheory.Subgroup.Actions
+import Mathlib.GroupTheory.Submonoid.MulOpposite
 
 #align_import group_theory.subgroup.mul_opposite from "leanprover-community/mathlib"@"f93c11933efbc3c2f0299e47b8ff83e9b539cbf6"
 
@@ -17,52 +18,6 @@ subgroup, subgroups
 
 
 variable {G : Type*} [Group G]
-variable {M : Type*} [MulOneClass M]
-
-namespace Submonoid
-
-
-/-- pull a submonoid back to an opposite submonoid along `unop`-/
-@[to_additive (attr := simps)]
-def op {M : Type*} [MulOneClass M] (x : Submonoid M) : Submonoid (MulOpposite M) where
-  carrier := MulOpposite.unop ⁻¹' x.1
-  mul_mem' {a b} ha hb := by
-    simp only [Set.mem_preimage, MulOpposite.unop_mul, SetLike.mem_coe] at ha hb ⊢
-    exact mul_mem hb ha
-  one_mem' := Submonoid.one_mem' _
-
-/-- pull an opposite submonoid back to a submonoid along `op`-/
-@[to_additive (attr := simps)]
-def unop {M : Type*} [MulOneClass M] (x : Submonoid (MulOpposite M)) : Submonoid M where
-  carrier := MulOpposite.op ⁻¹' x.1
-  mul_mem' {a b} ha hb := by
-    simp only [Set.mem_preimage, MulOpposite.unop_mul, SetLike.mem_coe] at ha hb ⊢
-    exact mul_mem hb ha
-  one_mem' := Submonoid.one_mem' _
-
-/-- A submonoid `H` of `G` determines a submonoid `H.opposite` of the opposite group `Gᵐᵒᵖ`. -/
-@[to_additive (attr := simps) "A additive submonoid `H` of `G` determines an additive submonoid
-`H.opposite` of the opposite group `Gᵐᵒᵖ`."]
-def opposite : Submonoid M ≃ Submonoid Mᵐᵒᵖ where
-  toFun := op
-  invFun := unop
-  left_inv _ := SetLike.coe_injective rfl
-  right_inv _ := SetLike.coe_injective rfl
-
-/-- Bijection between a submonoid `H` and its opposite. -/
-@[to_additive (attr := simps!) "Bijection between an additive submonoid `H` and its opposite."]
-def oppositeEquiv (H : Submonoid M) : H ≃ opposite H :=
-  MulOpposite.opEquiv.subtypeEquiv fun _ => Iff.rfl
-
-@[to_additive]
-instance (H : Submonoid M) [Encodable H] : Encodable (opposite H) :=
-  Encodable.ofEquiv H H.oppositeEquiv.symm
-
-@[to_additive]
-instance (H : Submonoid M) [Countable H] : Countable (opposite H) :=
-  Countable.of_equiv H H.oppositeEquiv
-
-end Submonoid
 
 namespace Subgroup
 
@@ -85,6 +40,14 @@ def opposite : Subgroup G ≃ Subgroup Gᵐᵒᵖ
   right_inv _ := SetLike.coe_injective rfl
 #align subgroup.opposite Subgroup.opposite
 #align add_subgroup.opposite AddSubgroup.opposite
+
+@[to_additive (attr := simp, nolint simpNF)] lemma opposite_toSubmonoid (H : Subgroup G) :
+    (opposite H).toSubmonoid = Submonoid.opposite H.toSubmonoid :=
+  rfl
+
+@[to_additive (attr := simp, nolint simpNF)] lemma opposite_symm_toSubmonoid (H : Subgroup Gᵐᵒᵖ) :
+    (opposite.symm H).toSubmonoid = Submonoid.opposite.symm H.toSubmonoid :=
+  rfl
 
 /-- Bijection between a subgroup `H` and its opposite. -/
 @[to_additive (attr := simps!) "Bijection between an additive subgroup `H` and its opposite."]
---
+++ Mathlib/GroupTheory/Submonoid/MulOpposite.lean
@@ -0,0 +1,51 @@
+/-
+Copyright (c) 2023 Eric Wieser. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Eric Wieser, Jujian Zhang
+-/
+import Mathlib.GroupTheory.Submonoid.Basic
+import Mathlib.Algebra.Group.Opposite
+
+/-!
+# Submonoid of opposite monoids
+
+For every monoid `M`, we construct an equivalence between submonoids of `M` and that of `Mᵐᵒᵖ`.
+
+-/
+
+variable {M : Type*} [MulOneClass M]
+
+namespace Submonoid
+
+/-- pull a submonoid back to an opposite submonoid along `unop`-/
+@[to_additive (attr := simps) "pull an additive submonoid back to an opposite submonoid along
+`unop`"]
+def op {M : Type*} [MulOneClass M] (x : Submonoid M) : Submonoid (MulOpposite M) where
+  carrier := MulOpposite.unop ⁻¹' x.1
+  mul_mem' {a b} ha hb := by simpa using mul_mem hb ha
+  one_mem' := Submonoid.one_mem' _
+
+/-- pull an opposite submonoid back to a submonoid along `op`-/
+@[to_additive (attr := simps) "pull an opposite additive submonoid back to a submonoid along `op`"]
+def unop {M : Type*} [MulOneClass M] (x : Submonoid (MulOpposite M)) : Submonoid M where
+  carrier := MulOpposite.op ⁻¹' x.1
+  mul_mem' {a b} ha hb := by simpa using mul_mem hb ha
+  one_mem' := Submonoid.one_mem' _
+
+/-- A submonoid `H` of `G` determines a submonoid `H.opposite` of the opposite group `Gᵐᵒᵖ`. -/
+@[to_additive (attr := simps) "A additive submonoid `H` of `G` determines an additive submonoid
+`H.opposite` of the opposite group `Gᵐᵒᵖ`."]
+def opposite : Submonoid M ≃ Submonoid Mᵐᵒᵖ where
+  toFun := op
+  invFun := unop
+  left_inv _ := SetLike.coe_injective rfl
+  right_inv _ := SetLike.coe_injective rfl
+
+/-- Bijection between a submonoid `H` and its opposite. -/
+@[to_additive (attr := simps!) "Bijection between an additive submonoid `H` and its opposite."]
+def oppositeEquiv (H : Submonoid M) : H ≃ opposite H :=
+  MulOpposite.opEquiv.subtypeEquiv fun _ => Iff.rfl
+
+end Submonoid
+
+#lint
