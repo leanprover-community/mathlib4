d0e134276d62cd220632f0f6666e967b03a1cbd3
Jujian Zhang <jujian.zhang1998@outlook.com>
2023-09-15 15:16:20 +0100

    apply suggestion from @eric-wieser

--- Mathlib/RingTheory/NonZeroDivisors.lean
+++ Mathlib/RingTheory/NonZeroDivisors.lean
@@ -5,7 +5,7 @@ Authors: Kenny Lau, Devon Tuma
 -/
 import Mathlib.GroupTheory.Submonoid.Operations
 import Mathlib.GroupTheory.Submonoid.Membership
-import Mathlib.GroupTheory.GroupAction.Opposite
+import Mathlib.GroupTheory.Subgroup.MulOpposite
 
 #align_import ring_theory.non_zero_divisors from "leanprover-community/mathlib"@"1126441d6bccf98c81214a0780c73d499f6721fe"
 
