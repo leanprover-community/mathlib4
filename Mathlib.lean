786e4ed6e1cdb744fde393ac24c1141e752bed26
Chris Hughes <chrishughes24@gmail.com>
2023-09-15 19:12:19 +0100

    fix

--- Mathlib.lean
+++ Mathlib.lean
@@ -2515,6 +2515,7 @@ import Mathlib.MeasureTheory.Measure.WithDensityVectorMeasure
 import Mathlib.MeasureTheory.PiSystem
 import Mathlib.MeasureTheory.Tactic
 import Mathlib.ModelTheory.Algebra.Ring.Basic
+import Mathlib.ModelTheory.Algebra.Ring.FreeCommRing
 import Mathlib.ModelTheory.Basic
 import Mathlib.ModelTheory.Bundled
 import Mathlib.ModelTheory.Definability
