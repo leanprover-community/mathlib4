ef41506655f43b2511879810cf3c95970ec2ab76
Junyan Xu <junyanxumath@gmail.com>
2023-09-14 23:33:09 -0400

    fix

--- Mathlib/RingTheory/MvPolynomial/Symmetric/FundamentalTheorem.lean
+++ Mathlib/RingTheory/MvPolynomial/Symmetric/FundamentalTheorem.lean
@@ -33,24 +33,22 @@ declaration docstrings
 move maxDegree API
 -/
 
-namespace MvPolynomial
+variable {σ τ R : Type*} {n m : ℕ}
 
 open BigOperators Finset
 
-variable {σ τ R : Type*} {n m : ℕ}
+namespace Fin
 
 section accumulate
 
-/-- The `j`th entry of `accumulate m t` is the sum of `t i` over all `i ≤ j`. -/
+/-- The `j`th entry of `accumulate m t` is the sum of `t i` over all `i ≥ j`. -/
 @[simps] def accumulate (n m : ℕ) : (Fin n → ℕ) →+ (Fin m → ℕ) where
   toFun t j := ∑ i in univ.filter (fun i : Fin n => (j : ℕ) ≤ i), t i
   map_zero' := funext <| fun j => sum_eq_zero <| fun h _ => rfl
   map_add' t₁ t₂ := funext <| fun j => by dsimp only; exact sum_add_distrib
--- use Finset.Ici .. in Fin .. rather than filter ..
 
 def inv_accumulate (n m : ℕ) (s : Fin m → ℕ) (i : Fin n) : ℕ :=
   (if hi : i < m then s ⟨i, hi⟩ else 0) - (if hi : i + 1 < m then s ⟨i + 1, hi⟩ else 0)
--- use get?
 
 lemma accumulate_rec {i n m : ℕ} (hin : i < n) (him : i + 1 < m) (t : Fin n → ℕ) :
     accumulate n m t ⟨i, Nat.lt_of_succ_lt him⟩ = t ⟨i, hin⟩ + accumulate n m t ⟨i + 1, him⟩ := by
@@ -98,6 +96,12 @@ lemma surjective_accumulate {n m} (hmn : m ≤ n) {s : Fin m → ℕ} (hs : Anti
 
 end accumulate
 
+end Fin
+
+namespace MvPolynomial
+
+open Fin
+
 section CommSemiring
 
 variable (σ R n) [CommSemiring R]
@@ -142,20 +146,23 @@ lemma esymmAlgHom_monomial_add [Fintype σ] {t s : Fin n →₀ ℕ} :
 lemma esymmAlgHom_zero [Fintype σ] : esymmAlgHom_monomial σ (0 : Fin n →₀ ℕ) r = C r := by
   rw [esymmAlgHom_monomial, monomial_zero', esymmAlgHom_apply, aeval_C]; rfl
 
-lemma maxDegree_monic_esymm [Nontrivial R] {i : ℕ} (him : i < m) :
-    maxDegree (esymm (Fin m) R (i + 1)) =
-      toLex (Finsupp.indicator (Iic ⟨i, him⟩) fun _ _ => 1) ∧
-    Monic (esymm (Fin m) R (i + 1)) := by
-  have := maxDegree_leadingCoeff_sum_eq (univ.powersetLen (i + 1))
-    (a := Iic (⟨i, him⟩ : Fin m)) ?_ (fun s => monomial (∑ j in s, Finsupp.single j 1) (1 : R)) ?_
-  · rwa [← esymm_eq_sum_monomial, ← Finsupp.indicator_eq_sum_single,
-         maxDegree_monomial _ one_ne_zero, leadingCoeff_monomial] at this
+open AddMonoidAlgebra
+lemma supDegree_monic_esymm [Nontrivial R] {i : ℕ} (him : i < m) :
+    supDegree toLex (esymm (Fin m) R (i + 1)) =
+      toLex (Finsupp.indicator (Iic ⟨i, him⟩) fun _ _ ↦ 1) ∧
+    Monic toLex (esymm (Fin m) R (i + 1)) := by
+  have := supDegree_leadingCoeff_sum_eq (D := toLex) (s := univ.powersetLen (i + 1))
+    (i := Iic (⟨i, him⟩ : Fin m)) ?_ (f := fun s => monomial (∑ j in s, fun₀ | j => 1) (1 : R)) ?_
+  · rwa [← esymm_eq_sum_monomial, ← Finsupp.indicator_eq_sum_single, ← single_eq_monomial,
+         supDegree_single_ne_zero _ one_ne_zero, leadingCoeff_single toLex.injective] at this
   · exact mem_powersetLen.2 ⟨subset_univ _, Fin.card_Iic _⟩
   intro t ht hne
-  simp_rw [maxDegree_monomial _ one_ne_zero, ← Finsupp.indicator_eq_sum_single]
+  simp_rw [← single_eq_monomial, supDegree_single_ne_zero _ one_ne_zero,
+           ← Finsupp.indicator_eq_sum_single]
   rw [Ne, eq_comm, ← subset_iff_eq_of_card_le, not_subset] at hne
   · simp_rw [← mem_sdiff] at hne
-    refine ⟨min' _ hne, let hkm := mem_sdiff.1 (min'_mem _ hne); ⟨fun k hk => ?_, ?_⟩⟩
+    let hkm := mem_sdiff.1 (min'_mem _ hne)
+    refine ⟨min' _ hne, fun k hk => ?_, ?_⟩
     all_goals simp only [Pi.toLex_apply, ofLex_toLex, Finsupp.indicator_apply]
     · have hki := mem_Iic.2 (hk.le.trans <| mem_Iic.1 hkm.1)
       rw [dif_pos hki, dif_pos]
@@ -163,49 +170,62 @@ lemma maxDegree_monic_esymm [Nontrivial R] {i : ℕ} (him : i < m) :
     · rw [dif_neg hkm.2, dif_pos hkm.1]; exact Nat.zero_lt_one
   · rw [(mem_powersetLen.1 ht).2, Fin.card_Iic]
 
-lemma maxDegree_esymm [Nontrivial R] (him : i < m) :
-    ofLex (maxDegree <| esymm (Fin m) R (i + 1)) = accumulate n m (Finsupp.single i 1) := by
-  rw [(maxDegree_monic_esymm him).1, ofLex_toLex]
+lemma supDegree_esymm [Nontrivial R] (him : i < m) :
+    ofLex (supDegree toLex <| esymm (Fin m) R (i + 1)) = accumulate n m (Finsupp.single i 1) := by
+  rw [(supDegree_monic_esymm him).1, ofLex_toLex]
   ext j; simp_rw [Finsupp.indicator_apply, mem_Iic, accumulate_apply, Finsupp.single_apply,
     sum_ite_eq, mem_filter, mem_univ, true_and]; rfl
 
-lemma monic_esymm {i : ℕ} (him : i ≤ m) : Monic (esymm (Fin m) R i) := by
-  cases' i with i; rw [esymm_zero]; exact monic_one
+lemma monic_esymm {i : ℕ} (him : i ≤ m) : Monic toLex (esymm (Fin m) R i) := by
+  cases' i with i; rw [esymm_zero]; exact monic_one toLex.injective
   cases subsingleton_or_nontrivial R
   · rw [Monic]; apply Subsingleton.elim
-  · exact (maxDegree_monic_esymm him).2
+  · exact (supDegree_monic_esymm him).2
 
 lemma leadingCoeff_esymmAlgHom_monomial (t : Fin n →₀ ℕ) (hnm : n ≤ m) :
-    leadingCoeff (esymmAlgHom_monomial (Fin m) t r) = r := by
+    leadingCoeff toLex (esymmAlgHom_monomial (Fin m) t r) = r := by
   apply t.induction₂
-  · rw [esymmAlgHom_zero, leadingCoeff_C]
+  · rw [esymmAlgHom_zero, leadingCoeff_toLex_C]
   intro i _ _ _ _ ih
-  rw [esymmAlgHom_monomial_add, esymmAlgHom_monomial_single_one, Monic.leadingCoeff_mul, ih]
-  exact (monic_esymm <| i.2.trans_le hnm).pow
+  rw [esymmAlgHom_monomial_add, esymmAlgHom_monomial_single_one,
+      ((monic_esymm <| i.2.trans_le hnm).pow toLex_add toLex.injective).leadingCoeff_mul, ih]
+  exacts [toLex.injective, toLex_add]
 
-lemma maxDegree_esymmAlgHom_monomial (t : Fin n →₀ ℕ) (hnm : n ≤ m) :
-    ofLex (esymmAlgHom_monomial (Fin m) t r).maxDegree = accumulate n m t := by
+lemma supDegree_esymmAlgHom_monomial (t : Fin n →₀ ℕ) (hnm : n ≤ m) :
+    ofLex (supDegree toLex <| esymmAlgHom_monomial (Fin m) t r) = accumulate n m t := by
   nontriviality R
   apply t.induction₂
-  · simp_rw [esymmAlgHom_zero, maxDegree_C, Finsupp.coe_zero, map_zero]; rfl
+  · simp_rw [esymmAlgHom_zero, supDegree_toLex_C, Finsupp.coe_zero, map_zero]; rfl
   intro i _ _ _ _ ih
   have := i.2.trans_le hnm
-  rw [esymmAlgHom_monomial_add, esymmAlgHom_monomial_single_one, Monic.maxDegree_mul', ofLex_add,
-      Finsupp.coe_add, ih, Finsupp.coe_add, map_add, Monic.maxDegree_pow, ofLex_smul,
-      Finsupp.coe_smul, maxDegree_esymm, ← map_nsmul, ← Finsupp.coe_smul, Finsupp.smul_single,
+  rw [esymmAlgHom_monomial_add, esymmAlgHom_monomial_single_one,
+      Monic.supDegree_mul_of_ne_zero toLex.injective toLex_add, ofLex_add, Finsupp.coe_add, ih,
+      Finsupp.coe_add, map_add, Monic.supDegree_pow rfl toLex_add toLex.injective, ofLex_smul,
+      Finsupp.coe_smul, supDegree_esymm, ← map_nsmul, ← Finsupp.coe_smul, Finsupp.smul_single,
       nsmul_one]; rfl
   · exact this
   · exact monic_esymm this
-  · exact (monic_esymm this).pow
-  · rwa [Ne, ← leadingCoeff_eq_zero, leadingCoeff_esymmAlgHom_monomial _ hnm]
+  · exact (monic_esymm this).pow toLex_add toLex.injective
+  · rwa [Ne, ← leadingCoeff_eq_zero toLex.injective, leadingCoeff_esymmAlgHom_monomial _ hnm]
 
-/- lemma maxDegree_esymmAlgHom_single (him : i < m) :
-    maxDegree (esymmAlgHom_monomial (Fin m) (Finsupp.single i k) r) =
-    -- if (j : ℕ) ≤ i then k else 0 :=
-    accumulate m (Finsupp.single i k) := by
-  rw [esymmAlgHom_monomial_single, Monic.maxDegree_mul, maxDegree_C, zero_add, Monic.maxDegree_pow,
-      Finsupp.coe_smul, maxDegree_esymm him, ← map_nsmul, Finsupp.smul_single, nsmul_one]; rfl
-  exacts [monic_esymm him, (monic_esymm him).pow] -/
+lemma IsSymmetric.antitone_supDegree [LinearOrder σ] {p : MvPolynomial σ R} (hp : p.IsSymmetric) :
+    Antitone (ofLex <| p.supDegree toLex) := by
+  obtain rfl | h0 := eq_or_ne p 0
+  · rw [supDegree_zero]; exact fun _ _ _ => le_rfl
+  rw [Antitone]; by_contra' h
+  obtain ⟨i, j, hle, hlt⟩ := h
+  apply (le_sup (s := p.support) (f := toLex) _).not_lt
+  pick_goal 3
+  · rw [← hp (Equiv.swap i j), mem_support_iff, coeff_rename_mapDomain]
+    · rwa [Ne, ← leadingCoeff_eq_zero toLex.injective, leadingCoeff_eq] at h0
+    · apply Equiv.injective
+  refine ⟨i, fun k hk => ?_, ?_⟩
+  all_goals dsimp only [Pi.toLex_apply, ofLex_toLex]
+  · conv_rhs => rw [← Equiv.swap_apply_of_ne_of_ne hk.ne (hk.trans_le hle).ne]
+    rw [Finsupp.mapDomain_apply (Equiv.injective _)]; rfl
+  · apply hlt.trans_eq
+    convert ← (Finsupp.mapDomain_apply (Equiv.injective _) _ _).symm
+    exact Equiv.swap_apply_right i j
 
 end CommSemiring
 
@@ -216,21 +236,24 @@ variable (R) [Fintype σ] [CommRing R]
 -- shortcut instance necessary to avoid timeout
 instance {K} [CommRing K] : AddCommMonoid K := inferInstance
 
-/- May also holds for cancellative CommSemiring, not sure. -/
+open AddMonoidAlgebra
+
+/- Also holds for a cancellative CommSemiring. -/
 lemma injective_esymmAlgHom_fin (h : n ≤ m) :
     Function.Injective (esymmAlgHom (Fin m) R n) := by
   rw [injective_iff_map_eq_zero]
   refine fun p => (fun hp => ?_).mtr
   rw [p.as_sum, map_sum (esymmAlgHom (Fin m) R n), ← Subalgebra.coe_eq_zero,
       AddSubmonoidClass.coe_finset_sum]
-  refine sum_ne_zero_of_injOn_maxDegree (support_eq_empty.not.2 hp) _ (fun t ht => ?_)
+  refine sum_ne_zero_of_injOn_supDegree (D := toLex) (support_eq_empty.not.2 hp) (fun t ht => ?_)
     (fun t ht s hs he => FunLike.ext' <| injective_accumulate h ?_)
-  · rw [← esymmAlgHom_monomial, Ne, ← leadingCoeff_eq_zero, leadingCoeff_esymmAlgHom_monomial t h]
+  · rw [← esymmAlgHom_monomial, Ne, ← leadingCoeff_eq_zero toLex.injective,
+        leadingCoeff_esymmAlgHom_monomial t h]
     rwa [mem_support_iff] at ht
   rw [mem_coe, mem_support_iff] at ht hs
   dsimp only [Function.comp] at he
   rwa [← esymmAlgHom_monomial, ← esymmAlgHom_monomial, ← ofLex_inj, FunLike.ext'_iff,
-       maxDegree_esymmAlgHom_monomial ht t h, maxDegree_esymmAlgHom_monomial hs s h] at he
+       supDegree_esymmAlgHom_monomial ht t h, supDegree_esymmAlgHom_monomial hs s h] at he
 
 lemma injective_esymmAlgHom (hn : n ≤ Fintype.card σ) :
     Function.Injective (esymmAlgHom σ R n) := by
@@ -242,18 +265,18 @@ lemma bijective_esymmAlgHom_fin (n : ℕ) :
   refine ⟨injective_esymmAlgHom_fin R le_rfl, ?_⟩
   rintro ⟨p, hp⟩; rw [← AlgHom.mem_range]
   obtain rfl | h0 := eq_or_ne p 0; apply Subalgebra.zero_mem
-  induction' he : p.maxDegree using WellFoundedLT.induction with t ih generalizing p; subst he
-  let t := Finsupp.equivFunOnFinite.symm (inv_accumulate n n <| ofLex p.maxDegree)
-  have hd : (esymmAlgHom_monomial _ t p.leadingCoeff).maxDegree = p.maxDegree
-  · rw [← ofLex_inj, FunLike.ext'_iff, maxDegree_esymmAlgHom_monomial _ _ le_rfl]
-    · exact surjective_accumulate le_rfl hp.antitone_maxDegree
-    · rwa [Ne, leadingCoeff_eq_zero]
-  obtain he | hne := eq_or_ne p (esymmAlgHom_monomial _ t p.leadingCoeff)
-  · convert AlgHom.mem_range_self _ (monomial t p.leadingCoeff); exact he
-  have := (maxDegree_sub_lt_of_leadingCoeff_eq hd.symm ?_).resolve_right hne
+  induction' he : p.supDegree toLex using WellFoundedLT.induction with t ih generalizing p; subst he
+  let t := Finsupp.equivFunOnFinite.symm (inv_accumulate n n <| ofLex <| p.supDegree toLex)
+  have hd : (esymmAlgHom_monomial _ t <| p.leadingCoeff toLex).supDegree toLex = p.supDegree toLex
+  · rw [← ofLex_inj, FunLike.ext'_iff, supDegree_esymmAlgHom_monomial _ _ le_rfl]
+    · exact surjective_accumulate le_rfl hp.antitone_supDegree
+    · rwa [Ne, leadingCoeff_eq_zero toLex.injective]
+  obtain he | hne := eq_or_ne p (esymmAlgHom_monomial _ t <| p.leadingCoeff toLex)
+  · convert AlgHom.mem_range_self _ (monomial t <| p.leadingCoeff toLex); exact he
+  have := (supDegree_sub_lt_of_leadingCoeff_eq toLex.injective hd.symm ?_).resolve_right hne
   · specialize ih _ this _ (Subalgebra.sub_mem _ hp <| isSymmetric_esymmAlgHom_monomial _ _) _ rfl
     · rwa [sub_ne_zero]
-    convert ← Subalgebra.add_mem _ ih ⟨monomial t p.leadingCoeff, rfl⟩
+    convert ← Subalgebra.add_mem _ ih ⟨monomial t (p.leadingCoeff toLex), rfl⟩
     apply sub_add_cancel p
   · rw [leadingCoeff_esymmAlgHom_monomial t le_rfl]
 
