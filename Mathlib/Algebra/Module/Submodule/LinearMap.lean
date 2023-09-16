51861192dddf09760f4a7d9c9818941f0641b869
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2023-09-16 12:49:26 +0200

    chore: move some results about linear maps out of LinearAlgebra.Basic

--- Mathlib/Algebra/Module/Submodule/LinearMap.lean
+++ Mathlib/Algebra/Module/Submodule/LinearMap.lean
@@ -117,3 +117,201 @@ end AddCommMonoid
 end Submodule
 
 end
+
+section
+
+variable {R : Type*} {R₁ : Type*} {R₂ : Type*} {R₃ : Type*} {R₄ : Type*}
+variable {S : Type*}
+variable {K : Type*} {K₂ : Type*}
+variable {M : Type*} {M' : Type*} {M₁ : Type*} {M₂ : Type*} {M₃ : Type*} {M₄ : Type*}
+variable {N : Type*} {N₂ : Type*}
+variable {ι : Type*}
+variable {V : Type*} {V₂ : Type*}
+
+namespace LinearMap
+
+section AddCommMonoid
+
+variable [Semiring R] [Semiring R₂] [Semiring R₃] [Semiring R₄]
+variable [AddCommMonoid M] [AddCommMonoid M₁] [AddCommMonoid M₂]
+variable [AddCommMonoid M₃] [AddCommMonoid M₄]
+variable [Module R M] [Module R M₁] [Module R₂ M₂] [Module R₃ M₃] [Module R₄ M₄]
+variable {σ₁₂ : R →+* R₂} {σ₂₃ : R₂ →+* R₃} {σ₃₄ : R₃ →+* R₄}
+variable {σ₁₃ : R →+* R₃} {σ₂₄ : R₂ →+* R₄} {σ₁₄ : R →+* R₄}
+variable [RingHomCompTriple σ₁₂ σ₂₃ σ₁₃] [RingHomCompTriple σ₂₃ σ₃₄ σ₂₄]
+variable [RingHomCompTriple σ₁₃ σ₃₄ σ₁₄] [RingHomCompTriple σ₁₂ σ₂₄ σ₁₄]
+variable (f : M →ₛₗ[σ₁₂] M₂) (g : M₂ →ₛₗ[σ₂₃] M₃)
+
+#align linear_map.map_sum map_sumₓ
+
+
+/-- The restriction of a linear map `f : M → M₂` to a submodule `p ⊆ M` gives a linear map
+`p → M₂`. -/
+def domRestrict (f : M →ₛₗ[σ₁₂] M₂) (p : Submodule R M) : p →ₛₗ[σ₁₂] M₂ :=
+  f.comp p.subtype
+#align linear_map.dom_restrict LinearMap.domRestrict
+
+@[simp]
+theorem domRestrict_apply (f : M →ₛₗ[σ₁₂] M₂) (p : Submodule R M) (x : p) :
+    f.domRestrict p x = f x :=
+  rfl
+#align linear_map.dom_restrict_apply LinearMap.domRestrict_apply
+
+/-- A linear map `f : M₂ → M` whose values lie in a submodule `p ⊆ M` can be restricted to a
+linear map M₂ → p. -/
+def codRestrict (p : Submodule R₂ M₂) (f : M →ₛₗ[σ₁₂] M₂) (h : ∀ c, f c ∈ p) : M →ₛₗ[σ₁₂] p := by
+  refine' { toFun := fun c => ⟨f c, h c⟩.. } <;> intros <;> apply SetCoe.ext <;> simp
+#align linear_map.cod_restrict LinearMap.codRestrict
+
+@[simp]
+theorem codRestrict_apply (p : Submodule R₂ M₂) (f : M →ₛₗ[σ₁₂] M₂) {h} (x : M) :
+    (codRestrict p f h x : M₂) = f x :=
+  rfl
+#align linear_map.cod_restrict_apply LinearMap.codRestrict_apply
+
+@[simp]
+theorem comp_codRestrict (p : Submodule R₃ M₃) (h : ∀ b, g b ∈ p) :
+    ((codRestrict p g h).comp f : M →ₛₗ[σ₁₃] p) = codRestrict p (g.comp f) fun _ => h _ :=
+  ext fun _ => rfl
+#align linear_map.comp_cod_restrict LinearMap.comp_codRestrict
+
+@[simp]
+theorem subtype_comp_codRestrict (p : Submodule R₂ M₂) (h : ∀ b, f b ∈ p) :
+    p.subtype.comp (codRestrict p f h) = f :=
+  ext fun _ => rfl
+#align linear_map.subtype_comp_cod_restrict LinearMap.subtype_comp_codRestrict
+
+/-- Restrict domain and codomain of a linear map. -/
+def restrict (f : M →ₗ[R] M₁) {p : Submodule R M} {q : Submodule R M₁} (hf : ∀ x ∈ p, f x ∈ q) :
+    p →ₗ[R] q :=
+  (f.domRestrict p).codRestrict q <| SetLike.forall.2 hf
+#align linear_map.restrict LinearMap.restrict
+
+@[simp]
+theorem restrict_coe_apply (f : M →ₗ[R] M₁) {p : Submodule R M} {q : Submodule R M₁}
+    (hf : ∀ x ∈ p, f x ∈ q) (x : p) : ↑(f.restrict hf x) = f x :=
+  rfl
+#align linear_map.restrict_coe_apply LinearMap.restrict_coe_apply
+
+theorem restrict_apply {f : M →ₗ[R] M₁} {p : Submodule R M} {q : Submodule R M₁}
+    (hf : ∀ x ∈ p, f x ∈ q) (x : p) : f.restrict hf x = ⟨f x, hf x.1 x.2⟩ :=
+  rfl
+#align linear_map.restrict_apply LinearMap.restrict_apply
+
+theorem subtype_comp_restrict {f : M →ₗ[R] M₁} {p : Submodule R M} {q : Submodule R M₁}
+    (hf : ∀ x ∈ p, f x ∈ q) : q.subtype.comp (f.restrict hf) = f.domRestrict p :=
+  rfl
+#align linear_map.subtype_comp_restrict LinearMap.subtype_comp_restrict
+
+theorem restrict_eq_codRestrict_domRestrict {f : M →ₗ[R] M₁} {p : Submodule R M}
+    {q : Submodule R M₁} (hf : ∀ x ∈ p, f x ∈ q) :
+    f.restrict hf = (f.domRestrict p).codRestrict q fun x => hf x.1 x.2 :=
+  rfl
+#align linear_map.restrict_eq_cod_restrict_dom_restrict LinearMap.restrict_eq_codRestrict_domRestrict
+
+theorem restrict_eq_domRestrict_codRestrict {f : M →ₗ[R] M₁} {p : Submodule R M}
+    {q : Submodule R M₁} (hf : ∀ x, f x ∈ q) :
+    (f.restrict fun x _ => hf x) = (f.codRestrict q hf).domRestrict p :=
+  rfl
+#align linear_map.restrict_eq_dom_restrict_cod_restrict LinearMap.restrict_eq_domRestrict_codRestrict
+
+instance uniqueOfLeft [Subsingleton M] : Unique (M →ₛₗ[σ₁₂] M₂) :=
+  { inferInstanceAs (Inhabited (M →ₛₗ[σ₁₂] M₂)) with
+    uniq := fun f => ext fun x => by rw [Subsingleton.elim x 0, map_zero, map_zero] }
+#align linear_map.unique_of_left LinearMap.uniqueOfLeft
+
+instance uniqueOfRight [Subsingleton M₂] : Unique (M →ₛₗ[σ₁₂] M₂) :=
+  coe_injective.unique
+#align linear_map.unique_of_right LinearMap.uniqueOfRight
+
+/-- Evaluation of a `σ₁₂`-linear map at a fixed `a`, as an `AddMonoidHom`. -/
+def evalAddMonoidHom (a : M) : (M →ₛₗ[σ₁₂] M₂) →+ M₂ where
+  toFun f := f a
+  map_add' f g := LinearMap.add_apply f g a
+  map_zero' := rfl
+#align linear_map.eval_add_monoid_hom LinearMap.evalAddMonoidHom
+
+/-- `LinearMap.toAddMonoidHom` promoted to a `AddMonoidHom` -/
+def toAddMonoidHom' : (M →ₛₗ[σ₁₂] M₂) →+ M →+ M₂ where
+  toFun := toAddMonoidHom
+  map_zero' := by ext; rfl
+  map_add' := by intros; ext; rfl
+#align linear_map.to_add_monoid_hom' LinearMap.toAddMonoidHom'
+
+theorem sum_apply (t : Finset ι) (f : ι → M →ₛₗ[σ₁₂] M₂) (b : M) :
+    (∑ d in t, f d) b = ∑ d in t, f d b :=
+  _root_.map_sum ((AddMonoidHom.eval b).comp toAddMonoidHom') f _
+#align linear_map.sum_apply LinearMap.sum_apply
+
+section SMulRight
+
+variable [Semiring S] [Module R S] [Module S M] [IsScalarTower R S M]
+
+/-- When `f` is an `R`-linear map taking values in `S`, then `fun ↦ b, f b • x` is an `R`-linear
+map. -/
+def smulRight (f : M₁ →ₗ[R] S) (x : M) : M₁ →ₗ[R] M where
+  toFun b := f b • x
+  map_add' x y := by dsimp only; rw [f.map_add, add_smul]
+  map_smul' b y := by dsimp; rw [map_smul, smul_assoc]
+#align linear_map.smul_right LinearMap.smulRight
+
+@[simp]
+theorem coe_smulRight (f : M₁ →ₗ[R] S) (x : M) : (smulRight f x : M₁ → M) = fun c => f c • x :=
+  rfl
+#align linear_map.coe_smul_right LinearMap.coe_smulRight
+
+theorem smulRight_apply (f : M₁ →ₗ[R] S) (x : M) (c : M₁) : smulRight f x c = f c • x :=
+  rfl
+#align linear_map.smul_right_apply LinearMap.smulRight_apply
+
+end SMulRight
+
+instance [Nontrivial M] : Nontrivial (Module.End R M) := by
+  obtain ⟨m, ne⟩ := exists_ne (0 : M)
+  exact nontrivial_of_ne 1 0 fun p => ne (LinearMap.congr_fun p m)
+
+@[simp, norm_cast]
+theorem coeFn_sum {ι : Type*} (t : Finset ι) (f : ι → M →ₛₗ[σ₁₂] M₂) :
+    ⇑(∑ i in t, f i) = ∑ i in t, (f i : M → M₂) :=
+  _root_.map_sum
+    (show AddMonoidHom (M →ₛₗ[σ₁₂] M₂) (M → M₂)
+      from { toFun := FunLike.coe,
+             map_zero' := rfl
+             map_add' := fun _ _ => rfl }) _ _
+#align linear_map.coe_fn_sum LinearMap.coeFn_sum
+
+theorem submodule_pow_eq_zero_of_pow_eq_zero {N : Submodule R M} {g : Module.End R N}
+    {G : Module.End R M} (h : G.comp N.subtype = N.subtype.comp g) {k : ℕ} (hG : G ^ k = 0) :
+    g ^ k = 0 := by
+  ext m
+  have hg : N.subtype.comp (g ^ k) m = 0 := by
+    rw [← commute_pow_left_of_commute h, hG, zero_comp, zero_apply]
+  simpa using hg
+#align linear_map.submodule_pow_eq_zero_of_pow_eq_zero LinearMap.submodule_pow_eq_zero_of_pow_eq_zero
+
+section
+
+variable {f' : M →ₗ[R] M}
+
+theorem pow_apply_mem_of_forall_mem {p : Submodule R M} (n : ℕ) (h : ∀ x ∈ p, f' x ∈ p) (x : M)
+    (hx : x ∈ p) : (f' ^ n) x ∈ p := by
+  induction' n with n ih generalizing x
+  · simpa
+  · simpa only [iterate_succ, coe_comp, Function.comp_apply, restrict_apply] using ih _ (h _ hx)
+#align linear_map.pow_apply_mem_of_forall_mem LinearMap.pow_apply_mem_of_forall_mem
+
+theorem pow_restrict {p : Submodule R M} (n : ℕ) (h : ∀ x ∈ p, f' x ∈ p)
+    (h' := pow_apply_mem_of_forall_mem n h) :
+    (f'.restrict h) ^ n = (HPow.hPow f' n).restrict h' := by
+  ext x
+  have : Semiconj (↑) (f'.restrict h) f' := fun _ ↦ restrict_coe_apply _ _ _
+  simp [coe_pow, this.iterate_right _ _]
+#align linear_map.pow_restrict LinearMap.pow_restrict
+
+end
+
+end AddCommMonoid
+
+end LinearMap
+
+end
