eec55af474ea5763bc724b24779cba53b23d7d2d
Eric Wieser <wieser.eric@gmail.com>
2023-09-16 17:31:03 +0000

    feat: base change of Clifford algebras (#6778)
    The main result here is `CliffordAlgebra (Q.baseChange A) ≃ₐ[A] A ⊗[R] CliffordAlgebra Q`; that is, the `A ⊗[R]` can be moved from `_ : QuadraticForm A (A ⊗[R] _)` to the outside and vice versa.

--- Mathlib.lean
+++ Mathlib.lean
@@ -2213,6 +2213,7 @@ import Mathlib.LinearAlgebra.BilinearForm.TensorProduct
 import Mathlib.LinearAlgebra.BilinearMap
 import Mathlib.LinearAlgebra.Charpoly.Basic
 import Mathlib.LinearAlgebra.Charpoly.ToMatrix
+import Mathlib.LinearAlgebra.CliffordAlgebra.BaseChange
 import Mathlib.LinearAlgebra.CliffordAlgebra.Basic
 import Mathlib.LinearAlgebra.CliffordAlgebra.Conjugation
 import Mathlib.LinearAlgebra.CliffordAlgebra.Contraction
