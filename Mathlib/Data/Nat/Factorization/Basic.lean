78634da759b0d5d048bea5979849d556d1b79ba5
Eric Rodriguez <ericrboidi@gmail.com>
2023-09-16 16:04:59 +0100

    turn everything into letIs to make defn expansion better

--- Mathlib/Data/Nat/Factorization/Basic.lean
+++ Mathlib/Data/Nat/Factorization/Basic.lean
@@ -798,11 +798,11 @@ def recOnPrimePow {P : ℕ → Sort*} (h0 : P 0) (h1 : P 1)
     | 0 => fun _ => h0
     | 1 => fun _ => h1
     | k + 2 => fun hk => by
-      let p := (k + 2).minFac
-      have hp : Prime p := minFac_prime (succ_succ_ne_one k)
-      let t := (k + 2).factorization p
-      have hpt : p ^ t ∣ k + 2 := ord_proj_dvd _ _
-      have htp : 0 < t := hp.factorization_pos_of_dvd (k + 1).succ_ne_zero (k + 2).minFac_dvd
+      letI p := (k + 2).minFac
+      haveI hp : Prime p := minFac_prime (succ_succ_ne_one k)
+      letI t := (k + 2).factorization p
+      haveI hpt : p ^ t ∣ k + 2 := ord_proj_dvd _ _
+      haveI htp : 0 < t := hp.factorization_pos_of_dvd (k + 1).succ_ne_zero (k + 2).minFac_dvd
       convert h ((k + 2) / p ^ t) p t hp _ htp (hk _ (Nat.div_lt_of_lt_mul _)) using 1
       · rw [Nat.mul_div_cancel' hpt]
       · rw [Nat.dvd_div_iff hpt, ← pow_succ]
