5b04a2e0b346842e09881e9f401db7f618a1074c
Scott Carnahan <128885296+ScottCarnahan@users.noreply.github.com>
2023-09-17 12:26:49 +0900

    Update Binomial.lean
    redefined pochEval, some other reviewer recs.

--- Mathlib/RingTheory/Binomial.lean
+++ Mathlib/RingTheory/Binomial.lean
@@ -13,10 +13,10 @@ import Mathlib.RingTheory.Polynomial.Pochhammer
 In this file we introduce binomial semirings and binomial rings, together with related functions
 like binomial coefficients.
 
-According to our main reference [Elliott] (which lists many equivalent conditions), a binomial ring
+According to our main reference [elliott2006binomial] (which lists many equivalent conditions), a binomial ring
 is a torsion-free commutative ring R such that for any `x ∈ R` and any `k ∈ ℕ`, the product
-`x(x-1)⋯(x-k+1)` is divisible by `k!`.  The torsion-free condition lets us divide by `k!`
-unambiguously, so we get uniquely defined binomial coefficients.
+`x(x-1)⋯(x-k+1)` is divisible by `k!`.  The torsion-free condition lets us divide by `k!` unambiguously, so we get
+uniquely defined binomial coefficients.
 
 The defining condition doesn't require commutativity, and we get a theory with essentially the same
 power by replacing subtraction with addition.  Thus, we consider a semiring R in which
@@ -29,96 +29,109 @@ The quotient is called `multichoose r k`, following the convention given for nat
 
 -/
 
-namespace Ring
 
 universe u
 
 variable {R: Type u}
 
-section Semiring
+section Semiring 
 
 variable [Semiring R]
 
-/-- pochEval directly evaluates the n-th Pochhammer polynomial at an element r. -/
-def pochEval (r : R) : ℕ → R
-  | 0 => 1
-  | (k + 1) => (r + k) * (pochEval r k)
+namespace Ring
 
-theorem pochEval_zero (r : R) :
-  pochEval r 0 = 1 := rfl
+/-- `ascPochEval` directly evaluates the `n`-th ascending Pochhammer polynomial at an element `r`. -/
+def ascPochEval (r : R) (n : ℕ) : R :=
+  Polynomial.eval r (ascPochhammer R n)
+--  | 0 => 1
+--  | (k + 1) => (r + k) * (ascPochEval r k)
 
-theorem pochEval_succ (r : R) (k : ℕ) :
-  pochEval r (k.succ) = (r + k) * (pochEval r k) := by rfl
+theorem ascPochEval_zero (r : R) :
+  ascPochEval r 0 = 1 := by
+    unfold ascPochEval
+    rw [ascpochhammer_zero, Polynomial.eval_one]
 
-theorem pochEval_cast (n : ℕ) : ∀ (k : ℕ), pochEval (n : R) k = (pochEval n k)
-| 0 => by rw [pochEval_zero, pochEval_zero, Nat.cast_one]
+theorem ascPochEval_succ (r : R) (k : ℕ) :
+  ascPochEval r (k.succ) = (ascPochEval r k) * (r + k) := by
+    unfold ascPochEval
+    rw [ascpochhammer_succ_eval]
+
+theorem ascPochEval_cast (n : ℕ) : ∀ (k : ℕ), ascPochEval (n : R) k = (ascPochEval n k)
+| 0 => by rw [ascPochEval_zero, ascPochEval_zero, Nat.cast_one]
 | (k + 1) => by
-  rw [pochEval_succ, pochEval_succ, Nat.cast_mul, pochEval_cast n k, Nat.cast_add]
+  rw [ascPochEval_succ, ascPochEval_succ, Nat.cast_mul, ascPochEval_cast n k, Nat.cast_add]
   norm_cast
 
-theorem Commute.mem_comm_translate (r s : R) (n : ℕ) (h: Commute r s) : Commute r (s + n) := by
-  refine Commute.add_right h (Nat.commute_cast r n)
+end Ring
 
-theorem Commute.translate_comm_mem (r s : R) (n : ℕ) (h : Commute r s) : Commute (r + n) s := by
-  refine Commute.add_left h (Nat.cast_commute n s)
+namespace Commute
 
-theorem Commute.translates_comm (r s : R) (k n : ℕ) (h : Commute r s): Commute (r + k) (s + n) := by
-  refine Commute.add_right ?_ ?_
-  refine Commute.translate_comm_mem r s k h
-  exact Nat.commute_cast (r+k) n
+theorem mem_comm_translate (r s : R) (n : ℕ) (h: Commute r s) : Commute r (s + n) :=
+  h.add_right (Nat.commute_cast r n)
 
-theorem Commute.translate_comm_pochEval (r s : R) (k : ℕ) (h : Commute r s) : ∀ (n : ℕ),
-  Commute (r + k) (pochEval s n)
+theorem translate_comm_mem (r s : R) (n : ℕ) (h : Commute r s) : Commute (r + n) s :=
+  h.add_left (Nat.cast_commute n s)
+
+theorem translates_comm (r s : R) (k n : ℕ) (h : Commute r s): Commute (r + k) (s + n) :=
+  (translate_comm_mem r s k h).add_right (Nat.commute_cast (r+k) n)
+
+theorem translate_comm_ascPochEval (r s : R) (k : ℕ) (h : Commute r s) : ∀ (n : ℕ),
+  Commute (r + k) (Ring.ascPochEval s n)
 | 0 => by
-  rw [pochEval_zero]
-  exact Commute.one_right (r + ↑k)
+  rw [Ring.ascPochEval_zero]
+  exact one_right (r + ↑k)
 | (n + 1) => by
-  rw [pochEval_succ]
-  refine Commute.mul_right ?_ ?_
-  refine Commute.translates_comm r s k n h
-  exact Commute.translate_comm_pochEval r s k h n
+  rw [Ring.ascPochEval_succ]
+  exact (translate_comm_ascPochEval r s k h n).mul_right (translates_comm r s k n h)
 
-theorem pochEval_eq_pochhammer_eval (r : R) :
-  ∀ (k : ℕ), pochEval r k = Polynomial.eval r (pochhammer R k)
-| 0 => by rw [pochhammer_zero, Polynomial.eval_one, pochEval_zero]
-| (k + 1) => by
-  rw [pochhammer_succ_eval, ← pochEval_eq_pochhammer_eval r k, pochEval_succ,
-    Commute.translate_comm_pochEval]
-  rfl
+end Commute
 
-theorem pochEval_mul (r : R) (n : ℕ) : ∀ (k : ℕ),
-  pochEval r (n + k) = pochEval (r + n) k * pochEval r n
-| 0=> by rw [add_zero, pochEval_zero, one_mul]
+namespace Ring
+
+theorem ascPochEval_add_right (r : R) (n : ℕ) : ∀ (k : ℕ),
+  ascPochEval r (n + k) = ascPochEval r n * ascPochEval (r + n) k 
+| 0 => by rw [add_zero, ascPochEval_zero, mul_one]
 | (k + 1) => by
-  rw [← add_assoc, pochEval_succ, pochEval_mul r n k, pochEval_succ, mul_assoc, Nat.cast_add,
+  rw [← add_assoc, ascPochEval_succ, ascPochEval_add_right r n k, ascPochEval_succ, mul_assoc, Nat.cast_add,
     add_assoc]
+
+end Ring
 
 end Semiring
 
-/-- A semiring is binomial if multiplication by factorials is injective and pochhammer evaluations
+section BinomialSemiring
+
+namespace Ring
+
+/-- A semiring is binomial if multiplication by nonzero natural numbers is injective and pochhammer evaluations
 are divisible by the corresponding factorial. -/
 class BinomialSemiring (R: Type u) extends Semiring R where
-  /-- Multiplication by factorials is injective -/
-  inj_smul_factorial : ∀ (n : ℕ) (r s : R), n.factorial * r = n.factorial * s → r = s
+  /-- Multiplication by positive integers is injective -/
+  inj_smul_pos (n : ℕ) (r s : R) (h: n ≠ 0) : n * r = n * s → r = s
   /-- The multichoose function witnesses the divisibility of pochhammer n (evaluated at r) by n! -/
   multichoose : R → ℕ → R
   /-- pochhammer n (evaluated at r) is divisible by n! (witnessed by multichoose) -/
-  factorial_mul_multichoose : ∀ (r : R) (n : ℕ), n.factorial * multichoose r n = pochEval r n
+  factorial_mul_multichoose : ∀ (r : R) (n : ℕ), n.factorial * multichoose r n = ascPochEval r n
 
-theorem inj_smul_factorial [BinomialSemiring R] :  ∀ (n : ℕ) (r s : R),
-  n.factorial * r = n.factorial * s → r = s := BinomialSemiring.inj_smul_factorial
+theorem inj_smul_pos [BinomialSemiring R] (n : ℕ) (r s : R) (h: n ≠ 0) :
+  n * r = n * s → r = s := BinomialSemiring.inj_smul_pos n r s h
+
+--theorem inj_smul_factorial [BinomialSemiring R] (n : ℕ) (r s : R) : 
+--  n.factorial * r = n.factorial * s → r = s := inj_smul_pos n.factorial r s (Nat.factorial_ne_zero n)
 
 theorem eq_of_mul_eq_mul_factorial [BinomialSemiring R] {r s : R} (n : ℕ)
-    (h:n.factorial * r = n.factorial * s) : r = s := by
-  exact inj_smul_factorial n r s h
+    (h : n.factorial * r = n.factorial * s) : r = s := by
+    apply inj_smul_pos n.factorial r s (Nat.factorial_ne_zero n) h
 
 /-- This is a generalization of the combinatorial multichoose function, given by choosing with
 replacement. -/
-def multichoose [BinomialSemiring R] : R → ℕ → R :=
-  λ r n => BinomialSemiring.multichoose r n
+def multichoose [BinomialSemiring R] (r : R) (n : ℕ) : R :=
+  BinomialSemiring.multichoose r n
 
-theorem factorial_mul_multichoose_eq_pochEval [BinomialSemiring R] {r : R} (n : ℕ) :
-    n.factorial * multichoose r n = pochEval r n :=
+theorem factorial_mul_multichoose_eq_ascPochEval [BinomialSemiring R] (r : R) (n : ℕ) :
+    n.factorial * multichoose r n = ascPochEval r n :=
   BinomialSemiring.factorial_mul_multichoose r n
 
 end Ring
+
+end BinomialSemiring
