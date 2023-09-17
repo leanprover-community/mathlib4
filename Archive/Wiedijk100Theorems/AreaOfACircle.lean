e3189b818b18bb6e07d40d9e8423edef92c25c4b
Patrick Massot <patrickmassot@free.fr>
2023-09-17 17:22:52 -0400

    One more fix

--- Archive/Wiedijk100Theorems/AreaOfACircle.lean
+++ Archive/Wiedijk100Theorems/AreaOfACircle.lean
@@ -114,6 +114,7 @@ theorem area_disc : volume (disc r) = NNReal.pi * r ^ 2 := by
     · have h₁ : (r:ℝ) ^ 2 - x ^ 2 > 0 := sub_pos_of_lt (sq_lt_sq' hx1 hx2)
       have h : sqrt ((r:ℝ) ^ 2 - x ^ 2) ^ 3 = ((r:ℝ) ^ 2 - x ^ 2) * sqrt ((r: ℝ) ^ 2 - x ^ 2) := by
         rw [pow_three, ← mul_assoc, mul_self_sqrt (by positivity)]
+      unfold_let f
       field_simp
       ring_nf
       rw [h]
