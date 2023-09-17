b9f16d238add76317866dd871da6c6b608ad3848
Jujian Zhang <jujian.zhang1998@outlook.com>
2023-09-17 14:34:44 +0100

    fix

--- Mathlib/Algebra/MonoidAlgebra/Basic.lean
+++ Mathlib/Algebra/MonoidAlgebra/Basic.lean
@@ -1200,10 +1200,10 @@ instance coeFun : CoeFun k[G] fun _ => G → k :=
 
 end AddMonoidAlgebra
 
-instance instIsCancelAdd [IsCancelAdd k] [AddZeroClass G] [IsCancelAdd G] :
+protected instance instIsCancelAdd [IsCancelAdd k] [AddZeroClass G] [IsCancelAdd G] :
     IsCancelAdd (AddMonoidAlgebra k G) := by
   delta AddMonoidAlgebra
-  exact Finsupp.IsCancelAdd (α := G) (β := k)
+  exact Finsupp.instIsCancelAdd (α := G) (β := k)
 end
 
 namespace AddMonoidAlgebra
