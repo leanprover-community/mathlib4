bfaffcf10e6d088ae7c1c22462cbd7ae46f6c358
Scott Morrison <scott@tqft.net>
2023-09-17 11:27:10 +0000

    chore: bump to v4.1.0-rc1 (2nd attempt) (#7216)
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>
    Co-authored-by: Scott Morrison <scott.morrison@gmail.com>

--- lakefile.lean
+++ lakefile.lean
@@ -29,8 +29,11 @@ lean_lib Mathlib where
   moreLeanArgs := moreLeanArgs
   weakLeanArgs := weakLeanArgs
 
-lean_exe runLinter where
-  root := `scripts.runLinter
+-- Due to a change in Lake at v4.1.0-rc1, we need to give this a different name
+-- than the `lean_exe runLinter` inherited from Std, or we always run that.
+-- See https://github.com/leanprover/lean4/issues/2548
+lean_exe runMathlibLinter where
+  root := `scripts.runMathlibLinter
   supportInterpreter := true
 
 lean_exe checkYaml where
