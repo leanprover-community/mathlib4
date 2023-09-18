e59ec7dc9e12713b4451b7cc130634bde130d4d0
Eric Wieser <wieser.eric@gmail.com>
2023-09-18 21:57:56 +0000

    cursed hearbeats

--- Mathlib/LinearAlgebra/QuadraticForm/QuadraticModuleCat/Monoidal.lean
+++ Mathlib/LinearAlgebra/QuadraticForm/QuadraticModuleCat/Monoidal.lean
@@ -16,26 +16,58 @@ open CategoryTheory
 
 universe v u
 
-variable (R : Type u) [CommRing R] [Invertible (2 : R)]
+variable {R : Type u} [CommRing R] [Invertible (2 : R)]
 
 namespace QuadraticModuleCat
 
 open QuadraticForm
 
+namespace instMonoidalCategory
+
+/-- Auxiliary definition used to fight a tmieout when building
+`QuadraticModuleCat.instMonoidalCategory`. -/
+@[simps! form]
+def tensorObj (X Y : QuadraticModuleCat.{u} R) : QuadraticModuleCat.{u} R :=
+  of (X.form.tmul Y.form)
+
+/-- Auxiliary definition used to fight a tmieout when building
+`QuadraticModuleCat.instMonoidalCategory`. -/
+def tensorHom {W X Y Z : QuadraticModuleCat.{u} R} (f : W ⟶ X) (g : Y ⟶ Z) :
+    tensorObj W Y ⟶ tensorObj X Z :=
+  ⟨(f.toIsometry.tmul g.toIsometry :)⟩
+
+/-- Auxiliary definition used to fight a tmieout when building
+`QuadraticModuleCat.instMonoidalCategory`. -/
+@[simps! form]
+def tensorUnit : QuadraticModuleCat.{u} R := of (sq (R := R))
+
+count_hearbeats in
+/-- Auxiliary definition used to fight a tmieout when building
+`QuadraticModuleCat.instMonoidalCategory`. -/
+def associator (X Y Z : QuadraticModuleCat.{u} R) :
+    tensorObj (tensorObj X Y) Z ≅ tensorObj X (tensorObj Y Z) := by
+  refine ofIso ?_
+  dsimp
+  have := tensorAssoc X.form Y.form Z.form
+  exact this
+#check MonoidalCategory.associator
+
+#exit
+
 instance instMonoidalCategory : MonoidalCategory (QuadraticModuleCat.{u} R) :=
   Monoidal.induced
     (F := forget₂ (QuadraticModuleCat R) (ModuleCat R))
-    (tensorObj := fun X Y => of (X.form.tmul Y.form))
+    (tensorObj := instMonoidalCategory.tensorObj)
     (μIsoSymm := fun X Y => eqToIso rfl)
-    (whiskerLeft := fun X _ _ f => ⟨(Isometry.id X.form).tmul f.toIsometry⟩)
+    (whiskerLeft := fun X _ _ f => tensorHom (𝟙 _) f)
     (whiskerLeft_eq := sorry)
-    (whiskerRight := @fun X₁ X₂ (f : X₁ ⟶ X₂) Y => ⟨f.toIsometry.tmul (Isometry.id Y.form)⟩)
+    (whiskerRight := @fun X₁ X₂ (f : X₁ ⟶ X₂) Y => tensorHom f (𝟙 _))
     (whiskerRight_eq := sorry)
-    (tensorHom := fun f g => ⟨f.toIsometry.tmul g.toIsometry⟩)
+    (tensorHom := tensorHom)
     (tensorHom_eq := sorry)
-    (tensorUnit' := of (sq (R := R)))
+    (tensorUnit' := tensorUnit)
     (εIsoSymm := eqToIso rfl)
-    (associator := fun X Y Z => ofIso (tensorAssoc X.form Y.form Z.form))
+    (associator := associator)
     (associator_eq := sorry)
     (leftUnitor := fun X => ofIso (tensorLId X.form))
     (leftUnitor_eq := sorry)
