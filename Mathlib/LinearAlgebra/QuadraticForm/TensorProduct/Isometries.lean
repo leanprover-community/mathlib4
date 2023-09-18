29e62cb8cdb40284d11fc9372ad2f94726a04b15
Eric Wieser <wieser.eric@gmail.com>
2023-09-18 00:48:26 +0000

    Add map too

--- Mathlib/LinearAlgebra/QuadraticForm/TensorProduct/Isometries.lean
+++ Mathlib/LinearAlgebra/QuadraticForm/TensorProduct/Isometries.lean
@@ -13,22 +13,63 @@ These results are separate from the definition of `QuadraticForm.tmul` as that f
 
 ## Main definitions
 
+* `QuadraticForm.Isometry.tmul`: `TensorProduct.map` as a `QuadraticForm.Isometry`
 * `QuadraticForm.tensorComm`: `TensorProduct.comm` as a `QuadraticForm.IsometryEquiv`
 * `QuadraticForm.tensorAssoc`: `TensorProduct.assoc` as a `QuadraticForm.IsometryEquiv`
 * `QuadraticForm.tensorRId`: `TensorProduct.rid` as a `QuadraticForm.IsometryEquiv`
 * `QuadraticForm.tensorLId`: `TensorProduct.lid` as a `QuadraticForm.IsometryEquiv`
 -/
 
-universe uR uM₁ uM₂ uM₃
-variable {R : Type uR} {M₁ : Type uM₁} {M₂ : Type uM₂} {M₃ : Type uM₃}
+universe uR uM₁ uM₂ uM₃ uM₄
+variable {R : Type uR} {M₁ : Type uM₁} {M₂ : Type uM₂} {M₃ : Type uM₃} {M₄ : Type uM₄}
 
 open scoped TensorProduct
 
 namespace QuadraticForm
 
 variable [CommRing R]
-variable [AddCommGroup M₁] [AddCommGroup M₂] [AddCommGroup M₃]
-variable [Module R M₁] [Module R M₂] [Module R M₃] [Invertible (2 : R)]
+variable [AddCommGroup M₁] [AddCommGroup M₂] [AddCommGroup M₃] [AddCommGroup M₄]
+variable [Module R M₁] [Module R M₂] [Module R M₃] [Module R M₄] [Invertible (2 : R)]
+
+@[simp]
+theorem tmul_comp_tensorMap
+    {Q₁ : QuadraticForm R M₁} {Q₂ : QuadraticForm R M₂}
+    {Q₃ : QuadraticForm R M₃} {Q₄ : QuadraticForm R M₄}
+    (f : Q₁ →qᵢ Q₂) (g : Q₃ →qᵢ Q₄) :
+    (Q₂.tmul Q₄).comp (TensorProduct.map f.toLinearMap g.toLinearMap) = Q₁.tmul Q₃ := by
+  have h₁ : Q₁ = Q₂.comp f.toLinearMap := QuadraticForm.ext fun x => (f.map_app x).symm
+  have h₃ : Q₃ = Q₄.comp g.toLinearMap := QuadraticForm.ext fun x => (g.map_app x).symm
+  refine (QuadraticForm.associated_rightInverse R).injective <| BilinForm.toLin.injective ?_
+  ext m₁ m₃ m₁' m₃'
+  simp [-associated_apply, h₁, h₃, associated_tmul]
+
+@[simp]
+theorem tmul_tensorMap_apply
+    {Q₁ : QuadraticForm R M₁} {Q₂ : QuadraticForm R M₂}
+    {Q₃ : QuadraticForm R M₃} {Q₄ : QuadraticForm R M₄}
+    (f : Q₁ →qᵢ Q₂) (g : Q₃ →qᵢ Q₄) (x : M₁ ⊗[R] M₃) :
+    Q₂.tmul Q₄ (TensorProduct.map f.toLinearMap g.toLinearMap x) = Q₁.tmul Q₃ x :=
+  FunLike.congr_fun (tmul_comp_tensorMap f g) x
+
+namespace Isometry
+
+/-- `TensorProduct.map` for `Quadraticform.Isometry`s -/
+def tmul
+    {Q₁ : QuadraticForm R M₁} {Q₂ : QuadraticForm R M₂}
+    {Q₃ : QuadraticForm R M₃} {Q₄ : QuadraticForm R M₄}
+    (f : Q₁ →qᵢ Q₂) (g : Q₃ →qᵢ Q₄) : (Q₁.tmul Q₃) →qᵢ (Q₂.tmul Q₄) where
+  toLinearMap := TensorProduct.map f.toLinearMap g.toLinearMap
+  map_app' := tmul_tensorMap_apply f g
+
+@[simp]
+theorem tmul_apply
+    {Q₁ : QuadraticForm R M₁} {Q₂ : QuadraticForm R M₂}
+    {Q₃ : QuadraticForm R M₃} {Q₄ : QuadraticForm R M₄}
+    (f : Q₁ →qᵢ Q₂) (g : Q₃ →qᵢ Q₄) (x : M₁ ⊗[R] M₃) :
+    f.tmul g x = TensorProduct.map f.toLinearMap g.toLinearMap x :=
+  rfl
+
+end Isometry
 
 section tensorComm
 
