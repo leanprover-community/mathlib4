dd76134794663728f5b62400ce58eb959aea1919
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2023-09-18 03:18:56 -0400

    variable names in sobolev file

--- Mathlib/MeasureTheory/Integral/SobolevInequality.lean
+++ Mathlib/MeasureTheory/Integral/SobolevInequality.lean
@@ -78,18 +78,18 @@ local prefix:max "#" => Fintype.card
 
 /-! ## The grid-lines lemma -/
 
-variable {π : ι → Type*} [∀ i, MeasurableSpace (π i)]
-  (μ : ∀ i, Measure (π i)) [∀ i, SigmaFinite (μ i)]
+variable {A : ι → Type*} [∀ i, MeasurableSpace (A i)]
+  (μ : ∀ i, Measure (A i)) [∀ i, SigmaFinite (μ i)]
 
 namespace GridLines
 
 /-- The "grid-lines operation" (not a standard name) which is central in the inductive proof of the
 Sobolev inequality.
 
-For a finite dependent product `Π i : ι, π i` of sigma-finite measure spaces, a finite set `s` of
+For a finite dependent product `Π i : ι, A i` of sigma-finite measure spaces, a finite set `s` of
 indices from `ι`, and a (later assumed nonnegative) real number `p`, this operation acts on a
-function `f` from `Π i, π i` into the extended nonnegative reals.  The operation is to partially
-integrate, in the `s` co-ordinates, the function whose value at `x : Π i, π i` is obtained by
+function `f` from `Π i, A i` into the extended nonnegative reals.  The operation is to partially
+integrate, in the `s` co-ordinates, the function whose value at `x : Π i, A i` is obtained by
 multiplying a certain power of `f` with the product, for each co-ordinate `i` in `s`, of a certain
 power of the integral of `f` along the "grid line" in the `i` direction through `x`.
 
@@ -97,19 +97,19 @@ We are most interested in this operation when the set `s` is the universe in `ι
 "induction on dimension" we define it for a general set `s` of co-ordinates: the `s`-grid-lines
 operation on a function `f` which is constant along the co-ordinates in `sᶜ` is morally (that is, up
 to type-theoretic nonsense) the same thing as the universe-grid-lines operation on the associated
-function on the "lower-dimensional" space `Π i : s, π i`. -/
-def T (p : ℝ) (f : (∀ i, π i) → ℝ≥0∞) (s : Finset ι) : (∀ i, π i) → ℝ≥0∞ :=
+function on the "lower-dimensional" space `Π i : s, A i`. -/
+def T (p : ℝ) (f : (∀ i, A i) → ℝ≥0∞) (s : Finset ι) : (∀ i, A i) → ℝ≥0∞ :=
   ∫⋯∫_s, f ^ (1 - (s.card - 1 : ℝ) * p) * ∏ i in s, (∫⋯∫_{i}, f ∂μ) ^ p ∂μ
 
 variable {p : ℝ}
 
-@[simp] lemma T_univ (f : (∀ i, π i) → ℝ≥0∞) (x : ∀ i, π i) :
+@[simp] lemma T_univ (f : (∀ i, A i) → ℝ≥0∞) (x : ∀ i, A i) :
     T μ p f univ x
-    = ∫⁻ (x : ∀ i, π i), (f x ^ (1 - (#ι - 1 : ℝ) * p)
-      * ∏ i : ι, (∫⁻ t : π i, f (update x i t) ∂(μ i)) ^ p) ∂(.pi μ) := by
+    = ∫⁻ (x : ∀ i, A i), (f x ^ (1 - (#ι - 1 : ℝ) * p)
+      * ∏ i : ι, (∫⁻ t : A i, f (update x i t) ∂(μ i)) ^ p) ∂(.pi μ) := by
   simp [T, marginal_univ, marginal_singleton, card_univ]
 
-@[simp] lemma T_empty (f : (∀ i, π i) → ℝ≥0∞) (x : ∀ i, π i) :
+@[simp] lemma T_empty (f : (∀ i, A i) → ℝ≥0∞) (x : ∀ i, A i) :
     T μ p f ∅ x = f x ^ (1 + p) := by
   simp [T]
 
@@ -119,19 +119,19 @@ set_option maxHeartbeats 500000 in
 The grid-lines operation `GridLines.T` on a nonnegative function on a finitary product type is
 less than or equal to the grid-lines operation of its partial integral in one co-ordinate
 (the latter intuitively considered as a function on a space "one dimension down"). -/
-theorem T_insert_le_T_marginal_singleton (hp1 : 0 ≤ p) (u : Finset ι) (hp2 : (u.card : ℝ) * p ≤ 1)
-    (i : ι) (hi : i ∉ u) {f : (∀ i, π i) → ℝ≥0∞} (hf : Measurable f) :
-    T μ p f (insert i u) ≤ T μ p (∫⋯∫_{i}, f ∂μ) u := by
-  calc T μ p f (insert i u)
-      = ∫⋯∫_insert i u,
-            f ^ (1 - (u.card : ℝ) * p) * ∏ j in (insert i u), (∫⋯∫_{j}, f ∂μ) ^ p ∂μ := by
+theorem T_insert_le_T_marginal_singleton (hp₀ : 0 ≤ p) (s : Finset ι) (hp : (s.card : ℝ) * p ≤ 1)
+    (i : ι) (hi : i ∉ s) {f : (∀ i, A i) → ℝ≥0∞} (hf : Measurable f) :
+    T μ p f (insert i s) ≤ T μ p (∫⋯∫_{i}, f ∂μ) s := by
+  calc T μ p f (insert i s)
+      = ∫⋯∫_insert i s,
+            f ^ (1 - (s.card : ℝ) * p) * ∏ j in (insert i s), (∫⋯∫_{j}, f ∂μ) ^ p ∂μ := by
           simp_rw [T, card_insert_of_not_mem hi]
           congr!
           push_cast
           ring
-    _ = ∫⋯∫_u, (fun x ↦ ∫⁻ (t : π i),
-            (f (update x i t) ^ (1 - (u.card : ℝ) * p)
-            * ∏ j in (insert i u), (∫⋯∫_{j}, f ∂μ) (update x i t) ^ p)  ∂ (μ i)) ∂μ := by
+    _ = ∫⋯∫_u, (fun x ↦ ∫⁻ (t : A i),
+            (f (update x i t) ^ (1 - (s.card : ℝ) * p)
+            * ∏ j in (insert i s), (∫⋯∫_{j}, f ∂μ) (update x i t) ^ p)  ∂ (μ i)) ∂μ := by
           rw [marginal_insert' _ _ hi]
           · congr! with x t
             simp only [Pi.mul_apply, Pi.pow_apply, Finset.prod_apply]
@@ -139,25 +139,25 @@ theorem T_insert_le_T_marginal_singleton (hp1 : 0 ≤ p) (u : Finset ι) (hp2 : 
             simp only [Pi.mul_apply, Pi.pow_apply, Finset.prod_apply]
             refine (hf.pow_const _).mul <| Finset.measurable_prod _ ?_
             exact fun _ _ ↦ hf.marginal μ |>.pow_const _
-    _ ≤ T μ p (∫⋯∫_{i}, f ∂μ) u := marginal_mono (fun x ↦ ?_)
+    _ ≤ T μ p (∫⋯∫_{i}, f ∂μ) s := marginal_mono (fun x ↦ ?_)
   simp only [Pi.mul_apply, Pi.pow_apply, Finset.prod_apply]
   have hF₁ : ∀ {j : ι}, Measurable fun t ↦ (∫⋯∫_{j}, f ∂μ) (update x i t) :=
     fun {_} ↦ hf.marginal μ |>.comp <| measurable_update _
   have hF₀ : Measurable fun t ↦ f (update x i t) := hf.comp <| measurable_update _
-  let k : ℝ := u.card
-  have hk' : 0 ≤ 1 - k * p := by linarith only [hp2]
+  let k : ℝ := s.card
+  have hk' : 0 ≤ 1 - k * p := by linarith only [hp]
   let X := update x i
   calc ∫⁻ t, f (X t) ^ (1 - k * p)
-          * ∏ j in (insert i u), (∫⋯∫_{j}, f ∂μ) (X t) ^ p ∂ (μ i)
+          * ∏ j in (insert i s), (∫⋯∫_{j}, f ∂μ) (X t) ^ p ∂ (μ i)
       = ∫⁻ t, (∫⋯∫_{i}, f ∂μ) (X t) ^ p * (f (X t) ^ (1 - k * p)
-          * ∏ j in u, ((∫⋯∫_{j}, f ∂μ) (X t) ^ p)) ∂(μ i) := by
+          * ∏ j in s, ((∫⋯∫_{j}, f ∂μ) (X t) ^ p)) ∂(μ i) := by
               -- rewrite integrand so that `(∫⋯∫_insert i s, f ∂μ) ^ p` comes first
               clear_value X
               congr! 2 with t
               simp_rw [prod_insert hi]
               ring_nf
     _ = (∫⋯∫_{i}, f ∂μ) x ^ p *
-          ∫⁻ t, f (X t) ^ (1 - k * p) * ∏ j in u, ((∫⋯∫_{j}, f ∂μ) (X t)) ^ p ∂(μ i) := by
+          ∫⁻ t, f (X t) ^ (1 - k * p) * ∏ j in s, ((∫⋯∫_{j}, f ∂μ) (X t)) ^ p ∂(μ i) := by
               -- pull out this constant factor
               have : ∀ t, (∫⋯∫_{i}, f ∂μ) (X t) = (∫⋯∫_{i}, f ∂μ) x
               · intro t
@@ -168,7 +168,7 @@ theorem T_insert_le_T_marginal_singleton (hp1 : 0 ≤ p) (u : Finset ι) (hp2 : 
               exact (hF₀.pow_const _).mul <| Finset.measurable_prod _ fun _ _ ↦ hF₁.pow_const _
     _ ≤ (∫⋯∫_{i}, f ∂μ) x ^ p *
           ((∫⁻ t, f (X t) ∂μ i) ^ (1 - k * p)
-          * ∏ j in u, (∫⁻ t, (∫⋯∫_{j}, f ∂μ) (X t) ∂μ i) ^ p) := by
+          * ∏ j in s, (∫⁻ t, (∫⋯∫_{j}, f ∂μ) (X t) ∂μ i) ^ p) := by
               -- apply Hölder's inequality
               gcongr
               apply ENNReal.lintegral_mul_prod_norm_pow_le
@@ -178,9 +178,9 @@ theorem T_insert_le_T_marginal_singleton (hp1 : 0 ≤ p) (u : Finset ι) (hp2 : 
               · simp only [sum_const, nsmul_eq_mul]
                 ring
               · exact hk'
-              · exact fun _ _ ↦ hp1
+              · exact fun _ _ ↦ hp₀
     _ = (∫⋯∫_{i}, f ∂μ) x ^ p *
-          ((∫⋯∫_{i}, f ∂μ) x ^ (1 - k * p) * ∏ j in u, (∫⋯∫_{i, j}, f ∂μ) x ^ p) := by
+          ((∫⋯∫_{i}, f ∂μ) x ^ (1 - k * p) * ∏ j in s, (∫⋯∫_{i, j}, f ∂μ) x ^ p) := by
               -- absorb the newly-created integrals into `∫⋯∫`
               dsimp only
               congr! 2
@@ -190,14 +190,14 @@ theorem T_insert_le_T_marginal_singleton (hp1 : 0 ≤ p) (u : Finset ι) (hp2 : 
               · simp only [Finset.mem_singleton, Finset.mem_insert, Finset.mem_compl] at hj ⊢
                 exact fun h ↦ hi (h ▸ hj)
               rw [marginal_insert _ hf hi']
-    _ = (∫⋯∫_{i}, f ∂μ) x ^ (p + (1 - k * p)) *  ∏ j in u, (∫⋯∫_{i, j}, f ∂μ) x ^ p := by
+    _ = (∫⋯∫_{i}, f ∂μ) x ^ (p + (1 - k * p)) *  ∏ j in s, (∫⋯∫_{i, j}, f ∂μ) x ^ p := by
               -- combine two `(∫⋯∫_insert i s, f ∂μ) x` terms
               rw [ENNReal.rpow_add_of_nonneg]
               · ring
-              · exact hp1
+              · exact hp₀
               · exact hk'
-    _ ≤ (∫⋯∫_{i}, f ∂μ) x ^ (1 - (u.card - 1 : ℝ) * p) *
-          ∏ j in u, (∫⋯∫_{j}, (∫⋯∫_{i}, f ∂μ) ∂μ) x ^ p := by
+    _ ≤ (∫⋯∫_{i}, f ∂μ) x ^ (1 - (s.card - 1 : ℝ) * p) *
+          ∏ j in s, (∫⋯∫_{j}, (∫⋯∫_{i}, f ∂μ) ∂μ) x ^ p := by
               -- identify the result with the RHS integrand
               congr! 2 with j hj
               · push_cast
@@ -216,15 +216,15 @@ type indexed by `ι`, and a set `s` in `ι`, consider partially integrating over
 `sᶜ` and performing the "grid-lines operation" (see `GridLines.T`) to the resulting function in the
 variables `s`.  This theorem states that this operation decreases as the number of grid-lines taken
 increases. -/
-theorem T_marginal_antitone (hp1 : 0 ≤ p) (hp2 : (#ι - 1 : ℝ) * p ≤ 1)
-    {f : (∀ i, π i) → ℝ≥0∞} (hf : Measurable f) :
+theorem T_marginal_antitone (hp₀ : 0 ≤ p) (hp : (#ι - 1 : ℝ) * p ≤ 1)
+    {f : (∀ i, A i) → ℝ≥0∞} (hf : Measurable f) :
     Antitone (fun s ↦ T μ p (∫⋯∫_sᶜ, f ∂μ) s) := by
   -- Reformulate (by induction): a function is decreasing on `Finset ι` if it decreases under the
   -- insertion of any element to any set.
   rw [Finset.antitone_iff']
   intro s i hi
   -- apply the lemma designed to encapsulate the inductive step
-  convert T_insert_le_T_marginal_singleton μ hp1 s ?_ i hi (hf.marginal μ) using 2
+  convert T_insert_le_T_marginal_singleton μ hp₀ s ?_ i hi (hf.marginal μ) using 2
   · rw [← marginal_union μ f hf]
     · rw [← insert_compl_insert hi]
       rfl
@@ -232,7 +232,7 @@ theorem T_marginal_antitone (hp1 : 0 ≤ p) (hp2 : (#ι - 1 : ℝ) * p ≤ 1)
     exact mem_insert_self i s
   · -- the main nontrivial point is to check that an exponent `p` satisfying `0 ≤ p` and
     -- `(#ι - 1) * p ≤ 1` is in the valid range for the inductive-step lemma
-    refine le_trans ?_ hp2
+    refine le_trans ?_ hp
     gcongr
     suffices (s.card : ℝ) + 1 ≤ #ι by linarith
     rw [← s.card_add_card_compl]
@@ -246,46 +246,36 @@ end GridLines
 /-- The "grid-lines lemma" (not a standard name), stated with a general parameter `p` as the
 exponent.  Compare with `lintegral_prod_lintegral_pow_le`.
 
-For any finite dependent product `Π i : ι, π i` of sigma-finite measure spaces, for any
-nonnegative real number `p` such that `(#ι - 1) * p ≤ 1`, for any function `f` from `Π i, π i` into
+For any finite dependent product `Π i : ι, A i` of sigma-finite measure spaces, for any
+nonnegative real number `p` such that `(#ι - 1) * p ≤ 1`, for any function `f` from `Π i, A i` into
 the extended nonnegative reals, we consider an associated "grid-lines quantity", the integral of an
-associated function from `Π i, π i` into the extended nonnegative reals.  The value of this function
-at `x : Π i, π i` is obtained by multiplying a certain power of `f` with the product, for each
+associated function from `Π i, A i` into the extended nonnegative reals.  The value of this function
+at `x : Π i, A i` is obtained by multiplying a certain power of `f` with the product, for each
 co-ordinate `i`, of a certain power of the integral of `f` along the "grid line" in the `i`
 direction through `x`.
 
 This lemma bounds the Lebesgue integral of the grid-lines quantity by a power of the Lebesgue
 integral of `f`. -/
-theorem lintegral_mul_prod_lintegral_pow_le {p : ℝ} (hp1 : 0 ≤ p)
-    (hp2 : (#ι - 1 : ℝ) * p ≤ 1) {f} (hf : Measurable f) :
+theorem lintegral_mul_prod_lintegral_pow_le {p : ℝ} (hp₀ : 0 ≤ p)
+    (hp : (#ι - 1 : ℝ) * p ≤ 1) {f : (∀ i : ι, A i) → ℝ≥0∞} (hf : Measurable f) :
     ∫⁻ x, f x ^ (1 - (#ι - 1 : ℝ) * p) * ∏ i, (∫⁻ xᵢ, f (update x i xᵢ) ∂μ i) ^ p ∂.pi μ
     ≤ (∫⁻ x, f x ∂.pi μ) ^ (1 + p) := by
-  cases isEmpty_or_nonempty (∀ i, π i)
+  cases isEmpty_or_nonempty (∀ i, A i)
   · simp_rw [lintegral_of_isEmpty]; refine' zero_le _
-  inhabit ∀ i, π i
+  inhabit ∀ i, A i
   have H : (∅ : Finset ι) ≤ Finset.univ := Finset.empty_subset _
-  simpa [marginal_univ] using GridLines.T_marginal_antitone μ hp1 hp2 hf H default
+  simpa [marginal_univ] using GridLines.T_marginal_antitone μ hp₀ hp hf H default
 
-/-- The "grid-lines lemma" (not a standard name), stated with the extremal exponent
-`p = (#ι - 1)⁻¹`. Compare with `lintegral_mul_prod_lintegral_pow_le`.
-
-For any finite dependent product `Π i : ι, π i` of at least two sigma-finite measure spaces, for any
-function `f` from `Π i, π i` into the extended nonnegative reals, we consider an associated
-"grid-lines quantity", the integral of an associated function from `Π i, π i` into the extended
-nonnegative reals.  The value of this function at `x : Π i, π i` is the product, for each
-co-ordinate `i`, of the `(#ι - 1)⁻¹`-th power of the integral of `f` along the "grid line" in the
-`i` direction through `x`.
-
-This lemma bounds the Lebesgue integral of the grid-lines quantity by the `#ι / (#ι - 1)`-th power
-of the Lebesgue integral of `f`. -/
+/-- Special case of the grid-lines lemma `lintegral_mul_prod_lintegral_pow_le`, taking the extremal
+exponent `p = (#ι - 1)⁻¹`. -/
 theorem lintegral_prod_lintegral_pow_le [Nontrivial ι] {f} (hf : Measurable f) :
     ∫⁻ x, ∏ i, (∫⁻ xᵢ, f (update x i xᵢ) ∂μ i) ^ ((1 : ℝ) / (#ι - 1 : ℝ)) ∂.pi μ
     ≤ (∫⁻ x, f x ∂.pi μ) ^ ((#ι : ℝ) / (#ι - 1 : ℝ)) := by
   have : (1:ℝ) < #ι := by norm_cast; exact Fintype.one_lt_card
   have : (0:ℝ) < #ι - 1 := by linarith
-  have hp1 : 0 ≤ ((1 : ℝ) / (#ι - 1 : ℝ)) := by positivity
-  have hp2 : (#ι - 1 : ℝ) * ((1 : ℝ) / (#ι - 1 : ℝ)) ≤ 1 := by field_simp
-  convert lintegral_mul_prod_lintegral_pow_le μ hp1 hp2 hf using 2 <;> field_simp
+  have hp₀ : 0 ≤ ((1 : ℝ) / (#ι - 1 : ℝ)) := by positivity
+  have hp : (#ι - 1 : ℝ) * ((1 : ℝ) / (#ι - 1 : ℝ)) ≤ 1 := by field_simp
+  convert lintegral_mul_prod_lintegral_pow_le μ hp₀ hp hf using 2 <;> field_simp
 
 /-! ## The Gagliardo-Nirenberg-Sobolev inequality -/
 
