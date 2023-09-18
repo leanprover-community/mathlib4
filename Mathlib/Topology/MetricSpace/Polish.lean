a5d11536d75ec593de15ade6551ecea1c0e8c930
Felix-Weilacher <fweilach@andrew.cmu.edu>
2023-09-18 15:14:03 -0400

    added StandardBorelSpace

--- Mathlib/Topology/MetricSpace/Polish.lean
+++ Mathlib/Topology/MetricSpace/Polish.lean
@@ -125,6 +125,14 @@ instance sigma {ι : Type*} [Countable ι] {E : ι → Type*} [∀ n, Topologica
   inferInstance
 #align polish_space.sigma PolishSpace.sigma
 
+/-- The product of two Polish spaces is Polish. -/
+instance prod [TopologicalSpace α] [PolishSpace α] [TopologicalSpace β] [PolishSpace β] :
+    PolishSpace (α × β) :=
+  letI := upgradePolishSpace α
+  letI := upgradePolishSpace β
+  letI : MetricSpace (α × β) := Prod.metricSpaceMax
+  inferInstance
+
 /-- The disjoint union of two Polish spaces is Polish. -/
 instance sum [TopologicalSpace α] [PolishSpace α] [TopologicalSpace β] [PolishSpace β] :
     PolishSpace (α ⊕ β) :=
