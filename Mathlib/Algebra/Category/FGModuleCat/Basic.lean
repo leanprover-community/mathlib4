1bedd33be9c74cea5a649917d5c7cf591ef23138
Eric Wieser <wieser.eric@gmail.com>
2023-09-18 16:35:31 +0000

    revert

--- Mathlib/Algebra/Category/FGModuleCat/Basic.lean
+++ Mathlib/Algebra/Category/FGModuleCat/Basic.lean
@@ -156,7 +156,7 @@ instance monoidalPredicate_module_finite :
 
 instance : MonoidalCategory (FGModuleCat R) := by
   dsimp [FGModuleCat]
-  show_term infer_instance
+  infer_instance
 
 open MonoidalCategory
 
