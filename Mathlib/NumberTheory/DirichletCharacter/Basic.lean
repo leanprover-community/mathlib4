e419f62d3af8977b7a9a148a73b4dbb91f5e5070
Moritz Firsching <firsching@google.com>
2023-09-19 22:30:19 +0200

    add author

--- Mathlib/NumberTheory/DirichletCharacter/Basic.lean
+++ Mathlib/NumberTheory/DirichletCharacter/Basic.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2023 Ashvni Narayanan. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Ashvni Narayanan, Moritz Firsching
+Authors: Ashvni Narayanan, Moritz Firsching, Michael Stoll
 -/
 import Mathlib.Algebra.Periodic
 import Mathlib.Data.ZMod.Algebra
