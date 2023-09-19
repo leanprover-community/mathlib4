9c4c2c47d5325d5409a18065f3b6f5934ceace29
Richard <r.m.hill@ucl.ac.uk>
2023-09-19 15:10:22 +0100

    corrections found by CI

--- Mathlib/RingTheory/PowerSeries/Comp.lean
+++ Mathlib/RingTheory/PowerSeries/Comp.lean
@@ -4,8 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Richard M. Hill
 -/
 import Mathlib.RingTheory.PowerSeries.Basic
--- import Mathlib.RingTheory.PowerSeries.Truncation_lemmas
-
 /-!
 # Definitions
 
