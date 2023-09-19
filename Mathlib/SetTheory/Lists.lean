e6a3b5fdedffb7c5dce8a2015bba86f98af0324b
Alex J. Best <alex.j.best@gmail.com>
2023-09-19 10:37:34 +0100

    fixes

--- Mathlib/SetTheory/Lists.lean
+++ Mathlib/SetTheory/Lists.lean
@@ -437,6 +437,8 @@ termination_by
 #align lists.subset.decidable Lists.Subset.decidable
 #align lists.mem.decidable Lists.mem.decidable
 
+attribute [nolint nonClassInstance] Lists.Equiv.decidable._unary._mutual
+
 end Decidable
 
 end Lists
