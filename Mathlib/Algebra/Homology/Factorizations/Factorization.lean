f94a8be23d67070bab5af2c07f7cbfafbb72b550
Joël Riou <joel.riou@universite-paris-saclay.fr>
2023-09-19 15:07:25 +0200

    wip

--- Mathlib/Algebra/Homology/Factorizations/Factorization.lean
+++ Mathlib/Algebra/Homology/Factorizations/Factorization.lean
@@ -1,7 +1,7 @@
 import Mathlib.Algebra.Homology.DerivedCategory.Basic
 import Mathlib.CategoryTheory.Abelian.Injective
 
-open CategoryTheory Limits Category Preadditive
+open CategoryTheory Limits Category Preadditive ZeroObject
 
 variable {C : Type*} [Category C] [Abelian C]
 
@@ -13,7 +13,7 @@ def epiWithInjectiveKernel : MorphismProperty C :=
 lemma epiWithInjectiveKernel_iff {X Y : C} (f : X ⟶ Y) :
     epiWithInjectiveKernel f ↔
     ∃ (I : C) (_ : Injective I) (i : I ⟶ X) (s : Y ⟶ X) (q : X ⟶ I)
-      (_ : i ≫ f = 0) (_ : i ≫ q = 𝟙 I)
+      (_ : i ≫ f = 0) (_ : s ≫ q = 0) (_ : i ≫ q = 𝟙 I)
       (_ : s ≫ f = 𝟙 Y), 𝟙 X = q ≫ i + f ≫ s := by
   constructor
   · rintro ⟨_, _⟩
@@ -21,8 +21,8 @@ lemma epiWithInjectiveKernel_iff {X Y : C} (f : X ⟶ Y) :
     have hS : S.Exact := S.exact_of_f_is_kernel (kernelIsKernel f)
     let σ : S.Splitting := ShortComplex.Splitting.ofExactOfRetraction S hS
         (Injective.factorThru (𝟙 _) (kernel.ι f)) (by simp) inferInstance
-    exact ⟨kernel f, inferInstance, kernel.ι f, σ.s, σ.r, by simp, σ.f_r, σ.s_g, σ.id.symm⟩
-  · rintro ⟨I, _, i, s, q, hif, hiq, hsf, H⟩
+    exact ⟨kernel f, inferInstance, kernel.ι f, σ.s, σ.r, by simp, by simp, σ.f_r, σ.s_g, σ.id.symm⟩
+  · rintro ⟨I, _, i, s, q, hif, _, hiq, hsf, H⟩
     have : IsSplitEpi f := ⟨s, hsf⟩
     refine' ⟨inferInstance, _⟩
     have e : I ≅ kernel f :=
@@ -36,6 +36,32 @@ lemma epiWithInjectiveKernel_iff {X Y : C} (f : X ⟶ Y) :
           rw [H, comp_add]
           simp }
     exact Injective.of_iso e inferInstance
+
+instance : (epiWithInjectiveKernel : MorphismProperty C).ContainsIdentities where
+  id_mem' := fun X => by
+    rw [epiWithInjectiveKernel_iff]
+    exact ⟨0, inferInstance, 0, 𝟙 X, 0, by simp, by simp, by simp, by simp⟩
+
+instance : (epiWithInjectiveKernel : MorphismProperty C).IsMultiplicative where
+  stableUnderComposition := fun X Y Z f g hf hg => by
+    rw [epiWithInjectiveKernel_iff] at hf hg ⊢
+    obtain ⟨I, _, i, s, q, hif, hsq, hiq, hsf, H⟩ := hf
+    obtain ⟨J, _, j, t, q', hjg, htq', hjq', htg, H'⟩ := hg
+    refine' ⟨I ⊞ J, inferInstance, biprod.fst ≫ i + biprod.snd ≫ j ≫ s, t ≫ s,
+      q ≫ biprod.inl + f ≫ q' ≫ biprod.inr, _, _, _, _, _⟩
+    · ext
+      · simp [reassoc_of% hif]
+      · simp [reassoc_of% hsf, hjg]
+    · simp [reassoc_of% hsq, reassoc_of% hsf, reassoc_of% htq']
+    · ext
+      · simp [hiq]
+      · simp [reassoc_of% hif]
+      · simp [hsq]
+      · simp [reassoc_of% hsf, hjq']
+    · simp [reassoc_of% hsf, htg]
+    · simp only [comp_add, add_comp, assoc, biprod.inl_fst_assoc, BinaryBicone.inr_fst_assoc, zero_comp,
+        comp_zero, add_zero, biprod.inl_snd_assoc, BinaryBicone.inr_snd_assoc, zero_add]
+      rw [add_assoc, ← comp_add, ← assoc q', ← assoc g, ← add_comp, ← H', id_comp, H]
 
 end CategoryTheory
 
