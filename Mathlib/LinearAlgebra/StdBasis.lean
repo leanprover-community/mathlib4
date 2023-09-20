8f68302c73726c20320556ae95dde19e84d32b0b
Oliver Nash <github@olivernash.org>
2023-09-20 16:49:52 +0100

    Use `LinearMap.range` rather than `Set.range` in statement of `Module.range_piEquiv`.

--- Mathlib/LinearAlgebra/StdBasis.lean
+++ Mathlib/LinearAlgebra/StdBasis.lean
@@ -311,12 +311,13 @@ lemma piEquiv_apply_apply (v : ι → M) (w : ι → R) :
   congr
 
 @[simp] lemma range_piEquiv (v : ι → M) :
-    range (piEquiv ι R M v) = span R (range v) := by
+    LinearMap.range (piEquiv ι R M v) = span R (range v) := by
   ext; simp [mem_span_range_iff_exists_fun, piEquiv_apply_apply]
 
 @[simp] lemma surjective_piEquiv_apply_iff (v : ι → M) :
     Surjective (piEquiv ι R M v) ↔ span R (range v) = ⊤ := by
-  rw [← range_iff_surjective, range_piEquiv, ← top_coe (R := R), SetLike.coe_set_eq]
+  rw [← range_iff_surjective, ← LinearMap.range_coe, range_piEquiv, ← top_coe (R := R),
+    SetLike.coe_set_eq]
 
 end Module
 
