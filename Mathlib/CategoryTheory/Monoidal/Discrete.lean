e397cce8ee20a88a4b322725073d54852494f3e9
Joël Riou <joel.riou@universite-paris-saclay.fr>
2023-09-20 10:34:50 +0200

    fixed the build

--- Mathlib/CategoryTheory/Monoidal/Discrete.lean
+++ Mathlib/CategoryTheory/Monoidal/Discrete.lean
@@ -17,7 +17,7 @@ Multiplicative morphisms induced monoidal functors.
 -/
 
 
-universe u
+universe u u'
 
 open CategoryTheory Discrete MonoidalCategory
 
@@ -43,7 +43,7 @@ instance Discrete.monoidal : MonoidalCategory (Discrete M)
 lemma Discrete.monoidal_tensorUnit_as :
   (𝟙_ (Discrete M)).as = 1 := rfl
 
-variable {M} {N : Type u} [Monoid N]
+variable {M} {N : Type u'} [Monoid N]
 
 /-- A multiplicative morphism between monoids gives a monoidal functor between the corresponding
 discrete monoidal categories.
