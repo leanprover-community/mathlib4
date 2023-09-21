46d554abf8fdd609bf67e8487993467872d05bdb
Matthew Ballard <matt@mrb.email>
2023-09-21 13:27:49 -0400

    make noncomputable

--- Mathlib/LinearAlgebra/DirectSum/Finsupp.lean
+++ Mathlib/LinearAlgebra/DirectSum/Finsupp.lean
@@ -34,8 +34,9 @@ open TensorProduct
 open TensorProduct
 
 open scoped Classical in
+-- `noncomputable` is a performance workaround for mathlib4#7103
 /-- The tensor product of `ι →₀ M` and `κ →₀ N` is linearly equivalent to `(ι × κ) →₀ (M ⊗ N)`. -/
-def finsuppTensorFinsupp (R M N ι κ : Sort _) [CommRing R] [AddCommGroup M] [Module R M]
+noncomputable def finsuppTensorFinsupp (R M N ι κ : Sort _) [CommRing R] [AddCommGroup M] [Module R M]
     [AddCommGroup N] [Module R N] : (ι →₀ M) ⊗[R] (κ →₀ N) ≃ₗ[R] ι × κ →₀ M ⊗[R] N :=
   TensorProduct.congr (finsuppLEquivDirectSum R M ι) (finsuppLEquivDirectSum R N κ) ≪≫ₗ
     ((TensorProduct.directSum R (fun _ : ι => M) fun _ : κ => N) ≪≫ₗ
