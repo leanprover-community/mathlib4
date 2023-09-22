717a0ffe2980df8a855109f44fd4f57166bce3dd
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2023-09-22 09:11:50 +0200

    fix

--- Mathlib/Tactic/SuppressCompilation.lean
+++ Mathlib/Tactic/SuppressCompilation.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2023 Sébastien Gouëzel. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Alex J. Best
+Authors: Alex J. Best, Mac Malone
 -/
 import Lean
 
