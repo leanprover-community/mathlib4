c97e6da04bc2bb4f3eee3b91c971524b34539cf5
Joël Riou <joel.riou@universite-paris-saclay.fr>
2023-09-22 12:58:10 +0000

    feat: the shift on the opposite category (#7324)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1230,6 +1230,7 @@ import Mathlib.CategoryTheory.Sums.Associator
 import Mathlib.CategoryTheory.Sums.Basic
 import Mathlib.CategoryTheory.Thin
 import Mathlib.CategoryTheory.Triangulated.Basic
+import Mathlib.CategoryTheory.Triangulated.Opposite
 import Mathlib.CategoryTheory.Triangulated.Pretriangulated
 import Mathlib.CategoryTheory.Triangulated.Rotate
 import Mathlib.CategoryTheory.Triangulated.TriangleShift
