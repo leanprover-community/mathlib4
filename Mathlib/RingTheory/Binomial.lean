801626b800be36ed194ba0fbfd9891c123503336
Scott Carnahan <128885296+ScottCarnahan@users.noreply.github.com>
2023-09-22 16:23:27 +0900

    Update Binomial.lean
    computable ascPochEval, commutativity thms now superfluous

--- Mathlib/RingTheory/Binomial.lean
+++ Mathlib/RingTheory/Binomial.lean
@@ -13,10 +13,10 @@ import Mathlib.RingTheory.Polynomial.Pochhammer
 In this file we introduce binomial semirings and binomial rings, together with related functions
 like binomial coefficients.
 
-According to our main reference [elliott2006binomial] (which lists many equivalent conditions), a binomial ring
-is a torsion-free commutative ring R such that for any `x ∈ R` and any `k ∈ ℕ`, the product
-`x(x-1)⋯(x-k+1)` is divisible by `k!`.  The torsion-free condition lets us divide by `k!` unambiguously, so we get
-uniquely defined binomial coefficients.
+According to our main reference [elliott2006binomial] (which lists many equivalent conditions), a
+binomial ring is a torsion-free commutative ring R such that for any `x ∈ R` and any `k ∈ ℕ`, the
+product `x(x-1)⋯(x-k+1)` is divisible by `k!`.  The torsion-free condition lets us divide by `k!`
+unambiguously, so we get uniquely defined binomial coefficients.
 
 The defining condition doesn't require commutativity, and we get a theory with essentially the same
 power by replacing subtraction with addition.  Thus, we consider a semiring R in which
@@ -29,32 +29,25 @@ The quotient is called `multichoose r k`, following the convention given for nat
 
 -/
 
-
 universe u
 
 variable {R: Type u}
 
-section Semiring 
+section Semiring
 
 variable [Semiring R]
 
 namespace Ring
 
-/-- `ascPochEval` directly evaluates the `n`-th ascending Pochhammer polynomial at an element `r`. -/
-def ascPochEval (r : R) (n : ℕ) : R :=
-  Polynomial.eval r (ascPochhammer R n)
---  | 0 => 1
---  | (k + 1) => (r + k) * (ascPochEval r k)
+/-- `ascPochEval` directly evaluates the `n`-th ascending Pochhammer polynomial at an element `r`.-/
+def ascPochEval (r : R) : ℕ → R
+  | 0 => 1
+  | (k + 1) => (ascPochEval r k) * (r + k)
 
-theorem ascPochEval_zero (r : R) :
-  ascPochEval r 0 = 1 := by
-    unfold ascPochEval
-    rw [ascpochhammer_zero, Polynomial.eval_one]
+theorem ascPochEval_zero (r : R) :  ascPochEval r 0 = 1 := rfl
 
 theorem ascPochEval_succ (r : R) (k : ℕ) :
-  ascPochEval r (k.succ) = (ascPochEval r k) * (r + k) := by
-    unfold ascPochEval
-    rw [ascpochhammer_succ_eval]
+  ascPochEval r (k.succ) = (ascPochEval r k) * (r + k) := rfl
 
 theorem ascPochEval_cast (n : ℕ) : ∀ (k : ℕ), ascPochEval (n : R) k = (ascPochEval n k)
 | 0 => by rw [ascPochEval_zero, ascPochEval_zero, Nat.cast_one]
@@ -62,18 +55,18 @@ theorem ascPochEval_cast (n : ℕ) : ∀ (k : ℕ), ascPochEval (n : R) k = (asc
   rw [ascPochEval_succ, ascPochEval_succ, Nat.cast_mul, ascPochEval_cast n k, Nat.cast_add]
   norm_cast
 
+theorem ascPochEval_eq_ascPochhammer_eval (r:R) :
+  ∀ (k:ℕ), ascPochEval r k = Polynomial.eval r (ascPochhammer R k)
+  | 0 => by rw [ascPochhammer_zero, Polynomial.eval_one, ascPochEval_zero]
+  | (k + 1) => by
+    rw [ascPochhammer_succ_eval, ← ascPochEval_eq_ascPochhammer_eval r k, ascPochEval_succ]
+
 end Ring
 
 namespace Commute
 
-theorem mem_comm_translate (r s : R) (n : ℕ) (h: Commute r s) : Commute r (s + n) :=
-  h.add_right (Nat.commute_cast r n)
-
-theorem translate_comm_mem (r s : R) (n : ℕ) (h : Commute r s) : Commute (r + n) s :=
-  h.add_left (Nat.cast_commute n s)
-
-theorem translates_comm (r s : R) (k n : ℕ) (h : Commute r s): Commute (r + k) (s + n) :=
-  (translate_comm_mem r s k h).add_right (Nat.commute_cast (r+k) n)
+theorem translate_comm_translate (r s : R) (k n : ℕ) (h : Commute r s): Commute (r + k) (s + n) :=
+  (h.add_left (Nat.cast_commute k s)).add_right (Nat.commute_cast (r+k) n)
 
 theorem translate_comm_ascPochEval (r s : R) (k : ℕ) (h : Commute r s) : ∀ (n : ℕ),
   Commute (r + k) (Ring.ascPochEval s n)
@@ -82,18 +75,18 @@ theorem translate_comm_ascPochEval (r s : R) (k : ℕ) (h : Commute r s) : ∀ (
   exact one_right (r + ↑k)
 | (n + 1) => by
   rw [Ring.ascPochEval_succ]
-  exact (translate_comm_ascPochEval r s k h n).mul_right (translates_comm r s k n h)
+  exact (translate_comm_ascPochEval r s k h n).mul_right (translate_comm_translate r s k n h)
 
 end Commute
 
 namespace Ring
 
 theorem ascPochEval_add_right (r : R) (n : ℕ) : ∀ (k : ℕ),
-  ascPochEval r (n + k) = ascPochEval r n * ascPochEval (r + n) k 
+  ascPochEval r (n + k) = ascPochEval r n * ascPochEval (r + n) k
 | 0 => by rw [add_zero, ascPochEval_zero, mul_one]
 | (k + 1) => by
-  rw [← add_assoc, ascPochEval_succ, ascPochEval_add_right r n k, ascPochEval_succ, mul_assoc, Nat.cast_add,
-    add_assoc]
+  rw [← add_assoc, ascPochEval_succ, ascPochEval_add_right r n k, ascPochEval_succ, mul_assoc,
+    Nat.cast_add, add_assoc]
 
 end Ring
 
@@ -103,8 +96,8 @@ section BinomialSemiring
 
 namespace Ring
 
-/-- A semiring is binomial if multiplication by nonzero natural numbers is injective and pochhammer evaluations
-are divisible by the corresponding factorial. -/
+/-- A semiring is binomial if multiplication by nonzero natural numbers is injective and pochhammer
+evaluations are divisible by the corresponding factorial. -/
 class BinomialSemiring (R: Type u) extends Semiring R where
   /-- Multiplication by positive integers is injective -/
   inj_smul_pos (n : ℕ) (r s : R) (h: n ≠ 0) : n * r = n * s → r = s
@@ -115,9 +108,6 @@ class BinomialSemiring (R: Type u) extends Semiring R where
 
 theorem inj_smul_pos [BinomialSemiring R] (n : ℕ) (r s : R) (h: n ≠ 0) :
   n * r = n * s → r = s := BinomialSemiring.inj_smul_pos n r s h
-
---theorem inj_smul_factorial [BinomialSemiring R] (n : ℕ) (r s : R) : 
---  n.factorial * r = n.factorial * s → r = s := inj_smul_pos n.factorial r s (Nat.factorial_ne_zero n)
 
 theorem eq_of_mul_eq_mul_factorial [BinomialSemiring R] {r s : R} (n : ℕ)
     (h : n.factorial * r = n.factorial * s) : r = s := by
