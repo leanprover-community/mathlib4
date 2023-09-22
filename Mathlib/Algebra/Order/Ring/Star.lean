e05201a5cd2618a87d379d8a0cd3219ae8a0b7f2
Jireh Loreaux <loreaujy@gmail.com>
2023-09-22 13:03:38 -0500

    lint style

--- Mathlib/Algebra/Order/Ring/Star.lean
+++ Mathlib/Algebra/Order/Ring/Star.lean
@@ -41,8 +41,8 @@ example {R : Type*} [OrderedSemiring R] [StarOrderedRing R] {x y : R} (hx : 0 �
 
 /- This will be implied by the instance below, we only prove it to avoid duplicating the
 argument in the instance below for `mul_le_mul_of_nonneg_right`. -/
-private lemma mul_le_mul_of_nonneg_left {R : Type*} [CommSemiring R] [PartialOrder R] [StarOrderedRing R]
-    {a b c : R} (hab : a ≤ b) (hc : 0 ≤ c) : c * a ≤ c * b := by
+private lemma mul_le_mul_of_nonneg_left {R : Type*} [CommSemiring R] [PartialOrder R]
+    [StarOrderedRing R] {a b c : R} (hab : a ≤ b) (hc : 0 ≤ c) : c * a ≤ c * b := by
   rw [StarOrderedRing.nonneg_iff] at hc
   induction hc using AddSubmonoid.closure_induction' with
   | Hs _ h =>
