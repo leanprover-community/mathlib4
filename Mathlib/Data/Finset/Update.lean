dfbe25e68ab8306d15b0d58820017fb300a33ab5
Floris van Doorn <fpvdoorn@gmail.com>
2023-09-23 18:07:12 +0200

    rename

--- Mathlib/Data/Finset/Update.lean
+++ Mathlib/Data/Finset/Update.lean
@@ -8,51 +8,52 @@ import Mathlib.Data.Finset.Basic
 /-!
 # Update a function on a set of values
 
-This file defines `Function.updateSet`, the operation that updates a function on a
+This file defines `Function.updateFinset`, the operation that updates a function on a
 (finite) set of values.
 -/
 variable {ι : Sort _} {π : ι → Sort _} {x : ∀ i, π i}
 
 namespace Function
 
-/-- `updateSet x s y` is the vector `x` with the coordinates in `s` changed to the values of `y`.
+/-- `updateFinset x s y` is the vector `x` with the coordinates in `s` changed to the values of `y`.
 
-We should generalize `updateSet` to `SetLike` instead of restricting to `Finset`.
+We should generalize `updateFinset` to `SetLike` instead of restricting to `Finset`.
 However, at the moment `Finset` is not `SetLike`, so we cannot do this yet.
 -/
-def updateSet (x : ∀ i, π i) (s : Finset ι) [DecidablePred (· ∈ s)] (y : ∀ i : ↥s, π i) (i : ι) :
-    π i :=
+def updateFinset (x : ∀ i, π i) (s : Finset ι) [DecidablePred (· ∈ s)]
+    (y : ∀ i : ↥s, π i) (i : ι) : π i :=
   if hi : i ∈ s then y ⟨i, hi⟩ else x i
 
 open Finset Equiv
 
-theorem updateSet_empty [DecidableEq ι] {y} : updateSet x ∅ y = x :=
+theorem updateFinset_empty [DecidableEq ι] {y} : updateFinset x ∅ y = x :=
   rfl
 
-theorem updateSet_singleton [DecidableEq ι] {i y} :
-    updateSet x {i} y = Function.update x i (y ⟨i, mem_singleton_self i⟩) := by
+theorem updateFinset_singleton [DecidableEq ι] {i y} :
+    updateFinset x {i} y = Function.update x i (y ⟨i, mem_singleton_self i⟩) := by
   congr with j
   by_cases hj : j = i
   · cases hj
-    simp only [dif_pos, Finset.mem_singleton, update_same, updateSet]
-  · simp [hj, updateSet]
+    simp only [dif_pos, Finset.mem_singleton, update_same, updateFinset]
+  · simp [hj, updateFinset]
 
-theorem update_eq_updateSet [DecidableEq ι] {i y} :
-    Function.update x i y = updateSet x {i} (uniqueElim y) := by
+theorem update_eq_updateFinset [DecidableEq ι] {i y} :
+    Function.update x i y = updateFinset x {i} (uniqueElim y) := by
   congr with j
   by_cases hj : j = i
   · cases hj
-    simp only [dif_pos, Finset.mem_singleton, update_same, updateSet]
+    simp only [dif_pos, Finset.mem_singleton, update_same, updateFinset]
     exact uniqueElim_default (α := fun j : ({i} : Finset ι) => π j) y
-  · simp [hj, updateSet]
+  · simp [hj, updateFinset]
 
-theorem updateSet_updateSet [DecidableEq ι] {s t : Finset ι} (hst : Disjoint s t) {y z} :
-    updateSet (updateSet x s y) t z =
-    updateSet x (s ∪ t) (Equiv.piFinsetUnion π hst ⟨y, z⟩) := by
+theorem updateFinset_updateFinset [DecidableEq ι] {s t : Finset ι} (hst : Disjoint s t)
+    {y : ∀ i : ↥s, π i} {z : ∀ i : ↥t, π i} :
+    updateFinset (updateFinset x s y) t z =
+    updateFinset x (s ∪ t) (Equiv.piFinsetUnion π hst ⟨y, z⟩) := by
   set e₁ := finsetUnionEquivSum s t hst |>.symm
   congr with i
   by_cases his : i ∈ s <;> by_cases hit : i ∈ t <;>
-    simp only [updateSet, his, hit, dif_pos, dif_neg, Finset.mem_union, true_or_iff, false_or_iff,
+    simp only [updateFinset, his, hit, dif_pos, dif_neg, Finset.mem_union, true_or_iff, false_or_iff,
       not_false_iff]
   · exfalso; exact Finset.disjoint_left.mp hst his hit
   · exact piCongrLeft_sum_inl (fun b : ↥(s ∪ t) => π b) e₁ y z ⟨i, his⟩ |>.symm
