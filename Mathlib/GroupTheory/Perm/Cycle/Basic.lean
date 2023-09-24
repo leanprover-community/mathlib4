895a84b641f0af225ba9db7a8cc7b0fcdc331633
Chris Wong <lambda.fairy@gmail.com>
2023-09-24 23:14:11 +1000

    feat: SameCycle.equivalence and SameCycle.setoid

--- Mathlib/GroupTheory/Perm/Cycle/Basic.lean
+++ Mathlib/GroupTheory/Perm/Cycle/Basic.lean
@@ -100,6 +100,12 @@ theorem SameCycle.trans : SameCycle f x y → SameCycle f y z → SameCycle f x 
   fun ⟨i, hi⟩ ⟨j, hj⟩ => ⟨j + i, by rw [zpow_add, mul_apply, hi, hj]⟩
 #align equiv.perm.same_cycle.trans Equiv.Perm.SameCycle.trans
 
+theorem SameCycle.equivalence : Equivalence (SameCycle f) :=
+  ⟨SameCycle.refl f, SameCycle.symm, SameCycle.trans⟩
+
+def SameCycle.setoid (f : Perm α) : Setoid α :=
+  ⟨SameCycle f, SameCycle.equivalence⟩
+
 @[simp]
 theorem sameCycle_one : SameCycle 1 x y ↔ x = y := by simp [SameCycle]
 #align equiv.perm.same_cycle_one Equiv.Perm.sameCycle_one
