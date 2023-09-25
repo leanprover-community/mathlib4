12a579fa5b9c0192670cf4e4d91ee6a4c1de8c68
Kalle <kalle.kytola@aalto.fi>
2023-09-26 00:13:52 +0300

    Remove unused arguments.

--- Mathlib/MeasureTheory/Function/L1Space.lean
+++ Mathlib/MeasureTheory/Function/L1Space.lean
@@ -1529,8 +1529,7 @@ lemma ae_restrict_eq_zero_iff_ae_eq_zero_of_mem [MeasurableSpace E] [MeasurableS
   · apply NullMeasurableSet.compl
     convert f_mble (MeasurableSet.singleton 0)
 
-lemma ae_restrict_eq_zero_iff_ae_eq_zero_of_mem' [MeasurableSpace E] [MeasurableSingletonClass E]
-    {s : Set α} (s_mble : MeasurableSet s) :
+lemma ae_restrict_eq_zero_iff_ae_eq_zero_of_mem' {s : Set α} (s_mble : MeasurableSet s) :
     f =ᵐ[Measure.restrict μ s] 0 ↔ ∀ᵐ x ∂μ, x ∈ s → f x = 0 := by
   simp only [Measure.ae, MeasurableSet.compl_iff, EventuallyEq, Filter.Eventually,
              Pi.zero_apply, Filter.mem_mk, mem_setOf_eq]
