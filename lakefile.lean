d384a834b66939928405dd216595cb76bcb75f7c
Joachim Breitner <mail@joachim-breitner.de>
2023-09-25 11:13:11 +0200

    Also fixed aesop

--- lakefile.lean
+++ lakefile.lean
@@ -46,7 +46,7 @@ require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
 
 require std from git "https://github.com/nomeata/std4" @ "lean-pr-testing-2577"
 require Qq from git "https://github.com/gebner/quote4" @ "master"
-require aesop from git "https://github.com/JLimperg/aesop" @ "master"
+require aesop from git "https://github.com/nomeata/aesop" @ "lean-pr-testing-2577"
 require Cli from git "https://github.com/mhuisi/lean4-cli.git" @ "nightly"
 require proofwidgets from git "https://github.com/EdAyers/ProofWidgets4" @ "v0.0.16"
 
