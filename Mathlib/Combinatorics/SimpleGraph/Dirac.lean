d3852efc29509e76eb2557a1d72f50079f4d66ef
Linus <95619282+linus-md@users.noreply.github.com>
2023-09-25 20:51:45 +0200

    formatting

--- Mathlib/Combinatorics/SimpleGraph/Dirac.lean
+++ Mathlib/Combinatorics/SimpleGraph/Dirac.lean
@@ -23,5 +23,7 @@ open BigOperators
 variable {V : Type} [Fintype V] [DecidableEq V] {G : SimpleGraph V} [DecidableRel G.Adj] {u v : V}
 
 /-- Dirac's theorem (1952): Let |G| = n ≥ 3 ∧ δ(G) ≥ n/2 → G is *hamiltonian*. -/
-theorem Dirac {G : SimpleGraph V} [DecidableRel G.Adj] (CardinalityCondition: Fintype.card V ≥ 3) (MinDegreeCondition: G.minDegree ≥ Fintype.card V/2) : G.IsHamiltonian := by
+theorem Dirac {G : SimpleGraph V} [DecidableRel G.Adj]
+(CardinalityCondition: Fintype.card V ≥ 3) (MinDegreeCondition: G.minDegree ≥ Fintype.card V/2) :
+G.IsHamiltonian := by
   sorry
