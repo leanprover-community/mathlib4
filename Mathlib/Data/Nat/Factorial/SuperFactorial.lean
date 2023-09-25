3ab652082cef15365720ae2df53f032aa05ef5fc
Moritz Firsching <firsching@google.com>
2023-09-25 17:42:29 +0200

    Update Mathlib/Data/Nat/Factorial/SuperFactorial.lean
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- Mathlib/Data/Nat/Factorial/SuperFactorial.lean
+++ Mathlib/Data/Nat/Factorial/SuperFactorial.lean
@@ -3,8 +3,8 @@ Copyright (c) 2023 Moritz Firsching. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Moritz Firsching
 -/
-import Mathlib.Data.Nat.Factorial.Basic
 import Mathlib.Algebra.BigOperators.Intervals
+import Mathlib.Data.Nat.Factorial.Basic
 import Mathlib.LinearAlgebra.Vandermonde
 
 /-!
