f7ec64659b413ae950a03456670b2a3565200699
Floris van Doorn <fpvdoorn@gmail.com>
2023-09-25 12:50:51 +0200

    some changes

--- Mathlib/Data/Finset/Update.lean
+++ Mathlib/Data/Finset/Update.lean
@@ -11,7 +11,7 @@ import Mathlib.Data.Finset.Basic
 This file defines `Function.updateFinset`, the operation that updates a function on a
 (finite) set of values.
 -/
-variable {ι : Sort _} {π : ι → Sort _} {x : ∀ i, π i}
+variable {ι : Sort _} {π : ι → Sort _} {x : ∀ i, π i} [DecidableEq ι]
 
 namespace Function
 
@@ -20,16 +20,15 @@ namespace Function
 We should generalize `updateFinset` to `SetLike` instead of restricting to `Finset`.
 However, at the moment `Finset` is not `SetLike`, so we cannot do this yet.
 -/
-def updateFinset (x : ∀ i, π i) (s : Finset ι) [DecidablePred (· ∈ s)]
-    (y : ∀ i : ↥s, π i) (i : ι) : π i :=
+def updateFinset (x : ∀ i, π i) (s : Finset ι) (y : ∀ i : ↥s, π i) (i : ι) : π i :=
   if hi : i ∈ s then y ⟨i, hi⟩ else x i
 
 open Finset Equiv
 
-theorem updateFinset_empty [DecidableEq ι] {y} : updateFinset x ∅ y = x :=
+@[simp] theorem updateFinset_empty {y} : updateFinset x ∅ y = x :=
   rfl
 
-theorem updateFinset_singleton [DecidableEq ι] {i y} :
+theorem updateFinset_singleton {i y} :
     updateFinset x {i} y = Function.update x i (y ⟨i, mem_singleton_self i⟩) := by
   congr with j
   by_cases hj : j = i
@@ -37,7 +36,7 @@ theorem updateFinset_singleton [DecidableEq ι] {i y} :
     simp only [dif_pos, Finset.mem_singleton, update_same, updateFinset]
   · simp [hj, updateFinset]
 
-theorem update_eq_updateFinset [DecidableEq ι] {i y} :
+theorem update_eq_updateFinset {i y} :
     Function.update x i y = updateFinset x {i} (uniqueElim y) := by
   congr with j
   by_cases hj : j = i
@@ -46,7 +45,7 @@ theorem update_eq_updateFinset [DecidableEq ι] {i y} :
     exact uniqueElim_default (α := fun j : ({i} : Finset ι) => π j) y
   · simp [hj, updateFinset]
 
-theorem updateFinset_updateFinset [DecidableEq ι] {s t : Finset ι} (hst : Disjoint s t)
+theorem updateFinset_updateFinset {s t : Finset ι} (hst : Disjoint s t)
     {y : ∀ i : ↥s, π i} {z : ∀ i : ↥t, π i} :
     updateFinset (updateFinset x s y) t z =
     updateFinset x (s ∪ t) (Equiv.piFinsetUnion π hst ⟨y, z⟩) := by
