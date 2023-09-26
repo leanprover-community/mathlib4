4f7a4b9f29390e17a961922a5651c052794563da
Christopher Hoskin <mans0954@users.noreply.github.com>
2023-09-26 08:25:24 +0100

    Update Mathlib/Topology/Order/ScottTopology.lean
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- Mathlib/Topology/Order/ScottTopology.lean
+++ Mathlib/Topology/Order/ScottTopology.lean
@@ -210,7 +210,7 @@ lemma UpperSetTopology_Monotone_coinduced_LE {t₁ : TopologicalSpace α} [Topol
   rw [IsScott.topology_eq β]
   apply UpperSetTopology_Monotone_coinduced_LE' hf
 
-lemma Monotone_From_UpperSetTopology_Continuous [TopologicalSpace α] [Topology.IsUpperSet α]
+lemma continuous_of_monotone_of_upperSetTopology [TopologicalSpace α] [Topology.IsUpperSet α]
     [TopologicalSpace β] [IsScott β] {f : α → β} (hf : Monotone f)  : Continuous f :=
   continuous_iff_coinduced_le.mpr (UpperSetTopology_Monotone_coinduced_LE hf)
 
