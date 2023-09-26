09eb773123d659656b1247c601d029a4efc18af9
madvorak <martin.dvorak@matfyz.cz>
2023-09-26 18:02:31 +0200

    for the linter

--- Mathlib/LinearAlgebra/Matrix/LinearProgramming.lean
+++ Mathlib/LinearAlgebra/Matrix/LinearProgramming.lean
@@ -1,4 +1,20 @@
+/-
+Copyright (c) 2023 Martin Dvorak. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Martin Dvorak
+-/
 import Mathlib.Data.Matrix.Basic
+
+/-! # Linear programming
+
+TODO
+
+## Main definitions
+
+ * `CanonicalLP` defines a linear program in the canonical form.
+ * `CanonicalLP.Admits` tells if a vector is an admissible solution to given LP.
+
+-/
 
 
 structure CanonicalLP where
@@ -10,8 +26,7 @@ structure CanonicalLP where
 def CanonicalLP.Admits (P : CanonicalLP) (x : (Fin P.n) → ℚ) : Prop :=
   P.A.mulVec x = P.b ∧ 0 ≤ x
 
-example : (CanonicalLP.mk ![![1, 2, 0], ![1, -1, 4]] ![5, 3] 0).Admits ![1, 2, 1] :=
-by
+example : (CanonicalLP.mk ![![1, 2, 0], ![1, -1, 4]] ![5, 3] 0).Admits ![1, 2, 1] := by
   constructor
   · ext i : 1
     match i with
