aca132dd17e513294ee7999bf7fa52e1d982abc8
Bolton Bailey <bolton.bailey@gmail.com>
2023-09-26 12:02:58 -0500

    fix mulLeftEmbedding use

--- Mathlib/FieldTheory/Finite/Basic.lean
+++ Mathlib/FieldTheory/Finite/Basic.lean
@@ -233,17 +233,17 @@ theorem sum_subgroup_units_zero_of_ne_bot {G : Subgroup Kˣ} [Fintype G] (hg : G
   rw [Subgroup.ne_bot_iff_exists_ne_one] at hg
   rcases hg with ⟨a, ha⟩
   -- The action of a on G as an embedding
-  have hinj := mul_right_injective a
-  let a_mul_emb : G ↪ G := ⟨(a * ·), hinj⟩
-  -- let a_mul_emb : G ↪ G := by exact mulLeftEmbedding a -- TODO factor this? In library?
+  let a_mul_emb : G ↪ G := mulLeftEmbedding a -- TODO factor this? In library?
   -- ... and leaves G unchanged
   have h_unchanged : Finset.univ.map a_mul_emb = Finset.univ := by simp
   -- Therefore the sum of x over a G is the sum of a x over G
   have h_sum_map := Finset.univ.sum_map a_mul_emb fun x => ((x : Kˣ) : K)
   -- ... and the former is the sum of x over G.
   -- By algebraic manipulation, we have Σ G, x = ∑ G, a x = a ∑ G, x
-  rw [h_unchanged, Function.Embedding.coeFn_mk] at h_sum_map
-  simp_rw [Subgroup.coe_mul, Units.val_mul, ← Finset.mul_sum] at h_sum_map
+  -- rw [h_unchanged, Function.Embedding.coeFn_mk] at h_sum_map
+  simp only [h_unchanged, Function.Embedding.coeFn_mk, Function.Embedding.toFun_eq_coe,
+    mulLeftEmbedding_apply, Submonoid.coe_mul, Subgroup.coe_toSubmonoid, Units.val_mul,
+    ← Finset.mul_sum] at h_sum_map
   -- thus one of (a - 1) or ∑ G, x is zero
   have hzero : (((a : Kˣ) : K) - 1) = 0 ∨ ∑ x : ↥G, ((x : Kˣ) : K) = 0 := by
     rw [←mul_eq_zero, sub_mul, ← h_sum_map, one_mul, sub_self]
