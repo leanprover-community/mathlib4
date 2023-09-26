aca56c8a89c5f654cd88bab2431728df1d474a7a
ADedecker <anatolededecker@gmail.com>
2023-09-26 12:44:52 +0200

    Create file

---
+++ Mathlib/Analysis/NormedSpace/Star/StoneCech.lean
@@ -0,0 +1,6 @@
+/-
+Copyright (c) 2023 Anatole Dedecker. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Anatole Dedecker
+-/
+import Mathlib.Topology.Algebra.Module.CharacterSpace
