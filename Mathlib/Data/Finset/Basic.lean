eeb5d4aa1ac87a2e498768c47c694a8da3b1b765
Floris van Doorn <fpvdoorn@gmail.com>
2023-09-26 15:39:47 +0200

    docstring

--- Mathlib/Data/Finset/Basic.lean
+++ Mathlib/Data/Finset/Basic.lean
@@ -3866,6 +3866,8 @@ theorem Finset.union_symm_inr (h : Disjoint s t) (y : t) :
     (Equiv.Finset.union s t h).symm (Sum.inr y) = ⟨y, Finset.mem_union.mpr <| Or.inr y.2⟩ :=
   rfl
 
+/-- The type of dependent functions on the disjoint union of finsets `s ∪ t` is equivalent to the
+  type of pairs of functions on `s` and on `t`. This is similar to `Equiv.sumPiEquivProdPi`. -/
 def piFinsetUnion {ι} [DecidableEq ι] (α : ι → Type*) {s t : Finset ι} (h : Disjoint s t) :
     ((∀ i : s, α i) × ∀ i : t, α i) ≃ ∀ i : (s ∪ t : Finset ι), α i :=
   let e := (Equiv.Finset.union s t h).symm
