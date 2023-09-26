6e3a9c1707988f6a03cf8ea99a40f155af00d114
Kalle <kalle.kytola@aalto.fi>
2023-09-27 00:35:28 +0300

    Fix broken proofs and align to new names.

--- Mathlib/MeasureTheory/Integral/BoundedContinuousFunction.lean
+++ Mathlib/MeasureTheory/Integral/BoundedContinuousFunction.lean
@@ -24,10 +24,10 @@ section NNRealValued
 
 variable {X : Type*} [MeasurableSpace X] [TopologicalSpace X] [OpensMeasurableSpace X]
 
-theorem NNReal.coe_ennreal_comp_measurable (f : X →ᵇ ℝ≥0) :
+theorem measurable_coe_ennreal_comp (f : X →ᵇ ℝ≥0) :
     Measurable fun x ↦ (f x : ℝ≥0∞) :=
   measurable_coe_nnreal_ennreal.comp f.continuous.measurable
-#align bounded_continuous_function.nnreal.to_ennreal_comp_measurable BoundedContinuousFunction.NNReal.coe_ennreal_comp_measurable
+#align bounded_continuous_function.nnreal.to_ennreal_comp_measurable BoundedContinuousFunction.measurable_coe_ennreal_comp
 
 variable (μ : Measure X) [IsFiniteMeasure μ]
 
@@ -37,24 +37,25 @@ theorem lintegral_lt_top_of_nnreal (f : X →ᵇ ℝ≥0) :
   refine ⟨nndist f 0, fun x ↦ ?_⟩
   have key := BoundedContinuousFunction.NNReal.upper_bound f x
   rwa [ENNReal.coe_le_coe]
-#align measure_theory.lintegral_lt_top_of_bounded_continuous_to_nnreal BoundedContinuousFunction.NNReal.lintegral_lt_top
+#align measure_theory.lintegral_lt_top_of_bounded_continuous_to_nnreal BoundedContinuousFunction.lintegral_lt_top_of_nnreal
 
 theorem integrable_of_nnreal (f : X →ᵇ ℝ≥0) :
     Integrable (((↑) : ℝ≥0 → ℝ) ∘ ⇑f) μ := by
   refine' ⟨(NNReal.continuous_coe.comp f.continuous).measurable.aestronglyMeasurable, _⟩
   simp only [HasFiniteIntegral, Function.comp_apply, NNReal.nnnorm_eq]
-  exact lintegral_lt_top _ f
-#align measure_theory.finite_measure.integrable_of_bounded_continuous_to_nnreal BoundedContinuousFunction.NNReal.integrable
+  exact lintegral_lt_top_of_nnreal _ f
+#align measure_theory.finite_measure.integrable_of_bounded_continuous_to_nnreal BoundedContinuousFunction.integrable_of_nnreal
 
 theorem integral_eq_integral_nnrealPart_sub (f : X →ᵇ ℝ) :
     ∫ x, f x ∂μ = (∫ x, (f.nnrealPart x : ℝ) ∂μ) - ∫ x, ((-f).nnrealPart x : ℝ) ∂μ := by
-  simp only [f.self_eq_nnrealPart_sub_nnrealPart_neg, Pi.sub_apply, integral_sub, NNReal.integrable]
+  simp only [f.self_eq_nnrealPart_sub_nnrealPart_neg, Pi.sub_apply, integral_sub,
+             integrable_of_nnreal]
   rfl
 #align bounded_continuous_function.integral_eq_integral_nnreal_part_sub BoundedContinuousFunction.integral_eq_integral_nnrealPart_sub
 
 theorem lintegral_of_real_lt_top (f : X →ᵇ ℝ) :
-    (∫⁻ x, ENNReal.ofReal (f x) ∂μ) < ∞ := NNReal.lintegral_lt_top _ f.nnrealPart
-#align measure_theory.finite_measure.lintegral_lt_top_of_bounded_continuous_to_real BoundedContinuousFunction.lintegral_lt_top_of_boundedContinuous_to_real
+    (∫⁻ x, ENNReal.ofReal (f x) ∂μ) < ∞ := lintegral_lt_top_of_nnreal _ f.nnrealPart
+#align measure_theory.finite_measure.lintegral_lt_top_of_bounded_continuous_to_real BoundedContinuousFunction.lintegral_of_real_lt_top
 
 theorem toReal_lintegral_coe_eq_integral (f : X →ᵇ ℝ≥0)
     (μ : Measure X) : (∫⁻ x, (f x : ℝ≥0∞) ∂μ).toReal = ∫ x, (f x : ℝ) ∂μ := by
@@ -62,7 +63,7 @@ theorem toReal_lintegral_coe_eq_integral (f : X →ᵇ ℝ≥0)
         (NNReal.continuous_coe.comp f.continuous).measurable.aestronglyMeasurable)]
   · simp only [ENNReal.ofReal_coe_nnreal]
   · exact eventually_of_forall (by simp only [Pi.zero_apply, NNReal.zero_le_coe, imp_true_iff])
-#align bounded_continuous_function.nnreal.to_real_lintegral_eq_integral BoundedContinuousFunction.NNReal.toReal_lintegral_eq_integral
+#align bounded_continuous_function.nnreal.to_real_lintegral_eq_integral BoundedContinuousFunction.toReal_lintegral_coe_eq_integral
 
 end NNRealValued
 
@@ -88,7 +89,6 @@ lemma integrable [IsFiniteMeasure μ] (f : X →ᵇ E) :
     _ ≤ ‖f‖₊ * (μ Set.univ)   := f.lintegral_nnnorm_le μ
     _ < ∞                     := ENNReal.mul_lt_top ENNReal.coe_ne_top (measure_ne_top μ Set.univ)
 #align measure_theory.finite_measure.integrable_of_bounded_continuous_to_real BoundedContinuousFunction.integrable
--- The new statement is more general (codomain `E` rather than `ℝ`). Is the `align` still ok?
 
 variable [NormedSpace ℝ E]
 
