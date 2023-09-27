7212811e10716366a3ea5ac66a1e45f55c8b12f2
Koundinya Vajjha <koundinya.vajjha@gmail.com>
2023-09-27 15:37:09 -0700

    add main result

--- Mathlib/NumberTheory/Padics/Harmonic.lean
+++ Mathlib/NumberTheory/Padics/Harmonic.lean
@@ -65,3 +65,10 @@ theorem padicValRat_two_harmonic {n : ℕ} : padicValRat 2 (harmonic n) = -Nat.l
         (harmonic_pos hn).ne' (inv_ne_zero (Nat.cast_ne_zero.mpr n.succ_ne_zero)) key, ih,
         padicValRat.inv, padicValRat.of_nat, min_neg_neg, neg_inj, ← Nat.cast_max, Nat.cast_inj]
     exact Nat.max_log_padicValNat_succ_eq_log_succ n
+
+/-- The n-th harmonic number is not an integer for n ≥ 2. -/
+theorem harmonic_not_int {n : ℕ} (h : 2 ≤ n) : ¬ (harmonic n).isInt := by
+  apply padicNorm.not_int_of_not_padic_int 2
+  rw [padicNorm.eq_zpow_of_nonzero (harmonic_pos (ne_zero_of_lt h)).ne',
+      padicValRat_two_harmonic, neg_neg, zpow_coe_nat]
+  exact one_lt_pow one_lt_two (Nat.log_pos one_lt_two h).ne'
