53cc4b790172277ac2df7d81c29b06e53688acee
Yaël Dillies <yael.dillies@gmail.com>
2023-09-27 10:25:15 +0200

    fix imports

--- Mathlib/Combinatorics/SetFamily/Shadow.lean
+++ Mathlib/Combinatorics/SetFamily/Shadow.lean
@@ -3,7 +3,7 @@ Copyright (c) 2021 Bhavik Mehta. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Bhavik Mehta, Alena Gusakov, Yaël Dillies
 -/
-import Mathlib.Data.Finset.Slice
+import Mathlib.Data.Finset.Sups
 import Mathlib.Logic.Function.Iterate
 
 #align_import combinatorics.set_family.shadow from "leanprover-community/mathlib"@"f7fc89d5d5ff1db2d1242c7bb0e9062ce47ef47c"
