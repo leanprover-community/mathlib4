d091ac041a0dc6b299ebc5092a05d38d62e0d821
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2023-09-28 15:44:34 -0400

    import all files

--- Mathlib.lean
+++ Mathlib.lean
@@ -2011,6 +2011,7 @@ import Mathlib.Geometry.Manifold.Metrizable
 import Mathlib.Geometry.Manifold.PartitionOfUnity
 import Mathlib.Geometry.Manifold.Sheaf.Basic
 import Mathlib.Geometry.Manifold.Sheaf.Smooth
+import Mathlib.Geometry.Manifold.Sheaf.LocallyRingedSpace
 import Mathlib.Geometry.Manifold.SmoothManifoldWithCorners
 import Mathlib.Geometry.Manifold.VectorBundle.Basic
 import Mathlib.Geometry.Manifold.VectorBundle.FiberwiseLinear
