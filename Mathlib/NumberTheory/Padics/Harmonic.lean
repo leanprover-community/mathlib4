aa60496c0f055540dda47982f22ccb2b95e82cd4
Koundinya Vajjha <koundinya.vajjha@gmail.com>
2023-09-28 15:23:02 -0700

    move lemmas around

--- Mathlib/NumberTheory/Padics/Harmonic.lean
+++ Mathlib/NumberTheory/Padics/Harmonic.lean
@@ -33,24 +33,6 @@ lemma harmonic_pos {n : ℕ} (Hn : n ≠ 0) : 0 < harmonic n :=
   Finset.sum_pos (fun _ _ => inv_pos.mpr (by norm_cast; linarith))
   (by rwa [Finset.nonempty_range_iff])
 
-lemma Nat.log_ne_padicValNat_succ {n : ℕ} (hn : n ≠ 0) : log 2 n ≠ padicValNat 2 (n + 1) := by
-  rw [Ne, log_eq_iff (by simp [hn])]
-  rintro ⟨h1, h2⟩
-  rw [← lt_add_one_iff, ← mul_one (2 ^ _)] at h1
-  rw [← add_one_le_iff, pow_succ] at h2
-  refine' not_dvd_of_between_consec_multiples h1 (lt_of_le_of_ne' h2 _) pow_padicValNat_dvd
-  exact pow_succ_padicValNat_not_dvd n.succ_ne_zero ∘ dvd_of_eq
-
-lemma Nat.max_log_padicValNat_succ_eq_log_succ (n : ℕ) :
-    max (log 2 n) (padicValNat 2 (n + 1)) = log 2 (n + 1) := by
-  apply le_antisymm (max_le (le_log_of_pow_le one_lt_two (pow_log_le_add_one 2 n))
-    (padicValNat_le_nat_log (n + 1)))
-  rw [le_max_iff, or_iff_not_imp_left, not_le]
-  intro h
-  replace h := le_antisymm (add_one_le_iff.mpr (lt_pow_of_log_lt one_lt_two h))
-    (pow_log_le_self 2 n.succ_ne_zero)
-  rw [h, padicValNat.prime_pow, ← h]
-
 /-- The 2-adic valuation of the n-th harmonic number is the negative of the logarithm
     of n. -/
 theorem padicValRat_two_harmonic {n : ℕ} : padicValRat 2 (harmonic n) = -Nat.log 2 n := by
