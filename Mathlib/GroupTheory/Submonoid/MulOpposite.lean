9bd23b5bfab906b8c91fcae86d1686a882eb9991
Jujian Zhang <jujian.zhang1998@outlook.com>
2023-09-28 11:43:17 +0100

    fix doc strings

--- Mathlib/GroupTheory/Submonoid/MulOpposite.lean
+++ Mathlib/GroupTheory/Submonoid/MulOpposite.lean
@@ -17,24 +17,25 @@ variable {M : Type*} [MulOneClass M]
 
 namespace Submonoid
 
-/-- pull a submonoid back to an opposite submonoid along `unop`-/
-@[to_additive (attr := simps) "pull an additive submonoid back to an opposite submonoid along
-`unop`"]
+/-- Pull a submonoid back to an opposite submonoid along `MulOpposite.unop`-/
+@[to_additive (attr := simps) "Pull an additive submonoid back to an opposite submonoid along
+`AddOpposite.unop`"]
 protected def op {M : Type*} [MulOneClass M] (x : Submonoid M) : Submonoid (MulOpposite M) where
   carrier := MulOpposite.unop ⁻¹' x.1
   mul_mem' ha hb := x.mul_mem hb ha
   one_mem' := Submonoid.one_mem' _
 
-/-- pull an opposite submonoid back to a submonoid along `op`-/
-@[to_additive (attr := simps) "pull an opposite additive submonoid back to a submonoid along `op`"]
+/-- Pull an opposite submonoid back to a submonoid along `MulOpposite.op`-/
+@[to_additive (attr := simps) "Pull an opposite additive submonoid back to a submonoid along
+`AddOpposite.op`"]
 protected def unop {M : Type*} [MulOneClass M] (x : Submonoid (MulOpposite M)) : Submonoid M where
   carrier := MulOpposite.op ⁻¹' x.1
   mul_mem' ha hb := x.mul_mem hb ha
   one_mem' := Submonoid.one_mem' _
 
-/-- A submonoid `H` of `G` determines a submonoid `H.opposite` of the opposite group `Gᵐᵒᵖ`. -/
+/-- A submonoid `H` of `G` determines a submonoid `H.op` of the opposite group `Gᵐᵒᵖ`. -/
 @[to_additive (attr := simps) "A additive submonoid `H` of `G` determines an additive submonoid
-`H.opposite` of the opposite group `Gᵐᵒᵖ`."]
+`H.op` of the opposite group `Gᵐᵒᵖ`."]
 def opEquiv : Submonoid M ≃ Submonoid Mᵐᵒᵖ where
   toFun := Submonoid.op
   invFun := Submonoid.unop
