cdcd0b3c41a6c1b4e0cfa372d83e3a26e2453686
Rémy Degenne <remydegenne@gmail.com>
2023-09-28 08:16:56 +0200

    Update Mathlib/MeasureTheory/Measure/Trim.lean

--- Mathlib/MeasureTheory/Measure/Trim.lean
+++ Mathlib/MeasureTheory/Measure/Trim.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2023 Rémy Degenne. All rights reserved.
+Copyright (c) 2021 Rémy Degenne. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Rémy Degenne
 -/
