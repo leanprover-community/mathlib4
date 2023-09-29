5205a6b71f3f6b1604841f52f190ba0d6b9af9bd
Moritz Firsching <firsching@google.com>
2023-09-29 13:57:52 +0200

    Update Mathlib/LinearAlgebra/Prod.lean
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- Mathlib/LinearAlgebra/Prod.lean
+++ Mathlib/LinearAlgebra/Prod.lean
@@ -608,7 +608,7 @@ def fst : Submodule R (M × M₂) :=
 /-- `M` as a submodule of `M × N` is isomorphic to `M`. -/
 @[simps]
 def fstEquiv : Submodule.fst R M M₂ ≃ₗ[R] M where
--- Porting note: proofs were `tidy` or `simp`
+  -- Porting note: proofs were `tidy` or `simp`
   toFun x := x.1.1
   invFun m := ⟨⟨m, 0⟩, by simp only [fst, comap_bot, mem_ker, snd_apply]⟩
   map_add' := by simp only [AddSubmonoid.coe_add, coe_toAddSubmonoid, Prod.fst_add, Subtype.forall,
