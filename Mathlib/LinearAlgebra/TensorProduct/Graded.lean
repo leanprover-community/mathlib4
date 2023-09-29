6fb1a4bba13ad1eeaa1b08fc39a74fa74f7bc27e
Eric Wieser <wieser.eric@gmail.com>
2023-09-29 10:27:32 +0000

    almost at the ring structure

--- Mathlib/LinearAlgebra/TensorProduct/Graded.lean
+++ Mathlib/LinearAlgebra/TensorProduct/Graded.lean
@@ -23,17 +23,9 @@ open scoped TensorProduct
 
 variable {R A B : Type*}
 
-section external
-variable (𝒜 : ZMod 2 → Type*) (ℬ : ZMod 2 → Type*)
-variable [CommRing R]
-variable [∀ i, AddCommGroup (𝒜 i)] [∀ i, AddCommGroup (ℬ i)]
-variable [∀ i, Module R (𝒜 i)] [∀ i, Module R (ℬ i)]
-variable [DirectSum.GRing 𝒜] [DirectSum.GRing ℬ]
-variable [DirectSum.GAlgebra R 𝒜] [DirectSum.GAlgebra R ℬ]
+section zmod2_pow
 
-instance (i : ℤ₂ × ℤ₂) : Module R (𝒜 (Prod.fst i) ⊗[R] ℬ (Prod.snd i)) :=
-  TensorProduct.leftModule
-
+/-- There is a canonical power operation by `ℤˣ` on `ZMod 2`. -/
 instance : Pow ℤˣ (ZMod 2) where
   pow s z₂ := s ^ z₂.val
 
@@ -46,11 +38,29 @@ lemma z₂pow_add (s : ℤˣ) (x y : ZMod 2) : s ^ (x + y) = s ^ x * s ^ y := by
   rw [ZMod.val_add, ←pow_eq_pow_mod, pow_add]
   fin_cases s <;> simp
 
+end zmod2_pow
+
+
+namespace TensorProduct
+
+section external
+variable (𝒜 : ZMod 2 → Type*) (ℬ : ZMod 2 → Type*)
+variable [CommRing R]
+variable [∀ i, AddCommGroup (𝒜 i)] [∀ i, AddCommGroup (ℬ i)]
+variable [∀ i, Module R (𝒜 i)] [∀ i, Module R (ℬ i)]
+variable [DirectSum.GRing 𝒜] [DirectSum.GRing ℬ]
+variable [DirectSum.GAlgebra R 𝒜] [DirectSum.GAlgebra R ℬ]
 
 local notation "𝒜ℬ" => (fun i : ℤ₂ × ℤ₂ => 𝒜 (Prod.fst i) ⊗[R] ℬ (Prod.snd i))
--- #exit
+
+-- this helps with performance
+instance (i : ℤ₂ × ℤ₂) : Module R (𝒜 (Prod.fst i) ⊗[R] ℬ (Prod.snd i)) :=
+  TensorProduct.leftModule
+
 variable (R) in
-noncomputable irreducible_def mulAux1 :
+/-- Auxliary construction used to build `gradedMul`. This operates on direct sums of tensors
+instead of tensors of direct sums. -/
+noncomputable irreducible_def gradedMulAux :
     (DirectSum _ 𝒜ℬ) →ₗ[R] (DirectSum _ 𝒜ℬ) →ₗ[R] (DirectSum _ 𝒜ℬ) := by
   refine TensorProduct.curry ?_
   refine ?_ ∘ₗ (TensorProduct.directSum R 𝒜ℬ 𝒜ℬ).toLinearMap
@@ -68,18 +78,19 @@ open GradedMonoid (GMul)
 
 set_option maxHeartbeats 400000 in
 @[simp]
-theorem mulAux1_lof_tmul_lof_tmul (i₁ j₁ i₂ j₂ : ℤ₂)
+theorem gradedMulAux_lof_tmul_lof_tmul (i₁ j₁ i₂ j₂ : ℤ₂)
     (a₁ : 𝒜 i₁) (b₁ : ℬ j₁) (a₂ : 𝒜 i₂) (b₂ : ℬ j₂) :
-    mulAux1 R 𝒜 ℬ (lof R _ 𝒜ℬ (i₁, j₁) (a₁ ⊗ₜ b₁)) (lof R _ 𝒜ℬ (i₂, j₂) (a₂ ⊗ₜ b₂)) =
+    gradedMulAux R 𝒜 ℬ (lof R _ 𝒜ℬ (i₁, j₁) (a₁ ⊗ₜ b₁)) (lof R _ 𝒜ℬ (i₂, j₂) (a₂ ⊗ₜ b₂)) =
       (-1 : ℤˣ)^(j₁ * i₂)
         • lof R _ 𝒜ℬ (_, _) (GMul.mul a₁ a₂ ⊗ₜ GMul.mul b₁ b₂) := by
-  rw [mulAux1]
+  rw [gradedMulAux]
   dsimp
   simp
 
 set_option maxHeartbeats 4000000
 variable (R) in
-noncomputable irreducible_def mulAux :
+/-- The multiplication operation for tensor products of externally `ZMod 2`-graded algebras. -/
+noncomputable irreducible_def gradedMul :
     letI AB := (⨁ i, 𝒜 i) ⊗[R] (⨁ i, ℬ i)
     letI : Module R AB := TensorProduct.leftModule
     AB →ₗ[R] AB →ₗ[R] AB := by
@@ -89,50 +100,50 @@ noncomputable irreducible_def mulAux :
   refine e' ∘ₗ ?_
   refine ?_ ∘ₗ TensorProduct.map e.toLinearMap e.toLinearMap
   refine TensorProduct.lift ?_
-  exact mulAux1 R 𝒜 ℬ
+  exact gradedMulAux R 𝒜 ℬ
 
-theorem mulAux_of_tmul_of (i₁ j₁ i₂ j₂ : ℤ₂)
+theorem gradedMul_of_tmul_of (i₁ j₁ i₂ j₂ : ℤ₂)
     (a₁ : 𝒜 i₁) (b₁ : ℬ j₁) (a₂ : 𝒜 i₂) (b₂ : ℬ j₂) :
-    mulAux R 𝒜 ℬ (lof R _ 𝒜 i₁ a₁ ⊗ₜ lof R _ ℬ j₁ b₁) (lof R _ 𝒜 i₂ a₂ ⊗ₜ lof R _ ℬ j₂ b₂) =
+    gradedMul R 𝒜 ℬ (lof R _ 𝒜 i₁ a₁ ⊗ₜ lof R _ ℬ j₁ b₁) (lof R _ 𝒜 i₂ a₂ ⊗ₜ lof R _ ℬ j₂ b₂) =
       (-1 : ℤˣ)^(j₁ * i₂)
         • (lof R _ 𝒜 _ (GMul.mul a₁ a₂) ⊗ₜ lof R _ ℬ _ (GMul.mul b₁ b₂)) := by
-  rw [mulAux]
+  rw [gradedMul]
   dsimp only [TensorProduct.curry_apply, LinearMap.coe_comp, LinearEquiv.coe_coe, Function.comp_apply,
     TensorProduct.map_tmul, TensorProduct.lift.tmul]
   rw [TensorProduct.directSum_lof_tmul_lof, TensorProduct.directSum_lof_tmul_lof,
-    mulAux1_lof_tmul_lof_tmul, Units.smul_def, zsmul_eq_smul_cast R, map_smul,
+    gradedMulAux_lof_tmul_lof_tmul, Units.smul_def, zsmul_eq_smul_cast R, map_smul,
     TensorProduct.directSum_symm_lof_tmul, ←zsmul_eq_smul_cast, ←Units.smul_def]
 
-theorem mulAux_one (x : (⨁ i, 𝒜 i) ⊗[R] (⨁ i, ℬ i)) :
-    mulAux R 𝒜 ℬ 1 x = x := by
-  suffices mulAux R 𝒜 ℬ 1 = LinearMap.id by
+theorem one_gradedMul (x : (⨁ i, 𝒜 i) ⊗[R] (⨁ i, ℬ i)) :
+    gradedMul R 𝒜 ℬ 1 x = x := by
+  suffices gradedMul R 𝒜 ℬ 1 = LinearMap.id by
     exact FunLike.congr_fun this x
   ext ia a ib b
   dsimp only [LinearMap.coe_comp, Function.comp_apply, TensorProduct.AlgebraTensorModule.curry_apply,
     TensorProduct.curry_apply, LinearMap.coe_restrictScalars, LinearMap.id_coe, id_eq]
   rw [Algebra.TensorProduct.one_def]
-  erw [mulAux_of_tmul_of]
+  erw [gradedMul_of_tmul_of]
   rw [zero_mul, z₂pow_zero, one_smul]
   simp_rw [DirectSum.lof_eq_of]
   rw [←DirectSum.of_mul_of, ←DirectSum.of_mul_of]
   erw [one_mul, one_mul]
 
-theorem one_mulAux (x : (⨁ i, 𝒜 i) ⊗[R] (⨁ i, ℬ i)) :
-    mulAux R 𝒜 ℬ x 1 = x := by
-  suffices (mulAux R 𝒜 ℬ).flip 1 = LinearMap.id by
+theorem gradedMul_one (x : (⨁ i, 𝒜 i) ⊗[R] (⨁ i, ℬ i)) :
+    gradedMul R 𝒜 ℬ x 1 = x := by
+  suffices (gradedMul R 𝒜 ℬ).flip 1 = LinearMap.id by
     exact FunLike.congr_fun this x
   ext
   dsimp
   rw [Algebra.TensorProduct.one_def]
-  erw [mulAux_of_tmul_of]
+  erw [gradedMul_of_tmul_of]
   rw [mul_zero, z₂pow_zero, one_smul]
   simp_rw [DirectSum.lof_eq_of]
   rw [←DirectSum.of_mul_of, ←DirectSum.of_mul_of]
   erw [mul_one, mul_one]
 
-theorem mulAux_assoc (x y z : (⨁ i, 𝒜 i) ⊗[R] (⨁ i, ℬ i)) :
-    mulAux R 𝒜 ℬ (mulAux R 𝒜 ℬ x y) z = mulAux R 𝒜 ℬ x (mulAux R 𝒜 ℬ y z) := by
-  let mA := mulAux R 𝒜 ℬ
+theorem gradedMul_assoc (x y z : (⨁ i, 𝒜 i) ⊗[R] (⨁ i, ℬ i)) :
+    gradedMul R 𝒜 ℬ (gradedMul R 𝒜 ℬ x y) z = gradedMul R 𝒜 ℬ x (gradedMul R 𝒜 ℬ y z) := by
+  let mA := gradedMul R 𝒜 ℬ
     -- restate as an equality of morphisms so that we can use `ext`
   suffices LinearMap.llcomp R _ _ _ mA ∘ₗ mA =
       (LinearMap.llcomp R _ _ _ LinearMap.lflip <| LinearMap.llcomp R _ _ _ mA.flip ∘ₗ mA).flip by
@@ -140,8 +151,8 @@ theorem mulAux_assoc (x y z : (⨁ i, 𝒜 i) ⊗[R] (⨁ i, ℬ i)) :
   ext ixa xa ixb xb iya ya iyb yb iza za izb zb
   dsimp
   save
-  simp_rw [mulAux_of_tmul_of, Units.smul_def, zsmul_eq_smul_cast R,
-    LinearMap.map_smul₂, LinearMap.map_smul, mulAux_of_tmul_of, DirectSum.lof_eq_of,
+  simp_rw [gradedMul_of_tmul_of, Units.smul_def, zsmul_eq_smul_cast R,
+    LinearMap.map_smul₂, LinearMap.map_smul, gradedMul_of_tmul_of, DirectSum.lof_eq_of,
     ←DirectSum.of_mul_of, mul_assoc]
   save
   simp_rw [←zsmul_eq_smul_cast R, ←Units.smul_def, smul_smul, ←z₂pow_add, add_mul, mul_add]
@@ -159,6 +170,7 @@ open DirectSum
 
 
 variable (R) in
+/-- A Type synonym for `A ⊗[R] B`, but with multiplication as `TensorProduct.gradedMul`. -/
 def SuperTensorProduct
     (𝒜 : ZMod 2 → Submodule R A) (ℬ : ZMod 2 → Submodule R B)
     [GradedAlgebra 𝒜] [GradedAlgebra ℬ] :
@@ -167,24 +179,87 @@ def SuperTensorProduct
 
 scoped[TensorProduct] notation:100 𝒜 " ⊗'[" R "] " ℬ:100 => SuperTensorProduct R 𝒜 ℬ
 
-instance : AddCommGroupWithOne (𝒜 ⊗'[R] ℬ) := Algebra.TensorProduct.instAddCommGroupWithOne
+instance instAddCommGroupWithOne : AddCommGroupWithOne (𝒜 ⊗'[R] ℬ) :=
+  Algebra.TensorProduct.instAddCommGroupWithOne
 instance : Module R (𝒜 ⊗'[R] ℬ) := TensorProduct.leftModule
+
+namespace SuperTensorProduct
+
+variable (R) in
+def of : A ⊗[R] B ≃ₗ[R] 𝒜 ⊗'[R] ℬ := LinearEquiv.refl _ _
+
+@[simp]
+theorem of_one : of R 𝒜 ℬ 1 = 1 := rfl
+
+@[simp]
+theorem of_symm_one : (of R 𝒜 ℬ).symm 1 = 1 := rfl
+
+@[simp] theorem of_symm_of (x : A ⊗[R] B) : (of R 𝒜 ℬ).symm (of R 𝒜 ℬ x) = x := rfl
+@[simp] theorem symm_of_of (x : 𝒜 ⊗'[R] ℬ) : of R 𝒜 ℬ ((of R 𝒜 ℬ).symm x) = x := rfl
+
+variable (R) {𝒜 ℬ} in
+abbrev tmul (a : A) (b : B) := of R 𝒜 ℬ (a ⊗ₜ b)
+
+notation:100 x " ⊗ₜ'" y:100 => tmul _ x y
+notation:100 x " ⊗ₜ'[" R "] " y:100 => tmul R x y
 
 local notation "↥" P => { x // x ∈ P}
 
-def mul : (𝒜 ⊗'[R] ℬ) →ₗ[R] (𝒜 ⊗'[R] ℬ) →ₗ[R] (𝒜 ⊗'[R] ℬ) := by
+variable (R) in
+noncomputable def auxEquiv : (𝒜 ⊗'[R] ℬ) ≃ₗ[R] (⨁ i, 𝒜 i) ⊗[R] (⨁ i, ℬ i) :=
   let fA := (decomposeAlgEquiv 𝒜).toLinearEquiv
   let fB := (decomposeAlgEquiv ℬ).toLinearEquiv
-  let fAB1 := TensorProduct.congr fA fB
-  let fAB2 := TensorProduct.directSum R (𝒜 ·) (ℬ ·)
-  let fAB := fAB1 ≪≫ₗ fAB2
-  let fAB' := TensorProduct.congr fAB fAB
-  letI tAB := fun i : ZMod 2 × ZMod 2 => 𝒜 i.1 ⊗[R] ℬ i.2
-  let fAB'' := fAB' ≪≫ₗ TensorProduct.directSum R tAB tAB
-  -- refine TensorProduct.curry ?_
-  -- refine ?_ ∘ₗ TensorProduct.map fAB.toLinearMap fAB.toLinearMap
-  -- `(a_i ⊗ b_j) * (a_k ⊗ b_l) = -1^(jk) • (a_i*a_k ⊗ b_j*b_l)``
+  (of R 𝒜 ℬ).symm.trans (TensorProduct.congr fA fB)
 
-  -- refine TensorProduct.uncurry R _ _ _ ∘ₗ TensorProduct.lift ?_
-  -- refine TensorProduct.homTensorHomMap R A B A B ∘ₗ TensorProduct.lift ?_
+@[simp] theorem auxEquiv_one : auxEquiv R 𝒜 ℬ 1 = 1 := by
+  dsimp [auxEquiv]
+  simp_rw [Algebra.TensorProduct.one_def, congr_tmul]
+  dsimp
+  rw [AlgEquiv.map_one, AlgEquiv.map_one]
+
+/-- Auxiliary construction used to build the `Mul` instance and get distributivity of `+` and
+`\smul`. -/
+noncomputable def mulHom : (𝒜 ⊗'[R] ℬ) →ₗ[R] (𝒜 ⊗'[R] ℬ) →ₗ[R] (𝒜 ⊗'[R] ℬ) := by
+  letI fAB1 := auxEquiv R 𝒜 ℬ
+  have := ((gradedMul R (𝒜 ·) (ℬ ·)).compl₁₂ fAB1.toLinearMap fAB1.toLinearMap).compr₂ fAB1.symm.toLinearMap
+  exact this
+
+attribute [pp_dot] AlgEquiv.toLinearEquiv LinearEquiv.symm LinearEquiv.trans
+
+
+theorem mulHom_apply (x y : 𝒜 ⊗'[R] ℬ) :
+    mulHom 𝒜 ℬ x y
+      = (auxEquiv R 𝒜 ℬ).symm (gradedMul R (𝒜 ·) (ℬ ·) (auxEquiv R 𝒜 ℬ x) (auxEquiv R 𝒜 ℬ y)) :=
+  rfl
+
+noncomputable instance : Mul (𝒜 ⊗'[R] ℬ) where mul x y := mulHom 𝒜 ℬ x y
+
+theorem mul_def (x y : 𝒜 ⊗'[R] ℬ) : x * y = mulHom 𝒜 ℬ x y := rfl
+
+noncomputable instance : Monoid (𝒜 ⊗'[R] ℬ) where
+  mul_one x := by
+    rw [mul_def, mulHom_apply, auxEquiv_one, gradedMul_one, LinearEquiv.symm_apply_apply]
+  one_mul x := by
+    rw [mul_def, mulHom_apply, auxEquiv_one, one_gradedMul, LinearEquiv.symm_apply_apply]
+  mul_assoc x y z := by
+    simp_rw [mul_def, mulHom_apply, LinearEquiv.apply_symm_apply]
+    rw [gradedMul_assoc]
+
+noncomputable instance : Ring (𝒜 ⊗'[R] ℬ) where
+  __ := instAddCommGroupWithOne
+  right_distrib x y z := by rw [mul_def, map_add]
+  left_distrib x y z := by rw [mul_def, map_add]
+
+theorem coe_mul_coe {i₁ j₁ i₂ j₂ : ℤ₂} (a₁ : 𝒜 i₁) (b₁ : ℬ j₁) (a₂ : 𝒜 i₂) (b₂ : ℬ j₂) :
+  ((a₁ : A) ⊗ₜ'[R] (b₁ : B) * (a₂ : A) ⊗ₜ'[R] (b₂ : B) : 𝒜 ⊗'[R] ℬ) =
+      (-1 : ℤˣ)^(j₁ * i₂) • ((a₁ * a₂ : A) ⊗ₜ' (b₁ * b₂ : B)) := by
+  dsimp only [mul_def, mulHom_apply, of_symm_of]
+  save
+  dsimp [auxEquiv, tmul]
+  rw [congr_symm_tmul]
+  rw [gradedMul_of_tmul_of]
   sorry
+
+end SuperTensorProduct
+
+end TensorProduct
