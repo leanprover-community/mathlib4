893bcd12f681c9eab5e750b074949fa6eca95288
Vierkantor <vierkantor@vierkantor.com>
2023-09-29 14:17:46 +0000

    Fix imports

--- Mathlib/Tactic/NormNum/Mod.lean
+++ Mathlib/Tactic/NormNum/Mod.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Anne Baanen
 -/
 import Mathlib.Tactic.NormNum.Basic
+import Mathlib.Tactic.NormNum.Ineq
 
 /-!
 # `norm_num` extension for modulo operator
