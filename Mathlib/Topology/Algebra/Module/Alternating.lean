c4f658737c329b8568fe82e81b85400ce9d2f574
Yury G. Kudryashov <urkud@urkud.name>
2023-09-30 18:47:08 -0500

    Snapshot

--- Mathlib/Topology/Algebra/Module/Alternating.lean
+++ Mathlib/Topology/Algebra/Module/Alternating.lean
@@ -3,7 +3,7 @@ Copyright (c) 2023 Yury Kudryashov. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yury Kudryashov, Heather Macbeth, Sébastien Gouëzel
 -/
-import Mathlib.LinearAlgebra.Alternating
+import Mathlib.LinearAlgebra.Alternating.Basic
 import Mathlib.Topology.Algebra.Module.Multilinear
 
 /-!
