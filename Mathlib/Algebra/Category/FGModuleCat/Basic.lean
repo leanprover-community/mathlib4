58522b4f8d085c806696b371e0cded3c5e900760
Chris Hughes <chrishughes24@gmail.com>
2023-09-30 13:45:42 +0100

    fix build

--- Mathlib/Algebra/Category/FGModuleCat/Basic.lean
+++ Mathlib/Algebra/Category/FGModuleCat/Basic.lean
@@ -265,7 +265,7 @@ theorem FGModuleCatEvaluation_apply (f : FGModuleCatDual K V) (x : V) :
 
 -- Porting note: extremely slow, was fast in mathlib3.
 -- I tried many things using `dsimp` and `change`, but couldn't find anything faster than this.
-set_option maxHeartbeats 1500000 in
+set_option maxHeartbeats 2000000 in
 private theorem coevaluation_evaluation :
     letI V' : FGModuleCat K := FGModuleCatDual K V
     (𝟙 V' ⊗ FGModuleCatCoevaluation K V) ≫ (α_ V' V V').inv ≫ (FGModuleCatEvaluation K V ⊗ 𝟙 V') =
