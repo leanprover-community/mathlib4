bc5c743a8910528dabd217df3e83fa0d9ca3c937
Alex Keizer <alex@keizer.dev>
2023-09-30 23:48:02 +0100

    Fix doc-string of ldiff

--- Mathlib/Init/Data/Nat/Bitwise.lean
+++ Mathlib/Init/Data/Nat/Bitwise.lean
@@ -279,9 +279,9 @@ def bitwise' (f : Bool → Bool → Bool) : ℕ → ℕ → ℕ :=
 #align nat.land Nat.land
 #align nat.lxor Nat.xor
 
-/--`ldiff' a b` performs bitwise set difference. For each corresponding
+/--`ldiff a b` performs bitwise set difference. For each corresponding
   pair of bits taken as booleans, say `aᵢ` and `bᵢ`, it applies the
-  boolean operation `aᵢ ∧ bᵢ` to obtain the `iᵗʰ` bit of the result.-/
+  boolean operation `aᵢ ∧ ¬bᵢ` to obtain the `iᵗʰ` bit of the result.-/
 def ldiff : ℕ → ℕ → ℕ :=
   bitwise fun a b => a && not b
 #align nat.ldiff Nat.ldiff
