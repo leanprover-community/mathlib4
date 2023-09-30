86831e868a1fc76bfa49a25cb554ff97cd8db284
kkytola <kalle.kytola@aalto.fi>
2023-09-30 17:02:19 +0000

    feat: Add a basic layercake formula for Bochner integral. (#7167)
    Layer cake formulas currently exist for `ENNReal`-valued functions and Lebesgue integrals. This PR adds the most common version of the layer cake formula for integrable a.e.-nonnegative real-valued functions and Bochner integrals.
    
    
    
    Co-authored-by: kkytola <“kalle.kytola@aalto.fi”>
    Co-authored-by: kkytola <39528102+kkytola@users.noreply.github.com>

--- Mathlib/MeasureTheory/Measure/MeasureSpace.lean
+++ Mathlib/MeasureTheory/Measure/MeasureSpace.lean
@@ -2712,6 +2712,28 @@ theorem ae_eventually_not_mem {s : ℕ → Set α} (hs : (∑' i, μ (s i)) ≠ 
   measure_setOf_frequently_eq_zero hs
 #align measure_theory.ae_eventually_not_mem MeasureTheory.ae_eventually_not_mem
 
+lemma NullMeasurable.measure_preimage_eq_measure_restrict_preimage_of_ae_compl_eq_const
+    {β : Type*} [MeasurableSpace β] {b : β} {f : α → β} {s : Set α}
+    (f_mble : NullMeasurable f (μ.restrict s)) (hs : f =ᵐ[Measure.restrict μ sᶜ] (fun _ ↦ b))
+    {t : Set β} (t_mble : MeasurableSet t) (ht : b ∉ t) :
+    μ (f ⁻¹' t) = μ.restrict s (f ⁻¹' t) := by
+  rw [Measure.restrict_apply₀ (f_mble t_mble)]
+  simp only [EventuallyEq, Filter.Eventually, Pi.zero_apply, Measure.ae,
+             MeasurableSet.compl_iff, Filter.mem_mk, mem_setOf_eq] at hs
+  rw [Measure.restrict_apply₀] at hs
+  · apply le_antisymm _ (measure_mono (inter_subset_left _ _))
+    apply (measure_mono (Eq.symm (inter_union_compl (f ⁻¹' t) s)).le).trans
+    apply (measure_union_le _ _).trans
+    have obs : μ ((f ⁻¹' t) ∩ sᶜ) = 0 := by
+      apply le_antisymm _ (zero_le _)
+      rw [← hs]
+      apply measure_mono (inter_subset_inter_left _ _)
+      intro x hx hfx
+      simp only [mem_preimage, mem_setOf_eq] at hx hfx
+      exact ht (hfx ▸ hx)
+    simp only [obs, add_zero, le_refl]
+  · exact NullMeasurableSet.of_null hs
+
 section Intervals
 
 theorem biSup_measure_Iic [Preorder α] {s : Set α} (hsc : s.Countable)
