522645c4d6b4ba86ebd4d8cadbfb9fd05b831c87
Antoine Chambert-Loir <antoine.chambert-loir@math.univ-paris-diderot.fr>
2023-10-03 17:57:43 +0200

    really repair copyright header

--- Mathlib/Data/Finset/Antidiagonal.lean
+++ Mathlib/Data/Finset/Antidiagonal.lean
@@ -1,6 +1,7 @@
 /-
 Copyright (c) 2023 Antoine Chambert-Loir and María Inés de Frutos-Fernández. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Antoine Chambert-Loir, María Inés de Frutos-Fernández, Bhavik Mehta
 -/
 
 import Mathlib.Data.Finset.Basic
