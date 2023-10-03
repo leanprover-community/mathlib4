443758ec51ab2d82dd9b9dfd6e7baa41b3bf4474
newell <newell.jensen@gmail.com>
2023-10-02 23:53:38 -0700

    Add file to Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -3399,6 +3399,7 @@ import Mathlib.Topology.MetricSpace.CantorScheme
 import Mathlib.Topology.MetricSpace.CauSeqFilter
 import Mathlib.Topology.MetricSpace.Closeds
 import Mathlib.Topology.MetricSpace.Completion
+import Mathlib.Topology.MetricSpace.Congruence
 import Mathlib.Topology.MetricSpace.Contracting
 import Mathlib.Topology.MetricSpace.Dilation
 import Mathlib.Topology.MetricSpace.DilationEquiv
