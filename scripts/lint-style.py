ad24f3a01c15140e0674c4fb324ed76033ab9245
Bolton Bailey <bolton.bailey@gmail.com>
2023-10-03 22:15:13 -0500

    allow simp ; rfl

--- scripts/lint-style.py
+++ scripts/lint-style.py
@@ -178,7 +178,7 @@ def four_spaces_in_second_line(lines, path):
                                           line):
             # Calculate the number of spaces before the first non-space character in the next line
             stripped_next_line = next_line.lstrip()
-            if not (next_line == '\n' or next_line.startswith("#") or stripped_next_line.startswith("--")):
+            if not (next_line == '\n' or next_line.startswith("#") or stripped_next_line.startswith("--") or "rfl" in next_line):
                 num_spaces = len(next_line) - len(stripped_next_line)
                 # The match with "| " could potentially match with a different usage of the same
                 # symbol, e.g. some sort of norm. In that case a space is not necessary, so
