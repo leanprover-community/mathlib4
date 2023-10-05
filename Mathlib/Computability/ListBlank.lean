ad8aa5c21d46433ca9b20496c1c164f7708f13b2
Bolton Bailey <bolton.bailey@gmail.com>
2023-10-05 13:02:36 -0500

    put myself as primary author

--- Mathlib/Computability/ListBlank.lean
+++ Mathlib/Computability/ListBlank.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2022 Bolton Bailey. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Mario Carneiro, Bolton Bailey
+Authors: Bolton Bailey, Mario Carneiro
 -/
 import Mathlib.Data.Fintype.Option
 import Mathlib.Data.Fintype.Prod
