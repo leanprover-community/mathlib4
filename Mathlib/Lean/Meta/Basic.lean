80f1571f18fd2aa89639b0f554ec322b379e1e55
adamtopaz <github@adamtopaz.com>
2023-10-05 15:17:33 -0600

    moved + fix docstring

--- Mathlib/Lean/Meta/Basic.lean
+++ Mathlib/Lean/Meta/Basic.lean
@@ -19,3 +19,31 @@ Restore the metavariable context after execution.
 def Lean.Meta.preservingMCtx (x : MetaM α) : MetaM α := do
   let mctx ← getMCtx
   try x finally setMCtx mctx
+
+open Lean Meta
+
+/--
+This function is similar to `forallMetaTelescopeReducing` except that
+it will construct mvars until it reaches one whose type is defeq to the given
+type `t`. It uses `forallMetaTelescopeReducing`.
+-/
+def Lean.Meta.forallMetaTelescopeReducingUntilDefEq
+    (e t : Expr) (kind : MetavarKind := MetavarKind.natural) :
+    MetaM (Array Expr × Array BinderInfo × Expr) := do
+  let mut mvs : Array Expr := #[]
+  let mut bis : Array BinderInfo := #[]
+  let (ms, bs, tp) ← forallMetaTelescopeReducing e (some 1) kind
+  unless ms.size == 1 do
+    if ms.size == 0 then throwError m!"Failed: {← ppExpr e} is not the type of a function."
+    else throwError m!"Failed"
+  mvs := ms
+  bis := bs
+  let mut out : Expr := tp
+  while !(← isDefEq (← inferType mvs.toList.getLast!) t) do
+    let (ms, bs, tp) ← forallMetaTelescopeReducing out (some 1) kind
+    unless ms.size == 1 do
+      throwError m!"Failed to find {← ppExpr t} as the type of a parameter of {← ppExpr e}."
+    mvs := mvs ++ ms
+    bis := bis ++ bs
+    out := tp
+  return (mvs, bis, out)
--- Mathlib/Tactic/ApplyAt.lean
+++ Mathlib/Tactic/ApplyAt.lean
@@ -3,7 +3,8 @@ Copyright (c) 2023 Adam Topaz. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Adam Topaz
 -/
-import Lean
+import Lean.Elab.Tactic
+import Mathlib.Lean.Meta.Basic
 
 /-!
 # Apply at
@@ -13,36 +14,10 @@ A tactic for applying functions at hypotheses.
 
 open Lean Meta Elab Tactic Term
 
-/--
-This function is similar to `forallMetaTelescopeReducingUntilDefEq` except that
-it will construct mvars until it reaches one whose type is defeq to the given
-type `t`. It uses `forallMetaTelescopeReducing`.
--/
-def Lean.Meta.forallMetaTelescopeReducingUntilDefEq
-    (e t : Expr) (kind : MetavarKind := MetavarKind.natural) :
-    MetaM (Array Expr × Array BinderInfo × Expr) := do
-  let mut mvs : Array Expr := #[]
-  let mut bis : Array BinderInfo := #[]
-  let (ms, bs, tp) ← forallMetaTelescopeReducing e (some 1) kind
-  unless ms.size == 1 do
-    if ms.size == 0 then throwError m!"Failed: {← ppExpr e} is not the type of a function."
-    else throwError m!"Failed"
-  mvs := ms
-  bis := bs
-  let mut out : Expr := tp
-  while !(← isDefEq (← inferType mvs.toList.getLast!) t) do
-    let (ms, bs, tp) ← forallMetaTelescopeReducing out (some 1) kind
-    unless ms.size == 1 do
-      throwError m!"Failed to find {← ppExpr t} as the type of a parameter of {← ppExpr e}."
-    mvs := mvs ++ ms
-    bis := bis ++ bs
-    out := tp
-  return (mvs, bis, out)
-
 namespace Mathlib.Tactic
 
 /--
-`apply t at i` will apply a function `t` at `i`.
+`apply t at i` will use forward reasoning with `t` at the hypothesis `i`.
 Explicitly, if `t : α₁ → ⋯ → αᵢ → ⋯ → αₙ` and `i` has type `αᵢ`, then this tactic will add
 metavariables/goals for any terms of `αⱼ` for `j = 1, …, i-1`,
 then replace the type of `i` with `αᵢ₊₁ → ⋯ → αₙ` by applying those metavariables and the
