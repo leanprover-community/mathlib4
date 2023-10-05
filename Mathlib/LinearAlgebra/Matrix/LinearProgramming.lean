00da03e3841c7e2860809f55f488d3d48ff716ee
madvorak <martin.dvorak@matfyz.cz>
2023-10-05 11:11:50 +0200

    actually it was not the standard form

--- Mathlib/LinearAlgebra/Matrix/LinearProgramming.lean
+++ Mathlib/LinearAlgebra/Matrix/LinearProgramming.lean
@@ -12,14 +12,14 @@ Minimizing a linear function on a region defined by linear inequalities.
 
 ## Main definitions
 
- * `StandardLP` defines a linear program in the standard form (generalization of "A x ≥ b").
- * `StandardLP.feasibles` is the set of all admissible solutions to given standard LP.
- * `StandardLP.MinAt` defines an optimum solution of given standard LP.
+ * `LinearProgram` defines a linear program in a form that generalizes "A x ≥ b".
+ * `LinearProgram.feasibles` is the set of all admissible solutions to given linear program.
+ * `LinearProgram.MinAt` defines an optimum solution of given linear program.
 
 -/
 
-/-- Linear program in the standard form. -/
-structure StandardLP (K : Type*) {V : Type*} (P : Type*)
+/-- Linear program in the form of inequalities with general variables. -/
+structure LinearProgram (K : Type*) {V : Type*} (P : Type*)
     [LinearOrderedField K] [AddCommGroup V] [Module K V] [AddTorsor V P] where
   /-- Inequality constraints (given in the form "aᵀx - b ≥ 0") -/
   constraints : List (P →ᵃ[K] K)
@@ -28,18 +28,18 @@ structure StandardLP (K : Type*) {V : Type*} (P : Type*)
 
 variable {K V P : Type*} [LinearOrderedField K] [AddCommGroup V] [Module K V] [AddTorsor V P]
 
-/-- Constructs a linear program in the standard form given a list of equalities,
-    a list of inequalities, and an objective function. -/
-def standardLP_of_equalities_inequalities_objective
+/-- Constructs a linear program given a list of equalities, a list of inequalities,
+    and an objective function. -/
+def linearProgram_of_equalities_inequalities_objective
     (equalities inequalities : List (P →ᵃ[K] K)) (objectiv : P →ᵃ[K] K) :
-    StandardLP K P where
+    LinearProgram K P where
   constraints := inequalities ++ equalities ++ equalities.map Neg.neg
   objective := objectiv
 
 /-- The set of all admissible solutions to given linear program. -/
-def StandardLP.feasibles (lp : StandardLP K P) : Set P :=
+def LinearProgram.feasibles (lp : LinearProgram K P) : Set P :=
   { x : P | ∀ a ∈ lp.constraints, 0 ≤ a x }
 
 /-- Given linear program is minimized at given point. -/
-def StandardLP.MinAt (lp : StandardLP K P) (x : P) : Prop :=
+def LinearProgram.MinAt (lp : LinearProgram K P) (x : P) : Prop :=
   x ∈ lp.feasibles ∧ ∀ y ∈ lp.feasibles, lp.objective x ≤ lp.objective y
