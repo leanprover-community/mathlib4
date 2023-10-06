6c6f666225dd20a9a4481c24e617af63aaefa20e
Chris Hughes <chrishughes24@gmail.com>
2023-10-06 16:31:51 +0100

    fix one thing

--- Mathlib/RingTheory/EuclideanDomain.lean
+++ Mathlib/RingTheory/EuclideanDomain.lean
@@ -76,7 +76,7 @@ def gcdMonoid (R) [EuclideanDomain R] [DecidableEq R] : GCDMonoid R where
   gcd_dvd_left := gcd_dvd_left
   gcd_dvd_right := gcd_dvd_right
   dvd_gcd := dvd_gcd
-  gcd_mul_lcm a b := by rw [EuclideanDomain.gcd_mul_lcm]
+  gcd_mul_lcm a b := by rw [EuclideanDomain.gcd_mul_lcm]; exact Associated.refl _
   lcm_zero_left := lcm_zero_left
   lcm_zero_right := lcm_zero_right
 #align euclidean_domain.gcd_monoid EuclideanDomain.gcdMonoid
