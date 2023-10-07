0f68badf54b118c55db280e5415a3221877c9a4e
Patrick Massot <patrickmassot@free.fr>
2023-10-07 11:59:57 -0400

    Remove unneeded lemmas that the simp linter didn't like

--- Mathlib/Data/Polynomial/Basic.lean
+++ Mathlib/Data/Polynomial/Basic.lean
@@ -758,12 +758,6 @@ theorem coeff_nat_cast_ite : (Nat.cast m : R[X]).coeff n = ite (n = 0) m 0 := by
   simp only [← C_eq_nat_cast, coeff_C, Nat.cast_ite, Nat.cast_zero]
 
 @[simp]
-theorem coeff_natCast_zero (a : ℕ) : coeff a 0 = a := coeff_monomial
-
-@[simp]
-theorem coeff_natCast_succ (a n : ℕ) : coeff a (n + 1) = 0 := by simp [coeff_C]
-
-@[simp]
 theorem coeff_ofNat_zero (a : ℕ) [a.AtLeastTwo] : coeff (no_index (OfNat.ofNat a : R[X])) 0 = a :=
   coeff_monomial
 
