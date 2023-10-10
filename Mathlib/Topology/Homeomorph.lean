8bbaf468ac03a0e725df643dc29de578dfb5c535
Michael Rothgang <rothgami@math.hu-berlin.de>
2023-10-10 13:22:13 +0200

    Use Equiv notation. Call the equivalence h instead of a.

--- Mathlib/Topology/Homeomorph.lean
+++ Mathlib/Topology/Homeomorph.lean
@@ -66,7 +66,7 @@ instance : EquivLike (X ≃ₜ Y) X Y where
 instance : CoeFun (X ≃ₜ Y) fun _ ↦ X → Y := ⟨FunLike.coe⟩
 
 @[simp]
-theorem homeomorph_mk_coe (a : Equiv X Y) (b c) : (Homeomorph.mk a b c : X → Y) = a :=
+theorem homeomorph_mk_coe (h : X ≃ₜ Y) (b c) : (Homeomorph.mk h b c : X → Y) = h :=
   rfl
 #align homeomorph.homeomorph_mk_coe Homeomorph.homeomorph_mk_coe
 
@@ -127,8 +127,8 @@ theorem symm_trans_apply (f : X ≃ₜ Y) (g : Y ≃ₜ Z) (z : Z) :
     (f.trans g).symm z = f.symm (g.symm z) := rfl
 
 @[simp]
-theorem homeomorph_mk_coe_symm (a : Equiv X Y) (b c) :
-    ((Homeomorph.mk a b c).symm : Y → X) = a.symm :=
+theorem homeomorph_mk_coe_symm (h : X ≃ₜ Y) (b c) :
+    ((Homeomorph.mk h b c).symm : Y → X) = h.symm :=
   rfl
 #align homeomorph.homeomorph_mk_coe_symm Homeomorph.homeomorph_mk_coe_symm
 
