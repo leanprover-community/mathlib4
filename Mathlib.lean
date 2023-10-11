44f823103eb8cf1803bed160cba59c1710fe05b9
David Loeffler <d.loeffler.01@cantab.net>
2023-10-11 18:40:02 +0200

    (words fail me)

--- Mathlib.lean
+++ Mathlib.lean
@@ -3360,6 +3360,7 @@ import Mathlib.Topology.Clopen
 import Mathlib.Topology.CompactOpen
 import Mathlib.Topology.Compactification.OnePoint
 import Mathlib.Topology.Compactness.Compact
+import Mathlib.Topology.Compactness.LocallyCompact
 import Mathlib.Topology.Compactness.Paracompact
 import Mathlib.Topology.Compactness.SigmaCompact
 import Mathlib.Topology.Connected
