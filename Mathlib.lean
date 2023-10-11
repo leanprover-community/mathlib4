536f35b8d75fca79982acced0a4d1dffe4e5ab6f
Eric Wieser <wieser.eric@gmail.com>
2023-10-11 09:40:24 +0000

    missing file

--- Mathlib.lean
+++ Mathlib.lean
@@ -37,6 +37,7 @@ import Mathlib.Algebra.Bounds
 import Mathlib.Algebra.Category.AlgebraCat.Basic
 import Mathlib.Algebra.Category.AlgebraCat.Limits
 import Mathlib.Algebra.Category.AlgebraCat.Monoidal
+import Mathlib.Algebra.Category.AlgebraCat.Symmetric
 import Mathlib.Algebra.Category.BoolRing
 import Mathlib.Algebra.Category.FGModuleCat.Basic
 import Mathlib.Algebra.Category.FGModuleCat.Limits
