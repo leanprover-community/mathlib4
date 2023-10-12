a46ee904e4527a14ef2349f1eefd8eaee5bff5f9
Bolton Bailey <bolton.bailey@gmail.com>
2023-10-12 16:21:18 -0500

    add lemmas

--- Mathlib/AlgebraicTopology/SimplexCategory.lean
+++ Mathlib/AlgebraicTopology/SimplexCategory.lean
@@ -315,7 +315,8 @@ theorem δ_comp_σ_of_gt {n} {i : Fin (n + 2)} {j : Fin (n + 1)} (H : Fin.castSu
   rcases j with ⟨j, hj⟩
   simp at H hk
   dsimp [δ, σ, Fin.predAbove, Fin.succAbove]
-  simp? [Fin.lt_iff_val_lt_val, Fin.ite_val, Fin.dite_val]
+  simp only [Fin.lt_iff_val_lt_val, Fin.dite_val, Fin.ite_val, Fin.coe_pred, ge_iff_le, Fin.coe_castLT, dite_eq_ite,
+    Fin.coe_castSucc, Fin.val_succ]
   split_ifs
   all_goals try simp <;> linarith
   all_goals cases k <;> simp at * <;> linarith
@@ -340,7 +341,7 @@ theorem σ_comp_σ {n} {i j : Fin (n + 1)} (H : i ≤ j) :
   rcases j with ⟨j, hj⟩
   simp at H hk
   dsimp [σ, Fin.predAbove]
-  simp? [Fin.lt_iff_val_lt_val, Fin.ite_val]
+  simp only [Fin.lt_iff_val_lt_val, Fin.ite_val, Fin.coe_pred, ge_iff_le, dite_eq_ite, Fin.coe_castLT]
   split_ifs
   all_goals try linarith
   all_goals cases k <;> simp at *; linarith
