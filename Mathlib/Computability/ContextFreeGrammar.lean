1a2f68588742bd33bc2bd3a9e3c26d30f7b05f32
Eric Wieser <wieser.eric@gmail.com>
2023-10-12 09:59:41 +0000

    universes

--- Mathlib/Computability/ContextFreeGrammar.lean
+++ Mathlib/Computability/ContextFreeGrammar.lean
@@ -12,26 +12,29 @@ This file contains the definition of a Context-Free Grammar, which is a grammar 
 nonterminal symbol on the left-hand side of each rule.
 -/
 
+universe uT uN in
 /-- Rule that rewrites a single nonterminal to any list of symbols. -/
-structure ContextFreeRule (T : Type*) (N : Type*) where
+structure ContextFreeRule (T : Type uT) (N : Type uN) where
   /-- Input nonterminal a.k.a. left-hand side -/
   input : N
   /-- Output string a.k.a. right-hand side -/
   output : List (Symbol T N)
 
+-- the `uN` universe comes first as it can't be inferred
 /-- Context-free grammar that generates words over the alphabet `T` (a type of terminals). -/
-structure ContextFreeGrammar (T : Type*) where
+structure ContextFreeGrammar.{uN,uT} (T : Type uT) where
   /-- Type of nonterminals -/
-  NT : Type*
+  NT : Type uN
   /-- Initial nonterminal -/
   initial : NT
   /-- Rewrite rules -/
   rules : List (ContextFreeRule T NT)
 
-variable {T : Type*}
+universe uT uN
+variable {T : Type uT} {N : Type uN}
 
 /-- One application of single context-free rule. -/
-inductive ContextFreeRule.RewritesTo {N : Type*} (r : ContextFreeRule T N) :
+inductive ContextFreeRule.RewritesTo (r : ContextFreeRule T N) :
     List (Symbol T N) → List (Symbol T N) → Prop
   /-- The replacement is at the start of the remaining string. -/
   | head (s : List (Symbol T N)) :
@@ -40,7 +43,7 @@ inductive ContextFreeRule.RewritesTo {N : Type*} (r : ContextFreeRule T N) :
   | cons (x : Symbol T N) {s₁ s₂ : List (Symbol T N)} (hrs : RewritesTo r s₁ s₂) :
       r.RewritesTo (x :: s₁) (x :: s₂)
 
-lemma ContextFreeRule.RewritesTo.exists_parts {N : Type _} {r : ContextFreeRule T N}
+lemma ContextFreeRule.RewritesTo.exists_parts {r : ContextFreeRule T N}
     {u v : List (Symbol T N)} (hyp : r.RewritesTo u v) :
     ∃ p : List (Symbol T N), ∃ q : List (Symbol T N),
       u = p ++ [Symbol.nonterminal r.input] ++ q ∧ v = p ++ r.output ++ q := by
@@ -53,14 +56,14 @@ lemma ContextFreeRule.RewritesTo.exists_parts {N : Type _} {r : ContextFreeRule 
     use x :: p', q'
     simp
 
-lemma ContextFreeRule.rewritesTo_of_exists_parts {N : Type _}
+lemma ContextFreeRule.rewritesTo_of_exists_parts
     (r : ContextFreeRule T N) (p q : List (Symbol T N)) :
     r.RewritesTo (p ++ [Symbol.nonterminal r.input] ++ q) (p ++ r.output ++ q) := by
   induction p with
   | nil         => exact ContextFreeRule.RewritesTo.head q
   | cons d l ih => exact ContextFreeRule.RewritesTo.cons d ih
 
-theorem ContextFreeRule.rewritesTo_iff {N : Type _} {r : ContextFreeRule T N}
+theorem ContextFreeRule.rewritesTo_iff {r : ContextFreeRule T N}
     (u v : List (Symbol T N)) :
     r.RewritesTo u v ↔
       ∃ p : List (Symbol T N), ∃ q : List (Symbol T N),
@@ -71,23 +74,23 @@ namespace ContextFreeGrammar
 
 /-- If `g` is a context-free grammar, `g.Produces u v` means that one step of context-free
     transformation sends `u` to `v`. -/
-def Produces (g : ContextFreeGrammar T) (u v : List (Symbol T g.NT)) : Prop :=
+def Produces (g : ContextFreeGrammar.{uN} T) (u v : List (Symbol T g.NT)) : Prop :=
   ∃ r ∈ g.rules, r.RewritesTo u v
 
 /-- Any number of steps of context-free transformation. -/
-abbrev Derives (g : ContextFreeGrammar T) : List (Symbol T g.NT) → List (Symbol T g.NT) → Prop :=
+abbrev Derives (g : ContextFreeGrammar.{uN} T) : List (Symbol T g.NT) → List (Symbol T g.NT) → Prop :=
   Relation.ReflTransGen g.Produces
 
 /-- The set of words that can be derived from the initial nonterminal. -/
-def language (g : ContextFreeGrammar T) : Language T :=
+def language (g : ContextFreeGrammar.{uN} T) : Language T :=
   { w | g.Derives [Symbol.nonterminal g.initial] (List.map Symbol.terminal w) }
 
 @[refl]
-lemma mem_language_iff (g : ContextFreeGrammar T) (w : List T) :
+lemma mem_language_iff (g : ContextFreeGrammar.{uN} T) (w : List T) :
     w ∈ g.language ↔ g.Derives [Symbol.nonterminal g.initial] (List.map Symbol.terminal w) := by
   rfl
 
-variable {g : ContextFreeGrammar T}
+variable {g : ContextFreeGrammar.{uN} T}
 
 lemma Derives.refl (w : List (Symbol T g.NT)) : g.Derives w w :=
   Relation.ReflTransGen.refl
