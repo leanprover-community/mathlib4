74b6c351ef9356954435a322f5b600897d7c2276
Scott Carnahan <128885296+ScottCarnahan@users.noreply.github.com>
2023-10-13 14:07:46 +0900

    Revert "update"
    This reverts commit d0f91eee2c8ed3933ea3fe6fb28f3ee60f3c5272.

--- Mathlib/RingTheory/Binomial.lean
+++ Mathlib/RingTheory/Binomial.lean
@@ -14,12 +14,12 @@ In this file we introduce binomial semirings and binomial rings, together with r
 like binomial coefficients.
 
 According to our main reference [elliott2006binomial] (which lists many equivalent conditions), a
-binomial ring is a torsion-free commutative ring `R` such that for any `x ∈ R` and any `k ∈ ℕ`, the
+binomial ring is a torsion-free commutative ring R such that for any `x ∈ R` and any `k ∈ ℕ`, the
 product `x(x-1)⋯(x-k+1)` is divisible by `k!`.  The torsion-free condition lets us divide by `k!`
 unambiguously, so we get uniquely defined binomial coefficients.
 
 The defining condition doesn't require commutativity, and we get a theory with essentially the same
-power by replacing subtraction with addition.  Thus, we consider a semiring `R` in which
+power by replacing subtraction with addition.  Thus, we consider a semiring R in which
 multiplication by factorials is injective, and demand that for all `x ∈ R` and any `k ∈ ℕ`,
 `x(x+1)⋯(x+(k-1))` is divisible by `k!`.
 The quotient is called `multichoose r k`, following the convention given for natural numbers.
@@ -31,7 +31,7 @@ The quotient is called `multichoose r k`, following the convention given for nat
 
 universe u
 
-variable {R : Type u}
+variable {R: Type u}
 
 section Semiring
 
@@ -44,19 +44,19 @@ def ascPochEval (r : R) : ℕ → R
   | 0 => 1
   | (k + 1) => (ascPochEval r k) * (r + k)
 
-theorem ascPochEval_zero (r : R) : ascPochEval r 0 = 1 := rfl
+theorem ascPochEval_zero (r : R) :  ascPochEval r 0 = 1 := rfl
 
 theorem ascPochEval_succ (r : R) (k : ℕ) :
   ascPochEval r (k.succ) = (ascPochEval r k) * (r + k) := rfl
 
-theorem ascPochEval_cast (n : ℕ) : ∀ (k : ℕ), ascPochEval (n : R) k = ascPochEval n k
+theorem ascPochEval_cast (n : ℕ) : ∀ (k : ℕ), ascPochEval (n : R) k = (ascPochEval n k)
 | 0 => by rw [ascPochEval_zero, ascPochEval_zero, Nat.cast_one]
 | (k + 1) => by
   rw [ascPochEval_succ, ascPochEval_succ, Nat.cast_mul, ascPochEval_cast n k, Nat.cast_add]
   norm_cast
 
-theorem ascPochEval_eq_ascPochhammer_eval (r : R) :
-  ∀ (k : ℕ), ascPochEval r k = Polynomial.eval r (ascPochhammer R k)
+theorem ascPochEval_eq_ascPochhammer_eval (r:R) :
+  ∀ (k:ℕ), ascPochEval r k = Polynomial.eval r (ascPochhammer R k)
   | 0 => by rw [ascPochhammer_zero, Polynomial.eval_one, ascPochEval_zero]
   | (k + 1) => by
     rw [ascPochhammer_succ_eval, ← ascPochEval_eq_ascPochhammer_eval r k, ascPochEval_succ]
@@ -99,8 +99,8 @@ theorem inj_smul_pos [BinomialSemiring R] (n : ℕ) (r s : R) (h: n ≠ 0) :
   n * r = n * s → r = s := BinomialSemiring.inj_smul_pos n r s h
 
 theorem eq_of_mul_eq_mul_factorial [BinomialSemiring R] {r s : R} (n : ℕ)
-    (h : n.factorial * r = n.factorial * s) : r = s :=
-  inj_smul_pos n.factorial r s (Nat.factorial_ne_zero n) h
+    (h : n.factorial * r = n.factorial * s) : r = s := by
+    apply inj_smul_pos n.factorial r s (Nat.factorial_ne_zero n) h
 
 /-- This is a generalization of the combinatorial multichoose function, given by choosing with
 replacement. -/
