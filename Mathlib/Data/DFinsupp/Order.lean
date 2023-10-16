176e8a5749cff7b912ddb1624f07929ba0163be2
Yaël Dillies <yael.dillies@gmail.com>
2023-10-16 09:30:22 +0000

    subset versions

--- Mathlib/Data/DFinsupp/Order.lean
+++ Mathlib/Data/DFinsupp/Order.lean
@@ -174,8 +174,10 @@ theorem le_iff : f ≤ g ↔ ∀ i ∈ f.support, f i ≤ g i :=
   le_iff' <| Subset.refl _
 #align dfinsupp.le_iff DFinsupp.le_iff
 
-lemma support_mono : Monotone (support (ι := ι) (β := α)) :=
+lemma support_monotone : Monotone (support (ι := ι) (β := α)) :=
   fun f g h a ha ↦ by rw [mem_support_iff, ←pos_iff_ne_zero] at ha ⊢; exact ha.trans_le (h _)
+
+lemma support_mono (hfg : f ≤ g) : f.support ⊆ g.support := support_monotone hfg
 
 variable (α)
 
