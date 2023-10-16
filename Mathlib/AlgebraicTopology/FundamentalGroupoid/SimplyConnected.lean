8ab9fd6c615ccbcbcdff5562c8d8b8eda0d9fbd6
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2023-10-16 14:25:42 +0200

    fix

--- Mathlib/AlgebraicTopology/FundamentalGroupoid/SimplyConnected.lean
+++ Mathlib/AlgebraicTopology/FundamentalGroupoid/SimplyConnected.lean
@@ -22,6 +22,7 @@ A topological space is simply connected if its fundamental groupoid is equivalen
   - `SimplyConnectedSpace.ofContractible` - A contractible space is simply connected
 -/
 
+universe u
 
 noncomputable section
 
@@ -64,17 +65,13 @@ theorem paths_homotopic {x y : X} (p₁ p₂ : Path x y) : Path.Homotopic p₁ p
   Quotient.eq.mp (@Subsingleton.elim (Path.Homotopic.Quotient x y) _ _ _)
 #align simply_connected_space.paths_homotopic SimplyConnectedSpace.paths_homotopic
 
-instance (priority := 100) ofContractible (Y : Type*) [TopologicalSpace Y] [ContractibleSpace Y] :
+instance (priority := 100) ofContractible (Y : Type u) [TopologicalSpace Y] [ContractibleSpace Y] :
     SimplyConnectedSpace Y where
-  equiv_unit := by
-    have H : TopCat.of Y ≃ₕ TopCat.of PUnit := (ContractibleSpace.hequiv_unit Y).some
-    have Z := FundamentalGroupoidFunctor.equivOfHomotopyEquiv H
-
-    refine ⟨((FundamentalGroupoidFunctor.equivOfHomotopyEquiv H).trans
-      FundamentalGroupoid.punitEquivDiscretePUnit).trans ?_⟩
+  equiv_unit :=
+    let H : TopCat.of Y ≃ₕ TopCat.of PUnit.{u+1} := (ContractibleSpace.hequiv Y PUnit.{u+1}).some
+    ⟨(FundamentalGroupoidFunctor.equivOfHomotopyEquiv H).trans
+      FundamentalGroupoid.punitEquivDiscretePUnit⟩
 #align simply_connected_space.of_contractible SimplyConnectedSpace.ofContractible
-
-#exit
 
 end SimplyConnectedSpace
 
