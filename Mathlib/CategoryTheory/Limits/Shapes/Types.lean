9c245b6d30643a05cd5b3b1f692bf5a2ff5a2561
Junyan Xu <junyanxumath@gmail.com>
2023-10-17 00:58:14 -0400

    failure at productIso in Shapes/Types

--- Mathlib/CategoryTheory/Limits/Shapes/Types.lean
+++ Mathlib/CategoryTheory/Limits/Shapes/Types.lean
@@ -3,7 +3,7 @@ Copyright (c) 2020 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison
 -/
-import Mathlib.CategoryTheory.Limits.Types
+import Mathlib.CategoryTheory.Limits.UnivLE
 import Mathlib.CategoryTheory.Limits.Shapes.Products
 import Mathlib.CategoryTheory.Limits.Shapes.BinaryProducts
 import Mathlib.CategoryTheory.Limits.Shapes.Terminal
