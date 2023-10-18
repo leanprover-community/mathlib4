a629cd045647f98bc3b45334652d77d4fdea1191
Komyyy <pol_tta@outlook.jp>
2023-10-18 18:13:43 +0900

    space

--- Mathlib/Data/Stream/Init.lean
+++ Mathlib/Data/Stream/Init.lean
@@ -713,7 +713,7 @@ theorem take_zero (s : Stream' α) : take 0 s = [] :=
 -- This lemma used to be simp, but we removed it from the simp set because:
 -- 1) It duplicates the (often large) `s` term, resulting in large tactic states.
 -- 2) It conflicts with the very useful `dropLast_take` lemma below (causing nonconfluence).
-theorem take_succ (n : Nat) (s : Stream' α) : take (succ n) s = head s::take n (tail s) :=
+theorem take_succ (n : Nat) (s : Stream' α) : take (succ n) s = head s :: take n (tail s) :=
   rfl
 #align stream.take_succ Stream'.take_succ
 
@@ -722,7 +722,7 @@ theorem take_succ_cons (n : Nat) (s : Stream' α) :
     take (n + 1) (a ::ₛ s) = a :: take n s := by
   simp [take_succ]
 
-theorem take_succ' {s : Stream' α} : ∀ n, s.take (n+1) = s.take n ++ [s.get n]
+theorem take_succ' {s : Stream' α} : ∀ n, s.take (n + 1) = s.take n ++ [s.get n]
   | 0 => rfl
   | n + 1 => by rw [take_succ, take_succ' n, ← List.cons_append, ← take_succ, get_tail]
 
