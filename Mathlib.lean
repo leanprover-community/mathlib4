f4770fb1a146d8942c961555e7af50b9f17af1e7
Kyle Miller <kmill31415@gmail.com>
2023-10-18 09:57:53 +0000

    feat: pp.beta (#7205)
    I do not recommend using this feature, but since Lean 3 had it, and since it's possible for us to implement it as mathlib code, here it is.
    
    This is future-proof against core implementing it in the sense that [if it gets implemented](https://github.com/leanprover/lean4/issues/715) there will likely be a name collision, and then these two modules can be deleted.
    
    
    
    
    Co-authored-by: Oliver Nash <github@olivernash.org>

--- Mathlib.lean
+++ Mathlib.lean
@@ -3581,6 +3581,8 @@ import Mathlib.Util.Imports
 import Mathlib.Util.IncludeStr
 import Mathlib.Util.LongNames
 import Mathlib.Util.MemoFix
+import Mathlib.Util.PPBeta
+import Mathlib.Util.PPBetaOption
 import Mathlib.Util.PiNotation
 import Mathlib.Util.Qq
 import Mathlib.Util.SleepHeartbeats
