45b492002e4155dc1ed8c9aa01052315f6ff23a9
Andrew Yang <the.erd.one@gmail.com>
2023-10-19 03:57:40 +0800

    fix import

--- Mathlib/AlgebraicGeometry/AffineScheme.lean
+++ Mathlib/AlgebraicGeometry/AffineScheme.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Andrew Yang
 -/
 import Mathlib.AlgebraicGeometry.GammaSpecAdjunction
-import Mathlib.AlgebraicGeometry.OpenImmersion
+import Mathlib.AlgebraicGeometry.Restrict
 import Mathlib.CategoryTheory.Limits.Opposites
 import Mathlib.RingTheory.Localization.InvSubmonoid
 
