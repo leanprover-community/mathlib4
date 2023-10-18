cdbac020ad1cc4fab8faf6b210d10a4237d91ef3
Andrew Yang <the.erd.one@gmail.com>
2023-10-18 19:47:30 +0800

    Update Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -1084,6 +1084,7 @@ import Mathlib.CategoryTheory.Limits.Shapes.ZeroObjects
 import Mathlib.CategoryTheory.Limits.SmallComplete
 import Mathlib.CategoryTheory.Limits.Types
 import Mathlib.CategoryTheory.Limits.Unit
+import Mathlib.CategoryTheory.Limits.VanKampen
 import Mathlib.CategoryTheory.Limits.Yoneda
 import Mathlib.CategoryTheory.Linear.Basic
 import Mathlib.CategoryTheory.Linear.FunctorCategory
