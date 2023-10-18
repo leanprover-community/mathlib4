f46bb28e04a784cd029a2279c9916254ac008e5a
erdOne <the.erd.one@gmail.com>
2023-10-18 16:21:59 +0000

    move stuff

--- Mathlib/CategoryTheory/Limits/VanKampen.lean
+++ Mathlib/CategoryTheory/Limits/VanKampen.lean
@@ -245,6 +245,21 @@ theorem hasStrictInitial_of_isUniversal [HasInitial C]
       intro A f
       suffices IsColimit (BinaryCofan.mk (𝟙 A) (𝟙 A)) by
         obtain ⟨l, h₁, h₂⟩ := Limits.BinaryCofan.IsColimit.desc' this (f ≫ initial.to A) (𝟙 A)
+        rcases (Category.id_comp _).symm.trans h₂ with rfl
+        exact ⟨⟨_, ((Category.id_comp _).symm.trans h₁).symm, initialIsInitial.hom_ext _ _⟩⟩
+      refine' (H (BinaryCofan.mk (𝟙 _) (𝟙 _)) (mapPair f f) f (by ext ⟨⟨⟩⟩ <;> dsimp <;> simp)
+        (mapPair_equifibered _) _).some
+      rintro ⟨⟨⟩⟩ <;> dsimp <;>
+        exact IsPullback.of_horiz_isIso ⟨(Category.id_comp _).trans (Category.comp_id _).symm⟩)
+#align category_theory.has_strict_initial_of_is_universal CategoryTheory.hasStrictInitial_of_isUniversal
+
+theorem hasStrictInitial_of_isUniversal [HasInitial C]
+    (H : IsUniversalColimit (BinaryCofan.mk (𝟙 (⊥_ C)) (𝟙 (⊥_ C)))) : HasStrictInitialObjects C :=
+  hasStrictInitialObjects_of_initial_is_strict
+    (by
+      intro A f
+      suffices IsColimit (BinaryCofan.mk (𝟙 A) (𝟙 A)) by
+        obtain ⟨l, h₁, h₂⟩ := Limits.BinaryCofan.IsColimit.desc' this (f ≫ initial.to A) (𝟙 A)
         rcases(Category.id_comp _).symm.trans h₂ with rfl
         exact ⟨⟨_, ((Category.id_comp _).symm.trans h₁).symm, initialIsInitial.hom_ext _ _⟩⟩
       refine' (H (BinaryCofan.mk (𝟙 _) (𝟙 _)) (mapPair f f) f (by ext ⟨⟨⟩⟩ <;> dsimp <;> simp)
