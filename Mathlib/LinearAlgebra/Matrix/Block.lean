17475f04ab15b3e19e9b365db28543fb83022329
Wen Yang <yang-wen@139.com>
2023-10-19 21:08:07 +0800

    restate

--- Mathlib/LinearAlgebra/Matrix/Block.lean
+++ Mathlib/LinearAlgebra/Matrix/Block.lean
@@ -1,9 +1,9 @@
 /-
 Copyright (c) 2019 Johannes Hölzl. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Johannes Hölzl, Patrick Massot, Casper Putz, Anne Baanen
+Authors: Johannes Hölzl, Patrick Massot, Casper Putz, Anne Baanen, Wen Yang
 -/
-import Mathlib.LinearAlgebra.Matrix.Determinant
+import Mathlib.LinearAlgebra.Matrix.Transvection
 import Mathlib.LinearAlgebra.Matrix.NonsingularInverse
 import Mathlib.Tactic.FinCases
 
@@ -119,6 +119,38 @@ theorem blockTriangular_blockDiagonal [DecidableEq α] (d : α → Matrix m m R)
   rw [blockDiagonal'_eq_blockDiagonal, blockTriangular_blockDiagonal']
   exact h
 #align matrix.block_triangular_block_diagonal Matrix.blockTriangular_blockDiagonal
+
+variable [DecidableEq m]
+
+theorem blockTriangular_id : BlockTriangular (1 : Matrix m m R) b := by
+  rw [← @diagonal_one]
+  exact blockTriangular_diagonal fun _ ↦ 1
+
+theorem blockTriangular_stdBasisMatrix {i j : m} (hij : b i ≤ b j) (c : R) :
+    BlockTriangular (stdBasisMatrix i j c) b := by
+  by_cases h : b i = b j
+  · unfold BlockTriangular stdBasisMatrix
+    aesop
+  · push_neg at h
+    intro r s hrs
+    unfold stdBasisMatrix
+    simp only [of_apply, ite_eq_right_iff, and_imp]
+    intro hr hs
+    rw [← hr, ← hs] at hrs
+    exact hij.trans_lt hrs |>.false.elim
+
+theorem blockTriangular_stdBasisMatrix' {i j : m} (hij : b j ≤ b i) (c : R) :
+    BlockTriangular (stdBasisMatrix i j c) (toDual ∘ b) := by
+  have : (toDual ∘ b) i ≤ (toDual ∘ b) j := hij
+  exact blockTriangular_stdBasisMatrix this c
+
+theorem blockTriangular_transvection {i j : m} (hij : b i ≤ b j) (c : R) :
+    BlockTriangular (transvection i j c) b :=
+  blockTriangular_id.add (blockTriangular_stdBasisMatrix hij c)
+
+theorem blockTriangular_transvection' {i j : m} (hij : b j ≤ b i) (c : R) :
+    BlockTriangular (transvection i j c) (OrderDual.toDual ∘ b) :=
+  blockTriangular_id.add (blockTriangular_stdBasisMatrix' hij c)
 
 end Preorder
 
