081bee05b456658c183c9c94498fda154a4d9ade
Wen Yang <yang-wen@139.com>
2023-10-19 11:49:50 +0800

    feat(LinearAlgebra/Matrix): elementary matrix is block triangular

--- Mathlib/LinearAlgebra/Matrix/ElementaryMatrix.lean
+++ Mathlib/LinearAlgebra/Matrix/ElementaryMatrix.lean
@@ -3,7 +3,7 @@ Copyright (c) 2023 Wen Yang. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Wen Yang
 -/
-import Mathlib.Data.Matrix.Basic
+import Mathlib.LinearAlgebra.Matrix.Block
 
 /-!
 # Elementary Matrix
@@ -29,3 +29,49 @@ Multiplying this matrix on the right is equivalent to
 adding `c` times the i-th column to the j-th column.
 -/
 def Elementary [AddMonoidWithOne R] (i j : n) (c : R) : Matrix n n R := 1 + Single i j c
+
+variable [CommRing R]
+
+section BlockTriangular
+
+variable {α : Type*} (b : n → α)
+
+theorem blockTriangular_id  [Preorder α] : BlockTriangular (1 : Matrix n n R) b := by
+  rw [← @diagonal_one]
+  exact blockTriangular_diagonal fun _ ↦ 1
+
+theorem blockTriangular_Single [LinearOrder α] (i j : n) (c : R) :
+    BlockTriangular (Single i j c) b ∨
+    BlockTriangular (Single i j c) (OrderDual.toDual ∘ b) := by
+  by_cases hij : b i = b j
+  · left
+    unfold BlockTriangular Single
+    aesop
+  · by_cases h : b i < b j
+    · left
+      intro r s hrs
+      unfold Single
+      simp only [of_apply, ite_eq_right_iff, and_imp]
+      intro hr hs
+      rw [hr, hs] at hrs
+      exact h.trans hrs |>.false.elim
+    · right
+      push_neg at hij
+      push_neg at h
+      replace h := hij.symm.lt_of_le h
+      replace h : (OrderDual.toDual ∘ b) i < (OrderDual.toDual ∘ b) j := h
+      intro r s hrs
+      unfold Single
+      simp only [of_apply, ite_eq_right_iff, and_imp]
+      intro hr hs
+      rw [hr, hs] at hrs
+      exact h.trans hrs |>.false.elim
+
+theorem blockTriangular_Elementary [LinearOrder α] (i j : n) (c : R) :
+    BlockTriangular (Elementary i j c) b ∨
+    BlockTriangular (Elementary i j c) (OrderDual.toDual ∘ b) :=
+  (blockTriangular_Single b i j c).imp
+    (BlockTriangular.add <| blockTriangular_id b)
+    (BlockTriangular.add <| blockTriangular_id <| OrderDual.toDual ∘ b)
+
+end BlockTriangular
