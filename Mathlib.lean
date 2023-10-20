bd6067cebb81fc90f0eb08b33bc42283490ff950
cyl <2158864003@qq.com>
2023-10-20 20:51:28 +0000

    feat: Uniformly and strongly convex functions (#7040)
    We define uniform convex functions with modulus `φ` and `m`-strongly convex functions on a normed space.
    
    Co-authored-by: Ziyu Wang <tropicalfish910@gmail.com>
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>
    
    
    
    Co-authored-by: cyl <108876608+chenyili0818@users.noreply.github.com>
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -678,6 +678,7 @@ import Mathlib.Analysis.Convex.StoneSeparation
 import Mathlib.Analysis.Convex.Strict
 import Mathlib.Analysis.Convex.StrictConvexBetween
 import Mathlib.Analysis.Convex.StrictConvexSpace
+import Mathlib.Analysis.Convex.Strong
 import Mathlib.Analysis.Convex.Topology
 import Mathlib.Analysis.Convex.Uniform
 import Mathlib.Analysis.Convolution
