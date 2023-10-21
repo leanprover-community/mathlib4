23055a5f6be417e0db226ba9f7f05e217c23b874
Kyle Miller <kmill31415@gmail.com>
2023-10-21 15:45:31 +0000

    simpler test

--- test/Use2.lean
+++
@@ -1,8 +0,0 @@
-import Mathlib.RingTheory.Ideal.Operations
-
-example {R : Type*} [CommRing R] {I J : Ideal R} {x : R} (hx : x ∈ I • J) :
-      ∃ n: ℕ, True := by
-   let P : R → Prop := fun x ↦ ∃ n : ℕ, True
-   apply Submodule.smul_induction_on (p := P) hx
-   intro a ha b hb
-   use 1
