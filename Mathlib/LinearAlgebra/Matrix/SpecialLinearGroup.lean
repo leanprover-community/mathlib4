a52faf21f62e5443fd675b511549541d1a48dc9a
Wen Yang <yang-wen@139.com>
2023-10-21 23:28:37 +0800

    minimize imports

--- Mathlib/LinearAlgebra/Matrix/SpecialLinearGroup.lean
+++ Mathlib/LinearAlgebra/Matrix/SpecialLinearGroup.lean
@@ -4,8 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Anne Baanen, Wen Yang
 -/
 import Mathlib.LinearAlgebra.GeneralLinearGroup
-import Mathlib.LinearAlgebra.Matrix.Adjugate
-import Mathlib.LinearAlgebra.Matrix.ToLin
 import Mathlib.LinearAlgebra.Matrix.Transvection
 import Mathlib.RingTheory.RootsOfUnity.Basic
 
