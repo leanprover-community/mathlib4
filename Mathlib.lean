5fb3b9f89fbd70ea4555f3c169a2e65770df970b
Joël Riou <joel.riou@universite-paris-saclay.fr>
2023-10-21 11:59:59 +0200

    feat(CategoryTheory): allow diagram chases by arguing up to refinements

--- Mathlib.lean
+++ Mathlib.lean
@@ -252,6 +252,7 @@ import Mathlib.Algebra.Homology.ShortComplex.Limits
 import Mathlib.Algebra.Homology.ShortComplex.Preadditive
 import Mathlib.Algebra.Homology.ShortComplex.PreservesHomology
 import Mathlib.Algebra.Homology.ShortComplex.QuasiIso
+import Mathlib.Algebra.Homology.ShortComplex.Refinements
 import Mathlib.Algebra.Homology.ShortComplex.RightHomology
 import Mathlib.Algebra.Homology.ShortExact.Abelian
 import Mathlib.Algebra.Homology.ShortExact.Preadditive
