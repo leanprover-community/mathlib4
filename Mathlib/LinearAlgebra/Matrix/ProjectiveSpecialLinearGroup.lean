2ee6717709862e311233e852b534953831e875a8
Wen Yang <yang-wen@139.com>
2023-10-22 23:26:59 +0800

    feat(LinearAlgebra/Matrix): `lift` for projective special linear group
    and `induction_on` (work in progress)

--- Mathlib/LinearAlgebra/Matrix/ProjectiveSpecialLinearGroup.lean
+++ Mathlib/LinearAlgebra/Matrix/ProjectiveSpecialLinearGroup.lean
@@ -31,3 +31,29 @@ abbrev ProjectiveSpecialLinearGroup : Type _ :=
 
 /-- `PSL(n, R)` is the projective special linear group `SL(n, R)/Z(SL(n, R))`.-/
 scoped[MatrixGroups] notation "PSL(" n ", " R ")" => Matrix.ProjectiveSpecialLinearGroup (Fin n) R
+
+namespace ProjectiveSpecialLinearGroup
+
+variable {n : Type u} [DecidableEq n] [Fintype n] {R : Type v} [CommRing R] [Inhabited n]
+    {α : Type*}
+
+def lift (f : SpecialLinearGroup n R → α)
+    (hf : ∀ (A B : SpecialLinearGroup n R), ∀ (c : R),
+    (c ^ Fintype.card n = 1 ∧ A.val = c • B.val) → f A = f B)
+    (g : ProjectiveSpecialLinearGroup n R) : α :=
+  Quotient.liftOn' g f fun A B hAB => by
+    rw [@QuotientGroup.leftRel_apply] at hAB
+    replace ⟨hc, hAB⟩ := SpecialLinearGroup.mem_center_iff.mp hAB
+    set c := (A⁻¹ * B).val default default
+    refine hf A B (c ^ (Fintype.card n - 1)) ⟨?hc, ?_⟩
+    · rw [@pow_right_comm, hc, @one_pow]
+    · replace hAB := congrArg (HSMul.hSMul <| c ^ (Fintype.card n - 1)) hAB
+      rw [SpecialLinearGroup.coe_mul] at hAB
+      have hn : 0 < Fintype.card n := Fintype.card_pos_iff.mpr instNonempty
+      rw [@smul_smul, ← @pow_succ', Nat.sub_add_cancel hn, hc, one_smul, ← mul_smul_comm] at hAB
+      have : A.val * A⁻¹.val * (c ^ (Fintype.card n - 1) • B.val) = A.val := by
+        rw [mul_assoc, hAB, mul_one]
+      rw [← this, ← @SpecialLinearGroup.coe_mul, @mul_inv_self,
+          SpecialLinearGroup.coe_one, @Matrix.one_mul]
+
+end ProjectiveSpecialLinearGroup
