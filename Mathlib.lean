e2375f776a73b76fa0ee41950b220ae810408bb9
thorimur <68410468+thorimur@users.noreply.github.com>
2023-10-21 22:59:43 -0400

    chore: alphabetize Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -2203,8 +2203,8 @@ import Mathlib.Lean.LocalContext
 import Mathlib.Lean.Message
 import Mathlib.Lean.Meta
 import Mathlib.Lean.Meta.Basic
-import Mathlib.Lean.Meta.CongrTheorems
 import Mathlib.Lean.Meta.Compare
+import Mathlib.Lean.Meta.CongrTheorems
 import Mathlib.Lean.Meta.DiscrTree
 import Mathlib.Lean.Meta.Simp
 import Mathlib.Lean.Name
