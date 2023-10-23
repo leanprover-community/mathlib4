38a6dbe46f309b3a1f9601c2849cfac68261b0b2
negiizhao <egresf@gmail.com>
2023-10-24 05:11:11 +0800

    fix

--- Mathlib/Algebra/Group/Ext.lean
+++ Mathlib/Algebra/Group/Ext.lean
@@ -35,8 +35,8 @@ theorem Monoid.ext {M : Type u} ⦃m₁ m₂ : Monoid M⦄ (h_mul : m₁.mul = m
   have : m₁.npow = m₂.npow := by
     ext n x
     exact @MonoidHom.map_pow M M m₁ m₂ f x n
-  rcases m₁ with @⟨@⟨⟨_⟩⟩, ⟨_⟩⟩
-  rcases m₂ with @⟨@⟨⟨_⟩⟩, ⟨_⟩⟩
+  rcases m₁
+  rcases m₂
   congr
 #align monoid.ext Monoid.ext
 #align add_monoid.ext AddMonoid.ext
