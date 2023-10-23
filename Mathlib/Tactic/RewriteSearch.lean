9ee78bf456e7b634dd984e26424771280925fb23
Scott Morrison <scott@tqft.net>
2023-10-24 10:00:25 +1100

    Update Mathlib/Tactic/RewriteSearch.lean

--- Mathlib/Tactic/RewriteSearch.lean
+++ Mathlib/Tactic/RewriteSearch.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2023 Kim Liesinger. All rights reserved.
+Copyright (c) 2023 Lean FRO, LLC. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Kim Liesinger
+Authors: Scott Morrison
 -/
 import Mathlib.Data.List.EditDistance.Estimator
 import Mathlib.Data.MLList.BestFirst
