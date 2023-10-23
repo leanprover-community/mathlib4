9de0322cf5dac5d0eb508f6d0bf88497711be114
Geoffrey Irving <irving@naml.us>
2023-10-23 17:32:38 +0100

    Update Mathlib/Analysis/Analytic/Constructions.lean
    Fix copyright line.
    
    Co-authored-by: sgouezel <sebastien.gouezel@univ-rennes1.fr>

--- Mathlib/Analysis/Analytic/Constructions.lean
+++ Mathlib/Analysis/Analytic/Constructions.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2023 David Loeffler, Geoffrey Irving. All rights reserved.
+Copyright (c) 2023 Geoffrey Irving. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: David Loeffler, Geoffrey Irving
 -/
