323957d1dd4f5cbeb2f906cc42156be8b1428043
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2023-10-23 10:02:21 +0200

    cherry pick

--- Mathlib/Topology/Separation.lean
+++ Mathlib/Topology/Separation.lean
@@ -1512,7 +1512,7 @@ class RegularSpace (X : Type u) [TopologicalSpace X] : Prop where
   regular : ∀ {s : Set X} {a}, IsClosed s → a ∉ s → Disjoint (𝓝ˢ s) (𝓝 a)
 #align regular_space RegularSpace
 
-theorem regularSpace_TFAE (X : Type u) [ TopologicalSpace X ] :
+theorem regularSpace_TFAE (X : Type u) [TopologicalSpace X] :
     List.TFAE [RegularSpace X,
       ∀ (s : Set X) a, a ∉ closure s → Disjoint (𝓝ˢ s) (𝓝 a),
       ∀ (a : X) (s : Set X), Disjoint (𝓝ˢ s) (𝓝 a) ↔ a ∉ closure s,
@@ -1560,6 +1560,12 @@ theorem RegularSpace.ofExistsMemNhdsIsClosedSubset
     (h : ∀ (a : α), ∀ s ∈ 𝓝 a, ∃ t ∈ 𝓝 a, IsClosed t ∧ t ⊆ s) : RegularSpace α :=
   Iff.mpr ((regularSpace_TFAE α).out 0 3) h
 #align regular_space.of_exists_mem_nhds_is_closed_subset RegularSpace.ofExistsMemNhdsIsClosedSubset
+
+/-- A locally compact T2 space is regular. -/
+instance [LocallyCompactSpace α] [T2Space α] : RegularSpace α := by
+  apply RegularSpace.ofExistsMemNhdsIsClosedSubset (fun x s hx ↦ ?_)
+  rcases local_compact_nhds hx with ⟨k, kx, ks, hk⟩
+  exact ⟨k, kx, hk.isClosed, ks⟩
 
 variable [RegularSpace α] {a : α} {s : Set α}
 
@@ -1683,6 +1689,60 @@ instance {ι : Type*} {π : ι → Type*} [∀ i, TopologicalSpace (π i)] [∀ 
   regularSpace_iInf fun _ => regularSpace_induced _
 
 end RegularSpace
+
+section ClosableCompactSubsetOpenSpace
+
+/-- A class of topological spaces in which, given a compact set included inside an open set, then
+the closure of the compact set is also included in the open set.
+Satisfied notably for T2 spaces and regular spaces, and useful
+when discussing classes of regular measures. -/
+class ClosableCompactSubsetOpenSpace (α : Type*) [TopologicalSpace α] : Prop :=
+  closure_subset_of_isOpen : ∀ (K U : Set α), IsCompact K → IsOpen U → K ⊆ U → closure K ⊆ U
+
+theorem IsCompact.closure_subset_of_isOpen [ClosableCompactSubsetOpenSpace α]
+    {s : Set α} (hs : IsCompact s) {u : Set α} (hu : IsOpen u) (h : s ⊆ u) :
+    closure s ⊆ u :=
+  ClosableCompactSubsetOpenSpace.closure_subset_of_isOpen s u hs hu h
+
+instance [T2Space α] : ClosableCompactSubsetOpenSpace α :=
+  ⟨fun K _U K_comp _U_open KU ↦ by rwa [K_comp.isClosed.closure_eq]⟩
+
+/-- In a (possibly non-Hausdorff) regular space, if a compact set `s` is contained in an
+open set `u`, then its closure is also contained in `u`. -/
+instance [RegularSpace α] : ClosableCompactSubsetOpenSpace α := by
+  refine ⟨fun s u hs hu h ↦ ?_⟩
+  obtain ⟨F, sF, F_closed, Fu⟩ : ∃ F, s ⊆ F ∧ IsClosed F ∧ F ⊆ u := by
+    apply hs.induction_on (p := fun t ↦ ∃ F, t ⊆ F ∧ IsClosed F ∧ F ⊆ u)
+    · exact ⟨∅, by simp⟩
+    · intro t' t ht't ⟨F, tF, F_closed, Fu⟩
+      exact ⟨F, ht't.trans tF, F_closed, Fu⟩
+    · intro t t' ⟨F, tF, F_closed, Fu⟩ ⟨F', t'F', F'_closed, F'u⟩
+      exact ⟨F ∪ F', union_subset_union tF t'F', F_closed.union F'_closed, union_subset Fu F'u⟩
+    · intro x hx
+      rcases exists_mem_nhds_isClosed_subset (hu.mem_nhds (h hx)) with ⟨F, F_mem, F_closed, Fu⟩
+      exact ⟨F, nhdsWithin_le_nhds F_mem, F, Subset.rfl, F_closed, Fu⟩
+  exact (closure_minimal sF F_closed).trans Fu
+
+/-- In a (possibly non-Hausdorff) regular locally compact space, for every containment `K ⊆ U`
+  of a compact set `K` in an open set `U`, there is a compact closed neighborhood `L` such
+  that `K ⊆ L ⊆ U`: equivalently, there is a compact closed set `L` such that `K ⊆ interior L`
+  and `L ⊆ U`. -/
+theorem exists_compact_closed_between [LocallyCompactSpace α] [ClosableCompactSubsetOpenSpace α]
+    {K U : Set α} (hK : IsCompact K) (hU : IsOpen U) (h_KU : K ⊆ U) :
+    ∃ L, IsCompact L ∧ IsClosed L ∧ K ⊆ interior L ∧ L ⊆ U := by
+  rcases exists_compact_between hK hU h_KU with ⟨L, L_comp, KL, LU⟩
+  rcases exists_compact_between hK isOpen_interior KL with ⟨M, M_comp, KM, ML⟩
+  refine ⟨closure M, ?_, isClosed_closure, ?_, ?_⟩
+  · have : closure M ∩ L = closure M := by
+      apply inter_eq_self_of_subset_left
+      exact (M_comp.closure_subset_of_isOpen isOpen_interior ML).trans interior_subset
+    rw [← this]
+    apply L_comp.inter_left isClosed_closure
+  · exact KM.trans (interior_mono subset_closure)
+  · apply M_comp.closure_subset_of_isOpen hU
+    exact ML.trans (interior_subset.trans LU)
+
+end ClosableCompactSubsetOpenSpace
 
 section T3
 
