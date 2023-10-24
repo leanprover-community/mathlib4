84874398414bc3e99c3ff4c45dcf3bb27209c876
David Renshaw <dwrenshaw@gmail.com>
2023-10-24 14:33:30 -0400

    chore: use Qq in new Gamma positivity function

--- Mathlib/Analysis/SpecialFunctions/Gamma/Basic.lean
+++ Mathlib/Analysis/SpecialFunctions/Gamma/Basic.lean
@@ -543,15 +543,15 @@ open Lean.Meta Qq in
 /-- The `positivity` extension which identifies expressions of the form `Gamma a`. -/
 @[positivity Gamma (_ : ℝ)]
 def _root_.Mathlib.Meta.Positivity.evalGamma :
-    Mathlib.Meta.Positivity.PositivityExt where eval {_ _α} zα pα e := do
-  let (.app _ (a : Q($_α))) ← withReducible (whnf e) | throwError "not Gamma ·"
+    Mathlib.Meta.Positivity.PositivityExt where eval {_ _α} zα pα (e : Q(ℝ)) := do
+  let ~q(Gamma $a) := e | throwError "failed to match on Gamma application"
   match ← Mathlib.Meta.Positivity.core zα pα a with
   | .positive pa =>
-    let pa' ← mkAppM ``Gamma_pos_of_pos #[pa]
-    pure (.positive pa')
+    have pa' : Q(0 < $a) := pa
+    pure (.positive (q(Gamma_pos_of_pos $pa') : Q(0 < $e)))
   | .nonnegative pa =>
-    let pa' ← mkAppM ``Gamma_nonneg_of_nonneg #[pa]
-    pure (.nonnegative pa')
+    have pa' : Q(0 ≤ $a) := pa
+    pure (.nonnegative (q(Gamma_nonneg_of_nonneg $pa') : Q(0 ≤ $e)))
   | _ => pure .none
 
 /-- The Gamma function does not vanish on `ℝ` (except at non-positive integers, where the function
