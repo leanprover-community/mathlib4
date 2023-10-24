6df429e096e72449aef47d9a345e68a7bbda4b63
thorimur <68410468+thorimur@users.noreply.github.com>
2023-10-23 22:44:02 -0400

    update to thorimur/std4:lean-pr-testing-2679

--- lakefile.lean
+++ lakefile.lean
@@ -46,7 +46,7 @@ lean_exe checkYaml where
 meta if get_config? doc = some "on" then -- do not download and build doc-gen4 by default
 require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
 
-require std from git "https://github.com/thorimur/std4" @ "lean4-2679"
+require std from git "https://github.com/thorimur/std4" @ "lean-pr-testing-2679"
 require Qq from git "https://github.com/gebner/quote4" @ "master"
 require aesop from git "https://github.com/semorrison/aesop" @ "use_replay"
 require Cli from git "https://github.com/mhuisi/lean4-cli.git" @ "nightly"
