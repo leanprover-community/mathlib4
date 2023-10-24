e8bf5198dc7d824962e74e7f0dcb926e932ba433
Gareth Ma <grhkm21@gmail.com>
2023-10-24 20:47:43 +0100

    fixing imports and CI

--- Mathlib.lean
+++ Mathlib.lean
@@ -3230,6 +3230,7 @@ import Mathlib.Tactic.Says
 import Mathlib.Tactic.ScopedNS
 import Mathlib.Tactic.Set
 import Mathlib.Tactic.SetLike
+import Mathlib.Tactic.Setm
 import Mathlib.Tactic.SimpIntro
 import Mathlib.Tactic.SimpRw
 import Mathlib.Tactic.Simps.Basic
