35c94d5bee34a8f9dc9c559e5c420d0d5ebd016c
samvang <59202064+samvang@users.noreply.github.com>
2023-10-24 20:38:46 +0200

    small comments

--- Mathlib/Computability/Transducer.lean
+++ Mathlib/Computability/Transducer.lean
@@ -1,10 +1,44 @@
+/-
+Copyright (c) 2023 Sam v. Gool. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Author: Sam v. Gool
+-/
 import Mathlib.Computability.Language
 import Mathlib.Data.Rel
 
-open Computability
+/-!
+# Transducers
+
+This file defines a notion of *transducer* in the sense of Büchi, their semantics, and how
+they can be composed.
+
+## Main declarations
+
+TODO
+
+## Motivation
+
+TODO
+
+## Implementation notes
+
+TODO
+
+## References
+
+* [Gastin]
+
+## Tags
+
+-/
+
+open Set Computability
+
+namespace Transducer
+
 universe u v
 
-def word (A : Type u) := ℕ → A
+def stream (A : Type u) := ℕ → A
 
 /-- A transducer consists of a type `state` together with a transition relation
 between states and letters from the input alphabet `A`, a labeling function `out`
@@ -18,27 +52,28 @@ structure Transducer (A B : Type u) where
   final : Set (Set state)
 
 variable {A B C : Type u}
-/-- A run of a transducer `T` on a word `w ∈ word A`  consists of a function `stateAt` which
+/-- A run of a transducer `T` on a stream `w ∈ stream A`  consists of a function `stateAt` which
 records the state of the transducer at time `t ∈ ℕ`, together with the initial condition
 `initialCond` that the run starts in the start state, the condition `transitionCond` that the
 run follows the transition relation, and the condition `finalCond` that the run visits each
 of the final sets infinitely often. -/
-structure Run (T : Transducer A B) (wᵢ : word A) where
-  stateAt : word T.state
+structure Run (T : Transducer A B) (wᵢ : stream A) where
+  stateAt : stream T.state
   initialCond : stateAt 0 = T.start
   transitionCond : ∀t, (stateAt t, wᵢ t, stateAt (t+1)) ∈ T.transition
   finalCond : ∀ F ∈ T.final, ∀ t, ∃t' ≥ t, stateAt t ∈ F
 
 /-- The output of a run `r` is the sequence of outputs of transitions of the run. -/
-def output {T : Transducer A B} {wᵢ : word A} (r : Run T wᵢ) :=
+def output {T : Transducer A B} {wᵢ : stream A} (r : Run T wᵢ) :=
   λ t ↦ T.out (⟨(r.stateAt t, wᵢ t, r.stateAt (t+1)), r.transitionCond t⟩)
 
-/-- The semantics of a transducer `T` is the relation which relates a word `u` to a word `v`
+/-- The semantics of a transducer `T` is the relation which relates a stream `u` to a stream `v`
 iff `v` is the output of a run of `T` on `u`.-/
-def semantics (T : Transducer A B) : Rel (word A) (word B) :=
+def semantics (T : Transducer A B) : Rel (stream A) (stream B) :=
   λ u v ↦ ∃ r : Run T u, output r = v
 
 section Composition
+-- TODO: this is very messy for now,  wanted to get it sorry-free first.
 def forget (T₁ : Transducer A B) (T₂ : Transducer B C) : T₁.transition × (T₂.state × T₂.state) → (T₁.state × A × T₁.state) × (T₂.state × T₂.state) := λ ⟨⟨x,_⟩, y⟩ ↦ ⟨x,y⟩
 
 def assemble {Q Q'} : (Q × A × Q) × (Q' × Q') → (Q × Q') × A × (Q × Q') :=
@@ -48,16 +83,6 @@ def good (T₁ : Transducer A B) (T₂ : Transducer B C) := λ (t, (p',q')) ↦ 
 
 def transition_compose (T₁ : Transducer A B) (T₂ : Transducer B C) :=
   assemble '' ((forget T₁ T₂) '' (good T₁ T₂))
-
--- theorem comp_trans (T₁ : Transducer A B) (T₂ : Transducer B C) (p q : T₁.state) (p' q' : T₂.state)
--- (a : A) (b : B) (h : (p,a,q) ∈ T₁.transition) (hb : b = T₁.out ⟨(p,a,q),h⟩)
--- (h2 : (p',b,q') ∈ T₂.transition) : ((p,p'),a,(q,q')) ∈ transition_compose T₁ T₂ := by
---   simp_rw [transition_compose, Set.mem_image]
---   use ((p,a,q),(p',q'))
---   constructor
---   simp [good]
---   aesop
---   rw [assemble]
 
 theorem comp_trans1 (T₁ : Transducer A B) (T₂ : Transducer B C)
 (p q : T₁.state) (p' q' : T₂.state) (a : A)
