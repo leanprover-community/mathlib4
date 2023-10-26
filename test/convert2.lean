16cf3e8902a7c9743e1936bed8438d70e62a97c6
Scott Morrison <scott.morrison@gmail.com>
2023-10-26 15:44:18 +1100

    better comment

--- test/convert2.lean
+++ test/convert2.lean
@@ -4,7 +4,9 @@ import Mathlib.Data.Nat.Interval
 
 open BigOperators
 
--- Prior to #7945 this failed with `(kernel) declaration has metavariables '_example'`
+-- Prior to #7945 this failed with `(kernel) declaration has metavariables '_example'`.
+-- There is in fact an internal error being suppressed here, that is reported only with
+-- `set_option trace.congr! true`
 example (h : False) (_h₁ : ∑ k in Finset.Icc 0 122, 0 = 0) : 0 ∣ 1 := by
   apply Nat.dvd_of_mul_dvd_mul_left zero_lt_one
   convert dvd_mul_left 0 1
