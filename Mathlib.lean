820e235c3df15fadcb8dc08dd49f803db0936ed4
Yaël Dillies <yael.dillies@gmail.com>
2023-10-26 21:10:36 +0000

    feat: Sublattices (#7549)
    Basic API for sublattices (there's more to come).

--- Mathlib.lean
+++ Mathlib.lean
@@ -2803,6 +2803,7 @@ import Mathlib.Order.RelIso.Group
 import Mathlib.Order.RelIso.Set
 import Mathlib.Order.RelSeries
 import Mathlib.Order.SemiconjSup
+import Mathlib.Order.Sublattice
 import Mathlib.Order.SuccPred.Basic
 import Mathlib.Order.SuccPred.IntervalSucc
 import Mathlib.Order.SuccPred.Limit
