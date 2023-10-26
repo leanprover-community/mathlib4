dd24d3ee1156a4e604272a7426ad37e815fe972d
Eric Wieser <wieser.eric@gmail.com>
2023-10-26 21:43:30 +0000

    fix test

--- test/convert2.lean
+++ test/convert2.lean
@@ -2,9 +2,10 @@ import Mathlib.Data.List.Defs
 import Mathlib.Data.Nat.Basic
 
 -- Prior to #7945 this failed with `(kernel) declaration has metavariables '_example'`.
--- There is in fact an internal error being suppressed here, that is reported only with
--- `set_option trace.congr! true`
-example (h : False) (_h₁ : ((List.range 128).map (fun _ => 0)).sum = 0) : 0 ∣ 1 := by
+/--
+error: maximum recursion depth has been reached (use `set_option maxRecDepth <num>` to increase limit)
+-/
+#guard_msgs (error) in
+example (_h₁ : ((List.range 128).map (fun _ => 0)).sum = 0) : 0 ∣ 1 := by
   apply Nat.dvd_of_mul_dvd_mul_left Nat.zero_lt_one
   convert Nat.dvd_mul_left 0 1
-  all_goals contradiction
