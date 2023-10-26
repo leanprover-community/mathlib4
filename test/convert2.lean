fed391fa224125233d1162a84a033de011c4aa49
Scott Morrison <scott.morrison@gmail.com>
2023-10-26 15:42:02 +1100

    comment test

--- test/convert2.lean
+++ test/convert2.lean
@@ -4,6 +4,7 @@ import Mathlib.Data.Nat.Interval
 
 open BigOperators
 
+-- Prior to #7945 this failed with `(kernel) declaration has metavariables '_example'`
 example (h : False) (_h₁ : ∑ k in Finset.Icc 0 122, 0 = 0) : 0 ∣ 1 := by
   apply Nat.dvd_of_mul_dvd_mul_left zero_lt_one
   convert dvd_mul_left 0 1
