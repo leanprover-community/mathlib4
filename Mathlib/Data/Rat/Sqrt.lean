c976b8b1d80e01f57da2ef43fa387e42e34ba09d
Eric Wieser <wieser.eric@gmail.com>
2023-10-26 10:22:47 +0000

    whitespace

--- Mathlib/Data/Rat/Sqrt.lean
+++ Mathlib/Data/Rat/Sqrt.lean
@@ -42,7 +42,7 @@ theorem sqrt_nonneg (q : ℚ) : 0 ≤ Rat.sqrt q :=
       Int.coe_nat_nonneg _
 #align rat.sqrt_nonneg Rat.sqrt_nonneg
 
-/-- `IsSquare` can be decided on `ℚ` by checking against the square root.-/
+/-- `IsSquare` can be decided on `ℚ` by checking against the square root. -/
 instance : DecidablePred (@IsSquare ℚ _) :=
   fun m => decidable_of_iff' (sqrt m * sqrt m = m) <| by
     simp_rw [←exists_mul_self m, IsSquare, eq_comm]
