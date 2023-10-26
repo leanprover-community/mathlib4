166d38f8d5a89e1ac1003a50f95188b657894c2c
Eric Wieser <wieser.eric@gmail.com>
2023-10-26 15:04:11 +0000

    remove even more dependencies from the test

--- test/convert2.lean
+++ test/convert2.lean
@@ -1,11 +1,10 @@
-import Mathlib.Algebra.BigOperators.Basic
-
-open BigOperators
+import Mathlib.Data.List.Defs
+import Mathlib.Data.Nat.Basic
 
 -- Prior to #7945 this failed with `(kernel) declaration has metavariables '_example'`.
 -- There is in fact an internal error being suppressed here, that is reported only with
 -- `set_option trace.congr! true`
-example (h : False) (_h₁ : ∑ k in Finset.range 128, 0 = 0) : 0 ∣ 1 := by
-  apply Nat.dvd_of_mul_dvd_mul_left zero_lt_one
-  convert dvd_mul_left 0 1
+example (h : False) (_h₁ : ((List.range 128).map (fun _ => 0)).sum = 0) : 0 ∣ 1 := by
+  apply Nat.dvd_of_mul_dvd_mul_left Nat.zero_lt_one
+  convert Nat.dvd_mul_left 0 1
   all_goals contradiction
