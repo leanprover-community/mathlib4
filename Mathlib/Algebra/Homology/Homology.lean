e174a803e56077c60754f80f1c55236228a17cfb
Joël Riou <joel.riou@universite-paris-saclay.fr>
2023-10-27 15:15:53 +0200

    fixed docstring

--- Mathlib/Algebra/Homology/Homology.lean
+++ Mathlib/Algebra/Homology/Homology.lean
@@ -21,10 +21,11 @@ as `cyclesMap' f i` and `boundariesMap f i`.
 As a consequence we construct `homologyFunctor' i : HomologicalComplex V c ⥤ V`,
 computing the `i`-th homology.
 
-Note: Some definitions (specifically, names containing components `homology`, `cycles`, and `quasiIso`) in this file have the suffix `'` so as to allow the development
-of the new homology API of homological complex (starting from
+Note: Some definitions (specifically, names containing components `homology`, `cycles`)
+in this file have the suffix `'` so as to allow the development of the
+new homology API of homological complex (starting from
 `Algebra.Homology.ShortComplex.HomologicalComplex`). It is planned that these definitions
-shall be removed and removed by the new API.
+shall be removed and replaced by the new API.
 
 -/
 
