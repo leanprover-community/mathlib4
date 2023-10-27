24f21185ef985c7a29767bf676f3661b5d3147a0
newell <newell.jensen@gmail.com>
2023-10-26 23:28:40 -0700

    Update definition used for `KleinFourGroup` to use `Multiplicative (ZMod 2) x (ZMod 2)`.

--- Mathlib/GroupTheory/SpecificGroups/KleinFour.lean
+++ Mathlib/GroupTheory/SpecificGroups/KleinFour.lean
@@ -15,87 +15,73 @@ produces the third one.
 
 ## Main definitions
 
-* `KleinFourGroup` is the Klein four-group with elements `e`, `a`, `b`, and `c`.
+* `KleinFourGroup` is the Klein four-group with elements `e`, `a`, `b`, and `c`,
+  which is defined in terms of `Multiplicative ((ZMod 2) × (ZMod 2))`.
 
 ## Main results
 
-* `KleinFourGroup_eq_DihedralGroup2` proves the Klein four-group is `MulEquiv` to
+* `KleinFourGroup_eq_DihedralGroup2` proves the Klein four-group is isomorphic to
   `DihedralGroup 2`.
-* `KleinFourGroup_eq_ProdZMod2` proves the Klein four-group is `Equiv` to the direct
-  product `(ZMod 2) × (ZMod 2)`.
 
 ## References
 
 * https://en.wikipedia.org/wiki/Klein_four-group
+* https://en.wikipedia.org/wiki/Alternating_group
+
+## TODO
+
+* Prove `KleinFourGroup` is isomorphic to the normal subgroup of `alternatingGroup (Fin 4)`
+  with the permutation cycles `V = {(), (1 2)(3 4), (1 3)(2 4), (1 4)(2 3)}`.  This is the kernel
+  of the surjection of `alternatingGroup (Fin 4)` onto `alternatingGroup (Fin 3) ≃ (ZMod 3)`.
+  In other words, we have the exact sequence `V → A₄ → A₃`.
+
+* The outer automorphism group of `A₆` is the Klein four-group `V = (ZMod 2) × (ZMod 2)`,
+  and is related to the outer automorphism of `S₆`. The extra outer automorphism in `A₆`
+  swaps the 3-cycles (like `(1 2 3)`) with elements of shape `3²` (like `(1 2 3)(4 5 6)`).
 
 ## Tags
 non-cyclic abelian group
 -/
 
 /-- Klein four-group. -/
-inductive KleinFourGroup : Type
-  | e : KleinFourGroup
-  | a : KleinFourGroup
-  | b : KleinFourGroup
-  | c : KleinFourGroup
-  deriving DecidableEq
+abbrev KleinFourGroup := Multiplicative ((ZMod 2) × (ZMod 2))
 
 namespace KleinFourGroup
 
 open DihedralGroup Equiv
 
-/-- Multiplication of the Klein four-group. -/
-private def mul : KleinFourGroup → KleinFourGroup → KleinFourGroup
-  | e, x => x
-  | x, e => x
-  | a, a => e
-  | a, b => c
-  | a, c => b
-  | b, a => c
-  | b, b => e
-  | b, c => a
-  | c, a => b
-  | c, b => a
-  | c, c => e
+/-- Elements of the KleinFourGroup.-/
+def e := (1 : KleinFourGroup)
+def a := Multiplicative.ofAdd ((0, 1) : KleinFourGroup)
+def b := Multiplicative.ofAdd ((1, 0) : KleinFourGroup)
+def c := Multiplicative.ofAdd ((1, 1) : KleinFourGroup)
 
-/-- `1` is the identity matrix. -/
+/-- `(0, 0)` is the identity element. -/
 private def one : KleinFourGroup :=
   e
 
 instance : Inhabited (KleinFourGroup) :=
   ⟨one⟩
 
-/-- The inverse of an element of the Klein four-group. -/
-private def inv : KleinFourGroup → KleinFourGroup
-  | e => e
-  | a => a
-  | b => b
-  | c => c
+@[simp]
+theorem one_def : ((0, 0) : KleinFourGroup) = (1 : KleinFourGroup) :=
+  rfl
 
-/-- The group structure on `KleinFourGroup`. -/
-instance : Group (KleinFourGroup) where
-  mul := mul
-  mul_assoc := by
-    rintro (a | a) (b | b) (c | c) <;> simp only [(· * ·), mul]
-  one := one
-  one_mul := by rintro (a | a) <;> rfl
-  mul_one := by rintro (a | a) <;> rfl
-  inv := inv
-  mul_left_inv := by rintro (a | a) <;> rfl
+/-- `KleinFourGroup` is a group. -/
+instance : Group (KleinFourGroup) :=
+  Multiplicative.group
 
 /-- Klein four-group is an abelian group. -/
-instance commGroup : CommGroup (KleinFourGroup) where
-  mul_comm k₁ k₂ := by
-    cases k₁ <;> cases k₂ <;> rfl
+instance commGroup : CommGroup (KleinFourGroup) :=
+  inferInstance
 
 /-- Klein four-group is a finite group. -/
-instance fintype : Fintype (KleinFourGroup) where
-  elems := [e, a, b, c].toFinset
-  complete i := by cases i <;> simp
+instance fintype : Fintype (KleinFourGroup) :=
+  inferInstance
 
 /-- Klein four-group is a group of order 4. -/
-theorem card : Fintype.card (KleinFourGroup) = 4 := by
-  simp
+theorem card : Fintype.card (KleinFourGroup) = 4 :=
+  rfl
 
 /-- Klein four-group is a group of order 4. -/
 theorem nat_card : Nat.card (KleinFourGroup) = 4 := by
@@ -113,26 +99,35 @@ theorem nat_card : Nat.card (KleinFourGroup) = 4 := by
 @[simp] theorem orderOf_a : orderOf a = 2 := by
   apply orderOf_eq_prime
   · exact a_order_two
-  · simp
+  · intro ha
+    rw [a, ← one_def] at ha
+    simp only [one_def, ofAdd_eq_one] at ha
 
 @[simp] theorem orderOf_b : orderOf b = 2 := by
   apply orderOf_eq_prime
   · exact b_order_two
-  · simp
+  · intro hb
+    rw [b, ← one_def] at hb
+    simp only [one_def, ofAdd_eq_one] at hb
 
 @[simp] theorem orderOf_c : orderOf c = 2 := by
   apply orderOf_eq_prime
   · exact c_order_two
-  · simp
-
-@[simp] theorem one_def : (1 : KleinFourGroup) = e :=
-  rfl
+  · intro hc
+    rw [c, ← one_def] at hc
+    simp only [one_def, ofAdd_eq_one] at hc
 
 theorem exponent : Monoid.exponent (KleinFourGroup) = 2 := by
   have : Monoid.exponent (KleinFourGroup) = lcm 2 2 := by
     apply Nat.dvd_antisymm
     · apply Monoid.exponent_dvd_of_forall_pow_eq_one
-      simp
+      simp only [lcm_same, normalize_apply, normUnit_eq_one, Units.val_one, mul_one]
+      intro k
+      match k with
+      | (0, 0) => rfl
+      | (0, 1) => rfl
+      | (1, 0) => rfl
+      | (1, 1) => rfl
     · apply lcm_dvd
       repeat {convert Monoid.order_dvd_exponent a; exact orderOf_a.symm}
   assumption
@@ -146,34 +141,25 @@ theorem notIsCyclic : ¬ IsCyclic (KleinFourGroup) := by
 
 /-- Klein four-group is isomorphic to the Dihedral group of order 4. -/
 def KleinFourGroup_eq_DihedralGroup2 : KleinFourGroup ≃* DihedralGroup 2 where
-  toFun k := match k with
-    | e => (1 : DihedralGroup 2)
-    | a => (sr 1 : DihedralGroup 2)
-    | b => (r 1 : DihedralGroup 2)
-    | c => (sr 1 * r 1 : DihedralGroup 2)
-  invFun d := match d with
-    | 1 => e
-    | (sr 1 : DihedralGroup 2) => a
-    | (r 1 : DihedralGroup 2) => b
-    | (sr 1 * r 1 : DihedralGroup 2) => c
-  left_inv := by simp
+  toFun k :=
+    match k with
+    | (0, 0) => 1
+    | (0, 1) => sr 1
+    | (1, 0) => r 1
+    | (1, 1) => sr 1 * r 1
+  invFun d :=
+    match d with
+    | 1 => (0, 0)
+    | sr 1 => (0, 1)
+    | r 1 => (1, 0)
+    | sr 1 * r 1 => (1, 1)
+  left_inv k := by
+    match k with
+    | (0, 0) => rfl
+    | (0, 1) => rfl
+    | (1, 0) => rfl
+    | (1, 1) => rfl
   right_inv := by simp
   map_mul' := by simp
-
-/-- Klein four-group is equivalent to direct product of two cyclic groups of
-order two. -/
-def KleinFourGroup_eq_ProdZMod2 : KleinFourGroup ≃ (ZMod 2) × (ZMod 2) where
-  toFun k := match k with
-    | e => (0, 0)
-    | a => (0, 1)
-    | b => (1, 0)
-    | c => (1, 1)
-  invFun d := match d with
-    | (0, 0) => e
-    | (0, 1) => a
-    | (1, 0) => b
-    | (1, 1) => c
-  left_inv := by simp
-  right_inv := by simp
 
 end KleinFourGroup
