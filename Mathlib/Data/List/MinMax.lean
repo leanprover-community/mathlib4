a73745e10653a2e906eb78ae93bd8a9b0233b729
Seasawher <tellagoodstory@icloud.com>
2023-10-27 18:14:19 +0900

    swap `maximum` and `minimum`

--- Mathlib/Data/List/MinMax.lean
+++ Mathlib/Data/List/MinMax.lean
@@ -449,15 +449,15 @@ theorem minimum_of_length_pos_le_iff (h : 0 < l.length) :
     minimum_of_length_pos h ≤ b ↔ l.minimum ≤ b :=
   le_maximum_of_length_pos_iff (α := αᵒᵈ) h
 
-theorem minimum_of_length_pos_mem (h : 0 < l.length) :
-    minimum_of_length_pos h ∈ l := by
-  apply minimum_mem
-  simp only [coe_minimum_of_length_pos]
-
 theorem maximum_of_length_pos_mem (h : 0 < l.length) :
     maximum_of_length_pos h ∈ l := by
   apply maximum_mem
   simp only [coe_maximum_of_length_pos]
+
+theorem minimum_of_length_pos_mem (h : 0 < l.length) :
+    minimum_of_length_pos h ∈ l := by
+  apply minimum_mem
+  simp only [coe_minimum_of_length_pos]
 
 theorem le_maximum_of_length_pos_of_mem (h : a ∈ l) (w : 0 < l.length) :
     a ≤ l.maximum_of_length_pos w := by
