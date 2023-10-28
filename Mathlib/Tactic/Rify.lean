af00ea83c6daaa93c5627b3df640a8e2dbc4bdfb
Patrick Massot <patrickmassot@free.fr>
2023-10-27 20:08:22 -0400

    Update Mathlib/Tactic/Rify.lean
    Co-authored-by: Timo <timorcb@gmail.com>

--- Mathlib/Tactic/Rify.lean
+++ Mathlib/Tactic/Rify.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2023 Patrick Massit. All rights reserved.
+Copyright (c) 2023 Patrick Massot. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Moritz Doll, Mario Carneiro, Robert Y. Lewis, Patrick Massot
 -/
