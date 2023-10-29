8ba2c15a192b2b01be9138be3ca13e8807cd3943
Junyan Xu <junyanxumath@gmail.com>
2023-10-29 02:34:15 -0400

    temp

---
+++ Mathlib/AlgebraicTopology/FundamentalGroupoid/VanKampen.lean
@@ -0,0 +1,7 @@
+/-
+Copyright (c) 2023 Junyan Xu. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Junyan Xu
+-/
+import Mathlib.AlgebraicTopology.FundamentalGroupoid.Basic
+import Mathlib.Topology.Sheaves.SheafCondition.PairwiseIntersections
