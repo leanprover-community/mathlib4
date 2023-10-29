f167a7050ce92c835275f1c7e9577d0e053459d1
Junyan Xu <junyanxumath@gmail.com>
2023-10-29 02:23:29 -0400

    no need to change import

--- Mathlib/RingTheory/Adjoin/Field.lean
+++ Mathlib/RingTheory/Adjoin/Field.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Chris Hughes
 -/
 import Mathlib.Data.Polynomial.Splits
-import Mathlib.RingTheory.Adjoin.PowerBasis
+import Mathlib.RingTheory.Adjoin.Basic
 import Mathlib.RingTheory.AdjoinRoot
 
 #align_import ring_theory.adjoin.field from "leanprover-community/mathlib"@"c4658a649d216f57e99621708b09dcb3dcccbd23"
