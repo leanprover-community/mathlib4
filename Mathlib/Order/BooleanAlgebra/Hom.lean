5e0e05748df32e5ad209d3f4b4b11eea59795c47
Sam van G <59202064+samvang@users.noreply.github.com>
2023-10-30 11:02:11 +0100

    create new file

---
+++ Mathlib/Order/BooleanAlgebra/Hom.lean
@@ -0,0 +1,7 @@
+/-
+Copyright (c) 2023 Sam van Gool, Filippo A. E. Nuccio. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Sam van Gool, Filippo A. E. Nuccio
+-/
+
+/-! # Morphisms between Boolean algebras and related notions -/
