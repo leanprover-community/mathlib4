ff14014878631787f83068abfaec65d2d988e512
Eric Wieser <wieser.eric@gmail.com>
2023-11-01 09:32:06 +0000

    restore original order

--- Mathlib/GroupTheory/Subsemigroup/Center.lean
+++ Mathlib/GroupTheory/Subsemigroup/Center.lean
@@ -58,8 +58,6 @@ structure IsMulCentral [Mul M] (z : M) : Prop where
 
 namespace IsMulCentral
 
-section IsMulCentral
-
 variable {a b c : M} [Mul M]
 
 -- c.f. Commute.left_comm
@@ -74,13 +72,10 @@ protected theorem right_comm (h : IsMulCentral c) (a b) : a * b * c = a * c * b 
 
 end IsMulCentral
 
-end IsMulCentral
+namespace Set
 
 section Mul
-
 variable (M) [Mul M]
-
-namespace Set
 
 /-- The center of a magma. -/
 @[to_additive addCenter " The center of an additive magma. "]
@@ -124,84 +119,27 @@ theorem mul_mem_center [Mul M] {z₁ z₂ : M} (hz₁ : z₁ ∈ Set.center M) (
 #align set.mul_mem_center Set.mul_mem_center
 #align set.add_mem_add_center Set.add_mem_addCenter
 
-end Set
-
-namespace Subsemigroup
-
-/-- The center of a semigroup `M` is the set of elements that commute with everything in `M` -/
-@[to_additive
-      "The center of a semigroup `M` is the set of elements that commute with everything in `M`"]
-def center : Subsemigroup M where
-  carrier := Set.center M
-  mul_mem' := Set.mul_mem_center
-#align subsemigroup.center Subsemigroup.center
-#align add_subsemigroup.center AddSubsemigroup.center
-
--- porting note: `coe_center` is now redundant
-#noalign subsemigroup.coe_center
-#noalign add_subsemigroup.coe_center
-
-variable {M}
-
-/-- The center of a magma is commutative and associative. -/
-@[to_additive "The center of an additive magma is commutative and associative."]
-instance center.commSemigroup : CommSemigroup (center M) where
-  mul_assoc _ b _ := Subtype.ext <| b.2.mid_assoc _ _
-  mul_comm a _ := Subtype.ext <| a.2.comm _
-#align subsemigroup.center.comm_semigroup Subsemigroup.center.commSemigroup
-#align add_subsemigroup.center.add_comm_semigroup AddSubsemigroup.center.addCommSemigroup
-
-end Subsemigroup
-
 end Mul
 
 section Semigroup
-
 variable [Semigroup M]
 
 @[to_additive]
-theorem Semigroup.mem_center_iff {z : M} :
+theorem _root_.Semigroup.mem_center_iff {z : M} :
     z ∈ Set.center M ↔ ∀ g, g * z = z * g := ⟨fun a g ↦ by rw [IsMulCentral.comm a g],
   fun h ↦ ⟨fun _ ↦ (Commute.eq (h _)).symm, fun _ _ ↦ (mul_assoc z _ _).symm,
   fun _ _ ↦ mul_assoc _ z _, fun _ _ ↦ mul_assoc _ _ z⟩ ⟩
 
 variable (M)
 
-namespace Set
-
 -- TODO Add `instance : Decidable (IsMulCentral a)` for `instance decidableMemCenter [Mul M]`
 instance decidableMemCenter [∀ a : M, Decidable <| ∀ b : M, b * a = a * b] :
     DecidablePred (· ∈ center M) := fun _ => decidable_of_iff' _ (Semigroup.mem_center_iff)
 #align set.decidable_mem_center Set.decidableMemCenter
 
-end Set
-
-namespace Subsemigroup
-
-variable {M} [Semigroup M]
-
-@[to_additive]
-theorem mem_center_iff {z : M} : z ∈ center M ↔ ∀ g, g * z = z * g := by
-  rw [← Semigroup.mem_center_iff]
-  exact Iff.rfl
-#align subsemigroup.mem_center_iff Subsemigroup.mem_center_iff
-#align add_subsemigroup.mem_center_iff AddSubsemigroup.mem_center_iff
-
-@[to_additive]
-instance decidableMemCenter (a) [Decidable <| ∀ b : M, b * a = a * b] :
-    Decidable (a ∈ center M) :=
-  decidable_of_iff' _ Semigroup.mem_center_iff
-#align subsemigroup.decidable_mem_center Subsemigroup.decidableMemCenter
-#align add_subsemigroup.decidable_mem_center AddSubsemigroup.decidableMemCenter
-
-end Subsemigroup
-
 end Semigroup
 
 section CommSemigroup
-
-namespace Set
-
 variable (M)
 
 @[to_additive (attr := simp) addCenter_eq_univ]
@@ -210,23 +148,7 @@ theorem center_eq_univ [CommSemigroup M] : center M = univ :=
 #align set.center_eq_univ Set.center_eq_univ
 #align set.add_center_eq_univ Set.addCenter_eq_univ
 
-end Set
-
-namespace Subsemigroup
-
-variable (M) [CommSemigroup M]
-
-@[to_additive (attr := simp)]
-theorem center_eq_top : center M = ⊤ :=
-  SetLike.coe_injective (Set.center_eq_univ M)
-#align subsemigroup.center_eq_top Subsemigroup.center_eq_top
-#align add_subsemigroup.center_eq_top AddSubsemigroup.center_eq_top
-
-end Subsemigroup
-
 end CommSemigroup
-
-namespace Set
 
 variable (M)
 
@@ -380,6 +302,71 @@ theorem div_mem_center₀ [GroupWithZero M] {a b : M} (ha : a ∈ Set.center M)
 #align set.div_mem_center₀ Set.div_mem_center₀
 
 end Set
+
+/-! ### `Set.center` as a `Subsemigroup`. -/
+
+namespace Subsemigroup
+
+section Mul
+variable (M) [Mul M]
+
+/-- The center of a semigroup `M` is the set of elements that commute with everything in `M` -/
+@[to_additive
+      "The center of a semigroup `M` is the set of elements that commute with everything in `M`"]
+def center : Subsemigroup M where
+  carrier := Set.center M
+  mul_mem' := Set.mul_mem_center
+#align subsemigroup.center Subsemigroup.center
+#align add_subsemigroup.center AddSubsemigroup.center
+
+-- porting note: `coe_center` is now redundant
+#noalign subsemigroup.coe_center
+#noalign add_subsemigroup.coe_center
+
+variable {M}
+
+/-- The center of a magma is commutative and associative. -/
+@[to_additive "The center of an additive magma is commutative and associative."]
+instance center.commSemigroup : CommSemigroup (center M) where
+  mul_assoc _ b _ := Subtype.ext <| b.2.mid_assoc _ _
+  mul_comm a _ := Subtype.ext <| a.2.comm _
+#align subsemigroup.center.comm_semigroup Subsemigroup.center.commSemigroup
+#align add_subsemigroup.center.add_comm_semigroup AddSubsemigroup.center.addCommSemigroup
+
+end Mul
+
+section Semigroup
+variable [Semigroup M]
+
+@[to_additive]
+theorem mem_center_iff {z : M} : z ∈ center M ↔ ∀ g, g * z = z * g := by
+  rw [← Semigroup.mem_center_iff]
+  exact Iff.rfl
+#align subsemigroup.mem_center_iff Subsemigroup.mem_center_iff
+#align add_subsemigroup.mem_center_iff AddSubsemigroup.mem_center_iff
+
+@[to_additive]
+instance decidableMemCenter (a) [Decidable <| ∀ b : M, b * a = a * b] :
+    Decidable (a ∈ center M) :=
+  decidable_of_iff' _ Semigroup.mem_center_iff
+#align subsemigroup.decidable_mem_center Subsemigroup.decidableMemCenter
+#align add_subsemigroup.decidable_mem_center AddSubsemigroup.decidableMemCenter
+
+end Semigroup
+
+section CommSemigroup
+variable (M) [CommSemigroup M]
+
+@[to_additive (attr := simp)]
+theorem center_eq_top : center M = ⊤ :=
+  SetLike.coe_injective (Set.center_eq_univ M)
+#align subsemigroup.center_eq_top Subsemigroup.center_eq_top
+#align add_subsemigroup.center_eq_top AddSubsemigroup.center_eq_top
+
+
+end CommSemigroup
+
+end Subsemigroup
 
 -- Guard against import creep
 assert_not_exists Finset
