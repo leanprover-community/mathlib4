f336073ee0168fbf66424e72622b15636f2c734c
Christopher Hoskin <christopher.hoskin@gmail.com>
2023-11-01 00:43:13 +0000

    Lint

--- Mathlib/RingTheory/NonUnitalSubsemiring/Basic.lean
+++ Mathlib/RingTheory/NonUnitalSubsemiring/Basic.lean
@@ -469,7 +469,8 @@ theorem eq_top_iff' (A : NonUnitalSubsemiring R) : A = ⊤ ↔ ∀ x : R, x ∈ 
 
 section Center
 
-/-- The center of a semiring `R` is the set of elements that commute and associate with everything in `R` -/
+/-- The center of a semiring `R` is the set of elements that commute and associate with everything
+in `R` -/
 def center (R) [NonUnitalNonAssocSemiring R] : NonUnitalSubsemiring R :=
   { Subsemigroup.center R with
     zero_mem' := Set.zero_mem_center R
