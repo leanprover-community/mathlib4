dceb627e981b8ee235be5193b48d9d885e40d280
dagurtomas <dagurtomas@gmail.com>
2023-11-05 14:46:39 +0000

    reduce to one TODO for ulift

--- Mathlib/Condensed/Functors.lean
+++ Mathlib/Condensed/Functors.lean
@@ -1,4 +1,7 @@
 import Mathlib.CategoryTheory.Limits.Preserves.Opposites
+import Mathlib.CategoryTheory.Limits.Preserves.Ulift
+import Mathlib.CategoryTheory.Sites.Whiskering
+import Mathlib.CategoryTheory.Sites.Adjunction
 import Mathlib.Condensed.Abelian
 import Mathlib.Condensed.Explicit
 import Mathlib.Topology.Category.Yoneda
@@ -6,6 +9,24 @@ import Mathlib.Topology.Category.Yoneda
 universe u v
 
 open CategoryTheory Limits ContinuousMap
+
+section Universes
+
+-- noncomputable instance : PreservesLimitsOfSize.{u+1} uliftFunctor.{u+1, u} := sorry
+
+def Condensed.ulift : Condensed.{u} (Type u) ⥤ CondensedSet.{u} := by
+  refine @sheafCompose _ _ _ _ _ _ (coherentTopology CompHaus) uliftFunctor.{u+1, u} ?_
+  intro X S P
+  let K := MulticospanIndex.multicospan (GrothendieckTopology.Cover.index S P)
+  let I := WalkingMulticospan (GrothendieckTopology.Cover.index S P).fstTo
+    (GrothendieckTopology.Cover.index S P).sndTo
+  cases' S with S hS
+  sorry
+  -- We need to preserve a large limit...
+  -- TODO: define `sheafCompose` for the coherent topology, where it is enough to preserve a finite
+  -- limit. Then the stuff in `CategoryTheory/Preserves/Ulift` applies.
+
+end Universes
 
 section Topology
 
@@ -27,22 +48,24 @@ def topCatToCondensed : TopCat.{u+1} ⥤ CondensedSet.{u} where
   obj X := X.toCondensed
   map f := ⟨⟨fun _ g ↦ ContinuousMap.comp f g, by aesop⟩⟩
 
-def CompHaus.toCondensed (S : CompHaus.{u}) : CondensedSet.{u} := sorry
+def CompHaus.toCondensed_aux (S : CompHaus.{u}) : Condensed.{u} (Type u) where
+  val := yoneda.obj S
+  cond := by
+    rw [isSheaf_iff_isSheaf_of_type]
+    exact coherentTopology.isSheaf_yoneda_obj S
 
-def compHausToCondensed : CompHaus.{u} ⥤ CondensedSet.{u} := sorry
+def compHausToCondensed_aux : CompHaus.{u} ⥤ Condensed.{u} (Type u) where
+  obj S := S.toCondensed_aux
+  map f := ⟨yoneda.map f⟩
 
-def Profinite.toCondensed (S : Profinite.{u}) : CondensedSet.{u} := sorry
+def compHausToCondensed : CompHaus.{u} ⥤ CondensedSet.{u} :=
+  compHausToCondensed_aux ⋙ Condensed.ulift
 
-def profiniteToCondensed : Profinite.{u} ⥤ CondensedSet.{u} := sorry
+def profiniteToCondensed : Profinite.{u} ⥤ CondensedSet.{u} :=
+  profiniteToCompHaus ⋙ compHausToCondensed
 
-def Stonean.toCondensed (S : Stonean.{u}) : CondensedSet.{u} := sorry
-
-def stoneanToCondensed : Stonean.{u} ⥤ CondensedSet.{u} := sorry
+def stoneanToCondensed : Stonean.{u} ⥤ CondensedSet.{u} :=
+  Stonean.toCompHaus -- TODO: make this naming consistent.
+  ⋙ compHausToCondensed
 
 end Topology
-
-section Abelian
-
-def Condensed.forget : CondensedAb.{u} ⥤ CondensedSet.{u} := sorry
-
-end Abelian
