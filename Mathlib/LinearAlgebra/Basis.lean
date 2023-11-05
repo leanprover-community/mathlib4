5e7744351759a889f86c34e0141fdc97a0d275ea
Christopher Hoskin <christopher.hoskin@gmail.com>
2023-11-05 17:34:51 +0000

    Remove LibrarySearch

--- Mathlib/LinearAlgebra/Basis.lean
+++ Mathlib/LinearAlgebra/Basis.lean
@@ -10,7 +10,6 @@ import Mathlib.LinearAlgebra.Finsupp
 import Mathlib.LinearAlgebra.LinearIndependent
 import Mathlib.LinearAlgebra.LinearPMap
 import Mathlib.LinearAlgebra.Projection
-import Mathlib.Tactic.LibrarySearch
 
 #align_import linear_algebra.basis from "leanprover-community/mathlib"@"13bce9a6b6c44f6b4c91ac1c1d2a816e2533d395"
 
