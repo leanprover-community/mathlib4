8f9dd34ac49e3c2bb96a7c1fc0c7a54c38aeac68
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2023-11-06 17:20:48 -0300

    Add succ_mod_two lemmas to `Data.Nat.Parity`

--- Mathlib/Data/Nat/Parity.lean
+++ Mathlib/Data/Nat/Parity.lean
@@ -236,6 +236,14 @@ theorem one_add_div_two_mul_two_of_odd (h : Odd n) : 1 + n / 2 * 2 = n := by
   rw [← odd_iff.mp h, mod_add_div']
 #align nat.one_add_div_two_mul_two_of_odd Nat.one_add_div_two_mul_two_of_odd
 
+theorem succ_mod_two_eq_zero_iff {m : ℕ} : (m + 1) % 2 = 0 ↔ m % 2 = 1 := by
+  simp [← Nat.even_iff, ← Nat.not_even_iff, parity_simps]
+#align nat.succ_mod_two_eq_zero_iff Nat.succ_mod_two_eq_zero_iff
+
+theorem succ_mod_two_eq_one_iff {m : ℕ} : (m + 1) % 2 = 1 ↔ m % 2 = 0 := by
+  simp [← Nat.even_iff, ← Nat.not_even_iff, parity_simps]
+#align nat.succ_mod_two_eq_one_iff Nat.succ_mod_two_eq_one_iff
+
 set_option linter.deprecated false in
 section
 
