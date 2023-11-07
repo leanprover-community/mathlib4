916abcaefd0b9c4c5b20d2c69a8856850cc1abac
Antoine Chambert-Loir <antoine.chambert-loir@math.univ-paris-diderot.fr>
2023-11-07 19:20:37 +0100

    learn that caps are before lowercase…

--- Mathlib.lean
+++ Mathlib.lean
@@ -1455,10 +1455,10 @@ import Mathlib.Data.Finset.NatAntidiagonal
 import Mathlib.Data.Finset.NoncommProd
 import Mathlib.Data.Finset.Option
 import Mathlib.Data.Finset.Order
-import Mathlib.Data.Finset.PiAntidiagonal
 import Mathlib.Data.Finset.PImage
 import Mathlib.Data.Finset.Pairwise
 import Mathlib.Data.Finset.Pi
+import Mathlib.Data.Finset.PiAntidiagonal
 import Mathlib.Data.Finset.PiInduction
 import Mathlib.Data.Finset.Pointwise
 import Mathlib.Data.Finset.Powerset
