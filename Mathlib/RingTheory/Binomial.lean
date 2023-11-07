5cd3d4193577f73c00443bf854e836c9bd774a54
Scott Carnahan <128885296+ScottCarnahan@users.noreply.github.com>
2023-11-08 02:23:26 +0900

    rm ascFactorial def, commute lemmas
    replaced Ring.ascFactorial with ascPochhammer evaluation.

--- Mathlib/RingTheory/Binomial.lean
+++ Mathlib/RingTheory/Binomial.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Carnahan
 -/
 
-import Mathlib.Data.Rat.NNRat
 import Mathlib.RingTheory.Polynomial.Pochhammer
 
 /-!
@@ -33,57 +32,6 @@ universe u
 
 variable {R : Type u}
 
-section Semiring
-
-variable [Semiring R]
-
-namespace Ring
-
-/-- `ascFactorial`, also known as the rising factorial function, is the product
-`r (r+1) ⋯ (r + n - 1)`.  In other words, it recursively evaluates the `n`-th ascending Pochhammer
-polynomial at a semiring element `r`.-/
-def ascFactorial (r : R) : ℕ → R
-  | 0 => 1
-  | (k + 1) => (ascFactorial r k) * (r + k)
-
-theorem ascFactorial_zero (r : R) : ascFactorial r 0 = 1 := rfl
-
-theorem ascFactorial_succ (r : R) (k : ℕ) :
-    ascFactorial r (k.succ) = (ascFactorial r k) * (r + k) := rfl
-
-theorem ascFactorial_cast (n : ℕ) : ∀ (k : ℕ), ascFactorial (n : R) k = ascFactorial n k
-  | 0 => by rw [ascFactorial_zero, ascFactorial_zero, Nat.cast_one]
-  | (k + 1) => by
-    rw [ascFactorial_succ, ascFactorial_succ, Nat.cast_mul, ascFactorial_cast n k, Nat.cast_add]
-    norm_cast
-
-theorem ascFactorial_eq_ascPochhammer_eval (r : R) :
-    ∀ (k : ℕ), ascFactorial r k = Polynomial.eval r (ascPochhammer R k)
-  | 0 => by rw [ascPochhammer_zero, Polynomial.eval_one, ascFactorial_zero]
-  | (k + 1) => by
-    rw [ascPochhammer_succ_eval, ← ascFactorial_eq_ascPochhammer_eval r k, ascFactorial_succ]
-
-theorem translate_comm_ascFactorial (r s : R) (k : ℕ) (h : Commute r s) : ∀ (n : ℕ),
-    Commute (r + k) (ascFactorial s n)
-  | 0 => by
-    rw [ascFactorial_zero]
-    exact Commute.one_right (r + ↑k)
-  | (n + 1) => by
-    rw [ascFactorial_succ]
-    exact (translate_comm_ascFactorial r s k h n).mul_right
-      (Nat.add_cast_commute_add_cast r s k n h)
-
-theorem ascFactorial_add_right (r : R) (n : ℕ) : ∀ (k : ℕ),
-    ascFactorial r (n + k) = ascFactorial r n * ascFactorial (r + n) k
-  | 0 => by rw [add_zero, ascFactorial_zero, mul_one]
-  | (k + 1) => by
-    rw [← add_assoc, ascFactorial_succ, ascFactorial_add_right r n k, ascFactorial_succ, mul_assoc,
-    Nat.cast_add, add_assoc]
-
-end Ring
-
-end Semiring
-
 section BinomialSemiring
 
 /-- A semiring is binomial if multiplication by nonzero natural numbers is injective and ascending
@@ -95,7 +43,7 @@ class BinomialSemiring (R: Type u) extends Semiring R where
   multichoose : R → ℕ → R
   /-- ascFactorial r n is divisible by n! (witnessed by multichoose) -/
   factorial_mul_multichoose : ∀ (r : R) (n : ℕ),
-    n.factorial * multichoose r n = Ring.ascFactorial r n
+    n.factorial * multichoose r n = Polynomial.eval r (ascPochhammer R n)
 
 namespace Ring
 
@@ -111,8 +59,8 @@ replacement. -/
 def multichoose [BinomialSemiring R] (r : R) (n : ℕ) : R :=
   BinomialSemiring.multichoose r n
 
-theorem factorial_mul_multichoose_eq_ascFactorial [BinomialSemiring R] (r : R) (n : ℕ) :
-    n.factorial * multichoose r n = ascFactorial r n :=
+theorem factorial_mul_multichoose_eq_ascPochhammer [BinomialSemiring R] (r : R) (n : ℕ) :
+    n.factorial * multichoose r n = Polynomial.eval r (ascPochhammer R n) :=
   BinomialSemiring.factorial_mul_multichoose r n
 
 end Ring
