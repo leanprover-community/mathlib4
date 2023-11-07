96951eff5eee4ed2b42735edca4a3b1daf0da7ba
Dagur Tómas Ásgeirsson <dagurtomas@gmail.com>
2023-11-07 10:29:07 +0000

    Update Ulift.lean
    add preserves

--- Mathlib/CategoryTheory/Limits/Preserves/Ulift.lean
+++ Mathlib/CategoryTheory/Limits/Preserves/Ulift.lean
@@ -9,8 +9,12 @@ import Mathlib.CategoryTheory.Limits.Types
 /-!
 # `ULift` creates small (co)limits
 
-This file shows that `uliftFunctor.{v, u}` creates (and hence preserves) `u`-small limits and
-colimits.
+This file shows that `uliftFunctor.{v, u}` creates (and hence preserves) limits and colimits indexed
+by `J` with `[Category.{u, u} J]`.
+
+It also shows that `uliftFunctor.{v, u}` *preserves* "all" limits (i.e. of size `max u v`,
+potentially too big to exist in `Type u`).
+
 -/
 
 universe v u
@@ -32,7 +36,7 @@ def sectionsEquiv {J : Type u} [Category.{u, u} J] (K : J ⥤ Type u) :
 /--
 The functor `uliftFunctor : Type u ⥤ Type (max u v)` creates `u`-small limits.
 -/
-noncomputable instance : CreatesLimitsOfSize.{u} uliftFunctor.{v, u} where
+noncomputable instance : CreatesLimitsOfSize.{u, u} uliftFunctor.{v, u} where
   CreatesLimitsOfShape := {
     CreatesLimit := fun {K} =>
       @createsLimitOfFullyFaithfulOfIso _ _ _ _ _ _ K uliftFunctor _ _ _ (limit K)
@@ -40,6 +44,33 @@ noncomputable instance : CreatesLimitsOfSize.{u} uliftFunctor.{v, u} where
         (limit.isLimit K))) ≪≫ (equivEquivIso (sectionsEquiv K)) ≪≫
         (equivEquivIso (isLimitEquivSections.{u, max u v}
         (limit.isLimit (K ⋙ uliftFunctor.{v, u})))).symm) }
+
+/--
+The equivalence between `K.sections` and `(K ⋙ uliftFunctor.{v, u}).sections`. This is used to show
+that `uliftFunctor` preserves limits that are too possibly too large to exist in the source
+category.
+-/
+def sectionsEquiv' {J : Type*} [Category J] (K : J ⥤ Type u) :
+    K.sections ≃ (K ⋙ uliftFunctor.{v, u}).sections where
+  toFun := fun ⟨u, hu⟩ => ⟨fun j => ⟨u j⟩, fun f => by simp [hu f]⟩
+  invFun := fun ⟨u, hu⟩ => ⟨fun j => (u j).down, @fun j j' f => by simp [← hu f]⟩
+  left_inv := by intro; ext; rfl
+  right_inv := by intro; ext; rfl
+
+/--
+The functor `uliftFunctor : Type u ⥤ Type (max u v)` preserves limits.
+-/
+noncomputable
+instance : PreservesLimits uliftFunctor.{v, u} where
+  preservesLimitsOfShape {J} := {
+    preservesLimit := fun {K} => {
+      preserves := fun {c} hc => by
+        suffices Nonempty (IsLimit (uliftFunctor.{v, u}.mapCone c)) by exact this.some
+        rw [Types.isLimit_iff ((uliftFunctor.{v, u}).mapCone c)]
+        intro s hs
+        obtain ⟨x, hx₁, hx₂⟩ := (Types.isLimit_iff c).mp ⟨hc⟩ _ ((sectionsEquiv' K).symm ⟨s, hs⟩).2
+        exact ⟨⟨x⟩, fun i => ULift.ext _ _ (hx₁ i),
+          fun y hy => ULift.ext _ _ (hx₂ y.down fun i ↦ (ULift.ext_iff _ _).mp (hy i))⟩ } }
 
 /--
 The equivalence between the ulift of the explicit colimit cocone of a functor `K` and the explicit
