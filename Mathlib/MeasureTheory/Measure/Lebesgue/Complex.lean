297193347ee1477e61e2042bc360462a3f4cea69
Xavier-François Roblot <46200072+xroblot@users.noreply.github.com>
2023-11-08 17:45:18 +0100

    Review

--- Mathlib/MeasureTheory/Measure/Lebesgue/Complex.lean
+++ Mathlib/MeasureTheory/Measure/Lebesgue/Complex.lean
@@ -3,7 +3,6 @@ Copyright (c) 2021 Yury Kudryashov. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yury Kudryashov
 -/
-import Mathlib.Analysis.SpecialFunctions.PolarCoord
 import Mathlib.MeasureTheory.Measure.Haar.InnerProductSpace
 
 #align_import measure_theory.measure.lebesgue.complex from "leanprover-community/mathlib"@"fd5edc43dc4f10b85abfe544b88f82cf13c5f844"
@@ -82,39 +81,5 @@ theorem volume_ball (a : ℂ) (r : ℝ) :
 theorem volume_closedBall (a : ℂ) (r : ℝ) :
     volume (Metric.closedBall a r) = NNReal.pi * ENNReal.ofReal r ^ 2 := by
   rw [MeasureTheory.Measure.addHaar_closedBall_eq_addHaar_ball, Complex.volume_ball]
-
-section polar
-
-/-- The polar coordinates local homeomorphism in `ℂ`, mapping `r (cos θ + I * sin θ)` to `(r, θ)`.
-It is a homeomorphism between `ℂ - ℝ≤0` and `(0, +∞) × (-π, π)`. -/
-protected noncomputable def polarCoord : LocalHomeomorph ℂ (ℝ × ℝ) :=
-  equivRealProdClm.toHomeomorph.toLocalHomeomorph.trans polarCoord
-
-protected theorem polarCoord_apply (a : ℂ) :
-    Complex.polarCoord a = (Complex.abs a, Complex.arg a) := by
-  simp_rw [Complex.abs_def, Complex.normSq_apply, ← pow_two]
-  rfl
-
-protected theorem polarCoord_source :
-    Complex.polarCoord.source = {a | 0 < a.re} ∪ {a | a.im ≠ 0} := by simp [Complex.polarCoord]
-
-alias polarCoord_target := polarCoord_target
-
-@[simp]
-protected theorem polarCoord_symm_apply (p : ℝ × ℝ) :
-    Complex.polarCoord.symm p = p.1 * (Real.cos p.2 + Real.sin p.2 * Complex.I) := by
-  simp [Complex.polarCoord, equivRealProdClm_symm_apply, mul_add, mul_assoc]
-
-theorem polardCoord_symm_abs (p : ℝ × ℝ) :
-    Complex.abs (Complex.polarCoord.symm p) = |p.1| := by simp
-
-protected theorem integral_comp_polarCoord_symm {E : Type*} [NormedAddCommGroup E]
-    [NormedSpace ℝ E] (f : ℂ → E) :
-    (∫ p in polarCoord.target, p.1 • f (Complex.polarCoord.symm p)) = ∫ p, f p := by
-  rw [← (Complex.volume_preserving_equiv_real_prod.symm).integral_comp
-    measurableEquivRealProd.symm.measurableEmbedding, ← integral_comp_polarCoord_symm]
-  rfl
-
-end polar
 
 end Complex
