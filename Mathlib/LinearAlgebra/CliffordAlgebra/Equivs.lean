f97a7f4d22d6b07f39dd8e513acb5844bf5e2580
Eric Wieser <wieser.eric@gmail.com>
2023-11-08 11:08:16 +0000

    fix

--- Mathlib/LinearAlgebra/CliffordAlgebra/Equivs.lean
+++ Mathlib/LinearAlgebra/CliffordAlgebra/Equivs.lean
@@ -415,10 +415,8 @@ protected def equiv : CliffordAlgebra (0 : QuadraticForm R R) ≃ₐ[R] R[ε] :=
       fun _ => (Algebra.commutes _ _).symm⟩)
     (by
       ext : 1
-      · simp
-      · ext : 1
-        -- This used to be a single `simp` before leanprover/lean4#2644
-        simp; erw [lift_ι_apply]; simp)
+      -- This used to be a single `simp` before leanprover/lean4#2644
+      simp; erw [lift_ι_apply]; simp)
     -- This used to be a single `simp` before leanprover/lean4#2644
     (by ext : 2; simp; erw [lift_ι_apply]; simp)
 #align clifford_algebra_dual_number.equiv CliffordAlgebraDualNumber.equiv
