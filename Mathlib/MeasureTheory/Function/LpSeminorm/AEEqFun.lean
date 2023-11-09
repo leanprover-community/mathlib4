3579e269628cd63aa52d5979a5ebab83aa707e9c
Yury G. Kudryashov <urkud@urkud.name>
2023-11-09 13:03:18 -0600

    Update

--- Mathlib/MeasureTheory/Function/LpSeminorm/AEEqFun.lean
+++ Mathlib/MeasureTheory/Function/LpSeminorm/AEEqFun.lean
@@ -1,6 +1,22 @@
+import Mathlib.MeasureTheory.Function.AEEqFun
+import Mathlib.MeasureTheory.Function.LpSeminorm.TriangleIneq
+import Mathlib.MeasureTheory.Function.LpSeminorm.MeasureOps
 
-theorem AEEqFun.snorm_compMeasurePreserving {ν : MeasureTheory.Measure β} (g : β →ₘ[ν] E)
-    (hf : MeasurePreserving f μ ν) :
+open scoped ENNReal
+
+namespace MeasureTheory
+
+variable {α β E : Type*} {_ : MeasurableSpace α} {_ : MeasurableSpace β} [NormedAddCommGroup E]
+  {μ : Measure α} {ν : Measure β} {p : ℝ≥0∞}
+
+namespace AEEqFun
+
+@[simp]
+lemma snorm_compMeasurePreserving {f : α → β} (g : β →ₘ[ν] E) (hf : MeasurePreserving f μ ν) :
     snorm (g.compMeasurePreserving f hf) p μ = snorm g p ν := by
   rw [snorm_congr_ae (g.coeFn_compMeasurePreserving _)]
   exact snorm_comp_measurePreserving g.aestronglyMeasurable hf
+
+protected lemma snorm_add_le (f g : α →ₘ[μ] E) : snorm (⇑(f + g)) p μ ≤ snorm f p μ + snorm g p μ :=
+  (snorm_congr_ae <| coeFn_add f g).trans_le
+    (snorm_add_le f.aestronglyMeasurable g.aestronglyMeasurable)
