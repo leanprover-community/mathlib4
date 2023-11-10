0ccf4506a5fdb304b4ba5d6dd00186519a90987d
Alex Keizer <alex@keizer.dev>
2023-11-10 14:03:49 +0000

    Use `setOf` notation

--- Mathlib/Data/Fin/Tuple/Basic.lean
+++ Mathlib/Data/Fin/Tuple/Basic.lean
@@ -1012,12 +1012,12 @@ section ExtendFun
 /-- Extend a function `f : Fin n → α` to a function on all natural numbers, by
     defining `f i = a` for all `i ≥ n` and some given constant `a` -/
 abbrev extendFun {α : Type*} {n : ℕ} (f : Fin n → α) (a : α) : ℕ → α :=
-  Set.piecewiseMem (· < n) (fun i h => f ⟨i, h⟩) (fun _ _ => a)
+  Set.piecewiseMem {x | x < n} (fun i h => f ⟨i, h⟩) (fun _ _ => a)
 
 @[simp] lemma extendFun_val' {α a n} (m) {f : Fin (n+m) → α} {i : Fin n} :
     extendFun f a i.val = f (i.castAdd _) := by
   have : i.val < n + m := Nat.lt_of_lt_of_le (i.prop) (Nat.le_add_right ..)
-  simp only [extendFun, this, dite_true, Set.piecewiseMem, Membership.mem, Set.Mem]
+  simp only [extendFun, this, dite_true, Set.piecewiseMem, Membership.mem, Set.Mem, setOf]
   rfl
 
 @[simp] lemma extendFun_val {α a n} {f : Fin n → α} {i : Fin n} :
@@ -1037,8 +1037,8 @@ abbrev extendFun {α : Type*} {n : ℕ} (f : Fin n → α) (a : α) : ℕ → α
   simp [extendFun, Set.piecewiseMem, Membership.mem, Set.Mem]
 
 @[simp] lemma extendFun_succ {α a n} {f : Fin (n+1) → α} :
-    (extendFun f a) n = f ⟨n, Nat.lt.base ..⟩ := by
-  simp [extendFun]
+    (extendFun f a) n = f (Fin.last n) := by
+  simp [extendFun, Set.piecewiseMem, Fin.last]
 
 end ExtendFun
 
