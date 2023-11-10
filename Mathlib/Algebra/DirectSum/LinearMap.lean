83f09497077387a2c0caf11761dcd92b376f2083
Oliver Nash <github@olivernash.org>
2023-11-10 13:30:09 +0000

    feat: the trace of a direct sum is the sum of the traces

---
+++ Mathlib/Algebra/DirectSum/LinearMap.lean
@@ -0,0 +1,63 @@
+/-
+Copyright (c) 2023 Oliver Nash. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Oliver Nash
+-/
+import Mathlib.Algebra.DirectSum.Module
+import Mathlib.LinearAlgebra.Trace
+
+/-!
+# Linear maps between direct sums
+
+This file contains results about linear maps which respect direct sum decompositions of their
+domain and codomain.
+
+-/
+
+open Set BigOperators DirectSum
+
+variable {ι R M : Type*} [CommRing R] [AddCommGroup M] [Module R M]
+  {N : ι → Submodule R M} [DecidableEq ι] (h : IsInternal N)
+
+namespace LinearMap
+
+/-- If a linear map `f : M → M₂` respects direct sum decompositions of `M` and `M₂`, then it has a
+block diagonal matrix with respect to bases compatible with the direct sum decompositions. -/
+lemma toMatrix_directSum_collectedBasis_eq_blockDiagonal' [Fintype ι]
+    {M₂ : Type*} [AddCommGroup M₂] [Module R M₂] {N₂ : ι → Submodule R M₂} (h₂ : IsInternal N₂)
+    {κ κ₂ : ι → Type*} [∀ i, Fintype (κ i)] [∀ i, Fintype (κ₂ i)] [∀ i, DecidableEq (κ i)]
+    (b : (i : ι) → Basis (κ i) R (N i)) (b₂ : (i : ι) → Basis (κ₂ i) R (N₂ i))
+    {f : M →ₗ[R] M₂} (hf : ∀ i, MapsTo f (N i) (N₂ i)) :
+    toMatrix (h.collectedBasis b) (h₂.collectedBasis b₂) f =
+    Matrix.blockDiagonal' fun i ↦ toMatrix (b i) (b₂ i) (f.restrict (hf i)) := by
+  ext ⟨i, _⟩ ⟨j, _⟩
+  simp only [toMatrix_apply, Matrix.blockDiagonal'_apply]
+  rcases eq_or_ne i j with rfl | hij
+  · simp [h₂.collectedBasis_repr_of_mem _ (hf _ (Subtype.mem _)), restrict_apply]
+  · simp [hij, h₂.collectedBasis_repr_of_mem_ne _ hij.symm (hf _ (Subtype.mem _))]
+
+variable [∀ i, Module.Finite R (N i)] [∀ i, Module.Free R (N i)]
+
+lemma trace_eq_sum_trace_restrict [Fintype ι]
+    {f : M →ₗ[R] M} (hf : ∀ i, MapsTo f (N i) (N i)) :
+    trace R M f = ∑ i, trace R (N i) (f.restrict (hf i)) := by
+  let b : (i : ι) → Basis _ R (N i) := fun i ↦ Module.Free.chooseBasis R (N i)
+  simp_rw [trace_eq_matrix_trace R (h.collectedBasis b),
+    toMatrix_directSum_collectedBasis_eq_blockDiagonal' h h b b hf, Matrix.trace_blockDiagonal',
+    ← trace_eq_matrix_trace]
+
+lemma trace_eq_sum_trace_restrict' (hN : {i | N i ≠ ⊥}.Finite)
+    {f : M →ₗ[R] M} (hf : ∀ i, MapsTo f (N i) (N i)) :
+    trace R M f = ∑ i in hN.toFinset, trace R (N i) (f.restrict (hf i)) := by
+  set s : Finset ι := hN.toFinset
+  have h_ind : CompleteLattice.Independent (fun i : s ↦ N i) :=
+    h.submodule_independent.comp Subtype.val_injective
+  have h_sup : ⨆ i, N i = ⊤ := h.submodule_iSup_eq_top
+  replace h_sup : ⨆ (i : s), N i = ⊤ := by
+    suffices ⨆ i, ⨆ (_ : N i = ⊥), N i = ⊥ by
+      rw [iSup_split _ (fun i ↦ N i = ⊥), this, bot_sup_eq] at h_sup; simpa [iSup_subtype]
+    simp [iSup_eq_bot]
+  replace h := DirectSum.isInternal_submodule_of_independent_of_iSup_eq_top h_ind h_sup
+  rw [← Finset.sum_coe_sort, trace_eq_sum_trace_restrict h (fun i ↦ hf i)]
+
+end LinearMap
