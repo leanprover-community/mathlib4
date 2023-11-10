49178d322b2d263bfbedc77fce166621b720dbe9
Kalle <kalle.kytola@aalto.fi>
2023-11-11 00:04:24 +0200

    Clarify proof as requested.

--- Mathlib/MeasureTheory/Measure/Portmanteau.lean
+++ Mathlib/MeasureTheory/Measure/Portmanteau.lean
@@ -658,18 +658,20 @@ theorem tendsto_of_forall_isOpen_le_liminf {μ : ProbabilityMeasure Ω}
   intro G G_open
   specialize h_opens G G_open
   simp only at h_opens
-  have aux := Monotone.map_liminf_of_continuousAt (F := atTop) ENNReal.coe_mono (μs · G) ?_ ?_ ?_
-  · have obs := ENNReal.coe_mono h_opens
-    simp only [ne_eq, ProbabilityMeasure.ennreal_coeFn_eq_coeFn_toMeasure, aux] at obs
-    convert obs
-    simp only [Function.comp_apply, ne_eq, ProbabilityMeasure.ennreal_coeFn_eq_coeFn_toMeasure]
-  · apply ENNReal.continuous_coe.continuousAt
-  · use 1
-    simp only [eventually_map, ProbabilityMeasure.apply_le_one, eventually_atTop, ge_iff_le,
-      implies_true, forall_const, exists_const]
-  · use 0
-    simp only [zero_le, eventually_map, eventually_atTop, ge_iff_le, implies_true, forall_const,
-      exists_const]
+  have aux : ENNReal.some (liminf (fun i ↦ ENNReal.toNNReal ((μs i : Measure Ω) G)) atTop) =
+              liminf (ENNReal.some ∘ fun i ↦ (ENNReal.toNNReal ((μs i : Measure Ω) G))) atTop := by
+    refine Monotone.map_liminf_of_continuousAt (F := atTop) ENNReal.coe_mono (μs · G) ?_ ?_ ?_
+    · apply ENNReal.continuous_coe.continuousAt
+    · use 1
+      simp only [eventually_map, ProbabilityMeasure.apply_le_one, eventually_atTop, ge_iff_le,
+        implies_true, forall_const, exists_const]
+    · use 0
+      simp only [zero_le, eventually_map, eventually_atTop, ge_iff_le, implies_true, forall_const,
+        exists_const]
+  have obs := ENNReal.coe_mono h_opens
+  simp only [ne_eq, ProbabilityMeasure.ennreal_coeFn_eq_coeFn_toMeasure, aux] at obs
+  convert obs
+  simp only [Function.comp_apply, ne_eq, ProbabilityMeasure.ennreal_coeFn_eq_coeFn_toMeasure]
 
 end le_liminf_open_implies_convergence
 
