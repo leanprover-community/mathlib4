4817f629518d1a4f71433fa267f53a6e6032627b
Alex Keizer <alex@keizer.dev>
2023-11-10 16:07:44 +0000

    import `ExtendFun`

--- Mathlib.lean
+++ Mathlib.lean
@@ -2204,6 +2204,7 @@ import Mathlib.Init.Data.Rat.Basic
 import Mathlib.Init.Data.Sigma.Basic
 import Mathlib.Init.Data.Sigma.Lex
 import Mathlib.Init.Data.Subtype.Basic
+import Mathlib.Init.Data.Subtype.ExtendFun
 import Mathlib.Init.Function
 import Mathlib.Init.IteSimp
 import Mathlib.Init.Logic
