d0ffdef62d74b6689f21105265ec9fc5431f1b5c
Scott Morrison <scott.morrison@gmail.com>
2023-11-11 15:36:28 +1100

    fix merge

--- test/norm_cast.lean
+++ test/norm_cast.lean
@@ -69,9 +69,9 @@ example : p 42 := by
   guard_target = p 42
   exact ⟨⟩
 
-example (h : (an : ℝ) = 0) : an = 0 := by exact_mod_cast h
-example (h : (an : ℝ) = 42) : an = 42 := by exact_mod_cast h
-example (h : (an + 42) ≠ 42) : (an : ℝ) + 42 ≠ 42 := by exact_mod_cast h
+example (h : (an : ℝ) = 0) : an = 0 := mod_cast h
+example (h : (an : ℝ) = 42) : an = 42 := mod_cast h
+example (h : (an + 42) ≠ 42) : (an : ℝ) + 42 ≠ 42 := mod_cast h
 
 example (n : ℤ) (h : n + 1 > 0) : ((n + 1 : ℤ) : ℚ) > 0 := mod_cast h
 
