adff0271703eeaade7be7bbc4daf74d5c4d9a015
Alex Keizer <alex@keizer.dev>
2023-11-11 10:54:38 +0000

    general refactorings
    Minimize imports,
    Add `zeroExtendLE` and use it in arithmetic defs

--- Mathlib/Data/BitVec/Defs.lean
+++ Mathlib/Data/BitVec/Defs.lean
@@ -4,10 +4,12 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Harun Khan, Alex Keizer, Abdalrhman M Mohamed
 -/
 
+import Mathlib.Algebra.GroupPower.Order
 import Mathlib.Data.Fin.Basic
-import Mathlib.Data.Nat.Bitwise
-import Mathlib.Data.ZMod.Defs
+-- import Mathlib.Data.Nat.Bitwise
+-- import Mathlib.Data.ZMod.Defs
 import Std.Data.BitVec
+import Mathlib.Tactic
 
 
 /-!
@@ -74,6 +76,36 @@ namespace Std.BitVec
 -- #align bitvec.one Std.BitVec.one
 
 /-!
+## Structural
+-/
+
+/-- Zero extend vector `x` of length `w` by adding zeros in the high bits until it has length `v`.
+    Behaves exactly like `zeroExtend`, except it takes a proof that `w ≤ v`, so that no truncating
+    is necessary. `zeroExtendLE` is easier to reason about than `zeroExtend`, and should thus be
+    preferred whenever such a proof is available.
+-/
+def zeroExtendLE {w v : ℕ} (h : w ≤ v) (x : BitVec w) : BitVec v :=
+  ⟨x.toFin.castLE <| pow_le_pow (by decide) h⟩
+
+/-- Turn a `Bool` into a bitvector of length `1` -/
+def ofBool : Bool → BitVec 1
+  | true  => 1
+  | false => 0
+
+/-- The empty bitvector -/
+def nil : BitVec 0 :=
+  BitVec.zero 0
+
+/-- Append a single bit to the end of a bitvector, using big endian order (see `append`).
+    That is, the new bit is the least significant bit. -/
+def concat {n} (msbs : BitVec n) (lsb : Bool) : BitVec (n+1) := msbs ++ (ofBool lsb)
+
+/-- Prepend a single bit to the front of a bitvector, using big endian order (see `append`).
+    That is, the new bit is the most significant bit. -/
+def cons {n} (msb : Bool) (lsbs : BitVec n) : BitVec (n+1) :=
+  ((ofBool msb) ++ lsbs).cast (Nat.add_comm ..)
+
+/-!
 ## Bitwise operations
 -/
 
@@ -99,12 +131,15 @@ protected def uge (x y : BitVec w) : Bool := BitVec.ule y x
 
 /-- Add with carry (no overflow) -/
 def adc {n} (x y : BitVec n) (c : Bool) : BitVec (n+1) :=
-  ofFin (x.toNat + y.toNat + c.toNat)
+  let x := x.zeroExtendLE (Nat.le_succ n)
+  let y := y.zeroExtendLE (Nat.le_succ n)
+  let c : BitVec (n + 1) := (ofBool c).zeroExtendLE (Nat.le_add_left 1 n)
+  x + y + c
 -- #align bitvec.adc Std.BitVec.adc
 
 /-- Subtract with borrow -/
 def sbb {n} (x y : BitVec n) (b : Bool) : Bool × BitVec n :=
-  let y := y + ofFin b.toNat
+  let y := y + (ofBool b).zeroExtend n
   (x < y, x - y)
 -- #align bitvec.sbb Std.BitVec.sbb
 
@@ -113,27 +148,5 @@ def sbb {n} (x y : BitVec n) (b : Bool) : Bool × BitVec n :=
 def addLsb (r : ℕ) (b : Bool) :=
   Nat.bit b r
 -- #align bitvec.add_lsb Std.BitVec.addLsb
-
-/-!
-## Structural
--/
-
-/-- Turn a `Bool` into a bitvector of length `1` -/
-def ofBool : Bool → BitVec 1
-  | true  => 1
-  | false => 0
-
-/-- The empty bitvector -/
-def nil : BitVec 0 :=
-  BitVec.zero 0
-
-/-- Append a single bit to the end of a bitvector, using big endian order (see `append`).
-    That is, the new bit is the least significant bit. -/
-def concat {n} (msbs : BitVec n) (lsb : Bool) : BitVec (n+1) := msbs ++ (ofBool lsb)
-
-/-- Prepend a single bit to the front of a bitvector, using big endian order (see `append`).
-    That is, the new bit is the most significant bit. -/
-def cons {n} (msb : Bool) (lsbs : BitVec n) : BitVec (n+1) :=
-  ((ofBool msb) ++ lsbs).cast (Nat.add_comm ..)
 
 end Std.BitVec
