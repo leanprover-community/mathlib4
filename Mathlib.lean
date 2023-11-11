5091dbd8ab234c96695df6da090db42e96112dd1
Alex Keizer <alex@keizer.dev>
2023-11-11 19:43:24 +0000

    import

--- Mathlib.lean
+++ Mathlib.lean
@@ -1914,6 +1914,7 @@ import Mathlib.Data.Sigma.Interval
 import Mathlib.Data.Sigma.Lex
 import Mathlib.Data.Sigma.Order
 import Mathlib.Data.Sign
+import Mathlib.Data.StdBitVec.Lemmas
 import Mathlib.Data.Stream.Defs
 import Mathlib.Data.Stream.Init
 import Mathlib.Data.String.Basic
