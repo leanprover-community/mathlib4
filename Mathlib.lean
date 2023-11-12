b79b26338a8e05877c0e43f5b023d9f6fb0d956a
Sebastian Zimmer <sebastian.zim@googlemail.com>
2023-11-12 01:06:15 +0000

    Import grw from mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -3181,6 +3181,9 @@ import Mathlib.Tactic.Find
 import Mathlib.Tactic.GCongr
 import Mathlib.Tactic.GCongr.Core
 import Mathlib.Tactic.GCongr.ForwardAttr
+import Mathlib.Tactic.GRW
+import Mathlib.Tactic.GRW.Core
+import Mathlib.Tactic.GRW.Lemmas
 import Mathlib.Tactic.GeneralizeProofs
 import Mathlib.Tactic.Group
 import Mathlib.Tactic.GuardGoalNums
