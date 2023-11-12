cb78091382e2ce0fba27f7d9c924a9673f20a576
thorimur <68410468+thorimur@users.noreply.github.com>
2023-11-12 01:22:06 -0500

    feat: `hasNewErrors` * checks if any more errors have been logged during some computation

--- Mathlib/Lean/CoreM.lean
+++ Mathlib/Lean/CoreM.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2023 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Scott Morrison
+Authors: Scott Morrison, Thomas R. Murrills
 -/
 import Lean
 import Mathlib.Tactic.ToExpr
@@ -27,3 +27,24 @@ def CoreM.withImportModules (modules : Array Name) (run : CoreM α)
     let state := {env}
     Prod.fst <$> (CoreM.toIO · ctx state) do
       run
+
+/--
+Evaluates `k`, and returns the result along with an indicator of whether new errors were
+produced during its execution.
+
+Note that `hasNewErrors` only counts the number of errors present in `(← Core.getMessageLog).msgs`
+before and after `k`; if `k` has the ability to e.g. reset the `CoreM` state,
+`hasNewErrors` may not behave as expected.
+-/
+def hasNewErrors [Monad m] [MonadOptions m] [MonadLiftT CoreM m] (k : m α) : m (α × Bool) := do
+  let warningAsError := warningAsError.get (← getOptions)
+  let isError : MessageSeverity → Bool
+  | .error => true
+  | .warning => warningAsError
+  | _ => false
+  let getNumErrors :=
+    return (← Core.getMessageLog).msgs.filter (fun msg => isError msg.severity) |>.size
+  let numErrors ← getNumErrors
+  let val ← k
+  let numErrors' ← getNumErrors
+  return (val, numErrors < numErrors')
