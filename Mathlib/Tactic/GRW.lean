b8799016e204257ecdb996a0de36064b73c27af8
Sebastian Zimmer <sebastian.zim@googlemail.com>
2023-11-13 15:16:08 +0000

    use StateT in withRwRulesSeq alternative

--- Mathlib/Tactic/GRW.lean
+++ Mathlib/Tactic/GRW.lean
@@ -34,6 +34,33 @@ private partial def _root_.Lean.MVarId.grw (goal : MVarId) (rule : Expr) (rev : 
   goal.assign prf
   return ⟨mvar, subgoals⟩
 
+/-- A version of `withRWRulesSeq` (in core) that doesn't attempt to find equation lemmas, and
+allows the caller to maintain state via StateT. -/
+private partial def withRWRulesSeqState {State : Type} (token : Syntax) (rwRulesSeqStx : Syntax)
+    (x : (symm : Bool) → (term : Syntax) → StateT State TacticM Unit) :
+    StateT State TacticM Unit := do
+  let lbrak := rwRulesSeqStx[0]
+  let rules := rwRulesSeqStx[1].getArgs
+  -- show initial state up to (incl.) `[`
+  withTacticInfoContext (mkNullNode #[token, lbrak]) (pure ())
+  let numRules := (rules.size + 1) / 2
+  for i in [:numRules] do
+    let rule := rules[i * 2]!
+    let sep  := rules.getD (i * 2 + 1) Syntax.missing
+    let state ← get
+    -- show rule state up to (incl.) next `,`
+    let newState ← withTacticInfoContext (mkNullNode #[rule, sep]) do
+      -- show errors on rule
+      withRef rule do
+        let symm := !rule[0].isNone
+        let term := rule[1]
+        -- let processId (id : Syntax) : TacticM Unit := do
+        let ⟨_, newState⟩ ← (x symm term).run state
+        return newState
+    set newState
+
+
+
 /--
 `grw` is a generalization of the `rw` tactic that takes other relations than equality.  For example,
 ```lean
@@ -50,37 +77,37 @@ relation) then the resulting type will be `p[x/y]` or the tactic will fail. This
 if side goals are created that it can not fill itself, which it does using `positivity` or
 `assumption`. These two facts make it safe to use in a nonterminal position.
 -/
-elab tok:"grw" rules:rwRuleSeq loc:(location)? : tactic =>
-  withMainContext do
+elab tok:"grw" rules:rwRuleSeq loc:(location)? : tactic => do
   withLocation (expandOptLocation (Lean.mkOptionalNode loc))
-    (atLocal := λ fvar => do
-      -- TODO this is a horrible hack, maybe just don't use withRWRulesSeq?
-      -- we use an mvar to keep track of which fvar we are up to (since we replace the fvar after
-      -- applying each rule )
-
-      let mvar := (← mkFreshExprMVar none).mvarId!
-      mvar.assign (Expr.fvar fvar)
-
-      withRWRulesSeq tok rules fun rev syn ↦ withMainContext do
-        let fvar ← if let some fvarExpr := ← (Lean.getExprMVarAssignment? mvar) then
-          pure fvarExpr.fvarId!
-        else
-          throwError "Lost track of fvar"
+    (atLocal := λ fvar => withMainContext do
+      let _ ← (withRWRulesSeqState tok rules fun rev syn ↦ do
+        let fvar ← get
         let rulePrf ← elabTerm syn none
         let ⟨newType, newHyp, subgoals⟩ ← grwHypothesis (Expr.fvar fvar) rulePrf rev
         let name ← fvar.getUserName
         let ⟨newFvar, newGoal, _⟩ ← (← getMainGoal).assertAfter fvar name newType newHyp
         replaceMainGoal (subgoals.push (← newGoal.clear fvar)).toList
-        mvar.assign (Expr.fvar newFvar)
+        set newFvar
+      ).run fvar
     )
-    (atTarget := withRWRulesSeq tok rules fun rev syn ↦ withMainContext do
-      let rulePrf ← elabTerm syn none
-      let ⟨newGoal, subgoals⟩ ← (← getMainGoal).grw rulePrf rev
-      try
-        withReducible newGoal.applyRfl
-        trace[GRW] "Solve main goal with `rfl`"
-        replaceMainGoal subgoals.toList
-      catch _ =>
-        replaceMainGoal (subgoals.push newGoal).toList
+    (atTarget := withMainContext do
+      let ⟨_, newGoal, subgoals⟩ ← (withRWRulesSeqState tok rules fun rev syn ↦ do
+        let ⟨currentTarget, subgoals⟩ ← get
+        trace[GRW] "Processing step {currentTarget} {subgoals}"
+        let ⟨newGoal, newSubgoals⟩ ← currentTarget.withContext do
+          let rulePrf ← elabTerm syn none
+          currentTarget.grw rulePrf rev
+        trace[GRW] "Finished step {newGoal} {subgoals.append newSubgoals}"
+        set (⟨newGoal, subgoals.append newSubgoals⟩ : MVarId × Array MVarId)
+      ).run (⟨← getMainGoal, #[]⟩ : MVarId × Array MVarId)
+      -- try
+      --   trace[GRW] "Trying to solve main goal with rfl"
+      --   newGoal.withContext $ withReducible newGoal.applyRfl
+      --   trace[GRW] "Solve main goal with `rfl`"
+      --   replaceMainGoal subgoals.toList
+      -- catch _ =>
+      trace[GRW] "Could not solve main goal with rfl"
+      trace[GRW] "Replacing main goals with {subgoals.push newGoal} {← newGoal.isAssigned}"
+      replaceMainGoal (subgoals.push newGoal).toList
     )
     (failed := fun _ ↦ throwError "grw failed")
