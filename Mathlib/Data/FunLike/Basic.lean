e7b66f4e224b26efc8a5a57db402df2c6f67588f
Eric Wieser <wieser.eric@gmail.com>
2023-11-13 12:29:17 +0000

    perf: improve elaboration of `FunLike.coe`

--- Mathlib/Data/FunLike/Basic.lean
+++ Mathlib/Data/FunLike/Basic.lean
@@ -147,7 +147,8 @@ namespace FunLike
 
 variable {F α β} [i : FunLike F α β]
 
-instance (priority := 100) hasCoeToFun : CoeFun F fun _ ↦ ∀ a : α, β a where coe := FunLike.coe
+instance (priority := 100) hasCoeToFun : CoeFun F fun _ ↦ ∀ a : α, β a where
+  coe := @FunLike.coe _ _ β _ -- explicit arguments avoid unnecessary beta expansion
 
 #eval Lean.Elab.Command.liftTermElabM do
   Std.Tactic.Coe.registerCoercion ``FunLike.coe
