6eb9f7b3857e15a807352d127e949865f5f9ba97
Yury G. Kudryashov <urkud@urkud.name>
2023-11-13 06:59:23 +0000

    chore(InfiniteSum): use dot notation (#8358)
    ## Rename
    
    - `summable_of_norm_bounded` -> `Summable.of_norm_bounded`;
    - `summable_of_norm_bounded_eventually` -> `Summable.of_norm_bounded_eventually`;
    - `summable_of_nnnorm_bounded` -> `Summable.of_nnnorm_bounded`;
    - `summable_of_summable_norm` -> `Summable.of_norm`;
    - `summable_of_summable_nnnorm` -> `Summable.of_nnnorm`;
    
    ## New lemmas
    
    - `Summable.of_norm_bounded_eventually_nat`
    - `Summable.norm`
    
    ## Misc changes
    
    - Golf a few proofs.

--- Mathlib/Analysis/NormedSpace/Spectrum.lean
+++ Mathlib/Analysis/NormedSpace/Spectrum.lean
@@ -498,7 +498,7 @@ theorem exp_mem_exp [IsROrC 𝕜] [NormedRing A] [NormedAlgebra 𝕜 A] [Complet
       sub_add_cancel]
   let b := ∑' n : ℕ, ((n + 1).factorial⁻¹ : 𝕜) • (a - ↑ₐ z) ^ n
   have hb : Summable fun n : ℕ => ((n + 1).factorial⁻¹ : 𝕜) • (a - ↑ₐ z) ^ n := by
-    refine' summable_of_norm_bounded_eventually _ (Real.summable_pow_div_factorial ‖a - ↑ₐ z‖) _
+    refine' .of_norm_bounded_eventually _ (Real.summable_pow_div_factorial ‖a - ↑ₐ z‖) _
     filter_upwards [Filter.eventually_cofinite_ne 0] with n hn
     rw [norm_smul, mul_comm, norm_inv, IsROrC.norm_natCast, ← div_eq_mul_inv]
     exact div_le_div (pow_nonneg (norm_nonneg _) n) (norm_pow_le' (a - ↑ₐ z) (zero_lt_iff.mpr hn))
