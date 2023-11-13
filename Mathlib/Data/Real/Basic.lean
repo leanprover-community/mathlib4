f772c434e1a162a9e874b8cd7961889d4e9859ad
Scott Morrison <scott.morrison@gmail.com>
2023-11-13 18:29:29 +1100

    doc-string

--- Mathlib/Data/Real/Basic.lean
+++ Mathlib/Data/Real/Basic.lean
@@ -15,6 +15,11 @@ import Mathlib.Data.Real.CauSeqCompletion
 This file defines `ℝ` as the type of equivalence classes of Cauchy sequences of rational numbers.
 This choice is motivated by how easy it is to prove that `ℝ` is a commutative ring, by simply
 lifting everything to `ℚ`.
+
+The facts that the real numbers are an Archimedean floor ring,
+and a conditionally complete linear order,
+have been deferred to the file `Mathlib/Data/Real/Archimedean.lean`,
+in order to keep the imports here simple.
 -/
 
 
