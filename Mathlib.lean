ed7e5710cea0fc302b0f0dc33fbd8ddfcd09eece
Floris van Doorn <fpvdoorn@gmail.com>
2023-11-13 11:14:29 +0100

    add marginal file

--- Mathlib.lean
+++ Mathlib.lean
@@ -2545,6 +2545,7 @@ import Mathlib.MeasureTheory.Integral.IntervalIntegral
 import Mathlib.MeasureTheory.Integral.Layercake
 import Mathlib.MeasureTheory.Integral.Lebesgue
 import Mathlib.MeasureTheory.Integral.LebesgueNormedSpace
+import Mathlib.MeasureTheory.Integral.Marginal
 import Mathlib.MeasureTheory.Integral.MeanInequalities
 import Mathlib.MeasureTheory.Integral.PeakFunction
 import Mathlib.MeasureTheory.Integral.Periodic
