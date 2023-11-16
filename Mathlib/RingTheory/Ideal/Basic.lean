1cda8c10e52d0e882f1d03154060bfef6d14214c
Oliver Nash <github@olivernash.org>
2023-11-16 12:49:29 +0000

    Fix after merging master

--- Mathlib/RingTheory/Ideal/Basic.lean
+++ Mathlib/RingTheory/Ideal/Basic.lean
@@ -3,6 +3,7 @@ Copyright (c) 2018 Kenny Lau. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kenny Lau, Chris Hughes, Mario Carneiro
 -/
+import Mathlib.Tactic.FinCases
 import Mathlib.Data.Nat.Choose.Sum
 import Mathlib.LinearAlgebra.Finsupp
 
