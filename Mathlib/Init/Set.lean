576416f318f9396661d3b9645ca0b734f4371466
Eric Wieser <wieser.eric@gmail.com>
2023-11-16 04:47:35 +0000

    doc: explain extended set notation (#8425)

--- Mathlib/Init/Set.lean
+++ Mathlib/Init/Set.lean
@@ -20,6 +20,7 @@ Given a type `X` and a predicate `p : X → Prop`:
 * `Set X` : the type of sets whose elements have type `X`
 * `{a : X | p a} : Set X` : the set of all elements of `X` satisfying `p`
 * `{a | p a} : Set X` : a more concise notation for `{a : X | p a}`
+* `{f x y | (x : X) (y : Y)} : Set Z` : a more concise notation for `{z : Z | ∃ x y, f x y = z}`
 * `{a ∈ S | p a} : Set X` : given `S : Set X`, the subset of `S` consisting of
    its elements satisfying `p`.
 
@@ -84,6 +85,13 @@ def setOf.unexpander : Lean.PrettyPrinter.Unexpander
   | _ => throw ()
 
 open Std.ExtendedBinder in
+/--
+`{ f x y | (x : X) (y : Y) }` is notation for the set of elements `f x y` constructed from the
+binders `x` and `y`, equivalent to `{z : Z | ∃ x y, f x y = z}`.
+
+If `f x y` is a single identifier, it must be parenthesized to avoid ambiguity with `{x | p x}`;
+for instance, `{(x) | (x : Nat) (y : Nat) (_hxy : x = y^2)}`.
+-/
 macro (priority := low) "{" t:term " | " bs:extBinders "}" : term =>
   `({x | ∃ᵉ $bs:extBinders, $t = x})
 
