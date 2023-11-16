9dcecede9eb33c1be195494e9010389095f1a8ce
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2023-11-16 09:38:11 +0100

    define {arg|log}_mul as alias via X_mul_eq_add_X_iff

--- Mathlib/Analysis/SpecialFunctions/Complex/Arg.lean
+++ Mathlib/Analysis/SpecialFunctions/Complex/Arg.lean
@@ -520,10 +520,7 @@ lemma arg_mul_eq_add_arg_iff {x y : ℂ} (hx₀ : x ≠ 0) (hy₀ : y ≠ 0) :
   rw [← arg_coe_angle_toReal_eq_arg, arg_mul_coe_angle hx₀ hy₀, ← Real.Angle.coe_add,
       Real.Angle.toReal_coe_eq_self_iff_mem_Ioc]
 
-lemma arg_mul {x y : ℂ} (hx₀ : x ≠ 0) (hx₁ : -π / 2 < x.arg) (hx₂ : x.arg ≤ π / 2)
-    (hy₀ : y ≠ 0) (hy₁ : -π / 2 < y.arg) (hy₂ : y.arg ≤ π / 2) :
-    (x * y).arg = x.arg + y.arg :=
-  (arg_mul_eq_add_arg_iff hx₀ hy₀).mpr (by rw [Set.mem_Ioc]; constructor <;> linarith)
+alias ⟨_, arg_mul⟩ := arg_mul_eq_add_arg_iff
 
 section Continuity
 
