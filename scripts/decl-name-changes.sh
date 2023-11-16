2a4935c5d7cf904aef2f658879a26c8dd8824966
Jireh Loreaux <loreaujy@gmail.com>
2023-11-16 09:13:21 -0600

    add optional output file and help message

--- scripts/decl-name-changes.sh
+++ scripts/decl-name-changes.sh
@@ -1,5 +1,39 @@
 #!/bin/bash
 
+while getopts :ho: flag
+do
+    case "${flag}" in
+        h) HELP=true;;
+        o) OUTFILE=${OPTARG};;
+    esac
+done
+
+usage () {
+  echo -e "A tool to find which declarations have been added or removed.
+It will compare HEAD to the provided COMMIT, or, if no commit is provided,
+it compares against the most recent common ancestor of HEAD and master.
+
+USAGE:
+\tdecl-name-changes.sh [OPTIONS] [COMMIT]
+
+OPTIONS:
+\t-h\tprint this help message
+\t-o\tspecify an output file for the diff
+
+Warning: Early termination of this script may result in a *detached head*
+state. You can return to your original place with 'git switch -'.
+
+If the calls to 'lake exe cache get' fail, or otherwise the oleans on disk
+do not match, this script will fail to behave as expected."
+}
+
+if [ "$HELP" = true ]; then
+  usage
+  exit
+fi
+
+shift $(($OPTIND - 1))
+
 if [ -z $1 ]; then
   SHA=$(git merge-base master HEAD)
 else
@@ -50,6 +84,10 @@ lake exe declsIn $OLD $MV_OLD > declsOld.txt
 # return to start point
 git checkout -
 
-diff -u declsOld.txt declsNew.txt
+if [ -z $OUTFILE ]; then
+  diff -u declsOld.txt declsNew.txt
+else
+  diff -u declsOld.txt declsNew.txt | awk '/^[+-]/' > $OUTFILE
+fi
 
 rm declsOld.txt declsNew.txt
