a2e871e6c2ac5f743fc3e3f1f6e883c8cf4fa35b
Wrenna Robson <wren.robson@gmail.com>
2023-11-16 16:14:18 +0000

    Fix style issue

--- Mathlib/Topology/LocalHomeomorph.lean
+++ Mathlib/Topology/LocalHomeomorph.lean
@@ -356,7 +356,8 @@ theorem symm_target : e.symm.target = e.source :=
 @[simp, mfld_simps] theorem symm_symm : e.symm.symm = e := rfl
 #align local_homeomorph.symm_symm LocalHomeomorph.symm_symm
 
-theorem symm_bijective : Function.Bijective (LocalHomeomorph.symm : LocalHomeomorph α β → LocalHomeomorph β α) :=
+theorem symm_bijective : Function.Bijective
+    (LocalHomeomorph.symm : LocalHomeomorph α β → LocalHomeomorph β α) :=
   Equiv.bijective ⟨LocalHomeomorph.symm, LocalHomeomorph.symm, symm_symm, symm_symm⟩
 
 /-- A local homeomorphism is continuous at any point of its source -/
