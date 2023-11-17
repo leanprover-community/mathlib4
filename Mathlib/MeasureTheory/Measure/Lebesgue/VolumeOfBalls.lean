64184e3c386de5e68b5d86373caa27dc16a741e1
Xavier-François Roblot <46200072+xroblot@users.noreply.github.com>
2023-11-17 15:30:31 +0100

    Add file with slight generalization

--- Mathlib/MeasureTheory/Measure/Lebesgue/VolumeOfBalls.lean
+++ Mathlib/MeasureTheory/Measure/Lebesgue/VolumeOfBalls.lean
@@ -5,6 +5,7 @@ Authors: Xavier Roblot
 -/
 import Mathlib.Analysis.SpecialFunctions.Gamma.BohrMollerup
 import Mathlib.MeasureTheory.Constructions.HaarToSphere
+import Mathlib.MeasureTheory.Integral.Pi
 
 /-!
 # Volume of balls
@@ -35,8 +36,6 @@ Using these formula, we compute the volume of the unit balls in several cases.
 
 * `Complex.volume_ball` / `Complex.volume_closedBall`: volume of open and closed balls in `ℂ`.
 -/
-
-local macro_rules | `($x ^ $y) => `(HPow.hPow $x $y) -- See issue lean4#2220
 
 section integrals
 
@@ -157,38 +156,6 @@ theorem Complex.integral_exp_neg_mul_rpow {p b : ℝ} (hp : 1 ≤ p) (hb : 0 < b
     ring
 
 end integrals
-
-section move_me
-
-open BigOperators Fintype MeasureTheory MeasureTheory.Measure
-
-theorem MeasureTheory.integral_finset_prod_eq_pow' {E : Type*} {n : ℕ} (f : E → ℝ) [MeasureSpace E]
-    [SigmaFinite (volume : Measure E)] : ∫ x : (Fin n) → E, ∏ i, f (x i) = (∫ x, f x) ^ n := by
-  induction n with
-  | zero =>
-      simp only [Nat.zero_eq, volume_pi, Finset.univ_eq_empty, Finset.prod_empty, integral_const,
-        pi_empty_univ, ENNReal.one_toReal, smul_eq_mul, mul_one, pow_zero]
-  | succ n n_ih =>
-      calc
-        _ = ∫ x : E × (Fin n → E), (f x.1) * ∏ i : Fin n, f (x.2 i) := by
-          rw [volume_pi, ← ((measurePreserving_piFinSuccAboveEquiv
-            (fun _ => (volume : Measure E)) 0).symm).integral_comp']
-          simp_rw [MeasurableEquiv.piFinSuccAboveEquiv_symm_apply, Fin.insertNth_zero',
-            Fin.prod_univ_succ, Fin.cons_zero, Fin.cons_succ]
-          rfl
-        _ = (∫ x, f x) * (∫ x, f x) ^ n := by rw [← n_ih, ← integral_prod_mul, volume_eq_prod]
-        _ = (∫ x, f x) ^ n.succ := by rw [← pow_succ]
-
-theorem MeasureTheory.integral_finset_prod_eq_pow {E : Type*} (ι : Type*) [Fintype ι] (f : E → ℝ)
-    [MeasureSpace E] [SigmaFinite (volume : Measure E)] :
-    ∫ x : ι → E, ∏ i, f (x i) = (∫ x, f x) ^ (card ι) := by
-  let p := measurePreserving_piCongrLeft (fun _ => (volume : Measure E)) (equivFin ι)
-  rw [volume_pi, ← (p.symm).integral_comp', ← integral_finset_prod_eq_pow']
-  congr!
-  rw [Fintype.prod_equiv (equivFin ι)]
-  exact fun _ => rfl
-
-end move_me
 
 section general_case
 
