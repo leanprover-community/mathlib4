570b1734c7f5fc08df455a001b5dd522f39e4e1e
Scott Morrison <scott.morrison@gmail.com>
2023-11-17 14:49:12 +1100

    .

--- test/LibrarySearch/basic.lean
+++ test/LibrarySearch/basic.lean
@@ -3,6 +3,7 @@ import Mathlib.Util.AssertNoSorry
 import Mathlib.Algebra.Order.Ring.Canonical
 import Mathlib.Data.Quot
 import Mathlib.Data.Nat.Prime
+import Mathlib.Data.Real.Basic
 
 set_option autoImplicit true
 
@@ -221,3 +222,6 @@ lemma prime_of_prime (n : ℕ) : Prime n ↔ Nat.Prime n := by
 -- Example from https://leanprover.zulipchat.com/#narrow/stream/287929-mathlib4/topic/exact.3F.20recent.20regression.3F/near/387691588
 lemma ex' (x : ℕ) (_h₁ : x = 0) (h : 2 * 2 ∣ x) : 2 ∣ x := by
   exact? says exact dvd_of_mul_left_dvd h
+
+example {x y : ℚ} (hxy : x ≤ y) (hyx : y ≤ x) : x = y := by
+  exact?
