b9e2e6567d11cc8a870b2e187d01aa583ecad9b9
Kyle Miller <kmill31415@gmail.com>
2023-11-18 03:56:53 +0000

    feat: remove `DecidableEq` argument from `Finset.sym2` (#8211)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1617,6 +1617,7 @@ import Mathlib.Data.List.Sections
 import Mathlib.Data.List.Sigma
 import Mathlib.Data.List.Sort
 import Mathlib.Data.List.Sublists
+import Mathlib.Data.List.Sym
 import Mathlib.Data.List.TFAE
 import Mathlib.Data.List.ToFinsupp
 import Mathlib.Data.List.Zip
@@ -1663,6 +1664,7 @@ import Mathlib.Data.Multiset.Range
 import Mathlib.Data.Multiset.Sections
 import Mathlib.Data.Multiset.Sort
 import Mathlib.Data.Multiset.Sum
+import Mathlib.Data.Multiset.Sym
 import Mathlib.Data.MvPolynomial.Basic
 import Mathlib.Data.MvPolynomial.Cardinal
 import Mathlib.Data.MvPolynomial.Comap
