cbd22f4410bc2235941f3569cf88ce56e7b679c7
Michael Rothgang <rothgami@math.hu-berlin.de>
2023-11-19 12:45:20 +0100

    One more lemma.

--- Mathlib/Geometry/Manifold/InteriorBoundary.lean
+++ Mathlib/Geometry/Manifold/InteriorBoundary.lean
@@ -34,6 +34,8 @@ Define the interior and boundary of a smooth manifold.
 manifold, interior, boundary
 -/
 
+open Set
+
 -- Let `M` be a smooth manifold with corners over the pair `(E, H)`.
 variable {𝕜 : Type*} [NontriviallyNormedField 𝕜]
   {E : Type*} [NormedAddCommGroup E] [NormedSpace 𝕜 E]
@@ -59,19 +61,6 @@ variable (I M) in
 /-- The **boundary** of a manifold `M` is the set of its boundary points. -/
 protected def boundary : Set M := { x : M | I.isBoundaryPoint x}
 
--- FIXME(MR): find a better wording for the next two docstrings
-/-- Whether `x` is an interior point can equivalently be described by any chart
-  whose source contains `x`. -/
-lemma isInteriorPoint_iff {e : LocalHomeomorph M H} (he : e ∈ atlas H M) {x : M}
-    (hx : x ∈ e.source) : I.isInteriorPoint x ↔ (e.extend I) x ∈ interior (e.extend I).target := by
-  sorry
-
-/-- Whether `x` is a boundary point of `M` can equivalently be described by any chart
-whose source contains `x`. -/
-lemma isBoundaryPoint_iff {e : LocalHomeomorph M H} (he : e ∈ atlas H M) {x : M}
-    (hx : x ∈ e.source) : I.isBoundaryPoint x ↔ (e.extend I) x ∈ frontier (e.extend I).target := by
-  sorry
-
 /-- If `e` and `e'` are two charts, the transition map maps interior points to interior points. -/
 lemma foobar {e e' : LocalHomeomorph M H} (he : e ∈ atlas H M) (he' : e' ∈ atlas H M) {x : M}
     (hx : x ∈ e.source ∩ e'.source) :
@@ -86,6 +75,19 @@ lemma foobar' {e e' : LocalHomeomorph M H} (he : e ∈ atlas H M) (he' : e' ∈ 
 
 -- more abstract result: a local homeomorphism maps interior to interior and boundary to boundary
 
+-- FIXME(MR): find a better wording for the next two docstrings
+/-- Whether `x` is an interior point can equivalently be described by any chart
+  whose source contains `x`. -/
+lemma isInteriorPoint_iff {e : LocalHomeomorph M H} (he : e ∈ atlas H M) {x : M}
+    (hx : x ∈ e.source) : I.isInteriorPoint x ↔ (e.extend I) x ∈ interior (e.extend I).target := by
+  sorry
+
+/-- Whether `x` is a boundary point of `M` can equivalently be described by any chart
+whose source contains `x`. -/
+lemma isBoundaryPoint_iff {e : LocalHomeomorph M H} (he : e ∈ atlas H M) {x : M}
+    (hx : x ∈ e.source) : I.isBoundaryPoint x ↔ (e.extend I) x ∈ frontier (e.extend I).target := by
+  sorry
+
 /-- Every point is either an interior or a boundary point. -/ -- FIXME: better name?!
 lemma isInteriorPoint_or_isBoundaryPoint (x : M) : I.isInteriorPoint x ∨ I.isBoundaryPoint x := by
   set e := extChartAt I x
@@ -96,11 +98,11 @@ lemma isInteriorPoint_or_isBoundaryPoint (x : M) : I.isInteriorPoint x ∨ I.isB
   have : y ∈ interior e.target ∪ frontier e.target := by
     rw [← closure_eq_interior_union_frontier (e.target), this.closure_eq]
     exact mem_extChartAt_target I x
-  exact (Set.mem_union y _ _).mp this
+  exact (mem_union y _ _).mp this
 
 /-- A smooth manifold decomposes into interior and boundary. -/
 lemma univ_eq_interior_union_boundary : (SmoothManifoldWithCorners.interior I M) ∪
-    (SmoothManifoldWithCorners.boundary I M) = (Set.univ : Set M) := by
+    (SmoothManifoldWithCorners.boundary I M) = (univ : Set M) := by
   apply le_antisymm
   · exact fun x _ ↦ trivial
   · exact fun x _ ↦ isInteriorPoint_or_isBoundaryPoint x
@@ -124,17 +126,27 @@ lemma interior_isOpen : IsOpen (SmoothManifoldWithCorners.interior I M) := by
   let e := chartAt H x
   -- Its extended chart has open interior (should be easy).
   let U := interior (e.extend I).target
-  have : IsOpen U := sorry
-
+  have hU : IsOpen U := isOpen_interior
   -- For all `y ∈ e.source`, `y` is an interior point iff its image lies in `U`.
+  -- FIXME: extract this into a separate lemma?
   have : ∀ y, y ∈ e.source → (I.isInteriorPoint y ↔ (e.extend I) y ∈ U) :=
     fun y hy ↦ isInteriorPoint_iff (chart_mem_atlas H x) hy
-  use (e.extend I) ⁻¹' U
-  refine ⟨hx, ?_, ?_⟩
-  · sorry
-  · sorry -- this one I'm not 100% sure about
+  use (e.extend I).source ∩ (e.extend I) ⁻¹' U
+  refine ⟨?_, ?_, ?_⟩
+  · have : x ∈ (e.extend I).source := by
+      rw [e.extend_source]
+      exact mem_chart_source H x
+    exact mem_inter this hx
+  · intro y hy
+    rw [e.extend_source] at hy
+    apply (this y (mem_of_mem_inter_left hy)).mpr
+    have : y ∈ (LocalHomeomorph.extend e I) ⁻¹' U := mem_of_mem_inter_right hy
+    exact this
+  · exact (e.continuousOn_extend I).preimage_open_of_open (e.isOpen_extend_source I) hU
 
 /-- The boundary of any extended chart has empty interior. -/
+-- NB: this is *false* for any set instead of (e.extend I).target:
+-- for instance, $ℚ ⊆ ℝ$ has frontiert ℝ (ℚ is dense in ℝ and ℚ has empty interior).
 -- xxx: do I need that e is in the atlas? I think not; not double-checked.
 -- xxx: is this lemma fully true, or do I need a stronger definition of boundary?
 lemma __root__.LocalHomeomorph.extend_interior_boundary_eq_empty {e : LocalHomeomorph M H} :
