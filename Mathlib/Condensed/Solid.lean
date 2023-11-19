5447ff167f4fde65ebc981a483bc9641289ff346
dagurtomas <dagurtomas@gmail.com>
2023-11-19 21:16:15 +0000

    add todo etc

--- Mathlib/Condensed/Solid.lean
+++ Mathlib/Condensed/Solid.lean
@@ -14,6 +14,13 @@ import Mathlib.Condensed.Limits
 
 This file contains the definition of a solid abelian group: `CondensedAb.isSolid`. Solid abelian
 groups were introduced in [scholze2019condensed], Definition 5.1.
+
+## Main definition
+
+* `CondensedAb.IsSolid`: the predicate on condensed abelian groups describing the property of being
+  solid.
+
+TODO (hard): prove that `(profiniteSolid.obj S).IsSolid` for `S : Profinite`.
 -/
 
 universe u
@@ -22,22 +29,27 @@ open CategoryTheory Limits Profinite Condensed
 
 noncomputable section
 
+namespace Condensed
+
 /-- The free condensed abelian group on a finite set. -/
-abbrev Condensed.finFree : FintypeCat.{u} ⥤ CondensedAb.{u} :=
+abbrev finFree : FintypeCat.{u} ⥤ CondensedAb.{u} :=
   FintypeCat.toProfinite ⋙ profiniteToCondensed ⋙ freeAb.{u}
 
 /-- The free condensed abelian group on a profinite space. -/
-abbrev Condensed.profiniteFree : Profinite.{u} ⥤ CondensedAb.{u} :=
+abbrev profiniteFree : Profinite.{u} ⥤ CondensedAb.{u} :=
   profiniteToCondensed ⋙ freeAb.{u}
 
 /-- The functor sending a profinite space `S` to the condensed abelian group `ℤ[S]^\solid`. -/
-def profiniteSolid : Profinite.{u} ⥤ Condensed.{u} Ab.{u+1} :=
+def profiniteSolid : Profinite.{u} ⥤ CondensedAb.{u} :=
   Ran.loc FintypeCat.toProfinite finFree
 
 /-- The natural transformation `ℤ[S] ⟶ ℤ[S]^\solid`. -/
 def profiniteSolidification : profiniteFree ⟶ profiniteSolid :=
   (Ran.equiv FintypeCat.toProfinite finFree profiniteFree).symm (NatTrans.id _)
 
+end Condensed
+
 /-- The predicate on condensed abelian groups describing the property of being solid. -/
-def CondensedAb.IsSolid (A : CondensedAb.{u}) : Prop :=
-  ∀ X : Profinite.{u}, IsIso ((yoneda.obj A).map (profiniteSolidification.app X).op)
+class CondensedAb.IsSolid (A : CondensedAb.{u}) : Prop where
+  isIso_solidification_map : ∀ X : Profinite.{u}, IsIso ((yoneda.obj A).map
+    (profiniteSolidification.app X).op)
