cf455bcd3aebd3dae16e4f6841077b958a158092
dagurtomas <dagurtomas@gmail.com>
2023-11-20 10:58:25 +0000

    define structure

--- Mathlib/Topology/Category/Profinite/Light.lean
+++ Mathlib/Topology/Category/Profinite/Light.lean
@@ -1,7 +1,25 @@
-import Mathlib.Topology.Category.Profinite.AsLimit
+import Mathlib.Topology.Category.Profinite.Basic
 
 open CategoryTheory Limits FintypeCat
 
+structure LightProfinite where
+  diagram : ℕ ⥤ FintypeCat
+  cone : Cone (diagram ⋙ toProfinite)
+  isLimit : IsLimit cone
 
-noncomputable
-def Light : (ℕ ⥤ FintypeCat) ⥤ Profinite := (whiskeringRight _ _ _).obj toProfinite ⋙ lim
+namespace LightProfinite
+
+def toProfinite (S : LightProfinite) : Profinite := S.cone.pt
+
+instance : Category LightProfinite := InducedCategory.category toProfinite
+
+instance concreteCategory : ConcreteCategory LightProfinite := InducedCategory.concreteCategory _
+
+@[simps!]
+def lightToProfinite : LightProfinite ⥤ Profinite := inducedFunctor _
+
+instance : Faithful lightToProfinite := show Faithful <| inducedFunctor _ from inferInstance
+
+instance : Full lightToProfinite := show Full <| inducedFunctor _ from inferInstance
+
+end LightProfinite
