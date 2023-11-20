091ee15bfc5ba99ed1d2532d25fbd9c3323e6e04
Junyan Xu <junyanxu.math@gmail.com>
2023-11-19 23:02:15 -0500

    Discard changes to Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -301,7 +301,6 @@ import Mathlib.Algebra.Module.Basic
 import Mathlib.Algebra.Module.BigOperators
 import Mathlib.Algebra.Module.Bimodule
 import Mathlib.Algebra.Module.DedekindDomain
-import Mathlib.Algebra.Module.DirectLimitAndTensorProduct
 import Mathlib.Algebra.Module.Equiv
 import Mathlib.Algebra.Module.GradedModule
 import Mathlib.Algebra.Module.Hom
