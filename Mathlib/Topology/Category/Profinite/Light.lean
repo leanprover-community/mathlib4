4ec57bbc11a678dcd0b9206fcb5a9c556f7dc1d8
dagurtomas <dagurtomas@gmail.com>
2023-11-20 23:18:31 +0000

    start fighting with lean about the equivalence

--- Mathlib/Topology/Category/Profinite/Light.lean
+++ Mathlib/Topology/Category/Profinite/Light.lean
@@ -26,6 +26,31 @@ instance : Full lightToProfinite := show Full <| inducedFunctor _ from inferInst
 
 end LightProfinite
 
+structure LightProfiniteExplicit : Type (u+1) where
+  diagram : ℕ ⥤ FintypeCat.{u}
+
+namespace LightProfiniteExplicit
+
+noncomputable section
+
+def toProfinite (S : LightProfiniteExplicit) : Profinite := limit <|
+  S.diagram ⋙ FintypeCat.toProfinite
+
+instance : Category LightProfiniteExplicit := InducedCategory.category toProfinite
+
+instance concreteCategory : ConcreteCategory LightProfinite := InducedCategory.concreteCategory _
+
+@[simps!]
+def lightToProfinite : LightProfinite ⥤ Profinite := inducedFunctor _
+
+instance : Faithful lightToProfinite := show Faithful <| inducedFunctor _ from inferInstance
+
+instance : Full lightToProfinite := show Full <| inducedFunctor _ from inferInstance
+
+end
+
+end LightProfiniteExplicit
+
 structure LightProfinite' : Type u where
   diagram : ℕ ⥤ FintypeCat.Skeleton.{u}
 
@@ -52,3 +77,42 @@ instance : Full lightToProfinite' := show Full <| inducedFunctor _ from inferIns
 end
 
 end LightProfinite'
+
+noncomputable section Equivalence
+
+def explicitToSmall_aux : LightProfiniteExplicit ⥤ LightProfinite' where
+  obj X := ⟨X.diagram ⋙ Skeleton.equivalence.inverse⟩
+  map f := sorry
+  map_id := sorry
+  map_comp := sorry
+
+def lightToSmall_aux : LightProfinite ⥤ LightProfiniteExplicit where
+  obj X := ⟨X.diagram⟩
+  map {X Y} f := ((IsLimit.conePointUniqueUpToIso X.isLimit (limit.isLimit _)).inv ≫ f ≫
+      (IsLimit.conePointUniqueUpToIso Y.isLimit (limit.isLimit _)).hom : _)
+  map_id X := by
+    simp only [limit.cone_x]
+    erw [Category.id_comp]
+    simp only [Iso.inv_hom_id]
+    rfl
+  map_comp {X Y Z} f g := by
+    simp only [limit.cone_x]
+    change _ = _ ≫ _ ≫ (IsLimit.conePointUniqueUpToIso Y.isLimit (limit.isLimit _)).hom  ≫
+      (IsLimit.conePointUniqueUpToIso Y.isLimit (limit.isLimit _)).inv ≫ _ ≫ _
+    rw [Iso.hom_inv_id_assoc]
+    rfl
+
+def lightToSmall : LightProfinite ⥤ LightProfinite' := lightToSmall_aux ⋙ explicitToSmall_aux
+
+def smallToLight : LightProfinite' ⥤ LightProfinite where
+  obj X := ⟨X.diagram ⋙ Skeleton.equivalence.functor, _, limit.isLimit _⟩
+  map f := f
+
+def LightProfinite.equivSmall : LightProfinite ≌ LightProfinite' where
+  functor := lightToSmall_aux ⋙ explicitToSmall_aux
+  inverse := smallToLight
+  unitIso := sorry
+  counitIso := sorry
+  functor_unitIso_comp := sorry
+
+end Equivalence
