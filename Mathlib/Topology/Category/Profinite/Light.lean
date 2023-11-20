cd541ac365a26d3bcad39d37bf771c3eb264bbba
dagurtomas <dagurtomas@gmail.com>
2023-11-20 11:08:23 +0000

    add small version

--- Mathlib/Topology/Category/Profinite/Light.lean
+++ Mathlib/Topology/Category/Profinite/Light.lean
@@ -1,10 +1,12 @@
 import Mathlib.Topology.Category.Profinite.Basic
 
+universe u
+
 open CategoryTheory Limits FintypeCat
 
-structure LightProfinite where
+structure LightProfinite : Type (u+1) where
   diagram : ℕ ⥤ FintypeCat
-  cone : Cone (diagram ⋙ toProfinite)
+  cone : Cone (diagram ⋙ toProfinite.{u})
   isLimit : IsLimit cone
 
 namespace LightProfinite
@@ -23,3 +25,30 @@ instance : Faithful lightToProfinite := show Faithful <| inducedFunctor _ from i
 instance : Full lightToProfinite := show Full <| inducedFunctor _ from inferInstance
 
 end LightProfinite
+
+structure LightProfinite' : Type u where
+  diagram : ℕ ⥤ FintypeCat.Skeleton.{u}
+
+namespace LightProfinite'
+
+noncomputable section
+
+def toProfinite (S : LightProfinite') : Profinite :=
+  limit (S.diagram  ⋙ FintypeCat.Skeleton.equivalence.functor ⋙ FintypeCat.toProfinite.{u})
+
+instance : Category LightProfinite' := InducedCategory.category toProfinite
+
+instance : SmallCategory LightProfinite' := inferInstance
+
+instance concreteCategory : ConcreteCategory LightProfinite' := InducedCategory.concreteCategory _
+
+@[simps!]
+def lightToProfinite' : LightProfinite' ⥤ Profinite := inducedFunctor _
+
+instance : Faithful lightToProfinite' := show Faithful <| inducedFunctor _ from inferInstance
+
+instance : Full lightToProfinite' := show Full <| inducedFunctor _ from inferInstance
+
+end
+
+end LightProfinite'
