12c41bf171a136c69633711f7d72ec443e0cdb5e
Junyan Xu <junyanxumath@gmail.com>
2023-11-21 04:10:35 -0500

    redefine UnivLE

--- Mathlib/Logic/UnivLE.lean
+++ Mathlib/Logic/UnivLE.lean
@@ -4,17 +4,14 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison
 -/
 import Mathlib.Logic.Small.Basic
-
+import Mathlib.SetTheory.Ordinal.Basic
 /-!
 # UnivLE
 
 A proposition expressing a universe inequality. `UnivLE.{u, v}` expresses that `u ≤ v`,
-in the form `∀ α : Type max u v, Small.{v} α`.
+in the form `∀ α : Type u, Small.{v} α`.
 
 See the doc-string for the comparison with an alternative weaker definition.
-
-See also `Mathlib.CategoryTheory.UnivLE` for the statement
-`UnivLE.{u,v} ↔ EssSurj (uliftFunctor : Type v ⥤ Type max u v)`.
 -/
 
 set_option autoImplicit true
@@ -24,31 +21,59 @@ noncomputable section
 /--
 A class expressing a universe inequality. `UnivLE.{u, v}` expresses that `u ≤ v`.
 
-There are (at least) two plausible definitions for `u ≤ v`:
-* strong: `∀ α : Type max u v, Small.{v} α`
-* weak: `∀ α : Type u, Small.{v} α`
+There used to be a stronger definition `∀ α : Type max u v, Small.{v} α` that immediately implies
+`Small.{v} ((α : Type u) → (β : Type v))` which is essential for proving that `Type v` has
+`Type u`-indexed limits when `u ≤ v`. However the current weaker condition
+`∀ α : Type u, Small.{v} α` also implies the same, so we switched to use it for
+its simplicity and transitivity.
 
-The weak definition has the advantage of being transitive.
-However only under the strong definition do we have `Small.{v} ((α : Type u) → (β : Type v))`,
-which is essential for proving that `Type v` has `Type u`-indexed limits when `u ≤ v`.
-
-The strong definition implies the weaker definition (see below),
+The strong definition easily implies the weaker definition (see below),
 but we can not prove the reverse implication.
 This is because in Lean's type theory, while `max u v` is at least at big as `u` and `v`,
 it could be bigger than both!
+See also `Mathlib.CategoryTheory.UnivLE` for the statement that the stronger definition is
+equivalent to `EssSurj (uliftFunctor : Type v ⥤ Type max u v)`.
 -/
 @[pp_with_univ]
-abbrev UnivLE.{u, v} : Prop := ∀ α : Type max u v, Small.{v} α
+abbrev UnivLE.{u, v} : Prop := ∀ α : Type u, Small.{v} α
 
 example : UnivLE.{u, u} := inferInstance
 example : UnivLE.{u, u+1} := inferInstance
 example : UnivLE.{0, u} := inferInstance
-example : UnivLE.{u, max u v} := inferInstance
+/- This is useless as an instance due to https://github.com/leanprover/lean4/issues/2297 -/
+theorem univLE_max : UnivLE.{u, max u v} := fun α ↦ small_max.{v} α
 
-instance [UnivLE.{u, v}] (α : Type u) : Small.{v} α :=
-  ⟨Shrink.{v, max u v} (ULift.{v} α),
-    ⟨Equiv.ulift.symm.trans (equivShrink (ULift α))⟩⟩
+theorem Small.trans_univLE.{u, v} (α : Type w) [hα : Small.{u} α] [h : UnivLE.{u, v}] :
+    Small.{v} α :=
+  let ⟨β, ⟨f⟩⟩ := hα.equiv_small
+  let ⟨_, ⟨g⟩⟩ := (h β).equiv_small
+  ⟨_, ⟨f.trans g⟩⟩
+
+theorem UnivLE.trans [UnivLE.{u, v}] [UnivLE.{v, w}] : UnivLE.{u, w} :=
+  fun α ↦ Small.trans_univLE α
+
+/- This is the crucial instance that subsumes `univLE_max`. -/
+instance univLE_of_max [UnivLE.{max u v, v}] : UnivLE.{u, v} := @UnivLE.trans univLE_max.{v,u} ‹_›
+
+/- uses small_Pi -/
+example (α : Type u) (β : Type v) [UnivLE.{u, v}] : Small.{v} (α → β) := inferInstance
 
 example : ¬ UnivLE.{u+1, u} := by
   simp only [Small_iff, not_forall, not_exists, not_nonempty_iff]
   exact ⟨Type u, fun α => ⟨fun f => Function.not_surjective_Type.{u, u} f.symm f.symm.surjective⟩⟩
+
+open Cardinal
+
+theorem univLE_iff_cardinal_le : UnivLE.{u, v} ↔ univ.{u, v+1} ≤ univ.{v, u+1} := by
+  rw [← not_iff_not, UnivLE]; simp_rw [small_iff_lift_mk_lt_univ]; push_neg
+  -- strange: simp_rw [univ_umax.{v,u}] doesn't work
+  refine ⟨fun ⟨α, le⟩ ↦ ?_, fun h ↦ ?_⟩
+  · rw [univ_umax.{v,u}, ← lift_le.{u+1}, lift_univ, lift_lift] at le
+    exact le.trans_lt (lift_lt_univ'.{u,v+1} #α)
+  · obtain ⟨⟨α⟩, h⟩ := lt_univ'.mp h; use α
+    rw [univ_umax.{v,u}, ← lift_le.{u+1}, lift_univ, lift_lift]
+    exact h.le
+
+/-- Together with transitivity, this shows UnivLE "IsTotalPreorder". -/
+theorem univLE_total : UnivLE.{u, v} ∨ UnivLE.{v, u} := by
+  simp_rw [univLE_iff_cardinal_le]; apply le_total
