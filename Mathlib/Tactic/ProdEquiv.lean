a6b784282b49da15a67aafb575b822e4eeccbff1
adamtopaz <github@adamtopaz.com>
2023-11-21 08:51:57 -0700

    still some bugs

--- Mathlib/Tactic/ProdEquiv.lean
+++ Mathlib/Tactic/ProdEquiv.lean
@@ -1,32 +1,60 @@
+/-
+Copyright (c) 2023 Adam Topaz. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Adam Topaz
+-/
 import Mathlib.Lean.Expr.Basic
 import Mathlib.Logic.Equiv.Basic
-import Qq
+
+/-!
+# Associativity of products
+
+This file constructs a term elaborator for "obvious" equivalences between iterated products.
+For example,
+```lean
+prod_assoc((α × β) × (γ × δ), α × (β × γ) × δ)
+```
+gives the "obvious" equivalence between `(α × β) × (γ × δ)` and `α × (β × γ) × δ`.
+-/
 
 namespace Lean.Expr
 
 open Lean Meta
 
+/-- A helper type to keep track of universe levels and types in iterated produts. -/
 inductive ProdTree where
   | type (tp : Expr) (l : Level)
   | prod (fst snd : ProdTree) (lfst lsnd : Level)
 deriving Repr
 
+/-- The iterated product corresponding to a `ProdTree`. -/
 def ProdTree.getType : ProdTree → Expr
   | type tp _ => tp
   | prod fst snd u v => mkAppN (.const ``Prod [u,v]) #[fst.getType, snd.getType]
 
+/-- The number of types appearing in an iterated product encoded as a `ProdTree`. -/
 def ProdTree.size : ProdTree → Nat
   | type _ _ => 1
   | prod fst snd _ _ => fst.size + snd.size
 
+/-- Make a `ProdTree` out of an `Expr`. -/
 def mkProdTree (e : Expr) : MetaM ProdTree :=
   match e with
     | .app (.app (.const ``Prod [u,v]) X) Y => do
         return .prod (← X.mkProdTree) (← Y.mkProdTree) u v
     | X => do
-      let some u := (← inferType X).type? | throwError "Type expected."
+      let some u := (← inferType X).type? | throwError "Not a type{indentExpr X}"
       return .type X u
 
+/-- Given `P : ProdTree` representing an iterated product and `e : Expr` which
+should correspond to a term of the iterated product, this will return
+a list, whose items correspond to the leaves of `P` (i.e. the types appearing in the product),
+where each item is the appropriate composition of `Prod.fst` and `Prod.snd` applied to `e`
+resulting in an element of the type corresponding to the leaf.
+
+For example, if `P` corresponds to `(X × Y) × Z` and `t : (X × Y) × Z`, then this
+should return `[t.fst.fst, t.fst.snd, t.snd]`.
+-/
 def ProdTree.unpack (t : Expr) : ProdTree → MetaM (List Expr)
   | type _ _ => return [t]
   | prod fst snd u v => do
@@ -34,6 +62,8 @@ def ProdTree.unpack (t : Expr) : ProdTree → MetaM (List Expr)
       let snd' ← snd.unpack <| mkAppN (.const ``Prod.snd [u,v]) #[fst.getType, snd.getType, t]
       return fst' ++ snd'
 
+/-- This function should act as the "reverse" of `ProdTree.unpack`, constructing
+a term of the iterated product out of a list of terms of the types appearing in the product. -/
 def ProdTree.pack (ts : List Expr) : ProdTree → MetaM Expr
   | type tp _ => do
     match ts with
@@ -51,25 +81,31 @@ def ProdTree.pack (ts : List Expr) : ProdTree → MetaM Expr
     let mk : Expr := mkAppN (.const ``Prod.mk [u,v]) #[fst.getType, snd.getType]
     return .app (.app mk (← fst.pack tsfst)) (← snd.pack tssnd)
 
-def mkFun (a b : Expr) : MetaM Expr := do
+/-- Given two expressions corresponding to iterated products of the same types, associated in
+possibly different ways, this constructs the "obvious" function from one to the other. -/
+def mkProdFun (a b : Expr) : MetaM Expr := do
   let pa ← a.mkProdTree
   let pb ← b.mkProdTree
   return .lam `t a (← pb.pack <| (← pa.unpack <| .bvar 0)) .default
 
-def mkEquiv (a b : Expr) : MetaM Expr := do
-  let some u := (← inferType a).type? | throwError "Type expected."
-  let some v := (← inferType b).type? | throwError "Type expected."
+/-- Construct the equivalence between iterated products of the same type, associated
+in possibly different ways. -/
+def mkProdEquiv (a b : Expr) : MetaM Expr := do
+  let some u := (← inferType a).type? | throwError "Not a type{indentExpr a}"
+  let some v := (← inferType b).type? | throwError "Not a type{indentExpr b}"
   return mkAppN (.const ``Equiv.mk [.succ u,.succ v])
-    #[a, b, ← mkFun a b, ← mkFun b a,
+    #[a, b, ← mkProdFun a b, ← mkProdFun b a,
       .app (.const ``rfl [.succ u]) a,
       .app (.const ``rfl [.succ v]) b]
 
+
+/-- An elaborator version of `Lean.Expr.mkEquiv`. -/
 elab "prod_assoc(" a:term "," b:term ")" : term => do
   let a ← Elab.Term.elabTerm a none
   let b ← Elab.Term.elabTerm b none
-  mkEquiv a b
+  mkProdEquiv a b
 
-example {α β γ δ : Type*} (a : α) (b : β) (g : γ) (d : δ) :
-  prod_assoc((α × β) × (γ × δ), α × (β × γ) × δ) ((a,b),(g,d)) = (a,(b,g),d) := rfl
+#check prod_assoc(Nat,Nat)
 
 end Lean.Expr
+#lint
