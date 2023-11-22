aec2216135dda29faabdfd7770569e410219a9af
Yaël Dillies <yael.dillies@gmail.com>
2023-11-22 21:03:54 +0000

    feat: Marica-Schönheim special case of Graham's conjecture

--- Mathlib.lean
+++ Mathlib.lean
@@ -2685,6 +2685,7 @@ import Mathlib.NumberTheory.Liouville.Measure
 import Mathlib.NumberTheory.Liouville.Residual
 import Mathlib.NumberTheory.LucasLehmer
 import Mathlib.NumberTheory.LucasPrimality
+import Mathlib.NumberTheory.MaricaSchoenheim
 import Mathlib.NumberTheory.Modular
 import Mathlib.NumberTheory.ModularForms.Basic
 import Mathlib.NumberTheory.ModularForms.CongruenceSubgroups
