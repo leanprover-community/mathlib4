5504a0049cfd3d2f9be663daaa38d3ab2f149662
Junyan Xu <junyanxu.math@gmail.com>
2023-11-22 05:21:07 -0500

    Cardinal/UnivLE: I guess I'll take authorship of this ...

--- Mathlib/SetTheory/Cardinal/UnivLE.lean
+++ Mathlib/SetTheory/Cardinal/UnivLE.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2023 Scott Morrison. All rights reserved.
+Copyright (c) 2023 Junyan Xu. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Scott Morrison
+Authors: Junyan Xu
 -/
 import Mathlib.Logic.UnivLE
 import Mathlib.SetTheory.Ordinal.Basic
