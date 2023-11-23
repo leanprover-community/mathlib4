c9fe09849ded34ff118a00d1fc2cebfe2e93eb0b
Yaël Dillies <yael.dillies@gmail.com>
2023-11-23 12:56:43 +0000

    chore: Move `Data.Nat.Fib` → `Data.Nat.Fib.Basic` (#8576)
    This was postponed to after the Zeckendorf PR.

--- Mathlib.lean
+++ Mathlib.lean
@@ -1717,7 +1717,7 @@ import Mathlib.Data.Nat.Factorial.SuperFactorial
 import Mathlib.Data.Nat.Factorization.Basic
 import Mathlib.Data.Nat.Factorization.PrimePow
 import Mathlib.Data.Nat.Factors
-import Mathlib.Data.Nat.Fib
+import Mathlib.Data.Nat.Fib.Basic
 import Mathlib.Data.Nat.Fib.Zeckendorf
 import Mathlib.Data.Nat.ForSqrt
 import Mathlib.Data.Nat.GCD.Basic
