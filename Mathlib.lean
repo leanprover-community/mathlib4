87a64b11a839fb9fcdb19d84d20f9b4b7d5fdaeb
Michael Rothgang <rothgami@math.hu-berlin.de>
2023-11-23 18:11:19 +0100

    Snapshot: add current state of inverse function theorem.

--- Mathlib.lean
+++ Mathlib.lean
@@ -2072,6 +2072,7 @@ import Mathlib.Geometry.Manifold.Diffeomorph
 import Mathlib.Geometry.Manifold.Instances.Real
 import Mathlib.Geometry.Manifold.Instances.Sphere
 import Mathlib.Geometry.Manifold.Instances.UnitsOfNormedAlgebra
+import Mathlib.Geometry.Manifold.InverseFunctionTheorem
 import Mathlib.Geometry.Manifold.LocalDiffeomorph
 import Mathlib.Geometry.Manifold.LocalInvariantProperties
 import Mathlib.Geometry.Manifold.MFDeriv
