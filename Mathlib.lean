3f07ec65684facd91a8b18c4d03a7d0a13eb6225
FMLJohn <417963616@qq.com>
2023-11-23 15:30:51 +0000

    updated mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -2940,6 +2940,7 @@ import Mathlib.RingTheory.FreeRing
 import Mathlib.RingTheory.GradedAlgebra.Basic
 import Mathlib.RingTheory.GradedAlgebra.HomogeneousIdeal
 import Mathlib.RingTheory.GradedAlgebra.HomogeneousLocalization
+import Mathlib.RingTheory.GradedAlgebra.Noetherian
 import Mathlib.RingTheory.GradedAlgebra.Radical
 import Mathlib.RingTheory.HahnSeries
 import Mathlib.RingTheory.Henselian
