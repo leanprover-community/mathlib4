265a6e4cef0b2f61aa62ee79b4d98a672daf9474
Eric Wieser <wieser.eric@gmail.com>
2023-11-23 22:47:07 +0000

    extern and refactor tests

--- test/delabLinearIndependent.lean
+++ test/delabLinearIndependent.lean
@@ -3,11 +3,19 @@ import Mathlib.LinearAlgebra.LinearIndependent
 set_option pp.unicode.fun true
 
 variable {K V : Type*} [DivisionRing K] [AddCommGroup V] [Module K V] {s : Set V} {x : V}
-  (hs : LinearIndependent K (fun b => b : s → V))
-  (hs' : LinearIndependent K (Subtype.val : s → V))
 
-/-- info: hs : LinearIndependent K fun (b : ↑s) ↦ ↑b -/
-#guard_msgs in #check hs
+variable (h : LinearIndependent K (fun b => b : s → V)) in
+/-- info: h : LinearIndependent K fun (b : ↑s) ↦ ↑b -/
+#guard_msgs in #check h
 
-/-- info: hs' : LinearIndependent (ι := { x // x ∈ s }) K Subtype.val -/
-#guard_msgs in #check hs'
+variable (h : LinearIndependent K (Subtype.val : s → V)) in
+/-- info: h : LinearIndependent (ι := { x // x ∈ s }) K Subtype.val -/
+#guard_msgs in #check h
+
+variable (h : LinearIndependent K (by exact Subtype.val : s → V)) in
+/-- info: h : LinearIndependent (ι := ↑s) K Subtype.val -/
+#guard_msgs in #check h
+
+variable (h : LinearIndependent K (fun b => (fun b => b : s → V) b)) in
+/-- info: h : LinearIndependent K fun (b : ↑s) ↦ (fun b ↦ ↑b) b -/
+#guard_msgs in #check h
