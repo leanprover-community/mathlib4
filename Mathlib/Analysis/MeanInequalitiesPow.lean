c2f4febf2bc9509ce16036366f2b7c92ce3c407d
Yaël Dillies <yael.dillies@gmail.com>
2023-11-24 08:12:17 +0000

    remove useless import

--- Mathlib/Analysis/MeanInequalitiesPow.lean
+++ Mathlib/Analysis/MeanInequalitiesPow.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yury Kudryashov, Sébastien Gouëzel, Rémy Degenne
 -/
 import Mathlib.Analysis.Convex.Jensen
-import Mathlib.Analysis.Convex.Mul
 import Mathlib.Analysis.Convex.SpecificFunctions.Basic
 import Mathlib.Analysis.SpecialFunctions.Pow.NNReal
 import Mathlib.Tactic.Positivity
