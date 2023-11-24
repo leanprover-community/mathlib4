988870b7bb3605e9e8aba8f9097778a2fd4190a7
Junyan Xu <junyanxumath@gmail.com>
2023-11-24 09:10:51 +0000

    feat: switch to weaker UnivLE (#8556)
    Switch from the strong version of UnivLE `∀ α : Type max u v, Small.{v} α` to the weaker version `∀ α : Type u, Small.{v} α`.
    
    Transfer Has/Preserves/Reflects(Co)limitsOfSize from a larger size (higher universe) to a smaller size.
    
    In a few places it's now necessary to make the type explicit (for Lean to infer the `Small` instance, I think).
    
    Also prove a characterization of UnivLE and the totality of the UnivLE relation.
    
    A pared down version of #7695.
    
    
    
    Co-authored-by: Scott Morrison <scott.morrison@gmail.com>
    Co-authored-by: Junyan Xu <junyanxu.math@gmail.com>

---
+++ Mathlib/SetTheory/Cardinal/UnivLE.lean
@@ -0,0 +1,31 @@
+/-
+Copyright (c) 2023 Junyan Xu. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Junyan Xu
+-/
+import Mathlib.Logic.UnivLE
+import Mathlib.SetTheory.Ordinal.Basic
+
+/-!
+# UnivLE and cardinals
+-/
+
+set_option autoImplicit true
+
+noncomputable section
+
+open Cardinal
+
+theorem univLE_iff_cardinal_le : UnivLE.{u, v} ↔ univ.{u, v+1} ≤ univ.{v, u+1} := by
+  rw [← not_iff_not, UnivLE]; simp_rw [small_iff_lift_mk_lt_univ]; push_neg
+  -- strange: simp_rw [univ_umax.{v,u}] doesn't work
+  refine ⟨fun ⟨α, le⟩ ↦ ?_, fun h ↦ ?_⟩
+  · rw [univ_umax.{v,u}, ← lift_le.{u+1}, lift_univ, lift_lift] at le
+    exact le.trans_lt (lift_lt_univ'.{u,v+1} #α)
+  · obtain ⟨⟨α⟩, h⟩ := lt_univ'.mp h; use α
+    rw [univ_umax.{v,u}, ← lift_le.{u+1}, lift_univ, lift_lift]
+    exact h.le
+
+/-- Together with transitivity, this shows UnivLE "IsTotalPreorder". -/
+theorem univLE_total : UnivLE.{u, v} ∨ UnivLE.{v, u} := by
+  simp_rw [univLE_iff_cardinal_le]; apply le_total
