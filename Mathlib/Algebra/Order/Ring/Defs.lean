a233d42910279b6ad75440d3cf035534a9a6627f
madvorak <martin.dvorak@matfyz.cz>
2023-11-24 14:54:54 +0100

    delete two lines

--- Mathlib/Algebra/Order/Ring/Defs.lean
+++ Mathlib/Algebra/Order/Ring/Defs.lean
@@ -100,12 +100,10 @@ immediate predecessors and what conditions are added to each of them.
   - `StrictOrderedRing` & totality of the order
   - `LinearOrderedSemiring` & additive inverses
   - `LinearOrderedAddCommGroup` & multiplication & `*` respects `<`
-  - `IsDomain` & linear order structure
 * `LinearOrderedCommRing`
   - `StrictOrderedCommRing` & totality of the order
   - `LinearOrderedRing` & commutativity of multiplication
   - `LinearOrderedCommSemiring` & additive inverses
-  - `IsDomain` & linear order structure & commutativity of multiplication
 
 -/
 
