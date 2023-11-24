a9838e2bdf6f1c21b6acc2356a7ec2d649ab14db
Gareth Ma <grhkm21@gmail.com>
2023-11-24 14:02:47 +0000

    chore: lint

--- Mathlib/Data/Nat/Choose/Lucas.lean
+++ Mathlib/Data/Nat/Choose/Lucas.lean
@@ -7,23 +7,25 @@ import Mathlib.Data.Real.Basic
 import Mathlib.Data.ZMod.Basic
 import Mathlib.RingTheory.Polynomial.Basic
 
+/-!
+# Funny docstring placeholder blablabla
+-/
+
 open Nat BigOperators Polynomial
 /- An alternative is to use n.choose k everywhere. Otherwise, this imports `Finset.choose` -/
 open Finset hiding choose
-
-/- TODO: Documentation -/
 
 variable {n k p : ℕ} [hp : Fact p.Prime]
 
 theorem lucas : choose n k ≡ choose (n % p) (k % p) * choose (n / p) (k / p) [ZMOD p] := by
   have decompose : ((X : (ZMod p)[X]) + 1) ^ n = (X + 1) ^ (n % p) * (X ^ p + 1) ^ (n / p) := by
     nth_rw 1 [←mod_add_div n p, pow_add, pow_mul, add_pow_char (ZMod p)[X] (p := p), one_pow]
-  simp only [←ZMod.int_cast_eq_int_cast_iff, Int.cast_mul, Int.cast_ofNat, ←coeff_X_add_one_pow _ n k]
-  rw [←eq_intCast (Int.castRingHom (ZMod p)), ←coeff_map, Polynomial.map_pow, Polynomial.map_add,
-    Polynomial.map_one, map_X, decompose]
+  simp only [←ZMod.int_cast_eq_int_cast_iff, Int.cast_mul, Int.cast_ofNat,
+    ←coeff_X_add_one_pow _ n k, ←eq_intCast (Int.castRingHom (ZMod p)), ←coeff_map,
+    Polynomial.map_pow, Polynomial.map_add, Polynomial.map_one, map_X, decompose]
   simp only [add_pow, one_pow, mul_one, ←pow_mul, sum_mul_sum]
   conv =>
-    lhs ; arg 1 ; arg 2 ; ext k
+    lhs; arg 1; arg 2; ext k
     rw [←mul_assoc, mul_right_comm _ _ (X ^ (p * k.2)), ←pow_add, mul_assoc, ←cast_mul]
   rw [finset_sum_coeff]
   simp only [coeff_mul_natCast, coeff_X_pow, ite_mul, zero_mul, one_mul, ←cast_mul]
