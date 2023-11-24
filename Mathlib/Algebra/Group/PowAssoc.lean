163d0e3c3a23126ed099471f74124470e7255e40
Scott Carnahan <128885296+ScottCarnahan@users.noreply.github.com>
2023-11-25 00:03:58 +0900

    revise ppowRec definition and results
    also, minor cleanup

--- Mathlib/Algebra/Group/PowAssoc.lean
+++ Mathlib/Algebra/Group/PowAssoc.lean
@@ -7,40 +7,48 @@ Authors: Scott Carnahan
 import Mathlib.Algebra.Group.Defs
 import Mathlib.Algebra.GroupPower.Basic
 import Mathlib.Data.PNat.Basic
-import Mathlib.Tactic.ComputeDegree
 
 /-!
 # Typeclasses for power-associative structures
 
 In this file we define power-associativity for algebraic structures with a multiplication operation.
 The class is a Prop-valued mixin named `PNatPowAssoc`, where `PNat` means only strictly positive
-powers are used.
+powers are considered.
 
-We also produce the following instance:
+## Results
 
+- `ppow_add` a defining property: `x ^ (k + n) = x ^ k * x ^ n`
+- `ppow_one` a defining property: `x ^ 1 = x`
+- `ppow_assoc` strictly positive powers of an element have associative multiplication.
+- `ppow_comm` `x ^ m * x ^ n = x ^ n * x ^ m` for strictly positive `m` and `n`.
+- `ppow_mul` `x ^ (m * n) = (x ^ m) ^ n` for strictly positive `m` and `n`.
+
+## Instances
+
+- `Pow M ℕ+` for semigroups `M`.
 - `PNatPowAssoc` for semigroups.
 
 ## Todo
 
-* `NatPowAssoc` for `MulWithOne` classes.
+* `NatPowAssoc` for `MulWithOne` classes - this should be easier.
 * It seems unlikely that anyone will want `NatSMulAssoc` and `PNatSmulAssoc` as additive versions of
-power-associativity, but it is not hard to write.
+power-associativity, but we have found that it is not hard to write.
 * simp tags?
 
 -/
 
 universe u
 
-variable {G M : Type u}
+variable {M : Type u}
 
 section PNatPowAssoc
 
 variable [Mul M] [Pow M ℕ+]
 
-/-- A mixin for power-associative multiplication in the non-unital setting. -/
+/-- A `Prop`-valued mixin for power-associative multiplication in the non-unital setting. -/
 class PNatPowAssoc (M : Type u) [Mul M] [Pow M ℕ+] : Prop where
   /-- Multiplication is power-associative. -/
-  protected ppow_add : ∀ (k n: ℕ+) (x : M), x ^ (k + n) = x ^ k * x ^ n
+  protected ppow_add : ∀ (k n : ℕ+) (x : M), x ^ (k + n) = x ^ k * x ^ n
   /-- Exponent one is identity. -/
   protected ppow_one : ∀ (x : M), x ^ (1 : ℕ+) = x
 
@@ -54,6 +62,9 @@ theorem ppow_assoc [PNatPowAssoc M] (k m n : ℕ+) (x : M) :
     x ^ k * (x ^ m * x ^ n) = (x ^ k * x ^ m) * x ^ n := by
   simp only [← ppow_add, add_assoc]
 
+theorem ppow_comm [PNatPowAssoc M] (m n : ℕ+) (x : M) :
+    x ^ m * x ^ n = x ^ n * x ^ m := by simp only [← ppow_add, add_comm]
+
 theorem ppow_mul [PNatPowAssoc M] (x : M) (m n : ℕ+) : x ^ (m * n) = (x ^ m) ^ n := by
   refine PNat.recOn n ?_ ?_
   rw [ppow_one, mul_one]
@@ -64,56 +75,40 @@ end PNatPowAssoc
 
 section ppowRec
 
-variable [Mul G]
+variable [Mul M]
 
-/-- A power operation. `ppowRec n a = a*(a*(⋯*a)⋯)` n times. -/
-def ppowRec : ℕ+ → G → G :=
-  let rec
-  /-- Auxiliary function because ℕ-recursion is easier. -/
-    loop : ℕ → G → G
-    | 0 => fun x ↦ x
-    | n + 1 => fun x ↦ x * loop n x
-  fun n x => loop (PNat.natPred n) x
+/-- A strictly positive integer power operation. `ppowRec n a = a*(a*(⋯*a)⋯)` n times. -/
+def ppowRec : ℕ+ → M → M :=
+  fun n => PNat.recOn n (fun x ↦ x) (fun _ y => fun x => x * y x)
 
-theorem ppowRec_loop_zero (x : G) : ppowRec.loop 0 x = x := rfl
+theorem ppowRec_one (x : M) : ppowRec 1 x = x := rfl
 
-theorem ppowRec_loop_succ (x : G) (n : ℕ) : ppowRec.loop (n + 1) x = x * ppowRec.loop n x :=
-  rfl
-
-theorem ppowRec_one (x : G) : ppowRec 1 x = x := rfl
-
-theorem ppowRec_succ (x : G) (n: ℕ+) : ppowRec (n+1) x = x * ppowRec n x := by
-  unfold ppowRec
-  rw [show (PNat.natPred (n + 1) = PNat.natPred n + 1) from (PNat.natPred_add_one n).symm]
+theorem ppowRec_succ (x : M) (n : ℕ+) : ppowRec (n + 1) x = x * ppowRec n x := by
+  refine PNat.recOn n rfl ?_
+  intro k _
   exact rfl
 
 end ppowRec
 
 section Semigroup
 
-variable [Semigroup G]
+variable [Semigroup M]
 
-instance (G: Type u) [Semigroup G] : Pow G ℕ+ :=
+instance (M: Type u) [Semigroup M] : Pow M ℕ+ :=
   {
-    pow := fun x ⟨k, h⟩ => ppowRec ⟨k, h⟩ x
+    pow := fun x n => ppowRec n x
   }
 
-theorem ppow_eq_ppowRec (x : G) (n : ℕ+) : x^n = ppowRec n x := rfl
+theorem ppow_eq_ppowRec (x : M) (n : ℕ+) : x ^ n = ppowRec n x := rfl
 
-theorem ppowRec_loop_add (x : G) (k n : ℕ) :
-    ppowRec.loop (k + n + 1) x = ppowRec.loop k x * ppowRec.loop n x := by
-  induction k with
-  | zero =>
-    rw [Nat.zero_add, ppowRec_loop_zero, ppowRec_loop_succ]
-  | succ n ih =>
-    rw [ppowRec_loop_succ, ppowRec_loop_succ, Nat.succ_add, ih, mul_assoc]
-
-theorem semigroup_ppow_add (x : G) (k n : ℕ+) : x^(k+n) = x^k * x^n := by
+theorem semigroup_ppow_add (x : M) (k n : ℕ+) : x ^ (k + n) = x ^ k * x ^ n := by
   simp only [ppow_eq_ppowRec]
-  unfold ppowRec
-  rw [← ppowRec_loop_add, PNat.natPred_add]
+  refine PNat.recOn k ?_ ?_
+  rw [add_comm, ppowRec_one, ppowRec_succ]
+  intro k hk
+  rw [ppowRec_succ, mul_assoc, ← hk, ← ppowRec_succ, add_right_comm]
 
-instance (G: Type u) [Semigroup G] : PNatPowAssoc G :=
+instance (M: Type u) [Semigroup M] : PNatPowAssoc M :=
   {
     ppow_add := by
       intro k n x
