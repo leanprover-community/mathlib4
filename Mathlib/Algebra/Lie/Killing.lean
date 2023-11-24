56bd6967579e16b9e7483c375cd66833aaf7caf4
Oliver Nash <github@olivernash.org>
2023-11-24 16:26:25 +0000

    Add informal account of key lemma

--- Mathlib/Algebra/Lie/Killing.lean
+++ Mathlib/Algebra/Lie/Killing.lean
@@ -210,6 +210,26 @@ lemma traceForm_eq_sum_weightSpaceOf [IsTriangularizable R L M] (z : L) :
 -- In characteristic zero a stronger result holds (no `⊓ LieAlgebra.center K L`) TODO prove this!
 lemma lowerCentralSeries_one_inf_center_le_ker_traceForm :
     lowerCentralSeries R L L 1 ⊓ LieAlgebra.center R L ≤ LinearMap.ker (traceForm R L M) := by
+  /- Sketch of proof (due to Zassenhaus):
+
+  Let `z ∈ lowerCentralSeries R L L 1 ⊓ LieAlgebra.center R L` and `x : L`. We must show that
+  `trace (φ x ∘ φ z) = 0` where `φ z : End R M` indicates the action of `z` on `M` (and likewise
+  for `φ x`).
+
+  Because `z` belongs to the indicated intersection, it has two key properties:
+  (a) the trace of the action of `z` vanishes on any Lie module of `L`
+      (see `LieModule.trace_toEndomorphism_eq_zero_of_mem_lcs`),
+  (b) `z` commutes with all elements of `L`.
+
+  If `φ x` were triangularizable, we could write `M` as a direct sum of generalized eigenspaces of
+  `φ x`. Because `L` is nilpotent these are all Lie submodules, thus Lie modules in their own right,
+  and thus by (a) above we learn that `trace (φ z) = 0` restricted to each generalized eigenspace.
+  Because `z` commutes with `x`, this forces `trace (φ x ∘ φ z) = 0` on each generalized eigenspace,
+  and so by summing the traces on each generalized eigenspace we learn the total trace is zero, as
+  required (see `LinearMap.trace_comp_eq_zero_of_commute_of_trace_restrict_eq_zero`).
+
+  To cater for the fact that `φ x` may not be triangularizable, we first extend the scalars from `R`
+  to `AlgebraicClosure (FractionRing R)` and argue using the action of `A ⊗ L` on `A ⊗ M`. -/
   rintro z ⟨hz : z ∈ lowerCentralSeries R L L 1, hzc : z ∈ LieAlgebra.center R L⟩
   ext x
   rw [traceForm_apply_apply, LinearMap.zero_apply]
