156506ff0fdfb2816c2d29370ca7e9b70dec7e12
dagurtomas <dagurtomas@gmail.com>
2023-11-26 00:21:23 +0000

    resolve TODO

--- Mathlib/Topology/Category/Profinite/CofilteredLimit.lean
+++ Mathlib/Topology/Category/Profinite/CofilteredLimit.lean
@@ -24,7 +24,6 @@ This file contains some theorems about cofiltered limits of profinite sets.
   of profinite sets factors through one of the components.
 -/
 
-
 namespace Profinite
 
 open scoped Classical
@@ -32,19 +31,6 @@ open scoped Classical
 open CategoryTheory
 
 open CategoryTheory.Limits
-
-/--
-Universe inequalities in Mathlib 3 are expressed through use of `max u v`. Unfortunately,
-this leads to unbound universes which cannot be solved for during unification, eg
-`max u v =?= max v ?`.
-The current solution is to wrap `Type max u v` in `TypeMax.{u,v}`
-to expose both universe parameters directly.
-
-Similarly, for other concrete categories for which we need to refer to the maximum of two universes
-(e.g. any category for which we are constructing limits), we need an analogous abbreviation.
--/
-@[nolint checkUnivs]
-abbrev ProfiniteMax.{u, v} := Profinite.{max u v}
 
 universe u v
 
