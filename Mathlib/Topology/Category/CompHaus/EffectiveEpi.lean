e0cd4866941d3f8560764584b8e9e8008e68b8f5
dagurtomas <dagurtomas@gmail.com>
2023-11-27 10:50:54 +0000

    add myself as an author to a file I just completely rewrote

--- Mathlib/Topology/Category/CompHaus/EffectiveEpi.lean
+++ Mathlib/Topology/Category/CompHaus/EffectiveEpi.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2023 Adam Topaz. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Adam Topaz
+Authors: Adam Topaz, Dagur Asgeirsson
 -/
 import Mathlib.CategoryTheory.Sites.RegularExtensive
 import Mathlib.Topology.Category.CompHaus.Limits
