3cfdddc102258403e74913eccee801e9a8e62017
Komyyy <pol_tta@outlook.jp>
2023-11-27 11:11:32 +0900

    import

--- Mathlib/Control/Fix.lean
+++ Mathlib/Control/Fix.lean
@@ -5,7 +5,6 @@ Authors: Simon Hudon
 -/
 import Mathlib.Data.Part
 import Mathlib.Data.Nat.Upto
-import Mathlib.Tactic.Common
 
 #align_import control.fix from "leanprover-community/mathlib"@"207cfac9fcd06138865b5d04f7091e46d9320432"
 
