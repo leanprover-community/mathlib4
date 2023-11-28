c28500a03a9662365b1bcac08f4c662246f63452
Joël Riou <joel.riou@universite-paris-saclay.fr>
2023-11-28 18:08:16 +0000

    feat(CategoryTheory/Sites): internal hom of (pre)sheaves (#8622)
    In this PR, we define a presheaf `presheafHom F G` when `F` and `G` are presheaves `Cᵒᵖ ⥤ A` and show that it is a sheaf when `G` is a sheaf (for a certain Grothendieck topology on `C`).
    
    
    
    Co-authored-by: Joël Riou <37772949+joelriou@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1250,6 +1250,7 @@ import Mathlib.CategoryTheory.Sites.Pretopology
 import Mathlib.CategoryTheory.Sites.Pullback
 import Mathlib.CategoryTheory.Sites.RegularExtensive
 import Mathlib.CategoryTheory.Sites.Sheaf
+import Mathlib.CategoryTheory.Sites.SheafHom
 import Mathlib.CategoryTheory.Sites.SheafOfTypes
 import Mathlib.CategoryTheory.Sites.Sheafification
 import Mathlib.CategoryTheory.Sites.Sieves
