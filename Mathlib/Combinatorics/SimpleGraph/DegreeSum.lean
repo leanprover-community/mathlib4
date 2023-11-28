bb7ec68baff76cc9c8b2a01775eb662ca2f8935f
Jeremy Tan Jie Rui <reddeloostw@gmail.com>
2023-11-28 08:18:52 +0800

    Update DegreeSum.lean

--- Mathlib/Combinatorics/SimpleGraph/DegreeSum.lean
+++ Mathlib/Combinatorics/SimpleGraph/DegreeSum.lean
@@ -51,6 +51,9 @@ section DegreeSum
 
 variable [Fintype V] [DecidableRel G.Adj]
 
+-- Porting note: Changed to `Fintype (Sym2 V)` to match Combinatorics.SimpleGraph.Basic
+variable [Fintype (Sym2 V)]
+
 theorem dart_fst_fiber [DecidableEq V] (v : V) :
     (univ.filter fun d : G.Dart => d.fst = v) = univ.image (G.dartOfNeighborSet v) := by
   ext d
