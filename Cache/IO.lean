a33ab3c7183ecde90f0dbcf9f04182185a79e13b
Mario Carneiro <di.gama@gmail.com>
2023-11-27 23:26:35 -0500

    chore: bump leantar 0.1.9

--- Cache/IO.lean
+++ Cache/IO.lean
@@ -70,7 +70,7 @@ def CURLBIN :=
 
 /-- leantar version at https://github.com/digama0/leangz -/
 def LEANTARVERSION :=
-  "0.1.8"
+  "0.1.9"
 
 def EXE := if System.Platform.isWindows then ".exe" else ""
 
