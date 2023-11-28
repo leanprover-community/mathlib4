a2db5c2be1bfddd04c72c5bafcde4f6f7fb54768
Andrew Yang <36414270+erdOne@users.noreply.github.com>
2023-11-29 05:04:03 +0800

    Update Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -2156,7 +2156,7 @@ import Mathlib.GroupTheory.Nilpotent
 import Mathlib.GroupTheory.NoncommPiCoprod
 import Mathlib.GroupTheory.NonZeroDivisors
 import Mathlib.GroupTheory.Order.Min
-import Mathlib.GroupTheory.NoncommPiCoprod
+import Mathlib.GroupTheory.OrderOfElement
 import Mathlib.GroupTheory.PGroup
 import Mathlib.GroupTheory.Perm.Basic
 import Mathlib.GroupTheory.Perm.Cycle.Basic
