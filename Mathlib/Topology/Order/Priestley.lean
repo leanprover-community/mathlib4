27503c55e07bc7875049b72ba91a06d871f05da1
Michael Rothgang <rothgami@math.hu-berlin.de>
2023-11-29 20:12:15 +0100

    Rename exists_clopen_{upper,lower}_of_not_le.

--- Mathlib/Topology/Order/Priestley.lean
+++ Mathlib/Topology/Order/Priestley.lean
@@ -47,16 +47,16 @@ section Preorder
 
 variable [Preorder α] [PriestleySpace α] {x y : α}
 
-theorem exists_clopen_upper_of_not_le :
+theorem exists_isClopen_upper_of_not_le :
     ¬x ≤ y → ∃ U : Set α, IsClopen U ∧ IsUpperSet U ∧ x ∈ U ∧ y ∉ U :=
   PriestleySpace.priestley
-#align exists_clopen_upper_of_not_le exists_clopen_upper_of_not_le
+#align exists_clopen_upper_of_not_le exists_isClopen_upper_of_not_le
 
-theorem exists_clopen_lower_of_not_le (h : ¬x ≤ y) :
+theorem exists_isClopen_lower_of_not_le (h : ¬x ≤ y) :
     ∃ U : Set α, IsClopen U ∧ IsLowerSet U ∧ x ∉ U ∧ y ∈ U :=
-  let ⟨U, hU, hU', hx, hy⟩ := exists_clopen_upper_of_not_le h
+  let ⟨U, hU, hU', hx, hy⟩ := exists_isClopen_upper_of_not_le h
   ⟨Uᶜ, hU.compl, hU'.compl, Classical.not_not.2 hx, hy⟩
-#align exists_clopen_lower_of_not_le exists_clopen_lower_of_not_le
+#align exists_clopen_lower_of_not_le exists_isClopen_lower_of_not_le
 
 end Preorder
 
@@ -67,8 +67,8 @@ variable [PartialOrder α] [PriestleySpace α] {x y : α}
 theorem exists_clopen_upper_or_lower_of_ne (h : x ≠ y) :
     ∃ U : Set α, IsClopen U ∧ (IsUpperSet U ∨ IsLowerSet U) ∧ x ∈ U ∧ y ∉ U := by
   obtain h | h := h.not_le_or_not_le
-  · exact (exists_clopen_upper_of_not_le h).imp fun _ ↦ And.imp_right <| And.imp_left Or.inl
-  · obtain ⟨U, hU, hU', hy, hx⟩ := exists_clopen_lower_of_not_le h
+  · exact (exists_isClopen_upper_of_not_le h).imp fun _ ↦ And.imp_right <| And.imp_left Or.inl
+  · obtain ⟨U, hU, hU', hy, hx⟩ := exists_isClopen_lower_of_not_le h
     exact ⟨U, hU, Or.inr hU', hx, hy⟩
 #align exists_clopen_upper_or_lower_of_ne exists_clopen_upper_or_lower_of_ne
 
