55dd357910726103a9cf10d1aa70b11ed855b4f1
adomani <adomani@gmail.com>
2023-11-29 07:35:01 +0000

    Add List and Multiset versions

--- Mathlib/Data/Finset/NatDivisors.lean
+++ Mathlib/Data/Finset/NatDivisors.lean
@@ -13,17 +13,26 @@ The divisors of a product of natural numbers are the pointwise product of the di
 factors.
 -/
 
-namespace Nat
-
 open Pointwise Finset
+
+namespace Nat
 
 lemma divisors_mul {m n : ℕ} : divisors (m * n) = divisors m * divisors n := by
   ext k
   simp_rw [mem_mul, mem_divisors, dvd_mul, mul_ne_zero_iff, ← exists_and_right]
   simp only [and_assoc, and_comm, and_left_comm]
 
-lemma Prime.divisors_sq {p : ℕ} (hp : p.Prime) : (p ^ 2).divisors = {1, p, p ^ 2} := by
-  ext
-  simp only [sq, Nat.divisors_mul, hp.divisors, mem_singleton, mem_mul, mem_insert, exists_and_left,
-    exists_eq_or_imp, mul_one, exists_eq_left, one_mul, ne_eq]
-  aesop
+lemma Prime.divisors_sq {p : ℕ} (hp : p.Prime) : (p ^ 2).divisors = {p ^ 2, p, 1} := by
+  simp [divisors_prime_pow hp, range_succ]
+
+end Nat
+
+open Nat
+
+lemma List.nat_divisors_prod (l : List ℕ) : divisors l.prod = (l.map divisors).prod := by
+  induction l with
+  | nil => simp; rfl
+  | cons head tail hl => simp [divisors_mul, hl]
+
+lemma Multiset.divisors_prod (s : Multiset ℕ) : divisors s.prod = (s.map divisors).prod := by
+  apply s.induction (by simp; rfl) fun  a s hs ↦ by simp [hs, divisors_mul]
