7b2cfb35e6b7f04bc0552eec88d07befb2c6c38b
madvorak <martin.dvorak@matfyz.cz>
2023-11-30 12:55:02 +0100

    docstrings

--- Mathlib/Util/ExtraDelaborators.lean
+++ Mathlib/Util/ExtraDelaborators.lean
@@ -17,31 +17,39 @@ This file makes infoview display:
 
 -/
 
+/-- Given `n : ℕ` and `l : List α`, print `List.take n l` as `l.take n` in Infoview. -/
 @[app_unexpander List.take]
 def List.take.unexpander : Lean.PrettyPrinter.Unexpander
   | `($_ $n $l) => `($(l).$(Lean.mkIdent `take) $n)
   | _ => throw ()
 
+/-- Given `n : ℕ` and `l : List α`, print `List.drop n l` as `l.drop n` in Infoview. -/
 @[app_unexpander List.drop]
 def List.drop.unexpander : Lean.PrettyPrinter.Unexpander
   | `($_ $n $l) => `($(l).$(Lean.mkIdent `drop) $n)
   | _ => throw ()
 
+/-- Given `p : α → Bool` and `l : List α`, print `List.takeWhile p l` as `l.takeWhile p` in
+Infoview. -/
 @[app_unexpander List.takeWhile]
 def List.takeWhile.unexpander : Lean.PrettyPrinter.Unexpander
   | `($_ $p $l) => `($(l).$(Lean.mkIdent `takeWhile) $p)
   | _ => throw ()
 
+/-- Given `p : α → Bool` and `l : List α`, print `List.dropWhile p l` as `l.dropWhile p` in
+Infoview. -/
 @[app_unexpander List.dropWhile]
 def List.dropWhile.unexpander : Lean.PrettyPrinter.Unexpander
   | `($_ $p $l) => `($(l).$(Lean.mkIdent `dropWhile) $p)
   | _ => throw ()
 
+/-- Given `f : α → β` and `l : List α`, print `List.map f l` as `l.map f` in Infoview. -/
 @[app_unexpander List.map]
 def List.map.unexpander : Lean.PrettyPrinter.Unexpander
   | `($_ $f $l) => `($(l).$(Lean.mkIdent `map) $f)
   | _ => throw ()
 
+/-- Given `f : α → β` and `s : Multiset α`, print `Multiset.map f s` as `s.map f` in Infoview. -/
 @[app_unexpander Multiset.map]
 def Multiset.map.unexpander : Lean.PrettyPrinter.Unexpander
   | `($_ $f $s) => `($(s).$(Lean.mkIdent `map) $f)
