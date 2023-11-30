77bf861e67c815fafb45d52b32f75c59b1cddab3
Mario Carneiro <di.gama@gmail.com>
2023-11-30 05:42:21 -0500

    fix: improve `recall` impl / error reporting

--- Mathlib/Tactic/Recall.lean
+++ Mathlib/Tactic/Recall.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2023 Mac Malone. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Mac Malone, Kyle Miller
+Authors: Mac Malone, Kyle Miller, Mario Carneiro
 -/
 import Mathlib.Init
 import Lean.Elab.Command
@@ -39,43 +39,31 @@ open Lean Meta Elab Command Term
 
 elab_rules : command
   | `(recall $id $sig:optDeclSig $[$val?]?) => withoutModifyingEnv do
-    let declName := id.getId
-    let some info := (← getEnv).find? declName
-      | throwError "unknown constant '{declName}'"
-    let declConst : Expr := mkConst declName <| info.levelParams.map Level.param
-    discard <| liftTermElabM <| addTermInfo id declConst
-    let newId := mkIdentFrom id (← mkAuxName declName 1)
-    if let some val := val? then
-      let some infoVal := info.value?
-        | throwErrorAt val "constant '{declName}' has no defined value"
-      elabCommand <| ← `(noncomputable def $newId $sig:optDeclSig $val)
-      let some newInfo := (← getEnv).find? newId.getId | return -- def already threw
-      liftTermElabM do
-        let mvs ← newInfo.levelParams.mapM fun _ => mkFreshLevelMVar
-        let newType := newInfo.type.instantiateLevelParams newInfo.levelParams mvs
-        unless (← isDefEq info.type newType) do
-          throwTypeMismatchError none info.type newInfo.type declConst
-        let newVal := newInfo.value?.get!.instantiateLevelParams newInfo.levelParams mvs
-        unless (← isDefEq infoVal newVal) do
-          let err := m!"\
-            value mismatch{indentExpr declConst}\nhas value{indentExpr newVal}\n\
-            but is expected to have value{indentExpr infoVal}"
-          throwErrorAt val err
-    else
-      let (binders, type?) := expandOptDeclSig sig
-      if let some type := type? then
-        runTermElabM fun vars => do
-          withAutoBoundImplicit do
-            elabBinders binders.getArgs fun xs => do
-              let xs ← addAutoBoundImplicits xs none
-              let type ← elabType type
-              Term.synthesizeSyntheticMVarsNoPostponing
-              let type ← mkForallFVars xs type
-              let type ← mkForallFVars vars type (usedOnly := true)
-              unless (← isDefEq info.type type) do
-                throwTypeMismatchError none info.type type declConst
-      else
-        unless binders.getNumArgs == 0 do
-          throwError "expected type after ':'"
+    let declName ← liftCoreM <| realizeGlobalConstNoOverloadWithInfo id
+    let info ← getConstInfo declName
+    let declConst : Expr := mkConst declName <| info.levelParams.map .param
+    let newId ← liftCoreM <| mkFreshUserName `_recall
+    let val := val?.getD (← `(Parser.Command.declVal| := lcUnreachable))
+    elabCommand <| ← `(noncomputable unsafe def $(mkIdent newId) $sig:optDeclSig $val)
+    if val?.isNone && sig.raw[1].isNone then return
+    let valRef := match val? with
+      | some val => if let `(Parser.Command.declVal| := $e) := val then e.raw else val
+      | none => id
+    let newInfo ← getConstInfoDefn ((← getCurrNamespace) ++ newId)
+    liftTermElabM do
+    let mvs ← newInfo.levelParams.mapM fun _ => mkFreshLevelMVar
+    let newType := newInfo.type.instantiateLevelParams newInfo.levelParams mvs
+    unless (← isDefEq info.type newType) do
+      withRef (if sig.raw[1].isNone then valRef else sig.raw[1][0][1]) do
+        throwTypeMismatchError none info.type newInfo.type declConst
+    if val?.isNone then return
+    let some infoVal := info.value?
+      | throwErrorAt valRef "constant '{declName}' has no defined value"
+    let newVal := newInfo.value.instantiateLevelParams newInfo.levelParams mvs
+    unless (← isDefEq infoVal newVal) do
+      let err := m!"\
+        value mismatch{indentExpr declConst}\nhas value{indentExpr newVal}\n\
+        but is expected to have value{indentExpr infoVal}"
+      throwErrorAt valRef err
 
 end Mathlib.Tactic.Recall
