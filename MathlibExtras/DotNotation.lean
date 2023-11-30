4c00e0f76c37c31e8833f4c28bedcaeaaf9a81c0
madvorak <martin.dvorak@matfyz.cz>
2023-11-30 11:15:09 +0100

    improvement

--- MathlibExtras/DotNotation.lean
+++ MathlibExtras/DotNotation.lean
@@ -7,32 +7,36 @@ import Mathlib.Algebra.BigOperators.Multiset.Basic
 
 @[app_unexpander List.take]
 def List.take.unexpander : Lean.PrettyPrinter.Unexpander
-  | `($_ $n $xs) => `($(xs).$(Lean.mkIdent `take) $n)
+  | `($_ $n $l) => `($(l).$(Lean.mkIdent `take) $n)
   | _ => throw ()
 
 @[app_unexpander List.drop]
 def List.drop.unexpander : Lean.PrettyPrinter.Unexpander
-  | `($_ $f $μ) => `($(μ).$(Lean.mkIdent `drop) $f)
+  | `($_ $n $l) => `($(l).$(Lean.mkIdent `drop) $n)
   | _ => throw ()
 
 @[app_unexpander List.takeWhile]
 def List.takeWhile.unexpander : Lean.PrettyPrinter.Unexpander
-  | `($_ $f $μ) => `($(μ).$(Lean.mkIdent `takeWhile) $f)
+  | `($_ $p $l) => `($(l).$(Lean.mkIdent `takeWhile) $p)
   | _ => throw ()
 
 @[app_unexpander List.dropWhile]
 def List.dropWhile.unexpander : Lean.PrettyPrinter.Unexpander
-  | `($_ $f $μ) => `($(μ).$(Lean.mkIdent `dropWhile) $f)
+  | `($_ $p $l) => `($(l).$(Lean.mkIdent `dropWhile) $p)
   | _ => throw ()
 
 @[app_unexpander List.map]
 def List.map.unexpander : Lean.PrettyPrinter.Unexpander
-  | `($_ $f $μ) => `($(μ).$(Lean.mkIdent `map) $f)
+  | `($_ $f $l) => `($(l).$(Lean.mkIdent `map) $f)
   | _ => throw ()
 
 @[app_unexpander Multiset.map]
 def Multiset.map.unexpander : Lean.PrettyPrinter.Unexpander
-  | `($_ $f $μ) => `($(μ).$(Lean.mkIdent `map) $f)
+  | `($_ $f $s) => `($(s).$(Lean.mkIdent `map) $f)
   | _ => throw ()
 
-attribute [pp_dot] List.get List.sum Multiset.sum Sigma.fst Sigma.snd
+attribute [pp_dot] List.get List.length List.sum Multiset.sum Sigma.fst Sigma.snd
+
+example (l : List ℕ) : l.take l.length = l.takeWhile (fun _ => true) := sorry
+example (l : List ℕ) : l.drop 0 = l.dropWhile (fun _ => false) := sorry
+example (l : List ℕ) : (Multiset.ofList l).map Nat.succ = Multiset.ofList (l.map Nat.succ) := sorry
