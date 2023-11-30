4ff17db0bf9f0c23fa615a501dad730ca2fb3a69
grunweg <rothgami@math.hu-berlin.de>
2023-11-30 19:19:07 +0000

    chore: split Geometry/Manifold/ContMDiff (#8726)
    At about 2200 lines, this is currently the longest file in the Geometry/Manifolds.
    
    (It also moves the slowly compiling proof of `ContMDiffWithinAt.cle_arrowCongr` out of a common recompilation path.)

--- Mathlib/Geometry/Manifold/VectorBundle/Basic.lean
+++ Mathlib/Geometry/Manifold/VectorBundle/Basic.lean
@@ -3,6 +3,7 @@ Copyright (c) 2022 Floris van Doorn, Heather Macbeth. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Floris van Doorn, Heather Macbeth
 -/
+import Mathlib.Geometry.Manifold.ContMDiff.Atlas
 import Mathlib.Geometry.Manifold.VectorBundle.FiberwiseLinear
 import Mathlib.Topology.VectorBundle.Constructions
 
