a3f0a2ee2f617e2357f5c962b37cc69423e21bd0
Floris van Doorn <fpvdoorn@gmail.com>
2023-11-30 18:29:28 +0100

    fix

--- Mathlib/Algebra/CharZero/Lemmas.lean
+++ Mathlib/Algebra/CharZero/Lemmas.lean
@@ -217,7 +217,7 @@ variable {R : Type*} [Ring R] [CharZero R]
 @[simp]
 theorem units_ne_neg_self (u : Rˣ) : u ≠ -u := by
   simp_rw [ne_eq, Units.ext_iff, Units.val_neg, eq_neg_iff_add_eq_zero, ← two_mul,
-    Units.mul_left_eq_zero, two_ne_zero]
+    Units.mul_left_eq_zero, two_ne_zero, not_false_iff]
 
 @[simp]
 theorem neg_units_ne_self (u : Rˣ) : -u ≠ u := (units_ne_neg_self u).symm
