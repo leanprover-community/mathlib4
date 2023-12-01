7fc584b478beac8e0e013d25146b5ed92943641e
Alex Keizer <alex@keizer.dev>
2023-12-01 21:07:17 +0000

    docs

--- Mathlib/Data/BitVec/Equiv.lean
+++ Mathlib/Data/BitVec/Equiv.lean
@@ -1,5 +1,14 @@
+/-
+Copyright (c) 2023 Alex Keizer. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Alex Keizer
+-/
 import Mathlib.Data.BitVec.Lemmas
 import Mathlib.Algebra.BigOperators.Fin
+
+/-!
+This file shows various equivalences of bitvectors.
+-/
 
 namespace Std.BitVec
 
