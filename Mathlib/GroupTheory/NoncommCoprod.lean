6421439630229d63023b0e6e4c39c3d4fe65f7eb
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2023-12-01 18:58:30 +0100

    add docstring and correct typo

--- Mathlib/GroupTheory/NoncommCoprod.lean
+++ Mathlib/GroupTheory/NoncommCoprod.lean
@@ -3,15 +3,32 @@ Copyright (c) 2023 Antoine Chambert-Loir. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Antoine Chambert-Loir
 -/
+
 import Mathlib.Algebra.Group.Prod
 import Mathlib.Algebra.Hom.Commute
+
+open Tactic
 
 /-!
 # Canonical homomorphism from a pair of monoids
 
 This file defines the construction of the canonical homomorphism from a pair of monoids.
 
-TODO: write the rest of this docstrnig copying (and/or referenceing) `NoncommPiCoprod`
+Given two morphisms of monoids `f : M →* P` and `g : N →* P` where elements in the images
+of the two morphisms commute, we obtain a canonical morphism
+`MonoidHom.noncommCoprod : M × N →* P` whose composition with `inl M N` coincides with `f`
+and whose composition with `inr M N` coincides with `g`.
+
+There is an analogue `MulHom.noncommCoprod` when `f` and `g` are only `MulHom`.
+
+Main theorems :
+
+* `noncommCoprod_comp_inr` and `noncommCoprod_comp_inl` prove that the compositions of `MonoidHom.noncommCoprod f g _` with `inl M N` and `inr M N` coincide with `f` and `g``
+
+* `comp_noncommCoprod` proves that the composition of a morphism of monoids `h` with `noncommCoprod f g _` coincides with `noncommCoprod (h.comp f) (h.comp g)`.
+
+For a product of a family of morphisms of monoids, see `MonoidHom.noncommPiCoprod`.
+
 -/
 
 namespace MulHom
@@ -35,7 +52,6 @@ theorem comp_noncommCoprod {Q : Type*} [Semigroup Q] (h : P →ₙ* Q) :
     h.comp (f.noncommCoprod g comm) =
       (h.comp f).noncommCoprod (h.comp g) (fun m n ↦ (comm m n).map h) :=
   ext fun _ => map_mul h _ _
-
 
 end MulHom
 
@@ -68,13 +84,13 @@ theorem noncommCoprod_comp_inr : (f.noncommCoprod g comm).comp (inr M N) = g :=
 
 @[to_additive (attr := simp)]
 theorem noncommCoprod_unique (f : M × N →* P) :
-    (f.comp (inl M N)).noncommCoprod (f.comp (inr M N)) (fun _ _ => (inl_inr_commute _ _).map f)
+    (f.comp (inl M N)).noncommCoprod (f.comp (inr M N)) (fun _ _ => (commute_inl_inr _ _).map f)
       = f :=
   ext fun x => by simp [coprod_apply, inl_apply, inr_apply, ← map_mul]
 
 @[to_additive (attr := simp)]
 theorem noncommCoprod_inl_inr {M N : Type*} [Monoid M] [Monoid N]:
-    (inl M N).noncommCoprod (inr M N) (inl_inr_commute (M := M) (N := N)) = id (M × N) :=
+    (inl M N).noncommCoprod (inr M N) commute_inl_inr = id (M × N) :=
   noncommCoprod_unique <| .id (M × N)
 
 @[to_additive]
