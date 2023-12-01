d6c65c487a8bb02a0014b4292d27fa17c3222bb9
Yaël Dillies <yael.dillies@gmail.com>
2023-12-01 07:48:40 +0000

    comment out `exact?` test

--- test/LibrarySearch/basic.lean
+++ test/LibrarySearch/basic.lean
@@ -223,9 +223,11 @@ lemma prime_of_prime (n : ℕ) : Prime n ↔ Nat.Prime n := by
 lemma ex' (x : ℕ) (_h₁ : x = 0) (h : 2 * 2 ∣ x) : 2 ∣ x := by
   exact? says exact dvd_of_mul_left_dvd h
 
+-- This example non-deterministically picks between `le_antisymm hxy hyx` and `ge_antisymm hxy hyx`.
+-- Commenting out for now.
 -- Example from https://leanprover.zulipchat.com/#narrow/stream/287929-mathlib4/topic/Exact.3F.20fails.20on.20le_antisymm/near/388993167
-example {x y : ℝ} (hxy : x ≤ y) (hyx : y ≤ x) : x = y := by
-  exact? says exact le_antisymm hxy hyx
+-- example {x y : ℝ} (hxy : x ≤ y) (hyx : y ≤ x) : x = y := by
+--   exact? says exact le_antisymm hxy hyx
 
 -- https://leanprover.zulipchat.com/#narrow/stream/287929-mathlib4/topic/apply.3F.20failure/near/402534407
 example (P Q : Prop) (h : P → Q) (h' : ¬Q) : ¬P := by
