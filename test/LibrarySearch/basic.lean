9ed4b0c2ee1b808583ae7ad9de74337eaf05dd0f
Eric Wieser <wieser.eric@gmail.com>
2023-12-01 20:34:55 +0000

    tidy

--- test/LibrarySearch/basic.lean
+++ test/LibrarySearch/basic.lean
@@ -223,10 +223,10 @@ lemma prime_of_prime (n : ℕ) : Prime n ↔ Nat.Prime n := by
 lemma ex' (x : ℕ) (_h₁ : x = 0) (h : 2 * 2 ∣ x) : 2 ∣ x := by
   exact? says exact dvd_of_mul_left_dvd h
 
--- This example non-deterministically picks between `le_antisymm hxy hyx` and `ge_antisymm hyx hxy`.
 -- Example from https://leanprover.zulipchat.com/#narrow/stream/287929-mathlib4/topic/Exact.3F.20fails.20on.20le_antisymm/near/388993167
 set_option linter.unreachableTactic false in
 example {x y : ℝ} (hxy : x ≤ y) (hyx : y ≤ x) : x = y := by
+  -- This example non-deterministically picks between `le_antisymm hxy hyx` and `ge_antisymm hyx hxy`.
   first
   | exact? says exact le_antisymm hxy hyx
   | exact? says exact ge_antisymm hyx hxy
