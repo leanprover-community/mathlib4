9df2cbf62c359b33807151422ca975894413d876
Scott Morrison <scott@tqft.net>
2023-12-02 12:22:52 +0000

    feat: add slim_check generators for Rat (#8759)
    Co-authored-by: Scott Morrison <scott.morrison@gmail.com>

--- Mathlib/Testing/SlimCheck/Sampleable.lean
+++ Mathlib/Testing/SlimCheck/Sampleable.lean
@@ -160,6 +160,10 @@ instance Fin.shrinkable {n : Nat} : Shrinkable (Fin n.succ) where
 instance Int.shrinkable : Shrinkable Int where
   shrink n := Nat.shrink n.natAbs |>.map (λ x => ([x, -x] : List ℤ)) |>.join
 
+instance Rat.shrinkable : Shrinkable Rat where
+  shrink r :=
+    (Shrinkable.shrink r.num).bind fun d => Nat.shrink r.den |>.map fun n => Rat.divInt d n
+
 instance Bool.shrinkable : Shrinkable Bool := {}
 instance Char.shrinkable : Shrinkable Char := {}
 
@@ -203,6 +207,13 @@ instance Int.sampleableExt : SampleableExt Int :=
   mkSelfContained (do
     choose Int (-(←getSize)) (←getSize)
       (le_trans (Int.neg_nonpos_of_nonneg (Int.ofNat_zero_le _)) (Int.ofNat_zero_le _)))
+
+instance Rat.sampleableExt : SampleableExt Rat :=
+  mkSelfContained (do
+    let d ← choose Int (-(←getSize)) (←getSize)
+      (le_trans (Int.neg_nonpos_of_nonneg (Int.ofNat_zero_le _)) (Int.ofNat_zero_le _))
+    let n ← choose Nat 0 (←getSize) (Nat.zero_le _)
+    return Rat.divInt d n)
 
 instance Bool.sampleableExt : SampleableExt Bool :=
   mkSelfContained $ chooseAny Bool
