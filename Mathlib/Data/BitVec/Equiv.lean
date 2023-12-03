1060776e9017354dead1c5d8d0325404e7904130
Alex Keizer <alex@keizer.dev>
2023-12-03 14:45:27 +0000

    prove that `getLsb'` is an isomorphism

--- Mathlib/Data/BitVec/Equiv.lean
+++ Mathlib/Data/BitVec/Equiv.lean
@@ -5,10 +5,38 @@ Authors: Alex Keizer
 -/
 import Mathlib.Data.BitVec.Lemmas
 import Mathlib.Algebra.BigOperators.Fin
+import Mathlib.Tactic.Ring
 
 /-!
 This file shows various equivalences of bitvectors.
 -/
+
+namespace Nat
+
+theorem bit_add_bit (x₀ y₀ : Bool) (x y : Nat) :
+    bit x₀ x + bit y₀ y = bit (Bool.xor x₀ y₀) (x + y + (x₀ && y₀).toNat) := by
+  simp only [bit_val]
+  cases x₀
+  <;> cases y₀
+  <;> simp only [
+        cond_true, cond_false, Bool.toNat_true, Bool.toNat_false, add_zero,
+        Bool.and_false, Bool.false_and, Bool.and_self,
+        Bool.xor_false, Bool.xor_true, Bool.xor_self, Bool.not_false]
+  <;> ring_nf
+
+/-- If two numbers have no bits in common (i.e., `x &&& y = 0`),
+then addition is the same as bitwise disjunction -/
+theorem add_eq_lor_of_and_eq_zero {x y : Nat} (h : x &&& y = 0) : x + y = x ||| y := by
+  induction' x using Nat.binaryRec with x₀ x ih generalizing y
+  · simp only [zero_add, or_zero]
+  · cases' y using Nat.binaryRec with y₀ y
+    · simp only [add_zero, zero_or]
+    · obtain ⟨h₁, (h₂ : x₀ = false ∨ y₀ = false)⟩ := by simpa using h
+      have hand : (x₀ && y₀) = false := by rcases h₂ with rfl|rfl <;> simp
+      simp [bit_add_bit, hand, ih h₁]
+      cases x₀ <;> cases y₀ <;> simp_all
+
+end Nat
 
 namespace Std.BitVec
 
@@ -24,5 +52,72 @@ def finFunctionEquiv : BitVec w ≃ (Fin w → Bool) := calc
   BitVec w  ≃ (Fin (2 ^ w))     := finEquiv
   _         ≃ (Fin w -> Fin 2)  := finFunctionFinEquiv.symm
   _         ≃ (Fin w -> Bool)   := Equiv.arrowCongr (.refl _) finTwoEquiv
+
+-- TODO: I think this lemma exists in some PR already
+@[simp] lemma val_toFin (x : BitVec w) : x.toFin.val = x.toNat := rfl
+
+/-- Create a bitvector from a function that maps index `i` to the `i`-th least significant bit -/
+def ofLEFn {w} (f : Fin w → Bool) : BitVec w :=
+  match w with
+  | 0   => .nil
+  | w+1 => .concat (ofLEFn <| Fin.tail f) (f ⟨0, Nat.succ_pos w⟩)
+
+@[simp] lemma ofLEFn_zero (f : Fin 0 → Bool) : ofLEFn f = nil := rfl
+
+@[simp] lemma ofLEFn_cons {w} (b : Bool) (f : Fin w → Bool) :
+    ofLEFn (Fin.cons b f) = concat (ofLEFn f) b :=
+  rfl
+
+@[simp]
+theorem coe_finFunctionEquiv_eq_getLsb' :
+    (finFunctionEquiv : BitVec w → Fin w → Bool) = getLsb' := by
+  funext x i
+  simp only [finFunctionEquiv, finEquiv, finFunctionFinEquiv, ← Nat.shiftRight_eq_div_pow,
+    Equiv.instTransSortSortSortEquivEquivEquiv_trans, finTwoEquiv, Matrix.vecCons, Matrix.vecEmpty,
+    Equiv.trans_apply, Equiv.coe_fn_mk, Equiv.ofRightInverseOfCardLE_symm_apply, val_toFin,
+    Equiv.arrowCongr_apply, Equiv.refl_symm, Equiv.coe_refl, Function.comp.right_id,
+    Function.comp_apply, getLsb', getLsb, Nat.testBit, Nat.and_one_is_mod]
+  cases (x.toNat >>> i.val).mod_two_eq_zero_or_one
+  next h => simp only [h, Fin.zero_eta, Fin.cons_zero, bne_self_eq_false]
+  next h => simp only [h, Fin.mk_one, Fin.cons_one, Fin.cons_zero]; rfl
+
+private theorem Bool.val_rec_eq_toNat (b : Bool) :
+    (Fin.val (n:=2) <| Bool.rec 0 1 b) = b.toNat := by
+  cases b <;> rfl
+
+set_option linter.deprecated false in
+private theorem Nat.mul_two_eq_bit (x : ℕ) :
+    x * 2 = Nat.bit false x := by
+  simp only [mul_two, Nat.bit_false, bit0]
+
+theorem Bool.toNat_eq_bit_zero (b : Bool) : b.toNat = Nat.bit b 0 := by
+  cases b <;> rfl
+
+theorem coe_symm_finFunctionEquiv_eq_ofLEFn :
+    (finFunctionEquiv.symm : (Fin w → Bool) → BitVec w) = ofLEFn := by
+  funext f
+  induction' f using Fin.consInduction with w x₀ f ih
+  · rw [ofLEFn_zero]; rfl
+  · simp only [finFunctionEquiv, finEquiv, finFunctionFinEquiv, Fin.univ_succ,
+      Finset.cons_eq_insert, Finset.mem_map, Finset.mem_univ, Function.Embedding.coeFn_mk, true_and,
+      Fin.exists_succ_eq_iff, ne_eq, not_true_eq_false, not_false_eq_true, Finset.sum_insert,
+      Fin.val_zero, pow_zero, mul_one, Finset.sum_map, Fin.val_succ, pow_succ,
+      Equiv.instTransSortSortSortEquivEquivEquiv_trans, finTwoEquiv, Equiv.symm_trans_apply,
+      Equiv.arrowCongr_symm, Equiv.refl_symm, Equiv.symm_symm, Equiv.ofRightInverseOfCardLE_apply,
+      Equiv.arrowCongr_apply, Equiv.coe_fn_symm_mk, Equiv.coe_refl, Function.comp.right_id,
+      Function.comp_apply, Fin.cons_zero, Bool.val_rec_eq_toNat, Fin.cons_succ,
+      Nat.add_comm x₀.toNat, ofLEFn_cons, concat, HAppend.hAppend, append, HOr.hOr, OrOp.or,
+      BitVec.or, shiftLeftZeroExtend, ← ih, toNat_ofFin, Nat.shiftLeft_eq_mul_pow,
+      zeroExtend', toNat_ofBool, ofFin.injEq, Fin.mk.injEq]
+    rw [Nat.add_eq_lor_of_and_eq_zero ?_, Finset.sum_mul]
+    · congr! 2; ring
+    · have (i) : (f i).toNat * (2 * 2 ^ i.val) = (f i).toNat * 2 ^ i.val * 2 := by ring
+      simp only [this, ← Finset.sum_mul]
+      simp only [Bool.toNat_eq_bit_zero, Nat.mul_two_eq_bit, Nat.land_bit, Bool.false_and,
+        Nat.and_zero, Nat.bit_eq_zero, and_self]
+
+
+
+
 
 end Std.BitVec
