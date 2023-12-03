8e423d28f85615d5d18b4cba6791db12096faa49
dagurtomas <dagurtomas@gmail.com>
2023-12-03 21:18:01 +0000

    .

--- Mathlib/CategoryTheory/Sites/HasSheafify.lean
+++ Mathlib/CategoryTheory/Sites/HasSheafify.lean
@@ -10,20 +10,20 @@ variable (A : Type u') [Category.{v'} A]
 class HasSheafify : Prop where
   isRightAdjoint : Nonempty <| IsRightAdjoint (sheafToPresheaf J A)
 
-noncomputable
+noncomputable section
+
 instance [HasSheafify J A] : IsRightAdjoint (sheafToPresheaf J A) := HasSheafify.isRightAdjoint.some
 
-noncomputable
 def presheafToSheaf [HasSheafify J A] : (Cᵒᵖ ⥤ A) ⥤ Sheaf J A :=
   leftAdjoint (sheafToPresheaf J A)
 
-noncomputable
 def sheafificationAdjunction [HasSheafify J A] : presheafToSheaf J A ⊣ sheafToPresheaf J A :=
   HasSheafify.isRightAdjoint.some.adj
 
-noncomputable
 instance [HasSheafify J A] : IsLeftAdjoint <| presheafToSheaf J A where
   right := sheafToPresheaf J A
   adj := sheafificationAdjunction J A
+
+end
 
 end CategoryTheory
