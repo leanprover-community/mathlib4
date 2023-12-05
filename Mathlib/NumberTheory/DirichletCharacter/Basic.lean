d8ebbc79bc398c13b184046003c241cb0e796d65
Moritz Firsching <firsching@google.com>
2023-12-05 11:19:45 +0100

    review comments

--- Mathlib/NumberTheory/DirichletCharacter/Basic.lean
+++ Mathlib/NumberTheory/DirichletCharacter/Basic.lean
@@ -224,19 +224,25 @@ lemma primitiveCharacter_one (hn : n ≠ 0) :
       (isPrimitive_def _).1 (1 : DirichletCharacter R n).primitiveCharacter_isPrimitive,
       conductor_one hn]
 
-/-- Primitive character associated to multiplication of Dirichlet characters,
+/-- Dirichlet character associated to multiplication of Dirichlet characters,
 after changing both levels to the same -/
 noncomputable def mul {m : ℕ} (χ₁ : DirichletCharacter R n) (χ₂ : DirichletCharacter R m) :
     DirichletCharacter R
-      (changeLevel (Nat.dvd_lcm_left n m) χ₁ * changeLevel (Nat.dvd_lcm_right n m) χ₂).conductor :=
-  primitiveCharacter _
+      (Nat.lcm n m) :=
+  changeLevel (Nat.dvd_lcm_left n m) χ₁ * changeLevel (Nat.dvd_lcm_right n m) χ₂
+
+/-- Primitive character associated to multiplication of Dirichlet characters,
+after changing both levels to the same -/
+noncomputable def primitive_mul {m : ℕ} (χ₁ : DirichletCharacter R n) (χ₂ : DirichletCharacter R m) :
+    DirichletCharacter R (mul χ₁ χ₂).conductor :=
+  primitiveCharacter (mul χ₁ χ₂)
 
 lemma mul_def {n m : ℕ} {χ : DirichletCharacter R n} {ψ : DirichletCharacter R m} :
-    χ.mul ψ = primitiveCharacter _ :=
+    χ.primitive_mul ψ = primitiveCharacter _ :=
   rfl
 
 namespace isPrimitive
-lemma mul {m : ℕ} (ψ : DirichletCharacter R m) : (mul χ ψ).isPrimitive :=
+lemma primitive_mul {m : ℕ} (ψ : DirichletCharacter R m) : (primitive_mul χ ψ).isPrimitive :=
   primitiveCharacter_isPrimitive _
 end isPrimitive
 
