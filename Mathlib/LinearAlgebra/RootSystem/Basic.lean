f4e55c0aa74fc57f71d8096a6dbb6f8bec676f7f
Oliver Nash <github@olivernash.org>
2023-12-07 20:07:21 +0000

    feat: define root data / systems

---
+++ Mathlib/LinearAlgebra/RootSystem/Basic.lean
@@ -0,0 +1,324 @@
+/-
+Copyright (c) 2023 Oliver Nash. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Oliver Nash, Deepro Choudhury
+-/
+import Mathlib.LinearAlgebra.PerfectPairing
+import Mathlib.LinearAlgebra.RootSystem.Symmetries
+
+/-!
+# Root data and root systems
+
+This file contains basic definitions for root systems and root data.
+
+## Main definitions / results:
+ * `RootPairing`: Given two perfectly-paired `R`-modules `M` and `N`, a root pairing with finite
+   indexing set `ι` is the data of an `ι`-indexed subset of `M` ("the roots") and an `ι`-indexed
+   subset of `N` ("the coroots") satisfying the axioms familiar from the traditional theory of root
+   systems / data.
+ * `RootDatum`: A root datum is a root pairing for which the roots and coroots take values in
+   finitely-generated free Abelian groups.
+ * `RootSystem`: A root system is a root pairing for which the roots span their ambient module and
+   the perfect pairing is the canonical one between `M` and `Dual R M`.
+ * `RootPairing.IsCrystallographic`: A root pairing is said to be crystallographic if the pairing
+   between a root and coroot is always an integer.
+ * `RootPairing.IsReduced`: A root pairing is said to be reduced if it never contains the double of
+   a root.
+ * `RootPairing.ext`: In characteristic zero if there is no torsion, the correspondence between
+   roots and coroots is unique.
+ * `RootSystem.ext`: In characteristic zero if there is no torsion, a root system is determined
+   entirely by its roots.
+ * `RootSystem.mk'`: In characteristic zero if there is no torsion, to check that a collection of
+   roots form a root system, we do not need to check that the coroots are stable under reflections
+   since this follows from the corresponding property for the roots.
+
+## Implementation details
+
+TODO write, as this was fairly tricky to get into good state, potential points to mention:
+ * Concept of `RootPairing`
+ * Reason for `ι`-indexing to solve DTT hell
+ * Various extensionality issues
+ * Data-bearing or `Prop`-valued
+ * `⊆ roots` or `= roots`
+ * Roots / coroots as subsets or injections
+ * Junk-value pattern for bijection to avoid DTT hell
+ * etc.
+
+-/
+
+open Set Function Module
+open Submodule (span)
+open AddSubgroup (zmultiples)
+
+noncomputable section
+
+variable (ι : Type*) [Finite ι] {R M N : Type*}
+  [CommRing R] [AddCommGroup M] [Module R M] [AddCommGroup N] [Module R N]
+  [IsReflexive R M] (p : N ≃ₗ[R] Dual R M)
+
+/-- Given two perfectly-paired `R`-modules `M` and `N`, a root pairing with finite indexing set `ι`
+is the data of an `ι`-indexed subset of `M` ("the roots") and an `ι`-indexed subset of `N`
+("the coroots") satisfying the axioms familiar from the traditional theory of root systems / data.
+
+It exists to allow for a convenient unification of the theories of root systems and root data. -/
+structure RootPairing :=
+  root : ι ↪ M
+  coroot : ι ↪ N
+  coroot_root_two : ∀ i, p (coroot i) (root i) = 2
+  symmetry_image_root_eq : ∀ i, toPreSymmetry (root i) (p (coroot i)) '' (range root) ⊆ range root
+  symmetry_image_coroot_eq : ∀ i,
+    toPreSymmetry (coroot i) (p.flip (root i)) '' (range coroot) ⊆ range coroot
+
+attribute [nolint docBlame] RootPairing.root
+attribute [nolint docBlame] RootPairing.coroot
+
+/-- A root datum is a root pairing for which the roots and coroots take values in finitely-generated
+free Abelian groups. -/
+abbrev RootDatum {X₁ X₂ : Type*}
+    [AddCommGroup X₁] [Free ℤ X₁] [Finite ℤ X₁]
+    [AddCommGroup X₂]
+    (p : X₂ ≃+ Dual ℤ X₁) :=
+  RootPairing ι p.toIntLinearEquiv
+
+variable (R M) in
+/-- A root system is a root pairing for which the roots span their ambient module and the perfect
+pairing is the canonical one between `M` and `Dual R M`. -/
+structure RootSystem extends RootPairing ι (LinearEquiv.refl R (Dual R M)) :=
+  span_eq_top : span R (range root) = ⊤
+
+namespace RootPairing
+
+variable {p ι}
+variable (rp : RootPairing ι p) (i : ι)
+
+/-- A root pairing is said to be crystallographic if the pairing between a root and coroot is
+always an integer.-/
+def IsCrystallographic : Prop :=
+  ∀ i, p (rp.coroot i) '' (range rp.root) ⊆ (zmultiples (1 : R) : Set R)
+
+-- TODO: automatically crystallographic when `R` is `ℤ`
+
+/-- A root pairing is said to be reduced if it never contains the double of a root.-/
+def IsReduced : Prop :=
+  ∀ i, 2 • rp.root i ∉ range rp.root
+
+lemma ne_zero [CharZero R] : (rp.root i : M) ≠ 0 :=
+  fun h ↦ by simpa [h] using rp.coroot_root_two i
+
+lemma ne_zero' [CharZero R] : (rp.coroot i : N) ≠ 0 :=
+  fun h ↦ by simpa [h] using rp.coroot_root_two i
+
+lemma root_coroot_two :
+    (p.flip (rp.root i)) (rp.coroot i) = 2 := by
+  simpa only [LinearEquiv.flip_apply] using rp.coroot_root_two i
+
+/-- The symmetry associated to a root. -/
+def symmetryOfRoot : M ≃ₗ[R] M :=
+  toSymmetry (rp.coroot_root_two i)
+
+lemma symmetryOfRoot_apply (x : M) :
+    rp.symmetryOfRoot i x = x - (p (rp.coroot i) x) • rp.root i :=
+  rfl
+
+/-- The symmetry associated to a coroot. -/
+def symmetryOfCoroot : N ≃ₗ[R] N :=
+  toSymmetry (rp.root_coroot_two i)
+
+lemma symmetryOfCoroot_apply (f : N) :
+    rp.symmetryOfCoroot i f = f - (p.flip (rp.root i) f) • rp.coroot i :=
+  rfl
+
+@[simp]
+lemma symmetry_image_eq :
+    rp.symmetryOfRoot i '' (range rp.root) = range rp.root := by
+  change (rp.symmetryOfRoot i).toEquiv '' (range rp.root) = range rp.root
+  rw [(finite_range rp.root).equiv_image_eq_iff_subset]
+  exact rp.symmetry_image_root_eq i
+
+@[simp]
+lemma symmetry_image_eq' :
+    rp.symmetryOfCoroot i '' (range rp.coroot) = range rp.coroot := by
+  change (rp.symmetryOfCoroot i).toEquiv '' (range rp.coroot) = range rp.coroot
+  rw [(finite_range rp.coroot).equiv_image_eq_iff_subset]
+  exact rp.symmetry_image_coroot_eq i
+
+lemma symmetryOfRoot_dualMap_eq_symmetryOfCoroot :
+    p.trans (rp.symmetryOfRoot i).dualMap = (rp.symmetryOfCoroot i).trans p := by
+  ext n m
+  -- TODO Sort out messy proof
+  simp only [LinearEquiv.trans_apply, LinearEquiv.dualMap_apply, symmetryOfRoot_apply, map_sub,
+    map_smulₛₗ, RingHom.id_apply, smul_eq_mul, symmetryOfCoroot_apply, LinearEquiv.flip_apply]
+  rw [LinearEquiv.map_sub]
+  simp only [map_smul, LinearMap.sub_apply, LinearMap.smul_apply, smul_eq_mul, sub_right_inj]
+  rw [mul_comm]
+
+-- This proof is horrendous (partly as a result of surviving several refactors of the base
+-- definitions). Once I fix it I am confident I can drop the `maxHeartbeats` bump. TODO do this!
+set_option maxHeartbeats 800000 in
+/-- Even though the coroots may not span, roots are distinguished by their pairing with the
+coroots. The proof depends crucially on the fact that there are finitely-many roots.
+
+Modulo trivial generalisations, this statement is exactly Lemma 1.1.4 on page 87 of SGA 3 XXI.
+See also `RootPairing.injOn_dualMap_subtype_span_roots_coroots` for a more useful restatement. -/
+lemma eq_of_forall_coroot_of_root_eq [NoZeroSMulDivisors ℤ M] (i j : ι)
+    (h : ∀ k, p (rp.coroot i) (rp.root k) = p (rp.coroot j) (rp.root k)) :
+    i = j := by
+  let α := rp.root i
+  let β := rp.root j
+  let sα : M ≃ₗ[R] M := rp.symmetryOfRoot i
+  let sβ : M ≃ₗ[R] M := rp.symmetryOfRoot j
+  have hα : sα β = β - (2 : R) • α := by rw [rp.symmetryOfRoot_apply, h j, rp.coroot_root_two j]
+  have hβ : sβ α = α - (2 : R) • β := by rw [rp.symmetryOfRoot_apply, ← h i, rp.coroot_root_two i]
+  let sαβ : M ≃ₗ[R] M := sα * sβ
+  have key : ∀ n : ℕ, (sαβ^[n]) β = β + (2 * n : ℤ) • (α - β) := by
+    intro n
+    induction' n with n ih; simp
+    simp only [iterate_succ', ih, comp_apply, Nat.cast_succ, zsmul_sub, zsmul_sub, map_add, map_sub,
+      map_zsmul]
+    change sα (sβ β) + (_ • sα (sβ _) - _ • sα (sβ _)) = _
+    erw [toSymmetry_apply_self]
+    rw [hβ, map_sub, map_smul, map_neg, hα]
+    erw [toSymmetry_apply_self]
+    simp only [two_smul, neg_sub, zsmul_sub, smul_neg, smul_add, mul_add, mul_one, add_smul]
+    abel
+  replace key : ∀ n : ℕ, (β : M) + (2 * n : ℤ) • (α - β) ∈ range rp.root := by
+    intros n
+    have hsαβ : (sαβ : M → M) '' (range rp.root) = (range rp.root) := by
+      change (sα : M → M) ∘ (sβ : M → M) '' (range rp.root) = (range rp.root)
+      simp only [image_comp, rp.symmetry_image_eq]
+    replace hsαβ : (sαβ^[n]) '' (range rp.root) = (range rp.root) := by
+      rw [← IsFixedPt, image_iterate]
+      exact IsFixedPt.iterate hsαβ n
+    conv_rhs => rw [← hsαβ]
+    rw [← key]
+    exact mem_image_of_mem _ (mem_range_self j)
+  let f : ℕ → range rp.root := fun n ↦ ⟨β + (2 * n : ℤ) • (α - β), key n⟩
+  have : ¬ Injective f := by
+    have : Finite (range rp.root) := Finite.Set.finite_range rp.root
+    exact not_injective_infinite_finite f
+  contrapose! this
+  replace this : α ≠ β := by contrapose! this; exact rp.root.injective this
+  intros n m hnm
+  rw [Subtype.mk_eq_mk, add_right_inj, ← sub_eq_zero, ← sub_smul, smul_eq_zero, sub_eq_zero,
+    sub_eq_zero] at hnm
+  linarith [hnm.resolve_right this]
+
+lemma injOn_dualMap_subtype_span_roots_coroots [NoZeroSMulDivisors ℤ M] :
+    InjOn ((span R (range rp.root)).subtype.dualMap ∘ₗ p) (range rp.coroot) := by
+  rintro - ⟨i, rfl⟩ - ⟨j, rfl⟩ hij
+  congr
+  refine rp.eq_of_forall_coroot_of_root_eq i j fun k ↦ ?_
+  simpa using LinearMap.congr_fun hij ⟨rp.root k, Submodule.subset_span (mem_range_self k)⟩
+
+/-- In characteristic zero if there is no torsion, the correspondence between roots and coroots is
+unique.
+
+Formally, the point is that the hypothesis `hc` depends only on the range of the coroot mappings. -/
+@[ext]
+protected lemma ext [CharZero R] [NoZeroSMulDivisors R M]
+    {rp₁ rp₂ : RootPairing ι p}
+    (hr : rp₁.root = rp₂.root)
+    (hc : range rp₁.coroot = range rp₂.coroot) :
+    rp₁ = rp₂ := by
+  suffices rp₁.coroot = rp₂.coroot by cases rp₁; cases rp₂; congr
+  have := NoZeroSMulDivisors.IntOfCharZero R M
+  ext i
+  apply rp₁.injOn_dualMap_subtype_span_roots_coroots (mem_range_self i) (hc ▸ mem_range_self i)
+  simp only [LinearMap.coe_comp, LinearEquiv.coe_coe, comp_apply]
+  apply eq_of_toPreSymmetry_image_eq' (rp₁.ne_zero i) (finite_range rp₁.root)
+  · exact Submodule.subset_span (mem_range_self i)
+  · exact rp₁.coroot_root_two i
+  · exact rp₁.symmetry_image_eq i
+  · exact hr ▸ rp₂.coroot_root_two i
+  · exact hr ▸ rp₂.symmetry_image_eq i
+
+end RootPairing
+
+namespace RootSystem
+
+/-- In characteristic zero if there is no torsion, a root system is determined entirely by its
+roots. -/
+@[ext]
+protected lemma ext [CharZero R] [NoZeroSMulDivisors R M]
+    {rp₁ rp₂ : RootSystem ι R M}
+    (hr : rp₁.root = rp₂.root) :
+    rp₁ = rp₂ := by
+  suffices ∀ (rp₁ rp₂ : RootSystem ι R M) (_hr : rp₁.root = rp₂.root),
+      range rp₁.coroot ⊆ range rp₂.coroot by
+    have h₁ := this rp₁ rp₂ hr
+    have h₂ := this rp₂ rp₁ hr.symm
+    cases' rp₁ with rp₁
+    cases' rp₂ with rp₂
+    congr
+    exact RootPairing.ext hr (le_antisymm h₁ h₂)
+  clear! rp₁ rp₂
+  rintro rp₁ rp₂ hr - ⟨i, rfl⟩
+  use i
+  apply (LinearEquiv.refl R (Dual R M)).injective
+  apply eq_of_toPreSymmetry_image_eq (rp₁.ne_zero i) (finite_range rp₁.root) rp₁.span_eq_top
+  · rw [hr]
+    exact rp₂.coroot_root_two i
+  · rw [hr]
+    exact rp₂.symmetry_image_eq i
+  · exact rp₁.coroot_root_two i
+  · exact rp₁.symmetry_image_eq i
+
+variable {ι p}
+
+-- TODO (1) sensible name (should I state this without using `p`?)
+-- TODO (2) make another version of this lemma, stated for coroots in a true `RootSystem`
+lemma mk_aux [CharZero R] [NoZeroSMulDivisors R M]
+    (root : ι ↪ M)
+    (coroot : ι ↪ N)
+    (hp : ∀ i, p (coroot i) (root i) = 2)
+    (hs : ∀ i, toPreSymmetry (root i) (p (coroot i)) '' (range root) = range root)
+    (hsp : span R (range root) = ⊤) {i j k : ι}
+    (hk : root k = toPreSymmetry (root i) (p (coroot i)) (root j)) :
+    coroot k = toPreSymmetry (coroot i) (p.flip (root i)) (coroot j) := by
+  apply p.injective
+  have hk₀ : root k ≠ 0 := fun h ↦ by simpa [h] using hp k
+  set si := toPreSymmetry (root i) (p (coroot i))
+  set sj := toPreSymmetry (root j) (p (coroot j))
+  have hij : toPreSymmetry (toPreSymmetry (root i) (p (coroot i)) (root j))
+        (p <| toPreSymmetry (coroot i) (p.flip (root i)) (coroot j)) = si * sj * si := by
+    ext
+    simp [← toPreSymmetry_toPreSymmetry (root i) (root j) (p (coroot i)) (p (coroot j)) (hp i),
+      toPreSymmetry_apply, map_sub p]
+  apply eq_of_toPreSymmetry_image_eq_fixed hk₀ (finite_range root) hsp (hp k) ((hs k).symm ▸ refl _)
+  · rw [hk]
+    simp only [toPreSymmetry_apply, LinearEquiv.flip_apply, map_sub, map_smulₛₗ, RingHom.id_apply,
+      smul_eq_mul]
+    erw [map_sub p] -- Why is `erw` necessary? TODO Fix this.
+    simp only [map_smul, LinearMap.sub_apply, LinearMap.smul_apply, smul_eq_mul, hp i, hp j,
+      mul_two, sub_add_cancel'', mul_neg, sub_neg_eq_add]
+    rw [mul_comm, sub_add_cancel]
+  · rw [hk, hij]
+    change (si ∘ sj ∘ si) '' _ ⊆ _
+    rw [← comp.assoc]
+    simp only [image_comp, hs i, hs j]
+    rfl
+
+/-- In characteristic zero if there is no torsion, to check that a collection of roots form a root
+system, we do not need to check that the coroots are stable under reflections since this follows
+from the corresponding property for the roots. -/
+def mk' [CharZero R] [NoZeroSMulDivisors R M]
+    (root : ι ↪ M)
+    (coroot : ι ↪ Dual R M)
+    (hp : ∀ i, (coroot i) (root i) = 2)
+    (hs : ∀ i, toPreSymmetry (root i) (coroot i) '' (range root) = range root) -- TODO ⊆
+    (hsp : span R (range root) = ⊤) :
+    RootSystem ι R M where
+  root := root
+  coroot := coroot
+  coroot_root_two := hp
+  symmetry_image_root_eq := fun i ↦ by rw [LinearEquiv.refl_apply, hs i]
+  span_eq_top := hsp
+  symmetry_image_coroot_eq := by
+    intro i f hf
+    simp only [mem_image, mem_range, exists_exists_eq_and] at hf
+    obtain ⟨j, rfl⟩ := hf
+    obtain ⟨k : ι, hk : root k = toPreSymmetry (root i) (coroot i) (root j)⟩ :=
+      (hs i) ▸ mem_image_of_mem _ (mem_range_self j)
+    exact ⟨k, mk_aux root coroot hp (by simp [hs]) hsp hk⟩
+
+end RootSystem
---
+++ Mathlib/LinearAlgebra/RootSystem/Symmetries.lean
@@ -0,0 +1,165 @@
+/-
+Copyright (c) 2023 Oliver Nash. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Oliver Nash, Deepro Choudhury
+-/
+import Mathlib.GroupTheory.OrderOfElement
+import Mathlib.LinearAlgebra.Contraction
+import Mathlib.LinearAlgebra.RootSystem.FindHome
+
+/-!
+# Symmetries and reflections
+
+Hmm, does this definitely belong in the `RootSystem` directory? Maybe could go one level up?
+
+## Implementation details
+
+Explain point is that we don't assume existence of inner product
+
+## TODO
+
+Show symmetries arise from inner product spaces
+
+-/
+
+open Set Function Pointwise
+open Module hiding Finite
+open Submodule (span)
+
+noncomputable section
+
+variable {R M : Type*} [CommRing R] [AddCommGroup M] [Module R M]
+
+/-- TODO -/
+def toPreSymmetry (x : M) (f : Dual R M) : End R M :=
+  LinearMap.id - dualTensorHom R M M (f ⊗ₜ x)
+
+lemma toPreSymmetry_apply (x y : M) (f : Dual R M) :
+    toPreSymmetry x f y = y - (f y) • x := by
+  simp [toPreSymmetry]
+
+lemma toPreSymmetry_apply_self {x : M} {f : Dual R M} (h : f x = 2) :
+    toPreSymmetry x f x = - x := by
+  rw [toPreSymmetry_apply, h, two_smul]; abel
+
+lemma involutive_toPreSymmetry {x : M} {f : Dual R M} (h : f x = 2) :
+    Involutive (toPreSymmetry x f) :=
+  fun y ↦ by simp [h, smul_sub, two_smul, toPreSymmetry_apply]
+
+/-- TODO -/
+def toSymmetry {x : M} {f : Dual R M} (h : f x = 2) : M ≃ₗ[R] M :=
+  LinearEquiv.ofBijective (toPreSymmetry x f) (involutive_toPreSymmetry h).bijective
+
+lemma toSymmetry_apply {x : M} (y : M) {f : Dual R M} (h : f x = 2) :
+    toSymmetry h y = y - (f y) • x :=
+  toPreSymmetry_apply x y f
+
+lemma toSymmetry_apply_self {x : M} {f : Dual R M} (h : f x = 2) :
+    toSymmetry h x = - x :=
+  toPreSymmetry_apply_self h
+
+lemma LinearEquiv.apply_toPreSymmetry {N : Type*} [AddCommGroup N] [Module R N] (e : M ≃ₗ[R] N)
+    (x : M) (f : Dual R M) :
+    e ∘ₗ toPreSymmetry x f = toPreSymmetry (e x) (e.symm.dualMap f) ∘ₗ e := by
+  ext; simp [toPreSymmetry_apply]
+
+lemma toPreSymmetry_toPreSymmetry (a b : M) (f g : Dual R M) (h : f a = 2) :
+    toPreSymmetry (toPreSymmetry a f b) (toPreSymmetry f (Dual.eval R M a) g) =
+    (toPreSymmetry a f) * (toPreSymmetry b g) * (toPreSymmetry a f) := by
+  ext m
+  simp only [h, toPreSymmetry_apply, mul_comm (g a) (f m), mul_two, mul_assoc, LinearMap.sub_apply,
+    LinearMap.mul_apply, LinearMap.smul_apply, dualTensorHom_apply, smul_eq_mul,
+    smul_sub, sub_smul, smul_smul, sub_mul, map_sub, map_smul, add_smul, Dual.eval_apply]
+  abel
+
+lemma eq_of_toPreSymmetry_image_eq [CharZero R] [NoZeroSMulDivisors R M]
+    {x : M} (hx : x ≠ 0)
+    {Φ : Set M} (hΦ₁ : Φ.Finite) (hΦ₂ : span R Φ = ⊤)
+    {f g : Dual R M} (hf₁ : f x = 2) (hf₂ : toPreSymmetry x f '' Φ = Φ)
+                     (hg₁ : g x = 2) (hg₂ : toPreSymmetry x g '' Φ = Φ) :
+    f = g := by
+  let u := toSymmetry hg₁ * toSymmetry hf₁
+  suffices IsOfFinOrder u by
+    have hu : u = LinearMap.id (R := R) (M := M) + dualTensorHom R M M ((f - g) ⊗ₜ x) := by
+      ext y
+      simp only [toSymmetry, LinearEquiv.coe_mul, LinearMap.mul_apply, LinearEquiv.coe_coe,
+        LinearEquiv.ofBijective_apply, toPreSymmetry_apply, map_sub, map_smul, hg₁, two_smul,
+        sub_add_cancel', smul_neg, sub_neg_eq_add, LinearMap.add_apply, LinearMap.id_coe, id.def,
+        dualTensorHom_apply, LinearMap.sub_apply, sub_smul]
+      abel
+    replace hu : ∀ (n : ℕ), ↑(u^n) = LinearMap.id (R := R) (M := M) + (n : R) • dualTensorHom R M M ((f - g) ⊗ₜ x) := by
+      intros n
+      induction' n with n ih; simp
+      have aux : (dualTensorHom R M M ((f - g) ⊗ₜ[R] x)).comp
+        ((n : R) • dualTensorHom R M M ((f - g) ⊗ₜ[R] x)) = 0 := by ext v; simp [hf₁, hg₁]
+      rw [pow_succ, LinearEquiv.coe_mul, ih, hu, add_mul, mul_add, mul_add]
+      simp only [LinearMap.mul_eq_comp, LinearMap.id_comp, LinearMap.comp_id, Nat.cast_succ,
+        aux, add_zero, add_smul, one_smul, add_assoc]
+    obtain ⟨n, hn₀, hn₁⟩ := isOfFinOrder_iff_pow_eq_one.mp this
+    replace hn₁ : (↑(u ^ n) : M →ₗ[R] M) = LinearMap.id := LinearEquiv.toLinearMap_inj.mpr hn₁
+    simpa [hn₁, hn₀.ne', hx, sub_eq_zero] using hu n
+  suffices IsOfFinOrder u.toLinearMap by
+    have hf : Injective (LinearEquiv.automorphismGroup.toLinearMapMonoidHom (R := R) (M := M)) :=
+      LinearEquiv.toLinearMap_injective
+    exact hf.isOfFinOrder_iff.mp this
+  refine isOfFinOrder_of_finite_of_span_eq_top_of_image_eq hΦ₁ hΦ₂ ?_
+  change (toPreSymmetry x g ∘ toPreSymmetry x f '' Φ) = Φ
+  rw [image_comp, hf₂, hg₂]
+
+-- TODO Replace the lemma above with this (more generally use `⊆` instead of `=` in corresponding
+-- hypotheses across all relevant lemmas)
+lemma eq_of_toPreSymmetry_image_eq_fixed [CharZero R] [NoZeroSMulDivisors R M]
+    {x : M} (hx : x ≠ 0)
+    {Φ : Set M} (hΦ₁ : Φ.Finite) (hΦ₂ : span R Φ = ⊤)
+    {f g : Dual R M} (hf₁ : f x = 2) (hf₂ : toPreSymmetry x f '' Φ ⊆ Φ)
+                     (hg₁ : g x = 2) (hg₂ : toPreSymmetry x g '' Φ ⊆ Φ) :
+    f = g := by
+  change (toSymmetry hf₁).toEquiv '' Φ ⊆ Φ at hf₂
+  rw [← hΦ₁.equiv_image_eq_iff_subset] at hf₂
+  change (toSymmetry hg₁).toEquiv '' Φ ⊆ Φ at hg₂
+  rw [← hΦ₁.equiv_image_eq_iff_subset] at hg₂
+  exact eq_of_toPreSymmetry_image_eq hx hΦ₁ hΦ₂ hf₁ hf₂ hg₁ hg₂
+
+lemma eq_of_toPreSymmetry_image_eq' [CharZero R] [NoZeroSMulDivisors R M]
+    {x : M} (hx : x ≠ 0)
+    {Φ : Set M} (hΦ₁ : Φ.Finite) (hx' : x ∈ span R Φ)
+    {f g : Dual R M} (hf₁ : f x = 2) (hf₂ : toPreSymmetry x f '' Φ = Φ)
+                     (hg₁ : g x = 2) (hg₂ : toPreSymmetry x g '' Φ = Φ) :
+    (span R Φ).subtype.dualMap f = (span R Φ).subtype.dualMap g := by
+  let Φ' : Set (span R Φ) := range (inclusion <| Submodule.subset_span (R := R) (s := Φ))
+  have hΦ'₁ : Φ'.Finite := by
+    rw [← finite_coe_iff] at hΦ₁; exact finite_range (inclusion Submodule.subset_span)
+  have hΦ'₂ : span R Φ' = ⊤ := by simp
+  let x' : span R Φ := ⟨x, hx'⟩
+  have hx' : x' ≠ 0 := Subtype.ne_of_val_ne hx
+  let f' := (span R Φ).subtype.dualMap f
+  let g' := (span R Φ).subtype.dualMap g
+  replace hf₂ : toPreSymmetry x' f' '' Φ' = Φ' := by
+    ext ⟨y, hy⟩
+    -- Fix (and abstract) this 🤢🤮 proof
+    simp only [toPreSymmetry_apply, LinearMap.dualMap_apply, Submodule.coeSubtype,
+      SetLike.mk_smul_mk, range_inclusion, SetLike.coe_sort_coe, mem_image, mem_setOf_eq,
+      Subtype.exists, exists_and_left]
+    simp_rw [Subtype.ext_iff]
+    simp only [AddSubgroupClass.coe_sub, exists_prop]
+    simp_rw [← toPreSymmetry_apply]
+    conv_rhs => rw [← hf₂]
+    simp only [mem_image]
+    refine exists_congr fun z ↦ ?_
+    simp only [and_congr_right_iff, and_iff_right_iff_imp]
+    exact fun hz _ ↦ Submodule.subset_span hz
+  replace hg₂ : toPreSymmetry x' g' '' Φ' = Φ' := by
+    ext ⟨y, hy⟩
+    -- Fix (and abstract) this 🤢🤮 proof
+    simp only [toPreSymmetry_apply, LinearMap.dualMap_apply, Submodule.coeSubtype,
+      SetLike.mk_smul_mk, range_inclusion, SetLike.coe_sort_coe, mem_image, mem_setOf_eq,
+      Subtype.exists, exists_and_left]
+    simp_rw [Subtype.ext_iff]
+    simp only [AddSubgroupClass.coe_sub, exists_prop]
+    simp_rw [← toPreSymmetry_apply]
+    conv_rhs => rw [← hg₂]
+    simp only [mem_image]
+    refine exists_congr fun z ↦ ?_
+    simp only [and_congr_right_iff, and_iff_right_iff_imp]
+    exact fun hz _ ↦ Submodule.subset_span hz
+  exact eq_of_toPreSymmetry_image_eq hx' hΦ'₁ hΦ'₂ (f := f') (g := g') hf₁ hf₂ hg₁ hg₂
