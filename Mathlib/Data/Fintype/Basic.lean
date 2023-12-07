b95030051fb5e4b28c5adb52b0e7ba96b8ff3857
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2023-12-07 14:39:20 +0100

    typo

--- Mathlib/Data/Fintype/Basic.lean
+++ Mathlib/Data/Fintype/Basic.lean
@@ -597,7 +597,7 @@ theorem univ_ofSubsingleton (a : α) [Subsingleton α] : @univ _ (ofSubsingleton
 #align fintype.univ_of_subsingleton Fintype.univ_ofSubsingleton
 
 /-- An empty type is a fintype. Not registered as an instance, to make sure that there aren't two
-conflicting `Fintype ι` instances around when casing over wheter a fintype `ι` is empty or not. -/
+conflicting `Fintype ι` instances around when casing over whether a fintype `ι` is empty or not. -/
 def ofIsEmpty [IsEmpty α] : Fintype α :=
   ⟨∅, isEmptyElim⟩
 #align fintype.of_is_empty Fintype.ofIsEmpty
