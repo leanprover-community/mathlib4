758e4d1cbdbcbf439be124a29910b555a9d52946
Joël Riou <joel.riou@universite-paris-saclay.fr>
2023-12-07 19:28:56 +0100

    little cleaning up

--- Mathlib/CategoryTheory/Localization/Pi.lean
+++ Mathlib/CategoryTheory/Localization/Pi.lean
@@ -12,7 +12,7 @@ In this file, it is shown that if for all `j : J` (with `J` finite),
 functors `L j : C j ⥤ D j` are localization functors with respect
 to a class of morphism `W j : MorphismProperty (C j)`, then the product
 functor `Functor.pi L : (∀ j, C j) ⥤ ∀ j, D j` is a localization
-functor for the product class of morphism `MorphismProperty.pi W`.
+functor for the product class of morphisms `MorphismProperty.pi W`.
 The proof proceeds by induction on the cardinal of `J` using the
 main result of the file `Mathlib.CategoryTheory.Localization.Prod`.
 
@@ -30,17 +30,14 @@ instance pi {J : Type w} [Finite J] {C : J → Type u₁} {D : J → Type u₂}
   revert J
   apply Finite.induction_empty_option
   · intro J₁ J₂ e hJ₁ C₂ D₂ _ _ L₂ W₂ _ _
-    let C₁ := fun j => (C₂ (e j))
-    let D₁ := fun j => (D₂ (e j))
-    let L₁ : ∀ j, C₁ j ⥤ D₁ j := fun j => (L₂ (e j))
-    let W₁ : ∀ j, MorphismProperty (C₁ j) := fun j => (W₂ (e j))
-    replace hJ₁ := hJ₁ L₁ W₁
+    let L₁ := fun j => (L₂ (e j))
     let E := Pi.equivalenceOfEquiv C₂ e
     let E' := Pi.equivalenceOfEquiv D₂ e
     haveI : CatCommSq E.functor (Functor.pi L₁) (Functor.pi L₂) E'.functor :=
       (CatCommSq.hInvEquiv E (Functor.pi L₁) (Functor.pi L₂) E').symm ⟨Iso.refl _⟩
     refine IsLocalization.of_equivalences (Functor.pi L₁)
-      (MorphismProperty.pi W₁) (Functor.pi L₂) (MorphismProperty.pi W₂) E E' ?_
+      (MorphismProperty.pi (fun j => (W₂ (e j)))) (Functor.pi L₂)
+      (MorphismProperty.pi W₂) E E' ?_
       (MorphismProperty.IsInvertedBy.pi _ _ (fun _ => Localization.inverts _ _))
     intro _ _ f hf
     refine ⟨_, _, E.functor.map f, fun i => ?_, ⟨Iso.refl _⟩⟩
@@ -55,17 +52,12 @@ instance pi {J : Type w} [Finite J] {C : J → Type u₁} {D : J → Type u₂}
     rw [MorphismProperty.isomorphisms.iff, isIso_pi_iff]
     rintro ⟨⟩
   · intro J _ hJ C D _ _ L W _ _
-    let C' := fun j => C (some j)
-    let D' := fun j => D (some j)
-    let L' : ∀ j, C' j ⥤ D' j := fun j => L (some j)
-    let W' : ∀ j, MorphismProperty (C' j) := fun j => W (some j)
-    replace hJ := hJ L' W'
-    let L₁ := (L none).prod (Functor.pi L')
-    let L₂ := Functor.pi L
-    haveI : CatCommSq (Pi.optionEquivalence C).symm.functor L₁ L₂
+    let L₁ := (L none).prod (Functor.pi (fun j => L (some j)))
+    haveI : CatCommSq (Pi.optionEquivalence C).symm.functor L₁ (Functor.pi L)
       (Pi.optionEquivalence D).symm.functor :=
         ⟨NatIso.pi' (by rintro (_|i) <;> apply Iso.refl)⟩
-    refine IsLocalization.of_equivalences L₁ ((W none).prod (MorphismProperty.pi W')) L₂ _
+    refine IsLocalization.of_equivalences L₁
+      ((W none).prod (MorphismProperty.pi (fun j => W (some j)))) (Functor.pi L) _
       (Pi.optionEquivalence C).symm (Pi.optionEquivalence D).symm ?_ ?_
     · intro ⟨X₁, X₂⟩ ⟨Y₁, Y₂⟩ f ⟨hf₁, hf₂⟩
       refine ⟨_, _, (Pi.optionEquivalence C).inverse.map f, ?_, ⟨Iso.refl _⟩⟩
