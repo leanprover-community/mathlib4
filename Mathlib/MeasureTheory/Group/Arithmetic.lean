9c211d2d9199684bab519afcdb64256657ec97c5
Yaël Dillies <yael.dillies@gmail.com>
2023-12-07 12:41:15 +0000

    import PFR material

--- Mathlib/MeasureTheory/Group/Arithmetic.lean
+++ Mathlib/MeasureTheory/Group/Arithmetic.lean
@@ -44,12 +44,11 @@ measurable function, arithmetic operator
   in the conclusion of `MeasurableSMul`.)
 -/
 
+open MeasureTheory
+open scoped BigOperators Pointwise
 
 universe u v
-
-open BigOperators Pointwise MeasureTheory
-
-open MeasureTheory
+variable {α : Type*}
 
 /-!
 ### Binary operations: `(· + ·)`, `(· * ·)`, `(· - ·)`, `(· / ·)`
@@ -524,6 +523,11 @@ instance (priority := 100) measurableDiv₂_of_mul_inv (G : Type*) [MeasurableSp
 #align has_measurable_div₂_of_mul_inv measurableDiv₂_of_mul_inv
 #align has_measurable_div₂_of_add_neg measurableDiv₂_of_add_neg
 
+-- See note [lower instance priority]
+instance (priority := 100) MeasurableDiv.toMeasurableInv [MeasurableSpace α] [Group α]
+    [MeasurableDiv α] : MeasurableInv α where
+  measurable_inv := by simpa using measurable_const_div (1 : α)
+
 /-- We say that the action of `M` on `α` has `MeasurableVAdd` if for each `c` the map `x ↦ c +ᵥ x`
 is a measurable function and for each `x` the map `c ↦ c +ᵥ x` is a measurable function. -/
 class MeasurableVAdd (M α : Type*) [VAdd M α] [MeasurableSpace M] [MeasurableSpace α] :
@@ -957,33 +961,28 @@ theorem Finset.aemeasurable_prod (s : Finset ι) (hf : ∀ i ∈ s, AEMeasurable
 
 end CommMonoid
 
-section DiscreteMeasurableSpace
+variable [MeasurableSpace α] [Mul α] [Div α] [Inv α]
 
-variable {α : Type*} [MeasurableSpace α] [DiscreteMeasurableSpace α]
+@[to_additive] -- See note [lower instance priority]
+instance DiscreteMeasurableSpace.toMeasurableMul [DiscreteMeasurableSpace α] :
+    MeasurableMul α where
+  measurable_const_mul _ := measurable_discrete _
+  measurable_mul_const _ := measurable_discrete _
 
-@[to_additive]
-instance [Mul α] : MeasurableMul α :=
-  ⟨fun _ ↦ DiscreteMeasurableSpace.measurable _, fun _ ↦ DiscreteMeasurableSpace.measurable _⟩
+@[to_additive DiscreteMeasurableSpace.toMeasurableAdd₂] -- See note [lower instance priority]
+instance DiscreteMeasurableSpace.toMeasurableMul₂ [DiscreteMeasurableSpace (α × α)] :
+    MeasurableMul₂ α := ⟨measurable_discrete _⟩
 
-@[to_additive]
-instance [Div α] : MeasurableDiv α :=
-  ⟨fun _ ↦ DiscreteMeasurableSpace.measurable _, fun _ ↦ DiscreteMeasurableSpace.measurable _⟩
+@[to_additive] -- See note [lower instance priority]
+instance DiscreteMeasurableSpace.toMeasurableInv [DiscreteMeasurableSpace α] :
+    MeasurableInv α := ⟨measurable_discrete _⟩
 
-@[to_additive]
-instance [Inv α] : MeasurableInv α := ⟨DiscreteMeasurableSpace.measurable _⟩
+@[to_additive] -- See note [lower instance priority]
+instance DiscreteMeasurableSpace.toMeasurableDiv [DiscreteMeasurableSpace α] :
+    MeasurableDiv α where
+  measurable_const_div _ := measurable_discrete _
+  measurable_div_const _ := measurable_discrete _
 
-end DiscreteMeasurableSpace
-
-section Countable
-
-variable {α : Type*} [MeasurableSpace α] [Countable α] [MeasurableSingletonClass α]
-
-instance [Add α] : MeasurableAdd₂ α := ⟨DiscreteMeasurableSpace.measurable _⟩
-
-instance [Mul α] : MeasurableMul₂ α := ⟨DiscreteMeasurableSpace.measurable _⟩
-
-instance [Sub α] : MeasurableSub₂ α := ⟨DiscreteMeasurableSpace.measurable _⟩
-
-instance [Div α] : MeasurableDiv₂ α := ⟨DiscreteMeasurableSpace.measurable _⟩
-
-end Countable
+@[to_additive DiscreteMeasurableSpace.toMeasurableSub₂] -- See note [lower instance priority]
+instance DiscreteMeasurableSpace.toMeasurableDiv₂ [DiscreteMeasurableSpace (α × α)] :
+    MeasurableDiv₂ α := ⟨measurable_discrete _⟩
--- Mathlib/MeasureTheory/MeasurableSpace/Defs.lean
+++ Mathlib/MeasureTheory/MeasurableSpace/Defs.lean
@@ -318,23 +318,6 @@ theorem Set.Countable.measurableSet {s : Set α} (hs : s.Countable) : Measurable
 
 end MeasurableSingletonClass
 
-/-- A typeclass mixin for `MeasurableSpace`s such that all sets are measurable. -/
-class DiscreteMeasurableSpace (α : Type*) [MeasurableSpace α] : Prop where
-  /-- Any set of a discrete measurable space is measurable. -/
-  measurableSet : ∀ (s : Set α), MeasurableSet s
-
-attribute [measurability] DiscreteMeasurableSpace.measurableSet
-
-section DiscreteMeasurableSpace
-
-instance [MeasurableSpace α] [MeasurableSingletonClass α] [Countable α] :
-    DiscreteMeasurableSpace α := ⟨fun _ ↦ Set.Countable.measurableSet (Set.to_countable _)⟩
-
-instance [MeasurableSpace α] [DiscreteMeasurableSpace α] :
-    MeasurableSingletonClass α := ⟨fun _ ↦ DiscreteMeasurableSpace.measurableSet _⟩
-
-end DiscreteMeasurableSpace
-
 namespace MeasurableSpace
 
 /-- Copy of a `MeasurableSpace` with a new `MeasurableSet` equal to the old one. Useful to fix
@@ -543,10 +526,6 @@ theorem generateFrom_iUnion_measurableSet (m : ι → MeasurableSpace α) :
   (@giGenerateFrom α).l_iSup_u m
 #align measurable_space.generate_from_Union_measurable_set MeasurableSpace.generateFrom_iUnion_measurableSet
 
-instance : @DiscreteMeasurableSpace α ⊤ := by
-  letI : MeasurableSpace α := ⊤
-  exact ⟨fun s ↦ measurableSet_top⟩
-
 end CompleteLattice
 
 end MeasurableSpace
@@ -596,13 +575,33 @@ theorem Measurable.le {α} {m m0 : MeasurableSpace α} {_ : MeasurableSpace β} 
     {f : α → β} (hf : Measurable[m] f) : Measurable[m0] f := fun _ hs => hm _ (hf hs)
 #align measurable.le Measurable.le
 
-@[measurability]
-theorem DiscreteMeasurableSpace.measurable {α β : Type*} [MeasurableSpace α] [MeasurableSpace β]
-    [DiscreteMeasurableSpace α] (f : α → β) : Measurable f :=
-  fun _ _ => DiscreteMeasurableSpace.measurableSet _
-
-theorem MeasurableSpace.Top.measurable {α β : Type*} [MeasurableSpace β] (f : α → β) :
-    Measurable[⊤] f := @DiscreteMeasurableSpace.measurable α β ⊤ _ _ f
-#align measurable_space.top.measurable MeasurableSpace.Top.measurable
-
 end MeasurableFunctions
+
+/-- A typeclass mixin for `MeasurableSpace`s such that all sets are measurable. -/
+class DiscreteMeasurableSpace (α : Type*) [MeasurableSpace α] : Prop where
+  /-- Do not use this. Use `measurableSet_discrete` instead. -/
+  forall_measurableSet : ∀ s : Set α, MeasurableSet s
+
+instance : @DiscreteMeasurableSpace α ⊤ :=
+  @DiscreteMeasurableSpace.mk _ (_) fun _ ↦ MeasurableSpace.measurableSet_top
+
+instance MeasurableSingletonClass.toDiscreteMeasurableSpace [MeasurableSpace α]
+    [MeasurableSingletonClass α] [Countable α] : DiscreteMeasurableSpace α where
+  forall_measurableSet _ := (Set.to_countable _).measurableSet
+
+section DiscreteMeasurableSpace
+variable [MeasurableSpace α] [MeasurableSpace β] [DiscreteMeasurableSpace α]
+
+@[simp, measurability] lemma measurableSet_discrete (s : Set α) : MeasurableSet s :=
+  DiscreteMeasurableSpace.forall_measurableSet _
+
+@[measurability]
+lemma measurable_discrete (f : α → β) : Measurable f := fun _ _ ↦ measurableSet_discrete _
+#align measurable_space.top.measurable measurable_discrete
+
+/-- Warning: Creates a typeclass loop with `MeasurableSingletonClass.toDiscreteMeasurableSpace`.
+To be monitored. -/
+instance DiscreteMeasurableSpace.toMeasurableSingletonClass : MeasurableSingletonClass α where
+  measurableSet_singleton _ := measurableSet_discrete _
+
+end DiscreteMeasurableSpace
