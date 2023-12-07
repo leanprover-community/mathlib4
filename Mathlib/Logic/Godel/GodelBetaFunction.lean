c2b23ca1e232b83caab41b3d3044ba0489697aaf
Hunter Monroe <github.com@huntermonroe.com>
2023-12-07 10:51:52 -0500

    Bib file

--- Mathlib/Logic/Godel/GodelBetaFunction.lean
+++ Mathlib/Logic/Godel/GodelBetaFunction.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2023 Shogo Saito. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Shogo Saito
+Authors: Shogo Saito. Adapted for mathlib by Hunter Monroe
 -/
 
 import Mathlib.Data.Nat.ModEq
