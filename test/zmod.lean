905aec070a5e6520b19f69604c4521c8b4225778
Scott Morrison <scott.morrison@gmail.com>
2023-12-08 14:57:43 +1100

    silence test

--- test/zmod.lean
+++ test/zmod.lean
@@ -3,4 +3,6 @@ import Mathlib.Data.ZMod.Defs
 -- We ensure here that the `@[csimp]` attribute successfully replaces (at runtime)
 -- the default value of `npowRec` for the exponentation operation in `Monoid`
 -- with a tail-recursive implementation by repeated squaring.
+/-- info: 1 -/
+#guard_msgs in
 #eval (3 : ZMod 49999)^49998
