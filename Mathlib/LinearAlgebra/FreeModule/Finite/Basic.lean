310687161c55e3f50e6739bce173b467435dc209
erd1 <the.erd.one@gmail.com>
2023-12-09 05:20:11 +0800

    chore: Generalize results on `finrank` to rings.

--- Mathlib/LinearAlgebra/FreeModule/Finite/Basic.lean
+++ Mathlib/LinearAlgebra/FreeModule/Finite/Basic.lean
@@ -31,6 +31,13 @@ section Ring
 
 variable [Ring R] [AddCommGroup M] [Module R M] [Module.Free R M]
 
+/-- If a free module is finite, then any arbitrary basis is finite. -/
+lemma finite_basis {R M} [Ring R] [Nontrivial R] [AddCommGroup M] [Module R M]
+    {ι} [Module.Finite R M] (b : Basis ι R M) :
+    _root_.Finite ι :=
+  let ⟨s, hs⟩ := ‹Module.Finite R M›
+  basis_finite_of_finite_spans (↑s) s.finite_toSet hs b
+
 /-- If a free module is finite, then the arbitrary basis is finite. -/
 noncomputable instance ChooseBasisIndex.fintype [Module.Finite R M] :
     Fintype (Module.Free.ChooseBasisIndex R M) := by
@@ -39,8 +46,7 @@ noncomputable instance ChooseBasisIndex.fintype [Module.Finite R M] :
   · have := Module.subsingleton R M
     rw [ChooseBasisIndex]
     infer_instance
-  · obtain ⟨s, hs⟩ := id ‹Module.Finite R M›
-    exact basis_finite_of_finite_spans (↑s) s.finite_toSet hs (chooseBasis _ _)
+  · exact finite_basis (chooseBasis _ _)
 #align module.free.choose_basis_index.fintype Module.Free.ChooseBasisIndex.fintype
 
 end Ring
@@ -54,7 +60,7 @@ variable [AddCommGroup N] [Module R N] [Module.Free R N]
 variable {R}
 
 /-- A free module with a basis indexed by a `Fintype` is finite. -/
-theorem _root_.Module.Finite.of_basis {R M ι : Type*} [CommRing R] [AddCommGroup M] [Module R M]
+theorem _root_.Module.Finite.of_basis {R M ι : Type*} [Semiring R] [AddCommMonoid M] [Module R M]
     [_root_.Finite ι] (b : Basis ι R M) : Module.Finite R M := by
   cases nonempty_fintype ι
   classical
