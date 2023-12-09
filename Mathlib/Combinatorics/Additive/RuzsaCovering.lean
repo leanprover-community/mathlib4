bae41ae60b916ba0a42715aceeffdf2fabe86ec6
Eric Wieser <wieser.eric@gmail.com>
2023-12-09 10:37:50 +0000

    Update Mathlib/Combinatorics/Additive/RuzsaCovering.lean
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Mathlib/Combinatorics/Additive/RuzsaCovering.lean
+++ Mathlib/Combinatorics/Additive/RuzsaCovering.lean
@@ -70,7 +70,7 @@ lemma exists_subset_mul_div (hs : s.Finite) (ht' : t.Finite) (ht : t.Nonempty) :
   obtain ⟨u, hu, hsut⟩ := Finset.exists_subset_mul_div s ht
   refine ⟨u, ?_⟩
   -- `norm_cast` would find these automatically, but breaks `to_additive` when it does so
-  rw [←Finset.coe_mul, ←Finset.coe_mul, ←Finset.coe_div]
+  rw [← Finset.coe_mul, ← Finset.coe_mul, ← Finset.coe_div]
   norm_cast
   simp [*]
 
