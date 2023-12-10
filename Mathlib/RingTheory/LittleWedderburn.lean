948666804c8078782fd4c934ad91ef754e868142
Eric Rodriguez <ericrboidi@gmail.com>
2023-12-10 01:42:09 +0000

    zeta := false

--- Mathlib/RingTheory/LittleWedderburn.lean
+++ Mathlib/RingTheory/LittleWedderburn.lean
@@ -66,39 +66,40 @@ private theorem center_eq_top [Finite D] (hD : InductionHyp D) : Subring.center 
     rintro y - z
     obtain ⟨r, rfl⟩ := hx y
     obtain ⟨s, rfl⟩ := hx z
-    simp only [Subring.smul_def, smul_eq_mul]
+    simp (config := {zeta := false}) only [Subring.smul_def, smul_eq_mul]
     rw [← r.2, ← s.2, mul_assoc, mul_assoc, ← r.2, ← s.2, mul_assoc, mul_assoc, r.2]
   have card_D : Fintype.card D = q ^ n := card_eq_pow_finrank
   have h1qn : 1 ≤ q ^ n := by rw [← card_D]; exact Fintype.card_pos
   have key := Group.nat_card_center_add_sum_card_noncenter_eq_card (Dˣ)
-  simp only [Nat.card_eq_fintype_card] at key
+  simp (config := {zeta := false}) only [Nat.card_eq_fintype_card] at key
   rw [Fintype.card_congr (show _ ≃* Zˣ from Subgroup.centerUnitsEquivUnitsCenter D).toEquiv,
       Fintype.card_units, ← card_Z, Fintype.card_units, card_D] at key
   let Φ := cyclotomic n ℤ
   have aux : Φ.eval ↑q ∣ (q : ℤ) ^ n - 1 := by
-    simpa only [eval_X, eval_one, eval_pow, eval_sub, coe_evalRingHom] using
+    simpa (config := {zeta := false}) only [eval_X, eval_one, eval_pow, eval_sub, coe_evalRingHom] using
       (evalRingHom (q : ℤ)).map_dvd (cyclotomic.dvd_X_pow_sub_one n ℤ)
   apply_fun (Nat.cast : ℕ → ℤ) at key
-  simp only [Nat.cast_one, Nat.cast_pow, Nat.cast_add, Nat.cast_sub h1qn] at key aux
+  simp (config := {zeta := false}) only [Nat.cast_add] at key aux
+  rw [Nat.cast_sub h1qn, Nat.cast_one, Nat.cast_pow] at key
   rw [← key, dvd_add_left, ← Int.natAbs_dvd, ← Int.dvd_natAbs] at aux
-  simp only [Int.natAbs_ofNat, Int.coe_nat_dvd] at aux
+  simp (config := {zeta := false}) only [Int.natAbs_ofNat, Int.coe_nat_dvd] at aux
   · refine' (Nat.le_of_dvd _ aux).not_lt (sub_one_lt_natAbs_cyclotomic_eval hn hq.ne')
     exact tsub_pos_of_lt hq
   suffices : Φ.eval ↑q ∣ ↑(∑ x in (ConjClasses.noncenter (Dˣ)).toFinset, Fintype.card x.carrier)
   · convert this using 2
     convert finsum_cond_eq_sum_of_cond_iff _ _
-    simp only [iff_self_iff, Set.mem_toFinset, imp_true_iff]
-  simp only [Nat.cast_sum]
+    simp (config := {zeta := false}) only [iff_self_iff, Set.mem_toFinset, imp_true_iff]
+  simp (config := {zeta := false}) only [Nat.cast_sum]
   apply Finset.dvd_sum
   rintro ⟨x⟩ hx
-  simp only [ConjClasses.quot_mk_eq_mk]
+  simp (config := {zeta := false}) only [ConjClasses.quot_mk_eq_mk]
   set Zx := Subring.centralizer ({↑x} : Set D)
   rw [ConjClasses.card_carrier, ←Fintype.card_congr
         (show Zxˣ ≃* _ from unitsCentralizerEquiv _ x).toEquiv, Fintype.card_units, card_D]
   have hZx : Zx < ⊤ := by
     rw [lt_top_iff_ne_top]
     intro hZx
-    simp only [Set.mem_toFinset, ConjClasses.quot_mk_eq_mk] at hx
+    simp (config := {zeta := false}) only [Set.mem_toFinset, ConjClasses.quot_mk_eq_mk] at hx
     refine (ConjClasses.mk_bijOn (Dˣ)).1 (Set.subset_center_units ?_) hx
     exact Subring.centralizer_eq_top_iff_subset.mp hZx <| Set.mem_singleton _
   letI : Field Zx := hD.field hZx
@@ -110,20 +111,19 @@ private theorem center_eq_top [Finite D] (hD : InductionHyp D) : Subring.center 
   haveI : IsScalarTower Z Zx D := ⟨fun x y z ↦ mul_assoc _ _ _⟩
   have hdn : d ∣ n := ⟨_, (finrank_mul_finrank Z Zx D).symm⟩
   rw [Fintype.card_units, card_Zx, Int.coe_nat_div]
-  simp only [Nat.cast_sub h1qd, Nat.cast_sub h1qn, Nat.cast_one, Nat.cast_pow]
+  rw [Nat.cast_sub h1qd, Nat.cast_sub h1qn]
+  simp (config := {zeta := false}) only [Nat.cast_one, Nat.cast_pow]
   suffices hd : d < n
   · apply Int.dvd_div_of_mul_dvd
     have aux : ∀ {k : ℕ}, ((X : ℤ[X]) ^ k - 1).eval ↑q = (q : ℤ) ^ k - 1 := by
-      simp only [eval_X, eval_one, eval_pow, eval_sub, eq_self_iff_true, forall_const]
+      simp (config := {zeta := false}) only [eval_X, eval_one, eval_pow, eval_sub, eq_self_iff_true, forall_const]
     rw [← aux, ← aux, ← eval_mul]
     refine' (evalRingHom ↑q).map_dvd (X_pow_sub_one_mul_cyclotomic_dvd_X_pow_sub_one_of_dvd ℤ _)
     exact Nat.mem_properDivisors.mpr ⟨hdn, hd⟩
-  rw [← (Nat.pow_right_strictMono hq).lt_iff_lt]
-  dsimp
-  rw [← card_D, ← card_Zx]
+  rw [← (Nat.pow_right_strictMono hq).lt_iff_lt, ← card_D, ← card_Zx]
   obtain ⟨b, -, hb⟩ := SetLike.exists_of_lt hZx
   refine' Fintype.card_lt_of_injective_of_not_mem _ Subtype.val_injective (_ : b ∉ _)
-  simp only [not_exists, Set.mem_range]
+  simp (config := {zeta := false}) only [not_exists, Set.mem_range]
   rintro y rfl
   exact hb y.2
 
