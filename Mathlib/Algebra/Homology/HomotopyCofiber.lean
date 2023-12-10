5fbb7f5425eda5faf1ae3c9f5a2a5570b3b6fdd6
Joël Riou <joel.riou@universite-paris-saclay.fr>
2023-12-10 19:17:43 +0100

    renamed the file

--- Mathlib/Algebra/Homology/HomotopyCofiber.lean
+++ Mathlib/Algebra/Homology/HomotopyCofiber.lean
@@ -3,7 +3,7 @@ Copyright (c) 2023 Joël Riou. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joël Riou
 -/
-import Mathlib.Algebra.Homology.HomotopyCategory.HomComplex
+import Mathlib.Algebra.Homology.Homotopy
 
 /-! The homotopy cofiber of a morphism of homological complexes
 
