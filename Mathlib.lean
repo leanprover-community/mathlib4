cef4ebdedb91760a2defce90c0a922bdc804f81c
dagurtomas <dagurtomas@gmail.com>
2023-12-10 22:22:18 +0000

    typo

--- Mathlib.lean
+++ Mathlib.lean
@@ -1392,7 +1392,7 @@ import Mathlib.Condensed.Light.Abelian
 import Mathlib.Condensed.Light.Adjunctions
 import Mathlib.Condensed.Light.Basic
 import Mathlib.Condensed.Light.Discrete
-import Mathlib.Condensed.Light.Functor
+import Mathlib.Condensed.Light.Functors
 import Mathlib.Condensed.Limits
 import Mathlib.Condensed.Solid
 import Mathlib.Control.Applicative
