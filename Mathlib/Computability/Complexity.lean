4200a16d851635acf1958c87755ae433f7eb8cb5
Hunter Monroe <github.com@huntermonroe.com>
2023-12-11 17:52:22 -0500

    For review

--- Mathlib/Computability/Complexity.lean
+++ Mathlib/Computability/Complexity.lean
@@ -3,23 +3,21 @@ Copyright (c) 2023 Hunter Monroe. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Hunter Monroe (and others to be added)
 -/
-import Mathlib.Computability.Encoding
 import Mathlib.Computability.TMComputable
-import Mathlib.Computability.Language
-import Mathlib.Data.Polynomial.Eval
 
 /-!
 # Complexity
 
-This file defines the concepts in complexity theory of polynomial-time computability,
-nondeterministic polynomial time (NP) computation (defined using efficiently verifiable
-certificates), polynomial time reductions, and NP completeness and hardness.
+This file defines concepts from complexity theory related to polynomial-time computability on
+Turing machines (TMs), nondeterministic polynomial time (NP) computation (defined using efficiently
+verifiable certificates), polynomial time reductions, and NP completeness and hardness.
 
-Key results remain to be filled in: existence of a univeral Turing machine (TM); halting
-problem; and Rice's Theorem (`Computability.Partrec` provide ); Cook-Levin Theorem; definition
-of space bounded computation; equivalence of resource bounds across machine models
-(`TM0`, `TM1`, and `TM2` in `Computability.TuringMachine`); and complexity bounds for functions in
-mathlib with an acceptable encoding. An extensive set of TODO items are in comments below.
+The file focuses on definitions. Key results remain to be filled in: computability including
+existence of a univeral TM, halting problem, and Rice's Theorem (`Computability.Partrec` provide );
+Cook-Levin Theorem; definition of space bounded computation; equivalence of resource bounds across
+machine models (`TM0`, `TM1`, and `TM2` in `Computability.TuringMachine`); and complexity bounds
+for functions in mathlib with an acceptable encoding. An extensive set of TODO items are in
+comments below.
 
 ## Main Definitions
 
@@ -43,18 +41,12 @@ open Computability
 
 namespace Turing
 
-/-- Defines polynomial time function of a type, assuming an acceptable coding the input type e.g.
-not unary -/
-def isPolyTimeFunctionByType {α β: Type} {ea : FinEncoding α} {eb : FinEncoding β}
-    (f : α → β) : Prop := Nonempty (@TM2ComputableInPolyTime α β ea eb f)
--- TODO: Time complexity for other types than BitStrings: ℕ, Lists, Graphs, with required use of
--- Acceptable coding. Time complexity of number functions throughout mathlib
-
 /-- Defines polynomial time functions on binary strings to binary strings -/
 def isPolyTimeTM (tm : BitString → BitString) : Prop := Nonempty (@TM2ComputableInPolyTime
     BitString BitString finEncodingBitString finEncodingBitString tm)
 -- TODO: Time complexity measures for TM0 and TM1. Equivalence of TM1 and TM2 polynomial time
 -- TODO: Space complexity measures for TM0-2. Relation to time measures. PSPACE. NL=coNL
+-- TODO: Time complexity for other types than BitStrings: ℕ, Lists, Graphs
 
 /-- Complexity class FP: the set of functions computable in polynomial time -/
 def PolyTimeTMs : Set (BitString → BitString) := {tm | isPolyTimeTM tm}
@@ -70,7 +62,7 @@ def PolyTimeLanguage (tm : PolyTimeTMs) : Set BitString := L_M tm
 /-- Complexity class P: languages accepted by polynomial time TMs -/
 def PolyTimeLanguages : Set (Set BitString) := {L_M tm | tm : PolyTimeTMs}
 
-/-- A verifier for a nondeterministic polynomial time language -/
+/-- A verifier for a nondeterministic polynomial time (NP) language -/
 def isVerifier (tm : PolyTimeTMs) : Prop :=
   ∃ (p: Polynomial ℕ), ∀ (x certificate : BitString), (Bpair x certificate) ∈ L_M tm →
     certificate.length < p.eval x.length
@@ -84,29 +76,31 @@ def NPLanguage (tm : verifiers) : Set BitString :=
 
 /-- Complexity class NP: languages accepted in nondeterministic polynomial time -/
 def NPLanguages : Set (Set BitString) := {NPLanguage tm| tm : verifiers}
--- TODO: Other definitions of NP and their equivalance
 
-/-- Polynomial time reduction (also known as Karp-reducible, many-to-one reducible, or mapping
-reducible) -/
+/-- Polynomial time reduction (also known as Karp, many-to-one, or mapping) of language L1 to L2 -/
 def PolyTimeReduction (tm : BitString → BitString) (L1 L2 : Set BitString) : Prop :=
 tm ∈ PolyTimeTMs ∧ ∀ (x: BitString), x∈L1 ↔ tm x ∈ L2
--- TODO: transitivity of PolyTimeReductions
 
 /-- A language is NP hard if any NP language is reducible to it -/
-def NPHard (L1 : Set BitString): Prop :=
-  ∀ (L2 : NPLanguages), ∃ (tm : PolyTimeTMs), PolyTimeReduction tm L1 L2
--- If NP hard language is in P, then P=NP
+def NPHard (L2 : Set BitString): Prop :=
+  ∀ (L1 : NPLanguages), ∃ (tm : PolyTimeTMs), PolyTimeReduction tm L1 L2
 
 /-- A language is NP complete if it is NP hard and is an NP language -/
 def NPComplete (L1 : Set BitString) : Prop := NPHard L1 ∧ L1 ∈ NPLanguages
 
+-- TODO: existence of universal TM, undecidability of halting; Rice's Theorem
+-- TODO: Other definitions of NP and their equivalance
+-- TODO: transitivity of PolyTimeReductions
+-- TODO: If NP hard language is in P, then P=NP
 -- TODO: An NP complete language is in P iff P=NP
 -- TODO: Cook-Levin thm drawing on universal TM and bounded halting and THEOREMS as NP complete
 -- TODO: NP completeness for other specific languages especially 3SAT
+-- TODO: equivalence of complexity class P across machine models
 -- TODO: Decision vs search
 -- TODO: Deterministic and nondeterministic time hierarchy, space hierarchy, nonuniform
 -- TODO: NP intermediate (Ladner)
 -- TODO: BGS contrary relativizations
+-- TODO: Space-bounded computation
 -- TODO: EXP≠NEXP implies P≠NP
 -- TODO: Circuit complexity, P/poly. Shannon bound
 -- TODO: coNP-completeness of TAUT. NP≠coNP imples P≠NP
