47057a203c248268095ef13acd54158b2fc681c8
Yaël Dillies <yael.dillies@gmail.com>
2023-12-12 10:27:39 +0000

    cleanup

--- Mathlib/Topology/Algebra/Order/IccExtendFromIoo.lean
+++ Mathlib/Topology/Algebra/Order/IccExtendFromIoo.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Wen Yang
 -/
 import Mathlib.Data.Set.Lattice
-import Mathlib.Tactic.DefEqTransformations
 
 /-!
 # Extend the domain of f from an open interval to the closed interval
@@ -22,54 +21,35 @@ variable {α β : Type*} {f : α → β} [DecidableEq α]
 section update
 variable {s : Set α} {a : α} {b : β}
 
-/-- Modifying the value of `f` at one point does not affect its value elsewhere.​-/
-theorem Function.update.EqOn (f : α → β) (ha : a ∉ s) : EqOn (update f a b) f s := by
-  intro x hx
-  unfold update
-  simp only [eq_rec_constant, dite_eq_ite]
-  have : x ≠ a := ne_of_mem_of_not_mem hx ha
-  aesop
-
-theorem Function.update.image (f : α → β) (ha : a ∉ s) :
-    (update f a b) '' (s ∪ {a}) = f '' s ∪ {b} := by
-  calc
-    _ = (update f a b) '' s ∪ (update f a b) '' {a} := image_union (update f a b) s {a}
-    _ = (update f a b) '' s ∪ {b} := by simp
-    _ = f '' s ∪ {b} := by simp only [(update.EqOn f ha).image_eq]
+lemma Function.update.image (f : α → β) (ha : a ∉ s) :
+    update f a b '' (s ∪ {a}) = f '' s ∪ {b} := by
+  simp [Set.image_insert_eq, (update_eqOn ha).image_eq]
 
 /-- If `a` is a strict upper bound of `s`,
 `b` is a strict upper bound of `f(s)`,
 and `f` is strictly monotone (increasing) on `s`,
 then `f` can be extended to be strictly monotone (increasing) on `s ∪ {a}`.-/
-theorem StrictMonoOn.update_strict_upper_bound  [PartialOrder α] [Preorder β]
+lemma StrictMonoOn.update_strict_upper_bound  [PartialOrder α] [Preorder β]
     (hf_mono : StrictMonoOn f s) (hf_mapsto : f '' s ⊆ Iio b)
     (ha : ∀ x ∈ s, x < a) :
     StrictMonoOn (update f a b) (s ∪ {a}) := by
-  unfold update
-  simp only [eq_rec_constant, dite_eq_ite, union_singleton]
+  simp only [update, dite_eq_ite, union_singleton]
   intro x hx y hy hxy
-  simp only
   have hxa : x ≠ a := by
-    by_contra' hxa
-    rw [hxa] at hxy
-    cases hy with
-    | inl h => rw [h] at hxy; exact hxy.false
-    | inr h => exact (hxy.trans (ha y h)).false
-  by_cases hya : y = a
-  aesop
-  aesop
+    rintro rfl
+    obtain rfl | hy := hy
+    · exact hxy.false
+    · exact hxy.asymm $ ha _ hy
+  by_cases hya : y = a <;> aesop
 
 /-- If `a` is a strict lower bound of `s`,
 `b` is a strict lower bound of `f(s)`,
 and `f` is strictly antitone (decreasing) on `s`,
 then `f` can be extended to be strictly antitone (decreasing) on `s ∪ {a}`.-/
-theorem StrictMonoOn.update_strict_lower_bound [PartialOrder α] [Preorder β]
+lemma StrictMonoOn.update_strict_lower_bound [PartialOrder α] [Preorder β]
     (hf_mono : StrictMonoOn f s) (hf_mapsto : f '' s ⊆ Ioi b)
     (ha : ∀ x ∈ s, a < x) :
-    StrictMonoOn (update f a b) (s ∪ {a}) := by
-  let g : OrderDual α → OrderDual β := f
-  have hg_mono : StrictMonoOn g s := strict_mono_on_dual_iff.mp hf_mono
-  have := hg_mono.update_strict_upper_bound hf_mapsto ha
-  exact strict_mono_on_dual_iff.mp this
+    StrictMonoOn (update f a b) (s ∪ {a}) :=
+  strictMonoOn_dual_iff.2 (hf_mono.dual.update_strict_upper_bound hf_mapsto ha)
 
 end update
