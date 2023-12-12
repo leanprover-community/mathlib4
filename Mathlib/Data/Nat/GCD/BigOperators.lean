c6d6cad70acb66cb7008522c7111a931aaf69102
jeh <hodellurt@gmail.com>
2023-12-12 15:51:40 +0000

    add: coprime_xxx_left_iff, coprime_xxx_right_iff

--- Mathlib/Data/Nat/GCD/BigOperators.lean
+++ Mathlib/Data/Nat/GCD/BigOperators.lean
@@ -30,4 +30,38 @@ theorem coprime_prod_right {ι : Type*} {x : ℕ} {s : ι → ℕ} {t : Finset �
   Finset.prod_induction s (fun y ↦ x.Coprime y) (fun a b ↦ Coprime.mul_right) (by simp)
 #align nat.coprime_prod_right Nat.coprime_prod_right
 
+theorem coprime_prod_left_iff {ι : Type*} [DecidableEq ι] {x : ℕ} {s : ι → ℕ} {t : Finset ι} :
+    Coprime (∏ i : ι in t, s i) x ↔ (∀ i : ι, i ∈ t → Coprime (s i) x) :=
+  Finset.induction_on t (by simp) fun i s his ih ↦ by
+    rw[Finset.prod_insert his, Nat.coprime_mul_iff_left, ih, Finset.forall_mem_insert]
+
+theorem coprime_prod_right_iff {ι : Type*} [DecidableEq ι] {x : ℕ} {s : ι → ℕ} {t : Finset ι} :
+    Coprime x (∏ i : ι in t, s i) ↔ (∀ i : ι, i ∈ t → Coprime x (s i)) :=
+  Finset.induction_on t (by simp) fun i s his ih ↦ by
+    rw[Finset.prod_insert his, Nat.coprime_mul_iff_right, ih, Finset.forall_mem_insert]
+
+theorem coprime_fintype_prod_left_iff {ι : Type*} [Fintype ι] [DecidableEq ι] {x : ℕ} {s : ι → ℕ} :
+    Coprime (∏ i, s i) x ↔ (∀ i : ι, Coprime (s i) x) := by
+  simp[coprime_prod_left_iff]
+
+theorem coprime_fintype_prod_right_iff {ι : Type*} [Fintype ι] [DecidableEq ι]
+    {x : ℕ} {s : ι → ℕ} : Coprime x (∏ i, s i) ↔ (∀ i : ι, Coprime x (s i)) := by
+  simp[coprime_prod_right_iff]
+
+lemma coprime_list_prod_left_iff {k} {l : List ℕ} :
+    Coprime l.prod k ↔ ∀ n ∈ l, Coprime n k := by
+  induction l <;> simp[Nat.coprime_mul_iff_left, *]
+
+lemma coprime_list_prod_right_iff {k} {l : List ℕ} :
+    Coprime k l.prod ↔ ∀ n ∈ l, Coprime k n := by
+  induction l <;> simp[Nat.coprime_mul_iff_right, *]
+
+lemma coprime_multiset_prod_left_iff {k} {m : Multiset ℕ} :
+    Coprime m.prod k ↔ ∀ n ∈ m, Coprime n k := by
+  induction m using Multiset.induction <;> simp[Nat.coprime_mul_iff_left, *]
+
+lemma coprime_multiset_prod_right_iff {k} {m : Multiset ℕ} :
+    Coprime k m.prod ↔ ∀ n ∈ m, Coprime k n := by
+  induction m using Multiset.induction <;> simp[Nat.coprime_mul_iff_right, *]
+
 end Nat
