38341a89c5b9925176bfc9db1ac9c3223f4ea531
Jireh Loreaux <loreaujy@gmail.com>
2023-12-11 23:10:08 -0600

    `←` space
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Mathlib/Data/Nat/PrimeFin.lean
+++ Mathlib/Data/Nat/PrimeFin.lean
@@ -69,7 +69,7 @@ lemma le_of_mem_primeFactors (h : p ∈ n.primeFactors) : p ≤ n :=
 
 @[simp]
 lemma Nat.nonempty_primeFactors {n : ℕ} : n.primeFactors.Nonempty ↔ 1 < n := by
-  rw [←not_iff_not]
+  rw [← not_iff_not]
   simp only [Finset.not_nonempty_iff_eq_empty, primeFactors_eq_empty, not_lt]
   match n with
   | 0 => simp
