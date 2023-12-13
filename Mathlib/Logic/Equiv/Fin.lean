be5d358ad86c1f148078d650c7d05a6451726fee
Bolton Bailey <bolton.bailey@gmail.com>
2023-12-13 16:57:16 -0600

    lemmas

--- Mathlib/Logic/Equiv/Fin.lean
+++ Mathlib/Logic/Equiv/Fin.lean
@@ -314,16 +314,20 @@ def Equiv.piFinSucc (n : ℕ) (β : Type u) : (Fin (n + 1) → β) ≃ β × (Fi
 #align equiv.pi_fin_succ_apply Equiv.piFinSucc_apply
 #align equiv.pi_fin_succ_symm_apply Equiv.piFinSucc_symm_apply
 
+@[simps! (config := .asFn)]
 def Equiv.piFinCastSucc (n : ℕ) (β : Type u) : (Fin (n + 1) → β) ≃ β × (Fin n → β) :=
   Equiv.piFinSuccAbove (fun _ => β) (.last _)
 
-lemma Equiv.piFinCastSucc_apply (n : ℕ) (β : Type u) :
-    ⇑(Equiv.piFinCastSucc n β) = fun f ↦ (f (.last _), fun j ↦ f (Fin.castSucc j)) := by
-  simp [Equiv.piFinCastSucc]
+#check Equiv.piFinCastSucc_apply
+#check Equiv.piFinCastSucc_symm_apply
 
-lemma Equiv.piFinCastSucc_symm_apply (n : ℕ) (β : Type u) :
-    ⇑(Equiv.piFinCastSucc n β).symm = fun f ↦ Fin.snoc f.2 f.1 := by
-  simp [Equiv.piFinCastSucc]
+-- lemma Equiv.piFinCastSucc_apply (n : ℕ) (β : Type u) :
+--     ⇑(Equiv.piFinCastSucc n β) = fun f ↦ (f (.last _), fun j ↦ f (Fin.castSucc j)) := by
+--   simp [Equiv.piFinCastSucc]
+
+-- lemma Equiv.piFinCastSucc_symm_apply (n : ℕ) (β : Type u) :
+--     ⇑(Equiv.piFinCastSucc n β).symm = fun f ↦ Fin.snoc f.2 f.1 := by
+--   simp [Equiv.piFinCastSucc]
 
 /-- Equivalence between `Fin m ⊕ Fin n` and `Fin (m + n)` -/
 def finSumFinEquiv : Sum (Fin m) (Fin n) ≃ Fin (m + n)
