9d55716c0c05ba318cd0b0d76029aa931bb58b97
Floris van Doorn <fpvdoorn@gmail.com>
2023-12-13 14:08:57 +0100

    cleanup, one more todo

--- Mathlib/Tactic/Show.lean
+++ Mathlib/Tactic/Show.lean
@@ -1,53 +1,28 @@
 import Mathlib.Geometry.Manifold.SmoothManifoldWithCorners
--- todo: clean up a lot
 
 open Lean Meta Elab Command PrettyPrinter Delaborator Parser.Command
-open Lean.Meta
-open Lean.Parser.Term
-open SubExpr
-open TSyntax.Compat
+open Parser.Term SubExpr TSyntax.Compat
 
-/- similar to `Lean.PrettyPrinter.Delaborator.shouldGroupWithNext` (which is private),
-that also works if we just have an array of arguments we want to print (that is not necessarily
-part of a sequence of lambda's/pi's from an expression). -/
-def shouldGroupWithNext (bis : Array Expr) (k : Nat) : DelabM Bool := do
-  if h : k + 1 < bis.size then
-    let ppEType ← getPPOption getPPPiBinderTypes
-    let fvarId := bis[k]!.fvarId!
-    let fvarId' := bis[k+1].fvarId!
-    return (← fvarId.getBinderInfo) == (← fvarId'.getBinderInfo) &&
-      (← fvarId.getType) == (← fvarId'.getType) &&
-      (← fvarId.getBinderInfo) != BinderInfo.instImplicit &&
-      ((← fvarId.getBinderInfo) != BinderInfo.default || ppEType)
-  else
-    return false
+open private shouldGroupWithNext in delabConstWithSignature.delabParams
 
-/-- `info` optionally contains the identifier and type. -/
-/- similar to `Lean.PrettyPrinter.Delaborator.delabConstWithSignature.delabParams`
-but doesn't print the identifier `idStx`.
--/
-partial def delabParamsImpl (groups : TSyntaxArray ``bracketedBinder)
-    (curIds : Array Ident) (bis : Array Expr) (k : Nat) : Delab := do
-  if h : k < bis.size then
-    let fvarId := bis[k].fvarId!
-    let n ← fvarId.getUserName
-    let d ← fvarId.getType
-    let i ← fvarId.getBinderInfo
+namespace Std.Command.Show
+
+/-- Almost the same as `Lean.PrettyPrinter.Delaborator.delabConstWithSignature.delabParams`
+but with the option to not print the type or identifier -/
+partial def delabParamsImpl (idStx? : Option Ident) (groups : TSyntaxArray ``bracketedBinder)
+    (curIds : Array Ident) := do
+  if let .forallE n d _ i ← getExpr then
     let stxN ← annotateCurPos (mkIdent n)
-    let curIds := curIds.push ⟨mkIdent n⟩
-    if ← shouldGroupWithNext bis k then
-      delabParamsImpl groups curIds bis (k+1)
+    let curIds := curIds.push ⟨stxN⟩
+    if ← shouldGroupWithNext then
+      withBindingBody n <| delabParamsImpl idStx? groups curIds
     else
-      let delabTy := withOptions (pp.piBinderTypes.set · true) <| delab d
-      let group ← do
+      let delabTy := withOptions (pp.piBinderTypes.set · true) delab
+      let group ← withBindingDomain do
         match i with
         | .implicit       => `(bracketedBinderF|{$curIds* : $(← delabTy)})
         | .strictImplicit => `(bracketedBinderF|⦃$curIds* : $(← delabTy)⦄)
-        | .instImplicit   =>
-          if n.hasMacroScopes then
-            `(bracketedBinderF|[$(← delabTy)])
-          else
-            `(bracketedBinderF|[$curIds.back : $(← delabTy)])
+        | .instImplicit   => `(bracketedBinderF|[$curIds.back : $(← delabTy)])
         | _ =>
           if d.isOptParam then
             `(bracketedBinderF|($curIds* : $(← withAppFn <| withAppArg delabTy) := $(← withAppArg delabTy)))
@@ -56,70 +31,76 @@ partial def delabParamsImpl (groups : TSyntaxArray ``bracketedBinder)
             `(bracketedBinderF|($curIds* : $(← withAppFn <| withAppArg delabTy) := by $tacticSyntax))
           else
             `(bracketedBinderF|($curIds* : $(← delabTy)))
-      delabParamsImpl (groups.push group) #[] bis (k+1)
+      withBindingBody n <| delabParamsImpl idStx? (groups.push group) #[]
   else
-    `(optDeclSig| $groups*)
+    if let some idStx := idStx? then
+      let type ← delab
+      `(declSigWithId| $idStx:ident $groups* : $type)
+    else
+      `(optDeclSig| $groups*)
 
-def delabParams (bis : Array Expr) : MetaM FormatWithInfos := do
-  let (stx, infos) ← delabCore default (delab := delabParamsImpl #[] #[] bis 0)
-  -- delabParamsImpl info #[] #[] bis 0
-  --   { optionsPerPos := .empty
-  --     currNamespace := (← getCurrNamespace)
-  --     openDecls := (← getOpenDecls)
-  --     subExpr := ⟨default, Pos.root⟩
-  --     inPattern := (← getOptions).getInPattern } |>.run {}
+/-- Pretty-prints a constant `c` as `c <params> : <type>`.
+Here `type` need not be the actual type of `c`.
+If `c` is `none`, then only print `<params>`.
+This is similar to `delabConstWithSignature`, but with an explicit type `eType` given,
+and the flexibility to not print `c` and `<type>`. -/
+def delabWithType (e : Option Expr) (eType : Expr) : MetaM FormatWithInfos := do
+  let (stx, infos) ← delabCore default (delab := delabConstOfTypeWithSignature e eType)
   return ⟨← ppTerm ⟨stx⟩, infos⟩
+where
+  /- Delaborator for `delabWithType`. -/
+  delabConstOfTypeWithSignature (c : Option Expr) (type : Expr) : Delab := do
+    -- use virtual expression node of arity 2 to separate name and type info
+    let idStx ← c.mapM (descend · 0 <| withOptions (pp.fullNames.set · true) delabConst)
+    descend type 1 <| delabParamsImpl idStx #[] #[]
 
-/-- Pretty-prints a constant `c` as `c.{<levels>} <explicit params> : <type>`. -/
--- similar to `delabConstWithSignature`
-partial def delabConstOfTypeWithSignature (eType : Expr) : Delab := do
-  let e ← getExpr
-  -- use virtual expression node of arity 2 to separate name and type info
-  let idStx ← descend e 0 <|
-    withOptions (/-pp.universes.set · true |>-/ (pp.fullNames.set · true)) delabConst
-  descend eType 1 <|
-    delabConstWithSignature.delabParams idStx #[] #[]
-
-def delabExplicits (eValue e : Expr) : MetaM FormatWithInfos := do
-  let (stx, infos) ← delabCore eValue (delab := delabConstOfTypeWithSignature e)
-  return ⟨← ppTerm ⟨stx⟩, infos⟩
-
+/-- Turn a `MetaM FormatWithInfos` into a `MessageData`. -/
 def ofFormatWithInfos (t : MetaM FormatWithInfos) : MessageData :=
 .ofPPFormat { pp := fun
   | some ctx => ctx.runMetaM t
   | none     => pure "" }
 
-syntax (name := myshow) "#show" term : command
+/- Returns a pretty-printed version of the type of `e`,
+but only printing the arguments satisfying `p`.
+If `showName` is false, then the name and the conclusion are not printed.
+-/
+def printFilteredDecl (e : Expr) (p : Expr → MetaM Bool) (showName := true) :
+    MetaM (Option MessageData) := do
+  forallTelescope (← inferType e) fun bis t => do
+    let args ← bis.filterM p
+    if args.isEmpty && !showName then
+      return none
+    let filteredType ← mkForallFVars args t
+    return  m!"{ofFormatWithInfos <| delabWithType (if showName then e else none) filteredType}"
 
-def showCmd (eValue : Expr) : MetaM MessageData := do
-  let e ← inferType eValue
-  forallTelescope e fun bis t => do
-    let explicits ← bis.filterM fun bi => return (← bi.fvarId!.getBinderInfo) == BinderInfo.default
-    let implicits ← bis.filterM fun bi =>
-      return [BinderInfo.implicit, BinderInfo.strictImplicit].contains (← bi.fvarId!.getBinderInfo)
-    let insts ← bis.filterM fun bi => return (← bi.fvarId!.getBinderInfo) == BinderInfo.instImplicit
-    let explicittype ← mkForallFVars explicits t
-    let explicitMsg : MessageData :=
-      m!"{ofFormatWithInfos <| delabExplicits eValue explicittype}"
-    let instMsg : MessageData :=
-      if insts.isEmpty then "" else m!"\ninstances:{ofFormatWithInfos <| delabParams insts}"
-    let implicitMsg : MessageData :=
-      if implicits.isEmpty then "" else m!"\nimplicits:{ofFormatWithInfos <| delabParams implicits}"
-    logInfo m!"{explicitMsg}{instMsg}{implicitMsg}"
-    return ""
+/-- `print sep m?` prints `m?` if it is not `none`, with separator `sep`. -/
+def print : MessageData → Option MessageData → MessageData
+  | _, none => ""
+  | sep, some msg => sep ++ msg
 
-elab "#show " stx:term : command =>
+/-- The implementation for the `#show` command. `e` must be a constant. -/
+def showCmd (e : Expr) : MetaM MessageData := do
+  let some explicitMsg ← printFilteredDecl e
+    fun bi => return (← bi.fvarId!.getBinderInfo) == BinderInfo.default | unreachable!
+  let instMsg ← printFilteredDecl e (showName := false)
+    fun bi => return (← bi.fvarId!.getBinderInfo) == BinderInfo.instImplicit
+  let implicitMsg ← printFilteredDecl e (showName := false) fun bi =>
+    return [BinderInfo.implicit, BinderInfo.strictImplicit].contains (← bi.fvarId!.getBinderInfo)
+  return explicitMsg ++ print "\ninstances: " instMsg ++ print "\nimplicits: " implicitMsg
+
+/- The `#show <id>` prints the identifier `<id>` in a similar way to `#check <id>`,
+but separates the arguments into explicit, instance and implicit arguments,
+so that the most important information is more easily seen at a glance. -/
+elab "#show " stx:ident : command =>
   withoutModifyingEnv <| runTermElabM fun _ => Term.withDeclName `_show do
-    -- show signature for `#show id`/`#show @id`
-    if let `($_:ident) := stx then
-      try
-        for c in (← resolveGlobalConstWithInfos stx) do
-          addCompletionInfo <| .id stx c (danglingDot := false) {} none
-          let decl ← getConstInfo c
-          let e := .const c (decl.levelParams.map mkLevelParam)
-          logInfo m!"{← showCmd e}"
-          return
-      catch _ => pure ()  -- identifier might not be a constant but constant + projection
+    try
+      for c in (← resolveGlobalConstWithInfos stx) do
+        addCompletionInfo <| .id stx c (danglingDot := false) {} none
+        let decl ← getConstInfo c
+        let e := .const c (decl.levelParams.map mkLevelParam)
+        logInfo m!"{← showCmd e}"
+        return
+    catch _ => pure ()  -- identifier might not be a constant but constant + projection
     let e ← Term.elabTerm stx none
     Term.synthesizeSyntheticMVarsNoPostponing (ignoreStuckTC := true)
     let e ← Term.levelMVarToParam (← instantiateMVars e)
@@ -127,11 +108,14 @@ elab "#show " stx:term : command =>
       return
     logInfo m!"{← showCmd e}"
 
--- todo: mouseover on (inst)implicits doesn't work yet
+-- todo: don't show instance names. I think it worked in a previous commit.
 
 #show smoothManifoldWithCorners_of_contDiffOn
 #show extChartAt_source_mem_nhdsWithin
 #show ContDiff.of_le
 #show Lean.Elab.Term.synthesizeSyntheticMVarsNoPostponing
-#check ContDiff.of_le
-#check ContDiff.of_le
+-- #show 1 + 1
+-- #show Prod.mk
+-- #check ContDiff.of_le
+
+end Std.Command.Show
