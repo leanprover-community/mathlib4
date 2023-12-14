f3885a1eb3f51b0b59893dad831a20d1aa7aa5a0
Yury G. Kudryashov <urkud@urkud.name>
2023-12-14 16:55:08 -0600

    feat(Set/Lattice): add `Set.seq_eq_image2`, use it to golf some proofs

--- Mathlib/Data/Set/Lattice.lean
+++ Mathlib/Data/Set/Lattice.lean
@@ -2002,16 +2002,17 @@ section Image2
 
 variable (f : α → β → γ) {s : Set α} {t : Set β}
 
+/-- The `Set.image2` version of `Set.image_eq_iUnion` -/
+theorem image2_eq_iUnion (s : Set α) (t : Set β) : image2 f s t = ⋃ (i ∈ s) (j ∈ t), {f i j} := by
+  ext; simp [eq_comm]
+#align set.image2_eq_Union Set.image2_eq_iUnion
+
 theorem iUnion_image_left : ⋃ a ∈ s, f a '' t = image2 f s t := by
-  ext y
-  constructor <;> simp only [mem_iUnion] <;> rintro ⟨a, ha, x, hx, ax⟩ <;> exact ⟨a, x, ha, hx, ax⟩
+  simp only [image2_eq_iUnion, image_eq_iUnion]
 #align set.Union_image_left Set.iUnion_image_left
 
-theorem iUnion_image_right : ⋃ b ∈ t, (fun a => f a b) '' s = image2 f s t := by
-  ext y
-  constructor <;> simp only [mem_iUnion] <;> rintro ⟨a, b, c, d, e⟩
-  exact ⟨c, a, d, b, e⟩
-  exact ⟨b, d, a, c, e⟩
+theorem iUnion_image_right : ⋃ b ∈ t, (f · b) '' s = image2 f s t := by
+  rw [image2_swap, iUnion_image_left]
 #align set.Union_image_right Set.iUnion_image_right
 
 theorem image2_iUnion_left (s : ι → Set α) (t : Set β) :
@@ -2024,14 +2025,10 @@ theorem image2_iUnion_right (s : Set α) (t : ι → Set β) :
   simp only [← image_prod, prod_iUnion, image_iUnion]
 #align set.image2_Union_right Set.image2_iUnion_right
 
-/- ./././Mathport/Syntax/Translate/Expr.lean:107:6: warning: expanding binder group (i j) -/
-/- ./././Mathport/Syntax/Translate/Expr.lean:107:6: warning: expanding binder group (i j) -/
 theorem image2_iUnion₂_left (s : ∀ i, κ i → Set α) (t : Set β) :
     image2 f (⋃ (i) (j), s i j) t = ⋃ (i) (j), image2 f (s i j) t := by simp_rw [image2_iUnion_left]
 #align set.image2_Union₂_left Set.image2_iUnion₂_left
 
-/- ./././Mathport/Syntax/Translate/Expr.lean:107:6: warning: expanding binder group (i j) -/
-/- ./././Mathport/Syntax/Translate/Expr.lean:107:6: warning: expanding binder group (i j) -/
 theorem image2_iUnion₂_right (s : Set α) (t : ∀ i, κ i → Set β) :
     image2 f s (⋃ (i) (j), t i j) = ⋃ (i) (j), image2 f s (t i j) :=
   by simp_rw [image2_iUnion_right]
@@ -2049,33 +2046,22 @@ theorem image2_iInter_subset_right (s : Set α) (t : ι → Set β) :
   exact fun x hx y hy i => mem_image2_of_mem hx (hy _)
 #align set.image2_Inter_subset_right Set.image2_iInter_subset_right
 
-/- ./././Mathport/Syntax/Translate/Expr.lean:107:6: warning: expanding binder group (i j) -/
-/- ./././Mathport/Syntax/Translate/Expr.lean:107:6: warning: expanding binder group (i j) -/
 theorem image2_iInter₂_subset_left (s : ∀ i, κ i → Set α) (t : Set β) :
     image2 f (⋂ (i) (j), s i j) t ⊆ ⋂ (i) (j), image2 f (s i j) t := by
   simp_rw [image2_subset_iff, mem_iInter]
   exact fun x hx y hy i j => mem_image2_of_mem (hx _ _) hy
 #align set.image2_Inter₂_subset_left Set.image2_iInter₂_subset_left
 
-/- ./././Mathport/Syntax/Translate/Expr.lean:107:6: warning: expanding binder group (i j) -/
-/- ./././Mathport/Syntax/Translate/Expr.lean:107:6: warning: expanding binder group (i j) -/
 theorem image2_iInter₂_subset_right (s : Set α) (t : ∀ i, κ i → Set β) :
     image2 f s (⋂ (i) (j), t i j) ⊆ ⋂ (i) (j), image2 f s (t i j) := by
   simp_rw [image2_subset_iff, mem_iInter]
   exact fun x hx y hy i j => mem_image2_of_mem hx (hy _ _)
 #align set.image2_Inter₂_subset_right Set.image2_iInter₂_subset_right
 
-/-- The `Set.image2` version of `Set.image_eq_iUnion` -/
-theorem image2_eq_iUnion (s : Set α) (t : Set β) : image2 f s t = ⋃ (i ∈ s) (j ∈ t), {f i j} := by
-  simp_rw [← image_eq_iUnion, iUnion_image_left]
-#align set.image2_eq_Union Set.image2_eq_iUnion
-
-/- ./././Mathport/Syntax/Translate/Expr.lean:177:8: unsupported: ambiguous notation -/
 theorem prod_eq_biUnion_left : s ×ˢ t = ⋃ a ∈ s, (fun b => (a, b)) '' t := by
   rw [iUnion_image_left, image2_mk_eq_prod]
 #align set.prod_eq_bUnion_left Set.prod_eq_biUnion_left
 
-/- ./././Mathport/Syntax/Translate/Expr.lean:177:8: unsupported: ambiguous notation -/
 theorem prod_eq_biUnion_right : s ×ˢ t = ⋃ b ∈ t, (fun a => (a, b)) '' s := by
   rw [iUnion_image_right, image2_mk_eq_prod]
 #align set.prod_eq_bUnion_right Set.prod_eq_biUnion_right
@@ -2090,20 +2076,22 @@ def seq (s : Set (α → β)) (t : Set α) : Set β :=
   { b | ∃ f ∈ s, ∃ a ∈ t, (f : α → β) a = b }
 #align set.seq Set.seq
 
-theorem seq_def {s : Set (α → β)} {t : Set α} : seq s t = ⋃ f ∈ s, f '' t :=
-  Set.ext <| by simp [seq]
-#align set.seq_def Set.seq_def
-
 @[simp]
 theorem mem_seq_iff {s : Set (α → β)} {t : Set α} {b : β} :
     b ∈ seq s t ↔ ∃ f ∈ s, ∃ a ∈ t, (f : α → β) a = b :=
   Iff.rfl
 #align set.mem_seq_iff Set.mem_seq_iff
 
+lemma seq_eq_image2 (s : Set (α → β)) (t : Set α) : seq s t = image2 (fun f a ↦ f a) s t := by
+  ext; simp
+
+theorem seq_def {s : Set (α → β)} {t : Set α} : seq s t = ⋃ f ∈ s, f '' t := by
+  rw [seq_eq_image2, iUnion_image_left]
+#align set.seq_def Set.seq_def
+
 theorem seq_subset {s : Set (α → β)} {t : Set α} {u : Set β} :
-    seq s t ⊆ u ↔ ∀ f ∈ s, ∀ a ∈ t, (f : α → β) a ∈ u :=
-  Iff.intro (fun h f hf a ha => h ⟨f, hf, a, ha, rfl⟩) fun h _ ⟨f, hf, a, ha, eq⟩ =>
-    eq ▸ h f hf a ha
+    seq s t ⊆ u ↔ ∀ f ∈ s, ∀ a ∈ t, (f : α → β) a ∈ u := by
+  rw [seq_eq_image2, image2_subset_iff]
 #align set.seq_subset Set.seq_subset
 
 @[gcongr]
@@ -2111,21 +2099,17 @@ theorem seq_mono {s₀ s₁ : Set (α → β)} {t₀ t₁ : Set α} (hs : s₀ �
     seq s₀ t₀ ⊆ seq s₁ t₁ := fun _ ⟨f, hf, a, ha, eq⟩ => ⟨f, hs hf, a, ht ha, eq⟩
 #align set.seq_mono Set.seq_mono
 
-theorem singleton_seq {f : α → β} {t : Set α} : Set.seq ({f} : Set (α → β)) t = f '' t :=
-  Set.ext <| by simp
+theorem singleton_seq {f : α → β} {t : Set α} : Set.seq ({f} : Set (α → β)) t = f '' t := by
+  rw [seq_eq_image2, image2_singleton_left]
 #align set.singleton_seq Set.singleton_seq
 
-theorem seq_singleton {s : Set (α → β)} {a : α} : Set.seq s {a} = (fun f : α → β => f a) '' s :=
-  Set.ext <| by simp
+theorem seq_singleton {s : Set (α → β)} {a : α} : Set.seq s {a} = (fun f : α → β => f a) '' s := by
+  rw [seq_eq_image2, image2_singleton_right]
 #align set.seq_singleton Set.seq_singleton
 
 theorem seq_seq {s : Set (β → γ)} {t : Set (α → β)} {u : Set α} :
     seq s (seq t u) = seq (seq ((· ∘ ·) '' s) t) u := by
-  refine' Set.ext fun c => Iff.intro _ _
-  · rintro ⟨f, hfs, b, ⟨g, hg, a, hau, rfl⟩, rfl⟩
-    exact ⟨f ∘ g, ⟨(· ∘ ·) f, mem_image_of_mem _ hfs, g, hg, rfl⟩, a, hau, rfl⟩
-  · rintro ⟨fg, ⟨fc, ⟨f, hfs, rfl⟩, g, hgt, rfl⟩, a, ha, rfl⟩
-    exact ⟨f, hfs, g a, ⟨g, hgt, a, ha, rfl⟩, rfl⟩
+  simp only [seq_eq_image2, image2_image_left, image2_image2_left, image2_image2_right, comp_apply]
 #align set.seq_seq Set.seq_seq
 
 theorem image_seq {f : β → γ} {s : Set (α → β)} {t : Set α} :
@@ -2133,15 +2117,8 @@ theorem image_seq {f : β → γ} {s : Set (α → β)} {t : Set α} :
   rw [← singleton_seq, ← singleton_seq, seq_seq, image_singleton]
 #align set.image_seq Set.image_seq
 
-/- ./././Mathport/Syntax/Translate/Expr.lean:177:8: unsupported: ambiguous notation -/
 theorem prod_eq_seq {s : Set α} {t : Set β} : s ×ˢ t = (Prod.mk '' s).seq t := by
-  ext ⟨a, b⟩
-  constructor
-  · rintro ⟨ha, hb⟩
-    exact ⟨Prod.mk a, ⟨a, ha, rfl⟩, b, hb, rfl⟩
-  · rintro ⟨f, ⟨x, hx, rfl⟩, y, hy, eq⟩
-    rw [← eq]
-    exact ⟨hx, hy⟩
+  rw [seq_eq_image2, image2_image_left, image2_mk_eq_prod]
 #align set.prod_eq_seq Set.prod_eq_seq
 
 theorem prod_image_seq_comm (s : Set α) (t : Set β) :
@@ -2150,8 +2127,7 @@ theorem prod_image_seq_comm (s : Set α) (t : Set β) :
 #align set.prod_image_seq_comm Set.prod_image_seq_comm
 
 theorem image2_eq_seq (f : α → β → γ) (s : Set α) (t : Set β) : image2 f s t = seq (f '' s) t := by
-  ext
-  simp
+  rw [seq_eq_image2, image2_image_left]
 #align set.image2_eq_seq Set.image2_eq_seq
 
 end Seq
