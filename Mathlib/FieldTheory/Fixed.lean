b9296563038a659c23745d0fed62c024df6becbf
Junyan Xu <junyanxu.math@gmail.com>
2023-12-14 15:15:52 -0500

    fix call site of duplicate lemma

--- Mathlib/FieldTheory/Fixed.lean
+++ Mathlib/FieldTheory/Fixed.lean
@@ -268,7 +268,7 @@ theorem rank_le_card : Module.rank (FixedPoints.subfield G F) F ≤ Fintype.card
   rank_le fun s hs => by
     simpa only [rank_fun', Cardinal.mk_coe_finset, Finset.coe_sort_coe, Cardinal.lift_natCast,
       Cardinal.natCast_le] using
-      cardinal_lift_le_rank_of_linearIndependent'
+      cardinal_lift_le_rank_of_linearIndependent
         (linearIndependent_smul_of_linearIndependent G F hs)
 #align fixed_points.rank_le_card FixedPoints.rank_le_card
 
