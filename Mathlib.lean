3465d1aaae242475fad59e688648a2285031d19e
Andrew Yang <the.erd.one@gmail.com>
2023-12-14 13:12:38 +0000

    feat: Define the dual of a fractional ideal. (#8833)
    Also moved `Algebra.discr_eq_discr_of_toMatrix_coeff_isIntegral` to `Mathlib/NumberTheory/NumberField/Discriminant.lean`.
    
    
    
    Co-authored-by: Andrew Yang <36414270+erdOne@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -3014,6 +3014,7 @@ import Mathlib.RingTheory.Coprime.Ideal
 import Mathlib.RingTheory.Coprime.Lemmas
 import Mathlib.RingTheory.DedekindDomain.AdicValuation
 import Mathlib.RingTheory.DedekindDomain.Basic
+import Mathlib.RingTheory.DedekindDomain.Different
 import Mathlib.RingTheory.DedekindDomain.Dvr
 import Mathlib.RingTheory.DedekindDomain.Factorization
 import Mathlib.RingTheory.DedekindDomain.FiniteAdeleRing
