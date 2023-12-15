ed748fd8932c4644915cef83fcd702370240f9e9
Yury G. Kudryashov <urkud@urkud.name>
2023-12-15 06:44:49 -0600

    Update Mathlib/Order/Filter/Basic.lean
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- Mathlib/Order/Filter/Basic.lean
+++ Mathlib/Order/Filter/Basic.lean
@@ -181,7 +181,7 @@ theorem congr_sets (h : { x | x ∈ s ↔ x ∈ t } ∈ f) : s ∈ f ↔ t ∈ f
 #align filter.congr_sets Filter.congr_sets
 
 /-- Override `sets` field of a filter to provide better definitional equality. -/
-protected def copy (f : Filter α) (S : Set (Set α)) (hmem : ∀ s, s ∈ f ↔ s ∈ S) : Filter α where
+protected def copy (f : Filter α) (S : Set (Set α)) (hmem : ∀ s, s ∈ s ↔ s ∈ f) : Filter α where
   sets := S
   univ_sets := (hmem _).mp univ_mem
   sets_of_superset h hsub := (hmem _).1 <| mem_of_superset ((hmem _).2 h) hsub
