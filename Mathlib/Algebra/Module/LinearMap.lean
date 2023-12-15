f14e5a0406442b384e7ea1c72de6e39e8c7e650e
Junyan Xu <junyanxu.math@gmail.com>
2023-12-15 20:17:22 +0000

    generalize compatibleSMul_ring

--- Mathlib/Algebra/Module/LinearMap.lean
+++ Mathlib/Algebra/Module/LinearMap.lean
@@ -429,11 +429,6 @@ class CompatibleSMul (R S : Type*) [Semiring S] [SMul R M] [Module S M] [SMul R 
 
 variable {M M₂}
 
-instance IsScalarTower.compatibleSMul_ring [Module R S] [Module S M] [IsScalarTower R S M] :
-    CompatibleSMul S M R S where
-  map_smul f r s := by
-    rw [← mul_one (r • s), ← smul_eq_mul, map_smul, smul_assoc, ← map_smul, smul_eq_mul, mul_one]
-
 section
 
 variable {R S : Type*} [Semiring S] [SMul R M] [Module S M] [SMul R M₂] [Module S M₂]
@@ -443,6 +438,11 @@ instance (priority := 100) IsScalarTower.compatibleSMul [SMul R S]
     CompatibleSMul M M₂ R S :=
   ⟨fun fₗ c x ↦ by rw [← smul_one_smul S c x, ← smul_one_smul S c (fₗ x), map_smul]⟩
 #align linear_map.is_scalar_tower.compatible_smul LinearMap.IsScalarTower.compatibleSMul
+
+instance IsScalarTower.compatibleSMul_ring [SMul R S] [IsScalarTower R S M] :
+    CompatibleSMul S M R S where
+  map_smul f r s := by
+    rw [← mul_one (r • s), ← smul_eq_mul, map_smul, smul_assoc, ← map_smul, smul_eq_mul, mul_one]
 
 @[simp]
 theorem map_smul_of_tower [CompatibleSMul M M₂ R S] (fₗ : M →ₗ[S] M₂) (c : R) (x : M) :
