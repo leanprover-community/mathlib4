fcb38fd40677abd2b8d868b0954d66def81e5be9
Winston Yin <winstonyin@gmail.com>
2023-12-15 00:19:58 -0800

    to additive noisy

--- Mathlib/Algebra/Order/Interval.lean
+++ Mathlib/Algebra/Order/Interval.lean
@@ -238,7 +238,7 @@ section Pow
 variable [Monoid α] [Preorder α] [CovariantClass α α (· * ·) (· ≤ ·)]
   [CovariantClass α α (swap (· * ·)) (· ≤ ·)]
 
-@[to_additive existing NonemptyInterval.hasNSMul]
+@[to_additive]
 instance NonemptyInterval.hasPow : Pow (NonemptyInterval α) ℕ :=
   ⟨fun s n => ⟨s.toProd ^ n, pow_le_pow_of_le_left' s.fst_le_snd _⟩⟩
 #align nonempty_interval.has_pow NonemptyInterval.hasPow
