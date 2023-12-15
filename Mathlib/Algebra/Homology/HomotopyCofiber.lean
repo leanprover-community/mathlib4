4cb357f8915992539bf82d1626b416bdef4c217e
Joël Riou <joel.riou@universite-paris-saclay.fr>
2023-12-15 12:47:23 +0100

    cleaning up

--- Mathlib/Algebra/Homology/HomotopyCofiber.lean
+++ Mathlib/Algebra/Homology/HomotopyCofiber.lean
@@ -7,12 +7,12 @@ import Mathlib.Algebra.Homology.Homotopy
 
 /-! The homotopy cofiber of a morphism of homological complexes
 
-In this file, we construct the homotopy cofibre of a morphism `φ : F ⟶ G`
+In this file, we construct the homotopy cofiber of a morphism `φ : F ⟶ G`
 between homological complexes in `HomologicalComplex C c`. In degree `i`,
 it is isomorphic to `(F.X j) ⊞ (G.X i)` if there is a `j` such that `c.Rel i j`,
 and `G.X i` otherwise. (This is also known as the mapping cone of `φ`. Under
 the name `CochainComplex.mappingCone`, a specific API shall be developed
-for the case of cochains complexes indexed by `ℤ` (TODO).)
+for the case of cochain complexes indexed by `ℤ` (TODO).)
 
 When we assume `hc : ∀ j, ∃ i, c.Rel i j` (which holds in the case of chain complexes,
 or cochain complexes indexed by `ℤ`), then for any homological complex `K`,
@@ -20,11 +20,11 @@ there is a bijection `HomologicalComplex.homotopyCofiber.descEquiv φ K hc`
 between `homotopyCofiber φ ⟶ K` and the tuples `(α, hα)` with
 `α : G ⟶ K` and `hα : Homotopy (φ ≫ α) 0` (TODO).
 
-We shall also study the cylinder of an homological complex `K`: this is the
-homotopy cofibre of the morphism  `biprod.lift (𝟙 K) (-𝟙 K) : K ⟶ K ⊞ K`.
+We shall also study the cylinder of a homological complex `K`: this is the
+homotopy cofiber of the morphism  `biprod.lift (𝟙 K) (-𝟙 K) : K ⟶ K ⊞ K`.
 Then, a morphism `K.cylinder ⟶ M` is determined by the data of two
 morphisms `φ₀ φ₁ : K ⟶ M` and a homotopy `h : Homotopy φ₀ φ₁`,
-see `cylinder.desc`. There is also an homotopy equivalence
+see `cylinder.desc`. There is also a homotopy equivalence
 `cylinder.homotopyEquiv K : HomotopyEquiv K.cylinder K`. From the construction of
 the cylinder, we deduce the lemma `Homotopy.map_eq_of_inverts_homotopyEquivalences`
 which assert that if a functor inverts homotopy equivalences, then the image of
@@ -41,20 +41,13 @@ namespace HomologicalComplex
 
 variable {ι : Type*} {c : ComplexShape ι} {F G K : HomologicalComplex C c} (φ : F ⟶ G)
 
-lemma dNext_eq_zero (ho : ∀ i j, F.X i ⟶ G.X j) (i : ι) (hi : ¬ c.Rel i (c.next i)) :
-    dNext i ho = 0 := by
-  dsimp [dNext]
-  rw [shape _ _ _ hi, zero_comp]
-
-lemma prevD_eq_zero (ho : ∀ i j, F.X i ⟶ G.X j) (i : ι) (hi : ¬ c.Rel (c.prev i) i) :
-    prevD i ho = 0 := by
-  dsimp [prevD]
-  rw [shape _ _ _ hi, comp_zero]
-
 /-- A morphism of homological complexes `φ : F ⟶ G` has a homotopy cofiber if for all
 indices `i` and `j` such that `c.Rel i j`, the binary biproduct `F.X j ⊞ G.X i` exists. -/
 class HasHomotopyCofiber (φ : F ⟶ G) : Prop where
   hasBinaryBiproduct (i j : ι) (hij : c.Rel i j) : HasBinaryBiproduct (F.X j) (G.X i)
+
+instance [HasBinaryBiproducts C] : HasHomotopyCofiber φ where
+  hasBinaryBiproduct _ _ _ := inferInstance
 
 variable [HasHomotopyCofiber φ] [DecidableRel c.Rel]
 
@@ -68,19 +61,19 @@ noncomputable def X (i : ι) : C :=
     (F.X (c.next i)) ⊞ (G.X i)
   else G.X i
 
-/-- The canonical isomorphism `(homotopyCofibre φ).X i ≅ F.X j ⊞ G.X i` when `c.Rel i j`. -/
+/-- The canonical isomorphism `(homotopyCofiber φ).X i ≅ F.X j ⊞ G.X i` when `c.Rel i j`. -/
 noncomputable def XIsoBiprod (i j : ι) (hij : c.Rel i j) [HasBinaryBiproduct (F.X j) (G.X i)] :
     X φ i ≅ F.X j ⊞ G.X i :=
   eqToIso (by
     obtain rfl := c.next_eq' hij
     apply dif_pos hij)
 
-/-- The canonical isomorphism `(homotopyCofibre φ).X i ≅ G.X i` when `¬ c.Rel i (c.next i)`. -/
+/-- The canonical isomorphism `(homotopyCofiber φ).X i ≅ G.X i` when `¬ c.Rel i (c.next i)`. -/
 noncomputable def XIso (i : ι) (hi : ¬ c.Rel i (c.next i)) :
     X φ i ≅ G.X i :=
   eqToIso (dif_neg hi)
 
-/-- The second projection `(homotopyCofibre φ).X i ⟶ G.X i`. -/
+/-- The second projection `(homotopyCofiber φ).X i ⟶ G.X i`. -/
 noncomputable def sndX (i : ι) : X φ i ⟶ G.X i :=
   if hi : c.Rel i (c.next i)
   then
@@ -89,7 +82,7 @@ noncomputable def sndX (i : ι) : X φ i ⟶ G.X i :=
   else
     (XIso φ i hi).hom
 
-/-- The right inclusion `G.X i ⟶ (homotopyCofibre φ).X i`. -/
+/-- The right inclusion `G.X i ⟶ (homotopyCofiber φ).X i`. -/
 noncomputable def inrX (i : ι) : G.X i ⟶ X φ i :=
   if hi : c.Rel i (c.next i)
   then
@@ -109,12 +102,12 @@ lemma sndX_inrX (i : ι) (hi : ¬ c.Rel i (c.next i)) :
   dsimp [sndX, inrX]
   simp only [dif_neg hi, Iso.hom_inv_id]
 
-/-- The first projection `(homotopyCofibre φ).X i ⟶ F.X j` when `c.Rel i j`. -/
+/-- The first projection `(homotopyCofiber φ).X i ⟶ F.X j` when `c.Rel i j`. -/
 noncomputable def fstX (i j : ι) (hij : c.Rel i j) : X φ i ⟶ F.X j :=
   haveI := HasHomotopyCofiber.hasBinaryBiproduct φ _ _ hij
   (XIsoBiprod φ i j hij).hom ≫ biprod.fst
 
-/-- The left inclusion `F.X i ⟶ (homotopyCofibre φ).X j` when `c.Rel j i`. -/
+/-- The left inclusion `F.X i ⟶ (homotopyCofiber φ).X j` when `c.Rel j i`. -/
 noncomputable def inlX (i j : ι) (hij : c.Rel j i) : F.X i ⟶ X φ j :=
   haveI := HasHomotopyCofiber.hasBinaryBiproduct φ _ _ hij
   biprod.inl ≫ (XIsoBiprod φ j i hij).inv
