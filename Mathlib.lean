965cfc45565303b2d349113ab1a27961fa77fc23
erd1 <the.erd.one@gmail.com>
2023-12-17 02:12:35 +0800

    fix

--- Mathlib.lean
+++ Mathlib.lean
@@ -3065,6 +3065,7 @@ import Mathlib.RingTheory.Ideal.QuotientOperations
 import Mathlib.RingTheory.Int.Basic
 import Mathlib.RingTheory.IntegralClosure
 import Mathlib.RingTheory.IntegralDomain
+import Mathlib.RingTheory.IntegralRestrict
 import Mathlib.RingTheory.IntegrallyClosed
 import Mathlib.RingTheory.IsAdjoinRoot
 import Mathlib.RingTheory.IsTensorProduct
