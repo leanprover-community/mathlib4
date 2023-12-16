76c7ca3ce3e770ad70c9757c6d86d592a6ed3d54
Utensil <utensilcandel@gmail.com>
2023-12-17 00:33:44 +0800

    reviewdog 🐶
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Mathlib/LinearAlgebra/CliffordAlgebra/SpinGroup.lean
+++ Mathlib/LinearAlgebra/CliffordAlgebra/SpinGroup.lean
@@ -55,7 +55,7 @@ def inv_of_inv_sq (m : M) [Invertible (ι Q m)] [Invertible (2 : R)] :
     Invertible ((ι Q m) * (ι Q m)) := Invertible.mul ‹_› ‹_›
 
 def inv_algebraMap_of_inv_ι (m : M) [Invertible (ι Q m)] [Invertible (2 : R)] :
-  Invertible (algebraMap _ _ (Q m) : CliffordAlgebra Q) :=
+    Invertible (algebraMap _ _ (Q m) : CliffordAlgebra Q) :=
     Invertible.copy (inv_of_inv_sq m) _ (ι_sq_scalar _ _).symm
 
 def inv_of_inv_ι (m : M) [Invertible (ι Q m)] [Invertible (2 : R)] : Invertible (Q m) :=
