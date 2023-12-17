790cb340af97dafd086035ba939906b53870ba5d
Gabin Kolly <gabin1999@hotmail.com>
2023-12-18 00:01:06 +0100

    extensionality partial iso

--- Mathlib/ModelTheory/PartialIsomorphisms.lean
+++ Mathlib/ModelTheory/PartialIsomorphisms.lean
@@ -41,35 +41,58 @@ structure PartialIso where
 /-- A partial isomorphism of first-order structures is an injective partial map
 that commutes with the interpretations of functions and relations -/
 structure PartialIso where
-  domain : Set M
-  toFun : domain → N
-  inj' : Function.Injective toFun
-  map_fun' : ∀ {n} (f : L.Functions n) (x : Fin n → domain)
-    (h : funMap f ((fun x => x.1) ∘ x) ∈ domain),
-    toFun ⟨(funMap f ((fun x => x.1) ∘ x)), h⟩ = funMap f (toFun ∘ x) := by
-    intros; trivial
-  map_rel' : ∀ {n} (r : L.Relations n) (x : Fin n → domain),
-    RelMap r ((fun x => x.1) ∘ x) ↔ RelMap r (toFun ∘ x) := by
-    intros; trivial
+  toFun : Set (M × N)
+  isFun : ∀ p ∈ toFun, ∀ q ∈ toFun, p.1 = q.1 → p.2 = q.2
+  inj' : ∀ p ∈ toFun, ∀ q ∈ toFun, p.2 = q.2 → p.1 = q.1
+  map_fun' : ∀ {n} (f : L.Functions n) (x : Fin n → M × N),
+    ∀ m, x m ∈ toFun → funMap f (Prod.fst ∘ x) ∈ image Prod.fst toFun
+    → ⟨funMap f (Prod.fst ∘ x), funMap f (Prod.snd ∘ x)⟩ ∈ toFun
+  map_rel' : ∀ {n} (r : L.Relations n) (x : Fin n → M × N),
+    RelMap r (Prod.fst ∘ x) ↔ RelMap r (Prod.snd ∘ x)
+
 
 @[inherit_doc]
 scoped[FirstOrder] notation:25 A " ↪ₚ[" L "] " B => FirstOrder.Language.PartialIso L A B
 
-namespace Partial
+namespace PartialIso
 
-instance hasCoeToFun : CoeFun (M ↪ₚ[L] N) fun f => (f.domain → N) where
-  coe := fun (f : M ↪ₚ[L] N) ↦ f.toFun
+def domain : (M ↪ₚ[L] N) → Set M := fun f ↦ image Prod.fst f.toFun
 
-@[simp]
-theorem toFun_eq_coe {f : M ↪ₚ[L] N} : f.toFun = (f : f.domain → N) :=
-  rfl
+def mem_domain (f : M ↪ₚ[L] N) {p : M × N}: p ∈ f.toFun → p.1 ∈ f.domain := fun hp ↦ ⟨p, hp, rfl⟩
+
+def codomain : (M ↪ₚ[L] N) → Set N := fun f ↦ image Prod.snd f.toFun
+
+noncomputable def eval (f : M ↪ₚ[L] N) {m : M} : (m ∈ f.domain) → N := by intro h ; exact (Classical.choose h).snd
+
+theorem eval_spec (f : M ↪ₚ[L] N) {m : M} (h : m ∈ f.domain) : (m, f.eval h) ∈ f.toFun :=
+  (congrFun (congrArg Prod.mk (Classical.choose_spec h).2.symm) (f.eval h)).symm
+  ▸ (Classical.choose_spec h).1
+
+theorem eval_is_image {f : M ↪ₚ[L] N} {m : M} {n : N} (h : m ∈ f.domain) :
+  (m,n) ∈ f.toFun → f.eval h = n := fun h' ↦ f.isFun _ (f.eval_spec h) _ h' rfl
+
+theorem eval_is_eval {f : M ↪ₚ[L] N} {m : M} (h h' : m ∈ f.domain) :
+  f.eval h = f.eval h' := rfl
+
+noncomputable instance hasCoeToFun : CoeFun (M ↪ₚ[L] N) fun f ↦ {m : M} → (m ∈ f.domain) → N where
+  coe := fun (f : M ↪ₚ[L] N) ↦ f.eval
 
 @[ext]
-theorem ext ⦃f g : M ↪ₚ[L] N⦄ (h : ∀ {x}, x ∈ f.domain ↔ x ∈ g.domain)
-  (h' : ∀ x (u : x ∈ f.domain), f ⟨x, u⟩ = g ⟨x, h.1 u⟩) : f = g := by
-  rcases f with ⟨domain, toFun, _⟩
-  rcases g with ⟨domain_g, toFun_g, _⟩
-  cases Set.ext @h
-  cases (funext (fun x ↦ h' x.val x.property) : toFun = toFun_g)
-  rfl
+theorem ext ⦃f g : M ↪ₚ[L] N⦄ (h : f.domain = g.domain)
+  (h' : ∀ x (u : x ∈ f.domain), f.eval u = g.eval (h ▸ u)) : f = g := by
+  have : f.toFun = g.toFun := by
+    apply Set.ext
+    rintro ⟨m, n⟩
+    constructor
+    intro hmn
+    have := g.eval_spec (h ▸ f.mem_domain hmn)
+    rw [←(h' m (f.mem_domain hmn)), f.eval_is_image ⟨(m,n), hmn, rfl⟩ hmn] at this
+    exact this
+    intro hmn
+    have := f.eval_spec (h.symm ▸ g.mem_domain hmn)
+    rw [(h' m (h.symm ▸ g.mem_domain hmn)), g.eval_is_image ⟨(m,n), hmn, rfl⟩ hmn] at this
+    exact this
 
+  rcases f with ⟨toFun, isFun, _⟩
+  cases this
+  rfl
