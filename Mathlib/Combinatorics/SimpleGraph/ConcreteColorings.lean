13d22ca4a03572f555843b62a226b2d693fc136b
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2023-12-17 10:45:29 -0300

    Add `pathGraph_G_Hom_coloring`

--- Mathlib/Combinatorics/SimpleGraph/ConcreteColorings.lean
+++ Mathlib/Combinatorics/SimpleGraph/ConcreteColorings.lean
@@ -48,4 +48,76 @@ theorem chromaticNumber_pathGraph (n : ℕ) (h : 2 ≤ n) :
   · simpa only [pathGraph_two_eq_top, chromaticNumber_top] using
       hc.chromaticNumber_mono_of_embedding (pathGraph_two_embedding n h)
 
+/-- In a bicolored graph colors alternate on every path -/
+theorem pathGraph_G_Hom_coloring {α} (G : SimpleGraph α) (c : G.Coloring Bool) {n : ℕ} (hn : 1 ≤ n)
+    (hom : pathGraph n →g G) (hc0 : c (hom ⟨0, hn⟩) = true) (u : Fin n) :
+    c (hom u) = (u.val % 2 = 0) := by
+  induction n with
+  | zero => exact (Nat.not_succ_le_zero 0 hn).elim
+  | succ n ih =>
+    have hn_cases : n = 0 ∨ 1 ≤ n := or_iff_not_imp_right.mpr Nat.eq_zero_of_not_pos
+    apply Or.elim hn_cases
+    · intro (hn' : n = 0)
+      have hu : u = 0 :=
+        Fin.le_zero_iff.mp (le_of_le_of_eq (Fin.le_val_last u) (congrArg Nat.cast hn'))
+      simp [hu]
+      apply hc0
+    · intro (hn' : 1 ≤ n)
+      let new_hom : pathGraph n →g G :=
+        pathGraph_smaller_Hom G hom n (Nat.le_add_right n 1)
+      have hhom0 : c (new_hom ⟨0, hn'⟩) = true := by
+        simp [pathGraph_smaller_Hom_val]
+        simp_all only [Fin.zero_eta]
+      have h_new_hom := ih hn' new_hom hhom0
+      have hu : u.val < n ∨ u.val = n := le_iff_lt_or_eq.mp (Fin.is_le u)
+      apply Or.elim hu
+      · intro (hun : u.val < n)
+        exact h_new_hom ⟨u.val, hun⟩
+      · intro (hun : u.val = n)
+        -- c (hom n) = true ↔ n % 2 = 0
+        let last : Fin (n + 1) := ⟨n, Nat.lt.base n⟩
+        let prev_last : Fin (n + 1) := ⟨n - 1, Nat.sub_lt_succ n 1⟩
+        have hpgadj : (pathGraph (n + 1)).Adj prev_last last := by
+          simp [pathGraph_adj]
+          exact Or.intro_left (n + 1 = n - 1) (Nat.sub_add_cancel hn')
+        have hGadj : G.Adj (hom prev_last) (hom last) := hom.map_rel hpgadj
+        have h_c_prev_last : c (hom prev_last) = ((n - 1) % 2 = 0) :=
+          h_new_hom ⟨n-1, Nat.sub_lt hn' Nat.one_pos⟩
+        have h_c_last : c (hom last) = ((n - 1) % 2 ≠ 0) := by
+          rw [Bool.eq_not_iff.mpr (Coloring.valid c hGadj).symm]
+          simp [h_c_prev_last.symm]
+        simp [Fin.eq_mk_iff_val_eq.mpr hun, h_c_last]
+        rw [← @Nat.sub_add_cancel n 1 hn']
+        apply (Nat.succ_mod_two_eq_zero_iff).symm
+
+-- What name shuld this have?
+theorem aux2 {α} (p : Bool) (x y : α) (h : ((!p) = true) = (x = y)) : (p = true) = (x ≠ y) := by
+  simp_all only [Bool.not_eq_true', eq_iff_iff, iff_true, ne_eq]
+  apply Iff.intro
+  · intro a
+    subst a
+    simp_all only [eq_iff_iff, false_iff, not_false_eq_true]
+  · intro a
+    simp_all only [iff_false, Bool.not_eq_false]
+
+theorem pathGraph_G_Hom_coloring' {α} (G : SimpleGraph α) (c : G.Coloring Bool) {n : ℕ} (hn : 1 ≤ n)
+    (hom : pathGraph n →g G) (hc0 : c (hom ⟨0, hn⟩) = false) (u : Fin n) :
+    c (hom u) = (u.val % 2 ≠ 0) := by
+  let c' : G.Coloring Bool := Coloring.mk (fun v ↦ !(c v)) <| by
+    intro v w
+    intro (h : G.Adj v w)
+    simp
+    exact (Bool.eq_not_iff.mpr (c.valid h.symm)).symm
+  have hc'c : ∀ (a : α), c' a = !(c a) := fun a ↦ rfl
+  have hcc' : ∀ (a : α), c a = !(c' a) := by
+    intro a
+    simp_all only [Bool.not_not]
+  have hc'0 : c' (hom ⟨0, hn⟩) = true := by
+    rw [hc'c, hc0]
+    rfl
+  rw [hcc']
+  apply aux2
+  rw [Bool.not_not]
+  exact pathGraph_G_Hom_coloring G c' hn hom hc'0 u
+
 end SimpleGraph
