991c00f901527d6f520caf23ea308f0dd07f6a25
AlexKontorovich <58564076+AlexKontorovich@users.noreply.github.com>
2023-12-18 17:48:42 -0500

    fix

--- Mathlib/MeasureTheory/Group/FundamentalDomain.lean
+++ Mathlib/MeasureTheory/Group/FundamentalDomain.lean
@@ -739,8 +739,9 @@ local notation "π" => @Quotient.mk _ α_mod_G
 variable {G}
 
 @[to_additive NullMeasurableSet.addQuotientMeasure_apply]
-lemma NullMeasurableSet.quotientMeasure_apply (_ : NullMeasurableSet s μ) {U : Set (Quotient α_mod_G)}
-    (meas_U : MeasurableSet U)  : (μ.restrict s).map π U = μ ((π ⁻¹' U) ∩ s) := by
+lemma NullMeasurableSet.quotientMeasure_apply (_ : NullMeasurableSet s μ)
+    {U : Set (Quotient α_mod_G)} (meas_U : MeasurableSet U)  :
+    (μ.restrict s).map π U = μ ((π ⁻¹' U) ∩ s) := by
   rw [Measure.map_apply (f := π) (fun V hV ↦ measurableSet_quotient.mp hV) meas_U,
     Measure.restrict_apply (t := (Quotient.mk α_mod_G ⁻¹' U)) (measurableSet_quotient.mp meas_U)]
 
