b81a83e31eb047a94a5d7588e1fbd1f14720424d
Andrew Yang <36414270+erdOne@users.noreply.github.com>
2023-12-18 20:23:45 +0800

    Delete Mathlib/Test.lean

--- Mathlib/Test.lean
+++
@@ -1,9 +0,0 @@
-import Mathlib.Data.Nat.Basic
-def foo (x : ℕ) (h : x < 5) : ℕ := x
-def bar (x : ℕ) (h : x ≥ 6) : ℕ :=
-  if h₀ : x < 4 then
-    foo x (by have h₁ : 4 ≤ 5 := by simp
-              revert h₀
-              revert h₁
-              apply le.trans_lt')
-  else 0
