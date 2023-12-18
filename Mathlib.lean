2913e9f65f15af17583517c6a6484bc3722117b9
Scott Carnahan <128885296+ScottCarnahan@users.noreply.github.com>
2023-12-18 16:00:09 +0900

    Update Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -1847,6 +1847,7 @@ import Mathlib.Data.Polynomial.Monomial
 import Mathlib.Data.Polynomial.PartialFractions
 import Mathlib.Data.Polynomial.Reverse
 import Mathlib.Data.Polynomial.RingDivision
+import Mathlib.Data.Polynomial.Smeval
 import Mathlib.Data.Polynomial.Splits
 import Mathlib.Data.Polynomial.Taylor
 import Mathlib.Data.Polynomial.UnitTrinomial
