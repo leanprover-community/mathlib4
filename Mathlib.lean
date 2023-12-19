748fcce1376b9a9c82477fda66905b23d1e2b995
Yaël Dillies <yael.dillies@gmail.com>
2023-12-19 11:57:24 +0000

    feat: Flooring/ceiling division This PR defines flooring and ceiling division as the right and left adjoints to the map `b ↦ a • b`.

--- Mathlib.lean
+++ Mathlib.lean
@@ -357,6 +357,7 @@ import Mathlib.Algebra.Order.Field.InjSurj
 import Mathlib.Algebra.Order.Field.Pi
 import Mathlib.Algebra.Order.Field.Power
 import Mathlib.Algebra.Order.Floor
+import Mathlib.Algebra.Order.Floor.Div
 import Mathlib.Algebra.Order.Group.Abs
 import Mathlib.Algebra.Order.Group.Bounds
 import Mathlib.Algebra.Order.Group.Defs
