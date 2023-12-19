5934c22f0587a39e6b8bde63fd00728efdeaa749
Jujian Zhang <jujian.zhang1998@outlook.com>
2023-12-19 10:27:38 +0000

    Update Card.lean

--- Mathlib/Data/Finset/Card.lean
+++ Mathlib/Data/Finset/Card.lean
@@ -269,7 +269,7 @@ theorem fiber_card_ne_zero_iff_mem_image (s : Finset α) (f : α → β) [Decida
 
 lemma card_filter_le_iff (s : Finset α) (P : α → Prop) [DecidablePred P] (n : ℕ) :
     (s.filter P).card ≤ n ↔ ∀ s' ⊆ s, n < s'.card → ∃ a ∈ s', ¬ P a :=
-  (s.1.filter_card_le_iff P n).trans ⟨fun H s' hs' h ↦ H s'.1 (by aesop) h,
+  (s.1.card_filter_le_iff P n).trans ⟨fun H s' hs' h ↦ H s'.1 (by aesop) h,
     fun H s' hs' h ↦ H ⟨s', nodup_of_le hs' s.2⟩ (fun x hx ↦ subset_of_le hs' hx) h⟩
 
 @[simp]
