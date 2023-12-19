3929e38fbfdf509f83503fce46392c8575cd53d2
Joël Riou <joel.riou@universite-paris-saclay.fr>
2023-12-19 16:16:25 +0100

    wip

--- Mathlib/Algebra/Homology/SpectralSequenceNew/Construction.lean
+++ Mathlib/Algebra/Homology/SpectralSequenceNew/Construction.lean
@@ -16,24 +16,23 @@ variable (X : SpectralObject C ℤt)
 
 namespace ToE₂
 
-def pageX (r : ℤ) (hr : 2 ≤ r) (pq : ℤ × ℤ) : C := by
+/-def pageX (r : ℤ) (hr : 2 ≤ r) (pq : ℤ × ℤ) : C := by
+  have pif := (X.E (pq.1 + pq.2 - 1) (pq.1 + pq.2) (pq.1 + pq.2 + 1) (by linarith) (by linarith)).obj
   sorry
 
---  (X.E (pq.1+pq.2-1) (pq.1+pq.2) (pq.1+pq.2+1) (by linarith) (by linarith)).obj
---    (ιℤt.mapArrow₃.obj (Arrow₃.mkOfLE (pq.2-r+2) pq.2 (pq.2+1) (pq.2+r-1)))
 def page (r : ℤ) (hr : 2 ≤ r) : HomologicalComplex C (ComplexShape.up' (r, 1 - r)) where
   X := by
-    have pif:= X
+    have := X
     sorry
   d := sorry
   shape := sorry
-  d_comp_d' := sorry
+  d_comp_d' := sorry-/
 
 end ToE₂
 
-noncomputable def toE₂ : E₂CohomologicalSpectralSequence C where
+/-noncomputable def toE₂ : E₂CohomologicalSpectralSequence C where
   page' := ToE₂.page X
-  iso' := sorry
+  iso' := sorry-/
 
 end SpectralObject
 
