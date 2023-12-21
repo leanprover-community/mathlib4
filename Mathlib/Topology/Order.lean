4b87079b29a4c98367cb3e3956190fee9e86b1bf
laughinggas <ashvni.n@gmail.com>
2023-12-21 17:47:48 +1100

    linter

--- Mathlib/Topology/Order.lean
+++ Mathlib/Topology/Order.lean
@@ -881,8 +881,8 @@ eq_of_nhds_eq_nhds ( λ x => by
     hf.preimage_image, nhds_discrete X]
   simp)
 
-lemma DiscreteTopology_induced {X Y : Type*} [tY : TopologicalSpace Y] [DiscreteTopology Y]
-    {f : X → Y} (hf : Function.Injective f) : @DiscreteTopology X (TopologicalSpace.induced f tY) := by
+lemma DiscreteTopology_induced {X Y : Type*} [tY : TopologicalSpace Y] [DiscreteTopology Y] {f :
+  X → Y} (hf : Function.Injective f) : @DiscreteTopology X (TopologicalSpace.induced f tY) := by
   apply @DiscreteTopology.mk _ (TopologicalSpace.induced f tY) _
   rw [@DiscreteTopology.eq_bot Y _ _, induced_bot hf]
 
