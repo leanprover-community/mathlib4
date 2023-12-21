78ba0fe0d7ffeb1f1f335784afdb57bb08dbccab
Moritz Firsching <firsching@google.com>
2023-12-21 10:39:23 +0100

    Update Mathlib/Data/ZMod/Defs.lean

--- Mathlib/Data/ZMod/Defs.lean
+++ Mathlib/Data/ZMod/Defs.lean
@@ -6,7 +6,6 @@ Authors: Eric Rodriguez
 import Mathlib.Algebra.NeZero
 import Mathlib.Data.Nat.ModEq
 import Mathlib.Data.Fintype.Lattice
-import Mathlib.Topology.Order
 
 #align_import data.zmod.defs from "leanprover-community/mathlib"@"3a2b5524a138b5d0b818b858b516d4ac8a484b03"
 
