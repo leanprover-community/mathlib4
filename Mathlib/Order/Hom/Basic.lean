676ec5b77ed03f08ad7a9bb5ed33bee4a68a4548
Parcly Taxel <reddeloostw@gmail.com>
2023-12-22 12:07:41 +0800

    `@[norm_cast]`

--- Mathlib/Order/Hom/Basic.lean
+++ Mathlib/Order/Hom/Basic.lean
@@ -898,6 +898,7 @@ theorem toEquiv_symm (e : α ≃o β) : e.toEquiv.symm = e.symm.toEquiv :=
   rfl
 #align order_iso.to_equiv_symm OrderIso.toEquiv_symm
 
+@[norm_cast]
 theorem coe_symm (e : α ≃o β) : (e : α ≃ β).symm = e.symm := rfl
 
 /-- Composition of two order isomorphisms is an order isomorphism. -/
