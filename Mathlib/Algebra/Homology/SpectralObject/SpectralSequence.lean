899f6fd96ff16bb58c2f228c678b6ed202e93cae
Joël Riou <joel.riou@universite-paris-saclay.fr>
2023-12-22 18:54:48 +0100

    construction of the pages of the spectral sequence

---
+++ Mathlib/Algebra/Homology/SpectralObject/SpectralSequence.lean
@@ -0,0 +1,135 @@
+import Mathlib.Algebra.Homology.SpectralObject.Differentials
+import Mathlib.Algebra.Homology.SpectralSequenceNew.Basic
+
+namespace CategoryTheory
+
+open Category Limits
+
+abbrev homOfLE' {ι : Type*} [Preorder ι] (a b : ι) (h : a ≤ b) : a ⟶ b := homOfLE h
+
+namespace Abelian
+
+namespace SpectralObject
+
+variable {C ι κ : Type*} [Category C] [Abelian C] [Preorder ι]
+  (X : SpectralObject C ι)
+  (c : ℤ → ComplexShape κ) (r₀ : ℤ)
+  [∀ r, DecidableRel (c r).Rel]
+
+variable (ι)
+
+structure SpectralSequenceMkData where
+  deg : κ → ℤ
+  i₀ (r : ℤ) (hr : r₀ ≤ r) (pq : κ) : ι
+  i₁ (pq : κ) : ι
+  i₂ (pq : κ) : ι
+  i₃ (r : ℤ) (hr : r₀ ≤ r) (pq : κ) : ι
+  le₀₁ (r : ℤ) (hr : r₀ ≤ r) (pq : κ) : i₀ r hr pq ≤ i₁ pq
+  le₁₂ (pq : κ) : i₁ pq ≤ i₂ pq
+  le₂₃ (r : ℤ) (hr : r₀ ≤ r) (pq : κ) : i₂ pq ≤ i₃ r hr pq
+  hc (r : ℤ) (hr : r₀ ≤ r) (pq pq' : κ) (hpq : (c r).Rel pq pq') : deg pq + 1 = deg pq'
+  hc₀₂ (r : ℤ) (hr : r₀ ≤ r) (pq pq' : κ) (hpq : (c r).Rel pq pq') : i₀ r hr pq = i₂ pq'
+  hc₁₃ (r : ℤ) (hr : r₀ ≤ r) (pq pq' : κ) (hpq : (c r).Rel pq pq') : i₁ pq = i₃ r hr pq'
+
+variable {ι c r₀}
+
+variable (data : SpectralSequenceMkData ι c r₀)
+
+namespace SpectralSequence
+
+noncomputable def pageX (r : ℤ) (hr : r₀ ≤ r) (pq : κ) : C :=
+  X.E (data.deg pq - 1) (data.deg pq) (data.deg pq + 1) (by simp) rfl
+      (homOfLE (data.le₀₁ r hr pq)) (homOfLE (data.le₁₂ pq)) (homOfLE (data.le₂₃ r hr pq))
+
+noncomputable def pageXIso (r : ℤ) (hr : r₀ ≤ r) (pq : κ) (n₀ n₁ n₂ : ℤ)
+    (hn₁ : n₀ + 1 = n₁) (hn₂ : n₁ + 1 = n₂) (h : n₁ = data.deg pq)
+    (i₀ i₁ i₂ i₃ : ι) (h₀ : i₀ = data.i₀ r hr pq) (h₁ : i₁ = data.i₁ pq)
+    (h₂ : i₂ = data.i₂ pq) (h₃ : i₃ = data.i₃ r hr pq) :
+    pageX X data r hr pq ≅ X.E n₀ n₁ n₂ hn₁ hn₂
+      (homOfLE' i₀ i₁ (by subst h₀ h₁; exact data.le₀₁ r hr pq))
+      (homOfLE' i₁ i₂ (by subst h₁ h₂; exact data.le₁₂ pq))
+      (homOfLE' i₂ i₃ (by subst h₂ h₃; exact data.le₂₃ r hr pq)) :=
+  eqToIso (by
+    obtain rfl : n₀ = n₁ - 1 := by linarith
+    obtain rfl := hn₂
+    subst h h₀ h₁ h₂ h₃
+    rfl)
+
+noncomputable def paged (r : ℤ) (hr : r₀ ≤ r) (pq pq' : κ) :
+    pageX X data r hr pq ⟶ pageX X data r hr pq' :=
+  if hpq : (c r).Rel pq pq'
+    then
+      X.d (data.deg pq - 1) (data.deg pq) (data.deg pq + 1) (data.deg pq + 2) _ rfl
+        (by linarith) (homOfLE (data.le₀₁ r hr pq'))
+        (homOfLE (by simpa only [data.hc₀₂ r hr pq pq' hpq] using data.le₁₂ pq'))
+        (homOfLE (data.le₀₁ r hr pq)) (homOfLE (data.le₁₂ pq)) (homOfLE (data.le₂₃ r hr pq)) ≫
+      (pageXIso _ _ _ _ _ _ _ _ _ _ (data.hc r hr pq pq' hpq) _ _ _ _ rfl rfl
+        (data.hc₀₂ r hr pq pq' hpq) (data.hc₁₃ r hr pq pq' hpq)).inv
+    else 0
+
+lemma paged_eq (r : ℤ) (hr : r₀ ≤ r) (pq pq' : κ) (hpq : (c r).Rel pq pq')
+    (n₀ n₁ n₂ n₃ : ℤ) (hn₁ : n₀ + 1 = n₁) (hn₂ : n₁ + 1 = n₂) (hn₃ : n₂ + 1 = n₃)
+    {i₀ i₁ i₂ i₃ i₄ i₅ : ι} (f₁ : i₀ ⟶ i₁) (f₂ : i₁ ⟶ i₂) (f₃ : i₂ ⟶ i₃)
+    (f₄ : i₃ ⟶ i₄) (f₅ : i₄ ⟶ i₅) (hn₁' : n₁ = data.deg pq)
+    (h₀ : i₀ = data.i₀ r hr pq') (h₁ : i₁ = data.i₁ pq') (h₂ : i₂ = data.i₀ r hr pq)
+    (h₃ : i₃ = data.i₁ pq) (h₄ : i₄ = data.i₂ pq) (h₅ : i₅ = data.i₃ r hr pq) :
+    paged X data r hr pq pq' = by
+      refine'
+        (pageXIso _ _ _ _ _ _ _ _ _ _ hn₁' _ _ _ _ h₂ h₃ h₄ h₅).hom ≫
+        X.d n₀ n₁ n₂ n₃ hn₁ hn₂ hn₃ f₁ f₂ f₃ f₄ f₅ ≫
+        (pageXIso _ _ _ _ _ _ _ _ _ _
+          (by simpa only [← hn₂, hn₁'] using data.hc r hr pq pq' hpq) _ _ _ _ h₀ h₁
+          (by rw [h₂, data.hc₀₂ r hr pq pq' hpq])
+          (by rw [h₃, data.hc₁₃ r hr pq pq' hpq])).inv := by
+  subst hn₁' h₀ h₁ h₂ h₃ h₄ h₅
+  obtain rfl : n₀ = data.deg pq - 1 := by linarith
+  obtain rfl : n₂ = data.deg pq + 1 := by linarith
+  obtain rfl : n₃ = data.deg pq + 2 := by linarith
+  dsimp [paged, pageXIso]
+  rw [dif_pos hpq, id_comp]
+  rfl
+
+@[reassoc (attr := simp)]
+lemma paged_paged (r : ℤ) (hr : r₀ ≤ r) (pq pq' pq'' : κ) :
+    paged X data r hr pq pq' ≫ paged X data r hr pq' pq'' = 0 := by
+  by_cases hpq : (c r).Rel pq pq'
+  · by_cases hpq' : (c r).Rel pq' pq''
+    · let f₁ := homOfLE (data.le₀₁ r hr pq'')
+      let f₂ := homOfLE (data.le₁₂ pq'')
+      let f₃ := homOfLE (data.le₂₃ r hr pq'')
+      let f₄ : data.i₃ r hr pq'' ⟶ data.i₀ r hr pq := homOfLE (by
+        simpa only [← data.hc₁₃ r hr pq' pq'' hpq',
+          data.hc₀₂ r hr pq pq' hpq] using data.le₁₂ pq')
+      let f₅ := homOfLE (data.le₀₁ r hr pq)
+      let f₆ := homOfLE (data.le₁₂ pq)
+      let f₇ := homOfLE (data.le₂₃ r hr pq)
+      rw [paged_eq X data r hr pq pq' hpq (data.deg pq - 1) (data.deg pq) _ _ (by simp)
+        rfl rfl f₃ f₄ f₅ f₆ f₇ rfl (data.hc₀₂ r hr pq' pq'' hpq').symm
+        (data.hc₁₃ r hr pq' pq'' hpq').symm rfl rfl rfl rfl,
+        paged_eq X data r hr pq' pq'' hpq' (data.deg pq) _ _ _ rfl rfl rfl f₁ f₂ f₃ f₄ f₅
+        (data.hc r hr pq pq' hpq) rfl rfl (data.hc₀₂ r hr pq' pq'' hpq').symm
+        (data.hc₁₃ r hr pq' pq'' hpq').symm (data.hc₀₂ r hr pq pq' hpq)
+        (data.hc₁₃ r hr pq pq' hpq), assoc, assoc, Iso.inv_hom_id_assoc,
+        d_d_assoc, zero_comp, comp_zero]
+    · dsimp only [paged]
+      rw [dif_neg hpq', comp_zero]
+  · dsimp only [paged]
+    rw [dif_neg hpq, zero_comp]
+
+noncomputable def page (r : ℤ) (hr : r₀ ≤ r) :
+    HomologicalComplex C (c r) where
+  X pq := pageX X data r hr pq
+  d := paged X data r hr
+  shape pq pq' hpq := dif_neg hpq
+
+end SpectralSequence
+
+/-def spectralSequence : SpectralSequence C c r₀ where
+  page' := SpectralSequence.page X data
+  iso' := sorry-/
+
+end SpectralObject
+
+end Abelian
+
+end CategoryTheory
