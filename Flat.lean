274674687f251a8b6283bbd79ae52d2af40f940f
Coleton Kotch <coleton.kotch@gmail.com>
2023-12-22 00:05:07 -0700

    adjunction + much more

--- Flat.lean
+++ Flat.lean
@@ -1,4 +1,5 @@
 --import Mathlib.Tactic
+--import Mathlib.
 import Mathlib.RingTheory.Flat
 import Mathlib.Algebra.Algebra.Basic
 import Mathlib.RingTheory.Ideal.Basic
@@ -7,6 +8,7 @@ import Mathlib.Algebra.Category.ModuleCat.Basic
 import Mathlib.CategoryTheory.Functor.Basic
 import Mathlib.CategoryTheory.Limits.ExactFunctor
 import Mathlib.CategoryTheory.Limits.Preserves.Shapes.Zero
+import Mathlib.Algebra.Category.ModuleCat.Monoidal.Basic
 
 -- import Mathlib.Algebra.DirectSum.Module
 -- import Mathlib.Algebra.DirectSum.Finsupp
@@ -46,41 +48,173 @@ open CategoryTheory
 
 open CategoryTheory.Limits
 
-open Functor
+open Functor --(PreservesZeroMorphisms) why won't this work?
 
 namespace ModuleCat
 
-variable (R : Type u) (M : Type v) [CommRing R] [AddCommGroup M] [Module R M]
+variable {R : Type u} [CommRing R] (M : ModuleCat.{v} R) (N : ModuleCat.{v} R)
 
-noncomputable def rTensorFunctor : ModuleCat.{v} R ⥤ ModuleCat.{v} R where
+
+-- need to look over a good chunk of this section
+namespace HomFunctor
+
+-- mostly just modeling code, idk if I should remove this or not as it is a single line
+def obj' : ModuleCat R where
+  carrier := M ⟶ N
+
+def map' {N L : ModuleCat.{v} R} (g : N ⟶ L) : obj' M N ⟶ obj' M L where
+  toFun := g.comp
+  map_add' f f' := LinearMap.comp_add f f' g
+  map_smul' := comp_smul g
+
+end HomFunctor
+
+def homFunctor : ModuleCat.{v} R ⥤ ModuleCat.{v} R where
+  obj :=  HomFunctor.obj' M
+  map := HomFunctor.map' M
+
+@[simp]
+lemma homFunctor.obj_eqHom : ((homFunctor M).obj N) = (M ⟶ N) := rfl
+
+lemma homFunctor.obj_toHom : ((homFunctor M).obj N) ⟶ of R (M ⟶ N) where
+  toFun := 𝟙 (M ⟶ N)
+  map_add' := by simp only [obj_eqHom, types_id_apply, forall_const]
+  map_smul' := by simp only [obj_eqHom, types_id_apply, RingHom.id_apply, forall_const]
+
+
+@[ext]
+lemma homFunctor.ext {f g : (homFunctor M).obj N} (h : ∀ m, f.toFun m = g.toFun m) : f = g :=
+  LinearMap.ext h
+
+
+
+-- lemma homFunctor.ForgetOfHomIs : Functor.hom (ModuleCat.{v} R) := sorry
+
+
+
+-- namespace TensorProduct
+
+-- noncomputable def robj (N : ModuleCat.{v} R) : ModuleCat R where
+--   carrier := N ⊗[R] M
+
+-- noncomputable def lobj (N : ModuleCat.{v} R) : ModuleCat R where
+--   carrier := M ⊗[R] N
+
+-- -- noncomputable def rmap' {N L : ModuleCat.{v} R} (f : N ⟶ L) : robj' M N ⟶ robj' M L where
+-- --   toFun := f.rTensor M
+-- --   map_add' := map_add <| f.rTensor M
+-- --   map_smul' := map_smul <| f.rTensor M
+
+-- noncomputable def rmap {N L : ModuleCat.{v} R} (f : N ⟶ L) : robj M N ⟶ robj M L :=
+--   ofHom <| rTensor M f
+
+-- noncomputable def lmap {N L : ModuleCat.{v} R} (f : N ⟶ L) : lobj M N ⟶ lobj M L :=
+--   ofHom <| lTensor M f
+
+-- end TensorProduct
+
+suppress_compilation
+
+variable {R : Type u} [CommRing R] (M : ModuleCat.{v} R)
+
+def rTensorFunctor : ModuleCat.{v} R ⥤ ModuleCat.{v} R where
   obj N := of R (N ⊗[R] M)
   map f := ofHom <| rTensor M f
   map_id N := rTensor_id M N
   map_comp f g := rTensor_comp M g f
 
-noncomputable def lTensorFunctor : ModuleCat.{v} R ⥤ ModuleCat.{v} R where
+def lTensorFunctor : ModuleCat.{v} R ⥤ ModuleCat.{v} R where
   obj N := of R (M ⊗[R] N)
   map f := ofHom <| lTensor M f
   map_id N := lTensor_id M N
   map_comp f g := lTensor_comp M g f
 
-instance rTensorFunctorPreservesZeroMorphisms : PreservesZeroMorphisms (rTensorFunctor R M) where
+--used MonoidalCat tensor stuff here, idea was for compatibility reasons, but it seems
+-- lean is smart enough that it just made dsimp work worse
+-- noncomputable def rTensorFunctor : ModuleCat.{v} R ⥤ ModuleCat.{v} R where
+--   obj N := MonoidalCategory.tensorObj N M
+--   map f := MonoidalCategory.tensorHom f <| 𝟙 M
+--   map_id N := rTensor_id M N
+--   map_comp f g := rTensor_comp M g f
+
+-- noncomputable def lTensorFunctor : ModuleCat.{v} R ⥤ ModuleCat.{v} R where
+--   obj N :=  MonoidalCategory.tensorObj M N
+--   map f :=  MonoidalCategory.tensorHom (𝟙 M) f
+--   map_id N := lTensor_id M N
+--   map_comp f g := lTensor_comp M g f
+
+
+instance rTensorFunctorPreservesZeroMorphisms : PreservesZeroMorphisms (rTensorFunctor M) where
   map_zero := fun _ _ => rTensor_zero M
 
-instance lTensorFunctorPreservesZeroMorphisms : PreservesZeroMorphisms (lTensorFunctor R M) where
+instance lTensorFunctorPreservesZeroMorphisms : PreservesZeroMorphisms (lTensorFunctor M) where
   map_zero := fun _ _ => lTensor_zero M
 
-instance rTensorFunctorPreservesFiniteColimits : PreservesFiniteColimits (rTensorFunctor R M) where
+
+namespace TensorHomFunctorsAdj
+
+def HomEquiv (N L : ModuleCat.{v} R) :
+  ((rTensorFunctor M).obj N ⟶ L) ≃ (N ⟶ (homFunctor M).obj L) where
+    toFun := curry
+    invFun := uncurry R N M L --fix namespace bloat****
+    left_inv := (lift.equiv R N M L).right_inv
+    right_inv := (lift.equiv R N M L).left_inv
+
+-- noncomputable def lhomEquiv (N L : ModuleCat.{v} R) :
+--   ((rTensorFunctor M).obj N ⟶ L) ≃ (N ⟶ (homFunctor M).obj L) where
+--     toFun := curry
+--     invFun := uncurry R N M L --fix namespace bloat****
+--     left_inv := (lift.equiv R N M L).right_inv
+--     right_inv := (lift.equiv R N M L).left_inv
+
+
+def UnitApp (N : ModuleCat.{v} R) :
+  (𝟭 (ModuleCat R)).obj N ⟶ (rTensorFunctor M ⋙ homFunctor M).obj N where
+    toFun n := {
+      toFun := tmul R n
+      map_add' := tmul_add n
+      map_smul' := by simp only [tmul_smul, RingHom.id_apply, forall_const]
+    }
+    map_add' n₁ n₂ := by
+      simp only [comp_obj, id_obj]
+      ext m
+      simp [add_tmul]
+      rfl
+    map_smul' _ _ := rfl
+
+
+def CounitApp (N : ModuleCat.{v} R) :
+  (homFunctor M ⋙ rTensorFunctor M).obj N ⟶ (𝟭 (ModuleCat R)).obj N :=
+    uncurry R (M ⟶ N) M N <| homFunctor.obj_toHom M N
+    -- toFun := by
+    --   simp at *
+    --   sorry
+    -- map_add' := sorry
+    -- map_smul' := sorry
+
+
+
+end TensorHomFunctorsAdj
+
+
+def rTensorHomAdj : rTensorFunctor M ⊣ homFunctor M := sorry --where
+  -- homEquiv := TensorHomFunctorsAdj.HomEquiv M
+  -- unit := {app := TensorHomFunctorsAdj.UnitApp M }
+  -- counit := {app := uncurry R (M ⟶ N) M N <| homFunctor.obj_toHom M N}
+
+
+instance rTensorFunctorPreservesFiniteColimits : PreservesFiniteColimits (rTensorFunctor M) where
   preservesFiniteColimits J := sorry
 
-instance lTensorFunctorPreservesFiniteColimits : PreservesFiniteColimits (lTensorFunctor R M) where
+instance lTensorFunctorPreservesFiniteColimits : PreservesFiniteColimits (lTensorFunctor M) where
   preservesFiniteColimits J := sorry
 
-noncomputable def rTensorFunctorRightExact : ModuleCat.{v} R ⥤ᵣ ModuleCat.{v} R :=
-  RightExactFunctor.of (rTensorFunctor R M)
+def rTensorFunctorRightExact : ModuleCat.{v} R ⥤ᵣ ModuleCat.{v} R :=
+  RightExactFunctor.of (rTensorFunctor M)
 
-noncomputable def lTensorFunctorRightExact : ModuleCat.{v} R ⥤ᵣ ModuleCat.{v} R :=
-  RightExactFunctor.of (lTensorFunctor R M)
+def lTensorFunctorRightExact : ModuleCat.{v} R ⥤ᵣ ModuleCat.{v} R :=
+  RightExactFunctor.of (lTensorFunctor M)
+
 
 end ModuleCat
 
@@ -117,7 +251,8 @@ variable (R : Type u) (S : Type v) (M : Type w)
 [Module R M] [Module S M] [IsScalarTower R S M]
 
 #check Equiv.comp_injective
-#check EquivLike
+#check Submodule
+#check range
 --LinearAlgebra.TensorProduct.Tower.assoc
 lemma of_flat_hom [hM : Flat S M] [hS : Flat R S] : Flat R M := by
     rw [iff_rTensor_injective' R M]
@@ -125,23 +260,38 @@ lemma of_flat_hom [hM : Flat S M] [hS : Flat R S] : Flat R M := by
     rw [iff_rTensor_injective' S M] at hM
     intro I
     let hS := hS I
-    let J := @Ideal.span S _ <| LinearMap.range (TensorProduct.lift ((lsmul R S).comp I.subtype))
-    let hM := hM J
-    -- have h : (TensorProduct.lift ((lsmul R S).comp I.subtype)).comp <|
-    --   TensorProduct.lift ((lsmul S M).comp J.subtype) = TensorProduct.lift ((lsmul R M).comp I.subtype)
-    let e₁ := (AlgebraTensorModule.assoc R S S M S I).restrictScalars R
-    --letI := k S R R I S M
+    -- let L := LinearMap.range (TensorProduct.lift ((lsmul R S).comp I.subtype))
+    let J := Submodule.baseChange S I
+    -- letI := (TensorProduct.rid R S).symm
+    let e := AlgebraTensorModule.rid R S S
+    --letI :=  @Equiv.module S J S _  e.symm
+    -- --let J := @Ideal.span S _ <| LinearMap.range (TensorProduct.lift ((lsmul R S).comp I.subtype))
+    -- --let hM := hM J
+    -- -- have h : (TensorProduct.lift ((lsmul R S).comp I.subtype)).comp <|
+    -- --   TensorProduct.lift ((lsmul S M).comp J.subtype) = TensorProduct.lift ((lsmul R M).comp I.subtype)
+    --let e₁ := (AlgebraTensorModule.assoc R S S M S I).restrictScalars R
+    -- --letI := k S R R I S M
     --letI := AlgebraTensorModule.leftComm
-    let e₂ := (TensorProduct.lid S M).restrictScalars R
-    --let σ₃ := (TensorProduct.lid S M).toLinearMap
-    let e₃ := TensorProduct.lid R (S ⊗[S] M)
-    let e := e₃ ≪≫ₗ e₂
+    -- let e₂ := (TensorProduct.rid S M).restrictScalars R
+    -- --let σ₃ := (TensorProduct.lid S M).toLinearMap
+    -- let e₃ := TensorProduct.congr e₂ (LinearEquiv.refl R I)
+    -- let e := e₃.symm ≪≫ₗ e₁
+    let f := TensorProduct.lift ((lsmul R S).comp I.subtype)
+    --let f' := AlgebraTensorModule.lift ((lsmul S R).comp I.subtype)
+    -- have eq : f = f' := sorry
+    --have hf : ∀ x ∈ I, f x ∈ J := sorry
+    --let f := f.restrict (sorry)
+    --let g := TensorProduct.lift ((lsmul S M).comp J.subtype)
+    let h :=  TensorProduct.lift ((lsmul R M).comp I.subtype)
+      --let g :=  TensorProduct.lift ((lsmul S M).comp J.subtype)
+
+
     sorry
 
 
-lemma iff_rTensorFunctorPreservesFiniteLimits :  Flat R M ↔
-  Nonempty (PreservesFiniteLimits (ModuleCat.rTensorFunctor R M)) := by
-  sorry
+-- lemma iff_rTensorFunctorPreservesFiniteLimits :  Flat R M ↔
+--   Nonempty (PreservesFiniteLimits (ModuleCat.rTensorFunctor M)) := by
+--   sorry
 
 end Flat
 
@@ -177,8 +327,8 @@ variable {R : Type u} {S : Type v} {T : Type w} [CommRing R] [CommRing S] [Ring 
 
 lemma comp {g : S →+* T} {f : R →+* S} (hg : g.Flat) (hf : f.Flat) :
   --[UnivLE.{u, v}] [UnivLE.{v, w}] [UnivLE.{u, w}]
-  Flat (RingHom.comp g f) :=
-    @Module.Flat.of_flat_hom R S T _ _  f.toAlgebra _ (g.comp f).toModule g.toModule (sorry) hg hf
+  Flat (RingHom.comp g f) := sorry
+   -- @Module.Flat.of_flat_hom R S T _ _ f.toAlgebra _ (g.comp f).toModule g.toModule (sorry) hg hf
 
 end Flat
 
@@ -194,7 +344,7 @@ open Localization
 
 --namespace Flat
 
-variable (R : Type u) (M : Type v) [CommRing R] [AddCommGroup M] [Module R M]
+variable (R : Type u) (M : Type v) [CommRing R] [AddCommGroup M] [Module R M] [Flat R M]
 
 --class or def?
 class FaithfullyFlat extends Flat R M where
@@ -210,11 +360,11 @@ instance self : FaithfullyFlat R R where
 def ReflectsExact : Prop := sorry
 
 --Faithfully flat iff tensor with any R-module is nonzero
-def TensorNonzero : Prop :=
+def TensorNonzero [Flat R M] : Prop :=
   ∀ {N : Type v} [AddCommGroup N] [Module R N], Nontrivial N → Nontrivial (M ⊗[R] N)
 
 -- Faithfully flat iff tensor with all prime residues is nonzero
-def PrimeResidues : Prop :=
+def PrimeResidues [Flat R M] : Prop :=
   ∀ ⦃p : Ideal R⦄ (_ : p.IsPrime), Nontrivial <| M ⊗[R] FractionRing (R ⧸ p)
   -- @TensorProduct R _ M (LocalRing.ResidueField (Localization.AtPrime p)) _ _ _ <|
   -- ((LocalRing.residue (Localization.AtPrime p)).comp
@@ -227,18 +377,48 @@ def PrimeResidues : Prop :=
   -- ((LocalRing.residue (Localization.AtPrime m)).comp
   -- (algebraMap R <| Localization.AtPrime m)).toModule
 
+
+lemma TensorNonzero_to_PrimeResidues : TensorNonzero R M → PrimeResidues R M := by
+  intro h p hp
+  let h' := IsFractionRing.nontrivial (R ⧸ p) (FractionRing (R ⧸ p))
+  sorry
+
+lemma PrimeResidues_to_FaithfullyFlat : PrimeResidues R M → FaithfullyFlat R M := by
+  intro h
+  refine mk ?faithful
+  intro m hm
+  letI := ((Ideal.Quotient.maximal_ideal_iff_isField_quotient m).1 hm).toField
+  let e := FractionRing.algEquiv (R ⧸ m) (R ⧸ m)
+  --letI := TensorProduct.congr e.toLinearEquiv (LinearEquiv.refl R M)
+  -- let k : (R ⧸ m) = FractionRing (R ⧸ m) := sorry
+  -- letI := h <| Ideal.IsMaximal.isPrime hm
+  -- let l : IsFractionRing (R ⧸ m) (R ⧸ m) := by
+  --   let s := (Ideal.Quotient.maximal_ideal_iff_isField_quotient m).1 hm
+  --   apply?
+  --rw [← k] at this
+  sorry
+
+
 variable (R : Type u) (S : Type v) (M : Type w)
 [CommRing R] [CommRing S]
 [Algebra R S] [AddCommGroup M]
 [Module R M] [Module S M] [IsScalarTower R S M]
 
+#check Equiv.nontrivial
 def of_faithfully_flat_hom [hM : FaithfullyFlat S M] [hS : FaithfullyFlat R S] :
   FaithfullyFlat R M where
     out := (Flat.of_flat_hom R S M).out
     faithful m hm := by
-      let e₁ := (TensorProduct.lid S M).restrictScalars R
-      let e₂ := TensorProduct.lid R (S ⊗[S] M)
-      let e3 := e₂ ≪≫ₗ e₁
+      let e₁ :=  (TensorProduct.rid S M).restrictScalars R
+      --let e₁ := TensorProduct.lid R (S ⊗[S] M)
+      --let e3 := e₂ ≪≫ₗ e₁
+      let e₂ := TensorProduct.congr e₁ <| LinearEquiv.refl R (R ⧸ m)
+      let e₃ := (AlgebraTensorModule.assoc R S S M S (R ⧸ m)).restrictScalars R
+      let e := e₃.symm ≪≫ₗ e₂
+
+      let J := S ⊗[R] (R ⧸ m)
+      let h : Nontrivial J := hS.faithful hm
+
       sorry
 
 end FaithfullyFlat
