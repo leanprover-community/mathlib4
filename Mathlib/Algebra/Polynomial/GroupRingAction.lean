d0ea967c30400c667892455df1c886da6a116b65
Antoine Chambert-Loir <antoine.chambert-loir@math.univ-paris-diderot.fr>
2023-12-23 20:04:42 +0100

    update (MonoidHom.id_apply is not automatic…)

--- Mathlib/Algebra/Polynomial/GroupRingAction.lean
+++ Mathlib/Algebra/Polynomial/GroupRingAction.lean
@@ -137,11 +137,11 @@ protected noncomputable def polynomial (g : P →+*[M] Q) : P[X] →+*[M] Q[X] w
   toFun := map g
   map_smul' m p :=
     Polynomial.induction_on p
-      (fun b ↦ by rw [smul_C, map_C, coe_fn_coe, g.map_smul, map_C, coe_fn_coe, smul_C])
+      (fun b ↦ by rw [smul_C, map_C, coe_fn_coe, g.map_smul, map_C, coe_fn_coe, smul_C, MonoidHom.id_apply, id_eq])
       (fun p q ihp ihq ↦ by
         rw [smul_add, Polynomial.map_add, ihp, ihq, Polynomial.map_add, smul_add])
       fun n b _ ↦ by
-      rw [smul_mul', smul_C, smul_pow', smul_X, Polynomial.map_mul, map_C, Polynomial.map_pow,
+      rw [MonoidHom.id_apply, id_eq, smul_mul', smul_C, smul_pow', smul_X, Polynomial.map_mul, map_C, Polynomial.map_pow,
         map_X, coe_fn_coe, g.map_smul, Polynomial.map_mul, map_C, Polynomial.map_pow, map_X,
         smul_mul', smul_C, smul_pow', smul_X, coe_fn_coe]
   -- porting note: added `.toRingHom`
