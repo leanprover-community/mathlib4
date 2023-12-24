54c17417f9e1c779ab610cd5d8eeaa08412076ad
Antoine Chambert-Loir <antoine.chambert-loir@math.univ-paris-diderot.fr>
2023-12-24 23:23:17 +0100

    unexecuted tactic

--- Mathlib/Algebra/Module/Submodule/Bilinear.lean
+++ Mathlib/Algebra/Module/Submodule/Bilinear.lean
@@ -72,8 +72,6 @@ theorem map₂_span_span (f : M →ₗ[R] N →ₗ[R] P) (s : Set M) (t : Set N)
     all_goals intros; simp only [*, add_mem, smul_mem, zero_mem, _root_.map_zero, map_add,
                                  LinearMap.zero_apply, LinearMap.add_apply, LinearMap.smul_apply,
                                  map_smul]
-    all_goals intros; simp [*, add_mem, smul_mem, zero_mem, _root_.map_zero, map_add,
-      LinearMap.zero_apply, LinearMap.add_apply, LinearMap.smul_apply, map_smulₛₗ]
   · rw [span_le]
     rintro _ ⟨a, b, ha, hb, rfl⟩
     exact apply_mem_map₂ _ (subset_span ha) (subset_span hb)
