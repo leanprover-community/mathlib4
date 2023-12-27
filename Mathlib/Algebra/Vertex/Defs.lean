d4f055b1452be54467639ff093e6ff05a947b454
Scott Carnahan <128885296+ScottCarnahan@users.noreply.github.com>
2023-12-27 10:34:22 +0900

    Identity field instance

--- Mathlib/Algebra/Vertex/Defs.lean
+++ Mathlib/Algebra/Vertex/Defs.lean
@@ -31,7 +31,10 @@ We postpone the proofs of equivalences of various identities to Mathlib.Algebra.
 
 ## To do:
 
-* Clean up instances - apparently Mathlib.Logic.Equiv.TransferInstance can help.
+* Clean up instances - apparently Mathlib.Logic.Equiv.TransferInstance can help.  Maybe just use
+abbrev.
+
+* One (VertexOperator R V) instance with identity field.
 
 * residue products, order of associativity, weak associativity
 
@@ -44,6 +47,8 @@ We postpone the proofs of equivalences of various identities to Mathlib.Algebra.
 ## References
 
 G. Mason `Vertex rings and Pierce bundles` ArXiv 1707.00328
+A. Matsuo, K. Nagatomo `On axioms for a vertex algebra and locality of quantum fields`
+arXiv:hep-th/9706118
 
 -/
 
@@ -56,13 +61,20 @@ variable {V : Type*} {R : Type*}
 section VertexOperator
 
 /-- A vertex operator over a semiring `R` is an `R`-linear map from an `R`-module `V` to Laurent
-series with coefficients in `V`-/
+series with coefficients in `V`. -/
 @[ext]
 structure VertexOperator (R : Type*) (V : Type*) [CommRing R] [AddCommGroup V] [Module R V] where
 /-- The underlying structure map of a vertex operator. -/
   toMap : V →ₗ[R] HahnSeries ℤ V
 
 namespace VertexAlg
+
+theorem VertexOperator_equiv_LinearMap (R : Type*) (V : Type*) [CommRing R] [AddCommGroup V]
+    [Module R V] : VertexOperator R V ≃ (V →ₗ[R] HahnSeries ℤ V) := by
+  use VertexOperator.toMap
+  exact fun a ↦ { toMap := a }
+  exact congrFun rfl
+  exact congrFun rfl
 
 instance LinearMapClass (R : Type*) (V : Type*) [CommRing R] [AddCommGroup V] [Module R V] :
     LinearMapClass (VertexOperator R V) R V (HahnSeries ℤ V) where
@@ -115,6 +127,7 @@ theorem index_eq_coeff_neg [CommRing R] [AddCommGroup V] [Module R V] (A : Verte
 scoped[VertexAlg] notation A "⁅" n "⁆" => index A n
 
 instance [CommRing R] [AddCommGroup V] [Module R V] : Zero (VertexOperator R V) :=
+-- How do I use Equiv.zero_def (VertexOperator_equiv_LinearMap R V)???
   {
   zero :=
     {
@@ -172,6 +185,52 @@ instance [CommRing R] [AddCommGroup V] [Module R V] : Add (VertexOperator R V) :
 @[simp] lemma add_index_eq [CommRing R] [AddCommGroup V] [Module R V] (A B : VertexOperator R V)
   (n : ℤ) : (A + B) ⁅n⁆ = A ⁅n⁆ + B ⁅n⁆ := by exact rfl
 
+noncomputable instance [CommRing R] [AddCommGroup V] [Module R V] : One (VertexOperator R V) :=
+  {
+    one :=
+    {
+      toMap := -- can I use HahnSeries.single.addMonoidHom ???
+      {
+        toFun := fun x => HahnSeries.single 0 x
+        map_add' := by
+          intro x y
+          unfold HahnSeries.single Pi.single Function.update
+          simp only [eq_rec_constant, Pi.zero_apply, dite_eq_ite, ZeroHom.coe_mk,
+            HahnSeries.ext_iff, HahnSeries.add_coeff']
+          refine Function.funext_iff.mpr ?_
+          intro a
+          cases a with
+          | ofNat n =>
+            cases n with
+            | zero =>
+              simp only [Nat.zero_eq, Int.ofNat_eq_coe, Int.Nat.cast_ofNat_Int, ite_true, Pi.add_apply]
+            | succ n =>
+              simp only [Int.ofNat_eq_coe, Nat.cast_succ, Pi.add_apply]
+              exact ite_add_zero
+          | negSucc _ =>
+            simp_all only [Int.negSucc_ne_zero, ite_false, Pi.add_apply, add_zero]
+        map_smul' := by
+          intro r x
+          unfold HahnSeries.single Pi.single Function.update
+          simp only [eq_rec_constant, Pi.zero_apply, dite_eq_ite, ZeroHom.coe_mk, RingHom.id_apply]
+          aesop
+      }
+    }
+  }
+
+/-!
+This should be V →ₗ[R] HahnSeries Γ V !!!
+
+variable [PartialOrder Γ] [Semiring R] {V : Type*} [AddCommMonoid V] [Module R V]
+
+@[simps]
+def single.linearMap (a : Γ) : R →ₗ[R] HahnSeries Γ R :=
+  { single.addMonoidHom a with
+    map_smul' := fun r s => by
+      ext b
+      by_cases h : b = a <;> simp [h] }
+
+-/
 instance [CommRing R] [AddCommGroup V] [Module R V] : Neg (VertexOperator R V) :=
   {
     neg := fun A =>
@@ -193,7 +252,7 @@ instance [CommRing R] [AddCommGroup V] [Module R V] : Neg (VertexOperator R V) :
 @[simp] lemma neg_toMap_eq [CommRing R] [AddCommGroup V] [Module R V] (A : VertexOperator R V)
     (x : V): (-A).toMap x = - A.toMap x := rfl
 
--- can I use LinearMap.AddCommGroup here?  On Zulip,
+-- can I use LinearMap.AddCommGroup here?  On Zulip, Equiv was recommended.
 instance [CommRing R] [AddCommGroup V] [Module R V] : AddCommGroup (VertexOperator R V) :=
   {
     add_assoc := by
@@ -288,7 +347,198 @@ def isLocalToOrderLeq (R: Type*) (V : Type*) [CommRing R] [AddCommGroup V] [Modu
 def isLocal (R: Type*) (V : Type*) [CommRing R] [AddCommGroup V] [Module R V]
     (A B : VertexOperator R V) : Prop := ∃(n : ℕ), isLocalToOrderLeq R V A B n
 
--- residue products, order of associativity
+
+
+/-!
+
+Put Hasse derivatives of Hahn Series in the Binomial Ring file?
+We need exponents in a partially ordered binomial ring.
+The `k`th Hasse derivative of a HahnSeries `∑ a_i X^i` is `∑ (i.choose k) a_i X^(i-k)`.
+Coefficient function is
+Need to show that support is pwo.
+
+Then, define `hasseDeriv k A` for Vertex operator `A` as `v ↦ hasseDeriv k (A v)`.
+
+/--
+It satisfies `k! * (hasseDeriv k f) = derivative^[k] f`. -/
+def hasseDeriv (k : ℕ) : VertexOperator R V →ₗ[R] VertexOperator R V :=
+  lsum fun i => monomial (i - k) ∘ₗ DistribMulAction.toLinearMap R R (i.choose k)
+
+
+/-- The `k`th Hasse derivative of a vertex operator `∑ A_i X^i` is `∑ (i.choose k) A_i X^(i-k)`.
+It satisfies `k! * (hasseDeriv k f) = derivative^[k] f`. -/
+def hasseDeriv (k : ℕ) : VertexOperator R V →ₗ[R] VertexOperator R V :=
+  lsum fun i => monomial (i - k) ∘ₗ DistribMulAction.toLinearMap R R (i.choose k)
+
+
+divided-power derivatives of fields
+maybe need divided-power derivatives of formal power series with exponents in a binomial ring?
+
+open BigOperators Polynomial
+
+open Function
+
+open Nat hiding nsmul_eq_mul
+
+variable {R : Type*} [Semiring R] (k : ℕ) (f : R[X])
+
+/-- The `k`th Hasse derivative of a polynomial `∑ a_i X^i` is `∑ (i.choose k) a_i X^(i-k)`.
+It satisfies `k! * (hasse_deriv k f) = derivative^[k] f`. -/
+def hasseDeriv (k : ℕ) : R[X] →ₗ[R] R[X] :=
+  lsum fun i => monomial (i - k) ∘ₗ DistribMulAction.toLinearMap R R (i.choose k)
+
+theorem hasseDeriv_apply :
+    hasseDeriv k f = f.sum fun i r => monomial (i - k) (↑(i.choose k) * r) := by
+  dsimp [hasseDeriv]
+  congr; ext; congr
+  apply nsmul_eq_mul
+#align polynomial.hasse_deriv_apply Polynomial.hasseDeriv_apply
+
+theorem hasseDeriv_coeff (n : ℕ) :
+    (hasseDeriv k f).coeff n = (n + k).choose k * f.coeff (n + k) := by
+  rw [hasseDeriv_apply, coeff_sum, sum_def, Finset.sum_eq_single (n + k), coeff_monomial]
+  · simp only [if_true, add_tsub_cancel_right, eq_self_iff_true]
+  · intro i _hi hink
+    rw [coeff_monomial]
+    by_cases hik : i < k
+    · simp only [Nat.choose_eq_zero_of_lt hik, ite_self, Nat.cast_zero, zero_mul]
+    · push_neg at hik
+      rw [if_neg]
+      contrapose! hink
+      exact (tsub_eq_iff_eq_add_of_le hik).mp hink
+  · intro h
+    simp only [not_mem_support_iff.mp h, monomial_zero_right, mul_zero, coeff_zero]
+#align polynomial.hasse_deriv_coeff Polynomial.hasseDeriv_coeff
+
+theorem hasseDeriv_zero' : hasseDeriv 0 f = f := by
+  simp only [hasseDeriv_apply, tsub_zero, Nat.choose_zero_right, Nat.cast_one, one_mul,
+    sum_monomial_eq]
+#align polynomial.hasse_deriv_zero' Polynomial.hasseDeriv_zero'
+
+@[simp]
+theorem hasseDeriv_zero : @hasseDeriv R _ 0 = LinearMap.id :=
+  LinearMap.ext <| hasseDeriv_zero'
+#align polynomial.hasse_deriv_zero Polynomial.hasseDeriv_zero
+
+theorem hasseDeriv_eq_zero_of_lt_natDegree (p : R[X]) (n : ℕ) (h : p.natDegree < n) :
+    hasseDeriv n p = 0 := by
+  rw [hasseDeriv_apply, sum_def]
+  refine' Finset.sum_eq_zero fun x hx => _
+  simp [Nat.choose_eq_zero_of_lt ((le_natDegree_of_mem_supp _ hx).trans_lt h)]
+#align polynomial.hasse_deriv_eq_zero_of_lt_nat_degree Polynomial.hasseDeriv_eq_zero_of_lt_natDegree
+
+theorem hasseDeriv_one' : hasseDeriv 1 f = derivative f := by
+  simp only [hasseDeriv_apply, derivative_apply, ← C_mul_X_pow_eq_monomial, Nat.choose_one_right,
+    (Nat.cast_commute _ _).eq]
+#align polynomial.hasse_deriv_one' Polynomial.hasseDeriv_one'
+
+@[simp]
+theorem hasseDeriv_one : @hasseDeriv R _ 1 = derivative :=
+  LinearMap.ext <| hasseDeriv_one'
+#align polynomial.hasse_deriv_one Polynomial.hasseDeriv_one
+
+@[simp]
+theorem hasseDeriv_monomial (n : ℕ) (r : R) :
+    hasseDeriv k (monomial n r) = monomial (n - k) (↑(n.choose k) * r) := by
+  ext i
+  simp only [hasseDeriv_coeff, coeff_monomial]
+  by_cases hnik : n = i + k
+  · rw [if_pos hnik, if_pos, ← hnik]
+    apply tsub_eq_of_eq_add_rev
+    rwa [add_comm]
+  · rw [if_neg hnik, mul_zero]
+    by_cases hkn : k ≤ n
+    · rw [← tsub_eq_iff_eq_add_of_le hkn] at hnik
+      rw [if_neg hnik]
+    · push_neg at hkn
+      rw [Nat.choose_eq_zero_of_lt hkn, Nat.cast_zero, zero_mul, ite_self]
+#align polynomial.hasse_deriv_monomial Polynomial.hasseDeriv_monomial
+
+theorem hasseDeriv_C (r : R) (hk : 0 < k) : hasseDeriv k (C r) = 0 := by
+  rw [← monomial_zero_left, hasseDeriv_monomial, Nat.choose_eq_zero_of_lt hk, Nat.cast_zero,
+    zero_mul, monomial_zero_right]
+set_option linter.uppercaseLean3 false in
+#align polynomial.hasse_deriv_C Polynomial.hasseDeriv_C
+
+theorem hasseDeriv_apply_one (hk : 0 < k) : hasseDeriv k (1 : R[X]) = 0 := by
+  rw [← C_1, hasseDeriv_C k _ hk]
+#align polynomial.hasse_deriv_apply_one Polynomial.hasseDeriv_apply_one
+
+theorem hasseDeriv_X (hk : 1 < k) : hasseDeriv k (X : R[X]) = 0 := by
+  rw [← monomial_one_one_eq_X, hasseDeriv_monomial, Nat.choose_eq_zero_of_lt hk, Nat.cast_zero,
+    zero_mul, monomial_zero_right]
+set_option linter.uppercaseLean3 false in
+#align polynomial.hasse_deriv_X Polynomial.hasseDeriv_X
+
+theorem factorial_smul_hasseDeriv : ⇑(k ! • @hasseDeriv R _ k) = (@derivative R _)^[k] := by
+  induction' k with k ih
+  · rw [hasseDeriv_zero, factorial_zero, iterate_zero, one_smul, LinearMap.id_coe]
+  ext f n : 2
+  rw [iterate_succ_apply', ← ih]
+  simp only [LinearMap.smul_apply, coeff_smul, LinearMap.map_smul_of_tower, coeff_derivative,
+    hasseDeriv_coeff, ← @choose_symm_add _ k]
+  simp only [nsmul_eq_mul, factorial_succ, mul_assoc, succ_eq_add_one, ← add_assoc,
+    add_right_comm n 1 k, ← cast_succ]
+  rw [← (cast_commute (n + 1) (f.coeff (n + k + 1))).eq]
+  simp only [← mul_assoc]
+  norm_cast
+  congr 2
+  rw [mul_comm (k+1) _, mul_assoc, mul_assoc]
+  congr 1
+  have : n + k + 1 = n + (k + 1) := by apply add_assoc
+  rw [← choose_symm_of_eq_add this, choose_succ_right_eq, mul_comm]
+  congr
+  rw [add_assoc, add_tsub_cancel_left]
+#align polynomial.factorial_smul_hasse_deriv Polynomial.factorial_smul_hasseDeriv
+
+theorem hasseDeriv_comp (k l : ℕ) :
+    (@hasseDeriv R _ k).comp (hasseDeriv l) = (k + l).choose k • hasseDeriv (k + l) := by
+  ext i : 2
+  simp only [LinearMap.smul_apply, comp_apply, LinearMap.coe_comp, smul_monomial, hasseDeriv_apply,
+    mul_one, monomial_eq_zero_iff, sum_monomial_index, mul_zero, ←
+    tsub_add_eq_tsub_tsub, add_comm l k]
+  rw_mod_cast [nsmul_eq_mul]
+  rw [← Nat.cast_mul]
+  congr 2
+  by_cases hikl : i < k + l
+  · rw [choose_eq_zero_of_lt hikl, mul_zero]
+    by_cases hil : i < l
+    · rw [choose_eq_zero_of_lt hil, mul_zero]
+    · push_neg at hil
+      rw [← tsub_lt_iff_right hil] at hikl
+      rw [choose_eq_zero_of_lt hikl, zero_mul]
+  push_neg at hikl
+  apply @cast_injective ℚ
+  have h1 : l ≤ i := le_of_add_le_right hikl
+  have h2 : k ≤ i - l := le_tsub_of_add_le_right hikl
+  have h3 : k ≤ k + l := le_self_add
+  push_cast
+  rw [cast_choose ℚ h1, cast_choose ℚ h2, cast_choose ℚ h3, cast_choose ℚ hikl]
+  rw [show i - (k + l) = i - l - k by rw [add_comm]; apply tsub_add_eq_tsub_tsub]
+  simp only [add_tsub_cancel_left]
+  field_simp; ring
+#align polynomial.hasse_deriv_comp Polynomial.hasseDeriv_comp
+
+/-- The left sum of the `m`-th residue product `A(z)_n B(z)`, given by the residue of
+`(x-y)^m A(x)B(y)` at `|x| > |y|`. -/
+def res_prod_left (A B : VertexOperator R V) (m : ℤ) : VertexOperator R V :=
+  {
+    toMap :=
+    {
+      toFun := sorry
+      map_add' := sorry
+      map_smul' := sorry
+    }
+  }
+
+/-- The right sum of the `m`-th residue product `A(z)_n B(z)`, given by the residue of
+`(x-y)^m B(y)A(x)` at `|y| > |x|`. -/
+def res_prod_right (A B : VertexOperator R V) (m : ℤ)
+
+residue products: to show that the residue product is a vertex operator, we need
+-- residue products, order of associativity (uses residue products)
+
+-/
 
 end VertexAlg
 
--- Mathlib/RingTheory/Binomial.lean
+++ Mathlib/RingTheory/Binomial.lean
@@ -10,7 +10,7 @@ import Mathlib.Data.Polynomial.Smeval
 /-!
 # Binomial rings
 
-In this file we introduce the binomial property as a Prop-valued mixin, and define the `multichoose`
+In this file we introduce the binomial property as a mixin, and define the `multichoose`
 and `choose` functions generalizing binomial coefficients.
 
 According to our main reference [elliott2006binomial] (which lists many equivalent conditions), a
@@ -20,10 +20,10 @@ unambiguously, so we get uniquely defined binomial coefficients.
 
 The defining condition doesn't require commutativity or associativity, and we get a theory with
 essentially the same power by replacing subtraction with addition.  Thus, we consider any
-non-associative semiring `R` with notion of natural nunber exponents, in which multiplication by
-factorials is injective, and demand that the evaluation of the ascending Pochhammer polynomial
-`X(X+1)⋯(X+(k-1))` at any element is divisible by `k!`.  The quotient is called `multichoose r k`,
-following the convention given for natural numbers.
+non-associative semiring `R` with notion of natural number exponents, in which scalar multiplication
+by positive integers is injective, and demand that the evaluation of the ascending Pochhammer
+polynomial `X(X+1)⋯(X+(k-1))` at any element `r` is divisible by `k!`.  The quotient is called
+`multichoose r k`, following the convention given for natural numbers.
 
 ## References
 
@@ -31,50 +31,60 @@ following the convention given for natural numbers.
 
 ## To Do
 
-Replace Nat.multichoose with Ring.multichoose (or Nat.choose with adjusted parameters)
+Further results in Elliot's paper:
 
-* Data.List.Sym done
-* Data.Sym.Card done
+* A CommRing is binomial if and only if it admits a λ-ring structure with trivial Adams operations.
 
+* The free commutative binomial ring on a set `X` is the ring of integer-valued polynomials in the
+variables `X`.  (also, noncommutative version?)
+
+* Given a commutative binomial ring `A` and an `A`-algebra `B` that is complete with respect to an
+ideal `I`, formal exponentiation induces an `A`-module structure on `1 + I`.
 
 -/
 
 section Multichoose
 
 open Function
-
-/-- A mixin for multi-binomial coefficients. -/
-class IsBinomialRing (R : Type*) [NonAssocSemiring R] [Pow R ℕ] where
+/-- A binomial ring is a ring for which ascending Pochhammer evaluations are uniquely divisible by
+suitable factorials.  We define this notion as a mixin for non-associative semirings, but retain the
+ring name.  We introduce `Ring.multichoose` as the uniquely defined quotient. -/
+class BinomialRing (R : Type*) [NonAssocSemiring R] [Pow R ℕ] where
   /-- Scalar multiplication by positive integers is injective. -/
 nsmul_right_injective (n : ℕ) (h : n ≠ 0) : Injective (n • · : R → R)
   /-- A multichoose function, giving the quotient of Pochhammer by factorial -/
   multichoose : R → ℕ → R
   /-- The `n`th ascending Pochhammer polynomial evaluated at any element is divisible by `n!`. -/
-  factorial_smul_multichoose : ∀ (r : R) (n : ℕ),
+  factorial_nsmul_multichoose : ∀ (r : R) (n : ℕ),
     n.factorial • multichoose r n = Polynomial.smeval r (ascPochhammer ℕ n)
 
 namespace Ring
 
 open Polynomial
 
-theorem nsmul_right_injective (R : Type*) [NonAssocSemiring R] [Pow R ℕ] [IsBinomialRing R] (n : ℕ)
-    (h : n ≠ 0) : Injective (n • · : R → R) := IsBinomialRing.nsmul_right_injective n h
+theorem nsmul_right_injective (R : Type*) [NonAssocSemiring R] [Pow R ℕ] [BinomialRing R] (n : ℕ)
+    (h : n ≠ 0) : Injective (n • · : R → R) := BinomialRing.nsmul_right_injective n h
 
-variable {R : Type*} [NonAssocSemiring R] [Pow R ℕ] [IsBinomialRing R]
+variable {R : Type*} [NonAssocSemiring R] [Pow R ℕ] [BinomialRing R]
 
-/-- This is a generalization of the combinatorial multichoose function, given by choosing with
-replacement. -/
+/-- The multichoose function is the quotient of ascending Pochhammer evaluation by the corresponding
+factorial. When applied to natural numbers, `multichoose k n` describes choosing a multiset of `n`
+items from a group of `k`, i.e., choosing with replacement. -/
 def multichoose (r : R) (n : ℕ) : R :=
-  IsBinomialRing.multichoose r n
+  BinomialRing.multichoose r n
 
-theorem factorial_smul_multichoose_eq_ascPochhammer (r : R) (n : ℕ) :
+@[simp]
+theorem multichoose_eq_multichoose (r : R) (n : ℕ) :
+    BinomialRing.multichoose r n = multichoose r n := rfl
+
+theorem factorial_nsmul_multichoose_eq_ascPochhammer (r : R) (n : ℕ) :
     n.factorial • multichoose r n = smeval r (ascPochhammer ℕ n) :=
-  IsBinomialRing.factorial_smul_multichoose r n
+  BinomialRing.factorial_nsmul_multichoose r n
 
 theorem multichoose_zero_right' (r : R) : multichoose r 0 = r ^ 0 := by
   refine nsmul_right_injective R (Nat.factorial 0) (Nat.factorial_ne_zero 0) ?_
   simp only
-  rw [factorial_smul_multichoose_eq_ascPochhammer, ascPochhammer_zero, smeval_one, Nat.factorial]
+  rw [factorial_nsmul_multichoose_eq_ascPochhammer, ascPochhammer_zero, smeval_one, Nat.factorial]
 
 @[simp]
 theorem multichoose_zero_right [NatPowAssoc R]
@@ -86,7 +96,7 @@ theorem multichoose_zero_succ [NatPowAssoc R]
     (k : ℕ) : multichoose (0 : R) (k + 1) = 0 := by
   refine nsmul_right_injective R (Nat.factorial (k + 1)) (Nat.factorial_ne_zero (k + 1)) ?_
   simp only
-  rw [factorial_smul_multichoose_eq_ascPochhammer, smul_zero, ascPochhammer_succ_left, smeval_X_mul,
+  rw [factorial_nsmul_multichoose_eq_ascPochhammer, smul_zero, ascPochhammer_succ_left, smeval_X_mul,
     zero_mul]
 
 theorem ascPochhammer_succ_succ [NatPowAssoc R] (r : R) (k : ℕ) :
@@ -94,7 +104,7 @@ theorem ascPochhammer_succ_succ [NatPowAssoc R] (r : R) (k : ℕ) :
     smeval r (ascPochhammer ℕ (k + 1)) := by
   nth_rw 1 [ascPochhammer_succ_right, ascPochhammer_succ_left, mul_comm (ascPochhammer ℕ k)]
   simp only [smeval_mul, smeval_comp ℕ r, smeval_add, smeval_X]
-  rw [Nat.factorial, mul_smul, factorial_smul_multichoose_eq_ascPochhammer]
+  rw [Nat.factorial, mul_smul, factorial_nsmul_multichoose_eq_ascPochhammer]
   simp only [smeval_one, npow_one, npow_zero, one_smul]
   rw [← C_eq_nat_cast, smeval_C, npow_zero, add_assoc, add_mul, add_comm 1, @nsmul_one, add_mul]
   rw [← @nsmul_eq_mul, @add_rotate', @succ_nsmul', add_assoc]
@@ -103,7 +113,7 @@ theorem ascPochhammer_succ_succ [NatPowAssoc R] (r : R) (k : ℕ) :
 theorem multichoose_succ_succ [NatPowAssoc R] (r : R) (k : ℕ) :
     multichoose (r + 1) (k + 1) = multichoose r (k + 1) + multichoose (r + 1) k := by
   refine nsmul_right_injective R (Nat.factorial (k + 1)) (Nat.factorial_ne_zero (k + 1)) ?_
-  simp only [factorial_smul_multichoose_eq_ascPochhammer, smul_add]
+  simp only [factorial_nsmul_multichoose_eq_ascPochhammer, smul_add]
   rw [add_comm (smeval r (ascPochhammer ℕ (k+1)))]
   exact @ascPochhammer_succ_succ R _ _ _ _ r k
 
@@ -128,7 +138,7 @@ theorem multichoose_two [NatPowAssoc R] (k : ℕ) : multichoose (2 : R) k = k + 
 theorem multichoose_one_right' (r : R) : multichoose r 1 = r ^ 1 := by
   refine nsmul_right_injective R (Nat.factorial 1) (Nat.factorial_ne_zero 1) ?_
   simp only
-  rw [factorial_smul_multichoose_eq_ascPochhammer, ascPochhammer_one, smeval_X, Nat.factorial_one,
+  rw [factorial_nsmul_multichoose_eq_ascPochhammer, ascPochhammer_one, smeval_X, Nat.factorial_one,
     one_smul]
 
 theorem multichoose_one_right [NatPowAssoc R] (r : R) : multichoose r 1 = r := by
@@ -144,14 +154,12 @@ namespace Ring
 
 open Polynomial
 
-instance naturals_binomial : IsBinomialRing ℕ := by
-  refine IsBinomialRing.mk ?nsmul_right_injective ?multichoose ?factorial_mul_multichoose
-  intro n npos r s h
-  exact Nat.eq_of_mul_eq_mul_left (Nat.pos_of_ne_zero npos) h
-  use fun n k => Nat.choose (n + k - 1) k
-  intro n k
-  rw [smul_eq_mul, ← Nat.descFactorial_eq_factorial_mul_choose,
-    smeval_eq_eval (ascPochhammer ℕ k) n, ascPochhammer_nat_eq_descFactorial]
+instance Nat.instBinomialRing : BinomialRing ℕ where
+  nsmul_right_injective n hn r s hrs := Nat.eq_of_mul_eq_mul_left (Nat.pos_of_ne_zero hn) hrs
+  multichoose := fun n k => Nat.choose (n + k - 1) k
+  factorial_nsmul_multichoose r n := by
+    rw [smul_eq_mul, ← Nat.descFactorial_eq_factorial_mul_choose,
+      smeval_eq_eval (ascPochhammer ℕ n) r, ascPochhammer_nat_eq_descFactorial]
 
 theorem multichoose_eq (n k : ℕ) : multichoose n k = (n + k - 1).choose k := rfl
 
@@ -167,26 +175,24 @@ theorem ascPochhammer_smeval_eq_eval {R : Type*} [Semiring R] (r : R) (k : ℕ) 
       pow_zero, nsmul_one, Nat.cast_id, eval_add, eval_mul_X, ← Nat.cast_comm, eval_nat_cast_mul,
       mul_add, Nat.cast_comm]
 
-instance integers_binomial_ring : IsBinomialRing ℤ := by
-  refine IsBinomialRing.mk ?_ ?_ ?_
-  intro n hn r s hmul
-  exact nat_mul_inj' hmul hn
-  intro r k
-  cases r with
-  | ofNat n =>
-    use ((Nat.choose (n + k - 1) k):ℤ)
-  | negSucc n =>
-    use (-1)^k * Nat.choose n.succ k
-  intro r k
-  cases r with
-  | ofNat n =>
-    simp only [nsmul_eq_mul, Int.ofNat_eq_coe, Int.ofNat_mul_out]
-    rw [← Nat.descFactorial_eq_factorial_mul_choose, @smeval_at_nat_cast,
-      smeval_eq_eval (ascPochhammer ℕ k) n, ascPochhammer_nat_eq_descFactorial]
-  | negSucc n =>
-    rw [nsmul_eq_mul, mul_comm, mul_assoc, ← Nat.cast_mul, mul_comm _ (k.factorial),
-      ← Nat.descFactorial_eq_factorial_mul_choose, ← descPochhammer_int_eq_descFactorial,
-      ascPochhammer_smeval_eq_eval, ← Int.neg_ofNat_succ, ascPochhammer_eval_neg_eq_descPochhammer]
+instance Int.instBinomialRing : BinomialRing ℤ where
+  nsmul_right_injective n hn r s hrs := Int.eq_of_mul_eq_mul_left (Int.ofNat_ne_zero.mpr hn) hrs
+  multichoose r k := by
+    cases r with
+    | ofNat n =>
+      use ((Nat.choose (n + k - 1) k):ℤ)
+    | negSucc n => use (-1)^k * Nat.choose n.succ k
+  factorial_nsmul_multichoose r k := by
+    cases r with
+    | ofNat n =>
+      simp only [nsmul_eq_mul, Int.ofNat_eq_coe, Int.ofNat_mul_out]
+      rw [← Nat.descFactorial_eq_factorial_mul_choose, @smeval_at_nat_cast,
+        smeval_eq_eval (ascPochhammer ℕ k) n, ascPochhammer_nat_eq_descFactorial]
+    | negSucc n =>
+      rw [nsmul_eq_mul, mul_comm, mul_assoc, ← Nat.cast_mul, mul_comm _ (k.factorial),
+        ← Nat.descFactorial_eq_factorial_mul_choose, ← descPochhammer_int_eq_descFactorial,
+        ascPochhammer_smeval_eq_eval, ← Int.neg_ofNat_succ,
+        ascPochhammer_eval_neg_eq_descPochhammer]
 
 end Ring
 
@@ -200,7 +206,7 @@ open Polynomial
 
 variable {R : Type*}
 
-variable [NonAssocRing R] [Pow R ℕ] [IsBinomialRing R]
+variable [NonAssocRing R] [Pow R ℕ] [BinomialRing R]
 
 theorem ascPochhammer_smeval_neg : ∀(n : ℕ),
     smeval (-n : ℤ) (ascPochhammer ℕ n) = (-1)^n * n.factorial
@@ -241,30 +247,30 @@ theorem multichoose_neg (n : ℕ) : multichoose (-n : ℤ) n = (-1)^n := by
     refine @nsmul_right_injective ℤ _ _ _ (Nat.factorial n) (Nat.factorial_ne_zero n)
       (multichoose (-n : ℤ) n) ((-1)^n) ?_
     simp only
-    rw [factorial_smul_multichoose_eq_ascPochhammer, ascPochhammer_smeval_neg, nsmul_eq_mul,
+    rw [factorial_nsmul_multichoose_eq_ascPochhammer, ascPochhammer_smeval_neg, nsmul_eq_mul,
       Nat.cast_comm]
 
 theorem multichoose_succ_neg (n : ℕ) : multichoose (-n : ℤ) (n + 1) = 0 := by
   refine @nsmul_right_injective ℤ _ _ _ (Nat.factorial (n + 1)) (Nat.factorial_ne_zero (n + 1))
     (multichoose (-n : ℤ) (n + 1)) 0 ?_
   simp only
-  rw [factorial_smul_multichoose_eq_ascPochhammer, ascPochhammer_succ_smeval_neg, smul_zero]
+  rw [factorial_nsmul_multichoose_eq_ascPochhammer, ascPochhammer_succ_smeval_neg, smul_zero]
 
 theorem multichoose_neg_add (n k : ℕ) : multichoose (-n : ℤ) (n + k + 1) = 0 := by
   refine nsmul_right_injective ℤ (Nat.factorial (n + k + 1)) (Nat.factorial_ne_zero (n + k + 1)) ?_
   simp only
-  rw [factorial_smul_multichoose_eq_ascPochhammer, ascPochhammer_smeval_neg_add, smul_zero]
+  rw [factorial_nsmul_multichoose_eq_ascPochhammer, ascPochhammer_smeval_neg_add, smul_zero]
 
 theorem multichoose_neg_lt (n k : ℕ) (h : n < k) : multichoose (-n : ℤ) k = 0 := by
   refine nsmul_right_injective ℤ (Nat.factorial k) (Nat.factorial_ne_zero k) ?_
   simp only
-  rw [factorial_smul_multichoose_eq_ascPochhammer, ascPochhammer_smeval_neg_lt n k h, smul_zero]
+  rw [factorial_nsmul_multichoose_eq_ascPochhammer, ascPochhammer_smeval_neg_lt n k h, smul_zero]
 
 theorem multichoose_succ_neg_cast [NatPowAssoc R] (n : ℕ) :
     multichoose (-n : R) (n + 1) = 0 := by
   refine nsmul_right_injective R (Nat.factorial (n + 1)) (Nat.factorial_ne_zero (n + 1)) ?_
   simp only
-  rw [factorial_smul_multichoose_eq_ascPochhammer, smul_zero, smeval_at_neg_nat,
+  rw [factorial_nsmul_multichoose_eq_ascPochhammer, smul_zero, smeval_at_neg_nat,
     ascPochhammer_succ_smeval_neg, Int.cast_zero]
 
 theorem ascPochhammer_smeval_nat_int [NatPowAssoc R] (r : R) : ∀(n : ℕ),
@@ -279,14 +285,14 @@ theorem ascPochhammer_smeval_nat_int [NatPowAssoc R] (r : R) : ∀(n : ℕ),
 
 /-- The binomial coefficient `choose r n` generalizes the natural number choose function,
   interpreted in terms of choosing without replacement. -/
-noncomputable def choose {R: Type _} [NonAssocRing R] [Pow R ℕ] [IsBinomialRing R]
+noncomputable def choose {R: Type _} [NonAssocRing R] [Pow R ℕ] [BinomialRing R]
     (r : R) (n : ℕ): R :=
   multichoose (r-n+1) n
 
 theorem descPochhammer_eq_factorial_smul_choose [NatPowAssoc R] (r : R) (n : ℕ) :
     smeval r (descPochhammer ℤ n) = n.factorial • choose r n := by
   unfold choose
-  rw [factorial_smul_multichoose_eq_ascPochhammer, descPochhammer_eq_ascPochhammer, smeval_comp ℤ r,
+  rw [factorial_nsmul_multichoose_eq_ascPochhammer, descPochhammer_eq_ascPochhammer, smeval_comp ℤ r,
     add_comm_sub, smeval_add, smeval_X, npow_one]
   have h : smeval r (1 - n : Polynomial ℤ) = 1 - n := by
     rw [← C_eq_nat_cast, ← C_1, ← C_sub, smeval_C]
@@ -297,23 +303,23 @@ theorem choose_zero_right' (r : R) : choose r 0 = (r + 1) ^ 0 := by
   unfold choose
   refine nsmul_right_injective R (Nat.factorial 0) (Nat.factorial_ne_zero 0) ?_
   simp only
-  rw [factorial_smul_multichoose_eq_ascPochhammer, Nat.factorial_zero, ascPochhammer_zero,
+  rw [factorial_nsmul_multichoose_eq_ascPochhammer, Nat.factorial_zero, ascPochhammer_zero,
     smeval_one, one_smul, one_smul, Nat.cast_zero, sub_zero]
 
 theorem choose_zero_right [NatPowAssoc R] (r : R) : choose r 0 = 1 := by
   rw [choose_zero_right', npow_zero]
 
-theorem choose_zero_succ (S : Type*) [NonAssocRing S] [Pow S ℕ] [NatPowAssoc S] [IsBinomialRing S]
+theorem choose_zero_succ (S : Type*) [NonAssocRing S] [Pow S ℕ] [NatPowAssoc S] [BinomialRing S]
     (n : ℕ) : choose (0 : S) (Nat.succ n) = 0 := by
   unfold choose
   rw [Nat.cast_succ, zero_sub, neg_add, neg_add_cancel_right, ← Nat.add_one,
     multichoose_succ_neg_cast]
 
-theorem choose_zero_pos (S : Type*) [NonAssocRing S] [Pow S ℕ] [NatPowAssoc S] [IsBinomialRing S]
+theorem choose_zero_pos (S : Type*) [NonAssocRing S] [Pow S ℕ] [NatPowAssoc S] [BinomialRing S]
     (k : ℕ) (h_pos: 0 < k) : choose (0 : S) k = 0 := by
   rw [← Nat.succ_pred_eq_of_pos h_pos, choose_zero_succ]
 
-theorem choose_zero_ite (S : Type*) [NonAssocRing S] [Pow S ℕ] [NatPowAssoc S] [IsBinomialRing S]
+theorem choose_zero_ite (S : Type*) [NonAssocRing S] [Pow S ℕ] [NatPowAssoc S] [BinomialRing S]
     (k : ℕ) : choose (0 : S) k = if k = 0 then 1 else 0 := by
   rw [eq_ite_iff]
   by_cases hk: k = 0
