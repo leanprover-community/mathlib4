97cba027a5dc23da6172f3351615e3de3c85656e
Arend Mellendijk <arend.mellendijk@gmail.com>
2023-12-28 00:40:09 +0100

    add tactic imports

--- Mathlib/Tactic.lean
+++ Mathlib/Tactic.lean
@@ -91,6 +91,8 @@ import Mathlib.Tactic.Monotonicity.Attr
 import Mathlib.Tactic.Monotonicity.Basic
 import Mathlib.Tactic.Monotonicity.Lemmas
 import Mathlib.Tactic.MoveAdd
+import Mathlib.Tactic.Multiplicativity
+import Mathlib.Tactic.Multiplicativity.Init
 import Mathlib.Tactic.NoncommRing
 import Mathlib.Tactic.Nontriviality
 import Mathlib.Tactic.Nontriviality.Core
