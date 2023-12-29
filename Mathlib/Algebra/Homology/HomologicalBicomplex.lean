6de21981a7c25e0766ee8278f219a1829f7d5c9f
Joël Riou <joel.riou@universite-paris-saclay.fr>
2023-12-29 17:19:11 +0100

    introduced HomologicalComplex₂

--- Mathlib/Algebra/Homology/HomologicalBicomplex.lean
+++ Mathlib/Algebra/Homology/HomologicalBicomplex.lean
@@ -1,125 +1,127 @@
 /-
 Copyright (c) 2021 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Scott Morrison
+Authors: Scott Morrison, Joël Riou
 -/
 import Mathlib.Algebra.Homology.HomologicalComplex
 
 #align_import algebra.homology.flip from "leanprover-community/mathlib"@"ff511590476ef357b6132a45816adc120d5d7b1d"
 
 /-!
-# Flip a complex of complexes
+# Bicomplexes
 
-For now we don't have double complexes as a distinct thing,
-but we can model them as complexes of complexes.
+Given a category `C` with zero morphisms and two complex shapes
+`c₁ : ComplexShape I₁` and `c₂ : ComplexShape I₂`, we define
+the type of bicomplexes `HomologicalComplex₂ C c₁ c₂` as an
+abbreviation for `HomologicalComplex (HomologicalComplex C c₂) c₁`.
+In particular, if `K : HomologicalComplex₂ C c₁ c₂`, then
+for each `i₁ : I₁`, `K.X i₁` is a column of `K`.
 
-Here we show how to flip a complex of complexes over the diagonal,
-exchanging the horizontal and vertical directions.
+In this file, we obtain the equivalence of categories
+`HomologicalComplex₂.flipEquivalence : HomologicalComplex₂ C c₁ c₂ ≌ HomologicalComplex₂ C c₂ c₁`
+which is obtained by exchanging the horizontal and vertical directions.
 
 -/
 
 
-universe v u
+open CategoryTheory Limits
 
-open CategoryTheory CategoryTheory.Limits
+variable (C : Type*) [Category C] [HasZeroMorphisms C]
+  {I₁ I₂ : Type*} (c₁ : ComplexShape I₁) (c₂ : ComplexShape I₂)
 
-namespace HomologicalComplex
+/-- Given a category `C` and two complex shapes `c₁` and `c₂` on types `I₁` and `I₂`,
+the associated type of bicomplexes `HomologicalComplex₂ C c₁ c₂` is
+`K : HomologicalComplex (HomologicalComplex C c₂) c₁`. Then, the object in
+position `⟨i₁, i₂⟩` can be obtained as `(K.X i₁).X i₂`. -/
+abbrev HomologicalComplex₂ :=
+  HomologicalComplex (HomologicalComplex C c₂) c₁
 
-variable {V : Type u} [Category.{v} V] [HasZeroMorphisms V]
+namespace HomologicalComplex₂
 
-variable {ι : Type*} {c : ComplexShape ι} {ι' : Type*} {c' : ComplexShape ι'}
+open HomologicalComplex
+
+variable {C c₁ c₂}
+
+/-- The graded object indexed by `I₁ × I₂` induced by a bicomplex. -/
+def toGradedObject (K : HomologicalComplex₂ C c₁ c₂) :
+    GradedObject (I₁ × I₂) C :=
+  fun ⟨i₁, i₂⟩ => (K.X i₁).X i₂
+
+lemma shape_f (K : HomologicalComplex₂ C c₁ c₂) (i₁ i₁' : I₁) (h : ¬ c₁.Rel i₁ i₁') (i₂ : I₂) :
+    (K.d i₁ i₁').f i₂ = 0 := by
+  rw [K.shape _ _ h, zero_f]
+
+@[reassoc (attr := simp)]
+lemma d_f_comp_d_f (K : HomologicalComplex₂ C c₁ c₂)
+    (i₁ i₁' i₁'' : I₁) (i₂ : I₂) :
+    (K.d i₁ i₁').f i₂ ≫ (K.d i₁' i₁'').f i₂ = 0 := by
+  rw [← comp_f, d_comp_d, zero_f]
+
+@[reassoc]
+lemma d_comm (K : HomologicalComplex₂ C c₁ c₂) (i₁ i₁' : I₁) (i₂ i₂' : I₂) :
+    (K.d i₁ i₁').f i₂ ≫ (K.X i₁').d i₂ i₂' = (K.X i₁).d i₂ i₂' ≫ (K.d i₁ i₁').f i₂' := by
+  simp
+
+@[reassoc (attr := simp)]
+lemma comm_f {K L : HomologicalComplex₂ C c₁ c₂} (f : K ⟶ L) (i₁ i₁' : I₁) (i₂ : I₂) :
+    (f.f i₁).f i₂ ≫ (L.d i₁ i₁').f i₂ = (K.d i₁ i₁').f i₂ ≫ (f.f i₁').f i₂ :=
+  congr_hom (f.comm i₁ i₁') i₂
 
 /-- Flip a complex of complexes over the diagonal,
 exchanging the horizontal and vertical directions.
 -/
 @[simps]
-def flipObj (C : HomologicalComplex (HomologicalComplex V c) c') :
-    HomologicalComplex (HomologicalComplex V c') c where
+def flip (K : HomologicalComplex₂ C c₁ c₂) : HomologicalComplex₂ C c₂ c₁ where
   X i :=
-    { X := fun j => (C.X j).X i
-      d := fun j j' => (C.d j j').f i
-      shape := fun j j' w => by
-        simp_all only [not_false_eq_true, shape, zero_f]
-      d_comp_d' := fun j₁ j₂ j₃ _ _ => congr_hom (C.d_comp_d j₁ j₂ j₃) i }
-  d i i' :=
-    { f := fun j => (C.X j).d i i'
-      comm' := fun j j' _ => ((C.d j j').comm i i').symm }
+    { X := fun j => (K.X j).X i
+      d := fun j j' => (K.d j j').f i
+      shape := fun j j' w => K.shape_f _ _ w i }
+  d i i' := { f := fun j => (K.X j).d i i' }
   shape i i' w := by
     ext j
-    exact (C.X j).shape i i' w
-#align homological_complex.flip_obj HomologicalComplex.flipObj
+    exact (K.X j).shape i i' w
+#align homological_complex.flip_obj HomologicalComplex₂.flip
 
-variable (V c c')
+variable (C c₁ c₂)
 
 /-- Flipping a complex of complexes over the diagonal, as a functor. -/
 @[simps]
-def flip :
-    HomologicalComplex (HomologicalComplex V c) c' ⥤ HomologicalComplex (HomologicalComplex V c') c
-    where
-  obj C := flipObj C
-  map {C D} f :=
+def flipFunctor :
+    HomologicalComplex₂ C c₁ c₂ ⥤ HomologicalComplex₂ C c₂ c₁ where
+  obj K := K.flip
+  map {K L} f :=
     { f := fun i =>
         { f := fun j => (f.f j).f i
-          comm' := fun j j' _ => congr_hom (f.comm j j') i } }
-#align homological_complex.flip HomologicalComplex.flip
+          comm' := by intros; simp }
+      comm' := by intros; ext; simp }
+#align homological_complex.flip HomologicalComplex₂.flipFunctor
 
-/-- Auxiliary definition for `HomologicalComplex.flipEquivalence`. -/
+/-- Auxiliary definition for `HomologicalComplex₂.flipEquivalence`. -/
 @[simps!]
 def flipEquivalenceUnitIso :
-    𝟭 (HomologicalComplex (HomologicalComplex V c) c') ≅ flip V c c' ⋙ flip V c' c :=
-  NatIso.ofComponents
-    (fun C =>
-      { hom :=
-          { f := fun i => { f := fun j => 𝟙 ((C.X i).X j) }
-            comm' := fun i j _ => by
-              ext
-              dsimp
-              simp only [Category.id_comp, Category.comp_id] }
-        inv :=
-          { f := fun i => { f := fun j => 𝟙 ((C.X i).X j) }
-            comm' := fun i j _ => by
-              ext
-              dsimp
-              simp only [Category.id_comp, Category.comp_id] } })
-    fun {X Y} f => by
-      ext
-      dsimp
-      simp only [Category.id_comp, Category.comp_id]
-#align homological_complex.flip_equivalence_unit_iso HomologicalComplex.flipEquivalenceUnitIso
+    𝟭 (HomologicalComplex₂ C c₁ c₂) ≅ flipFunctor C c₁ c₂ ⋙ flipFunctor C c₂ c₁ :=
+  NatIso.ofComponents (fun K => HomologicalComplex.Hom.isoOfComponents (fun i₁ =>
+    HomologicalComplex.Hom.isoOfComponents (fun i₂ => Iso.refl _)
+    (by aesop_cat)) (by aesop_cat)) (by aesop_cat)
+#align homological_complex.flip_equivalence_unit_iso HomologicalComplex₂.flipEquivalenceUnitIso
 
-/-- Auxiliary definition for `HomologicalComplex.flipEquivalence`. -/
+/-- Auxiliary definition for `HomologicalComplex₂.flipEquivalence`. -/
 @[simps!]
 def flipEquivalenceCounitIso :
-    flip V c' c ⋙ flip V c c' ≅ 𝟭 (HomologicalComplex (HomologicalComplex V c') c) :=
-  NatIso.ofComponents
-    (fun C =>
-      { hom :=
-          { f := fun i => { f := fun j => 𝟙 ((C.X i).X j) }
-            comm' := fun i j _ => by
-              ext
-              dsimp
-              simp only [Category.id_comp, Category.comp_id] }
-        inv :=
-          { f := fun i => { f := fun j => 𝟙 ((C.X i).X j) }
-            comm' := fun i j _ => by
-              ext
-              dsimp
-              simp only [Category.id_comp, Category.comp_id] } })
-    fun {X Y} f => by
-      ext
-      dsimp
-      simp only [Category.id_comp, Category.comp_id]
-#align homological_complex.flip_equivalence_counit_iso HomologicalComplex.flipEquivalenceCounitIso
+    flipFunctor C c₂ c₁ ⋙ flipFunctor C c₁ c₂ ≅ 𝟭 (HomologicalComplex₂ C c₂ c₁) :=
+  NatIso.ofComponents (fun K => HomologicalComplex.Hom.isoOfComponents (fun i₂ =>
+    HomologicalComplex.Hom.isoOfComponents (fun i₁ => Iso.refl _)
+    (by aesop_cat)) (by aesop_cat)) (by aesop_cat)
+#align homological_complex.flip_equivalence_counit_iso HomologicalComplex₂.flipEquivalenceCounitIso
 
 /-- Flipping a complex of complexes over the diagonal, as an equivalence of categories. -/
 @[simps]
 def flipEquivalence :
-    HomologicalComplex (HomologicalComplex V c) c' ≌ HomologicalComplex (HomologicalComplex V c') c
-    where
-  functor := flip V c c'
-  inverse := flip V c' c
-  unitIso := flipEquivalenceUnitIso V c c'
-  counitIso := flipEquivalenceCounitIso V c c'
-#align homological_complex.flip_equivalence HomologicalComplex.flipEquivalence
+    HomologicalComplex₂ C c₁ c₂ ≌ HomologicalComplex₂ C c₂ c₁ where
+  functor := flipFunctor C c₁ c₂
+  inverse := flipFunctor C c₂ c₁
+  unitIso := flipEquivalenceUnitIso C c₁ c₂
+  counitIso := flipEquivalenceCounitIso C c₁ c₂
+#align homological_complex.flip_equivalence HomologicalComplex₂.flipEquivalence
 
-end HomologicalComplex
+end HomologicalComplex₂
