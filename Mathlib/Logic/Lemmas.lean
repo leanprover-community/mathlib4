a2bc091a6cc34b45b0d6e6195fba03d91a03cf01
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2023-12-29 19:25:42 -0300

    Delete `import Mathlib.Init.Function` in `Mathlib/Logic/Lemmas.lean`

--- Mathlib/Logic/Lemmas.lean
+++ Mathlib/Logic/Lemmas.lean
@@ -3,7 +3,6 @@ Copyright (c) 2022 Yaël Dillies. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yaël Dillies
 -/
-import Mathlib.Init.Function
 import Mathlib.Logic.Basic
 import Mathlib.Tactic.Convert
 import Mathlib.Tactic.SplitIfs
