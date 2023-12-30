cd0f48a2a0a2d3a1def6f4b905c31626b6392919
Christopher Hoskin <christopher.hoskin@gmail.com>
2023-12-30 13:06:56 +0000

    Lint

--- Mathlib/LinearAlgebra/Matrix/PosDef.lean
+++ Mathlib/LinearAlgebra/Matrix/PosDef.lean
@@ -292,7 +292,8 @@ theorem PosSemidef.dotProduct_mulVec_zero_iff [DecidableEq n]
 theorem PosSemidef.toLinearMap₂'_zero_iff [DecidableEq n]
     {A : Matrix n n 𝕜} (hA : PosSemidef A) (x : n → 𝕜) :
     Matrix.toLinearMap₂' A (star x) x = 0 ↔ Matrix.toLin' A x = 0 := by
-    simpa [toLinearMap₂'_apply', toLin'_apply, ScalarMatrix.mulVec_eq_Matrix_mulVec] using hA.dotProduct_mulVec_zero_iff x
+    simpa [toLinearMap₂'_apply', toLin'_apply, ScalarMatrix.mulVec_eq_Matrix_mulVec]
+      using hA.dotProduct_mulVec_zero_iff x
 
 /-!
 ## Positive definite matrices
