0d9f91bce233bc31a8f19ead79c150d5f30ac446
Alex J Best <alex.j.best@gmail.com>
2023-12-30 14:58:35 +0000

    doc

--- Mathlib/Tactic/TryAssumption.lean
+++ Mathlib/Tactic/TryAssumption.lean
@@ -5,5 +5,17 @@ Authors: Alex J. Best
 -/
 import Std.Tactic.ShowTerm
 
+/-!
+# A helper macro for replacing assumptions with exact statements
+
+## Summary
+
+This file adds a `assumption?` macro that expands to `show_term assumption`
+the goal is to make it more efficient to clean up proofs with `assumption`s in by making them
+more explicit.
+
+-/
+
+
 /-- `assumption?` is equivalent to `show_term assumption` -/
 macro "assumption?" : tactic => `(tactic| show_term assumption)
