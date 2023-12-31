266747c740568780e944f8ad1a5c6c1d956e73c4
erd1 <the.erd.one@gmail.com>
2023-12-31 14:19:07 +0800

    fix

--- Mathlib.lean
+++ Mathlib.lean
@@ -2404,7 +2404,6 @@ import Mathlib.LinearAlgebra.FreeModule.Basic
 import Mathlib.LinearAlgebra.FreeModule.Determinant
 import Mathlib.LinearAlgebra.FreeModule.Finite.Basic
 import Mathlib.LinearAlgebra.FreeModule.Finite.Matrix
-import Mathlib.LinearAlgebra.FreeModule.Finite.Rank
 import Mathlib.LinearAlgebra.FreeModule.IdealQuotient
 import Mathlib.LinearAlgebra.FreeModule.Norm
 import Mathlib.LinearAlgebra.FreeModule.PID
