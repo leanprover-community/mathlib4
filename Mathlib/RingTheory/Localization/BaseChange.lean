b3691ce83ec50ca807e7740a045f253dd6a7a7b1
Xavier-François Roblot <46200072+xroblot@users.noreply.github.com>
2023-12-31 15:46:30 +0100

    Fix authors

--- Mathlib/RingTheory/Localization/BaseChange.lean
+++ Mathlib/RingTheory/Localization/BaseChange.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2022 Anne Baanen. All rights reserved.
+Copyright (c) 2022 Jujian Zhang. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Junyan Xu, Anne Baanen
+Authors: Andrew Yang, Jujian Zhang
 -/
 import Mathlib.RingTheory.IsTensorProduct
 import Mathlib.RingTheory.Localization.Module
