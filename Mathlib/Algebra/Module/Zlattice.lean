3d52a6a8c1b62b73f9aa89214981d31cbc2eebf4
Andrew Yang <36414270+erdOne@users.noreply.github.com>
2023-12-31 13:53:46 +0800

    Update Zlattice.lean

--- Mathlib/Algebra/Module/Zlattice.lean
+++ Mathlib/Algebra/Module/Zlattice.lean
@@ -418,7 +418,7 @@ theorem Zlattice.module_free : Module.Free ℤ L := by
   have : NoZeroSMulDivisors ℤ L := by
     change NoZeroSMulDivisors ℤ (AddSubgroup.toIntSubmodule L)
     exact noZeroSMulDivisors _
-  inferInstance
+  infer_instance
 
 open FiniteDimensional
 
