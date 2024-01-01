f6cc31fc1ee3ccec8cc4d30cb61941848a1bf4e2
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2024-01-01 23:10:42 +0100

    Update Mathlib/Data/Finset/PiAntidiagonal.lean
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Mathlib/Data/Finset/PiAntidiagonal.lean
+++ Mathlib/Data/Finset/PiAntidiagonal.lean
@@ -84,7 +84,7 @@ where
             ext
             · exact hij'.1
             · obtain ⟨-, rfl⟩ := hij'
-              rw [←(aux d i.2).prop ai |>.mp hai, ←(aux d j.2).prop ai |>.mp haj])
+              rw [← (aux d i.2).prop ai |>.mp hai, ← (aux d j.2).prop ai |>.mp haj])
         property := fun f => by
           simp_rw [mem_disjiUnion, mem_antidiagonal, mem_map, Embedding.coeFn_mk, Prod.exists,
             (aux d _).prop, Fin.sum_univ_succ]
