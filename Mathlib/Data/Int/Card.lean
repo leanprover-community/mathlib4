c78ebb266eac29d82e73439a493656e3e8c9b07c
Parcly Taxel <reddeloostw@gmail.com>
2024-01-01 17:19:17 +0800

    How small can this get?

--- Mathlib/Data/Int/Card.lean
+++ Mathlib/Data/Int/Card.lean
@@ -11,7 +11,7 @@ import Mathlib.Data.Rat.Floor
 /-!
 # Counting elements in an interval with given residue
 
-The theorems in this file generalise `card_multiples'` in `Data.Nat.Factorization.Basic` to
+The theorems in this file generalise `card_multiples` in `Data.Nat.Factorization.Basic` to
 all integer intervals and any fixed residue (not just zero, which reduces to the multiples).
 Theorems are given for `Ico` and `Ioc` intervals.
 -/
@@ -23,14 +23,14 @@ namespace Int
 
 variable (a b : ℤ) {r : ℤ} (hr : 0 < r)
 
-theorem Ico_filter_dvd_eq : (Ico a b).filter (r ∣ ·) =
+lemma Ico_filter_dvd_eq : (Ico a b).filter (r ∣ ·) =
     (Ico ⌈a / (r : ℚ)⌉ ⌈b / (r : ℚ)⌉).map ⟨_, mul_left_injective₀ hr.ne'⟩ := by
   ext x
   simp only [mem_map, mem_filter, mem_Ico, ceil_le, lt_ceil, div_le_iff, lt_div_iff,
     dvd_iff_exists_eq_mul_left, cast_pos.2 hr, ← cast_mul, cast_lt, cast_le]
   aesop
 
-theorem Ioc_filter_dvd_eq : (Ioc a b).filter (r ∣ ·) =
+lemma Ioc_filter_dvd_eq : (Ioc a b).filter (r ∣ ·) =
     (Ioc ⌊a / (r : ℚ)⌋ ⌊b / (r : ℚ)⌋).map ⟨_, mul_left_injective₀ hr.ne'⟩ := by
   ext x
   simp only [mem_map, mem_filter, mem_Ioc, floor_lt, le_floor, div_lt_iff, le_div_iff,
@@ -47,13 +47,13 @@ theorem Ioc_filter_dvd_card : ((Ioc a b).filter (r ∣ ·)).card =
     max (⌊b / (r : ℚ)⌋ - ⌊a / (r : ℚ)⌋) 0 := by
   rw [Ioc_filter_dvd_eq _ _ hr, card_map, card_Ioc, toNat_eq_max]
 
-theorem Ico_filter_modEq_eq (v : ℤ) : (Ico a b).filter (· ≡ v [ZMOD r]) =
+lemma Ico_filter_modEq_eq (v : ℤ) : (Ico a b).filter (· ≡ v [ZMOD r]) =
     ((Ico (a - v) (b - v)).filter (r ∣ ·)).map ⟨_, add_left_injective v⟩ := by
   ext x
   simp_rw [mem_map, mem_filter, mem_Ico, Function.Embedding.coeFn_mk, ← eq_sub_iff_add_eq,
     exists_eq_right, modEq_comm, modEq_iff_dvd, sub_lt_sub_iff_right, sub_le_sub_iff_right]
 
-theorem Ioc_filter_modEq_eq (v : ℤ) : (Ioc a b).filter (· ≡ v [ZMOD r]) =
+lemma Ioc_filter_modEq_eq (v : ℤ) : (Ioc a b).filter (· ≡ v [ZMOD r]) =
     ((Ioc (a - v) (b - v)).filter (r ∣ ·)).map ⟨_, add_left_injective v⟩ := by
   ext x
   simp_rw [mem_map, mem_filter, mem_Ioc, Function.Embedding.coeFn_mk, ← eq_sub_iff_add_eq,
@@ -77,7 +77,7 @@ namespace Nat
 
 variable (a b : ℕ) {r : ℕ} (hr : 0 < r)
 
-theorem Ico_filter_modEq_cast (v r : ℕ) : ((Ico a b).filter (· ≡ v [MOD r])).map castEmbedding =
+lemma Ico_filter_modEq_cast {v : ℕ} : ((Ico a b).filter (· ≡ v [MOD r])).map castEmbedding =
     (Ico ↑a ↑b).filter (· ≡ v [ZMOD r]) := by
   ext x
   simp only [mem_map, mem_filter, mem_Ico, castEmbedding_apply]
@@ -85,7 +85,7 @@ theorem Ico_filter_modEq_cast (v r : ℕ) : ((Ico a b).filter (· ≡ v [MOD r])
   · simp_rw [forall_exists_index, ← coe_nat_modEq_iff]; intro y ⟨h, c⟩; subst c; exact_mod_cast h
   · intro h; lift x to ℕ using (by linarith); exact ⟨x, by simp_all [coe_nat_modEq_iff]⟩
 
-theorem Ioc_filter_modEq_cast (v r : ℕ) : ((Ioc a b).filter (· ≡ v [MOD r])).map castEmbedding =
+lemma Ioc_filter_modEq_cast {v : ℕ} : ((Ioc a b).filter (· ≡ v [MOD r])).map castEmbedding =
     (Ioc ↑a ↑b).filter (· ≡ v [ZMOD r]) := by
   ext x
   simp only [mem_map, mem_filter, mem_Ioc, castEmbedding_apply]
@@ -96,22 +96,22 @@ theorem Ioc_filter_modEq_cast (v r : ℕ) : ((Ioc a b).filter (· ≡ v [MOD r])
 /-- `Int.Ico_filter_modEq_card` restricted to natural numbers. -/
 theorem Ico_filter_modEq_card (v : ℕ) : ((Ico a b).filter (· ≡ v [MOD r])).card =
     max (⌈(b - v) / (r : ℚ)⌉ - ⌈(a - v) / (r : ℚ)⌉) 0 := by
-  have := Ico_filter_modEq_cast a b v r ▸ card_map _
-  simp_rw [← this, Int.Ico_filter_modEq_card _ _ (ofNat_pos.mpr hr), Int.cast_ofNat]
+  simp_rw [← Ico_filter_modEq_cast _ _ ▸ card_map _,
+    Int.Ico_filter_modEq_card _ _ (ofNat_lt.mpr hr), Int.cast_ofNat]
 
 /-- `Int.Ioc_filter_modEq_card` restricted to natural numbers. -/
 theorem Ioc_filter_modEq_card (v : ℕ) : ((Ioc a b).filter (· ≡ v [MOD r])).card =
     max (⌊(b - v) / (r : ℚ)⌋ - ⌊(a - v) / (r : ℚ)⌋) 0 := by
-  have := Ioc_filter_modEq_cast a b v r ▸ card_map _
-  simp_rw [← this, Int.Ioc_filter_modEq_card _ _ (ofNat_pos.mpr hr), Int.cast_ofNat]
+  simp_rw [← Ioc_filter_modEq_cast _ _ ▸ card_map _,
+    Int.Ioc_filter_modEq_card _ _ (ofNat_lt.mpr hr), Int.cast_ofNat]
 
 /-- There are `⌈(b - v % r) / r⌉` numbers in `[0, b)` congruent to `v` mod `r`. -/
-theorem count_modEq_card' (v : ℕ) :
+theorem count_modEq_card_eq_ceil (v : ℕ) :
     b.count (· ≡ v [MOD r]) = ⌈(b - (v % r : ℕ)) / (r : ℚ)⌉ := by
-  rw [count_eq_card_filter_range, ← Ico_zero_eq_range, Ico_filter_modEq_card _ _ hr]
   have hr' : 0 < (r : ℚ) := by positivity
-  rw [max_eq_left (sub_nonneg.mpr <| Int.ceil_le_ceil _ _ <| div_le_div_of_le hr'.le <|
-    sub_le_sub_right (cast_le.mpr b.zero_le) ↑v)]
+  rw [count_eq_card_filter_range, ← Ico_zero_eq_range, Ico_filter_modEq_card _ _ hr,
+    max_eq_left (sub_nonneg.mpr <| Int.ceil_le_ceil _ _ <| div_le_div_of_le hr'.le <|
+      sub_le_sub_right (cast_le.mpr b.zero_le) ↑v)]
   conv_lhs =>
     rw [← div_add_mod v r, cast_add, cast_mul, add_comm]
     tactic => simp_rw [← sub_sub, sub_div (_ - _), mul_div_cancel_left _ hr'.ne', ceil_sub_nat]
@@ -125,16 +125,16 @@ where `[·]` is the Iverson bracket. -/
 theorem count_modEq_card (v : ℕ) :
     b.count (· ≡ v [MOD r]) = b / r + if v % r < b % r then 1 else 0 := by
   have hr' : 0 < (r : ℚ) := by positivity
-  rw [← ofNat_inj, count_modEq_card' b hr, cast_add]
+  rw [← ofNat_inj, count_modEq_card_eq_ceil _ hr, cast_add]
   conv_lhs => rw [← div_add_mod b r, cast_add, cast_mul, ← add_sub, _root_.add_div,
     mul_div_cancel_left _ hr'.ne', add_comm, Int.ceil_add_nat, add_comm]
   rw [add_right_inj]
   split_ifs with h
   · rw [← cast_sub h.le, Int.ceil_eq_iff, div_le_iff hr', lt_div_iff hr', cast_one, Int.cast_one,
       sub_self, zero_mul, cast_pos, tsub_pos_iff_lt, one_mul, cast_le, tsub_le_iff_right]
-    exact ⟨h, ((mod_lt b hr).trans_le (by simp)).le⟩
+    exact ⟨h, ((mod_lt _ hr).trans_le (by simp)).le⟩
   · rw [cast_zero, ceil_eq_zero_iff, Set.mem_Ioc, div_le_iff hr', lt_div_iff hr', zero_mul,
       tsub_nonpos, ← neg_eq_neg_one_mul, neg_lt_sub_iff_lt_add, ← cast_add, cast_lt, cast_le]
-    exact ⟨(mod_lt v hr).trans_le (by simp), not_lt.mp h⟩
+    exact ⟨(mod_lt _ hr).trans_le (by simp), not_lt.mp h⟩
 
 end Nat
