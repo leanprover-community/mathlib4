da8b983ed8fb5b7dd631cc0ba2fd4d512c4f06d6
RemyDegenne <Remydegenne@gmail.com>
2024-01-02 15:55:57 +0100

    sigma-(sub)additive contents

--- Mathlib/MeasureTheory/Measure/AddContent.lean
+++ Mathlib/MeasureTheory/Measure/AddContent.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Rémy Degenne, Peter Pfaffelhuber
 -/
 import Mathlib.MeasureTheory.SetSemiring
+import Mathlib.Topology.Instances.ENNReal
 
 /-!
 # Additive Contents
@@ -35,11 +36,26 @@ If `C` is a set ring (`IsSetRing C`), we have, for `s, t ∈ C`,
 * `AddContent.union_le`: `m (s ∪ t) ≤ m s + m t`
 * `AddContent.diff_le`: `m s - m t ≤ m (s \ t)`
 
+### σ-additive and σ-subadditive contents
+
+We say that an additive content `m` is σ-additive on `C` if for all sequences of pairwise disjoints
+sets `s : ℕ → Set α` in `C`, `m (⋃ i, s i) = ∑' i, m (s i)`.
+We say that an additive content is σ-subadditive on `C` if for all sequences of sets of `C` (not
+necessarily disjoint) `s : ℕ → Set α`, `m (⋃ i, s i) ≤ ∑' i, m (s i)`.
+
+* `AddContent.iUnion_eq_tsum_of_disjoint_of_iUnion_le`: if an `AddContent` is σ-subadditive on
+  a semi-ring of sets, then it is σ-additive.
+* `AddContent.iUnion_le_of_iUnion_eq_tsum`: if an `AddContent` is σ-additive on a ring of sets,
+  then it is σ-subadditive.
+
+* `AddContent.tendsto_atTop_iUnion_of_iUnion_eq_tsum`: if an additive content is σ-additive on a
+  ring of sets, then the content of a monotone sequence of sets tends to the content of the union.
+
 -/
 
-open Set Finset
+open Set Finset Filter
 
-open scoped ENNReal BigOperators
+open scoped ENNReal BigOperators Topology
 
 namespace MeasureTheory
 
@@ -54,7 +70,7 @@ structure AddContent (C : Set (Set α)) where
       (_h_mem : ⋃₀ ↑I ∈ C), toFun (⋃₀ I) = ∑ u in I, toFun u
 
 instance : Inhabited (AddContent C) :=
-  ⟨{toFun := fun _ => 0
+  ⟨{toFun := fun _ ↦ 0
     empty' := by simp
     add' := by simp }⟩
 
@@ -94,14 +110,14 @@ protected lemma union' (m : AddContent C) (hs : s ∈ C) (ht : t ∈ C) (hst : s
   · simp only [coe_pair, Set.insert_subset_iff, hs, ht, Set.singleton_subset_iff, and_self_iff]
   · simp only [coe_pair, Set.pairwiseDisjoint_insert, pairwiseDisjoint_singleton,
       mem_singleton_iff, Ne.def, id.def, forall_eq, true_and_iff]
-    exact fun _ => h_dis
+    exact fun _ ↦ h_dis
   · simp only [coe_pair, sUnion_insert, sUnion_singleton]
     exact hst
   convert h
   · simp only [coe_pair, sUnion_insert, sUnion_singleton]
   · rw [sum_insert, sum_singleton]
     simp only [Finset.mem_singleton]
-    refine' fun hs_eq_t => hs_empty _
+    refine fun hs_eq_t ↦ hs_empty ?_
     rw [← hs_eq_t] at h_dis
     exact Disjoint.eq_bot_of_self h_dis
 
@@ -140,6 +156,68 @@ protected lemma mono (m : AddContent C) (hC : IsSetSemiring C) (hs : s ∈ C) (h
   · simp only [coe_singleton, sUnion_singleton]
     exact hst
 
+lemma sUnion_le_sum (m : AddContent C) (hC : IsSetSemiring C)
+    (J : Finset (Set α)) (h_ss : ↑J ⊆ C) (h_mem : ⋃₀ ↑J ∈ C) :
+    m (⋃₀ ↑J) ≤ ∑ u in J, m u := by
+  classical
+  rw [← hC.sUnion_allDiffFinset₀ J h_ss, m.add]
+  rotate_left
+  · exact hC.allDiffFinset₀_subset J h_ss
+  · exact hC.pairwiseDisjoint_allDiffFinset₀ J h_ss
+  · rwa [hC.sUnion_allDiffFinset₀ J h_ss]
+  rw [IsSetSemiring.allDiffFinset₀, sum_disjiUnion, ← sum_ordered J]
+  refine sum_le_sum fun i _ ↦ m.sum_le_of_subset hC ?_ ?_ ?_ ?_
+  · exact hC.indexedDiffFinset₀_subset J h_ss i
+  · exact hC.pairwiseDisjoint_indexedDiffFinset₀' J h_ss i
+  · exact ordered_mem' h_ss i
+  · exact hC.sUnion_indexedDiffFinset₀_subset J h_ss i
+
+lemma le_sum_of_subset_sUnion (m : AddContent C) (hC : IsSetSemiring C)
+    (J : Finset (Set α)) (h_ss : ↑J ⊆ C) (ht : t ∈ C) (htJ : t ⊆ ⋃₀ ↑J) :
+    m t ≤ ∑ u in J, m u := by
+  classical
+  let Jt := Finset.image (fun u ↦ t ∩ u) J
+  have ht_eq : t = ⋃₀ Jt := by
+    rw [coe_image, sUnion_image, ← inter_iUnion₂, inter_eq_self_of_subset_left]
+    rwa [← sUnion_eq_biUnion]
+  rw [ht_eq]
+  refine' (m.sUnion_le_sum hC Jt _ _).trans _
+  · intro s
+    simp only [coe_image, Set.mem_image, mem_coe, forall_exists_index, and_imp]
+    rintro u hu rfl
+    exact hC.inter_mem _ ht _ (h_ss hu)
+  · rwa [← ht_eq]
+  refine (Finset.sum_image_le J _ m fun _ _ ↦ zero_le _).trans ?_
+  refine sum_le_sum fun u hu ↦ ?_
+  exact m.mono hC (hC.inter_mem _ ht _ (h_ss hu)) (h_ss hu) (inter_subset_right _ _)
+
+/-- If an `AddContent` is σ-subadditive on a semi-ring of sets, then it is σ-additive. -/
+theorem iUnion_eq_tsum_of_disjoint_of_iUnion_le (m : AddContent C) (hC : IsSetSemiring C)
+    (m_subadd : ∀ (f : ℕ → Set α) (hf : ∀ i, f i ∈ C) (hf_Union : ⋃ i, f i ∈ C)
+      (_hf_disj : Pairwise (Disjoint on f)), m (⋃ i, f i) ≤ ∑' i, m (f i))
+    (f : ℕ → Set α) (hf : ∀ i, f i ∈ C) (hf_Union : (⋃ i, f i) ∈ C)
+    (hf_disj : Pairwise (Disjoint on f)) :
+    m (⋃ i, f i) = ∑' i, m (f i) := by
+  refine le_antisymm (m_subadd f hf hf_Union hf_disj) ?_
+  refine tsum_le_of_sum_le ENNReal.summable fun I ↦ ?_
+  classical
+  rw [← Finset.sum_image_of_disjoint m m.empty f hf_disj]
+  refine m.sum_le_of_subset hC (I := I.image f) ?_ ?_ ?_ ?_
+  · simp only [coe_image, Set.image_subset_iff]
+    refine (subset_preimage_image f I).trans (preimage_mono ?_)
+    rintro i ⟨j, _, rfl⟩
+    exact hf j
+  · simp only [coe_image]
+    intro s hs t ht hst
+    rw [Set.mem_image] at hs ht
+    obtain ⟨i, _, rfl⟩ := hs
+    obtain ⟨j, _, rfl⟩ := ht
+    have hij : i ≠ j := by intro h_eq; rw [h_eq] at hst ; exact hst rfl
+    exact hf_disj hij
+  · exact hf_Union
+  · simp only [coe_image, sUnion_image, mem_coe, iUnion_subset_iff]
+    exact fun i _ ↦ subset_iUnion _ i
+
 end IsSetSemiring
 
 section IsSetRing
@@ -176,6 +254,72 @@ protected lemma diff_le (m : AddContent C) (hC : IsSetRing C) (hs : s ∈ C) (ht
   refine add_tsub_le_assoc.trans_eq ?_
   rw [tsub_eq_zero_of_le (m.mono hC.isSetSemiring (hC.inter_mem hs ht) ht (inter_subset_right _ _)),
     add_zero]
+
+/-- If an additive content is σ-additive on a set ring, then the content of a monotone sequence of
+sets tends to the content of the union. -/
+theorem tendsto_atTop_iUnion_of_iUnion_eq_tsum (m : AddContent C) (hC : IsSetRing C)
+    (m_add : ∀ (f : ℕ → Set α) (hf : ∀ i, f i ∈ C) (hf_Union : (⋃ i, f i) ∈ C)
+        (_hf_disj : Pairwise (Disjoint on f)), m (⋃ i, f i) = ∑' i, m (f i))
+    (f : ℕ → Set α) (hf_mono : Monotone f) (hf : ∀ i, f i ∈ C) (hf_Union : ⋃ i, f i ∈ C) :
+    Tendsto (fun n ↦ m (f n)) atTop (𝓝 (m (⋃ i, f i))) := by
+  classical
+  let g := disjointed f
+  have hg_Union : (⋃ i, g i) = ⋃ i, f i := iUnion_disjointed
+  specialize m_add g (hC.disjointed_mem hf) _ (disjoint_disjointed f)
+  · rwa [hg_Union]
+  rw [← hg_Union]
+  simp_rw [m_add]
+  have h : ∀ n, m (f n) = ∑ i in range (n + 1), m (g i) := by
+    intro n
+    have h1 : f n = ⋃₀ Finset.image g (range (n + 1)) := by
+      rw [← Monotone.partialSups_eq hf_mono, ← partialSups_disjointed, ←
+        partialSups_eq_sUnion_image g]
+    rw [h1, m.add]
+    rotate_left
+    · intro s
+      rw [mem_coe, Finset.mem_image]
+      rintro ⟨i, _, rfl⟩
+      exact hC.disjointed_mem hf i
+    · intro s hs t ht hst
+      rw [mem_coe, Finset.mem_image] at hs ht
+      obtain ⟨i, _, rfl⟩ := hs
+      obtain ⟨j, _, rfl⟩ := ht
+      have hij : i ≠ j := by intro h_eq; rw [h_eq] at hst ; exact hst rfl
+      exact disjoint_disjointed f hij
+    · rw [← h1]; exact hf n
+    rw [sum_image_of_disjoint m m.empty g (disjoint_disjointed f)]
+  simp_rw [h]
+  change Tendsto (fun n ↦ (fun k ↦ ∑ i in range k, m (g i)) (n + 1)) atTop (𝓝 (∑' i, m (g i)))
+  rw [tendsto_add_atTop_iff_nat (f := (fun k ↦ ∑ i in range k, m (g i))) 1]
+  exact ENNReal.tendsto_nat_tsum _
+
+/-- If an additive content is σ-additive on a set ring, then it is σ-subadditive. -/
+theorem iUnion_le_of_iUnion_eq_tsum (m : AddContent C) (hC : IsSetRing C)
+    (m_add : ∀ (f : ℕ → Set α) (hf : ∀ i, f i ∈ C) (hf_Union : (⋃ i, f i) ∈ C)
+      (_hf_disj : Pairwise (Disjoint on f)), m (⋃ i, f i) = ∑' i, m (f i))
+    (f : ℕ → Set α) (hf : ∀ i, f i ∈ C) (hf_Union : ⋃ i, f i ∈ C) :
+    m (⋃ i, f i) ≤ ∑' i, m (f i) := by
+  classical
+  have h_tendsto : Tendsto (fun n ↦ m (partialSups f n)) atTop (𝓝 (m (⋃ i, f i))) := by
+    rw [← iSup_eq_iUnion, ← iSup_partialSups_eq]
+    refine m.tendsto_atTop_iUnion_of_iUnion_eq_tsum hC m_add (partialSups f)
+      (monotone_partialSups f) (hC.partialSups_mem hf) ?_
+    rwa [← iSup_eq_iUnion, iSup_partialSups_eq]
+  have h_tendsto' :
+      Tendsto (fun n ↦ ∑ i in range (n + 1), m (f i)) atTop (𝓝 (∑' i, m (f i))) := by
+    rw [tendsto_add_atTop_iff_nat (f := (fun k ↦ ∑ i in range k, m (f i))) 1]
+    exact ENNReal.tendsto_nat_tsum _
+  refine le_of_tendsto_of_tendsto' h_tendsto h_tendsto' fun n ↦ ?_
+  rw [partialSups_eq_sUnion_image]
+  refine (m.le_sum_of_subset_sUnion hC.isSetSemiring
+    ((Finset.range (n + 1)).image f) ?_ ?_ subset_rfl).trans ?_
+  · intro s
+    rw [mem_coe, Finset.mem_image]
+    rintro ⟨i, _, rfl⟩
+    exact hf i
+  · rw [← partialSups_eq_sUnion_image]
+    exact hC.partialSups_mem hf n
+  · exact Finset.sum_image_le _ _ _ fun _ _ ↦ zero_le _
 
 end IsSetRing
 
--- Mathlib/MeasureTheory/SetSemiring.lean
+++ Mathlib/MeasureTheory/SetSemiring.lean
@@ -44,6 +44,180 @@ open Finset Set
 
 open scoped ENNReal BigOperators
 
+-- TODO: move this
+lemma monotone_partialSups {α : Type*} [SemilatticeSup α] (f : ℕ → α) :
+    Monotone fun n ↦ partialSups f n :=
+  fun n _ hnm ↦ partialSups_le f n _ (fun _ hm'n ↦ le_partialSups_of_le _ (hm'n.trans hnm))
+
+-- TODO: move this
+lemma partialSups_eq_sUnion_image {α : Type*} [DecidableEq (Set α)] (s : ℕ → Set α) (n : ℕ) :
+    partialSups s n = ⋃₀ ↑(Finset.image s (range (n + 1))) := by
+  ext
+  simp only [partialSups_eq_biSup, iSup_eq_iUnion, Set.mem_sUnion, mem_iUnion, exists_prop, mem_coe,
+  Finset.mem_image, Finset.mem_range, exists_exists_and_eq_and, Nat.lt_succ_iff]
+
+-- TODO: move this
+lemma partialSups_eq_biUnion_range {α : Type*} (s : ℕ → Set α) (n : ℕ) :
+    partialSups s n = ⋃ i ∈ Finset.range (n + 1), s i := by
+  ext a
+  simp only [Set.le_eq_subset, partialSups_eq_biSup, iSup_eq_iUnion, mem_iUnion, exists_prop,
+    Finset.mem_range, Nat.lt_succ]
+
+lemma Finset.sUnion_disjUnion {α β : Type _} {f : α → Finset (Set β)} (I : Finset α)
+    (hf : (I : Set α).PairwiseDisjoint f) :
+    ⋃₀ (I.disjiUnion f hf : Set (Set β)) = ⋃ a ∈ I, ⋃₀ ↑(f a) := by
+  ext1 b
+  simp only [mem_sUnion, mem_iUnion, mem_coe, exists_prop, mem_disjiUnion]
+  constructor
+  · rintro ⟨t, ⟨a, haI, hatf⟩, hbt⟩
+    exact ⟨a, haI, t, hatf, hbt⟩
+  · rintro ⟨a, haI, t, hatf, hbt⟩
+    exact ⟨t, ⟨a, haI, hatf⟩, hbt⟩
+
+-- TODO: move this
+lemma Finset.sum_image_of_disjoint {α ι : Type*} [PartialOrder α] [OrderBot α] [DecidableEq α]
+    (m : α → ℝ≥0∞) (m_bot : m ⊥ = 0)
+    (f : ι → α) (hf_disj : Pairwise (Disjoint on f)) (I : Finset ι) :
+    ∑ s in image f I, m s = ∑ i in I, m (f i) := by
+  rw [sum_image']
+  intro n hnI
+  by_cases hfn : f n = ⊥
+  · simp only [hfn, m_bot]
+    refine (sum_eq_zero fun i hi ↦ ?_).symm
+    rw [mem_filter] at hi
+    rw [hi.2, m_bot]
+  · have : (fun j ↦ f j = f n) = fun j ↦ j = n := by
+      ext1 j
+      rw [eq_iff_iff]
+      refine ⟨fun h ↦ ?_, fun h ↦ by rw [h]⟩
+      by_contra hij
+      have h_dis : Disjoint (f j) (f n) := hf_disj hij
+      rw [h] at h_dis
+      simp only [disjoint_self] at h_dis
+      exact hfn h_dis
+    classical
+    simp_rw [this]
+    simp only [sum_filter, sum_ite_eq', if_pos hnI]
+
+-- TODO: move this
+lemma Finset.sum_image_le {ι α β : Type _} [DecidableEq α] [OrderedSemiring β] (J : Finset ι)
+    (g : ι → α) (f : α → β) (hf : ∀ u ∈ J.image g, 0 ≤ f u) :
+    ∑ u in J.image g, f u ≤ ∑ u in J, f (g u) := by
+  rw [sum_comp f g]
+  refine sum_le_sum fun a hag => ?_
+  let hag' := hag
+  rw [Finset.mem_image] at hag'
+  obtain ⟨i, hi, hig⟩ := hag'
+  suffices 1 ≤ (J.filter (fun j => g j = a)).card by
+    conv_lhs => rw [← one_smul ℕ (f a)]
+    simp_rw [nsmul_eq_mul]
+    exact mul_le_mul (Nat.mono_cast this) le_rfl (hf a hag) (Nat.cast_nonneg _)
+  rw [Nat.succ_le_iff, card_pos]
+  refine ⟨i, ?_⟩
+  rw [mem_filter]
+  exact ⟨hi, hig⟩
+
+section Ordered
+
+variable {α : Type*}
+
+lemma Finset.mem_map_univ_asEmbedding {α β : Type*} [Fintype α] {p : β → Prop}
+    (e : α ≃ Subtype p) {b : β} :
+    b ∈ Finset.map e.asEmbedding univ ↔ p b := by
+  rw [mem_map]
+  simp only [Finset.mem_univ, Equiv.asEmbedding_apply, Function.comp_apply, exists_true_left,
+    true_and]
+  refine ⟨fun h ↦ ?_, fun h ↦ ?_⟩
+  · obtain ⟨a, rfl⟩ := h
+    exact (e a).prop
+  · suffices ∃ a, e a = ⟨b, h⟩ by
+      obtain ⟨a, ha⟩ := this
+      refine ⟨a, ?_⟩
+      rw [ha]
+    exact e.surjective _
+
+/-- An ordering of the elements of a finset. -/
+noncomputable def _root_.Finset.ordered (J : Finset α) : Fin J.card ↪ α :=
+  J.equivFin.symm.asEmbedding
+
+lemma map_ordered (J : Finset α) :
+    Finset.map J.ordered (univ : Finset (Fin J.card)) = J := by
+  ext1 s; simp_rw [Finset.ordered, Finset.mem_map_univ_asEmbedding]
+
+lemma ordered_mem {J : Finset α} (n : Fin J.card) : J.ordered n ∈ J := by
+  simp_rw [Finset.ordered]
+  exact coe_mem _
+
+lemma sum_ordered {β : Type _} [AddCommMonoid β] (J : Finset α) (m : α → β) :
+    ∑ i : Fin J.card, m (J.ordered i) = ∑ u in J, m u := by
+  conv_rhs => rw [← map_ordered J]
+  rw [sum_map]
+
+/-- The n first sets in `J.ordered`. -/
+noncomputable def finsetLT (J : Finset α) : Fin J.card → Finset α := fun n ↦
+  (Finset.filter (fun j : Fin J.card ↦ j < n) univ).map J.ordered
+
+lemma finsetLT_zero {J : Finset α} (hJ : 0 < J.card) : finsetLT J ⟨0, hJ⟩ = ∅ := by
+  rw [finsetLT]
+  simp only [univ_eq_attach, map_eq_empty]
+  rw [filter_eq_empty_iff]
+  intro n _
+  simp only [not_lt]
+  rw [← Fin.eta n n.2, Fin.mk_le_mk]
+  exact zero_le'
+
+lemma finsetLT_mono (J : Finset α) : Monotone (finsetLT J) := by
+  intro n m hnm s
+  rw [finsetLT, mem_map]
+  rintro ⟨i, hi, rfl⟩
+  simp only [Finset.ordered, finsetLT, Equiv.asEmbedding_apply, Function.comp_apply, mem_map,
+    mem_filter, Finset.mem_univ, true_and_iff, exists_prop]
+  refine ⟨i, ?_, rfl⟩
+  rw [mem_filter] at hi
+  exact hi.2.trans_le hnm
+
+lemma finsetLT_subset (J : Finset α) (n : Fin J.card) : finsetLT J n ⊆ J := by
+  intro u; rw [finsetLT, mem_map]; rintro ⟨i, _, rfl⟩; exact ordered_mem i
+
+lemma mem_finsetLT (J : Finset α) (n : Fin J.card) {s : α} :
+    s ∈ finsetLT J n ↔ ∃ m < n, s = J.ordered m := by
+  rw [finsetLT, mem_map]
+  simp only [mem_filter, Finset.mem_univ, true_and_iff, Equiv.asEmbedding_apply,
+    Function.comp_apply, exists_prop]
+  simp_rw [@eq_comm _ _ s]
+
+section FinsetSet
+
+variable {C : Set (Set α)} {J : Finset (Set α)}
+
+lemma ordered_mem' (hJ : ↑J ⊆ C) (n : Fin J.card) : J.ordered n ∈ C := hJ (ordered_mem n)
+
+lemma iUnion_ordered (J : Finset (Set α)) : (⋃ i : Fin J.card, J.ordered i) = ⋃₀ J := by
+  conv_rhs => rw [← map_ordered J]
+  simp_rw [sUnion_eq_biUnion, coe_map, Set.biUnion_image]
+  simp only [mem_coe, Finset.mem_univ, iUnion_true]
+
+lemma ordered_mem_finsetLT (J : Finset (Set α)) {n m : Fin J.card} (hnm : n < m) :
+    J.ordered n ∈ finsetLT J m := by rw [mem_finsetLT _ _]; exact ⟨n, hnm, rfl⟩
+
+lemma finsetLT_subset' (J : Finset (Set α)) (hJ : ↑J ⊆ C) (n : Fin J.card) :
+    ↑(finsetLT J n) ⊆ C :=
+  (Finset.coe_subset.mpr (finsetLT_subset J n)).trans hJ
+
+lemma sUnion_finsetLT_eq_bUnion (J : Finset (Set α)) (n : Fin J.card) :
+    ⋃₀ (finsetLT J n : Set (Set α)) = ⋃ i < n, J.ordered i := by
+  ext1 a
+  simp_rw [mem_sUnion, mem_coe, mem_finsetLT, mem_iUnion]
+  constructor
+  · rintro ⟨t, ⟨m, hmn, rfl⟩, hat⟩
+    exact ⟨m, hmn, hat⟩
+  · rintro ⟨m, hmn, hat⟩
+    exact ⟨J.ordered m, ⟨m, hmn, rfl⟩, hat⟩
+
+end FinsetSet
+
+end Ordered
+
 namespace MeasureTheory
 
 variable {α : Type*} {C : Set (Set α)} {s t : Set α}
@@ -278,6 +452,158 @@ lemma sUnion_union_diffFinset₀_of_subset (hC : IsSetSemiring C) (hs : s ∈ C)
 
 end diffFinset₀
 
+section indexedDiffFinset₀
+
+variable [DecidableEq (Set α)]
+
+/-- A finite set of sets in `C` such that
+`⋃₀ ↑(hC.indexedDiffFinset₀ J hJ n) = J.ordered n \ ⋃₀ finsetLT J n`. -/
+noncomputable def indexedDiffFinset₀ (hC : IsSetSemiring C) (J : Finset (Set α)) (hJ : ↑J ⊆ C)
+    (n : Fin J.card) : Finset (Set α) :=
+  hC.diffFinset₀ (ordered_mem' hJ n) (finsetLT_subset' J hJ n)
+
+lemma sUnion_indexedDiffFinset₀ (hC : IsSetSemiring C) (J : Finset (Set α)) (hJ : ↑J ⊆ C)
+    (n : Fin J.card) : ⋃₀ ↑(hC.indexedDiffFinset₀ J hJ n) = J.ordered n \ ⋃₀ finsetLT J n :=
+  (hC.diff_sUnion_eq_sUnion_diffFinset₀ _ _).symm
+
+lemma indexedDiffFinset₀_subset (hC : IsSetSemiring C) (J : Finset (Set α)) (hJ : ↑J ⊆ C)
+    (n : Fin J.card) : ↑(hC.indexedDiffFinset₀ J hJ n) ⊆ C :=
+  hC.diffFinset₀_subset _ _
+
+lemma sUnion_indexedDiffFinset₀_subset (hC : IsSetSemiring C) (J : Finset (Set α)) (hJ : ↑J ⊆ C)
+    (n : Fin J.card) :
+    ⋃₀ ↑(hC.indexedDiffFinset₀ J hJ n) ⊆ J.ordered n :=
+  subset_trans (hC.sUnion_indexedDiffFinset₀ J hJ n).subset (Set.diff_subset _ _)
+
+lemma empty_not_mem_indexedDiffFinset₀ (hC : IsSetSemiring C) (J : Finset (Set α)) (hJ : ↑J ⊆ C)
+    (n : Fin J.card) :
+    ∅ ∉ hC.indexedDiffFinset₀ J hJ n := by
+  rw [indexedDiffFinset₀]; exact hC.empty_not_mem_diffFinset₀ _ _
+
+lemma subset_ordered_of_mem_indexedDiffFinset₀ (hC : IsSetSemiring C)
+    (J : Finset (Set α)) (hJ : ↑J ⊆ C)
+    {n : Fin J.card} (h : s ∈ hC.indexedDiffFinset₀ J hJ n) :
+    s ⊆ J.ordered n :=
+  (subset_sUnion_of_mem h).trans
+    (hC.sUnion_diffFinset₀_subset (ordered_mem' hJ n) (finsetLT_subset' J hJ n))
+
+lemma iUnion_sUnion_indexedDiffFinset₀ (hC : IsSetSemiring C) (J : Finset (Set α)) (hJ : ↑J ⊆ C) :
+    (⋃ i, ⋃₀ (hC.indexedDiffFinset₀ J hJ i : Set (Set α))) = ⋃₀ J := by
+  rw [← iUnion_ordered]
+  refine subset_antisymm (fun a ↦ ?_) (fun a ↦ ?_)
+  · simp_rw [mem_iUnion, mem_sUnion]
+    rintro ⟨i, t, ht, hat⟩
+    exact ⟨i, subset_ordered_of_mem_indexedDiffFinset₀ hC J hJ ht hat⟩
+  · simp_rw [mem_iUnion]
+    intro h
+    have h' : ∃ (i : ℕ) (hi : i < J.card), a ∈ J.ordered ⟨i, hi⟩ := by
+      obtain ⟨i, hai⟩ := h
+      refine ⟨i.1, i.2, ?_⟩
+      convert hai
+    classical
+    let i : ℕ := Nat.find h'
+    have hi : i < J.card := (Nat.find_spec h').choose
+    have ha_mem_i : a ∈ J.ordered ⟨i, hi⟩ := (Nat.find_spec h').choose_spec
+    refine ⟨⟨i, hi⟩, ?_⟩
+    rw [sUnion_indexedDiffFinset₀, Set.mem_diff]
+    refine ⟨ha_mem_i, ?_⟩
+    rw [sUnion_finsetLT_eq_bUnion]
+    simp only [mem_iUnion, exists_prop, not_exists, not_and]
+    intro j hj_lt hj
+    have hj_lt' : ↑j < i := by rwa [← Fin.eta j j.2, Fin.mk_lt_mk] at hj_lt
+    refine (Nat.lt_find_iff h' j).mp hj_lt' j le_rfl ⟨hj_lt'.trans hi, ?_⟩
+    convert hj
+
+lemma disjoint_sUnion_finsetLT_of_mem_indexedDiffFinset₀
+    (hC : IsSetSemiring C) (J : Finset (Set α))
+    (hJ : ↑J ⊆ C) {n : Fin J.card} (h : s ∈ hC.indexedDiffFinset₀ J hJ n) :
+    Disjoint s (⋃₀ finsetLT J n) := by
+  refine Disjoint.mono_left (subset_sUnion_of_mem h : s ⊆ ⋃₀ ↑(hC.indexedDiffFinset₀ J hJ n)) ?_
+  rw [sUnion_indexedDiffFinset₀ hC J hJ n, Set.disjoint_iff_inter_eq_empty, Set.inter_comm,
+    inter_diff_self]
+
+lemma disjoint_ordered_of_mem_indexedDiffFinset₀ (hC : IsSetSemiring C)
+    (J : Finset (Set α)) (hJ : ↑J ⊆ C)
+    {n m : Fin J.card} (h : s ∈ hC.indexedDiffFinset₀ J hJ n) (hnm : m < n) :
+    Disjoint s (J.ordered m) := by
+  refine Disjoint.mono_right ?_ (hC.disjoint_sUnion_finsetLT_of_mem_indexedDiffFinset₀ J hJ h)
+  exact subset_sUnion_of_mem (ordered_mem_finsetLT J hnm)
+
+lemma disjoint_of_mem_indexedDiffFinset₀_of_lt (hC : IsSetSemiring C)
+    (J : Finset (Set α)) (hJ : ↑J ⊆ C)
+    {n m : Fin J.card} (hnm : n < m) (hs : s ∈ hC.indexedDiffFinset₀ J hJ n)
+    (ht : t ∈ hC.indexedDiffFinset₀ J hJ m) : Disjoint s t := by
+  have hs_subset : s ⊆ J.ordered n := hC.subset_ordered_of_mem_indexedDiffFinset₀ J hJ hs
+  have hs_disj : Disjoint t (J.ordered n) :=
+    hC.disjoint_ordered_of_mem_indexedDiffFinset₀ J hJ ht hnm
+  exact Disjoint.mono_left hs_subset hs_disj.symm
+
+lemma disjoint_of_mem_indexedDiffFinset₀ (hC : IsSetSemiring C) (J : Finset (Set α)) (hJ : ↑J ⊆ C)
+    {n m : Fin J.card} (hnm : n ≠ m) (hs : s ∈ hC.indexedDiffFinset₀ J hJ n)
+    (ht : t ∈ hC.indexedDiffFinset₀ J hJ m) : Disjoint s t := by
+  cases' lt_or_lt_iff_ne.mpr hnm with h h
+  · exact hC.disjoint_of_mem_indexedDiffFinset₀_of_lt J hJ h hs ht
+  · exact (hC.disjoint_of_mem_indexedDiffFinset₀_of_lt J hJ h ht hs).symm
+
+lemma disjoint_indexedDiffFinset₀ (hC : IsSetSemiring C) (J : Finset (Set α)) (hJ : ↑J ⊆ C)
+    {n m : Fin J.card} (hnm : n ≠ m) :
+    Disjoint (hC.indexedDiffFinset₀ J hJ n) (hC.indexedDiffFinset₀ J hJ m) := by
+  rw [Finset.disjoint_iff_inter_eq_empty]
+  ext1 s
+  simp only [Finset.mem_inter, Finset.not_mem_empty, iff_false_iff, not_and]
+  intro hsn hsm
+  have : Disjoint s s := hC.disjoint_of_mem_indexedDiffFinset₀ J hJ hnm hsn hsm
+  rw [Set.disjoint_iff_inter_eq_empty, Set.inter_self] at this
+  rw [this] at hsn
+  exact hC.empty_not_mem_indexedDiffFinset₀ _ _ _ hsn
+
+lemma pairwiseDisjoint_indexedDiffFinset₀ (hC : IsSetSemiring C)
+    (J : Finset (Set α)) (hJ : ↑J ⊆ C) :
+    PairwiseDisjoint (↑(univ : Finset (Fin J.card))) (hC.indexedDiffFinset₀ J hJ) :=
+  fun _ _ _ _ hnm ↦ hC.disjoint_indexedDiffFinset₀ J hJ hnm
+
+lemma pairwiseDisjoint_indexedDiffFinset₀' (hC : IsSetSemiring C) (J : Finset (Set α)) (hJ : ↑J ⊆ C)
+    (n : Fin J.card) : PairwiseDisjoint ↑(hC.indexedDiffFinset₀ J hJ n) (id : Set α → Set α) :=
+  hC.pairwiseDisjoint_diffFinset₀ _ _
+
+end indexedDiffFinset₀
+
+section AllDiffFinset₀
+
+variable [DecidableEq (Set α)]
+
+/-- This is a finset of pairwise disjoint sets in the set semi-ring `C`, such that
+`⋃₀ hC.allDiffFinset₀ J hJ = ⋃₀ J`. -/
+noncomputable def allDiffFinset₀ (hC : IsSetSemiring C) (J : Finset (Set α)) (hJ : ↑J ⊆ C) :
+    Finset (Set α) :=
+  Finset.disjiUnion univ (hC.indexedDiffFinset₀ J hJ) (hC.pairwiseDisjoint_indexedDiffFinset₀ J hJ)
+
+lemma pairwiseDisjoint_allDiffFinset₀ (hC : IsSetSemiring C) (J : Finset (Set α)) (hJ : ↑J ⊆ C) :
+    PairwiseDisjoint ↑(hC.allDiffFinset₀ J hJ) (id : Set α → Set α) := by
+  intro u hu v hv huv
+  simp_rw [Function.onFun, id.def]
+  simp_rw [allDiffFinset₀, mem_coe, Finset.mem_disjiUnion] at hu hv
+  obtain ⟨n, _, huBn⟩ := hu
+  obtain ⟨m, _, hvBm⟩ := hv
+  by_cases hnm : n = m
+  · rw [← hnm] at hvBm
+    exact hC.pairwiseDisjoint_indexedDiffFinset₀' _ _ n huBn hvBm huv
+  · exact hC.disjoint_of_mem_indexedDiffFinset₀ J hJ hnm huBn hvBm
+
+lemma allDiffFinset₀_subset (hC : IsSetSemiring C) (J : Finset (Set α)) (hJ : ↑J ⊆ C) :
+    ↑(hC.allDiffFinset₀ J hJ) ⊆ C := by
+  intro s
+  rw [mem_coe, allDiffFinset₀, mem_disjiUnion]
+  rintro ⟨n, _, h_mem⟩
+  exact hC.indexedDiffFinset₀_subset J hJ n h_mem
+
+lemma sUnion_allDiffFinset₀ (hC : IsSetSemiring C) (J : Finset (Set α)) (hJ : ↑J ⊆ C) :
+    ⋃₀ (hC.allDiffFinset₀ J hJ : Set (Set α)) = ⋃₀ J := by
+  simp only [allDiffFinset₀, Finset.sUnion_disjUnion, Finset.mem_univ, iUnion_true,
+    iUnion_sUnion_indexedDiffFinset₀]
+
+end AllDiffFinset₀
+
 end IsSetSemiring
 
 /-- A ring of sets `C` is a family of sets containing `∅`, stable by union and set difference.
@@ -294,9 +620,9 @@ lemma inter_mem (hC : IsSetRing C) (hs : s ∈ C) (ht : t ∈ C) : s ∩ t ∈ C
 
 lemma isSetSemiring (hC : IsSetRing C) : IsSetSemiring C :=
   { empty_mem := hC.empty_mem
-    inter_mem := fun s hs t ht => hC.inter_mem hs ht
+    inter_mem := fun s hs t ht ↦ hC.inter_mem hs ht
     diff_eq_Union' := by
-      refine' fun s hs t ht => ⟨{s \ t}, _, _, _⟩
+      refine fun s hs t ht ↦ ⟨{s \ t}, ?_, ?_, ?_⟩
       · simp only [coe_singleton, Set.singleton_subset_iff]
         exact hC.diff_mem hs ht
       · simp only [coe_singleton, pairwiseDisjoint_singleton]
@@ -326,6 +652,17 @@ lemma biInter_mem {ι : Type*} (hC : IsSetRing C) {s : ι → Set α}
     simp only [cons_eq_insert, Finset.mem_insert, forall_eq_or_imp] at hs
     refine hC.inter_mem hs.1 ?_
     exact h (fun n hnS ↦ hs.2 n hnS)
+
+lemma partialSups_mem (hC : IsSetRing C) {s : ℕ → Set α} (hs : ∀ n, s n ∈ C) (n : ℕ) :
+    partialSups s n ∈ C := by
+  rw [partialSups_eq_biUnion_range]
+  exact hC.biUnion_mem _ (fun n _ ↦ hs n)
+
+lemma disjointed_mem (hC : IsSetRing C) {s : ℕ → Set α} (hs : ∀ n, s n ∈ C) (n : ℕ) :
+    disjointed s n ∈ C := by
+  cases n with
+  | zero => rw [disjointed_zero]; exact hs 0
+  | succ n => rw [disjointed_succ]; exact hC.diff_mem (hs n.succ) (hC.partialSups_mem hs n)
 
 end IsSetRing
 
