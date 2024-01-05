ca8ba1f00347a57f204b1ea9e3a166d7825d3c54
Parcly Taxel <reddeloostw@gmail.com>
2024-01-05 19:20:30 +0800

    Redundant import

--- Mathlib/Tactic/GCongr.lean
+++ Mathlib/Tactic/GCongr.lean
@@ -3,7 +3,6 @@ Copyright (c) 2023 Mario Carneiro, Heather Macbeth. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro, Heather Macbeth
 -/
-import Mathlib.Algebra.Order.Field.Power
 import Mathlib.Tactic.Positivity
 import Mathlib.Tactic.GCongr.Core
 
