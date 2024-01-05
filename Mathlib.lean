3107b2f7da300d52eb7a0a94d15462f03c0e39a2
Yaël Dillies <yael.dillies@gmail.com>
2024-01-05 08:19:02 +0000

    chore: Move scalar compatibility instance for `ℤ` and `ℕ` on rings to their own files Part of #9411

--- Mathlib.lean
+++ Mathlib.lean
@@ -2197,6 +2197,7 @@ import Mathlib.GroupTheory.GroupAction.Option
 import Mathlib.GroupTheory.GroupAction.Pi
 import Mathlib.GroupTheory.GroupAction.Prod
 import Mathlib.GroupTheory.GroupAction.Quotient
+import Mathlib.GroupTheory.GroupAction.Ring
 import Mathlib.GroupTheory.GroupAction.Sigma
 import Mathlib.GroupTheory.GroupAction.SubMulAction
 import Mathlib.GroupTheory.GroupAction.SubMulAction.Pointwise
