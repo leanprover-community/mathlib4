e3c0da2f1ffc2331b63f958d6a3e239ab7aa2911
Yaël Dillies <yael.dillies@gmail.com>
2024-01-05 13:01:32 +0000

    fix archive

--- Archive/Imo/Imo2011Q5.lean
+++ Archive/Imo/Imo2011Q5.lean
@@ -3,6 +3,7 @@ Copyright (c) 2021 Alain Verberkmoes. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Alain Verberkmoes
 -/
+import Mathlib.Algebra.Order.Ring.Abs
 import Mathlib.Data.Int.Dvd.Basic
 import Mathlib.Tactic.Common
 
