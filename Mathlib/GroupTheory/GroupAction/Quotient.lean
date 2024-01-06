96a7af95fae5460bbf626243c431fbe589f2a8b8
Adrien Burgun <adrien.burgun@orange.fr>
2024-01-06 13:59:45 +0100

    chore(GroupTheory/GroupAction): replace minimalPeriod with period in Quotient.lean

--- Mathlib/GroupTheory/GroupAction/Quotient.lean
+++ Mathlib/GroupTheory/GroupAction/Quotient.lean
@@ -120,9 +120,9 @@ theorem Quotient.coe_smul_out' [QuotientAction β H] (b : β) (q : α ⧸ H) : �
 #align add_action.quotient.coe_vadd_out' AddAction.Quotient.coe_vadd_out'
 
 theorem _root_.QuotientGroup.out'_conj_pow_minimalPeriod_mem (a : α) (q : α ⧸ H) :
-    q.out'⁻¹ * a ^ Function.minimalPeriod (a • ·) q * q.out' ∈ H := by
+    q.out'⁻¹ * a ^ MulAction.period a q * q.out' ∈ H := by
   rw [mul_assoc, ← QuotientGroup.eq', QuotientGroup.out_eq', ← smul_eq_mul, Quotient.mk_smul_out',
-    eq_comm, pow_smul_eq_iff_minimalPeriod_dvd]
+    eq_comm, smul_pow_period_fixed]
 #align quotient_group.out'_conj_pow_minimal_period_mem QuotientGroup.out'_conj_pow_minimalPeriod_mem
 
 end QuotientAction
