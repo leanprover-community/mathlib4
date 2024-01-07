eb829b82246cfe2b44638235632e94e0ecc9e0d6
Junyan Xu <junyanxu.math@gmail.com>
2024-01-07 18:00:33 -0500

    trailing space
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Mathlib/FieldTheory/Tower.lean
+++ Mathlib/FieldTheory/Tower.lean
@@ -73,7 +73,7 @@ theorem right [hf : FiniteDimensional F A] : FiniteDimensional K A :=
 #align finite_dimensional.right FiniteDimensional.right
 
 /-- Tower law: if `A` is a `K`-vector space and `K` is a field extension of `F` then
-`dim_F(A) = dim_F(K) * dim_K(A)`. 
+`dim_F(A) = dim_F(K) * dim_K(A)`.
   A special case of `FiniteDimensional.finrank_mul_finrank'` which assumes `Module.Free`.
 We keep this because Lean sometimes has trouble finding the `Module.Free` instances. -/
 theorem finrank_mul_finrank : finrank F K * finrank K A = finrank F A := finrank_mul_finrank' F K A
