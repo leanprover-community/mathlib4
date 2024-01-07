2004ae62563232af7d1e66f7d92347ef82134bfd
Junyan Xu <junyanxu.math@gmail.com>
2024-01-07 01:30:32 -0500

    generalize `norm_eq_norm_adjoin`

--- Mathlib/RingTheory/Norm.lean
+++ Mathlib/RingTheory/Norm.lean
@@ -200,17 +200,16 @@ open IntermediateField
 
 variable (K)
 
-theorem norm_eq_norm_adjoin [FiniteDimensional K L] [IsSeparable K L] (x : L) :
+theorem norm_eq_norm_adjoin [FiniteDimensional K L] (x : L) :
     norm K x = norm K (AdjoinSimple.gen K x) ^ finrank K⟮x⟯ L := by
-  letI := isSeparable_tower_top_of_isSeparable K K⟮x⟯ L
-  let pbL := Field.powerBasisOfFiniteOfSeparable K⟮x⟯ L
-  let pbx := IntermediateField.adjoin.powerBasis (IsSeparable.isIntegral K x)
+  let pbL := Module.Free.chooseBasis K⟮x⟯ L
+  let pbx := IntermediateField.adjoin.powerBasis (.of_finite K x)
   -- This used to be `rw`, but we need `erw` after leanprover/lean4#2644
-  erw [← AdjoinSimple.algebraMap_gen K x, norm_eq_matrix_det (pbx.basis.smul pbL.basis) _,
+  erw [← AdjoinSimple.algebraMap_gen K x, norm_eq_matrix_det (pbx.basis.smul pbL) _,
     smul_leftMulMatrix_algebraMap, det_blockDiagonal, norm_eq_matrix_det pbx.basis]
   simp only [Finset.card_fin, Finset.prod_const]
   congr
-  rw [← PowerBasis.finrank, AdjoinSimple.algebraMap_gen K x]
+  rw [← Fintype.card, ← finrank_eq_card_basis pbL, AdjoinSimple.algebraMap_gen K x]
 #align algebra.norm_eq_norm_adjoin Algebra.norm_eq_norm_adjoin
 
 variable {K}
