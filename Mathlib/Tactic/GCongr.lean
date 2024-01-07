ab498703ac092142636dc67b6df248d0471ecdd6
Yury G. Kudryashov <urkud@urkud.name>
2024-01-07 13:37:00 -0600

    Reduce imports

--- Mathlib/Tactic/GCongr.lean
+++ Mathlib/Tactic/GCongr.lean
@@ -3,8 +3,7 @@ Copyright (c) 2023 Mario Carneiro, Heather Macbeth. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro, Heather Macbeth
 -/
-import Mathlib.Algebra.Order.Field.Power
-import Mathlib.Tactic.Positivity
+import Mathlib.Tactic.Positivity.Core
 import Mathlib.Tactic.GCongr.Core
 
 /-! # Setup for the `gcongr` tactic
