3d1cf78f4a4d3898ff5c5236f2af4e4bcf6dcdb4
Yury G. Kudryashov <urkud@urkud.name>
2024-01-08 13:15:27 +0000

    chore(*): use `∃ x ∈ s, p x` instead of `∃ x (_ : x ∈ s), p x` (#9326)
    This is a follow-up to #9215. It changes the following theorems and definitions:
    
    - `IsOpen.exists_subset_affineIndependent_span_eq_top`
    - `IsConformalMap`
    - `SimpleGraph.induce_connected_of_patches`
    - `Submonoid.exists_list_of_mem_closure`
    - `AddSubmonoid.exists_list_of_mem_closure`
    - `AffineSubspace.mem_affineSpan_insert_iff`
    - `AffineBasis.exists_affine_subbasis`
    - `exists_affineIndependent`
    - `LinearMap.mem_submoduleImage`
    - `Basis.basis_singleton_iff`
    - `atom_iff_nonzero_span`
    - `finrank_eq_one_iff'`
    - `Submodule.basis_of_pid_aux`
    - `exists_linearIndependent_extension`
    - `exists_linearIndependent`
    - `countable_cover_nhdsWithin_of_sigma_compact`
    - `mem_residual`
    
    Also deprecate `ENNReal.exists_ne_top'`.

--- Mathlib/Data/Real/ENNReal.lean
+++ Mathlib/Data/Real/ENNReal.lean
@@ -260,11 +260,13 @@ theorem forall_ne_top {p : ℝ≥0∞ → Prop} : (∀ a, a ≠ ∞ → p a) ↔
   Option.ball_ne_none
 #align ennreal.forall_ne_top ENNReal.forall_ne_top
 
+@[deprecated]
 theorem exists_ne_top' {p : ℝ≥0∞ → Prop} : (∃ (a : ℝ≥0∞) (_ : a ≠ ∞), p a) ↔ ∃ r : ℝ≥0, p r :=
   Option.bex_ne_none
 #align ennreal.exists_ne_top ENNReal.exists_ne_top'
 
-theorem exists_ne_top {p : ℝ≥0∞ → Prop} : (∃ a : ℝ≥0∞, a ≠ ∞ ∧ p a) ↔ ∃ r : ℝ≥0, p r := by
+set_option linter.deprecated false in
+theorem exists_ne_top {p : ℝ≥0∞ → Prop} : (∃ a ≠ ∞, p a) ↔ ∃ r : ℝ≥0, p r := by
   simp only [exists_ne_top', ← exists_prop]
 
 theorem toNNReal_eq_zero_iff (x : ℝ≥0∞) : x.toNNReal = 0 ↔ x = 0 ∨ x = ∞ :=
