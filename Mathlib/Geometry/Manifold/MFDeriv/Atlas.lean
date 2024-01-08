cd4cda00a1a7df9f55f2d76be62d0bed0870c51b
Michael Rothgang <rothgami@math.hu-berlin.de>
2024-01-08 22:47:52 +0100

    Move results about differentiability of specific functions to a separate file.
    - The module docstring was partially present, but was incomplete.
    - Update the module docstring in Basic.lean for the moving, and indicate
    what the other files have now.
    - Make all my newly created files build: adjust imports for the further split.

--- Mathlib/Geometry/Manifold/MFDeriv/Atlas.lean
+++ Mathlib/Geometry/Manifold/MFDeriv/Atlas.lean
@@ -3,7 +3,7 @@ Copyright (c) 2020 Sébastien Gouëzel. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Sébastien Gouëzel, Floris van Doorn
 -/
-import Mathlib.Geometry.Manifold.MFDeriv.Basic
+import Mathlib.Geometry.Manifold.MFDeriv.SpecificFunctions
 
 /-!
 # Differentiability of models with corners and (extended) charts
