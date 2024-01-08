b14706ba85f700dfbd562d0bce8fc6a36f672e95
Christian Merten <christian@merten.dev>
2024-01-08 21:28:32 +0100

    add more docstrings

--- Mathlib/CategoryTheory/Galois/Basic.lean
+++ Mathlib/CategoryTheory/Galois/Basic.lean
@@ -42,40 +42,47 @@ namespace Galois
 
 section Def
 
-/- Lenstra, Def 3.1, (G1)-(G3) -/
+/-- Definition of a Galois category. Lenstra, Def 3.1, (G1)-(G3) -/
 class PreGaloisCategory (C : Type u₁) [Category.{u₂, u₁} C] : Prop where
-  -- (G1)
+  /-- `C` has a terminal object (G1). -/
   hasTerminalObject : HasTerminal C
+  /-- `C` has pullbacks (G1). -/
   hasPullbacks : HasPullbacks C
-  -- (G2)
+  /-- `C` has finite coproducts (G2). -/
   hasFiniteCoproducts : HasFiniteCoproducts C
+  /-- `C` has quotients by finite groups (G2). -/
   hasQuotientsByFiniteGroups (G : Type u₂) [Group G] [Finite G] :
     HasColimitsOfShape (SingleObj G) C
-  -- (G3)
+  /-- Every monomorphism in `C` induces an isomorphism on a direct summand (G3). -/
   monoInducesIsoOnDirectSummand {X Y : C} (i : X ⟶ Y) [Mono i] : ∃ (Z : C) (u : Z ⟶ Y),
     Nonempty (IsColimit (BinaryCofan.mk i u))
 
-/- Lenstra, Def 3.1, (G4)-(G6) -/
+/-- Definition of a fibre functor from a Galois category. Lenstra, Def 3.1, (G4)-(G6) -/
 class FibreFunctor {C : Type u₁} [Category.{u₂, u₁} C] [PreGaloisCategory C]
     (F : C ⥤ FintypeCat.{w}) where
-  -- (G4)
+  /-- `F` preserves terminal objects (G4). -/
   preservesTerminalObjects : PreservesLimitsOfShape (CategoryTheory.Discrete PEmpty.{1}) F
+  /-- `F` preserves pullbacks (G4). -/
   preservesPullbacks : PreservesLimitsOfShape WalkingCospan F
-  -- (G5)
+  /-- `F` preserves finite coproducts (G5). -/
   preservesFiniteCoproducts : PreservesFiniteCoproducts F
+  /-- `F` preserves epimorphisms (G5). -/
   preservesEpis : Functor.PreservesEpimorphisms F
+  /-- `F` preserves quotients by finite groups (G5). -/
   preservesQuotientsByFiniteGroups (G : Type u₂) [Group G] [Finite G] :
     PreservesColimitsOfShape (SingleObj G) F
-  -- (G6)
+  /-- `F` reflects isomorphisms (G6). -/
   reflectsIsos : ReflectsIsomorphisms F
 
-/- Lenstra, 3.12. An object of a category `C` is connected if it is not initial
-and has no non-trivial subobjects. -/
+/-- An object of a category `C` is connected if it is not initial
+and has no non-trivial subobjects. Lenstra, 3.12. -/
 class ConnectedObject {C : Type u₁} [Category.{u₂, u₁} C] (X : C) : Prop where
+  /-- `X` is not an initial object. -/
   notInitial : IsInitial X → False
+  /-- `X` has no non-trivial subobjects. -/
   noTrivialComponent (Y : C) (i : Y ⟶ X) [Mono i] : (IsInitial Y → False) → IsIso i
 
-/- A functor is said to preserve connected objects if it sends
+/-- A functor is said to preserve connected objects if it sends
 connected objects to connected objects. -/
 class PreservesConnectedObjects {C : Type u₁} [Category.{u₂, u₁} C] {D : Type v₁}
     [Category.{v₂, v₁} D] (F : C ⥤ D) : Prop where
