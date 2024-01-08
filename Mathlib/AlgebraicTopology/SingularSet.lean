cee07713de62e2200d35dd335057eb242b63fef1
Johan Commelin <johan@commelin.net>
2024-01-08 11:40:59 +0100

    Update Mathlib/AlgebraicTopology/SingularSet.lean

--- Mathlib/AlgebraicTopology/SingularSet.lean
+++ Mathlib/AlgebraicTopology/SingularSet.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2024 Johan Commelin. All rights reserved.
+Copyright (c) 2023 Johan Commelin. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johan Commelin, Scott Morrison, Adam Topaz
 -/
