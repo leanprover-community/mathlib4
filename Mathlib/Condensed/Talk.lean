3907556bc4e93df8076746355d3e071dfdc9f9ed
dagurtomas <dagurtomas@gmail.com>
2024-01-08 20:21:04 +0000

    .

--- Mathlib/Condensed/Talk.lean
+++ Mathlib/Condensed/Talk.lean
@@ -8,49 +8,46 @@ noncomputable section
 
 universe u
 
-open CategoryTheory Opposite
+open CategoryTheory
 
-namespace CondensedSets
+section One
 
 /-!
 
 # (Light) condensed sets
-
-We can let our sheaves take values in abelian groups.
 -/
 
 /-- I want my condensed sets to be defined on profinite spaces, not all compact Hausdorff spaces -/
 abbrev CondensedSet' := Sheaf (coherentTopology Profinite.{u}) (Type (u+1))
 
 /-- Fortunately they are equivalent to mathlib's condensed sets. -/
-def eq : CondensedSet' ≌ CondensedSet := Condensed.ProfiniteCompHaus.equivalence _
+example : CondensedSet' ≌ CondensedSet := Condensed.ProfiniteCompHaus.equivalence _
 
 variable (S : Profinite.{u}) (X : CondensedSet'.{u})
 
+#check X.val
 #check X.val.obj ⟨S⟩
 #check X.cond
 
 variable (T : LightProfinite.{u}) (Y : LightCondSet.{u})
 
-#check Y.val.obj (op T)
+#check Y.val.obj ⟨T⟩
 #check Y.cond
 
 /- Given a presheaf `F` on light profinite spaces... -/
 variable (F : LightProfinite.{u}ᵒᵖ ⥤ Type u)
 
 /-- ...we can sheafify it -/
-def Fs : LightCondSet.{u} := (presheafToSheaf _ _).obj F
+example : LightCondSet.{u} := (presheafToSheaf _ _).obj F
 
--- def SmallCondensedSet := Sheaf (coherentTopology Profinite.{u}) (Type u)
+-- abbrev SmallCondensedSet := Sheaf (coherentTopology Profinite.{u}) (Type u)
 -- variable (G : Profinite.{u}ᵒᵖ ⥤ Type u)
--- def Gs : SmallCondensedSet.{u} := (presheafToSheaf _ _).obj G
+-- example : SmallCondensedSet.{u} := (presheafToSheaf _ _).obj G
 
 -- variable (G : Profinite.{u}ᵒᵖ ⥤ Type (u + 1))
--- def Gs : CondensedSet'.{u} := (presheafToSheaf _ _).obj G
+-- example : CondensedSet'.{u} := (presheafToSheaf _ _).obj G
 
-end CondensedSets
-
-
+end One
 
 
 
@@ -58,7 +55,9 @@ end CondensedSets
 
 
 
-namespace CondensedAbs
+
+
+section Two
 
 /-!
 
@@ -69,7 +68,7 @@ We can let our sheaves take values in abelian groups.
 
 abbrev CondensedAb' := Sheaf (coherentTopology Profinite.{u}) AddCommGroupCat.{u+1}
 
-def eq : CondensedAb' ≌ CondensedAb := Condensed.ProfiniteCompHaus.equivalence AddCommGroupCat
+example : CondensedAb' ≌ CondensedAb := Condensed.ProfiniteCompHaus.equivalence AddCommGroupCat
 
 
 variable (S : Profinite.{u}) (X : CondensedAb'.{u})
@@ -80,19 +79,75 @@ variable (S : Profinite.{u}) (X : CondensedAb'.{u})
 
 variable (T : LightProfinite.{u}) (Y : LightCondAb.{u})
 
-#check Y.val.obj (op T)
+#check Y.val.obj ⟨T⟩
 #check Y.cond
 
 
 variable (F : LightProfinite.{u}ᵒᵖ ⥤ AddCommGroupCat.{u})
-def Fs : LightCondAb.{u} := (presheafToSheaf _ _).obj F
+example : LightCondAb.{u} := (presheafToSheaf _ _).obj F
 
 
 variable (G : Profinite.{u}ᵒᵖ ⥤ AddCommGroupCat.{u+1})
-def Gs : CondensedAb'.{u} := (presheafToSheaf _ _).obj G
+example : CondensedAb'.{u} := (presheafToSheaf _ _).obj G
+
+example : Abelian CondensedAb := inferInstance
+
+example : Abelian LightCondAb := inferInstance
+
+end Two
 
 
 
 
 
-end CondensedAbs
+
+
+
+
+section Three
+
+/-!
+
+# Benefits of light condensed objects
+-/
+
+#check Condensed.discrete
+
+-- def CondensedIntegers : CondensedAb :=
+--   (Condensed.discrete AddCommGroupCat).obj (AddCommGroupCat.of ℤ)
+
+#check LightCondensed.discrete
+
+-- def LightCondensedIntegers : LightCondAb :=
+--   (LightCondensed.discrete AddCommGroupCat).obj (AddCommGroupCat.of ℤ)
+
+
+
+end Three
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+section Four
+
+/-!
+# Categorical foundations for condensed mathematics
+-/
+
+#check coherentTopology
+#check regularCoverage
+#check extensiveCoverage
+
+end Four
