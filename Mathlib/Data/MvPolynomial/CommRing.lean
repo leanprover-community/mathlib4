5a38830c3ed97541c03d58af8185430becee92a7
Jujian Zhang <jujian.zhang1998@outlook.com>
2024-01-09 16:08:52 +0000

    golf

--- Mathlib/Data/MvPolynomial/CommRing.lean
+++ Mathlib/Data/MvPolynomial/CommRing.lean
@@ -72,21 +72,18 @@ set_option linter.uppercaseLean3 false in
 
 variable {σ} in
 lemma X_sub_C_ne_zero (i : σ) (c : R) [Nontrivial R] : X i - C c ≠ 0 := by
-  convert X_add_C_ne_zero i (- c) using 1
-  rw [C_neg, sub_eq_add_neg]
-
-variable {σ} in
-lemma X_sub_C_not_isUnit (i : σ) (c : R) [Nontrivial R] : ¬ IsUnit (X i - C c) := by
-  classical
-  rintro ⟨⟨_, m, hm1, hm2⟩, rfl⟩
-  have r := congr_arg (eval <| Finsupp.single i c) hm1
-  simp only [map_mul, map_sub, eval_X, Finsupp.single_eq_same, eval_C, sub_self, zero_mul,
-    map_one] at r
-  exact (one_ne_zero' R r.symm)
+  simpa [C_neg, sub_eq_add_neg] using X_add_C_ne_zero i (- c)
 
 variable {σ} in
 lemma X_add_C_not_isUnit (i : σ) (c : R) [Nontrivial R] : ¬ IsUnit (X i + C c) := by
-  simpa only [map_neg, sub_neg_eq_add] using X_sub_C_not_isUnit i (- c)
+  rintro ⟨⟨_, m, hm1, hm2⟩, rfl⟩
+  have r := congr_arg (eval <| Finsupp.single i (-c)) hm1
+  simp only [single_neg, coe_neg, map_mul, map_add, eval_X, Pi.neg_apply, single_eq_same, eval_C,
+    add_left_neg, zero_mul, map_one, zero_ne_one] at r
+
+variable {σ} in
+lemma X_sub_C_not_isUnit (i : σ) (c : R) [Nontrivial R] : ¬ IsUnit (X i - C c) := by
+  simpa [map_neg, sub_eq_add_neg] using X_add_C_not_isUnit i (-c)
 
 @[simp]
 theorem coeff_neg (m : σ →₀ ℕ) (p : MvPolynomial σ R) : coeff m (-p) = -coeff m p :=
