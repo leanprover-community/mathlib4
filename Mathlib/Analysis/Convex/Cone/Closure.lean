c368c14b9e1d994650fd034d70bdfed1cabbbbf0
Apurva Nakade <apurvanakade@gmail.com>
2024-01-09 10:25:29 -0500

    Fix PR review suggestion

--- Mathlib/Analysis/Convex/Cone/Closure.lean
+++ Mathlib/Analysis/Convex/Cone/Closure.lean
@@ -63,7 +63,7 @@ lemma toConvexCone_closure_pointed (K : PointedCone 𝕜 E) : (K : ConvexCone �
 /-- The closure of a pointed cone inside a topological space as a pointed cone. This
 construction is mainly used for defining maps between proper cones. -/
 protected def closure (K : PointedCone 𝕜 E) : PointedCone 𝕜 E :=
-  ConvexCone.toPointedCone K.closure_aux
+  ConvexCone.toPointedCone K.toConvexCone_closure_pointed
 
 @[simp, norm_cast]
 theorem coe_closure (K : PointedCone 𝕜 E) : (K.closure : Set E) = closure K :=
