0b0ec81ca9d00d9abf58928e422b8573a9b4b90f
Michael Rothgang <rothgami@math.hu-berlin.de>
2024-01-11 00:52:28 +0100

    Various fixes, enhancements and polish:
    - correct definition of smooth embeddings (they need to be immersions,
    otherwise there are counterexamples)
    - add open embeddings and relations
    - sketch/mostly add proof that surjective smooth embeddings are diffeos,
    following Lee (missing a linear algebra lemma; fighting with universes there)
    - tweak and proof-read docs; fix linter errors

--- Mathlib/Geometry/Manifold/Maps.lean
+++ Mathlib/Geometry/Manifold/Maps.lean
@@ -13,14 +13,17 @@ and prove their various relations and basic properties.
 
 ## Main definitions (xxx flesh out)
 * `Immersion`: a differentiable immersion
-* `InjImmersion`
-* `Submersion`
+* `InjImmersion`: an injective immersion
+* `Submersion`: a smooth submersion (not assumed to be surjective)
+* `OpenSmoothEmbedding`, `SmoothEmbedding`: (open) smooth embeddings
+
 
 ## Main results
 * `IsLocalDiffeomorph.toImmersion`: a `C^n` local diffeomorphism (`n≥1`) is an immersion
 * `IsLocalDiffeomorph.toSubmersion`: a `C^n` local diffeomorphism (`n≥1`) is a submersion
 * `Diffeomorph.toImmersion`: a `C^n` diffeomorphism (`n≥1`) is an immersion
 * `Diffeomorph.toSubmersion`: a `C^n` diffeomorphism (`n≥1`) is a submersion
+* `Diffeomorph.toOpenSmoothEmbedding`: a `C^n` diffeomorphism (`n≥1`) is an open smooth embedding
 
 * `IsLocalDiffeomorph.of_immersion_submersion`: if `f` is both an immersion and submersion,
   it is a local diffeomorphism.
@@ -29,14 +32,23 @@ and prove their various relations and basic properties.
 * `Diffeomorph.of_injImmersion_submersion_isProperMap`: a proper injective immersion and submersion
   is a diffeomorphism
 
+* `SmoothEmbedding.diffeomorph_of_surjective`: surjective smooth embeddings of finite-dimensional
+  manifolds are diffeomorphisms
+* `SmoothEmbedding.toInjImmersion`: smooth embeddings are injective immersions
+
 ## TODO
 - A submersion has open range (by the inverse function theorem).
-- Define smooth embeddings; show smooth embeddings are injective immersions;
-  show surjective embeddings are diffeomorphisms (and vice versa).
+- `SmoothEmbedding.toDiffeomorphRange`: a smooth embedding is a diffeomorphism to its range
+  (this requires submanifolds to *state*)
 
 ## Implementation notes
 - design decision: bundled (vs unbundled, like sphere-eversion's Immersion)
 - omit differentiability for immersions, following sphere-eversion?
+  (probably not: at best provide a constructor omitting this)
+- smooth embeddings don't assume smoothness of the inverse; this is automatic in finite dimension
+
+**NOTE.** These are **not** the correct definitions in the infinite-dimensional context,
+but in finite dimensions, the general definitions are equivalent to the ones in this file.
 
 ## Tags
 manifold, immersion, submersion, smooth embedding
@@ -51,7 +63,7 @@ open scoped Manifold
 -- Let `M` be a manifold with corners over the pair `(E, H)`.
 -- Let `M'` be a manifold with corners over the pair `(E', H')`.
 variable {𝕜 : Type*} [NontriviallyNormedField 𝕜]
-  {E : Type*} [NormedAddCommGroup E] [NormedSpace 𝕜 E]
+  {E : Type*} [instE: NormedAddCommGroup E] [instE': NormedSpace 𝕜 E]
   {H : Type*} [TopologicalSpace H] (I : ModelWithCorners 𝕜 E H)
   {M : Type*} [TopologicalSpace M] [ChartedSpace H M]
   [SmoothManifoldWithCorners I M]
@@ -62,7 +74,7 @@ variable {𝕜 : Type*} [NontriviallyNormedField 𝕜]
 
 section Definitions
 
--- for reference: design from sphere-eversion -- unbundled; omitted differentiability
+-- for reference: design from sphere-eversion -- omitted differentiability
 /-- A map between manifolds is an immersion if it is differentiable and its differential at
 any point is injective. Note the formalized definition doesn't require differentiability.
 If `f` is not differentiable at `m` then, by definition, `mfderiv I I' f m` is zero, which
@@ -70,7 +82,7 @@ is not injective unless the source dimension is zero, which implies differentiab
 def Immersion' (f : M → M') : Prop :=
   ∀ p, Injective (mfderiv I I' f p)
 
-/-- A `C^n` immersion `f : M → M` is a `C^n` map whose differential is injective at any point. -/
+/-- A `C^n` immersion `f : M → M` is a `C^n` map whose differential is injective at every point. -/
 -- We don't require immersions to be injective: for instance, the figure eight shall be an immersed
 -- manifold, whose most natural parametrisation is non-injective.
 structure Immersion (f : M → M') (n : ℕ∞) : Prop :=
@@ -78,15 +90,33 @@ structure Immersion (f : M → M') (n : ℕ∞) : Prop :=
   diff_injective : ∀ p, Injective (mfderiv I I' f p)
 
 /-- An injective `C^n immersion -/
-structure InjImmersion (f : M → M') (n : ℕ∞) extends
-  Immersion I I' f n : Prop :=
+structure InjImmersion (f : M → M') (n : ℕ∞) extends Immersion I I' f n : Prop :=
   injective : Injective f
 
-/-- A `C^n` submersion `f : M → M` is a `C^n` map whose differential is surjective at any point.
-We don't ask for a submersion to be surjective. -/
+/-- A `C^n` submersion `f : M → M` is a `C^n` map whose differential is surjective at every point.
+  We do not require submersions to be surjective. -/
 structure Submersion (f : M → M') (n : ℕ∞) : Prop :=
   differentiable : ContMDiff I I' n f
   diff_surjective : ∀ p, Surjective (mfderiv I I' f p)
+
+/-- A `C^n` embedding `f : M → M'` is a `C^n` map which is both an immersion and a topological
+  embedding. (We do not assume smoothness of the inverse, as this follows automatically.
+  See `SmoothEmbedding.diffeomorph_of_surjective` and variants.) -/
+-- FIXME: this should extend both Embedding and Immersion... possible? in which order?
+structure SmoothEmbedding (f : M → M') (n : ℕ∞) extends Embedding f /-Immersion I I' f n-/ : Prop :=
+  differentiable : ContMDiff I I' n f
+  diff_injective : ∀ p, Injective (mfderiv I I' f p)
+
+-- FIXME: this could also extend OpenEmbedding... same question as above
+/-- A `SmoothEmbedding` with open range. -/
+structure OpenSmoothEmbedding (f : M → M') (n : ℕ∞) extends SmoothEmbedding I I' f n : Prop :=
+  open_range : IsOpen <| range f
+
+-- FIXME: can I avoid this lemma, or is this a necessary by-product of no multiple extend's?
+lemma OpenSmoothEmbedding.toOpenEmbedding (f : M → M') (n : ℕ∞) (h : OpenSmoothEmbedding I I' f n) :
+    OpenEmbedding f where
+  toEmbedding := h.toEmbedding
+  open_range := h.open_range
 
 end Definitions
 
@@ -94,9 +124,10 @@ section LocalDiffeo
 variable {f : M → M'} {n : ℕ∞}
 variable {I I'}
 
--- TODO: prove this, using the inverse function theorem. this might require a complete space,
--- (can apply the open mapping theorem, to circumvent type theory raisins)
--- perhaps can weaken differentiability requirement
+-- TODO: prove this, using the inverse function theorem
+-- perhaps we can weaken differentiability requirement
+-- this might require a complete space, for type theory reasons
+-- (in a complete space, we can apply the open mapping theorem)
 lemma IsLocalDiffeomorphAt.of_bijective_differential {x : M} (hf : ContMDiff I I' n f)
     (h : Bijective (mfderiv I I' f x)) : IsLocalDiffeomorphAt I I' n f x := sorry
 
@@ -121,6 +152,7 @@ lemma IsLocalDiffeomorph.toSubmersion (hf : IsLocalDiffeomorph I I' n f) (hn : 1
   differentiable := hf.contMDiff
   diff_surjective x := (hf x).mfderiv_surjective hn
 
+-- TODO: deduplicate these proofs with toOpenSmoothEmbedding below?
 /-- A `C^n` diffeomorphism (`n≥1`) is an immersion. -/
 lemma Diffeomorph.toImmersion (h : Diffeomorph I I' M M' n) (hn : 1 ≤ n) : Immersion I I' h n where
   differentiable := h.contMDiff_toFun
@@ -138,6 +170,15 @@ lemma Diffeomorph.toSubmersion (h : Diffeomorph I I' M M' n) (hn : 1 ≤ n) :
   differentiable := h.contMDiff_toFun
   diff_surjective x := (h.isLocalDiffeomorph x).mfderiv_surjective hn
 
+/-- A `C^n` diffeomorphism (`n≥1`) is an open smooth embedding. -/
+lemma Diffeomorph.toOpenSmoothEmbedding (h : Diffeomorph I I' M M' n) (hn : 1 ≤ n) :
+    OpenSmoothEmbedding I I' h n where
+  differentiable := h.contMDiff_toFun
+  diff_injective x := (h.isLocalDiffeomorph x).mfderiv_injective hn
+  induced := h.toHomeomorph.inducing.induced
+  inj := h.toHomeomorph.injective
+  open_range := h.toHomeomorph.isOpenMap.isOpen_range
+
 /-- If `f` is both an immersion and submersion, it is a local diffeomorphism. -/
 theorem IsLocalDiffeomorph.of_immersion_submersion (h : Immersion I I' f n)
     (hf : Submersion I I' f n) : IsLocalDiffeomorph I I' n f :=
@@ -149,13 +190,13 @@ def Diffeomorph.of_immersion_submersion_bijective (h : Immersion I I' f n)
     (hf : Submersion I I' f n) (hbij : Bijective f) : Diffeomorph I I' M M' n :=
   (IsLocalDiffeomorph.of_immersion_submersion h hf).diffeomorph_of_bijective hbij
 
--- xxx: necessary?
+-- xxx: is this lemma useful enough?
 lemma Diffeomorph.isProperMap (h : Diffeomorph I I' M M' n) : IsProperMap h.toFun :=
   h.toHomeomorph.isProperMap
 
 /-- If `M'` is non-empty connected, an injective proper immersion `f : M → M'` which is a submersion
  is a diffeomorphism. -/
-theorem Diffeomorph.of_injImmersion_submersion_isProperMap [Nonempty M] (hconn : ConnectedSpace M')
+def Diffeomorph.of_injImmersion_submersion_isProperMap [Nonempty M] (hconn : ConnectedSpace M')
     (himm : InjImmersion I I' f n) (hsub : Submersion I I' f n) (hprop : IsProperMap f) :
     Diffeomorph I I' M M' n :=
   Diffeomorph.of_immersion_submersion_bijective himm.toImmersion hsub
@@ -163,32 +204,69 @@ theorem Diffeomorph.of_injImmersion_submersion_isProperMap [Nonempty M] (hconn :
 
 end LocalDiffeo
 
-/-- A `C^n` embedding -/
-structure SmoothEmbedding (f : M → M') (n : ℕ∞) extends Embedding f : Prop :=
-  differentiable : ContMDiff I I' n f
-
--- xxx: which structure to extend? not sure yet!
--- sphere-eversion avoided extending anything, which is not ideal...
-structure OpenSmoothEmbedding (f : M → M') (n : ℕ∞) extends SmoothEmbedding I I' f n : Prop :=
-  open_range : IsOpen <| range f
-
-namespace SmoothEmbedding
 variable (f : M → M') (n : ℕ∞)
 
--- xxx: do I need M to be T0?
-def toInjImmersion [T0Space M] (h : SmoothEmbedding I I' f n) : InjImmersion I I' f n := {
-  differentiable := h.differentiable
-  injective := h.toEmbedding.injective
-  diff_injective := sorry -- xxx: extra conditions needed?
-}
+namespace SmoothEmbedding
 
--- an injective immersion need not be an embedding. standard example with bijective cont, not homeo
+/-- A smooth embedding is an injective immersion. -/
+lemma toInjImmersion (h : SmoothEmbedding I I' f n) : InjImmersion I I' f n where
+  differentiable := h.differentiable
+  diff_injective := h.diff_injective
+  injective := h.toEmbedding.inj
+
+-- an injective immersion need not be an embedding: cue the standard example
+
+section missing -- TODO: move, #find_home says Mathlib.Topology.Algebra.Module.FiniteDimensio
+variable {E F : Type*} [SeminormedAddCommGroup E] [NormedSpace 𝕜 E] [SeminormedAddCommGroup F]
+  [NormedSpace 𝕜 F] {f : E →L[𝕜] F}
+
+/-- An injective linear endomorphism of a finite-dimensional space is a linear equivalence. -/
+def _root_.LinearEquiv.of_injective_endo_of_finiteDimensional {𝕜 E : Type*} [NormedField 𝕜]
+    [SeminormedAddCommGroup E] [NormedSpace 𝕜 E] [FiniteDimensional 𝕜 E]
+    {f : E →L[𝕜] E} (hf : Function.Injective f) : LinearEquiv (RingHom.id 𝕜) E E := sorry
+
+universe v v'
+/-- An injective linear map between finite-dimensional normed space of equal dimension
+  is a linear equivalence. -/
+-- TODO: make the statement type-check
+def _root_.LinearEquiv.of_injective_of_finiteDimensional {𝕜 : Type v} {E : Type v} {F : Type v'} [NormedField 𝕜]
+    [SeminormedAddCommGroup E] [NormedSpace 𝕜 E] [FiniteDimensional 𝕜 E]
+    [SeminormedAddCommGroup F] [NormedSpace 𝕜 F] [FiniteDimensional 𝕜 F]
+    {f : E →L[𝕜] F} (hf : Function.Injective f) :
+    -- TODO: how to compare these ranks? live in different universes...
+    -- (hdim : Cardinal.lift.{v, max v v'} Module.rank 𝕜 E = Cardinal.lift.{v', max v v'} Module.rank 𝕜 F) :
+    LinearEquiv (RingHom.id 𝕜) E F := by
+  -- let r : Cardinal.{v} := Module.rank 𝕜 E
+  -- let r' : Cardinal.{v'} := Module.rank 𝕜 F
+  -- xxx how to compare these ranks?
+  sorry
+
+end missing
+
+/-- `TangentSpace I x` is defeq to `E`, hence also a normed additive abelian group. -/
+local instance (x : M) : NormedAddCommGroup (TangentSpace I x) := instE
+/-- `TangentSpace I x` is defeq to `E`, hence also a normed space. -/
+local instance (x : M) : NormedSpace 𝕜 (TangentSpace I x) := instE'
+
+/-- A surjective smooth embedding of finite-dimensional manifolds is a diffeomorphism:
+  in particular, its inverse map is smooth. -/
+def diffeomorph_of_surjective [ifin: FiniteDimensional 𝕜 E]
+    (h : SmoothEmbedding I I' f n) (hf : Surjective f) : Diffeomorph I I' M M' n := by
+  -- we follow Lee, Proposition 5.5.7 (but avoid passing to local charts)
+  suffices h' : IsLocalDiffeomorph I I' n f from
+    IsLocalDiffeomorph.diffeomorph_of_bijective h' ⟨h.toEmbedding.inj, hf⟩
+  intro x
+  have aux : Injective (mfderiv I I' f x) := h.diff_injective x
+  -- as E is finite-dimensional, the differential is also surjective
+  haveI : FiniteDimensional 𝕜 (TangentSpace I' (f x)) := sorry -- ifin
+  -- let r := LinearEquiv.of_injective_of_finiteDimensional aux (𝕜 := 𝕜) --(E := TangentSpace)
+  have : Bijective (mfderiv I I' f x) := sorry -- LinearEquiv is bijective
+  exact IsLocalDiffeomorphAt.of_bijective_differential h.differentiable this
 
 end SmoothEmbedding
 
-def IsLocalDiffeomorph.toSmoothEmbedding {f : M → M'} {n : ℕ∞} (hf : IsLocalDiffeomorph I I' n f) :
-  SmoothEmbedding I I' f n := {
-    differentiable := sorry
-    induced := sorry
-    inj := sorry--diff_injective := sorry
-  }
+/- cannot state the following yet: needs that the range of a smooth embedding is a submanifold
+  would follow from the same proof as SmoothEmbedding.diffeomorph_of_surjective
+/-- A smooth embedding (in finite dimensions!) is a diffeomorphism to its image. -/
+def Diffeomorph.ofSmoothEmbedding [FiniteDimensional 𝕜 E] (h : SmoothEmbedding I I' f n) :
+    Diffeomorph I I' M (range f) n := sorry -/
