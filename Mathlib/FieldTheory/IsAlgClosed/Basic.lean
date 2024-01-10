43b13cb1982110436798d4416260bb91abc93a12
Junyan Xu <junyanxu.math@gmail.com>
2024-01-10 13:45:32 -0500

    Docstring&reference for surjective_comp_algebraMap_of_isAlgebraic

--- Mathlib/FieldTheory/IsAlgClosed/Basic.lean
+++ Mathlib/FieldTheory/IsAlgClosed/Basic.lean
@@ -212,6 +212,9 @@ namespace IsAlgClosed
 variable {K : Type u} [Field K] {L : Type v} {M : Type w} [Field L] [Algebra K L] [Field M]
   [Algebra K M] [IsAlgClosed M]
 
+/-- If E/L/K is a tower of field extensions with E/L algebraic, and if M is an algebraically
+  closed extension of K, then any embedding of L/K into M/K extends to an embedding of E/K.
+  Known as the extension lemma in https://math.stackexchange.com/a/687914. -/
 theorem surjective_comp_algebraMap_of_isAlgebraic {E : Type*}
     [Field E] [Algebra K E] [Algebra L E] [IsScalarTower K L E] (hE : Algebra.IsAlgebraic L E) :
     Function.Surjective fun φ : E →ₐ[K] M ↦ φ.comp (IsScalarTower.toAlgHom K L E) :=
