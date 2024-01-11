ec235e02b69e27735706151bca637f9db6c333d6
Joachim Breitner <mail@joachim-breitner.de>
2024-01-11 13:25:41 +0100

    Trace std4 nightly testing again

--- lakefile.lean
+++ lakefile.lean
@@ -26,7 +26,7 @@ package mathlib where
 meta if get_config? doc = some "on" then -- do not download and build doc-gen4 by default
 require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
 
-require std from git "https://github.com/nomeata/std4" @ "joachim/fix-nightly-2023-01-11"
+require std from git "https://github.com/leanprover/std4" @ "nightly-testing"
 require Qq from git "https://github.com/leanprover-community/quote4" @ "master"
 require aesop from git "https://github.com/leanprover-community/aesop" @ "nightly-testing"
 require proofwidgets from git "https://github.com/leanprover-community/ProofWidgets4" @ "v0.0.25"
