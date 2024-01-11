a3be30dae0dc727481527390c392dbae0f248823
Alex Kontorovich <58564076+AlexKontorovich@users.noreply.github.com>
2024-01-11 10:22:04 -0500

    Update Mathlib/MeasureTheory/Group/FundamentalDomain.lean

--- Mathlib/MeasureTheory/Group/FundamentalDomain.lean
+++ Mathlib/MeasureTheory/Group/FundamentalDomain.lean
@@ -777,8 +777,8 @@ TODO: Formalize the existence of a Dirichlet domain as in Kapovich's paper.
 
 section HasFundamentalDomain
 
-/-- "We say a quotient of `α` by `G` `HasFundamentalDomain` if there is a measurable set
-  `s` for which `IsFundamentalDomain G s` holds." -/
+/-- We say a quotient of `α` by `G` `HasFundamentalDomain` if there is a measurable set
+  `s` for which `IsAddFundamentalDomain G s` holds. -/
 class HasAddFundamentalDomain (G α : Type*) [Zero G] [VAdd G α] [MeasureSpace α] : Prop where
   has_add_fundamental_domain_characterization : ∃ s : Set α, IsAddFundamentalDomain G s
 
