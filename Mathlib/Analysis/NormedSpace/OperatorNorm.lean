5b4ebf745e54e2c75c817b7b8711d474820cc715
Matthew Ballard <matt@mrb.email>
2024-01-11 13:33:19 -0500

    fix

--- Mathlib/Analysis/NormedSpace/OperatorNorm.lean
+++ Mathlib/Analysis/NormedSpace/OperatorNorm.lean
@@ -999,7 +999,6 @@ def prodMapL : (M₁ →L[𝕜] M₂) × (M₃ →L[𝕜] M₄) →L[𝕜] M₁ 
       rw [flip_apply, flip_apply]
       rw [compL_apply, compL_apply, compL_apply, compL_apply]
       rw [comp_apply, comp_apply, comp_apply, comp_apply]
-      -- repeat first | rw [add_apply] | rw [comp_apply] | rw [flip_apply] | rw [compL_apply]
       simp only [coe_prodMap', Prod_map, coe_fst', inl_apply, coe_snd', inr_apply, Prod.mk_add_mk,
         add_zero, zero_add])
 #align continuous_linear_map.prod_mapL ContinuousLinearMap.prodMapL
