f18bb9016224658280a925292f30386d92929373
Michael Rothgang <rothgami@math.hu-berlin.de>
2024-01-12 00:19:44 +0100

    Better name: H and H' are used elsewhere; no assumptions made on them.

--- Mathlib/Geometry/Manifold/ChartedSpace.lean
+++ Mathlib/Geometry/Manifold/ChartedSpace.lean
@@ -112,7 +112,7 @@ composition of partial equivs with `≫`.
 
 noncomputable section
 
-open Classical Topology Filter
+open Topology
 
 universe u
 
@@ -789,7 +789,7 @@ instance prodChartedSpace (H : Type*) [TopologicalSpace H] (M : Type*) [Topologi
 section prodChartedSpace
 
 @[ext]
-theorem ModelProd.ext {α β : Type*} {x y : ModelProd α β} (h₁ : x.1 = y.1) (h₂ : x.2 = y.2) : x = y :=
+theorem ModelProd.ext {x y : ModelProd H H'} (h₁ : x.1 = y.1) (h₂ : x.2 = y.2) : x = y :=
   Prod.ext h₁ h₂
 
 variable [TopologicalSpace H] [TopologicalSpace M] [ChartedSpace H M] [TopologicalSpace H']
