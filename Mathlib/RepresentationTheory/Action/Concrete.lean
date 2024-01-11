5a2b8f1e878df4d5f30593a9976419ffcded23af
Christian Merten <christian@merten.dev>
2024-01-11 22:50:00 +0100

    add ofMulAction constructor for FintypeCat

--- Mathlib/RepresentationTheory/Action/Concrete.lean
+++ Mathlib/RepresentationTheory/Action/Concrete.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison
 -/
 import Mathlib.RepresentationTheory.Action.Basic
+import Mathlib.CategoryTheory.FintypeCat
 
 /-!
 # Constructors for `Action V G` for some concrete categories
@@ -75,5 +76,21 @@ def diagonalOneIsoLeftRegular (G : Type u) [Monoid G] : diagonal G 1 ≅ leftReg
   Action.mkIso (Equiv.funUnique _ _).toIso fun _ => rfl
 set_option linter.uppercaseLean3 false in
 #align Action.diagonal_one_iso_left_regular Action.diagonalOneIsoLeftRegular
+
+section FintypeCat
+
+/-- Bundles a finite type `H` with a multiplicative action of `G` as an `Action`. -/
+@[simps]
+def FintypeCat.ofMulAction (G : Type u) (H : FintypeCat.{u}) [Monoid G] [MulAction G H] :
+    Action FintypeCat (MonCat.of G) where
+  V := H
+  ρ := @MulAction.toEndHom _ _ _ (by assumption)
+
+@[simp]
+theorem FintypeCat.ofMulAction_apply {G : Type u} {H : FintypeCat.{u}} [Monoid G] [MulAction G H]
+    (g : G) (x : H) : (ofMulAction G H).ρ g x = (g • x : H) :=
+  rfl
+
+end FintypeCat
 
 end Action
