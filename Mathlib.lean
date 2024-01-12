006ccbeb21b1777316eb4b7180813738d0e9415e
acmepjz <acme_pjz@hotmail.com>
2024-01-12 09:21:56 +0800

    rename file

--- Mathlib.lean
+++ Mathlib.lean
@@ -2094,7 +2094,7 @@ import Mathlib.FieldTheory.IsSepClosed
 import Mathlib.FieldTheory.KrullTopology
 import Mathlib.FieldTheory.KummerExtension
 import Mathlib.FieldTheory.Laurent
-import Mathlib.FieldTheory.LinearlyDisjoint
+import Mathlib.FieldTheory.LinearDisjoint
 import Mathlib.FieldTheory.Minpoly.Basic
 import Mathlib.FieldTheory.Minpoly.Field
 import Mathlib.FieldTheory.Minpoly.IsIntegrallyClosed
