fabbeae02c284888ebef8e0a15a829b0c1d385cb
Junyan Xu <junyanxu.math@gmail.com>
2024-01-12 01:16:18 -0500

    one more >100

--- Mathlib/LinearAlgebra/FreeModule/Finite/Matrix.lean
+++ Mathlib/LinearAlgebra/FreeModule/Finite/Matrix.lean
@@ -55,7 +55,8 @@ theorem FiniteDimensional.rank_linearMap :
     ← finrank_eq_card_chooseBasisIndex, ← finrank_eq_rank R, lift_natCast]
 
 /-- The finrank of `M →ₗ[R] N` as an `S`-module is `(finrank R M) * (finrank S N)`. -/
-theorem FiniteDimensional.finrank_linearMap : finrank S (M →ₗ[R] N) = finrank R M * finrank S N := by
+theorem FiniteDimensional.finrank_linearMap :
+    finrank S (M →ₗ[R] N) = finrank R M * finrank S N := by
   simp_rw [finrank, rank_linearMap, toNat_mul, toNat_lift]
 #align finite_dimensional.finrank_linear_map FiniteDimensional.finrank_linearMap
 
