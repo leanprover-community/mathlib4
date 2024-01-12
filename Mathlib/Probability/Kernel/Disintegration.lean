576e5527825821237700a36f2f97a6535a487377
JADekker <114015169+JADekker@users.noreply.github.com>
2024-01-12 10:25:16 +0100

    fix : incorrect import

--- Mathlib/Probability/Kernel/Disintegration.lean
+++ Mathlib/Probability/Kernel/Disintegration.lean
@@ -3,7 +3,7 @@ Copyright (c) 2023 Rémy Degenne. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Rémy Degenne, Kexing Ying
 -/
-import Mathlib.Probability.Kernel.CondCDF
+import Mathlib.Probability.Kernel.CondCdf
 import Mathlib.MeasureTheory.Constructions.Polish
 import Mathlib.Probability.Kernel.MeasureCompProd
 
