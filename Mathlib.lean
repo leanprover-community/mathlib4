6a9d7bb9ed51eb713a8f2368d99a413034540f47
madvorak <martin.dvorak@matfyz.cz>
2024-01-12 15:13:55 +0100

    added new file

--- Mathlib.lean
+++ Mathlib.lean
@@ -2463,6 +2463,7 @@ import Mathlib.LinearAlgebra.Matrix.Gershgorin
 import Mathlib.LinearAlgebra.Matrix.Hermitian
 import Mathlib.LinearAlgebra.Matrix.InvariantBasisNumber
 import Mathlib.LinearAlgebra.Matrix.IsDiag
+import Mathlib.LinearAlgebra.Matrix.LinearProgramming
 import Mathlib.LinearAlgebra.Matrix.LDL
 import Mathlib.LinearAlgebra.Matrix.MvPolynomial
 import Mathlib.LinearAlgebra.Matrix.Nondegenerate
