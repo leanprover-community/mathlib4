25e70565ea428ed596426610582b093aa2a9560b
Yury G. Kudryashov <urkud@urkud.name>
2024-01-13 14:51:40 -0600

    Snapshot

--- Mathlib/Algebra/AddConstMap/Basic.lean
+++ Mathlib/Algebra/AddConstMap/Basic.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2023 Yury Kudryashov. All rights reserved.
+Copyright (c) 2024 Yury Kudryashov. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yury Kudryashov
 -/
