d5e9ff69eca2b740bc17483b587393b09d33d1f8
Scott Morrison <scott.morrison@gmail.com>
2024-01-14 13:20:43 +1100

    feat: add heartbeats_variation command

--- Mathlib/Util/CountHeartbeats.lean
+++ Mathlib/Util/CountHeartbeats.lean
@@ -60,3 +60,28 @@ elab "count_heartbeats " "in" ppLine cmd:command : command => do
       Command.liftCoreM <| MetaM.run' do
         Std.Tactic.TryThis.addSuggestion (← getRef)
           (← set_option hygiene false in `(command| set_option maxHeartbeats $m in $cmd))
+
+/--
+Run a command, but then restore the original state, and report just the number of heartbeats.
+-/
+def elabForHeartbeats (cmd : TSyntax `command) : CommandElabM Nat := do
+  let start ← IO.getNumHeartbeats
+  -- FIXME really should revert more state here:
+  -- can someone advise how to roll back CommandElabM state?
+  let env ← getEnv
+  elabCommand (← `(command| set_option maxHeartbeats 0 in $cmd))
+  setEnv env
+  return (← IO.getNumHeartbeats) - start
+
+elab "heartbeat_variation " "in" ppLine cmd:command : command => do
+  let n := 10
+  let counts ← (List.range n).mapM fun _ => elabForHeartbeats cmd
+  let counts := counts.map fun i => i / 1000 -- convert to user-facing heartbeats
+  let counts := counts.toArray.qsort (· < ·)
+  let μ := counts.foldl (· + · ) 0 / n
+  -- We jump through some hoops here to get access to `Float.sqrt`, to avoid imports.
+  let var := Float.sqrt <| UInt64.toFloat <| UInt64.ofNat
+    ((counts.map fun i => (i - μ)^2).foldl (· + · ) 0)
+  let stddev := var.toUInt64.toNat * 100 / μ
+  elabCommand cmd
+  logInfo s!"Min: {counts[0]!} Max: {counts[n - 1]!} StdDev: {stddev}%"
