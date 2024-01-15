e2447d236e27f560fb4688de33d38cdbcadae985
David Kurniadi Angdinata <dka31@cantab.ac.uk>
2024-01-15 21:30:17 +0000

    Replace numerals with notation
    Co-authored-by: Michael Stoll <99838730+MichaelStollBayreuth@users.noreply.github.com>

--- Mathlib/AlgebraicGeometry/EllipticCurve/Projective.lean
+++ Mathlib/AlgebraicGeometry/EllipticCurve/Projective.lean
@@ -78,7 +78,7 @@ lemma smul_fin3 {R : Type u} [CommRing R] (P : Fin 3 → R) (u : Rˣ) :
   rfl
 
 lemma smul_fin3_ext {R : Type u} [CommRing R] (P : Fin 3 → R) (u : Rˣ) :
-    (u • P) 0 = u * P x ∧ (u • P) 1 = u * P y ∧ (u • P) 2 = u * P z :=
+    (u • P) x = u * P x ∧ (u • P) y = u * P y ∧ (u • P) z = u * P z :=
   ⟨rfl, rfl, rfl⟩
 
 /-! ## Weierstrass curves -/
