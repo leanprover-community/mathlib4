6a368321990075919cf4ca62ecaa8570327e226f
Yury G. Kudryashov <urkud@urkud.name>
2024-01-15 07:44:58 -0600

    Update Mathlib/Topology/Bornology/Absorbs.lean
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- Mathlib/Topology/Bornology/Absorbs.lean
+++ Mathlib/Topology/Bornology/Absorbs.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2023 Yury Kudryashov. All rights reserved.
+Copyright (c) 2020 Jean Lo, Yury Kudryashov. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Yury Kudryashov
+Authors: Jean Lo,Yury Kudryashov
 -/
 import Mathlib.Data.Set.Pointwise.SMul
 import Mathlib.Topology.Bornology.Constructions
