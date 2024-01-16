8f76a332dc5993a7e8236a49c6298531acf91ff1
smorel394 <67864981+smorel394@users.noreply.github.com>
2024-01-16 17:46:37 +0100

    Update Mathlib/LinearAlgebra/Multilinear/Basic.lean
    Co-authored-by: sgouezel <sebastien.gouezel@univ-rennes1.fr>

--- Mathlib/LinearAlgebra/Multilinear/Basic.lean
+++ Mathlib/LinearAlgebra/Multilinear/Basic.lean
@@ -808,7 +808,7 @@ lemma domDomRestrict_apply [DecidableEq ι] (f : MultilinearMap R M₁ M₂) (P 
     f.domDomRestrict P z x = f (fun j => if h : P j then x ⟨j, h⟩ else z ⟨j, h⟩) := rfl
 
 -- TODO: Should add a ref here when available.
-/-- The "derivative" of a multilinear map, as a linear map from `(i : ι) → M₁` to `M₂`.
+/-- The "derivative" of a multilinear map, as a linear map from `(i : ι) → M₁ i` to `M₂`.
 For continuous multilinear maps, this will indeed be the derivative.-/
 def linearDeriv [DecidableEq ι] [Fintype ι] (f : MultilinearMap R M₁ M₂)
     (x : (i : ι) → M₁ i) : ((i : ι) → M₁ i) →ₗ[R] M₂ :=
