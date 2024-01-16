8561dc3343c28b34317ba35c77f1a673cccf0692
JADekker <114015169+JADekker@users.noreply.github.com>
2024-01-16 10:54:16 +0100

    fix : feedback

--- Mathlib/Probability/Distributions/Geometric.lean
+++ Mathlib/Probability/Distributions/Geometric.lean
@@ -25,13 +25,16 @@ open MeasureTheory Real Set Filter Topology
 
 namespace ProbabilityTheory
 
+variable {p : ℝ}
+
 section GeometricPMF
+
 
 /-- The pmf of the geometric distribution depending on its success probability. -/
 noncomputable
 def geometricPMFReal (p : ℝ) (n : ℕ) : ℝ := (1-p) ^ n * p
 
-lemma geometricPMFRealSum {p : ℝ} (hp_pos : 0 < p) (hp_le_one : p <= 1) :
+lemma geometricPMFRealSum (hp_pos : 0 < p) (hp_le_one : p <= 1) :
     HasSum (fun n ↦ geometricPMFReal p n) 1 := by
   unfold geometricPMFReal
   have := hasSum_geometric_of_lt_1 (sub_nonneg.mpr hp_le_one) (sub_lt_self 1 hp_pos)
@@ -41,13 +44,13 @@ lemma geometricPMFRealSum {p : ℝ} (hp_pos : 0 < p) (hp_le_one : p <= 1) :
   exact this
 
 /-- The geometric pmf is positive for all natural numbers -/
-lemma geometricPMFReal_pos {p : ℝ} {n : ℕ} (hp_pos : 0 < p) (hp_lt_one : p < 1) :
+lemma geometricPMFReal_pos {n : ℕ} (hp_pos : 0 < p) (hp_lt_one : p < 1) :
     0 < geometricPMFReal p n := by
   rw [geometricPMFReal]
   have : 0 < 1-p := sub_pos.mpr hp_lt_one
   positivity
 
-lemma geometricPMFReal_nonneg {p : ℝ} {n : ℕ}  (hp_pos : 0 < p) (hp_le_one : p <= 1) :
+lemma geometricPMFReal_nonneg {n : ℕ} (hp_pos : 0 < p) (hp_le_one : p <= 1) :
     0 ≤ geometricPMFReal p n := by
   rw [geometricPMFReal]
   have : 0 ≤ 1-p := sub_nonneg.mpr hp_le_one
@@ -55,7 +58,7 @@ lemma geometricPMFReal_nonneg {p : ℝ} {n : ℕ}  (hp_pos : 0 < p) (hp_le_one :
 
 /-- Geometric distribution with success probability `p`. -/
 noncomputable
-def geometricPMF {p : ℝ} (hp_pos : 0 < p) (hp_le_one : p <= 1) : PMF ℕ := by
+def geometricPMF (hp_pos : 0 < p) (hp_le_one : p <= 1) : PMF ℕ := by
   refine ⟨fun n ↦ ENNReal.ofReal (geometricPMFReal p n), ?_⟩
   apply ENNReal.hasSum_coe.mpr
   rw [← toNNReal_one]
@@ -64,11 +67,11 @@ def geometricPMF {p : ℝ} (hp_pos : 0 < p) (hp_le_one : p <= 1) : PMF ℕ := by
 
 /-- The geometric pmf is measurable. -/
 @[measurability]
-lemma measurable_geometricPMFReal {p : ℝ} : Measurable (geometricPMFReal p) := by
+lemma measurable_geometricPMFReal : Measurable (geometricPMFReal p) := by
   measurability
 
 @[measurability]
-lemma stronglyMeasurable_geometricPMFReal {p : ℝ} :
+lemma stronglyMeasurable_geometricPMFReal :
     StronglyMeasurable (geometricPMFReal p) :=
   stronglyMeasurable_iff_measurable.mpr measurable_geometricPMFReal
 
@@ -76,10 +79,10 @@ end GeometricPMF
 
 /-- Measure defined by the geometric distribution -/
 noncomputable
-def geometricMeasure {p : ℝ} (hp_pos : 0 < p) (hp_le_one : p <= 1) : Measure ℕ :=
+def geometricMeasure (hp_pos : 0 < p) (hp_le_one : p <= 1) : Measure ℕ :=
     (geometricPMF hp_pos hp_le_one).toMeasure
 
-lemma isProbabilityMeasureGeometric {p : ℝ} (hp_pos : 0 < p) (hp_le_one : p <= 1) :
+lemma isProbabilityMeasureGeometric (hp_pos : 0 < p) (hp_le_one : p <= 1) :
     IsProbabilityMeasure (geometricMeasure hp_pos hp_le_one) :=
   PMF.toMeasure.isProbabilityMeasure (geometricPMF hp_pos hp_le_one)
 
