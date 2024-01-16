b10df9e8828ea4939b5cbdbfc9ae7b6e3375bab1
Richard Copley <rcopley@gmail.com>
2024-01-16 11:24:07 +0000

    doc(Algebra/Group/Defs): add note on names `SMul`, `VAdd`; typo fix

--- Mathlib/Algebra/Group/Defs.lean
+++ Mathlib/Algebra/Group/Defs.lean
@@ -26,11 +26,15 @@ The file does not contain any lemmas except for
 
 For basic lemmas about these classes see `Algebra.Group.Basic`.
 
-We also introduce notation classes `lul` and `VAdd` for multiplicative and additive
+We also introduce notation classes `SMul` and `VAdd` for multiplicative and additive
 actions and register the following instances:
 
 - `Pow M ℕ`, for monoids `M`, and `Pow G ℤ` for groups `G`;
 - `SMul ℕ M` for additive monoids `M`, and `SMul ℤ G` for additive groups `G`.
+
+`SMul` is typically, but not exclusively, used for scalar multiplication-like operators.
+A motivating example for the name `VAdd` (vector addition) is the action of a vector space
+on a Euclidean space, where the result of adding a vector to a point is a point.
 
 ## Notation
 
