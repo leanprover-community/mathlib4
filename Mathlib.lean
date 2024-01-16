cb454ea4eb8d2ab7a144866c82ec16c7c4e6b0f2
CBirkbeck <c.birkbeck@ucl.ac.uk>
2024-01-16 17:57:53 +0000

    fix file error

--- Mathlib.lean
+++ Mathlib.lean
@@ -2807,6 +2807,7 @@ import Mathlib.NumberTheory.MaricaSchoenheim
 import Mathlib.NumberTheory.Modular
 import Mathlib.NumberTheory.ModularForms.Basic
 import Mathlib.NumberTheory.ModularForms.CongruenceSubgroups
+import Mathlib.NumberTheory.ModularForms.EisensteinSeries.Basic
 import Mathlib.NumberTheory.ModularForms.JacobiTheta.Basic
 import Mathlib.NumberTheory.ModularForms.JacobiTheta.Manifold
 import Mathlib.NumberTheory.ModularForms.SlashActions
