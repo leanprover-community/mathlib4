66c0f1ad555bfbb4825be04ad262d7974b7d872e
adamtopaz <github@adamtopaz.com>
2024-01-16 14:07:11 -0700

    wip

--- Mathlib/FieldTheory/Cyclotomic.lean
+++ Mathlib/FieldTheory/Cyclotomic.lean
@@ -10,7 +10,7 @@ import Mathlib
 
 variable (k K : Type*) [Field k] [Field K] [Algebra k K]
 
-theorem exists_isPrimitiveRoot_of_isSepClosed {n : ℕ} [IsSepClosed K] [NeZero (n : K)] :
+theorem exists_isPrimitiveRoot_of_isSepClosed (n : ℕ) [IsSepClosed K] [NeZero (n : K)] :
     ∃ (ξ : K), IsPrimitiveRoot ξ n := by
   apply exists_isPrimitiveRoot_of_splits_cyclotomic
   apply IsSepClosed.splits_domain (Polynomial.cyclotomic n K)
@@ -20,11 +20,20 @@ noncomputable
 def cyclotomicCharacter (n : ℕ+) [IsSepClosure k K] [NeZero (n : K)] :
     (K ≃ₐ[k] K) →* (ZMod n)ˣ :=
   letI : IsSepClosed K := IsSepClosure.sep_closed k
-  IsPrimitiveRoot.autToPow k (exists_isPrimitiveRoot_of_isSepClosed K).choose_spec
+  IsPrimitiveRoot.autToPow k (exists_isPrimitiveRoot_of_isSepClosed K n).choose_spec
 
 lemma cyclotomicCharacter_spec (n : ℕ+) [IsSepClosure k K] [NeZero (n : K)]
     (ξ : Kˣ) (hξ : ξ ∈ rootsOfUnity n K) (σ : (K ≃ₐ[k] K)) :
-    ξ ^ (cyclotomicCharacter k K n σ : ZMod n).val = σ ξ := sorry
+    ξ ^ (cyclotomicCharacter k K n σ : ZMod n).val = σ ξ := by
+  letI : IsSepClosed K := IsSepClosure.sep_closed k
+  let hζ := exists_isPrimitiveRoot_of_isSepClosed K n |>.choose_spec
+  let ζ' : Kˣ := hζ.toRootsOfUnity.val
+  have hζ' : IsPrimitiveRoot ζ' n := IsPrimitiveRoot.coe_units_iff.mp hζ
+  obtain ⟨i,_,rfl⟩ := hζ'.eq_pow_of_mem_rootsOfUnity hξ
+  push_cast
+  rw [σ.map_pow, ← pow_mul, mul_comm, pow_mul]
+  congr 1
+  exact IsPrimitiveRoot.autToPow_spec k hζ σ
 
 lemma cyclotomicCharacter_unique (n : ℕ+) [IsSepClosure k K] [NeZero (n : K)]
     (χ : (K ≃ₐ[k] K) →* (ZMod n)ˣ)
