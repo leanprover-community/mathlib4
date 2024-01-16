dbf0ff3d607b99f1ff632f47e0c6dbd9583a16c7
Vierkantor <vierkantor@vierkantor.com>
2024-01-16 16:03:35 +0000

    Disable a `simpNF` check due to timeouts :(

--- Mathlib/FieldTheory/Subfield.lean
+++ Mathlib/FieldTheory/Subfield.lean
@@ -477,6 +477,9 @@ def topEquiv : (⊤ : Subfield K) ≃+* K :=
   Subsemiring.topEquiv
 #align subfield.top_equiv Subfield.topEquiv
 
+-- This triggers a timeout since #8386.
+attribute [nolint simpNF] topEquiv_apply
+
 /-! # comap -/
 
 
