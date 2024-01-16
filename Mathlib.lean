4c74b90c69cd5b9c6a98e7c85eaa63e114bcffb8
Oliver Nash <github@olivernash.org>
2024-01-16 15:36:15 +0000

    feat: define complete sublattices (#9763)

--- Mathlib.lean
+++ Mathlib.lean
@@ -2886,6 +2886,7 @@ import Mathlib.Order.CompleteBooleanAlgebra
 import Mathlib.Order.CompleteLattice
 import Mathlib.Order.CompleteLatticeIntervals
 import Mathlib.Order.CompletePartialOrder
+import Mathlib.Order.CompleteSublattice
 import Mathlib.Order.Concept
 import Mathlib.Order.ConditionallyCompleteLattice.Basic
 import Mathlib.Order.ConditionallyCompleteLattice.Finset
