203733c24eaaf3c3407628926cc1d5642bcb9f49
madvorak <martin.dvorak@matfyz.cz>
2024-01-16 13:50:34 +0100

    docs

--- Mathlib/LinearAlgebra/Matrix/LinearProgramming.lean
+++ Mathlib/LinearAlgebra/Matrix/LinearProgramming.lean
@@ -9,12 +9,21 @@ import Mathlib.LinearAlgebra.Matrix.DotProduct
 
 # Linear programming
 
-TODO
+We define linear programs over a `LinearOrderedField K` in the standard matrix form.
 
 ## Main definitions
 
  * `StandardLP` defines a linear program in the standard form $ \max cᵀx, A x ≤ b, x ≥ 0 $.
  * `StandardLP.IsSolution` tells if given vector is a solution satisfying given standard LP.
+ * `StandardLP.IsFeasible` tells if given standard LP has any solution.
+ * `StandardLP.Reaches` tells if given value can be obtained as a cost of any solution of given
+    standard LP.
+ * `StandardLP.dual` defines a dual problem to given standard LP.
+
+## Main results
+
+* `StandardLP.weakDuality`: The weak duality theorem (`cᵀx` such that `A x ≤ b` and `x ≥ 0` is
+   always less or equal to `bᵀy` such that `Aᵀ y ≥ c` and `y ≥ 0`).
 
 -/
 
@@ -31,18 +40,31 @@ structure StandardLP (m n K : Type) [Fintype m] [Fintype n] [LinearOrderedField 
 
 variable {m n K : Type} [Fintype m] [Fintype n] [LinearOrderedField K]
 
+/-- Vector `x` is a solution to linear program `P` iff all entries of `x` are nonnegative and its
+multiplication by matrix `A` from the left yields a vector whose all entries are less or equal to
+corresponding entries of the vector `b`. -/
 def StandardLP.IsSolution (P : StandardLP m n K) (x : n → K) : Prop :=
   P.A.mulVec x ≤ P.b ∧ 0 ≤ x
 
+/-- Linear program `P` is feasible iff there is a vector `x` that is a solution to `P`. -/
 def StandardLP.IsFeasible (P : StandardLP m n K) : Prop :=
   ∃ x : n → K, P.IsSolution x
 
+/-- Linear program `P` reaches objective value `v` iff there is a solution `x` such that,
+when its entries are elementwise multiplied by the costs (given by the vector `c`) and summed up,
+the result is the value `v`. -/
 def StandardLP.Reaches (P : StandardLP m n K) (v : K) : Prop :=
   ∃ x : n → K, P.IsSolution x ∧ P.c ⬝ᵥ x = v
 
+/-- Dual linear program to given linear program `P` in the standard form.
+The matrix gets transposed and its values flip signs.
+The original cost function gets flipped signs as well and becomes the new righ-hand side vector.
+The original righ-hand side vector becomes the new vector of objective function coefficients. -/
 def StandardLP.dual (P : StandardLP m n K) : StandardLP n m K :=
   ⟨-P.Aᵀ, -P.c, P.b⟩
 
+/-- Objective values reached by linear program `P` are all less or equal to all objective values
+reached by the dual of `P`. -/
 theorem StandardLP.weakDuality {P : StandardLP m n K}
     {v : K} (hP : P.Reaches v) {w : K} (hD : P.dual.Reaches w) :
     v ≤ w := by
