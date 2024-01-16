2f538acef9c195cdfd34feca76510797f6b93056
Anatole Dedecker <anatolededecker@gmail.com>
2024-01-16 00:56:23 +0000

    chore: replace SubgroupClass.inv by InvMemClass.inv (#9761)

--- Mathlib/Analysis/Convex/Cone/Extension.lean
+++ Mathlib/Analysis/Convex/Cone/Extension.lean
@@ -75,7 +75,7 @@ theorem step (nonneg : ∀ x : f.domain, (x : E) ∈ s → 0 ≤ f x)
       simpa only [Set.Nonempty, upperBounds, lowerBounds, ball_image_iff] using this
     refine' exists_between_of_forall_le (Nonempty.image f _) (Nonempty.image f (dense y)) _
     · rcases dense (-y) with ⟨x, hx⟩
-      rw [← neg_neg x, AddSubgroupClass.coe_neg, ← sub_eq_add_neg] at hx
+      rw [← neg_neg x, NegMemClass.coe_neg, ← sub_eq_add_neg] at hx
       exact ⟨_, hx⟩
     rintro a ⟨xn, hxn, rfl⟩ b ⟨xp, hxp, rfl⟩
     have := s.add_mem hxp hxn
