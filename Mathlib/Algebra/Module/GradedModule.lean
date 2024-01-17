3482d184f37df4b8338c750f0122dc4bb40c3106
Jujian Zhang <jujian.zhang1998@outlook.com>
2024-01-17 19:12:33 +0000

    generalize

--- Mathlib/Algebra/Module/GradedModule.lean
+++ Mathlib/Algebra/Module/GradedModule.lean
@@ -247,4 +247,20 @@ def linearEquiv [DecidableEq ιA] [DecidableEq ιM] [GradedRing 𝓐] [DirectSum
   rfl
 #align graded_module.linear_equiv GradedModule.linearEquiv
 
+/--
+The projection map of an internally graded module.
+-/
+@[simps]
+def proj [DecidableEq ιM] [Decomposition 𝓜]  (i : ιM) : M →+ M where
+  toFun m := decompose 𝓜 m i
+  map_zero' := by simp
+  map_add' := by simp
+
+/--
+For each `a : M`, `GradedModule.homogeneousComponents ℳ a` is the collection of the
+homogeneous components of `a`, which is a finite subset of `M`.
+-/
+def homogeneousComponents [DecidableEq ιM] [Decomposition 𝓜] [DecidableEq M] (a : M) : Finset M :=
+    (decompose 𝓜 a).support.image (decompose 𝓜 a ·)
+
 end GradedModule
