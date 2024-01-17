059c4da1b94fa05c6502230920dd0d0ee6dcf722
Apurva Nakade <apurvanakade@gmail.com>
2024-01-17 12:01:02 -0500

    Rename file

--- Mathlib.lean
+++ Mathlib.lean
@@ -700,8 +700,8 @@ import Mathlib.Analysis.Convex.Combination
 import Mathlib.Analysis.Convex.Complex
 import Mathlib.Analysis.Convex.Cone.Basic
 import Mathlib.Analysis.Convex.Cone.Closure
-import Mathlib.Analysis.Convex.Cone.Dual
 import Mathlib.Analysis.Convex.Cone.Extension
+import Mathlib.Analysis.Convex.Cone.InnerDual
 import Mathlib.Analysis.Convex.Cone.Pointed
 import Mathlib.Analysis.Convex.Cone.Proper
 import Mathlib.Analysis.Convex.Contractible
