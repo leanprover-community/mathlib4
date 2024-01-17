390083a20bb91595e2baefdb18bcad0995a325a9
Christopher Hoskin <christopher.hoskin@gmail.com>
2024-01-17 19:07:46 +0000

    Lint

--- Mathlib/LinearAlgebra/Matrix/SesquilinearForm.lean
+++ Mathlib/LinearAlgebra/Matrix/SesquilinearForm.lean
@@ -10,8 +10,6 @@ import Mathlib.LinearAlgebra.Matrix.NonsingularInverse
 import Mathlib.LinearAlgebra.Matrix.ToLinearEquiv
 import Mathlib.LinearAlgebra.SesquilinearForm
 
---set_option trace.Meta.synthInstance.instances true
-
 #align_import linear_algebra.matrix.sesquilinear_form from "leanprover-community/mathlib"@"84582d2872fb47c0c17eec7382dc097c9ec7137a"
 
 /-!
