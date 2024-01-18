07c91e915404ecff7be5e7c92a8fcf55dc5b411e
grunweg <rothgami@math.hu-berlin.de>
2024-01-18 12:30:26 +0000

    chore(Data/Real/ENNReal): move to Data/ENNReal/Basic (#9823)
    In preparation for splitting that file. Location suggested by @j-loreaux on [zulip](https://leanprover.zulipchat.com/#narrow/stream/287929-mathlib4/topic/Linter.20for.20large.20files.3F/near/416093669).

--- Mathlib.lean
+++ Mathlib.lean
@@ -1507,6 +1507,7 @@ import Mathlib.Data.DFinsupp.WellFounded
 import Mathlib.Data.DList.Basic
 import Mathlib.Data.DList.Defs
 import Mathlib.Data.DList.Instances
+import Mathlib.Data.ENNReal.Basic
 import Mathlib.Data.ENat.Basic
 import Mathlib.Data.ENat.Lattice
 import Mathlib.Data.Equiv.Functor
@@ -1934,7 +1935,6 @@ import Mathlib.Data.Real.Cardinality
 import Mathlib.Data.Real.CauSeq
 import Mathlib.Data.Real.CauSeqCompletion
 import Mathlib.Data.Real.ConjugateExponents
-import Mathlib.Data.Real.ENNReal
 import Mathlib.Data.Real.ENatENNReal
 import Mathlib.Data.Real.EReal
 import Mathlib.Data.Real.GoldenRatio
