c3e4dfc5286e8e4151cc8d75869b8b00418af5ed
Mario Carneiro <di.gama@gmail.com>
2024-01-18 03:18:12 -0500

    fix

--- Mathlib/NumberTheory/ModularForms/JacobiTheta/OneVariable.lean
+++ Mathlib/NumberTheory/ModularForms/JacobiTheta/OneVariable.lean
@@ -3,8 +3,7 @@ Copyright (c) 2023 David Loeffler. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: David Loeffler
 -/
-import Mathlib.Analysis.SpecialFunctions.Gaussian
-import Mathlib.Analysis.Complex.LocallyUniformLimit
+import Mathlib.NumberTheory.ModularForms.JacobiTheta.TwoVariable
 import Mathlib.Analysis.Complex.UpperHalfPlane.Basic
 
 #align_import number_theory.modular_forms.jacobi_theta.basic from "leanprover-community/mathlib"@"57f9349f2fe19d2de7207e99b0341808d977cdcf"
