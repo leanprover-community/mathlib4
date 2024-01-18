2a14ea01062f34008447611f6eab6e3f3fe72df1
Yury G. Kudryashov <urkud@urkud.name>
2024-01-18 00:39:46 +0000

    chore(Order/Notation): move notation classes from other files (#9750)
    With this change (and future similar changes), we can avoid importing heavier files if we only need notation, not lemmas.

--- Mathlib.lean
+++ Mathlib.lean
@@ -2967,6 +2967,7 @@ import Mathlib.Order.Monotone.Extension
 import Mathlib.Order.Monotone.Monovary
 import Mathlib.Order.Monotone.Odd
 import Mathlib.Order.Monotone.Union
+import Mathlib.Order.Notation
 import Mathlib.Order.OmegaCompletePartialOrder
 import Mathlib.Order.OrdContinuous
 import Mathlib.Order.OrderIsoNat
