b71c5f26d2d017eac8f6c3403db93603ce7dd95c
Scott Morrison <scott@tqft.net>
2024-01-19 00:32:12 +0000

    chore: bump Std to leanprover/std4#541 (#9828)
    Notable changes: lemmas were added in https://github.com/leanprover/std4/pull/538 about `gcd` and `lcm`, that now have implicit arguments. Mostly this is a positive change in Mathlib, we can just delete the arguments. The one to consider in review is in [`ModEq`](https://github.com/leanprover-community/mathlib4/pull/9828/files#diff-bf7908935cfca2e7d7959ec20d701fcda99586d0b7620b05b0a803988fdbd4fc).
    
    
    
    Co-authored-by: Scott Morrison <scott.morrison@gmail.com>

--- Mathlib/Tactic/ExtractGoal.lean
+++ Mathlib/Tactic/ExtractGoal.lean
@@ -3,6 +3,8 @@ Copyright (c) 2017 Simon Hudon. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Simon Hudon, Kyle Miller, Damiano Testa
 -/
+import Lean.Elab.Tactic
+import Lean.Meta.Tactic.Cleanup
 import Std.Lean.Meta.Inaccessible
 
 /-!
