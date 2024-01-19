4fd6b8ea7e0586c3d5dc9698cebb305350d25a53
Oliver Nash <github@olivernash.org>
2024-01-19 20:41:48 +0100

    Trivial examples

--- Mathlib/LinearAlgebra/Semisimple.lean
+++ Mathlib/LinearAlgebra/Semisimple.lean
@@ -14,29 +14,33 @@ TODO explain
 
 -/
 
-open Set Function Module Polynomial
+open Set Function Polynomial
 
 variable {R M : Type*} [CommRing R] [AddCommGroup M] [Module R M]
 
-namespace LinearMap -- Or should we use `Module.End`?
+namespace Module.End
 
-variable (f : M →ₗ[R] M)
+variable (f : End R M)
 
 /-- A linear endomorphism of an `R`-module `M` is called semisimple if the induced `R[X]`-module
 structure on `M` is semisimple. This is equivalent to saying that every `f`-invariant `R`-submodule
 of `M` has an `f`-invariant complement: see `LinearMap.isSemisimple_iff`. -/
-protected abbrev IsSemisimple := IsSemisimpleModule R[X] (AEval' f)
+abbrev IsSemisimple := IsSemisimpleModule R[X] (AEval' f)
 
 lemma isSemisimple_iff :
     f.IsSemisimple ↔ ∀ p : Submodule R M, p ≤ p.comap f → ∃ q, q ≤ q.comap f ∧ IsCompl p q := by
   set s := (AEval.comapSubmodule R M f).range
   have h : s = {p : Submodule R M | p ≤ p.comap f} := AEval.range_comapSubmodule R M f
   let e := CompleteLatticeHom.toOrderIsoRangeOfInjective _ (AEval.injective_comapSubmodule R M f)
-  simp_rw [LinearMap.IsSemisimple, IsSemisimpleModule, e.complementedLattice_iff,
+  simp_rw [Module.End.IsSemisimple, IsSemisimpleModule, e.complementedLattice_iff,
     s.isComplemented_iff, ← SetLike.mem_coe, h, mem_setOf_eq]
 
--- TODO Zero map, identity are semisimple (when `IsSemisimpleModule R M`, e.g., over division ring).
+lemma isSemisimple_zero [IsSemisimpleModule R M] : IsSemisimple (0 : Module.End R M) := by
+  simpa [isSemisimple_iff] using exists_isCompl
+
+lemma isSemisimple_id [IsSemisimpleModule R M] : IsSemisimple (LinearMap.id : Module.End R M) := by
+  simpa [isSemisimple_iff] using exists_isCompl
 
 -- TODO generalized eigenspaces are eigenspaces + minpoly characterisation
 
-end LinearMap
+end Module.End
