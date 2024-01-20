72fddad9c450d4d4503d0e8591ca198f27588176
negiizhao <egresf@gmail.com>
2024-01-21 05:05:46 +0800

    golf

--- Mathlib/NumberTheory/Transcendental/Lindemann/AnalyticalPart.lean
+++ Mathlib/NumberTheory/Transcendental/Lindemann/AnalyticalPart.lean
@@ -29,63 +29,61 @@ theorem Differentiable.real_of_complex {e : ℂ → ℂ} (h : Differentiable ℂ
 #align differentiable.real_of_complex Differentiable.real_of_complex
 
 theorem deriv_eq_f (p : ℂ[X]) (s : ℂ) :
-    (deriv fun x : ℝ =>
-        -(exp (-(x • exp (s.arg • I))) * p.sumIderiv.eval (x • exp (s.arg • I))) /
-          exp (s.arg • I)) =
-      fun x : ℝ => exp (-(x • exp (s.arg • I))) * p.eval (x • exp (s.arg • I)) := by
+    (deriv fun x ↦ -(cexp (-(x • s)) * p.sumIderiv.eval (x • s))) =
+      fun x : ℝ ↦ s * (cexp (-(x • s)) * p.eval (x • s)) := by
   have h :
-    (fun y : ℝ => p.sumIderiv.eval (y • exp (s.arg • I))) =
-      (fun x => p.sumIderiv.eval x) ∘ fun y => y • exp (s.arg • I) :=
+    (fun y : ℝ => p.sumIderiv.eval (y • s)) =
+      (fun x => p.sumIderiv.eval x) ∘ fun y => y • s :=
     rfl
   funext x
-  rw [deriv_div_const, deriv.neg, deriv_mul, deriv_cexp, deriv.neg]
+  rw [deriv.neg, deriv_mul, deriv_cexp, deriv.neg]
   any_goals simp_rw [h]; clear h
   rw [deriv_smul_const, deriv_id'', deriv.comp, Polynomial.deriv, deriv_smul_const, deriv_id'']
   simp_rw [one_smul, mul_assoc, ← mul_add]
   have h :
-    exp (s.arg • I) * p.sumIderiv.eval (x • exp (s.arg • I)) -
-        (derivative (R := ℂ) (sumIderiv p)).eval (x • exp (s.arg • I)) * exp (s.arg • I) =
-      p.eval (x • exp (s.arg • I)) * exp (s.arg • I)
+    s * p.sumIderiv.eval (x • s) -
+        (derivative (R := ℂ) (sumIderiv p)).eval (x • s) * s =
+      s * p.eval (x • s)
   · conv_lhs =>
       congr
       rw [sumIderiv_eq_self_add, sumIderiv_derivative]
-    rw [mul_comm, eval_add, add_mul, add_sub_cancel]
-  rw [← mul_neg, neg_add', neg_mul, neg_neg, h, ← mul_assoc, mul_div_cancel]
-  exact exp_ne_zero _
+    rw [mul_comm _ s, eval_add, mul_add, add_sub_cancel]
+  rw [← mul_neg, neg_add', neg_mul, neg_neg, h, mul_left_comm]
   any_goals apply Differentiable.differentiableAt
-  rotate_left 5; apply @Differentiable.real_of_complex fun c : ℂ => exp (-(c * exp (s.arg • I)))
+  rotate_left 5; apply @Differentiable.real_of_complex fun c : ℂ => exp (-(c * s))
   rotate_left; apply Differentiable.comp; apply @Differentiable.restrictScalars ℝ _ ℂ
   any_goals
     repeat'
       first
+      | exact differentiable_id
+      | apply Polynomial.differentiable
+      | apply Differentiable.smul_const
       | apply Differentiable.neg
       | apply Differentiable.cexp
       | apply Differentiable.mul_const
-      | apply Polynomial.differentiable
-      | apply Differentiable.smul_const
-      | exact differentiable_id
 #align deriv_eq_f deriv_eq_f
 
 theorem integral_f_eq (p : ℂ[X]) (s : ℂ) :
-    ∫ x in (0)..(Complex.abs s), exp (-(x • exp (s.arg • I))) * p.eval (x • exp (s.arg • I)) =
-      -(exp (-s) * p.sumIderiv.eval s) / exp (s.arg • I) -
-        -p.sumIderiv.eval 0 / exp (s.arg • I) := by
+    s * ∫ x in (0)..1, exp (-(x • s)) * p.eval (x • s) =
+      -(exp (-s) * p.sumIderiv.eval s) -
+        -p.sumIderiv.eval 0 := by
+  rw [← intervalIntegral.integral_const_mul]
   convert
     intervalIntegral.integral_deriv_eq_sub'
-      (fun x : ℝ =>
-        -(exp (-(x • exp (s.arg • I))) * p.sumIderiv.eval (x • exp (s.arg • I))) / exp (s.arg • I))
+      (fun x : ℝ => -(exp (-(x • s)) * p.sumIderiv.eval (x • s)))
       (deriv_eq_f p s) _ _
-  any_goals simp_rw [real_smul, abs_mul_exp_arg_mul_I]
+  · rw [one_smul]
+  · rw [one_smul]
   · simp_rw [zero_smul, neg_zero, Complex.exp_zero, one_mul]
-  · intro x _; apply ((Differentiable.mul _ _).neg.div_const _).differentiableAt
-    apply @Differentiable.real_of_complex fun c : ℂ => exp (-(c * exp (s.arg • I)))
+  · intro x _; apply (Differentiable.mul _ _).neg.differentiableAt
+    apply @Differentiable.real_of_complex fun c : ℂ => exp (-(c * s))
     refine' (differentiable_id.mul_const _).neg.cexp
-    change Differentiable ℝ ((fun y : ℂ => p.sumIderiv.eval y) ∘ fun x : ℝ => x • exp (s.arg • I))
+    change Differentiable ℝ ((fun y : ℂ => p.sumIderiv.eval y) ∘ fun x : ℝ => x • s)
     apply Differentiable.comp
     apply @Differentiable.restrictScalars ℝ _ ℂ; exact Polynomial.differentiable _
     exact differentiable_id'.smul_const _
-  · refine' ((continuous_id'.smul continuous_const).neg.cexp.mul _).continuousOn
-    change Continuous ((fun y : ℂ => p.eval y) ∘ fun x : ℝ => x • exp (s.arg • I))
+  · refine' (continuous_const.mul ((continuous_id'.smul continuous_const).neg.cexp.mul _)).continuousOn
+    change Continuous ((fun y : ℂ => p.eval y) ∘ fun x : ℝ => x • s)
     exact p.continuous_aeval.comp (continuous_id'.smul continuous_const)
 #align integral_f_eq integral_f_eq
 
@@ -96,37 +94,37 @@ set_option linter.uppercaseLean3 false in
 
 theorem P_le' (p : ℕ → ℂ[X]) (s : ℂ)
     (h :
-      ∃ c, ∀ (q : ℕ), ∀ x ∈ Set.Ioc 0 (Complex.abs s),
-        Complex.abs ((p q).eval (x • exp (s.arg • I))) ≤ c ^ q) :
+      ∃ c, ∀ (q : ℕ), ∀ x ∈ Set.Ioc (0 : ℝ) 1,
+        Complex.abs ((p q).eval (x • s)) ≤ c ^ q) :
     ∃ c ≥ 0, ∀ q : ℕ,
       Complex.abs (P (p q) s) ≤
         Real.exp s.re * (Real.exp (Complex.abs s) * c ^ q * (Complex.abs s)) := by
   simp_rw [P]; cases' h with c hc; replace hc := fun q x hx => (hc q x hx).trans (le_abs_self _)
   simp_rw [_root_.abs_pow] at hc; use |c|, abs_nonneg _; intro q
   have h := integral_f_eq (p q) s
-  rw [← sub_div, eq_div_iff (exp_ne_zero _), ← @mul_right_inj' _ _ (exp s) _ _ (exp_ne_zero _),
+  rw [← @mul_right_inj' _ _ (exp s) _ _ (exp_ne_zero _),
     neg_sub_neg, mul_sub, ← mul_assoc _ (exp _), ← exp_add, add_neg_self, exp_zero, one_mul] at h
   replace h := congr_arg Complex.abs h
-  simp_rw [map_mul, abs_exp, smul_re, I_re, smul_zero, Real.exp_zero, mul_one] at h
-  rw [← h, mul_le_mul_left (Real.exp_pos _), ← Complex.norm_eq_abs,
-    intervalIntegral.integral_of_le (Complex.abs.nonneg _)]
+  simp_rw [map_mul, abs_exp] at h
+  rw [← h, mul_le_mul_left (Real.exp_pos _), mul_comm]
+  apply mul_le_mul_of_nonneg_right _ (Complex.abs.nonneg _)
+  rw [intervalIntegral.integral_of_le zero_le_one, ← Complex.norm_eq_abs, ← mul_one (_ * _)]
   clear h
   convert MeasureTheory.norm_set_integral_le_of_norm_le_const' _ _ _
-  · rw [Real.volume_Ioc, sub_zero, ENNReal.toReal_ofReal (Complex.abs.nonneg _)]
+  · rw [Real.volume_Ioc, sub_zero, ENNReal.toReal_ofReal zero_le_one]
   · rw [Real.volume_Ioc, sub_zero]; exact ENNReal.ofReal_lt_top
   · exact measurableSet_Ioc
   intro x hx; rw [norm_mul]; refine' mul_le_mul _ (hc q x hx) (norm_nonneg _) (Real.exp_pos _).le
   rw [norm_eq_abs, abs_exp, Real.exp_le_exp]; apply (re_le_abs _).trans;
-  rw [← norm_eq_abs, norm_neg, norm_smul, norm_eq_abs, abs_exp, smul_re, I_re, smul_zero,
-    Real.exp_zero, mul_one, Real.norm_eq_abs, abs_eq_self.mpr hx.1.le]
-  exact hx.2
+  rw [← norm_eq_abs, norm_neg, norm_smul, norm_eq_abs, Real.norm_of_nonneg hx.1.le]
+  exact mul_le_of_le_one_left (Complex.abs.nonneg _) hx.2
 set_option linter.uppercaseLean3 false in
 #align P_le' P_le'
 
 theorem P_le (p : ℕ → ℂ[X]) (s : ℂ)
     (h :
-      ∃ c, ∀ (q : ℕ), ∀ x ∈ Set.Ioc 0 (Complex.abs s),
-        Complex.abs ((p q).eval (x • exp (s.arg • I))) ≤ c ^ q) :
+      ∃ c, ∀ (q : ℕ), ∀ x ∈ Set.Ioc (0 : ℝ) 1,
+        Complex.abs ((p q).eval (x • s)) ≤ c ^ q) :
     ∃ c ≥ 0, ∀ q ≥ 1, Complex.abs (P (p q) s) ≤ c ^ q := by
   simp_rw []; obtain ⟨c', hc', h'⟩ := P_le' p s h; clear h
   let c₁ := max (Real.exp s.re) 1
@@ -159,37 +157,33 @@ theorem exp_polynomial_approx (p : ℤ[X]) (p0 : p.eval 0 ≠ 0) :
           ∀ {r : ℂ}, r ∈ p.aroots ℂ →
             Complex.abs (n • exp r - q • aeval r gp : ℂ) ≤ c ^ q / (q - 1)! := by
   let p' q := (X ^ (q - 1) * p ^ q).map (algebraMap ℤ ℂ)
-  have : ∀ s : ℂ, ∃ c, ∀ (q : ℕ), ∀ x ∈ Set.Ioc 0 (Complex.abs s),
-      Complex.abs ((p' q).eval (x • exp (s.arg • I))) ≤ c ^ q
+  have : ∀ s : ℂ, ∃ c, ∀ (q : ℕ), ∀ x ∈ Set.Ioc (0 : ℝ) 1,
+      Complex.abs ((p' q).eval (x • s)) ≤ c ^ q
   · intro s; dsimp only
     simp_rw [Polynomial.map_mul, Polynomial.map_pow, map_X, eval_mul, eval_pow, eval_X, map_mul,
-      Complex.abs_pow, real_smul, map_mul, abs_exp_ofReal_mul_I, abs_ofReal, mul_one, ←
-      eval₂_eq_eval_map, ← aeval_def]
+      Complex.abs_pow, real_smul, map_mul, abs_ofReal, ← eval₂_eq_eval_map, ← aeval_def]
     have : Bornology.IsBounded
-        ((fun x : ℝ => max |x| 1 * Complex.abs (aeval (x * exp (s.arg * I)) p)) ''
-          Set.Ioc 0 (abs s))
+        ((fun x : ℝ => max (x * abs s) 1 * Complex.abs (aeval (x * s) p)) '' Set.Ioc 0 1)
     · have h :
-        (fun x : ℝ => max |x| 1 * Complex.abs (aeval (↑x * exp (↑s.arg * I)) p)) ''
-            Set.Ioc 0 (abs s) ⊆
-          (fun x : ℝ => max |x| 1 * Complex.abs (aeval (↑x * exp (↑s.arg * I)) p)) ''
-              Set.Icc 0 (abs s) :=
+        (fun x : ℝ => max (x * abs s) 1 * Complex.abs (aeval (↑x * s) p)) '' Set.Ioc 0 1 ⊆
+          (fun x : ℝ => max (x * abs s) 1 * Complex.abs (aeval (↑x * s) p)) '' Set.Icc 0 1 :=
         Set.image_subset _ Set.Ioc_subset_Icc_self
       refine' (IsCompact.image isCompact_Icc _).isBounded.subset h
-      · refine' (continuous_id.abs.max continuous_const).mul _
+      · refine' ((continuous_id.mul continuous_const).max continuous_const).mul _
         refine' Complex.continuous_abs.comp (p.continuous_aeval.comp _)
         exact continuous_ofReal.mul continuous_const
     cases' this.exists_norm_le with c h
     use c; intro q x hx
-    specialize h (max |x| 1 * Complex.abs (aeval (↑x * exp (↑s.arg * I)) p))
-      (Set.mem_image_of_mem _ hx)
+    specialize h (max (x * abs s) 1 * Complex.abs (aeval (↑x * s) p)) (Set.mem_image_of_mem _ hx)
     refine' le_trans _ (pow_le_pow_left (norm_nonneg _) h _)
-    simp_rw [norm_mul, Real.norm_eq_abs, Complex.abs_abs, mul_pow]
+    simp_rw [norm_mul, Real.norm_eq_abs, Complex.abs_abs, mul_pow, abs_of_pos hx.1]
     refine' mul_le_mul_of_nonneg_right _ (pow_nonneg (Complex.abs.nonneg _) _)
     rw [max_def]; split_ifs with hx1
-    · rw [_root_.abs_one, one_pow]
-      exact pow_le_one _ (abs_nonneg _) hx1
+    · rw [_root_.abs_one, one_pow, ← mul_pow]
+      exact pow_le_one _ (mul_nonneg hx.1.le (Complex.abs.nonneg _)) hx1
     · push_neg at hx1
-      rw [_root_.abs_abs]; exact pow_le_pow_right hx1.le (Nat.sub_le _ _)
+      rw [_root_.abs_mul, Complex.abs_abs, ← mul_pow, abs_of_pos hx.1]
+      exact pow_le_pow_right hx1.le (Nat.sub_le _ _)
   choose c' c'0 Pp'_le using fun r ↦ P_le p' r (this r)
   let c :=
     if h : ((p.aroots ℂ).map c').toFinset.Nonempty then ((p.aroots ℂ).map c').toFinset.max' h else 0
