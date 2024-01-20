58c965ee3c915c4e35c5ca144a4b12dc0310ccd4
RemyDegenne <Remydegenne@gmail.com>
2024-01-20 11:26:48 +0100

    fix

--- Mathlib/MeasureTheory/Measure/AddContent.lean
+++ Mathlib/MeasureTheory/Measure/AddContent.lean
@@ -60,6 +60,20 @@ open Set Finset Filter
 
 open scoped ENNReal BigOperators Topology
 
+-- TODO: move this
+lemma Finset.sum_image_le_of_nonneg {ι α β : Type*} [DecidableEq α]
+    [OrderedAddCommMonoid β] [SMulPosMono ℕ β]
+    {J : Finset ι} {g : ι → α} {f : α → β} (hf : ∀ u ∈ J.image g, 0 ≤ f u) :
+    ∑ u in J.image g, f u ≤ ∑ u in J, f (g u) := by
+  rw [sum_comp f g]
+  refine sum_le_sum fun a hag => ?_
+  obtain ⟨i, hi, hig⟩ := Finset.mem_image.mp hag
+  conv_lhs => rw [← one_smul ℕ (f a)]
+  refine smul_le_smul_of_nonneg_right ?_ (hf a hag)
+  rw [Nat.one_le_iff_ne_zero, ← Nat.pos_iff_ne_zero, card_pos]
+  exact ⟨i, mem_filter.mpr ⟨hi, hig⟩⟩
+
+-- TODO: move this
 instance {α : Type*} [AddMonoid α] [Preorder α]
     [CovariantClass α α (Function.swap (· + ·)) (· ≤ ·)] :
     SMulPosMono ℕ α := by
@@ -78,6 +92,7 @@ instance {α : Type*} [AddMonoid α] [Preorder α]
       conv_lhs => rw [← zero_add (m • b)]
       exact add_le_add_right hb _
 
+-- TODO: move this
 instance {α : Type*} [OrderedAddCommMonoid α] : PosSMulMono ℕ α := by
   constructor
   intro n hn a b hab
