b5bbddb08ea9ec3d09abd6f6440684ecd5225ab8
negiizhao <egresf@gmail.com>
2024-01-21 20:06:04 +0800

    golf

--- Mathlib/NumberTheory/Transcendental/Lindemann/Init/ExistsLargePrime.lean
+++ Mathlib/NumberTheory/Transcendental/Lindemann/Init/ExistsLargePrime.lean
@@ -12,63 +12,45 @@ import Mathlib.Data.Real.Archimedean
 
 open scoped Nat
 
-theorem linear_independent_exp_exists_prime_nat'' (c : ℕ) : ∃ n > c, c ^ n < (n - 1)! := by
-  refine' ⟨2 * (c ^ 2 + 1), _, _⟩; · have : c ≤ c * c := Nat.le_mul_self _; linarith
-  rw [pow_mul, two_mul, add_right_comm, add_tsub_cancel_right]
-  refine' lt_of_lt_of_le _ Nat.factorial_mul_pow_le_factorial
+theorem linear_independent_exp_exists_prime_nat' (c : ℕ) : ∃ n0, ∀ n ≥ n0, c ^ n < (n - 1)! := by
+  obtain (rfl | c0) := c.eq_zero_or_pos
+  · use 1
+    intro n hn
+    rw [zero_pow hn]
+    exact Nat.factorial_pos _
+  refine' ⟨2 * (c ^ 2 + 1), _⟩
+  intro n hn
+  obtain ⟨d, rfl⟩ := Nat.exists_eq_add_of_le hn
+  refine (Nat.le_mul_of_pos_right _ (pow_pos c0 d)).trans_lt ?_
+  convert_to (c ^ 2) ^ (c ^ 2 + d + 1) < (c ^ 2 + (c ^ 2 + d + 1))!
+  · ring
+  · congr; omega
+  refine lt_of_lt_of_le ?_ Nat.factorial_mul_pow_le_factorial
   rw [← one_mul (_ ^ _ : ℕ)]
   exact Nat.mul_lt_mul_of_le_of_lt (Nat.one_le_of_lt (Nat.factorial_pos _))
     (Nat.pow_lt_pow_left (Nat.lt_succ_self _) (Nat.succ_ne_zero _)) (Nat.factorial_pos _)
-#align linear_independent_exp_exists_prime_nat'' linear_independent_exp_exists_prime_nat''
 
-theorem linear_independent_exp_exists_prime_nat' (n : ℕ) (c : ℕ) :
-    ∃ p > n, p.Prime ∧ c ^ p < (p - 1)! := by
-  obtain ⟨m, hm, h⟩ := linear_independent_exp_exists_prime_nat'' c
-  let N := max (n + 2) (m + 1)
-  obtain ⟨p, hp', prime_p⟩ := Nat.exists_infinite_primes N
-  have hnp : n + 1 < p := (Nat.add_one_le_iff.mp (le_max_left _ _)).trans_le hp'
-  have hnp' : n < p := lt_of_add_lt_of_nonneg_left hnp zero_le_one
-  have hmp : m < p := (Nat.add_one_le_iff.mp (le_max_right _ _)).trans_le hp'
-  use p, hnp', prime_p
-  cases' lt_or_ge m 2 with m2 m2
-  · have : c = 0 := by linarith
-    rw [this, zero_pow prime_p.pos]
-    exact Nat.factorial_pos _
-  rcases Nat.eq_zero_or_pos c with (rfl | c0)
-  · rw [zero_pow prime_p.pos]
-    exact Nat.factorial_pos _
-  have m1 : 1 ≤ m := one_le_two.trans m2
-  have one_le_m_sub_one : 1 ≤ m - 1 := by rwa [Nat.le_sub_iff_add_le m1]
-  have : m - 1 - 1 < p - 1
-  · rw [tsub_lt_tsub_iff_right one_le_m_sub_one]
-    exact tsub_le_self.trans_lt hmp
-  refine' lt_of_lt_of_le _ (Nat.factorial_mul_pow_sub_le_factorial this)
-  have : (m - 1 - 1).succ = m - 1 := by rwa [Nat.succ_eq_add_one, tsub_add_cancel_of_le]
-  rw [this]
-  convert_to c ^ m * c ^ (p - m) < _
-  · rw [← pow_add, add_tsub_cancel_of_le]; exact hmp.le
-  rw [tsub_tsub_tsub_cancel_right m1]
-  exact Nat.mul_lt_mul_of_lt_of_le' h (pow_le_pow_left' (Nat.le_pred_of_lt hm) _) (pow_pos c0 _)
-#align linear_independent_exp_exists_prime_nat' linear_independent_exp_exists_prime_nat'
-
-theorem linear_independent_exp_exists_prime_nat (n : ℕ) (a : ℕ) (c : ℕ) :
-    ∃ p > n, p.Prime ∧ a * c ^ p < (p - 1)! := by
-  obtain ⟨p, hp, prime_p, h⟩ := linear_independent_exp_exists_prime_nat' n (a * c)
-  use p, hp, prime_p
+theorem linear_independent_exp_exists_prime_nat (a : ℕ) (c : ℕ) :
+    ∃ n0, ∀ n ≥ n0, a * c ^ n < (n - 1)! := by
+  obtain ⟨n0, h⟩ := linear_independent_exp_exists_prime_nat' (a * c)
+  use max n0 1
+  intro n hn
+  have n0_le_n := (le_max_left _ _).trans hn
+  have one_le_n := (le_max_right _ _).trans hn
+  specialize h n n0_le_n
   refine' lt_of_le_of_lt _ h
-  rcases Nat.eq_zero_or_pos a with (rfl | a0)
-  · simp_rw [MulZeroClass.zero_mul, zero_pow' _ prime_p.ne_zero, le_rfl]
+  obtain (rfl | a0) := a.eq_zero_or_pos
+  · simp only [zero_mul, zero_le]
   rw [mul_pow]
   apply Nat.mul_le_mul_right
-  convert_to a ^ 1 ≤ a ^ p; · rw [pow_one]
-  exact Nat.pow_le_pow_of_le_right a0 (Nat.one_le_of_lt prime_p.pos)
-#align linear_independent_exp_exists_prime_nat linear_independent_exp_exists_prime_nat
+  apply le_self_pow <;> omega
 
 theorem linear_independent_exp_exists_prime (n : ℕ) (a : ℝ) (c : ℝ) :
     ∃ p > n, p.Prime ∧ a * c ^ p / (p - 1)! < 1 := by
   simp_rw [@div_lt_one ℝ _ _ _ (Nat.cast_pos.mpr (Nat.factorial_pos _))]
-  obtain ⟨p, hp, prime_p, h⟩ := linear_independent_exp_exists_prime_nat n ⌈|a|⌉.natAbs ⌈|c|⌉.natAbs
-  use p, hp, prime_p
+  obtain ⟨n0, h⟩ := linear_independent_exp_exists_prime_nat ⌈|a|⌉.natAbs ⌈|c|⌉.natAbs
+  obtain ⟨p, hp, prime_p⟩ := (max (n + 1) n0).exists_infinite_primes
+  use p, (le_max_left _ _).trans hp, prime_p
   have : a * c ^ p ≤ ⌈|a|⌉ * (⌈|c|⌉ : ℝ) ^ p
   · refine' (le_abs_self _).trans _
     rw [_root_.abs_mul, _root_.abs_pow]
@@ -79,5 +61,6 @@ theorem linear_independent_exp_exists_prime (n : ℕ) (a : ℝ) (c : ℝ) :
   refine' lt_of_eq_of_lt (_ : _ = ((⌈|a|⌉.natAbs * ⌈|c|⌉.natAbs ^ p : ℕ) : ℝ)) _
   · simp_rw [Nat.cast_mul, Nat.cast_pow, Int.cast_natAbs,
       abs_eq_self.mpr (Int.ceil_nonneg (_root_.abs_nonneg (_ : ℝ)))]
-  rwa [Nat.cast_lt]
+  rw [Nat.cast_lt]
+  exact h _ ((le_max_right _ _).trans hp)
 #align linear_independent_exp_exists_prime linear_independent_exp_exists_prime
