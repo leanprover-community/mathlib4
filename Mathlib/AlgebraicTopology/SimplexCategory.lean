12179d574c2cfc183da75773e22d5ad6718a9769
jstoobysmith <72603918+jstoobysmith@users.noreply.github.com>
2024-01-22 15:49:08 -0500

    feat(AlgebraicTopology/SimplicialSet): Generalized horn constructor

--- Mathlib/AlgebraicTopology/SimplexCategory.lean
+++ Mathlib/AlgebraicTopology/SimplexCategory.lean
@@ -456,11 +456,11 @@ lemma factor_δ_comp_lt {n m : ℕ } (f : ([m] : SimplexCategory) ⟶ [n+2])
         · rw [Fin.eq_iff_veq,Fin.coe_castSucc]
           unfold Fin.predAbove
           split <;> split
-          any_goals simp
+          any_goals simp only [Fin.coe_castPred, Fin.coe_pred]
           all_goals
             rename_i h1 h2
-            rw [Fin.lt_def] at h1 h2
-            simp at h1 h2
+            simp only [Fin.castSucc_zero, Fin.lt_def, Fin.val_zero, Fin.coe_castPred, not_lt,
+              nonpos_iff_eq_zero] at h1 h2
             simp only [h1,h2, ge_iff_le, zero_le, tsub_eq_zero_of_le]
         · rw [Fin.le_def,Fin.predAbove_above 0 l' hl',Fin.coe_pred]
           unfold Fin.predAbove
@@ -477,6 +477,56 @@ lemma factor_δ_comp_lt {n m : ℕ } (f : ([m] : SimplexCategory) ⟶ [n+2])
             simp only [Fin.coe_castPred,h1]
             exact Nat.zero_le (↑i2 - 1 - 1)
 
+lemma factor_δ_comp_spec_lt {n m : ℕ } {f : ([m] : SimplexCategory) ⟶ [n+2]}
+    {i1 i2 : Fin (n+3)} (i1_lt_i2 : i1<i2)
+    (exclude_i1 :  ∀ k, f.toOrderHom k ≠  i1)
+    (exclude_i2 :  ∀ k, f.toOrderHom k ≠  i2):
+    (factor_δ (factor_δ f i1)
+    (Fin.predAbove 0 i2)) ≫ δ (Fin.predAbove 0 i2)
+    = factor_δ f i1 := by
+  apply factor_δ_spec
+  intro x
+  by_contra hXp
+  apply (congrArg ((δ i1).toOrderHom )) at hXp
+  change  ((factor_δ f i1)≫δ _ ).toOrderHom x = _ at hXp
+  rw [(factor_δ_spec f i1 exclude_i1)] at hXp
+  have hi2: 0<i2 := by
+     contrapose! i1_lt_i2
+     simp_all
+  have hi22:  (Hom.toOrderHom (δ i1)) (Fin.predAbove 0 i2)= i2 := by
+    change Fin.succAbove i1 _=_
+    rw [Fin.predAbove_above 0 i2 hi2,Fin.succAbove_above i1 _]
+    exact Fin.succ_pred i2 (LT.lt.ne' (LE.le.trans_lt (Fin.zero_le (Fin.castSucc 0)) hi2))
+    exact (Nat.lt_iff_le_pred hi2).mp i1_lt_i2
+  rw [hi22] at hXp
+  exact exclude_i2 x hXp
+
+lemma factor_δ_comp_spec_lt' {n m : ℕ } {f : ([m] : SimplexCategory) ⟶ [n+2]}
+    {i1 i2 : Fin (n+3)} (i1_lt_i2 : i1<i2)
+    (exclude_i1 :  ∀ k, f.toOrderHom k ≠  i1)
+    (exclude_i2 :  ∀ k, f.toOrderHom k ≠  i2):
+    (factor_δ (factor_δ f i2)
+    (Fin.predAbove (Fin.last (n+1)) i1))≫ δ (Fin.predAbove (Fin.last (n+1)) i1)
+    = (factor_δ f i2) := by
+  apply factor_δ_spec
+  intro x
+  by_contra hXp
+  apply (congrArg ((δ i2).toOrderHom )) at hXp
+  change  ((factor_δ f i2)≫δ _ ).toOrderHom x = _ at hXp
+  rw [(factor_δ_spec f i2 exclude_i2)] at hXp
+  have hi1: i1≤ Fin.castSucc (Fin.last (n+1)) := by
+      let h2 := (Fin.le_last i2)
+      rw [Fin.le_def] at h2
+      rw [Fin.lt_def] at i1_lt_i2
+      rw [Fin.le_def,Fin.coe_castSucc, Fin.val_last]
+      linarith
+  have hi12: (Hom.toOrderHom (δ i2)) (Fin.predAbove (Fin.last (n+1)) i1)= i1 := by
+    change Fin.succAbove i2 _=_
+    rw [Fin.predAbove_below (Fin.last (n+1)) i1 hi1,Fin.succAbove_below i2 ]
+    rfl
+    exact i1_lt_i2
+  rw [hi12] at hXp
+  exact exclude_i1 x hXp
 
 end Generators
 
--- Mathlib/AlgebraicTopology/SimplicialSet.lean
+++ Mathlib/AlgebraicTopology/SimplicialSet.lean
@@ -326,18 +326,6 @@ lemma hom_ext {n : ℕ} {i : Fin (n+2)} {S : SSet} (σ₁ σ₂ : Λ[n+1, i] ⟶
   dsimp at H₁ H₂
   erw [H, H₁, H₂, h _ hji]
 
-/-- The `(δ i).toOrderHom j`th subface of the `i`-th horn, such that `j:Fin (n+1)` sequentially
-labels the subfaces which exist. -/
-def face' {n : ℕ} (i  : Fin (n+2)) (j: Fin (n+1)) : (Λ[n+1, i]: SSet) _[n] :=by
-    refine face i ((δ i).toOrderHom j) ?_
-    unfold δ
-    simp
-    unfold Fin.succAbove
-    by_contra h
-    split at h
-    all_goals
-      subst h
-      simp_all only [lt_self_iff_false,Fin.castSucc_lt_succ_iff, le_refl, not_true_eq_false]
 
 /-- Every `face` can be written as a `face'`.-/
 lemma face_eq_face' {n : ℕ} (i  : Fin (n+2)) (j: Fin (n+2)) (h: j≠i): face i j h
@@ -426,186 +414,281 @@ lemma factorization_of_φ_comp_α_i1:
     (Fin.predAbove 0 ((δ i).toOrderHom i2))) ≫ δ (Fin.predAbove 0 ((δ i).toOrderHom i2))
     = factor_δ (φ ≫ α.val.down) ((δ i).toOrderHom i1) := by
   fin_cases i1, i2
-  any_goals
-    rw [Fin.lt_def] at i1_lt_i2
-    simp at i1_lt_i2
-  case' head.tail.head           => let i1':Fin 3 := 0
-  case' head.tail.tail.head      => let i1':Fin 3 := 0
-  case' tail.head.tail.tail.head => let i1':Fin 3 := 1
-  all_goals
-    let hα :=  (α.prop)∘Set.eq_univ_iff_forall.mpr
+    · exact hl
+    · split
+      · exact Nat.lt.base l
+      · rename_i h1 h2
+        by_cases hn : (l+1) < n+2
+        · let ht:= lt_minAsNat_of_succ (l+1) hn
+          exact Nat.lt_of_succ_lt ht
+        · have hlp1 : (l+1)+1 > n+2  := by
+            exact Nat.not_le.mp hn
+          unfold minAsNat
+          linarith
+termination_by _  => (n+2) - l
+decreasing_by
+    simp_wf
+    rename_i h1 h2
+    push_neg at h1 h2
+    apply tsub_lt_tsub_right_of_le
+    exact Nat.lt_succ.mp hl
+    exact Nat.lt.base (n + 1)
+
+lemma not_in_range_if_eq_self (l: ℕ)  (hl:  l<n+2) (heq: minAsNat α l=l) :
+    ∀ k, α.1.down.toOrderHom k ≠
+    (δ i).toOrderHom l:=by
+      unfold minAsNat at heq
+      have h1 : ¬ (l >n+1) := by
+        linarith
+      simp [h1] at heq
+      let h3:= lt_minAsNat_of_succ α l hl
+      have h2: minAsNat α (l+1) ≠ l := by
+        exact Nat.ne_of_gt h3
+      intro k
+      simp_all only [gt_iff_lt, not_lt, len_mk, imp_false, ne_eq, not_false_eq_true]
+
+lemma in_range_if_neq_self (l: ℕ)  (hl:  l<n+2) (heq: minAsNat α l≠ l) :
+    ¬ ∀ k, α.1.down.toOrderHom k ≠
+    (δ i).toOrderHom l:=by
+      unfold minAsNat at heq
+      have h1 : ¬ (l >n+1) := by
+         linarith
+      simp [h1] at heq
+      let h3:= lt_minAsNat_of_succ α l hl
+      have h2: minAsNat α (l+1) ≠ l := by
+        exact Nat.ne_of_gt h3
+      tauto
+
+lemma in_range_if_lt_minAsNat_zero (l:ℕ)  (hl: l< (minAsNat α 0)) : (¬ ∀ k, α.1.down.toOrderHom k ≠
+    (δ i).toOrderHom l) ∧ (minAsNat α 0= minAsNat α l):=by
+     induction' l with k hk
+     · have h1 : 0 < n+2 := by
+          exact Nat.succ_pos (n+1)
+       have h2 : minAsNat α 0≠ 0:= by
+          exact Nat.pos_iff_ne_zero.mp hl
+       apply And.intro
+       exact in_range_if_neq_self α Nat.zero h1 h2
+       rfl
+     · have hkl : k < minAsNat α 0:= by
+          exact Nat.lt_of_succ_lt hl
+       have k_lt_np1 :¬ (k> n+1)  := by
+             let hr:= minAsNat_lt α 0
+             simp
+             linarith
+       apply hk at hkl
+       rw [hkl.right] at hl
+       apply And.intro
+       · unfold minAsNat at hl
+         simp [k_lt_np1] at hl
+         split at hl
+         · tauto
+         · have ht: minAsNat α (Nat.succ k) ≠ Nat.succ k  := by
+              exact Nat.ne_of_gt hl
+           apply in_range_if_neq_self
+           · let hr:= minAsNat_lt α (k+1)
+             linarith
+           · exact ht
+       · rw [hkl.right]
+         have hx: ¬ ∀ (k_1 : Fin (len X.unop + 1)), ¬(Hom.toOrderHom α.1.down) k_1
+             = (Hom.toOrderHom (δ i)) k := by
+            exact hkl.left
+         have hf:  minAsNat α k
+             = (if k > n + 1 then n + 2  else if ∀ l, α.1.down.toOrderHom l ≠ (δ i).toOrderHom k
+         then k
+         else minAsNat α (k+1) ):= by
+            rw [minAsNat]
+         rw [hf]
+         simp [k_lt_np1]
+         exact fun a => (hx a).elim
+
+lemma minAsNat_zero_neq : minAsNat α 0 ≠ n+2 := by
+    by_contra h
+    have h1: ∀ (l : Fin (n+2)) , (¬ ∀ k , α.1.down.toOrderHom k ≠ (δ i).toOrderHom l) := by
+        intro l
+        have hj: @Nat.cast (Fin (len [n + 1] + 1)) AddMonoidWithOne.toNatCast l.val  = l := by
+          exact Fin.cast_val_eq_self l
+        rw [← hj]
+        have htt : l.val < minAsNat α 0 := by
+            simp_all only [len_mk, Fin.is_lt]
+        let ht:= in_range_if_lt_minAsNat_zero α  l htt
+        exact ht.left
+    have hα:= α.prop∘Set.eq_univ_iff_forall.mpr
     simp only [ne_eq, Set.union_singleton, Set.mem_insert_iff, Set.mem_range, imp_false,
-    not_forall, not_or, not_exists] at hα
-    obtain ⟨x1,hx1⟩ :=  hα
-    apply factor_δ_spec
-    intro x
-    by_contra hXp
-    apply (congrArg ((δ ((δ i).toOrderHom i1')).toOrderHom )) at hXp
-    change  ((factor_δ (φ ≫ α.val.down) ((δ i).toOrderHom i1'))≫δ _ ).toOrderHom x = _ at hXp
-    rw [(factor_δ_spec (φ ≫ α.val.down) ((δ i).toOrderHom i1') exclude_i1 )] at hXp
-    fin_cases x1
-    all_goals fin_cases i
-    all_goals tauto
+            not_forall, not_or, not_exists] at hα
+    obtain ⟨x, hx⟩ := hα
+    let lx:= (Fin.predAbove (Fin.predAbove 0 i)) x
+    have ht :   (Hom.toOrderHom (δ i)) lx =x  := by
+        change Fin.succAbove i ((Fin.predAbove (Fin.predAbove 0 i)) x) =_
+        by_cases hi: i≠ 0
+        · rw [Fin.predAbove_zero hi]
+          rw [Fin.eq_iff_veq]
+          unfold Fin.succAbove
+          unfold Fin.predAbove
+          split <;> split
+          · rename_i  h2 h3
+            rw [Fin.lt_def] at h2
+            simp at h2 h3
+            let hx2:= hx.left
+            rw [Fin.eq_iff_veq] at hx2
+            exact (hx2 (Nat.le_antisymm (Nat.le_of_pred_lt h3) (Nat.le_of_pred_lt h2))).elim
+          · simp
+          · simp
+          · rename_i  h2 h3
+            rw [Fin.lt_def] at h2
+            simp at h2 h3
+            rw [Fin.le_def] at h3
+            have h4: i.val ≤ i.val -1 := by
+                exact Nat.le_trans h3 h2
+            change  ¬(i=0)  at hi
+            rw [Fin.eq_iff_veq] at hi
+            have ht : 0< i.val :=  Nat.pos_of_ne_zero hi
+            change i.val ≤ Nat.pred i.val at h4
+            rw [Nat.le_pred_iff_lt ht] at h4
+            simp at h4
+        ·
+          simp at hi
+          rw [hi]
+          rw [Fin.eq_iff_veq]
+          simp
+          have hpr: Fin.predAbove (0: Fin (n+2)) 0=0 := by
+              rfl
+          rw [hpr]
+          unfold Fin.predAbove
+          split
+          · rename_i h2
+            rw [Fin.lt_def] at h2
+            simp at h2
+            simp
+            exact Nat.sub_add_cancel h2
+          ·  rename_i h2
+             simp at h2
+             rw [h2] at hx
+             rw [← hi] at hx
+             simp at hx
+    rw [← ht] at hx
+    let hxr:= hx.right
+    exact h1 lx hxr
+
+lemma minAsNat_zero_lt  : minAsNat α 0 < n+2 :=
+    Nat.lt_of_le_of_ne (Nat.lt_succ.mp (  minAsNat_lt α 0) ) (minAsNat_zero_neq α)
+
+def min : Fin (n+2) := ⟨minAsNat α 0,  minAsNat_zero_lt α⟩
+
+lemma minAsNat_eq_minAsNat_of_self (l:ℕ)  (hl: l= (minAsNat α 0)) :(minAsNat α 0= minAsNat α l):=by
+    by_cases hl2: l=0
+    · rw [hl2]
+    · let lm1:= Nat.pred l
+      have hl1: lm1< l:= Nat.pred_lt hl2
+      rw [hl] at hl1
+      let h3:= (in_range_if_lt_minAsNat_zero α lm1 hl1).right
+      let h4:= (in_range_if_lt_minAsNat_zero α lm1 hl1).left
+      rw [h3]
+      have hf:  minAsNat α lm1 =
+          (if lm1 > n + 1 then n + 2  else if ∀ l, α.1.down.toOrderHom l ≠ (δ i).toOrderHom lm1
+         then lm1
+         else minAsNat α (lm1+1) ):= by
+            rw [minAsNat]
+      rw [hf]
+      have hlm1N: ¬ (lm1>  n+1):=
+         Nat.not_lt.mpr (Nat.lt_succ.mp (Nat.lt_trans hl1 (minAsNat_zero_lt α)))
+
+      simp only [gt_iff_lt, hlm1N, len_mk, ne_eq, ite_false]
+      rw [if_neg]
+      · rw [show Nat.pred l+1 =l from Nat.succ_pred hl2  ]
+      · exact h4
+
+lemma gt_min_if_not_in_range (j: Fin (n+2)) : ( ∀ k, α.1.down.toOrderHom k ≠
+    (δ i).toOrderHom j )→ ((min α)≤ j):= by
+      intro h
+      rw [Fin.le_def]
+      change minAsNat α 0 ≤ j.val
+      by_contra hn
+      simp at hn
+      let hn2:=(in_range_if_lt_minAsNat_zero α j.val hn).left
+      have hj:  @Nat.cast (Fin (len [n + 1] + 1)) AddMonoidWithOne.toNatCast j.val  = j := by
+          exact Fin.cast_val_eq_self j
+      rw [hj] at hn2
+      exact hn2 h
+
+lemma min_not_in_range: ∀ k, α.1.down.toOrderHom k ≠
+    (δ i).toOrderHom (min α):=by
+      let l :=  (minAsNat α 0)
+      have h1: (minAsNat α 0) = (minAsNat α l) := by
+       apply  (minAsNat_eq_minAsNat_of_self α l)
+       rfl
+      have hj:  @Nat.cast (Fin (len [n + 1] + 1)) AddMonoidWithOne.toNatCast l  = (min α) := by
+          exact Fin.cast_val_eq_self (min α)
+      rw [← hj]
+      apply not_in_range_if_eq_self
+      · exact minAsNat_zero_lt α
+      · exact  id h1.symm
 
 
-lemma factorization_of_φ_comp_α_i2:
-    (factor_δ (factor_δ (φ ≫ α.val.down) ((δ i).toOrderHom i2))
-    (Fin.predAbove 2 ((δ i).toOrderHom i1)))≫ δ (Fin.predAbove 2 ((δ i).toOrderHom i1))
-    = (factor_δ (φ ≫ α.val.down) ((δ i).toOrderHom i2)) := by
-  fin_cases i1, i2
-  any_goals
-      rw [Fin.lt_def] at i1_lt_i2
-      simp at i1_lt_i2
-  case' head.tail.head           => let i2':Fin 3 := 1
-  case' head.tail.tail.head      => let i2':Fin 3 := 2
-  case' tail.head.tail.tail.head => let i2':Fin 3 := 2
-  all_goals
-      let hα :=  (α.prop)∘Set.eq_univ_iff_forall.mpr
-      simp only [ne_eq, Set.union_singleton, Set.mem_insert_iff, Set.mem_range, imp_false,
-      not_forall, not_or, not_exists] at hα
-      obtain ⟨x1,hx1⟩ :=  hα
-      apply factor_δ_spec
-      intro x
-      by_contra hXp
-      apply (congrArg ((δ ((δ i).toOrderHom i2')).toOrderHom )) at hXp
-      change  ((factor_δ (φ ≫ α.val.down) ((δ i).toOrderHom i2'))≫δ _).toOrderHom x = _ at hXp
-      rw [factor_δ_spec (φ ≫ α.val.down) ((δ i).toOrderHom i2')  exclude_i2] at hXp
-      fin_cases x1
-      all_goals fin_cases i
-      all_goals tauto
+
+variable {Y : SimplexCategoryᵒᵖ } (φ':X⟶ Y)
+lemma min_not_in_range_φ : ∀ k, (φ'.unop ≫ α.1.down).toOrderHom k ≠
+    (δ i).toOrderHom (min α):= by
+        exact fun k ↦ min_not_in_range α ((Hom.toOrderHom φ'.unop) k)
+lemma min_comp_le_min:
+    min (Λ[n+2,i].map φ' α) ≤  min α:= by
+       exact gt_min_if_not_in_range (Λ[n+2, i].map φ' α) (min α)
+          (fun k ↦ min_not_in_range α ((Hom.toOrderHom φ'.unop) k) )
+end FactorMinFace
 
 
-lemma naturality_lt {S : SSet}
-    {face_map : Fin (3) →  S _[2]}
-    (hface : (i1 : Fin (3))→ (i2 : Fin (3)) → (i1< i2) →
-    S.map (δ (Fin.predAbove 0 ((δ i).toOrderHom i2))).op (face_map i1)
-    = S.map (δ (Fin.predAbove 2 ((δ i).toOrderHom i1))).op (face_map i2) ):
-    S.map ( ((Λ[3, i].map φ.op α).val.down) ≫ σ  ( Fin.predAbove 0 ((δ i).toOrderHom i1))).op
-    (face_map i1)=S.map φ.op (S.map ( (α.val.down)≫  σ (Fin.predAbove 0 ((δ i).toOrderHom i2))).op
-    (face_map i2))  := by
-  let α' :([(unop X).len]: SimplexCategory)⟶  [3]:= α.val.down
-  change S.map (factor_δ (φ ≫ α.val.down) ((δ i).toOrderHom i1)).op (face_map i1)
-             = (S.map (factor_δ α' ((δ i).toOrderHom i2)).op ≫ S.map φ.op) (face_map i2)
+lemma naturality_lt {S : SSet} {n  : ℕ } {i : Fin (n+3)} {X Y :SimplexCategoryᵒᵖ}
+    (α : Λ[n+2,i].obj X ) (φ: ([len Y.unop]: SimplexCategory)⟶ [len X.unop])
+    (f1 f2 :  S _[n+1])
+    (i1 i2 : Fin (n+3))
+    (i1_lt_i2 : i1<i2)
+    (exclude_i1 :  ∀ k, (φ ≫ α.val.down).toOrderHom k ≠  i1)
+    (exclude_i2 :  ∀ k, (φ ≫ α.val.down).toOrderHom k ≠  i2)
+    (hface : S.map (δ (Fin.predAbove 0 i2)).op f1
+    = S.map (δ (Fin.predAbove (Fin.last (n+1)) i1)).op f2 ):
+    S.map ( ((Λ[n+2, i].map φ.op α).val.down) ≫ σ  ( Fin.predAbove 0 i1)).op
+    (f1)=S.map φ.op (S.map ( (α.val.down)≫  σ (Fin.predAbove 0 i2)).op
+    (f2))  := by
+  let α' :([(unop X).len]: SimplexCategory)⟶  [n+2]:= α.val.down
+  change S.map (factor_δ (φ ≫ α.val.down) i1).op (_)
+             = (S.map (factor_δ α' i2).op ≫ S.map φ.op) (_)
   rw [← S.map_comp, ← op_comp]
-  change S.map (factor_δ (φ ≫ α.val.down) ((δ i).toOrderHom i1)).op (face_map i1)
-            = (S.map (factor_δ (φ ≫ α.val.down) ((δ i).toOrderHom i2)).op ) (face_map i2)
-  rw [← (factorization_of_φ_comp_α_i1 i1_lt_i2 exclude_i1 exclude_i2)]
-  rw [← (factorization_of_φ_comp_α_i2 i1_lt_i2 exclude_i1 exclude_i2)]
+  change _= (S.map (factor_δ (φ ≫ α.val.down) i2).op ) (_)
+  rw [← (factor_δ_comp_spec_lt i1_lt_i2 exclude_i1 exclude_i2)]
+  rw [← (factor_δ_comp_spec_lt' i1_lt_i2 exclude_i1 exclude_i2)]
   rw [op_comp,S.map_comp,op_comp,S.map_comp,types_comp_apply,types_comp_apply]
-  rw [(hface i1 i2 i1_lt_i2)]
-  change _ = S.map ((φ ≫ α.val.down)≫(σ (Fin.predAbove 0 ((δ i).toOrderHom i2))
-                ≫ σ (Fin.predAbove 0 (Fin.predAbove 2 ((δ i).toOrderHom i1))) ) ).op _
-  have dr :
-   (φ ≫ α.val.down)≫ (σ (Fin.predAbove 0 ((δ i).toOrderHom i2))
-    ≫ σ (Fin.predAbove 0 (Fin.predAbove 2 ((δ i).toOrderHom i1))) )
-    = (φ ≫ α.val.down)≫(σ ( (Fin.predAbove 0 ((δ i).toOrderHom i1)))
-    ≫ σ (Fin.predAbove 0 (Fin.predAbove 0 ((δ i).toOrderHom i2)))) := by
-        apply factor_δ_comp_lt
-        exact (Fin.strictMono_succAbove i i1_lt_i2)
-  rw [dr]
-  rfl
+  rw [(hface),← (factor_δ_comp_lt _ _ _ i1_lt_i2)]
 
-end HomMk₃
-
-/-- The horn `Λ[3,i]⟶ S` constructed from the image of the appropriate to 2-simplies and
+/-- The horn `Λ[n+2,i]⟶ S` constructed from the image of the appropriate to (n+1)-simplies and
 the appropriate compatiblity conditions on their faces. -/
-def homMk₃ {S : SSet}  (i: Fin 4)  (face_map : Fin (3) →  S _[2])
-    (hface : (i1 : Fin (3))→ (i2 : Fin (3)) → (i1< i2) →
+def homMk {S : SSet}  {n:ℕ} (i: Fin (n+3))  (face_map : Fin (n+2) →  S _[n+1])
+    (hface : (i1 : Fin (n+2))→ (i2 : Fin (n+2)) → (i1< i2) →
     S.map (δ (Fin.predAbove 0 ((δ i).toOrderHom i2))).op (face_map i1)
-    =S.map (δ (Fin.predAbove 2 ((δ i).toOrderHom i1))).op (face_map i2) ) : Λ[3,i]⟶ S where
+    =S.map (δ (Fin.predAbove (Fin.last (n+1)) ((δ i).toOrderHom i1))).op (face_map i2) )
+    : Λ[n+2,i]⟶ S where
   app X α := by
-    let α' :([(unop X).len]: SimplexCategory)⟶  [3]:= α.val.down
-    let id: Fin 3:= if ∀ k, α.1.down.toOrderHom k ≠  (δ i).toOrderHom 0 then 0
-                    else
-                      if ∀ k, α.1.down.toOrderHom k ≠ (δ i).toOrderHom 1 then 1
-                      else 2
-    exact S.map (factor_δ α'  ((δ i).toOrderHom  id)).op (face_map id)
+    let α' :([(unop X).len]: SimplexCategory)⟶  [n+2]:= α.1.down
+    let id:= FactorMinFace.min α
+    exact S.map (factor_δ α' ((δ i).toOrderHom  id)).op (face_map id)
   naturality X Y φ' := by
      funext α
+     let φ: ([len Y.unop]: SimplexCategory)⟶ [len X.unop] := φ'.unop
      simp only [mk_len, op_unop, len_mk, types_comp_apply]
-     split
-     all_goals split
-     all_goals rename_i h1 h2
-     all_goals try split
-     all_goals try split
-     any_goals
-         rw  [← (types_comp_apply (S.map _) (S.map _)),← S.map_comp]
-         rfl
-     all_goals rename_i h3
-     case inr.inl.inl | inr.inr.inl  =>
-         exact False.elim (h1 (fun k => h3 (φ'.unop.toOrderHom k)))
-     case inr.inr.inr.inl =>
-        exact False.elim (h2 (fun k => h3 (φ'.unop.toOrderHom k)))
-     all_goals
-        apply HomMk₃.naturality_lt
-        · rw [Fin.lt_def]
-          simp
-        · assumption
-        · try exact fun k => (HomMk₃.range_include01_exclude2 α h2 h3) (φ'.unop.toOrderHom k)
-          try exact fun k => h3 (φ'.unop.toOrderHom k)
-          try rename_i  h4;
-          try exact fun k => (HomMk₃.range_include01_exclude2 α h4 h3) (φ'.unop.toOrderHom k)
-        · exact hface
-
-/-- The faces of a `Λ[3,i]` horn made from a specification of 2-simplies are those 2-simplies.-/
-lemma homMk₃_face {S:SSet} (i: Fin 4) (j : Fin 3) (face_map : Fin (3) →  S _[2])
-    (hface : (i1 : Fin (3))→ (i2 : Fin (3)) → (i1< i2) →
-    S.map (δ (Fin.predAbove 0 ((δ i).toOrderHom i2))).op (face_map i1)
-    =S.map (δ (Fin.predAbove 2 ((δ i).toOrderHom i1))).op (face_map i2) ) :
-    (homMk₃ i face_map hface).app (op [2]) (face'.{u} i j)= face_map j:= by
-      let id :Fin 3:= if ∀ k, (δ ((δ i).toOrderHom j)).toOrderHom k ≠  (δ i).toOrderHom 0 then 0
-                    else
-                      if ∀ k, (δ ((δ i).toOrderHom j)).toOrderHom k  ≠ (δ i).toOrderHom 1 then 1
-                      else 2
-      have hid : id = j := by
-          fin_cases j <;> (dsimp; split;)
-          case' tail.tail.head.inr | tail.head.inr =>
-             split
-          any_goals rfl
-          all_goals (rename_i h2; exfalso;)
-          any_goals (refine ((?_ : (_→ False)) h2).elim;fin_cases i <;> decide )
-      change _ = 𝟙 (S _[2]) (face_map j)
-      rw [← S.map_id, ← op_id, ← (face'_factor.{u} i j)]
-      nth_rewrite 4 [← hid]
-      nth_rewrite 3 [← hid]
-      unfold homMk₃
-      simp
-      rfl
-
-lemma homMk₃_surjective {S:SSet} (i: Fin 4) (f : Λ[3,i] ⟶ S)   : ∃ (fa: Fin (3)→S _[2])
-    (hfa : (i1 : Fin (3))→ (i2 : Fin (3)) → (i1< i2) →
-    S.map (δ (Fin.predAbove 0 ((δ i).toOrderHom i2))).op (fa i1)
-    =S.map (δ (Fin.predAbove 2 ((δ i).toOrderHom i1))).op (fa i2) ), f=homMk₃ i fa hfa :=by
-    refine ⟨fun (j: Fin 3) =>  f.app (op [2]) (face' i j),?_,?_⟩
-    · intro i1 i2 i1_lt_i2
-      simp
-      rw [← (types_comp_apply (f.app _) (S.map _))]
-      rw [← (types_comp_apply (f.app _) (S.map _))]
-      rw [← f.naturality,← f.naturality]
-      rw [types_comp_apply,types_comp_apply]
-      apply congrArg
-      apply Subtype.ext
-      unfold horn
-      simp
-      rw [standardSimplex.map_apply,standardSimplex.map_apply]
-      simp
-      apply Hom.ext'
-      rw [← DFunLike.coe_fn_eq,← OrderHom.toFun_eq_coe]
-      fin_cases i1, i2
-      any_goals
-        rw [Fin.lt_def] at i1_lt_i2
-        simp at i1_lt_i2
-      all_goals
-        funext x
-        fin_cases i
-        all_goals fin_cases x
-        all_goals rfl
-    · apply horn.hom_ext
-      intro j h
-      rw [face_eq_face' i j h]
-      rw [homMk₃_face]
-
-
+     let i1 := FactorMinFace.min (Λ[n+2, i].map φ' α)
+     let i2 := FactorMinFace.min α
+     let i1_le_i2 : i1≤i2 := FactorMinFace.min_comp_le_min α φ'
+     have h : i1<i2 ∨ i1=i2 := by
+      exact lt_or_eq_of_le i1_le_i2
+     change S.map (factor_δ _ ((δ i).toOrderHom i1)).op (face_map i1)
+        = S.map φ.op (S.map (factor_δ _ (((δ i).toOrderHom i2))).op (face_map i2))
+     cases h with
+     | inl h =>
+                apply naturality_lt
+                · exact  Fin.strictMono_succAbove i h
+                · exact FactorMinFace.min_not_in_range (Λ[n+2, i].map φ' α)
+                · exact FactorMinFace.min_not_in_range_φ α φ'
+                · exact hface i1 i2 h
+     | inr h => rw [← h,← (types_comp_apply (S.map _) (S.map _)),← S.map_comp, ← op_comp]
+                rfl
 
 
 end horn
