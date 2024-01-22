89e10995f93b7fd554ad6b1ecde33b9fb8fcce60
Xavier-François Roblot <46200072+xroblot@users.noreply.github.com>
2024-01-22 17:58:31 +0100

    Add oneLePart and leOnePart

--- Mathlib/MeasureTheory/Order/Group/Lattice.lean
+++ Mathlib/MeasureTheory/Order/Group/Lattice.lean
@@ -3,8 +3,10 @@ Copyright (c) 2024 Xavier Roblot. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Xavier Roblot
 -/
+import Mathlib.Algebra.Order.Group.PosPart
 import Mathlib.MeasureTheory.Group.Arithmetic
 import Mathlib.MeasureTheory.Order.Lattice
+
 
 /-!
 # Measurability results on groups with a lattice structure.
@@ -15,8 +17,17 @@ measurable function, group, lattice operation
 -/
 
 variable {R : Type*} [Lattice R] [Group R] [MeasurableSpace R] [MeasurableSup₂ R]
-  [MeasurableInv R]
+
+@[to_additive (attr := measurability)]
+theorem measurable_oneLePart : Measurable fun x : R ↦ oneLePart x := by
+  refine Measurable.sup measurable_id' measurable_const
+
+variable [MeasurableInv R]
 
 @[to_additive (attr := measurability)]
 theorem measurable_mabs : Measurable fun x : R ↦ mabs x :=
   Measurable.sup measurable_id' measurable_inv
+
+@[to_additive (attr := measurability)]
+theorem measurable_leOnePart : Measurable fun x : R ↦ leOnePart x := by
+  refine Measurable.sup measurable_inv measurable_const
