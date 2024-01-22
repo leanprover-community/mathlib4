ffd4f914d5b6167ca07d042e8a4b1a3f15fd2137
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2024-01-22 12:00:53 +0100

    fix

--- Mathlib/MeasureTheory/Measure/Haar/Disintegration.lean
+++ Mathlib/MeasureTheory/Measure/Haar/Disintegration.lean
@@ -89,7 +89,7 @@ theorem LinearMap.exists_map_addHaar_eq_smul_addHaar' (h : Function.Surjective L
     refine ⟨addHaarScalarFactor (μ.map M.symm) (μS.prod μT), ?_, ENNReal.coe_ne_top,
       isAddHaarMeasure_eq_smul _ _⟩
     simpa only [ne_eq, ENNReal.coe_eq_zero] using
-      (addHaarScalarFactor_pos_of_isOpenPosMeasure (μ.map M.symm) (μS.prod μT)).ne'
+      (addHaarScalarFactor_pos_of_isAddHaarMeasure (μ.map M.symm) (μS.prod μT)).ne'
   have J : (μS.prod μT).map P = (μS univ) • μT := map_snd_prod
   obtain ⟨c₁, c₁_pos, c₁_fin, h₁⟩ : ∃ c₁ : ℝ≥0∞, c₁ ≠ 0 ∧ c₁ ≠ ∞ ∧ μT.map L' = c₁ • ν := by
     have : IsAddHaarMeasure (μT.map L') :=
@@ -97,7 +97,7 @@ theorem LinearMap.exists_map_addHaar_eq_smul_addHaar' (h : Function.Surjective L
     refine ⟨addHaarScalarFactor (μT.map L') ν, ?_, ENNReal.coe_ne_top,
       isAddHaarMeasure_eq_smul _ _⟩
     simpa only [ne_eq, ENNReal.coe_eq_zero] using
-      (addHaarScalarFactor_pos_of_isOpenPosMeasure (μT.map L') ν).ne'
+      (addHaarScalarFactor_pos_of_isAddHaarMeasure (μT.map L') ν).ne'
   refine ⟨c₀ * c₁, by simp [pos_iff_ne_zero, c₀_pos, c₁_pos], ENNReal.mul_lt_top c₀_fin c₁_fin, ?_⟩
   simp only [I, h₀, Measure.map_smul, J, smul_smul, h₁]
   rw [mul_assoc, mul_comm _ c₁, ← mul_assoc]
