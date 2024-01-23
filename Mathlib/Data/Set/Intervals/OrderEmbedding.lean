d3acbb08d58c17a04c703160f16a8b1550e7fd58
Yury G. Kudryashov <urkud@urkud.name>
2024-01-22 21:28:00 -0600

    Add lemmas about images of intervals under `OrderEmbedding`s

--- Mathlib/Data/Set/Intervals/OrderEmbedding.lean
+++ Mathlib/Data/Set/Intervals/OrderEmbedding.lean
@@ -3,7 +3,6 @@ Copyright (c) 2024 Yury Kudryashov. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yury Kudryashov
 -/
-import Mathlib.Data.Set.Intervals.Basic
 import Mathlib.Data.Set.Intervals.UnorderedInterval
 import Mathlib.Order.Hom.Basic
 
