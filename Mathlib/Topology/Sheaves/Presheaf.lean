0505484959245466336acddff8cdb0ad6ad75d13
Scott Morrison <scott.morrison@gmail.com>
2024-01-23 11:03:36 +1100

    fixes

--- Mathlib/Topology/Sheaves/Presheaf.lean
+++ Mathlib/Topology/Sheaves/Presheaf.lean
@@ -77,24 +77,26 @@ macro "sheaf_restrict" : attr =>
 
 attribute [sheaf_restrict] bot_le le_top le_refl inf_le_left inf_le_right
   le_sup_left le_sup_right
-
+#check Aesop.Options
 /-- `restrict_tac` solves relations among subsets (copied from `aesop cat`) -/
 macro (name := restrict_tac) "restrict_tac" c:Aesop.tactic_clause* : tactic =>
 `(tactic| first | assumption |
   aesop $c*
-    (config := { terminal := true
-                 assumptionTransparency := .reducible
-                 enableSimp := false })
+    (options :=
+      { terminal := true
+        assumptionTransparency := .reducible
+        enableSimp := false })
     (rule_sets [-default, -builtin, $(Lean.mkIdent `Restrict):ident]))
 
 /-- `restrict_tac?` passes along `Try this` from `aesop` -/
 macro (name := restrict_tac?) "restrict_tac?" c:Aesop.tactic_clause* : tactic =>
 `(tactic|
   aesop? $c*
-    (config := { terminal := true
-                 assumptionTransparency := .reducible
-                 enableSimp := false
-                 maxRuleApplications := 300 })
+    (options :=
+      { terminal := true
+        assumptionTransparency := .reducible
+        enableSimp := false
+        maxRuleApplications := 300 })
   (rule_sets [-default, -builtin, $(Lean.mkIdent `Restrict):ident]))
 
 attribute[aesop 10% (rule_sets [Restrict])] le_trans
