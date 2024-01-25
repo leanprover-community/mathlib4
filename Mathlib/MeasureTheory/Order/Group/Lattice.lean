b68fc37255be499c57209cd2fefc085a587f8880
Xavier-François Roblot <46200072+xroblot@users.noreply.github.com>
2024-01-25 10:01:56 +0100

    More review

--- Mathlib/MeasureTheory/Order/Group/Lattice.lean
+++ Mathlib/MeasureTheory/Order/Group/Lattice.lean
@@ -16,33 +16,35 @@ import Mathlib.MeasureTheory.Order.Lattice
 measurable function, group, lattice operation
 -/
 
-variable {α β : Type*} [Lattice α] [Group α] [MeasurableSpace α] [MeasurableSup α]
+variable {α β : Type*} [Lattice α] [Group α] [MeasurableSpace α]
   [MeasurableSpace β] {f : β → α} (hf : Measurable f)
 
 variable [MeasurableSup α]
 
 @[to_additive (attr := measurability)]
 theorem measurable_oneLePart : Measurable fun x : α ↦ oneLePart x :=
-    measurable_sup_const 1
+  measurable_sup_const _
 
 @[to_additive (attr := measurability)]
-theorem Measurable.oneLePart : Measurable fun x ↦ oneLePart (f x) :=
-  Measurable.comp measurable_oneLePart hf
+protected theorem Measurable.oneLePart : Measurable fun x ↦ oneLePart (f x) :=
+  measurable_oneLePart.comp hf
 
-variable [MeasurableSup₂ α] [MeasurableInv α]
-
-@[to_additive (attr := measurability)]
-theorem measurable_mabs : Measurable fun x : α ↦ mabs x :=
-    Measurable.sup measurable_id' measurable_inv
-
-@[to_additive (attr := measurability)]
-theorem Measurable.mabs : Measurable fun x ↦ mabs (f x) :=
-  Measurable.comp measurable_mabs hf
+variable [MeasurableInv α]
 
 @[to_additive (attr := measurability)]
 theorem measurable_leOnePart : Measurable fun x : α ↦ leOnePart x :=
-  Measurable.sup measurable_inv measurable_const
+  (measurable_sup_const _).comp  measurable_inv
 
 @[to_additive (attr := measurability)]
-theorem Measurable.leOnePart : Measurable fun x ↦ leOnePart (f x) :=
-  Measurable.comp measurable_leOnePart hf
+protected theorem Measurable.leOnePart : Measurable fun x ↦ leOnePart (f x) :=
+  measurable_leOnePart.comp  hf
+
+variable [MeasurableSup₂ α]
+
+@[to_additive (attr := measurability)]
+theorem measurable_mabs : Measurable fun x : α ↦ mabs x :=
+  measurable_id'.sup measurable_inv
+
+@[to_additive (attr := measurability)]
+theorem Measurable.mabs : Measurable fun x ↦ mabs (f x) :=
+  measurable_mabs.comp  hf
