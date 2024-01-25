820f97a377780dd93a62e169ef9f405c7ae8d3d0
Johan Commelin <johan@commelin.net>
2024-01-25 15:54:35 +0000

    refactor(ZMod): remove coe out of ZMod (#9839)
    Co-authored-by: Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
    Co-authored-by: Vierkantor <vierkantor@vierkantor.com>

--- Mathlib/Data/ZMod/Units.lean
+++ Mathlib/Data/ZMod/Units.lean
@@ -32,7 +32,7 @@ lemma unitsMap_comp {d : ℕ} (hm : n ∣ m) (hd : m ∣ d) :
 lemma unitsMap_self (n : ℕ) : unitsMap (dvd_refl n) = MonoidHom.id _ := by
   simp [unitsMap, castHom_self]
 
-lemma IsUnit_cast_of_dvd (hm : n ∣ m) (a : Units (ZMod m)) : IsUnit ((a : ZMod m) : ZMod n) :=
+lemma IsUnit_cast_of_dvd (hm : n ∣ m) (a : Units (ZMod m)) : IsUnit (cast (a : ZMod m) : ZMod n) :=
   Units.isUnit (unitsMap hm a)
 
 theorem unitsMap_surjective [hm : NeZero m] (h : n ∣ m) :
