3c27ccc1e867ea6a30e7cc7acf72c11e1606302c
Xavier-François Roblot <46200072+xroblot@users.noreply.github.com>
2024-01-26 09:17:30 +0100

    Changed back to eta-expansion

--- Mathlib/MeasureTheory/Order/Group/Lattice.lean
+++ Mathlib/MeasureTheory/Order/Group/Lattice.lean
@@ -26,7 +26,7 @@ theorem measurable_oneLePart : Measurable (oneLePart : α → α) :=
   measurable_sup_const _
 
 @[to_additive (attr := measurability)]
-protected theorem Measurable.oneLePart : Measurable (oneLePart ∘ f) :=
+protected theorem Measurable.oneLePart : Measurable (fun x ↦ oneLePart (f x)) :=
   measurable_oneLePart.comp hf
 
 variable [MeasurableInv α]
@@ -36,7 +36,7 @@ theorem measurable_leOnePart : Measurable (leOnePart : α → α) :=
   (measurable_sup_const _).comp measurable_inv
 
 @[to_additive (attr := measurability)]
-protected theorem Measurable.leOnePart : Measurable (leOnePart ∘ f) :=
+protected theorem Measurable.leOnePart : Measurable (fun x ↦ leOnePart (f x)) :=
   measurable_leOnePart.comp hf
 
 variable [MeasurableSup₂ α]
@@ -46,5 +46,5 @@ theorem measurable_mabs : Measurable (mabs : α → α) :=
   measurable_id'.sup measurable_inv
 
 @[to_additive (attr := measurability)]
-theorem Measurable.mabs : Measurable (mabs ∘ f) :=
+theorem Measurable.mabs : Measurable (fun x ↦ mabs (f x)) :=
   measurable_mabs.comp hf
