9c26fe7bd71d5dfaed81a5afb46c5f36718e6fc8
Tomáš Skřivan <skrivantomas@seznam.cz>
2024-01-26 22:08:12 +0100

    Update Mathlib/Tactic.lean
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Mathlib/Tactic.lean
+++ Mathlib/Tactic.lean
@@ -54,7 +54,6 @@ import Mathlib.Tactic.FProp.Core
 import Mathlib.Tactic.FProp.Elab
 import Mathlib.Tactic.FProp.FPropAttr
 import Mathlib.Tactic.FProp.FPropDecl
-import Mathlib.Tactic.FProp.Theorems
 import Mathlib.Tactic.FProp.FPropTypes
 import Mathlib.Tactic.FProp.RefinedDiscrTree
 import Mathlib.Tactic.FProp.StateList
