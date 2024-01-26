48acd634f6d350edea2cb6139fdc779589d55a64
Christopher Hoskin <christopher.hoskin@gmail.com>
2024-01-26 18:18:40 +0000

    Only import Basic

--- Mathlib/GroupTheory/GroupAction/Group.lean
+++ Mathlib/GroupTheory/GroupAction/Group.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Chris Hughes
 -/
 import Mathlib.Algebra.Group.Aut
-import Mathlib.Algebra.Invertible.Defs
 import Mathlib.Algebra.Invertible.Basic
 import Mathlib.GroupTheory.GroupAction.Units
 
