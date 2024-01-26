8fb776927b3793761f91a6bfbda2a4e98eb9fa21
Xavier-François Roblot <46200072+xroblot@users.noreply.github.com>
2024-01-26 08:54:46 +0100

    eta-reduced statements

--- Mathlib/MeasureTheory/Order/Group/Lattice.lean
+++ Mathlib/MeasureTheory/Order/Group/Lattice.lean
@@ -22,29 +22,29 @@ variable {α β : Type*} [Lattice α] [Group α] [MeasurableSpace α]
 variable [MeasurableSup α]
 
 @[to_additive (attr := measurability)]
-theorem measurable_oneLePart : Measurable fun x : α ↦ oneLePart x :=
+theorem measurable_oneLePart : Measurable (oneLePart : α → α) :=
   measurable_sup_const _
 
 @[to_additive (attr := measurability)]
-protected theorem Measurable.oneLePart : Measurable fun x ↦ oneLePart (f x) :=
+protected theorem Measurable.oneLePart : Measurable (oneLePart ∘ f) :=
   measurable_oneLePart.comp hf
 
 variable [MeasurableInv α]
 
 @[to_additive (attr := measurability)]
-theorem measurable_leOnePart : Measurable fun x : α ↦ leOnePart x :=
+theorem measurable_leOnePart : Measurable (leOnePart : α → α) :=
   (measurable_sup_const _).comp  measurable_inv
 
 @[to_additive (attr := measurability)]
-protected theorem Measurable.leOnePart : Measurable fun x ↦ leOnePart (f x) :=
+protected theorem Measurable.leOnePart : Measurable (leOnePart ∘ f) :=
   measurable_leOnePart.comp  hf
 
 variable [MeasurableSup₂ α]
 
 @[to_additive (attr := measurability)]
-theorem measurable_mabs : Measurable fun x : α ↦ mabs x :=
+theorem measurable_mabs : Measurable (mabs : α → α) :=
   measurable_id'.sup measurable_inv
 
 @[to_additive (attr := measurability)]
-theorem Measurable.mabs : Measurable fun x ↦ mabs (f x) :=
+theorem Measurable.mabs : Measurable (mabs ∘ f) :=
   measurable_mabs.comp  hf
