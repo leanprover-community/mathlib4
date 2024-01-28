9cf28c86b8985ab9dcb03fcc5c891854afe272d2
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2024-01-28 17:22:49 +0100

    fix another typo

--- Mathlib/MeasureTheory/Integral/CircleIntegral.lean
+++ Mathlib/MeasureTheory/Integral/CircleIntegral.lean
@@ -600,7 +600,7 @@ theorem hasSum_two_pi_I_cauchyPowerSeries_integral {f : ℂ → E} {c : ℂ} {R 
     simp only [smul_smul]
     refine' HasSum.smul_const _ _
     have : ‖w / (circleMap c R θ - c)‖ < 1 := by simpa [abs_of_pos hR] using hwR.2
-    convert (hasSum_geometric_of_norm_lt_ne this).mul_right _ using 1
+    convert (hasSum_geometric_of_norm_lt_one this).mul_right _ using 1
     simp [← sub_sub, ← mul_inv, sub_mul, div_mul_cancel _ (circleMap_ne_center hR.ne')]
 set_option linter.uppercaseLean3 false in
 #align has_sum_two_pi_I_cauchy_power_series_integral hasSum_two_pi_I_cauchyPowerSeries_integral
