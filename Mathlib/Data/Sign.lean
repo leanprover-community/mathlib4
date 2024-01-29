8883f77d5fc3f6ddffccd6e277e36f4105fc3930
Scott Morrison <scott.morrison@gmail.com>
2024-01-29 23:19:06 +1100

    tauto power up from solve_by_elim

--- Mathlib/Data/Sign.lean
+++ Mathlib/Data/Sign.lean
@@ -111,7 +111,7 @@ private lemma le_antisymm (a b : SignType) (_ : a ≤ b) (_: b ≤ a) : a = b :=
   cases a <;> cases b <;> trivial
 
 private lemma le_trans (a b c : SignType) (_ : a ≤ b) (_: b ≤ c) : a ≤ c := by
-  cases a <;> cases b <;> cases c <;> first | tauto
+  cases a <;> cases b <;> cases c <;> tauto
 
 instance : LinearOrder SignType where
   le := (· ≤ ·)
