f022b9aa853737d629026d45e722868d1e18e5ff
Joe Hendrix <joe@lean-fro.org>
2024-01-29 08:29:54 -0800

    fix: remove spurious changes from previous commit

--- Mathlib/Tactic/ToAdditive.lean
+++ Mathlib/Tactic/ToAdditive.lean
@@ -804,8 +804,6 @@ partial def transformDeclAux
   -- now add declaration ranges so jump-to-definition works
   -- note: we currently also do this for auxiliary declarations, while they are not normally
   -- generated for those. We could change that.
-  if env.getModuleIdxFor? tgt |>.isSome then
-    panic! "Adding declaration ranges!"
   addDeclarationRanges tgt {
     range := ← getDeclarationRange (← getRef)
     selectionRange := ← getDeclarationRange cfg.ref }
