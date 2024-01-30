130867e697eaba69347d67b608a927baeea8d44a
Tomáš Skřivan <skrivantomas@seznam.cz>
2024-01-30 15:46:58 -0500

    Update Mathlib/Tactic.lean
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Mathlib/Tactic.lean
+++ Mathlib/Tactic.lean
@@ -49,6 +49,8 @@ import Mathlib.Tactic.ExtractGoal
 import Mathlib.Tactic.ExtractLets
 import Mathlib.Tactic.FBinop
 import Mathlib.Tactic.FProp
+import Mathlib.Tactic.FProp.ContDiff
+import Mathlib.Tactic.FProp.Continuous
 import Mathlib.Tactic.FProp.Attr
 import Mathlib.Tactic.FProp.Core
 import Mathlib.Tactic.FProp.Decl
