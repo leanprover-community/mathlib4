5bb4b4bb5c6079433bfa655c1470f87f0277c1b6
Yury G. Kudryashov <urkud@urkud.name>
2024-01-30 14:58:48 +0000

    feat: images of intervals under `(↑) : ℕ → ℤ` (#9927)
    Also generalize `IsUpperSet.Ioi_subset` and `IsLowerSet.Iio_subset`
    from a `PartialOrder` to a `Preorder`.

--- Mathlib.lean
+++ Mathlib.lean
@@ -1779,6 +1779,7 @@ import Mathlib.Data.Nat.Cast.Field
 import Mathlib.Data.Nat.Cast.NeZero
 import Mathlib.Data.Nat.Cast.Order
 import Mathlib.Data.Nat.Cast.Prod
+import Mathlib.Data.Nat.Cast.SetInterval
 import Mathlib.Data.Nat.Cast.Synonym
 import Mathlib.Data.Nat.Cast.WithTop
 import Mathlib.Data.Nat.Choose.Basic
