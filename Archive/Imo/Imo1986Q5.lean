1325c96327b4a7ce013b27ab18859156327dd207
Yury G. Kudryashov <urkud@urkud.name>
2024-01-30 16:59:38 -0600

    Fix statement

--- Archive/Imo/Imo1986Q5.lean
+++ Archive/Imo/Imo1986Q5.lean
@@ -8,17 +8,22 @@ import Mathlib.Data.Real.NNReal
 /-!
 # IMO 1986 Q5
 
-Find all (if any) functions $f$ taking the non-negative reals onto the non-negative reals, such that
+Find all functions `f`, defined on the non-negative real numbers and taking nonnegative real values,
+such that:
 
-- $f(xf(y))f(y) = f(x+y)$ for all non-negative $x$, $y$;
-- $f(2) = 0$;
-- $f(x) \neq 0$ for every $0 \leq x < 2$.
+- $f(xf(y))f(y) = f(x + y)$ for all $x, y \ge 0$,
+- $f(2) = 0$,
+- $f(x) \ne 0$ for $0 \le x < 2$.
 
 We formalize the assumptions on `f` in `Imo1986Q5.IsGood` predicate,
 then prove `Imo1986Q5.IsGood f ↔ f = fun x ↦ 2 / (2 - x)`.
 
 Note that this formalization relies on the fact that Mathlib uses 0 as the "garbage value",
 namely for `2 ≤ x` we have `2 - x = 0` and `2 / (2 - x) = 0`.
+
+Formalization is based on
+[Art of Problem Solving](https://artofproblemsolving.com/wiki/index.php/1986_IMO_Problems/Problem_5)
+with minor modifications.
 -/
 
 open Set NNReal Classical
@@ -26,9 +31,9 @@ open Set NNReal Classical
 namespace Imo1986Q5
 
 structure IsGood (f : ℝ≥0 → ℝ≥0) : Prop where
+  map_add_rev x y : f (x * f y) * f y = f (x + y)
   map_two : f 2 = 0
   map_ne_zero : ∀ x < 2, f x ≠ 0
-  map_add_rev x y : f (x * f y) * f y = f (x + y)
 
 namespace IsGood
 
