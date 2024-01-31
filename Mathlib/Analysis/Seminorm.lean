d3756c4a19a1026ebbb68e8512786f7679540f75
s1m7u <68893305+s1m7u@users.noreply.github.com>
2024-01-31 17:37:31 +1100

    Update Mathlib/Analysis/Seminorm.lean
    Co-authored-by: Yury G. Kudryashov <urkud@urkud.name>

--- Mathlib/Analysis/Seminorm.lean
+++ Mathlib/Analysis/Seminorm.lean
@@ -963,9 +963,9 @@ theorem closedBall_smul_closedBall (p : Seminorm 𝕜 E) (r₁ r₂ : ℝ) :
   exact (norm_nonneg _).trans ha
 #align seminorm.closed_ball_smul_closed_ball Seminorm.closedBall_smul_closedBall
 
-theorem neg_mem_ball_zero_iff (r : ℝ) {x : E} : x ∈ ball p 0 r ↔ -x ∈ ball p 0 r := by
+theorem neg_mem_ball_zero {r : ℝ} {x : E} : -x ∈ ball p 0 r ↔ x ∈ ball p 0 r := by
   simp only [mem_ball_zero, map_neg_eq_map]
-#align seminorm.symmetric_ball_zero Seminorm.neg_mem_ball_zero_iff
+#align seminorm.symmetric_ball_zero Seminorm.neg_mem_ball_zero
 
 @[simp]
 theorem neg_ball (p : Seminorm 𝕜 E) (r : ℝ) (x : E) : -ball p x r = ball p (-x) r := by
