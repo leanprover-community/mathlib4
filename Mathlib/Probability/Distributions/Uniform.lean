92228a25597efedeb022726b0c3f71e3c3d8ec61
JADekker <114015169+JADekker@users.noreply.github.com>
2024-01-31 16:05:48 +0100

    Apply suggestions from code review
    Co-authored-by: Rémy Degenne <remydegenne@gmail.com>

--- Mathlib/Probability/Distributions/Uniform.lean
+++ Mathlib/Probability/Distributions/Uniform.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2024 Josha Dekker. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Josha Dekker
+Authors: Josha Dekker, Kexing Ying
 -/
 import Mathlib.Probability.Notation
 import Mathlib.Probability.Cdf
