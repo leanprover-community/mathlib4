20ccd19e66e0f91dc81d62ece4397897bb9e528b
Christian Merten <christian@merten.dev>
2024-02-01 01:00:42 +0100

    forgot adding new files

---
+++ Mathlib/RingTheory/Flat/Algebra.lean
@@ -0,0 +1,73 @@
+/-
+Copyright (c) 2024 Christian Merten. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Christian Merten
+-/
+import Mathlib.RingTheory.Flat.Stability
+import Mathlib.LinearAlgebra.TensorProduct.Tower
+
+/-!
+# Flat algebras and flat ring homomorphisms
+
+We define flat algebras and flat ring homomorphisms.
+
+## Main definitions
+
+* `Algebra.Flat`: an `R`-algebra `S` is flat if it is flat as `R`-module
+* `RingHom.Flat`: a ring homomorphism `f : R → S` is flat, if it makes `S` into a flat `R`-algebra
+
+-/
+
+universe u v w t
+
+open Function (Injective Surjective)
+
+open LinearMap (lsmul rTensor lTensor)
+
+open TensorProduct
+
+/-- An `R`-algebra `S` is flat if it is flat as `R`-module. -/
+class Algebra.Flat (R : Type u) (S : Type v) [CommRing R] [CommRing S] [Algebra R S] where
+  out : Module.Flat R S
+
+namespace Algebra.Flat
+
+attribute [instance] out
+
+instance self (R : Type u) [CommRing R] : Algebra.Flat R R where
+  out := Module.Flat.self R
+
+variable (R : Type u) (S : Type v) (T : Type w) [CommRing R] [CommRing S] [CommRing T]
+
+/-- If `T` is a flat `S`-algebra and `S` is a flat `R`-algebra,
+then `T` is a flat `R`-algebra. -/
+theorem comp [Algebra R S] [Algebra R T] [Algebra S T] [IsScalarTower R S T]
+    [Algebra.Flat R S] [Algebra.Flat S T] : Algebra.Flat R T where
+  out := Module.Flat.comp R S T
+
+end Algebra.Flat
+
+/-- A `RingHom` is flat if `R` is flat as an `S` algebra. -/
+class RingHom.Flat {R : Type u} {S : Type v} [CommRing R] [CommRing S] (f : R →+* S) where
+  out : f.toAlgebra.Flat := by infer_instance
+
+namespace RingHom.Flat
+
+attribute [instance] out
+
+/-- The identity of a ring is flat. -/
+instance identity (R : Type u) [CommRing R] : RingHom.Flat (1 : R →+* R) where
+
+variable {R : Type u} {S : Type v} {T : Type w} [CommRing R] [CommRing S] [CommRing T]
+  (f : R →+* S) (g : S →+* T)
+
+/-- Composition of flat ring homomorphisms is flat. -/
+noncomputable instance comp [RingHom.Flat f] [RingHom.Flat g] : RingHom.Flat (g.comp f) where
+  out :=
+    letI : Algebra R S := f.toAlgebra
+    letI : Algebra S T := g.toAlgebra
+    letI : Algebra R T := (g.comp f).toAlgebra
+    letI : IsScalarTower R S T := IsScalarTower.of_algebraMap_eq (congrFun rfl)
+    Algebra.Flat.comp R S T
+
+end RingHom.Flat
---
+++ Mathlib/RingTheory/Flat/Stability.lean
@@ -0,0 +1,82 @@
+/-
+Copyright (c) 2024 Christian Merten. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Christian Merten
+-/
+import Mathlib.RingTheory.Flat.Basic
+import Mathlib.LinearAlgebra.TensorProduct.Tower
+
+/-!
+# Flatness is stable under composition and base change
+
+We show that flatness is stable under composition and base change. The latter is not formalized yet.
+
+## Main theorems
+
+* `Module.Flat.comp`: if `S` is a flat `R`-algebra and `M` is a flat `S`-module,
+                      then `M` is a flat `R`-module
+
+## TODO
+
+* Show that flatness is stable under base change, i.e. if `S` is any `R`-algebra and `M` is a flat `R`-module,
+  then `M ⊗[R] S` is a flat `S`-module.
+
+-/
+
+universe u v w t
+
+open Function (Injective Surjective)
+
+open LinearMap (lsmul rTensor lTensor)
+
+open TensorProduct
+
+namespace Module.Flat
+
+variable (R : Type u) (S : Type v) (M : Type w) (N : Type t)
+  [CommRing R] [CommRing S] [Algebra R S]
+  [AddCommGroup M] [Module R M] [Module S M] [IsScalarTower R S M]
+
+private noncomputable abbrev auxRightMul (I : Ideal R) : M ⊗[R] I →ₗ[S] M := by
+  letI i : M ⊗[R] I →ₗ[S] M ⊗[R] R := AlgebraTensorModule.map LinearMap.id I.subtype
+  letI e' : M ⊗[R] R →ₗ[S] M := AlgebraTensorModule.rid R S M
+  exact AlgebraTensorModule.rid R S M ∘ₗ i
+
+private noncomputable abbrev auxJ (I : Ideal R) : Ideal S := LinearMap.range (auxRightMul R S S I)
+
+private noncomputable abbrev auxIsoJ [Module.Flat R S] {I : Ideal R} :
+    S ⊗[R] I ≃ₗ[S] auxJ R S I := by
+  apply LinearEquiv.ofInjective (auxRightMul R S S I)
+  simp only [LinearMap.coe_comp, LinearEquiv.coe_coe, EquivLike.comp_injective]
+  exact (Module.Flat.iff_lTensor_injective' R S).mp inferInstance I
+
+private noncomputable abbrev auxLTensor [Module.Flat R S] (I : Ideal R) :
+    M ⊗[R] I →ₗ[S] M := by
+  letI e1 : M ⊗[R] I ≃ₗ[S] M ⊗[S] (S ⊗[R] I) :=
+    (AlgebraTensorModule.cancelBaseChange R S S M I).symm
+  letI e2 : M ⊗[S] (S ⊗[R] I) ≃ₗ[S] M ⊗[S] (auxJ R S I) :=
+    TensorProduct.congr (LinearEquiv.refl S M) (auxIsoJ R S)
+  letI e3 : M ⊗[S] (auxJ R S I) →ₗ[S] M ⊗[S] S := lTensor M (auxJ R S I).subtype
+  letI e4 : M ⊗[S] S →ₗ[S] M := TensorProduct.rid S M
+  exact e4 ∘ₗ e3 ∘ₗ (e1 ≪≫ₗ e2)
+
+private lemma auxLTensor_eq [Module.Flat R S] {I : Ideal R} :
+    (auxLTensor R S M I : M ⊗[R] I →ₗ[R] M) =
+    TensorProduct.rid R M ∘ₗ lTensor M (I.subtype) := by
+  apply TensorProduct.ext'
+  intro m x
+  erw [TensorProduct.rid_tmul]
+  simp
+
+/-- If `S` is a flat `R`-algebra, then any flat `S`-Module is also `R`-flat. -/
+theorem comp [Module.Flat R S] [Module.Flat S M] : Module.Flat R M := by
+  rw [Module.Flat.iff_lTensor_injective']
+  intro I
+  rw [← EquivLike.comp_injective _ (TensorProduct.rid R M)]
+  haveI h : TensorProduct.rid R M ∘ lTensor M (Submodule.subtype I) =
+    TensorProduct.rid R M ∘ₗ lTensor M I.subtype := rfl
+  simp only [h, ← auxLTensor_eq R S M, LinearMap.coe_restrictScalars, LinearMap.coe_comp,
+    LinearEquiv.coe_coe, EquivLike.comp_injective, EquivLike.injective_comp]
+  exact (Module.Flat.iff_lTensor_injective' S M).mp inferInstance _
+
+end Module.Flat
