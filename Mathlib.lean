aec7e37bee16fc16528bd4b5831dbad11810ed97
Wrenna Robson <wren.robson@gmail.com>
2024-02-01 18:45:54 +0000

    Add new file

--- Mathlib.lean
+++ Mathlib.lean
@@ -1526,6 +1526,7 @@ import Mathlib.Data.Fin.Basic
 import Mathlib.Data.Fin.Fin2
 import Mathlib.Data.Fin.FlagRange
 import Mathlib.Data.Fin.Interval
+import Mathlib.Data.Fin.OrderHom
 import Mathlib.Data.Fin.SuccPred
 import Mathlib.Data.Fin.Tuple.Basic
 import Mathlib.Data.Fin.Tuple.BubbleSortInduction
