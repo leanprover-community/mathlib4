6dd08a56727cfc02b47890cbf06b7c06cd86555b
Christian Merten <christian@merten.dev>
2024-02-01 18:09:25 +0000

    feat(RingTheory/Flat): definition of flat ring homomorphism and stability under composition (#10148)
    Adds the definition of flat algebras, flat ring homomorphisms and shows that flatness is stable under composition.

--- Mathlib.lean
+++ Mathlib.lean
@@ -3134,7 +3134,9 @@ import Mathlib.RingTheory.FinitePresentation
 import Mathlib.RingTheory.FiniteType
 import Mathlib.RingTheory.Finiteness
 import Mathlib.RingTheory.Fintype
+import Mathlib.RingTheory.Flat.Algebra
 import Mathlib.RingTheory.Flat.Basic
+import Mathlib.RingTheory.Flat.Stability
 import Mathlib.RingTheory.FractionalIdeal.Basic
 import Mathlib.RingTheory.FractionalIdeal.Norm
 import Mathlib.RingTheory.FractionalIdeal.Operations
