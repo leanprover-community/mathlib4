a9e5c97a54df0543783bff424b120a30b61a4aac
grunweg <rothgami@math.hu-berlin.de>
2024-02-01 10:54:47 +0000

    refactor(PartialHomeomorph): Remove explicit variable (x : M). (#10083)
    Instead, supply it as needed.
    
    This replaces an explicit argument (x : M) by an implicit `{x : M}` in the following lemmas:
    - extChartAt_source_mem_nhds'
    - extChartAt_source_mem_nhdsWithin'
    - continuousAt_extChartAt'
    - extChartAt_image_nhd_mem_nhds_of_boundaryless
    - extChartAt_target_mem_nhdsWithin'
    - nhdsWithin_extChartAt_target_eq'
    - continuousAt_extChartAt_symm'
    - continuousAt_extChartAt_symm''
    - map_extChartAt_nhdsWithin_eq_image'
    - map_extChartAt_nhdsWithin'
    - map_extChartAt_symm_nhdsWithin'
    - map_extChartAt_symm_nhdsWithin_range'
    - extChartAt_preimage_mem_nhdsWithin'
    - extChartAt_preimage_mem_nhdsWithin
    - extChartAt_preimage_mem_nhds'
    - extChartAt_preimage_mem_nhds
    
    
    
    Co-authored-by: grunweg <grunweg@posteo.de>

--- Mathlib/Geometry/Manifold/ContMDiffMFDeriv.lean
+++ Mathlib/Geometry/Manifold/ContMDiffMFDeriv.lean
@@ -137,8 +137,8 @@ protected theorem ContMDiffAt.mfderiv {x₀ : N} (f : N → M → M') (g : N →
       rw [(extChartAt I (g x₂)).left_inv hx, (extChartAt I' (f x₂ (g x₂))).left_inv h2x]
     refine' Filter.EventuallyEq.fderivWithin_eq_nhds _
     refine' eventually_of_mem (inter_mem _ _) this
-    · exact extChartAt_preimage_mem_nhds' _ _ hx₂ (extChartAt_source_mem_nhds I (g x₂))
-    refine' extChartAt_preimage_mem_nhds' _ _ hx₂ _
+    · exact extChartAt_preimage_mem_nhds' _ hx₂ (extChartAt_source_mem_nhds I (g x₂))
+    refine' extChartAt_preimage_mem_nhds' _ hx₂ _
     exact h2x₂.continuousAt.preimage_mem_nhds (extChartAt_source_mem_nhds _ _)
   /- The conclusion is equal to the following, when unfolding coord_change of
       `tangentBundleCore` -/
