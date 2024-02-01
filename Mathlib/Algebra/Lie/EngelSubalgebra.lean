35d4186e59d9ebac7eeb88aa24bc7753a91229b8
Johan Commelin <johan@commelin.net>
2024-02-01 13:20:11 +0100

    oops, copyright

--- Mathlib/Algebra/Lie/EngelSubalgebra.lean
+++ Mathlib/Algebra/Lie/EngelSubalgebra.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2024 Johan Commelin. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Johan Commelin
+-/
 import Mathlib.Algebra.Lie.Engel
 import Mathlib.Algebra.Lie.Normalizer
 import Mathlib.Algebra.Lie.OfAssociative
