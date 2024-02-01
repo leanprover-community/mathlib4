4c63ee539c603d925d996e33b9f5044f070f6ed3
Scott Morrison <scott.morrison@gmail.com>
2024-02-01 14:59:14 +1100

    missing doc-string

--- Mathlib/Tactic/NormNum/Core.lean
+++ Mathlib/Tactic/NormNum/Core.lean
@@ -189,6 +189,7 @@ initialize registerBuiltinAttribute {
     modifyEnv fun env => normNumExt.modifyState env fun _ => s
 }
 
+/-- A simplifier step for `norm_num`. -/
 def tryNormNum (post := false) (e : Expr) : SimpM Simp.Step := do
   try
     return .done (← eval e post)
