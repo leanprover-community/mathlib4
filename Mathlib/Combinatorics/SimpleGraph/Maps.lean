73e9db67ea118b067cb15b4fee439edb10b31e2a
Jeremy Tan Jie Rui <reddeloostw@gmail.com>
2024-02-03 06:56:30 +0800

    Update Maps.lean

--- Mathlib/Combinatorics/SimpleGraph/Maps.lean
+++ Mathlib/Combinatorics/SimpleGraph/Maps.lean
@@ -24,6 +24,15 @@ injective, surjective and bijective, and have corresponding notation.
 * `SimpleGraph.Hom`, `G →g H`: a graph homomorphism from `G` to `H`.
 * `SimpleGraph.Embedding`, `G ↪g H`: a graph embedding of `G` in `H`.
 * `SimpleGraph.Iso`, `G ≃g H`: a graph isomorphism between `G` and `H`.
+
+Note that a graph embedding is a stronger notion than an injective graph homomorphism,
+since its image is an induced subgraph.
+
+## Implementation notes
+
+Morphisms of graphs are abbreviations for `RelHom`, `RelEmbedding` and `RelIso`.
+To make use of pre-existing simp lemmas, definitions involving morphisms are
+abbreviations as well.
 -/
 
 
