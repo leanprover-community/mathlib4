8815098af71d96dbd8e38d4738cc63434e7947ff
qawbecrdtey <qawbecrdtey@kaist.ac.kr>
2024-02-02 22:34:46 +0900

    Added copyright header

--- Mathlib/Data/Greedoid/Predefs.lean
+++ Mathlib/Data/Greedoid/Predefs.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2024 Jihoon Hyun. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Jihoon Hyun
+-/
 import Mathlib.Data.Finset.Basic
 import Mathlib.Data.Finset.Card
 
