40a6aed5638d34fc3ddf4946b186f20c604a8455
Yuma Mizuno <mizuno.y.aj@gmail.com>
2024-02-02 11:22:10 +0900

    merge

--- Mathlib/CategoryTheory/Bicategory/Extension.lean
+++ Mathlib/CategoryTheory/Bicategory/Extension.lean
@@ -3,7 +3,7 @@ Copyright (c) 2023 Yuma Mizuno. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yuma Mizuno
 -/
-import Mathlib.Tactic.CategoryTheory.Coherence
+import Mathlib.CategoryTheory.Bicategory.Basic
 import Mathlib.CategoryTheory.Comma.StructuredArrow
 
 /-!
