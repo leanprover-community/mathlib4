cacd9444ad1e34cf34fa85e49ec2a030494fd092
Junyan Xu <junyanxumath@gmail.com>
2024-02-02 21:03:07 +0000

    refactor: generalize `CharP+Prime` to `ExpChar` in `frobenius`  (#10016)
    Consequently, the part about `frobenius` in Algebra/CharP/Basic is moved to CharP/ExpChar, and imports are adjusted as necessary.
    
    + Add instances from `CharP+Fact(Nat.Prime)` and `CharZero` to `ExpChar`, to allow lemmas generalized to ExpChar still apply to CharP.
    
    + Remove lemmas in Algebra/CharP/ExpChar from [#9799](https://github.com/leanprover-community/mathlib4/commit/1e74fcfff8d5ffe5a3a9881864cf10fa39f619e6) because they coincide with the generalized lemmas, and golf the other lemmas (in Algebra/CharP/Basic).
    
    + Define the RingHom `iterateFrobenius` and the semilinear map `LinearMap.(iterate)Frobenius` for an algebra. When the characteristic is zero (ExpChar is 1), these are all equal to the identity map (· ^ 1). Also define `iterateFrobeniusEquiv` for perfect rings.
    
    + Fix and/or generalize other files.
    
    
    
    Co-authored-by: Junyan Xu <junyanxu.math@gmail.com>
    Co-authored-by: acmepjz <acme_pjz@hotmail.com>

--- Mathlib/Data/Polynomial/Expand.lean
+++ Mathlib/Data/Polynomial/Expand.lean
@@ -224,11 +224,12 @@ theorem contract_expand {f : R[X]} (hp : p ≠ 0) : contract p (expand R p f) = 
   simp [coeff_contract hp, coeff_expand hp.bot_lt, Nat.mul_div_cancel _ hp.bot_lt]
 #align polynomial.contract_expand Polynomial.contract_expand
 
-section CharP
+theorem contract_one {f : R[X]} : contract 1 f = f :=
+  ext fun n ↦ by rw [coeff_contract one_ne_zero, mul_one]
 
-variable [CharP R p]
+section ExpChar
 
-theorem expand_contract [NoZeroDivisors R] {f : R[X]} (hf : Polynomial.derivative f = 0)
+theorem expand_contract [CharP R p] [NoZeroDivisors R] {f : R[X]} (hf : Polynomial.derivative f = 0)
     (hp : p ≠ 0) : expand R p (contract p f) = f := by
   ext n
   rw [coeff_expand hp.bot_lt, coeff_contract hp]
@@ -245,11 +246,17 @@ theorem expand_contract [NoZeroDivisors R] {f : R[X]} (hf : Polynomial.derivativ
     exact absurd h' h
 #align polynomial.expand_contract Polynomial.expand_contract
 
-variable [hp : Fact p.Prime]
+variable [ExpChar R p]
+
+theorem expand_contract' [NoZeroDivisors R] {f : R[X]} (hf : Polynomial.derivative f = 0) :
+    expand R p (contract p f) = f := by
+  obtain _ | @⟨_, hprime, hchar⟩ := ‹ExpChar R p›
+  · rw [expand_one, contract_one]
+  · haveI := Fact.mk hchar; exact expand_contract p hf hprime.ne_zero
 
 theorem expand_char (f : R[X]) : map (frobenius R p) (expand R p f) = f ^ p := by
   refine' f.induction_on' (fun a b ha hb => _) fun n a => _
-  · rw [AlgHom.map_add, Polynomial.map_add, ha, hb, add_pow_char]
+  · rw [AlgHom.map_add, Polynomial.map_add, ha, hb, add_pow_expChar]
   · rw [expand_monomial, map_monomial, ← C_mul_X_pow_eq_monomial, ← C_mul_X_pow_eq_monomial,
       mul_pow, ← C.map_pow, frobenius_def]
     ring
@@ -264,7 +271,7 @@ theorem map_expand_pow_char (f : R[X]) (n : ℕ) :
     expand_mul, ← map_expand]
 #align polynomial.map_expand_pow_char Polynomial.map_expand_pow_char
 
-end CharP
+end ExpChar
 
 end CommSemiring
 
