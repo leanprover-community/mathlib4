e2aba2bf169bab55d4dac6805471a9bed5531729
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2024-02-03 15:50:38 +0000

    Update Mathlib/RingTheory/Bialgebra.lean

--- Mathlib/RingTheory/Bialgebra.lean
+++ Mathlib/RingTheory/Bialgebra.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2024 Ali Ramsey. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Ali Ramsey
+Authors: Ali Ramsey, Kevin Buzzard
 -/
 import Mathlib.RingTheory.Coalgebra
 import Mathlib.RingTheory.TensorProduct
