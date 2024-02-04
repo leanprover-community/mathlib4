e313407c3ca6ba2ce97341ddfa40a4640caf1945
Yury G. Kudryashov <urkud@urkud.name>
2024-02-03 22:45:55 -0600

    Snapshot

--- Mathlib/Tactic/Lint.lean
+++ Mathlib/Tactic/Lint.lean
@@ -34,3 +34,25 @@ Linter that checks whether a structure should be in Prop.
     let allProofs ← projs.allM (do isProof <| ← mkConstWithLevelParams <| declName ++ ·)
     unless allProofs do return none
     return m!"all fields are propositional but the structure isn't."
+
+/--
+Linter that checks for theorems that assume `[Decidable p]`
+but don't use this assumption in the type.
+-/
+@[std_linter] def decidableClassical : Linter where
+  noErrorsFound := "No uses of `Decidable` arguments should be replaced with `classical`"
+  errorsFound := "USES OF `Decidable` SHOULD BE REPLACED WITH `classical` IN THE PROOF."
+  test declName := do
+    if (← isAutoDecl declName) then return none
+    if !(← isProp (← getConstInfo declName).type) then return none
+    forallTelescopeReducing (← inferType (← mkConstWithLevelParams declName)) fun args ty => do
+      let argTys ← args.mapM inferType
+      let impossibleArgs ← args.zipWithIndex.filterMapM fun (arg, i) => do
+        let t ← inferType arg
+        if !#[`Decidable, `DecidableEq, `DecidablePred].any t.getForallBody.isAppOf then return none
+        let fv := arg.fvarId!
+        if ty.containsFVar fv then return none
+        if argTys[i+1:].any (·.containsFVar fv) then return none
+        return some m!"argument {i+1} {arg} : {t}"
+      if impossibleArgs.isEmpty then return none
+      addMessageContextFull <| .joinSep impossibleArgs.toList ", "
