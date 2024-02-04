f4b7710fee5e44dad235bdcdd61d5d2ab51bf889
L Lllvvuu <git@llllvvuu.dev>
2024-02-04 15:18:28 -0800

    Remove section

--- Mathlib/MeasureTheory/Integral/Asymptotics.lean
+++ Mathlib/MeasureTheory/Integral/Asymptotics.lean
@@ -11,8 +11,6 @@ import Mathlib.MeasureTheory.Integral.IntegrableOn
 We establish integrability of `f` from `f = O(g)`.
 -/
 
-noncomputable section
-
 open Asymptotics MeasureTheory Set Filter
 
 variable {α E : Type*} [MeasurableSpace α] [NormedDivisionRing E] [NormedSpace ℝ E]
@@ -38,7 +36,7 @@ private theorem _root_.Asymptotics.IsBigO.integrableAtFilter_aux
 theorem _root_.Asymptotics.IsBigO.integrableAtFilter'
     (hfm : AEStronglyMeasurable f μ) (hgm : ∀ s ∈ l, MeasurableSet s)
     (hf : f =O[l] g) (hg : IntegrableAtFilter g l μ) : IntegrableAtFilter f l μ := by
-  refine hf.integrableAtFilter_aux hfm (fun C' s hs t ht ↦ ?_) hg
+  refine hf.integrableAtFilter_aux hfm (fun _ s hs t ht ↦ ?_) hg
   exact ae_restrict_iff' <| (hgm s hs).inter (hgm t ht)
 
 variable [MeasurableSpace E] [OpensMeasurableSpace E] [SecondCountableTopology E]
@@ -49,5 +47,3 @@ theorem _root_.Asymptotics.IsBigO.integrableAtFilter (hfm : Measurable f) (hgm :
   refine ae_restrict_iff (measurableSet_le hfm.norm ?_)
   convert hgm.norm.const_mul ‖C'‖
   exact norm_mul C' _
-
-end
