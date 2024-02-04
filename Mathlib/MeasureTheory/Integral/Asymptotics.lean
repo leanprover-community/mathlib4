8efc35dd55e84c64d9480bdf4a55e54653a649f2
L Lllvvuu <git@llllvvuu.dev>
2024-02-04 15:17:56 -0800

    deduplicate code

--- Mathlib/MeasureTheory/Integral/Asymptotics.lean
+++ Mathlib/MeasureTheory/Integral/Asymptotics.lean
@@ -18,8 +18,11 @@ open Asymptotics MeasureTheory Set Filter
 variable {α E : Type*} [MeasurableSpace α] [NormedDivisionRing E] [NormedSpace ℝ E]
  {f g : α → E} {a b : α} {μ : Measure α} {l : Filter α}
 
-theorem _root_.Asymptotics.IsBigO.integrableAtFilter'
-    (hfm : AEStronglyMeasurable f μ) (hgm : ∀ s ∈ l, MeasurableSet s)
+private theorem _root_.Asymptotics.IsBigO.integrableAtFilter_aux
+    (hfm : AEStronglyMeasurable f μ)
+    (hae_restrict_iff : ∀ C' : E, ∀ s ∈ l, ∀ t ∈ l,
+      (∀ᵐ (x : α) ∂μ.restrict (s ∩ t), ‖f x‖ ≤ ‖C' * g x‖) ↔
+      ∀ᵐ (x : α) ∂μ, x ∈ (s ∩ t) → ‖f x‖ ≤ ‖C' * g x‖)
     (hf : f =O[l] g) (hg : IntegrableAtFilter g l μ) : IntegrableAtFilter f l μ := by
   obtain ⟨C, hC⟩ := hf.bound
   obtain ⟨C', hC'⟩ := exists_norm_eq E <| le_max_right C 0
@@ -27,28 +30,24 @@ theorem _root_.Asymptotics.IsBigO.integrableAtFilter'
   obtain ⟨t, htl, ht⟩ := hg
   use s ∩ t, inter_mem hsl htl, hfm.restrict
   refine ht.mono_set (inter_subset_right s t) |>.const_mul C' |>.2.mono
-    <| (ae_restrict_iff' <| hgm _ <| inter_mem hsl htl).mpr
-    <| ae_of_all _ fun x hx => (hs x hx.1).trans ?_
+    <| (hae_restrict_iff C' s hsl t htl).mpr <| ae_of_all _ fun x hx => (hs x hx.1).trans ?_
   rewrite [norm_mul, hC']
   gcongr
   apply le_max_left
+
+theorem _root_.Asymptotics.IsBigO.integrableAtFilter'
+    (hfm : AEStronglyMeasurable f μ) (hgm : ∀ s ∈ l, MeasurableSet s)
+    (hf : f =O[l] g) (hg : IntegrableAtFilter g l μ) : IntegrableAtFilter f l μ := by
+  refine hf.integrableAtFilter_aux hfm (fun C' s hs t ht ↦ ?_) hg
+  exact ae_restrict_iff' <| (hgm s hs).inter (hgm t ht)
 
 variable [MeasurableSpace E] [OpensMeasurableSpace E] [SecondCountableTopology E]
 
 theorem _root_.Asymptotics.IsBigO.integrableAtFilter (hfm : Measurable f) (hgm : Measurable g)
     (hf : f =O[l] g) (hg : IntegrableAtFilter g l μ) : IntegrableAtFilter f l μ := by
-  obtain ⟨C, hC⟩ := hf.bound
-  obtain ⟨C', hC'⟩ := exists_norm_eq E <| le_max_right C 0
-  obtain ⟨s, hsl, hs⟩ := hC.exists_mem
-  obtain ⟨t, htl, ht⟩ := hg
-  use s ∩ t, inter_mem hsl htl, hfm.aestronglyMeasurable.restrict
-  refine ht.mono_set (inter_subset_right s t) |>.const_mul C' |>.2.mono
-    <| (ae_restrict_iff (measurableSet_le hfm.norm ?_)).mpr
-    <| (ae_of_all _ fun x hx => (hs x hx.1).trans ?_)
-  · convert hgm.norm.const_mul ‖C'‖
-    exact norm_mul C' _
-  · rewrite [norm_mul, hC']
-    gcongr
-    apply le_max_left
+  refine hf.integrableAtFilter_aux hfm.aestronglyMeasurable (fun C' _ _ _ _ ↦ ?_) hg
+  refine ae_restrict_iff (measurableSet_le hfm.norm ?_)
+  convert hgm.norm.const_mul ‖C'‖
+  exact norm_mul C' _
 
 end
