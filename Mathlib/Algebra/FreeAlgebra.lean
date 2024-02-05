6d578c588734ab5f076a14045b4d53b7deaee7a5
Richard Copley <buster@buster.me.uk>
2024-02-05 11:50:25 +0000

    feat: tensor algebra of free module over integral domain is a domain (#9890)
    Provide instances
      * `Nontrivial (TensorAlgebra R M)` when `M` is a module over
        a nontrivial semiring `R`
      * `NoZeroDivisors (FreeAlgebra R X)` when `R` is a commutative
        semiring with no zero-divisors and `X` any type
      * `IsDomain (FreeAlgebra R X)` when `R` is an integral domain
        and `X` is any type
      * `TwoUniqueProds (FreeMonoid X)` where `X` is any type
        (this provides `NoZeroDivisors (MonoidAlgebra R (FreeMonoid X))`
        when `R` is a semiring and `X` any type,
        via `TwoUniqueProds.toUniqueProds`
        and `MonoidAlgebra.instNoZeroDivisorsOfUniqueProds`)
      * `NoZeroDivisors (TensorAlgebra R M)` when `M` is a free module
        over a commutative semiring `R` with no zero-divisors
      * `IsDomain (TensorAlgebra R M)` when `M` is a free module over
        an integral domain `R`
    
    In Algebra.Group.UniqueProds:
    * Rename `UniqueProds.mulHom_image_of_injective` to
      `UniqueProds.of_injective_mulHom`.
    * New lemmas `UniqueMul.of_mulHom_image`, `UniqueProds.of_mulHom`,
      `TwoUniqueProds.of_mulHom` show the relevant property holds in the
      domain of a multiplicative homomorphism if it holds in the codomain,
      under a certain hypothesis on the homomorphism.
    
    
    Co-authored-by: Richard Copley <rcopley@gmail.com>
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>
    Co-authored-by: Junyan Xu <junyanxu.math@gmail.com>

--- Mathlib/Algebra/FreeAlgebra.lean
+++ Mathlib/Algebra/FreeAlgebra.lean
@@ -5,7 +5,7 @@ Authors: Scott Morrison, Adam Topaz, Eric Wieser
 -/
 import Mathlib.Algebra.Algebra.Subalgebra.Basic
 import Mathlib.Algebra.Algebra.Tower
-import Mathlib.Algebra.MonoidAlgebra.Basic
+import Mathlib.Algebra.MonoidAlgebra.NoZeroDivisors
 import Mathlib.RingTheory.Adjoin.Basic
 
 #align_import algebra.free_algebra from "leanprover-community/mathlib"@"6623e6af705e97002a9054c1c05a980180276fc1"
@@ -471,8 +471,17 @@ noncomputable def equivMonoidAlgebraFreeMonoid :
       simp)
 #align free_algebra.equiv_monoid_algebra_free_monoid FreeAlgebra.equivMonoidAlgebraFreeMonoid
 
+/-- `FreeAlgebra R X` is nontrivial when `R` is. -/
 instance [Nontrivial R] : Nontrivial (FreeAlgebra R X) :=
   equivMonoidAlgebraFreeMonoid.surjective.nontrivial
+
+/-- `FreeAlgebra R X` has no zero-divisors when `R` has no zero-divisors. -/
+instance instNoZeroDivisors [NoZeroDivisors R] : NoZeroDivisors (FreeAlgebra R X) :=
+  equivMonoidAlgebraFreeMonoid.toMulEquiv.noZeroDivisors
+
+/-- `FreeAlgebra R X` is a domain when `R` is an integral domain. -/
+instance instIsDomain {R X} [CommRing R] [IsDomain R] : IsDomain (FreeAlgebra R X) :=
+  NoZeroDivisors.to_isDomain _
 
 section
 
