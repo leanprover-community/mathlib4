99983944c4d2c89150fe57406d0ce19c89ecc414
Yaël Dillies <yael.dillies@gmail.com>
2024-02-05 13:02:17 +0000

    remove unused argument

--- Mathlib/Algebra/Order/Group/Abs.lean
+++ Mathlib/Algebra/Order/Group/Abs.lean
@@ -567,7 +567,7 @@ variable {ι : Type*} {α : ι → Type*} [∀ i, AddGroup (α i)] [∀ i, Latti
 
 @[simp] lemma abs_apply (f : ∀ i, α i) (i : ι) : |f| i = |f i| := rfl
 
-lemma abs_def [∀ i, Neg (α i)] [∀ i, Sup (α i)] (f : ∀ i, α i) : |f| = fun i ↦ |f i| := rfl
+lemma abs_def (f : ∀ i, α i) : |f| = fun i ↦ |f i| := rfl
 
 end Pi
 
