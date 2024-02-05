4daf1622e123ce4738ba554f15eb4c7c1e0cc35e
L Lllvvuu <git@llllvvuu.dev>
2024-02-05 00:48:38 -0800

    downgrade instance

--- Mathlib/MeasureTheory/Integral/Asymptotics.lean
+++ Mathlib/MeasureTheory/Integral/Asymptotics.lean
@@ -18,8 +18,8 @@ We establish integrability of `f` from `f = O(g)`.
 
 open Asymptotics MeasureTheory Set Filter
 
-variable {α E F : Type*} [MeasurableSpace α] [NormedAddCommGroup E] [NormedDivisionRing F]
-  [NormedSpace ℝ F] {f : α → E} {g : α → F} {a b : α} {μ : Measure α} {l : Filter α}
+variable {α E F : Type*} [MeasurableSpace α] [NormedAddCommGroup E] [NontriviallyNormedField F]
+  {f : α → E} {g : α → F} {a b : α} {μ : Measure α} {l : Filter α}
 
 /-- Given `f = O[l] g` on measurably generated `l`,
 `IntegrableAtFilter g l μ`, and `f` `AEStronglyMeasurable` at `l`, `IntegrableAtFilter f l μ`. -/
@@ -27,7 +27,7 @@ theorem _root_.Asymptotics.IsBigO.integrableAtFilter [IsMeasurablyGenerated l]
     (hfm : ∃ s ∈ l, AEStronglyMeasurable f (μ.restrict s))
     (hf : f =O[l] g) (hg : IntegrableAtFilter g l μ) : IntegrableAtFilter f l μ := by
   obtain ⟨C, hC⟩ := hf.bound
-  obtain ⟨C', hC'⟩ := exists_norm_eq F <| le_max_right C 0
+  obtain ⟨C', hC'⟩ := NormedField.exists_lt_norm F C
   obtain ⟨s, hsl, hs⟩ := hC.exists_mem
   obtain ⟨t, htl, ht⟩ := hg
   obtain ⟨u, hul, hu⟩ := hfm
@@ -36,9 +36,8 @@ theorem _root_.Asymptotics.IsBigO.integrableAtFilter [IsMeasurablyGenerated l]
   use S, hS, hu.mono_measure <| Measure.restrict_mono (fun _ hx ↦ (hs_le hx).2) le_rfl
   refine ht.mono_set (fun _ hx ↦ (hs_le hx).1.2) |>.const_mul C' |>.2.mono ?_
   refine (ae_restrict_iff' hs_meas).mpr <| ae_of_all _ fun x hx => (hs x (hs_le hx).1.1).trans ?_
-  rewrite [norm_mul, hC']
+  rewrite [norm_mul]
   gcongr
-  apply le_max_left
 
 /-- Variant of `MeasureTheory.Integrable.mono` taking `f =O[⊤] (g)` instead of `‖f(x)‖ ≤ ‖g(x)‖` -/
 theorem _root_.Asymptotics.IsBigO.integrable (hfm : AEStronglyMeasurable f μ)
