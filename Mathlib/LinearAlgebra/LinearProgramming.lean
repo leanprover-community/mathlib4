9c80efa16b7b3e1bf1de4d6999dfa430e8c559e8
madvorak <martin.dvorak@matfyz.cz>
2024-02-05 10:06:35 +0100

    linear program is a special case of cone program

--- Mathlib/LinearAlgebra/LinearProgramming.lean
+++ Mathlib/LinearAlgebra/LinearProgramming.lean
@@ -15,7 +15,7 @@ TODO
 -/
 
 /-- Typically `M` is `ℝ^m` and `N` is `ℝ^n` -/
-structure LinearProgram (R V W M N : Type*) [OrderedRing R]
+structure ConeProgram (R V W M N : Type*) [OrderedRing R]
     [AddCommGroup V] [Module R V] [AddTorsor V M]
     [AddCommGroup W] [Module R W] [AddTorsor W N]
     where
@@ -28,35 +28,41 @@ structure LinearProgram (R V W M N : Type*) [OrderedRing R]
   /-- Cone defines nonnegative elements -/
   cone : PointedCone R W
 
+abbrev LinearProgram {R V W M N : Type*} [OrderedRing R]
+    [AddCommGroup V] [Module R V] [AddTorsor V M]
+    [OrderedAddCommGroup W] [Module R W] [OrderedSMul R W] [AddTorsor W N]
+    (l : M →ᵃ[R] N) (u : N) (o : M →ᵃ[R] R) :=
+  ConeProgram.mk l u o (PointedCone.positive R W)
+
 variable {R V W M N : Type*} [OrderedRing R]
     [AddCommGroup V] [Module R V] [AddTorsor V M]
     [AddCommGroup W] [Module R W] [AddTorsor W N]
 
 /-- `LP.primal = { x : M | LP.linmap x ≤ LP.upper }` -/
-def LinearProgram.primal (LP : LinearProgram R V W M N) :=
+def ConeProgram.primal (LP : ConeProgram R V W M N) :=
   { x : M | LP.upper -ᵥ LP.linmap x ∈ LP.cone }
 
 /-- `LP.dual = { g : N →ᵃ[R] R | LP.objective = g ∘ LP.linmap ∧ ∀ a, ∀ b, a ≤ b → g a ≤ g b }` -/
-def LinearProgram.dual (LP : LinearProgram R V W M N) :=
+def ConeProgram.dual (LP : ConeProgram R V W M N) :=
   { g : N →ᵃ[R] R | LP.objective = g ∘ LP.linmap ∧
       ∀ a : N, ∀ b : N, (b -ᵥ a) ∈ LP.cone → g a ≤ g b }
 
 -- From here on, we will need more assumptions than currently written
 
-theorem LinearProgram.weakDuality (LP : LinearProgram R V W M N)
+theorem ConeProgram.weakDuality (LP : ConeProgram R V W M N)
     {c : M} (hc : c ∈ LP.primal) {d : N →ᵃ[R] R} (hd : d ∈ LP.dual) :
     LP.objective c ≤ d LP.upper := by
-  simp_all [LinearProgram.primal, LinearProgram.dual]
+  simp_all [ConeProgram.primal, ConeProgram.dual]
 
 /-- Theorem 1.4.1.a, TODO we probably need more assumptions (finite-dimensional `M` and `N` ?) -/
-theorem LinearProgram.strongDuality (LP : LinearProgram R V W M N)
+theorem ConeProgram.strongDuality (LP : ConeProgram R V W M N)
     (hC : LP.primal.Nonempty) (hD : LP.dual.Nonempty) :
     ∃ c ∈ LP.primal, ∃ d ∈ LP.dual, LP.objective c = d LP.upper :=
   sorry
 
 /-- Theorem 1.4.1.b (TODO maybe add item (iii), which is easy,
     and item (iv), which holds when `N` is `ℝ^n` and `LP.cone` is the positive ortant) -/
-theorem LinearProgram.min_max (LP : LinearProgram R V W M N)
+theorem ConeProgram.min_max (LP : ConeProgram R V W M N)
     {c : M} (hc : c ∈ LP.primal) {d : N →ᵃ[R] R} (hd : d ∈ LP.dual) (hs : LP.cone.FG) :
     -- TODO maybe `hs` is not needed
     (∀ x ∈ LP.primal, LP.objective x ≤ LP.objective c) ∧ (∀ g ∈ LP.dual, d LP.upper ≤ g LP.upper) ↔
@@ -64,13 +70,13 @@ theorem LinearProgram.min_max (LP : LinearProgram R V W M N)
   sorry
 
 /-- Theorem 1.4.1.c(1) -/
-theorem LinearProgram.empty_dual (LP : LinearProgram R V W M N)
+theorem ConeProgram.empty_dual (LP : ConeProgram R V W M N)
     (hC : LP.primal.Nonempty) (hD : LP.dual = ∅) :
     ∀ r : R, ∃ d ∈ LP.dual, d LP.upper < r :=
   sorry
 
 /-- Theorem 1.4.1.c(2) -/
-theorem LinearProgram.empty_primal (LP : LinearProgram R V W M N)
+theorem ConeProgram.empty_primal (LP : ConeProgram R V W M N)
     (hC : LP.primal = ∅) (hD : LP.dual.Nonempty) :
     ∀ r : R, ∃ c ∈ LP.primal, r < LP.objective c :=
   sorry
