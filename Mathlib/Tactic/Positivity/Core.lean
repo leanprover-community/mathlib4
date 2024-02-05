123736f9ee7f7da2bbc02c88c2aeee37bd30c80d
Yaël Dillies <yael.dillies@gmail.com>
2024-02-05 16:09:33 +0000

    feat(Positivity): Strictness combinators Define combinators to turn strictness assumptions into proofs of nonnegativity or nonzeroness.
    These are useful to write `positivity` extensions where having `0 < a` doesn't help.
    
    We might want binary combinators in the future. Eg the pattern "Binary function `foo` is positive/nonnegative/nonzero if its inputs are" is very common.

--- Mathlib/Tactic/Positivity/Core.lean
+++ Mathlib/Tactic/Positivity/Core.lean
@@ -49,6 +49,20 @@ def Strictness.toString : Strictness zα pα e → String
   | nonzero _ => "nonzero"
   | none => "none"
 
+/-- Combinator to turn a `Strictness` information about `e` into a proof that `e` is nonnegative, if
+possible. -/
+def Strictness.toNonneg {e} : Strictness zα pα e → Option Q(0 ≤ $e)
+  | .positive pf => some q(le_of_lt $pf)
+  | .nonnegative pf => some pf
+  | _ => .none
+
+/-- Combinator to turn a `Strictness` information about `e` into a proof that `e` is nonzero, if
+possible. -/
+def Strictness.toNonzero {e} : Strictness zα pα e → Option Q($e ≠ 0)
+  | .positive pf => some q(ne_of_gt $pf)
+  | .nonzero pf => some pf
+  | _ => .none
+
 /-- An extension for `positivity`. -/
 structure PositivityExt where
   /-- Attempts to prove an expression `e : α` is `>0`, `≥0`, or `≠0`. -/
