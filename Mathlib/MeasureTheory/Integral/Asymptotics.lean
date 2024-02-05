fd1d18aa3353318185249de780361a28e0dc5d8d
L Lllvvuu <git@llllvvuu.dev>
2024-02-05 00:14:11 -0800

    address comments except NormedSpace one

--- Mathlib/MeasureTheory/Integral/Asymptotics.lean
+++ Mathlib/MeasureTheory/Integral/Asymptotics.lean
@@ -12,54 +12,37 @@ We establish integrability of `f` from `f = O(g)`.
 
 ## Main results
 
-* `Asymptotics.IsBigO.integrableAtFilter`: Given measurable functions `f`, `g` with `f = O[l] g`,
-  `IntegrableAtFilter g l μ` implies `IntegrableAtFilter f l μ`.
+* `Asymptotics.IsBigO.integrableAtFilter`: Given `f = O[l] g` on measurably generated `l`,
+  `IntegrableAtFilter g l μ`, and `f` `AEStronglyMeasurable` at `l`, `IntegrableAtFilter f l μ`.
 -/
 
 open Asymptotics MeasureTheory Set Filter
 
-variable {α E : Type*} [MeasurableSpace α] [NormedDivisionRing E] [NormedSpace ℝ E]
- {f g : α → E} {a b : α} {μ : Measure α} {l : Filter α}
+variable {α E F : Type*} [MeasurableSpace α] [NormedDivisionRing E] [NormedSpace ℝ E]
+  [NormedDivisionRing F] [NormedSpace ℝ F] {f : α → E} {g : α → F} {a b : α} {μ : Measure α}
+  {l : Filter α}
 
-private theorem _root_.Asymptotics.IsBigO.integrableAtFilter_aux
-    (hfm : AEStronglyMeasurable f μ)
-    (hae_restrict_iff : ∀ C' : E, ∀ s ∈ l, ∀ t ∈ l,
-      (∀ᵐ (x : α) ∂μ.restrict (s ∩ t), ‖f x‖ ≤ ‖C' * g x‖) ↔
-      ∀ᵐ (x : α) ∂μ, x ∈ (s ∩ t) → ‖f x‖ ≤ ‖C' * g x‖)
+/-- Given `f = O[l] g` on measurably generated `l`,
+`IntegrableAtFilter g l μ`, and `f` `AEStronglyMeasurable` at `l`, `IntegrableAtFilter f l μ`. -/
+theorem _root_.Asymptotics.IsBigO.integrableAtFilter [IsMeasurablyGenerated l]
+    (hfm : ∃ s ∈ l, AEStronglyMeasurable f (μ.restrict s))
     (hf : f =O[l] g) (hg : IntegrableAtFilter g l μ) : IntegrableAtFilter f l μ := by
   obtain ⟨C, hC⟩ := hf.bound
-  obtain ⟨C', hC'⟩ := exists_norm_eq E <| le_max_right C 0
+  obtain ⟨C', hC'⟩ := exists_norm_eq F <| le_max_right C 0
   obtain ⟨s, hsl, hs⟩ := hC.exists_mem
   obtain ⟨t, htl, ht⟩ := hg
-  use s ∩ t, inter_mem hsl htl, hfm.restrict
-  refine ht.mono_set (inter_subset_right s t) |>.const_mul C' |>.2.mono
-    <| (hae_restrict_iff C' s hsl t htl).mpr <| ae_of_all _ fun x hx => (hs x hx.1).trans ?_
+  obtain ⟨u, hul, hu⟩ := hfm
+  obtain ⟨S, hS, hs_meas, hs_le⟩ :=
+    IsMeasurablyGenerated.exists_measurable_subset <| inter_mem (inter_mem hsl htl) hul
+  use S, hS, hu.mono_measure <| Measure.restrict_mono (fun _ hx ↦ (hs_le hx).2) le_rfl
+  refine ht.mono_set (fun _ hx ↦ (hs_le hx).1.2) |>.const_mul C' |>.2.mono ?_
+  refine (ae_restrict_iff' hs_meas).mpr <| ae_of_all _ fun x hx => (hs x (hs_le hx).1.1).trans ?_
   rewrite [norm_mul, hC']
   gcongr
   apply le_max_left
-
-/-- `Asymptotics.IsBigO.integrableAtFilter` with measurability hypothesis on `l` instead of `g`. -/
-theorem _root_.Asymptotics.IsBigO.integrableAtFilter'
-    (hfm : AEStronglyMeasurable f μ) (hgm : ∀ s ∈ l, MeasurableSet s)
-    (hf : f =O[l] g) (hg : IntegrableAtFilter g l μ) : IntegrableAtFilter f l μ := by
-  refine hf.integrableAtFilter_aux hfm (fun _ s hs t ht ↦ ?_) hg
-  exact ae_restrict_iff' <| (hgm s hs).inter (hgm t ht)
 
 /-- Variant of `MeasureTheory.Integrable.mono` taking `f =O[⊤] (g)` instead of `‖f(x)‖ ≤ ‖g(x)‖` -/
 theorem _root_.Asymptotics.IsBigO.integrable (hfm : AEStronglyMeasurable f μ)
     (hf : f =O[⊤] g) (hg : Integrable g μ) : Integrable f μ := by
   rewrite [← IntegrableAtFilter.top] at *
-  refine hf.integrableAtFilter' hfm (fun _ hs ↦ ?_) hg
-  convert MeasurableSet.univ
-  exact mem_top.mp hs
-
-variable [MeasurableSpace E] [OpensMeasurableSpace E] [SecondCountableTopology E]
-
-/-- Given measurable functions `f`, `g` with `f = O[l] g`,
-`IntegrableAtFilter g l μ` implies `IntegrableAtFilter f l μ`. -/
-theorem _root_.Asymptotics.IsBigO.integrableAtFilter (hfm : Measurable f) (hgm : Measurable g)
-    (hf : f =O[l] g) (hg : IntegrableAtFilter g l μ) : IntegrableAtFilter f l μ := by
-  refine hf.integrableAtFilter_aux hfm.aestronglyMeasurable (fun C' _ _ _ _ ↦ ?_) hg
-  refine ae_restrict_iff (measurableSet_le hfm.norm ?_)
-  convert hgm.norm.const_mul ‖C'‖
-  exact norm_mul C' _
+  exact hf.integrableAtFilter ⟨univ, univ_mem, hfm.restrict⟩ hg
