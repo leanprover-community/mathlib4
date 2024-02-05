63416dacf717f252899ff6a33bf4b008efa690b1
Yury G. Kudryashov <urkud@urkud.name>
2024-02-05 03:07:09 -0600

    Move to a new file
    This way we don't have to rebuild the whole library every time

--- Mathlib/Tactic/Lint.lean
+++ Mathlib/Tactic/Lint.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Floris van Doorn
 -/
 import Std.Tactic.Lint
-import Mathlib.Lean.Expr.Basic
 
 /-!
 # Linters for Mathlib
@@ -35,38 +34,3 @@ Linter that checks whether a structure should be in Prop.
     let allProofs ← projs.allM (do isProof <| ← mkConstWithLevelParams <| declName ++ ·)
     unless allProofs do return none
     return m!"all fields are propositional but the structure isn't."
-
-/--
-Linter that checks for theorems that assume `[Decidable p]`
-but don't use this assumption in the type.
--/
-@[std_linter] def decidableClassical : Linter where
-  noErrorsFound := "No uses of `Decidable` arguments should be replaced with `classical`"
-  errorsFound := "USES OF `Decidable` SHOULD BE REPLACED WITH `classical` IN THE PROOF."
-  test declName := do
-    if (← isAutoDecl declName) then return none
-    let info ← getConstInfo declName
-    let type := info.type
-    let names :=
-      if Name.isPrefixOf `Decidable declName then #[`Fintype, `Encodable]
-      else if Name.isPrefixOf `Fintype declName
-        then #[`Decidable, `DecidableEq, `DecidablePred, `Encodable]
-        else if Name.isPrefixOf `Encodable declName
-        then #[`Decidable, `DecidableEq, `DecidablePred, `Fintype]
-        else #[`Decidable, `DecidableEq, `DecidablePred, `Fintype, `Encodable]
-    let mut impossibleArgs ← forallTelescopeReducing type fun args ty => do
-      let argTys ← args.mapM inferType
-      let ty ← ty.eraseProofs
-      return ← (args.zip argTys.zipWithIndex).filterMapM fun (arg, t, i) => do
-        unless names.any t.cleanupAnnotations.getForallBody.isAppOf do return none
-        let fv := arg.fvarId!
-        if ty.containsFVar fv then return none
-        if argTys[i+1:].any (·.containsFVar fv) then return none
-        return some (i, (← addMessageContextFull m!"argument {i+1} {arg} : {t}"))
-    if !(← isProp type) then
-      if let some e := info.value? then
-        impossibleArgs ← lambdaTelescope e fun args e => do
-          let e ← e.eraseProofs
-          return impossibleArgs.filter fun (k, _) => !e.containsFVar args[k]!.fvarId!
-    if impossibleArgs.isEmpty then return none
-    return some <| .joinSep (impossibleArgs.toList.map Prod.snd) ", "
