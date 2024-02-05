06962e7380510072c3f4260fda9df708374a730b
Yury G. Kudryashov <urkud@urkud.name>
2024-02-05 13:51:14 -0600

    Shake

--- Mathlib/Algebra/Order/Floor.lean
+++ Mathlib/Algebra/Order/Floor.lean
@@ -8,12 +8,12 @@ import Mathlib.Data.Int.Basic
 import Mathlib.Data.Int.Lemmas
 import Mathlib.Data.Int.CharZero
 import Mathlib.Data.Set.Intervals.Group
-import Mathlib.Data.Set.Lattice
 import Mathlib.Init.Data.Nat.Lemmas
 import Mathlib.Tactic.Abel
 import Mathlib.Tactic.Linarith
 import Mathlib.Tactic.Positivity
 import Mathlib.Data.Set.Basic
+import Mathlib.Order.GaloisConnection
 
 #align_import algebra.order.floor from "leanprover-community/mathlib"@"afdb43429311b885a7988ea15d0bac2aac80f69c"
 
