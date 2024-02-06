c5c054cf3fbdae9691b59453859c332ee133bea0
Eric Rodriguez <ericrboidi@gmail.com>
2024-02-06 12:31:07 +0000

    fix tests

--- test/positivity.lean
+++ test/positivity.lean
@@ -32,7 +32,7 @@ example {a : ℤ} (ha : 0 ≤ a) : 0 ≤ a := by positivity
 example {a : ℤ} (ha : a ≠ 0) : a ≠ 0 := by positivity
 example {a : ℤ} (ha : a = 0) : 0 ≤ a := by positivity
 
-variable {α} [Zero α] [PartialOrder α] {a : α}
+variable [Zero α] [PartialOrder α] {a : α}
 example (ha : 0 < a) : 0 < a := by positivity
 example (ha : 0 < a) : 0 ≤ a := by positivity
 example (ha : 0 < a) : a ≠ 0 := by positivity
