d7eae98ecc136c2523189f08a0b1ca10921a66fa
Ali Ramsey <s2158261@ed.ac.uk>
2024-02-06 17:35:05 +0000

    feat(RingTheory): bialgebra definition (#10076)
    Add definition of a bialgebra. For FLT.
    
    
    
    Co-authored-by: Kevin Buzzard <k.buzzard@imperial.ac.uk>
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -3115,6 +3115,7 @@ import Mathlib.RingTheory.Algebraic
 import Mathlib.RingTheory.AlgebraicIndependent
 import Mathlib.RingTheory.Artinian
 import Mathlib.RingTheory.Bezout
+import Mathlib.RingTheory.Bialgebra
 import Mathlib.RingTheory.Binomial
 import Mathlib.RingTheory.ChainOfDivisors
 import Mathlib.RingTheory.ClassGroup
