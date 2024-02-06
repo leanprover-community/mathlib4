fbd6710a8857d5db2575f65acba937647aa5289e
Yury G. Kudryashov <urkud@urkud.name>
2024-02-06 07:47:10 +0000

    chore(MeasureTheory): `Fintype` -> `Finite` (#10289)
    * Rename `hasFiniteIntegral_of_fintype` to `HasFiniteIntegral.of_finite`, generalize to `[Finite α]`, golf.
    * Rename `integrable_of_fintype` to `Integrable.of_finite`, generalize to `[Finite α]`.
    * Rename `SimpleFunc.ofFintype` to `SimpleFunc.ofFinite`, generalize to `[Finite α]`, use it to golf `SimpleFunc.ofIsEmpty`.
    * Rename `stronglyMeasurable_of_fintype` to `StronglyMeasurable.of_finite`, deprecate `stronglyMeasurable_of_isEmpty`.

--- Mathlib/MeasureTheory/Function/SimpleFunc.lean
+++ Mathlib/MeasureTheory/Function/SimpleFunc.lean
@@ -84,18 +84,18 @@ theorem apply_mk (f : α → β) (h h') (x : α) : SimpleFunc.mk f h h' x = f x 
   rfl
 #align measure_theory.simple_func.apply_mk MeasureTheory.SimpleFunc.apply_mk
 
-/-- Simple function defined on the empty type. -/
-def ofIsEmpty [IsEmpty α] : α →ₛ β where
-  toFun := isEmptyElim
-  measurableSet_fiber' x := Subsingleton.measurableSet
-  finite_range' := by simp [range_eq_empty]
-#align measure_theory.simple_func.of_is_empty MeasureTheory.SimpleFunc.ofIsEmpty
-
 /-- Simple function defined on a finite type. -/
-def ofFintype [Fintype α] [MeasurableSingletonClass α] (f : α → β) : α →ₛ β where
+def ofFinite [Finite α] [MeasurableSingletonClass α] (f : α → β) : α →ₛ β where
   toFun := f
-  measurableSet_fiber' x := Finite.measurableSet (toFinite (f ⁻¹' {x}))
+  measurableSet_fiber' x := (toFinite (f ⁻¹' {x})).measurableSet
   finite_range' := Set.finite_range f
+
+@[deprecated] -- Since 2024/02/05
+alias ofFintype := ofFinite
+
+/-- Simple function defined on the empty type. -/
+def ofIsEmpty [IsEmpty α] : α →ₛ β := ofFinite isEmptyElim
+#align measure_theory.simple_func.of_is_empty MeasureTheory.SimpleFunc.ofIsEmpty
 
 /-- Range of a simple function `α →ₛ β` as a `Finset β`. -/
 protected def range (f : α →ₛ β) : Finset β :=
