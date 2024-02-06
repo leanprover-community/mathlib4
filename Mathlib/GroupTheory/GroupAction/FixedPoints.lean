5ed49e79fae163dc008f235899bd39996b2b9a01
Adrien Burgun <adrien.burgun@orange.fr>
2024-02-06 14:57:03 +0100

    style(GroupTheory/GroupAction): small cleanups and golfing

--- Mathlib/GroupTheory/GroupAction/FixedPoints.lean
+++ Mathlib/GroupTheory/GroupAction/FixedPoints.lean
@@ -56,8 +56,8 @@ variable (α) in
 /-- In a multiplicative group action, the points fixed by `g` are also fixed by `g⁻¹` -/
 @[to_additive (attr := simp)
   "In an additive group action, the points fixed by `g` are also fixed by `g⁻¹`"]
-theorem fixedBy_inv_eq_fixedBy (g : G) : fixedBy α g⁻¹ = fixedBy α g := by
-  ext x
+theorem fixedBy_inv (g : G) : fixedBy α g⁻¹ = fixedBy α g := by
+  ext
   rw [mem_fixedBy, mem_fixedBy, inv_smul_eq_iff, eq_comm]
 
 @[to_additive]
@@ -69,12 +69,12 @@ theorem smul_mem_fixedBy_iff_mem_fixedBy {a : α} {g : G} :
 @[to_additive]
 theorem smul_inv_mem_fixedBy_iff_mem_fixedBy {a : α} {g : G} :
     g⁻¹ • a ∈ fixedBy α g ↔ a ∈ fixedBy α g := by
-  rw [← fixedBy_inv_eq_fixedBy, smul_mem_fixedBy_iff_mem_fixedBy, fixedBy_inv_eq_fixedBy]
+  rw [← fixedBy_inv, smul_mem_fixedBy_iff_mem_fixedBy, fixedBy_inv]
 
-@[to_additive minimalPeriod_eq_one_of_fixedBy]
-theorem minimalPeriod_eq_one_of_fixedBy {a : α} {g : G} (a_in_fixedBy : a ∈ fixedBy α g) :
-    Function.minimalPeriod (fun x => g • x) a = 1 := by
-  rwa [Function.minimalPeriod_eq_one_iff_isFixedPt]
+@[to_additive minimalPeriod_eq_one_iff_fixedBy]
+theorem minimalPeriod_eq_one_iff_fixedBy {a : α} {g : G} :
+    Function.minimalPeriod (fun x => g • x) a = 1 ↔ a ∈ fixedBy α g :=
+  Function.minimalPeriod_eq_one_iff_isFixedPt
 
 variable (α) in
 @[to_additive]
@@ -82,13 +82,13 @@ theorem fixedBy_subset_fixedBy_zpow (g : G) (j : ℤ) :
     fixedBy α g ⊆ fixedBy α (g ^ j) := by
   intro a a_in_fixedBy
   rw [mem_fixedBy, zpow_smul_eq_iff_minimalPeriod_dvd,
-    minimalPeriod_eq_one_of_fixedBy a_in_fixedBy, Nat.cast_one]
+    minimalPeriod_eq_one_iff_fixedBy.mpr a_in_fixedBy, Nat.cast_one]
   exact one_dvd j
 
 variable (M α) in
 @[to_additive (attr := simp)]
 theorem fixedBy_one_eq_univ : fixedBy α (1 : M) = Set.univ :=
-  Set.eq_univ_iff_forall.mpr (fun a => one_smul M a)
+  Set.eq_univ_iff_forall.mpr <| one_smul M
 
 variable (α) in
 @[to_additive]
@@ -98,18 +98,10 @@ theorem fixedBy_mul (m₁ m₂ : M) : fixedBy α m₁ ∩ fixedBy α m₂ ⊆ fi
 
 variable (α) in
 @[to_additive]
-theorem fixedBy_conj (g h : G) :
-    fixedBy α (h * g * h⁻¹) = (fun a => h⁻¹ • a) ⁻¹' fixedBy α g := by
-  ext a
-  rw [Set.mem_preimage, mem_fixedBy, mem_fixedBy]
-  repeat rw [mul_smul]
-  exact smul_eq_iff_eq_inv_smul h
-
-variable (α) in
-@[to_additive]
 theorem smul_fixedBy (g h: G) :
     h • fixedBy α g = fixedBy α (h * g * h⁻¹) := by
-  rw [fixedBy_conj, Set.preimage_smul, inv_inv]
+  ext a
+  simp_rw [Set.mem_smul_set_iff_inv_smul_mem, mem_fixedBy, mul_smul, smul_eq_iff_eq_inv_smul h]
 
 end FixedPoints
 
@@ -129,21 +121,11 @@ moved by `g`.
 @[to_additive "If a set `s : Set α` is in `fixedBy (Set α) g`, then all points of `s` will stay in
 `s` after being moved by `g`."]
 theorem set_mem_fixedBy_iff (s : Set α) (g : G) :
-    s ∈ fixedBy (Set α) g ↔ ∀ x, x ∈ s ↔ g • x ∈ s := by
-  constructor
-  · intro fixed x
-    refine ⟨
-      fun x_in_s => fixed ▸ Set.smul_mem_smul_set x_in_s,
-      fun gx_in_s => ?inv_in_set⟩
-    rwa [← fixed, Set.smul_mem_smul_set_iff] at gx_in_s
-  · intro closed
-    rw [← fixedBy_inv_eq_fixedBy]
-    ext x
-    rw [Set.mem_inv_smul_set_iff]
-    exact ⟨fun gxs => (closed x).mpr gxs, fun xs => (closed x).mp xs⟩
+    s ∈ fixedBy (Set α) g ↔ ∀ x, g • x ∈ s ↔ x ∈ s := by
+  simp_rw [mem_fixedBy, ← eq_inv_smul_iff, Set.ext_iff, Set.mem_inv_smul_set_iff, Iff.comm]
 
 theorem smul_mem_of_set_mem_fixedBy {s : Set α} {g : G} (s_in_fixedBy : s ∈ fixedBy (Set α) g)
-    {x : α} : x ∈ s ↔ g • x ∈ s := (set_mem_fixedBy_iff s g).mp s_in_fixedBy x
+    {x : α} : g • x ∈ s ↔ x ∈ s := (set_mem_fixedBy_iff s g).mp s_in_fixedBy x
 
 /--
 If `s ⊆ fixedBy α g`, then `g • s = s`, which means that `s ∈ fixedBy (Set α) g`.
@@ -157,22 +139,19 @@ Note that the reverse implication is in general not true, as `s ∈ fixedBy (Set
 weaker statement (it allows for points `x ∈ s` for which `g +ᵥ x ≠ x` and `g +ᵥ x ∈ s`)."]
 theorem set_mem_fixedBy_of_subset_fixedBy {s : Set α} {g : G} (s_ss_fixedBy : s ⊆ fixedBy α g) :
     s ∈ fixedBy (Set α) g := by
-  rw [← fixedBy_inv_eq_fixedBy]
+  rw [← fixedBy_inv]
   ext x
   rw [Set.mem_inv_smul_set_iff]
-  constructor
-  · intro gxs
-    rw [← fixedBy_inv_eq_fixedBy] at s_ss_fixedBy
-    rwa [← s_ss_fixedBy gxs, inv_smul_smul] at gxs
-  · intro xs
-    rwa [← s_ss_fixedBy xs] at xs
+  refine ⟨fun gxs => ?xs, fun xs => (s_ss_fixedBy xs).symm ▸ xs⟩
+  rw [← fixedBy_inv] at s_ss_fixedBy
+  rwa [← s_ss_fixedBy gxs, inv_smul_smul] at gxs
 
 theorem smul_subset_of_set_mem_fixedBy {s t : Set α} {g : G} (t_ss_s : t ⊆ s)
     (s_in_fixedBy : s ∈ fixedBy (Set α) g) : g • t ⊆ s := by
   intro x x_in_gt
   rw [Set.mem_smul_set_iff_inv_smul_mem] at x_in_gt
   apply t_ss_s at x_in_gt
-  rw [← fixedBy_inv_eq_fixedBy] at s_in_fixedBy
+  rw [← fixedBy_inv] at s_in_fixedBy
   rwa [← s_in_fixedBy, Set.smul_mem_smul_set_iff] at x_in_gt
 
 /-!
@@ -184,15 +163,14 @@ then no point or subset of `s` can be moved outside of `s` by the group action o
 @[to_additive "If `(fixedBy α g)ᶜ ⊆ s`, then `g` cannot move a point of `s` outside of `s`."]
 theorem set_mem_fixedBy_of_movedBy_subset {s : Set α} {g : G} (s_subset : (fixedBy α g)ᶜ ⊆ s) :
     s ∈ fixedBy (Set α) g := by
-  rw [← fixedBy_inv_eq_fixedBy]
+  rw [← fixedBy_inv]
   ext a
   rw [Set.mem_inv_smul_set_iff]
   by_cases a ∈ fixedBy α g
   case pos a_fixed =>
     rw [a_fixed]
   case neg a_moved =>
-    constructor
-    all_goals (intro; apply s_subset)
+    constructor <;> (intro; apply s_subset)
     · exact a_moved
     · rwa [Set.mem_compl_iff, smul_mem_fixedBy_iff_mem_fixedBy]
 
@@ -262,17 +240,13 @@ then `fixedBy α m = Set.univ` implies that `m = 1`. -/
 @[to_additive "If the additive action of `M` on `α` is faithful,
 then `fixedBy α m = Set.univ` implies that `m = 1`."]
 theorem fixedBy_eq_univ_iff_eq_one {m : M} : fixedBy α m = Set.univ ↔ m = 1 := by
-  constructor
-  · intro moved_empty
-    apply FaithfulSMul.eq_of_smul_eq_smul (α := α)
-    intro a
-    rw [one_smul]
-    by_contra ma_ne_a
-    rw [← mem_fixedBy, moved_empty] at ma_ne_a
-    exact ma_ne_a (Set.mem_univ a)
-  · intro eq_one
-    rw [eq_one]
-    exact fixedBy_one_eq_univ α M
+  refine ⟨fun moved_empty => ?eq_one, fun eq_one => eq_one ▸ fixedBy_one_eq_univ α M⟩
+  apply FaithfulSMul.eq_of_smul_eq_smul (α := α)
+  intro a
+  rw [one_smul]
+  by_contra ma_ne_a
+  rw [← mem_fixedBy, moved_empty] at ma_ne_a
+  exact ma_ne_a (Set.mem_univ a)
 
 /--
 If the image of the `(fixedBy α g)ᶜ` set by the pointwise action of `h: G`
