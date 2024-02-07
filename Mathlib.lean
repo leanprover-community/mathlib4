01a069281257343b49518fcd439429ad67cd1492
Parcly Taxel <reddeloostw@gmail.com>
2024-02-07 19:16:02 +0800

    oops

--- Mathlib.lean
+++ Mathlib.lean
@@ -1402,6 +1402,7 @@ import Mathlib.Combinatorics.SimpleGraph.Connectivity.Basic
 import Mathlib.Combinatorics.SimpleGraph.Connectivity.Bridge
 import Mathlib.Combinatorics.SimpleGraph.Connectivity.CircuitCycle
 import Mathlib.Combinatorics.SimpleGraph.Connectivity.Subgraph
+import Mathlib.Combinatorics.SimpleGraph.Connectivity.TrailPath
 import Mathlib.Combinatorics.SimpleGraph.Dart
 import Mathlib.Combinatorics.SimpleGraph.DegreeSum
 import Mathlib.Combinatorics.SimpleGraph.Density
