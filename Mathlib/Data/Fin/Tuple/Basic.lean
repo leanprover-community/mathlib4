0e7a53d6547fb02a3c8fc7c901248efa3d4e3043
Wrenna Robson <wren.robson@gmail.com>
2024-02-07 22:43:18 +0000

    feat(Data/Fin/OrderHom): Factor out `succAbove` and `predAbove` from `Data.Fin.Basic`. (#10166)
    Move `succAbove` and `predAbove` to their own file, updating their documentation and adding an `OrderHom` instance for `predAbove`, `predAboveOrderHom`.

--- Mathlib/Data/Fin/Tuple/Basic.lean
+++ Mathlib/Data/Fin/Tuple/Basic.lean
@@ -3,7 +3,7 @@ Copyright (c) 2019 Floris van Doorn. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Floris van Doorn, Yury Kudryashov, Sébastien Gouëzel, Chris Hughes
 -/
-import Mathlib.Data.Fin.Basic
+import Mathlib.Data.Fin.OrderHom
 import Mathlib.Data.Pi.Lex
 import Mathlib.Data.Set.Intervals.Basic
 
