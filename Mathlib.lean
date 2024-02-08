a1d9afe78d3b8306b06260f7428180b6fbed74c1
Joël Riou <joel.riou@universite-paris-saclay.fr>
2024-02-08 08:31:06 +0000

    feat(CategoryTheory/Functor): definition of Kan extensions (#10195)
    This PR introduces basic definitions for Kan extensions of functors. It prepares for future development of derived functors and a refactor of the file `CategoryTheory.Limits.KanExtension`.
    
    
    
    Co-authored-by: Joël Riou <37772949+joelriou@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1057,6 +1057,7 @@ import Mathlib.CategoryTheory.Functor.Flat
 import Mathlib.CategoryTheory.Functor.FullyFaithful
 import Mathlib.CategoryTheory.Functor.Functorial
 import Mathlib.CategoryTheory.Functor.Hom
+import Mathlib.CategoryTheory.Functor.KanExtension.Basic
 import Mathlib.CategoryTheory.Functor.ReflectsIso
 import Mathlib.CategoryTheory.Functor.Trifunctor
 import Mathlib.CategoryTheory.Galois.Basic
