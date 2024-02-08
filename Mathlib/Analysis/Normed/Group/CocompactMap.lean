50be8dc528f6eddaf38565dd752fe5592f8dd53b
Moritz Doll <moritz.doll@googlemail.com>
2024-02-09 00:19:44 +1100

    unused imports linter

--- Mathlib/Analysis/Normed/Group/CocompactMap.lean
+++ Mathlib/Analysis/Normed/Group/CocompactMap.lean
@@ -4,8 +4,8 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Moritz Doll
 -/
 
+import Mathlib.Analysis.Normed.Group.Basic
 import Mathlib.Topology.ContinuousFunction.CocompactMap
-import Mathlib.Analysis.NormedSpace.Basic
 
 /-!
 # CocompactMaps in normed groups
