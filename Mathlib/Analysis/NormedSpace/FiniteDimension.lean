512a89d7dca406db8497d7816ae9ba3045fd481f
damiano <adomani@gmail.com>
2024-02-08 09:11:41 +0000

    Deprecate allowing auto-replacement (#10302)
    Following [these](https://leanprover.zulipchat.com/#narrow/stream/287929-mathlib4/topic/Thank.20you.20for.20the.20deprecation.20warnings!/near/420078161) [Zulip](https://leanprover.zulipchat.com/#narrow/stream/270676-lean4/topic/machineApplicableDeprecated.20tag.20attribute/near/397630551) discussions, I realised that my deprecation script produced a deprecation syntax that did not allow for auto-replacement in Sébastien's #10185.
    
    This PR fixes the deprecation statements, allowing self-correction: 119 times I replaced
    
    `@[deprecated xxx] --> @[deprecated]`.

--- Mathlib/Analysis/NormedSpace/FiniteDimension.lean
+++ Mathlib/Analysis/NormedSpace/FiniteDimension.lean
@@ -289,7 +289,7 @@ theorem Basis.opNNNorm_le {ι : Type*} [Fintype ι] (v : Basis ι 𝕜 E) {u : E
       _ = Fintype.card ι • ‖φ‖₊ * M * ‖e‖₊ := by simp only [smul_mul_assoc, mul_right_comm]
 #align basis.op_nnnorm_le Basis.opNNNorm_le
 
-@[deprecated Basis.opNNNorm_le]
+@[deprecated]
 alias Basis.op_nnnorm_le :=
   Basis.opNNNorm_le -- deprecated on 2024-02-02
 
@@ -299,7 +299,7 @@ theorem Basis.opNorm_le {ι : Type*} [Fintype ι] (v : Basis ι 𝕜 E) {u : E �
   simpa using NNReal.coe_le_coe.mpr (v.opNNNorm_le ⟨M, hM⟩ hu)
 #align basis.op_norm_le Basis.opNorm_le
 
-@[deprecated Basis.opNorm_le]
+@[deprecated]
 alias Basis.op_norm_le :=
   Basis.opNorm_le -- deprecated on 2024-02-02
 
@@ -313,7 +313,7 @@ theorem Basis.exists_opNNNorm_le {ι : Type*} [Finite ι] (v : Basis ι 𝕜 E) 
       (v.opNNNorm_le M hu).trans <| mul_le_mul_of_nonneg_right (le_max_left _ _) (zero_le M)⟩
 #align basis.exists_op_nnnorm_le Basis.exists_opNNNorm_le
 
-@[deprecated Basis.exists_opNNNorm_le]
+@[deprecated]
 alias Basis.exists_op_nnnorm_le :=
   Basis.exists_opNNNorm_le -- deprecated on 2024-02-02
 
@@ -327,7 +327,7 @@ theorem Basis.exists_opNorm_le {ι : Type*} [Finite ι] (v : Basis ι 𝕜 E) :
   simpa using h ⟨M, hM⟩ H
 #align basis.exists_op_norm_le Basis.exists_opNorm_le
 
-@[deprecated Basis.exists_opNorm_le]
+@[deprecated]
 alias Basis.exists_op_norm_le :=
   Basis.exists_opNorm_le -- deprecated on 2024-02-02
 
