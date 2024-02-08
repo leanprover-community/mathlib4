f4188dfa95d12b55eaf2ae9cc638dd20257264ad
JADekker <114015169+JADekker@users.noreply.github.com>
2024-02-08 17:08:45 +0100

    add

---
+++ testing.lean
@@ -0,0 +1,11 @@
+import Mathlib
+variable {α : Type*} [MetricSpace α] [CompactSpace α]
+#synth TopologicalSpace.SeparableSpace α
+#synth SecondCountableTopology α
+#synth SecondCountableTopology α
+
+
+
+
+variable {α : Type*} [MetricSpace α] [LindelofSpace α]
+#synth TopologicalSpace.SeparableSpace α
