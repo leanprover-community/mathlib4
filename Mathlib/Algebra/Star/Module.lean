180df36c8f48141ef6c8d7c9085c656914748cd0
Jireh Loreaux <loreaujy@gmail.com>
2024-02-09 02:03:09 -0600

    add iff version

--- Mathlib/Algebra/Star/Module.lean
+++ Mathlib/Algebra/Star/Module.lean
@@ -202,4 +202,8 @@ protected lemma IsSelfAdjoint.algebraMap {r : R} (hr : IsSelfAdjoint r) :
     IsSelfAdjoint (algebraMap R A r) := by
   simpa using congr(algebraMap R A $(hr.star_eq))
 
+lemma isSelfAdjoint_algebraMap_iff {r : R} (h : Function.Injective (algebraMap R A)) :
+    IsSelfAdjoint (algebraMap R A r) ↔ IsSelfAdjoint r :=
+  ⟨fun hr ↦ h <| algebraMap_star_comm r (A := A) ▸ hr.star_eq, IsSelfAdjoint.algebraMap A⟩
+
 end algebraMap
