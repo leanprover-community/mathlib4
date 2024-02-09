c66bd976c5c23c54ef3a1576d0183d98d228ab89
Johan Commelin <johan@commelin.net>
2024-02-09 11:31:56 +0100

    wip

--- Mathlib/Algebra/Lie/CartanExists.lean
+++ Mathlib/Algebra/Lie/CartanExists.lean
@@ -19,6 +19,7 @@ variable [Module.Finite R M] [Module.Free R M]
 
 open FiniteDimensional Module.Free Polynomial
 
+local notation "φ" => LieModule.toEndomorphism R L M
 namespace engel_le_engel
 
 variable (x y : L)
@@ -39,6 +40,37 @@ lemma lieCharpoly₁_natDegree : (lieCharpoly₁ R M x y).natDegree = finrank R 
     rw [lieCharpoly₁, (lieCharpoly_monic _ _).natDegree_map, lieCharpoly_natDegree,
       finrank_eq_card_chooseBasisIndex]
 
+lemma lieCharpoly₁_map_eval (r : R) :
+    (lieCharpoly₁ R M x y).map (evalRingHom r) = (φ (r • y + x)).charpoly := by
+  rw [lieCharpoly₁, map_map, ← lieCharpoly_map (chooseBasis R L) (chooseBasis R M)]
+  congr 1
+  apply MvPolynomial.ringHom_ext
+  · intro;
+    simp? [algebraMap_apply] says
+      simp only [RingHom.coe_comp, coe_evalRingHom, RingHom.coe_coe,
+        Function.comp_apply, MvPolynomial.aeval_C, algebraMap_apply, Algebra.id.map_eq_id,
+        RingHom.id_apply, eval_C, map_add, map_smul, Finsupp.coe_add, Finsupp.coe_smul,
+        MvPolynomial.eval_C]
+  · intro;
+    simp? [mul_comm r] says
+      simp only [RingHom.coe_comp, coe_evalRingHom, RingHom.coe_coe,
+        Function.comp_apply, MvPolynomial.aeval_X, eval_add, eval_mul, eval_C, eval_X, map_add,
+        map_smul, Finsupp.coe_add, Finsupp.coe_smul, MvPolynomial.eval_X, Pi.add_apply,
+        Pi.smul_apply, smul_eq_mul, mul_comm r]
+
+lemma toEndomorphism_charpoly_eq_X_pow_iff :
+    (φ x).charpoly = X ^ finrank R M ↔ ∀ m : M, ∃ (n : ℕ), ((φ x) ^ n) m = 0 := by
+  constructor
+  · intro h m
+    use finrank R M
+    suffices (φ x) ^ finrank R M = 0 by simp only [this, LinearMap.zero_apply]
+    simpa only [h, map_pow, aeval_X] using (φ x).aeval_self_charpoly
+  · sorry -- We don't need this direction
+
+lemma lieCharpoly₁_coeff_natDegree (i j : ℕ) (hij : i + j = finrank R M) :
+    ((lieCharpoly₁ R M x y).coeff i).natDegree = j := by
+  sorry
+
 end engel_le_engel
 
 end CommRing
@@ -50,25 +82,32 @@ variable [Field K] [LieRing L] [LieAlgebra K L] [Module.Finite K L]
 
 open FiniteDimensional LieSubalgebra Module.Free Polynomial
 
-open LieSubalgebra Polynomial in
+open LieSubalgebra Polynomial engel_le_engel in
 lemma engel_le_engel (U : LieSubalgebra K L) (x : L) (hx : x ∈ U) (hUx : U ≤ engel K x)
     (hmin : ∀ y : L, y ∈ U → engel K y ≤ engel K x → engel K y = engel K x)
     (y : L) (hy : y ∈ U) :
     engel K x ≤ engel K y := by
-  -- let E : LieSubmodule K U L :=
-  -- { engel K x with
-  --   lie_mem := by rintro ⟨u, hu⟩ y hy; exact (engel K x).lie_mem (hUx hu) hy }
-  -- let Q := L ⧸ E
-  -- let BU := chooseBasis K U
-  -- let BE := chooseBasis K E
-  -- let BQ := chooseBasis K Q
-  -- let r := finrank K E
-  -- let χ : U → Polynomial (K[X]) := fun u₁ ↦
-  --   (lieCharpoly BU BE).map (MvPolynomial.aeval fun i ↦
-  --     C (BU.repr ⟨x, hx⟩ i) + C (BU.repr u₁ i) * X).toRingHom
-  -- let ψ : U → Polynomial (K[X]) := fun u₁ ↦
-  --   (lieCharpoly BU BQ).map (MvPolynomial.aeval fun i ↦
-  --     C (BU.repr ⟨x, hx⟩ i) + C (BU.repr u₁ i) * X).toRingHom
+  let E : LieSubmodule K U L :=
+  { engel K x with
+    lie_mem := by rintro ⟨u, hu⟩ y hy; exact (engel K x).lie_mem (hUx hu) hy }
+  let Q := L ⧸ E
+  let BU := chooseBasis K U
+  let BE := chooseBasis K E
+  let BQ := chooseBasis K Q
+  let r := finrank K E
+  let χ : U → Polynomial (K[X]) := fun u₁ ↦ lieCharpoly₁ K E ⟨x, hx⟩ u₁
+  let ψ : U → Polynomial (K[X]) := fun u₁ ↦ lieCharpoly₁ K Q ⟨x, hx⟩ u₁
+  suffices ∀ u, χ u = X ^ r by
+    specialize this (⟨y, hy⟩ - ⟨x, hx⟩)
+    apply_fun (fun p ↦ p.map (evalRingHom 1)) at this
+    simp only [lieCharpoly₁_map_eval, coe_bracket_of_module, one_smul, sub_add_cancel,
+      Polynomial.map_pow, map_X, toEndomorphism_charpoly_eq_X_pow_iff] at this
+    intro z hz
+    obtain ⟨n, hn⟩ := this ⟨z, hz⟩
+    rw [mem_engel_iff]
+    use n
+    rw [Subtype.ext_iff, LieSubmodule.coe_toEndomorphism_pow] at hn
+    simpa only [toEndomorphism_mk, ZeroMemClass.coe_zero] using hn
   sorry
 
 end Field
