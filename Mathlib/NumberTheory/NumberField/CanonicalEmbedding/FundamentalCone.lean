43ca0130cde619534b08c357684fd43ea41b05ef
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2024-02-09 16:12:12 +0100

    feat: coe_toLinearMap, coe_toIntAlgHom

--- Mathlib/NumberTheory/NumberField/CanonicalEmbedding/FundamentalCone.lean
+++ Mathlib/NumberTheory/NumberField/CanonicalEmbedding/FundamentalCone.lean
@@ -262,7 +262,7 @@ def integerSet : Set (mixedSpace K) :=
 theorem mem_integerSet {a : mixedSpace K} :
     a ∈ integerSet K ↔ a ∈ fundamentalCone K ∧ ∃ x : 𝓞 K, mixedEmbedding K x = a := by
   simp only [integerSet, Set.mem_inter_iff, SetLike.mem_coe, LinearMap.mem_range,
-    AlgHom.toLinearMap_apply, RingHom.toIntAlgHom_coe, RingHom.coe_comp, Function.comp_apply]
+    AlgHom.toLinearMap_apply, RingHom.coe_toIntAlgHom, RingHom.coe_comp, Function.comp_apply]
 
 /-- If `a` is in `integerSet`, then there is a *unique* algebraic integer in `𝓞 K` such
 that `mixedEmbedding K x = a`. -/
@@ -483,7 +483,7 @@ theorem mem_idealSet :
       mixedEmbedding K (a : 𝓞 K) = x := by
   simp_rw [idealSet, Set.mem_inter_iff, idealLattice, SetLike.mem_coe, FractionalIdeal.coe_mk0,
     LinearMap.mem_range, LinearMap.coe_comp, LinearMap.coe_restrictScalars, coe_subtype,
-    Function.comp_apply, AlgHom.toLinearMap_apply, RingHom.toIntAlgHom_coe, Subtype.exists,
+    Function.comp_apply, AlgHom.toLinearMap_apply, RingHom.coe_toIntAlgHom, Subtype.exists,
     FractionalIdeal.mem_coe, FractionalIdeal.mem_coeIdeal, exists_prop', nonempty_prop,
     exists_exists_and_eq_and]
 
