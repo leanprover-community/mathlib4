92062a0ff78b91116f38075870383c7a7893ff08
Noam Atar <atarnoam@gmail.com>
2024-02-10 19:37:32 +0200

    changed proofs of lemmas

--- Mathlib/MeasureTheory/Group/ModularCharacter.lean
+++ Mathlib/MeasureTheory/Group/ModularCharacter.lean
@@ -19,16 +19,16 @@ open Classical
 
 /-- Definition of the modularCharacter map -/
 @[to_additive addModularCharacter_map "Definition of the addModularCharacter map"]
-noncomputable def modularCharacter_map (μ : Measure G) [IsHaarMeasure μ] [InnerRegular μ] :
-    G → ℝ≥0 := fun g => haarScalarFactor (map (· * g) μ) μ
+noncomputable def modularCharacter_map (μ : Measure G) [IsHaarMeasure μ] : G → ℝ≥0 :=
+  fun g => haarScalarFactor (map (· * g) μ) μ
 
 @[to_additive]
 lemma eq_modularCharacter_smul (μ : Measure G) [IsHaarMeasure μ] [InnerRegular μ]
     (g : G) : (map (· * g) μ) = modularCharacter_map μ g • μ :=
   isMulLeftInvariant_eq_smul_of_innerRegular _ _
 
-@[to_additive addModularCharacter_ge_zero]
-lemma modularCharacter_ge_zero (μ : Measure G) [IsHaarMeasure μ] [InnerRegular μ] (g : G) :
+@[to_additive addModularCharacter_pos]
+lemma modularCharacter_pos (μ : Measure G) [IsHaarMeasure μ] (g : G) :
     0 < modularCharacter_map μ g := haarScalarFactor_pos_of_isOpenPosMeasure _ μ
 
 /-- The modular character does not depend on the choice of the haar measure. -/
@@ -38,38 +38,39 @@ theorem modularCharacter_map_eq (μ μ': Measure G) [IsHaarMeasure μ] [IsHaarMe
     [InnerRegular μ] [InnerRegular μ'] : modularCharacter_map μ' = modularCharacter_map μ := by
   ext g
   rw [modularCharacter_map, NNReal.coe_eq]
-  apply haarScalarFactor_of_eq_smul (map (· * g) μ') μ'
-  calc
-    map (· * g) μ' = map (· * g) ((haarScalarFactor μ' μ) • μ) := by
-      rw [←isMulLeftInvariant_eq_smul_of_innerRegular]
-    _ = haarScalarFactor μ' μ • (map (· * g) μ) := by rw [Measure.map_smul_nnreal]
-    _ = haarScalarFactor μ' μ • (modularCharacter_map μ g • μ) := by rw [eq_modularCharacter_smul]
-    _ = modularCharacter_map μ g • (haarScalarFactor μ' μ • μ) := by rw [smul_algebra_smul_comm]
-    _ = modularCharacter_map μ g • μ' := by rw [←isMulLeftInvariant_eq_smul_of_innerRegular]
+  have : map (· * g) μ' = modularCharacter_map μ g • μ' := by
+    calc
+      map (· * g) μ' = map (· * g) ((haarScalarFactor μ' μ) • μ) := by
+        rw [←isMulLeftInvariant_eq_smul_of_innerRegular]
+      _ = haarScalarFactor μ' μ • (map (· * g) μ) := by rw [Measure.map_smul_nnreal]
+      _ = haarScalarFactor μ' μ • (modularCharacter_map μ g • μ) := by rw [eq_modularCharacter_smul]
+      _ = modularCharacter_map μ g • (haarScalarFactor μ' μ • μ) := by rw [smul_algebra_smul_comm]
+      _ = modularCharacter_map μ g • μ' := by rw [←isMulLeftInvariant_eq_smul_of_innerRegular]
+  simp [this]
 
 -- theorem test (g h : G) : (· * (g * h)) = (· * h) ∘ (· * g) := by simp only [comp_mul_right]
 
-noncomputable def modularCharacter (μ : Measure G) [IsHaarMeasure μ] [InnerRegular μ] :
-    G →* ℝ≥0 where
-  toFun := modularCharacter_map μ
-  map_one' := by simp [modularCharacter_map, haarScalarFactor_self_eq_one μ]
-  map_mul' := fun g h => by
-    haveI : IsHaarMeasure (map ((fun x ↦ x * h) ∘ fun x ↦ x * g) μ) := by
-      simp
-      exact inferInstance
-    have mul_g_meas : Measurable (· * g) := Measurable.mul_const (fun ⦃t⦄ a ↦ a) g
-    have mul_h_meas : Measurable (· * h) := Measurable.mul_const (fun ⦃t⦄ a ↦ a) h
-    haveI : IsHaarMeasure ((map (· * h) (modularCharacter_map μ g • μ))) := by
-      simp
-      refine IsHaarMeasure.smul (_ : modularCharacter_map μ g ≠ ∞) _
-    calc
-      modularCharacter_map μ (g * h) = haarScalarFactor (map (· * (g * h)) μ) μ := rfl
-      _ = haarScalarFactor (map ((· * h) ∘ (· * g)) μ) μ := by simp
-      _ = haarScalarFactor (map (· * h) (map (· * g) μ)) μ := by
-        simp_rw [←map_map mul_h_meas mul_g_meas]
-      _ = haarScalarFactor (map (· * h) (modularCharacter_map μ g • μ)) μ := by
-        simp_rw [← eq_modularCharacter_smul _ _]
-      _ = modularCharacter_map μ g * modularCharacter_map μ h := by sorry
+-- noncomputable def modularCharacter (μ : Measure G) [IsHaarMeasure μ] [InnerRegular μ] :
+--     G →* ℝ≥0 where
+--   toFun := modularCharacter_map μ
+--   map_one' := by simp [modularCharacter_map, haarScalarFactor_self_eq_one μ]
+--   map_mul' := fun g h => by
+--     haveI : IsHaarMeasure (map ((fun x ↦ x * h) ∘ fun x ↦ x * g) μ) := by
+--       simp
+--       exact inferInstance
+--     have mul_g_meas : Measurable (· * g) := Measurable.mul_const (fun ⦃t⦄ a ↦ a) g
+--     have mul_h_meas : Measurable (· * h) := Measurable.mul_const (fun ⦃t⦄ a ↦ a) h
+--     haveI : IsHaarMeasure ((map (· * h) (modularCharacter_map μ g • μ))) := by
+--       simp
+--       refine IsHaarMeasure.smul (_ : modularCharacter_map μ g ≠ ∞) _
+--     calc
+--       modularCharacter_map μ (g * h) = haarScalarFactor (map (· * (g * h)) μ) μ := rfl
+--       _ = haarScalarFactor (map ((· * h) ∘ (· * g)) μ) μ := by simp
+--       _ = haarScalarFactor (map (· * h) (map (· * g) μ)) μ := by
+--         simp_rw [←map_map mul_h_meas mul_g_meas]
+--       _ = haarScalarFactor (map (· * h) (modularCharacter_map μ g • μ)) μ := by
+--         simp_rw [← eq_modularCharacter_smul _ _]
+--       _ = modularCharacter_map μ g * modularCharacter_map μ h := by sorry
 
 
 
