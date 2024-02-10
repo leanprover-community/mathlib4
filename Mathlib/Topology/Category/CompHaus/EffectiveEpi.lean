120334db6bd1644200a0101ca523070340f20491
Dagur Tómas Ásgeirsson <dagurtomas@gmail.com>
2024-02-10 10:41:51 +0000

    chore(CategoryTheory/Sites): reorganise the material related to the coherent, regular and extensive topologies (#9944)
    This PR adds essentially no new material (except the names `regularTopology` and `extensiveTopology` which were missing). It only reorganises the material that was previously in `CategoryTheory/Sites/Coherent/Basic` and `CategoryTheory/Sites/Coherent/RegularExtensive`, spreading it across six files in the `Coherent ` folder.
    
    - [x] depends on: #9920

--- Mathlib/Topology/Category/CompHaus/EffectiveEpi.lean
+++ Mathlib/Topology/Category/CompHaus/EffectiveEpi.lean
@@ -3,7 +3,7 @@ Copyright (c) 2023 Adam Topaz. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Adam Topaz, Dagur Asgeirsson
 -/
-import Mathlib.CategoryTheory.Sites.Coherent.RegularExtensive
+import Mathlib.CategoryTheory.Sites.Coherent.Comparison
 import Mathlib.Topology.Category.CompHaus.Limits
 /-!
 
