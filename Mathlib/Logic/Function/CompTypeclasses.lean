0f402c8ad31dab766313ac226808cefd558c4c8c
Antoine Chambert-Loir <antoine.chambert-loir@math.univ-paris-diderot.fr>
2024-02-10 11:18:13 +0100

    change import : Mathlib.Init.Function

--- Mathlib/Logic/Function/CompTypeclasses.lean
+++ Mathlib/Logic/Function/CompTypeclasses.lean
@@ -4,8 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Antoine Chambert-Loir
 -/
 
-import Mathlib.Logic.Function.Basic
-
+import Mathlib.Init.Function
 
 /-!
 # Propositional typeclasses on several monoid homs
