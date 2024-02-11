18b534f53c5b732d3457ce4dd46aac10469e5c60
Christopher Hoskin <christopher.hoskin@gmail.com>
2024-02-11 22:08:40 +0000

    Deduce BilinearForm/TensorProduct results from TensorProduct/LinearMap

--- Mathlib/LinearAlgebra/BilinearForm/TensorProduct.lean
+++ Mathlib/LinearAlgebra/BilinearForm/TensorProduct.lean
@@ -59,36 +59,36 @@ def tensorDistrib : BilinForm A M₁ ⊗[R] BilinForm R M₂ →ₗ[A] BilinForm
 theorem tensorDistrib_tmul (B₁ : BilinForm A M₁) (B₂ : BilinForm R M₂) (m₁ : M₁) (m₂ : M₂)
     (m₁' : M₁) (m₂' : M₂) :
     tensorDistrib R A (B₁ ⊗ₜ B₂) (m₁ ⊗ₜ m₂) (m₁' ⊗ₜ m₂')
-      = B₂ m₂ m₂' • B₁ m₁ m₁' :=
-  rfl
+      = B₂ m₂ m₂' • B₁ m₁ m₁' := LinearMap.tensorDistrib_tmul (toLin B₁) (toLin B₂) m₁ m₂ m₁' m₂'
 #align bilin_form.tensor_distrib_tmul BilinForm.tensorDistrib_tmulₓ
 
 /-- The tensor product of two bilinear forms, a shorthand for dot notation. -/
 @[reducible]
 protected def tmul (B₁ : BilinForm A M₁) (B₂ : BilinForm R M₂) : BilinForm A (M₁ ⊗[R] M₂) :=
-  tensorDistrib R A (B₁ ⊗ₜ[R] B₂)
+  LinearMap.toBilin (LinearMap.tmul (toLin B₁) (toLin B₂))
 #align bilin_form.tmul BilinForm.tmul
 
 attribute [ext] TensorProduct.ext in
 /-- A tensor product of symmetric bilinear forms is symmetric. -/
-lemma IsSymm.tmul {B₁ : BilinForm A M₁} {B₂ : BilinForm R M₂}
-    (hB₁ : B₁.IsSymm) (hB₂ : B₂.IsSymm) : (B₁.tmul B₂).IsSymm := LinearMap.IsSymm.tmul hB₁ hB₂
+lemma IsSymm.tmul {B₁ : BilinForm A M₁} {B₂ : BilinForm R M₂} (hB₁ : B₁.IsSymm) (hB₂ : B₂.IsSymm) :
+    (B₁.tmul B₂).IsSymm := LinearMap.IsSymm.tmul hB₁ hB₂
 
 variable (A) in
 /-- The base change of a bilinear form. -/
 protected def baseChange (B : BilinForm R M₂) : BilinForm A (A ⊗[R] M₂) :=
-  BilinForm.tmul (R := R) (A := A) (M₁ := A) (M₂ := M₂) (LinearMap.toBilin <| LinearMap.mul A A) B
+  LinearMap.toBilin (LinearMap.baseChange₂ A (toLin B))
 
 @[simp]
 theorem baseChange_tmul (B₂ : BilinForm R M₂) (a : A) (m₂ : M₂)
     (a' : A) (m₂' : M₂) :
     B₂.baseChange A (a ⊗ₜ m₂) (a' ⊗ₜ m₂') = (B₂ m₂ m₂') • (a * a') :=
-  rfl
+  LinearMap.baseChange₂_tmul (toLin B₂) a m₂ a' m₂'
+
 
 variable (A) in
 /-- The base change of a symmetric bilinear form is symmetric. -/
 lemma IsSymm.baseChange {B₂ : BilinForm R M₂} (hB₂ : B₂.IsSymm) : (B₂.baseChange A).IsSymm :=
-  IsSymm.tmul mul_comm hB₂
+  LinearMap.IsSymm.baseChange _ hB₂
 
 end CommSemiring
 
