88b49a887d1795faa63038aafd33fc98c32cb49c
Yaël Dillies <yael.dillies@gmail.com>
2024-02-11 14:11:14 +0100

    shake

--- Mathlib/Analysis/Complex/Angle.lean
+++ Mathlib/Analysis/Complex/Angle.lean
@@ -3,8 +3,8 @@ Copyright (c) 2024 Yaël Dillies. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yaël Dillies
 -/
-import Mathlib.Geometry.Euclidean.Angle.Unoriented.Affine
 import Mathlib.Analysis.SpecialFunctions.Complex.Arg
+import Mathlib.Geometry.Euclidean.Angle.Unoriented.Basic
 
 /-!
 # Angle between complex numbers
