bc0533d76f18a54b65a7f580b7315b25ea17df36
Yaël Dillies <yael.dillies@gmail.com>
2024-02-11 23:08:17 +0000

    match the universe

--- Mathlib/Tactic/Positivity/Basic.lean
+++ Mathlib/Tactic/Positivity/Basic.lean
@@ -415,28 +415,23 @@ private alias ⟨_, num_ne_zero_of_ne_zero⟩ := num_ne_zero
 such that `positivity` successfully recognises `a`. -/
 @[positivity Rat.num _]
 def evalRatNum : PositivityExt where eval {u α} _ _ e := do
-  if let 0 := u then -- lean4#3060 means we can't combine this with the match below
-    match α, e with
-    | ~q(ℤ), ~q(Rat.num $a) =>
-      let zα : Q(Zero ℚ) := q(inferInstance)
-      let pα : Q(PartialOrder ℚ) := q(inferInstance)
-      assumeInstancesCommute
-      match ← core zα pα a with
-      | .positive pa =>
-        return .positive q(num_pos_of_pos $pa)
-      | .nonnegative pa => return .nonnegative q(num_nonneg_of_nonneg $pa)
-      | .nonzero pa => return .nonzero q(num_ne_zero_of_ne_zero $pa)
-      | .none => return .none
-    | _, _ => throwError "not Rat.num"
-  else throwError "not Rat.num"
+  match u, α, e with
+  | 0, ~q(ℤ), ~q(Rat.num $a) =>
+    let zα : Q(Zero ℚ) := q(inferInstance)
+    let pα : Q(PartialOrder ℚ) := q(inferInstance)
+    assumeInstancesCommute
+    match ← core zα pα a with
+    | .positive pa => pure $ .positive q(num_pos_of_pos $pa)
+    | .nonnegative pa => pure $ .nonnegative q(num_nonneg_of_nonneg $pa)
+    | .nonzero pa => pure $ .nonzero q(num_ne_zero_of_ne_zero $pa)
+    | .none => pure .none
+  | _, _ => throwError "not Rat.num"
 
 /-- The `positivity` extension which identifies expressions of the form `Rat.den a`. -/
 @[positivity Rat.den _]
 def evalRatDen : PositivityExt where eval {u α} _ _ e := do
-  if let 0 := u then -- lean4#3060 means we can't combine this with the match below
-    match α, e with
-    | ~q(ℕ), ~q(Rat.den $a) =>
-      assumeInstancesCommute
-      return .positive q(den_pos $a)
-    | _, _ => throwError "not Rat.num"
-  else throwError "not Rat.num"
+  match u, α, e with
+  | 0, ~q(ℕ), ~q(Rat.den $a) =>
+    assumeInstancesCommute
+    pure $ .positive q(den_pos $a)
+  | _, _ => throwError "not Rat.num"
