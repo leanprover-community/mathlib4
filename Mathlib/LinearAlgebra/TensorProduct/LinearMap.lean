61a25f9a36962d3510ef86d722a002690bebe7ae
Christopher Hoskin <mans0954@users.noreply.github.com>
2024-02-11 10:33:48 +0000

    Update Mathlib/LinearAlgebra/TensorProduct/LinearMap.lean
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- Mathlib/LinearAlgebra/TensorProduct/LinearMap.lean
+++ Mathlib/LinearAlgebra/TensorProduct/LinearMap.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2024 Christopher Hoskin. All rights reserved.
+Copyright (c) 2023 Eric Wieser. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Christopher Hoskin
+Authors: Eric Wieser, Christopher Hoskin
 -/
 import Mathlib.LinearAlgebra.Dual
 import Mathlib.LinearAlgebra.TensorProduct.Tower
