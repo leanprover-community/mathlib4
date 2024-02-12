f3f7bd1144799e059a38f73f21b441d4056d8bef
JADekker <114015169+JADekker@users.noreply.github.com>
2024-02-12 10:20:41 +0100

    fix : remove unused import

--- Mathlib/Probability/Distributions/Uniform.lean
+++ Mathlib/Probability/Distributions/Uniform.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Josha Dekker, Devon Tuma, Kexing Ying
 -/
 import Mathlib.Probability.Notation
-import Mathlib.Probability.Cdf
 import Mathlib.Probability.Density
 import Mathlib.Probability.ProbabilityMassFunction.Constructions
 
