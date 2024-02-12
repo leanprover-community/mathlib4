b076c2f595650e381c01c1620e488f53840a8a54
thorimur <68410468+thorimur@users.noreply.github.com>
2024-02-12 00:59:41 -0500

    chore: (test) update imports

--- test/wlog.lean
+++ test/wlog.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Simon Hudon, Johan Commelin
 -/
 import Mathlib.Tactic.WLOG
+import Mathlib.Tactic.SuccessIfFailWithMsg
 import Mathlib.Data.Nat.Basic
 
 example {x y : ℕ} : True := by
