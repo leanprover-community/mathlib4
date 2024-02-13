f6d0b12c6778c0bfd047c68b7e5cb881f4033276
CBirkbeck <c.birkbeck@ucl.ac.uk>
2024-02-13 21:45:54 +0000

    fix

--- Mathlib/Data/Int/Interval.lean
+++ Mathlib/Data/Int/Interval.lean
@@ -5,7 +5,7 @@ Authors: Yaël Dillies
 -/
 import Mathlib.Algebra.CharZero.Lemmas
 import Mathlib.Order.LocallyFinite
-import Mathlib.Data.Finset.LocallyFinite.Basic
+import Mathlib.Data.Finset.LocallyFinite
 
 #align_import data.int.interval from "leanprover-community/mathlib"@"1d29de43a5ba4662dd33b5cfeecfc2a27a5a8a29"
 
