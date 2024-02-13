32386d65270b1310eb9be7d27450ecbd3c18899d
Dagur Tómas Ásgeirsson <dagurtomas@gmail.com>
2024-02-13 22:35:02 +0000

    Update Mathlib/Topology/LocallyConstant/Basic.lean
    Co-authored-by: Johan Commelin <johan@commelin.net>

--- Mathlib/Topology/LocallyConstant/Basic.lean
+++ Mathlib/Topology/LocallyConstant/Basic.lean
@@ -608,8 +608,8 @@ a two-element set
 -/
 def equivClopens [∀ (s : Set X) x, Decidable (x ∈ s)] :
     LocallyConstant X (Fin 2) ≃ TopologicalSpace.Clopens X where
-  invFun s := ofIsClopen s.2
   toFun f := ⟨f ⁻¹' {0}, f.2.isClopen_fiber _⟩
+  invFun s := ofIsClopen s.2
   left_inv _ := locallyConstant_eq_of_fiber_zero_eq _ _ (by simp)
   right_inv _ := by simp
 
