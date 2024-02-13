56ac24a86ffbc818b9580ad3b9cf2c83963e2487
Riccardo Brasca <riccardo.brasca@gmail.com>
2024-02-13 18:30:27 +0100

    golf

--- Mathlib/NumberTheory/Cyclotomic/Embeddings.lean
+++ Mathlib/NumberTheory/Cyclotomic/Embeddings.lean
@@ -32,34 +32,11 @@ theorem nrRealPlaces_eq_zero_odd_prime_pow [IsCyclotomicExtension {p ^ (k + 1)} 
     haveI := IsCyclotomicExtension.numberField {p ^ (k + 1)} ℚ K
     NrRealPlaces K = 0 := by
   have := IsCyclotomicExtension.numberField {p ^ (k + 1)} ℚ K
-  refine (@Fintype.card_eq_zero_iff _ (_)).2 ⟨fun ⟨w, hwreal⟩ ↦ ?_⟩
-  rw [isReal_iff] at hwreal
-  let f := w.embedding
-  let ζ := IsCyclotomicExtension.zeta (p ^ (k + 1)) ℚ K
-  have hζ := (IsCyclotomicExtension.zeta_spec (p ^ (k + 1)) ℚ K)
-  have hζ' : IsPrimitiveRoot (f ζ) (p ^ (k + 1)) := hζ.map_of_injective f.injective
-  have him : (f ζ).im = 0 := by
-    · rw [← conj_eq_iff_im, ← ComplexEmbedding.conjugate_coe_eq]
-      congr
-  have hre : (f ζ).re = 1 ∨ (f ζ).re = -1 := by
-    · rw [← abs_re_eq_abs] at him
-      have := Complex.norm_eq_one_of_pow_eq_one hζ'.pow_eq_one (by norm_num)
-      rw [Complex.norm_eq_abs, ← him] at this
-      by_cases hpos : 0 ≤ (f ζ).re
-      · rw [_root_.abs_of_nonneg hpos] at this
-        left
-        exact this
-      · rw [_root_.abs_of_neg (not_le.1 hpos)] at this
-        right
-        simp [← this]
-  cases hre with
-  | inl hone =>
-    exact hζ'.ne_one (one_lt_pow hp.1.one_lt (succ_ne_zero k)) <| ext (by simp [hone])
-      (by simp [him])
-  | inr hnegone =>
-    replace hζ' := hζ'.pow_eq_one
-    rw [← re_add_im (f ζ), him, hnegone] at hζ'
-    simp [(Prime.odd_of_ne_two hp.1 (fun H ↦ hodd <| PNat.eq H)).pow.neg_one_pow] at hζ'
+  apply (IsCyclotomicExtension.zeta_spec (p ^ (k + 1)) ℚ K).nrRealPlaces_eq_zero_of_two_lt
+  refine lt_of_lt_of_le (lt_of_le_of_ne hp.1.two_le (fun h ↦ hodd (PNat.coe_inj.1 ?_)))
+    <| le_self_pow hp.1.one_lt.le (succ_ne_zero k)
+  rw [← h]
+  rfl
 
 theorem nrRealPlaces_eq_zero_odd_prime [IsCyclotomicExtension {p} ℚ K]
     (hodd : p ≠ 2) :
