56c04aa1850d8dc3a0a57bd0d845fd41d1a70f15
Xavier Généreux <xaviergenereux@hotmail.com>
2024-02-14 08:50:38 -0500

    Update Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -580,6 +580,7 @@ import Mathlib.Algebra.Ring.Units
 import Mathlib.Algebra.Ring.WithZero
 import Mathlib.Algebra.RingQuot
 import Mathlib.Algebra.SMulWithZero
+import Mathlib.Algebra.SkewMonoidAlgebra.Basic
 import Mathlib.Algebra.Squarefree.Basic
 import Mathlib.Algebra.Star.Basic
 import Mathlib.Algebra.Star.BigOperators
