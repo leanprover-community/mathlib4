870cd1ad6d80f7a4d095fc365c373ca4eb31e603
Eric Wieser <wieser.eric@gmail.com>
2024-02-14 09:39:28 +0000

    feat(Analysis/Asymptotics/Asymptotics): generalize smul lemmas to normed rings (#9811)
    Using `BoundedSMul` instead of `NormedSpace` makes these true more generally. The old proofs do not generalize, so are replaced with copies of the `mul` proofs.
    
    The `const_smul_self` lemmas match the existing `const_mul_self` ones.
    
    `shake` then reports that the imports can be reduced.

--- Mathlib/Analysis/Asymptotics/Theta.lean
+++ Mathlib/Analysis/Asymptotics/Theta.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yury G. Kudryashov
 -/
 import Mathlib.Analysis.Asymptotics.Asymptotics
+import Mathlib.Analysis.NormedSpace.Basic
 
 #align_import analysis.asymptotics.theta from "leanprover-community/mathlib"@"f2ce6086713c78a7f880485f7917ea547a215982"
 
