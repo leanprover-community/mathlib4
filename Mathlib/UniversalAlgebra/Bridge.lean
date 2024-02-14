cf0a121c8679fbf930d173d69c85b857a1a0e916
adamtopaz <github@adamtopaz.com>
2024-02-14 06:15:51 -0700

    wip

--- Mathlib/UniversalAlgebra/Bridge.lean
+++ Mathlib/UniversalAlgebra/Bridge.lean
@@ -3,80 +3,129 @@ import Mathlib.Tactic
 
 open Lean Meta
 
-class AA (X Y : Type*) where
-  one : X
-  two : Y → X → Y
-  mul : X → X → X
-  one_mul (x : X) : mul one x = x
+inductive LawvereExpr : Type where
+  | proj (output : ℕ) : LawvereExpr
+  | op (opName : Name) (output : ℕ) : Array LawvereExpr → LawvereExpr
+deriving Repr
 
-inductive LawvereWord : List Name → List Name → Type where
-  | of (opName : Name) (inputs : List Name) (output : Name) : LawvereWord inputs [output]
-  | id (sorts : List Name) : LawvereWord sorts sorts
-  | comp (A B C : List Name) : LawvereWord A B → LawvereWord B C → LawvereWord A C
-  | fst (A B : List Name) : LawvereWord (A ++ B) A
-  | snd (A B : List Name) : LawvereWord (A ++ B) B
-  | lift (T A B : List Name) : LawvereWord T A → LawvereWord T B → LawvereWord T (A ++ B)
-  | toNil (A : List Name) : LawvereWord A []
+structure LawvereOp where
+  name : Name
+  inputs : Array ℕ
+  output : ℕ
+deriving Repr
+
+structure LawvereAxiom where
+  name : Name
+  inputs : Array ℕ
+  output : ℕ
+  lhs : LawvereExpr
+  rhs : LawvereExpr
+deriving Repr
+
+def Lean.StructureInfo.getType (info : StructureInfo) : MetaM Expr := do
+  let env ← getEnv
+  let some c := env.find? info.structName
+    | throwError "Failed to find {info.structName} in environment."
+  return c.type
+
+def Lean.StructureInfo.telescope {α : Type}
+    (info : StructureInfo) (e : Array Expr → Expr → MetaM α) : MetaM α := do
+  let tp ← info.getType
+  Meta.forallTelescope tp e
+
+def Lean.StructureInfo.elabOperations (info : StructureInfo) :
+    MetaM (Array LawvereOp) := do
+  let mut out := #[]
+  let env ← getEnv
+  for c in info.fieldInfo do
+    let some projFn := env.find? c.projFn | continue
+    let .sort u ← Meta.inferType projFn.type | continue
+    if u == 0 then continue
+    let new ← Meta.forallTelescope projFn.type fun fvars body => do
+      let idxOfStruct : ℕ := ← do
+        let mut out := 0
+        for fvar in fvars do
+          out := out + 1
+          let (nm, _) := (← Meta.inferType fvar).getAppFnArgs
+          if nm == info.structName then return out
+        return out
+      let sorts := fvars[:idxOfStruct].toArray.pop
+      let inputs := fvars[idxOfStruct:].toArray
+      let some body := sorts.getIdx? body | throwError "ERROR1"
+      let inputs : Array (MetaM (Option ℕ)) := inputs.map fun e => do
+        let tp ← Meta.inferType e
+        let idx := sorts.getIdx? tp
+        return idx
+      let inputs : Array (Option ℕ) ← inputs.mapM id
+      let inputs : Option (Array ℕ) := inputs.mapM id
+      let some inputs := inputs | throwError "ERROR2"
+      return .mk c.projFn inputs body
+    out := out.push new
+  return out
+
+partial
+def Lean.Expr.parseLawvereExpr
+    (sorts : Array Expr) (ops : Array LawvereOp) (e : Expr) : MetaM LawvereExpr :=
+  match e with
+  | .app .. => do
+    let (nm, args) := e.getAppFnArgs
+    let some op := ops.find? fun op => nm == op.name | throwError "ERROR"
+    let rest ← args.mapM fun e => e.parseLawvereExpr sorts ops
+    return .op op.name op.output rest
+  | e => do
+    match sorts.getIdx? e with
+    | some idx => return .proj idx
+    | none => throwError "ERROR2"
+
+def Lean.StructureInfo.elabAxioms (info : StructureInfo) :
+    MetaM (Array LawvereAxiom) := do
+  let operations ← info.elabOperations
+  let mut out := #[]
+  let env ← getEnv
+  for c in info.fieldInfo do
+    let some projFn := env.find? c.projFn | continue
+    let .sort u ← Meta.inferType projFn.type | continue
+    if u != 0 then continue
+    let new ← Meta.forallTelescope projFn.type fun fvars body => do
+      let idxOfStruct : ℕ := ← do
+        let mut out := 0
+        for fvar in fvars do
+          out := out + 1
+          let (nm, _) := (← Meta.inferType fvar).getAppFnArgs
+          if nm == info.structName then return out
+        return out
+      let sorts := fvars[:idxOfStruct].toArray.pop
+      let inputs := fvars[idxOfStruct:].toArray
+      let .app (.app (.app (.const ``Eq ..) S) lhs) rhs := body | throwError "ERROR"
+      let some sortIndex := sorts.getIdx? S | throwError "ERROR"
+      let inputs : Array (MetaM (Option ℕ)) := inputs.map fun e => do
+        let tp ← Meta.inferType e
+        let idx := sorts.getIdx? tp
+        return idx
+      let inputs : Array (Option ℕ) ← inputs.mapM id
+      let inputs : Option (Array ℕ) := inputs.mapM id
+      let some inputs := inputs | throwError "ERROR2"
+      return .mk c.projFn inputs sortIndex
+        (← lhs.parseLawvereExpr sorts operations)
+        (← rhs.parseLawvereExpr sorts operations)
+    out := out.push new
+  return out
 
 def foobar (str : Name) : MetaM Unit := do
   let env ← getEnv
-  let some constInfo := env.find? str | throwError s!"{str} not found in the environment."
-  let tp := constInfo.type
-  let numSorts : Array Name ← Meta.forallTelescope tp fun as _ =>
-    as.mapM fun e => do
-      let .fvar id := e | throwError "error"
-      id.getUserName
-  IO.println s!"Found the following sort names {numSorts}"
-  let some strInfo := getStructureInfo? env str |
-    throwError s!"{str} is not the name of a structure in the environment."
-  let mut operations : Array (StructureFieldInfo × ConstantInfo) := #[]
-  let mut axioms : Array (StructureFieldInfo × ConstantInfo) := #[]
-  for c in strInfo.fieldInfo do
-    let some projFnConst := env.find? c.projFn | unreachable!
-    let Expr.sort u ← Meta.inferType projFnConst.type | continue
-    if u == 0 then
-      axioms := axioms.push (c, projFnConst)
-    else
-      operations := operations.push (c, projFnConst)
-  IO.println s!"Found {operations.size} operations"
-  for (opInfo, opConstInfo) in operations do
-    IO.println "---"
-    IO.println <| ← Meta.ppExpr opConstInfo.type
-    let opData : Array Name × Name ← Meta.forallTelescope opConstInfo.type fun fvars body => do
-      let mut inputs : Array Name := #[]
-      let mut collect : Bool := False
-      for fvar in fvars do
-        let .fvar id := fvar | continue
-        let tp ← id.getType
-        if tp.getAppFnArgs.fst == str then collect := True ; continue
-        unless collect do continue
-        let .fvar id := tp | continue
-        let nm ← id.getUserName
-        inputs := inputs.push nm
-      let .fvar bodyId := body | throwError "ERROR"
-      return (inputs, ← bodyId.getUserName)
-    IO.println opData
-  for (axmInfo, axmConstInfo) in axioms do
-    IO.println "---"
-    IO.println <| ← Meta.ppExpr axmConstInfo.type
-    let axData : Array Name ← Meta.forallTelescope axmConstInfo.type fun fvars body => do
-      let mut vars : Array Name := #[]
-      let mut collect : Bool := False
-      for fvar in fvars do
-        let .fvar id := fvar | continue
-        let tp ← id.getType
-        if tp.getAppFnArgs.fst == str then collect := True ; continue
-        unless collect do continue
-        let .fvar id := tp | continue
-        let nm ← id.getUserName
-        vars := vars.push nm
-        match body with
-        | .app (.app (.app (.const ``Eq _) S) _) _ =>
-          let .fvar id := S | throwError "EEE"
-          let output ← id.getUserName
-          IO.println s!"Output: {output}"
-        | _ => continue
-      return vars
-    IO.println axData
+  let strInfo ← getStructureInfo? env str
+  let ops ← strInfo.elabOperations
+  IO.println <| repr ops
+  let axs ← strInfo.elabAxioms
+  IO.println <| repr axs
+
+class AA (X Y : Type*) where
+  one : X
+  two : Y → X → Y
+  mul1 : X → X → X
+  ff : X → Y
+  mul4 : X → X → X → Y → X → Y
+  one_mul (x : X) : ff (mul1 one x) = ff x
+  onke_mul (x : X) : ff (mul1 one x) = ff x
 
 #eval foobar `AA
