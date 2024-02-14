a6d5ba2b58d93008cf224d2507262425df697871
Parcly Taxel <reddeloostw@gmail.com>
2024-02-14 23:24:15 +0800

    Switch to cones

--- Mathlib/Analysis/Complex/AbelLimit.lean
+++ Mathlib/Analysis/Complex/AbelLimit.lean
@@ -3,20 +3,19 @@ Copyright (c) 2024 Jeremy Tan. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jeremy Tan
 -/
-import Mathlib.Analysis.Complex.Basic
-import Mathlib.Analysis.SpecificLimits.Normed
+import Mathlib.Geometry.Euclidean.Triangle
 
 /-!
 # Abel's limit theorem
 
 If a real or complex power series for a function has radius of convergence 1 and the series is only
 known to converge conditionally at 1, Abel's limit theorem gives the value at 1 as the limit of the
-function at 1 from the left. "Left" for complex numbers means "within a Stolz set", a subset of the
-open unit disc with 1 on its boundary.
+function at 1 from the left. "Left" for complex numbers means within a fixed cone opening to the
+left with angle less than `π`.
 
 ## Main theorems
 
-* `Complex.tendsto_tsum_powerSeries_nhdsWithin_stolzSet`:
+* `Complex.tendsto_tsum_powerSeries_nhdsWithin_stolzCone`:
   Abel's limit theorem for complex power series.
 * `Real.tendsto_tsum_powerSeries_nhdsWithin_lt`: Abel's limit theorem for real power series.
 
@@ -61,6 +60,126 @@ theorem nhdsWithin_lt_le_nhdsWithin_stolzSet (hM : 1 < M) :
   simp only [Set.mem_setOf_eq, stolzSet, ← ofReal_one, ← ofReal_sub, norm_eq_abs, abs_ofReal,
     abs_of_pos hx.1.1, abs_of_pos <| sub_pos.mpr hx.2]
   exact ⟨hx.2, lt_mul_left (sub_pos.mpr hx.2) hM⟩
+
+open InnerProductGeometry in
+theorem stolzSet_polar' (hM : 1 < M) : stolzSet M =
+    {z | ‖z‖ < 1 ∧ ‖1 - z‖ < 2 * M ^ 2 / (M ^ 2 - 1) * (Real.cos (1 - z).arg - 1 / M)} := by
+  ext z
+  rw [stolzSet, Set.mem_setOf_eq, Set.mem_setOf_eq, ← div_lt_iff' (by positivity), lt_tsub_comm,
+    and_congr_right_iff]
+  intro hz
+  have hnz : 0 < ‖1 - z‖ := by
+    contrapose! hz
+    rw [norm_le_zero_iff, sub_eq_zero] at hz
+    rw [← hz, norm_one]
+  have q : ‖z‖ < 1 - ‖1 - z‖ / M ↔ ‖z‖ ^ 2 < (1 - ‖1 - z‖ / M) ^ 2 := by
+    constructor <;> intro h
+    · rw [sq, sq]
+      exact (mul_self_lt_mul_self_iff (norm_nonneg z) ((norm_nonneg z).trans h.le)).mp h
+    · rw [sq_lt_sq, abs_norm, lt_abs] at h
+      cases' h with c c
+      · exact c
+      · nth_rw 1 [neg_sub, lt_sub_iff_add_lt', ← norm_one (α := ℂ)] at c
+        have : ‖1 - z‖ / M ≤ ‖1 - z‖ := by nth_rw 2 [← div_one ‖_‖]; gcongr
+        replace c := c.trans_le this
+        rw [← not_le] at c
+        exact absurd (norm_sub_le _ _) c
+  have r := norm_sub_sq_eq_norm_sq_add_norm_sq_sub_two_mul_norm_mul_norm_mul_cos_angle 1 (1 - z)
+  nth_rw 1 [q, show z = 1 - (1 - z) by ring]
+  rw [sq, r, sub_sq, norm_one, one_pow, mul_one, mul_one,
+    show _ + _ - _ = ‖1 - z‖ * (‖1 - z‖ - 2 * Real.cos (angle 1 (1 - z))) + 1 by ring,
+    show 1 - _ + _ = ‖1 - z‖ * (‖1 - z‖ / M ^ 2 - 2 / M) + 1 by ring, add_lt_add_iff_right,
+    mul_lt_mul_left hnz, sub_lt_sub_iff, add_comm (_ / M ^ 2), ← sub_lt_sub_iff,
+    ← mul_lt_mul_left (show 0 < M ^ 2 by positivity), mul_sub, mul_div_cancel' _ (by positivity),
+    show M ^ 2 * ‖1 - z‖ - ‖1 - z‖ = ‖1 - z‖ * (M ^ 2 - 1) by ring, ← lt_div_iff (by nlinarith),
+    show M ^ 2 * _ / _ = 2 * M ^ 2 / (M ^ 2 - 1) * (Real.cos (angle 1 (1 - z)) - 1 / M) by ring]
+  congr! 3
+  rw [cos_angle, cos_arg (by simp_all)]
+  simp
+
+open InnerProductGeometry in
+theorem stolzSet_polar (hM : 1 < M) : stolzSet M =
+    {z | z ≠ 1 ∧ ‖1 - z‖ < 2 * M ^ 2 / (M ^ 2 - 1) * (Real.cos (1 - z).arg - 1 / M)} := by
+  rw [stolzSet_polar' hM]
+  ext z
+  simp only [Set.mem_setOf_eq]
+  constructor <;> (intro ⟨b, l⟩; refine' ⟨_, l⟩)
+  · contrapose! b
+    rw [b, norm_one]
+  · have : ‖1 - z‖ < 2 * Real.cos (1 - z).arg := by
+      have p1 : 0 < M ^ 2 - 1 := by nlinarith
+      have cnn : 0 ≤ Real.cos (1 - z).arg := by
+        by_contra! h
+        have p2 : Real.cos (arg (1 - z)) - 1 / M < 0 := by
+          rw [sub_neg]
+          exact h.trans (by positivity)
+        have p3 :=
+          l.trans (mul_neg_of_pos_of_neg (show 0 < 2 * M ^ 2 / (M ^ 2 - 1) by positivity) p2)
+        rw [← not_le] at p3
+        exact absurd (norm_nonneg _) p3
+      calc
+        ‖1 - z‖ < 2 * M ^ 2 / (M ^ 2 - 1) * (Real.cos (1 - z).arg - 1 / M) := l
+        _ ≤ 2 * M ^ 2 / (M ^ 2 - 1) * (Real.cos (1 - z).arg - Real.cos (1 - z).arg * 1 / M) := by
+          gcongr; rw [mul_one]; exact Real.cos_le_one _
+        _ = 2 * Real.cos (1 - z).arg * (M / (M + 1)) := by
+          rw [mul_div_assoc _ 1, ← mul_one_sub, show _ * (_ * _) =
+            2 * Real.cos (1 - z).arg * (M * (M * (1 - 1 / M)) / ((M + 1) * (M - 1))) by ring,
+            mul_one_sub, mul_one_div_cancel (by positivity), mul_div_mul_right _ _ (by linarith)]
+        _ ≤ _ := by
+          nth_rw 2 [← mul_one (2 * _)]
+          gcongr
+          rw [div_le_iff (by positivity)]
+          linarith
+    have r := norm_sub_sq_eq_norm_sq_add_norm_sq_sub_two_mul_norm_mul_norm_mul_cos_angle 1 (1 - z)
+    have hnz : 0 < ‖1 - z‖ := by
+      contrapose! b
+      rw [norm_le_zero_iff, sub_eq_zero] at b
+      exact b.symm
+    have s : Real.cos (1 - z).arg = Real.cos (angle 1 (1 - z)) := by
+      rw [cos_angle, cos_arg (by simp_all)]
+      simp
+    rw [sub_sub_cancel, norm_one, mul_one, mul_one, ← s] at r
+    rw [← mul_lt_mul_left hnz, ← add_lt_add_iff_left 1, ← sub_lt_iff_lt_add,
+      mul_comm 2, ← mul_assoc, ← mul_rotate, ← r] at this
+    rwa [mul_self_lt_mul_self_iff (norm_nonneg _) zero_le_one, one_mul]
+
+/-- The cone around 1 with angle `2θ`. -/
+def stolzCone (θ : ℝ) : Set ℂ := {z | ‖1 - z‖ ≠ 0 ∧ |arg (1 - z)| < θ}
+
+variable {θ : ℝ}
+
+theorem stolzCone_empty (hθ : θ ≤ 0) : stolzCone θ = ∅ := by
+  ext z
+  rw [stolzCone, Set.mem_setOf, Set.mem_empty_iff_false, iff_false, and_comm, not_and]
+  intro h
+  exact absurd (h.trans_le hθ) (by simp)
+
+open Real in
+theorem nhdsWithin_stolzCone_le_nhdsWithin_stolzSet (hθ : θ < π / 2) (hM : (Real.cos θ)⁻¹ < M) :
+    𝓝[stolzCone θ] 1 ≤ 𝓝[stolzSet M] 1 := by
+  cases' le_or_lt θ 0 with hl hl
+  · rw [stolzCone_empty hl]; simp
+  rw [nhdsWithin_le_iff, Metric.mem_nhdsWithin_iff]
+  have cpos : 0 < Real.cos θ := cos_pos_of_mem_Ioo ⟨by linarith, hθ⟩
+  have one_lt_M : 1 < M := (one_le_inv cpos (cos_le_one θ)).trans_lt hM
+  rw [stolzSet_polar one_lt_M]
+  have p1 : 0 < M ^ 2 - 1 := by nlinarith
+  have p2 : 0 < Real.cos θ - 1 / M := by rw [sub_pos, one_div_lt (by positivity) cpos]; simpa
+  use 2 * M ^ 2 / (M ^ 2 - 1) * (Real.cos θ - 1 / M), by positivity
+  intro z
+  rw [Set.mem_inter_iff]
+  intro ⟨m1, m2⟩
+  rw [mem_ball_iff_norm'] at m1
+  rw [stolzCone] at m2
+  rw [Set.mem_setOf_eq] at m2 ⊢
+  constructor
+  · replace m2 := m2.1
+    contrapose! m2
+    simp [m2]
+  · apply m1.trans _
+    gcongr
+    rw [← cos_abs (arg _)]
+    exact cos_lt_cos_of_nonneg_of_le_pi_div_two (abs_nonneg _) hθ.le m2.2
 
 end StolzSet
 
@@ -180,6 +299,15 @@ theorem tendsto_tsum_powerSeries_nhdsWithin_stolzSet
           div_mul_cancel _ (by linarith only [hM])]
   convert add_lt_add S₁ S₂ using 1
   linarith only
+
+open Real in
+/-- **Abel's limit theorem**. Given a power series converging at 1, the corresponding function
+is continuous at 1 when approaching 1 within a fixed cone opening to the left with angle `< π`. -/
+theorem tendsto_tsum_powerSeries_nhdsWithin_stolzCone
+    (h : Tendsto (fun n ↦ ∑ i in range n, f i) atTop (𝓝 l)) {θ : ℝ} (hθ : θ < π / 2) :
+    Tendsto (fun z ↦ ∑' n, f n * z ^ n) (𝓝[stolzCone θ] 1) (𝓝 l) :=
+  (tendsto_tsum_powerSeries_nhdsWithin_stolzSet (M := (Real.cos θ)⁻¹ + 1) h).mono_left
+    (nhdsWithin_stolzCone_le_nhdsWithin_stolzSet hθ (lt_add_one _))
 
 theorem tendsto_tsum_powerSeries_nhdsWithin_lt
     (h : Tendsto (fun n ↦ ∑ i in range n, f i) atTop (𝓝 l)) :
