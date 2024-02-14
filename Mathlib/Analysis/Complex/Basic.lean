1bce7374e7f2e2409bc5299d03384b3f2faedbc9
Yaël Dillies <yael.dillies@gmail.com>
2024-02-14 14:34:36 +0000

    chore: Turn `Complex.int_cast_abs` A goal about real numbers should not suddenly turn into a goal about complex numbers!

--- Mathlib/Analysis/Complex/Basic.lean
+++ Mathlib/Analysis/Complex/Basic.lean
@@ -164,8 +164,7 @@ theorem norm_nat (n : ℕ) : ‖(n : ℂ)‖ = n :=
   abs_natCast _
 #align complex.norm_nat Complex.norm_nat
 
-@[simp 1100]
-theorem norm_int {n : ℤ} : ‖(n : ℂ)‖ = |(n : ℝ)| := (int_cast_abs n).symm
+@[simp 1100] lemma norm_int {n : ℤ} : ‖(n : ℂ)‖ = |(n : ℝ)| := abs_intCast n
 #align complex.norm_int Complex.norm_int
 
 theorem norm_int_of_nonneg {n : ℤ} (hn : 0 ≤ n) : ‖(n : ℂ)‖ = n := by
