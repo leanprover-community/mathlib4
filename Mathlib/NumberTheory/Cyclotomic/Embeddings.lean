e9f5d8d158642962720be062d300cb615f962c9f
Riccardo Brasca <riccardo.brasca@gmail.com>
2024-02-14 15:46:23 +0100

    this is more general

--- Mathlib/NumberTheory/Cyclotomic/Embeddings.lean
+++ Mathlib/NumberTheory/Cyclotomic/Embeddings.lean
@@ -25,40 +25,20 @@ open NumberField InfinitePlace FiniteDimensional Complex Nat
 
 open scoped Cyclotomic
 
-variable {p : ℕ+} (k : ℕ) (K : Type u) [Field K] [CharZero K] [hp : Fact (p : ℕ).Prime]
+variable {n : ℕ+} (K : Type u) [Field K] [CharZero K]
 
-theorem nrRealPlaces_eq_zero_odd_prime_pow [IsCyclotomicExtension {p ^ (k + 1)} ℚ K]
-    (hodd : p ≠ 2) :
-    haveI := IsCyclotomicExtension.numberField {p ^ (k + 1)} ℚ K
+theorem nrRealPlaces_eq_zero [IsCyclotomicExtension {n} ℚ K]
+    (hn : 2 < n) :
+    haveI := IsCyclotomicExtension.numberField {n} ℚ K
     NrRealPlaces K = 0 := by
-  have := IsCyclotomicExtension.numberField {p ^ (k + 1)} ℚ K
-  apply (IsCyclotomicExtension.zeta_spec (p ^ (k + 1)) ℚ K).nrRealPlaces_eq_zero_of_two_lt
-  refine lt_of_lt_of_le (lt_of_le_of_ne hp.1.two_le (fun h ↦ hodd (PNat.coe_inj.1 ?_)))
-    <| le_self_pow hp.1.one_lt.le (succ_ne_zero k)
-  rw [← h]
-  rfl
+  have := IsCyclotomicExtension.numberField {n} ℚ K
+  apply (IsCyclotomicExtension.zeta_spec n ℚ K).nrRealPlaces_eq_zero_of_two_lt hn
 
-theorem nrRealPlaces_eq_zero_odd_prime [IsCyclotomicExtension {p} ℚ K]
-    (hodd : p ≠ 2) :
-    haveI := IsCyclotomicExtension.numberField {p} ℚ K
-    NrRealPlaces K = 0 := by
-  have : IsCyclotomicExtension {p ^ (0 + 1)} ℚ K := by
-    rw [zero_add, pow_one]
-    infer_instance
-  exact nrRealPlaces_eq_zero_odd_prime_pow 0 K hodd
-
-theorem nrComplexPlaces_eq_finrank_odd_prime_pow [IsCyclotomicExtension {p ^ (k + 1)} ℚ K]
-    (hodd : p ≠ 2) :
-    haveI := IsCyclotomicExtension.numberField {p ^ (k + 1)} ℚ K
+theorem nrComplexPlaces_eq_finrank [IsCyclotomicExtension {n} ℚ K]
+    (hn : 2 < n) :
+    haveI := IsCyclotomicExtension.numberField {n} ℚ K
     2 * NrComplexPlaces K = FiniteDimensional.finrank ℚ K := by
-  have := IsCyclotomicExtension.numberField {p ^ (k + 1)} ℚ K
-  simpa [nrRealPlaces_eq_zero_odd_prime_pow k K hodd] using card_add_two_mul_card_eq_rank K
-
-theorem nrComplexPlaces_eq_finrank_odd_prime [IsCyclotomicExtension {p} ℚ K]
-    (hodd : p ≠ 2) :
-    haveI := IsCyclotomicExtension.numberField {p} ℚ K
-    2 * NrComplexPlaces K = FiniteDimensional.finrank ℚ K := by
-  have := IsCyclotomicExtension.numberField {p} ℚ K
-  simpa [nrRealPlaces_eq_zero_odd_prime K hodd] using card_add_two_mul_card_eq_rank K
+  have := IsCyclotomicExtension.numberField {n} ℚ K
+  simpa [nrRealPlaces_eq_zero K hn] using card_add_two_mul_card_eq_rank K
 
 end IsCyclotomicExtension.Rat
