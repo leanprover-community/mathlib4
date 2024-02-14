0427d0c6f05aa93fe029980b24666f5f6642482b
Xavier Roblot <46200072+xroblot@users.noreply.github.com>
2024-02-14 17:15:40 +0100

    Update Mathlib/NumberTheory/NumberField/Embeddings.lean
    Co-authored-by: Johan Commelin <johan@commelin.net>

--- Mathlib/NumberTheory/NumberField/Embeddings.lean
+++ Mathlib/NumberTheory/NumberField/Embeddings.lean
@@ -521,7 +521,7 @@ theorem prod_eq_abs_norm (x : K) :
   · rw [eq_ratCast, Rat.cast_abs, ← Complex.abs_ofReal, Complex.ofReal_rat_cast]
 #align number_field.infinite_place.prod_eq_abs_norm NumberField.InfinitePlace.prod_eq_abs_norm
 
-theorem ge_one_of_lt_one {w : InfinitePlace K} {a : (𝓞 K)} (ha : a ≠ 0)
+theorem one_le_of_lt_one {w : InfinitePlace K} {a : (𝓞 K)} (ha : a ≠ 0)
     (h : ∀ ⦃z⦄, z ≠ w → z a < 1) : 1 ≤ w a := by
   suffices (1:ℝ) ≤ |(Algebra.norm ℚ) (a:K)| by
     contrapose! this
