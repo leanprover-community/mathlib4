c9a8b43ee3517aaed7ab3439cdf6eef6bf3421e7
Yury G. Kudryashov <urkud@urkud.name>
2024-02-15 06:24:15 +0000

    feat(Topology/Constructions): add `continuous_bool_rng` (#10513)
    Provide continuity criteria for functions `X → Bool`
    and, more generally, `X → Y`, where `Y` has discrete topology.

--- Mathlib/Topology/Order.lean
+++ Mathlib/Topology/Order.lean
@@ -297,6 +297,14 @@ theorem continuous_of_discreteTopology [TopologicalSpace β] {f : α → β} : C
   continuous_def.2 fun _ _ => isOpen_discrete _
 #align continuous_of_discrete_topology continuous_of_discreteTopology
 
+/-- A function to a discrete topological space is continuous if and only if the preimage of every
+singleton is open. -/
+theorem continuous_discrete_rng [TopologicalSpace α] [TopologicalSpace β] [DiscreteTopology β]
+    {f : α → β} : Continuous f ↔ ∀ b : β, IsOpen (f ⁻¹' {b}) :=
+  ⟨fun h b => (isOpen_discrete _).preimage h, fun h => ⟨fun s _ => by
+    rw [← biUnion_of_singleton s, preimage_iUnion₂]
+    exact isOpen_biUnion fun _ _ => h _⟩⟩
+
 @[simp]
 theorem nhds_discrete (α : Type*) [TopologicalSpace α] [DiscreteTopology α] : @nhds α _ = pure :=
   le_antisymm (fun _ s hs => (isOpen_discrete s).mem_nhds hs) pure_le_nhds
