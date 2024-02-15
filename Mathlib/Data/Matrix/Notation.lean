9c107dd80f1909761526253568dffb87966f0566
Eric Wieser <wieser.eric@gmail.com>
2024-02-15 13:32:59 +0000

    fix parens

--- Mathlib/Data/Matrix/Notation.lean
+++ Mathlib/Data/Matrix/Notation.lean
@@ -438,7 +438,6 @@ theorem one_fin_three : (1 : Matrix (Fin 3) (Fin 3) α) = !![1, 0, 0; 0, 1, 0; 0
 end One
 
 section AddMonoidWithOne
-
 variable [AddMonoidWithOne α]
 
 theorem natCast_fin_two (n : ℕ) : (n : Matrix (Fin 2) (Fin 2) α) = !![↑n, 0; 0, ↑n] := by
@@ -452,13 +451,15 @@ theorem natCast_fin_three (n : ℕ) :
 
 -- See note [no_index around OfNat.ofNat]
 theorem ofNat_fin_two (n : ℕ) [n.AtLeastTwo] :
-    (no_index (OfNat.ofNat) n : Matrix (Fin 2) (Fin 2) α) =
-      !![OfNat.ofNat n, 0; 0, OfNat.ofNat n] := natCast_fin_two n
+    (no_index (OfNat.ofNat n) : Matrix (Fin 2) (Fin 2) α) =
+      !![OfNat.ofNat n, 0; 0, OfNat.ofNat n] :=
+  natCast_fin_two n
 
 -- See note [no_index around OfNat.ofNat]
 theorem ofNat_fin_three (n : ℕ) [n.AtLeastTwo] :
-    (no_index (OfNat.ofNat) n : Matrix (Fin 3) (Fin 3) α) =
-      !![OfNat.ofNat n, 0, 0; 0, OfNat.ofNat n, 0; 0, 0, OfNat.ofNat n] := natCast_fin_three _
+    (no_index (OfNat.ofNat n) : Matrix (Fin 3) (Fin 3) α) =
+      !![OfNat.ofNat n, 0, 0; 0, OfNat.ofNat n, 0; 0, 0, OfNat.ofNat n] :=
+  natCast_fin_three _
 
 end AddMonoidWithOne
 
