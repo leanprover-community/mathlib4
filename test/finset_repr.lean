2d016e0f0db6e5956e268d1aae27b717f442de26
Scott Morrison <scott.morrison@gmail.com>
2024-02-15 20:38:27 +1100

    some FIXMEs

--- test/finset_repr.lean
+++ test/finset_repr.lean
@@ -1,13 +1,11 @@
 import Mathlib.Data.Finset.Sort
 
--- FIXME temporarily commented out: unsafe can't run outside of a declaration.
-
--- run_cmd Lean.Elab.Command.liftTermElabM do
---   unsafe guard <|
---    (repr (0 : Multiset (List ℕ)) |>.pretty 15) = "0"
---   unsafe guard <|
---     (repr ({[1, 2, 3], [4, 5, 6]} : Multiset (List ℕ)) |>.pretty 15) = "{[1, 2, 3],\n [4, 5, 6]}"
---   unsafe guard <|
---     (repr (∅ : Finset (List ℕ)) |>.pretty 15) = "∅"
---   unsafe guard <|
---     (repr ({[1, 2, 3], [4, 5, 6]} : Finset (List ℕ)) |>.pretty 15) = "{[1, 2, 3],\n [4, 5, 6]}"
+run_cmd Lean.Elab.Command.liftTermElabM do
+  unsafe guard <|
+   (repr (0 : Multiset (List ℕ)) |>.pretty 15) = "0"
+  unsafe guard <|
+    (repr ({[1, 2, 3], [4, 5, 6]} : Multiset (List ℕ)) |>.pretty 15) = "{[1, 2, 3],\n [4, 5, 6]}"
+  unsafe guard <|
+    (repr (∅ : Finset (List ℕ)) |>.pretty 15) = "∅"
+  unsafe guard <|
+    (repr ({[1, 2, 3], [4, 5, 6]} : Finset (List ℕ)) |>.pretty 15) = "{[1, 2, 3],\n [4, 5, 6]}"
