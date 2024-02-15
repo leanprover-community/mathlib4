be374861c489ccd859c1da257d16c1321423f7ba
Eric Wieser <wieser.eric@gmail.com>
2024-02-15 20:51:01 +0000

    Update Mathlib/Data/Matrix/Notation.lean
    Co-authored-by: damiano <adomani@gmail.com>

--- Mathlib/Data/Matrix/Notation.lean
+++ Mathlib/Data/Matrix/Notation.lean
@@ -453,7 +453,7 @@ theorem natCast_fin_three (n : ℕ) :
 theorem ofNat_fin_two (n : ℕ) [n.AtLeastTwo] :
     (no_index (OfNat.ofNat n) : Matrix (Fin 2) (Fin 2) α) =
       !![OfNat.ofNat n, 0; 0, OfNat.ofNat n] :=
-  natCast_fin_two n
+  natCast_fin_two _
 
 -- See note [no_index around OfNat.ofNat]
 theorem ofNat_fin_three (n : ℕ) [n.AtLeastTwo] :
