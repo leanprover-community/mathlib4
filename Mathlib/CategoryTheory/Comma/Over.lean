76e9597c4920fd0421f70311e28cfc396367a5d1
Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>
2024-02-16 00:49:08 +0100

    Update Over.lean

--- Mathlib/CategoryTheory/Comma/Over.lean
+++ Mathlib/CategoryTheory/Comma/Over.lean
@@ -63,7 +63,7 @@ theorem OverMorphism.ext {X : T} {U V : Over X} {f g : U ⟶ V} (h : f.left = g.
   simp only [eq_iff_true_of_subsingleton]
 #align category_theory.over.over_morphism.ext CategoryTheory.Over.OverMorphism.ext
 
--- @[simp] : Porting note : simp can prove this
+-- @[simp] : Porting note (#10618): simp can prove this
 theorem over_right (U : Over X) : U.right = ⟨⟨⟩⟩ := by simp only
 #align category_theory.over.over_right CategoryTheory.Over.over_right
 
