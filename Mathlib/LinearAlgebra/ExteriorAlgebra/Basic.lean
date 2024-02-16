0bad6e3c9e03a3af62ed25200e197f60a7b3940d
Eric Wieser <wieser.eric@gmail.com>
2024-02-16 10:58:58 +0000

    promote statement to an Iff

--- Mathlib/LinearAlgebra/ExteriorAlgebra/Basic.lean
+++ Mathlib/LinearAlgebra/ExteriorAlgebra/Basic.lean
@@ -418,17 +418,22 @@ theorem ι_range_map_map (f : M →ₗ[R] N) :
   CliffordAlgebra.ι_range_map_map _
 
 open Function in
-/-- If a linear map `f` from `M` to `N` admits a linear retraction `g : M →ₗ[R] N`, then
-`ExteriorAlgebra.map g` is a retraction of `ExteriorAlgebra.map f`.-/
-lemma map_leftInverse_of_leftInverse {f : M →ₗ[R] N} {g : N →ₗ[R] M}
-    (h : g.comp f = LinearMap.id) : LeftInverse (map g) (map f) :=
-  fun _ ↦ by rw [← AlgHom.comp_apply, map_comp_map, h, map_id, AlgHom.coe_id, id_eq]
+/-- For a linear map `f` from `M` to `N`,
+`ExteriorAlgebra.map g` is a retraction of
+`ExteriorAlgebra.map f` iff `g` is a retractions of `f`. -/
+@[simp]
+lemma leftInverse_map_iff {f : M →ₗ[R] N} {g : N →ₗ[R] M} :
+    LeftInverse (map g) (map f) ↔ LeftInverse g f := by
+  refine ⟨fun h x => ?_, fun h x => ?_⟩
+  · simpa using h (ι _ x)
+  · replace h : g ∘ₗ f = .id := DFunLike.ext _ _ h
+    rw [← AlgHom.comp_apply, map_comp_map, h, map_id, AlgHom.coe_id, id_eq]
 
 /-- A morphism of modules that admits a linear retraction induces an injective morphism of
 exterior algebras.-/
 lemma map_injective {f : M →ₗ[R] N} (hf : ∃ (g : N →ₗ[R] M), g.comp f = LinearMap.id) :
     Function.Injective (map f) :=
-  let ⟨_, hgf⟩ := hf; Function.LeftInverse.injective (map_leftInverse_of_leftInverse hgf)
+  let ⟨_, hgf⟩ := hf; (leftInverse_map_iff.mpr (DFunLike.congr_fun hgf)).injective
 
 /-- A surjective morphism of modules induces a surjective morphism of exterior algebras.-/
 lemma map_surjective {f : M →ₗ[R] N} (hf : Function.Surjective f) :
