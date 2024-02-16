06f683fd88c3091cd5049a69a83b3c876218cddc
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2024-02-17 00:05:11 +0100

    shake autofix

--- Mathlib/FieldTheory/GalConj.lean
+++ Mathlib/FieldTheory/GalConj.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yuyang Zhao
 -/
 
-import Mathlib.FieldTheory.PolynomialGaloisGroup
+import Mathlib.FieldTheory.Normal
 
 /-!
 TODO
