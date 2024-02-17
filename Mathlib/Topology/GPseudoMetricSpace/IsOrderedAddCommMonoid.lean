a0d32b673ae0f861b341643a301a851a83b2ca45
Blizzard_inc <thrashy4inbox@gmail.com>
2024-02-17 20:59:11 +0100

    add a mixin, add comments new mixin: IsOrderedCancelAddCommMonoid. extra comments for GPseudoMetricSpace and others

--- Mathlib/Topology/GPseudoMetricSpace/IsOrderedAddCommMonoid.lean
+++ Mathlib/Topology/GPseudoMetricSpace/IsOrderedAddCommMonoid.lean
@@ -1,9 +1,17 @@
 import Mathlib.Algebra.Order.Monoid.Defs
 
 class IsOrderedAddCommMonoid (α : Type*) [PartialOrder α] [AddCommMonoid α] where
+  /- Adding a constant on the left does not change the order -/
   protected add_le_add_left : ∀ a b : α, a ≤ b → ∀ c, c + a ≤ c + b
 
-instance (α : Type*) [h1: LinearOrder α] [h2: AddCommMonoid α] [h3:IsOrderedAddCommMonoid α]:
-  LinearOrderedAddCommMonoid α := {
-  h1,h2,h3 with
-}
+class IsOrderedCancelAddCommMonoid
+  (α :Type*) [PartialOrder α] [AddCommMonoid α] extends IsOrderedAddCommMonoid α where
+  protected le_of_add_le_add_left : ∀ (a b c : α), a + b ≤ a + c → b ≤ c
+
+instance (α : Type*) [h1: LinearOrder α] [h2: AddCommMonoid α] [h3:IsOrderedAddCommMonoid α] :
+    LinearOrderedAddCommMonoid α := {
+  h1,h2,h3 with }
+
+instance (α : Type*) [h1:LinearOrder α] [h2:AddCommMonoid α] [h3:IsOrderedCancelAddCommMonoid α] :
+    LinearOrderedCancelAddCommMonoid α := {
+  h1,h2,h3 with }
