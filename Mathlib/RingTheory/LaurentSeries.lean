c8ef093d1e22232de7e3d0bd6d0da5a83cf2524e
Scott Carnahan <128885296+ScottCarnahan@users.noreply.github.com>
2024-02-18 01:34:35 +0900

    fix merge conflict

--- Mathlib/RingTheory/LaurentSeries.lean
+++ Mathlib/RingTheory/LaurentSeries.lean
@@ -3,6 +3,7 @@ Copyright (c) 2021 Aaron Anderson. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Aaron Anderson
 -/
+import Mathlib.Data.Int.Interval
 import Mathlib.RingTheory.HahnSeries.PowerSeries
 import Mathlib.RingTheory.HahnSeries.Summable
 import Mathlib.RingTheory.Localization.FractionRing
