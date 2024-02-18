418e53f3c5845c162a5484093e4873f259457d75
Yaël Dillies <yael.dillies@gmail.com>
2024-02-18 16:23:55 +0000

    cleanup

--- Mathlib/Data/Complex/Order.lean
+++ Mathlib/Data/Complex/Order.lean
@@ -138,19 +138,15 @@ private alias ⟨_, ofReal_ne_zero_of_ne_zero⟩ := ofReal_ne_zero
 input is. -/
 @[positivity Complex.ofReal' _]
 def evalComplexOfReal : PositivityExt where eval {u α} _ _ e := do
-  let zα : Q(Zero ℝ) := q(inferInstance)
-  let pα : Q(PartialOrder ℝ) := q(inferInstance)
-  if let 0 := u then -- lean4#3060 means we can't combine this with the match below
-    match α, e with
-    | ~q(ℂ), ~q(Complex.ofReal' $a) =>
-      assumeInstancesCommute
-      match ← core zα pα a with
-      | .positive pa => return .positive q(ofReal_pos $pa)
-      | .nonnegative pa => return .nonnegative q(ofReal_nonneg $pa)
-      | .nonzero pa => return .nonzero q(ofReal_ne_zero_of_ne_zero $pa)
-      | _ => return .none
-    | _, _ => throwError "not Complex.ofReal'"
-  else throwError "not Complex.ofReal'"
+  match u, α, e with
+  | 0, ~q(ℂ), ~q(Complex.ofReal' $a) =>
+    assumeInstancesCommute
+    match ← core q(inferInstance) q(inferInstance) a with
+    | .positive pa => return .positive q(ofReal_pos $pa)
+    | .nonnegative pa => return .nonnegative q(ofReal_nonneg $pa)
+    | .nonzero pa => return .nonzero q(ofReal_ne_zero_of_ne_zero $pa)
+    | _ => return .none
+  | _, _ => throwError "not Complex.ofReal'"
 
 example (x : ℝ) (hx : 0 < x) : 0 < (x : ℂ) := by positivity
 example (x : ℝ) (hx : 0 ≤ x) : 0 ≤ (x : ℂ) := by positivity
