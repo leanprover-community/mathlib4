19c38e686353c92ac5d38ac9d531213e51c5407d
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2024-02-18 15:36:27 +0100

    fix: rename Irreducible.nat_degree_le_two

--- Mathlib/Analysis/Complex/Polynomial.lean
+++ Mathlib/Analysis/Complex/Polynomial.lean
@@ -219,5 +219,7 @@ lemma Irreducible.degree_le_two {p : ℝ[X]} (hp : Irreducible p) : degree p ≤
     rwa [isUnit_iff_degree_eq_zero.1 hq, add_zero]
 
 /-- An irreducible real polynomial has natural degree at most two. -/
-lemma Irreducible.nat_degree_le_two {p : ℝ[X]} (hp : Irreducible p) : natDegree p ≤ 2 :=
+lemma Irreducible.natDegree_le_two {p : ℝ[X]} (hp : Irreducible p) : natDegree p ≤ 2 :=
   natDegree_le_iff_degree_le.2 hp.degree_le_two
+
+@[deprecated] alias Irreducible.nat_degree_le_two := Irreducible.natDegree_le_two
