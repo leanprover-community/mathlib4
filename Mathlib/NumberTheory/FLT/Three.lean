f5679ac82d5a89832add6e1d339c79425c054b21
Riccardo Brasca <riccardo.brasca@gmail.com>
2024-02-18 19:48:18 +0100

    unused import

--- Mathlib/NumberTheory/FLT/Three.lean
+++ Mathlib/NumberTheory/FLT/Three.lean
@@ -3,7 +3,6 @@ Copyright (c) 2024 Riccardo Brasca. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Riccardo Brasca
 -/
-import Mathlib.NumberTheory.FLT.Basic
 import Mathlib.Data.ZMod.Basic
 
 /-!
