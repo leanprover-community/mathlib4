b62ed3f6236934bc467a9480cb12847b60ffe1e3
Eric Wieser <wieser.eric@gmail.com>
2024-02-18 13:48:56 +0000

    Remove replace

--- Mathlib/Tactic/Common.lean
+++ Mathlib/Tactic/Common.lean
@@ -66,7 +66,6 @@ import Mathlib.Tactic.Observe
 import Mathlib.Tactic.ProjectionNotation
 import Mathlib.Tactic.Propose
 import Mathlib.Tactic.PushNeg
-import Mathlib.Tactic.Replace
 import Mathlib.Tactic.RSuffices
 import Mathlib.Tactic.Recover
 import Mathlib.Tactic.Relation.Symm
