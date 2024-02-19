b4bc3a43828e20375699eab00ff70f094a8400f6
Markus Himmel <markus@himmel-villmar.de>
2024-02-19 21:02:52 +0100

    Add to mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -1064,6 +1064,7 @@ import Mathlib.CategoryTheory.EssentiallySmall
 import Mathlib.CategoryTheory.Extensive
 import Mathlib.CategoryTheory.Filtered.Basic
 import Mathlib.CategoryTheory.Filtered.Connected
+import Mathlib.CategoryTheory.Filtered.Final
 import Mathlib.CategoryTheory.Filtered.Small
 import Mathlib.CategoryTheory.FinCategory
 import Mathlib.CategoryTheory.FintypeCat
