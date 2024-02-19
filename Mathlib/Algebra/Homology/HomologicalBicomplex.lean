6ba0d806bff1da84a38877c2cf6344fbed25251f
Joël Riou <joel.riou@universite-paris-saclay.fr>
2024-02-19 02:57:54 +0100

    added docstrings

--- Mathlib/Algebra/Homology/HomologicalBicomplex.lean
+++ Mathlib/Algebra/Homology/HomologicalBicomplex.lean
@@ -47,6 +47,7 @@ def toGradedObject (K : HomologicalComplex₂ C c₁ c₂) :
     GradedObject (I₁ × I₂) C :=
   fun ⟨i₁, i₂⟩ => (K.X i₁).X i₂
 
+/-- The morphism of graded objects induced by a morphism of bicomplexes. -/
 def toGradedObjectMap {K L : HomologicalComplex₂ C c₁ c₂} (φ : K ⟶ L) :
     K.toGradedObject ⟶ L.toGradedObject :=
   fun ⟨i₁, i₂⟩ => (φ.f i₁).f i₂
@@ -56,6 +57,7 @@ lemma toGradedObjectMap_apply {K L : HomologicalComplex₂ C c₁ c₂} (φ : K 
     toGradedObjectMap φ ⟨i₁, i₂⟩ = (φ.f i₁).f i₂ := rfl
 
 variable (C c₁ c₂) in
+/-- The functor which sends a bicomplex to its associated graded object. -/
 @[simps]
 def toGradedObjectFunctor : HomologicalComplex₂ C c₁ c₂ ⥤ GradedObject (I₁ × I₂) C where
   obj K := K.toGradedObject
