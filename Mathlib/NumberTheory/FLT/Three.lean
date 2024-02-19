591f32b42a6239eab293014b665f37c13af37195
Riccardo Brasca <riccardo.brasca@gmail.com>
2024-02-19 10:47:00 +0100

    Update Mathlib/NumberTheory/FLT/Three.lean
    Co-authored-by: Thomas Browning <tb65536@users.noreply.github.com>

--- Mathlib/NumberTheory/FLT/Three.lean
+++ Mathlib/NumberTheory/FLT/Three.lean
@@ -21,48 +21,22 @@ open ZMod
 
 private lemma three_dvd_nine : 3 ∣ 9 := by norm_num
 
-private lemma mem_of_castHom_ne_zero {a : ZMod 9} (ha : castHom three_dvd_nine (ZMod 3) a ≠ 0) :
-    a ∈ ({1, 2, 4, 5, 7, 8} : Finset (ZMod 9)) := by
-  fin_cases a
-  · aesop
-  rotate_left 2
-  · exfalso
-    exact ha rfl
-  rotate_left 2
-  · exfalso
-    exact ha rfl
-  all_goals decide
+private lemma cube_of_castHom_ne_zero {n : ZMod 9} (h : castHom three_dvd_nine (ZMod 3) n ≠ 0) :
+    n ^ 3 = 1 ∨ n ^ 3 = 8 := by
+  revert h; fin_cases n <;> decide
 
-private lemma mem_of_coe_ne_zero {a : ℕ} (ha : (a : ZMod 3) ≠ 0) :
-    (a : ZMod 9) ∈ ({1, 2, 4, 5, 7, 8} : Finset (ZMod 9)) :=
-  mem_of_castHom_ne_zero (fun h ↦ ha <| by simpa using h)
-
-private lemma cube_mem_of_mem {a : ZMod 9} (ha : a ∈ ({1, 2, 4, 5, 7, 8} : Finset (ZMod 9))) :
-    a ^ 3 ∈ ({1, 8} : Finset (ZMod 9)) := by
-  fin_cases ha
-  all_goals decide
-
-private lemma fermatLastTheoremThree_ZMod9_case_1 {a b c : ZMod 9}
-  (ha : a ∈ ({1, 2, 4, 5, 7, 8} : Finset (ZMod 9)))
-  (hb : b ∈ ({1, 2, 4, 5, 7, 8} : Finset (ZMod 9)))
-  (hc : c ∈ ({1, 2, 4, 5, 7, 8} : Finset (ZMod 9))) :
-    a ^ 3 + b ^ 3 ≠ c ^ 3 := by
-  replace ha := cube_mem_of_mem ha
-  replace hb := cube_mem_of_mem hb
-  replace hc := cube_mem_of_mem hc
-  simp only [Finset.mem_insert, Finset.mem_singleton] at ha hb hc
-  rcases ha with (ha | ha) <;> rcases hb with (hb | hb) <;> rcases hc with (hc | hc)
-  all_goals simp only [ha, hb, hc]; decide
+private lemma cube_of_not_dvd {n : ℕ} (h : ¬ 3 ∣ n) :
+    (n : ZMod 9) ^ 3 = 1 ∨ (n : ZMod 9) ^ 3 = 8 := by
+  apply cube_of_castHom_ne_zero
+  rwa [map_natCast, Ne, Fin.nat_cast_eq_zero]
 
 /--If `a b c : ℕ` are such that `¬ 3 ∣ a * b * c`, then `a ^ 3 + b ^ 3 ≠ c ^ 3`. -/
 theorem fermatLastTheoremThree_case_1 {a b c : ℕ} (hdvd : ¬ 3 ∣ a * b * c) :
     a ^ 3 + b ^ 3 ≠ c ^ 3 := by
-  intro H
-  have hA := mem_of_coe_ne_zero <| fun h ↦
-    hdvd <| (((nat_cast_zmod_eq_zero_iff_dvd _ _).1 h).mul_right b).mul_right c
-  have hB := mem_of_coe_ne_zero <| fun h ↦
-    hdvd <| (((nat_cast_zmod_eq_zero_iff_dvd _ _).1 h).mul_left a).mul_right c
-  have hC := mem_of_coe_ne_zero <| fun h ↦
-    hdvd <| mul_assoc a b c ▸ (((nat_cast_zmod_eq_zero_iff_dvd _ _).1 h).mul_left b).mul_left a
-  exact fermatLastTheoremThree_ZMod9_case_1 hA hB hC
-    (by convert congr_arg ((↑) : ℕ → ZMod 9) H using 1 <;> simp)
+  simp_rw [Nat.prime_three.dvd_mul, not_or] at hdvd
+  apply mt (congrArg (Nat.cast : ℕ → ZMod 9))
+  simp_rw [Nat.cast_add, Nat.cast_pow]
+  rcases cube_of_not_dvd hdvd.1.1 with ha | ha <;>
+  rcases cube_of_not_dvd hdvd.1.2 with hb | hb <;>
+  rcases cube_of_not_dvd hdvd.2 with hc | hc <;>
+  rw [ha, hb, hc] <;> decide
