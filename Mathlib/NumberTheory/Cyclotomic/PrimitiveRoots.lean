1d02e3b149e0ac572775f24a8e6284f8903cd054
Riccardo Brasca <riccardo.brasca@gmail.com>
2024-02-19 12:43:02 +0100

    implicit variable

--- Mathlib/NumberTheory/Cyclotomic/PrimitiveRoots.lean
+++ Mathlib/NumberTheory/Cyclotomic/PrimitiveRoots.lean
@@ -189,6 +189,7 @@ theorem finrank (hirr : Irreducible (cyclotomic n K)) : finrank K L = (n : ℕ).
     (zeta_spec n K L).minpoly_eq_cyclotomic_of_irreducible hirr, natDegree_cyclotomic]
 #align is_cyclotomic_extension.finrank IsCyclotomicExtension.finrank
 
+variable {L} in
 /-- If `L` contains both a primitive `p`-th root of unity and `q`-th root of unity, and
 `Irreducible (cyclotomic (lcm p q) K)` (in particular for `K = ℚ`), then the `finrank K L` is at
 least `(lcm p q).totient`. -/
