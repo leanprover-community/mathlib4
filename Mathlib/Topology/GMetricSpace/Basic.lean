14c97c72fc8d0cd7bdbc2679734247cc4a160e73
Blizzard_inc <thrashy4inbox@gmail.com>
2024-02-20 19:07:42 +0100

    rewrite GMetricSpace to GMetric this mirrors the change from GPseudoMetricSpace to GPseudoMetric, in that it changes from a class to a bundled structure

--- Mathlib/Topology/GMetricSpace/Basic.lean
+++ Mathlib/Topology/GMetricSpace/Basic.lean
@@ -3,36 +3,56 @@ Copyright (c) 2024 Edward van de Meent. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Edward van de Meent
 -/
-import Mathlib.Topology.GPseudoMetricSpace.Basic
+import Mathlib.Topology.GPseudoMetric.Basic
 
 /-!
 # General Metric Spaces
 
-this file defines General Metric Spaces, which are a generalisation of Metric Spaces and Extended
-metric spaces. In this case, the codomain can be any linearly ordered (additive) commutative monoid,
+this file defines Generic Metrics, which are a generalisation of Metrics and Extended
+Metrics. In this case, the codomain can be any linearly ordered (additive) commutative monoid,
 rather than only ℝ or ℝ≥0∞.
 
 ## Main Definitions
 
-- `GMetricSpace α β`: a space endowed with a distance function with codomain `β`, which returns 0
+- `GMetric α β`: a distance function on `α` with codomain `β`, which returns 0
 iff the arguments are equal.
 
 ## Implementation Notes
 
 A lot of elementary properties don't require `eq_of_gdist_eq_zero`, hence are stated and proven
-for `GPseudoMetricSpace`s in `GPseudoMetric/Basic.lean`.
+for `GPseudoMetric`s in `GPseudoMetric/Basic.lean`.
 -/
 
-/-- We now define `GMetricSpace`, extending `GPseudoMetricSpace`. -/
-class GMetricSpace (α : Type*) (β : Type*) [LinearOrder β] [AddCommMonoid β]
-    [IsOrderedAddCommMonoid β] extends GPseudoMetricSpace α β where
-  eq_of_dist_eq_zero : ∀ {x y : α}, gdist x y = 0 → x = y
+/-- We now define `GMetric`, extending `GPseudoMetric`. -/
+structure GMetric (α : Type*) (β : Type*) [LinearOrder β] [AddCommMonoid β]
+    [IsOrderedAddCommMonoid β] extends GPseudoMetric α β where
+  eq_of_dist_eq_zero : ∀ {x y : α}, toFun x y = 0 → x = y
 
 variable {α:Type*} {β:Type*} [LinearOrder β] [AddCommMonoid β]
     [IsOrderedAddCommMonoid β]
 
-/-- Two generalised metric space structures with the same distance coincide. -/
+/-- Two generalised metrics with the same distance function coincide. -/
 @[ext]
-theorem GMetricSpace.ext {m m' : GMetricSpace α β} (h : m.toGDist = m'.toGDist) :
-    m = m' := by
-  cases m; cases m'; congr; ext1; assumption
+theorem GMetric.ext {gdist₁ gdist₂ : GMetric α β} (h : gdist₁.toFun = gdist₂.toFun) :
+    gdist₁ = gdist₂ := by
+  cases gdist₁; cases gdist₂; congr; ext1; assumption
+
+/-- the class of types that are generalised metrics on α to β-/
+class GMetricClass
+    (T:Type*) (α β :outParam Type*) [LinearOrder β] [AddCommMonoid β] [IsOrderedAddCommMonoid β]
+    [FunLike T α (α → β)] extends GPseudoMetricClass T α β : Prop where
+  eq_of_dist_eq_zero : ∀ (gdist:T), ∀ {x y : α}, gdist x y = 0 → x = y
+
+instance GMetric.instFunLike: FunLike (GMetric α β) α (α → β) where
+  coe := fun x => x.toFun
+  coe_injective' := by apply GMetric.ext
+
+instance : GMetricClass (GMetric α β) α β where
+  gdist_self := fun x => x.gdist_self
+  comm' := fun x => x.comm'
+  triangle' := fun x => x.triangle'
+  eq_of_dist_eq_zero := GMetric.eq_of_dist_eq_zero
+
+variable {T:Type*} [FunLike T α (α → β)] [GMetricClass T α β] (gdist : T)
+
+#lint
