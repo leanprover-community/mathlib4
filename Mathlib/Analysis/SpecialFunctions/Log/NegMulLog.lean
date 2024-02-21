b1cf046b8e9007bce1d55f8ed40ee7b1436eda35
Yaël Dillies <yael.dillies@gmail.com>
2024-02-21 21:30:31 +0000

    feat: Jensen's inequality for the entropy From PFR

--- Mathlib/Analysis/SpecialFunctions/Log/NegMulLog.lean
+++ Mathlib/Analysis/SpecialFunctions/Log/NegMulLog.lean
@@ -18,7 +18,7 @@ which is notably used in the theory of Shannon entropy.
 
 -/
 
-open scoped Topology
+open scoped BigOperators Topology
 
 namespace Real
 
@@ -126,6 +126,33 @@ lemma strictConcaveOn_negMulLog : StrictConcaveOn ℝ (Set.Ici (0 : ℝ)) negMul
 
 lemma concaveOn_negMulLog : ConcaveOn ℝ (Set.Ici (0 : ℝ)) negMulLog :=
   strictConcaveOn_negMulLog.concaveOn
+
+variable {ι : Type*} {s : Finset ι} {w : ι → ℝ} {p : ι → ℝ}
+
+/-- Jensen's inequality for the entropy function. -/
+lemma sum_negMulLog_le (h₀ : ∀ i ∈ s, 0 ≤ w i) (h₁ : ∑ i in s, w i = 1) (hmem : ∀ i ∈ s, 0 ≤ p i) :
+    ∑ i in s, w i * negMulLog (p i) ≤ negMulLog (∑ i in s, w i * p i) :=
+  concaveOn_negMulLog.le_map_sum h₀ h₁ hmem
+
+/-- The strict Jensen inequality for the entropy function. -/
+lemma sum_negMulLog_lt (h₀ : ∀ i ∈ s, 0 < w i) (h₁ : ∑ i in s, w i = 1) (hmem : ∀ i ∈ s, 0 ≤ p i)
+    (hp : ∃ j ∈ s, ∃ k ∈ s, p j ≠ p k) :
+    ∑ i in s, w i * negMulLog (p i) < negMulLog (∑ i in s, w i * p i) :=
+  strictConcaveOn_negMulLog.lt_map_sum h₀ h₁ hmem hp
+
+/-- The equality case of Jensen's inequality for the entropy function. -/
+lemma negMulLog_sum_eq_iff (h₀ : ∀ i ∈ s, 0 < w i) (h₁ : ∑ i in s, w i = 1)
+    (hmem : ∀ i ∈ s, 0 ≤ p i) :
+    negMulLog (∑ i in s, w i * p i) = ∑ i in s, w i * negMulLog (p i) ↔
+      ∀ j ∈ s, p j = ∑ i in s, w i * p i :=
+  strictConcaveOn_negMulLog.map_sum_eq_iff h₀ h₁ hmem
+
+/-- The equality case of Jensen's inequality for the entropy function. -/
+lemma negMulLog_sum_eq_iff' (h₀ : ∀ i ∈ s, 0 ≤ w i) (h₁ : ∑ i in s, w i = 1)
+    (hmem : ∀ i ∈ s, 0 ≤ p i) :
+    negMulLog (∑ i in s, w i * p i) = ∑ i in s, w i * negMulLog (p i) ↔
+      ∀ j ∈ s, w j ≠ 0 → p j = ∑ i in s, w i * p i :=
+  strictConcaveOn_negMulLog.map_sum_eq_iff' h₀ h₁ hmem
 
 end negMulLog
 
