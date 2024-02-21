95a9da07810a4aa661e86d72253d1da17472d1c5
Floris van Doorn <fpvdoorn@gmail.com>
2024-02-21 20:10:32 +0100

    fix name

--- Mathlib/Algebra/Group/Units.lean
+++ Mathlib/Algebra/Group/Units.lean
@@ -640,7 +640,7 @@ def IsUnit [Monoid M] (a : M) : Prop :=
 #align is_add_unit IsAddUnit
 
 @[to_additive]
-lemma isUnit_iff [Monoid M] {x : M} : IsUnit x ↔ ∃ b, x * b = 1 ∧ b * x = 1 := by
+lemma isUnit_iff_exists [Monoid M] {x : M} : IsUnit x ↔ ∃ b, x * b = 1 ∧ b * x = 1 := by
   refine ⟨fun ⟨u, hu⟩ => ?_, fun ⟨b, h1b, h2b⟩ => ⟨⟨x, b, h1b, h2b⟩, rfl⟩⟩
   subst x
   exact ⟨u.inv, u.val_inv, u.inv_val⟩
