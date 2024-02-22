5f26e278d4bc9c4dfcabc73af134d698ecca14d5
Felix-Weilacher <fweilach@andrew.cmu.edu>
2024-02-22 13:17:52 -0500

    EventuallyMeasurable imports

--- Mathlib/MeasureTheory/Constructions/EventuallyMeasurable.lean
+++ Mathlib/MeasureTheory/Constructions/EventuallyMeasurable.lean
@@ -4,7 +4,8 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Felix Weilacher
 -/
 
-import Mathlib.MeasureTheory.MeasurableSpace.Basic
+import Mathlib.MeasureTheory.MeasurableSpace.Defs
+import Mathlib.Order.Filter.CountableInter
 
 /-!
 # Measurability modulo a filter
