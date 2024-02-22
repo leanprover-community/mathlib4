e27a26d4ea5deda8755d3066e805c641abc15de2
Geoffrey Irving <irving@naml.us>
2024-02-22 22:14:28 +0000

    feat: Complex `exp`, `log`, and `cpow` are analytic (#10811)
    These are all immediate from differentiability.
    
    We also record several rewrite lemmas for switching between analyticity and differentiability.  These are immediate from existing theorems, but as far as I can tell don't yet exist as iffs.

--- Mathlib.lean
+++ Mathlib.lean
@@ -906,6 +906,7 @@ import Mathlib.Analysis.Seminorm
 import Mathlib.Analysis.SpecialFunctions.Arsinh
 import Mathlib.Analysis.SpecialFunctions.Bernstein
 import Mathlib.Analysis.SpecialFunctions.CompareExp
+import Mathlib.Analysis.SpecialFunctions.Complex.Analytic
 import Mathlib.Analysis.SpecialFunctions.Complex.Arctan
 import Mathlib.Analysis.SpecialFunctions.Complex.Arg
 import Mathlib.Analysis.SpecialFunctions.Complex.Circle
