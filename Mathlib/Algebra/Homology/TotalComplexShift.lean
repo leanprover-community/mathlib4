91178054acbb6f408f6c47001dbc41ceb698583b
Joël Riou <joel.riou@universite-paris-saclay.fr>
2024-02-22 17:19:57 +0100

    pleasing lint

--- Mathlib/Algebra/Homology/TotalComplexShift.lean
+++ Mathlib/Algebra/Homology/TotalComplexShift.lean
@@ -1,4 +1,8 @@
-import Mathlib.Algebra.Homology.Bifunctor
+/-
+Copyright (c) 2024 Joël Riou. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Joël Riou
+-/
 import Mathlib.Algebra.Homology.HomotopyCategory.Shift
 import Mathlib.Algebra.Homology.TotalComplex
 /-!
