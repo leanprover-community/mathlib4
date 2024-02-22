7382b7bc394b37ffe797254c001cec835f46ac13
madvorak <martin.dvorak@matfyz.cz>
2024-02-22 11:12:30 +0100

    minimized imports

--- Mathlib/Combinatorics/Optimization/ValuedCSP.lean
+++ Mathlib/Combinatorics/Optimization/ValuedCSP.lean
@@ -3,10 +3,10 @@ Copyright (c) 2023 Martin Dvorak. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Martin Dvorak
 -/
-import Mathlib.Algebra.Module.BigOperators
+import Mathlib.Algebra.Module.Basic
+import Mathlib.Data.Fintype.Basic
 import Mathlib.Data.Fin.VecNotation
 import Mathlib.Data.List.OfFn
-import Mathlib.Data.List.Perm
 
 /-!
 
