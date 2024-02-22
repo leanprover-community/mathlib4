8f18efc60aed2006380c8bd8860b4d31367d38b7
Christopher Hoskin <christopher.hoskin@gmail.com>
2024-02-22 06:17:29 +0000

    Fix

--- Mathlib/LinearAlgebra/QuadraticForm/Basic.lean
+++ Mathlib/LinearAlgebra/QuadraticForm/Basic.lean
@@ -1236,11 +1236,12 @@ theorem exists_orthogonal_basis [hK : Invertible (2 : K)] {B : BilinForm K V} (h
   · let b := FiniteDimensional.finBasis K V
     rw [hd] at b
     exact ⟨b, fun i j _ => rfl⟩
-  obtain ⟨x, hx⟩ := exists_bilinForm_self_ne_zero hB₁ hB₂
+  obtain ⟨x, hx⟩ := exists_linearMap₂_self_ne_zero hB₁ hB₂
   rw [← Submodule.finrank_add_eq_of_isCompl (isCompl_span_singleton_orthogonal hx).symm,
-    finrank_span_singleton (ne_zero_of_not_isOrtho_self x hx)] at hd
-  let B' := B.restrict (B.orthogonal <| K ∙ x)
-  obtain ⟨v', hv₁⟩ := ih (hB₂.restrict _ : B'.IsSymm) (Nat.succ.inj hd)
+    finrank_span_singleton (ne_zero_of_map hx)] at hd
+  let B' :=  B.domRestrict₁₂ (Submodule.orthogonalBilin (K ∙ x) B )
+    (Submodule.orthogonalBilin (K ∙ x) B )
+  obtain ⟨v', hv₁⟩ := ih (hB₂.domRestrict _ : B'.IsSymm) (Nat.succ.inj hd)
   -- concatenate `x` with the basis obtained by induction
   let b :=
     Basis.mkFinCons x v'
