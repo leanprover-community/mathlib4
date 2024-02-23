43060b484a510b80dd28ce8f195f568140967c54
Joël Riou <joel.riou@universite-paris-saclay.fr>
2024-02-23 16:59:20 +0100

    fixed imports

--- Mathlib/CategoryTheory/Quotient/Linear.lean
+++ Mathlib/CategoryTheory/Quotient/Linear.lean
@@ -3,7 +3,7 @@ Copyright (c) 2024 Joël Riou. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joël Riou
 -/
-import Mathlib.CategoryTheory.Quotient.Preadditive
+import Mathlib.CategoryTheory.Quotient
 import Mathlib.CategoryTheory.Linear.LinearFunctor
 
 /-!
