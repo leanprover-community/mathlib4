e9113fe930607705eb1b268b49ad9e832ef55ebb
Joël Riou <joel.riou@universite-paris-saclay.fr>
2024-02-23 13:17:03 +0100

    feat(Algebra/Homology): action of a bifunctor on cochain complexes and shifts

---
+++ Mathlib/Algebra/Homology/BifunctorShift.lean
@@ -0,0 +1,118 @@
+/-
+Copyright (c) 2024 Joël Riou. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Joël Riou
+-/
+import Mathlib.Algebra.Homology.Bifunctor
+import Mathlib.Algebra.Homology.TotalComplexShift
+
+/-!
+# Behavior of the action of a bifunctor on cochain complexes with respect to shifts
+
+In this file, given cochain complexes `K₁ : CochainComplex C₁ ℤ`, `K₂ : CochainComplex C₂ ℤ` and
+a functor `F : C₁ ⥤ C₂ ⥤ D`, we define an isomorphism of cochain complexes in `D`:
+- `CochainComplex.mapBifunctorShift₁Iso K₁ K₂ F x` of type
+`(mapBifunctor (K₁⟦x⟧) K₂ F) ≅ (mapBifunctor K₁ K₂ F)⟦x⟧` for `x : ℤ`.
+- `CochainComplex.mapBifunctorShift₂Iso K₁ K₂ F y` of type
+`(mapBifunctor K₁ (K₂⟦y⟧) F) ≅ (mapBifunctor K₁ K₂ F)⟦y⟧` for `y : ℤ`.
+
+## TODO
+
+- obtain various compatibilities
+
+-/
+
+open CategoryTheory Limits
+
+variable {C₁ C₂ D : Type*} [Category C₁] [Category C₂] [Category D]
+
+namespace NatTrans
+
+variable {F G : C₁ ⥤ D} [Preadditive D]
+
+@[simp]
+lemma zsmul_app (a : ℤ) (φ : F ⟶ G) (X : C₁) : (a • φ).app X = a • φ.app X := by
+  sorry
+
+@[simp]
+lemma units_zsmul_app (a : ℤˣ) (φ : F ⟶ G) (X : C₁) : (a • φ).app X = a • φ.app X := by
+  apply zsmul_app
+
+end NatTrans
+
+namespace HomologicalComplex
+
+variable {I : Type*} {c : ComplexShape I} [Preadditive D]
+  {K L : HomologicalComplex D c}
+
+@[simp]
+lemma units_zsmul_f (a : ℤˣ) (φ : K ⟶ L) (i : I) : (a • φ).f i = a • φ.f i := rfl
+
+end HomologicalComplex
+
+namespace CochainComplex
+
+section
+
+variable [HasZeroMorphisms C₁] [HasZeroMorphisms C₂]
+  (K₁ : CochainComplex C₁ ℤ) (K₂ : CochainComplex C₂ ℤ) [Preadditive D]
+  (F : C₁ ⥤ C₂ ⥤ D) [F.PreservesZeroMorphisms]
+  [∀ (X₁ : C₁), (F.obj X₁).PreservesZeroMorphisms]
+
+abbrev HasMapBifunctor := HomologicalComplex.HasMapBifunctor K₁ K₂ F (ComplexShape.up ℤ)
+
+noncomputable abbrev mapBifunctor [HasMapBifunctor K₁ K₂ F] : CochainComplex D ℤ :=
+  HomologicalComplex.mapBifunctor K₁ K₂ F (ComplexShape.up ℤ)
+
+end
+
+section
+
+variable [Preadditive C₁] [HasZeroMorphisms C₂] [Preadditive D]
+  (K₁ : CochainComplex C₁ ℤ) (K₂ : CochainComplex C₂ ℤ)
+  (F : C₁ ⥤ C₂ ⥤ D) [F.Additive] [∀ (X₁ : C₁), (F.obj X₁).PreservesZeroMorphisms] (x : ℤ)
+  [HasMapBifunctor K₁ K₂ F] [HasMapBifunctor (K₁⟦x⟧) K₂ F]
+  [HomologicalComplex₂.HasTotal ((HomologicalComplex₂.shiftFunctor₁ D x).obj
+    (((F.mapBifunctorHomologicalComplex _ _ ).obj K₁).obj K₂)) (ComplexShape.up ℤ)]
+
+def mapBifunctorHomologicalComplexShift₁Iso :
+  ((F.mapBifunctorHomologicalComplex _ _).obj (K₁⟦x⟧)).obj K₂ ≅
+    (HomologicalComplex₂.shiftFunctor₁ D x).obj
+      (((F.mapBifunctorHomologicalComplex _ _).obj K₁).obj K₂) :=
+  HomologicalComplex.Hom.isoOfComponents (fun i₁ => Iso.refl _)
+
+noncomputable def mapBifunctorShift₁Iso :
+    (mapBifunctor (K₁⟦x⟧) K₂ F) ≅ (mapBifunctor K₁ K₂ F)⟦x⟧ :=
+  HomologicalComplex₂.total.mapIso (mapBifunctorHomologicalComplexShift₁Iso K₁ K₂ F x) _ ≪≫
+    (((F.mapBifunctorHomologicalComplex _ _).obj K₁).obj K₂).totalShift₁Iso x
+
+end
+
+section
+
+variable [HasZeroMorphisms C₁] [Preadditive C₂] [Preadditive D]
+  (K₁ : CochainComplex C₁ ℤ) (K₂ : CochainComplex C₂ ℤ)
+  (F : C₁ ⥤ C₂ ⥤ D) [F.PreservesZeroMorphisms]
+  [∀ (X₁ : C₁), (F.obj X₁).Additive]
+  (y : ℤ)
+  [HasMapBifunctor K₁ K₂ F] [HasMapBifunctor K₁ (K₂⟦y⟧) F]
+  [HomologicalComplex₂.HasTotal
+    ((HomologicalComplex₂.shiftFunctor₂ D y).obj
+      (((F.mapBifunctorHomologicalComplex _ _ ).obj K₁).obj K₂)) (ComplexShape.up ℤ)]
+
+def mapBifunctorHomologicalComplexShift₂Iso :
+  ((F.mapBifunctorHomologicalComplex _ _).obj K₁).obj (K₂⟦y⟧) ≅
+    (HomologicalComplex₂.shiftFunctor₂ D y).obj
+      (((F.mapBifunctorHomologicalComplex _ _).obj K₁).obj K₂) :=
+  HomologicalComplex.Hom.isoOfComponents
+    (fun i₁ =>  HomologicalComplex.Hom.isoOfComponents (fun i₂ => Iso.refl _))
+
+noncomputable def mapBifunctorShift₂Iso :
+    (mapBifunctor K₁ (K₂⟦y⟧) F) ≅ (mapBifunctor K₁ K₂ F)⟦y⟧ :=
+  HomologicalComplex₂.total.mapIso
+    (mapBifunctorHomologicalComplexShift₂Iso K₁ K₂ F y) (ComplexShape.up ℤ) ≪≫
+    (((F.mapBifunctorHomologicalComplex _ _).obj K₁).obj K₂).totalShift₂Iso y
+
+end
+
+end CochainComplex
