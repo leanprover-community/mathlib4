fec1a6461080dda51209582f0b3bf59c652585b6
Wen Yang <yang-wen@139.com>
2024-02-23 22:32:59 +0000

    feat(LinearAlgebra/Matrix): define projective special linear group (#7791)
    Co-authored-by: Mario Carneiro <di.gama@gmail.com>
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -2553,6 +2553,7 @@ import Mathlib.LinearAlgebra.Matrix.NonsingularInverse
 import Mathlib.LinearAlgebra.Matrix.Orthogonal
 import Mathlib.LinearAlgebra.Matrix.Polynomial
 import Mathlib.LinearAlgebra.Matrix.PosDef
+import Mathlib.LinearAlgebra.Matrix.ProjectiveSpecialLinearGroup
 import Mathlib.LinearAlgebra.Matrix.Reindex
 import Mathlib.LinearAlgebra.Matrix.SchurComplement
 import Mathlib.LinearAlgebra.Matrix.SesquilinearForm
