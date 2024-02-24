d44deafab3bb145b4391935fedf7ecec69712856
Antoine Chambert-Loir <antoine.chambert-loir@math.univ-paris-diderot.fr>
2024-02-24 20:22:59 +0100

    fail better

--- Mathlib/GroupTheory/Perm/Option.lean
+++ Mathlib/GroupTheory/Perm/Option.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Eric Wieser
 -/
 import Mathlib.Data.Fintype.Perm
--- import Mathlib.GroupTheory.Perm.Sign
+import Mathlib.GroupTheory.Perm.Sign
 import Mathlib.Data.Fintype.Prod
 import Mathlib.Data.Fintype.Option
 --
