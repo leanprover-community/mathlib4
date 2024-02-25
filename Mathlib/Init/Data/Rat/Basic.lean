b3f281071d98a7bddff9653b3a565c4291beced4
Scott Morrison <scott.morrison@gmail.com>
2024-02-25 21:42:26 +1100

    fix

--- Mathlib/Init/Data/Rat/Basic.lean
+++ Mathlib/Init/Data/Rat/Basic.lean
@@ -11,7 +11,5 @@ import Mathlib.Mathport.Rename
 # Align statements for declarations from Std
 -/
 
-#align rat.ext Rat.ext
 #align rat Rat
-#align rat.ext_iff Rat.ext_iff
 #align rat.floor Rat.floor
