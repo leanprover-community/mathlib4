02bfb16d53f0054dac1805542b14df26150c98e4
RemyDegenne <Remydegenne@gmail.com>
2024-02-25 08:42:03 +0100

    feat: density of a finite kernel wrt another kernel

--- Mathlib.lean
+++ Mathlib.lean
@@ -3127,6 +3127,7 @@ import Mathlib.Probability.Kernel.CondCdf
 import Mathlib.Probability.Kernel.CondDistrib
 import Mathlib.Probability.Kernel.Condexp
 import Mathlib.Probability.Kernel.Disintegration
+import Mathlib.Probability.Kernel.Disintegration.Density
 import Mathlib.Probability.Kernel.IntegralCompProd
 import Mathlib.Probability.Kernel.Invariance
 import Mathlib.Probability.Kernel.MeasurableIntegral
