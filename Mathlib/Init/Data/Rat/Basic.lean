2344f433e56433feed173f1612c177530744f0f4
Scott Morrison <scott.morrison@gmail.com>
2024-02-25 22:24:03 +1100

    fix

--- Mathlib/Init/Data/Rat/Basic.lean
+++ Mathlib/Init/Data/Rat/Basic.lean
@@ -4,14 +4,14 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johan Commelin
 -/
 
-import Std.Data.Rat.Basic
+import Std.Data.Rat.Lemmas
 import Mathlib.Mathport.Rename
 
 /-!
 # Align statements for declarations from Std
 -/
 
-#noalign rat.ext
+#align rat.ext Rat.ext
 #align rat Rat
 #noalign rat.ext_iff
 #align rat.floor Rat.floor
