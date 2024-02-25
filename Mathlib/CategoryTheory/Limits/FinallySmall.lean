e6d1bfb9775f92d91aeeb6588588d79d2abf9bd3
Markus Himmel <markus@himmel-villmar.de>
2024-02-25 09:07:07 +0100

    Add code

--- Mathlib/CategoryTheory/Limits/FinallySmall.lean
+++ Mathlib/CategoryTheory/Limits/FinallySmall.lean
@@ -3,7 +3,7 @@ Copyright (c) 2023 Markus Himmel. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Markus Himmel
 -/
-import Mathlib.CategoryTheory.Limits.Final
+import Mathlib.CategoryTheory.Filtered.Final
 
 /-!
 # Finally small categories
@@ -121,6 +121,60 @@ theorem initiallySmall_of_initial_of_essentiallySmall [EssentiallySmall.{w} K] :
   initiallySmall_of_initial_of_initiallySmall F
 
 end InitiallySmall
+
+section WeaklyTerminal
+
+variable (J : Type u) [Category.{v} J]
+
+theorem FinallySmall.exists_small_weakly_terminal_set [FinallySmall.{w} J] :
+    ∃ (s : Set J) (_ : Small.{w} s), ∀ i, ∃ j ∈ s, Nonempty (i ⟶ j) := by
+  refine ⟨Set.range (fromFinalModel J).obj, inferInstance, fun i => ?_⟩
+  obtain ⟨f⟩ : Nonempty (StructuredArrow i (fromFinalModel J)) := IsConnected.is_nonempty
+  exact ⟨(fromFinalModel J).obj f.right, Set.mem_range_self _, ⟨f.hom⟩⟩
+
+variable {J} in
+theorem finallySmall_of_small_weakly_terminal_set [IsFilteredOrEmpty J] (s : Set J) [Small.{v} s]
+    (hs : ∀ i, ∃ j ∈ s, Nonempty (i ⟶ j)) : FinallySmall.{v} J := by
+  suffices Functor.Final (fullSubcategoryInclusion (· ∈ s)) from
+    finallySmall_of_final_of_essentiallySmall (fullSubcategoryInclusion (· ∈ s))
+  refine Functor.final_of_exists_of_isFiltered_of_fullyFaithful _ (fun i => ?_)
+  obtain ⟨j, hj₁, hj₂⟩ := hs i
+  exact ⟨⟨j, hj₁⟩, hj₂⟩
+
+theorem finallySmall_iff_exists_small_weakly_terminal_set [IsFilteredOrEmpty J] :
+    FinallySmall.{v} J ↔ ∃ (s : Set J) (_ : Small.{v} s), ∀ i, ∃ j ∈ s, Nonempty (i ⟶ j) := by
+  refine ⟨fun _ => FinallySmall.exists_small_weakly_terminal_set _, fun h => ?_⟩
+  rcases h with ⟨s, hs, hs'⟩
+  exact finallySmall_of_small_weakly_terminal_set s hs'
+
+end WeaklyTerminal
+
+section WeaklyInitial
+
+variable (J : Type u) [Category.{v} J]
+
+theorem InitiallySmall.exists_small_weakly_initial_set [InitiallySmall.{w} J] :
+    ∃ (s : Set J) (_ : Small.{w} s), ∀ i, ∃ j ∈ s, Nonempty (j ⟶ i) := by
+  refine ⟨Set.range (fromInitialModel J).obj, inferInstance, fun i => ?_⟩
+  obtain ⟨f⟩ : Nonempty (CostructuredArrow (fromInitialModel J) i) := IsConnected.is_nonempty
+  exact ⟨(fromInitialModel J).obj f.left, Set.mem_range_self _, ⟨f.hom⟩⟩
+
+variable {J} in
+theorem initiallySmall_of_small_weakly_initial_set [IsCofilteredOrEmpty J] (s : Set J) [Small.{v} s]
+    (hs : ∀ i, ∃ j ∈ s, Nonempty (j ⟶ i)) : InitiallySmall.{v} J := by
+  suffices Functor.Initial (fullSubcategoryInclusion (· ∈ s)) from
+    initiallySmall_of_initial_of_essentiallySmall (fullSubcategoryInclusion (· ∈ s))
+  refine Functor.initial_of_exists_of_isCofiltered_of_fullyFaithful _ (fun i => ?_)
+  obtain ⟨j, hj₁, hj₂⟩ := hs i
+  exact ⟨⟨j, hj₁⟩, hj₂⟩
+
+theorem initiallySmall_iff_exists_small_weakly_initial_set [IsCofilteredOrEmpty J] :
+    InitiallySmall.{v} J ↔ ∃ (s : Set J) (_ : Small.{v} s), ∀ i, ∃ j ∈ s, Nonempty (j ⟶ i) := by
+  refine ⟨fun _ => InitiallySmall.exists_small_weakly_initial_set _, fun h => ?_⟩
+  rcases h with ⟨s, hs, hs'⟩
+  exact initiallySmall_of_small_weakly_initial_set s hs'
+
+end WeaklyInitial
 
 namespace Limits
 
