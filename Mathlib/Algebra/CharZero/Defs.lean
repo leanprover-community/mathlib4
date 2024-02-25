925576c7c37cee6c28266b23b7a1af29aa9537b3
timotree3 <timorcb@gmail.com>
2024-02-24 23:46:09 -0500

    remove unused import

--- Mathlib/Algebra/CharZero/Defs.lean
+++ Mathlib/Algebra/CharZero/Defs.lean
@@ -3,7 +3,6 @@ Copyright (c) 2014 Mario Carneiro. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro
 -/
-import Mathlib.Init.Data.Nat.Lemmas
 import Mathlib.Data.Int.Cast.Defs
 import Mathlib.Tactic.Cases
 import Mathlib.Algebra.NeZero
