5e83a6345653f93e18142f1df2306bbc31cf08cb
Matthew Ballard <matt@mrb.email>
2024-02-24 20:13:44 -0500

    fix

--- Mathlib/RingTheory/Subring/Units.lean
+++ Mathlib/RingTheory/Subring/Units.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2021 Chris Birbeck. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Damiano Testa
+Authors: Chris Birbeck
 -/
 
 import Mathlib.GroupTheory.Submonoid.Order
