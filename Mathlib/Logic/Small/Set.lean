d03fe16a04e63cb9ff4d16619a48ea13ab792140
Markus Himmel <markus@himmel-villmar.de>
2024-02-25 14:59:22 +0100

    Code review suggestions, switch to instance

--- Mathlib/Logic/Small/Set.lean
+++ Mathlib/Logic/Small/Set.lean
@@ -7,15 +7,20 @@ import Mathlib.Data.Set.Lattice
 import Mathlib.Logic.Small.Basic
 
 /-!
-# Additional results about small sets
+# Additional results about `Small` on coerced sets
 -/
 
 universe u v w
 
-theorem small_iUnion {α : Type v} {ι : Type w} [Small.{u} ι] (s : ι → Set α)
+instance small_iUnion {α : Type v} {ι : Type w} [Small.{u} ι] (s : ι → Set α)
     [∀ i, Small.{u} (s i)] : Small.{u} (⋃ i, s i) :=
   small_of_surjective <| Set.sigmaToiUnion_surjective _
 
-theorem small_sUnion {α : Type v} (s : Set (Set α)) [Small.{u} s] [∀ t : s, Small.{u} t] :
+instance small_sUnion {α : Type v} (s : Set (Set α)) [Small.{u} s] [∀ t : s, Small.{u} t] :
     Small.{u} (⋃₀ s) :=
   Set.sUnion_eq_iUnion ▸ small_iUnion _
+
+instance small_union {α : Type v} (s t : Set α) [Small.{u} s] [Small.{u} t] :
+    Small.{u} (s ∪ t : Set α) := by
+  rw [← Subtype.range_val (s := s), ← Subtype.range_val (s := t), ← Set.Sum.elim_range]
+  infer_instance
