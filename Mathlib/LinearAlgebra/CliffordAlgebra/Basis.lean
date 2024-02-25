bad5bb1aa76a34b5a84f1c8c6983d3c4b9b34dba
Eric Wieser <wieser.eric@gmail.com>
2024-02-25 14:22:37 +0000

    progress

--- Mathlib/LinearAlgebra/CliffordAlgebra/Basis.lean
+++ Mathlib/LinearAlgebra/CliffordAlgebra/Basis.lean
@@ -6,55 +6,103 @@ Authors: Eric Wieser
 import Mathlib.LinearAlgebra.CliffordAlgebra.Basic
 import Mathlib.Data.Sign
 
+/-!
+# A basis for the Clifford algebra
+
+This file constructs `Model ι B`, which is a model for `CliffordAlgebra (_ : ι →₀ R)` that also
+works as a basis.
+-/
+
 variable  (ι : Type) [LinearOrder ι]
 
 noncomputable section
 
--- a list of indices, sorted
+/-- A sorted list of indices.
+
+This is chosen instead of `Finset ι` as it makes computing signs more efficient. -/
 abbrev Model.Index := {l : List ι // l.Sorted (· < ·) }
 
 
 variable {R : Type*} [CommRing R]
 
-def Model (_B : ι → ι → R) := Model.Index ι →₀ R
+set_option linter.unusedVariables false in
+/-- A model of the clifford algebra.
 
+For a basis `b : Basis ι R M`, we have `B i i = Q (b i)` and `B i j = Q.polar (b i) (b j)`.
+Note that in some sense this means the diagonal is halved.
+-/
+@[nolint unusedArguments]
+def Model (B : ι → ι → R) : Type _ := Model.Index ι →₀ R
 
 variable (B :  ι → ι → R)
 instance : AddCommGroup (Model ι B) := inferInstanceAs <| AddCommGroup (Model.Index ι →₀ R)
 instance : Module R (Model ι B) := inferInstanceAs <| Module R (Model.Index ι →₀ R)
 
+namespace Model
+
 variable {B ι} in
-def Model.ofFinsupp : (Model.Index ι →₀ R) ≃ₗ[R] Model ι B :=
+/-- Casting function to interpret the model as a finitely-supported function. -/
+def ofFinsupp : (Model.Index ι →₀ R) ≃ₗ[R] Model ι B :=
   LinearEquiv.refl _ _
-
-instance : One (Model ι B) where
-  one := Model.ofFinsupp <| Finsupp.single ⟨[], by simp⟩ 1
-
-@[simp]
-lemma Model.ofFinsupp_symm_one :
-    Model.ofFinsupp.symm (1 : Model ι B) = Finsupp.single ⟨[], by simp⟩ 1 := by
-  rfl
 
 variable {ι}
 
-@[simps]
-def Model.Index.single (i : ι) : Model.Index ι := ⟨[i], by simp⟩
+/-- The index corresponding to a single basis vector `i`. -/
+@[match_pattern]
+abbrev Index.nil : Model.Index ι := ⟨[], by simp⟩
 
-def Model.single (i : ι) : Model ι B :=
-  Model.ofFinsupp <| Finsupp.single (Model.Index.single i) 1
+/-- The index corresponding to a single basis vector `i`. -/
+@[simps]
+def Index.single (i : ι) : Model.Index ι := ⟨[i], by simp⟩
+
+/-- The element corresponding to a single basis element `is`. -/
+def single (is : Model.Index ι) (r : R) : Model ι B :=
+  Model.ofFinsupp <| Finsupp.single is r
+/-- `Model.single` as a linear map. -/
+def lsingle (is : Model.Index ι) : R →ₗ[R] Model ι B :=
+  Model.ofFinsupp (B := B) ∘ₗ Finsupp.lsingle is
 
 @[simp]
-lemma Model.ofFinsupp_single_single (i : ι) :
-  Model.ofFinsupp (Finsupp.single (.single i) 1) = Model.single B i := rfl
+theorem lsingle_apply (is : Model.Index ι) (r : R) :
+    Model.lsingle B is r = Model.single B is r := rfl
 
-#check Finsupp.supported
+@[ext]
+theorem lhom_ext {N : Type*} [AddCommMonoid N] [Module R N] ⦃f g : Model ι B →ₗ[R] N⦄
+    (h : ∀ is, f ∘ₗ Model.lsingle B is = g ∘ₗ Model.lsingle B is) : f = g :=
+  Finsupp.lhom_ext' h
+
+@[simp]
+lemma ofFinsupp_single (is : Model.Index ι) (r : R):
+    Model.ofFinsupp (Finsupp.single is r) = Model.single B is r := rfl
+
+@[simp]
+lemma ofFinsupp_symm_single (is : Model.Index ι) (r : R) :
+    Model.ofFinsupp.symm (Model.single B is r) = Finsupp.single is r := rfl
+
+instance : One (Model ι B) where
+  one := Model.single B .nil 1
+
+lemma one_def : 1 = Model.single B .nil 1 := rfl
+
+@[simp]
+lemma ofFinsupp_symm_one :
+    Model.ofFinsupp.symm (1 : Model ι B) = Finsupp.single .nil 1 := by
+  rfl
+
+lemma single_nil_eq_smul_one (r : R) : Model.single B .nil r = r • 1 :=
+  (Model.ofFinsupp (B := B)).symm.injective <| by simp
+
+
 
 open List in
-/-- Multiply a single vector by a basis element. -/
-def Model.Index.singleMul (i : ι) (l : Model.Index ι) :
-    (Finsupp.supported _ R {i' | i'.1.erase i <+ l}).comap (Model.ofFinsupp (ι := ι) (B := B)).symm :=
+/-- Multiply a single vector `i` by a basis element `l`.
+
+The support of the result is `l` with `i` inserted at the appropriate point. -/
+def Index.singleMul (i : ι) (l : Model.Index ι) :
+    (Finsupp.supported _ R {i' | i'.1.erase i <+ l}).comap
+      (Model.ofFinsupp (ι := ι) (B := B)).symm :=
   match l with
-  | ⟨[], h⟩ => ⟨.single B i, Finsupp.single_mem_supported _ _ <| show _ <+ _ by simp⟩
+  | .nil => ⟨.single B (.single i) 1, Finsupp.single_mem_supported _ _ <| show _ <+ _ by simp⟩
   | ⟨j :: xs, h⟩ =>
     ltByCases i j
       (fun hlt : i < j =>
@@ -81,128 +129,146 @@ def Model.Index.singleMul (i : ι) (l : Model.Index ι) :
                       aux.subset <| (List.mem_erase_of_ne hij'.symm).2 hj'
                 haveI pfoo := foo.prop
                 ⟨foo, fun i' hi' => show _ <+ _ from
-                  have pfooi : _ <+ _ := ((pfoo hi').erase i).trans aux
-                  by
+                  have pfooi : _ <+ _ := ((pfoo hi').erase i).trans aux by
                     have pf' := pfoo hi'
                     dsimp at *
                     rw [List.erase_comm] at pfooi
                     sorry⟩))))
 where
   ofLt (i : ι) (l : Model.Index ι) (h : ∀ j ∈ l.1, i < j) :
-      (Finsupp.supported _ R {i' | i'.1.erase i <+ l}).comap (Model.ofFinsupp (ι := ι) (B := B)).symm :=
+      (Finsupp.supported _ R {i' | i'.1.erase i <+ l}).comap
+        (Model.ofFinsupp (ι := ι) (B := B)).symm :=
     ⟨.ofFinsupp <| Finsupp.single ⟨i :: l, List.sorted_cons.mpr ⟨h, l.prop⟩⟩ 1,
       Finsupp.single_mem_supported R _ <| show _ <+ _ by simp⟩
 
--- def Model.Index.reverseRecOn {P : Model.Index ι → Sort*}
---   (h : P ⟨[], .nil⟩)
---   (h : ∀ m : Model.Index ι P ⟨[], .nil⟩)
---   (l : Model.Index ι) : P l :=
+@[simp]
+lemma Index.singleMul_nil (i : ι) :
+    Model.Index.singleMul B i (.nil) = Model.single B (.single i) 1 := rfl
 
+lemma Index.singleMul_single_same (i : ι) :
+    Model.Index.singleMul B i (.single i) = B i i • (1 : Model ι B) := by
+  erw [Model.Index.singleMul]
+  rw [ltByCases]
+  simp only [single_coe, Submodule.comap_coe, lt_self_iff_false, ↓reduceDite, ofFinsupp_single,
+    Model.single_nil_eq_smul_one]
 
-def Model.Index.mul (l₁ l₂ : Model.Index ι) : Model ι B :=
+/-- Multiply two basis elements together to get an element of the model. -/
+def Index.mul (l₁ l₂ : Model.Index ι) : Model ι B :=
   l₁.val.reverseRecOn
-    (.ofFinsupp <| Finsupp.single l₂ 1)
-    (fun _ i x => x.sum fun ind val => val • ind.singleMul B i)
+    (.single B l₂ 1)
+    (fun _ i x => (Model.ofFinsupp.symm x).sum fun ind val => val • ind.singleMul B i)
 
-#exit
-def merge_with_sign : SignType → List ι → List ι → List ι × SignType
-  | σ, [], l' => ⟨l',σ⟩
-  | σ, l, [] => ⟨l,σ⟩
-  | σ, a :: l, b :: l' =>
-    if a=b
-    then ⟨[],0⟩ -- the first entry will be ignored later anyway
-    else if a < b
-    then ⟨a :: (merge_with_sign σ l (b :: l')).1,σ⟩
-    else
-      let ⟨m,s⟩ := merge_with_sign σ (a :: l) l'
-      ⟨b :: m,s*(-1)^(List.length l +1)⟩
-  termination_by s l₁ l₂ => List.length l₁ +  List.length l₂
+@[simp]
+lemma Index.nil_mul_single (is : Model.Index ι) :
+    Model.Index.mul B (.nil) is = .single B is 1 := by
+  rw [Model.Index.mul]
+  rw [List.reverseRecOn]
+  simp_rw [List.reverse_nil]
+  simp [Model.Index.singleMul_single_same]
 
-example : merge_with_sign 1 [1] [2] = ⟨[1,2],1⟩  := by
-  unfold merge_with_sign
-  simp
-
-example : merge_with_sign 1 [2] [1] = ⟨[1,2],-1⟩  := by
-  unfold merge_with_sign
-  simp
-
-example : merge_with_sign 1 [2] [2] = ⟨[],0⟩  := by
-  unfold merge_with_sign
-  simp
-
-example : merge_with_sign 1 [4] [1,2,3] = ⟨[1,2,3,4],-1⟩  := by
-  unfold merge_with_sign
-  simp
-
-
-def mul_helper : Model.Index ι → Model.Index ι → Model.Index ι × SignType := by
-  intro l k
-  let ms := merge_with_sign 1 l.1 k.1
-  refine (⟨ms.1,?_⟩,ms.2)
+@[simp]
+lemma Index.single_mul_nil (is : Model.Index ι) :
+    Model.Index.mul B is (.nil) = .single B is 1 := by
+  rw [Model.Index.mul]
   sorry
 
-lemma single_mul_single_helper (i : ι) :
-  (mul_helper (Model.Index.single i) (Model.Index.single i)).2 = 0 := by
-  sorry
+@[simp]
+lemma Index.single_mul_single_same (i : ι) :
+    Model.Index.mul B (.single i) (.single i) = B i i • (1 : Model ι B) := by
+  rw [Model.Index.mul, single_coe]
+  rw [List.reverseRecOn]
+  simp_rw [List.reverse_singleton, List.reverse_nil]
+  rw [List.reverseRecOn]
+  simp_rw [List.reverse_nil]
+  simp [Model.Index.singleMul_single_same]
 
--- def list_sort_concat
 open scoped BigOperators
 
-variable {Q}
-def mul (v w : Model ι B) : Model ι B :=
-  (Model.ofFinsupp.symm v).sum fun i vi ↦
-    (Model.ofFinsupp.symm w).sum fun j wj ↦
-      let ⟨k,s⟩:=mul_helper i j
-      Model.ofFinsupp <| Finsupp.single k (s * vi * wj)
+protected def mul : Model ι B →ₗ[R] Model ι B →ₗ[R] Model ι B :=
+  (Finsupp.lsum R fun i => LinearMap.flip <|
+    Finsupp.lsum R fun j => LinearMap.flip <|
+      (LinearMap.mul R R).compr₂ (LinearMap.toSpanSingleton _ _ (i.mul B j))).compl₁₂
+        (ofFinsupp (B := B)).symm.toLinearMap
+        (ofFinsupp (B := B)).symm.toLinearMap
 
 instance : Mul (Model ι B) where
-  -- multiply pairwise
-  mul v w :=
-  (Model.ofFinsupp.symm v).sum fun i vi ↦
-    (Model.ofFinsupp.symm w).sum fun j wj ↦
-      let ⟨k,s⟩:=mul_helper i j
-      Model.ofFinsupp <| Finsupp.single k (s * vi * wj)
+  mul v w := Model.mul B v w
 
-#print instMulModel
+lemma mul_def (v w : Model ι B) : v * w = Model.mul B v w := rfl
 
-lemma single_mul_single (i : ι) : Model.single B i * Model.single B i = 0 := by
-  change Finsupp.sum _ _ = _
-  dsimp only [mul,Model.single]
-  simp
+@[simp]
+lemma single_mul_single (i j : Model.Index ι) (r s : R):
+    Model.single B i r * Model.single B j s = (r * s) • i.mul B j := by
+  change Model.mul _ _ _ = _
+  simp [Model.mul]
+
+-- Associativity
+lemma single_mul_indexMul (i j k : Model.Index ι) (r : R) :
+    single B i r * j.mul B k = i.mul B j * single B k r :=
   sorry
 
-#check Finsupp.sum_single_index
 
+/-! ### Algebraic structure instances -/
+
+instance : NonUnitalNonAssocRing (Model ι B) where
+  left_distrib _ _ _ := map_add _ _ _
+  right_distrib _ _ _ := LinearMap.map_add₂ _ _ _ _
+  mul_zero _ := map_zero _
+  zero_mul _ := LinearMap.map_zero₂ _ _
+
+instance : SMulCommClass R (Model ι B) (Model ι B) where
+  smul_comm r x y := (LinearMap.map_smul (Model.mul B x) r y).symm
+
+instance : IsScalarTower R (Model ι B) (Model ι B) where
+  smul_assoc r x y := LinearMap.map_smul₂ (Model.mul B) r x y
+
+instance : NonAssocRing (Model ι B) where
+  one_mul x := by
+    suffices Model.mul B 1 = .id from DFunLike.congr_fun this x
+    ext xi
+    simp [one_def, ← mul_def]
+  mul_one x := by
+    suffices (Model.mul B).flip 1 = .id from DFunLike.congr_fun this x
+    ext xi
+    simp [one_def, ← mul_def]
 
 instance : Ring (Model ι B) where
-  -- inheritance in lean 4 is (somewhat) broken currently
-  __ := inferInstanceAs (AddCommGroup (Model ι B))
-
-  left_distrib := sorry
-  right_distrib := sorry
-
-  zero_mul := sorry
-  mul_zero := sorry
-  mul_assoc := sorry
-  one_mul := sorry
-  mul_one := sorry
+  __ := inferInstanceAs (NonAssocRing (Model ι B))
+  mul_assoc x y z := by
+    -- restate as an equality of morphisms so that we can use `ext`
+    suffices LinearMap.llcomp R _ _ _ (Model.mul B) ∘ₗ (Model.mul B) =
+        (LinearMap.llcomp R _ _ _ LinearMap.lflip <|
+          LinearMap.llcomp R _ _ _ (Model.mul B).flip ∘ₗ (Model.mul B)).flip from
+      DFunLike.congr_fun (DFunLike.congr_fun (DFunLike.congr_fun this x) y) z
+    ext xi yi zi
+    dsimp [← mul_def]
+    simp [single_mul_indexMul]
 
 instance : Algebra R (Model ι B) where
-  toFun := Finsupp.single ⟨[], by simp⟩
+  toFun r := .single B .nil r
   map_one' := rfl
-  map_mul' := sorry
-  -- we came up with this by using `by simp`
+  map_mul' r s := by
+    rw [single_mul_single]
+    simp only [Model.single_nil_eq_smul_one]
+    rfl
   map_zero' := Finsupp.single_zero _
-  map_add' x y := sorry
-  commutes' r x := sorry
-  smul_def' r x := sorry
+  map_add' x y := Finsupp.single_add _ _ _
+  commutes' r x := by
+    dsimp
+    rw [Model.single_nil_eq_smul_one, smul_one_mul, mul_smul_one]
+  smul_def' r x := by
+    dsimp
+    rw [Model.single_nil_eq_smul_one, smul_one_mul]
 
-variable {r: R}
 @[simp]
-lemma Model.ofFinsupp_symm_algebra_map :
+lemma Model.ofFinsupp_symm_algebra_map (r : R) :
     Model.ofFinsupp.symm (algebraMap R (Model ι B) r) = Finsupp.single ⟨[], by simp⟩ r := by
   rfl
 
+/-!
+Un-adapted from below
+
+-/
 
 noncomputable def model_of_free_vsp : (ι →₀ R) →ₗ[R] Model ι B :=
   Model.ofFinsupp.toLinearMap ∘ₗ Finsupp.lmapDomain R R (fun i ↦ Model.Index.single i)
@@ -214,7 +280,7 @@ lemma model_of_free_vsp_single (i : ι) :
   simp
 
 lemma two_vectors_square_zero (m: ι →₀ R) :
-  model_of_free_vsp m * model_of_free_vsp m = 0 := by
+    model_of_free_vsp m * model_of_free_vsp m = 0 := by
   sorry
 
 
@@ -276,7 +342,7 @@ lemma liftToFun_composed_single (i : ι) (f : (ι →₀ R) →ₗ[R] A) (hf) :
 
 
 @[ext high]
-theorem Model.hom_ext {f g : Model ι B →ₐ[R] A} :
+theorem hom_ext {f g : Model ι B →ₐ[R] A} :
     f.toLinearMap.comp (model_of_free_vsp) = g.toLinearMap.comp (model_of_free_vsp) → f = g := by
   intro h
   apply lift.symm.injective
