9cab416eb193a39250d672356159b08ec13097c7
madvorak <martin.dvorak@matfyz.cz>
2024-02-26 09:27:09 +0100

    @[simp] removed

--- Mathlib/Data/Fin/VecNotation.lean
+++ Mathlib/Data/Fin/VecNotation.lean
@@ -602,7 +602,6 @@ theorem tail_neg (a : Fin n.succ → α) : vecTail (-a) = -vecTail a :=
 
 end Neg
 
-@[simp]
 lemma const_fin1_eq (x : α) : (fun _ : Fin 1 => x) = ![x] :=
   (cons_fin_one x _).symm
 
