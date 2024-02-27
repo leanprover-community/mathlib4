dfa0dd037815e76836f218e1e2adb91763d8c062
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2024-02-27 10:52:05 +0000

    chore: make natDegree_sub_C rw_search test more robust (#11007)
    Sometimes it comes up with rw [@natDegree_sub, @sub_eq_neg_add, @natDegree_add_C, @natDegree_neg] instead.

--- test/RewriteSearch/Polynomial.lean
+++ test/RewriteSearch/Polynomial.lean
@@ -11,7 +11,7 @@ open Polynomial
 #guard_msgs in
 example {R : Type*} [Ring R] {p : Polynomial R} {a : R} :
     natDegree (p - C a) = natDegree p := by
-  rw_search [-Polynomial.natDegree_sub_C]
+  rw_search [-Polynomial.natDegree_sub_C, -sub_eq_neg_add]
 
 
 -- This one works, but is very slow:
