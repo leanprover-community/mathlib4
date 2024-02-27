a71364fa27c05777a940128a07a4bf484d6c8bc5
Riccardo Brasca <riccardo.brasca@gmail.com>
2024-02-27 13:47:14 +0100

    golf

--- Mathlib/NumberTheory/NumberField/Embeddings.lean
+++ Mathlib/NumberTheory/NumberField/Embeddings.lean
@@ -607,14 +607,7 @@ theorem card_add_two_mul_card_eq_rank :
 variable {K}
 
 theorem nrComplexPlaces_eq_zero_of_finrank_eq_one (h : finrank ℚ K = 1) :
-    NrComplexPlaces K = 0 := by
-  have := card_add_two_mul_card_eq_rank K
-  rw [h, Nat.add_eq_one_iff] at this
-  rcases this with (⟨-, H⟩ | ⟨-, H⟩)
-  · exfalso
-    exact (Nat.even_iff_not_odd.1 <| (even_iff_exists_two_mul 1).2 ⟨NrComplexPlaces K, H.symm⟩)
-      ⟨0, rfl⟩
-  · simpa using H
+    NrComplexPlaces K = 0 := by linarith [card_add_two_mul_card_eq_rank K]
 
 theorem nrRealPlaces_eq_one_of_finrank_eq_one (h : finrank ℚ K = 1) :
     NrRealPlaces K = 1 := by
