4ff5a989902f2a90eb8ac70a8410911c26056056
faenuccio <filippo.nuccio@univ-st-etienne.fr>
2024-02-27 11:19:00 +0100

    some comments

--- Mathlib/Topology/Category/TopCat/EffectiveEpi.lean
+++ Mathlib/Topology/Category/TopCat/EffectiveEpi.lean
@@ -10,7 +10,7 @@ import Mathlib.Topology.Category.TopCat.Limits.Pullbacks
 # Effective epimorphisms in `TopCat`
 
 This file proves the result `TopCat.effectiveEpi_iff_quotientMap`:
-The effective epimorphisms in `TopCat` are precisely the quotient maps
+The effective epimorphisms in `TopCat` are precisely the quotient maps.
 
 -/
 
@@ -22,11 +22,11 @@ namespace TopCat
 
 /--
 Implementation: If `π` is a morphism in `TopCat` which is a quotient map, then it is an effective
-epi. The theorem `TopCat.effectiveEpi_iff_quotientMap` should be used instead.
+epimorphism. The theorem `TopCat.effectiveEpi_iff_quotientMap` should be used instead of
+the definition.
 -/
 noncomputable
-def struct {B X : TopCat.{u}} (π : X ⟶ B) (hπ : QuotientMap π) :
-    EffectiveEpiStruct π where
+def QuotientEpiStruct {B X : TopCat.{u}} (π : X ⟶ B) (hπ : QuotientMap π) : EffectiveEpiStruct π where
   desc e h := hπ.lift e fun a b hab ↦
     DFunLike.congr_fun (h ⟨fun _ ↦ a, continuous_const⟩ ⟨fun _ ↦ b, continuous_const⟩
     (by ext; exact hab)) a
@@ -44,25 +44,21 @@ def struct {B X : TopCat.{u}} (π : X ⟶ B) (hπ : QuotientMap π) :
     rfl
 
 /-- The effective epimorphisms in `TopCat` are precisely the quotient maps. -/
-theorem effectiveEpi_iff_quotientMap
-    {B X : TopCat.{u}} (π : X ⟶ B) :
+theorem effectiveEpi_iff_quotientMap {B X : TopCat.{u}} (π : X ⟶ B) :
     EffectiveEpi π ↔ QuotientMap π := by
-  constructor
-  · intro
-    have hπ : RegularEpi π := inferInstance
-    let F := parallelPair hπ.left hπ.right
-    let i : B ≅ colimit F := hπ.isColimit.coconePointUniqueUpToIso (colimit.isColimit _)
-    have : QuotientMap (homeoOfIso i ∘ π) := by
-      constructor
-      · change Function.Surjective (π ≫ i.hom)
-        rw [← epi_iff_surjective]
-        infer_instance
-      · ext U
-        have : π ≫ i.hom = colimit.ι F WalkingParallelPair.one := by simp [← Iso.eq_comp_inv]
-        rw [isOpen_coinduced (f := (homeoOfIso i ∘ π)), coequalizer_isOpen_iff _ U, ← this]
-        rfl
-    simpa [← Function.comp.assoc] using (homeoOfIso i).symm.quotientMap.comp this
-  · intro hπ
-    exact ⟨⟨struct π hπ⟩⟩
+  refine ⟨fun _ ↦ ?_, fun hπ ↦ ⟨⟨QuotientEpiStruct π hπ⟩⟩⟩
+  have hπ : RegularEpi π := inferInstance
+  let F := parallelPair hπ.left hπ.right
+  let i : B ≅ colimit F := hπ.isColimit.coconePointUniqueUpToIso (colimit.isColimit _)
+  have : QuotientMap (homeoOfIso i ∘ π) := by
+    constructor
+    · change Function.Surjective (π ≫ i.hom)
+      rw [← epi_iff_surjective]
+      infer_instance
+    · ext U
+      have : π ≫ i.hom = colimit.ι F WalkingParallelPair.one := by simp [← Iso.eq_comp_inv]
+      rw [isOpen_coinduced (f := (homeoOfIso i ∘ π)), coequalizer_isOpen_iff _ U, ← this]
+      rfl
+  simpa [← Function.comp.assoc] using (homeoOfIso i).symm.quotientMap.comp this
 
 end TopCat
