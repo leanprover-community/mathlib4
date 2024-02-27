97890350c6f9c54b5f8f78ada631b78669e5fd64
David Loeffler <d.loeffler.01@cantab.net>
2024-02-27 14:32:05 +0100

    merge

--- Mathlib.lean
+++ Mathlib.lean
@@ -1465,6 +1465,7 @@ import Mathlib.Combinatorics.SimpleGraph.Girth
 import Mathlib.Combinatorics.SimpleGraph.Hasse
 import Mathlib.Combinatorics.SimpleGraph.IncMatrix
 import Mathlib.Combinatorics.SimpleGraph.Init
+import Mathlib.Combinatorics.SimpleGraph.LapMatrix
 import Mathlib.Combinatorics.SimpleGraph.Maps
 import Mathlib.Combinatorics.SimpleGraph.Matching
 import Mathlib.Combinatorics.SimpleGraph.Metric
