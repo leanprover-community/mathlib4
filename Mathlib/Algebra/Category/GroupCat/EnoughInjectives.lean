e702ea9bf4c58be39a8eb5381b0ca126ade5b6bd
Jujian Zhang <jujian.zhang1998@outlook.com>
2024-02-27 23:51:06 +0000

    fix

--- Mathlib/Algebra/Category/GroupCat/EnoughInjectives.lean
+++ Mathlib/Algebra/Category/GroupCat/EnoughInjectives.lean
@@ -36,18 +36,17 @@ instance enoughInjectives : EnoughInjectives (AddCommGroupCat.{u}) where
         injective_of_divisible _
       f := ⟨⟨fun a i ↦ ULift.up <| i a, by aesop⟩, by aesop⟩
       mono := (AddCommGroupCat.mono_iff_injective _).mpr <|
-      (injective_iff_map_eq_zero _).mpr fun a h0 ↦ by
-        refine eq_zero_of_ofSpanSingleton_apply_self a ?_ -- <|
+      (injective_iff_map_eq_zero _).mpr fun a h0 ↦ eq_zero_of_ofSpanSingleton_apply_self a <| by
         let f : of (ℤ ∙ a) ⟶ of (ULift.{u} <| AddCircle (1 : ℚ)) :=
-          ((ULift.moduleEquiv (R := ℤ)).symm.toLinearMap.toAddMonoidHom.comp <|
+          ((ULift.moduleEquiv (R := ℤ)).symm.toAddMonoidHom.comp <|
             ofSpanSingleton a)
-        suffices H : f ⟨a, Submodule.mem_span_singleton_self a⟩ = 0
-        · exact ULift.ext_iff _ _ |>.mp H
+        suffices H : f ⟨a, Submodule.mem_span_singleton_self a⟩ = 0 from
+          ULift.ext_iff _ _ |>.mp H
         let g : of (ℤ ∙ a) ⟶ A_ := AddSubgroupClass.subtype _
         have : Mono g := (mono_iff_injective _).mpr Subtype.val_injective
         erw [← DFunLike.congr_fun (Injective.comp_factorThru f g),
           show Injective.factorThru f g a = 0 from congr_fun h0
-            ((ULift.moduleEquiv (R := ℤ)).toLinearMap.toAddMonoidHom.comp
+            ((ULift.moduleEquiv (R := ℤ)).toAddMonoidHom.comp
               (Injective.factorThru f g))] }
 
 end AddCommGroupCat
