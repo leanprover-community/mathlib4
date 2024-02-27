70a45c220206171b9713dc270779230da749f5a3
Vierkantor <vierkantor@vierkantor.com>
2024-02-27 11:54:47 +0000

    Actually fix `FieldTheory/SeparableDegree.lean` now!

--- Mathlib/FieldTheory/SeparableDegree.lean
+++ Mathlib/FieldTheory/SeparableDegree.lean
@@ -642,10 +642,10 @@ theorem Polynomial.Separable.comap_minpoly_of_isSeparable [Algebra E K] [IsScala
   clear_value g
   have hx : x ∈ restrictScalars F E'⟮x⟯ := mem_adjoin_simple_self _ x
   have hzero : aeval x g = 0 := by
-    simpa only [E', ← h, aeval_map_algebraMap] using minpoly.aeval E x
+    simpa only [← show _ = minpoly E x from h, aeval_map_algebraMap] using minpoly.aeval E x
   have halg : IsIntegral E' x :=
     isIntegral_trans (IsSeparable.isAlgebraic F E).isIntegral _ hsep.isIntegral |>.tower_top
-  simp_rw [← h, separable_map] at hsep
+  simp_rw [← show _ = minpoly E x from h, separable_map] at hsep
   replace hsep := hsep.of_dvd <| minpoly.dvd _ _ hzero
   haveI : IsSeparable F E' := isSeparable_tower_bot_of_isSeparable F E' E
   haveI := (isSeparable_adjoin_simple_iff_separable _ _).2 hsep
