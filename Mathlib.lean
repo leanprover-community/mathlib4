5354fbd9c32c13b6176bb070de67234b7d21f551
Jujian Zhang <jujian.zhang1998@outlook.com>
2024-02-27 22:35:23 +0000

    feat(Algebra/Module/CharacterModule): definition of character module (#10767)
    For an `R`-module `M`, its character module is defined as the group homorphisms from `M` to the unit rational circle where the `R`-module structure is given by `rf(m) = f(rm)`. Let us denote the character module by `M*`
    
    Then each linear map `f : A -> B` gives a linear map `f*` from `B*` to `A*` by composition.
    When `f` is injective, `f*` is surjective.
    
    There is a natural isomorphism between `Hom(A, B*)` and `(A ⊗ B)*`
    
    
    
    
    Co-authored-by: Junyan Xu <junyanxu.math@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -327,6 +327,7 @@ import Mathlib.Algebra.Module.Algebra
 import Mathlib.Algebra.Module.Basic
 import Mathlib.Algebra.Module.BigOperators
 import Mathlib.Algebra.Module.Bimodule
+import Mathlib.Algebra.Module.CharacterModule
 import Mathlib.Algebra.Module.DedekindDomain
 import Mathlib.Algebra.Module.DirectLimitAndTensorProduct
 import Mathlib.Algebra.Module.Equiv
