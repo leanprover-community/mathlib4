4b673b15b8f69853e418beabc4482f766b126da8
David Loeffler <d.loeffler.01@cantab.net>
2024-02-27 16:39:13 +0100

    clean up imports

--- Mathlib/Combinatorics/SimpleGraph/LapMatrix.lean
+++ Mathlib/Combinatorics/SimpleGraph/LapMatrix.lean
@@ -4,9 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Adrian Wüthrich
 -/
 import Mathlib.Combinatorics.SimpleGraph.AdjMatrix
-import Mathlib.Combinatorics.SimpleGraph.IncMatrix
 import Mathlib.LinearAlgebra.Matrix.PosDef
-import Mathlib.LinearAlgebra.Matrix.IsDiag
 
 /-!
 # Laplacian Matrix
