16e7d1e6ad3d344f71c23a8f9ca3a7a50438ac3f
Christopher Hoskin <christopher.hoskin@gmail.com>
2024-02-28 20:55:45 +0000

    Fix

--- Mathlib/LinearAlgebra/SesquilinearForm/Hom.lean
+++ Mathlib/LinearAlgebra/SesquilinearForm/Hom.lean
@@ -3,7 +3,7 @@ Copyright (c) 2018 Andreas Swerdlow. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Andreas Swerdlow, Kexing Ying, Christopher Hoskin
 -/
-import Mathlib.LinearAlgebra.SesquilinearForm
+import Mathlib.LinearAlgebra.BilinearMap
 
 /-!
 # Bilinear form and linear maps
