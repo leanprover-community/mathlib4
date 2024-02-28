6ddb1efa3fdc59c205310b8abfcc815aa0a56e1d
Alessandro Iraci <SashaIr@users.noreply.github.com>
2024-02-28 23:04:01 +0100

    Computability of oneEquiv

--- Mathlib/Data/Sym/Basic.lean
+++ Mathlib/Data/Sym/Basic.lean
@@ -532,13 +532,17 @@ theorem mem_append_iff {s' : Sym α m} : a ∈ s.append s' ↔ a ∈ s ∨ a ∈
   Multiset.mem_add
 #align sym.mem_append_iff Sym.mem_append_iff
 
-open Multiset Classical in
 @[simps apply]
-noncomputable def one_equiv : α ≃ Sym α 1 where
-  toFun := fun i ↦ ⟨{i}, by simp⟩
-  invFun := fun x ↦ (card_eq_one.1 x.2).choose
-  left_inv := fun i ↦ by simpa using (card_eq_one.1 (⟨{i}, by simp⟩ : Sym α 1).2).choose_spec.symm
-  right_inv := fun x ↦ ext <| by simp_rw [← choose_spec <| card_eq_one.1 x.2]; rfl
+def oneEquiv : α ≃ Sym α 1 where
+  toFun a := ⟨{a}, by simp⟩
+  invFun s := Quotient.liftOn (Equiv.subtypeQuotientEquivQuotientSubtype
+      (·.length = 1) _ (fun l ↦ Iff.rfl) (fun l l' ↦ by rfl) s)
+    (fun l ↦ l.1.head <| List.length_pos.mp <| by simp)
+    fun ⟨l, h⟩ ⟨l', h'⟩ _perm ↦ by
+      obtain ⟨a, rfl⟩ := List.length_eq_one.mp h'
+      exact List.eq_of_mem_singleton (_perm.mem_iff.mp <| List.head_mem _)
+  left_inv a := by rfl
+  right_inv := by rintro ⟨⟨l⟩, h⟩; obtain ⟨a, rfl⟩ := List.length_eq_one.mp h; rfl
 
 /-- Fill a term `m : Sym α (n - i)` with `i` copies of `a` to obtain a term of `Sym α n`.
 This is a convenience wrapper for `m.append (replicate i a)` that adjusts the term using
--- Mathlib/RingTheory/MvPolynomial/Symmetric.lean
+++ Mathlib/RingTheory/MvPolynomial/Symmetric.lean
@@ -317,9 +317,9 @@ theorem hsymm_zero : hsymm σ R 0 = 1 := by
 theorem hsymm_one : hsymm σ R 1 = ∑ i, X i := by
   simp only [hsymm, univ_unique]
   symm
-  apply Fintype.sum_equiv one_equiv
+  apply Fintype.sum_equiv oneEquiv
   intro i
-  simp only [one_equiv_apply, Multiset.map_singleton, Multiset.prod_singleton]
+  simp only [oneEquiv_apply, Multiset.map_singleton, Multiset.prod_singleton]
 
 theorem map_hsymm (n : ℕ) (f : R →+* S) : map f (hsymm σ R n) = hsymm σ S n := by
   simp_rw [hsymm, map_sum, ← Multiset.prod_hom']
