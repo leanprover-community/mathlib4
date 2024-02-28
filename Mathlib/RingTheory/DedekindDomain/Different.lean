93cbd4df298cfad48a51c9da6afc7843aa20e9bf
Christopher Hoskin <christopher.hoskin@gmail.com>
2024-02-28 22:49:03 +0000

    Build

--- Mathlib/RingTheory/DedekindDomain/Different.lean
+++ Mathlib/RingTheory/DedekindDomain/Different.lean
@@ -3,6 +3,7 @@ Copyright (c) 2023 Andrew Yang. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Andrew Yang
 -/
+import Mathlib.LinearAlgebra.BilinearForm.Basic
 import Mathlib.RingTheory.DedekindDomain.Ideal
 import Mathlib.RingTheory.Discriminant
 import Mathlib.RingTheory.DedekindDomain.IntegralClosure
