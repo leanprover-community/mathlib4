b0e31b54b36c10120861f390390c6539cd62263c
Christopher Hoskin <christopher.hoskin@gmail.com>
2024-02-28 22:47:06 +0000

    Shake

--- Mathlib/RingTheory/DedekindDomain/IntegralClosure.lean
+++ Mathlib/RingTheory/DedekindDomain/IntegralClosure.lean
@@ -3,12 +3,13 @@ Copyright (c) 2020 Kenji Nakagawa. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kenji Nakagawa, Anne Baanen, Filippo A. E. Nuccio
 -/
+import Mathlib.LinearAlgebra.BilinearForm.Basic
 import Mathlib.LinearAlgebra.FreeModule.PID
 import Mathlib.LinearAlgebra.FreeModule.Finite.Basic
-import Mathlib.LinearAlgebra.BilinearForm.DualLattice
 import Mathlib.RingTheory.DedekindDomain.Basic
 import Mathlib.RingTheory.Localization.Module
 import Mathlib.RingTheory.Trace
+import Mathlib.LinearAlgebra.SesquilinearForm.DualLattice
 
 #align_import ring_theory.dedekind_domain.integral_closure from "leanprover-community/mathlib"@"4cf7ca0e69e048b006674cf4499e5c7d296a89e0"
 
