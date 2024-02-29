703e4b2f856f09893f9f0dba48b1428ccd41501a
faenuccio <filippo.nuccio@univ-st-etienne.fr>
2024-02-29 09:52:24 +0100

    removed two empty lines

--- Mathlib/RingTheory/PowerSeries/Basic.lean
+++ Mathlib/RingTheory/PowerSeries/Basic.lean
@@ -3,7 +3,6 @@ Copyright (c) 2019 Johan Commelin. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johan Commelin, Kenny Lau
 -/
-
 import Mathlib.RingTheory.MvPowerSeries.Basic
 import Mathlib.Data.Polynomial.Basic
 import Mathlib.Data.Polynomial.AlgebraMap
