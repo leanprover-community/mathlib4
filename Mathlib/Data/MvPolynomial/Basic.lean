d2b70011c507f7589d57942a99f01700b3039c61
Bolton Bailey <bolton.bailey@gmail.com>
2024-02-29 08:26:08 -0600

    prove theorem

--- Mathlib/Data/MvPolynomial/Basic.lean
+++ Mathlib/Data/MvPolynomial/Basic.lean
@@ -819,6 +819,13 @@ theorem ne_zero_iff {p : MvPolynomial σ R} : p ≠ 0 ↔ ∃ d, coeff d p ≠ 0
 #align mv_polynomial.ne_zero_iff MvPolynomial.ne_zero_iff
 
 @[simp]
+theorem X_ne_zero [Nontrivial R] (s : σ) :
+    X (R := R) s ≠ 0 := by
+  rw [ne_zero_iff]
+  use Finsupp.single s 1
+  simp only [coeff_X, ne_eq, one_ne_zero, not_false_eq_true]
+
+@[simp]
 theorem support_eq_empty {p : MvPolynomial σ R} : p.support = ∅ ↔ p = 0 :=
   Finsupp.support_eq_empty
 #align mv_polynomial.support_eq_empty MvPolynomial.support_eq_empty
