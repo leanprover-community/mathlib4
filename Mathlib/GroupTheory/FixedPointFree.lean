84e3d5e7d80315158619249c3a1ccd6a9068aac0
Thomas Browning <tb65536@uw.edu>
2024-03-01 11:27:06 -0800

    more fixes

--- Mathlib/GroupTheory/FixedPointFree.lean
+++ Mathlib/GroupTheory/FixedPointFree.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Thomas Browning
 -/
 import Mathlib.Algebra.Group.Aut
+import Mathlib.Algebra.GroupPower.IterateHom
 import Mathlib.Data.Fintype.Card
 
 /-!
@@ -23,8 +24,6 @@ namespace FixedPointFree
 
 variable {G : Type*} [Group G]
 
-section MonoidHom
-
 variable {F : Type*} [FunLike F G G] [MonoidHomClass F G G] {φ : F} (hφ : FixedPointFree φ)
 
 theorem injective_commutatorMap : Function.Injective fun g => g⁻¹ * φ g := by
@@ -36,26 +35,16 @@ variable [Finite G]
 theorem surjective_commutatorMap : Function.Surjective fun g => g⁻¹ * φ g :=
   Finite.surjective_of_injective hφ.injective_commutatorMap
 
-end MonoidHom
-
-variable [Finite G]
-
-section MulEquiv
-
-variable {φ : G ≃* G} (hφ : FixedPointFree φ)
-
-theorem prod_pow_eq_one {n : ℕ} (hn : φ ^ n = 1) (g : G) :
-    ((List.range n).map (fun k ↦ (φ ^ k) g)).prod = 1 := by
+theorem prod_pow_eq_one {n : ℕ} (hn : φ^[n] = 1) (g : G) :
+    ((List.range n).map (fun k ↦ φ^[k] g)).prod = 1 := by
   obtain ⟨g, rfl⟩ := surjective_commutatorMap hφ g
-  simp only [MonoidHom.coe_coe, map_mul, map_inv, ← MulAut.mul_apply, ← pow_succ']
+  simp [iterate_map_mul, MonoidHom.iterate_map_inv, ← Function.iterate_succ_apply]
   rw [List.prod_map_range_cancel, pow_zero, hn, inv_mul_self]
 
 theorem eq_inv_of_sq_eq_one (hn : φ ^ 2 = 1) : ⇑φ = (·⁻¹) := by
   ext g
   have key : 1 * g * φ g = 1 := hφ.prod_pow_eq_one hn g
   rwa [one_mul, ← inv_eq_iff_mul_eq_one, eq_comm] at key
-
-end MulEquiv
 
 lemma eq_inv_of_involutive {F : Type*} [FunLike F G G] [MonoidHomClass F G G] {φ : F}
     (hφ₁ : FixedPointFree φ) (hφ₂ : Function.Involutive φ) : ⇑φ = (·⁻¹) :=
