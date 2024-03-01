7ebc5b388be2de5b9ca692af2173f51596881fab
Scott Morrison <scott.morrison@gmail.com>
2024-03-01 23:49:00 +1100

    more comments

--- Mathlib/MeasureTheory/Function/SimpleFunc.lean
+++ Mathlib/MeasureTheory/Function/SimpleFunc.lean
@@ -1329,6 +1329,7 @@ theorem _root_.Measurable.add_simpleFunc
         = (Function.support f).piecewise (g + (f : α → E)) (g + f') := by
       ext x
       by_cases hx : x ∈ Function.support f
+      -- Adaptation note: after nightly-2024-03-02 we can remove `_root_.` again below.
       · simpa only [SimpleFunc.coe_add, Pi.add_apply, Function.mem_support, ne_eq, not_not,
           Set.piecewise_eq_of_mem _ _ _ hx, _root_.add_right_inj, add_right_eq_self]
           using Set.disjoint_left.1 hff' hx
@@ -1354,6 +1355,7 @@ theorem _root_.Measurable.simpleFunc_add
         = (Function.support f).piecewise ((f : α → E) + g) (f' + g) := by
       ext x
       by_cases hx : x ∈ Function.support f
+      -- Adaptation note: after nightly-2024-03-02 we can remove `_root_.` again below.
       · simpa only [coe_add, Pi.add_apply, Function.mem_support, ne_eq, not_not,
           Set.piecewise_eq_of_mem _ _ _ hx, _root_.add_left_inj, add_right_eq_self]
           using Set.disjoint_left.1 hff' hx
