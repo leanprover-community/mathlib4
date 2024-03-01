fdc24cf4176184deb77324fb7fb9172729513806
smorel394 <67864981+smorel394@users.noreply.github.com>
2024-03-01 15:56:09 +0100

    Update Mathlib/LinearAlgebra/TensorPower.lean
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- Mathlib/LinearAlgebra/TensorPower.lean
+++ Mathlib/LinearAlgebra/TensorPower.lean
@@ -305,7 +305,7 @@ variable (R)
 
 /-- A family `f` of linear forms on `M` indexed by `Fin n` defines a linear form on
 `⨂[R]^n M`, by multiplying the components of `f`. -/
-noncomputable def linearFormOfFamily (f : (_ : Fin n) → (M →ₗ[R] R)) : (⨂[R]^n) M →ₗ[R] R :=
+noncomputable def linearFormOfFamily (f : (_ : Fin n) → Module.Dual R M) : Module.Dual R (⨂[R]^n M) :=
   PiTensorProduct.lift (MultilinearMap.compLinearMap (MultilinearMap.mkPiRing R (Fin n) 1) f)
 
 open BigOperators in
