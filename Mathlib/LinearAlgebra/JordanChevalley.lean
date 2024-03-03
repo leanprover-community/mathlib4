aa3be5773713c180c3879a464b16416aed513200
Junyan Xu <junyanxu.math@gmail.com>
2024-03-03 14:25:26 -0500

    Discard changes to Mathlib/LinearAlgebra/JordanChevalley.lean

--- Mathlib/LinearAlgebra/JordanChevalley.lean
+++ Mathlib/LinearAlgebra/JordanChevalley.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Oliver Nash
 -/
 import Mathlib.Dynamics.Newton
+import Mathlib.FieldTheory.Perfect
 import Mathlib.LinearAlgebra.Semisimple
 
 /-!
