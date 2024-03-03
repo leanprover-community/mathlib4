7ef239aae96dbfd7507cbd8735c7fb76238c98ba
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2024-03-03 22:03:20 +0100

    chore: tidy various files

--- Mathlib/RingTheory/PiTensorProduct.lean
+++ Mathlib/RingTheory/PiTensorProduct.lean
@@ -53,8 +53,7 @@ def mul : (⨂[R] i, A i) →ₗ[R] (⨂[R] i, A i) →ₗ[R] (⨂[R] i, A i) :=
 
 @[simp] lemma mul_tprod_tprod (x y : (i : ι) → A i) :
     mul (tprod R x) (tprod R y) = tprod R (x * y) := by
-  simp only [mul, piTensorHomMap₂_tprod_tprod_tprod, LinearMap.mul_apply']
-  rfl
+  simp only [mul, piTensorHomMap₂_tprod_tprod_tprod, LinearMap.mul_apply', Pi.mul_def]
 
 instance instMul : Mul (⨂[R] i, A i) where
   mul x y := mul x y
@@ -76,9 +75,7 @@ nonrec theorem _root_.Commute.tprod {a₁ a₂ : Π i, A i} (ha : Commute a₁ a
 
 lemma smul_tprod_mul_smul_tprod (r s : R) (x y : Π i, A i) :
     (r • tprod R x) * (s • tprod R y) = (r * s) • tprod R (x * y) := by
-  change mul _ _ = _
-  rw [map_smul, map_smul, mul_comm r s, mul_smul]
-  simp only [LinearMap.smul_apply, mul_tprod_tprod]
+  simp only [mul_def, map_smul, LinearMap.smul_apply, mul_tprod_tprod, mul_comm r s, mul_smul]
 
 instance instNonUnitalNonAssocSemiring : NonUnitalNonAssocSemiring (⨂[R] i, A i) where
   __ := instMul
@@ -180,8 +177,8 @@ lemma algebraMap_apply (r : R') (i : ι) [DecidableEq ι] :
   change r • tprod R 1 = _
   have : Pi.mulSingle i (algebraMap R' (A i) r) = update (fun i ↦ 1) i (r • 1) := by
     rw [Algebra.algebraMap_eq_smul_one]; rfl
-  rw [this, ← smul_one_smul R r (1 : A i), MultilinearMap.map_smul, update_eq_self, smul_one_smul]
-  congr
+  rw [this, ← smul_one_smul R r (1 : A i), MultilinearMap.map_smul, update_eq_self, smul_one_smul,
+    Pi.one_def]
 
 /--
 The map `Aᵢ ⟶ ⨂ᵢ Aᵢ` given by `a ↦ 1 ⊗ ... ⊗ a ⊗ 1 ⊗ ...`
