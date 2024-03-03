1418c11c157fe97f87665ec371e3dc13c3255048
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2024-03-03 18:54:18 +0100

    wip

--- Mathlib/Tactic/RSuffices.lean
+++ Mathlib/Tactic/RSuffices.lean
@@ -13,14 +13,60 @@ of any syntax that would be valid in an `obtain` block. This tactic just calls `
 on the expression, and then `rotate_left`.
 -/
 
-/--
-The `rsuffices` tactic is an alternative version of `suffices`, that allows the usage
-of any syntax that would be valid in an `obtain` block. This tactic just calls `obtain`
-on the expression, and then `rotate_left`.
--/
-syntax (name := rsuffices) "rsuffices"
-  (ppSpace Std.Tactic.RCases.rcasesPatMed)? (" : " term)? (" := " term,+)? : tactic
+-- /--
+-- The `rsuffices` tactic is an alternative version of `suffices`, that allows the usage
+-- of any syntax that would be valid in an `obtain` block. This tactic just calls `obtain`
+-- on the expression, and then `rotate_left`.
+-- -/
+-- syntax (name := rsuffices) "rsuffices"
+--   (ppSpace Std.Tactic.RCases.rcasesPatMed)? (" : " term)? : tactic
 
-macro_rules
-| `(tactic| rsuffices $[$pred]? $[: $foo]? $[:= $bar]?) =>
-`(tactic | (obtain $[$pred]? $[: $foo]? $[:= $bar]?; rotate_left))
+-- open Lean.Parser Term in
+-- def rsufficesDecl := leading_parser
+--   (atomic (group (binderIdent >> " : ")) <|> hygieneInfo) >> termParser >> ppSpace >> showRhs
+
+-- macro_rules
+-- | `(tactic| rsuffices $[$pred]? $[: $foo]? $[:= $bar]?) =>
+-- `(tactic | (obtain $[$pred]? $[: $foo]? $[:= $bar]?; rotate_left))
+
+-- macro_rules
+--   | `(tactic| rsuffices $[$pred]? $[: $foo]? "from" $val; $body)            => `(have%$tk $x : $type := $body; $val)
+--   | `(tactic| rsuffices%$tk _%$x          : $type from $val; $body)            => `(have%$tk _%$x : $type := $body; $val)
+--   | `(tactic| rsuffices%$tk $hy:hygieneInfo $type from $val; $body)            => `(have%$tk $hy:hygieneInfo : $type := $body; $val)
+--   | `(tactic| rsuffices%$tk $x:ident      : $type by%$b $tac:tacticSeq; $body) => `(have%$tk $x : $type := $body; by%$b $tac)
+--   | `(tactic| rsuffices%$tk _%$x          : $type by%$b $tac:tacticSeq; $body) => `(have%$tk _%$x : $type := $body; by%$b $tac)
+--   | `(tactic| rsuffices%$tk $hy:hygieneInfo $type by%$b $tac:tacticSeq; $body) => `(have%$tk $hy:hygieneInfo : $type := $body; by%$b $tac)
+
+
+#check Lean.Parser.Tactic.tacticRefine_lift_
+-- macro "refine_lift " e:term : tactic =>
+-- `(tactic| focus (refine no_implicit_lambda% $e; rotate_right))
+
+-- macro "rsuffices " d:sufficesDecl : tactic => `(tactic| refine_lift suffices $d; ?_)
+
+section
+-- namespace Lean.Elab.Term
+#check Std.Tactic.RCases.rcasesPatMed
+#check Lean.ParserDescr
+open Lean.Parser Term
+def rsufficesDecl := leading_parser
+  (atomic Std.Tactic.RCases.rcasesPatMed) >> termParser >> ppSpace >> showRhs
+@[builtin_term_parser] def «rsuffices» := leading_parser:leadPrec
+  withPosition ("rsuffices " >> rsufficesDecl) >> optSemicolon termParser
+
+-- end Lean.Elab.Term
+end
+
+open Lean in
+@[builtin_macro Lean.Parser.Term.rsuffices]
+def expandRSuffices : Macro
+  | `(rsuffices%$tk $x:ident      : $type from $val; $body)            => `(have%$tk $x : $type := $body; $val)
+  | `(rsuffices%$tk _%$x          : $type from $val; $body)            => `(have%$tk _%$x : $type := $body; $val)
+  | `(rsuffices%$tk $hy:hygieneInfo $type from $val; $body)            => `(have%$tk $hy:hygieneInfo : $type := $body; $val)
+  | `(rsuffices%$tk $x:ident      : $type by%$b $tac:tacticSeq; $body) => `(have%$tk $x : $type := $body; by%$b $tac)
+  | `(rsuffices%$tk _%$x          : $type by%$b $tac:tacticSeq; $body) => `(have%$tk _%$x : $type := $body; by%$b $tac)
+  | `(rsuffices%$tk $hy:hygieneInfo $type by%$b $tac:tacticSeq; $body) => `(have%$tk $hy:hygieneInfo : $type := $body; by%$b $tac)
+  | _                                                                 => Macro.throwUnsupported
+
+
+macro "rsuffices " d:rsufficesDecl : tactic => `(tactic| refine_lift rsuffices $d; ?_)
