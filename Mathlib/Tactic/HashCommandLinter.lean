19025e67670819499c1aaec8f71123c14187c607
adomani <adomani@gmail.com>
2024-03-03 10:22:15 +0100

    Remove getAtomStx + some of Thomas' comments

--- Mathlib/Tactic/HashCommandLinter.lean
+++ Mathlib/Tactic/HashCommandLinter.lean
@@ -17,7 +17,7 @@ namespace Mathlib.Linter
 
 /--
 Setting to `true` enables the '`#`-command' linter.  When `true`, Lean warns on any command
-beginning with `#`.  For example, `#eval, #guard, #guard_msgs` all trigger a message.
+beginning with `#`.  For example, `#eval`, `#guard`, `#guard_msgs` all trigger a message.
 -/
 register_option linter.hashCommand : Bool := {
   defValue := false
@@ -28,29 +28,24 @@ namespace HashCommandLinter
 
 open Lean Elab
 
-/-- `getAtomStx stx` extracts the array of all `.atom` nodes in `stx`. -/
-def getAtomStx : Syntax → Array Syntax
-  | .node _ _ args => (args.attach.map fun ⟨a, _h⟩ => getAtomStx a).foldl (· ++ ·) default
-  | s@(.atom ..) => #[s]
-  | _ => default
-
 /-- Gets the value of the `linter.hashCommand` option. -/
 def getLinterHash (o : Options) : Bool := Linter.getLinterValue linter.hashCommand o
 
-/-- `whiteList` is the array of `#`-commands that are allowed in 'Mathlib'. -/
-private abbrev whiteList : Array String := #["#align", "#align_import", "#noalign"]
+/-- `whitelist` is the array of `#`-commands that are allowed in 'Mathlib'. -/
+private abbrev whitelist : Array String := #["#align", "#align_import", "#noalign"]
 
 /-- Checks that no command beginning with `#` is present in 'Mathlib', except for the ones in
-`whiteList`. -/
+`whitelist`. -/
 def hashCommandLinter : Linter where run := withSetOptionIn fun stx => do
   let currentModule := (← getEnv).mainModule
   if getLinterHash (← getOptions) &&
-     (← getInfoState).enabled &&
      (currentModule.getRoot == `Mathlib || currentModule == `test.HashCommandLinter) then
-    let sa := (getAtomStx stx)[0]!
-    let a := sa.getAtomVal
-    if ("#".isPrefixOf a && (!' ' ∈ a.toList) && whiteList.all (· != a)) then
-      logWarningAt sa f!"`#`-commands, such as '{a}', are not allowed in 'Mathlib'\n\
-      [linter.hashCommand]"
+    match stx.getHead? with
+      | some sa =>
+        let a := sa.getAtomVal
+        if ("#".isPrefixOf a && (!' ' ∈ a.toList) && whitelist.all (· != a)) then
+          logWarningAt sa f!"`#`-commands, such as '{a}', are not allowed in 'Mathlib'\n\
+            [linter.hashCommand]"
+      | none => return
 
 initialize addLinter hashCommandLinter
