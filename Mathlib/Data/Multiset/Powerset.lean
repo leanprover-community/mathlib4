7e6f6d671a9baa232edc2b5441945321342015dc
Yaël Dillies <yael.dillies@gmail.com>
2024-03-04 07:58:23 +0000

    chore: Rename lemmas about the coercion `List → Multiset` (#11099)
    These did not respect the naming convention by having the `coe` as a prefix instead of a suffix, or vice-versa. Also add a bunch of `norm_cast`

--- Mathlib/Data/Multiset/Powerset.lean
+++ Mathlib/Data/Multiset/Powerset.lean
@@ -112,7 +112,7 @@ theorem mem_powerset {s t : Multiset α} : s ∈ powerset t ↔ s ≤ t :=
 
 theorem map_single_le_powerset (s : Multiset α) : s.map singleton ≤ powerset s :=
   Quotient.inductionOn s fun l => by
-    simp only [powerset_coe, quot_mk_to_coe, coe_le, coe_map]
+    simp only [powerset_coe, quot_mk_to_coe, coe_le, map_coe]
     show l.map (((↑) : List α → Multiset α) ∘ List.ret) <+~ (sublists l).map (↑)
     rw [← List.map_map]
     exact ((map_ret_sublist_sublists _).map _).subperm
