b5af0bbdbaee854abcea8980902a646adafe9239
Hunter Monroe <github.com@huntermonroe.com>
2024-03-03 21:00:57 -0500

    Remove file

--- Mathlib/Logic/Proof/LogicSymbol.lean
+++ Mathlib/Logic/Proof/LogicSymbol.lean
@@ -3,7 +3,7 @@ Copyright (c) 2024 Shogo Saito. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Shogo Saito. Adapted for mathlib by Hunter Monroe
 -/
-import Mathlib.Logic.Proof.VecNotation
+import Mathlib.Data.Fin.VecNotation
 import Mathlib.Data.Finset.Basic
 import Mathlib.Data.FunLike.Basic
 /-!
