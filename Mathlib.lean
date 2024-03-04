d608ed9beced74112f76a53adc77832a0724addc
Riccardo Brasca <riccardo.brasca@gmail.com>
2024-03-04 18:12:34 +0000

    feat: prove case 1 of FLT in the case n=3 (#10698)
    We prove the so called case 1 of Fermat Last Theorem for exponent `n = 3`: if `a b c : ℕ` are such that `¬ 3 ∣ a * b * c` then `a ^ 3 + b ^ 3 ≠ c ^ 3`.
    
    From flt-regular

--- Mathlib.lean
+++ Mathlib.lean
@@ -2913,6 +2913,7 @@ import Mathlib.NumberTheory.EulerProduct.Basic
 import Mathlib.NumberTheory.EulerProduct.DirichletLSeries
 import Mathlib.NumberTheory.FLT.Basic
 import Mathlib.NumberTheory.FLT.Four
+import Mathlib.NumberTheory.FLT.Three
 import Mathlib.NumberTheory.FermatPsp
 import Mathlib.NumberTheory.FrobeniusNumber
 import Mathlib.NumberTheory.FunctionField
