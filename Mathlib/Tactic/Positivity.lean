f65b577d3db033702e9d110d5f653d7305140db8
Bolton Bailey <bolton.bailey@gmail.com>
2024-03-04 13:09:41 -0600

    reappear

--- Mathlib/Tactic/Positivity.lean
+++
@@ -1,3 +0,0 @@
-import Mathlib.Tactic.Positivity.Basic
-import Mathlib.Tactic.NormNum.Basic
-import Mathlib.Init.Data.Int.Order
--- Mathlib/Tactic/Ring.lean
+++
@@ -1,3 +0,0 @@
-import Mathlib.Tactic.Ring.Basic
-import Mathlib.Tactic.Ring.RingNF
-import Mathlib.Tactic.Ring.PNat
