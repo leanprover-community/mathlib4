0dcf75b75b642237b5fa756c3bd5e0545bfe7ead
Calle Sönne <calle.sonne@gmail.com>
2024-03-05 08:38:08 +0000

    added lemmas

--- Mathlib/CategoryTheory/CommSq.lean
+++ Mathlib/CategoryTheory/CommSq.lean
@@ -75,6 +75,31 @@ theorem vert_inv {g : W ≅ Y} {h : X ≅ Z} (p : CommSq f g.hom h.hom i) :
     CommSq i g.inv h.inv f :=
   ⟨by rw [Iso.comp_inv_eq, Category.assoc, Iso.eq_inv_comp, p.w]⟩
 
+/-- If the two inner squares below commute, then so does the outer square.
+```
+  W ---f---> X ---f'--> X'
+  |          |         |
+  g          h          h'
+  |          |          |
+  v          v          v
+  Y ---i---> Z ---i'--> Z'
+
+```
+-/
+lemma CommSq_comp {W X X' Y Z Z' : C} {f : W ⟶ X} {f' : X ⟶ X'} {g : W ⟶ Y} {h : X ⟶ Z}
+  {h' : X' ⟶ Z'} {i : Y ⟶ Z} {i' : Z ⟶ Z'} (hsq₁ : CommSq f g h i) (hsq₂ : CommSq f' h h' i') :
+  CommSq (f ≫ f') g h' (i ≫ i') :=
+  ⟨by rw [←Category.assoc, Category.assoc, ←hsq₁.w, hsq₂.w, Category.assoc]⟩
+
+/-- If the vertical morphisms of a commutative square are isomorphism, then we
+  also get a commutative square by replacing them with there inverses -/
+lemma CommSq_of_CommSq_of_vertical_isos (f : W ⟶ X) (i : Y ⟶ Z) (g : W ≅ Y) (h : X ≅ Z)
+    (hcomm : CommSq f g.hom h.hom i) : CommSq i g.inv h.inv f := by
+  cases' hcomm with w
+  constructor
+  rw [← @Iso.eq_comp_inv] at w
+  aesop_cat
+
 end CommSq
 
 namespace Functor
