47f657f57c57525df3c93cfda620b40e0a362bdf
adomani <adomani@gmail.com>
2024-03-05 17:13:15 +0000

    Remove non-existent import

--- Mathlib/Tactic/Lint.lean
+++ Mathlib/Tactic/Lint.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Floris van Doorn
 -/
 import Std.Data.Array.Basic
-import Std.Lean.Command
 import Std.Tactic.Lint
 
 /-!
