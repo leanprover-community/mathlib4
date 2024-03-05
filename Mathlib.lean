51242e190152ac21fd81ea0a0adcc4712ad634a1
Joël Riou <joel.riou@universite-paris-saclay.fr>
2024-03-05 13:00:12 +0000

    feat(Algebra/Homology): the category of homological complexes is linear (#10891)
    In this PR, it is shown that if `C` is a `R`-linear category, then the category of homological complexes in `C` is `R`-linear. (This is also obtained for the homotopy category.)
    
    
    
    Co-authored-by: Joël Riou <37772949+joelriou@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -262,6 +262,7 @@ import Mathlib.Algebra.Homology.HomotopyCategory.Pretriangulated
 import Mathlib.Algebra.Homology.HomotopyCategory.Shift
 import Mathlib.Algebra.Homology.HomotopyCofiber
 import Mathlib.Algebra.Homology.ImageToKernel
+import Mathlib.Algebra.Homology.Linear
 import Mathlib.Algebra.Homology.LocalCohomology
 import Mathlib.Algebra.Homology.Localization
 import Mathlib.Algebra.Homology.ModuleCat
