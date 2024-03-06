5db481016795ff79eb6dd3377ae0573780852976
Luigi Massacci <luigi.massacci.it@gmail.com>
2024-03-06 11:52:44 +0100

    CI/Linter fixes, add docs

--- Mathlib/Topology/MetricSpace/Katetov.lean
+++ Mathlib/Topology/MetricSpace/Katetov.lean
@@ -28,6 +28,8 @@ the Kuratowski embedding.
 
 variable {α : Type _} [MetricSpace α]
 
+/-- A real valued function from a metric space is katetov if
+  it satisfies the following inequalities: -/
 @[mk_iff]
 structure IsKatetov (f : α → ℝ) : Prop where
   /-- Proposition that `f` is 1-Lipschitz -/
@@ -35,17 +37,20 @@ structure IsKatetov (f : α → ℝ) : Prop where
   /-- Second defining inequality of a Katetov map  -/
   dist_le_add : ∀ x y, dist x y ≤ f x + f y
 
+/-- The type of Katetov maps from `α` -/
 structure KatetovMap (α : Type*) [MetricSpace α] where
   /-- The function `α → ℝ` -/
   protected toFun : α → ℝ
   /-- Proposition that `toFun` is a Katetov map -/
   protected IsKatetovtoFun : IsKatetov toFun
 
+/-- The type of Katetov maps from `α`. -/
 notation "E(" α ")" => KatetovMap α
 
 section
 
-class KatetovMapClass (F : Type*) (α : Type*) [MetricSpace α] [FunLike F α  ℝ] where
+/-- `KatetovMapClass F α` states that `F` is a type of Katetov maps. -/
+class KatetovMapClass (F : Type*) (α : Type*) [MetricSpace α] [FunLike F α  ℝ] : Prop where
   map_katetov (f : F) : IsKatetov f
 
 end
@@ -57,6 +62,7 @@ section KatetovMapClass
 variable {F α : Type*} [MetricSpace α] [FunLike F α  ℝ]
 variable [KatetovMapClass F α]
 
+/-- Coerce a bundled morphism with a `KatetovMapClass` instance to a `KatetovMap`. -/
 @[coe] def toKatetovMap (f : F) : E(α) := ⟨f, map_katetov f⟩
 
 instance : CoeTC F E(α) := ⟨toKatetovMap⟩
@@ -79,6 +85,7 @@ theorem toFun_eq_coe {f : E(α)} : f.toFun = (f : α → ℝ) := rfl
 
 instance : CanLift (α → ℝ) E(α) DFunLike.coe IsKatetov := ⟨fun f hf ↦ ⟨⟨f, hf⟩, rfl⟩⟩
 
+/-- See Topology/ContinuousFunction.Basic -/
 def Simps.apply (f : E(α)) : α → ℝ := f
 
 initialize_simps_projections KatetovMap (toFun → apply)
@@ -90,6 +97,8 @@ protected theorem coe_coe {F : Type*} [FunLike F α ℝ] [KatetovMapClass F α] 
 @[ext]
 theorem ext {f g : E(α)} (h : ∀ a, f a = g a) : f = g := DFunLike.ext _ _ h
 
+/-- Copy of a `KatetovMap` with a new `toFun` equal to the old one. Useful to fix definitional
+equalities. See Topology/ContinuousFunction.Basic -/
 protected def copy (f : E(α)) (f' : α → ℝ) (h : f' = f) : E(α) where
   toFun := f'
   IsKatetovtoFun := h.symm ▸ f.IsKatetovtoFun
@@ -235,6 +244,7 @@ noncomputable instance : CompleteSpace E(α) :=
 
 namespace KatetovKuratowskiEmbedding
 
+/-- The Katetov map from an empty type -/
 def instKatetovMapOfEmpty [IsEmpty α] : E(α) := by
   refine ⟨fun x ↦ (IsEmpty.false x).elim, ?_⟩
   constructor <;> {intro x; exact (IsEmpty.false x).elim}
@@ -263,6 +273,8 @@ end KatetovKuratowskiEmbedding
 
 open KatetovKuratowskiEmbedding
 
+/-- The Katetov-Kuratowski embedding is an isometric embedding of a metric space into its space
+  of Katetov maps.-/
 noncomputable def katetovKuratowskiEmbedding (α : Type*) [MetricSpace α] : α ↪ E(α) := by
   choose f h using exists_isometric_embedding α; refine ⟨f, h.injective⟩
 
