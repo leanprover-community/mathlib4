6436be2bb27a380d8b710c18d2a4ec996b29e9fe
uni marx <uniwuni@protonmail.com>
2024-03-06 11:55:57 +0100

    oops

--- Mathlib/RingTheory/Localization/AtMinimalPrime.lean
+++ Mathlib/RingTheory/Localization/AtMinimalPrime.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2024 Uni Marx. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Author: Joe Cool.
+Authors: Uni Marx.
 -/
 import Mathlib.RingTheory.Localization.AtPrime
 import Mathlib.RingTheory.Nilpotent
