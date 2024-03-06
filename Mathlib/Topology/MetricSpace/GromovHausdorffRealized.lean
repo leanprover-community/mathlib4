a1b943c1e6b8ee5f3a18045639ad453318e37794
Eric Rodriguez <ericrboidi@gmail.com>
2024-03-05 22:07:02 -0800

    add a `Classical.` in GromovHausdorff

--- Mathlib/Topology/MetricSpace/GromovHausdorffRealized.lean
+++ Mathlib/Topology/MetricSpace/GromovHausdorffRealized.lean
@@ -102,7 +102,7 @@ section Constructions
 variable {X : Type u} {Y : Type v} [MetricSpace X] [CompactSpace X] [Nonempty X] [MetricSpace Y]
   [CompactSpace Y] [Nonempty Y] {f : ProdSpaceFun X Y} {x y z t : X ⊕ Y}
 
-attribute [local instance 10] inhabited_of_nonempty'
+attribute [local instance 10] Classical.inhabited_of_nonempty'
 
 private theorem maxVar_bound : dist x y ≤ maxVar X Y :=
   calc
