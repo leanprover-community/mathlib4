1ddfcbe3a14c51143510e764ee2f34585885908f
Bolton Bailey <bolton.bailey@gmail.com>
2024-03-07 07:50:14 -0600

    remove unneeded simps

--- Mathlib/Data/MvPolynomial/Rename.lean
+++ Mathlib/Data/MvPolynomial/Rename.lean
@@ -57,7 +57,6 @@ def rename (f : σ → τ) : MvPolynomial σ R →ₐ[R] MvPolynomial τ R :=
   aeval (X ∘ f)
 #align mv_polynomial.rename MvPolynomial.rename
 
-@[simp]
 theorem rename_C (f : σ → τ) (r : R) : rename f (C r) = C r :=
   eval₂_C _ _ _
 set_option linter.uppercaseLean3 false in
@@ -133,7 +132,6 @@ def killCompl : MvPolynomial τ R →ₐ[R] MvPolynomial σ R :=
   aeval fun i => if h : i ∈ Set.range f then X <| (Equiv.ofInjective f hf).symm ⟨i, h⟩ else 0
 #align mv_polynomial.kill_compl MvPolynomial.killCompl
 
-@[simp]
 theorem killCompl_C (r : R) : killCompl hf (C r) = C r := by
   simp [algHom_C]
 
