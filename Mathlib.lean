63c42539d452765fdb40cbd11d245fb13ff5b694
Jireh Loreaux <loreaujy@gmail.com>
2024-03-07 10:35:28 -0600

    update `Mathlib.lean`

--- Mathlib.lean
+++ Mathlib.lean
@@ -3819,6 +3819,7 @@ import Mathlib.Topology.ContinuousFunction.Algebra
 import Mathlib.Topology.ContinuousFunction.Basic
 import Mathlib.Topology.ContinuousFunction.Bounded
 import Mathlib.Topology.ContinuousFunction.CocompactMap
+import Mathlib.Topology.ContinuousFunction.ContinuousMapZero
 import Mathlib.Topology.ContinuousFunction.Compact
 import Mathlib.Topology.ContinuousFunction.FunctionalCalculus
 import Mathlib.Topology.ContinuousFunction.Ideals
