48ff9edeb64a1afc41e54a970cff2c773aee1c5a
Jireh Loreaux <loreaujy@gmail.com>
2024-03-08 16:33:55 -0600

    naming

--- Mathlib/Algebra/Algebra/Quasispectrum.lean
+++ Mathlib/Algebra/Algebra/Quasispectrum.lean
@@ -288,12 +288,12 @@ lemma Unitization.zero_mem_spectrum_inr (R S : Type*) {A : Type*} [CommSemiring 
   rintro ⟨u, hu⟩
   simpa [-Units.mul_inv, hu] using congr($(u.mul_inv).fst)
 
-lemma Unitization.quasispectrum_eq_spectrum_union (R S : Type*) {A : Type*} [Semifield R]
+lemma Unitization.quasispectrum_eq_spectrum_inr (R S : Type*) {A : Type*} [Semifield R]
     [Field S] [NonUnitalRing A] [Algebra R S] [Module S A] [IsScalarTower S A A]
     [SMulCommClass S A A] [Module R A] [IsScalarTower R S A] (a : A) :
     quasispectrum R a = spectrum R (a : Unitization S A) := by
   ext r
   have := Set.singleton_subset_iff.mpr (Unitization.zero_mem_spectrum_inr R S a)
-  rw [← Set.union_eq_self_of_subset_right this, ← _root_.quasispectrum_eq_spectrum_union_zero]
+  rw [← Set.union_eq_self_of_subset_right this, ← quasispectrum_eq_spectrum_union_zero]
   apply forall_congr' fun x ↦ ?_
   rw [not_iff_not, Units.smul_def, Units.smul_def, ← inr_smul, ← inr_neg, isQuasiregular_inr_iff]
