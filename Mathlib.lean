8a6d7d86d007e54d3b4c1e34fdf949cf180c53af
Hunter Monroe <github.com@huntermonroe.com>
2024-03-08 15:57:10 -0500

    Update Mathlib.lean
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -3203,6 +3203,7 @@ import Mathlib.ProofTheory.Calculus
 import Mathlib.ProofTheory.HilbertStyle
 import Mathlib.ProofTheory.HilbertStyle2
 import Mathlib.ProofTheory.FirstOrder.Basic.Syntax.Language
+import Mathlib.ProofTheory.FirstOrder.Basic.Syntax.Language
 import Mathlib.ProofTheory.Lindenbaum
 import Mathlib.ProofTheory.LogicSymbol
 import Mathlib.ProofTheory.Predicate
