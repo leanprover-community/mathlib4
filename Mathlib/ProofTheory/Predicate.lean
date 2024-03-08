6eac3968d2cacf20e2450f0ff9f7628f00dd0fa8
Hunter Monroe <github.com@huntermonroe.com>
2024-03-07 20:31:03 -0500

    Lint

--- Mathlib/ProofTheory/Predicate.lean
+++ Mathlib/ProofTheory/Predicate.lean
@@ -3,7 +3,7 @@ Copyright (c) 2024 Shogo Saito. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Shogo Saito. Adapted for mathlib by Hunter Monroe
 -/
-import Mathlib.ProofTheory.System
+import Mathlib.ProofTheory.LogicSymbol
 
 /-!
 # Predicate calculus
