1205b49513476ec408e43a313a52e7a7a9a3944e
Hunter Monroe <github.com@huntermonroe.com>
2024-03-09 17:13:01 -0500

    Lint

--- Mathlib/ProofTheory/Matrix.lean
+++ Mathlib/ProofTheory/Matrix.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2024 Shogo Saito. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Shogo Saito. Adapted for mathlib by Hunter Monroe
+-/
 import Mathlib.Data.Fin.VecNotation
 import Mathlib.Data.Nat.Pairing
 
