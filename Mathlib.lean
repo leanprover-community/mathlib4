9d8c555ac6e85ac23cdbdc63144776e94f228281
Hunter Monroe <github.com@huntermonroe.com>
2024-03-08 19:19:51 -0500

    Lint

--- Mathlib.lean
+++ Mathlib.lean
@@ -3205,7 +3205,6 @@ import Mathlib.ProofTheory.FirstOrder.Basic.Syntax.Language
 import Mathlib.ProofTheory.FirstOrder.Basic.Syntax.Rew
 import Mathlib.ProofTheory.FirstOrder.Basic.Syntax.Term
 import Mathlib.ProofTheory.HilbertStyle
-import Mathlib.ProofTheory.HilbertStyle2
 import Mathlib.ProofTheory.Lindenbaum
 import Mathlib.ProofTheory.LogicSymbol
 import Mathlib.ProofTheory.Matrix
