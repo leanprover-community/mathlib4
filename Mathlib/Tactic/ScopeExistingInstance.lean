de4140aa99a6f81e658c574912caa5d3298dfc63
Matthew Ballard <matt@mrb.email>
2024-03-08 19:23:08 -0500

    add

---
+++ Mathlib/Tactic/ScopeExistingInstance.lean
@@ -0,0 +1,6 @@
+import Mathlib.Tactic.ScopedNS
+
+macro (name := scopeExistingInstance) "#scope_existing_instance" "[" name:ident "]" e:ident : command =>
+  `(attribute [-instance] $e
+    scoped[$name] attribute [instance] $e)
+
