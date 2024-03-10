74d63a1af13a9110657af52de4ec7545995949e2
Antoine Chambert-Loir <antoine.chambert-loir@math.univ-paris-diderot.fr>
2024-03-10 20:40:42 +0100

    update import line in files importing Mathlib/Algebra/TensorProduct

--- Mathlib/Data/Matrix/Kronecker.lean
+++ Mathlib/Data/Matrix/Kronecker.lean
@@ -7,7 +7,7 @@ import Mathlib.Data.Matrix.Basic
 import Mathlib.Data.Matrix.Block
 import Mathlib.LinearAlgebra.Matrix.Determinant
 import Mathlib.LinearAlgebra.Matrix.NonsingularInverse
-import Mathlib.LinearAlgebra.TensorProduct
+import Mathlib.LinearAlgebra.TensorProduct.Basic
 import Mathlib.RingTheory.TensorProduct
 
 #align_import data.matrix.kronecker from "leanprover-community/mathlib"@"3e068ece210655b7b9a9477c3aff38a492400aa1"
