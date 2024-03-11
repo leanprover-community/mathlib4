42578ef5689fac216a286dad9c05c2c7e8a298ed
Thomas Browning <tb65536@uw.edu>
2024-03-11 14:55:59 -0700

    partial shake

--- Mathlib/RingTheory/Subring/Basic.lean
+++ Mathlib/RingTheory/Subring/Basic.lean
@@ -3,6 +3,7 @@ Copyright (c) 2020 Ashvni Narayanan. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Ashvni Narayanan
 -/
+import Mathlib.Algebra.GroupRingAction.Subobjects
 import Mathlib.GroupTheory.Subgroup.Basic
 import Mathlib.RingTheory.Subsemiring.Basic
 
