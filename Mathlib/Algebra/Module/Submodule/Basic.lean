7d6225bf88f587b91af4a7976b21b1447c96ca38
Thomas Browning <tb65536@uw.edu>
2024-03-11 10:48:24 -0700

    fix

--- Mathlib/Algebra/Module/Submodule/Basic.lean
+++ Mathlib/Algebra/Module/Submodule/Basic.lean
@@ -363,13 +363,6 @@ These instances work particularly well in conjunction with `AddGroup.toAddAction
 
 variable {α β : Type*}
 
-instance [VAdd M α] : VAdd p α :=
-  p.toAddSubmonoid.vadd
-
-instance vaddCommClass [VAdd M β] [VAdd α β] [VAddCommClass M α β] : VAddCommClass p α β :=
-  ⟨fun a => (vadd_comm (a : M) : _)⟩
-#align submodule.vadd_comm_class Submodule.vaddCommClass
-
 instance [VAdd M α] [FaithfulVAdd M α] : FaithfulVAdd p α :=
   ⟨fun h => Subtype.ext <| eq_of_vadd_eq_vadd h⟩
 
