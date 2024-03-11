fdf60d83d524fdfdd1847115cb13fd3645e50e4e
Thomas Browning <tb65536@uw.edu>
2024-03-11 15:54:02 -0700

    partial shake

--- Mathlib/GroupTheory/GroupAction/FixingSubgroup.lean
+++ Mathlib/GroupTheory/GroupAction/FixingSubgroup.lean
@@ -3,7 +3,6 @@ Copyright (c) 2022 Antoine Chambert-Loir. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Antoine Chambert-Loir
 -/
-import Mathlib.GroupTheory.Subgroup.Actions
 import Mathlib.GroupTheory.GroupAction.Basic
 import Mathlib.GroupTheory.GroupAction.FixedPoints
 
