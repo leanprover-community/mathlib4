91bb29f1be708cfedea7eb114a1c789b2a7c6340
Riccardo Brasca <riccardo.brasca@gmail.com>
2024-03-11 11:04:36 +0100

    clarify doc

--- Mathlib/Data/Nat/Factorization/Basic.lean
+++ Mathlib/Data/Nat/Factorization/Basic.lean
@@ -658,7 +658,13 @@ def factorization_lcm_left (a b : ℕ) := (Nat.lcm a b).factorization.prod fun p
   if b.factorization p ≤ a.factorization p then p ^ n else 1
 
 /-- If `a = ∏ pᵢ ^ nᵢ` and `b = ∏ pᵢ ^ mᵢ`, then `factorization_lcm_right = ∏ pᵢ ^ kᵢ`, where
-`kᵢ = mᵢ` if `nᵢ ≤ mᵢ` and `0` otherwise. -/
+`kᵢ = mᵢ` if `nᵢ ≤ mᵢ` and `0` otherwise.
+
+Note that `factorization_lcm_right a b` is *not* `factorization_lcm_left a b`: the difference is
+that in `factorization_lcm_left a b` there are the primes whose exponent in `a` is bigger or equal
+than the exponent in `b`, while in `factorization_lcm_right a b` there are the primes primes whose
+exponent in `b` is strictly bigger than in `a`. For example `factorization_lcm_left 2 2 = 2`, but
+`factorization_lcm_right 2 2 = 1`. -/
 def factorization_lcm_right (a b : ℕ) := (Nat.lcm a b).factorization.prod fun p n ↦
   if b.factorization p ≤ a.factorization p then 1 else p ^ n
 
