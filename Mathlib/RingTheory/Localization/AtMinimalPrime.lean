c6b10401b4f1bb2fcf238529d3d077ee47c6634e
uni marx <uniwuni@protonmail.com>
2024-03-11 23:24:53 +0100

    small refactors

--- Mathlib/RingTheory/Localization/AtMinimalPrime.lean
+++ Mathlib/RingTheory/Localization/AtMinimalPrime.lean
@@ -9,38 +9,34 @@ import Mathlib.RingTheory.Ideal.MinimalPrime
 
 /-!
 # Localization at minimal prime ideals
+
 ## Main results
-- `AtMinimalPrime.prime_unique`: When localizing at a minimal prime ideal `I`, the resulting
+
+  *  `AtMinimalPrime.prime_unique`: When localizing at a minimal prime ideal `I`, the resulting
   ring only has a single prime ideal.
+
 ## Tags
 localization, prime, minimal, nilpotent
 -/
 
-variable {R : Type*} [CommSemiring R] (M : Submonoid R) (S : Type*) [CommSemiring S]
 
-variable [Algebra R S]
-
-section AtMinimalPrime
 namespace Localization
+namespace AtMinimalPrime
 
 open IsLocalization
-
-attribute [local instance] Classical.propDecidable
-
+variable {R : Type*} [CommSemiring R] (M : Submonoid R)
 variable {I : Ideal R} [hI : I.IsPrime] (hMin : I ∈ minimalPrimes R)
 
-theorem AtMinimalPrime.prime_unique (J : Ideal (Localization I.primeCompl)) [hPrime : J.IsPrime] :
+theorem prime_unique (J : Ideal (Localization I.primeCompl)) [hPrime : J.IsPrime] :
     J = LocalRing.maximalIdeal (Localization I.primeCompl) := by
   let i_sub : {p : Ideal (Localization I.primeCompl) // Ideal.IsPrime p} :=
     ⟨_, Ideal.IsMaximal.isPrime' <| LocalRing.maximalIdeal (Localization (Ideal.primeCompl I))⟩
   let j_sub : {p : Ideal (Localization I.primeCompl) // Ideal.IsPrime p} := ⟨_, hPrime⟩
-
   suffices h: j_sub ≤ i_sub → i_sub ≤ j_sub by
     apply le_antisymm (LocalRing.le_maximalIdeal hPrime.ne_top)
     rw [← Subtype.mk_le_mk]
     apply h
     exact LocalRing.le_maximalIdeal hPrime.ne_top
-
   rw [← (IsLocalization.orderIsoOfPrime I.primeCompl (Localization I.primeCompl)).le_iff_le,
      ← (IsLocalization.orderIsoOfPrime I.primeCompl (Localization I.primeCompl)).le_iff_le]
   unfold orderIsoOfPrime at *
@@ -48,19 +44,15 @@ theorem AtMinimalPrime.prime_unique (J : Ideal (Localization I.primeCompl)) [hPr
              Localization.AtPrime.comap_maximalIdeal] at *
   exact hMin.2 ⟨Ideal.comap_isPrime (algebraMap R (Localization (Ideal.primeCompl I))) J, bot_le⟩
 
-theorem AtMinimalPrime.nilpotent_iff_mem_maximal {x : _} :
+theorem nilpotent_iff_mem_maximal {x : _} :
     IsNilpotent x ↔ x ∈ LocalRing.maximalIdeal (Localization I.primeCompl) := by
   rw [nilpotent_iff_mem_prime]
-  constructor
-  · exact fun h => h (LocalRing.maximalIdeal _) (Ideal.IsMaximal.isPrime' _)
-  · intro h J hJ
-    rw [prime_unique hMin J]
-    exact h
+  exact ⟨(· (LocalRing.maximalIdeal _) (Ideal.IsMaximal.isPrime' _)), fun _ J _ =>
+    by simpa [prime_unique hMin J]⟩
 
-theorem AtMinimalPrime.nilpotent_iff_not_unit {x : Localization I.primeCompl} :
+theorem nilpotent_iff_not_unit {x : Localization I.primeCompl} :
     IsNilpotent x ↔ x ∈ nonunits _ := by
-  rw [← LocalRing.mem_maximalIdeal]
-  exact AtMinimalPrime.nilpotent_iff_mem_maximal hMin
+  simpa only [← LocalRing.mem_maximalIdeal] using AtMinimalPrime.nilpotent_iff_mem_maximal hMin
 
-end Localization
 end AtMinimalPrime
+end Localization
