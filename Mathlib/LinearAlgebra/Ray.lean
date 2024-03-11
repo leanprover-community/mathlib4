a2320b3c0ee1143e2157d66e8ba37e617ff80996
Thomas Browning <tb65536@uw.edu>
2024-03-11 14:52:50 -0700

    Revert "shake imports"
    This reverts commit eee52d6151d4d79c8a4fd412fbbfe07d26fcf1fa.
    
    revert

--- Mathlib/LinearAlgebra/Ray.lean
+++ Mathlib/LinearAlgebra/Ray.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joseph Myers
 -/
 import Mathlib.Algebra.Order.Module.Algebra
+import Mathlib.GroupTheory.Subgroup.Actions
 import Mathlib.LinearAlgebra.LinearIndependent
 import Mathlib.RingTheory.Subring.Units
 
