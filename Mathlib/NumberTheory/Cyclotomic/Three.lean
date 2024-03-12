d8030746a37074480cff41c7785be93bf84fb83d
Riccardo Brasca <riccardo.brasca@gmail.com>
2024-03-12 17:54:18 +0100

    unused import

--- Mathlib/NumberTheory/Cyclotomic/Three.lean
+++ Mathlib/NumberTheory/Cyclotomic/Three.lean
@@ -4,7 +4,8 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Riccardo Brasca
 -/
 
-import Mathlib.NumberTheory.Cyclotomic.PID
+import Mathlib.NumberTheory.Cyclotomic.Embeddings
+import Mathlib.NumberTheory.Cyclotomic.Rat
 import Mathlib.NumberTheory.NumberField.Units
 
 /-!
