9129ebb56f56a578cc5a610f2940dc9b50b5ea38
Scott Morrison <scott.morrison@gmail.com>
2024-03-12 15:42:24 +1100

    fixes

--- Mathlib/NumberTheory/Padics/PadicVal.lean
+++ Mathlib/NumberTheory/Padics/PadicVal.lean
@@ -660,7 +660,7 @@ theorem padicValNat_eq_zero_of_mem_Ioo {m k : ℕ}
 theorem padicValNat_factorial_mul_add {n : ℕ} (m : ℕ) [hp : Fact p.Prime] (h : n < p) :
     padicValNat p (p * m + n) ! = padicValNat p (p * m) ! := by
   induction' n with n hn
-  · rw [zero_eq, add_zero]
+  · rw [add_zero]
   · rw [add_succ, factorial_succ,
       padicValNat.mul (succ_ne_zero (p * m + n)) <| factorial_ne_zero (p * m + _),
       hn <| lt_of_succ_lt h, ← add_succ,
@@ -690,8 +690,11 @@ Taking (`p - 1`) times the `p`-adic valuation of `n!` equals `n` minus the sum o
 of `n`. -/
 theorem sub_one_mul_padicValNat_factorial [hp : Fact p.Prime] (n : ℕ):
     (p - 1) * padicValNat p (n !) = n - (p.digits n).sum := by
-  rw [padicValNat_factorial <| lt_succ_of_lt <| lt.base (log p n), ← Finset.sum_Ico_add' _ 0 _ 1,
-    Ico_zero_eq_range, ← sub_one_mul_sum_log_div_pow_eq_sub_sum_digits]
+  rw [padicValNat_factorial <| lt_succ_of_lt <| lt.base (log p n)]
+  norm_cast; nth_rw 2 [← zero_add 1]
+  rw [Nat.succ_eq_add_one, ← Finset.sum_Ico_add' _ 0 _ 1,
+    Ico_zero_eq_range, ← sub_one_mul_sum_log_div_pow_eq_sub_sum_digits, Nat.succ_eq_add_one]
+  rfl
 
 /-- **Kummer's Theorem**
 
