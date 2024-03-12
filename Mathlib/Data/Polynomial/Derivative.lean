cc600f2c799d6349854b42eaceeb23acaa95535b
grunweg <rothgami@math.hu-berlin.de>
2024-03-12 11:33:08 +0000

    chore: golf using omega (#11318)
    Backported from #11314.

--- Mathlib/Data/Polynomial/Derivative.lean
+++ Mathlib/Data/Polynomial/Derivative.lean
@@ -449,7 +449,7 @@ theorem iterate_derivative_mul {n} (p q : R[X]) :
     refine' sum_congr rfl fun k hk => _
     rw [mem_range] at hk
     congr
-    rw [tsub_add_eq_add_tsub (Nat.succ_le_of_lt hk), Nat.succ_sub_succ]
+    omega
   · rw [Nat.choose_zero_right, tsub_zero]
 #align polynomial.iterate_derivative_mul Polynomial.iterate_derivative_mul
 
