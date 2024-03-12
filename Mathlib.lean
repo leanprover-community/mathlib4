d1d17722b84ca3ebd060fd318f7ffa0542c1fe9f
Dagur Tómas Ásgeirsson <dagurtomas@gmail.com>
2024-03-12 11:06:43 +0000

    feat(LightProfinite): being light is a property of a profinite space (#10391)
    This PR defines the class `Profinite.IsLight` which is the property of a profinite space to have countably many clopens. We prove that such a profinite space gives rise to a `LightProfinite`, and the underlying profinite space of a `LightProfinite` is light. 
    
    - [x] depends on: #10390

--- Mathlib.lean
+++ Mathlib.lean
@@ -3776,6 +3776,7 @@ import Mathlib.Topology.Category.CompHaus.Limits
 import Mathlib.Topology.Category.CompHaus.Projective
 import Mathlib.Topology.Category.Compactum
 import Mathlib.Topology.Category.LightProfinite.Basic
+import Mathlib.Topology.Category.LightProfinite.IsLight
 import Mathlib.Topology.Category.Locale
 import Mathlib.Topology.Category.Profinite.AsLimit
 import Mathlib.Topology.Category.Profinite.Basic
