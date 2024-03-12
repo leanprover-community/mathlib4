110a0a3feaae9ac852066dd4c6dfc38f1b956a6c
David Loeffler <d.loeffler.01@cantab.net>
2024-03-12 11:06:45 +0000

    chore(Algebra/Group/AddChar): move results (#11312)
    Currently the definition of additive characters (from an additive to a multiplicative monoid) is hidden away in 
    `NumberTheory/LegendreSymbol`. These constructions seem to be getting used more widely, e.g. in Yaël's LeanAPAP project; so this PR carves off the parts of `NumberTheory/LegendreSymbol/AddCharacter` which don't depend on cyclotomic field arithmetic and moves them to `Algebra/Group`.

--- Mathlib.lean
+++ Mathlib.lean
@@ -172,6 +172,7 @@ import Mathlib.Algebra.GCDMonoid.Multiset
 import Mathlib.Algebra.GeomSum
 import Mathlib.Algebra.GradedMonoid
 import Mathlib.Algebra.GradedMulAction
+import Mathlib.Algebra.Group.AddChar
 import Mathlib.Algebra.Group.Aut
 import Mathlib.Algebra.Group.Basic
 import Mathlib.Algebra.Group.Commutator
