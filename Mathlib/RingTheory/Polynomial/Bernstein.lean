c3b43c0d807e8900bfce5e1bcfb92c903d5d8697
grunweg <rothgami@math.hu-berlin.de>
2024-03-12 11:33:08 +0000

    chore: golf using omega (#11318)
    Backported from #11314.

--- Mathlib/RingTheory/Polynomial/Bernstein.lean
+++ Mathlib/RingTheory/Polynomial/Bernstein.lean
@@ -227,9 +227,9 @@ theorem iterate_derivative_at_1 (n ν : ℕ) (h : ν ≤ n) :
   simp [Polynomial.eval_comp, h]
   obtain rfl | h' := h.eq_or_lt
   · simp
-  · congr
-    norm_cast
-    rw [← tsub_add_eq_tsub_tsub, tsub_tsub_cancel_of_le (Nat.succ_le_iff.mpr h')]
+  · norm_cast
+    congr
+    omega
 #align bernstein_polynomial.iterate_derivative_at_1 bernsteinPolynomial.iterate_derivative_at_1
 
 theorem iterate_derivative_at_1_ne_zero [CharZero R] (n ν : ℕ) (h : ν ≤ n) :
