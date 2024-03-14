beec5dcf619d4aa0784196bae175bb67b7b749fe
adomani <adomani@gmail.com>
2024-03-14 07:35:54 +0000

    Remove porting note

--- Mathlib/Data/Finset/Lattice.lean
+++ Mathlib/Data/Finset/Lattice.lean
@@ -1383,7 +1383,7 @@ theorem max_of_nonempty {s : Finset α} (h : s.Nonempty) : ∃ a : α, s.max = a
 theorem max_eq_bot {s : Finset α} : s.max = ⊥ ↔ s = ∅ :=
   ⟨fun h ↦ s.eq_empty_or_nonempty.elim id fun H ↦ by
       obtain ⟨a, ha⟩ := max_of_nonempty H
-      rw [h] at ha; cases ha; , -- Porting note: error without `done`
+      rw [h] at ha; cases ha; , -- the `;` is needed since the `cases` syntax allows `cases a, b`
     fun h ↦ h.symm ▸ max_empty⟩
 #align finset.max_eq_bot Finset.max_eq_bot
 
