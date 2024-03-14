1892ab90233c9e2d4c039f54e89a74f87943ba62
adomani <adomani@gmail.com>
2024-03-14 17:33:00 +0000

    Overall improvements

--- Mathlib/Tactic/UnusedTactic.lean
+++ Mathlib/Tactic/UnusedTactic.lean
@@ -6,11 +6,42 @@ Authors: Damiano Testa
 import Lean.Elab.Command
 import Lean.Linter.Util
 import Std.Tactic.Unreachable
+import Std.Data.List.Basic
 
 /-!
 #  The unused tactic linter
 
 The unused linter makes sure that every tactic call actually changes *something*.
+
+The inner workings of the linter as as follows.
+
+The linter inspects the goals before and after each tactic execution.
+If they are not identical, the linter is happy.
+If they are identical, then the linter checks if the tactic is whitelisted.
+Possible reason for whitelisting are
+* tactics that emit messages, such as `have?`, `extract_goal`, or `says`;
+* tactics that are in place to assert something, such as `guard`;
+* tactics that allow to work on a specific goal, such as `on_goal`;
+* "flow control" tactics, such as `success_if_fail` and related.
+
+The only tactic that has a bespoke criterion is `swap_var`: the reason is that the only change that
+`swap_var` has is to relabel the usernames of local declarations.
+Thus, to check that `swap_var` was used, so we inspect the names of all the local declarations
+before and after and see if there is some change.
+
+###  Notable exclusions
+
+* `conv` is completely ignored by the linter.
+
+* The linter does not enter a "sequence tactic": upon finding `tac <;> [tac1, tac2, ...]`
+  the linter assumes that the tactic is doing something and does not recurse into each
+  `tac1, tac2, ...`.
+  This is just for lack of an implementation: it may not be hard to do this.
+
+* The tactic does not check the discharger for `linear_combination`,
+  but checks `linear_combination` itself.
+  The main reason is that `skip` is a common discharger tactic and the linter would
+  then always fail whenever the user explicitly chose to pass `skip` as a discharger tactic.
 
 ##  Implementation notes
 
@@ -29,13 +60,24 @@ register_option linter.unusedTactic : Bool := {
 
 namespace unusedTactic
 
-/-- The monad for collecting `simp`s that are not `simp only`. -/
+/-- The monad for collecting the ranges of the syntaxes that do not modify any goal. -/
 abbrev M := StateRefT (HashMap String.Range Syntax) IO
 
 /-- `Parser`s allowed to not change the tactic state. -/
 abbrev allowed := [
   `Mathlib.Tactic.Says.says,
-  `Std.Tactic.«tacticOn_goal-_=>_»
+  `Std.Tactic.«tacticOn_goal-_=>_»,
+  -- the following `SyntaxNodeKind`s play a role in silencing `test`s
+  ``Lean.Parser.Tactic.guardHyp,
+  ``Lean.Parser.Tactic.guardTarget,
+  ``Lean.Parser.Tactic.failIfSuccess,
+  `Mathlib.Tactic.successIfFailWithMsg,
+  `Mathlib.Tactic.failIfNoProgress,
+  `Mathlib.Tactic.ExtractGoal.extractGoal,
+  `Mathlib.Tactic.Propose.propose',
+  `Lean.Parser.Tactic.traceState,
+  `Mathlib.Tactic.tacticMatch_target_,
+  `change?
 ]
 
 /--
@@ -55,6 +97,11 @@ initialize ignoreTacticKindsRef : IO.Ref NameHashSet ←
     |>.insert ``Lean.Parser.Tactic.Conv.conv
     |>.insert `Std.Tactic.seq_focus
     |>.insert `Mathlib.Tactic.Hint.registerHintStx
+    |>.insert `Mathlib.Tactic.LinearCombination.linearCombination
+    -- the following `SyntaxNodeKind`s play a role in silencing `test`s
+    |>.insert ``Lean.Parser.Tactic.failIfSuccess
+    |>.insert `Mathlib.Tactic.successIfFailWithMsg
+    |>.insert `Mathlib.Tactic.failIfNoProgress
 
 /-- Is this a syntax kind that contains intentionally unused tactic subterms? -/
 def isIgnoreTacticKind (ignoreTacticKinds : NameHashSet) (k : SyntaxNodeKind) : Bool :=
@@ -81,17 +128,34 @@ variable (ignoreTacticKinds : NameHashSet) (isTacKind : SyntaxNodeKind → Bool)
       if let some r := stx.getRange? true then
         modify fun m => m.insert r stx
 
+/-- `getNames mctx` extracts the names of all the local declarations implied by the
+`MetavarContext` `mctx`. -/
+def getNames (mctx : MetavarContext) : List Name :=
+  let lcts := mctx.decls.toList.map (MetavarDecl.lctx ∘ Prod.snd)
+  let locDecls := (lcts.map (PersistentArray.toList ∘ LocalContext.decls)).join.reduceOption
+  locDecls.map LocalDecl.userName
+
 mutual
-/-- Search for tactic executions in the info tree and remove executed tactic syntaxes. -/
+/-- Search for tactic executions in the info tree and remove the syntax of the tactics that
+changed something. -/
 partial def eraseUsedTacticsList (trees : PersistentArray InfoTree) : M Unit :=
   trees.forM eraseUsedTactics
 
-/-- Search for tactic executions in the info tree and remove executed tactic syntaxes. -/
+/-- Search for tactic executions in the info tree and remove the syntax of the tactics that
+changed something. -/
 partial def eraseUsedTactics : InfoTree → M Unit
   | .node i c => do
     if let .ofTacticInfo i := i then
       if let some r := i.stx.getRange? true then
-        if i.goalsAfter != i.goalsBefore || i.stx.getKind ∈ allowed then
+        let namesBefore := getNames i.mctxBefore
+        let namesAfter  := getNames i.mctxAfter
+        -- the goals have changed, or the tactic is allowed to not change the goals
+        let actual_change := i.goalsAfter != i.goalsBefore || i.stx.getKind ∈ allowed
+        -- bespoke check for `swap_var`: the only change that it does is
+        -- in the usernames of local declarations, so we check the names before and after
+        let name_change := (i.stx.getKind == `Mathlib.Tactic.«tacticSwap_var__,,») &&
+                           (namesBefore != namesAfter)
+        if actual_change || name_change then
           modify (·.erase r)
     eraseUsedTacticsList c
   | .context _ t => eraseUsedTactics t
@@ -103,7 +167,7 @@ end
 def getLinterHash (o : Options) : Bool := Linter.getLinterValue linter.unusedTactic o
 
 /-- The main entry point to the unused tactic linter. -/
-def unusedTactic : Linter where run := withSetOptionIn fun stx => do
+def unusedTacticLinter : Linter where run := withSetOptionIn fun stx => do
   unless getLinterHash (← getOptions) && (← getInfoState).enabled do
     return
   if (← get).messages.hasErrors then
@@ -128,4 +192,4 @@ def unusedTactic : Linter where run := withSetOptionIn fun stx => do
     Linter.logLint linter.unusedTactic stx m!"'{stx}' tactic does nothing"
     last := r
 
-initialize addLinter unusedTactic
+initialize addLinter unusedTacticLinter
