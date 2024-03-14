f9ddf34f0bb205208eb244d9a63922312388423e
jt496 <john.talbot@gmail.com>
2024-03-14 17:17:34 +0000

    possibly better names

--- Mathlib/Combinatorics/SimpleGraph/Chromatic.lean
+++ Mathlib/Combinatorics/SimpleGraph/Chromatic.lean
@@ -26,14 +26,14 @@ section Fin
 
 variable {r : ℕ}
 /-- If r < χ(G) then G is not r-colorable -/
-lemma not_col_lt_chrom (hr : r < G.chromaticNumber) : ¬ G.Colorable r := by
+lemma not_col_lt_chromaticNumber (hr : r < G.chromaticNumber) : ¬ G.Colorable r := by
   contrapose! hr
   exact Colorable.chromaticNumber_le hr
 
 
-/-- In particular, if χ(G) = r + 1 then G is not r-colorable -/
-lemma not_col_chrom_succ (hcrom: G.chromaticNumber = r + 1) : ¬ G.Colorable r := by
-  apply not_col_lt_chrom
+/-- If χ(G) = r + 1 then G is not r-colorable -/
+lemma not_col_chromaticNumber_eq_succ (hcrom: G.chromaticNumber = r + 1) : ¬ G.Colorable r := by
+  apply not_col_lt_chromaticNumber
   rw [hcrom]
   exact_mod_cast Nat.lt_succ_self _
 
@@ -54,7 +54,7 @@ def Coloring.not_surj_toColoring {i : Fin (r + 1)} (C : G.Coloring (Fin (r + 1))
     · exact C.valid had hne⟩
 
 
-/-- If r ≤ χ(G) and C is an r-coloring then C is surjective  -/
+/-- If r ≤ χ(G) and C is an r-coloring then C is surjective -/
 lemma Coloring.le_chromatic_surj (C : G.Coloring (Fin r)) (hcrom: r ≤ G.chromaticNumber) :
     Function.Surjective C := by
   intro i
@@ -67,7 +67,7 @@ lemma Coloring.le_chromatic_surj (C : G.Coloring (Fin r)) (hcrom: r ≤ G.chroma
 
 
 /-- If G has an (r + 1)-coloring in which no vertex of color i is adjacent to a vertex of color j
-and i ≠ j then we can define an r - coloring of G -/
+and i ≠ j then we can define an r-coloring of G -/
 def Coloring.not_colorClasses_adj_toColoring {i j: Fin (r + 1)} (C : G.Coloring (Fin (r + 1)))
     (hc: ∀ v w, C v = i → C w = j → ¬G.Adj v w) (hij : i ≠ j) : G.Coloring (Fin r) := by
   let C': G.Coloring (Fin (r + 1)) := ⟨fun v ↦ ite (C v = j) i (C v), by
@@ -92,11 +92,12 @@ lemma Coloring.colorClasses_adj  {i j : Fin r} (C : G.Coloring (Fin r))
   | zero => exact i.elim0
   | succ r =>
     by_contra! hc
-    exact not_col_chrom_succ hcrom ⟨_,(C.not_colorClasses_adj_toColoring hc hij).valid⟩
+    exact not_col_chromaticNumber_eq_succ hcrom ⟨_,(C.not_colorClasses_adj_toColoring hc hij).valid⟩
 
 end Fin
 
 variable {β : Type v}
+
 /-- If G has a β-Coloring then χ(G) = ‖β‖ ↔ every β-Coloring of G is surjective -/
 theorem chromaticNumber_eq_card_iff_forall_surj [Fintype β] (C : G.Coloring β) :
     G.chromaticNumber = Fintype.card β  ↔ (∀ C': G.Coloring β, Function.Surjective C') := by
