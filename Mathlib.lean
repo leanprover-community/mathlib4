be4d10d9f2e01a5ac8be77d71d17314747274fae
Mitchell Lee <trivial171@gmail.com>
2024-03-15 16:22:37 -0400

    Rename Exchange.lean to Matsumoto.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -2320,7 +2320,7 @@ import Mathlib.GroupTheory.Coset
 import Mathlib.GroupTheory.Coxeter.Basic
 import Mathlib.GroupTheory.Coxeter.Length
 import Mathlib.GroupTheory.Coxeter.StandardGeometricRepresentation
-import Mathlib.GroupTheory.Coxeter.Exchange
+import Mathlib.GroupTheory.Coxeter.Matsumoto
 import Mathlib.GroupTheory.Divisible
 import Mathlib.GroupTheory.DoubleCoset
 import Mathlib.GroupTheory.EckmannHilton
