276ef52a41fd8100ebe95e859866b95662daad9b
Riccardo Brasca <riccardo.brasca@gmail.com>
2024-03-15 14:15:17 +0100

    progress

--- Mathlib/NumberTheory/FLT/Three.lean
+++ Mathlib/NumberTheory/FLT/Three.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Riccardo Brasca
 -/
 import Mathlib.Data.ZMod.Basic
+import Mathlib.NumberTheory.FLT.Basic
 
 /-!
 # Fermat Last Theorem in the case `n = 3`
@@ -38,3 +39,26 @@ theorem fermatLastTheoremThree_case_1 {a b c : ℕ} (hdvd : ¬ 3 ∣ a * b * c) 
   rcases cube_of_not_dvd hdvd.1.2 with hb | hb <;>
   rcases cube_of_not_dvd hdvd.2 with hc | hc <;>
   rw [ha, hb, hc] <;> decide
+
+theorem fermatLastTheoremThree_of_three_dvd_c
+    (H : ∀ a b c : ℤ, a ≠ 0 → b ≠ 0 → c ≠ 0 → 3 ∣ c → a ^ 3 + b ^ 3 ≠ c ^ 3) :
+    FermatLastTheoremFor 3 := by
+  intro a b c ha hb hc habc
+  by_cases h1 : 3 ∣ a * b * c
+  swap
+  · exact fermatLastTheoremThree_case_1 h1 habc
+  rw [Nat.Prime.dvd_mul (Nat.prime_three), Nat.Prime.dvd_mul (Nat.prime_three)] at h1
+  rcases h1 with ((⟨k, hk⟩ | ⟨k, hk⟩) | ⟨k, hk⟩)
+  · refine H (-(c : ℤ)) b (-(a : ℤ)) (by simp [hc]) (by simp [hb]) (by simp [ha]) ?_ ?_
+    · exact ⟨-k, by simp [hk]⟩
+    · rw [Odd.neg_pow (by decide), Odd.neg_pow (by decide), add_comm, ← sub_eq_add_neg,
+        sub_eq_iff_eq_add, add_comm, ← sub_eq_add_neg, eq_sub_iff_add_eq, add_comm]
+      exact_mod_cast habc
+  · refine H a (-(c : ℤ)) ((-(b : ℤ))) (by simp [ha]) (by simp [hc]) (by simp [hb]) ?_ ?_
+    · exact ⟨-k, by simp [hk]⟩
+    · rw [Odd.neg_pow (by decide), Odd.neg_pow (by decide), ← sub_eq_add_neg, sub_eq_iff_eq_add,
+        add_comm, ← sub_eq_add_neg, eq_sub_iff_add_eq]
+      exact_mod_cast habc
+  · refine H a b c (by simp [ha]) (by simp [hb]) (by simp [hc]) ?_ ?_
+    · exact ⟨k, by simp [hk]⟩
+    · exact_mod_cast habc
