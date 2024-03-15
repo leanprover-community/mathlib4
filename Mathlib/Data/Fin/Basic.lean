a79b866224cbb9bea6e6318d7e13f3cf051fe49a
Oliver Nash <github@olivernash.org>
2024-03-15 08:59:52 +0000

    Update Mathlib/Data/Fin/Basic.lean
    Co-authored-by: damiano <adomani@gmail.com>

--- Mathlib/Data/Fin/Basic.lean
+++ Mathlib/Data/Fin/Basic.lean
@@ -673,7 +673,7 @@ theorem val_cast_of_lt {n : ℕ} [NeZero n] {a : ℕ} (h : a < n) : (a : Fin n).
   Nat.mod_eq_of_lt h
 #align fin.coe_val_of_lt Fin.val_cast_of_lt
 
-/-- Converting the value of a `Fin (n + 1)` to `Fin (n + 1)` results
+/-- If `n` is non-zero, converting the value of a `Fin n` to `Fin n` results
 in the same value.  -/
 @[simp] theorem cast_val_eq_self {n : ℕ} [NeZero n] (a : Fin n) : (a.val : Fin n) = a :=
   ext <| val_cast_of_lt a.isLt
