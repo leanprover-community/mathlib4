f43592903ca2cf36172ae6e050461d9315a3738a
Mitchell Lee <trivial171@gmail.com>
2024-03-15 16:05:01 -0400

    delete exchange lemma material

--- Mathlib.lean
+++ Mathlib.lean
@@ -2319,7 +2319,6 @@ import Mathlib.GroupTheory.Coset
 import Mathlib.GroupTheory.Coxeter.Basic
 import Mathlib.GroupTheory.Coxeter.Length
 import Mathlib.GroupTheory.Coxeter.StandardGeometricRepresentation
-import Mathlib.GroupTheory.Coxeter.Exchange
 import Mathlib.GroupTheory.Divisible
 import Mathlib.GroupTheory.DoubleCoset
 import Mathlib.GroupTheory.EckmannHilton
