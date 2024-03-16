52c224796dc4cdf11163d45c5092d493870dfa80
David Loeffler <d.loeffler.01@cantab.net>
2024-03-16 17:43:03 +0100

    review comments

--- Mathlib/Algebra/Group/AddChar.lean
+++ Mathlib/Algebra/Group/AddChar.lean
@@ -52,13 +52,16 @@ interested in much more specific cases (e.g. when `A` is a group and `M` a commu
  -/
 structure AddChar where
   /-- The underlying function.
+
   Do not use this function directly. Instead use the coercion coming from the `FunLike`
   instance. -/
   toFun : A → M
   /-- The function maps `0` to `1`.
+
   Do not use this directly. Instead use `AddChar.map_zero_one`. -/
   map_zero_one' : toFun 0 = 1
   /- The function maps addition in `A` to multiplication in `M`.
+
   Do not use this directly. Instead use `AddChar.map_add_mul`. -/
   map_add_mul' : ∀ a b : A, toFun (a + b) = toFun a * toFun b
 
@@ -107,7 +110,8 @@ def toMonoidHom (φ : AddChar A M) : Multiplicative A →* M where
 -- this instance was a bad idea and conflicted with `instFunLike` above
 #noalign add_char.monoid_hom_class
 
-@[simp] lemma toMonoidHom_apply (ψ : AddChar A M) (a : Multiplicative A) : ψ.toMonoidHom a = ψ a :=
+@[simp] lemma toMonoidHom_apply (ψ : AddChar A M) (a : Multiplicative A) :
+  ψ.toMonoidHom a = ψ (Multiplicative.toAdd a) :=
   rfl
 #align add_char.coe_to_fun_apply AddChar.toMonoidHom_apply
 
