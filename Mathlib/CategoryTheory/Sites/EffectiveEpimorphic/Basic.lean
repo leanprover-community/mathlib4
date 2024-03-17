aed42ad18c26823e0fe692ea214f67eea73e9125
dagurtomas <dagurtomas@gmail.com>
2024-03-17 20:47:32 +0000

    unused import

--- Mathlib/CategoryTheory/Sites/EffectiveEpimorphic/Basic.lean
+++ Mathlib/CategoryTheory/Sites/EffectiveEpimorphic/Basic.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Adam Topaz
 -/
 import Mathlib.CategoryTheory.Sites.Sieves
-import Mathlib.CategoryTheory.Limits.Shapes.FiniteProducts
+import Mathlib.CategoryTheory.Limits.Shapes.Products
 /-!
 
 # Effective epimorphisms
