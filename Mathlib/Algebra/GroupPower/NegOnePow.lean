f056bef158869298d22803f5a704abba9917a7cc
Mitchell Lee <trivial171@gmail.com>
2024-03-16 23:23:25 -0400

    Add name to author list of NegOnePow.lean

--- Mathlib/Algebra/GroupPower/NegOnePow.lean
+++ Mathlib/Algebra/GroupPower/NegOnePow.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2023 Joël Riou. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Joël Riou, Johan Commelin
+Authors: Joël Riou, Johan Commelin, Mitchell Lee
 -/
 import Mathlib.Data.Int.Parity
 import Mathlib.Data.ZMod.IntUnitsPower
