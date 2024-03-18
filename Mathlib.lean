aea00c064a302c4ea3eb9a0b6c8c33a9495a9e48
Markus Himmel <markus@himmel-villmar.de>
2024-03-18 16:16:25 +0100

    feat: limits in the category of elements

--- Mathlib.lean
+++ Mathlib.lean
@@ -1167,6 +1167,7 @@ import Mathlib.CategoryTheory.Limits.Constructions.Pullbacks
 import Mathlib.CategoryTheory.Limits.Constructions.WeaklyInitial
 import Mathlib.CategoryTheory.Limits.Constructions.ZeroObjects
 import Mathlib.CategoryTheory.Limits.Creates
+import Mathlib.CategoryTheory.Limits.Elements
 import Mathlib.CategoryTheory.Limits.EpiMono
 import Mathlib.CategoryTheory.Limits.EssentiallySmall
 import Mathlib.CategoryTheory.Limits.ExactFunctor
