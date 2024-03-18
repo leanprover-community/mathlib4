eb92a4b6c9dc8537e5e2878c67866d2d7bea6980
palalansouki <palalansouki@gmail.com>
2024-03-18 11:24:31 +0900

    Lint

--- Mathlib.lean
+++ Mathlib.lean
@@ -1519,6 +1519,7 @@ import Mathlib.Computability.RegularExpressions
 import Mathlib.Computability.TMComputable
 import Mathlib.Computability.TMToPartrec
 import Mathlib.Computability.TuringMachine
+import Mathlib.Computability.UniformlyPrimcodable
 import Mathlib.Condensed.Abelian
 import Mathlib.Condensed.Adjunctions
 import Mathlib.Condensed.Basic
