4538285bdf2fc5cf45496991db6db7ed86bc99bd
dagurtomas <dagurtomas@gmail.com>
2024-03-18 22:53:14 +0000

    feat(CategoryTheory): a functor between FinitaryPreExtensive categories reflects finite effective epi families under mild assumptions

---
+++ Mathlib/CategoryTheory/EffectiveEpi/Extensive.lean
@@ -0,0 +1,29 @@
+import Mathlib.CategoryTheory.EffectiveEpi.Preserves
+import Mathlib.CategoryTheory.EffectiveEpi.Coproduct
+import Mathlib.CategoryTheory.Extensive
+import Mathlib.CategoryTheory.Limits.Preserves.Finite
+
+namespace CategoryTheory
+
+open Limits
+
+variable {C : Type*} [Category C] [FinitaryPreExtensive C]
+
+theorem effectiveEpi_desc_iff_effectiveEpiFamily {α : Type} [Finite α]
+    {B : C} (X : α → C) (π : (a : α) → X a ⟶ B) :
+    EffectiveEpi (Sigma.desc π) ↔ EffectiveEpiFamily X π := by
+  exact ⟨fun h ↦ ⟨⟨@effectiveEpiFamilyStructOfEffectiveEpiDesc _ _ _ _ X π _ h _ _ (fun g ↦
+    (FinitaryPreExtensive.sigma_desc_iso (fun a ↦ Sigma.ι X a) g inferInstance).epi_of_iso)⟩⟩,
+    fun _ ↦ inferInstance⟩
+
+variable {D : Type*} [Category D] [FinitaryPreExtensive D]
+variable (F : C ⥤ D) [F.ReflectsEffectiveEpis] [PreservesFiniteCoproducts F]
+
+instance : F.ReflectsFiniteEffectiveEpiFamilies where
+  reflects {α _ B} X π h := by
+    have : Fintype α := Fintype.ofFinite _
+    simp only [← effectiveEpi_desc_iff_effectiveEpiFamily]
+    apply F.effectiveEpi_of_map
+    convert (inferInstance :
+      EffectiveEpi (inv (sigmaComparison F X) ≫ (Sigma.desc (fun a ↦ F.map (π a)))))
+    simp
