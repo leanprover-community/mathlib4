1cd8c312c130507db7dc4189ae0cb76d85315664
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2024-03-18 21:30:08 +0100

    feat: NumberField.InfinitePlace.infinitePlace_apply (TODO REVIEW)

--- Mathlib/NumberTheory/NumberField/InfinitePlace/Ramification.lean
+++ Mathlib/NumberTheory/NumberField/InfinitePlace/Ramification.lean
@@ -109,7 +109,6 @@ lemma card_mono [NumberField k] [NumberField K] :
 variable {k K}
 
 /-- The action of the Galois group on infinite places. -/
-@[simps! smul_coe_apply]
 instance : MulAction Gal(K/k) (InfinitePlace K) where
   smul := fun σ w ↦ w.comap σ.symm
   one_smul := fun _ ↦ rfl
