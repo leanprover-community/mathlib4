8c9cd1bcc1016be82c66d83721b9d1dbca91a32d
Yaël Dillies <yael.dillies@gmail.com>
2024-03-19 15:42:54 +0000

    fix

--- Mathlib/Combinatorics/SetFamily/AhlswedeZhang.lean
+++ Mathlib/Combinatorics/SetFamily/AhlswedeZhang.lean
@@ -5,9 +5,8 @@ Authors: Yaël Dillies, Vladimir Ivanov
 -/
 import Mathlib.Algebra.BigOperators.Intervals
 import Mathlib.Algebra.BigOperators.Order
-import Mathlib.Algebra.BigOperators.Ring
+import Mathlib.Algebra.Order.Field.Basic
 import Mathlib.Data.Finset.Sups
-import Mathlib.Order.Hom.Lattice
 import Mathlib.Tactic.FieldSimp
 import Mathlib.Tactic.Ring
 
@@ -417,3 +416,4 @@ lemma infSum_eq_one (h𝒜₁ : 𝒜.Nonempty) (h𝒜₀ : ∅ ∉ 𝒜) : infSu
   simpa
 
 end AhlswedeZhang
+#minimize_imports
