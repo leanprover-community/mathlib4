4157f3e13652c0788fb51d09dd3898568b876452
adomani <adomani@gmail.com>
2024-03-19 11:04:19 +0100

    add real goals

--- Mathlib/Tactic/NonTerminalSimp.lean
+++ Mathlib/Tactic/NonTerminalSimp.lean
@@ -45,14 +45,100 @@ def onlyOrNotSimp : Syntax → Bool
 /-- The monad for collecting `simp`s that are not `simp only`. -/
 abbrev M := StateRefT (HashMap String.Range Syntax) IO
 
+def inertGoals (t : TacticInfo) : List MVarId :=
+  t.goalsBefore.filter (·.name ∈ t.goalsAfter.map (·.name))
+
+def willChange (t : TacticInfo) : List MVarId :=
+  t.goalsBefore.filter (·.name ∉ t.goalsAfter.map (·.name))
+
+def haveChanged (t : TacticInfo) : List MVarId :=
+  t.goalsAfter.filter (·.name ∉ t.goalsBefore.map (·.name))
+
+variable (kind : SyntaxNodeKind) in
+partial
+def extractRealGoals : InfoTree → Array (Syntax × List MVarId)
+  | .node k args =>
+    let kargs := (args.map extractRealGoals).foldl (· ++ ·) #[]
+    if let Lean.Elab.Info.ofTacticInfo i := k then
+      if k.stx.getKind == kind then
+        kargs.push (k.stx, haveChanged i) else kargs
+    else kargs
+  | .context _ t => extractRealGoals t
+  | _ => default
+
+variable (kind : SyntaxNodeKind) in
+partial
+def extractGoals : InfoTree → Array (Syntax × List MVarId × List MVarId)
+  | .node i args =>
+    let kargs := (args.map extractGoals).foldl (· ++ ·) #[]
+    if let Lean.Elab.Info.ofTacticInfo i := i then
+--      dbg_trace "{i.stx}"
+      if i.stx.getKind == kind then kargs.push (i.stx, i.goalsBefore, i.goalsAfter) else kargs
+    else kargs
+  | .context _ t => extractGoals t
+  | _ => default
+
+variable (mvs : List MVarId) in
+partial
+def getRealFollowers : InfoTree → Array Syntax
+  | .node k args =>
+    let kargs := (args.map getRealFollowers).foldl (· ++ ·) #[]
+    if let Lean.Elab.Info.ofTacticInfo i := k then
+      if (mvs.map fun x => (x.name ∈ i.goalsBefore.map (·.name) : Bool)).any (·) then kargs.push k.stx else kargs
+    else kargs
+  | .context _ t => getRealFollowers t
+  | _ => default
+
+variable (mvs : List MVarId) in
+partial
+def getFollowers : InfoTree → Array Syntax
+  | .node k args =>
+    let kargs := (args.map getFollowers).foldl (· ++ ·) #[]
+    if let Lean.Elab.Info.ofTacticInfo i := k then
+      if i.goalsBefore == mvs then kargs.push k.stx else kargs
+    else kargs
+  | _ => default
+
+
 mutual
 /-- Search for `simp`s that
 * are not `simp only` and
 * do not close a goal.
 
 Add such `simp`s to the state. -/
-partial def addNonSimpOnlysList (trees : PersistentArray InfoTree) : M Unit :=
+partial def addNonSimpOnlysList (trees : PersistentArray InfoTree) : M Unit := do
+  let gls := trees.map (extractGoals ``Lean.Parser.Tactic.refine ·)
+/-
+  Command.liftCoreM do
+    Meta.MetaM.run do
+      let res := trees.foldl (fun d e => d ++ extractGoals `Lean.Parser.Tactic.refine e) #[]
+      for (a, b) in res do
+        let mvs ← b.mapM (·.getType)
+        logInfo "()"
+-/
+
+/-
+  let mut rest := #[]
+  for d in gls do
+  --let postGoals ← gls.mapM fun d =>
+    for (s, _, aft) in d do
+      dbg_trace "* Following {s}\n"
+      let _x_ ← trees.mapM fun s => (for y in (getRealFollowers aft s) do dbg_trace "** {y}\n"; return )
+      rest := rest.push aft
+-/
+
+--      return aft --(return Prod.snd d)
+--  for r in rest do
+--    dbg_trace "following {r.map (·.name)}"
+--    let x ← trees.mapM fun s => (dbg_trace (getFollowers r s); return )
+--  for g in gls do
+--    for (stx, goals_before, goals_after) in g do
+--      IO.println s!"{stx}"
+--      IO.println <| goals_before.map (·.name)
+--      IO.println <| goals_after.map (·.name)
   trees.forM addNonSimpOnlys
+
+
 
 @[inherit_doc addNonSimpOnlysList]
 partial def addNonSimpOnlys : InfoTree → M Unit
@@ -78,7 +164,43 @@ def nonTerminalSimpLinter : Linter where run := withSetOptionIn fun _stx => do
     return
   if (← get).messages.hasErrors then
     return
-  let (_, map) ← (addNonSimpOnlysList (← getInfoTrees)).run {}
+--  dbg_trace "processing"
+  let trees ← getInfoTrees
+  let (_, map) ← (addNonSimpOnlysList trees).run {}
+--  dbg_trace "processing1"
+
+  trees.forM fun tree => do
+--    dbg_trace "processing2"
+    let d := extractRealGoals ``Lean.Parser.Tactic.refine tree
+--    dbg_trace "processing3 {d.map Prod.fst} {d.size}"
+    for (st, aft) in d do
+--      dbg_trace "* Following {st}\n"
+
+      --for s in trees do
+--      let mut tot := #[]
+      for y in (getRealFollowers aft tree) do
+
+--        tots := tots.push (y, st)
+          dbg_trace "{y.getAtomVal} follows {st.getAtomVal}"
+          Linter.logLint linter.nonTerminalSimp y m!"follows {st}"
+--          tot := tot.push m!"follows {st}"
+--      logInfo m!"{tot}"
+/-
+  let gls := trees.map (extractGoals ``Lean.Parser.Tactic.refine ·)
+  for d in gls do
+  --let postGoals ← gls.mapM fun d =>
+    for (st, _, aft) in d do
+      --dbg_trace "* Following {s}\n"
+      for s in trees do
+        for y in (getRealFollowers aft s) do
+
+--        tots := tots.push (y, st)
+          Linter.logLint linter.nonTerminalSimp y m!"follows {st}"
+-/
+--          dbg_trace "** {y} {r}\n"; --return
+--      rest := rest.push aft
+
+
   let simps := map.toArray
   let key (r : String.Range) := (r.start.byteIdx, (-r.stop.byteIdx : Int))
   let mut last : String.Range := ⟨0, 0⟩
