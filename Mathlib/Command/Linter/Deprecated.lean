22f910daf08bedae06aa6bd06378d892255e58f7
thorimur <68410468+thorimur@users.noreply.github.com>
2024-03-19 18:58:57 -0400

    feat: deprecated syntax linter

---
+++ Mathlib/Command/Linter/Deprecated.lean
@@ -0,0 +1,27 @@
+/-
+Copyright (c) 2024 Thomas R. Murrills. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Thomas R. Murrills
+-/
+import Mathlib.Command.Linter.LintingRules
+
+open Lean Linter
+
+/--
+The `deprecated` `linting_rules` category can be used to deprecate syntax.
+
+Example:
+```lean
+linting_rules : deprecated
+| `(node|stx) => do
+  ...
+  return (msg : MessageData)
+```
+-/
+register_linting_rules_cat deprecated := {
+  Out := MessageData
+  resolve := fun msg => do
+    logWarning <| .tagged ``deprecatedAttr msg
+    return .done
+  opt := .existing linter.deprecated
+}
