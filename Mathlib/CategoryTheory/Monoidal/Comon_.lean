464d7b7deb1c892c9287d59c18fc0e07fd91cb3e
Scott Morrison <scott.morrison@gmail.com>
2024-03-19 21:11:20 +1100

    cleanup

--- Mathlib/CategoryTheory/Monoidal/Comon_.lean
+++ Mathlib/CategoryTheory/Monoidal/Comon_.lean
@@ -9,7 +9,6 @@ import Mathlib.CategoryTheory.Monoidal.Transport
 import Mathlib.CategoryTheory.Monoidal.CoherenceLemmas
 import Mathlib.CategoryTheory.Limits.Shapes.Terminal
 import Mathlib.CategoryTheory.Monoidal.Mon_
-import Mathlib.Tactic.RewriteSearch
 
 /-!
 # The category of comonoids in a monoidal category.
