ac5258664f3466658010c0af3c8e8ece2ca547ed
Scott Morrison <scott.morrison@gmail.com>
2024-03-19 16:44:26 +1100

    shake

--- Mathlib/CategoryTheory/Monoidal/Comon_.lean
+++ Mathlib/CategoryTheory/Monoidal/Comon_.lean
@@ -3,8 +3,8 @@ Copyright (c) 2024 Lean FRO LLC. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison
 -/
-import Mathlib.CategoryTheory.Monoidal.Mon_
-import Mathlib.CategoryTheory.Monoidal.Opposite
+import Mathlib.CategoryTheory.Monoidal.CoherenceLemmas
+import Mathlib.CategoryTheory.Limits.Shapes.Terminal
 
 /-!
 # The category of comonoids in a monoidal category.
