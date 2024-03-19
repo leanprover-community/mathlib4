6cb8334f9352abd2cfcfcaf5489ca3d5e00ec8ce
FMLJohn <417963616@qq.com>
2024-03-19 13:46:16 +0000

    minimized the number of imports

--- Mathlib/Algebra/HilbertSerre/HilbertPolynomial.lean
+++ Mathlib/Algebra/HilbertSerre/HilbertPolynomial.lean
@@ -3,7 +3,6 @@ Copyright (c) 2024 Fangming Li. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Fangming Li
 -/
-import Mathlib.Data.Real.Basic
 import Mathlib.Algebra.HilbertSerre.Theorem
 
 /-!
