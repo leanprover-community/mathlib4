f5bec9de6af39ca7858b3fc8f8b88a8c79815ebf
dagurtomas <dagurtomas@gmail.com>
2024-03-20 22:48:52 +0000

    sorry free

--- Mathlib/Condensed/EquivalenceGeneral.lean
+++ Mathlib/Condensed/EquivalenceGeneral.lean
@@ -11,22 +11,13 @@ namespace CategoryTheory
 open Limits
 
 variable {C D : Type*} [Category C] [Category D] (F : C ⥤ D)
-  [F.PreservesEffectiveEpis] [F.ReflectsEffectiveEpis]
-  (h : ∀ (X : D), ∃ (W : C) (π : F.obj W ⟶ X), EffectiveEpi π)
-  [Preregular D] [Full F] [Faithful F]
-  [FinitaryExtensive D] [Preregular D]
-  [PreservesFiniteCoproducts F]
-  [HasFiniteCoproducts C] [HasPullbacksOfInclusions C]
-  [PreservesPullbacksOfInclusions F]
-  [ReflectsLimitsOfShape WalkingCospan F]
-  [ReflectsColimitsOfShape (Discrete WalkingPair) F]
 
 noncomputable section EffectivePresentation
 
 structure Functor.EffectivePresentation (X : D) where
   p : C
   f : F.obj p ⟶ X
-  effectiveEpi : EffectiveEpi f
+  effectiveEpi : IsSplitEpi f
 
 class Functor.EffectivelyEnough : Prop where
   presentation : ∀ (X : D), Nonempty (F.EffectivePresentation X)
@@ -49,15 +40,26 @@ from the arbitrarily chosen projective object over `X`.
 def π (X : D) : over F X ⟶ X :=
   (EffectivelyEnough.presentation (F := F) X).some.f
 
-instance π_effectiveEpi (X : D) : EffectiveEpi (π F X) :=
+instance π_isSplitEpi (X : D) : IsSplitEpi (π F X) :=
   (EffectivelyEnough.presentation X).some.effectiveEpi
+
+instance π_effectiveEpi (X : D) : EffectiveEpi (π F X) := by
+  rw [← Category.comp_id (π F X)]
+  infer_instance
+  -- todo: add general split epi -> effective epi instance
 
 end Projective'
 
 end EffectivePresentation
 
-variable [F.EffectivelyEnough] [∀ (X : D), Projective X]
--- variable [FinitaryExtensive (Projs C)] [Preregular (Projs C)]
+variable [F.PreservesEffectiveEpis] [F.ReflectsEffectiveEpis]
+  [Preregular D] [Full F] [Faithful F]
+  [FinitaryExtensive D] [Preregular D]
+  [HasFiniteCoproducts C]
+  [HasPullbacksOfInclusions C]
+  [PreservesFiniteCoproducts F]
+  [PreservesPullbacksOfInclusions F]
+  [F.EffectivelyEnough] [∀ (X : D), Projective X]
 
 open Projective'
 
@@ -88,8 +90,6 @@ instance isCoverDense : F.IsCoverDense (coherentTopology _) := by
   · cases hypo1
     exact ⟨⟨_, h, π F B, hf⟩⟩
 
--- Need the additional property that if a composition is effective epi, then the second map in
--- the composition is effective epi
 theorem coverDense.inducedTopology_Sieve_iff_EffectiveEpiFamily (X : C) (S : Sieve X) :
     (∃ (α : Type) (_ : Finite α) (Y : α → C) (π : (a : α) → (Y a ⟶ X)),
       EffectiveEpiFamily Y π ∧ (∀ a : α, (S.arrows) (π a)) ) ↔
@@ -103,12 +103,19 @@ theorem coverDense.inducedTopology_Sieve_iff_EffectiveEpiFamily (X : C) (S : Sie
     exact F.map_finite_effectiveEpiFamily _ _
   · obtain ⟨α, _, Y, π, ⟨H₁, H₂⟩⟩ := (coherentTopology.mem_sieves_iff_hasEffectiveEpiFamily _).mp hS
     refine ⟨α, inferInstance, ?_⟩
-    obtain ⟨Y₀, H₂⟩ := Classical.skolem.mp H₂
-    obtain ⟨π₀, H₂⟩ := Classical.skolem.mp H₂
-    obtain ⟨f₀, H₂⟩ := Classical.skolem.mp H₂
-    refine ⟨Y₀ , π₀, ?_, fun i ↦ (H₂ i).1⟩
-    refine F.finite_effectiveEpiFamily_of_map _ _ ?_
-    sorry
+    let Z : α → C := fun a ↦ (Functor.EffectivelyEnough.presentation (F := F) (Y a)).some.p
+    let g₀ : (a : α) → F.obj (Z a) ⟶ Y a := fun a ↦ Projective'.π F (Y a)
+    have : EffectiveEpiFamily _ (fun a ↦ g₀ a ≫ π a) := inferInstance
+    refine ⟨Z , fun a ↦ Full.preimage (g₀ a ≫ π a), ?_, fun a ↦ (?_ : S.arrows (Full.preimage _))⟩
+    · refine F.finite_effectiveEpiFamily_of_map _ _ ?_
+      simpa using this
+    · obtain ⟨W, g₁, g₂, h₁, h₂⟩ := H₂ a
+      rw [h₂]
+      have : Full.preimage (g₀ a ≫ g₂ ≫ F.map g₁) = (Full.preimage (g₀ a ≫ g₂)) ≫ g₁ := by
+        apply Faithful.map_injective (F := F)
+        simp
+      rw [this]
+      exact S.downward_closed h₁ _
 
 lemma coherentTopology_is_induced : haveI := finitaryExtensive_of_preserves_and_reflects F
     haveI : Preregular C := reflects_preregular F (fun X ↦ ⟨_, π F X, inferInstance⟩)
@@ -137,12 +144,26 @@ instance : haveI := finitaryExtensive_of_preserves_and_reflects F
     F.IsContinuous (coherentTopology _) (coherentTopology _) :=
   Functor.IsCoverDense.isContinuous _ _ _ (coverPreserving F)
 
--- Need to figure out the universes here
--- /-- The equivalence from coherent sheaves on `Stonean` to coherent sheaves on `CompHaus`
---     (i.e. condensed sets). -/
--- noncomputable
--- def equivalence (A : Type _) [Category A] [HasLimits A] :
---     haveI := finitaryExtensive_of_preserves_and_reflects F
---     haveI : Preregular C := sorry
---     Sheaf (coherentTopology C) A ≌ Sheaf (coherentTopology D) A :=
---   Functor.IsCoverDense.sheafEquivOfCoverPreservingCoverLifting F _ _ _
+section SheafEquiv
+
+universe u
+
+variable {C D : Type (u+1)} [LargeCategory C] [LargeCategory D] (F : C ⥤ D)
+  [F.PreservesEffectiveEpis] [F.ReflectsEffectiveEpis]
+  [Preregular D] [Full F] [Faithful F]
+  [FinitaryExtensive D] [Preregular D]
+  [PreservesFiniteCoproducts F]
+  [HasFiniteCoproducts C] [HasPullbacksOfInclusions C]
+  [PreservesPullbacksOfInclusions F]
+  [ReflectsLimitsOfShape WalkingCospan F]
+  [ReflectsColimitsOfShape (Discrete WalkingPair) F]
+  [F.EffectivelyEnough] [∀ (X : D), Projective X]
+
+/-- The equivalence from coherent sheaves on `Stonean` to coherent sheaves on `CompHaus`
+    (i.e. condensed sets). -/
+noncomputable
+def equivalence (A : Type _) [Category.{u+1} A] [HasLimits A] :
+    haveI := finitaryExtensive_of_preserves_and_reflects F
+    haveI : Preregular C := reflects_preregular F (fun X ↦ ⟨_, π F X, inferInstance⟩)
+    Sheaf (coherentTopology C) A ≌ Sheaf (coherentTopology D) A :=
+  Functor.IsCoverDense.sheafEquivOfCoverPreservingCoverLifting F _ _ _
