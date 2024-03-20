a230953d74ae1cdbc39ce29b3c61d2be074e3e7d
Rémy Degenne <Remydegenne@gmail.com>
2024-03-20 15:45:55 +0000

    feat: density of a finite kernel wrt another kernel (#10948)
    Let `κ : kernel α (γ × β)` and `ν : kernel α γ` be two finite kernels with `kernel.fst κ ≤ ν`, where `γ` has a countably generated σ-algebra (true in particular for standard Borel spaces).
    We build a function `f : α → γ → Set β → ℝ` jointly measurable in the first two arguments such that for all `a : α` and all measurable sets `s : Set β` and `A : Set γ`, `∫ x in A, f a x s ∂(ν a) = (κ a (A ×ˢ s)).toReal`.
    
    
    
    Co-authored-by: Rémy Degenne <remydegenne@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -3200,6 +3200,7 @@ import Mathlib.Probability.Kernel.CondDistrib
 import Mathlib.Probability.Kernel.Condexp
 import Mathlib.Probability.Kernel.Disintegration
 import Mathlib.Probability.Kernel.Disintegration.CdfToKernel
+import Mathlib.Probability.Kernel.Disintegration.Density
 import Mathlib.Probability.Kernel.Disintegration.MeasurableStieltjes
 import Mathlib.Probability.Kernel.IntegralCompProd
 import Mathlib.Probability.Kernel.Invariance
