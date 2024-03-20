2690391cf5f46b19936822eb36d2ba9da83c7757
Yaël Dillies <yael.dillies@gmail.com>
2024-03-20 09:04:06 +0000

    chore: Move basic ordered field lemmas (#11503)
    These lemmas are needed to define the semifield structure on `NNRat`, hence I am repurposing `Algebra.Order.Field.Defs` from avoiding a timeout (which I believe was solved long ago) to avoiding to import random stuff in the definition of the semifield structure on `NNRat` (although this PR doesn't actually reduce imports there, it will be in a later PR).
    
    Reduce the diff of #11203

--- Mathlib/Order/RelSeries.lean
+++ Mathlib/Order/RelSeries.lean
@@ -3,12 +3,13 @@ Copyright (c) 2023 Jujian Zhang. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jujian Zhang, Fangming Li
 -/
+import Mathlib.Algebra.Order.WithZero
 import Mathlib.Data.Int.Basic
 import Mathlib.Data.List.Chain
 import Mathlib.Data.List.OfFn
 import Mathlib.Data.Rel
-import Mathlib.Tactic.Linarith
 import Mathlib.Tactic.Abel
+import Mathlib.Tactic.Linarith
 
 /-!
 # Series of a relation
--- Mathlib/Tactic/CancelDenoms/Core.lean
+++ Mathlib/Tactic/CancelDenoms/Core.lean
@@ -3,7 +3,8 @@ Copyright (c) 2020 Robert Y. Lewis. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Robert Y. Lewis
 -/
-import Mathlib.Algebra.Order.Field.Basic
+import Mathlib.Algebra.Field.Basic
+import Mathlib.Algebra.Order.Field.Defs
 import Mathlib.Data.Tree
 import Mathlib.Logic.Basic
 import Mathlib.Tactic.NormNum.Core
