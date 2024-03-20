caca27382e3f7446cc76cdb81463761487104244
Markus Himmel <markus@himmel-villmar.de>
2024-03-20 22:11:17 +0000

    feat: remove superseded `small_of_fintype` (#11326)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1715,7 +1715,6 @@ import Mathlib.Data.Fintype.Powerset
 import Mathlib.Data.Fintype.Prod
 import Mathlib.Data.Fintype.Quotient
 import Mathlib.Data.Fintype.Sigma
-import Mathlib.Data.Fintype.Small
 import Mathlib.Data.Fintype.Sort
 import Mathlib.Data.Fintype.Sum
 import Mathlib.Data.Fintype.Units
