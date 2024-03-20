327b452c1ced12fd6f904df445c1897b3385fbaf
Bolton Bailey <bolton.bailey@gmail.com>
2024-03-20 14:37:46 -0500

    add import

--- Mathlib.lean
+++ Mathlib.lean
@@ -1774,6 +1774,7 @@ import Mathlib.Data.List.Func
 import Mathlib.Data.List.GetD
 import Mathlib.Data.List.Indexes
 import Mathlib.Data.List.Infix
+import Mathlib.Data.List.InsertNth
 import Mathlib.Data.List.Intervals
 import Mathlib.Data.List.Join
 import Mathlib.Data.List.Lattice
