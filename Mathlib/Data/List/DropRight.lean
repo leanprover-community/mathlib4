3bdd6c4b7a0a82444ccb1c2d59a821c8e5944148
Bolton Bailey <bolton.bailey@gmail.com>
2024-03-20 17:47:09 -0500

    remove unneeded imports

--- Mathlib/Data/List/DropRight.lean
+++ Mathlib/Data/List/DropRight.lean
@@ -3,7 +3,6 @@ Copyright (c) 2022 Yakov Pechersky. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yakov Pechersky
 -/
-import Mathlib.Data.List.InsertNth
 import Mathlib.Data.List.Infix
 import Mathlib.Data.Nat.Order.Basic
 
