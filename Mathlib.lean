30dbd65228e496c82371dcdb66aa4a9ffc6a77dc
David Loeffler <d.loeffler.01@cantab.net>
2024-03-20 16:24:08 +0000

    feat(NumberTheory/LSeries): abstract functional equations (#11176)
    A general framework for proving analytic continuation & functional equations via Mellin transforms.

--- Mathlib.lean
+++ Mathlib.lean
@@ -2955,6 +2955,7 @@ import Mathlib.NumberTheory.Harmonic.Bounds
 import Mathlib.NumberTheory.Harmonic.Defs
 import Mathlib.NumberTheory.Harmonic.Int
 import Mathlib.NumberTheory.KummerDedekind
+import Mathlib.NumberTheory.LSeries.AbstractFuncEq
 import Mathlib.NumberTheory.LSeries.Basic
 import Mathlib.NumberTheory.LSeries.Convergence
 import Mathlib.NumberTheory.LSeries.Linearity
