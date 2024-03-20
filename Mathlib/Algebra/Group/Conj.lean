22fb2abba3b65af1faaf0a76c0e2d1e957ea3541
Yaël Dillies <yael.dillies@gmail.com>
2024-03-20 09:41:52 +0000

    chore: Rename `zpow_coe_nat` to `zpow_natCast` This is mostly just me discovering the power of `F2`

--- Mathlib/Algebra/Group/Conj.lean
+++ Mathlib/Algebra/Group/Conj.lean
@@ -113,7 +113,7 @@ theorem conj_pow {i : ℕ} {a b : α} : (a * b * a⁻¹) ^ i = a * b ^ i * a⁻�
 theorem conj_zpow {i : ℤ} {a b : α} : (a * b * a⁻¹) ^ i = a * b ^ i * a⁻¹ := by
   induction' i
   · change (a * b * a⁻¹) ^ (_ : ℤ) = a * b ^ (_ : ℤ) * a⁻¹
-    simp [zpow_coe_nat]
+    simp [zpow_natCast]
   · simp only [zpow_negSucc, conj_pow, mul_inv_rev, inv_inv]
     rw [mul_assoc]
 -- Porting note: Added `change`, `zpow_coe_nat`, and `rw`.
