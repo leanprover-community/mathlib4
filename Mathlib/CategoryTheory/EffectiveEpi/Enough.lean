44ccbe66f8b654798f627dd674ca1417b21c5fea
dagurtomas <dagurtomas@gmail.com>
2024-03-21 10:53:17 +0000

    docstrings

--- Mathlib/CategoryTheory/EffectiveEpi/Enough.lean
+++ Mathlib/CategoryTheory/EffectiveEpi/Enough.lean
@@ -20,12 +20,24 @@ variable {C D : Type*} [Category C] [Category D] (F : C ⥤ D)
 
 namespace Functor
 
+/--
+An effective presentation of an object `X` with respect to a functor `F` is the data of an effective
+epimorphism of the form `F.obj p ⟶ X`.
+-/
 structure EffectivePresentation (X : D) where
+  /-- The object of `C` giving the source of the effective epi -/
   p : C
+  /-- The morphism `F.obj p ⟶ X` -/
   f : F.obj p ⟶ X
+  /-- `f` is an effective epi -/
   effectiveEpi : EffectiveEpi f
 
+/--
+`D` has *effectively enough objects with respect to the functor `F` if every object has an
+effective presentation.
+-/
 class EffectivelyEnough : Prop where
+  /-- For every `X : D`, there exists an object `p` of `C` with an effective epi `F.obj p ⟶ X`. -/
   presentation : ∀ (X : D), Nonempty (F.EffectivePresentation X)
 
 variable [F.EffectivelyEnough]
@@ -47,13 +59,14 @@ noncomputable def π (X : D) : over F X ⟶ X :=
 instance (X : D) : EffectiveEpi (F.π X) :=
   (EffectivelyEnough.presentation X).some.effectiveEpi
 
-def isEquivalenceEffectivePresentation (e : C ≌ D) (X : D) :
+/-- An effective presentation of an object with respect to an equivalence of categories. -/
+def equivalenceEffectivePresentation (e : C ≌ D) (X : D) :
     EffectivePresentation e.functor X where
       p := e.inverse.obj X
       f := e.counit.app _
       effectiveEpi := inferInstance
 
 instance [IsEquivalence F] : EffectivelyEnough F where
-  presentation X := ⟨isEquivalenceEffectivePresentation F.asEquivalence X⟩
+  presentation X := ⟨equivalenceEffectivePresentation F.asEquivalence X⟩
 
 end Functor
