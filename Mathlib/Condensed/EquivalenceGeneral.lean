5ebc05df9af822df8297cf86da089afe2e0d5558
adamtopaz <github@adamtopaz.com>
2024-03-20 18:12:17 -0600

    reindex

--- Mathlib/Condensed/EquivalenceGeneral.lean
+++ Mathlib/Condensed/EquivalenceGeneral.lean
@@ -108,8 +108,8 @@ theorem coverDense.inducedTopology_Sieve_iff_EffectiveEpiFamily (X : C) (S : Sie
     have : ∀ a, EffectiveEpi (g₀ a) := inferInstance
     simp_rw [effectiveEpi_iff_effectiveEpiFamily] at this
     have : EffectiveEpiFamily _ (fun a ↦ g₀ a ≫ π a) := by
-      have := EffectiveEpiFamily.transitive_of_finite (β := fun a ↦ Unit) _ H₁ _ this
-      sorry
+      have := EffectiveEpiFamily.transitive_of_finite (β := fun _ ↦ Unit) _ H₁ _ this
+      exact EffectiveEpiFamily.reindex (e := Equiv.sigmaPUnit α) _ _ this
     refine ⟨Z , fun a ↦ Full.preimage (g₀ a ≫ π a), ?_, fun a ↦ (?_ : S.arrows (Full.preimage _))⟩
     · refine F.finite_effectiveEpiFamily_of_map _ _ ?_
       simpa using this
