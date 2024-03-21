36945a8bb86fcc4a5816b1c9da01f4375fd90fbc
Mitchell Lee <trivial171@gmail.com>
2024-03-21 02:09:36 +0000

    feat: the generators of a presented group generate the presented group (#11493)
    - Add `FreeGroup.closure_range_of`: the subgroup closure of the range of `FreeGroup.of : α → FreeGroup α` is `⊤`. (That is, the generators of a free group generate the free group.)
    - Add `PresentedGroup.closure_range_of`: the subgroup closure of the range of `PresentedGroup.of : α → PresentedGroup rels` is `⊤`. (That is, the generators of a presented group generate the presented group.)

--- Mathlib/GroupTheory/FreeGroup/Basic.lean
+++ Mathlib/GroupTheory/FreeGroup/Basic.lean
@@ -753,8 +753,12 @@ theorem ext_hom {G : Type*} [Group G] (f g : FreeGroup α →* G) (h : ∀ a, f 
 #align free_add_group.ext_hom FreeAddGroup.ext_hom
 
 @[to_additive]
+theorem lift_of_eq_id (α) : lift of = MonoidHom.id (FreeGroup α) :=
+  lift.apply_symm_apply (MonoidHom.id _)
+
+@[to_additive]
 theorem lift.of_eq (x : FreeGroup α) : lift FreeGroup.of x = x :=
-  DFunLike.congr_fun (lift.apply_symm_apply (MonoidHom.id _)) x
+  DFunLike.congr_fun (lift_of_eq_id α) x
 #align free_group.lift.of_eq FreeGroup.lift.of_eq
 #align free_add_group.lift.of_eq FreeAddGroup.lift.of_eq
 
@@ -776,6 +780,14 @@ theorem lift.range_eq_closure : (lift f).range = Subgroup.closure (Set.range f) 
   exact ⟨FreeGroup.of a, by simp only [lift.of]⟩
 #align free_group.lift.range_eq_closure FreeGroup.lift.range_eq_closure
 #align free_add_group.lift.range_eq_closure FreeAddGroup.lift.range_eq_closure
+
+/-- The generators of `FreeGroup α` generate `FreeGroup α`. That is, the subgroup closure of the
+set of generators equals `⊤`. -/
+@[to_additive (attr := simp)]
+theorem closure_range_of (α) :
+    Subgroup.closure (Set.range (FreeGroup.of : α → FreeGroup α)) = ⊤ := by
+  rw [← lift.range_eq_closure, lift_of_eq_id]
+  exact MonoidHom.range_top_of_surjective _ Function.surjective_id
 
 end lift
 
