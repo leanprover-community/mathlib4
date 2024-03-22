56589e75094aea7773cb831211c622a177ea8937
dagurtomas <dagurtomas@gmail.com>
2024-03-22 15:18:19 +0000

    unnecessary import

--- Mathlib/Topology/Connected/Separation.lean
+++ Mathlib/Topology/Connected/Separation.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Dagur Asgeirsson
 -/
 import Mathlib.Topology.Separation
-import Mathlib.Topology.Connected.TotallyDisconnected
 /-!
 
 # Separation and (dis)connectedness properties of topological spaces.
