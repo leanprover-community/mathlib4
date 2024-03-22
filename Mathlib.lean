122dc8fdb742c35cf11bf8bc98a56aef9572a04d
David Loeffler <d.loeffler.01@cantab.net>
2024-03-22 11:10:43 +0100

    feat(NumberTheory/LSeries): Mellin transforms as Dirichlet series

--- Mathlib.lean
+++ Mathlib.lean
@@ -2960,6 +2960,7 @@ import Mathlib.NumberTheory.LSeries.Basic
 import Mathlib.NumberTheory.LSeries.Convergence
 import Mathlib.NumberTheory.LSeries.Deriv
 import Mathlib.NumberTheory.LSeries.Linearity
+import Mathlib.NumberTheory.LSeries.MellinEqDirichlet
 import Mathlib.NumberTheory.LegendreSymbol.AddCharacter
 import Mathlib.NumberTheory.LegendreSymbol.Basic
 import Mathlib.NumberTheory.LegendreSymbol.GaussEisensteinLemmas
