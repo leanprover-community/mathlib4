3a6df84f0f1e476d9a49ee460f9c3c2724055edc
FordUniver <61389961+FordUniver@users.noreply.github.com>
2024-03-22 13:41:16 +0100

    fixed aligns and using deprecated alias

--- Mathlib/Data/Finset/Basic.lean
+++ Mathlib/Data/Finset/Basic.lean
@@ -1805,43 +1805,27 @@ theorem inter_right_idem (s t : Finset α) : s ∩ t ∩ t = s ∩ t := inf_righ
 
 theorem inter_union_distrib_left (s t u : Finset α) : s ∩ (t ∪ u) = s ∩ t ∪ s ∩ u :=
   inf_sup_left _ _ _
-#align finset.inter_union_distrib_left Finset.inter_union_distrib_left
+#align finset.inter_distrib_left Finset.inter_union_distrib_left
 
-/-- Deprecated, use `Finset.inter_union_distrib_left` instead. -/
-@[deprecated inter_union_distrib_left]
-theorem inter_distrib_left (s t u : Finset α) : s ∩ (t ∪ u) = s ∩ t ∪ s ∩ u :=
-  inf_sup_left _ _ _
-#align finset.inter_distrib_left Finset.inter_distrib_left
+@[deprecated] alias inter_distrib_left := inter_union_distrib_left
 
 theorem inter_union_distrib_right (s t u : Finset α) : s ∩ t ∪ u = (s ∪ u) ∩ (t ∪ u) :=
   sup_inf_right _ _ _
-#align finset.inter_union_distrib_right Finset.inter_union_distrib_right
+#align finset.union_distrib_right Finset.inter_union_distrib_right
 
-/-- Deprecated, use `Finset.inter_union_distrib_right` instead. -/
-@[deprecated inter_union_distrib_right]
-theorem union_distrib_right (s t u : Finset α) : s ∩ t ∪ u = (s ∪ u) ∩ (t ∪ u) :=
-  sup_inf_right _ _ _
-#align finset.union_distrib_right Finset.union_distrib_right
+@[deprecated] alias union_distrib_right := inter_union_distrib_right
 
 theorem union_inter_distrib_left (s t u : Finset α) : s ∪ t ∩ u = (s ∪ t) ∩ (s ∪ u) :=
   sup_inf_left _ _ _
-#align finset.union_inter_distrib_left Finset.union_inter_distrib_left
+#align finset.union_distrib_left Finset.union_inter_distrib_left
 
-/-- Deprecated, use `Finset.union_inter_distrib_left` instead. -/
-@[deprecated union_inter_distrib_left]
-theorem union_distrib_left (s t u : Finset α) : s ∪ t ∩ u = (s ∪ t) ∩ (s ∪ u) :=
-  sup_inf_left _ _ _
-#align finset.union_distrib_left Finset.union_distrib_left
+@[deprecated] alias union_distrib_left := union_inter_distrib_left
 
 theorem union_inter_distrib_right (s t u : Finset α) : (s ∪ t) ∩ u = s ∩ u ∪ t ∩ u :=
   inf_sup_right _ _ _
-#align finset.union_inter_distrib_right Finset.union_inter_distrib_right
+#align finset.inter_distrib_right Finset.union_inter_distrib_right
 
-/-- Deprecated, use `Finset.union_inter_distrib_right` instead. -/
-@[deprecated union_inter_distrib_right]
-theorem inter_distrib_right (s t u : Finset α) : (s ∪ t) ∩ u = s ∩ u ∪ t ∩ u :=
-  inf_sup_right _ _ _
-#align finset.inter_distrib_right Finset.inter_distrib_right
+@[deprecated] alias inter_distrib_right := union_inter_distrib_right
 
 theorem union_union_distrib_left (s t u : Finset α) : s ∪ (t ∪ u) = s ∪ t ∪ (s ∪ u) :=
   sup_sup_distrib_left _ _ _
