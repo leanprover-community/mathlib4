a747d74e6a76e529ba9e424ee53360d0186de24d
Dagur Tómas Ásgeirsson <dagurtomas@gmail.com>
2024-03-22 15:34:04 +0000

    Update Extensive.lean

--- Mathlib/CategoryTheory/EffectiveEpi/Extensive.lean
+++ Mathlib/CategoryTheory/EffectiveEpi/Extensive.lean
@@ -1,7 +1,19 @@
+/-
+Copyright (c) 2024 Dagur Asgeirsson. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Dagur Asgeirsson
+-/
 import Mathlib.CategoryTheory.EffectiveEpi.Preserves
 import Mathlib.CategoryTheory.EffectiveEpi.Coproduct
 import Mathlib.CategoryTheory.Extensive
 import Mathlib.CategoryTheory.Limits.Preserves.Finite
+/-!
+
+# Preserving and reflecting effective epis on extensive categories
+
+We prove that a functor between `FinitaryPreExtensive` categories preserves (resp. reflects) finite
+effective epi families if it preserves (resp. reflects) effective epis.
+-/
 
 namespace CategoryTheory
 
@@ -17,13 +29,21 @@ theorem effectiveEpi_desc_iff_effectiveEpiFamily {α : Type} [Finite α]
     fun _ ↦ inferInstance⟩
 
 variable {D : Type*} [Category D] [FinitaryPreExtensive D]
-variable (F : C ⥤ D) [F.ReflectsEffectiveEpis] [PreservesFiniteCoproducts F]
+variable (F : C ⥤ D) [PreservesFiniteCoproducts F]
 
-instance : F.ReflectsFiniteEffectiveEpiFamilies where
+instance [F.ReflectsEffectiveEpis] : F.ReflectsFiniteEffectiveEpiFamilies where
   reflects {α _ B} X π h := by
     have : Fintype α := Fintype.ofFinite _
     simp only [← effectiveEpi_desc_iff_effectiveEpiFamily]
     apply F.effectiveEpi_of_map
     convert (inferInstance :
       EffectiveEpi (inv (sigmaComparison F X) ≫ (Sigma.desc (fun a ↦ F.map (π a)))))
+    simp
+
+instance [F.PreservesEffectiveEpis] : F.PreservesFiniteEffectiveEpiFamilies where
+  preserves {α _ B} X π h := by
+    have : Fintype α := Fintype.ofFinite _
+    simp only [← effectiveEpi_desc_iff_effectiveEpiFamily]
+    convert (inferInstance :
+      EffectiveEpi ((sigmaComparison F X) ≫ (F.map (Sigma.desc π))))
     simp
