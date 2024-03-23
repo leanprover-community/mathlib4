bd21f911df0ce552655837858344aa4aee3482a3
Yaël Dillies <yael.dillies@gmail.com>
2024-03-23 16:16:15 +0100

    update Mathlib
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -3756,11 +3756,11 @@ import Mathlib.Topology.Algebra.Nonarchimedean.Basic
 import Mathlib.Topology.Algebra.OpenSubgroup
 import Mathlib.Topology.Algebra.Order.Archimedean
 import Mathlib.Topology.Algebra.Order.Compact
+import Mathlib.Topology.Algebra.Order.Field
 import Mathlib.Topology.Algebra.Order.Floor
+import Mathlib.Topology.Algebra.Order.Group
 import Mathlib.Topology.Algebra.Order.LiminfLimsup
 import Mathlib.Topology.Algebra.Order.Rolle
-import Mathlib.Topology.Algebra.Order.Group
-import Mathlib.Topology.Algebra.Order.Field
 import Mathlib.Topology.Algebra.Order.UpperLower
 import Mathlib.Topology.Algebra.Polynomial
 import Mathlib.Topology.Algebra.PontryaginDual
