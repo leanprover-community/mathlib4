c5114b43a3a9c34ab71162c00210d8947d58907a
Hongyu Ouyang <96765450+casavaca@users.noreply.github.com>
2024-03-24 15:00:59 -0700

    fixup: add 2 iff theorems

--- Mathlib/Data/Nat/Prime.lean
+++ Mathlib/Data/Nat/Prime.lean
@@ -472,6 +472,12 @@ theorem not_prime_of_dvd2 (n2 : 2 ≤ n) : (∃ m, m ∣ n ∧ 2 ≤ m ∧ m < n
   · exact ne_of_gt (lt_of_succ_le hm2)
   · exact ne_of_lt hm
 
+theorem exists_dvd_iff_not_prime {n : ℕ} (n2 : 2 ≤ n) : (¬Prime n) ↔ ∃ m, m ∣ n ∧ m ≠ 1 ∧ m ≠ n :=
+  ⟨exists_dvd_of_not_prime n2, not_prime_of_dvd n2⟩
+
+theorem exists_dvd_iff_not_prime2 {n : ℕ} (n2 : 2 ≤ n) : (¬Prime n) ↔ ∃ m, m ∣ n ∧ 2 ≤ m ∧ m < n :=
+  ⟨exists_dvd_of_not_prime2 n2, not_prime_of_dvd2 n2⟩
+
 theorem exists_prime_and_dvd {n : ℕ} (hn : n ≠ 1) : ∃ p, Prime p ∧ p ∣ n :=
   ⟨minFac n, minFac_prime hn, minFac_dvd _⟩
 #align nat.exists_prime_and_dvd Nat.exists_prime_and_dvd
