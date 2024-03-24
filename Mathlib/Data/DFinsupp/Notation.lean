98e313acff09dec6f49b7fc8bb7fe2d243958d7c
Eric Wieser <wieser.eric@gmail.com>
2024-03-24 20:59:58 +0000

    lintfix

--- Mathlib/Data/DFinsupp/Notation.lean
+++ Mathlib/Data/DFinsupp/Notation.lean
@@ -27,21 +27,21 @@ attribute [term_parser] Finsupp.stxSingle₀ Finsupp.stxUpdate₀
 
 @[term_elab Finsupp.stxSingle₀]
 def elabSingle₀ : Elab.Term.TermElab
-| `(term| single₀ $i $x) => fun ty? => do
+  | `(term| single₀ $i $x) => fun ty? => do
     Elab.Term.tryPostponeIfNoneOrMVar ty?
     let .some ty := ty? | Elab.throwUnsupportedSyntax
     let_expr DFinsupp _ _ _ := ← Meta.withReducible (Meta.whnf ty) | Elab.throwUnsupportedSyntax
     Elab.Term.elabTerm (← `(DFinsupp.single $i $x)) ty?
-| _ => fun _ => Elab.throwUnsupportedSyntax
+  | _ => fun _ => Elab.throwUnsupportedSyntax
 
 @[term_elab Finsupp.stxUpdate₀]
 def elabUpdate₀ : Elab.Term.TermElab
-| `(term| update₀ $f $i $x) => fun ty? => do
+  | `(term| update₀ $f $i $x) => fun ty? => do
     Elab.Term.tryPostponeIfNoneOrMVar ty?
     let .some ty := ty? | Elab.throwUnsupportedSyntax
     let_expr DFinsupp _ _ _ := ← Meta.withReducible (Meta.whnf ty) | Elab.throwUnsupportedSyntax
     Elab.Term.elabTerm (← `(DFinsupp.update $i $f $x)) ty?
-| _ => fun _ => Elab.throwUnsupportedSyntax
+  | _ => fun _ => Elab.throwUnsupportedSyntax
 
 /-- Unexpander for the `fun₀ | i => x` notation. -/
 @[app_unexpander Finsupp.single]
--- Mathlib/Data/Finsupp/Notation.lean
+++ Mathlib/Data/Finsupp/Notation.lean
@@ -37,13 +37,13 @@ local syntax:lead (name := stxUpdate₀) "update₀" term:arg term:arg term:arg 
 
 @[term_elab stxSingle₀]
 def elabSingle₀ : Elab.Term.TermElab
-| `(term| single₀ $i $x) => fun ty => do Elab.Term.elabTerm (← `(Finsupp.single $i $x)) ty
-| _ => fun _ => Elab.throwUnsupportedSyntax
+  | `(term| single₀ $i $x) => fun ty => do Elab.Term.elabTerm (← `(Finsupp.single $i $x)) ty
+  | _ => fun _ => Elab.throwUnsupportedSyntax
 
 @[term_elab stxUpdate₀]
 def elabUpdate₀ : Elab.Term.TermElab
-| `(term| update₀ $f $i $x) => fun ty => do Elab.Term.elabTerm (← `(Finsupp.update $f $i $x)) ty
-| _ => fun _ => Elab.throwUnsupportedSyntax
+  | `(term| update₀ $f $i $x) => fun ty => do Elab.Term.elabTerm (← `(Finsupp.update $f $i $x)) ty
+  | _ => fun _ => Elab.throwUnsupportedSyntax
 
 macro_rules
   | `(term| fun₀ $x:matchAlt*) => do
