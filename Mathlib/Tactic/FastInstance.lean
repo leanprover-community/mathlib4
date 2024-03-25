5a252c4060fa85449b6a47a5bf0d17595468b696
Eric Wieser <wieser.eric@gmail.com>
2024-03-25 18:02:13 +0000

    typo

--- Mathlib/Tactic/FastInstance.lean
+++ Mathlib/Tactic/FastInstance.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2024 Eric Wieser. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Eric Wieserf
+Authors: Eric Wieser
 -/
 
 import Lean.Elab.ComputedFields
