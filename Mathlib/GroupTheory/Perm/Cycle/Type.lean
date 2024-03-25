2f91116c75ce3603e51535e7ce76c4991bb790aa
Yaël Dillies <yael.dillies@gmail.com>
2024-03-25 14:29:05 +0000

    move(Combinatorics/Enumerative): Create folder Move `Catalan`, `Composition`, `DoubleCounting`, `Partition` to a new folder `Combinatorics.Enumerative`.

--- Mathlib/GroupTheory/Perm/Cycle/Type.lean
+++ Mathlib/GroupTheory/Perm/Cycle/Type.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Thomas Browning
 -/
 import Mathlib.Algebra.GCDMonoid.Multiset
-import Mathlib.Combinatorics.Partition
+import Mathlib.Combinatorics.Enumerative.Partition
 import Mathlib.Data.List.Rotate
 import Mathlib.GroupTheory.Perm.Cycle.Factors
 import Mathlib.GroupTheory.Perm.Closure
