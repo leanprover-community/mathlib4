6baf99e688c688a98d5ffb973559193e7d21203e
Christopher Hoskin <christopher.hoskin@gmail.com>
2024-03-25 06:00:11 +0000

    inferInstance

--- Mathlib/LinearAlgebra/BilinearForm/Basic.lean
+++ Mathlib/LinearAlgebra/BilinearForm/Basic.lean
@@ -134,7 +134,7 @@ theorem ext_iff : B = D ↔ ∀ x y, B x y = D x y :=
   ⟨congr_fun, ext₂⟩
 #align bilin_form.ext_iff LinearMap.BilinForm.ext_iff
 
-instance : Zero (BilinForm R M) := LinearMap.instZeroLinearMap
+instance : Zero (BilinForm R M) := inferInstance
 
 theorem coe_zero : ⇑(0 : BilinForm R M) = 0 :=
   rfl
@@ -147,7 +147,7 @@ theorem zero_apply (x y : M) : (0 : BilinForm R M) x y = 0 :=
 
 variable (B D B₁ D₁)
 
-instance : Add (BilinForm R M) := LinearMap.instAddLinearMap
+instance : Add (BilinForm R M) := inferInstance
 
 theorem coe_add : ⇑(B + D) = B + D :=
   rfl
@@ -163,29 +163,26 @@ multiplication.
 
 When `R` itself is commutative, this provides an `R`-action via `Algebra.id`. -/
 instance {α} [Monoid α] [DistribMulAction α R] [SMulCommClass R α R] : SMul α (BilinForm R M) :=
-  LinearMap.instSMulLinearMap
+  inferInstance
 
 #noalign bilin_form.coe_smul
 #noalign bilin_form.smul_apply
 
 instance {α β} [Monoid α] [Monoid β] [DistribMulAction α R] [DistribMulAction β R]
     [SMulCommClass R α R] [SMulCommClass R β R] [SMulCommClass α β R] :
-    SMulCommClass α β (BilinForm R M) :=
-  LinearMap.instSMulCommClassLinearMapInstSMulLinearMapInstSMulLinearMap
+    SMulCommClass α β (BilinForm R M) := inferInstance
 
 instance {α β} [Monoid α] [Monoid β] [SMul α β] [DistribMulAction α R] [DistribMulAction β R]
     [SMulCommClass R α R] [SMulCommClass R β R] [IsScalarTower α β R] :
-    IsScalarTower α β (BilinForm R M) :=
-  LinearMap.instIsScalarTowerLinearMapInstSMulLinearMapInstSMulLinearMap
+    IsScalarTower α β (BilinForm R M) := inferInstance
 
 instance {α} [Monoid α] [DistribMulAction α R] [DistribMulAction αᵐᵒᵖ R]
     [SMulCommClass R α R] [IsCentralScalar α R] :
-    IsCentralScalar α (BilinForm R M) :=
-  LinearMap.instIsCentralScalarLinearMapInstSMulLinearMapMulOppositeMonoid
+    IsCentralScalar α (BilinForm R M) := inferInstance
 
-instance : AddCommMonoid (BilinForm R M) := by infer_instance
+instance : AddCommMonoid (BilinForm R M) := inferInstance
 
-instance : Neg (BilinForm R₁ M₁) := LinearMap.instNegLinearMapToAddCommMonoid
+instance : Neg (BilinForm R₁ M₁) := inferInstance
 
 theorem coe_neg : ⇑(-B₁) = -B₁ :=
   rfl
@@ -196,7 +193,7 @@ theorem neg_apply (x y : M₁) : (-B₁) x y = -B₁ x y :=
   rfl
 #align bilin_form.neg_apply LinearMap.BilinForm.neg_apply
 
-instance : Sub (BilinForm R₁ M₁) := LinearMap.instSubLinearMapToAddCommMonoid
+instance : Sub (BilinForm R₁ M₁) := inferInstance
 
 theorem coe_sub : ⇑(B₁ - D₁) = B₁ - D₁ :=
   rfl
@@ -207,10 +204,9 @@ theorem sub_apply (x y : M₁) : (B₁ - D₁) x y = B₁ x y - D₁ x y :=
   rfl
 #align bilin_form.sub_apply LinearMap.BilinForm.sub_apply
 
-instance : AddCommGroup (BilinForm R₁ M₁) := by infer_instance
+instance : AddCommGroup (BilinForm R₁ M₁) := inferInstance
 
-instance : Inhabited (BilinForm R M) :=
-  ⟨0⟩
+instance : Inhabited (BilinForm R M) := inferInstance
 
 /-- `coeFn` as an `AddMonoidHom` -/
 def coeFnAddMonoidHom : BilinForm R M →+ M → M → R where
@@ -220,11 +216,10 @@ def coeFnAddMonoidHom : BilinForm R M →+ M → M → R where
 #align bilin_form.coe_fn_add_monoid_hom LinearMap.BilinForm.coeFnAddMonoidHom
 
 instance {α} [Monoid α] [DistribMulAction α R] [SMulCommClass R α R] :
-    DistribMulAction α (BilinForm R M) :=
-  LinearMap.instDistribMulActionLinearMapToAddMonoidAddCommMonoid
+    DistribMulAction α (BilinForm R M) := inferInstance
 
 instance {α} [CommSemiring α] [Module α R] [SMulCommClass R α R] : Module α (BilinForm R M) :=
-  by infer_instance
+  inferInstance
 
 section flip
 
