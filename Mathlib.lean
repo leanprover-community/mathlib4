01a87c1d889b665b83e3816cf6c97ff02a6c45ab
Dagur Tómas Ásgeirsson <dagurtomas@gmail.com>
2024-03-26 13:32:22 +0000

    feat(CategoryTheory/Sites): a functor into a precoherent category satisfying `Functor.EffectivelyEnough` is cover dense (#11686)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1379,6 +1379,7 @@ import Mathlib.CategoryTheory.Sites.Coherent.Basic
 import Mathlib.CategoryTheory.Sites.Coherent.CoherentSheaves
 import Mathlib.CategoryTheory.Sites.Coherent.CoherentTopology
 import Mathlib.CategoryTheory.Sites.Coherent.Comparison
+import Mathlib.CategoryTheory.Sites.Coherent.CoverDense
 import Mathlib.CategoryTheory.Sites.Coherent.Equivalence
 import Mathlib.CategoryTheory.Sites.Coherent.ExtensiveSheaves
 import Mathlib.CategoryTheory.Sites.Coherent.RegularSheaves
