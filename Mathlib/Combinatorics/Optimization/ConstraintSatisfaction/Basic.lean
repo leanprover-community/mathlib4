c180ebdf81ffcaf1739ca242ec8e76b83536b757
madvorak <martin.dvorak@matfyz.cz>
2024-03-26 14:45:23 +0100

    shaken

--- Mathlib/Combinatorics/Optimization/ConstraintSatisfaction/Basic.lean
+++ Mathlib/Combinatorics/Optimization/ConstraintSatisfaction/Basic.lean
@@ -3,7 +3,8 @@ Copyright (c) 2023 Martin Dvorak. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Martin Dvorak
 -/
-import Mathlib.Algebra.BigOperators.Fin
+import Mathlib.Algebra.BigOperators.Basic
+import Mathlib.Data.Fin.VecNotation
 
 /-!
 
--- Mathlib/Combinatorics/Optimization/ConstraintSatisfaction/Hardness.lean
+++ Mathlib/Combinatorics/Optimization/ConstraintSatisfaction/Hardness.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Martin Dvorak
 -/
 import Mathlib.Combinatorics.Optimization.ConstraintSatisfaction.Expressibility
-import Mathlib.Data.Fin.VecNotation
 
 /-!
 
