56511da378c955f4e1d4638856d30f698c3e3c36
Yury G. Kudryashov <urkud@urkud.name>
2024-03-26 09:27:17 -0500

    Update

--- Mathlib/Data/List/Enum.lean
+++ Mathlib/Data/List/Enum.lean
@@ -1,10 +1,17 @@
+/-
+Copyright (c) 2017 Mario Carneiro. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Mario Carneiro, Yakov Pechersky, Eric Wieser
+-/
 import Mathlib.Data.List.Basic
+
+/-!
+# Properties of `List.enum`
+-/
 
 namespace List
 
 variable {α β : Type*}
-
-/-! ### enum -/
 
 #align list.length_enum_from List.enumFrom_length
 #align list.length_enum List.enum_length
