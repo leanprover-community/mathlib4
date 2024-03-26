30850dba6554df47eeb9a8aafae445d585f49e73
Joseph Myers <jsm@polyomino.org.uk>
2024-03-26 09:53:37 +0000

    feat: `Pairwise` and composition with an equiv (#10706)
    Add a lemma about `Pairwise` being preserved by composing the arguments of the pairwise relation with an equiv.
    
    ```lean
    lemma EquivLike.pairwise_comp_iff {X : Type*} {Y : Type*} {F} [EquivLike F Y X]
        (f : F) (p : X → X → Prop) : Pairwise (fun y z ↦ p (f y) (f z)) ↔ Pairwise p := by
    ```
    
    This depends on `Mathlib.Data.FunLike.Equiv` and
    `Mathlib.Logic.Pairwise`, neither of which imports the other, so to avoid increasing the imports of very basic files I put it in its own new file, but feel free to suggest a better location of this lemma (and to golf the proof).
    
    From AperiodicMonotilesLean.

--- Mathlib.lean
+++ Mathlib.lean
@@ -2707,6 +2707,7 @@ import Mathlib.Logic.Equiv.Functor
 import Mathlib.Logic.Equiv.List
 import Mathlib.Logic.Equiv.Nat
 import Mathlib.Logic.Equiv.Option
+import Mathlib.Logic.Equiv.Pairwise
 import Mathlib.Logic.Equiv.PartialEquiv
 import Mathlib.Logic.Equiv.Set
 import Mathlib.Logic.Equiv.TransferInstance
