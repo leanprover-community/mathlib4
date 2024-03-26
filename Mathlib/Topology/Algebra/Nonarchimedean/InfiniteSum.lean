4368c6479b80a7e2ef0038607ff673af5088c439
Mitchell Lee <trivial171@gmail.com>
2024-03-26 03:50:58 -0400

    Correct copyright date

--- Mathlib/Topology/Algebra/Nonarchimedean/InfiniteSum.lean
+++ Mathlib/Topology/Algebra/Nonarchimedean/InfiniteSum.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2021 Mitchell Lee. All rights reserved.
+Copyright (c) 2024 Mitchell Lee. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mitchell Lee
 -/
