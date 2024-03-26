d791818a7b4231d62c9b2f369ffca3d4a55c255e
Yury G. Kudryashov <urkud@urkud.name>
2024-03-26 10:54:30 -0500

    Shake imports

--- Mathlib/Data/List/Enum.lean
+++ Mathlib/Data/List/Enum.lean
@@ -3,7 +3,9 @@ Copyright (c) 2017 Mario Carneiro. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro, Yakov Pechersky, Eric Wieser
 -/
-import Mathlib.Data.List.Basic
+import Mathlib.Data.Nat.Basic
+import Mathlib.Init.Data.List.Basic
+import Mathlib.Init.Data.Nat.Lemmas
 
 /-!
 # Properties of `List.enum`
