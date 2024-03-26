3bb0eeb74adbcb7fc555d594117d8797370f4928
Moritz Firsching <firsching@google.com>
2024-03-26 14:58:56 +0100

    Update Mathlib/Data/Nat/Digits.lean
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- Mathlib/Data/Nat/Digits.lean
+++ Mathlib/Data/Nat/Digits.lean
@@ -640,7 +640,7 @@ theorem ofDigits_mod (b k : ℕ) (L : List ℕ) : ofDigits b L % k = ofDigits (b
   ofDigits_modEq b k L
 #align nat.of_digits_mod Nat.ofDigits_mod
 
-theorem ofDigits_mod_eq_head! (b : ℕ) (l : List ℕ) : (Nat.ofDigits b l) % b = l.head! %b := by
+theorem ofDigits_mod_eq_head! (b : ℕ) (l : List ℕ) : ofDigits b l % b = l.head! %b := by
   induction l <;> simp [Nat.ofDigits, Int.ModEq]
 
 theorem digits_head! {b n : ℕ} (h : b ≠ 1) : (Nat.digits b n).head! = n % b := by
