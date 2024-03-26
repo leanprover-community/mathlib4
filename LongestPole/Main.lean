cfb8663616a08f64b81e17f7ca722bba555fac2a
Bolton Bailey <bolton.bailey@gmail.com>
2024-03-26 02:12:36 -0500

    fix import

--- LongestPole/Main.lean
+++ LongestPole/Main.lean
@@ -3,7 +3,7 @@ Copyright (c) 2023 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison
 -/
-import Mathlib.Util.Imports
+import ImportGraph
 import Mathlib.Data.String.Defs
 import Std.Lean.Util.Path
 import Cli
