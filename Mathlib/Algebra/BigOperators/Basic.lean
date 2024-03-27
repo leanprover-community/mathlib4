c34c8c38f1dd03bf5a30145dcad6216f535ff7a6
Yaël Dillies <yael.dillies@gmail.com>
2024-03-27 07:50:46 +0000

    fix Algebra.BigOperators.Basic

--- Mathlib/Algebra/BigOperators/Basic.lean
+++ Mathlib/Algebra/BigOperators/Basic.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl
 -/
 import Mathlib.Algebra.BigOperators.Multiset.Lemmas
+import Mathlib.Algebra.BigOperators.Multiset.Order
 import Mathlib.Algebra.Function.Indicator
 import Mathlib.Algebra.Ring.Opposite
 import Mathlib.Data.Finset.Powerset
