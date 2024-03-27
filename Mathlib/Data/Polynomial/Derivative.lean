dd2abff723838a2d772d3a6c8fcccfa8bd95667d
Thomas Browning <tb65536@uw.edu>
2024-03-27 09:30:56 +0000

    refactor(Algebra/GroupPower/IterateHom): Use `HomClass` to generalize `map_iterate` statements (#11266)
    This PR uses the `HomClass` architecture to generalize the `map_iterate` statements in `Algebra/GroupPower/IterateHom.lean`.

--- Mathlib/Data/Polynomial/Derivative.lean
+++ Mathlib/Data/Polynomial/Derivative.lean
@@ -78,11 +78,8 @@ theorem derivative_zero : derivative (0 : R[X]) = 0 :=
   derivative.map_zero
 #align polynomial.derivative_zero Polynomial.derivative_zero
 
-@[simp]
-theorem iterate_derivative_zero {k : ℕ} : derivative^[k] (0 : R[X]) = 0 := by
-  induction' k with k ih
-  · simp
-  · simp [ih]
+theorem iterate_derivative_zero {k : ℕ} : derivative^[k] (0 : R[X]) = 0 :=
+  iterate_map_zero derivative k
 #align polynomial.iterate_derivative_zero Polynomial.iterate_derivative_zero
 
 @[simp]
@@ -595,9 +592,8 @@ theorem derivative_neg (f : R[X]) : derivative (-f) = -derivative f :=
   LinearMap.map_neg derivative f
 #align polynomial.derivative_neg Polynomial.derivative_neg
 
-@[simp]
 theorem iterate_derivative_neg {f : R[X]} {k : ℕ} : derivative^[k] (-f) = -derivative^[k] f :=
-  (@derivative R _).toAddMonoidHom.iterate_map_neg _ _
+  iterate_map_neg derivative k f
 #align polynomial.iterate_derivative_neg Polynomial.iterate_derivative_neg
 
 -- Porting note (#10618): removed `simp`: `simp` can prove it.
@@ -611,10 +607,9 @@ theorem derivative_X_sub_C (c : R) : derivative (X - C c) = 1 := by
 set_option linter.uppercaseLean3 false in
 #align polynomial.derivative_X_sub_C Polynomial.derivative_X_sub_C
 
-@[simp]
 theorem iterate_derivative_sub {k : ℕ} {f g : R[X]} :
-    derivative^[k] (f - g) = derivative^[k] f - derivative^[k] g := by
-  induction' k with k ih generalizing f g <;> simp [*]
+    derivative^[k] (f - g) = derivative^[k] f - derivative^[k] g :=
+  iterate_map_sub derivative k f g
 #align polynomial.iterate_derivative_sub Polynomial.iterate_derivative_sub
 
 @[simp]
