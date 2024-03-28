24be15cb0d243022c946d09ae89a9aceea4cabc3
Joël Riou <joel.riou@universite-paris-saclay.fr>
2024-03-28 10:10:47 +0100

    feat(CategoryTheory): more lemmas for the left calculus of fractions

--- Mathlib.lean
+++ Mathlib.lean
@@ -1265,6 +1265,7 @@ import Mathlib.CategoryTheory.Linear.LinearFunctor
 import Mathlib.CategoryTheory.Linear.Yoneda
 import Mathlib.CategoryTheory.Localization.Adjunction
 import Mathlib.CategoryTheory.Localization.CalculusOfFractions
+import Mathlib.CategoryTheory.Localization.CalculusOfFractions.Fractions
 import Mathlib.CategoryTheory.Localization.Composition
 import Mathlib.CategoryTheory.Localization.Construction
 import Mathlib.CategoryTheory.Localization.Equivalence
