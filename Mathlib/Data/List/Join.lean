0a4709f50418a87bcb2b67950b623b677cb3fc88
Yaël Dillies <yael.dillies@gmail.com>
2024-03-28 14:29:40 +0000

    shake

--- Mathlib/Data/List/Join.lean
+++ Mathlib/Data/List/Join.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Sébastien Gouëzel, Floris van Doorn, Mario Carneiro, Martin Dvorak
 -/
 import Mathlib.Data.List.BigOperators.Basic
-import Mathlib.Data.List.Count
 
 #align_import data.list.join from "leanprover-community/mathlib"@"18a5306c091183ac90884daa9373fa3b178e8607"
 
