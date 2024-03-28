827c4a27db2aa4a4f6c811d2bb73ace81256333c
Yaël Dillies <yael.dillies@gmail.com>
2024-03-28 17:09:50 +0000

    fix imports

--- Mathlib/Algebra/BigOperators/List/Order.lean
+++ Mathlib/Algebra/BigOperators/List/Order.lean
@@ -3,9 +3,9 @@ Copyright (c) 2021 Yakov Pechersky. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yakov Pechersky
 -/
+import Mathlib.Algebra.BigOperators.List.Basic
 import Mathlib.Algebra.Order.Monoid.OrderDual
 import Mathlib.Algebra.Order.Ring.Canonical
-import Mathlib.Algebra.BigOperators.List.Order
 
 /-!
 # Order properties of big operators
--- Mathlib/Algebra/BigOperators/Multiset/Basic.lean
+++ Mathlib/Algebra/BigOperators/Multiset/Basic.lean
@@ -3,7 +3,7 @@ Copyright (c) 2015 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro
 -/
-import Mathlib.Data.List.BigOperators.Basic
+import Mathlib.Algebra.BigOperators.List.Basic
 import Mathlib.Algebra.Divisibility.Basic
 import Mathlib.Algebra.Group.Hom.Basic
 import Mathlib.Algebra.GroupPower.Hom
