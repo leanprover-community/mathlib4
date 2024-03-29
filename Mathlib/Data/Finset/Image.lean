d7eb9b89d6d19b3dd3937dbd54600c8b3093bc59
Yaël Dillies <yael.dillies@gmail.com>
2024-03-29 15:59:37 +0000

    chore(Data/Finset/Basic): Depend on less order theory (#11732)
    Move `Finset.biUnion` and `Finset.disjiUnion` to a new file so that `Data.Finset.Basic` doesn't depend on that much order theory.

--- Mathlib/Data/Finset/Image.lean
+++ Mathlib/Data/Finset/Image.lean
@@ -5,7 +5,7 @@ Authors: Leonardo de Moura, Jeremy Avigad, Minchao Wu, Mario Carneiro
 -/
 import Mathlib.Algebra.Group.Embedding
 import Mathlib.Data.Fin.Basic
-import Mathlib.Data.Finset.Basic
+import Mathlib.Data.Finset.Union
 import Mathlib.Data.Int.Order.Basic
 
 #align_import data.finset.image from "leanprover-community/mathlib"@"65a1391a0106c9204fe45bc73a039f056558cb83"
