06127b65628a2b84020cddb951378ce1a850ccdc
Vincent Guirardel <vincent.guirardel@univ-rennes1.fr>
2024-03-29 08:26:39 +0100

    added separation theorem, litlle cleaning

--- Mathlib/Topology/Algebra/ProperAction.lean
+++ Mathlib/Topology/Algebra/ProperAction.lean
@@ -69,7 +69,8 @@ lemma isProperMap_smul_pair (G X : Type*) [Group G] [MulAction G X]
 open Filter Topology
 
 variable {G X Y Z W : Type*} [Group G] [MulAction G X] [MulAction G Y]
-variable [TopologicalSpace G] [TopologicalSpace X] [TopologicalSpace Y] [TopologicalSpace Z] [TopologicalSpace W]
+variable [TopologicalSpace G] [TopologicalSpace X] [TopologicalSpace Y]
+variable [TopologicalSpace Z] [TopologicalSpace W]
 
 @[to_additive]
 instance continuousSmul_of_properSMul [ProperSMul G X] : ContinuousSMul G X where
@@ -78,9 +79,51 @@ instance continuousSmul_of_properSMul [ProperSMul G X] : ContinuousSMul G X wher
 
 
 
-
+/-
 theorem properSMul_iff_continuousSMul_ultrafilter_tendsto_t2 [T2Space X]:
-ProperSMul G X ↔ ContinuousSMul G X ∧ (∀ ℱ : Ultrafilter (G × X), ∀ x₁ x₂ : X,
+    ProperSMul G X ↔ ContinuousSMul G X ∧ (∀ ℱ : Ultrafilter (G × X), ∀ x₁ x₂ : X,
     Tendsto (fun gx ↦ ⟨gx.1 • gx.2, gx.2⟩ : G × X → X × X) ℱ (𝓝 (x₁, x₂)) →
     ∃ g : G, Tendsto Prod.fst (ℱ : Filter (G × X)) (𝓝 g)) := by
   sorry
+
+
+-/
+
+/-- If G acts properly on X, then the quotient space is Hausdorff (T2) -/
+theorem t2Space_of_ProperSMul (hproper:ProperSMul G X) :
+  T2Space (Quotient (MulAction.orbitRel G X)) := by
+  rw [t2_iff_isClosed_diagonal] -- T2 if the diagonal is closed
+  set R := MulAction.orbitRel G X -- the orbit relation
+  set XmodG := Quotient R -- the quotient
+  set π : X → XmodG := Quotient.mk' -- the projection
+  have hpiopen: IsOpenMap π := -- the projection is open
+    isOpenMap_quotient_mk'_mul
+  have picont : Continuous π := continuous_quotient_mk' -- π is continuous
+  have pisurj : Function.Surjective π := by apply surjective_quotient_mk' -- π is surjective
+  set pipi := Prod.map π π
+  have pipiopen : IsOpenMap pipi := IsOpenMap.prod hpiopen hpiopen -- π × π open
+  have pipisurj : (Function.Surjective (pipi) ) :=  -- π × π surj
+    Function.Surjective.Prod_map pisurj pisurj
+  have pipipquotient := -- π × π is q QuotientMap because open, continuous and surj
+    IsOpenMap.to_quotientMap pipiopen (Continuous.prod_map picont picont) pipisurj
+  rw [<-QuotientMap.isClosed_preimage pipipquotient] -- closed iff preimage closed
+  set gr' := pipi ⁻¹' Set.diagonal (Quotient R) -- preimage of the diag
+  set m : G × X → X × X := fun (g,x) => (g • x, x)
+  set gr := Set.range m -- graph of the orbit relation
+  have r_eq_r' : gr' = gr := by -- the preimage of the diag is the graph of the relation
+    ext ⟨x,y⟩
+    conv_lhs => -- we work only on the left hand side for now
+      rw [Set.mem_preimage]
+      rw [Set.mem_diagonal_iff]
+      change (π x = π y)  --↔ (x, y) ∈ gr
+      rw [Quotient.eq']
+      change ((MulAction.orbitRel G X).Rel x y)-- ↔ (x, y) ∈ gr
+      rw [MulAction.orbitRel_apply]
+      rw [MulAction.orbit]
+      change (∃ g : G, g • y = x)
+    conv_rhs =>
+      rw [Set.mem_range]
+      simp
+    simp [m]
+  rw [r_eq_r']
+  exact hproper.isProperMap_smul_pair'.isClosedMap.closed_range
