16e57dec4beedb8ee0b6d064f3cdfcc9fe50bac3
Yaël Dillies <yael.dillies@gmail.com>
2024-03-29 12:54:41 +0000

    fix

--- Mathlib/Algebra/Ring/Aut.lean
+++ Mathlib/Algebra/Ring/Aut.lean
@@ -45,19 +45,14 @@ variable (R : Type*) [Mul R] [Add R]
 `fun g h => RingEquiv.trans h g`.
 This means that multiplication agrees with composition, `(g*h)(x) = g (h x)`.
 -/
-instance : Group (RingAut R) :=
-  { mul := fun g h => RingEquiv.trans h g
-    one := RingEquiv.refl R
-    inv := RingEquiv.symm
-    div := _
-    npow := @npowRec _ ⟨RingEquiv.refl R⟩ ⟨fun g h => RingEquiv.trans h g⟩
-    zpow :=
-      @zpowRec _ ⟨RingEquiv.refl R⟩ ⟨fun g h => RingEquiv.trans h g⟩
-        ⟨RingEquiv.symm⟩
-    mul_assoc := by intros; rfl
-    one_mul := by intros; rfl
-    mul_one := by intros; rfl
-    mul_left_inv := by intros; ext; apply Equiv.left_inv }
+instance : Group (RingAut R) where
+  mul g h := RingEquiv.trans h g
+  one := RingEquiv.refl R
+  inv := RingEquiv.symm
+  mul_assoc := by intros; rfl
+  one_mul := by intros; rfl
+  mul_one := by intros; rfl
+  mul_left_inv := RingEquiv.self_trans_symm
 /- Porting note: was by
   refine_struct
     { mul := fun g h => RingEquiv.trans h g
