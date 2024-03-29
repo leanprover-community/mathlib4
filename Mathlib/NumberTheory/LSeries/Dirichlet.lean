ca1c1895172156922fb9b14110bc644f9adc5ef3
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2024-03-29 11:04:37 +0100

    sort imports

--- Mathlib/NumberTheory/LSeries/Dirichlet.lean
+++ Mathlib/NumberTheory/LSeries/Dirichlet.lean
@@ -3,12 +3,12 @@ Copyright (c) 2024 Michael Stoll. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Michael Stoll
 -/
-import Mathlib.NumberTheory.SumPrimeReciprocals
-import Mathlib.NumberTheory.VonMangoldt
-import Mathlib.NumberTheory.ZetaFunction
 import Mathlib.NumberTheory.DirichletCharacter.Bounds
 import Mathlib.NumberTheory.LSeries.Convolution
 import Mathlib.NumberTheory.LSeries.Deriv
+import Mathlib.NumberTheory.SumPrimeReciprocals
+import Mathlib.NumberTheory.VonMangoldt
+import Mathlib.NumberTheory.ZetaFunction
 
 /-!
 # L-series of Dirichlet characters and arithmetic functions
