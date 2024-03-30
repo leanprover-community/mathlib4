3aeb10e02c636e3e071d386b0847cafb57cd9001
Damiano Testa <adomani@gmail.com>
2024-03-30 09:02:06 +0100

    Add Thomas' recusion into `in` and tests

--- Mathlib/Tactic/HashCommandLinter.lean
+++ Mathlib/Tactic/HashCommandLinter.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Damiano Testa
 -/
 
+import Lean.Linter.Util
 import Std.Lean.Command
 
 /-!
@@ -30,12 +31,27 @@ open Lean Elab
 /-- Gets the value of the `linter.hashCommand` option. -/
 def getLinterHash (o : Options) : Bool := Linter.getLinterValue linter.hashCommand o
 
+open Command in
+/-- Exactly like `withSetOptionIn`, but recursively discards any other use of `in`.
+Intended to be used in the hashCommand linter,
+where we don't need to handle non-`set_option` `in` commands. -/
+private partial def withSetOptionIn' (cmd : CommandElab) : CommandElab := fun stx => do
+  if stx.getKind == ``Lean.Parser.Command.in then
+    if stx[0].getKind == ``Lean.Parser.Command.set_option then
+      let opts ← Elab.elabSetOption stx[0][1] stx[0][2]
+      Command.withScope (fun scope => { scope with opts }) do
+        withSetOptionIn' cmd stx[2]
+    else
+      withSetOptionIn' cmd stx[2]
+  else
+    cmd stx
+
 /-- `whitelist` is the array of `#`-commands that are allowed in 'Mathlib'. -/
 private abbrev whitelist : Array String := #["#align", "#align_import", "#noalign"]
 
 /-- Checks that no command beginning with `#` is present in 'Mathlib', except for the ones in
 `whitelist`. -/
-def hashCommandLinter : Linter where run := withSetOptionIn fun stx => do
+def hashCommandLinter : Linter where run := withSetOptionIn' fun stx => do
   let currentModule := (← getEnv).mainModule
   if getLinterHash (← getOptions) &&
      (currentModule.getRoot == `Mathlib || currentModule == `test.HashCommandLinter) then
@@ -43,8 +59,8 @@ def hashCommandLinter : Linter where run := withSetOptionIn fun stx => do
       | some sa =>
         let a := sa.getAtomVal
         if ("#".isPrefixOf a && whitelist.all (· != a)) then
-          logWarningAt sa f!"`#`-commands, such as '{a}', are not allowed in 'Mathlib'\n\
-            [linter.hashCommand]"
+          Linter.logLint linter.hashCommand sa
+            m!"`#`-commands, such as '{a}', are not allowed in 'Mathlib'"
       | none => return
 
 initialize addLinter hashCommandLinter
