6b5bbba23a7858414923cbc38bdcc11bbd065d99
Alessandro Iraci <SashaIr@users.noreply.github.com>
2024-03-30 10:30:47 +0100

    Update Mathlib/Combinatorics/Enumerative/Partition.lean
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- Mathlib/Combinatorics/Enumerative/Partition.lean
+++ Mathlib/Combinatorics/Enumerative/Partition.lean
@@ -108,7 +108,7 @@ def ofSym {n : ℕ} {σ : Type*} (s : Sym σ n) [DecidableEq σ] : n.Partition w
   parts := s.1.dedup.map s.1.count
   parts_pos := by simp [Multiset.count_pos]
   parts_sum := by
-    have : sum (map (fun a ↦ count a s.1) (toFinset s.1).val) =
+    have : sum (map (fun a ↦ count a s.1) s.1.dedup) =
       Finset.sum (toFinset s.1) fun a ↦ count a s.1 := rfl
     rw [this, Multiset.toFinset_sum_count_eq]
     exact s.2
