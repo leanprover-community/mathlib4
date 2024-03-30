ff809fdffdbe19c20b7de448c9d7b53cdf68fcb2
adomani <adomani@gmail.com>
2024-03-30 12:15:38 +0100

    Lint only silent `#`-commands

--- test/HashCommandLinter.lean
+++ test/HashCommandLinter.lean
@@ -2,109 +2,74 @@ import Lean.Elab.GuardMsgs
 import Mathlib.Mathport.Rename
 import Mathlib.Tactic.HashCommandLinter
 
-section inactive_hash_linter
-
-/-!
-#  Set the `#`-command linter to `false`
--/
-set_option linter.hashCommand false
-
+section ignored_commands
 theorem fo₁ : True := .intro
+-- `#align` is allowed by the linter
 #align true fo₁
 
 /-- info: 0 -/
 #guard_msgs in
+-- emits a message -- the linter allows it
 #eval 0
 
 /-- info: constructor PUnit.unit.{u} : PUnit -/
 #guard_msgs in
+-- emits a message -- the linter allows it
 #print PUnit.unit
 
 /-- info: 0 : Nat -/
 #guard_msgs in
-#check 0
-
-end inactive_hash_linter
-
-section active_hash_linter
-/-!
-#  Set the `#`-command linter to `true`
--/
-set_option linter.hashCommand true
-
-theorem fo₂ : True := .intro
--- `#align` is allowed
-#align false fo₂
-
-/--
-info: 0
----
-warning: `#`-commands, such as '#eval', are not allowed in 'Mathlib'
-[linter.hashCommand]
--/
-#guard_msgs in
-#eval 0
-
-/--
-info: constructor PUnit.unit.{u} : PUnit
----
-warning: `#`-commands, such as '#print', are not allowed in 'Mathlib'
-[linter.hashCommand]
--/
-#guard_msgs in
-#print PUnit.unit
-
-/--
-info: 0 : Nat
----
-warning: `#`-commands, such as '#check', are not allowed in 'Mathlib'
-[linter.hashCommand]
--/
-#guard_msgs in
+-- emits a message -- the linter allows it
 #check 0
 
 /--
-info: constructor PUnit.unit.{u} : PUnit
----
-warning: `#`-commands, such as '#print', are not allowed in 'Mathlib'
-[linter.hashCommand]
+info:
 -/
 #guard_msgs in
-#print PUnit.unit
+-- emits an empty message -- the linter allows it
+#eval show Lean.MetaM _ from do guard true
 
--- `run_cmd` is allowed
+-- not a `#`-command and not emitting a message: the linter allows it
 run_cmd if false then Lean.logInfo "0"
+end ignored_commands
 
--- Testing the the linter enters `in` recursively.
+section linted_commands
+/--
+warning: `#`-commands, such as '#guard', are not allowed in 'Mathlib' [linter.hashCommand]
+-/
+#guard_msgs in
+#guard true
 
 /--
-info: n : Nat
----
-warning: `#`-commands, such as '#check', are not allowed in 'Mathlib' [linter.hashCommand]
+warning: `#`-commands, such as '#check_tactic', are not allowed in 'Mathlib' [linter.hashCommand]
+-/
+#guard_msgs in
+#check_tactic True ~> True by skip
+
+-- Testing that the linter enters `in` recursively.
+
+/--
+warning: `#`-commands, such as '#guard', are not allowed in 'Mathlib' [linter.hashCommand]
 -/
 #guard_msgs in
 variable (n : Nat) in
-#check n
+#guard true
 
 /--
-info: n : Nat
----
-warning: `#`-commands, such as '#check', are not allowed in 'Mathlib' [linter.hashCommand]
+warning: `#`-commands, such as '#guard', are not allowed in 'Mathlib' [linter.hashCommand]
 -/
 #guard_msgs in
 open Nat in
 variable (n : Nat) in
 variable (n : Nat) in
-#check n
+#guard true
 
 /--
-info: n : Nat
----
-warning: `#`-commands, such as '#check', are not allowed in 'Mathlib' [linter.hashCommand]
+warning: `#`-commands, such as '#guard', are not allowed in 'Mathlib' [linter.hashCommand]
 -/
 #guard_msgs in
 open Nat in
 variable (n : Nat) in
-#check n
+#guard true
 
-end active_hash_linter
+end linted_commands
