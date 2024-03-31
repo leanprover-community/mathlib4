248f7440dd21de9df609573a302f044a1b307fc5
Eric Wieser <wieser.eric@gmail.com>
2024-03-31 20:28:26 +0000

    generalize further

--- Mathlib/Algebra/Order/Group/Action.lean
+++ Mathlib/Algebra/Order/Group/Action.lean
@@ -15,21 +15,19 @@ Notably these are relevant for pointwise actions on set-like objects.
 
 variable {ι : Sort*} {M α : Type*}
 
-theorem smul_mono_right [Mul M] [SMul M α] [Preorder α] [CovariantClass M α HSMul.hSMul LE.le]
+theorem smul_mono_right [SMul M α] [Preorder α] [CovariantClass M α HSMul.hSMul LE.le]
     (m : M) : Monotone (HSMul.hSMul m : α → α) :=
   fun _ _ => CovariantClass.elim _
 
-theorem smul_inf_le [Mul M] [SMul M α] [SemilatticeInf α] [CovariantClass M α HSMul.hSMul LE.le]
+theorem smul_inf_le [SMul M α] [SemilatticeInf α] [CovariantClass M α HSMul.hSMul LE.le]
     (m : M) (a₁ a₂ : α) : m • (a₁ ⊓ a₂) ≤ m • a₁ ⊓ m • a₂ :=
   le_inf (smul_mono_right _ inf_le_left) (smul_mono_right _ inf_le_right)
 
-theorem smul_iInf_le
-    [Mul M] [SMul M α] [CompleteLattice α] [CovariantClass M α HSMul.hSMul LE.le]
+theorem smul_iInf_le [SMul M α] [CompleteLattice α] [CovariantClass M α HSMul.hSMul LE.le]
     {m : M} {t : ι → α} :
     m • iInf t ≤ ⨅ i, m • t i :=
   le_iInf fun _ => smul_mono_right _ (iInf_le _ _)
 
-theorem smul_strict_mono_right
-    [Mul M] [SMul M α] [Preorder α] [CovariantClass M α HSMul.hSMul LT.lt] (m : M) :
-    StrictMono (HSMul.hSMul m : α → α) :=
+theorem smul_strict_mono_right [SMul M α] [Preorder α] [CovariantClass M α HSMul.hSMul LT.lt]
+    (m : M) : StrictMono (HSMul.hSMul m : α → α) :=
   fun _ _ => CovariantClass.elim _
