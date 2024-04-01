86a7879fd0abd192832a20befb77235398050c04
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2024-04-01 16:53:35 +0000

    feat(Mathlib/RingTheory/Ideal/Operations): simplify definition of radical (#11723)
    * `add_pow_mem_of_pow_mem_of_le` says that (a + b) ^ k belongs to an ideal I if a ^ m and b ^ n belong to that ideal, and `m + n ≤ k + 1`.
    * `Commute.add_pow_of_add_le_succ_eq_zero_of_pow_eq_zero` says that `(a + b) ^ k = 0` if `a ^ m = 0`, `b^ n = 0`, and `m + n ≤ k + 1`.
    * this is used to simplify the definition of docs#Ideal.radical and the definition of docs#Commute.isNilpotent_add

--- Mathlib/RingTheory/Ideal/Basic.lean
+++ Mathlib/RingTheory/Ideal/Basic.lean
@@ -578,6 +578,32 @@ theorem pow_mem_of_mem (ha : a ∈ I) (n : ℕ) (hn : 0 < n) : a ^ n ∈ I :=
     (fun m _hm => (pow_succ a m).symm ▸ I.mul_mem_left (a ^ m) ha) hn
 #align ideal.pow_mem_of_mem Ideal.pow_mem_of_mem
 
+theorem pow_mem_of_pow_mem {m n : ℕ} (ha : a ^ m ∈ I) (h : m ≤ n) : a ^ n ∈ I := by
+  rw [← Nat.add_sub_of_le h, pow_add]
+  exact I.mul_mem_right _ ha
+
+theorem add_pow_mem_of_pow_mem_of_le {m n k : ℕ}
+    (ha : a ^ m ∈ I) (hb : b ^ n ∈ I) (hk : m + n ≤ k + 1) :
+    (a + b) ^ k ∈ I := by
+  rw [add_pow]
+  apply I.sum_mem
+  intro c _
+  apply mul_mem_right
+  by_cases h : m ≤ c
+  · exact I.mul_mem_right _ (I.pow_mem_of_pow_mem ha h)
+  · refine I.mul_mem_left _ (I.pow_mem_of_pow_mem hb ?_)
+    simp only [not_le, Nat.lt_iff_add_one_le] at h
+    have hck : c ≤ k := by
+      rw [← add_le_add_iff_right 1]
+      exact le_trans h (le_trans (Nat.le_add_right _ _) hk)
+    rw [Nat.le_sub_iff_add_le hck, ← add_le_add_iff_right 1]
+    exact le_trans (by rwa [add_comm _ n, add_assoc, add_le_add_iff_left]) hk
+
+theorem add_pow_add_pred_mem_of_pow_mem  {m n : ℕ}
+    (ha : a ^ m ∈ I) (hb : b ^ n ∈ I) :
+    (a + b) ^ (m + n - 1) ∈ I :=
+  I.add_pow_mem_of_pow_mem_of_le ha hb <| by rw [← Nat.sub_le_iff_le_add]
+
 theorem IsPrime.mul_mem_iff_mem_or_mem {I : Ideal α} (hI : I.IsPrime) :
     ∀ {x y : α}, x * y ∈ I ↔ x ∈ I ∨ y ∈ I := @fun x y =>
   ⟨hI.mem_or_mem, by
