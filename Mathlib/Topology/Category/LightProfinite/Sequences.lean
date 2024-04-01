ce911f096e2af562940b46bc08466653ad26ceb5
dagurtomas <dagurtomas@gmail.com>
2024-04-01 23:21:04 +0000

    some work on N union infty

--- Mathlib/Topology/Category/LightProfinite/Sequences.lean
+++ Mathlib/Topology/Category/LightProfinite/Sequences.lean
@@ -1,15 +1,9 @@
-import Mathlib.Topology.Category.LightProfinite.Basic
+import Mathlib.Topology.Category.LightProfinite.IsLightIso
 import Mathlib.Topology.Compactification.OnePoint
 
 open CategoryTheory Limits Opposite Option
 
 namespace Profinite
-
-example : TopologicalSpace (OnePoint ℕ) := inferInstance
-
-example : CompactSpace (OnePoint ℕ) := inferInstance
-
-example : T2Space (OnePoint ℕ) := inferInstance
 
 instance : TotallySeparatedSpace (OnePoint ℕ) where
   isTotallySeparated_univ x _ y _ hxy := by
@@ -36,6 +30,9 @@ instance : TotallySeparatedSpace (OnePoint ℕ) where
         all_goals aesop
 
 def NatUnionInfty := of (OnePoint ℕ)
+
+-- TODO: prove `IsLight.of_countable` and deduce this
+instance : IsLight NatUnionInfty := sorry
 
 end Profinite
 
@@ -65,11 +62,66 @@ def NatUnionInftyDiagram : ℕᵒᵖ ⥤ FintypeCat where
 
 noncomputable def NatUnionInfty := of NatUnionInftyDiagram
 
+def extracted_1 (n : ℕ) : LocallyConstant (ofIsLight Profinite.NatUnionInfty).toProfinite
+    (NatUnionInfty.diagram.obj ⟨n⟩) where
+  toFun a := by
+    cases a with
+    | none => exact ⟨n, Finset.self_mem_range_succ n⟩
+    | some val => exact if h : _ then ⟨val, h⟩ else ⟨n, Finset.self_mem_range_succ n⟩
+  isLocallyConstant := by
+    rw [IsLocallyConstant.iff_isOpen_fiber]
+    intro ⟨y, hy⟩
+    by_cases h : y = n
+    · rw [OnePoint.isOpen_iff_of_mem]
+      · simp only [Finset.mem_range, isClosed_discrete, true_and]
+        rw [isCompact_iff_finite, ← Set.preimage_compl]
+        subst h
+        refine Set.Finite.preimage (Function.Injective.injOn OnePoint.coe_injective _) ?_
+        rw [← Set.preimage_compl]
+        refine Set.Finite.preimage ?_ (inferInstance : Finite _)
+        intro a ha b hb h
+        cases a with
+        | none => simp [OnePoint.infty] at ha
+        | some a =>
+          cases b with
+          | none => simp [OnePoint.infty] at hb
+          | some b =>
+            simp only [Set.preimage_compl, Set.mem_compl_iff, Set.mem_preimage,
+              Set.mem_singleton_iff, dite_eq_right_iff, not_forall] at ha hb
+            obtain ⟨ha, _⟩ := ha
+            obtain ⟨hb, _⟩ := hb
+            rw [Subtype.ext_iff_val] at h
+            simp only [ha, reduceDite, hb] at h
+            rw [h]
+      · simp [OnePoint.infty, h]
+    · rw [OnePoint.isOpen_iff_of_not_mem]
+      · simp
+      · simp only [OnePoint.infty, Finset.mem_range, Set.mem_preimage, Set.mem_singleton_iff]
+        rw [Subtype.ext_iff_val]
+        aesop
+
+noncomputable def NatUnionInftyIso_hom :
+    LightProfinite.ofIsLight Profinite.NatUnionInfty ⟶ NatUnionInfty := by
+  refine homMk' extracted_1 ?_
+  · intro n
+    ext x
+    cases x with
+    | none =>
+      simp only [transitionMap, extracted_1, Finset.mem_range, LocallyConstant.coe_mk,
+        Function.comp_apply]
+      simp [NatUnionInfty, of, NatUnionInftyDiagram]
+    | some val =>
+      simp [transitionMap, extracted_1]
+      simp [NatUnionInfty, of, NatUnionInftyDiagram]
+      split_ifs with h₁ h₂ h₃ h₄
+      · rfl
+      · rfl
+      · simp at h₃
+      · simpa using lt_of_le_of_lt (not_lt.mp h₁) h₄
+      · rfl
+
 noncomputable def NatUnionInftyIso : Profinite.NatUnionInfty ≅ NatUnionInfty.toProfinite  where
-  hom := by
-    refine ⟨fun a ↦ ?_, ?_⟩
-    · sorry
-    · sorry
+  hom := NatUnionInftyIso_hom
   inv := sorry
   hom_inv_id := sorry
   inv_hom_id := sorry
