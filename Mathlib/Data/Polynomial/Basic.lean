f25667d0bcd33623da9f5bae5a0319b8c573e8bc
Bolton Bailey <bolton.bailey@gmail.com>
2024-04-01 12:49:06 -0500

    Discard changes to Mathlib/Data/Polynomial/Basic.lean

--- Mathlib/Data/Polynomial/Basic.lean
+++ Mathlib/Data/Polynomial/Basic.lean
@@ -423,9 +423,8 @@ theorem support_eq_empty : p.support = ∅ ↔ p = 0 := by
   simp [support]
 #align polynomial.support_eq_empty Polynomial.support_eq_empty
 
-@[simp]
-lemma support_nonempty_iff : p.support.Nonempty ↔ p ≠ 0 := by
-  rw [Finset.nonempty_iff_ne_empty, ne_eq, support_eq_empty]
+@[simp] lemma support_nonempty : p.support.Nonempty ↔ p ≠ 0 :=
+  Finset.nonempty_iff_ne_empty.trans support_eq_empty.not
 
 theorem card_support_eq_zero : p.support.card = 0 ↔ p = 0 := by simp
 #align polynomial.card_support_eq_zero Polynomial.card_support_eq_zero
