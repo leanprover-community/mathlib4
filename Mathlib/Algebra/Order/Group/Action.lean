b61b65bf7db7b9aa95d931e4bca2cfc0a7788d98
Eric Wieser <wieser.eric@gmail.com>
2024-04-01 14:37:03 +0100

    Update Action.lean

--- Mathlib/Algebra/Order/Group/Action.lean
+++ Mathlib/Algebra/Order/Group/Action.lean
@@ -3,7 +3,8 @@ Copyright (c) 2024 Eric Wieser. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Eric Wieser
 -/
-import Mathlib.GroupTheory.GroupAction.Basic
+import Mathlib.Algebra.CovariantAndContravariant
+import Mathlib.Order.ConditionallyCompleteLattice.Basic
 
 /-!
 # Results about `CovariantClass G α HSMul.hSMul LE.le`
