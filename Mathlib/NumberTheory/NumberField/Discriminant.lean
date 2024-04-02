ccda87e647e2fb7a72374c0d835a7c5453b1f7f2
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2024-04-02 13:53:38 +0000

    fix: align Int.norm_eq_abs with its mathlib3 meaning (#11841)

--- Mathlib/NumberTheory/NumberField/Discriminant.lean
+++ Mathlib/NumberTheory/NumberField/Discriminant.lean
@@ -309,8 +309,8 @@ theorem minkowskiBound_lt_boundOfDiscBdd : minkowskiBound K ↑1 < boundOfDiscBd
     coe_mul, ENNReal.coe_pow, coe_ofNat, show sqrt N = (1:ℝ≥0∞) * sqrt N by rw [one_mul]]
   gcongr
   · exact pow_le_one _ (by positivity) (by norm_num)
-  · rw [sqrt_le_sqrt, ← NNReal.coe_le_coe, coe_nnnorm, Int.norm_eq_abs]
-    exact Int.cast_le.mpr hK
+  · rwa [sqrt_le_sqrt, ← NNReal.coe_le_coe, coe_nnnorm, Int.norm_eq_abs, ← Int.cast_abs,
+      NNReal.coe_nat_cast, ← Int.cast_ofNat, Int.cast_le]
   · exact one_le_two
   · exact rank_le_rankOfDiscrBdd hK
 
