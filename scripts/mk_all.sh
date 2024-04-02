3f23e089bc8fb7cd179c33ffc74b793911780932
adomani <adomani@gmail.com>
2024-04-02 14:05:12 +0200

    fix: mk_all ignores locally removed files

--- scripts/mk_all.sh
+++ scripts/mk_all.sh
@@ -2,5 +2,7 @@
 cd "$(git rev-parse --show-toplevel)" || exit 1
 for gp in Mathlib MathlibExtras Mathlib/Tactic Counterexamples Archive
 do
-  git ls-files "$gp/*.lean" | LC_ALL=C sort | sed 's/\.lean//;s,/,.,g;s/^/import /' > "$gp.lean"
+  ## show all the files and remove the ones that are locally `-d`eleted
+  comm -23 <(git ls-files "$gp/*.lean" | sort) <(git ls-files -d "$gp/*.lean" | sort) |
+    LC_ALL=C sort | sed 's/\.lean//;s,/,.,g;s/^/import /' > "$gp.lean"
 done
