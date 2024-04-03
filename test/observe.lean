6c8cfadbbc2986c50cad3827e9dfa3db21f3bb5d
Scott Morrison <scott.morrison@gmail.com>
2024-04-03 13:50:36 +1100

    restore observe

--- test/observe.lean
+++ test/observe.lean
@@ -4,9 +4,7 @@ open Nat
 
 -- Adaptation note: at nightly-2024-03-27,
 -- we had to increase `maxHeartbeats` here from 8000 to 16000.
--- Adaptation note: at nightly-2024-04-01,
--- we had to increase `maxHeartbeats` here from 16000 to 24000.
-set_option maxHeartbeats 24000 in
+set_option maxHeartbeats 16000 in
 theorem euclid (n : ℕ) : ∃ N, n < N ∧ N.Prime := by
   let N := n.factorial + 1
   let p := minFac N
@@ -17,9 +15,9 @@ theorem euclid (n : ℕ) : ∃ N, n < N ∧ N.Prime := by
     omega
   constructor
   · by_contra!
-    have this₁ : p ∣ n.factorial := (Prime.dvd_factorial prime).mpr this
-    have this₂ : p ∣ N := minFac_dvd N
-    have this₃ : p ∣ 1 := (Nat.dvd_add_iff_right this₁).mpr this₂
+    observe : p ∣ n.factorial
+    observe : p ∣ N
+    observe : p ∣ 1
     observe : ¬ p ∣ 1
     contradiction
   · exact prime
