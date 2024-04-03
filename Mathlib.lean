e5e4ab29aec95360b42271feea1788e4c241ba13
Eric Wieser <wieser.eric@gmail.com>
2024-04-03 21:33:45 +0000

    import

--- Mathlib.lean
+++ Mathlib.lean
@@ -2341,6 +2341,7 @@ import Mathlib.GroupTheory.Coprod.Basic
 import Mathlib.GroupTheory.CoprodI
 import Mathlib.GroupTheory.Coset
 import Mathlib.GroupTheory.Coxeter.Basic
+import Mathlib.GroupTheory.Coxeter.Matrix
 import Mathlib.GroupTheory.Divisible
 import Mathlib.GroupTheory.DoubleCoset
 import Mathlib.GroupTheory.EckmannHilton
