e06c8dc3b6307dc7bafd9fb4fc545b1b0bf9d2fb
Sam Ezeh <sam.z.ezeh@gmail.com>
2024-04-03 11:25:36 +0100

    test: add abel tests for commutative monoids and groups

--- test/abel.lean
+++ test/abel.lean
@@ -1,6 +1,9 @@
 import Mathlib.Tactic.Abel
 
 variable {α : Type _} {a b : α}
+def id' (x : α) := x
+
+section additive
 
 example [AddCommMonoid α] : a + (b + a) = a + a + b := by abel
 example [AddCommGroup α] : (a + b) - ((b + a) + a) = -a := by abel
@@ -52,7 +55,6 @@ example [AddCommGroup α] (a b c : α) :
   right; trivial
 
 -- `abel!` should see through terms that are definitionally equal,
-def id' (x : α) := x
 example [AddCommGroup α] (a b : α) : a + b - b - id' a = 0 := by
   fail_if_success
     abel1
@@ -124,3 +126,128 @@ example [AddCommGroup α] (x y z : α) (h : False) (w : x - x = y + z) : False :
   abel_nf at *
   guard_hyp w : 0 = y + z
   assumption
+end additive
+
+section multiplicative
+example [CommMonoid α] : a * (b * a) = a * a * b := by abel
+example [CommGroup α] : (a * b) / ((b * a) * a) = a⁻¹ := by abel
+example [CommGroup α] (x : α) : x / 1 = x := by abel
+example [CommMonoid α] : a^(3 : ℕ) = a * a^(2 : ℕ) := by abel
+example [CommGroup α] : a^(3 : ℤ) = a * a^(2 : ℤ) := by abel
+example [CommGroup α] (a b : α) : a/b^2 = a * b^(-2 : ℤ) := by abel
+
+example [CommMonoid α] (a b : α) : a * (b * a) = a * a * b := by abel1
+example [CommGroup α] (a b : α) : (a * b) / ((b * a) * a) = a⁻¹ := by abel1
+example [CommGroup α] (x : α) : x / 1 = x := by abel1
+example [CommMonoid α] (a : α) : a^(3 : ℕ) = a * a^(2 : ℕ) := by abel1
+example [CommGroup α] (a : α) : a^(3 : ℤ) = a * a^(2 : ℤ) := by abel1
+example [CommGroup α] (a b : α) : a / b^2 = a / b^2 := by abel1
+example [CommGroup α] (a : α) : 1 * a = a := by abel1
+example [CommGroup α] (n : ℕ) (a : α) : a^n = a^n := by abel1
+example [CommGroup α] (n : ℕ) (a : α) : 1 * a^n = a^n := by abel1
+
+
+example [CommMonoid α] (a b c d e : α) :
+  a * (b * (a * (c * (a * (d * (a * e)))))) = e * d * c * b * a^4 := by abel1
+example [CommGroup α] (a b c d e : α) :
+  a * (b * (a * (c * (a * (d * (a * e)))))) = e * d * c * b * a^4 := by abel1
+example [CommGroup α] (a b c d : α) :
+  a * b * (c * d / a) = b * c * d := by abel1
+example [CommGroup α] (a b c : α) :
+  a * b * c * (c / a / a) = a^(-1 : ℤ) * b * c^2 := by abel1
+
+-- Make sure we fail on some non-equalities.
+example [CommMonoid α] (a b c d e : α) :
+    a * (b * (a * (c * (a * (d * (a * e)))))) = e * d * c * b * a^3 ∨ True := by
+  fail_if_success
+    left; abel1
+  right; trivial
+example [CommGroup α] (a b c d e : α) :
+    a * (b * (a * (c * (a * (d * (a * e)))))) = e * d * c * b * a^3 ∨ True := by
+  fail_if_success
+    left; abel1
+  right; trivial
+example [CommGroup α] (a b c d : α) :
+    a * b * (c * d / a) = b * c / d ∨ True := by
+  fail_if_success
+    left; abel1
+  right; trivial
+example [CommGroup α] (a b c : α) :
+    a * b * c * (c / a / a) = a^(-1 : ℤ) * b * c ∨ True := by
+  fail_if_success
+    left; abel1
+  right; trivial
+
+-- `abel!` should see through terms that are definitionally equal,
+example [CommGroup α] (a b : α) : a * b / b / id' a = 1 := by
+  fail_if_success
+    abel1
+  abel1!
+
+-- https://leanprover.zulipchat.com/#narrow/stream/287929-mathlib4/topic/Interaction.20of.20abel.20with.20casting/near/319895001
+example [CommGroup α] : True := by
+  have : ∀ (p q r s : α), s * p / q = s / r / (q / r / p) := by
+    intro p q r s
+    abel
+  trivial
+
+-- https://leanprover.zulipchat.com/#narrow/stream/287929-mathlib4/topic/Interaction.20of.20abel.20with.20casting/near/319897374
+example [CommGroup α] (x y z : α) : y = x * z / (x / y * z) := by
+  have : True := trivial
+  abel
+
+-- https://leanprover.zulipchat.com/#narrow/stream/287929-mathlib4/topic/abel.20bug.3F/near/368707560
+example [CommGroup α] (a b s : α) : b⁻¹ * (s / a) = s / b / a := by abel_nf
+
+/--
+error: abel_nf made no progress
+-/
+#guard_msgs in
+example : False := by abel_nf
+
+/--
+error: abel_nf made no progress
+-/
+#guard_msgs in
+example [CommGroup α] (x y z : α) (w : x = y * z) : False := by
+  abel_nf at w
+
+example [CommGroup α] (x y z : α) (h : False) (w : x / x = y * z) : False := by
+  abel_nf at w
+  guard_hyp w : 1 = y * z
+  assumption
+
+/--
+error: abel_nf made no progress
+-/
+#guard_msgs in
+example [CommGroup α] (x y z : α) (_w : x = y * z) : False := by
+  abel_nf at *
+
+-- Prior to https://github.com/leanprover/lean4/pull/2917 this would fail
+-- (the `at *` would close the goal,
+-- and then error when trying to work on the hypotheses because there was no goal.)
+example [CommGroup α] (x y z : α) (_w : x = y * z) : x / x = 1 := by
+  abel_nf at *
+
+/--
+error: abel_nf made no progress
+-/
+-- Ideally this would specify that it made no progress at `w`.
+#guard_msgs in
+example [CommGroup α] (x y z : α) (w : x = y * z) : x / x = 1 := by
+  abel_nf at w ⊢
+
+/--
+error: abel_nf made no progress
+-/
+-- Ideally this would specify that it made no progress at `⊢`.
+#guard_msgs in
+example [CommGroup α] (x y z : α) (w : x / x = y * z) : x = 1 := by
+  abel_nf at w ⊢
+
+example [CommGroup α] (x y z : α) (h : False) (w : x / x = y * z) : False := by
+  abel_nf at *
+  guard_hyp w : 1 = y * z
+  assumption
+end multiplicative
