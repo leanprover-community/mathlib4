3259db8050106e698659e99cc7601d08a5bd8039
Scott Morrison <scott.morrison@gmail.com>
2024-04-03 12:44:16 +1100

    fix

--- Mathlib/Data/List/Intervals.lean
+++ Mathlib/Data/List/Intervals.lean
@@ -112,8 +112,6 @@ theorem inter_consecutive (n m l : ℕ) : Ico n m ∩ Ico m l = [] := by
 
 @[simp]
 theorem bagInter_consecutive (n m l : Nat) : @List.bagInter ℕ instBEq (Ico n m) (Ico m l) = [] :=
-  -- Adaptation note: 2024-04-01
-  -- The `by convert` was not previously necessary.
   (bagInter_nil_iff_inter_nil _ _).2 (by convert inter_consecutive n m l)
 #align list.Ico.bag_inter_consecutive List.Ico.bagInter_consecutive
 
