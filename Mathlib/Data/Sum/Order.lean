d0fa9dafd469a62529d375928372a517980da3d6
adomani <adomani@gmail.com>
2024-04-03 19:29:45 +0200

    Data.Sum.Order

--- Mathlib/Data/Sum/Order.lean
+++ Mathlib/Data/Sum/Order.lean
@@ -688,7 +688,7 @@ theorem sumLexAssoc_symm_apply_inr_inr : (sumLexAssoc α β γ).symm (inr (inr c
 def sumLexDualAntidistrib (α β : Type*) [LE α] [LE β] : (α ⊕ₗ β)ᵒᵈ ≃o βᵒᵈ ⊕ₗ αᵒᵈ :=
   { Equiv.sumComm α β with
     map_rel_iff' := @fun a b => by
-      rcases a with (a | a) <;> rcases b with (b | b); simp
+      rcases a with (a | a) <;> rcases b with (b | b); simp only
       · change
           toLex (inr <| toDual a) ≤ toLex (inr <| toDual b) ↔
             toDual (toLex <| inl a) ≤ toDual (toLex <| inl b)
