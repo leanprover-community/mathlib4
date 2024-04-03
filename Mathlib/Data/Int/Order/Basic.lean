c2ff7c73d914032e31abb2e70e3fe1325ec8e101
adomani <adomani@gmail.com>
2024-04-03 17:31:37 +0200

    Data.Int.Order.Basic

--- Mathlib/Data/Int/Order/Basic.lean
+++ Mathlib/Data/Int/Order/Basic.lean
@@ -107,6 +107,7 @@ theorem coe_nat_ne_zero_iff_pos {n : ℕ} : (n : ℤ) ≠ 0 ↔ 0 < n :=
 @[norm_cast] lemma abs_coe_nat (n : ℕ) : |(n : ℤ)| = n := abs_of_nonneg (coe_nat_nonneg n)
 #align int.abs_coe_nat Int.abs_coe_nat
 
+set_option linter.flexible false in  -- simp; rw
 theorem sign_add_eq_of_sign_eq : ∀ {m n : ℤ}, m.sign = n.sign → (m + n).sign = n.sign := by
   have : (1 : ℤ) ≠ -1 := by decide
   rintro ((_ | m) | m) ((_ | n) | n) <;> simp [this, this.symm, Int.negSucc_add_negSucc]
