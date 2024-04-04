18133e6fe8af535e0d22c9db6ded5f8e2dd70abc
Joël Riou <joel.riou@universite-paris-saclay.fr>
2024-04-04 20:11:13 +0200

    fixed imports

--- Mathlib/Algebra/Homology/Embedding/Basic.lean
+++ Mathlib/Algebra/Homology/Embedding/Basic.lean
@@ -3,7 +3,8 @@ Copyright (c) 2024 Joël Riou. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joël Riou
 -/
-import Mathlib.Algebra.Homology.HomologicalComplex
+import Mathlib.Algebra.Homology.ComplexShape
+import Mathlib.Data.Int.Order.Basic
 
 /-! # Embeddings of complex shapes
 
