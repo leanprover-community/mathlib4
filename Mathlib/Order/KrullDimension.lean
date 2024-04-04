9e33fe985723b29fe4f91ade55eee77b1f170a4e
Jujian Zhang <jujian.zhang1998@outlook.com>
2024-04-04 20:14:36 +0000

    inline

--- Mathlib/Order/KrullDimension.lean
+++ Mathlib/Order/KrullDimension.lean
@@ -55,7 +55,7 @@ noncomputable def coheight (a : α) : WithBot (WithTop ℕ) := krullDim (Set.Ici
 
 end definitions
 
-namespace krullDim
+section krullDim
 
 variable {α β : Type*}
 
@@ -64,9 +64,9 @@ variable [Preorder α] [Preorder β]
 lemma krullDim_nonneg_of_nonempty [Nonempty α] : 0 ≤ krullDim α :=
   le_sSup ⟨⟨0, fun _ ↦ @Nonempty.some α inferInstance, fun f ↦ f.elim0⟩, rfl⟩
 
-lemma eq_bot_of_isEmpty [IsEmpty α] : krullDim α = ⊥ := WithBot.ciSup_empty _
+lemma krullDim_eq_bot_of_isEmpty [IsEmpty α] : krullDim α = ⊥ := WithBot.ciSup_empty _
 
-lemma eq_top_of_infiniteDimensionalOrder [InfiniteDimensionalOrder α] :
+lemma krullDim_eq_top_of_infiniteDimensionalOrder [InfiniteDimensionalOrder α] :
     krullDim α = ⊤ :=
 le_antisymm le_top <| le_iSup_iff.mpr <| fun m hm ↦ match m, hm with
 | ⊥, hm => False.elim <| by
@@ -78,55 +78,50 @@ le_antisymm le_top <| le_iSup_iff.mpr <| fun m hm ↦ match m, hm with
   erw [WithBot.coe_lt_coe, WithTop.coe_lt_coe]
   simp
 
-lemma eq_length_of_finiteDimensionalType [FiniteDimensionalOrder α] :
+lemma krullDim_eq_length_of_finiteDimensionalOrder [FiniteDimensionalOrder α] :
     krullDim α = (LTSeries.longestOf α).length :=
 le_antisymm
   (iSup_le <| fun _ ↦ WithBot.coe_le_coe.mpr <| WithTop.coe_le_coe.mpr <|
     RelSeries.length_le_length_longestOf _ _) <|
   le_iSup (fun (i : LTSeries _) ↦ (i.length : WithBot (WithTop ℕ))) <| LTSeries.longestOf _
 
-lemma eq_zero_of_unique [Unique α] : krullDim α = 0 :=  by
-  rw [eq_len_of_finiteDimensionalType (α := α), Nat.cast_eq_zero]
+lemma krullDim_eq_zero_of_unique [Unique α] : krullDim α = 0 :=  by
+  rw [krullDim_eq_length_of_finiteDimensionalOrder (α := α), Nat.cast_eq_zero]
   refine (LTSeries.longestOf_len_unique (default : LTSeries α) fun q ↦ show _ ≤ 0 from ?_).symm
   by_contra r
   exact ne_of_lt (q.step ⟨0, not_le.mp r⟩) <| Subsingleton.elim _ _
 
 /-- If `f : α → β` is a strictly monotonic function and `α` is an infinite dimensional type then so
   is `β`. -/
-lemma infiniteDimensional_of_strictMono
+lemma infiniteDimensionalOrder_of_strictMono
     (f : α → β) (hf : StrictMono f) [InfiniteDimensionalOrder α] :
     InfiniteDimensionalOrder β :=
   ⟨fun n ↦ ⟨(LTSeries.withLength _ n).map f hf, LTSeries.length_withLength α n⟩⟩
 
-lemma le_of_strictMono (f : α → β) (hf : StrictMono f) : krullDim α ≤ krullDim β :=
+lemma krullDim_le_of_strictMono (f : α → β) (hf : StrictMono f) : krullDim α ≤ krullDim β :=
   iSup_le <| fun p ↦ le_sSup ⟨p.map f hf, rfl⟩
 
 lemma height_mono {a b : α} (h : a ≤ b) : height α a ≤ height α b :=
-  le_of_strictMono (fun x ↦ ⟨x, le_trans x.2 h⟩) <| fun _ _ h ↦ h
+  krullDim_le_of_strictMono (fun x ↦ ⟨x, le_trans x.2 h⟩) <| fun _ _ h ↦ h
 
-lemma le_of_strictComono_and_surj
+lemma krullDim_le_of_strictComono_and_surj
     (f : α → β) (hf : ∀ ⦃a b⦄, f a < f b → a < b) (hf' : Function.Surjective f) :
     krullDim β ≤ krullDim α :=
   iSup_le fun p ↦ le_sSup ⟨p.comap _ hf hf', rfl⟩
 
-lemma eq_of_orderIso (f : α ≃o β) : krullDim α = krullDim β :=
-  le_antisymm (le_of_strictMono _ f.strictMono) <| le_of_strictMono _ f.symm.strictMono
+lemma krullDim_eq_of_orderIso (f : α ≃o β) : krullDim α = krullDim β :=
+  le_antisymm (krullDim_le_of_strictMono _ f.strictMono) <|
+    krullDim_le_of_strictMono _ f.symm.strictMono
 
-lemma eq_iSup_height : krullDim α = ⨆ (a : α), height α a :=
+lemma krullDim_eq_iSup_height : krullDim α = ⨆ (a : α), height α a :=
   le_antisymm
-    (iSup_le fun i ↦ le_iSup_of_le (i ⟨i.length, lt_add_one _⟩)
-      <| le_sSup ⟨⟨_, fun m ↦ ⟨i m, i.strictMono.monotone <| show m.1 ≤ i.length by omega⟩,
-        i.step⟩, rfl⟩) <|
-    iSup_le <| fun a ↦ le_of_strictMono Subtype.val <| fun _ _ h ↦ h
+    (iSup_le fun i ↦ le_iSup_of_le (i ⟨i.length, lt_add_one _⟩) <|
+    le_sSup ⟨⟨_, fun m ↦ ⟨i m, i.strictMono.monotone <| show m.1 ≤ i.length by omega⟩,
+      i.step⟩, rfl⟩) <|
+    iSup_le fun a ↦ krullDim_le_of_strictMono Subtype.val fun _ _ h ↦ h
 
-lemma le_orderDual : krullDim α ≤ krullDim αᵒᵈ :=
-  iSup_le <| fun i ↦ le_sSup <| ⟨i.reverse, rfl⟩
-
-lemma orderDual_le : krullDim αᵒᵈ ≤ krullDim α :=
-  le_orderDual.trans <| le_of_strictMono
-    (OrderDual.ofDual ∘ OrderDual.ofDual) <| fun _ _ h ↦ h
-
-lemma eq_orderDual : krullDim α = krullDim αᵒᵈ :=
-  le_antisymm le_orderDual <| orderDual_le
+lemma krullDim_eq_orderDual : krullDim α = krullDim αᵒᵈ :=
+  le_antisymm (iSup_le fun i ↦ le_sSup ⟨i.reverse, rfl⟩) <|
+    iSup_le fun i ↦ le_sSup ⟨i.reverse, rfl⟩
 
 end krullDim
