5697e674eea692ad46c8d1d6adeaa8d520de4243
Riccardo Brasca <riccardo.brasca@gmail.com>
2024-04-04 21:21:52 +0200

    faster

--- Mathlib/NumberTheory/FLT/Three.lean
+++ Mathlib/NumberTheory/FLT/Three.lean
@@ -25,7 +25,7 @@ open ZMod
 
 private lemma cube_of_castHom_ne_zero {n : ZMod 9} :
     castHom (show 3 ∣ 9 by norm_num) (ZMod 3) n ≠ 0 → n ^ 3 = 1 ∨ n ^ 3 = 8 := by
-  fin_cases n <;> decide
+  revert n; decide
 
 private lemma cube_of_not_dvd {n : ℤ} (h : ¬ 3 ∣ n) :
     (n : ZMod 9) ^ 3 = 1 ∨ (n : ZMod 9) ^ 3 = 8 := by
