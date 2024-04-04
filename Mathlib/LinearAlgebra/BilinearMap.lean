a640495dc1ea04114b6ccaa733e6447e62d249fd
Christopher Hoskin <christopher.hoskin@gmail.com>
2024-04-04 16:01:49 +0100

    BilinMap and BilinForm

--- Mathlib/LinearAlgebra/BilinearMap.lean
+++ Mathlib/LinearAlgebra/BilinearMap.lean
@@ -402,14 +402,14 @@ variable {R M}
 theorem lsmul_apply (r : R) (m : M) : lsmul R M r m = r • m := rfl
 #align linear_map.lsmul_apply LinearMap.lsmul_apply
 
+variable (R M Nₗ) in
+protected abbrev BilinMap : Type _ := M →ₗ[R] M →ₗ[R] Nₗ
+
 variable (R M) in
 /-- For convenience, a shorthand for the type of bilinear forms from `M` to `R`.
 
 This should eventually replace `_root_.BilinForm`. -/
-protected abbrev BilinForm : Type _ := M →ₗ[R] M →ₗ[R] R
-
-variable (R M Nₗ) in
-protected abbrev BilinMap : Type _ := M →ₗ[R] M →ₗ[R] Nₗ
+protected abbrev BilinForm : Type _ := LinearMap.BilinMap R M R
 
 /-- The restriction of a bilinear form to a submodule. -/
 abbrev _root_.Submodule.restrictBilinear (p : Submodule R M) (f : LinearMap.BilinForm R M) :
