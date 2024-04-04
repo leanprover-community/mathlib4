e4598671c40099ad9c9fdf1962a164b4b6473685
acmepjz <acme_pjz@hotmail.com>
2024-04-05 04:01:57 +0800

    apply suggestions and remove some results

--- Mathlib/LinearAlgebra/TensorProduct/Finiteness.lean
+++ Mathlib/LinearAlgebra/TensorProduct/Finiteness.lean
@@ -17,12 +17,17 @@ This file contains some finiteness results of tensor product.
   any element of `M ⊗[R] N` can be written as a finite sum of pure tensors.
   See also `TensorProduct.span_tmul_eq_top`.
 
-- `TensorProduct.exists_finite_submodule_left_of_fintype`,
-  `TensorProduct.exists_finite_submodule_right_of_fintype`,
-  `TensorProduct.exists_finite_submodule_of_fintype` and 21 more:
-  any finitely many elements `M ⊗[R] N` is contained in `M' ⊗[R] N'`
+- `TensorProduct.exists_finite_submodule_left_of_finite`,
+  `TensorProduct.exists_finite_submodule_right_of_finite`,
+  `TensorProduct.exists_finite_submodule_of_finite`:
+  any finite subset of `M ⊗[R] N` is contained in `M' ⊗[R] N`,
+  resp. `M ⊗[R] N'`, resp. `M' ⊗[R] N'`,
   for some finitely generated submodules `M'` and `N'` of `M` and `N`, respectively.
-  Each of these 3 functions has 8 variants.
+
+- `TensorProduct.exists_finite_submodule_left_of_finite'`,
+  `TensorProduct.exists_finite_submodule_right_of_finite'`,
+  `TensorProduct.exists_finite_submodule_of_finite'`:
+  variation of the above results where `M` and `N` are already submodules.
 
 ## Tags
 
@@ -38,7 +43,7 @@ variable {R M N : Type*}
 
 variable [CommSemiring R] [AddCommMonoid M] [AddCommMonoid N] [Module R M] [Module R N]
 
-variable {M₁ : Submodule R M} {N₁ : Submodule R N}
+variable {M₁ M₂ : Submodule R M} {N₁ N₂ : Submodule R N}
 
 namespace TensorProduct
 
@@ -87,218 +92,88 @@ theorem exists_finset (x : M ⊗[R] N) :
   rw [h, Finsupp.sum]
   refine' Finset.sum_nbij' (fun m ↦ ⟨m, S m⟩) Prod.fst .. <;> simp
 
-/-- If `x : ι → M ⊗[R] N` are finitely many elements, then there exists a finitely generated
-submodule `M'` of `M` and `x' : ι → M' ⊗[R] N`, whose image in `M ⊗[R] N` is equal to `x`.
-There are 8 variants of this function. -/
-theorem exists_finite_submodule_left_of_fintype {ι : Type*} [Fintype ι] (x : ι → M ⊗[R] N) :
-    ∃ (M' : Submodule R M) (x' : ι → M' ⊗[R] N), Module.Finite R M' ∧
-    ∀ (i : ι), M'.subtype.rTensor N (x' i) = x i := by
-  classical
-  choose S hS using fun i ↦ (x i).exists_multiset
-  let T := ((Finset.sum Finset.univ S).map Prod.fst).toFinset
-  let M' := span R (T : Set M)
-  let f (i : M × N) : M' ⊗[R] N := if h : i.1 ∈ T then ⟨i.1, subset_span h⟩ ⊗ₜ[R] i.2 else 0
-  let x' (i : ι) := ((S i).map f).sum
-  refine ⟨M', x', inferInstance, fun j ↦ ?_⟩
-  simp_rw [x', hS, map_multiset_sum, Multiset.map_map]
-  congr 1
-  refine Multiset.map_congr rfl fun i hi ↦ ?_
-  replace hi : i.1 ∈ T := by
-    simp only [Multiset.mem_toFinset, Multiset.mem_map, Prod.exists, exists_and_right,
-      exists_eq_right, T]
-    exact ⟨i.2, Multiset.mem_of_le (Finset.single_le_sum (by simp) (by simp)) hi⟩
-  simp_rw [Function.comp_apply, f, dif_pos hi]; rfl
+lemma range_mapIncl_mono (hM : M₁ ≤ M₂) (hN : N₁ ≤ N₂) :
+    LinearMap.range (mapIncl M₁ N₁) ≤ LinearMap.range (mapIncl M₂ N₂) := by
+  simp_rw [mapIncl, ← subtype_comp_inclusion _ _ hM, ← subtype_comp_inclusion _ _ hN, map_comp]
+  exact LinearMap.range_comp_le_range _ _
 
-/-- If `x : ι → M ⊗[R] N` are finitely many elements, then there exists a finitely generated
-submodule `N'` of `N` and `x' : ι → M ⊗[R] N'`, whose image in `M ⊗[R] N` is equal to `x`.
-There are 8 variants of this function. -/
-theorem exists_finite_submodule_right_of_fintype {ι : Type*} [Fintype ι] (x : ι → M ⊗[R] N) :
-    ∃ (N' : Submodule R N) (x' : ι → M ⊗[R] N'), Module.Finite R N' ∧
-    ∀ (i : ι), N'.subtype.lTensor M (x' i) = x i := by
-  obtain ⟨N', x', hfin, hx⟩ := exists_finite_submodule_left_of_fintype fun i ↦
-    TensorProduct.comm R M N (x i)
-  have key : N'.subtype.lTensor M ∘ₗ TensorProduct.comm R N' M =
-      (TensorProduct.comm R M N).symm ∘ₗ N'.subtype.rTensor M := ext' fun _ _ ↦ rfl
-  refine ⟨N', fun i ↦ TensorProduct.comm R N' M (x' i), hfin, fun i ↦ ?_⟩
-  replace hx := congr((TensorProduct.comm R M N).symm $(hx i))
-  rw [LinearEquiv.symm_apply_apply] at hx
-  simpa only [← hx] using congr($key (x' i))
+/-- For a finite subset `s` of `M ⊗[R] N`, there are finitely generated
+submodules `M'` and `N'` of `M` and `N`, respectively, such that `s` is contained in the image
+of `M' ⊗[R] N'` in `M ⊗[R] N`. -/
+theorem exists_finite_submodule_of_finite (s : Set (M ⊗[R] N)) (hs : s.Finite) :
+    ∃ (M' : Submodule R M) (N' : Submodule R N), Module.Finite R M' ∧ Module.Finite R N' ∧
+      s ⊆ LinearMap.range (mapIncl M' N') := by
+  simp_rw [Module.Finite.iff_fg]
+  refine hs.induction_on ⟨_, _, fg_bot, fg_bot, Set.empty_subset _⟩ ?_
+  rintro a s - - ⟨M', N', hM', hN', h⟩
+  refine TensorProduct.induction_on a ?_ (fun x y ↦ ?_) fun x y hx hy ↦ ?_
+  · exact ⟨M', N', hM', hN', Set.insert_subset (zero_mem _) h⟩
+  · refine ⟨_, _, hM'.sup (fg_span_singleton x),
+      hN'.sup (fg_span_singleton y), Set.insert_subset ?_ fun z hz ↦ ?_⟩
+    · exact ⟨⟨x, mem_sup_right (mem_span_singleton_self x)⟩ ⊗ₜ
+        ⟨y, mem_sup_right (mem_span_singleton_self y)⟩, rfl⟩
+    · exact range_mapIncl_mono le_sup_left le_sup_left (h hz)
+  · obtain ⟨M₁', N₁', hM₁', hN₁', h₁⟩ := hx
+    obtain ⟨M₂', N₂', hM₂', hN₂', h₂⟩ := hy
+    refine ⟨_, _, hM₁'.sup hM₂', hN₁'.sup hN₂', Set.insert_subset (add_mem ?_ ?_) fun z hz ↦ ?_⟩
+    · exact range_mapIncl_mono le_sup_left le_sup_left (h₁ (Set.mem_insert x s))
+    · exact range_mapIncl_mono le_sup_right le_sup_right (h₂ (Set.mem_insert y s))
+    · exact range_mapIncl_mono le_sup_left le_sup_left (h₁ (Set.subset_insert x s hz))
 
-/-- If `x : ι → M ⊗[R] N` are finitely many elements, then there are finitely generated
-submodules `M'` and `N'` of `M` and `N`, respectively, and `x' : ι → M' ⊗[R] N'`,
-whose image in `M ⊗[R] N` is equal to `x`. There are 8 variants of this function. -/
-theorem exists_finite_submodule_of_fintype {ι : Type*} [Fintype ι] (x : ι → M ⊗[R] N) :
-    ∃ (M' : Submodule R M) (N' : Submodule R N) (x' : ι → M' ⊗[R] N'),
-    Module.Finite R M' ∧ Module.Finite R N' ∧
-    ∀ (i : ι), TensorProduct.map M'.subtype N'.subtype (x' i) = x i := by
-  obtain ⟨M', x', h1, hx⟩ := exists_finite_submodule_left_of_fintype x
-  obtain ⟨N', x'', h2, hx'⟩ := exists_finite_submodule_right_of_fintype x'
-  refine ⟨M', N', x'', h1, h2, fun i ↦ ?_⟩
-  rw [← hx, ← hx', ← LinearMap.rTensor_comp_lTensor]; rfl
+/-- For a finite subset `s` of `M ⊗[R] N`, there exists a finitely generated
+submodule `M'` of `M`, such that `s` is contained in the image
+of `M' ⊗[R] N` in `M ⊗[R] N`. -/
+theorem exists_finite_submodule_left_of_finite (s : Set (M ⊗[R] N)) (hs : s.Finite) :
+    ∃ (M' : Submodule R M), Module.Finite R M' ∧
+      s ⊆ LinearMap.range (M'.subtype.rTensor N) := by
+  obtain ⟨M', _, hfin, _, h⟩ := exists_finite_submodule_of_finite s hs
+  refine ⟨M', hfin, ?_⟩
+  rw [mapIncl, ← LinearMap.rTensor_comp_lTensor] at h
+  exact h.trans (LinearMap.range_comp_le_range _ _)
 
-theorem exists_finite_submodule_left_of_fintype' {ι : Type*} [Fintype ι] (x : ι → M₁ ⊗[R] N₁) :
-    ∃ (M' : Submodule R M) (x' : ι → M' ⊗[R] N₁) (hM : M' ≤ M₁), Module.Finite R M' ∧
-    ∀ (i : ι), (inclusion hM).rTensor N₁ (x' i) = x i := by
-  obtain ⟨M', x', hfin, hx⟩ := exists_finite_submodule_left_of_fintype x
-  refine ⟨_, (M₁.subtype.submoduleMap M').rTensor N₁ ∘ x', map_subtype_le _ _, .map _ _, fun i ↦ ?_⟩
-  have key : M'.subtype.rTensor N₁ =
-      (inclusion (map_subtype_le _ _)).rTensor N₁ ∘ₗ (M₁.subtype.submoduleMap M').rTensor N₁ :=
-    ext' fun _ _ ↦ rfl
-  rw [← hx, key]; rfl
+/-- For a finite subset `s` of `M ⊗[R] N`, there exists a finitely generated
+submodule `N'` of `N`, such that `s` is contained in the image
+of `M ⊗[R] N'` in `M ⊗[R] N`. -/
+theorem exists_finite_submodule_right_of_finite (s : Set (M ⊗[R] N)) (hs : s.Finite) :
+    ∃ (N' : Submodule R N), Module.Finite R N' ∧
+      s ⊆ LinearMap.range (N'.subtype.lTensor M) := by
+  obtain ⟨_, N', _, hfin, h⟩ := exists_finite_submodule_of_finite s hs
+  refine ⟨N', hfin, ?_⟩
+  rw [mapIncl, ← LinearMap.lTensor_comp_rTensor] at h
+  exact h.trans (LinearMap.range_comp_le_range _ _)
 
-theorem exists_finite_submodule_right_of_fintype' {ι : Type*} [Fintype ι] (x : ι → M₁ ⊗[R] N₁) :
-    ∃ (N' : Submodule R N) (x' : ι → M₁ ⊗[R] N') (hN : N' ≤ N₁), Module.Finite R N' ∧
-    ∀ (i : ι), (inclusion hN).lTensor M₁ (x' i) = x i := by
-  obtain ⟨N', x', hfin, hx⟩ := exists_finite_submodule_right_of_fintype x
-  refine ⟨_, (N₁.subtype.submoduleMap N').lTensor M₁ ∘ x', map_subtype_le _ _, .map _ _, fun i ↦ ?_⟩
-  have key : N'.subtype.lTensor M₁ =
-      (inclusion (map_subtype_le _ _)).lTensor M₁ ∘ₗ (N₁.subtype.submoduleMap N').lTensor M₁ :=
-    ext' fun _ _ ↦ rfl
-  rw [← hx, key]; rfl
+/-- Variation of `TensorProduct.exists_finite_submodule_of_finite` where `M` and `N` are
+already submodules. -/
+theorem exists_finite_submodule_of_finite' (s : Set (M₁ ⊗[R] N₁)) (hs : s.Finite) :
+    ∃ (M' : Submodule R M) (N' : Submodule R N) (hM : M' ≤ M₁) (hN : N' ≤ N₁),
+      Module.Finite R M' ∧ Module.Finite R N' ∧
+        s ⊆ LinearMap.range (TensorProduct.map (inclusion hM) (inclusion hN)) := by
+  obtain ⟨M', N', _, _, h⟩ := exists_finite_submodule_of_finite s hs
+  have hM := map_subtype_le M₁ M'
+  have hN := map_subtype_le N₁ N'
+  refine ⟨_, _, hM, hN, .map _ _, .map _ _, ?_⟩
+  rw [mapIncl, show M'.subtype = inclusion hM ∘ₗ M₁.subtype.submoduleMap M' from rfl,
+    show N'.subtype = inclusion hN ∘ₗ N₁.subtype.submoduleMap N' from rfl, map_comp] at h
+  exact h.trans (LinearMap.range_comp_le_range _ _)
 
-theorem exists_finite_submodule_of_fintype' {ι : Type*} [Fintype ι] (x : ι → M₁ ⊗[R] N₁) :
-    ∃ (M' : Submodule R M) (N' : Submodule R N) (x' : ι → M' ⊗[R] N') (hM : M' ≤ M₁) (hN : N' ≤ N₁),
-    Module.Finite R M' ∧ Module.Finite R N' ∧
-    ∀ (i : ι), TensorProduct.map (inclusion hM) (inclusion hN) (x' i) = x i := by
-  obtain ⟨M', x', hM, h1, hx⟩ := exists_finite_submodule_left_of_fintype' x
-  obtain ⟨N', x'', hN, h2, hx'⟩ := exists_finite_submodule_right_of_fintype' x'
-  refine ⟨M', N', x'', hM, hN, h1, h2, fun i ↦ ?_⟩
-  rw [← hx, ← hx', ← LinearMap.rTensor_comp_lTensor]; rfl
+/-- Variation of `TensorProduct.exists_finite_submodule_left_of_finite` where `M` and `N` are
+already submodules. -/
+theorem exists_finite_submodule_left_of_finite' (s : Set (M₁ ⊗[R] N₁)) (hs : s.Finite) :
+    ∃ (M' : Submodule R M) (hM : M' ≤ M₁), Module.Finite R M' ∧
+      s ⊆ LinearMap.range ((inclusion hM).rTensor N₁) := by
+  obtain ⟨M', _, hM, _, hfin, _, h⟩ := exists_finite_submodule_of_finite' s hs
+  refine ⟨M', hM, hfin, ?_⟩
+  rw [← LinearMap.rTensor_comp_lTensor] at h
+  exact h.trans (LinearMap.range_comp_le_range _ _)
 
-theorem exists_finite_submodule_left_of_finsupp {ι : Type*} (x : ι →₀ M ⊗[R] N) :
-    ∃ (M' : Submodule R M) (x' : ι →₀ M' ⊗[R] N), Module.Finite R M' ∧
-    ∀ (i : ι), M'.subtype.rTensor N (x' i) = x i := by
-  classical
-  obtain ⟨M', x', hfin, hx⟩ := exists_finite_submodule_left_of_fintype fun i : x.support ↦ x i
-  refine ⟨M', (Finsupp.equivFunOnFinite.symm x').extendDomain, hfin, fun i ↦ ?_⟩
-  by_cases h : i ∈ x.support
-  · simp [h, hx]
-  simp [h, Finsupp.not_mem_support_iff.1 h]
-
-theorem exists_finite_submodule_right_of_finsupp {ι : Type*} (x : ι →₀ M ⊗[R] N) :
-    ∃ (N' : Submodule R N) (x' : ι →₀ M ⊗[R] N'), Module.Finite R N' ∧
-    ∀ (i : ι), N'.subtype.lTensor M (x' i) = x i := by
-  classical
-  obtain ⟨N', x', hfin, hx⟩ := exists_finite_submodule_right_of_fintype fun i : x.support ↦ x i
-  refine ⟨N', (Finsupp.equivFunOnFinite.symm x').extendDomain, hfin, fun i ↦ ?_⟩
-  by_cases h : i ∈ x.support
-  · simp [h, hx]
-  simp [h, Finsupp.not_mem_support_iff.1 h]
-
-theorem exists_finite_submodule_of_finsupp {ι : Type*} (x : ι →₀ M ⊗[R] N) :
-    ∃ (M' : Submodule R M) (N' : Submodule R N) (x' : ι →₀ M' ⊗[R] N'),
-    Module.Finite R M' ∧ Module.Finite R N' ∧
-    ∀ (i : ι), TensorProduct.map M'.subtype N'.subtype (x' i) = x i := by
-  obtain ⟨M', x', h1, hx⟩ := exists_finite_submodule_left_of_finsupp x
-  obtain ⟨N', x'', h2, hx'⟩ := exists_finite_submodule_right_of_finsupp x'
-  refine ⟨M', N', x'', h1, h2, fun i ↦ ?_⟩
-  rw [← hx, ← hx', ← LinearMap.rTensor_comp_lTensor]; rfl
-
-theorem exists_finite_submodule_left_of_finsupp' {ι : Type*} (x : ι →₀ M₁ ⊗[R] N₁) :
-    ∃ (M' : Submodule R M) (x' : ι →₀ M' ⊗[R] N₁) (hM : M' ≤ M₁), Module.Finite R M' ∧
-    ∀ (i : ι), (inclusion hM).rTensor N₁ (x' i) = x i := by
-  classical
-  obtain ⟨M', x', hM, hfin, hx⟩ := exists_finite_submodule_left_of_fintype' fun i : x.support ↦ x i
-  refine ⟨M', (Finsupp.equivFunOnFinite.symm x').extendDomain, hM, hfin, fun i ↦ ?_⟩
-  by_cases h : i ∈ x.support
-  · simp [h, hx]
-  simp [h, Finsupp.not_mem_support_iff.1 h]
-
-theorem exists_finite_submodule_right_of_finsupp' {ι : Type*} (x : ι →₀ M₁ ⊗[R] N₁) :
-    ∃ (N' : Submodule R N) (x' : ι →₀ M₁ ⊗[R] N') (hN : N' ≤ N₁), Module.Finite R N' ∧
-    ∀ (i : ι), (inclusion hN).lTensor M₁ (x' i) = x i := by
-  classical
-  obtain ⟨N', x', hN, hfin, hx⟩ := exists_finite_submodule_right_of_fintype' fun i : x.support ↦ x i
-  refine ⟨N', (Finsupp.equivFunOnFinite.symm x').extendDomain, hN, hfin, fun i ↦ ?_⟩
-  by_cases h : i ∈ x.support
-  · simp [h, hx]
-  simp [h, Finsupp.not_mem_support_iff.1 h]
-
-theorem exists_finite_submodule_of_finsupp' {ι : Type*} (x : ι →₀ M₁ ⊗[R] N₁) :
-    ∃ (M' : Submodule R M) (N' : Submodule R N) (x' : ι →₀ M' ⊗[R] N')
-    (hM : M' ≤ M₁) (hN : N' ≤ N₁), Module.Finite R M' ∧ Module.Finite R N' ∧
-    ∀ (i : ι), TensorProduct.map (inclusion hM) (inclusion hN) (x' i) = x i := by
-  obtain ⟨M', x', hM, h1, hx⟩ := exists_finite_submodule_left_of_finsupp' x
-  obtain ⟨N', x'', hN, h2, hx'⟩ := exists_finite_submodule_right_of_finsupp' x'
-  refine ⟨M', N', x'', hM, hN, h1, h2, fun i ↦ ?_⟩
-  rw [← hx, ← hx', ← LinearMap.rTensor_comp_lTensor]; rfl
-
-theorem exists_finite_submodule_left_of_pair (x y : M ⊗[R] N) :
-    ∃ (M' : Submodule R M) (x' y' : M' ⊗[R] N), Module.Finite R M' ∧
-    M'.subtype.rTensor N x' = x ∧ M'.subtype.rTensor N y' = y := by
-  obtain ⟨M', x', hfin, hx⟩ := exists_finite_submodule_left_of_fintype ![x, y]
-  exact ⟨M', x' 0, x' 1, hfin, hx 0, hx 1⟩
-
-theorem exists_finite_submodule_right_of_pair (x y : M ⊗[R] N) :
-    ∃ (N' : Submodule R N) (x' y' : M ⊗[R] N'), Module.Finite R N' ∧
-    N'.subtype.lTensor M x' = x ∧ N'.subtype.lTensor M y' = y := by
-  obtain ⟨N', x', hfin, hx⟩ := exists_finite_submodule_right_of_fintype ![x, y]
-  exact ⟨N', x' 0, x' 1, hfin, hx 0, hx 1⟩
-
-theorem exists_finite_submodule_of_pair (x y : M ⊗[R] N) :
-    ∃ (M' : Submodule R M) (N' : Submodule R N) (x' y' : M' ⊗[R] N'),
-    Module.Finite R M' ∧ Module.Finite R N' ∧
-    TensorProduct.map M'.subtype N'.subtype x' = x ∧
-    TensorProduct.map M'.subtype N'.subtype y' = y := by
-  obtain ⟨M', N', x', h1, h2, hx⟩ := exists_finite_submodule_of_fintype ![x, y]
-  exact ⟨M', N', x' 0, x' 1, h1, h2, hx 0, hx 1⟩
-
-theorem exists_finite_submodule_left_of_pair' (x y : M₁ ⊗[R] N₁) :
-    ∃ (M' : Submodule R M) (x' y' : M' ⊗[R] N₁) (hM : M' ≤ M₁), Module.Finite R M' ∧
-    (inclusion hM).rTensor N₁ x' = x ∧ (inclusion hM).rTensor N₁ y' = y := by
-  obtain ⟨M', x', hM, hfin, hx⟩ := exists_finite_submodule_left_of_fintype' ![x, y]
-  exact ⟨M', x' 0, x' 1, hM, hfin, hx 0, hx 1⟩
-
-theorem exists_finite_submodule_right_of_pair' (x y : M₁ ⊗[R] N₁) :
-    ∃ (N' : Submodule R N) (x' y' : M₁ ⊗[R] N') (hN : N' ≤ N₁), Module.Finite R N' ∧
-    (inclusion hN).lTensor M₁ x' = x ∧ (inclusion hN).lTensor M₁ y' = y := by
-  obtain ⟨N', x', hN, hfin, hx⟩ := exists_finite_submodule_right_of_fintype' ![x, y]
-  exact ⟨N', x' 0, x' 1, hN, hfin, hx 0, hx 1⟩
-
-theorem exists_finite_submodule_of_pair' (x y : M₁ ⊗[R] N₁) :
-    ∃ (M' : Submodule R M) (N' : Submodule R N) (x' y' : M' ⊗[R] N') (hM : M' ≤ M₁) (hN : N' ≤ N₁),
-    Module.Finite R M' ∧ Module.Finite R N' ∧
-    TensorProduct.map (inclusion hM) (inclusion hN) x' = x ∧
-    TensorProduct.map (inclusion hM) (inclusion hN) y' = y := by
-  obtain ⟨M', N', x', hM, hN, h1, h2, hx⟩ := exists_finite_submodule_of_fintype' ![x, y]
-  exact ⟨M', N', x' 0, x' 1, hM, hN, h1, h2, hx 0, hx 1⟩
-
-theorem exists_finite_submodule_left (x : M ⊗[R] N) :
-    ∃ (M' : Submodule R M) (x' : M' ⊗[R] N), Module.Finite R M' ∧
-    M'.subtype.rTensor N x' = x := by
-  obtain ⟨M', x', hfin, hx⟩ := exists_finite_submodule_left_of_fintype ![x]
-  exact ⟨M', x' 0, hfin, hx 0⟩
-
-theorem exists_finite_submodule_right (x : M ⊗[R] N) :
-    ∃ (N' : Submodule R N) (x' : M ⊗[R] N'), Module.Finite R N' ∧
-    N'.subtype.lTensor M x' = x := by
-  obtain ⟨N', x', hfin, hx⟩ := exists_finite_submodule_right_of_fintype ![x]
-  exact ⟨N', x' 0, hfin, hx 0⟩
-
-theorem exists_finite_submodule (x : M ⊗[R] N) :
-    ∃ (M' : Submodule R M) (N' : Submodule R N) (x' : M' ⊗[R] N'),
-    Module.Finite R M' ∧ Module.Finite R N' ∧
-    TensorProduct.map M'.subtype N'.subtype x' = x := by
-  obtain ⟨M', N', x', h1, h2, hx⟩ := exists_finite_submodule_of_fintype ![x]
-  exact ⟨M', N', x' 0, h1, h2, hx 0⟩
-
-theorem exists_finite_submodule_left' (x : M₁ ⊗[R] N₁) :
-    ∃ (M' : Submodule R M) (x' : M' ⊗[R] N₁) (hM : M' ≤ M₁), Module.Finite R M' ∧
-    (inclusion hM).rTensor N₁ x' = x := by
-  obtain ⟨M', x', hM, hfin, hx⟩ := exists_finite_submodule_left_of_fintype' ![x]
-  exact ⟨M', x' 0, hM, hfin, hx 0⟩
-
-theorem exists_finite_submodule_right' (x : M₁ ⊗[R] N₁) :
-    ∃ (N' : Submodule R N) (x' : M₁ ⊗[R] N') (hN : N' ≤ N₁), Module.Finite R N' ∧
-    (inclusion hN).lTensor M₁ x' = x := by
-  obtain ⟨N', x', hN, hfin, hx⟩ := exists_finite_submodule_right_of_fintype' ![x]
-  exact ⟨N', x' 0, hN, hfin, hx 0⟩
-
-theorem exists_finite_submodule' (x : M₁ ⊗[R] N₁) :
-    ∃ (M' : Submodule R M) (N' : Submodule R N) (x' : M' ⊗[R] N') (hM : M' ≤ M₁) (hN : N' ≤ N₁),
-    Module.Finite R M' ∧ Module.Finite R N' ∧
-    TensorProduct.map (inclusion hM) (inclusion hN) x' = x := by
-  obtain ⟨M', N', x', hM, hN, h1, h2, hx⟩ := exists_finite_submodule_of_fintype' ![x]
-  exact ⟨M', N', x' 0, hM, hN, h1, h2, hx 0⟩
+/-- Variation of `TensorProduct.exists_finite_submodule_right_of_finite` where `M` and `N` are
+already submodules. -/
+theorem exists_finite_submodule_right_of_finite' (s : Set (M₁ ⊗[R] N₁)) (hs : s.Finite) :
+    ∃ (N' : Submodule R N) (hN : N' ≤ N₁), Module.Finite R N' ∧
+      s ⊆ LinearMap.range ((inclusion hN).lTensor M₁) := by
+  obtain ⟨_, N', _, hN, _, hfin, h⟩ := exists_finite_submodule_of_finite' s hs
+  refine ⟨N', hN, hfin, ?_⟩
+  rw [← LinearMap.lTensor_comp_rTensor] at h
+  exact h.trans (LinearMap.range_comp_le_range _ _)
 
 end TensorProduct
