e7abd4b87685b161d128eaa5794c4d25b4c46da7
Lucas Whitfield <lucaswhitfield@hotmail.com>
2024-04-05 16:34:53 +0200

    feat(RingTheory/Noetherian): nilpotent endomorphisms of finite modules

--- Mathlib/RingTheory/Noetherian.lean
+++ Mathlib/RingTheory/Noetherian.lean
@@ -619,3 +619,21 @@ theorem IsNoetherianRing.isNilpotent_nilradical (R : Type*) [CommRing R] [IsNoet
   obtain ⟨n, hn⟩ := Ideal.exists_radical_pow_le_of_fg (⊥ : Ideal R) (IsNoetherian.noetherian _)
   exact ⟨n, eq_bot_iff.mpr hn⟩
 #align is_noetherian_ring.is_nilpotent_nilradical IsNoetherianRing.isNilpotent_nilradical
+
+theorem isNilpotent_iff_of_Finite (R M : Type*) [CommRing R] [AddCommGroup M] [Module R M]
+    [Module.Finite R M] (f : M →ₗ[R] M) :
+    IsNilpotent f ↔ ∀ m : M, ∃ n : ℕ, (f^n) m = 0 := by
+  constructor
+  · rintro ⟨n, hn⟩ m
+    exact ⟨n, by simp [hn]⟩
+  · intro h
+    rcases Module.Finite.out (R := R) (M := M) with ⟨S, hS⟩
+    choose g hg using h
+    use Finset.sup S g
+    ext m
+    have hm : m ∈ Submodule.span R S := by simp [hS]
+    induction hm using Submodule.span_induction'
+    · next x hx => exact LinearMap.pow_map_zero_of_le (Finset.le_sup hx) (hg x)
+    · simp
+    · simp_all
+    · simp_all
