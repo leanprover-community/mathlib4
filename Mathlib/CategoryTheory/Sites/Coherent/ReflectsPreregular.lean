933b4ca2d0993ef3b2614de961e6f68f0990047a
Dagur Tómas Ásgeirsson <dagurtomas@gmail.com>
2024-04-05 20:14:48 +0000

    Update Mathlib/CategoryTheory/Sites/Coherent/ReflectsPreregular.lean
    Co-authored-by: Joël Riou <37772949+joelriou@users.noreply.github.com>

--- Mathlib/CategoryTheory/Sites/Coherent/ReflectsPreregular.lean
+++ Mathlib/CategoryTheory/Sites/Coherent/ReflectsPreregular.lean
@@ -26,7 +26,7 @@ lemma Functor.reflects_preregular : Preregular C where
   exists_fac f g _ := by
     obtain ⟨W, f', _, i, w⟩ := Preregular.exists_fac (F.map f) (F.map g)
     refine ⟨_, F.preimage (F.effectiveEpiOver W ≫ f'),
-      ⟨F.effectiveEpi_of_map _ ?_, F.preimage (F.effectiveEpiOver  W ≫ i), ?_⟩⟩
+      ⟨F.effectiveEpi_of_map _ ?_, F.preimage (F.effectiveEpiOver W ≫ i), ?_⟩⟩
     · simp only [Functor.image_preimage]
       infer_instance
     · apply F.map_injective
