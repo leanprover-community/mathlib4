8db6e4323e77346ac60126d646462f03fa8bd13b
damiano <adomani@gmail.com>
2024-04-05 23:39:45 +0200

    Update Mathlib/Util/CountHeartbeats.lean

--- Mathlib/Util/CountHeartbeats.lean
+++ Mathlib/Util/CountHeartbeats.lean
@@ -72,7 +72,7 @@ elab "count_heartbeats! " n:(num)? "in" ppLine cmd:command : command => do
            | some j => j.getNat
            | none => 10
   -- First run the command `n-1` times, reverting the state.
-  let counts ← (List.range (n - 1)).mapM fun _ => runTacForHeartbeats tac
+  let counts ← (List.range (n - 1)).mapM fun _ => runTacForHeartbeats cmd
   -- Then run once more, keeping the state.
   let counts := (← runTacForHeartbeats tac (revert := false)) :: counts
   logVariation counts
