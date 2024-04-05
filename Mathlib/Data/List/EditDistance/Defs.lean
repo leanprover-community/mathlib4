4e82e278f1ad7385014f97cfedf29d5bb2b22fc5
Scott Morrison <scott.morrison@gmail.com>
2024-04-05 11:35:05 +1100

    shake

--- Mathlib/Data/List/EditDistance/Defs.lean
+++ Mathlib/Data/List/EditDistance/Defs.lean
@@ -3,7 +3,7 @@ Copyright (c) 2023 Kim Liesinger. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kim Liesinger
 -/
-import Mathlib.Data.Nat.Monoid
+import Mathlib.Algebra.Group.Defs
 
 /-!
 # Levenshtein distances
--- Mathlib/Data/Nat/Monoid.lean
+++ Mathlib/Data/Nat/Monoid.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Floris van Doorn, Leonardo de Moura, Jeremy Avigad, Mario Carneiro
 -/
 import Mathlib.Algebra.Group.TypeTags
-import Mathlib.Algebra.GroupWithZero.Defs
 import Mathlib.Data.Nat.Cast.Defs
 
 #align_import data.nat.basic from "leanprover-community/mathlib"@"bd835ef554f37ef9b804f0903089211f89cb370b"
