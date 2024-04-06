3899eea2bd5d440fbe7a70523c14fcde26c2f3b3
Yaël Dillies <yael.dillies@gmail.com>
2024-04-06 15:08:51 +0000

    fix

--- Mathlib/Data/Multiset/Basic.lean
+++ Mathlib/Data/Multiset/Basic.lean
@@ -3,7 +3,6 @@ Copyright (c) 2015 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro
 -/
-import Mathlib.Algebra.GroupPower.Basic
 import Mathlib.Algebra.Order.Sub.Canonical
 import Mathlib.Data.List.Perm
 import Mathlib.Data.Nat.Basic
