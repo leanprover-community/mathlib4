0aaaee937e3db98f4c34235f944b863039a18122
Michael Rothgang <rothgami@math.hu-berlin.de>
2024-04-06 02:42:35 +0200

    Tweak my script, avoid false positives due to an old name being re-assigned as a new name (e.g. CommRing).

--- Mathlib/Util/lint_backticks_comments.py
+++ Mathlib/Util/lint_backticks_comments.py
@@ -69,9 +69,8 @@ if __name__ == '__main__':
             continue
         old_file = line[:line.find(' from ')]
         old_files.append(old_file)
-    # Read in all #align statements, parse the names of the old and new declaration;
-    # add any old declarations which were renamed.
-    old_declarations = []
+    # Read in all #align statements and parse the names of the old and new declaration.
+    aligns = dict()
     for line in open('/home/michael/all_aligns.txt', 'r', encoding='utf-8'):
         line = line.strip()
         if not line.startswith("#align "):
@@ -80,20 +79,29 @@ if __name__ == '__main__':
         if len(parts) != 3:
             continue
         _align, old_decl, new_decl = parts
-        if old_decl != new_decl:
-            old_declarations.append(old_decl)
-
+        aligns[old_decl] = new_decl
+    # For each old declaration, if the new declaration is different
+    # *and* there is no new declaration of the same name, add these to our list.
+    # We do this in two stages; a naive algorithm is extremely slow (quadratic?).
+    old_declarations = []
+    for old, new in aligns.items():
+        if old != new: # and old not in aligns.values():
+            old_declarations.append(old)
+    same = set(aligns.values()).intersection(set(old_declarations))
+    print(f"Found {len(same)} old declarations which occur as a different new declaration:\n{same}")
+    old_declarations = [s for s in old_declarations if s not in same]
+    assert "CommRing" not in old_declarations
     # we had all mathlib lemma names... no, we don't need these.
     # known_decls = []
     # for decl in open('/home/michael/first-10k.txt', 'r', encoding='utf-8'):
     #     known_decls.append(decl.strip())
-
     # Read the input file: flag any string in backticks which is exactly an old file or declaration.
     # TODO: a substring of the old declaration should also work, e.g. just the lemma name
     for line in open('/home/michael/geo-v2.txt', 'r', encoding='utf-8'):
-        s = extract_backticks(line.strip())
+        line=line.strip()
+        s = extract_backticks(line)
         if s is None:
-            print(f'what, what input did we get: "{line.strip()}"')
+            print(f'what, what input did we get: "{line}"')
             assert False # omit invalid input for now
         for item in s:
             if item in old_files:
