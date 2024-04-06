692f8a642d3d22e092d020db11a40909683d9dd8
grunweg <rothgami@math.hu-berlin.de>
2024-04-06 21:14:24 +0200

    Apply suggestions from code review
    Co-authored-by: Kyle Miller <kmill31415@gmail.com>

--- Mathlib/Tactic/Attr/Register.lean
+++ Mathlib/Tactic/Attr/Register.lean
@@ -81,5 +81,5 @@ discharge theorems about the trivial case (where we know `Subsingleton α` and m
 in e.g. groups are trivially true). -/
 register_simp_attr nontriviality
 
-/-- A stub attribute for `IsPoly`. -/
+/-- A stub attribute for `is_poly`. -/
 register_label_attr is_poly
