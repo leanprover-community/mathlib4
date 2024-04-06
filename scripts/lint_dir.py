78e72d7bab1dd34c5fa597371098d804fbb36c12
Michael Rothgang <rothgami@math.hu-berlin.de>
2024-04-06 22:15:11 +0200

    Allow excluding directories from the script.

--- scripts/lint_dir.py
+++ scripts/lint_dir.py
@@ -12,13 +12,14 @@ from pathlib import Path
 
 if __name__ == '__main__':
     # Parse the list of all files to lint, the the given directory.
-    dir = 'Util'
+    dir = ''
+    exclude = tuple(''.split(' '))
     assert '/' not in dir
-    print(f"about to lint all files in directory {dir}")
+    #print(f"about to lint all files in directory {dir}")
     files = []
     for line in open('Mathlib.lean', 'r', encoding='utf-8'):
         line = line[len('import Mathlib.'):].strip()
-        if line.startswith(dir):
+        if line.startswith(dir) and not line.startswith(exclude):
             files.append(line)
     for filename in files:
         path = f"Mathlib/{filename.replace('.', '/')}.lean"
