71947226d0db1a1b5aae3c6ab381b0075ca71afa
Yaël Dillies <yael.dillies@gmail.com>
2024-04-07 13:04:28 +0000

    unify proofs

--- Mathlib/Analysis/Complex/Angle.lean
+++ Mathlib/Analysis/Complex/Angle.lean
@@ -76,30 +76,9 @@ This section shows that the arc and chord distances between two unit complex num
 up to a factor of `π / 2`.
 -/
 
-/-- Chord-length is always less than arc-length. -/
-lemma norm_sub_le_angle (hx : ‖x‖ = 1) (hy : ‖y‖ = 1) : ‖x - y‖ ≤ angle x y := by
-  clear a
-  wlog h : y = 1
-  · have := @this (x / y) 1 (by simp only [norm_div, hx, hy, div_one]) norm_one rfl
-    rwa [angle_div_left_eq_angle_mul_right, div_sub_one, norm_div, hy, div_one, one_mul]
-      at this
-    rintro rfl
-    simp at hy
-  subst y
-  rw [norm_eq_abs, abs_eq_one_iff'] at hx
-  obtain ⟨θ, hθ, rfl⟩ := hx
-  rw [angle_exp_one, exp_mul_I, add_sub_right_comm, (toIocMod_eq_self _).2]
-  norm_cast
-  rw [norm_eq_abs, abs_add_mul_I, Real.sqrt_le_left (by positivity), ← _root_.abs_pow, abs_sq]
-  calc
-    _ = 2 * (1 - θ.cos) := by linear_combination θ.cos_sq_add_sin_sq
-    _ ≤ 2 * (1 - (1 - θ ^ 2 / 2)) := by gcongr; exact Real.one_sub_sq_div_two_le_cos
-    _ = _ := by ring
-  · convert hθ
-    ring
-
-/-- Chord-length is always greater than a multiple of arc-length. -/
-lemma mul_angle_le_norm_sub (hx : ‖x‖ = 1) (hy : ‖y‖ = 1) : 2 / π * angle x y ≤ ‖x - y‖ := by
+/-- Chord-length is a multiple of arc-length up to constants. -/
+lemma norm_sub_mem_Icc_angle (hx : ‖x‖ = 1) (hy : ‖y‖ = 1) :
+    ‖x - y‖ ∈ Icc (2 / π * angle x y) (angle x y) := by
   clear a
   wlog h : y = 1
   · have := @this (x / y) 1 (by simp only [norm_div, hx, hy, div_one]) norm_one rfl
@@ -113,15 +92,28 @@ lemma mul_angle_le_norm_sub (hx : ‖x‖ = 1) (hy : ‖y‖ = 1) : 2 / π * ang
   rw [angle_exp_one, exp_mul_I, add_sub_right_comm, (toIocMod_eq_self _).2]
   norm_cast
   rw [norm_eq_abs, abs_add_mul_I]
-  refine Real.le_sqrt_of_sq_le ?_
-  rw [mul_pow, ← _root_.abs_pow, abs_sq]
-  calc
-    _ = 2 * (1 - (1 - 2 / π ^ 2 * θ ^ 2)) := by ring
-    _ ≤ 2 * (1 - θ.cos) := by
-        gcongr; exact Real.cos_quadratic_upper_bound <| abs_le.2 <| Ioc_subset_Icc_self hθ
-    _  = _ := by linear_combination -θ.cos_sq_add_sin_sq
+  refine ⟨Real.le_sqrt_of_sq_le ?_, ?_⟩
+  · rw [mul_pow, ← _root_.abs_pow, abs_sq]
+    calc
+      _ = 2 * (1 - (1 - 2 / π ^ 2 * θ ^ 2)) := by ring
+      _ ≤ 2 * (1 - θ.cos) := by
+          gcongr; exact Real.cos_quadratic_upper_bound <| abs_le.2 <| Ioc_subset_Icc_self hθ
+      _  = _ := by linear_combination -θ.cos_sq_add_sin_sq
+  · rw [Real.sqrt_le_left (by positivity), ← _root_.abs_pow, abs_sq]
+    calc
+      _ = 2 * (1 - θ.cos) := by linear_combination θ.cos_sq_add_sin_sq
+      _ ≤ 2 * (1 - (1 - θ ^ 2 / 2)) := by gcongr; exact Real.one_sub_sq_div_two_le_cos
+      _ = _ := by ring
   · convert hθ
     ring
+
+/-- Chord-length is always less than arc-length. -/
+lemma norm_sub_le_angle (hx : ‖x‖ = 1) (hy : ‖y‖ = 1) : ‖x - y‖ ≤ angle x y :=
+  (norm_sub_mem_Icc_angle hx hy).2
+
+/-- Chord-length is always greater than a multiple of arc-length. -/
+lemma mul_angle_le_norm_sub (hx : ‖x‖ = 1) (hy : ‖y‖ = 1) : 2 / π * angle x y ≤ ‖x - y‖ :=
+  (norm_sub_mem_Icc_angle hx hy).1
 
 /-- Arc-length is always less than a multiple of chord-length. -/
 lemma angle_le_mul_norm_sub (hx : ‖x‖ = 1) (hy : ‖y‖ = 1) : angle x y ≤ π / 2 * ‖x - y‖ := by
