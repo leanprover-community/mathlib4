945b28577f813b556ec3c679d50473908f1fd42e
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2024-04-07 02:24:52 +0200

    minimize import

--- Mathlib/Lean/Meta/RefinedDiscrTree.lean
+++ Mathlib/Lean/Meta/RefinedDiscrTree.lean
@@ -3,7 +3,7 @@ Copyright (c) 2023 Jovan Gerbscheid. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jovan Gerbscheid
 -/
-import Lean.Meta
+import Lean.Meta.DiscrTree
 import Std.Data.List.Basic
 import Mathlib.Lean.Meta.RefinedDiscrTree.StateList
 
