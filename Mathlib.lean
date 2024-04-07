2f5e40bfad5a9012702f409e81ff73c488bf4ae2
101damnations <101damnations@github.com>
2024-04-07 03:48:35 +0100

    add Coalgebra.TensorProduct

--- Mathlib.lean
+++ Mathlib.lean
@@ -3289,6 +3289,7 @@ import Mathlib.RingTheory.ClassGroup
 import Mathlib.RingTheory.Coalgebra.Basic
 import Mathlib.RingTheory.Coalgebra.Equiv
 import Mathlib.RingTheory.Coalgebra.Hom
+import Mathlib.RingTheory.Coalgebra.TensorProduct
 import Mathlib.RingTheory.Complex
 import Mathlib.RingTheory.Congruence
 import Mathlib.RingTheory.Coprime.Basic
