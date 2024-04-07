1fcc751490843d9aacc53b2b0d1a82c1ffab3fc6
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2024-04-07 10:56:23 +0200

    revert

--- Mathlib/Data/Ordmap/Ordnode.lean
+++ Mathlib/Data/Ordmap/Ordnode.lean
@@ -6,6 +6,7 @@ Authors: Mario Carneiro
 import Mathlib.Order.Compare
 import Mathlib.Data.List.Defs
 import Mathlib.Data.Nat.PSub
+import Mathlib.Data.Option.Basic
 
 #align_import data.ordmap.ordnode from "leanprover-community/mathlib"@"dd71334db81d0bd444af1ee339a29298bef40734"
 
