089b855bb173a91bf65d182b40d8300c36a67a2f
Lucas Whitfield <lucaswhitfield@hotmail.com>
2024-04-08 11:08:04 +0200

    Update according to @ocfnash 's nitpicks

--- Mathlib/RingTheory/Finiteness.lean
+++ Mathlib/RingTheory/Finiteness.lean
@@ -668,23 +668,20 @@ instance span_finset (s : Finset M) : Module.Finite R (span R (s : Set M)) :=
   ⟨(Submodule.fg_top _).mpr ⟨s, rfl⟩⟩
 
 
-theorem Module.End.isNilpotent_iff_of_finite {R M : Type*} [CommRing R] [AddCommGroup M]
-    [Module R M] [Module.Finite R M] {f : M →ₗ[R] M} :
-    IsNilpotent f ↔ ∀ m : M, ∃ n : ℕ, (f^n) m = 0 := by
-  constructor
-  · rintro ⟨n, hn⟩ m
-    exact ⟨n, by simp [hn]⟩
-  · intro h
-    rcases Module.Finite.out (R := R) (M := M) with ⟨S, hS⟩
-    choose g hg using h
-    use Finset.sup S g
-    ext m
-    have hm : m ∈ Submodule.span R S := by simp [hS]
-    induction hm using Submodule.span_induction'
-    · next x hx => exact LinearMap.pow_map_zero_of_le (Finset.le_sup hx) (hg x)
-    · simp
-    · simp_all
-    · simp_all
+theorem Module.End.isNilpotent_iff_of_finite {R M : Type*} [CommSemiring R] [AddCommMonoid M]
+    [Module R M] [Module.Finite R M] {f : End R M} :
+    IsNilpotent f ↔ ∀ m : M, ∃ n : ℕ, (f ^ n) m = 0 := by
+  refine ⟨fun ⟨n, hn⟩ m ↦ ⟨n, by simp [hn]⟩, fun h ↦ ?_⟩
+  rcases Module.Finite.out (R := R) (M := M) with ⟨S, hS⟩
+  choose g hg using h
+  use Finset.sup S g
+  ext m
+  have hm : m ∈ Submodule.span R S := by simp [hS]
+  induction hm using Submodule.span_induction'
+  · next x hx => exact LinearMap.pow_map_zero_of_le (Finset.le_sup hx) (hg x)
+  · simp
+  · simp_all
+  · simp_all
 
 variable {R}
 
