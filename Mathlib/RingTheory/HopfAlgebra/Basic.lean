300e592eec3b55757f468fe99103f429b7db3ea2
101damnations <101damnations@github.com>
2024-04-08 02:39:00 +0100

    lol whoops, undelete Hopf algebras

--- Mathlib/RingTheory/HopfAlgebra/Basic.lean
+++ Mathlib/RingTheory/HopfAlgebra/Basic.lean
@@ -1,41 +1,92 @@
 /-
-Copyright (c) 2024 Amelia Livingston. All rights reserved.
+Copyright (c) 2024 Ali Ramsey. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Amelia Livingston
+Authors: Ali Ramsey
 -/
-import Mathlib.RingTheory.HopfAlgebra.Basic
-import Mathlib.RingTheory.Bialgebra.TensorProduct
+import Mathlib.RingTheory.Bialgebra.Basic
 
 /-!
-# Tensor products of Hopf algebras
+# Hopf algebras
 
-We define the Hopf algebra instance on the tensor product of two Hopf algebras.
+In this file we define `HopfAlgebra`, and provide instances for:
+
+* Commutative semirings: `CommSemiring.toHopfAlgebra`
+
+# Main definitions
+
+* `HopfAlgebra R A` : the Hopf algebra structure on an `R`-bialgebra `A`.
+* `HopfAlgebra.antipode` : The `R`-linear map `A →ₗ[R] A`.
+
+## TODO
+
+* Uniqueness of Hopf algebra structure on a bialgebra (i.e. if the algebra and coalgebra structures
+agree then the antipodes must also agree).
+
+* `antipode 1 = 1` and `antipode (a * b) = antipode b * antipode a`, so in particular if `A` is
+  commutative then `antipode` is an algebra homomorphism.
+
+* If `A` is commutative then `antipode` is necessarily a bijection and its square is
+  the identity.
+
+## References
+
+* <https://en.wikipedia.org/wiki/Hopf_algebra>
+
+* [C. Kassel, *Quantum Groups* (§III.3)][Kassel1995]
+
 
 -/
 
-universe u
-namespace TensorProduct
-variable {R A B : Type u} [CommRing R] [Ring A] [Ring B] [HopfAlgebra R A] [HopfAlgebra R B]
+suppress_compilation
+
+universe u v
+
+/-- A Hopf algebra over a commutative (semi)ring `R` is a bialgebra over `R` equipped with an
+`R`-linear endomorphism `antipode` satisfying the antipode axioms. -/
+class HopfAlgebra (R : Type u) (A : Type v) [CommSemiring R] [Semiring A] extends
+    Bialgebra R A where
+  /-- The antipode of the Hopf algebra. -/
+  antipode : A →ₗ[R] A
+  /-- One of the antipode axioms for a Hopf algebra. -/
+  mul_antipode_rTensor_comul :
+    LinearMap.mul' R A ∘ₗ antipode.rTensor A ∘ₗ comul = (Algebra.linearMap R A) ∘ₗ counit
+  /-- One of the antipode axioms for a Hopf algebra. -/
+  mul_antipode_lTensor_comul :
+    LinearMap.mul' R A ∘ₗ antipode.lTensor A ∘ₗ comul = (Algebra.linearMap R A) ∘ₗ counit
+
+namespace HopfAlgebra
+
+variable {R : Type u} {A : Type v} [CommSemiring R] [Semiring A] [HopfAlgebra R A]
+
+@[simp]
+theorem mul_antipode_rTensor_comul_apply (a : A) :
+    LinearMap.mul' R A (antipode.rTensor A (Coalgebra.comul a)) =
+    algebraMap R A (Coalgebra.counit a) :=
+  LinearMap.congr_fun mul_antipode_rTensor_comul a
+
+@[simp]
+theorem mul_antipode_lTensor_comul_apply (a : A) :
+    LinearMap.mul' R A (antipode.lTensor A (Coalgebra.comul a)) =
+    algebraMap R A (Coalgebra.counit a) :=
+  LinearMap.congr_fun mul_antipode_lTensor_comul a
+
+end HopfAlgebra
+
+section CommSemiring
+
+variable (R : Type u) [CommSemiring R]
 
 open HopfAlgebra
 
-noncomputable instance instHopfAlgebra : HopfAlgebra R (A ⊗[R] B) :=
-  { antipode := map antipode antipode
-    mul_antipode_rTensor_comul := by
-      simp only [instCoalgebraStruct_comul, instCoalgebraStruct_counit, LinearMap.rTensor_def,
-        ← map_id, ← LinearMap.comp_assoc (map _ _), ← tensorTensorTensorComm_comp_map,
-        Algebra.mul'_comp_tensorTensorTensorComm, ← map_comp]
-      simp only [← rTensor_def, LinearMap.comp_assoc, mul_antipode_rTensor_comul, map_comp,
-        Algebra.linearMap_comp_mul']
-    mul_antipode_lTensor_comul := by
-      simp only [instCoalgebraStruct_comul, instCoalgebraStruct_counit, LinearMap.lTensor_def,
-        ← map_id, ← LinearMap.comp_assoc (map _ _), ← tensorTensorTensorComm_comp_map,
-        Algebra.mul'_comp_tensorTensorTensorComm, ← map_comp]
-      simp only [← lTensor_def, LinearMap.comp_assoc, mul_antipode_lTensor_comul, map_comp,
-        Algebra.linearMap_comp_mul'] }
+namespace CommSemiring
+
+/-- Every commutative (semi)ring is a Hopf algebra over itself -/
+instance toHopfAlgebra : HopfAlgebra R R where
+  antipode := .id
+  mul_antipode_rTensor_comul := by ext; simp
+  mul_antipode_lTensor_comul := by ext; simp
 
 @[simp]
-theorem antipode_def :
-    HopfAlgebra.antipode (R := R) (A := A ⊗[R] B) = map antipode antipode := rfl
+theorem antipode_eq_id : antipode (R := R) (A := R) = .id := rfl
 
-end TensorProduct
+end CommSemiring
