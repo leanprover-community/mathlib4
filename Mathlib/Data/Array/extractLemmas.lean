f87916d2bd0c458c88ab58c109061eb52418c922
Hydrogenbear <hydrogenbear@gmail.com>
2024-04-08 14:45:13 +0800

    Almost rewrite everything based on ext

--- Mathlib/Data/Array/extractLemmas.lean
+++ Mathlib/Data/Array/extractLemmas.lean
@@ -13,10 +13,6 @@ set_option autoImplicit true
 
 namespace Array
 
-theorem extract_overflow_eq_extract_to_stop {a : Array α} :
-    (a.size ≤ l) →  a.extract p l = a.extract p a.size := by
-  intro h
-  simp only [extract, Nat.min_eq_right h, Nat.sub_eq, mkEmpty_eq, Nat.min_self]
 
 @[simp]
 theorem extract_zero_length {a : Array α} :
@@ -24,103 +20,21 @@ theorem extract_zero_length {a : Array α} :
   refine extract_empty_of_stop_le_start a ?h
   exact Nat.le_refl i
 
-theorem extract_succ_start_aux {l: Array α} (h: i < l.size):
-    i < j → j ≤ l.size →
-    l.extract i j = #[l[i]] ++ l.extract i.succ j := by
-  intro h1 h2
-  simp only [extract, h2, Nat.min_eq_left, Nat.sub_eq, mkEmpty_eq]
-  conv =>
-    left
-    unfold extract.loop
-  have : j - i = (j - i - 1).succ := by
-    rw [Nat.succ_eq_add_one]
-    refine (Nat.sub_eq_iff_eq_add ?h).mp rfl
-    exact Nat.le_sub_of_add_le' h1
-  simp only [h, ↓reduceDite, get_eq_getElem]
-  rw [this]
-  simp only
-  rw [extract_loop_eq_aux]
-  exact rfl
-
-
-theorem extract_succ_start {l: Array α} (h: i < l.size):
-    i < j →
-    l.extract i j = #[l[i]] ++ l.extract i.succ j := by
-  by_cases h1: j ≤ l.size
-  · exact fun a ↦ extract_succ_start_aux h a h1
-  · intro _
-    have h2 :l.size ≤ j := by exact Nat.le_of_not_ge h1
-    rw [extract_overflow_eq_extract_to_stop h2, extract_overflow_eq_extract_to_stop h2]
-    refine extract_succ_start_aux h h ?_
-    exact anyM.proof_1 l
-
-theorem extract_succ_stop_aux {α: Type u_1}
-    {j i: Nat} {l: Array α} {h1: j < l.size} (h2: i ≤ j) :
-    extract.loop l (Nat.succ j - i) i #[] = extract.loop l (j - i) i #[] ++ #[l[j]'h1] := by
-  by_cases h3: i = j
-  · rw [h3]
-    simp only [Nat.succ_eq_one_add, Nat.add_sub_cancel, Nat.sub_self]
-    unfold extract.loop
-    simp only [h1, get_eq_getElem, dite_eq_ite, ite_self, nil_append, ↓reduceDite]
-    exact extract_loop_zero l (push #[] l[j]) (j + 1)
-  · unfold extract.loop
-    have h4 : i < l.size := Nat.lt_of_le_of_lt h2 h1
-    simp only [h4, ↓reduceDite, get_eq_getElem]
-    have h5 : Nat.succ j - i = (j - i).succ := Nat.succ_sub h2
-    have h6: i < j := by exact Nat.lt_of_le_of_ne h2 h3
-    have h7 : j - i = (j - (i + 1)).succ := by
-      rw [Nat.succ_eq_add_one]
-      refine (Nat.sub_eq_iff_eq_add ?h).mp rfl
-      exact (Nat.le_sub_iff_add_le' h2).mpr h6
-    simp only [h5, h7]
-    have h8: Nat.succ (j - (i + 1)) = Nat.succ j - (i + 1) := by
-      rw [Nat.succ_eq_add_one, Nat.succ_eq_add_one, Nat.sub_add_comm]
-      exact h6
-    rw [extract_loop_eq_aux _ (push #[] l[i]), extract_loop_eq_aux _ (push #[] l[i]), h8]
-    rw [extract_succ_stop_aux, Array.append_assoc]
-    exact h6
-termination_by j - i
-
-theorem extract_succ_stop {l: Array α} (h1: j < l.size) (h2: i ≤ j):
-    l.extract i j.succ = l.extract i j ++ #[l[j]] := by
-  have h3: j.succ ≤ l.size := by exact h1
-  simp only [extract, Nat.min_eq_left h3, Nat.sub_eq, mkEmpty_eq, Nat.min_eq_left (Nat.le_of_lt h1)]
-  exact extract_succ_stop_aux h2
-
 theorem size_extract_to_stop {a: Array α} : (e ≤ a.size) → (a.extract s e).size = e - s := by
   intro h1
   simp only [size_extract]
   rw [Nat.min_eq_left h1]
 
-theorem extract_mid_split {a: Array α} (h1: i ≤ j)  (h2 : j ≤ k) (h3: k ≤ a.size):
-    a.extract i k = a.extract i j ++ a.extract j k := by
-  by_cases h : j = k
-  · rw [h]
-    simp only [extract_zero_length, append_nil]
-  · have h4: j < k := Nat.lt_of_le_of_ne h2 h
-    have h5: j < a.size := Nat.lt_of_lt_of_le h4 h3
-    rw [extract_succ_start h5 h4,
-      ← append_assoc,
-      ← extract_succ_stop h5 h1,
-      ← extract_mid_split (Nat.le_succ_of_le h1) h4 h3]
-termination_by k - j
-
 theorem extract_append_left {a b: Array α} {i j : Nat} (h: j ≤ a.size):
-    (a ++ b).extract i j = a.extract i j :=
-  if h1 : i ≥ j
-  then by
-    rw [extract_empty_of_stop_le_start _ h1, extract_empty_of_stop_le_start _ h1]
-  else by
-    have h2 : i < j := Nat.gt_of_not_le h1
-    have hia :i < a.size := Nat.lt_of_lt_of_le h2 h
-    have hiab : i < (a++b).size := by
-      rw [size_append]
-      exact Nat.lt_add_right (size b) hia
-    rw [extract_succ_start hiab h2,
-        extract_succ_start hia h2]
-    have : (a ++ b)[i] = a[i] := get_append_left hia
-    rw [this, extract_append_left h]
-termination_by j - i
+    (a ++ b).extract i j = a.extract i j := by
+  apply ext
+  · have h' : j ≤ a.size + b.size := by
+      apply Nat.le_trans h
+      exact Nat.le_add_right (size a) (size b)
+    simp only [size_extract, size_append, h', Nat.min_eq_left, h]
+  · intro h1 h2 h3
+    simp only [get_extract]
+    rw [get_append_left]
 
 theorem extract_append_right {a b: Array α} {i j : Nat} (h: i ≥ a.size):
     (a ++ b).extract i j = b.extract (i - a.size) (j - a.size) := by
@@ -135,79 +49,71 @@ theorem extract_append_right {a b: Array α} {i j : Nat} (h: i ≥ a.size):
       exact (Nat.le_sub_iff_add_le' h).mpr h2
     rw [h3, h4]
   · simp only [ge_iff_le, Nat.not_le] at h1
-    simp only [ge_iff_le, Nat.not_le] at h2
-    have hiab: (i - size a) < b.size := by
-      rw [size_append] at h2
-      exact Nat.sub_lt_left_of_lt_add h h2
-    have hiaja: i - size a < j - size a := by
-      apply Nat.lt_sub_of_add_lt
-      rw [Nat.sub_add_cancel h]
-      exact h1
-    rw [extract_succ_start h2 h1,
-        extract_succ_start hiab hiaja]
-    have : (a ++ b)[i] = b[i - a.size] := by exact get_append_right h hiab
-    rw [this, extract_append_right (Nat.le_succ_of_le h), Nat.succ_sub h]
-  termination_by j - i
+    simp only [size_append, ge_iff_le, Nat.not_le] at h2
+    apply ext
+    · simp only [size_extract]
+      by_cases h3: j ≤ a.size + b.size
+      · have h4: j - a.size ≤ b.size := Nat.sub_le_iff_le_add'.mpr h3
+        simp only [size_append, h3, Nat.min_eq_left, h4]
+        rw [Nat.sub_sub, Nat.add_comm, Nat.sub_add_cancel h]
+      · have h3': j ≥ a.size + b.size := Nat.le_of_not_ge h3
+        have h4:  j - a.size ≥ b.size := Nat.le_sub_of_add_le' h3'
+        simp only [size_append, h3', Nat.min_eq_right, h4]
+        apply @Nat.add_right_cancel _ (i - a.size)
+        have h5 : i ≤ a.size + b.size := Nat.le_of_succ_le h2
+        have h6 : i - a.size ≤ b.size := Nat.sub_le_iff_le_add'.mpr h5
+        rw [← Nat.add_sub_assoc h, Nat.sub_add_cancel h5, Nat.add_comm,
+          Nat.add_sub_cancel, Nat.sub_add_cancel h6]
+    · intro k h4 h5
+      simp only [get_extract, get_append_right (Nat.le_trans h (Nat.le_add_right i k)),
+        Nat.sub_add_comm h]
 
-theorem extract_extract_aux {a : Array α } : (s1 + s2 ≤ a.size) →
+theorem extract_overflow_eq_extract_to_end {a : Array α} :
+    (a.size ≤ l) →  a.extract p l = a.extract p a.size := by
+  intro h
+  simp only [extract, Nat.min_eq_right h, Nat.sub_eq, mkEmpty_eq, Nat.min_self]
+
+theorem extract_extract_aux {a : Array α } :
     (s1 + e2 ≤ e1) → (e1 ≤ a.size) →
     (a.extract s1 e1).extract s2 e2 = a.extract (s1 + s2) (s1 + e2) := by
-  intro h1 h2 h3
-  by_cases h4: e2 ≤ s2
-  · have hem0: (a.extract s1 e1).extract s2 e2 = #[] :=
-      extract_empty_of_stop_le_start (extract a s1 e1) h4
-    have hem1: a.extract (s1 + s2) (s1 + e2) = #[] := by
-      apply extract_empty_of_stop_le_start a
-      exact Nat.add_le_add_left h4 s1
-    rw [hem0, hem1]
-  · have hle1: s1 + s2 ≤ s1 + e2 := by
-      refine Nat.add_le_add ?h₁ ?h₂
-      simp only [Nat.le_refl]
-      exact Nat.le_of_not_ge h4
-    have happend1 : a.extract s1 e1 = a.extract s1 (s1 + s2) ++ a.extract (s1 + s2) e1 := by
-      refine extract_mid_split ?h1 ?h2 h3
-      exact Nat.le_add_right s1 s2
-      apply Nat.le_trans hle1 h2
-    have hsize1: s2 ≥ size (extract a s1 (s1 + s2)) := by
-      rw [size_extract_to_stop h1, Nat.add_comm, Nat.add_sub_cancel]
-      exact Nat.le_refl s2
-    rw [happend1, extract_append_right hsize1]
-    simp only [size_extract, h1, Nat.min_eq_left]
-    have hs: (s1 + s2 - s1) = s2 := by
-      rw [Nat.add_comm, Nat.add_sub_cancel]
-    rw [hs, Nat.sub_self]
-    have happend2: a.extract (s1 + s2) e1 =
-      a.extract (s1 + s2) (s1 + e2) ++ a.extract (s1 + e2) e1 := by
-      apply extract_mid_split hle1 h2 h3
-    have hsize2: e2 - s2 = size (extract a (s1 + s2) (s1 + e2)) := by
-      rw [size_extract_to_stop (Nat.le_trans h2 h3),
-       Nat.sub_add_eq, Nat.add_comm, Nat.add_sub_cancel]
-    rw [happend2, extract_append_left (Nat.le_of_eq hsize2),hsize2, extract_all]
+  intro h1 h2
+  apply ext
+  · have hsize1 : s1 + e2 ≤ a.size := Nat.le_trans h1 h2
+    have hsize2 : e2 ≤ e1 - s1 := Nat.le_sub_of_add_le' h1
+    simp only [size_extract, h2, Nat.min_eq_left, hsize2, hsize1]
+    rw [Nat.sub_add_eq, Nat.add_comm, Nat.add_sub_cancel]
+  · intro i h1 h2
+    simp only [get_extract, Nat.add_assoc]
 
-theorem extract_extract {a : Array α } (h1 : s1 + s2 ≤ a.size)
+theorem extract_extract {a : Array α }
     (h2 : s1 + e2 ≤ e1) :
     (a.extract s1 e1).extract s2 e2 = a.extract (s1 + s2) (s1 + e2) := by
   by_cases h: e1 ≤ a.size
-  · exact extract_extract_aux h1 h2 h
+  · exact extract_extract_aux h2 h
   by_cases h3 : s1 + e2 ≤ a.size
-  · rw [extract_overflow_eq_extract_to_stop (Nat.le_of_not_ge h : e1 ≥ a.size),
-        extract_extract_aux h1 _ (anyM.proof_1 a : a.size ≤ a.size)]
-    exact h3
+  · rw [extract_overflow_eq_extract_to_end (Nat.le_of_not_ge h : e1 ≥ a.size),
+        extract_extract_aux h3 (Nat.le_of_eq rfl : a.size ≤ a.size)]
+  by_cases h1 : s1 ≤ a.size
   · have h4: s1 + (extract a s1 a.size).size = a.size := by
-      have : s1 ≤ a.size := by
-        apply Nat.le_trans _  h1
-        exact Nat.le_add_right s1 s2
       rw [size_extract_to_stop (Nat.le_of_eq rfl),
           Nat.add_comm,
-          Nat.sub_add_cancel this]
+          Nat.sub_add_cancel h1]
     have h5: e2 ≥ (extract a s1 (size a)).size := by
       apply @Nat.le_of_add_le_add_left s1
       rw [h4]
       exact Nat.le_of_not_ge h3
-    rw [extract_overflow_eq_extract_to_stop (Nat.le_of_not_ge h : e1 ≥ a.size),
-    extract_overflow_eq_extract_to_stop (Nat.le_of_not_ge h3 : s1 + e2 ≥ a.size),
-    extract_overflow_eq_extract_to_stop h5,
-    extract_extract_aux h1 (Nat.le_of_eq h4) (Nat.le_of_eq rfl),
+    rw [extract_overflow_eq_extract_to_end (Nat.le_of_not_ge h : e1 ≥ a.size),
+    extract_overflow_eq_extract_to_end (Nat.le_of_not_ge h3 : s1 + e2 ≥ a.size),
+    extract_overflow_eq_extract_to_end h5,
+    extract_extract_aux (Nat.le_of_eq h4) (Nat.le_of_eq rfl),
     h4]
+  · simp only [Nat.not_le] at h1
+    have hsize1: a.size ≤ s1 := Nat.le_of_lt h1
+    have hsize2: a.size ≤ s1 + s2 := by
+      apply Nat.le_trans hsize1
+      exact Nat.le_add_right s1 s2
+    rw [extract_empty_of_size_le_start a hsize1,
+      extract_empty_of_size_le_start a hsize2,
+      extract_empty]
 
 end Array
