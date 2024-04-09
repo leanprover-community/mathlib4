17f756e3fb3c9d030cb01255ebb74ee56d41eb91
dagurtomas <dagurtomas@gmail.com>
2024-04-09 23:24:10 +0000

    some work on preserving products API

--- Mathlib/Condensed/LocallyConstant.lean
+++ Mathlib/Condensed/LocallyConstant.lean
@@ -1,6 +1,25 @@
+import Mathlib.Condensed.PreservesProductExplicit
 import Mathlib.Condensed.TopComparison
 
 universe u
+
+@[simps! apply toEquiv]
+def _root_.Homeomorph.sigmaCongrLeft {α₁ α₂ : Type*} {β : α₁ → Type*} [∀ a, TopologicalSpace (β a)]
+    (f : α₁ ≃ α₂) : (a : α₁) × β a ≃ₜ (a : α₂) × β (f.symm a) where
+  toEquiv := Equiv.sigmaCongrLeft' f
+  continuous_toFun := by
+    apply continuous_sigma
+    rw [f.forall_congr_left']
+    intro i
+    simp only [Equiv.sigmaCongrLeft', Equiv.sigmaCongrLeft, Equiv.symm_symm_apply,
+      Equiv.toFun_as_coe, Equiv.coe_fn_symm_mk]
+    convert continuous_sigmaMk (ι := α₂) (σ := fun a ↦ β (f.symm a))
+    all_goals simp
+  continuous_invFun := by
+    apply continuous_sigma
+    rw [f.symm.forall_congr_left']
+    intro i
+    exact continuous_sigmaMk
 
 noncomputable section
 
@@ -53,17 +72,51 @@ namespace DiscreteAdjunction
 
 variable {S : CompHaus.{u}} {Y : CondensedSet.{u}} (f : LocallyConstant S (Y.val.obj (op (⊤_ _))))
 
-instance indexSetFintype : Fintype (Set.range f) := by
+instance indexSetFinite : Finite (Set.range f) := by
   letI : TopologicalSpace (Y.val.obj (op (⊤_ _))) := ⊥
   haveI : DiscreteTopology (Y.val.obj (op (⊤_ _))) := ⟨rfl⟩
-  have := (isCompact_range f.continuous).finite_of_discrete
-  rw [Set.Finite] at this
-  exact Fintype.ofFinite _
+  exact (isCompact_range f.continuous).finite_of_discrete
+
+def σ : Set.range f → Type u := fun x ↦ f ⁻¹' {x.val}
+
+instance (x : Set.range f) : TopologicalSpace (σ f x) :=
+  (inferInstance : TopologicalSpace (f ⁻¹' {x.val}))
+
+instance (x : Set.range f) : T2Space (σ f x) :=
+  (inferInstance : T2Space (f ⁻¹' {x.val}))
+
+instance (x : Set.range f) : CompactSpace (σ f x) :=
+  isCompact_iff_compactSpace.mp (f.2.isClosed_fiber _).isCompact
+
+def homeoSigma {X Y : Type*} [TopologicalSpace X] (f : LocallyConstant X Y)
+    (p : Y → Prop) (h : ∀ x, p (f x)) :
+    (Σ (y : {y // p y}), f ⁻¹' {y.val}) ≃ₜ X where
+  toEquiv := Equiv.sigmaSubtypeFiberEquiv f p h
+  continuous_invFun := by
+    rw [continuous_def]
+    intro U hU
+    have : (Equiv.sigmaSubtypeFiberEquiv f p h).invFun ⁻¹' U =
+        (Equiv.sigmaSubtypeFiberEquiv f p h).toFun '' U := by ext; simp
+    rw [this]
+    refine (?_ : IsOpenMap _) U hU
+    rw [isOpenMap_sigma]
+    exact fun i ↦ IsOpen.isOpenMap_subtype_val (f.2.isOpen_fiber _)
+
+-- def map := Condensed.sigmaComparison Y (σ f) -- (Set.range f) has type u+1
+
+-- def isoSigma : S ⟶ CompHaus.of <| (x : Set.range f) × (σ f x) := sorry
+
+-- def counit_app_app' (S : CompHaus.{u}) (Y : CondensedSet.{u}) :
+--     LocallyConstant S (Y.val.obj (op (⊤_ _))) ⟶ Y.val.obj (op S) :=
+--   sorry
+--   -- fun f => (asIso (Condensed.sigmaComparison Y (σ f))).inv _
 
 def Z' : Set.range f → CompHaus.{u}ᵒᵖ :=
   haveI : ∀ x, CompactSpace (f ⁻¹' {x}) := fun _ =>
     isCompact_iff_compactSpace.mp (f.2.isClosed_fiber _).isCompact
   fun x => op (CompHaus.of (f ⁻¹' {x.val}))
+
+instance indexSetFintype : Fintype (Set.range f) := Fintype.ofFinite _
 
 instance indexSetSmall : Small.{0} (Set.range f) := Countable.toSmall _
 
@@ -82,38 +135,6 @@ def Z : indexSet f → CompHaus.{u}ᵒᵖ :=
 instance : PreservesLimit (Discrete.functor (Z f)) Y.val := by
   have : PreservesFiniteProducts Y.val := inferInstance
   exact (this.preserves (indexSet f)).1
-
-def homeoSigma {X Y : Type*} [TopologicalSpace X] (f : LocallyConstant X Y)
-    (p : Y → Prop) (h : ∀ x, p (f x)) :
-    (Σ (y : {y // p y}), f ⁻¹' {y.val}) ≃ₜ X where
-      toEquiv := Equiv.sigmaSubtypeFiberEquiv f p h
-      continuous_invFun := by
-        rw [continuous_def]
-        intro U hU
-        have : (Equiv.sigmaSubtypeFiberEquiv f p h).invFun ⁻¹' U =
-            (Equiv.sigmaSubtypeFiberEquiv f p h).toFun '' U := by ext; simp
-        rw [this]
-        refine (?_ : IsOpenMap _) U hU
-        rw [isOpenMap_sigma]
-        exact fun i ↦ IsOpen.isOpenMap_subtype_val (f.2.isOpen_fiber _)
-
-@[simps! apply toEquiv]
-def _root_.Homeomorph.sigmaCongrLeft {α₁ α₂ : Type*} {β : α₁ → Type*} [∀ a, TopologicalSpace (β a)]
-    (f : α₁ ≃ α₂) : (a : α₁) × β a ≃ₜ (a : α₂) × β (f.symm a) where
-  toEquiv := Equiv.sigmaCongrLeft' f
-  continuous_toFun := by
-    apply continuous_sigma
-    rw [f.forall_congr_left']
-    intro i
-    simp only [Equiv.sigmaCongrLeft', Equiv.sigmaCongrLeft, Equiv.symm_symm_apply,
-      Equiv.toFun_as_coe, Equiv.coe_fn_symm_mk]
-    convert continuous_sigmaMk (ι := α₂) (σ := fun a ↦ β (f.symm a))
-    all_goals simp
-  continuous_invFun := by
-    apply continuous_sigma
-    rw [f.symm.forall_congr_left']
-    intro i
-    exact continuous_sigmaMk
 
 def extracted_1 :
     CompHaus.finiteCoproduct (fun i ↦ (Z f i).unop) ≃ₜ
@@ -155,22 +176,25 @@ def index_map (i : indexSet (f.comap g)) : indexSet f :=
 -- theorem component_map (i : indexSet (f.comap g)) :
 --   component _ i = component f (index_map f T g i) ≫ Y.val.map (Z_map f T g i) := sorry
 
-def counit_app (Y : CondensedSet.{u}) : LC'.obj (Y.val.obj (op (⊤_ _))) ⟶ Y where
-  val := {
-    app := fun ⟨S⟩ => counit_app_app S Y
-    naturality := by
-      intro S T f
-      ext g
-      simp only [LC', LC, Eq.ndrec, id_eq, eq_mpr_eq_cast, types_comp_apply]
-      apply_fun (isoProduct' (g.comap f.unop)).hom using injective_of_mono _
-      simp only [op_unop, counit_app_app, inv_hom_id_apply]
-      apply_fun (Types.productIso (fun i => Y.val.obj (Z (g.comap f.unop) i))).hom
-        using injective_of_mono _
-      simp only [inv_hom_id_apply]
-      ext y
-      simp only [component, Types.productIso_hom_comp_eval_apply]
-      simp [isoProduct']
-      sorry
-  }
+-- def counit_app (Y : CondensedSet.{u}) : LC'.obj (Y.val.obj (op (⊤_ _))) ⟶ Y where
+--   val := {
+--     app := fun ⟨S⟩ => counit_app_app S Y
+--     naturality := by
+--       intro S T f
+--       ext g
+--       simp only [LC', LC, Eq.ndrec, id_eq, eq_mpr_eq_cast, types_comp_apply]
+--       apply_fun (isoProduct' (g.comap f.unop)).hom using injective_of_mono _
+--       simp only [op_unop, counit_app_app, inv_hom_id_apply]
+--       apply_fun (Types.productIso (fun i => Y.val.obj (Z (g.comap f.unop) i))).hom
+--         using injective_of_mono _
+--       simp only [inv_hom_id_apply]
+--       ext y
+--       simp only [component, Types.productIso_hom_comp_eval_apply]
+--       simp only [Pi.π, isoProduct', op_unop, isoProduct, Functor.mapIso_trans, piComparison,
+--         Iso.trans_assoc, Iso.trans_inv, asIso_inv, Functor.mapIso_inv, Iso.op_inv,
+--         CompHaus.isoOfHomeo_inv, types_comp_apply, Iso.trans_hom, Functor.mapIso_hom, Iso.op_hom,
+--         CompHaus.isoOfHomeo_hom, asIso_hom, Types.pi_lift_π_apply]
+--       sorry
+--   }
 
 end DiscreteAdjunction
