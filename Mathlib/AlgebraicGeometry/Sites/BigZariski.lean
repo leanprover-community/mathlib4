95cadbafc77eed1fe69faf2dfb7f4ecc89694a2d
adamtopaz <github@adamtopaz.com>
2024-04-08 21:07:06 -0600

    add author

--- Mathlib/AlgebraicGeometry/Sites/BigZariski.lean
+++ Mathlib/AlgebraicGeometry/Sites/BigZariski.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2023 Joël Riou. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Joël Riou
+Authors: Joël Riou, Adam Topaz
 -/
 import Mathlib.AlgebraicGeometry.Pullbacks
 import Mathlib.CategoryTheory.Sites.Pretopology
