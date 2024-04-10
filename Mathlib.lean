510ed4ef99f9770e95e914d2c7c83ea522da2f6a
Ira Fesefeldt <fesefeldt@cs.rwth-aachen.de>
2024-04-10 09:29:52 +0000

    feat: Ordinal Approximants of the least/greatest fixed point in a complete lattice (#8996)
    Define ordinal approximants for the least/greatest fixed points of a monotone function in a complete lattice. It is sometimes also called the constructive Knaster-Tarski Theorem, first introduced by Cousot & Cousot in 1979. This implementation is however not constructive.
    
    This has a variety of applications in non standard logics and proofs such as quantitative logics or cyclic proofs.
    
    
    
    Co-authored-by: Ira Fesefeldt <public@feuervogel.me>

--- Mathlib.lean
+++ Mathlib.lean
@@ -3522,6 +3522,7 @@ import Mathlib.SetTheory.Ordinal.Basic
 import Mathlib.SetTheory.Ordinal.CantorNormalForm
 import Mathlib.SetTheory.Ordinal.Exponential
 import Mathlib.SetTheory.Ordinal.FixedPoint
+import Mathlib.SetTheory.Ordinal.FixedPointApproximants
 import Mathlib.SetTheory.Ordinal.NaturalOps
 import Mathlib.SetTheory.Ordinal.Notation
 import Mathlib.SetTheory.Ordinal.Principal
