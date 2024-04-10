4a0224307ee59f1e40d5d60a174e60217884fc12
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2024-04-10 18:57:16 +0200

    Update Mathlib/GroupTheory/GroupAction/Blocks.lean
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Mathlib/GroupTheory/GroupAction/Blocks.lean
+++ Mathlib/GroupTheory/GroupAction/Blocks.lean
@@ -469,7 +469,7 @@ theorem IsBlock.of_orbit' {H : Subgroup G} {a : X} (hH : stabilizer G a ≤ H) :
     IsBlock G (MulAction.orbit H a) := by
   rw [IsBlock.mk_subset]; intro g b
   rintro ⟨h, rfl⟩
-  simp
+  simp?
   intro hb'
   suffices g ∈ H by
     rw [← Subgroup.coe_mk H g this, ← Subgroup.smul_def]
