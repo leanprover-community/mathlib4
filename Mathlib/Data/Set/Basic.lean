e94da975ddecc4a53b7c3a0e1af7a7b8f54aee83
Moritz Firsching <firsching@google.com>
2024-04-11 07:51:35 +0200

    Update Mathlib/Data/Set/Basic.lean

--- Mathlib/Data/Set/Basic.lean
+++ Mathlib/Data/Set/Basic.lean
@@ -1575,6 +1575,7 @@ lemma disjoint_sdiff_left : Disjoint (t \ s) s := disjoint_sdiff_self_left
 lemma disjoint_sdiff_right : Disjoint s (t \ s) := disjoint_sdiff_self_right
 #align set.disjoint_sdiff_right Set.disjoint_sdiff_right
 
+-- TODO: prove this in terms of a lattice lemma
 theorem disjoint_sdiff_inter : Disjoint (s \ t) (s ∩ t) :=
   disjoint_of_subset_right (inter_subset_right _ _) disjoint_sdiff_left
 #align set.disjoint_sdiff_inter Set.disjoint_sdiff_inter
