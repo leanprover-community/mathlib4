36bf6b4d9ea6c628839aba0d94fb741240dc9eba
acmepjz <acme_pjz@hotmail.com>
2024-04-11 19:44:48 +0800

    apply suggestions

--- Mathlib/RingTheory/TensorProduct/Basic.lean
+++ Mathlib/RingTheory/TensorProduct/Basic.lean
@@ -1160,7 +1160,7 @@ theorem Subalgebra.finite_sup {K L : Type*} [CommRing K] [CommRing L] [Algebra K
   rw [← E1.range_val, ← E2.range_val, ← Algebra.TensorProduct.productMap_range]
   exact Module.Finite.range (Algebra.TensorProduct.productMap E1.val E2.val).toLinearMap
 
-@[deprecated Subalgebra.finite_sup]
+@[deprecated Subalgebra.finite_sup] -- 2024-04-11
 theorem Subalgebra.finiteDimensional_sup {K L : Type*} [Field K] [CommRing L] [Algebra K L]
     (E1 E2 : Subalgebra K L) [FiniteDimensional K E1] [FiniteDimensional K E2] :
     FiniteDimensional K (E1 ⊔ E2 : Subalgebra K L) := Subalgebra.finite_sup E1 E2
