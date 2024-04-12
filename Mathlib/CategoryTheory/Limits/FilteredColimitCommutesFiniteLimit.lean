74d171fbd97a2af4896f0ff0ba834a201d0d3ed3
grunweg <rothgami@math.hu-berlin.de>
2024-04-12 10:34:23 +0200

    Tweak note.

--- Mathlib/CategoryTheory/Limits/FilteredColimitCommutesFiniteLimit.lean
+++ Mathlib/CategoryTheory/Limits/FilteredColimitCommutesFiniteLimit.lean
@@ -138,8 +138,8 @@ theorem colimitLimitToLimitColimit_injective :
     -- Now it's just a calculation using `W` and `w`.
     simp only [Functor.comp_map, Limit.map_π_apply, curry_obj_map_app, swap_map]
     rw [← W _ _ (fH j), ← W _ _ (gH j)]
-    -- Porting note: had to add `Limit.map_π_apply` (which was un-tagged simp since "simp can
-    -- prove it") is this a confluence issue?
+    -- Porting note(#10745): had to add `Limit.map_π_apply`
+    -- (which was un-tagged simp since "simp can prove it")
     simp [Limit.map_π_apply, w]
 #align category_theory.limits.colimit_limit_to_limit_colimit_injective CategoryTheory.Limits.colimitLimitToLimitColimit_injective
 
