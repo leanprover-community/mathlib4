80ca53be0945528b101f1e0e137f8406c7e8a756
adomani <adomani@gmail.com>
2024-04-12 23:32:18 +0200

    test: ibench with original

--- Mathlib.lean
+++ Mathlib.lean
@@ -3727,6 +3727,7 @@ import Mathlib.Tactic.Trace
 import Mathlib.Tactic.TypeCheck
 import Mathlib.Tactic.TypeStar
 import Mathlib.Tactic.UnsetOption
+import Mathlib.Tactic.UnusedTactic
 import Mathlib.Tactic.Use
 import Mathlib.Tactic.Variable
 import Mathlib.Tactic.WLOG
