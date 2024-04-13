c854786fdfaa521ac42f3b104eac1d7b3328d064
dagurtomas <dagurtomas@gmail.com>
2024-04-13 21:07:07 +0000

    initial done

--- Mathlib/Condensed/DiscreteColimit.lean
+++ Mathlib/Condensed/DiscreteColimit.lean
@@ -1,4 +1,6 @@
 import Mathlib.Condensed.LocallyConstant
+import Mathlib.CategoryTheory.Filtered.Final
+import Mathlib.Topology.Category.Profinite.CofilteredLimit
 
 universe u
 
@@ -6,7 +8,7 @@ open CategoryTheory Functor Limits Condensed FintypeCat StructuredArrow
 
 namespace Condensed
 
-variable {I : Type*} [Category I] [IsCofiltered I] (F : I ⥤ FintypeCat.{u})
+variable {I : Type u} [Category.{u} I] [IsCofiltered I] (F : I ⥤ FintypeCat.{u})
     (c : Cone <| F ⋙ toProfinite) (hc : IsLimit c)
 
 namespace ToStructuredArrow
@@ -20,5 +22,40 @@ def functor : I ⥤ StructuredArrow c.pt toProfinite where
   map_comp _ _ := by simp only [Functor.map_comp, hom_eq_iff, mk_right, homMk_right, comp_right]
 
 def functorIso : functor F c ⋙ StructuredArrow.proj c.pt toProfinite ≅ F := Iso.refl _
+
+attribute [local instance] FintypeCat.discreteTopology
+
+-- TODO: PR
+instance : Faithful toProfinite where
+  map_injective h := funext fun _ ↦ (DFunLike.ext_iff.mp h) _
+
+-- TODO: PR
+instance : Full toProfinite where
+  preimage f := fun x ↦ f x
+  witness _ := rfl
+
+instance [∀ i, Epi (c.π.app i)] : Initial (functor F c) := by
+  rw [initial_iff_of_isCofiltered (F := functor F c)]
+  constructor
+  · intro ⟨_, X, (f : c.pt ⟶ _)⟩
+    have : DiscreteTopology (toProfinite.obj X) := by
+      simp only [toProfinite, Profinite.of]
+      infer_instance
+    let f' : LocallyConstant c.pt (toProfinite.obj X) := ⟨f, by
+      rw [IsLocallyConstant.iff_continuous]
+      exact f.continuous⟩
+    obtain ⟨i, g, h⟩ := Profinite.exists_locallyConstant.{_, u, u} c hc f'
+    refine ⟨i, ⟨homMk g.toFun ?_⟩⟩
+    ext x
+    have := (LocallyConstant.congr_fun h x).symm
+    erw [LocallyConstant.coe_comap_apply _ _ (c.π.app i).continuous] at this
+    exact this
+  · intro ⟨_, X, (f : c.pt ⟶ _)⟩ i ⟨_, (s : F.obj i ⟶ X), (w : f = c.π.app i ≫ _)⟩
+      ⟨_, (s' : F.obj i ⟶ X), (w' : f = c.π.app i ≫ _)⟩
+    simp only [functor_obj, functor_map, hom_eq_iff, mk_right, comp_right, homMk_right]
+    refine ⟨i, 𝟙 _, ?_⟩
+    simp only [CategoryTheory.Functor.map_id, Category.id_comp]
+    rw [w] at w'
+    exact toProfinite.map_injective <| Epi.left_cancellation _ _ w'
 
 end Condensed.ToStructuredArrow
