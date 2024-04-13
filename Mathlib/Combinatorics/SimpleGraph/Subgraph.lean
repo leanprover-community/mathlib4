f85b34df22ece951266ce5d2a360a142ce6612ff
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2024-04-13 09:30:11 +0200

    fixup! fixes

--- Mathlib/Combinatorics/SimpleGraph/Subgraph.lean
+++ Mathlib/Combinatorics/SimpleGraph/Subgraph.lean
@@ -966,7 +966,7 @@ theorem neighborSet_subgraphOfAdj_of_ne_of_ne {u v w : V} (hvw : G.Adj v w) (hv 
 
 theorem neighborSet_subgraphOfAdj [DecidableEq V] {u v w : V} (hvw : G.Adj v w) :
     (G.subgraphOfAdj hvw).neighborSet u = (if u = v then {w} else ∅) ∪ if u = w then {v} else ∅ :=
-  by split_ifs <;> subst_vars <;> simp [*]
+  by split_ifs <;> subst_vars <;> simp [*, Set.singleton_def]
 #align simple_graph.neighbor_set_subgraph_of_adj SimpleGraph.neighborSet_subgraphOfAdj
 
 theorem singletonSubgraph_fst_le_subgraphOfAdj {u v : V} {h : G.Adj u v} :
