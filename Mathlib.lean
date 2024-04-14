4c7b30a8175ffa1cc136e826785350f56a814575
David Loeffler <d.loeffler.01@cantab.net>
2024-04-14 12:41:16 +0000

    feat(NumberTheory/ModularForms): Asymptotics of Jacobi theta functions (#12020)
    This is a (rather boring) technical step in developing the theory of Hurwitz zeta functions: one needs to show that certain
    sums related to Jacobi theta series decay exponentially for large `t`.

--- Mathlib.lean
+++ Mathlib.lean
@@ -3012,6 +3012,7 @@ import Mathlib.NumberTheory.Modular
 import Mathlib.NumberTheory.ModularForms.Basic
 import Mathlib.NumberTheory.ModularForms.CongruenceSubgroups
 import Mathlib.NumberTheory.ModularForms.EisensteinSeries.Basic
+import Mathlib.NumberTheory.ModularForms.JacobiTheta.Bounds
 import Mathlib.NumberTheory.ModularForms.JacobiTheta.Manifold
 import Mathlib.NumberTheory.ModularForms.JacobiTheta.OneVariable
 import Mathlib.NumberTheory.ModularForms.JacobiTheta.TwoVariable
