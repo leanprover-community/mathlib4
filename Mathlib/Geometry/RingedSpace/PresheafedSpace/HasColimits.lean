a49654541d6283214d807f0aa6403c04cc8ccb59
damiano <adomani@gmail.com>
2024-04-15 12:06:04 +0000

    chore: remove remaining `cdot`s that were not `·` (#12146)
    A simple replacement `. --> ·`.
    
    See #12143 for the source of these replacements.

--- Mathlib/Geometry/RingedSpace/PresheafedSpace/HasColimits.lean
+++ Mathlib/Geometry/RingedSpace/PresheafedSpace/HasColimits.lean
@@ -394,7 +394,7 @@ def colimitPresheafObjIsoComponentwiseLimit (F : J ⥤ PresheafedSpace.{_, _, v}
     refine' (F.obj (unop X)).presheaf.mapIso (eqToIso _)
     simp only [Functor.op_obj, unop_op, op_inj_iff, Opens.map_coe, SetLike.ext'_iff,
       Set.preimage_preimage]
-    refine congr_arg (Set.preimage . U.1) (funext fun x => ?_)
+    refine congr_arg (Set.preimage · U.1) (funext fun x => ?_)
     erw [← TopCat.comp_app]
     congr
     exact ι_preservesColimitsIso_inv (forget C) F (unop X)
