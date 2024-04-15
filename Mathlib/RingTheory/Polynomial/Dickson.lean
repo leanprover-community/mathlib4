cbb6e06e5fb5cc768050a07e61c361efb6059d8a
grunweg <rothgami@math.hu-berlin.de>
2024-04-15 08:19:06 +0000

    chore: remove more simp-related porting notes which are fixed now (#12128)
    Co-authored-by: Moritz Firsching <firsching@google.com>

--- Mathlib/RingTheory/Polynomial/Dickson.lean
+++ Mathlib/RingTheory/Polynomial/Dickson.lean
@@ -125,14 +125,10 @@ There is exactly one other Lambda structure on `ℤ[X]` in terms of binomial pol
 
 -/
 
-
 theorem dickson_one_one_eval_add_inv (x y : R) (h : x * y = 1) :
     ∀ n, (dickson 1 (1 : R) n).eval (x + y) = x ^ n + y ^ n
   | 0 => by
-    -- Porting note: Original proof was
-    -- `simp only [bit0, eval_one, eval_add, pow_zero, dickson_zero]; norm_num`
-    suffices eval (x + y) 2 = 2 by convert this <;> norm_num
-    exact eval_nat_cast
+    simp only [eval_one, eval_add, pow_zero, dickson_zero]; norm_num
   | 1 => by simp only [eval_X, dickson_one, pow_one]
   | n + 2 => by
     simp only [eval_sub, eval_mul, dickson_one_one_eval_add_inv x y h _, eval_X, dickson_add_two,
--- Mathlib/RingTheory/TensorProduct/Basic.lean
+++ Mathlib/RingTheory/TensorProduct/Basic.lean
@@ -516,11 +516,9 @@ instance instCommSemiring : CommSemiring (A ⊗[R] B) where
       · intro a₂ b₂
         simp [mul_comm]
       · intro a₂ b₂ ha hb
-        -- porting note (#10745): was `simp` not `rw`
-        rw [mul_add, add_mul, ha, hb]
+        simp [mul_add, add_mul, ha, hb]
     · intro x₁ x₂ h₁ h₂
-      -- porting note (#10745): was `simp` not `rw`
-      rw [mul_add, add_mul, h₁, h₂]
+      simp [mul_add, add_mul, h₁, h₂]
 
 end CommSemiring
 
@@ -1214,7 +1212,7 @@ protected def module : Module (A ⊗[R] B) M where
   smul_add x m₁ m₂ := by simp only [(· • ·), map_add]
   add_smul x y m := by simp only [(· • ·), map_add, LinearMap.add_apply]
   one_smul m := by
-    -- Porting note: was one `simp only` not two in lean 3
+    -- Porting note: was one `simp only`, not two
     simp only [(· • ·), Algebra.TensorProduct.one_def]
     simp only [moduleAux_apply, one_smul]
   mul_smul x y m := by
@@ -1227,28 +1225,22 @@ protected def module : Module (A ⊗[R] B) M where
     · intro a b
       simp only [(· • ·), mul_zero, map_zero, LinearMap.zero_apply]
     · intro a₁ b₁ a₂ b₂
-      -- porting note; was one `simp only` not two and a `rw` in mathlib3
+      -- Porting note: was one `simp only`, not two
       simp only [(· • ·), Algebra.TensorProduct.tmul_mul_tmul]
-      simp only [moduleAux_apply, mul_smul]
-      rw [smul_comm a₁ b₂]
+      simp only [moduleAux_apply, mul_smul, smul_comm a₁ b₂]
     · intro z w hz hw a b
-      -- Porting note: was one `simp only` but random stuff doesn't work
+      -- Porting note: was one `simp only`, but random stuff doesn't work
       simp only [(· • ·)] at hz hw ⊢
-      simp only [moduleAux_apply]
-      rw [mul_add]  -- simp only doesn't work
-      simp only [LinearMap.map_add, LinearMap.add_apply, moduleAux_apply, hz, hw, smul_add]
+      simp only [moduleAux_apply, mul_add, LinearMap.map_add,
+        LinearMap.add_apply, moduleAux_apply, hz, hw, smul_add]
     · intro z w _ _
       simp only [(· • ·), mul_zero, map_zero, LinearMap.zero_apply]
     · intro a b z w hz hw
-      simp only [(· • ·)] at hz hw
-      simp only [(· • ·), LinearMap.map_add, add_mul, LinearMap.add_apply, hz, hw]
+      simp only [(· • ·)] at hz hw ⊢
+      simp only [LinearMap.map_add, add_mul, LinearMap.add_apply, hz, hw]
     · intro u v _ _ z w hz hw
-      simp only [(· • ·)] at hz hw
-      -- Porting note: no idea why this is such a struggle
-      simp only [(· • ·)]
-      rw [add_mul, LinearMap.map_add, LinearMap.add_apply, hz, hw]
-      simp only [LinearMap.map_add, LinearMap.add_apply]
-      rw [add_add_add_comm]
+      simp only [(· • ·)] at hz hw ⊢
+      simp only [add_mul, LinearMap.map_add, LinearMap.add_apply, hz, hw, add_add_add_comm]
 #align tensor_product.algebra.module TensorProduct.Algebra.module
 
 attribute [local instance] TensorProduct.Algebra.module
