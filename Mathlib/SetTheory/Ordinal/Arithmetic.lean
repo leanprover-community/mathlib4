7eeaaffb8b9ec3690b084f6602085e4291ac9b11
grunweg <rothgami@math.hu-berlin.de>
2024-04-15 06:46:13 +0000

    chore: classify porting notes about additional necessary beta reduction (#12130)
    This subsumes some of the notes in #10752 and #10971.
    I'm on the fence as to whether replacing the dsimp only by beta_reduce is useful; this is easy to revert if needed.

--- Mathlib/SetTheory/Ordinal/Arithmetic.lean
+++ Mathlib/SetTheory/Ordinal/Arithmetic.lean
@@ -803,9 +803,9 @@ theorem mul_le_of_limit {a b c : Ordinal} (h : IsLimit b) : a * b ≤ c ↔ ∀ 
 #align ordinal.mul_le_of_limit Ordinal.mul_le_of_limit
 
 theorem mul_isNormal {a : Ordinal} (h : 0 < a) : IsNormal (a * ·) :=
-  -- Porting note: `dsimp only` is required for beta reduction.
+  -- Porting note(#12129): additional beta reduction needed
   ⟨fun b => by
-      dsimp only
+      beta_reduce
       rw [mul_succ]
       simpa only [add_zero] using (add_lt_add_iff_left (a * b)).2 h,
     fun b l c => mul_le_of_limit l⟩
