6a4f46a66806181e35b684edfc575dcb6af1277f
Christopher Hoskin <christopher.hoskin@gmail.com>
2024-04-16 08:06:59 +0100

    Tidy up

--- Mathlib/Topology/Order/ScottTopology.lean
+++ Mathlib/Topology/Order/ScottTopology.lean
@@ -236,6 +236,9 @@ lemma isUpperSet_of_isOpen : IsOpen s → IsUpperSet s := fun h ↦
 lemma isLowerSet_of_isClosed : IsClosed s → IsLowerSet s := fun h ↦
   (isClosed_iff_isLowerSet_and_dirSupClosed.mp h).left
 
+lemma DirSupClosed_of_isClosed : IsClosed s → DirSupClosed s := fun h ↦
+  (isClosed_iff_isLowerSet_and_dirSupClosed.mp h).right
+
 lemma lowerClosure_subset_closure : ↑(lowerClosure s) ⊆ closure s := by
   convert closure.mono (@upperSet_le_scott α _)
   rw [@IsUpperSet.closure_eq_lowerClosure α _ (upperSet α) ?_ s]
@@ -357,21 +360,9 @@ section LinearOrder
 
 variable [CompleteLinearOrder α] [TopologicalSpace α] [Topology.IsScott α]
 
-#check DirSupClosed
+namespace Topology
 
-/-
-def Ioi (a : α) :=
-  { x | a < x }
-
-  def Iic (b : α) :=
-  { x | x ≤ b }
-
-def DirSupInacc (s : Set α) : Prop :=
-  ∀ ⦃d⦄, d.Nonempty → DirectedOn (· ≤ ·) d → ∀ ⦃a⦄, IsLUB d a → a ∈ s → (d ∩ s).Nonempty
-
-def DirSupClosed (s : Set α) : Prop :=
-  ∀ ⦃d⦄, d.Nonempty → DirectedOn (· ≤ ·) d → ∀ ⦃a⦄, IsLUB d a → d ⊆ s → a ∈ s
--/
+namespace IsScott
 
 lemma isOpen_iff_Iic_compl_or_univ (U : Set α) :
     IsOpen U ↔ (∃ (a : α), U = (Iic a)ᶜ) ∨ U = univ := by
@@ -383,19 +374,17 @@ lemma isOpen_iff_Iic_compl_or_univ (U : Set α) :
       let b := sSup Uᶜ
       use b
       rw [eq_compl_comm, le_antisymm_iff]
-      constructor
-      . have d1 : DirSupClosed Uᶜ :=
-          (Topology.IsScott.isClosed_iff_isLowerSet_and_dirSupClosed.mp (isClosed_compl_iff.mpr hU)).right
-        have m1 : b ∈ Uᶜ :=
-          (dirSupClosed_iff_forall_sSup.mp d1) neUc (IsChain.directedOn (isChain_of_trichotomous Uᶜ)) (le_refl Uᶜ)
-        have c1 : IsLowerSet Uᶜ := Topology.IsScott.isLowerSet_of_isClosed (isClosed_compl_iff.mpr hU)
-        exact IsLowerSet.Iic_subset c1 m1
-      · exact fun  _ ha ↦ CompleteLattice.le_sSup Uᶜ _ ha
+      exact ⟨IsLowerSet.Iic_subset (isLowerSet_of_isClosed (isClosed_compl_iff.mpr hU)) ((dirSupClosed_iff_forall_sSup.mp (DirSupClosed_of_isClosed (isClosed_compl_iff.mpr hU))) neUc (IsChain.directedOn (isChain_of_trichotomous Uᶜ)) (le_refl Uᶜ)),
+        fun  _ ha ↦ CompleteLattice.le_sSup Uᶜ _ ha⟩
   · intro H
     rcases H with ⟨a,ha⟩ | hU
     · rw [← isClosed_compl_iff, ha, compl_compl]
-      exact Topology.IsScott.isClosed_Iic
+      exact isClosed_Iic
     · rw [hU]
       exact isOpen_univ
+
+end IsScott
+
+end Topology
 
 end LinearOrder
