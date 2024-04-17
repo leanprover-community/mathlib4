a5ffeec90bc71baf26adcd86eb1d4b0262682e1d
Johan Commelin <johan@commelin.net>
2024-04-17 20:41:01 +0000

    feat: characteristic polynomials of linear families of endomorphisms (#11773)
    Co-authored-by: Oliver Nash <github@olivernash.org>

--- Mathlib.lean
+++ Mathlib.lean
@@ -353,6 +353,7 @@ import Mathlib.Algebra.Module.Hom
 import Mathlib.Algebra.Module.Injective
 import Mathlib.Algebra.Module.LinearMap.Basic
 import Mathlib.Algebra.Module.LinearMap.End
+import Mathlib.Algebra.Module.LinearMap.Polynomial
 import Mathlib.Algebra.Module.LocalizedModule
 import Mathlib.Algebra.Module.MinimalAxioms
 import Mathlib.Algebra.Module.Opposites
