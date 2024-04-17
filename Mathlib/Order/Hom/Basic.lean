4741a0523b0425baf672f56733ff625e15988544
Frederic Marbach <66742248+frederic-marbach@users.noreply.github.com>
2024-04-17 22:43:48 +0200

    feat(Order/Hom): fix typos

--- Mathlib/Order/Hom/Basic.lean
+++ Mathlib/Order/Hom/Basic.lean
@@ -760,27 +760,27 @@ lemma Disjoint.of_orderEmbedding [OrderBot α] [OrderBot β] {a₁ a₂ : α} :
   have hx₁ : f x ≤ f a₁ := (OrderEmbedding.le_iff_le f).mpr h₁
   have hx₂ : f x ≤ f a₂ := (OrderEmbedding.le_iff_le f).mpr h₂
   have hxb : f x ≤ ⊥ := h hx₁ hx₂
-  have hxeb : f x ≤ e ⊥ := le_trans hxb (OrderBot.bot_le (e ⊥))
-  exact (OrderEmbedding.le_iff_le e).mp hxeb
+  have hxeb : f x ≤ f ⊥ := le_trans hxb (OrderBot.bot_le (f ⊥))
+  exact (OrderEmbedding.le_iff_le f).mp hxeb
 
 /-- If the images by an order embedding of two elements are codisjoint, then they are themselves
 codisjoint. -/
-lemma Codisjoint.of_orderEmbedding [OrderTop α] [OrderTop β] (e : OrderEmbedding α β) {a₁ a₂ : α} :
-    Codisjoint (e a₁) (e a₂) → Codisjoint a₁ a₂ := by
+lemma Codisjoint.of_orderEmbedding [OrderTop α] [OrderTop β] {a₁ a₂ : α} :
+    Codisjoint (f a₁) (f a₂) → Codisjoint a₁ a₂ := by
   simp_rw [Codisjoint]
   intro h x h₁ h₂
-  have hx₁ : e a₁ ≤ e x := (OrderEmbedding.le_iff_le e).mpr h₁
-  have hx₂ : e a₂ ≤ e x := (OrderEmbedding.le_iff_le e).mpr h₂
-  have hxt : ⊤ ≤ e x := h hx₁ hx₂
-  have hxet : e ⊤ ≤ e x := le_trans (OrderTop.le_top (e ⊤)) hxt
-  exact (OrderEmbedding.le_iff_le e).mp hxet
+  have hx₁ : f a₁ ≤ f x := (OrderEmbedding.le_iff_le f).mpr h₁
+  have hx₂ : f a₂ ≤ f x := (OrderEmbedding.le_iff_le f).mpr h₂
+  have hxt : ⊤ ≤ f x := h hx₁ hx₂
+  have hxet : f ⊤ ≤ f x := le_trans (OrderTop.le_top (f ⊤)) hxt
+  exact (OrderEmbedding.le_iff_le f).mp hxet
 
 /-- If the images by an order embedding of two elements are complements, then they are themselves
 complements. -/
-lemma IsCompl.of_orderEmbedding [BoundedOrder α] [BoundedOrder β] (e : OrderEmbedding α β)
-    {a₁ a₂ : α} : IsCompl (e a₁) (e a₂) → IsCompl a₁ a₂ := by
+lemma IsCompl.of_orderEmbedding [BoundedOrder α] [BoundedOrder β] {a₁ a₂ : α} :
+    IsCompl (f a₁) (f a₂) → IsCompl a₁ a₂ := by
   intro ⟨hd, hcd⟩
-  exact ⟨Disjoint.of_orderEmbedding e hd, Codisjoint.of_orderEmbedding e hcd⟩
+  exact ⟨Disjoint.of_orderEmbedding f hd, Codisjoint.of_orderEmbedding f hcd⟩
 
 end Disjoint
 
