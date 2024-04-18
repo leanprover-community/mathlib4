2984084bb8d901d026e014eebb1c4e3a7af947b3
Yaël Dillies <yael.dillies@gmail.com>
2024-04-18 07:15:22 +0000

    chore: Rename `Int` and `Rat` instances Fix a few names and deduplicate the `AddCommGroup ℤ` instance

--- Mathlib/Algebra/Ring/Int.lean
+++ Mathlib/Algebra/Ring/Int.lean
@@ -20,9 +20,9 @@ See note [foundational algebra order theory].
 
 namespace Int
 
-instance instCommRingInt : CommRing ℤ where
+instance instCommRing : CommRing ℤ where
   __ := instAddCommGroup
-  __ := instCommSemigroupInt
+  __ := instCommSemigroup
   zero_mul := Int.zero_mul
   mul_zero := Int.mul_zero
   left_distrib := Int.mul_add
@@ -64,7 +64,7 @@ these instances non-computably.
 
 instance instCommSemiring : CommSemiring ℤ := inferInstance
 instance instSemiring     : Semiring ℤ     := inferInstance
-instance instRingInt      : Ring ℤ         := inferInstance
+instance instRing         : Ring ℤ         := inferInstance
 instance instDistrib      : Distrib ℤ      := inferInstance
 
 instance instCharZero : CharZero ℤ where
