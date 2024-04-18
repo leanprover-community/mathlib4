220e123f0e252af0afb733e10cf7115e47882f0a
Rida Hamadani <106540880+Rida-Hamadani@users.noreply.github.com>
2024-04-18 11:55:00 +0300

    Update Mathlib/Combinatorics/SimpleGraph/Connectivity.lean
    Co-authored-by: Yury G. Kudryashov <urkud@urkud.name>

--- Mathlib/Combinatorics/SimpleGraph/Connectivity.lean
+++ Mathlib/Combinatorics/SimpleGraph/Connectivity.lean
@@ -417,7 +417,7 @@ theorem eq_of_length_eq_zero {u v : V} : ∀ {p : G.Walk u v}, p.length = 0 → 
   | nil, _ => rfl
 #align simple_graph.walk.eq_of_length_eq_zero SimpleGraph.Walk.eq_of_length_eq_zero
 
-theorem eq_of_length_eq_one {u v : V} : ∀ {p : G.Walk u v}, p.length = 1 → G.Adj u v
+theorem adj_of_length_eq_one {u v : V} : ∀ {p : G.Walk u v}, p.length = 1 → G.Adj u v
   | nil => by simp
   | cons h nil => by intro; exact h
   | cons h _ => by
