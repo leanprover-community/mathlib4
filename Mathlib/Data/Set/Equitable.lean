f4fcee51050ef8d3ca9378c5d78f440c3ddb40a3
Bolton Bailey <bolton.bailey@gmail.com>
2024-04-18 05:39:14 +0000

    chore: split `Subsingleton,Nontrivial` off of `Data.Set.Basic` (#11832)
    Moves definition of and lemmas related to `Set.Subsingleton` and `Set.Nontrivial` to a new file, so that `Basic` can be shorter.

--- Mathlib/Data/Set/Equitable.lean
+++ Mathlib/Data/Set/Equitable.lean
@@ -3,6 +3,7 @@ Copyright (c) 2021 Yaël Dillies, Bhavik Mehta. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yaël Dillies, Bhavik Mehta
 -/
+import Mathlib.Data.Set.Subsingleton
 import Mathlib.Algebra.Order.BigOperators.Group.Finset
 import Mathlib.Algebra.Ring.Nat
 import Mathlib.Data.Set.Basic
