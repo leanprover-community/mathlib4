29a38b492f171a52d5646f27c5e239cfb63c9538
faenuccio <filippo.nuccio@univ-st-etienne.fr>
2024-04-18 17:40:44 +0200

    removed useless line

--- Mathlib/RingTheory/Valuation/Integers.lean
+++ Mathlib/RingTheory/Valuation/Integers.lean
@@ -84,8 +84,6 @@ theorem one_of_isUnit' {x : O} (hx : IsUnit x) (H : ∀ x, v (algebraMap O R x) 
       (algebraMap O R).map_mul, v.map_mul]
     exact mul_le_mul_left' (H (u⁻¹ : Units O)) _
 
-#check one_of_isUnit'
-
 theorem isUnit_of_one {x : O} (hx : IsUnit (algebraMap O R x)) (hvx : v (algebraMap O R x) = 1) :
     IsUnit x :=
   let ⟨u, hu⟩ := hx
