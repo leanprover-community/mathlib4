35717cf69b09aa97bca5bc55a1482d135906ed65
Scott Carnahan <128885296+ScottCarnahan@users.noreply.github.com>
2024-04-19 03:02:28 +0900

    minimize imports

--- Mathlib/RingTheory/HahnSeries/Valuation.lean
+++ Mathlib/RingTheory/HahnSeries/Valuation.lean
@@ -3,7 +3,6 @@ Copyright (c) 2021 Aaron Anderson. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Aaron Anderson, Scott Carnahan
 -/
-import Mathlib.RingTheory.Binomial
 import Mathlib.RingTheory.HahnSeries.Summable
 import Mathlib.RingTheory.Valuation.Basic
 
