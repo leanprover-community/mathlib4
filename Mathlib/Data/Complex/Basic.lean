d69542dece7f581eaad459815c89bc2aad31d7bb
Scott Morrison <scott.morrison@gmail.com>
2024-04-18 14:09:25 +1000

    links to issues

--- Mathlib/Data/Complex/Basic.lean
+++ Mathlib/Data/Complex/Basic.lean
@@ -677,6 +677,7 @@ theorem normSq_pos {z : ℂ} : 0 < normSq z ↔ z ≠ 0 :=
 
 -- Adaptation note: nightly-2024-04-01
 -- The simpNF linter now times out on this lemma.
+-- See https://github.com/leanprover-community/mathlib4/issues/12228
 @[simp, nolint simpNF]
 theorem normSq_neg (z : ℂ) : normSq (-z) = normSq z := by simp [normSq]
 #align complex.norm_sq_neg Complex.normSq_neg
