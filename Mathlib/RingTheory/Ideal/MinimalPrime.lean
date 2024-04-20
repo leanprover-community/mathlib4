be4b9287b278075394ae17a3c156043e08fea028
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2024-04-20 21:03:45 +0200

    chore: split out Ideal.IsPrimary
    This splits out a small but self-contained part of RingTheory.Ideal.Operations.

--- Mathlib/RingTheory/Ideal/MinimalPrime.lean
+++ Mathlib/RingTheory/Ideal/MinimalPrime.lean
@@ -3,6 +3,7 @@ Copyright (c) 2022 Andrew Yang. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Andrew Yang
 -/
+import Mathlib.RingTheory.Ideal.IsPrimary
 import Mathlib.RingTheory.Localization.AtPrime
 import Mathlib.Order.Minimal
 
