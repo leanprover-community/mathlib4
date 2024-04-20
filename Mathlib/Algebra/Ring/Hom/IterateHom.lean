5c397602a86633cb328ac6fd26251243a427d2a2
Scott Morrison <scott.morrison@gmail.com>
2024-04-20 16:50:24 +1000

    missing copyright header

--- Mathlib/Algebra/Ring/Hom/IterateHom.lean
+++ Mathlib/Algebra/Ring/Hom/IterateHom.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2020 Yury Kudryashov. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Yury Kudryashov
+-/
 import Mathlib.Algebra.Ring.Hom.Defs
 import Mathlib.Algebra.GroupPower.IterateHom
 
