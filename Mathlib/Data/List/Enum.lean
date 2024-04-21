491934e8d59ff8067de3b6dd5c3d685be3a36349
Yury G. Kudryashov <urkud@urkud.name>
2024-04-20 23:27:42 -0500

    shake imports

--- Mathlib/Data/List/Enum.lean
+++ Mathlib/Data/List/Enum.lean
@@ -3,7 +3,6 @@ Copyright (c) 2017 Mario Carneiro. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro, Yakov Pechersky, Eric Wieser
 -/
-import Mathlib.Init.Data.List.Basic
 import Mathlib.Tactic.Cases
 import Mathlib.Tactic.Convert
 
