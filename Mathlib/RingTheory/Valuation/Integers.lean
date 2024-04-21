63ff97f9bc8ae1dc33c4069750aa85aa1dda9b47
faenuccio <filippo.nuccio@univ-st-etienne.fr>
2024-04-21 12:19:19 +0200

    addressed reviewer's comment

--- Mathlib/RingTheory/Valuation/Integers.lean
+++ Mathlib/RingTheory/Valuation/Integers.lean
@@ -65,16 +65,7 @@ theorem integer.integers : v.Integers v.integer :=
 
 namespace Integers
 
-variable {v O} [CommRing O] [Algebra O R] (hv : Integers v O)
-
-
-theorem one_of_isUnit {x : O} (hx : IsUnit x) : v (algebraMap O R x) = 1 :=
-  let ⟨u, hu⟩ := hx
-  le_antisymm (hv.2 _) <| by
-    rw [← v.map_one, ← (algebraMap O R).map_one, ← u.mul_inv, ← mul_one (v (algebraMap O R x)), hu,
-      (algebraMap O R).map_mul, v.map_mul]
-    exact mul_le_mul_left' (hv.2 (u⁻¹ : Units O)) _
-#align valuation.integers.one_of_is_unit Valuation.Integers.one_of_isUnit
+variable {v O} [CommRing O] [Algebra O R]
 
 theorem one_of_isUnit' {x : O} (hx : IsUnit x) (H : ∀ x, v (algebraMap O R x) ≤ 1) :
     v (algebraMap O R x) = 1 :=
@@ -83,6 +74,11 @@ theorem one_of_isUnit' {x : O} (hx : IsUnit x) (H : ∀ x, v (algebraMap O R x) 
     rw [← v.map_one, ← (algebraMap O R).map_one, ← u.mul_inv, ← mul_one (v (algebraMap O R x)), hu,
       (algebraMap O R).map_mul, v.map_mul]
     exact mul_le_mul_left' (H (u⁻¹ : Units O)) _
+
+variable (hv : Integers v O)
+
+theorem one_of_isUnit {x : O} (hx : IsUnit x) : v (algebraMap O R x) = 1 :=
+  one_of_isUnit' hx hv.map_le_one
 
 theorem isUnit_of_one {x : O} (hx : IsUnit (algebraMap O R x)) (hvx : v (algebraMap O R x) = 1) :
     IsUnit x :=
