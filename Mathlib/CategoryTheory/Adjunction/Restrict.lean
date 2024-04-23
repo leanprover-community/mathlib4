734aa0a7c679cd08b2d5a1534503b1b4d8bc3a31
dagurtomas <dagurtomas@gmail.com>
2024-04-23 08:54:23 +0000

    start splitting

---
+++ Mathlib/CategoryTheory/Adjunction/Restrict.lean
@@ -0,0 +1,49 @@
+/-
+Copyright (c) 2019 Scott Morrison. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Scott Morrison
+-/
+import Mathlib.CategoryTheory.Conj
+import Mathlib.CategoryTheory.Adjunction.FullyFaithful
+
+namespace CategoryTheory
+
+universe v₁ v₂ u₁ u₂ v₃ v₄ u₃ u₄
+
+open Category Opposite
+
+variable {C : Type u₁} [Category.{v₁} C]
+variable {D : Type u₂} [Category.{v₂} D]
+variable {C' : Type u₃} [Category.{v₃} C']
+variable {D' : Type u₄} [Category.{v₄} D']
+variable (iC : C ⥤ C') (iD : D ⥤ D') {L' : C' ⥤ D'} {R' : D' ⥤ C'} (adj : L' ⊣ R')
+variable {L : C ⥤ D} {R : D ⥤ C} (comm1 : iC ⋙ L' ≅ L ⋙ iD) (comm2 : iD ⋙ R' ≅ R ⋙ iC)
+variable [iC.Full] [iC.Faithful] [iD.Full] [iD.Faithful]
+
+-- TODO: This needs some lemmas describing the produced adjunction, probably in terms of `adj`,
+-- `iC` and `iD`.
+/-- If `C` is a full subcategory of `C'` and `D` is a full subcategory of `D'`, then we can restrict
+an adjunction `L' ⊣ R'` where `L' : C' ⥤ D'` and `R' : D' ⥤ C'` to `C` and `D`.
+The construction here is slightly more general, in that `C` is required only to have a full and
+faithful "inclusion" functor `iC : C ⥤ C'` (and similarly `iD : D ⥤ D'`) which commute (up to
+natural isomorphism) with the proposed restrictions.
+-/
+def Adjunction.restrictFullyFaithful : L ⊣ R :=
+  Adjunction.mkOfHomEquiv
+    { homEquiv := fun X Y =>
+        calc
+          (L.obj X ⟶ Y) ≃ (iD.obj (L.obj X) ⟶ iD.obj Y) := equivOfFullyFaithful iD
+          _ ≃ (L'.obj (iC.obj X) ⟶ iD.obj Y) := Iso.homCongr (comm1.symm.app X) (Iso.refl _)
+          _ ≃ (iC.obj X ⟶ R'.obj (iD.obj Y)) := adj.homEquiv _ _
+          _ ≃ (iC.obj X ⟶ iC.obj (R.obj Y)) := Iso.homCongr (Iso.refl _) (comm2.app Y)
+          _ ≃ (X ⟶ R.obj Y) := (equivOfFullyFaithful iC).symm
+
+      homEquiv_naturality_left_symm := fun {X' X Y} f g => by
+        apply iD.map_injective
+        simpa [Trans.trans] using (comm1.inv.naturality_assoc f _).symm
+      homEquiv_naturality_right := fun {X Y' Y} f g => by
+        apply iC.map_injective
+        suffices R'.map (iD.map g) ≫ comm2.hom.app Y = comm2.hom.app Y' ≫ iC.map (R.map g) by
+          simp [Trans.trans, this]
+        apply comm2.hom.naturality g }
+#align category_theory.adjunction.restrict_fully_faithful CategoryTheory.Adjunction.restrictFullyFaithful
