6b51c84c4e8dece2eb0c1894507daad70d7702b1
Joël Riou <joel.riou@universite-paris-saclay.fr>
2024-04-23 17:44:38 +0200

    generalization to any adjunction

--- Mathlib/CategoryTheory/Sites/Localization.lean
+++ Mathlib/CategoryTheory/Sites/Localization.lean
@@ -76,32 +76,57 @@ lemma W_iff_isIso (hP₁ : Presheaf.IsSheaf J P₁) (hP₂ : Presheaf.IsSheaf J 
 
 end
 
-variable [HasWeakSheafify J A]
+section
 
-lemma W_toSheafify (P : Cᵒᵖ ⥤ A) : J.W (toSheafify J P) := fun Q hQ => by
-  let e : (sheafify J P ⟶ Q) ≃ (P ⟶ Q) :=
-    equivOfFullyFaithful (F := sheafToPresheaf J A).symm.trans
-      ((sheafificationAdjunction J A).homEquiv P ⟨Q, hQ⟩)
+variable {G : (Cᵒᵖ ⥤ A) ⥤ Sheaf J A} (adj : G ⊣ sheafToPresheaf J A)
+
+lemma W_adj_unit_app (P : Cᵒᵖ ⥤ A) : J.W (adj.unit.app P) := fun Q hQ => by
+  let e := equivOfFullyFaithful (F := sheafToPresheaf J A).symm.trans (adj.homEquiv P ⟨Q, hQ⟩)
   convert e.bijective
   ext1 f
   dsimp [e]
-  obtain ⟨g, rfl⟩ : ∃ (g : (presheafToSheaf J A).obj P ⟶ ⟨Q, hQ⟩),
+  obtain ⟨g, rfl⟩ : ∃ (g : G.obj P ⟶ ⟨Q, hQ⟩),
     f = (sheafToPresheaf J A).map g := ⟨⟨f⟩, rfl⟩
   erw [equivOfFullyFaithful_symm_apply]
   rw [Functor.preimage_map]
   erw [Adjunction.homEquiv_unit]
 
-lemma W_iff {P₁ P₂ : Cᵒᵖ ⥤ A} (f : P₁ ⟶ P₂) :
-    J.W f ↔ IsIso ((presheafToSheaf J A).map f) := by
-  rw [← W_postcomp_iff J f (toSheafify J P₂) (J.W_toSheafify P₂),
-    toSheafify_naturality, W_precomp_iff J _ _ (J.W_toSheafify P₁),
-    J.W_iff_isIso _ ((presheafToSheaf J A).obj P₁).cond ((presheafToSheaf J A).obj P₂).cond]
+lemma W_iff_isIso_map_of_adjunction {P₁ P₂ : Cᵒᵖ ⥤ A} (f : P₁ ⟶ P₂) :
+    J.W f ↔ IsIso (G.map f) := by
+  rw [← W_postcomp_iff J f _ (J.W_adj_unit_app adj P₂)]
+  erw [adj.unit.naturality f]
+  rw [W_precomp_iff J _ _ (J.W_adj_unit_app adj P₁),
+    J.W_iff_isIso _ (G.obj P₁).cond (G.obj P₂).cond]
+  dsimp
   constructor
-  · intro (h : IsIso ((sheafToPresheaf J A).map ((presheafToSheaf J A).map f)))
+  · intro (h : IsIso ((sheafToPresheaf J A).map (G.map f)))
     exact isIso_of_reflects_iso _ (sheafToPresheaf J A)
   · intro
-    change IsIso ((sheafToPresheaf J A).map ((presheafToSheaf J A).map f))
+    change IsIso ((sheafToPresheaf J A).map (G.map f))
     infer_instance
+
+lemma W_eq_inverseImage_isomorphisms_of_adjunction :
+    J.W = (MorphismProperty.isomorphisms _).inverseImage G := by
+  ext P₁ P₂ f
+  rw [J.W_iff_isIso_map_of_adjunction adj]
+  rfl
+
+lemma isLocalization_of_adjunction : G.IsLocalization J.W := by
+  rw [J.W_eq_inverseImage_isomorphisms_of_adjunction adj]
+  exact adj.isLocalization
+
+end
+
+section
+
+variable [HasWeakSheafify J A]
+
+lemma W_toSheafify (P : Cᵒᵖ ⥤ A) : J.W (toSheafify J P) :=
+  J.W_adj_unit_app (sheafificationAdjunction J A) P
+
+lemma W_iff {P₁ P₂ : Cᵒᵖ ⥤ A} (f : P₁ ⟶ P₂) :
+    J.W f ↔ IsIso ((presheafToSheaf J A).map f) :=
+  J.W_iff_isIso_map_of_adjunction (sheafificationAdjunction J A) f
 
 variable (A)
 
@@ -115,6 +140,10 @@ instance : (presheafToSheaf J A).IsLocalization J.W := by
   rw [W_eq_inverseImage_isomorphisms]
   exact (sheafificationAdjunction J A).isLocalization
 
+end
+
 end GrothendieckTopology
 
 end CategoryTheory
+
+#lint
