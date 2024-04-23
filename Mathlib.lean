bdfbc7fa97ddce28d63bfcea7a6d60c1e7258bed
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2024-04-23 14:29:29 +0000

    chore: split RingTheory.Nilpotent (#12184)
    `Mathlib.RingTheory.Nilpotent` has a few very simple definitions (`Mathlib.Data.Nat.Lattice` is sufficient to state them), but needs some pretty heavy imports (ideals, linear algebra) towards the end. This change moves the heavier parts into a new file.

--- Mathlib.lean
+++ Mathlib.lean
@@ -3443,7 +3443,9 @@ import Mathlib.RingTheory.MvPowerSeries.Basic
 import Mathlib.RingTheory.MvPowerSeries.Inverse
 import Mathlib.RingTheory.MvPowerSeries.Trunc
 import Mathlib.RingTheory.Nakayama
-import Mathlib.RingTheory.Nilpotent
+import Mathlib.RingTheory.Nilpotent.Basic
+import Mathlib.RingTheory.Nilpotent.Defs
+import Mathlib.RingTheory.Nilpotent.Lemmas
 import Mathlib.RingTheory.Noetherian
 import Mathlib.RingTheory.NonUnitalSubring.Basic
 import Mathlib.RingTheory.NonUnitalSubsemiring.Basic
