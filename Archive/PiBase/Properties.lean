dce81a847d280704a429f83b66083de5a2a27bbf
Steven Clontz <steven.clontz@gmail.com>
2024-04-24 20:25:46 +0000

    switch from def to abbrev

--- Archive/PiBase/Properties.lean
+++ Archive/PiBase/Properties.lean
@@ -2,24 +2,30 @@ import Mathlib.Topology.Basic
 import Mathlib.Topology.Separation
 import Mathlib.Order.Filter.Basic
 import Mathlib.Logic.Nontrivial.Basic
+import Mathlib.Topology.Category.CompHaus.Basic
 
-open Filter Topology Nontrivial
+open Filter Topology Nontrivial CompHaus
 
 universe u
 
 namespace πBase
 
-def P1 (X : Type u) [TopologicalSpace X] := T0Space X
+abbrev P1 (X : Type u) [TopologicalSpace X] := T0Space X
 
-def P2 (X : Type u) [TopologicalSpace X] := T1Space X
+abbrev P2 (X : Type u) [TopologicalSpace X] := T1Space X
 
-def P78 (X : Type u) [TopologicalSpace X] := Finite X
+abbrev P78 (X : Type u) [TopologicalSpace X] := Finite X
 
-def P99 (X : Type u) [TopologicalSpace X] :=
+abbrev P99 (X : Type u) [TopologicalSpace X] :=
   ∀ x y : X, ∀ f : ℕ → X,
     Tendsto f atTop (𝓝 x) → Tendsto f atTop (𝓝 y) →
     x = y
 
-def P125 (X : Type u) [TopologicalSpace X] := Nontrivial X
+abbrev P125 (X : Type u) [TopologicalSpace X] := Nontrivial X
+
+-- def P171 (X : Type u) [TopologicalSpace X] :=
+--   ∀ K, CompHaus K →
+--     ∀ f : K → X, ∀ k l : K, f k ≠ f l →
+--     ∃ N_k : 𝓝 k, ∃ N_l : 𝓝 l, f'' N_k ∩ f'' N_l = ∅
 
 end πBase
