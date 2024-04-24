878f598fa7c9983c516ac9c3f0e8ee975ace3f78
JADekker <114015169+JADekker@users.noreply.github.com>
2024-04-24 22:05:15 +0200

    shake

--- Mathlib/MeasureTheory/Measure/Separable.lean
+++ Mathlib/MeasureTheory/Measure/Separable.lean
@@ -3,7 +3,7 @@ Copyright (c) 2024 Josha Dekker. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Josha Dekker
 -/
-import Mathlib.MeasureTheory.Measure.Regular
+import Mathlib.MeasureTheory.Measure.MeasureSpaceDef
 
 /-!
 # Separable measures
