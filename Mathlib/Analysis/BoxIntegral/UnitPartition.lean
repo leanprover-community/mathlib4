3e714cf0a9749424860b1be202dccd6e9bac47f6
Xavier Roblot <roblot@math.univ-lyon1.fr>
2024-04-24 17:24:06 +0200

    Lint

--- Mathlib/Analysis/BoxIntegral/UnitPartition.lean
+++ Mathlib/Analysis/BoxIntegral/UnitPartition.lean
@@ -1,7 +1,7 @@
 /-
- Copyright (c) 2024 Xavier Roblot. All rights reserved.
- Released under Apache 2.0 license as described in the file LICENSE.
- Authors: Xavier Roblot
+Copyright (c) 2024 Xavier Roblot. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Xavier Roblot
  -/
 import Mathlib.Algebra.Module.Zlattice.Basic
 import Mathlib.Analysis.BoxIntegral.Integrability
