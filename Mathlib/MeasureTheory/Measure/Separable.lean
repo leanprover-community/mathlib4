e72a00bdf9a653acca05c4c8af54ee21b07f6577
JADekker <114015169+JADekker@users.noreply.github.com>
2024-04-24 12:33:49 +0200

    feat : define separable measures

---
+++ Mathlib/MeasureTheory/Measure/Separable.lean
@@ -0,0 +1,40 @@
+/-
+Copyright (c) 2024 Josha Dekker. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Josha Dekker
+-/
+import Mathlib.MeasureTheory.Measure.Regular
+
+/-!
+# Separable measures
+
+## Main definitions
+
+* `IsSeparable`: A measure `μ` is separable if there is a separable set `S` such that
+  `μ S = μ Set.univ`.
+
+## Main statements
+
+*
+
+## Notation
+
+
+## Implementation details
+
+-/
+
+open scoped ENNReal
+
+namespace MeasureTheory
+
+variable {α : Type*} {mα : MeasurableSpace α} {μ : Measure α}
+
+/-- A measure `μ` is separable if there is a separable set `S` such that `μ S = μ Set.univ`. -/
+def IsSeparable [TopologicalSpace α] (μ : Measure α) : Prop :=
+  ∃ S : Set α, TopologicalSpace.IsSeparable S ∧ μ S = μ Set.univ
+
+namespace IsSeparable
+
+end IsSeparable
+end MeasureTheory
