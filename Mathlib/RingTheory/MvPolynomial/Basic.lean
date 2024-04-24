80e77ab1fbfa66115132480e802e32e5c6ce6e49
judithludwig <judith.ludwig@iwr.uni-heidelberg.de>
2024-04-24 09:40:44 +0200

    moved as suggested

--- Mathlib/RingTheory/MvPolynomial/Basic.lean
+++ Mathlib/RingTheory/MvPolynomial/Basic.lean
@@ -135,15 +135,15 @@ def basisMonomials : Basis (σ →₀ ℕ) R (MvPolynomial σ R) :=
   Finsupp.basisSingleOne
 #align mv_polynomial.basis_monomials MvPolynomial.basisMonomials
 
-/-- The `R`-module `MvPolynomial σ R` is free. -/
-instance : Module.Free R (MvPolynomial σ R) :=
-  Module.Free.of_basis (MvPolynomial.basisMonomials σ R)
-
 @[simp]
 theorem coe_basisMonomials :
     (basisMonomials σ R : (σ →₀ ℕ) → MvPolynomial σ R) = fun s => monomial s 1 :=
   rfl
 #align mv_polynomial.coe_basis_monomials MvPolynomial.coe_basisMonomials
+
+/-- The `R`-module `MvPolynomial σ R` is free. -/
+instance : Module.Free R (MvPolynomial σ R) :=
+  Module.Free.of_basis (MvPolynomial.basisMonomials σ R)
 
 theorem linearIndependent_X : LinearIndependent R (X : σ → MvPolynomial σ R) :=
   (basisMonomials σ R).linearIndependent.comp (fun s : σ => Finsupp.single s 1)
