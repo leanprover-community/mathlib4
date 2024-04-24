072e6860411969e657cdadd1a140feee22d79418
Yaël Dillies <yael.dillies@gmail.com>
2024-04-24 13:40:26 +0000

    chore: Move UFD instance for `Nat` This reduces the imports to `RingTheory.Int.Basic`. Maybe those instances could go to a new file `RingTheory.UniqueFactorizationDomain.Nat` to avoid having to import `Data.Nat.Factors` in `RingTheory.UniqueFactorizationDomain`, but also that new import is much more lightweight than the existing ones.

--- Mathlib/RingTheory/Int/Basic.lean
+++ Mathlib/RingTheory/Int/Basic.lean
@@ -3,10 +3,13 @@ Copyright (c) 2018 Johannes Hölzl. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl, Jens Wagemaker, Aaron Anderson
 -/
+import Mathlib.Algebra.CharZero.Lemmas
 import Mathlib.Algebra.EuclideanDomain.Basic
-import Mathlib.Data.Nat.Factors
+import Mathlib.Algebra.EuclideanDomain.Instances
+import Mathlib.Data.Nat.Prime
+import Mathlib.GroupTheory.Subgroup.ZPowers
 import Mathlib.RingTheory.Coprime.Basic
-import Mathlib.RingTheory.PrincipalIdealDomain
+import Mathlib.RingTheory.Ideal.Basic
 
 #align_import ring_theory.int.basic from "leanprover-community/mathlib"@"e655e4ea5c6d02854696f97494997ba4c31be802"
 
@@ -29,30 +32,6 @@ prime, irreducible, natural numbers, integers, normalization monoid, gcd monoid,
 greatest common divisor, prime factorization, prime factors, unique factorization,
 unique factors
 -/
-
-
-namespace Nat
-
-instance : WfDvdMonoid ℕ :=
-  ⟨by
-    refine RelHomClass.wellFounded
-      (⟨fun x : ℕ => if x = 0 then (⊤ : ℕ∞) else x, ?_⟩ : DvdNotUnit →r (· < ·)) wellFounded_lt
-    intro a b h
-    cases' a with a
-    · exfalso
-      revert h
-      simp [DvdNotUnit]
-    cases b
-    · simpa [succ_ne_zero] using WithTop.coe_lt_top (a + 1)
-    cases' dvd_and_not_dvd_iff.2 h with h1 h2
-    simp only [succ_ne_zero, cast_lt, if_false]
-    refine lt_of_le_of_ne (Nat.le_of_dvd (Nat.succ_pos _) h1) fun con => h2 ?_
-    rw [con]⟩
-
-instance : UniqueFactorizationMonoid ℕ :=
-  ⟨fun {_} => Nat.irreducible_iff_prime⟩
-
-end Nat
 
 namespace Int
 
@@ -259,42 +238,6 @@ theorem Int.exists_prime_and_dvd {n : ℤ} (hn : n.natAbs ≠ 1) : ∃ p, Prime 
   exact ⟨p, Nat.prime_iff_prime_int.mp pp, Int.natCast_dvd.mpr pd⟩
 #align int.exists_prime_and_dvd Int.exists_prime_and_dvd
 
-open UniqueFactorizationMonoid
-
-theorem Nat.factors_eq {n : ℕ} : normalizedFactors n = n.factors := by
-  cases n with
-  | zero => simp
-  | succ n =>
-    rw [← Multiset.rel_eq, ← associated_eq_eq]
-    apply UniqueFactorizationMonoid.factors_unique irreducible_of_normalized_factor _
-    · rw [Multiset.prod_coe, Nat.prod_factors n.succ_ne_zero]
-      apply normalizedFactors_prod (Nat.succ_ne_zero _)
-    · intro x hx
-      rw [Nat.irreducible_iff_prime, ← Nat.prime_iff]
-      exact Nat.prime_of_mem_factors hx
-#align nat.factors_eq Nat.factors_eq
-
-theorem Nat.factors_multiset_prod_of_irreducible {s : Multiset ℕ}
-    (h : ∀ x : ℕ, x ∈ s → Irreducible x) : normalizedFactors s.prod = s := by
-  rw [← Multiset.rel_eq, ← associated_eq_eq]
-  apply
-    UniqueFactorizationMonoid.factors_unique irreducible_of_normalized_factor h
-      (normalizedFactors_prod _)
-  rw [Ne, Multiset.prod_eq_zero_iff]
-  intro con
-  exact not_irreducible_zero (h 0 con)
-#align nat.factors_multiset_prod_of_irreducible Nat.factors_multiset_prod_of_irreducible
-
-theorem induction_on_primes {P : ℕ → Prop} (h₀ : P 0) (h₁ : P 1)
-    (h : ∀ p a : ℕ, p.Prime → P a → P (p * a)) (n : ℕ) : P n := by
-  apply UniqueFactorizationMonoid.induction_on_prime
-  · exact h₀
-  · intro n h
-    rw [Nat.isUnit_iff.1 h]
-    exact h₁
-  · intro a p _ hp ha
-    exact h p a hp.nat_prime ha
-#align induction_on_primes induction_on_primes
 
 theorem Int.associated_natAbs (k : ℤ) : Associated k k.natAbs :=
   associated_of_dvd_dvd (Int.dvd_natCast.mpr dvd_rfl) (Int.natAbs_dvd.mpr dvd_rfl)
@@ -328,27 +271,5 @@ theorem span_natAbs (a : ℤ) : Ideal.span ({(a.natAbs : ℤ)} : Set ℤ) = Idea
   exact (associated_natAbs _).symm
 #align int.span_nat_abs Int.span_natAbs
 
-section bit
-set_option linter.deprecated false
-
-theorem eq_pow_of_mul_eq_pow_bit1_left {a b c : ℤ} (hab : IsCoprime a b) {k : ℕ}
-    (h : a * b = c ^ bit1 k) : ∃ d, a = d ^ bit1 k := by
-  obtain ⟨d, hd⟩ := exists_associated_pow_of_mul_eq_pow' hab h
-  replace hd := hd.symm
-  rw [associated_iff_natAbs, natAbs_eq_natAbs_iff, ← neg_pow_bit1] at hd
-  obtain rfl | rfl := hd <;> exact ⟨_, rfl⟩
-#align int.eq_pow_of_mul_eq_pow_bit1_left Int.eq_pow_of_mul_eq_pow_bit1_left
-
-theorem eq_pow_of_mul_eq_pow_bit1_right {a b c : ℤ} (hab : IsCoprime a b) {k : ℕ}
-    (h : a * b = c ^ bit1 k) : ∃ d, b = d ^ bit1 k :=
-  eq_pow_of_mul_eq_pow_bit1_left hab.symm (by rwa [mul_comm] at h)
-#align int.eq_pow_of_mul_eq_pow_bit1_right Int.eq_pow_of_mul_eq_pow_bit1_right
-
-theorem eq_pow_of_mul_eq_pow_bit1 {a b c : ℤ} (hab : IsCoprime a b) {k : ℕ}
-    (h : a * b = c ^ bit1 k) : (∃ d, a = d ^ bit1 k) ∧ ∃ e, b = e ^ bit1 k :=
-  ⟨eq_pow_of_mul_eq_pow_bit1_left hab h, eq_pow_of_mul_eq_pow_bit1_right hab h⟩
-#align int.eq_pow_of_mul_eq_pow_bit1 Int.eq_pow_of_mul_eq_pow_bit1
-
-end bit
-
 end Int
+#minimize_imports
