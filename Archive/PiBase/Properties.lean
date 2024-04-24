8db105cbc220872a58754850288497f287f8baf9
Steven Clontz <steven.clontz@gmail.com>
2024-04-24 02:13:42 +0000

    prove T226

--- Archive/PiBase/Properties.lean
+++ Archive/PiBase/Properties.lean
@@ -9,17 +9,13 @@ variable (X : Type u) [TopologicalSpace X]
 
 namespace πBase
 
-def P1 :=
-  -- ∀ x y : X, Inseparable x y → x = y
-  T0Space X
+def P1 := T0Space X
 
-def P2 :=
-  -- ∀ x : X, IsClosed ({x} : Set X)
-  T1Space X
+def P2 := T1Space X
 
 def P99 :=
   ∀ x y : X, ∀ f : ℕ → X,
-    Tendsto f atTop (𝓝 x) ∧ Tendsto f atTop (𝓝 y) →
+    Tendsto f atTop (𝓝 x) → Tendsto f atTop (𝓝 y) →
     x = y
 
 end πBase
--- Archive/PiBase/Theorems.lean
+++ Archive/PiBase/Theorems.lean
@@ -1,9 +1,8 @@
 import Mathlib.Topology.Basic
-import Mathlib.Topology.Inseparable
 import Mathlib.Data.Set.Basic
-import Mathlib.Topology.Separation
+import Mathlib.Order.Filter.Basic
 import Archive.PiBase.Properties
-open Topology TopologicalSpace Set
+open Topology Set Filter
 
 universe u
 variable (X : Type u) [TopologicalSpace X]
@@ -14,5 +13,36 @@ theorem T119 : P2 X → P1 X := by
   rw [P1, P2]
   intro h
   exact T1Space.t0Space
+
+theorem T226 : P99 X → P2 X := by
+  rw [P99, P2]
+  intro h
+  rw [t1Space_iff_exists_open]
+  intro x y
+  contrapose; simp at *
+  intro hyp
+  let f : ℕ → X := fun _ ↦ y
+  replace h : Tendsto f atTop (𝓝 x) →
+      Tendsto f atTop (𝓝 y) → x = y := by
+    apply h
+  apply h
+  · intro N NNx
+    have yinN : y ∈ N := by
+      rw [mem_nhds_iff] at NNx
+      rcases NNx with ⟨ U, ⟨ UsubN, Uopen, xinU⟩ ⟩
+      apply UsubN
+      apply hyp
+      exact Uopen
+      exact xinU
+    apply mem_map.mpr
+    simp
+    use 0
+    simp
+    intro b
+    have : f b = y := by
+      simp
+    rw [this]
+    exact yinN
+  · exact tendsto_const_nhds
 
 end πBase
