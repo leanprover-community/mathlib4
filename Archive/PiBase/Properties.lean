5addb289f33c1db1bb0da5fb739da521178151f6
Steven Clontz <steven.clontz@gmail.com>
2024-04-24 14:51:15 +0000

    move arguments into definitions

--- Archive/PiBase/Properties.lean
+++ Archive/PiBase/Properties.lean
@@ -5,15 +5,14 @@ import Mathlib.Order.Filter.Basic
 open Filter Topology
 
 universe u
-variable (X : Type u) [TopologicalSpace X]
 
 namespace πBase
 
-def P1 := T0Space X
+def P1 (X : Type u) [TopologicalSpace X] := T0Space X
 
-def P2 := T1Space X
+def P2 (X : Type u) [TopologicalSpace X] := T1Space X
 
-def P99 :=
+def P99 (X : Type u) [TopologicalSpace X] :=
   ∀ x y : X, ∀ f : ℕ → X,
     Tendsto f atTop (𝓝 x) → Tendsto f atTop (𝓝 y) →
     x = y
