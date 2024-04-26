ab4e8594979476b51fff8414699eee2aed22b418
Chris Birkbeck <c.birkbeck@ucl.ac.uk>
2024-04-26 15:35:56 +0100

    name update

--- Mathlib/Analysis/Complex/UpperHalfPlane/Topology.lean
+++ Mathlib/Analysis/Complex/UpperHalfPlane/Topology.lean
@@ -74,15 +74,15 @@ instance : NoncompactSpace ℍ := by
 instance : LocallyCompactSpace ℍ :=
   openEmbedding_coe.locallyCompactSpace
 
-section slices
+section strips
 
 /-- The vertical strip of width A and height B. -/
 def verticalStrip (A B : ℝ) := {z : ℍ | |z.re| ≤ A ∧ B ≤ z.im}
 
-theorem slice_mem_iff (A B : ℝ) (z : ℍ) : z ∈ verticalStrip A B ↔ |z.re| ≤ A ∧ B ≤ z.im :=
+theorem strip_mem_iff (A B : ℝ) (z : ℍ) : z ∈ verticalStrip A B ↔ |z.re| ≤ A ∧ B ≤ z.im :=
   Iff.rfl
 
-lemma subset_slice_of_isCompact {K : Set ℍ} (hK : IsCompact K) :
+lemma subset_strip_of_isCompact {K : Set ℍ} (hK : IsCompact K) :
     ∃ A B : ℝ, 0 < B ∧ K ⊆ verticalStrip A B := by
   rcases K.eq_empty_or_nonempty with rfl | hne
   · exact ⟨1, 1, Real.zero_lt_one, empty_subset _⟩
@@ -90,6 +90,6 @@ lemma subset_slice_of_isCompact {K : Set ℍ} (hK : IsCompact K) :
   obtain ⟨v, _, hv⟩ := hK.exists_isMaxOn hne (_root_.continuous_abs.comp continuous_re).continuousOn
   exact ⟨|re v|, im u, u.im_pos, fun k hk ↦ ⟨isMaxOn_iff.mp hv _ hk, isMinOn_iff.mp hu _ hk⟩⟩
 
-end slices
+end strips
 
 end UpperHalfPlane
