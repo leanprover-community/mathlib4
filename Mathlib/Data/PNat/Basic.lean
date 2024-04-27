0b8a6e6ad6085c3f0be854bb9fd2c154ebf193c0
Ralf Stephan <ralf@ark.in-berlin.de>
2024-04-27 08:33:16 +0200

    typo

--- Mathlib/Data/PNat/Basic.lean
+++ Mathlib/Data/PNat/Basic.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2017 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Mario Carneiro, Ralf Stephan, Neil Strickland, Ruben van de Velde
+Authors: Mario Carneiro, Ralf Stephan, Neil Strickland, Ruben Van de Velde
 -/
 import Mathlib.Data.PNat.Defs
 import Mathlib.Data.Nat.Bits
