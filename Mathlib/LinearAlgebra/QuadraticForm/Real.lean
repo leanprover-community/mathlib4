9805dedefd6a3c56177c18e6e10340cae124fff2
Christopher Hoskin <christopher.hoskin@gmail.com>
2024-04-28 12:03:53 +0100

    fix after merge

--- Mathlib/LinearAlgebra/QuadraticForm/Real.lean
+++ Mathlib/LinearAlgebra/QuadraticForm/Real.lean
@@ -45,7 +45,7 @@ noncomputable def isometryEquivSignWeightedSumSquares (w : ι → ℝ) :
     by positivity
   have hwu : ∀ i, w i / |(u i : ℝ)| = sign (w i) := fun i ↦ by
     by_cases hi : w i = 0 <;> field_simp [hi, u]
-  convert (weightedSumSquares ℝ w).isometryEquivBasisRepr
+  convert QuadraticForm.isometryEquivBasisRepr (weightedSumSquares ℝ w)
     ((Pi.basisFun ℝ ι).unitsSMul fun i => .mk0 _ (hu i))
   ext1 v
   classical
