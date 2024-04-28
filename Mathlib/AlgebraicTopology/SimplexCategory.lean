69d8a2934dc9dd22b85d67b91660c812f67a2978
Joël Riou <joel.riou@universite-paris-saclay.fr>
2024-04-28 16:00:09 +0200

    fixing the build

--- Mathlib/AlgebraicTopology/SimplexCategory.lean
+++ Mathlib/AlgebraicTopology/SimplexCategory.lean
@@ -490,8 +490,7 @@ instance : skeletalFunctor.EssSurj where
         show f (f.symm i) ≤ f (f.symm j)
         simpa only [OrderIso.apply_symm_apply]⟩⟩
 
-noncomputable instance isEquivalence : skeletalFunctor.IsEquivalence :=
-  Functor.IsEquivalence.ofFullyFaithfullyEssSurj skeletalFunctor
+noncomputable instance isEquivalence : skeletalFunctor.IsEquivalence where
 #align simplex_category.skeletal_functor.is_equivalence SimplexCategory.SkeletalFunctor.isEquivalence
 
 end SkeletalFunctor
