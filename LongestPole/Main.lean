78f15853302ca8359efd184d6b805dfdeab90809
Bolton Bailey <bolton.bailey@gmail.com>
2024-04-28 01:15:34 -0500

    fix d

--- LongestPole/Main.lean
+++ LongestPole/Main.lean
@@ -107,7 +107,7 @@ def totalInstructions (instructions : NameMap Float) (graph : NameMap (Array Nam
     NameMap Float :=
   let transitive := graph.transitiveClosure
   transitive.filterMap
-    fun n s => some <| s.fold (init := (instructions.find? n).getD 0)
+    fun n s => some <| s.fold (init := instructions.findD n 0)
       fun t n' => t + (instructions.findD n' 0)
 
 /-- Convert a float to a string with a fixed number of decimal places. -/
