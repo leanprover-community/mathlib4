8645a6fccc05db5e81b38587912a32f232f3482f
Eric Wieser <wieser.eric@gmail.com>
2024-04-28 21:47:35 +0000

    blank lines

--- test/DualNumber.lean
+++ test/DualNumber.lean
@@ -6,16 +6,13 @@ open DualNumber
 #guard_msgs in
 #eval (0 : ℕ[ε])
 
-
 /-- info: 2 + 0*ε -/
 #guard_msgs in
 #eval (2 : ℕ[ε])
 
-
 /-- info: 6 + 0*ε -/
 #guard_msgs in
 #eval (2 + 4 : ℕ[ε])
-
 
 /-- info: 2 + 0*ε + (0 + 0*ε)*ε -/
 #guard_msgs in
