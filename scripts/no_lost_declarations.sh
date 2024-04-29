b09fa8781a2dba40bad91ff2b40182065312ff5c
adomani <adomani@gmail.com>
2024-04-29 22:05:34 +0100

    Use Eric's `...` diff!

--- scripts/no_lost_declarations.sh
+++ scripts/no_lost_declarations.sh
@@ -3,18 +3,14 @@
 ## we narrow the diff to lines beginning with `theorem`, `lemma` and a few other commands
 begs="(theorem|lemma|inductive|structure|def|class|instance)"
 
-## the reference commit:
-## * it *should* be the version of master from the last merge
-## * it probably is the commit from where the PR branched from master
+## if an input commit is given, compute the diff with that, otherwise, use the git-magic `...`
 if [ -n "${1}" ]; then
-  commit="${1}"
+  git diff --unified=0 "${1}"
 else
-  commit="$( git merge-base origin/master -- "origin/$( git rev-parse --abbrev-ref HEAD )" )"
-fi;
-
-## extract lines that begin with '[+-]' followed by the input `theorem` or `lemma`
-## in the `git diff`
-git diff --unified=0 "${commit}" |
+  git diff origin/master...HEAD
+fi |
+  ## extract lines that begin with '[+-]' followed by the input `theorem` or `lemma`
+  ## in the `git diff`
   awk -v regex="^[+-]${begs}" 'BEGIN{ paired=0; added=0; removed=0 }
     ($0 ~ regex){
       pm=substr($0, 1, 1)
@@ -33,11 +29,11 @@ git diff --unified=0 "${commit}" |
     print fin| "sort -k3"; close("sort -k3")
     printf("---\n* %s  added declarations\n* %s  removed declarations\n* %s  paired declarations", added, removed, paired)
   }'
-printf $'\n---\n\nReference commit: `%s`\n\n---\n\nYou can run this locally using
+printf $'\n---\n\nYou can run this locally using
 ```bash
 ./scripts/no_lost_declarations.sh <optional_commit>
 ```
-' "${commit}"
+'
 
  : <<ReferenceTest
 theorem hello
