d246abda5bf152754693315b35a08de739b66f4e
Dagur Tómas Ásgeirsson <dagurtomas@gmail.com>
2024-04-29 22:17:07 +0000

    Update Mathlib/Topology/Category/LightProfinite/Limits.lean
    Co-authored-by: Riccardo Brasca <riccardo.brasca@gmail.com>

--- Mathlib/Topology/Category/LightProfinite/Limits.lean
+++ Mathlib/Topology/Category/LightProfinite/Limits.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2023 Dagur Asgeirsson. All rights reserved.
+Copyright (c) 2024 Dagur Asgeirsson. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Dagur Asgeirsson
 -/
