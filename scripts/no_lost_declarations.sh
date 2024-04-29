a193ba2131783c6b7873b5bdbed948b4f4dddf31
adomani <adomani@gmail.com>
2024-04-29 13:48:13 +0100

    use awk

--- scripts/no_lost_declarations.sh
+++ scripts/no_lost_declarations.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ## we narrow the diff to lines beginning with `theorem`, `lemma` and a few other commands
-begs="\(theorem\|lemma\|inductive\|structure\|def\|class\|instance\)"
+begs="(theorem|lemma|inductive|structure|def|class|instance)"
 
 ## the reference commit:
 ## * it *should* be the version of master from the last merge
@@ -15,24 +15,28 @@ fi;
 ## extract lines that begin with '[+-]' followed by the input `theorem` or `lemma`
 ## in the `git diff`
 git diff --unified=0 "${commit}" |
-  grep "^[+-]${begs}" |
-  sed 's=^\(.\)\(.*\)$=\2 \1=' |
-  awk 'BEGIN{ paired=0; mismatched=0 }{
-    pm=$NF
-    gsub(/..$/, "")
-    acc[$0]=acc[$0] pm
-  } END {
+  awk -v regex="^[+-]${begs}" 'BEGIN{ paired=0; mismatched=0 }
+    ($0 ~ regex){
+      pm=substr($0, 1, 1)
+      rest=substr($0, 2)
+      acc[rest]=acc[rest] pm
+    }
+  END {
     fin=""
     for(res in acc) {
       pm=acc[res]
       if(pm != "-+") {
         mismatched++
-        fin=fin sprintf("%s%s\n", pm == "+" ? pm "\\" : pm "\\", res)
+        fin=fin sprintf("%s/%s\n", pm == "+" ? pm : pm, res)
       } else paired++
     }
-    print fin| "sort -k2"; close("sort -k2")
+    print fin| "sort -k2 | column -s/ -t"; close("sort -k2 | column -s/ -t")
     printf("---\n%s  mismatched declarations\n%s  paired declarations", mismatched, paired)
-  }' |
-  column -s'\' -t
+  }'
 printf $'\nReference commit: %s\n\nYou can run this locally using
 ./scripts/no_lost_declarations.sh <optional_commit>\n' "${commit}"
+
+ : <<BYE
+theorem hello
+inductives counted even if it is
+BYE
