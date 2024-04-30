af4141e61ab50e1f1894c1745e18340e68081705
Scott Morrison <scott.morrison@gmail.com>
2024-05-01 09:30:58 +1000

    fix

--- Mathlib/Data/Nat/Bits.lean
+++ Mathlib/Data/Nat/Bits.lean
@@ -124,7 +124,7 @@ lemma bodd_add_div2 : ∀ n, cond (bodd n) 1 0 + 2 * div2 n = n
     simp only [bodd_succ, Bool.cond_not, div2_succ, Nat.mul_comm]
     refine' Eq.trans _ (congr_arg succ (bodd_add_div2 n))
     cases bodd n
-    · simp; omega
+    · simp
     · simp; omega
 #align nat.bodd_add_div2 Nat.bodd_add_div2
 
