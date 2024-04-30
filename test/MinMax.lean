7ca00a3a8c2e0bcc5f1165ad1c05708bbad44682
adomani <adomani@gmail.com>
2024-04-30 01:28:58 +0100

    tests and docs

--- test/MinMax.lean
+++ test/MinMax.lean
@@ -2,51 +2,16 @@ import Mathlib.Tactic.MinMax
 
 open Mathlib.ToNatDegree
 
-/-
-true
+run_cmd Lean.Elab.Command.liftTermElabM do
+  guard (splitUpper "SemilatticeSup" == ["Semilattice", "Sup"])
 
--/
-#eval do
-  let res := "SemilatticeSup"
-  IO.println (splitUpper res == ["Semilattice", "Sup"])
+run_cmd Lean.Elab.Command.liftTermElabM do
+  guard <| "hello I am `WithTop' α` and also `top'`" ==
+    stringReplacements "hello I am `WithBot' α` and also `bot'`"
 
-/-
-"hello I am `WithTop' α` and also `top'`"
+run_cmd Lean.Elab.Command.liftTermElabM do
+  guard (["le", "inf"] == swapWords ["inf", "le"])
 
--/
-#eval
-  stringReplacements "hello I am `WithBot' α` and also `bot'`"
-
-/-
-[['a', 'b', 'c'], ['D', 'e'], ['F']]
-
--/
-#eval "abcDeF".toList.groupBy fun a b => a.isUpper || (a.isLower && b.isLower)
-
-/-
-[le, inf]
-
--/
-#eval do
-  let res := ["le", "inf"]
-  let res := ["inf", "le"]
-  IO.println (swapWords res) -- == ["Semilattice", "Sup"])
-
-/-
-[NNReal, HHi, ,,  , but,  , it, ', s,  ]
-false
-
--/
-#eval do
-  let res := "NNRealHHi, but it's "
-  IO.println (splitUpper res)
-  IO.println (splitUpper res == ["NNReal", "HHi"])
-
-/-
-[hello,  , I,  , am,  , `, aDocString,  , 0, `,  , and,  , I,  , have, ,,  , some,  , punctuation, .]
-
--/
-#eval do
-  let test := "hello I am `aDocString 0` and I have, some punctuation."
-  let gps := test.toList.groupBy fun x y : Char => x.isAlpha && y.isAlpha
-  IO.println <| gps.map fun w => (⟨w⟩ : String)
+run_cmd Lean.Elab.Command.liftTermElabM do
+  guard (splitUpper "NNRealHHi, but it's " ==
+    ["NNReal", "HHi", ",", " ", "but", " ", "it", "'", "s", " "])
