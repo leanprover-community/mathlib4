8e8017acb6ebcdb0473ce3f36895da338bc300aa
Riccardo Brasca <riccardo.brasca@gmail.com>
2024-04-30 13:35:29 +0200

    fix build

--- Mathlib/NumberTheory/NumberField/Units/DirichletTheorem.lean
+++ Mathlib/NumberTheory/NumberField/Units/DirichletTheorem.lean
@@ -46,127 +46,7 @@ open NumberField NumberField.InfinitePlace NumberField.Units BigOperators
 
 variable (K : Type*) [Field K] [NumberField K]
 
-theorem Rat.RingOfIntegers.isUnit_iff {x : 𝓞 ℚ} :
-    IsUnit x ↔ (x : ℚ) = 1 ∨ (x : ℚ) = -1 := by
-  simp_rw [(isUnit_map_iff (Rat.ringOfIntegersEquiv : 𝓞 ℚ →+* ℤ) x).symm, Int.isUnit_iff,
-    RingEquiv.coe_toRingHom, RingEquiv.map_eq_one_iff, RingEquiv.map_eq_neg_one_iff, ←
-    Subtype.coe_injective.eq_iff]; rfl
-#align rat.ring_of_integers.is_unit_iff Rat.RingOfIntegers.isUnit_iff
-
-end Rat
-
-variable (K : Type*) [Field K]
-
-section IsUnit
-
-variable {K}
-
-theorem NumberField.isUnit_iff_norm [NumberField K] {x : 𝓞 K} :
-    IsUnit x ↔ |((RingOfIntegers.norm ℚ x) : ℚ)| = 1 := by
-  convert (RingOfIntegers.isUnit_norm ℚ (F := K)).symm
-  rw [← abs_one, abs_eq_abs, ← Rat.RingOfIntegers.isUnit_iff]
-#align is_unit_iff_norm NumberField.isUnit_iff_norm
-
-end IsUnit
-
-namespace NumberField.Units
-
-section coe
-
--- TODO: figure out precise coercion
-instance : CoeHTC (𝓞 K)ˣ K :=
-  ⟨fun x => algebraMap _ K (Units.val x)⟩
-
-theorem coe_injective : Function.Injective ((↑) : (𝓞 K)ˣ → K) :=
-  RingOfIntegers.coe_injective.comp Units.ext
-
-variable {K}
-
-theorem coe_mul (x y : (𝓞 K)ˣ) : ((x * y : (𝓞 K)ˣ) : K) = (x : K) * (y : K) := rfl
-
-theorem coe_pow (x : (𝓞 K)ˣ) (n : ℕ) : ((x ^ n : (𝓞 K)ˣ) : K) = (x : K) ^ n := by
-  rw [← map_pow, ← val_pow_eq_pow_val]
-
-theorem coe_zpow (x : (𝓞 K)ˣ) (n : ℤ) : ((x ^ n : (𝓞 K)ˣ) : K) = (x : K) ^ n := by
-  change ((Units.coeHom K).comp (map (algebraMap (𝓞 K) K))) (x ^ n) = _
-  exact map_zpow _ x n
-
-theorem coe_one : ((1 : (𝓞 K)ˣ) : K) = (1 : K) := rfl
-
-theorem coe_neg_one : ((-1 : (𝓞 K)ˣ) : K) = (-1 : K) := rfl
-
-theorem coe_ne_zero (x : (𝓞 K)ˣ) : (x : K) ≠ 0 :=
-  mt RingOfIntegers.coe_eq_zero_iff.mp (_root_.Units.ne_zero x)
-
-end coe
-
-open NumberField.InfinitePlace
-
-section torsion
-
-/-- The torsion subgroup of the group of units. -/
-def torsion : Subgroup (𝓞 K)ˣ := CommGroup.torsion (𝓞 K)ˣ
-
-theorem mem_torsion {x : (𝓞 K)ˣ} [NumberField K] :
-    x ∈ torsion K ↔ ∀ w : InfinitePlace K, w x = 1 := by
-  rw [eq_iff_eq (x : K) 1, torsion, CommGroup.mem_torsion]
-  refine ⟨fun hx φ ↦ (((φ.comp $ algebraMap (𝓞 K) K).toMonoidHom.comp $
-    Units.coeHom _).isOfFinOrder hx).norm_eq_one, fun h ↦ isOfFinOrder_iff_pow_eq_one.2 ?_⟩
-  obtain ⟨n, hn, hx⟩ := Embeddings.pow_eq_one_of_norm_eq_one K ℂ x.val.isIntegral_coe h
-  exact ⟨n, hn, by ext; rw [NumberField.RingOfIntegers.coe_eq_algebraMap, coe_pow, hx,
-    NumberField.RingOfIntegers.coe_eq_algebraMap, coe_one]⟩
-
-/-- Shortcut instance because Lean tends to time out before finding the general instance. -/
-instance : Nonempty (torsion K) := One.instNonempty
-
-/-- The torsion subgroup is finite. -/
-instance [NumberField K] : Fintype (torsion K) := by
-  refine @Fintype.ofFinite _ (Set.finite_coe_iff.mpr ?_)
-  refine Set.Finite.of_finite_image ?_ ((coe_injective K).injOn _)
-  refine (Embeddings.finite_of_norm_le K ℂ 1).subset
-    (fun a ⟨u, ⟨h_tors, h_ua⟩⟩ => ⟨?_, fun φ => ?_⟩)
-  · rw [← h_ua]
-    exact u.val.prop
-  · rw [← h_ua]
-    exact le_of_eq ((eq_iff_eq _ 1).mp ((mem_torsion K).mp h_tors) φ)
-
--- a shortcut instance to stop the next instance from timing out
-instance [NumberField K] : Finite (torsion K) := inferInstance
-
-/-- The torsion subgroup is cylic. -/
-instance [NumberField K] : IsCyclic (torsion K) := subgroup_units_cyclic _
-
-/-- The order of the torsion subgroup as a positive integer. -/
-def torsionOrder [NumberField K] : ℕ+ := ⟨Fintype.card (torsion K), Fintype.card_pos⟩
-
-/-- If `k` does not divide `torsionOrder` then there are no nontrivial roots of unity of
-  order dividing `k`. -/
-theorem rootsOfUnity_eq_one [NumberField K] {k : ℕ+} (hc : Nat.Coprime k (torsionOrder K))
-    {ζ : (𝓞 K)ˣ} : ζ ∈ rootsOfUnity k (𝓞 K) ↔ ζ = 1 := by
-  rw [mem_rootsOfUnity]
-  refine ⟨fun h => ?_, fun h => by rw [h, one_pow]⟩
-  refine orderOf_eq_one_iff.mp (Nat.eq_one_of_dvd_coprimes hc ?_ ?_)
-  · exact orderOf_dvd_of_pow_eq_one h
-  · have hζ : ζ ∈ torsion K := by
-      rw [torsion, CommGroup.mem_torsion, isOfFinOrder_iff_pow_eq_one]
-      exact ⟨k, k.prop, h⟩
-    rw [orderOf_submonoid (⟨ζ, hζ⟩ : torsion K)]
-    exact orderOf_dvd_card
-
-/-- The group of roots of unity of order dividing `torsionOrder` is equal to the torsion
-group. -/
-theorem rootsOfUnity_eq_torsion [NumberField K] :
-    rootsOfUnity (torsionOrder K) (𝓞 K) = torsion K := by
-  ext ζ
-  rw [torsion, mem_rootsOfUnity]
-  refine ⟨fun h => ?_, fun h => ?_⟩
-  · rw [CommGroup.mem_torsion, isOfFinOrder_iff_pow_eq_one]
-    exact ⟨↑(torsionOrder K), (torsionOrder K).prop, h⟩
-  · exact Subtype.ext_iff.mp (@pow_card_eq_one (torsion K) _ _ ⟨ζ, h⟩)
-
-end torsion
-
-namespace dirichletUnitTheorem
+namespace NumberField.Units.dirichletUnitTheorem
 
 /-!
 ### Dirichlet Unit Theorem
@@ -192,13 +72,10 @@ def w₀ : InfinitePlace K := (inferInstance : Nonempty (InfinitePlace K)).some
 variable (K)
 
 /-- The logarithmic embedding of the units (seen as an `Additive` group). -/
-def logEmbedding : Additive ((𝓞 K)ˣ) →+ ({w : InfinitePlace K // w ≠ w₀} → ℝ) where
-  toFun := fun x w => mult w.val * Real.log (w.val ↑(Additive.toMul x))
+def logEmbedding : Additive ((𝓞 K)ˣ) →+ ({w : InfinitePlace K // w ≠ w₀} → ℝ) :=
+{ toFun := fun x w => mult w.val * Real.log (w.val ↑(Additive.toMul x))
   map_zero' := by simp; rfl
-  map_add' _ _ := by
-    ext x
-    simp only [toMul_add, val_mul, map_mul, Pi.add_apply]
-    rw [Real.log_mul, mul_add] <;> simp [coe_ne_zero]
+  map_add' := fun _ _ => by simp [Real.log_mul, mul_add]; rfl }
 
 variable {K}
 
