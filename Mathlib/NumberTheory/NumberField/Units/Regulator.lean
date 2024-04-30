d99dca4e8544610d74b55247cc902a943076cd1f
Xavier Roblot <roblot@math.univ-lyon1.fr>
2024-04-30 18:21:05 +0200

    Fix after merge

--- Mathlib/NumberTheory/NumberField/Units/Regulator.lean
+++ Mathlib/NumberTheory/NumberField/Units/Regulator.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Xavier Roblot
 -/
 import Mathlib.Algebra.Module.Zlattice.Covolume
-import Mathlib.NumberTheory.NumberField.Units
+import Mathlib.NumberTheory.NumberField.Units.DirichletTheorem
 
 /-!
 # Regulator of a number field
