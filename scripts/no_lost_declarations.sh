18a289f6c771afe3b8da455458ed8c498cd59f6b
damiano <adomani@gmail.com>
2024-04-30 10:57:41 +0000

    output in md-table

--- scripts/no_lost_declarations.sh
+++ scripts/no_lost_declarations.sh
@@ -12,18 +12,22 @@ fi |
   ## extract lines that begin with '[+-]' followed by the input `theorem` or `lemma`
   ## in the `git diff`
   awk -v regex="^[+-]${begs}" 'BEGIN{ paired=0; added=0; removed=0 }
+    /^--- a\//    { minusFile=$2 }
+    /^\+\+\+ b\// { plusFile=$2 }
     ($0 ~ regex){
       pm=substr($0, 1, 1)
       rest=substr($0, 2)
+      fil[rest]= (pm == "+") ? plusFile : minusFile
       acc[rest]=acc[rest] pm
     }
   END {
+    printf "|File|+-|Declaration|\n|-|-|-|"
     fin=""
     for(res in acc) {
       pm=acc[res]
       if(pm == "-+" || pm == "+-") { paired++ } else {
         if(pm == "+") { added++ } else removed++
-        fin=fin sprintf("* `%s` `%s`\n", pm, res)
+        fin=fin sprintf("| %s | `%s` | `%s` |\n", fil[res], pm, res)
       }
     }
     print fin| "sort -k3"; close("sort -k3")
