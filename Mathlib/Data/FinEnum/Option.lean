9cd00adbc51aca544a0e70f7f0e4d3fc1097b9bd
Tom Kranz <TpmKranz@users.noreply.github.com>
2024-05-01 13:11:31 +0200

    documentation for FinEnum.Option

--- Mathlib/Data/FinEnum/Option.lean
+++ Mathlib/Data/FinEnum/Option.lean
@@ -10,47 +10,46 @@ import Mathlib.Data.ULift
 /-!
 # FinEnum instance for Option
 
-Provides a recursor for FinEnum types like the one for Fintypes, but capable of producing
+Provides a recursor for FinEnum types like `Fintype.truncRecEmptyOption`, but capable of producing
 non-truncated data.
+
+## TODO
+* recreate rest of `Mathlib.Data.Fintype.Option`
 -/
 
 namespace FinEnum
 universe u v
 
-instance instEquivFin0Empty : ULift.{u} (Fin Nat.zero) ≃ PEmpty.{u + 1} :=
-  Equiv.trans Equiv.ulift <| Equiv.equivOfIsEmpty _ PEmpty.{u+1}
-
-instance instEquivFinNSuccOptionFinN (n:ℕ) :
-    ULift.{u} (Fin n.succ) ≃ Option (ULift.{u} (Fin n)) :=
-  Equiv.trans
-    Equiv.ulift <|
-    Equiv.trans finSuccEquivLast <| Equiv.optionCongr Equiv.ulift.symm
-
+/-- The additional `Option.none` element doesn't negate enumerability. -/
 instance instFinEnumOptionOfFinEnum (α : Type u) [FinEnum α] :
     FinEnum (Option α) :=
   FinEnum.ofEquiv _ (Equiv.optionEquivSumPUnit.{u,0} α)
 
+/-- A recursor principle for finite-and-enumerable types, analogous to `Nat.rec`.
+It effectively says that every `FinEnum` is either `Empty` or `Option α`, up to an `Equiv`.
+In contrast to the `Fintype` case, data can be transported along such an `Equiv`. -/
 def recEmptyOption {P : (α : Type u) → [FinEnum α] → Sort v}
     (of_equiv : {α β : Type u} → [FinEnum α] → [FinEnum β] → α ≃ β → P α → P β)
     (h_empty : P PEmpty.{u + 1})
     (h_option : {α : Type u} → [FinEnum α] → P α → P (Option α))
     (α : Type u) [aenum : FinEnum α] :
     P α := by
-  have ⟨card, cardeq⟩ : (n : ℕ) ×' (aenum.card = n) := ⟨aenum.card, rfl⟩
+  generalize cardeq : aenum.card = card
   induction card generalizing α
   case zero =>
-    have fzeroeq := Equiv.trans (Equiv.ulift.{u}) (cardeq ▸ aenum.equiv.symm)
-    have emptyeq := instEquivFin0Empty.{u}
+    have fzeroeq := Equiv.trans Equiv.ulift.{u} <| cardeq ▸ aenum.equiv.symm
+    have emptyeq := Equiv.trans Equiv.ulift <| Equiv.equivOfIsEmpty (Fin Nat.zero) PEmpty.{u+1}
     have fzerofe := FinEnum.ofEquiv _ emptyeq
     exact of_equiv fzeroeq <| of_equiv emptyeq.symm h_empty
   case succ n ih =>
-    have ⟨fe, fecardeq⟩ :
-      (e : FinEnum <| ULift.{u} <| Fin n) ×' (e.card = n) := ⟨FinEnum.mk n Equiv.ulift.{u}, rfl⟩
-    have fsucceq := Equiv.trans (Equiv.ulift.{u}) (cardeq ▸ aenum.equiv.symm)
-    have optioeq := (instEquivFinNSuccOptionFinN.{u} n)
-    have opsumeq := Equiv.optionEquivSumPUnit.{u,u} (ULift <| Fin n)
+    have ⟨fe, fecardeq⟩ : (e : _) ×' (e.card = n) := ⟨FinEnum.mk n Equiv.ulift.{u}, rfl⟩
+    have fsucceq := Equiv.trans Equiv.ulift.{u} <| cardeq ▸ aenum.equiv.symm
+    have optioeq :=
+      Equiv.trans
+        Equiv.ulift <|
+        Equiv.trans (@finSuccEquivLast n) <| Equiv.optionCongr Equiv.ulift.symm
+    have opsumeq := Equiv.optionEquivSumPUnit.{u,u} <| ULift (Fin n)
     have fssumeq := Equiv.trans optioeq opsumeq
     have fssumfe := @FinEnum.sum _ _ fe FinEnum.punit.{u}
     have fsuccfe := FinEnum.ofEquiv _ fssumeq
-    exact of_equiv fsucceq <| of_equiv optioeq.symm
-      <| h_option <| @ih (ULift.{u} <| Fin n) fe fecardeq
+    exact of_equiv fsucceq <| of_equiv optioeq.symm <| h_option <| @ih _ fe fecardeq
