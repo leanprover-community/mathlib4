7780c8ce5a6ddcdd4c868188658e5ea091bc27f3
Jireh Loreaux <loreaujy@gmail.com>
2024-05-01 13:34:06 -0500

    copyright

--- Mathlib/Analysis/NormedSpace/UnitizationL1.lean
+++ Mathlib/Analysis/NormedSpace/UnitizationL1.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2024 Jireh Loreaux. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Jireh Loreaux
+-/
 import Mathlib.Algebra.Algebra.Unitization
 import Mathlib.Analysis.NormedSpace.ProdLp
 
