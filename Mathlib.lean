61053d5833deadeaf227922c62e341d414dc367d
Christian Merten <christian@merten.dev>
2024-05-01 19:00:13 +0200

    fix mathlib

--- Mathlib.lean
+++ Mathlib.lean
@@ -3336,6 +3336,7 @@ import Mathlib.RepresentationTheory.Invariants
 import Mathlib.RepresentationTheory.Maschke
 import Mathlib.RepresentationTheory.Rep
 import Mathlib.RingTheory.AdicCompletion.Basic
+import Mathlib.RingTheory.AdicCompletion.Functoriality
 import Mathlib.RingTheory.Adjoin.Basic
 import Mathlib.RingTheory.Adjoin.FG
 import Mathlib.RingTheory.Adjoin.Field
