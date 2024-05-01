923f3989a1ce1ac1c1c5a7bfe6eeb0d92795d8d4
Gareth Ma <grhkm21@gmail.com>
2024-05-01 01:34:55 +0100

    feat: fix `lucas` to work in latest Mathlib

--- Mathlib/Algebra/BigOperators/Finsupp.lean
+++ Mathlib/Algebra/BigOperators/Finsupp.lean
@@ -144,15 +144,6 @@ theorem sum_ite_self_eq_aux [DecidableEq α] {N : Type*} [AddCommMonoid N] (f : 
   simp only [mem_support_iff, ne_eq, ite_eq_left_iff, not_not]
   exact fun h ↦ h.symm
 
-/-- A restatement of `prod_ite_eq` with the equality test reversed. -/
-@[to_additive (attr := simp) "A restatement of `sum_ite_eq` with the equality test reversed."]
-theorem prod_ite_eq' [DecidableEq α] (f : α →₀ M) (a : α) (b : α → M → N) :
-    (f.prod fun x v => ite (x = a) (b x v) 1) = ite (a ∈ f.support) (b a (f a)) 1 := by
-  dsimp [Finsupp.prod]
-  rw [f.support.prod_ite_eq']
-#align finsupp.prod_ite_eq' Finsupp.prod_ite_eq'
-#align finsupp.sum_ite_eq' Finsupp.sum_ite_eq'
-
 -- Porting note (#10618): simp can prove this
 -- @[simp]
 theorem sum_ite_self_eq' [DecidableEq α] {N : Type*} [AddCommMonoid N] (f : α →₀ N) (a : α) :
--- Mathlib/Data/Nat/Choose/Lucas.lean
+++ Mathlib/Data/Nat/Choose/Lucas.lean
@@ -21,32 +21,29 @@ variable {n k p : ℕ} [hp : Fact p.Prime]
 
 theorem lucas : choose n k ≡ choose (n % p) (k % p) * choose (n / p) (k / p) [ZMOD p] := by
   have decompose : ((X : (ZMod p)[X]) + 1) ^ n = (X + 1) ^ (n % p) * (X ^ p + 1) ^ (n / p) := by
-    nth_rw 1 [←mod_add_div n p, pow_add, pow_mul, add_pow_char (ZMod p)[X] (p := p), one_pow]
-  simp only [←ZMod.int_cast_eq_int_cast_iff, Int.cast_mul, Int.cast_ofNat,
-    ←coeff_X_add_one_pow _ n k, ←eq_intCast (Int.castRingHom (ZMod p)), ←coeff_map,
+    nth_rw 1 [← mod_add_div n p, pow_add, pow_mul, add_pow_char (ZMod p)[X] (p := p), one_pow]
+  simp only [← ZMod.intCast_eq_intCast_iff, Int.cast_mul, Int.cast_ofNat,
+    ← coeff_X_add_one_pow _ n k, ← eq_intCast (Int.castRingHom (ZMod p)), ← coeff_map,
     Polynomial.map_pow, Polynomial.map_add, Polynomial.map_one, map_X, decompose]
-  simp only [add_pow, one_pow, mul_one, ←pow_mul, sum_mul_sum]
-  conv =>
-    lhs; arg 1; arg 2; ext k
-    rw [←mul_assoc, mul_right_comm _ _ (X ^ (p * k.2)), ←pow_add, mul_assoc, ←cast_mul]
-  rw [finset_sum_coeff]
-  simp only [coeff_mul_natCast, coeff_X_pow, ite_mul, zero_mul, one_mul, ←cast_mul]
-  have step2 :
-      ∀ x ∈ range (n % p + 1) ×ˢ range (n / p + 1), k = x.1 + p * x.2 ↔ x = (k % p, k / p) := by
-    intro ⟨x₁, x₂⟩ hx
-    simp only [Prod.mk.injEq]
+  simp only [add_pow, one_pow, mul_one, ← pow_mul, sum_mul_sum]
+  conv_lhs =>
+    arg 1; arg 2; ext k; arg 2; ext k'
+    rw [← mul_assoc, mul_right_comm _ _ (X ^ (p * k')), ← pow_add, mul_assoc, ← cast_mul]
+  simp only [finset_sum_coeff, coeff_mul_natCast, coeff_X_pow, ite_mul, zero_mul, ← cast_mul]
+  rw [← sum_product', sum_ite_eq_iff (range (n % p + 1) ×ˢ range (n / p + 1)) (k % p, k / p)]
+  · simp_rw [mem_product, mem_range]
+    split_ifs with h
+    · simp
+    · rw [not_and_or, lt_succ, not_le, not_lt] at h
+      cases h with | _ h => simp [choose_eq_zero_of_lt h]
+  · intro ⟨x₁, x₂⟩ hx
+    rw [Prod.mk.injEq]
     constructor <;> intro h
     · simp only [mem_product, mem_range] at hx
       have h' : x₁ < p := lt_of_lt_of_le hx.left $ mod_lt _ Fin.size_pos'
-      rw [h, add_mul_mod_self_left, add_mul_div_left _ _ Fin.size_pos', self_eq_add_left]
-      exact ⟨(mod_eq_of_lt h').symm, div_eq_of_lt h'⟩
-    · rw [h.left, h.right, mod_add_div]
-  rw [sum_ite_iff_eq step2]
-  simp_rw [mem_product, mem_range]
-  split_ifs with h
-  · rfl
-  · simp only [lt_succ, not_and_or, not_le] at h
-    cases' h with h h <;> simp only [choose_eq_zero_of_lt h, zero_mul, mul_zero, cast_zero]
+      rw [h, add_mul_mod_self_left, add_mul_div_left _ _ Fin.size_pos', eq_comm (b := x₂)]
+      exact ⟨mod_eq_of_lt h', self_eq_add_left.mpr (div_eq_of_lt h')⟩
+    · rw [← h.left, ← h.right, mod_add_div]
 
 /- TODO: Finish proof -/
 theorem lucas' {a : ℕ} (ha₁ : n ≤ p ^ a) (ha₂ : k ≤ p ^ a) :
