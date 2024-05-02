2371af48cb7a6277b3410c2c6fc5770daf1e27ab
Chris Birkbeck <c.birkbeck@ucl.ac.uk>
2024-05-02 15:37:19 +0100

    fix mathlib imports

--- Mathlib.lean
+++ Mathlib.lean
@@ -3047,6 +3047,7 @@ import Mathlib.NumberTheory.Modular
 import Mathlib.NumberTheory.ModularForms.Basic
 import Mathlib.NumberTheory.ModularForms.CongruenceSubgroups
 import Mathlib.NumberTheory.ModularForms.EisensteinSeries.Basic
+import Mathlib.NumberTheory.ModularForms.EisensteinSeries.IsBoundedAtImInfty
 import Mathlib.NumberTheory.ModularForms.EisensteinSeries.MDifferentiable
 import Mathlib.NumberTheory.ModularForms.EisensteinSeries.UniformConvergence
 import Mathlib.NumberTheory.ModularForms.Identities
