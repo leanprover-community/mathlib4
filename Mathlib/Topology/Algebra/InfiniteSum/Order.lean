c6ac6d75f7c63fe4239cd9bde93d2e5c9fadffb0
Chris Birkbeck <c.birkbeck@ucl.ac.uk>
2024-05-02 16:54:30 +0100

    try build again?

--- Mathlib/Topology/Algebra/InfiniteSum/Order.lean
+++ Mathlib/Topology/Algebra/InfiniteSum/Order.lean
@@ -83,7 +83,7 @@ theorem tprod_le_tprod_of_inj {g : κ → α} (e : ι → κ) (he : Injective e)
 
 @[to_additive]
 lemma tprod_subtype_le {κ γ : Type*} [OrderedCommGroup γ] [UniformSpace γ] [UniformGroup γ]
-    [OrderClosedTopology γ ] [ CompleteSpace γ] (f : κ → γ) (β : Set κ) (h : ∀ a : κ, 1 ≤ f a)
+    [OrderClosedTopology γ] [ CompleteSpace γ] (f : κ → γ) (β : Set κ) (h : ∀ a : κ, 1 ≤ f a)
     (hf : Multipliable f) : (∏' (b : β), f b) ≤ (∏' (a : κ), f a) := by
   apply tprod_le_tprod_of_inj _ (Subtype.coe_injective) (by simp only [Subtype.range_coe_subtype,
     Set.setOf_mem_eq, h, implies_true]) (by simp only [le_refl,
