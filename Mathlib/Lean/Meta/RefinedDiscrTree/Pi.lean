6c0aff5f8a9ef888d2e5781774e13381ec8a26e0
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2024-05-02 18:28:10 +0100

    .

--- Mathlib/Lean/Meta/RefinedDiscrTree/Pi.lean
+++ Mathlib/Lean/Meta/RefinedDiscrTree/Pi.lean
@@ -3,7 +3,7 @@ Copyright (c) 2024 Jovan Gerbscheid. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jovan Gerbscheid
 -/
-import Lean.Meta.AppBuilder
+import Lean.Meta.Whnf
 
 /-!
 # Reducing Pi instances for indexing in the RefinedDiscrTree
