29506b6df3b94d562beb12f026fec95cf88a87fc
Riccardo Brasca <riccardo.brasca@gmail.com>
2024-05-02 18:37:32 +0200

    this is now useless

--- Mathlib/NumberTheory/NumberField/Units/Basic.lean
+++ Mathlib/NumberTheory/NumberField/Units/Basic.lean
@@ -116,9 +116,6 @@ instance [NumberField K] : Fintype (torsion K) := by
   · rw [← h_ua]
     exact le_of_eq ((eq_iff_eq _ 1).mp ((mem_torsion K).mp h_tors) φ)
 
--- a shortcut instance to stop the next instance from timing out
-instance [NumberField K] : Finite (torsion K) := inferInstance
-
 /-- The torsion subgroup is cylic. -/
 instance [NumberField K] : IsCyclic (torsion K) := subgroup_units_cyclic _
 
