3fd92ee3c3757b6dd98a5c104981d5acc72baed6
Eric Wieser <wieser.eric@gmail.com>
2024-05-02 23:26:22 +0000

    module docstring

--- Mathlib/Tactic/DefaultFieldLinter.lean
+++ Mathlib/Tactic/DefaultFieldLinter.lean
@@ -4,7 +4,9 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Eric Wieser
 -/
 import Lean
-
+/-!
+# The `linter.structureDiamondDefaults` linter
+-/
 
 register_option linter.structureDiamondDefaults : Bool := {
   defValue := true
