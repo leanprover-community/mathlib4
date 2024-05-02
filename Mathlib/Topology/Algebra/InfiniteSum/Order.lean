dc9689ba03882c0591800b37b2f684696be5eb08
Chris Birkbeck <c.birkbeck@ucl.ac.uk>
2024-05-02 15:51:10 +0100

    feat(Topology/Algebra/InfiniteSum/Order): Add tprod_subtype_le

--- Mathlib/Topology/Algebra/InfiniteSum/Order.lean
+++ Mathlib/Topology/Algebra/InfiniteSum/Order.lean
@@ -82,6 +82,15 @@ theorem tprod_le_tprod_of_inj {g : κ → α} (e : ι → κ) (he : Injective e)
 #align tsum_le_tsum_of_inj tsum_le_tsum_of_inj
 
 @[to_additive]
+lemma tprod_subtype_le {κ γ : Type*} [OrderedCommGroup γ] [UniformSpace γ] [UniformGroup γ]
+    [OrderClosedTopology γ ] [ CompleteSpace γ] (f : κ → γ) (β : Set κ) (h : ∀ a : κ, 1 ≤ f a)
+    (hf : Multipliable f) : (∏' (b : β), f b) ≤ (∏' (a : κ), f a) := by
+  apply tprod_le_tprod_of_inj _ (Subtype.coe_injective) (by simp only [Subtype.range_coe_subtype,
+    Set.setOf_mem_eq, h, implies_true]) (by simp only [le_refl,
+    Subtype.forall, implies_true]) (by apply hf.subtype)
+  apply hf
+
+@[to_additive]
 theorem prod_le_hasProd (s : Finset ι) (hs : ∀ i, i ∉ s → 1 ≤ f i) (hf : HasProd f a) :
     ∏ i in s, f i ≤ a :=
   ge_of_tendsto hf (eventually_atTop.2
