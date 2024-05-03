ddae0dd5816d80615103f21f6c9674db6c97e660
Christopher Hoskin <christopher.hoskin@gmail.com>
2024-05-03 07:45:14 +0100

    Use A in NonUnitalNonAssocCommSemiring

--- Mathlib/LinearAlgebra/QuadraticForm/Basic.lean
+++ Mathlib/LinearAlgebra/QuadraticForm/Basic.lean
@@ -77,7 +77,7 @@ quadratic map, homogeneous polynomial, quadratic polynomial
 universe u v w
 
 variable {S T : Type*}
-variable {R : Type*} {M N : Type*}
+variable {R : Type*} {M N A : Type*}
 
 open BigOperators
 
@@ -610,19 +610,19 @@ def _root_.LinearMap.compQuadraticMap [CommSemiring S] [Algebra S R] [Module S N
 
 end Comp
 
-section CommRing
+section NonUnitalNonAssocCommSemiring
 
-variable [CommSemiring R]  [NonUnitalNonAssocCommSemiring N]  [AddCommMonoid M] [Module R M]
-variable [Module R N] [SMulCommClass R N N] [IsScalarTower R N N]
+variable [CommSemiring R]  [NonUnitalNonAssocCommSemiring A]  [AddCommMonoid M] [Module R M]
+variable [Module R A] [SMulCommClass R A A] [IsScalarTower R A A]
 
 /-- The product of linear forms is a quadratic form. -/
-def linMulLin (f g : M →ₗ[R] N) : QuadraticMap R M N where
+def linMulLin (f g : M →ₗ[R] A) : QuadraticMap R M A where
   toFun := f * g
   toFun_smul a x := by
     rw [Pi.mul_apply, Pi.mul_apply, LinearMap.map_smulₛₗ, RingHom.id_apply, LinearMap.map_smulₛₗ,
       RingHom.id_apply, smul_mul_assoc, mul_smul_comm, ← smul_assoc, (smul_eq_mul R)]
   exists_companion' :=
-    ⟨(LinearMap.mul R N).compl₁₂ f g + (LinearMap.mul R N).compl₁₂ g f, fun x y => by
+    ⟨(LinearMap.mul R A).compl₁₂ f g + (LinearMap.mul R A).compl₁₂ g f, fun x y => by
       simp only [Pi.mul_apply, map_add, left_distrib, right_distrib, LinearMap.add_apply,
         LinearMap.compl₁₂_apply, LinearMap.mul_apply']
       rw [mul_comm (g x) (f y)]
@@ -630,47 +630,47 @@ def linMulLin (f g : M →ₗ[R] N) : QuadraticMap R M N where
 #align quadratic_form.lin_mul_lin QuadraticMap.linMulLin
 
 @[simp]
-theorem linMulLin_apply (f g : M →ₗ[R] N) (x) : linMulLin f g x = f x * g x :=
+theorem linMulLin_apply (f g : M →ₗ[R] A) (x) : linMulLin f g x = f x * g x :=
   rfl
 #align quadratic_form.lin_mul_lin_apply QuadraticMap.linMulLin_apply
 
 @[simp]
-theorem add_linMulLin (f g h : M →ₗ[R] N) : linMulLin (f + g) h = linMulLin f h + linMulLin g h :=
+theorem add_linMulLin (f g h : M →ₗ[R] A) : linMulLin (f + g) h = linMulLin f h + linMulLin g h :=
   ext fun _ => add_mul _ _ _
 #align quadratic_form.add_lin_mul_lin QuadraticMap.add_linMulLin
 
 @[simp]
-theorem linMulLin_add (f g h : M →ₗ[R] N) : linMulLin f (g + h) = linMulLin f g + linMulLin f h :=
+theorem linMulLin_add (f g h : M →ₗ[R] A) : linMulLin f (g + h) = linMulLin f g + linMulLin f h :=
   ext fun _ => mul_add _ _ _
 #align quadratic_form.lin_mul_lin_add QuadraticMap.linMulLin_add
 
 variable {N' : Type*} [AddCommMonoid N'] [Module R N']
 
 @[simp]
-theorem linMulLin_comp (f g : M →ₗ[R] N) (h : N' →ₗ[R] M) :
+theorem linMulLin_comp (f g : M →ₗ[R] A) (h : N' →ₗ[R] M) :
     (linMulLin f g).comp h = linMulLin (f.comp h) (g.comp h) :=
   rfl
 #align quadratic_form.lin_mul_lin_comp QuadraticMap.linMulLin_comp
 
 variable {n : Type*}
 
-/-- `sq` is the quadratic form mapping the vector `x : N` to `x * x` -/
+/-- `sq` is the quadratic form mapping the vector `x : A` to `x * x` -/
 @[simps!]
-def sq : QuadraticMap R N N :=
+def sq : QuadraticMap R A A :=
   linMulLin LinearMap.id LinearMap.id
 #align quadratic_form.sq QuadraticMap.sq
 
 /-- `proj i j` is the quadratic form mapping the vector `x : n → R` to `x i * x j` -/
-def proj (i j : n) : QuadraticMap R (n → N) N :=
-  linMulLin (@LinearMap.proj _ _ _ (fun _ => N) _ _ i) (@LinearMap.proj _ _ _ (fun _ => N) _ _ j)
+def proj (i j : n) : QuadraticMap R (n → A) A :=
+  linMulLin (@LinearMap.proj _ _ _ (fun _ => A) _ _ i) (@LinearMap.proj _ _ _ (fun _ => A) _ _ j)
 #align quadratic_form.proj QuadraticMap.proj
 
 @[simp]
-theorem proj_apply (i j : n) (x : n → N) : proj (R := R) i j x = x i * x j :=
+theorem proj_apply (i j : n) (x : n → A) : proj (R := R) i j x = x i * x j :=
   rfl
 #align quadratic_form.proj_apply QuadraticMap.proj_apply
 
-end CommRing
+end NonUnitalNonAssocCommSemiring
 
 end QuadraticMap
 
@@ -1148,7 +1148,7 @@ instance (priority := 100) _root_.CommSemiring.toNonUnitalNonAssocCommSemiring {
 theorem linMulLinSelfPosDef {R} {N' : Type*} [LinearOrderedCommRing R] [Module R M]
     [LinearOrderedCommSemiring N'] [ExistsAddOfLE N'] [Module R N'] [SMulCommClass R N' N']
     [IsScalarTower R N' N']
-    (f : M →ₗ[R] N') (hf : LinearMap.ker f = ⊥) : PosDef (linMulLin (R := R) (N := N') f f) :=
+    (f : M →ₗ[R] N') (hf : LinearMap.ker f = ⊥) : PosDef (linMulLin (R := R) (A := N') f f) :=
   fun _x hx =>
     mul_self_pos.2 fun h => hx <| LinearMap.ker_eq_bot'.mp hf _ h
 #align quadratic_form.lin_mul_lin_self_pos_def QuadraticMap.linMulLinSelfPosDef
