f47ea63764596cbb42c7ffd003a99b99bee0c219
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2024-05-04 16:18:39 +0200

    cancel modif in MvPolynomial

--- Mathlib/Algebra/MvPolynomial/Basic.lean
+++ Mathlib/Algebra/MvPolynomial/Basic.lean
@@ -645,14 +645,6 @@ def coeffAddMonoidHom (m : σ →₀ ℕ) : MvPolynomial σ R →+ R
   map_add' := coeff_add m
 #align mv_polynomial.coeff_add_monoid_hom MvPolynomial.coeffAddMonoidHom
 
-variable (R) in
-/-- `MvPolynomial.coeff m` but promoted to a `LinearMap`. -/
-@[simps apply]
-def lcoeff (m : σ →₀ ℕ) : MvPolynomial σ R →ₗ[R] R where
-  toFun := coeff m
-  map_add' := coeff_add m
-  map_smul' := coeff_smul m
-
 theorem coeff_sum {X : Type*} (s : Finset X) (f : X → MvPolynomial σ R) (m : σ →₀ ℕ) :
     coeff m (∑ x in s, f x) = ∑ x in s, coeff m (f x) :=
   map_sum (@coeffAddMonoidHom R σ _ _) _ s
