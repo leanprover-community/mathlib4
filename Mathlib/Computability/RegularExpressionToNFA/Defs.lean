d10f180c2ef16e0e31a045e0fe258c6d3d7559d3
Tom Kranz <TpmKranz@users.noreply.github.com>
2024-05-04 10:18:03 +0200

    bookkeeping

--- Mathlib/Computability/RegularExpressionToNFA/Defs.lean
+++ Mathlib/Computability/RegularExpressionToNFA/Defs.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2022 Russell Emerine. All rights reserved.
+Copyright (c) 2022 Russell Emerine, 2024 Tom Kranz. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Russell Emerine
+Authors: Russell Emerine, Tom Kranz
 -/
 import Mathlib.Computability.RegularExpressions
 import Mathlib.Computability.NFA
