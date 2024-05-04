c47d3b4843eea735a3a09d1ff663639f8d482def
adomani <adomani@gmail.com>
2024-05-04 08:07:02 +0200

    long line

--- Mathlib/Algebra/MonoidAlgebra/Basic.lean
+++ Mathlib/Algebra/MonoidAlgebra/Basic.lean
@@ -279,7 +279,8 @@ theorem _root_.AddMonoidAlgebra.liftNC_mul {g_hom : Type*}
     [FunLike g_hom (Multiplicative G) R] [MulHomClass g_hom (Multiplicative G) R]
     (f : k →+* R) (g : g_hom) (a b : k[G])
     (h_comm : ∀ {x y}, y ∈ a.support → Commute (f (b x)) (g <| Multiplicative.ofAdd y)) :
-    AddMonoidAlgebra.liftNC (f : k →+ R) g (a * b) = AddMonoidAlgebra.liftNC (f : k →+ R) g a * AddMonoidAlgebra.liftNC (f : k →+ R) g b :=
+    AddMonoidAlgebra.liftNC (f : k →+ R) g (a * b) =
+      AddMonoidAlgebra.liftNC (f : k →+ R) g a * AddMonoidAlgebra.liftNC (f : k →+ R) g b :=
   (MonoidAlgebra.liftNC_mul f g _ _ @h_comm : _)
 #align add_monoid_algebra.lift_nc_mul AddMonoidAlgebra.liftNC_mul
 
