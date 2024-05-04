8bcec0db6895ab37264e785435d39b43b38757a8
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2024-05-04 08:48:06 +0200

    chore: rename instances

--- Mathlib/Analysis/Normed/Field/Basic.lean
+++ Mathlib/Analysis/Normed/Field/Basic.lean
@@ -643,7 +643,7 @@ instance MulOpposite.instNormedCommRing : NormedCommRing αᵐᵒᵖ where
 end NormedCommRing
 
 -- see Note [lower instance priority]
-instance (priority := 100) semi_normed_ring_top_monoid [NonUnitalSeminormedRing α] :
+instance (priority := 100) NonUnitalSeminormedRing.instContinuousMul [NonUnitalSeminormedRing α] :
     ContinuousMul α :=
   ⟨continuous_iff_continuousAt.2 fun x =>
       tendsto_iff_norm_sub_tendsto_zero.2 <| by
@@ -666,13 +666,13 @@ instance (priority := 100) semi_normed_ring_top_monoid [NonUnitalSeminormedRing 
         · show Tendsto _ _ _
           exact tendsto_const_nhds
         · simp⟩
-#align semi_normed_ring_top_monoid semi_normed_ring_top_monoid
+#align semi_normed_ring_top_monoid NonUnitalSeminormedRing.instContinuousMul
 
 -- see Note [lower instance priority]
 /-- A seminormed ring is a topological ring. -/
-instance (priority := 100) semi_normed_top_ring [NonUnitalSeminormedRing α] : TopologicalRing α
-    where
-#align semi_normed_top_ring semi_normed_top_ring
+instance (priority := 100) NonUnitalSeminormedRing.instTopologicalRing [NonUnitalSeminormedRing α] :
+    TopologicalRing α where
+#align semi_normed_top_ring NonUnitalSeminormedRing.instTopologicalRing
 
 section NormedDivisionRing
 
