a16d2ff3bcef58cbf303fdc7a4976c6b1946e4db
Eric Wieser <wieser.eric@gmail.com>
2024-05-05 13:25:09 +0000

    remove parens

--- Mathlib/Data/Rat/Field.lean
+++ Mathlib/Data/Rat/Field.lean
@@ -69,12 +69,12 @@ lemma inv_def (q : ℚ≥0) : q⁻¹ = divNat q.den q.num := by ext; simp [Rat.i
 lemma div_def (p q : ℚ≥0) : p / q = divNat (p.num * q.den) (p.den * q.num) := by
   ext; simp [Rat.div_def', num_coe, den_coe]
 
-lemma num_inv_of_ne_zero {q : ℚ≥0} (hq : q ≠ 0) : (q⁻¹).num = q.den := by
+lemma num_inv_of_ne_zero {q : ℚ≥0} (hq : q ≠ 0) : q⁻¹.num = q.den := by
   rw [inv_def, divNat, num, coe_mk, Rat.divInt_ofNat, ← Rat.mk_eq_mkRat _ _ (num_ne_zero.mpr hq),
     Int.natAbs_ofNat]
   simpa using q.coprime_num_den.symm
 
-lemma den_inv_of_ne_zero {q : ℚ≥0} (hq : q ≠ 0) : (q⁻¹).den = q.num := by
+lemma den_inv_of_ne_zero {q : ℚ≥0} (hq : q ≠ 0) : q⁻¹.den = q.num := by
   rw [inv_def, divNat, den, coe_mk, Rat.divInt_ofNat, ← Rat.mk_eq_mkRat _ _ (num_ne_zero.mpr hq)]
   simpa using q.coprime_num_den.symm
 
