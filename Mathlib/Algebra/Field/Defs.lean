84869b43af7ffc98be64c90ab8d0b9386ecf544f
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2024-05-05 10:21:34 +0200

    Update Mathlib/Algebra/Field/Defs.lean

--- Mathlib/Algebra/Field/Defs.lean
+++ Mathlib/Algebra/Field/Defs.lean
@@ -229,5 +229,5 @@ end Rat
 
 /-- `OfScientific.ofScientific` is the simp-normal form. -/
 @[simp]
-theorem Rat.ofScientific_eq_ofScientific (m : ℕ) (s : Bool) (e : ℕ):
+theorem Rat.ofScientific_eq_ofScientific (m : ℕ) (s : Bool) (e : ℕ) :
     Rat.ofScientific (OfNat.ofNat m) s (OfNat.ofNat e) = OfScientific.ofScientific m s e := rfl
