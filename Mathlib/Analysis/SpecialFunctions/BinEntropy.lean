ee2ddfba025196774d647867e13ce26b425aa0e5
Adomas Baliuka <A.Baliuka@physik.uni-muenchen.de>
2024-05-05 23:14:44 +0200

    Hopefully now fixed all unused imports

--- Mathlib/Analysis/SpecialFunctions/BinEntropy.lean
+++ Mathlib/Analysis/SpecialFunctions/BinEntropy.lean
@@ -3,7 +3,6 @@ Copyright (c) 2023 Adomas Baliuka. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Adomas Baliuka
 -/
-import Mathlib.Analysis.SpecialFunctions.Log.Base
 import Mathlib.Analysis.SpecialFunctions.Log.NegMulLog
 import Mathlib.Analysis.Convex.SpecificFunctions.Basic
 
