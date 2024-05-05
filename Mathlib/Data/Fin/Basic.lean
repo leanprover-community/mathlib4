8e73b9533059ce5fb8e4c56e4fdaecdfa772a3e1
Yaël Dillies <yael.dillies@gmail.com>
2024-05-05 08:32:48 +0000

    rename to `natCast_strictMono`

--- Mathlib/Data/Fin/Basic.lean
+++ Mathlib/Data/Fin/Basic.lean
@@ -701,17 +701,17 @@ theorem le_val_last (i : Fin (n + 1)) : i ≤ n := by
 
 variable {a b : ℕ}
 
-lemma natCast_le_natCast (han : a ≤ n) (hbn : b ≤ n) : (a : Fin n.succ) ≤ b ↔ a ≤ b := by
+lemma natCast_le_natCast (han : a ≤ n) (hbn : b ≤ n) : (a : Fin (n + 1)) ≤ b ↔ a ≤ b := by
   rw [← Nat.lt_succ_iff] at han hbn
   simp [le_iff_val_le_val, -val_fin_le, Nat.mod_eq_of_lt, han, hbn]
 
-lemma natCast_lt_natCast (han : a ≤ n) (hbn : b ≤ n) : (a : Fin n.succ) < b ↔ a < b := by
+lemma natCast_lt_natCast (han : a ≤ n) (hbn : b ≤ n) : (a : Fin (n + 1)) < b ↔ a < b := by
   rw [← Nat.lt_succ_iff] at han hbn; simp [lt_iff_val_lt_val, Nat.mod_eq_of_lt, han, hbn]
 
-lemma natCast_mono (hbn : b ≤ n) (hab : a ≤ b) : (a : Fin n.succ) ≤ b :=
+lemma natCast_mono (hbn : b ≤ n) (hab : a ≤ b) : (a : Fin (n + 1)) ≤ b :=
   (natCast_le_natCast (hab.trans hbn) hbn).2 hab
 
-lemma cast_strictMono (hbn : b ≤ n) (hab : a < b) : (a : Fin n.succ) < b :=
+lemma natCast_strictMono (hbn : b ≤ n) (hab : a < b) : (a : Fin (n + 1)) < b :=
   (natCast_lt_natCast (hab.le.trans hbn) hbn).2 hab
 
 end OfNatCoe
