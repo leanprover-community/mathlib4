7f1129a82b9a42292b7c36d11f124f3f68768177
Moritz Firsching <firsching@google.com>
2024-05-05 14:16:31 +0200

    add default_eq

--- Mathlib/Data/Nat/Digits.lean
+++ Mathlib/Data/Nat/Digits.lean
@@ -639,19 +639,16 @@ theorem ofDigits_mod (b k : ℕ) (L : List ℕ) : ofDigits b L % k = ofDigits (b
 
 theorem ofDigits_mod_eq_head! (b : ℕ) (l : List ℕ) : ofDigits b l % b = l.head! % b := by
   induction l <;> simp [Nat.ofDigits, Int.ModEq]
-  rfl
 
 theorem head!_digits {b n : ℕ} (h : b ≠ 1) : (Nat.digits b n).head! = n % b := by
   by_cases hb : 1 < b
   · rcases n with _ | n
     · simp
-      rfl
     · nth_rw 2 [← Nat.ofDigits_digits b (n + 1)]
       rw [Nat.ofDigits_mod_eq_head! _ _]
       exact (Nat.mod_eq_of_lt (Nat.digits_lt_base hb <| List.head!_mem_self <|
           Nat.digits_ne_nil_iff_ne_zero.mpr <| Nat.succ_ne_zero n)).symm
   · rcases n with _ | _ <;> simp_all [show b = 0 by omega]
-    rfl
 
 theorem ofDigits_zmodeq' (b b' : ℤ) (k : ℕ) (h : b ≡ b' [ZMOD k]) (L : List ℕ) :
     ofDigits b L ≡ ofDigits b' L [ZMOD k] := by
