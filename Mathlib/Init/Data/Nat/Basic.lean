7a4dcaa1299e5ac5b5e68bd083b849305b44bbbb
Moritz Firsching <firsching@google.com>
2024-05-05 18:09:31 +0200

    mv default_eq_zero

--- Mathlib/Init/Data/Nat/Basic.lean
+++ Mathlib/Init/Data/Nat/Basic.lean
@@ -13,8 +13,6 @@ set_option linter.deprecated false
 
 theorem add_one_pos (n : ℕ) : 0 < n + 1 := succ_pos n
 
-@[simp] theorem default_eq_zero : default = 0 := rfl
-
 protected theorem bit0_succ_eq (n : ℕ) : bit0 (succ n) = succ (succ (bit0 n)) :=
   show succ (succ n + n) = succ (succ (n + n)) from congrArg succ (succ_add n n)
 #align nat.bit0_succ_eq Nat.bit0_succ_eq
