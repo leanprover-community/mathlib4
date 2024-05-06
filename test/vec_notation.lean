5cd34a4c0f19855c7452d390b6385bedb52ab729
Komyyy <pol_tta@outlook.jp>
2024-05-06 11:50:59 +0900

    test

--- test/vec_notation.lean
+++ test/vec_notation.lean
@@ -9,6 +9,8 @@ import Mathlib.Data.Fin.VecNotation
 
 open Lean
 open Lean.Meta
+open Lean.Elab
+open Lean.PrettyPrinter
 open Qq
 
 run_cmd Elab.Command.liftTermElabM do
@@ -28,3 +30,15 @@ run_cmd Elab.Command.liftTermElabM do
 #guard (toString (repr (![] : _ → ℕ)) = "![]")
 #guard (toString (repr ![1, 2, 3]) = "![1, 2, 3]")
 #guard (toString (repr ![![1, 2], ![3, 4]]) = "![![1, 2], ![3, 4]]")
+
+syntax "#guard_delab" term:51 " :→ " term : command
+
+elab_rules : command
+  | `(command| #guard_delab $t₁ :→ $t₂) =>
+    Command.liftTermElabM <| Term.withDeclName `_guard_delab <| do
+      let e₁ ← Term.elabTerm t₁ none
+      let t₁' ← delab e₁
+      guard <| t₁' == t₂
+
+#guard_delab fun x => (![0, 1] : Fin 2 → ℕ) x :→ fun x => ![0, 1] x
+#guard_delab fun x => (![] : Fin 0 → ℕ) x :→ fun x => ![] x
