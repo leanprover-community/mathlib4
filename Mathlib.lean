a8b4cf4e160afe205722aa13991ac45e507bbd0a
Johan Commelin <johan@commelin.net>
2024-05-06 07:35:13 +0000

    chore(Tactic): command/tactic/term elaborator for adaptation notes (#12662)
    This PR introduces `#adaptation_note`. The main benefit is that this prevents typos in the string "Adaptation note". Such typos would make it hard to find the note later. A side benefit is that the command comes with a docstring that explains the purpose of adaptation notes in general.
    
    Co-authored-by: Kyle Miller <kmill31415@gmail.com>
    
    
    
    Co-authored-by: adomani <adomani@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -3607,6 +3607,7 @@ import Mathlib.SetTheory.ZFC.Basic
 import Mathlib.SetTheory.ZFC.Ordinal
 import Mathlib.Tactic
 import Mathlib.Tactic.Abel
+import Mathlib.Tactic.AdaptationNote
 import Mathlib.Tactic.ApplyAt
 import Mathlib.Tactic.ApplyCongr
 import Mathlib.Tactic.ApplyFun
--- Mathlib/Tactic.lean
+++ Mathlib/Tactic.lean
@@ -1,4 +1,5 @@
 import Mathlib.Tactic.Abel
+import Mathlib.Tactic.AdaptationNote
 import Mathlib.Tactic.ApplyAt
 import Mathlib.Tactic.ApplyCongr
 import Mathlib.Tactic.ApplyFun
