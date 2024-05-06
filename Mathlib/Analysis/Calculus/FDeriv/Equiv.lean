84962d1bd04403a4a6f9afabe3eab01117514b2b
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2024-05-06 11:41:15 +0200

    cleanup

--- Mathlib/Analysis/Calculus/FDeriv/Equiv.lean
+++ Mathlib/Analysis/Calculus/FDeriv/Equiv.lean
@@ -169,12 +169,24 @@ theorem comp_fderiv {f : G → E} {x : G} :
   exact iso.comp_fderivWithin uniqueDiffWithinAt_univ
 #align continuous_linear_equiv.comp_fderiv ContinuousLinearEquiv.comp_fderiv
 
-lemma _root_.fderiv_continuousLinearEquiv_comp (L : G ≃L[𝕜] G') (f : E → (F →L[𝕜] G)) :
+lemma _root_.fderivWithin_continuousLinearEquiv_comp (L : G ≃L[𝕜] G') (f : E → (F →L[𝕜] G))
+    (hs : UniqueDiffWithinAt 𝕜 s x) :
+    fderivWithin 𝕜 (fun x ↦ (L : G →L[𝕜] G').comp (f x)) s x =
+      (((ContinuousLinearEquiv.refl 𝕜 F).arrowCongr L)) ∘L (fderivWithin 𝕜 f s x) := by
+  change fderivWithin 𝕜 (((ContinuousLinearEquiv.refl 𝕜 F).arrowCongr L) ∘ f) s x = _
+  rw [ContinuousLinearEquiv.comp_fderivWithin _ hs]
+
+lemma _root_.fderiv_continuousLinearEquiv_comp (L : G ≃L[𝕜] G') (f : E → (F →L[𝕜] G)) (x : E) :
+    fderiv 𝕜 (fun x ↦ (L : G →L[𝕜] G').comp (f x)) x =
+      (((ContinuousLinearEquiv.refl 𝕜 F).arrowCongr L)) ∘L (fderiv 𝕜 f x) := by
+  change fderiv 𝕜 (((ContinuousLinearEquiv.refl 𝕜 F).arrowCongr L) ∘ f) x = _
+  rw [ContinuousLinearEquiv.comp_fderiv]
+
+lemma _root_.fderiv_continuousLinearEquiv_comp' (L : G ≃L[𝕜] G') (f : E → (F →L[𝕜] G)) :
     fderiv 𝕜 (fun x ↦ (L : G →L[𝕜] G').comp (f x)) =
       fun x ↦ (((ContinuousLinearEquiv.refl 𝕜 F).arrowCongr L)) ∘L (fderiv 𝕜 f x) := by
   ext x : 1
-  change fderiv 𝕜 (((ContinuousLinearEquiv.refl 𝕜 F).arrowCongr L) ∘ f) x = _
-  rw [ContinuousLinearEquiv.comp_fderiv]
+  exact fderiv_continuousLinearEquiv_comp L f x
 
 theorem comp_right_differentiableWithinAt_iff {f : F → G} {s : Set F} {x : E} :
     DifferentiableWithinAt 𝕜 (f ∘ iso) (iso ⁻¹' s) x ↔ DifferentiableWithinAt 𝕜 f s (iso x) := by
