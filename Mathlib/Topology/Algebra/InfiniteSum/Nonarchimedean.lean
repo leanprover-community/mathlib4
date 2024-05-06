96bccd2a796a9da7d69b294359bf8d3017fb7f46
Mitchell Lee <trivial171@gmail.com>
2024-05-05 22:48:57 -0400

    Remove unnecessary imports

--- Mathlib/Topology/Algebra/InfiniteSum/Nonarchimedean.lean
+++ Mathlib/Topology/Algebra/InfiniteSum/Nonarchimedean.lean
@@ -3,11 +3,7 @@ Copyright (c) 2024 Mitchell Lee. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mitchell Lee
 -/
-import Mathlib.Topology.Algebra.InfiniteSum.Group
-import Mathlib.Topology.Algebra.Nonarchimedean.Basic
-import Mathlib.Topology.Algebra.Monoid
 import Mathlib.Topology.Algebra.InfiniteSum.Ring
-import Mathlib.Topology.Algebra.UniformRing
 import Mathlib.Topology.Algebra.Nonarchimedean.Completion
 
 /-!
