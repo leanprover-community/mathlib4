b57f39b7368a3c7f93ad873ef1a5c95f2f89b133
adomani <adomani@gmail.com>
2024-05-06 14:12:55 +0200

    Array of matches, streamline cases

--- Mathlib/Tactic/MinMaxTwo.lean
+++ Mathlib/Tactic/MinMaxTwo.lean
@@ -5,6 +5,7 @@ Authors: Damiano Testa
 -/
 
 import Lean.Elab.Command
+import Mathlib.Algebra.Group.Defs
 /-!
 #  `to_ama` a command to convert from `MonoidAlgebra` to `AddMonoidAlgebra`
 
@@ -43,12 +44,12 @@ def nameToTop : Name → Name
   | .str a b => .str (nameToTop a) (stringReplacements convs b)
   | _ => default
 
-variable {m} [Monad m] [MonadRef m] [MonadQuotation m]
-  (convs : HashMap String String) (toMultArrow : Name) (toMult : Name) (toPlus : Syntax) in
+variable {m} [Monad m] [MonadRef m] [MonadQuotation m] --[MonadLog m] [AddMessageContext m] [MonadOptions m]
+  (convs : HashMap String String) (toMultArrow : Name) (toMult : Name) (toPlus : Array Syntax) in
 /-- converts `WithBot _` to `ℕ∞` and `⊥` to `⊤`.
 Useful when converting a `degree` with values in `WithBot ℕ` to a `trailingDegree` with values
 in `ℕ∞`. -/
-def MaxToMin(stx : Syntax) : m Syntax := do
+def MaxToMin (stx : Syntax) : m Syntax := do
   let stx ← stx.replaceM fun s => do
     match s.getId with
       | .anonymous => return none
@@ -57,19 +58,55 @@ def MaxToMin(stx : Syntax) : m Syntax := do
   stx.replaceM fun s => do
     match s with
       | .node _ `«term_*_» #[a, _, b] =>
-        if a == toPlus then
+        if toPlus.contains a then
           return some <| ← `($(⟨a⟩) + $(⟨b⟩))
         else return none
+      | .node _ ``Lean.Parser.Term.app #[.ident _ _ `AddMonoidAlgebra _, .node _ _ #[_, G]] =>
+        if G.getId == toMult then return s else return none
+
+      | .node _ ``Lean.Parser.Term.app #[.ident _ _ azc _, .node _ _ #[x]] =>
+        match azc with
+          | `One =>
+            if toPlus.contains x then return some <| ← `($(mkIdent `Zero) $(⟨x⟩))
+            else return none
+
+          | _ =>
+            if azc == toMultArrow then
+                    return some <| ← `($(mkIdent azc) ($(mkIdent `Multiplicative.ofAdd) $(⟨x⟩)))
+            else
+            if x.getId == toMult && azc == `Add then return s else return none
+
       | .node _ ``Lean.Parser.Term.app
           #[.ident _ _ `single _, .node _ _ #[one, c]] =>
         match one with
           | `(1)           => return some <| ← `($(mkIdent `single) 0 $(⟨c⟩))
           | `((1 : $type)) => return some <| ← `($(mkIdent `single) (0 : $(⟨type⟩)) $(⟨c⟩))
           | _ => return none
-      | .node _ ``Lean.Parser.Term.app #[.ident _ _ na _, .node _ _ #[b]] =>
-        match na with
-          | _ => if na != toMultArrow then return none else
-                    return some <| ← `($(mkIdent na) ($(mkIdent `Multiplicative.ofAdd) $(⟨b⟩)))
+
+      | .node _ ``Lean.Parser.Term.app #[.ident _ _ cls _, .node _ _ #[F, a, b]] =>
+        if cls ∉ [`OneHomClass, `MulHomClass] then return none else
+        if toPlus.contains a then
+          let repl := if cls == `OneHomClass then `ZeroHomClass else `AddHomClass
+          let zhc := mkNode ``Lean.Parser.Term.app #[mkIdent repl, mkNullNode #[F, a, b]]
+          return some zhc
+        else return none
+
+--      | .node _ ``Lean.Parser.Term.app #[.ident _ _ `One _, .node _ _ #[a]] =>
+--        if toPlus.contains a then return some <| ← `($(mkIdent `Zero) $(⟨a⟩))
+--        else return none
+
+--      | .node _ ``Lean.Parser.Term.app #[.ident _ _ na _, .node _ _ #[b]] =>
+--        match na with
+--          | _ => if na != toMultArrow then return none else
+--                    return some <| ← `($(mkIdent na) ($(mkIdent `Multiplicative.ofAdd) $(⟨b⟩)))
+      | .node _ ``Lean.Parser.Term.app #[.ident _ _ `Commute _, .node _ _ #[a, b]] =>
+        if toPlus.contains a then return some <| ← `($(mkIdent `AddCommute) $(⟨a⟩) $(⟨b⟩))
+        else return none
+
+      | .node _ `«term_^_» #[a, _, b] =>
+        if toPlus.contains a then return some <| ← `($(⟨b⟩) • $(⟨a⟩))
+        else return none
+
       | .ident _ _ x _ => if x != toMult then return none else
                 return some <| ← `($(mkIdent `Multiplicative) $(mkIdent x))
       | .node _ ``Lean.Parser.Command.docComment #[init, .atom _ docs] =>
@@ -100,54 +137,30 @@ environment the analogous result about `AddMonoidAlgebra`.
 
 Writing `to_ama?` also prints the extra declaration added by `to_ama`.
 -/
-elab (name := to_amaCmd) "to_ama " tk:("?")? "[" id1:(ident)? "]" id2:(ident)?
-    "plus"? id3:(term)? "noplus"? cmd:command :
-    command => do
-  let g := match id1 with | some id => id.getId | _ => default
-  let h := match id2 with | some id => id.getId | _ => default
-  let i := match id3 with | some id => id.raw | _ => default
-  let newCmd ← MaxToMin toAddWords g h i cmd
-  if tk.isSome then logInfo m!"-- adding\n{newCmd}"
-  elabCommand cmd
-  if (← get).messages.hasErrors then return
-  let currNS ← getCurrNamespace
-  withScope (fun s => { s with currNamespace := nameToTop toAddWords currNS }) <| elabCommand newCmd
-
-def toAmaCmd (verbose? : Bool) (id1 id2 : TSyntax `ident) (id3 : Syntax) (cmd : Syntax) :
+def toAmaCmd (verbose? : Bool) (id1 id2 : TSyntax `ident) (id3 : Array Syntax) (cmd : Syntax) :
     CommandElabM Unit := do
-  let g := id1.getId
-  let h := id2.getId
---  let i := id3.getId
-  let newCmd ← MaxToMin toAddWords g h id3 cmd
+  let id1 := id1.getId
+  let id2 := id2.getId
+  let newCmd ← MaxToMin toAddWords id1 id2 id3 cmd
   if verbose? then logInfo m!"-- adding\n{newCmd}"
   elabCommand cmd
   if (← get).messages.hasErrors then return
   let currNS ← getCurrNamespace
   withScope (fun s => { s with currNamespace := nameToTop toAddWords currNS }) <| elabCommand newCmd
 
-@[inherit_doc to_amaCmd]
+@[inherit_doc toAmaCmd]
 elab "to_ama " tk:("?")? "[" id:(ident)? "]" cmd:command : command =>
   let rid := mkIdent `hi
-  toAmaCmd tk.isSome (id.getD default) rid default cmd
---  return (← `(to_ama ? [$(id.getD default)] $rid noplus $cmd))
+  toAmaCmd tk.isSome (id.getD default) rid #[] cmd
 
---@[inherit_doc to_amaCmd]
---macro "to_ama? " cmd:command : command =>
---  let rid := mkIdent `hi
---  return (← `(to_ama ? [] $rid noplus $cmd))
-
-@[inherit_doc to_amaCmd]
+@[inherit_doc toAmaCmd]
 elab "to_ama " tk:("?")? id:ident cmd:command : command =>
-  toAmaCmd tk.isSome default id default cmd
---  return (← `(to_ama [] $id noplus $cmd))
+  toAmaCmd tk.isSome default id #[] cmd
 
-@[inherit_doc to_amaCmd]
-elab "to_ama " tk:("?")? "plus" id:ident cmd:command : command => do
-  toAmaCmd tk.isSome default default id cmd
---  return (← `(to_ama [] $id plus $id noplus $cmd))
+@[inherit_doc toAmaCmd]
+elab "to_ama " tk:("?")? i1:(ident)? "plus" id:term,* cmd:command : command => do
+  toAmaCmd tk.isSome default (i1.getD default) (id.getElems.map (·.raw)) cmd
 
-@[inherit_doc to_amaCmd]
+@[inherit_doc toAmaCmd]
 elab "to_ama " tk:("?")? cmd:command : command =>
-  toAmaCmd tk.isSome default default default cmd
---  let rid := mkIdent `hi
---  return (← `(to_ama [] $rid noplus $cmd))
+  toAmaCmd tk.isSome default default #[] cmd
