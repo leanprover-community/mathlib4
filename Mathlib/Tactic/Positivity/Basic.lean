a7e03d1de484f708e008c3fbdba103f95c7f8e22
Yaël Dillies <yael.dillies@gmail.com>
2024-05-07 15:11:13 +0000

    feat: Positivity extension for `NNRat.num`/`NNRat.den` From LeanAPAP

--- Mathlib/Tactic/Positivity/Basic.lean
+++ Mathlib/Tactic/Positivity/Basic.lean
@@ -5,6 +5,7 @@ Authors: Mario Carneiro, Heather Macbeth, Yaël Dillies
 -/
 import Mathlib.Algebra.GroupPower.Order
 import Mathlib.Data.Int.CharZero
+import Mathlib.Data.NNRat.Defs
 import Mathlib.Algebra.Order.Ring.Int
 import Mathlib.Data.Nat.Factorial.Basic
 import Mathlib.Data.Rat.Order
@@ -419,6 +420,48 @@ def evalAscFactorial : PositivityExt where eval {u α} _ _ e := do
     assertInstancesCommute
     pure (.positive q(Nat.ascFactorial_pos $n $k))
   | _, _, _ => throwError "failed to match Nat.ascFactorial"
+
+section NNRat
+open NNRat
+
+private alias ⟨_, NNRat.num_pos_of_pos⟩ := num_pos
+private alias ⟨_, NNRat.num_ne_zero_of_ne_zero⟩ := num_ne_zero
+
+/-- The `positivity` extension which identifies expressions of the form `NNRat.num q`,
+such that `positivity` successfully recognises `q`. -/
+@[positivity NNRat.num _]
+def evalNNRatNum : PositivityExt where eval {u α} _ _ e := do
+  if let 0 := u then -- lean4#3060 means we can't combine this with the match below
+    match α, e with
+    | ~q(ℕ), ~q(NNRat.num $a) =>
+      let zα : Q(Zero ℚ≥0) := q(inferInstance)
+      let pα : Q(PartialOrder ℚ≥0) := q(inferInstance)
+      assumeInstancesCommute
+      match ← core zα pα a with
+      | .positive pa => return .positive q(NNRat.num_pos_of_pos $pa)
+      | .nonzero pa => return .nonzero q(NNRat.num_ne_zero_of_ne_zero $pa)
+      | _ => return .none
+    | _, _ => throwError "not NNRat.num"
+  else throwError "not NNRat.num"
+
+/-- The `positivity` extension which identifies expressions of the form `Rat.den a`. -/
+@[positivity NNRat.den _]
+def evalNNRatDen : PositivityExt where eval {u α} _ _ e := do
+  if let 0 := u then -- lean4#3060 means we can't combine this with the match below
+    match α, e with
+    | ~q(ℕ), ~q(NNRat.den $a) =>
+      assumeInstancesCommute
+      return .positive q(den_pos $a)
+    | _, _ => throwError "not NNRat.num"
+  else throwError "not NNRat.num"
+
+variable {q : ℚ≥0}
+
+example (hq : 0 < q) : 0 < q.num := by positivity
+example (hq : q ≠ 0) : q.num ≠ 0 := by positivity
+example : 0 < q.den := by positivity
+
+end NNRat
 
 open Rat
 
