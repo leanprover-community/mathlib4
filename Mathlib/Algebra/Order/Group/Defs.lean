50c1ac026442eb866703b246d3b22c2cf7adcd6f
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2024-05-07 20:13:33 +0200

    lower priority of another instance in Algebra.Order.Group.Defs

--- Mathlib/Algebra/Order/Group/Defs.lean
+++ Mathlib/Algebra/Order/Group/Defs.lean
@@ -1299,9 +1299,11 @@ assert_not_exists NeZero
 -- lower instance priorities to avoid instance synthesis trying this early
 attribute [instance 50] OrderedAddCommGroup.toAddCommGroup
 attribute [instance 50] OrderedCommGroup.toCommGroup
+attribute [instance 50] LinearOrderedAddCommGroupWithTop.toSubNegMonoid
 
 -- add higer-priority versions in scope `AlgebraOrderInstances`
 namespace AlgebraOrderInstances
 attribute [scoped instance 200] OrderedAddCommGroup.toAddCommGroup
 attribute [scoped instance 200] OrderedCommGroup.toCommGroup
+attribute [scoped instance 200] LinearOrderedAddCommGroupWithTop.toSubNegMonoid
 end AlgebraOrderInstances
