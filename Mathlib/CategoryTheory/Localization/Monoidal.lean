3706cdd3ab6bc2fd43bba6abca9a50733cedf879
Joël Riou <joel.riou@universite-paris-saclay.fr>
2024-05-07 16:59:50 +0200

    wip

--- Mathlib/CategoryTheory/Localization/Monoidal.lean
+++ Mathlib/CategoryTheory/Localization/Monoidal.lean
@@ -1,5 +1,5 @@
 import Mathlib.CategoryTheory.Localization.Bifunctor
-import Mathlib.CategoryTheory.Monoidal.Category
+import Mathlib.CategoryTheory.Monoidal.Functor
 
 namespace CategoryTheory
 
@@ -29,16 +29,16 @@ lemma tensorHom_mem {X₁ X₂ : C} (f : X₁ ⟶ X₂) {Y₁ Y₂ : C} (g : Y�
 
 end MorphismProperty
 
-namespace Localization
-
 @[nolint unusedArguments]
-def localizedMonoidal (L : C ⥤ D) (W : MorphismProperty C)
+def LocalizedMonoidal (L : C ⥤ D) (W : MorphismProperty C)
     [W.Monoidal] [L.IsLocalization W] {unit : D}
     (_ : L.obj (𝟙_ C) ≅ unit) := D
 
 variable [W.Monoidal] [L.IsLocalization W] {unit : D} (ε : L.obj (𝟙_ C) ≅ unit)
 
-instance : Category (localizedMonoidal L W ε) :=
+namespace Localization
+
+instance : Category (LocalizedMonoidal L W ε) :=
   inferInstanceAs (Category D)
 
 namespace Monoidal
@@ -54,6 +54,11 @@ lemma isInvertedBy₂ :
 
 noncomputable abbrev tensorBifunctor : D ⥤ D ⥤ D :=
   Localization.lift₂ _ (isInvertedBy₂ L W) L L
+
+noncomputable abbrev tensorBifunctorIso :
+    (whiskeringLeft₂ObjObj L L D).obj (tensorBifunctor L W) ≅
+      (whiskeringRight₂' C C L).obj (curriedTensor C) :=
+  Lifting₂.iso L L W W (((whiskeringRight₂' _ _ L).obj (curriedTensor C))) (tensorBifunctor L W)
 
 noncomputable instance (X : C) :
     Lifting L W (tensorLeft X ⋙ L) ((tensorBifunctor L W).obj (L.obj X)) :=
@@ -76,17 +81,63 @@ noncomputable def rightUnitor : (tensorBifunctor L W).flip.obj unit ≅ 𝟭 _ :
       (isoWhiskerRight (rightUnitorNatIso C) _ ≪≫ L.leftUnitor)
 
 noncomputable instance monoidalCategoryStruct :
-    MonoidalCategoryStruct (localizedMonoidal L W ε) where
+    MonoidalCategoryStruct (LocalizedMonoidal L W ε) where
   tensorObj X Y := ((tensorBifunctor L W).obj X).obj Y
   whiskerLeft X _ _ g := ((tensorBifunctor L W).obj X).map g
   whiskerRight f Y := ((tensorBifunctor L W).map f).app Y
   tensorUnit := unit
   associator := sorry -- needs localization of trifunctors
-  leftUnitor X := (leftUnitor L W ε).app X
-  rightUnitor Y := (rightUnitor L W ε).app Y
+  leftUnitor Y := (leftUnitor L W ε).app Y
+  rightUnitor X := (rightUnitor L W ε).app X
+
+def toMonoidalCategory : C ⥤ LocalizedMonoidal L W ε := L
+
+def ε' : (toMonoidalCategory L W ε).obj (𝟙_ C) ≅ unit := ε
+
+local notation "L'" => toMonoidalCategory L W ε
+
+noncomputable def μ (X Y : C) : (L').obj X ⊗ (L').obj Y ≅ (L').obj (X ⊗ Y) :=
+  ((tensorBifunctorIso L W).app X).app Y
+
+@[reassoc (attr := simp)]
+lemma μ_natural_left {X₁ X₂ : C} (f : X₁ ⟶ X₂) (Y : C) :
+    (L').map f ▷ (L').obj Y ≫ (μ L W ε X₂ Y).hom =
+      (μ L W ε X₁ Y).hom ≫ (L').map (f ▷ Y) :=
+  NatTrans.naturality_app (tensorBifunctorIso L W).hom Y f
+
+@[reassoc (attr := simp)]
+lemma μ_natural_right (X : C) {Y₁ Y₂ : C} (g : Y₁ ⟶ Y₂) :
+    (L').obj X ◁ (L').map g ≫ (μ L W ε X Y₂).hom =
+      (μ L W ε X Y₁).hom ≫ (toMonoidalCategory L W ε).map (X ◁ g) :=
+  ((tensorBifunctorIso L W).hom.app X).naturality g
+
+lemma leftUnitor_hom_app (Y : C) :
+    (λ_ ((L').obj Y)).hom =
+      (ε' L W ε).inv ▷ (L').obj Y ≫ (μ _ _ _ _ _).hom ≫ (L').map (λ_ Y).hom := by
+  sorry
+
+lemma rightUnitor_hom_app (X : C) :
+    (ρ_ ((L').obj X)).hom =
+      (L').obj X ◁ (ε' L W ε).inv ≫ (μ _ _ _ _ _).hom ≫
+        (L').map (ρ_ X).hom := by
+  sorry
+
+variable {L W ε} in
+lemma pentagon (Y₁ Y₂ Y₃ Y₄ : LocalizedMonoidal L W ε) :
+    (α_ Y₁ Y₂ Y₃).hom ▷ Y₄ ≫ (α_ Y₁ (Y₂ ⊗ Y₃) Y₄).hom ≫ Y₁ ◁ (α_ Y₂ Y₃ Y₄).hom =
+      (α_ (Y₁ ⊗ Y₂) Y₃ Y₄).hom ≫ (α_ Y₁ Y₂ (Y₃ ⊗ Y₄)).hom := by
+  suffices ∀ (X₁ X₂ X₃ X₄ : C),
+    (α_ ((L').obj X₁) ((L').obj X₂) ((L').obj X₃)).hom ▷ (L').obj X₄ ≫
+      (α_ ((L').obj X₁) (((L').obj X₂) ⊗ ((L').obj X₃)) ((L').obj X₄)).hom ≫
+      ((L').obj X₁) ◁ (α_ ((L').obj X₂) ((L').obj X₃) ((L').obj X₄)).hom =
+    (α_ (((L').obj X₁) ⊗ ((L').obj X₂)) ((L').obj X₃) ((L').obj X₄)).hom ≫
+      (α_ ((L').obj X₁) ((L').obj X₂) (((L').obj X₃) ⊗ ((L').obj X₄))).hom by
+    -- better do a general lemma `pentagon_of_iso` assuming `MonoidalCategoryStruct`
+    sorry
+  sorry
 
 noncomputable instance :
-    MonoidalCategory (localizedMonoidal L W ε) where
+    MonoidalCategory (LocalizedMonoidal L W ε) where
   tensorHom_def := by intros; simp [monoidalCategoryStruct]
   tensor_id := by intros; simp [monoidalCategoryStruct]
   tensor_comp := by intros; simp [monoidalCategoryStruct]
@@ -95,15 +146,22 @@ noncomputable instance :
   associator_naturality := sorry
   leftUnitor_naturality := by intros; simp [monoidalCategoryStruct]
   rightUnitor_naturality f := (rightUnitor L W ε).hom.naturality f
-  pentagon := sorry
+  pentagon := pentagon
   triangle := sorry
 
 end Monoidal
 
 end Localization
 
-namespace Localization
+open Localization.Monoidal
 
-end Localization
+noncomputable def toLocalizedMonoidal :
+    MonoidalFunctor C (LocalizedMonoidal L W ε) where
+  toFunctor := toMonoidalCategory L W ε
+  ε := ε.inv
+  μ X Y := (μ L W ε X Y).hom
+  associativity := sorry
+  left_unitality Y := leftUnitor_hom_app L W ε Y
+  right_unitality X := rightUnitor_hom_app L W ε X
 
 end CategoryTheory
