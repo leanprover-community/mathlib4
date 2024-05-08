85e9d713f502c95936699e7bf49fa92207197801
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2024-05-08 22:54:19 +0200

    fix Analysis.Calculus.ContDiff.Defs

--- Mathlib/Analysis/Calculus/ContDiff/Defs.lean
+++ Mathlib/Analysis/Calculus/ContDiff/Defs.lean
@@ -856,7 +856,9 @@ theorem iteratedFDerivWithin_succ_apply_right {n : ℕ} (hs : UniqueDiffOn 𝕜 
         rw [fderivWithin_congr A (A x hx)]
       _ = (I ∘ fderivWithin 𝕜 (iteratedFDerivWithin 𝕜 n (fderivWithin 𝕜 f s) s) s x :
               E → E[×n + 1]→L[𝕜] F) (m 0) (tail m) := by
-        simp only [LinearIsometryEquiv.comp_fderivWithin _ (hs x hx)]; rfl
+        -- added `I` and implicit argument `f`
+        simp only [LinearIsometryEquiv.comp_fderivWithin I
+          (f := iteratedFDerivWithin 𝕜 n (fderivWithin 𝕜 f s) s) (hs x hx)]; rfl
       _ = (fderivWithin 𝕜 (iteratedFDerivWithin 𝕜 n (fun y => fderivWithin 𝕜 f s y) s) s x :
               E → E[×n]→L[𝕜] E →L[𝕜] F) (m 0) (init (tail m)) ((tail m) (last n)) := rfl
       _ = iteratedFDerivWithin 𝕜 (Nat.succ n) (fun y => fderivWithin 𝕜 f s y) s x (init m)
