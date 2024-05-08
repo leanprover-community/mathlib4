b3e2476547c403137d1626d20b28fda02478bb19
Scott Morrison <scott.morrison@gmail.com>
2024-05-08 12:58:20 +1000

    bump aesop

--- lakefile.lean
+++ lakefile.lean
@@ -23,7 +23,7 @@ require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
 
 require batteries from git "https://github.com/leanprover-community/batteries" @ "nightly-testing"
 require Qq from git "https://github.com/leanprover-community/quote4" @ "master"
-require aesop from git "https://github.com/leanprover-community/aesop" @ "master"
+require aesop from git "https://github.com/leanprover-community/aesop" @ "lean-pr-testing-4096"
 require proofwidgets from git "https://github.com/leanprover-community/ProofWidgets4" @ "v0.0.36"
 require Cli from git "https://github.com/leanprover/lean4-cli" @ "main"
 require importGraph from git "https://github.com/leanprover-community/import-graph.git" @ "main"
