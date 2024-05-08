a0bf71bf438e52fa0526c609a40e0995f0e1bb30
Joël Riou <37772949+joelriou@users.noreply.github.com>
2024-05-08 11:13:45 +0200

    Update Mathlib/CategoryTheory/Closed/Ideal.lean
    Co-authored-by: Matthew Robert Ballard <100034030+mattrobball@users.noreply.github.com>

--- Mathlib/CategoryTheory/Closed/Ideal.lean
+++ Mathlib/CategoryTheory/Closed/Ideal.lean
@@ -211,7 +211,7 @@ theorem bijection_symm_apply_id (A B : C) :
   dsimp only [Functor.comp_obj]
   rw [prod.comp_lift_assoc, prod.lift_snd, prod.lift_fst_assoc, prod.lift_fst_comp_snd_comp,
     ← Adjunction.eq_homEquiv_apply, Adjunction.homEquiv_unit, Iso.comp_inv_eq, assoc]
-  erw [PreservesLimitPair.iso_hom i ((reflector i).obj A) ((reflector i).obj B)]
+  rw [PreservesLimitPair.iso_hom i ((reflector i).obj A) ((reflector i).obj B)]
   apply prod.hom_ext
   · rw [Limits.prod.map_fst, assoc, assoc, prodComparison_fst, ← i.map_comp, prodComparison_fst]
     apply (reflectorAdjunction i).unit.naturality
