9fdb3e8e8f7d15cd1c6e8e389ee96e3aaf8e5d0e
Joël Riou <joel.riou@universite-paris-saclay.fr>
2024-05-09 17:00:46 +0200

    feat(CategoryTheory): right derived functors

--- Mathlib.lean
+++ Mathlib.lean
@@ -1263,6 +1263,7 @@ import Mathlib.CategoryTheory.Functor.Basic
 import Mathlib.CategoryTheory.Functor.Category
 import Mathlib.CategoryTheory.Functor.Const
 import Mathlib.CategoryTheory.Functor.Currying
+import Mathlib.CategoryTheory.Functor.Derived.RightDerived
 import Mathlib.CategoryTheory.Functor.EpiMono
 import Mathlib.CategoryTheory.Functor.Flat
 import Mathlib.CategoryTheory.Functor.FullyFaithful
