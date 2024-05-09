18974fc3dc250ebf36f92fc6bfc277b3d25c26cc
Vasily Nesterov <vas.nesterov63@gmail.com>
2024-05-09 19:18:06 +0300

    unused import

--- Mathlib/Tactic/Linarith/SimplexAlgorithm/SimplexAlgorithm.lean
+++ Mathlib/Tactic/Linarith/SimplexAlgorithm/SimplexAlgorithm.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Vasily Nesterov
 -/
 import Mathlib.Tactic.Linarith.SimplexAlgorithm.Datatypes
-import Mathlib.Tactic.Linarith.Datatypes
 
 /-!
 # Simplex Algorithm
