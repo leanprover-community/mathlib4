07be63a4ffc6e1cd8eaa77f4862c4c40d010784e
Michael Rothgang <rothgami@math.hu-berlin.de>
2024-05-10 23:58:03 +0200

    More comments.

--- Mathlib/Data/List/DropRight.lean
+++ Mathlib/Data/List/DropRight.lean
@@ -252,12 +252,19 @@ lemma rdrop_add (i j : ℕ) : (l.rdrop i).rdrop j = l.rdrop (i + j) := by
 @[simp]
 lemma rdrop_append_length {l₁ l₂ : List α} :
     List.rdrop (l₁ ++ l₂) (List.length l₂) = l₁:= by
-  rw [List.rdrop_eq_reverse_drop_reverse, ← length_reverse l₂,
+  rw [rdrop_eq_reverse_drop_reverse, ← length_reverse l₂,
       reverse_append, drop_left, reverse_reverse]
 
+lemma rdrop_append_of_le_length {l₁ l₂ : List α} (k : ℕ) :
+    k ≤ length l₂ → List.rdrop (l₁ ++ l₂) k = l₁ ++ List.rdrop l₂ k := by
+  intro hk
+  rw [← length_reverse] at hk
+  rw [rdrop_eq_reverse_drop_reverse, reverse_append, drop_append_of_le_length hk,
+    reverse_append, reverse_reverse, ← rdrop_eq_reverse_drop_reverse]
+
 @[simp]
-lemma rdrop_append_length_add {l₁ l₂ : List α} (i : ℕ) :
-    List.rdrop (l₁ ++ l₂) (List.length l₂ + i)  = List.rdrop l₁ i:= by
+lemma rdrop_append_length_add {l₁ l₂ : List α} (k : ℕ) :
+    List.rdrop (l₁ ++ l₂) (length l₂ + k)  = List.rdrop l₁ k:= by
   rw [← rdrop_add, rdrop_append_length]
 
 end List
