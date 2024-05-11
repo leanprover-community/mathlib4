e2d928188e93fa0596622c7eba77ddc5aab32807
RemyDegenne <Remydegenne@gmail.com>
2024-05-11 10:56:12 +0200

    fix

--- Mathlib/MeasureTheory/Measure/WithDensityFinite.lean
+++ Mathlib/MeasureTheory/Measure/WithDensityFinite.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Rémy Degenne
 -/
 import Mathlib.MeasureTheory.Decomposition.RadonNikodym
-import Mathlib.MeasureTheory.Decomposition.Exhaustion
 import Mathlib.Probability.ConditionalProbability
 
 /-!
