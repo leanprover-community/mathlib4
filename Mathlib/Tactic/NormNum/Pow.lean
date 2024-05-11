de34d769cf60f2ea02d9ebfa1ae65b9e94769f48
Yaël Dillies <yael.dillies@gmail.com>
2024-05-11 11:40:47 +0000

    shake

--- Mathlib/Tactic/NormNum/Pow.lean
+++ Mathlib/Tactic/NormNum/Pow.lean
@@ -3,7 +3,6 @@ Copyright (c) 2021 Mario Carneiro. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro, Thomas Murrills
 -/
-import Mathlib.Algebra.GroupPower.Ring
 import Mathlib.Data.Int.Cast.Lemmas
 import Mathlib.Tactic.NormNum.Basic
 
