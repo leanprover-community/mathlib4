b5b307b9f0dc97bf94a89b8548ccb1a879467d79
Yaël Dillies <yael.dillies@gmail.com>
2024-05-11 14:08:37 +0000

    chore: Delete `Data.Int.Dvd.Pow` These three lemmas are oddly stated and completely unused in mathlib.

--- Mathlib.lean
+++ Mathlib.lean
@@ -1914,7 +1914,6 @@ import Mathlib.Data.Int.ConditionallyCompleteOrder
 import Mathlib.Data.Int.Defs
 import Mathlib.Data.Int.Div
 import Mathlib.Data.Int.Dvd.Basic
-import Mathlib.Data.Int.Dvd.Pow
 import Mathlib.Data.Int.GCD
 import Mathlib.Data.Int.Interval
 import Mathlib.Data.Int.LeastGreatest
