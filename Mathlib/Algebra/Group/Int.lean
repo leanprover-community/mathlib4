5f568c66d3e3fa0d5e1beeb134e089ca4a2b2c21
Yaël Dillies <yael.dillies@gmail.com>
2024-05-12 14:20:44 +0000

    lint

--- Mathlib/Algebra/Group/Int.lean
+++ Mathlib/Algebra/Group/Int.lean
@@ -100,7 +100,7 @@ end Multiplicative
 
 variable {u v : ℤ}
 
-@[simp] lemma units_natAbs (u : ℤˣ) : natAbs u = 1 :=
+lemma units_natAbs (u : ℤˣ) : natAbs u = 1 :=
   Units.ext_iff.1 <|
     Nat.units_eq_one
       ⟨natAbs u, natAbs ↑u⁻¹, by rw [← natAbs_mul, Units.mul_inv]; rfl, by
