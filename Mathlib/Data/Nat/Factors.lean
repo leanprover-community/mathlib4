39854d8fc91be22e16902b3bd6a53803bce92c6d
Yaël Dillies <yael.dillies@gmail.com>
2024-05-12 15:41:09 +0000

    shake, lint

--- Mathlib/Data/Nat/Factors.lean
+++ Mathlib/Data/Nat/Factors.lean
@@ -3,6 +3,7 @@ Copyright (c) 2015 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Leonardo de Moura, Jeremy Avigad, Mario Carneiro
 -/
+import Mathlib.Algebra.BigOperators.Ring.List
 import Mathlib.Data.Nat.Prime
 import Mathlib.Data.List.Prime
 import Mathlib.Data.List.Sort
