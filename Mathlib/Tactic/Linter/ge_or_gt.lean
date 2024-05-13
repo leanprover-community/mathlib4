9bb7ea0ea9daaff0f792df9de5d386e79eb08f04
Michael Rothgang <rothgami@math.hu-berlin.de>
2024-05-13 17:08:07 +0200

    Add new file; almost no boilerplate.

---
+++ Mathlib/Tactic/Linter/ge_or_gt.lean
@@ -0,0 +1,17 @@
+/-
+Copyright (c) 2024 Michael Rothgang. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Michael Rothgang
+-/
+
+import Mathlib -- minimize later!
+
+/-!
+## The `ge_or_gt` linter
+
+A linter for checking whether a declaration contains `≥` or `>`.
+
+TODO currently only in the conclusion? xxx compare with mathlib3!
+-/
+
+-- content here
