757e593ea428bb1198609962946f561027cfe7e9
Joël Riou <joel.riou@universite-paris-saclay.fr>
2024-05-13 14:38:41 +0200

    fixing the build

--- Mathlib/Algebra/Category/ModuleCat/Presheaf/Abelian.lean
+++ Mathlib/Algebra/Category/ModuleCat/Presheaf/Abelian.lean
@@ -24,12 +24,12 @@ variable {C : Type u₁} [Category.{v₁} C] (R : Cᵒᵖ ⥤ RingCat.{u})
 noncomputable instance : NormalEpiCategory (PresheafOfModules.{v} R) where
   normalEpiOfEpi p _ := NormalEpi.mk _ (kernel.ι p)  (kernel.condition _)
     (evaluationJointlyReflectsColimits _ _ (fun _ =>
-      Abelian.isColimitMapCoconeOfCokernelCoforkOfπKernelConditionOfEpi _ _))
+      Abelian.isColimitMapCoconeOfCokernelCoforkOfπ _ _))
 
 noncomputable instance : NormalMonoCategory (PresheafOfModules.{v} R) where
   normalMonoOfMono i _ := NormalMono.mk _ (cokernel.π i) (cokernel.condition _)
     (evaluationJointlyReflectsLimits _ _ (fun _ =>
-      Abelian.isLimitMapConeOfKernelForkOfιCokernelConditionOfMono _ _))
+      Abelian.isLimitMapConeOfKernelForkOfι _ _))
 
 noncomputable instance : Abelian (PresheafOfModules.{v} R) where
 
