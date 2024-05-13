7dbcb051e690d086129ab91e98caf9106938d86f
Michael Rothgang <rothgami@math.hu-berlin.de>
2024-05-13 17:20:14 +0200

    Slightly more boilerplate.

--- Mathlib/Tactic/Linter/ge_or_gt.lean
+++ Mathlib/Tactic/Linter/ge_or_gt.lean
@@ -19,6 +19,10 @@ open Lean Elab Command
 
 namespace Mathlib.Linter.ge_or_gt
 
+def contains_illegal_ge_gt : Syntax → Bool
+  -- TODO: fill in!
+  | _stx => true
+
 /-- The `ge_or_gt` linter emits a warning if a declaration contains `≥` or `>`
   in illegal places. -/
 register_option linter.geOrGt : Bool := {
@@ -38,9 +42,13 @@ def getOrGtLinter : Linter where
     if (← MonadState.get).messages.hasErrors then
       return
     -- content here
-    -- match thmNoDoc stx with
+    match contains_illegal_ge_gt stx with
+      | false => return
+      | true => return
     -- | none => return
     -- | some (thm, _id) =>
+    -- Linter.logLint linter.geOrGt thm m!"'theorem' requires a doc-string. \
+    --                                           Either add one to '{id}' or use 'lemma' instead."
     --   logInfoAt thm ""
 
 initialize addLinter getOrGtLinter
