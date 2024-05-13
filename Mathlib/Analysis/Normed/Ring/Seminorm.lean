4b6e4fbdf9428ce3fae3fe462906b9e97bb4aad3
NirvanaC93 <nirvanac93@gmail.com>
2024-05-13 12:51:15 +0200

    linting

--- Mathlib/Analysis/Normed/Ring/Seminorm.lean
+++ Mathlib/Analysis/Normed/Ring/Seminorm.lean
@@ -394,7 +394,7 @@ open Int
 
 /-- A multiplicative norm composed with the absolute value on integers equals the norm itself. -/
 lemma MulRingNorm.apply_natAbs_eq {R : Type*} [Ring R] (x : ℤ) (f : MulRingNorm R) : f (natAbs x) =
-  f x := by
+    f x := by
   obtain ⟨n,rfl|rfl⟩ := eq_nat_or_neg x
   · simp only [natAbs_ofNat, cast_natCast]
   · simp only [natAbs_neg, natAbs_ofNat, cast_neg, cast_natCast, map_neg_eq_map]
