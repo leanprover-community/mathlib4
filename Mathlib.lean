cec3ec0be2b624a38a6d59dd8f51f2377e946144
Yaël Dillies <yael.dillies@gmail.com>
2024-05-13 11:45:26 +0000

    chore: Delete `Algebra.GroupWithZero.Power` (#12825)
    After #12736, all lemmas in `Algebra.GroupWithZero.Power` can move to the earlier files where they belong. The changes to their proofs are minimal. It's just a matter of using the new Lean 4 `Nat`- and `Int`-specific lemmas instead of the generic algebraic order ones.

--- Mathlib.lean
+++ Mathlib.lean
@@ -266,7 +266,6 @@ import Mathlib.Algebra.GroupWithZero.InjSurj
 import Mathlib.Algebra.GroupWithZero.NeZero
 import Mathlib.Algebra.GroupWithZero.NonZeroDivisors
 import Mathlib.Algebra.GroupWithZero.Pi
-import Mathlib.Algebra.GroupWithZero.Power
 import Mathlib.Algebra.GroupWithZero.Semiconj
 import Mathlib.Algebra.GroupWithZero.Units.Basic
 import Mathlib.Algebra.GroupWithZero.Units.Equiv
