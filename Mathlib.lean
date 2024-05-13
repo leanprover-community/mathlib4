f68d476e0209f205bc39e747e28987c1f4d3c864
Yury G. Kudryashov <urkud@urkud.name>
2024-05-12 21:38:06 -0500

    feat: define `MeasureTheory.OuterMeasureClass`

--- Mathlib.lean
+++ Mathlib.lean
@@ -2971,6 +2971,7 @@ import Mathlib.MeasureTheory.Measure.WithDensityVectorMeasure
 import Mathlib.MeasureTheory.Order.Group.Lattice
 import Mathlib.MeasureTheory.Order.Lattice
 import Mathlib.MeasureTheory.OuterMeasure.Basic
+import Mathlib.MeasureTheory.OuterMeasure.Defs
 import Mathlib.MeasureTheory.PiSystem
 import Mathlib.MeasureTheory.SetSemiring
 import Mathlib.MeasureTheory.Tactic
