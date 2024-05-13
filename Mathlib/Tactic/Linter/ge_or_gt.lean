d37b917c51167c65d2697dce41fe142a51caaa81
Michael Rothgang <rothgami@math.hu-berlin.de>
2024-05-13 17:14:42 +0200

    Add boilerplate for defining a new linter.

--- Mathlib/Tactic/Linter/ge_or_gt.lean
+++ Mathlib/Tactic/Linter/ge_or_gt.lean
@@ -4,7 +4,8 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Michael Rothgang
 -/
 
-import Mathlib -- minimize later!
+import Lean.Elab.Command
+import Lean.Linter.Util
 
 /-!
 ## The `ge_or_gt` linter
@@ -14,4 +15,34 @@ A linter for checking whether a declaration contains `≥` or `>`.
 TODO currently only in the conclusion? xxx compare with mathlib3!
 -/
 
--- content here
+open Lean Elab Command
+
+namespace Mathlib.Linter.ge_or_gt
+
+/-- The `ge_or_gt` linter emits a warning if a declaration contains `≥` or `>`
+  in illegal places. -/
+register_option linter.geOrGt : Bool := {
+  defValue := true
+  descr := "enable the `ge_or_gt` linter"
+}
+
+-- xxx: should this be moved to a different place?
+/-- Gets the value of the `linter.geOrGt` option. -/
+def getLinterHash (o : Options) : Bool := Linter.getLinterValue linter.geOrGt o
+
+/-- docstring here -/
+def getOrGtLinter : Linter where
+  run := withSetOptionIn fun stx => do
+    unless getLinterHash (← getOptions) do
+      return
+    if (← MonadState.get).messages.hasErrors then
+      return
+    -- content here
+    -- match thmNoDoc stx with
+    -- | none => return
+    -- | some (thm, _id) =>
+    --   logInfoAt thm ""
+
+initialize addLinter getOrGtLinter
+
+end Mathlib.Linter.ge_or_gt
