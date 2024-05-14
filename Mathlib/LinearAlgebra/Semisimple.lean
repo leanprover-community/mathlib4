106d2576c5e42843e0c0e62d3557a42add84519c
Kim Morrison <kim@tqft.net>
2024-05-15 09:50:44 +1000

    revert a change

--- Mathlib/LinearAlgebra/Semisimple.lean
+++ Mathlib/LinearAlgebra/Semisimple.lean
@@ -160,8 +160,11 @@ theorem IsSemisimple.of_mem_adjoin_pair {a : End K M} (ha : a ∈ Algebra.adjoin
   /--
   After https://github.com/leanprover/lean4/pull/4119 we either need
   to specify the `(S := R)` argument, or use `set_option maxSynthPendingDepth 2 in`.
+
+  In either case this step is too slow!
   -/
-  have : IsScalarTower K R S := .of_algebraMap_eq (S := R) fun _ ↦ rfl
+  set_option maxSynthPendingDepth 2 in
+  have : IsScalarTower K R S := .of_algebraMap_eq fun _ ↦ rfl
   have : Finite K S := .trans R S
   have : IsArtinianRing R := .of_finite K R
   have : IsReduced R := (Ideal.isRadical_iff_quotient_reduced _).mp <|
