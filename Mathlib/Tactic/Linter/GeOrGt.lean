a106a393a709c2f7a77368a2dea3934318d14de5
Michael Rothgang <rothgami@math.hu-berlin.de>
2024-05-14 17:08:17 +0200

    Clean up code and polish tests.

--- Mathlib/Tactic/Linter/GeOrGt.lean
+++ Mathlib/Tactic/Linter/GeOrGt.lean
@@ -47,8 +47,9 @@ register_option linter.geOrGt : Bool := {
 def getLinterHash (o : Options) : Bool := Linter.getLinterValue linter.geOrGt o
 
 -- bug in the elaborator: inlining this fails
-def is_bla (start : Syntax) : Bool :=
-  match start with
+/-- Whether `stx` matches a `>` or `≥` in a comparator. -/
+def is_in_comparator (stx : Syntax) : Bool :=
+  match stx with
   | `((· > · )) | `((· ≥ ·)) => true
   | _ => false
 
@@ -61,28 +62,12 @@ def getOrGtLinter : Linter where
       return
     match stx.findStack? (fun _ ↦ true) is_ge_or_gt with
     | some ((head, _n)::chain) =>
-      -- if the `≥` or `>` is surrounded by ⬝, this is a comparator: continue
+      -- if the `≥` or `>` symbol is contained in a `(⬝ > ⬝)`, this is a comparator: continue
       let r : Bool := match chain with
-       | (start, _n)::_rest =>
-        is_bla start
+       | (start, _n)::_rest => is_in_comparator start
        | _ => false
       if r then
-        logInfo (toMessageData chain)
         return
-      -- let r := head.find? is_ge_or_gt
-      -- if let some s ← r then
-      --   return
-      -- let dot := `(sdf)
-      -- xxx how to get item before and after!
-      --match head with
-      --| `(⬝ > ⬝) => return--`(⬝)::[]]
-
-      -- match _chain with
-      --  | body::last =>
-      --   have : _chain ≠ [] := sorry
-      --   return
-      --  | _ => return--if let some l ← _chain.getLast then
-      --Linter.logLint linter.geOrGt last sorry m!"last item is"
       Linter.logLint linter.geOrGt head m!"'≥ or > is used in an illegal position\n\
       please change the statement to use ≤ or < instead"
     | _ => return
--- test/GeOrGt.lean
+++ test/GeOrGt.lean
@@ -7,22 +7,28 @@ import Mathlib.Tactic.Common
 
 -- Custom notation (e.g. `ℚ≥0`) is also ignored, as the `≥` is part of a token
 -- and not a "greater or equal".
---local notation3 "𝕜≥0" => ℕ
---lemma fine : ℚ≥0 := 1
-/-
+local notation "ℚ≥0" => ℕ
+def fine : ℚ≥0 := 1
+
+-- The linter can be disabled.
 set_option linter.geOrGt false in
-lemma test : 3 ≥ 2 := sorry
+lemma test : 3 ≥ 2 := by omega
 
--- ≥ and > under binders ("predicate binders") are also not matched
--- I don't have to do anything, as these are a different syntax kind.
-lemma test2 : ∀ n ≥ 2, n = 2 := sorry
+/-- warning: '≥ or > is used in an illegal position
+please change the statement to use ≤ or < instead
+note: this linter can be disabled with `set_option linter.geOrGt false` -/
+#guard_msgs in
+lemma test' : 3 ≥ 2 := by omega
 
+-- ≥ and > under binders ("predicate binders") are not matched.
+-- This applies to conclusions and hypotheses.
+lemma test2 : ∀ n ≥ 2, True := fun _ _ ↦ trivial
 lemma test3 : ∃ n ≥ 2, n = 2 := by use 2 ; trivial
 
 lemma test4 (_h : ∃ n ≥ 2, n = 2) : True := trivial
-
--- the second one is linted, the first not!
 lemma test5 (_h : ∀ n ≥ 42, n = 0) : True := trivial
+
+-- The linter errors on > and ≥ in the conclusion.
 
 /-- warning: '≥ or > is used in an illegal position
 please change the statement to use ≤ or < instead
@@ -35,7 +41,7 @@ lemma test6 (_h : ∀ n ≥ 42, n = 0) : ∃ m, m > 42 := by use 43; omega
 please change the statement to use ≤ or < instead
 note: this linter can be disabled with `set_option linter.geOrGt false` -/
 #guard_msgs in
-lemma test7 : ∃ k : ℤ, k > 100 := by use 200; omega
+lemma test7 : ∃ k : ℤ, k ≥ 100 := by use 200; omega
 
 -- We also check hypotheses.
 
@@ -44,51 +50,81 @@ please change the statement to use ≤ or < instead
 note: this linter can be disabled with `set_option linter.geOrGt false` -/
 #guard_msgs in
 lemma test8 (h : ∀ n : ℤ, n > 100) : ∃ k : ℤ, k > 100 := ⟨200, h 200⟩
--/
+
+/-- warning: '≥ or > is used in an illegal position
+please change the statement to use ≤ or < instead
+note: this linter can be disabled with `set_option linter.geOrGt false` -/
+#guard_msgs in
+lemma test8' (h : ∀ n : ℤ, n ≥ 100) : ∃ k : ℤ, k ≥ 100 := ⟨200, h 200⟩
 
 -- Exclude ≥ as a comparator/unapplied relation; this should not be linted!
 def dummy (_r : ℕ → ℕ → Prop) : Bool := True
 lemma foo (_hf : dummy (· ≥ ·) ) : True := trivial
+lemma foo' (_hf : dummy (· > ·) ) : True := trivial
 
--- Perhaps, we check types of definitions.
+-- We also check types of definitions.
 
--- We do not lint the bodies of proofs.
+/--
+warning: '≥ or > is used in an illegal position
+please change the statement to use ≤ or < instead
+note: this linter can be disabled with `set_option linter.geOrGt false`
+-/
+#guard_msgs in
+def stupid : 4 > 2 := Nat.lt_of_sub_eq_succ rfl
+
+-- We do lint the bodies of proofs: we deemed the trade-off acceptable, for now.
+
+/--
+warning: '≥ or > is used in an illegal position
+please change the statement to use ≤ or < instead
+note: this linter can be disabled with `set_option linter.geOrGt false`
+-/
+#guard_msgs in
 lemma proof1 : True := by
-  have : 42 > 0 := sorry
+  have : 42 > 0 := Nat.zero_lt_succ 41
   trivial
 
+/--
+warning: '≥ or > is used in an illegal position
+please change the statement to use ≤ or < instead
+note: this linter can be disabled with `set_option linter.geOrGt false`
+-/
+#guard_msgs in
 lemma proof2 : 42 > 0 := by
-  have hm (m : ℕ) : m > 0 := sorry
+  have _hm (m : ℕ) : m ≥ 0 := by omega
   -- Meta blocks like `if ...` are also allowed.
   show 42 > 0
   omega
 
+/--
+warning: '≥ or > is used in an illegal position
+please change the statement to use ≤ or < instead
+note: this linter can be disabled with `set_option linter.geOrGt false`
+-/
+#guard_msgs in
 lemma proof3 {m n : ℕ} : True := by
   by_cases _h : m ≥ n
   repeat trivial
 
--- We do not check the bodies of definitions either:
--- we would only see these when unfolding anyway.
-
--- TODO: this should not be linted!
-def dummy2 (_r : ℕ → ℕ → Prop) : Bool := True
-lemma foo2 (_hf : dummy (· ≥ ·) ) : True := trivial
--- another case in SuccPred/Basic.lean: h : `IsWellOrder α (· > ·)` should be fine
+-- We do check the bodies of definitions... no tests since this file is long enough.
 
 /- Looking at all of mathlib, the following are probably false positives
 - most common issue: used as a comparator function, e.g. in
    SuccPred/Basic, Data/List/Chain, Data/List/Range, Data/List/Sort, Data/Fintype/Card,
    Algebra/Lie/Nilpotent, Algebra/Lie/Submodule; MeasureTheory/Function/EssSup
   or as an order, e.g. IsWellOrder, WellFounded, Directed(On)
+=> this should be fixed now!
 
-- in Order/Field/Basic:669, have `>` in a calc proof -> could be fixed
+- in Order/Field/Basic:669, have `>` in a calc proof -> could be fixed there
 - in Order/Ring/Defs:885, entire proof is (line 889 similar):
-  `le_of_not_gt fun ha : a > 0 => (mul_pos ha hb).not_le h`
+  `le_of_not_gt fun ha : a > 0 => (mul_pos ha hb).not_le h` --> similar
 
 - as a branch in meta programming, e.g. `if qa > 0 then` in in NormNum/Inv:150
 other occurrences in Tactic/Ring/Basic, Tactic/Linarith/Datatypes; Data/Finset/Basic
+=< similar, or locally allow the lemma
 
-- Tactic/Linarith/Lemmas has a bunch of `have : xxx > 0`;
+- similarly, allow if needed
+  Tactic/Linarith/Lemmas has a bunch of `have : xxx > 0`;
 ... perhaps these are not ideal, but they probably shouldn't get linted
 - `Data/PNat/Basic` has `have hm : (m : ℕ) > 0 := m.pos` (that is later changed)
 
