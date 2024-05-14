4c2e765b8c090a7fe189cf3e48d07aae91e47e27
Alena Gusakov <agusakov@udel.edu>
2024-05-14 14:31:41 -0400

    fjdsk

--- Mathlib/Data/Nat/Gauss.lean
+++ Mathlib/Data/Nat/Gauss.lean
@@ -687,7 +687,7 @@ lemma divNatThing (j : ℕ) : (j * (j + 1) / 2 : ℕ) = (j * (j + 1) : ℚ) / 2 
 
 -- the second one is dependent on the first
 def subspacesBijection2 : {X : Submodule K (V × W) | ¬ Submodule.map (LinearMap.inr K V W) ⊤ ≤ X} ≃
-  {((X, φ) : (Submodule K V × W) × (V × W →ₗ[K] W)) | ∀ x ∈ X, x.2 = 0 ∧ ∀ x ∉ X, φ x = 0} where
+  {(X : (Submodule K (V × W)) × (V × W →ₗ[K] W)) | ∀ x ∈ X.1, x.2 = 0 ∧ ∀ x ∉ X.1, X.2 x = 0} where
     toFun := _
     invFun := _
     left_inv := _
--- Mathlib/Data/Nat/Order/mwe.lean
+++ Mathlib/Data/Nat/Order/mwe.lean
@@ -17,7 +17,7 @@ variable {K : Type u} {V W : Type v}
 variable [Field K] [AddCommGroup V] [Module K V] [AddCommGroup W] [Module K W]
 
 def subspacesBijection2 : {X : Submodule K (V × W) | ¬ Submodule.map (LinearMap.inr K V W) ⊤ ≤ X} ≃
-  {((X, φ) : (Submodule K V × W) × (V × W →ₗ[K] W)) | ∀ x ∈ X, x.2 = 0 ∧ ∀ x ∉ X, φ x = 0} where
+  {(X : (Submodule K (V × W)) × (V × W →ₗ[K] W)) | ∀ x ∈ X.1, x.2 = 0 ∧ ∀ x ∉ X.1, X.2 x = 0} where
     toFun := sorry
     invFun := sorry
     left_inv := sorry
