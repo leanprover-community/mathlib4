c583f878f40f055d88c0489d1a2c7be454e5cb6e
adomani <adomani@gmail.com>
2024-05-14 13:39:05 +0100

    add copyright

--- Archive/UnusedVariable.lean
+++ Archive/UnusedVariable.lean
@@ -1,1 +1,7 @@
+/-
+Copyright (c) 2024 Damiano Testa. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Damiano Testa
+-/
+
 example {n : Nat} : True := .intro
