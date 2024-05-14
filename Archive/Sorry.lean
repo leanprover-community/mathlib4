9ec86740719c20d0215c3ba8ec506c1ad7c8b913
adomani <adomani@gmail.com>
2024-05-14 14:12:21 +0100

    test: sorry in theorem

---
+++ Archive/Sorry.lean
@@ -0,0 +1,13 @@
+/-
+Copyright (c) 2024 Damiano Testa. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Damiano Testa
+-/
+
+/-!
+This file is just noisy.
+-/
+
+/-- This file is not too small to contain my proof. -/
+theorem guessMyName {n a b c : Nat} (hn : 3 ≤ n) (h : a ^ n + b ^ n = c ^ n ) : a * b * c = 0 :=
+  sorry
