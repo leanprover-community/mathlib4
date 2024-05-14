95df9103e949eb71d085ab9c5a6a311c176556d5
Michael Rothgang <rothgami@math.hu-berlin.de>
2024-05-14 18:06:06 +0200

    wip set_option

--- Mathlib/Tactic/Linter/Style.lean
+++ Mathlib/Tactic/Linter/Style.lean
@@ -20,15 +20,10 @@ namespace Mathlib.Linter.Style
 /-- Whether a syntax element is a `set_option` call:
 Return the name of the option being set, if any. -/
 def parse_set_option : Syntax → Option (Ident)
-  -- TODO: output "name" instead! optional: validate value!
-  -- TODO how to escape set_option
-  --| `(set_optionS $name:ident) => some ("profiler.42")
-  --| `(set_optionS $name:ident Sin) => some ("42") -- how to escape "in"
-  --| `(command|set_option $name:ident true) => some name
-  | `(command|set_option $name:ident $val) => some name
-  -- test if true or false are handled here! if not, duplicate!!
-  -- four cases; times three more!
-  --| `(set_optionS $name:ident $_val:term Sin) => some ("42") -- escape in
+  -- This handles all four cases: string, number, true and false
+  | `(command|set_option $name:ident $_val) => some name
+  | `(set_option $name:ident $_val in $_x) => some name
+  | `(tactic|set_option $name:ident $_val in $_x) => some name
   | _ => none
 
 def is_set_option : Syntax → Bool :=
@@ -54,16 +49,18 @@ def setOptionLinter : Linter where
     match stx.findStack? (fun _ ↦ true) is_set_option with
     | some ((head, _n)::_chain) =>
       match parse_set_option head with
-      | some (name) =>
+      | some (__name) =>
+        Linter.logLint linter.setOption head m!"Forbidden set_option {__name}; please remove"
+        /-let name := "foo"
         if name.startsWith "pp." || name.startsWith "profiler." || name.startsWith "trace." then
-          Linter.logLint linter.setOption head m!"Forbidden set_option {name}; please remove"
+          Linter.logLint linter.setOption head m!"Forbidden set_option {name}; please remove" -/
       | _ => return
     | _ => return
 
 initialize addLinter setOptionLinter
 
 def parse_import : Syntax → Option (String)
-  | `(import $name:ident) => some ("profiler.42")
+  --| `(import $name:ident) => some ("profiler.42")
   | _ => none
 
 def is_import : Syntax → Bool :=
@@ -79,7 +76,7 @@ register_option linter.broadImport : Bool := {
 }
 
 /-- Gets the value of the `linter.broadImport` option. -/
-def getLinterHash (o : Options) : Bool := Linter.getLinterValue linter.broadImport o
+def getLinterHash' (o : Options) : Bool := Linter.getLinterValue linter.broadImport o
 
 
 end Mathlib.Linter.Style
--- test/lint_style.lean
+++ test/lint_style.lean
@@ -4,5 +4,9 @@ import Mathlib.Tactic.Common
 /-! Tests for the style linter rewrite. -/
 
 set_option pp.all true
+set_option pp.all false
+set_option pp.raw.maxDepth 32
+set_option trace.profiler.output "foo"
+
 set_option autoImplicit true in
 lemma foo : True := trivial
