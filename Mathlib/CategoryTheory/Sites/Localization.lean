59536d852b0b2365b159de101d5ee30e6f9594e6
Joël Riou <joel.riou@universite-paris-saclay.fr>
2024-05-14 11:48:52 +0200

    added lemmas using a general adjunction

--- Mathlib/CategoryTheory/Sites/Localization.lean
+++ Mathlib/CategoryTheory/Sites/Localization.lean
@@ -38,30 +38,52 @@ lemma W_eq_W_range_sheafToPresheaf_obj :
   · rintro ⟨F, rfl⟩
     exact F.cond
 
-section
+lemma W_sheafToPreheaf_map_iff_isIso {F₁ F₂ : Sheaf J A} (φ : F₁ ⟶ F₂) :
+    J.W ((sheafToPresheaf J A).map φ) ↔ IsIso φ := by
+  rw [W_eq_W_range_sheafToPresheaf_obj, LeftBousfield.W_iff_isIso _ _ ⟨_, rfl⟩ ⟨_, rfl⟩,
+    isIso_iff_of_reflects_iso]
+
+section Adjunction
+
+variable {G : (Cᵒᵖ ⥤ A) ⥤ Sheaf J A} (adj : G ⊣ sheafToPresheaf J A)
+
+lemma W_adj_unit_app (P : Cᵒᵖ ⥤ A) : J.W (adj.unit.app P) := by
+  rw [W_eq_W_range_sheafToPresheaf_obj]
+  exact LeftBousfield.W_adj_unit_app adj P
+
+lemma W_iff_isIso_map_of_adjunction {P₁ P₂ : Cᵒᵖ ⥤ A} (f : P₁ ⟶ P₂) :
+    J.W f ↔ IsIso (G.map f) := by
+  rw [W_eq_W_range_sheafToPresheaf_obj]
+  exact LeftBousfield.W_iff_isIso_map adj f
+
+lemma W_eq_inverseImage_isomorphisms_of_adjunction :
+    J.W = (MorphismProperty.isomorphisms _).inverseImage G := by
+  rw [W_eq_W_range_sheafToPresheaf_obj,
+    LeftBousfield.W_eq_inverseImage_isomorphisms adj]
+
+end Adjunction
+
+section HasWeakSheafify
 
 variable [HasWeakSheafify J A]
 
-lemma W_toSheafify (P : Cᵒᵖ ⥤ A) : J.W (toSheafify J P) := by
-  rw [W_eq_W_range_sheafToPresheaf_obj]
-  exact LeftBousfield.W_adj_unit_app (sheafificationAdjunction J A) P
+lemma W_toSheafify (P : Cᵒᵖ ⥤ A) : J.W (toSheafify J P) :=
+  J.W_adj_unit_app (sheafificationAdjunction J A) P
 
 lemma W_iff {P₁ P₂ : Cᵒᵖ ⥤ A} (f : P₁ ⟶ P₂) :
-    J.W f ↔ IsIso ((presheafToSheaf J A).map f) := by
-  rw [W_eq_W_range_sheafToPresheaf_obj]
-  exact LeftBousfield.W_iff_isIso_map (sheafificationAdjunction J A) f
+    J.W f ↔ IsIso ((presheafToSheaf J A).map f) :=
+  J.W_iff_isIso_map_of_adjunction (sheafificationAdjunction J A) f
 
 variable (A) in
 lemma W_eq_inverseImage_isomorphisms :
-    J.W = (MorphismProperty.isomorphisms _).inverseImage (presheafToSheaf J A) := by
-  rw [W_eq_W_range_sheafToPresheaf_obj,
-    LeftBousfield.W_eq_inverseImage_isomorphisms (sheafificationAdjunction J A)]
+    J.W = (MorphismProperty.isomorphisms _).inverseImage (presheafToSheaf J A) :=
+  J.W_eq_inverseImage_isomorphisms_of_adjunction (sheafificationAdjunction J A)
 
 instance : (presheafToSheaf J A).IsLocalization J.W := by
   rw [W_eq_inverseImage_isomorphisms]
   exact (sheafificationAdjunction J A).isLocalization
 
-end
+end HasWeakSheafify
 
 end GrothendieckTopology
 
