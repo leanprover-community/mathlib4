1c1c0038dea85707c91f8d169a512533ed3d26db
Joël Riou <37772949+joelriou@users.noreply.github.com>
2024-05-15 19:39:09 +0200

    Update Mathlib/CategoryTheory/Localization/Bousfield.lean
    Co-authored-by: Johan Commelin <johan@commelin.net>

--- Mathlib/CategoryTheory/Localization/Bousfield.lean
+++ Mathlib/CategoryTheory/Localization/Bousfield.lean
@@ -44,7 +44,7 @@ section
 
 variable (P : C → Prop)
 
-/-- Given a functor `P : C → Prop`, this is the class of morphisms `f : X ⟶ Y`
+/-- Given a predicate `P : C → Prop`, this is the class of morphisms `f : X ⟶ Y`
 such that for all `Z : C` such that `P Z`, the precomposition with `f` induces
 a bijection `(Y ⟶ Z) ≃ (X ⟶ Z)`. -/
 def W : MorphismProperty C := fun _ _ f =>
