4d2fef2005567f3f264932864b938385eeca77fb
Michael Rothgang <rothgami@math.hu-berlin.de>
2024-05-15 08:24:06 +0200

    One true positive.

--- scripts/lint-style.py
+++ scripts/lint-style.py
@@ -499,7 +499,7 @@ if not argv:
     exclude = tuple('foobar'.split(' '))
     # Lint all non-excluded files whose module name starts with this.
     # So "Foo.Bar" will lint all files in module "Foo.Bar" and "Foo.Bar.Baz", etc.
-    dir = 'Tactic'
+    dir = 'Lean.Meta'
     assert '/' not in dir
     print(f"about to lint all files in directory {dir}")
     files = []
