0260e840888b649619c4b5da038a0bf701913e03
Yaël Dillies <yael.dillies@gmail.com>
2024-05-16 17:48:48 +0000

    shake again

--- Mathlib/Data/Int/Order/Lemmas.lean
+++ Mathlib/Data/Int/Order/Lemmas.lean
@@ -3,6 +3,7 @@ Copyright (c) 2016 Jeremy Avigad. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jeremy Avigad
 -/
+import Mathlib.Algebra.GroupWithZero.Divisibility
 import Mathlib.Algebra.Order.Ring.Abs
 
 #align_import data.int.order.lemmas from "leanprover-community/mathlib"@"fc2ed6f838ce7c9b7c7171e58d78eaf7b438fb0e"
