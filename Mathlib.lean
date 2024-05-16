2bff23430d1b714a6c212ce750b5fac093d6b78b
Yaël Dillies <yael.dillies@gmail.com>
2024-05-16 08:10:34 +0000

    chore: Move basic `Int` lemmas earlier (#12821)
    A bunch of lemmas about `Int` can be moved to `Data.Int.Defs` at little cost. This PR moves them and deletes the now empty `Data.Int.Div` and `Data.Int.Dvd.Basic`.
    
    The story is that those lemmas used to rely on lemmas proved for algebraic order structures, hence required the algebraic order instances on `Int` to be available. In Lean 4, those preliminary lemmas have been special-cased to `Nat` and `Int`, so there is no need for the general instances anymore (at least in basic files).

--- Mathlib.lean
+++ Mathlib.lean
@@ -1919,8 +1919,6 @@ import Mathlib.Data.Int.Cast.Prod
 import Mathlib.Data.Int.CharZero
 import Mathlib.Data.Int.ConditionallyCompleteOrder
 import Mathlib.Data.Int.Defs
-import Mathlib.Data.Int.Div
-import Mathlib.Data.Int.Dvd.Basic
 import Mathlib.Data.Int.GCD
 import Mathlib.Data.Int.Interval
 import Mathlib.Data.Int.LeastGreatest
