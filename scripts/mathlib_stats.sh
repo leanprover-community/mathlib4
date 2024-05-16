e504c130b83aed4df7fd48487da4a4f5fa690eae
Damiano Testa <adomani@gmail.com>
2024-05-16 09:17:27 +0200

    docs

--- scripts/mathlib_stats.sh
+++ scripts/mathlib_stats.sh
@@ -1,16 +1,22 @@
-#git ls-files 'Mathlib/*.lean' | xargs cat | grep -v "^$" | wc
+#!/usr/bin/env bash
 
 statsURL=""
 
+## results in 'hash YYYY-MM-DD'
 hashAndDate="$(git log master --since='one week ago' --date=short --pretty="%h %ad" | tail -1)"
 
+## just the commit hash
 commit=${hashAndDate/% */}
+## just the date
 date=${hashAndDate/#* /}
 
+## 'x files changed, y insertions(+), z deletions(-)'
 gdiff="$(git diff --shortstat "${commit}"...HEAD)"
 
+## percentage breakdown of changes
 percent="$(git diff --dirstat "${commit}"...HEAD)"
 
+## insertions-deletions
 net=$(awk -v gd="${gdiff}" 'BEGIN{
   tot=0
   n=split(gd, gda, " ")
@@ -19,4 +25,5 @@ net=$(awk -v gd="${gdiff}" 'BEGIN{
   }
   print -tot }')
 
+## final report
 printf '%s, %s total((+)-(-))\n\n%s\n\n since %s (commit: %s).\n\nTake also a look at the [`Mathlib` stats page](%s).\n' "${gdiff}" "${net}" "${percent}" "${date}" "${commit}" "${statsURL}"
