d11fd8fc4c3dd29749816f7c20fe32540b3e994e
Chris Birkbeck <c.birkbeck@ucl.ac.uk>
2024-05-17 18:53:24 +0100

    merge updates, still wip

--- Mathlib/NumberTheory/ModularForms/EisensteinSeries/IsBoundedAtImInfty.lean
+++ Mathlib/NumberTheory/ModularForms/EisensteinSeries/IsBoundedAtImInfty.lean
@@ -25,19 +25,18 @@ open scoped Topology BigOperators Nat Classical MatrixGroups
 
 namespace EisensteinSeries
 
-lemma eisSummand_bound (k : ℤ) (hk : 3 ≤ k) (z : ℍ) (b : Fin 2 → ℤ) :
-    Complex.abs (eisSummand k b z) ≤
-    ((r z ^ k) * max (Int.natAbs (b 0) : ℝ) (Int.natAbs (b 1)) ^ k)⁻¹ := by
-  have hk0 : 0 ≤ k := by omega
-  lift k to ℕ using hk0
-  simpa only [zpow_natCast, Fin.isValue, _root_.mul_inv_rev, Nat.cast_max] using
-    (eisSummand_is_bounded_on_box (k := k) (max (b 0).natAbs (b 1).natAbs) z b (Int.ofNat_zero_le k)
-      (by simp))
-
 lemma summable_lem (k : ℤ) (hk : 3 ≤ k) (z : ℍ) : Summable fun (x : Fin 2 → ℤ) =>
     Complex.abs (eisSummand k x z) := by
-  apply (summable_upper_bound hk z).of_nonneg_of_le (fun _ => Complex.abs.nonneg _)
-    (eisSummand_bound k hk z)
+  have hk' : (2 : ℝ) < k := by norm_cast
+  apply ((summable_one_div_norm_rpow hk').mul_left <| r z ^ (-k : ℝ)).of_nonneg_of_le
+    (fun _ => Complex.abs.nonneg _)
+  intro b
+  rw [eisSummand]
+  simp only [Fin.isValue, one_div, map_inv₀, map_zpow₀]
+  rw [← inv_zpow, inv_zpow']
+  have hk0 : 0 ≤ (k : ℝ) := by norm_cast; omega
+  have := summand_bound z hk0 b
+  norm_cast at *
 
 lemma abs_le_tsum_abs (N : ℕ) (a : Fin 2 → ZMod N) (k : ℤ) (hk : 3 ≤ k) (z : ℍ):
     Complex.abs ((((eisensteinSeries a k))) z) ≤ ∑' (x : Fin 2 → ℤ),
@@ -50,24 +49,29 @@ lemma abs_le_tsum_abs (N : ℕ) (a : Fin 2 → ZMod N) (k : ℤ) (hk : 3 ≤ k) 
 theorem eisensteinSeries_IsBoundedAtImInfty {N : ℕ+} (a : Fin 2 → ZMod N) (k : ℤ) (hk : 3 ≤ k)
     (A : SL(2, ℤ)) : IsBoundedAtImInfty ((eisensteinSeries_SIF a k).toFun ∣[(k : ℤ)] A) := by
     simp_rw [UpperHalfPlane.bounded_mem, eisensteinSeries_SIF] at *
-    refine ⟨∑'(x : Fin 2 → ℤ),
-      (r ⟨⟨N, 2⟩, Nat.ofNat_pos⟩ ^ k * (max (x 0).natAbs (x 1).natAbs : ℝ) ^ k)⁻¹, 2, ?_⟩
+    refine ⟨∑'(x : Fin 2 → ℤ), (r ⟨⟨N, 2⟩, Nat.ofNat_pos⟩ ^ (-k)) * ‖x‖ ^ (-k), 2, ?_⟩
     intro z hz
     obtain ⟨n, hn⟩ := (ModularGroup_T_zpow_mem_verticalStrip z N N.2)
     rw [eisensteinSeries_slash_apply, ← eisensteinSeries_SIF_apply,
       ← SIF_lvl_N_periodic N k (eisensteinSeries_SIF (a ᵥ* A) k) z n]
     let Z := ((ModularGroup.T ^ ((N : ℤ) * n))) • z
     apply le_trans (abs_le_tsum_abs N _ _ hk (Z))
-    apply tsum_le_tsum _ ((summable_upper_bound hk Z).of_nonneg_of_le
-      (fun _ => Complex.abs.nonneg _) (eisSummand_bound k hk Z)) (summable_upper_bound hk _)
-    intro x
-    apply le_trans ((eisSummand_bound k hk Z x))
-    simp only [Fin.isValue, _root_.mul_inv_rev]
-    gcongr
-    · apply zpow_pos_of_pos (r_pos _)
-    · have hk0 : 0 ≤ k := by linarith
-      lift k to ℕ using hk0
-      push_cast
-      apply pow_le_pow_left (r_pos _).le
-      apply r_lower_bound_on_verticalStrip (A := N) (B := 2)
-          (z:= ⟨Z, (verticalStrip_anti_right (N : ℕ) hz) hn⟩)
+    apply tsum_le_tsum _ (summable_lem k hk _)
+    · have hk' : (2 : ℝ) < k := by norm_cast
+      have := (summable_one_div_norm_rpow hk').mul_left <| r ⟨⟨N, 2⟩, Nat.ofNat_pos⟩ ^ (-k : ℝ)
+      norm_cast at this
+    · intro x
+      have hk0 : 0 ≤ (k : ℝ) := by norm_cast; omega
+      have := summand_bound Z hk0 x
+      simp only [eisSummand, Fin.isValue, one_div, map_inv₀, map_zpow₀, zpow_neg, ge_iff_le]
+      rw [← inv_zpow, inv_zpow']
+      simp_rw [ ←  Int.cast_neg,   Real.rpow_intCast] at this
+      apply le_trans this
+      simp only [zpow_neg]
+      gcongr
+      · apply zpow_pos_of_pos (r_pos _)
+      · have hk0 : 0 ≤ k := by linarith
+        lift k to ℕ using hk0
+        apply pow_le_pow_left (r_pos _).le
+        apply r_lower_bound_on_verticalStrip (A := N) (B := 2) _ _
+          ((verticalStrip_anti_right (N : ℕ) hz) hn)
