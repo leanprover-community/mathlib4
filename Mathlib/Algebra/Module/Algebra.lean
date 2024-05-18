b206221d74dd2a140bff64562de49567146b23ba
Brendan Murphy <shamrockfrost@gmail.com>
2024-05-18 14:12:41 -0700

    Try to golf `smul_restrictScalars_eq_restrictScalars_map_smul` and fail

--- Mathlib/Algebra/Module/Algebra.lean
+++ Mathlib/Algebra/Module/Algebra.lean
@@ -12,14 +12,13 @@ import Mathlib.Algebra.Algebra.Basic
 # Additional facts about modules over algebras.
 -/
 
-
-namespace LinearMap
-
 section RestrictScalars
 
 variable (k : Type*) [CommSemiring k] (A : Type*) [Semiring A] [Algebra k A]
 variable (M : Type*) [AddCommMonoid M] [Module k M] [Module A M] [IsScalarTower k A M]
 variable (N : Type*) [AddCommMonoid N] [Module k N] [Module A N] [IsScalarTower k A N]
+
+namespace LinearMap
 
 /-- Restriction of scalars for linear maps between modules over a `k`-algebra is itself `k`-linear.
 -/
@@ -30,6 +29,17 @@ def restrictScalarsLinearMap : (M →ₗ[A] N) →ₗ[k] M →ₗ[k] N where
   map_smul' _ _ := rfl
 #align linear_map.restrict_scalars_linear_map LinearMap.restrictScalarsLinearMap
 
-end RestrictScalars
-
 end LinearMap
+
+namespace Submodule
+
+lemma restrictScalars_map (f : M →ₗ[A] N) (M' : Submodule A M) :
+    restrictScalars k (map f M') =
+      map (f.restrictScalars k) (restrictScalars k M') :=
+  toAddSubmonoid_injective <| by
+    simp only [map_toAddSubmonoid, toAddSubmonoid_restrictScalars]
+    rfl
+
+end Submodule
+
+end RestrictScalars
