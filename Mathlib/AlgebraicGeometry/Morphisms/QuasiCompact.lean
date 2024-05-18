b493d8565e80fbd1d1869bea9004818293a6f650
JovanGerb <jovan.gerbscheid@gmail.com>
2024-05-18 06:35:47 +0000

    fix error

--- Mathlib/AlgebraicGeometry/Morphisms/QuasiCompact.lean
+++ Mathlib/AlgebraicGeometry/Morphisms/QuasiCompact.lean
@@ -136,7 +136,7 @@ theorem isCompact_basicOpen (X : Scheme) {U : Opens X.carrier} (hU : IsCompact (
     use V.1 ⊓ X.basicOpen f
     have : V.1.1 ⟶ U := by
       apply homOfLE; change _ ⊆ (U : Set X.carrier); rw [e]
-      convert @Set.subset_iUnion₂ _ _ _
+      convert @Set.subset_iUnion₂ X.toPresheafedSpace _ _
         (fun (U : X.affineOpens) (_ : U ∈ s) => ↑U) V V.prop using 1
     erw [← X.toLocallyRingedSpace.toRingedSpace.basicOpen_res this.op]
     exact IsAffineOpen.basicOpenIsAffine V.1.prop _
