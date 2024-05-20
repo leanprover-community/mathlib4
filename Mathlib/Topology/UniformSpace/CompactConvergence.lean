6a732fa509f4feb5db788cabe726bde80a634657
ADedecker <anatolededecker@gmail.com>
2024-05-20 16:56:43 +0200

    Bundle congruence

--- Mathlib/Topology/UniformSpace/CompactConvergence.lean
+++ Mathlib/Topology/UniformSpace/CompactConvergence.lean
@@ -280,50 +280,44 @@ theorem tendstoLocallyUniformly_of_tendsto [WeaklyLocallyCompactSpace α] (h : T
 
 section Functorial
 
-variable {γ : Type*}
+variable {γ δ : Type*} [TopologicalSpace γ] [UniformSpace δ]
 
-theorem uniformContinuous_comp [UniformSpace γ] (g : C(β, γ)) (hg : UniformContinuous g) :
-    UniformContinuous (ContinuousMap.comp g : C(α, β) → C(α, γ)) :=
+theorem uniformContinuous_comp (g : C(β, δ)) (hg : UniformContinuous g) :
+    UniformContinuous (ContinuousMap.comp g : C(α, β) → C(α, δ)) :=
   uniformEmbedding_toUniformOnFunIsCompact.uniformContinuous_iff.mpr <|
     UniformOnFun.postcomp_uniformContinuous hg |>.comp
       uniformEmbedding_toUniformOnFunIsCompact.uniformContinuous
 
-theorem uniformInducing_comp [UniformSpace γ] (g : C(β, γ)) (hg : UniformInducing g) :
-    UniformInducing (ContinuousMap.comp g : C(α, β) → C(α, γ)) :=
+theorem uniformInducing_comp (g : C(β, δ)) (hg : UniformInducing g) :
+    UniformInducing (ContinuousMap.comp g : C(α, β) → C(α, δ)) :=
   uniformEmbedding_toUniformOnFunIsCompact.toUniformInducing.of_comp_iff.mp <|
     UniformOnFun.postcomp_uniformInducing hg |>.comp
       uniformEmbedding_toUniformOnFunIsCompact.toUniformInducing
 
-theorem uniformEmbedding_comp [UniformSpace γ] (g : C(β, γ)) (hg : UniformEmbedding g) :
-    UniformEmbedding (ContinuousMap.comp g : C(α, β) → C(α, γ)) :=
+theorem uniformEmbedding_comp (g : C(β, δ)) (hg : UniformEmbedding g) :
+    UniformEmbedding (ContinuousMap.comp g : C(α, β) → C(α, δ)) :=
   uniformEmbedding_toUniformOnFunIsCompact.of_comp_iff.mp <|
     UniformOnFun.postcomp_uniformEmbedding hg |>.comp
       uniformEmbedding_toUniformOnFunIsCompact
 
-theorem uniformContinuous_comp_left [TopologicalSpace γ] (g : C(α, γ)) :
+theorem uniformContinuous_comp_left (g : C(α, γ)) :
     UniformContinuous (fun f ↦ f.comp g : C(γ, β) → C(α, β)) :=
   uniformEmbedding_toUniformOnFunIsCompact.uniformContinuous_iff.mpr <|
     UniformOnFun.precomp_uniformContinuous (fun _ hK ↦ hK.image g.continuous) |>.comp
       uniformEmbedding_toUniformOnFunIsCompact.uniformContinuous
 
-/-- Any isomorphism `Y ≃ᵤ Z` of uniform spaces gives rise to an isomorphism `C(X, Y) ≃ᵤ C(X, Z)`. -/
-protected def congrRightUniformEquiv [UniformSpace γ] (φ : β ≃ᵤ γ) : C(α, β) ≃ᵤ C(α, γ) where
-  toFun := ContinuousMap.comp φ.toHomeomorph
-  invFun := ContinuousMap.comp φ.symm.toHomeomorph
-  left_inv _ := ext fun _ ↦ φ.left_inv _
-  right_inv _ := ext fun _ ↦ φ.right_inv _
-  uniformContinuous_toFun := uniformContinuous_comp _ φ.uniformContinuous
-  uniformContinuous_invFun := uniformContinuous_comp _ φ.symm.uniformContinuous
-
-/-- Any homeomorphism `X ≃ₜ Y` gives rise to an isomorphism `C(X, Z) ≃ᵤ C(Y, Z)` of uniform
-spaces. -/
-protected def congrLeftUniformEquiv [TopologicalSpace γ] (φ : α ≃ₜ γ) : C(α, β) ≃ᵤ C(γ, β) where
-  toFun f := f.comp φ.symm
-  invFun f := f.comp φ
-  left_inv _ := ext fun _ ↦ congrArg _ (φ.left_inv _)
-  right_inv _ := ext fun _ ↦ congrArg _ (φ.right_inv _)
-  uniformContinuous_toFun := uniformContinuous_comp_left _
-  uniformContinuous_invFun := uniformContinuous_comp_left _
+/-- Any pair of a homeomorphism `X ≃ₜ Z` and an isomorphism `Y ≃ᵤ T` of uniform spaces gives rise
+to an isomorphism `C(X, Y) ≃ᵤ C(Z, T)`. -/
+protected def _root_.UniformEquiv.continuousMapCongr (φ : α ≃ₜ γ) (ψ : β ≃ᵤ δ) :
+    C(α, β) ≃ᵤ C(γ, δ) where
+  toFun f := .comp ψ.toHomeomorph <| f.comp φ.symm
+  invFun f := .comp ψ.symm.toHomeomorph <| f.comp φ
+  left_inv f := ext fun _ ↦ ψ.left_inv (f _) |>.trans <| congrArg f <| φ.left_inv _
+  right_inv f := ext fun _ ↦ ψ.right_inv (f _) |>.trans <| congrArg f <| φ.right_inv _
+  uniformContinuous_toFun := uniformContinuous_comp _ ψ.uniformContinuous |>.comp <|
+    uniformContinuous_comp_left _
+  uniformContinuous_invFun := uniformContinuous_comp _ ψ.symm.uniformContinuous |>.comp <|
+    uniformContinuous_comp_left _
 
 end Functorial
 
