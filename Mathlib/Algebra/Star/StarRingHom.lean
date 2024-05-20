f77e904a3beb4c765933b1f31d87e41059c25b8f
Christopher Hoskin <christopher.hoskin@gmail.com>
2024-05-20 22:37:06 +0100

    Fix

--- Mathlib/Algebra/Star/StarRingHom.lean
+++ Mathlib/Algebra/Star/StarRingHom.lean
@@ -132,8 +132,8 @@ theorem coe_mk (f : A →ₙ+* B) (h) :
   rfl
 
 @[simp]
-theorem mk_coe (f : A →ₙ+* B) (h₁ h₂ h₃ h₄ h₅) :
-    (⟨⟨⟨f, h₁⟩, h₂, h₃⟩, h₄⟩⟩ : A →ₙ+* B) = f := by
+theorem mk_coe (f : A →⋆ₙ+* B) (h₁ h₂ h₃ h₄) :
+    (⟨⟨⟨f, h₁⟩, h₂, h₃⟩, h₄⟩ : A →⋆ₙ+* B) = f := by
   ext
   rfl
 
