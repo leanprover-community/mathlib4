4130749cbdbe43b4e392e68c0b125fff4f24a26f
Hannah Fechtner <hfechtne@andrew.cmu.edu>
2024-05-20 23:05:48 -0400

    strict on authors

--- Mathlib/Algebra/PresentedMonoid.lean/Basic.lean
+++ Mathlib/Algebra/PresentedMonoid.lean/Basic.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2024. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Hannah Fechtner with enormous help from Jeremy Avigad and Patrick Massot
+Authors: Hannah Fechtner
 -/
 
 import Mathlib.Algebra.FreeMonoid.Basic
