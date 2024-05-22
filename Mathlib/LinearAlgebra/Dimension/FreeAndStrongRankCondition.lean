e9c947c32ff1b3ed699a3d811bf5bcc5b503edea
acmepjz <acme_pjz@hotmail.com>
2024-05-22 17:21:48 +0800

    typo

--- Mathlib/LinearAlgebra/Dimension/FreeAndStrongRankCondition.lean
+++ Mathlib/LinearAlgebra/Dimension/FreeAndStrongRankCondition.lean
@@ -11,7 +11,7 @@ import Mathlib.LinearAlgebra.Dimension.Constructions
 # Some results on free modules over rings satisfying strong rank condition
 
 This file contains some results on free modules over rings satisfying strong rank condition.
-Most of then are generalized from the same result assuming the base ring being division ring,
+Most of them are generalized from the same result assuming the base ring being division ring,
 and are moved from the files `Mathlib/LinearAlgebra/Dimension/DivisionRing.lean`
 and `Mathlib/LinearAlgebra/FiniteDimensional.lean`.
 
