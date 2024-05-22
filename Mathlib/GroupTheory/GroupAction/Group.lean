b37c4294a08fbb5c62fd8d42ee117435179cf49c
Yaël Dillies <yael.dillies@gmail.com>
2024-05-22 18:33:27 +0000

    fix GroupTheory.GroupAction.Group

--- Mathlib/GroupTheory/GroupAction/Group.lean
+++ Mathlib/GroupTheory/GroupAction/Group.lean
@@ -5,6 +5,7 @@ Authors: Chris Hughes
 -/
 import Mathlib.Algebra.Group.Aut
 import Mathlib.Algebra.Group.Invertible.Basic
+import Mathlib.Algebra.GroupWithZero.Units.Basic
 import Mathlib.GroupTheory.GroupAction.Units
 
 #align_import group_theory.group_action.group from "leanprover-community/mathlib"@"3b52265189f3fb43aa631edffce5d060fafaf82f"
