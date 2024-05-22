2321d6e00225f6d204824a14c92daee701e560f0
Chris Birkbeck <c.birkbeck@uea.ac.uk>
2024-05-22 15:37:52 +0100

    mathlib fix

--- Mathlib.lean
+++ Mathlib.lean
@@ -3096,6 +3096,7 @@ import Mathlib.NumberTheory.ModularForms.Basic
 import Mathlib.NumberTheory.ModularForms.CongruenceSubgroups
 import Mathlib.NumberTheory.ModularForms.EisensteinSeries.Basic
 import Mathlib.NumberTheory.ModularForms.EisensteinSeries.UniformConvergence
+import Mathlib.NumberTheory.ModularForms.Identities
 import Mathlib.NumberTheory.ModularForms.JacobiTheta.Bounds
 import Mathlib.NumberTheory.ModularForms.JacobiTheta.Manifold
 import Mathlib.NumberTheory.ModularForms.JacobiTheta.OneVariable
