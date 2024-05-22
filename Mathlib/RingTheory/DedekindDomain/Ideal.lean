6458a12925d76997dcfbeb9f5ed81a386fff3cff
Filippo A. E. Nuccio <filippo.nuccio@univ-st-etienne.fr>
2024-05-22 15:48:30 +0200

    Update Mathlib/RingTheory/DedekindDomain/Ideal.lean
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Mathlib/RingTheory/DedekindDomain/Ideal.lean
+++ Mathlib/RingTheory/DedekindDomain/Ideal.lean
@@ -1555,7 +1555,7 @@ theorem count_normalizedFactors_eq_count_normalizedFactors_span
 
 open Classical in
 theorem count_normalizedFactors_eq_associates_count
-  (R : Type*) [CommRing R] [IsDomain R] [IsPrincipalIdealRing R]--cannot comment this!
+    (R : Type*) [CommRing R] [IsDomain R] [IsPrincipalIdealRing R]--cannot comment this!
     (I J : Ideal R) (hI : I ≠ 0) (hJ : J.IsPrime) (hJ₀ : J ≠ ⊥) :
     Multiset.count J (normalizedFactors I) = (Associates.mk J).count (Associates.mk I).factors := by
   replace hI : Associates.mk I ≠ 0 := Associates.mk_ne_zero.mpr hI
