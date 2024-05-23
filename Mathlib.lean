dff169d73ffa832a18973f4ad52c30709a447fb3
Yaël Dillies <yael.dillies@gmail.com>
2024-05-23 11:18:15 +0000

    chore: Remove order dependencies to `Data.Fin.Basic` (#13005)
    Move all the declarations using the mathlib order hierarchy to a new file `Order.Fin`. There were a bunch of order embeddings in `Data.Fin.Basic`, so I kept an embedding version of them around and renamed the order embedding version.

--- Mathlib.lean
+++ Mathlib.lean
@@ -3227,6 +3227,7 @@ import Mathlib.Order.Filter.SmallSets
 import Mathlib.Order.Filter.Subsingleton
 import Mathlib.Order.Filter.Ultrafilter
 import Mathlib.Order.Filter.ZeroAndBoundedAtFilter
+import Mathlib.Order.Fin
 import Mathlib.Order.FixedPoints
 import Mathlib.Order.GaloisConnection
 import Mathlib.Order.GameAdd
