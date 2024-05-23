4072ed9348f54aa781f3eea78b24fcb39e78a285
Mitchell Lee <trivial171@gmail.com>
2024-05-23 01:17:37 +0000

    feat: reflections, inversion sequences in Coxeter groups (#11466)
    - Add theorem `List.length_eraseIdx_add_one` to `Data/List/Basic.lean`, which states that if `i < l.length`, then the length of `l.eraseIdx i plus one` is equal to the length of `l`.
    - Add new file `GroupTheory/Coxeter/Inversion.lean`. Define reflections, left inversions, right inversions, and inversion sequences. Prove their basic properties.
    
    I decided to split #11408 (now closed) into two pull requests. This is the second one. The first is #11465.

--- Mathlib.lean
+++ Mathlib.lean
@@ -2432,6 +2432,7 @@ import Mathlib.GroupTheory.Coprod.Basic
 import Mathlib.GroupTheory.CoprodI
 import Mathlib.GroupTheory.Coset
 import Mathlib.GroupTheory.Coxeter.Basic
+import Mathlib.GroupTheory.Coxeter.Inversion
 import Mathlib.GroupTheory.Coxeter.Length
 import Mathlib.GroupTheory.Coxeter.Matrix
 import Mathlib.GroupTheory.Divisible
