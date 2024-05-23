dc414fb3816b9ec2c4ff2fd2e8136fb255412f89
Brendan Seamas Murphy <shamrockfrost@gmail.com>
2024-05-22 21:22:37 -0600

    Add review golf
    Co-authored-by: Johan Commelin <johan@commelin.net>

--- Mathlib/LinearAlgebra/Quotient.lean
+++ Mathlib/LinearAlgebra/Quotient.lean
@@ -488,7 +488,7 @@ theorem ker_liftQ_eq_bot (f : M →ₛₗ[τ₁₂] M₂) (h) (h' : ker f ≤ p)
 
 theorem ker_liftQ_eq_bot' (f : M →ₛₗ[τ₁₂] M₂) (h : p = ker f) :
     ker (p.liftQ f (le_of_eq h)) = ⊥ :=
-  ker_liftQ_eq_bot p f (le_of_eq h) (le_of_eq h.symm)
+  ker_liftQ_eq_bot p f h.le h.ge
 
 /-- The correspondence theorem for modules: there is an order isomorphism between submodules of the
 quotient of `M` by `p`, and submodules of `M` larger than `p`. -/
