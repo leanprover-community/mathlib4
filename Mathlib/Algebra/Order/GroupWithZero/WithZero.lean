a4f3d35132a3495428dc79a3343a3f5f033462b2
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2024-05-23 12:24:52 +0100

    add two more instances

--- Mathlib/Algebra/Order/GroupWithZero/WithZero.lean
+++ Mathlib/Algebra/Order/GroupWithZero/WithZero.lean
@@ -23,9 +23,6 @@ which is `PosMulStrictMono (WithZero α)`.
 The type `ℤₘ₀ := WithZero (Multiplicative ℤ)` is used a lot in mathlib's valuation
 theory. These instances enable lemmas such as `mul_pos` to fire on `ℤₘ₀`.
 
-## TODO
-
-Add MulPosStrictMono and PosMulMono instances.
 -/
 
 -- this makes `mul_lt_mul_left`, `mul_pos` etc work on `ℤₘ₀`
@@ -39,6 +36,32 @@ instance {α : Type*} [Mul α] [Preorder α] [CovariantClass α α (· * ·) (·
         dsimp only
         norm_cast at h ⊢
         exact mul_lt_mul_left' h x
+
+open Function in
+instance {α : Type*} [Mul α] [Preorder α] [CovariantClass α α (swap (· * ·)) (· < ·)]:
+    MulPosStrictMono (WithZero α) where
+  elim := @fun
+    | ⟨(x : α), hx⟩, 0, (b : α), _ => by
+        rw [zero_mul]
+        exact WithZero.zero_lt_coe _
+    | ⟨(x : α), hx⟩, (a : α), (b : α), h => by
+        dsimp only
+        norm_cast at h ⊢
+        exact mul_lt_mul_right' h x
+
+instance {α : Type*} [Mul α] [Preorder α] [CovariantClass α α (· * ·) (· ≤ ·)]:
+    PosMulMono (WithZero α) where
+  elim := @fun
+    | ⟨0, _⟩, a, b, _ => by
+        simp only [zero_mul, le_refl]
+    | ⟨(x : α), _⟩, 0, _, _ => by
+        simp only [mul_zero, WithZero.zero_le]
+    | ⟨(x : α), hx⟩, (a : α), 0, h =>
+        (lt_irrefl 0 (lt_of_lt_of_le (WithZero.zero_lt_coe a) h)).elim
+    | ⟨(x : α), hx⟩, (a : α), (b : α), h => by
+        dsimp only
+        norm_cast at h ⊢
+        exact mul_le_mul_left' h x
 
 -- This makes `lt_mul_of_le_of_one_lt'` work on `ℤₘ₀`
 open Function in
