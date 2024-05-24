ec6def3b0eaf587509d243e64c873b3e71e4b8f7
Rida Hamadani <106540880+Rida-Hamadani@users.noreply.github.com>
2024-05-24 22:12:45 +0300

    use tauto

--- Mathlib/Combinatorics/SimpleGraph/Connectivity.lean
+++ Mathlib/Combinatorics/SimpleGraph/Connectivity.lean
@@ -418,7 +418,7 @@ theorem eq_of_length_eq_zero {u v : V} : ∀ {p : G.Walk u v}, p.length = 0 → 
 #align simple_graph.walk.eq_of_length_eq_zero SimpleGraph.Walk.eq_of_length_eq_zero
 
 theorem adj_of_length_eq_one {u v : V} : ∀ {p : G.Walk u v}, p.length = 1 → G.Adj u v
-  | nil => by simp
+  | nil => by tauto
   | cons h nil => fun _ => h
   | cons h _ => fun l => (eq_of_length_eq_zero <| add_left_eq_self.mp <| length_cons _ _ ▸ l) ▸ h
 
