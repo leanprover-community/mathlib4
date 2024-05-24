04baf616875e1d86cb45f57e7c3bf76bfbb0867d
Martin Dvořák <martin.dvorak@matfyz.cz>
2024-05-24 18:38:34 +0200

    Apply suggestions from code review
    Co-authored-by: Rida Hamadani <106540880+Rida-Hamadani@users.noreply.github.com>

--- Mathlib/Data/Sum/LE.lean
+++ Mathlib/Data/Sum/LE.lean
@@ -16,27 +16,13 @@ namespace Sum
 variable {α₁ α₂ β : Type*} [LE β]
 
 lemma elim_le_elim_iff (u₁ v₁ : α₁ → β) (u₂ v₂ : α₂ → β) :
-    Sum.elim u₁ u₂ ≤ Sum.elim v₁ v₂ ↔ u₁ ≤ v₁ ∧ u₂ ≤ v₂ := by
-  constructor <;> intro hyp
-  · constructor
-    · intro i₁
-      have hi₁ := hyp (Sum.inl i₁)
-      rwa [Sum.elim_inl, Sum.elim_inl] at hi₁
-    · intro i₂
-      have hi₂ := hyp (Sum.inr i₂)
-      rwa [Sum.elim_inr, Sum.elim_inr] at hi₂
-  · intro j
-    cases j with
-    | inl j₁ =>
-      rw [Sum.elim_inl, Sum.elim_inl]
-      exact hyp.left j₁
-    | inr j₂ =>
-      rw [Sum.elim_inr, Sum.elim_inr]
-      exact hyp.right j₂
+    Sum.elim u₁ u₂ ≤ Sum.elim v₁ v₂ ↔ u₁ ≤ v₁ ∧ u₂ ≤ v₂ :=
+  ⟨fun h => ⟨fun _ => h <| inl _, fun _ => h <| inr _⟩,
+  fun h j => match j with | inl _ => h.1 _ | inr _ => h.2 _⟩
 
 lemma const_le_elim_iff (b : β) (v₁ : α₁ → β) (v₂ : α₂ → β) :
     (Function.const _ b) ≤ Sum.elim v₁ v₂ ↔ (Function.const _ b) ≤ v₁ ∧ (Function.const _ b) ≤ v₂ :=
-  by rw [← elim_const_const, elim_le_elim_iff]
+  elim_const_const _ ▸ elim_le_elim_iff _ _ _ _
 
 lemma zero_le_elim_iff [Zero β] (v₁ : α₁ → β) (v₂ : α₂ → β) :
     0 ≤ Sum.elim v₁ v₂ ↔ 0 ≤ v₁ ∧ 0 ≤ v₂ :=
@@ -48,7 +34,7 @@ lemma one_le_elim_iff [One β] (v₁ : α₁ → β) (v₂ : α₂ → β) :
 
 lemma elim_le_const_iff (b : β) (u₁ : α₁ → β) (u₂ : α₂ → β) :
     Sum.elim u₁ u₂ ≤ (Function.const _ b) ↔ u₁ ≤ (Function.const _ b) ∧ u₂ ≤ (Function.const _ b) :=
-  by rw [← elim_const_const, elim_le_elim_iff]
+  elim_const_const _ ▸ elim_le_elim_iff _ _ _ _
 
 lemma elim_le_zero_iff [Zero β] (u₁ : α₁ → β) (u₂ : α₂ → β) :
     Sum.elim u₁ u₂ ≤ 0 ↔ u₁ ≤ 0 ∧ u₂ ≤ 0 :=
