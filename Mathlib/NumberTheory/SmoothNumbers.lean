3e06d96d7036f4d7ec374d8ab6d5d19937a648db
Ralf Stephan <ralf@ark.in-berlin.de>
2024-05-24 16:02:13 +0200

    feat: mem_smoothNumbers_iff_primeFactors_subset

--- Mathlib/NumberTheory/SmoothNumbers.lean
+++ Mathlib/NumberTheory/SmoothNumbers.lean
@@ -281,6 +281,12 @@ lemma mem_smoothNumbers_of_primeFactors_subset {m n : ℕ} (hm : m ≠ 0)
     (hp : m.primeFactors ⊆ n.primesBelow) : m ∈ n.smoothNumbers :=
   ⟨hm, fun _ h => lt_of_mem_primesBelow <| hp <| mem_primeFactors_iff_mem_factors.mpr h⟩
 
+/-- `m` is an `n`-smooth number iff all of its prime factors are contained in the set of
+  primes below `n` -/
+lemma mem_smoothNumbers_iff_primeFactors_subset {m n : ℕ} (hm : m ≠ 0)
+    : m.primeFactors ⊆ n.primesBelow ↔ m ∈ n.smoothNumbers :=
+  Iff.intro (mem_smoothNumbers_of_primeFactors_subset hm) primeFactors_subset_of_mem_smoothNumbers
+
 /-- Zero is never a smooth number -/
 lemma ne_zero_of_mem_smoothNumbers {n m : ℕ} (h : m ∈ smoothNumbers n) : m ≠ 0 := h.1
 
