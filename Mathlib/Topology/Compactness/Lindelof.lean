64c056bc01654c6a149bfd69f608d5d72ea5b8fe
Steven Clontz <steven.clontz@gmail.com>
2024-05-24 17:01:45 +0000

    factor out SecondCountableTopology.ofPseudoMetrizableSpaceLindelofSpace

--- Mathlib/Topology/Compactness/Lindelof.lean
+++ Mathlib/Topology/Compactness/Lindelof.lean
@@ -6,7 +6,6 @@ Authors: Josha Dekker
 import Mathlib.Topology.Bases
 import Mathlib.Order.Filter.CountableInter
 import Mathlib.Topology.Compactness.SigmaCompact
-import Mathlib.Topology.Metrizable.Basic
 
 /-!
 # Lindelöf sets and Lindelöf spaces
@@ -708,25 +707,6 @@ instance (priority := 100) SecondCountableTopology.toHereditarilyLindelof
     rcases this with ⟨t, ⟨htc, htu⟩⟩
     use t, htc
     exact subset_of_subset_of_eq hcover (id htu.symm)
-
-instance SecondCountableTopology.ofPseudoMetrizableSpaceLindelofSpace [PseudoMetrizableSpace X]
-    [LindelofSpace X] : SecondCountableTopology X := by
-  letI : PseudoMetricSpace X := TopologicalSpace.pseudoMetrizableSpacePseudoMetric X
-  have h_dense : ∀ ε > 0, ∃ s : Set X, s.Countable ∧ ∀ x, ∃ y ∈ s, dist x y ≤ ε := by
-    intro ε hpos
-    let U := fun (z : X) ↦ Metric.ball z ε
-    have hU : ∀ z, U z ∈ 𝓝 z := by
-      intro z
-      have : IsOpen (U z) := Metric.isOpen_ball
-      refine IsOpen.mem_nhds this ?hx
-      simp only [U, Metric.mem_ball, dist_self, hpos]
-    have ⟨t, hct, huniv⟩ := LindelofSpace.elim_nhds_subcover U hU
-    refine ⟨t, hct, ?_⟩
-    intro z
-    have ⟨y, ht, hzy⟩ : ∃ y ∈ t, z ∈ U y := exists_set_mem_of_union_eq_top t (fun i ↦ U i) huniv z
-    simp only [Metric.mem_ball, U] at hzy
-    exact ⟨y, ht, hzy.le⟩
-  exact Metric.secondCountable_of_almost_dense_set h_dense
 
 lemma eq_open_union_countable [HereditarilyLindelofSpace X] {ι : Type u} (U : ι → Set X)
     (h : ∀ i, IsOpen (U i)) : ∃ t : Set ι, t.Countable ∧ ⋃ i∈t, U i = ⋃ i, U i := by
