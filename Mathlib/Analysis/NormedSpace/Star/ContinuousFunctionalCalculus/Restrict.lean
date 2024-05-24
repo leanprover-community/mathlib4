00b2e19590442ef735cb24cab857489caeb4570d
ADedecker <anatolededecker@gmail.com>
2024-05-24 15:27:06 +0200

    Fix

--- Mathlib/Analysis/NormedSpace/Star/ContinuousFunctionalCalculus/Restrict.lean
+++ Mathlib/Analysis/NormedSpace/Star/ContinuousFunctionalCalculus/Restrict.lean
@@ -80,7 +80,7 @@ lemma closedEmbedding_starAlgHom {a : A} {φ : C(spectrum S a, S) →⋆ₐ[S] A
   have := h.compactSpace
   hφ.comp <| UniformEmbedding.toClosedEmbedding <| .comp
     (ContinuousMap.uniformEmbedding_comp _ halg)
-    (UniformEquiv.continuousMapCongr h.homeomorph.symm (.refl _) |>.uniformEmbedding)
+    (UniformEquiv.arrowCongr h.homeomorph.symm (.refl _) |>.uniformEmbedding)
 
 lemma starAlgHom_id {a : A} {φ : C(spectrum S a, S) →⋆ₐ[S] A} {f : C(S, R)}
     (h : SpectrumRestricts a f) (h_id : φ (.restrict (spectrum S a) <| .id S) = a) :
