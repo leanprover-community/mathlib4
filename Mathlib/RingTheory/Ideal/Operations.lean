9315c5b014b1eef0b95e7af2bb592b64e421241f
Brendan Seamas Murphy <shamrockfrost@gmail.com>
2024-05-24 03:47:08 +0000

    Feat(RingTheory/Ideal/Operations): Misc lemmas about smul actions on submodules (#13121)
    Misc lemmas about smul actions on submodules.

--- Mathlib/RingTheory/Ideal/Operations.lean
+++ Mathlib/RingTheory/Ideal/Operations.lean
@@ -307,6 +307,7 @@ theorem mem_of_span_eq_top_of_smul_pow_mem (M' : Submodule R M) (s : Set R) (hs 
 
 variable {M' : Type w} [AddCommMonoid M'] [Module R M']
 
+@[simp]
 theorem map_smul'' (f : M →ₗ[R] M') : (I • N).map f = I • N.map f :=
   le_antisymm
       (map_le_iff_le_comap.2 <|
@@ -317,6 +318,12 @@ theorem map_smul'' (f : M →ₗ[R] M') : (I • N).map f = I • N.map f :=
       let ⟨p, hp, hfp⟩ := mem_map.1 hn
       hfp ▸ f.map_smul r p ▸ mem_map_of_mem (smul_mem_smul hr hp)
 #align submodule.map_smul'' Submodule.map_smul''
+
+open Pointwise in
+@[simp]
+theorem map_pointwise_smul (r : R) (N : Submodule R M) (f : M →ₗ[R] M') :
+    (r • N).map f = r • N.map f :=
+  by simp_rw [← ideal_span_singleton_smul, map_smul'']
 
 variable {I}
 
@@ -1401,5 +1408,11 @@ lemma span_smul_eq
     (s : Set R) (N : Submodule R M) :
     Ideal.span s • N = s • N := by
   rw [← coe_set_smul, coe_span_smul]
+
+@[simp]
+theorem set_smul_top_eq_span (s : Set R) :
+    s • ⊤ = Ideal.span s :=
+  Eq.trans (span_smul_eq s ⊤).symm <|
+    Eq.trans (smul_eq_mul (Ideal R)) (Ideal.mul_top (.span s))
 
 end Submodule
