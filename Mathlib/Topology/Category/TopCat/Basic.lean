374d63a62ee7571c1309cb0cc24a74ef9e4d234e
adomani <adomani@gmail.com>
2024-05-25 21:40:36 +0200

    lint

--- Mathlib/Topology/Category/TopCat/Basic.lean
+++ Mathlib/Topology/Category/TopCat/Basic.lean
@@ -57,7 +57,7 @@ set_option linter.uppercaseLean3 false in
 #align Top.topological_space_unbundled TopCat.topologicalSpaceUnbundled
 
 -- Porting note: cannot find a coercion to function otherwise
-attribute [instance] ConcreteCategory.instFunLike in
+attribute [instance] ConcreteCategory.instFunLike
 instance (X Y : TopCat.{u}) : CoeFun (X ⟶ Y) fun _ => X → Y where
   coe f := f
 
