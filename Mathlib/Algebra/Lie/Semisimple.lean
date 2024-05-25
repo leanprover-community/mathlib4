8d88c058fa24d7a4d7fc827f88ebe316f41bc816
Johan Commelin <johan@commelin.net>
2024-05-25 20:01:13 +0200

    indent

--- Mathlib/Algebra/Lie/Semisimple.lean
+++ Mathlib/Algebra/Lie/Semisimple.lean
@@ -370,68 +370,68 @@ class IsSemisimple : Prop where
 variable {R L} in
 lemma isSimple_of_isAtom [IsSemisimple R L] (I : LieIdeal R L) (hI : IsAtom I) : IsSimple R I where
   non_abelian := IsSemisimple.non_abelian_of_isAtom I hI
-    eq_bot_or_eq_top := by
-      -- Suppose that `J` is an ideal of `I`.
-      intro J
-      -- We first show that `J` is also an ideal of the ambient Lie algebra `L`.
-      let J' : LieIdeal R L :=
-      { __ := J.toSubmodule.map I.incl.toLinearMap
-        lie_mem := by
-          rintro x _ ⟨y, hy, rfl⟩
-          dsimp
-          -- We need to show that `⁅x, y⁆ ∈ J` for any `x ∈ L` and `y ∈ J`.
-          -- Since `L` is semisimple, `x` is contained
-          -- in the supremum of `I` and the atoms not equal to `I`.
-          have hx : x ∈ I ⊔ sSup ({I' : LieIdeal R L | IsAtom I'} \ {I}) := by
-            nth_rewrite 1 [← sSup_singleton (a := I)]
-            rw [← sSup_union, Set.union_diff_self, Set.union_eq_self_of_subset_left,
-              IsSemisimple.sSup_isAtom_eq_top]
-            · apply LieSubmodule.mem_top
-            · simp only [Set.singleton_subset_iff, Set.mem_setOf_eq, hI]
-          -- Hence we can write `x` as `a + b` with `a ∈ I`
-          -- and `b` in the supremum of the atoms not equal to `I`.
-          rw [LieSubmodule.mem_sup] at hx
-          obtain ⟨a, ha, b, hb, rfl⟩ := hx
-          -- Therefore it suffices to show that `⁅a, y⁆ ∈ J` and `⁅b, y⁆ ∈ J`.
-          simp only [add_lie, AddSubsemigroup.mem_carrier, AddSubmonoid.mem_toSubsemigroup,
-            Submodule.mem_toAddSubmonoid]
-          apply add_mem
-          -- Now `⁅a, y⁆ ∈ J` since `a ∈ I`, `y ∈ J`, and `J` is an ideal of `I`.
-          · simp only [Submodule.mem_map, LieSubmodule.mem_coeSubmodule, Submodule.coeSubtype,
-              Subtype.exists, exists_and_right, exists_eq_right, ha, lie_mem_left, exists_true_left]
-            exact lie_mem_right R I J ⟨a, ha⟩ y hy
-          -- Finally `⁅b, y⁆ = 0`, by the independence of the atoms.
-          · suffices ⁅b, y.val⁆ = 0 by simp only [this, zero_mem]
-            rw [← LieSubmodule.mem_bot (R := R) (L := L),
-                ← (IsSemisimple.setIndependent_isAtom hI).eq_bot]
-            exact ⟨lie_mem_right R L I b y y.2, lie_mem_left _ _ _ _ _ hb⟩ }
-      -- Now that we know that `J` is an ideal of `L`,
-      -- we start with the proof that `I` is a simple Lie algebra.
-      -- Assume that `J ≠ ⊤`.
-      rw [or_iff_not_imp_right]
-      intro hJ
-      suffices J' = ⊥ by
-        rw [eq_bot_iff] at this ⊢
-        intro x hx
-        suffices x ∈ J → x = 0 from this hx
-        simpa [J'] using @this x.1
-      -- We need to show that `J = ⊥`.
-      -- Since `J` is an ideal of `L`, and `I` is an atom,
-      -- it suffices to show that `J < I`.
-      apply hI.2
-      rw [lt_iff_le_and_ne]
-      constructor
-      -- We know that `J ≤ I` since `J` is an ideal of `I`.
-      · rintro _ ⟨x, -, rfl⟩
-        exact x.2
-      -- So we need to show `J ≠ I` as ideals of `L`.
-      -- This follows from our assumption that `J ≠ ⊤` as ideals of `I`.
-      contrapose! hJ
-      rw [eq_top_iff]
-      rintro ⟨x, hx⟩ -
-      rw [← hJ] at hx
-      rcases hx with ⟨y, hy, rfl⟩
-      exact hy
+  eq_bot_or_eq_top := by
+    -- Suppose that `J` is an ideal of `I`.
+    intro J
+    -- We first show that `J` is also an ideal of the ambient Lie algebra `L`.
+    let J' : LieIdeal R L :=
+    { __ := J.toSubmodule.map I.incl.toLinearMap
+      lie_mem := by
+        rintro x _ ⟨y, hy, rfl⟩
+        dsimp
+        -- We need to show that `⁅x, y⁆ ∈ J` for any `x ∈ L` and `y ∈ J`.
+        -- Since `L` is semisimple, `x` is contained
+        -- in the supremum of `I` and the atoms not equal to `I`.
+        have hx : x ∈ I ⊔ sSup ({I' : LieIdeal R L | IsAtom I'} \ {I}) := by
+          nth_rewrite 1 [← sSup_singleton (a := I)]
+          rw [← sSup_union, Set.union_diff_self, Set.union_eq_self_of_subset_left,
+            IsSemisimple.sSup_isAtom_eq_top]
+          · apply LieSubmodule.mem_top
+          · simp only [Set.singleton_subset_iff, Set.mem_setOf_eq, hI]
+        -- Hence we can write `x` as `a + b` with `a ∈ I`
+        -- and `b` in the supremum of the atoms not equal to `I`.
+        rw [LieSubmodule.mem_sup] at hx
+        obtain ⟨a, ha, b, hb, rfl⟩ := hx
+        -- Therefore it suffices to show that `⁅a, y⁆ ∈ J` and `⁅b, y⁆ ∈ J`.
+        simp only [add_lie, AddSubsemigroup.mem_carrier, AddSubmonoid.mem_toSubsemigroup,
+          Submodule.mem_toAddSubmonoid]
+        apply add_mem
+        -- Now `⁅a, y⁆ ∈ J` since `a ∈ I`, `y ∈ J`, and `J` is an ideal of `I`.
+        · simp only [Submodule.mem_map, LieSubmodule.mem_coeSubmodule, Submodule.coeSubtype,
+            Subtype.exists, exists_and_right, exists_eq_right, ha, lie_mem_left, exists_true_left]
+          exact lie_mem_right R I J ⟨a, ha⟩ y hy
+        -- Finally `⁅b, y⁆ = 0`, by the independence of the atoms.
+        · suffices ⁅b, y.val⁆ = 0 by simp only [this, zero_mem]
+          rw [← LieSubmodule.mem_bot (R := R) (L := L),
+              ← (IsSemisimple.setIndependent_isAtom hI).eq_bot]
+          exact ⟨lie_mem_right R L I b y y.2, lie_mem_left _ _ _ _ _ hb⟩ }
+    -- Now that we know that `J` is an ideal of `L`,
+    -- we start with the proof that `I` is a simple Lie algebra.
+    -- Assume that `J ≠ ⊤`.
+    rw [or_iff_not_imp_right]
+    intro hJ
+    suffices J' = ⊥ by
+      rw [eq_bot_iff] at this ⊢
+      intro x hx
+      suffices x ∈ J → x = 0 from this hx
+      simpa [J'] using @this x.1
+    -- We need to show that `J = ⊥`.
+    -- Since `J` is an ideal of `L`, and `I` is an atom,
+    -- it suffices to show that `J < I`.
+    apply hI.2
+    rw [lt_iff_le_and_ne]
+    constructor
+    -- We know that `J ≤ I` since `J` is an ideal of `I`.
+    · rintro _ ⟨x, -, rfl⟩
+      exact x.2
+    -- So we need to show `J ≠ I` as ideals of `L`.
+    -- This follows from our assumption that `J ≠ ⊤` as ideals of `I`.
+    contrapose! hJ
+    rw [eq_top_iff]
+    rintro ⟨x, hx⟩ -
+    rw [← hJ] at hx
+    rcases hx with ⟨y, hy, rfl⟩
+    exact hy
 
 lemma IsSemisimple.semisimpleGenerators_atoms [IsSemisimple R L] :
     SemisimpleGenerators {I : LieIdeal R L | IsAtom I} := by
