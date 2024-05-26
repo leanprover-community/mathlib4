4e5526752e4d503da13046b40c016f775bf59069
adomani <adomani@gmail.com>
2024-05-26 17:07:50 +0200

    syntax

--- Mathlib/Tactic/Linter/GenericSyntaxLinter.lean
+++ Mathlib/Tactic/Linter/GenericSyntaxLinter.lean
@@ -64,6 +64,79 @@ def is_attribute_instance_in : Syntax → Array Syntax
   | stx@`(command|attribute [instance $_priority] $_decl:ident in $_) => #[stx]
   | _ => #[]
 
+
+/-
+inspect: 'set_option linter.unusedVariables false in
+example {n : Nat} : n = n :=
+  rfl'
+
+node Lean.Parser.Command.in, none
+|-node Lean.Parser.Command.set_option, none
+|   |-atom original: ⟨⟩⟨ ⟩-- 'set_option'
+|   |-ident original: ⟨⟩⟨ ⟩-- (linter.unusedVariables,linter.unusedVariables)
+|   |-node null, none
+|   |-atom original: ⟨⟩⟨ ⟩-- 'false'
+|-atom original: ⟨⟩⟨\n⟩-- 'in'
+|-node Lean.Parser.Command.declaration, none
+|   |-node Lean.Parser.Command.declModifiers, none
+|   |   |-node null, none
+|   |   |-node null, none
+|   |   |-node null, none
+|   |   |-node null, none
+|   |   |-node null, none
+|   |   |-node null, none
+|   |-node Lean.Parser.Command.example, none
+|   |   |-atom original: ⟨⟩⟨ ⟩-- 'example'
+|   |   |-node Lean.Parser.Command.optDeclSig, none
+|   |   |   |-node null, none
+|   |   |   |   |-node Lean.Parser.Term.implicitBinder, none
+|   |   |   |   |   |-atom original: ⟨⟩⟨⟩-- '{'
+|   |   |   |   |   |-node null, none
+|   |   |   |   |   |   |-ident original: ⟨⟩⟨ ⟩-- (n,n)
+|   |   |   |   |   |-node null, none
+|   |   |   |   |   |   |-atom original: ⟨⟩⟨ ⟩-- ':'
+|   |   |   |   |   |   |-ident original: ⟨⟩⟨⟩-- (Nat,Nat)
+|   |   |   |   |   |-atom original: ⟨⟩⟨ ⟩-- '}'
+|   |   |   |-node null, none
+|   |   |   |   |-node Lean.Parser.Term.typeSpec, none
+|   |   |   |   |   |-atom original: ⟨⟩⟨ ⟩-- ':'
+|   |   |   |   |   |-node «term_=_», none
+|   |   |   |   |   |   |-ident original: ⟨⟩⟨ ⟩-- (n,n)
+|   |   |   |   |   |   |-atom original: ⟨⟩⟨ ⟩-- '='
+|   |   |   |   |   |   |-ident original: ⟨⟩⟨ ⟩-- (n,n)
+|   |   |-node Lean.Parser.Command.declValSimple, none
+|   |   |   |-atom original: ⟨⟩⟨ ⟩-- ':='
+|   |   |   |-ident original: ⟨⟩⟨\n\n⟩-- (rfl,rfl)
+|   |   |   |-node Lean.Parser.Termination.suffix, none
+|   |   |   |   |-node null, none
+|   |   |   |   |-node null, none
+|   |   |   |-node null, none
+-/
+open Command in
+private partial def withSetOptionIn' (cmd : CommandElab) : CommandElab := fun stx => do
+  if stx.getKind == ``Lean.Parser.Command.in then
+    if stx[0].getKind == ``Lean.Parser.Command.set_option then
+      let opts ← Elab.elabSetOption stx[0][1] stx[0][3]
+      withScope (fun scope => { scope with opts }) do
+        withSetOptionIn' cmd stx[2]
+    else
+      withSetOptionIn' cmd stx[2]
+  else
+    cmd stx
+
+
+
+def is_soi : Syntax → Option Syntax
+  | s@(.node _ ``Lean.Parser.Command.in _
+  --#[
+  --    .node _ ``Lean.Parser.Command.set_option _,
+  --    _,
+  --    cmd
+  --  ]
+    ) => dbg_trace s.getKind; some s
+  | s => dbg_trace s.getKind; none
+
+
 end generic
 
 end Mathlib.Linter
@@ -74,7 +147,8 @@ namespace Mathlib.Linter.generic
 def getLinterHash (o : Options) : Bool := Linter.getLinterValue linter.generic o
 
 /-- The main implementation of the generic syntax linter. -/
-def genericSyntaxLinter (contains? : Syntax → Array (Syntax × MessageData)) : Linter where
+def genericSyntaxLinter (contains? : Syntax → Array (Syntax × MessageData))
+    (toElab : Syntax → Option Syntax := fun _ => none) : Linter where
   run := withSetOptionIn fun stx => do
     unless getLinterHash (← getOptions) do
       return
@@ -82,6 +156,12 @@ def genericSyntaxLinter (contains? : Syntax → Array (Syntax × MessageData)) :
       return
     let _ ← (contains? stx).mapM fun (s, msg) =>
       Linter.logLint linter.generic s msg
+    dbg_trace "{is_soi stx}"
+    if let some stx := toElab stx then
+      Command.elabCommand stx
+      if (← MonadState.get).messages.isEmpty then logInfoAt stx  "no messages"
+
+initialize addLinter (genericSyntaxLinter (fun _ => #[]) is_soi)
 
 initialize addLinter (genericSyntaxLinter fun stx =>
   (unwanted.cDot stx).map (·, "cdots should use `·`"))
