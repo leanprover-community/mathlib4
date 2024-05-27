222160ae9058c8d16eab07c395f4617792f483dd
Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>
2024-05-27 15:35:50 +0200

    Update Mathlib/Probability/IdentDistrib.lean
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Mathlib/Probability/IdentDistrib.lean
+++ Mathlib/Probability/IdentDistrib.lean
@@ -377,9 +377,9 @@ lemma indepFun_of_identDistrib_pair
     (indepFun_iff_map_prod_eq_prod_map_map _ _).1 h_indep]
   · exact congr (congrArg Measure.prod <| (h_ident.comp measurable_fst).map_eq)
       (h_ident.comp measurable_snd).map_eq
-  . exact measurable_fst.aemeasurable.comp_aemeasurable h_ident.aemeasurable_fst
-  . exact measurable_snd.aemeasurable.comp_aemeasurable h_ident.aemeasurable_fst
-  . exact measurable_fst.aemeasurable.comp_aemeasurable h_ident.aemeasurable_snd
-  . exact measurable_snd.aemeasurable.comp_aemeasurable h_ident.aemeasurable_snd
+  · exact measurable_fst.aemeasurable.comp_aemeasurable h_ident.aemeasurable_fst
+  · exact measurable_snd.aemeasurable.comp_aemeasurable h_ident.aemeasurable_fst
+  · exact measurable_fst.aemeasurable.comp_aemeasurable h_ident.aemeasurable_snd
+  · exact measurable_snd.aemeasurable.comp_aemeasurable h_ident.aemeasurable_snd
 
 end ProbabilityTheory
