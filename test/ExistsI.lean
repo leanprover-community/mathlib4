4b137ad3e2ef084a65842b54e21812170217c04b
Komyyy <pol_tta@outlook.jp>
2024-05-27 23:40:35 +0900

    rename wrongly capitalized filenames

--- test/ExistsI.lean
+++ test/ExistsI.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Moritz Doll
 -/
 
-import Mathlib.Tactic.Existsi
+import Mathlib.Tactic.ExistsI
 
 example : ∃ x : Nat, x = x := by
   existsi 42
