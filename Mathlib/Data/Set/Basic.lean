4e5381fcca3aab13b24106dc5b7d3b6c75129bae
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2024-05-27 11:19:23 +0200

    try to debug olean issue

--- Mathlib/Data/Set/Basic.lean
+++ Mathlib/Data/Set/Basic.lean
@@ -2642,3 +2642,5 @@ end Disjoint
 @[simp] theorem Prop.compl_singleton (p : Prop) : ({p}ᶜ : Set Prop) = {¬p} :=
   ext fun q ↦ by simpa [@Iff.comm q] using not_iff
 #align Prop.compl_singleton Prop.compl_singleton
+
+#eval 0
