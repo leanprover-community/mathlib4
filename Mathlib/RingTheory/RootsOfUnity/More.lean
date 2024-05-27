92f4ad8b9f8ee602f38f6d827cb5ac14c74070e2
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2024-05-27 22:08:23 +0200

    replace n by n+1, golf

--- Mathlib/RingTheory/RootsOfUnity/More.lean
+++ Mathlib/RingTheory/RootsOfUnity/More.lean
@@ -32,35 +32,30 @@ namespace IsPrimitiveRoot
 
 open Finset Polynomial BigOperators
 
-/-- If `μ` is a primitive `n`th root of unity in `R`, then `∏(1≤k<n) (1-μ^k) = n`. -/
-lemma prod_one_sub_pow_eq_order {n : ℕ} (hn : n ≠ 0) {μ : R} (hμ : IsPrimitiveRoot μ n) :
-    ∏ k ∈ range (n - 1), (1 - μ ^ (k + 1)) = n := by
-  have := X_pow_sub_C_eq_prod hμ (Nat.pos_of_ne_zero hn) (one_pow n)
-  have HH : (X ^ n - C 1 : R[X]) = (X - C 1) * (range n).sum (fun k ↦ X ^ k) :=
-    (mul_geom_sum X n).symm
-  rw [HH] at this; clear HH
-  let m + 1 := n
-  rw [prod_range_succ', pow_zero, mul_one, mul_comm, eq_comm] at this
+/-- If `μ` is a primitive `n`th root of unity in `R`, then `∏(1≤k<n) (1-μ^k) = n`.
+(Stated with `n+1` in place of `n` to avoid the condition `n ≠ 0`.) -/
+lemma prod_one_sub_pow_eq_order {n : ℕ} {μ : R} (hμ : IsPrimitiveRoot μ (n + 1)) :
+    ∏ k ∈ range n, (1 - μ ^ (k + 1)) = n + 1 := by
+  have := X_pow_sub_C_eq_prod hμ n.zero_lt_succ (one_pow (n + 1))
+  rw [C_1, ← mul_geom_sum, prod_range_succ', pow_zero, mul_one, mul_comm, eq_comm] at this
   replace this := mul_right_cancel₀ (Polynomial.X_sub_C_ne_zero 1) this
   apply_fun Polynomial.eval 1 at this
-  simpa only [Nat.cast_add, Nat.cast_one, mul_one, map_pow, eval_prod, eval_sub, eval_X, eval_pow,
-    eval_C, eval_geom_sum, one_pow, sum_const, card_range, nsmul_eq_mul] using this
+  simpa only [mul_one, map_pow, eval_prod, eval_sub, eval_X, eval_pow, eval_C, eval_geom_sum,
+    one_pow, sum_const, card_range, nsmul_eq_mul, Nat.cast_add, Nat.cast_one] using this
 
-/-- If `μ` is a primitive `n`th root of unity in `R`, then `(-1)^(n-1) * ∏(1≤k<n) (μ^k-1) = n`. -/
-lemma prod_pow_sub_one_eq_order {n : ℕ} (hn : n ≠ 0) {μ : R} (hμ : IsPrimitiveRoot μ n) :
-    (-1) ^ (n - 1) * ∏ k ∈ range (n - 1), (μ ^ (k + 1) - 1) = n := by
-  have : (-1 : R) ^ (n - 1) = ∏ k ∈ range (n - 1), -1 := by rw [prod_const, card_range]
-  simp only [this, ← prod_mul_distrib, neg_one_mul, neg_sub, ← prod_one_sub_pow_eq_order hn hμ]
+/-- If `μ` is a primitive `n`th root of unity in `R`, then `(-1)^(n-1) * ∏(1≤k<n) (μ^k-1) = n`.
+(Stated with `n+1` in place of `n` to avoid the condition `n ≠ 0`.) -/
+lemma prod_pow_sub_one_eq_order {n : ℕ} {μ : R} (hμ : IsPrimitiveRoot μ (n + 1)) :
+    (-1) ^ n * ∏ k ∈ range n, (μ ^ (k + 1) - 1) = n + 1 := by
+  have : (-1 : R) ^ n = ∏ k ∈ range n, -1 := by rw [prod_const, card_range]
+  simp only [this, ← prod_mul_distrib, neg_one_mul, neg_sub, ← prod_one_sub_pow_eq_order hμ]
 
 /-- If `μ` is a primitive `n`th root of unity in `R` and `k < n`, then `n` is divisible
 by `(μ-1)^k` in `ℤ[μ] ⊆ R`. -/
 lemma order_eq_mul_self_sub_one_pow {k n : ℕ} (hn : k < n) {μ : R} (hμ : IsPrimitiveRoot μ n) :
     ∃ z ∈ Algebra.adjoin ℤ {μ}, n = z * (μ - 1) ^ k := by
-  have := hμ.prod_pow_sub_one_eq_order (k.zero_le.trans_lt hn).ne'
-  obtain ⟨m, rfl⟩ : ∃ m : ℕ, n = m + k + 1 := by
-    simp only [add_assoc]
-    exact Nat.exists_eq_add_of_le' hn
-  simp only [add_tsub_cancel_right, Nat.cast_add, Nat.cast_one] at this
+  let n' + 1 := n
+  obtain ⟨m, rfl⟩ := Nat.exists_eq_add_of_le' (Nat.le_of_lt_succ hn)
   let Z k := Classical.choose <| Algebra.adjoin.sub_one_dvd_pow_sub_one ℤ μ k
   have Zdef k : Z k ∈ Algebra.adjoin ℤ {μ} ∧ μ ^ k - 1 = Z k * (μ - 1) :=
     Classical.choose_spec <| Algebra.adjoin.sub_one_dvd_pow_sub_one ℤ μ k
@@ -69,15 +64,16 @@ lemma order_eq_mul_self_sub_one_pow {k n : ℕ} (hn : k < n) {μ : R} (hμ : IsP
   · apply Subalgebra.mul_mem
     · apply Subalgebra.mul_mem
       · exact Subalgebra.pow_mem _ (Subalgebra.neg_mem _ <| Subalgebra.one_mem _) _
-      · exact Subalgebra.prod_mem _ fun j _ ↦ (Zdef _).1
-    · refine Subalgebra.prod_mem _ fun j _ ↦ ?_
+      · exact Subalgebra.prod_mem _ fun _ _ ↦ (Zdef _).1
+    · refine Subalgebra.prod_mem _ fun _ _ ↦ ?_
       apply Subalgebra.sub_mem
       · exact Subalgebra.pow_mem _ (Algebra.self_mem_adjoin_singleton ℤ μ) _
       · exact Subalgebra.one_mem _
   · push_cast
+    have := Nat.cast_add (R := R) m k ▸ hμ.prod_pow_sub_one_eq_order
     rw [← this, mul_assoc, mul_assoc]
     congr 1
     conv => enter [2, 2, 2]; rw [← card_range k]
-    rw [← prod_range_mul_prod_Ico (fun j : ℕ ↦ μ ^ (j + 1) - 1) (Nat.le_add_left k m),
-      mul_comm _ (_ ^ card _), ← mul_assoc, prod_mul_pow_card]
+    rw [← prod_range_mul_prod_Ico _ (Nat.le_add_left k m), mul_comm _ (_ ^ card _), ← mul_assoc,
+      prod_mul_pow_card]
     conv => enter [2, 1, 2, j]; rw [← (Zdef _).2]
