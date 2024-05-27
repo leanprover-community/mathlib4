93525e4675664113e459be92c8db23cf6e50d80f
Yaël Dillies <yael.dillies@gmail.com>
2024-05-27 12:42:23 +0000

    chore: Split `Algebra.Function.Support`/`Algebra.Function.Indicator` (#13244)
    ... in a file about `Group` and another one about `GroupWithZero`. Credit Yury for https://github.com/leanprover-community/mathlib/pull/2340 and Zhouhang for https://github.com/leanprover-community/mathlib/pull/1949.

--- Mathlib/Algebra/Group/FiniteSupport.lean
+++ Mathlib/Algebra/Group/FiniteSupport.lean
@@ -3,7 +3,7 @@ Copyright (c) 2021 Oliver Nash. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Oliver Nash
 -/
-import Mathlib.Algebra.Function.Support
+import Mathlib.Algebra.Group.Support
 import Mathlib.Data.Set.Finite
 
 /-!
