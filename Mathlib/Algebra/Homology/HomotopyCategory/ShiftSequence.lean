d7332c63869083df14746a7365f42ff59aade307
Michael Rothgang <rothgami@math.hu-berlin.de>
2024-05-27 20:23:09 +0200

    Convert some refine -> apply: not sure if this is better.

--- Mathlib/Algebra/Homology/HomotopyCategory/ShiftSequence.lean
+++ Mathlib/Algebra/Homology/HomotopyCategory/ShiftSequence.lean
@@ -119,7 +119,7 @@ instance {K L : CochainComplex C ℤ} (φ : K ⟶ L) (n : ℤ) [QuasiIso φ] :
     QuasiIso (φ⟦n⟧') where
   quasiIsoAt a := by
     rw [quasiIsoAt_iff_isIso_homologyMap]
-    refine (NatIso.isIso_map_iff
+    apply (NatIso.isIso_map_iff
       ((homologyFunctor C (ComplexShape.up ℤ) 0).shiftIso n a (n + a) rfl) φ).2 ?_
     change IsIso (homologyMap φ _)
     infer_instance
