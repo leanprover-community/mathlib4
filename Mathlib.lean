f67b8a027132f0ca6db2fbfa7da11d5de79144b6
Patrick Massot <patrickmassot@free.fr>
2024-05-27 12:08:38 -0400

    chore: Get rid of BitVec

--- Mathlib.lean
+++ Mathlib.lean
@@ -1778,8 +1778,6 @@ import Mathlib.Data.Array.Defs
 import Mathlib.Data.Array.ExtractLemmas
 import Mathlib.Data.Array.Lemmas
 import Mathlib.Data.BinaryHeap
-import Mathlib.Data.BitVec.Defs
-import Mathlib.Data.BitVec.Lemmas
 import Mathlib.Data.Bool.AllAny
 import Mathlib.Data.Bool.Basic
 import Mathlib.Data.Bool.Count
