e236261147de222b7bcdb81f349ad3c34f1b304d
Eric Wieser <wieser.eric@gmail.com>
2024-05-27 23:13:05 +0000

    import

--- Mathlib.lean
+++ Mathlib.lean
@@ -3492,6 +3492,7 @@ import Mathlib.RingTheory.Ideal.MinimalPrime
 import Mathlib.RingTheory.Ideal.Norm
 import Mathlib.RingTheory.Ideal.Operations
 import Mathlib.RingTheory.Ideal.Over
+import Mathlib.RingTheory.Ideal.Pointwise
 import Mathlib.RingTheory.Ideal.Prod
 import Mathlib.RingTheory.Ideal.Quotient
 import Mathlib.RingTheory.Ideal.QuotientOperations
