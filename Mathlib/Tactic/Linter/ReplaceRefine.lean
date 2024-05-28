e194a22f887db0649c41a26ec324e05a4d4d1896
adomani <adomani@gmail.com>
2024-05-28 19:42:27 +0100

    cleanup

--- Mathlib/Tactic/Linter/ReplaceRefine.lean
+++ Mathlib/Tactic/Linter/ReplaceRefine.lean
@@ -61,16 +61,6 @@ Converting `refine'` to `refine`, these are the candidate nodes for the replacem
 def getHoles (stx : Syntax) : Array Syntax :=
   stx.findAll (·.isOfKind ``Lean.Parser.Term.hole)
 
-/-
-|-node Lean.Parser.Term.syntheticHole, none
-|   |-atom original: ⟨⟩⟨⟩-- '?'
-|   |-node Lean.Parser.Term.hole, none
-|   |   |-atom original: ⟨⟩⟨ ⟩-- '_'
-
-|-node Lean.Parser.Term.hole, none
-|   |-atom original: ⟨⟩⟨⟩-- '_'
--/
-
 /-- converts an "anonymous hole" `_` to a "synthetic hole" `?_` with comparable
 `SourceInfo`.
 Leaves unchanged inputs that are not "anonymous holes".
@@ -96,21 +86,10 @@ def candidateRefines (stx : Syntax) : Array Syntax := Id.run do
     for s in sub do
       newCmd ← newCmd.replaceM (fun t =>
         if t == s && t.getPos? == s.getPos? then some (holeToSyntHole s) else none)
-    --let ncmd ← stx.replaceM (fun s => if sub.contains s then )
     cands := cands.push newCmd
   return cands
 
---/-- extracts `refine'` from the given `Syntax`, returning also the `SourceInfo`, the arguments
---of the `refine'` node and whether `refine'` contains `..`. -/
---partial
---def refine'ToRefines : Syntax → Array Syntax
---  | stx@(.node si ``Lean.Parser.Tactic.refine' args) =>
---    dbg_trace "refine' found"
---    let rest := (args.map getRefine').flatten
---    rest.push (stx, si, args, stx.find? (·.isOfKind ``Lean.Parser.Term.optEllipsis))
---  | .node _ _ args => (args.map getRefine').flatten
---  | _ => default
-
+/-- converts each `refine'` with a `refine` in `stx`. -/
 def refine'ToRefine (stx : Syntax) : Syntax := Id.run do
   stx.replaceM (fun s => match s with
     | .node si ``Lean.Parser.Tactic.refine' args =>
@@ -118,119 +97,34 @@ def refine'ToRefine (stx : Syntax) : Syntax := Id.run do
       return some (.node si ``Lean.Parser.Tactic.refine args)
     | _ => return none)
 
-
-elab "ho " cmd:command : command => do
-  let noDashCmd := refine'ToRefine cmd
-  logInfo noDashCmd
-  let s ← get
-  --let holes := getHoles noDashCmd
-  --let sholes := holes.map (holeToSyntHole ·)
-  --logInfo m!"{holes}, {holes.map (·.getPos?)}, {sholes}, {sholes.map (·.getPos?)}"
-  let cands := candidateRefines noDashCmd
-  logInfo m!"{cands}"
-  let mut opts := #[]
-  for cand in cands do
-    Command.elabCommand cand
-    if !(← get).messages.hasErrors then opts := opts.push cand
-    set s
-  logInfo m!"{opts}"
-  logInfo m!"{opts.map fun s => let hs := getHoles s; hs.map (·.getPos?)}"
-  Command.elabCommand cmd
-
-/-- replaces each `refine'` by `refine` in succession in `cmd` and, each time, catches the errors
-of missing `?`, collecting their positions.  Eventually, it returns an array of pairs
+/-- replaces each `refine'` by `refine` in succession in `cmd`, trying to replace every subset of
+`?_` with `_`.
+Eventually, it returns an array of pairs
 `(1/0, position)`, where
 * `1` means that the `position` is the beginning of `refine'` and
-* `0` means that the `position` is a missing `?`.
+* `0` means that the `position` is a missing `?`,
+for each successful replacement.
 -/
-def getQuestions' (cmd : Syntax) :
+def getQuestions (cmd : Syntax) :
     Command.CommandElabM (List (Nat × String.Pos)) := do
   let exm ← toExample cmd
   let st ← get
-  --let origMsgsSize := s.messages.msgs.toArray.size
-  --let mut mms := #[]
-  --dbg_trace origMsgsSize
   let refine's := getRefine's cmd
-  --logInfo m!"{refine's}"
   let mut opts := #[]
   let mut refs := #[]
   for refine' in refine's do
-    --let mut current := #[]
     let refine := refine'ToRefine refine'
     let cands := candidateRefines refine
     for cand in cands do
       let repl ← exm.replaceM fun s => if s == refine' then return some cand else return none
       Command.elabCommand repl
-      --mms := mms.push (repl, ← (← get).messages.msgs.toArray.mapM (·.toString))
-      --dbg_trace (← get).messages.msgs.toArray.size
       if !(← get).messages.hasErrors then
         opts := opts.push cand
         refs := refs.push refine'.getPos?
-        --current := current.push (0, cand.getPos?)
       set st
-    --newOpts := newOpts.push ((1, refine'.getPos?), current)
-
-    --logInfo m!"{newOpts}"
---    logInfo m!"{refine'.getPos?}"
   let positions := opts.map fun s => let hs := getHoles s; hs.map (·.getPos?)
---  let newPositions := newOpts.map fun (n, s) =>
---    let hs := getHoles s
---    hs.map fun h : Syntax => (n, h.getPos?)
---  --logInfo m!"{mms}"
-  logInfo m!"{opts.zip positions}"
   return (refs.zipWith positions fun a b =>
     #[(1, a.getD default)] ++ (b.map (0, ·.getD default))).flatten.toList
-
-/-- checks whether a `MessageData` refers to an error of a missing `?` is `refine`. -/
-def isSyntPlaceHolder : MessageData → Bool
-  | .withNamingContext _ (.withContext _ (.tagged `Elab.synthPlaceholder _)) => true
-  | _ => false
-
-/-- extracts `refine'` from the given `Syntax`, returning also the `SourceInfo`, the arguments
-of the `refine'` node and whether `refine'` contains `..`. -/
-partial
-def getRefine' : Syntax → Array (Syntax × SourceInfo × Array Syntax × Option Syntax)
-  | stx@(.node si ``Lean.Parser.Tactic.refine' args) =>
-    --dbg_trace "refine' found"
-    let rest := (args.map getRefine').flatten
-    rest.push (stx, si, args, stx.find? (·.isOfKind ``Lean.Parser.Term.optEllipsis))
-  | .node _ _ args => (args.map getRefine').flatten
-  | _ => default
-
-/-- replaces each `refine'` by `refine` in succession in `cmd` and, each time, catches the errors
-of missing `?`, collecting their positions.  Eventually, it returns an array of pairs
-`(1/0, position)`, where
-* `1` means that the `position` is the beginning of `refine'` and
-* `0` means that the `position` is a missing `?`.
--/
-def getQuestions (cmd : Syntax) : Command.CommandElabM (Array (Nat × Position)) := do
-  let s ← get
-  let fm ← getFileMap
-  let refine's := getRefine' cmd
-  let newCmds := refine's.map fun (r, si, args, dots?) => Id.run do
-      if let some dots := dots? then dbg_trace "{dots} present"
-      let ncm ← cmd.replaceM fun s => if s == r then
-        let args := args.modify 0 fun _ => mkAtomFrom args[0]! "refine "
-        return some (.node si ``Lean.Parser.Tactic.refine args)
-        else return none
-      return (r, ncm)
-  let mut poss := #[]
-  for (r, ncmd) in newCmds do
-    let exm ← toExample ncmd
-    Elab.Command.elabCommand exm
-    let msgs := (← get).messages.msgs
-    --dbg_trace msgs.toArray.map (·.endPos)
-    let ph := msgs.filter (fun m => isSyntPlaceHolder m.data)
-    if ! ph.toArray.isEmpty then
-      -- a repetition in `Position`s is an indication that `refine` cannot replace `refine'`
-      let positions := (ph.map (·.pos)).toList
-      if positions == positions.eraseDup then
-        --dbg_trace ph.size == msgs.size
-        --dbg_trace ph.toArray.map (·.endPos)
-        poss := poss ++
-          (ph.map (0, ·.pos)).toArray.push (1, fm.toPosition (r.getPos?.getD default))
-  set s
-  return poss
 
 /-- Gets the value of the `linter.refine'ToRefine` option. -/
 def getLinterHash (o : Options) : Bool := Linter.getLinterValue linter.refine'ToRefine o
@@ -238,7 +132,7 @@ def getLinterHash (o : Options) : Bool := Linter.getLinterValue linter.refine'To
 /-- Reports the positions of missing `?`. -/
 def refine'ToRefineLinter : Linter where run stx := do
   if getLinterHash (← getOptions) then
-    let pos ← getQuestions' stx
+    let pos ← getQuestions stx
     if ! pos.isEmpty then logInfo m!"{pos}" --else logInfo "fail"
 
 initialize addLinter refine'ToRefineLinter
