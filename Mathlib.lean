fb71a39e6de328116e212e3fe22e45731163eba6
Johan Commelin <johan@commelin.net>
2024-05-28 14:16:40 +0000

    feat(Algebra/Lie/Semisimple): API for semisimple Lie algebras (#13217)
    Including the result that ideals in a semisimple Lie algebra
    are in a unique way the direct sum of simple ideals.
    This result makes no assumptions on the ring of coefficients or the Lie algebra;
    it holds over arbitrary base rings, and in arbitrary dimensions.

--- Mathlib.lean
+++ Mathlib.lean
@@ -3170,6 +3170,7 @@ import Mathlib.Order.Atoms.Finite
 import Mathlib.Order.Basic
 import Mathlib.Order.Birkhoff
 import Mathlib.Order.BooleanAlgebra
+import Mathlib.Order.BooleanGenerators
 import Mathlib.Order.Booleanisation
 import Mathlib.Order.Bounded
 import Mathlib.Order.BoundedOrder
