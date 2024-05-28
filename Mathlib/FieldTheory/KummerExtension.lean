c3c2b3d73216d4c0fe5f9ab5bca3094f9f054e2c
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2024-05-28 11:56:32 +0000

    feat(RingTheory/RootsOfUnity): product representation of the order of a primitive root of unity (#13290)
    This
    * weakens the assumptions in [X_pow_sub_C_eq_prod](https://leanprover-community.github.io/mathlib4_docs/Mathlib/FieldTheory/KummerExtension.html#X_pow_sub_C_eq_prod) on the coefficient ring from "field" to "integral domain", and
    * adds results on representing the order `n` of a primitive root of unity `μ` in `R` as a product involving terms like `μ^k - 1`. (In a new file `RingTheory.RootsOfUnity.Lemmas` because of fairly heavy imports.)
    
    This will  be useful for results on Jacobi sums (to be added eventually).
    
    
    
    Co-authored-by: Michael Stoll <99838730+MichaelStollBayreuth@users.noreply.github.com>

--- Mathlib/FieldTheory/KummerExtension.lean
+++ Mathlib/FieldTheory/KummerExtension.lean
@@ -82,12 +82,27 @@ theorem X_pow_sub_C_splits_of_isPrimitiveRoot
   | inr hn =>
     rw [splits_iff_card_roots, ← nthRoots, hζ.card_nthRoots, natDegree_X_pow_sub_C, if_pos ⟨α, e⟩]
 
-theorem X_pow_sub_C_eq_prod
+open BigOperators
+
+-- make this private, as we only use it to prove a strictly more general version
+private
+theorem X_pow_sub_C_eq_prod'
     {n : ℕ} {ζ : K} (hζ : IsPrimitiveRoot ζ n) {α a : K} (hn : 0 < n) (e : α ^ n = a) :
     (X ^ n - C a) = ∏ i ∈ Finset.range n, (X - C (ζ ^ i * α)) := by
   rw [eq_prod_roots_of_monic_of_splits_id (monic_X_pow_sub_C _ (Nat.pos_iff_ne_zero.mp hn))
     (X_pow_sub_C_splits_of_isPrimitiveRoot hζ e), ← nthRoots, hζ.nthRoots_eq e, Multiset.map_map]
   rfl
+
+lemma X_pow_sub_C_eq_prod {R : Type*} [CommRing R] [IsDomain R]
+    {n : ℕ} {ζ : R} (hζ : IsPrimitiveRoot ζ n) {α a : R} (hn : 0 < n) (e : α ^ n = a) :
+    (X ^ n - C a) = ∏ i ∈ Finset.range n, (X - C (ζ ^ i * α)) := by
+  let K := FractionRing R
+  let i := algebraMap R K
+  have h := NoZeroSMulDivisors.algebraMap_injective R K
+  apply_fun Polynomial.map i using map_injective i h
+  simpa only [Polynomial.map_sub, Polynomial.map_pow, map_X, map_C, map_mul, map_pow,
+    Polynomial.map_prod, Polynomial.map_mul]
+    using X_pow_sub_C_eq_prod' (hζ.map_of_injective h) hn <| map_pow i α n ▸ congrArg i e
 
 end Splits
 
