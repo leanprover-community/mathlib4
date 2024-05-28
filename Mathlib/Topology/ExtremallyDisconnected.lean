c1bfefd220fabfc7335ab97ab7cd93e6790f1c0e
Filippo A. E. Nuccio <filippo.nuccio@univ-st-etienne.fr>
2024-05-28 09:02:56 +0000

    doc(Mathlib/Topology/ExtremallyDisconnected): remove one extra bracket (#13300)
    Remove one extra curly bracket from the doc that is causing a compilation error on the online doc.

--- Mathlib/Topology/ExtremallyDisconnected.lean
+++ Mathlib/Topology/ExtremallyDisconnected.lean
@@ -176,7 +176,7 @@ lemma exists_compact_surjective_zorn_subset [T1Space A] [CompactSpace D] {π : D
 /-- Lemma 2.1 in [Gleason, *Projective topological spaces*][gleason1958]:
 if $\rho$ is a continuous surjection from a topological space $E$ to a topological space $A$
 satisfying the "Zorn subset condition", then $\rho(G)$ is contained in
-the closure of $A \setminus \rho(E \setminus G)}$ for any open set $G$ of $E$. -/
+the closure of $A \setminus \rho(E \setminus G)$ for any open set $G$ of $E$. -/
 lemma image_subset_closure_compl_image_compl_of_isOpen {ρ : E → A} (ρ_cont : Continuous ρ)
     (ρ_surj : ρ.Surjective) (zorn_subset : ∀ E₀ : Set E, E₀ ≠ univ → IsClosed E₀ → ρ '' E₀ ≠ univ)
     {G : Set E} (hG : IsOpen G) : ρ '' G ⊆ closure ((ρ '' Gᶜ)ᶜ) := by
