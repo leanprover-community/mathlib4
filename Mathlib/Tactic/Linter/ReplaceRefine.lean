0e6340365c7a6d8f7cdf0f278f471a05522a53c9
adomani <adomani@gmail.com>
2024-05-28 14:40:06 +0100

    docs

--- Mathlib/Tactic/Linter/ReplaceRefine.lean
+++ Mathlib/Tactic/Linter/ReplaceRefine.lean
@@ -63,7 +63,7 @@ def toExample {m : Type → Type} [Monad m] [MonadQuotation m] : Syntax → m Sy
 
 /-- replaces each `refine'` by `refine` in succession in `cmd` and, each time, catches the errors
 of missing `?`, collecting their positions.  Eventually, it returns an array of pairs
-`(true/false, position)`, where
+`(1/0, position)`, where
 * `1` means that the `position` is the beginning of `refine'` and
 * `0` means that the `position` is a missing `?`.
 -/
