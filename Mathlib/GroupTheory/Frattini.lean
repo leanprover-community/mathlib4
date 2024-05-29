f155c969645b13dec05193a356b694d413bc44de
Kim Morrison <kim@tqft.net>
2024-05-30 03:21:28 +1000

    initial work on the Frattini subgroup, with Colva, Inna, Sussana

---
+++ Mathlib/GroupTheory/Frattini.lean
@@ -0,0 +1,39 @@
+import Mathlib
+
+def frattini (G : Type*) [Group G] : Subgroup G :=
+   ⨅ a ∈ {H | IsCoatom H}, a
+
+variable {G H : Type*} [Group G] [Group H]
+
+open Subgroup
+
+-- Provided by Eric Wieser at https://leanprover.zulipchat.com/#narrow/stream/217875-Is-there-code-for-X.3F/topic/Group.20isomorphism.20gives.20an.20order.20isomorphism.20on.20subgroups/near/441230172
+-- We may actually prefer the version via `comap` below!
+def MulEquiv.mapSubgroup (f : G ≃* H) : Subgroup G ≃o Subgroup H where
+  toFun := Subgroup.map f
+  invFun := Subgroup.map f.symm
+  left_inv sg := by simp [Subgroup.map_map]
+  right_inv sh := by simp [Subgroup.map_map]
+  map_rel_iff' {sg1 sg2} :=
+    ⟨fun h => by simpa [Subgroup.map_map] using
+      Subgroup.map_mono (f := (f.symm : H →* G)) h, Subgroup.map_mono⟩
+
+def MulEquiv.comapSubgroup (f : G ≃* H) : Subgroup H ≃o Subgroup G where
+  toFun := Subgroup.comap f
+  invFun := Subgroup.comap f.symm
+  left_inv sg := by simp [Subgroup.comap_comap]
+  right_inv sh := by simp [Subgroup.comap_comap]
+  map_rel_iff' {sg1 sg2} :=
+    ⟨fun h => by simpa [Subgroup.comap_comap] using
+      Subgroup.comap_mono (f := (f.symm : H →* G)) h, Subgroup.comap_mono⟩
+
+variable (G)
+
+theorem frattini_characteristic :  (frattini G).Characteristic := by
+    rw [characteristic_iff_comap_eq]
+    intro φ
+    unfold frattini
+    simp only [comap_iInf]
+    fapply Equiv.iInf_congr
+    · exact φ.comapSubgroup.toEquiv
+    · sorry
