162671dd837faba809f991fd481eb9718e836ae4
damiano <adomani@gmail.com>
2024-05-29 15:57:18 +0000

    fix: extend attributeInstanceIn linter to lint all global attributes declared with `in` (#13293)
    Extend the `AttributeInstanceIn` linter in two ways:
    - allow for a list of attributes and flag the ones that are not `scoped` or `local`
    - also lint any other attribute declared in an `attribute in` line which is neither `scoped` nor `local` (such as `ext` or `simp`).
    
    This uncovered a global `TensorProduct.ext` attribute in `Mathlib.LinearAlgebra.BilinearForm.TensorProduct`.
    It would have caught #7678 as well.
    
    See #13190, especially the final comments by [Eric](https://github.com/leanprover-community/mathlib4/pull/13190#discussion_r1615652567) and [Mauricio](https://github.com/leanprover-community/mathlib4/pull/13190#issuecomment-2133615950).
    
    ```lean
    theorem what : False := sorry
    
    attribute [simp] what in
    #guard true
    
    -- the `simp` attribute on `what` persists
    example : False := by simp  -- `simp` finds `what`
    
    theorem who {x y : Nat} : x = y := sorry
    
    attribute [ext] who in
    #guard true
    
    -- the `ext` attribute on `who` persists
    example {x y : Nat} : x = y := by ext
    ```
    
    
    
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>
    Co-authored-by: grunweg <rothgami@math.hu-berlin.de>
    Co-authored-by: Michael Rothgang <rothgami@math.hu-berlin.de>
    Co-authored-by: Mauricio Collares <mauricio@collares.org>

--- Mathlib.lean
+++ Mathlib.lean
@@ -3800,7 +3800,7 @@ import Mathlib.Tactic.Linarith.SimplexAlgorithm.SimplexAlgorithm
 import Mathlib.Tactic.Linarith.Verification
 import Mathlib.Tactic.LinearCombination
 import Mathlib.Tactic.Linter
-import Mathlib.Tactic.Linter.AttributeInstanceIn
+import Mathlib.Tactic.Linter.GlobalAttributeIn
 import Mathlib.Tactic.Linter.HashCommandLinter
 import Mathlib.Tactic.Linter.Lint
 import Mathlib.Tactic.Measurability
