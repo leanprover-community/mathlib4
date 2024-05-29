507d57948a86a846bc23b07d0916f37178e412f8
dagurtomas <dagurtomas@gmail.com>
2024-05-29 23:20:27 +0000

    mathlib

--- Mathlib.lean
+++ Mathlib.lean
@@ -4000,6 +4000,7 @@ import Mathlib.Topology.Category.Compactum
 import Mathlib.Topology.Category.LightProfinite.Basic
 import Mathlib.Topology.Category.LightProfinite.EffectiveEpi
 import Mathlib.Topology.Category.LightProfinite.Limits
+import Mathlib.Topology.Category.LightProfinite.Sequence
 import Mathlib.Topology.Category.Locale
 import Mathlib.Topology.Category.Profinite.AsLimit
 import Mathlib.Topology.Category.Profinite.Basic
