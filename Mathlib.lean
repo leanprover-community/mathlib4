fd5a6948b6365a57eb86d48c2d1e535d998c3415
Joël Riou <joel.riou@universite-paris-saclay.fr>
2024-05-30 17:49:11 +0000

    feat(CategoryTheory/MorphismProperty): the factorization axiom (#13331)
    In this PR, we formalize the factorization axiom which asserts that a morphism in a category can be factored as a morphism in a certain class `W₁` followed by a morphism in a class `W₂`. A functorial version of the axiom is also  defined.

--- Mathlib.lean
+++ Mathlib.lean
@@ -1512,6 +1512,7 @@ import Mathlib.CategoryTheory.Monoidal.Types.Symmetric
 import Mathlib.CategoryTheory.MorphismProperty.Basic
 import Mathlib.CategoryTheory.MorphismProperty.Composition
 import Mathlib.CategoryTheory.MorphismProperty.Concrete
+import Mathlib.CategoryTheory.MorphismProperty.Factorization
 import Mathlib.CategoryTheory.MorphismProperty.IsInvertedBy
 import Mathlib.CategoryTheory.MorphismProperty.Limits
 import Mathlib.CategoryTheory.NatIso
