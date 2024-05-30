ea1a918a9e9225e0b430a23d66c3039b59d1c7b2
Kim Morrison <kim@tqft.net>
2024-05-30 16:55:24 +1000

    copyright

--- docs/CategoryTheory.lean
+++ docs/CategoryTheory.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2024 Lean FRO LLC. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Kim Morrison
+-/
 import Mathlib
 
 noncomputable section -- Much of the category theory library is noncomputable,
