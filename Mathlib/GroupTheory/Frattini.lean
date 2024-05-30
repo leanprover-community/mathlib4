37f795a896d3a5151eb6f4f807062f8edc4dd814
Kim Morrison <kim@tqft.net>
2024-05-31 08:46:40 +1000

    finish the proof and some cleanup

--- Mathlib/GroupTheory/Frattini.lean
+++ Mathlib/GroupTheory/Frattini.lean
@@ -33,7 +33,7 @@ theorem MulEquiv.isCoatom_comap (f : G ≃* H) {K : Subgroup H} :
 variable (G)
 
 /-- The Frattini subgroup is characteristic. -/
-theorem frattini_characteristic : (frattini G).Characteristic := by
+instance frattini_characteristic : (frattini G).Characteristic := by
     rw [characteristic_iff_comap_eq]
     intro φ
     unfold frattini
@@ -54,45 +54,61 @@ theorem frattini_nongenerating_subset : ∀ X : Set (G), Subgroup.closure ( X) �
   simp at h
   sorry
 
-#check Or
-
-lemma frattini_le_coatom(K : Subgroup G)(h: IsCoatom K) : frattini G ≤ K :=
-  biInf_le _ h
-
 variable {G}
 
-theorem frattini_nongenerating[IsCoatomic (Subgroup G)] {K : Subgroup G} (h: K ⊔ frattini G = ⊤): K = ⊤  := by
-  --unfold frattini at h
-  --simp at h
+lemma frattini_le_coatom {K : Subgroup G} (h: IsCoatom K) : frattini G ≤ K :=
+  biInf_le _ h
+
+theorem frattini_nongenerating [IsCoatomic (Subgroup G)] {K : Subgroup G} (h : K ⊔ frattini G = ⊤) :
+    K = ⊤ := by
   have w := eq_top_or_exists_le_coatom K
   cases w
   case inl p => assumption
   case inr p =>
     obtain ⟨ M, c, le⟩:= p
     have q : K ⊔ frattini G ≤ M := by
-      have r:= frattini_le_coatom G M c
+      have r := frattini_le_coatom c
       exact sup_le le r
     rw [h] at q
     simp at q
     have:= c.1 q
     contradiction
 
--- fix implicitness of Sylow.nonempty
+noncomputable section
 
-theorem frattini_nilpotent[Fintype G] : Group.IsNilpotent (frattini G) := by
-  have n : Normal (frattini G) := inferInstance
-      -- normal_of_characteristic (frattini G)
-  have frat_fin : Fintype (frattini G) := sorry
-  have q := isNilpotent_of_finite_tFAE (G:= frattini G)
+-- The Sylow files unnecessarily use `Fintype` (computable) where often `Finite` would suffice,
+-- so wwe need this:
+attribute [local instance] Fintype.ofFinite
+
+-- This is surely in Mathlib?!
+-- Asked at https://leanprover.zulipchat.com/#narrow/stream/217875-Is-there-code-for-X.3F/topic/normal.20subgroups/near/441573924
+theorem foo {K : Subgroup G} {L : Subgroup K} (n : (map K.subtype L).Normal) : L.Normal := by
+  obtain ⟨conj_mem⟩ := n
+  simp at conj_mem
+  constructor
+  intro l l_mem k
+  specialize conj_mem l l.2 l_mem k
+  obtain ⟨_, h⟩ := conj_mem
+  exact h
+
+/-- When `G` is finite, the Frattini subgroup is nilpotent. -/
+theorem frattini_nilpotent [Fintype G] : Group.IsNilpotent (frattini G) := by
+  -- We use the characterisation of nilpotency in terms of all Sylow subgroups being normal.
+  have q := isNilpotent_of_finite_tFAE (G := frattini G)
   have r := q.out 0 3
-  rw [r]
+  rw [r]; clear q r
+  -- Consider each prime `p` and Sylow `p`-subgroup `P` of `frattini G`.
   intro p p_prime P
-  --obtain ⟨ P ⟩:= Sylow.nonempty ( p:= p) (G:= frattini G)
+  -- The Frattini argument shows that the normalizer of `P` in `G`
+  -- together with `frattini G` generates `G`.
   have frat_arg := Sylow.normalizer_sup_eq_top P
+  -- and hence by the nongenerating property of the Frattini subgroup that
+  -- the normalzer of `P` in `G` is `G`.
   have := frattini_nongenerating frat_arg
-  have pn : Normal (G := frattini G) P := by
-        exact normal_inf_normal (G:= G) (map (frattini G).subtype P) (frattini G)
-
+  -- This means that `P` is normal as a subgroup of `G`
+  have : (map (frattini G).subtype ↑P).Normal := by exact normalizer_eq_top.mp this
+  -- and hence also as a subgroup of `frattini G`.
+  have pn : Normal (G := frattini G) P := foo this
   exact pn
 
 
