5432298bf69b6e62318ad3312b24fad823c62656
Kim Morrison <kim@tqft.net>
2024-05-30 22:49:44 +1000

    .

--- Mathlib/GroupTheory/Frattini.lean
+++ Mathlib/GroupTheory/Frattini.lean
@@ -1,4 +1,4 @@
-import Mathlib
+import Mathlib.Algebra.Group.Subgroup.Basic
 
 def frattini (G : Type*) [Group G] : Subgroup G :=
    ⨅ a ∈ {H | IsCoatom H}, a
@@ -11,6 +11,7 @@ open Subgroup
 -- We may actually prefer the version via `comap` below!
 -- See also `Submodule.orderIsoMapComap`, which is stated in a slightly more general form,
 -- and uses `map` for the forward direction and `comap` for the reverse direction.
+@[simps]
 def MulEquiv.mapSubgroup (f : G ≃* H) : Subgroup G ≃o Subgroup H where
   toFun := Subgroup.map f
   invFun := Subgroup.map f.symm
@@ -20,6 +21,8 @@ def MulEquiv.mapSubgroup (f : G ≃* H) : Subgroup G ≃o Subgroup H where
     ⟨fun h => by simpa [Subgroup.map_map] using
       Subgroup.map_mono (f := (f.symm : H →* G)) h, Subgroup.map_mono⟩
 
+-- @[simps] -- This breaks the proof below,
+-- but it can be fixed by proving `MulEquiv.isCoatom_comap` even further below!
 def MulEquiv.comapSubgroup (f : G ≃* H) : Subgroup H ≃o Subgroup G where
   toFun := Subgroup.comap f
   invFun := Subgroup.comap f.symm
@@ -28,6 +31,9 @@ def MulEquiv.comapSubgroup (f : G ≃* H) : Subgroup H ≃o Subgroup G where
   map_rel_iff' {sg1 sg2} :=
     ⟨fun h => by simpa [Subgroup.comap_comap] using
       Subgroup.comap_mono (f := (f.symm : H →* G)) h, Subgroup.comap_mono⟩
+
+
+
 
 variable (G)
 
@@ -38,4 +44,24 @@ theorem frattini_characteristic :  (frattini G).Characteristic := by
     simp only [comap_iInf]
     fapply Equiv.iInf_congr
     · exact φ.comapSubgroup.toEquiv
-    · sorry
+    · intros
+      simp
+      rfl
+
+
+/-!
+Some results we don't actually use above, but seem like they are missing and related!
+-/
+
+@[simp]
+theorem Subgroup.map_symm (f : G ≃* H) {K : Subgroup H} : map f.symm K = comap (f : G →* H) K := by
+  aesop
+@[simp]
+theorem Subgroup.comap_symm (f : G ≃* H) {K : Subgroup G} : comap (f.symm : H →* G) K = map f K := by
+  aesop
+
+@[simp]
+theorem MulEquiv.isCoatom_comap (f : G ≃* H) {K : Subgroup H} :
+    IsCoatom (comap (f : G →* H) K) ↔ IsCoatom K := by
+  dsimp [IsCoatom]
+  sorry
