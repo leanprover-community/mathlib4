fabd1e1b0ea280522fffa8c20fc3eed06a1a286a
acmepjz <acme_pjz@hotmail.com>
2024-06-01 04:29:54 +0800

    left-Noetherian rings and commutative rings satisfy `OrzechProperty`

--- Mathlib.lean
+++ Mathlib.lean
@@ -410,6 +410,7 @@ import Mathlib.Algebra.Module.Prod
 import Mathlib.Algebra.Module.Projective
 import Mathlib.Algebra.Module.Submodule.Basic
 import Mathlib.Algebra.Module.Submodule.Bilinear
+import Mathlib.Algebra.Module.Submodule.IterateMapComap
 import Mathlib.Algebra.Module.Submodule.Ker
 import Mathlib.Algebra.Module.Submodule.Lattice
 import Mathlib.Algebra.Module.Submodule.LinearMap
