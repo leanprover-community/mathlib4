b694e972352bf501cb3cec0db9d827be2193b68e
colva <colva.roney-dougal@st-andrews.ac.uk>
2024-05-31 11:11:03 +0100

    files related to frattini subgroup

--- Mathlib/GroupTheory/Frattini.lean
+++ Mathlib/GroupTheory/Frattini.lean
@@ -1,41 +1,22 @@
---import Mathlib.Algebra.Group.Subgroup.Basic
-import Mathlib
-
-/--
-The infimum of all coatoms.
-
-This notion specializes, e.g. in the subgroup lattice of a group to the Frattini subgroup,
-or in the lattices of ideals in a ring `R` to the Jacobson ideal.
+/-
+Copyright (c) 2024 Colva Roney-Dougal. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Colva Roney-Dougal, Inna Capdeboscq, Susanna Fishel, Kim Morrison
 -/
-def Order.radical (α : Type*) [Preorder α] [OrderTop α] [InfSet α] : α :=
-   ⨅ a ∈ {H | IsCoatom H}, a
+import Mathlib.Algebra.Group.Subgroup.Basic
+import Mathlib.Data.Fintype.Card
+import Mathlib.GroupTheory.Nilpotent
+import Mathlib.Order.Radical
 
-section Order.radical
+/-!
+This contains the definition of the Frattini subgroup of a group, and
+three elementary results
+* The frattini subgroup is characteristic
+* If every subgroup of a group is contained in a maximal subgroup, then
+the frattini subgroup consists of the non-generating elements of the group
+* The frattini subgroup of a finite group is nilpotent
+-/
 
-variable {α : Type*} [CompleteLattice α]
-
-lemma Order.radical_le_coatom {a : α} (h : IsCoatom a) : radical α ≤ a := biInf_le _ h
-
-variable {β : Type*} [CompleteLattice β]
-
-theorem OrderIso.map_radical(f : α ≃o β) : f (Order.radical α) = Order.radical β := by
-  unfold Order.radical
-  simp only [OrderIso.map_iInf]
-  fapply Equiv.iInf_congr
-  · exact f.toEquiv
-  · intros
-    simp
-
-theorem Order.radical_nongenerating [IsCoatomic α] {a : α} (h : a ⊔ radical α = ⊤) :
-    a = ⊤ := by
-  obtain (rfl | w) := eq_top_or_exists_le_coatom a
-  · rfl
-  · obtain ⟨m, c, le⟩ := w
-    have q : a ⊔ radical α ≤ m := sup_le le (radical_le_coatom c)
-    rw [h, top_le_iff] at q
-    simpa using c.1 q
-
-end Order.radical
 
 /-- The Frattini subgroup of a group is the intersection of the maximal subgroups. -/
 def frattini (G : Type*) [Group G] : Subgroup G :=
@@ -45,26 +26,6 @@ variable {G H : Type*} [Group G] [Group H]
 
 open Subgroup
 
-/--
-An isomorphism of groups gives an order isomorphism between the lattices of subgroups,
-defined by sending subgroups to their inverse images.
-
-See also `MulEquiv.mapSubgroup` which maps subgroups to their forward images.
--/
-@[simps]
-def MulEquiv.comapSubgroup (f : G ≃* H) : Subgroup H ≃o Subgroup G where
-  toFun := Subgroup.comap f
-  invFun := Subgroup.comap f.symm
-  left_inv sg := by simp [Subgroup.comap_comap]
-  right_inv sh := by simp [Subgroup.comap_comap]
-  map_rel_iff' {sg1 sg2} :=
-    ⟨fun h => by simpa [Subgroup.comap_comap] using
-      Subgroup.comap_mono (f := (f.symm : H →* G)) h, Subgroup.comap_mono⟩
-
-@[simp]
-theorem MulEquiv.isCoatom_comap (f : G ≃* H) {K : Subgroup H} :
-    IsCoatom (comap (f : G →* H) K) ↔ IsCoatom K :=
-  OrderIso.isCoatom_iff (f.comapSubgroup) K
 
 variable (G)
 
@@ -122,59 +83,3 @@ theorem frattini_nilpotent [Fintype G] : Group.IsNilpotent (frattini G) := by
   have P_normal_in_G : (map (frattini G).subtype ↑P).Normal := normalizer_eq_top.mp normalizer_P
   -- and hence also as a subgroup of `frattini G`, which was the remaining goal.
   exact normal_of_map_subtype_normal P_normal_in_G
-
-
-/-!
-Some results we don't actually use above, but seem like they are missing and related!
--/
-
--- Provided by Eric Wieser at https://leanprover.zulipchat.com/#narrow/stream/217875-Is-there-code-for-X.3F/topic/Group.20isomorphism.20gives.20an.20order.20isomorphism.20on.20subgroups/near/441230172
--- We may actually prefer the version via `comap` below!
--- See also `Submodule.orderIsoMapComap`, which is stated in a slightly more general form,
--- and uses `map` for the forward direction and `comap` for the reverse direction.
-@[simps]
-def MulEquiv.mapSubgroup (f : G ≃* H) : Subgroup G ≃o Subgroup H where
-  toFun := Subgroup.map f
-  invFun := Subgroup.map f.symm
-  left_inv sg := by simp [Subgroup.map_map]
-  right_inv sh := by simp [Subgroup.map_map]
-  map_rel_iff' {sg1 sg2} :=
-    ⟨fun h => by simpa [Subgroup.map_map] using
-      Subgroup.map_mono (f := (f.symm : H →* G)) h, Subgroup.map_mono⟩
-
-@[simp]
-theorem MulEquiv.isCoatom_map (f : G ≃* H) {K : Subgroup G} :
-    IsCoatom (map (f : G →* H) K) ↔ IsCoatom K :=
-  OrderIso.isCoatom_iff (f.mapSubgroup) K
-
-@[simp]
-theorem Subgroup.map_symm (f : G ≃* H) {K : Subgroup H} : map f.symm K = comap (f : G →* H) K := by
-  aesop
-@[simp]
-theorem Subgroup.comap_symm (f : G ≃* H) {K : Subgroup G} : comap (f.symm : H →* G) K = map f K := by
-  aesop
-
-theorem comap_equiv_strictMono (f : G ≃* H) : StrictMono (comap (f : G →* H)) :=
-  OrderIso.strictMono (f.comapSubgroup)
-
-@[simp]
-theorem MulEquiv.range (f : G ≃* H) : (f : G →* H).range = ⊤ := by
-  ext x
-  simp only [MonoidHom.mem_range, MonoidHom.coe_coe, mem_top, iff_true]
-  exact ⟨f.symm x, by simp⟩
-
-@[simp]
-theorem Subgroup.map_comap_equiv (f : G ≃* H) {K : Subgroup H} :
-    map (f : G →* H) (comap (f : G →* H) K) = K := by
-  rw [map_comap_eq_self]
-  simp
-
-@[simp]
-theorem Subgroup.map_equiv_top (f : G ≃* H) : map (f : G →* H) ⊤ = ⊤ :=
-  OrderIso.map_top (f.mapSubgroup)
-
-@[simp]
-theorem Subgroup.comap_equiv_top (f : G ≃* H) : comap (f : G →* H) ⊤ = ⊤ :=
-  OrderIso.map_top (f.comapSubgroup)
-
-attribute [simp] OrderIso.map_top -- why is this not a global simp lemma?
