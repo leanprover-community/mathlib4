c2c9ad5e9f42cf14c5c03cc6640d66d92937f69f
David Loeffler <d.loeffler.01@cantab.net>
2024-05-31 14:33:30 +0000

    feat(NumberTheory/LSeries): Riemann zeta as special case of Hurwitz (#13273)
    We now have a general treatment of the Hurwitz zeta function. The Riemann zeta is just the Hurwitz zeta for `a = 0`, and this PR reimplements it as such. 
    
    I have tried to carry over the `align` statements from the old treatment wherever possible (many of the intermediate lemmas from the previous iteration don't have direct analogues, but the main results do).

---
+++ Mathlib/Analysis/PSeriesComplex.lean
@@ -0,0 +1,26 @@
+/-
+Copyright (c) 2024 David Loeffler. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: David Loeffler
+-/
+
+import Mathlib.Analysis.PSeries
+import Mathlib.Analysis.NormedSpace.FiniteDimension
+
+/-!
+# Convergence of `p`-series (complex case)
+
+Here we show convergence of `∑ n : ℕ, 1 / n ^ p` for complex `p`. This is done in a separate file
+rather than in `Analysis.PSeries` in order to keep the prerequisites of the former relatively light.
+
+## Tags
+
+p-series, Cauchy condensation test
+-/
+
+lemma Complex.summable_one_div_nat_cpow {p : ℂ} :
+    Summable (fun n : ℕ ↦ 1 / (n : ℂ) ^ p) ↔ 1 < re p := by
+  rw [← Real.summable_one_div_nat_rpow, ← summable_nat_add_iff 1 (G := ℝ),
+    ← summable_nat_add_iff 1 (G := ℂ), ← summable_norm_iff]
+  simp only [norm_div, norm_one, norm_eq_abs, ← ofReal_natCast, abs_cpow_eq_rpow_re_of_pos
+    (Nat.cast_pos.mpr <| Nat.succ_pos _)]
