ea32f822ef054929826c089fb67328775b09a40e
Scott Morrison <scott.morrison@gmail.com>
2024-06-01 08:33:06 +0100

    cleanup

--- Mathlib.lean
+++ Mathlib.lean
@@ -1801,7 +1801,6 @@ import Mathlib.Data.Array.ExtractLemmas
 import Mathlib.Data.Array.Lemmas
 import Mathlib.Data.BinaryHeap
 import Mathlib.Data.BitVec
-import Mathlib.Data.BitVec.Lemmas
 import Mathlib.Data.Bool.AllAny
 import Mathlib.Data.Bool.Basic
 import Mathlib.Data.Bool.Count
