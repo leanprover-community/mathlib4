71822f1cf0f880d62944abac9b0aff9b08a0ca59
Yaël Dillies <yael.dillies@gmail.com>
2024-06-01 08:19:33 +0000

    chore: Split `Algebra.BigOperators.Basic` (#13277)
    ... into two files:
    * `Algebra.BigOperators.Group.Finset` for lemmas about group-like objects.
    * `Algebra.BigOperators.GroupWithZero.Finset` for lemmas about group-with-zero-like objects. Credit Johannes for https://github.com/leanprover-community/mathlib/commit/edfbf3cf.

--- Mathlib/RingTheory/Multiplicity.lean
+++ Mathlib/RingTheory/Multiplicity.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Robert Y. Lewis, Chris Hughes
 -/
 import Mathlib.Algebra.Associated
-import Mathlib.Algebra.BigOperators.Basic
+import Mathlib.Algebra.BigOperators.Group.Finset
 import Mathlib.Algebra.SMulWithZero
 import Mathlib.Data.Nat.PartENat
 import Mathlib.Tactic.Linarith
