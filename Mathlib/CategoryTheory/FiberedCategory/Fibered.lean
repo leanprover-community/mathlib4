45e68daabf43423b3db9e9dad2b2126743b29667
Calle Sönne <calle.sonne@gmail.com>
2024-06-01 11:45:57 +0100

    fixing proofs

--- Mathlib/CategoryTheory/FiberedCategory/Fibered.lean
+++ Mathlib/CategoryTheory/FiberedCategory/Fibered.lean
@@ -13,8 +13,10 @@ import Mathlib.CategoryTheory.FiberedCategory.Cartesian
 This file defines what it means for a functor `p : 𝒳 ⥤ 𝒮` to be (pre)fibered.
 
 ## Main definitions
+
 - `IsPreFibered p` expresses that `p` gives `𝒳` the structure of a prefibered category over `𝒮`,
 as in SGA VI.6.1
+
 - `IsFibered p` expresses `𝒳` is fibered over `𝒮` via a functor `p : 𝒳 ⥤ 𝒮`, as in SGA VI.6.1.
 This means that it is prefibered, and that the composition of any two cartesian morphisms is
 cartesian.
@@ -45,6 +47,7 @@ namespace CategoryTheory
 variable {𝒮 : Type u₁} {𝒳 : Type u₂} [Category.{v₁} 𝒮] [Category.{v₂} 𝒳]
 
 /-- Definition of a prefibered category.
+
 See SGA 1 VI.6.1. -/
 class Functor.IsPreFibered (p : 𝒳 ⥤ 𝒮) : Prop where
   has_pullbacks' {a : 𝒳} {R : 𝒮} (f : R ⟶ p.obj a) : ∃ (b : 𝒳) (φ : b ⟶ a), IsCartesian p f φ
@@ -71,35 +74,18 @@ open IsCartesian
 
 variable {p : 𝒳 ⥤ 𝒮} [IsPreFibered p] {R S : 𝒮} {a : 𝒳} (ha : p.obj a = S) (f : R ⟶ S)
 
-/-- Given a prefibered category `p : 𝒳 ⥤ 𝒫`, and a diagram
-```
-           a
-           -
-           |
-           v
-  R --f--> S
-```
-`pullbackObj` is defined as the domain `R ×_S a` of some cartesian arrow lying over
-`f`, which exists by the fibered category structure on `p`. -/
-noncomputable def pullbackObj {p : 𝒳 ⥤ 𝒮} [IsPreFibered p] {R S : 𝒮}
-    {a : 𝒳} (ha : p.obj a = S) (f : R ⟶ S) : 𝒳 :=
+/-- Given a fibered category `p : 𝒳 ⥤ 𝒫`, a morphism `f : R ⟶ S` and an object `a` lying over `S`,
+then `pullbackObj` is the domain of some choice of a cartesian morphism lying over `f` with
+codomain `a`. -/
+noncomputable def pullbackObj : 𝒳 :=
   Classical.choose (IsPreFibered.has_pullbacks ha f)
 
-/-- Given a fibered category `p : 𝒳 ⥤ 𝒫`, and a diagram
-```
-          a
-          -
-          |
-          v
-R --f--> S
-```
-we get a map `R ×_S b ⟶ a` -/
-noncomputable def pullbackMap {p : 𝒳 ⥤ 𝒮} [IsPreFibered p] {R S : 𝒮}
-    {a : 𝒳} (ha : p.obj a = S) (f : R ⟶ S) : pullbackObj ha f ⟶ a :=
+/-- Given a fibered category `p : 𝒳 ⥤ 𝒫`, a morphism `f : R ⟶ S` and an object `a` lying over `S`,
+then `pullbackMap` is a choice of a cartesian morphism lying over `f` with codomain `a`. -/
+noncomputable def pullbackMap  : pullbackObj ha f ⟶ a :=
   Classical.choose (Classical.choose_spec (IsPreFibered.has_pullbacks ha f))
 
-instance pullbackMap.IsCartesian {p : 𝒳 ⥤ 𝒮} [IsPreFibered p] {R S : 𝒮}
-    {a : 𝒳} (ha : p.obj a = S) (f : R ⟶ S) : IsCartesian p f (pullbackMap ha f) :=
+instance pullbackMap.IsCartesian : IsCartesian p f (pullbackMap ha f) :=
   Classical.choose_spec (Classical.choose_spec (IsPreFibered.has_pullbacks ha f))
 
 lemma pullbackObj_proj : p.obj (pullbackObj ha f) = R :=
@@ -111,61 +97,58 @@ namespace IsFibered
 
 open IsCartesian IsPreFibered
 
+/-- In a fibered category, any cartesian morphism is strongly cartesian. -/
 instance isStronglyCartesian_of_isCartesian (p : 𝒳 ⥤ 𝒮) [p.IsFibered] {R S : 𝒮} (f : R ⟶ S)
-    {a b : 𝒳} (φ : a ⟶ b) [p.IsCartesian f φ] : p.IsStronglyCartesian f φ := by
-  apply IsStronglyCartesian.mk
-  intro a' g φ' hφ'
-  -- Let `ψ` be a cartesian arrow lying over `g`
-  let ψ := pullbackMap (domain_eq p f φ) g
-  --have : IsCartesian p (g ≫ f) (ψ ≫ φ) := inferInstance
-  -- Let `τ : a' ⟶ ..` be the map induced by the universal property of `ψ ≫ φ`
-  let τ := inducedMap p (g ≫ f) (ψ ≫ φ) φ'
-  use τ ≫ ψ
-  refine ⟨⟨?_, ?_⟩, ?_⟩
-  -- TODO: order of values (+ make type-class instance?)
-  apply comp_lift_id_left p (p.obj a') τ g ψ
-  rw [assoc, inducedMap_comp] -- TODO: comp simp lemma?
-  intro π ⟨hπ, hπ_comp⟩
-  -- Let `τ'` be the map induced from `π` and the universal property of `ψ`
-  rw [← inducedMap_comp p g ψ π]
-  congr 1
-  apply inducedMap_unique
-  rw [← assoc, inducedMap_comp]
-  exact hπ_comp
+    {a b : 𝒳} (φ : a ⟶ b) [p.IsCartesian f φ] : p.IsStronglyCartesian f φ where
+  universal_property' := by
+    intro a' g φ' hφ'
+    -- Let `ψ` be a cartesian arrow lying over `g`
+    let ψ := pullbackMap (domain_eq p f φ) g
+    -- Let `τ` be the map induced by the universal property of `ψ ≫ φ`.
+    let τ := inducedMap p (g ≫ f) (ψ ≫ φ) φ'
+    use τ ≫ ψ
+    -- It is easily verified that `τ ≫ ψ` lifts `g` and `τ ≫ ψ ≫ φ = φ'`
+    refine ⟨⟨inferInstance, by simp only [assoc, inducedMap_comp, τ]⟩, ?_⟩
+    -- It remains to check that `τ ≫ ψ` is unique.
+    -- So fix another lift `π` of `g` satisfying `π ≫ φ = φ'`.
+    intro π ⟨hπ, hπ_comp⟩
+    -- Write `π` as `π = τ' ≫ ψ` for some `τ'` induced by the universal property of `ψ`.
+    rw [← inducedMap_comp p g ψ π]
+    -- It remains to show that `τ' = τ`. This follows again from the universal property of `ψ`.
+    congr 1
+    apply inducedMap_unique
+    rwa [← assoc, inducedMap_comp]
 
+/-- In a category which admits strongly cartesian pullbacks, any cartesian morphism is
+strongly cartesian. This is a helper-lemma for the fact that admitting strongly cartesian pullbacks
+implies being fibered. -/
 lemma isStronglyCartesian_of_has_pullbacks' (p : 𝒳 ⥤ 𝒮) (h : ∀ (a : 𝒳) (R : 𝒮) (f : R ⟶ p.obj a),
     ∃ (b : 𝒳) (φ : b ⟶ a), IsStronglyCartesian p f φ) {R S : 𝒮} (f : R ⟶ S) {a b : 𝒳}
       (φ : a ⟶ b) [p.IsCartesian f φ] : p.IsStronglyCartesian f φ := by
-  apply IsStronglyCartesian.mk
+  constructor
   intro c g φ' hφ'
   subst_hom_lift p f φ; clear a b R S
   -- Let `ψ` be a cartesian arrow lying over `g`
   obtain ⟨a', ψ, hψ⟩ := h _ _ (p.map φ)
   -- Let `τ' : c ⟶ a'` be the map induced by the universal property of `ψ`
   let τ' := IsStronglyCartesian.inducedMap p (p.map φ) ψ (f':= g ≫ p.map φ) rfl φ'
-  -- Let `τ : a' ⟶ a` be the map induced by the universal property of `φ`
+  -- Let `Φ : a' ⟶ a` be natural isomorphism induced between `φ` and `ψ`.
   let Φ := naturalIso p (p.map φ) φ ψ
-  --let τ := inducedMap p (p.map φ) φ ψ
+  -- The map induced by `φ` will be `τ' ≫ Φ.hom`
   use τ' ≫ Φ.hom
-  refine ⟨⟨?_, ?_⟩, ?_⟩
-  -- TODO: order of values (+ make type-class instance?)
-  have : p.IsHomLift (𝟙 (p.obj a)) Φ.hom := by simp only [naturalIso_hom, Φ]; infer_instance
-  apply comp_lift_id_right p g τ' (p.obj a) Φ.hom
-  simp only [naturalIso_hom, assoc, inducedMap_comp, Φ]
-  rw [IsStronglyCartesian.inducedMap_comp] -- TODO: comp simp lemma?
+  -- It is easily verified that `τ' ≫ Φ.hom` lifts `g` and `τ' ≫ Φ.hom ≫ φ = φ'`
+  refine ⟨⟨inferInstance, ?_⟩, ?_⟩
+  · simp [Φ, IsStronglyCartesian.inducedMap_comp p (p.map φ) ψ rfl φ']
+  -- It remains to check that it is unique. This follows from the universal property of `ψ`.
   intro π ⟨hπ, hπ_comp⟩
   rw [← Iso.comp_inv_eq]
-  have : p.IsHomLift g (π ≫ Φ.inv) := by
-    simp only [naturalIso_inv, Φ]
-    apply comp_lift_id_right p g π (p.obj a)
-  apply IsStronglyCartesian.inducedMap_unique p
+  apply IsStronglyCartesian.inducedMap_unique p (p.map φ) ψ rfl φ'
   simp [Φ, hπ_comp]
 
 
 lemma of_has_pullbacks' {p : 𝒳 ⥤ 𝒮} (h : ∀ (a : 𝒳) (R : 𝒮) (f : R ⟶ p.obj a),
     ∃ (b : 𝒳) (φ : b ⟶ a), IsStronglyCartesian p f φ) : IsFibered p where
-  toIsPreFibered := by
-    apply IsPreFibered.mk
+  has_pullbacks' := by
     intro a R f
     obtain ⟨b, φ, hφ⟩ := h a R f
     refine ⟨b, φ, inferInstance⟩
