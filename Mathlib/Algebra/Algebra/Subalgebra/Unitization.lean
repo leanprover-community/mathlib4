c2792f3e98d040104d2d5b843d0afae9bebef760
Eric Wieser <wieser.eric@gmail.com>
2024-06-03 08:41:19 +0000

    chore: tidy induction principles for Unitization and TrivSqZeroExt
    This adds `elab_as_elim`, `induction_eliminator`, and `cases_eliminator` attributes,
    which eliminates the need to use the `using` term of `induction`.
    
    Also choose a better argument name, since this is used as the case name.

--- Mathlib/Algebra/Algebra/Subalgebra/Unitization.lean
+++ Mathlib/Algebra/Algebra/Subalgebra/Unitization.lean
@@ -148,7 +148,7 @@ theorem _root_.AlgHomClass.unitization_injective' {F R S A : Type*} [CommRing R]
     [FunLike F (Unitization R s) A] [AlgHomClass F R (Unitization R s) A]
     (f : F) (hf : ∀ x : s, f x = x) : Function.Injective f := by
   refine (injective_iff_map_eq_zero f).mpr fun x hx => ?_
-  induction' x using Unitization.ind with r a
+  induction' x with r a
   simp_rw [map_add, hf, ← Unitization.algebraMap_eq_inl, AlgHomClass.commutes] at hx
   rw [add_eq_zero_iff_eq_neg] at hx ⊢
   by_cases hr : r = 0
