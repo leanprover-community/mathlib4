de86a52db62b659d53af1cbd76e0ab0ccdbd4347
Joël Riou <joel.riou@universite-paris-saclay.fr>
2024-06-03 18:57:15 +0000

    feat(CategoryTheory/Shift): functors from a category to a category with a shift (#12607)
    Given a category `C`, and a category `D` equipped with a shift by a monoid `A`, we define a structure `SingleFunctors C D A` which contains the data of functors `functor a : C ⥤ D` for all `a : A` and isomorphisms `shiftIso n a a' h : functor a' ⋙ shiftFunctor D n ≅ functor a` whenever `n + a = a'`. These isomorphisms should satisfy certain compatibilities with respect to the shift on `D`.
    
    
    
    
    Co-authored-by: Joël Riou <37772949+joelriou@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1570,6 +1570,7 @@ import Mathlib.CategoryTheory.Shift.Predicate
 import Mathlib.CategoryTheory.Shift.Pullback
 import Mathlib.CategoryTheory.Shift.Quotient
 import Mathlib.CategoryTheory.Shift.ShiftSequence
+import Mathlib.CategoryTheory.Shift.SingleFunctors
 import Mathlib.CategoryTheory.Sigma.Basic
 import Mathlib.CategoryTheory.Simple
 import Mathlib.CategoryTheory.SingleObj
