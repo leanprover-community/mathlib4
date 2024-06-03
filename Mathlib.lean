33fb1e56acdd90d6ee4086ff4d2307e2c70c59df
Xavier Roblot <roblot@math.univ-lyon1.fr>
2024-06-03 10:32:10 +0200

    1st commit

--- Mathlib.lean
+++ Mathlib.lean
@@ -2667,7 +2667,8 @@ import Mathlib.LinearAlgebra.Coevaluation
 import Mathlib.LinearAlgebra.Contraction
 import Mathlib.LinearAlgebra.CrossProduct
 import Mathlib.LinearAlgebra.DFinsupp
-import Mathlib.LinearAlgebra.Determinant
+import Mathlib.LinearAlgebra.Determinant.Basic
+import Mathlib.LinearAlgebra.Determinant.Misc
 import Mathlib.LinearAlgebra.Dimension.Basic
 import Mathlib.LinearAlgebra.Dimension.Constructions
 import Mathlib.LinearAlgebra.Dimension.DivisionRing
@@ -2723,7 +2724,8 @@ import Mathlib.LinearAlgebra.Matrix.Charpoly.LinearMap
 import Mathlib.LinearAlgebra.Matrix.Charpoly.Minpoly
 import Mathlib.LinearAlgebra.Matrix.Charpoly.Univ
 import Mathlib.LinearAlgebra.Matrix.Circulant
-import Mathlib.LinearAlgebra.Matrix.Determinant
+import Mathlib.LinearAlgebra.Matrix.Determinant.Basic
+import Mathlib.LinearAlgebra.Matrix.Determinant.Misc
 import Mathlib.LinearAlgebra.Matrix.Diagonal
 import Mathlib.LinearAlgebra.Matrix.DotProduct
 import Mathlib.LinearAlgebra.Matrix.Dual
