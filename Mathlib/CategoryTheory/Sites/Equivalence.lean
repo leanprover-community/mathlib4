326d95d3e3ee966026523f804567a4a55e3e53f6
dagurtomas <dagurtomas@gmail.com>
2024-06-04 09:44:36 +0000

    minimize imports

--- Mathlib/CategoryTheory/Sites/Equivalence.lean
+++ Mathlib/CategoryTheory/Sites/Equivalence.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Dagur Asgeirsson
 -/
 import Mathlib.CategoryTheory.Sites.InducedTopology
-import Mathlib.CategoryTheory.Sites.Whiskering
 import Mathlib.CategoryTheory.Sites.EpiMono
 /-!
 # Equivalences of sheaf categories
