9885406b34bda394c96af41f21db05706dae5b35
Yaël Dillies <yael.dillies@gmail.com>
2024-06-04 14:50:53 +0000

    chore: Delete `Data.Int.Parity` (#12904)
    Scatter the lemmas in `Data.Int.Parity` to earlier files:
    * `Algebra.Group.Int`
    * `Algebra.Ring.Int`

--- Mathlib.lean
+++ Mathlib.lean
@@ -1981,7 +1981,6 @@ import Mathlib.Data.Int.ModEq
 import Mathlib.Data.Int.NatPrime
 import Mathlib.Data.Int.Order.Lemmas
 import Mathlib.Data.Int.Order.Units
-import Mathlib.Data.Int.Parity
 import Mathlib.Data.Int.Range
 import Mathlib.Data.Int.Sqrt
 import Mathlib.Data.Int.Star
