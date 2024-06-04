f59c0bd2d5d655f45f8da7b30438236269b0cad5
dagurtomas <dagurtomas@gmail.com>
2024-06-04 15:07:07 +0000

    .

--- Mathlib/Topology/Category/LightProfinite/SequentialLimit.lean
+++ Mathlib/Topology/Category/LightProfinite/SequentialLimit.lean
@@ -1,8 +1,14 @@
 import Mathlib.Topology.Category.LightProfinite.Basic
 import Mathlib.CategoryTheory.Functor.OfSequence
 
-open CategoryTheory
+open CategoryTheory Limits
+
+attribute [local instance] ConcreteCategory.instFunLike
 
 namespace LightProfinite
 
 variable (F : ℕᵒᵖ ⥤ LightProfinite)
+
+-- lemma limit_of_surjections_surjective
+--     (hF : ∀ n, Function.Surjective (F.map (homOfLE (Nat.le_succ n)).op)) :
+--       Function.Surjective (sorry : limit F ⟶ F.obj ⟨0⟩) := sorry
