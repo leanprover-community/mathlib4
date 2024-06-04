402551ea1c46304ddce80e92f8c389b4dfa47e4f
Calle Sönne <calle.sonne@gmail.com>
2024-06-04 21:50:28 +0100

    added file

--- Mathlib.lean
+++ Mathlib.lean
@@ -1291,6 +1291,7 @@ import Mathlib.CategoryTheory.Equivalence
 import Mathlib.CategoryTheory.EssentialImage
 import Mathlib.CategoryTheory.EssentiallySmall
 import Mathlib.CategoryTheory.Extensive
+import Mathlib.CategoryTheory.FiberedCategory.BasedCategory
 import Mathlib.CategoryTheory.FiberedCategory.HomLift
 import Mathlib.CategoryTheory.Filtered.Basic
 import Mathlib.CategoryTheory.Filtered.Connected
