ca5c6dc920ba95bf9ca42313bbd0fd29d608383f
Xavier Roblot <46200072+xroblot@users.noreply.github.com>
2024-06-04 18:30:14 +0200

    Update Mathlib/Analysis/InnerProductSpace/PiL2.lean
    Co-authored-by: Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>

--- Mathlib/Analysis/InnerProductSpace/PiL2.lean
+++ Mathlib/Analysis/InnerProductSpace/PiL2.lean
@@ -648,7 +648,7 @@ protected theorem reindex_apply (b : OrthonormalBasis ι 𝕜 E) (e : ι ≃ ι'
 #align orthonormal_basis.reindex_apply OrthonormalBasis.reindex_apply
 
 @[simp]
-theorem reindex_toBasis (b : OrthonormalBasis ι 𝕜 E)  (e : ι ≃ ι') :
+theorem reindex_toBasis (b : OrthonormalBasis ι 𝕜 E) (e : ι ≃ ι') :
     (b.reindex e).toBasis = b.toBasis.reindex e := Basis.eq_ofRepr_eq_repr fun _ ↦ congr_fun rfl
 
 @[simp]
