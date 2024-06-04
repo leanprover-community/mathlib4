566af729efa542591254441d0d545acfda09d9d7
adomani <adomani@gmail.com>
2024-06-04 20:08:19 +0200

    remove unused imports

--- Mathlib/Tactic/Linter/Tally.lean
+++ Mathlib/Tactic/Linter/Tally.lean
@@ -4,8 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Damiano Testa
 -/
 import Lean.Elab.Print
---import Lean.Elab.Command
---import Lean.Linter.Util
 import Mathlib.Lean.Expr.Basic
 import Mathlib.Tactic.Lemma
 
