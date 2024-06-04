5e02d7f072fa19ecfffd4723a1e71fff0b540fc1
Dagur Tómas Ásgeirsson <dagurtomas@gmail.com>
2024-06-04 22:02:55 +0000

    Update SequentialLimit.lean

--- Mathlib/Topology/Category/LightProfinite/SequentialLimit.lean
+++ Mathlib/Topology/Category/LightProfinite/SequentialLimit.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Dagur Asgeirsson
 -/
 import Mathlib.Topology.Category.LightProfinite.Basic
-import Mathlib.CategoryTheory.Functor.OfSequence
 /-!
 
 # Limits of surjections in `LightProfinite`
