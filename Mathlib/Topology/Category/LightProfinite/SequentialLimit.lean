aa50c0b0a8463aa59c87d579b41b83403f12f211
dagurtomas <dagurtomas@gmail.com>
2024-06-04 14:31:18 +0000

    add file

--- Mathlib/Topology/Category/LightProfinite/SequentialLimit.lean
+++ Mathlib/Topology/Category/LightProfinite/SequentialLimit.lean
@@ -1,2 +1,8 @@
 import Mathlib.Topology.Category.LightProfinite.Basic
 import Mathlib.CategoryTheory.Functor.OfSequence
+
+open CategoryTheory
+
+namespace LightProfinite
+
+variable (F : ℕᵒᵖ ⥤ LightProfinite)
