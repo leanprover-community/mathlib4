e6d835272b36b8df76973f55d77e79b3dd1ca732
dagurtomas <dagurtomas@gmail.com>
2024-06-04 13:49:14 +0000

    add modules

--- Mathlib/Condensed/Light/Epi.lean
+++ Mathlib/Condensed/Light/Epi.lean
@@ -3,13 +3,16 @@ Copyright (c) 2024 Dagur Asgeirsson. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Dagur Asgeirsson
 -/
+import Mathlib.CategoryTheory.ConcreteCategory.EpiMono
 import Mathlib.CategoryTheory.Sites.Coherent.LocallySurjective
-import Mathlib.CategoryTheory.Sites.Equivalence
-import Mathlib.Condensed.Light.Basic
+import Mathlib.Condensed.Light.Module
 /-!
 
 # Epimorphisms of light condensed objects
 
+This file characterises epimorphisms in light condensed sets and modules as the locally surjective
+morphisms. Here, the condition of locally surjective is phrased in terms of continuous surjections
+of light profinite sets.
 -/
 
 universe v u w u' v'
@@ -21,7 +24,7 @@ attribute [local instance] ConcreteCategory.hasCoeToSort ConcreteCategory.instFu
 namespace LightCondensed
 
 variable (A : Type u') [Category.{v'} A] [ConcreteCategory.{w} A]
-  [PreservesFiniteProducts (forget A)]
+  [PreservesFiniteProducts (CategoryTheory.forget A)]
 
 variable {X Y : LightCondensed.{u} A} (f : X ⟶ Y)
 
@@ -47,3 +50,16 @@ lemma epi_iff_locallySurjective_on_lightProfinite : Epi f ↔
   exact LightCondensed.isLocallySurjective_iff_locallySurjective_on_lightProfinite _ f
 
 end LightCondSet
+
+namespace LightCondMod
+
+variable (R : Type u) [Ring R] {X Y : LightCondMod.{u} R} (f : X ⟶ Y)
+
+lemma epi_iff_locallySurjective_on_lightProfinite : Epi f ↔
+    ∀ (S : LightProfinite) (y : Y.val.obj ⟨S⟩),
+      (∃ (S' : LightProfinite) (φ : S' ⟶ S) (_ : Function.Surjective φ) (x : X.val.obj ⟨S'⟩),
+        f.val.app ⟨S'⟩ x = Y.val.map ⟨φ⟩ y) := by
+  rw [← isLocallySurjective_iff_epi_of_site_essentiallySmall]
+  exact LightCondensed.isLocallySurjective_iff_locallySurjective_on_lightProfinite _ f
+
+end LightCondMod
