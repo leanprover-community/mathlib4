992a28cec147ef616aa8f10e642582f47a9f049f
Mitchell Lee <trivial171@gmail.com>
2024-06-04 09:13:03 +0000

    feat: the equational criterion for flatness (#12666)
    We prove the [equational criterion for flatness](https://stacks.math.columbia.edu/tag/00HK). As a consequence, every homomorphism from a finitely presented module to a flat module factors through a finite free module.

--- Mathlib.lean
+++ Mathlib.lean
@@ -3495,6 +3495,7 @@ import Mathlib.RingTheory.Finiteness
 import Mathlib.RingTheory.Fintype
 import Mathlib.RingTheory.Flat.Algebra
 import Mathlib.RingTheory.Flat.Basic
+import Mathlib.RingTheory.Flat.EquationalCriterion
 import Mathlib.RingTheory.Flat.Stability
 import Mathlib.RingTheory.FractionalIdeal.Basic
 import Mathlib.RingTheory.FractionalIdeal.Norm
