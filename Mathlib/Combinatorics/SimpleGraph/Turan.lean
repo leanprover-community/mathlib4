a68c86fddfd588f3cbcfd3b11c67785b3c8f0666
Parcly Taxel <reddeloostw@gmail.com>
2024-06-05 12:01:02 +0800

    I

--- Mathlib/Combinatorics/SimpleGraph/Turan.lean
+++ Mathlib/Combinatorics/SimpleGraph/Turan.lean
@@ -3,7 +3,6 @@ Copyright (c) 2024 Jeremy Tan. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jeremy Tan
 -/
-import Mathlib.Algebra.Order.Ring.Nat
 import Mathlib.Combinatorics.SimpleGraph.Clique
 import Mathlib.Order.Partition.Equipartition
 
