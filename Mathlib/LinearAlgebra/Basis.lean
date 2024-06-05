d59318109c107955d03f80af1ee45e1bc3c92851
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2024-06-05 12:11:18 +0200

    workarounds

--- Mathlib/LinearAlgebra/Basis.lean
+++ Mathlib/LinearAlgebra/Basis.lean
@@ -1165,8 +1165,9 @@ protected noncomputable def span : Basis ι R (span R (range v)) :=
       rw [← span_image, Submodule.coeSubtype]
       -- Porting note: why doesn't `rw [h₁]` work here?
       exact congr_arg _ h₁
+    #adaptation_note /--  nightly-2024-06-05: needed to specify `this i` -/
     have h₃ : (x : M) ∈ map (Submodule.subtype (span R (range v)))
-        (span R (Set.range fun i => Subtype.mk (v i) _)) := by
+        (span R (Set.range fun i => Subtype.mk (v i) (this i))) := by
       rw [h₂]
       apply Subtype.mem x
     rcases mem_map.1 h₃ with ⟨y, hy₁, hy₂⟩
