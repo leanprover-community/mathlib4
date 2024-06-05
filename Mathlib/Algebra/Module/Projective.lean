da0ab785b8a6c3cb791a741e9d50fce8111ebff1
Andrew Yang <36414270+erdOne@users.noreply.github.com>
2024-06-06 02:33:19 +0800

    Update Projective.lean

--- Mathlib/Algebra/Module/Projective.lean
+++ Mathlib/Algebra/Module/Projective.lean
@@ -193,7 +193,7 @@ theorem Projective.iff_split : Module.Projective R P ↔
 theorem Projective.iff_split_of_projective [Module.Projective R M] (s : M →ₗ[R] P)
     (hs : Function.Surjective s) :
     Module.Projective R P ↔ ∃ i, s ∘ₗ i = LinearMap.id :=
-  ⟨fun h ↦ projective_lifting_property _ _ hs, fun ⟨i, H⟩ ↦ Projective.of_split i s H⟩
+  ⟨fun _ ↦ projective_lifting_property _ _ hs, fun ⟨i, H⟩ ↦ Projective.of_split i s H⟩
 
 open TensorProduct in
 instance Projective.tensorProduct [hM : Module.Projective R M] [hN : Module.Projective R₀ N] :
