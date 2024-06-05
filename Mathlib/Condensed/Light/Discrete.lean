334c44a434dd28b1380468d094d98bbd02d39ad3
Dagur Tómas Ásgeirsson <dagurtomas@gmail.com>
2024-06-05 11:52:17 +0000

    Apply suggestions from code review
    Co-authored-by: Joël Riou <37772949+joelriou@users.noreply.github.com>

--- Mathlib/Condensed/Light/Discrete.lean
+++ Mathlib/Condensed/Light/Discrete.lean
@@ -15,7 +15,7 @@ import Mathlib.Condensed.Light.Basic
 Given a well-behaved concrete category `C`, we define a functor `C ⥤ Condensed C` which associates
 to an object of `C` the corresponding "discrete" condensed object (see `Condensed.discrete`).
 
-In `Condensed.discrete_underlying_adj` we prove that this functor is left adjoint to the forgetful
+In `Condensed.discreteUnderlyingAdj` we prove that this functor is left adjoint to the forgetful
 functor from `Condensed C` to `C`.
 -/
 
@@ -23,7 +23,7 @@ universe u v w
 
 open CategoryTheory Limits Opposite GrothendieckTopology
 
-variable (C : Type w) [Category.{u} C] [HasSheafify (coherentTopology LightProfinite) C]
+variable (C : Type w) [Category.{u} C] [HasSheafify (coherentTopology LightProfinite.{u}) C]
 
 /--
 The discrete condensed object associated to an object of `C` is the constant sheaf at that object.
@@ -43,7 +43,7 @@ noncomputable def LightCondensed.underlying : LightCondensed.{u} C ⥤ C :=
 Discreteness is left adjoint to the forgetful functor. When `C` is `Type*`, this is analogous to
 `TopCat.adj₁ : TopCat.discrete ⊣ forget TopCat`.  
 -/
-noncomputable def LightCondensed.discrete_underlying_adj : discrete C ⊣ underlying C :=
+noncomputable def LightCondensed.discreteUnderlyingAdj : discrete C ⊣ underlying C :=
   constantSheafAdj _ _ terminalIsTerminal
 
 /-- A version of `LightCondensed.discrete` in the `LightCondSet` namespace -/
@@ -53,5 +53,5 @@ noncomputable abbrev LightCondSet.discrete := LightCondensed.discrete (Type u)
 noncomputable abbrev LightCondSet.underlying := LightCondensed.underlying (Type u)
 
 /-- A version of `LightCondensed.discrete_underlying_adj` in the `LightCondSet` namespace -/
-noncomputable abbrev LightCondSet.discrete_underlying_adj : discrete ⊣ underlying :=
+noncomputable abbrev LightCondSet.discreteUnderlyingAdj : discrete ⊣ underlying :=
   LightCondensed.discrete_underlying_adj _
