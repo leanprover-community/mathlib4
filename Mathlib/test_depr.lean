8553fb45ae0733efdab33fda59a3fa2e073b7986
adomani <adomani@gmail.com>
2024-06-05 22:01:49 +0200

    add CI steps with update_deprecations

--- Mathlib/test_depr.lean
+++ Mathlib/test_depr.lean
@@ -1,3 +1,13 @@
+/-
+Copyright (c) 2024 Damiano Testa. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Damiano Testa
+-/
+
+/-!
+A test file for `lake exe update_deprecations`.
+-/
+
 theorem not_useful (t : True) : True := t
 
 @[deprecated not_useful] theorem True.lhish (t : True) : True := t
--- Mathlib/test_depr_no_change.lean
+++ Mathlib/test_depr_no_change.lean
@@ -1,4 +1,14 @@
+/-
+Copyright (c) 2024 Damiano Testa. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Damiano Testa
+-/
+
 import Mathlib.test_depr
+
+/-!
+A test file for `lake exe update_deprecations`.
+-/
 
 open True
 example : True ∧ True := by
