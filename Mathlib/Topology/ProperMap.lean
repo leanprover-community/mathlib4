f28f7fe8cb7ab376ebe6ae0ced448dc6cf5e1101
Jireh Loreaux <loreaujy@gmail.com>
2024-06-05 11:55:09 -0500

    fix

--- Mathlib/Topology/ProperMap.lean
+++ Mathlib/Topology/ProperMap.lean
@@ -129,7 +129,7 @@ lemma IsProperMap.ultrafilter_le_nhds_of_tendsto (h : IsProperMap f) ⦃𝒰 : U
 /-- The composition of two proper maps is proper. -/
 lemma IsProperMap.comp (hf : IsProperMap f) (hg : IsProperMap g) :
     IsProperMap (g ∘ f) := by
-  refine ⟨by continuity, fun ℱ z h ↦ ?_⟩
+  refine ⟨by fun_prop, fun ℱ z h ↦ ?_⟩
   rw [mapClusterPt_comp] at h
   rcases hg.clusterPt_of_mapClusterPt h with ⟨y, rfl, hy⟩
   rcases hf.clusterPt_of_mapClusterPt hy with ⟨x, rfl, hx⟩
