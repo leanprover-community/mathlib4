fc85a3fb7152b720afbafd22285aae44a30d0248
Salvatore Mercuri <smercuri@ed.ac.uk>
2024-06-06 20:15:53 +0100

    Add the infinite adele ring of a number field

---
+++ Mathlib/NumberTheory/NumberField/InfiniteAdeleRing.lean
@@ -0,0 +1,67 @@
+/-
+Copyright (c) 2024 Salvatore Mercuri, María Inés de Frutos-Fernández. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Salvatore Mercuri, María Inés de Frutos-Fernández
+-/
+import Mathlib.NumberTheory.NumberField.Embeddings
+
+/-!
+# Infinite adele ring of a number field
+
+This file formalises the definition of the infinite adele ring of a number field `K` as the
+product of completions of `K` over its infinite places and we show that it is locally compact.
+Completions `v.completion` of a number field at an infinite place `v` are formalised in
+`Mathlib.NumberTheory.NumberField.Embeddings`.
+
+## Main definitions
+* `NumberField.infiniteAdeleRing K` of a number field `K` is defined as the product of
+the completions of `K` over its Archimedean places.
+* `NumberField.InfiniteAdeleRing.globalEmbedding K` is the embedding from `K` to
+`infiniteAdeleRing K` sending `x` to `(x)ᵥ`.
+
+## Main results
+* `NumberField.InfiniteAdeleRing.locallyCompactSpace K` : the infinite adele ring of `K` is a
+locally compact space.
+
+## Tags
+infinite adele ring, number field
+-/
+
+noncomputable section
+
+namespace NumberField
+
+variable (K : Type*) [Field K] [NumberField K] (v : InfinitePlace K)
+
+/-- The infinite adele ring of a number field. -/
+def infiniteAdeleRing := (v : InfinitePlace K) → v.completion
+
+namespace InfiniteAdeleRing
+
+section DerivedInstances
+
+instance : CommRing (infiniteAdeleRing K) := Pi.commRing
+
+instance : Inhabited (infiniteAdeleRing K) := ⟨0⟩
+
+instance : Nontrivial (infiniteAdeleRing K) := Pi.nontrivial
+
+end DerivedInstances
+
+instance topologicalSpace : TopologicalSpace (infiniteAdeleRing K) := Pi.topologicalSpace
+
+instance topologicalRing : TopologicalRing (infiniteAdeleRing K) := Pi.instTopologicalRing
+
+def globalEmbedding : K →+* infiniteAdeleRing K :=
+  Pi.ringHom (fun (v : InfinitePlace K) => InfinitePlace.Completion.coeRingHom v)
+
+theorem globalEmbedding_injective : Function.Injective (globalEmbedding K) :=
+  (globalEmbedding K).injective
+
+/-- The infinite adele ring is locally compact. -/
+theorem locallyCompactSpace : LocallyCompactSpace (infiniteAdeleRing K) :=
+  Pi.locallyCompactSpace_of_finite
+
+end InfiniteAdeleRing
+
+end NumberField
--- Mathlib/Topology/Algebra/UniformField.lean
+++ Mathlib/Topology/Algebra/UniformField.lean
@@ -177,6 +177,28 @@ instance : TopologicalDivisionRing (hat K) :=
 
 end Completion
 
+/-- The pullback of a completable topological field along a ring homomorphism
+is a completable topological field. -/
+theorem comap_completableTopField {α β : Type*}
+    [Field β] [Field α] {f : α →+* β} [b : UniformSpace β]
+    [@T0Space α (UniformSpace.comap f b).toTopologicalSpace] [CompletableTopField β] :
+    @CompletableTopField _ _ (UniformSpace.comap f b) := by
+  have h_ui : @UniformInducing _ _ (UniformSpace.comap f b) _ f := by
+    rw [@uniformInducing_iff_uniformSpace]
+  apply @CompletableTopField.mk _ _ (UniformSpace.comap f b)
+  intro F F_cau inf_F
+  have h_cau_i := @UniformInducing.cauchy_map_iff _ _ (UniformSpace.comap f b) _ _ h_ui
+  rw [← h_cau_i] at F_cau ⊢
+  have h_comm : (f ∘ fun x => x⁻¹) = (fun x => x⁻¹) ∘ f := by
+    ext; simp only [Function.comp_apply, map_inv₀, Subfield.coe_inv]
+  rw [Filter.map_comm h_comm]
+  apply CompletableTopField.nice _ F_cau
+  rw [← Filter.push_pull', ← map_zero f]
+  have h_inducing := (@UniformInducing.inducing _ _ (UniformSpace.comap f b) _ _ h_ui)
+  have h_nhds :=
+    @Inducing.nhds_eq_comap _ _ _ (UniformSpace.comap f b).toTopologicalSpace _ h_inducing
+  rw [← h_nhds, inf_F, Filter.map_bot]
+
 end UniformSpace
 
 variable (L : Type*) [Field L] [UniformSpace L] [CompletableTopField L]
