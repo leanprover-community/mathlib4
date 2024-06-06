b1515d0677805ffe4569fd44ec10bcb7db69781d
fbarroero <fbarroero@gmail.com>
2024-06-06 23:20:08 +0200

    moved part one

--- Mathlib/NumberTheory/SiegelsLemma.lean
+++ Mathlib/NumberTheory/SiegelsLemma.lean
@@ -18,7 +18,9 @@ coefficients.
 
  - `‖_‖ ` : Matrix.seminormedAddCommGroup is the sup norm, the maximum of the absolute values of
  the entries of the matrix
+## References
 
+See [M. Hindry and J. Silverman, Diophantine Geometry: an Introduction][hindrysilverman00] .
 -/
 
 /- We set ‖⬝‖ to be Matrix.seminormedAddCommGroup  -/
@@ -26,14 +28,12 @@ attribute [local instance] Matrix.seminormedAddCommGroup
 
 open Finset Matrix
 
-
 variable (m n a : ℕ) (A : Matrix (Fin m) (Fin n) ℤ) (v : Fin n → ℤ) (hn : m < n)
 (hm : 0 < m)
 
 --Some definitions and relative properties
 
 local notation3 "e" => m / ((n : ℝ ) - m) --exponent
-
 local notation3 "B" => Nat.floor (((n : ℝ) * ‖A‖) ^ e)
 -- B' is the vector with all components = B
 local notation3 "B'" => fun _ : Fin n => (B  : ℤ)
@@ -49,6 +49,30 @@ local notation3  "S" => Finset.Icc N P
 section preparation
 
 --In order to apply Pigeohole we need:  (1) ∀ v ∈  T, (A.mulVec v) ∈  S and (2) S.card < T.card
+
+--(1)
+
+private lemma image_T_subset_S : ∀ v ∈ T, (A.mulVec v) ∈ S := by
+  intro v hv
+  rw [Finset.mem_Icc] at hv
+  rw [Finset.mem_Icc]
+  rw [Matrix.mulVec_def] --unfolds def of MulVec
+  refine ⟨fun i ↦ ?_, fun i ↦ ?_⟩ --this gives 2 goals
+  all_goals simp only [mul_neg]
+  all_goals gcongr (∑ _ : Fin n, ?_) with j _ -- gets rid of sums
+  all_goals rw [← mul_comm (v j)] -- moves A i j to the right of the products
+  all_goals by_cases hsign : 0 ≤ A i j   --we have to distinguish cases: we have now 4 goals
+  ·  rw [negPart_eq_zero.2 hsign]
+     exact mul_nonneg (hv.1 j) hsign
+  ·  rw [not_le] at hsign
+     rw [negPart_eq_neg.2 (le_of_lt hsign)]
+     simp only [mul_neg, neg_neg]
+     exact mul_le_mul_of_nonpos_right (hv.2 j) (le_of_lt hsign)
+  ·  rw [posPart_eq_self.2  hsign]
+     exact mul_le_mul_of_nonneg_right (hv.2 j) hsign
+  ·  rw [not_le] at hsign
+     rw [posPart_eq_zero.2 (le_of_lt hsign)]
+     exact mul_nonpos_of_nonneg_of_nonpos (hv.1 j) (le_of_lt hsign)
 
 --Preparation for (2)
 
