549edc93dea2e89e01e1b1acca9dfb279e862427
Calle Sönne <calle.sonne@gmail.com>
2024-06-06 21:58:28 +0100

    lint fix

--- Mathlib/CategoryTheory/FiberedCategory/Fiber.lean
+++ Mathlib/CategoryTheory/FiberedCategory/Fiber.lean
@@ -9,37 +9,13 @@ import Mathlib.CategoryTheory.Functor.Const
 
 /-!
 
-# Fibers of functors
+# Fibers of a functors
 
-In this file we develop the theory of fibers of functors. Given a functor `p : 𝒳 ⥤ 𝒮`, we define
-the fiber categories `Fiber p S` for every `S : 𝒮` as follows:
+In this file we define, for a functor `p : 𝒳 ⥤ 𝒴` the fiber categories `Fiber p S` for every
+`S : 𝒮` as follows:
 - An object in `Fiber p S` is a pair `(a, ha)` where `a : 𝒳` and `ha : p.obj a = S`.
 - A morphism in `Fiber p S` is a morphism `φ : a ⟶ b` in 𝒳 such that `p.map φ = 𝟙 S`.
 
-We also introduce a typeclass `HasFibers` for a functor `p : 𝒳 ⥤ 𝒮`, consisting of:
-- A collection of categories `Fib S` for every `S` in `𝒮` (the fiber categories)
-- Functors `ι : Fib S ⥤ 𝒳` such that `ι ⋙ p = const (Fib S) S
-- The induced functor `Fib S ⥤ Fiber p S` is an equivalence.
-
-The reason for introducing this typeclass is that in practice, when working with fibered categories
-one often already has a collection of categories `Fib S` for every `S` that are equivalent to the
-fibers `Fiber p S`. One would then like to use these categories `Fib S` directly, instead of working
-through this equivalence of categories. By developing an API for the `HasFibers` typeclass, this
-will be possible.For example, we develop the following lemmas:
-- `HasFibersEssSurj` any object `a : 𝒳` lying over some `S : 𝒮` is isomorphic to the image of some
-  `a' : Fib S`
-- `HasFibersPullback` allows one to take pullbacks such that the codomain lies in one of the fibers
-  `Fib S`.
-- `HasFibersFactorization` (TODO: maybe call it `HasFibersInducedMap`, and the next `HasFibersFactorization`)
-- `fiber_factorization` any morphism in `𝒳` can be factored as a morphism in some fiber `Fib S`
-  followed by a pullback. (TODO: rename this lemma)
-
-Here is an example of when this typeclass is useful. Suppose we have a presheaf of types
-`F : 𝒮ᵒᵖ ⥤ Type _`. The associated fibered category then has objects `(S, a)` where `S : 𝒮` and `a`
-is an element of `F(S)`. The fiber category `Fiber p S` is then equivalent to the discrete category
-`Fib S` with objects `a` in `F(S)`. In this case, the `HasFibers` instance is given by the
-categories `F(S)` and the functor `ι` sends `a : F(S)` to `(S, a)` in the fibered category.
-See `Presheaf.lean` for more details.
 -/
 
 -- TODO: u, v should be flipped here?
@@ -57,7 +33,6 @@ namespace Fiber
 
 variable {p : 𝒳 ⥤ 𝒮} {S : 𝒮}
 
--- TODO: bundled? Coe?
 def Hom (a b : Fiber p S) := {φ : a.1 ⟶ b.1 // IsHomLift p (𝟙 S) φ}
 
 instance (a b : Fiber p S) (φ : Hom a b) : IsHomLift p (𝟙 S) φ.1 := φ.2
--- Mathlib/CategoryTheory/FiberedCategory/HasFibers.lean
+++ Mathlib/CategoryTheory/FiberedCategory/HasFibers.lean
@@ -11,32 +11,36 @@ import Mathlib.CategoryTheory.Functor.Const
 
 # Fibers of functors
 
-In this file we develop the theory of fibers of functors. Given a functor `p : 𝒳 ⥤ 𝒮`, we define
-the fiber categories `Fiber p S` for every `S : 𝒮` as follows:
-- An object in `Fiber p S` is a pair `(a, ha)` where `a : 𝒳` and `ha : p.obj a = S`.
-- A morphism in `Fiber p S` is a morphism `φ : a ⟶ b` in 𝒳 such that `p.map φ = 𝟙 S`.
-
-We also introduce a typeclass `HasFibers` for a functor `p : 𝒳 ⥤ 𝒮`, consisting of:
+In this file we introduce a typeclass `HasFibers` for a functor `p : 𝒳 ⥤ 𝒮`, consisting of:
 - A collection of categories `Fib S` for every `S` in `𝒮` (the fiber categories)
 - Functors `ι : Fib S ⥤ 𝒳` such that `ι ⋙ p = const (Fib S) S
 - The induced functor `Fib S ⥤ Fiber p S` is an equivalence.
 
-The reason for introducing this typeclass is that in practice, when working with fibered categories
-one often already has a collection of categories `Fib S` for every `S` that are equivalent to the fibers
-`Fiber p S`. One would then like to use these categories `Fib S` directly, instead of working through this
-equivalence of categories. By developing an API for the `HasFibers` typeclass, this will be possible.
-For example, we develop the following lemmas:
-- `HasFibersEssSurj` any object `a : 𝒳` lying over some `S : 𝒮` is isomorphic to the image of some `a' : Fib S`
-- `HasFibersPullback` allows one to take pullbacks such that the codomain lies in one of the fibers `Fib S`.
-- `HasFibersFactorization` (TODO: maybe call it `HasFibersInducedMap`, and the next `HasFibersFactorization`)
-- `fiber_factorization` any morphism in `𝒳` can be factored as a morphism in some fiber `Fib S` followed by
-  a pullback. (TODO: rename this lemma)
+The reason for introducing this typeclass is that in practice, when working with (pre)fibered
+categories one often already has a collection of categories `Fib S` for every `S` that are
+equivalent to the fibers `Fiber p S`. One would then like to use these categories `Fib S` directly,
+instead of working through this equivalence of categories. By developing an API for the `HasFibers`
+typeclass, this will be possible.
 
-Here is an example of when this typeclass is useful. Suppose we have a presheaf of types `F : 𝒮ᵒᵖ ⥤ Type _`.
-The associated fibered category then has objects `(S, a)` where `S : 𝒮` and `a` is an element of `F(S)`.
-The fiber category `Fiber p S` is then equivalent to the discrete category `Fib S` with objects `a` in `F(S)`.
-In this case, the `HasFibers` instance is given by the categories `F(S)` and the functor `ι` sends
-`a : F(S)` to `(S, a)` in the fibered category. See `Presheaf.lean` for more details.
+
+TODO: fix this comment based on new changes
+For example, we develop the following lemmas:
+- `HasFibersEssSurj` any object `a : 𝒳` lying over some `S : 𝒮` is isomorphic to the image of some
+`a' : Fib S`
+- `HasFibersPullback` allows one to take pullbacks such that the codomain lies in one of the fibers
+`Fib S`.
+- `HasFibersFactorization` (TODO: maybe call it `HasFibersInducedMap`, and the next
+`HasFibersFactorization`)
+- `fiber_factorization` any morphism in `𝒳` can be factored as a morphism in some fiber `Fib S`
+followed by a pullback. (TODO: rename this lemma)
+
+Here is an example of when this typeclass is useful. Suppose we have a presheaf of types
+`F : 𝒮ᵒᵖ ⥤ Type _`. The associated fibered category then has objects `(S, a)` where `S : 𝒮` and `a`
+is an element of `F(S)`. The fiber category `Fiber p S` is then equivalent to the discrete category
+`Fib S` with objects `a` in `F(S)`. In this case, the `HasFibers` instance is given by the
+categories `F(S)` and the functor `ι` sends `a : F(S)` to `(S, a)` in the fibered category. See
+`Presheaf.lean` for more details.
+
 -/
 
 -- TODO: port this to use `BasedCategory` later?
