50e58a77ec603fa326b6f46a655aed67c32b1f3d
Joël Riou <joel.riou@universite-paris-saclay.fr>
2024-06-06 22:10:11 +0200

    fixed imports

--- Mathlib/CategoryTheory/Shift/ShiftedHom.lean
+++ Mathlib/CategoryTheory/Shift/ShiftedHom.lean
@@ -3,7 +3,7 @@ Copyright (c) 2024 Joël Riou. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joël Riou
 -/
-import Mathlib.CategoryTheory.Shift.CommShift
+import Mathlib.CategoryTheory.Shift.Basic
 import Mathlib.CategoryTheory.Preadditive.AdditiveFunctor
 
 /-! Shifted morphisms
