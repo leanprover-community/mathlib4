34dcb933fcf333bba62b905b47caf70b77680d6c
adomani <adomani@gmail.com>
2024-06-07 15:23:52 +0200

    reformat output

--- scripts/technical-debt-counters.sh
+++ scripts/technical-debt-counters.sh
@@ -2,12 +2,12 @@
 
 # This script quantifies some of the technical debt in mathlib4.
 
-titles=( "Number of porting notes:"
-         "Number of backwards compatibility flags:"
-         "Number of adaptation notes:"
-         "Number of disabled simpNF lints:"
-         "Number of disabled deprecation lints:"
-         "Number of erw:")
+titles=( "porting notes"
+         "backwards compatibility flags"
+         "adaptation notes"
+         "disabled simpNF lints"
+         "disabled deprecation lints"
+         "erw")
 
 regexes=( "Porting note"
           "set_option.*backward"
@@ -16,20 +16,19 @@ regexes=( "Porting note"
           "set_option linter.deprecated false"
           "erw \[")
 
+printf '|Number|Type|\n|-:|:-|\n'
 for i in ${!titles[@]}; do
   if [ "${titles[$i]}" == "Number of porting notes:" ]
   then fl="-i"  # just for porting notes we ignore the case in the regex
   else fl="--"
   fi
-  printf '%s\n%s\n\n' "${titles[$i]}" "$(git grep "${fl}" "${regexes[$i]}" | wc -l)"
+  printf '| %s | %s |\n' "$(git grep "${fl}" "${regexes[$i]}" | wc -l)" "${titles[$i]}"
 done
 
 initFiles="$(git ls-files '**/Init/*.lean' | xargs wc -l)"
 
-printf '%s: %s\n\n' "Number of \`Init\` files" "$(printf '%s' "${initFiles}" | wc -l)"
+printf '|%s | %s |\n\n' "$(printf '%s' "${initFiles}" | wc -l)" "\`Init\` files"
 
-printf '```spoiler %s: %s\n%s\n```\n' "Number of LoC in 'Init' files" "$(
-    printf '%s\n' "${initFiles}" | grep total
-  )" "$(
+printf '```spoiler %s %s\n%s\n```\n' "$(printf '%s\n' "${initFiles}" | grep total)"  "LoC in 'Init' files" "$(
     printf '%s\n' "${initFiles}" | awk 'BEGIN{print"|LoC|File|\n|-:|-|"} {printf("|%s|%s|\n", $1, $2)}'
   )"
