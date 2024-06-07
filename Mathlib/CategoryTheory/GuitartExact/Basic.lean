531cd73ac8f58ea7fbcefcfabe943d52678532da
Joël Riou <37772949+joelriou@users.noreply.github.com>
2024-06-07 18:28:53 +0200

    Update Mathlib/CategoryTheory/GuitartExact/Basic.lean
    Co-authored-by: Andrew Yang <36414270+erdOne@users.noreply.github.com>

--- Mathlib/CategoryTheory/GuitartExact/Basic.lean
+++ Mathlib/CategoryTheory/GuitartExact/Basic.lean
@@ -228,8 +228,8 @@ instance [hw : w.GuitartExact] (X₂ : C₂) :
 
 /-- When the left and right functors of a 2-square are equivalences, and the natural
 transformation of the 2-square is an isomorphism, then the 2-square is Guitart exact. -/
-instance guitartExact_of_isEquivalence_of_isIso [L.IsEquivalence] [R.IsEquivalence] [IsIso w] :
-    GuitartExact w := by
+instance (priority := 100) guitartExact_of_isEquivalence_of_isIso
+    [L.IsEquivalence] [R.IsEquivalence] [IsIso w] : GuitartExact w := by
   rw [guitartExact_iff_initial]
   intro X₂
   have := StructuredArrow.isEquivalence_post X₂ T R
