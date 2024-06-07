a50bdca022f7a3263452b40d5212464d8e76f9d9
adomani <adomani@gmail.com>
2024-06-07 13:30:18 +0200

    uniformize script

--- scripts/technical-debt-counters.sh
+++ scripts/technical-debt-counters.sh
@@ -2,40 +2,27 @@
 
 # This script quantifies some of the technical debt in mathlib4.
 
-echo "Number of porting notes:"
-git grep -i "Porting note" | wc -l
+titles=( "Number of porting notes:"
+         "Number of backwards compatibility flags:"
+         "Number of adaptation notes:"
+         "Number of disabled simpNF lints:"
+         "Number of disabled deprecation lints:"
+         "Number of erw:")
 
-echo
+regexes=( "Porting note"
+          "set_option.*backward"
+          "adaptation_note"
+          "nolint simpNF"
+          "set_option linter.deprecated false"
+          "erw \[")
 
-echo "Number of backwards compatibility flags:"
-git grep "set_option.*backward" | wc -l
+for i in ${!titles[@]}; do
+  if [ "${titles[$i]}" == "Number of porting notes:" ]
+  then fl="-i"  # just for porting notes we ignore the case in the regex
+  else fl="--"
+  fi
+  printf '%s\n%s\n\n' "${titles[$i]}" "$(git grep "${fl}" "${regexes[$i]}" | wc -l)"
+done
 
-echo
-
-echo "Number of adaptation notes:"
-git grep "adaptation_note" | wc -l
-
-echo
-
-echo "Number of disabled simpNF lints:"
-git grep "nolint simpNF" | wc -l
-
-echo
-
-echo "Number of disabled deprecation lints:"
-git grep "set_option linter.deprecated false" | wc -l
-
-echo
-
-echo "Number of erw:"
-git grep "erw \[" | wc -l
-
-echo
-
-echo "Number of \`Init\` files:"
-git ls-files | grep "Init" | wc -l
-
-echo
-
-echo "Number of LoC in \`Init\` files:"
-git ls-files | grep "Init" | xargs wc -l | grep total
+printf '%s\n%s\n\n' "Number of \`Init\` files:"        "$(git ls-files '*Init*' | wc -l)"
+printf '%s\n%s\n\n' "Number of LoC in \`Init\` files:" "$(git ls-files '*Init*' | xargs wc -l | grep total)"
