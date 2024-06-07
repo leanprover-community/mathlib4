1bd936ee48cd2e3eb21a3c406899a1a05b4a13a0
101damnations <101damnations@github.com>
2024-06-07 17:37:31 +0100

    switching branch lol

---
+++ Mathlib/Algebra/Category/BialgebraCat/BimonEquivalence.lean
@@ -0,0 +1,7 @@
+import Mathlib.Algebra.Category.AlgebraCat.Symmetric
+import Mathlib.Algebra.Category.BialgebraCat.Basic
+import Mathlib.CategoryTheory.Monoidal.Bimon_
+
+universe v u v' u'
+
+variable (R : Type u) [CommRing R]
