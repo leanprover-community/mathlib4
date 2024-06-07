166ec9265106e986e9ef3c2a4be37c142974b4ca
dagurtomas <dagurtomas@gmail.com>
2024-06-07 22:04:43 +0000

    only equivalence stuff left

--- Mathlib/CategoryTheory/Sites/Equivalence.lean
+++ Mathlib/CategoryTheory/Sites/Equivalence.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Dagur Asgeirsson
 -/
 import Mathlib.CategoryTheory.Sites.InducedTopology
-import Mathlib.CategoryTheory.Sites.EpiMono
+import Mathlib.CategoryTheory.Sites.Whiskering
 /-!
 # Equivalences of sheaf categories
 
@@ -43,63 +43,6 @@ open Functor Limits GrothendieckTopology
 variable {C : Type*} [Category C] (J : GrothendieckTopology C)
 variable {D : Type*} [Category D] (K : GrothendieckTopology D) (e : C ≌ D)
 variable (A : Type*) [Category A]
-
-namespace Presheaf
-
-variable (H : C ⥤ D) {F G : Dᵒᵖ ⥤ A} (f : F ⟶ G)
-
-variable [ConcreteCategory A]
-
-lemma isLocallyInjective_whisker [H.IsCocontinuous J K] [IsLocallyInjective K f] :
-    IsLocallyInjective J (whiskerLeft H.op f) where
-  equalizerSieve_mem x y h := H.cover_lift J K (equalizerSieve_mem K f x y h)
-
-lemma isLocallyInjective_of_whisker (hH : CoverPreserving J K H)
-    [H.IsCoverDense K] [IsLocallyInjective J (whiskerLeft H.op f)] : IsLocallyInjective K f where
-  equalizerSieve_mem {X} a b h := by
-    apply K.transitive (H.is_cover_of_isCoverDense K X.unop)
-    intro Y g ⟨⟨Z, lift, map, fac⟩⟩
-    rw [← fac, Sieve.pullback_comp]
-    apply K.pullback_stable
-    refine K.superset_covering (Sieve.functorPullback_pushforward_le H _) ?_
-    refine K.superset_covering (Sieve.functorPushforward_monotone H _ ?_)
-      (hH.cover_preserve <| equalizerSieve_mem J (whiskerLeft H.op f)
-        ((forget A).map (F.map map.op) a) ((forget A).map (F.map map.op) b) ?_)
-    · intro W q hq
-      simpa using hq
-    · simp only [comp_obj, op_obj, whiskerLeft_app, Opposite.op_unop]
-      erw [NatTrans.naturality_apply, NatTrans.naturality_apply, h]
-
-lemma isLocallyInjective_iff_whisker (hH : CoverPreserving J K H) [H.IsCocontinuous J K]
-    [H.IsCoverDense K] : IsLocallyInjective K f ↔ IsLocallyInjective J (whiskerLeft H.op f) :=
-  ⟨fun _ ↦ isLocallyInjective_whisker J K A H f,
-    fun _ ↦ isLocallyInjective_of_whisker J K A H f hH⟩
-
-lemma isLocallySurjective_whisker [H.IsCocontinuous J K] [IsLocallySurjective K f] :
-    IsLocallySurjective J (whiskerLeft H.op f) where
-  imageSieve_mem a := H.cover_lift J K (imageSieve_mem K f a)
-
-lemma isLocallySurjective_of_whisker (hH : CoverPreserving J K H)
-    [H.IsCoverDense K] [IsLocallySurjective J (whiskerLeft H.op f)] : IsLocallySurjective K f where
-  imageSieve_mem {X} a := by
-    apply K.transitive (H.is_cover_of_isCoverDense K X)
-    intro Y g ⟨⟨Z, lift, map, fac⟩⟩
-    rw [← fac, Sieve.pullback_comp]
-    apply K.pullback_stable
-    have hh := hH.cover_preserve <|
-      imageSieve_mem J (whiskerLeft H.op f) ((forget A).map (G.map map.op) a)
-    refine K.superset_covering (Sieve.functorPullback_pushforward_le H _) ?_
-    refine K.superset_covering (Sieve.functorPushforward_monotone H _ ?_) hh
-    intro W q ⟨x, h⟩
-    simp only [Sieve.functorPullback_apply, Presieve.functorPullback_mem, Sieve.pullback_apply]
-    exact ⟨x, by simpa using h⟩
-
-lemma isLocallySurjective_iff_whisker (hH : CoverPreserving J K H) [H.IsCocontinuous J K]
-    [H.IsCoverDense K] : IsLocallySurjective K f ↔ IsLocallySurjective J (whiskerLeft H.op f) :=
-  ⟨fun _ ↦ isLocallySurjective_whisker J K A H f,
-    fun _ ↦ isLocallySurjective_of_whisker J K A H f hH⟩
-
-end Presheaf
 
 namespace Equivalence
 
