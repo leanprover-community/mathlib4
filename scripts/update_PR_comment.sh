d0eb08eb09b532964fbd1ef454e4177ec4a54126
adomani <adomani@gmail.com>
2024-06-07 08:46:14 +0200

    pass the PR number

--- scripts/update_PR_comment.sh
+++ scripts/update_PR_comment.sh
@@ -4,12 +4,14 @@
   message="${1}" #=$(python ./scripts/import-graph-report.py base.json head.json changed_files.txt)
   # the start of the message to locate it among all messages in the PR
   comment_init="${2}"
+  # the PR number
+  PR="${3}"
 
   data=$(jq -n --arg msg "$message" '{"body": $msg}')
   baseURL="https://api.github.com/repos/${GITHUB_REPOSITORY}/issues"
   method="POST"
   if [[ -n "$message" ]]; then
-      url="${baseURL}/${{ github.event.pull_request.number }}/comments"
+      url="${baseURL}/${PR}/comments"
       headers="Authorization: token ${GITHUB_TOKEN}"
       comment_id=$(curl -s -S -H "Content-Type: application/json" -H "$headers" "$url" |
         jq -r '.[] | select(.body | startswith("${comment_init}")) | .id' | head -1)
