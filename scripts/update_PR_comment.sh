a82ba9b063c8d244a23b435f60756ef42500e951
adomani <adomani@gmail.com>
2024-06-07 13:43:41 +0200

    Add docs

--- scripts/update_PR_comment.sh
+++ scripts/update_PR_comment.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+ : <<BASH_DOC_MODULE
+This script takes care of maintaining and updating the first message that starts with a given string in a PR.
+It takes 3 inputs:
+
+1. the new message;
+2. the beginning of the message, to identify whether to create a new or update an existing message;
+3. the PR number.
+BASH_DOC_MODULE
+
   # the text of the message that will replace the current one
   message="${1}"
   # the start of the message to locate it among all messages in the PR
