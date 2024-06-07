8619f28d6f74d2a9ee86a7223164a5879b8366a3
Kim Morrison <kim@tqft.net>
2024-06-07 17:47:47 +0000

    chore: deprecate unused Array/List function (#13587)

--- Mathlib/Data/Array/Basic.lean
+++ Mathlib/Data/Array/Basic.lean
@@ -5,8 +5,11 @@ Authors: Mario Carneiro
 -/
 import Batteries.Tactic.Alias
 
+-- TODO 2024-06-07: This has been upstreamed in https://github.com/leanprover/lean4/pull/4389
 attribute [simp] Array.toArrayAux_eq
 
-alias List.toArray_data := Array.data_toArray
+-- This file is not imported in Mathlib, and I would like to delete its contents after checking
+-- that is unused downstream.
+@[deprecated (since := "2024-06-07")] alias List.toArray_data := Array.data_toArray
 
 namespace Array
