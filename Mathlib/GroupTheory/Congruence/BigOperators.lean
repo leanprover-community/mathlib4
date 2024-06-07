efdb751f3acb40d681e9374793c84606e255c6b6
Jujian Zhang <jujian.zhang1998@outlook.com>
2024-06-07 17:53:17 +0100

    more lemmas

--- Mathlib/GroupTheory/Congruence/BigOperators.lean
+++ Mathlib/GroupTheory/Congruence/BigOperators.lean
@@ -6,13 +6,45 @@ Authors: Amelia Livingston
 
 import Mathlib.GroupTheory.Congruence.Basic
 import Mathlib.Algebra.BigOperators.Group.Finset
+import Mathlib.Algebra.BigOperators.Group.Multiset
+import Mathlib.Algebra.BigOperators.Group.List
 
 /-!
 # Interactions between `∑, ∏` and `(Add)Con`
 
 -/
 
+open BigOperators
+
 namespace Con
+
+/-- Multiplicative congruence relations preserve product indexed by a list. -/
+@[to_additive "Additive congruence relations preserve sum indexed by a list."]
+protected theorem list_prod {ι M : Type*} [CommMonoid M] (c : Con M) {l : List ι} {f g : ι → M}
+    (h : ∀ x ∈ l, c (f x) (g x)) :
+    c (l.map f).prod (l.map g).prod := by
+  induction l with
+  | nil =>
+    simpa only [List.map_nil, List.prod_nil] using c.refl 1
+  | cons x xs ih =>
+    rw [List.map_cons, List.map_cons, List.prod_cons, List.prod_cons]
+    refine c.mul (h _ (by simp)) <| ih fun k hk => h _ ?_
+    simp only [List.mem_cons]
+    tauto
+
+/-- Multiplicative congruence relations preserve product indexed by a multiset. -/
+@[to_additive "Additive congruence relations preserve sum indexed by a multiset."]
+protected theorem multiset_prod {ι M : Type*} [CommMonoid M] (c : Con M) {s : Multiset ι}
+    {f g : ι → M} (h : ∀ x ∈ s, c (f x) (g x)) :
+    c (s.map f).prod (s.map g).prod := by
+  induction s using Multiset.induction_on with
+  | empty =>
+    simpa only [Multiset.map_zero, Multiset.prod_zero] using c.refl 1
+  | cons x xs ih =>
+    rw [Multiset.map_cons, Multiset.map_cons, Multiset.prod_cons, Multiset.prod_cons]
+    refine c.mul (h _ (by simp)) <| ih fun k hk => h _ ?_
+    simp only [Multiset.mem_cons]
+    tauto
 
 /-- Multiplicative congruence relations preserve finite product. -/
 @[to_additive "Additive congruence relations preserve finite sum."]
