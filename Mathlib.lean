cce8437ae54afddaaf6bc9c0aadc6b6000326c8d
Joël Riou <joel.riou@universite-paris-saclay.fr>
2024-06-08 04:33:28 +0000

    feat: the derived category of an abelian category (#11806)
    The derived category of an abelian category is defined and it is shown that it is a triangulated category.
    
    
    
    Co-authored-by: Joël Riou <37772949+joelriou@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -287,6 +287,7 @@ import Mathlib.Algebra.Homology.BifunctorShift
 import Mathlib.Algebra.Homology.ComplexShape
 import Mathlib.Algebra.Homology.ComplexShapeSigns
 import Mathlib.Algebra.Homology.ConcreteCategory
+import Mathlib.Algebra.Homology.DerivedCategory.Basic
 import Mathlib.Algebra.Homology.DifferentialObject
 import Mathlib.Algebra.Homology.Embedding.Basic
 import Mathlib.Algebra.Homology.Exact
