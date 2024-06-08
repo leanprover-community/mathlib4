96fdc41c753c946c78b826c3f9de612d49bd24b6
Brendan Seamas Murphy <shamrockfrost@gmail.com>
2024-06-08 00:58:21 -0600

    Fix attribution
    Co-authored-by: Andrew Yang <36414270+erdOne@users.noreply.github.com>

--- Mathlib/Algebra/Polynomial/Module/AEval.lean
+++ Mathlib/Algebra/Polynomial/Module/AEval.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2022 Andrew Yang. All rights reserved.
+Copyright (c) 2022 Richard M. Hill. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Andrew Yang
+Authors: Richard M. Hill
 -/
 import Mathlib.Algebra.Polynomial.AlgebraMap
 import Mathlib.RingTheory.Finiteness
