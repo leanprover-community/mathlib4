7625cd03507338cf61574e990b76bba82451abcb
negiizhao <zhao.yu-yang@foxmail.com>
2024-06-09 06:58:26 +0800

    Update SingleObj.lean

--- Mathlib/CategoryTheory/Preadditive/SingleObj.lean
+++ Mathlib/CategoryTheory/Preadditive/SingleObj.lean
@@ -19,6 +19,7 @@ namespace CategoryTheory
 variable {α : Type*} [Ring α]
 
 instance : Preadditive (SingleObj α) where
+  homGroup := _
   add_comp _ _ _ f f' g := mul_add g f f'
   comp_add _ _ _ f g g' := add_mul g g' f
 
