c7e13d9442080b13d13adf357fc033328e1144ed
Yury G. Kudryashov <urkud@urkud.name>
2024-06-08 18:34:16 +0000

    chore(Data/Tree): split file (#13629)
    Move 3 definitions that depend on `Num` to a new file.

--- Mathlib/Data/Tree/Basic.lean
+++ Mathlib/Data/Tree/Basic.lean
@@ -3,11 +3,11 @@ Copyright (c) 2019 mathlib community. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro, Wojciech Nawrocki
 -/
-import Mathlib.Data.Num.Basic
-import Mathlib.Init.Data.Ordering.Basic
-import Mathlib.Init.Order.LinearOrder
-import Mathlib.Util.CompileInductive
 import Batteries.Data.RBMap.Basic
+import Mathlib.Init.Data.Nat.Notation
+import Mathlib.Mathport.Rename
+import Mathlib.Tactic.TypeStar
+import Mathlib.Util.CompileInductive
 
 #align_import data.tree from "leanprover-community/mathlib"@"ed989ff568099019c6533a4d94b27d852a5710d8"
 
@@ -59,37 +59,6 @@ def ofRBNode : RBNode α → Tree α
   | RBNode.node _color l key r => node key (ofRBNode l) (ofRBNode r)
 #align tree.of_rbnode Tree.ofRBNode
 
-/-- Finds the index of an element in the tree assuming the tree has been
-constructed according to the provided decidable order on its elements.
-If it hasn't, the result will be incorrect. If it has, but the element
-is not in the tree, returns none. -/
-def indexOf (lt : α → α → Prop) [DecidableRel lt] (x : α) : Tree α → Option PosNum
-  | nil => none
-  | node a t₁ t₂ =>
-    match cmpUsing lt x a with
-    | Ordering.lt => PosNum.bit0 <$> indexOf lt x t₁
-    | Ordering.eq => some PosNum.one
-    | Ordering.gt => PosNum.bit1 <$> indexOf lt x t₂
-#align tree.index_of Tree.indexOf
-
-/-- Retrieves an element uniquely determined by a `PosNum` from the tree,
-taking the following path to get to the element:
-- `bit0` - go to left child
-- `bit1` - go to right child
-- `PosNum.one` - retrieve from here -/
-def get : PosNum → Tree α → Option α
-  | _, nil => none
-  | PosNum.one, node a _t₁ _t₂ => some a
-  | PosNum.bit0 n, node _a t₁ _t₂ => t₁.get n
-  | PosNum.bit1 n, node _a _t₁ t₂ => t₂.get n
-#align tree.get Tree.get
-
-/-- Retrieves an element from the tree, or the provided default value
-if the index is invalid. See `Tree.get`. -/
-def getOrElse (n : PosNum) (t : Tree α) (v : α) : α :=
-  (t.get n).getD v
-#align tree.get_or_else Tree.getOrElse
-
 /-- Apply a function to each value in the tree.  This is the `map` function for the `Tree` functor.
 -/
 def map {β} (f : α → β) : Tree α → Tree β
@@ -128,11 +97,10 @@ theorem numLeaves_pos (x : Tree α) : 0 < x.numLeaves := by
 #align tree.num_leaves_pos Tree.numLeaves_pos
 
 theorem height_le_numNodes : ∀ x : Tree α, x.height ≤ x.numNodes
-  | nil => le_rfl
-  | node _ a b =>
-    Nat.succ_le_succ
-      (max_le (_root_.trans a.height_le_numNodes <| a.numNodes.le_add_right _)
-        (_root_.trans b.height_le_numNodes <| b.numNodes.le_add_left _))
+  | nil => Nat.le_refl _
+  | node _ a b => Nat.succ_le_succ <|
+    Nat.max_le.2 ⟨Nat.le_trans a.height_le_numNodes <| a.numNodes.le_add_right _,
+      Nat.le_trans b.height_le_numNodes <| b.numNodes.le_add_left _⟩
 #align tree.height_le_num_nodes Tree.height_le_numNodes
 
 /-- The left child of the tree, or `nil` if the tree is `nil` -/
