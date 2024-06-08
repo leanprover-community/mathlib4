c9b94cb07e895d31c7b9013286585506599c68c2
Christian Merten <christian@merten.dev>
2024-06-08 16:29:26 +0000

    feat(CategoryTheory/Galois): fundamental group is limit of automorphism groups (#12843)
    We show that the automorphism group of a fiber functor is isomorphic to the limit of the automorphism groups of all Galois objects.
    
    From this we deduce that the automorphism group of a fiber functor acts transitively on the fibers of connected objects. This is the last essential step towards showing that a fiber functor induces a fully faithful embedding into the category of finite `Aut F`-types.

--- Mathlib/CategoryTheory/Limits/Shapes/ConcreteCategory.lean
+++ Mathlib/CategoryTheory/Limits/Shapes/ConcreteCategory.lean
@@ -73,7 +73,7 @@ variable {J : Type w} (f : J → C) [HasProduct f] {D : Type t} [Category.{r} D]
 lemma Pi.map_ext (x y : F.obj (∏ᶜ f : C))
     (h : ∀ i, F.map (Pi.π f i) x = F.map (Pi.π f i) y) : x = y := by
   apply ConcreteCategory.injective_of_mono_of_preservesPullback (PreservesProduct.iso F f).hom
-  apply @Concrete.limit_ext.{w, r, t} D
+  apply @Concrete.limit_ext.{w, w, r, t} D
     _ _ (Discrete J) _ _ _ _ (piComparison F _ x) (piComparison F _ y)
   intro ⟨(j : J)⟩
   show ((forget D).map (piComparison F f) ≫ (forget D).map (limit.π _ _)) x =
