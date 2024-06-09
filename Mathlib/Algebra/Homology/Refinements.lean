18af13ed25b48ab8a07d122d4996612ce25ceb21
Joël Riou <37772949+joelriou@users.noreply.github.com>
2024-06-10 00:01:25 +0200

    Update Mathlib/Algebra/Homology/Refinements.lean
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Mathlib/Algebra/Homology/Refinements.lean
+++ Mathlib/Algebra/Homology/Refinements.lean
@@ -15,7 +15,7 @@ variable {C ι : Type*} [Category C] [Abelian C] {c : ComplexShape ι}
 namespace HomologicalComplex
 
 lemma eq_liftCycles_homologyπ_up_to_refinements {A : C} {i : ι} (γ : A ⟶ K.homology i)
-  (j : ι) (hj : c.next i = j) :
+    (j : ι) (hj : c.next i = j) :
     ∃ (A' : C) (π : A' ⟶ A) (_ : Epi π) (z : A' ⟶ K.X i) (hz : z ≫ K.d i j = 0),
       π ≫ γ = K.liftCycles z j hj hz ≫ K.homologyπ i := by
   subst hj
