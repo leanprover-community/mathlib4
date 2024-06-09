c894e1a4ee2c2ca3dae120c6fd21941cf558e638
Michael Rothgang <rothgami@math.hu-berlin.de>
2024-06-09 12:17:27 +0200

    A few more tests.

--- test/UnusedSetOptionIn.lean
+++ test/UnusedSetOptionIn.lean
@@ -8,7 +8,26 @@ note: this linter can be disabled with `set_option linter.unusedSetOptionIn fals
 set_option maxHeartbeats 2 in
 example : True := .intro
 
-set_option maxHeartbeats 1
-
+-- Test disabling the linter.
+set_option linter.unusedSetOptionIn false in
 set_option maxHeartbeats 2 in
 example : True := .intro
+
+set_option maxHeartbeats 1
+
+-- The linter doesn't act because of the low heartbeats count.
+set_option maxHeartbeats 2 in
+example : True := .intro
+
+set_option maxHeartbeats 20000
+
+def foo : True := by trivial
+
+-- XXX: why is this line not flagged as "unnecessary"?
+set_option genSizeOfSpec false in
+def foo' : True := by trivial
+
+-- This linter would generate a warning: suppressing it should not be considered unnecessary".
+#guard_msgs in
+set_option linter.unusedVariables false in
+lemma bar (h : Nat) : True := by trivial
