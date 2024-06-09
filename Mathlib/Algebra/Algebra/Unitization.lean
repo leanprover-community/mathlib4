fd50ca9d7229f6cbfa118ab5395dc55a0b431b2b
Frédéric Dupuis <dupuisf@iro.umontreal.ca>
2024-06-09 11:04:20 -0400

    Unitization: subtraction

--- Mathlib/Algebra/Algebra/Unitization.lean
+++ Mathlib/Algebra/Algebra/Unitization.lean
@@ -285,6 +285,11 @@ theorem inl_neg [Neg R] [AddGroup A] (r : R) : (inl (-r) : Unitization R A) = -i
 #align unitization.inl_neg Unitization.inl_neg
 
 @[simp]
+theorem inl_sub [AddGroup R] [AddGroup A] (r₁ r₂ : R) :
+    (inl (r₁ - r₂) : Unitization R A) = inl r₁ - inl r₂ :=
+  ext rfl (sub_zero 0).symm
+
+@[simp]
 theorem inl_smul [Monoid S] [AddMonoid A] [SMul S R] [DistribMulAction S A] (s : S) (r : R) :
     (inl (s • r) : Unitization R A) = s • inl r :=
   ext rfl (smul_zero s).symm
@@ -310,6 +315,10 @@ theorem inr_add [AddZeroClass R] [Add A] (m₁ m₂ : A) : (↑(m₁ + m₂) : U
 theorem inr_neg [AddGroup R] [Neg A] (m : A) : (↑(-m) : Unitization R A) = -m :=
   ext neg_zero.symm rfl
 #align unitization.coe_neg Unitization.inr_neg
+
+@[simp]
+theorem inr_sub [AddGroup R] [AddGroup A] (m₁ m₂ : A) : (↑(m₁ - m₂) : Unitization R A) = m₁ - m₂ :=
+  ext (sub_zero 0).symm rfl
 
 @[simp]
 theorem inr_smul [Zero R] [Zero S] [SMulWithZero S R] [SMul S A] (r : S) (m : A) :
