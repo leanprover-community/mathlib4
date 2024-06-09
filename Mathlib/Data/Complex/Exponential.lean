1db6a08210067a8964a0969c09f032fc98575469
Michael Rothgang <rothgami@math.hu-berlin.de>
2024-06-09 17:20:55 +0200

    chore: replace manual adaptation notes by #adaptation_note

--- Mathlib/Data/Complex/Exponential.lean
+++ Mathlib/Data/Complex/Exponential.lean
@@ -1725,9 +1725,9 @@ end Mathlib.Meta.Positivity
 
 namespace Complex
 
--- Adaptation note: nightly-2024-04-01
--- The simpNF linter now times out on this lemma.
--- See https://github.com/leanprover-community/mathlib4/issues/12230
+#adaptation_note /-- nightly-2024-04-01
+The simpNF linter now times out on this lemma.
+See https://github.com/leanprover-community/mathlib4/issues/12230 -/
 @[simp, nolint simpNF]
 theorem abs_cos_add_sin_mul_I (x : ℝ) : abs (cos x + sin x * I) = 1 := by
   have := Real.sin_sq_add_cos_sq x
