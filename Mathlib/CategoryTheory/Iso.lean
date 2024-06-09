c75978918deaa103ff4f7dcd889a761d7cf1c495
Joël Riou <joel.riou@universite-paris-saclay.fr>
2024-06-09 23:30:16 +0000

    feat(CategoryTheory/GradedObject): the triangle equality (#11687)
    In this PR, we obtain the lemma `GradedObject.mapBifunctor_triangle` which promotes a triangle identity involving functors to a triangle identity for the induced functors on graded objects. More simp lemmas for isomorphisms like `Iso.map_hom_inv_id_eval_app ` are also added in order to ease automation.
    
    
    
    Co-authored-by: Joël Riou <37772949+joelriou@users.noreply.github.com>

--- Mathlib/CategoryTheory/Iso.lean
+++ Mathlib/CategoryTheory/Iso.lean
@@ -578,6 +578,22 @@ theorem cancel_iso_inv_right_assoc {W X X' Y Z : C} (f : W ⟶ X) (g : X ⟶ Y) 
   simp only [← Category.assoc, cancel_mono]
 #align category_theory.iso.cancel_iso_inv_right_assoc CategoryTheory.Iso.cancel_iso_inv_right_assoc
 
+section
+
+variable {D E : Type*} [Category D] [Category E] {X Y : C} (e : X ≅ Y)
+
+@[reassoc (attr := simp)]
+lemma map_hom_inv_id (F : C ⥤ D) :
+    F.map e.hom ≫ F.map e.inv = 𝟙 _ := by
+  rw [← F.map_comp, e.hom_inv_id, F.map_id]
+
+@[reassoc (attr := simp)]
+lemma map_inv_hom_id (F : C ⥤ D) :
+    F.map e.inv ≫ F.map e.hom = 𝟙 _ := by
+  rw [← F.map_comp, e.inv_hom_id, F.map_id]
+
+end
+
 end Iso
 
 namespace Functor
@@ -592,8 +608,6 @@ variable [Category.{v₂} D]
 def mapIso (F : C ⥤ D) {X Y : C} (i : X ≅ Y) : F.obj X ≅ F.obj Y where
   hom := F.map i.hom
   inv := F.map i.inv
-  hom_inv_id := by rw [← map_comp, Iso.hom_inv_id, ← map_id]
-  inv_hom_id := by rw [← map_comp, Iso.inv_hom_id, ← map_id]
 #align category_theory.functor.map_iso CategoryTheory.Functor.mapIso
 #align category_theory.functor.map_iso_inv CategoryTheory.Functor.mapIso_inv
 #align category_theory.functor.map_iso_hom CategoryTheory.Functor.mapIso_hom
@@ -624,10 +638,12 @@ theorem map_inv (F : C ⥤ D) {X Y : C} (f : X ⟶ Y) [IsIso f] : F.map (inv f) 
   simp [← F.map_comp]
 #align category_theory.functor.map_inv CategoryTheory.Functor.map_inv
 
+@[reassoc]
 theorem map_hom_inv (F : C ⥤ D) {X Y : C} (f : X ⟶ Y) [IsIso f] :
     F.map f ≫ F.map (inv f) = 𝟙 (F.obj X) := by simp
 #align category_theory.functor.map_hom_inv CategoryTheory.Functor.map_hom_inv
 
+@[reassoc]
 theorem map_inv_hom (F : C ⥤ D) {X Y : C} (f : X ⟶ Y) [IsIso f] :
     F.map (inv f) ≫ F.map f = 𝟙 (F.obj Y) := by simp
 #align category_theory.functor.map_inv_hom CategoryTheory.Functor.map_inv_hom
