93d896d853c6a3c34cb0bf64dd9956be53b15c73
NirvanaC93 <nirvanac93@gmail.com>
2024-06-10 16:08:15 +0200

    Minor corretions

--- Mathlib/NumberTheory/Ostrowski.lean
+++ Mathlib/NumberTheory/Ostrowski.lean
@@ -14,48 +14,42 @@ import Mathlib.Analysis.Normed.Ring.Seminorm
 # Ostrowski’s Theorem
 
 The goal of this file is to prove Ostrowski’s Theorem which gives a list of all the nontrivial
-absolute values on a number field up to equivalence.
+absolute values on a number field up to equivalence. (TODO)
 
 ## References
 * https://kconrad.math.uconn.edu/blurbs/gradnumthy/ostrowskiQ.pdf
 * https://kconrad.math.uconn.edu/blurbs/gradnumthy/ostrowskinumbfield.pdf
+* J. W. S. Cassels, "Local fields", Cambridge University Press, 1986 https://doi.org/10.1017/CBO9781139171885
 
 ## Tags
 ring_norm, ostrowski
 -/
 
-noncomputable section
 namespace Rat.MulRingNorm
 open Int
 
 variable {f g : MulRingNorm ℚ}
 
-/-- Values of a multiplicative norm of the rationals are determined by the values on the
-integers. -/
-lemma NormRat_eq_on_Int_iff_eq_on_Nat : (∀ n : ℕ , f n = g n) ↔ (∀ n : ℤ , f n = g n) := by
-  refine' ⟨_, fun a n => a n⟩
-  intro h z
-  obtain ⟨n,rfl | rfl⟩ := eq_nat_or_neg z
+/-- Values of a multiplicative norm of the rationals coincide on ℕ if and only if they coincide
+on ℤ. -/
+lemma eq_on_Nat_iff_eq_on_Int : (∀ n : ℕ , f n = g n) ↔ (∀ n : ℤ , f n = g n) := by
+  refine ⟨fun h z ↦ ?_, fun a n ↦ a n⟩
+  obtain ⟨n , rfl | rfl⟩ := eq_nat_or_neg z
   · exact h n
-  · simp only [Int.cast_neg, Int.cast_natCast, map_neg_eq_map]
-    exact h n
+  · simp only [Int.cast_neg, Int.cast_natCast, map_neg_eq_map, h n]
 
 /-- Values of a multiplicative norm of the rationals are determined by the values on the natural
 numbers. -/
-lemma NormRat_eq_iff_eq_on_Nat : (∀ n : ℕ , f n = g n) ↔ f = g := by
-  refine' ⟨_, fun h n => congrFun (congrArg DFunLike.coe h) ↑n⟩
-  intro h
+lemma eq_on_Nat_iff_eq : (∀ n : ℕ , f n = g n) ↔ f = g := by
+  refine ⟨fun h ↦ ?_, fun h n ↦ congrFun (congrArg DFunLike.coe h) ↑n⟩
   ext z
-  rw [← Rat.num_div_den z, map_div₀, map_div₀, h, NormRat_eq_on_Int_iff_eq_on_Nat.mp h]
+  rw [← Rat.num_div_den z, map_div₀, map_div₀, h, eq_on_Nat_iff_eq_on_Int.mp h]
 
 /-- The equivalence class of a multiplicative norm on the rationals is determined by its values on
 the natural numbers. -/
-lemma NormRat_equiv_iff_equiv_on_Nat : (∃ c : ℝ, 0 < c ∧ (∀ n : ℕ , (f n)^c = g n)) ↔
+lemma equiv_iff_equiv_on_Nat : (∃ c : ℝ, 0 < c ∧ (∀ n : ℕ , (f n)^c = g n)) ↔
     f.equiv g := by
-    refine ⟨?_, fun ⟨c, hc, h⟩ ↦ ⟨c, ⟨hc, fun n ↦ by rw [← h]⟩⟩⟩
-    intro ⟨c, hc, h⟩
-    use c
-    refine ⟨hc, ?_⟩
+    refine ⟨fun ⟨c, hc, h⟩ ↦ ⟨c, ⟨hc, ?_⟩⟩, fun ⟨c, hc, h⟩ ↦ ⟨c, ⟨hc, fun n ↦ by rw [← h]⟩⟩⟩
     ext x
     rw [← Rat.num_div_den x, map_div₀, map_div₀, Real.div_rpow (apply_nonneg f _)
       (apply_nonneg f _), h x.den, ← MulRingNorm.apply_natAbs_eq,← MulRingNorm.apply_natAbs_eq,
