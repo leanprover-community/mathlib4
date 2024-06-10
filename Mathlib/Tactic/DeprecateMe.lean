66af185476ead54181a92342f37fd910c5bf1519
adomani <adomani@gmail.com>
2024-06-10 18:46:56 +0200

    remove unneeded import

--- Mathlib/Tactic/DeprecateMe.lean
+++ Mathlib/Tactic/DeprecateMe.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Damiano Testa
 -/
 
-import Batteries.Data.Array.Init.Lemmas
 import Mathlib.Lean.Expr.Basic
 import Mathlib.Tactic.Lemma
 
