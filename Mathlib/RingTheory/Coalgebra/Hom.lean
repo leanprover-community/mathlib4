5c842cdadc983e5465cc2a24b17e8f12e6caca77
Jujian Zhang <jujian.zhang1998@outlook.com>
2024-06-10 11:36:01 +0100

    long line

--- Mathlib/RingTheory/Coalgebra/Hom.lean
+++ Mathlib/RingTheory/Coalgebra/Hom.lean
@@ -302,7 +302,8 @@ variable {A B}
 If `φ : A → B` is a coalgebra map and `a = ∑ xᵢ ⊗ yᵢ`, then `φ a = ∑ φ xᵢ ⊗ φ yᵢ`
 -/
 @[simps]
-def _root_.CoalgebraStruct.Repr.induced {a : A} (repr : Repr R a) {F : Type*} [FunLike F A B] [CoalgHomClass F R A B]
+def _root_.CoalgebraStruct.Repr.induced {a : A} (repr : Repr R a)
+    {F : Type*} [FunLike F A B] [CoalgHomClass F R A B]
     (φ : F) : Repr R (φ a) where
   index := repr.index
   left := φ ∘ repr.left
