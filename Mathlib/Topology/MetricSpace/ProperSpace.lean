8ecf3628c9ee2cb3cfbdcaf49d61c8f116187461
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2024-06-10 10:01:12 +0000

    chore: move totallyBounded_Ixx lemmas up a bit (#13631)

--- Mathlib/Topology/MetricSpace/ProperSpace.lean
+++ Mathlib/Topology/MetricSpace/ProperSpace.lean
@@ -3,6 +3,7 @@ Copyright (c) 2018 Sébastien Gouëzel. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Sébastien Gouëzel
 -/
+import Mathlib.Topology.Algebra.Order.Compact
 import Mathlib.Topology.MetricSpace.PseudoMetric
 
 /-! ## Proper spaces
