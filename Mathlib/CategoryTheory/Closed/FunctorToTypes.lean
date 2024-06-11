04bcd9dc1681a80dc477da569dca8fd999f549c2
Jack McKoen <mckoen@ualberta.ca>
2024-06-10 20:36:11 -0600

    move file and add defs

--- Mathlib/CategoryTheory/Closed/FunctorToTypes.lean
+++ Mathlib/CategoryTheory/Closed/FunctorToTypes.lean
@@ -1,7 +1,20 @@
+/-
+Copyright (c) 2024 Jack McKoen. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Jack McKoen
+-/
 import Mathlib.CategoryTheory.Monoidal.FunctorCategory
 import Mathlib.CategoryTheory.Monoidal.Types.Basic
 import Mathlib.CategoryTheory.Closed.Cartesian
 import Mathlib.Tactic.ApplyFun
+
+/-!
+# Functors to Type are closed.
+
+Show that `C ⥤ Type max w v u` is cartesian closed for `C` a category in `Type u` with morphisms in
+`Type v`, where `u`, `v`, and `w` are arbitrary universes.
+-/
+
 
 namespace CategoryTheory
 
@@ -13,23 +26,31 @@ variable {C : Type u} [Category.{v} C]
 
 namespace FunctorToTypes
 
+/-- The ulift functor `(C ⥤ Type v) ⥤ C ⥤ Type max w v u` on functors to Type. -/
 def uliftFunctor : (C ⥤ Type v) ⥤ C ⥤ Type max w v u :=
   (whiskeringRight _ _ _).obj CategoryTheory.uliftFunctor.{max w v u}
 
+/-- the co-Yoneda embedding ulifted from `Type v` to `Type max w v u` -/
 def coyoneda {C : Type u} [Category.{v} C] : Cᵒᵖ ⥤ C ⥤ Type max w v u :=
     CategoryTheory.coyoneda ⋙ uliftFunctor.{w}
 
 noncomputable section
 
+/-- The internal hom of two functors `C ⥤ Type max w v u`. -/
 def ihom (F G : C ⥤ Type max w v u) : C ⥤ Type max w v u where
-  obj c := (coyoneda.obj (.op c)) ⊗ F ⟶ G
+  obj c := coyoneda.obj (.op c) ⊗ F ⟶ G
   map := fun f g ↦ coyoneda.map (.op f) ▷ F ≫ g
 
+/-- The right adjoint of `tensorLeft F`. -/
 def rightAdj (F : C ⥤ Type max w v u) : (C ⥤ Type max w v u) ⥤ C ⥤ Type max w v u where
   obj G := ihom F G
   map f := { app := fun _ h ↦ h ≫ f }
 
-def homEquiv_toFun_app {F G H : C ⥤ Type max w v u} (f : F ⊗ G ⟶ H) (c : C) (y : G.obj c) :
+variable {F G H : C ⥤ Type max w v u}
+
+/-- Given a morphism `F ⊗ G ⟶ H`, an object of `(c : C)` and an element of `G.obj c`, construct a
+morphism `coyoneda.obj (.op c) ⊗ F ⟶ G`. Used to construct a morphism `G ⟶ ihom F H`. -/
+def homEquiv_toFun_app (f : F ⊗ G ⟶ H) (c : C) (y : G.obj c) :
     (ihom F H).obj c where
   app := fun d ⟨g, x⟩ ↦ f.app d (x, G.map g.down y)
   naturality a b h := by
@@ -40,10 +61,11 @@ def homEquiv_toFun_app {F G H : C ⥤ Type max w v u} (f : F ⊗ G ⟶ H) (c : C
     aesop
 
 @[ext]
-lemma homEquiv_toFun_app_ext {F G : C ⥤ Type max w v u} {c : C} {f g : (ihom F G).obj c} :
+lemma homEquiv_toFun_app_ext {c : C} {f g : (ihom F G).obj c} :
     f.app = g.app → f = g := NatTrans.ext _ _
 
-def homEquiv_toFun {F G H : C ⥤ Type max w v u} (f : F ⊗ G ⟶ H) : G ⟶ ihom F H where
+/-- Given a morphism `F ⊗ G ⟶ H`, construct a morphism `G ⟶ ihom F H`. -/
+def homEquiv_toFun (f : F ⊗ G ⟶ H) : G ⟶ ihom F H where
   app := homEquiv_toFun_app f
   naturality c d g := by
     ext
@@ -51,10 +73,11 @@ def homEquiv_toFun {F G H : C ⥤ Type max w v u} (f : F ⊗ G ⟶ H) : G ⟶ ih
     aesop
 
 @[ext]
-lemma homEquiv_toFun_ext {F G H : C ⥤ Type max w v u} {f g : G ⟶ ihom F H} :
+lemma homEquiv_toFun_ext {f g : G ⟶ ihom F H} :
   f.app = g.app → f = g := NatTrans.ext _ _
 
-def homEquiv_invFun {F G H : C ⥤ Type max w v u} (f : G ⟶ ihom F H) : F ⊗ G ⟶ H where
+/-- Given a morphism `G ⟶ ihom F H`, construct a morphism `F ⊗ G ⟶ H`. -/
+def homEquiv_invFun (f : G ⟶ ihom F H) : F ⊗ G ⟶ H where
   app c x := (f.app c x.2).app c (Equiv.ulift.symm (𝟙 c), x.1)
   naturality c d g := by
     ext ⟨x, y⟩
@@ -66,10 +89,11 @@ def homEquiv_invFun {F G H : C ⥤ Type max w v u} (f : G ⟶ ihom F H) : F ⊗ 
     aesop
 
 @[ext]
-lemma homEquiv_invFun_ext {F G H : C ⥤ Type max w v u} {f g : F ⊗ G ⟶ H} :
+lemma homEquiv_invFun_ext {f g : F ⊗ G ⟶ H} :
   f.app = g.app → f = g := NatTrans.ext _ _
 
-def homEquiv (F G H : C ⥤ Type max w v u) : (F ⊗ G ⟶ H) ≃ (G ⟶ (rightAdj F).obj H) where
+/-- The bijection between morphisms `F ⊗ G ⟶ H` and morphisms `G ⟶ ihom F H`. -/
+def homEquiv (F G H : C ⥤ Type max w v u) : (F ⊗ G ⟶ H) ≃ (G ⟶ ihom F H) where
   toFun := homEquiv_toFun
   invFun := homEquiv_invFun
   left_inv _ := by ext ; dsimp only [homEquiv_invFun, homEquiv_toFun, homEquiv_toFun_app] ; aesop
@@ -80,6 +104,7 @@ def homEquiv (F G H : C ⥤ Type max w v u) : (F ⊗ G ⟶ H) ≃ (G ⟶ (rightA
     dsimp only [rightAdj, ihom, coyoneda, uliftFunctor] at b ⊢
     aesop
 
+/-- The adjunction `tensorLeft F ⊣ rightAdj F`. -/
 def adj (F : C ⥤ Type max w v u) : tensorLeft F ⊣ rightAdj F where
   homEquiv G H := homEquiv F G H
   unit := {
@@ -92,5 +117,21 @@ def adj (F : C ⥤ Type max w v u) : tensorLeft F ⊣ rightAdj F where
       rw [Eq.symm (FunctorToTypes.naturality G H f g.down y)]
   }
   counit := { app := fun G ↦ homEquiv_invFun (𝟙 _) }
+
+instance closed (F : C ⥤ Type max w v u) : Closed F where
+  rightAdj := rightAdj F
+  adj := adj F
+
+instance monoidalClosed : MonoidalClosed (C ⥤ Type max w v u) where
+  closed := inferInstance
+
+/-
+instance : Limits.HasFiniteProducts (C ⥤ Type max w v u) := sorry
+
+instance cartesianClosed : CartesianClosed (C ⥤ Type max w v u) :=
+  {
+  closed := _
+  }
+-/
 
 end
