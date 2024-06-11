b5bc5a0b5d2283d689703c719c71c0a362d9acb4
adomani <adomani@gmail.com>
2024-06-11 12:33:11 +0200

    Use meaningful names, clarify docs

--- test/DeprecateMe.lean
+++ test/DeprecateMe.lean
@@ -4,22 +4,22 @@ import Mathlib.Tactic
 
 /--
 info: * Pairings:
-#[(`good_mul, aDeprecatable_mul), (`good_add, aDeprecatable_add)]
+#[(`new_name_mul, mul_easy_deprecated), (`new_name_add, add_easy_deprecated)]
 
 Try this:
 /-- I also have a doc-string -/
-@[to_additive "As do I"]
-theorem good_mul : True :=
+@[to_additive "With its additive doc-string"]
+theorem new_name_mul : True :=
   .intro
 
 @[deprecated (since := "YYYY-MM-DD")]
-alias aDeprecatable_mul := good_mul
+alias mul_easy_deprecated := new_name_mul
 
 @[deprecated (since := "YYYY-MM-DD")]
-alias aDeprecatable_add := good_add
+alias add_easy_deprecated := new_name_add
 -/
 #guard_msgs in
-deprecate to good_mul good_add "YYYY-MM-DD"
+deprecate to new_name_mul new_name_add "YYYY-MM-DD"
 /-- I also have a doc-string -/
-@[to_additive "As do I"]
-theorem aDeprecatable_mul : True := .intro
+@[to_additive "With its additive doc-string"]
+theorem mul_easy_deprecated : True := .intro
