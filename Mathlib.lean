db9e4407e3d32fcb3f4839b9555cc66020ce1091
Jack McKoen <mckoen@ualberta.ca>
2024-06-10 21:07:59 -0600

    add to mathlib

--- Mathlib.lean
+++ Mathlib.lean
@@ -1213,6 +1213,7 @@ import Mathlib.CategoryTheory.ChosenFiniteProducts.FunctorCategory
 import Mathlib.CategoryTheory.Closed.Cartesian
 import Mathlib.CategoryTheory.Closed.Functor
 import Mathlib.CategoryTheory.Closed.FunctorCategory
+import Mathlib.CategoryTheory.Closed.FunctorToTypes
 import Mathlib.CategoryTheory.Closed.Ideal
 import Mathlib.CategoryTheory.Closed.Monoidal
 import Mathlib.CategoryTheory.Closed.Types
