a8840f5aa03a5040d87a7a3dd8f1c3b14e463768
Christian Merten <christian@merten.dev>
2024-06-11 16:57:36 +0200

    fix

--- Mathlib/RingTheory/RingHom/FiniteType.lean
+++ Mathlib/RingTheory/RingHom/FiniteType.lean
@@ -3,6 +3,7 @@ Copyright (c) 2021 Andrew Yang. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Andrew Yang
 -/
+import Mathlib.RingTheory.FiniteStability
 import Mathlib.RingTheory.LocalProperties
 import Mathlib.RingTheory.Localization.InvSubmonoid
 
