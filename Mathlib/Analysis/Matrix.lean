0fe37d6605c8a0977f5a799a10101c09438fbc9f
Fabrizio Barroero <fbarroero@gmail.com>
2024-06-11 13:06:21 +0000

    feat(Analysis/Matrix): lemmas about norms of matrix, preparation for Siegel's lemma (#13470)
    This PR contains `norm_eq_sup_sup_nnnorm (A : Matrix m n α) : ‖A‖ = Finset.sup Finset.univ fun i ↦ Finset.sup Finset.univ fun j ↦ ‖A i j‖₊`. It is used in the proof of Siegel's lemma.
    
    Some preliminary results used in the proof of Siegel's lemma can be found at #13487
    
    [Zulip thread](https://leanprover.zulipchat.com/#narrow/stream/287929-mathlib4/topic/Siegel's.20Lemma)
    
    Co-authored-by: Laura Capuano and Amos Turchet @amosturchet

--- Mathlib/Analysis/Matrix.lean
+++ Mathlib/Analysis/Matrix.lean
@@ -79,6 +79,11 @@ attribute [local instance] Matrix.seminormedAddCommGroup
 -- Porting note (#10756): new theorem (along with all the uses of this lemma below)
 theorem norm_def (A : Matrix m n α) : ‖A‖ = ‖fun i j => A i j‖ := rfl
 
+/-- The norm of a matrix is the sup of the sup of the nnnorm of the entries -/
+lemma norm_eq_sup_sup_nnnorm (A : Matrix m n α) :
+    ‖A‖ = Finset.sup Finset.univ fun i ↦ Finset.sup Finset.univ fun j ↦ ‖A i j‖₊ := by
+  simp_rw [Matrix.norm_def, Pi.norm_def, Pi.nnnorm_def]
+
 -- Porting note (#10756): new theorem (along with all the uses of this lemma below)
 theorem nnnorm_def (A : Matrix m n α) : ‖A‖₊ = ‖fun i j => A i j‖₊ := rfl
 
