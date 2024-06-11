908ad69d7511a44a685ff831c7bbbd2973eb3eb1
Junyan Xu <junyanxu.math@gmail.com>
2024-06-11 14:48:02 +0000

    `le_add_iff_lt_of_dvd_sub`

--- Mathlib/Algebra/Order/Ring/Int.lean
+++ Mathlib/Algebra/Order/Ring/Int.lean
@@ -71,7 +71,7 @@ lemma two_le_iff_pos_of_even {m : ℤ} (even : Even m) : 2 ≤ m ↔ 0 < m :=
   le_iff_zero_lt_of_dvd (by decide) (even_iff_two_dvd.mp even)
 
 lemma add_two_le_iff_lt_of_even_sub {m n : ℤ} (even : Even (n - m)) : m + 2 ≤ n ↔ m < n := by
-  rw [add_comm, ← le_sub_iff_add_le, ← sub_pos, two_le_iff_pos_of_even even]
+  rw [add_comm]; exact le_add_iff_lt_of_dvd_sub (by decide) (even_iff_two_dvd.mp even)
 
 end Int
 
