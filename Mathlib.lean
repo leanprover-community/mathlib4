2b917d2adaaf284a4250db04c989391b8089a211
Geoffrey Irving <irving@naml.us>
2024-06-12 22:49:16 +0100

    refactor: Move analyticGroupoid to a new AnalyticManifold.lean
    I'm about to rewrite the definition of `analyticGroupoid` and define
    `AnalyticManifold` on top of it, in
    https://github.com/leanprover-community/mathlib4/pull/10853. @grunweg
    suggested splitting out the move of `analyticGroupoid` into a separate
    commit, so that the diff is easier. It used to be in
    `SmoothManifoldWithCorners.lean`, but this makes no sense as nothing
    downstream of that file uses `analyticGroupoid`.

--- Mathlib.lean
+++ Mathlib.lean
@@ -2439,6 +2439,7 @@ import Mathlib.Geometry.Manifold.Algebra.LieGroup
 import Mathlib.Geometry.Manifold.Algebra.Monoid
 import Mathlib.Geometry.Manifold.Algebra.SmoothFunctions
 import Mathlib.Geometry.Manifold.Algebra.Structures
+import Mathlib.Geometry.Manifold.AnalyticManifold
 import Mathlib.Geometry.Manifold.BumpFunction
 import Mathlib.Geometry.Manifold.ChartedSpace
 import Mathlib.Geometry.Manifold.Complex
