7466596819d65c19d79a532ff88fcdbee101a3c6
David Kurniadi Angdinata <dka31@cantab.ac.uk>
2024-06-12 23:57:25 +0100

    Swap even and odd order

--- Mathlib/NumberTheory/EllipticDivisibilitySequence.lean
+++ Mathlib/NumberTheory/EllipticDivisibilitySequence.lean
@@ -229,21 +229,36 @@ lemma preNormEDS_three : preNormEDS b c d 3 = c := by
 lemma preNormEDS_four : preNormEDS b c d 4 = d := by
   erw [preNormEDS_ofNat, preNormEDS'_four]
 
-lemma preNormEDS_odd' (m : ℕ) : preNormEDS b c d (2 * (m + 2) + 1) =
-    preNormEDS b c d (m + 4) * preNormEDS b c d (m + 2) ^ 3 * (if Even m then b else 1) -
-      preNormEDS b c d (m + 1) * preNormEDS b c d (m + 3) ^ 3 * (if Even m then 1 else b) := by
-  repeat erw [preNormEDS_ofNat]
-  exact preNormEDS'_odd ..
-
 lemma preNormEDS_even' (m : ℕ) : preNormEDS b c d (2 * (m + 3)) =
     preNormEDS b c d (m + 2) ^ 2 * preNormEDS b c d (m + 3) * preNormEDS b c d (m + 5) -
       preNormEDS b c d (m + 1) * preNormEDS b c d (m + 3) * preNormEDS b c d (m + 4) ^ 2 := by
   repeat erw [preNormEDS_ofNat]
   exact preNormEDS'_even ..
 
+lemma preNormEDS_odd' (m : ℕ) : preNormEDS b c d (2 * (m + 2) + 1) =
+    preNormEDS b c d (m + 4) * preNormEDS b c d (m + 2) ^ 3 * (if Even m then b else 1) -
+      preNormEDS b c d (m + 1) * preNormEDS b c d (m + 3) ^ 3 * (if Even m then 1 else b) := by
+  repeat erw [preNormEDS_ofNat]
+  exact preNormEDS'_odd ..
+
 @[simp]
 lemma preNormEDS_neg (n : ℤ) : preNormEDS b c d (-n) = -preNormEDS b c d n := by
   rw [preNormEDS, Int.sign_neg, Int.cast_neg, neg_mul, Int.natAbs_neg, preNormEDS]
+
+lemma preNormEDS_even (m : ℤ) : preNormEDS b c d (2 * m) =
+    preNormEDS b c d (m - 1) ^ 2 * preNormEDS b c d m * preNormEDS b c d (m + 2) -
+      preNormEDS b c d (m - 2) * preNormEDS b c d m * preNormEDS b c d (m + 1) ^ 2 := by
+  induction m using Int.negInduction with
+  | nat m =>
+    rcases m with _ | _ | _ | m; simp; simp; simp
+    simp only [Int.natCast_add, Nat.cast_one]
+    rw [Int.add_sub_cancel, show (m : ℤ) + 1 + 1 + 1 = m + 1 + 2 by rfl, Int.add_sub_cancel]
+    exact preNormEDS_even' ..
+  | neg h m =>
+    simp_rw [show 2 * -(m : ℤ) = -(2 * m) by omega, show -(m : ℤ) - 1 = -(m + 1) by omega,
+      show -(m : ℤ) + 2 = -(m - 2) by omega, show -(m : ℤ) - 2 = -(m + 2) by omega,
+      show -(m : ℤ) + 1 = -(m - 1) by omega, preNormEDS_neg, h m]
+    ring1
 
 lemma preNormEDS_odd (m : ℤ) : preNormEDS b c d (2 * m + 1) =
     preNormEDS b c d (m + 2) * preNormEDS b c d m ^ 3 * (if Even m then b else 1) -
@@ -259,21 +274,6 @@ lemma preNormEDS_odd (m : ℤ) : preNormEDS b c d (2 * m + 1) =
     simp_rw [Int.natCast_add, Nat.cast_one, show 2 * -(m + 1 : ℤ) + 1 = -(2 * m + 1) by rfl,
       show -(m + 1 : ℤ) + 2 = -(m - 1) by omega, show -(m + 1 : ℤ) - 1 = -(m + 2) by rfl,
       show -(m + 1 : ℤ) + 1 = -m by omega, preNormEDS_neg, even_neg, Int.even_add_one, ite_not, h m]
-    ring1
-
-lemma preNormEDS_even (m : ℤ) : preNormEDS b c d (2 * m) =
-    preNormEDS b c d (m - 1) ^ 2 * preNormEDS b c d m * preNormEDS b c d (m + 2) -
-      preNormEDS b c d (m - 2) * preNormEDS b c d m * preNormEDS b c d (m + 1) ^ 2 := by
-  induction m using Int.negInduction with
-  | nat m =>
-    rcases m with _ | _ | _ | m; simp; simp; simp
-    simp only [Int.natCast_add, Nat.cast_one]
-    rw [Int.add_sub_cancel, show (m : ℤ) + 1 + 1 + 1 = m + 1 + 2 by rfl, Int.add_sub_cancel]
-    exact preNormEDS_even' ..
-  | neg h m =>
-    simp_rw [show 2 * -(m : ℤ) = -(2 * m) by omega, show -(m : ℤ) - 1 = -(m + 1) by omega,
-      show -(m : ℤ) + 2 = -(m - 2) by omega, show -(m : ℤ) - 2 = -(m + 2) by omega,
-      show -(m : ℤ) + 1 = -(m - 1) by omega, preNormEDS_neg, h m]
     ring1
 
 end PreNormEDS
@@ -312,13 +312,6 @@ lemma normEDS_three : normEDS b c d 3 = c := by
 lemma normEDS_four : normEDS b c d 4 = d * b := by
   erw [normEDS_ofNat, preNormEDS'_four, if_pos <| by decide]
 
-lemma normEDS_odd' (m : ℕ) : normEDS b c d (2 * (m + 2) + 1) =
-    normEDS b c d (m + 4) * normEDS b c d (m + 2) ^ 3 -
-      normEDS b c d (m + 1) * normEDS b c d (m + 3) ^ 3 := by
-  repeat erw [normEDS_ofNat]
-  simp_rw [preNormEDS'_odd, if_neg (m + 2).not_even_two_mul_add_one, Nat.even_add_one, ite_not]
-  split_ifs <;> ring1
-
 lemma normEDS_even' (m : ℕ) : normEDS b c d (2 * (m + 3)) * b =
     normEDS b c d (m + 2) ^ 2 * normEDS b c d (m + 3) * normEDS b c d (m + 5) -
       normEDS b c d (m + 1) * normEDS b c d (m + 3) * normEDS b c d (m + 4) ^ 2 := by
@@ -326,17 +319,16 @@ lemma normEDS_even' (m : ℕ) : normEDS b c d (2 * (m + 3)) * b =
   simp only [preNormEDS'_even, if_pos <| even_two_mul _, Nat.even_add_one, ite_not]
   split_ifs <;> ring1
 
+lemma normEDS_odd' (m : ℕ) : normEDS b c d (2 * (m + 2) + 1) =
+    normEDS b c d (m + 4) * normEDS b c d (m + 2) ^ 3 -
+      normEDS b c d (m + 1) * normEDS b c d (m + 3) ^ 3 := by
+  repeat erw [normEDS_ofNat]
+  simp_rw [preNormEDS'_odd, if_neg (m + 2).not_even_two_mul_add_one, Nat.even_add_one, ite_not]
+  split_ifs <;> ring1
+
 @[simp]
 lemma normEDS_neg (n : ℤ) : normEDS b c d (-n) = -normEDS b c d n := by
   simp only [normEDS, preNormEDS_neg, neg_mul, even_neg]
-
-lemma normEDS_odd (m : ℤ) : normEDS b c d (2 * m + 1) =
-    normEDS b c d (m + 2) * normEDS b c d m ^ 3 -
-      normEDS b c d (m - 1) * normEDS b c d (m + 1) ^ 3 := by
-  simp only [normEDS, preNormEDS_odd, if_neg m.not_even_two_mul_add_one]
-  conv_lhs => rw [← @one_pow R _ 4, ← ite_pow, ← ite_pow]
-  simp only [show m + 2 = m + 1 + 1 by ring1, Int.even_add_one, Int.even_sub_one, ite_not]
-  ring1
 
 lemma normEDS_even (m : ℤ) : normEDS b c d (2 * m) * b =
     normEDS b c d (m - 1) ^ 2 * normEDS b c d m * normEDS b c d (m + 2) -
@@ -348,6 +340,14 @@ lemma normEDS_even (m : ℤ) : normEDS b c d (2 * m) * b =
     ring1
   · simp only [if_neg hm]
     ring1
+
+lemma normEDS_odd (m : ℤ) : normEDS b c d (2 * m + 1) =
+    normEDS b c d (m + 2) * normEDS b c d m ^ 3 -
+      normEDS b c d (m - 1) * normEDS b c d (m + 1) ^ 3 := by
+  simp only [normEDS, preNormEDS_odd, if_neg m.not_even_two_mul_add_one]
+  conv_lhs => rw [← @one_pow R _ 4, ← ite_pow, ← ite_pow]
+  simp only [show m + 2 = m + 1 + 1 by ring1, Int.even_add_one, Int.even_sub_one, ite_not]
+  ring1
 
 end NormEDS
 
