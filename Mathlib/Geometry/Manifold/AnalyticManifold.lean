dfcd09900feae32202075273aae99b269a66e3ee
Geoffrey Irving <irving@naml.us>
2024-06-13 21:55:11 +0100

    Fix copyright

--- Mathlib/Geometry/Manifold/AnalyticManifold.lean
+++ Mathlib/Geometry/Manifold/AnalyticManifold.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2024 Geoffrey Irving. All rights reserved.
+Copyright (c) 2023 Michael Lee. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Geoffrey Irving
+Authors: Michael Lee
 -/
 import Mathlib.Analysis.Analytic.Composition
 import Mathlib.Analysis.Analytic.Linear
