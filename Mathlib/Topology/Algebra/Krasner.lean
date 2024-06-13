ab0a631f4707d1247957dfc0f1323f2834ef475e
jjdishere <emailboxofjjd@163.com>
2024-06-13 12:00:03 +0200

    Create Krasner.lean

---
+++ Mathlib/Topology/Algebra/Krasner.lean
@@ -0,0 +1,7 @@
+
+/-!
+# Krasner's Lemma
+In this file, we define the type class for field extensions that Krasner's lemma holds and prove that for extensions between complete non-archimediean fields, the Krasner's lemma holds.
+
+#
+-/
