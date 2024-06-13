89dc101849ee3b136208592f5f53512ec213566c
adomani <adomani@gmail.com>
2024-06-13 13:16:39 +0200

    lint and silence

--- Mathlib/Tactic/DeprSinceAction.lean
+++ Mathlib/Tactic/DeprSinceAction.lean
@@ -14,7 +14,8 @@ This file contains the code to perform the auto-replacements of `deprecated` dec
 Running `lake exe update_deprecations` assumes that there is a working cache and
 uses the information from deprecations to automatically substitute deprecated declarations.
 
-The script handles namespacing, replacing a possibly non-fully-qualified, deprecated name with the fully-qualified non-deprecated name.
+The script handles namespacing, replacing a possibly non-fully-qualified, deprecated name with the
+fully-qualified non-deprecated name.
 
 It also handles dot-notation.
 
@@ -41,42 +42,47 @@ def List.getCommon {α} [BEq α] : List α → List α → List α × List α ×
     else
       (common, l::left, r::right)
 
-/-- `splitWithNamespace fullName dotNot` returns `(common, declName, (termName, rightComponents))`, where
+/-- `splitWithNamespace fullName dotNot` returns `(common, declName, (termName, rightComponents))`,
+where
 * `fullName = declName ++ common`;
 * `dotNot = termName ++ common ++ rightComponents`
 
-and `common` is as large as possible, subject to the constraint that `rightComponents` does *not* contain
-a segment equal to the last segment of `fullName`.
+and `common` is as large as possible, subject to the constraint that `rightComponents` does *not*
+contain a segment equal to the last segment of `fullName`.
 
 For example, with input `Nat.Prime.ne_two` and `hp.ne_two.symm` it returns
 `(["ne_two"], ["Nat", "Prime"], (["hp"], ["symm"]))`.
 Assuming the restriction on the right-most occurrence mentioned above, we can therefore deduce that
 * the type of `hp` is `Nat.Prime`, i.e. `hp : Nat.Prime`;
 * the lemma `Nat.Prime.ne_two` exists;
-* the type of `hp.ne_two` is something that has a `.symm` in its namespace, i.e. `hp.ne_two : xx` and `xx.symm` exists
+* the type of `hp.ne_two` is something that has a `.symm` in its namespace, i.e. `hp.ne_two : xx`
+  and `xx.symm` exists
   (in this case, `xx = Ne`).
 -/
-def splitWithNamespace (fullName dotNot : String) : List String × List String × (List String × List String) :=
+def splitWithNamespace (fullName dotNot : String) :
+    List String × List String × (List String × List String) :=
   let fns := (fullName.splitOn ".").reverse
   let dotns := (dotNot.splitOn ".").reverse
   let tail := fns.headD ""
-  --if tail == "" then none else
   let rightMostCommon := dotns.dropWhile (· != tail)  -- consider using matches further left as well
-  --if rightMostCommon == [] then none else
   let (common, fns', rm') := (List.getCommon fns) rightMostCommon
   (common.reverse, fns'.reverse, (rm'.reverse, (dotns.takeWhile (· != tail)).reverse))
 
-/-- `mIntercalate l` similar to `".".intercalate l`, except that it returns `[]` if the input list if `[]` and
+/-- `mIntercalate l` similar to `".".intercalate l`, except that
+it returns `[]` if the input list if `[]` and
 it returns the singleton list `[".".intercalate l]` otherwise. -/
 def mIntercalate (l : List String) : List String :=
   match ".".intercalate l with
     | "" => []
     | str => [str]
 
-/-- `recombineNamespace fullName dotNot newName` returns a candidate for "fixing" a deprecation in a namespace.
+/-- `recombineNamespace fullName dotNot newName` returns a candidate for "fixing" a deprecation
+in a namespace.
 * `dotNot` is a string representing dot-notation being used and involving a deprecated name
-* `fullName` is the fully-qualified string associated to the name that is deprecated and appears in `dotNot`
-* `newName` is the fully-qualified string associated to the name that is the non-deprecated version of `fullName`
+* `fullName` is the fully-qualified string associated to the name that is deprecated and
+  appears in `dotNot`
+* `newName` is the fully-qualified string associated to the name that is the non-deprecated version
+  of `fullName`
 
 It returns `none` if the final segment of `fullName` does not appear in `dotName`.
 -/
@@ -94,13 +100,6 @@ def recombineNamespace (fullName dotNot newName : String) : Option String :=
   if termName == [] then newName else
   ".".intercalate <| beginning ++ (mIntercalate rightComponents)
 
-def findFirstEnd (fullName s : String) : Option String :=
-  match (fullName.splitOn ".").getLast! with
-    | last =>
-      match s.splitOn last with
-        | h :: _ :: _ => h ++ last
-        | _ => none
-
 /--
 `ReplData` is the data extracted from the warning emitted by `lake build` for a deprecated name:
 |`ReplData.` | Description                                     |
@@ -111,45 +110,56 @@ def findFirstEnd (fullName s : String) : Option String :=
 | `col`      | the column where the replacement should happen  |
 -/
 structure ReplData :=
+  /--`fullName` is the fully qualified, deprecated name -/
   fullName : String
+  /--`newName` is the fully qualified, new name -/
   newName  : String
+  /--`line` is the line number -/
   line     : Nat
+  /--`col` is the column number -/
   col      : Nat
   deriving BEq, Inhabited
 
 /-- `ReplData.newLine r` returns the new line obtained by performing the substitution,
 taking into account the deprecation.
-If no substitution is necessary, then it returns `none`: this is an indication that something went wrong. -/
+If no substitution is necessary, then it returns `none`: this is an indication that something went
+wrong. -/
 def ReplData.newLine (r : ReplData) (lines : Array String) : Option String :=
   let line := lines.getD (r.line - 1) ""
   line.take r.col ++ " (since := \"2024-06-12\")" ++ line.drop r.col
 
-/-- We put the lexicographic order on the pairs `(line, col)` of `ReplData`, placing first positions that happen later.
+/-- We put the lexicographic order on the pairs `(line, col)` of `ReplData`,
+placing first positions that happen later.
 Most substitutions work on a line-by-line basis, so this is not necessary for "line information".
-However, in order to preserve column information, it is easier to perform the replacements on any given line from
-right to left.
-Since in the future some replacements may span several lines (or remove some of them), working backwards on the lines
-seems reasonable as well. -/
+However, in order to preserve column information, it is easier to perform the replacements on any
+given line from right to left.
+Since in the future some replacements may span several lines (or remove some of them),
+working backwards on the lines seems reasonable as well. -/
 instance : LT ReplData where
   lt a b := (b.line < a.line) || (a.line == b.line && b.col < a.col)
 
-theorem ReplData.lt_iff {a b : ReplData} : a < b ↔ (b.line < a.line) || (a.line == b.line && b.col < a.col) := by rfl
+theorem ReplData.lt_iff {a b : ReplData} :
+    a < b ↔ (b.line < a.line) || (a.line == b.line && b.col < a.col) := by
+  rfl
 
 instance : DecidableRel (LT.lt (α := ReplData)) := fun _ _ => decidable_of_iff' _ ReplData.lt_iff
 
 /-- `substitutions lines dat` takes as input the array `lines` of strings and the "instructions"
 `dat : Array ReplData`.
-The elements of `dat` contain the `(line, col)` information of the start of each `(deprecatedName, newName)` pair.
+The elements of `dat` contain the `(line, col)` information of the start of each
+`(deprecatedName, newName)` pair.
 
-For each replacement instruction, if the substring of `lines[line]!` starting at `col` is "compatible" with being
-a `deprecatedName`, then `substitutions` replaces `deprecatedName` with a possible use of `newName`.
+For each replacement instruction, if the substring of `lines[line]!` starting at `col` is
+"compatible" with being a `deprecatedName`, then `substitutions` replaces `deprecatedName`
+with a possible use of `newName`.
 Otherwise, it leaves the string unchanged.
 
 Once all the instructions have been parsed, `substitutions` returns a count of the number of
 successful substitutions, the number of unsuccessful substitutions and the array of strings
 incorporating all the substitutions.
 -/
-def substitutions (lines : Array String) (dat : Array ReplData) : (Nat × Nat) × Array String := Id.run do
+def substitutions (lines : Array String) (dat : Array ReplData) : (Nat × Nat) × Array String :=
+  Id.run do
   let mut new := lines
   let mut replaced := 0
   let mut unreplaced := 0
@@ -177,7 +187,8 @@ def getBuild (mods : Array String := #[]) : IO String := do
   let build ← IO.Process.output { cmd := "lake", args := #["build", "--no-build"] ++ mods }
   if build.exitCode != 0 then
     IO.println s!"`lake build --no-build` failed: the oleans may be out of date oleans. \
-                Try running `lake build{mods.foldl (init := "") fun x y => s!"{x} {y}"}` or `lake exe cache get` first"
+                Try running `lake build{mods.foldl (init := "") fun x y => s!"{x} {y}"}` or \
+                `lake exe cache get` first"
     return default
   return build.stdout
 
@@ -212,7 +223,8 @@ syntax "warning:" build ":" num ":" num ": `" ident
   "` has been deprecated, use `" ident "` instead" : build
 
 /-- a `deprecated` with a missing `since` -/
-syntax "warning:" build ":" num ":" num ": After here, please add (since := " str ") or whatever date is appropriate `⟨"num "," num "⟩`" : build
+syntax "warning:" build ":" num ":" num ": After here, please add (since := " str
+  ") or whatever date is appropriate `⟨"num "," num "⟩`" : build
 
 end build_syntax
 
@@ -231,10 +243,10 @@ def toFile : TSyntax `build → System.FilePath
 
 section elabs
 
-/-- `FromLinter` is the `HashMap` storing information about corrections that can be read off from the output of
-`lake build`.
-Each `System.FilePath` contains an array of `ReplData` containing information about `(deprecatedName, newName)` and
-the `(line, col)` pairs with the start of the deprecation.
+/-- `FromLinter` is the `HashMap` storing information about corrections that can be read off
+from the output of `lake build`.
+Each `System.FilePath` contains an array of `ReplData` containing information about
+`(deprecatedName, newName)` and the `(line, col)` pairs with the start of the deprecation.
 -/
 abbrev FromLinter := HashMap System.FilePath (Array ReplData)
 
@@ -247,7 +259,8 @@ def getFromLinter (fl : FromLinter) : TSyntax `build → FromLinter
                             col      := f.getNat }
     let fil := toFile fil
     fl.insert fil <| ((fl.find? fil).getD #[]).push rd
-  | `(build| warning: $fil:build: $_ : $_ : After here, please add (since := $_) or whatever date is appropriate `⟨$line, $col⟩`) =>
+  | `(build| warning: $fil:build:$_ :$_ : After here, please add (since := $_
+        ) or whatever date is appropriate `⟨$line, $col⟩`) =>
     let rd : ReplData :=  { fullName := default
                             newName  := default
                             line     := line.getNat
@@ -257,12 +270,6 @@ def getFromLinter (fl : FromLinter) : TSyntax `build → FromLinter
     fl.insert fil <| ((fl.find? fil).getD #[]).push rd
   | _ => fl
 open UpdateDeprecations
-
---run_cmd
---  let stx ← `(build| warning: ././././Mathlib/NumberTheory/SumFourSquares.lean:98:13: After here, please add (since := "2024-06-13") or whatever date is appropriate `⟨98, 44⟩`)
---  let s := getFromLinter {} ⟨stx⟩
---  let xx := s.toList[0]!.2[0]!
---  logInfo s!"{(xx.line, xx.col)}"
 
 /-- Parse the output of `lake build` and perform the relevant substitutions. -/
 elab bds:build* tk:"Build completed successfully." : command => do
@@ -284,7 +291,8 @@ elab bds:build* tk:"Build completed successfully." : command => do
     if noFiles == 0 then m!"No modifications needed"
     else if modifiedFiles.toArray.all (fun (_, _, x) => x == 0) then
       let totalModifications := modifiedFiles.fold (fun a _ (x, _) => a + x) 0
-      let toMo := m!"{totalModifications} modification" ++ if totalModifications == 1 then m!"" else "s"
+      let toMo := m!"{totalModifications} modification" ++
+        if totalModifications == 1 then m!"" else "s"
       let moFi := m!" across {noFiles} file" ++ if noFiles == 1 then m!"" else "s"
       toMo ++ moFi ++ ", all successful"
     else
@@ -308,7 +316,8 @@ def updateDeprecationsCLI (args : Parsed) : IO UInt32 := do
   if buildOutput.isEmpty then return 1
   Lean.initSearchPath (← Lean.findSysroot)
   -- create the environment with `import Mathlib.Tactic.DeprSinceAction`
-  let env : Environment ← importModules (leakEnv := true) #[{module := `Mathlib.Tactic.DeprSinceAction}] {}
+  let env : Environment
+    ← importModules (leakEnv := true) #[{module := `Mathlib.Tactic.DeprSinceAction}] {}
   -- process the `lake build` output, catching messages
   let (_, msgLog) ← Lean.Elab.process buildOutput env {}
   let exitCode := ← match msgLog.unreported.toArray with
@@ -331,7 +340,8 @@ def updateDeprecations : Cli.Cmd := `[Cli|
   You can run this on some modules only, using the optional `--mods`-flag: running\n\n\
   lake exe update_deprecations --mods One.Two.Three,Dd.Ee.Ff\n\n\
   only updates the deprecations in `One.Two.Three` and `Dd.Ee.Ff`. \
-  Note that you should provide a comma-separated list of module names, with no spaces between them. \
+  Note that you should provide a comma-separated list of module names, with no spaces between \
+  them. \
   As a convenience, the script tries to parse *paths* instead of *module names*: \
   passing\n\n\
   lake exe update_deprecations --mods One/Two/Three.lean,Dd.Ee.Ff\n\n\
--- test/DeprecateNoSince.lean
+++ test/DeprecateNoSince.lean
@@ -1,17 +1,27 @@
 import Mathlib.Tactic.Linter.DeprecateNoSince
 
+set_option linter.deprecateNoSince false
+
+/--
+warning: After here, please add (since := "2024-06-13") or whatever date is appropriate `⟨11, 44⟩`
+note: this linter can be disabled with `set_option linter.deprecateNoSince false`
+-/
+#guard_msgs in
+set_option linter.deprecateNoSince true in
 /-- βk𝕜⟨⟩-/@[simp, deprecated Nat "wh `Nat`"] theorem oh1 : True := .intro
 
 /--
-warning: After here, please add (since := "2024-06-13") or whatever date is appropriate `⟨11, 22⟩`
+warning: After here, please add (since := "2024-06-13") or whatever date is appropriate `⟨19, 22⟩`
 note: this linter can be disabled with `set_option linter.deprecateNoSince false`
 -/
 #guard_msgs in
+set_option linter.deprecateNoSince true in
 @[simp, deprecated Nat] theorem oh2 : True := .intro
 
 /--
-warning: After here, please add (since := "2024-06-13") or whatever date is appropriate `⟨18, 18⟩`
+warning: After here, please add (since := "2024-06-13") or whatever date is appropriate `⟨27, 18⟩`
 note: this linter can be disabled with `set_option linter.deprecateNoSince false`
 -/
 #guard_msgs in
+set_option linter.deprecateNoSince true in
 @[simp, deprecated] theorem oh3 : True := .intro
