66848ea2e6ce39ab6a238c6817bcfb884854080a
adomani <adomani@gmail.com>
2024-06-13 18:37:43 +0200

    simplify linter, add test

---
+++ test/RefineLinter.lean
@@ -0,0 +1,14 @@
+import Mathlib.Tactic.Linter.RefineLinter
+
+set_option linter.refine false
+/--
+warning: Please, use `refine` instead of `refine'`!
+note: this linter can be disabled with `set_option linter.refine false`
+---
+warning: Please, use `refine` instead of `refine'`!
+note: this linter can be disabled with `set_option linter.refine false`
+-/
+#guard_msgs in
+set_option linter.refine true in
+example : True := by
+  refine' (by refine' .intro)
