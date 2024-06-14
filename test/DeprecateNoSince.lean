57f3dc8c22af29ffc02e37538915a51cd9393779
adomani <adomani@gmail.com>
2024-06-14 20:46:55 +0200

    date change

--- test/DeprecateNoSince.lean
+++ test/DeprecateNoSince.lean
@@ -3,7 +3,7 @@ import Mathlib.Tactic.Linter.DeprecateNoSince
 set_option linter.deprecateNoSince false
 
 /--
-warning: After here, please add (since := "2024-06-13") or whatever date is appropriate `⟨11, 44⟩`
+warning: After here, please add (since := "2024-06-14") or whatever date is appropriate `⟨11, 44⟩`
 note: this linter can be disabled with `set_option linter.deprecateNoSince false`
 -/
 #guard_msgs in
@@ -11,7 +11,7 @@ set_option linter.deprecateNoSince true in
 /-- βk𝕜⟨⟩-/@[simp, deprecated Nat "wh `Nat`"] theorem oh1 : True := .intro
 
 /--
-warning: After here, please add (since := "2024-06-13") or whatever date is appropriate `⟨19, 22⟩`
+warning: After here, please add (since := "2024-06-14") or whatever date is appropriate `⟨19, 22⟩`
 note: this linter can be disabled with `set_option linter.deprecateNoSince false`
 -/
 #guard_msgs in
@@ -19,7 +19,7 @@ set_option linter.deprecateNoSince true in
 @[simp, deprecated Nat] theorem oh2 : True := .intro
 
 /--
-warning: After here, please add (since := "2024-06-13") or whatever date is appropriate `⟨27, 18⟩`
+warning: After here, please add (since := "2024-06-14") or whatever date is appropriate `⟨27, 18⟩`
 note: this linter can be disabled with `set_option linter.deprecateNoSince false`
 -/
 #guard_msgs in
