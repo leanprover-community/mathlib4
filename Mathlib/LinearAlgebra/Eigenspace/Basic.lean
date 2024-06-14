1e978da287df1bed80088210146260651acfac7d
Jon Bannon <jbannon@siena.edu>
2024-06-14 10:32:01 -0400

    Trying to insert diagoonal spectrum section

--- Mathlib/LinearAlgebra/Eigenspace/Basic.lean
+++ Mathlib/LinearAlgebra/Eigenspace/Basic.lean
@@ -578,3 +578,54 @@ lemma map_add_of_iInf_genEigenspace_ne_bot_of_commute [NoZeroSMulDivisors R M]
 end End
 
 end Module
+
+section SpectrumDiagonal
+
+variable {R : Type*} [Field R] {n : Type*} [DecidableEq n][Fintype n]
+
+open Module.End
+
+/--  Standard basis vectors are eigenvectors of any associated diagonal linear operator. -/
+lemma Matrix.hasEigenvector_toLin'_diagonal (d : n → R) (i : n) :
+    Module.End.HasEigenvector (Matrix.toLin' (diagonal d)) (d i) (Pi.basisFun R n i) := by
+  constructor
+  · rw [mem_eigenspace_iff]
+    ext j
+    simp only [diagonal, Pi.basisFun_apply, toLin'_apply, mulVec_stdBasis_apply, transpose_apply,
+      of_apply, Pi.smul_apply, LinearMap.stdBasis_apply', smul_eq_mul, mul_ite, mul_one, mul_zero]
+    split_ifs
+    all_goals simp_all
+  · rw [Function.ne_iff]; simp
+
+/-- Eigenvalues of a diagonal linear operator are the diagonal entries. -/
+lemma Matrix.hasEigenvalue_toLin'_diagonal_iff (d : n → R) {μ : R} :
+    HasEigenvalue (toLin' (diagonal d)) μ ↔ ∃ i, d i = μ := by
+  have (i : n) : HasEigenvalue (toLin' (diagonal d)) (d i) := by
+    exact hasEigenvalue_of_hasEigenvector <| hasEigenvector_toLin'_diagonal d i
+  constructor
+  · contrapose!
+    intro hμ h_eig
+    have h_iSup : ⨆ μ ∈ Set.range d, eigenspace (toLin' (diagonal d)) μ = ⊤ := by
+      rw [eq_top_iff, ← (Pi.basisFun R n).span_eq, Submodule.span_le]
+      rintro - ⟨i, rfl⟩
+      simp only [SetLike.mem_coe]
+      apply Submodule.mem_iSup_of_mem (d i)
+      apply Submodule.mem_iSup_of_mem ⟨i, rfl⟩
+      rw [mem_eigenspace_iff]
+      exact (hasEigenvector_toLin'_diagonal d i).apply_eq_smul
+    have hμ_not_mem : μ ∉ Set.range d := by simpa using fun i ↦ (hμ i)
+    have := eigenspaces_independent (toLin' (diagonal d)) |>.disjoint_biSup hμ_not_mem
+    rw [h_iSup, disjoint_top] at this
+    exact h_eig this
+  · rintro ⟨i, rfl⟩
+    exact this i
+
+/-- The spectrum of the diagonal operator is the range of the diagonal viewed as a function. -/
+lemma Matrix.spectrum_diagonal (d : n → R) :
+    spectrum R (diagonal d) = Set.range d := by
+  ext μ
+  rw [← AlgEquiv.spectrum_eq (Matrix.toLinAlgEquiv <| Pi.basisFun R n),
+    ← hasEigenvalue_iff_mem_spectrum, Set.mem_range]
+  exact Matrix.hasEigenvalue_toLin'_diagonal_iff d
+
+end SpectrumDiagonal
