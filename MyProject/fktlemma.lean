63b8b3cc33a09a7dd34168102b251b091edbc7c9
Pietro Lavino <pietrolavino@dhcp-128-189-91-13.ubcsecure.wireless.ubc.ca>
2024-06-13 18:18:00 -0700

    Add fktlemma.lean contains fktlemma theorems from mathlib

---
+++ MyProject/fktlemma.lean
@@ -0,0 +1,9 @@
+
+import Mathlib.Analysis.Subadditive
+
+--fkt lemma is fully contained in lean
+--and is described by the followinf theorems
+
+#check Subadditive --definition
+#check Subadditive.lim
+#check Subadditive.tendsto_lim
