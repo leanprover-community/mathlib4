4e76c3eb87125cc8666edb45ea84609fe9d25467
David Loeffler <d.loeffler.01@cantab.net>
2024-06-16 11:51:22 +0200

    Update L1Space.lean

--- Mathlib/MeasureTheory/Function/L1Space.lean
+++ Mathlib/MeasureTheory/Function/L1Space.lean
@@ -713,7 +713,7 @@ lemma integrable_left_of_integrable_add_of_nonneg {f g : α → ℝ}
     (h_int : Integrable (f + g) μ) : Integrable f μ := by
   refine h_int.mono' h_meas ?_
   filter_upwards [hf, hg] with a haf hag
-  exact (Real.norm_of_nonneg haf).symm ▸ (le_add_iff_nonneg_right _).mpr hag
+  exact (Real.norm_of_nonneg haf).symm ▸ le_add_of_nonneg_right hag
 
 lemma integrable_right_of_integrable_add_of_nonneg {f g : α → ℝ}
     (h_meas : AEStronglyMeasurable f μ) (hf : 0 ≤ᵐ[μ] f) (hg : 0 ≤ᵐ[μ] g)
