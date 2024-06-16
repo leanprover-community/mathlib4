c7cbd911e03998c124faa4e656eb368793de8c91
Joël Riou <joel.riou@universite-paris-saclay.fr>
2024-06-16 17:05:22 +0200

    fixed coding style

--- Mathlib/CategoryTheory/Triangulated/Opposite.lean
+++ Mathlib/CategoryTheory/Triangulated/Opposite.lean
@@ -426,8 +426,8 @@ open Pretriangulated.Opposite Pretriangulated
 variable {C}
 
 lemma map_distinguished_op_exact [HasShift C ℤ] [HasZeroObject C] [Preadditive C]
-  [∀ (n : ℤ), (shiftFunctor C n).Additive]
-  [Pretriangulated C]{A : Type*} [Category A] [Abelian A] (F : Cᵒᵖ ⥤ A)
+    [∀ (n : ℤ), (shiftFunctor C n).Additive]
+    [Pretriangulated C]{A : Type*} [Category A] [Abelian A] (F : Cᵒᵖ ⥤ A)
     [F.PreservesZeroMorphisms] [F.IsHomological] (T : Triangle C) (hT : T ∈ distTriang C) :
     ((shortComplexOfDistTriangle T hT).op.map F).Exact :=
   F.map_distinguished_exact _ (op_distinguished T hT)
