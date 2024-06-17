95d704d1e5369813a1d51f60a92a40f918bdee2d
Junyan Xu <junyanxumath@gmail.com>
2024-06-17 15:26:35 -0400

    more Bivariate lemmas

--- Mathlib/Algebra/Polynomial/Bivariate.lean
+++ Mathlib/Algebra/Polynomial/Bivariate.lean
@@ -26,7 +26,7 @@ namespace Polynomial
 
 noncomputable section
 
-variable {R : Type*}
+variable {R S : Type*}
 
 /-- `evalEval x y p` is the evaluation `p(x,y)` of a two-variable polynomial `p : R[X][Y]`. -/
 abbrev evalEval [Semiring R] (x y : R) (p : R[X][Y]) : R := eval x (eval (C y) p)
@@ -41,7 +41,32 @@ abbrev CC [Semiring R] (r : R) : R[X][Y] := C (C r)
 lemma coe_algebraMap_eq_CC [CommSemiring R] : algebraMap R R[X][Y] = CC (R := R) := rfl
 lemma coe_evalEvalRingHom [CommSemiring R] (x y : R) : evalEvalRingHom x y = evalEval x y := rfl
 
-variable {S} [CommSemiring R] [CommSemiring S]
+lemma evalEvalRingHom_eq [CommSemiring R] (x : R) :
+    evalEvalRingHom x = eval₂RingHom (evalRingHom x) := by
+  ext <;> simp
+
+lemma eval₂_evalRingHom [CommSemiring R] (x y : R) (p : R[X][Y]) :
+    eval₂ (evalRingHom x) y p = evalEval x y p := by
+  rw [← coe_evalEvalRingHom, evalEvalRingHom_eq]; rfl
+
+section
+
+variable [Semiring R] [Semiring S] (f : R →+* S) (p : R[X][Y]) (q : R[X])
+
+lemma map_mapRingHom_eval_map : (p.map <| mapRingHom f).eval (q.map f) = (p.eval q).map f := by
+  rw [eval_map, ← coe_mapRingHom, eval₂_hom]
+
+lemma map_mapRingHom_eval_map_eval (r : R) :
+    ((p.map <| mapRingHom f).eval <| q.map f).eval (f r) = f ((p.eval q).eval r) := by
+  rw [map_mapRingHom_eval_map, eval_map, eval₂_hom]
+
+lemma map_mapRingHom_evalEval (x y : R) :
+    (p.map <| mapRingHom f).evalEval (f x) (f y) = f (p.evalEval x y) := by
+  rw [evalEval, ← map_mapRingHom_eval_map_eval, map_C]
+
+end
+
+variable [CommSemiring R] [CommSemiring S]
 
 /-- Two equivalent ways to express the evaluation of a bivariate polynomial over `R`
 at a point in the affine plane over an `R`-algebra `S`. -/
@@ -59,7 +84,7 @@ lemma eval_C_X_comp_eval₂_map_C_X :
       .id _ := by
   ext <;> simp
 
-/-- Since `R[X,Y,X']` is an `R[X']`-algebra, a polynomial `p : R[X',Y']` can be evaluated at
+/-- Viewing `R[X,Y,X']` as an `R[X']`-algebra, a polynomial `p : R[X',Y']` can be evaluated at
 `Y : R[X,Y,X']` (substitution of `Y'` by `Y`), obtaining another polynomial in `R[X,Y,X']`.
 When this polynomial is then evaluated at `X' = X`, the original polynomial `p` is recovered. -/
 lemma eval_C_X_eval₂_map_C_X {p : R[X][Y]} :
