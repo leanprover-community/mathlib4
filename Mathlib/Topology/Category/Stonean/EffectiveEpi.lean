7d5d6eb658906f2c7b67de823bc202579000fb2e
dagurtomas <dagurtomas@gmail.com>
2024-06-17 23:48:50 +0200

    refactor(Topology/Category): refactor Stonean.Basic

--- Mathlib/Topology/Category/Stonean/EffectiveEpi.lean
+++ Mathlib/Topology/Category/Stonean/EffectiveEpi.lean
@@ -34,6 +34,8 @@ universe u
 
 open CategoryTheory Limits
 
+attribute [local instance] ConcreteCategory.instFunLike
+
 namespace Stonean
 
 /--
@@ -76,11 +78,13 @@ theorem effectiveEpi_tfae
 
 instance : Stonean.toCompHaus.PreservesEffectiveEpis where
   preserves f h :=
-    ((CompHaus.effectiveEpi_tfae f).out 0 2).mpr (((Stonean.effectiveEpi_tfae f).out 0 2).mp h)
+    ((CompHaus.effectiveEpi_tfae (Stonean.toCompHaus.map f)).out 0 2).mpr
+      (((Stonean.effectiveEpi_tfae f).out 0 2).mp h)
 
 instance : Stonean.toCompHaus.ReflectsEffectiveEpis where
   reflects f h :=
-    ((Stonean.effectiveEpi_tfae f).out 0 2).mpr (((CompHaus.effectiveEpi_tfae f).out 0 2).mp h)
+    ((Stonean.effectiveEpi_tfae f).out 0 2).mpr
+      (((CompHaus.effectiveEpi_tfae (Stonean.toCompHaus.map f)).out 0 2).mp h)
 
 /--
 An effective presentation of an `X : CompHaus` with respect to the inclusion functor from `Stonean`
