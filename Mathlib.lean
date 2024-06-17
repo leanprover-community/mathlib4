c1d36fcacbc0017d36f22925bae62cee6d9d61b4
zjj <j.jj@j.jj>
2024-06-17 21:07:02 +0100

    all import

--- Mathlib.lean
+++ Mathlib.lean
@@ -3693,6 +3693,7 @@ import Mathlib.RingTheory.Smooth.Basic
 import Mathlib.RingTheory.TensorProduct.Basic
 import Mathlib.RingTheory.TensorProduct.MvPolynomial
 import Mathlib.RingTheory.Trace
+import Mathlib.RingTheory.TwoSidedIdeal
 import Mathlib.RingTheory.UniqueFactorizationDomain
 import Mathlib.RingTheory.Unramified.Basic
 import Mathlib.RingTheory.Unramified.Derivations
