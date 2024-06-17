2c304bfb9a9e029b6e6ea2140725222163dd99bc
David Kurniadi Angdinata <dka31@cantab.ac.uk>
2024-06-17 16:07:49 +0000

    feat(AlgebraicGeometry/EllipticCurve/DivisionPolynomial/Degree): compute degrees of division polynomials (#10878)
    Co-authored-by: Junyan Xu <junyanxumath@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -709,6 +709,7 @@ import Mathlib.Algebra.Vertex.HVertexOperator
 import Mathlib.AlgebraicGeometry.AffineScheme
 import Mathlib.AlgebraicGeometry.EllipticCurve.Affine
 import Mathlib.AlgebraicGeometry.EllipticCurve.DivisionPolynomial.Basic
+import Mathlib.AlgebraicGeometry.EllipticCurve.DivisionPolynomial.Degree
 import Mathlib.AlgebraicGeometry.EllipticCurve.Group
 import Mathlib.AlgebraicGeometry.EllipticCurve.Jacobian
 import Mathlib.AlgebraicGeometry.EllipticCurve.Projective
