1498f5a4aa5af2ad61ec044940c8108267291991
Yaël Dillies <yael.dillies@gmail.com>
2024-06-17 13:46:05 +0000

    feat: The diagonal of a finset From LeanAPAP

--- Mathlib/Data/Fintype/Pi.lean
+++ Mathlib/Data/Fintype/Pi.lean
@@ -3,6 +3,7 @@ Copyright (c) 2017 Mario Carneiro. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro
 -/
+import Mathlib.Data.Finset.Card
 import Mathlib.Data.Finset.Pi
 import Mathlib.Data.Fintype.Basic
 
@@ -10,10 +11,9 @@ import Mathlib.Data.Fintype.Basic
 # Fintype instances for pi types
 -/
 
+open Finset Function
 
 variable {α : Type*}
-
-open Finset
 
 namespace Fintype
 
@@ -149,3 +149,21 @@ theorem Finset.univ_pi_univ {α : Type*} {β : α → Type*} [DecidableEq α] [F
     [∀ a, Fintype (β a)] :
     (Finset.univ.pi fun a : α => (Finset.univ : Finset (β a))) = Finset.univ := by
   ext; simp
+
+/-! ### Diagonal -/
+
+namespace Finset
+variable {ι : Type*} [DecidableEq (ι → α)] {s : Finset α} {f : ι → α}
+
+/-- The diagonal of a finset `s : Finset α` as a finset of functions `ι → α`. -/
+def piDiag (s : Finset α) (ι : Type*) [DecidableEq (ι → α)] : Finset (ι → α) := s.image (const ι)
+
+@[simp] lemma mem_piDiag : f ∈ s.piDiag ι ↔ ∃ a ∈ s, const ι a = f := mem_image
+
+lemma piDiag_subset_piFinset [DecidableEq ι] [Fintype ι] :
+    s.piDiag ι ⊆ Fintype.piFinset fun _ ↦ s := by aesop (add simp [subset_iff])
+
+@[simp] lemma card_piDiag (s : Finset α) (ι : Type*) [DecidableEq (ι → α)] [Nonempty ι] :
+    (s.piDiag ι).card = s.card := by rw [piDiag, card_image_of_injective _ const_injective]
+
+end Finset
