32953b2784955e0c1ebbd9fee26663d53fd013d2
Yaël Dillies <yael.dillies@gmail.com>
2024-06-18 16:42:38 +0000

    feat: The Erdős–Ginzburg–Ziv theorem Prove the Erdős–Ginzburg–Ziv theorem, namely that among any (not necessarily distinct) `2 * n - 1` elements of `ZMod n`, we can find `n` elements of sum zero.

--- Mathlib/RingTheory/UniqueFactorizationDomain.lean
+++ Mathlib/RingTheory/UniqueFactorizationDomain.lean
@@ -1979,4 +1979,13 @@ lemma _root_.induction_on_primes {P : ℕ → Prop} (h₀ : P 0) (h₁ : P 1)
     exact h₁
   · exact fun a p _ hp ↦ h p a hp.nat_prime
 
+lemma prime_composite_induction {P : ℕ → Prop} (zero : P 0) (one : P 1)
+    (prime : ∀ p : ℕ, p.Prime → P p) (composite : ∀ a, 2 ≤ a → P a → ∀ b, 2 ≤ b → P b → P (a * b))
+    (n : ℕ) : P n := by
+  refine induction_on_primes zero one ?_ _
+  rintro p (_ | _ | a) hp ha
+  · simpa
+  · simpa using prime _ hp
+  · exact composite _ hp.two_le (prime _ hp) _ a.one_lt_succ_succ ha
+
 end Nat
