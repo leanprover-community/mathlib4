67ae4b52c0dcec62653106324c4fd7a217290c38
Calle Sönne <calle.sonne@gmail.com>
2024-06-18 08:30:55 +0100

    some progress

--- Mathlib/CategoryTheory/Bicategory/Functor/Lax.lean
+++ Mathlib/CategoryTheory/Bicategory/Functor/Lax.lean
@@ -3,3 +3,78 @@ Copyright (c) 2024 Calle Sönne. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Calle Sönne
 -/
+import Mathlib.CategoryTheory.Bicategory.Functor.Prelax
+
+/-!
+# Lax functors
+
+A lax functor `F` between bicategories `B` and `C` consists of
+* a function between objects `F.obj : B ⟶ C`,
+* a family of functions between 1-morphisms `F.map : (a ⟶ b) → (F.obj a ⟶ F.obj b)`,
+* a family of functions between 2-morphisms `F.map₂ : (f ⟶ g) → (F.map f ⟶ F.map g)`,
+-- TODO: flip arrows?
+* a family of 2-morphisms `F.mapId a : F.map (𝟙 a) ⟶ 𝟙 (F.obj a)`,
+* a family of 2-morphisms `F.mapComp f g : F.map (f ≫ g) ⟶ F.map f ≫ F.map g`, and
+* certain consistency conditions on them.
+
+## Main definitions
+
+* `CategoryTheory.LaxFunctor B C` : an oplax functor between bicategories `B` and `C`
+* `CategoryTheory.LaxFunctor.comp F G` : the composition of oplax functors
+
+-/
+
+namespace CategoryTheory
+
+open Category Bicategory
+
+open Bicategory
+
+universe w₁ w₂ w₃ v₁ v₂ v₃ u₁ u₂ u₃
+
+section
+
+variable {B : Type u₁} [Bicategory.{w₁, v₁} B] {C : Type u₂} [Bicategory.{w₂, v₂} C]
+
+/-- An oplax functor `F` between bicategories `B` and `C` consists of a function between objects
+`F.obj`, a function between 1-morphisms `F.map`, and a function between 2-morphisms `F.map₂`.
+
+Unlike functors between categories, `F.map` do not need to strictly commute with the composition,
+and do not need to strictly preserve the identity. Instead, there are specified 2-morphisms
+`F.map (𝟙 a) ⟶ 𝟙 (F.obj a)` and `F.map (f ≫ g) ⟶ F.map f ≫ F.map g`.
+
+`F.map₂` strictly commute with compositions and preserve the identity. They also preserve the
+associator, the left unitor, and the right unitor modulo some adjustments of domains and codomains
+of 2-morphisms.
+-/
+structure OplaxFunctor (B: Type u₁) [Bicategory.{w₁, v₁} B] (C : Type u₂)
+  [Bicategory.{w₂, v₂} C] extends PrelaxFunctor B C where
+  mapId (a : B) : 𝟙 (obj a) ⟶ map (𝟙 a)
+  mapComp {a b c : B} (f : a ⟶ b) (g : b ⟶ c) : map f ≫ map g ⟶ map (f ≫ g)
+  -- mapComp_naturality_left :
+  --   ∀ {a b c : B} {f f' : a ⟶ b} (η : f ⟶ f') (g : b ⟶ c),
+  --     map₂ (η ▷ g) ≫ mapComp f' g = mapComp f g ≫ map₂ η ▷ map g := by
+  --   aesop_cat
+  -- mapComp_naturality_right :
+  --   ∀ {a b c : B} (f : a ⟶ b) {g g' : b ⟶ c} (η : g ⟶ g'),
+  --     map₂ (f ◁ η) ≫ mapComp f g' = mapComp f g ≫ map f ◁ map₂ η := by
+  --   aesop_cat
+  -- map₂_associator :
+  --   ∀ {a b c d : B} (f : a ⟶ b) (g : b ⟶ c) (h : c ⟶ d),
+  --     map₂ (α_ f g h).hom ≫ mapComp f (g ≫ h) ≫ map f ◁ mapComp g h =
+  --   mapComp (f ≫ g) h ≫ mapComp f g ▷ map h ≫ (α_ (map f) (map g) (map h)).hom := by
+  --   aesop_cat
+  -- map₂_leftUnitor :
+  --   ∀ {a b : B} (f : a ⟶ b),
+  --     map₂ (λ_ f).hom = mapComp (𝟙 a) f ≫ mapId a ▷ map f ≫ (λ_ (map f)).hom := by
+  --   aesop_cat
+  -- map₂_rightUnitor :
+  --   ∀ {a b : B} (f : a ⟶ b),
+  --     map₂ (ρ_ f).hom = mapComp f (𝟙 b) ≫ map f ◁ mapId b ≫ (ρ_ (map f)).hom := by
+  --   aesop_cat
+
+
+-- TODO: comp & pseudocore
+
+
+-- Later: associator API
