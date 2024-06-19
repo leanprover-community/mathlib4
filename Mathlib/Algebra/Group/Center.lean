42606198c785950ae318d72b24e3f433e9aafa68
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2024-06-19 23:29:26 +0200

    chore: use sections consistently

--- Mathlib/Algebra/Group/Center.lean
+++ Mathlib/Algebra/Group/Center.lean
@@ -97,7 +97,7 @@ theorem mem_center_iff {z : M} : z ∈ center M ↔ IsMulCentral z :=
 #align set.mem_add_center Set.mem_addCenter_iff
 
 @[to_additive (attr := simp) add_mem_addCenter]
-theorem mul_mem_center [Mul M] {z₁ z₂ : M} (hz₁ : z₁ ∈ Set.center M) (hz₂ : z₂ ∈ Set.center M) :
+theorem mul_mem_center {z₁ z₂ : M} (hz₁ : z₁ ∈ Set.center M) (hz₂ : z₂ ∈ Set.center M) :
     z₁ * z₂ ∈ Set.center M where
   comm a := calc
     z₁ * z₂ * a = z₂ * z₁ * a := by rw [hz₁.comm]
@@ -143,19 +143,23 @@ instance decidableMemCenter [∀ a : M, Decidable <| ∀ b : M, b * a = a * b] :
 end Semigroup
 
 section CommSemigroup
+variable [CommSemigroup M]
 
 variable (M) in
 @[to_additive (attr := simp) addCenter_eq_univ]
-theorem center_eq_univ [CommSemigroup M] : center M = univ :=
+theorem center_eq_univ : center M = univ :=
   (Subset.antisymm (subset_univ _)) fun _ _ => Semigroup.mem_center_iff.mpr (fun _ => mul_comm _ _)
 #align set.center_eq_univ Set.center_eq_univ
 #align set.add_center_eq_univ Set.addCenter_eq_univ
 
 end CommSemigroup
 
+section MulOneClass
+variable [MulOneClass M]
+
 variable (M) in
 @[to_additive (attr := simp) zero_mem_addCenter]
-theorem one_mem_center [MulOneClass M] : (1 : M) ∈ Set.center M where
+theorem one_mem_center : (1 : M) ∈ Set.center M where
   comm _  := by rw [one_mul, mul_one]
   left_assoc _ _ := by rw [one_mul, one_mul]
   mid_assoc _ _ := by rw [mul_one, one_mul]
@@ -163,16 +167,13 @@ theorem one_mem_center [MulOneClass M] : (1 : M) ∈ Set.center M where
 #align set.one_mem_center Set.one_mem_center
 #align set.zero_mem_add_center Set.zero_mem_addCenter
 
-@[to_additive (attr := simp) neg_mem_addCenter]
-theorem inv_mem_center [DivisionMonoid M] {a : M} (ha : a ∈ Set.center M) : a⁻¹ ∈ Set.center M := by
-  rw [_root_.Semigroup.mem_center_iff]
-  intro _
-  rw [← inv_inj, mul_inv_rev, inv_inv, ha.comm, mul_inv_rev, inv_inv]
-#align set.inv_mem_center Set.inv_mem_centerₓ
-#align set.neg_mem_add_center Set.neg_mem_addCenterₓ
+end MulOneClass
+
+section Monoid
+variable [Monoid M]
 
 @[to_additive subset_addCenter_add_units]
-theorem subset_center_units [Monoid M] : ((↑) : Mˣ → M) ⁻¹' center M ⊆ Set.center Mˣ :=
+theorem subset_center_units : ((↑) : Mˣ → M) ⁻¹' center M ⊆ Set.center Mˣ :=
   fun _ ha => by
   rw [_root_.Semigroup.mem_center_iff]
   intro _
@@ -181,24 +182,35 @@ theorem subset_center_units [Monoid M] : ((↑) : Mˣ → M) ⁻¹' center M ⊆
 #align set.subset_add_center_add_units Set.subset_addCenter_add_units
 
 @[to_additive (attr := simp)]
-theorem units_inv_mem_center [Monoid M] {a : Mˣ} (ha : ↑a ∈ Set.center M) :
-    ↑a⁻¹ ∈ Set.center M := by
+theorem units_inv_mem_center {a : Mˣ} (ha : ↑a ∈ Set.center M) : ↑a⁻¹ ∈ Set.center M := by
   rw [Semigroup.mem_center_iff] at *
   exact (Commute.units_inv_right <| ha ·)
 
 @[simp]
-theorem invOf_mem_center [Monoid M] {a : M} [Invertible a] (ha : a ∈ Set.center M) :
-    ⅟a ∈ Set.center M := by
+theorem invOf_mem_center {a : M} [Invertible a] (ha : a ∈ Set.center M) : ⅟a ∈ Set.center M := by
   rw [Semigroup.mem_center_iff] at *
   exact (Commute.invOf_right <| ha ·)
 
+end Monoid
+
+section DivisionMonoid
+variable [DivisionMonoid M] {a b : M}
+
+@[to_additive (attr := simp) neg_mem_addCenter]
+theorem inv_mem_center (ha : a ∈ Set.center M) : a⁻¹ ∈ Set.center M := by
+  rw [_root_.Semigroup.mem_center_iff]
+  intro _
+  rw [← inv_inj, mul_inv_rev, inv_inv, ha.comm, mul_inv_rev, inv_inv]
+#align set.inv_mem_center Set.inv_mem_centerₓ
+#align set.neg_mem_add_center Set.neg_mem_addCenterₓ
+
 @[to_additive (attr := simp) sub_mem_addCenter]
-theorem div_mem_center [DivisionMonoid M]
-    {a b : M} (ha : a ∈ Set.center M) (hb : b ∈ Set.center M) :
-    a / b ∈ Set.center M := by
+theorem div_mem_center (ha : a ∈ Set.center M) (hb : b ∈ Set.center M) : a / b ∈ Set.center M := by
   rw [div_eq_mul_inv]
   exact mul_mem_center ha (inv_mem_center hb)
 #align set.div_mem_center Set.div_mem_centerₓ
 #align set.sub_mem_add_center Set.sub_mem_addCenterₓ
+
+end DivisionMonoid
 
 end Set
