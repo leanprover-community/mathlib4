21143292d315f44a4b3db8b7ae97f13f287b7991
417963616@qq.com <417963616@qq.com>
2024-06-19 20:29:28 +0100

    constructed the map from Spec(O_x) to X for X a scheme

--- Mathlib/Geometry/RingedSpace/LocallyRingedSpace/ResidueField.lean
+++ Mathlib/Geometry/RingedSpace/LocallyRingedSpace/ResidueField.lean
@@ -6,29 +6,12 @@ Authors: Fangming Li
 import Mathlib.AlgebraicGeometry.Scheme
 
 /-!
-# The Residue Field of a Point in a Locally Ringed Space and Related Morphisms
+# The Residue Field of a Point in a Locally Ringed Space and the Canonical Morphism
 
-In this file, we have defined the residue field of a point in a locally ringed space and some
-useful homomorphisms. We aim to construct the following:
-• given a locally ringed space `X` and a point `x : X.toTopCat`, the canonical morphism from the
-  spectrum of the residue field of `x` to `X`.
+In this file, we have defined the residue field of a point in a locally ringed space. Also, given a
+scheme `X` and a point `x : X`, we have constructed the canonical morphism from the spectrum of the
+residue field of `x` to `X`.
 -/
-
-instance AlgebraicGeometry.SheafedSpace.section_over_bot_unique {X : SheafedSpace CommRingCat} :
-    Unique (X.presheaf.obj { unop := ⊥ }) where
-  default := 0
-  uniq := fun a ↦ by
-    let U : Empty → TopologicalSpace.Opens X.toPresheafedSpace := fun _ ↦ ⊥
-    have h := TopCat.Sheaf.eq_of_locally_eq X.sheaf U
-    simp only [IsEmpty.forall_iff, true_implies] at h
-    rw [show iSup U = ⊥ by simp only [iSup_eq_bot, implies_true]] at h
-    exact h a 0
-
-instance AlgebraicGeometry.Spec.structureSheaf.section_over_bot_unique {R : Type _} [CommRing R] :
-    Unique ((Spec.structureSheaf R).val.obj {unop := ⊥}) := by
-  rw [show (Spec.structureSheaf R).val.obj {unop := ⊥} =
-    (Scheme.specObj ⟨R, _⟩).presheaf.obj {unop := ⊥} by exact rfl]
-  exact SheafedSpace.section_over_bot_unique
 
 namespace AlgebraicGeometry
 
@@ -49,80 +32,53 @@ lemma ResidueField.isField {X : LocallyRingedSpace} (x : X) :
 noncomputable instance {X : LocallyRingedSpace} (x : X) : Field (X.ResidueField x) :=
   LocalRing.ResidueField.field (X.stalk x)
 
-/--
-Given some `O : (TopologicalSpace.Opens ↑↑X.toPresheafedSpace)ᵒᵖ` which contains the point `x`,
-there is a canonocal homomorphism from `X.presheaf.obj O` to the section over `O` in terms of the
-pushforward of `(Scheme.specObj (X.stalk x)).presheaf` along the constant map `fun _ ↦ x`.
--/
-noncomputable def ObjToPushforwardObjHomOfMem {X : LocallyRingedSpace} (x : X)
-    (O : (TopologicalSpace.Opens ↑↑X.toPresheafedSpace)ᵒᵖ) (hxO : x ∈ O.unop) :
-    X.presheaf.obj O ⟶ (⟨fun _ ↦ x, continuous_const⟩ _*
-    (Scheme.specObj (X.stalk x)).presheaf).obj O :=
-  let x' : O.unop := ⟨x, hxO⟩
-  let hom1 := @TopCat.Presheaf.germ CommRingCat _ _ X.toTopCat X.presheaf O.unop x'
-  let hom2 := StructureSheaf.toOpen (X.stalk x) ⊤
-  CategoryTheory.CategoryStruct.comp hom1 (CategoryTheory.CategoryStruct.comp hom2
-    ((Scheme.specObj (X.stalk x)).presheaf.map (TopologicalSpace.Opens.leTop _).op))
-
-/--
-Given some `O : (TopologicalSpace.Opens ↑↑X.toPresheafedSpace)ᵒᵖ` which does not contain `x`,
-`fun _ ↦ 0` is the canonocal homomorphism from `X.presheaf.obj O` to the section over `O` in terms
-of the pushforward of `(Scheme.specObj (X.stalk x)).presheaf` along the constant map `fun _ ↦ x`.
--/
-noncomputable def ObjToPushforwardObjHomOfNotMem {X : LocallyRingedSpace} (x : X)
-    (O : (TopologicalSpace.Opens ↑↑X.toPresheafedSpace)ᵒᵖ) (hxO : x ∉ O.unop) :
-    X.presheaf.obj O ⟶ (⟨fun _ ↦ x, continuous_const⟩ _*
-    (Scheme.specObj (X.stalk x)).presheaf).obj O where
-  toFun := fun _ ↦ 0
-  map_one' := by
-    rw [TopCat.Presheaf.pushforwardObj_obj, CategoryTheory.Functor.op_obj,
-      (le_bot_iff.mp fun _ ↦ hxO : (@TopologicalSpace.Opens.map (Scheme.specObj (X.stalk x))
-      X.toTopCat ⟨fun _ ↦ x, continuous_const⟩).obj O.unop = ⊥)]
-    exact Eq.symm (Subsingleton.eq_zero 1)
-  map_mul' := by simp_rw [mul_zero, implies_true]
-  map_zero' := rfl
-  map_add' := by simp_rw [add_zero, implies_true]
-
-/--
-Given an arbitrary `O : (TopologicalSpace.Opens ↑↑X.toPresheafedSpace)ᵒᵖ`, there is a canonocal
-homomorphism from `X.presheaf.obj O` to the section over `O` in terms of the pushforward of
-`(Scheme.specObj (X.stalk x)).presheaf` along the constant map `fun _ ↦ x`.
--/
-noncomputable def ObjToPushforwardObjHom {X : LocallyRingedSpace} (x : X)
-    (O : (TopologicalSpace.Opens ↑↑X.toPresheafedSpace)ᵒᵖ) :
-    X.presheaf.obj O ⟶ (⟨fun _ ↦ x, continuous_const⟩ _*
-    (Scheme.specObj (X.stalk x)).presheaf).obj O :=
-  let _ := Classical.propDecidable (x ∈ O.unop)
-  if hxO : x ∈ O.unop then ObjToPushforwardObjHomOfMem x O hxO
-  else ObjToPushforwardObjHomOfNotMem x O hxO
-
-/--
-`AlgebraicGeometry.LocallyRingedSpace.ObjToPushforwardObjHom x` satisfies the natural property.
-This theorem is a basis for constructing the canonical morphism from the spectrum of `X.stalk x` to
-`X`.
--/
-theorem ObjToPushforwardObjHom_naturality {X : LocallyRingedSpace} (x : X) :
-    ∀ {O1 O2 : (TopologicalSpace.Opens ↑↑X.toPresheafedSpace)ᵒᵖ} (f : O1 ⟶ O2),
-    CategoryTheory.CategoryStruct.comp (X.presheaf.map f) (ObjToPushforwardObjHom x O2) =
-    CategoryTheory.CategoryStruct.comp (ObjToPushforwardObjHom x O1)
-    ((⟨fun _ ↦ x, continuous_const⟩ _*
-    (Scheme.specObj (X.stalk x)).presheaf).map f) := by
-  intro O1 O2 f
-  ext s
-  rw [ObjToPushforwardObjHom, ObjToPushforwardObjHom]
-  by_cases hxO2 : x ∈ O2.unop
-  · have hxO1 : x ∈ O1.unop := CategoryTheory.le_of_op_hom f hxO2
-    simp only [hxO2, hxO1, CategoryTheory.comp_apply]
-    erw [RingHom.comp_apply, TopCat.Presheaf.germ_res_apply X.presheaf f.unop ⟨x, hxO2⟩ s]
-    rfl
-  · simp only [hxO2]
-    have : Unique (((Scheme.specObj (X.stalk x)).presheaf.obj
-        { unop := (TopologicalSpace.Opens.map ⟨fun _ ↦ x, continuous_const⟩).obj O2.unop })) := by
-      erw [(le_bot_iff.mp fun _ ↦ hxO2 : (@TopologicalSpace.Opens.map (Scheme.specObj (X.stalk x))
-        X.toTopCat ⟨fun _ ↦ x, continuous_const⟩).obj O2.unop = ⊥)]
-      exact @Spec.structureSheaf.section_over_bot_unique (X.stalk x) _
-    exact Eq.trans (this.eq_default _) (this.default_eq _)
-
 end LocallyRingedSpace
+
+namespace Scheme
+
+/--
+The canonical morphism from the spectrum of the stalk at `x` to `X`.
+-/
+noncomputable def HomFromStalkSpec {X : Scheme} (x : X) :
+    (specObj (X.stalk x)) ⟶ X :=
+-- Because `X` is a scheme, `x` has an affine open neighbourhood `U`.
+  let U := (X.local_affine x).choose
+-- The ring `R` such that `U` is isomorphic to Spec(`R`).
+  let R := (X.local_affine x).choose_spec.choose
+-- The isomorphism from `U` to Spec(`R`).
+  let iUR := Classical.choice (X.local_affine x).choose_spec.choose_spec
+-- Because `U` is isomorphic to Spec(`R`) and the global section of Spec(`R`) is `R` itself, `R`
+-- is isomorphic to the section over `U`. In particular, we have a map from `R` to the the section
+-- over `U`.
+  let hRU := CategoryTheory.CategoryStruct.comp (StructureSheaf.toOpen R ⊤)
+        (CategoryTheory.CategoryStruct.comp (iUR.hom.val.c.1 (Opposite.op ⊤))
+          (CategoryTheory.eqToHom (by simp only [LocallyRingedSpace.restrict_carrier,
+            TopCat.Presheaf.pushforwardObj_obj, CategoryTheory.Functor.op_obj,
+            TopologicalSpace.Opens.map_top, LocallyRingedSpace.restrict_presheaf_obj,
+            TopologicalSpace.Opens.openEmbedding_obj_top])))
+-- We regard `x` as an element of `U`.
+  let x' : U.1 := ⟨x, U.2⟩
+-- The homomorphism from the section over `U` to the stalk at `x`.
+  let hUs := TopCat.Presheaf.germ X.presheaf x'
+-- According to the above setting, the composition of `hRU` and `hUs` is a map from `R` to the
+-- stalk of `x`. Due to the contravariance of the spectrum functor, we get a morphism from the
+-- spectrum of the stalk at `x` to Spec(`R`).
+  let hxR := Spec.locallyRingedSpaceMap (CategoryTheory.CategoryStruct.comp hRU hUs)
+-- The locally ringed space morphism induced by the open embedding from `U` to `X`.
+  let hUX := X.ofRestrict (TopologicalSpace.OpenNhds.openEmbedding U)
+-- The final morphism we use is the composition of `hxR`, `iUR.inv` and `hUX`.
+  let this := CategoryTheory.CategoryStruct.comp (CategoryTheory.CategoryStruct.comp hxR iUR.inv)
+    hUX
+  this
+
+/--
+The canonical scheme morphism from the spectrum of the residue field of `x` to `X`.
+-/
+noncomputable def HomFromResidueFieldSpec {X : Scheme} (x : X) :
+    (specObj (LocallyRingedSpace.ResidueField x)) ⟶ X :=
+  CategoryTheory.CategoryStruct.comp (specMap (CommRingCat.ofHom (LocalRing.residue (X.stalk x))))
+    (HomFromStalkSpec x)
+
+end Scheme
 
 end AlgebraicGeometry
