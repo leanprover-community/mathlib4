af5047866ded1a027b8d9f0bc8be73ba08feed7c
Kalle <kalle.kytola@aalto.fi>
2024-06-19 21:59:02 +0300

    Move to a more appropriate file. (Should the file be split?)

--- Mathlib/Order/LiminfLimsup.lean
+++ Mathlib/Order/LiminfLimsup.lean
@@ -294,6 +294,67 @@ theorem IsCobounded.mono (h : f ≤ g) : f.IsCobounded r → g.IsCobounded r
 
 end Relation
 
+section operations
+
+open Filter BigOperators Set
+
+variable {α : Type*} {f : Filter α} [NeBot f]
+variable {R : Type*} [Preorder R]
+
+lemma isBoundedUnder_ge_add [Add R]
+    [CovariantClass R R (fun a b ↦ a + b) (· ≤ ·)] [CovariantClass R R (fun a b ↦ b + a) (· ≤ ·)]
+    {u v : α → R} (u_bdd_ge : f.IsBoundedUnder (· ≥ ·) u) (v_bdd_ge : f.IsBoundedUnder (· ≥ ·) v) :
+    f.IsBoundedUnder (· ≥ ·) (u + v) := by
+  obtain ⟨U, hU⟩ := u_bdd_ge
+  obtain ⟨V, hV⟩ := v_bdd_ge
+  use U + V
+  simp only [eventually_map, Pi.add_apply] at hU hV ⊢
+  filter_upwards [hU, hV] with a hu hv using add_le_add hu hv
+
+lemma isBoundedUnder_le_add [Add R]
+    [CovariantClass R R (fun a b ↦ a + b) (· ≤ ·)] [CovariantClass R R (fun a b ↦ b + a) (· ≤ ·)]
+    {u v : α → R} (u_bdd_ge : f.IsBoundedUnder (· ≤ ·) u) (v_bdd_ge : f.IsBoundedUnder (· ≤ ·) v) :
+    f.IsBoundedUnder (· ≤ ·) (u + v) := by
+  obtain ⟨U, hU⟩ := u_bdd_ge
+  obtain ⟨V, hV⟩ := v_bdd_ge
+  use U + V
+  simp only [eventually_map, Pi.add_apply] at hU hV ⊢
+  filter_upwards [hU, hV] with a hu hv using add_le_add hu hv
+
+lemma isBoundedUnder_sum {κ : Type*} [DecidableEq κ] [AddCommMonoid R] {r : R → R → Prop}
+    [CovariantClass R R (fun a b ↦ a + b) r] [CovariantClass R R (fun a b ↦ b + a) r]
+    (hr : ∀ (v₁ v₂ : α → R), f.IsBoundedUnder r v₁ → f.IsBoundedUnder r v₂
+      → f.IsBoundedUnder r (v₁ + v₂)) (hr₀ : r 0 0)
+    {u : κ → α → R} (s : Finset κ) :
+    (∀ k ∈ s, f.IsBoundedUnder r (u k)) →
+      f.IsBoundedUnder r (∑ k ∈ s, u k) := by
+  apply Finset.induction_on (p := fun s ↦
+      (∀ k ∈ s, f.IsBoundedUnder r (u k)) → f.IsBoundedUnder r (∑ k ∈ s, u k))
+  · simp only [ge_iff_le, Finset.not_mem_empty, false_implies, implies_true, nonempty_Prop,
+               forall_const, Finset.sum_empty]
+    refine ⟨0, by simp_all only [eventually_map, Pi.zero_apply, eventually_true]⟩
+  · simp only [ge_iff_le, Finset.mem_insert, forall_eq_or_imp, and_imp] at *
+    intro k₀ s k₀_notin_s ih bdd_k₀ bdd_rest
+    simpa only [Finset.sum_insert k₀_notin_s] using hr _ _ bdd_k₀ (ih bdd_rest)
+
+lemma isBoundedUnder_le_sum {κ : Type*} [DecidableEq κ] [AddCommMonoid R]
+    [CovariantClass R R (fun a b ↦ a + b) (· ≤ ·)] [CovariantClass R R (fun a b ↦ b + a) (· ≤ ·)]
+    {u : κ → α → R} (s : Finset κ) :
+    (∀ k ∈ s, f.IsBoundedUnder (· ≤ ·) (u k)) →
+      f.IsBoundedUnder (· ≤ ·) (∑ k ∈ s, u k) := by
+  apply isBoundedUnder_sum (fun _ _ ↦ isBoundedUnder_le_add) le_rfl
+
+lemma isBoundedUnder_ge_sum {κ : Type*} [DecidableEq κ] [AddCommMonoid R]
+    [CovariantClass R R (fun a b ↦ a + b) (· ≤ ·)] [CovariantClass R R (fun a b ↦ b + a) (· ≤ ·)]
+    {u : κ → α → R} (s : Finset κ) :
+    (∀ k ∈ s, f.IsBoundedUnder (· ≥ ·) (u k)) →
+      f.IsBoundedUnder (· ≥ ·) (∑ k ∈ s, u k) := by
+  have aux : CovariantClass R R (fun a b ↦ a + b) (· ≥ ·) :=
+    { elim := fun x _ _ hy ↦ add_le_add_left hy x }
+  apply isBoundedUnder_sum (fun _ _ ↦ isBoundedUnder_ge_add) le_rfl
+
+end operations
+
 section Nonempty
 variable [Preorder α] [Nonempty α] {f : Filter β} {u : β → α}
 
