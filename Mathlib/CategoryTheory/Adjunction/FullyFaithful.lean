604ab5be88273c64b883d3ea8db6706918bedca2
Dagur Tómas Ásgeirsson <dagurtomas@gmail.com>
2024-06-19 18:27:35 +0200

    Update Mathlib/CategoryTheory/Adjunction/FullyFaithful.lean

--- Mathlib/CategoryTheory/Adjunction/FullyFaithful.lean
+++ Mathlib/CategoryTheory/Adjunction/FullyFaithful.lean
@@ -200,7 +200,7 @@ theorem mem_essImage_of_unit_isIso (A : C)
   ⟨L.obj A, ⟨(asIso (h.unit.app A)).symm⟩⟩
 #align category_theory.mem_ess_image_of_unit_is_iso CategoryTheory.Adjunction.mem_essImage_of_unit_isIso
 
-@[deprecated (since := "2024.06.19")] alias _root_.CategoryTheory.mem_essImage_of_unit_isIso :=
+@[deprecated (since := "2024-06-19")] alias _root_.CategoryTheory.mem_essImage_of_unit_isIso :=
   mem_essImage_of_unit_isIso
 
 lemma isIso_unit_app_of_iso [R.Faithful] [R.Full] {X : D} {Y : C} (e : Y ≅ R.obj X) :
