820907133cd59aac9e00face3e93ac503bafe472
Andrew Yang <the.erd.one@gmail.com>
2024-06-20 22:48:21 +0800

    fix import

--- Mathlib/AlgebraicGeometry/Morphisms/Affine.lean
+++ Mathlib/AlgebraicGeometry/Morphisms/Affine.lean
@@ -3,10 +3,7 @@ Copyright (c) 2024 Andrew Yang. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Andrew Yang
 -/
-import Mathlib.AlgebraicGeometry.OpenImmersion
 import Mathlib.AlgebraicGeometry.Morphisms.QuasiCompact
-import Mathlib.CategoryTheory.MorphismProperty.Composition
-import Mathlib.RingTheory.LocalProperties
 
 /-!
 
