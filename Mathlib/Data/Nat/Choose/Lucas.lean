72d5597ac174ac1b9c24cee9729f47c00ffda8d3
Gareth Ma <grhkm21@gmail.com>
2024-06-20 19:15:18 +0100

    chore: rename theorems to symbol-reading names

--- Mathlib/Data/Nat/Choose/Lucas.lean
+++ Mathlib/Data/Nat/Choose/Lucas.lean
@@ -31,7 +31,8 @@ variable {n k p : ℕ} [Fact p.Prime]
 
 /-- **Lucas's Theorem**: For primes `p`, `choose n k` is congruent to `choose (n % p) (k % p) *
 choose (n / p) (k / p)` modulo `p`. -/
-theorem lucas_theorem :choose n k ≡ choose (n % p) (k % p) * choose (n / p) (k / p) [ZMOD p] := by
+theorem choose_modEq_choose_mod_mul_choose_div :
+    choose n k ≡ choose (n % p) (k % p) * choose (n / p) (k / p) [ZMOD p] := by
   have decompose : ((X : (ZMod p)[X]) + 1) ^ n = (X + 1) ^ (n % p) * (X ^ p + 1) ^ (n / p) := by
     simpa using add_pow_eq_add_pow_mod_mul_pow_add_pow_div _ (X : (ZMod p)[X]) 1
   simp only [← ZMod.intCast_eq_intCast_iff, Int.cast_mul, Int.cast_ofNat,
@@ -61,23 +62,26 @@ theorem lucas_theorem :choose n k ≡ choose (n % p) (k % p) * choose (n / p) (k
 /-- **Lucas's Theorem**: For primes `p`, `choose n k` is congruent to the product of
 `choose (⌊n / p ^ i⌋ % p) (⌊k / p ^ i⌋ % p)` over i < a, multiplied by
 `choose (⌊n / p ^ a⌋) (⌊k / p ^ a⌋)`, modulo `p`. -/
-theorem lucas_theorem' (a : ℕ) :
+theorem choose_modEq_choose_mul_prod_range_choose (a : ℕ) :
     choose n k ≡ choose (n / p ^ a) (k / p ^ a) *
       ∏ i in range a, choose (n / p ^ i % p) (k / p ^ i % p) [ZMOD p] :=
   match a with
   | Nat.zero => by simp
-  | Nat.succ a => (lucas_theorem' a).trans <| by
+  | Nat.succ a => (choose_modEq_choose_mul_prod_range_choose a).trans <| by
     rw [prod_range_succ, cast_mul, ← mul_assoc, mul_right_comm]
     gcongr
-    apply lucas_theorem.trans
+    apply choose_modEq_choose_mod_mul_choose_div.trans
     simp_rw [pow_succ, Nat.div_div_eq_div_mul, mul_comm]
     rfl
 
 /-- **Lucas's Theorem**: For primes `p`, `choose n k` is congruent to the product of
 `choose (⌊n / p ^ i⌋ % p) (⌊k / p ^ i⌋ % p)` over `i` modulo `p`. -/
-theorem lucas_theorem'' {a : ℕ} (ha₁ : n < p ^ a) (ha₂ : k < p ^ a) :
+theorem choose_modEq_prod_range_choose {a : ℕ} (ha₁ : n < p ^ a) (ha₂ : k < p ^ a) :
     choose n k ≡ ∏ i in range a, choose (n / p ^ i % p) (k / p ^ i % p) [ZMOD p] := by
-  apply (lucas_theorem' a).trans
+  apply (choose_modEq_choose_mul_prod_range_choose a).trans
   simp_rw [(Nat.div_eq_zero_iff $ by omega).mpr ha₁, (Nat.div_eq_zero_iff $ by omega).mpr ha₂,
     choose, cast_one, one_mul, cast_prod]
   rfl
+
+alias lucas_theorem := choose_modEq_choose_mod_mul_choose_div
+alias lucas_theorem' := choose_modEq_prod_range_choose
