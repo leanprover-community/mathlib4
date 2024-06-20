9dcb609a3787f12da07eacaad81ad321a86e8794
Peter Nelson <71660771+apnelson1@users.noreply.github.com>
2024-06-20 15:59:45 -0400

    relhom instance

--- Mathlib/Data/Fintype/Pi.lean
+++ Mathlib/Data/Fintype/Pi.lean
@@ -157,7 +157,12 @@ noncomputable instance _root_.Function.Embedding.fintype {α β} [Fintype α] [F
   classical exact Fintype.ofEquiv _ (Equiv.subtypeInjectiveEquivEmbedding α β)
 #align function.embedding.fintype Function.Embedding.fintype
 
-noncomputable instance _root_.RelEmbedding.instFintype {α β} [Fintype α] [Fintype β]
+instance RelHom.instFintype {α β} [Fintype α] [Fintype β] [DecidableEq α] {r : α → α → Prop}
+    {s : β → β → Prop} [DecidableRel r] [DecidableRel s] : Fintype (r →r s) :=
+  Fintype.ofEquiv {f : α → β // ∀ {x y}, r x y → s (f x) (f y)} <| Equiv.mk
+    (fun f ↦ ⟨f.1, f.2⟩) (fun f ↦ ⟨f.1, f.2⟩) (fun _ ↦ rfl) (fun _ ↦ rfl)
+
+noncomputable instance RelEmbedding.instFintype {α β} [Fintype α] [Fintype β]
     {r : α → α → Prop} {s : β → β → Prop} : Fintype (r ↪r s) :=
   Fintype.ofInjective _ RelEmbedding.toEmbedding_injective
 
