0a90491e510ce44a6d30e0835baf29aaeaa104d6
Parcly Taxel <reddeloostw@gmail.com>
2024-06-20 17:47:21 +0800

    generalise `crossEdges` to any finpartition

--- Mathlib.lean
+++ Mathlib.lean
@@ -1759,6 +1759,7 @@ import Mathlib.Combinatorics.SimpleGraph.Density
 import Mathlib.Combinatorics.SimpleGraph.Ends.Defs
 import Mathlib.Combinatorics.SimpleGraph.Ends.Properties
 import Mathlib.Combinatorics.SimpleGraph.Finite
+import Mathlib.Combinatorics.SimpleGraph.Finpartition
 import Mathlib.Combinatorics.SimpleGraph.Finsubgraph
 import Mathlib.Combinatorics.SimpleGraph.Girth
 import Mathlib.Combinatorics.SimpleGraph.Hamiltonian
