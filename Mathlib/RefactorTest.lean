98da43f3a7a08dc4e5f5d5fc58186848987115ff
adomani <adomani@gmail.com>
2024-06-20 02:00:25 +0100

    remove test

--- Mathlib/RefactorTest.lean
+++
@@ -1,8 +0,0 @@
-import Mathlib.Tactic.Lemma
-
-theorem replaceMe : True := .intro
-
-/-- i have a doc-string -/
-theorem doNotReplaceMe : True := .intro
-
-lemma doNotReplaceMeEither : True := .intro
