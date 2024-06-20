691fc69f89efbb89bcc665d2f69f02b8869c6d10
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2024-06-20 15:11:48 +0200

    broken

--- Mathlib/RingTheory/DedekindDomain/Frobenius.lean
+++ Mathlib/RingTheory/DedekindDomain/Frobenius.lean
@@ -7,6 +7,7 @@ import Mathlib.RingTheory.DedekindDomain.Ideal
 import Mathlib.RingTheory.IntegralRestrict
 import Mathlib.RingTheory.Ideal.QuotientOperations
 import Mathlib.FieldTheory.Cardinality
+import Mathlib.RingTheory.Ideal.Pointwise
 
 /-
 
@@ -79,19 +80,22 @@ i.e. supply the finiteness typeclasses and descent hypothesis in this case.
 
 variable (A : Type*) [CommRing A] {B : Type*} [CommRing B] [Algebra A B]
 
--- PR #13294
-variable {α : Type*} in
-instance Ideal.pointwiseMulSemiringAction
-    [Monoid α] [MulSemiringAction α B] : MulSemiringAction α (Ideal B) where
-  smul a I := Ideal.map (MulSemiringAction.toRingHom _ _ a) I
-  one_smul I :=
-    congr_arg (I.map ·) (RingHom.ext <| one_smul α) |>.trans I.map_id
-  mul_smul _a₁ _a₂ I :=
-    congr_arg (I.map ·) (RingHom.ext <| mul_smul _ _) |>.trans (I.map_map _ _).symm
-  smul_one a := by simp only [Ideal.one_eq_top]; exact Ideal.map_top _
-  smul_mul a I J := Ideal.map_mul _ I J
-  smul_add a I J := Ideal.map_sup _ I J
-  smul_zero a := Ideal.map_bot
+-- -- PR #13294
+-- variable {α : Type*} in
+-- instance Ideal.pointwiseMulSemiringAction
+--     [Monoid α] [MulSemiringAction α B] : MulSemiringAction α (Ideal B) where
+--   smul a I := Ideal.map (MulSemiringAction.toRingHom _ _ a) I
+--   one_smul I :=
+--     congr_arg (I.map ·) (RingHom.ext <| one_smul α) |>.trans I.map_id
+--   mul_smul _a₁ _a₂ I :=
+--     congr_arg (I.map ·) (RingHom.ext <| mul_smul _ _) |>.trans (I.map_map _ _).symm
+--   smul_one a := by simp only [Ideal.one_eq_top]; exact Ideal.map_top _
+--   smul_mul a I J := Ideal.map_mul _ I J
+--   smul_add a I J := Ideal.map_sup _ I J
+--   smul_zero a := Ideal.map_bot
+-- 3993
+
+open scoped Pointwise -- to get Galois action on ideals
 
 -- should be in #13294?
 variable {α : Type*} in
