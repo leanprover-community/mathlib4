252a66c13d53a357edebe35d9a7d1f595678b153
Calle Sönne <calle.sonne@gmail.com>
2024-06-21 10:06:01 +0100

    first commit

--- Mathlib/CategoryTheory/Bicategory/Functor/Pseudofunctor.lean
+++ Mathlib/CategoryTheory/Bicategory/Functor/Pseudofunctor.lean
@@ -219,6 +219,88 @@ def comp (F : Pseudofunctor B C) (G : Pseudofunctor C D) : Pseudofunctor B D whe
 -- -- so we defer creating its `@[simp]` lemmas until a separate command.
 -- attribute [simps] comp
 
+-- API
+section
+
+variable (F : Pseudofunctor B C) {a b : B}
+
+-- These should be for Oplax functors!!!!!!!
+lemma mapComp_assoc_right_hom {c d : B} (f : a ⟶ b) (g : b ⟶ c) (h : c ⟶ d) :
+    (F.mapComp f (g ≫ h)).hom ≫ F.map f ◁ (F.mapComp g h).hom = (F.map₂ (α_ f g h).inv) ≫
+    (F.mapComp (f ≫ g) h).hom ≫ (F.mapComp f g).hom ▷ F.map h ≫
+    (α_ (F.map f) (F.map g) (F.map h)).hom := by
+  sorry
+
+lemma mapComp_assoc_left_hom {c d : B} (f : a ⟶ b) (g : b ⟶ c) (h : c ⟶ d) :
+    (F.mapComp (f ≫ g) h).hom ≫ (F.mapComp f g).hom ▷ F.map h =
+    (F.map₂ (α_ f g h).hom) ≫ (F.mapComp f (g ≫ h)).hom ≫ F.map f ◁ (F.mapComp g h).hom
+    ≫ (α_ (F.map f) (F.map g) (F.map h)).inv := by
+  simp
+
+-- TODO: this should follow from a statement about lax functors, once implemented
+lemma mapComp_assoc_right_inv {c d : B} (f : a ⟶ b) (g : b ⟶ c) (h : c ⟶ d) :
+    F.map f ◁ (F.mapComp g h).inv ≫ (F.mapComp f (g ≫ h)).inv =
+    (α_ (F.map f) (F.map g) (F.map h)).inv ≫ (F.mapComp f g).inv ▷ F.map h ≫
+    (F.mapComp (f ≫ g) h).inv ≫ (F.map₂ (α_ f g h).hom) := by
+  simp
+
+-- TODO: this should follow from a statement about lax functors, once implemented
+lemma mapComp_assoc_left_inv {c d : B} (f : a ⟶ b) (g : b ⟶ c) (h : c ⟶ d) :
+    (F.mapComp f g).inv ▷ F.map h ≫ (F.mapComp (f ≫ g) h).inv =
+    (α_ (F.map f) (F.map g) (F.map h)).hom ≫ F.map f ◁ (F.mapComp g h).inv ≫
+    (F.mapComp f (g ≫ h)).inv ≫ (F.map₂ (α_ f g h).inv) := by
+  sorry
+
+lemma mapComp_id_left (f : a ⟶ b) : (F.mapComp (𝟙 a) f) = F.map₂Iso (λ_ f) ≪≫
+    (λ_ (F.map f)).symm ≪≫ (whiskerRightIso (F.mapId a) (F.map f)).symm := by
+  ext; simp
+
+lemma mapComp_id_left_hom (f : a ⟶ b) : (F.mapComp (𝟙 a) f).hom =
+    F.map₂ (λ_ f).hom ≫ (λ_ (F.map f)).inv ≫ (F.mapId a).inv ▷ (F.map f) := by
+  simp
+
+lemma mapComp_id_left_inv (f : a ⟶ b) : (F.mapComp (𝟙 a) f).inv =
+    (F.mapId a).hom ▷ (F.map f) ≫ (λ_ (F.map f)).hom ≫ F.map₂ (λ_ f).inv := by
+  simp [mapComp_id_left]
+
+lemma mapId_whiskerRightIso (f : a ⟶ b) : whiskerRightIso (F.mapId a) (F.map f) =
+    (F.mapComp (𝟙 a) f).symm ≪≫ F.map₂Iso (λ_ f) ≪≫ (λ_ (F.map f)).symm := by
+  simp [mapComp_id_left]
+
+lemma mapId_whiskerRight_hom (f : a ⟶ b) : (F.mapId a).hom ▷ F.map f =
+    (F.mapComp (𝟙 a) f).inv ≫ F.map₂ (λ_ f).hom ≫ (λ_ (F.map f)).inv := by
+  simp [mapId_whiskerRightIso]
+
+lemma mapId_whiskerRight_inv (f : a ⟶ b) : (F.mapId a).inv ▷ F.map f =
+    (λ_ (F.map f)).hom ≫ F.map₂ (λ_ f).inv ≫ (F.mapComp (𝟙 a) f).hom := by
+  simpa using congrArg (·.inv) (F.mapId_whiskerRightIso f)
+
+lemma mapComp_id_right (f : a ⟶ b) : (F.mapComp f (𝟙 b)) = F.map₂Iso (ρ_ f) ≪≫
+    (ρ_ (F.map f)).symm ≪≫ (whiskerLeftIso (F.map f) (F.mapId b)).symm := by
+  ext; simp
+
+lemma mapComp_id_right_hom (f : a ⟶ b) : (F.mapComp f (𝟙 b)).hom =
+    F.map₂ (ρ_ f).hom ≫ (ρ_ (F.map f)).inv ≫ F.map f ◁ (F.mapId b).inv := by
+  simp
+
+lemma mapComp_id_right_inv (f : a ⟶ b) : (F.mapComp f (𝟙 b)).inv =
+    (F.map f) ◁ (F.mapId b).hom ≫ (ρ_ (F.map f)).hom ≫ F.map₂ (ρ_ f).inv := by
+  simp [mapComp_id_right]
+
+lemma mapId_whiskerLeftIso (f : a ⟶ b) : whiskerLeftIso (F.map f) (F.mapId b) =
+    (F.mapComp f (𝟙 b)).symm ≪≫ F.map₂Iso (ρ_ f) ≪≫ (ρ_ (F.map f)).symm := by
+  ext; simp
+
+lemma mapId_whiskerLeft_hom (f : a ⟶ b) : F.map f ◁ (F.mapId b).hom =
+    (F.mapComp f (𝟙 b)).inv ≫ F.map₂ (ρ_ f).hom ≫ (ρ_ (F.map f)).inv := by
+  simp [mapId_whiskerLeftIso]
+
+lemma mapId_whiskerLeft_inv (f : a ⟶ b) : F.map f ◁ (F.mapId b).inv =
+    (ρ_ (F.map f)).hom ≫ F.map₂ (ρ_ f).inv ≫ (F.mapComp f (𝟙 b)).hom := by
+  simpa using congrArg (·.inv) (F.mapId_whiskerLeftIso f)
+
+end
+
 /-- Construct a pseudofunctor from an oplax functor whose `mapId` and `mapComp` are isomorphisms.
 -/
 @[simps]
