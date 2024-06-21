bbe58537dad669b52c0dab97a0ad0e40023b8175
Jeremy Tan Jie Rui <reddeloostw@gmail.com>
2024-06-21 08:09:20 +0800

    Update Mathlib/Data/Nat/Choose/Lucas.lean

--- Mathlib/Data/Nat/Choose/Lucas.lean
+++ Mathlib/Data/Nat/Choose/Lucas.lean
@@ -16,7 +16,7 @@ respectively.
 
 ## Main statements
 
-* `lucas_theorem''`: the binomial coefficient `n choose k` is congruent to the
+* `choose_modEq_prod_range_choose`: the binomial coefficient `n choose k` is congruent to the
 product of `n_i choose k_i` modulo `p`, where `n_i` and `k_i` are the base-`p`
 digits of `n` and `k`, respectively.
 -/
