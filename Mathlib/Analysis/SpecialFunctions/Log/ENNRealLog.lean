41a1ae41af840ed5e3a28aaa44bd6189d30eb09b
Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>
2024-06-21 17:35:52 +0200

    Update ENNRealLog.lean
    Co-Authored-By: D-Thomine <100795491+D-Thomine@users.noreply.github.com>

--- Mathlib/Analysis/SpecialFunctions/Log/ENNRealLog.lean
+++ Mathlib/Analysis/SpecialFunctions/Log/ENNRealLog.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2024 Damien Thomine. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Author: Damien Thomine, Pietro Monticone
+Authors: Damien Thomine, Pietro Monticone
 -/
 import Mathlib.Analysis.SpecialFunctions.Pow.NNReal
 import Mathlib.Topology.Instances.EReal
