effbea166cb50b62c75770a9b422b4e2a31f486c
Peter Nelson <71660771+apnelson1@users.noreply.github.com>
2024-06-21 13:29:40 -0400

    remove redundant import

--- Mathlib/Order/KonigLemma.lean
+++ Mathlib/Order/KonigLemma.lean
@@ -3,7 +3,6 @@ Copyright (c) 2024 Peter Nelson. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Peter Nelson
 -/
-import Mathlib.Data.Finite.Set
 import Mathlib.Order.Atoms.Finite
 
 /-!
