82741327fda61050ae19353c70cd5b0fd660d734
adomani <adomani@gmail.com>
2024-06-22 20:12:29 +0100

    chop heads

--- scripts/decls_diff_hybrid.sh
+++ scripts/decls_diff_hybrid.sh
@@ -48,7 +48,5 @@ processDeclsInOneCommit "${commit2}" "${temp2}"
 # restore the cache in the initial branch
 lake exe cache get
 
-sort "${temp1}" | head
-
-printf 'Declarations only in %s\n%s\n\n' "${commit1}" "$(comm -23 <(sort "${temp1}") <(sort "${temp2}"))" | head
-printf 'Declarations only in %s\n%s\n\n' "${commit2}" "$(comm -13 <(sort "${temp1}") <(sort "${temp2}"))" | head
+printf 'Declarations only in %s\n%s\n\n' "${commit1}" "$(comm -23 <(sort "${temp1}") <(sort "${temp2}"))"
+printf 'Declarations only in %s\n%s\n\n' "${commit2}" "$(comm -13 <(sort "${temp1}") <(sort "${temp2}"))"
