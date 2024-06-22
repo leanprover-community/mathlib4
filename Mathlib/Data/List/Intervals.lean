d98fc759fc02cd05bc12ef1a04b3fa35c279ba93
Kim Morrison <kim@tqft.net>
2024-06-22 20:53:37 +1000

    fixes

--- Mathlib/Data/List/Intervals.lean
+++ Mathlib/Data/List/Intervals.lean
@@ -220,8 +220,7 @@ theorem filter_lt_of_succ_bot {n m : ℕ} (hnm : n < m) :
 theorem filter_le_of_bot {n m : ℕ} (hnm : n < m) : ((Ico n m).filter fun x => x ≤ n) = [n] := by
   rw [← filter_lt_of_succ_bot hnm]
   exact filter_congr fun _ _ => by
-    rw [decide_eq_true_eq, decide_eq_true_eq]
-    exact Nat.lt_succ_iff.symm
+    simpa using Nat.lt_succ_iff.symm
 #align list.Ico.filter_le_of_bot List.Ico.filter_le_of_bot
 
 /-- For any natural numbers n, a, and b, one of the following holds:
