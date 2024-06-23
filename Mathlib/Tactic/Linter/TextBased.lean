leanprover-community/mathlib4

commit 00d112fb17411751fb85ca3ba230ab87bbb413d4

feat: rewrite file length check in Lean (#13620)
This is the first in a series of PRs rewriting most checks from `lint-style.py` in Lean.
This PR sets up the basic infrastructure and ports the check for files longer than 1500 lines.
This includes parsing `style-exceptions.txt`.

To enable a gradual conversion of the linters, this PR adds a new executable `lint_style` (which runs the rewritten linters), and has `lint-style.sh` call this one also.

Mathlib/Tactic/Linter/TextBased.lean
@@ -1,0 +1,194 @@
+/-
+Copyright (c) 2024 Michael Rothgang. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Michael Rothgang
+-/
+
+import Batteries.Data.String.Basic
+import Mathlib.Init.Data.Nat.Notation
+
+/-!
+## Text-based linters
+
+This file defines various mathlib linters which are based on reading the source code only.
+In practice, only style linters will have this form.
+All of these have been rewritten from the `lint-style.py` script.
+
+For now, this only contains the linter for too large files:
+further linters will be ported in subsequent PRs.
+
+-/
+
+open System
+
+/-- Possible errors that text-based linters can report. -/
+-- We collect these in one inductive type to centralise error reporting.
+inductive StyleError where
+  /-- The current file was too large: this error contains the current number of lines
+  as well as a size limit (slightly larger). On future runs, this linter will allow this file
+  to grow up to this limit. -/
+  | fileTooLong (number_lines : ℕ) (new_size_limit : ℕ) : StyleError
+  deriving BEq
+
+/-- Create the underlying error message for a given `StyleError`. -/
+def StyleError.errorMessage (err : StyleError) : String := match err with
+  | StyleError.fileTooLong current_size size_limit =>
+      s!"{size_limit} file contains {current_size} lines, try to split it up"
+
+/-- The error code for a given style error. Keep this in sync with `parse?_errorContext` below! -/
+-- FUTURE: we're matching the old codes in `lint-style.py` for compatibility;
+-- in principle, we could also print something more readable.
+def StyleError.errorCode (err : StyleError) : String := match err with
+  | StyleError.fileTooLong _ _ => "ERR_NUM_LIN"
+
+/-- Context for a style error: the actual error, the line number in the file we're reading
+and the path to the file. -/
+structure ErrorContext where
+  /-- The underlying `StyleError`-/
+  error : StyleError
+  /-- The line number of the error -/
+  lineNumber : ℕ
+  /-- The path to the file which was linted -/
+  path : FilePath
+
+/-- The parts of a `StyleError` which are considered when matching against the existing
+  style exceptions: for example, we ignore the particular line length of a "line too long" error. -/
+def StyleError.normalise (err : StyleError) : StyleError := match err with
+  -- NB: keep this in sync with `parse?_errorContext` below.
+  | StyleError.fileTooLong _ _ => StyleError.fileTooLong 0 0
+
+/-- Careful: we do not want to compare `ErrorContexts` exactly; we ignore some details. -/
+instance : BEq ErrorContext where
+  beq ctx ctx' :=
+      ctx.path == ctx'.path
+      -- We completely ignore line numbers of errors. Not sure if this is best.
+      -- We normalise errors before comparing them.
+      && (ctx.error).normalise == (ctx'.error).normalise
+
+/-- Output the formatted error message, containing its context. -/
+def outputMessage (errctx : ErrorContext) : String :=
+  -- We are outputting for github: duplicate file path, line number and error code,
+  -- so that they are also visible in the plain text output.
+  let path := errctx.path
+  let nr := errctx.lineNumber
+  let code := errctx.error.errorCode
+  s!"::ERR file={path},line={nr},code={code}::{path}:{nr} {code}: {errctx.error.errorMessage}"
+
+/-- Try parsing an `ErrorContext` from a string: return `some` if successful, `none` otherwise. -/
+def parse?_errorContext (line : String) : Option ErrorContext := Id.run do
+  let parts := line.split (· == ' ')
+  match parts with
+    | filename :: ":" :: "line" :: _line_number :: ":" :: error_code :: ":" :: error_message =>
+      -- Turn the filename into a path. In general, this is ambiguous if we don't know if we're
+      -- dealing with e.g. Windows or POSIX paths. In our setting, this is fine, since no path
+      -- component contains any path separator.
+      let path := mkFilePath (filename.split (FilePath.pathSeparators.contains ·))
+      -- Parse the error kind from the error code, ugh.
+      -- NB: keep this in sync with `StyleError.errorCode` above!
+      let err : Option StyleError := match error_code with
+        -- Use default values for parameters which are normalised.
+        -- NB: keep this in sync with `normalise` above!
+        | "ERR_NUM_LIN" =>
+          -- Parse the error message in the script. `none` indicates invalid input.
+          match (error_message.get? 0, error_message.get? 3) with
+          | (some limit, some current) =>
+            match (String.toNat? limit, String.toNat? current) with
+            | (some size_limit, some current_size) =>
+              some (StyleError.fileTooLong current_size size_limit)
+            | _ => none
+          | _ => none
+        | _ => none
+      -- Omit the line number, as we don't use it anyway.
+      return (err.map fun e ↦ (ErrorContext.mk e 0 path))
+    -- It would be nice to print an error on any line which doesn't match the above format,
+    -- but is awkward to do so (this `def` is not in any IO monad). Hopefully, this is not necessary
+    -- anyway as the style exceptions file is mostly automatically generated.
+    | _ => none
+
+/-- Parse all style exceptions for a line of input.
+Return an array of all exceptions which could be parsed: invalid input is ignored. -/
+def parseStyleExceptions (lines : Array String) : Array ErrorContext := Id.run do
+  -- We treat all lines starting with "--" as a comment and ignore them.
+  Array.filterMap (parse?_errorContext ·) (lines.filter (fun line ↦ !line.startsWith "--"))
+
+/-- Print information about all errors encountered to standard output. -/
+def formatErrors (errors : Array ErrorContext) : IO Unit := do
+  for e in errors do
+    IO.println (outputMessage e)
+
+/-- Core logic of a text based linter: given a collection of lines,
+return an array of all style errors with line numbers. -/
+abbrev TextbasedLinter := Array String → Array (StyleError × ℕ)
+
+/-! Definitions of the actual text-based linters. -/
+section
+
+/-- Whether a collection of lines consists *only* of imports, blank lines and single-line comments.
+In practice, this means it's an imports-only file and exempt from almost all linting. -/
+def isImportsOnlyFile (lines : Array String) : Bool :=
+  -- The Python version also excluded multi-line comments: for all files generated by `mk_all`,
+  -- this is in fact not necessary.
+  lines.all (fun line ↦ line.startsWith "import " || line == "" || line.startsWith "-- ")
+
+/-- Error if a collection of lines is too large. "Too large" means more than 1500 lines
+**and** longer than an optional previous limit.
+If the file is too large, return a matching `StyleError`, which includes a new size limit
+(which is somewhat larger than the current size). -/
+def checkFileLength (lines : Array String) (existing_limit : Option ℕ) : Option StyleError :=
+  Id.run do
+  if lines.size > 1500 then
+    let is_larger : Bool := match existing_limit with
+    | some mark => lines.size > mark
+    | none => true
+    if is_larger then
+      -- We add about 200 lines of slack to the current file size: small PRs will be unaffected,
+      -- but sufficiently large PRs will get nudged towards splitting up this file.
+      return some (StyleError.fileTooLong lines.size ((Nat.div lines.size 100) * 100 + 200))
+  none
+
+end
+
+/-- Read a file, apply all text-based linters and print formatted errors.
+Return `true` if there were new errors (and `false` otherwise).
+`sizeLimit` is any pre-existing limit on this file's size. -/
+def lintFile (path : FilePath) (sizeLimit : Option ℕ) : IO Bool := do
+  let lines ← IO.FS.lines path
+  -- We don't need to run any checks on imports-only files.
+  -- NB. The Python script used to still run a few linters; this is in fact not necessary.
+  if isImportsOnlyFile lines then
+    return false
+  if let some (StyleError.fileTooLong n limit) := checkFileLength lines sizeLimit then
+    let arr := Array.mkArray1 (ErrorContext.mk (StyleError.fileTooLong n limit) 1 path)
+    formatErrors arr
+    return true
+  return false
+
+/-- Lint all files referenced in a given import-only file.
+Return the number of files which had new style errors. -/
+def lintAllFiles (path : FilePath) : IO UInt32 := do
+  -- Read all module names from the file at `path`.
+  let allModules ← IO.FS.lines path
+  -- Read the style exceptions file.
+  let exceptions_file ← IO.FS.lines (mkFilePath ["scripts", "style-exceptions.txt"])
+  let mut style_exceptions := parseStyleExceptions exceptions_file
+  let mut number_error_files := 0
+  for module in allModules do
+    let module := module.stripPrefix "import "
+    -- Convert the module name to a file name, then lint that file.
+    let path := (mkFilePath (module.split (· == '.'))).addExtension "lean"
+    -- Find the size limit for this given file.
+    -- If several size limits are given (unlikely in practice), we use the first one.
+    let size_limits := (style_exceptions.filter (·.path == path)).filterMap (fun errctx ↦
+      match errctx.error with
+      | StyleError.fileTooLong _ limit => some limit)
+    if ← lintFile path (size_limits.get? 0) then
+      number_error_files := number_error_files + 1
+  return number_error_files
+
+/-- The entry point to the `lake exe lint_style` command. -/
+def main : IO UInt32 := do
+  let mut number_error_files := 0
+  for s in ["Archive.lean", "Counterexamples.lean", "Mathlib.lean"] do
+    let n ← lintAllFiles (mkFilePath [s])
+    number_error_files := number_error_files + n
+  return number_error_files

scripts/lint-style.sh
@@ -26,6 +26,7 @@ set -exo pipefail
 # 1. A Python script `scripts/lint-style.py` that lints the contents of a Lean file.
 #    This script is called below on all Lean files in the repository.
 #    Exceptions are maintained in `scripts/style-exceptions.txt`.
+#    (Rewriting these checks in Lean is work in progress.)
 # 2. The remainder of this shell script
 #    contains some lints on the global repository.
 #
@@ -40,6 +41,9 @@ touch scripts/style-exceptions.txt
 git ls-files 'Mathlib/*.lean' | xargs ./scripts/lint-style.py "$@"
 git ls-files 'Archive/*.lean' | xargs ./scripts/lint-style.py "$@"
 git ls-files 'Counterexamples/*.lean' | xargs ./scripts/lint-style.py "$@"
+
+# Call the in-progress Lean rewrite of these Python lints.
+lake exe lint_style
 
 # 2. Global checks on the mathlib repository
 

scripts/update-style-exceptions.sh
@@ -16,3 +16,6 @@
 # use C locale so that sorting is the same on macOS and Linux
 # see https://unix.stackexchange.com/questions/362728/why-does-gnu-sort-sort-differently-on-my-osx-machine-and-linux-machine
 find Mathlib -name '*.lean' | xargs ./scripts/lint-style.py | LC_ALL=C sort > scripts/style-exceptions.txt
+
+# Append the warnings of the Lean linter, on the file length.
+lake exe lint_style >> scripts/style-exceptions.txt

.github/workflows/bors.yml
@@ -50,7 +50,14 @@ jobs:
         with:
           python-version: 3.8
 
-      - name: lint
+      - name: install elan
+        run: |
+          set -o pipefail
+          curl -sSfL https://github.com/leanprover/elan/releases/download/v3.0.0/elan-x86_64-unknown-linux-gnu.tar.gz | tar xz
+          ./elan-init -y --default-toolchain none
+          echo "$HOME/.elan/bin" >> "${GITHUB_PATH}"
+
+      - name: "run style linters: Python ones and their Lean rewrite"
         run: |
           ./scripts/lint-style.sh
 

.github/workflows/build.yml
@@ -57,7 +57,14 @@ jobs:
         with:
           python-version: 3.8
 
-      - name: lint
+      - name: install elan
+        run: |
+          set -o pipefail
+          curl -sSfL https://github.com/leanprover/elan/releases/download/v3.0.0/elan-x86_64-unknown-linux-gnu.tar.gz | tar xz
+          ./elan-init -y --default-toolchain none
+          echo "$HOME/.elan/bin" >> "${GITHUB_PATH}"
+
+      - name: "run style linters: Python ones and their Lean rewrite"
         run: |
           ./scripts/lint-style.sh
 

.github/workflows/build.yml.in
@@ -36,7 +36,14 @@ jobs:
         with:
           python-version: 3.8
 
-      - name: lint
+      - name: install elan
+        run: |
+          set -o pipefail
+          curl -sSfL https://github.com/leanprover/elan/releases/download/v3.0.0/elan-x86_64-unknown-linux-gnu.tar.gz | tar xz
+          ./elan-init -y --default-toolchain none
+          echo "$HOME/.elan/bin" >> "${GITHUB_PATH}"
+
+      - name: "run style linters: Python ones and their Lean rewrite"
         run: |
           ./scripts/lint-style.sh
 

.github/workflows/build_fork.yml
@@ -54,7 +54,14 @@ jobs:
         with:
           python-version: 3.8
 
-      - name: lint
+      - name: install elan
+        run: |
+          set -o pipefail
+          curl -sSfL https://github.com/leanprover/elan/releases/download/v3.0.0/elan-x86_64-unknown-linux-gnu.tar.gz | tar xz
+          ./elan-init -y --default-toolchain none
+          echo "$HOME/.elan/bin" >> "${GITHUB_PATH}"
+
+      - name: "run style linters: Python ones and their Lean rewrite"
         run: |
           ./scripts/lint-style.sh
 

Mathlib.lean
@@ -3894,6 +3894,7 @@ import Mathlib.Tactic.Linter.GlobalAttributeIn
 import Mathlib.Tactic.Linter.HashCommandLinter
 import Mathlib.Tactic.Linter.Lint
 import Mathlib.Tactic.Linter.Style
+import Mathlib.Tactic.Linter.TextBased
 import Mathlib.Tactic.Measurability
 import Mathlib.Tactic.Measurability.Init
 import Mathlib.Tactic.MkIffOfInductiveProp

commit ca9e9db115e3be073fd4072551e646990c1ca24b

feat(lint-style): fix `update-style-exceptions.py`; produce human-readable output by default (#14012)
- Fix the output of `update-style-exceptions.py` by making `lake exe lint_style` optionally produce output in the right format: this regressed in #13620
- The current error messages are tailored for github annotations, which are not very readable for running the linter locally. Produce a human-readable and clickable error by default, but add a flag (which CI sets) for producing github-style output.

This entails adding a small CLI for the lint-style executable.

Mathlib/Tactic/Linter/TextBased.lean
@@ -5,6 +5,7 @@ Authors: Michael Rothgang
 -/
 
 import Batteries.Data.String.Basic
+import Cli.Basic
 import Mathlib.Init.Data.Nat.Notation
 
 /-!
@@ -30,10 +31,27 @@ inductive StyleError where
   | fileTooLong (number_lines : ℕ) (new_size_limit : ℕ) : StyleError
   deriving BEq
 
+/-- How to format style errors -/
+inductive ErrorFormat
+  /-- Produce style error output aimed at humans: no error code, clickable file name -/
+  | humanReadable : ErrorFormat
+  /-- Produce an entry in the style-exceptions file: mention the error code, slightly uglier
+  than humand-readable output -/
+  | exceptionsFile : ErrorFormat
+  /-- Produce output suitable for Github error annotations: in particular,
+  duplicate the file path, line number and error code -/
+  | github : ErrorFormat
+  deriving BEq
+
 /-- Create the underlying error message for a given `StyleError`. -/
-def StyleError.errorMessage (err : StyleError) : String := match err with
+def StyleError.errorMessage (err : StyleError) (style : ErrorFormat) : String := match err with
   | StyleError.fileTooLong current_size size_limit =>
-      s!"{size_limit} file contains {current_size} lines, try to split it up"
+    match style with
+    | ErrorFormat.github =>
+        s!"file contains {current_size} lines (at most {size_limit} allowed), try to split it up"
+    | ErrorFormat.exceptionsFile =>
+        s!"{size_limit} file contains {current_size} lines, try to split it up"
+    | ErrorFormat.humanReadable => s!"file contains {current_size} lines, try to split it up"
 
 /-- The error code for a given style error. Keep this in sync with `parse?_errorContext` below! -/
 -- FUTURE: we're matching the old codes in `lint-style.py` for compatibility;
@@ -65,14 +83,24 @@ instance : BEq ErrorContext where
       -- We normalise errors before comparing them.
       && (ctx.error).normalise == (ctx'.error).normalise
 
-/-- Output the formatted error message, containing its context. -/
-def outputMessage (errctx : ErrorContext) : String :=
-  -- We are outputting for github: duplicate file path, line number and error code,
-  -- so that they are also visible in the plain text output.
-  let path := errctx.path
-  let nr := errctx.lineNumber
-  let code := errctx.error.errorCode
-  s!"::ERR file={path},line={nr},code={code}::{path}:{nr} {code}: {errctx.error.errorMessage}"
+/-- Output the formatted error message, containing its context.
+`style` specifies if the error should be formatted for humans or for github output matchers -/
+def outputMessage (errctx : ErrorContext) (style : ErrorFormat) : String :=
+  let error_message := errctx.error.errorMessage style
+  match style with
+  | ErrorFormat.github =>
+   -- We are outputting for github: duplicate file path, line number and error code,
+    -- so that they are also visible in the plain text output.
+    let path := errctx.path
+    let nr := errctx.lineNumber
+    let code := errctx.error.errorCode
+    s!"::ERR file={path},line={nr},code={code}::{path}:{nr} {code}: {error_message}"
+  | ErrorFormat.exceptionsFile =>
+    -- Produce an entry in the exceptions file: with error code and "line" in front of the number.
+    s!"{errctx.path} : line {errctx.lineNumber} : {errctx.error.errorCode} : {error_message}"
+  | ErrorFormat.humanReadable =>
+    -- Print for humans: clickable file name and omit the error code
+    s!"error: {errctx.path}:{errctx.lineNumber}: {error_message}"
 
 /-- Try parsing an `ErrorContext` from a string: return `some` if successful, `none` otherwise. -/
 def parse?_errorContext (line : String) : Option ErrorContext := Id.run do
@@ -111,10 +139,11 @@ def parseStyleExceptions (lines : Array String) : Array ErrorContext := Id.run d
   -- We treat all lines starting with "--" as a comment and ignore them.
   Array.filterMap (parse?_errorContext ·) (lines.filter (fun line ↦ !line.startsWith "--"))
 
-/-- Print information about all errors encountered to standard output. -/
-def formatErrors (errors : Array ErrorContext) : IO Unit := do
+/-- Print information about all errors encountered to standard output.
+`style` specifies if we print errors for humand or github consumption. -/
+def formatErrors (errors : Array ErrorContext) (style : ErrorFormat) : IO Unit := do
   for e in errors do
-    IO.println (outputMessage e)
+    IO.println (outputMessage e style)
 
 /-- Core logic of a text based linter: given a collection of lines,
 return an array of all style errors with line numbers. -/
@@ -150,8 +179,9 @@ end
 
 /-- Read a file, apply all text-based linters and print formatted errors.
 Return `true` if there were new errors (and `false` otherwise).
-`sizeLimit` is any pre-existing limit on this file's size. -/
-def lintFile (path : FilePath) (sizeLimit : Option ℕ) : IO Bool := do
+`sizeLimit` is any pre-existing limit on this file's size.
+`style` specifies if errors should be formatted for github or human consumption. -/
+def lintFile (path : FilePath) (sizeLimit : Option ℕ) (style : ErrorFormat) : IO Bool := do
   let lines ← IO.FS.lines path
   -- We don't need to run any checks on imports-only files.
   -- NB. The Python script used to still run a few linters; this is in fact not necessary.
@@ -159,13 +189,14 @@ def lintFile (path : FilePath) (sizeLimit : Option ℕ) : IO Bool := do
     return false
   if let some (StyleError.fileTooLong n limit) := checkFileLength lines sizeLimit then
     let arr := Array.mkArray1 (ErrorContext.mk (StyleError.fileTooLong n limit) 1 path)
-    formatErrors arr
+    formatErrors arr style
     return true
   return false
 
 /-- Lint all files referenced in a given import-only file.
-Return the number of files which had new style errors. -/
-def lintAllFiles (path : FilePath) : IO UInt32 := do
+Return the number of files which had new style errors.
+`style` specifies if errors should be formatted for github or human consumption. -/
+def lintAllFiles (path : FilePath) (style : ErrorFormat) : IO UInt32 := do
   -- Read all module names from the file at `path`.
   let allModules ← IO.FS.lines path
   -- Read the style exceptions file.
@@ -181,14 +212,36 @@ def lintAllFiles (path : FilePath) : IO UInt32 := do
     let size_limits := (style_exceptions.filter (·.path == path)).filterMap (fun errctx ↦
       match errctx.error with
       | StyleError.fileTooLong _ limit => some limit)
-    if ← lintFile path (size_limits.get? 0) then
+    if ← lintFile path (size_limits.get? 0) style then
       number_error_files := number_error_files + 1
   return number_error_files
 
-/-- The entry point to the `lake exe lint_style` command. -/
-def main : IO UInt32 := do
-  let mut number_error_files := 0
+open Cli in
+/-- Implementation of the `lint_style` command line program. -/
+def lintStyleCli (args : Cli.Parsed) : IO UInt32 := do
+  let errorStyle := match (args.hasFlag "github", args.hasFlag "update") with
+    | (true, _) => ErrorFormat.github
+    | (false, true) => ErrorFormat.exceptionsFile
+    | (false, false) => ErrorFormat.humanReadable
+  let mut number_error_files : UInt32 := 0
   for s in ["Archive.lean", "Counterexamples.lean", "Mathlib.lean"] do
-    let n ← lintAllFiles (mkFilePath [s])
+    let n ← lintAllFiles (mkFilePath [s]) errorStyle
     number_error_files := number_error_files + n
-  return number_error_files
+   return number_error_files
+
+open Cli in
+/-- Setting up command line options and help text for `lake exe lint_style`. -/
+-- so far, no help options or so: perhaps that is fine?
+def lint_style : Cmd := `[Cli|
+  lint_style VIA lintStyleCli; ["0.0.1"]
+  "Run text-based style linters on every Lean file in Mathlib/, Archive/ and Counterexamples/.
+  Print errors about any unexpected style errors to standard output."
+
+  FLAGS:
+    github;     "Print errors in a format suitable for github problem matchers\n\
+                 otherwise, produce human-readable output"
+    update;     "Print errors solely for the style exceptions file"
+]
+
+/-- The entry point to the `lake exe lint_style` command. -/
+def main (args : List String) : IO UInt32 := do lint_style.validate args

scripts/lint-style.sh
@@ -43,7 +43,7 @@ git ls-files 'Archive/*.lean' | xargs ./scripts/lint-style.py "$@"
 git ls-files 'Counterexamples/*.lean' | xargs ./scripts/lint-style.py "$@"
 
 # Call the in-progress Lean rewrite of these Python lints.
-lake exe lint_style
+lake exe lint_style --github
 
 # 2. Global checks on the mathlib repository
 

scripts/update-style-exceptions.sh
@@ -18,4 +18,4 @@
 find Mathlib -name '*.lean' | xargs ./scripts/lint-style.py | LC_ALL=C sort > scripts/style-exceptions.txt
 
 # Append the warnings of the Lean linter, on the file length.
-lake exe lint_style >> scripts/style-exceptions.txt
+lake exe lint_style --update >> scripts/style-exceptions.txt