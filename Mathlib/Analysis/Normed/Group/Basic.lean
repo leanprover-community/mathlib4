b5efb8c90ba0ab1f668768892b760eccaa8f10fe
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2024-06-23 06:39:04 +0000

    chore: split some results out of Analysis.Normed.Group.Basic (#14035)
    This flips the longest pole back to ring theory / linear algebra.

--- Mathlib/Analysis/Normed/Group/Basic.lean
+++ Mathlib/Analysis/Normed/Group/Basic.lean
@@ -3,8 +3,10 @@ Copyright (c) 2018 Patrick Massot. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Patrick Massot, Johannes Hölzl, Yaël Dillies
 -/
+import Mathlib.Algebra.CharP.Defs
+import Mathlib.Algebra.Group.Subgroup.Basic
 import Mathlib.Analysis.Normed.Group.Seminorm
-import Mathlib.Topology.Instances.Rat
+import Mathlib.Topology.Metrizable.Uniformity
 import Mathlib.Topology.Sequences
 
 #align_import analysis.normed.group.basic from "leanprover-community/mathlib"@"41bef4ae1254365bc190aee63b947674d2977f01"
@@ -1513,80 +1515,6 @@ instance : NNNorm ℝ≥0 where
 
 end NNReal
 
-namespace Int
-
-instance instNormedAddCommGroup : NormedAddCommGroup ℤ where
-  norm n := ‖(n : ℝ)‖
-  dist_eq m n := by simp only [Int.dist_eq, norm, Int.cast_sub]
-
-@[norm_cast]
-theorem norm_cast_real (m : ℤ) : ‖(m : ℝ)‖ = ‖m‖ :=
-  rfl
-#align int.norm_cast_real Int.norm_cast_real
-
-theorem norm_eq_abs (n : ℤ) : ‖n‖ = |(n : ℝ)| :=
-  rfl
-#align int.norm_eq_abs Int.norm_eq_abs
-
-@[simp]
-theorem norm_natCast (n : ℕ) : ‖(n : ℤ)‖ = n := by simp [Int.norm_eq_abs]
-#align int.norm_coe_nat Int.norm_natCast
-
-@[deprecated (since := "2024-04-05")] alias norm_coe_nat := norm_natCast
-
-theorem _root_.NNReal.natCast_natAbs (n : ℤ) : (n.natAbs : ℝ≥0) = ‖n‖₊ :=
-  NNReal.eq <|
-    calc
-      ((n.natAbs : ℝ≥0) : ℝ) = (n.natAbs : ℤ) := by simp only [Int.cast_natCast, NNReal.coe_natCast]
-      _ = |(n : ℝ)| := by simp only [Int.natCast_natAbs, Int.cast_abs]
-      _ = ‖n‖ := (norm_eq_abs n).symm
-#align nnreal.coe_nat_abs NNReal.natCast_natAbs
-
-theorem abs_le_floor_nnreal_iff (z : ℤ) (c : ℝ≥0) : |z| ≤ ⌊c⌋₊ ↔ ‖z‖₊ ≤ c := by
-  rw [Int.abs_eq_natAbs, Int.ofNat_le, Nat.le_floor_iff (zero_le c), NNReal.natCast_natAbs z]
-#align int.abs_le_floor_nnreal_iff Int.abs_le_floor_nnreal_iff
-
-end Int
-
-namespace Rat
-
-instance instNormedAddCommGroup : NormedAddCommGroup ℚ where
-  norm r := ‖(r : ℝ)‖
-  dist_eq r₁ r₂ := by simp only [Rat.dist_eq, norm, Rat.cast_sub]
-
-@[norm_cast, simp 1001]
--- Porting note: increase priority to prevent the left-hand side from simplifying
-theorem norm_cast_real (r : ℚ) : ‖(r : ℝ)‖ = ‖r‖ :=
-  rfl
-#align rat.norm_cast_real Rat.norm_cast_real
-
-@[norm_cast, simp]
-theorem _root_.Int.norm_cast_rat (m : ℤ) : ‖(m : ℚ)‖ = ‖m‖ := by
-  rw [← Rat.norm_cast_real, ← Int.norm_cast_real]; congr 1
-#align int.norm_cast_rat Int.norm_cast_rat
-
-end Rat
-
--- Now that we've installed the norm on `ℤ`,
--- we can state some lemmas about `zsmul`.
-section
-
-variable [SeminormedCommGroup α]
-
-@[to_additive norm_zsmul_le]
-theorem norm_zpow_le_mul_norm (n : ℤ) (a : α) : ‖a ^ n‖ ≤ ‖n‖ * ‖a‖ := by
-  rcases n.eq_nat_or_neg with ⟨n, rfl | rfl⟩ <;> simpa using norm_pow_le_mul_norm n a
-#align norm_zpow_le_mul_norm norm_zpow_le_mul_norm
-#align norm_zsmul_le norm_zsmul_le
-
-@[to_additive nnnorm_zsmul_le]
-theorem nnnorm_zpow_le_mul_norm (n : ℤ) (a : α) : ‖a ^ n‖₊ ≤ ‖n‖₊ * ‖a‖₊ := by
-  simpa only [← NNReal.coe_le_coe, NNReal.coe_mul] using norm_zpow_le_mul_norm n a
-#align nnnorm_zpow_le_mul_norm nnnorm_zpow_le_mul_norm
-#align nnnorm_zsmul_le nnnorm_zsmul_le
-
-end
-
 end SeminormedCommGroup
 
 section NormedGroup
@@ -1765,45 +1693,3 @@ instance normedCommGroup [NormedCommGroup E] {s : Subgroup E} : NormedCommGroup 
 #align add_subgroup.normed_add_comm_group AddSubgroup.normedAddCommGroup
 
 end Subgroup
-
-/-! ### Submodules of normed groups -/
-
-
-namespace Submodule
-
--- See note [implicit instance arguments]
-/-- A submodule of a seminormed group is also a seminormed group, with the restriction of the norm.
--/
-instance seminormedAddCommGroup [Ring 𝕜] [SeminormedAddCommGroup E] [Module 𝕜 E]
-    (s : Submodule 𝕜 E) : SeminormedAddCommGroup s :=
-  SeminormedAddCommGroup.induced _ _ s.subtype.toAddMonoidHom
-#align submodule.seminormed_add_comm_group Submodule.seminormedAddCommGroup
-
--- See note [implicit instance arguments].
-/-- If `x` is an element of a submodule `s` of a normed group `E`, its norm in `s` is equal to its
-norm in `E`. -/
-@[simp]
-theorem coe_norm [Ring 𝕜] [SeminormedAddCommGroup E] [Module 𝕜 E] {s : Submodule 𝕜 E}
-    (x : s) : ‖x‖ = ‖(x : E)‖ :=
-  rfl
-#align submodule.coe_norm Submodule.coe_norm
-
--- See note [implicit instance arguments].
-/-- If `x` is an element of a submodule `s` of a normed group `E`, its norm in `E` is equal to its
-norm in `s`.
-
-This is a reversed version of the `simp` lemma `Submodule.coe_norm` for use by `norm_cast`. -/
-@[norm_cast]
-theorem norm_coe [Ring 𝕜] [SeminormedAddCommGroup E] [Module 𝕜 E] {s : Submodule 𝕜 E}
-    (x : s) : ‖(x : E)‖ = ‖x‖ :=
-  rfl
-#align submodule.norm_coe Submodule.norm_coe
-
--- See note [implicit instance arguments].
-/-- A submodule of a normed group is also a normed group, with the restriction of the norm. -/
-instance normedAddCommGroup [Ring 𝕜] [NormedAddCommGroup E] [Module 𝕜 E]
-    (s : Submodule 𝕜 E) : NormedAddCommGroup s :=
-  { Submodule.seminormedAddCommGroup s with
-    eq_of_dist_eq_zero := eq_of_dist_eq_zero }
-
-end Submodule
