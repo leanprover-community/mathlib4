6cb157b368d9d4ab151eb397c51aa77e3271e7bd
Kim Morrison <kim@tqft.net>
2024-06-24 13:44:41 +1000

    fix

--- Archive/Examples/IfNormalization/Result.lean
+++ Archive/Examples/IfNormalization/Result.lean
@@ -72,7 +72,7 @@ def normalize (l : AList (fun _ : ℕ => Bool)) :
       ⟨if t' = e' then t' else .ite (var v) t' e', by
         refine ⟨fun f => ?_, ?_, fun w b => ?_⟩
         · -- eval = eval
-          simp? says simp only [apply_ite, eval_ite_var, Option.elim, ite_eq_iff']
+          simp only [apply_ite, eval_ite_var]
           cases hfv : f v
           · simp_all
             congr
