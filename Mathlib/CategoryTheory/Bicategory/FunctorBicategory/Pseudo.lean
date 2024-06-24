72b6bace8ad9d17f12019dbc860c893465fbc4ed
Calle Sönne <calle.sonne@gmail.com>
2024-06-24 12:12:57 +0100

    fix documentation

--- Mathlib/CategoryTheory/Bicategory/FunctorBicategory/Pseudo.lean
+++ Mathlib/CategoryTheory/Bicategory/FunctorBicategory/Pseudo.lean
@@ -7,11 +7,6 @@ Authors: Calle Sönne
 import Mathlib.CategoryTheory.Bicategory.NaturalTransformation.Strong
 import Mathlib.CategoryTheory.Bicategory.FunctorBicategory.Oplax
 
--- probably optional?
-import Mathlib.CategoryTheory.EqToHom
-import Mathlib.Tactic.CategoryTheory.Coherence
-import Mathlib.CategoryTheory.Bicategory.Coherence
-
 /-!
 # The bicategory of pseudofunctors between two bicategories
 
@@ -32,56 +27,52 @@ universe w₁ w₂ v₁ v₂ u₁ u₂
 
 variable {B : Type u₁} [Bicategory.{w₁, v₁} B] {C : Type u₂} [Bicategory.{w₂, v₂} C]
 
-namespace StrongNatTrans
+namespace Pseudofunctor.Bicategory
 
--- TODO: these hold for oplax functors too (but then we don't have bicategory structure)
 variable {F G H I : Pseudofunctor B C}
 
--- TODO: need locally fully faithful sub-bicategory structure/API....
-
-/-- Left whiskering of a strong natural transformation and a modification. -/
-@[simps]
+/-- Left whiskering of a strong natural transformation between pseudofunctors
+and a modification. -/
+@[simps!]
 def whiskerLeft (η : F ⟶ G) {θ ι : G ⟶ H} (Γ : θ ⟶ ι) : η ≫ θ ⟶ η ≫ ι :=
   OplaxNatTrans.whiskerLeft η.toOplax Γ
 
-/-- Right whiskering of an oplax natural transformation and a modification. -/
-@[simps]
+/-- Right whiskering of an strong natural transformation between pseudofunctors
+and a modification. -/
+@[simps!]
 def whiskerRight {η θ : F ⟶ G} (Γ : η ⟶ θ) (ι : G ⟶ H) : η ≫ ι ⟶ θ ≫ ι :=
   OplaxNatTrans.whiskerRight Γ ι.toOplax
 
-/-- Associator for the vertical composition of oplax natural transformations. -/
--- Porting note: verified that projections are correct and changed @[simps] to @[simps!]
--- @[simps!]
+/-- Associator for the vertical composition of strong natural transformations
+between pseudofunctors. -/
+@[simps!]
 def associator (η : F ⟶ G) (θ : G ⟶ H) (ι : H ⟶ I) : (η ≫ θ) ≫ ι ≅ η ≫ θ ≫ ι :=
-  { OplaxNatTrans.associator η.toOplax θ.toOplax ι.toOplax with}
+  { OplaxNatTrans.associator η.toOplax θ.toOplax ι.toOplax with }
 
-/-- Left unitor for the vertical composition of oplax natural transformations. -/
--- Porting note: verified that projections are correct and changed @[simps] to @[simps!]
+/-- Left unitor for the vertical composition of strong natural transformations
+between pseudofunctors. -/
 @[simps!]
 def leftUnitor (η : F ⟶ G) : 𝟙 F ≫ η ≅ η :=
   { OplaxNatTrans.leftUnitor η.toOplax with }
-  --OplaxNatTrans.ModificationIso.ofComponents (fun a => λ_ (η.app a)) (by aesop_cat)
 
-/-- Right unitor for the vertical composition of oplax natural transformations. -/
--- Porting note: verified that projections are correct and changed @[simps] to @[simps!]
+/-- Right unitor for the vertical composition of strong natural transformations
+between pseudofunctors. -/
 @[simps!]
 def rightUnitor (η : F ⟶ G) : η ≫ 𝟙 G ≅ η :=
   { OplaxNatTrans.rightUnitor η.toOplax with }
 
-end StrongNatTrans
+end Pseudofunctor.Bicategory
 
 variable (B C)
 
-#check Bicategory.comp_whiskerLeft
-
-/-- A bicategory structure on the oplax functors between bicategories. -/
+/-- A bicategory structure on the pseudofunctors between two bicategories. -/
 @[simps!]
 instance Pseudofunctor.bicategory : Bicategory (Pseudofunctor B C) where
-  whiskerLeft {F G H} η _ _ Γ := StrongNatTrans.whiskerLeft η Γ
-  whiskerRight {F G H} _ _ Γ η := StrongNatTrans.whiskerRight Γ η
-  associator {F G H} I := StrongNatTrans.associator
-  leftUnitor {F G} := StrongNatTrans.leftUnitor
-  rightUnitor {F G} := StrongNatTrans.rightUnitor
+  whiskerLeft {F G H} η _ _ Γ := Pseudofunctor.Bicategory.whiskerLeft η Γ
+  whiskerRight {F G H} _ _ Γ η := Pseudofunctor.Bicategory.whiskerRight Γ η
+  associator {F G H} I := Pseudofunctor.Bicategory.associator
+  leftUnitor {F G} := Pseudofunctor.Bicategory.leftUnitor
+  rightUnitor {F G} := Pseudofunctor.Bicategory.rightUnitor
   whisker_exchange {a b c f g h i} η θ := by ext; exact whisker_exchange _ _
   pentagon f g h i := by ext; exact pentagon _ _ _ _
   triangle f g := by ext; exact triangle _ _
