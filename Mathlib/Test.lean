a8cfabf01fb85ea69eef4ae62fc6a10e8c74a560
adomani <adomani@gmail.com>
2024-06-24 08:16:50 +0100

    lint

--- Mathlib/Test.lean
+++ Mathlib/Test.lean
@@ -1,1 +1,10 @@
+/-
+Copyright (c) 2024 Damiano Testa. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Damiano Testa
+-/
+
+/-!
+This file only serves the purpose of existing and not being in the import all files.
+-/
 namespace Test
