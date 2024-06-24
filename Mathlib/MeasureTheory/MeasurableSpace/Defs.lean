e82afa40085d72918674d6ee91f4e12fc70d717d
grunweg <rothgami@math.hu-berlin.de>
2024-06-24 11:40:42 +0000

    chore: distribute fun_prop tagging of measurability lemmas (#13869)
    into the respective files: essentially dissolves the `FunProp/Measurable` and `AEMeasurable` files.
    (The latter is kept to note two missing statements. It is not imported anywhere any more.)
    
    This is a pre-requisite for #13781, so fun_prop can be used without breaking existing the `assert_not_exists` statements.

--- Mathlib/MeasureTheory/MeasurableSpace/Defs.lean
+++ Mathlib/MeasureTheory/MeasurableSpace/Defs.lean
@@ -5,6 +5,7 @@ Authors: Johannes Hölzl, Mario Carneiro
 -/
 import Mathlib.Data.Set.Countable
 import Mathlib.Order.Disjointed
+import Mathlib.Tactic.FunProp.Attr
 import Mathlib.Tactic.Measurability
 
 #align_import measure_theory.measurable_space_def from "leanprover-community/mathlib"@"001ffdc42920050657fd45bd2b8bfbec8eaaeb29"
@@ -550,6 +551,7 @@ end MeasurableSpace
 
 /-- A function `f` between measurable spaces is measurable if the preimage of every
   measurable set is measurable. -/
+@[fun_prop]
 def Measurable [MeasurableSpace α] [MeasurableSpace β] (f : α → β) : Prop :=
   ∀ ⦃t : Set β⦄, MeasurableSet t → MeasurableSet (f ⁻¹' t)
 #align measurable Measurable
@@ -568,7 +570,7 @@ section MeasurableFunctions
 theorem measurable_id {_ : MeasurableSpace α} : Measurable (@id α) := fun _ => id
 #align measurable_id measurable_id
 
-@[measurability]
+@[fun_prop, measurability]
 theorem measurable_id' {_ : MeasurableSpace α} : Measurable fun a : α => a := measurable_id
 #align measurable_id' measurable_id'
 
@@ -579,12 +581,12 @@ protected theorem Measurable.comp {_ : MeasurableSpace α} {_ : MeasurableSpace 
 #align measurable.comp Measurable.comp
 
 -- This is needed due to reducibility issues with the `measurability` tactic.
-@[aesop safe 50 (rule_sets := [Measurable])]
+@[fun_prop, aesop safe 50 (rule_sets := [Measurable])]
 protected theorem Measurable.comp' {_ : MeasurableSpace α} {_ : MeasurableSpace β}
     {_ : MeasurableSpace γ} {g : β → γ} {f : α → β} (hg : Measurable g) (hf : Measurable f) :
     Measurable (fun x => g (f x)) := Measurable.comp hg hf
 
-@[simp, measurability]
+@[simp, fun_prop, measurability]
 theorem measurable_const {_ : MeasurableSpace α} {_ : MeasurableSpace β} {a : α} :
     Measurable fun _ : β => a := fun s _ => .const (a ∈ s)
 #align measurable_const measurable_const
