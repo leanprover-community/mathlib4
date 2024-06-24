62ceaacadcaf0f125c51a24a037711de8f3083dd
Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>
2024-06-25 00:44:33 +0200

    Update Mathlib/Data/Real/EReal.lean
    Co-authored-by: Jireh Loreaux <loreaujy@gmail.com>

--- Mathlib/Data/Real/EReal.lean
+++ Mathlib/Data/Real/EReal.lean
@@ -800,7 +800,7 @@ theorem top_add_coe (x : ℝ) : (⊤ : EReal) + x = ⊤ :=
   rfl
 #align ereal.top_add_coe EReal.top_add_coe
 
-/--For any extended real number `x` which is not `⊥`, the sum of `⊤` and `x` is equal to `⊤`.-/
+/-- For any extended real number `x` which is not `⊥`, the sum of `⊤` and `x` is equal to `⊤`. -/
 @[simp]
 theorem top_add_of_ne_bot {x : EReal} (h : x ≠ ⊥) : ⊤ + x = ⊤ := by
   induction x using EReal.rec
