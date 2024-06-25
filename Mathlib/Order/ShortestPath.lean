504b258c6e5aa20c1b69ee2ff4ae0a9057fc7d89
Peter Nelson <71660771+apnelson1@users.noreply.github.com>
2024-06-25 16:17:28 -0400

    wip

--- Mathlib/Order/ShortestPath.lean
+++ Mathlib/Order/ShortestPath.lean
@@ -1,10 +1,13 @@
 import Mathlib.Combinatorics.SimpleGraph.Metric
 import Mathlib.Combinatorics.SimpleGraph.Finite
+import Mathlib.Order.Grade
 import Mathlib.Tactic.Linarith
 
 section Graph
 
 namespace SimpleGraph
+
+open Walk
 
 variable {V : Type*} {x y z a b : V} {G : SimpleGraph V} {W : G.Walk x y}
 
@@ -16,6 +19,43 @@ variable {V : Type*} {x y z a b : V} {G : SimpleGraph V} {W : G.Walk x y}
 --   have hs : {n : ℕ | ∃ (P : G.Path x y), P.1.length = n}.Nonempty := ⟨_, h.some.toPath , rfl⟩
 --   obtain ⟨P, hP⟩ := Nat.sInf_mem hs
 --   exact ⟨P, fun Q ↦ hP ▸ Nat.sInf_le ⟨Q, rfl⟩⟩
+
+
+lemma foo (W W' : G.Walk x y) (h : W.support = W'.support) : W = W' := by
+  by_cases hW : W.support = [x]
+  · rw [hW] at h
+
+  -- by_cases hW : W.Nil
+  -- · sorry
+  -- by_cases hW' : W'.Nil
+  -- · sorry
+  -- rw [← W.cons_tail_eq hW, ← W'.cons_tail_eq hW']
+  -- convert rfl using 2
+
+  -- obtain (rfl | ⟨hadj : (G.Adj x _),W⟩) := W
+  -- · rw [eq_comm, ← Walk.nil_iff_eq_nil, Walk.nil_iff_support_eq, ← h, support_nil]
+
+  -- induction' W with _ x y z hxy W₁ IH W₂
+  -- · rw [eq_comm, ← Walk.nil_iff_eq_nil, Walk.nil_iff_support_eq, ← h, support_nil]
+
+  -- induction' W' with _ x' y' z' hxy' W₁' IH W₂'
+  -- · simp at h
+  -- simp at h
+  -- have := IH W₁'
+
+
+
+lemma Walk.sndOfNotNil_mem_support (hW : ¬ W.Nil) : W.sndOfNotNil hW ∈ W.support := by
+  induction' W with W hW
+  · simp at hW
+  simp
+
+lemma Walk.tail_eq_dropUntil [DecidableEq V] (hW : ¬ W.Nil) :
+    W.tail hW = W.dropUntil _ (W.sndOfNotNil_mem_support hW) := by
+  induction' W with W h a b hab W' IH
+  · simp at hW
+
+
 def Walk.IsShortest (W : G.Walk x y) := W.length = G.dist x y
 
 lemma Walk.IsShortest.length_eq (hW : W.IsShortest) : W.length = G.dist x y := hW
@@ -33,6 +73,12 @@ lemma Reachable.dist_triangle (hxy : G.Reachable x y) (hyz : G.Reachable y z) :
   convert G.dist_le (P.append Q)
   rw [Walk.length_append, hP, hQ]
 
+lemma Reachable.exists_isShortest (hxy : G.Reachable x y) : ∃ W : G.Walk x y, W.IsShortest :=
+  hxy.exists_walk_of_dist
+
+lemma Connected.exists_isShortest (hG : G.Connected) (x y : V) : ∃ W : G.Walk x y, W.IsShortest :=
+  hG.exists_walk_of_dist x y
+
 lemma Walk.IsShortest.takeUntil [DecidableEq V] (hW : W.IsShortest)
     {a : V} (ha : a ∈ W.support) : (W.takeUntil a ha).IsShortest := by
   refine isShortest_of_le ?_
@@ -43,7 +89,21 @@ lemma Walk.IsShortest.takeUntil [DecidableEq V] (hW : W.IsShortest)
 
 lemma Walk.IsShortest.dropUntil [DecidableEq V] (hW : W.IsShortest)
     {a : V} (ha : a ∈ W.support) : (W.dropUntil a ha).IsShortest := by
-  sorry
+  refine isShortest_of_le ?_
+  have h := congr_arg Walk.length <| W.take_spec ha
+  rw [length_append, hW] at h
+  linarith [dist_le (W.takeUntil a ha),
+    Reachable.dist_triangle ⟨W.takeUntil _ ha⟩ ⟨W.dropUntil _ ha⟩]
+
+
+
+lemma Walk.IsShortest.tail (hW : W.IsShortest) (hWnil : ¬ W.Nil) : (W.tail hWnil).IsShortest := by
+  rw [IsShortest, ← Nat.add_left_inj (n := 1), length_tail_add_one]
+
+  have := congr_arg Walk.length <| W.cons_tail_eq hWnil
+
+
+
 
 lemma Walk.IsShortest.dist_add_of_mem (hW : W.IsShortest)
     (ha : a ∈ W.support) : G.dist x a + G.dist a y = G.dist x y := by
@@ -67,29 +127,95 @@ lemma Walk.length_dropUntil_lt [DecidableEq V] (W : G.Walk x y) (ha : a ∈ W.su
   rw [length_append, lt_add_iff_pos_left, zero_lt_iff, Ne, ← Walk.nil_iff_length_eq]
   exact not_nil_of_ne hax.symm
 
-@[ext] structure bfsFrom (G : SimpleGraph V) (v : V) where ofVertex :: x : V
+def BfsFrom (_ : SimpleGraph V) (_ : V) := V
 
-instance {G : SimpleGraph V} {v : V} : CoeOut (G.bfsFrom v) V where coe := fun ⟨x⟩ ↦ x
+def BfsFrom.ofVert (v : V) : G.BfsFrom a := v
 
-instance {G : SimpleGraph V} {v : V} : PartialOrder (G.bfsFrom a) where
-  le x y := x.1 = y.1 ∨ ∃ W : G.Walk x a, W.IsShortest ∧ (y : V) ∈ W.support
+instance {G : SimpleGraph V} {v : V} : CoeOut (G.BfsFrom v) V where coe x := x
+
+instance : PartialOrder (G.BfsFrom a) where
+  le x y := x = y ∨ ∃ W : G.Walk y a, W.IsShortest ∧ (x : V) ∈ W.support
   le_refl _ :=  .inl rfl
   le_trans := by
     classical
-    rintro ⟨x⟩ ⟨y⟩ ⟨z⟩ (rfl | ⟨P, hP, hxP⟩) (rfl | ⟨Q, hQ, hzQ : z ∈ Q.support⟩)
+    rintro x y z (rfl | ⟨P, hP, hxP⟩) (rfl | ⟨Q, hQ, hyQ⟩)
     · exact .inl rfl
-    · exact .inr ⟨Q, hQ, hzQ⟩
+    · exact .inr ⟨Q, hQ, hyQ⟩
     · exact .inr ⟨P, hP, hxP⟩
-    simp only at P Q
-    refine .inr ⟨(Q.takeUntil _ hyQ).append P, ?_, by simp [hxP]⟩
-    -- obtain (rfl | ⟨Q, hQ, hyQ⟩) := h
-    -- · exact .inr ⟨P, hP, hxP⟩
-    -- ·
+    refine .inr ⟨(Q.takeUntil y hyQ).append P, ?_, by simp [hxP]⟩
+    rw [IsShortest, length_append, hP, ← hQ.dropUntil hyQ, ← length_append, Q.take_spec, hQ]
+  le_antisymm := by
+    classical
+    rintro x y (rfl | ⟨P, hP, hxP⟩) (h' | ⟨Q, hQ, hyQ⟩)
+    · rfl
+    · rfl
+    · simpa using h'.symm
+    have hxy : G.dist x y = 0 := by
+      linarith [hQ.dist_add_of_mem hyQ, G.dist_comm ▸ hP.dist_add_of_mem hxP]
+    rwa [(show G.Reachable x y from ⟨Q.append P.reverse⟩).dist_eq_zero_iff] at hxy
 
-    -- simp only at P hxP Q hyQ
+variable [Fact G.Connected] {x y z : G.BfsFrom a}
+
+lemma exists_isShortest_walk (G : SimpleGraph V) [Fact G.Connected] (x y : V) :
+    ∃ W : G.Walk x y, W.IsShortest :=
+  Connected.exists_isShortest Fact.out x y
+
+lemma BfsFrom.exists_isShortest_walk_of_le {x y : G.BfsFrom a} (hxy : x ≤ y) :
+    ∃ W : G.Walk y a, W.IsShortest ∧ x ∈ W.support := by
+  obtain (rfl | ⟨W, hW⟩) := hxy
+  · exact ⟨_, ((show G.Connected from Fact.out).exists_isShortest x a).choose_spec, by simp⟩
+  exact ⟨_, hW⟩
+
+lemma BfsFrom.exists_isShortest_walk_iff_le {x y : G.BfsFrom a} :
+    x ≤ y ↔ ∃ W : G.Walk y a, W.IsShortest ∧ x ∈ W.support :=
+  ⟨exists_isShortest_walk_of_le, Or.inr⟩
+
+lemma BfsFrom.le_iff_dist_add {x y : G.BfsFrom a} :
+    x ≤ y ↔ G.dist y a = G.dist y x + G.dist x a := by
+  classical
+  rw [exists_isShortest_walk_iff_le]
+  refine ⟨fun ⟨W, hW, hxW⟩ ↦ (hW.dist_add_of_mem hxW).symm, fun h ↦ ?_⟩
+  obtain ⟨P, hP⟩ := G.exists_isShortest_walk y x
+  obtain ⟨Q, hQ⟩ := G.exists_isShortest_walk x a
+  exact ⟨P.append Q, by rwa [IsShortest, eq_comm, length_append, hP, hQ], by simp⟩
+
+lemma Walk.IsShortest.le_of_mem_support {W : G.Walk y a} (hW : W.IsShortest) (hx : x ∈ W.support) :
+    x ≤ y :=
+  .inr ⟨W, hW, hx⟩
 
 
-  le_antisymm := _
+
+
+
+-- lemma Connected.exists_shortest_walk_of_le
+
+instance [Fact G.Connected] : GradeOrder ℕ (G.BfsFrom a) where
+  grade x := G.dist x a
+  grade_strictMono := by
+    simp only [StrictMono, and_imp, lt_iff_le_and_ne, BfsFrom.le_iff_dist_add]
+    intro x y h_eq hne
+    simp [h_eq, hne.symm, (show G.Connected from Fact.out) y x]
+  covBy_grade := by
+    classical
+    simp_rw [Nat.covBy_iff_succ_eq]
+    rintro x y hcb
+    obtain ⟨P, hP, hxP⟩ := BfsFrom.exists_isShortest_walk_of_le hcb.le
+    let Q := P.takeUntil x hxP
+
+    have hQ := hP.takeUntil hxP
+    let w := Q.sndOfNotNil <| Q.not_nil_of_ne hcb.ne.symm
+    have hwQ : w ∈ Q.support := Q.sndOfNotNil_mem _
+    have hwP : w ∈ P.support := P.support_takeUntil_subset hxP hwQ
+
+    have hwy := hP.le_of_mem_support hwP
+    have := (hP.dropUntil hwP).le_of_mem_support (x := x)
+    simp at this
+
+    -- have hnil := Q.not_nil_of_ne hcb.ne.symm
+    -- have := hcb.eq_or_eq (c := BfsFrom.ofVert <| Q.sndOfNotNil <| Q.not_nil_of_ne hcb.ne.symm)
+    --   ()
+
+
 
 
 -- example (G : SimpleGraph V) (hG : G.LocallyFinite) (hconn : G.Connected) (a : V) :
