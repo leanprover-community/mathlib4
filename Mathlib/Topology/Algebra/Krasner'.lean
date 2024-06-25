b1f644a4d2cc6140314ad3e36bed2052b2fa72c9
jjdishere <emailboxofjjd@163.com>
2024-06-25 23:43:59 +0200

    add normed lemmas

--- Mathlib/Topology/Algebra/Krasner'.lean
+++ Mathlib/Topology/Algebra/Krasner'.lean
@@ -1,9 +1,3 @@
-/-
-Copyright (c) 2024 Jiedong Jiang. All rights reserved.
-Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Jiedong Jiang
--/
-
 import Mathlib.Topology.Algebra.Valuation
 import Mathlib.FieldTheory.Minpoly.Basic
 import Mathlib.FieldTheory.Separable
@@ -15,33 +9,21 @@ import Mathlib.RingTheory.Henselian
 import Mathlib.Topology.Algebra.Module.FiniteDimension
 import Mathlib.Topology.Connected.Separation
 
+
+import Mathlib.Topology.Algebra.KrasnerDependency
+
 /-!
-# TODO:
-1. delete IsConjSet
-2. decompose val_eq
-
-# Krasner's Lemma
-
-In this file, we define the type class for field extensions that Krasner's lemma holds and prove
-that for extensions between complete non-archimediean fields, the Krasner's lemma hold.
-
-Let `A` be a valued `R` algebra. We say that the Krasner's property holds for `A` over `R`, if for
-any pair of elements `x y` in `A` satisfying that for all conjugates `x'` of `x` over `R` not
-equal to `x`, `|x - y| < |x - x'|`, we have `y` falls in the image of `R` in `A` adjoining `x`.
-
-## Main Definition
-
-* `IsKrasner R A` : `A` over `R` satisfies the Krasner's property
-
-## Reference
-
-## Tag
-Krasner's lemma, Valued, Non-archimedean field
+## TODO
+1. uniform space eq
+2. proof ‖z‖ = ‖z'‖
+3. add dictionary
+4. fill the version of valuations
 -/
 
 variable {R : Type*} {A : Type*} [CommRing R] [Ring A] [Algebra R A]
 
 variable (K : Type*) {L : Type*} {ΓK ΓL : outParam Type*} [LinearOrderedCommGroupWithZero ΓK] [LinearOrderedCommGroupWithZero ΓL] [Field K] [Field L] [Algebra K L] [vK : Valued K ΓK] [vL : Valued L ΓL]
+
 
 section conj
 
@@ -88,73 +70,6 @@ theorem ne_zero {x x' : A} (hx : x ≠ 0) (h : IsConjRoot R x x') : x' ≠ 0 := 
 
 end IsConjRoot
 
--- Key theorem -- K with restriction topology
--- theorem Polynomial.val_coeff_pow_natDegree [vR : Valued R ΓL] [vR.v.RankOne] (f : Polynomial R) (h : Irreducible f) (hm : f.Monic) (i : ℕ) : vR.v (f.coeff i) ^ (f.natDegree) ≤ vR.v (f.coeff 0) ^ (f.natDegree - i) := sorry
-
--- theorem Polynomial.val_coeff_pow_natDegree' [vR : Valued R ΓL] [vR.v.RankOne] (f : Polynomial R) (h : Irreducible f) (hm : f.Monic) (h0 : vR.v (f.coeff 0) = 1) (i : ℕ) : vR.v (f.coeff i) ^ (f.natDegree) ≤ 1 := sorry
-
--- `this theorem need topology and rank 1 condition on vK!`
-theorem val_algebraMap_minpoly_coeff_le_iff_val_le_one (v : Valuation L ΓL) (x : L) (h : vK.v.IsEquiv <| v.comap <| algebraMap K L) (hx : IsIntegral K x) : (∀ i : ℕ, v (algebraMap K L ((minpoly K x).coeff i)) ≤ 1) ↔ v x ≤ 1 := by sorry
-
-theorem minpoly_inv_eq (x : L) : minpoly K x⁻¹ = ((minpoly K x).coeff 0)⁻¹ • (minpoly K x).reverse := sorry
-
-variable {K} in
-theorem IsIntegral.inv {x : L} (h : IsIntegral K x) : IsIntegral K x⁻¹ := sorry
-
-variable {K} in
-theorem IsIntegral.div {x y : L} (hx : IsIntegral K x) (hx : IsIntegral K y) : IsIntegral K (x/y) := sorry
-
--- this need vK of rank 1 or at least there exist rank one is equiv.
-theorem IsConjRoot.val_eq {ΓK : outParam Type*} [LinearOrderedCommGroupWithZero ΓK] [UniformSpace K] [CompleteSpace K] (hv : vK.v.IsEquiv <| vL.v.comap <| algebraMap K L) {x x' : L} (hx : IsIntegral K x) (h : IsConjRoot K x x') : vL.v x = vL.v x' := by
-  by_cases h0 : x = 0
-  simp only [h0, map_zero, IsConjRoot.iff_eq_zero.mp (h0 ▸ h)]
-  let y := x/x'
-  have hy : IsIntegral K y := hx.div (IsConjRoot.isIntegral hx h)
-  suffices vL.v y = 1 by sorry
-  by_cases g : vL.v y ≤ 1
-  · let F := minpoly K y
-    have : F.coeff 0 = 1 := sorry -- need calculation to show
-    have : vL.v y⁻¹ ≤ 1 := by
-      rw [← val_algebraMap_minpoly_coeff_le_iff_val_le_one (K := K) vL.v _ hv hy.inv, show minpoly K y⁻¹ = (minpoly K y).reverse by simpa [this] using minpoly_inv_eq K y]
-      intro i
-      rw [Polynomial.coeff_reverse]
-      exact ((val_algebraMap_minpoly_coeff_le_iff_val_le_one K vL.v y) hv hy).mpr g _
-    simp only [map_inv₀, inv_le_one₀ <| vL.v.ne_zero_iff.mpr <| div_ne_zero h0 (h.ne_zero h0)] at this
-    exact eq_of_le_of_le g this
-  · sorry -- use y⁻¹ instead of y
-
-
--- show this for x in vL.v.integer
--- x algebraic/v.integer same as x'
--- show x/x' in (vL.v.integer)ˣ, one of x/x', x'/x falls in, minpoly has every coefficient val ≤ 1, and a₀ = 1! => the inverse also satiesfies an monic poly, coeffents ≤ 1, => the inverse is integer.
--- v (x/x') is one
-
-
-
-
-/-
-
-#check Polynomial.roots
-def conjRootSet (R : Type*) {A : Type*} [CommRing R] [Ring A] [Algebra R A] (x : A) : Set A := {x' | IsConjRoot R x x'}
-
-theorem finite {x : A} (h : IsIntegral R x) : Set.Finite {x' | IsConjRoot R x x'} := sorry
-
--- open Classical in
--- noncomputable def conjRootSet (R : Type*) {A : Type*} [CommRing R] [Ring A] [Algebra R A] (x : A) : Finset A :=
---   if h : IsIntegral R x then Set.Finite.toFinset (IsConjRoot.finite h) else ∅
-
-theorem conjRootSet.self_mem (x : A) : x ∈ conjRootSet R x := sorry
-
--- theorem mem_of_conj_eq_singleton (x : L) (sep : (minpoly K x).Separable) [Subsingleton (conjRootSet K x)] : x ∈ (⊥ : Subalgebra K L) := sorry
-
-variable {K} in
-theorem not_mem_iff_nontrvial {x : L} (sep : (minpoly K x).Separable) : x ∉ (⊥ : Subalgebra K L) ↔ Nontrivial (conjRootSet K x) := sorry
-
--/
-
-
-end conj
-
 section Separable
 -- do we need elementwise IsSeparable (just like IsIntegral) and rename old one into Field.IsSeparable
 
@@ -178,80 +93,28 @@ theorem Polynomial.minpoly_separable_of_isScalarTower {x : B} (h : (minpoly R x)
 
 end Separable
 
-section RankOne
+section NormValued
 
-
-
-end RankOne
--- section topology
-
--- variable {X Y} [TopologicalSpace X] [TopologicalSpace Y] {f : X → Y}
--- #check IsClosed.isComplete
--- def ClosedEmbedding.uniformSpace [UniformSpace Y] (h : ClosedEmbedding f) : UniformSpace X := sorry
-
--- def ClosedEmbedding.completeSpace [UniformSpace Y] [CompleteSpace Y] (h : ClosedEmbedding f) : @CompleteSpace X (h.uniformSpace) := sorry
-
--- end topology
-
-section valuedfield
-
--- do this need L being a field?
-instance : TotallySeparatedSpace L := sorry
-
-instance (R : Type*) [Ring R] [UniformSpace R] [UniformAddGroup R] (M : Subring R) : UniformAddGroup M := sorry
-
--- into Mathlib.Topology.Algebra.NormedValued
-def Valued.toNontriviallyNormedField [h : vL.v.RankOne] : NontriviallyNormedField L := {
+def Valued.toNontriviallyNormedField [vL.v.RankOne] : NontriviallyNormedField L := {
   vL.toNormedField with
-  non_trivial := by
-    obtain ⟨x, ⟨h0, h1⟩⟩ := h.nontrivial'
-    by_cases g : 1 < v x
-    · use x
-      simpa only [map_one] using h.strictMono' g
-    · use x⁻¹
-      have g : 1 < v x⁻¹ := by
-        rw [Valuation.one_lt_val_iff _ <| inv_ne_zero <| (Valuation.ne_zero_iff _).mp h0]
-        simpa only [inv_inv] using lt_of_le_of_ne (not_lt.mp g) h1
-      simpa only [map_one] using h.strictMono' g
+  non_trivial := by sorry
 }
 
-theorem Valued.toNormedField.toUniformSpace_eq [vL.v.RankOne] : (Valued.toNormedField L ΓL).toUniformSpace = vL.toUniformSpace:= rfl
+end NormValued
 
 
-theorem Valued.toUniformSpace_eq_of_isEquiv_comap {ΓK : outParam Type*} [LinearOrderedCommGroupWithZero ΓK] [vK : Valued K ΓK] (h : vK.v.IsEquiv <| vL.v.comap (algebraMap K L)) : vK.toUniformSpace = vL.toUniformSpace.comap (algebraMap K L) := sorry
+section T2
 
-#synth ContinuousSMul L L
-variable (A : Subfield L)
-#synth ContinuousSMul A L
+-- instance : TotallySeparatedSpace L := sorry
 
-variable (M : IntermediateField K L)
-#synth ContinuousSMul M L
--- #synth IsScalarTower K (⊥ : IntermediateField K L) M
-instance : ContinuousSMul (⊥ : IntermediateField K L) M := Inducing.continuousSMul (N := (⊥ : IntermediateField K L)) (Y := M) (M := (⊥ : IntermediateField K L)) (X := L) inducing_subtype_val continuous_id rfl
--- typo in docs of Inducing.continuousSMul, X Y is reversed.
+end T2
 
--- #synth ContinuousSMul (⊥ : IntermediateField K L) M
--- #synth UniformAddGroup M
+section ContinuousSMul
 
-end valuedfield
+instance ContinuousSMul.instBotIntermediateField {K L : Type*} [Field K] [Field L] [Algebra K L] [TopologicalSpace L] [TopologicalRing L] (M : IntermediateField K L) : ContinuousSMul (⊥ : IntermediateField K L) M := Inducing.continuousSMul (N := (⊥ : IntermediateField K L)) (Y := M) (M := (⊥ : IntermediateField K L)) (X := L) inducing_subtype_val continuous_id rfl
 
-/-
-section TrivialValuation
+end ContinuousSMul
 
-class Valuation.IsTrivial {ΓL₀ : Type*} [LinearOrderedCommMonoidWithZero ΓL₀] (v : Valuation R ΓL₀) : Prop where
-  val_eq_one_of_ne_zero : ∀ x : R, x ≠ 0 → v x = 1
-
-#check Valued.is_topological_valuation
-#check discreteTopology_iff_singleton_mem_nhds
-instance [vL.v.IsTrivial] : DiscreteTopology L := by
-  apply singletons_open_iff_discrete.mp
-  intro a
-  sorry
-
-end TrivialValuation
--/
-
-section krasner
 
 open Algebra
 open IntermediateField
@@ -262,35 +125,29 @@ class IsKrasner : Prop where
     (∀ x' : L, IsConjRoot K x x' →  x ≠ x' → vL.v (x - y) < vL.v (x - x')) →
       x ∈ K⟮y⟯
 
-namespace IsKrasner
+class IsKrasnerNorm (K L : Type*) [Field K] [NormedField L] [Algebra K L] : Prop where
+  krasner_norm' : ∀ {x y : L}, (minpoly K x).Separable → IsIntegral K y →
+    (∀ x' : L, IsConjRoot K x x' →  x ≠ x' → ‖x - y‖ < ‖x - x'‖) →
+      x ∈ K⟮y⟯
 
 variable [IsKrasner K L]
 
-theorem krasner {x y : L} (hx : (minpoly K x).Separable) (hy : IsIntegral K y) (h : (∀ x' : L, IsConjRoot K x x' → x ≠ x' → vL.v (x - y) < vL.v (x - x'))) : x ∈ K⟮y⟯ := IsKrasner.krasner' hx hy h
+theorem IsKrasner.krasner {x y : L} (hx : (minpoly K x).Separable) (hy : IsIntegral K y) (h : (∀ x' : L, IsConjRoot K x x' → x ≠ x' → vL.v (x - y) < vL.v (x - x'))) : x ∈ K⟮y⟯ := IsKrasner.krasner' hx hy h
 -- Algebra.adjoin R {x} ≤ Algebra.adjoin R {y}
 
--- #check FiniteDimensional.complete
-#check TotallySeparatedSpace.t2Space
--- more is needed, K has restriction topology, or embedding
+theorem IsKrasnerNorm.krasner_norm {K L : Type*} [Field K] [NormedField L] [Algebra K L] [IsKrasnerNorm K L] {x y : L} (hx : (minpoly K x).Separable) (hy : IsIntegral K y) (h : (∀ x' : L, IsConjRoot K x x' → x ≠ x' → ‖x - y‖ < ‖x - x'‖)) : x ∈ K⟮y⟯ := IsKrasnerNorm.krasner_norm' hx hy h
 
-instance id : IsKrasner L L where
-  krasner' := by
-    simp [Subsingleton.allEq _ (⊤ : IntermediateField L L)]
+namespace IsKrasnerNorm
 
-/-
-instance of_isTrivial [vL.v.IsTrivial] : IsKrasner K L := sorry
--/
+variable {K L : Type*} [NK : NontriviallyNormedField K] (is_na : IsNonarchimedean (norm : K → ℝ)) [NL : NormedField L] [Algebra K L] (ext : FunctionExtends (norm : K → ℝ) (norm : L → ℝ))
 
-theorem of_completeSpace [u : UniformSpace K] [c : CompleteSpace K] [rk1 : (vL.v.comap (algebraMap K L)).RankOne] (h : UniformEmbedding (algebraMap K L)) : IsKrasner K L := by
+theorem of_completeSpace [CompleteSpace K] : IsKrasnerNorm K L := by
   constructor
   intro x y xsep hyK hxy
   let z := x - y
   let M := K⟮y⟯
   let FDM := IntermediateField.adjoin.finiteDimensional hyK
-  let I'' : UniformAddGroup M := inferInstanceAs (UniformAddGroup M.toSubfield)
-  let vK : Valued K ΓL := Valued.mk' (vL.v.comap (algebraMap K L))
-  letI : vK.v.RankOne := rk1
-  let NNFK : NontriviallyNormedField K := vK.toNontriviallyNormedField
+  let uniM : UniformAddGroup M := inferInstanceAs (UniformAddGroup M.toSubfield)
   have : ContinuousSMul K M := by -- decompose as `ContinuousSMul K L implies ContinuousSMul K M`
     apply Inducing.continuousSMul (N := K) (M := (⊥ : IntermediateField K L)) (X := M) (Y := M) (f := (IntermediateField.botEquiv K L).symm) inducing_id
     · simpa only [bot_toSubalgebra] using
@@ -300,14 +157,6 @@ theorem of_completeSpace [u : UniformSpace K] [c : CompleteSpace K] [rk1 : (vL.v
     · intro r m
       ext
       simp
-  have : u = NNFK.toUniformSpace := by
-    rw [Valued.toNormedField.toUniformSpace_eq]
-    rw [Valued.toUniformSpace_eq_of_isEquiv_comap (vK := vK) (Valuation.IsEquiv.refl)]
-    ext
-    rw [← h.comap_uniformity]
-    rfl
-  subst this
-  let hM : Embedding (algebraMap M L) := Function.Injective.embedding_induced Subtype.val_injective -- M with UniformSpace already, as subspace of L
   letI : CompleteSpace M := FiniteDimensional.complete K M-- @FiniteDimensional.complete K M sorry sorry _ _ _ sorry _ _ _  -- this need all topology on M is the same and complete?
   have hy : y ∈ K⟮y⟯ := IntermediateField.subset_adjoin K {y} rfl
   have zsep : (minpoly M z).Separable := by
@@ -320,27 +169,31 @@ theorem of_completeSpace [u : UniformSpace K] [c : CompleteSpace K] [rk1 : (vL.v
   by_contra hnin
   have : z ∈ K⟮y⟯ ↔ z ∈ (⊥ : Subalgebra M L) := by simp [Algebra.mem_bot]
   rw [this.not] at hnin
-  obtain ⟨z', hne, h1⟩ := (IsConjRoot.not_mem_iff_exist_ne zsep).mp hnin
+  obtain ⟨z', hne, h1⟩ := (IsConjRoot.not_mem_iff_exist_ne zsep).mp hnin -- this is where the separablity is used.
   -- rw [not_mem_iff_nontrvial zsep] at hnin
   -- obtain ⟨⟨z', h1⟩, hne⟩ := exists_ne (⟨z, conjRootSet.self_mem z⟩ : { x // x ∈ conjRootSet M z })
   simp only [ne_eq, Subtype.mk.injEq] at hne
   -- simp only [conjRootSet, Set.coe_setOf, Set.mem_toFinset, Set.mem_setOf_eq] at h1
   -- let vM : Valued M NNReal := sorry
-  have : vL.v z = vL.v z' := IsConjRoot.val_eq M hM (Polynomial.Separable.isIntegral zsep) h1 -- need rank one
-  have : vL.v (z - z') < vL.v (z - z') := by
+  have : ‖z‖ = ‖z'‖ := IsConjRoot.val_eq M hM (Polynomial.Separable.isIntegral zsep) h1 -- need rank one
+  have : ‖z - z'‖ < ‖z - z'‖ := by
     calc
-      _ ≤ vL.v (x - y) := by simpa only [max_self, ← this] using Valuation.map_sub vL.v z z'
-      _ < vL.v (x - (z' + y)) := by
+      _ ≤ ‖x - y‖ := by simpa only [max_self, ← this] using Valuation.map_sub vL.v z z'
+      _ < ‖x - (z' + y)‖ := by
         apply hxy (z' + y)
         · apply IsConjRoot.of_isScalarTower (S := M) K
           simpa only [IntermediateField.algebraMap_apply, sub_add_cancel, z] using
             IsConjRoot.add_algebraMap ⟨y, hy⟩ h1
         · simpa [z, sub_eq_iff_eq_add] using hne
-      _ = vL.v (z - z') := by congr 1; ring
+      _ = ‖z - z'‖ := by congr 1; ring
   simp only [lt_self_iff_false] at this
 
--- a better version for val extension
-theorem of_completeSpace' {ΓK : outParam Type*} [LinearOrderedCommGroupWithZero ΓK] [vK : Valued K ΓK] [vK.v.RankOne] [CompleteSpace K] (h : vK.v.IsEquiv <| vL.v.comap (algebraMap K L)): IsKrasner K L := by
+
+end IsKrasnerNorm
+
+namespace IsKrasner
+-- it is possible to remove the condition `vL.v.RankOne`
+theorem of_completeSpace [vL.v.RankOne] {ΓK : outParam Type*} [LinearOrderedCommGroupWithZero ΓK] [vK : Valued K ΓK] [vK.v.RankOne] [CompleteSpace K] (h : vK.v.IsEquiv <| vL.v.comap (algebraMap K L)): IsKrasner K L := by
   constructor
   intro x y xsep hyK hxy
   let z := x - y
@@ -377,7 +230,7 @@ theorem of_completeSpace' {ΓK : outParam Type*} [LinearOrderedCommGroupWithZero
   by_contra hnin
   have : z ∈ K⟮y⟯ ↔ z ∈ (⊥ : Subalgebra M L) := by simp [Algebra.mem_bot]
   rw [this.not] at hnin
-  obtain ⟨z', hne, h1⟩ := (IsConjRoot.not_mem_iff_exist_ne zsep).mp hnin
+  obtain ⟨z', hne, h1⟩ := (IsConjRoot.not_mem_iff_exist_ne zsep).mp hnin -- this is where the separablity is used.
   -- rw [not_mem_iff_nontrvial zsep] at hnin
   -- obtain ⟨⟨z', h1⟩, hne⟩ := exists_ne (⟨z, conjRootSet.self_mem z⟩ : { x // x ∈ conjRootSet M z })
   simp only [ne_eq, Subtype.mk.injEq] at hne
@@ -395,8 +248,3 @@ theorem of_completeSpace' {ΓK : outParam Type*} [LinearOrderedCommGroupWithZero
         · simpa [z, sub_eq_iff_eq_add] using hne
       _ = vL.v (z - z') := by congr 1; ring
   simp only [lt_self_iff_false] at this
-
-
-end IsKrasner
-
-end krasner
---
+++ Mathlib/Topology/Algebra/KrasnerDependency.lean
@@ -0,0 +1,185 @@
+import Mathlib.Analysis.Normed.Ring.Seminorm
+import Mathlib.Analysis.Seminorm
+import Mathlib.RingTheory.Algebraic
+
+/-
+In this file, we state Maria and Fillipo's result. waiting for their work wo be merged into Mathlib.
+
+-/
+
+def FunctionExtends {α : Type _} [CommRing α] (g : α → ℝ) {β : Type _} [Ring β] [Algebra α β]
+    (f : β → ℝ) : Prop :=
+  ∀ x : α, f (algebraMap α β x) = g x
+
+/-- An algebra norm on an `R`-algebra norm `S` is a ring norm on `S` compatible with the
+  action of `R`. -/
+structure AlgebraNorm (R : Type _) [SeminormedCommRing R] (S : Type _) [Ring S]
+    [Algebra R S] extends RingNorm S, Seminorm R S
+
+attribute [nolint docBlame] AlgebraNorm.toSeminorm AlgebraNorm.toRingNorm
+
+instance (K : Type _) [NormedField K] : Inhabited (AlgebraNorm K K) :=
+  ⟨{  toFun     := norm
+      map_zero' := norm_zero
+      add_le'   := norm_add_le
+      neg'      := norm_neg
+      smul'     := norm_mul
+      mul_le'   := norm_mul_le
+      eq_zero_of_map_eq_zero' := fun _ => norm_eq_zero.mp }⟩
+
+/-- `algebra_norm_class F α` states that `F` is a type of algebra norms on the ring `β`.
+You should extend this class when you extend `algebra_norm`. -/
+class AlgebraNormClass (F : Type _) (R : outParam <| Type _) [SeminormedCommRing R]
+    (S : outParam <| Type _) [Ring S] [Algebra R S] [FunLike F S ℝ] extends RingNormClass F S ℝ,
+    SeminormClass F R S
+
+-- `R` is an `out_param`, so this is a false positive.
+--attribute [nolint DangerousInstance] AlgebraNormClass.toRingNormClass
+
+namespace AlgebraNorm
+
+variable {R : Type _} [SeminormedCommRing R] {S : Type _} [Ring S] [Algebra R S]
+  {f : AlgebraNorm R S}
+
+/-- The ring_seminorm underlying an algebra norm. -/
+def toRingSeminorm' (f : AlgebraNorm R S) : RingSeminorm S :=
+  f.toRingNorm.toRingSeminorm
+
+instance : FunLike (AlgebraNorm R S) S ℝ where
+  coe f := f.toFun
+  coe_injective' f f' h := by
+    simp only [AddGroupSeminorm.toFun_eq_coe, RingSeminorm.toFun_eq_coe] at h
+    cases f; cases f'; congr;
+    simp only at h
+    ext s
+    erw [h]
+    rfl
+
+instance algebraNormClass : AlgebraNormClass (AlgebraNorm R S) R S where
+  map_zero f        := f.map_zero'
+  map_add_le_add f  := f.add_le'
+  map_mul_le_mul f  := f.mul_le'
+  map_neg_eq_map f  := f.neg'
+  eq_zero_of_map_eq_zero f := f.eq_zero_of_map_eq_zero' _
+  map_smul_eq_mul f := f.smul'
+
+/-- Helper instance for when there's too many metavariables to apply `fun_like.has_coe_to_fun`. -/
+instance : CoeFun (AlgebraNorm R S) fun _ => S → ℝ :=
+  DFunLike.hasCoeToFun
+
+theorem toFun_eq_coe (p : AlgebraNorm R S) : p.toFun = p := rfl
+
+@[ext]
+theorem ext {p q : AlgebraNorm R S} : (∀ x, p x = q x) → p = q :=
+  DFunLike.ext p q
+
+/-- An `R`-algebra norm such that `f 1 = 1` extends the norm on `R`. -/
+theorem extends_norm' {f : AlgebraNorm R S} (hf1 : f 1 = 1) (a : R) : f (a • (1 : S)) = ‖a‖ := by
+  rw [← mul_one ‖a‖, ← hf1]; exact f.smul' _ _
+
+/-- An `R`-algebra norm such that `f 1 = 1` extends the norm on `R`. -/
+theorem extends_norm {f : AlgebraNorm R S} (hf1 : f 1 = 1) (a : R) : f (algebraMap R S a) = ‖a‖ :=
+  by rw [Algebra.algebraMap_eq_smul_one]; exact extends_norm' hf1 _
+
+end AlgebraNorm
+
+section spectralNorm
+
+variable (K : Type _) [NormedField K] (L : Type _) [Field L] [Algebra K L]
+
+/-- The spectral norm `|y|_sp` is the spectral value of the minimal of `y` over `K`. -/
+def spectralNorm (K : Type _) [NormedField K] (L : Type _) [Field L] [Algebra K L] (y : L) : ℝ :=
+  sorry
+
+/-- The `K`-algebra automorphisms of `L` are isometries with respect to the spectral norm. -/
+theorem spectralNorm_aut_isom (σ : L ≃ₐ[K] L) (x : L) :
+    spectralNorm K L x = spectralNorm K L (σ x) := by sorry
+
+/-- The spectral norm extends the norm on `K`. -/
+theorem spectralNorm_extends (k : K) : spectralNorm K L (algebraMap K L k) = ‖k‖ := sorry
+
+variable {K L : Type _} [NormedField K] [Field L] [Algebra K L]
+
+/-- The spectral norm is a `K`-algebra norm on `L`. -/
+def spectralAlgNorm (hna : IsNonarchimedean (norm : K → ℝ)) :
+    AlgebraNorm K L where
+      toFun := spectralNorm K L
+      map_zero' := sorry
+      add_le' := sorry
+      neg' := sorry
+      mul_le' := sorry
+      eq_zero_of_map_eq_zero' := sorry
+      smul' := sorry
+
+
+
+end spectralNorm
+
+/-- A function `f : R → ℝ` is power-multiplicative if for all `r ∈ R` and all positive `n ∈ ℕ`,
+  `f (r ^ n) = (f r) ^ n`. -/
+def IsPowMul {R : Type _} [Ring R] (f : R → ℝ) :=
+  ∀ (a : R) {n : ℕ} (_ : 1 ≤ n), f (a ^ n) = f a ^ n
+
+variable {K : Type _} [NontriviallyNormedField K] {L : Type _} [Field L] [Algebra K L]
+  [Algebra.IsAlgebraic K L]
+
+theorem spectral_norm_unique' [CompleteSpace K] {f : AlgebraNorm K L} (hf_pm : IsPowMul f)
+    (hna : IsNonarchimedean (norm : K → ℝ)) : f = spectralAlgNorm hna := sorry
+
+/-- If `K` is a field complete with respect to a nontrivial nonarchimedean multiplicative norm and
+  `L/K` is an algebraic extension, then any multiplicative ring norm on `L` extending the norm on
+  `K` coincides with the spectral norm. -/
+theorem spectralNorm_unique_field_norm_ext [CompleteSpace K]
+    {f : MulRingNorm L} (hf_ext : FunctionExtends (norm : K → ℝ) f)
+    (hna : IsNonarchimedean (norm : K → ℝ)) (x : L) : f x = spectralNorm K L x := sorry
+
+/-- `L` with the spectral norm is a `normed_field`. -/
+def spectralNormToNormedField [CompleteSpace K] (h : IsNonarchimedean (norm : K → ℝ)) :
+    NormedField L :=
+  { (inferInstance : Field L) with
+    norm := fun x : L => (spectralNorm K L x : ℝ)
+    dist := fun x y : L => (spectralNorm K L (x - y) : ℝ)
+    dist_self := sorry
+    dist_comm := sorry
+    dist_triangle := sorry
+    edist_dist := sorry
+    eq_of_dist_eq_zero := sorry
+    dist_eq := sorry
+    norm_mul' := sorry
+  }
+
+/-- `L` with the spectral norm is a `normed_add_comm_group`. -/
+def spectralNormToNormedAddCommGroup [CompleteSpace K] (h : IsNonarchimedean (norm : K → ℝ)) :
+     NormedAddCommGroup L :=
+  by
+  haveI : NormedField L := spectralNormToNormedField h
+  infer_instance
+
+/-- `L` with the spectral norm is a `seminormed_add_comm_group`. -/
+def spectralNormToSeminormedAddCommGroup [CompleteSpace K] (h : IsNonarchimedean (norm : K → ℝ)) :
+    SeminormedAddCommGroup L := by
+  have : NormedField L := spectralNormToNormedField h
+  infer_instance
+
+/-- `L` with the spectral norm is a `normed_space` over `K`. -/
+def spectralNormToNormedSpace [CompleteSpace K]
+    (h : IsNonarchimedean (norm : K → ℝ)) :
+    @NormedSpace K L _ (spectralNormToSeminormedAddCommGroup h) := sorry
+
+/-- The metric space structure on `L` induced by the spectral norm. -/
+def ms [CompleteSpace K] (h : IsNonarchimedean (norm : K → ℝ)) : MetricSpace L :=
+  (spectralNormToNormedField h).toMetricSpace
+
+/-- The uniform space structure on `L` induced by the spectral norm. -/
+def us [CompleteSpace K] (h : IsNonarchimedean (norm : K → ℝ)) : UniformSpace L :=
+  (ms h).toUniformSpace
+
+-- normed_field.to_uniform_space
+/-- If `L/K` is finite dimensional, then `L` is a complete space with respect to topology induced
+  by the spectral norm. -/
+instance (priority := 100) spectral_norm_completeSpace [CompleteSpace K]
+    (h : IsNonarchimedean (norm : K → ℝ)) [h_fin : FiniteDimensional K L] :
+    @CompleteSpace L (us h) := by
+  letI := (spectralNormToNormedAddCommGroup (L := L) h)
+  letI := (spectralNormToNormedSpace (L :=L) h)
+  sorry--exact FiniteDimensional.complete K L
