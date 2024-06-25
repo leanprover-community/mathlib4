2804ff9dc0f597b0dd850d8a5f5b099ccbbe70f9
Hannah Fechtner <hfechtne@andrew.cmu.edu>
2024-06-25 19:47:38 -0400

    update imports in presented monoid

--- Mathlib/Algebra/PresentedMonoid/Basic.lean
+++ Mathlib/Algebra/PresentedMonoid/Basic.lean
@@ -6,7 +6,7 @@ Authors: Hannah Fechtner
 
 import Mathlib.Algebra.FreeMonoid.Basic
 import Mathlib.Algebra.Group.Submonoid.Operations
-import Mathlib.GroupTheory.Congruence
+import Mathlib.GroupTheory.Congruence.Basic
 
 /-!
 # Defining a monoid given by generators and relations
