926ab685da40ce425ec30ecc5dbdc8bd5070d5be
Joël Riou <joel.riou@universite-paris-saclay.fr>
2024-06-25 11:21:12 +0200

    developing the API

--- Mathlib/Algebra/Category/ModuleCat/Sheaf/Free.lean
+++ Mathlib/Algebra/Category/ModuleCat/Sheaf/Free.lean
@@ -9,14 +9,16 @@ import Mathlib.Algebra.Category.ModuleCat.Sheaf.Colimits
 /-!
 # Free sheaves of modules
 
-Given a type `I`, we construct `SheafOfModules.free I : SheafOfModules R` as
-the coproduct of copies indexed by `I` of `unit R`.
+In this file, we construct the functor
+`SheafOfModules.freeFunctor : Type u ⥤ SheafOfModules.{u} R` which sends
+a type `I` to the coproduct of copies indexed by `I` of `unit R`.
 
 ## TODO
 
-* promote the definition of `SheafOfModules.free` as a functor
-`Type u ⥤ SheafOfModules.{u} R` and show it is the left adjoint
-to the sections functor using `freeHomEquiv`
+* In case the category `C` has a terminal object `X`, promote `freeHomEquiv`
+into an adjunction between `freeFunctor` and the evaluation functor at `X`.
+(Alternatively, assuming specific universe parameters, we could show that
+`freeHomEquiv` is a left adjoint to `SheafOfModules.sectionsFunctor`.)
 
 -/
 
@@ -49,5 +51,39 @@ lemma freeHomEquiv_symm_comp {M N : SheafOfModules.{u} R} {I : Type u} (s : I �
     (p : M ⟶ N) :
     M.freeHomEquiv.symm s ≫ p = N.freeHomEquiv.symm (fun i ↦ sectionsMap p (s i)) :=
   N.freeHomEquiv.injective (by ext; simp [freeHomEquiv_comp_apply])
+
+/-- The tautological section of `free I : SheafOfModules R` corresponding to `i : I`. -/
+noncomputable abbrev freeSection {I : Type u} (i : I) : (free (R := R) I).sections :=
+  (free (R := R) I).freeHomEquiv (𝟙 (free I)) i
+
+section
+
+variable {I J : Type u} (f : I → J)
+
+/-- The morphism of presheaves of `R`-modules `free I ⟶ free J` induced by
+a map `f : I → J`. -/
+noncomputable def freeMap : free (R := R) I ⟶ free J :=
+  (freeHomEquiv _).symm (fun i ↦ freeSection (f i))
+
+@[simp]
+lemma freeHomEquiv_freeMap :
+    (freeHomEquiv _ (freeMap (R := R) f)) = freeSection.comp f :=
+  (freeHomEquiv _).symm.injective (by simp; rfl)
+
+@[simp]
+lemma sectionMap_freeMap_freeSection (i : I) :
+    sectionsMap (freeMap (R := R) f) (freeSection i) = freeSection (f i) := by
+  simp [← freeHomEquiv_comp_apply]
+
+end
+
+/-- The functor `Type u ⥤ SheafOfModules.{u} R` which sends a type `I` to
+`free I` which is a coproduct indexed by `I` of copies of `R` (thought as a
+presheaf of modules over itself). --/
+noncomputable def freeFunctor : Type u ⥤ SheafOfModules.{u} R where
+  obj := free
+  map f := freeMap f
+  map_id X := (freeHomEquiv _).injective (by ext1 i; simp)
+  map_comp {I J K} f g := (freeHomEquiv _).injective (by ext1; simp [freeHomEquiv_comp_apply])
 
 end SheafOfModules
