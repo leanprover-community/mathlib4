a6af321b8473fa3d4a0da705b7cb75f355eac6ce
Pim Otte <potte@quintor.nl>
2024-06-25 19:09:41 +0200

    Apply suggestions from code review
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- Mathlib/Combinatorics/SimpleGraph/Matching.lean
+++ Mathlib/Combinatorics/SimpleGraph/Matching.lean
@@ -163,7 +163,7 @@ lemma odd_matches_node_outside {u : Set V} {c : ConnectedComponent (Subgraph.del
       obtain ⟨⟨v', hv'⟩, ⟨hv , rfl⟩⟩ := hv
       use w
       have hwnu : w ∉ u := fun hw' ↦ h w hw' ⟨v', hv'⟩ (hw.1) hv
-      refine ⟨⟨⟨⟨v', hv'⟩, ⟨hv, rfl⟩⟩, ⟨?_, hw.1⟩⟩, fun _ hy ↦ hw.2 _ hy.2.2⟩
+      refine ⟨⟨⟨⟨v', hv'⟩, hv, rfl⟩, ?_, hw.1⟩, fun _ hy ↦ hw.2 _ hy.2.2⟩
       apply ConnectedComponent.mem_coe_supp_of_adj ⟨⟨v', hv'⟩, ⟨hv, rfl⟩⟩ ⟨by trivial, hwnu⟩
       simp only [Subgraph.induce_verts, Subgraph.verts_top, Set.mem_diff, Set.mem_univ, true_and,
         Subgraph.induce_adj, hwnu, not_false_eq_true, and_self, Subgraph.top_adj, M.adj_sub hw.1,
