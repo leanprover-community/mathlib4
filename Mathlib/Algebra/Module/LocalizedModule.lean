836ac88b4305b9c78747f7a5dcf882b3448da26d
js2357 <James.Sundstrom@gmail.com>
2024-06-25 01:24:35 -0400

    Further automate proof of map_iso_commute

--- Mathlib/Algebra/Module/LocalizedModule.lean
+++ Mathlib/Algebra/Module/LocalizedModule.lean
@@ -1151,8 +1151,7 @@ lemma map_iso_commute (g : M₀ →ₗ[R] M₁) : (map S f₀ f₁) g ∘ₗ (is
     (iso S f₁) ∘ₗ (map S (mkLinearMap S M₀) (mkLinearMap S M₁)) g := by
   ext x
   refine induction_on (fun m s ↦ ((Module.End_isUnit_iff _).1 (map_units f₁ s)).1 ?_) x
-  repeat rw [Module.algebraMap_end_apply, ← CompatibleSMul.map_smul, smul'_mk, ← mk_smul, mk_cancel,
-    coe_comp, coe_coe, Function.comp_apply, iso_apply]
+  repeat rw [Module.algebraMap_end_apply, ← CompatibleSMul.map_smul, smul'_mk, ← mk_smul, mk_cancel]
   simp -- Can't be combined with next simp. This uses map_apply, which would be preempted by map.
   simp [map, lift, iso_localizedModule_eq_refl, lift_mk]
 
