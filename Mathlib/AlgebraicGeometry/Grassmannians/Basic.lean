36f2ad7e91ef5df6933d78ade4d3d132efd1518c
morel <smorel@math.princeton.edu>
2024-06-26 23:11:24 +0200

    stuck again

--- Mathlib/AlgebraicGeometry/Grassmannians/Basic.lean
+++ Mathlib/AlgebraicGeometry/Grassmannians/Basic.lean
@@ -51,7 +51,8 @@ def matrix_G (i j : Basis (Fin (finrank K V)) K V) :
     Matrix (Fin (finrank K V - r)) (Fin r) A :=
   fun φ  ↦ Matrix.submatrix (matrix K V r hr i j φ) (Fin.castLE (by simp)) id
 
-lemma chart_eq_id_of_diagonal (i : Basis (Fin (finrank K V)) K V)
+
+lemma matrix_F_eq_id_of_diagonal (i : Basis (Fin (finrank K V)) K V)
     (M : ((Fin (finrank K V - r)) × Fin r) → A) :
     matrix_F K V r hr i i M = 1 := by
   ext a b
@@ -62,6 +63,10 @@ lemma chart_eq_id_of_diagonal (i : Basis (Fin (finrank K V)) K V)
   · simp only [ne_eq, h, not_false_eq_true, one_apply_ne, ite_eq_right_iff]
     rw [← Fin.ext_iff]
     simp only [h, false_implies]
+
+lemma matrix_G_eq_of_diagonal (i : Basis (Fin (finrank K V)) K V)
+    (M : ((Fin (finrank K V - r)) × Fin r) → A) :
+    matrix_G K V r hr i i M = Matrix.of (fun p q ↦ M (p,q)) := by sorry
 
 def element (i j : Basis (Fin (finrank K V)) K V) :
     (MvPolynomial ((Fin (finrank K V - r)) × Fin r) K) := by
@@ -78,6 +83,14 @@ abbrev matrix_G' (i j : Basis (Fin (finrank K V)) K V) :=
   (algebraMap (MvPolynomial (Fin (finrank K V - r) × Fin r) K)
   (Localization.Away (element K V r hr i j)))
 
+lemma matrix_F'_eq_id_of_diagonal (i : Basis (Fin (finrank K V)) K V) :
+    matrix_F' K V r hr i i = 1 := sorry
+
+lemma matrix_G'_eq_X_of_diagonal (i : Basis (Fin (finrank K V)) K V) :
+    matrix_G' K V r hr i i = Matrix.of (fun p q ↦
+    (algebraMap (MvPolynomial (Fin (finrank K V - r) × Fin r) K)
+    (Localization.Away (element K V r hr i i)) (MvPolynomial.X (p,q)))) := by sorry
+
 lemma isUnit_F' (i j : Basis (Fin (finrank K V)) K V) :
     IsUnit (matrix_F' K V r hr i j) := by
     rw [Matrix.isUnit_iff_isUnit_det]
@@ -91,7 +104,7 @@ lemma isUnit_F' (i j : Basis (Fin (finrank K V)) K V) :
 lemma element_eq_one_of_diagonal (i : Basis (Fin (finrank K V)) K V) :
     element K V r hr i i = 1 := by
   simp only [element]
-  rw [chart_eq_id_of_diagonal]
+  rw [matrix_F_eq_id_of_diagonal]
   simp only [det_one]
 
 abbrev open_immersion (i j : Basis (Fin (finrank K V)) K V) :=
@@ -122,7 +135,25 @@ def glueData : GlueData where
     · apply MvPolynomial.eval₂Hom (algebraMap K (Localization.Away (element K V r hr i j)))
       exact fun pq ↦ ((matrix_G' K V r hr i j) * (matrix_F' K V r hr i j)⁻¹) pq.1 pq.2
     · simp only [RingHom.mapMatrix_apply, MvPolynomial.coe_eval₂Hom]
-  t_id := sorry
+      sorry
+  t_id i := by
+    simp only [RingHom.mapMatrix_apply, id_eq]
+    rw [← Spec.map_id]
+    apply congrArg
+    change _ = CommRingCat.ofHom (RingHom.id _)
+    apply congrArg
+    simp_rw [matrix_F_eq_id_of_diagonal]
+    rw [matrix_G'_eq_X_of_diagonal]
+    simp only [map_zero, _root_.map_one, Matrix.map_one, inv_one, Matrix.mul_one, of_apply,
+      Prod.mk.eta]
+    have heq : MvPolynomial.eval₂Hom (algebraMap K (Localization.Away (element K V r hr i i)))
+      (fun pq ↦ (algebraMap (MvPolynomial (Fin (finrank K V - r) × Fin r) K) (Localization.Away
+      (element K V r hr i i))) (MvPolynomial.X pq)) = algebraMap _ _ := sorry
+    simp_rw [heq]
+    ext a
+    simp only [RingHom.id_apply]
+    change IsLocalization.awayLift _ _ = _
+    erw [IsLocalization.Away.AwayMap.lift_eq]
   t' := sorry
   t_fac := sorry
   cocycle := sorry
