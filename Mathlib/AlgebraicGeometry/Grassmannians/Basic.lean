bccb0d88b0d883d3648ac2b56c7a8f45629a7dca
morel <smorel@math.princeton.edu>
2024-06-26 21:53:45 +0200

    we're stuck!

--- Mathlib/AlgebraicGeometry/Grassmannians/Basic.lean
+++ Mathlib/AlgebraicGeometry/Grassmannians/Basic.lean
@@ -1,6 +1,7 @@
 import Mathlib.AlgebraicGeometry.Gluing
 import Mathlib.Algebra.Category.ModuleCat.Basic
 import Mathlib.LinearAlgebra.Matrix.Basis
+import Mathlib.LinearAlgebra.Matrix.NonsingularInverse
 import Mathlib.AlgebraicGeometry.OpenImmersion
 
 open AlgebraicGeometry Scheme FiniteDimensional CategoryTheory Matrix
@@ -18,25 +19,43 @@ variable (K V : Type u) [Field K] [AddCommGroup V] [Module K V]
 variable (r : ℕ) (hr : r < finrank K V)
 variable {A : Type*} [CommRing A] [Algebra K A]
 
-def Grassmannian.chart :=
+variable (R : Type*) [CommRing R] (f : R)
+
+example : IsUnit (algebraMap R (Localization.Away f) f) := by
+   refine isUnit_iff_exists_inv.mpr ?_
+   existsi IsLocalization.Away.invSelf f
+   simp only [IsLocalization.Away.mul_invSelf]
+
+namespace Grassmannian
+
+def chart :=
   Spec (CommRingCat.of (MvPolynomial ((Fin (finrank K V - r)) × Fin r) K))
 
-def Grassmannian.matrix_chart (i j : Basis (Fin (finrank K V)) K V) :
+def matrix (i j : Basis (Fin (finrank K V)) K V) :
     (((Fin (finrank K V - r)) × Fin r) → A) →
-    Matrix (Fin r) (Fin r) A := by
+    Matrix (Fin (finrank K V)) (Fin r) A := by
   intro φ
   set φ' : Matrix (Fin (finrank K V)) (Fin r) A := Matrix.of
     (fun x y ↦
       if x < r then if x.1 = y.1 then 1 else 0
-      else φ (⟨x.1 - r, by have := x.2; omega⟩, y)) -- φ ⟨x - r, y⟩
-  set M := (Basis.toMatrix i j).map (algebraMap K A) * φ'
-  exact Matrix.submatrix M (Fin.castLE hr.le) id
+      else φ (⟨x.1 - r, by have := x.2; omega⟩, y))
+  exact (Basis.toMatrix i j).map (algebraMap K A) * φ'
 
-lemma Grassmannian.chart_eq_id_of_diagonal (i : Basis (Fin (finrank K V)) K V)
+def matrix_F (i j : Basis (Fin (finrank K V)) K V) :
+    (((Fin (finrank K V - r)) × Fin r) → A) →
+    Matrix (Fin r) (Fin r) A :=
+  fun φ  ↦ Matrix.submatrix (matrix K V r hr i j φ) (Fin.castLE hr.le) id
+
+def matrix_G (i j : Basis (Fin (finrank K V)) K V) :
+    (((Fin (finrank K V - r)) × Fin r) → A) →
+    Matrix (Fin (finrank K V - r)) (Fin r) A :=
+  fun φ  ↦ Matrix.submatrix (matrix K V r hr i j φ) (Fin.castLE (by simp)) id
+
+lemma chart_eq_id_of_diagonal (i : Basis (Fin (finrank K V)) K V)
     (M : ((Fin (finrank K V - r)) × Fin r) → A) :
-    Grassmannian.matrix_chart K V r hr i i M = 1 := by
+    matrix_F K V r hr i i M = 1 := by
   ext a b
-  simp only [matrix_chart, Basis.toMatrix_self, map_zero, _root_.map_one, Matrix.map_one,
+  simp only [matrix_F, matrix, Basis.toMatrix_self, map_zero, _root_.map_one, Matrix.map_one,
     Matrix.one_mul, submatrix_apply, id_eq, of_apply, Fin.coe_castLE, Fin.is_lt, ↓reduceIte]
   by_cases h : a = b
   · simp only [h, ↓reduceIte, one_apply_eq]
@@ -44,39 +63,68 @@ lemma Grassmannian.chart_eq_id_of_diagonal (i : Basis (Fin (finrank K V)) K V)
     rw [← Fin.ext_iff]
     simp only [h, false_implies]
 
-def Grassmannian.element (i j : Basis (Fin (finrank K V)) K V) :
+def element (i j : Basis (Fin (finrank K V)) K V) :
     (MvPolynomial ((Fin (finrank K V - r)) × Fin r) K) := by
-  exact (Grassmannian.matrix_chart (hr := hr) i j MvPolynomial.X
+  exact (matrix_F (hr := hr) i j MvPolynomial.X
     (A := MvPolynomial ((Fin (finrank K V - r)) × Fin r) K)).det
 
-lemma Grassmannian.element_eq_one_of_diagonal (i : Basis (Fin (finrank K V)) K V) :
-    Grassmannian.element K V r hr i i = 1 := by
+abbrev matrix_F' (i j : Basis (Fin (finrank K V)) K V) :=
+  (algebraMap (MvPolynomial (Fin (finrank K V - r) × Fin r) K)
+    (Localization.Away (element K V r hr i j))).mapMatrix
+    (matrix_F K V r hr i j (MvPolynomial.X (R := K) (σ := Fin (finrank K V - r) × Fin r)))
+
+abbrev matrix_G' (i j : Basis (Fin (finrank K V)) K V) :=
+  (algebraMap (MvPolynomial (Fin (finrank K V - r) × Fin r) K)
+    (Localization.Away (element K V r hr i j))).mapMatrix
+    (matrix_F K V r hr i j (MvPolynomial.X (R := K) (σ := Fin (finrank K V - r) × Fin r)))
+
+lemma isUnit_F (i j : Basis (Fin (finrank K V)) K V) :
+    IsUnit (matrix_F' K V r hr i j) := by
+    rw [Matrix.isUnit_iff_isUnit_det]
+    rw [← RingHom.map_det]
+    simp only [element]
+    refine isUnit_iff_exists_inv.mpr ?_
+    existsi IsLocalization.Away.invSelf (matrix_F K V r hr i j
+      (MvPolynomial.X (R := K))).det
+    simp only [IsLocalization.Away.mul_invSelf]
+
+lemma element_eq_one_of_diagonal (i : Basis (Fin (finrank K V)) K V) :
+    element K V r hr i i = 1 := by
   simp only [element]
-  rw [Grassmannian.chart_eq_id_of_diagonal]
+  rw [chart_eq_id_of_diagonal]
   simp only [det_one]
 
-abbrev Grassmannian.open_immersion (i j : Basis (Fin (finrank K V)) K V) :=
+abbrev open_immersion (i j : Basis (Fin (finrank K V)) K V) :=
   Spec.map (CommRingCat.ofHom (algebraMap (MvPolynomial ((Fin (finrank K V - r)) × Fin r) K)
     (Localization.Away (Grassmannian.element K V r hr i j))))
 
-def Grassmannian.glueData : GlueData where
+def glueData : GlueData where
   J := Basis (Fin (finrank K V)) K V
-  U _ := Grassmannian.chart K V r
-  V ij := Spec (CommRingCat.of (Localization.Away (Grassmannian.element K V r hr ij.1 ij.2)))
-  f i j := Grassmannian.open_immersion K V r hr i j
+  U _ := chart K V r
+  V ij := Spec (CommRingCat.of (Localization.Away (element K V r hr ij.1 ij.2)))
+  f i j := open_immersion K V r hr i j
   f_mono _ _ := inferInstance
   f_hasPullback := inferInstance
   f_id i := by
-    simp only [Grassmannian.open_immersion]
+    simp only [open_immersion]
     suffices h : IsIso ((CommRingCat.ofHom (algebraMap (MvPolynomial
       (Fin (finrank K V - r) × Fin r) K) (Localization.Away (element K V r hr i i))))) by
       exact inferInstance
-    rw [Grassmannian.element_eq_one_of_diagonal]
+    rw [element_eq_one_of_diagonal]
     exact localization_unit_isIso (CommRingCat.of
       (MvPolynomial ((Fin (finrank K V - r)) × Fin r) K))
-  t := sorry
+  t i j := by
+    simp only
+    apply Spec.map
+    apply CommRingCat.ofHom
+    apply Localization.awayLift (r := element K V r hr j i)
+    swap
+    ·
+    · sorry
   t_id := sorry
   t' := sorry
   t_fac := sorry
   cocycle := sorry
   f_open _ _ := inferInstance
+
+end Grassmannian
