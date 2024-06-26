c012d79c0dd0086ae983a7d22b86e2a4fd293ad8
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2024-06-26 12:43:15 +0100

    better name

--- Mathlib/RingTheory/Ideal/QuotientOperations.lean
+++ Mathlib/RingTheory/Ideal/QuotientOperations.lean
@@ -417,7 +417,7 @@ theorem Quotient.mkₐ_ker (I : Ideal A) : RingHom.ker (Quotient.mkₐ R₁ I : 
   Ideal.mk_ker
 #align ideal.quotient.mkₐ_ker Ideal.Quotient.mkₐ_ker
 
-lemma cast_eq_cast_iff_sub_mem {I : Ideal A}
+lemma Quotient.cast_eq_cast_iff_sub_mem {I : Ideal A}
     (x y : A) : (Algebra.cast x : A ⧸ I) = y ↔ x - y ∈ I := Ideal.Quotient.mk_eq_mk_iff_sub_mem _ _
 
 variable {R₁}
