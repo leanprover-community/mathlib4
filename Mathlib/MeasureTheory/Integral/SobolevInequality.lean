2e64505b7ef95646c760428b0578a5851b3f74c8
Floris van Doorn <fpvdoorn@gmail.com>
2024-06-26 23:03:58 +0200

    small

--- Mathlib/MeasureTheory/Integral/SobolevInequality.lean
+++ Mathlib/MeasureTheory/Integral/SobolevInequality.lean
@@ -27,7 +27,7 @@ Let `n` be the dimension of the domain.
   Note that in this case the constant depends on the support of `u`.
 -/
 
--- removeme
+-- remove these once merging master
 set_option pp.explicit false --intentionally fail linter
 alias Continuous.memℒp_of_hasCompactSupport := MeasureTheory.Continuous.memℒp_of_hasCompactSupport
 alias Real.nnnorm_rpow_of_nonneg := NNReal.nnnorm_rpow_of_nonneg
