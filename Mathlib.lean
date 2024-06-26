ea5d7b758ef0789d80fe239e82628b273f3c1a6e
Yaël Dillies <yael.dillies@gmail.com>
2024-06-26 12:43:20 +0000

    feat: Fintype instance for `Shrink` (#13988)

--- Mathlib.lean
+++ Mathlib.lean
@@ -2007,6 +2007,7 @@ import Mathlib.Data.Fintype.Pi
 import Mathlib.Data.Fintype.Powerset
 import Mathlib.Data.Fintype.Prod
 import Mathlib.Data.Fintype.Quotient
+import Mathlib.Data.Fintype.Shrink
 import Mathlib.Data.Fintype.Sigma
 import Mathlib.Data.Fintype.Sort
 import Mathlib.Data.Fintype.Sum
