4000570d83490dfeaaeb3019671d0f87582af612
Jon Eugster <eugster.jon@gmail.com>
2024-06-26 18:01:31 +0200

    Apply suggestions from code review
    Co-authored-by: Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>

--- Mathlib/Geometry/RingedSpace/SheafedSpace.lean
+++ Mathlib/Geometry/RingedSpace/SheafedSpace.lean
@@ -93,7 +93,7 @@ instance : Category (SheafedSpace C) :=
   show Category (InducedCategory (PresheafedSpace C) SheafedSpace.toPresheafedSpace) by
     infer_instance
 
--- Porting note (#5229): adding an ext lemma.
+-- Porting note (#5229): adding an `ext` lemma.
 @[ext]
 theorem ext {X Y : SheafedSpace C} (α β : X ⟶ Y) (w : α.base = β.base)
     (h : α.c ≫ whiskerRight (eqToHom (by rw [w])) _ = β.c) : α = β :=
