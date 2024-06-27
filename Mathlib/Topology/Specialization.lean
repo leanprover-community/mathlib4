c51a25bf30b21fca0b5a0f56e58ac2f2f3574f59
Eric Wieser <wieser.eric@gmail.com>
2024-06-27 23:16:16 +0100

    Update Specialization.lean

--- Mathlib/Topology/Specialization.lean
+++ Mathlib/Topology/Specialization.lean
@@ -40,8 +40,9 @@ Iff.rfl
 
 /-- A recursor for `Specialization`. Use as `induction x`. -/
 @[elab_as_elim, cases_eliminator, induction_eliminator]
-protected def rec {β : Specialization α → Sort*} (h : ∀ a, β (toEquiv a)) (a : α) : β a :=
-h (ofEquiv a)
+protected def rec {β : Specialization α → Sort*} (h : ∀ a, β (toEquiv a)) (a : Specialization α) :
+    β a :=
+  h (ofEquiv a)
 
 variable [TopologicalSpace α] [TopologicalSpace β] [TopologicalSpace γ]
 
