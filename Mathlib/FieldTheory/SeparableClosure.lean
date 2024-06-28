67bc82d1ddb7cf6ad83d9d67c3f839281acb4d63
jjdishere <emailboxofjjd@163.com>
2024-06-28 15:27:03 +0200

    style

--- Mathlib/FieldTheory/SeparableClosure.lean
+++ Mathlib/FieldTheory/SeparableClosure.lean
@@ -219,7 +219,8 @@ theorem separableClosure.eq_top_iff : separableClosure F E = ⊤ ↔ Algebra.IsS
 /-- If `K / E / F` is a field extension tower, then `separableClosure F K` is contained in
 `separableClosure E K`. -/
 theorem separableClosure.le_restrictScalars [Algebra E K] [IsScalarTower F E K] :
-    separableClosure F K ≤ (separableClosure E K).restrictScalars F := fun _ h ↦ IsSeparable.of_isScalarTower E h
+    separableClosure F K ≤ (separableClosure E K).restrictScalars F :=
+  fun _ h ↦ IsSeparable.of_isScalarTower E h
 
 /-- If `K / E / F` is a field extension tower, such that `E / F` is separable, then
 `separableClosure F K` is equal to `separableClosure E K`. -/
