6a48520b2dd69a1a1238016e7d1c048ef6e8a9a1
jjdishere <emailboxofjjd@163.com>
2024-06-28 15:00:55 +0200

    Update CheckList.lean

--- Mathlib/FieldTheory/CheckList.lean
+++ Mathlib/FieldTheory/CheckList.lean
@@ -29,6 +29,6 @@ import Mathlib.FieldTheory.PurelyInseparable
 -/
 
 -- -1. add notation, replace into new notation (finished)
--- 0. rename IsSeparable lemmas (not finished) (check every lemma's name in change notation)
--- 1. add lemmas as in IsIntegral
+-- 0. rename IsSeparable lemmas (finished) (check every lemma's name in change notation)
+-- 1. add lemmas as in IsIntegral (aborted)
 -- 2. add separable_neg separable_smul and Field.separable_sub separable_pow,..., rename them (finished)
