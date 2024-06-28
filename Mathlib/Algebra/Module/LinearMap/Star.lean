9aaac0857311c5e41796ebe2b22de7691d195b74
Kim Morrison <kim@tqft.net>
2024-06-28 23:09:24 +1000

    Update Mathlib/Algebra/Module/LinearMap/Star.lean
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- Mathlib/Algebra/Module/LinearMap/Star.lean
+++ Mathlib/Algebra/Module/LinearMap/Star.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2024 Lean FRO. All rights reserved.
+Copyright (c) 2021 Frédéric Dupuis. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Kim Morrison
+Authors: Frédéric Dupuis
 -/
 import Mathlib.Algebra.Module.Equiv
 import Mathlib.Algebra.Star.Basic
