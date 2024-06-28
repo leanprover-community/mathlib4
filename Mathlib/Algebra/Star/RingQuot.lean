86e656ec42a4cfb04b52e01214de799dfb8f688c
Kim Morrison <kim@tqft.net>
2024-06-28 23:09:45 +1000

    Update Mathlib/Algebra/Star/RingQuot.lean
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- Mathlib/Algebra/Star/RingQuot.lean
+++ Mathlib/Algebra/Star/RingQuot.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2024 Lean FRO. All rights reserved.
+Copyright (c) 2020 Eric Wieser. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Kim Morrison
+Authors: Eric Wieser
 -/
 import Mathlib.Algebra.RingQuot
 import Mathlib.Algebra.Star.Basic
