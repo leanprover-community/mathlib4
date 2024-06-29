ce5fde505dd50530ecdd6969108652b8193639e5
Kim Morrison <kim@tqft.net>
2024-06-29 13:27:58 +1000

    fix

--- Mathlib/Algebra/Module/Equiv.lean
+++ Mathlib/Algebra/Module/Equiv.lean
@@ -7,7 +7,6 @@ Authors: Nathaniel Thomas, Jeremy Avigad, Johannes Hölzl, Mario Carneiro, Anne 
 import Mathlib.Algebra.Module.Hom
 import Mathlib.Algebra.Module.LinearMap.End
 import Mathlib.Algebra.Module.Pi
-import Mathlib.Algebra.Star.Basic
 
 #align_import algebra.module.equiv from "leanprover-community/mathlib"@"ea94d7cd54ad9ca6b7710032868abb7c6a104c9c"
 
