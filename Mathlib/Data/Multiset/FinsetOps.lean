105f1d25b785330ad71b1e6dfc548f322559168b
Eric Wieser <wieser.eric@gmail.com>
2024-06-30 08:18:49 +0000

    a few more

--- Mathlib/Data/Multiset/FinsetOps.lean
+++ Mathlib/Data/Multiset/FinsetOps.lean
@@ -285,6 +285,12 @@ theorem ndinter_eq_zero_iff_disjoint {s t : Multiset α} : ndinter s t = 0 ↔ D
   rw [← subset_zero]; simp [subset_iff, Disjoint]
 #align multiset.ndinter_eq_zero_iff_disjoint Multiset.ndinter_eq_zero_iff_disjoint
 
+alias ⟨_, Disjoint.ndinter_eq⟩ := ndinter_eq_zero_iff_disjoint
+
+theorem Subset.ndinter_eq_right {s t : Multiset α} (h : s ⊆ t) : s.ndinter t = s := by
+  induction s, t using Quot.induction_on₂
+  rw [quot_mk_to_coe'', quot_mk_to_coe'', coe_ndinter, List.Subset.inter_eq_right h]
+
 end Multiset
 
 -- Assert that we define `Finset` without the material on the set lattice.
