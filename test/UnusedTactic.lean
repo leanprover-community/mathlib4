37dbd78768b2f803f1bade8cbe5c907489520ab0
adomani <adomani@gmail.com>
2024-06-30 05:40:37 +0200

    add tests

--- test/UnusedTactic.lean
+++ test/UnusedTactic.lean
@@ -6,3 +6,19 @@ def why2 : True → True := (by refine ·)
 example : True := by
   #adaptation_note /--hi-/
   exact .intro
+
+-- both `;` and `<;>` are unseen by the linter
+example : True ∧ True := by
+  constructor <;> trivial;
+
+set_option linter.unusedTactic false
+/--
+warning: 'congr' tactic does nothing
+note: this linter can be disabled with `set_option linter.unusedTactic false`
+-/
+#guard_msgs in
+set_option linter.unusedTactic true in
+-- the linter notices that `congr` is unused
+example : True := by
+  congr
+  constructor
