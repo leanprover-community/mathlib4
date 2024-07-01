9824a8c64d23117b06212363909da4193a6eee41
Kim Morrison <kim@tqft.net>
2024-07-01 22:53:21 +1000

    fix imports

--- Mathlib/Algebra/Star/Basic.lean
+++ Mathlib/Algebra/Star/Basic.lean
@@ -3,6 +3,7 @@ Copyright (c) 2020 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison
 -/
+import Mathlib.Algebra.Field.Defs
 import Mathlib.Algebra.Group.Invertible.Defs
 import Mathlib.Algebra.Regular.Basic
 import Mathlib.Algebra.Ring.Aut
