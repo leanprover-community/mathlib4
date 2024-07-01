35dbcb364b7021816a730e6800c117c2128009d8
Matthew Ballard <matt@mrb.email>
2024-07-01 16:09:14 -0400

    disambiguate variables

--- Mathlib/Algebra/BigOperators/Finsupp.lean
+++ Mathlib/Algebra/BigOperators/Finsupp.lean
@@ -690,10 +690,10 @@ namespace Nat
 
 -- Porting note: Needed to replace pow with (· ^ ·)
 /-- If `0 : ℕ` is not in the support of `f : ℕ →₀ ℕ` then `0 < ∏ x ∈ f.support, x ^ (f x)`. -/
-theorem prod_pow_pos_of_zero_not_mem_support {f : ℕ →₀ ℕ} (hf : 0 ∉ f.support) :
+theorem prod_pow_pos_of_zero_not_mem_support {f : ℕ →₀ ℕ} (nhf : 0 ∉ f.support) :
     0 < f.prod (· ^ ·) :=
-  Nat.pos_iff_ne_zero.mpr <| Finset.prod_ne_zero_iff.mpr fun _ H =>
-    pow_ne_zero _ fun H => by subst H; exact hf H
+  Nat.pos_iff_ne_zero.mpr <| Finset.prod_ne_zero_iff.mpr fun _ hf =>
+    pow_ne_zero _ fun H => by subst H; exact nhf hf
 #align nat.prod_pow_pos_of_zero_not_mem_support Nat.prod_pow_pos_of_zero_not_mem_support
 
 end Nat
