1b2148509b9dfd6074d89ab6f7c92e41b28c3ee0
Riccardo Brasca <riccardo.brasca@gmail.com>
2024-07-01 10:52:44 +0200

    Update Mathlib/NumberTheory/FLT/Three.lean
    Co-authored-by: Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>

--- Mathlib/NumberTheory/FLT/Three.lean
+++ Mathlib/NumberTheory/FLT/Three.lean
@@ -430,7 +430,7 @@ lemma associated_of_dvd_a_add_b_of_dvd_a_add_eta_mul_b {p : 𝓞 K} (hp : Prime 
 is associated with `λ`. -/
 lemma associated_of_dvd_a_add_b_of_dvd_a_add_eta_sq_mul_b {p : 𝓞 K} (hp : Prime p)
     (hpab : p ∣ (S.a + S.b)) (hpaetasqb : p ∣ (S.a + η ^ 2 * S.b)) : Associated p λ := by
-  suffices p_lam : (p ∣ λ) from hp.associated_of_dvd hζ.zeta_sub_one_prime' p_lam
+  suffices p_lam : p ∣ λ from hp.associated_of_dvd hζ.zeta_sub_one_prime' p_lam
   by_contra p_lam
   refine hp.not_unit <| S.coprime.isUnit_of_dvd' ?_ ?_
   · refine (hp.dvd_or_dvd ?_).resolve_left p_lam
