68758f0c665f3151854f291346fb0b4589a3c7f3
Scott Carnahan <128885296+ScottCarnahan@users.noreply.github.com>
2024-07-02 01:49:49 +0900

    minimize imports

--- Mathlib/Algebra/Vertex/HVertexOperator.lean
+++ Mathlib/Algebra/Vertex/HVertexOperator.lean
@@ -3,7 +3,6 @@ Copyright (c) 2024 Scott Carnahan. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Carnahan
 -/
-import Mathlib.Algebra.BigOperators.Group.Finset
 import Mathlib.RingTheory.HahnSeries.Multiplication
 
 /-!
