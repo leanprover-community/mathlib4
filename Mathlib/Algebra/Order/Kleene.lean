a1d9be921d293aab896417ef12ad386b0a00ad7a
Matthew Ballard <matt@mrb.email>
2024-07-01 18:15:41 -0400

    shake

--- Mathlib/Algebra/Order/Kleene.lean
+++ Mathlib/Algebra/Order/Kleene.lean
@@ -3,8 +3,9 @@ Copyright (c) 2022 Siddhartha Prasad, Yaël Dillies. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Siddhartha Prasad, Yaël Dillies
 -/
-import Mathlib.Algebra.Order.Ring.Prod
+import Mathlib.Algebra.Order.Monoid.Canonical.Defs
 import Mathlib.Algebra.Ring.Pi
+import Mathlib.Algebra.Ring.Prod
 import Mathlib.Algebra.Ring.InjSurj
 import Mathlib.Tactic.Monotonicity.Attr
 
