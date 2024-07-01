154ed21b9acb485888a6ff794e16f386e35d4cef
Yuma Mizuno <mizuno.y.aj@gmail.com>
2024-07-01 15:52:38 +0100

    update test

--- test/CategoryTheory/Monoidal.lean
+++ test/CategoryTheory/Monoidal.lean
@@ -21,6 +21,6 @@ variable {X Y Z W : C} (f : X ⟶ Y) (g : Y ⟶ Z)
 #guard_expr normalize% (α_ X Y Z).hom = (α_ _ _ _).hom
 #guard_expr normalize% (α_ X Y Z).inv = (α_ _ _ _).inv
 #guard_expr normalize% 𝟙 (X ⊗ Y) = 𝟙 (X ⊗ Y)
-#guard_expr normalize% f ⊗ g = _ ≫ f ▷ _ ≫ _ ≫ _ ◁ g ≫ _
+#guard_expr normalize% f ⊗ g = _ ≫ (f ⊗ g) ≫ _
 variable {V₁ V₂ V₃ : C} (R : ∀ V₁ V₂ : C, V₁ ⊗ V₂ ⟶ V₂ ⊗ V₁) in
 #guard_expr normalize% R V₁ V₂ ▷ V₃ ⊗≫ V₂ ◁ R V₁ V₃ = _ ≫ R V₁ V₂ ▷ V₃ ≫ _ ≫ V₂ ◁ R V₁ V₃ ≫ _
