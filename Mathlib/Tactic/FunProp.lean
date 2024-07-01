bdee5cb90aad63e9e6ee2bf9582cadfb67d5f09a
lecopivo <skrivantomas@seznam.cz>
2024-07-01 17:27:09 -0400

    fix imports and license

--- Mathlib/Tactic/FunProp.lean
+++ Mathlib/Tactic/FunProp.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2024 Tomas Skrivan. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Tomas Skrivan
+-/
 import Mathlib.Tactic.FunProp.Attr
 import Mathlib.Tactic.FunProp.Core
 import Mathlib.Tactic.FunProp.Decl
