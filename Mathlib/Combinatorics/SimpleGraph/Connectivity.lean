0d28e97dd3f450046bc0eedd7c13312955c268a7
Rida Hamadani <mridahamadani@gmail.com>
2024-07-01 09:04:24 +0300

    `not_reachable_iff_Walk_isEmpty`

--- Mathlib/Combinatorics/SimpleGraph/Connectivity.lean
+++ Mathlib/Combinatorics/SimpleGraph/Connectivity.lean
@@ -2001,6 +2001,10 @@ theorem reachable_iff_nonempty_univ {u v : V} :
   Set.nonempty_iff_univ_nonempty
 #align simple_graph.reachable_iff_nonempty_univ SimpleGraph.reachable_iff_nonempty_univ
 
+
+lemma not_reachable_iff_Walk_isEmpty {u v : V} : ¬G.Reachable u v ↔ IsEmpty (G.Walk u v) := by
+   rw [reachable_iff_nonempty_univ, ← Set.nonempty_iff_univ_nonempty, not_nonempty_iff]
+
 protected theorem Reachable.elim {p : Prop} {u v : V} (h : G.Reachable u v)
     (hp : G.Walk u v → p) : p :=
   Nonempty.elim h hp
