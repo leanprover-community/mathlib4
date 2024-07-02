c6f5dee0d4f9e0088f6e2bf8e2cc726991a84c96
Peter Nelson <71660771+apnelson1@users.noreply.github.com>
2024-07-02 09:20:38 -0400

    import

--- Mathlib.lean
+++ Mathlib.lean
@@ -2164,9 +2164,9 @@ import Mathlib.Data.Multiset.Sym
 import Mathlib.Data.NNRat.BigOperators
 import Mathlib.Data.NNRat.Defs
 import Mathlib.Data.NNRat.Lemmas
-import Mathlib.Data.Nat.BitIndices
 import Mathlib.Data.NNReal.Basic
 import Mathlib.Data.NNReal.Star
+import Mathlib.Data.Nat.BitIndices
 import Mathlib.Data.Nat.Bits
 import Mathlib.Data.Nat.Bitwise
 import Mathlib.Data.Nat.Cast.Basic
