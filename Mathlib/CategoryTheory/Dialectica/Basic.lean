b22c23796aa1eb9d873956ae7e724085a7f649c1
Mario Carneiro <di.gama@gmail.com>
2024-07-02 23:44:59 +0200

    add Valeria's notes
    Co-Authored-By: Valeria de Paiva <valeria.depaiva@gmail.com>

--- Mathlib/CategoryTheory/Dialectica/Basic.lean
+++ Mathlib/CategoryTheory/Dialectica/Basic.lean
@@ -11,10 +11,29 @@ import Mathlib.CategoryTheory.Limits.Shapes.FiniteProducts
 
 We define the category `Dial` of the Dialectica interpretation, after [dialectica1989].
 
+## Background
+
+Dialectica categories are important models of linear type theory. They satisfy most of the
+distinctions that linear logic was meant to introduce and many models do not satisfy, like the
+independence of constants. Many linear type theories are being used at the
+moment--[nLab] describes some of them: for quantum systems, for effects in programming, for linear
+dependent types. In particular, dialectica categories are connected to polynomial functors, being a
+slightly more sophisticated version of polynomial types, as discussed, for instance, in Moss and
+von Glehn's [*Dialectica models of type theory*]. As such they are related to the polynomial
+constructions being [developed][Poly] by Awodey, Riehl, and Hazratpour. For the non-dependent
+version developed here several applications are known to Petri Nets, small cardinals
+in Set Theory, state in imperative programming, and others, see [Dialectica Categories].
+
 ## References
 
 * [Valeria de Paiva, The Dialectica Categories.][dialectica1989]
   ([pdf](https://www.cl.cam.ac.uk/techreports/UCAM-CL-TR-213.pdf))
+
+[nLab]: https://ncatlab.org/nlab/show/linear+type+theory
+[*Dialectica models of type theory*]: https://arxiv.org/abs/2105.00283
+[Poly]: https://github.com/sinhp/Poly
+[Dialectica Categories]: https://github.com/vcvpaiva/DialecticaCategories
+
 -/
 
 noncomputable section
