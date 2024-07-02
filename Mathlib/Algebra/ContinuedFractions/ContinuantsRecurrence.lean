17f0270ee26ad94b1ade703b28d1d9d662743781
Komyyy <pol_tta@outlook.jp>
2024-07-03 07:56:20 +0900

    isolated `by`

--- Mathlib/Algebra/ContinuedFractions/ContinuantsRecurrence.lean
+++ Mathlib/Algebra/ContinuedFractions/ContinuantsRecurrence.lean
@@ -26,8 +26,8 @@ variable {K : Type*} {g : GCF K} {n : ℕ} [DivisionRing K]
 theorem contsAux_recurrence {gp ppred pred : Pair K} (nth_s_eq : g.s.get? n = some gp)
     (nth_contsAux_eq : g.contsAux n = ppred)
     (succ_nth_contsAux_eq : g.contsAux (n + 1) = pred) :
-    g.contsAux (n + 2) = ⟨gp.b * pred.a + gp.a * ppred.a, gp.b * pred.b + gp.a * ppred.b⟩ :=
-  by simp [*, contsAux, nextConts, nextDen, nextNum]
+    g.contsAux (n + 2) = ⟨gp.b * pred.a + gp.a * ppred.a, gp.b * pred.b + gp.a * ppred.b⟩ := by
+  simp [*, contsAux, nextConts, nextDen, nextNum]
 #align generalized_continued_fraction.continuants_aux_recurrence GCF.contsAux_recurrence
 
 theorem conts_recurrenceAux {gp ppred pred : Pair K} (nth_s_eq : g.s.get? n = some gp)
