d49ce0b0c52e7599a20a5532be82c66fffa4ab6f
Peter Nelson <71660771+apnelson1@users.noreply.github.com>
2024-07-02 12:25:08 -0400

    shake

--- Mathlib/Logic/Embedding/Set.lean
+++ Mathlib/Logic/Embedding/Set.lean
@@ -3,10 +3,11 @@ Copyright (c) 2017 Johannes Hölzl. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl, Mario Carneiro
 -/
-import Mathlib.Data.Set.Pairwise.Basic
 import Mathlib.Data.Set.Notation
 import Mathlib.Order.SetNotation
 import Mathlib.Logic.Embedding.Basic
+import Mathlib.Logic.Pairwise
+import Mathlib.Data.Set.Image
 
 
 #align_import logic.embedding.set from "leanprover-community/mathlib"@"fc2ed6f838ce7c9b7c7171e58d78eaf7b438fb0e"
