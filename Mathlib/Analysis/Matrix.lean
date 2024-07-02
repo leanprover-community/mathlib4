82b3f82fb4e410810fd83c23d90a0a0434f76d8d
adomani <adomani@gmail.com>
2024-07-02 18:30:18 +0200

    another import removal

--- Mathlib/Analysis/Matrix.lean
+++ Mathlib/Analysis/Matrix.lean
@@ -3,8 +3,6 @@ Copyright (c) 2021 Heather Macbeth. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Heather Macbeth, Eric Wieser
 -/
-import Mathlib.Analysis.NormedSpace.PiLp
-import Mathlib.Analysis.InnerProductSpace.PiL2
 
 #align_import analysis.matrix from "leanprover-community/mathlib"@"46b633fd842bef9469441c0209906f6dddd2b4f5"
 
