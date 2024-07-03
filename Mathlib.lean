bddc940754b2e5aeeb3054721f344a8483189568
Matthew Ballard <matt@mrb.email>
2024-07-03 05:35:36 -0400

    mk_all

--- Mathlib.lean
+++ Mathlib.lean
@@ -535,6 +535,7 @@ import Mathlib.Algebra.Order.Group.PosPart
 import Mathlib.Algebra.Order.Group.Prod
 import Mathlib.Algebra.Order.Group.Synonym
 import Mathlib.Algebra.Order.Group.TypeTags
+import Mathlib.Algebra.Order.Group.Unbundled.Basic
 import Mathlib.Algebra.Order.Group.Units
 import Mathlib.Algebra.Order.Group.WithTop
 import Mathlib.Algebra.Order.GroupWithZero.Canonical
