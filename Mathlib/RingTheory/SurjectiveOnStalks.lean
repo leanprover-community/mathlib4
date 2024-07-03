e23c155d39e4e2720c49acfa5bcaec924dca56ff
Andrew Yang <36414270+erdOne@users.noreply.github.com>
2024-07-03 14:21:58 +0800

    Update SurjectiveOnStalks.lean

--- Mathlib/RingTheory/SurjectiveOnStalks.lean
+++ Mathlib/RingTheory/SurjectiveOnStalks.lean
@@ -155,7 +155,7 @@ lemma SurjectiveOnStalks.baseChange
       one_mul, mul_one, id_apply, ← e]
     rw [Algebra.algebraMap_eq_smul_one, ← smul_tmul', smul_mul_assoc]
 
-lemma surjectiveOnStalks_iff_of_isLocalRingHom [LocalRing R] [LocalRing S] [IsLocalRingHom f] :
+lemma surjectiveOnStalks_iff_of_isLocalRingHom [LocalRing S] [IsLocalRingHom f] :
     f.SurjectiveOnStalks ↔ Function.Surjective f := by
   refine ⟨fun H x ↦ ?_, fun h ↦ SurjectiveOnStalks.of_surjective h⟩
   obtain ⟨y, r, c, hc, hr, e⟩ :=
