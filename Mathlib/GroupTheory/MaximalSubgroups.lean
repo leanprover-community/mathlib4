b96d5e55aff732670430d4f2a93f199af9fa30b5
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2024-07-04 00:20:06 +0200

    fix two import

--- Mathlib/GroupTheory/MaximalSubgroups.lean
+++ Mathlib/GroupTheory/MaximalSubgroups.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Antoine Chambert-Loir
 
 -/
-import Mathlib.GroupTheory.Subgroup.Basic
+import Mathlib.Algebra.Group.Subgroup.Basic
 
 /-! # Maximal subgroups
 
