0b5117f6f2ed7e9a273633ba12f67f4fa699403d
Christian Merten <christian@merten.dev>
2024-07-03 23:02:46 +0200

    apply review suggestions

--- Mathlib/RingTheory/Smooth/StandardSmooth.lean
+++ Mathlib/RingTheory/Smooth/StandardSmooth.lean
@@ -10,69 +10,66 @@ import Mathlib.RingTheory.Presentation
 /-!
 # Standard smooth algebras
 
-In this file we define standard smooth presentations for algebras. This notion
-is introduced for `SubmersivePresentations`, which is a `Presentation`
-that has less relations than generators. More precisely there exists
-an injective map from `P.relations` to `P.vars`.
+In this file we define standard smooth algebras. For this we introduce
+the notion of a `PreSubmersivePresentation`. This is a presentation `P` that has
+fewer relations than generators. More precisely there exists an injective map from `P.rels`
+to `P.vars`. To such a presentation we may associate a jacobian. `P` is then a submersive
+presentation, if its jacobian is invertible.
 
-Finally, we introduce the notion of standard smooth algebras, which are algebras that
-admit a standard smooth presentation.
+Finally, a standard smooth algebra is an algebra that admits a submersive presentation.
 
 ## Main definitions
 
 All of these are in the `Algebra` namespace. Let `S` be an `R`-algebra.
 
-- `SubmersivePresentation`: A `Presentation` of `S` as `R`-algebra, equipped with an injective map
-  from `P.relations` to `P.vars`. This map is used to define the differential of a submersive
-  presentation.
+- `PreSubmersivePresentation`: A `Presentation` of `S` as `R`-algebra, equipped with an injective
+  map `P.map` from `P.rels` to `P.vars`. This map is used to define the differential of a
+  presubmersive presentation.
 
-Let now `P` be a submersive presentation of `S` over `R`.
+For a presubmersive presentation `P` of `S` over `R` we make the following definitions:
 
-- `SubmersivePresentation.differential`: A linear endomorphism of `P.rels → P.ring` sending
+- `PreSubmersivePresentation.differential`: A linear endomorphism of `P.rels → P.Ring` sending
   the `j`-th standard basis vector, corresponding to the `j`-th relation, to the vector
   of partial derivatives of `P.relation j` with respect to the coordinates `P.map i` for
   `i : P.rels`.
-- `SubmersivePresentation.jacobian`: The determinant of `P.differential`.
-- `SubmersivePresentation.jacobiMatrix`: If `P.rels` has a `Fintype` instance, we may form
+- `PreSubmersivePresentation.jacobian`: The determinant of `P.differential`.
+- `PreSubmersivePresentation.jacobiMatrix`: If `P.rels` has a `Fintype` instance, we may form
   the matrix corresponding to `P.differential`. Its determinant is `P.jacobian`.
-- `SubmersivePresentation.IsStandardSmooth`: `P` is standard smooth, if it's jacobian
-  is a unit in `S` and it is finite.
+- `SubmersivePresentation`: A submersive presentation is a finite, presubmersive presentation `P`
+  with in `S` invertible jacobian.
 
-Furthermore, we define:
+Furthermore, for algebras we define:
 
-- `IsStandardSmooth`: `S` is `R`-standard smooth if `S` has a standard smooth submersive
+- `IsStandardSmooth`: `S` is `R`-standard smooth if `S` admits a submersive
   `R`-presentation.
-- `IsOfRelativeDimension n`: A standard smooth `R`-algebra `S` is of relative dimension `n`
-  if `S` has a standard smooth submersive `R`-presentation of dimension `n`. Equivalently,
-  that every standard smooth has dimension `n` (TODO, see below).
-- `IsStandardSmoothOfRelativeDimension n`: The combination of the first two.
+- `IsStandardSmooth.relativeDimension`: If `S` is `R`-standard smooth this is the dimension
+  of an arbitrary submersive `R`-presentation of `S`. This is independent of the choice
+  of the presentation (TODO, see below).
 
 Finally, in the `RingHom` namespace we define
 
-- `IsStandardSmooth`: A ring hom `R →+* S` is standard smooth if `S` is standard smooth as
-  `R`-algebra.
+- `IsStandardSmooth`: A ring homomorphism `R →+* S` is standard smooth if `S` is standard smooth
+  as `R`-algebra.
+- `IsStandardSmoothOfRelativeDimension n`: A ring homomorphism `R →+* S` is standard smooth of
+  relative dimension `n` if it is standard smooth and the relative dimension of `S` as an
+  `R`-algebra is `n`.
 
 ## TODO
 
 - Show that the canonical presentation for localization away from an element is standard smooth
   of relative dimension 0.
-- Show that the base change of a standard smooth presentation is standard smooth of equal relative
+- Show that the base change of a submersive presentation is submersive of equal relative
   dimension.
-- Show that the composition of standard smooth presentations of relative dimensions `n` and `m` is
-  standard smooth of relative dimension `n + m`.
-- Show that the Kaehler differentials of a standard smooth `R`-algebra `S` of relative dimension `n`
-  are `S`-free of rank `n`. In particular this shows that the relative dimension is independent
-  of the choice of the standard smooth presentation.
+- Show that the composition of submersive presentations of relative dimensions `n` and `m` is
+  submersive of relative dimension `n + m`.
+- Show that the module of Kaehler differentials of a standard smooth `R`-algebra `S` of relative
+  dimension `n` is `S`-free of rank `n`. In particular this shows that the relative dimension
+  is independent of the choice of the standard smooth presentation.
 
 ## Implementation details
 
-We don't bundle `IsStandardSmooth` and `IsOfRelativeDimension n` in one typeclass, because then
-`IsStandardSmooth` can't be inferred from `StandardSmoothOfRelativeDimension n` as Lean can't know
-which `n` to choose. Mathematically this is still correct, since the dimension
-does not depend on the choice of the standard smooth presentation.
-
-Standard smooth algebras and ring homs feature 4 universe levels: The universe levels of the rings
-involved and the universe levels of the types of the variables and relations.
+Standard smooth algebras and ring homomorphisms feature 4 universe levels: The universe levels of
+the rings involved and the universe levels of the types of the variables and relations.
 
 ## Notes
 
@@ -91,25 +88,25 @@ open TensorProduct
 namespace Algebra
 
 /--
-A `SubmersivePresentation` of an `R`-algebra `S` is a `Presentation`
+A `PreSubmersivePresentation` of an `R`-algebra `S` is a `Presentation`
 with finitely-many relations equipped with an injective `map : relations → vars`.
 
 This map determines how the differential of `P` is constructed. See
-`SubmersivePresentation.differential` for details.
+`PreSubmersivePresentation.differential` for details.
 -/
 @[nolint checkUnivs]
-structure SubmersivePresentation extends Algebra.Presentation.{t, w} R S where
+structure PreSubmersivePresentation extends Algebra.Presentation.{t, w} R S where
   /-- A map from the relations type to the variables type. Used to compute the differential. -/
   map : rels → vars
   map_inj : Function.Injective map
   relations_finite : Finite rels
 
-namespace SubmersivePresentation
+namespace PreSubmersivePresentation
 
 attribute [instance] relations_finite
 
 variable {R S}
-variable (P : SubmersivePresentation R S)
+variable (P : PreSubmersivePresentation R S)
 
 lemma card_relations_le_card_vars_of_isFinite [P.IsFinite] :
     Nat.card P.rels ≤ Nat.card P.vars :=
@@ -120,21 +117,21 @@ noncomputable abbrev basis : Basis P.rels P.Ring (P.rels → P.Ring) :=
   Pi.basisFun P.Ring P.rels
 
 /--
-The differential of a `P : SubmersivePresentation` is a `P.Ring`-linear map on
+The differential of a `P : PreSubmersivePresentation` is a `P.Ring`-linear map on
 `P.rels → P.Ring`:
 
-The `j`-th standard basis vector, corresponding to the `j`-th relation of `P` is mapped
+The `j`-th standard basis vector, corresponding to the `j`-th relation of `P`, is mapped
 to the vector of partial derivatives of `P.relation j` with respect
 to the coordinates `P.map i` for all `i : P.rels`.
 
-The determinant of this map is the jacobian of `P` used to define when a `SubmersivePresentation`
-is standard smooth. See `SubmersivePresentation.jacobian`.
+The determinant of this map is the jacobian of `P` used to define when a `PreSubmersivePresentation`
+is submersive. See `PreSubmersivePresentation.jacobian`.
 -/
 noncomputable def differential : (P.rels → P.Ring) →ₗ[P.Ring] (P.rels → P.Ring) :=
   Basis.constr P.basis P.Ring
     (fun j i : P.rels ↦ MvPolynomial.pderiv (P.map i) (P.relation j))
 
-/-- The jacobian of a `P : SubmersivePresentation` is the determinant
+/-- The jacobian of a `P : PreSubmersivePresentation` is the determinant
 of `P.linearMap` viewed as element of `S`. -/
 noncomputable def jacobian : S :=
   algebraMap P.Ring S <| LinearMap.det P.differential
@@ -159,34 +156,35 @@ lemma jacobiMatrix_apply (i j : P.rels) :
 
 end Matrix
 
+end PreSubmersivePresentation
+
 /--
-A `SubmersivePresentation` is standard smooth if its jacobian is a unit in `S`
+A `PreSubmersivePresentation` is submersive if its jacobian is a unit in `S`
 and the presentation is finite.
 -/
-class IsStandardSmooth : Prop where
-  jacobian_isUnit : IsUnit P.jacobian
-  isFinite : P.IsFinite := by infer_instance
+@[nolint checkUnivs]
+structure SubmersivePresentation extends PreSubmersivePresentation.{t, w} R S where
+  jacobian_isUnit : IsUnit toPreSubmersivePresentation.jacobian
+  isFinite : toPreSubmersivePresentation.IsFinite := by infer_instance
 
-attribute [instance] IsStandardSmooth.isFinite
-
-end SubmersivePresentation
+attribute [instance] SubmersivePresentation.isFinite
 
 /--
 An `R`-algebra `S` is called standard smooth, if there
-exists a standard smooth submersive presentation.
+exists a submersive presentation.
 -/
 class IsStandardSmooth : Prop where
-  out : ∃ (P : SubmersivePresentation.{t, w} R S), P.IsStandardSmooth
+  out : Nonempty (SubmersivePresentation.{t, w} R S)
 
 /--
 The relative dimension of a standard smooth `R`-algebra `S` is
-the dimension of an arbitrarily chosen standard smooth `R`-presentation of `S`.
+the dimension of an arbitrarily chosen submersive `R`-presentation of `S`.
 
 Note: This number is independent of the choice of the presentation as it is equal to
 the `S`-rank of `Ω[S/R]` (TODO).
 -/
 noncomputable def IsStandardSmooth.relativeDimension [IsStandardSmooth R S] : ℕ :=
-  ‹IsStandardSmooth R S›.out.choose.dimension
+  ‹IsStandardSmooth R S›.out.some.dimension
 
 end Algebra
 
@@ -194,11 +192,11 @@ namespace RingHom
 
 variable {R S}
 
-/-- A ring hom `R →+* S` is standard smooth if `S` is standard smooth as `S`-algebra. -/
+/-- A ring homomorphism `R →+* S` is standard smooth if `S` is standard smooth as `S`-algebra. -/
 def IsStandardSmooth (f : R →+* S) : Prop :=
   @Algebra.IsStandardSmooth.{t, w} _ _ _ _ f.toAlgebra
 
-/-- A ring hom `R →+* S` is standard smooth of relative dimension `n` if
+/-- A ring homomorphism `R →+* S` is standard smooth of relative dimension `n` if
 it is both standard smooth and is of relative dimension `n`. -/
 structure IsStandardSmoothOfRelativeDimension (n : ℕ) (f : R →+* S) : Prop where
   isStandardSmooth : IsStandardSmooth.{t, w} f
