d2e8846f9bb14855a6d84e36552bc60e38bab88f
Jujian Zhang <jujian.zhang1998@outlook.com>
2024-07-05 15:27:06 +0000

    feat(RingTheory/TwosidedIdeal/Basic): define two-sided-ideal (#13902)
    Co-authored-by: zjj <j.jj@j.jj>

--- Mathlib.lean
+++ Mathlib.lean
@@ -3789,6 +3789,7 @@ import Mathlib.RingTheory.Smooth.Basic
 import Mathlib.RingTheory.TensorProduct.Basic
 import Mathlib.RingTheory.TensorProduct.MvPolynomial
 import Mathlib.RingTheory.Trace
+import Mathlib.RingTheory.TwoSidedIdeal.Basic
 import Mathlib.RingTheory.UniqueFactorizationDomain
 import Mathlib.RingTheory.Unramified.Basic
 import Mathlib.RingTheory.Unramified.Derivations
