11ba2729e93354a7b28067b23273827554f54b5e
adomani <adomani@gmail.com>
2024-07-05 07:21:43 +0200

    use `getVisited`, parse also generic syntax

--- Mathlib/Tactic/MinImports.lean
+++ Mathlib/Tactic/MinImports.lean
@@ -26,14 +26,30 @@ def getSyntaxNodeKinds : Syntax → NameSet
   | .ident _ _ nm _ => NameSet.empty.insert nm
   | _ => {}
 
+/-- extracts the names of the declarations in `env` on which `decl` depends. -/
+-- source:
+-- https://leanprover.zulipchat.com/#narrow/stream/287929-mathlib4/topic/Counting.20prerequisites.20of.20a.20theorem/near/425370265
+def getVisited (env : Environment) (decl : Name) : NameSet :=
+  let (_, { visited, .. }) := CollectAxioms.collect decl |>.run env |>.run {}
+  visited
+
+def getDeclId (stx : Syntax) : Syntax :=
+  (stx.find? (·.isOfKind ``Lean.Parser.Command.declId)).getD default
+
+def getId (stx : Syntax) : Syntax :=
+  match stx.find? (·.isOfKind ``Lean.Parser.Command.declId) with
+    | some declId => declId[0]
+    | none => stx
+
 /--`getSyntaxImports cmd` takes a `Syntax` input `cmd` and returns the `NameSet` of all the
 module names that are implied by the `SyntaxNodeKinds` and the identifiers contained in `cmd`. -/
 def getSyntaxImports {m : Type → Type} [Monad m] [MonadResolveName m] [MonadEnv m]
     [MonadQuotation m] (cmd : Syntax) (dbg? : Bool := false) :
     m NameSet := do
   let env ← getEnv
-  let ts := getSyntaxNodeKinds cmd
-  if dbg? then dbg_trace "{ts.toArray.qsort (·.toString < ·.toString)}"
+  --let nm ← liftCoreM do realizeGlobalConstNoOverloadWithInfo (getId cmd)
+  let ts := (getSyntaxNodeKinds cmd).append <| getVisited env (getId cmd).getId
+  if dbg? then dbg_trace "{ts.toArray.qsort Name.lt}"
   let mut hm : HashMap Nat Name := {}
   for imp in env.header.moduleNames do
     hm := hm.insert ((env.getModuleIdx? imp).getD default) imp
@@ -47,22 +63,52 @@ def getSyntaxImports {m : Type → Type} [Monad m] [MonadResolveName m] [MonadEn
         if !fins.contains new then fins := fins.insert new
   return fins
 
-/-- `#min_imps cmd` scans the syntax `cmd` and the declaration obtained by elaborating `cmd`
-to find a collection of minimal imports that should be sufficient for `cmd` to work. -/
-elab "#min_imps " tk:("!")? cmd:command : command => do
-  Elab.Command.elabCommand cmd
+/-
+def importTips (ns : NameSet) : CommandElabM NameSet := do
+  let all := (← getEnv).allImportedModuleNames
+  dbg_trace ← getBuildDir
+  dbg_trace ← getLibDir ""
+  let allFileNames := all.map (modToFilePath "." · "lean")
+  dbg_trace allFileNames
+  let importDatas ← allFileNames.mapM fun n => return (← readModuleData n).1
+  dbg_trace importDatas.map fun i => i.imports
+  return ns
+
+run_cmd
+  let _ ← importTips <| (NameSet.empty).insert `Mathlib.Init.Data.Nat.Basic
+-/
+
+def getAllImports {m : Type → Type} [Monad m] [MonadResolveName m] [MonadEnv m] [MonadQuotation m]
+    (stx : Syntax) : m NameSet := do
   let env ← getEnv
-  let exprImports := ← match cmd.raw.find? (·.isOfKind ``Lean.Parser.Command.declId) with
-                      | none => return .empty
-                      | some declId => liftCoreM do
-                        (← realizeGlobalConstNoOverloadWithInfo declId[0]).findHome (some env)
-  let fins ← getSyntaxImports cmd true
+  let exprImports := {} --← match cmd.raw.find? (·.isOfKind ``Lean.Parser.Command.declId) with
+                     -- | none => return .empty
+                     -- | some declId => liftCoreM do
+                     --   (← realizeGlobalConstNoOverloadWithInfo declId[0]).findHome (some env)
+  let fins ← getSyntaxImports stx --true
   let mut tot := (fins.append exprImports).erase default
   let redundant := env.findRedundantImports tot.toArray
   for n in redundant do
     tot := tot.erase n
-  let fileNames := tot.toArray.qsort Name.lt
-  let fileNames := if tk.isSome then (fileNames).filter (`Mathlib).isPrefixOf else fileNames
-  logInfoAt (← getRef) m!"{"\n".intercalate (fileNames.map (s!"import {·}")).toList}"
+  return tot
+
+def minImpsCore (stx : Syntax) : CommandElabM Unit := do
+    let tot ← getAllImports stx
+    let fileNames := tot.toArray.qsort Name.lt
+    --let fileNames := if tk.isSome then (fileNames).filter (`Mathlib).isPrefixOf else fileNames
+    logInfoAt (← getRef) m!"{"\n".intercalate (fileNames.map (s!"import {·}")).toList}"
+
+/-- `#min_imps cmd` scans the syntax `cmd` and the declaration obtained by elaborating `cmd`
+to find a collection of minimal imports that should be sufficient for `cmd` to work. -/
+syntax (name := minImpsStx) "#min_imps" command : command
+
+@[inherit_doc minImpsStx]
+syntax "#min_imps" term : command
+
+elab_rules : command
+  | `(#min_imps $cmd:command)=> do
+    Elab.Command.elabCommand cmd <|> pure ()
+    minImpsCore cmd
+  | `(#min_imps $cmd:term)=> minImpsCore cmd
 
 end Mathlib.Command.MinImps
