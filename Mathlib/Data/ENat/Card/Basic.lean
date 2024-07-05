4a04952b167e711dc180adf3dd0690cefba74743
Yury G. Kudryashov <urkud@urkud.name>
2024-07-05 12:34:14 -0500

    Update

--- Mathlib/Data/ENat/Card/Basic.lean
+++ Mathlib/Data/ENat/Card/Basic.lean
@@ -1,6 +1,12 @@
+/-
+Copyright (c) 2024 Yury Kudryashov. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Yury Kudryashov
+-/
 import Mathlib.Data.ENat.Card.Defs
 
-open Classical
+open Function
+open scoped Classical
 
 namespace ENat
 
