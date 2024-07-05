f8e4901fcb30dc2ea12ca3bb9c72e0bfaa7545ea
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2024-07-05 20:08:00 +0200

    update mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -3657,7 +3657,6 @@ import Mathlib.RingTheory.MvPolynomial.WeightedHomogeneous
 import Mathlib.RingTheory.MvPowerSeries.Basic
 import Mathlib.RingTheory.MvPowerSeries.Inverse
 import Mathlib.RingTheory.MvPowerSeries.NoZeroDivisors
-import Mathlib.RingTheory.MvPowerSeries.Order
 import Mathlib.RingTheory.MvPowerSeries.Trunc
 import Mathlib.RingTheory.Nakayama
 import Mathlib.RingTheory.Nilpotent.Basic
