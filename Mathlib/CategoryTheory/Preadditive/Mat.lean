1060fe4319f3b5729e4ba1445e0a9c3362973c6e
Matthew Ballard <matt@mrb.email>
2024-07-05 08:03:47 -0400

    fix

--- Mathlib/CategoryTheory/Preadditive/Mat.lean
+++ Mathlib/CategoryTheory/Preadditive/Mat.lean
@@ -341,7 +341,7 @@ def isoBiproductEmbedding (M : Mat_ C) : M ≅ ⨁ fun i => (embedding C).obj (M
     rw [Finset.sum_apply, Finset.sum_apply, Finset.sum_eq_single i]; rotate_left
     · intro b _ hb
       dsimp
-      simp only [Finset.sum_singleton]
+      erw [Finset.sum_singleton]
       rw [dif_neg hb.symm, zero_comp]
     · intro h
       simp at h
