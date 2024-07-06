4e54d4bf61b5f9dcdf5073a231f54a668ac481a9
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2024-07-06 01:48:32 +0100

    add comment about `@[simp, reassoc]`

--- Mathlib/Tactic/CategoryTheory/Reassoc.lean
+++ Mathlib/Tactic/CategoryTheory/Reassoc.lean
@@ -63,6 +63,8 @@ that are already right associated.
 
 Note that if you want both the lemma and the reassociated lemma to be
 `simp` lemmas, you should tag the lemma `@[reassoc (attr := simp)]`.
+The variants `@[simp, reassoc]` and `@[reassoc, simp]` on a lemma `F` will tag `F` with `@[simp]`,
+but not `F_apply` (this is sometimes useful).
 -/
 syntax (name := reassoc) "reassoc" (" (" &"attr" ":=" Parser.Term.attrInstance,* ")")? : attr
 
