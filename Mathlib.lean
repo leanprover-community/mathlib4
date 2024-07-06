c62bce90ab511244d3190e84aa8bc876b93fea82
Yaël Dillies <yael.dillies@gmail.com>
2024-07-06 08:39:01 +0000

    feat: Bergelson's Intersectivity Lemma (#11143)
    Prove a weak version of the Bergelson intersectivity lemma. The proof gives the strong version, but we need natural density to state it. This is a prerequisite to Tao and Ziegler's recent paper [Infinite partial sumsets in the primes](https://arxiv.org/abs/2301.10303).

--- Mathlib.lean
+++ Mathlib.lean
@@ -3037,6 +3037,7 @@ import Mathlib.MeasureTheory.Function.ConvergenceInMeasure
 import Mathlib.MeasureTheory.Function.Egorov
 import Mathlib.MeasureTheory.Function.EssSup
 import Mathlib.MeasureTheory.Function.Floor
+import Mathlib.MeasureTheory.Function.Intersectivity
 import Mathlib.MeasureTheory.Function.Jacobian
 import Mathlib.MeasureTheory.Function.L1Space
 import Mathlib.MeasureTheory.Function.L2Space
