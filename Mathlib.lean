5f5d19b0935e9093415c131b027cd4113c26e24a
Andrew Yang <the.erd.one@gmail.com>
2024-07-06 21:49:39 +0800

    update mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -3798,6 +3798,7 @@ import Mathlib.RingTheory.TwoSidedIdeal.Basic
 import Mathlib.RingTheory.UniqueFactorizationDomain
 import Mathlib.RingTheory.Unramified.Basic
 import Mathlib.RingTheory.Unramified.Derivations
+import Mathlib.RingTheory.Unramified.Finite
 import Mathlib.RingTheory.Valuation.Basic
 import Mathlib.RingTheory.Valuation.ExtendToLocalization
 import Mathlib.RingTheory.Valuation.Integers
