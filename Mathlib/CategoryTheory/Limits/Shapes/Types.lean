15fdf9210300fb9ef3e2f1bc4374a5fc54eb8cef
Joël Riou <joel.riou@universite-paris-saclay.fr>
2024-07-06 17:23:04 +0200

    better phrasing

--- Mathlib/CategoryTheory/Limits/Shapes/Types.lean
+++ Mathlib/CategoryTheory/Limits/Shapes/Types.lean
@@ -937,9 +937,8 @@ section Multiequalizer
 
 variable (I : MulticospanIndex (Type u))
 
-/-- Given a multiequalizer diagram `I : MulticospanIndex (Type u)` in the category
-of types, this is a type which identifies to the sections of the
-functor `I.multicospan.sections`. -/
+/-- Given `I : MulticospanIndex (Type u)`, this is a type which identifies
+to the sections of the functor `I.multicospan`. -/
 @[ext]
 structure MulticospanIndex.sections where
   /-- The data of an element in `I.left i` for each `i : I.L`. -/
