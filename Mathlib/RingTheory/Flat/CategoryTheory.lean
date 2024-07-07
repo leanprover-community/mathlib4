992021136a6cf90206892e44ff55a4f7ea87d018
Jujian Zhang <jujian.zhang1998@outlook.com>
2024-07-07 19:38:47 +0100

    half way

--- Mathlib/RingTheory/Flat/CategoryTheory.lean
+++ Mathlib/RingTheory/Flat/CategoryTheory.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2024 Johan Commelin. All rights reserved.
+Copyright (c) 2024 Jujian Zhang. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jujian Zhang
 -/
@@ -8,6 +8,7 @@ import Mathlib.Algebra.Category.ModuleCat.Monoidal.Symmetric
 import Mathlib.Algebra.Category.ModuleCat.Projective
 import Mathlib.Algebra.Category.ModuleCat.Abelian
 import Mathlib.CategoryTheory.Monoidal.Tor
+import Mathlib.Algebra.Homology.ShortComplex.Exact
 
 /-!
 # Tensoring with a flat module is an exact functor
@@ -38,6 +39,44 @@ namespace Module.Flat
 
 variable {R : Type u} [CommRing R] (M : ModuleCat.{u} R)
 
+lemma lTensor_shortComplex_exact [Flat R M] (C : ShortComplex $ ModuleCat R) (hC : C.Exact) :
+    C.map (tensorLeft M) |>.Exact := by
+  rw [ModuleCat.iff_shortComplex_exact, Eq.comm, ← LinearMap.exact_iff]
+  exact lTensor_exact M $ LinearMap.exact_iff.2 $ Eq.symm $
+    ModuleCat.iff_shortComplex_exact _ _ C.zero |>.1 hC
+
+lemma rTensor_shortComplex_exact [Flat R M] (C : ShortComplex $ ModuleCat R) (hC : C.Exact) :
+    C.map (tensorRight M) |>.Exact := by
+  rw [ModuleCat.iff_shortComplex_exact, Eq.comm, ← LinearMap.exact_iff]
+  exact rTensor_exact M $ LinearMap.exact_iff.2 $ Eq.symm $
+    ModuleCat.iff_shortComplex_exact _ _ C.zero |>.1 hC
+
+lemma iff_lTensor_preserves_shortComplex_exact :
+    Flat R M ↔
+    ∀ (C : ShortComplex $ ModuleCat R) (_ : C.Exact), (C.map (tensorLeft M) |>.Exact) :=
+  ⟨fun _ _=> lTensor_shortComplex_exact _ _, fun H => by
+    rw [iff_lTensor_exact]
+    intro N N' N'' _ _ _ _ _ _ f g h
+    specialize H (.mk (ModuleCat.ofHom f) (ModuleCat.ofHom g)
+      (DFunLike.ext _ _ h.apply_apply_eq_zero)) (ModuleCat.shortComplex_exact _ _ $ Eq.symm $
+        LinearMap.exact_iff |>.1 $ h)
+    rw [LinearMap.exact_iff, Eq.comm]
+    rw [ModuleCat.iff_shortComplex_exact] at H
+    convert H⟩
+
+lemma iff_rTensor_preserves_shortComplex_exact :
+    Flat R M ↔
+    ∀ (C : ShortComplex $ ModuleCat R) (_ : C.Exact), (C.map (tensorRight M) |>.Exact) :=
+  ⟨fun _ _=> rTensor_shortComplex_exact _ _, fun H => by
+    rw [iff_rTensor_exact]
+    intro N N' N'' _ _ _ _ _ _ f g h
+    specialize H (.mk (ModuleCat.ofHom f) (ModuleCat.ofHom g)
+      (DFunLike.ext _ _ h.apply_apply_eq_zero)) (ModuleCat.shortComplex_exact _ _ $ Eq.symm $
+        LinearMap.exact_iff |>.1 $ h)
+    rw [LinearMap.exact_iff, Eq.comm]
+    rw [ModuleCat.iff_shortComplex_exact] at H
+    convert H⟩
+
 open scoped MonoidalCategory in
 set_option maxHeartbeats 400000 in
 -- In two goals, we need to use `simpa` in one; and `simp` in the other.
@@ -53,10 +92,10 @@ noncomputable instance [flat : Flat R M] {X Y : ModuleCat.{u} R} (f : X ⟶ Y) :
 
           rw [hc.uniq c' g, hc.uniq c' h] <;>
           rintro (⟨j⟩|⟨j⟩) <;> simpa [ι] using H }
-    have exact0 : Exact ι f := by
-      refine Abelian.exact_of_is_kernel (w := by simp [ι])
-        (h := Limits.IsLimit.equivOfNatIsoOfIso (Iso.refl _) _ _
-          ⟨⟨?_, ?_⟩, ⟨?_, ?_⟩, ?_, ?_⟩ hc)
+    let s : ShortComplex (ModuleCat R) := .mk ι f $ by simp [ι]
+    have exact0 : s.Exact:= by
+      refine ShortComplex.exact_of_f_is_kernel _ $
+        Limits.IsLimit.equivOfNatIsoOfIso (Iso.refl _) _ _ ⟨⟨?_, ?_⟩, ⟨?_, ?_⟩, ?_, ?_⟩ hc
       · exact 𝟙 c.pt
       · rintro (⟨⟩|⟨⟩) <;> simp [ι]
       · exact 𝟙 c.pt
@@ -64,28 +103,49 @@ noncomputable instance [flat : Flat R M] {X Y : ModuleCat.{u} R} (f : X ⟶ Y) :
       · rfl
       · rfl
 
+    let s' := s.map (tensorLeft M)
     let f' := M ◁ f; let ι' := M ◁ ι
-    have exact1 : Exact ι' f' := by
-      rw [ModuleCat.exact_iff, Eq.comm, ← LinearMap.exact_iff] at exact0 ⊢
-      exact lTensor_exact M exact0
+    have exact1 : s'.Exact := by
+      apply lTensor_shortComplex_exact; assumption
+
     have mono1 : Mono ι' := by
+
       rw [ModuleCat.mono_iff_injective] at mono0 ⊢
       exact lTensor_preserves_injective_linearMap _ mono0
 
+    have := ShortComplex.exact_and_mono_f_iff_f_is_kernel s' |>.1 ⟨exact1, mono1⟩ |>.some
+    -- convert this
+    -- simp? [s']
     refine Limits.IsLimit.equivOfNatIsoOfIso
       ⟨⟨fun | .zero => 𝟙 _ | .one => 𝟙 _, ?_⟩,
         ⟨fun | .zero => 𝟙 _ | .one => 𝟙 _, ?_⟩, ?_, ?_⟩ _ _ ⟨⟨?_, ?_⟩, ⟨?_, ?_⟩, ?_, ?_⟩ $
-        Abelian.isLimitOfExactOfMono ι' f' exact1
-    · rintro _ _ (⟨⟩ | ⟨⟩ | ⟨_⟩) <;> simp
-    · rintro _ _ (⟨⟩ | ⟨⟩ | ⟨_⟩) <;> simp
-    · ext (⟨⟩|⟨⟩) <;> simp
-    · ext (⟨⟩|⟨⟩) <;> simp
+        this
+        -- Abelian.isLimitOfExactOfMono ι' f' exact1
+    · rintro _ _ (⟨⟩ | ⟨⟩ | ⟨_⟩) <;> simp [s']
+    · rintro _ _ (⟨⟩ | ⟨⟩ | ⟨_⟩) <;> simp [s']
+    · ext (⟨⟩|⟨⟩) <;> simp [s']
+    · ext (⟨⟩|⟨⟩) <;> simp [s']
     · exact 𝟙 _
-    · rintro (⟨⟩ | ⟨⟩) <;> simpa [ι', ι, f', Eq.comm] using exact1.w
-    · exact 𝟙 _
-    · rintro (⟨⟩ | ⟨⟩) <;> simpa [ι', ι, f', Eq.comm] using exact1.w
-    · ext (⟨⟩ | ⟨⟩); simp [ι', ι, f']
-    · ext (⟨⟩ | ⟨⟩); simp [ι', ι, f']
+    · rintro (⟨⟩ | ⟨⟩) <;>
+
+      simp only [ShortComplex.map_X₂, tensorLeft_obj, ShortComplex.map_X₃, ShortComplex.map_g,
+        tensorLeft_map, Functor.comp_obj, Limits.parallelPair_obj_zero, Limits.parallelPair_obj_one,
+        Functor.comp_map, Limits.walkingParallelPairHom_id, NatTrans.comp_app, NatTrans.id_app,
+        ModuleCat.coe_comp, Function.comp_apply, ModuleCat.id_apply, ShortComplex.map_X₁,
+        ShortComplex.map_f, Limits.Fork.app_zero_eq_ι, Limits.Cones.postcompose_obj_pt,
+        Limits.Fork.ofι_pt, Functor.mapCone_pt, Functor.mapCone_π_app, Category.id_comp,
+        Limits.Cones.postcompose_obj_π, Functor.const_obj_obj, Limits.Fork.ofι_π_app,
+        Category.comp_id, s', ι]
+      rw [← MonoidalCategory.whiskerLeft_comp]
+      congr
+      simp
+  #exit
+       -- [c]
+      -- <;> simpa [ι', ι, f', Eq.comm] using exact1.w
+    -- · exact 𝟙 _
+    -- · rintro (⟨⟩ | ⟨⟩) <;> simpa [ι', ι, f', Eq.comm] using exact1.w
+    -- · ext (⟨⟩ | ⟨⟩); simp [ι', ι, f']
+    -- · ext (⟨⟩ | ⟨⟩); simp [ι', ι, f']
 
 noncomputable instance tensorLeft_preservesFiniteLimits [Flat R M] :
     Limits.PreservesFiniteLimits (tensorLeft M) :=
