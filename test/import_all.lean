6157a7c8fe9c94858ebde0ac890c330ca5ded8e1
Kim Morrison <kim@tqft.net>
2024-07-09 00:00:32 +1000

    chore: add test t file to import everything

---
+++ test/import_all.lean
@@ -0,0 +1,13 @@
+import Lean
+import Std
+import Qq
+import Batteries
+import Aesop
+import ProofWidgets
+import Cli
+import ImportGraph
+import Mathlib
+
+/-!
+Verify that Mathlib and all its upstream dependencies can be simultaneously imported.
+-/
