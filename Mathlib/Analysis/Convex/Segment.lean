leanprover-community/mathlib4

commit ed825b9d394a21bbadbf8bdcc25d6539b1718380

chore: tidy various files (#14516)

Mathlib/Analysis/Convex/Segment.lean
@@ -560,16 +560,14 @@ theorem segment_eq_uIcc (x y : 𝕜) : [x -[𝕜] y] = uIcc x y :=
 /-- A point is in an `Icc` iff it can be expressed as a convex combination of the endpoints. -/
 theorem Convex.mem_Icc (h : x ≤ y) :
     z ∈ Icc x y ↔ ∃ a b, 0 ≤ a ∧ 0 ≤ b ∧ a + b = 1 ∧ a * x + b * y = z := by
-  rw [← segment_eq_Icc h]
-  rfl
+  simp [← segment_eq_Icc h, segment]
 #align convex.mem_Icc Convex.mem_Icc
 
 /-- A point is in an `Ioo` iff it can be expressed as a strict convex combination of the endpoints.
 -/
 theorem Convex.mem_Ioo (h : x < y) :
     z ∈ Ioo x y ↔ ∃ a b, 0 < a ∧ 0 < b ∧ a + b = 1 ∧ a * x + b * y = z := by
-  rw [← openSegment_eq_Ioo h]
-  rfl
+  simp [← openSegment_eq_Ioo h, openSegment]
 #align convex.mem_Ioo Convex.mem_Ioo
 
 /-- A point is in an `Ioc` iff it can be expressed as a semistrict convex combination of the

Mathlib/Algebra/Order/Group/PosPart.lean
@@ -267,10 +267,10 @@ section LinearOrder
 variable [LinearOrder α] [Group α] [CovariantClass α α (· * ·) (· ≤ ·)] {a : α}
 
 @[to_additive] lemma oneLePart_eq_ite : a⁺ᵐ = if 1 ≤ a then a else 1 := by
-  rw [← maxDefault, ← sup_eq_maxDefault]; simp_rw [sup_comm]; rfl
+  rw [oneLePart, ← maxDefault, ← sup_eq_maxDefault]; simp_rw [sup_comm]
 
 @[to_additive] lemma leOnePart_eq_ite : a⁻ᵐ = if a ≤ 1 then a⁻¹ else 1 := by
-  simp_rw [← one_le_inv']; rw [← maxDefault, ← sup_eq_maxDefault]; simp_rw [sup_comm]; rfl
+  simp_rw [← one_le_inv']; rw [leOnePart, ← maxDefault, ← sup_eq_maxDefault]; simp_rw [sup_comm]
 
 @[to_additive (attr := simp) posPart_pos_iff] lemma one_lt_oneLePart_iff : 1 < a⁺ᵐ ↔ 1 < a :=
   lt_iff_lt_of_le_iff_le $ (one_le_oneLePart _).le_iff_eq.trans oneLePart_eq_one

Mathlib/Analysis/Convex/SpecificFunctions/Basic.lean
@@ -26,11 +26,11 @@ path to Hölder's and Minkowski's inequalities and after that to Lp spaces and m
 theory.
 
 (Strict) concavity of `fun x ↦ x ^ p` for `0 < p < 1` (`0 ≤ p ≤ 1`) can be found in
-`Analysis.Convex.SpecificFunctions.Pow`.
+`Mathlib.Analysis.Convex.SpecificFunctions.Pow`.
 
 ## See also
 
-`Analysis.Convex.Mul` for convexity of `x ↦ x ^ n`
+`Mathlib.Analysis.Convex.Mul` for convexity of `x ↦ x ^ n`
 -/
 
 open Real Set NNReal

Mathlib/Analysis/SpecialFunctions/Gaussian/FourierTransform.lean
@@ -316,10 +316,9 @@ theorem integral_cexp_neg_sum_mul_add {ι : Type*} [Fintype ι] {b : ι → ℂ}
 theorem integral_cexp_neg_mul_sum_add {ι : Type*} [Fintype ι] (hb : 0 < b.re) (c : ι → ℂ) :
     ∫ v : ι → ℝ, cexp (- b * ∑ i, (v i : ℂ) ^ 2 + ∑ i, c i * v i)
       = (π / b) ^ (Fintype.card ι / 2 : ℂ) * cexp ((∑ i, c i ^ 2) / (4 * b)) := by
-  simp_rw [neg_mul, Finset.mul_sum, integral_cexp_neg_sum_mul_add (fun _ ↦ hb) c]
-  simp only [one_div, Finset.prod_mul_distrib, Finset.prod_const, ← cpow_nat_mul, ← Complex.exp_sum,
-    Fintype.card, Finset.sum_div]
-  rfl
+  simp_rw [neg_mul, Finset.mul_sum, integral_cexp_neg_sum_mul_add (fun _ ↦ hb) c, one_div,
+    Finset.prod_mul_distrib, Finset.prod_const, ← cpow_nat_mul, ← Complex.exp_sum, Fintype.card,
+    Finset.sum_div, div_eq_mul_inv]
 
 theorem integral_cexp_neg_mul_sq_norm_add_of_euclideanSpace
     {ι : Type*} [Fintype ι] (hb : 0 < b.re) (c : ℂ) (w : EuclideanSpace ℝ ι) :

Mathlib/CategoryTheory/Adjunction/Reflective.lean
@@ -15,7 +15,7 @@ import Mathlib.CategoryTheory.Functor.ReflectsIso
 Basic properties of reflective functors, especially those relating to their essential image.
 
 Note properties of reflective functors relating to limits and colimits are included in
-`CategoryTheory.Monad.Limits`.
+`Mathlib.CategoryTheory.Monad.Limits`.
 -/
 
 

Mathlib/CategoryTheory/Monad/Adjunction.lean
@@ -92,7 +92,7 @@ def adjToComonadIso (G : Comonad C) : G.adj.toComonad ≅ G :=
 Given an adjunction `L ⊣ R`, if `L ⋙ R` is abstractly isomorphic to the identity functor, then the
 unit is an isomorphism.
 -/
-def unitAsIsoOfIso (adj : L ⊣ R) (i : L ⋙ R ≅ 𝟭 C) :  𝟭 C ≅ L ⋙ R where
+def unitAsIsoOfIso (adj : L ⊣ R) (i : L ⋙ R ≅ 𝟭 C) : 𝟭 C ≅ L ⋙ R where
   hom := adj.unit
   inv :=  i.hom ≫ (adj.toMonad.transport i).μ
   hom_inv_id := by

Mathlib/Control/Basic.lean
@@ -251,7 +251,7 @@ theorem CommApplicative.commutative_map {m : Type u → Type v} [h : Applicative
   f <$> a <*> b = flip f <$> b <*> a :=
   calc
     f <$> a <*> b = (fun p : α × β => f p.1 p.2) <$> (Prod.mk <$> a <*> b) := by
-      simp only [map_seq, map_map]; rfl
+      simp only [map_seq, map_map, Function.comp_def]
     _ = (fun b a => f a b) <$> b <*> a := by
       rw [@CommApplicative.commutative_prod m h]
       simp [seq_map_assoc, map_seq, seq_assoc, seq_pure, map_map, (· ∘ ·)]

Mathlib/Control/Bitraversable/Instances.lean
@@ -103,7 +103,7 @@ instance (priority := 10) Bitraversable.isLawfulTraversable [LawfulBitraversable
     LawfulTraversable (t α) := by
   constructor <;> intros <;>
     simp [traverse, comp_tsnd, functor_norm]
-  · simp only [tsnd_eq_snd_id, Id.pure_eq]; rfl
+  · simp [tsnd_eq_snd_id, (· <$> ·), id.mk]
   · simp [tsnd, binaturality, Function.comp, functor_norm]
 #align bitraversable.is_lawful_traversable Bitraversable.isLawfulTraversable
 

commit a78833135be0437d8cedbcdbb895823f8b54d5d4

perf(Analysis/Convex/Segment): squeeze two slow simps (#14537)
These simp calls take 1 and 0.6s, respectively: squeeze them instead. This regressed in #14516.

Mathlib/Analysis/Convex/Segment.lean
@@ -31,7 +31,6 @@ Should we rename `segment` and `openSegment` to `convex.Icc` and `convex.Ioo`? S
 define `clopenSegment`/`convex.Ico`/`convex.Ioc`?
 -/
 
-
 variable {𝕜 E F G ι : Type*} {π : ι → Type*}
 
 open Function Set
@@ -560,14 +559,14 @@ theorem segment_eq_uIcc (x y : 𝕜) : [x -[𝕜] y] = uIcc x y :=
 /-- A point is in an `Icc` iff it can be expressed as a convex combination of the endpoints. -/
 theorem Convex.mem_Icc (h : x ≤ y) :
     z ∈ Icc x y ↔ ∃ a b, 0 ≤ a ∧ 0 ≤ b ∧ a + b = 1 ∧ a * x + b * y = z := by
-  simp [← segment_eq_Icc h, segment]
+  simp only [← segment_eq_Icc h, segment, mem_setOf_eq, smul_eq_mul, exists_and_left]
 #align convex.mem_Icc Convex.mem_Icc
 
 /-- A point is in an `Ioo` iff it can be expressed as a strict convex combination of the endpoints.
 -/
 theorem Convex.mem_Ioo (h : x < y) :
     z ∈ Ioo x y ↔ ∃ a b, 0 < a ∧ 0 < b ∧ a + b = 1 ∧ a * x + b * y = z := by
-  simp [← openSegment_eq_Ioo h, openSegment]
+  simp only [← openSegment_eq_Ioo h, openSegment, smul_eq_mul, exists_and_left, mem_setOf_eq]
 #align convex.mem_Ioo Convex.mem_Ioo
 
 /-- A point is in an `Ioc` iff it can be expressed as a semistrict convex combination of the