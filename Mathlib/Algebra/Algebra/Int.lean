7a84033e96032d28184ed341c9a7006493c59402
Matthew Ballard <matt@mrb.email>
2024-07-08 08:47:15 -0400

    split

---
+++ Mathlib/Algebra/Algebra/Int.lean
@@ -0,0 +1,56 @@
+/-
+Copyright (c) 2020 Yury G. Kudryashov. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Yury G. Kudryashov, Oliver Nash
+-/
+
+import Mathlib.Algebra.CharZero.Lemmas
+import Mathlib.Algebra.Algebra.Basic
+import Mathlib.Data.Int.CharZero
+import Mathlib.Data.Rat.Cast.CharZero
+
+/-!
+# Algebra instances for actions by ℚ and theorems about them
+
+-/
+
+variable (R : Type*) [Ring R]
+
+-- Lower the priority so that `Algebra.id` is picked most of the time when working with
+-- `ℤ`-algebras. This is only an issue since `Algebra.id ℤ` and `algebraInt ℤ` are not yet defeq.
+-- TODO: fix this by adding an `ofInt` field to rings.
+/-- Ring ⥤ ℤ-Alg -/
+instance (priority := 99) algebraInt : Algebra ℤ R where
+  commutes' := Int.cast_commute
+  smul_def' _ _ := zsmul_eq_mul _ _
+  toRingHom := Int.castRingHom R
+#align algebra_int algebraInt
+
+theorem intCast_smul {k V : Type*} [CommRing k] [AddCommGroup V] [Module k V] (r : ℤ) (x : V) :
+    (r : k) • x = r • x :=
+  algebraMap_smul k r x
+#align int_cast_smul intCast_smul
+
+/-- A special case of `eq_intCast'` that happens to be true definitionally -/
+@[simp]
+theorem algebraMap_int_eq : algebraMap ℤ R = Int.castRingHom R :=
+  rfl
+#align algebra_map_int_eq algebraMap_int_eq
+
+variable {R}
+
+instance int_algebra_subsingleton : Subsingleton (Algebra ℤ R) :=
+  ⟨fun P Q => Algebra.algebra_ext P Q <| RingHom.congr_fun <| Subsingleton.elim _ _⟩
+#align int_algebra_subsingleton int_algebra_subsingleton
+
+namespace NoZeroSMulDivisors
+
+variable {R : Type*}
+
+-- see note [lower instance priority]
+instance (priority := 100) CharZero.noZeroSMulDivisors_int [Ring R] [NoZeroDivisors R]
+    [CharZero R] : NoZeroSMulDivisors ℤ R :=
+  NoZeroSMulDivisors.of_algebraMap_injective <| (algebraMap ℤ R).injective_int
+#align no_zero_smul_divisors.char_zero.no_zero_smul_divisors_int NoZeroSMulDivisors.CharZero.noZeroSMulDivisors_int
+
+
---
+++ Mathlib/Algebra/Algebra/Nat.lean
@@ -0,0 +1,46 @@
+/-
+Copyright (c) 2020 Yury G. Kudryashov. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Yury G. Kudryashov, Oliver Nash
+-/
+
+import Mathlib.Algebra.CharZero.Lemmas
+import Mathlib.Algebra.Algebra.Basic
+import Mathlib.Data.Int.CharZero
+import Mathlib.Data.Rat.Cast.CharZero
+
+/-!
+# Algebra instances for actions by ℕ and theorems specifically about them
+
+-/
+
+section Nat
+
+variable {R : Type*} [Semiring R]
+
+-- Lower the priority so that `Algebra.id` is picked most of the time when working with
+-- `ℕ`-algebras. This is only an issue since `Algebra.id` and `algebraNat` are not yet defeq.
+-- TODO: fix this by adding an `ofNat` field to semirings.
+/-- Semiring ⥤ ℕ-Alg -/
+instance (priority := 99) algebraNat : Algebra ℕ R where
+  commutes' := Nat.cast_commute
+  smul_def' _ _ := nsmul_eq_mul _ _
+  toRingHom := Nat.castRingHom R
+#align algebra_nat algebraNat
+
+instance nat_algebra_subsingleton : Subsingleton (Algebra ℕ R) :=
+  ⟨fun P Q => by ext; simp⟩
+#align nat_algebra_subsingleton nat_algebra_subsingleton
+
+end Nat
+
+namespace NoZeroSMulDivisors
+
+variable {R : Type*}
+
+-- see note [lower instance priority]
+instance (priority := 100) CharZero.noZeroSMulDivisors_nat [Semiring R] [NoZeroDivisors R]
+    [CharZero R] : NoZeroSMulDivisors ℕ R :=
+  NoZeroSMulDivisors.of_algebraMap_injective <| (algebraMap ℕ R).injective_nat
+#align no_zero_smul_divisors.char_zero.no_zero_smul_divisors_nat NoZeroSMulDivisors.CharZero.noZeroSMulDivisors_nat
+
