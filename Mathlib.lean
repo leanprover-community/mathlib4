e6a07243e45db82a27001960bef3e5fe8b8177c0
Andrew Yang <the.erd.one@gmail.com>
2024-07-08 18:29:40 +0800

    fix mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -3791,6 +3791,7 @@ import Mathlib.RingTheory.RootsOfUnity.Lemmas
 import Mathlib.RingTheory.RootsOfUnity.Minpoly
 import Mathlib.RingTheory.SimpleModule
 import Mathlib.RingTheory.Smooth.Basic
+import Mathlib.RingTheory.Support
 import Mathlib.RingTheory.TensorProduct.Basic
 import Mathlib.RingTheory.TensorProduct.MvPolynomial
 import Mathlib.RingTheory.Trace
