05cf3d6a19a13e95ca3855ef18bd7f047a583bb2
Colin Jones <137637335+Colin166@users.noreply.github.com>
2024-07-08 10:07:23 -0400

    Update FactorisationProperties.lean

--- Mathlib/NumberTheory/FactorisationProperties.lean
+++ Mathlib/NumberTheory/FactorisationProperties.lean
@@ -47,7 +47,7 @@ open BigOperators Finset
 
 namespace Nat
 
-variable {n : ℕ}
+variable {n m p : ℕ}
 
 /-- `n : ℕ` is _abundant_ if the sum of the proper divisors of `n` is greater than `n`. -/
 def Abundant (n : ℕ) : Prop := ∑ i in properDivisors n, i > n
