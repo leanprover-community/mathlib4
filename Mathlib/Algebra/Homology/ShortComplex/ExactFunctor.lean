1003e285d5c047d3b3f5b498e826c3c83d1dacd4
Jujian Zhang <jujian.zhang1998@outlook.com>
2024-07-08 22:04:01 +0100

    add author

--- Mathlib/Algebra/Homology/ShortComplex/ExactFunctor.lean
+++ Mathlib/Algebra/Homology/ShortComplex/ExactFunctor.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2023 Joël Riou. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Joël Riou
+Authors: Joël Riou, Jujian Zhang
 -/
 import Mathlib.Algebra.Homology.ShortComplex.PreservesHomology
 import Mathlib.Algebra.Homology.ShortComplex.ShortExact
