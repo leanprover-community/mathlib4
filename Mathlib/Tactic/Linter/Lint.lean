3a9ee9ffd405aa955c4e1fb832192dd07c160652
Kim Morrison <kim@tqft.net>
2024-07-08 20:28:30 +0000

    chore: adaptations for nightly-2024-07-07 (#14532)

--- Mathlib/Tactic/Linter/Lint.lean
+++ Mathlib/Tactic/Linter/Lint.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Floris van Doorn
 -/
 import Lean.Linter.Util
-import Batteries.Data.Array.Basic
 import Batteries.Tactic.Lint
 
 /-!
