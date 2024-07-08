40b54c1610c3b79fb6590f70f6ad110dc7128b1e
Kim Morrison <kim@tqft.net>
2024-07-09 06:59:47 +1000

    don't import Cli

--- test/import_all.lean
+++ test/import_all.lean
@@ -4,10 +4,12 @@ import Qq
 import Batteries
 import Aesop
 import ProofWidgets
-import Cli
 import ImportGraph
 import Mathlib
 
 /-!
 Verify that Mathlib and all its upstream dependencies can be simultaneously imported.
+
+We don't `import Cli` here as that is only used by scripts,
+and it need to have been built during CI when we run the tests.
 -/
