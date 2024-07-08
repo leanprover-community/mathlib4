71daabc5131a40a103602d386317ebae1c2442e7
Jujian Zhang <jujian.zhang1998@outlook.com>
2024-07-08 10:40:40 +0100

    fix

--- Mathlib/RingTheory/TwoSidedIdeal/IsSimpleRing.lean
+++ Mathlib/RingTheory/TwoSidedIdeal/IsSimpleRing.lean
@@ -26,7 +26,7 @@ variable (R : Type*) [NonUnitalNonAssocRing R]
 /--
 A ring `R` is **simple** if it has only two two-sided ideals, namely `0` and `⟨1⟩`.
 -/
-class IsSimpleRing where
+class IsSimpleRing : Prop where
   simple : IsSimpleOrder (TwoSidedIdeal R)
 
 namespace IsSimpleRing
@@ -34,24 +34,33 @@ namespace IsSimpleRing
 variable {R}
 
 instance nontrivial [simple : IsSimpleRing R] : Nontrivial R := by
-  refine subsingleton_or_nontrivial R |>.resolve_left fun r => ?_
+  refine subsingleton_or_nontrivial R |>.resolve_left fun r ↦ ?_
   obtain ⟨x, y, hxy⟩ := simple.1.1
-  exact hxy $ SetLike.ext fun a => (show a = 0 from Subsingleton.elim _ _) ▸ by
+  exact hxy $ SetLike.ext fun a ↦ (show a = 0 from Subsingleton.elim _ _) ▸ by
     simp [TwoSidedIdeal.zero_mem]
 
 lemma twoSidedIdeal_elim [simple : IsSimpleRing R] (I : TwoSidedIdeal R) : I = ⊥ ∨ I = ⊤ :=
     simple.1.2 I
 
+lemma one_mem_of_ne_bot {A : Type*} [NonAssocRing A] [IsSimpleRing A] (I : TwoSidedIdeal A)
+    (hI : I ≠ ⊥) : (1 : A) ∈ I :=
+  (twoSidedIdeal_elim I).resolve_left hI ▸ ⟨⟩
+
+lemma one_mem_of_ne_zero_mem {A : Type*} [NonAssocRing A] [IsSimpleRing A] (I : TwoSidedIdeal A)
+    {x : A} (hx : x ≠ 0) (hxI : x ∈ I) : (1 : A) ∈ I :=
+  one_mem_of_ne_bot I (by rintro rfl; exact hx hxI)
+
 instance of_divisionRing (A : Type*) [DivisionRing A] : IsSimpleRing A where
   simple :=
   { exists_pair_ne := ⟨⊥, ⊤, bot_ne_top⟩
-    eq_bot_or_eq_top := fun I => (eq_or_ne I ⊥).elim .inl fun H => .inr $ by
-      suffices mem : 1 ∈ I from eq_top_iff.2 $ fun x _ => mul_one x ▸ I.mul_mem_left x _ mem
+    eq_bot_or_eq_top := fun I ↦ (eq_or_ne I ⊥).elim .inl fun H ↦ .inr $ by
+      suffices mem : 1 ∈ I from eq_top_iff.2 $ fun x _ ↦ mul_one x ▸ I.mul_mem_left x _ mem
       rw [← bot_lt_iff_ne_bot, TwoSidedIdeal.lt_iff, Set.ssubset_def,
         Set.not_subset_iff_exists_mem_not_mem] at H
       obtain ⟨-, x, hx1, (hx2 : x ≠ 0)⟩ := H
       simpa [inv_mul_cancel hx2] using I.mul_mem_left x⁻¹ _ hx1 }
 
+open TwoSidedIdeal in
 lemma center_isField (A : Type*) [Ring A] [IsSimpleRing A] : IsField (Subring.center A) where
   exists_pair_ne := ⟨0, 1, zero_ne_one⟩
   mul_comm := by
@@ -68,12 +77,10 @@ lemma center_isField (A : Type*) [Ring A] [IsSimpleRing A] : IsField (Subring.ce
       (by rintro _ ⟨x, rfl⟩; exact ⟨-x, by simp⟩)
       (by rintro a _ ⟨c, rfl⟩; exact ⟨a * c, by dsimp; rw [← mul_assoc, ← hx1, mul_assoc]⟩)
       (by rintro _ b ⟨a, rfl⟩; exact ⟨a * b, by dsimp; rw [← mul_assoc, ← hx1, mul_assoc]⟩)
-    have hI : I ≠ ⊥ := fun H ↦ hx2 (H ▸ by simpa [I, TwoSidedIdeal.mem_mk'] using ⟨1, by simp⟩ :
-        x ∈ (⊥ : TwoSidedIdeal A))
-    have mem : 1 ∈ I := (twoSidedIdeal_elim I).resolve_left hI ▸ ⟨⟩
+    have mem : 1 ∈ I := one_mem_of_ne_zero_mem I hx2 (by simpa [I, mem_mk'] using ⟨1, by simp⟩)
     simp only [TwoSidedIdeal.mem_mk', Set.mem_range, I] at mem
     obtain ⟨y, hy⟩ := mem
-    refine ⟨⟨y, Subring.mem_center_iff.2 fun a => ?_⟩, by ext; exact hy⟩
+    refine ⟨⟨y, Subring.mem_center_iff.2 fun a ↦ ?_⟩, by ext; exact hy⟩
     calc a * y
       _ = (x * y) * a * y := by rw [hy, one_mul]
       _ = (y * x) * a * y := by rw [hx1]
@@ -82,5 +89,15 @@ lemma center_isField (A : Type*) [Ring A] [IsSimpleRing A] : IsField (Subring.ce
       _ = y * ((a * x) * y) := by rw [mul_assoc]
       _ = y * (a * (x * y)) := by rw [mul_assoc a x y]
       _ = y * a := by rw [hy, mul_one]
+
+lemma iff_isField_of_commutative (A : Type*) [CommRing A]  :
+    IsField A ↔ IsSimpleRing A :=
+  ⟨fun h ↦ letI := IsField.toField h; inferInstance, fun _ ↦ by
+    have h := center_isField A
+    refine ⟨⟨0, 1, zero_ne_one⟩, mul_comm, fun {a} ha ↦ ?_⟩
+    obtain ⟨⟨b, _⟩, hb⟩ := h.mul_inv_cancel
+      (a := ⟨a, Subring.mem_center_iff.2 fun b ↦ mul_comm _ _⟩)
+      (by contrapose! ha; simpa [Subtype.ext_iff] using ha)
+    exact ⟨b, Subtype.ext_iff |>.1 hb⟩⟩
 
 end IsSimpleRing
