635e96decf084abec6f511b387096cfc19f45308
Michael Rothgang <rothgami@math.hu-berlin.de>
2024-07-09 16:25:56 +0200

    try new version

--- Mathlib/Tactic/Linter/TextBased.lean
+++ Mathlib/Tactic/Linter/TextBased.lean
@@ -108,7 +108,7 @@ structure ErrorContext where
   lineNumber : ℕ
   /-- The path to the file which was linted -/
   path : FilePath
-  deriving BEq
+--  deriving BEq
 
 /-- The parts of a `StyleError` which are considered when matching against the existing
   style exceptions: for example, we ignore the particular line length of a "line too long" error. -/
@@ -433,10 +433,7 @@ def lintModules (moduleNames : Array String) (mode : OutputSetting) : IO UInt32 
       -- Is the current error covered by some existing style exception?
       -- If so, prefer the existing exception; otherwise, we take the current one.
       (styleExceptions.find? fun e ↦ e.isSimilar err).getD err
-    if newExceptions != newExceptions2 then
-      IO.println "something went wrong!"
-
-    let this_output := "\n".intercalate (newExceptions.map
+    let this_output := "\n".intercalate (newExceptions2.map
         (fun err ↦ outputMessage err ErrorFormat.exceptionsFile)).toList
     IO.FS.writeFile exceptionsFilePath s!"{python_output}{this_output}\n"
   return numberErrorFiles
