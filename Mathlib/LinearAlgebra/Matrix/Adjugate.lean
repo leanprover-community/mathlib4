5fcb7ac2a3f1aaad3b8b794d12477dfd62a01721
Michael Rothgang <rothgami@math.hu-berlin.de>
2024-07-09 12:38:54 +0200

    One more.

--- Mathlib/LinearAlgebra/Matrix/Adjugate.lean
+++ Mathlib/LinearAlgebra/Matrix/Adjugate.lean
@@ -421,10 +421,10 @@ theorem adjugate_fin_three (A : Matrix (Fin 3) (Fin 3) α) :
     adjugate A =
     !![A 1 1 * A 2 2 - A 1 2 * A 2 1,
       -(A 0 1 * A 2 2) + A 0 2 * A 2 1,
-      A 0 1 * A 1 2 - A 0 2 * A 1 1
+      A 0 1 * A 1 2 - A 0 2 * A 1 1;
       -(A 1 0 * A 2 2) + A 1 2 * A 2 0,
       A 0 0 * A 2 2 - A 0 2 * A 2 0,
-      -(A 0 0 * A 1 2) + A 0 2 * A 1 0
+      -(A 0 0 * A 1 2) + A 0 2 * A 1 0;
       A 1 0 * A 2 1 - A 1 1 * A 2 0,
       -(A 0 0 * A 2 1) + A 0 1 * A 2 0,
       A 0 0 * A 1 1 - A 0 1 * A 1 0] := by
