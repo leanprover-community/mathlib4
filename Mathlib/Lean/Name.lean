6d9d3c3b98c5d1196de83b26ed5012eab9230b6d
Kim Morrison <kim@tqft.net>
2024-07-10 02:33:52 +1000

    remove import

--- Mathlib/Lean/Name.lean
+++ Mathlib/Lean/Name.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison
 -/
 import Std.Data.HashMap
-import Batteries.Lean.SMap
 import Lean.Meta.Match.MatcherInfo
 
 /-!
