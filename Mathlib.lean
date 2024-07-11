a2a619c1786daec303b8c3460d464751dc6f4c45
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2024-07-11 21:36:18 +0200

    chore: split Mathlib.LinearAlgebra.FiniteDimensional

--- Mathlib.lean
+++ Mathlib.lean
@@ -2824,6 +2824,7 @@ import Mathlib.LinearAlgebra.ExteriorAlgebra.Basic
 import Mathlib.LinearAlgebra.ExteriorAlgebra.Grading
 import Mathlib.LinearAlgebra.ExteriorAlgebra.OfAlternating
 import Mathlib.LinearAlgebra.FiniteDimensional
+import Mathlib.LinearAlgebra.FiniteDimensional.Defs
 import Mathlib.LinearAlgebra.FiniteSpan
 import Mathlib.LinearAlgebra.Finsupp
 import Mathlib.LinearAlgebra.FinsuppVectorSpace
