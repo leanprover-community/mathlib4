37031938d34c376cca2456d748c3a0f55b2dde4a
Calle Sönne <calle.sonne@gmail.com>
2024-07-11 15:48:21 +0000

    feat(Bicategory/Functor/Lax): define lax functors (#14303)
    We define lax functors and provide ways to construct pseudofunctors from a given a lax functor.

--- Mathlib.lean
+++ Mathlib.lean
@@ -1294,6 +1294,7 @@ import Mathlib.CategoryTheory.Bicategory.Coherence
 import Mathlib.CategoryTheory.Bicategory.End
 import Mathlib.CategoryTheory.Bicategory.Extension
 import Mathlib.CategoryTheory.Bicategory.Free
+import Mathlib.CategoryTheory.Bicategory.Functor.Lax
 import Mathlib.CategoryTheory.Bicategory.Functor.Oplax
 import Mathlib.CategoryTheory.Bicategory.Functor.Prelax
 import Mathlib.CategoryTheory.Bicategory.Functor.Pseudofunctor
