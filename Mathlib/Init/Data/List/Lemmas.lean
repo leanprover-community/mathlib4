759a3241dd0cfca1d67e1d66147a1bc36c19f74c
Kim Morrison <kim@tqft.net>
2024-07-11 12:18:07 +1000

    noshake

--- Mathlib/Init/Data/List/Lemmas.lean
+++ Mathlib/Init/Data/List/Lemmas.lean
@@ -3,6 +3,7 @@ Copyright (c) 2014 Parikshit Khanna. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Parikshit Khanna, Jeremy Avigad, Leonardo de Moura, Floris van Doorn
 -/
+import Batteries.Data.List.Lemmas
 import Mathlib.Mathport.Rename
 import Mathlib.Tactic.Cases
 
