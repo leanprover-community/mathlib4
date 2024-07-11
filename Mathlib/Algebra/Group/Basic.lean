08872e37d6f5279b4afec52ef2234c46ede7ec74
Johan Commelin <johan@commelin.net>
2024-07-11 09:42:43 +0200

    wip

--- Mathlib/Algebra/Group/Basic.lean
+++ Mathlib/Algebra/Group/Basic.lean
@@ -217,17 +217,17 @@ set_option linter.deprecated false
 
 variable {M : Type u} [AddCommSemigroup M]
 
-/- theorem bit0_add (a b : M) : bit0 (a + b) = bit0 a + bit0 b := -/
-/-   add_add_add_comm _ _ _ _ -/
-#noalign bit0_add
+theorem bit0_add (a b : M) : (a + b) + (a + b) = (a + a) + (b + b) :=
+  add_add_add_comm _ _ _ _
+#align bit0_add bit0_add
 
-/- theorem bit1_add [One M] (a b : M) : bit1 (a + b) = bit0 a + bit1 b := -/
-/-   (congr_arg (· + (1 : M)) <| bit0_add a b : _).trans (add_assoc _ _ _) -/
-#noalign bit1_add
+theorem bit1_add [One M] (a b : M) : ((a + b) + (a + b)) + 1 = (a + a) + ((b + b) + 1) :=
+  (congr_arg (· + (1 : M)) <| bit0_add a b : _).trans (add_assoc _ _ _)
+#align bit1_add bit1_add
 
-/- theorem bit1_add' [One M] (a b : M) : bit1 (a + b) = bit1 a + bit0 b := by -/
-/-   rw [add_comm, bit1_add, add_comm] -/
-#noalign bit1_add'
+theorem bit1_add' [One M] (a b : M) : ((a + b) + (a + b)) + 1 = ((a + a) + 1) + (b + b) := by
+  rw [add_comm a b, bit1_add, add_comm]
+#align bit1_add' bit1_add'
 
 end AddCommSemigroup
 
@@ -236,14 +236,14 @@ set_option linter.deprecated false
 
 variable {M : Type u} [AddMonoid M] {a b c : M}
 
-/- @[simp] -/
-/- theorem bit0_zero : bit0 (0 : M) = 0 := -/
-/-   add_zero _ -/
-#noalign bit0_zero
+@[simp]
+theorem bit0_zero : 2 • (0 : M) = 0 :=
+  nsmul_zero _
+#align bit0_zero bit0_zero
 
-/- @[simp] -/
-/- theorem bit1_zero [One M] : bit1 (0 : M) = 1 := by rw [bit1, bit0_zero, zero_add] -/
-#noalign bit1_zero
+@[simp]
+theorem bit1_zero [One M] : (2 • (0 : M) + 1) = 1 := by rw [bit0_zero, zero_add]
+#align bit1_zero bit1_zero
 
 end AddMonoid
 
@@ -711,8 +711,9 @@ section SubtractionMonoid
 
 set_option linter.deprecated false
 
-/- lemma bit0_neg [SubtractionMonoid α] (a : α) : bit0 (-a) = -bit0 a := (neg_add_rev _ _).symm -/
-#noalign bit0_neg
+lemma bit0_neg [SubtractionMonoid α] (a : α) : (2:ℕ) • (-a) = -(2 • a) := by
+  rw [two_nsmul, two_nsmul, (neg_add_rev _ _).symm]
+#align bit0_neg bit0_neg
 
 end SubtractionMonoid
 
