603ec5d48335770e0ee9486dbd0a64ae4a005ae4
emilyriehl <eriehl@jhu.edu>
2024-07-11 14:52:59 +0200

    work towards product preservation

--- Mathlib/InfinityCosmos.lean
+++ Mathlib/InfinityCosmos.lean
@@ -1,6 +1,7 @@
 import Mathlib.AlgebraicTopology.Nerve
 import Mathlib.CategoryTheory.Category.Quiv
 import Mathlib.CategoryTheory.Limits.Presheaf
+import Mathlib.CategoryTheory.Limits.Shapes.BinaryProducts
 import Mathlib.CategoryTheory.Monad.Limits
 
 namespace CategoryTheory
@@ -675,6 +676,42 @@ instance : Reflective nerveFunctor.{u} :=
 
 instance : HasColimits Cat :=
   hasColimits_of_reflective nerveFunctor
+
+section preservesProducts
+
+open Limits
+
+/-- ER: This should be an instance of a theorem in mathlib about evaluation in functor categories preserving limits that exist. Statement has a universe level error.-/
+-- def simplexBinaryProducts (X Y : SSet) (n : ℕ) : (X × Y) _[n] ≅ X _[n] × Y _[n] := by sorry
+
+def hoFunctorPreservesExplicitBinaryProduct {X Y : SSet.{u}}
+    (s : BinaryFan X Y) (hyp : IsLimit s) :
+    IsLimit (BinaryFan.mk (SSet.hoFunctor.map (BinaryFan.fst s)) (SSet.hoFunctor.map (BinaryFan.snd s))) := by
+  have := limitObjIsoLimitCompEvaluation (pair X Y) (op [0])
+  simp at this
+  refine BinaryFan.isLimitMk ?lift ?fac_left ?fac_right ?uniq
+  · sorry
+  · sorry
+  · sorry
+  · sorry
+
+
+
+def hoFunctorPreservesBinaryProduct {X Y : SSet.{u}} : PreservesLimit (pair X Y) SSet.hoFunctor where
+  preserves := by
+    intro c clim
+    sorry
+
+def hoFunctorPreservesBinaryProducts :
+    PreservesLimitsOfShape (Discrete WalkingPair) SSet.hoFunctor where
+      preservesLimit := by
+        rintro K
+        have := diagramIsoPair K
+        sorry
+
+
+end preservesProducts
+
 -- -- nerve E c = (F c → E)
 -- def Functor.nerve : E ⥤ Cᵒᵖ ⥤ Type v :=
 --   .flip <| curryObj (F.homRestriction (Functor.id E))
