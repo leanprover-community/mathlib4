2e7919e0312cc3e18356bb6303960a545e87db37
Joël Riou <joel.riou@universite-paris-saclay.fr>
2024-07-11 15:33:36 +0200

    feat(Algebra/Homology): boundaries of embeddings of complex shapes

--- Mathlib.lean
+++ Mathlib.lean
@@ -315,6 +315,7 @@ import Mathlib.Algebra.Homology.DerivedCategory.ShortExact
 import Mathlib.Algebra.Homology.DerivedCategory.SingleTriangle
 import Mathlib.Algebra.Homology.DifferentialObject
 import Mathlib.Algebra.Homology.Embedding.Basic
+import Mathlib.Algebra.Homology.Embedding.Boundary
 import Mathlib.Algebra.Homology.Embedding.Extend
 import Mathlib.Algebra.Homology.Embedding.Restriction
 import Mathlib.Algebra.Homology.ExactSequence
