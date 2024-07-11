48f2fa0feb898855650ab0dd7b42e85455b059be
jjdishere <emailboxofjjd@163.com>
2024-07-11 11:04:18 +0200

    Update Almost.lean

--- Mathlib/RingTheory/Almost.lean
+++ Mathlib/RingTheory/Almost.lean
@@ -5,6 +5,21 @@ import Mathlib.RingTheory.Flat.Basic
 # Setup of Almost Ring Theory
 
 In this file we define the basic setup of almost ring theory, and basic notations in almost
+
+AlmostZero
+
+AlmostInjective
+
+AlmostSurjective
+
+AlmostIsom
+
+AlmostFree
+
+AlmostFree module over V = Almost Free module over V/m
+
+Goal: Lemma 4.1. Let M be an A-module which is π-adically complete and without π-torsion; let d ≥ 0. Then the A-module M is almost free of rank d if and only if the A/πA-module M/πM is almost free of rank d.
+Let K be a perfectoid field of characteristic p, and L/K a finite field exten- sion. Then the OK-module OL (= the integral closure of OK inside L) is almost free of rank |L : K|.
 -/
 
 open scoped TensorProduct
@@ -13,6 +28,9 @@ open Module
 
 variable {V : Type*} [CommRing V] (m : Ideal V)
 
-class AlmostIdeal : Prop where
+class AlmostBasicSetup : Prop where
   isIdempotent : IsIdempotentElem m
   flat_tensor : Flat V (m ⊗[V] m)
+
+def IsAlmostFreeOfRank {M : Type*} [Module V M] (d : ℕ) :
+  ∀ e > 0, ∃
