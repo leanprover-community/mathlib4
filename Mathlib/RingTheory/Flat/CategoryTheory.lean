f6a9eb02398acaeae28268df1d8ba691c02cf9f0
Jujian Zhang <jujian.zhang1998@outlook.com>
2024-07-12 01:02:45 +0100

    golf

--- Mathlib/RingTheory/Flat/CategoryTheory.lean
+++ Mathlib/RingTheory/Flat/CategoryTheory.lean
@@ -19,12 +19,10 @@ In this file we prove that tensoring with a flat module is an exact functor.
 ## Main results
 
 - `Module.Flat.iff_lTensor_preserves_shortComplex_exact`: an `R`-module `M` is flat if and only if
-  for every short exact sequence `0 ⟶ A ⟶ B ⟶ C ⟶ 0`, `0 ⟶ M ⊗ A ⟶ M ⊗ B ⟶ M ⊗ C ⟶ 0` is also
-  a short exact sequence.
+  for every exact sequence `A ⟶ B ⟶ C`, `M ⊗ A ⟶ M ⊗ B ⟶ M ⊗ C` is also exact.
 
 - `Module.Flat.iff_rTensor_preserves_shortComplex_exact`: an `R`-module `M` is flat if and only if
-  for every short exact sequence `0 ⟶ A ⟶ B ⟶ C ⟶ 0`, `0 ⟶ A ⊗ M ⟶ B ⊗ M ⟶ C ⊗ M ⟶ 0` is also
-  a short exact sequence.
+  for every short exact sequence `A ⟶ B ⟶ C`, `A ⊗ M ⟶ B ⊗ M ⟶ C ⊗ M` is also exact.
 
 ## TODO
 
@@ -36,7 +34,7 @@ In this file we prove that tensoring with a flat module is an exact functor.
 
 universe u
 
-open CategoryTheory MonoidalCategory Abelian
+open CategoryTheory MonoidalCategory ShortComplex.ShortExact
 
 namespace Module.Flat
 
@@ -44,42 +42,32 @@ variable {R : Type u} [CommRing R] (M : ModuleCat.{u} R)
 
 lemma lTensor_shortComplex_exact [Flat R M] (C : ShortComplex $ ModuleCat R) (hC : C.Exact) :
     C.map (tensorLeft M) |>.Exact := by
-  rw [ShortComplex.moduleCat_exact_iff_range_eq_ker, Eq.comm, ← LinearMap.exact_iff]
-  exact lTensor_exact M $ LinearMap.exact_iff.2 $ Eq.symm $
-    ShortComplex.moduleCat_exact_iff_range_eq_ker _ |>.1 hC
+  rw [moduleCat_exact_iff_function_exact] at hC ⊢
+  exact lTensor_exact M hC
 
 lemma rTensor_shortComplex_exact [Flat R M] (C : ShortComplex $ ModuleCat R) (hC : C.Exact) :
     C.map (tensorRight M) |>.Exact := by
-  rw [ShortComplex.moduleCat_exact_iff_range_eq_ker, Eq.comm, ← LinearMap.exact_iff]
-  exact rTensor_exact M $ LinearMap.exact_iff.2 $ Eq.symm $
-    ShortComplex.moduleCat_exact_iff_range_eq_ker _ |>.1 hC
+  rw [moduleCat_exact_iff_function_exact] at hC ⊢
+  exact rTensor_exact M hC
 
 lemma iff_lTensor_preserves_shortComplex_exact :
     Flat R M ↔
     ∀ (C : ShortComplex $ ModuleCat R) (_ : C.Exact), (C.map (tensorLeft M) |>.Exact) :=
-  ⟨fun _ _=> lTensor_shortComplex_exact _ _, fun H => by
-    rw [iff_lTensor_exact]
-    intro N N' N'' _ _ _ _ _ _ f g h
-    specialize H (.mk (ModuleCat.ofHom f) (ModuleCat.ofHom g)
-      (DFunLike.ext _ _ h.apply_apply_eq_zero))
-      (ShortComplex.moduleCat_exact_iff_range_eq_ker _ |>.2 $ Eq.symm $
-        LinearMap.exact_iff |>.1 $ h)
-    rw [LinearMap.exact_iff, Eq.comm]
-    rw [ShortComplex.moduleCat_exact_iff_range_eq_ker] at H
-    convert H⟩
+  ⟨fun _ _=> lTensor_shortComplex_exact _ _, fun H => iff_lTensor_exact.2 $
+    fun _ _ _ _ _ _ _ _ _ f g h =>
+      moduleCat_exact_iff_function_exact _ |>.1 $
+      H (.mk (ModuleCat.ofHom f) (ModuleCat.ofHom g)
+        (DFunLike.ext _ _ h.apply_apply_eq_zero))
+          (moduleCat_exact_iff_function_exact _ |>.2 h)⟩
 
 lemma iff_rTensor_preserves_shortComplex_exact :
     Flat R M ↔
     ∀ (C : ShortComplex $ ModuleCat R) (_ : C.Exact), (C.map (tensorRight M) |>.Exact) :=
-  ⟨fun _ _=> rTensor_shortComplex_exact _ _, fun H => by
-    rw [iff_rTensor_exact]
-    intro N N' N'' _ _ _ _ _ _ f g h
-    specialize H (.mk (ModuleCat.ofHom f) (ModuleCat.ofHom g)
-      (DFunLike.ext _ _ h.apply_apply_eq_zero))
-      (ShortComplex.moduleCat_exact_iff_range_eq_ker _ |>.2 $ Eq.symm $
-        LinearMap.exact_iff |>.1 $ h)
-    rw [LinearMap.exact_iff, Eq.comm]
-    rw [ShortComplex.moduleCat_exact_iff_range_eq_ker] at H
-    convert H⟩
+  ⟨fun _ _=> rTensor_shortComplex_exact _ _, fun H => iff_rTensor_exact.2 $
+    fun _ _ _ _ _ _ _ _ _ f g h =>
+      moduleCat_exact_iff_function_exact _ |>.1 $
+      H (.mk (ModuleCat.ofHom f) (ModuleCat.ofHom g)
+        (DFunLike.ext _ _ h.apply_apply_eq_zero))
+          (moduleCat_exact_iff_function_exact _ |>.2 h)⟩
 
 end Module.Flat
