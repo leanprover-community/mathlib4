5895ca7de57cd0fd076a50a20554387102fd614b
morel <smorel@math.princeton.edu>
2024-07-12 18:14:03 +0200

    still trying to appease the linter...

--- Mathlib/AlgebraicGeometry/Grassmannian/Basic.lean
+++ Mathlib/AlgebraicGeometry/Grassmannian/Basic.lean
@@ -1,6 +1,6 @@
 /-
-Copyright (c) 2024 Patricio Gallardo Candela, Yun Liu, Sophie Morel, David Swinarski, Weihong Xu.
-All rights reserved. Released under Apache 2.0 license as described in the file LICENSE.
+Copyright (c) 2024 Patricio Gallardo Candela, Yun Liu, Sophie Morel, David Swinarski, Weihong Xu. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Patricio Gallardo Candela, Yun Liu, Sophie Morel, David Swinarski, Weihong Xu
 This contribution was created as part of the AIM workshop "Formalizing algebraic geometry" in
 June 2024.
