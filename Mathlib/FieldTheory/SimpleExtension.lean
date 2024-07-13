13ba78a42e553bc41bbc1e42fcc16924612ffced
Daniel Weber <commandmaster2017@gmail.com>
2024-07-13 12:38:56 +0300

    Simple extension

---
+++ Mathlib/FieldTheory/SimpleExtension.lean
@@ -0,0 +1,122 @@
+/-
+Copyright (c) 2024 Daniel Weber. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Daniel Weber
+-/
+import Mathlib.FieldTheory.PrimitiveElement
+import Mathlib.RingTheory.Henselian
+import Mathlib.Algebra.Lie.OfAssociative
+
+/-!
+# Simple field extensions
+
+We define a typeclass `SimpleExtension` for one field being a simple extension of another
+(this is data because it contains which element was adjoined).
+
+We also define `TranscendentalExtension F K`, and show that it implies that `K` is `F[X]`'s fraction
+field.
+-/
+
+open Polynomial IntermediateField algebraMap
+
+/--
+A typeclass for K being a simple extension of F, F⟮k⟯ = K.
+-/
+class SimpleExtension (F K : Type*) [CommSemiring F] [Field K] [Algebra F K] where
+  /- The element of K which is adjoined to F. -/
+  k : K
+  /- Every value in K is a fraction of two polynomials with coefficients in F, evaluated at k. -/
+  is_adjoin' : ∀ (x : K), ∃ (r s : Polynomial F), x = (aeval k) r / (aeval k) s
+
+/--
+We can use `Field.exists_primitive_element` to noncomputable create a simple extension.
+-/
+noncomputable def SimpleExtesion_of_primitive (F K : Type*) [Field F] [Field K] [Algebra F K]
+    [FiniteDimensional F K] [Algebra.IsSeparable F K] : SimpleExtension F K where
+  k := (Field.exists_primitive_element F K).choose
+  is_adjoin' x := by
+    rw [← IntermediateField.mem_adjoin_simple_iff, (Field.exists_primitive_element F K).choose_spec]
+    apply IntermediateField.mem_top
+
+section CommSemiring
+
+variable (F K : Type*) [CommSemiring F] [Field K] [Algebra F K] [se : SimpleExtension F K]
+
+instance (priority := 100) toAlgebra : Algebra F[X] K := (aeval se.k).toAlgebra
+
+lemma SimpleExtension.is_adjoin : ∀ (x : K), ∃ (r s : Polynomial F), x = r / s := se.is_adjoin'
+
+@[simp]
+lemma algebraMap_eq_aeval : algebraMap F[X] K = aeval (R := F) se.k := rfl
+
+lemma SimpleExtension.X_eq_k : algebraMap F[X] K X = se.k := aeval_X _
+
+end CommSemiring
+
+section CommRing
+
+variable (F K : Type*) [CommRing F] [Nontrivial F] [IsDomain F] [Field K] [Algebra F K]
+    [se : SimpleExtension F K]
+
+/--
+A typeclass for K being a transcendental simple extension of F.
+-/
+class TranscendentalExtension : Prop where
+  is_transcendental : Transcendental F se.k
+
+variable [is_trans : TranscendentalExtension F K]
+
+instance (priority := 100) toIsFractionRing : IsFractionRing F[X] K where
+  map_units' := by
+    simp only [isUnit_iff_ne_zero, ne_eq, Subtype.forall]
+    intro a ha
+    change (a : K) ≠ 0
+    intro h
+    absurd is_trans.is_transcendental
+    exists a
+    exact ⟨nonZeroDivisors.ne_zero ha, h⟩
+  surj' z := by
+    by_cases hz : z = 0
+    · exists ⟨0, ⟨1, ?_⟩⟩
+      intro
+      simp only [mul_one, imp_self]
+      simp [hz]
+    obtain ⟨r, s, h⟩ := se.is_adjoin z
+    have : (s : K) ≠ 0 := fun nh ↦ hz (div_zero (G₀ := K) _ ▸ (nh ▸ h))
+    have s0 : s ≠ 0 := fun nh ↦ by rw [nh] at this; simp at this
+    exists ⟨r, s, ?_⟩
+    · intro _ h
+      simp only [mul_eq_zero, s0, or_false] at h
+      exact h
+    rw [h]
+    field_simp
+    rfl
+  exists_of_eq {x y h} := by
+    exists 1
+    simp only [OneMemClass.coe_one, one_mul]
+    by_contra! nh
+    rw [algebraMap_eq_aeval, RingHom.coe_coe] at h
+    exact is_trans.is_transcendental ⟨x - y, sub_ne_zero_of_ne nh, by simp [h]⟩
+
+end CommRing
+
+section Field
+
+variable (F K : Type*) [Field F] [Field K] [Algebra F K] [se : SimpleExtension F K]
+
+lemma SimpleExtension.adjoin_eq_top : F⟮se.k⟯ = ⊤ := by
+  ext x
+  simp only [mem_adjoin_simple_iff, mem_top, iff_true]
+  apply SimpleExtension.is_adjoin'
+
+/--
+If K is a finite extension of F then it's equivalent to adjoining to F a root of
+`minpoly F k`.
+-/
+noncomputable def SimpleExtension.equivAdjoinRoot [FiniteDimensional F K] :
+    AdjoinRoot (minpoly F se.k) ≃ₐ[F] K :=
+  IntermediateField.adjoinRootEquivAdjoin F (IsAlgebraic.of_finite ..).isIntegral
+  |>.trans <| IntermediateField.equivOfEq (SimpleExtension.adjoin_eq_top F K)
+  |>.trans <| IntermediateField.topEquiv
+
+end Field
