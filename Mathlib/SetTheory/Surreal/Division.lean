ae6528b8a888f0d3b19e25f3342e19954720a032
Theodore Hwa <hwatheod@cs.stanford.edu>
2024-07-13 18:26:34 -0700

    import ringNF

--- Mathlib/SetTheory/Surreal/Division.lean
+++ Mathlib/SetTheory/Surreal/Division.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Theodore Hwa
 -/
 import Mathlib.SetTheory.Surreal.Multiplication
+import Mathlib.Tactic.Ring.RingNF
 
 /-!
 # Surreal division
