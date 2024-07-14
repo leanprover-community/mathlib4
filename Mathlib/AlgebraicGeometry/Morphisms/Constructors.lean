46397be139d69d323170da071c397b4d3daa7b48
Andrew Yang <the.erd.one@gmail.com>
2024-07-15 01:04:10 +0800

    fix

--- Mathlib/AlgebraicGeometry/Morphisms/Constructors.lean
+++ Mathlib/AlgebraicGeometry/Morphisms/Constructors.lean
@@ -138,11 +138,14 @@ theorem HasAffineProperty.affineProperty_diagonal (P) {Q} [HasAffineProperty P Q
   ext X Y f _
   exact of_affineProperty_diagonal P
 
+-- Doesn't build without the `IsAffine` instance but the linter complains
+@[nolint unusedHavesSuffices]
 instance HasAffineProperty.diagonal_affineProperty_isLocal
     {Q : AffineTargetMorphismProperty} [Q.IsLocal] :
     Q.diagonal.IsLocal where
   respectsIso := inferInstance
   to_basicOpen {X Y} _ f r hf :=
+    have : IsAffine (Y ∣_ᵤ Y.basicOpen r) := (isAffineOpen_top Y).basicOpen r
     affineProperty_diagonal_of_diagonal_of_isPullback (targetAffineLocally Q)
       (isPullback_morphismRestrict f (Y.basicOpen r)).flip
       ((of_affineProperty_diagonal (targetAffineLocally Q)).mp hf)
