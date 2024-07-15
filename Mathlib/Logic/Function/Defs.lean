4e7d2143bde8625e6667edb83f52d5f724ce2db7
Johan Commelin <johan@commelin.net>
2024-07-15 17:12:01 +0200

    fix shake

--- Mathlib/Logic/Function/Defs.lean
+++ Mathlib/Logic/Function/Defs.lean
@@ -3,9 +3,10 @@ Copyright (c) 2014 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Leonardo de Moura, Jeremy Avigad, Haitao Zhang
 -/
-import Mathlib.Tactic.Basic
 import Mathlib.Mathport.Rename
+import Mathlib.Tactic.AdaptationNote
 import Mathlib.Tactic.Attr.Register
+import Mathlib.Tactic.Basic
 import Mathlib.Tactic.Eqns
 import Mathlib.Tactic.TypeStar
 
