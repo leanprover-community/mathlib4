4241610f1b097c6d5e843f6405c689b6a5f85ec3
Johan Commelin <johan@commelin.net>
2024-07-15 19:14:57 +0200

    chore(Data/Bool): merge Init.Data.Bool.* into Data.Bool.Basic

--- Mathlib.lean
+++ Mathlib.lean
@@ -2705,8 +2705,6 @@ import Mathlib.Init.Classical
 import Mathlib.Init.Control.Combinators
 import Mathlib.Init.Control.Lawful
 import Mathlib.Init.Core
-import Mathlib.Init.Data.Bool.Basic
-import Mathlib.Init.Data.Bool.Lemmas
 import Mathlib.Init.Data.Buffer.Parser
 import Mathlib.Init.Data.Fin.Basic
 import Mathlib.Init.Data.Int.Basic
