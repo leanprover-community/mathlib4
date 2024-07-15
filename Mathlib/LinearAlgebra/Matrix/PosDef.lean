03e7f95ed5b80a22dea994a1213f6178be038538
Christopher Hoskin <christopher.hoskin@gmail.com>
2024-07-15 18:46:16 +0100

    Missed one

--- Mathlib/LinearAlgebra/Matrix/PosDef.lean
+++ Mathlib/LinearAlgebra/Matrix/PosDef.lean
@@ -283,7 +283,7 @@ theorem PosSemidef.dotProduct_mulVec_zero_iff
 /-- For `A` positive semidefinite, we have `x⋆ A x = 0` iff `A x = 0` (linear maps version). -/
 theorem PosSemidef.toLinearMap₂'_zero_iff [DecidableEq n]
     {A : Matrix n n 𝕜} (hA : PosSemidef A) (x : n → 𝕜) :
-    Matrix.toLinearMap₂' (R := 𝕜) A (star x) x = 0 ↔ Matrix.toLin' A x = 0 := by
+    Matrix.toLinearMap₂' 𝕜 A (star x) x = 0 ↔ Matrix.toLin' A x = 0 := by
   simpa only [toLinearMap₂'_apply', toLin'_apply] using hA.dotProduct_mulVec_zero_iff x
 
 /-!
