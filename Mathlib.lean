4831f315dbf1a41e8d9ea02d50dd0037efef429a
Amelia Livingston <101damnations@github.com>
2024-07-15 20:56:23 +0000

    feat: the category of Hopf algebras (#12010)
    The category of Hopf algebras over a commutative ring. Mimics `Mathlib.LinearAlgebra.QuadraticForm.QuadraticModuleCat`.
    
    
    
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -77,6 +77,7 @@ import Mathlib.Algebra.Category.Grp.Subobject
 import Mathlib.Algebra.Category.Grp.ZModuleEquivalence
 import Mathlib.Algebra.Category.Grp.Zero
 import Mathlib.Algebra.Category.GrpWithZero
+import Mathlib.Algebra.Category.HopfAlgebraCat.Basic
 import Mathlib.Algebra.Category.ModuleCat.Abelian
 import Mathlib.Algebra.Category.ModuleCat.Adjunctions
 import Mathlib.Algebra.Category.ModuleCat.Algebra
