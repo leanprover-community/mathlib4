ee837d3fc2902ba07ffd53c2f229b39d373650bf
Johan Commelin <johan@commelin.net>
2024-07-15 18:50:44 +0200

    fix cyclic import

--- Mathlib/Data/Int/Defs.lean
+++ Mathlib/Data/Int/Defs.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jeremy Avigad
 -/
 import Mathlib.Data.Nat.Defs
-import Mathlib.Data.Int.Defs
 import Mathlib.Init.ZeroOne
 import Mathlib.Logic.Nontrivial.Defs
 import Mathlib.Tactic.Convert
