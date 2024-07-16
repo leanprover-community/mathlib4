20524cc1ad6e1bbb06021b2355cfdc63bd283b8d
grunweg <rothgami@math.hu-berlin.de>
2024-07-16 09:12:32 +0000

    chore: move 300 lines off Combinatorics/SimpleGraph/Connectivity (#14647)
    Move material about walks as subgraphs and walk-counting to separate files.
    
    Mostly import-directed; I'm not a graph theorist - please comment if this split doesn't make sense!

--- Mathlib.lean
+++ Mathlib.lean
@@ -1844,6 +1844,7 @@ import Mathlib.Combinatorics.SimpleGraph.Coloring
 import Mathlib.Combinatorics.SimpleGraph.ConcreteColorings
 import Mathlib.Combinatorics.SimpleGraph.Connectivity
 import Mathlib.Combinatorics.SimpleGraph.Connectivity.Subgraph
+import Mathlib.Combinatorics.SimpleGraph.Connectivity.WalkCounting
 import Mathlib.Combinatorics.SimpleGraph.Dart
 import Mathlib.Combinatorics.SimpleGraph.DegreeSum
 import Mathlib.Combinatorics.SimpleGraph.Density
