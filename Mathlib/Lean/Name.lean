9b451c6c9f788691d643f83a0ae92111884a0bec
Kim Morrison <kim@tqft.net>
2024-07-16 10:56:21 +1000

    fix imports

--- Mathlib/Lean/Name.lean
+++ Mathlib/Lean/Name.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison
 -/
 import Lean.Meta.Match.MatcherInfo
+import Lean.Meta.Tactic.Delta
 import Std.Data.HashMap.Basic
 
 /-!
