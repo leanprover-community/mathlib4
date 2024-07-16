91b69c2a948c941b0a7e1067fa1a06c59d40b0f4
Riccardo Brasca <riccardo.brasca@gmail.com>
2024-07-16 13:20:17 +0200

    better

--- Mathlib/RingTheory/Trace/Quotient.lean
+++ Mathlib/RingTheory/Trace/Quotient.lean
@@ -20,7 +20,7 @@ quotients and localizations.
 
 variable {R S : Type*} [CommRing R] [CommRing S] [Algebra R S] [LocalRing R]
 
-open LocalRing FiniteDimensional
+open LocalRing FiniteDimensional Submodule
 
 section LocalRing
 
@@ -32,30 +32,30 @@ variable [Module.Free R S] [Module.Finite R S]
 attribute [local instance] Ideal.Quotient.field
 
 theorem quotient_span_eq_top_iff_span_eq_top_of_localRing (s : Set S) :
-    Submodule.span (R ⧸ p) ((Ideal.Quotient.mk (I := pS)) '' s) = ⊤ ↔
-    Submodule.span R s = ⊤ := by
-  have H : (Submodule.span (R ⧸ p) ((Ideal.Quotient.mk (I := pS)) '' s)).restrictScalars R =
-      (Submodule.span R s).map (IsScalarTower.toAlgHom R S (S ⧸ pS)) := by
-    rw [Submodule.map_span, ← Submodule.restrictScalars_span R (R ⧸ p) Ideal.Quotient.mk_surjective]
+    span (R ⧸ p) ((Ideal.Quotient.mk (I := pS)) '' s) = ⊤ ↔
+    span R s = ⊤ := by
+  have H : (span (R ⧸ p) ((Ideal.Quotient.mk (I := pS)) '' s)).restrictScalars R =
+      (span R s).map (IsScalarTower.toAlgHom R S (S ⧸ pS)) := by
+    rw [map_span, ← restrictScalars_span R (R ⧸ p) Ideal.Quotient.mk_surjective]
     rfl
   constructor
   · intro hs
     rw [← top_le_iff]
-    apply Submodule.le_of_le_smul_of_le_jacobson_bot
+    apply le_of_le_smul_of_le_jacobson_bot
     · exact Module.finite_def.mp ‹_›
     · exact (jacobson_eq_maximalIdeal ⊥ bot_ne_top).ge
     · rw [Ideal.smul_top_eq_map]
       rintro x -
       have : LinearMap.ker (IsScalarTower.toAlgHom R S (S ⧸ pS)) =
-          Submodule.restrictScalars R pS := by
+          restrictScalars R pS := by
         ext; simp [Ideal.Quotient.eq_zero_iff_mem]
-      rw [← this, ← Submodule.comap_map_eq, Submodule.mem_comap, ← H, hs]
+      rw [← this, ← comap_map_eq, mem_comap, ← H, hs]
       trivial
   · intro hs
     rw [hs, Submodule.map_top] at H
     change _ = LinearMap.range (Ideal.Quotient.mkₐ _ _) at H
     rwa [LinearMap.range_eq_top.mpr (Ideal.Quotient.mkₐ_surjective _ _),
-      Submodule.restrictScalars_eq_top_iff] at H
+      restrictScalars_eq_top_iff] at H
 
 theorem finrank_quotient_map_of_localRing :
     finrank (R ⧸ p) (S ⧸ pS) = finrank R S := by
