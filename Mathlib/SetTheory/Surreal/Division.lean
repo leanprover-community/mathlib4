eb58d3715b6881a36aba7f32433bc155f1c40939
Theodore Hwa <hwatheod@cs.stanford.edu>
2024-07-16 09:08:36 -0700

    use field_simp for eq2

--- Mathlib/SetTheory/Surreal/Division.lean
+++ Mathlib/SetTheory/Surreal/Division.lean
@@ -408,61 +408,50 @@ lemma eq2 {l r} (L : l → PGame) (R : r → PGame)
     case _ j _ => exact h3 j j.2
   on_goal 4 =>
     case _ j _ => exact h3 j j.2
-  all_goals
+  case' left₁ i j =>
+    apply_fun (fun x => x - mk (R i) (h2 i) * mk (PGame.mk l r L R).inv' inv_numeric)
+      using sub_left_injective
     ring_nf
-    first | simp only [inv_r] | simp only [inv_l]
-  · case _ j i1 =>
-      apply_fun (fun x => x - mk (R j) (h2 j) * mk (PGame.mk l r L R).inv' inv_numeric)
-        using sub_left_injective
-      ring_nf
-      conv_lhs => simp [inv']
-      conv_rhs => {
-        rw [mul_assoc (mk (R j) _) _ _, mul_comm (mk (R j) _), mul_assoc]
-        simp [inv_r]
-        rw [pow_two, mul_assoc (mk (R j) _) _ _, mul_assoc (mk (R j) _) _ _,
-          mul_comm (mk (R j) _) (mk (invVal _ _ _ _ _ _) _), mul_assoc]
-        simp [inv_r]
-      }
-      ring
-  · case _ j i1 =>
-      apply_fun (fun x => x - mk (L j) (h1 j) * mk (PGame.mk l r L R).inv' inv_numeric)
-        using sub_left_injective
-      ring_nf
-      conv_lhs => simp [inv']
-      conv_rhs => {
-        rw [mul_assoc (mk (L j) _) _ _, mul_comm (mk (L j) _), mul_assoc]
-        simp [inv_l]
-        rw [pow_two, mul_assoc (mk (L j) _) _ _, mul_assoc (mk (L j) _) _ _,
-          mul_comm (mk (L j) _) (mk (invVal _ _ _ _ _ _) _), mul_assoc]
-        simp [inv_l]
-      }
-      ring
-  · case _ j i1 =>
-      apply_fun (fun x => x - mk (L j) (h1 j) * mk (PGame.mk l r L R).inv' inv_numeric)
-        using sub_left_injective
-      ring_nf
-      conv_lhs => simp [inv']
-      conv_rhs => {
-        rw [mul_assoc (mk (L j) _) _ _, mul_comm (mk (L j) _), mul_assoc]
-        simp [inv_l]
-        rw [pow_two, mul_assoc (mk (L j) _) _ _, mul_assoc (mk (L j) _) _ _,
-          mul_comm (mk (L j) _) (mk (invVal _ _ _ _ _ _) _), mul_assoc]
-        simp [inv_l]
-      }
-      ring
-  · case _ j i1 =>
-      apply_fun (fun x => x - mk (R j) (h2 j) * mk (PGame.mk l r L R).inv' inv_numeric)
-        using sub_left_injective
-      ring_nf
-      conv_lhs => simp [inv']
-      conv_rhs => {
-        rw [mul_assoc (mk (R j) _) _ _, mul_comm (mk (R j) _), mul_assoc]
-        simp [inv_r]
-        rw [pow_two, mul_assoc (mk (R j) _) _ _, mul_assoc (mk (R j) _) _ _,
-          mul_comm (mk (R j) _) (mk (invVal _ _ _ _ _ _) _), mul_assoc]
-        simp [inv_r]
-      }
-      ring
+    conv_lhs => simp [inv']
+    specialize inv_r i
+    have : IsUnit (Surreal.mk (R i) (by tauto)) := by
+      use ⟨_, _, inv_r, by rw [mul_comm]; exact inv_r⟩
+    lift (Surreal.mk (R i) (by tauto)) to Surrealˣ using id this with s
+    have : mk (R i).inv' (by tauto) = ↑s⁻¹ := Eq.symm (Units.inv_eq_of_mul_eq_one_right inv_r)
+  case' left₂ i j =>
+    apply_fun (fun x => x - mk (L i) (h1 i) * mk (PGame.mk l r L R).inv' inv_numeric)
+      using sub_left_injective
+    ring_nf
+    conv_lhs => simp [inv']
+    specialize inv_l i
+    have : IsUnit (Surreal.mk (L i) (by tauto)) := by
+      use ⟨_, _, inv_l, by rw [mul_comm]; exact inv_l⟩
+    lift (Surreal.mk (L i) (by tauto)) to Surrealˣ using id this with s
+    have : mk (L i).inv' (h3 i i.2) = ↑s⁻¹ := Eq.symm (Units.inv_eq_of_mul_eq_one_right inv_l)
+  case' right₁ i j =>
+    apply_fun (fun x => x - mk (L i) (h1 i) * mk (PGame.mk l r L R).inv' inv_numeric)
+      using sub_left_injective
+    ring_nf
+    conv_lhs => simp [inv']
+    specialize inv_l i
+    have : IsUnit (Surreal.mk (L i) (by tauto)) := by
+      use ⟨_, _, inv_l, by rw [mul_comm]; exact inv_l⟩
+    lift (Surreal.mk (L i) (by tauto)) to Surrealˣ using id this with s
+    have : mk (L i).inv' (h3 i i.2) = ↑s⁻¹ := Eq.symm (Units.inv_eq_of_mul_eq_one_right inv_l)
+  case' right₂ i j =>
+    apply_fun (fun x => x - mk (R i) (h2 i) * mk (PGame.mk l r L R).inv' inv_numeric)
+      using sub_left_injective
+    ring_nf
+    conv_lhs => simp [inv']
+    specialize inv_r i
+    have : IsUnit (Surreal.mk (R i) (by tauto)) := by
+      use ⟨_, _, inv_r, by rw [mul_comm]; exact inv_r⟩
+    lift (Surreal.mk (R i) (by tauto)) to Surrealˣ using id this with s
+    have : mk (R i).inv' (by tauto) = ↑s⁻¹ := Eq.symm (Units.inv_eq_of_mul_eq_one_right inv_r)
+  all_goals
+    rw [this]
+    field_simp
+    ring
 
 lemma onag_1_10_iii_left' {l r} (L : l → PGame) (R : r → PGame)
     (h1 : ∀ i, (L i).Numeric) (h2 : ∀ j, (R j).Numeric) (h3 : ∀ i, 0 < (L i) → (L i).inv'.Numeric)
