b9b2266b3b548f68519495264c2c0bc6e31978b8
Daniel Weber <commandmaster2017@gmail.com>
2024-07-17 15:12:00 +0300

    Style 2

--- Mathlib/Analysis/Analytic/Basic.lean
+++ Mathlib/Analysis/Analytic/Basic.lean
@@ -617,8 +617,8 @@ theorem AnalyticOn.add {s : Set E} (hf : AnalyticOn 𝕜 f s) (hg : AnalyticOn �
   fun z hz => (hf z hz).add (hg z hz)
 #align analytic_on.add AnalyticOn.add
 
-theorem AnalyticOn.neg {s : Set E} (hf : AnalyticOn 𝕜 f s) :
-    AnalyticOn 𝕜 (-f) s := fun z hz ↦ (hf z hz).neg
+theorem AnalyticOn.neg {s : Set E} (hf : AnalyticOn 𝕜 f s) : AnalyticOn 𝕜 (-f) s :=
+  fun z hz ↦ (hf z hz).neg
 
 theorem AnalyticOn.sub {s : Set E} (hf : AnalyticOn 𝕜 f s) (hg : AnalyticOn 𝕜 g s) :
     AnalyticOn 𝕜 (f - g) s :=
