f73b2b673438f9b9db3a4b1b098509e4363709c1
Riccardo Brasca <riccardo.brasca@gmail.com>
2024-07-17 14:01:54 +0200

    shake

--- Mathlib/RingTheory/LocalRing/Defs.lean
+++ Mathlib/RingTheory/LocalRing/Defs.lean
@@ -3,11 +3,8 @@ Copyright (c) 2018 Kenny Lau. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kenny Lau, Chris Hughes, Mario Carneiro
 -/
-import Mathlib.Algebra.Algebra.Defs
-import Mathlib.RingTheory.Ideal.Operations
-import Mathlib.RingTheory.JacobsonIdeal
-import Mathlib.Logic.Equiv.TransferInstance
 import Mathlib.Tactic.TFAE
+import Mathlib.RingTheory.Ideal.Basic
 
 #align_import ring_theory.ideal.local_ring from "leanprover-community/mathlib"@"ec1c7d810034d4202b0dd239112d1792be9f6fdc"
 
