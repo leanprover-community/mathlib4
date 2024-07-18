a5bd149781f8ded8b78802115213fcc9f4a63a00
Ralf Stephan <ralf@ark.in-berlin.de>
2024-07-18 19:23:03 +0200

    fix

--- Mathlib/Data/Nat/Prime/EulerFermat.lean
+++ Mathlib/Data/Nat/Prime/EulerFermat.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2024 Ralf Stephan. All rights reserved.
-Released under CC0 license as described in the file LICENSE.
-Authors: Ralf Stephan
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Ralf Stephan, Thomas Browning
 -/
 import Mathlib.Data.Nat.Prime.Basic
 import Mathlib.Data.Nat.Factorization.Basic
