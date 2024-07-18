1b3ad61910e6c23c85c605fb65aa3e7f99477da0
Daniel Weber <commandmaster2017@gmail.com>
2024-07-18 06:25:20 +0300

    Fix imports

--- Mathlib/RingTheory/Derivation/Polynomial.lean
+++ Mathlib/RingTheory/Derivation/Polynomial.lean
@@ -3,7 +3,7 @@ Copyright (c) 2024 Daniel Weber. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Daniel Weber
 -/
-import Mathlib.Algebra.Polynomial.Derivation
+import Mathlib.RingTheory.Derivation.Basic
 import Mathlib.Algebra.Polynomial.Module.Basic
 
 /-!
