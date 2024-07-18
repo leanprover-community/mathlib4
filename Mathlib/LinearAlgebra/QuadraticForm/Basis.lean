092cd72f4dc960cc7462842e9cdb79efe35ef42e
Eric Wieser <wieser.eric@gmail.com>
2024-07-18 22:46:12 +0000

    QuadraticMap

--- Mathlib/LinearAlgebra/QuadraticForm/Basis.lean
+++ Mathlib/LinearAlgebra/QuadraticForm/Basis.lean
@@ -8,9 +8,9 @@ import Mathlib.Data.Finset.Sym
 import Mathlib.Data.Sym.Sym2.Order
 
 /-!
-# Constructing a bilinear form from a quadratic form, given a basis
+# Constructing a bilinear map from a quadratic map, given a basis
 
-This file provides an alternative to `QuadraticForm.associated`; unlike that definition, this one
+This file provides an alternative to `QuadraticMap.associated`; unlike that definition, this one
 does not require `Invertible (2 : R)`. Unlike that definition, this only works in the presence of
 a basis.
 -/
@@ -28,50 +28,51 @@ theorem Finset.sum_sym2_filter_not_isDiag {ι α} [LinearOrder ι] [AddCommMonoi
   · rintro ⟨⟨i₁, j₁⟩, hij₁⟩
     simp
 
-namespace QuadraticForm
+namespace QuadraticMap
 
-variable {ι R M} [LinearOrder ι] [CommRing R] [AddCommGroup M] [Module R M]
+variable {ι R M N} [LinearOrder ι]
+variable [CommRing R] [AddCommGroup M] [AddCommGroup N] [Module R M] [Module R N]
 
 /-- Given an ordered basis, produce a bilinear form associated with the quadratic form.
 
-Unlike `QuadraticForm.associated`, this is not symmetric; however, as a result it can be used even
+Unlike `QuadraticMap.associated`, this is not symmetric; however, as a result it can be used even
 in characteristic two. When considered as a matrix, the form is triangular. -/
-noncomputable def toBilin (Q : QuadraticForm R M) (bm : Basis ι R M) : LinearMap.BilinForm R M :=
+noncomputable def toBilin (Q : QuadraticMap R M N) (bm : Basis ι R M) : LinearMap.BilinMap R M N :=
   bm.constr (S := R) fun i =>
     bm.constr (S := R) fun j =>
       if i = j then Q (bm i) else if i < j then polar Q (bm i) (bm j) else 0
 
-theorem toBilin_apply (Q : QuadraticForm R M) (bm : Basis ι R M) (i j : ι) :
+theorem toBilin_apply (Q : QuadraticMap R M N) (bm : Basis ι R M) (i j : ι) :
     Q.toBilin bm (bm i) (bm j) =
       if i = j then Q (bm i) else if i < j then polar Q (bm i) (bm j) else 0 := by
   simp [toBilin]
 
-theorem toQuadraticForm_toBilin (Q : QuadraticForm R M) (bm : Basis ι R M) :
-    (Q.toBilin bm).toQuadraticForm = Q := by
+theorem toQuadraticMap_toBilin (Q : QuadraticMap R M N) (bm : Basis ι R M) :
+    (Q.toBilin bm).toQuadraticMap = Q := by
   ext x
-  rw [← bm.total_repr x, LinearMap.BilinForm.toQuadraticForm_apply, Finsupp.total_apply,
+  rw [← bm.total_repr x, LinearMap.BilinMap.toQuadraticMap_apply, Finsupp.total_apply,
     Finsupp.sum]
   simp_rw [LinearMap.map_sum₂, map_sum, LinearMap.map_smul₂, _root_.map_smul, toBilin_apply,
     smul_ite, smul_zero, ← Finset.sum_product', ← Finset.diag_union_offDiag,
     Finset.sum_union (Finset.disjoint_diag_offDiag _), Finset.sum_diag, if_true]
-  rw [Finset.sum_ite_of_false, QuadraticForm.map_sum, ← Finset.sum_filter]
-  · simp_rw [smul_eq_mul, ← polar_smul_right _ (bm.repr x <| Prod.snd _),
+  rw [Finset.sum_ite_of_false, QuadraticMap.map_sum, ← Finset.sum_filter]
+  · simp_rw [← polar_smul_right _ (bm.repr x <| Prod.snd _),
       ← polar_smul_left _ (bm.repr x <| Prod.fst _)]
-    simp_rw [QuadraticForm.map_smul, ← mul_assoc, Finset.sum_sym2_filter_not_isDiag]
+    simp_rw [QuadraticMap.map_smul, mul_smul, Finset.sum_sym2_filter_not_isDiag]
     rfl
   · intro x hx
     rw [Finset.mem_offDiag] at hx
     simpa using hx.2.2
 
-/-- In a free module, every quadratic form can be built from a bilinear form.
+/-- From a free module, every quadratic map can be built from a bilinear form.
 
-See `QuadraticForm.not_forall_toQuadraticForm_surjective` for a counterexample when the module is
+See `QuadraticMap.not_forall_toQuadraticMap_surjective` for a counterexample when the module is
 not free. -/
-theorem toQuadraticForm_surjective [Module.Free R M] :
-    Function.Surjective (LinearMap.BilinForm.toQuadraticForm : LinearMap.BilinForm R M → _) := by
+theorem toQuadraticMap_surjective [Module.Free R M] :
+    Function.Surjective (LinearMap.BilinMap.toQuadraticMap : LinearMap.BilinMap R M N → _) := by
   intro Q
   obtain ⟨ι, b⟩ := Module.Free.exists_basis (R := R) (M := M)
   letI : LinearOrder ι := IsWellOrder.linearOrder WellOrderingRel
-  exact ⟨_, toQuadraticForm_toBilin _ b⟩
+  exact ⟨_, toQuadraticMap_toBilin _ b⟩
 
-end QuadraticForm
+end QuadraticMap
