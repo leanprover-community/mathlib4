df5d048a702c59ddbaed5e4b393fe67ef7c5bdf7
Riccardo Brasca <riccardo.brasca@gmail.com>
2024-07-18 13:24:02 +0200

    fix also this

--- Mathlib/RingTheory/LocalRing/ResidueField/Defs.lean
+++ Mathlib/RingTheory/LocalRing/ResidueField/Defs.lean
@@ -3,7 +3,7 @@ Copyright (c) 2018 Kenny Lau. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kenny Lau, Chris Hughes, Mario Carneiro
 -/
-import Mathlib.RingTheory.LocalRing.ResidueField.Basic
+import Mathlib.RingTheory.LocalRing.RingHom.Basic
 
 #align_import ring_theory.ideal.local_ring from "leanprover-community/mathlib"@"ec1c7d810034d4202b0dd239112d1792be9f6fdc"
 
