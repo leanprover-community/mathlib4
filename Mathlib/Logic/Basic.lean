8a8216d15c35ea26ba9c4cabb2ec65c5fdd0599b
Michael Rothgang <rothgami@math.hu-berlin.de>
2024-07-19 22:03:01 +0200

    Fix.

--- Mathlib/Logic/Basic.lean
+++ Mathlib/Logic/Basic.lean
@@ -3,7 +3,6 @@ Copyright (c) 2016 Jeremy Avigad. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jeremy Avigad, Leonardo de Moura
 -/
-import Mathlib.Init.Logic
 import Mathlib.Init.Algebra.Classes
 import Mathlib.Tactic.Attr.Register
 import Mathlib.Tactic.Basic
