2fd4c7b67689018db48c2e594b7090d7d1019ae7
adomani <adomani@gmail.com>
2024-07-19 14:21:32 +0200

    deal with protected and test

--- test/MinImports.lean
+++ test/MinImports.lean
@@ -1,5 +1,32 @@
 import Mathlib.Tactic.MinImports
-import Mathlib.Tactic
+import Mathlib.Tactic.ExtractGoal
+import Mathlib.Tactic.Lemma
+import Mathlib.Data.Nat.Notation
+import Mathlib.Data.Int.Notation
+import Mathlib.Tactic.NormNum.Basic
+import Mathlib.Tactic.FunProp
+
+/-- info: import Mathlib.Tactic.FunProp.Attr -/
+#guard_msgs in
+#min_imports in (← `(declModifiers|@[fun_prop]))
+
+namespace X
+/-- info: import Mathlib.Algebra.Ring.Nat -/
+#guard_msgs in
+#min_imports in
+protected def xxx : Semiring Nat := inferInstance
+end X
+
+/-- info: import Mathlib.Algebra.Ring.Nat -/
+#guard_msgs in
+#min_imports in
+-- If `#min_imports` were parsing just the syntax, the imports would be `Mathlib.Algebra.Ring.Defs`.
+instance : Semiring Nat := inferInstance
+
+/-- info: import Mathlib.Algebra.Ring.Nat -/
+#guard_msgs in
+#min_imports in
+instance withName : Semiring Nat := inferInstance
 
 /--
 warning: Imports increased to
