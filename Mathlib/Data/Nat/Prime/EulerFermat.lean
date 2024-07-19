baaf415774de8358a9d408304056fb30b2554ee2
Ralf Stephan <ralf@ark.in-berlin.de>
2024-07-19 09:44:54 +0200

    Update Mathlib/Data/Nat/Prime/EulerFermat.lean
    Co-authored-by: Thomas Browning <tb65536@users.noreply.github.com>

--- Mathlib/Data/Nat/Prime/EulerFermat.lean
+++ Mathlib/Data/Nat/Prime/EulerFermat.lean
@@ -21,27 +21,10 @@ theorem pow_of_pow_add_prime (a n : ℕ) (ha : 1 < a) (hn : 1 < n) (hP : Nat.Pri
   obtain ⟨k, m, hm, rfl⟩ := exists_eq_two_pow_mul_odd (one_pos.trans hn).ne'
   rw [pow_mul] at hP
   use k
-  apply (mul_eq_left₀ (Ne.symm (NeZero.ne' (2 ^ k)))).mpr
-  by_contra hm'
-  have nat_add_one_dvd_pow_add_one (x : ℕ) {n : ℕ} (hn : Odd n) : x + 1 ∣ x ^ n + 1 := by
-    simpa only [one_pow] using hn.nat_add_dvd_pow_add_pow x 1
-  have not_prime : ¬ ((a ^ 2 ^ k) ^ m + 1).Prime := by
-    apply (not_prime_iff_exists_dvd_ne _).mpr
-    · use a ^ 2 ^ k + 1
-      have h₁ : a ^ 2 ^ k + 1 ≠ 1 := by
-        rw [ne_eq, add_left_eq_self, pow_eq_zero_iff]
-        · exact not_eq_zero_of_lt ha
-        exact Ne.symm (NeZero.ne' (2 ^ k))
-      have h₂ : a ^ 2 ^ k + 1 ≠ (a ^ 2 ^ k) ^ m + 1 := by
-        apply Ne.symm
-        rw [ne_eq, add_left_inj, pow_eq_self_iff (a := a ^ 2 ^ k) (b := m)]
-        · exact hm'
-        · simp only [ne_eq, pow_eq_zero_iff', OfNat.ofNat_ne_zero, false_and,
-            not_false_eq_true, Nat.one_lt_pow_iff, ha]
-      exact ⟨nat_add_one_dvd_pow_add_one (a ^ 2 ^ k) hm, h₁, h₂⟩
-    · simp only [reduceLeDiff]
-      apply_rules [one_le_pow, pos_pow_of_pos, zero_lt_of_lt ha]
-  contradiction
+  replace ha : 1 < a ^ 2 ^ k := one_lt_pow (pow_ne_zero k two_ne_zero) ha
+  have h := hm.nat_add_dvd_pow_add_pow (a ^ 2 ^ k) 1
+  rw [one_pow, hP.dvd_iff_eq (Nat.lt_add_right 1 ha).ne', add_left_inj, pow_eq_self_iff ha] at h
+  rw [h, mul_one]
 
 theorem not_all_pow_pow_add_prime : ¬ ∀ n : ℕ, Nat.Prime (2 ^ (2 ^ n) + 1) := by
   intro h
