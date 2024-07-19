8144c2d02d2dfe5b09c9fe4bc162288514dee491
Michael Rothgang <rothgami@math.hu-berlin.de>
2024-07-19 16:33:37 +0200

    Remove #align statement paragraph from non-empty Init files.

--- Mathlib/Init/Quot.lean
+++ Mathlib/Init/Quot.lean
@@ -13,10 +13,6 @@ The files in `Mathlib/Init` are leftovers from the port from Mathlib3.
 We intend to move all the content of these files out into the main `Mathlib` directory structure.
 Contributions assisting with this are appreciated.
 
-`#align` statements without corresponding declarations
-(i.e. because the declaration is in Batteries or Lean) can be left here.
-These will be deleted soon so will not significantly delay deleting otherwise empty `Init` files.
-
 # Quot
 
 Some induction principles tagged with `elab_as_elim`, since the attribute is missing in core.
