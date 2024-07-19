9922bd5bffc2d473dc5a6c162a0be104ece99074
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2024-07-19 22:13:23 +0200

    adjust Mathlib.lean (bad merge)

--- Mathlib.lean
+++ Mathlib.lean
@@ -2689,7 +2689,9 @@ import Mathlib.GroupTheory.GroupAction.Units
 import Mathlib.GroupTheory.HNNExtension
 import Mathlib.GroupTheory.Index
 import Mathlib.GroupTheory.MaximalSubgroups
-import Mathlib.GroupTheory.MonoidLocalization
+import Mathlib.GroupTheory.MonoidLocalization.Basic
+import Mathlib.GroupTheory.MonoidLocalization.MonoidWithZero
+import Mathlib.GroupTheory.MonoidLocalization.Order
 import Mathlib.GroupTheory.Nilpotent
 import Mathlib.GroupTheory.NoncommCoprod
 import Mathlib.GroupTheory.NoncommPiCoprod
