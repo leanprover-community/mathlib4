3d4048bc2028fdbf526ade82e9d4b9f69ca8d641
Florent Schaffhauser <7537413+matematiflo@users.noreply.github.com>
2024-07-19 21:36:40 +0000

    chore: add `@[simp]` attribute

--- Mathlib/Algebra/Module/LinearMap/Defs.lean
+++ Mathlib/Algebra/Module/LinearMap/Defs.lean
@@ -992,6 +992,7 @@ def toAddMonoidHom' : (M →ₛₗ[σ₁₂] M₂) →+ M →+ M₂ where
 #align linear_map.to_add_monoid_hom' LinearMap.toAddMonoidHom'
 
 /-- If `M` is the zero module, then  the identity map of `M` is the zero map. -/
+@[simp]
 theorem identityMapOfZeroModuleIsZero [Subsingleton M] : id (R := R₁) (M := M) = 0 :=
   Subsingleton.eq_zero id
 
