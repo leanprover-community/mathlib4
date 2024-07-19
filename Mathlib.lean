1674b343e4ae7ff5b245cbeb881005fe220a7fce
igorkhavkine <igor@tichy>
2024-07-19 10:46:21 +0000

    feat(MeasureTheory/Function): Uniform tightness of functions in Lp (#14646)
    Uniform tightness is a hypothesis necessary for the general version of the Vitali Convergence Theorem proven in #9163.
    
    
    
    Co-authored-by: igorkhavkine <133780155+igorkhavkine@users.noreply.github.com>
    Co-authored-by: Michael Rothgang <rothgami@math.hu-berlin.de>

--- Mathlib.lean
+++ Mathlib.lean
@@ -3114,6 +3114,7 @@ import Mathlib.MeasureTheory.Function.StronglyMeasurable.Basic
 import Mathlib.MeasureTheory.Function.StronglyMeasurable.Inner
 import Mathlib.MeasureTheory.Function.StronglyMeasurable.Lemmas
 import Mathlib.MeasureTheory.Function.StronglyMeasurable.Lp
+import Mathlib.MeasureTheory.Function.UnifTight
 import Mathlib.MeasureTheory.Function.UniformIntegrable
 import Mathlib.MeasureTheory.Group.AEStabilizer
 import Mathlib.MeasureTheory.Group.Action
