9123cf2457ce8ee877c14fd423c0c04d47984b57
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2024-07-19 23:08:54 +0200

    linter 100

--- Mathlib/RingTheory/MvPowerSeries/Basic.lean
+++ Mathlib/RingTheory/MvPowerSeries/Basic.lean
@@ -777,7 +777,8 @@ theorem coeff_eq_zero_of_constantCoeff_nilpotent [DecidableEq σ]
   suffices m ≤ s.card by
     obtain ⟨m', hm'⟩ := Nat.exists_eq_add_of_le this
     rw [hm', pow_add, hf, MulZeroClass.zero_mul]
-  rw [← Nat.add_le_add_iff_right, add_comm s.card, Finset.card_sdiff_add_card_eq_card (filter_subset _ _), card_range]
+  rw [← Nat.add_le_add_iff_right, add_comm s.card,
+    Finset.card_sdiff_add_card_eq_card (filter_subset _ _), card_range]
   apply le_trans _ hn
   simp only [add_comm m, Nat.add_le_add_iff_right, ← hk.1,
     ← sum_sdiff (filter_subset _ _ : s ⊆ range n), sum_eq_zero (s := s) hs'', add_zero]
