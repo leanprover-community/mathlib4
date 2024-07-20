934d402ddcde96c42e51d8ef83eaf4e1ec91cd1c
matematiflo <7537413+matematiflo@users.noreply.github.com>
2024-07-20 11:31:34 +0200

    chore: fix formatting in Mathlib.Algebra.Ring.Semireal.Defs module

--- Mathlib/Algebra/Ring/Semireal/Defs.lean
+++ Mathlib/Algebra/Ring/Semireal/Defs.lean
@@ -1,7 +1,7 @@
 /-
- Copyright (c) 2024 Florent Schaffhauser. All rights reserved.
- Released under Apache 2.0 license as described in the file LICENSE.
- Authors: Florent Schaffhauser
+Copyright (c) 2024 Florent Schaffhauser. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Florent Schaffhauser
 -/
 import Mathlib.Algebra.Ring.Defs
 import Mathlib.Algebra.Order.Field.Defs
