58e38c454dde35557f1c0524c18f4e97450c2278
JovanGerb <jovan.gerbscheid@gmail.com>
2024-07-20 01:12:47 +0000

    feat: interactive `unfold?` tactic (#12016)
    This PR defines an interactive unfolding tactic. It suggests a list of unfolds, and when you select one, it pastes a rewrite tactic of the form `rw [show current = unfolded from rfl]`.
    
    This is part of #11768, which is a library rewrite tactic that also suggests the unfolds from this PR.
    
    
    
    Co-authored-by: Jon Eugster <eugster.jon@gmail.com>
    Co-authored-by: JovanGerb <56355248+JovanGerb@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -2768,6 +2768,7 @@ import Mathlib.Lean.Expr
 import Mathlib.Lean.Expr.Basic
 import Mathlib.Lean.Expr.ExtraRecognizers
 import Mathlib.Lean.Expr.ReplaceRec
+import Mathlib.Lean.GoalsLocation
 import Mathlib.Lean.Json
 import Mathlib.Lean.LocalContext
 import Mathlib.Lean.Message
@@ -2775,6 +2776,7 @@ import Mathlib.Lean.Meta
 import Mathlib.Lean.Meta.Basic
 import Mathlib.Lean.Meta.CongrTheorems
 import Mathlib.Lean.Meta.DiscrTree
+import Mathlib.Lean.Meta.KAbstractPositions
 import Mathlib.Lean.Meta.Simp
 import Mathlib.Lean.Name
 import Mathlib.Lean.PrettyPrinter.Delaborator
@@ -4169,6 +4171,7 @@ import Mathlib.Tactic.Widget.CommDiag
 import Mathlib.Tactic.Widget.CongrM
 import Mathlib.Tactic.Widget.Conv
 import Mathlib.Tactic.Widget.GCongr
+import Mathlib.Tactic.Widget.InteractiveUnfold
 import Mathlib.Tactic.Widget.SelectInsertParamsClass
 import Mathlib.Tactic.Widget.SelectPanelUtils
 import Mathlib.Tactic.Zify
