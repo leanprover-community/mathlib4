4ca6f93559a93d68ab1b48637326b6d4034b2743
matematiflo <7537413+matematiflo@users.noreply.github.com>
2024-07-20 11:23:57 +0200

    chore: remove blank line that was messing up CI checks

--- Mathlib/Algebra/Ring/Semireal/Defs.lean
+++ Mathlib/Algebra/Ring/Semireal/Defs.lean
@@ -3,7 +3,6 @@ Copyright (c) 2024 Florent Schaffhauser. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Florent Schaffhauser.
 -/
-
 import Mathlib.Algebra.Ring.Defs
 import Mathlib.Algebra.Order.Field.Defs
 
