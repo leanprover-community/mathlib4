88b9b71380fa08898c219399d9c6453cbac731a6
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2024-07-21 23:55:12 +0200

    import linter

--- Mathlib/RingTheory/MvPowerSeries/Order.lean
+++ Mathlib/RingTheory/MvPowerSeries/Order.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Antoine Chambert-Loir, María Inés de Frutos Fernandez
 -/
 
-import Mathlib.RingTheory.PowerSeries.Basic
+import Mathlib.RingTheory.MvPowerSeries.Basic
 import Mathlib.Data.Finsupp.Weight
 
 /-! # Order of multivariate power series
