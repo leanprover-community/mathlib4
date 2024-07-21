da304111c783cfa4f9524be5e18445609acf7ffd
Michael Rothgang <rothgami@math.hu-berlin.de>
2024-07-21 12:36:35 +0200

    feat: define n-manifolds and closed manifolds
    Define structures `NManifold` and `ClosedManifold` for n-dimensional resp. closed smooth manifolds (i.e., compact without boundary).
    These are useful in their own right, and needed to even state the basic definition in bordism theory.
    
    To show these definitions are workable, we state some standard examples:
    - Euclidean space is an n-manifold
    - the n-sphere is a closed n-manifold
    - the two-torus is a closed 2-manifold
    
    Defining n-manifolds posed a design question, whether to have the n-dimensionality as a typeclass assumption or a proof-valued field.
    The former could avoid unnecessary boilerplate, say when the dimension is ambient and always fixed.
    However, encoding the dimension in a typeclass is inflexible for dimension-changing constructions. Three examples:
    - The product of an n- and an m-manifold is an n+m-manifold: I would prefer that a 2+3 and a 4+1-manifold are the same as a 5-manifold; the typeclass system and addition don't mix well.
    - Bordism theory requires such equalities: a cobordism between two 4-manifolds is a certain 5-manifold;
    the general definition would state this as a 4+1-manifold, which needs to be unified with 5.
    - Surgery between manifolds involves k-handles, the product of a k- and an n-k-manifold (for all k): this uses quite extensively that n=k+(n-k) for all k.
    
    For these reasons, I have decided for the latter option.
    
    I am open to also defining a class IsNManifold and adding an instance SmoothNManifold -> IsNManifold.

---
+++ Mathlib/Geometry/Manifold/ClosedNManifold.lean
@@ -0,0 +1,119 @@
+/-
+Copyright (c) 2024 Michael Rothgang. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Michael Rothgang
+-/
+import Mathlib.Geometry.Manifold.Instances.Real
+import Mathlib.Geometry.Manifold.Instances.Sphere
+import Mathlib.Geometry.Manifold.InteriorBoundary
+
+/-!
+# Closed `n`-dimensional manifolds
+
+We define closed `n`-dimensional manifolds and show a few basic properties.
+
+## Main definitions
+TODO add parameters!
+* `ClosedManifold M I`: a topological manifold `M` is closed if it is compact and boundaryless
+* `NManifold n M I`: an n-manifold is a smooth `n`-dimensional manifold `M`.
+* `ClosedNManifold n M I`: a closed n-manifold `M` is both closed and an `n`-manifold
+
+## Main results
+* `ClosedManifold.prod`: the product of closed manifolds is closed
+* `NManifold.prod`: the product of an `n`-manifold and an `m`-manifold is an `n+m`-manifold
+* `ClosedNManifold.prod`: the product of a closed `n`-manifold and a closed `m`-manifold is a closed
+`n+m`-manifold
+* We prove a few basic examples
+  - the empty set is closed and an `n`-manifold, for every `n` (TODO insert!)
+  - Euclidean n-space is an n-manifold
+  - the standard n-sphere is a closed n-manifold
+  - the standard two-torus `S¹ × S¹` is a closed 2-manifold
+
+TODO: wait for the product results to be merged! investigate why the spheres fail!
+
+-/
+
+open scoped Manifold
+open Metric (sphere)
+open FiniteDimensional Set
+
+noncomputable section
+
+variable (n : ℕ) {𝕜 : Type*} [NontriviallyNormedField 𝕜]
+  -- declare a smooth manifold `M` over the pair `(E, H)`.
+  {E : Type*} [NormedAddCommGroup E] [NormedSpace 𝕜 E] {H : Type*} [TopologicalSpace H]
+  (M : Type*) [TopologicalSpace M] [ChartedSpace H M]
+  (I : ModelWithCorners 𝕜 E H) [SmoothManifoldWithCorners I M]
+
+/-- A topological manifold is called **closed** iff it is compact without boundary. -/
+structure ClosedManifold [CompactSpace M] [BoundarylessManifold I M]
+
+variable {E' : Type*} [NormedAddCommGroup E'] [NormedSpace 𝕜 E']
+  {H' : Type*} [TopologicalSpace H'] (N : Type*) [TopologicalSpace N] [ChartedSpace H' N]
+  (J : ModelWithCorners 𝕜 E' H') [SmoothManifoldWithCorners J N]
+
+-- instance ClosedManifold.prod [CompactSpace M] [BoundarylessManifold I M]
+--     [CompactSpace N] [BoundarylessManifold J N] :
+--   ClosedManifold (M × N) (I.prod J) where
+
+/-- An **n-manifold** is a smooth `n`-dimensional manifold. -/
+structure NManifold [NormedAddCommGroup E]  [NormedSpace 𝕜 E] [FiniteDimensional 𝕜 E]
+    {H : Type*} [TopologicalSpace H] (M : Type*) [TopologicalSpace M] [ChartedSpace H M]
+    (I : ModelWithCorners 𝕜 E H) [SmoothManifoldWithCorners I M] where
+  hdim : finrank 𝕜 E = n
+
+/-- The product of an `n`- and and an `m`-manifold is an `n+m`-manifold. -/
+instance NManifold.prod {m n : ℕ} [FiniteDimensional 𝕜 E] [FiniteDimensional 𝕜 E']
+    (s : NManifold m M I) (t : NManifold n N J) : NManifold (m + n) (M × N) (I.prod J) where
+  hdim := by rw [s.hdim.symm, t.hdim.symm]; apply finrank_prod
+
+structure ClosedNManifold [CompactSpace M] [BoundarylessManifold I M] [FiniteDimensional 𝕜 E]
+    extends NManifold n M I
+
+instance ClosedNManifold.ClosedManifold [CompactSpace M] [BoundarylessManifold I M] [FiniteDimensional 𝕜 E] :
+  ClosedManifold M I where
+
+variable {n}
+
+-- /-- The product of a closed `n`- and a closed closed `m`-manifold is a closed `n+m`-manifold. -/
+-- instance ClosedNManifold.prod {m n : ℕ} [FiniteDimensional 𝕜 E] [FiniteDimensional 𝕜 E']
+--     [CompactSpace M] [BoundarylessManifold I M] [CompactSpace N] [BoundarylessManifold J N]
+--     (s : ClosedNManifold m M I) (t : ClosedNManifold n N J) :
+--     ClosedNManifold (m + n) (M × N) (I.prod J) where
+--   -- TODO: can I inherit this from `NManifold.prod`?
+--   hdim := by rw [s.hdim.symm, t.hdim.symm]; apply finrank_prod
+
+section examples
+
+-- Let `E` be a finite-dimensional real normed space.
+variable {E : Type*} [NormedAddCommGroup E] [NormedSpace ℝ E]
+
+-- TODO: move the empty manifold here, once its definition is in a separate file
+
+/- TODO: these two examples worked when ClosedManifold only demanded `I.Boundaryless`;
+-- diagnose and fix this!
+/-- The standard `n`-sphere is a closed manifold. -/
+example {n : ℕ} [FiniteDimensional ℝ E] [Fact (finrank ℝ E = n + 1)] :
+  ClosedManifold (sphere (0 : E) 1) (𝓡 n) where
+/-- The standard `2`-torus is a closed manifold. -/
+example [FiniteDimensional ℝ E] [Fact (finrank ℝ E = 1 + 1)] :
+    ClosedManifold ((sphere (0 : E) 1) × (sphere (0 : E) 1)) ((𝓡 2).prod (𝓡 2)) where
+-/
+
+-- The standard Euclidean space is an `n`-manifold. -/
+example {n : ℕ} {M : Type*} [TopologicalSpace M] [ChartedSpace (EuclideanSpace ℝ (Fin n)) M]
+    [SmoothManifoldWithCorners (𝓡 n) M] : NManifold n M (𝓡 n) where
+  hdim := finrank_euclideanSpace_fin
+
+variable {F : Type*} [NormedAddCommGroup F] [InnerProductSpace ℝ F] [FiniteDimensional ℝ F]
+
+/-- The standard `n`-sphere is a closed `n`-manifold. -/
+example {n : ℕ} [Fact (finrank ℝ F = n + 1)] : ClosedNManifold n (sphere (0 : F) 1) (𝓡 n) where
+  hdim := finrank_euclideanSpace_fin
+
+/-- The standard 2-torus is a closed two-manifold. -/
+example [Fact (finrank ℝ F = 1 + 1)] :
+    ClosedNManifold 2 ((sphere (0 : F) 1) × (sphere (0 : F) 1)) ((𝓡 1).prod (𝓡 1)) where
+  hdim := by rw [finrank_prod, finrank_euclideanSpace_fin]
+
+end examples
