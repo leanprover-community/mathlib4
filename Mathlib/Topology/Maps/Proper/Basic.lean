3d66557641ddb3080eff37f4a335acdc2b441cc7
Etienne Marion <etienne.marion@ens-lyon.fr>
2024-07-21 10:23:56 +0200

    Solve build issue

--- Mathlib/Topology/Maps/Proper/Basic.lean
+++ Mathlib/Topology/Maps/Proper/Basic.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Anatole Dedecker, Etienne Marion
 -/
 import Mathlib.Topology.Homeomorph
-import Mathlib.Topology.StoneCech
 import Mathlib.Topology.Filter
 import Mathlib.Order.Filter.Cofinite
 import Mathlib.Topology.Defs.Sequences
