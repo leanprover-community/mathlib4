ece35d76514e96976701a8132104dbca08e62fcb
Michael Rothgang <rothgami@math.hu-berlin.de>
2024-07-21 19:38:41 +0200

    Last fix; one proof is rather tricky.

--- Mathlib/Data/Finsupp/Weight.lean
+++ Mathlib/Data/Finsupp/Weight.lean
@@ -158,8 +158,9 @@ theorem weight_eq_zero_iff_eq_zero
     simp only [Finsupp.coe_zero, Pi.zero_apply]
     by_contra hs
     apply NonTorsionWeight.ne_zero w _
-    rw [← nonpos_iff_eq_zero, ← h]
-    exact le_weight_of_ne_zero' w hs
+    -- TODO: this is subtle, but I'm not sure what to best say in a comment...
+    · rw [← nonpos_iff_eq_zero, ← h]
+      exact le_weight_of_ne_zero' w hs
   · intro h
     rw [h, map_zero]
 
