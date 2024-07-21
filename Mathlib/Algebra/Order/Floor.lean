52e2fae3d465935da1ae091ac89619109ba1f230
Colin Jones <137637335+Colin166@users.noreply.github.com>
2024-07-20 22:10:07 -0400

    Update Floor.lean

--- Mathlib/Algebra/Order/Floor.lean
+++ Mathlib/Algebra/Order/Floor.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro, Kevin Kappelmann
 -/
 import Mathlib.Algebra.CharZero.Lemmas
-import Mathlib.Algebra.EuclideanDomain.Instances
 import Mathlib.Algebra.Order.Interval.Set.Group
 import Mathlib.Algebra.Group.Int
 import Mathlib.Data.Int.Lemmas
@@ -15,6 +14,7 @@ import Mathlib.Order.GaloisConnection
 import Mathlib.Tactic.Abel
 import Mathlib.Tactic.Linarith
 import Mathlib.Tactic.Positivity
+import Mathlib.Tactic.Ring
 
 /-!
 # Floor and ceil
