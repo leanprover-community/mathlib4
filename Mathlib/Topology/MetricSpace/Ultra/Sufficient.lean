d4dc5487fb00c59fee4197791729464a47670b26
Yakov Pechersky <ypechersky@treeline.bio>
2024-07-23 17:56:44 -0400

    remove unnecessary import

--- Mathlib/Topology/MetricSpace/Ultra/Sufficient.lean
+++ Mathlib/Topology/MetricSpace/Ultra/Sufficient.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yakov Pechersky
 -/
 import Mathlib.Topology.MetricSpace.Ultra.Normed
-import Mathlib.Analysis.SpecialFunctions.Pow.Real
 
 /-!
 ## Sufficient contidition to have an ultrametric norm
