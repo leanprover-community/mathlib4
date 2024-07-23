0975ab8edf53bb56198be76087b6a4fb25a295a6
Michael Rothgang <rothgami@math.hu-berlin.de>
2024-07-23 12:14:28 +0200

    more

--- scripts/lint-style.sh
+++ scripts/lint-style.sh
@@ -9,9 +9,6 @@ set -exo pipefail
 # A Python script `scripts/lint-style.py` lints the contents of a Lean file.
 # This script is called below on all Lean files in the repository.
 # Exceptions are maintained in `scripts/style-exceptions.txt`.
-# (Rewriting these checks in Lean is work in progress.)
-#
-# TODO: This is adapted from the linter for mathlib3. It should be rewritten in Lean.
 
 ################################################################################
 
