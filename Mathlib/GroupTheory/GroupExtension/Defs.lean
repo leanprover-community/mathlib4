604d8e066654af32d52e485face5cb75811107c2
yu-yama <yu-yama@users.noreply.github.com>
2024-07-23 11:43:46 +0100

    chore: add full stops to comments in full sentences

--- Mathlib/GroupTheory/GroupExtension/Defs.lean
+++ Mathlib/GroupTheory/GroupExtension/Defs.lean
@@ -18,7 +18,7 @@ equivalences.
 - `GroupExtension N E G`: structure for extensions of `G` by `N` as short exact sequences
   `1 → N → E → G → 1`
 - `GroupExtension.Equiv S S'`: structure for equivalences of two group extensions `S` and `S'` as
-  specific homomorphisms `E → E'` such that the diagram below is commutative.
+  specific homomorphisms `E → E'` such that the diagram below is commutative
 
 ```text
       ↗︎ E  ↘
@@ -44,17 +44,17 @@ If `N` is Abelian,
 
 variable (N E G : Type*) [Group N] [Group E] [Group G]
 
-/-- `GroupExtension N E G` is a short exact sequence of groups `1 → N → E → G → 1` -/
+/-- `GroupExtension N E G` is a short exact sequence of groups `1 → N → E → G → 1`. -/
 structure GroupExtension where
   /-- The inclusion homomorphism `N →* E` -/
   inl : N →* E
   /-- The projection homomorphism `E →* G` -/
   rightHom : E →* G
-  /-- The inclusion map is injective -/
+  /-- The inclusion map is injective. -/
   inl_injective : Function.Injective inl
-  /-- The range of the inclusion map is equal to the kernel of the projection map -/
+  /-- The range of the inclusion map is equal to the kernel of the projection map. -/
   range_inl_eq_ker_rightHom : inl.range = rightHom.ker
-  /-- The projection map is surjective -/
+  /-- The projection map is surjective. -/
   rightHom_surjective : Function.Surjective rightHom
 
 variable {N E G}
@@ -63,7 +63,7 @@ namespace GroupExtension
 
 variable (S : GroupExtension N E G)
 
-/-- The range of the inclusion map is a normal subgroup -/
+/-- The range of the inclusion map is a normal subgroup. -/
 instance normal_inl_range : (S.inl.range).Normal :=
   S.range_inl_eq_ker_rightHom ▸ S.rightHom.normal_ker
 
@@ -76,7 +76,7 @@ theorem rightHom_comp_inl : S.rightHom.comp S.inl = 1 := by
   rw [MonoidHom.one_apply, MonoidHom.comp_apply]
   exact S.rightHom_inl n
 
-/-- `E` acts on `N` by conjugation -/
+/-- `E` acts on `N` by conjugation. -/
 noncomputable def conjAct : E →* MulAut N := {
   toFun := fun e ↦ (MonoidHom.ofInjective S.inl_injective).trans <|
     (MulAut.conjNormal e).trans (MonoidHom.ofInjective S.inl_injective).symm
@@ -88,26 +88,26 @@ noncomputable def conjAct : E →* MulAut N := {
     simp only [map_mul, MulEquiv.trans_apply, MulAut.mul_apply, MulEquiv.apply_symm_apply]
 }
 
-/-- The inclusion and a conjugation commute -/
+/-- The inclusion and a conjugation commute. -/
 theorem inl_conjAct_comm {e : E} {n : N} : S.inl (S.conjAct e n) = e * S.inl n * e⁻¹ := by
   simp only [conjAct, MonoidHom.coe_mk, OneHom.coe_mk, MulEquiv.trans_apply,
     MonoidHom.apply_ofInjective_symm]
   rfl
 
-/-- `GroupExtension`s are equivalent iff there is a homomorphism making a commuting diagram -/
+/-- `GroupExtension`s are equivalent iff there is a homomorphism making a commuting diagram. -/
 structure Equiv {E' : Type*} [Group E'] (S' : GroupExtension N E' G) where
   /-- The homomorphism -/
   toMonoidHom : E →* E'
-  /-- The left-hand side of the diagram commutes -/
+  /-- The left-hand side of the diagram commutes. -/
   inl_comm : toMonoidHom.comp S.inl = S'.inl
-  /-- The right-hand side of the diagram commutes -/
+  /-- The right-hand side of the diagram commutes. -/
   rightHom_comm : S'.rightHom.comp toMonoidHom = S.rightHom
 
-/-- `Splitting` of a group extension is a section homomorphism -/
+/-- `Splitting` of a group extension is a section homomorphism. -/
 structure Splitting where
   /-- A section homomorphism -/
   sectionHom : G →* E
-  /-- The section is a left inverse of the projection map -/
+  /-- The section is a left inverse of the projection map. -/
   rightHom_comp_sectionHom : S.rightHom.comp sectionHom = MonoidHom.id G
 
 instance : FunLike (S.Splitting) G E where
