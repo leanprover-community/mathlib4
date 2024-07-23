a27da596ec6e6a91dfdf659ce197db941d5996cb
Kim Morrison <kim@tqft.net>
2024-07-24 09:24:17 +1000

    fix

--- Mathlib/AlgebraicGeometry/Scheme.lean
+++ Mathlib/AlgebraicGeometry/Scheme.lean
@@ -151,7 +151,7 @@ noncomputable def homeomorph [IsIso f] : X ≃ₜ Y :=
 protected lemma ext {f g : X ⟶ Y} (h_base : f.1.base = g.1.base)
     (h_app : ∀ U, f.app U ≫ X.presheaf.map
       (eqToHom congr((Opens.map $h_base.symm).obj U)).op = g.app U) : f = g :=
-  LocallyRingedSpace.Hom.ext _ _ <| SheafedSpace.ext h_base
+  LocallyRingedSpace.Hom.ext <| SheafedSpace.ext _ _ h_base
     (TopCat.Presheaf.ext fun U ↦ by simpa using h_app U)
 
 lemma preimage_iSup {ι} (U : ι → Opens Y) : f ⁻¹ᵁ iSup U = ⨆ i, f ⁻¹ᵁ U i :=
