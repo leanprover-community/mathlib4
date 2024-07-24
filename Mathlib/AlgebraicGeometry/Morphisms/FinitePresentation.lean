233ae27a4c35026743822b77e6c3f858f2988738
Christian Merten <christian@merten.dev>
2024-07-24 12:44:05 +0200

    fix shake

--- Mathlib/AlgebraicGeometry/Morphisms/FinitePresentation.lean
+++ Mathlib/AlgebraicGeometry/Morphisms/FinitePresentation.lean
@@ -3,7 +3,7 @@ Copyright (c) 2024 Christian Merten. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Christian Merten
 -/
-import Mathlib.AlgebraicGeometry.Morphisms.FiniteType
+import Mathlib.AlgebraicGeometry.Morphisms.RingHomProperties
 import Mathlib.RingTheory.RingHom.FinitePresentation
 
 /-!
