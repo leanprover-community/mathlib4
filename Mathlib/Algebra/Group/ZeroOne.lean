920c97f04588f9d258d3bb0084b9c2626d69e0d0
negiizhao <zhao.yu-yang@foxmail.com>
2024-07-24 14:53:23 +0800

    fix

--- Mathlib/Algebra/Group/ZeroOne.lean
+++ Mathlib/Algebra/Group/ZeroOne.lean
@@ -9,13 +9,13 @@ import Mathlib.Tactic.ToAdditive
 ## Classes for `Zero` and `One`
 -/
 
-class Zero.{u} (α : Type u) where
+universe u
+
+class Zero (α : Type u) where
   zero : α
 
 instance (priority := 300) Zero.toOfNat0 {α} [Zero α] : OfNat α (nat_lit 0) where
   ofNat := ‹Zero α›.1
-
-universe u
 
 @[to_additive]
 class One (α : Type u) where
@@ -26,3 +26,13 @@ instance (priority := 300) One.toOfNat1 {α} [One α] : OfNat α (nat_lit 1) whe
   ofNat := ‹One α›.1
 
 attribute [to_additive_change_numeral 2] OfNat OfNat.ofNat
+
+namespace Nat
+
+instance instZero : Zero Nat where
+  zero := 0
+
+instance instOne : One Nat where
+  one := 1
+
+end Nat
