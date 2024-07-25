de49f39eb0c7a2beff4256519be190e828fa1b1b
Andrew Yang <the.erd.one@gmail.com>
2024-07-25 23:15:17 +0000

    feat(AlgebraicGeometry): Define affine morphisms (#13996)
    Co-authored-by: Andrew Yang <36414270+erdOne@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -794,6 +794,7 @@ import Mathlib.AlgebraicGeometry.Limits
 import Mathlib.AlgebraicGeometry.Modules.Presheaf
 import Mathlib.AlgebraicGeometry.Modules.Sheaf
 import Mathlib.AlgebraicGeometry.Modules.Tilde
+import Mathlib.AlgebraicGeometry.Morphisms.Affine
 import Mathlib.AlgebraicGeometry.Morphisms.Basic
 import Mathlib.AlgebraicGeometry.Morphisms.ClosedImmersion
 import Mathlib.AlgebraicGeometry.Morphisms.Constructors
