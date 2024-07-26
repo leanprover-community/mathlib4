8f1d684a1aa22474c3a7041c300cf2f6952ef738
Joël Riou <joel.riou@universite-paris-saclay.fr>
2024-07-26 21:56:57 +0530

    fixed ConnectedComponents.lean

--- Mathlib/CategoryTheory/ConnectedComponents.lean
+++ Mathlib/CategoryTheory/ConnectedComponents.lean
@@ -126,11 +126,11 @@ instance : (decomposedTo J).Full where
       rw [← hX, ← hY, Quotient.eq'']
       exact Relation.ReflTransGen.single (Or.inl ⟨f⟩)
     subst this
-    exact ⟨Sigma.SigmaHom.mk f, rfl⟩
+    exact ⟨Sigma.SigmaHom.mk { hom := f }, rfl⟩
 
 instance : (decomposedTo J).Faithful where
   map_injective := by
-    rintro ⟨_, j, rfl⟩ ⟨_, k, hY⟩ ⟨f⟩ ⟨_⟩ rfl
+    rintro ⟨_, j, rfl⟩ ⟨_, k, hY⟩ ⟨⟨f⟩⟩ ⟨⟨g⟩⟩ rfl
     rfl
 
 instance : (decomposedTo J).EssSurj where mem_essImage j := ⟨⟨_, j, rfl⟩, ⟨Iso.refl _⟩⟩
