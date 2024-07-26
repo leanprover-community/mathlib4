63494be94200393a18a15a192309ff813d0562f5
Rida Hamadani <mridahamadani@gmail.com>
2024-07-26 20:20:30 +0300

    fix imports

--- Mathlib/Combinatorics/SimpleGraph/Connectivity.lean
+++ Mathlib/Combinatorics/SimpleGraph/Connectivity.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kyle Miller
 -/
 import Mathlib.Combinatorics.SimpleGraph.Walk
+import Mathlib.Combinatorics.SimpleGraph.Subgraph
 
 /-!
 
