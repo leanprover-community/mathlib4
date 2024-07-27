bbf665656637a930ff86c28d4415ab8a0b248e74
Daniel Weber <55664973+Command-Master@users.noreply.github.com>
2024-07-27 07:49:37 +0300

    Update Mathlib/Topology/Perfect.lean
    Co-authored-by: Jireh Loreaux <loreaujy@gmail.com>

--- Mathlib/Topology/Perfect.lean
+++ Mathlib/Topology/Perfect.lean
@@ -183,9 +183,9 @@ lemma IsPreconnected.preperfect_of_nontrivial [T1Space α] {U : Set α} (hu : U.
     simp only [Set.mem_inter_iff, hy, true_and]
     apply subset_closure
     simp [hy]
-  apply Set.Nonempty.right at h
-  rw [Set.singleton_inter_nonempty, mem_closure_iff_clusterPt, ← acc_principal_iff_cluster] at h
-  exact h
+  · apply Set.Nonempty.right at h
+    rw [Set.singleton_inter_nonempty, mem_closure_iff_clusterPt, ← acc_principal_iff_cluster] at h
+    exact h
 
 end Preperfect
 
