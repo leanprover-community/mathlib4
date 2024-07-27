72ad92d4b328471e91aca7481a7de0e48d0fdebd
Blizzard_inc <thrashy4inbox@gmail.com>
2024-07-27 18:17:28 +0200

    add tsum_const lemma

---
+++ Mathlib/Topology/Algebra/InfiniteSum/ENNReal.lean
@@ -0,0 +1,48 @@
+import Mathlib.Data.Real.ENatENNReal
+import Mathlib.Data.Set.Card
+import Mathlib.Topology.Instances.ENNReal
+
+open Set Function
+namespace ENNReal
+
+lemma tsum_one_eq' {α : Type*} (s : Set α) : ∑' (_:s), (1 : ℝ≥0∞) = s.encard := by
+  if hfin : s.Finite then
+    have hfin' : Finite s := by exact hfin
+    rw [tsum_def]
+    simp only [ENNReal.summable, ↓reduceDIte]
+    have hsup: support (fun (_ : s) ↦ (1 : ℝ≥0∞)) = Set.univ := by
+      ext i
+      simp only [mem_support, ne_eq, one_ne_zero, not_false_eq_true, mem_univ]
+    have hsupfin: (Set.univ : Set s).Finite := by exact finite_univ
+    rw [← hsup] at hsupfin
+    rw [if_pos hsupfin]
+    rw [hfin.encard_eq_coe_toFinset_card]
+    simp only [ENat.toENNReal_coe]
+    rw [Finset.card_eq_sum_ones]
+    rw [finsum_eq_sum (fun (_ : s) ↦ (1 :ℝ≥0∞)) hsupfin]
+    simp only [Finset.sum_const, nsmul_eq_mul, mul_one, smul_eq_mul, Nat.cast_inj]
+    apply Finset.card_bij (fun a _ => a.val)
+    · intro a
+      simp only [Finite.mem_toFinset, mem_support, ne_eq, one_ne_zero, not_false_eq_true,
+        Subtype.coe_prop, imp_self]
+    · intro a _ a' _ heq
+      ext
+      exact heq
+    · intro a ha
+      use ⟨a,by
+        simp only [Finite.mem_toFinset] at ha
+        exact ha⟩
+      simp only [Finite.mem_toFinset, mem_support, ne_eq, one_ne_zero, not_false_eq_true,
+        exists_const]
+  else
+  have : Infinite s := by exact infinite_coe_iff.mpr hfin
+  rw [tsum_const_eq_top_of_ne_zero (by norm_num)]
+  rw [Set.encard_eq_top_iff.mpr hfin]
+  simp only [ENat.toENNReal_top]
+
+lemma tsum_const_eq' {α : Type*} (s : Set α) (c : ℝ≥0∞) :
+    ∑' (_:s), (c : ℝ≥0∞) = s.encard * c := by
+  nth_rw 1 [← one_mul c]
+  rw [ENNReal.tsum_mul_right,tsum_one_eq']
+
+end ENNReal
