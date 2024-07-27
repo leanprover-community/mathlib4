7c2f150bb085ba9cc727014a1a85ee553ab5f196
Kim Morrison <kim@tqft.net>
2024-07-27 10:53:00 +1000

    shake

--- Mathlib/Data/List/Join.lean
+++ Mathlib/Data/List/Join.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Sébastien Gouëzel, Floris van Doorn, Mario Carneiro, Martin Dvorak
 -/
 import Mathlib.Data.List.Basic
-import Batteries.Data.Nat.Lemmas
 
 /-!
 # Join of a list of lists
