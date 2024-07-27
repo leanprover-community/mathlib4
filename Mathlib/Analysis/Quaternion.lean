918839ed6134ed33f4f9b59f73f7142df3125e4e
grunweg <rothgami@math.hu-berlin.de>
2024-07-27 23:01:29 +0000

    chore: remove some obsolete porting notes mentioning noncomputable (#15172)
    - Two porting notes are obsolete: the `noncomputable` modifier could simply be removed.
    - A few others don't seem actionable.
    - lean4#2077 is fixed now: no point referencing this issue any more
    (The `noncomputable` needs to stay, as the definition relies on other noncomputable items.)

--- Mathlib/Analysis/Quaternion.lean
+++ Mathlib/Analysis/Quaternion.lean
@@ -86,7 +86,6 @@ noncomputable instance : NormedDivisionRing ℍ where
     simp only [norm_eq_sqrt_real_inner, inner_self, normSq.map_mul]
     exact Real.sqrt_mul normSq_nonneg _
 
--- Porting note: added `noncomputable`
 noncomputable instance : NormedAlgebra ℝ ℍ where
   norm_smul_le := norm_smul_le
   toAlgebra := Quaternion.algebra
