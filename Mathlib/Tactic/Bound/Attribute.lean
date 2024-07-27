6e23e1fca9a98fddcebbc80efdb249149492a918
Geoffrey Irving <irving@naml.us>
2024-07-27 16:25:10 +0100

    Fix ZeroOne import

--- Mathlib/Tactic/Bound/Attribute.lean
+++ Mathlib/Tactic/Bound/Attribute.lean
@@ -3,7 +3,7 @@ Copyright (c) 2024 Geoffrey Irving. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Geoffrey Irving
 -/
-import Mathlib.Init.ZeroOne
+import Mathlib.Algebra.Group.ZeroOne
 import Mathlib.Tactic.Bound.Init
 import Qq
 
