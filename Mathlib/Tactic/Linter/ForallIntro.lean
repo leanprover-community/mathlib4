da68d019ed7e84919a067ff23726d9f4ddcb79fe
adomani <adomani@gmail.com>
2024-07-27 22:32:52 +0200

    copyright and module docs

--- Mathlib/Tactic/Linter/ForallIntro.lean
+++ Mathlib/Tactic/Linter/ForallIntro.lean
@@ -1,5 +1,15 @@
+/-
+Copyright (c) 2024 Damiano Testa. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Johan Commelin, Damiano Testa
+-/
+
 import Lean.Elab.Command
 import Lean.Linter.Util
+
+/-!
+# Collapse `∀`/`intro` in `have`
+-/
 
 namespace Lean.Syntax
 /-!
