9fe56ecbbc7f02aa0e00e708b20f04601d5b9252
negiizhao <zhao.yu-yang@foxmail.com>
2024-07-27 17:56:54 +0800

    feat: add some basic term elaborators

--- Mathlib.lean
+++ Mathlib.lean
@@ -4600,7 +4600,8 @@ import Mathlib.Util.SleepHeartbeats
 import Mathlib.Util.Superscript
 import Mathlib.Util.SynthesizeUsing
 import Mathlib.Util.Tactic
-import Mathlib.Util.TermBeta
+import Mathlib.Util.Term.Basic
+import Mathlib.Util.Term.Beta
 import Mathlib.Util.Time
 import Mathlib.Util.WhatsNew
 import Mathlib.Util.WithWeakNamespace
--- Mathlib/Tactic/IrreducibleDef.lean
+++ Mathlib/Tactic/IrreducibleDef.lean
@@ -4,8 +4,9 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Gabriel Ebner
 -/
 import Lean
-import Mathlib.Tactic.Eqns
 import Mathlib.Data.Subtype
+import Mathlib.Tactic.Eqns
+import Mathlib.Util.Term.Basic
 
 /-!
 # Irreducible definitions
