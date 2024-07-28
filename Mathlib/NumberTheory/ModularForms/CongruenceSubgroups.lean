30381d0135ac77e4bef31b3b760c0f855a8620aa
Michael Rothgang <rothgami@math.hu-berlin.de>
2024-07-28 12:12:08 +0200

    Fourty

--- Mathlib/NumberTheory/ModularForms/CongruenceSubgroups.lean
+++ Mathlib/NumberTheory/ModularForms/CongruenceSubgroups.lean
@@ -127,7 +127,7 @@ theorem Gamma0_mem (N : ℕ) (A : SL(2, ℤ)) : A ∈ Gamma0 N ↔ ((↑ₘA 1 0
 theorem Gamma0_det (N : ℕ) (A : Gamma0 N) : (A.1.1.det : ZMod N) = 1 := by simp [A.1.property]
 #align Gamma0_det CongruenceSubgroup.Gamma0_det
 
-/-- The group homomorphism from `Gamma0` to `ZMod N` given by mapping a matrix to its lower
+/-- The group homomorphism from `CongruenceSubgroup.Gamma0` to `ZMod N` given by mapping a matrix to its lower
 right-hand entry. -/
 def Gamma0Map (N : ℕ) : Gamma0 N →* ZMod N where
   toFun g := ((↑ₘg 1 1 : ℤ) : ZMod N)
@@ -144,7 +144,7 @@ def Gamma0Map (N : ℕ) : Gamma0 N →* ZMod N where
     simp
 #align Gamma_0_map CongruenceSubgroup.Gamma0Map
 
-/-- The congruence subgroup `Gamma1` (as a subgroup of `Gamma0`) of matrices whose bottom
+/-- The congruence subgroup `CongruenceSubgroup.Gamma1` (as a subgroup of `CongruenceSubgroup.Gamma0`) of matrices whose bottom
 row is congruent to `(0,1)` modulo `N`. -/
 def Gamma1' (N : ℕ) : Subgroup (Gamma0 N) :=
   (Gamma0Map N).ker
@@ -174,7 +174,7 @@ theorem Gamma1_to_Gamma0_mem (N : ℕ) (A : Gamma0 N) : A ∈ Gamma1' N ↔
     exact ha.2.1
 #align Gamma1_to_Gamma0_mem CongruenceSubgroup.Gamma1_to_Gamma0_mem
 
-/-- The congruence subgroup `Gamma1` of `SL(2, ℤ)` consisting of matrices whose bottom
+/-- The congruence subgroup `CongruenceSubgroup.Gamma1` of `SL(2, ℤ)` consisting of matrices whose bottom
 row is congruent to `(0,1)` modulo `N`. -/
 def Gamma1 (N : ℕ) : Subgroup SL(2, ℤ) :=
   Subgroup.map ((Gamma0 N).subtype.comp (Gamma1' N).subtype) ⊤
@@ -210,7 +210,7 @@ theorem Gamma1_in_Gamma0 (N : ℕ) : Gamma1 N ≤ Gamma0 N := by
 
 section CongruenceSubgroups
 
-/-- A congruence subgroup is a subgroup of `SL(2, ℤ)` which contains some `Gamma N` for some
+/-- A congruence subgroup is a subgroup of `SL(2, ℤ)` which contains some `CongruenceSubgroup.Gamma N` for some
 `(N : ℕ+)`. -/
 def IsCongruenceSubgroup (Γ : Subgroup SL(2, ℤ)) : Prop :=
   ∃ N : ℕ+, Gamma N ≤ Γ
