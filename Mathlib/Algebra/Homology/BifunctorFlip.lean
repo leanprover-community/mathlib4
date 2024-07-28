aaac00e658b1d6afb3fd1274daa20ff94c5f0952
Joël Riou <joel.riou@universite-paris-saclay.fr>
2024-07-28 16:52:28 +0530

    fixing imports

--- Mathlib/Algebra/Homology/BifunctorFlip.lean
+++ Mathlib/Algebra/Homology/BifunctorFlip.lean
@@ -3,7 +3,7 @@ Copyright (c) 2024 Joël Riou. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joël Riou
 -/
-import Mathlib.Algebra.Homology.BifunctorHomotopy
+import Mathlib.Algebra.Homology.Bifunctor
 import Mathlib.Algebra.Homology.TotalComplexSymmetry
 
 /-!
--- Mathlib/Algebra/Homology/TotalComplexSymmetry.lean
+++ Mathlib/Algebra/Homology/TotalComplexSymmetry.lean
@@ -3,7 +3,7 @@ Copyright (c) 2024 Joël Riou. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joël Riou
 -/
-import Mathlib.Algebra.Homology.TotalComplexShift
+import Mathlib.Algebra.Homology.TotalComplex
 
 /-! The symmetry of the total complex of a bicomplex
 
