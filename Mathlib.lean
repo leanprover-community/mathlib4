7413855d08df994c496f932ed2999eff9ea380d6
Anthony Bordg <bordg.anthony@gmail.com>
2024-07-28 15:30:42 +0200

    remove previous file

--- Mathlib.lean
+++ Mathlib.lean
@@ -1776,6 +1776,7 @@ import Mathlib.CategoryTheory.Sites.Spaces
 import Mathlib.CategoryTheory.Sites.Subsheaf
 import Mathlib.CategoryTheory.Sites.Types
 import Mathlib.CategoryTheory.Sites.Whiskering
+import Mathlib.CategoryTheory.Sites.«CoveringFamilies - Copy»
 import Mathlib.CategoryTheory.Skeletal
 import Mathlib.CategoryTheory.SmallObject.Construction
 import Mathlib.CategoryTheory.SmallObject.Iteration
