c10858039cda90fa5505c6f3bcc709f01380b6ce
Yaël Dillies <yael.dillies@gmail.com>
2024-07-28 15:48:24 +0000

    chore(Init/Order): Rework files Merge `Init.Order.Defs`,  `Init.Order.Lemmas` and the part of `Init.Algebra.Classes` about relations to a new file `Order.Defs` * Delete the unused commented out classes from `Init.Algebra.Classes` * Golf proofs

--- Mathlib.lean
+++ Mathlib.lean
@@ -2820,7 +2820,7 @@ import Mathlib.Init.Data.List.Lemmas
 import Mathlib.Init.Data.Nat.Lemmas
 import Mathlib.Init.Data.Quot
 import Mathlib.Init.Logic
-import Mathlib.Init.Order.LinearOrder
+import Mathlib.Init.Order.Defs
 import Mathlib.Init.Quot
 import Mathlib.Lean.CoreM
 import Mathlib.Lean.Elab.Tactic.Basic
