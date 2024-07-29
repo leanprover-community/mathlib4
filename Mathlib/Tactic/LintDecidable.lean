911240ca6dc1c3f4c4814a97cca1167426c0f72b
Yury G. Kudryashov <urkud@urkud.name>
2024-07-29 15:36:29 -0400

    Make style linter happy

--- Mathlib/Tactic/LintDecidable.lean
+++ Mathlib/Tactic/LintDecidable.lean
@@ -1,5 +1,14 @@
+/-
+Copyright (c) 2024 Yury Kudryashov. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Yury Kudryashov
+-/
 import Batteries.Tactic.Lint
 import Mathlib.Lean.Expr.Basic
+
+/-!
+# Linter for `Decidable`/`Encodable`/`Inhabited`
+-/
 
 namespace Std.Tactic.Lint
 open Lean Meta
