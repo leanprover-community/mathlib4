a9d567da622586dd083f820432372517b6cee63b
Christian Merten <christian@merten.dev>
2024-07-29 18:29:50 +0000

    refactor(AlgebraicGeometry): stalk maps (#15070)
    Introduces `Scheme.Hom.stalkMap` and `LocallyRingedSpace.Hom.stalkMap` with the API copied from presheafed spaces. Removes `LocallyRingedSpace.stalk` and `LocallyRingedSpace.ΓToStalk`. Instead uses `Presheaf.stalk` and `Presheaf.germ` directly.
    
    
    
    Co-authored-by: Andrew Yang <36414270+erdOne@users.noreply.github.com>
    Co-authored-by: Andrew Yang <the.erd.one@gmail.com>
    Co-authored-by: erdOne <the.erd.one@gmail.com>

--- Mathlib/AlgebraicGeometry/Morphisms/ClosedImmersion.lean
+++ Mathlib/AlgebraicGeometry/Morphisms/ClosedImmersion.lean
@@ -40,7 +40,7 @@ topological map is a closed embedding and the induced stalk maps are surjective.
 @[mk_iff]
 class IsClosedImmersion {X Y : Scheme} (f : X ⟶ Y) : Prop where
   base_closed : ClosedEmbedding f.1.base
-  surj_on_stalks : ∀ x, Function.Surjective (PresheafedSpace.stalkMap f.1 x)
+  surj_on_stalks : ∀ x, Function.Surjective (f.stalkMap x)
 
 namespace IsClosedImmersion
 
@@ -49,7 +49,7 @@ lemma closedEmbedding {X Y : Scheme} (f : X ⟶ Y)
   IsClosedImmersion.base_closed
 
 lemma surjective_stalkMap {X Y : Scheme} (f : X ⟶ Y)
-    [IsClosedImmersion f] (x : X) : Function.Surjective (PresheafedSpace.stalkMap f.1 x) :=
+    [IsClosedImmersion f] (x : X) : Function.Surjective (f.stalkMap x) :=
   IsClosedImmersion.surj_on_stalks x
 
 lemma eq_inf : @IsClosedImmersion = (topologically ClosedEmbedding) ⊓
@@ -67,7 +67,7 @@ instance : MorphismProperty.IsMultiplicative @IsClosedImmersion where
   id_mem _ := inferInstance
   comp_mem {X Y Z} f g hf hg := by
     refine ⟨hg.base_closed.comp hf.base_closed, fun x ↦ ?_⟩
-    erw [PresheafedSpace.stalkMap.comp]
+    rw [Scheme.stalkMap_comp]
     exact (hf.surj_on_stalks x).comp (hg.surj_on_stalks (f.1.1 x))
 
 /-- Composition of closed immersions is a closed immersion. -/
@@ -86,13 +86,13 @@ theorem spec_of_surjective {R S : CommRingCat} (f : R ⟶ S) (h : Function.Surje
     IsClosedImmersion (Spec.map f) where
   base_closed := PrimeSpectrum.closedEmbedding_comap_of_surjective _ _ h
   surj_on_stalks x := by
-    erw [← localRingHom_comp_stalkIso, CommRingCat.coe_comp, CommRingCat.coe_comp]
-    apply Function.Surjective.comp (Function.Surjective.comp _ _) _
-    · exact (ConcreteCategory.bijective_of_isIso (StructureSheaf.stalkIso S x).inv).2
-    · exact surjective_localRingHom_of_surjective f h x.asIdeal
-    · let g := (StructureSheaf.stalkIso ((CommRingCat.of R))
-        ((PrimeSpectrum.comap (CommRingCat.ofHom f)) x)).hom
-      exact (ConcreteCategory.bijective_of_isIso g).2
+    haveI : (RingHom.toMorphismProperty (fun f ↦ Function.Surjective f)).RespectsIso := by
+      rw [← RingHom.toMorphismProperty_respectsIso_iff]
+      exact surjective_respectsIso
+    apply (MorphismProperty.arrow_mk_iso_iff
+      (RingHom.toMorphismProperty (fun f ↦ Function.Surjective f))
+      (Scheme.arrowStalkMapSpecIso f x)).mpr
+    exact surjective_localRingHom_of_surjective f h x.asIdeal
 
 /-- For any ideal `I` in a commutative ring `R`, the quotient map `specObj R ⟶ specObj (R ⧸ I)`
 is a closed immersion. -/
@@ -122,7 +122,7 @@ theorem of_comp {X Y Z : Scheme} (f : X ⟶ Y) (g : Y ⟶ Z) [IsClosedImmersion 
       exact ClosedEmbedding.isClosedMap h _ hZ
   surj_on_stalks x := by
     have h := surjective_stalkMap (f ≫ g) x
-    erw [Scheme.comp_val, PresheafedSpace.stalkMap.comp] at h
+    simp_rw [Scheme.comp_val, Scheme.stalkMap_comp] at h
     exact Function.Surjective.of_comp h
 
 instance {X Y : Scheme} (f : X ⟶ Y) [IsClosedImmersion f] : QuasiCompact f where
