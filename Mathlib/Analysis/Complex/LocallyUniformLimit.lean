db68c63ec8db3b705e830499a51aa844b0fae0f2
Chris Birkbeck <c.birkbeck@ucl.ac.uk>
2024-07-29 17:43:42 +0000

    feat: define logarithmic derivatives (#12804)
    We define the logarithmic derivative of a function as `f'/f` and then prove some basic facts about this. Specifially some things that will be needed for the Mittag-Leffler Expansion for Cotangent Function.
    
    
    
    Co-authored-by: Chris Birkbeck <c.birkbeck@uea.ac.uk>
    Co-authored-by: Yury G. Kudryashov <urkud@urkud.name>

--- Mathlib/Analysis/Complex/LocallyUniformLimit.lean
+++ Mathlib/Analysis/Complex/LocallyUniformLimit.lean
@@ -187,4 +187,17 @@ theorem hasSum_deriv_of_summable_norm {u : ι → ℝ} (hu : Summable u)
 
 end Tsums
 
+section LogDeriv
+
+/-- The logarithmic derivative of a sequence of functions converging locally uniformly to a
+function is the logarithmic derivative of the limit function. -/
+theorem logDeriv_tendsto {ι : Type*} {p : Filter ι} (f : ι → ℂ → ℂ) (g : ℂ → ℂ)
+    {s : Set ℂ} (hs : IsOpen s) (x : s) (hF : TendstoLocallyUniformlyOn f g p s)
+    (hf : ∀ᶠ n : ι in p, DifferentiableOn ℂ (f n) s) (hg : g x ≠ 0) :
+    Tendsto (fun n : ι => logDeriv (f n) x) p (𝓝 ((logDeriv g) x)) := by
+  simp_rw [logDeriv]
+  apply Tendsto.div ((hF.deriv hf hs).tendsto_at x.2) (hF.tendsto_at x.2) hg
+
+end LogDeriv
+
 end Complex
