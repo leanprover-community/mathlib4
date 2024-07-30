adbdeaecd81b4b1f772fad079c2f8f0bc0d5c103
Kim Morrison <kim@tqft.net>
2024-07-30 13:32:35 +0000

    chore: backports for leanprover/lean4#4814 (part 5) (#15310)
    See https://github.com/leanprover-community/mathlib4/pull/15245

--- Mathlib/Order/Monotone/Basic.lean
+++ Mathlib/Order/Monotone/Basic.lean
@@ -961,7 +961,8 @@ theorem strictAnti_int_of_succ_lt {f : ℤ → α} (hf : ∀ n, f (n + 1) < f n)
 
 namespace Int
 
-variable (α) [Preorder α] [Nonempty α] [NoMinOrder α] [NoMaxOrder α]
+variable (α)
+variable [Nonempty α] [NoMinOrder α] [NoMaxOrder α]
 
 /-- If `α` is a nonempty preorder with no minimal or maximal elements, then there exists a strictly
 monotone function `f : ℤ → α`. -/
