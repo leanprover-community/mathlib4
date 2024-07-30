08398b7d369b9cda71a3a67769ef1f2c1cf74036
Yaël Dillies <yael.dillies@gmail.com>
2024-07-30 17:21:12 +0000

    chore: Generalise `Pi.exists_forall_pos_add_lt` to monoids (#15327)
    ... as spotted by Yury.

--- Mathlib/Algebra/Order/Field/Pi.lean
+++ Mathlib/Algebra/Order/Field/Pi.lean
@@ -1,9 +1,8 @@
 /-
-Copyright (c) 2014 Robert Lewis. All rights reserved.
+Copyright (c) 2023 Yaël Dillies. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yaël Dillies
 -/
-import Mathlib.Algebra.Order.Field.Basic
 import Mathlib.Data.Finset.Lattice
 import Mathlib.Data.Fintype.Card
 
@@ -13,17 +12,17 @@ import Mathlib.Data.Fintype.Card
 We split this from `Algebra.Order.Field.Basic` to avoid importing the finiteness hierarchy there.
 -/
 
-
-variable {α ι : Type*} [LinearOrderedSemifield α]
+variable {α ι : Type*} [LinearOrderedCancelAddCommMonoid α] [Nontrivial α] [DenselyOrdered α]
 
 theorem Pi.exists_forall_pos_add_lt [ExistsAddOfLE α] [Finite ι] {x y : ι → α}
     (h : ∀ i, x i < y i) : ∃ ε, 0 < ε ∧ ∀ i, x i + ε < y i := by
   cases nonempty_fintype ι
   cases isEmpty_or_nonempty ι
-  · exact ⟨1, zero_lt_one, isEmptyElim⟩
+  · obtain ⟨a, ha⟩ := exists_ne (0 : α)
+    obtain ha | ha := ha.lt_or_lt <;> obtain ⟨b, hb, -⟩ := exists_pos_add_of_lt' ha <;>
+      exact ⟨b, hb, isEmptyElim⟩
   choose ε hε hxε using fun i => exists_pos_add_of_lt' (h i)
   obtain rfl : x + ε = y := funext hxε
   have hε : 0 < Finset.univ.inf' Finset.univ_nonempty ε := (Finset.lt_inf'_iff _).2 fun i _ => hε _
-  exact
-    ⟨_, half_pos hε, fun i =>
-      add_lt_add_left ((half_lt_self hε).trans_le <| Finset.inf'_le _ <| Finset.mem_univ _) _⟩
+  obtain ⟨δ, hδ, hδε⟩ := exists_between hε
+  exact ⟨δ, hδ, fun i ↦ add_lt_add_left (hδε.trans_le <| Finset.inf'_le _ <| Finset.mem_univ _) _⟩
