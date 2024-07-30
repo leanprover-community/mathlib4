75fef9f78f6ecd8bdcde3d5bb60563d136badfea
Jireh Loreaux <loreaujy@gmail.com>
2024-07-30 16:32:41 -0500

    improve

--- Mathlib/Analysis/CstarAlgebra/ContinuousFunctionalCalculus/Unitary.lean
+++ Mathlib/Analysis/CstarAlgebra/ContinuousFunctionalCalculus/Unitary.lean
@@ -11,41 +11,16 @@ import Mathlib.Analysis.CstarAlgebra.ContinuousFunctionalCalculus.Unital
 
 ## Main theorems
 
-* `unitary_iff_isStarNormal_and_spectrum_subset_circle`: An element is unitary if and only if it is
+* `unitary_iff_isStarNormal_and_spectrum_subset_unitary`: An element is unitary if and only if it is
   star-normal and its spectrum lies on the unit circle.
 
 -/
 
-section prereq
+section Generic
 
-instance IsStarNormal.cfc_map {R A : Type*} {p : A → Prop} [CommSemiring R] [StarRing R]
-    [MetricSpace R] [TopologicalSemiring R] [ContinuousStar R] [TopologicalSpace A] [Ring A]
-    [StarRing A] [Algebra R A] [ContinuousFunctionalCalculus R p] (a : A) (f : R → R) :
-    IsStarNormal (cfc f a) where
-  star_comm_self := by
-    rw [Commute, SemiconjBy]
-    by_cases h : ContinuousOn f (spectrum R a)
-    · rw [← cfc_star, ← cfc_mul .., ← cfc_mul ..]
-      congr! 2
-      exact mul_comm _ _
-    · simp [cfc_apply_of_not_continuousOn a h]
-
-theorem sq_eq_one_iff_of_pos {R : Type*} [LinearOrderedRing R] {a : R} (ha : 0 ≤ a) :
-    a ^ 2 = 1 ↔ a = 1 := by
-  rw [sq_eq_one_iff, or_iff_left_iff_imp]
-  rintro rfl
-  norm_num at ha
-
-
-
-end prereq
-
-section Unitary
-
-variable {R A : Type*} {p : A → Prop}
-variable [CommRing R] [StarRing R] [MetricSpace R] [TopologicalRing R] [ContinuousStar R]
-variable [TopologicalSpace A] [Ring A] [StarRing A] [Algebra R A] [StarModule R A]
-variable [ContinuousFunctionalCalculus R p]
+variable {R A : Type*} {p : A → Prop} [CommRing R] [StarRing R] [MetricSpace R]
+variable [TopologicalRing R] [ContinuousStar R] [TopologicalSpace A] [Ring A] [StarRing A]
+variable [Algebra R A] [StarModule R A] [ContinuousFunctionalCalculus R p]
 
 lemma cfc_unitary_iff (f : R → R) (a : A) (ha : p a := by cfc_tac)
     (hf : ContinuousOn f (spectrum R a) := by cfc_cont_tac) :
@@ -55,29 +30,27 @@ lemma cfc_unitary_iff (f : R → R) (a : A) (ha : p a := by cfc_tac)
     ← cfc_star, ← cfc_mul .., cfc_eq_cfc_iff_eqOn]
   exact Iff.rfl
 
-end Unitary
-
-section Unitary
+end Generic
+section Complex
 
 variable {A : Type*} [TopologicalSpace A] [Ring A] [StarRing A] [Algebra ℂ A]
   [StarModule ℂ A] [ContinuousFunctionalCalculus ℂ (IsStarNormal : A → Prop)]
 
-lemma unitary_iff_isStarNormal_and_spectrum_subset_sphere {u : A} :
-    u ∈ unitary A ↔ IsStarNormal u ∧ spectrum ℂ u ⊆ Metric.sphere 0 1 := by
+lemma unitary_iff_isStarNormal_and_spectrum_subset_unitary {u : A} :
+    u ∈ unitary A ↔ IsStarNormal u ∧ spectrum ℂ u ⊆ unitary ℂ := by
   rw [← and_iff_right_of_imp isStarNormal_of_mem_unitary]
   refine and_congr_right fun hu ↦ ?_
   nth_rw 1 [← cfc_id ℂ u]
   rw [cfc_unitary_iff id u, Set.subset_def]
   congr! with x -
-  simp only [id_eq, RCLike.star_def, mem_sphere_iff_norm, sub_zero, Complex.conj_mul']
-  exact_mod_cast sq_eq_one_iff_of_pos (norm_nonneg x)
+  simp [unitary.mem_iff_star_mul_self]
 
-lemma mem_unitary_of_spectrum_subset_sphere {u : A}
-    [IsStarNormal u] (hu : spectrum ℂ u ⊆ circle) : u ∈ unitary A :=
-  unitary_iff_isStarNormal_and_spectrum_subset_sphere.mpr ⟨‹_›, hu⟩
+lemma mem_unitary_of_spectrum_subset_unitary {u : A}
+    [IsStarNormal u] (hu : spectrum ℂ u ⊆ unitary ℂ) : u ∈ unitary A :=
+  unitary_iff_isStarNormal_and_spectrum_subset_unitary.mpr ⟨‹_›, hu⟩
 
-lemma spectrum_subset_circle_of_mem_unitary {u : A} (hu : u ∈ unitary A) :
-    spectrum ℂ u ⊆ circle :=
-  unitary_iff_isStarNormal_and_spectrum_subset_sphere.mp hu |>.right
+lemma spectrum_subset_unitary_of_mem_unitary {u : A} (hu : u ∈ unitary A) :
+    spectrum ℂ u ⊆ unitary ℂ :=
+  unitary_iff_isStarNormal_and_spectrum_subset_unitary.mp hu |>.right
 
-end Unitary
+end Complex
