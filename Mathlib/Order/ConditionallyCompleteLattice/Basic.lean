d7c2b63ac081be9daf7322f3f7c0e2b872f9f3ba
Blizzard_inc <thrashy4inbox@gmail.com>
2024-07-30 20:03:49 +0200

    some more includes
    NOTE: regarding the change to `Mathlib.Data.Set.Sigma`:
    the issue is that `singleton_sigma` (in the same file) had an unused
    parameter `a` pulled in because of the line `rintro ⟨rfl : j = i, ha⟩`.
    the new variable behaviour fixed this issue, and as a result the deleted
    line was no longer necessary. in mathlib, we could leave the issue as-is
    until this branch is merged,
    or we could add a line `clear`ing the variable.

--- Mathlib/Order/ConditionallyCompleteLattice/Basic.lean
+++ Mathlib/Order/ConditionallyCompleteLattice/Basic.lean
@@ -1261,6 +1261,7 @@ end WithTop
 namespace Monotone
 
 variable [Preorder α] [ConditionallyCompleteLattice β] {f : α → β} (h_mono : Monotone f)
+include h_mono
 
 /-! A monotone function into a conditionally complete lattice preserves the ordering properties of
 `sSup` and `sInf`. -/
