3c18faad6360800196227d1f4716e1ce53e3a168
adomani <adomani@gmail.com>
2024-07-31 15:36:02 +0200

    commit in range once

--- scripts/find_labels.sh
+++ scripts/find_labels.sh
@@ -21,8 +21,6 @@ end_date="${3}"
 # find how many commits to master there have been in the last month
 commits_in_range="$(git log --since="${start_date}" --until="${end_date}" --pretty=oneline | wc -l)"
 
-printf $'\n%s commits between %s and %s\n' "${commits_in_range}" "${start_date}" "${end_date}"
-
 # Retrieve merged PRs from the given range
 prs=$(gh pr list --repo "$repository" --state closed --base master --search "closed:${start_date}..${end_date}" --json number,labels,title --limit "$((commits_in_range * 2))")
 
@@ -36,6 +34,8 @@ echo "$prs" | jq -r '.[] | select(.title | startswith("[Merged by Bors]")) | "(#
 git log --pretty=oneline --since="${start_date}" --until="${end_date}" |
   sed -n 's=.*\((#[0-9]*)\)$=\1=p' | sort >> found_by_git.txt
 }
+
+commits_in_range="$(git log --since=2024-07-01T00:00:00 --until="$(date -d '2024-07-01 + 1 month - 1 day' +%Y-%m-%d)T23:59:59" --pretty=oneline | wc -l)"
 
 findInRange "${1}" '2024-07-01T00:00:00' '2024-07-15T23:59:59'
 findInRange "${1}" '2024-07-16T00:00:00' "$(date -d '2024-07-01 + 1 month - 1 day' +%Y-%m-%d)T23:59:59"
