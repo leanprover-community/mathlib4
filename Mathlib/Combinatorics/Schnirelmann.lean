71d26ec4b57f95c30efd2dd43354d8778bc2a6e1
Floris van Doorn <fpvdoorn@gmail.com>
2024-07-31 19:18:09 +0000

    feat: gcongr works with eta and proofs (#15364)
    * `gcongr` now recognizes eta-expanded variables as variables
    * `gcongr` now recognizes all proofs as "defeq" even if the proofs have different types (in e.g. `Finset.sup'_mono`
    * Give a bit more information in the error message when not accepting a `gcongr`-lemma.

--- Mathlib/Combinatorics/Schnirelmann.lean
+++ Mathlib/Combinatorics/Schnirelmann.lean
@@ -108,7 +108,7 @@ lemma schnirelmannDensity_eq_zero_of_one_not_mem (h : 1 ∉ A) : schnirelmannDen
 lemma schnirelmannDensity_le_of_subset {B : Set ℕ} [DecidablePred (· ∈ B)] (h : A ⊆ B) :
     schnirelmannDensity A ≤ schnirelmannDensity B :=
   ciInf_mono ⟨0, fun _ ⟨_, hx⟩ ↦ hx ▸ by positivity⟩ fun _ ↦ by
-    gcongr; exact monotone_filter_right _ h
+    gcongr; exact h
 
 /-- The Schnirelmann density of `A` is `1` if and only if `A` contains all the positive naturals. -/
 lemma schnirelmannDensity_eq_one_iff : schnirelmannDensity A = 1 ↔ {0}ᶜ ⊆ A := by
