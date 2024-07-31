09c64748e40e273342690799aa1ea2cc670da690
adomani <adomani@gmail.com>
2024-07-31 13:02:13 +0200

    search closed

--- scripts/find_labels.sh
+++ scripts/find_labels.sh
@@ -21,7 +21,7 @@ commits_in_range="$(git log --since="${start_date}" --until="${end_date}" --pret
 printf '%s commits between %s and %s\n' "${commits_in_range}" "${start_date}" "${end_date}"
 
 # Retrieve merged PRs from the last month, paginated
-prs=$(gh pr list --repo "$repo_owner/$repo_name" --state closed --search "${start_date}T00:00:00..${end_date}T23:59:59" --json number,labels,title,closed --limit "$((commits_in_range * 2))")
+prs=$(gh pr list --repo "$repo_owner/$repo_name" --state closed --search "closed:${start_date}T00:00:00..${end_date}T23:59:59" --json number,labels,title,closed --limit "$((commits_in_range * 2))")
 
 # Print PR numbers and their labels
 echo "$prs" | jq -r '.[] | select(.title | startswith("[Merged by Bors]")) | "PR #\(.number) - Labels: \((.labels | map(.name) | join(", ")) // "No labels") - Closed: \(.closed) - Title: \(.title)"'
