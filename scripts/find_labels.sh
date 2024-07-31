d7abc43d3b26d53f112be3de7d7ce8d4b9963709
adomani <adomani@gmail.com>
2024-07-31 13:05:20 +0200

    embed time in date

--- scripts/find_labels.sh
+++ scripts/find_labels.sh
@@ -12,8 +12,8 @@ repo_name=$2
 git switch master
 
 # Get the start and end dates
-start_date=$(date -d '15 days ago - 1 day' +%Y-%m-%d)
-end_date=$(date -d 'today' +%Y-%m-%d)
+start_date="$(date -d '15 days ago - 1 day' +%Y-%m-%d)T00:00:00"
+end_date="$(date -d 'today' +%Y-%m-%d)T23:59:59"
 
 # find how many commits to master there have been in the last month
 commits_in_range="$(git log --since="${start_date}" --until="${end_date}" --pretty=oneline | wc -l)"
@@ -21,7 +21,7 @@ commits_in_range="$(git log --since="${start_date}" --until="${end_date}" --pret
 printf '%s commits between %s and %s\n' "${commits_in_range}" "${start_date}" "${end_date}"
 
 # Retrieve merged PRs from the last month, paginated
-prs=$(gh pr list --repo "$repo_owner/$repo_name" --state closed --search "closed:${start_date}T00:00:00..${end_date}T23:59:59" --json number,labels,title,closed --limit "$((commits_in_range * 2))")
+prs=$(gh pr list --repo "$repo_owner/$repo_name" --state closed --search "closed:${start_date}..${end_date}" --json number,labels,title,closed --limit "$((commits_in_range * 2))")
 
 # Print PR numbers and their labels
 echo "$prs" | jq -r '.[] | select(.title | startswith("[Merged by Bors]")) | "PR #\(.number) - Labels: \((.labels | map(.name) | join(", ")) // "No labels") - Closed: \(.closed) - Title: \(.title)"'
