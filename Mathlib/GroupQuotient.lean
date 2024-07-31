b4a8797ba80f977449f0fd13b8e1eea582acd071
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2024-07-31 18:26:15 +0200

    cleanup

--- Mathlib/GroupQuotient.lean
+++ Mathlib/GroupQuotient.lean
@@ -1,9 +1,11 @@
 import Mathlib.Algebra.Group.MinimalAxioms
-import Mathlib.Analysis.Normed.Field.Basic
+import Mathlib.Algebra.Quotient
+import Mathlib.Algebra.Group.Subgroup.Basic
+import Mathlib.Tactic.Group
 
 variable {G : Type*} [Group G] (N : Subgroup G) [hN : N.Normal]
 
-def bar : Equivalence (· * ·⁻¹ ∈ N) where
+def Subgroup.sameCoset_equivalence : Equivalence (· * ·⁻¹ ∈ N) where
   refl := fun _ ↦ by
     convert N.one_mem
     group
@@ -14,41 +16,38 @@ def bar : Equivalence (· * ·⁻¹ ∈ N) where
     convert N.mul_mem hxy hyz using 1
     group
 
-def foo : Setoid G where
-  r := (· * ·⁻¹ ∈ N)
-  iseqv := bar N
+instance : HasQuotient G (Subgroup G) where
+  quotient' N := Quotient ({r := (· * ·⁻¹ ∈ N), iseqv := N.sameCoset_equivalence})
 
-instance i₁ : One (Quotient (foo N)) where
+instance : One (G ⧸ N) where
   one := ⟦1⟧
 
-instance i₂ : Mul (Quotient (foo N)) where
+instance : Mul (G ⧸ N) where
   mul := Quotient.lift₂ (⟦· * ·⟧) <| by
     intro a₁ b₁ a₂ b₂ (ha : _ ∈ N) (hb : _ ∈ N)
-    simp
+    apply Quotient.sound
     show _ ∈ N
     convert N.mul_mem ha (hN.conj_mem _ hb a₂) using 1
     group
 
-instance i₃ : Inv (Quotient (foo N)) where
+instance : Inv (G ⧸ N) where
   inv := Quotient.lift (⟦· ⁻¹⟧) <| by
     intro a b (h : _ ∈ N)
-    simp
+    apply Quotient.sound
     show _ ∈ N
     convert hN.conj_mem _ (N.inv_mem h) a⁻¹ using 1
     group
 
-instance : Group (Quotient (foo N)) := Group.ofLeftAxioms
+instance : Group (G ⧸ N) := Group.ofLeftAxioms
   (assoc := fun a b c ↦ Quotient.inductionOn₃ a b c <| fun a b c ↦ Quotient.sound <| by
     show _ ∈ N
     convert N.one_mem
-    group) -- ⊢ a * b * c * (a * (b * c))⁻¹ = 1
+    group)
   (one_mul := Quotient.ind <| fun a ↦ Quotient.sound <| by
     show _ ∈ N
     convert N.one_mem
-    group) -- ⊢ 1 * a * a⁻¹ = 1
+    group)
   (mul_left_inv := Quotient.ind <| fun a ↦ Quotient.sound <| by
     show _ ∈ N
     convert N.one_mem
-    group) -- ⊢ a⁻¹ * a * 1⁻¹ = 1
-
-#min_imports -- debug this!
+    group)
