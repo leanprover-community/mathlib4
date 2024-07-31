e629d5d22da449284ea108f93a7d39d6911ffa54
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2024-07-31 02:43:56 +0000

    chore: move Init.Order.Defs to Order.Defs (#15210)

--- Mathlib/Logic/Nontrivial/Basic.lean
+++ Mathlib/Logic/Nontrivial/Basic.lean
@@ -3,13 +3,13 @@ Copyright (c) 2020 Sébastien Gouëzel. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Sébastien Gouëzel
 -/
-import Mathlib.Init.Order.Defs
-import Mathlib.Logic.Nontrivial.Defs
-import Mathlib.Tactic.Attr.Register
 import Mathlib.Data.Prod.Basic
 import Mathlib.Data.Subtype
 import Mathlib.Logic.Function.Basic
+import Mathlib.Logic.Nontrivial.Defs
 import Mathlib.Logic.Unique
+import Mathlib.Order.Defs
+import Mathlib.Tactic.Attr.Register
 
 /-!
 # Nontrivial types
