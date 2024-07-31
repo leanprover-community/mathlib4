fe7add826cb8c2d7b4721ffa47d0a23d20b81b37
Aaron Anderson <awainverse@gmail.com>
2024-07-31 06:03:29 +0000

    chore(ModelTheory/Complexity): move quantifier complexity into a new file (#15275)
    Several important files in the model theory library touch on quantifier complexity of formulas, an idea that is not yet used for anything else in `mathlib`.
    This PR moves all quantifier complexity content to a new file, `ModelTheory/Complexity` and improves the documentation of these concepts.
    This will hopefully make these basic files more legible and easier to refactor.
    
    
    
    Co-authored-by: Aaron Anderson <65780815+awainverse@users.noreply.github.com>

--- Mathlib/ModelTheory/Semantics.lean
+++ Mathlib/ModelTheory/Semantics.lean
@@ -25,8 +25,6 @@ evaluated in the structure `M`. Also denoted `M ⊨ φ`.
 sentence of `T` is realized in `M`. Also denoted `T ⊨ φ`.
 
 ## Main Results
-* `FirstOrder.Language.BoundedFormula.realize_toPrenex` shows that the prenex normal form of a
-formula has the same realization as the original formula.
 * Several results in this file show that syntactic constructions such as `relabel`, `castLE`,
 `liftAt`, `subst`, and the actions of language maps commute with realization of terms, formulas,
 sentences, and theories.
@@ -457,62 +455,6 @@ theorem realize_all_liftAt_one_self {n : ℕ} {φ : L.BoundedFormula α n} {v : 
     simp
   · refine (congr rfl (funext fun i => ?_)).mp h
     simp
-
-theorem realize_toPrenexImpRight {φ ψ : L.BoundedFormula α n} (hφ : IsQF φ) (hψ : IsPrenex ψ)
-    {v : α → M} {xs : Fin n → M} :
-    (φ.toPrenexImpRight ψ).Realize v xs ↔ (φ.imp ψ).Realize v xs := by
-  induction' hψ with _ _ hψ _ _ _hψ ih _ _ _hψ ih
-  · rw [hψ.toPrenexImpRight]
-  · refine _root_.trans (forall_congr' fun _ => ih hφ.liftAt) ?_
-    simp only [realize_imp, realize_liftAt_one_self, snoc_comp_castSucc, realize_all]
-    exact ⟨fun h1 a h2 => h1 h2 a, fun h1 h2 a => h1 a h2⟩
-  · unfold toPrenexImpRight
-    rw [realize_ex]
-    refine _root_.trans (exists_congr fun _ => ih hφ.liftAt) ?_
-    simp only [realize_imp, realize_liftAt_one_self, snoc_comp_castSucc, realize_ex]
-    refine ⟨?_, fun h' => ?_⟩
-    · rintro ⟨a, ha⟩ h
-      exact ⟨a, ha h⟩
-    · by_cases h : φ.Realize v xs
-      · obtain ⟨a, ha⟩ := h' h
-        exact ⟨a, fun _ => ha⟩
-      · inhabit M
-        exact ⟨default, fun h'' => (h h'').elim⟩
-
-theorem realize_toPrenexImp {φ ψ : L.BoundedFormula α n} (hφ : IsPrenex φ) (hψ : IsPrenex ψ)
-    {v : α → M} {xs : Fin n → M} : (φ.toPrenexImp ψ).Realize v xs ↔ (φ.imp ψ).Realize v xs := by
-  revert ψ
-  induction' hφ with _ _ hφ _ _ _hφ ih _ _ _hφ ih <;> intro ψ hψ
-  · rw [hφ.toPrenexImp]
-    exact realize_toPrenexImpRight hφ hψ
-  · unfold toPrenexImp
-    rw [realize_ex]
-    refine _root_.trans (exists_congr fun _ => ih hψ.liftAt) ?_
-    simp only [realize_imp, realize_liftAt_one_self, snoc_comp_castSucc, realize_all]
-    refine ⟨?_, fun h' => ?_⟩
-    · rintro ⟨a, ha⟩ h
-      exact ha (h a)
-    · by_cases h : ψ.Realize v xs
-      · inhabit M
-        exact ⟨default, fun _h'' => h⟩
-      · obtain ⟨a, ha⟩ := not_forall.1 (h ∘ h')
-        exact ⟨a, fun h => (ha h).elim⟩
-  · refine _root_.trans (forall_congr' fun _ => ih hψ.liftAt) ?_
-    simp
-
-@[simp]
-theorem realize_toPrenex (φ : L.BoundedFormula α n) {v : α → M} :
-    ∀ {xs : Fin n → M}, φ.toPrenex.Realize v xs ↔ φ.Realize v xs := by
-  induction' φ with _ _ _ _ _ _ _ _ _ f1 f2 h1 h2 _ _ h
-  · exact Iff.rfl
-  · exact Iff.rfl
-  · exact Iff.rfl
-  · intros
-    rw [toPrenex, realize_toPrenexImp f1.toPrenex_isPrenex f2.toPrenex_isPrenex, realize_imp,
-      realize_imp, h1, h2]
-  · intros
-    rw [realize_all, toPrenex, realize_all]
-    exact forall_congr' fun a => h
 
 end BoundedFormula
 
