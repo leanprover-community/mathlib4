53557eab90dd6689051b6b4b162c92bbc42d515a
adomani <adomani@gmail.com>
2024-07-31 15:21:56 +0200

    start extracting dates and docs

--- scripts/find_labels.sh
+++ scripts/find_labels.sh
@@ -6,26 +6,31 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
-repository=$1
+findInRange () {
 
-git switch master
+repository="${1}"
 
 # Get the start and end dates
-start_date="$(date -d '15 days ago - 1 day' +%Y-%m-%d)T00:00:00"
-end_date="$(date -d 'today' +%Y-%m-%d)T23:59:59"
+start_date="${2}"
+end_date="${3}"
+
+git switch master
 
 # find how many commits to master there have been in the last month
 commits_in_range="$(git log --since="${start_date}" --until="${end_date}" --pretty=oneline | wc -l)"
 
 printf $'\n%s commits between %s and %s\n' "${commits_in_range}" "${start_date}" "${end_date}"
 
-# Retrieve merged PRs from the last month, paginated
+# Retrieve merged PRs from the given range
 prs=$(gh pr list --repo "$repository" --state closed --base master --search "closed:${start_date}..${end_date}" --json number,labels,title --limit "$((commits_in_range * 2))")
 
-# Print PR numbers and their labels
+# Print PR numbers, their labels and their title
 echo "$prs" | jq -r '.[] | select(.title | startswith("[Merged by Bors]")) | "PR #\(.number) - Labels: \((.labels | map(.name) | join(", ")) // "No labels") - Title: \(.title)"'
 
+# Store the PR numbers, as found by `gh`
 echo "$prs" | jq -r '.[] | select(.title | startswith("[Merged by Bors]")) | "(#\(.number))"' | sort > found_by_gh.txt
+
+# Store the PR numbers, as found by looking at the commits to `master`
 git log --pretty=oneline --since="${start_date}" --until="${end_date}" |
   sed -n 's=.*\((#[0-9]*)\)$=\1=p' | sort > found_by_git.txt
 
@@ -53,3 +58,6 @@ fi
 printf $'\n---\n'
 
 git checkout -
+}
+
+findInRange "${1}" "$(date -d '15 days ago - 1 day' +%Y-%m-%d)T00:00:00" "$(date -d 'today' +%Y-%m-%d)T23:59:59"
