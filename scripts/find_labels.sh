5d291342453bd89e93f70d32cee04e38e3cb198b
adomani <adomani@gmail.com>
2024-07-31 12:22:39 +0200

    15 days

--- scripts/find_labels.sh
+++ scripts/find_labels.sh
@@ -11,18 +11,20 @@ repo_name=$2
 
 # Get the date for one month ago
 one_month_ago=$(date -d '1 month ago' +%Y-%m-%d)
+one_month_ago=$(date -d '15 days ago' +%Y-%m-%d)
 
 git switch master
 
 # find how many commits to master there have been in the last month
 last_month_commits="$(git log --since="$one_month_ago" --pretty=oneline | wc -l)"
 
-one_month_ago=$(date -d '1 month ago - 1 day' +%Y-%m-%d)
+start_date=$(date -d '15 days ago - 1 day' +%Y-%m-%d)
+end_date=$(date -d 'today' +%Y-%m-%d)
 
-printf '%s commits since %s\n' "${last_month_commits}" "${one_month_ago}"
+printf '%s commits between %s and %s\n' "${start_date}" "${end_date}"
 
 # Retrieve merged PRs from the last month, paginated
-prs=$(gh pr list --repo "$repo_owner/$repo_name" --state closed --search "closed:>$one_month_ago" --json number,labels,title --limit "$((last_month_commits * 2))")
+prs=$(gh pr list --repo "$repo_owner/$repo_name" --state closed --search "closed:>$start_date closed:<$end_date" --json number,labels,title --limit "$((last_month_commits * 2))")
 
 # Check if any PRs are found
 if [ -z "$prs" ] || [ "$prs" = "[]" ]; then
