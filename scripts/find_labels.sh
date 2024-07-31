5c6aee83f4997f00ec1d6be8a4706c8437c3c4c0
adomani <adomani@gmail.com>
2024-07-31 12:45:51 +0200

    prune

--- scripts/find_labels.sh
+++ scripts/find_labels.sh
@@ -9,12 +9,9 @@ fi
 repo_owner=$1
 repo_name=$2
 
-# Get the date for one month ago
-one_month_ago=$(date -d '1 month ago' +%Y-%m-%d)
-one_month_ago=$(date -d '15 days ago' +%Y-%m-%d)
-
 git switch master
 
+# Get the start and end dates
 start_date=$(date -d '15 days ago - 1 day' +%Y-%m-%d)
 end_date=$(date -d 'today' +%Y-%m-%d)
 
@@ -25,11 +22,6 @@ printf '%s commits between %s and %s\n' "${commits_in_range}" "${start_date}" "$
 
 # Retrieve merged PRs from the last month, paginated
 prs=$(gh pr list --repo "$repo_owner/$repo_name" --state closed --search "closed:>$start_date closed:<$end_date" --json number,labels,title --limit "$((commits_in_range * 2))")
-
-# Check if any PRs are found
-if [ -z "$prs" ] || [ "$prs" = "[]" ]; then
-    break
-fi
 
 # Print PR numbers and their labels
 echo "$prs" | jq -r '.[] | select(.title | startswith("[Merged by Bors]")) | "PR #\(.number) - Labels: \((.labels | map(.name) | join(", ")) // "No labels") - Title: \(.title)"'
