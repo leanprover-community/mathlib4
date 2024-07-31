48dcddf0891a27e13f5caac36609af885c41b847
Johan Commelin <johan@commelin.net>
2024-07-31 19:08:02 +0200

    golf

--- Mathlib/GroupQuotient.lean
+++ Mathlib/GroupQuotient.lean
@@ -6,13 +6,13 @@ import Mathlib.Tactic.Group
 variable {G : Type*} [Group G] (N : Subgroup G) [hN : N.Normal]
 
 def Subgroup.sameCoset_equivalence : Equivalence (· * ·⁻¹ ∈ N) where
-  refl := fun _ ↦ by
+  refl _ := by
     convert N.one_mem
     group
-  symm := fun hxy ↦ by
+  symm hxy := by
     convert N.inv_mem hxy using 1
     group
-  trans := fun hxy hyz ↦ by
+  trans hxy hyz := by
     convert N.mul_mem hxy hyz using 1
     group
 
@@ -25,29 +25,30 @@ instance : One (G ⧸ N) where
 instance : Mul (G ⧸ N) where
   mul := Quotient.lift₂ (⟦· * ·⟧) <| by
     intro a₁ b₁ a₂ b₂ (ha : _ ∈ N) (hb : _ ∈ N)
-    apply Quotient.sound
-    show _ ∈ N
+    suffices _ ∈ N from Quotient.sound this
     convert N.mul_mem ha (hN.conj_mem _ hb a₂) using 1
     group
 
 instance : Inv (G ⧸ N) where
   inv := Quotient.lift (⟦· ⁻¹⟧) <| by
     intro a b (h : _ ∈ N)
-    apply Quotient.sound
-    show _ ∈ N
+    suffices _ ∈ N from Quotient.sound this
     convert hN.conj_mem _ (N.inv_mem h) a⁻¹ using 1
     group
 
 instance : Group (G ⧸ N) := Group.ofLeftAxioms
-  (assoc := fun a b c ↦ Quotient.inductionOn₃ a b c <| fun a b c ↦ Quotient.sound <| by
-    show _ ∈ N
+  (assoc := fun a b c ↦ Quotient.inductionOn₃ a b c <| by
+    intro a b c
+    suffices _ ∈ N from Quotient.sound this
     convert N.one_mem
     group)
-  (one_mul := Quotient.ind <| fun a ↦ Quotient.sound <| by
-    show _ ∈ N
+  (one_mul := Quotient.ind <| by
+    intro a
+    suffices _ ∈ N from Quotient.sound this
     convert N.one_mem
     group)
-  (mul_left_inv := Quotient.ind <| fun a ↦ Quotient.sound <| by
-    show _ ∈ N
+  (mul_left_inv := Quotient.ind <| by
+    intro a
+    suffices _ ∈ N from Quotient.sound this
     convert N.one_mem
     group)
