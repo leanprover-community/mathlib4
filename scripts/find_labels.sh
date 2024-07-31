53da2c4493c3b91be78b0a8a16e64a72efbac5b7
adomani <adomani@gmail.com>
2024-07-31 14:13:43 +0200

    formatting

--- scripts/find_labels.sh
+++ scripts/find_labels.sh
@@ -17,7 +17,7 @@ end_date="$(date -d 'today' +%Y-%m-%d)T23:59:59"
 # find how many commits to master there have been in the last month
 commits_in_range="$(git log --since="${start_date}" --until="${end_date}" --pretty=oneline | wc -l)"
 
-printf '%s commits between %s and %s\n' "${commits_in_range}" "${start_date}" "${end_date}"
+printf $'\n%s commits between %s and %s\n' "${commits_in_range}" "${start_date}" "${end_date}"
 
 # Retrieve merged PRs from the last month, paginated
 prs=$(gh pr list --repo "$repository" --state closed --base master --search "closed:${start_date}..${end_date}" --json number,labels,title --limit "$((commits_in_range * 2))")
@@ -31,7 +31,7 @@ only_gh="$(comm -23 <(echo "${prs_to_print}" | awk '{print $2}' | sort) <(git lo
 
 only_git="$(comm -13 <(echo "${prs_to_print}" | awk '{print $2}' | sort) <(git log --pretty=oneline --since="${start_date}" --until="${end_date}" | sed -n 's=.*(\(#[0-9]*\))$=\1=p' | sort))"
 
-printf $'\n\nReports\n\n---\n'
+printf $'\nReports\n\n---\n'
 
 if [ -z "${only_gh}" ]
 then
