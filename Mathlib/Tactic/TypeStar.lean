f77c74ed2199f29722f17814e3daa9af24a6e229
damiano <adomani@gmail.com>
2024-07-31 11:55:39 +0200

    Update Mathlib/Tactic/TypeStar.lean
    Co-authored-by: Matthew Robert Ballard <100034030+mattrobball@users.noreply.github.com>

--- Mathlib/Tactic/TypeStar.lean
+++ Mathlib/Tactic/TypeStar.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2023 Matthew Robert Ballard. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Matthew Robert Ballard
+Authors: Kyle Miller
 -/
 import Lean.Elab.Term
 
