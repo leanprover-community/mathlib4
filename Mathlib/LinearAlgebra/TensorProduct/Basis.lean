2767f8bc3642591ad488214073b51e3f3d8d56fd
Andrew Yang <the.erd.one@gmail.com>
2024-07-31 08:12:19 +0000

    feat(RingTheory/Kaehler): Cotangent complex associated to an embedding into affine space. (#14859)
    Co-authored-by: Andrew Yang <36414270+erdOne@users.noreply.github.com>

--- Mathlib/LinearAlgebra/TensorProduct/Basis.lean
+++ Mathlib/LinearAlgebra/TensorProduct/Basis.lean
@@ -18,6 +18,8 @@ noncomputable section
 
 open Set LinearMap Submodule
 
+open scoped TensorProduct
+
 section CommSemiring
 
 variable {R : Type*} {S : Type*} {M : Type*} {N : Type*} {ι : Type*} {κ : Type*}
@@ -26,26 +28,43 @@ variable {R : Type*} {S : Type*} {M : Type*} {N : Type*} {ι : Type*} {κ : Type
 
 /-- If `b : ι → M` and `c : κ → N` are bases then so is `fun i ↦ b i.1 ⊗ₜ c i.2 : ι × κ → M ⊗ N`. -/
 def Basis.tensorProduct (b : Basis ι S M) (c : Basis κ R N) :
-    Basis (ι × κ) S (TensorProduct R M N) :=
+    Basis (ι × κ) S (M ⊗[R] N) :=
   Finsupp.basisSingleOne.map
     ((TensorProduct.AlgebraTensorModule.congr b.repr c.repr).trans <|
         (finsuppTensorFinsupp R S _ _ _ _).trans <|
           Finsupp.lcongr (Equiv.refl _) (TensorProduct.AlgebraTensorModule.rid R S S)).symm
 
 @[simp]
-theorem Basis.tensorProduct_apply (b : Basis ι R M) (c : Basis κ R N) (i : ι) (j : κ) :
+theorem Basis.tensorProduct_apply (b : Basis ι S M) (c : Basis κ R N) (i : ι) (j : κ) :
     Basis.tensorProduct b c (i, j) = b i ⊗ₜ c j := by
   simp [Basis.tensorProduct]
 
-theorem Basis.tensorProduct_apply' (b : Basis ι R M) (c : Basis κ R N) (i : ι × κ) :
+theorem Basis.tensorProduct_apply' (b : Basis ι S M) (c : Basis κ R N) (i : ι × κ) :
     Basis.tensorProduct b c i = b i.1 ⊗ₜ c i.2 := by
   simp [Basis.tensorProduct]
 
 @[simp]
-theorem Basis.tensorProduct_repr_tmul_apply (b : Basis ι R M) (c : Basis κ R N) (m : M) (n : N)
+theorem Basis.tensorProduct_repr_tmul_apply (b : Basis ι S M) (c : Basis κ R N) (m : M) (n : N)
     (i : ι) (j : κ) :
-    (Basis.tensorProduct b c).repr (m ⊗ₜ n) (i, j) = b.repr m i * c.repr n j := by
+    (Basis.tensorProduct b c).repr (m ⊗ₜ n) (i, j) = c.repr n j • b.repr m i := by
   simp [Basis.tensorProduct, mul_comm]
+
+variable (S)
+
+/-- The lift of an `R`-basis of `M` to an `S`-basis of the base change `S ⊗[R] M`. -/
+noncomputable
+def Basis.baseChange (b : Basis ι R M) : Basis ι S (S ⊗[R] M) :=
+  ((Basis.singleton Unit S).tensorProduct b).reindex (Equiv.punitProd ι)
+
+@[simp]
+lemma Basis.baseChange_repr_tmul (b : Basis ι R M) (x y i) :
+    (b.baseChange S).repr (x ⊗ₜ y) i = b.repr y i • x := by
+  simp [Basis.baseChange, Basis.tensorProduct]
+
+@[simp]
+lemma Basis.baseChange_apply (b : Basis ι R M) (i) :
+    b.baseChange S i = 1 ⊗ₜ b i := by
+  simp [Basis.baseChange, Basis.tensorProduct]
 
 end CommSemiring
 end
