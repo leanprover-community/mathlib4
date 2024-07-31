057084fd11e10a6e10fe1ca12bc8e5be475e8176
Yaël Dillies <yael.dillies@gmail.com>
2024-07-31 05:03:35 +0000

    shake

--- Mathlib/Logic/Function/Basic.lean
+++ Mathlib/Logic/Function/Basic.lean
@@ -3,9 +3,10 @@ Copyright (c) 2016 Johannes Hölzl. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl, Mario Carneiro
 -/
-import Mathlib.Logic.Nonempty
+import Mathlib.Init.Algebra.Classes
 import Mathlib.Init.Set
 import Mathlib.Logic.Basic
+import Mathlib.Logic.Nonempty
 
 /-!
 # Miscellaneous function constructions and lemmas
