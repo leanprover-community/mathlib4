e724ef3598c7431c98e0eb689da739fd2f9a36fe
Joachim Breitner <mail@joachim-breitner.de>
2024-07-31 11:07:50 -0300

    Move lemma further below

--- Mathlib/Data/ENat/Lattice.lean
+++ Mathlib/Data/ENat/Lattice.lean
@@ -64,14 +64,14 @@ variable {f : ι → ℕ∞} {s : Set ℕ∞}
 lemma sSup_eq_zero : sSup s = 0 ↔ ∀ a ∈ s, a = 0 :=
   sSup_eq_bot
 
-lemma iSup_eq_zero : iSup f = 0 ↔ ∀ i, f i = 0 :=
-  iSup_eq_bot
-
 lemma sInf_eq_zero : sInf s = 0 ↔ 0 ∈ s := by
   rw [← lt_one_iff_eq_zero]
   simp only [sInf_lt_iff, lt_one_iff_eq_zero, exists_eq_right]
 
 lemma sSup_eq_zero' : sSup s = 0 ↔ s = ∅ ∨ s = {0} :=
   sSup_eq_bot'
+
+lemma iSup_eq_zero : iSup f = 0 ↔ ∀ i, f i = 0 :=
+  iSup_eq_bot
 
 end ENat
