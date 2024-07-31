2fb50ae74a2e835050e99c517c41b98aa4d410ea
David Loeffler <d.loeffler.01@cantab.net>
2024-07-31 20:15:02 +0000

    feat(LegendreSymbol/AddCharacter): std add char of ZMod N is primitive (#14495)
    The standard additive character of `ZMod N` is a primitive character.

--- Mathlib/Analysis/Fourier/ZMod.lean
+++ Mathlib/Analysis/Fourier/ZMod.lean
@@ -3,7 +3,7 @@ Copyright (c) 2024 David Loeffler. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: David Loeffler
 -/
-import Mathlib.Analysis.SpecialFunctions.Complex.Circle
+import Mathlib.Analysis.SpecialFunctions.Complex.CircleAddChar
 import Mathlib.Analysis.Fourier.FourierTransform
 import Mathlib.NumberTheory.DirichletCharacter.GaussSum
 
