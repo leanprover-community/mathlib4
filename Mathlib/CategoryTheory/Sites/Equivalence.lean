4a5362d7993809f63cfed91aaba27ced7d0bad84
Dagur Asgeirsson <dagurtomas@gmail.com>
2024-07-31 17:07:38 +0000

    Update Mathlib/CategoryTheory/Sites/Equivalence.lean

--- Mathlib/CategoryTheory/Sites/Equivalence.lean
+++ Mathlib/CategoryTheory/Sites/Equivalence.lean
@@ -79,22 +79,6 @@ instance : e.functor.IsDenseSubsite J K := by
   rw [e.eq_inducedTopology_of_isDenseSubsite J K]
   infer_instance
 
-instance : e.symm.TransportsGrothendieckTopology K J where
-  eq_inducedTopology := by
-    rw [e.eq_inducedTopology_of_transports J K]
-    ext X S
-    change _ ↔ _ ∈ J.sieves _
-    simp only [symm_inverse]
-    constructor
-    · intro h
-      convert J.pullback_stable (e.unitInv.app X) h
-      exact Sieve.functorPushforward_equivalence_eq_pullback e S
-    · intro h
-      convert J.pullback_stable (e.unit.app X) h
-      exact (Sieve.pullback_functorPushforward_equivalence_eq e S).symm
-
-instance : e.inverse.IsDenseSubsite K J := inferInstanceAs (e.symm.functor.IsDenseSubsite _ _)
-
 /-- The functor in the equivalence of sheaf categories. -/
 @[simps!]
 def sheafCongr.functor : Sheaf J A ⥤ Sheaf K A where
