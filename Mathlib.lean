3aad643dd7b28ed9dda006c96ddc7eb9d82f3278
Dagur Asgeirsson <dagurtomas@gmail.com>
2024-08-01 20:50:36 +0000

    refactor(Condensed): merge files about discreteness (#15402)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1961,13 +1961,12 @@ import Mathlib.Computability.TMComputable
 import Mathlib.Computability.TMToPartrec
 import Mathlib.Computability.TuringMachine
 import Mathlib.Condensed.Basic
-import Mathlib.Condensed.Discrete
+import Mathlib.Condensed.Discrete.Basic
 import Mathlib.Condensed.Epi
 import Mathlib.Condensed.Equivalence
 import Mathlib.Condensed.Explicit
 import Mathlib.Condensed.Functors
 import Mathlib.Condensed.Light.Basic
-import Mathlib.Condensed.Light.Discrete
 import Mathlib.Condensed.Light.Explicit
 import Mathlib.Condensed.Light.Functors
 import Mathlib.Condensed.Light.Module
