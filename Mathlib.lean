c4ac899e2d4bf3a4191fcc8aff535c077d970fcc
Etienne <etienne.marion@ens-lyon.fr>
2024-08-01 09:13:24 +0000

    feat(Topology): add API for compactly generated spaces (#15203)
    Define compactly generated topological spaces, which are spaces whose topology is coinduced by continuous maps whose domain is compact Hausdorff.
    Prove basic properties and instances.
    Provide instances for sequential spaces and Hausdorff weakly locally compact spaces.
    
    
    
    Co-authored-by: Etienne <66847262+EtienneC30@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -4371,6 +4371,7 @@ import Mathlib.Topology.ClopenBox
 import Mathlib.Topology.CompactOpen
 import Mathlib.Topology.Compactification.OnePoint
 import Mathlib.Topology.Compactness.Compact
+import Mathlib.Topology.Compactness.CompactlyGeneratedSpace
 import Mathlib.Topology.Compactness.Lindelof
 import Mathlib.Topology.Compactness.LocallyCompact
 import Mathlib.Topology.Compactness.Paracompact
