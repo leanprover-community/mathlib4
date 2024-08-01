795f4ca4804598f20198f410e92afdafe9321abf
Jireh Loreaux <loreaujy@gmail.com>
2024-08-01 04:08:26 +0000

    refactor: remove some dependence on `circle` in C⋆-algebras (#15343)

--- Mathlib/Analysis/CstarAlgebra/ContinuousFunctionalCalculus/Unitary.lean
+++ Mathlib/Analysis/CstarAlgebra/ContinuousFunctionalCalculus/Unitary.lean
@@ -3,51 +3,55 @@ Copyright (c) 2024 Jireh Loreaux. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jireh Loreaux
 -/
-import Mathlib.Analysis.Complex.Circle
 import Mathlib.Tactic.Peel
 import Mathlib.Analysis.CstarAlgebra.ContinuousFunctionalCalculus.Unital
+import Mathlib.Analysis.Complex.Basic
 
 /-! # Conditions on unitary elements imposed by the continuous functional calculus
 
 ## Main theorems
 
-* `unitary_iff_isStarNormal_and_spectrum_subset_circle`: An element is unitary if and only if it is
+* `unitary_iff_isStarNormal_and_spectrum_subset_unitary`: An element is unitary if and only if it is
   star-normal and its spectrum lies on the unit circle.
 
 -/
-section Unitary
+
+section Generic
+
+variable {R A : Type*} {p : A → Prop} [CommRing R] [StarRing R] [MetricSpace R]
+variable [TopologicalRing R] [ContinuousStar R] [TopologicalSpace A] [Ring A] [StarRing A]
+variable [Algebra R A] [StarModule R A] [ContinuousFunctionalCalculus R p]
+
+lemma cfc_unitary_iff (f : R → R) (a : A) (ha : p a := by cfc_tac)
+    (hf : ContinuousOn f (spectrum R a) := by cfc_cont_tac) :
+    cfc f a ∈ unitary A ↔ ∀ x ∈ spectrum R a, star (f x) * f x = 1 := by
+  simp only [unitary, Submonoid.mem_mk, Subsemigroup.mem_mk, Set.mem_setOf_eq]
+  rw [← IsStarNormal.cfc_map (p := p) f a |>.star_comm_self |>.eq, and_self, ← cfc_one R a,
+    ← cfc_star, ← cfc_mul .., cfc_eq_cfc_iff_eqOn]
+  exact Iff.rfl
+
+end Generic
+
+section Complex
 
 variable {A : Type*} [TopologicalSpace A] [Ring A] [StarRing A] [Algebra ℂ A]
   [StarModule ℂ A] [ContinuousFunctionalCalculus ℂ (IsStarNormal : A → Prop)]
 
-lemma unitary_iff_isStarNormal_and_spectrum_subset_circle {u : A} :
-    u ∈ unitary A ↔ IsStarNormal u ∧ spectrum ℂ u ⊆ circle := by
-  refine ⟨fun hu ↦ ?_, ?_⟩
-  · have h_normal := isStarNormal_of_mem_unitary hu
-    refine ⟨h_normal, ?_⟩
-    have h := unitary.star_mul_self_of_mem hu
-    rw [← cfc_id ℂ u, ← cfc_star id u, ← cfc_mul .., ← cfc_one ℂ u] at h
-    have := eqOn_of_cfc_eq_cfc h
-    peel this with x hx _
-    rw [SetLike.mem_coe, mem_circle_iff_normSq]
-    simpa using congr($(this).re)
-  · rintro ⟨_, hu⟩
-    rw [unitary.mem_iff, ← cfc_id ℂ u, ← cfc_star, ← cfc_mul .., ← cfc_mul .., ← cfc_one ℂ u]
-    simp only [id_eq]
-    constructor
-    all_goals
-      apply cfc_congr (fun x hx ↦ ?_)
-      simp only [RCLike.star_def, mul_comm x]
-      apply hu at hx
-      rwa [SetLike.mem_coe, mem_circle_iff_normSq, ← Complex.ofReal_injective.eq_iff,
-        Complex.normSq_eq_conj_mul_self] at hx
+lemma unitary_iff_isStarNormal_and_spectrum_subset_unitary {u : A} :
+    u ∈ unitary A ↔ IsStarNormal u ∧ spectrum ℂ u ⊆ unitary ℂ := by
+  rw [← and_iff_right_of_imp isStarNormal_of_mem_unitary]
+  refine and_congr_right fun hu ↦ ?_
+  nth_rw 1 [← cfc_id ℂ u]
+  rw [cfc_unitary_iff id u, Set.subset_def]
+  congr! with x -
+  simp [unitary.mem_iff_star_mul_self]
 
-lemma mem_unitary_of_spectrum_subset_circle {u : A}
-    [IsStarNormal u] (hu : spectrum ℂ u ⊆ circle) : u ∈ unitary A :=
-  unitary_iff_isStarNormal_and_spectrum_subset_circle.mpr ⟨‹_›, hu⟩
+lemma mem_unitary_of_spectrum_subset_unitary {u : A}
+    [IsStarNormal u] (hu : spectrum ℂ u ⊆ unitary ℂ) : u ∈ unitary A :=
+  unitary_iff_isStarNormal_and_spectrum_subset_unitary.mpr ⟨‹_›, hu⟩
 
-lemma spectrum_subset_circle_of_mem_unitary {u : A} (hu : u ∈ unitary A) :
-    spectrum ℂ u ⊆ circle :=
-  unitary_iff_isStarNormal_and_spectrum_subset_circle.mp hu |>.right
+lemma spectrum_subset_unitary_of_mem_unitary {u : A} (hu : u ∈ unitary A) :
+    spectrum ℂ u ⊆ unitary ℂ :=
+  unitary_iff_isStarNormal_and_spectrum_subset_unitary.mp hu |>.right
 
-end Unitary
+end Complex
