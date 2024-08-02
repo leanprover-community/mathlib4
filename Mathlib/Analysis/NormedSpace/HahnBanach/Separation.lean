54d6297c8c44dc46318368b4e2188b161dba96c6
Jon Bannon <jbannon@siena.edu>
2024-08-02 11:53:48 -0400

    removed `_RCLike` since these are in the `RCLike` namespace.

--- Mathlib/Analysis/NormedSpace/HahnBanach/Separation.lean
+++ Mathlib/Analysis/NormedSpace/HahnBanach/Separation.lean
@@ -221,41 +221,41 @@ noncomputable def extendTo𝕜'ₗ : (E →L[ℝ] ℝ) →ₗ[ℝ] (E →L[𝕜]
 
 variable [ContinuousSMul ℝ E]
 
-theorem separate_convex_open_set_RCLike {s : Set E}
+theorem separate_convex_open_set {s : Set E}
     (hs₀ : (0 : E) ∈ s) (hs₁ : Convex ℝ s) (hs₂ : IsOpen s) {x₀ : E} (hx₀ : x₀ ∉ s) :
     ∃ f : E →L[𝕜] 𝕜, re (f x₀) = 1 ∧ ∀ x ∈ s, re (f x) < 1 := by
-  obtain ⟨g, hg⟩ := separate_convex_open_set hs₀ hs₁ hs₂ hx₀
+  obtain ⟨g, hg⟩ := _root_.separate_convex_open_set hs₀ hs₁ hs₂ hx₀
   use extendTo𝕜'ₗ g
   simp only [extendTo𝕜'ₗ_apply_toFun, map_sub, ofReal_re, mul_re, I_re, zero_mul, ofReal_im,
     mul_zero, sub_self, sub_zero]
   exact hg
 
-theorem geometric_hahn_banach_open_RCLike (hs₁ : Convex ℝ s) (hs₂ : IsOpen s) (ht : Convex ℝ t)
+theorem geometric_hahn_banach_open (hs₁ : Convex ℝ s) (hs₂ : IsOpen s) (ht : Convex ℝ t)
     (disj : Disjoint s t) : ∃ (f : E →L[𝕜] 𝕜) (u : ℝ), (∀ a ∈ s, re (f a) < u) ∧
     ∀ b ∈ t, u ≤ re (f b) := by
-  obtain ⟨f, u, h⟩ := geometric_hahn_banach_open hs₁ hs₂ ht disj
+  obtain ⟨f, u, h⟩ := _root_.geometric_hahn_banach_open hs₁ hs₂ ht disj
   use extendTo𝕜'ₗ f
   simp only [extendTo𝕜'ₗ_apply_toFun, map_sub, ofReal_re, mul_re, I_re, zero_mul, ofReal_im,
     mul_zero, sub_self, sub_zero]
   exact Exists.intro u h
 
-theorem geometric_hahn_banach_open_point_RCLike (hs₁ : Convex ℝ s) (hs₂ : IsOpen s) (disj : x ∉ s) :
+theorem geometric_hahn_banach_open_point (hs₁ : Convex ℝ s) (hs₂ : IsOpen s) (disj : x ∉ s) :
     ∃ f : E →L[𝕜] 𝕜, ∀ a ∈ s, re (f a) < re (f x) := by
-  obtain ⟨f, h⟩ := geometric_hahn_banach_open_point hs₁ hs₂ disj
+  obtain ⟨f, h⟩ := _root_.geometric_hahn_banach_open_point hs₁ hs₂ disj
   use extendTo𝕜'ₗ f
   simp only [extendTo𝕜'ₗ_apply_toFun, map_sub, ofReal_re, mul_re, I_re, zero_mul, ofReal_im,
     mul_zero, sub_self, sub_zero]
   exact fun a a_1 ↦ h a a_1
 
-theorem geometric_hahn_banach_point_open_RCLike (ht₁ : Convex ℝ t) (ht₂ : IsOpen t) (disj : x ∉ t) :
+theorem geometric_hahn_banach_point_open (ht₁ : Convex ℝ t) (ht₂ : IsOpen t) (disj : x ∉ t) :
     ∃ f : E →L[𝕜] 𝕜, ∀ b ∈ t, re (f x) < re (f b) :=
-  let ⟨f, hf⟩ := geometric_hahn_banach_open_point_RCLike ht₁ ht₂ disj
+  let ⟨f, hf⟩ := geometric_hahn_banach_open_point ht₁ ht₂ disj
   ⟨-f, by simpa⟩
 
-theorem geometric_hahn_banach_open_open_RCLike (hs₁ : Convex ℝ s) (hs₂ : IsOpen s)
+theorem geometric_hahn_banach_open_open (hs₁ : Convex ℝ s) (hs₂ : IsOpen s)
     (ht₁ : Convex ℝ t) (ht₃ : IsOpen t) (disj : Disjoint s t) :
     ∃ (f : E →L[𝕜] 𝕜) (u : ℝ), (∀ a ∈ s, re (f a) < u) ∧ ∀ b ∈ t, u < re (f b) := by
-  obtain ⟨f, u, h⟩ := geometric_hahn_banach_open_open hs₁ hs₂ ht₁ ht₃ disj
+  obtain ⟨f, u, h⟩ := _root_.geometric_hahn_banach_open_open hs₁ hs₂ ht₁ ht₃ disj
   use extendTo𝕜'ₗ f
   simp only [extendTo𝕜'ₗ_apply_toFun, map_sub, ofReal_re, mul_re, I_re, zero_mul, ofReal_im,
     mul_zero, sub_self, sub_zero]
@@ -263,10 +263,10 @@ theorem geometric_hahn_banach_open_open_RCLike (hs₁ : Convex ℝ s) (hs₂ : I
 
 variable [LocallyConvexSpace ℝ E]
 
-theorem geometric_hahn_banach_compact_closed_RCLike (hs₁ : Convex ℝ s) (hs₂ : IsCompact s)
+theorem geometric_hahn_banach_compact_closed (hs₁ : Convex ℝ s) (hs₂ : IsCompact s)
     (ht₁ : Convex ℝ t) (ht₂ : IsClosed t) (disj : Disjoint s t) :
     ∃ (f : E →L[𝕜] 𝕜) (u v : ℝ), (∀ a ∈ s, re (f a) < u) ∧ u < v ∧ ∀ b ∈ t, v < re (f b) := by
-  obtain ⟨g, u, v, h1⟩ := geometric_hahn_banach_compact_closed hs₁ hs₂ ht₁ ht₂ disj
+  obtain ⟨g, u, v, h1⟩ := _root_.geometric_hahn_banach_compact_closed hs₁ hs₂ ht₁ ht₂ disj
   use extendTo𝕜'ₗ g
   simp only [extendTo𝕜'ₗ_apply_toFun, map_sub, ofReal_re, mul_re, I_re, zero_mul, ofReal_im,
     mul_zero, sub_self, sub_zero, exists_and_left]
@@ -276,39 +276,39 @@ theorem geometric_hahn_banach_compact_closed_RCLike (hs₁ : Convex ℝ s) (hs�
   use v
   exact h1.2
 
-theorem geometric_hahn_banach_closed_compact_RCLike (hs₁ : Convex ℝ s) (hs₂ : IsClosed s)
+theorem geometric_hahn_banach_closed_compact (hs₁ : Convex ℝ s) (hs₂ : IsClosed s)
     (ht₁ : Convex ℝ t) (ht₂ : IsCompact t) (disj : Disjoint s t) :
     ∃ (f : E →L[𝕜] 𝕜) (u v : ℝ), (∀ a ∈ s, re (f a) < u) ∧ u < v ∧ ∀ b ∈ t, v < re (f b) :=
-  let ⟨f, s, t, hs, st, ht⟩ := geometric_hahn_banach_compact_closed_RCLike ht₁ ht₂ hs₁ hs₂ disj.symm
+  let ⟨f, s, t, hs, st, ht⟩ := geometric_hahn_banach_compact_closed ht₁ ht₂ hs₁ hs₂ disj.symm
   ⟨-f, -t, -s, by simpa using ht, by simpa using st, by simpa using hs⟩
 
-theorem geometric_hahn_banach_point_closed_RCLike (ht₁ : Convex ℝ t) (ht₂ : IsClosed t)
+theorem geometric_hahn_banach_point_closed (ht₁ : Convex ℝ t) (ht₂ : IsClosed t)
     (disj : x ∉ t) : ∃ (f : E →L[𝕜] 𝕜) (u : ℝ), re (f x) < u ∧ ∀ b ∈ t, u < re (f b) :=
   let ⟨f, _u, v, ha, hst, hb⟩ :=
-    geometric_hahn_banach_compact_closed_RCLike (convex_singleton x) isCompact_singleton ht₁ ht₂
+    geometric_hahn_banach_compact_closed (convex_singleton x) isCompact_singleton ht₁ ht₂
       (disjoint_singleton_left.2 disj)
   ⟨f, v, hst.trans' <| ha x <| mem_singleton _, hb⟩
 
-theorem geometric_hahn_banach_closed_point_RCLike (hs₁ : Convex ℝ s) (hs₂ : IsClosed s)
+theorem geometric_hahn_banach_closed_point (hs₁ : Convex ℝ s) (hs₂ : IsClosed s)
     (disj : x ∉ s) : ∃ (f : E →L[𝕜] 𝕜) (u : ℝ), (∀ a ∈ s, re (f a) < u) ∧ u < re (f x) :=
   let ⟨f, s, _t, ha, hst, hb⟩ :=
-    geometric_hahn_banach_closed_compact_RCLike hs₁ hs₂ (convex_singleton x) isCompact_singleton
+    geometric_hahn_banach_closed_compact hs₁ hs₂ (convex_singleton x) isCompact_singleton
       (disjoint_singleton_right.2 disj)
   ⟨f, s, ha, hst.trans <| hb x <| mem_singleton _⟩
 
-theorem geometric_hahn_banach_point_point_RCLike [T1Space E] (hxy : x ≠ y) :
+theorem geometric_hahn_banach_point_point [T1Space E] (hxy : x ≠ y) :
     ∃ f : E →L[𝕜] 𝕜, re (f x) < re (f y) := by
   obtain ⟨f, s, t, hs, st, ht⟩ :=
-    geometric_hahn_banach_compact_closed_RCLike (𝕜 := 𝕜) (convex_singleton x) isCompact_singleton
+    geometric_hahn_banach_compact_closed (𝕜 := 𝕜) (convex_singleton x) isCompact_singleton
       (convex_singleton y) isClosed_singleton (disjoint_singleton.2 hxy)
   exact ⟨f, by linarith [hs x rfl, ht y rfl]⟩
 
-theorem iInter_halfspaces_eq_RCLike (hs₁ : Convex ℝ s) (hs₂ : IsClosed s) :
+theorem iInter_halfspaces_eq (hs₁ : Convex ℝ s) (hs₂ : IsClosed s) :
     ⋂ l : E →L[𝕜] 𝕜, { x | ∃ y ∈ s, re (l x) ≤ re (l y) } = s := by
   rw [Set.iInter_setOf]
   refine Set.Subset.antisymm (fun x hx => ?_) fun x hx l => ⟨x, hx, le_rfl⟩
   by_contra h
-  obtain ⟨l, s, hlA, hl⟩ := geometric_hahn_banach_closed_point_RCLike (𝕜 := 𝕜) hs₁ hs₂ h
+  obtain ⟨l, s, hlA, hl⟩ := geometric_hahn_banach_closed_point (𝕜 := 𝕜) hs₁ hs₂ h
   obtain ⟨y, hy, hxy⟩ := hx l
   exact ((hxy.trans_lt (hlA y hy)).trans hl).false
 
