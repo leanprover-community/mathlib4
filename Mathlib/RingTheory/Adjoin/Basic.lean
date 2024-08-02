5e5ca03972a1312599ff65d1735b5a5b45711dcc
Kim Morrison <kim@tqft.net>
2024-08-02 14:34:33 +1000

    chore: backports for leanprover/lean4#4814 (part 17)

--- Mathlib/RingTheory/Adjoin/Basic.lean
+++ Mathlib/RingTheory/Adjoin/Basic.lean
@@ -319,7 +319,7 @@ theorem adjoin_adjoin_of_tower (s : Set A) : adjoin S (adjoin R s : Set A) = adj
     exact subset_adjoin
 
 @[simp]
-theorem adjoin_top :
+theorem adjoin_top {A} [Semiring A] [Algebra S A] (t : Set A) :
     adjoin (⊤ : Subalgebra R S) t = (adjoin S t).restrictScalars (⊤ : Subalgebra R S) :=
   let equivTop : Subalgebra (⊤ : Subalgebra R S) A ≃o Subalgebra S A :=
     { toFun := fun s => { s with algebraMap_mem' := fun r => s.algebraMap_mem ⟨r, trivial⟩ }
