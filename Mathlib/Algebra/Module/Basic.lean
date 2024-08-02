30baaa8e69235b6190db5acfda11c750c9f93ce6
james18lpc <kyjwong0501@gmail.com>
2024-08-02 14:42:18 +0000

    feat: Scalar multiplication of an indicator function (#15311)
    from GibbsMeasure
    
    
    Co-authored-by: Yael Dillies <yael.dillies@gmail.com>

--- Mathlib/Algebra/Module/Basic.lean
+++ Mathlib/Algebra/Module/Basic.lean
@@ -191,6 +191,16 @@ lemma indicator_smul_const (s : Set α) (r : α → R) (m : M) :
   funext <| indicator_smul_const_apply _ _ _
 
 end SMulWithZero
+
+section MulZeroOneClass
+
+variable [MulZeroOneClass R]
+
+lemma smul_indicator_one_apply (s : Set α) (r : R) (a : α) :
+    r • s.indicator (1 : α → R) a = s.indicator (fun _ ↦ r) a := by
+  simp_rw [← indicator_const_smul_apply, Pi.one_apply, smul_eq_mul, mul_one]
+
+end MulZeroOneClass
 end Set
 
 assert_not_exists Multiset
