f758d8b81f6b4d6070b2b78840b764a5d9b5957a
Jireh Loreaux <loreaujy@gmail.com>
2024-08-04 10:58:30 +0000

    chore: move more files into `Analysis.Normed.Algebra` (#15168)

--- Mathlib/Analysis/CstarAlgebra/Exponential.lean
+++ Mathlib/Analysis/CstarAlgebra/Exponential.lean
@@ -3,7 +3,7 @@ Copyright (c) 2022 Jireh Loreaux. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jireh Loreaux
 -/
-import Mathlib.Analysis.NormedSpace.Exponential
+import Mathlib.Analysis.Normed.Algebra.Exponential
 
 /-! # The exponential map from selfadjoint to unitary
 In this file, we establish various properties related to the map `fun a ↦ exp ℂ A (I • a)`
