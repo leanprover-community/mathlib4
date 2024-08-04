a505fd9849928bacf6efe7229a7ebdc64e595faa
Yakov Pechersky <ypechersky@treeline.bio>
2024-08-04 14:32:22 +0000

    feat: Real.exists_natCast_add_one_le_pow_of_one_le (#15074)
    Necessary for arguments in approaching a power from above
    
    
    
    
    Co-authored-by: Yakov Pechersky <pechersky@users.noreply.github.com>

--- Mathlib/Data/Real/Archimedean.lean
+++ Mathlib/Data/Real/Archimedean.lean
@@ -335,4 +335,30 @@ theorem iUnion_Iic_rat : ⋃ r : ℚ, Iic (r : ℝ) = univ := by
 theorem iInter_Iic_rat : ⋂ r : ℚ, Iic (r : ℝ) = ∅ := by
   exact iInter_Iic_eq_empty_iff.mpr not_bddBelow_coe
 
+/-- Exponentiation is eventually larger than linear growth. -/
+lemma exists_natCast_add_one_lt_pow_of_one_lt {a : ℝ} (ha : 1 < a) :
+    ∃ m : ℕ, (m + 1 : ℝ) < a ^ m := by
+  obtain ⟨k, posk, hk⟩ : ∃ k : ℕ, 0 < k ∧ 1 / k + 1 < a := by
+    contrapose! ha
+    refine le_of_forall_lt_rat_imp_le ?_
+    intro q hq
+    refine (ha q.den (by positivity)).trans ?_
+    rw [← le_sub_iff_add_le, div_le_iff (by positivity), sub_mul, one_mul]
+    norm_cast at hq ⊢
+    rw [← q.num_div_den, one_lt_div (by positivity)] at hq
+    rw [q.mul_den_eq_num]
+    norm_cast at hq ⊢
+    rw [le_tsub_iff_left hq.le]
+    exact hq
+  use 2 * k ^ 2
+  refine (pow_lt_pow_left hk (by positivity) (by simp [posk.ne'])).trans_le' ?_
+  rcases k.zero_le.eq_or_lt with rfl|kpos
+  · simp
+  rw [pow_two, mul_left_comm, pow_mul]
+  have := mul_add_one_le_add_one_pow (a := 1 / k) (by simp) k
+  rw [div_mul_cancel₀ _ (by simp [kpos.ne'])] at this
+  refine (pow_le_pow_left (by positivity) this _).trans' ?_
+  rw [mul_left_comm, ← pow_two]
+  exact_mod_cast Nat.two_mul_sq_add_one_le_two_pow_two_mul _
+
 end Real
