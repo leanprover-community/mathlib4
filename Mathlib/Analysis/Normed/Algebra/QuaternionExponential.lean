459d066e56ddf7ba6a06484f16958350ae8d3b2d
Jireh Loreaux <loreaujy@gmail.com>
2024-08-04 10:58:30 +0000

    chore: move more files into `Analysis.Normed.Algebra` (#15168)

--- Mathlib/Analysis/Normed/Algebra/QuaternionExponential.lean
+++ Mathlib/Analysis/Normed/Algebra/QuaternionExponential.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Eric Wieser
 -/
 import Mathlib.Analysis.Quaternion
-import Mathlib.Analysis.NormedSpace.Exponential
+import Mathlib.Analysis.Normed.Algebra.Exponential
 import Mathlib.Analysis.SpecialFunctions.Trigonometric.Series
 
 /-!
