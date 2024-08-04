561841ed61243789f224d6cfe985135685e7ffc8
Dagur Asgeirsson <dagurtomas@gmail.com>
2024-08-04 13:29:38 +0000

    feat(LightCondensed): characterise epimorphisms in light condensed sets and modules (#13495)
    This PR characterises epimorphisms in light condensed sets and modules as the locally surjective
    morphisms. Here, the condition of locally surjective is phrased in terms of continuous surjections
    of light profinite sets.

--- Mathlib.lean
+++ Mathlib.lean
@@ -1968,6 +1968,7 @@ import Mathlib.Condensed.Equivalence
 import Mathlib.Condensed.Explicit
 import Mathlib.Condensed.Functors
 import Mathlib.Condensed.Light.Basic
+import Mathlib.Condensed.Light.Epi
 import Mathlib.Condensed.Light.Explicit
 import Mathlib.Condensed.Light.Functors
 import Mathlib.Condensed.Light.Module
