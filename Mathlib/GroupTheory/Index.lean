75077dd47ddbb858ab09d2d55749958045b86250
Joseph Myers <jsm@polyomino.org.uk>
2024-08-04 10:58:27 +0000

    feat(GroupTheory/Index): finite quotient lemmas (#14921)
    Add some lemmas about finiteness of `MulAction.orbitRel.Quotient` for actions by a subgroup of finite index.
    
    From AperiodicMonotilesLean.

--- Mathlib/GroupTheory/Index.lean
+++ Mathlib/GroupTheory/Index.lean
@@ -379,6 +379,20 @@ noncomputable def fintypeOfIndexNeZero (hH : H.index ≠ 0) : Fintype (G ⧸ H) 
 theorem one_lt_index_of_ne_top [Finite (G ⧸ H)] (hH : H ≠ ⊤) : 1 < H.index :=
   Nat.one_lt_iff_ne_zero_and_ne_one.mpr ⟨index_ne_zero_of_finite, mt index_eq_one.mp hH⟩
 
+@[to_additive]
+lemma finite_quotient_of_finite_quotient_of_index_ne_zero {X : Type*} [MulAction G X]
+    [Finite <| MulAction.orbitRel.Quotient G X] (hi : H.index ≠ 0) :
+    Finite <| MulAction.orbitRel.Quotient H X := by
+  have := fintypeOfIndexNeZero hi
+  exact MulAction.finite_quotient_of_finite_quotient_of_finite_quotient
+
+@[to_additive]
+lemma finite_quotient_of_pretransitive_of_index_ne_zero {X : Type*} [MulAction G X]
+    [MulAction.IsPretransitive G X] (hi : H.index ≠ 0) :
+    Finite <| MulAction.orbitRel.Quotient H X := by
+  have := (MulAction.pretransitive_iff_subsingleton_quotient G X).1 inferInstance
+  exact finite_quotient_of_finite_quotient_of_index_ne_zero hi
+
 section FiniteIndex
 
 variable (H K)
