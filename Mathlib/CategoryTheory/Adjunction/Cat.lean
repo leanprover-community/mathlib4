2f9ae3196fc4aeec8123a0486c0eba12679d4407
nicolas <nrolland@users.noreply.github.com>
2024-08-05 12:28:49 +0200

    cleanup

--- Mathlib/CategoryTheory/Adjunction/Cat.lean
+++ Mathlib/CategoryTheory/Adjunction/Cat.lean
@@ -3,11 +3,8 @@ Copyright (c) 2024 Nicolas Rolland. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Nicolas Rolland
 -/
-import Mathlib.CategoryTheory.DiscreteCategory
-import Mathlib.CategoryTheory.Types
 import Mathlib.CategoryTheory.Category.Cat
 import Mathlib.CategoryTheory.Adjunction.Basic
-
 /-!
 # Adjunctions related to Cat, the category of categories
 
@@ -24,63 +21,33 @@ each category to its set of connected components.
 
 -/
 
-
-universe v u
-
+universe u
 namespace CategoryTheory
 
-section AdjDiscObj
-
-variable (X : Type u)
-variable (C : Cat)
-
-private def lxyToxry : (typeToCat.obj X ⟶ C) → (X ⟶ Cat.objects.obj C) := fun f x ↦ f.obj ⟨x⟩
-private def xryTolxy : (X ⟶ Cat.objects.obj C) → (typeToCat.obj X ⟶ C) := fun f ↦ Discrete.functor f
-
-private lemma linverse : Function.LeftInverse (xryTolxy X C) (lxyToxry X C) :=
-  fun (fctr : typeToCat.obj X ⥤ C) ↦ by
-  fapply Functor.hext
-  · intro x; rfl
-  · intro ⟨x⟩ ⟨y⟩ f
-    simp
-    obtain rfl := (Discrete.eq_of_hom f : x = y)
-    calc
-      (xryTolxy X C (lxyToxry X C fctr)).map f = 𝟙 (fctr.obj { as := x }) :=
-        Discrete.functor_map_id (xryTolxy X C (lxyToxry X C fctr)) f
-      _                                        = fctr.map f := (Discrete.functor_map_id fctr f).symm
-
-private lemma rightinverse : Function.RightInverse (xryTolxy X C) (lxyToxry X C) := fun _ ↦
-  funext (fun _ => rfl)
+variable (X : Type u) (C : Cat)
 
 private def homEquiv : (typeToCat.obj X ⟶ C) ≃ (X ⟶ Cat.objects.obj C) where
-      toFun:= lxyToxry X C
-      invFun:= (xryTolxy X C)
-      left_inv:=(linverse X C)
-      right_inv:= (rightinverse X C)
+      toFun f x := f.obj ⟨x⟩
+      invFun := Discrete.functor
+      left_inv F := Functor.ext (fun _ ↦ rfl)
+                                (fun⟨_⟩ ⟨_⟩ f => by
+                                        obtain rfl := Discrete.eq_of_hom f
+                                        simp)
+      right_inv := fun _ ↦ funext (fun _ => rfl)
 
-private def counit_app : ∀ C,  (Cat.objects ⋙ typeToCat).obj C ⥤ C := fun C =>
-    { obj := Discrete.as
-      map := eqToHom ∘ Discrete.eq_of_hom }
+private def typeToCatObjectsAdjCounitApp : (Cat.objects ⋙ typeToCat).obj C ⥤ C where
+     obj := Discrete.as
+     map := eqToHom ∘ Discrete.eq_of_hom
 
-
-/-- typeToCat : Type ⥤ Cat is left adjoint to Cat.objects : Cat ⥤ Type
--/
-def adjTypeToCatCatobjects : typeToCat ⊣ Cat.objects where
+/-- typeToCat : Type ⥤ Cat is left adjoint to Cat.objects : Cat ⥤ Type -/
+def typeToCatObjectsAdj : typeToCat ⊣ Cat.objects where
   homEquiv  := homEquiv
-  unit : 𝟭 (Type u) ⟶ typeToCat ⋙ Cat.objects := { app:= fun X  ↦ Discrete.mk }
-  counit : Cat.objects ⋙ typeToCat ⟶ 𝟭 Cat :=
-  {
-    app := counit_app
-    naturality := by intro C D (f : C ⥤ D)
-                     fapply Functor.hext
-                     · intro c
-                       rfl
-                     · intro ⟨_⟩ ⟨_⟩ f
-                       obtain rfl := Discrete.eq_of_hom f
-                       aesop_cat
-  }
-
-end AdjDiscObj
-
+  unit : 𝟭 (Type u) ⟶ typeToCat ⋙ Cat.objects := { app:= fun _  ↦ Discrete.mk }
+  counit : Cat.objects ⋙ typeToCat ⟶ 𝟭 Cat := {
+    app := typeToCatObjectsAdjCounitApp
+    naturality := fun _ _ _  ↦  Functor.hext (fun _ ↦ rfl)
+                                             (by intro ⟨_⟩ ⟨_⟩ f
+                                                 obtain rfl := Discrete.eq_of_hom f
+                                                 aesop_cat ) }
 
 end CategoryTheory
