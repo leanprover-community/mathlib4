26ba50261d9bad2b47352a004b32aad6f55cfae5
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2024-08-05 14:06:10 -0300

    Rename `CirculantGraph` to `circulantGraph`

--- Mathlib/Combinatorics/SimpleGraph/Circulant.lean
+++ Mathlib/Combinatorics/SimpleGraph/Circulant.lean
@@ -15,7 +15,7 @@ are adjacent if and only if `u - v ∈ s` or `v - u ∈ s`. The elements of `s` 
 
 ## Main declarations
 
-* `SimpleGraph.CirculantGraph s`: the circulant graph over `G` with jumps `s`.
+* `SimpleGraph.circulantGraph s`: the circulant graph over `G` with jumps `s`.
 * `SimpleGraph.cycleGraph n`: the cycle graph over `Fin n`.
 -/
 
@@ -23,14 +23,14 @@ namespace SimpleGraph
 
 /-- Circulant graph over additive group `G` with jumps `s` -/
 @[simps!]
-def CirculantGraph {G : Type*} [AddGroup G] (s : Set G) : SimpleGraph G :=
+def circulantGraph {G : Type*} [AddGroup G] (s : Set G) : SimpleGraph G :=
   fromRel (· - · ∈ s)
 
 variable {G : Type*} [AddGroup G] (s : Set G)
 
-theorem circulantGraph_eq_erase_zero : CirculantGraph s = CirculantGraph (s \ {0}) := by
+theorem circulantGraph_eq_erase_zero : circulantGraph s = circulantGraph (s \ {0}) := by
   ext (u v : G)
-  simp only [CirculantGraph, fromRel_adj, and_congr_right_iff]
+  simp only [circulantGraph, fromRel_adj, and_congr_right_iff]
   intro (h : u ≠ v)
   apply Iff.intro
   · intro h1
@@ -42,27 +42,27 @@ theorem circulantGraph_eq_erase_zero : CirculantGraph s = CirculantGraph (s \ {0
       | inl h1 => exact Or.inl h1.left
       | inr h1 => exact Or.inr h1.left
 
-theorem circulantGraph_eq_symm : CirculantGraph s = CirculantGraph (s ∪ (-s)) := by
+theorem circulantGraph_eq_symm : circulantGraph s = circulantGraph (s ∪ (-s)) := by
   ext (u v : G)
-  simp only [CirculantGraph, fromRel_adj, Set.mem_union, Set.mem_neg, neg_sub, and_congr_right_iff,
+  simp only [circulantGraph, fromRel_adj, Set.mem_union, Set.mem_neg, neg_sub, and_congr_right_iff,
     iff_self_or]
   intro _ h
   exact Or.symm h
 
-instance [DecidableEq G] [DecidablePred (· ∈ s)] : DecidableRel (CirculantGraph s).Adj :=
+instance [DecidableEq G] [DecidablePred (· ∈ s)] : DecidableRel (circulantGraph s).Adj :=
   fun _ _ => inferInstanceAs (Decidable (_ ∧ _))
 
 theorem circulantGraph_adj_translate {s : Set G} {u v d : G} :
-    (CirculantGraph s).Adj (u + d) (v + d) ↔ (CirculantGraph s).Adj u v := by simp
+    (circulantGraph s).Adj (u + d) (v + d) ↔ (circulantGraph s).Adj u v := by simp
 
 /-- Cycle graph over `Fin n` -/
 def cycleGraph : (n : ℕ) → SimpleGraph (Fin n)
   | 0 => ⊥
-  | _ + 1 => CirculantGraph {1}
+  | _ + 1 => circulantGraph {1}
 
 instance : (n : ℕ) → DecidableRel (cycleGraph n).Adj
   | 0 => fun _ _ => inferInstanceAs (Decidable False)
-  | _ + 1 => inferInstanceAs (DecidableRel (CirculantGraph _).Adj)
+  | _ + 1 => inferInstanceAs (DecidableRel (circulantGraph _).Adj)
 
 theorem cycleGraph_zero_adj {u v : Fin 0} : ¬(cycleGraph 0).Adj u v := id
 
@@ -85,7 +85,7 @@ theorem cycleGraph_one_adj {u v : Fin 1} : ¬(cycleGraph 1).Adj u v := by
 
 theorem cycleGraph_adj {n : ℕ} {u v : Fin (n + 2)} :
     (cycleGraph (n + 2)).Adj u v ↔ u - v = 1 ∨ v - u = 1 := by
-  simp only [cycleGraph, CirculantGraph_adj, Set.mem_singleton_iff, and_iff_right_iff_imp]
+  simp only [cycleGraph, circulantGraph_adj, Set.mem_singleton_iff, and_iff_right_iff_imp]
   intro _ _
   simp_all
 
