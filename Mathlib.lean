3f064af44e4e1212db60783acd59759905d7cb48
adomani <adomani@gmail.com>
2024-08-06 06:13:28 +0200

    feat: the pedantic linter
    This linter tries to make sure that source code looks like the pretty printed one.
    
    It is not intended to be always active, since not every pretty-printed syntax is desirable!

--- Mathlib.lean
+++ Mathlib.lean
@@ -2783,6 +2783,7 @@ import Mathlib.Init.Data.Int.Order
 import Mathlib.Init.Data.List.Basic
 import Mathlib.Init.Data.List.Instances
 import Mathlib.Init.Data.List.Lemmas
+import Mathlib.Init.Data.Nat.GCD
 import Mathlib.Init.Data.Nat.Lemmas
 import Mathlib.Init.Data.Quot
 import Mathlib.Init.Data.Sigma.Basic
@@ -4117,6 +4118,7 @@ import Mathlib.Tactic.Linter.HashCommandLinter
 import Mathlib.Tactic.Linter.Lint
 import Mathlib.Tactic.Linter.MinImports
 import Mathlib.Tactic.Linter.OldObtain
+import Mathlib.Tactic.Linter.PedanticLinter
 import Mathlib.Tactic.Linter.RefineLinter
 import Mathlib.Tactic.Linter.Style
 import Mathlib.Tactic.Linter.TextBased
