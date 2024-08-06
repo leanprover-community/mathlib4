8a2d665bfb17b2d2715a4447d508424753af79b7
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2024-08-06 09:49:12 -0300

    Run `lake exe mk_all`

--- Mathlib.lean
+++ Mathlib.lean
@@ -1933,6 +1933,7 @@ import Mathlib.Combinatorics.SimpleGraph.Regularity.Lemma
 import Mathlib.Combinatorics.SimpleGraph.Regularity.Uniform
 import Mathlib.Combinatorics.SimpleGraph.StronglyRegular
 import Mathlib.Combinatorics.SimpleGraph.Subgraph
+import Mathlib.Combinatorics.SimpleGraph.Sum
 import Mathlib.Combinatorics.SimpleGraph.Trails
 import Mathlib.Combinatorics.SimpleGraph.Triangle.Basic
 import Mathlib.Combinatorics.SimpleGraph.Triangle.Counting
