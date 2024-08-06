6bc124d090da7323e23d9844ef0374c3bbebf28a
negiizhao <zhao.yu-yang@foxmail.com>
2024-08-07 06:41:27 +0800

    shake

--- Mathlib/Data/Num/Basic.lean
+++ Mathlib/Data/Num/Basic.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Leonardo de Moura, Mario Carneiro
 -/
 import Lean.Linter.Deprecated
-import Mathlib.Algebra.Group.Nat
 import Mathlib.Algebra.Group.ZeroOne
 import Mathlib.Data.Int.Notation
 import Mathlib.Data.Nat.BinaryRec
