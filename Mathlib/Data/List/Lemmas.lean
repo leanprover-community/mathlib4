67527886b58d0be8c66f387d6262fbf1821beddd
Shuhao Song <znssong_meow@sina.com>
2024-08-07 07:39:13 +0800

    Update Mathlib/Data/List/Lemmas.lean
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- Mathlib/Data/List/Lemmas.lean
+++ Mathlib/Data/List/Lemmas.lean
@@ -18,7 +18,7 @@ variable {α β γ : Type*}
 
 namespace List
 
-theorem getElem_reverse' {α} (l : List α) (i : Nat) (h1 h2) :
+theorem getElem_reverse' (l : List α) (i : Nat) (h1 h2) :
     (reverse l)[i]'h1 = l[length l - 1 - i]'h2 := by
   rw [← getElem_reverse l _ (by omega) (by omega)]
   congr
