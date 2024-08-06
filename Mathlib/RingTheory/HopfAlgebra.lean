af3bdaa4b05b37d50da9c5e06513e4ea27bff674
Jujian Zhang <jujian.zhang1998@outlook.com>
2024-08-06 05:13:04 +0000

    feat(RingTheory/Coalgebra/*): add basics about sweedler notation (#12109)
    Though the name is called sweedler notation, no new notations are actually introduced. This PR is actually about expanding tensor product.
    
    
    
    Co-authored-by: Lihan Jiang <liihan@192.168.1.141>
    Co-authored-by: 101damnations <101damnations@github.com>

--- Mathlib/RingTheory/HopfAlgebra.lean
+++ Mathlib/RingTheory/HopfAlgebra.lean
@@ -70,6 +70,30 @@ theorem mul_antipode_lTensor_comul_apply (a : A) :
     algebraMap R A (Coalgebra.counit a) :=
   LinearMap.congr_fun mul_antipode_lTensor_comul a
 
+open Coalgebra
+
+@[simp]
+lemma sum_antipode_mul_eq {a : A} (repr : Repr R a) :
+    ∑ i ∈ repr.index, antipode (R := R) (repr.left i) * repr.right i =
+      algebraMap R A (counit a) := by
+  simpa [← repr.eq, map_sum] using congr($(mul_antipode_rTensor_comul (R := R)) a)
+
+@[simp]
+lemma sum_mul_antipode_eq {a : A} (repr : Repr R a) :
+    ∑ i ∈ repr.index, repr.left i * antipode (R := R) (repr.right i) =
+      algebraMap R A (counit a) := by
+  simpa [← repr.eq, map_sum] using congr($(mul_antipode_lTensor_comul (R := R)) a)
+
+lemma sum_antipode_mul_eq_smul {a : A} (repr : Repr R a) :
+    ∑ i ∈ repr.index, antipode (R := R) (repr.left i) * repr.right i =
+      counit (R := R) a • 1 := by
+  rw [sum_antipode_mul_eq, Algebra.smul_def, mul_one]
+
+lemma sum_mul_antipode_eq_smul {a : A} (repr : Repr R a) :
+    ∑ i ∈ repr.index, repr.left i * antipode (R := R) (repr.right i) =
+      counit (R := R) a • 1 := by
+  rw [sum_mul_antipode_eq, Algebra.smul_def, mul_one]
+
 end HopfAlgebra
 
 namespace CommSemiring
