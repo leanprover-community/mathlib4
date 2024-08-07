3c6bed0ff316775dae8278dc653704bbf2c2166e
Shuhao Song <znssong_meow@sina.com>
2024-08-07 13:34:58 +0800

    fix: Fix imports

--- Mathlib/Dynamics/FixedPoints/Increasing.lean
+++ Mathlib/Dynamics/FixedPoints/Increasing.lean
@@ -3,8 +3,7 @@ Copyright (c) 2024 Shuhao Song. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Shuhao Song
 -/
-import Mathlib.Algebra.Order.Ring.Nat
-import Mathlib.Data.Finite.Card
+import Mathlib.Data.Fintype.Card
 
 /-!
 # Fixed points of function `f` with `f(x) ≥ x`
