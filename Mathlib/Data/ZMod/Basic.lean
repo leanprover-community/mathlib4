4b35c61e54854aa30f7884ca6cca791ad33611ea
Kim Morrison <kim@tqft.net>
2024-08-07 19:53:52 +1000

    fix imports

--- Mathlib/Data/ZMod/Basic.lean
+++ Mathlib/Data/ZMod/Basic.lean
@@ -6,7 +6,8 @@ Authors: Chris Hughes
 import Mathlib.Algebra.Ring.Prod
 import Mathlib.GroupTheory.OrderOfElement
 import Mathlib.Tactic.FinCases
-import Mathlib.Tactic.Linarith.Lemmas
+import Mathlib.Tactic.Linarith
+
 
 /-!
 # Integers mod `n`
