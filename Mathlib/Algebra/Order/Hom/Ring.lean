99ba09f41aad01effa08bfab0a4d11a649972759
Kim Morrison <kim@tqft.net>
2024-08-07 19:44:38 +1000

    fix

--- Mathlib/Algebra/Order/Hom/Ring.lean
+++ Mathlib/Algebra/Order/Hom/Ring.lean
@@ -33,8 +33,8 @@ making some typeclasses and instances irrelevant.
 ordered ring homomorphism, order homomorphism
 -/
 
--- assert_not_exists FloorRing
--- assert_not_exists Archimedean
+assert_not_exists FloorRing
+assert_not_exists Archimedean
 
 open Function
 
