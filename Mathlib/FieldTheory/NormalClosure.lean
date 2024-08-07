3fa2349c00e0c562ffd74276cf361227912a75eb
Kim Morrison <kim@tqft.net>
2024-08-07 12:42:08 +1000

    remove includes

--- Mathlib/FieldTheory/NormalClosure.lean
+++ Mathlib/FieldTheory/NormalClosure.lean
@@ -87,7 +87,6 @@ lemma isNormalClosure_iff : IsNormalClosure F K L ↔
     simpa only [normalClosure_eq_iSup_adjoin_of_splits splits] using h
 -- TODO: IntermediateField.isNormalClosure_iff similar to IntermediateField.isSplittingField_iff
 
-include splits in
 /-- `normalClosure F K L` is a valid normal closure if `K/F` is algebraic
   and all minimal polynomials of `K/F` splits in `L/F`. -/
 lemma isNormalClosure_normalClosure : IsNormalClosure F K (normalClosure F K L) := by
