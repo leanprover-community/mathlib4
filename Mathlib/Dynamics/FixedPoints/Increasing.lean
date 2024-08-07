bb17c91d1843145645cb6bd95b4c23bef60a956a
Shuhao Song <znssong_meow@sina.com>
2024-08-07 12:19:03 +0800

    fix: Fixing copyright notices

--- Mathlib/Dynamics/FixedPoints/Increasing.lean
+++ Mathlib/Dynamics/FixedPoints/Increasing.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2024 Shuhao Song. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Shuhao Song
+-/
 import Mathlib.Order.Basic
 import Mathlib.Data.Finite.Basic
 
