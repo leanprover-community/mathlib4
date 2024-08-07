013b62ffe6d88d527fd7d37d9c562565ed3625eb
Kyle Miller <kmill31415@gmail.com>
2024-08-07 15:24:36 +0000

    chore: remove some useless `simp`s (#15588)
    They are just instantiating some metavariables that are incidentally there.

--- Mathlib/AlgebraicTopology/DoldKan/Degeneracies.lean
+++ Mathlib/AlgebraicTopology/DoldKan/Degeneracies.lean
@@ -96,7 +96,6 @@ theorem σ_comp_P_eq_zero (X : SimplicialObject C) {n q : ℕ} (i : Fin (n + 1))
           decomposition_Q n q, sum_comp, sum_comp, Finset.sum_eq_zero, add_zero, add_neg_eq_zero]
         intro j hj
         simp only [true_and_iff, Finset.mem_univ, Finset.mem_filter] at hj
-        simp only [Nat.succ_eq_add_one] at hi
         obtain ⟨k, hk⟩ := Nat.le.dest (Nat.lt_succ_iff.mp (Fin.is_lt j))
         rw [add_comm] at hk
         have hi' : i = Fin.castSucc ⟨i, by omega⟩ := by
