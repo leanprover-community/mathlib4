1ea76814d3a25a9732618cd2ec17c1f00a4c4479
adomani <adomani@gmail.com>
2024-08-07 22:42:44 +0200

    lint also `let` vs `have`

--- Mathlib/Tactic/Linter/HaveLetLinter.lean
+++ Mathlib/Tactic/Linter/HaveLetLinter.lean
@@ -3,14 +3,16 @@ Copyright (c) 2024 Damiano Testa. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Damiano Testa
 -/
-
 import Lean.Elab.Command
-import Lean.Server.InfoUtils
+import Lean.Linter.Util
+import Batteries.Data.List.Basic
 
 /-!
 #  The `have` vs `let` linter
 
-The `have` vs `let` linter flags uses of `have` to introduce a hypothesis whose Type is not `Prop`.
+The `have` vs `let` linter flags uses of
+* `have` to introduce a hypothesis whose Type is not `Prop`;
+* `let` to introduce a hypothesis whose Type is `Prop`.
 
 The option for this linter is a natural number, but really there are only 3 settings:
 * `0` -- inactive;
@@ -18,8 +20,7 @@ The option for this linter is a natural number, but really there are only 3 sett
 * `2` or more -- always active.
 
 TODO:
-* Also lint `let` vs `have`.
-* `haveI` may need to change to `let/letI`?
+* `haveI` may need to change to `let`/`letI`?
 * `replace`, `classical!`, `classical`, `tauto` internally use `have`:
   should the linter act on them as well?
 -/
@@ -47,11 +48,21 @@ register_option linter.haveLet : Nat := {
 
 namespace haveLet
 
-/-- find the `have` syntax. -/
-partial
-def isHave? : Syntax → Bool
+/--
+* on input `have`, returns `some true`,
+* on input `let`, returns `some false`,
+* on all other inputs, returns `none`.
+-/
+def have_or_let? : Syntax → Option Bool
   | .node _ ``Lean.Parser.Tactic.tacticHave_ _ => true
-  |_ => false
+  | .node _ `Lean.Parser.Tactic.tacticLet_ _ => false
+  |_ => none
+
+/-- `SyntaxNodeKind`s that imply a `have` but should be ignored anyway. -/
+abbrev exclusions : HashSet SyntaxNodeKind := HashSet.empty
+  |>.insert ``Lean.Parser.Tactic.replace
+  |>.insert `Std.Tactic.classical!
+  |>.insert `Mathlib.Tactic.Tauto.tauto
 
 end haveLet
 
@@ -65,31 +76,38 @@ def InfoTree.foldInfoM {α m} [Monad m] (f : ContextInfo → Info → α → m �
     InfoTree → m α :=
   InfoTree.foldInfo (fun ctx i ma => do f ctx i (← ma)) (pure init)
 
-/-- given a `ContextInfo`, a `LocalContext` and an `Array` of `Expr`essions `es`,
+/-- given a `LocalContext`, `MetavarContext` and an `Array` of `Expr`essions `es`,
 `areProp_toFormat` creates a `MetaM` context, and returns an array of pairs consisting of
 * a `Bool`ean, answering the question of whether the Type of `e` is a `Prop` or not, and
 * the pretty-printed `Format` of `e`
 for each `Expr`ession `e` in `es`.
+
 Concretely, `areProp_toFormat` runs `inferType` in `CommandElabM`.
 This is the kind of monadic lift that `nonPropHaves` uses to decide whether the Type of a `have`
 is in `Prop` or not.
+
 The output `Format` is just so that the linter displays a better message. -/
-def areProp_toFormat (ctx : ContextInfo) (lc : LocalContext) (es : Array Expr) :
+def areProp_toFormat (lc : LocalContext) (mctx : MetavarContext) (es : Array Expr) :
     CommandElabM (Array (Bool × Format)) := do
-  ctx.runMetaM lc do
+  let res ← liftCoreM do MetaM.run (ctx := { lctx := lc }) (s := { mctx := mctx }) <| do
     es.mapM fun e => do
       let typ ← inferType (← instantiateMVars e)
       return (typ.isProp, ← ppExpr e)
+  return res.1
 
-/-- returns the `have` syntax whose corresponding hypothesis does not have Type `Prop` and
-also a `Format`ted version of the corresponding Type. -/
+/-- returns
+* the `have` syntax nodes whose corresponding hypothesis does not have Type `Prop`, and
+* the `let` syntax nodes whose corresponding hypothesis has Type `Prop`.
+It also returns a `Format`ted version of the corresponding `Type`/`Prop` and a `Bool`ean
+that is `true` for `have` and `false` for `let`. -/
 partial
-def nonPropHaves : InfoTree → CommandElabM (Array (Syntax × Format)) :=
-  InfoTree.foldInfoM (init := #[]) fun ctx info args => return args ++ (← do
+def nonPropHaves : InfoTree → CommandElabM (Array (Syntax × Format × Bool)) :=
+  InfoTree.foldInfoM (init := #[]) fun _ctx info args => return args ++ (← do
     let .ofTacticInfo i := info | return #[]
     let stx := i.stx
     let .original .. := stx.getHeadInfo | return #[]
-    unless isHave? stx do return #[]
+    if exclusions.contains stx.getKind then return #[]
+    let some haveLet? := have_or_let? stx | return #[]
     let mctx := i.mctxAfter
     let mvdecls := (i.goalsAfter.map (mctx.decls.find? ·)).reduceOption
     -- we extract the `MetavarDecl` with largest index after a `have`, since this one
@@ -98,7 +116,7 @@ def nonPropHaves : InfoTree → CommandElabM (Array (Syntax × Format)) :=
     -- the relevant `LocalContext`
     let lc := (largestIdx.getD 0 default).lctx
     -- we also accumulate all `fvarId`s from all local contexts before the use of `have`
-    -- so that we can then isolate the `fvarId`s that are created by `have`
+    -- so that we can then isolate the `fvarId`s that `have`/`let` introduce
     let oldMvdecls := (i.goalsBefore.map (mctx.decls.find? ·)).reduceOption
     let oldLctx := oldMvdecls.map (·.lctx)
     let oldDecls := (oldLctx.map (·.decls.toList.reduceOption)).join
@@ -107,10 +125,18 @@ def nonPropHaves : InfoTree → CommandElabM (Array (Syntax × Format)) :=
     -- effectively they are the declarations that we want to test for being in `Prop` or not.
     let newDecls := lc.decls.toList.reduceOption.filter (! oldFVars.contains ·.fvarId)
     -- now, we get the `MetaM` state up and running to find the types of each entry of `newDecls`
-    let fmts ← areProp_toFormat ctx lc (newDecls.map (·.type)).toArray
-    let (_propFmts, typeFmts) := (fmts.zip (newDecls.map (·.userName)).toArray).partition (·.1.1)
-    -- everything that is a Type triggers a warning on `have`
-    return typeFmts.map fun ((_, fmt), na) => (stx, f!"{na} : {fmt}"))
+    let fmts ← areProp_toFormat lc mctx (newDecls.map (·.type)).toArray
+    let (propFmts, typeFmts) := (fmts.zip (newDecls.map (·.userName)).toArray).partition (·.1.1)
+    let propFmts := propFmts.map fun ((_, fmt), na) => (na, fmt)
+    let typeFmts := typeFmts.map fun ((_, fmt), na) => (na, fmt)
+    return if haveLet? then
+      -- everything that is a Type triggers a warning on `have`
+      (typeFmts.map fun (na, fmt) => (stx, f!"{na} : {fmt}", true))
+    else
+      -- only if everything is a Prop, we trigger a warning on `let`
+      if typeFmts.size == 0 then
+        (propFmts.map fun (na, fmt) => (stx, f!"{na} : {fmt}", false))
+      else #[])
 
 /-- The main implementation of the `have` vs `let` linter. -/
 def haveLetLinter : Linter where run := withSetOptionIn fun _stx => do
@@ -120,11 +146,16 @@ def haveLetLinter : Linter where run := withSetOptionIn fun _stx => do
   unless gh == 1 && (← MonadState.get).messages.unreported.isEmpty do
     let trees ← getInfoTrees
     for t in trees.toArray do
-      for (s, fmt) in ← nonPropHaves t do
+      for (s, fmt, haveLet?) in ← nonPropHaves t do
         -- Since the linter option is not in `Bool`, the standard `Linter.logLint` does not work.
         -- We emulate it with `logWarningAt`
-        logWarningAt s <| .tagged linter.haveLet.name
-          m!"'{fmt}' is a Type and not a Prop. Consider using 'let' instead of 'have'.\n\
-          You can disable this linter using `set_option linter.haveLet 0`"
+        if haveLet? then
+          logWarningAt s <| .tagged linter.haveLet.name
+            m!"'{fmt}' is a Type and not a Prop. Consider using 'let' instead of 'have'.\n\
+            You can disable this linter using `set_option linter.haveLet 0`"
+        else
+          logWarningAt s <| .tagged linter.haveLet.name
+            m!"'{fmt}' is a Prop and not a Type. Consider using 'have' instead of 'let'.\n\
+            You can disable this linter using `set_option linter.haveLet 0` {s}"
 
 initialize addLinter haveLetLinter
