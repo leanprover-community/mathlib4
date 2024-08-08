2567df15a4b58c073467c87082f4072da5501302
adomani <adomani@gmail.com>
2024-08-08 11:59:22 +0200

    lint

--- Mathlib/TestHashOption.lean
+++ Mathlib/TestHashOption.lean
@@ -1,3 +1,12 @@
+/-
+Copyright (c) 2024 Damiano Testa. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Damiano Testa
+-/
 import Mathlib.Tactic.Linter.HashCommandLinter
+
+/-!
+Trying out the new setting options feature.
+-/
 
 #guard true
