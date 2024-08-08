d77dc74c7b55ac7569c0bbb567e012bb2956eb49
Kalle <kalle.kytola@aalto.fi>
2024-08-09 01:02:41 +0300

    Delete commented out now unused lemmas.

--- Mathlib/Data/ENNReal/Floor.lean
+++ Mathlib/Data/ENNReal/Floor.lean
@@ -85,53 +85,7 @@ lemma floor_le_of_lt_add_one (h : x < m + 1) :
 
 @[simp] lemma floor_one_eq_one : floor 1 = 1 := by exact_mod_cast ENNReal.floor_coe_eq 1
 
---/-- There exists a natural number on any closed interval of length one in ℝ≥0. -/
---lemma _root_.NNReal.exists_nat_mem_Icc (x : ℝ≥0) : ∃ (n : ℕ), ↑n ∈ Set.Icc x (x+1) := by
---  refine ⟨FloorSemiring.ceil x, ⟨Nat.le_ceil x, ?_⟩⟩
---  apply le_trans ?_ <| add_le_add (Nat.floor_le (zero_le x)) le_rfl
---  exact_mod_cast Nat.ceil_le_floor_add_one x
---
---/-- There exists a natural number on any left-closed right-open interval of length one in ℝ≥0. -/
---lemma _root_.NNReal.exists_nat_mem_Ico (x : ℝ≥0) : ∃ (n : ℕ), ↑n ∈ Set.Ico x (x+1) := by
---  obtain ⟨m, hm⟩ := exists_nat_mem_Icc x
---  by_cases h : m = x + 1
---  · exact ⟨m-1, ⟨by simp [h], by simp [h]⟩⟩
---  · exact ⟨m, ⟨hm.1, lt_of_le_of_ne hm.2 h⟩⟩
---
---lemma range_nat_coe_inter_Iic_top_eq :
---    (Set.range (fun (n : ℕ) ↦ (n : ℝ≥0∞))) ∩ Iic ∞ = (Set.range (fun (n : ℕ) ↦ (n : ℝ≥0∞))) := by
---  simp only [Iic_top, inter_univ]
---
---lemma range_nat_coe_inter_Iio_top_eq :
---    (Set.range (fun (n : ℕ) ↦ (n : ℝ≥0∞))) ∩ Iio ∞ = (Set.range (fun (n : ℕ) ↦ (n : ℝ≥0∞))) :=
---  inter_eq_self_of_subset_left <| fun x ↦ by simpa using fun n hn ↦ hn.symm ▸ coe_lt_top (r := n)
-
 @[simp] lemma floor_top_eq : floor ∞ = ⊤ := floor_coe_eq ⊤
-
---lemma _root_.Nat.setOf_le_ENNReal_eq_Iic (x_ne_top : x ≠ ∞) :
---    {n : ℕ | n ≤ x} = Iic (Nat.floor x.toNNReal) := by
---  apply subset_antisymm
---  · intro n n_le_x
---    simp only [floor, mem_Iic]
---    apply Nat.le_floor
---    simpa using (toNNReal_le_toNNReal coe_ne_top x_ne_top).mpr n_le_x
---  · intro n n_le_floor
---    simp only [mem_Iic, mem_setOf_eq] at *
---    apply le_trans ?_ <| @coe_toNNReal_le_self x
---    apply le_trans ?_ <| ENNReal.coe_le_coe.mpr <| Nat.floor_le (a := x.toNNReal) (zero_le _)
---    exact_mod_cast n_le_floor
---
---private lemma range_nat_coe_inter_Iic_eq (x : ℝ≥0∞) :
---    (Set.range (fun (n : ℕ) ↦ (n : ℝ≥0∞))) ∩ Iic x = (fun (n : ℕ) ↦ (n : ℝ≥0∞)) '' {n | n ≤ x} := by
---  apply subset_antisymm
---  · intro z hz
---    simp only [mem_inter_iff, mem_range, mem_Iic, mem_image, mem_setOf_eq] at *
---    obtain ⟨n, hn⟩ := hz.1
---    exact ⟨n, ⟨hn ▸ hz.2, hn⟩⟩
---  · intro z hz
---    simp only [mem_image, mem_setOf_eq, mem_inter_iff, mem_range, mem_Iic] at *
---    obtain ⟨n, hn⟩ := hz
---    exact ⟨⟨n, hn.2⟩, hn.2.symm ▸ hn.1⟩
 
 lemma floor_eq_natFloor_toNNReal (x_ne_top : x ≠ ∞) :
     floor x = Nat.floor x.toNNReal := by
@@ -151,14 +105,6 @@ lemma floor_eq_natFloor_toNNReal (x_ne_top : x ≠ ∞) :
     exact_mod_cast Nat.floor_le (show 0 ≤ x.toNNReal by simp)
 
 lemma floor_floor (x : ℝ≥0∞) : (x.floor : ℝ≥0∞).floor = x.floor := by simp
-
---lemma _root_.Nat.bddAbove_le_ennreal {x : ℝ≥0∞} (x_ne_top : x ≠ ∞) :
---    BddAbove {n : ℕ | n ≤ x} := by
---  use Nat.ceil x.toNNReal
---  simp only [mem_upperBounds, mem_setOf_eq]
---  intro m hm
---  have obs : m ≤ x.toNNReal := (toNNReal_le_toNNReal coe_ne_top x_ne_top).mpr hm
---  exact_mod_cast obs.trans <| Nat.le_ceil x.toNNReal
 
 lemma floor_lt_top {x : ℝ≥0∞} (x_ne_top : x ≠ ∞) :
     x.floor < ∞ := by
