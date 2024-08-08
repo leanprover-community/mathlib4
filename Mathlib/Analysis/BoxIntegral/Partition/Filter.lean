7d74ba40feb361cb3ad32243d87d3c5c78f0d431
Kim Morrison <kim@tqft.net>
2024-08-08 17:31:21 +1000

    fix?

--- Mathlib/Analysis/BoxIntegral/Partition/Filter.lean
+++ Mathlib/Analysis/BoxIntegral/Partition/Filter.lean
@@ -348,9 +348,10 @@ theorem MemBaseSet.exists_common_compl
     (hU : π₁.iUnion = π₂.iUnion) :
     ∃ π : Prepartition I, π.iUnion = ↑I \ π₁.iUnion ∧
       (l.bDistortion → π.distortion ≤ c₁) ∧ (l.bDistortion → π.distortion ≤ c₂) := by
+  clear l₁ l₂
   wlog hc : c₁ ≤ c₂ with H
   · simpa [hU, _root_.and_comm] using
-        @H _ _ I c₂ c₁ r₂ r₁ π₂ π₁ _ h₂ h₁ hU.symm (le_of_not_le hc)
+        @H _ _ I J c c₂ c₁ l r₂ r₁ π π₂ π₁ _ h₂ h₁ hU.symm (le_of_not_le hc)
   by_cases hD : (l.bDistortion : Prop)
   · rcases h₁.4 hD with ⟨π, hπU, hπc⟩
     exact ⟨π, hπU, fun _ => hπc, fun _ => hπc.trans hc⟩
