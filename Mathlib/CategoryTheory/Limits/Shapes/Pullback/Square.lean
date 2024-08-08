8250d4c32fd59cc4fd71e7729299f74da4058ba8
Joël Riou <joel.riou@universite-paris-saclay.fr>
2024-08-08 20:16:39 +0530

    cleaning up

--- Mathlib/CategoryTheory/Limits/Shapes/Pullback/Square.lean
+++ Mathlib/CategoryTheory/Limits/Shapes/Pullback/Square.lean
@@ -107,54 +107,6 @@ lemma IsPullback.op {sq : Square C} (h : sq.IsPullback) : sq.op.IsPushout :=
 lemma IsPullback.unop {sq : Square Cᵒᵖ} (h : sq.IsPullback) : sq.unop.IsPushout :=
   CategoryTheory.IsPullback.unop h.flip
 
-variable {D : Type u'} [Category.{v'} D] (F : C ⥤ D)
-
-lemma IsPullback.map {sq : Square C} [PreservesLimitsOfShape WalkingCospan F] (h : sq.IsPullback) :
-    (sq.map F).IsPullback :=
-  F.map_isPullback h
-
-lemma IsPullback.of_map {sq : Square C} [ReflectsLimitsOfShape WalkingCospan F]
-    (h : (sq.map F).IsPullback) : sq.IsPullback :=
-  CategoryTheory.IsPullback.of_map F sq.fac h
-
-lemma IsPullback.map_iff {sq : Square C} [PreservesLimitsOfShape WalkingCospan F]
-    [ReflectsLimitsOfShape WalkingCospan F] : (sq.map F).IsPullback ↔ sq.IsPullback :=
-  ⟨IsPullback.of_map _, IsPullback.map _⟩
-
-section
-
-variable
-  {sq₁ : Square (Type v)}
-  {sq₂ : Square (Type u)}
-  (e₁ : sq₁.X₁ ≃ sq₂.X₁) (e₂ : sq₁.X₂ ≃ sq₂.X₂)
-  (e₃ : sq₁.X₃ ≃ sq₂.X₃) (e₄ : sq₁.X₄ ≃ sq₂.X₄)
-  (comm₁₂ : e₂ ∘ sq₁.f₁₂ = sq₂.f₁₂ ∘ e₁)
-  (comm₁₃ : e₃ ∘ sq₁.f₁₃ = sq₂.f₁₃ ∘ e₁)
-  (comm₂₄ : e₄ ∘ sq₁.f₂₄ = sq₂.f₂₄ ∘ e₂)
-  (comm₃₄ : e₄ ∘ sq₁.f₃₄ = sq₂.f₃₄ ∘ e₃)
-
-variable (sq₁ sq₂) in
-lemma IsPullback.iff_of_equiv : sq₁.IsPullback ↔ sq₂.IsPullback := by
-  rw [← IsPullback.map_iff (sq := sq₁) uliftFunctor.{max u v},
-      ← IsPullback.map_iff (sq := sq₂) uliftFunctor.{max u v}]
-  apply iff_of_iso
-  refine Square.isoMk
-    (((Equiv.trans Equiv.ulift e₁).trans Equiv.ulift.symm).toIso)
-    (((Equiv.trans Equiv.ulift e₂).trans Equiv.ulift.symm).toIso)
-    (((Equiv.trans Equiv.ulift e₃).trans Equiv.ulift.symm).toIso)
-    (((Equiv.trans Equiv.ulift e₄).trans Equiv.ulift.symm).toIso)
-    ?_ ?_ ?_ ?_
-  all_goals ext; apply ULift.down_injective
-  · simpa [types_comp, uliftFunctor_map] using congrFun comm₁₂ _
-  · simpa [types_comp, uliftFunctor_map] using congrFun comm₁₃ _
-  · simpa [types_comp, uliftFunctor_map] using congrFun comm₂₄ _
-  · simpa [types_comp, uliftFunctor_map] using congrFun comm₃₄ _
-
-lemma IsPullback.of_equiv (h₁ : sq₁.IsPullback) : sq₂.IsPullback :=
-  (iff_of_equiv sq₁ sq₂ e₁ e₂ e₃ e₄ comm₁₂ comm₁₃ comm₂₄ comm₃₄).1 h₁
-
-end
-
 end Square
 
 end CategoryTheory
