edb8666b01ce29d41543858154d7c831564d4b75
Kim Morrison <kim@tqft.net>
2024-08-09 09:45:13 +1000

    fixes

--- Mathlib/RingTheory/Polynomial/SeparableDegree.lean
+++ Mathlib/RingTheory/Polynomial/SeparableDegree.lean
@@ -110,8 +110,9 @@ agree, then they have the same degree. -/
 theorem contraction_degree_eq_or_insep [hq : NeZero q] [CharP F q] (g g' : F[X]) (m m' : ℕ)
     (h_expand : expand F (q ^ m) g = expand F (q ^ m') g') (hg : g.Separable) (hg' : g'.Separable) :
     g.natDegree = g'.natDegree := by
+  clear f hf
   wlog hm : m ≤ m'
-  · exact (this q hf g' g m' m h_expand.symm hg' hg (le_of_not_le hm)).symm
+  · exact (this q g' g m' m h_expand.symm hg' hg (le_of_not_le hm)).symm
   obtain ⟨s, rfl⟩ := exists_add_of_le hm
   rw [pow_add, expand_mul, expand_inj (pow_pos (NeZero.pos q) m)] at h_expand
   subst h_expand
