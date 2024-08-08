dbd402aa89999d6ff17d7e5a81d3edf7bef4d07a
Jack McKoen <mckoen@ualberta.ca>
2024-08-07 20:26:34 -0600

    scrap work

---
+++ Mathlib/AlgebraicTopology/_Terminal.lean
@@ -0,0 +1,60 @@
+import Mathlib.AlgebraicTopology.SimplicialSet
+import Mathlib.CategoryTheory.Limits.Shapes.BinaryProducts
+import Mathlib.CategoryTheory.Limits.Shapes.Terminal
+import Mathlib.Tactic
+import Mathlib.Data.Fin.Basic
+
+open CategoryTheory Limits Simplicial SimplexCategory
+
+namespace CategoryTheory
+
+def isTerminalHom {C : Type _} [Category C] (X Y : C) (hY : IsTerminal Y) :
+    IsTerminal (X ⟶ Y) :=
+  letI : ∀ (W : Type _), Unique (W ⟶ (X ⟶ Y)) := fun W =>
+    { default := fun _ => hY.from _
+      uniq := fun a => by ext ; apply hY.hom_ext }
+  IsTerminal.ofUnique _
+
+def Functor.isTerminalOfObjIsTerminal {C D : Type _} [Category C] [Category D]
+    (F : C ⥤ D) (hF : ∀ X : C, IsTerminal (F.obj X)) :
+    IsTerminal F :=
+  letI : ∀ (G : C ⥤ D), Unique (G ⟶ F) := fun _ => {
+    default := {
+      app := fun _ => (hF _).from _
+      naturality := fun _ _ _ => (hF _).hom_ext _ _ }
+    uniq := fun _ => NatTrans.ext _ _ <| funext fun _ => (hF _).hom_ext _ _ }
+  IsTerminal.ofUnique _
+
+end CategoryTheory
+
+namespace SimplexCategory
+
+def isTerminalZero : IsTerminal ([0] : SimplexCategory) :=
+  letI : ∀ t : SimplexCategory, Unique (t ⟶ [0]) := fun t => {
+    default := SimplexCategory.Hom.mk <| OrderHom.const _ 0
+    uniq := fun m => SimplexCategory.Hom.ext _ _ <| OrderHom.ext _ _ <|
+      funext fun _ => Fin.ext <| by simp }
+  IsTerminal.ofUnique _
+
+end SimplexCategory
+
+namespace SSet
+
+def ptIsTerminal : IsTerminal Δ[0] := Functor.isTerminalOfObjIsTerminal Δ[0] <| by
+  intro t
+  sorry
+
+def binaryFan (X : SSet.{0}) : BinaryFan Δ[0] X :=
+  BinaryFan.mk (ptIsTerminal.from X) (𝟙 X)
+
+def isLimitBinaryFan (X : SSet.{0}) : IsLimit (binaryFan X) where
+  lift := fun (S : BinaryFan _ _) => S.snd
+  fac := fun (S : BinaryFan _ _) => by
+    rintro ⟨(_|_)⟩
+    · apply ptIsTerminal.hom_ext
+    · dsimp [binaryFan] ; simp
+  uniq := fun (S : BinaryFan _ _) m hm => by
+    specialize hm ⟨WalkingPair.right⟩
+    simpa [binaryFan] using hm
+
+end SSet
