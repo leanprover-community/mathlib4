3433c7033e6530573507358e7c47385d20eaef6a
FR <zhao.yu-yang@foxmail.com>
2024-08-09 01:00:51 +0800

    chore: `isSemireal` -> `IsSemireal`

--- Mathlib/Algebra/Ring/Semireal/Defs.lean
+++ Mathlib/Algebra/Ring/Semireal/Defs.lean
@@ -3,7 +3,6 @@ Copyright (c) 2024 Florent Schaffhauser. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Florent Schaffhauser
 -/
-
 import Mathlib.Algebra.Ring.SumsOfSquares
 import Mathlib.Algebra.Order.Field.Defs
 
@@ -19,7 +18,7 @@ semireal.
 
 ## Main declaration
 
-- `isSemireal`: the predicate asserting that a commutative ring `R` is semireal.
+- `IsSemireal`: the predicate asserting that a commutative ring `R` is semireal.
 
 ## References
 
@@ -31,14 +30,14 @@ variable (R : Type*)
 
 /--
 A semireal ring is a non-trivial commutative ring (with unit) in which `-1` is *not* a sum of
-squares. Note that `-1` does not make sense in a semiring. Below we define the class `isSemiReal R`
+squares. Note that `-1` does not make sense in a semiring. Below we define the class `IsSemireal R`
 for all additive monoid `R` equipped with a multiplication, a multiplicative unit and a negation.
 -/
 @[mk_iff]
-class isSemireal [AddMonoid R] [Mul R] [One R] [Neg R] : Prop where
+class IsSemireal [AddMonoid R] [Mul R] [One R] [Neg R] : Prop where
   non_trivial        : (0 : R) ≠ 1
   neg_one_not_SumSq  : ¬isSumSq (-1 : R)
 
-instance [LinearOrderedField R] : isSemireal R where
+instance [LinearOrderedField R] : IsSemireal R where
   non_trivial := zero_ne_one
   neg_one_not_SumSq := fun h ↦ (not_le (α := R)).2 neg_one_lt_zero h.nonneg
