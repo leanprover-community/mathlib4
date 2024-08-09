2eb69b94bf7515216fa3dbff9c36cfae26ccf34f
Matthew Robert Ballard <matt@mrb.email>
2024-08-09 11:40:09 +0000

    chore (Algebra.Order.Group.Int): split file into unbundled and bundled ordered algebra (#15069)
    All but ~5 lines of this file did not require bundled ordered algebra classes. We split them off into `Algebra.Order.Group.Unbundled.Int` leaving the `LinearOrderedAddCommGroup` instance.

--- Mathlib/Algebra/Order/Ring/Int.lean
+++ Mathlib/Algebra/Order/Ring/Int.lean
@@ -3,9 +3,9 @@ Copyright (c) 2016 Jeremy Avigad. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jeremy Avigad
 -/
-import Mathlib.Algebra.Ring.Int
-import Mathlib.Algebra.Order.Group.Int
+import Mathlib.Algebra.Order.Group.Unbundled.Int
 import Mathlib.Algebra.Order.Ring.Defs
+import Mathlib.Algebra.Ring.Int
 import Mathlib.Data.Set.Basic
 
 /-!
--- Mathlib/Data/Int/Star.lean
+++ Mathlib/Data/Int/Star.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yaël Dillies
 -/
 import Mathlib.Algebra.Star.Order
+import Mathlib.Algebra.Order.Group.Abs
 import Mathlib.Algebra.Order.Monoid.Submonoid
 import Mathlib.Algebra.Order.Ring.Basic
 
