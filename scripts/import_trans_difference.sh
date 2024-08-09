25afe3bdff342a9267514d09067919ac1fab9feb
adomani <adomani@gmail.com>
2024-08-09 12:59:56 +0200

    add zero

--- scripts/import_trans_difference.sh
+++ scripts/import_trans_difference.sh
@@ -65,17 +65,17 @@ getFormattedTransImports () {
          ($2 == "")   { name[$1]="`"$1"`"$3 }
       END {
         for(fil in name)
-        { printf("%s,%s\n", name[fil], record[fil]) }
+        { printf("%s,%s\n", name[fil], record[fil]+0) }
       }'
 }
 
 git checkout "${commit1}"
-git checkout master scripts/count-trans-deps.py
+#git checkout master scripts/count-trans-deps.py
 getFormattedTransImports "${commit2}" > transImports1.txt
 git checkout "${currCommit}"
 
 git checkout "${commit2}"
-git checkout master scripts/count-trans-deps.py
+#git checkout master scripts/count-trans-deps.py
 getFormattedTransImports "${commit1}" > transImports2.txt
 git checkout "${currCommit}"
 
