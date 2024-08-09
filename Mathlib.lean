f73cb98a63ded1ed03f569db41ecfefa9e208c65
Tom Kranz <TpmKranz@users.noreply.github.com>
2024-08-09 16:00:10 +0200

    Introduce intermediary between NFA and RegEx

--- Mathlib.lean
+++ Mathlib.lean
@@ -1954,6 +1954,7 @@ import Mathlib.Computability.ContextFreeGrammar
 import Mathlib.Computability.DFA
 import Mathlib.Computability.Encoding
 import Mathlib.Computability.EpsilonNFA
+import Mathlib.Computability.GNFA
 import Mathlib.Computability.Halting
 import Mathlib.Computability.Language
 import Mathlib.Computability.NFA
