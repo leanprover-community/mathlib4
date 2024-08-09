bae7ce0e5b5319d1dba9f7fa865a109405e77475
blizzard_inc <edegeltje@gmail.com>
2024-08-09 12:10:22 +0200

    Apply suggestions from code review
    Co-authored-by: Jireh Loreaux <loreaujy@gmail.com>

--- Mathlib/Topology/Algebra/InfiniteSum/ENNReal.lean
+++ Mathlib/Topology/Algebra/InfiniteSum/ENNReal.lean
@@ -12,46 +12,25 @@ import Mathlib.Topology.Instances.ENNReal
 
 This file provides lemmas relating sums of constants to the cardinality of the domain of these sums.
 
+## TODO
+
++ Once we have a topology on `ENat`, provide an `ENat` valued version
++ Once we replace `PartENat` entirely with `ENat` (and get a `ENat.card` to replace `PartENat.card`), provide versions which sum over the whole type.
 -/
 
 open Set Function
 
+variable {α : Type*} (s : Set α)
+
 namespace ENNReal
 
-lemma tsum_one_eq' {α : Type*} (s : Set α) : ∑' (_:s), (1 : ℝ≥0∞) = s.encard := by
-  if hfin : s.Finite then
-    have hfin' : Finite s := by exact hfin
-    rw [tsum_def]
-    simp only [ENNReal.summable, ↓reduceDIte]
-    have hsup: support (fun (_ : s) ↦ (1 : ℝ≥0∞)) = Set.univ := by
-      ext i
-      simp only [mem_support, ne_eq, one_ne_zero, not_false_eq_true, mem_univ]
-    have hsupfin: (Set.univ : Set s).Finite := by exact finite_univ
-    rw [← hsup] at hsupfin
-    rw [if_pos hsupfin]
-    rw [hfin.encard_eq_coe_toFinset_card]
-    simp only [ENat.toENNReal_coe]
-    rw [Finset.card_eq_sum_ones]
-    rw [finsum_eq_sum (fun (_ : s) ↦ (1 :ℝ≥0∞)) hsupfin]
-    simp only [Finset.sum_const, nsmul_eq_mul, mul_one, smul_eq_mul, Nat.cast_inj]
-    apply Finset.card_bij (fun a _ => a.val)
-    · intro a
-      simp only [Finite.mem_toFinset, mem_support, ne_eq, one_ne_zero, not_false_eq_true,
-        Subtype.coe_prop, imp_self]
-    · intro a _ a' _ heq
-      ext
-      exact heq
-    · intro a ha
-      use ⟨a,by
-        simp only [Finite.mem_toFinset] at ha
-        exact ha⟩
-      simp only [Finite.mem_toFinset, mem_support, ne_eq, one_ne_zero, not_false_eq_true,
-        exists_const]
-  else
-  have : Infinite s := by exact infinite_coe_iff.mpr hfin
-  rw [tsum_const_eq_top_of_ne_zero (by norm_num)]
-  rw [Set.encard_eq_top_iff.mpr hfin]
-  simp only [ENat.toENNReal_top]
+@[simp]
+lemma tsum_set_one_eq : ∑' (_ : s), (1 : ℝ≥0∞) = s.encard := by
+  obtain (hfin | hinf) := Set.finite_or_infinite s
+  · lift s to Finset α using hfin
+    simp [tsum_fintype]
+  · have : Infinite s := infinite_coe_iff.mpr hinf
+    rw [tsum_const_eq_top_of_ne_zero one_ne_zero, encard_eq_top hinf, ENat.toENNReal_top]
 
 lemma tsum_const_eq' {α : Type*} (s : Set α) (c : ℝ≥0∞) :
     ∑' (_:s), (c : ℝ≥0∞) = s.encard * c := by
