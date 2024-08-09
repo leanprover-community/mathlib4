b36c6f8a07fbe46614becebf73dc671efb5e8699
blizzard_inc <thrashy4inbox@gmail.com>
2024-08-09 14:31:43 +0000

    feat: add lemma relating Set.encard to tsum over ennreal  (#15214)
    two lemmas: `s.tsum (1:ENNReal) = s.encard` and `s.tsum c = s.encard * c`, where `c:ENNReal` and `s : Set α`.
    
    
    
    Co-authored-by: blizzard_inc <edegeltje@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -4263,6 +4263,7 @@ import Mathlib.Topology.Algebra.GroupWithZero
 import Mathlib.Topology.Algebra.InfiniteSum.Basic
 import Mathlib.Topology.Algebra.InfiniteSum.Constructions
 import Mathlib.Topology.Algebra.InfiniteSum.Defs
+import Mathlib.Topology.Algebra.InfiniteSum.ENNReal
 import Mathlib.Topology.Algebra.InfiniteSum.Group
 import Mathlib.Topology.Algebra.InfiniteSum.GroupCompletion
 import Mathlib.Topology.Algebra.InfiniteSum.Module
