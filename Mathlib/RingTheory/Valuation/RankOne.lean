cd23601b1aa5ad9c7944dd93cc3f0f1d985cfda2
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2024-08-10 22:47:47 +0200

    add valuations of rank at most one

--- Mathlib/RingTheory/Valuation/RankOne.lean
+++ Mathlib/RingTheory/Valuation/RankOne.lean
@@ -12,8 +12,11 @@ import Mathlib.RingTheory.Valuation.Basic
 We define rank one valuations.
 
 ## Main Definitions
-* `RankOne` : A valuation `v` has rank one if it is nontrivial and its image is contained in `ℝ≥0`.
-  Note that this class contains the data of the inclusion of the codomain of `v` into `ℝ≥0`.
+
+* `RankLeOne` : A valuation `v` has rank at most one it its image is contained in `ℝ≥0` 
+  Note that this class contains the data of the inclusion of the codomain of `v` into `ℝ≥0`. 
+
+* `RankOne` : A valuation `v` has rank one if it is nontrivial and of rank at most one. 
 
 ## Tags
 
@@ -30,17 +33,21 @@ variable {R : Type*} [Ring R] {Γ₀ : Type*} [LinearOrderedCommGroupWithZero Γ
 
 namespace Valuation
 
-/-- A valuation has rank one if it is nontrivial and its image is contained in `ℝ≥0`.
+/-- A valuation has rank at most one if its image is contained in `ℝ≥0`.
   Note that this class includes the data of an inclusion morphism `Γ₀ → ℝ≥0`. -/
-class RankOne (v : Valuation R Γ₀) where
+class RankLeOne (v : Valuation R Γ₀) where
   /-- The inclusion morphism from `Γ₀` to `ℝ≥0`. -/
   hom : Γ₀ →*₀ ℝ≥0
   strictMono' : StrictMono hom
+
+/-- A valuation has rank one if it is nontrivial and its image is contained in `ℝ≥0`.
+  Note that this class includes the data of an inclusion morphism `Γ₀ → ℝ≥0`. -/
+class RankOne (v : Valuation R Γ₀) extends RankLeOne v where
   nontrivial' : ∃ r : R, v r ≠ 0 ∧ v r ≠ 1
 
-namespace RankOne
+namespace RankLeOne
 
-variable (v : Valuation R Γ₀) [RankOne v]
+variable (v : Valuation R Γ₀) [RankLeOne v]
 
 lemma strictMono : StrictMono (hom v) := strictMono'
 
@@ -53,20 +60,26 @@ theorem strictMono_rangeGroup : StrictMono (hom_rangeGroup v) := by
   intro x y h
   simpa only [Units.val_lt_val, Subtype.coe_lt_coe, h] using (strictMono v h)
 
-lemma nontrivial : ∃ r : R, v r ≠ 0 ∧ v r ≠ 1 := nontrivial'
-
-/-- If `v` is a rank one valuation and `x : Γ₀` has image `0` under `RankOne.hom v`, then
-  `x = 0`. -/
+/-- If `v` is a valuation of rank at most one, 
+and if `x : Γ₀` has image `0` under `RankLeOne.hom v`, then `x = 0`. -/
 theorem zero_of_hom_zero {x : Γ₀} (hx : hom v x = 0) : x = 0 := by
   refine (eq_of_le_of_not_lt (zero_le' (a := x)) fun h_lt ↦ ?_).symm
   have hs := strictMono v h_lt
   rw [_root_.map_zero, hx] at hs
   exact hs.false
 
-/-- If `v` is a rank one valuation, then`x : Γ₀` has image `0` under `RankOne.hom v` if and
-  only if `x = 0`. -/
-theorem hom_eq_zero_iff {x : Γ₀} : RankOne.hom v x = 0 ↔ x = 0 :=
+/-- If `v` is a valuation of rank at most one, 
+then`x : Γ₀` has image `0` under `RankLeOne.hom v` if and only if `x = 0`. -/
+theorem hom_eq_zero_iff {x : Γ₀} : hom v x = 0 ↔ x = 0 :=
   ⟨fun h ↦ zero_of_hom_zero v h, fun h ↦ by rw [h, _root_.map_zero]⟩
+
+end RankLeOne
+
+namespace RankOne
+
+variable (v : Valuation R Γ₀) [RankOne v]
+
+lemma nontrivial : ∃ r : R, v r ≠ 0 ∧ v r ≠ 1 := nontrivial'
 
 /-- A nontrivial unit of `Γ₀`, given that there exists a rank one `v : Valuation R Γ₀`. -/
 def unit : Γ₀ˣ :=
