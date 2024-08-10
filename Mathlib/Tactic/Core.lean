f201c5be88564da0ef68ee5d6f2642f3c22704bc
Kim Morrison <kim@tqft.net>
2024-08-10 15:38:38 +0000

    chore: cleanup Init imports (#15638)

--- Mathlib/Tactic/Core.lean
+++ Mathlib/Tactic/Core.lean
@@ -4,7 +4,9 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Arthur Paulino, Aurélien Saue, Mario Carneiro
 -/
 import Lean.Elab.PreDefinition.Basic
+import Lean.Elab.Tactic.ElabTerm
 import Lean.Util.Paths
+import Lean.Meta.Tactic.Intro
 import Mathlib.Lean.Expr.Basic
 import Batteries.Tactic.OpenPrivate
 
