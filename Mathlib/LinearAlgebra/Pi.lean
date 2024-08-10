509c08cb28b436396ee178b4dd92530999bf9edc
Patrick Massot <patrickmassot@free.fr>
2024-08-10 16:15:58 +0200

    Remove an import no longer needed

--- Mathlib/LinearAlgebra/Pi.lean
+++ Mathlib/LinearAlgebra/Pi.lean
@@ -3,7 +3,6 @@ Copyright (c) 2017 Johannes Hölzl. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl, Mario Carneiro, Kevin Buzzard, Yury Kudryashov, Eric Wieser
 -/
-import Mathlib.Data.Finsupp.Defs
 import Mathlib.Algebra.Group.Fin.Tuple
 import Mathlib.Algebra.BigOperators.Pi
 import Mathlib.Algebra.Module.Prod
