38f88b614faaf941931f4228d3eb39c9c5f53ebf
Rida Hamadani <mridahamadani@gmail.com>
2024-08-10 12:43:33 +0000

    only connectivity

--- Mathlib.lean
+++ Mathlib.lean
@@ -1911,7 +1911,6 @@ import Mathlib.Combinatorics.SimpleGraph.Connectivity.WalkCounting
 import Mathlib.Combinatorics.SimpleGraph.Dart
 import Mathlib.Combinatorics.SimpleGraph.DegreeSum
 import Mathlib.Combinatorics.SimpleGraph.Density
-import Mathlib.Combinatorics.SimpleGraph.Diameter
 import Mathlib.Combinatorics.SimpleGraph.Ends.Defs
 import Mathlib.Combinatorics.SimpleGraph.Ends.Properties
 import Mathlib.Combinatorics.SimpleGraph.Finite
