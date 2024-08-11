755f05def6139ad4b635f22e4c30b70257ae2e2a
adomani <adomani@gmail.com>
2024-08-11 22:47:01 +0200

    add gitinfo

--- scripts/import_trans_difference.sh
+++ scripts/import_trans_difference.sh
@@ -99,6 +99,35 @@ importsDifferences () {
   }' transImports*.txt
 }
 
+addGitInfo () {
+(
+  importsDifferences
+  #getTransImports #| grep "Topology\.Shea"
+  git diff --name-status "${commit2}".."${commit1}" |
+    awk '/^[^M]/{
+      gsub(/\//, ".")
+      gsub(/\.lean/, "")
+      printf("%s,,%s\n", $NF, $(NF-1))
+      if(NF == "3") {printf("%s,rev,%s\n", $(NF-1), $NF)}
+    }'
+) |
+  awk -F, '
+    (NF == 2) { imports[$1]=$2 }
+    (NF == 3) { status[$1]=$3; if($2 == "rev") { rename[$3]=$1 } }
+    END {
+      for(fil in imports) {
+        if (fil in rename) { stat=sprintf(" (was called `%s`)", status[fil]) }
+        else if (status[fil] == "A") { stat=" (new file)" }
+        else if (status[fil] == "D") { stat=" (deleted file)" }
+        else if (status[fil] == "") { stat="" }
+        else { stat=sprintf(" (became `%s`)", status[fil]) }
+        printf("`%s`%s,%s\n", fil, stat, imports[fil])
+      }
+    }'
+}
+
+git diff --name-status "${commit2}".."${commit1}" |
+  awk '/^[^M]/{ gsub(/\//, "."); gsub(/\.lean/, ""); printf("%s,,%s\n", $NF, $(NF-1))}'
 
 printf '\n\n<details><summary>Import changes for all files</summary>\n\n%s\n\n</details>\n' "$(
   printf "|Files|Import difference|\n|-|-|\n"
@@ -125,3 +154,25 @@ formatGitDiff "${commit1}" &&
 printf 'formatGitDiff %s\n' "${commit2}" &&
 formatGitDiff "${commit2}"
 head -50 transImports1.txt
+
+
+
+printf '\n\n<details><summary>Import changes for all files</summary>\n\n%s\n\n</details>\n' "$(
+  printf "|Files|Import difference|\n|-|-|\n"
+  (addGitInfo | awk -F, '{ diff[$1]+=$2 } END {
+    con=0
+    for(fil in diff) {
+      if(!(diff[fil] == 0)) {
+        con++
+        nums[diff[fil]]++
+        reds[diff[fil]]=reds[diff[fil]]" "fil
+      }
+    }
+    if (10000 <= con) { printf("There are %s files with changed transitive imports: this is too many to display!\n", con) } else {
+      for(x in reds) {
+        if (nums[x] <= 2) { printf("|%s|%s|\n", reds[x], x) }
+        else { printf("|<details><summary>%s files</summary>%s</details>|%s|\n", nums[x], reds[x], x) }
+      }
+    }
+  }' | sort -t'|' -n -k3 | grep "ew"
+  ))"
