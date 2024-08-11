33a6d1f97b6c334692a9eb3d360ad23280bb7e14
Shuhao Song <znssong_meow@sina.com>
2024-08-12 06:22:17 +0800

    fix: Update `Increasing.lean`

--- Mathlib/Dynamics/FixedPoints/Increasing.lean
+++ Mathlib/Dynamics/FixedPoints/Increasing.lean
@@ -1,8 +1,11 @@
 /-
 Copyright (c) 2024 Shuhao Song. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Shuhao Song
+Authors: Shuhao Song, Yury Kudryashov
 -/
+import Mathlib.Order.Filter.EventuallyConst
+import Mathlib.Order.Iterate
+import Mathlib.Order.WellFounded
 import Mathlib.Data.Fintype.Card
 
 /-!
@@ -19,80 +22,49 @@ a finite type, then the sequence `x, f(x), f(f(x)), ...` will eventually be a co
 -/
 namespace Function
 
-variable {α : Type*} [PartialOrder α] [Fintype α] {f : α → α} (hf : ∀ x, x ≤ f x)
+open Filter
+variable {α : Type*} [PartialOrder α] [Finite α] {f : α → α}
 
-/-- If `x` is a fixed point of `f`, then the nᵗʰ iteration of `f` on `x` is `x`. -/
-lemma iterate_fixed_point {x : α} (hx : IsFixedPt f x) (n : ℕ) : f^[n] x = x := by
-  induction n with
-  | zero => simp
-  | succ m ih =>
-    rw [add_comm, iterate_add_apply]
-    simpa [ih] using hx
-
-/-- If `f^[m + 1] x = f^[m] x`, then the sequence `n ↦ f^[n] x` is constant after index `m`. -/
-lemma iterate_fixed_point_from_index
-    {m n : ℕ} {x : α} (hx : f^[m + 1] x = f^[m] x) (hn : n ≥ m) : f^[n] x = f^[m] x := by
-  rw [add_comm, iterate_add_apply] at hx
-  rw [show n = n - m + m by omega, iterate_add_apply]
-  apply iterate_fixed_point hx
-
-/-- Monotonicity of `n ↦ f^[n] x`. -/
-lemma monotone_iterate (x : α) : Monotone (fun n => f^[n] x) := by
-  intro i j le
-  simp only
-  induction j with
-  | zero => simp at le; rw [le]
-  | succ k ih =>
-    replace le : i ≤ k ∨ i = k + 1 := by omega
-    cases' le with le le
-    · rw [add_comm, iterate_add_apply _ 1 k]
-      exact (ih le).trans (hf _)
-    · rw [le]
+/-- The function `g : ι → α` will eventually be constant if `>` relation on `α` is well-ordered. -/
+lemma eventually_constant_monotone {ι : Type*}
+    [SemilatticeSup ι] [Nonempty ι] [hα : WellFoundedGT α] {g : ι → α} (hg : Monotone g) :
+    EventuallyConst g atTop := by
+  rw [eventuallyConst_atTop]
+  let S := Set.range g
+  have hS : S.Nonempty := by simp [S, Set.range_nonempty]
+  let y := hα.wf.min S hS
+  obtain ⟨x, hx⟩ : ∃ x, g x = y := hα.wf.min_mem S hS
+  exact ⟨x, fun z hz =>
+    eq_of_ge_of_not_gt (hg hz) (hx ▸ hα.wf.not_lt_min S hS (by simp [S]))⟩
 
 /-- The theorem states that the iteration will eventually become a constant. -/
-lemma eventually_constant_iterate (x : α) :
-    ∃ n, ∀ m ≥ n, f^[m] x = f^[n] x := by
-  obtain ⟨m, n, h₁, h₂⟩ := Finite.exists_ne_map_eq_of_infinite (fun n => f^[n] x)
-  use min m n
-  intro n' hn'
-  have eq : f^[min m n] x = f^[min m n + 1] x := by
-    have ineq₁ : f^[min m n] x ≤ f^[min m n + 1] x := by
-      apply monotone_iterate hf
-      omega
-    have ineq₂ : f^[min m n + 1] x ≤ f^[max m n] x := by
-      apply monotone_iterate hf
-      dsimp; omega
-    replace h₂ : f^[min m n] x = f^[max m n] x := by
-      by_cases h : m <= n
-      · simpa [h]
-      · replace h : n ≤ m := by omega
-        simp [h]
-        exact h₂.symm
-    exact le_antisymm ineq₁ (h₂ ▸ ineq₂)
-  exact iterate_fixed_point_from_index eq.symm hn'
+lemma eventually_constant_iterate (hf : id ≤ f) (x : α) :
+    EventuallyConst (fun n => f^[n] x) atTop :=
+  eventually_constant_monotone (fun _ _ h => monotone_iterate_of_id_le hf h x)
 
 /-- The index at which the iteration sequence of `f` on `x` starts to become constant. -/
-noncomputable def fixedPointIndex (x : α) : ℕ := (eventually_constant_iterate hf x).choose
+noncomputable def fixedPointIndex (hf : id ≤ f) (x : α) : ℕ :=
+  (eventuallyConst_atTop.mp (eventually_constant_iterate hf x)).choose
 
-lemma fixedPointIndex_spec (x : α) :
+lemma fixedPointIndex_spec (hf : id ≤ f) (x : α) :
     ∀ m ≥ fixedPointIndex hf x, f^[m] x = f^[fixedPointIndex hf x] x :=
-  (eventually_constant_iterate hf x).choose_spec
+  (eventuallyConst_atTop.mp (eventually_constant_iterate hf x)).choose_spec
 
 /-- The eventual value of iteration of `f` on `x`. -/
-noncomputable def eventualValue (x : α) := f^[fixedPointIndex hf x] x
+noncomputable def eventualValue (hf : id ≤ f) (x : α) := f^[fixedPointIndex hf x] x
 
 /-- The eventual value is a fixed point of `f`. -/
-lemma fixed_eventualValue (x : α) : IsFixedPt f (eventualValue hf x) := by
+lemma fixed_eventualValue (hf : id ≤ f) (x : α) : IsFixedPt f (eventualValue hf x) := by
   unfold IsFixedPt
   simp only [eventualValue, ← iterate_succ_apply']
   apply fixedPointIndex_spec
   simp
 
 /-- The eventual value is larger or equal than `x` itself. -/
-lemma self_le_eventualValue (x : α) : x ≤ eventualValue hf x := by
+lemma self_le_eventualValue (hf : id ≤ f) (x : α) : x ≤ eventualValue hf x := by
   simp only [eventualValue]
   conv_lhs => rw [show x = f^[0] x from rfl]
-  apply monotone_iterate hf
+  apply f.monotone_iterate_of_id_le hf
   simp
 
 end Function
