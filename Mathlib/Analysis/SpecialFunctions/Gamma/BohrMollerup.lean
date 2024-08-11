46d7226dfdded8802f57eaef93aae3cd213ad421
Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>
2024-08-11 17:39:38 +0000

    Update BohrMollerup.lean

--- Mathlib/Analysis/SpecialFunctions/Gamma/BohrMollerup.lean
+++ Mathlib/Analysis/SpecialFunctions/Gamma/BohrMollerup.lean
@@ -152,13 +152,14 @@ theorem f_nat_eq (hf_feq : ∀ {y : ℝ}, 0 < y → f (y + 1) = f y + log y) (hn
 
 theorem f_add_nat_eq (hf_feq : ∀ {y : ℝ}, 0 < y → f (y + 1) = f y + log y) (hx : 0 < x) (n : ℕ) :
     f (x + n) = f x + ∑ m ∈ Finset.range n, log (x + m) := by
-  induction' n with n hn
-  · simp
-  · have : x + n.succ = x + n + 1 := by push_cast; ring
-    rw [this, hf_feq, hn]
-    · rw [Finset.range_succ, Finset.sum_insert Finset.not_mem_range_self]
-      abel
-    · linarith [(Nat.cast_nonneg n : 0 ≤ (n : ℝ))]
+  induction n with
+  | zero => simp
+  | succ n hn =>
+      have : x + n.succ = x + n + 1 := by push_cast; ring
+      rw [this, hf_feq, hn]
+      · rw [Finset.range_succ, Finset.sum_insert Finset.not_mem_range_self]
+        abel
+      · linarith [(Nat.cast_nonneg n : 0 ≤ (n : ℝ))]
 
 /-- Linear upper bound for `f (x + n)` on unit interval -/
 theorem f_add_nat_le (hf_conv : ConvexOn ℝ (Ioi 0) f)
