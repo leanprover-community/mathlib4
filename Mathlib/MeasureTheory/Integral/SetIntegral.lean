a194bc687dd19b04b3246c7e8cf743123d8bc9f6
Kim Morrison <kim@tqft.net>
2024-08-12 12:10:16 +1000

    fix

--- Mathlib/MeasureTheory/Integral/SetIntegral.lean
+++ Mathlib/MeasureTheory/Integral/SetIntegral.lean
@@ -1268,7 +1268,7 @@ end ContinuousLinearEquiv
 
 namespace ContinuousMap
 
-lemma integral_apply [TopologicalSpace Y] [CompactSpace Y] [NormedAddCommGroup E] [NormedSpace ℝ E]
+lemma integral_apply [TopologicalSpace Y] [CompactSpace Y] [NormedSpace ℝ E]
     [CompleteSpace E] {f : X → C(Y, E)} (hf : Integrable f μ) (y : Y) :
     (∫ x, f x ∂μ) y = ∫ x, f x y ∂μ := by
   calc (∫ x, f x ∂μ) y = ContinuousMap.evalCLM ℝ y (∫ x, f x ∂μ) := rfl
