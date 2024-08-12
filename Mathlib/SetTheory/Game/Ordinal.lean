579b0099f2e4297b617ddf64fdff3ff0242ff941
Violeta Hernández <vi.hdz.p@gmail.com>
2024-08-12 02:05:33 -0600

    Add reference + fix

--- Mathlib/SetTheory/Game/Ordinal.lean
+++ Mathlib/SetTheory/Game/Ordinal.lean
@@ -214,7 +214,7 @@ theorem nat_toGame : ∀ n : ℕ, toGame n = n
   | 0 => Quot.sound (zeroToPGameRelabelling).equiv
   | n + 1 => by
     have : toGame 1 = 1 := Quot.sound oneToPGameRelabelling.equiv
-    rw [Nat.cast_add, ← nadd_nat, toGame_add, nat_toGame, Nat.cast_one, this]
+    rw [Nat.cast_add, ← nadd_nat, toGame_nadd, nat_toGame, Nat.cast_one, this]
     rfl
 
 theorem nat_toPGame (n : ℕ) : toPGame n ≈ n := by
