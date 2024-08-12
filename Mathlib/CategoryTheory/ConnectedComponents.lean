6582149d19dc4f37028414cc3c118132daaf5391
Nicolas Rolland <nrolland@users.noreply.github.com>
2024-08-12 22:49:32 +0000

    feat(CategoryTheory/Adjunction): `typeToCat` is right adjoint to `connectedComponents` functor (#15614)
    The embedding of `Type` into `Cat`, which views a set as a discrete category, is right adjoint to the functor `connectedComponents : Cat -> Set` which maps a category to its set of connected components.

--- Mathlib/CategoryTheory/ConnectedComponents.lean
+++ Mathlib/CategoryTheory/ConnectedComponents.lean
@@ -31,7 +31,6 @@ namespace CategoryTheory
 attribute [instance 100] IsConnected.is_nonempty
 
 variable {J : Type u₁} [Category.{v₁} J]
-variable {C : Type u₂} [Category.{u₁} C]
 
 /-- This type indexes the connected components of the category `J`. -/
 def ConnectedComponents (J : Type u₁) [Category.{v₁} J] : Type u₁ :=
@@ -39,6 +38,32 @@ def ConnectedComponents (J : Type u₁) [Category.{v₁} J] : Type u₁ :=
 
 instance [Inhabited J] : Inhabited (ConnectedComponents J) :=
   ⟨Quotient.mk'' default⟩
+
+/-- Every function from connected components of a category gives a functor to discrete category -/
+def ConnectedComponents.functorToDiscrete   (X : Type*)
+    (f : ConnectedComponents J → X) : J ⥤ Discrete X where
+  obj Y :=  Discrete.mk (f (Quotient.mk (Zigzag.setoid _) Y))
+  map g := Discrete.eqToHom (congrArg f (Quotient.sound (Zigzag.of_hom g)))
+
+/-- Every functor to a discrete category gives a function from connected components -/
+def ConnectedComponents.liftFunctor  (J) [Category J] {X : Type*} (F :J ⥤ Discrete X) :
+    (ConnectedComponents J → X) :=
+  Quotient.lift (fun c => (F.obj c).as)
+    (fun _ _ h => eq_of_zigzag X (zigzag_obj_of_zigzag F h))
+
+/-- Functions from connected components and functors to discrete category are in bijection -/
+def ConnectedComponents.typeToCatHomEquiv (J) [Category J] (X : Type*) :
+    (ConnectedComponents J → X) ≃ (J ⥤ Discrete X)   where
+  toFun := ConnectedComponents.functorToDiscrete _
+  invFun := ConnectedComponents.liftFunctor _
+  left_inv := fun f ↦ funext fun x ↦ by
+    obtain ⟨x, h⟩ := Quotient.exists_rep x
+    rw [← h]
+    rfl
+  right_inv  := fun fctr ↦
+    Functor.hext (fun _ ↦ rfl) (fun c d f ↦
+      have : Subsingleton (fctr.obj c ⟶ fctr.obj d) := Discrete.instSubsingletonDiscreteHom _ _
+      (Subsingleton.elim (fctr.map f) _).symm.heq)
 
 /-- Given an index for a connected component, produce the actual component as a full subcategory. -/
 def Component (j : ConnectedComponents J) : Type u₁ :=
