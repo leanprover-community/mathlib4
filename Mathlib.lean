953a06889352a7014313839b044c8f34548b6c8e
mariainesdff <mariaines.dff@gmail.com>
2024-08-12 14:07:03 +0200

    feat(RingTheory.Valuation.Minpoly): add valuation lemmas

--- Mathlib.lean
+++ Mathlib.lean
@@ -3940,6 +3940,7 @@ import Mathlib.RingTheory.Valuation.Basic
 import Mathlib.RingTheory.Valuation.ExtendToLocalization
 import Mathlib.RingTheory.Valuation.Integers
 import Mathlib.RingTheory.Valuation.Integral
+import Mathlib.RingTheory.Valuation.Minpoly
 import Mathlib.RingTheory.Valuation.PrimeMultiplicity
 import Mathlib.RingTheory.Valuation.Quotient
 import Mathlib.RingTheory.Valuation.RamificationGroup
