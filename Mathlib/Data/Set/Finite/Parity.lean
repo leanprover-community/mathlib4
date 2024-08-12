daba549fdc74b3868dbb738d1d749ebc8494c134
Pim Otte <otte.pim@gmail.com>
2024-08-12 09:24:40 +0200

    Address PR comments and some golfing

--- Mathlib/Data/Set/Finite/Parity.lean
+++ Mathlib/Data/Set/Finite/Parity.lean
@@ -17,42 +17,31 @@ namespace Set.Finite
 variable {V : Type _} {s : Set V}
 
 lemma one_lt_ncard_of_nonempty_of_even (hs : Set.Finite s) (hn : Set.Nonempty s)
-    (he : Even (Nat.card s)) : 1 < s.ncard := by
+    (he : Even (s.ncard)) : 1 < s.ncard := by
   have : s.ncard ≠ 0 := by
     intro h
     rw [@Set.nonempty_iff_ne_empty] at hn
     exact hn <| (Set.ncard_eq_zero hs).mp h
   have : s.ncard ≠ 1 := by
     intro h
-    rw [Set.Nat.card_coe_set_eq, h] at he
-    simp at he
+    simp [h] at he
   omega
 
-lemma two_of_even_of_nonempty (hs : Set.Finite s) (hn : Set.Nonempty s) (he : Even (Nat.card s)) :
-    ∃ a b, a ∈ s ∧ b ∈ s ∧ a ≠ b := by
-  exact (Set.one_lt_ncard_iff hs).mp (one_lt_ncard_of_nonempty_of_even hs hn he)
-
 lemma even_card_diff_pair [DecidableEq V] {x y : V} {u : Set V} (hu : Set.Finite u)
-    (he : Even (Nat.card u)) (hx : x ∈ u) (hy : y ∈ u) (hxy : x ≠ y) :
-    Even (Nat.card (u \ {x, y} : Set V)) := by
+    (he : Even u.ncard) (hx : x ∈ u) (hy : y ∈ u) (hxy : x ≠ y) :
+    Even ((u \ {x, y} : Set V).ncard) := by
   haveI : Fintype u := hu.fintype
-  rw [Nat.card_eq_card_finite_toFinset (hu.diff _), Set.Finite.toFinset.eq_1]
-  rw [Set.toFinset_diff]
-  rw [Finset.card_sdiff (by
-    simp only [Set.toFinset_insert, Set.toFinset_singleton, Set.subset_toFinset,
-      Finset.coe_insert, Finset.coe_singleton]
-    exact Set.pair_subset hx hy
+  rw [Set.ncard_eq_toFinset_card', Set.toFinset_diff, Finset.card_sdiff (by
+    simp only [Set.toFinset_insert, Set.toFinset_singleton, Set.subset_toFinset, Finset.coe_insert,
+      Finset.coe_singleton, Set.pair_subset hx hy]
     )]
   simp only [Set.toFinset_card, Set.toFinset_insert, Set.toFinset_singleton,
     Finset.mem_singleton, hxy, not_false_eq_true, Finset.card_insert_of_not_mem,
     Finset.card_singleton, Nat.reduceAdd]
   rw [Nat.even_sub (by
-    have := Set.Finite.one_lt_ncard_of_nonempty_of_even hu (Set.nonempty_of_mem hx) he
-    rw [← Set.Nat.card_coe_set_eq,] at this
-    rwa [Fintype.card_eq_nat_card]
-    )]
+    simpa [← Set.Nat.card_coe_set_eq, ← Fintype.card_eq_nat_card] using
+      Set.Finite.one_lt_ncard_of_nonempty_of_even hu (Set.nonempty_of_mem hx) he)]
   simp only [even_two, iff_true]
-  rw [Fintype.card_eq_nat_card]
-  exact he
+  rwa [Fintype.card_eq_nat_card, Nat.card_coe_set_eq]
 
 end Set.Finite
