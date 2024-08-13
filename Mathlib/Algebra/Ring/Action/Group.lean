b6eff951512970bb553cef680bb3b792fb799018
Yaël Dillies <yael.dillies@gmail.com>
2024-08-13 08:09:53 +0000

    chore(Algebra/Group/Aut): Do not import `MonoidWithZero` (#15430)
    Move what needs it to a new file `Algebra.GroupWithZero.Action.Basic` which will eventually contain more content coming from `Algebra.SMulWithZero`.

--- Mathlib/Algebra/Ring/Action/Group.lean
+++ Mathlib/Algebra/Ring/Action/Group.lean
@@ -3,8 +3,8 @@ Copyright (c) 2020 Kenny Lau. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kenny Lau
 -/
+import Mathlib.Algebra.GroupWithZero.Action.Basic
 import Mathlib.Algebra.Ring.Action.Basic
-import Mathlib.GroupTheory.GroupAction.Group
 import Mathlib.Algebra.Ring.Equiv
 
 /-!
