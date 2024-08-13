52ec943616f9d2f3f838c3cf11296308323353fa
Jireh Loreaux <loreaujy@gmail.com>
2024-08-13 17:25:05 +0000

    feat: convenience lemmas for replacing the uniformity and bornology via bilipschitz equivalence (#15760)

--- Mathlib.lean
+++ Mathlib.lean
@@ -4491,6 +4491,7 @@ import Mathlib.Topology.Maps.Proper.UniversallyClosed
 import Mathlib.Topology.MetricSpace.Algebra
 import Mathlib.Topology.MetricSpace.Antilipschitz
 import Mathlib.Topology.MetricSpace.Basic
+import Mathlib.Topology.MetricSpace.Bilipschitz
 import Mathlib.Topology.MetricSpace.Bounded
 import Mathlib.Topology.MetricSpace.CantorScheme
 import Mathlib.Topology.MetricSpace.CauSeqFilter
