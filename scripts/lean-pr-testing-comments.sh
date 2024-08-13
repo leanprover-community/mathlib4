cc02f583b3e331ab4c06c740e4fc9518c480983e
Joachim Breitner <mail@joachim-breitner.de>
2024-08-13 17:54:42 +0200

    Update scripts/lean-pr-testing-comments.sh

--- scripts/lean-pr-testing-comments.sh
+++ scripts/lean-pr-testing-comments.sh
@@ -58,7 +58,7 @@ if [[ "$branch_name" =~ ^lean-pr-testing-([0-9]+)$ ]]; then
       -H "Authorization: Bearer $TOKEN" \
       -H "X-GitHub-Api-Version: 2022-11-28" \
       https://api.github.com/repos/leanprover/lean4/issues/$pr_number/labels/builds-mathlib
-    echo "Adding labels breaks-mathlib"
+    echo "Adding label breaks-mathlib"
     curl -L -s \
       -X POST \
       -H "Accept: application/vnd.github+json" \
