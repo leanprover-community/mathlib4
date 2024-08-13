a7d454c1aa13915414ca35e3a8390ea1de2c98df
Etienne Marion <etienne.marion@ens-lyon.fr>
2024-08-13 13:45:36 +0200

    WIP

--- Mathlib/Analysis/GodefroyLipschitz/Annexe.lean
+++ Mathlib/Analysis/GodefroyLipschitz/Annexe.lean
@@ -77,6 +77,34 @@ theorem differentiableAt_norm_smul {x : E} {t : ℝ} (ht : t ≠ 0) :
     convert (hasFDerivAt_norm_smul (inv_ne_zero ht) hd.hasFDerivAt).differentiableAt
     rw [smul_smul, inv_mul_cancel ht, one_smul]
 
+theorem not_differentiableAt_norm_zero (E : Type*) [NormedAddCommGroup E] [NormedSpace ℝ E]
+    [Nontrivial E] :
+    ¬DifferentiableAt ℝ (‖·‖) (0 : E) := by
+  rcases NormedSpace.exists_lt_norm ℝ E 0 with ⟨x, hx⟩
+  intro h
+  have : DifferentiableAt ℝ (fun t : ℝ ↦ ‖t • x‖) 0 := by
+    apply DifferentiableAt.comp
+    · simpa
+    · simp
+  have : DifferentiableAt ℝ (|·|) (0 : ℝ) := by
+    simp_rw [norm_smul, norm_eq_abs] at this
+    have aux : abs = fun t ↦ (1 / ‖x‖) * (|t| * ‖x‖) := by
+      ext t
+      rw [mul_comm, mul_assoc, mul_one_div_cancel hx.ne.symm, mul_one]
+    rw [aux]
+    exact this.const_mul _
+  exact not_differentiableAt_abs_zero this
+
+theorem differentiableAt_norm_of_smul {x : E} {t : ℝ} (h : DifferentiableAt ℝ (‖·‖) (t • x)) :
+    DifferentiableAt ℝ (‖·‖) x := by
+  rcases eq_or_ne t 0 with rfl | ht
+  · by_cases hE : Nontrivial E
+    · rw [zero_smul] at h
+      exact not_differentiableAt_norm_zero E h |>.elim
+    · rw [not_nontrivial_iff_subsingleton] at hE
+      exact (hasFDerivAt_of_subsingleton _ _).differentiableAt
+  · exact differentiableAt_norm_smul ht |>.2 h
+
 theorem deriv_abs (x : ℝ) : deriv (|·|) x = SignType.sign x := by
   rcases lt_trichotomy x 0 with hx | rfl | hx
   · rw [EventuallyEq.deriv_eq (f := fun x ↦ -x)]
@@ -113,20 +141,38 @@ theorem fderiv_norm_self {x : E} (h : DifferentiableAt ℝ (‖·‖) x) :
     · exact differentiableAt_id.const_add _
   · exact (differentiableAt_abs (by norm_num)).comp _ (differentiableAt_id.const_add _)
 
-theorem not_differentiableAt_norm_zero (E : Type*) [NormedAddCommGroup E] [NormedSpace ℝ E]
-    [Nontrivial E] :
-    ¬DifferentiableAt ℝ (‖·‖) (0 : E) := by
-  rcases NormedSpace.exists_lt_norm ℝ E 0 with ⟨x, hx⟩
-  intro h
-  have : DifferentiableAt ℝ (fun t : ℝ ↦ ‖t • x‖) 0 := by
-    apply DifferentiableAt.comp
-    · simpa
-    · simp
-  have : DifferentiableAt ℝ (|·|) (0 : ℝ) := by
-    simp_rw [norm_smul, norm_eq_abs] at this
-    have aux : abs = fun t ↦ (1 / ‖x‖) * (|t| * ‖x‖) := by
-      ext t
-      rw [mul_comm, mul_assoc, mul_one_div_cancel hx.ne.symm, mul_one]
-    rw [aux]
-    exact this.const_mul _
-  exact not_differentiableAt_abs_zero this
+theorem fderiv_norm_smul (x : E) (t : ℝ) :
+    fderiv ℝ (‖·‖) (t • x) = (SignType.sign t : ℝ) • (fderiv ℝ (‖·‖) x) := by
+  by_cases hE : Nontrivial E
+  · by_cases hd : DifferentiableAt ℝ (‖·‖) x
+    · rcases eq_or_ne t 0 with rfl | ht
+      · simp only [zero_smul, _root_.sign_zero, SignType.coe_zero]
+        exact fderiv_zero_of_not_differentiableAt <| not_differentiableAt_norm_zero E
+      · rw [(hasFDerivAt_norm_smul ht hd.hasFDerivAt).fderiv]
+    · rw [fderiv_zero_of_not_differentiableAt hd, fderiv_zero_of_not_differentiableAt]
+      · simp
+      · exact mt differentiableAt_norm_of_smul hd
+  · rw [not_nontrivial_iff_subsingleton] at hE
+    simp_rw [(hasFDerivAt_of_subsingleton _ _).fderiv, smul_zero]
+
+theorem fderiv_norm_smul_pos {x : E} {t : ℝ} (ht : 0 < t) :
+    fderiv ℝ (‖·‖) (t • x) = fderiv ℝ (‖·‖) x := by
+  simp [fderiv_norm_smul, ht]
+
+theorem fderiv_norm_smul_neg {x : E} {t : ℝ} (ht : t < 0) :
+    fderiv ℝ (‖·‖) (t • x) = -fderiv ℝ (‖·‖) x := by
+  simp [fderiv_norm_smul, ht]
+
+theorem norm_fderiv_norm [Nontrivial E] {x : E} (h : DifferentiableAt ℝ (‖·‖) x) :
+    ‖fderiv ℝ (‖·‖) x‖ = 1 := by
+  have : x ≠ 0 := by
+    refine fun hx ↦ not_differentiableAt_norm_zero E ?_
+    rwa [← hx]
+  apply le_antisymm
+  · rw [show (1 : ℝ) = ↑(1 : ℝ≥0) by rfl]
+    exact norm_fderiv_le_of_lipschitz ℝ lipschitzWith_one_norm
+  · apply le_of_mul_le_mul_right _ (norm_pos_iff.2 this)
+    calc
+      1 * ‖x‖ = fderiv ℝ (‖·‖) x x := by rw [one_mul, fderiv_norm_self h]
+      _ ≤ ‖fderiv ℝ (‖·‖) x x‖ := le_norm_self _
+      _ ≤ ‖fderiv ℝ (‖·‖) x‖ * ‖x‖ := ContinuousLinearMap.le_opNorm _ _
