5edd01178d3b72767f45ea469232cf3690d25fad
Joël Riou <joel.riou@universite-paris-saclay.fr>
2024-08-13 19:44:30 +0530

    fixing the build

--- Mathlib/CategoryTheory/Sites/Subsheaf.lean
+++ Mathlib/CategoryTheory/Sites/Subsheaf.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Andrew Yang
 -/
 import Mathlib.CategoryTheory.Elementwise
-import Mathlib.CategoryTheory.Adjunction.Evaluation
 import Mathlib.CategoryTheory.Limits.FunctorCategoryEpiMono
 import Mathlib.Tactic.CategoryTheory.Elementwise
 import Mathlib.CategoryTheory.Adhesive
