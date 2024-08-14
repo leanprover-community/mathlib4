b9c24280b3be6019e11f2584bdeee1eebed68b9c
adomani <adomani@gmail.com>
2024-08-14 17:41:01 +0200

    all dirs

--- scripts/autolabel_PRs.sh
+++ scripts/autolabel_PRs.sh
@@ -1,19 +1,24 @@
 git diff --name-only master |
   awk -F/ '((3 <= NF) && (($1 == "Mathlib") || ($1 == ".github"))) {
     rootDir=tolower($2)
-    if(rootDie == "workflows") { rootDir="CI" } else { rootDir="t-"rootDir }
+    if(rootDir == "workflows") { rootDir="CI" } else { rootDir="t-"rootDir }
     roots[rootDir]++
   } END { for(r in roots) {
     printf("%s files modified in `%s`\n", roots[r], r) }
   }'
 
 
-git diff --name-only master |
-  awk -F/ '((3 <= NF) && (($1 == "Mathlib") || ($1 == ".github"))) {
-    rootDir=tolower($2)
-    if(rootDie == "workflows") { rootDir="CI" } else { rootDir="t-"rootDir }
+#git diff --name-only master
+git ls-files |
+  awk -F/ '(2 <= NF) {
+    if ($1 == "Mathlib") {
+      rootDir="f-"$2
+      #rootDir=tolower($2)
+    } else if ($1 == ".github") { rootDir="CI"
+    } else { rootDir="f-"$1 }
     roots[rootDir]++
   } END { for(r in roots) {
-    printf("%s,", r) }
-    print ""
-  }'
+    printf("`%s`\n", r) }
+    #printf("%s,", r) }
+    #print ""
+  }' | sort
