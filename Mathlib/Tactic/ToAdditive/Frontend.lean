af1456c00dc41643657a41dc3016facc30a10d92
Joachim Breitner <mail@joachim-breitner.de>
2024-08-15 15:49:23 +0200

    Fix call to getEqnsFor?

--- Mathlib/Tactic/ToAdditive/Frontend.lean
+++ Mathlib/Tactic/ToAdditive/Frontend.lean
@@ -1213,7 +1213,7 @@ partial def copyMetaData (cfg : Config) (src tgt : Name) : CoreM (Array Name) :=
     definitions. If we don't do that, the equation lemma for `src` might be generated later
     when doing a `rw`, but it won't be generated for `tgt`. -/
     additivizeLemmas #[src, tgt] "equation lemmas" fun nm ↦
-      (·.getD #[]) <$> MetaM.run' (getEqnsFor? nm true)
+      (·.getD #[]) <$> MetaM.run' (getEqnsFor? nm)
   MetaM.run' <| Elab.Term.TermElabM.run' <|
     applyAttributes cfg.ref cfg.attrs `to_additive src tgt
 
