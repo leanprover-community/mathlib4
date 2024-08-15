499e87b1e659059dd1ba4ad7b40b6bbd73fe7416
Christopher Hoskin <mans0954@users.noreply.github.com>
2024-08-15 21:04:18 +0100

    Update Mathlib/Topology/Order/LowerUpperTopology.lean
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- Mathlib/Topology/Order/LowerUpperTopology.lean
+++ Mathlib/Topology/Order/LowerUpperTopology.lean
@@ -288,9 +288,9 @@ variable [LinearOrder α] [TopologicalSpace α] [IsLower α]
 lemma isTopologicalBasis_insert_univ_subbasis :
     IsTopologicalBasis (insert univ {s : Set α | ∃ a, (Ici a)ᶜ = s}) :=
   isTopologicalBasis_of_subbasis_of_inter (by rw [topology_eq α]; rfl) (by
-    rintro _ ⟨b, hb⟩ _ ⟨c, hc⟩
+    rintro _ ⟨b, rfl⟩ _ ⟨c, rfl⟩
     use b ⊓ c
-    rw [← hc, ← hb, compl_Ici, compl_Ici, compl_Ici, Iio_inter_Iio])
+    rw [compl_Ici, compl_Ici, compl_Ici, Iio_inter_Iio])
 
 end LinearOrder
 
