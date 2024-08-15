16db297d242fbc96729bdfeb1727b4fa67d9c907
Artie Khovanov <artem.khovanov@gmail.com>
2024-08-15 17:20:21 +0100

    Update Defs.lean
    fix imports

--- Mathlib/Algebra/Ring/Semireal/Defs.lean
+++ Mathlib/Algebra/Ring/Semireal/Defs.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Florent Schaffhauser
 -/
 import Mathlib.Algebra.Ring.SumsOfSquares
-import Mathlib.Algebra.Order.Field.Defs
 
 /-!
 # Semireal rings
