556400714a7762d8ab7221754295449ae4fd3611
adomani <adomani@gmail.com>
2024-08-15 15:00:20 +0200

    fix: remove check for `olean`s in `assert_not_imported`

--- Mathlib/Util/AssertExists.lean
+++ Mathlib/Util/AssertExists.lean
@@ -79,5 +79,3 @@ elab "assert_not_imported " ids:ident+ : command => do
   let mods := (← getEnv).allImportedModuleNames
   for id in ids do
     if mods.contains id.getId then logWarningAt id m!"the module '{id}' is (transitively) imported"
-    if let none ← (← searchPathRef.get).findModuleWithExt "olean" id.getId then
-      logErrorAt id m!"the module '{id}' does not exist"
