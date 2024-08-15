7307bae5ca20422e7d589c4bd419fb99c5fa5853
damiano <adomani@gmail.com>
2024-08-15 15:27:02 +0000

    chore: create Mathlib.Init (#15811)
    This PR creates the `Mathlib.Init` file.
    
    This file is intended to be imported by virtually all `Mathlib` files.
    This PR also imports `Mathlib.Init` in all `Mathlib` files that import no `Mathlib` file.
    
    In the long run, some very basic files, some tactics and some linters will be imported in `Mathlib.Init`, so that the commands defined there would be available as widely as possible in `Mathlib`.
    
    There is also the file `scripts/init_creation.sh` that contains the instructions to regenerate the `Mathlib/Init.lean` file.

--- Mathlib/Lean/Exception.lean
+++ Mathlib/Lean/Exception.lean
@@ -3,6 +3,7 @@ Copyright (c) 2022 Edward Ayers. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Edward Ayers
 -/
+import Mathlib.Init
 import Lean.Exception
 
 /-!
