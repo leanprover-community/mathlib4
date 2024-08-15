297fd79c4394c087d426b8499bbdeaecda228ef9
Yaël Dillies <yael.dillies@gmail.com>
2024-08-15 20:55:28 +0000

    feat(Analysis/SpecialFunctions/Pow/Real): Specialise some lemmas to natural/integers powers From LeanAPAP

--- Mathlib/Analysis/SpecialFunctions/Pow/Real.lean
+++ Mathlib/Analysis/SpecialFunctions/Pow/Real.lean
@@ -712,6 +712,36 @@ theorem lt_rpow_of_log_lt (hx : 0 ≤ x) (hy : 0 < y) (h : Real.log x < z * Real
   · exact (lt_rpow_iff_log_lt hx hy).2 h
   exact Real.rpow_pos_of_pos hy z
 
+lemma rpow_le_iff_le_log (hx : 0 < x) (hy : 0 < y) : x ^ z ≤ y ↔ z * log x ≤ log y := by
+  rw [← log_le_log_iff (rpow_pos_of_pos hx _) hy, log_rpow hx]
+
+lemma rpow_lt_iff_lt_log (hx : 0 < x) (hy : 0 < y) : x ^ z < y ↔ z * log x < log y := by
+  rw [← log_lt_log_iff (rpow_pos_of_pos hx _) hy, log_rpow hx]
+
+lemma pow_le_iff_le_log {n : ℕ} (hx : 0 < x) (hy : 0 < y) :
+    x ^ n ≤ y ↔ n * log x ≤ log y := by rw [← rpow_le_iff_le_log hx hy, rpow_natCast]
+
+lemma zpow_le_iff_le_log {n : ℤ} (hx : 0 < x) (hy : 0 < y) :
+    x ^ n ≤ y ↔ n * log x ≤ log y := by rw [← rpow_le_iff_le_log hx hy, rpow_intCast]
+
+lemma pow_lt_iff_lt_log {n : ℕ} (hx : 0 < x) (hy : 0 < y) :
+    x ^ n < y ↔ n * log x < log y := by rw [← rpow_lt_iff_lt_log hx hy, rpow_natCast]
+
+lemma zpow_lt_iff_lt_log {n : ℤ} (hx : 0 < x) (hy : 0 < y) :
+    x ^ n < y ↔ n * log x < log y := by rw [← rpow_lt_iff_lt_log hx hy, rpow_intCast]
+
+lemma le_pow_iff_log_le {n : ℕ} (hx : 0 < x) (hy : 0 < y) : x ≤ y ^ n ↔ log x ≤ n * log y := by
+  rw [← le_rpow_iff_log_le hx hy, rpow_natCast]
+
+lemma le_zpow_iff_log_le {n : ℤ} (hx : 0 < x) (hy : 0 < y) : x ≤ y ^ n ↔ log x ≤ n * log y := by
+  rw [← le_rpow_iff_log_le hx hy, rpow_intCast]
+
+lemma lt_pow_iff_log_lt {n : ℕ} (hx : 0 < x) (hy : 0 < y) : x < y ^ n ↔ log x < n * log y := by
+  rw [← lt_rpow_iff_log_lt hx hy, rpow_natCast]
+
+lemma lt_zpow_iff_log_lt {n : ℤ} (hx : 0 < x) (hy : 0 < y) : x < y ^ n ↔ log x < n * log y := by
+  rw [← lt_rpow_iff_log_lt hx hy, rpow_intCast]
+
 theorem rpow_le_one_iff_of_pos (hx : 0 < x) : x ^ y ≤ 1 ↔ 1 ≤ x ∧ y ≤ 0 ∨ x ≤ 1 ∧ 0 ≤ y := by
   rw [rpow_def_of_pos hx, exp_le_one_iff, mul_nonpos_iff, log_nonneg_iff hx, log_nonpos_iff hx]
 
