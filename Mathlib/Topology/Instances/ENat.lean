fa44811943e531d07365cdb0444e0ffe7f0762a9
Yury G. Kudryashov <urkud@urkud.name>
2024-08-15 21:59:50 -0400

    Continuity of subtraction

--- Mathlib/Topology/Instances/ENat.lean
+++ Mathlib/Topology/Instances/ENat.lean
@@ -46,6 +46,11 @@ theorem mem_nhds_iff {x : ℕ∞} {s : Set ℕ∞} (hx : x ≠ ⊤) : s ∈ 𝓝
 theorem mem_nhds_natCast_iff (n : ℕ) {s : Set ℕ∞} : s ∈ 𝓝 (n : ℕ∞) ↔ (n : ℕ∞) ∈ s :=
   mem_nhds_iff (coe_ne_top _)
 
+theorem tendsto_nhds_top_iff_natCast_lt {α : Type*} {l : Filter α} {f : α → ℕ∞} :
+    Tendsto f l (𝓝 ⊤) ↔ ∀ n : ℕ, ∀ᶠ a in l, n < f a := by
+  simp_rw [nhds_top_order, lt_top_iff_ne_top, tendsto_iInf, tendsto_principal]
+  exact Option.ball_ne_none
+
 instance : ContinuousAdd ℕ∞ := by
   refine ⟨continuous_iff_continuousAt.2 fun (a, b) ↦ ?_⟩
   match a, b with
@@ -70,4 +75,45 @@ instance : ContinuousMul ℕ∞ where
       | (a : ℕ), (b : ℕ) => by
         simp [ContinuousAt, nhds_prod_eq, tendsto_pure_nhds]
 
+protected theorem continuousAt_sub {a b : ℕ∞} (h : a ≠ ⊤ ∨ b ≠ ⊤) :
+    ContinuousAt (· - ·).uncurry (a, b) := by
+  match a, b, h with
+  | (a : ℕ), (b : ℕ), _ =>
+    simpa [ContinuousAt, nhds_prod_eq] using tendsto_pure_nhds _ _
+  | (a : ℕ), ⊤, _ =>
+    suffices ∀ᶠ b in 𝓝 ⊤, (a - b : ℕ∞) = 0 by
+      simpa [ContinuousAt, nhds_prod_eq]
+    filter_upwards [le_mem_nhds (WithTop.coe_lt_top a)] with b using tsub_eq_zero_of_le
+  | ⊤, (b : ℕ), _ =>
+    suffices ∀ n : ℕ, ∀ᶠ a : ℕ∞ in 𝓝 ⊤, b + n < a by
+      simpa [ContinuousAt, nhds_prod_eq, (· ∘ ·), lt_tsub_iff_left, tendsto_nhds_top_iff_natCast_lt]
+    exact fun n ↦ lt_mem_nhds <| WithTop.coe_lt_top (b + n)
+
 end ENat
+
+theorem Filter.Tendsto.enatSub {α : Type*} {l : Filter α} {f g : α → ℕ∞} {a b : ℕ∞}
+    (hf : Tendsto f l (𝓝 a)) (hg : Tendsto g l (𝓝 b)) (h : a ≠ ⊤ ∨ b ≠ ⊤) :
+    Tendsto (fun x ↦ f x - g x) l (𝓝 (a - b)) :=
+  (ENat.continuousAt_sub h).tendsto.comp (hf.prod_mk_nhds hg)
+
+variable {X : Type*} [TopologicalSpace X] {f g : X → ℕ∞} {s : Set X} {x : X}
+
+nonrec theorem ContinuousWithinAt.enatSub
+    (hf : ContinuousWithinAt f s x) (hg : ContinuousWithinAt g s x) (h : f x ≠ ⊤ ∨ g x ≠ ⊤) :
+    ContinuousWithinAt (fun x ↦ f x - g x) s x :=
+  hf.enatSub hg h
+
+nonrec theorem ContinuousAt.enatSub
+    (hf : ContinuousAt f x) (hg : ContinuousAt g x) (h : f x ≠ ⊤ ∨ g x ≠ ⊤) :
+    ContinuousAt (fun x ↦ f x - g x) x :=
+  hf.enatSub hg h
+
+nonrec theorem ContinuousOn.enatSub
+    (hf : ContinuousOn f s) (hg : ContinuousOn g s) (h : ∀ x ∈ s, f x ≠ ⊤ ∨ g x ≠ ⊤) :
+    ContinuousOn (fun x ↦ f x - g x) s := fun x hx ↦
+  (hf x hx).enatSub (hg x hx) (h x hx)
+
+nonrec theorem Continuous.enatSub
+    (hf : Continuous f) (hg : Continuous g) (h : ∀ x, f x ≠ ⊤ ∨ g x ≠ ⊤) :
+    Continuous (fun x ↦ f x - g x) :=
+  continuous_iff_continuousAt.2 fun x ↦ hf.continuousAt.enatSub hg.continuousAt (h x)
