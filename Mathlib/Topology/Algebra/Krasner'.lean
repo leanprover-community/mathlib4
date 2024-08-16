06280e237f7b2f5c6f0702aebb9dc20d3f82b6ff
jjdishere <emailboxofjjd@163.com>
2024-08-16 10:28:32 +0200

    Update Krasner'.lean

--- Mathlib/Topology/Algebra/Krasner'.lean
+++ Mathlib/Topology/Algebra/Krasner'.lean
@@ -131,19 +131,54 @@ end Separable
 
 section NormValued
 
+@[simp]
+theorem Valued.toNormedField.norm_le_iff [vL.v.RankOne] {x x' : L} :
+    letI := vL.toNormedField
+    ‖x‖ ≤ ‖x'‖ ↔ vL.v x ≤ vL.v x' := (Valuation.RankOne.strictMono vL.v).le_iff_le
+
+@[simp]
+theorem Valued.toNormedField.norm_lt_iff [vL.v.RankOne] {x x' : L} :
+    letI := vL.toNormedField
+    ‖x‖ < ‖x'‖ ↔ vL.v x < vL.v x' := (Valuation.RankOne.strictMono vL.v).lt_iff_lt
+
+@[simp]
+theorem Valued.toNormedField.norm_le_one_iff [vL.v.RankOne] {x : L} :
+    letI := vL.toNormedField
+    ‖x‖ ≤ 1 ↔ vL.v x ≤ 1 := by
+  simpa only [map_one] using (Valuation.RankOne.strictMono vL.v).le_iff_le (b := 1)
+
+@[simp]
+theorem Valued.toNormedField.norm_lt_one_iff [vL.v.RankOne] {x : L} :
+    letI := vL.toNormedField
+    ‖x‖ < 1 ↔ vL.v x < 1 := by
+  simpa only [map_one] using (Valuation.RankOne.strictMono vL.v).lt_iff_lt (b := 1)
+
+@[simp]
+theorem Valued.toNormedField.one_le_norm_iff [vL.v.RankOne] {x : L} :
+    letI := vL.toNormedField
+    1 ≤ ‖x‖ ↔ 1 ≤ vL.v x := by
+  simpa only [map_one] using (Valuation.RankOne.strictMono vL.v).le_iff_le (a := 1)
+
+@[simp]
+theorem Valued.toNormedField.one_lt_norm_iff [vL.v.RankOne] {x : L} :
+    letI := vL.toNormedField
+    1 < ‖x‖ ↔ 1 < vL.v x := by
+  simpa only [map_one] using (Valuation.RankOne.strictMono vL.v).lt_iff_lt (a := 1)
+
 def Valued.toNontriviallyNormedField [vL.v.RankOne] : NontriviallyNormedField L := {
   vL.toNormedField with
-  non_trivial := by sorry
+  non_trivial := by
+    obtain ⟨x, hx⟩ := Valuation.RankOne.nontrivial vL.v
+    have : x ≠ 0 := vL.v.ne_zero_iff.mp hx.1
+    rcases Valuation.val_le_one_or_val_inv_le_one vL.v x with h | h
+    · use x⁻¹
+      simp only [toNormedField.one_lt_norm_iff, map_inv₀, one_lt_inv₀ hx.1, lt_of_le_of_ne h hx.2]
+    · use x
+      simp only [map_inv₀, inv_le_one₀ hx.1] at h
+      simp only [toNormedField.one_lt_norm_iff, lt_of_le_of_ne h hx.2.symm]
 }
 
 end NormValued
-
-
-section T2
-
--- instance : TotallySeparatedSpace L := sorry
-
-end T2
 
 section ContinuousSMul
 
@@ -171,12 +206,26 @@ end NormedField
 
 section FunctionExtends
 
-theorem functionExtends_of_functionExtends_of_functionExtends [CommRing A] [Algebra R A]
+theorem functionExtends_of_functionExtends_of_functionExtends {A} [CommRing A] [Algebra R A]
     {B : Type*} [Ring B] [Algebra R B] [Algebra A B] [IsScalarTower R A B] {fR : R → ℝ}
     {fA : A → ℝ} {fB : B → ℝ} (h1 : FunctionExtends fR fA) (h2 : FunctionExtends fA fB) :
-    FunctionExtends fR fB := sorry
+    FunctionExtends fR fB := by
+  intro x
+  calc
+    fB ((algebraMap R B) x) = fA (algebraMap R A x) :=
+      h2 _ ▸ IsScalarTower.algebraMap_apply R A B x ▸ rfl
+    _ = fR x := h1 x
 
 end FunctionExtends
+
+-- section AlgEquiv
+-- -- a missing lemma
+-- variable (R) in
+-- @[simp]
+-- theorem AlgEquiv.refl_apply (x : A) : (AlgEquiv.refl : A ≃ₐ[R] A) x = x := rfl
+
+-- end AlgEquiv
+
 
 open Algebra
 open IntermediateField
@@ -209,12 +258,17 @@ namespace IsKrasnerNorm
 variable {K L : Type*} [NK : NontriviallyNormedField K] (is_na : IsNonarchimedean (norm : K → ℝ))
     [NL : NormedField L] [Algebra K L] (extd : FunctionExtends (norm : K → ℝ) (norm : L → ℝ))
 
+
 variable (M : IntermediateField K L)
 
+#synth Algebra K M
 #synth NormedField M
 #synth Algebra M L
+#synth Algebra (⊥ : IntermediateField K L) M
+-- #synth IsScalarTower K (⊥ : IntermediateField K L) M
 
-
+#check spectralNorm_aut_isom
+#check spectralNorm_unique_field_norm_ext
 theorem of_completeSpace [CompleteSpace K] : IsKrasnerNorm K L := by
   constructor
   intro x y xsep hyK hxy
@@ -230,12 +284,15 @@ theorem of_completeSpace [CompleteSpace K] : IsKrasnerNorm K L := by
         erw [extd x]
         simp only [one_mul, le_refl]
     }
-  have : ContinuousSMul K M := by -- decompose as `ContinuousSMul K L implies ContinuousSMul K M`
-    apply Inducing.continuousSMul (N := K) (M := (⊥ : IntermediateField K L)) (X := M) (Y := M) (f := (IntermediateField.botEquiv K L).symm) inducing_id
+  have : ContinuousSMul K M := by
+    -- decompose as `ContinuousSMul K L`
+    -- and `ContinuousSMul K L implies ContinuousSMul K M`
+    apply Inducing.continuousSMul (N := K) (M := (⊥ : IntermediateField K L)) (X := M) (Y := M)
+      (f := (IntermediateField.botEquiv K L).symm) inducing_id
     · exact BotEquiv.continuous
     · intros c x
-      ext
-      sorry
+      rw [Algebra.smul_def, @Algebra.smul_def (⊥ : IntermediateField K L) M _ _ _]
+      rfl
   letI : CompleteSpace M := FiniteDimensional.complete K M
   have hy : y ∈ K⟮y⟯ := IntermediateField.subset_adjoin K {y} rfl
   have zsep : (minpoly M z).Separable := by
@@ -274,13 +331,12 @@ theorem of_completeSpace [CompleteSpace K] : IsKrasnerNorm K L := by
     · intro m
       exact extd m
     · exact spectralNorm_extends M L -- a lemma for extends extends
-  have norm_eq: ‖z‖ = ‖z'‖ := by
-    rw [eq_spnML]
-    obtain ⟨σ , h⟩ := IsConjRoot.exist_algEquiv h1
-    -- `sigma should be L to L map? or M should be Normal closure of K(y)?`
-    exact h.symm ▸ @spectralNorm_aut_isom M _ L _ M.toAlgebra σ z
+  have norm_eq: ‖z‖ = ‖z'‖ := by -- a lemma
+    simp only [eq_spnML, spectralNorm]
+    congr 1
     -- spectralNorm K L = spectralnorm M L
-  -- IsConjRoot.val_eq M hM (Polynomial.Separable.isIntegral zsep) h1 -- need rank one -- exist_algEquiv
+  -- IsConjRoot.val_eq M hM (Polynomial.Separable.isIntegral zsep) h1
+  -- need rank one -- exist_algEquiv
   have : ‖z - z'‖ < ‖z - z'‖ := by
     calc
       _ ≤ max ‖z‖ ‖z'‖ := by
@@ -296,30 +352,24 @@ theorem of_completeSpace [CompleteSpace K] : IsKrasnerNorm K L := by
       _ = ‖z - z'‖ := by congr 1; ring
   simp only [lt_self_iff_false] at this
 
-
 end IsKrasnerNorm
 
+section Valued
 
+variable {Γ Γ' : Type*} [LinearOrderedCommGroupWithZero Γ] [LinearOrderedCommGroupWithZero Γ']
+    {vK : Valued K Γ} {vK' : Valued K Γ'}
+/-- Equivalent valuations induces the same topology -/
+theorem Valued.toUniformSpace_eq_of_isEquiv (h : vK.v.IsEquiv vK'.v) :
+  vK.toUniformSpace = vK'.toUniformSpace := by
+  sorry -- must use new definition of is_topological_valuation
 
-
-section NormedValued
-
-theorem Valued.toNormedField.norm_le_iff_val_le [vL.v.RankOne] {x x' : L} :
-    letI := vL.toNormedField
-    ‖x‖ ≤ ‖x'‖ ↔ vL.v x ≤ vL.v x' := sorry
-
-theorem Valued.toNormedField.norm_lt_iff_val_lt [vL.v.RankOne] {x x' : L} :
-    letI := vL.toNormedField
-    ‖x‖ < ‖x'‖ ↔ vL.v x < vL.v x' := sorry
-
-/- valuation to norm, then valued to normed-/
---
-
-end NormedValued
+end Valued
 
 namespace IsKrasner
 
--- it is possible to remove the condition `vL.v.RankOne`
+-- TODO: it is possible to remove the condition `vL.v.RankOne`, since
+-- any algebraic extension of rank one valued field K is rank one valued.
+-- Just extract the maximal algebraic extension of K in L
 -- it should be an instance after h is a type class
 theorem of_completeSpace [rk1L : vL.v.RankOne] {ΓK : outParam Type*}
     [LinearOrderedCommGroupWithZero ΓK] [vK : Valued K ΓK] [rk1K : vK.v.RankOne] [CompleteSpace K]
@@ -341,8 +391,11 @@ theorem of_completeSpace [rk1L : vL.v.RankOne] {ΓK : outParam Type*}
       use x
       exact ⟨h.ne_zero.mp hx0 , ((Valuation.isEquiv_iff_val_eq_one _ _).mp h).not.mp hx1⟩
   }
-  letI := vK'.toNormedField
-  sorry
+  letI := vK'.toNontriviallyNormedField
+  refine (IsKrasnerNorm.of_completeSpace (K := K) (L := L) ?_ ?_).krasner_norm xsep hyK ?_
+  · sorry -- isNonArch
+  · sorry -- FunctionExtends
+  · sorry -- translation
   -- let z := x - y
   -- let M := K⟮y⟯
   -- let FDM := IntermediateField.adjoin.finiteDimensional hyK
