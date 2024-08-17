67d7502bb773ddf0384a3ac2309b8acd3530bede
Matthew Robert Ballard <matt@mrb.email>
2024-08-17 13:18:00 +0000

    chore(Algebra.Order.Ring.Rat): split into unbundled and bundled ordered algebra results (#15071)
    We split off all but the `LinearOrderedCommRing` and the inferred `OrderedAddCommMonoid` instances on `Rat` into `Algebra.Order.Ring.Unbundled.Rat` as these results do not require bundled ordered algebra classes.

--- Mathlib.lean
+++ Mathlib.lean
@@ -643,6 +643,7 @@ import Mathlib.Algebra.Order.Ring.Star
 import Mathlib.Algebra.Order.Ring.Synonym
 import Mathlib.Algebra.Order.Ring.Unbundled.Basic
 import Mathlib.Algebra.Order.Ring.Unbundled.Nonneg
+import Mathlib.Algebra.Order.Ring.Unbundled.Rat
 import Mathlib.Algebra.Order.Ring.WithTop
 import Mathlib.Algebra.Order.Sub.Basic
 import Mathlib.Algebra.Order.Sub.Defs
