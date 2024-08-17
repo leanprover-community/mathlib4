240bc04d257dc9ea1384d52f173de3ed7f3bce1d
adomani <adomani@gmail.com>
2024-08-17 07:53:10 +0200

    reduce imports, only get one cache

--- Mathlib/autolabel.lean
+++ Mathlib/autolabel.lean
@@ -3,7 +3,7 @@ Copyright (c) 2024 Damiano Testa. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Damiano Testa
 -/
-import Lean
+import Lean.Elab.Command
 
 /-!
 The file with the attribute and the basic API
