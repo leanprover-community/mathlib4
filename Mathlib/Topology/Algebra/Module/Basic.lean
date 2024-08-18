1338b64861cac4d5a3615cd3425dfb968ee1917a
grunweg <rothgami@math.hu-berlin.de>
2024-08-18 04:33:07 +0000

    chore: remove bare open Classical, part 6 (#15837)

--- Mathlib/Topology/Algebra/Module/Basic.lean
+++ Mathlib/Topology/Algebra/Module/Basic.lean
@@ -2232,8 +2232,6 @@ end ContinuousLinearEquiv
 
 namespace ContinuousLinearMap
 
-open scoped Classical
-
 variable {R : Type*} {M : Type*} {M₂ : Type*} [TopologicalSpace M] [TopologicalSpace M₂]
 
 section
@@ -2242,6 +2240,7 @@ variable [Semiring R]
 variable [AddCommMonoid M₂] [Module R M₂]
 variable [AddCommMonoid M] [Module R M]
 
+open Classical in
 /-- Introduce a function `inverse` from `M →L[R] M₂` to `M₂ →L[R] M`, which sends `f` to `f.symm` if
 `f` is a continuous linear equivalence and to `0` otherwise.  This definition is somewhat ad hoc,
 but one needs a fully (rather than partially) defined inverse function for some purposes, including
