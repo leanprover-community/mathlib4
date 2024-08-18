03242a358248f92ade39c3a7bacb247449323d5e
Michael Rothgang <rothgami@math.hu-berlin.de>
2024-08-18 16:48:42 +0200

    wip

--- Mathlib/Init.lean
+++ Mathlib/Init.lean
@@ -11,15 +11,46 @@ Here are some general guidelines:
   any exception to this rule should by accompanied by a comment explaining the transitive imports.
 -/
 
--- All these linters are meant to run on all of mathlib,
--- are syntax linters active by default (no need to be imported as early as possible),
--- and have no imports beyond `Lean` unless noted below.
-import Mathlib.Tactic.Linter.HashCommandLinter
+-- These are all syntax linters defined in Mathlib which are active in Mathlib by default.
+-- Syntax linters need to be imported to take effect, hence we would like them to be imported
+-- as early as possible.
+-- All these linters have no bulk imports; any import outside of `Lean` is explained below.
+
+-- This linter checks for surprising syntax in Lean, and is active by default.
 import Mathlib.Tactic.Linter.GlobalAttributeIn
--- This file imports Batteries.Tactic.Lint, where the `env_linter` attribute is defined.
-import Mathlib.Tactic.Linter.Lint
+-- This linter warns about unused tactics: occasionally, a new tactic needs to be excluded
+-- using #allow_unused_tactic (as in `#allow_unused_tactic Lean.Parser.Tactic.done`),
+-- but other that this, a warning usually indicates code that ought to be fixed.
+-- This linter is also active by default.
+import Mathlib.Tactic.Linter.UnusedTactic
+
+-- The hash command linter warns about #commands in the source file which emit no message
+-- (except for manual exceptions): these are usually meant for debugging.
+-- This linter is on by default in Mathlib.
+import Mathlib.Tactic.Linter.HashCommandLinter
+
+-- The following linters are "restriction lints": they warn about a particular idiom,
+-- where there are better alternatives. As this is somewhat subjective,
+-- they are only enabled in mathlib (and the Archive and Counterexamples).
 import Mathlib.Tactic.Linter.AdmitLinter
 import Mathlib.Tactic.Linter.OldObtain
 import Mathlib.Tactic.Linter.RefineLinter
-import Mathlib.Tactic.Linter.UnusedTactic
+
+
+
+-- uncat!
+
+-- uncat!
+
+
+-- All these linters are meant to run on all of mathlib,
+-- are syntax linters active by default (no need to be imported as early as possible),
+-- and have no imports beyond `Lean` unless noted below.
+
+
+
+-- This file imports Batteries.Tactic.Lint, where the `env_linter` attribute is defined.
+import Mathlib.Tactic.Linter.Lint
+
+
 import Mathlib.Tactic.Linter.Style
