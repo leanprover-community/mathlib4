5fab4776a8aaa3bb6ab9903f9d4f7020a5293026
Michael Rothgang <rothgami@math.hu-berlin.de>
2024-08-18 18:53:15 +0200

    A few more miscellaneous fixes.

--- Mathlib/Data/Nat/Bitwise.lean
+++ Mathlib/Data/Nat/Bitwise.lean
@@ -245,7 +245,7 @@ theorem bitwise_swap {f : Bool → Bool → Bool} :
   cases' m with m
   <;> cases' n with n
   <;> try rw [bitwise_zero_left, bitwise_zero_right]
-  · specialize ih ((m+1) / 2) (div_lt_self' ..)
+  · specialize ih ((m + 1) / 2) (div_lt_self' ..)
     simp [bitwise_of_ne_zero, ih]
 
 /-- If `f` is a commutative operation on bools such that `f false false = false`, then `bitwise f`
