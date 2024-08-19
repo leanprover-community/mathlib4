dc0a39d7d329896df747270083a279ee633bb68f
Christopher Hoskin <christopher.hoskin@gmail.com>
2024-08-19 08:31:03 +0100

    LC singleton lemmas

--- Mathlib/Analysis/LocallyConvex/Polar.lean
+++ Mathlib/Analysis/LocallyConvex/Polar.lean
@@ -111,6 +111,14 @@ theorem polar_weak_closed (s : Set E) : IsClosed[WeakBilin.instTopologicalSpace 
   refine isClosed_iInter fun x => isClosed_iInter fun _ => ?_
   exact isClosed_le (WeakBilin.eval_continuous B.flip x).norm continuous_const
 
+theorem polar_singleton {a : E} : B.polar {a} = { x | ‖B a x‖ ≤ 1 } := le_antisymm
+  (fun _ hx => hx _ rfl)
+  (fun x hx => (polar_mem_iff _ _ _).mp (fun _ hb => by rw [Set.mem_singleton_iff.mp hb]; exact hx))
+
+theorem mem_polar_singleton {a : E} (y : F) : y ∈ B.polar {a} ↔ ‖B a y‖ ≤ 1 := by
+  rw [polar_singleton]
+  exact Set.mem_setOf
+
 end NormedRing
 
 section NontriviallyNormedField
