d669e11b85422eccb6bdf9d307472babede3813e
adomani <adomani@gmail.com>
2024-08-19 14:24:18 +0200

    lint copyright and docmodule

--- Mathlib/AEtest.lean
+++ Mathlib/AEtest.lean
@@ -1,4 +1,13 @@
+/-
+Copyright (c) 2024 Damiano Testa. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Damiano Testa
+-/
 import Mathlib.Util.AssertExistsCopy
+
+/-!
+# A test file for `assert_not_exists1` and `assert_not_imported1`
+-/
 
 assert_not_exists1 NNReal
 assert_not_imported1 Mathlib.Tactic
