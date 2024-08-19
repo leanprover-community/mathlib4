5d092303356d03d194381eb6b3fee7235f0a45a7
Michael Rothgang <rothgami@math.hu-berlin.de>
2024-08-19 13:17:25 +0200

    Move docstring down; tweak comment.

--- Mathlib/Init.lean
+++ Mathlib/Init.lean
@@ -1,4 +1,30 @@
 /-
+See the module docstring below for guidelines on this file's imports.
+
+These are all syntax linters defined in Mathlib which are active in Mathlib by default.
+Syntax linters need to be imported to take effect, hence we would like them to be imported
+as early as possible.
+All these linters have no bulk imports; any import outside of `Lean` is explained below.
+
+**Not** imported in this file are
+- the text-based linters in `Linters/TextBased.lean`, as they can be imported later
+- the `minImports` linter, as that is still disabled by default.
+It *does* check a useful global coherence property, so perhaps changing this default at some
+point can be useful. `mathlib` might need a fair amount of fixes for this to become viable, though.
+-- XXX: the `haveLet` linter could also be imported here...
+
+-/
+
+import Mathlib.Tactic.Linter.HashCommandLinter
+import Mathlib.Tactic.Linter.GlobalAttributeIn
+-- This file imports Batteries.Tactic.Lint, where the `env_linter` attribute is defined.
+import Mathlib.Tactic.Linter.Lint
+import Mathlib.Tactic.Linter.OldObtain
+import Mathlib.Tactic.Linter.RefineLinter
+import Mathlib.Tactic.Linter.UnusedTactic
+import Mathlib.Tactic.Linter.Style
+
+/-!
 This is the root file in Mathlib: it is imported by virtually *all* Mathlib files.
 
 For this reason, the imports of this files are carefully curated.
@@ -10,15 +36,3 @@ Here are some general guidelines:
 * strong preference for avoiding files that themselves have imports beyond `Lean`, and
   any exception to this rule should by accompanied by a comment explaining the transitive imports.
 -/
-
--- All these linters are meant to run on all of mathlib,
--- are syntax linters active by default (no need to be imported as early as possible),
--- and have no imports beyond `Lean` unless noted below.
-import Mathlib.Tactic.Linter.HashCommandLinter
-import Mathlib.Tactic.Linter.GlobalAttributeIn
--- This file imports Batteries.Tactic.Lint, where the `env_linter` attribute is defined.
-import Mathlib.Tactic.Linter.Lint
-import Mathlib.Tactic.Linter.OldObtain
-import Mathlib.Tactic.Linter.RefineLinter
-import Mathlib.Tactic.Linter.UnusedTactic
-import Mathlib.Tactic.Linter.Style
