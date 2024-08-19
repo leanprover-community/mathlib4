30298f17c8c63c3a91bcf26f67180973244a9d8e
zeramorphic <zeramorphic@proton.me>
2024-08-19 10:32:46 +0100

    Add 4 suffix to images and add date tags
    Signed-off-by: zeramorphic <zeramorphic@proton.me>

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -3,11 +3,13 @@
 # check with scott.morrison@gmail.com for credentials.
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DATE=$(date +"%Y-%m-%d")
 
 cd $DIR && \
 ./docker_build.sh && \
-docker push leanprovercommunity/lean:latest && \
-docker push leanprovercommunity/lean:debian && \
-docker push leanprovercommunity/mathlib:latest && \
-docker push leanprovercommunity/mathlib:debian && \
-docker push leanprovercommunity/mathlib:gitpod
+docker push leanprovercommunity/lean4:$DATE && \
+docker push leanprovercommunity/lean4:latest && \
+docker push leanprovercommunity/gitpod4:$DATE && \
+docker push leanprovercommunity/gitpod4:latest && \
+docker push leanprovercommunity/gitpod4-blueprint:$DATE && \
+docker push leanprovercommunity/gitpod4-blueprint:latest
