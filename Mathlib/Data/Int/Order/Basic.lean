5764ad2611b4e5ab42961f9b8c0fce541c089a4f
Matthew Ballard <matt@mrb.email>
2024-08-19 07:14:18 -0400

    shake

--- Mathlib/Data/Int/Order/Basic.lean
+++ Mathlib/Data/Int/Order/Basic.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jeremy Avigad
 -/
 
-import Mathlib.Data.Int.Notation
 import Mathlib.Data.Nat.Notation
 import Mathlib.Order.Defs
 
