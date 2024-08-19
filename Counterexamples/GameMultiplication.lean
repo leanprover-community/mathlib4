211d9978577f1442aa65640595ff7f7d43784d08
Violeta Hernández <vi.hdz.p@gmail.com>
2024-08-18 23:10:40 -0600

    inline lemmas

--- Counterexamples/GameMultiplication.lean
+++ Counterexamples/GameMultiplication.lean
@@ -30,41 +30,41 @@ def star' : PGame := ofLists [0, -1] [0, 1]
 theorem neg_star' : -star' = star' := by
   simp [star']
 
-lemma star'_le_star : star' ≤ star := by
-  apply PGame.le_of_forall_lf
-  · rintro ⟨i⟩
-    fin_cases i
-    · exact zero_lf_star
-    · exact (neg_lt_zero_iff.2 PGame.zero_lt_one).trans_lf zero_lf_star
-  · exact fun _ => lf_zero_le.2 ⟨⟨0, Nat.zero_lt_two⟩, le_rfl⟩
-
+/-- `*'` is equivalent to `*`. -/
 theorem star'_equiv_star : star' ≈ star := by
+  have le : star' ≤ star := by
+    apply PGame.le_of_forall_lf
+    · rintro ⟨i⟩
+      fin_cases i
+      · exact zero_lf_star
+      · exact (neg_lt_zero_iff.2 PGame.zero_lt_one).trans_lf zero_lf_star
+    · exact fun _ => lf_zero_le.2 ⟨⟨0, Nat.zero_lt_two⟩, le_rfl⟩
   constructor
   case' right => rw [← neg_le_neg_iff, neg_star, neg_star']
-  all_goals exact star'_le_star
-
-lemma star_sq_le : star * star ≤ star := by
-  rw [le_iff_forall_lf]
-  constructor <;>
-  intro i
-  · apply leftMoves_mul_cases i <;>
-    intro _ _
-    case' hl => rw [mul_moveLeft_inl]
-    case' hr => rw [mul_moveLeft_inr];
-    all_goals rw [lf_iff_game_lf]; simpa using zero_lf_star
-  · refine lf_zero.2 ⟨default, ?_⟩
-    rintro (j | j) <;> -- Instance can't be inferred otherwise.
-    exact isEmptyElim j
+  all_goals exact le
 
 /-- The equation `** = *` is an identity, but not a relabelling. -/
 theorem star_sq : star * star ≈ star := by
+  have le : star * star ≤ star := by
+    rw [le_iff_forall_lf]
+    constructor <;>
+    intro i
+    · apply leftMoves_mul_cases i <;>
+      intro _ _
+      case' hl => rw [mul_moveLeft_inl]
+      case' hr => rw [mul_moveLeft_inr];
+      all_goals rw [lf_iff_game_lf]; simpa using zero_lf_star
+    · refine lf_zero.2 ⟨default, ?_⟩
+      rintro (j | j) <;> -- Instance can't be inferred otherwise.
+      exact isEmptyElim j
   constructor
   case' right =>
     rw [← neg_le_neg_iff];
     apply (negMulRelabelling _ _).symm.equiv.1.trans;
     rw [neg_star]
-  all_goals exact star_sq_le
+  all_goals exact le
 
+/-- `*'* ⧏ *` implies `*'* ≉ *`.-/
 theorem star'_mul_star_lf : star' * star ⧏ star := by
   rw [lf_iff_exists_le]
   refine Or.inr ⟨toRightMovesMul (Sum.inr ⟨⟨1, Nat.one_lt_two⟩, default⟩), ?_⟩
