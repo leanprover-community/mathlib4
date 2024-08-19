b9f1fe91345e3619d767ce7b616dea41566f755c
Jack McKoen <mckoen@ualberta.ca>
2024-08-19 16:31:06 -0600

    shorten proof and add docstrings

--- Mathlib/CategoryTheory/Closed/FunctorToTypes.lean
+++ Mathlib/CategoryTheory/Closed/FunctorToTypes.lean
@@ -7,72 +7,161 @@ import Mathlib.CategoryTheory.ChosenFiniteProducts.FunctorCategory
 import Mathlib.CategoryTheory.Closed.Cartesian
 import Mathlib.CategoryTheory.Monoidal.FunctorCategory
 import Mathlib.CategoryTheory.Monoidal.Types.Basic
-import Mathlib.Tactic.ApplyFun
 
 /-!
 # Functors to Type are closed.
 
 Show that `C ⥤ Type max w v u` is monoidal closed for `C` a category in `Type u` with morphisms in
-`Type v`, where `u`, `v`, and `w` are arbitrary universes.
+`Type v`, and `w` an arbitrary universe.
 
 ## TODO
-It should be shown that `C ⥤ Type max w v u` is also cartesian closed.
+It should be shown that `C ⥤ Type max w v u` is cartesian closed.
 -/
 
 
-namespace CategoryTheory
-
 universe w v' v u u'
 
-open MonoidalCategory
+open CategoryTheory MonoidalCategory
 
-variable {C : Type u} [Category.{v} C]
+variable {C : Type u} [Category.{v} C] {D : Type u'} [Category.{v'} D]
 
-namespace Functor
+namespace CategoryTheory.Functor
 
-variable {D : Type u'} [Category.{v'} D]
+variable (F G : C ⥤ D)
 
+/-- Given functors `F G : C ⥤ D`, `HomObj F G A` is a proxy for the type
+  of "morphisms" `F ⊗ A ⟶ G`, where `A : C ⥤ Type w` (`w` an arbitrary universe). -/
 @[ext]
-structure Hom₂ (F G : C ⥤ D) (A : C ⥤ Type w) where
+structure HomObj (A : C ⥤ Type w) where
   app (c : C) (a : A.obj c) : F.obj c ⟶ G.obj c
   naturality {c d : C} (f : c ⟶ d) (a : A.obj c) :
     F.map f ≫ app d (A.map f a) = app c a ≫ G.map f := by aesop_cat
 
-def hom₂Functor (F G : C ⥤ D) : (C ⥤ Type w)ᵒᵖ ⥤ Type max w v' u where
-  obj A := Hom₂ F G A.unop
+/-- When `F`, `G`, and `A` are all functors `C ⥤ Type w`, then `HomObj F G A` is in
+  bijection with `F ⊗ A ⟶ G`. -/
+def HomObjEquiv (F G A : C ⥤ Type w) : (F ⊗ A ⟶ G) ≃ (HomObj F G A) where
+  toFun a := ⟨fun X y x ↦ a.app X (x, y), fun φ y ↦ by
+    ext x
+    erw [congr_fun (a.naturality φ) (x, y)]
+    rfl ⟩
+  invFun a := ⟨fun X ⟨x, y⟩ ↦ a.app X y x, fun X Y f ↦ by
+    ext ⟨x, y⟩
+    erw [congr_fun (a.naturality f y) x]
+    rfl ⟩
+  left_inv _ := by aesop
+  right_inv _ := by aesop
+
+namespace HomObj
+
+attribute [reassoc (attr := simp)] naturality
+
+variable {F G} {A : C ⥤ Type w}
+
+lemma congr_app {f g : HomObj F G A} (h : f = g) (X : C)
+    (a : A.obj X) : f.app X a = g.app X a := by subst h; rfl
+
+/-- Given a natural transformation `F ⟶ G`, get a term of `HomObj F G A` by "ignoring" `A`. -/
+@[simps]
+def ofNatTrans {A : C ⥤ Type w} (f : F ⟶ G) : HomObj F G A where
+  app X _ := f.app X
+
+/-- The identity `HomObj F F A`. -/
+abbrev id (A : C ⥤ Type w) : HomObj F F A := ofNatTrans (𝟙 F)
+
+/- Composition of `f : HomObj F G A` with `g : HomObj G M A`. -/
+@[simps]
+def comp {M : C ⥤ D} (f : HomObj F G A) (g : HomObj G M A) : HomObj F M A where
+  app X a := f.app X a ≫ g.app X a
+
+/-- Given a morphism `A' ⟶ A`, send a term of `HomObj F G A` to a term of `HomObj F G A'`. -/
+@[simps]
+def map (x : HomObj F G A) {A' : C ⥤ Type w} (f : A' ⟶ A) : HomObj F G A' where
+  app Δ a := x.app Δ (f.app Δ a)
+  naturality {Δ Δ'} φ a := by
+    dsimp
+    rw [← x.naturality φ (f.app Δ a), FunctorToTypes.naturality _ _ f φ a]
+
+end HomObj
+
+/-- The contravariant functor taking `A : C ⥤ Type w` to `HomObj F G A`. -/
+def HomObjFunctor : (C ⥤ Type w)ᵒᵖ ⥤ Type max w v' u where
+  obj A := HomObj F G A.unop
   map {A A'} f x :=
     { app := fun X a ↦ x.app X (f.unop.app _ a)
       naturality := fun {X Y} φ a ↦ by
         dsimp
-        rw [← Hom₂.naturality]
+        rw [← HomObj.naturality]
         congr 2
         exact congr_fun (f.unop.naturality φ) a }
 
-def ihom (F G : C ⥤ D) : C ⥤ Type max u v v' := coyoneda.rightOp ⋙ hom₂Functor.{v} F G
+/-- Composition of `HomObjFunctor` with the co-Yoneda embedding.
+  When `F G : C ⥤ Type max v' v u`, this is the internal hom of `F` and `G`. -/
+def functorHom (F G : C ⥤ D) : C ⥤ Type max v' v u := coyoneda.rightOp ⋙ HomObjFunctor.{v} F G
+
+variable {F G} in
+@[ext]
+lemma functorHom_ext {X : C} {x y : (F.functorHom G).obj X}
+    (h : ∀ (Y : C) (f : X ⟶ Y), x.app Y f = y.app Y f) : x = y :=
+  HomObj.ext (by ext; apply h)
+
+/-- The bijection `HomObj F G A ≃ (A ⟶ F.functorHom G)` which
+  prefaces `FunctorToTypes.functorHomEquiv` -/
+def functorHomEquiv (A : C ⥤ Type max u v v') : HomObj F G A ≃ (A ⟶ F.functorHom G) where
+  toFun x :=
+    { app := fun X a ↦ { app := fun Y f => x.app Y (A.map f a) }
+      naturality := fun X Y f => by
+        ext a Z φ
+        dsimp only [types_comp_apply]
+        rw [← FunctorToTypes.map_comp_apply]
+        rfl }
+  invFun φ :=
+    { app := fun X a ↦ (φ.app X a).app X (𝟙 _)
+      naturality := fun {X Y} f a => by
+        rw [← (φ.app X a).naturality f (𝟙 _)]
+        have := HomObj.congr_app (congr_fun (φ.naturality f) a) Y (𝟙 _)
+        dsimp [functorHom, HomObjFunctor] at this
+        aesop }
+  left_inv x := by aesop
+  right_inv φ := by
+    ext X a Y f
+    exact (HomObj.congr_app (congr_fun (φ.naturality f) a) Y (𝟙 _)).trans
+      (congr_arg ((φ.app X a).app Y) (by simp))
+
+variable {F G} in
+/-- Morphisms `F ⟶ G` are in bijection with
+  morphisms `(𝟙_ (C ⥤ Type max v' v u) ⟶ F.functorHom G)`-/
+@[simps]
+def natTransEquiv : (F ⟶ G) ≃ (𝟙_ (C ⥤ Type max v' v u) ⟶ F.functorHom G) where
+  toFun f := ⟨fun _ _ ↦ HomObj.ofNatTrans f, _⟩
+  invFun f := ⟨fun X ↦ (f.app X (PUnit.unit)).app X (𝟙 _), by
+    intro X Y φ
+    rw [← (f.app X (PUnit.unit)).naturality φ]
+    congr 1
+    have := HomObj.congr_app (congr_fun (f.naturality φ) PUnit.unit) Y (𝟙 Y)
+    dsimp [functorHom, HomObjFunctor] at this
+    aesop ⟩
+  left_inv _ := rfl
+  right_inv f := by
+    ext X a Y φ
+    have := HomObj.congr_app (congr_fun (f.naturality φ) PUnit.unit) Y (𝟙 Y)
+    dsimp [functorHom, HomObjFunctor] at this
+    aesop
 
 end Functor
 
 namespace FunctorToTypes
 
-/-
-/-- The ulift functor `(C ⥤ Type v) ⥤ C ⥤ Type max w v u` on functors to Type. -/
-def uliftFunctor : (C ⥤ Type v) ⥤ C ⥤ Type max w v u :=
-  (whiskeringRight _ _ _).obj CategoryTheory.uliftFunctor.{max w v u}
+variable (F : C ⥤ Type max w v u)
 
-/-- the co-Yoneda embedding ulifted from `Type v` to `Type max w v u` -/
-def coyoneda {C : Type u} [Category.{v} C] : Cᵒᵖ ⥤ C ⥤ Type max w v u :=
-    CategoryTheory.coyoneda ⋙ uliftFunctor.{w}
+open Functor in
+/-- When `F G H : C ⥤ Type max w v u`, we have `(F ⊗ G ⟶ H) ≃ (G ⟶ F.functorHom H)`. -/
+def functorHomEquiv (G H : C ⥤ Type max w v u) : (F ⊗ G ⟶ H) ≃ (G ⟶ F.functorHom H) :=
+  (HomObjEquiv F H G).trans (Functor.functorHomEquiv F H G)
 
-/-- The internal hom of two functors `C ⥤ Type max w v u`. -/
-def ihom (F G : C ⥤ Type max w v u) : C ⥤ Type max w v u where
-  obj c := coyoneda.obj (.op c) ⊗ F ⟶ G
-  map := fun f g ↦ coyoneda.map (.op f) ▷ F ≫ g
--/
-
-/-- Given a morphism `f : G ⟶ H`, and object `(c : C)`, and an element of `(F.ihom G).obj c`,
-construct an element of `(F.ihom H).obj c`. -/
-def rightAdj_map {F G H : C ⥤ Type (max w v u)} (f : G ⟶ H) (c : C) (a : (F.ihom G).obj c) :
-    (F.ihom H).obj c where
+/-- Given a morphism `f : G ⟶ H`, an object `c : C`, and an element of `(F.functorHom G).obj c`,
+construct an element of `(F.functorHom H).obj c`. -/
+def rightAdj_map {F G H : C ⥤ Type max w v u} (f : G ⟶ H) (c : C) (a : (F.functorHom G).obj c) :
+    (F.functorHom H).obj c where
       app := fun d b ↦ (a.app d b) ≫ (f.app d)
       naturality g h := by
         have := a.naturality g h
@@ -80,85 +169,26 @@ def rightAdj_map {F G H : C ⥤ Type (max w v u)} (f : G ⟶ H) (c : C) (a : (F.
         aesop
 
 /-- A right adjoint of `tensorLeft F`. -/
-def rightAdj (F : C ⥤ Type max w v u) : (C ⥤ Type max w v u) ⥤ C ⥤ Type max w v u where
-  obj G := F.ihom G
+def rightAdj : (C ⥤ Type max w v u) ⥤ C ⥤ Type max w v u where
+  obj G := F.functorHom G
   map f := { app := rightAdj_map f }
 
-variable {F G H : C ⥤ Type max w v u}
-
-/-- Given a morphism `F ⊗ G ⟶ H`, an object `(c : C)` and an element of `G.obj c`, construct an
-element of `(F.ihom H).obj c`. Used to construct a morphism `G ⟶ F.ihom H`. -/
-def homEquiv_toFun_app (f : F ⊗ G ⟶ H) (c : C) (y : G.obj c) :
-    (F.ihom H).obj c where
-  app d g x := f.app d (x, G.map g y)
-  naturality g h := by
-    ext x
-    have := f.naturality g
-    apply_fun (fun f ↦ f (x, G.map h y)) at this
-    aesop
-
-@[ext]
-lemma homEquiv_toFun_app_ext {c : C} (f g : (F.ihom G).obj c) :
-    f.app = g.app → f = g := Functor.Hom₂.ext _ _
-
-/-- Given a morphism `F ⊗ G ⟶ H`, construct a morphism `G ⟶ F.ihom H`. -/
-def homEquiv_toFun (f : F ⊗ G ⟶ H) : G ⟶ F.ihom H where
-  app := homEquiv_toFun_app f
-  naturality _ _ _ := by
-    ext _
-    dsimp [homEquiv_toFun_app, Functor.ihom, Functor.hom₂Functor]
-    aesop
-
-@[ext]
-lemma homEquiv_toFun_ext (f g : G ⟶ F.ihom H) :
-    f.app = g.app → f = g := NatTrans.ext _ _
-
-/-- Given a morphism `G ⟶ F.ihom H`, construct a morphism `F ⊗ G ⟶ H`. -/
-def homEquiv_invFun (f : G ⟶ F.ihom H) : F ⊗ G ⟶ H where
-  app c x := (f.app c x.2).app c (𝟙 c) x.1
-  naturality c d g := by
-    ext ⟨x, y⟩
-    have h := f.naturality g
-    apply_fun (fun f ↦ (f y).app d (𝟙 d) (F.map g x)) at h
-    have h' := (f.app c y).naturality g (𝟙 c)
-    apply_fun (fun f ↦ f x) at h'
-    dsimp at h h' ⊢
-    rw [h, ← h']
-    dsimp [Functor.ihom, Functor.hom₂Functor]
-    aesop
-
-@[ext]
-lemma homEquiv_invFun_ext (f g : F ⊗ G ⟶ H) :
-    f.app = g.app → f = g := NatTrans.ext _ _
-
-/-- The bijection between morphisms `F ⊗ G ⟶ H` and morphisms `G ⟶ F.ihom H`. -/
-def homEquiv (F G H : C ⥤ Type max w v u) : (F ⊗ G ⟶ H) ≃ (G ⟶ F.ihom H) where
-  toFun := homEquiv_toFun
-  invFun := homEquiv_invFun
-  left_inv _ := by ext; dsimp only [homEquiv_invFun, homEquiv_toFun, homEquiv_toFun_app]; aesop
-  right_inv f := by
-    ext c y d g x
-    have b := f.naturality g
-    apply_fun (fun f ↦ (f y).app d (𝟙 d) x) at b
-    dsimp only [Functor.ihom, Functor.hom₂Functor] at b ⊢
-    aesop
-
+open Functor in
 /-- The adjunction `tensorLeft F ⊣ rightAdj F`. -/
-def adj (F : C ⥤ Type max w v u) : tensorLeft F ⊣ rightAdj F where
-  homEquiv := homEquiv F
+def adj : tensorLeft F ⊣ rightAdj F where
+  homEquiv := functorHomEquiv F
   unit := {
-    app := fun G ↦ homEquiv_toFun (𝟙 _)
+    app := fun G ↦ functorHomEquiv F _ _ (𝟙 _)
     naturality := fun G H f ↦ by
       ext c y
-      dsimp [rightAdj, homEquiv_toFun, homEquiv_toFun_app]
+      dsimp [rightAdj, functorHomEquiv, Functor.functorHomEquiv]
       ext d
-      dsimp only [Monoidal.tensorObj_obj, comp, Monoidal.whiskerLeft_app, whiskerLeft_apply]
+      dsimp only [Monoidal.tensorObj_obj, rightOp_obj]
       rw [Eq.symm (FunctorToTypes.naturality G H f _ y)]
-      rfl
-  }
-  counit := { app := fun G ↦ homEquiv_invFun (𝟙 _) }
+      rfl }
+  counit := { app := fun G ↦ (functorHomEquiv F _ _).2 (𝟙 _) }
 
-instance closed (F : C ⥤ Type max w v u) : Closed F where
+instance closed : Closed F where
   adj := adj F
 
 instance monoidalClosed : MonoidalClosed (C ⥤ Type max w v u) where
