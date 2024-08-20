42314ace4129340f1fad8e582d4d842865ffcf63
adomani <adomani@gmail.com>
2024-08-20 12:16:16 +0200

    `Simple` extension and docs

--- Mathlib/Util/Attr.lean
+++ Mathlib/Util/Attr.lean
@@ -4,40 +4,36 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Damiano Testa
 -/
 
-import Mathlib.Init
-import Lean.Elab.Command
+import Lean.Environment
 
 /-!
-# User commands for assert the (non-)existence of declaration or instances.
+# Environment extension for tracking existence of declarations and imports
 
-These commands are used to enforce the independence of different parts of mathlib.
-
-## TODO
-
-Potentially after the port reimplement the mathlib 3 linters to check that declarations asserted
-about do eventually exist.
-
-Implement `assert_instance` and `assert_no_instance`
+This is used by the `assert_not_exists` and `assert_not_imported` commands.
 -/
 
 section
-open Lean Elab Meta Command
+open Lean Elab Meta
 
 namespace Mathlib.AssertNotExist
-structure AssertExists where
-  /-- The fully qualified name of a declaration that is expected to exist. -/
-  isDecl : Bool
-  givenName : Name
-  modName : Name
 
-/-- Defines the `assertExistsExt` extension for adding an `Array` of `AssertExists`s
+/-- `AssertExists` is the structure that carries the data to check if a declaration or an
+import are meant to exist somewhere in Mathlib. -/
+structure AssertExists where
+  /-- The type of the assertion: `true` means declaration, `false` means import. -/
+  isDecl : Bool
+  /-- The fully qualified name of a declaration that is expected to exist. -/
+  givenName : Name
+  /-- The name of the module where the assertion was made. -/
+  modName : Name
+  deriving BEq, Hashable
+
+/-- Defines the `assertExistsExt` extension for adding a `HashSet` of `AssertExists`s
 to the environment. -/
-initialize assertExistsExt : PersistentEnvExtension AssertExists AssertExists (Array AssertExists) ←
-  registerPersistentEnvExtension {
-    mkInitial := pure {}
-    addImportedFn := (return .flatten ·)
-    addEntryFn := .push
-    exportEntriesFn := id
+initialize assertExistsExt : SimplePersistentEnvExtension AssertExists (HashSet AssertExists) ←
+  registerSimplePersistentEnvExtension {
+    addImportedFn := fun as => as.foldl HashSet.insertMany {}
+    addEntryFn := .insert
   }
 
 end Mathlib.AssertNotExist
