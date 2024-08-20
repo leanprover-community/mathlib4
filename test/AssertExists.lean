dd6013af3e284c0f5a44337994bd0cfb6b0e2137
adomani <adomani@gmail.com>
2024-08-20 13:21:09 +0200

    test file

--- test/AssertExists.lean
+++ test/AssertExists.lean
@@ -6,3 +6,20 @@ assert_not_imported
   Mathlib.Tactic.Common
   Lean.Elab.Command
   I_do_not_exist
+
+assert_not_exists Nats
+
+theorem Nats : True := .intro
+
+/--
+warning: ❌ 'Mathlib.Tactic.Common' (module) asserted in 'test.AssertExists'.
+---
+warning: ❌ 'I_do_not_exist' (module) asserted in 'test.AssertExists'.
+---
+info: ✅ 'Nats' (declaration) asserted in 'test.AssertExists'.
+---
+info: ✅ means the declaration or import exists.
+❌ means the declaration or import does not exist.
+-/
+#guard_msgs in
+#check_assertions
