b877586bd044c04a766828d9e8275e659e9ba8d5
Yaël Dillies <yael.dillies@gmail.com>
2024-08-20 12:46:28 +0000

    feat: Pontryagin duality for finite abelian groups Prove Pontryagin duality in case of finite abelian groups.
    This states that any finite abelian group is canonically isomorphic to its double dual (the space of complex-valued characters of its space of complex-valued characters).
    
    We first prove it for `ZMod n` and then extend to all finite abelian groups using the
    Structure Theorem.
    
    From LeanAPAP

--- Mathlib/Analysis/SpecialFunctions/Complex/CircleAddChar.lean
+++ Mathlib/Analysis/SpecialFunctions/Complex/CircleAddChar.lean
@@ -17,9 +17,41 @@ These results are separate from `Analysis.SpecialFunctions.Complex.Circle` in or
 the imports of that file.
 -/
 
-open Complex Function
+open AddChar Complex Function Real
 
-open scoped Real
+namespace Circle
+variable {r s : ℝ}
+
+/-- The standard character sending `r : ℝ` to $$e^{2πir}$$. -/
+noncomputable def e : AddChar ℝ Circle where
+  toFun r := exp (2 * π * r)
+  map_zero_eq_one' := by simp
+  map_add_eq_mul' := by simp [mul_add, Complex.exp_add]
+
+lemma e_apply (r : ℝ) : e r = exp (2 * π * r) := rfl
+
+@[simp, norm_cast] lemma coe_e (r : ℝ) : ↑(e r) = Complex.exp ((2 * π * r : ℝ) * Complex.I) := rfl
+
+@[simp] lemma e_int (z : ℤ) : e z = 1 := exp_two_pi_mul_int _
+@[simp] lemma e_one : e 1 = 1 := by simpa using e_int 1
+@[simp] lemma e_add_int {z : ℤ} : e (r + z) = e r := by rw [map_add_eq_mul, e_int, mul_one]
+@[simp] lemma e_sub_int {z : ℤ} : e (r - z) = e r := by rw [map_sub_eq_div, e_int, div_one]
+@[simp] lemma e_fract (r : ℝ) : e (Int.fract r) = e r := by rw [Int.fract, e_sub_int]
+
+@[simp] lemma e_mod_div {m : ℤ} {n : ℕ} : e ((m % n : ℤ) / n) = e (m / n) := by
+  obtain hn | hn := eq_or_ne (n : ℝ) 0
+  · rw [hn, div_zero, div_zero]
+  · rw [Int.emod_def, Int.cast_sub, sub_div, Int.cast_mul, Int.cast_natCast,
+      mul_div_cancel_left₀ _ hn, e_sub_int]
+
+lemma e_eq_one : e r = 1 ↔ ∃ n : ℤ, r = n := by
+  simp [e_apply, exp_eq_one, mul_comm (2 * π), pi_ne_zero]
+
+lemma e_inj : e r = e s ↔ r ≡ s [PMOD 1] := by
+  simp [AddCommGroup.ModEq, ← e_eq_one, div_eq_one, map_sub_eq_div, eq_comm (b := 1),
+    eq_comm (a := e r)]
+
+end Circle
 
 /-- The canonical map from the additive to the multiplicative circle, as an `AddChar`. -/
 noncomputable def AddCircle.toCircle_addChar {T : ℝ} : AddChar (AddCircle T) Circle where
