fca54dee7217d199ba665a6626ea7f43b1001a6e
adomani <adomani@gmail.com>
2024-08-21 14:49:00 +0200

    extract cache size

--- scripts/olean_comparison.sh
+++ scripts/olean_comparison.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
+getCacheSize () {
+  rm -rf .lake/build/lib/Mathlib
+  lake exe cache clean!
+  lake exe cache get
+  du --summarize --human-readable .lake
+}
+
 # Because the `lean-pr-testing-NNNN` branches use toolchains that are "updated in place"
 # the cache mechanism is unreliable, so we don't test it if we are on such a branch.
 if [[ ! $(cat lean-toolchain) =~ ^leanprover/lean4-pr-releases:pr-release-[0-9]+$ ]]; then
   git fetch origin master --depth 1
   git checkout origin/master
-  rm -rf .lake/build/lib/Mathlib
-  lake exe cache clean!
-  lake exe cache get
-  masterOleans="$(du --summarize --human-readable .lake)"
+  masterOleans="$(getCacheSize)"
   git checkout -
-  lake exe cache clean!
-  rm -rf .lake/build/lib/Mathlib
-  lake exe cache get
-  newOleans="$(du --summarize --human-readable .lake)"
+  newOleans="$(getCacheSize)"
   printf '.lake size\nmaster: %s\nthis PR: %s' "${masterOleans}" "${newOleans}"
 fi
