a4d6a3c834e1b6e699368909b5b5e6f12721ae72
Rida Hamadani <mridahamadani@gmail.com>
2024-08-21 15:26:19 +0300

    init

--- Mathlib/GroupTheory/Perm/Basic.lean
+++ Mathlib/GroupTheory/Perm/Basic.lean
@@ -154,6 +154,17 @@ theorem self_trans_inv (e : Perm α) : e.trans e⁻¹ = 1 :=
 theorem symm_mul (e : Perm α) : e.symm * e = 1 :=
   Equiv.self_trans_symm e
 
+theorem trans_eq_one_iff_eq_symm (e₁ e₂ : Equiv.Perm α) :
+  e₁.trans e₂ = 1 ↔ e₁ = e₂.symm := by
+  refine ⟨fun h ↦ ?_, fun h ↦ ?_⟩
+  · ext x
+    have : e₁.trans e₂ x = x := by rw [h, coe_one, id_eq]
+    apply congrArg (⇑(e₂.symm)) at this
+    rw [trans_apply, symm_apply_apply] at this
+    exact this
+  · rw [h, ← symm_mul]
+    rfl
+
 /-! Lemmas about `Equiv.Perm.sumCongr` re-expressed via the group structure. -/
 
 
