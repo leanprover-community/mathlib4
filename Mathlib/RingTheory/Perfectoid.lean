5e1137520f1bab9bfd51dc91124bd6049e2a79f6
jjdishere <emailboxofjjd@163.com>
2024-08-21 21:20:31 +0200

    Update Perfectoid.lean

--- Mathlib/RingTheory/Perfectoid.lean
+++ Mathlib/RingTheory/Perfectoid.lean
@@ -6,6 +6,6 @@ import Mathlib.RingTheory.Perfection
 
 open Valuation Valued
 
-class PerfectoidField (K : Type u) {Γ : outParam Type*} [Field K] [LinearOrderedCommGroup Γ] [vK : Valued K Γ] [vK.v.RankOne] : Prop where
-  complete : IsComplete K
-  
+class PerfectoidField (K : Type u) {Γ : outParam Type*} [Field K] [LinearOrderedCommGroup Γ] [vK : Valued K Γ] [vK.v.RankOne] [CompleteSpace K] : Prop where
+  exists_p_mem_span_pow_p : ∃ π : 𝒪[K], ¬ IsUnit π ∧ (p : 𝒪[K]) ∈ Ideal.span {π ^ p}
+  exist_p_th_root : ∀ x : 𝒪[K]⧸Ideal.span {(p : 𝒪[K])}, ∃ y : 𝒪[K]⧸Ideal.span {(p : 𝒪[K])} , x = y ^ p
