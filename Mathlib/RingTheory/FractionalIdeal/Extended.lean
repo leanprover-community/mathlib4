49461bc9869fd5a76fa37c0864742d7979c10f38
js2357 <James.Sundstrom@gmail.com>
2024-08-21 02:27:03 -0400

    Cleanup (as per review)

--- Mathlib/RingTheory/FractionalIdeal/Extended.lean
+++ Mathlib/RingTheory/FractionalIdeal/Extended.lean
@@ -58,75 +58,54 @@ local notation "map_f" => (IsLocalization.map (S := K) L f hf)
 lemma mem_extended_iff (x : L) : (x ∈ I.extended L hf) ↔ x ∈ span B (map_f '' I) := by
   constructor <;> { intro hx; simpa }
 
-lemma extended_eq : (I.extended L hf).coeToSubmodule = span B (map_f '' I) := by
+@[simp]
+lemma coe_extended_eq_span : I.extended L hf = span B (map_f '' I) := by
   ext; simp [mem_coe, mem_extended_iff]
 
-theorem extended_zero : extended L hf (0 : FractionalIdeal M K) = 0 := by
-  rw [extended, eq_zero_iff]
-  intro x hx
-  rw [← mem_coe, coe_mk] at hx
-  change x ∈ (0 : Submodule B L)
-  convert hx
-  convert Submodule.span_zero.symm
+@[simp]
+theorem extended_zero : extended L hf (0 : FractionalIdeal M K) = 0 :=
   have : ((0 : FractionalIdeal M K) : Set K) = {0} := by ext; simp
-  rw [this, Set.image_singleton, _root_.map_zero, Set.singleton_zero]
+  coeToSubmodule_injective (by simp [this])
 
+@[simp]
 theorem extended_one : extended L hf (1 : FractionalIdeal M K) = 1 := by
-  apply coeToSubmodule_injective
-  convert extended_eq L hf (1 : FractionalIdeal M K)
-  refine Submodule.ext <| fun x ↦ Iff.intro ?_  <| fun hx ↦
-    span_induction hx ?_ (zero_mem _) (fun y z hy hz ↦ add_mem hy hz) (fun b y hy ↦ smul_mem _ b hy)
+  refine coeToSubmodule_injective <| Submodule.ext fun x ↦ ⟨fun hx ↦ span_induction hx
+    ?_ (zero_mem _) (fun y z hy hz ↦ add_mem hy hz) (fun b y hy ↦ smul_mem _ b hy), ?_⟩
   · rintro ⟨b, _, rfl⟩
     rw [Algebra.linearMap_apply, Algebra.algebraMap_eq_smul_one]
-    exact smul_mem _ _ <| Submodule.subset_span ⟨1, by simp [one_mem_one M], _root_.map_one _⟩
+    exact smul_mem _ _ <| subset_span ⟨1, by simp [one_mem_one]⟩
   · rintro _ ⟨_, ⟨a, ha, rfl⟩, rfl⟩
     exact ⟨f a, ha, by rw [Algebra.linearMap_apply, Algebra.linearMap_apply, map_eq]⟩
 
 theorem extended_add : (I + J).extended L hf = (I.extended L hf) + (J.extended L hf) := by
-  refine le_antisymm (fun _ h ↦ ?_) ?_
-  · simp only [val_eq_coe, mem_coe] at h ⊢
-    refine span_induction ((mem_extended_iff L hf _ _).1 h) ?_ (zero_mem _)
-      (fun x y hx hy ↦ Submodule.add_mem _ hx hy) (fun b y hy ↦ smul_mem _ b hy)
-    · rintro _ ⟨y, hy, rfl⟩
-      rw [← FractionalIdeal.coeToSet_coeToSubmodule, SetLike.mem_coe, mem_coe, mem_add] at hy
-      rw [mem_add]
-      rcases hy with ⟨i, hi, j, hj, rfl⟩
-      use map_f i, (I.extended_eq L hf) ▸ subset_span <| Set.mem_image_of_mem map_f hi
-      use map_f j, (J.extended_eq L hf) ▸ subset_span <| Set.mem_image_of_mem map_f hj
-      rw [map_add]
-  · change (extended L hf I + extended L hf J).coeToSubmodule ≤ (extended L hf _).coeToSubmodule
-    rw [coe_add, Submodule.add_eq_sup]
-    refine sup_le (span_mono (Set.image_mono ?_)) (span_mono (Set.image_mono ?_))
-    · intro i hi
-      rw [← FractionalIdeal.coeToSet_coeToSubmodule, SetLike.mem_coe, mem_coe, mem_add]
-      use i, hi, 0, zero_mem J, add_zero i
-    · intro j hj
-      rw [← FractionalIdeal.coeToSet_coeToSubmodule, SetLike.mem_coe, mem_coe, mem_add]
-      use 0, zero_mem I, j, hj, zero_add j
+  apply coeToSubmodule_injective
+  simp only [coe_extended_eq_span, coe_add, Submodule.add_eq_sup, ← span_union, ← Set.image_union]
+  apply Submodule.span_eq_span
+  · rintro _ ⟨y, hy, rfl⟩
+    obtain ⟨i, hi, j, hj, rfl⟩ := (mem_add I J y).mp <| SetLike.mem_coe.mp hy
+    rw [RingHom.map_add]
+    exact add_mem (Submodule.subset_span ⟨i, Set.mem_union_left _ hi, by simp⟩)
+      (Submodule.subset_span ⟨j, Set.mem_union_right _ hj, by simp⟩)
+  · rintro _ ⟨y, hy, rfl⟩
+    suffices y ∈ I + J from SetLike.mem_coe.mpr <| Submodule.subset_span ⟨y, by simp [this]⟩
+    exact hy.elim (fun h ↦ (mem_add I J y).mpr ⟨y, h, 0, zero_mem J, add_zero y⟩)
+      (fun h ↦ (mem_add I J y).mpr ⟨0, zero_mem I, y, h, zero_add y⟩)
 
 theorem extended_mul : (I * J).extended L hf = (I.extended L hf) * (J.extended L hf) := by
-  refine FractionalIdeal.ext fun _ ↦ ⟨fun h ↦ ?_, fun h ↦ ?_⟩
-  · refine span_induction ((mem_extended_iff L hf _ _).1 h) ?_ (zero_mem _)
-      (fun y z hy hz ↦ Submodule.add_mem _ hy hz) (fun b y hy ↦ smul_mem _ b hy)
-    · rintro _ ⟨y, hy, rfl⟩
-      refine FractionalIdeal.mul_induction_on hy (fun i hi j hj ↦ ?_) (fun y z hy hz ↦ ?_)
-      · rw [_root_.map_mul]
-        apply mul_mem_mul <;> refine (mem_extended_iff L hf _ _).2 <| Submodule.subset_span ?_
-        · use i, hi
-        · use j, hj
-      · rw [_root_.map_add]
-        exact Submodule.add_mem _ hy hz
-  · refine FractionalIdeal.mul_induction_on h ?_ (fun x y hx hy ↦ Submodule.add_mem _ hx hy)
-    intro x hx
-    refine span_induction ((I.mem_extended_iff L hf x).1 hx) ?_ (by simp [zero_mem])
-      (fun x y hx hy z hz ↦ add_mul x y z ▸ Submodule.add_mem _ (hx z hz) (hy z hz))
-      (fun x y hy z hz ↦ smul_mul_assoc x y z ▸ smul_mem _ x (hy z hz))
-    rintro _ ⟨i, hi, rfl⟩ y hy
-    refine span_induction ((J.mem_extended_iff L hf y).1 hy) ?_
-      ((mul_zero (map_f i)).symm ▸ zero_mem _)
-      (fun z w hz hw ↦ mul_add (map_f i) z w ▸ Submodule.add_mem _ hz hw)
-      (fun z w hw ↦ mul_smul_comm z (map_f i) w ▸ smul_mem _ z hw)
-    rintro _ ⟨j, hj, rfl⟩
-    exact Submodule.subset_span ⟨i * j, mul_mem_mul hi hj, RingHom.map_mul map_f i j⟩
+  apply coeToSubmodule_injective
+  simp only [coe_extended_eq_span, coe_mul, span_mul_span]
+  refine Submodule.span_eq_span (fun _ h ↦ ?_) (fun _ h ↦ ?_)
+  · rcases h with ⟨x, hx, rfl⟩
+    replace hx : x ∈ (I : Submodule A K) * (J : Submodule A K) := coe_mul I J ▸ hx
+    rw [Submodule.mul_eq_span_mul_set] at hx
+    refine span_induction hx (fun y hy ↦ ?_) (by simp) (fun y z hy hz ↦ ?_) (fun a y hy ↦ ?_)
+    · rcases Set.mem_mul.mp hy with ⟨i, hi, j, hj, rfl⟩
+      exact subset_span <| Set.mem_mul.mpr
+        ⟨map_f i, ⟨i, hi, by simp [hi]⟩, map_f j, ⟨j, hj, by simp [hj]⟩, by simp⟩
+    · exact map_add map_f y z ▸ Submodule.add_mem _ hy hz
+    · rw [Algebra.smul_def, map_mul, map_eq, ← Algebra.smul_def]
+      exact smul_mem _ (f a) hy
+  · rcases Set.mem_mul.mp h with ⟨y, ⟨i, hi, rfl⟩, z, ⟨j, hj, rfl⟩, rfl⟩
+    exact Submodule.subset_span ⟨i * j, mul_mem_mul hi hj, by simp⟩
 
 end FractionalIdeal
