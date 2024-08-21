cd4b63aa645300d6c652ee3e225094717a7dc5ba
Kim Morrison <kim@tqft.net>
2024-08-21 13:22:26 +1000

    fix

--- Mathlib/CategoryTheory/Functor/FullyFaithful.lean
+++ Mathlib/CategoryTheory/Functor/FullyFaithful.lean
@@ -180,7 +180,7 @@ def preimageIso {X Y : C} (e : F.obj X ≅ F.obj Y) : X ≅ Y where
   hom_inv_id := hF.map_injective (by simp)
   inv_hom_id := hF.map_injective (by simp)
 
-lemma isIso_of_isIso_map (hF : F.FullyFaithful) {X Y : C} (f : X ⟶ Y) [IsIso (F.map f)] :
+lemma isIso_of_isIso_map {X Y : C} (f : X ⟶ Y) [IsIso (F.map f)] :
     IsIso f := by
   simpa using (hF.preimageIso (asIso (F.map f))).isIso_hom
 
