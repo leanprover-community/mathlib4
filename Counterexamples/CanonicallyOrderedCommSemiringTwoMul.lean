3ffd3ef5dbd2447d4fc58ae60dc0985dd9408be5
Yury G. Kudryashov <urkud@urkud.name>
2024-08-20 23:07:34 -0500

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Counterexamples/CanonicallyOrderedCommSemiringTwoMul.lean
+++ Counterexamples/CanonicallyOrderedCommSemiringTwoMul.lean
@@ -135,12 +135,12 @@ theorem mul_L {a b : ℕ × ZMod 2} (ha : a ≠ (0, 1)) (hb : b ≠ (0, 1)) : a 
   rcases b with ⟨b, b2⟩
   cases b
   · rcases mem_zmod_2 b2 with (rfl | rfl) <;> rcases mem_zmod_2 a2 with (rfl | rfl) <;>
-      simp only [Prod.mk_mul_mk, mul_zero, mul_one, ne_eq, Prod.mk.injEq, zero_ne_one, and_false, 
+      simp only [Prod.mk_mul_mk, mul_zero, mul_one, ne_eq, Prod.mk.injEq, zero_ne_one, and_false,
         not_false_eq_true, not_true_eq_false]
     exact hb rfl
   cases a
   · rcases mem_zmod_2 b2 with (rfl | rfl) <;> rcases mem_zmod_2 a2 with (rfl | rfl) <;>
-      simp only [Prod.mk_mul_mk, mul_zero, zero_mul, mul_one, ne_eq, Prod.mk.injEq, zero_ne_one, and_false, 
+      simp only [Prod.mk_mul_mk, mul_zero, zero_mul, mul_one, ne_eq, Prod.mk.injEq, zero_ne_one, and_false,
         not_false_eq_true, not_true_eq_false]
     exact ha rfl
   · simp [mul_ne_zero _ _, Nat.succ_ne_zero _]
