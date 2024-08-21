b38ee18109d0e02455f409202f147a2c2870742f
FM22 <artem.khovanov@gmail.com>
2024-08-21 17:58:10 +0100

    docstrings

--- Mathlib/Algebra/Order/Group/Cone.lean
+++ Mathlib/Algebra/Order/Group/Cone.lean
@@ -22,6 +22,7 @@ class CommGroup.GroupConeClass (S G : Type*) [CommGroup G] [SetLike S G] extends
     SubmonoidClass S G : Prop where
   eq_one_of_mem_of_inv_mem {C : S} {a : G} : a ∈ C → a⁻¹ ∈ C → a = 1
 
+/-- `AddGroupConeClass S G` says that `S` is a type of cones in `G`. -/
 class AddCommGroup.AddGroupConeClass (S G : Type*) [AddCommGroup G] [SetLike S G] extends
     AddSubmonoidClass S G : Prop where
   eq_zero_of_mem_of_neg_mem {C : S} {a : G} : a ∈ C → -a ∈ C → a = 0
@@ -32,12 +33,16 @@ export CommGroup.GroupConeClass (eq_one_of_mem_of_inv_mem)
 export AddCommGroup.AddGroupConeClass (eq_zero_of_mem_of_neg_mem)
 
 /-- A (positive) cone in an abelian group is a submonoid that
-does not contain both `a` and `-a` for any nonzero `a`.
-This is equivalent to being the set of non-negative elements of
+does not contain both `a` and `a⁻¹` for any non-identity `a`.
+This is equivalent to being the set of elements that are at least 1 in
 some order making the group into a partially ordered group. -/
 structure CommGroup.GroupCone (G : Type*) [CommGroup G] extends Submonoid G where
   eq_one_of_mem_of_inv_mem' {a} : a ∈ carrier → a⁻¹ ∈ carrier → a = 1
 
+/-- A (positive) cone in an abelian group is a submonoid that
+does not contain both `a` and `-a` for any nonzero `a`.
+This is equivalent to being the set of non-negative elements of
+some order making the group into a partially ordered group. -/
 structure AddCommGroup.AddGroupCone (G : Type*) [AddCommGroup G] extends AddSubmonoid G where
   eq_zero_of_mem_of_neg_mem' {a} : a ∈ carrier → -a ∈ carrier → a = 0
 
@@ -60,6 +65,7 @@ class CommGroup.MaximalGroupConeClass (S G : Type*) [CommGroup G] [SetLike S G]
     extends GroupConeClass S G : Prop where
   mem_or_inv_mem (C : S) (a : G) : a ∈ C ∨ a⁻¹ ∈ C
 
+/-- `MaximalAddGroupConeClass S G` says that `S` is a type of maximal cones in `G`. -/
 class AddCommGroup.MaximalAddGroupConeClass (S G : Type*) [AddCommGroup G] [SetLike S G]
     extends AddGroupConeClass S G : Prop where
   mem_or_neg_mem (C : S) (a : G) : a ∈ C ∨ -a ∈ C
@@ -70,12 +76,16 @@ export CommGroup.MaximalGroupConeClass (mem_or_inv_mem)
 export AddCommGroup.MaximalAddGroupConeClass (mem_or_neg_mem)
 
 /-- A maximal (positive) cone in an abelian group is a cone containing
-either `a` or `-a` for every `a`.
-This is equivalent to being the set of non-negative elements of
+either `a` or `a⁻¹` for every `a`.
+This is equivalent to being the set of elements that are at least 1 in
 some order making the group into a linearly ordered group. -/
 structure CommGroup.MaximalGroupCone (G : Type*) [CommGroup G] extends GroupCone G where
   mem_or_inv_mem' a : a ∈ carrier ∨ a⁻¹ ∈ carrier
 
+/-- A maximal (positive) cone in an abelian group is a cone containing
+either `a` or `-a` for every `a`.
+This is equivalent to being the set of non-negative elements of
+some order making the group into a linearly ordered group. -/
 structure AddCommGroup.MaximalAddGroupCone (G : Type*) [AddCommGroup G]
     extends AddCommGroup.AddGroupCone G where
   mem_or_neg_mem' a : a ∈ carrier ∨ -a ∈ carrier
@@ -104,8 +114,10 @@ namespace CommGroup.GroupCone
 variable {H : Type*} [OrderedCommGroup H] {a : H}
 
 variable (H) in
-/-- Construct a cone from the set of non-negative elements of a partially ordered abelian group. -/
-@[to_additive nonneg]
+/-- Construct a cone from the set of elements of
+a partially ordered abelian group that are at least 1. -/
+@[to_additive nonneg
+"Construct a cone from the set of non-negative elements of a partially ordered abelian group."]
 def oneLE : GroupCone H where
   __ := Submonoid.oneLE H
   eq_one_of_mem_of_inv_mem' {a} := by simpa using ge_antisymm
@@ -123,9 +135,11 @@ namespace MaximalGroupCone
 variable {H : Type*} [LinearOrderedCommGroup H] {a : H}
 
 variable (H) in
-/-- Construct a maximal cone from the set of non-negative elements of
-a linearly ordered abelian group. -/
-@[to_additive nonneg]
+/-- Construct a maximal cone from the set of elements of
+a linearly ordered abelian group that are at least 1. -/
+@[to_additive nonneg
+"Construct a maximal cone from the set of non-negative elements of
+a linearly ordered abelian group."]
 def oneLE : MaximalGroupCone H where
   __ := GroupCone.oneLE H
   mem_or_inv_mem' := by simpa using le_total 1
@@ -144,7 +158,8 @@ variable {S G : Type*} [CommGroup G] [SetLike S G] (C : S)
 open CommGroup
 
 /-- Construct a partially ordered abelian group by designating a cone in an abelian group. -/
-@[to_additive (attr := reducible)]
+@[to_additive (attr := reducible)
+"Construct a partially ordered abelian group by designating a cone in an abelian group."]
 def OrderedCommGroup.mkOfCone [GroupConeClass S G] :
     OrderedCommGroup G where
   le a b := b / a ∈ C
@@ -154,9 +169,9 @@ def OrderedCommGroup.mkOfCone [GroupConeClass S G] :
     simpa [div_eq_one, eq_comm] using eq_one_of_mem_of_inv_mem nab (by simpa using nba)
   mul_le_mul_left a b nab c := by simpa using nab
 
-/-- Construct a linearly ordered abelian group by
-designating a maximal cone in an abelian group. -/
-@[to_additive (attr := reducible)]
+/-- Construct a linearly ordered abelian group by designating a maximal cone in an abelian group. -/
+@[to_additive (attr := reducible)
+"Construct a linearly ordered abelian group by designating a maximal cone in an abelian group."]
 def LinearOrderedCommGroup.mkOfCone
     [MaximalGroupConeClass S G] (dec : DecidablePred (· ∈ C)) :
     LinearOrderedCommGroup G where
