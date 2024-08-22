c71367e81429fe4249255a30d96b2807993ed680
Johan Commelin <johan@commelin.net>
2024-08-22 14:15:31 +0200

    remove Init header

--- Mathlib/Data/Int/Order/Basic.lean
+++ Mathlib/Data/Int/Order/Basic.lean
@@ -9,13 +9,6 @@ import Mathlib.Data.Nat.Notation
 import Mathlib.Order.Defs
 
 /-!
-# Note about `Mathlib/Init/`
-The files in `Mathlib/Init` are leftovers from the port from Mathlib3.
-(They contain content moved from lean3 itself that Mathlib needed but was not moved to lean4.)
-
-We intend to move all the content of these files out into the main `Mathlib` directory structure.
-Contributions assisting with this are appreciated.
-
 # The order relation on the integers
 -/
 
