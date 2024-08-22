5fc185784dcc6d2c006604880a8b1cc67d26cfea
Siddharth Bhat <siddu.druid@gmail.com>
2024-08-21 21:40:20 -0500

    chore: add unsat production

--- Mathlib/Tactic/Lattice.lean
+++ Mathlib/Tactic/Lattice.lean
@@ -8,7 +8,9 @@ This file implement a decision theory for the free theory of monotone functions 
 following [1]. It could be extended to complete lattices, given an implementation of the
 Dedekind MacNeille completion [2]
 
-[1] https://link.springer.com/chapter/10.1007/11964810_15
+[1] A Decision Procedure for Monotone Functions
+    over Bounded and Complete Lattices
+    (https://link.springer.com/chapter/10.1007/11964810_15)
 [2] https://en.wikipedia.org/wiki/Dedekind%E2%80%93MacNeille_completion
 -/
 import Lean -- HashMap
@@ -23,98 +25,230 @@ open Lean
 namespace Lattice
 open Lean Elab Meta Tactic
 
-structure Context where
-  nvars : Nat
+class BoundedLattice (A : Sort _) extends Lattice A, BoundedOrder A where
 
-instance : EmptyCollection Context where
-  emptyCollection := { nvars := 0 }
+class ToExprM (α : Type) where
+  toExprM : α → MetaM Expr
+open ToExprM
 
-structure Context.Var (c : Context) where
-  var : Fin c.nvars
-deriving DecidableEq, Hashable, Repr
+class OfExpr? (α : Type) where
+  ofExpr? : Expr → Option α
 
-namespace Context.Var
+class OfExpr (α : Type) where
+  ofExpr : Expr → α
 
-@[simp]
-def emptyElim {α : Sort _} (v : Var ∅) : α := v.var.elim0
+structure VarExpr where
+  e : Expr
+deriving BEq, Hashable
 
-end Context.Var
+def VarExpr.toExpr (v : VarExpr) : Expr := v.e
 
+instance : ToExprM VarExpr where
+  toExprM v := return v.e
 
-/-- atoms in SSA form, so each primitive operation returns a new variable -/
-inductive NormAtom (Γ : Context)
-| intersectEq (v l r : Γ.Var) -- v = l ∩ r
-| uniteEq (v l r : Γ.Var) -- v = l ∪ r
-| eq (l r : Γ.Var) -- l = r
-| neq (l r : Γ.Var) -- l ≠ r
-| leq (l r : Γ.Var) -- l ≤ r
-| notLeq (l r : Γ.Var) -- l ≰ r
-deriving DecidableEq, Hashable, Repr
+def VarExpr.ofExpr (e : Expr) : VarExpr := ⟨e⟩
 
--- TODO: need a hypothesis for `Assumption`.
-inductive Derivation (Γ : Context)
-| Assumption (e : Expr) (n : NormAtom Γ) : Derivation Γ
-deriving BEq, Hashable, Repr
--- TODO: add all derivations here.
+structure EqExpr where
+  ty? : Option Expr := .none
+  lhs : VarExpr
+  rhs : VarExpr
+deriving BEq, Hashable
 
-/-- The atom that is derived from the derivation. -/
-def Derivation.derivedAtom {Γ : Context} (d : Derivation Γ) : NormAtom Γ :=
-  match d with
-  | Derivation.Assumption _ n => n
+def EqExpr.inferType (e : EqExpr) : MetaM Expr := do
+  match e.ty? with
+  | some ty => return ty
+  | none => do
+    let ty ← Meta.inferType e.lhs.e
+    return ty
 
-structure Model (Γ : Context) where
-  /-- mapping from variables to Lean expressions. -/
-  var2expr : HashMap Γ.Var Expr
-  hmodel : ∀ (v : Γ.Var), var2expr.contains v
+def EqExpr.toExprM (e : EqExpr) : MetaM Expr :=
+  mkAppOptM `Eq #[e.ty?, e.lhs.toExpr, e.rhs.toExpr]
 
-instance : EmptyCollection (Model ∅) where
-  emptyCollection := {
-      var2expr := HashMap.empty,
-      hmodel := fun v => v.emptyElim
-    }
+instance : ToExprM EqExpr where
+  toExprM e := e.toExprM
 
-def Context.Var.denote {Γ : Context} (v : Γ.Var) (m : Model Γ) : Expr :=
-  m.var2expr.find! v
+def EqExpr.ofExpr? (e : Expr) : Option EqExpr :=
+  match_expr e with
+  | Eq ty lhs rhs  => do
+    let lhs := VarExpr.ofExpr lhs
+    let rhs := VarExpr.ofExpr rhs
+    some { ty? := ty, lhs := lhs, rhs := rhs }
+  | _ => none
 
-def NormAtom.denote {Γ : Context}
-    (n : NormAtom Γ) (m : Model Γ) : Expr :=
-  match n with
-  | NormAtom.intersectEq v l r => l.denote m
-  | NormAtom.uniteEq v l r => l.denote m
-  | NormAtom.eq l r => l.denote m
-  | NormAtom.neq l r => l.denote m
-  | NormAtom.leq l r => l.denote m
-  | NormAtom.notLeq l r => l.denote m
+structure NotExpr (α : Type) where
+  e : α
+deriving BEq, Hashable
 
+/-- info: Not (a : Prop) : Prop -/
+#guard_msgs in #check Not
+
+def NotExpr.toExpr {α : Type} [ToExprM α]
+    (n : NotExpr α) : MetaM Expr := do
+  return mkApp (mkConst `Not) (← toExprM n.e)
+
+instance {α : Type} [ToExprM α] : ToExprM (NotExpr α) where
+  toExprM n := do
+    return mkApp (mkConst `NotExpr.mk) (← toExprM n.e)
+
+def NeqExpr.ofExpr? {α : Type} [E : OfExpr? α] (e : Expr): Option (NotExpr α) :=
+  match_expr e with
+  | Not e' => do
+    let e' ← E.ofExpr? e'
+    some { e := e' }
+  | _ => none
+
+abbrev NeqExpr := NotExpr EqExpr
+
+structure LeqExpr where
+  lhs : VarExpr
+  rhs : VarExpr
+deriving BEq, Hashable
+
+/-- info: LE.le.{u} {α : Type u} [self : LE α] : α → α → Prop -/
+#guard_msgs in #check LE.le
+
+def LeqExpr.ofExpr? (e : Expr) : Option LeqExpr :=
+    match_expr e with
+    | LE.le _α _self a b =>
+      let lhs := VarExpr.ofExpr a
+      let rhs := VarExpr.ofExpr b
+      some { lhs := lhs, rhs := rhs }
+    | _ => none
+
+def LeqExpr.toExpr (leq : LeqExpr) : MetaM Expr := do
+  mkAppOptM ``LE.le #[.none, .none, leq.lhs.toExpr, leq.rhs.toExpr]
+
+instance : ToExprM LeqExpr where
+  toExprM e := e.toExpr
+
+instance : OfExpr? LeqExpr where
+  ofExpr? e := LeqExpr.ofExpr? e
+
+abbrev NotLeqExpr := NotExpr LeqExpr
+
+/-- info: Inf.inf.{u_1} {α : Type u_1} [self : Inf α] : α → α → α -/
+#guard_msgs in #check Inf.inf -- ⊓
+
+structure InfExpr where
+  ty : Expr
+  self : Expr
+  lhs : VarExpr
+  rhs : VarExpr
+deriving BEq, Hashable
+
+def InfExpr.toExpr (e : InfExpr) :=
+  mkApp4 (mkConst `Inf.inf) e.ty e.self e.lhs.toExpr e.rhs.toExpr
+
+def InfExpr.ofExpr? (e : Expr) : Option InfExpr :=
+  match_expr e with
+  | Inf.inf α self a b =>
+    let lhs := VarExpr.ofExpr a
+    let rhs := VarExpr.ofExpr b
+    some { ty := α, self := self, lhs := lhs, rhs := rhs }
+  | _ => none
+
+namespace Algorithm
+
+/- TODO: Eventually use discrimination trees to store proofs. -/
+#check DiscrTree
 
 structure State where
-  Γ : Context
-  model : Model Γ
   /-- Current fuel available -/
   fuel : Nat
-  /-- a mapping from normalized atoms to their derivations -/
-  norm2derivation : HashMap (NormAtom Γ) (Derivation Γ)
+  leqs : HashSet LeqExpr
+  nleqs : HashSet NotLeqExpr
+  neqs : HashSet NeqExpr
+  eqs : HashSet EqExpr
+  infs : HashSet InfExpr
+  vars : HashSet VarExpr
 
 structure Config where
   /-- number of basic operations to perform before giving up. -/
   fuel : Nat
 
-namespace Algorithm
-variable {Γ : Context}
-/-- given the current set of derivations, perform a new derivation. Returns 'false' if no progress has been made. -/
-def doWork
-    (derivations : HashMap (NormAtom Γ) (Derivation Γ)) :
-    HashMap (NormAtom Γ) (Derivation Γ) × Bool := sorry
-
-/-- show falsity, by returning the proof of a derivation and its negation. -/
-def isUnsat? : Option (Derivation Γ × Derivation Γ) := .none
-
-def isSat? (atom : NormAtom Γ)
-    (norm2derivation : HashMap (NormAtom Γ) (Derivation Γ))
- : Option ({ d : Derivation Γ // d.derivedAtom = atom}) := none
-end Algorithm
+def State.ofConfig (cfg : Config) : State :=
+  { fuel := cfg.fuel,
+    leqs := {},
+    nleqs := {},
+    neqs := {},
+    eqs := {},
+    infs := {},
+    vars := {}
+  }
 
 /-- Core monad that tactic runs in. -/
 abbrev LatticeM := StateRefT State (ReaderT Config TacticM)
+
+namespace LatticeM
+
+def get : LatticeM State := StateRefT'.get
+
+def hasFuel? : LatticeM Bool := do
+  return (← getThe State).fuel > 0
+
+def consumeFuel : LatticeM Unit := do
+  modify fun s => { s with fuel := s.fuel - 1 }
+
+
+/-- returns if equation was new. -/
+def addEquality (lhs : VarExpr) (rhs : VarExpr) : LatticeM Bool := do
+  let refl := EqExpr.mk .none lhs rhs
+  let s ← get
+  if s.eqs.contains refl then
+    return false
+  else
+    modify fun s => { s with eqs := s.eqs.insert refl }
+    -- TODO: substitute all instances of lhs with rhs in all equations.
+    return true
+
+
+/--
+Given the current set of derivations, perform a new derivation.
+Returns 'false' if no progress has been made.
+-/
+def doWork : LatticeM Bool := do
+  if ! (← hasFuel?) then
+    return false
+  let mut changed := false
+  for v in (← get).vars do
+    changed := changed || (← addEquality v v)
+
+  return changed
+
+def falseFromPosNegLit  {P : Prop} (t : P) (f : ¬ P) : False := f t
+
+/-- info: Lattice.Algorithm.LatticeM.falseFromPosNegLit {P : Prop} (t : P) (f : ¬P) : False -/
+#guard_msgs in #check falseFromPosNegLit
+
+
+def tryUnsatFromEqNeq {α : Type} [ToExprM α]
+    (pos : α) (neg : NotExpr α) : LatticeM (Option Expr) := do
+  let epos ← toExprM pos
+  let eneg ← toExprM neg.e
+  if ← isDefEq epos eneg then
+    let proof ← mkAppOptM ``falseFromPosNegLit
+      #[none, epos, eneg]
+    return proof
+  return .none
+
+/-- If succeeds, produce a proof of False -/
+def tryUnsat : LatticeM (Option Expr) := do
+  for neq in (← get).neqs do
+    for eq in (← get).eqs do
+      if let some proof ← tryUnsatFromEqNeq eq neq then
+        return proof
+
+  for nleq in (← get).nleqs do
+    for leq in (← get).leqs do
+      if let some proof ← tryUnsatFromEqNeq leq nleq then
+        return proof
+
+  return .none
+
+end LatticeM
+
+end Algorithm
+
+
 
 end Lattice
