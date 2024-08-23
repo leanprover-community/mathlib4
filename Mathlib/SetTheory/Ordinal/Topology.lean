7542e3c9cb93e6039f65bc2a4a96f8ed5eb5d605
Violeta Hernández <vi.hdz.p@gmail.com>
2024-08-22 20:21:40 -0600

    golf + fix

--- Mathlib/SetTheory/Ordinal/Topology.lean
+++ Mathlib/SetTheory/Ordinal/Topology.lean
@@ -120,11 +120,9 @@ theorem mem_closure_tfae (a : Ordinal.{u}) (s : Set Ordinal) :
 
 theorem mem_closure_iff_sup :
     a ∈ closure s ↔
-      ∃ (ι : Type u) (_ : Nonempty ι) (f : ι → Ordinal), (∀ i, f i ∈ s) ∧ iSup f = a :=
-  calc
-    _ ↔ (∃ (ι : Type u), Nonempty ι ∧ ∃ f, (∀ (i : ι), f i ∈ s) ∧ sup f = a) :=
-             (mem_closure_tfae a s).out 0 5
-    _ ↔ _ := by simp only [exists_prop]
+      ∃ (ι : Type u) (_ : Nonempty ι) (f : ι → Ordinal), (∀ i, f i ∈ s) ∧ iSup f = a := by
+  apply ((mem_closure_tfae a s).out 0 5).trans
+  simp_rw [exists_prop]
 
 theorem mem_closed_iff_sup (hs : IsClosed s) :
     a ∈ s ↔ ∃ (ι : Type u) (_hι : Nonempty ι) (f : ι → Ordinal),
@@ -134,11 +132,9 @@ theorem mem_closed_iff_sup (hs : IsClosed s) :
 theorem mem_closure_iff_bsup :
     a ∈ closure s ↔
       ∃ (o : Ordinal) (_ho : o ≠ 0) (f : ∀ a < o, Ordinal),
-        (∀ i hi, f i hi ∈ s) ∧ bsup.{u, u} o f = a :=
-  calc
-    _ ↔ ∃ o, o ≠ 0 ∧ ∃ f, (∀ (x : Ordinal.{u}) (hx : x < o), f x hx ∈ s) ∧ o.bsup f = a :=
-             (mem_closure_tfae a s).out 0 4
-    _ ↔ _ := by simp only [exists_prop]
+        (∀ i hi, f i hi ∈ s) ∧ bsup.{u, u} o f = a := by
+  apply ((mem_closure_tfae a s).out 0 4).trans
+  simp_rw [exists_prop]
 
 theorem mem_closed_iff_bsup (hs : IsClosed s) :
     a ∈ s ↔
