edb052425d330b933c3dba4adf356e3b7e1b2a1e
Violeta Hernández <vi.hdz.p@gmail.com>
2024-08-22 22:18:04 -0600

    redundant

--- Mathlib/SetTheory/Ordinal/NaturalOps.lean
+++ Mathlib/SetTheory/Ordinal/NaturalOps.lean
@@ -56,8 +56,6 @@ instance NatOrdinal.linearOrder : LinearOrder NatOrdinal := {Ordinal.linearOrder
 instance NatOrdinal.succOrder : SuccOrder NatOrdinal := {Ordinal.succOrder with}
 instance NatOrdinal.orderBot : OrderBot NatOrdinal := {Ordinal.orderBot with}
 instance NatOrdinal.noMaxOrder : NoMaxOrder NatOrdinal := {Ordinal.noMaxOrder with}
-instance NatOrdinal.zeroLEOneClass : ZeroLEOneClass NatOrdinal := {Ordinal.zeroLEOneClass with}
-instance NatOrdinal.NeZero.one : NeZero (1 : NatOrdinal) := {Ordinal.NeZero.one with}
 
 /-- The identity function between `Ordinal` and `NatOrdinal`. -/
 @[match_pattern]
