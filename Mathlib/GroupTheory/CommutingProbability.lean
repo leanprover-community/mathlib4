ca675b4b4b8254c6d1b73f3bcb9d394826031111
Yaël Dillies <yael.dillies@gmail.com>
2024-08-23 09:30:30 +0000

    chore: Don't simplify `Odd n` to `¬ Even n`, but the other way around (#16024)
    This is simply not simpler
    
    From LeanAPAP

--- Mathlib/GroupTheory/CommutingProbability.lean
+++ Mathlib/GroupTheory/CommutingProbability.lean
@@ -165,8 +165,8 @@ lemma reciprocalFactors_odd {n : ℕ} (h1 : n ≠ 1) (h2 : Odd n) :
     reciprocalFactors n = n % 4 * n :: reciprocalFactors (n / 4 + 1) := by
   have h0 : n ≠ 0 := by
     rintro rfl
-    norm_num at h2
-  rw [reciprocalFactors, dif_neg h0, dif_neg h1, if_neg (Nat.odd_iff_not_even.mp h2)]
+    norm_num [← Nat.not_even_iff_odd] at h2
+  rw [reciprocalFactors, dif_neg h0, dif_neg h1, if_neg (Nat.not_even_iff_odd.2 h2)]
 
 /-- A finite product of Dihedral groups. -/
 abbrev Product (l : List ℕ) : Type :=
