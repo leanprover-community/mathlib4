b851847156e6513d661f6c31ca935ba007e3484a
Yaël Dillies <yael.dillies@gmail.com>
2024-08-23 10:19:29 +0000

    chore(Init/Order): Rework files (#15228)
    Merge `Init.Order.Lemmas` and the part of `Init.Algebra.Classes` about relations into `Order.Defs`.

--- Mathlib/Order/Basic.lean
+++ Mathlib/Order/Basic.lean
@@ -3,14 +3,14 @@ Copyright (c) 2014 Jeremy Avigad. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jeremy Avigad, Mario Carneiro
 -/
-import Mathlib.Init.Order.LinearOrder
 import Mathlib.Data.Prod.Basic
 import Mathlib.Data.Subtype
+import Mathlib.Order.Defs
+import Mathlib.Order.Notation
 import Mathlib.Tactic.Spread
 import Mathlib.Tactic.Convert
 import Mathlib.Tactic.SimpRw
 import Mathlib.Tactic.Cases
-import Mathlib.Order.Notation
 import Batteries.Data.Sum.Lemmas
 import Batteries.Tactic.Classical
 
