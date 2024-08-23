bea8c72e27c6258f0e62f9b39f281383b269ce7c
FM22 <artem.khovanov@gmail.com>
2024-08-23 14:03:37 +0100

    revert irrelevant changes from other PR

--- Mathlib/Algebra/Order/Group/Cone.lean
+++ Mathlib/Algebra/Order/Group/Cone.lean
@@ -1,174 +1,85 @@
 /-
 Copyright (c) 2017 Mario Carneiro. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Mario Carneiro, Scott Morrison, Artie Khovanov
+Authors: Mario Carneiro, Scott Morrison
 -/
 import Mathlib.Algebra.Order.Group.Defs
-import Mathlib.Algebra.Order.Monoid.Submonoid
 
 /-!
-# Construct ordered groups from groups with a specified positive cone.
+# Construct ordered groups from positive cones
 
-In this file we provide structures `GroupCone` and `MaximalGroupCone`
-that encode axioms of `OrderedCommGroup` and `LinearOrderedCommGroup`
-in terms of the subset of non-negative elements.
+In this file we provide structures `PositiveCone` and `TotalPositiveCone`
+that encode axioms of `OrderedAddCommGroup` and `LinearOrderedAddCommGroup`
+in terms of the `(0 ≤ ·)` predicate.
 
-We also provide constructors that convert between
-cones in groups and the corresponding ordered groups.
+We also provide two constructors,
+`OrderedAddCommGroup.mkOfPositiveCone` and `LinearOrderedAddCommGroup.mkOfPositiveCone`,
+that turn these structures into instances of the corresponding typeclasses.
 -/
 
-/-- `AddGroupConeClass S G` says that `S` is a type of cones in `G`. -/
-class AddGroupConeClass (S G : Type*) [AddCommGroup G] [SetLike S G] extends
-    AddSubmonoidClass S G : Prop where
-  eq_zero_of_mem_of_neg_mem {C : S} {a : G} : a ∈ C → -a ∈ C → a = 0
+namespace AddCommGroup
 
-/-- `GroupConeClass S G` says that `S` is a type of cones in `G`. -/
-@[to_additive]
-class GroupConeClass (S G : Type*) [CommGroup G] [SetLike S G] extends
-    SubmonoidClass S G : Prop where
-  eq_one_of_mem_of_inv_mem {C : S} {a : G} : a ∈ C → a⁻¹ ∈ C → a = 1
+/-- A collection of elements in an `AddCommGroup` designated as "non-negative".
+This is useful for constructing an `OrderedAddCommGroup`
+by choosing a positive cone in an existing `AddCommGroup`. -/
+-- Porting note(#5171): @[nolint has_nonempty_instance]
+structure PositiveCone (α : Type*) [AddCommGroup α] where
+  /-- The characteristic predicate of a positive cone. `nonneg a` means that `0 ≤ a` according to
+  the cone. -/
+  nonneg : α → Prop
+  /-- The characteristic predicate of a positive cone. `pos a` means that `0 < a` according to
+  the cone. -/
+  pos : α → Prop := fun a => nonneg a ∧ ¬nonneg (-a)
+  pos_iff : ∀ a, pos a ↔ nonneg a ∧ ¬nonneg (-a) := by intros; rfl
+  zero_nonneg : nonneg 0
+  add_nonneg : ∀ {a b}, nonneg a → nonneg b → nonneg (a + b)
+  nonneg_antisymm : ∀ {a}, nonneg a → nonneg (-a) → a = 0
 
-export GroupConeClass (eq_one_of_mem_of_inv_mem)
-export AddGroupConeClass (eq_zero_of_mem_of_neg_mem)
+/-- A positive cone in an `AddCommGroup` induces a linear order if
+for every `a`, either `a` or `-a` is non-negative. -/
+-- Porting note(#5171): @[nolint has_nonempty_instance]
+structure TotalPositiveCone (α : Type*) [AddCommGroup α] extends PositiveCone α where
+  /-- For any `a` the proposition `nonneg a` is decidable -/
+  nonnegDecidable : DecidablePred nonneg
+  /-- Either `a` or `-a` is `nonneg` -/
+  nonneg_total : ∀ a : α, nonneg a ∨ nonneg (-a)
 
-/-- A (positive) cone in an abelian group is a submonoid that
-does not contain both `a` and `-a` for any nonzero `a`.
-This is equivalent to being the set of non-negative elements of
-some order making the group into a partially ordered group. -/
-structure AddGroupCone (G : Type*) [AddCommGroup G] extends AddSubmonoid G where
-  eq_zero_of_mem_of_neg_mem' {a} : a ∈ carrier → -a ∈ carrier → a = 0
+/-- Forget that a `TotalPositiveCone` is total. -/
+add_decl_doc TotalPositiveCone.toPositiveCone
 
-/-- A (positive) cone in an abelian group is a submonoid that
-does not contain both `a` and `a⁻¹` for any non-identity `a`.
-This is equivalent to being the set of elements that are at least 1 in
-some order making the group into a partially ordered group. -/
-@[to_additive]
-structure GroupCone (G : Type*) [CommGroup G] extends Submonoid G where
-  eq_one_of_mem_of_inv_mem' {a} : a ∈ carrier → a⁻¹ ∈ carrier → a = 1
+end AddCommGroup
 
-@[to_additive]
-instance GroupCone.instSetLike (G : Type*) [CommGroup G] : SetLike (GroupCone G) G where
-  coe C := C.carrier
-  coe_injective' p q h := by cases p; cases q; congr; exact SetLike.ext' h
+namespace OrderedAddCommGroup
 
-@[to_additive]
-instance GroupCone.instGroupConeClass (G : Type*) [CommGroup G] :
-    GroupConeClass (GroupCone G) G where
-  mul_mem {C} := C.mul_mem'
-  one_mem {C} := C.one_mem'
-  eq_one_of_mem_of_inv_mem {C} := C.eq_one_of_mem_of_inv_mem'
+open AddCommGroup
 
-/-- `MaximalAddGroupConeClass S G` says that `S` is a type of maximal cones in `G`. -/
-class MaximalAddGroupConeClass (S G : Type*) [AddCommGroup G] [SetLike S G]
-    extends AddGroupConeClass S G : Prop where
-  mem_or_neg_mem (C : S) (a : G) : a ∈ C ∨ -a ∈ C
+/-- Construct an `OrderedAddCommGroup` by
+designating a positive cone in an existing `AddCommGroup`. -/
+def mkOfPositiveCone {α : Type*} [AddCommGroup α] (C : PositiveCone α) : OrderedAddCommGroup α :=
+  { ‹AddCommGroup α› with
+    le := fun a b => C.nonneg (b - a),
+    lt := fun a b => C.pos (b - a),
+    lt_iff_le_not_le := fun a b => by simp [C.pos_iff],
+    le_refl := fun a => by simp [C.zero_nonneg],
+    le_trans := fun a b c nab nbc => by simpa using C.add_nonneg nbc nab,
+    le_antisymm := fun a b nab nba =>
+      eq_of_sub_eq_zero <| C.nonneg_antisymm nba (by rwa [neg_sub]),
+    add_le_add_left := fun a b nab c => by simpa using nab }
 
-/-- `MaximalGroupConeClass S G` says that `S` is a type of maximal cones in `G`. -/
-@[to_additive]
-class MaximalGroupConeClass (S G : Type*) [CommGroup G] [SetLike S G]
-    extends GroupConeClass S G : Prop where
-  mem_or_inv_mem (C : S) (a : G) : a ∈ C ∨ a⁻¹ ∈ C
+end OrderedAddCommGroup
 
-export MaximalGroupConeClass (mem_or_inv_mem)
-export MaximalAddGroupConeClass (mem_or_neg_mem)
+namespace LinearOrderedAddCommGroup
 
-/-- A maximal (positive) cone in an abelian group is a cone containing
-either `a` or `-a` for every `a`.
-This is equivalent to being the set of non-negative elements of
-some order making the group into a linearly ordered group. -/
-structure MaximalAddGroupCone (G : Type*) [AddCommGroup G]
-    extends AddGroupCone G where
-  mem_or_neg_mem' a : a ∈ carrier ∨ -a ∈ carrier
+open AddCommGroup
 
-/-- A maximal (positive) cone in an abelian group is a cone containing
-either `a` or `a⁻¹` for every `a`.
-This is equivalent to being the set of elements that are at least 1 in
-some order making the group into a linearly ordered group. -/
-@[to_additive]
-structure MaximalGroupCone (G : Type*) [CommGroup G] extends GroupCone G where
-  mem_or_inv_mem' a : a ∈ carrier ∨ a⁻¹ ∈ carrier
+/-- Construct a `LinearOrderedAddCommGroup` by
+designating a positive cone in an existing `AddCommGroup`
+such that for every `a`, either `a` or `-a` is non-negative. -/
+def mkOfPositiveCone {α : Type*} [AddCommGroup α] (C : TotalPositiveCone α) :
+    LinearOrderedAddCommGroup α :=
+  { OrderedAddCommGroup.mkOfPositiveCone C.toPositiveCone with
+    -- Porting note: was `C.nonneg_total (b - a)`
+    le_total := fun a b => by simpa [neg_sub] using C.nonneg_total (b - a)
+    decidableLE := fun a b => C.nonnegDecidable _ }
 
-@[to_additive]
-instance MaximalGroupCone.instSetLike (G : Type*) [CommGroup G] :
-  SetLike (MaximalGroupCone G) G where
-  coe C := C.carrier
-  coe_injective' p q h := by cases p; cases q; congr; exact SetLike.ext' h
-
-@[to_additive]
-instance MaximalGroupCone.instGroupConeClass (G : Type*) [CommGroup G] :
-    GroupConeClass (MaximalGroupCone G) G where
-  mul_mem {C} := C.mul_mem'
-  one_mem {C} := C.one_mem'
-  eq_one_of_mem_of_inv_mem {C} := C.eq_one_of_mem_of_inv_mem'
-
-@[to_additive]
-instance MaximalGroupCone.instMaximalGroupConeClass (G : Type*) [CommGroup G] :
-    MaximalGroupConeClass (MaximalGroupCone G) G where
-  mem_or_inv_mem {C} := C.mem_or_inv_mem'
-
-namespace GroupCone
-variable {H : Type*} [OrderedCommGroup H] {a : H}
-
-variable (H) in
-/-- Construct a cone from the set of elements of
-a partially ordered abelian group that are at least 1. -/
-@[to_additive nonneg
-"Construct a cone from the set of non-negative elements of a partially ordered abelian group."]
-def oneLE : GroupCone H where
-  __ := Submonoid.oneLE H
-  eq_one_of_mem_of_inv_mem' {a} := by simpa using ge_antisymm
-
-@[to_additive (attr := simp) nonneg_toAddSubmonoid]
-lemma oneLE_toSubmonoid : (oneLE H).toSubmonoid = .oneLE H := rfl
-@[to_additive (attr := simp) mem_nonneg]
-lemma mem_oneLE : a ∈ oneLE H ↔ 1 ≤ a := Iff.rfl
-@[to_additive (attr := simp, norm_cast) coe_nonneg]
-lemma coe_oneLE : oneLE H = {x : H | 1 ≤ x} := rfl
-
-end GroupCone
-
-namespace MaximalGroupCone
-variable {H : Type*} [LinearOrderedCommGroup H] {a : H}
-
-variable (H) in
-/-- Construct a maximal cone from the set of elements of
-a linearly ordered abelian group that are at least 1. -/
-@[to_additive nonneg
-"Construct a maximal cone from the set of non-negative elements of
-a linearly ordered abelian group."]
-def oneLE : MaximalGroupCone H where
-  __ := GroupCone.oneLE H
-  mem_or_inv_mem' := by simpa using le_total 1
-
-@[to_additive (attr := simp) nonneg_toAddSubmonoid]
-lemma oneLE_toSubmonoid : (oneLE H).toSubmonoid = .oneLE H := rfl
-@[to_additive (attr := simp) mem_nonneg]
-lemma mem_oneLE : a ∈ oneLE H ↔ 1 ≤ a := Iff.rfl
-@[to_additive (attr := simp, norm_cast) coe_nonneg]
-lemma coe_oneLE : oneLE H = {x : H | 1 ≤ x} := rfl
-
-end MaximalGroupCone
-
-variable {S G : Type*} [CommGroup G] [SetLike S G] (C : S)
-
-/-- Construct a partially ordered abelian group by designating a cone in an abelian group. -/
-@[to_additive (attr := reducible)
-"Construct a partially ordered abelian group by designating a cone in an abelian group."]
-def OrderedCommGroup.mkOfCone [GroupConeClass S G] :
-    OrderedCommGroup G where
-  le a b := b / a ∈ C
-  le_refl a := by simp [one_mem]
-  le_trans a b c nab nbc := by simpa using mul_mem nbc nab
-  le_antisymm a b nab nba := by
-    simpa [div_eq_one, eq_comm] using eq_one_of_mem_of_inv_mem nab (by simpa using nba)
-  mul_le_mul_left a b nab c := by simpa using nab
-
-/-- Construct a linearly ordered abelian group by designating a maximal cone in an abelian group. -/
-@[to_additive (attr := reducible)
-"Construct a linearly ordered abelian group by designating a maximal cone in an abelian group."]
-def LinearOrderedCommGroup.mkOfCone
-    [MaximalGroupConeClass S G] (dec : DecidablePred (· ∈ C)) :
-    LinearOrderedCommGroup G where
-  __ := OrderedCommGroup.mkOfCone C
-  le_total a b := by simpa using mem_or_inv_mem C (b / a)
-  decidableLE a b := dec _
+end LinearOrderedAddCommGroup
--- Mathlib/Algebra/Order/Ring/Cone.lean
+++ Mathlib/Algebra/Order/Ring/Cone.lean
@@ -1,152 +1,68 @@
 /-
 Copyright (c) 2016 Jeremy Avigad. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Jeremy Avigad, Leonardo de Moura, Mario Carneiro, Artie Khovanov
+Authors: Jeremy Avigad, Leonardo de Moura, Mario Carneiro
 -/
 import Mathlib.Algebra.Order.Group.Cone
-import Mathlib.Algebra.Order.Ring.Basic
-import Mathlib.Algebra.Ring.Subsemiring.Order
+import Mathlib.Algebra.Order.Ring.Defs
 
 /-!
-# Construct ordered rings from rings with a specified positive cone.
+# Constructing an ordered ring from a ring with a specified positive cone.
 
-In this file we provide structures `RingCone` and `MaximalRingCone`
-that encode axioms of `OrderedRing` and `LinearOrderedRing`
-in terms of the subset of non-negative elements.
-
-We also provide constructors that convert between
-cones in rings and the corresponding ordered rings.
 -/
 
-/-- `RingConeClass S R` says that `S` is a type of cones in `R`. -/
-class RingConeClass (S R : Type*) [Ring R] [SetLike S R]
-    extends AddGroupConeClass S R, SubsemiringClass S R : Prop
 
-/-- A (positive) cone in a ring is a subsemiring that
-does not contain both `a` and `-a` for any nonzero `a`.
-This is equivalent to being the set of non-negative elements of
-some order making the ring into a partially ordered ring. -/
-structure RingCone (R : Type*) [Ring R] extends Subsemiring R where
-  eq_zero_of_mem_of_neg_mem' {a} : a ∈ carrier → -a ∈ carrier → a = 0
+/-! ### Positive cones -/
 
-instance RingCone.instSetLike (R : Type*) [Ring R] : SetLike (RingCone R) R where
-  coe C := C.carrier
-  coe_injective' p q h := by cases p; cases q; congr; exact SetLike.ext' h
 
-instance RingCone.instRingConeClass (R : Type*) [Ring R] :
-    RingConeClass (RingCone R) R where
-  add_mem {C} := C.add_mem'
-  zero_mem {C} := C.zero_mem'
-  mul_mem {C} := C.mul_mem'
-  one_mem {C} := C.one_mem'
-  eq_zero_of_mem_of_neg_mem {C} := C.eq_zero_of_mem_of_neg_mem'
+variable {α : Type*} [Ring α] [Nontrivial α]
 
-/-- `RingConeWithSquaresClass S R` says that `S` is
-a type of cones with squares in `R`. -/
-class RingConeWithSquaresClass (S R : Type*) [Ring R] [SetLike S R]
-    extends RingConeClass S R : Prop where
-  square_mem (C : S) (a : R) : a * a ∈ C
+namespace Ring
 
-export RingConeWithSquaresClass (square_mem)
+/-- A positive cone in a ring consists of a positive cone in underlying `AddCommGroup`,
+which contains `1` and such that the positive elements are closed under multiplication. -/
+structure PositiveCone (α : Type*) [Ring α] extends AddCommGroup.PositiveCone α where
+  /-- In a positive cone, `1` is `nonneg` -/
+  one_nonneg : nonneg 1
+  /-- In a positive cone, if `a` and `b` are `pos` then so is `a * b` -/
+  mul_pos : ∀ a b, pos a → pos b → pos (a * b)
 
-/-- A (positive) cone with squares in a ring is a cone containing all squares. -/
-structure RingConeWithSquares (R : Type*) [Ring R] extends RingCone R where
-  square_mem' a : a * a ∈ carrier
+/-- Forget that a positive cone in a ring respects the multiplicative structure. -/
+add_decl_doc PositiveCone.toPositiveCone
 
-instance PositiveConeWithSquares.instSetLike (R : Type*) [Ring R] :
-    SetLike (RingConeWithSquares R) R where
-  coe C := C.carrier
-  coe_injective' p q h := by cases p; cases q; congr; exact SetLike.ext' h
+/-- A total positive cone in a nontrivial ring induces a linear order. -/
+structure TotalPositiveCone (α : Type*) [Ring α] extends PositiveCone α,
+  AddCommGroup.TotalPositiveCone α
 
-instance RingConeWithSquares.instRingConeWithSquaresClass (R : Type*) [Ring R] :
-    RingConeWithSquaresClass (RingConeWithSquares R) R where
-  add_mem {C} := C.add_mem'
-  zero_mem {C} := C.zero_mem'
-  mul_mem {C} := C.mul_mem'
-  one_mem {C} := C.one_mem'
-  eq_zero_of_mem_of_neg_mem {C} := C.eq_zero_of_mem_of_neg_mem'
-  square_mem {C} := C.square_mem'
+/-- Forget that a `TotalPositiveCone` in a ring is total. -/
+add_decl_doc TotalPositiveCone.toPositiveCone_1
 
-/-- `MaximalRingConeClass S R` says that `S` is a type of maximal cones in `R`. -/
-class MaximalRingConeClass (S R : Type*) [Ring R] [IsDomain R] [SetLike S R]
-    extends MaximalAddGroupConeClass S R, RingConeClass S R : Prop
+/-- Forget that a `TotalPositiveCone` in a ring respects the multiplicative structure. -/
+add_decl_doc TotalPositiveCone.toTotalPositiveCone
 
-/-- A maximal (positive) cone in a domain is a cone containing
-either `a` or `-a` for every `a`.
-This is equivalent to being the set of non-negative elements of
-some order making the domain into a linearly ordered domain. -/
-structure MaximalRingCone (R : Type*) [Ring R] [IsDomain R] extends RingCone R where
-  mem_or_neg_mem' a : a ∈ carrier ∨ -a ∈ carrier
+theorem PositiveCone.one_pos (C : PositiveCone α) : C.pos 1 :=
+  (C.pos_iff _).2 ⟨C.one_nonneg, fun h => one_ne_zero <| C.nonneg_antisymm C.one_nonneg h⟩
 
-instance MaximalRingCone.instSetLike (R : Type*) [Ring R] [IsDomain R] :
-    SetLike (MaximalRingCone R) R where
-  coe C := C.carrier
-  coe_injective' p q h := by cases p; cases q; congr; exact SetLike.ext' h
+end Ring
 
-instance MaximalRingCone.instMaximalRingConeClass (R : Type*) [Ring R] [IsDomain R] :
-    MaximalRingConeClass (MaximalRingCone R) R where
-  add_mem {C} := C.add_mem'
-  zero_mem {C} := C.zero_mem'
-  mul_mem {C} := C.mul_mem'
-  one_mem {C} := C.one_mem'
-  eq_zero_of_mem_of_neg_mem {C} := C.eq_zero_of_mem_of_neg_mem'
-  mem_or_neg_mem {C} := C.mem_or_neg_mem'
+open Ring
 
-instance MaximalRingConeClass.instRingConeWithSquaresClass (S R : Type*)
-    [Ring R] [IsDomain R] [SetLike S R] [MaximalRingConeClass S R] :
-    RingConeWithSquaresClass S R where
-  square_mem C a := Or.elim (mem_or_neg_mem C a)
-                            (fun a_mem => mul_mem a_mem a_mem)
-                            (fun na_mem => by simpa using mul_mem na_mem na_mem)
+/-- Construct a `StrictOrderedRing` by designating a positive cone in an existing `Ring`. -/
+def StrictOrderedRing.mkOfPositiveCone (C : PositiveCone α) : StrictOrderedRing α :=
+  { ‹Ring α›, OrderedAddCommGroup.mkOfPositiveCone C.toPositiveCone with
+    exists_pair_ne := ⟨0, 1, fun h => by simpa [← h, C.pos_iff] using C.one_pos⟩,
+    zero_le_one := by
+      change C.nonneg (1 - 0)
+      convert C.one_nonneg
+      simp,
+    mul_pos := fun x y xp yp => by
+      change C.pos (x * y - 0)
+      -- Porting note: used to be convert, but it relied on unfolding definitions
+      rw [sub_zero]
+      exact C.mul_pos x y (by rwa [← sub_zero x]) (by rwa [← sub_zero y]) }
 
-namespace RingCone
-variable {T : Type*} [OrderedRing T] {a : T}
-
-variable (T) in
-/-- Construct a cone from the set of non-negative elements of a partially ordered ring. -/
-def nonneg : RingCone T where
-  __ := Subsemiring.nonneg T
-  eq_zero_of_mem_of_neg_mem' {a} := by simpa using ge_antisymm
-
-@[simp] lemma nonneg_toSubsemiring : (nonneg T).toSubsemiring = .nonneg T := rfl
-@[simp] lemma mem_nonneg : a ∈ nonneg T ↔ 0 ≤ a := Iff.rfl
-@[simp, norm_cast] lemma coe_nonneg : nonneg T = {x : T | 0 ≤ x} := rfl
-
-end RingCone
-
-namespace MaximalRingCone
-variable {T : Type*} [LinearOrderedRing T] {a : T}
-
-variable (T) in
-/-- Construct a maximal cone from the set of non-negative elements of a linearly ordered ring. -/
-def nonneg : MaximalRingCone T where
-  __ := RingCone.nonneg T
-  mem_or_neg_mem' := by simpa using le_total 0
-
-@[simp] lemma nonneg_toSubsemiring : (nonneg T).toSubsemiring = .nonneg T := rfl
-@[simp] lemma mem_nonneg : a ∈ nonneg T ↔ 0 ≤ a := Iff.rfl
-@[simp, norm_cast] lemma coe_nonneg : nonneg T = {x : T | 0 ≤ x} := rfl
-
-end MaximalRingCone
-
-variable {S R : Type*} [Ring R] [SetLike S R] (C : S)
-
-/-- A cone over a nontrivial ring does not conain -1. -/
-theorem RingConeClass.neg_one_not_mem [Nontrivial R] [RingConeClass S R] : -1 ∉ C :=
-  fun neg_one_mem => zero_ne_one' R (eq_zero_of_mem_of_neg_mem (one_mem C) neg_one_mem).symm
-
-/-- Construct a partially ordered ring by designating a positive cone in a ring. -/
-@[reducible] def OrderedRing.mkOfCone [RingConeClass S R] : OrderedRing R where
-  __ := ‹Ring R›
-  __ := OrderedAddCommGroup.mkOfCone C
-  zero_le_one := show _ ∈ C by simpa using one_mem C
-  mul_nonneg x y xnn ynn := show _ ∈ C by simpa using mul_mem xnn ynn
-
-/-- Construct a linearly ordered domain by designating a positive cone in a domain. -/
-@[reducible] def LinearOrderedRing.mkOfCone
-    [IsDomain R] [MaximalRingConeClass S R] (dec : DecidablePred (· ∈ C)) :
-    LinearOrderedRing R where
-  __ := OrderedRing.mkOfCone C
-  __ := OrderedRing.toStrictOrderedRing R
-  le_total a b := by simpa using mem_or_neg_mem C (b - a)
-  decidableLE a b := dec _
+/-- Construct a `LinearOrderedRing` by
+designating a positive cone in an existing `Ring`. -/
+def LinearOrderedRing.mkOfPositiveCone (C : TotalPositiveCone α) : LinearOrderedRing α :=
+  { LinearOrderedAddCommGroup.mkOfPositiveCone C.toTotalPositiveCone,
+    StrictOrderedRing.mkOfPositiveCone C.toPositiveCone_1 with }
