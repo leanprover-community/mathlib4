dbac9ad31bffee00ffe67b9cca0a6f01d90894b7
Yury G. Kudryashov <urkud@urkud.name>
2024-08-23 08:47:15 -0500

    Apply suggestions from code review

--- Mathlib/Order/Cover.lean
+++ Mathlib/Order/Cover.lean
@@ -528,11 +528,11 @@ namespace WithTop
 
 variable [Preorder α] {a b : α}
 
-@[simp] lemma coe_wcovBy_coe : (a : WithTop α) ⩿ b ↔ a ⩿ b :=
+@[simp, norm_cast] lemma coe_wcovBy_coe : (a : WithTop α) ⩿ b ↔ a ⩿ b :=
   Set.OrdConnected.apply_wcovBy_apply_iff OrderEmbedding.withTopCoe <| by
     simp [WithTop.range_coe, ordConnected_Iio]
 
-@[simp] lemma coe_covBy_coe : (a : WithTop α) ⋖ b ↔ a ⋖ b :=
+@[simp, norm_cast] lemma coe_covBy_coe : (a : WithTop α) ⋖ b ↔ a ⋖ b :=
   Set.OrdConnected.apply_covBy_apply_iff OrderEmbedding.withTopCoe <| by
     simp [WithTop.range_coe, ordConnected_Iio]
 
@@ -549,11 +549,11 @@ namespace WithBot
 
 variable [Preorder α] {a b : α}
 
-@[simp] lemma coe_wcovBy_coe : (a : WithBot α) ⩿ b ↔ a ⩿ b :=
+@[simp, norm_cast] lemma coe_wcovBy_coe : (a : WithBot α) ⩿ b ↔ a ⩿ b :=
   Set.OrdConnected.apply_wcovBy_apply_iff OrderEmbedding.withBotCoe <| by
     simp [WithBot.range_coe, ordConnected_Ioi]
 
-@[simp] lemma coe_covBy_coe : (a : WithBot α) ⋖ b ↔ a ⋖ b :=
+@[simp, norm_cast] lemma coe_covBy_coe : (a : WithBot α) ⋖ b ↔ a ⋖ b :=
   Set.OrdConnected.apply_covBy_apply_iff OrderEmbedding.withBotCoe <| by
     simp [WithBot.range_coe, ordConnected_Ioi]
 
