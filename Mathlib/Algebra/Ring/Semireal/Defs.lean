3796e0c7de99a7299a9242dceea2a58e6843105a
Artie Khovanov <artem.khovanov@gmail.com>
2024-08-24 19:46:17 +0100

    Update Mathlib/Algebra/Ring/Semireal/Defs.lean
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- Mathlib/Algebra/Ring/Semireal/Defs.lean
+++ Mathlib/Algebra/Ring/Semireal/Defs.lean
@@ -3,8 +3,8 @@ Copyright (c) 2024 Florent Schaffhauser. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Florent Schaffhauser, Artie Khovanov
 -/
-import Mathlib.Algebra.Ring.SumsOfSquares
 import Mathlib.Algebra.Order.Ring.Defs
+import Mathlib.Algebra.Ring.SumsOfSquares
 
 /-!
 # Semireal rings
