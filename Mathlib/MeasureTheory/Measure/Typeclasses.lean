452c481413139dec2d2b31b0a057032fb097aedd
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2024-08-24 10:27:22 +0200

    start

--- Mathlib/MeasureTheory/Measure/Typeclasses.lean
+++ Mathlib/MeasureTheory/Measure/Typeclasses.lean
@@ -201,6 +201,32 @@ theorem abs_toReal_measure_sub_le_measure_symmDiff [IsFiniteMeasure μ]
 
 end IsFiniteMeasure
 
+section IsZeroOrProbabilityMeasure
+
+/-- A measure `μ` is called a probability measure if `μ univ = 1`. -/
+class IsZeroOrProbabilityMeasure (μ : Measure α) : Prop where
+  measure_univ : μ univ = 1 ∨ μ univ = 0
+
+lemma isZeroOrProbabilityMeasure_iff : IsZeroOrProbabilityMeasure μ ↔ μ univ = 1 ∨ μ univ = 0 :=
+  ⟨fun _ ↦ IsZeroOrProbabilityMeasure.measure_univ, IsZeroOrProbabilityMeasure.mk⟩
+
+lemma prob_le_one {μ : Measure α} [IsZeroOrProbabilityMeasure μ] {s : Set α} : μ s ≤ 1 := by
+  apply (measure_mono (subset_univ _)).trans
+  rcases IsZeroOrProbabilityMeasure.measure_univ (μ := μ) with h | h <;> simp [h]
+
+@[simp]
+theorem one_le_prob_iff {μ : Measure α} [IsZeroOrProbabilityMeasure μ] : 1 ≤ μ s ↔ μ s = 1 :=
+  ⟨fun h => le_antisymm prob_le_one h, fun h => h ▸ le_refl _⟩
+
+instance (priority := 100) IsZeroOrProbabilityMeasure.toIsFiniteMeasure (μ : Measure α)
+    [IsZeroOrProbabilityMeasure μ] : IsFiniteMeasure μ :=
+  ⟨prob_le_one.trans_lt one_lt_top⟩
+
+instance : IsZeroOrProbabilityMeasure (0 : Measure α) :=
+  ⟨Or.inr rfl⟩
+
+end IsZeroOrProbabilityMeasure
+
 section IsProbabilityMeasure
 
 /-- A measure `μ` is called a probability measure if `μ univ = 1`. -/
@@ -214,9 +240,9 @@ attribute [simp] IsProbabilityMeasure.measure_univ
 lemma isProbabilityMeasure_iff : IsProbabilityMeasure μ ↔ μ univ = 1 :=
   ⟨fun _ ↦ measure_univ, IsProbabilityMeasure.mk⟩
 
-instance (priority := 100) IsProbabilityMeasure.toIsFiniteMeasure (μ : Measure α)
-    [IsProbabilityMeasure μ] : IsFiniteMeasure μ :=
-  ⟨by simp only [measure_univ, ENNReal.one_lt_top]⟩
+instance (priority := 100) (μ : Measure α) [IsProbabilityMeasure μ] :
+    IsZeroOrProbabilityMeasure μ :=
+  ⟨Or.inl measure_univ⟩
 
 theorem IsProbabilityMeasure.ne_zero (μ : Measure α) [IsProbabilityMeasure μ] : μ ≠ 0 :=
   mt measure_univ_eq_zero.2 <| by simp [measure_univ]
@@ -230,9 +256,6 @@ theorem IsProbabilityMeasure.ae_neBot [IsProbabilityMeasure μ] : NeBot (ae μ) 
 theorem prob_add_prob_compl [IsProbabilityMeasure μ] (h : MeasurableSet s) : μ s + μ sᶜ = 1 :=
   (measure_add_measure_compl h).trans measure_univ
 
-theorem prob_le_one [IsProbabilityMeasure μ] : μ s ≤ 1 :=
-  (measure_mono <| Set.subset_univ _).trans_eq measure_univ
-
 -- Porting note: made an `instance`, using `NeZero`
 instance isProbabilityMeasureSMul [IsFiniteMeasure μ] [NeZero μ] :
     IsProbabilityMeasure ((μ univ)⁻¹ • μ) :=
@@ -243,10 +266,6 @@ variable [IsProbabilityMeasure μ] {p : α → Prop} {f : β → α}
 theorem isProbabilityMeasure_map {f : α → β} (hf : AEMeasurable f μ) :
     IsProbabilityMeasure (map f μ) :=
   ⟨by simp [map_apply_of_aemeasurable, hf]⟩
-
-@[simp]
-theorem one_le_prob_iff : 1 ≤ μ s ↔ μ s = 1 :=
-  ⟨fun h => le_antisymm prob_le_one h, fun h => h ▸ le_refl _⟩
 
 /-- Note that this is not quite as useful as it looks because the measure takes values in `ℝ≥0∞`.
 Thus the subtraction appearing is the truncated subtraction of `ℝ≥0∞`, rather than the
@@ -259,17 +278,6 @@ Thus the subtraction appearing is the truncated subtraction of `ℝ≥0∞`, rat
 better-behaved subtraction of `ℝ`. -/
 theorem prob_compl_eq_one_sub (hs : MeasurableSet s) : μ sᶜ = 1 - μ s :=
   prob_compl_eq_one_sub₀ hs.nullMeasurableSet
-
-lemma prob_compl_lt_one_sub_of_lt_prob {p : ℝ≥0∞} (hμs : p < μ s) (s_mble : MeasurableSet s) :
-    μ sᶜ < 1 - p := by
-  rw [prob_compl_eq_one_sub s_mble]
-  apply ENNReal.sub_lt_of_sub_lt prob_le_one (Or.inl one_ne_top)
-  convert hμs
-  exact ENNReal.sub_sub_cancel one_ne_top (lt_of_lt_of_le hμs prob_le_one).le
-
-lemma prob_compl_le_one_sub_of_le_prob {p : ℝ≥0∞} (hμs : p ≤ μ s) (s_mble : MeasurableSet s) :
-    μ sᶜ ≤ 1 - p := by
-  simpa [prob_compl_eq_one_sub s_mble] using tsub_le_tsub_left hμs 1
 
 @[simp] lemma prob_compl_eq_zero_iff₀ (hs : NullMeasurableSet s μ) : μ sᶜ = 0 ↔ μ s = 1 := by
   rw [prob_compl_eq_one_sub₀ hs, tsub_eq_zero_iff_le, one_le_prob_iff]
@@ -299,6 +307,68 @@ lemma isProbabilityMeasure_comap (hf : Injective f) (hf' : ∀ᵐ a ∂μ, a ∈
     rw [comap_apply _ hf hf'' _ MeasurableSet.univ,
       ← mem_ae_iff_prob_eq_one (hf'' _ MeasurableSet.univ)]
     simpa
+
+protected lemma _root_.MeasurableEmbedding.isProbabilityMeasure_comap (hf : MeasurableEmbedding f)
+    (hf' : ∀ᵐ a ∂μ, a ∈ range f) : IsProbabilityMeasure (μ.comap f) :=
+  isProbabilityMeasure_comap hf.injective hf' hf.measurableSet_image'
+
+instance isProbabilityMeasure_map_up :
+    IsProbabilityMeasure (μ.map ULift.up) := isProbabilityMeasure_map measurable_up.aemeasurable
+
+instance isProbabilityMeasure_comap_down : IsProbabilityMeasure (μ.comap ULift.down) :=
+  MeasurableEquiv.ulift.measurableEmbedding.isProbabilityMeasure_comap <| ae_of_all _ <| by
+    simp [Function.Surjective.range_eq <| EquivLike.surjective _]
+
+end IsProbabilityMeasure
+
+section IsZeroOrProbabilityMeasure
+
+instance isZeroOrProbabilityMeasureSMul :
+    IsZeroOrProbabilityMeasure ((μ univ)⁻¹ • μ) := by
+  rcases eq_zero_or_neZero μ with rfl | h
+  · simp; infer_instance
+  rcases eq_top_or_lt_top (μ univ) with h | h
+  · simp [h]; infer_instance
+  have : IsFiniteMeasure μ := ⟨h⟩
+  infer_instance
+
+variable [IsZeroOrProbabilityMeasure μ] {p : α → Prop} {f : β → α}
+
+variable (μ) in
+lemma eq_zero_or_isProbabilityMeasure : μ = 0 ∨ IsProbabilityMeasure μ := by
+  rcases IsZeroOrProbabilityMeasure.measure_univ (μ := μ) with h | h
+  · exact Or.inr ⟨h⟩
+  · apply Or.inl (measure_univ_eq_zero.mp h)
+
+instance {f : α → β} : IsZeroOrProbabilityMeasure (map f μ) := by
+  by_cases hf : AEMeasurable f μ
+  · simpa [isZeroOrProbabilityMeasure_iff, hf] using IsZeroOrProbabilityMeasure.measure_univ
+  · simp [isZeroOrProbabilityMeasure_iff, hf]
+
+
+lemma prob_compl_lt_one_sub_of_lt_prob {p : ℝ≥0∞} (hμs : p < μ s) (s_mble : MeasurableSet s) :
+    μ sᶜ < 1 - p := by
+  rcases eq_zero_or_isProbabilityMeasure μ with rfl | h
+  · simp at hμs
+  · rw [prob_compl_eq_one_sub s_mble]
+    apply ENNReal.sub_lt_of_sub_lt prob_le_one (Or.inl one_ne_top)
+    convert hμs
+    exact ENNReal.sub_sub_cancel one_ne_top (lt_of_lt_of_le hμs prob_le_one).le
+
+lemma prob_compl_le_one_sub_of_le_prob {p : ℝ≥0∞} (hμs : p ≤ μ s) (s_mble : MeasurableSet s) :
+    μ sᶜ ≤ 1 - p := by
+  rcases eq_zero_or_isProbabilityMeasure μ with rfl | h
+  · simp
+  · simpa [prob_compl_eq_one_sub s_mble] using tsub_le_tsub_left hμs 1
+
+
+instance : IsZeroOrProbabilityMeasure (μ.comap f) := by
+  by_cases hf : Injective f ∧ ∀ s, MeasurableSet s → NullMeasurableSet (f '' s) μ
+  · rcases eq_zero_or_isProbabilityMeasure μ with rfl | h
+    · simp
+
+
+#exit
 
 protected lemma _root_.MeasurableEmbedding.isProbabilityMeasure_comap (hf : MeasurableEmbedding f)
     (hf' : ∀ᵐ a ∂μ, a ∈ range f) : IsProbabilityMeasure (μ.comap f) :=
