29333c2473e96c8b1fbed6ab2579d48b4d7e7285
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2024-08-24 19:09:57 +0200

    better

--- Mathlib/Data/ENNReal/Inv.lean
+++ Mathlib/Data/ENNReal/Inv.lean
@@ -97,10 +97,14 @@ protected theorem div_mul_cancel (h0 : a ≠ 0) (hI : a ≠ ∞) : b / a * a = b
 protected theorem mul_div_cancel' (h0 : a ≠ 0) (hI : a ≠ ∞) : a * (b / a) = b := by
   rw [mul_comm, ENNReal.div_mul_cancel h0 hI]
 
-protected lemma eq_one_of_mul_eq
-    {a b : ℝ≥0∞} (h : a * b = a) (ha : a ≠ 0) (h'a : a ≠ ⊤) : b = 1 := by
+protected theorem mul_eq_left (ha : a ≠ 0) (h'a : a ≠ ∞) : a * b = a ↔ b = 1 := by
+  refine ⟨fun h ↦ ?_, fun h ↦ by rw [h, mul_one]⟩
   have : a * b * a⁻¹ = a * a⁻¹ := by rw [h]
   rwa [mul_assoc, mul_comm b, ← mul_assoc, ENNReal.mul_inv_cancel ha h'a, one_mul] at this
+
+protected theorem mul_eq_right (ha : a ≠ 0) (h'a : a ≠ ∞) : b * a = a ↔ b = 1 := by
+  rw [mul_comm]
+  exact ENNReal.mul_eq_left ha h'a
 
 -- Porting note: `simp only [div_eq_mul_inv, mul_comm, mul_assoc]` doesn't work in the following two
 protected theorem mul_comm_div : a / b * c = a * (c / b) := by
