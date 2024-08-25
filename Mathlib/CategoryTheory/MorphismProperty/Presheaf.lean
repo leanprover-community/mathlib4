fbda581b46a4a585a8def0c526b6fb8158545c4b
Calle Sönne <calle.sonne@gmail.com>
2024-08-25 12:10:33 +0100

    mk_all + imports

--- Mathlib/CategoryTheory/MorphismProperty/Presheaf.lean
+++ Mathlib/CategoryTheory/MorphismProperty/Presheaf.lean
@@ -4,8 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Calle Sönne, Joël Riou, Ravi Vakil
 -/
 
-import Mathlib.CategoryTheory.Limits.Yoneda
-import Mathlib.CategoryTheory.Limits.Preserves.Shapes.Pullbacks
 import Mathlib.CategoryTheory.MorphismProperty.Limits
 
 /-!
