18ab3825e739b07baa3ddfcc400c78e5fcdf3f98
Kim Morrison <kim@tqft.net>
2024-08-26 09:43:07 +1000

    fix

--- Mathlib/Algebra/Lie/Weights/Basic.lean
+++ Mathlib/Algebra/Lie/Weights/Basic.lean
@@ -584,7 +584,7 @@ private lemma isCompl_genWeightSpace_zero_posFittingComp_aux
   · suffices IsNilpotent R L M by simp [M₀, M₁, isCompl_top_bot]
     replace h : M₀ = ⊤ := by simpa [M₀, genWeightSpace]
     rw [← LieModule.isNilpotent_of_top_iff', ← h]
-    infer_instance
+    exact LieModule.instIsNilpotentSubtypeMemSubmoduleGenWeightSpaceOfNatForallOfIsNoetherian M
   · set M₀ₓ := genWeightSpaceOf M (0 : R) x
     set M₁ₓ := posFittingCompOf R M x
     set M₀ₓ₀ := genWeightSpace M₀ₓ (0 : L → R)
