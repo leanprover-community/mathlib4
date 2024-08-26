8bac4494b274a17462ac7288f16ead880a4d4cab
Aaron Anderson <awainverse@gmail.com>
2024-08-26 13:45:56 +0000

    feat(ModelTheory): Substructure-closure and finite generation in relational languages (#16010)
    Shows that in relational languages, all sets are closed as substructures.
    As a consequence, finiteness is the same as finite generation.

--- Mathlib/ModelTheory/FinitelyGenerated.lean
+++ Mathlib/ModelTheory/FinitelyGenerated.lean
@@ -92,6 +92,18 @@ theorem FG.of_map_embedding {N : Type*} [L.Structure N] (f : M ↪[L] N) {s : L.
   rw [h] at h'
   exact Hom.map_le_range h'
 
+theorem FG.of_finite {s : L.Substructure M} [h : Finite s] : s.FG :=
+  ⟨Set.Finite.toFinset h, by simp only [Finite.coe_toFinset, closure_eq]⟩
+
+theorem FG.finite [L.IsRelational] {S : L.Substructure M} (h : S.FG) : Finite S := by
+  obtain ⟨s, rfl⟩ := h
+  have hs := s.finite_toSet
+  rw [← ((closure L).mem_closed_iff _).1 (mem_closed_of_isRelational L (↑s : Set M))] at hs
+  exact hs
+
+theorem fg_iff_finite [L.IsRelational] {S : L.Substructure M} : S.FG ↔ Finite S :=
+  ⟨FG.finite, fun _ => FG.of_finite⟩
+
 /-- A substructure of `M` is countably generated if it is the closure of a countable subset of `M`.
 -/
 def CG (N : L.Substructure M) : Prop :=
@@ -172,7 +184,6 @@ open Substructure
 
 namespace Structure
 
-
 variable (L) (M)
 
 /-- A structure is finitely generated if it is the closure of a finite subset. -/
@@ -226,6 +237,15 @@ theorem FG.countable_embedding (N : Type*) [L.Structure N] [Countable N] (_ : FG
 instance Fg.instCountable_embedding (N : Type*) [L.Structure N]
     [Countable N] [h : FG L M] : Countable (M ↪[L] N) :=
   FG.countable_embedding N h
+
+theorem FG.of_finite [Finite M] : FG L M := by
+  simp only [fg_def, Substructure.FG.of_finite, topEquiv.toEquiv.finite_iff]
+
+theorem FG.finite [L.IsRelational] (h : FG L M) : Finite M :=
+  Finite.of_finite_univ (Substructure.FG.finite (fg_def.1 h))
+
+theorem fg_iff_finite [L.IsRelational] : FG L M ↔ Finite M :=
+  ⟨FG.finite, fun _ => FG.of_finite⟩
 
 theorem cg_def : CG L M ↔ (⊤ : L.Substructure M).CG :=
   ⟨fun h => h.1, fun h => ⟨h⟩⟩
