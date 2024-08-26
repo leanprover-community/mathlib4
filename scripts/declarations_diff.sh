dd8a8160bccf83ea891354a3b855897c2ff822ae
adomani <adomani@gmail.com>
2024-08-26 17:35:43 +0200

    add abbrevs

--- scripts/declarations_diff.sh
+++ scripts/declarations_diff.sh
@@ -42,7 +42,7 @@ The script uses some heuristics to guide this process.
 BASH_DOC_MODULE
 
 ## we narrow the diff to lines beginning with `theorem`, `lemma` and a few other commands
-begs="(theorem|lemma|inductive|structure|def|class|instance|alias)"
+begs="(theorem|lemma|inductive|structure|def|class|instance|alias|abbrev)"
 
 if [ "${1}" == "long" ]
 then
