70e05eb4401028324991730f3634bea8efc3bb9e
María Inés de Frutos-Fernández <mariaines.dff@gmail.com>
2024-08-26 11:39:00 +0000

    feat(RingTheory.Valuation.Minpoly): add valuation lemmas (#15736)
    We prove some results about valuations of zero coefficients of minimal polynomials.
    
    Co-authored-by: Filippo A. E. Nuccio <filippo.nuccio@univ-st-etienne.fr>
    
    
    
    Co-authored-by: faenuccio <filippo.nuccio@univ-st-etienne.fr>

--- Mathlib/RingTheory/Valuation/Basic.lean
+++ Mathlib/RingTheory/Valuation/Basic.lean
@@ -211,6 +211,13 @@ theorem ne_zero_iff [Nontrivial Γ₀] (v : Valuation K Γ₀) {x : K} : v x ≠
 theorem unit_map_eq (u : Rˣ) : (Units.map (v : R →* Γ₀) u : Γ₀) = v u :=
   rfl
 
+theorem ne_zero_of_unit [Nontrivial Γ₀] (v : Valuation K Γ₀) (x : Kˣ) : v x ≠ (0 : Γ₀) := by
+  simp only [ne_eq, Valuation.zero_iff, Units.ne_zero x, not_false_iff]
+
+theorem ne_zero_of_isUnit [Nontrivial Γ₀] (v : Valuation K Γ₀) (x : K) (hx : IsUnit x) :
+    v x ≠ (0 : Γ₀) := by
+  simpa [hx.choose_spec] using ne_zero_of_unit v hx.choose
+
 /-- A ring homomorphism `S → R` induces a map `Valuation R Γ₀ → Valuation S Γ₀`. -/
 def comap {S : Type*} [Ring S] (f : S →+* R) (v : Valuation R Γ₀) : Valuation S Γ₀ :=
   { v.toMonoidWithZeroHom.comp f.toMonoidWithZeroHom with
