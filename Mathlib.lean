cfcaef67e46e87f0421cd861f5385e3ec8179df2
María Inés de Frutos-Fernández <mariaines.dff@gmail.com>
2024-08-26 11:39:00 +0000

    feat(RingTheory.Valuation.Minpoly): add valuation lemmas (#15736)
    We prove some results about valuations of zero coefficients of minimal polynomials.
    
    Co-authored-by: Filippo A. E. Nuccio <filippo.nuccio@univ-st-etienne.fr>
    
    
    
    Co-authored-by: faenuccio <filippo.nuccio@univ-st-etienne.fr>

--- Mathlib.lean
+++ Mathlib.lean
@@ -3979,6 +3979,7 @@ import Mathlib.RingTheory.Valuation.Basic
 import Mathlib.RingTheory.Valuation.ExtendToLocalization
 import Mathlib.RingTheory.Valuation.Integers
 import Mathlib.RingTheory.Valuation.Integral
+import Mathlib.RingTheory.Valuation.Minpoly
 import Mathlib.RingTheory.Valuation.PrimeMultiplicity
 import Mathlib.RingTheory.Valuation.Quotient
 import Mathlib.RingTheory.Valuation.RamificationGroup
