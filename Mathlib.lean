b7cc362d83df3e77bc3e32bf2c5c73d6f10e27c8
Yury G. Kudryashov <urkud@urkud.name>
2024-08-25 21:06:02 -0500

    Update

--- Mathlib.lean
+++ Mathlib.lean
@@ -2579,6 +2579,7 @@ import Mathlib.Dynamics.Ergodic.Action.Basic
 import Mathlib.Dynamics.Ergodic.Action.Regular
 import Mathlib.Dynamics.Ergodic.AddCircle
 import Mathlib.Dynamics.Ergodic.Conservative
+import Mathlib.Dynamics.Ergodic.Dissipative
 import Mathlib.Dynamics.Ergodic.Ergodic
 import Mathlib.Dynamics.Ergodic.Function
 import Mathlib.Dynamics.Ergodic.MeasurePreserving
