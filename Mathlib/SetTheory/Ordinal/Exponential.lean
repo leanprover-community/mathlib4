c6e68d5798cf8c3ce386170de8fa8bd6b9644279
Violeta Hernández <vi.hdz.p@gmail.com>
2024-08-26 04:43:44 -0600

    Update Mathlib/SetTheory/Ordinal/Exponential.lean
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- Mathlib/SetTheory/Ordinal/Exponential.lean
+++ Mathlib/SetTheory/Ordinal/Exponential.lean
@@ -397,7 +397,7 @@ theorem natCast_opow (m : ℕ) : ∀ n : ℕ, ↑(m ^ n : ℕ) = (m : Ordinal) ^
   | n + 1 => by
     rw [pow_succ, natCast_mul, natCast_opow m n, Nat.cast_succ, add_one_eq_succ, opow_succ]
 
-theorem sup_opow_nat {o : Ordinal} (ho : 0 < o) : (⨆ n : ℕ, o ^ (n : Ordinal)) = o ^ ω := by
+theorem sup_opow_nat {o : Ordinal} (ho : 0 < o) : ⨆ n : ℕ, o ^ (n : Ordinal) = o ^ ω := by
   rcases lt_or_eq_of_le (one_le_iff_pos.2 ho) with (ho₁ | rfl)
   · exact (opow_isNormal ho₁).apply_omega
   · rw [one_opow]
