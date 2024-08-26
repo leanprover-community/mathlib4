b126da46f12f98305620b07984dac4473444654e
faenuccio <filippo.nuccio@univ-st-etienne.fr>
2024-08-26 13:28:01 +0200

    generalized for arbitrary subrings

--- Mathlib/RingTheory/Valuation/IntPolynomial.lean
+++ Mathlib/RingTheory/Valuation/IntPolynomial.lean
@@ -7,33 +7,28 @@ import Mathlib.RingTheory.Valuation.ValuationSubring
 import Mathlib.Algebra.Order.Group.TypeTags
 
 /-!
-# Polynomials over the valuation subring.
+# Polynomials over subrings.
 
-Given a field `K` with a valuation `v`, in this file we construct a map from polynomials in `K[X]`
-with integer coefficients to `v.valuationSubring[X]`. We provide several lemmas to deal with
+Given a field `K` with a subring `R`, in this file we construct a map from polynomials in `K[X]`
+with coefficients in `R` to `R[X]`. We provide several lemmas to deal with
 coefficients, degree, and evaluation of `intPolynomial`.
 This is useful when dealing with integral elements in an extension of fields.
 
 # Main Definitions
 * `Valuation.intPolynomial` : given a polynomial in `K[X]` with coefficients in a field `K` with a
-  valuation `v` such that all coefficients belong to `v.valuationSubring`, `intPolynomial` is the
-  corresponding polynomial in `v.valuationSubring[X]`.
+  subring `R` such that all coefficients belong to `R`, `intPolynomial R ` is the
+  corresponding polynomial in `R[X]`.
 -/
 
-
-open scoped DiscreteValuation
-
-variable {K : Type*} [Field K] (v : Valuation K ℤₘ₀)
-
-namespace Valuation
+variable {K : Type*} [Field K] (R : Subring K)
 
 open Polynomial
 
 open scoped Polynomial
 
-/-- Given a polynomial in `K[X]` such that all coefficients belong to `v.valuationSubring`,
-  `intPolynomial` is the corresponding polynomial in `v.valuationSubring[X]`. -/
-def intPolynomial {P : K[X]} (hP : ∀ n : ℕ, P.coeff n ∈ v.valuationSubring) : v.valuationSubring[X]
+/-- Given a polynomial in `K[X]` such that all coefficients belong to the subring `R`,
+  `intPolynomial` is the corresponding polynomial in `R[X]`. -/
+def intPolynomial {P : K[X]} (hP : ∀ n : ℕ, P.coeff n ∈ R) : R[X]
     where toFinsupp :=
   { support := P.support
     toFun := fun n => ⟨P.coeff n, hP n⟩
@@ -42,24 +37,22 @@ def intPolynomial {P : K[X]} (hP : ∀ n : ℕ, P.coeff n ∈ v.valuationSubring
 
 namespace intPolynomial
 
-variable {P : K[X]} (hP : ∀ n : ℕ, P.coeff n ∈ v.valuationSubring)
+variable {P : K[X]} (hP : ∀ n : ℕ, P.coeff n ∈ R)
 
-theorem coeff_eq  (n : ℕ) : ↑((intPolynomial v hP).coeff n) = P.coeff n := rfl
+theorem coeff_eq  (n : ℕ) : ↑((intPolynomial R hP).coeff n) = P.coeff n := rfl
 
-theorem leadingCoeff_eq : ↑(intPolynomial v hP).leadingCoeff = P.leadingCoeff := rfl
+theorem leadingCoeff_eq : ↑(intPolynomial R hP).leadingCoeff = P.leadingCoeff := rfl
 
-theorem monic_iff : (intPolynomial v hP).Monic ↔ P.Monic := by
+theorem monic_iff : (intPolynomial R hP).Monic ↔ P.Monic := by
   rw [Monic, Monic, ← leadingCoeff_eq, OneMemClass.coe_eq_one]
 
-theorem natDegree : (intPolynomial v hP).natDegree = P.natDegree := rfl
+theorem natDegree : (intPolynomial R hP).natDegree = P.natDegree := rfl
 
 variable {L : Type*} [Field L] [Algebra K L]
 
 theorem eval₂_eq (x : L) :
-    eval₂ (algebraMap (↥v.valuationSubring) L) x (intPolynomial v hP) = aeval x P := by
+    eval₂ (algebraMap R L) x (intPolynomial R hP) = aeval x P := by
   rw [aeval_eq_sum_range, eval₂_eq_sum_range]
   exact Finset.sum_congr rfl (fun n _ => by rw [Algebra.smul_def]; rfl)
 
 end intPolynomial
-
-end Valuation
