9ce2e31b74a14119fb826fcb986535e4148fd604
adomani <adomani@gmail.com>
2024-08-27 15:21:28 +0200

    test tag must be present

--- test/StacksAttribute.lean
+++ test/StacksAttribute.lean
@@ -10,3 +10,12 @@ warning: Tag 'loA1' should only consist of digits and uppercase letters
 #guard_msgs in
 @[stacks 04 Q "", stacks A04Q "A comment", stacks 044QQ , stacks loA1]
 example : True := .intro
+
+/--
+warning: Please, enter a Tag after `stacks`.
+---
+warning: Please, enter a Tag after `stacks`.
+-/
+#guard_msgs in
+@[stacks "", stacks]
+example : True := .intro
