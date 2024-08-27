302bc2c70627525f33f68bc38f55f4fd433c309f
Yaël Dillies <yael.dillies@gmail.com>
2024-08-27 16:57:57 +0000

    feat: Actions on and by an order synonym (#15813)
    Complete the API about actions on and by `OrderDual`/`Lex`.

--- Mathlib/Algebra/Order/Module/Defs.lean
+++ Mathlib/Algebra/Order/Module/Defs.lean
@@ -3,9 +3,9 @@ Copyright (c) 2023 Yaël Dillies. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yaël Dillies
 -/
+import Mathlib.Algebra.Module.Defs
 import Mathlib.Algebra.Order.Field.Defs
-import Mathlib.Algebra.Order.GroupWithZero.Unbundled
-import Mathlib.Algebra.Order.Module.Synonym
+import Mathlib.Algebra.Order.GroupWithZero.Action.Synonym
 import Mathlib.Tactic.Positivity.Core
 
 /-!
