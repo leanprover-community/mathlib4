bcbf4c7f158d6f31c7bd31e2d176f768f3a5d3b5
adomani <adomani@gmail.com>
2024-08-27 14:55:42 +0200

    modify test

--- test/StacksAttribute.lean
+++ test/StacksAttribute.lean
@@ -5,8 +5,8 @@ warning: Tag '04 Q' should only consist of digits and uppercase letters
 ---
 warning: Tag '044QQ' is 5 characters long, but it should be 4 characters long
 ---
-warning: Tag 'lowe' should only consist of digits and uppercase letters
+warning: Tag 'loA1' should only consist of digits and uppercase letters
 -/
 #guard_msgs in
-@[stacks 04 Q "", stacks A04Q "A comment", stacks 044QQ , stacks lowe]
+@[stacks 04 Q "", stacks A04Q "A comment", stacks 044QQ , stacks loA1]
 example : True := .intro
