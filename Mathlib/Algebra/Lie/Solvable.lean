06ac9ec511b6ba6dd00c7a127a476c5744cf9eeb
Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>
2024-08-27 14:35:25 +0200

    Update Solvable.lean

--- Mathlib/Algebra/Lie/Solvable.lean
+++ Mathlib/Algebra/Lie/Solvable.lean
@@ -133,7 +133,8 @@ theorem derivedSeries_eq_derivedSeriesOfIdeal_comap (k : ℕ) :
     derivedSeries R I k = (derivedSeriesOfIdeal R L k I).comap I.incl := by
   induction k with
   | zero => simp only [derivedSeries_def, comap_incl_self, derivedSeriesOfIdeal_zero]
-  | succ k ih => simp only [derivedSeries_def, derivedSeriesOfIdeal_succ] at ih ⊢; rw [ih]
+  | succ k ih =>
+    simp only [derivedSeries_def, derivedSeriesOfIdeal_succ] at ih ⊢; rw [ih]
     exact comap_bracket_incl_of_le I (derivedSeriesOfIdeal_le_self I k)
       (derivedSeriesOfIdeal_le_self I k)
 
@@ -158,7 +159,8 @@ theorem derivedSeries_add_eq_bot {k l : ℕ} {I J : LieIdeal R L} (hI : derivedS
 theorem derivedSeries_map_le (k : ℕ) : (derivedSeries R L' k).map f ≤ derivedSeries R L k := by
   induction k with
   | zero => simp only [derivedSeries_def, derivedSeriesOfIdeal_zero, le_top]
-  | succ k ih => simp only [derivedSeries_def, derivedSeriesOfIdeal_succ] at ih ⊢
+  | succ k ih =>
+    simp only [derivedSeries_def, derivedSeriesOfIdeal_succ] at ih ⊢
     exact le_trans (map_bracket_le f) (LieSubmodule.mono_lie ih ih)
 
 theorem derivedSeries_map_eq (k : ℕ) (h : Function.Surjective f) :
