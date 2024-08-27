2ad82c329bd4c886a8d8159c56e69199a34e0fe7
zjj <zjj@zjj>
2024-08-27 04:48:04 +0100

    add 09GR

--- Mathlib/FieldTheory/IsAlgClosed/Basic.lean
+++ Mathlib/FieldTheory/IsAlgClosed/Basic.lean
@@ -194,7 +194,10 @@ end IsAlgClosed
 
 /-- If `k` is algebraically closed, `K / k` is a field extension, `L / k` is an intermediate field
 which is algebraic, then `L` is equal to `k`. A corollary of
-`IsAlgClosed.algebraMap_surjective_of_isAlgebraic`. -/
+`IsAlgClosed.algebraMap_surjective_of_isAlgebraic`.
+
+[Stacks: Lemma 09GQ, (4) ⟹ (2)](https://stacks.math.columbia.edu/tag/09GQ)
+-/
 theorem IntermediateField.eq_bot_of_isAlgClosed_of_isAlgebraic {k K : Type*} [Field k] [Field K]
     [IsAlgClosed k] [Algebra k K] (L : IntermediateField k K) [Algebra.IsAlgebraic k L] :
     L = ⊥ := bot_unique fun x hx ↦ by
