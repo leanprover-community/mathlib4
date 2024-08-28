88c2426defdb25e671c4a35cc5108e9640ae8621
Marcus Rossel <marcusrossel@icloud.com>
2024-08-28 12:55:43 +0200

    Integrate feedback

--- Mathlib/Data/List/Sort.lean
+++ Mathlib/Data/List/Sort.lean
@@ -345,6 +345,18 @@ theorem sublist_orderedInsert (x : α) (xs : List α) : xs <+ xs.orderedInsert r
   refine Sublist.trans ?_ (.append_left (.cons _ (.refl _)) _)
   rw [takeWhile_append_dropWhile]
 
+theorem cons_sublist_orderedInsert {l c : List α} {a : α} (hl : c <+ l) (ha : ∀ a' ∈ c, a ≼ a') :
+    a :: c <+ orderedInsert r a l := by
+  induction l with
+  | nil         => simp_all only [sublist_nil, orderedInsert, Sublist.refl]
+  | cons _ _ ih =>
+    unfold orderedInsert
+    split
+    · exact .cons₂ _ hl
+    · cases hl
+      · exact .cons _ <| ih ‹_›
+      · exact absurd (ha _ <| mem_cons_self ..) ‹_›
+
 section TotalAndTransitive
 
 variable [IsTotal α r] [IsTrans α r]
@@ -374,42 +386,17 @@ theorem sorted_insertionSort : ∀ l, Sorted r (insertionSort r l)
 
 end TotalAndTransitive
 
-theorem orderedInsert_stable {l c : List α} (a : α) (hl : c <+ l) : c <+ orderedInsert r a l := by
-  induction l generalizing c
-  case nil => simp_all only [sublist_nil, orderedInsert, sublist_cons_self]
-  case cons b bs ih =>
-    unfold orderedInsert
-    split
-    case isTrue => exact .cons _ hl
-    case isFalse =>
-      cases hl
-      case cons  h => exact .cons _ (ih h)
-      case cons₂ h => exact .cons₂ _ (ih h)
-
-theorem orderedInsert_stable_cons {l c : List α} {a : α} (ha : ∀ a' ∈ c, r a a') (hl : c <+ l) :
-    a :: c <+ orderedInsert r a l := by
-  induction l
-  case nil => simp_all only [sublist_nil, orderedInsert, Sublist.refl]
-  case cons b bs ih =>
-    unfold orderedInsert
-    split
-    case isTrue => exact .cons₂ _ hl
-    case isFalse hr =>
-      cases hl
-      case cons    h => exact .cons _ (ih h)
-      case cons₂ l h => exact absurd (ha _ <| mem_cons_self ..) hr
-
 /--
 If `c` is a sorted sublist of `l`, then `c` is still a sublist of `insertionSort r l`.
 -/
 theorem insertionSort_stable {l c : List α} (hr : c.Pairwise r) (hc : c <+ l) :
     c <+ insertionSort r l := by
-  induction l generalizing c
-  case nil => simp_all only [sublist_nil, insertionSort, Sublist.refl]
-  case cons a as ih =>
+  induction l generalizing c with
+  | nil         => simp_all only [sublist_nil, insertionSort, Sublist.refl]
+  | cons _ _ ih =>
     cases hc
-    case cons    hc => exact orderedInsert_stable a (ih hr hc)
-    case cons₂ c hc => cases hr; case cons hr ha => exact orderedInsert_stable_cons ha (ih hr hc)
+    · exact ih hr ‹_› |>.trans (sublist_orderedInsert ..)
+    · cases hr; exact cons_sublist_orderedInsert (ih ‹_› ‹_›) ‹_›
 
 /--
 Another statement of stability of insertion sort.
