0da2b84093d5bca045b0fc13f47b9809374b5705
Yury G. Kudryashov <urkud@urkud.name>
2024-08-28 16:26:39 -0500

    Update

--- Mathlib/Dynamics/Ergodic/Action/OfMinimal.lean
+++ Mathlib/Dynamics/Ergodic/Action/OfMinimal.lean
@@ -1,5 +1,16 @@
+/-
+Copyright (c) 2024 Yury Kudryashov. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Yury Kudryashov
+-/
 import Mathlib.Dynamics.Ergodic.Action.Regular
 import Mathlib.MeasureTheory.Measure.ContinuousPreimage
+
+/-!
+# Ergodicity from minimality
+
+
+-/
 
 open MeasureTheory Filter Set
 
@@ -38,14 +49,38 @@ theorem ErgodicSMul.trans_isMinimal (M N : Type*) [Monoid M] [MulAction M N]
     rintro _ ⟨g, rfl⟩
     simpa using hs g
 
+theorem ergodic_smul_of_denseRange_pow {M : Type*} [Monoid M] [TopologicalSpace M]
+    [MulAction M X] [ContinuousSMul M X] {g : M} (hg : DenseRange (g ^ · : ℕ → M))
+    (μ : Measure X) [IsFiniteMeasure μ] [μ.InnerRegular] [ErgodicSMul M X μ] :
+    Ergodic (g • ·) μ := by
+  borelize M
+  refine ⟨measurePreserving_smul _ _, ⟨fun s hsm hs ↦ eventuallyConst_set'.1 ?_⟩⟩
+  refine aeconst_of_dense_setOf_preimage_smul_ae M hsm.nullMeasurableSet (hg.mono ?_)
+  refine range_subset_iff.2 fun n ↦ mem_setOf.2 <| .of_eq ?_
+  rw [← smul_iterate, preimage_iterate_eq, Function.iterate_fixed hs]
+
+theorem ergodic_smul_of_denseRange_zpow {G : Type*} [Group G] [TopologicalSpace G]
+    [ContinuousInv G] [MulAction G X] [ContinuousSMul G X] {g : G} (hg : DenseRange (g ^ · : ℤ → G))
+    (μ : Measure X) [IsFiniteMeasure μ] [μ.InnerRegular] [ErgodicSMul G X μ] :
+    Ergodic (g • ·) μ := by
+  borelize G
+  refine ⟨measurePreserving_smul _ _, ⟨fun s hsm hs ↦ eventuallyConst_set'.1 ?_⟩⟩
+  refine aeconst_of_dense_aestabilizer hsm.nullMeasurableSet (hg.mono ?_)
+  rw [← Subgroup.coe_zpowers, SetLike.coe_subset_coe, ← Subgroup.zpowers_inv, Subgroup.zpowers_le,
+    MulAction.mem_aestabilizer, ← preimage_smul, hs]
+
+theorem ergodic_mul_left_of_denseRange_pow {G : Type*} [Group G]
+    [TopologicalSpace G] [TopologicalGroup G] [SecondCountableTopology G]
+    [MeasurableSpace G] [BorelSpace G]
+    {g : G} (hg : DenseRange (g ^ · : ℕ → G))
+    (μ : Measure G) [IsFiniteMeasure μ] [μ.InnerRegular] [μ.IsMulLeftInvariant] :
+    Ergodic (g * ·) μ :=
+  ergodic_smul_of_denseRange_pow hg μ
+
 theorem ergodic_mul_left_of_denseRange_zpow {G : Type*} [Group G]
     [TopologicalSpace G] [TopologicalGroup G] [SecondCountableTopology G]
     [MeasurableSpace G] [BorelSpace G]
     {g : G} (hg : DenseRange (g ^ · : ℤ → G))
     (μ : Measure G) [IsFiniteMeasure μ] [μ.InnerRegular] [μ.IsMulLeftInvariant] :
-    Ergodic (g * ·) μ := by
-  refine ⟨measurePreserving_mul_left μ g, ⟨fun s hsm hgs ↦ ?_⟩⟩
-  have : Subgroup.zpowers g ≤ MulAction.aestabilizer G μ s := by
-    rw [← Subgroup.zpowers_inv, Subgroup.zpowers_le, MulAction.mem_aestabilizer, ← preimage_smul]
-    simp only [smul_eq_mul, hgs, ae_eq_refl]
-  exact eventuallyConst_set'.1 <| aeconst_of_dense_aestabilizer hsm.nullMeasurableSet (hg.mono this)
+    Ergodic (g * ·) μ :=
+  ergodic_smul_of_denseRange_zpow hg μ
