387a912ac1812ba115af27e51a2b55f7143dfedd
Jakob von Raumer <jakob@von-raumer.de>
2024-08-28 15:53:56 +0200

    dualize

--- Mathlib/CategoryTheory/Abelian/GrothendieckAxioms.lean
+++ Mathlib/CategoryTheory/Abelian/GrothendieckAxioms.lean
@@ -58,6 +58,17 @@ instance [HasCoproducts C] [AB4 C] (α : Type v) :
     PreservesFiniteLimits (colim (J := Discrete α) (C := C)) :=
   AB4.preservesFiniteLimits _
 
+/-- A Category `C` which has products is said to have `CoAB4` (in literature `AB4*`)
+provided that products are exact. -/
+class CoAB4 [HasProducts C] where
+  /-- Exactness of products stated as `lim : (Discrete α ⥤ C) ⥤ C` preserving colimits. -/
+  preservesFiniteColimits (α : Type v) :
+    PreservesFiniteColimits (lim (J := Discrete α) (C := C))
+
+instance [HasProducts C] [CoAB4 C] (α : Type v) :
+    PreservesFiniteColimits (lim (J := Discrete α) (C := C)) :=
+  CoAB4.preservesFiniteColimits _
+
 /--
 A category `C` which has filtered colimits is said to have `AB5` provided that
 filtered colimits are exact.
@@ -71,5 +82,20 @@ class AB5 [HasFilteredColimits C] where
 instance [HasFilteredColimits C] [AB5 C] (J : Type v) [SmallCategory J] [IsFiltered J] :
     PreservesFiniteLimits (colim (J := J) (C := C)) :=
   AB5.preservesFiniteLimits _
+
+/--
+A category `C` which has cofiltered limits is said to have `CoAB5` (in literature `AB5*`)
+provided that cofiltered limits are exact.
+-/
+class CoAB5 [HasCofilteredLimits C] where
+  /-- Exactness of cofiltered limits stated as `lim : (J ⥤ C) ⥤ C` on cofiltered `J`
+  preserving colimits. -/
+  preservesFiniteColimits (J : Type v) [SmallCategory J] [IsCofiltered J] :
+    PreservesFiniteColimits (lim (J := J) (C := C))
+
+instance [HasCofilteredLimits C] [CoAB5 C] (J : Type v) [SmallCategory J] [IsCofiltered J] :
+    PreservesFiniteColimits (lim (J := J) (C := C)) :=
+  CoAB5.preservesFiniteColimits _
+
 
 end CategoryTheory
