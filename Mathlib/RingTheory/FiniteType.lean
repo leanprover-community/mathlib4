559260e739b6733b22463ba7e9b31416c34dcecb
Calle Sönne <calle.sonne@gmail.com>
2024-08-28 13:58:24 +0100

    fix

--- Mathlib/RingTheory/FiniteType.lean
+++ Mathlib/RingTheory/FiniteType.lean
@@ -3,8 +3,8 @@ Copyright (c) 2020 Johan Commelin. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johan Commelin
 -/
+
 import Mathlib.RingTheory.Adjoin.Tower
--- import Mathlib.Tactic.Algebraize
 
 /-!
 # Finiteness conditions in commutative algebra
