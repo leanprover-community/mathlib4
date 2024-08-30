557217d4774fbcf22bdbe20e51f233387a5e664f
Lorenzo Luccioli <lorenzoluccioli@gmail.com>
2024-08-30 17:46:24 +0200

    fix lint warning

--- Mathlib/MeasureTheory/Integral/Bochner.lean
+++ Mathlib/MeasureTheory/Integral/Bochner.lean
@@ -1472,7 +1472,7 @@ theorem integral_zero_measure {m : MeasurableSpace α} (f : α → G) :
 theorem setIntegral_zero_measure (f : α → G) {μ : Measure α} {s : Set α} (hs : μ s = 0) :
     ∫ x in s, f x ∂μ = 0 := Measure.restrict_eq_zero.mpr hs ▸ integral_zero_measure f
 
-lemma integral_of_isEmpty [IsEmpty α] : ∫ x, f x ∂μ = 0 :=
+lemma integral_of_isEmpty [IsEmpty α] {f : α → G} : ∫ x, f x ∂μ = 0 :=
     μ.eq_zero_of_isEmpty ▸ integral_zero_measure _
 
 theorem integral_finset_sum_measure {ι} {m : MeasurableSpace α} {f : α → G} {μ : ι → Measure α}
