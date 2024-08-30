3eb46aef99d7af7ec1acbd45f140f800f3061ca0
Yaël Dillies <yael.dillies@gmail.com>
2024-08-30 14:39:43 +0000

    feat: `‖c - a * b‖ ≤ ‖c - a‖ + ‖1 - b‖` when `‖a‖ ≤ 1` This inequality is particularly useful when `c = 1` and `‖a‖ = ‖b‖ = 1` as it then shows that chord length is a metric on the circle.
    From LeanAPAP
    
    Co-authored-by: Bhavik Mehta <bhavik.mehta8@gmail.com>

--- Mathlib/Analysis/Normed/Field/Basic.lean
+++ Mathlib/Analysis/Normed/Field/Basic.lean
@@ -326,7 +326,7 @@ end NonUnitalSeminormedRing
 
 section SeminormedRing
 
-variable [SeminormedRing α]
+variable [SeminormedRing α] {a b c : α}
 
 /-- A subalgebra of a seminormed ring is also a seminormed ring, with the restriction of the
 norm. -/
@@ -446,6 +446,21 @@ instance Pi.seminormedRing {π : ι → Type*} [Fintype ι] [∀ i, SeminormedRi
 instance MulOpposite.instSeminormedRing : SeminormedRing αᵐᵒᵖ where
   __ := instRing
   __ := instNonUnitalSeminormedRing
+
+lemma norm_one_sub_mul (ha : ‖a‖ ≤ 1) : ‖c - a * b‖ ≤ ‖c - a‖ + ‖1 - b‖ :=
+  calc
+    _ ≤ ‖c - a‖ + ‖a * (1 - b)‖ := by
+        simpa [mul_one_sub] using norm_sub_le_norm_sub_add_norm_sub c a (a * b)
+    _ ≤ ‖c - a‖ + ‖a‖ * ‖1 - b‖ := by gcongr; exact norm_mul_le ..
+    _ ≤ ‖c - a‖ + 1 * ‖1 - b‖ := by gcongr
+    _ = ‖c - a‖ + ‖1 - b‖ := by simp
+
+lemma norm_one_sub_mul' (hb : ‖b‖ ≤ 1) : ‖c - a * b‖ ≤ ‖1 - a‖ + ‖c - b‖ := by
+  rw [add_comm]; exact norm_one_sub_mul (α := αᵐᵒᵖ) hb
+
+lemma nnnorm_one_sub_mul (ha : ‖a‖₊ ≤ 1) : ‖c - a * b‖₊ ≤ ‖c - a‖₊ + ‖1 - b‖₊ := norm_one_sub_mul ha
+lemma nnnorm_one_sub_mul' (hb : ‖b‖₊ ≤ 1) : ‖c - a * b‖₊ ≤ ‖1 - a‖₊ + ‖c - b‖₊ :=
+  norm_one_sub_mul' hb
 
 end SeminormedRing
 
