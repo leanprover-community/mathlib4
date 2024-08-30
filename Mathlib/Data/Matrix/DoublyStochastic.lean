5cc20bb0cea32e78891147b6c5145620fda86696
Bhavik Mehta <bhavikmehta8@gmail.com>
2024-08-30 11:49:55 +0200

    shake more

--- Mathlib/Data/Matrix/DoublyStochastic.lean
+++ Mathlib/Data/Matrix/DoublyStochastic.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Bhavik Mehta
 -/
 
-import Mathlib.Analysis.Convex.Combination
+import Mathlib.Analysis.Convex.Basic
 import Mathlib.LinearAlgebra.Matrix.Permutation
 
 /-!
