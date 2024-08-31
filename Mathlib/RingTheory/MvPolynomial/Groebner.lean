b9e1875842cbaad44470f4165eb40980bc4bd427
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2024-08-31 15:53:28 +0200

    starting

--- Mathlib/RingTheory/MvPolynomial/Groebner.lean
+++ Mathlib/RingTheory/MvPolynomial/Groebner.lean
@@ -1,13 +1,22 @@
 import Mathlib.RingTheory.MvPolynomial.Homogeneous
+import Mathlib.Data.Finsupp.Lex
 
 /-! # Groebner bases -/
 
 
 namespace Finsupp
 
-variable (σ : Type*)
 
-def MonomialOrder (σ : Type*) : False := sorry
+structure MonomialOrder (σ : Type*) where 
+  syn : Type*
+  clacm : CanonicallyLinearOrderedAddCommMonoid syn
+  toSyn : (σ →₀ ℕ) ≃+ syn
 
+instance (σ : Type*) (m : MonomialOrder σ) : CanonicallyLinearOrderedAddCommMonoid m.syn := m.clacm
+
+theorem MonomialOrder.wf (σ : Type*) (m : MonomialOrder σ) : 
+--    haveI : CanonicallyLinearOrderedAddCommMonoid m.syn := m.clacm
+    WellFounded (fun d e ↦ m.toSyn d < m.toSyn e) := 
+  sorry
 
 end Finsupp
