e79b5ca6e5e17e770a6248a0a0a3fe1a304e1ec2
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2024-08-31 15:20:41 +0200

    initial commit

---
+++ Mathlib/RingTheory/MvPolynomial/Groebner.lean
@@ -0,0 +1,13 @@
+import Mathlib.RingTheory.MvPolynomial.Homogeneous
+
+/-! # Groebner bases -/
+
+
+namespace Finsupp
+
+variable (σ : Type*)
+
+def MonomialOrder (σ : Type*) : False := sorry
+
+
+end Finsupp
