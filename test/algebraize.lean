1f91bc4bd8f8e4b116f1a44a9943e5210a8db4b7
Calle Sönne <calle.sonne@gmail.com>
2024-08-31 18:48:54 +0100

    add file

---
+++ test/algebraize.lean
@@ -0,0 +1,15 @@
+import Mathlib.RingTheory.Flat.Algebra
+import Mathlib.RingTheory.FinitePresentation
+
+
+/-
+TODO:
+1. Test each of the basic properties.
+2. Test adding morphisms (sometimes algebra property already exists)
+
+
+3. Test possible error messages using #guard_hyps
+- Give non-ring homs to algebraize
+- Find some other possible errors.
+
+-/
