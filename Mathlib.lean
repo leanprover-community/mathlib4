c1227755eddd97bb49e71d32741274247ede5805
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2024-08-31 23:28:04 +0200

    move some files

--- Mathlib.lean
+++ Mathlib.lean
@@ -2866,7 +2866,6 @@ import Mathlib.Lean.Meta.RefinedDiscrTree.Pi
 import Mathlib.Lean.Meta.Simp
 import Mathlib.Lean.Name
 import Mathlib.Lean.PrettyPrinter.Delaborator
-import Mathlib.Lean.SubExpr
 import Mathlib.Lean.Thunk
 import Mathlib.LinearAlgebra.AffineSpace.AffineEquiv
 import Mathlib.LinearAlgebra.AffineSpace.AffineMap
