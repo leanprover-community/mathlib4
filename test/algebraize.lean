c66e69482ab3f67e2880dd937db67023514057c1
Calle Sönne <calle.sonne@gmail.com>
2024-09-01 17:29:52 +0100

    Add tests + fix IsIntegral

--- test/algebraize.lean
+++ test/algebraize.lean
@@ -1,12 +1,50 @@
 import Mathlib.RingTheory.Flat.Algebra
 import Mathlib.RingTheory.FinitePresentation
+import Mathlib.RingTheory.IntegralClosure.Algebra.Defs
 
+example (A B : Type*) [CommRing A] [CommRing B] (f : A →+* B) : True := by
+  algebraize f
+  have h : Algebra A B := inferInstance
+  guard_hyp h : Algebra A B
+  trivial
 
-/-
-TODO:
-1. Test each of the basic properties. (use #guard_hyps!)
-2. Test adding morphisms (sometimes algebra property already exists)
-3. Test possible error messages using #guard_msg
-- Give non-ring homs to algebraize
+example (A B C : Type*) [CommRing A] [CommRing B] [CommRing C] (f : A →+* B) (g : B →+* C) :
+    True := by
+  algebraize f g (g.comp f)
+  have h₁ : Algebra A C := inferInstance
+  guard_hyp h₁ : Algebra A C
+  clear h₁
+  have h₂ : IsScalarTower A B C := inferInstance
+  guard_hyp h₂ : IsScalarTower A B C
+  trivial
 
--/
+example (A B : Type*) [CommRing A] [CommRing B] (f : A →+* B) (hf : f.Finite) : True := by
+  algebraize f
+  have h : Module.Finite A B := inferInstance
+  guard_hyp h : Module.Finite A B
+  trivial
+
+example (A B : Type*) [CommRing A] [CommRing B] (f : A →+* B) (hf : f.FiniteType) : True := by
+  algebraize f
+  have h : Algebra.FiniteType A B := inferInstance
+  guard_hyp h : Algebra.FiniteType A B
+  trivial
+
+example (A B : Type*) [CommRing A] [CommRing B] (f : A →+* B) (hf : f.Flat) : True := by
+  algebraize f
+  have h : Algebra.Flat A B := inferInstance
+  guard_hyp h : Algebra.Flat A B
+  trivial
+
+example (A B : Type*) [CommRing A] [CommRing B] (f : A →+* B) (hf : f.IsIntegral) : True := by
+  algebraize f
+  have h : Algebra.IsIntegral A B := inferInstance
+  guard_hyp h : Algebra.IsIntegral A B
+  trivial
+
+example (A B C : Type*) [CommRing A] [CommRing B] [CommRing C] (f : A →+* B) (g : B →+* C)
+    (hfg : (g.comp f).Flat) : True := by
+  algebraize f g (g.comp f)
+  have h₁ : Algebra.Flat A C := inferInstance
+  guard_hyp h₁ : Algebra.Flat A C
+  trivial
