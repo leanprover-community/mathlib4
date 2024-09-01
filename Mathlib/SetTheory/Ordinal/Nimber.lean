7b7ab384ddcf2a6284d560d666dca7c0cc31c16a
Violeta Hernández <vi.hdz.p@gmail.com>
2024-09-01 10:55:30 -0600

    fix copy-paste stuff

--- Mathlib/SetTheory/Ordinal/Nimber.lean
+++ Mathlib/SetTheory/Ordinal/Nimber.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2022 Violeta Hernández Palacios. All rights reserved.
+Copyright (c) 2024 Violeta Hernández Palacios. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Violeta Hernández Palacios
 -/
@@ -46,7 +46,6 @@ noncomputable section
 
 /-- A type synonym for ordinals with natural addition and multiplication. -/
 def Nimber : Type _ :=
-  -- Porting note: used to derive LinearOrder & SuccOrder but need to manually define
   Ordinal deriving Zero, Inhabited, One, WellFoundedRelation
 
 instance Nimber.linearOrder : LinearOrder Nimber := {Ordinal.linearOrder with}
