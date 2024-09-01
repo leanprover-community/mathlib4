1f32a74b7b111fb26eda404dfdbb0f49c3318e9f
adomani <adomani@gmail.com>
2024-09-01 19:37:22 +0200

    adomani: update scripts

---
+++ scripts/get_tlabel.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+PR="${1}"
+
+>&2 printf $'Using PR: \'%s\'\n' "${PR}"
+
+tlabels="$(gh api --jq '.labels.[].name' "${PR}" | grep -- '^t-' || printf 'generic')"
+# print to error, since the stdout is captured into `GITHUB_OUTPUT
+>&2 printf 't-labels:\n---\n%s\n---\n' "${tlabels}"
+# if there isn't exactly 1 `t-xxx` label, use `generic`
+if [[ "$(wc -l <<<"${tlabels}")" -ne 1 ]]; then
+  tlabels="generic"
+fi
+streamName="${tlabels} maintainer merges"
+>&2 printf $'Post to topic: \'%s\'"\n' "${streamName}"
+echo "name=${streamName}"
