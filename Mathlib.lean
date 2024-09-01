66d9d46c4d0b09d0434c0412c38340ab308c722f
Parcly Taxel <reddeloostw@gmail.com>
2024-09-01 12:25:27 +0800

    chore: move `Init.Data.Quot` to `Logic.Relation`

--- Mathlib.lean
+++ Mathlib.lean
@@ -2836,7 +2836,6 @@ import Mathlib.Init
 import Mathlib.Init.Algebra.Classes
 import Mathlib.Init.Data.List.Lemmas
 import Mathlib.Init.Data.Nat.Lemmas
-import Mathlib.Init.Data.Quot
 import Mathlib.Init.Logic
 import Mathlib.Lean.CoreM
 import Mathlib.Lean.Elab.Tactic.Basic
