5e81dad773b32a93981557a8cbb52658040ff3af
Bjørn Kjos-Hanssen <bjoernkh@hawaii.edu>
2024-09-01 11:16:09 -1000

    better proof of isLocalMax_of_mono_anti thanks to @PatrickMassot

--- Mathlib/Topology/Order/OrderClosedExtr.lean
+++ Mathlib/Topology/Order/OrderClosedExtr.lean
@@ -1,23 +1,24 @@
 /-
 Copyright (c) 2024 Bjørn Kjos-Hanssen. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Bjørn Kjos-Hanssen
+Authors: Bjørn Kjos-Hanssen, Patrick Massot
 -/
-import Mathlib.Topology.Separation
-import Mathlib.Order.Interval.Set.Basic
-import Mathlib.Topology.Defs.Filter
+
 import Mathlib.Topology.Order.OrderClosed
 import Mathlib.Topology.Order.LocalExtr
 
 /-!
 # Local maxima from monotonicity and antitonicityOrder-closed topologies
 
-In this file we prove a lemma that is useful for the First Derivative Test in calculus.
+In this file we prove a lemma that is useful for the First Derivative Test in calculus,
+and its dual.
 
-## Main statement
+## Main statements
 
 * `isLocalMax_of_mono_anti` : if a function `f` is monotone to the left of `x`
   and antitone to the right of `x` then `f` has a local maximum at `x`.
+
+* `isLocalMin_of_anti_mono` : the dual statement for minima.
 
 -/
 
@@ -31,19 +32,18 @@ lemma isLocalMax_of_mono_anti.{u, v}
     {a b c : α} (g₀ : a < b) (g₁ : b < c) {f : α → β}
     (h₀ : MonotoneOn f (Ioc a b))
     (h₁ : AntitoneOn f (Ico b c)) : IsLocalMax f b := by
-  unfold IsLocalMax IsMaxFilter Filter.Eventually
-  rw [nhds_def, Filter.mem_iInf]
-  use {Ioo a c}, (toFinite _), (fun _ ↦ Ioo a c ∪ {x | f x ≤ f b})
-  simp only [mem_setOf_eq, Subtype.forall, mem_singleton_iff, forall_eq, mem_Ioo,
-    iInter_coe_set, iInter_iInter_eq_left]
-  constructor
-  · exact Filter.mem_iInf_of_mem
-      (by simp_all only [and_self, true_and]; apply isOpen_Ioo)
-      (by simp_all)
-  · ext u
-    simp only [mem_setOf_eq, mem_union, mem_Ioo, iff_or_self, and_imp]
-    intros
-    exact (em (u < b)).elim
-      (fun H => h₀ (by simp_all only [mem_Ioc, true_and]; exact le_of_lt H)
-        (by simp_all) (le_of_lt H))
-      (fun H => h₁ (by simp_all) (by simp_all) (le_of_not_lt H))
+
+  apply Filter.mem_of_superset (Ioo_mem_nhds g₀ g₁)
+  intro x hx
+  rcases le_total x b with hx' | hx' <;> aesop
+
+lemma isLocalMin_of_anti_mono.{u, v}
+    {α : Type u} [TopologicalSpace α] [LinearOrder α] [OrderClosedTopology α]
+    {β : Type v} [Preorder β]
+    {a b c : α} (g₀ : a < b) (g₁ : b < c) {f : α → β}
+    (h₀ : AntitoneOn f (Ioc a b))
+    (h₁ : MonotoneOn f (Ico b c)) : IsLocalMin f b := by
+
+  apply Filter.mem_of_superset (Ioo_mem_nhds g₀ g₁)
+  intro x hx
+  rcases le_total x b with hx' | hx' <;> aesop
