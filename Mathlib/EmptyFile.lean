682c54260e9d8343940dd5177a7bf1a3b271d41b
adomani <adomani@gmail.com>
2024-09-01 19:02:50 +0200

    extend unimported file

--- Mathlib/EmptyFile.lean
+++ Mathlib/EmptyFile.lean
@@ -1,1 +1,14 @@
--- an unnecessary file
+/-
+Copyright (c) 2024 Damiano Testa. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Damiano Testa
+-/
+
+import Mathlib.Data.Nat.Notation
+
+/-!
+# An unnecessary file
+-/
+
+/-- with an unnecessary lemma. -/
+theorem Unneeded : True := .intro
