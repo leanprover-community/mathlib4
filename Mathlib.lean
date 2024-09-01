fe52254ff3c3cc4ae913b406669df99b59caa779
Bjørn Kjos-Hanssen <bjoernkh@hawaii.edu>
2024-08-31 18:24:41 -1000

    Ran No update necessary to hopefully fix failed automatic test

--- Mathlib.lean
+++ Mathlib.lean
@@ -4347,6 +4347,7 @@ import Mathlib.Topology.Clopen
 import Mathlib.Topology.ClopenBox
 import Mathlib.Topology.CompactOpen
 import Mathlib.Topology.Compactification.OnePoint
+import Mathlib.Topology.Compactification.OnePointEquiv
 import Mathlib.Topology.Compactness.Compact
 import Mathlib.Topology.Compactness.Lindelof
 import Mathlib.Topology.Compactness.LocallyCompact
