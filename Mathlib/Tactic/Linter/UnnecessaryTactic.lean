69a3ba770d1c71c932fe0fb0d81516f07f7cd378
adomani <adomani@gmail.com>
2024-09-02 12:36:48 +0200

    work and ctors

--- Mathlib/Tactic/Linter/UnnecessaryTactic.lean
+++ Mathlib/Tactic/Linter/UnnecessaryTactic.lean
@@ -23,16 +23,20 @@ namespace Mathlib.Linter
 
 abbrev exclusions : HashSet SyntaxNodeKind := HashSet.empty
   |>.insert `Batteries.Tactic.«tacticOn_goal-_=>_»
-  |>.insert ``Lean.Parser.Tactic.induction
   |>.insert `«tactic#adaptation_note_»
+  |>.insert ``Lean.cdot
   |>.insert `«;»
   |>.insert `null
+  |>.insert ``Lean.Parser.Tactic.induction
+  |>.insert `Mathlib.Tactic.induction'
   --|>.insert ``Lean.Parser.Term.byTactic
   --|>.insert `by
-#check HashMap
+
 partial
 def findRanges (stx : Syntax) : HashSet (String.Range × SyntaxNodeKind) :=
   let next := stx.foldArgs (fun arg r => r.merge (findRanges arg)) {}
+  -- ignore default values when they involve tactics
+  if let .node _ ``Lean.Parser.Term.binderTactic _ := stx then {} else
   if let .node _ ``Lean.Parser.Tactic.tacticSeq1Indented #[.node _ _ args] := stx then
     let tacs := args.filter (! exclusions.contains ·.getKind)
     if 2 ≤ tacs.size then
@@ -41,29 +45,49 @@ def findRanges (stx : Syntax) : HashSet (String.Range × SyntaxNodeKind) :=
   else next
 
 partial
-def erasable (toErase rgs : HashSet (String.Range × SyntaxNodeKind)) (tree : InfoTree) :
+def erasable (rgs : HashSet (String.Range × SyntaxNodeKind)) (tree : InfoTree) :
     HashSet (String.Range × SyntaxNodeKind) := Id.run do
-  let mut toErase := toErase
   match tree with
-    | .context _ t => erasable toErase rgs t
+    | .context _ t => erasable rgs t
     | .node info args =>
-      let rest := args.foldl (fun a b => (erasable toErase rgs b).merge a) {}
       if let .ofTacticInfo i := info then
         let rg := rgs.find? ((i.stx.getRange? true).getD default, i.stx.getKind)
         if rg.isSome && i.goalsBefore != i.goalsAfter then
-          dbg_trace "{i.stx.getKind} -- erasing"
-          toErase := toErase.insert rg.get!
-          return args.foldl (fun a b => (erasable toErase rgs b).merge a) {}
+          --dbg_trace "{i.stx.getKind} -- modifies"
+          return (args.foldl (fun a b => (erasable (rgs.erase rg.get!) b).merge (a.erase rg.get!)) {}).insert rg.get!
         else
           if rg.isSome then
-            dbg_trace "{i.stx.getKind} -- unused"
-            args.foldl (fun a b => (wr rgs b).merge a) rgs
+            --dbg_trace "{i.stx.getKind} -- does not modify"
+            return args.foldl (fun a b => (erasable (rgs.erase rg.get!) b).merge a) {}
           else
-            args.foldl (fun a b => (wr rgs b).merge a) rgs
+            return args.foldl (fun a b => (erasable rgs b).merge a) {}
 
       else
-        args.foldl (fun a b => (wr rgs b).merge a) rgs
-    | _  => rgs
+        return args.foldl (fun a b => (erasable rgs b).merge a) {}
+    | _  => return {}
+
+def ctorInfo : Info → String
+  | .ofTacticInfo _ => "TacticInfo"
+  | .ofTermInfo _ => "TermInfo"
+  | .ofCommandInfo _ => "CommandInfo"
+  | .ofMacroExpansionInfo _ => "MacroExpansionInfo"
+  | .ofOptionInfo _ => "OptionInfo"
+  | .ofFieldInfo _ => "FieldInfo"
+  | .ofCompletionInfo _ => "CompletionInfo"
+  | .ofUserWidgetInfo _ => "UserWidgetInfo"
+  | .ofCustomInfo _ => "CustomInfo"
+  | .ofFVarAliasInfo _ => "FVarAliasInfo"
+  | .ofFieldRedeclInfo _ => "FieldRedeclInfo"
+  | .ofOmissionInfo _ => "OmissionInfo"
+
+
+
+partial
+def traceIT : InfoTree → String
+    | .context _ t => "context " ++ traceIT t
+    | .node info args => args.foldl (· ++ traceIT ·) s!"node {ctorInfo info}"
+    | _  => "mvar"
+
 
 partial
 def wr : HashSet (String.Range × SyntaxNodeKind) → InfoTree → HashSet (String.Range × SyntaxNodeKind)
@@ -75,11 +99,11 @@ def wr : HashSet (String.Range × SyntaxNodeKind) → InfoTree → HashSet (Stri
       if rg.isSome && i.goalsBefore != i.goalsAfter then
         let newRgs := rgs.erase rg.get!
         let rest := args.foldl (fun a b => (wr newRgs b).merge (a.erase rg.get!)) newRgs
-        dbg_trace "{i.stx.getKind} -- erasing"
+        --dbg_trace "{i.stx.getKind} -- erasing"
         rest.erase (i.stx.getRange?.getD default, i.stx.getKind)
       else
         if rg.isSome then
-          dbg_trace "{i.stx.getKind} -- unused"
+          --dbg_trace "{i.stx.getKind} -- unused"
           args.foldl (fun a b => (wr rgs b).merge a) rgs
         else
           args.foldl (fun a b => (wr rgs b).merge a) rgs
@@ -107,12 +131,25 @@ def unnecessaryTacticLinter : Linter where run := withSetOptionIn fun stx ↦ do
   --  logInfoAt (.ofRange r) m!"found you {(r.start, r.stop - r.start)}!"
   let trees ← getInfoTrees
   let mut fd := {}
-  dbg_trace "found {trees.size} infotrees"
+  --dbg_trace "found {trees.size} infotrees"
   for t in trees do
+    if let .node _ arg := t then -- (.ofOptionInfo i) (.ofCompletionInfo i)
+      if arg.isEmpty then
+        --dbg_trace traceIT t
+        --let rg := i.stx.getRange?.getD default
+        --dbg_trace "{(rg.start, rg.stop)} -- {rg.stop - rg.start}"
+        continue
+    --dbg_trace traceIT t
     fd := wr rgs t
+    fd := erasable rgs t
     --dbg_trace "size: {fd.size}, rgs: {rgs.size}"
-    for (e, str) in fd do
-      Linter.logLint linter.unnecessaryTactic (.ofRange e) m!"'{str}, {(e.start, e.stop - e.start)}' is unnecessary."
+    for (e, str) in rgs do
+      if ! fd.contains (e, str) then
+        Linter.logLint linter.unnecessaryTactic (.ofRange e)
+          m!"'{str}, {(e.start, e.stop - e.start)}' is unnecessary."
+    --dbg_trace fd.size
+    --for (e, str) in fd do
+    --  dbg_trace "erasable: {(e.start, e.stop - e.start)} {str}}"
 
 initialize addLinter unnecessaryTacticLinter
 
