4a2c1958bf7d0b529a6ca60ae8f7be966e4ad609
Jeremy Tan Jie Rui <reddeloostw@gmail.com>
2024-09-02 17:50:52 +0000

    chore: move `Init.Data.Quot` to `Logic.Relation` (#16378)

--- Mathlib.lean
+++ Mathlib.lean
@@ -2839,7 +2839,6 @@ import Mathlib.Init
 import Mathlib.Init.Algebra.Classes
 import Mathlib.Init.Data.List.Lemmas
 import Mathlib.Init.Data.Nat.Lemmas
-import Mathlib.Init.Data.Quot
 import Mathlib.Init.Logic
 import Mathlib.Lean.CoreM
 import Mathlib.Lean.Elab.Tactic.Basic
