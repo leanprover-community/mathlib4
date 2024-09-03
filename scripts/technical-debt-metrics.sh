8f836644d793169eb3d06d6b732cb44385884757
damiano <adomani@gmail.com>
2024-09-03 12:21:06 +0000

    fix: count long files, instead of printing all of them (#16418)
    Reported on [Zulip](https://leanprover.zulipchat.com/#narrow/stream/287929-mathlib4/topic/Technical.20Debt.20Counters/near/466993718)

--- scripts/technical-debt-metrics.sh
+++ scripts/technical-debt-metrics.sh
@@ -54,9 +54,8 @@ for i in ${!titlesAndRegexes[@]}; do
 done
 
 printf '%s|%s\n' "$(grep -c 'docBlame' scripts/nolints.json)" "documentation nolint entries"
-# the exceptions for files exceeding the 1500 line limit are split between the `longFile` and
-# the text-based linters.
-printf '%s|%s\n' "$(grep -c '^set_option linter.style.longFile [0-9]*' $(git ls-files '*.lean'))" "large files"
+# We count the number of large files, making sure to avoid counting the test file `test/Lint.lean`.
+printf '%s|%s\n' "$(git grep '^set_option linter.style.longFile [0-9]*' Mathlib | wc -l)" "large files"
 printf '%s|%s\n' "$(git grep "^open .*Classical" | grep -v " in$" -c)" "bare open (scoped) Classical"
 # We print the number of files, not the number of matches --- hence, the nested grep.
 printf '%s|%s\n' "$(git grep -c 'autoImplicit true' | grep -c -v 'test')" "non-test files with autoImplicit true"
