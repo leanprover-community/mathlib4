286f738297fcf1804c33bf132fef501341d9b24f
adomani <adomani@gmail.com>
2024-09-03 06:24:33 +0200

    remove unusedTactic options

--- Mathlib/Util/SleepHeartbeats.lean
+++ Mathlib/Util/SleepHeartbeats.lean
@@ -34,7 +34,7 @@ elab "sleep_heartbeats " n:num : tactic => do
      option -/
   | some m => sleepAtLeastHeartbeats (m * 1000)
 
-set_option linter.unusedTactic false in
+set_option linter.unnecessaryTactic false in
 example : 1 = 1 := by
   sleep_heartbeats 1000
   rfl
