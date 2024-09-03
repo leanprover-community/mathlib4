0c7bfeca86fbd669c6ca9078c0c17c74795c632f
yukiniu <127060846+yukiniu@users.noreply.github.com>
2024-09-03 10:55:15 +0800

    omega does the job

--- Mathlib/Data/Multiset/Basic.lean
+++ Mathlib/Data/Multiset/Basic.lean
@@ -9,6 +9,7 @@ import Mathlib.Data.List.Perm
 import Mathlib.Data.Set.List
 import Mathlib.Init.Quot
 import Mathlib.Order.Hom.Basic
+import Mathlib.Algebra.Order.Monoid.Unbundled.MinMax
 
 /-!
 # Multisets
@@ -996,7 +997,6 @@ theorem card_erase_eq_ite {a : α} {s : Multiset α} :
   · rwa [card_erase_of_mem h, if_pos]
   · rwa [erase_of_not_mem h, if_neg]
 
--- 2 of 5
 theorem in_of_ne_of_cons_erase {a a0: α} {M X : Multiset α}
     (H : M = (a0 ::ₘ X).erase a) (hyp : ¬ a = a0) : a0 ∈ M := by
   rw [H, Multiset.mem_erase_of_ne fun h ↦ hyp <| id <| Eq.symm h, Multiset.mem_cons]
@@ -2333,14 +2333,11 @@ theorem map_count_True_eq_filter_card (s : Multiset α) (p : α → Prop) [Decid
   simp only [count_eq_card_filter_eq, filter_map, card_map, Function.id_comp,
     eq_true_eq_id, Function.comp_apply]
 
-
--- 1 of 5
 theorem sub_singleton [DecidableEq α] (a : α) (s : Multiset α) : s - {a} = s.erase a := by
   ext
   simp [Multiset.erase_singleton, Multiset.count_singleton]
   split <;> simp_all
 
--- 3 of 5
 theorem singleton_add_sub_of_cons_add [DecidableEq α] {a a0: α} {M X : Multiset α}
     (H : a ::ₘ M = X + {a0}) : M = X + {a0} - {a} := by
   by_cases hyp : a = a0
@@ -2370,7 +2367,6 @@ theorem singleton_add_sub_of_cons_add [DecidableEq α] {a a0: α} {M X : Multise
       rw [this]
       simp_all
 
--- 5 of 5
 theorem mem_sub_of_not_mem_of_mem {α} [DecidableEq α] (x : α) (X Y: Multiset α) (x_in_X : x ∈ X)
     (x_notin_Y : x ∉ Y) : x ∈ X - Y  := by
   have : Multiset.count x X ≥ 1 := by
@@ -2379,7 +2375,6 @@ theorem mem_sub_of_not_mem_of_mem {α} [DecidableEq α] (x : α) (X Y: Multiset 
   rw [← Multiset.one_le_count_iff_mem, Multiset.count_sub]
   simp_all
 
--- 4 of 5
 theorem inter_add_sub_of_eq {α} [dec : DecidableEq α] {M N P Q: Multiset α} (h : M + N = P + Q) :
     N = N ∩ Q + (P - M) := by
   ext x
@@ -2387,15 +2382,7 @@ theorem inter_add_sub_of_eq {α} [dec : DecidableEq α] {M N P Q: Multiset α} (
   have h0 : M.count x + N.count x = P.count x + Q.count x := by
     rw [Multiset.ext] at h
     simp_all only [Multiset.mem_add, Multiset.count_add]
-  by_cases l_u : M.count x ≤ P.count x
-  · have : N.count x ≥ Q.count x := by sorry -- linarith?
-    simp_all only [ge_iff_le, min_eq_right]
-    -- have := tsub_add_cancel_of_le l_u
-    sorry --linarith
-  · simp_all only [not_le, gt_iff_lt]
-    have : Multiset.count x N ≤ Multiset.count x Q := by sorry --linarith?
-    have := le_of_lt l_u
-    simp_all
+  omega
 
 /-! ### Lift a relation to `Multiset`s -/
 
