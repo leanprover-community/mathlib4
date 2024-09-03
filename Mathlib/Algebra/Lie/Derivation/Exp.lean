2d9f2ab9366636c85618d23c2eba44aa428878a7
Calle Sönne <calle.sonne@gmail.com>
2024-09-03 19:36:14 +0100

    progress

--- Mathlib/Algebra/Lie/Derivation/Exp.lean
+++ Mathlib/Algebra/Lie/Derivation/Exp.lean
@@ -9,9 +9,12 @@ open algebraMap
 
 variable (R : Type u) (L : Type v) [CommRing R] [Algebra ℚ R] [LieRing L] [LieAlgebra R L]
 
+-- TODO: figure out good generality for this..
+-- Idea: Module over a ℚ-alg R, which is a ...? (Should be endomorphisms!)
+-- Maybe best is just endomorphisms of some module of some ℚ-alg R?
 noncomputable def expSum : (LieDerivation R L L) → L →ₗ[R] L := fun x ↦
   ∑ n in Finset.range (nilpotencyClass x.toLinearMap),
-    ((1 / n.factorial : ℚ) : R) • (x.toLinearMap ^ n) -- TODO: get pow inside LieDerivation!!
+    ((1 / n.factorial : ℚ) : R) • (x.toLinearMap ^ n)
 
 lemma expSum_eq_ge_range {x : LieDerivation R L L} (hx : IsNilpotent x.toLinearMap)
     {n : ℕ} (hn : nilpotencyClass x.toLinearMap ≤ n) :
@@ -32,13 +35,18 @@ lemma expSum_eq_range_add {x : LieDerivation R L L} (hx : IsNilpotent x.toLinear
 -- need nilpotencyClass smul le?
 
 -- multiplicativity is needed here?
-noncomputable def exp : (LieDerivation R L L) → (L →ₗ[R] L)ˣ := fun x ↦
-  { val := ∑ n in Finset.range (nilpotencyClass x.toLinearMap),
-    ((1 / n.factorial : ℚ) : R) • (x.toLinearMap ^ n)
-  -- todo do these later down
-    inv := sorry
-    val_inv := sorry
-    inv_val := sorry }
+noncomputable def exp : (LieDerivation R L L) → L →ₗ⁅R⁆ L := fun δ ↦ {
+  toLinearMap := expSum R L δ
+  map_lie' := by
+    intro x y
+    simp [expSum]
+    sorry -- need sum and ⁅⁆ interaction
+    -- Then need some sort of "Finset.sum prod" interaction (probably have to do it manually)
+    -- Then leibniz (done)
+    -- then ok!
+
+
+}
 
 -- Also need sum version of Leibniz rule
 
