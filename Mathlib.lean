5af800263e0f33351bc2f64ba8ebeecec0b0fad5
Jeremy Tan Jie Rui <reddeloostw@gmail.com>
2024-09-03 09:13:46 +0000

    chore: move `Init.Data.List.Lemmas` to `Data.List.Defs` (#16434)

--- Mathlib.lean
+++ Mathlib.lean
@@ -2840,7 +2840,6 @@ import Mathlib.GroupTheory.Transfer
 import Mathlib.InformationTheory.Hamming
 import Mathlib.Init
 import Mathlib.Init.Algebra.Classes
-import Mathlib.Init.Data.List.Lemmas
 import Mathlib.Init.Data.Nat.Lemmas
 import Mathlib.Init.Logic
 import Mathlib.Lean.CoreM
