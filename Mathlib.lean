e6fa3c7a6a7b9618473c895a33902c0848eea011
Salvatore Mercuri <smercuri@ed.ac.uk>
2024-09-04 17:09:25 +0100

    Update Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -3434,6 +3434,7 @@ import Mathlib.NumberTheory.NumberField.Basic
 import Mathlib.NumberTheory.NumberField.CanonicalEmbedding.Basic
 import Mathlib.NumberTheory.NumberField.CanonicalEmbedding.ConvexBody
 import Mathlib.NumberTheory.NumberField.ClassNumber
+import Mathlib.NumberTheory.NumberField.Completion
 import Mathlib.NumberTheory.NumberField.Discriminant
 import Mathlib.NumberTheory.NumberField.Embeddings
 import Mathlib.NumberTheory.NumberField.EquivReindex
