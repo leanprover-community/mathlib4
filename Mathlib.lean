6dbe2b2e513f12b1dd261353fb1d7db655e9db82
Chris Hughes <chrishughes24@gmail.com>
2024-09-04 10:38:44 +0000

    feat(ModelTheory): Lefschetz principle for AlgClosed Fields (#6617)
    The main question here was how to integrate the model theoretic classes with the normal type classes. The solution was the
    `CompatibleRing` class, and to apply a theorem about the model theory of Alg Closed Fields to a literal Alg Closed field,
    all you have to do is `letI := compatibleRingOfRing K`. Everything else can then be done by type class search. Going the other way, from a model theoretic field to a normal field is a bit more difficult, but I think people will do this more rarely, particularly with a complete theory like `ACF p`.
    
    
    
    Co-authored-by: Chris Hughes <33847686+ChrisHughes24@users.noreply.github.com>
    Co-authored-by: Aaron Anderson <awainverse@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -3314,6 +3314,7 @@ import Mathlib.MeasureTheory.SetAlgebra
 import Mathlib.MeasureTheory.SetSemiring
 import Mathlib.ModelTheory.Algebra.Field.Basic
 import Mathlib.ModelTheory.Algebra.Field.CharP
+import Mathlib.ModelTheory.Algebra.Field.IsAlgClosed
 import Mathlib.ModelTheory.Algebra.Ring.Basic
 import Mathlib.ModelTheory.Algebra.Ring.FreeCommRing
 import Mathlib.ModelTheory.Basic
