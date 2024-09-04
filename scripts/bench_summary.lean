d32d62c1ecfe35b72bc473c38e9db40476b40367
adomani <adomani@gmail.com>
2024-09-04 10:14:48 +0200

    lint, major cleanup, some docs

--- scripts/bench_summary.lean
+++ scripts/bench_summary.lean
@@ -1,11 +1,26 @@
+/-
+Copyright (c) 2024 Damiano Testa. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Damiano Testa
+-/
+
 import Lean.Data.Json.FromToJson
 import Lean.Data.Json.Parser
+
+/-!
+# Summary of `!bench` results
+
+This file contains a script that converts data retrieved from the speed-center into a
+shorter, more accessible format.
+-/
+
+namespace BenchAction
 
 open Lean
 
 /-- `Bench` is a structure with the data used to compute the `!bench` summary.
 It contains
-* a string `file` (that could be `build`);
+* a string `file` (that could be `build`, `lint` or `~Mathlib.Path.To.File`);
 * an integer `diff` representing the change in number of instructions for `file`;
 * a float `reldiff` representing the percentage change in number of instructions for `file`.
 -/
@@ -28,123 +43,86 @@ def intDecs (z : Int) (exp : Nat := 9) (prec : Nat := 3) : String × Int × Nat 
   let idec := z / p10
   (if sgn < 0 then "-" else "+", idec / (10 ^ prec), (idec % 10 ^ prec).toNat)
 
-/-- uses `intDecs` to format an integer as `±x.y⬝10⁹`. -/
+/-- `formatDiff z` uses `intDecs` to format the integer `z` as `±x.y⬝10⁹`. -/
 def formatDiff (z : Int) : String :=
   let (sgn, intDigs, decDigs) := intDecs z
   s!"{sgn}{intDigs}.{decDigs}⬝10⁹"
 
-/-- uses `intDecs` to format an integer as `±x.y%`. -/
-def formatPerc (z : Int) (exp : Nat := 9) (prec : Nat := 3) : String :=
+/-- `formatPercentAux z exp prec` uses `intDecs` to format an integer as `±x.y%`.
+This is used in `formatPercent`.
+-/
+def formatPercentAux (z : Int) (exp : Nat := 9) (prec : Nat := 3) : String :=
   let (sgn, intDigs, decDigs) := intDecs z exp prec
   s!"({sgn}{intDigs}.{decDigs}%)"
 
-/-- converts a `Bench.reldiff` into a formatted string of the form `±z.w%`. -/
+/-- converts a `Float` into a formatted string of the form `±z.w%`. -/
 def formatPercent (reldiff : Float) : String :=
+  -- shift by `2` twice: the first one, to get a `%`; the second, for 2 decimal digits of precision
   let reldiff := reldiff * 10 ^ 4
   let (sgn : Int) := if reldiff < 0 then -1 else 1
   let (sgnf : Float) := if reldiff < 0 then -1 else 1
   let reldiff := sgnf * reldiff
-  formatPerc (sgn * reldiff.toUInt32.val) 0 2
+  formatPercentAux (sgn * reldiff.toUInt32.val) 0 2
 
+#guard
+  let floats : Array Float := #[0, 1/7, 0.002]
+  floats.map formatPercent == #["(+0.0%)", "(+14.28%)", "(+0.20%)"]
+
+/-- converts a `String` into a formatted string of the form `` `file``, removing leading
+non-letters. -/
 def formatFile (file : String) : String :=
   "`" ++ file.dropWhile (!·.isAlpha) ++ "`"
 
 /-- converts a `Bench` into a formatted string of the form `| file | ±x.y⬝10⁹ | ±z.w% |`. -/
 def summary (bc : Bench) : String :=
-  let reldiff := bc.reldiff --* 10 ^ 4
-  --let (sgn : Int) := if reldiff < 0 then -1 else 1
+  let reldiff := bc.reldiff
   let (sgnf : Float) := if reldiff < 0 then -1 else 1
   let middle := [formatFile bc.file, formatDiff bc.diff, formatPercent (sgnf * reldiff)]
   "|".intercalate (""::middle ++ [""])
 
-def formatArrayBench (bound : Int) (arr : Array Bench) : String :=
-  if arr.size ≤ 2 then
-    "\n".intercalate (arr.map summary).toList
-    --"".intercalate (files.map fun (b, f) => s!"|{f.file}|{b}|{f.reldiff}|").toList
-  else
-    let fils := s!"<details><summary>{arr.size} files </summary>{", ".intercalate (arr.toList.map (s!"{·.file.dropWhile (!·.isAlpha)}"))}</details>"
-    let files := arr.map (s!"{bound}·10⁹", ·)
-    "|<" ++ (summary {file := fils, diff := bound*10^9, reldiff := default}).drop 1
-    --s!"|<details><summary>{arr.size} files </summary>{", ".intercalate (files.toList.map (fun (_, f) => s!"{f.file}, {f.reldiff}%"))}</details>|~{bound}||"
-
+/-- formats an array of `Bench`es into an md-table whose columns are `File`, `Instructions` and `%`.
+A typical entry may look like ``|`Mathlib.Analysis.Seminorm`|+2.509⬝10⁹|(+1.41%)|``
+-/
 def tableArrayBench (arr : Array Bench) : String :=
   let header := "|File|Instructions|%|\n|-|-:|:-:|"
   "\n".intercalate (header :: (arr.map summary).toList)
+
+def summaryTable (arr : Array Bench) (roundDiff : Int) : String :=
+  s!"<details><summary>{arr.size} files, Instructions {formatDiff <| roundDiff / (10 ^ 9) * 10 ^ 9}\
+    </summary>\n\n{tableArrayBench (arr.qsort (·.diff > ·.diff))}\n</details>\n"
 
 /-- Assuming that the input is a `json`-string formatted to produce an array of `Bench`,
 `benchOutput` prints the "significant" changes in numbers of instructions. -/
 def benchOutput (js : System.FilePath) : IO Unit := do
   let dats ← IO.ofExcept (Json.parse (← IO.FS.readFile js) >>= fromJson? (α := Array Bench))
-  --let dats := dats.push {file := "neg1", diff := -10^10, reldiff := -0.5}
-  --let dats := dats.push {file := "neg2", diff := -10^11, reldiff := -0.3}
+  -- `head` contains `build` and `lint`, `dats` contains the filenames, prefixed by `~`
   let (head, dats) := dats.partition (·.file.take 1 != "~")
-  let (pos, neg) := dats.partition (0 < ·.diff)
-  let grouped := dats.groupByKey (·.diff / (10 ^ 9))
-  --let mut (tallied) := (#[])
-  --for gp@(_i, arr) in grouped do
-    --if (arr.getD 0 default).file == "build" then
-    --  head := #[gp] ++ head
-    --else
-    --if (arr.getD 0 default).file == "lint" then
-    --  head := head.push gp
-    --else
-  --    tallied := tallied.push gp
-  let sortHead := (head.qsort (·.file < ·.file)).map (0, #[·])
-  let togetherSorted := sortHead ++ grouped.toArray.qsort (·.1 > ·.1)
-  let ts1 := togetherSorted.toList.groupBy (·.2.size == 1 && ·.2.size == 1)
+  -- gather together `Bench`es into subsets each containing `Bench`es with difference of
+  -- instructions in a `[n·10⁹, (n+1)·10⁹)` range
+  let grouped := ((dats.groupByKey (·.diff / (10 ^ 9))).toArray.qsort (·.1 > ·.1)).toList
+  -- we sort `build` and `lint` and consider them as their own individual groups
+  let sortHead := (head.qsort (·.file < ·.file)).toList.map (0, #[·])
+  let togetherSorted := sortHead ++ grouped
+  -- for better formatting, we collapse consecutive entries with singleton *file* entries
+  -- into a single entry. This covers the two steps `ts1` and `ts2`.
+  -- E.g. `[..., (bound₁, #[a₁]), (bound₂, #[a₂]), (bound₃, #[a₃]), ...]` collapses to
+  -- `[..., (none, #[a₁, a₂, a₃]), ...]`.
+  -- The `boundᵢ` entry become `none` for the collapsed entries, so that we know that these
+  -- should be printed individually instead of inside a `<details><summary>`-block.
+  let ts1 := togetherSorted.groupBy (·.2.size == 1 && ·.2.size == 1)
   let ts2 := List.join <| ts1.map fun l =>
     if (l.getD 0 default).2.size == 1 then
       [(none, l.foldl (· ++ ·.2) #[])]
-    else l.map fun (n, ar) => (some n, ar)
-/-
-  for (roundedDiff, g) in togetherSorted do
-    --let instrs := (g.getD 0 default).diff / 10^9
-    --let (s, a) :=
-    --  match g with
-    --    | #[g] => (g.file.dropWhile (!·.isAlpha), [formatDiff g.diff, formatPercent g.reldiff])
-    --    | _ => (s!"{g.size} files", [formatDiff <| roundedDiff * 10 ^ 9])
-    let entry :=
-      match g with
-        | #[g] => (formatFile g.file ++ s!", Instructions {", ".intercalate [formatDiff g.diff, formatPercent (g.reldiff)]}\n")
-        | _ => s!"<details><summary>{g.size} files, Instructions {formatDiff <| roundedDiff * 10 ^ 9}</summary>\n\n{tableArrayBench (g.qsort (·.diff > ·.diff))}\n</details>"
--/
-  for (roundedDiff, gs) in ts2 do
-    --let instrs := (g.getD 0 default).diff / 10^9
-    --let (s, a) :=
-    --  match g with
-    --    | #[g] => (g.file.dropWhile (!·.isAlpha), [formatDiff g.diff, formatPercent g.reldiff])
-    --    | _ => (s!"{g.size} files", [formatDiff <| roundedDiff * 10 ^ 9])
-    let entry :=
-      match roundedDiff with
-        | none => tableArrayBench gs
-        | some roundedDiff => s!"<details><summary>{gs.size} files, Instructions {formatDiff <| roundedDiff * 10 ^ 9}</summary>\n\n" ++ tableArrayBench (gs.qsort (·.diff > ·.diff)) ++ "\n</details>\n"
-        --(s!"{g.size} files", [formatDiff <| roundedDiff * 10 ^ 9])
-    --let summ := s!"<summary>{s}, Instructions {", ".intercalate a}</summary>"
-    --let indentTable := (tableArrayBench g).replace "\n" "\n  "
-    --IO.println <| s!"\n* <details>{summ}\n\n  {indentTable}\n  </details>"
-    IO.println <| entry
-  IO.println <| s!"\n* <details><summary>{grouped.size} files</summary>\n\n  ".intercalate (""::((grouped.toArray.map Prod.snd).map tableArrayBench).toList.map (·.replace "\n" "\n  " ++ "\n  </details>"))
-  let head := (head.qsort (·.file < ·.file)).map fun d => (d.diff / (10 ^ 9), #[d])
-  let withSize := head ++ grouped.toArray.qsort (·.1 > ·.1)
-  let fmt := withSize.map (fun (a, b) => formatArrayBench a b)
-  let header := "|File|Instructions|%|\n|-|-:|:-:|"
-  IO.println <| "\n".intercalate (header :: fmt.toList)
-  --for (i, arr) in withSize do
-  --  dbg_trace (i, arr.map (·.file))
-  let mut msg := #["Report", s!"{header}\n|`Increase`|||"]
-  --let mut msg' := #["Report", s!"{header}\n|`Increase`|||"]
-  for d in pos.qsort (·.diff > ·.diff) do
-    msg := msg.push (summary d)
-  msg := msg.push s!"|`Decrease`|||"
-  for d in neg.qsort (·.diff < ·.diff) do
-    msg := msg.push (summary d)
-  --IO.println ("\n".intercalate msg.toList)
+    else
+      l.map fun (n, ar) => (some n, ar)
+  for (bound, gs) in ts2 do
+    IO.println <|
+      match bound with
+        | none => -- `bound = none`: these entries are "singleton" files in their range
+          tableArrayBench gs
+        | some roundedDiff => -- these instead should be a collapsible summary
+          summaryTable gs roundedDiff
 
-#eval #[0, 1, -1, -2].qsort (· > ·)
-#eval #["~M", "b"].qsort (· > ·)
-#check List.groupBy
+#eval benchOutput "benchOutput.json"
 
-#eval
-  benchOutput "benchOutput.json"
-#check List.groupBy
-#check Array.groupByKey
+end BenchAction
