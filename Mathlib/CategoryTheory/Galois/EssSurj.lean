adb9b74cb74b63e3d1029ccf570eb30b2c00cd6c
Christian Merten <christian@merten.dev>
2024-09-04 16:58:06 +0200

    initial setup

---
+++ Mathlib/CategoryTheory/Galois/EssSurj.lean
@@ -0,0 +1,31 @@
+/-
+Copyright (c) 2024 Christian Merten. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Christian Merten
+-/
+import Mathlib.Algebra.Category.Grp.Limits
+import Mathlib.CategoryTheory.CofilteredSystem
+import Mathlib.CategoryTheory.Galois.Decomposition
+import Mathlib.CategoryTheory.Limits.FunctorCategory.Basic
+import Mathlib.CategoryTheory.Limits.IndYoneda
+import Mathlib.CategoryTheory.Limits.Preserves.Ulift
+
+/-!
+
+# Essential surjectivity of fiber functors
+
+-/
+
+universe u₁ u₂ w
+
+namespace CategoryTheory
+
+namespace PreGaloisCategory
+
+open Limits Functor
+
+
+
+end PreGaloisCategory
+
+end CategoryTheory
