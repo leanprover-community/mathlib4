bb1c5e49436550203e0b19c1f6ae3b7199113049
Michael Rothgang <rothgami@math.hu-berlin.de>
2024-09-04 10:58:55 +0200

    Further: rename old variant to be clearly deprecated; rename some fields. s.f cannot be renamed to map...

--- Mathlib/Geometry/Manifold/Bordisms.lean
+++ Mathlib/Geometry/Manifold/Bordisms.lean
@@ -16,9 +16,7 @@ open FiniteDimensional Set
 
 noncomputable section
 
-section temp
-
-structure SingularNManifoldDummy (X : Type*) [TopologicalSpace X] (n : ℕ) where
+structure SingularNManifold (X : Type*) [TopologicalSpace X] (n : ℕ) where
   E : Type*
   [normedAddCommGroup : NormedAddCommGroup E]
   [normedSpace: NormedSpace ℝ E]
@@ -27,39 +25,39 @@ structure SingularNManifoldDummy (X : Type*) [TopologicalSpace X] (n : ℕ) wher
   H : Type*
   [topSpaceH : TopologicalSpace H]
   [chartedSpace : ChartedSpace H M]
-  I : ModelWithCorners ℝ E H
-  [smoothMfd : SmoothManifoldWithCorners I M]
+  model : ModelWithCorners ℝ E H
+  [smoothMfd : SmoothManifoldWithCorners model M]
   [compactSpace : CompactSpace M]
-  [boundaryless: BoundarylessManifold I M]
+  [boundaryless: BoundarylessManifold model M]
   [findim: FiniteDimensional ℝ E]
-  [hdim : finrank ℝ E = n]
+  [dimension : finrank ℝ E = n]
   /-- The underlying map `M → X` of a singular `n`-manifold `(M,f)` on `X` -/
   f : M → X
   hf : Continuous f
 
 variable {X Y Z : Type*} [TopologicalSpace X] [TopologicalSpace Y] [TopologicalSpace Z]
 
-instance {n : ℕ} {s : SingularNManifoldDummy X n} : NormedAddCommGroup s.E := s.normedAddCommGroup
+instance {n : ℕ} {s : SingularNManifold X n} : NormedAddCommGroup s.E := s.normedAddCommGroup
 
-instance {n : ℕ} {s : SingularNManifoldDummy X n} : NormedSpace ℝ s.E := s.normedSpace
+instance {n : ℕ} {s : SingularNManifold X n} : NormedSpace ℝ s.E := s.normedSpace
 
-instance {n : ℕ} {s : SingularNManifoldDummy X n} : TopologicalSpace s.M := s.topSpaceM
+instance {n : ℕ} {s : SingularNManifold X n} : TopologicalSpace s.M := s.topSpaceM
 
-instance {n : ℕ} {s : SingularNManifoldDummy X n} : TopologicalSpace s.H := s.topSpaceH
+instance {n : ℕ} {s : SingularNManifold X n} : TopologicalSpace s.H := s.topSpaceH
 
-instance {n : ℕ} {s : SingularNManifoldDummy X n} : ChartedSpace s.H s.M := s.chartedSpace
+instance {n : ℕ} {s : SingularNManifold X n} : ChartedSpace s.H s.M := s.chartedSpace
 
-instance {n : ℕ} {s : SingularNManifoldDummy X n} : SmoothManifoldWithCorners s.I s.M := s.smoothMfd
+instance {n : ℕ} {s : SingularNManifold X n} : SmoothManifoldWithCorners s.model s.M := s.smoothMfd
 
-instance {n : ℕ} {s : SingularNManifoldDummy X n} : CompactSpace s.M := s.compactSpace
+instance {n : ℕ} {s : SingularNManifold X n} : CompactSpace s.M := s.compactSpace
 
-instance {n : ℕ} {s : SingularNManifoldDummy X n} : BoundarylessManifold s.I s.M := s.boundaryless
+instance {n : ℕ} {s : SingularNManifold X n} : BoundarylessManifold s.model s.M := s.boundaryless
 
-instance {n : ℕ} {s : SingularNManifoldDummy X n} : FiniteDimensional ℝ s.E := s.findim
+instance {n : ℕ} {s : SingularNManifold X n} : FiniteDimensional ℝ s.E := s.findim
 
-instance {n : ℕ} {s : SingularNManifoldDummy X n} : finrank ℝ s.E = n := s.hdim
+instance {n : ℕ} {s : SingularNManifold X n} : finrank ℝ s.E = n := s.dimension
 
-namespace SingularNManifoldDummy
+namespace SingularNManifold
 
 -- Let M, M' and W be smooth manifolds.
 variable {E E' E'' E''' H H' H'' H''' : Type*} [NormedAddCommGroup E] [NormedSpace ℝ E]
@@ -76,58 +74,53 @@ variable {M : Type*} [TopologicalSpace M] [ChartedSpace H M]
 
 variable (M) in
 /-- If `M` is `n`-dimensional and closed, it is a singular `n`-manifold over itself. -/
-noncomputable def refl (hdim : finrank ℝ E = n) : SingularNManifoldDummy M n where
+noncomputable def refl (hdim : finrank ℝ E = n) : SingularNManifold M n where
   H := H
-  I := I
-  hdim := hdim
+  model := I
+  dimension := hdim
   f := id
   hf := continuous_id
 
 /-- A map of topological spaces induces a corresponding map of singular n-manifolds. -/
 -- This is part of proving functoriality of the bordism groups.
-noncomputable def map (s : SingularNManifoldDummy X n)
-    {φ : X → Y} (hφ : Continuous φ) : SingularNManifoldDummy Y n where
-  E := s.E
-  H := s.H
-  I := s.I
+noncomputable def map (s : SingularNManifold X n)
+    {φ : X → Y} (hφ : Continuous φ) : SingularNManifold Y n where
+  model := s.model
   f := φ ∘ s.f
   hf := hφ.comp s.hf
-  hdim := s.hdim
+  dimension := s.dimension
 
 @[simp]
-lemma map_f (s : SingularNManifoldDummy X n) {φ : X → Y} (hφ : Continuous φ) :
+lemma map_f (s : SingularNManifold X n) {φ : X → Y} (hφ : Continuous φ) :
     (s.map hφ).f = φ ∘ s.f :=
   rfl
 
-lemma map_comp (s : SingularNManifoldDummy X n)
+lemma map_comp (s : SingularNManifold X n)
     {φ : X → Y} {ψ : Y → Z} (hφ : Continuous φ) (hψ : Continuous ψ) :
     ((s.map hφ).map hψ).f = (ψ ∘ φ) ∘ s.f := by
   simp [Function.comp_def]
   rfl
 
-/-- If `(M', f)` is a singular `n`-manifold on `X` and `M'` another `n`-dimensional smooth manifold,
-a smooth map `φ : M → M'` induces a singular `n`-manifold structure `(M, f ∘ φ)` on `X`. -/
-noncomputable def comap [Fact (finrank ℝ E = n)]
-    (s : SingularNManifoldDummy X n)
-    {φ : s.M → M'} (hφ : Smooth s.I I' φ) : SingularNManifoldDummy X n where
-  E := s.E
-  M := s.M
-  H := s.H
-  I := s.I
-  f := s.f ∘ φ
-  hf := s.hf.comp hφ.continuous
+-- TODO: fix these two!
+-- /-- If `(M', f)` is a singular `n`-manifold on `X` and `M'` another `n`-dimensional smooth manifold,
+-- a smooth map `φ : M → M'` induces a singular `n`-manifold structure `(M, f ∘ φ)` on `X`. -/
+-- noncomputable def comap [Fact (finrank ℝ E = n)]
+--     (s : SingularNManifoldDummy X n)
+--     {φ : s.M → M'} (hφ : Smooth s.I I' φ) : SingularNManifoldDummy X n where
+--   E := s.E
+--   M := s.M
+--   H := s.H
+--   I := s.I
+--   f := s.f ∘ φ
+--   hf := s.hf.comp hφ.continuous
 
-@[simp]
-lemma comap_f [Fact (finrank ℝ E = n)]
-    (s : SingularNManifold X n M' I') {φ : M → M'} (hφ : Smooth I I' φ) :
-    (s.comap hφ).f = s.f ∘ φ :=
-  rfl
+-- @[simp]
+-- lemma comap_f [Fact (finrank ℝ E = n)]
+--     (s : SingularNManifold X n M' I') {φ : M → M'} (hφ : Smooth I I' φ) :
+--     (s.comap hφ).f = s.f ∘ φ :=
+--   rfl
 
-end SingularNManifoldDummy
-
-end temp
-
-#exit
+end SingularNManifold
 
 -- Let M, M' and W be smooth manifolds.
 variable {E E' E'' E''' H H' H'' H''' : Type*} [NormedAddCommGroup E] [NormedSpace ℝ E]
@@ -140,7 +133,7 @@ variable {X Y Z : Type*} [TopologicalSpace X] [TopologicalSpace Y] [TopologicalS
 
 /-- A **singular `n`-manifold** on a topological space `X` consists of a
 closed smooth `n`-manifold `M` and a continuous map `f : M → X`. -/
-structure SingularNManifold (X : Type*) [TopologicalSpace X] (n : ℕ)
+structure SingularNManifoldOld (X : Type*) [TopologicalSpace X] (n : ℕ)
     (M : Type*) [TopologicalSpace M] [ChartedSpace H M]
     (I : ModelWithCorners ℝ E H) [SmoothManifoldWithCorners I M]
     [CompactSpace M] [BoundarylessManifold I M] [FiniteDimensional ℝ E] where
@@ -149,7 +142,7 @@ structure SingularNManifold (X : Type*) [TopologicalSpace X] (n : ℕ)
   f : M → X
   hf : Continuous f
 
-namespace SingularNManifold
+namespace SingularNManifoldOld
 
 -- We declare these variables *after* the definition above, so `SingularNManifold` can have
 -- its current order of arguments.
@@ -161,27 +154,25 @@ variable {M : Type*} [TopologicalSpace M] [ChartedSpace H M]
   [BoundarylessManifold I' M'] [CompactSpace M'] [FiniteDimensional ℝ E']
 
 /-- If `M` is `n`-dimensional and closed, it is a singular `n`-manifold over itself. -/
-noncomputable def refl (hdim : finrank ℝ E = n) : SingularNManifold M n M I where
+noncomputable def refl (hdim : finrank ℝ E = n) : SingularNManifoldOld M n M I where
   hdim := Fact.mk hdim
   f := id
   hf := continuous_id
 
 /-- A map of topological spaces induces a corresponding map of singular n-manifolds. -/
 -- This is part of proving functoriality of the bordism groups.
-noncomputable def map [Fact (finrank ℝ E = n)] (s : SingularNManifold X n M I)
-    {φ : X → Y} (hφ : Continuous φ) : SingularNManifold Y n M I where
+noncomputable def map [Fact (finrank ℝ E = n)] (s : SingularNManifoldOld X n M I)
+    {φ : X → Y} (hφ : Continuous φ) : SingularNManifoldOld Y n M I where
   f := φ ∘ s.f
   hf := hφ.comp s.hf
 
 
-#exit
-
 @[simp]
 lemma map_f [Fact (finrank ℝ E = n)]
-    (s : SingularNManifold X n M I) {φ : X → Y} (hφ : Continuous φ) : (s.map hφ).f = φ ∘ s.f :=
+    (s : SingularNManifoldOld X n M I) {φ : X → Y} (hφ : Continuous φ) : (s.map hφ).f = φ ∘ s.f :=
   rfl
 
-lemma map_comp [Fact (finrank ℝ E = n)] (s : SingularNManifold X n M I)
+lemma map_comp [Fact (finrank ℝ E = n)] (s : SingularNManifoldOld X n M I)
     {φ : X → Y} {ψ : Y → Z} (hφ : Continuous φ) (hψ : Continuous ψ) :
     ((s.map hφ).map hψ).f = (ψ ∘ φ) ∘ s.f := by
   simp [Function.comp_def]
@@ -189,28 +180,28 @@ lemma map_comp [Fact (finrank ℝ E = n)] (s : SingularNManifold X n M I)
 /-- If `(M', f)` is a singular `n`-manifold on `X` and `M'` another `n`-dimensional smooth manifold,
 a smooth map `φ : M → M'` induces a singular `n`-manifold structure on `M`. -/
 noncomputable def comap [Fact (finrank ℝ E = n)]
-    (s : SingularNManifold X n M' I')
-    {φ : M → M'} (hφ : Smooth I I' φ) : SingularNManifold X n M I where
+    (s : SingularNManifoldOld X n M' I')
+    {φ : M → M'} (hφ : Smooth I I' φ) : SingularNManifoldOld X n M I where
   f := s.f ∘ φ
   hf := s.hf.comp hφ.continuous
 
 @[simp]
 lemma comap_f [Fact (finrank ℝ E = n)]
-    (s : SingularNManifold X n M' I') {φ : M → M'} (hφ : Smooth I I' φ) :
+    (s : SingularNManifoldOld X n M' I') {φ : M → M'} (hφ : Smooth I I' φ) :
     (s.comap hφ).f = s.f ∘ φ :=
   rfl
 
 variable (M) in
 /-- The canonical singular `n`-manifold associated to the empty set (seen as an `n`-dimensional
 manifold, i.e. modelled on an `n`-dimensional space). -/
-def empty [Fact (finrank ℝ E = n)] [IsEmpty M] : SingularNManifold X n M I where
+def empty [Fact (finrank ℝ E = n)] [IsEmpty M] : SingularNManifoldOld X n M I where
   f := fun x ↦ (IsEmpty.false x).elim
   hf := by
     rw [continuous_iff_continuousAt]
     exact fun x ↦ (IsEmpty.false x).elim
 
 /-- An `n`-dimensional manifold induces a singular `n`-manifold on the one-point space. -/
-def trivial [Fact (finrank ℝ E = n)] : SingularNManifold PUnit n M I where
+def trivial [Fact (finrank ℝ E = n)] : SingularNManifoldOld PUnit n M I where
   f := fun _ ↦ PUnit.unit
   hf := continuous_const
 
@@ -219,9 +210,9 @@ is a singular `n+m`-manifold. -/
 -- FUTURE: prove that this observation inducess a commutative ring structure
 -- on the unoriented bordism group `Ω_n^O = Ω_n^O(pt)`.
 def prod {m n : ℕ} [h : Fact (finrank ℝ E = m)] [k : Fact (finrank ℝ E' = n)] :
-    SingularNManifold PUnit (m + n) (M × M') (I.prod I') where
+    SingularNManifoldOld PUnit (m + n) (M × M') (I.prod I') where
   f := fun _ ↦ PUnit.unit
   hf := continuous_const
   hdim := Fact.mk (by rw [finrank_prod, h.out, k.out])
 
-end SingularNManifold
+end SingularNManifoldOld
