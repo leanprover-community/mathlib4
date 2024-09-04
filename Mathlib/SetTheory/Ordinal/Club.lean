2504ba3785b1800490a69df8eddafe5d6457d333
YnirPaz <paznir83@gmail.com>
2024-09-04 21:41:04 +0300

    missed comment

--- Mathlib/SetTheory/Ordinal/Club.lean
+++ Mathlib/SetTheory/Ordinal/Club.lean
@@ -3,7 +3,6 @@ Copyright (c) 2024 Nir Paz. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Nir Paz
 -/
---import Mathlib.SetTheory.Cardinal.Cofinality
 import Mathlib.SetTheory.Cardinal.Cofinality
 
 /-!
