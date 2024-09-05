a0b335093634a94fea125e693bd03b96188cc9eb
Joachim Breitner <mail@joachim-breitner.de>
2024-09-05 11:22:45 +0200

    Code golf

--- Mathlib/Order/KrullDimension.lean
+++ Mathlib/Order/KrullDimension.lean
@@ -138,15 +138,12 @@ In a maximally long series, i.e one as long as the height of the last element, t
 element is its index in the series.
 -/
 lemma height_eq_index_of_length_eq_height_last (p : LTSeries α) (h : p.length = height p.last)
-    (i : Fin (p.length + 1)) :  height (p i) = i := by
-  suffices height (p i) ≤ i by
-    apply_rules [le_antisymm, index_le_height]
-  apply height_le
+    (i : Fin (p.length + 1)) : height (p i) = i := by
+  refine le_antisymm (height_le ?_) (index_le_height p i)
   intro p' hp'
-  simp only [Nat.cast_le]
   have hp'' := length_le_height_last (p := p'.smash (p.drop i) (by simpa))
   simp [← h] at hp''; clear h
-  norm_cast at hp''
+  norm_cast at *
   omega
 
 lemma height_mono : Monotone (α := α) height :=
