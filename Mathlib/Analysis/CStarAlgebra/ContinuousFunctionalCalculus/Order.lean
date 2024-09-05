f866c2ac2da988d25fdb795dcdac7ef77a04f38b
Jireh Loreaux <loreaujy@gmail.com>
2024-09-05 01:57:35 +0000

    feat: in a C⋆-algebra, `b⁻¹ ≤ a⁻¹ ↔ a ≤ b` for nonnegative `a b : Aˣ` (#16364)

--- Mathlib/Analysis/CStarAlgebra/ContinuousFunctionalCalculus/Order.lean
+++ Mathlib/Analysis/CStarAlgebra/ContinuousFunctionalCalculus/Order.lean
@@ -5,8 +5,9 @@ Authors: Frédéric Dupuis
 -/
 
 import Mathlib.Analysis.CStarAlgebra.ContinuousFunctionalCalculus.Instances
-import Mathlib.Topology.ContinuousFunction.StarOrdered
 import Mathlib.Analysis.CStarAlgebra.Unitization
+import Mathlib.Analysis.SpecialFunctions.ContinuousFunctionalCalculus.Rpow
+import Mathlib.Topology.ContinuousFunction.StarOrdered
 
 /-! # Facts about star-ordered rings that depend on the continuous functional calculus
 
@@ -27,6 +28,7 @@ the spectral order.
   `‖a‖ ≤ ‖b‖`.
 * `CStarRing.conjugate_le_norm_smul`: in a non-unital C⋆-algebra, we have that
   `star a * b * a ≤ ‖b‖ • (star a * a)` (and a primed version for the `a * b * star a` case).
+* `CStarRing.inv_le_inv_iff`: in a unital C⋆-algebra, `b⁻¹ ≤ a⁻¹` iff `a ≤ b`.
 
 ## Tags
 
@@ -188,6 +190,122 @@ lemma nnnorm_le_natCast_iff_of_nonneg (a : A) (n : ℕ) (ha : 0 ≤ a := by cfc_
   simpa using nnnorm_le_iff_of_nonneg a n
 
 end CStarRing
+
+section Inv
+
+open CFC
+
+variable [PartialOrder A] [StarOrderedRing A]
+
+-- TODO : relate everything in this section to strict positivity
+
+lemma CFC.conjugate_rpow_neg_one_half {a : A} (h₀ : IsUnit a) (ha : 0 ≤ a := by cfc_tac) :
+    a ^ (-(1 / 2) : ℝ) * a * a ^ (-(1 / 2) : ℝ) = 1 := by
+  lift a to Aˣ using h₀
+  nth_rw 2 [← rpow_one (a : A)]
+  simp only [← rpow_add (a.zero_not_mem_spectrum ℝ≥0)]
+  norm_num
+  exact rpow_zero _
+
+/-- In a unital C⋆-algebra, if `a` is nonnegative and invertible, and `a ≤ b`, then `b` is
+invertible. -/
+lemma CStarRing.isUnit_of_le {a b : A} (h₀ : IsUnit a) (ha : 0 ≤ a := by cfc_tac)
+    (hab : a ≤ b) : IsUnit b := by
+  rw [← spectrum.zero_not_mem_iff ℝ≥0] at h₀ ⊢
+  nontriviality A
+  have hb := (show 0 ≤ a from ha).trans hab
+  have ha' := IsSelfAdjoint.of_nonneg ha |>.spectrum_nonempty
+  have hb' := IsSelfAdjoint.of_nonneg hb |>.spectrum_nonempty
+  rw [zero_not_mem_iff, SpectrumRestricts.nnreal_lt_iff (.nnreal_of_nonneg ‹_›),
+    NNReal.coe_zero, ← CFC.exists_pos_algebraMap_le_iff ‹_›] at h₀ ⊢
+  peel h₀ with r hr _
+  exact this.trans hab
+
+lemma le_iff_norm_sqrt_mul_rpow {a b : A} (hbu : IsUnit b) (ha : 0 ≤ a) (hb : 0 ≤ (b : A)) :
+    a ≤ b ↔ ‖sqrt a * (b : A) ^ (-(1 / 2) : ℝ)‖ ≤ 1 := by
+  lift b to Aˣ using hbu
+  have hbab : 0 ≤ (b : A) ^ (-(1 / 2) : ℝ) * a * (b : A) ^ (-(1 / 2) : ℝ) :=
+    conjugate_nonneg_of_nonneg ha rpow_nonneg
+  conv_rhs =>
+    rw [← sq_le_one_iff (norm_nonneg _), sq, ← CStarRing.norm_star_mul_self, star_mul,
+      IsSelfAdjoint.of_nonneg sqrt_nonneg, IsSelfAdjoint.of_nonneg rpow_nonneg,
+      ← mul_assoc, mul_assoc _ _ (sqrt a), sqrt_mul_sqrt_self a,
+      CStarRing.norm_le_one_iff_of_nonneg _ hbab]
+  refine ⟨fun h ↦ ?_, fun h ↦ ?_⟩
+  · calc
+      _ ≤ ↑b ^ (-(1 / 2) : ℝ) * (b : A) * ↑b ^ (-(1 / 2) : ℝ) :=
+        IsSelfAdjoint.of_nonneg rpow_nonneg |>.conjugate_le_conjugate h
+      _ = 1 := conjugate_rpow_neg_one_half b.isUnit
+  · calc
+      a = (sqrt ↑b * ↑b ^ (-(1 / 2) : ℝ)) * a * (↑b ^ (-(1 / 2) : ℝ) * sqrt ↑b) := by
+        simp only [CFC.sqrt_eq_rpow .., ← CFC.rpow_add (b.zero_not_mem_spectrum ℝ≥0)]
+        norm_num
+        simp [CFC.rpow_zero (b : A)]
+      _ = sqrt ↑b * (↑b ^ (-(1 / 2) : ℝ) * a * ↑b ^ (-(1 / 2) : ℝ)) * sqrt ↑b := by
+        simp only [mul_assoc]
+      _ ≤ b := conjugate_le_conjugate_of_nonneg h sqrt_nonneg |>.trans <| by
+        simp [CFC.sqrt_mul_sqrt_self (b : A)]
+
+lemma le_iff_norm_sqrt_mul_sqrt_inv {a : A} {b : Aˣ} (ha : 0 ≤ a) (hb : 0 ≤ (b : A)) :
+    a ≤ b ↔ ‖sqrt a * sqrt (↑b⁻¹ : A)‖ ≤ 1 := by
+  rw [CFC.sqrt_eq_rpow (a := (↑b⁻¹ : A)), ← CFC.rpow_neg_one_eq_inv b,
+    CFC.rpow_rpow (b : A) _ _ (by simp) (by norm_num), le_iff_norm_sqrt_mul_rpow b.isUnit ha hb]
+  norm_num
+
+/-- In a unital C⋆-algebra, if `0 ≤ a ≤ b` and `a` and `b` are units, then `b⁻¹ ≤ a⁻¹`. -/
+protected lemma CStarRing.inv_le_inv {a b : Aˣ} (ha : 0 ≤ (a : A))
+    (hab : (a : A) ≤ b) : (↑b⁻¹ : A) ≤ a⁻¹ := by
+  have hb := ha.trans hab
+  have hb_inv : (0 : A) ≤ b⁻¹ := inv_nonneg_of_nonneg b hb
+  have ha_inv : (0 : A) ≤ a⁻¹ := inv_nonneg_of_nonneg a ha
+  rw [le_iff_norm_sqrt_mul_sqrt_inv ha hb, ← sq_le_one_iff (norm_nonneg _), sq,
+    ← CStarRing.norm_star_mul_self] at hab
+  rw [le_iff_norm_sqrt_mul_sqrt_inv hb_inv ha_inv, inv_inv, ← sq_le_one_iff (norm_nonneg _), sq,
+    ← CStarRing.norm_self_mul_star]
+  rwa [star_mul, IsSelfAdjoint.of_nonneg sqrt_nonneg,
+    IsSelfAdjoint.of_nonneg sqrt_nonneg] at hab ⊢
+
+/-- In a unital C⋆-algebra, if `0 ≤ a` and `0 ≤ b` and `a` and `b` are units, then `a⁻¹ ≤ b⁻¹`
+if and only if `b ≤ a`. -/
+protected lemma CStarRing.inv_le_inv_iff {a b : Aˣ} (ha : 0 ≤ (a : A)) (hb : 0 ≤ (b : A)) :
+    (↑a⁻¹ : A) ≤ b⁻¹ ↔ (b : A) ≤ a :=
+  ⟨CStarRing.inv_le_inv (inv_nonneg_of_nonneg a ha), CStarRing.inv_le_inv hb⟩
+
+lemma CStarRing.inv_le_iff {a b : Aˣ} (ha : 0 ≤ (a : A)) (hb : 0 ≤ (↑b : A)) :
+    (↑a⁻¹ : A) ≤ b ↔ (↑b⁻¹ : A) ≤ a := by
+  simpa using CStarRing.inv_le_inv_iff ha (inv_nonneg_of_nonneg b hb)
+
+lemma CStarRing.le_inv_iff {a b : Aˣ} (ha : 0 ≤ (a : A)) (hb : 0 ≤ (↑b : A)) :
+    a ≤ (↑b⁻¹ : A) ↔ b ≤ (↑a⁻¹ : A) := by
+  simpa using CStarRing.inv_le_inv_iff (inv_nonneg_of_nonneg a ha) hb
+
+lemma CStarRing.one_le_inv_iff_le_one {a : Aˣ} (ha : 0 ≤ (a : A)) :
+    1 ≤ (↑a⁻¹ : A) ↔ a ≤ 1 := by
+  simpa using CStarRing.le_inv_iff (a := 1) (by simp) ha
+
+lemma CStarRing.inv_le_one_iff_one_le {a : Aˣ} (ha : 0 ≤ (a : A)) :
+    (↑a⁻¹ : A) ≤ 1 ↔ 1 ≤ a := by
+  simpa using CStarRing.inv_le_iff ha (b := 1) (by simp)
+
+lemma CStarRing.inv_le_one {a : Aˣ} (ha : 1 ≤ a) : (↑a⁻¹ : A) ≤ 1 :=
+  CStarRing.inv_le_one_iff_one_le (zero_le_one.trans ha) |>.mpr ha
+
+lemma CStarRing.le_one_of_one_le_inv {a : Aˣ} (ha : 1 ≤ (↑a⁻¹ : A)) : (a : A) ≤ 1 := by
+  simpa using CStarRing.inv_le_one ha
+
+lemma CStarRing.rpow_neg_one_le_rpow_neg_one {a b : A} (ha : 0 ≤ a) (hab : a ≤ b) (hau : IsUnit a) :
+    b ^ (-1 : ℝ) ≤ a ^ (-1 : ℝ) := by
+  lift b to Aˣ using isUnit_of_le hau ha hab
+  lift a to Aˣ using hau
+  rw [rpow_neg_one_eq_inv a ha, rpow_neg_one_eq_inv b (ha.trans hab)]
+  exact CStarRing.inv_le_inv ha hab
+
+lemma CStarRing.rpow_neg_one_le_one {a : A} (ha : 1 ≤ a) : a ^ (-1 : ℝ) ≤ 1 := by
+  lift a to Aˣ using isUnit_of_le isUnit_one zero_le_one ha
+  rw [rpow_neg_one_eq_inv a (zero_le_one.trans ha)]
+  exact inv_le_one ha
+
+end Inv
 
 end CStar_unital
 
