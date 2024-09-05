31488dbe9290bad857b83e2b385c55ccdddeabfa
Christopher Hoskin <christopher.hoskin@overleaf.com>
2024-09-05 18:22:30 +0100

    Import CompleteLattice

--- Mathlib/Order/ScottContinuity.lean
+++ Mathlib/Order/ScottContinuity.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Christopher Hoskin
 -/
 import Mathlib.Order.Bounds.Basic
+import Mathlib.Order.CompleteLattice
 
 /-!
 # Scott continuity
