1b995d237c44a0691ae3792fe6ec4124f6321a40
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2024-09-05 04:39:04 +0000

    chore: split Analysis.Normed.Field.Basic (#16479)

--- Mathlib/Topology/MetricSpace/Polish.lean
+++ Mathlib/Topology/MetricSpace/Polish.lean
@@ -3,11 +3,12 @@ Copyright (c) 2022 Sébastien Gouëzel. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Sébastien Gouëzel
 -/
+import Mathlib.Analysis.Normed.Field.Basic
+import Mathlib.Topology.Instances.Nat
 import Mathlib.Topology.MetricSpace.PiNat
 import Mathlib.Topology.MetricSpace.Isometry
 import Mathlib.Topology.MetricSpace.Gluing
 import Mathlib.Topology.Sets.Opens
-import Mathlib.Analysis.Normed.Field.Basic
 
 /-!
 # Polish spaces
