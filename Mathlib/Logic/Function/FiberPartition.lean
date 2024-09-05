1586dcba7a1a8961a8d7d28b2044797d946092b5
Dagur Asgeirsson <dagurtomas@gmail.com>
2024-09-05 09:50:40 +0000

    Update Mathlib/Logic/Function/FiberPartition.lean
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- Mathlib/Logic/Function/FiberPartition.lean
+++ Mathlib/Logic/Function/FiberPartition.lean
@@ -20,7 +20,7 @@ variable {X Y Z : Type*}
 namespace Function
 
 /-- The indexing set of the partition. -/
-def Fibers (f : Y → Z) : Type _ := Set.range (fun (x : Set.range f) ↦ f ⁻¹' {x.val})
+def Fiber (f : Y → Z) : Type _ := Set.range (fun (x : Set.range f) ↦ f ⁻¹' {x.val})
 
 namespace Fibers
 
