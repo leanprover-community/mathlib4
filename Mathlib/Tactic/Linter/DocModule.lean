47bd8c13ee7d3c25753259fc8f4dfdccb4f80027
adomani <adomani@gmail.com>
2024-09-06 09:16:39 +0200

    somewhat works

--- Mathlib/Tactic/Linter/DocModule.lean
+++ Mathlib/Tactic/Linter/DocModule.lean
@@ -5,6 +5,7 @@ Authors: Damiano Testa
 -/
 import Lean.Linter.Util
 import Mathlib.Util.AssertExists
+import Mathlib.adomaniLeanUtils.inspect_syntax
 
 /-!
 #  The "header" linter
@@ -38,9 +39,14 @@ and nothing else.
 -/
 def onlyImportsModDocsAsserts : Syntax → Bool
   | .node _ ``Lean.Parser.Module.module #[_header, .node _ `null args] =>
-    let dropDocs := args.toList.dropWhile (·.isOfKind ``Lean.Parser.Command.moduleDoc)
-    let dropAssertNotExists := dropDocs.dropWhile (·.isOfKind ``commandAssert_not_exists_)
-    dropAssertNotExists.isEmpty
+    let args := args.toList.dropWhile (·.isOfKind ``Lean.Parser.Command.section)
+    let first := args.getD 0 default
+
+    dbg_trace first.getKind
+    first.isOfKind ``Lean.Parser.Command.moduleDoc
+    --let dropDocs := args.toList.dropWhile (·.isOfKind ``Lean.Parser.Command.moduleDoc)
+    --let dropAssertNotExists := dropDocs.dropWhile (·.isOfKind ``commandAssert_not_exists_)
+    --dropAssertNotExists.isEmpty
   | _=> false
 
 /-- `parseUpToHere stx post` takes as input a `Syntax` `stx` and a `String` `post`.
@@ -51,7 +57,7 @@ for instance in the presence of `#guard_msgs in` or `set_option ... in`.
 -/
 def parseUpToHere (stx : Syntax) (post : String := "") : CommandElabM Syntax := do
   let fm ← getFileMap
-  let startPos := stx.getPos?.getD default
+  let startPos := stx.getTailPos?.getD default
   let upToHere : Substring:= { str := fm.source, startPos := ⟨0⟩, stopPos := startPos}
   -- append a further string after the `upToHere` content
   Parser.testParseModule (← getEnv) "linter.header" (upToHere.toString ++ post)
@@ -76,24 +82,44 @@ register_option linter.style.header : Bool := {
   descr := "enable the header linter"
 }
 
+initialize firstCommand : IO.Ref (Option String.Pos) ← IO.mkRef none
+
+
 namespace Style.AssertNotExists
 
 /-- Gets the value of the `linter.header` option. -/
 def getLinterAssertNotExists (o : Options) : Bool :=
   Linter.getLinterValue linter.style.header o
-
+#check Syntax.getTrailingSize
 @[inherit_doc Mathlib.Linter.linter.style.header]
 def headerLinter : Linter where run := withSetOptionIn fun stx ↦ do
   unless getLinterAssertNotExists (← getOptions) do
     return
   if (← MonadState.get).messages.hasErrors then
     return
-  unless stx.isOfKind ``commandAssert_not_exists_ do return
-  let upToStx ← parseUpToHere stx "\nassert_not_exists XXX" <|> return Syntax.missing
-  if ! onlyImportsModDocsAsserts upToStx then
-    Linter.logLint linter.style.header stx
-      m!"`{stx}` appears too late: it can only be preceded by `import` statements \
-      doc-module strings and other `assert_not_exists` statements."
+  --unless stx.isOfKind ``commandAssert_not_exists_ do return
+  --dbg_trace "Starting position of syntax: {stx.getPos?}"
+  let mut firstPos ← firstCommand.get
+  --dbg_trace "Current value of ref: {firstPos}"
+  let offset : String.Pos := ⟨3⟩
+  if firstPos.isNone then
+    let upToStx ← parseUpToHere stx <|> return Syntax.missing
+    --dbg_trace "trailingSize: {upToStx.getTrailingSize}"
+    Meta.inspect upToStx
+
+    firstCommand.set upToStx.getTailPos?
+    --dbg_trace "New value of ref: {upToStx.getTailPos?}"
+    firstPos := upToStx.getTailPos?
+  if stx.getPos?.getD 0 ≤ firstPos.getD 0 + offset then
+    firstCommand.set stx.getPos?
+    let upToStx ← parseUpToHere stx <|> return Syntax.missing
+    if ! onlyImportsModDocsAsserts upToStx then
+      Linter.logLint linter.style.header stx
+        m!"`{stx}` appears too late: it can only be preceded by `import` statements \
+        doc-module strings and other `assert_not_exists` statements."
+    else return
+--  else
+
 
 initialize addLinter headerLinter
 
