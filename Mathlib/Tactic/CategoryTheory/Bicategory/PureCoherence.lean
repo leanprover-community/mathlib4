02550ada638c8beec6ad681046f76b712a8d6dfc
Yuma Mizuno <mizuno.y.aj@gmail.com>
2024-09-06 17:04:22 +0100

    Update PureCoherence.lean

--- Mathlib/Tactic/CategoryTheory/Bicategory/PureCoherence.lean
+++ Mathlib/Tactic/CategoryTheory/Bicategory/PureCoherence.lean
@@ -693,7 +693,6 @@ def isComp₁? (e : Expr) : BicategoryM (Option (Mor₁ × Mor₁)) := do
 
 /-- Construct a `Mor₁` expression from a Lean expression. -/
 partial def mor₁OfExpr (e : Expr) : BicategoryM Mor₁ := do
-  let ctx ← read
   if let some f := (← get).cache.find? e then
     return f
   let f ←
