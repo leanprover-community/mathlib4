71196a542237ba30da272b525131f9afaff4a0fb
Michael Rothgang <rothgami@math.hu-berlin.de>
2024-09-06 22:48:40 +0200

    Last edits, and add a docstring.

--- Mathlib/Geometry/Manifold/Bordisms.lean
+++ Mathlib/Geometry/Manifold/Bordisms.lean
@@ -7,7 +7,39 @@ import Mathlib.Geometry.Manifold.InteriorBoundary
 import Mathlib.Geometry.Manifold.ContMDiff.Defs
 
 /-!
-TODO docstring
+## (Unoriented) bordism theory
+
+This file defines the beginnings of (unoriented) bordism theory. For the full definition,
+a number of prerequisites are missing from mathlib, but a surprising amount of progress
+can already be made today.
+
+Currently, this file only contains the definition of *singular *n*-manifolds*:
+bordism classes are the equivalence classes of singular n-manifolds w.r.t. the (co)bordism relation.
+
+## Main definitions
+
+- **SingularNManifold**: a singular `n`-manifold on a topological space `X`, for `n ∈ ℕ`,
+is a pair `(M,f)` of a closed `n`-dimensional manifold `M` together with a continuous map `M → X`.
+- `SingularNManifold.map`: a map `X → Y` of topological spaces induces a map between the spaces
+of singular n-manifolds
+- `SingularNManifold.comap`: if `(N,f)` is a singular n-manifold on `X` and `φ: M → N` is smooth,
+the `comap` of `(N,f)` and `φ` is the induced singular n-manifold `(M, f ∘ φ)` on `X`.
+- `SingularNManifold.empty`: the empty set `M`, viewed as an `n`-manifold,
+as a singular `n`-manifold over any space `X`
+- `SingularNManifold.trivial`: an `n`-dimensional manifold induces a singular `n`-manifold
+on the one-point space.
+- `SingularNManifold.prod`: the product of a singular `n`-manifold and a singular `m`-manifold
+on the one-point space, is a singular `n+m`-manifold on the one-point space.
+
+## TODO
+- define cobordisms and the cobordism relation
+- prove that the cobordisms relation is an equivalence relation
+- define unoriented bordisms groups (as a set of equivalence classes),
+prove they are a group
+- prove that unoriented bordism groups define an extraordinary homology theory
+
+## Tags
+singular n-manifold, cobordism
 
 -/
 
@@ -103,24 +135,24 @@ noncomputable def refl (hdim : finrank ℝ E = n) : SingularNManifold M n where
   f := id
   hf := continuous_id
 
--- TODO: fix these two!
--- /-- If `(M', f)` is a singular `n`-manifold on `X` and `M'` another `n`-dimensional smooth manifold,
--- a smooth map `φ : M → M'` induces a singular `n`-manifold structure `(M, f ∘ φ)` on `X`. -/
--- noncomputable def comap [Fact (finrank ℝ E = n)]
---     (s : SingularNManifoldDummy X n)
---     {φ : s.M → M'} (hφ : Smooth s.I I' φ) : SingularNManifoldDummy X n where
---   E := s.E
---   M := s.M
---   H := s.H
---   I := s.I
---   f := s.f ∘ φ
---   hf := s.hf.comp hφ.continuous
+/-- If `(N, f)` is a singular `n`-manifold on `X` and `M` another `n`-dimensional smooth manifold,
+a smooth map `φ : M → N` induces a singular `n`-manifold structure `(M, f ∘ φ)` on `X`. -/
+noncomputable def comap [h: Fact (finrank ℝ E = n)]
+    (s : SingularNManifold X n)
+    {φ : M → s.M} (hφ : Smooth I s.model φ) : SingularNManifold X n where
+  E := E
+  M := M
+  H := H
+  model := I
+  f := s.f ∘ φ
+  hf := s.hf.comp hφ.continuous
+  dimension := h.out
 
--- @[simp]
--- lemma comap_f [Fact (finrank ℝ E = n)]
---     (s : SingularNManifold X n M' I') {φ : M → M'} (hφ : Smooth I I' φ) :
---     (s.comap hφ).f = s.f ∘ φ :=
---   rfl
+@[simp]
+lemma comap_f [Fact (finrank ℝ E = n)]
+    (s : SingularNManifold X n) {φ : M → s.M} (hφ : Smooth I s.model φ) :
+    (s.comap hφ).f = s.f ∘ φ :=
+  rfl
 
 variable (M) in
 /-- The canonical singular `n`-manifold associated to the empty set (seen as an `n`-dimensional
@@ -138,6 +170,7 @@ def empty [h: Fact (finrank ℝ E = n)] (M : Type*) [TopologicalSpace M] [Charte
     rw [continuous_iff_continuousAt]
     exact fun x ↦ (IsEmpty.false x).elim
 
+variable (M) in
 /-- An `n`-dimensional manifold induces a singular `n`-manifold on the one-point space. -/
 def trivial [h: Fact (finrank ℝ E = n)] : SingularNManifold PUnit n where
   E := E
@@ -160,52 +193,3 @@ def prod {m n : ℕ} (s : SingularNManifold PUnit n) (t : SingularNManifold PUni
   dimension := by rw [finrank_prod, s.dimension, t.dimension]
 
 end SingularNManifold
-
-#exit
--- Let M, M' and W be smooth manifolds.
-variable {E E' E'' E''' H H' H'' H''' : Type*} [NormedAddCommGroup E] [NormedSpace ℝ E]
-  [NormedAddCommGroup E'] [NormedSpace ℝ E'] [NormedAddCommGroup E'']  [NormedSpace ℝ E'']
-  [NormedAddCommGroup E'''] [NormedSpace ℝ E''']
-  [TopologicalSpace H] [TopologicalSpace H'] [TopologicalSpace H''] [TopologicalSpace H''']
-
-variable {X Y Z : Type*} [TopologicalSpace X] [TopologicalSpace Y] [TopologicalSpace Z]
-
-
-/-- A **singular `n`-manifold** on a topological space `X` consists of a
-closed smooth `n`-manifold `M` and a continuous map `f : M → X`. -/
-structure SingularNManifoldOld (X : Type*) [TopologicalSpace X] (n : ℕ)
-    (M : Type*) [TopologicalSpace M] [ChartedSpace H M]
-    (I : ModelWithCorners ℝ E H) [SmoothManifoldWithCorners I M]
-    [CompactSpace M] [BoundarylessManifold I M] [FiniteDimensional ℝ E] where
-  [hdim : Fact (finrank ℝ E = n)]
-  /-- The underlying map `M → X` of a singular `n`-manifold `(M,f)` on `X` -/
-  f : M → X
-  hf : Continuous f
-
-namespace SingularNManifoldOld
-
--- We declare these variables *after* the definition above, so `SingularNManifold` can have
--- its current order of arguments.
-variable {M : Type*} [TopologicalSpace M] [ChartedSpace H M]
-  {I : ModelWithCorners ℝ E H} [SmoothManifoldWithCorners I M]
-  {M' : Type*} [TopologicalSpace M'] [ChartedSpace H' M']
-  {I' : ModelWithCorners ℝ E' H'} [SmoothManifoldWithCorners I' M'] {n : ℕ}
-  [BoundarylessManifold I M] [CompactSpace M] [FiniteDimensional ℝ E]
-  [BoundarylessManifold I' M'] [CompactSpace M'] [FiniteDimensional ℝ E']
-
-
-/-- If `(M', f)` is a singular `n`-manifold on `X` and `M'` another `n`-dimensional smooth manifold,
-a smooth map `φ : M → M'` induces a singular `n`-manifold structure on `M`. -/
-noncomputable def comap [Fact (finrank ℝ E = n)]
-    (s : SingularNManifoldOld X n M' I')
-    {φ : M → M'} (hφ : Smooth I I' φ) : SingularNManifoldOld X n M I where
-  f := s.f ∘ φ
-  hf := s.hf.comp hφ.continuous
-
-@[simp]
-lemma comap_f [Fact (finrank ℝ E = n)]
-    (s : SingularNManifoldOld X n M' I') {φ : M → M'} (hφ : Smooth I I' φ) :
-    (s.comap hφ).f = s.f ∘ φ :=
-  rfl
-
-end SingularNManifoldOld
