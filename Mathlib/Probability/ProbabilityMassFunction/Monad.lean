cb193d1a7939bae004809efc545ab1993a7b7ef2
Kim Morrison <kim@tqft.net>
2024-09-06 17:37:59 +1000

    fixes

--- Mathlib/Probability/ProbabilityMassFunction/Monad.lean
+++ Mathlib/Probability/ProbabilityMassFunction/Monad.lean
@@ -222,7 +222,7 @@ theorem bindOnSupport_eq_bind (p : PMF α) (f : α → PMF β) :
     (p.bindOnSupport fun a _ => f a) = p.bind f := by
   ext b
   have : ∀ a, ite (p a = 0) 0 (p a * f a b) = p a * f a b :=
-    fun a => (ite_eq_right_iff _ _ _).2 fun h => h.symm ▸ symm (zero_mul <| f a b)
+    fun a => ite_eq_right_iff.2 fun h => h.symm ▸ symm (zero_mul <| f a b)
   simp only [bindOnSupport_apply fun a _ => f a, p.bind_apply f, dite_eq_ite, mul_ite,
     mul_zero, this]
 
