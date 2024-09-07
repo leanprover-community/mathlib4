f4b4fd77093865b9492e68f6460962cd8ab183f5
adomani <adomani@gmail.com>
2024-09-07 22:53:44 +0200

    some pre-linter adjustments

--- Mathlib/Init.lean
+++ Mathlib/Init.lean
@@ -6,6 +6,7 @@ import Mathlib.Tactic.Linter.OldObtain
 import Mathlib.Tactic.Linter.RefineLinter
 import Mathlib.Tactic.Linter.UnusedTactic
 import Mathlib.Tactic.Linter.Style
+import Mathlib.Tactic.Linter.DocModule
 
 /-!
 This is the root file in Mathlib: it is imported by virtually *all* Mathlib files.
