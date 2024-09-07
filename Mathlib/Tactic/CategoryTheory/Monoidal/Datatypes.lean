d5647491d5de8445de9912f5d3566480fbd80537
Yuma Mizuno <mizuno.y.aj@gmail.com>
2024-09-07 11:02:06 +0100

    Update Datatypes.lean

--- Mathlib/Tactic/CategoryTheory/Monoidal/Datatypes.lean
+++ Mathlib/Tactic/CategoryTheory/Monoidal/Datatypes.lean
@@ -106,8 +106,8 @@ theorem structuralIsoOfExpr_comp {f g h : C}
 
 theorem StructuralOfExpr_monoidalComp {f g h i : C} [MonoidalCoherence g h]
     (η : f ⟶ g) (η' : f ≅ g) (ih_η : η'.hom = η) (θ : h ⟶ i) (θ' : h ≅ i) (ih_θ : θ'.hom = θ) :
-    (monoidalIsoComp η' θ').hom = η ⊗≫ θ := by
-  simp [ih_η, ih_θ, monoidalIsoComp, monoidalComp, MonoidalCoherence.hom]
+    (η' ≪⊗≫ θ').hom = η ⊗≫ θ := by
+  simp [ih_η, ih_θ, monoidalIsoComp, monoidalComp, MonoidalCoherence.iso]
 
 variable [MonoidalCategory C]
 
