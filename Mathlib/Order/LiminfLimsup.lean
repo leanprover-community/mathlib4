7248e25fa23edb1a79c1b750d7d800067cba7e6a
Yury G. Kudryashov <urkud@urkud.name>
2024-09-08 12:00:21 -0500

    Update

--- Mathlib/Order/LiminfLimsup.lean
+++ Mathlib/Order/LiminfLimsup.lean
@@ -3,6 +3,7 @@ Copyright (c) 2018 Sébastien Gouëzel. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Sébastien Gouëzel, Johannes Hölzl, Rémy Degenne
 -/
+import Mathlib.Algebra.BigOperators.Group.Finset
 import Mathlib.Order.Filter.Cofinite
 import Mathlib.Order.Hom.CompleteLattice
 
