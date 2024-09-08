dd12236980e317f41b5bc297fdbf96f56be73122
Daniel Weber <commandmaster2017@gmail.com>
2024-09-08 12:47:00 +0000

    chore(Order/SuccPred/Basic): Move succ/pred being archimedean to a different file (#16559)
    Also prove `le_total_of_ge, le_total_of_le, lt_or_le_of_ge, lt_or_le_of_le`.

--- Mathlib.lean
+++ Mathlib.lean
@@ -3655,6 +3655,7 @@ import Mathlib.Order.ScottContinuity
 import Mathlib.Order.SemiconjSup
 import Mathlib.Order.SetNotation
 import Mathlib.Order.Sublattice
+import Mathlib.Order.SuccPred.Archimedean
 import Mathlib.Order.SuccPred.Basic
 import Mathlib.Order.SuccPred.CompleteLinearOrder
 import Mathlib.Order.SuccPred.IntervalSucc
