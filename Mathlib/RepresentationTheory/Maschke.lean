a6bc3d48e1e6ded1cea62574e231ab232f280a44
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2024-09-08 22:04:04 +0200

    x

--- Mathlib/RepresentationTheory/Maschke.lean
+++ Mathlib/RepresentationTheory/Maschke.lean
@@ -3,6 +3,7 @@ Copyright (c) 2020 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison
 -/
+import Mathlib.Algebra.Module.LinearMap.Ker
 import Mathlib.Algebra.MonoidAlgebra.Basic
 import Mathlib.LinearAlgebra.Basis.VectorSpace
 import Mathlib.RingTheory.SimpleModule
