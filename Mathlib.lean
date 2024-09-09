f3590789ad6c3ec9beac1b2ef53a7fddec8294f3
Yakov Pechersky <ffxen158@gmail.com>
2024-09-08 21:25:51 -0400

    mathlib

--- Mathlib.lean
+++ Mathlib.lean
@@ -4007,6 +4007,7 @@ import Mathlib.RingTheory.Unramified.Basic
 import Mathlib.RingTheory.Unramified.Derivations
 import Mathlib.RingTheory.Unramified.Finite
 import Mathlib.RingTheory.Valuation.AlgebraInstances
+import Mathlib.RingTheory.Valuation.Archimedean
 import Mathlib.RingTheory.Valuation.Basic
 import Mathlib.RingTheory.Valuation.ExtendToLocalization
 import Mathlib.RingTheory.Valuation.Integers
