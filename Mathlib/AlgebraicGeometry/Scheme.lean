ed1024a5578b3dce368266f70cb14fc6bf2540b7
Moritz Firsching <firsching@google.com>
2024-09-09 17:11:43 +0200

    chore(Mathlib.AlgebraicGeometry.Scheme): minimize imports

--- Mathlib/AlgebraicGeometry/Scheme.lean
+++ Mathlib/AlgebraicGeometry/Scheme.lean
@@ -4,8 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison
 -/
 import Mathlib.AlgebraicGeometry.Spec
-import Mathlib.Algebra.Category.Ring.Constructions
-import Mathlib.CategoryTheory.Elementwise
 
 /-!
 # The category of schemes
