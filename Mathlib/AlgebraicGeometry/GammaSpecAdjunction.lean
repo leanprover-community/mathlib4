00b7c04ef4e4d4e42e10306dc5309f2e3ad902f4
Moritz Firsching <firsching@google.com>
2024-09-09 17:41:19 +0200

    chore(Mathlib.AlgebraicGeometry.GammaSpecAdjunction): minimize imports

--- Mathlib/AlgebraicGeometry/GammaSpecAdjunction.lean
+++ Mathlib/AlgebraicGeometry/GammaSpecAdjunction.lean
@@ -4,8 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Junyan Xu
 -/
 import Mathlib.AlgebraicGeometry.Restrict
-import Mathlib.CategoryTheory.Adjunction.Limits
-import Mathlib.CategoryTheory.Adjunction.Reflective
 
 /-!
 # Adjunction between `Γ` and `Spec`
