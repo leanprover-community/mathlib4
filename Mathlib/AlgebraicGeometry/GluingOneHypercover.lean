7676314620db30557d3681c691efdb64d525dcfb
Moritz Firsching <firsching@google.com>
2024-09-09 17:12:39 +0200

    chore(Mathlib.AlgebraicGeometry.GluingOneHypercover): minimize imports

--- Mathlib/AlgebraicGeometry/GluingOneHypercover.lean
+++ Mathlib/AlgebraicGeometry/GluingOneHypercover.lean
@@ -3,9 +3,7 @@ Copyright (c) 2024 Joël Riou. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Calle Sönne, Joël Riou, Ravi Vakil
 -/
-import Mathlib.AlgebraicGeometry.Gluing
 import Mathlib.AlgebraicGeometry.Sites.BigZariski
-import Mathlib.CategoryTheory.Sites.OneHypercover
 
 /-!
 # The 1-hypercover of a glue data
