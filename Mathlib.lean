a0fe55214ef732804170035118bf631dda840886
Bjørn Kjos-Hanssen <bjoernkh@hawaii.edu>
2024-09-08 17:37:18 -1000

    Running lake exe mk_all

--- Mathlib.lean
+++ Mathlib.lean
@@ -3129,6 +3129,7 @@ import Mathlib.Logic.Small.Ring
 import Mathlib.Logic.Small.Set
 import Mathlib.Logic.Unique
 import Mathlib.Logic.UnivLE
+import Mathlib.Marginis.AhlmanKoponen2015
 import Mathlib.Marginis.Basic
 import Mathlib.Marginis.CalvertFranklin2015
 import Mathlib.Marginis.DienerHendtlass2020
