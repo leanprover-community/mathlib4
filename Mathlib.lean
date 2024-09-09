ea677731e393860b1b4ed24e4e448d4746398f10
Calle Sönne <calle.sonne@gmail.com>
2024-09-09 12:00:24 +0000

    feat(FiberedCategory/Fibered): define prefibered categories (#13416)
    We define the notion of prefibered categories, as in SGA 1 VI 6.1, and add some basic API.
    
    Co-authored-by: Paul Lezeau <paul.lezeau@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1481,6 +1481,7 @@ import Mathlib.CategoryTheory.Extensive
 import Mathlib.CategoryTheory.FiberedCategory.BasedCategory
 import Mathlib.CategoryTheory.FiberedCategory.Cartesian
 import Mathlib.CategoryTheory.FiberedCategory.Cocartesian
+import Mathlib.CategoryTheory.FiberedCategory.Fibered
 import Mathlib.CategoryTheory.FiberedCategory.HomLift
 import Mathlib.CategoryTheory.Filtered.Basic
 import Mathlib.CategoryTheory.Filtered.Connected
