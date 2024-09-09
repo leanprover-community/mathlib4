778342aa49b13f50ce2d44158501454f0b5e0e2d
Moritz Firsching <firsching@google.com>
2024-09-09 17:16:37 +0200

    chore(Mathlib.AlgebraicGeometry.Morphisms.UnderlyingMap): minimize imports

--- Mathlib/AlgebraicGeometry/Morphisms/UnderlyingMap.lean
+++ Mathlib/AlgebraicGeometry/Morphisms/UnderlyingMap.lean
@@ -3,8 +3,8 @@ Copyright (c) 2022 Andrew Yang. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Andrew Yang
 -/
-import Mathlib.Topology.LocalAtTarget
 import Mathlib.AlgebraicGeometry.Morphisms.Constructors
+import Mathlib.Topology.LocalAtTarget
 
 /-!
 
