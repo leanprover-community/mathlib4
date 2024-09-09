d1a4d55192bddc76b0d88826cbc5cd8fede0f9a3
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2024-09-09 23:02:52 +0200

    fix

--- Mathlib/NumberTheory/Transcendental/Lindemann/Init/AlgebraicPart.lean
+++ Mathlib/NumberTheory/Transcendental/Lindemann/Init/AlgebraicPart.lean
@@ -3,6 +3,7 @@ Copyright (c) 2022 Yuyang Zhao. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yuyang Zhao
 -/
+import Mathlib.Algebra.Group.UniqueProds.VectorSpace
 import Mathlib.Algebra.MonoidAlgebra.NoZeroDivisors
 import Mathlib.Analysis.Complex.Polynomial.Basic
 import Mathlib.Data.Complex.Exponential
