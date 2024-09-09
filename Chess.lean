4ad10cc7fc194ae6bffa9c35318413ddb8aa717f
Kim Morrison <kim@tqft.net>
2024-09-09 22:34:46 +1000

    .

--- Chess.lean
+++ Chess.lean
@@ -36,30 +36,36 @@ def toFin? (x : Int) : Option (Fin 8) :=
 
 namespace Option
 
-@[simp] theorem mem_ite_none_left {x : α} [Decidable p] {l : Option α} : (x ∈ if p then none else l) ↔ ¬ p ∧ x ∈ l := by
+@[simp] theorem mem_ite_none_left {x : α} [Decidable p] {l : Option α} :
+    (x ∈ if p then none else l) ↔ ¬ p ∧ x ∈ l := by
   split <;> simp_all
 
-@[simp] theorem mem_ite_none_right {x : α} [Decidable p] {l : Option α} : (x ∈ if p then l else none) ↔ p ∧ x ∈ l := by
+@[simp] theorem mem_ite_none_right {x : α} [Decidable p] {l : Option α} :
+    (x ∈ if p then l else none) ↔ p ∧ x ∈ l := by
   split <;> simp_all
 
 end Option
 
 namespace Array
 
-@[simp] theorem mem_ite_empty_left {x : α} [Decidable p] {l : Array α} : (x ∈ if p then #[] else l) ↔ ¬ p ∧ x ∈ l := by
+@[simp] theorem mem_ite_empty_left {x : α} [Decidable p] {l : Array α} :
+    (x ∈ if p then #[] else l) ↔ ¬ p ∧ x ∈ l := by
   split <;> simp_all
 
-@[simp] theorem mem_ite_empty_right {x : α} [Decidable p] {l : Array α} : (x ∈ if p then l else #[]) ↔ p ∧ x ∈ l := by
+@[simp] theorem mem_ite_empty_right {x : α} [Decidable p] {l : Array α} :
+    (x ∈ if p then l else #[]) ↔ p ∧ x ∈ l := by
   split <;> simp_all
 
 end Array
 
 namespace List
 
-@[simp] theorem mem_ite_nil_left {x : α} [Decidable p] {l : List α} : (x ∈ if p then [] else l) ↔ ¬ p ∧ x ∈ l := by
+@[simp] theorem mem_ite_nil_left {x : α} [Decidable p] {l : List α} :
+    (x ∈ if p then [] else l) ↔ ¬ p ∧ x ∈ l := by
   split <;> simp_all
 
-@[simp] theorem mem_ite_nil_right {x : α} [Decidable p] {l : List α} : (x ∈ if p then l else []) ↔ p ∧ x ∈ l := by
+@[simp] theorem mem_ite_nil_right {x : α} [Decidable p] {l : List α} :
+    (x ∈ if p then l else []) ↔ p ∧ x ∈ l := by
   split <;> simp_all
 
 -- Replace unprimed:
