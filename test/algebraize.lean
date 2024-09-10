6f40b86a23e2edcab1a686638ad53ff6ae734fa7
Calle Sönne <calle.sonne@gmail.com>
2024-09-10 12:19:15 +0100

    improve testing

--- test/algebraize.lean
+++ test/algebraize.lean
@@ -2,49 +2,81 @@ import Mathlib.RingTheory.Flat.Algebra
 import Mathlib.RingTheory.FinitePresentation
 import Mathlib.RingTheory.IntegralClosure.Algebra.Defs
 
+/-- Synthesize algebra instance from ring hom. -/
 example (A B : Type*) [CommRing A] [CommRing B] (f : A →+* B) : True := by
+  fail_if_success -- Check that this instance is not available by default
+    have h : Algebra A B := inferInstance
   algebraize f
-  have h : Algebra A B := inferInstance
-  guard_hyp h : Algebra A B
+  rename_i h₀
+  guard_hyp h₀ := f.toAlgebra
   trivial
 
+/-- Synthesize algebra instance from a composition -/
 example (A B C : Type*) [CommRing A] [CommRing B] [CommRing C] (f : A →+* B) (g : B →+* C) :
     True := by
-  algebraize f g (g.comp f)
-  have h₁ : Algebra A C := inferInstance
-  guard_hyp h₁ : Algebra A C
-  clear h₁
-  have h₂ : IsScalarTower A B C := inferInstance
-  guard_hyp h₂ : IsScalarTower A B C
+  fail_if_success -- Check that this instance is not available by default
+    have h : Algebra A C := inferInstance
+  algebraize (g.comp f)
+  rename_i h₀
+  guard_hyp h₀ := (g.comp f).toAlgebra
   trivial
 
+
+/-- Synthesize algebra instance and scalar tower instance from a composition -/
+example (A B C : Type*) [CommRing A] [CommRing B] [CommRing C] (f : A →+* B) (g : B →+* C) :
+    True := by
+  fail_if_success -- Check that this instance is not available by default
+    have h : IsScalarTower A B C := inferInstance
+  algebraize f g (g.comp f)
+  rename_i hT
+  guard_hyp hT := IsScalarTower.of_algebraMap_eq' rfl
+  trivial
+
+/-- Synthesize `Module.Finite` from morphism property. -/
 example (A B : Type*) [CommRing A] [CommRing B] (f : A →+* B) (hf : f.Finite) : True := by
+  fail_if_success -- Check that this instance is not available by default
+    have h : Module.Finite A B := inferInstance
   algebraize f
-  have h : Module.Finite A B := inferInstance
+  rename_i h
   guard_hyp h : Module.Finite A B
   trivial
 
+/-- Synthesize `Algebra.FiniteType` from morphism property. -/
 example (A B : Type*) [CommRing A] [CommRing B] (f : A →+* B) (hf : f.FiniteType) : True := by
+  fail_if_success -- Check that this instance is not available by default
+    have h : Algebra.FiniteType A B := inferInstance
   algebraize f
-  have h : Algebra.FiniteType A B := inferInstance
+  rename_i h
   guard_hyp h : Algebra.FiniteType A B
   trivial
 
+/-- Synthesize `Algebra.Flat` from morphism property. -/
 example (A B : Type*) [CommRing A] [CommRing B] (f : A →+* B) (hf : f.Flat) : True := by
+  fail_if_success -- Check that this instance is not available by default
+    have h : Algebra.Flat A B := inferInstance
   algebraize f
-  have h : Algebra.Flat A B := inferInstance
+  rename_i h
   guard_hyp h : Algebra.Flat A B
   trivial
 
+/-- Synthesize `Algebra.Integral` from morphism property. -/
 example (A B : Type*) [CommRing A] [CommRing B] (f : A →+* B) (hf : f.IsIntegral) : True := by
+  fail_if_success -- Check that this instance is not available by default
+    have h : Algebra.IsIntegral A B := inferInstance
   algebraize f
-  have h : Algebra.IsIntegral A B := inferInstance
+  rename_i h
   guard_hyp h : Algebra.IsIntegral A B
   trivial
 
+/-- Synthesize from morphism property of a composition (and check that tower is also synthesized). -/
 example (A B C : Type*) [CommRing A] [CommRing B] [CommRing C] (f : A →+* B) (g : B →+* C)
     (hfg : (g.comp f).Flat) : True := by
+  fail_if_success -- Check that this instance is not available by default
+    have h : Algebra.Flat A C := inferInstance
+  fail_if_success
+    have h : IsScalarTower A B C := inferInstance
   algebraize f g (g.comp f)
-  have h₁ : Algebra.Flat A C := inferInstance
+  rename_i h₂ h₁
   guard_hyp h₁ : Algebra.Flat A C
+  guard_hyp h₂ := IsScalarTower.of_algebraMap_eq' rfl
   trivial
