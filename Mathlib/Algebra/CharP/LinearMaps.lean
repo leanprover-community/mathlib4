1e8929df13f5ed46ab49597575c6633aaeea3072
Suzuka Yu <109365723+Yu-Misaka@users.noreply.github.com>
2024-09-10 14:50:36 +0800

    modifications on CharP/LinearMaps

--- Mathlib/Algebra/CharP/LinearMaps.lean
+++ Mathlib/Algebra/CharP/LinearMaps.lean
@@ -1,15 +1,47 @@
 /-
 Copyright (c) 2024 Wanyi He. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Wanyi He
+Authors: Wanyi He, Huanyu Zheng
 -/
 import Mathlib.Algebra.Ring.Subring.Basic
 import Mathlib.Algebra.CharP.Algebra
+
+variable {R M : Type*} [CommSemiring R] [AddCommMonoidWithOne M] [Module R M]
+
+instance char_eq_if {p : ℕ} [hchar : CharP R p]
+  (hconvert : ∀ r : R, r • (1 : M) = 0 → r = 0) : CharP (M →ₗ[R] M) p where
+  cast_eq_zero_iff' := by
+    intro n
+    replace hchar := hchar.1 n
+    have reduce : (n : M →ₗ[R] M) = (n : R) • 1 := by
+      simp only [Nat.cast_smul_eq_nsmul, nsmul_eq_mul, mul_one]
+    rw [reduce, LinearMap.ext_iff, hchar.symm]
+    simp only [LinearMap.smul_apply, LinearMap.one_apply, LinearMap.zero_apply]
+    refine ⟨fun h ↦ hconvert n <| h 1,
+      fun h ↦ (congrArg (fun t ↦ ∀ (x : M), t • x = 0) h).mpr fun x ↦ zero_smul R x⟩
 
 variable {D : Type*} [DivisionRing D]
 
 local notation "k" => (Subring.center D)
 
+instance : Module k (D →ₗ[k] D) := inferInstance
+
+instance : Algebra k D := Algebra.ofModule smul_mul_assoc mul_smul_comm
+
+theorem center_char_iff {p : ℕ} : CharP D p ↔ CharP k p :=
+  (@RingHom.charP_iff k D _ _ (algebraMap k D)
+    (NoZeroSMulDivisors.algebraMap_injective k D) p).symm
+
+instance {p : ℕ} [hchar : CharP D p] : CharP (D →ₗ[k] D) p := by
+  letI : CharP k p := center_char_iff.1 hchar
+  refine char_eq_if (p := p) (R := k) (M := D) ?_
+  intro r hr
+  have : r • (1 : D) = r := by
+    simp [(· • ·), SMul.smul]
+  rw [this] at hr
+  exact ZeroMemClass.coe_eq_zero.mp hr
+
+/-
 instance {p : ℕ} [CharP D p] : CharP (D →ₗ[k] D) p :=
   let f : D →+* (D →ₗ[k] D) := {
       toFun := fun a => {
@@ -30,3 +62,4 @@ instance {p : ℕ} [CharP D p] : CharP (D →ₗ[k] D) p :=
     rw [← eq x, ← eq y]
     exact congrFun (congrArg DFunLike.coe h) 1
   charP_of_injective_ringHom inj p
+  -/
