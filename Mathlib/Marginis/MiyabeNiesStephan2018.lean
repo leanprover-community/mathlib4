09bee949e560d4a261c97350a4d2c80857d39d46
Bjørn Kjos-Hanssen <bjoernkh@hawaii.edu>
2024-09-10 08:12:32 -1000

    Fix imports

--- Mathlib/Marginis/MiyabeNiesStephan2018.lean
+++ Mathlib/Marginis/MiyabeNiesStephan2018.lean
@@ -1,9 +1,9 @@
 /-
-Copyright (c) 2024 Bjørn Kjos-Hanssen· All rights reserved.
+Copyright (c) 2024 Bjørn Kjos-Hanssen. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Bjørn Kjos-Hanssen
 -/
-import Mathlib.MeasureTheory.Measure.Hausdorff
+import Mathlib.MeasureTheory.Measure.Haar.OfBasis
 
 /-!
 
