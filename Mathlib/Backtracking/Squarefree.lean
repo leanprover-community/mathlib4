99c6d404ea65d11a4dede4881da1b2b616002f2e
Bjørn Kjos-Hanssen <bjoernkh@hawaii.edu>
2024-09-10 11:33:47 -1000

    Fix squarefree imports

--- Mathlib/Backtracking/Squarefree.lean
+++ Mathlib/Backtracking/Squarefree.lean
@@ -1,5 +1,13 @@
+/-
+Copyright (c) 2024 Bjørn Kjos-Hanssen. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Bjørn Kjos-Hanssen
+-/
 import Mathlib.Data.Vector.Basic
-import Mathlib.Data.Nat.Digits
+import Mathlib.Algebra.Order.Ring.Int
+import Mathlib.Tactic.Linarith.Frontend
+import Mathlib.Tactic.Ring.Basic
+import Mathlib.Tactic.Zify
 import Mathlib.Data.Vector.Defs
 /-
 
