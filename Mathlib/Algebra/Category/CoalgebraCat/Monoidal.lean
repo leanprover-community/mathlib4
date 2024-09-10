baa2d613f9230d2b58ab2b9859fa809d8891464c
Amelia Livingston <al3717@ic.ac.uk>
2024-09-10 15:53:28 +0100

    fixes + style

--- Mathlib/Algebra/Category/CoalgebraCat/Monoidal.lean
+++ Mathlib/Algebra/Category/CoalgebraCat/Monoidal.lean
@@ -24,32 +24,35 @@ universe v u
 
 namespace CoalgebraCat
 variable (R : Type u) [CommRing R]
+
 open CategoryTheory Coalgebra
 open scoped TensorProduct MonoidalCategory
 
-@[simps] noncomputable instance instMonoidalCategoryStruct :
+@[simps]
+noncomputable instance instMonoidalCategoryStruct :
     MonoidalCategoryStruct.{u} (CoalgebraCat R) where
-  tensorObj := fun X Y => CoalgebraCat.of R (X ⊗[R] Y)
-  whiskerLeft := fun X _ _ f => CoalgebraCat.ofHom (f.1.lTensor X)
-  whiskerRight := fun f X => CoalgebraCat.ofHom (f.1.rTensor X)
-  tensorHom := fun f g => CoalgebraCat.ofHom (Coalgebra.TensorProduct.map f.1 g.1)
+  tensorObj X Y := of R (X ⊗[R] Y)
+  whiskerLeft X _ _ f := ofHom (f.1.lTensor X)
+  whiskerRight f X := ofHom (f.1.rTensor X)
+  tensorHom f g := ofHom (Coalgebra.TensorProduct.map f.1 g.1)
   tensorUnit := CoalgebraCat.of R R
-  associator := fun X Y Z => (Coalgebra.TensorProduct.assoc R X Y Z).toIso
-  leftUnitor := fun X => (Coalgebra.TensorProduct.lid R X).toIso
-  rightUnitor := fun X => (Coalgebra.TensorProduct.rid R X).toIso
+  associator X Y Z := (Coalgebra.TensorProduct.assoc R X Y Z).toCoalgebraCatIso
+  leftUnitor X := (Coalgebra.TensorProduct.lid R X).toCoalgebraCatIso
+  rightUnitor X := (Coalgebra.TensorProduct.rid R X).toCoalgebraCatIso
 
 /-- The data needed to induce a `MonoidalCategory` structure via
 `CoalgebraCat.instMonoidalCategoryStruct` and the forgetful functor to modules. -/
-@[simps] noncomputable def MonoidalCategory.inducingFunctorData :
+@[simps]
+noncomputable def MonoidalCategory.inducingFunctorData :
     Monoidal.InducingFunctorData (forget₂ (CoalgebraCat R) (ModuleCat R)) where
-  μIso := fun X Y => Iso.refl _
-  whiskerLeft_eq := fun X Y Z f => by ext; rfl
-  whiskerRight_eq := fun X f => by ext; rfl
-  tensorHom_eq := fun f g => by ext; rfl
+  μIso X Y := Iso.refl _
+  whiskerLeft_eq X Y Z f := by ext; rfl
+  whiskerRight_eq X f := by ext; rfl
+  tensorHom_eq f g := by ext; rfl
   εIso := Iso.refl _
-  associator_eq := fun X Y Z => TensorProduct.ext <| TensorProduct.ext <| by ext; rfl
-  leftUnitor_eq := fun X => TensorProduct.ext <| by ext; rfl
-  rightUnitor_eq := fun X => TensorProduct.ext <| by ext; rfl
+  associator_eq X Y Z := TensorProduct.ext <| TensorProduct.ext <| by ext; rfl
+  leftUnitor_eq X := TensorProduct.ext <| by ext; rfl
+  rightUnitor_eq X := TensorProduct.ext <| by ext; rfl
 
 noncomputable instance instMonoidalCategory : MonoidalCategory (CoalgebraCat R) :=
   Monoidal.induced (forget₂ _ (ModuleCat R)) (MonoidalCategory.inducingFunctorData R)
