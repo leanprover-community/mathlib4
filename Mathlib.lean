f70cb8c3970b0ca47d16468f17ba53d714343f08
Christian Merten <christian@merten.dev>
2024-09-10 09:38:05 +0000

    feat(CategoryTheory/Galois): any fiber functor of a Galois category is full (#16478)
    More precisely, for a fiber functor `F`, the induced functor to the category of finite `Aut F`-sets is full.

--- Mathlib.lean
+++ Mathlib.lean
@@ -1511,6 +1511,7 @@ import Mathlib.CategoryTheory.Functor.Trifunctor
 import Mathlib.CategoryTheory.Galois.Basic
 import Mathlib.CategoryTheory.Galois.Decomposition
 import Mathlib.CategoryTheory.Galois.Examples
+import Mathlib.CategoryTheory.Galois.Full
 import Mathlib.CategoryTheory.Galois.GaloisObjects
 import Mathlib.CategoryTheory.Galois.Prorepresentability
 import Mathlib.CategoryTheory.Galois.Topology
