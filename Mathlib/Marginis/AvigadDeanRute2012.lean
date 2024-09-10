ee5cc0ce6fb859819b5bf7984fbd767e9019ebd3
Bjørn Kjos-Hanssen <bjoernkh@hawaii.edu>
2024-09-09 18:07:07 -1000

    SemilatticeSup

--- Mathlib/Marginis/AvigadDeanRute2012.lean
+++ Mathlib/Marginis/AvigadDeanRute2012.lean
@@ -21,6 +21,49 @@ an arbitrary predicate `P x y` (in place of `|x - y| < ε`), over any type.
 
 open Classical
 
+/-- A 1-variable general version of metastability over an arbitrary preorder. -/
+theorem metastable_general₁' {R N : Type} [Preorder N] (P : R → Prop) (a : N → R) :
+    (∃ m, ∀ x ≥ m, P (a x)) ↔
+    ∀ F : N → N, ∃ m, ∀ x ∈ Set.Icc m (F m), P (a x) := by
+  constructor
+  · intro h F
+    obtain ⟨m,hm⟩ := h
+    use m
+    exact fun n hn => hm n hn.1
+  · intro h
+    contrapose h
+    push_neg at *
+    have φ : ∀ m : N, ∃ k : N, ∃ n ∈ Set.Icc m k, ¬ P (a n) := by
+      intro m
+      obtain ⟨n,hn⟩ := h m
+      exact ⟨n, n, ⟨ hn.1,le_refl _⟩, hn.2⟩
+    use (fun m => Classical.choose <| φ m)
+    intro m
+    obtain ⟨u,hu⟩ := Classical.choose_spec <| φ m
+    use u
+
+/-- 2-variable general metastability over an upper semilattice. -/
+theorem metastable_general₂' {R N : Type} [SemilatticeSup N]
+    (P : R → R → Prop) (a : N → R) :
+    (∃ m, ∀ x ≥ m, ∀ y ≥ m, P (a x) (a y)) ↔
+    ∀ F : N → N, ∃ m, ∀ x ∈ Set.Icc m (F m), ∀ y ∈ Set.Icc m (F m), P (a x) (a y) := by
+  constructor
+  · intro h F
+    obtain ⟨m,hm⟩ := h
+    use m
+    exact fun n hn n' hn' => hm n hn.1 n' hn'.1
+  · intro h
+    contrapose h
+    push_neg at *
+    have φ : ∀ m : N, ∃ k : N, ∃ n ∈ Set.Icc m k, ∃ n' ∈ Set.Icc m k, ¬ P (a n) (a n') :=
+      fun m => by
+      obtain ⟨n,hn⟩ := h m
+      obtain ⟨n',hn'⟩ := hn.2
+      exact ⟨ n ⊔ n', n, ⟨hn.1, by exact SemilatticeSup.le_sup_left n n'⟩,
+        ⟨n', by simp_all, hn'.2⟩⟩
+    exact ⟨fun m => Classical.choose <| φ m, fun m => Classical.choose_spec <| φ m⟩
+
+
 /-- A 1-variable general version of metastability. -/
 theorem metastable_general₁ {R : Type} (P : R → Prop) (a : ℕ → R) :
     (∃ m, ∀ x ≥ m, P (a x)) ↔
@@ -33,20 +76,11 @@ theorem metastable_general₁ {R : Type} (P : R → Prop) (a : ℕ → R) :
   · intro h
     contrapose h
     push_neg at *
-    have Q' : ∀ m : ℕ, ∃ k : ℕ, ∃ n ∈ Set.Icc m k, ¬ P (a n) := by
+    have φ : ∀ m : ℕ, ∃ k : ℕ, ∃ n ∈ Set.Icc m k, ¬ P (a n) := by
       intro m
-      have W := h m
-      let n := Nat.find W
-      have hn := Nat.find_spec W
-      use n, n
-      constructor
-      · simp only [Set.mem_Icc, le_refl, and_true];tauto
-      · exact (Nat.find_spec W).2
-    use fun m ↦ Nat.find <| Q' m
-    intro m
-    have W := Nat.find_spec <| Q' m
-    use Nat.find W
-    exact Nat.find_spec W
+      obtain ⟨n,hn⟩ := h m
+      exact ⟨n, n, by (simp only [Set.mem_Icc, le_refl, and_true];tauto), hn.2⟩
+    exact ⟨fun m => Nat.find <| φ m, fun m => Nat.find_spec <| φ m⟩
 
 
 /-- A 2-variable general version of metastability. -/
@@ -60,24 +94,13 @@ theorem metastable_general₂ {R : Type} (P : R → R → Prop) (a : ℕ → R) 
     exact fun n hn n' hn' => hm n hn.1 n' hn'.1
   · intro h
     contrapose h
-    push_neg
-    push_neg at h
-    have Q' : ∀ m : ℕ, ∃ k : ℕ, ∃ n ∈ Set.Icc m k, ∃ n' ∈ Set.Icc m k, ¬ P (a n) (a n') := by
-      intro m
-      have W := h m
-      let n := Nat.find W
-      have hn := Nat.find_spec W
-      let n' := Nat.find hn.2
-      use max n n', n
-      constructor
-      · exact ⟨hn.1, Nat.le_max_left n n'⟩
-      · use n'
-        exact ⟨⟨(Nat.find_spec hn.2).1, Nat.le_max_right n n'⟩, (Nat.find_spec hn.2).2⟩
-    use fun m ↦ Nat.find <| Q' m
-    intro m
-    have W := Nat.find_spec <| Q' m
-    use Nat.find W
-    exact Nat.find_spec W
+    push_neg at *
+    have φ : ∀ m : ℕ, ∃ k : ℕ, ∃ n ∈ Set.Icc m k, ∃ n' ∈ Set.Icc m k, ¬ P (a n) (a n') :=
+      fun m => by
+      obtain ⟨n,hn⟩ := h m
+      obtain ⟨n',hn'⟩ := hn.2
+      exact ⟨ max n n', n, ⟨hn.1, Nat.le_max_left n n'⟩, ⟨n', by simp_all, hn'.2⟩⟩
+    exact ⟨fun m => Nat.find <| φ m, fun m => Nat.find_spec <| φ m⟩
 
 /-- Equivalence between the two first displayed equations mentioned on
 page 2 of A metastable dominated convergence theorem by Jeremy Avigad, Edward T Dean, Jason Rute. -/
