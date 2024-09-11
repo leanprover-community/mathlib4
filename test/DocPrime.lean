5bd2aba4de11f7f2282a3d10fc3a9d8fccf3ee0d
adomani <adomani@gmail.com>
2024-09-11 09:19:50 +0200

    fix tests

--- test/DocPrime.lean
+++ test/DocPrime.lean
@@ -9,7 +9,7 @@ def X' := 0
 
 /--
 warning: `thm_no_doc'` is missing doc-string, please add one.
-Declarations whose name contains a `'` are expected to contain a justification for the presence of a `'` in their doc-string.
+Declarations whose name contains a `'` are expected to contain an explanation for the presence of a `'` in their doc-string. This may consist of discussion of the difference relative to the unprimed version, or an explanation as to why no better naming scheme is possible.
 note: this linter can be disabled with `set_option linter.docPrime false`
 -/
 #guard_msgs in
@@ -17,7 +17,7 @@ theorem thm_no_doc' : True := .intro
 
 /--
 warning: `thm_with_attr_no_doc'` is missing doc-string, please add one.
-Declarations whose name contains a `'` are expected to contain a justification for the presence of a `'` in their doc-string.
+Declarations whose name contains a `'` are expected to contain an explanation for the presence of a `'` in their doc-string. This may consist of discussion of the difference relative to the unprimed version, or an explanation as to why no better naming scheme is possible.
 note: this linter can be disabled with `set_option linter.docPrime false`
 -/
 #guard_msgs in
@@ -26,7 +26,7 @@ theorem thm_with_attr_no_doc' : True := .intro
 
 /--
 warning: `inst_no_doc'` is missing doc-string, please add one.
-Declarations whose name contains a `'` are expected to contain a justification for the presence of a `'` in their doc-string.
+Declarations whose name contains a `'` are expected to contain an explanation for the presence of a `'` in their doc-string. This may consist of discussion of the difference relative to the unprimed version, or an explanation as to why no better naming scheme is possible.
 note: this linter can be disabled with `set_option linter.docPrime false`
 -/
 #guard_msgs in
@@ -34,7 +34,7 @@ instance inst_no_doc' : True := .intro
 
 /--
 warning: `abbrev_no_doc'` is missing doc-string, please add one.
-Declarations whose name contains a `'` are expected to contain a justification for the presence of a `'` in their doc-string.
+Declarations whose name contains a `'` are expected to contain an explanation for the presence of a `'` in their doc-string. This may consist of discussion of the difference relative to the unprimed version, or an explanation as to why no better naming scheme is possible.
 note: this linter can be disabled with `set_option linter.docPrime false`
 -/
 #guard_msgs in
@@ -42,7 +42,7 @@ abbrev abbrev_no_doc' : True := .intro
 
 /--
 warning: `def_no_doc'` is missing doc-string, please add one.
-Declarations whose name contains a `'` are expected to contain a justification for the presence of a `'` in their doc-string.
+Declarations whose name contains a `'` are expected to contain an explanation for the presence of a `'` in their doc-string. This may consist of discussion of the difference relative to the unprimed version, or an explanation as to why no better naming scheme is possible.
 note: this linter can be disabled with `set_option linter.docPrime false`
 -/
 #guard_msgs in
