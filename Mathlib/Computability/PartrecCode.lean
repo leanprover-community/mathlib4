5e74d0cfedbdfc515724c643cabc026b4663325d
Kim Morrison <kim@tqft.net>
2024-09-11 14:40:34 +1000

    fix for leanprover/lean4#5300

--- Mathlib/Computability/PartrecCode.lean
+++ Mathlib/Computability/PartrecCode.lean
@@ -624,8 +624,8 @@ theorem evaln_mono : ∀ {k₁ k₂ c n x}, k₁ ≤ k₂ → x ∈ evaln k₁ c
     iterate 4 exact h
     · -- pair cf cg
       simp? [Seq.seq, Option.bind_eq_some] at h ⊢ says
-        simp only [Seq.seq, Option.map_eq_map, Option.mem_def, Option.bind_eq_some,
-          Option.map_eq_some', exists_exists_and_eq_and] at h ⊢
+        simp only [Seq.seq, Option.map_eq_map, Option.bind_map, Function.comp_apply,
+          Option.mem_def, Option.bind_eq_some, Option.map_eq_some'] at h ⊢
       exact h.imp fun a => And.imp (hf _ _) <| Exists.imp fun b => And.imp_left (hg _ _)
     · -- comp cf cg
       simp? [Bind.bind, Option.bind_eq_some] at h ⊢ says
