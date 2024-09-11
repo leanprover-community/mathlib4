e539ef5e3e478901e72ebee9d04cff74a38324b4
Yury G. Kudryashov <urkud@urkud.name>
2024-09-11 09:21:59 -0500

    Add to Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -3598,6 +3598,7 @@ import Mathlib.Order.Heyting.Hom
 import Mathlib.Order.Heyting.Regular
 import Mathlib.Order.Hom.Basic
 import Mathlib.Order.Hom.Bounded
+import Mathlib.Order.Hom.Circular
 import Mathlib.Order.Hom.CompleteLattice
 import Mathlib.Order.Hom.Lattice
 import Mathlib.Order.Hom.Order
