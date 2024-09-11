cff7fe751a1ea5c3c6ee37a1b35d738ad5042446
Calle Sönne <calle.sonne@gmail.com>
2024-09-11 21:55:56 +0100

    fix tests

--- test/algebraize.lean
+++ test/algebraize.lean
@@ -2,6 +2,46 @@ import Mathlib.RingTheory.Flat.Algebra
 import Mathlib.RingTheory.FinitePresentation
 import Mathlib.RingTheory.IntegralClosure.Algebra.Defs
 
+section example_definitions
+
+/-- Test property for when `RingHom` and `Algebra` properties are definitionally the same,
+see e.g. `RingHom.FiniteType` for a concrete example of this. -/
+class Algebra.testProperty1 (A B : Type*) [CommRing A] [CommRing B] [Algebra A B] : Prop where
+  out : ∀ x : A, algebraMap A B x = 0
+
+/-- Test property for when `RingHom` and `Algebra` properties are definitionally the same,
+see e.g. `RingHom.FiniteType` for a concrete example of this. -/
+@[algebraize]
+def RingHom.testProperty1 {A B : Type*} [CommRing A] [CommRing B] (f : A →+* B) : Prop :=
+  @Algebra.testProperty1 A B _ _ f.toAlgebra
+
+/-- Test property for when the `RingHom` porperty corresponds to a `Module` property (that is
+definitionally the same). See e.g. `Module.Finite` for a concrete example of this. -/
+class Module.testProperty2 (A M : Type*) [Semiring A] [AddCommMonoid M] [Module A M] : Prop where
+  out : ∀ x : A, ∀ M : M, x • M = 0
+
+/-- Test property for when the `RingHom` porperty corresponds to a `Module` property (that is
+definitionally the same). See e.g. `Module.Finite` for a concrete example of this. -/
+@[algebraize Module.testProperty2]
+def RingHom.testProperty2 {A B : Type*} [CommRing A] [CommRing B] (f : A →+* B) : Prop :=
+  letI : Algebra A B := f.toAlgebra
+  Module.testProperty2 A B
+
+/-- Test property for when the `RingHom` porperty corresponds to a `Algebra` property that is not
+definitionally the same, and needs to be created through a lemma. See e.g. `Algebra.IsIntegral` for
+an example. -/
+class Algebra.testProperty3 (A B : Type*) [CommRing A] [CommRing B] [Algebra A B] : Prop where
+  out : Algebra.testProperty1 A B
+
+/- Test property for when the `RingHom` porperty corresponds to a `Algebra` property that is not
+definitionally the same, and needs to be created through a lemma. See e.g. `Algebra.IsIntegral` for
+an example. -/
+@[algebraize Algebra.testProperty3.mk]
+def RingHom.testProperty3 {A B : Type*} [CommRing A] [CommRing B] (f : A →+* B) : Prop :=
+  f.testProperty1
+
+end example_definitions
+
 set_option tactic.hygienic false
 
 /-- Synthesize algebra instance from ring hom. -/
@@ -30,47 +70,30 @@ example (A B C : Type*) [CommRing A] [CommRing B] [CommRing C] (f : A →+* B) (
   guard_hyp scalarTowerInst := IsScalarTower.of_algebraMap_eq' rfl
   trivial
 
-/-- Synthesize `Module.Finite` from morphism property. -/
-example (A B : Type*) [CommRing A] [CommRing B] (f : A →+* B) (hf : f.Finite) : True := by
-  fail_if_success -- Check that this instance is not available by default
-    have h : Module.Finite A B := inferInstance
+example (A B : Type*) [CommRing A] [CommRing B] (f : A →+* B) (hf : f.testProperty1) : True := by
   algebraize f
-  guard_hyp algebraizeInst : Module.Finite A B
+  guard_hyp algebraizeInst : Algebra.testProperty1 A B
   trivial
 
-/-- Synthesize `Algebra.FiniteType` from morphism property. -/
-example (A B : Type*) [CommRing A] [CommRing B] (f : A →+* B) (hf : f.FiniteType) : True := by
-  fail_if_success -- Check that this instance is not available by default
-    have h : Algebra.FiniteType A B := inferInstance
+example (A B : Type*) [CommRing A] [CommRing B] (f : A →+* B) (hf : f.testProperty2) : True := by
   algebraize f
-  guard_hyp algebraizeInst : Algebra.FiniteType A B
+  guard_hyp algebraizeInst : Module.testProperty2 A B
   trivial
 
-/-- Synthesize `Algebra.Flat` from morphism property. -/
-example (A B : Type*) [CommRing A] [CommRing B] (f : A →+* B) (hf : f.Flat) : True := by
-  fail_if_success -- Check that this instance is not available by default
-    have h : Algebra.Flat A B := inferInstance
+example (A B : Type*) [CommRing A] [CommRing B] (f : A →+* B) (hf : f.testProperty3) : True := by
   algebraize f
-  guard_hyp algebraizeInst : Algebra.Flat A B
-  trivial
-
-/-- Synthesize `Algebra.Integral` from morphism property. -/
-example (A B : Type*) [CommRing A] [CommRing B] (f : A →+* B) (hf : f.IsIntegral) : True := by
-  fail_if_success -- Check that this instance is not available by default
-    have h : Algebra.IsIntegral A B := inferInstance
-  algebraize f
-  guard_hyp algebraizeInst : Algebra.IsIntegral A B
+  guard_hyp algebraizeInst : Algebra.testProperty3 A B
   trivial
 
 set_option tactic.hygienic false in
 /-- Synthesize from morphism property of a composition (and check that tower is also synthesized). -/
 example (A B C : Type*) [CommRing A] [CommRing B] [CommRing C] (f : A →+* B) (g : B →+* C)
-    (hfg : (g.comp f).Flat) : True := by
+    (hfg : (g.comp f).testProperty1) : True := by
   fail_if_success -- Check that this instance is not available by default
     have h : Algebra.Flat A C := inferInstance
   fail_if_success
     have h : IsScalarTower A B C := inferInstance
   algebraize f g (g.comp f)
-  guard_hyp algebraizeInst : Algebra.Flat A C
+  guard_hyp algebraizeInst : Algebra.testProperty1 A C
   guard_hyp scalarTowerInst := IsScalarTower.of_algebraMap_eq' rfl
   trivial
