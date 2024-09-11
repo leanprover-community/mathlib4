66e5867b00096b208a03fbd1a0c8fdb7fc62960e
Xavier Roblot <roblot@math.univ-lyon1.fr>
2024-09-11 17:46:09 +0200

    Shake

--- Mathlib/Analysis/BoxIntegral/UnitPartition.lean
+++ Mathlib/Analysis/BoxIntegral/UnitPartition.lean
@@ -3,7 +3,8 @@ Copyright (c) 2024 Xavier Roblot. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Xavier Roblot
 -/
-import Mathlib.Analysis.BoxIntegral.Integrability
+import Mathlib.Analysis.BoxIntegral.Partition.Measure
+import Mathlib.Analysis.BoxIntegral.Partition.Tagged
 
 /-!
 # Unit Partition
