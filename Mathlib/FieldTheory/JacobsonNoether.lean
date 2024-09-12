c51a117f2d42b1c758d1f97dbb42761997905f2d
yuanyi-350 <kysyy1@126.com>
2024-09-12 08:00:08 +0800

    Update JacobsonNoether.lean

--- Mathlib/FieldTheory/JacobsonNoether.lean
+++ Mathlib/FieldTheory/JacobsonNoether.lean
@@ -1,8 +1,7 @@
 /-
-Copyright (c) 2024 Filippo A. E. Nuccio, Huanyu Zheng, Wanyi He, Sihan Wu, Yi Yuan.
-All rights reserved.
+Copyright (c) 2024 F. Nuccio, H. Zheng, W. He, S. Wu, Y. Yuan, W. Jiao. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Filippo A. E. Nuccio, Huanyu Zheng, Sihan Wu, Wanyi He, Yi Yuan
+Authors: Filippo A. E. Nuccio, Huanyu Zheng, Sihan Wu, Wanyi He, Weichen Jiao, Yi Yuan
 -/
 import Mathlib.RingTheory.Algebraic
 import Mathlib.FieldTheory.Separable
