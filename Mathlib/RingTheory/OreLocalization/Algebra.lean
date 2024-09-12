09ed7bd73e16d45b26ff342111961ab3e8158fda
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2024-09-11 22:22:57 -0400

    don't need ring import?!

--- Mathlib/RingTheory/OreLocalization/Algebra.lean
+++ Mathlib/RingTheory/OreLocalization/Algebra.lean
@@ -3,8 +3,8 @@ Copyright (c) 2024 Kevin Buzzard. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kevin Buzzard
 -/
-import Mathlib.RingTheory.OreLocalization.Ring
-
+import Mathlib.Algebra.Algebra.Defs
+import Mathlib.RingTheory.OreLocalization.Basic
 /-!
 
 # Algebra instances of Ore Localizations
