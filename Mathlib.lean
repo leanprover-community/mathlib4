42507975de73f11b1de30df5160a248c962240d6
Alvan Caleb Arulandu <calebarulandu@gmail.com>
2024-09-13 14:05:15 -0400

    lake exe mk

--- Mathlib.lean
+++ Mathlib.lean
@@ -3699,6 +3699,7 @@ import Mathlib.Probability.CondCount
 import Mathlib.Probability.ConditionalExpectation
 import Mathlib.Probability.ConditionalProbability
 import Mathlib.Probability.Density
+import Mathlib.Probability.Distributions.Beta
 import Mathlib.Probability.Distributions.Exponential
 import Mathlib.Probability.Distributions.Gamma
 import Mathlib.Probability.Distributions.Gaussian
