772c56c164d02c38547fb8ebfa33668ba29fd4bd
Bhavik Mehta <bm489@cam.ac.uk>
2024-09-13 08:00:55 +0200

    Update Mathlib/Algebra/Group/Pointwise/Finset/Basic.lean
    Co-authored-by: Markus Himmel <markus@himmel-villmar.de>

--- Mathlib/Algebra/Group/Pointwise/Finset/Basic.lean
+++ Mathlib/Algebra/Group/Pointwise/Finset/Basic.lean
@@ -1605,7 +1605,7 @@ See `card_le_card_mul_self` for the version with left-cancellative multiplicatio
 -/
 @[to_additive
 "The size of `s + s` is at least the size of `s`, version with right-cancellative addition.
-See `card_le_card_add_self'` for the version with left-cancellative addition."
+See `card_le_card_add_self` for the version with left-cancellative addition."
 ]
 theorem card_le_card_mul_self' {s : Finset α} : s.card ≤ (s * s).card := by
   cases s.eq_empty_or_nonempty <;> simp [card_le_card_mul_right, *]
