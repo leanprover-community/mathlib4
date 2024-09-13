d17a672d11a287a68cdb19a602246c4252c47980
Johan Commelin <johan@commelin.net>
2024-09-13 08:34:14 +0200

    minimize diff

--- Mathlib/Algebra/Lie/TraceForm.lean
+++ Mathlib/Algebra/Lie/TraceForm.lean
@@ -227,11 +227,8 @@ lemma traceForm_eq_sum_genWeightSpaceOf
     exact LieSubmodule.coeSubmodule_eq_bot_iff (genWeightSpaceOf M _ _)
   classical
   have h := LieSubmodule.independent_iff_coe_toSubmodule.mp <| independent_genWeightSpaceOf R L M z
-  dsimp only [genWeightSpaceOf] at h hfin hxy ⊢
-  simp_rw [Module.End.maxGenEigenspace_def] at h
-  have hds := DirectSum.isInternal_submodule_of_independent_of_iSup_eq_top h
-    (IsTriangularizable.iSup_eq_top z)
-  simp_rw [← Module.End.maxGenEigenspace_def] at hds ⊢
+  have hds := DirectSum.isInternal_submodule_of_independent_of_iSup_eq_top h <| by
+    simp [← LieSubmodule.iSup_coe_toSubmodule]
   simp only [LinearMap.coeFn_sum, Finset.sum_apply, traceForm_apply_apply,
     LinearMap.trace_eq_sum_trace_restrict' hds hfin hxy]
   exact Finset.sum_congr (by simp) (fun χ _ ↦ rfl)
@@ -279,10 +276,8 @@ lemma lowerCentralSeries_one_inf_center_le_ker_traceForm [Module.Free R M] [Modu
     intro y
     exact y.induction_on rfl (fun a u ↦ by simp [hzc u]) (fun u v hu hv ↦ by simp [hu, hv])
   apply LinearMap.trace_comp_eq_zero_of_commute_of_trace_restrict_eq_zero
-  · simp_rw [Module.End.maxGenEigenspace_def]
-    exact IsTriangularizable.iSup_eq_top (1 ⊗ₜ[R] x)
-  · intro μ
-    exact trace_toEnd_eq_zero_of_mem_lcs A (A ⊗[R] L)
+  · simpa only [Module.End.maxGenEigenspace_def] using IsTriangularizable.iSup_eq_top (1 ⊗ₜ[R] x)
+  · exact fun μ ↦ trace_toEnd_eq_zero_of_mem_lcs A (A ⊗[R] L)
       (genWeightSpaceOf (A ⊗[R] M) μ ((1:A) ⊗ₜ[R] x)) (le_refl 1) hz
   · exact commute_toEnd_of_mem_center_right (A ⊗[R] M) hzc (1 ⊗ₜ x)
 
