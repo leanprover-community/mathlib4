05758e18ec451121674d73e91d0ab100c9c2f240
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2024-09-13 20:28:05 +0200

    initial commit

--- Mathlib/Algebra/Order/BigOperators/Ring/Multiset.lean
+++ Mathlib/Algebra/Order/BigOperators/Ring/Multiset.lean
@@ -3,8 +3,8 @@ Copyright (c) 2021 Ruben Van de Velde. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Ruben Van de Velde
 -/
-import Mathlib.Algebra.BigOperators.Group.Multiset
 import Mathlib.Algebra.Order.BigOperators.Ring.List
+import Mathlib.Algebra.Order.BigOperators.GroupWithZero.Multiset
 
 /-!
 # Big operators on a multiset in ordered rings
@@ -13,10 +13,12 @@ This file contains the results concerning the interaction of multiset big operat
 rings.
 -/
 
+variable {R : Type*} [CanonicallyOrderedCommSemiring R] [Nontrivial R] {m : Multiset R}
+
 @[simp]
-lemma CanonicallyOrderedCommSemiring.multiset_prod_pos {R : Type*}
-    [CanonicallyOrderedCommSemiring R] [Nontrivial R] {m : Multiset R} :
+lemma CanonicallyOrderedCommSemiring.multiset_prod_pos :
     0 < m.prod ↔ ∀ x ∈ m, 0 < x := by
   rcases m with ⟨l⟩
   rw [Multiset.quot_mk_to_coe'', Multiset.prod_coe]
   exact CanonicallyOrderedCommSemiring.list_prod_pos
+
