7579c66559e485900c0a2195ec621d55ffedce6a
jjdishere <emailboxofjjd@163.com>
2024-09-13 16:59:54 +0200

    finish

--- Mathlib/FieldTheory/AlgebraicClosure.lean
+++ Mathlib/FieldTheory/AlgebraicClosure.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2024 Jiedong Jiang. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Jiedong Jiang
+-/
 import Mathlib.FieldTheory.NormalClosure
 import Mathlib.FieldTheory.Galois
 
@@ -14,11 +19,7 @@ In this file we construct the relative algebraic closure of a field extension.
 -/
 noncomputable section
 
-open Polynomial
-
-section algebraicClosure
-
-open FiniteDimensional IntermediateField Field
+open Polynomial FiniteDimensional IntermediateField Field
 
 variable (F E : Type*) [Field F] [Field E] [Algebra F E]
 variable {K : Type*} [Field K] [Algebra F K]
@@ -129,69 +130,70 @@ theorem le_algebraicClosure_iff (L : IntermediateField F E) :
     Algebra.id.map_eq_id, RingHomCompTriple.comp_apply, mem_algebraicClosure_iff] using h x.2⟩,
     fun _ ↦ le_algebraicClosure _ _ _⟩
 
+namespace algebraicClosure
 /-- The algebraic closure in `E` of the algebraic closure of `F` in `E` is equal to itself. -/
-theorem algebraicClosure.algebraicClosure_eq_bot :
+theorem algebraicClosure_eq_bot :
     algebraicClosure (algebraicClosure F E) E = ⊥ :=
   bot_unique fun x hx ↦ mem_bot.2
     ⟨⟨x, isIntegral_trans x (mem_algebraicClosure_iff'.1 hx)⟩, rfl⟩
 
-
 /-- The normal closure in `E/F` of the algebraic closure of `F` in `E` is equal to itself. -/
-theorem algebraicClosure.normalClosure_eq_self :
+theorem normalClosure_eq_self :
     normalClosure F (algebraicClosure F E) E = algebraicClosure F E :=
   le_antisymm (normalClosure_le_iff.2 fun i ↦
     haveI : Algebra.IsAlgebraic F i.fieldRange := (AlgEquiv.ofInjectiveField i).isAlgebraic
     le_algebraicClosure F E _) (le_normalClosure _)
 
-/-- If `E` is normal over `F`, then the algebraic closure of `F` in `E` is Galois (i.e.
-normal and algebraic) over `F`. -/
-instance algebraicClosure.isGalois [Normal F E] : IsGalois F (algebraicClosure F E) where
-  to_isAlgebraic := algebraicClosure.isAlgebraic F E
-  to_normal := by
-    rw [← algebraicClosure.normalClosure_eq_self]
-    exact normalClosure.normal F _ E
+end algebraicClosure
 
-/-- If `E / F` is a field extension and `E` is separably closed, then the algebraic closure
-of `F` in `E` is equal to `F` if and only if `F` is separably closed. -/
-theorem IsSepClosed.algebraicClosure_eq_bot_iff [IsSepClosed E] :
-    algebraicClosure F E = ⊥ ↔ IsSepClosed F := by
-  refine ⟨fun h ↦ IsSepClosed.of_exists_root _ fun p _ hirr hsep ↦ ?_,
-    fun _ ↦ IntermediateField.eq_bot_of_isSepClosed_of_isAlgebraic _⟩
-  obtain ⟨x, hx⟩ := IsSepClosed.exists_aeval_eq_zero E p (degree_pos_of_irreducible hirr).ne' hsep
-  obtain ⟨x, rfl⟩ := h ▸ mem_algebraicClosure_iff.2 (hsep.of_dvd <| minpoly.dvd _ x hx)
+/-- If `E / F` is a field extension and `E` is algebraically closed, then the algebraic closure
+of `F` in `E` is equal to `F` if and only if `F` is algebraically closed. -/
+theorem IsAlgClosed.algebraicClosure_eq_bot_iff [IsAlgClosed E] :
+    algebraicClosure F E = ⊥ ↔ IsAlgClosed F := by
+  refine ⟨fun h ↦ IsAlgClosed.of_exists_root _ fun p hmon hirr ↦ ?_,
+    fun _ ↦ IntermediateField.eq_bot_of_isAlgClosed_of_isAlgebraic _⟩
+  obtain ⟨x, hx⟩ := IsAlgClosed.exists_aeval_eq_zero E p (degree_pos_of_irreducible hirr).ne'
+  obtain ⟨x, rfl⟩ := h ▸ mem_algebraicClosure_iff'.2 (minpoly.ne_zero_iff.1 <|
+    ne_zero_of_dvd_ne_zero hmon.ne_zero (minpoly.dvd _ x hx))
   exact ⟨x, by simpa [Algebra.ofId_apply] using hx⟩
 
-/-- If `E` is separably closed, then the algebraic closure of `F` in `E` is an absolute
+/-- `F(S) / F` is a algebraic extension if and only if all elements of `S` are
+algebraic elements. -/
+theorem IntermediateField.isAlgebraic_adjoin_iff_isAlgebraic {S : Set E} :
+    Algebra.IsAlgebraic F (adjoin F S) ↔ ∀ x ∈ S, IsAlgebraic F x :=
+  ((le_algebraicClosure_iff F E _).symm.trans (adjoin_le_iff.trans <| forall_congr' <|
+    fun _ => Iff.imp Iff.rfl mem_algebraicClosure_iff))
+
+namespace algebraicClosure
+/-- If `E` is algebraically closed, then the algebraic closure of `F` in `E` is an absolute
 algebraic closure of `F`. -/
-instance algebraicClosure.isSepClosure [IsSepClosed E] : IsSepClosure F (algebraicClosure F E) :=
-  ⟨(IsSepClosed.algebraicClosure_eq_bot_iff _ E).mp (algebraicClosure.algebraicClosure_eq_bot F E),
+instance isAlgClosure [IsAlgClosed E] : IsAlgClosure F (algebraicClosure F E) :=
+  ⟨(IsAlgClosed.algebraicClosure_eq_bot_iff _ E).mp (algebraicClosure_eq_bot F E),
     isAlgebraic F E⟩
+
+/-- The algebraic closure of `F` in `E` is equal to `E` if and only if `E / F` is
+algebraic. -/
+theorem eq_top_iff : algebraicClosure F E = ⊤ ↔ Algebra.IsAlgebraic F E :=
+  ⟨fun h ↦ ⟨fun _ ↦ mem_algebraicClosure_iff.1 (h ▸ mem_top)⟩,
+    fun _ ↦ top_unique fun x _ ↦ mem_algebraicClosure_iff.2 (Algebra.IsAlgebraic.isAlgebraic x)⟩
+
+/-- If `K / E / F` is a field extension tower, then `algebraicClosure F K` is contained in
+`algebraicClosure E K`. -/
+theorem le_restrictScalars [Algebra E K] [IsScalarTower F E K] :
+    algebraicClosure F K ≤ (algebraicClosure E K).restrictScalars F :=
+  fun _ h ↦ mem_algebraicClosure_iff.2 <| IsAlgebraic.tower_top E (mem_algebraicClosure_iff.1 h)
 
 /-- If `K / E / F` is a field extension tower, such that `E / F` is algebraic, then
 `algebraicClosure F K` is equal to `algebraicClosure E K`. -/
-theorem algebraicClosure.eq_restrictScalars_of_isAlgebraic [Algebra E K] [IsScalarTower F E K]
+theorem eq_restrictScalars_of_isAlgebraic [Algebra E K] [IsScalarTower F E K]
     [Algebra.IsAlgebraic F E] : algebraicClosure F K = (algebraicClosure E K).restrictScalars F :=
   (algebraicClosure.le_restrictScalars F E K).antisymm fun _ h ↦
-    IsAlgebraic.of_algebra_isAlgebraic_of_isAlgebraic F h
+    isIntegral_trans _ h
 
 /-- If `K / E / F` is a field extension tower, then `E` adjoin `algebraicClosure F K` is contained
 in `algebraicClosure E K`. -/
-theorem algebraicClosure.adjoin_le [Algebra E K] [IsScalarTower F E K] :
+theorem adjoin_le [Algebra E K] [IsScalarTower F E K] :
     adjoin E (algebraicClosure F K) ≤ algebraicClosure E K :=
   adjoin_le_iff.2 <| le_restrictScalars F E K
-
-/-- A compositum of two algebraic extensions is algebraic. -/
-instance IntermediateField.isAlgebraic_sup (L1 L2 : IntermediateField F E)
-    [h1 : Algebra.IsAlgebraic F L1] [h2 : Algebra.IsAlgebraic F L2] :
-    Algebra.IsAlgebraic F (L1 ⊔ L2 : IntermediateField F E) := by
-  rw [← le_algebraicClosure_iff] at h1 h2 ⊢
-  exact sup_le h1 h2
-
-/-- A compositum of algebraic extensions is algebraic. -/
-instance IntermediateField.isAlgebraic_iSup {ι : Type*} {t : ι → IntermediateField F E}
-    [h : ∀ i, Algebra.IsAlgebraic F (t i)] :
-    Algebra.IsAlgebraic F (⨆ i, t i : IntermediateField F E) := by
-  simp_rw [← le_algebraicClosure_iff] at h ⊢
-  exact iSup_le h
 
 end algebraicClosure
