acd520930ddaac902661fb3e3741336a0aecd053
Parcly Taxel <reddeloostw@gmail.com>
2024-09-14 19:35:32 +0800

    order imports

--- Mathlib/Order/MinMax.lean
+++ Mathlib/Order/MinMax.lean
@@ -3,8 +3,8 @@ Copyright (c) 2017 Mario Carneiro. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro
 -/
-import Mathlib.Order.Lattice
 import Mathlib.Logic.OpClass
+import Mathlib.Order.Lattice
 
 /-!
 # `max` and `min`
