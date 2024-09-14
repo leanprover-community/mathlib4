8c22145196ab82bd7d052b7ebeb6c7110a7140fb
Suzuka Yu <109365723+Yu-Misaka@users.noreply.github.com>
2024-09-14 12:46:59 +0800

    reductions and file changes 1. moved `conj_nonComm_Algebra` to Algebra/Field/Power 2. reduced `center_char_eq_iff` 3. convert `f` and `g` to `loacl notation` 4. reduced `calc` and some other things

--- Mathlib/FieldTheory/JacobsonNoether.lean
+++ Mathlib/FieldTheory/JacobsonNoether.lean
@@ -5,6 +5,7 @@ Authors: Filippo A. E. Nuccio, Huanyu Zheng, Sihan Wu, Wanyi He, Weichen Jiao, Y
 -/
 import Mathlib.FieldTheory.Separable
 import Mathlib.Algebra.CharP.LinearMaps
+import Mathlib.Algebra.Field.Power
 
 /-!
 # The Jacobson-Noether theorem
@@ -30,33 +31,17 @@ the noncommutative division algebra `D` with the center `k`.
 * <https://ysharifi.wordpress.com/2011/09/30/the-jacobson-noether-theorem/>
 -/
 
--- *Filippo* This should probably be moved to another file.
-/-- a⁻¹ * d ^ s * a = (a⁻¹ * d * a) ^ s when a is not zero -/
-lemma conj_nonComm_Algebra {D : Type*} [DivisionSemiring D] (s : ℕ) (a d : D) (ha : a ≠ 0) :
-    a⁻¹ * d ^ s * a = (a⁻¹ * d * a) ^ s := by
-  let u : Dˣ := ⟨a, a⁻¹, mul_inv_cancel₀ ha, inv_mul_cancel₀ ha⟩
-  exact (Units.conj_pow' u d s).symm
-
 namespace JacobsonNoether
 
 variable {D : Type*} [DivisionRing D]
 
 local notation3 "k" => (Subring.center D)
-
-private def f : D → D →ₗ[k] D := LinearMap.mulLeft k
-private def g : D → D →ₗ[k] D := LinearMap.mulRight k
+local notation3 "f" => (LinearMap.mulLeft k)
+local notation3 "g" => (LinearMap.mulRight k)
 private def δ : D → D →ₗ[k] D := f - g
 
-@[simp]
-private lemma f_def (a x : D) : f a x = a * x := rfl
-
-@[simp]
-private lemma g_def (a x : D) : g a x = x * a := rfl
-
-@[simp]
 private lemma δ_def (a x : D) : δ a x = f a x - g a x := rfl
 
-@[simp]
 private lemma δ_def' (a : D) : δ a = f a - g a := rfl
 
 private lemma fg_comm (a : D) : Commute (f a) (g a) := LinearMap.commute_mulLeft_right a a
@@ -64,34 +49,42 @@ private lemma fg_comm (a : D) : Commute (f a) (g a) := LinearMap.commute_mulLeft
 private lemma f_pow (a : D) (n : ℕ) : ∀ x : D, ((f a) ^ n).1 x = (a ^ n) * x := by
   intro x
   rw [LinearMap.coe_toAddHom, LinearMap.pow_apply]
-  induction n
-  · simp only [Function.iterate_zero, id_eq, pow_zero, one_mul]
-  · simp only [Function.iterate_succ', Function.comp_apply, *, pow_succ', mul_assoc, f_def]
+  induction n with
+  | zero => rw [Function.iterate_zero, id_eq, pow_zero, one_mul]
+  | succ _ h => rw [Function.iterate_succ', Function.comp_apply, h,
+    pow_succ', mul_assoc, LinearMap.mulLeft_apply]
 
 private lemma g_pow (a : D) (n : ℕ) : ∀ x : D, ((g a) ^ n).1 x = x * (a ^ n) := by
   intro x
   rw [LinearMap.coe_toAddHom, LinearMap.pow_apply]
-  induction n
-  · simp only [Function.iterate_zero, id_eq, pow_zero, mul_one]
-  · simp only [Function.iterate_succ', Function.comp_apply, *, pow_add, pow_one, mul_assoc, g_def]
+  induction n with
+  | zero => rw [Function.iterate_zero, id_eq, pow_zero, mul_one]
+  | succ _ h => rw [Function.iterate_succ', Function.comp_apply, h,
+    pow_add, pow_one, LinearMap.mulRight_apply, mul_assoc]
 
 private lemma δ_iterate_succ (x y : D) (n : ℕ) :
     δ x (((δ x) ^ n) y) = ((δ x) ^ (n + 1)) y := by
-  simp only [LinearMap.pow_apply, δ_def, f_def, g_def, Function.iterate_succ_apply']
+  simp only [LinearMap.pow_apply, δ_def, LinearMap.mulLeft_apply,
+    LinearMap.mulRight_apply, Function.iterate_succ_apply']
 
 variable [Algebra.IsAlgebraic (Subring.center D) D]
 
 open Polynomial
 
-/-- Any inseparable extension is an extension by some p^n th -root -/
+/-- If `D` is a purely inseparable extension of `k` with characteristic `p`,
+  then for every element `a` of `D`, there exists a natural number `n`
+  such that `a ^ (p ^ n)` is contained in `k`. -/
 lemma exists_pow_mem_center_ofInseparable (p : ℕ) [Fact p.Prime] [CharP D p] (a : D)
     (hinsep : ∀ x : D, IsSeparable k x → x ∈ k) : ∃ n, a ^ (p ^ n) ∈ k := by
-  obtain ⟨n, g, hg, geqf⟩ := @exists_separable_of_irreducible k _ p _ (minpoly k a)
+  obtain ⟨n, g', hg', geqf⟩ := @exists_separable_of_irreducible k _ p _ (minpoly k a)
     (minpoly.irreducible (Algebra.IsIntegral.isIntegral a)) ((NeZero.ne' p).symm)
-  have h1 : (aeval (a ^ p ^ n)) g = 0 := by
+  have h1 : (aeval (a ^ p ^ n)) g' = 0 := by
     rw [← expand_aeval, congrArg (⇑(aeval a)) geqf, minpoly.aeval k a]
-  refine ⟨n, hinsep (a ^ p ^ n) (Separable.of_dvd hg (minpoly.dvd_iff.2 h1))⟩
+  refine ⟨n, hinsep (a ^ p ^ n) (Separable.of_dvd hg' (minpoly.dvd_iff.2 h1))⟩
 
+/-- If `D` is a purely inseparable extension of `k` with characteristic `p`,
+  then for every element `a` of `D\k`, there exists a natural number `n`
+  **greater than 0** such that `a ^ (p ^ n)` is contained in `k`. -/
 lemma exists_pow_mem_center_ofInseparable' (p : ℕ) [Fact p.Prime] [CharP D p] {a : D}
     (ha : a ∉ k) (hinsep : ∀ x : D, IsSeparable k x → x ∈ k) : ∃ n ≥ 1, a ^ (p ^ n) ∈ k := by
   obtain ⟨n, hn⟩ := exists_pow_mem_center_ofInseparable p a hinsep
@@ -100,6 +93,10 @@ lemma exists_pow_mem_center_ofInseparable' (p : ℕ) [Fact p.Prime] [CharP D p] 
     exact False.elim <| ha hn
   · refine ⟨n, ⟨by omega, hn⟩⟩
 
+/-- If `D` is a purely inseparable extension of `k` with characteristic `p`,
+  then for every element `a` of `D\k`, there exists a natural number `m`
+  greater than 0 such that linear map `(a * x - x * a) ^ n = 0` for
+  any `n` greater than `(p ^ n)`. -/
 lemma exist_pow_eq_zero_of_le (p : ℕ) [Fact p.Prime] [CharP D p]
     {a : D} (ha : a ∉ k) (hinsep : ∀ x : D, IsSeparable k x → x ∈ k):
   ∃ m ≥ 1, ∀ n ≥ (p ^ m), (δ a) ^ n = 0 := by
@@ -154,39 +151,37 @@ theorem JacobsonNoether_charP (p : ℕ) [Fact p.Prime] [CharP D p]
     · rw [not_lt, Nat.le_zero] at choice
       have := Nat.eq_add_of_sub_eq (Nat.find_spec exist).1 ht.symm
       simp only [gt_iff_lt, choice, Nat.succ_eq_add_one, zero_add] at this
-      rw [this]
-      simp only [Function.iterate_one, ne_eq]
+      rw [this, pow_one]
       exact hb1
   -- We define `c` to be the value that we proved above to be non-zero.
   set c := ((δ a) ^ n) b with hc_def
   letI : Invertible c := ⟨c⁻¹, inv_mul_cancel₀ (hb.1), mul_inv_cancel₀ (hb.1)⟩
+  -- We prove that `c` is commute with `a`.
   have hc : c * a = a * c := by
     symm; apply eq_of_sub_eq_zero
-    rw [← f_def a c, ← g_def a c, ← δ_def, δ_iterate_succ a b n, hb.2]
-  -- We now make some computation to obtain the *absurd* equation `final_eq`, contradiction.
-  set d := c⁻¹ * a * (δ a) ^[n-1] b with hd_def
+    rw [← LinearMap.mulLeft_apply (R := k), ← LinearMap.mulRight_apply (R := k),
+      ← δ_def, δ_iterate_succ a b n, hb.2]
+  -- We now make some computation to obtain the final equation.
+  set d := c⁻¹ * a * ((δ a) ^ (n - 1)) b with hd_def
   have hc': c⁻¹ * a = a * c⁻¹ := by
     apply_fun (c⁻¹ * · * c⁻¹) at hc
     rw [← mul_assoc, inv_mul_cancel₀ hb.1, one_mul, mul_assoc, mul_assoc,
       mul_inv_cancel₀ hb.1, mul_one] at hc
     exact hc.symm
-
-  have c_eq : a * (δ a) ^[n - 1] b - (δ a) ^[n - 1] b * a = c := by
-    rw [hc_def, ← (LinearMap.pow_apply (δ a) (n - 1) b), ← Nat.sub_add_cancel hn,
-      show (δ a ^ (n - 1 + 1)) b = (δ a) ((δ a ^ (n - 1)) b) by rw [δ_iterate_succ]]
-    simp only [add_tsub_cancel_right, δ_def, f_def, g_def]
-  have eq1 : c⁻¹ * a * (δ a)^[n - 1] b - c⁻¹ * (δ a)^[n - 1] b * a = 1 := by
+  have c_eq : a * ((δ a) ^ (n - 1)) b - ((δ a) ^ (n - 1)) b * a = c := by
+    rw [hc_def, ← Nat.sub_add_cancel hn, ← δ_iterate_succ, δ_def]; rfl
+  have eq1 : c⁻¹ * a * ((δ a)^ (n - 1)) b - c⁻¹ * ((δ a) ^ (n - 1)) b * a = 1 := by
     simp_rw [mul_assoc, (mul_sub_left_distrib c⁻¹ _ _).symm, c_eq, inv_mul_cancel_of_invertible]
-
+  -- We show that `a` is commute with `d`.
   have deq : a * d - d * a = a := by
-    calc
-      _ = a * ((c⁻¹ * a * (δ a) ^[n - 1] b) - (c⁻¹ * (δ a) ^[n - 1] b * a)) := by
-        simp_rw [hd_def, hc', mul_assoc, ← mul_sub_left_distrib]
-      _ = _ := by simp only [eq1, mul_one]
+    nth_rw 3 [← mul_one a]
+    rw [hd_def, ← eq1, mul_sub, mul_assoc _ _ a, sub_right_inj, hc',
+      ← mul_assoc, ← mul_assoc, ← mul_assoc]
+  -- This then derives a contradiction.
   apply_fun (a⁻¹ * · ) at deq
   rw [mul_sub, ← mul_assoc, inv_mul_cancel₀ ha₀, one_mul, ← mul_assoc, sub_eq_iff_eq_add] at deq
   obtain ⟨r, hr⟩ := (exists_pow_mem_center_ofInseparable p d hinsep)
-  apply_fun fun x => x ^ (p ^ r) at deq
+  apply_fun (· ^ (p ^ r)) at deq
   rw [add_pow_char_pow_of_commute D 1 _ (Commute.one_left _) , one_pow,
     ← conj_nonComm_Algebra (ha := ha₀), ← hr.comm, mul_assoc, inv_mul_cancel₀ ha₀, mul_one,
     self_eq_add_left] at deq
