08aa3d3006eb65b7b4f69b81e56d0cde88ddc5dc
Bjørn Kjos-Hanssen <bjoernkh@hawaii.edu>
2024-09-14 10:30:08 -1000

    new branch due to merge conflict

--- Mathlib.lean
+++ Mathlib.lean
@@ -4495,6 +4495,7 @@ import Mathlib.Topology.Clopen
 import Mathlib.Topology.ClopenBox
 import Mathlib.Topology.CompactOpen
 import Mathlib.Topology.Compactification.OnePoint
+import Mathlib.Topology.Compactification.OnePointEquiv
 import Mathlib.Topology.Compactness.Compact
 import Mathlib.Topology.Compactness.CompactlyGeneratedSpace
 import Mathlib.Topology.Compactness.Lindelof
