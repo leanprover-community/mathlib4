481029b76d518193f86895b568a1a4888c9bfa58
Yury G. Kudryashov <urkud@urkud.name>
2024-09-15 20:20:18 +0000

    feat(Logic/Function): add `Function.extend_const` (#16825)

--- Mathlib/Logic/Function/Basic.lean
+++ Mathlib/Logic/Function/Basic.lean
@@ -655,6 +655,10 @@ lemma FactorsThrough.extend_comp {g : α → γ} (e' : β → γ) (hf : FactorsT
   funext fun a => hf.extend_apply e' a
 
 @[simp]
+lemma extend_const (f : α → β) (c : γ) : extend f (fun _ ↦ c) (fun _ ↦ c) = fun _ ↦ c :=
+  funext fun _ ↦ ite_id _
+
+@[simp]
 theorem extend_comp (hf : Injective f) (g : α → γ) (e' : β → γ) : extend f g e' ∘ f = g :=
   funext fun a ↦ hf.extend_apply g e' a
 
