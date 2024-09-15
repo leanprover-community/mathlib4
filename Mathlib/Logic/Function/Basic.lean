014470b8ed0bde0ffbe0616b3c4220ffd5f63faa
Yury G. Kudryashov <urkud@urkud.name>
2024-09-15 16:45:44 -0500

    Fix merge

--- Mathlib/Logic/Function/Basic.lean
+++ Mathlib/Logic/Function/Basic.lean
@@ -654,9 +654,6 @@ lemma FactorsThrough.extend_comp {g : α → γ} (e' : β → γ) (hf : FactorsT
     extend f g e' ∘ f = g :=
   funext fun a => hf.extend_apply e' a
 
-lemma extend_const (f : α → β) (c : γ) : extend f (fun _ ↦ c) (fun _ ↦ c) = fun _ ↦ c :=
-  funext fun _ ↦ ite_id _
-
 @[simp]
 lemma extend_const (f : α → β) (c : γ) : extend f (fun _ ↦ c) (fun _ ↦ c) = fun _ ↦ c :=
   funext fun _ ↦ ite_id _
