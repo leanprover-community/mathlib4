leanprover-community/mathlib4

commit 035936f986f1b6a83a6cf9ba5bae3c34ef9ec5dc

chore: use "bash strict mode" in some shell scripts (#16566)
If a script produces an error, it should rather do so loudly than silently. See http://redsymbol.net/articles/unofficial-bash-strict-mode/ for an explanation of this incantation.

This PR has the "easy" cases, where no change to the remaining script was necessary.

scripts/technical-debt-metrics.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# Make this script robust against unintentional errors.
+# See e.g. http://redsymbol.net/articles/unofficial-bash-strict-mode/ for explanation.
+set -euo pipefail
+IFS=$'\n\t'
+
 # `./scripts/technical-debt-metrics.sh` returns a tally of some technical debts in current Mathlib,
 # reporting also the change with respect to the same counts in
 # Mathlib from last week.

.github/workflows/mk_build_yml.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
-set -ex
+
+# Make this script robust against unintentional errors.
+# See e.g. http://redsymbol.net/articles/unofficial-bash-strict-mode/ for explanation.
+set -euo pipefail
+IFS=$'\n\t'
+
+set -x
 cd $(dirname "$(realpath "$0")")
 
 header() {

scripts/init_creation.sh
@@ -7,6 +7,11 @@ These are the commands to generate a "root" `Mathlib/Init.lean` file, imported b
 
 BASH_MODULE_DOC
 
+# Make this script robust against unintentional errors.
+# See e.g. http://redsymbol.net/articles/unofficial-bash-strict-mode/ for explanation.
+set -euo pipefail
+IFS=$'\n\t'
+
 # `mathlibNonImportingFiles` generates the list of `Mathlib` files that do not have Mathlib imports.
 # The output of `lake exe graph` are many lines like
 # `  "Mathlib..." -> "Mathlib...";`

scripts/install_debian.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-set -exo pipefail
+# Make this script robust against unintentional errors.
+# See e.g. http://redsymbol.net/articles/unofficial-bash-strict-mode/ for explanation.
+set -euo pipefail
+IFS=$'\n\t'
+
+set -x
 
 sudo apt install -y git curl
 

scripts/install_macos.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-set -exo pipefail
+# Make this script robust against unintentional errors.
+# See e.g. http://redsymbol.net/articles/unofficial-bash-strict-mode/ for explanation.
+set -euo pipefail
+IFS=$'\n\t'
+
+set -x
 
 # Install elan using the official script
 curl https://raw.githubusercontent.com/leanprover/elan/master/elan-init.sh -sSf | sh

scripts/lint-bib.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-set -exo pipefail
+# Make this script robust against unintentional errors.
+# See e.g. http://redsymbol.net/articles/unofficial-bash-strict-mode/ for explanation.
+set -euo pipefail
+IFS=$'\n\t'
+
+set -x
 # https://leanprover-community.github.io/contribute/doc.html#citing-other-works
 cp docs/references.bib docs/references.bib.old
 bibtool --preserve.key.case=on --preserve.keys=on --pass.comments=on --print.use.tab=off -s \

scripts/print-style-errors.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
+# Make this script robust against unintentional errors.
+# See e.g. http://redsymbol.net/articles/unofficial-bash-strict-mode/ for explanation.
+set -euo pipefail
+IFS=$'\n\t'
+
 # Print all errors of the python style linter. This script is temporary and should be removed
 # once the Python style linters have been rewritten in Lean.
 # Humans should never run this directly, but at most through `lean exe lint-style --fix`
 
 # use C locale so that sorting is the same on macOS and Linux
 # see https://unix.stackexchange.com/questions/362728/why-does-gnu-sort-sort-differently-on-my-osx-machine-and-linux-machine
-find Mathlib -name '*.lean' | xargs ./scripts/lint-style.py "$@" | LC_ALL=C sort
-find Archive -name '*.lean' | xargs ./scripts/lint-style.py "$@" | LC_ALL=C sort
-find Counterexamples -name '*.lean' | xargs ./scripts/lint-style.py "$@" | LC_ALL=C sort
+find Mathlib -name '*.lean' | xargs ./scripts/lint-style.py "$@" | LC_ALL=C sort || true
+find Archive -name '*.lean' | xargs ./scripts/lint-style.py "$@" | LC_ALL=C sort || true
+find Counterexamples -name '*.lean' | xargs ./scripts/lint-style.py "$@" | LC_ALL=C sort || true

scripts/update_nolints_CI.sh
@@ -3,7 +3,11 @@
 # Check if there are changes to `nolints.json` and file a PR updating it if necessary.
 # DO NOT run this as a human; this is meant only for automation usage!
 
-set -e
+# Make this script robust against unintentional errors.
+# See e.g. http://redsymbol.net/articles/unofficial-bash-strict-mode/ for explanation.
+set -euo pipefail
+IFS=$'\n\t'
+
 set -x
 
 remote_name=origin-bot

commit 15b775cafce43b590be87f6f3895c708aec5b129

fix(technical-debt-metrics.sh): make compatible with set -u (#16854)
...by using [parameter default values](https://www.gnu.org/savannah-checkouts/gnu/bash/manual/bash.html#Shell-Parameter-Expansion) per [the bash strict mode page](http://redsymbol.net/articles/unofficial-bash-strict-mode/#solution-positional-parameters).

Fallout from #16566. See [Zulip](https://leanprover.zulipchat.com/#narrow/stream/287929-mathlib4/topic/Technical.20Debt.20Counters/near/470576612).

scripts/technical-debt-metrics.sh
@@ -12,18 +12,8 @@ IFS=$'\n\t'
 # the script takes two optional arguments `<optCurrCommit> <optReferenceCommit>`
 # and tallies the same technical debts on `<optCurrCommit>` using `<optReferenceCommit>`
 # as a reference.
-
-if [ -n "${1}" ]; then
-  currCommit="${1}"
-else
-  currCommit="$(git rev-parse HEAD)"
-fi
-
-if [ -n "${2}" ]; then
-  refCommit="${2}"
-else
-  refCommit="$(git log --pretty=%H --since="$(date -I -d 'last week')" | tail -n -1)"
-fi
+currCommit="${1:-"$(git rev-parse HEAD)"}"
+refCommit="${2:-"$(git log --pretty=%H --since="$(date -I -d 'last week')" | tail -n -1)"}"
 
 # `tdc` produces a semi-formatted output of the form
 # ...