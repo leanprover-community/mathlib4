a03152ba787ed8d5f3e0f83f1b03d0423bab482a
Kim Morrison <kim@tqft.net>
2024-09-16 14:35:01 +0000

    chore: split Algebra.MonoidAlgebra.Basic (#16531)
    It's not the best justified split

--- Mathlib.lean
+++ Mathlib.lean
@@ -506,6 +506,7 @@ import Mathlib.Algebra.Module.ULift
 import Mathlib.Algebra.Module.ZLattice.Basic
 import Mathlib.Algebra.Module.ZLattice.Covolume
 import Mathlib.Algebra.MonoidAlgebra.Basic
+import Mathlib.Algebra.MonoidAlgebra.Defs
 import Mathlib.Algebra.MonoidAlgebra.Degree
 import Mathlib.Algebra.MonoidAlgebra.Division
 import Mathlib.Algebra.MonoidAlgebra.Grading
