94393363f83512dac2a7ac69c6ed6201c1d710f0
Jireh Loreaux <loreaujy@gmail.com>
2024-09-16 12:57:52 -0500

    copyright

--- Mathlib/Analysis/LocallyConvex/WeakSpace.lean
+++ Mathlib/Analysis/LocallyConvex/WeakSpace.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2024 Jireh Loreaux. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Jireh Loreaux
+-/
 import Mathlib.LinearAlgebra.Dual
 import Mathlib.Analysis.NormedSpace.HahnBanach.Separation
 import Mathlib.Analysis.LocallyConvex.WeakDual
