86a4bd30ad2bd41ad3aa04ac5ec0676d33ab9f5c
Violeta Hernández <vi.hdz.p@gmail.com>
2024-09-16 17:23:36 +0000

    feat(SetTheory/Ordinal/Nimber): Initial development of nimbers (#16088)
    For the moment, we only prove that they form an additive commutative group.

--- Mathlib.lean
+++ Mathlib.lean
@@ -4101,6 +4101,7 @@ import Mathlib.SetTheory.Ordinal.Exponential
 import Mathlib.SetTheory.Ordinal.FixedPoint
 import Mathlib.SetTheory.Ordinal.FixedPointApproximants
 import Mathlib.SetTheory.Ordinal.NaturalOps
+import Mathlib.SetTheory.Ordinal.Nimber
 import Mathlib.SetTheory.Ordinal.Notation
 import Mathlib.SetTheory.Ordinal.Principal
 import Mathlib.SetTheory.Ordinal.Topology
