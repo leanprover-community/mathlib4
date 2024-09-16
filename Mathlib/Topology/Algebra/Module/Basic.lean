cbb7c95b19e343c85f2de640e63d10ff80974894
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2024-09-16 07:15:00 +0200

    lint

--- Mathlib/Topology/Algebra/Module/Basic.lean
+++ Mathlib/Topology/Algebra/Module/Basic.lean
@@ -734,10 +734,10 @@ theorem comp_finset_sum {ι : Type*} {s : Finset ι}
   simp
 
 theorem finset_sum_comp {ι : Type*} {s : Finset ι}
-    [ContinuousAdd M₂] [ContinuousAdd M₃] (g : ι → M₂ →SL[σ₂₃] M₃)
+    [ContinuousAdd M₃] (g : ι → M₂ →SL[σ₂₃] M₃)
     (f : M₁ →SL[σ₁₂] M₂) : (∑ i ∈ s, g i).comp f = ∑ i ∈ s, (g i).comp f := by
   ext
-  simp
+  simp only [coe_comp', coe_sum', Function.comp_apply, Finset.sum_apply]
 
 theorem comp_assoc {R₄ : Type*} [Semiring R₄] [Module R₄ M₄] {σ₁₄ : R₁ →+* R₄} {σ₂₄ : R₂ →+* R₄}
     {σ₃₄ : R₃ →+* R₄} [RingHomCompTriple σ₁₃ σ₃₄ σ₁₄] [RingHomCompTriple σ₂₃ σ₃₄ σ₂₄]
