a0a98283f8218a62244a1783eaf08c6bd4b7df1b
Yuma Mizuno <mizuno.y.aj@gmail.com>
2024-09-16 13:37:41 +0100

    fix namespace

--- Mathlib/Tactic/CategoryTheory/Monoidal/Basic.lean
+++ Mathlib/Tactic/CategoryTheory/Monoidal/Basic.lean
@@ -25,11 +25,11 @@ implementation.
 open Lean Meta Elab Tactic
 open CategoryTheory Mathlib.Tactic.BicategoryLike
 
-namespace Mathlib.Tactic.Monoidal'
+namespace Mathlib.Tactic.Monoidal
 
 /-- Normalize the both sides of an equality. -/
 def monoidalNf (mvarId : MVarId) : MetaM (List MVarId) := do
-  BicategoryLike.normalForm Monoidal'.Context `monoidal mvarId
+  BicategoryLike.normalForm Monoidal.Context `monoidal mvarId
 
 @[inherit_doc monoidalNf]
 elab "monoidal_nf" : tactic => withMainContext do
@@ -46,10 +46,10 @@ That is, `monoidal` can handle goals of the form
 where `a = a'`, `b = b'`, and `c = c'` can be proved using `monoidal_coherence`.
 -/
 def monoidal (mvarId : MVarId) : MetaM (List MVarId) :=
-  BicategoryLike.main Monoidal'.Context `monoidal mvarId
+  BicategoryLike.main Monoidal.Context `monoidal mvarId
 
 @[inherit_doc monoidal]
 elab "monoidal" : tactic => withMainContext do
   replaceMainGoal <| ← monoidal <| ← getMainGoal
 
-end Mathlib.Tactic.Monoidal'
+end Mathlib.Tactic.Monoidal
