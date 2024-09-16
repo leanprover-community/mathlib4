9518f17bd90e8beb6ff5f76c82070b2ff8f7b08e
Violeta Hernández <vi.hdz.p@gmail.com>
2024-09-15 22:28:02 -0600

    add characterization

--- Mathlib/Data/List/RunLength.lean
+++ Mathlib/Data/List/RunLength.lean
@@ -12,7 +12,34 @@ import Mathlib.Order.TypeTags
 
 variable {α : Type*} [DecidableEq α]
 
+namespace List
+
+/-- Run-length encoding of a list. Returns a list of pairs `(n, a)` representing consecutive groups
+of `a` of length `n`. -/
 def RunLength (l : List α) : List (ℕ+ × α) :=
-  (l.groupBy (· == ·)).attach.map (fun ⟨m, hm⟩ ↦ by
-    
-  )
+  ((l.groupBy (· == ·)).attachWith _ (fun _ ↦ ne_nil_of_mem_groupBy _)).map
+    fun m ↦ (⟨m.1.length, length_pos_of_ne_nil m.2⟩, m.1.head m.2)
+
+@[simp]
+theorem runLength_nil : RunLength ([] : List α) = [] :=
+  rfl
+
+@[simp]
+theorem runLength_replicate {n : ℕ} (hn : 0 < n) (a : α) :
+    RunLength (replicate n a) = [(⟨n, hn⟩, a)] := by
+  
+
+def runLengthRecOn (l : List α) {p : List α → Sort*} (pn : p [])
+    (pr : ∀ (n : ℕ+) a, p (replicate n a))
+
+#exit
+theorem join_map_runLength (l : List α) :
+    (l.RunLength.map (fun x ↦ replicate x.1 x.2)).join = l := by
+  convert join_groupBy (· == ·) l
+  induction l with
+  | nil => rfl
+  | cons a l IH =>
+    rw [map_cons]
+
+
+end List
