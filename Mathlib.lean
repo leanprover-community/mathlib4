60e4a87afc0b5dbed16ad89a865475b3f8a20d00
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2024-09-17 19:29:30 +0000

    chore: split the `change origin` part from `Analytic.Basic` (#16891)
    Just moving things around, no mathematical change.

--- Mathlib.lean
+++ Mathlib.lean
@@ -913,6 +913,7 @@ import Mathlib.AlgebraicTopology.SplitSimplicialObject
 import Mathlib.AlgebraicTopology.TopologicalSimplex
 import Mathlib.Analysis.Analytic.Basic
 import Mathlib.Analysis.Analytic.CPolynomial
+import Mathlib.Analysis.Analytic.ChangeOrigin
 import Mathlib.Analysis.Analytic.Composition
 import Mathlib.Analysis.Analytic.Constructions
 import Mathlib.Analysis.Analytic.Inverse
