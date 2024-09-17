e61095eaa8620d8674ca6261f575f22540717f30
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2024-09-18 00:13:36 +0800

    refine namings

--- Mathlib/Topology/Algebra/Subgroup.lean
+++ Mathlib/Topology/Algebra/Subgroup.lean
@@ -585,7 +585,7 @@ end
 /-!
 ## The Open Normal Subgroup in a Clopen Neighborhood of One
 
-This section define `OpenSubgroupSubnhdsOfOne`, which can pick an open subgroup contained in a
+This section define `OpenSubgroupSubClopenNhdsOfOne`, which can pick an open subgroup contained in a
 clopen neighborhood of `1`.
 
 -/
@@ -721,14 +721,14 @@ private lemma nhdsSide'_cover [TopologicalSpace G]  [TopologicalGroup G]
   exact ⟨x, xinW, (w_mem_nhdsSide' WOpen einW ⟨x, xinW⟩)⟩
 
 /-- The index of the finite subcover of the rectangle neighbors covering `(W,1)`. -/
-noncomputable def openSymmSubnhdsOfOneIndex [TopologicalSpace G]  [TopologicalGroup G]
+noncomputable def openSymmSubClopenNhdsOfOneIndex [TopologicalSpace G]  [TopologicalGroup G]
     [CompactSpace G] {W : Set G} (WClopen : IsClopen W) (einW : 1 ∈ W) : Finset W :=
   Classical.choose (IsCompact.elim_finite_subcover (IsClosed.isCompact (IsClopen.isClosed WClopen))
     _ (fun (w : W) => (nhdsSide'_open WClopen.isOpen einW w)) (nhdsSide'_cover WClopen.isOpen einW))
 
-lemma openSymmSubnhdsOfOneIndex_spec [TopologicalSpace G]  [TopologicalGroup G]
+lemma openSymmSubClopenNhdsOfOneIndex_spec [TopologicalSpace G]  [TopologicalGroup G]
     [CompactSpace G] {W : Set G} (WClopen : IsClopen W) (einW : 1 ∈ W) :
-    W ⊆ ⋃ i ∈ (openSymmSubnhdsOfOneIndex WClopen einW), nhdsSide' WClopen.isOpen einW i :=
+    W ⊆ ⋃ i ∈ (openSymmSubClopenNhdsOfOneIndex WClopen einW), nhdsSide' WClopen.isOpen einW i :=
   Classical.choose_spec (IsCompact.elim_finite_subcover (IsClosed.isCompact
   (IsClopen.isClosed WClopen)) _ (fun (w : W) => (nhdsSide'_open WClopen.isOpen einW w))
   (nhdsSide'_cover WClopen.isOpen einW))
@@ -736,37 +736,37 @@ lemma openSymmSubnhdsOfOneIndex_spec [TopologicalSpace G]  [TopologicalGroup G]
 /-- Define an open symmetric neighborhood of `1` such that it is contained in a given
   clopen neighborhood of `1` and the given neighborhood is closed under multiplying by
   an element in it. -/
-def openSymmSubnhdsOfOne [TopologicalSpace G]  [TopologicalGroup G]
+def openSymmSubClopenNhdsOfOne [TopologicalSpace G]  [TopologicalGroup G]
     [CompactSpace G] {W : Set G} (WClopen : IsClopen W) (einW : 1 ∈ W) : Set G :=
-  ⋂ w ∈ (openSymmSubnhdsOfOneIndex WClopen einW) , nhdsSideCore WClopen.isOpen einW w
+  ⋂ w ∈ (openSymmSubClopenNhdsOfOneIndex WClopen einW) , nhdsSideCore WClopen.isOpen einW w
 
-namespace openSymmSubnhdsOfOne
+namespace openSymmSubClopenNhdsOfOne
 
 variable [TopologicalSpace G]  [TopologicalGroup G]
 
 lemma isopen [CompactSpace G] {W : Set G} (WClopen : IsClopen W) (einW : 1 ∈ W) :
-    IsOpen (openSymmSubnhdsOfOne WClopen einW) :=
+    IsOpen (openSymmSubClopenNhdsOfOne WClopen einW) :=
   isOpen_biInter_finset (fun w _ => nhdsSideCore_open WClopen.isOpen einW w)
 
 lemma symm [CompactSpace G] {W : Set G} (WClopen : IsClopen W) (einW : 1 ∈ W) :
-    (openSymmSubnhdsOfOne WClopen einW) = (openSymmSubnhdsOfOne WClopen einW)⁻¹ := by
-  simp only [openSymmSubnhdsOfOne, nhdsSideCore]
+    (openSymmSubClopenNhdsOfOne WClopen einW) = (openSymmSubClopenNhdsOfOne WClopen einW)⁻¹ := by
+  simp only [openSymmSubClopenNhdsOfOne, nhdsSideCore]
   apply inter_symmCore_symm
 
 lemma one_mem [CompactSpace G] {W : Set G} (WClopen : IsClopen W) (einW : 1 ∈ W) :
-    1 ∈ (openSymmSubnhdsOfOne WClopen einW) := by
-  simp only [openSymmSubnhdsOfOne, Set.mem_iInter]
+    1 ∈ (openSymmSubClopenNhdsOfOne WClopen einW) := by
+  simp only [openSymmSubClopenNhdsOfOne, Set.mem_iInter]
   exact fun w _ => one_mem_nhdsSideCore WClopen.isOpen einW w
 
 lemma mul_sub [CompactSpace G] {W : Set G} (WClopen : IsClopen W) (einW : 1 ∈ W) :
-    W * (openSymmSubnhdsOfOne WClopen einW) ⊆ W := by
+    W * (openSymmSubClopenNhdsOfOne WClopen einW) ⊆ W := by
   intro a ainmul
   rcases ainmul with ⟨x,xinW,y,yinInter,xmuly⟩
-  have fincover := openSymmSubnhdsOfOneIndex_spec WClopen einW
+  have fincover := openSymmSubClopenNhdsOfOneIndex_spec WClopen einW
   have := fincover xinW
   simp_rw [Set.mem_iUnion, exists_prop', nonempty_prop] at this
   rcases this with ⟨w,winfin,xinU⟩
-  simp only [openSymmSubnhdsOfOne, Set.iUnion_coe_set, Set.iInter_coe_set,
+  simp only [openSymmSubClopenNhdsOfOne, Set.iUnion_coe_set, Set.iInter_coe_set,
     Set.mem_iInter ] at yinInter
   have yinV := Set.mem_of_mem_inter_left (yinInter w w.2 winfin)
   have := Set.mem_of_mem_inter_left <|
@@ -774,11 +774,11 @@ lemma mul_sub [CompactSpace G] {W : Set G} (WClopen : IsClopen W) (einW : 1 ∈ 
   simpa only [Set.mem_preimage, xmuly] using this
 
 lemma sub [CompactSpace G] {W : Set G} (WClopen : IsClopen W) (einW : 1 ∈ W) :
-    (openSymmSubnhdsOfOne WClopen einW) ⊆ W :=
-  Set.Subset.trans (Set.subset_mul_right (openSymmSubnhdsOfOne WClopen einW) einW)
+    (openSymmSubClopenNhdsOfOne WClopen einW) ⊆ W :=
+  Set.Subset.trans (Set.subset_mul_right (openSymmSubClopenNhdsOfOne WClopen einW) einW)
     (mul_sub WClopen einW)
 
-end openSymmSubnhdsOfOne
+end openSymmSubClopenNhdsOfOne
 
 end TopologicalGroup
 
@@ -806,9 +806,9 @@ namespace TopologicalGroup
 
 /-- Define an open symmetric neighborhood of `1` that is contained in a given
   clopen neighborhood of `1`. -/
-def OpenSubgroupSubnhdsOfOne {G : Type*} [Group G] [TopologicalSpace G]  [TopologicalGroup G]
+def OpenSubgroupSubClopenNhdsOfOne {G : Type*} [Group G] [TopologicalSpace G]  [TopologicalGroup G]
     [CompactSpace G] {W : Set G} (WClopen : IsClopen W) (einW : 1 ∈ W) : OpenSubgroup G where
-  carrier := {x : G | ∃ n : ℕ, x ∈ (openSymmSubnhdsOfOne WClopen einW) ^ n}
+  carrier := {x : G | ∃ n : ℕ, x ∈ (openSymmSubClopenNhdsOfOne WClopen einW) ^ n}
   mul_mem':= by
     rintro a b ⟨na, hna⟩ ⟨nb, hnb⟩
     simp only [Set.mem_setOf_eq] at *
@@ -817,39 +817,57 @@ def OpenSubgroupSubnhdsOfOne {G : Type*} [Group G] [TopologicalSpace G]  [Topolo
     exact Set.mul_mem_mul hna hnb
   one_mem':= by
     use 1
-    simp only [pow_one, openSymmSubnhdsOfOne.one_mem WClopen einW]
+    simp only [pow_one, openSymmSubClopenNhdsOfOne.one_mem WClopen einW]
   inv_mem':= by
     simp only [Set.mem_setOf_eq, forall_exists_index] at *
     intro x m hm
     use m
-    rw [TopologicalGroup.openSymmSubnhdsOfOne.symm]
+    rw [TopologicalGroup.openSymmSubClopenNhdsOfOne.symm]
     simpa only [inv_pow, Set.mem_inv, inv_inv] using hm
   isOpen' := by
-    set V := (openSymmSubnhdsOfOne WClopen einW)
-    simp only [iUnion_pow (openSymmSubnhdsOfOne.one_mem WClopen einW)]
+    set V := (openSymmSubClopenNhdsOfOne WClopen einW)
+    simp only [iUnion_pow (openSymmSubClopenNhdsOfOne.one_mem WClopen einW)]
     apply isOpen_iUnion
     intro n
     rw [pow_succ]
-    exact IsOpen.mul_left (openSymmSubnhdsOfOne.isopen WClopen einW)
+    exact IsOpen.mul_left (openSymmSubClopenNhdsOfOne.isopen WClopen einW)
 
-theorem openSubgroupSubnhdsOfOne_spec {G : Type*} [Group G] [TopologicalSpace G]
+theorem openSubgroupSubClopenNhdsOfOne_spec {G : Type*} [Group G] [TopologicalSpace G]
     [TopologicalGroup G] [CompactSpace G] {W : Set G} (WClopen : IsClopen W) (einW : 1 ∈ W) :
-    ((OpenSubgroupSubnhdsOfOne WClopen einW) : Set G) ⊆ W := by
-  let V := (openSymmSubnhdsOfOne WClopen einW)
+    ((OpenSubgroupSubClopenNhdsOfOne WClopen einW) : Set G) ⊆ W := by
+  let V := (openSymmSubClopenNhdsOfOne WClopen einW)
   show {x : G | ∃ n : ℕ, x ∈ V ^ n} ⊆ W
-  simp_rw [iUnion_pow (openSymmSubnhdsOfOne.one_mem WClopen einW), Set.iUnion_subset_iff]
+  simp_rw [iUnion_pow (openSymmSubClopenNhdsOfOne.one_mem WClopen einW), Set.iUnion_subset_iff]
   intro n nge
   have mulVpow: W * V ^ (n + 1) ⊆ W := by
     induction' n with n ih
     · simp only [zero_add, pow_one]
-      exact openSymmSubnhdsOfOne.mul_sub WClopen einW
+      exact openSymmSubClopenNhdsOfOne.mul_sub WClopen einW
     · rw [pow_succ, ← mul_assoc]
-      exact le_trans (Set.mul_subset_mul_right ih) <| openSymmSubnhdsOfOne.mul_sub WClopen einW
+      exact le_trans (Set.mul_subset_mul_right ih) <|
+        openSymmSubClopenNhdsOfOne.mul_sub WClopen einW
   apply le_trans _ mulVpow
   intro x xin
   rw [Set.mem_mul]
   use 1, einW, x, xin
   rw [one_mul]
+
+/-- The open normal subgroup contained in a clopen nhd of `1` in a compact topological group. -/
+noncomputable def OpenNormalSubgroupSubClopenNhdsOfOne {G : Type*} [Group G] [TopologicalSpace G]
+    [TopologicalGroup G] [CompactSpace G] {U : Set G}
+    (UClopen : IsClopen U) (einU : 1 ∈ U) : OpenNormalSubgroup G :=
+  let H := OpenSubgroupSubClopenNhdsOfOne UClopen einU
+  letI : Subgroup.FiniteIndex H.1 := Subgroup.finiteIndex_of_finite_quotient H.1
+  { toSubgroup := Subgroup.normalCore H
+    isOpen' := TopologicalGroup.finiteindex_closedSubgroup_isOpen _ <|
+      TopologicalGroup.normalCore_isClosed H.1 <| OpenSubgroup.isClosed H }
+
+theorem openNormalSubgroupSubClopenNhdsOfOne_spec {G : Type*} [Group G] [TopologicalSpace G]
+    [TopologicalGroup G] [CompactSpace G] {U : Set G}
+    (UClopen : IsClopen U) (einU : 1 ∈ U) :
+    ((OpenNormalSubgroupSubClopenNhdsOfOne UClopen einU) : Set G) ⊆ U :=
+    fun _ b ↦ openSubgroupSubClopenNhdsOfOne_spec UClopen einU
+      (Subgroup.normalCore_le (OpenSubgroupSubClopenNhdsOfOne UClopen einU).1 b)
 
 end TopologicalGroup
 
