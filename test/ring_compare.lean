d7c50ab0d0749f1bcbb623593671d36c4721380d
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2024-09-16 22:02:28 -0400

    enforce test speed

--- test/ring_compare.lean
+++ test/ring_compare.lean
@@ -60,6 +60,7 @@ demanded by the lemmas it orchestrates.  This example took 1112 heartbeats (and 
 laptop) on an implementation with "minimal" typeclasses everywhere, e.g. lots of
 `CovariantClass`/`ContravariantClass`, and takes 662 heartbeats (28 ms on a good laptop) on the
 implementation at the time of joining Mathlib (September 2024). -/
+set_option maxHeartbeats 750 in
 example : x + y - x + 1 ≤ y + (4:K) := by ring_le
 
 /- The speed of `Mathlib.Tactic.Ring.proveLT` is very sensitive to how much typeclass inference is
