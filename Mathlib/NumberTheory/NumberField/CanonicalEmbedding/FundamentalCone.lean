a4e0b9d1bfbd9ab5cbf2977340b750e4a66432b0
Xavier Roblot <46200072+xroblot@users.noreply.github.com>
2024-09-18 16:25:07 +0200

    Update FundamentalCone.lean

--- Mathlib/NumberTheory/NumberField/CanonicalEmbedding/FundamentalCone.lean
+++ Mathlib/NumberTheory/NumberField/CanonicalEmbedding/FundamentalCone.lean
@@ -3,7 +3,6 @@ Copyright (c) 2024 Xavier Roblot. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Xavier Roblot
 -/
-import Mathlib.RingTheory.Ideal.IsPrincipal
 import Mathlib.NumberTheory.NumberField.Units.DirichletTheorem
 
 /-!
