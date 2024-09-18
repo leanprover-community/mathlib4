1bd91507ba03ce1b85739ecb560c6e55008b362b
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2024-09-18 20:47:23 +0800

    Update InfiniteDimensional.lean

--- Mathlib/FieldTheory/Galois/InfiniteDimensional.lean
+++ Mathlib/FieldTheory/Galois/InfiniteDimensional.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2024 Jujian Zhang. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Jujian Zhang, Nailin Guan, Yuyang Zhao
+Authors: Nailin Guan, Yuyang Zhao, Jujian Zhang
 -/
 import Mathlib.FieldTheory.KrullTopology
 import Mathlib.FieldTheory.NormalClosure
