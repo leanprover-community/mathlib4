716a03cefc4183d1bbad014e76c07ffaf457e048
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2024-09-18 21:44:07 +0800

    move lemmas

--- Mathlib/Algebra/Group/Subgroup/Pointwise.lean
+++ Mathlib/Algebra/Group/Subgroup/Pointwise.lean
@@ -392,6 +392,15 @@ theorem Normal.conjAct {G : Type*} [Group G] {H : Subgroup G} (hH : H.Normal) (g
 theorem smul_normal (g : G) (H : Subgroup G) [h : Normal H] : MulAut.conj g • H = H :=
   h.conjAct g
 
+theorem Normal.of_conjugate_fixed {G : Type*} [Group G] {H : Subgroup G}
+    (h : ∀ g : G, (MulAut.conj g) • H = H) : H.Normal := by
+  constructor
+  intro n hn g
+  rw [← h g, Subgroup.mem_pointwise_smul_iff_inv_smul_mem, ← map_inv, MulAut.smul_def,
+    MulAut.conj_apply, inv_inv, mul_assoc, mul_assoc, inv_mul_cancel, mul_one,
+    ← mul_assoc, inv_mul_cancel, one_mul]
+  exact hn
+
 end Group
 
 section GroupWithZero
--- Mathlib/FieldTheory/Galois/Basic.lean
+++ Mathlib/FieldTheory/Galois/Basic.lean
@@ -289,15 +289,6 @@ open IntermediateField
 
 open scoped Pointwise
 
-private theorem Subgroup.Normal.of_conjugate_fixed {G : Type*} [Group G] {H : Subgroup G}
-    (h : ∀ g : G, (MulAut.conj g) • H = H) : H.Normal := by
-  constructor
-  intro n hn g
-  rw [← h g, Subgroup.mem_pointwise_smul_iff_inv_smul_mem, ← map_inv, MulAut.smul_def,
-    MulAut.conj_apply, inv_inv, mul_assoc, mul_assoc, inv_mul_cancel, mul_one,
-    ← mul_assoc, inv_mul_cancel, one_mul]
-  exact hn
-
 namespace IsGalois
 
 variable (E : IntermediateField K L)
