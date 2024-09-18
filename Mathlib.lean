da505d070a9a471abfe445060f94676695d5ad54
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2024-09-18 20:22:44 +0800

    Update Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -4381,8 +4381,8 @@ import Mathlib.Topology.AlexandrovDiscrete
 import Mathlib.Topology.Algebra.Affine
 import Mathlib.Topology.Algebra.Algebra
 import Mathlib.Topology.Algebra.Algebra.Rat
-import Mathlib.Topology.Algebra.ClosedSubgroup
 import Mathlib.Topology.Algebra.Category.ProfiniteGrp
+import Mathlib.Topology.Algebra.ClosedSubgroup
 import Mathlib.Topology.Algebra.ConstMulAction
 import Mathlib.Topology.Algebra.Constructions
 import Mathlib.Topology.Algebra.Constructions.DomMulAct
