d0ef83d9c8abbd9006f64aafcdd371c8279da2ef
Johan Commelin <johan@commelin.net>
2024-09-19 12:20:08 +0200

    make both source and target outParams

--- Mathlib/Order/Hom/Bounded.lean
+++ Mathlib/Order/Hom/Bounded.lean
@@ -59,7 +59,7 @@ section
 /-- `TopHomClass F α β` states that `F` is a type of `⊤`-preserving morphisms.
 
 You should extend this class when you extend `TopHom`. -/
-class TopHomClass (F : Type*) (α : outParam Type*) (β : Type*) [Top α] [Top β] [FunLike F α β] :
+class TopHomClass (F : Type*) (α β : outParam Type*) [Top α] [Top β] [FunLike F α β] :
     Prop where
   /-- A `TopHomClass` morphism preserves the top element. -/
   map_top (f : F) : f ⊤ = ⊤
@@ -67,7 +67,7 @@ class TopHomClass (F : Type*) (α : outParam Type*) (β : Type*) [Top α] [Top �
 /-- `BotHomClass F α β` states that `F` is a type of `⊥`-preserving morphisms.
 
 You should extend this class when you extend `BotHom`. -/
-class BotHomClass (F : Type*) (α : outParam Type*) (β : Type*) [Bot α] [Bot β] [FunLike F α β] :
+class BotHomClass (F : Type*) (α β : outParam Type*) [Bot α] [Bot β] [FunLike F α β] :
     Prop where
   /-- A `BotHomClass` morphism preserves the bottom element. -/
   map_bot (f : F) : f ⊥ = ⊥
