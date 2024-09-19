fdca662207817c879a8151a0bacbe08e0ceb083e
Kexing Ying <kexing.ying@epfl.ch>
2024-09-19 18:20:27 +0200

    change o to oe

--- Mathlib/Topology/MetricSpace/HoelderNorm.lean
+++ Mathlib/Topology/MetricSpace/HoelderNorm.lean
@@ -3,7 +3,7 @@ Copyright (c) 2024 Kexing Ying. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kexing Ying
 -/
-import Mathlib.Topology.MetricSpace.Holder
+import Mathlib.Topology.MetricSpace.Hoelder
 
 /-!
 # Hölder norm
@@ -22,7 +22,7 @@ for which `WithHoelder C r f` is true.
 ## Main results
 
 * `eHoelderNorm_eq_zero`: the Hölder norm of a function is zero if and only if it is constant.
-* `MemHoelder.holderWith`: The Hölder norm of a Hölder function `f` is a Hölder constant of `f`.
+* `MemHoelder.hoelderWith`: The Hölder norm of a Hölder function `f` is a Hölder constant of `f`.
 
 ## Tags
 
@@ -44,7 +44,7 @@ variable [PseudoEMetricSpace X] [PseudoEMetricSpace Y] {r : ℝ≥0} {f : X → 
 number `C` for which `f` is `r`-Hölder continuous with constant `C`. This is `∞` if no such
 non-negative real exists. -/
 noncomputable
-def eHoelderNorm (r : ℝ≥0) (f : X → Y) : ℝ≥0∞ := ⨅ (C) (_ : HolderWith C r f), C
+def eHoelderNorm (r : ℝ≥0) (f : X → Y) : ℝ≥0∞ := ⨅ (C) (_ : HoelderWith C r f), C
 
 /-- The `r`-Hölder (semi)norm in `ℝ≥0`. -/
 noncomputable
@@ -53,9 +53,9 @@ def nnHoelderNorm (r : ℝ≥0) (f : X → Y) : ℝ≥0 := (eHoelderNorm r f).to
 /-- A function `f` is `MemHoelder r f` if it is Hölder continuous. Namely, `f` has a finite
 `r`-Hölder constant. This is equivalent to `f` having finite Hölder norm.
 c.f. `memHoelder_iff`. -/
-def MemHoelder (r : ℝ≥0) (f : X → Y) : Prop := ∃ C, HolderWith C r f
+def MemHoelder (r : ℝ≥0) (f : X → Y) : Prop := ∃ C, HoelderWith C r f
 
-lemma HolderWith.memHoelder {C : ℝ≥0} (hf : HolderWith C r f) : MemHoelder r f := ⟨C, hf⟩
+lemma HoelderWith.memHoelder {C : ℝ≥0} (hf : HoelderWith C r f) : MemHoelder r f := ⟨C, hf⟩
 
 @[simp] lemma eHoelderNorm_lt_top_iff : eHoelderNorm r f < ∞ ↔ MemHoelder r f := by
   refine ⟨fun h => ?_,
@@ -88,13 +88,13 @@ lemma eHoelderNorm_of_isEmpty [hX : IsEmpty X] :
   rw [eHoelderNorm, ← ENNReal.bot_eq_zero, iInf₂_eq_bot]
   exact fun ε hε => ⟨0, .of_isEmpty, hε⟩
 
-lemma HolderWith.eHoelderNorm_le {C : ℝ≥0} (hf : HolderWith C r f) :
+lemma HoelderWith.eHoelderNorm_le {C : ℝ≥0} (hf : HoelderWith C r f) :
     eHoelderNorm r f ≤ C :=
   iInf₂_le C hf
 
 @[simp]
 lemma memHoelder_const {c : Y} : MemHoelder r (Function.const X c) :=
-  (HolderWith.const (C := 0)).memHoelder
+  (HoelderWith.const (C := 0)).memHoelder
 
 @[simp]
 lemma memHoelder_zero [Zero Y] : MemHoelder r (0 : X → Y) :=
@@ -127,8 +127,8 @@ lemma eHoelderNorm_eq_zero {r : ℝ≥0} {f : X → Y} :
       congr
       simp [funext_iff, h _ hX.some]
 
-lemma MemHoelder.holderWith {r : ℝ≥0} {f : X → Y} (hf : MemHoelder r f) :
-    HolderWith (nnHoelderNorm r f) r f := by
+lemma MemHoelder.hoelderWith {r : ℝ≥0} {f : X → Y} (hf : MemHoelder r f) :
+    HoelderWith (nnHoelderNorm r f) r f := by
   intros x₁ x₂
   by_cases hx : x₁ = x₂
   · simp only [hx, edist_self, zero_le]
@@ -143,16 +143,16 @@ lemma MemHoelder.holderWith {r : ℝ≥0} {f : X → Y} (hf : MemHoelder r f) :
   rw [ENNReal.div_le_iff h₁ h₂]
   exact hC x₁ x₂
 
-lemma memHoelder_iff_holderWith {r : ℝ≥0} {f : X → Y} :
-    MemHoelder r f ↔ HolderWith (nnHoelderNorm r f) r f :=
-  ⟨MemHoelder.holderWith, HolderWith.memHoelder⟩
+lemma memHoelder_iff_hoelderWith {r : ℝ≥0} {f : X → Y} :
+    MemHoelder r f ↔ HoelderWith (nnHoelderNorm r f) r f :=
+  ⟨MemHoelder.hoelderWith, HoelderWith.memHoelder⟩
 
 lemma coe_nnHoelderNorm_le_eHoelderNorm {r : ℝ≥0} {f : X → Y} :
     (nnHoelderNorm r f : ℝ≥0∞) ≤ eHoelderNorm r f :=
   coe_toNNReal_le_self
 
-lemma HolderWith.coe_nnHoelderNorm_eq_eHoelderNorm
-    {C r : ℝ≥0} {f : X → Y} (hf : HolderWith C r f) :
+lemma HoelderWith.coe_nnHoelderNorm_eq_eHoelderNorm
+    {C r : ℝ≥0} {f : X → Y} (hf : HoelderWith C r f) :
     (nnHoelderNorm r f : ℝ≥0∞) = eHoelderNorm r f := by
   rw [nnHoelderNorm, coe_toNNReal]
   exact ne_of_lt <| lt_of_le_of_lt hf.eHoelderNorm_le <| coe_lt_top (r := C)
@@ -160,16 +160,16 @@ lemma HolderWith.coe_nnHoelderNorm_eq_eHoelderNorm
 lemma MemHoelder.coe_nnHoelderNorm_eq_eHoelderNorm
     {r : ℝ≥0} {f : X → Y} (hf : MemHoelder r f) :
     (nnHoelderNorm r f : ℝ≥0∞) = eHoelderNorm r f :=
-  hf.holderWith.coe_nnHoelderNorm_eq_eHoelderNorm
+  hf.hoelderWith.coe_nnHoelderNorm_eq_eHoelderNorm
 
-lemma HolderWith.nnhoelderNorm_le {C r : ℝ≥0} {f : X → Y} (hf : HolderWith C r f) :
+lemma HoelderWith.nnhoelderNorm_le {C r : ℝ≥0} {f : X → Y} (hf : HoelderWith C r f) :
     nnHoelderNorm r f ≤ C := by
   rw [← ENNReal.coe_le_coe, hf.coe_nnHoelderNorm_eq_eHoelderNorm]
   exact hf.eHoelderNorm_le
 
 lemma MemHoelder.comp {r s : ℝ≥0} {Z : Type*} [MetricSpace Z] {f : Z → X} {g : X → Y}
     (hf : MemHoelder r f) (hg : MemHoelder s g) : MemHoelder (s * r) (g ∘ f) :=
-  (hg.holderWith.comp hf.holderWith).memHoelder
+  (hg.hoelderWith.comp hf.hoelderWith).memHoelder
 
 end MetricSpace
 
@@ -179,19 +179,19 @@ variable [MetricSpace X] [NormedAddCommGroup Y]
 variable {C r : ℝ≥0} {f g : X → Y}
 
 lemma MemHoelder.add (hf : MemHoelder r f) (hg : MemHoelder r g) : MemHoelder r (f + g) :=
-  (hf.holderWith.add hg.holderWith).memHoelder
+  (hf.hoelderWith.add hg.hoelderWith).memHoelder
 
 lemma MemHoelder.smul {𝕜} [NormedDivisionRing 𝕜] [Module 𝕜 Y] [BoundedSMul 𝕜 Y]
     {c : 𝕜} (hf : MemHoelder r f) : MemHoelder r (c • f) :=
-  (hf.holderWith.smul c).memHoelder
+  (hf.hoelderWith.smul c).memHoelder
 
 lemma eHoelderNorm_add_le :
     eHoelderNorm r (f + g) ≤ eHoelderNorm r f + eHoelderNorm r g := by
   by_cases hfg : MemHoelder r f  ∧ MemHoelder r g
   · obtain ⟨hf, hg⟩ := hfg
     rw [← hf.coe_nnHoelderNorm_eq_eHoelderNorm, ← hg.coe_nnHoelderNorm_eq_eHoelderNorm, ← coe_add]
-    exact (hf.add hg).holderWith.eHoelderNorm_le.trans <|
-      coe_le_coe.2 (hf.holderWith.add hg.holderWith).nnhoelderNorm_le
+    exact (hf.add hg).hoelderWith.eHoelderNorm_le.trans <|
+      coe_le_coe.2 (hf.hoelderWith.add hg.hoelderWith).nnhoelderNorm_le
   · rw [Classical.not_and_iff_or_not_not, ← eHoelderNorm_eq_top, ← eHoelderNorm_eq_top] at hfg
     obtain (h | h) := hfg
     all_goals simp [h]
@@ -202,15 +202,15 @@ lemma eHoelderNorm_smul {α} [NormedDivisionRing α] [Module α Y] [BoundedSMul 
   · rw [nnnorm_eq_zero] at hc
     simp [hc]
   by_cases hf : MemHoelder r f
-  · refine le_antisymm ((hf.holderWith.smul c).eHoelderNorm_le.trans ?_) <| mul_le_of_le_div' ?_
+  · refine le_antisymm ((hf.hoelderWith.smul c).eHoelderNorm_le.trans ?_) <| mul_le_of_le_div' ?_
     · rw [coe_mul, hf.coe_nnHoelderNorm_eq_eHoelderNorm, mul_comm]
-    · rw [← (hf.holderWith.smul c).coe_nnHoelderNorm_eq_eHoelderNorm, ← coe_div hc]
-      refine HolderWith.eHoelderNorm_le fun x₁ x₂ => ?_
+    · rw [← (hf.hoelderWith.smul c).coe_nnHoelderNorm_eq_eHoelderNorm, ← coe_div hc]
+      refine HoelderWith.eHoelderNorm_le fun x₁ x₂ => ?_
       rw [coe_div hc, ← ENNReal.mul_div_right_comm,
         ENNReal.le_div_iff_mul_le (Or.inl <| coe_ne_zero.2 hc) <| Or.inl coe_ne_top,
         mul_comm, ← smul_eq_mul, ← ENNReal.smul_def, ← edist_smul₀, ← Pi.smul_apply,
         ← Pi.smul_apply]
-      exact hf.smul.holderWith x₁ x₂
+      exact hf.smul.hoelderWith x₁ x₂
   · rw [← eHoelderNorm_eq_top] at hf
     rw [hf, mul_top <| coe_ne_zero.2 hc, eHoelderNorm_eq_top]
     rw [nnnorm_eq_zero] at hc
