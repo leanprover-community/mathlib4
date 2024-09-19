79bd826dea0366c9536be1baef7b8c2f03640629
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2024-09-19 10:09:55 +0200

    Algebra.coe_ofId

--- Mathlib/Algebra/Algebra/Hom.lean
+++ Mathlib/Algebra/Algebra/Hom.lean
@@ -433,6 +433,10 @@ variable {R}
 
 @[simp] lemma ofId_self : ofId R R = .id R R := rfl
 
+-- @[simp]
+-- theorem coe_ofId : ⇑(ofId R A) = ⇑(algebraMap R A) :=
+--   rfl
+
 @[simp]
 theorem ofId_apply (r) : ofId R A r = algebraMap R A r :=
   rfl
