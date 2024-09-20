67ba1dfaf16dd7dcc698f1dc3b5982a25ea4a5a5
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2024-09-20 23:37:29 +0800

    Update OpenSubgroup.lean

--- Mathlib/Topology/Algebra/OpenSubgroup.lean
+++ Mathlib/Topology/Algebra/OpenSubgroup.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2019 Johan Commelin. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Johan Commelin
+Authors: Johan Commelin, Nailin Guan
 -/
 import Mathlib.RingTheory.Ideal.Basic
 import Mathlib.Topology.Algebra.Ring.Basic
