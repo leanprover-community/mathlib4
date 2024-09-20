33706589c8dbea952209552ff1b57e51382f921d
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2024-09-20 19:02:11 +0800

    Update Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -2653,6 +2653,7 @@ import Mathlib.FieldTheory.Finite.Trace
 import Mathlib.FieldTheory.Finiteness
 import Mathlib.FieldTheory.Fixed
 import Mathlib.FieldTheory.Galois.Basic
+import Mathlib.FieldTheory.Galois.InfiniteDimensional
 import Mathlib.FieldTheory.IntermediateField.Algebraic
 import Mathlib.FieldTheory.IntermediateField.Basic
 import Mathlib.FieldTheory.IsAlgClosed.AlgebraicClosure
