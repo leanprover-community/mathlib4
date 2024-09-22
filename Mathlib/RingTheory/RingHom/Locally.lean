106f75ca2ec165ab9f7b91cee83b91fd3247ecac
Christian Merten <christian@merten.dev>
2024-09-22 17:23:46 +0200

    add locally constructor

---
+++ Mathlib/RingTheory/RingHom/Locally.lean
@@ -0,0 +1,177 @@
+/-
+Copyright (c) 2024 Christian Merten. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Christian Merten
+-/
+import Mathlib.Algebra.Module.LocalizedModule
+import Mathlib.RingTheory.LocalProperties
+import Mathlib.RingTheory.Localization.BaseChange
+
+/-!
+# Target local closure of ring homomorphism properties
+
+If `P` is a property of ring homomorphisms, we call `Locally P` the closure of `P` with
+respect to standard open coverings on the (algebraic) target (i.e. geometric source). Hence
+for `f : R →+* S`, the property `Locally P` holds if it holds locally on `S`, i.e. if there exists
+a subset `{ t }` of `S` generating the unit ideal, such that `P` holds for all compositions
+`R →+* Sₜ`.
+
+Assuming without further mention that `P` is stable under composition with isomorphisms,
+`Locally P` is local on the target by construction, i.e. it satisfies
+`OfLocalizationSpanTarget`. If `P` itself is local on the target, `Locally P` coincides with `P`.
+
+The `Locally` construction preserves various properties of `P`, e.g. if `P` is stable under
+composition, base change, etc., so is `Locally P`.
+
+## Main results
+
+- `RingHom.locally_ofLocalizationSpanTarget`: `Locally P` is local on the target.
+
+-/
+
+universe u v
+
+open TensorProduct
+
+namespace RingHom
+
+variable (P : ∀ {R S : Type u} [CommRing R] [CommRing S] (_ : R →+* S), Prop)
+
+/--
+For a property of ring homomorphisms `P`, `Locally P` holds for `f : R →+* S` if
+it holds locally on `S`, i.e. if there exists a subset `{ t }` of `S` generating
+the unit ideal, such that `P` holds for all compositions `R →+* Sₜ`.
+
+We may require `s` to be finite here, for the equivalence, see `locally_iff_finite`.
+-/
+def Locally {R S : Type u} [CommRing R] [CommRing S] (f : R →+* S) : Prop :=
+  ∃ (s : Set S) (_ : Ideal.span s = ⊤),
+    ∀ t ∈ s, P ((algebraMap S (Localization.Away t)).comp f)
+
+variable {R S : Type u} [CommRing R] [CommRing S]
+
+lemma locally_iff_finite (f : R →+* S) :
+    Locally P f ↔ ∃ (s : Finset S) (_ : Ideal.span (s : Set S) = ⊤),
+      ∀ t ∈ s, P ((algebraMap S (Localization.Away t)).comp f) := by
+  constructor
+  · intro ⟨s, hsone, hs⟩
+    obtain ⟨s', h₁, h₂⟩ := (Ideal.span_eq_top_iff_finite s).mp hsone
+    exact ⟨s', h₂, fun t ht ↦ hs t (h₁ ht)⟩
+  · intro ⟨s, hsone, hs⟩
+    use s, hsone, hs
+
+variable {P}
+
+/-- If `P` respects isomorphisms, to check `P` holds locally for `f`, it suffices
+to check `P` holds on a standard open cover. -/
+lemma locally_of_exists (hP : RespectsIso P) (f : R →+* S) {ι : Type*} (s : ι → S)
+    (hsone : Ideal.span (Set.range s) = ⊤)
+    (Sₜ : ι → Type u) [∀ i, CommRing (Sₜ i)] [∀ i, Algebra S (Sₜ i)]
+    [∀ i, IsLocalization.Away (s i) (Sₜ i)] (hf : ∀ i, P ((algebraMap S (Sₜ i)).comp f)) :
+    Locally P f := by
+  use Set.range s, hsone
+  rintro - ⟨i, rfl⟩
+  let e : Localization.Away (s i) ≃+* Sₜ i :=
+    (IsLocalization.algEquiv (Submonoid.powers (s i)) _ _).toRingEquiv
+  have : algebraMap S (Localization.Away (s i)) = e.symm.toRingHom.comp (algebraMap S (Sₜ i)) :=
+    RingHom.ext (fun x ↦ (AlgEquiv.commutes (IsLocalization.algEquiv _ _ _).symm _).symm)
+  rw [this, RingHom.comp_assoc]
+  exact hP.left _ _ (hf i)
+
+/-- Equivalence variant of `locally_of_exists`. This is sometimes easier to use, if the
+`IsLocalization.Away` instance can't be automatically inferred. -/
+lemma locally_iff_exists (hP : RespectsIso P) (f : R →+* S) :
+    Locally P f ↔ ∃ (ι : Type u) (s : ι → S) (_ : Ideal.span (Set.range s) = ⊤) (Sₜ : ι → Type u)
+      (_ : (i : ι) → CommRing (Sₜ i)) (_ : (i : ι) → Algebra S (Sₜ i))
+      (_ : (i : ι) → IsLocalization.Away (s i : S) (Sₜ i)),
+      ∀ i, P ((algebraMap S (Sₜ i)).comp f) :=
+  ⟨fun ⟨s, hsone, hs⟩ ↦ ⟨s, fun t : s ↦ (t : S), by simpa, fun t ↦ Localization.Away (t : S),
+      inferInstance, inferInstance, inferInstance, fun t ↦ hs t.val t.property⟩,
+    fun ⟨ι, s, hsone, Sₜ, _, _, hislocal, hs⟩ ↦ locally_of_exists hP f s hsone Sₜ hs⟩
+
+/-- In the definition of `Locally` we may replace `Localization.Away` with an arbitrary
+algebra satisfying `IsLocalization.Away`. -/
+lemma locally_iff_isLocalization (hP : RespectsIso P) (f : R →+* S) :
+    Locally P f ↔ ∃ (s : Finset S) (_ : Ideal.span (s : Set S) = ⊤),
+      ∀ t ∈ s, ∀ (Sₜ : Type u) [CommRing Sₜ] [Algebra S Sₜ] [IsLocalization.Away t Sₜ],
+      P ((algebraMap S Sₜ).comp f) := by
+  rw [locally_iff_finite P f]
+  refine ⟨fun ⟨s, hsone, hs⟩ ↦ ⟨s, hsone, fun t ht Sₜ _ _ _ ↦ ?_⟩, fun ⟨s, hsone, hs⟩ ↦ ?_⟩
+  · let e : Localization.Away t ≃+* Sₜ :=
+      (IsLocalization.algEquiv (Submonoid.powers t) _ _).toRingEquiv
+    have : algebraMap S Sₜ = e.toRingHom.comp (algebraMap S (Localization.Away t)) :=
+      RingHom.ext (fun x ↦ (AlgEquiv.commutes (IsLocalization.algEquiv _ _ _) _).symm)
+    rw [this, RingHom.comp_assoc]
+    exact hP.left _ _ (hs t ht)
+  · exact ⟨s, hsone, fun t ht ↦ hs t ht _⟩
+
+/-- If `f` satisfies `P`, then in particular it satisfies `Locally P`. -/
+lemma locally_of (hP : RespectsIso P) (f : R →+* S) (hf : P f) : Locally P f := by
+  use {1}
+  let e : S ≃+* Localization.Away (1 : S) :=
+    (IsLocalization.atUnits S (Submonoid.powers 1) (by simp)).toRingEquiv
+  simp only [Set.mem_singleton_iff, forall_eq, Ideal.span_singleton_one, exists_const]
+  exact hP.left f e hf
+
+/-- If `P` is local on the target, then `Locally P` coincides with `P`. -/
+lemma locally_eq_of_localizationSpanTarget (hPi : RespectsIso P)
+    (hPs : OfLocalizationSpanTarget P) {R S : Type u} [CommRing R] [CommRing S] (f : R →+* S) :
+    Locally P f ↔ P f :=
+  ⟨fun ⟨s, hsone, hs⟩ ↦ hPs f s hsone (fun a ↦ hs a.val a.property), locally_of hPi f⟩
+
+section OfLocalizationSpanTarget
+
+private noncomputable abbrev locallyAuxFun (s : Set S) (t : (r : s) → Set (Localization.Away r.val))
+    (p : (a : s) × t a) : S :=
+  p.1 * (IsLocalization.Away.sec p.1.1 p.2.1).1
+
+private lemma span_range_locallyAuxFun_eq_top {s : Set S} (hsone : Ideal.span s = ⊤)
+    {t : (r : s) → Set (Localization.Away r.val)} (htone : ∀ (r : s), Ideal.span (t r) = ⊤) :
+    Ideal.span (Set.range (locallyAuxFun s t)) = ⊤ := by
+  rw [← Ideal.radical_eq_top, eq_top_iff, ← hsone, Ideal.span_le]
+  intro a ha
+  have h₁ : Ideal.span (t ⟨a, ha⟩) ≤ Ideal.span
+      (algebraMap S (Localization.Away a) '' Set.range (locallyAuxFun s t)) := by
+    rw [Ideal.span_le]
+    intro x hx
+    rw [SetLike.mem_coe, IsLocalization.mem_span_map (Submonoid.powers a)]
+    refine ⟨a * (IsLocalization.Away.sec a x).1, Ideal.subset_span ⟨⟨⟨a, ha⟩, ⟨x, hx⟩⟩, rfl⟩, ?_⟩
+    use ⟨a ^ ((IsLocalization.Away.sec a x).2 + 1), _, rfl⟩
+    rw [IsLocalization.eq_mk'_iff_mul_eq, map_pow, map_mul, ← map_pow, pow_add, map_mul,
+      ← mul_assoc, IsLocalization.Away.sec_spec a x, mul_comm, pow_one]
+  have h₂ : IsLocalization.mk' (Localization.Away a) 1 (1 : Submonoid.powers a) ∈ Ideal.span
+      (algebraMap S (Localization.Away a) '' (Set.range <| locallyAuxFun s t)) := by
+    rw [IsLocalization.mk'_one]
+    apply h₁
+    simp [htone]
+  rw [IsLocalization.mem_span_map (Submonoid.powers a)] at h₂
+  obtain ⟨y, hy, ⟨-, m, rfl⟩, hyz⟩ := h₂
+  rw [IsLocalization.eq] at hyz
+  obtain ⟨⟨-, n, rfl⟩, hc⟩ := hyz
+  simp only [← mul_assoc, OneMemClass.coe_one, one_mul, mul_one] at hc
+  use n + m
+  simpa [pow_add, hc] using Ideal.mul_mem_left _ _ hy
+
+/-- `Locally P` is local on the target. -/
+lemma locally_ofLocalizationSpanTarget (hP : RespectsIso P) :
+    OfLocalizationSpanTarget (Locally P) := by
+  intro R S _ _ f s hsone hs
+  choose t htone ht using hs
+  rw [locally_iff_exists hP]
+  refine ⟨(a : s) × t a, locallyAuxFun s t, span_range_locallyAuxFun_eq_top hsone htone,
+      fun ⟨a, b⟩ ↦ Localization.Away b.val, inferInstance, inferInstance, fun ⟨a, b⟩ ↦ ?_, ?_⟩
+  · haveI : IsLocalization.Away ((algebraMap S (Localization.Away a.val))
+        (IsLocalization.Away.sec a.val b.val).1) (Localization.Away b.val) := by
+      apply IsLocalization.Away.of_associated (r := b.val)
+      rw [← IsLocalization.Away.sec_spec]
+      apply associated_mul_unit_right
+      rw [map_pow _ _]
+      exact IsUnit.pow _ (IsLocalization.Away.algebraMap_isUnit _)
+    apply IsLocalization.Away.mul' (Localization.Away a.val) (Localization.Away b.val)
+  · intro ⟨a, b⟩
+    rw [IsScalarTower.algebraMap_eq S (Localization.Away a.val) (Localization.Away b.val)]
+    apply ht _ _ b.property
+
+end OfLocalizationSpanTarget
+
+end RingHom
