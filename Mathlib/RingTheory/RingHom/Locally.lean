3378039c15414503987588306f550cccca0cb9fe
Christian Merten <christian@merten.dev>
2024-09-22 17:37:41 +0200

    fix imports

--- Mathlib/RingTheory/RingHom/Locally.lean
+++ Mathlib/RingTheory/RingHom/Locally.lean
@@ -3,9 +3,7 @@ Copyright (c) 2024 Christian Merten. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Christian Merten
 -/
-import Mathlib.Algebra.Module.LocalizedModule
 import Mathlib.RingTheory.LocalProperties
-import Mathlib.RingTheory.Localization.BaseChange
 
 /-!
 # Target local closure of ring homomorphism properties
