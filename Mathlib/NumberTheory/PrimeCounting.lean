d385e50c8140a8ae7e0b79dcf852b852c5bdfb2a
Moritz Firsching <firsching@google.com>
2024-09-23 10:45:47 +0200

    don't use simp

--- Mathlib/NumberTheory/PrimeCounting.lean
+++ Mathlib/NumberTheory/PrimeCounting.lean
@@ -84,7 +84,7 @@ theorem surjective_primeCounting : Function.Surjective π := by
   suffices Function.Surjective (π ∘ fun n => n - 1) from this.of_comp
   convert surjective_primeCounting'
   ext
-  simp
+  exact primeCounting_sub_one _
 
 open Filter
 
@@ -92,7 +92,7 @@ theorem tendsto_primeCounting' : Tendsto π' atTop atTop := by
   apply tendsto_atTop_atTop_of_monotone' monotone_primeCounting'
   simp [Set.range_iff_surjective.mpr surjective_primeCounting']
 
-theorem tensto_primeCounting : Tendsto π atTop atTop := 
+theorem tensto_primeCounting : Tendsto π atTop atTop :=
   (tendsto_add_atTop_iff_nat 1).mpr tendsto_primeCounting'
 
 @[simp]
