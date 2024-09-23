0aa61183b0ba32a07b90fc6fb5fea72f876fde17
Henrik Böving <hargonix@gmail.com>
2024-09-23 16:07:13 +0200

    steps towards fixing Data.UInt

--- Mathlib/Data/UInt.lean
+++ Mathlib/Data/UInt.lean
@@ -22,6 +22,7 @@ cautious here.
 
 example : (0 : UInt8) = ⟨0⟩ := rfl
 
+
 set_option hygiene false in
 run_cmd
   for typeName' in [`UInt8, `UInt16, `UInt32, `UInt64, `USize] do
@@ -31,24 +32,24 @@ run_cmd
       instance neZero : NeZero size := ⟨by decide⟩
 
       instance : Neg $typeName where
-        neg a := mk (-a.val)
+        neg a := mk ⟨-a.val⟩
 
       instance : Pow $typeName ℕ where
-        pow a n := mk (a.val ^ n)
+        pow a n := mk ⟨a.val ^ n⟩
 
       instance : SMul ℕ $typeName where
-        smul n a := mk (n • a.val)
+        smul n a := mk ⟨n • a.val⟩
 
       instance : SMul ℤ $typeName where
-        smul z a := mk (z • a.val)
+        smul z a := mk ⟨z • a.val⟩
 
-      lemma neg_def (a : $typeName) : -a = ⟨-a.val⟩ := rfl
+      lemma neg_def (a : $typeName) : -a = ⟨⟨-a.val⟩⟩ := rfl
 
-      lemma pow_def (a : $typeName) (n : ℕ) : a ^ n = ⟨a.val ^ n⟩ := rfl
+      lemma pow_def (a : $typeName) (n : ℕ) : a ^ n = ⟨⟨a.val ^ n⟩⟩ := rfl
 
-      lemma nsmul_def (n : ℕ) (a : $typeName) : n • a = ⟨n • a.val⟩ := rfl
+      lemma nsmul_def (n : ℕ) (a : $typeName) : n • a = ⟨⟨n • a.val⟩⟩ := rfl
 
-      lemma zsmul_def (z : ℤ) (a : $typeName) : z • a = ⟨z • a.val⟩ := rfl
+      lemma zsmul_def (z : ℤ) (a : $typeName) : z • a = ⟨⟨z • a.val⟩⟩ := rfl
 
       open $typeName (eq_of_val_eq) in
       lemma val_injective : Function.Injective val := @eq_of_val_eq
@@ -75,7 +76,7 @@ run_cmd
       local instance instCommRing : CommRing $typeName :=
         Function.Injective.commRing val val_injective
           rfl rfl (fun _ _ => rfl) (fun _ _ => rfl) (fun _ => rfl) (fun _ _ => rfl)
-          (fun _ _ => rfl) (fun _ _ => rfl) (fun _ _ => rfl) (fun _ => rfl) (fun _ => rfl)
+          (fun _ _ => rfl) (fun _ _ => rfl) (fun _ _ => rfl) (fun _ => rfl) (fun _ => sorry)
 
       namespace CommRing
       attribute [scoped instance] instCommRing instNatCast instIntCast
@@ -120,6 +121,6 @@ def isASCIIAlphanum (c : UInt8) : Bool :=
 @[deprecated (since := "2024-06-06")] alias isAlphanum := isASCIIAlphanum
 
 /-- The numbers from 0 to 256 are all valid UTF-8 characters, so we can embed one in the other. -/
-def toChar (n : UInt8) : Char := ⟨n.toUInt32, .inl (n.1.2.trans (by decide))⟩
+def toChar (n : UInt8) : Char := ⟨n.toUInt32, .inl (Nat.lt_trans n.toBitVec.isLt (by decide))⟩
 
 end UInt8
