3c83d8edc11603770483581794268ffa342a227e
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2024-09-23 23:53:24 +0800

    Update Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -4392,6 +4392,7 @@ import Mathlib.Topology.Algebra.Affine
 import Mathlib.Topology.Algebra.Algebra
 import Mathlib.Topology.Algebra.Algebra.Rat
 import Mathlib.Topology.Algebra.Category.ProfiniteGrp.Basic
+import Mathlib.Topology.Algebra.Category.ProfiniteGrp.Limits
 import Mathlib.Topology.Algebra.ClosedSubgroup
 import Mathlib.Topology.Algebra.ConstMulAction
 import Mathlib.Topology.Algebra.Constructions
