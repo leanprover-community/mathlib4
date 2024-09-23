b0ef0c7c6152b0bd7f05b9311645ae9760ed01f4
Parcly Taxel <reddeloostw@gmail.com>
2024-09-23 12:17:57 +0800

    shake

--- Mathlib/Std/Data/HashMap.lean
+++ Mathlib/Std/Data/HashMap.lean
@@ -3,7 +3,7 @@ Copyright (c) 2024 Lean FRO. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kim Morrison
 -/
-import Std.Data.HashMap
+import Std.Data.HashMap.Basic
 
 /-!
 # Convenience functions for hash maps
