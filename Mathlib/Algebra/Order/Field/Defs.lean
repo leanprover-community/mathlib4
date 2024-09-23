af069f2a0b7375119e27122067654974168a4801
Yaël Dillies <yael.dillies@gmail.com>
2024-09-23 09:22:41 +0000

    nonpos versions

--- Mathlib/Algebra/Order/Field/Defs.lean
+++ Mathlib/Algebra/Order/Field/Defs.lean
@@ -48,14 +48,30 @@ lemma inv_mul_le_one : a⁻¹ * a ≤ 1 := by obtain rfl | ha := eq_or_ne a 0 <;
 lemma mul_inv_left_le (hb : 0 ≤ b) : a * (a⁻¹ * b) ≤ b := by
   obtain rfl | ha := eq_or_ne a 0 <;> simp [*]
 
+/-- Equality holds when `a ≠ 0`. See `mul_inv_cancel_left`. -/
+lemma le_mul_inv_left (hb : b ≤ 0) : b ≤ a * (a⁻¹ * b) := by
+  obtain rfl | ha := eq_or_ne a 0 <;> simp [*]
+
 /-- Equality holds when `a ≠ 0`. See `inv_mul_cancel_left`. -/
 lemma inv_mul_left_le (hb : 0 ≤ b) : a⁻¹ * (a * b) ≤ b := by
+  obtain rfl | ha := eq_or_ne a 0 <;> simp [*]
+
+/-- Equality holds when `a ≠ 0`. See `inv_mul_cancel_left`. -/
+lemma le_inv_mul_left (hb : b ≤ 0) : b ≤ a⁻¹ * (a * b) := by
   obtain rfl | ha := eq_or_ne a 0 <;> simp [*]
 
 /-- Equality holds when `b ≠ 0`. See `mul_inv_cancel_right`. -/
 lemma mul_inv_right_le (ha : 0 ≤ a) : a * b * b⁻¹ ≤ a := by
   obtain rfl | hb := eq_or_ne b 0 <;> simp [*]
 
+/-- Equality holds when `b ≠ 0`. See `mul_inv_cancel_right`. -/
+lemma le_mul_inv_right (ha : a ≤ 0) : a ≤ a * b * b⁻¹ := by
+  obtain rfl | hb := eq_or_ne b 0 <;> simp [*]
+
 /-- Equality holds when `b ≠ 0`. See `inv_mul_cancel_right`. -/
 lemma inv_mul_right_le (ha : 0 ≤ a) : a * b⁻¹ * b ≤ a := by
+  obtain rfl | hb := eq_or_ne b 0 <;> simp [*]
+
+/-- Equality holds when `b ≠ 0`. See `inv_mul_cancel_right`. -/
+lemma le_inv_mul_right (ha : a ≤ 0) : a ≤ a * b⁻¹ * b := by
   obtain rfl | hb := eq_or_ne b 0 <;> simp [*]
