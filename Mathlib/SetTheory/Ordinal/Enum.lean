7624c01a6340b5cf70706fdb0beeb75b2b625a1a
Violeta Hernández <vi.hdz.p@gmail.com>
2024-09-24 02:32:58 -0600

    fix

--- Mathlib/SetTheory/Ordinal/Enum.lean
+++ Mathlib/SetTheory/Ordinal/Enum.lean
@@ -102,7 +102,7 @@ theorem eq_enumOrd (f : Ordinal → Ordinal) (hs : ¬ BddAbove s) :
     rwa [← (enumOrd_strictMono hs).range_inj h₁, range_enumOrd hs, eq_comm]
 
 theorem enumOrd_range {f : Ordinal → Ordinal} (hf : StrictMono f) : enumOrd (range f) = f :=
-  (eq_enumOrd _ hf.not_bddAbove_range').2 ⟨hf, rfl⟩
+  (eq_enumOrd _ hf.not_bddAbove_range_of_wellFoundedLT).2 ⟨hf, rfl⟩
 
 @[simp]
 theorem enumOrd_univ : enumOrd Set.univ = id := by
