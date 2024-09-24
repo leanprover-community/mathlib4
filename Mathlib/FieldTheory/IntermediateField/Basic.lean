87fd2419df9e35b11fa2c145c02959abd8a0bca5
Junyan Xu <junyanxu.math@gmail.com>
2024-09-24 20:16:51 +0200

    shake

--- Mathlib/FieldTheory/IntermediateField/Basic.lean
+++ Mathlib/FieldTheory/IntermediateField/Basic.lean
@@ -6,7 +6,6 @@ Authors: Anne Baanen
 import Mathlib.Algebra.Field.Subfield
 import Mathlib.Algebra.Polynomial.AlgebraMap
 import Mathlib.RingTheory.LocalRing.Basic
-import Mathlib.RingTheory.IntegralClosure.IsIntegral.Defs
 
 /-!
 # Intermediate fields
