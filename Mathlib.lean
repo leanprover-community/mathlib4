be952f8338706af0b6b3bcc3ab6ee366cfcc0a25
D-Thomine <100795491+D-Thomine@users.noreply.github.com>
2024-09-25 13:57:04 +0000

    feat(Dynamics.TopologicalEntropy): add definition of topological entropy by nets (#16162)
    Third PR in a sequence to implement the notion of topological entropy for maps using Bowen-Dinaburg's formalism. This file introduces a definition of topological entropy using nets.
    
    - [x] Dynamical uniformities
    - [x] Topological entropy via covers
    - [x] Topological entropy via nets
    - [ ] Behaviour under semiconjugacy
    - [ ] Behaviour for subsets and unions
    - [ ] Behaviour under products
    - [ ] Full shift
    
    The main file is `Dynamics.TopologicalEntropy.NetEntropy`. It is slightly shorter and less technical than the previous one. There are also a few modifications to `Dynamics.TopologicalEntropy.CoverEntropy` (some golf, and renamed a lemma to enable dot notation -- which is quite convenient here).
    
    Paging @pitmonticone @sgouezel
    
    
    
    Co-authored-by: Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -2642,6 +2642,7 @@ import Mathlib.Dynamics.OmegaLimit
 import Mathlib.Dynamics.PeriodicPts
 import Mathlib.Dynamics.TopologicalEntropy.CoverEntropy
 import Mathlib.Dynamics.TopologicalEntropy.DynamicalEntourage
+import Mathlib.Dynamics.TopologicalEntropy.NetEntropy
 import Mathlib.FieldTheory.AbelRuffini
 import Mathlib.FieldTheory.AbsoluteGaloisGroup
 import Mathlib.FieldTheory.Adjoin
