a291fb391a57e7057b0a96e6983deb64c3b7a14b
Patrick Massot <patrickmassot@free.fr>
2024-09-25 10:57:04 +0200

    Fix build after rebasing

--- Mathlib/Tactic/Module.lean
+++ Mathlib/Tactic/Module.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Heather Macbeth
 -/
 import Mathlib.Algebra.Algebra.Tower
-import Mathlib.GroupTheory.GroupAction.BigOperators
+import Mathlib.Algebra.BigOperators.GroupWithZero.Action
 import Mathlib.Tactic.Ring
 import Mathlib.Util.AtomM
 
