c7dedd662f7e7e77cc0adc154e4ef928c9113e88
damiano <adomani@gmail.com>
2024-09-25 13:34:40 +0000

    feat: the docPrime linter (#16694)
    This syntax linter emits a warning on declarations whose name ends with a `'` and that have no doc-string.
    
    By default, the linter is
    * `on` on `mathlib` and
    * `off` on projects depending on mathlib.
    
    The file `scripts/no_lints_prime_decls.txt` contains the current exceptions.
    
    The exceptions need to be managed manually by design: the expectation is that, once the linter starts, the nolints file should only decrease and never increase.  In particular, after the linter flags a declaration, simply adding manually an exception is *not enough* for CI to be successful.  There needs to be a change in the file with the exception (or upstream from it), since otherwise `lake` will replay the `olean`s and keep emitting the initial warning.  The expectation is that the change will be the addition of a doc-string.
    
    
    
    Co-authored-by: Michael Rothgang <rothgami@math.hu-berlin.de>

--- Mathlib/Tactic.lean
+++ Mathlib/Tactic.lean
@@ -134,6 +134,7 @@ import Mathlib.Tactic.LinearCombination'
 import Mathlib.Tactic.LinearCombination.Lemmas
 import Mathlib.Tactic.Linter
 import Mathlib.Tactic.Linter.AdmitLinter
+import Mathlib.Tactic.Linter.DocPrime
 import Mathlib.Tactic.Linter.FlexibleLinter
 import Mathlib.Tactic.Linter.GlobalAttributeIn
 import Mathlib.Tactic.Linter.HashCommandLinter
