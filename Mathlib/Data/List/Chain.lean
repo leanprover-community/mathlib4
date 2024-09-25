799d2a89c100ccf7dbe4ff83868f00c9a905d678
Violeta Hernández <vi.hdz.p@gmail.com>
2024-09-25 16:01:01 -0600

    add proofs from bhavik

--- Mathlib/Data/List/Chain.lean
+++ Mathlib/Data/List/Chain.lean
@@ -419,29 +419,29 @@ lemma Chain'.iterate_eq_of_apply_eq {α : Type*} {f : α → α} {l : List α}
     apply hl
     omega
 
-theorem chain'_replicate (n : ℕ) (a : α) : Chain' (· = ·) (replicate n a) :=
+theorem chain_replicate [IsRefl α r] {n : ℕ} {a : α} : Chain r a (replicate n a) :=
+  match n with
+  | 0 => Chain.nil
+  | _ + 1 => Chain.cons (refl a) chain_replicate
+
+theorem chain_eq_iff_eq_replicate {a : α} {l : List α} :
+    Chain (· = ·) a l ↔ l = replicate l.length a :=
+  match l with
+  | [] => by simp
+  | b :: l => by
+    rw [chain_cons]
+    simp (config := {contextual := true}) [eq_comm, replicate_succ, chain_eq_iff_eq_replicate]
+
+theorem chain'_replicate [IsRefl α r] {n : ℕ} {a : α} : Chain' r (replicate n a) :=
   match n with
   | 0 => chain'_nil
-  | 1 => chain'_singleton _
-  | n + 2 => by
-    rw [replicate_succ, replicate_succ, chain'_cons]
-    exact ⟨rfl, chain'_replicate (n + 1) a⟩
+  | _ + 1 => chain_reflexive
 
 theorem chain'_eq_iff_eq_replicate {l : List α} :
     Chain' (· = ·) l ↔ ∀ a ∈ l.head?, l = replicate l.length a :=
   match l with
   | [] => by simp
-  | [a] => by simp
-  | a::b::l => by
-    constructor <;> intro H
-    · intro c hc
-      rw [head?_cons, Option.mem_def, Option.some.injEq] at hc
-      subst hc
-      rw [chain'_cons] at H
-      rw [H.1, length_cons, replicate_succ, cons_inj_right]
-      exact chain'_eq_iff_eq_replicate.1 H.2 _ head?_cons
-    · rw [H a head?_cons]
-      exact chain'_replicate _ _
+  | a :: l => by simp [Chain', chain_eq_iff_eq_replicate, replicate_succ]
 
 end List
 
