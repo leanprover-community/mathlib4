667f81533d165014861ebcc48e802f22dd5aaf19
Violeta Hernández <vi.hdz.p@gmail.com>
2024-09-25 16:21:44 -0600

    explicit args

--- Mathlib/Data/List/Chain.lean
+++ Mathlib/Data/List/Chain.lean
@@ -419,10 +419,10 @@ lemma Chain'.iterate_eq_of_apply_eq {α : Type*} {f : α → α} {l : List α}
     apply hl
     omega
 
-theorem chain_replicate [IsRefl α r] {n : ℕ} {a : α} : Chain r a (replicate n a) :=
+theorem chain_replicate [IsRefl α r] (n : ℕ) (a : α) : Chain r a (replicate n a) :=
   match n with
   | 0 => Chain.nil
-  | _ + 1 => Chain.cons (refl a) chain_replicate
+  | n + 1 => Chain.cons (refl a) (chain_replicate n a)
 
 theorem chain_eq_iff_eq_replicate {a : α} {l : List α} :
     Chain (· = ·) a l ↔ l = replicate l.length a :=
@@ -432,10 +432,10 @@ theorem chain_eq_iff_eq_replicate {a : α} {l : List α} :
     rw [chain_cons]
     simp (config := {contextual := true}) [eq_comm, replicate_succ, chain_eq_iff_eq_replicate]
 
-theorem chain'_replicate [IsRefl α r] {n : ℕ} {a : α} : Chain' r (replicate n a) :=
+theorem chain'_replicate [IsRefl α r] (n : ℕ) (a : α) : Chain' r (replicate n a) :=
   match n with
   | 0 => chain'_nil
-  | _ + 1 => chain_replicate
+  | n + 1 => chain_replicate n a
 
 theorem chain'_eq_iff_eq_replicate {l : List α} :
     Chain' (· = ·) l ↔ ∀ a ∈ l.head?, l = replicate l.length a :=
