f10faacdb46f1c29af0526d452ee797369f51a59
Jon Eugster <eugster.jon@gmail.com>
2024-09-25 12:48:15 +0200

    authour

--- Mathlib/Tactic/Linter/TextBased.lean
+++ Mathlib/Tactic/Linter/TextBased.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2024 Michael Rothgang. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Michael Rothgang, Jon Eugster
+Authors: Michael Rothgang, Adomas Baliuka, Jon Eugster
 -/
 
 import Batteries.Data.String.Matcher
