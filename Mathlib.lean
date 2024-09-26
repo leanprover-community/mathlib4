66205f068d0d50321c7c913232223f1a74bd4fac
Etienne Marion <etienne.marion@ens-lyon.fr>
2024-09-26 10:09:47 +0200

    update Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -1007,6 +1007,7 @@ import Mathlib.Analysis.Calculus.FDeriv.Extend
 import Mathlib.Analysis.Calculus.FDeriv.Linear
 import Mathlib.Analysis.Calculus.FDeriv.Measurable
 import Mathlib.Analysis.Calculus.FDeriv.Mul
+import Mathlib.Analysis.Calculus.FDeriv.Norm
 import Mathlib.Analysis.Calculus.FDeriv.Pi
 import Mathlib.Analysis.Calculus.FDeriv.Prod
 import Mathlib.Analysis.Calculus.FDeriv.RestrictScalars
