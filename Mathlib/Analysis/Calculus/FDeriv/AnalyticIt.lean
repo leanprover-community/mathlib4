1ee4e550262f1debfd98f74f84239b3a892ba5b6
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2024-09-26 09:07:49 +0200

    fix

--- Mathlib/Analysis/Calculus/FDeriv/AnalyticIt.lean
+++ Mathlib/Analysis/Calculus/FDeriv/AnalyticIt.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yury Kudryashov
 -/
 import Mathlib.Analysis.Calculus.FDeriv.Analytic
-import Mathlib.Analysis.Calculus.ContDiff.Basic
+import Mathlib.Analysis.Calculus.ContDiff.Defs
 
 /-!
 # Blank docstring to be filled in
