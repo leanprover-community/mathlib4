1f632e0b0fafb1c4ad326e8aca69e9bb55d1c546
Chris Birkbeck <c.birkbeck@uea.ac.uk>
2024-09-27 18:11:03 +0100

    rev updates

--- Mathlib/LinearAlgebra/Matrix/FixedDetMatrices.lean
+++ Mathlib/LinearAlgebra/Matrix/FixedDetMatrices.lean
@@ -12,36 +12,37 @@ import Mathlib.LinearAlgebra.Matrix.SpecialLinearGroup
 This file defines the type of matrices with fixed determinant `m` and proves some basic results
 about them.
 
-Note: Some of this was done originally in Lean 3 in the kbb repository, so credit to those authors.
+Note: Some of this was done originally in Lean 3 in the
+kbb (https://github.com/kim-em/kbb/tree/master) repository, so credit to those authors.
 -/
 
 variable (n : Type*) [DecidableEq n] [Fintype n] (R : Type*) [CommRing R]
 
 /--The set of matrices with fixed determinant `m`. -/
-def FixedDetMatrices (m : R) := { A : Matrix n n R // A.det = m }
+def FixedDetMatrix (m : R) := { A : Matrix n n R // A.det = m }
 
-namespace FixedDetMatrices
+namespace FixedDetMatrix
 
 open ModularGroup Matrix SpecialLinearGroup MatrixGroups
 
-lemma ext' (m : R) {A B : FixedDetMatrices n R m} (h : A.1 = B.1) : A = B := by
+lemma ext' (m : R) {A B : FixedDetMatrix n R m} (h : A.1 = B.1) : A = B := by
   cases A; cases B
   congr
 
 @[ext]
-lemma ext (m : R) {A B : FixedDetMatrices n R m} (h : ∀ i j , A.1 i j = B.1 i j) : A = B := by
+lemma ext (m : R) {A B : FixedDetMatrix n R m} (h : ∀ i j , A.1 i j = B.1 i j) : A = B := by
   apply ext'
   ext i j
   apply h
 
-instance (m : R) : HSMul (SpecialLinearGroup n R) (FixedDetMatrices n R m)
-    ((FixedDetMatrices n R m)) :=
+instance (m : R) : HSMul (SpecialLinearGroup n R) (FixedDetMatrix n R m)
+    ((FixedDetMatrix n R m)) :=
 { hSMul := fun g A => ⟨g * A.1, by simp only [det_mul, SpecialLinearGroup.det_coe, A.2, one_mul]⟩}
 
-lemma smul_def (m : R) (g : SpecialLinearGroup n R) (A : (FixedDetMatrices n R m)) : g • A =
+lemma smul_def (m : R) (g : SpecialLinearGroup n R) (A : (FixedDetMatrix n R m)) : g • A =
     ⟨g * A.1, by simp only [det_mul, SpecialLinearGroup.det_coe, A.2, one_mul]⟩ := rfl
 
-instance (m : R) : MulAction (SpecialLinearGroup n R) (FixedDetMatrices n R m) where
+instance (m : R) : MulAction (SpecialLinearGroup n R) (FixedDetMatrix n R m) where
   smul := fun g A => g • A
   one_smul := by intro b; rw [smul_def]; simp only [coe_one, one_mul, Subtype.coe_eta]
   mul_smul := by
@@ -49,8 +50,8 @@ instance (m : R) : MulAction (SpecialLinearGroup n R) (FixedDetMatrices n R m) w
       simp_rw [smul_def, ← mul_assoc]
       rfl
 
-lemma smul_coe (m : R) (g : SpecialLinearGroup n R) (A : FixedDetMatrices n R m) :
+lemma smul_coe (m : R) (g : SpecialLinearGroup n R) (A : FixedDetMatrix n R m) :
     (g • A).1 = g * A.1 := by
   rw [smul_def]
 
-end FixedDetMatrices
+end FixedDetMatrix
