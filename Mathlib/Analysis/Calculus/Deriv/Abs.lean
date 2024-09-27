1df77608d3883e8a5b2e5bd56dc797c0b4f4e7b9
Etienne Marion <etienne.marion@ens-lyon.fr>
2024-09-27 11:22:02 +0200

    WIP

--- Mathlib/Analysis/Calculus/Deriv/Abs.lean
+++ Mathlib/Analysis/Calculus/Deriv/Abs.lean
@@ -1,31 +1,130 @@
-import Mathlib.Analysis.Calculus.ContDiff.Basic
 import Mathlib.Analysis.Calculus.Deriv.Add
+import Mathlib.Analysis.InnerProductSpace.Calculus
 
 open Filter Real Set
 
 variable {E : Type*} [NormedAddCommGroup E] [NormedSpace ℝ E]
 variable {n : ℕ∞} {f g : E → ℝ} {s : Set E} {x : E}
 
-theorem contDiffAt_abs {x : ℝ} (hx : x ≠ 0) : ContDiffAt ℝ n (|·|) x := by
-  obtain hx | hx := hx.lt_or_lt
-  · apply contDiff_neg.contDiffAt.congr_of_eventuallyEq
-    exact EqOn.eventuallyEq_of_mem (fun y hy ↦ abs_of_neg (mem_Iio.1 hy)) (Iio_mem_nhds hx)
-  · apply contDiff_id.contDiffAt.congr_of_eventuallyEq
-    exact EqOn.eventuallyEq_of_mem (fun y hy ↦ abs_of_pos (mem_Iio.1 hy)) (Ioi_mem_nhds hx)
+theorem contDiffAt_abs {x : ℝ} (hx : x ≠ 0) : ContDiffAt ℝ n (|·|) x := contDiffAt_norm ℝ hx
 
 theorem ContDiffAt.abs (hf : ContDiffAt ℝ n f x) (h₀ : f x ≠ 0) :
-    ContDiffAt ℝ n (fun x ↦ |f x|) x :=
-  (contDiffAt_abs h₀).comp x hf
+    ContDiffAt ℝ n (fun x ↦ |f x|) x := hf.norm ℝ h₀
+
+theorem contDiffWithinAt_abs {x : ℝ} (hx : x ≠ 0) (s : Set ℝ) :
+    ContDiffWithinAt ℝ n (|·|) s x := (contDiffAt_abs hx).contDiffWithinAt
 
 theorem ContDiffWithinAt.abs (hf : ContDiffWithinAt ℝ n f s x) (h₀ : f x ≠ 0) :
     ContDiffWithinAt ℝ n (fun y ↦ |f y|) s x :=
   (contDiffAt_abs h₀).comp_contDiffWithinAt x hf
+
+theorem contDiffOn_abs {s : Set ℝ} (hs : ∀ x ∈ s, x ≠ 0) :
+    ContDiffOn ℝ n (|·|) s := fun x hx ↦ contDiffWithinAt_abs (hs x hx) s
 
 theorem ContDiffOn.abs (hf : ContDiffOn ℝ n f s) (h₀ : ∀ x ∈ s, f x ≠ 0) :
     ContDiffOn ℝ n (fun y ↦ |f y|) s := fun x hx ↦ (hf x hx).abs (h₀ x hx)
 
 theorem ContDiff.abs (hf : ContDiff ℝ n f) (h₀ : ∀ x, f x ≠ 0) : ContDiff ℝ n fun y ↦ |f y| :=
   contDiff_iff_contDiffAt.2 fun x ↦ hf.contDiffAt.abs (h₀ x)
+
+theorem hasStrictDerivAt_abs_neg {x : ℝ} (hx : x < 0) :
+    HasStrictDerivAt (|·|) (-1) x :=
+  (hasStrictDerivAt_neg x).congr_of_eventuallyEq <|
+    EqOn.eventuallyEq_of_mem (fun _ hy ↦ (abs_of_neg (mem_Iio.1 hy)).symm) (Iio_mem_nhds hx)
+
+theorem hasDerivAt_abs_neg {x : ℝ} (hx : x < 0) :
+    HasDerivAt (|·|) (-1) x := (hasStrictDerivAt_abs_neg hx).hasDerivAt
+
+theorem hasStrictDerivAt_abs_pos {x : ℝ} (hx : 0 < x) :
+    HasStrictDerivAt (|·|) 1 x :=
+  (hasStrictDerivAt_id x).congr_of_eventuallyEq <|
+    EqOn.eventuallyEq_of_mem (fun _ hy ↦ (abs_of_pos (mem_Iio.1 hy)).symm) (Ioi_mem_nhds hx)
+
+theorem hasDerivAt_abs_pos {x : ℝ} (hx : 0 < x) :
+    HasDerivAt (|·|) 1 x := (hasStrictDerivAt_abs_pos hx).hasDerivAt
+
+theorem hasStrictDerivAt_abs {x : ℝ} (hx : x ≠ 0) :
+    HasStrictDerivAt (|·|) (SignType.sign x : ℝ) x := by
+  obtain hx | hx := hx.lt_or_lt
+  · simpa [hx] using hasStrictDerivAt_abs_neg hx
+  · simpa [hx] using hasStrictDerivAt_abs_pos hx
+
+theorem hasDerivAt_abs {x : ℝ} (hx : x ≠ 0) :
+    HasDerivAt (|·|) (SignType.sign x : ℝ) x := (hasStrictDerivAt_abs hx).hasDerivAt
+
+theorem HasStrictFDerivAt.abs_of_neg {f' : E →L[ℝ] ℝ} (hf : HasStrictFDerivAt f f' x)
+    (h₀ : f x < 0) : HasStrictFDerivAt (fun x ↦ |f x|) (-f') x := by
+  convert (hasStrictDerivAt_abs_neg h₀).hasStrictFDerivAt.comp x hf using 1
+  ext y
+  simp
+
+theorem HasFDerivAt.abs_of_neg {f' : E →L[ℝ] ℝ} (hf : HasFDerivAt f f' x)
+    (h₀ : f x < 0) : HasFDerivAt (fun x ↦ |f x|) (-f') x := by
+  convert (hasDerivAt_abs_neg h₀).hasFDerivAt.comp x hf using 1
+  ext y
+  simp
+
+theorem HasStrictFDerivAt.abs_of_pos {f' : E →L[ℝ] ℝ} (hf : HasStrictFDerivAt f f' x)
+    (h₀ : 0 < f x) : HasStrictFDerivAt (fun x ↦ |f x|) f' x := by
+  convert (hasStrictDerivAt_abs_pos h₀).hasStrictFDerivAt.comp x hf using 1
+  ext y
+  simp
+
+theorem HasFDerivAt.abs_of_pos {f' : E →L[ℝ] ℝ} (hf : HasFDerivAt f f' x)
+    (h₀ : 0 < f x) : HasFDerivAt (fun x ↦ |f x|) f' x := by
+  convert (hasDerivAt_abs_pos h₀).hasFDerivAt.comp x hf using 1
+  ext y
+  simp
+
+theorem HasStrictFDerivAt.abs {f' : E →L[ℝ] ℝ} (hf : HasStrictFDerivAt f f' x)
+    (h₀ : f x ≠ 0) : HasStrictFDerivAt (fun x ↦ |f x|) ((SignType.sign (f x) : ℝ) • f') x := by
+  convert (hasStrictDerivAt_abs h₀).hasStrictFDerivAt.comp x hf using 1
+  ext y
+  simp [mul_comm]
+
+theorem HasFDerivAt.abs {f' : E →L[ℝ] ℝ} (hf : HasFDerivAt f f' x)
+    (h₀ : f x ≠ 0) : HasFDerivAt (fun x ↦ |f x|) ((SignType.sign (f x) : ℝ) • f') x := by
+  convert (hasDerivAt_abs h₀).hasFDerivAt.comp x hf using 1
+  ext y
+  simp [mul_comm]
+
+theorem hasDerivWithinAt_abs_neg (s : Set ℝ) {x : ℝ} (hx : x < 0) :
+    HasDerivWithinAt (|·|) (-1) s x := (hasDerivAt_abs_neg hx).hasDerivWithinAt
+
+theorem hasDerivWithinAt_abs_pos (s : Set ℝ) {x : ℝ} (hx : 0 < x) :
+    HasDerivWithinAt (|·|) 1 s x := (hasDerivAt_abs_pos hx).hasDerivWithinAt
+
+theorem hasDerivWithinAt_abs (s : Set ℝ) {x : ℝ} (hx : x ≠ 0) :
+    HasDerivWithinAt (|·|) (SignType.sign x : ℝ) s x := (hasDerivAt_abs hx).hasDerivWithinAt
+
+theorem hasDerivWithinAt_abs_pos {x : ℝ} (hx : 0 < x) :
+    HasDerivWithinAt (|·|) 1 x :=
+  (hasDerivWithinAt_id x).congr_of_eventuallyEq <|
+    EqOn.eventuallyEq_of_mem (fun _ hy ↦ (abs_of_pos (mem_Iio.1 hy)).symm) (Ioi_mem_nhds hx)
+
+theorem hasDerivWithinAt_abs {x : ℝ} (hx : x ≠ 0) :
+    HasDerivWithinAt (|·|) (SignType.sign x : ℝ) x := by
+  obtain hx | hx := hx.lt_or_lt
+  · simpa [hx] using hasDerivWithinAt_abs_neg hx
+  · simpa [hx] using hasDerivWithinAt_abs_pos hx
+
+theorem hasFDerivWithinAt.abs_of_neg {f' : E →L[ℝ] ℝ} (hf : hasFDerivWithinAt f f' x)
+    (h₀ : f x < 0) : hasFDerivWithinAt (fun x ↦ |f x|) (-f') x := by
+  convert (hasStrictDerivAt_abs_neg h₀).hasFDerivWithinAt.comp x hf using 1
+  ext y
+  simp
+
+theorem hasFDerivWithinAt.abs_of_pos {f' : E →L[ℝ] ℝ} (hf : hasFDerivWithinAt f f' x)
+    (h₀ : 0 < f x) : hasFDerivWithinAt (fun x ↦ |f x|) f' x := by
+  convert (hasStrictDerivAt_abs_pos h₀).hasFDerivWithinAt.comp x hf using 1
+  ext y
+  simp
+
+theorem hasFDerivWithinAt.abs {f' : E →L[ℝ] ℝ} (hf : hasFDerivWithinAt f f' x)
+    (h₀ : f x ≠ 0) : hasFDerivWithinAt (fun x ↦ |f x|) ((SignType.sign (f x) : ℝ) • f') x := by
+  convert (hasStrictDerivAt_abs h₀).hasFDerivWithinAt.comp x hf using 1
+  ext y
+  simp [mul_comm]
 
 theorem not_differentiableAt_abs_zero : ¬ DifferentiableAt ℝ (abs : ℝ → ℝ) 0 := by
   intro h
