f6acdac30c70d3f1078eb89342503b736c416cd5
Matthew Ballard <matt@mrb.email>
2024-09-27 06:41:08 -0400

    shake

--- Mathlib/Order/Filter/AtTopBot/ModEq.lean
+++ Mathlib/Order/Filter/AtTopBot/ModEq.lean
@@ -3,9 +3,12 @@ Copyright (c) 2021 Yury Kudryashov. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yury Kudryashov
 -/
-import Mathlib.Algebra.Order.Ring.Abs
 import Mathlib.Data.Nat.ModEq
 import Mathlib.Order.Filter.AtTopBot.Monoid
+import Mathlib.Algebra.Ring.Int
+import Mathlib.Algebra.Order.Ring.Basic
+import Mathlib.Algebra.Order.Ring.Nat
+import Mathlib.Algebra.Ring.Divisibility.Basic
 
 /-!
 # Numbers are frequently ModEq to fixed numbers
