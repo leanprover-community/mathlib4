bfdb15bbfa494aec0f4d193862444ccf7aa23efa
FMLJohn <417963616@qq.com>
2024-09-27 21:03:17 +0100

    deleted the unused import

--- Mathlib/RingTheory/PrimeSpectrum.lean
+++ Mathlib/RingTheory/PrimeSpectrum.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johan Commelin, Filippo A. E. Nuccio, Andrew Yang
 -/
 import Mathlib.LinearAlgebra.Finsupp
-import Mathlib.RingTheory.Ideal.MinimalPrime
 import Mathlib.RingTheory.Ideal.Prod
 import Mathlib.RingTheory.Localization.Ideal
 import Mathlib.RingTheory.Nilpotent.Lemmas
