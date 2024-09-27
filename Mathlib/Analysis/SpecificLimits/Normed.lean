9b836cc82ee86361f97bdfa7d9522d3802803b1c
Chris Birkbeck <c.birkbeck@uea.ac.uk>
2024-09-27 18:35:17 +0100

    Update Mathlib/Analysis/SpecificLimits/Normed.lean
    Co-authored-by: Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>

--- Mathlib/Analysis/SpecificLimits/Normed.lean
+++ Mathlib/Analysis/SpecificLimits/Normed.lean
@@ -230,7 +230,7 @@ theorem tendsto_pow_const_mul_const_pow_of_abs_lt_one (k : ℕ) {r : ℝ} (hr : 
   simpa [div_eq_mul_inv] using tendsto_pow_const_div_const_pow_of_one_lt k hr'
 
 /--For `k ≠ 0` and a constant `r` the function `r / n ^ k` tends to zero. -/
-lemma tendsto_const_div_pow  (r : ℝ) (k : ℕ) (hk : k ≠ 0) :
+lemma tendsto_const_div_pow (r : ℝ) (k : ℕ) (hk : k ≠ 0) :
     Tendsto (fun n : ℕ => r / n ^ k) atTop (𝓝 0) := by
   have h := Filter.Tendsto.const_mul r (l := atTop) (f := fun (n : ℕ ) => 1 / n^k) (c := 0) ?_
   simp only [one_div, mul_zero] at *
