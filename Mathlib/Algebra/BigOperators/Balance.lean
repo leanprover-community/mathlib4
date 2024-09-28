6c19822f84a99ad58276d0908e96a733f9e11c7d
Yaël Dillies <yael.dillies@gmail.com>
2024-09-28 15:42:36 +0000

    rephrase docstring

--- Mathlib/Algebra/BigOperators/Balance.lean
+++ Mathlib/Algebra/BigOperators/Balance.lean
@@ -8,11 +8,11 @@ import Mathlib.Algebra.BigOperators.Expect
 /-!
 # Balancing a function
 
-This file defines the balancing of a function `f`, namely the unique function `g` such that
-`f a - f b = g a - g b` for all `a` and `b`, and `∑ a, g a = 0`.
+This file defines the balancing of a function `f`, defined as `f` minus its average.
 
-This is particularly useful in Fourier analysis as `f` and `g` have the same Fourier transform,
-except in the `0`-th frequency where the Fourier transform of `g` vanishes.
+This is the unique function `g` such that `f a - f b = g a - g b` for all `a` and `b`, and
+`∑ a, g a = 0`. This is particularly useful in Fourier analysis as `f` and `g` then have the same
+Fourier transform, except in the `0`-th frequency where the Fourier transform of `g` vanishes.
 -/
 
 open Function
