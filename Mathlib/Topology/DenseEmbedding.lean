8df11eac8967ec8bf47a4d75aa8c762a9f42d552
Yaël Dillies <yael.dillies@gmail.com>
2024-09-28 14:39:17 +0000

    fix

--- Mathlib/Topology/DenseEmbedding.lean
+++ Mathlib/Topology/DenseEmbedding.lean
@@ -12,7 +12,7 @@ import Mathlib.Topology.Bases
 This file defines three properties of functions:
 
 * `DenseRange f`      means `f` has dense image;
-* `IsDenseInducing i`   means `i` is also `Inducing`, namely it induces the topology on its codomain;
+* `IsDenseInducing i` means `i` is also `Inducing`, namely it induces the topology on its codomain;
 * `DenseEmbedding e`  means `e` is further an `Embedding`, namely it is injective and `Inducing`.
 
 The main theorem `continuous_extend` gives a criterion for a function
