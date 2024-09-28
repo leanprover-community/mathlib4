488d6b54f90af375e6297be294a365284f0a756d
Yury G. Kudryashov <urkud@urkud.name>
2024-09-28 09:50:06 +0000

    feat(Topology): define `IsOpenQuotientMap` (#16549)

--- Mathlib.lean
+++ Mathlib.lean
@@ -4654,6 +4654,7 @@ import Mathlib.Topology.LocallyConstant.Algebra
 import Mathlib.Topology.LocallyConstant.Basic
 import Mathlib.Topology.LocallyFinite
 import Mathlib.Topology.Maps.Basic
+import Mathlib.Topology.Maps.OpenQuotient
 import Mathlib.Topology.Maps.Proper.Basic
 import Mathlib.Topology.Maps.Proper.UniversallyClosed
 import Mathlib.Topology.MetricSpace.Algebra
