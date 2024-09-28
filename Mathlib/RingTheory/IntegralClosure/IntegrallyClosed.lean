c8a27bef6eb959f32079a5599a59186382e6b089
Riccardo Brasca <riccardo.brasca@gmail.com>
2024-09-28 13:58:32 +0200

    fix

--- Mathlib/RingTheory/IntegralClosure/IntegrallyClosed.lean
+++ Mathlib/RingTheory/IntegralClosure/IntegrallyClosed.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Anne Baanen
 -/
 import Mathlib.RingTheory.Localization.Integral
+import Mathlib.RingTheory.Localization.LocalizationLocalization
 
 /-!
 # Integrally closed rings
