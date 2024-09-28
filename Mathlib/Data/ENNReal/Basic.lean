4875f2a6d8b64640d18969a25aa0eb925b26e5b4
Yaël Dillies <yael.dillies@gmail.com>
2024-09-28 10:36:02 +0000

    feat: `positivity` extension for `ENNReal`-valued `ofNat`

--- Mathlib/Data/ENNReal/Basic.lean
+++ Mathlib/Data/ENNReal/Basic.lean
@@ -386,6 +386,9 @@ theorem one_lt_two : (1 : ℝ≥0∞) < 2 := Nat.one_lt_ofNat
 
 @[simp] theorem two_lt_top : (2 : ℝ≥0∞) < ∞ := coe_lt_top
 
+@[simp] lemma ofNat_pos {n : ℕ} [n.AtLeastTwo] : 0 < (no_index OfNat.ofNat n : ℝ≥0∞) :=
+  Nat.cast_pos.2 Fin.size_pos'
+
 /-- `(1 : ℝ≥0∞) ≤ 1`, recorded as a `Fact` for use with `Lp` spaces. -/
 instance _root_.fact_one_le_one_ennreal : Fact ((1 : ℝ≥0∞) ≤ 1) :=
   ⟨le_rfl⟩
@@ -710,7 +713,25 @@ def evalENNRealOfNNReal : PositivityExt where eval {u α} _zα _pα e := do
     | _ => pure .none
   | _, _, _ => throwError "not ENNReal.ofNNReal"
 
+private lemma ennreal_one_pos : (0 : ℝ≥0∞) < 1 := zero_lt_one
+
+/-- The `positivity` extension which identifies expressions of the form `OfNat.ofNat n : ℝ≥0∞`. -/
+@[positivity OfNat.ofNat _] def evalOfNatENNReal : PositivityExt where eval {u} α _z _p e := do
+  match u, α, e with
+  | 0, ~q(ℝ≥0∞), ~q(@OfNat.ofNat _ $n $instn) =>
+    try
+      let instn ← synthInstanceQ q(Nat.AtLeastTwo $n)
+      return Strictness.positive (q(@ofNat_pos $n $instn) : Expr)
+    catch _ => do
+      match n with
+      | ~q(1) => return .positive (q(ennreal_one_pos) : Expr)
+      | _ => throwError "not positive"
+  | _ => throwError "not `ENNReal`-valued `ofNat`"
+
 end Mathlib.Meta.Positivity
+
+example : (0 : ℝ≥0∞) < 1 := by positivity
+example : (0 : ℝ≥0∞) < 2 := by positivity
 
 @[deprecated (since := "2023-12-23")] protected alias
 ENNReal.le_inv_smul_iff_of_pos := le_inv_smul_iff_of_pos
