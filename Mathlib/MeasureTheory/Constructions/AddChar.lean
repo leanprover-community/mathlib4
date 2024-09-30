9e49d5b04dbbe8898dbee68865e874d052a30151
Yaël Dillies <yael.dillies@gmail.com>
2024-09-30 09:35:34 +0000

    feat: `MeasurableSpace` instance for `AddChar` This is stupid, but it's the only case I care about in APAP. Can be made less stupid later.
    From LeanAPAP

---
+++ Mathlib/MeasureTheory/Constructions/AddChar.lean
@@ -0,0 +1,25 @@
+/-
+Copyright (c) 2024 Yaël Dillies. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Yaël Dillies
+-/
+import Mathlib.Algebra.Group.AddChar
+import Mathlib.MeasureTheory.MeasurableSpace.Defs
+
+/-!
+# Measurable space instance for additive characters
+
+This file endows `AddChar A M` with the trivial sigma-algebra.
+
+## TODO
+
+Give the definition in the correct generality.
+-/
+
+namespace AddChar
+variable {A M : Type*} [AddMonoid A] [Monoid M]
+
+instance instMeasurableSpace : MeasurableSpace (AddChar A M) := ⊤
+instance instDiscreteMeasurableSpace : DiscreteMeasurableSpace (AddChar A M) := ⟨fun _ ↦ trivial⟩
+
+end AddChar
