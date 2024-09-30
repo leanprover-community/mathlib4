c5afcfb893ee2ab40ac394a571022e5f32d74e1c
adomani <adomani@gmail.com>
2024-09-30 08:35:13 +0100

    enable both

--- Mathlib/Tactic/Linter/UnusedVariableCommand.lean
+++ Mathlib/Tactic/Linter/UnusedVariableCommand.lean
@@ -133,7 +133,7 @@ def unusedVariableCommandLinter : Linter where run := withSetOptionIn fun stx â†
     elabCommand renStx
     set s
 
---initialize addLinter unusedVariableCommandLinter
+initialize addLinter unusedVariableCommandLinter
 
 end UnusedVariableCommand
 
