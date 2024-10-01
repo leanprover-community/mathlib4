022e3d81042bc444fba46eb29f3aa68953d4c474
Bhavik Mehta <bhavikmehta8@gmail.com>
2024-10-01 12:39:57 +0100

    Update Mathlib/Data/Finset/Sigma.lean
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- Mathlib/Data/Finset/Sigma.lean
+++ Mathlib/Data/Finset/Sigma.lean
@@ -4,8 +4,8 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro, Yaël Dillies, Bhavik Mehta
 -/
 import Mathlib.Data.Finset.Lattice
-import Mathlib.Order.CompleteLattice.Finset
 import Mathlib.Data.Set.Sigma
+import Mathlib.Order.CompleteLattice.Finset
 
 /-!
 # Finite sets in a sigma type
