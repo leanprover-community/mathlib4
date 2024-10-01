3bd477710ac7b86c6e84c27b719dce175ececfa7
Yakov Pechersky <ffxen158@gmail.com>
2024-09-30 20:16:10 -0400

    move from field to ring

--- Mathlib/Analysis/Normed/Ring/Ultra.lean
+++ Mathlib/Analysis/Normed/Ring/Ultra.lean
@@ -7,14 +7,22 @@ import Mathlib.Analysis.Normed.Field.Basic
 import Mathlib.Analysis.Normed.Group.Ultra
 
 /-!
-# Ultrametric norms on division rings
+# Ultrametric norms on rings with a multiplicative norm
 
-This file contains results on the behavior of norms in ultrametric division rings.
+This file contains results on the behavior of norms in ultrametric normed rings.
+The norm must be multiplicative, not just submultiplicative.
 
 ## Main results
 
 * `norm_intCast_le_one`:
   the norm of the image of an integer in the division ring is always less than or equal to one
+
+## Implementation details
+
+A `[NormedRing R]` only assumes a submultiplicative norm. The weakest ring-like structure
+that has a bundled multiplicative norm is `[NormedDivisionRing K]`.
+Since the statements below hold in a multiplicative norm context, we can state them
+in an unbundled fashion using `[NormOneClass R]`.
 
 ## Tags
 
@@ -24,40 +32,40 @@ open Metric NNReal
 
 namespace IsUltrametricDist
 
-section DivisionRing
+section NormOneClass
 
-variable {K : Type*} [NormedDivisionRing K] [IsUltrametricDist K]
+variable {R : Type*} [SeminormedRing R] [NormOneClass R] [IsUltrametricDist R]
 
-lemma norm_add_one_le_max_norm_one (x : K) :
+lemma norm_add_one_le_max_norm_one (x : R) :
     ‖x + 1‖ ≤ max ‖x‖ 1 := by
   simpa using norm_add_le_max x 1
 
-lemma nnnorm_add_one_le_max_nnnorm_one (x : K) :
+lemma nnnorm_add_one_le_max_nnnorm_one (x : R) :
     ‖x + 1‖₊ ≤ max ‖x‖₊ 1 := by
   simpa using norm_add_le_max x 1
 
 lemma nnnorm_natCast_le_one (n : ℕ) :
-    ‖(n : K)‖₊ ≤ 1 := by
+    ‖(n : R)‖₊ ≤ 1 := by
   induction n with
   | zero => simp
-  | succ n hn => simpa [hn] using nnnorm_add_one_le_max_nnnorm_one (n : K)
+  | succ n hn => simpa [hn] using nnnorm_add_one_le_max_nnnorm_one (n : R)
 
 lemma norm_natCast_le_one (n : ℕ) :
-    ‖(n : K)‖ ≤ 1 := by
+    ‖(n : R)‖ ≤ 1 := by
   rw [← Real.toNNReal_le_toNNReal_iff zero_le_one]
   simpa using nnnorm_natCast_le_one n
 
 lemma nnnorm_intCast_le_one (z : ℤ) :
-    ‖(z : K)‖₊ ≤ 1 := by
+    ‖(z : R)‖₊ ≤ 1 := by
   induction z
   · simpa using nnnorm_natCast_le_one _
   · simpa only [Int.cast_negSucc, Nat.cast_one, nnnorm_neg] using nnnorm_natCast_le_one _
 
 lemma norm_intCast_le_one (z : ℤ) :
-    ‖(z : K)‖ ≤ 1 := by
+    ‖(z : R)‖ ≤ 1 := by
   rw [← Real.toNNReal_le_toNNReal_iff zero_le_one]
   simpa using nnnorm_intCast_le_one z
 
-end DivisionRing
+end NormOneClass
 
 end IsUltrametricDist
