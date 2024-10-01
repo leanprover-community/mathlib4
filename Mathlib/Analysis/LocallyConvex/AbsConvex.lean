c7d0b3f0d0367cce8473b8d64722f74a82e91c40
Christopher Hoskin <mans0954@users.noreply.github.com>
2024-10-01 08:46:51 +0100

    Update Mathlib/Analysis/LocallyConvex/AbsConvex.lean
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- Mathlib/Analysis/LocallyConvex/AbsConvex.lean
+++ Mathlib/Analysis/LocallyConvex/AbsConvex.lean
@@ -55,7 +55,7 @@ theorem AbsConvex.empty : AbsConvex 𝕜 (∅ : Set E) := ⟨balanced_empty, con
 theorem AbsConvex.univ : AbsConvex 𝕜 (Set.univ : Set E) := ⟨balanced_univ, convex_univ⟩
 
 theorem AbsConvex.inter {s t : Set E} (hs : AbsConvex 𝕜 s) (ht : AbsConvex 𝕜 t) :
-    AbsConvex 𝕜 (s ∩ t) := ⟨Balanced.inter hs.1 ht.1, Convex.inter hs.2 ht.2⟩
+    AbsConvex 𝕜 (s ∩ t) := ⟨hs.1.inter ht.1, hs.2.inter ht.2⟩
 
 theorem absConvex_sInter {S : Set (Set E)} (h : ∀ s ∈ S, AbsConvex 𝕜 s) : AbsConvex 𝕜 (⋂₀ S) :=
   ⟨Balanced.sInter (fun s hs => (h s hs).1), convex_sInter (fun s hs => (h s hs).2)⟩
