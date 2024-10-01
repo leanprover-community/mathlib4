c24c460adbd11a4ffd84cf75872e20bcf167bdbe
Eric Wieser <wieser.eric@gmail.com>
2024-10-01 11:19:30 +0000

    feat: `Irrational π` (#17103)
    This contains a port of @b-mehta's https://github.com/leanprover-community/mathlib3/commits/irrational-pi branch, with the conversion to Lean 4 performed by @b-mehta with his google hat on.
    
    My contributions here are minimal! 
    
    Co-authored-by: Bhavik Mehta <bhavikmehta8@gmail.com>
    Co-authored-by: Bhavik Mehta <bhavikmehta@google.com>
    
    
    
    Co-authored-by: Bhavik Mehta <bhavikmehta8@gmail.com>
    Co-authored-by: Eric Wieser <efw@google.com>
    Co-authored-by: leanprover-community-mathlib4-bot <leanprover-community-mathlib4-bot@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -2518,6 +2518,7 @@ import Mathlib.Data.Real.GoldenRatio
 import Mathlib.Data.Real.Hyperreal
 import Mathlib.Data.Real.Irrational
 import Mathlib.Data.Real.Pi.Bounds
+import Mathlib.Data.Real.Pi.Irrational
 import Mathlib.Data.Real.Pi.Leibniz
 import Mathlib.Data.Real.Pi.Wallis
 import Mathlib.Data.Real.Pointwise
