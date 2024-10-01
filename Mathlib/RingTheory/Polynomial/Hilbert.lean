8e26802e282cb7f91367f246de0a7c4ea7fb5603
FMLJohn <417963616@qq.com>
2024-10-01 17:25:15 +0100

    removed unused imports

--- Mathlib/RingTheory/Polynomial/Hilbert.lean
+++ Mathlib/RingTheory/Polynomial/Hilbert.lean
@@ -3,8 +3,8 @@ Copyright (c) 2024 Fangming Li. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Fangming Li, Jujian Zhang
 -/
-import Mathlib.RingTheory.LaurentSeries
-import Mathlib.RingTheory.PowerSeries.Inverse
+import Mathlib.Algebra.Module.Rat
+import Mathlib.Algebra.Polynomial.RingDivision
 import Mathlib.RingTheory.PowerSeries.WellKnown
 
 /-!
