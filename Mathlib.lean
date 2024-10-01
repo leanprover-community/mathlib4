280c191d2dddb7b889933c9dc0d39d8629238f20
daniel-carranza <carranzajdaniel@gmail.com>
2024-10-01 07:55:43 -0400

    update Mathlib imports to include Enrichment.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -1456,6 +1456,7 @@ import Mathlib.CategoryTheory.ChosenFiniteProducts
 import Mathlib.CategoryTheory.ChosenFiniteProducts.Cat
 import Mathlib.CategoryTheory.ChosenFiniteProducts.FunctorCategory
 import Mathlib.CategoryTheory.Closed.Cartesian
+import Mathlib.CategoryTheory.Closed.Enrichment
 import Mathlib.CategoryTheory.Closed.Functor
 import Mathlib.CategoryTheory.Closed.FunctorCategory
 import Mathlib.CategoryTheory.Closed.FunctorToTypes
