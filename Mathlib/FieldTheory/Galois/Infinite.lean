b1530de42157e0b3f4a5911071aa71d7a0f7432e
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2024-10-02 21:43:15 +0800

    refine docstring

--- Mathlib/FieldTheory/Galois/Infinite.lean
+++ Mathlib/FieldTheory/Galois/Infinite.lean
@@ -15,7 +15,8 @@ import Mathlib.FieldTheory.SeparableClosure
 
 In a field extension `K/k`
 
-* `FiniteGaloisIntermediateField` : The type of a finite Galois intermediate field of `K/k`
+* `FiniteGaloisIntermediateField` : The type of intermediate fields of `K/k`
+  that are finite and Galois over `k`
 
 * `adjoin` : The finite Galois intermediate field obtained from the normal closure of adjoining a
   `s : Set K` to `k`.
@@ -40,7 +41,7 @@ open scoped IntermediateField
 
 variable (k K : Type*) [Field k] [Field K] [Algebra k K]
 
-/-- The type of a finite Galois intermediate field of `K/k` -/
+/-- The type of intermediate fields of `K/k` that are finite and Galois over `k` -/
 @[ext]
 structure FiniteGaloisIntermediateField extends IntermediateField k K where
   [finiteDimensional : FiniteDimensional k toIntermediateField]
@@ -65,10 +66,10 @@ lemma val_injective : Function.Injective (toIntermediateField (k := k) (K := K))
   rintro ⟨⟩ ⟨⟩ eq
   simpa only [mk.injEq] using eq
 
-/--Make the Finite Galois IntermediateField of `K/k` into an lattice-/
+/-- Turns the collection of finite Galois IntermediateFields of `K/k` into a lattice. -/
 
 instance (L₁ L₂ : IntermediateField k K) [IsGalois k L₁] [IsGalois k L₂] :
-    IsGalois k ↑(L₁ ⊔ L₂) := {}
+    IsGalois k ↑(L₁ ⊔ L₂) where
 
 instance (L₁ L₂ : IntermediateField k K) [FiniteDimensional k L₁] :
     FiniteDimensional k ↑(L₁ ⊓ L₂) :=
