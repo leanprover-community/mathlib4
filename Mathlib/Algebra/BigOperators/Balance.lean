16ae30fcf5f1d4d00963b9dea6563d55cb63a12d
Yaël Dillies <yael.dillies@gmail.com>
2024-10-02 09:10:29 +0000

    this was written in 2023

--- Mathlib/Algebra/BigOperators/Balance.lean
+++ Mathlib/Algebra/BigOperators/Balance.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2024 Yaël Dillies, Bhavik Mehta. All rights reserved.
+Copyright (c) 2023 Yaël Dillies, Bhavik Mehta. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yaël Dillies, Bhavik Mehta
 -/
