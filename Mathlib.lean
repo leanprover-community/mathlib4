454049dd01ad1a99bb435debad62fec4a3b2e572
Christian Merten <christian@merten.dev>
2024-10-03 01:04:48 +0200

    make separate file

--- Mathlib.lean
+++ Mathlib.lean
@@ -3946,6 +3946,7 @@ import Mathlib.RingTheory.Localization.AsSubring
 import Mathlib.RingTheory.Localization.AtPrime
 import Mathlib.RingTheory.Localization.Away.AdjoinRoot
 import Mathlib.RingTheory.Localization.Away.Basic
+import Mathlib.RingTheory.Localization.Away.Lemmas
 import Mathlib.RingTheory.Localization.BaseChange
 import Mathlib.RingTheory.Localization.Basic
 import Mathlib.RingTheory.Localization.Cardinality
