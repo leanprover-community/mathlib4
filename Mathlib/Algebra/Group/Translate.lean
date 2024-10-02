62d67df811a7d2796fc0267127680608129dab10
Yaël Dillies <yael.dillies@gmail.com>
2024-10-02 17:03:51 +0000

    explain why we don't use `DomAddAct`

--- Mathlib/Algebra/Group/Translate.lean
+++ Mathlib/Algebra/Group/Translate.lean
@@ -13,9 +13,17 @@ import Mathlib.GroupTheory.GroupAction.DomAct.Basic
 
 This file defines the translation of a function from a group by an element of that group.
 
-# Notation
+## Notation
 
 `τ a f` is notation for `translate a f`.
+
+## See also
+
+Generally, translation is the same as acting on the domain by subtraction. This setting is
+abstracted by `DomAddAct` in such a way that `τ a f = DomAddAct.mk (-a) +ᵥ f` (see
+`translate_eq_domAddActMk_vadd`). Using `DomAddAct` is irritating in applications because of this
+negation appearing inside `DomAddAct.mk`. Although mathematically equivalent, the pen and paper
+convention is that translating is an action by subtraction, not by addition.
 -/
 
 open Function Set
