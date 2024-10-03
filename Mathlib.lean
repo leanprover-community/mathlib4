2d5610a78a9c94b0a15ec25e14a41a88bac202d0
Andrew Yang <the.erd.one@gmail.com>
2024-10-03 17:44:05 +0000

    feat(RingTheory/Unramified): Formally unramified product of rings (#15141)

--- Mathlib.lean
+++ Mathlib.lean
@@ -4091,6 +4091,7 @@ import Mathlib.RingTheory.Unramified.Basic
 import Mathlib.RingTheory.Unramified.Derivations
 import Mathlib.RingTheory.Unramified.Field
 import Mathlib.RingTheory.Unramified.Finite
+import Mathlib.RingTheory.Unramified.Pi
 import Mathlib.RingTheory.Valuation.AlgebraInstances
 import Mathlib.RingTheory.Valuation.Basic
 import Mathlib.RingTheory.Valuation.ExtendToLocalization
