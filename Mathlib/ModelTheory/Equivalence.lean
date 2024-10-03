94ac73551da7bf708255b72abdf850dd187fb5e8
Aaron Anderson <awainverse@gmail.com>
2024-10-03 16:36:12 +0200

    antisymmetrization

--- Mathlib/ModelTheory/Equivalence.lean
+++ Mathlib/ModelTheory/Equivalence.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Aaron Anderson
 -/
 import Mathlib.ModelTheory.Satisfiability
+import Mathlib.Order.Antisymmetrization
 
 /-!
 # Equivalence of Formulas
@@ -54,6 +55,51 @@ protected theorem trans {φ ψ θ : L.BoundedFormula α n} (h1 : φ ⟹[T] ψ) (
     φ ⟹[T] θ := fun M v xs => (h2 M v xs) ∘ (h1 M v xs)
 
 instance : IsTrans (L.BoundedFormula α n) T.Imp := ⟨fun _ _ _ => Imp.trans⟩
+
+instance : IsPreorder (L.BoundedFormula α n) T.Imp where
+
+def preorder (T : L.Theory) : Preorder (L.Formula α) where
+  le := T.Imp
+  le_refl := Imp.refl
+  le_trans _ _ _ := Imp.trans
+
+theorem realize_bd_imp {φ ψ : L.BoundedFormula α n} (h : φ ⟹[T] ψ)
+    {v : α → M} {xs : Fin n → M} : φ.Realize v xs → ψ.Realize v xs :=
+  BoundedFormula.realize_imp.1 (h.realize_boundedFormula M)
+
+theorem realize_imp {φ ψ : L.Formula α} {M : Type*} [Nonempty M]
+    [L.Structure M] [M ⊨ T] (h : φ ⟹[T] ψ) {v : α → M} :
+    φ.Realize v → ψ.Realize v :=
+  h.realize_bd_imp
+
+theorem models_sentence_imp {φ ψ : L.Sentence} {M : Type*} [Nonempty M]
+    [L.Structure M] [M ⊨ T] (h : φ ⟹[T] ψ) :
+    M ⊨ φ → M ⊨ ψ :=
+  h.realize_imp
+
+protected theorem not {φ ψ : L.BoundedFormula α n} (h : φ ⟹[T] ψ) :
+    ψ.not ⟹[T] φ.not :=
+  fun _ _ _ h1 h2 => h1 (h.realize_bd_imp h2)
+
+protected theorem all {φ ψ : L.BoundedFormula α (n + 1)}
+    (h : φ ⟹[T] ψ) : φ.all ⟹[T] ψ.all :=
+  fun _ _ _ => forall_imp fun _ => h.realize_bd_imp
+
+protected theorem ex {φ ψ : L.BoundedFormula α (n + 1)} (h : φ ⟹[T] ψ) :
+    φ.ex ⟹[T] ψ.ex :=
+  h.not.all.not
+
+theorem imp_mono {φ ψ φ' ψ' : L.BoundedFormula α n} (h : ψ ⟹[T] φ) (h' : φ' ⟹[T] ψ') :
+    (φ.imp φ') ⟹[T] (ψ.imp ψ') :=
+  fun _ _ _ h1 h2 => h'.realize_bd_imp (h1 (h.realize_bd_imp h2))
+
+theorem sup_mono {φ ψ φ' ψ' : L.BoundedFormula α n} (h : φ ⟹[T] ψ) (h' : φ' ⟹[T] ψ') :
+    (φ ⊔ φ') ⟹[T] (ψ ⊔ ψ') :=
+  h.not.imp_mono h'
+
+theorem inf_mono {φ ψ φ' ψ' : L.BoundedFormula α n} (h : φ ⟹[T] ψ) (h' : φ' ⟹[T] ψ') :
+    (φ ⊓ φ') ⟹[T] (ψ ⊓ ψ') :=
+  (h.imp_mono h'.not).not
 
 end Imp
 
@@ -106,17 +152,19 @@ end Imp
 /-- Two (bounded) formulas are semantically equivalent over a theory `T` when they have the same
 interpretation in every model of `T`. (This is also known as logical equivalence, which also has a
 proof-theoretic definition.) -/
-protected def Iff (T : L.Theory) (φ ψ : L.BoundedFormula α n) : Prop :=
-  T ⊨ᵇ φ.iff ψ
+protected def Iff (T : L.Theory) : L.BoundedFormula α n → L.BoundedFormula α n → Prop :=
+  AntisymmRel T.Imp
 
 @[inherit_doc FirstOrder.Language.Theory.Iff]
 scoped[FirstOrder]
 notation:51 φ:50 " ⇔[" T "] " ψ:51 => Language.Theory.Iff T φ ψ
 
 theorem iff_iff_imp_and_imp {φ ψ : L.BoundedFormula α n} :
-    (φ ⇔[T] ψ) ↔ (φ ⟹[T] ψ) ∧ (ψ ⟹[T] φ) := by
-  simp only [Theory.Imp, ModelsBoundedFormula, BoundedFormula.realize_imp, ← forall_and,
-    Theory.Iff, BoundedFormula.realize_iff, iff_iff_implies_and_implies]
+    (φ ⇔[T] ψ) ↔ (φ ⟹[T] ψ) ∧ (ψ ⟹[T] φ) := by rfl
+
+theorem iff_iff_models_iff {φ ψ : L.BoundedFormula α n} : φ ⇔[T] ψ ↔ T ⊨ᵇ φ.iff ψ := by
+  simp only [AntisymmRel, Theory.Imp, ModelsBoundedFormula, BoundedFormula.realize_imp,
+    ← forall_and, Theory.Iff, BoundedFormula.realize_iff, iff_iff_implies_and_implies]
 
 theorem imp_antisymm {φ ψ : L.BoundedFormula α n} (h₁ : φ ⟹[T] ψ) (h₂ : ψ ⟹[T] φ) :
     φ ⇔[T] ψ :=
@@ -124,43 +172,31 @@ theorem imp_antisymm {φ ψ : L.BoundedFormula α n} (h₁ : φ ⟹[T] ψ) (h₂
 
 namespace Iff
 
-protected theorem mp {φ ψ : L.BoundedFormula α n} (h : φ ⇔[T] ψ) :
-    φ ⟹[T] ψ := (iff_iff_imp_and_imp.1 h).1
-
-protected theorem mpr {φ ψ : L.BoundedFormula α n} (h : φ ⇔[T] ψ) :
-    ψ ⟹[T] φ := (iff_iff_imp_and_imp.1 h).2
-
 @[refl]
 protected theorem refl (φ : L.BoundedFormula α n) : φ ⇔[T] φ :=
-  fun M v xs => by rw [BoundedFormula.realize_iff]
+  antisymmRel_refl _ _
 
 instance : IsRefl (L.BoundedFormula α n) T.Iff :=
   ⟨Iff.refl⟩
 
 @[symm]
-protected theorem symm {φ ψ : L.BoundedFormula α n}
-    (h : φ ⇔[T] ψ) : ψ ⇔[T] φ := fun M v xs => by
-  rw [BoundedFormula.realize_iff, Iff.comm, ← BoundedFormula.realize_iff]
-  exact h M v xs
+protected theorem symm {φ ψ : L.BoundedFormula α n} : φ ⇔[T] ψ → ψ ⇔[T] φ :=
+  AntisymmRel.symm
 
 instance : IsSymm (L.BoundedFormula α n) T.Iff :=
   ⟨fun _ _ => Iff.symm⟩
 
 @[trans]
-protected theorem trans {φ ψ θ : L.BoundedFormula α n}
-    (h1 : φ ⇔[T] ψ) (h2 : ψ ⇔[T] θ) :
-    φ ⇔[T] θ := fun M v xs => by
-  have h1' := h1 M v xs
-  have h2' := h2 M v xs
-  rw [BoundedFormula.realize_iff] at *
-  exact ⟨h2'.1 ∘ h1'.1, h1'.2 ∘ h2'.2⟩
+protected theorem trans {φ ψ θ : L.BoundedFormula α n} :
+    φ ⇔[T] ψ → ψ ⇔[T] θ → φ ⇔[T] θ :=
+  AntisymmRel.trans
 
 instance : IsTrans (L.BoundedFormula α n) T.Iff :=
   ⟨fun _ _ _ => Iff.trans⟩
 
 theorem realize_bd_iff {φ ψ : L.BoundedFormula α n} (h : φ ⇔[T] ψ)
     {v : α → M} {xs : Fin n → M} : φ.Realize v xs ↔ ψ.Realize v xs :=
-  BoundedFormula.realize_iff.1 (h.realize_boundedFormula M)
+  BoundedFormula.realize_iff.1 ((iff_iff_models_iff.1 h).realize_boundedFormula M)
 
 theorem realize_iff {φ ψ : L.Formula α} {M : Type*} [Nonempty M]
     [L.Structure M] [M ⊨ T] (h : φ ⇔[T] ψ) {v : α → M} :
@@ -173,42 +209,30 @@ theorem models_sentence_iff {φ ψ : L.Sentence} {M : Type*} [Nonempty M]
   h.realize_iff
 
 protected theorem all {φ ψ : L.BoundedFormula α (n + 1)}
-    (h : φ ⇔[T] ψ) : φ.all ⇔[T] ψ.all := by
-  simp_rw [Theory.Iff, ModelsBoundedFormula, BoundedFormula.realize_iff,
-    BoundedFormula.realize_all]
-  exact fun M v xs => forall_congr' fun a => h.realize_bd_iff
+    (h : φ ⇔[T] ψ) : φ.all ⇔[T] ψ.all :=
+  ⟨h.1.all, h.2.all⟩
 
 protected theorem ex {φ ψ : L.BoundedFormula α (n + 1)} (h : φ ⇔[T] ψ) :
-    φ.ex ⇔[T] ψ.ex := by
-  simp_rw [Theory.Iff, ModelsBoundedFormula, BoundedFormula.realize_iff,
-    BoundedFormula.realize_ex]
-  exact fun M v xs => exists_congr fun a => h.realize_bd_iff
+    φ.ex ⇔[T] ψ.ex :=
+  ⟨h.1.ex, h.2.ex⟩
 
 protected theorem not {φ ψ : L.BoundedFormula α n} (h : φ ⇔[T] ψ) :
-    φ.not ⇔[T] ψ.not := by
-  simp_rw [Theory.Iff, ModelsBoundedFormula, BoundedFormula.realize_iff,
-    BoundedFormula.realize_not]
-  exact fun M v xs => not_congr h.realize_bd_iff
+    φ.not ⇔[T] ψ.not :=
+  ⟨h.2.not, h.1.not⟩
 
-protected theorem imp {φ ψ φ' ψ' : L.BoundedFormula α n} (h : φ ⇔[T] ψ) (h' : φ' ⇔[T] ψ') :
-    (φ.imp φ') ⇔[T] (ψ.imp ψ') := by
-  simp_rw [Theory.Iff, ModelsBoundedFormula, BoundedFormula.realize_iff,
-    BoundedFormula.realize_imp]
-  exact fun M v xs => imp_congr h.realize_bd_iff h'.realize_bd_iff
+protected theorem imp_congr {φ ψ φ' ψ' : L.BoundedFormula α n} (h : φ ⇔[T] ψ) (h' : φ' ⇔[T] ψ') :
+    (φ.imp φ') ⇔[T] (ψ.imp ψ') :=
+  ⟨h.2.imp_mono h'.1, h.1.imp_mono h'.2⟩
 
 protected theorem sup_congr {φ ψ φ' ψ' : L.BoundedFormula α n}
     (h : φ ⇔[T] ψ) (h' : φ' ⇔[T] ψ') :
-    (φ ⊔ φ') ⇔[T] (ψ ⊔ ψ') := by
-  simp_rw [Theory.Iff, ModelsBoundedFormula, BoundedFormula.realize_iff,
-    BoundedFormula.realize_sup]
-  exact fun M v xs => or_congr h.realize_bd_iff h'.realize_bd_iff
+    (φ ⊔ φ') ⇔[T] (ψ ⊔ ψ') :=
+  ⟨h.1.sup_mono h'.1, h.2.sup_mono h'.2⟩
 
 protected theorem inf_congr {φ ψ φ' ψ' : L.BoundedFormula α n}
     (h : φ ⇔[T] ψ) (h' : φ' ⇔[T] ψ') :
-    (φ ⊓ φ') ⇔[T] (ψ ⊓ ψ') := by
-  simp_rw [Theory.Iff, ModelsBoundedFormula, BoundedFormula.realize_iff,
-    BoundedFormula.realize_inf]
-  exact fun M v xs => and_congr h.realize_bd_iff h'.realize_bd_iff
+    (φ ⊓ φ') ⇔[T] (ψ ⊓ ψ') :=
+  ⟨h.1.inf_mono h'.1, h.2.inf_mono h'.2⟩
 
 end Iff
 
@@ -223,35 +247,39 @@ namespace BoundedFormula
 
 variable (φ ψ : L.BoundedFormula α n)
 
-theorem iff_not_not : φ ⇔[T] φ.not.not := fun M v xs => by
-  simp
+theorem iff_not_not : φ ⇔[T] φ.not.not :=
+  Theory.iff_iff_models_iff.2 (fun M v xs => by simp)
 
 theorem imp_iff_not_sup : (φ.imp ψ) ⇔[T] (φ.not ⊔ ψ) :=
-  fun M v xs => by simp [imp_iff_not_or]
+  Theory.iff_iff_models_iff.2 (fun M v xs => by simp [imp_iff_not_or])
 
 theorem sup_iff_not_inf_not : (φ ⊔ ψ) ⇔[T] (φ.not ⊓ ψ.not).not :=
-  fun M v xs => by simp [imp_iff_not_or]
+  Theory.iff_iff_models_iff.2 (fun M v xs => by simp [imp_iff_not_or])
 
 theorem inf_iff_not_sup_not : (φ ⊓ ψ) ⇔[T] (φ.not ⊔ ψ.not).not :=
-  fun M v xs => by simp
+  Theory.iff_iff_models_iff.2 (fun M v xs => by simp)
 
 theorem all_iff_not_ex_not (φ : L.BoundedFormula α (n + 1)) :
-    φ.all ⇔[T] φ.not.ex.not := fun M v xs => by simp
+    φ.all ⇔[T] φ.not.ex.not :=
+  Theory.iff_iff_models_iff.2 (fun M v xs => by simp)
 
 theorem ex_iff_not_all_not (φ : L.BoundedFormula α (n + 1)) :
-    φ.ex ⇔[T] φ.not.all.not := fun M v xs => by simp
+    φ.ex ⇔[T] φ.not.all.not :=
+  Theory.iff_iff_models_iff.2 (fun M v xs => by simp)
 
 theorem iff_all_liftAt : φ ⇔[T] (φ.liftAt 1 n).all :=
-  fun M v xs => by
-  rw [realize_iff, realize_all_liftAt_one_self]
+  Theory.iff_iff_models_iff.2 (fun M v xs => by
+    rw [realize_iff, realize_all_liftAt_one_self])
 
 lemma inf_not_iff_bot :
-    φ ⊓ ∼φ ⇔[T] ⊥ := fun M v xs => by
-  simp only [realize_iff, realize_inf, realize_not, and_not_self, realize_bot]
+    φ ⊓ ∼φ ⇔[T] ⊥ :=
+  Theory.iff_iff_models_iff.2 (fun M v xs => by
+    simp only [realize_iff, realize_inf, realize_not, and_not_self, realize_bot])
 
 lemma sup_not_iff_top :
-    φ ⊔ ∼φ ⇔[T] ⊤ := fun M v xs => by
-  simp only [realize_iff, realize_sup, realize_not, realize_top, iff_true, or_not]
+    φ ⊔ ∼φ ⇔[T] ⊤ :=
+  Theory.iff_iff_models_iff.2 (fun M v xs => by
+    simp only [realize_iff, realize_sup, realize_not, realize_top, iff_true, or_not])
 
 end BoundedFormula
 
@@ -278,30 +306,27 @@ namespace Theory
 variable (T) (α)
 
 /-- The type of formulas modulo a theory. -/
-protected def Formula := Quot (T.Iff : L.Formula α → _)
+protected def Formula := Antisymmetrization (L.Formula α) T.Imp
 
 variable {T} {α}
 
 namespace Formula
 
-instance : Coe (L.Formula α) (T.Formula α) := ⟨Quot.mk _⟩
+instance : Coe (L.Formula α) (T.Formula α) := ⟨toAntisymmetrization _⟩
+
+instance : PartialOrder (T.Formula α) := by
+  letI : Preorder (L.Formula α) := Imp.preorder T
+  exact instPartialOrderAntisymmetrization
 
 @[simps]
 instance : BooleanAlgebra (T.Formula α) where
-  le := Quot.lift₂ T.Imp
-    (fun _ _ _ h => iff_eq_eq.mp ⟨fun h' => h'.trans h.mp, fun h' => h'.trans h.mpr⟩)
-    (fun _ _ _ h => iff_eq_eq.mp ⟨h.mpr.trans, h.mp.trans⟩)
-  le_refl := Quot.ind Imp.refl
-  le_trans := Quot.ind (fun _ => Quot.ind (fun _ => Quot.ind (fun _ => Imp.trans)))
-  le_antisymm := Quot.ind (fun _ => Quot.ind (fun _ h₁ h₂ =>
-    (T.iffSetoid.iseqv.quot_mk_eq_iff _ _).2 (imp_antisymm h₁ h₂)))
-  sup := Quot.map₂ (· ⊔ ·) (fun _ _ _ => (Iff.refl _).sup_congr)
-     (fun _ _ _ h => h.sup_congr (refl _))
+  sup := Quot.map₂ (· ⊔ ·) (fun _ _ _ => Iff.sup_congr (refl _))
+    (fun _ _ _ _ => Iff.sup_congr _ (refl _))
   le_sup_left := Quot.ind (fun _ => Quot.ind (fun _ => imp_sup_left _ _))
   le_sup_right := Quot.ind (fun _ => Quot.ind (fun _ => imp_sup_right _ _))
   sup_le := Quot.ind (fun _ => Quot.ind (fun _ => (Quot.ind (fun _ => sup_imp))))
-  inf := Quot.map₂ (· ⊓ ·) (fun _ _ _ => (Iff.refl _).inf_congr)
-     (fun _ _ _ h => h.inf_congr (refl _))
+  inf := Quot.map₂ (· ⊓ ·) (fun _ _ _ => Iff.inf_congr (refl _))
+    (fun _ _ _ _ => Iff.inf_congr _ (refl _))
   inf_le_left := Quot.ind (fun _ => Quot.ind (fun _ => inf_imp_left _ _))
   inf_le_right := Quot.ind (fun _ => Quot.ind (fun _ => inf_imp_right _ _))
   le_inf := Quot.ind (fun _ => Quot.ind (fun _ => (Quot.ind (fun _ => imp_inf))))
@@ -312,18 +337,20 @@ instance : BooleanAlgebra (T.Formula α) where
   bot_le := Quot.ind bot_imp
   top := Quot.mk _ ⊤
   le_top := Quot.ind imp_top
-  compl := Quot.map Formula.not (fun _ _ h => h.not)
-  inf_compl_le_bot := Quot.ind (fun φ => φ.inf_not_iff_bot.mp)
-  top_le_sup_compl := Quot.ind (fun φ => φ.sup_not_iff_top.mpr)
+  compl := Quot.map Formula.not (fun _ _ h => Iff.not h)
+  inf_compl_le_bot := Quot.ind (fun φ => φ.inf_not_iff_bot.1)
+  top_le_sup_compl := Quot.ind (fun φ => φ.sup_not_iff_top.2)
 
 /-- Evaluates a formula modulo `T` as true or false in a nonempty model of `T`. -/
 @[simp]
 def Realize (φ : T.Formula α) {M : Type*} [L.Structure M] [M ⊨ T] [Nonempty M] (v : α → M) : Prop :=
-  Quot.lift (fun (ψ : L.Formula α) => ψ.Realize v) (fun _ _ h => iff_eq_eq.mp h.realize_iff) φ
+  Quotient.lift (fun (ψ : L.Formula α) => ψ.Realize v)
+    (fun _ _ h => iff_eq_eq.mp (Iff.realize_iff h)) φ
 
 @[simp]
 lemma realize_coe (φ : L.Formula α) {M : Type*} [L.Structure M] [M ⊨ T] [Nonempty M] (v : α → M) :
-    Formula.Realize (↑φ : T.Formula α) v ↔ φ.Realize v := by simp only [Realize]
+    Formula.Realize (↑φ : T.Formula α) v ↔ φ.Realize v := by
+  simp only [Realize, toAntisymmetrization, Quotient.lift_mk]
 
 end Formula
 
