d1b57775c3012de8f85729c2be2f58efca025bfc
jjdishere <emailboxofjjd@163.com>
2024-10-03 16:05:48 +0200

    Update Hom.lean

--- Mathlib/Algebra/Group/Units/Hom.lean
+++ Mathlib/Algebra/Group/Units/Hom.lean
@@ -17,7 +17,7 @@ also contains unrelated results about `Units` that depend on `MonoidHom`.
 * `Units.map`: Turn a homomorphism from `α` to `β` monoids into a homomorphism from `αˣ` to `βˣ`.
 * `MonoidHom.toHomUnits`: Turn a homomorphism from a group `α` to `β` into a homomorphism from
   `α` to `βˣ`.
-* `IsLocalRingHom`: A predicate on monoid homomorphisms, requiring that it maps nonunits
+* `IsLocalRingHom`: A predicate on monoid maps, requiring that it maps nonunits
   to nonunits. For local rings, this means that the image of the unique maximal ideal is again
   contained in the unique maximal ideal. This is developed earlier, and in the generality of
   monoids, as it allows its use in non-local-ring related contexts, but it does have the
