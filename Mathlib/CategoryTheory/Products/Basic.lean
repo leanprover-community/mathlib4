4b1f8fd740e55d286f7da9f07a76fd75db294f05
Markus Himmel <markus@himmel-villmar.de>
2024-10-04 14:21:03 +0200

    feat: variants of `Functor.flipCompEvaluation`

--- Mathlib/CategoryTheory/Products/Basic.lean
+++ Mathlib/CategoryTheory/Products/Basic.lean
@@ -298,6 +298,17 @@ end Equivalence
 def flipCompEvaluation (F : A ⥤ B ⥤ C) (a) : F.flip ⋙ (evaluation _ _).obj a ≅ F.obj a :=
   NatIso.ofComponents fun b => eqToIso rfl
 
+theorem flip_comp_evaluation (F : A ⥤ B ⥤ C) (a) : F.flip ⋙ (evaluation _ _).obj a = F.obj a :=
+  rfl
+
+/-- `F` composed with evaluation is the same as evaluating `F.flip`. -/
+@[simps!]
+def compEvaluation (F : A ⥤ B ⥤ C) (b) : F ⋙ (evaluation _ _).obj b ≅ F.flip.obj b :=
+  NatIso.ofComponents fun a => eqToIso rfl
+
+theorem comp_evaluation (F : A ⥤ B ⥤ C) (b) : F ⋙ (evaluation _ _).obj b = F.flip.obj b :=
+  rfl
+
 variable (A B C)
 
 /-- The forward direction for `functorProdFunctorEquiv` -/
