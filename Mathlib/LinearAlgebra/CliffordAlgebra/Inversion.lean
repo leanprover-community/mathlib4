6e967ac3e96c79778ced98e646bb094c92e268dd
Yury G. Kudryashov <urkud@urkud.name>
2024-10-05 18:26:03 -0500

    Update

--- Mathlib/LinearAlgebra/CliffordAlgebra/Inversion.lean
+++ Mathlib/LinearAlgebra/CliffordAlgebra/Inversion.lean
@@ -49,23 +49,19 @@ theorem invOf_ι_mul_ι_mul_ι (a b : M) [Invertible (ι Q a)] [Invertible (Q a)
   rw [invOf_ι, map_smul, smul_mul_assoc, smul_mul_assoc, ι_mul_ι_mul_ι, ← map_smul, smul_sub,
     smul_smul, smul_smul, invOf_mul_self, one_smul]
 
-section
-variable [Invertible (2 : R)]
-
 /-- Over a ring where `2` is invertible, `Q m` is invertible whenever `ι Q m`. -/
-def invertibleOfInvertibleι (m : M) [Invertible (ι Q m)] : Invertible (Q m) :=
+def invertibleOfInvertibleι [Invertible (2 : R)] (m : M) [Invertible (ι Q m)] : Invertible (Q m) :=
   ExteriorAlgebra.invertibleAlgebraMapEquiv M (Q m) <|
     .algebraMapOfInvertibleAlgebraMap (equivExterior Q).toLinearMap (by simp) <|
       .copy (.mul ‹Invertible (ι Q m)› ‹Invertible (ι Q m)›) _ (ι_sq_scalar _ _).symm
 
-theorem isUnit_of_isUnit_ι {m : M} (h : IsUnit (ι Q m)) : IsUnit (Q m) := by
+theorem isUnit_of_isUnit_ι [IsUnit (2 : R)] {m : M} (h : IsUnit (ι Q m)) : IsUnit (Q m) := by
+  cases ‹IsUnit (2 : R)›.nonempty_invertible
   cases h.nonempty_invertible
   letI := invertibleOfInvertibleι Q m
   exact isUnit_of_invertible (Q m)
 
-@[simp] theorem isUnit_ι_iff {m : M} : IsUnit (ι Q m) ↔ IsUnit (Q m) :=
+@[simp] theorem isUnit_ι_iff [IsUnit (2 : R)] {m : M} : IsUnit (ι Q m) ↔ IsUnit (Q m) :=
   ⟨isUnit_of_isUnit_ι Q, isUnit_ι_of_isUnit Q⟩
-
-end
 
 end CliffordAlgebra
