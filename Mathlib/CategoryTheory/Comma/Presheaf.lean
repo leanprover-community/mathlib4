ec9144ff64eb1e8a5eebd09db1bf79385b2544de
Markus Himmel <markus@himmel-villmar.de>
2024-10-05 19:01:54 +0200

    Shake

--- Mathlib/CategoryTheory/Comma/Presheaf.lean
+++ Mathlib/CategoryTheory/Comma/Presheaf.lean
@@ -3,7 +3,7 @@ Copyright (c) 2024 Markus Himmel. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Markus Himmel
 -/
-import Mathlib.CategoryTheory.Conj
+import Mathlib.CategoryTheory.HomCongr
 import Mathlib.CategoryTheory.Comma.Over
 import Mathlib.Tactic.CategoryTheory.Elementwise
 
