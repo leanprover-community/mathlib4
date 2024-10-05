9c4c60a863942dac60111b57d2d1b432ad8cb9e8
Yaël Dillies <yael.dillies@gmail.com>
2024-10-05 20:06:35 +0000

    fix

--- Mathlib/Order/Filter/AtTopBot/Ring.lean
+++ Mathlib/Order/Filter/AtTopBot/Ring.lean
@@ -3,8 +3,8 @@ Copyright (c) 2019 Yury Kudryashov. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yury Kudryashov
 -/
+import Mathlib.Algebra.Order.Ring.Defs
 import Mathlib.Order.Filter.AtTopBot.Group
-import Mathlib.Algebra.Order.Ring.Basic
 
 /-!
 # Convergence to ±infinity in ordered rings
