1a08f85a5cc23069e6283ff72ea2b73ea50ebcf1
Oliver Nash <github@olivernash.org>
2024-10-06 15:18:10 +0100

    Drop remaining explicit universe parameters

--- Mathlib/LinearAlgebra/Dual.lean
+++ Mathlib/LinearAlgebra/Dual.lean
@@ -651,15 +651,15 @@ instance _root_.MulOpposite.instModuleIsReflexive : IsReflexive R (MulOpposite M
 instance _root_.ULift.instModuleIsReflexive.{w} : IsReflexive R (ULift.{w} M) :=
   equiv ULift.moduleEquiv.symm
 
-instance instFiniteDimensionalOfIsReflexive
-    (K : Type uK) (V : Type uV) [Field K] [AddCommGroup V] [Module K V] [IsReflexive K V] :
+instance instFiniteDimensionalOfIsReflexive (K V : Type*)
+    [Field K] [AddCommGroup V] [Module K V] [IsReflexive K V] :
     FiniteDimensional K V := by
   rw [FiniteDimensional, ← rank_lt_aleph0_iff]
   by_contra! contra
   suffices lift (Module.rank K V) < Module.rank K (Dual K (Dual K V)) by
     have heq := lift_rank_eq_of_equiv_equiv (R := K) (R' := K) (M := V) (M' := Dual K (Dual K V))
       (ZeroHom.id K) (evalEquiv K V) bijective_id (fun r v ↦ (evalEquiv K V).map_smul _ _)
-    rw [← lift_umax.{uV, uK}, heq, lift_id'] at this
+    rw [← lift_umax, heq, lift_id'] at this
     exact lt_irrefl _ this
   have h₁ : lift (Module.rank K V) < Module.rank K (Dual K V) := lift_rank_lt_rank_dual contra
   have h₂ : Module.rank K (Dual K V) < Module.rank K (Dual K (Dual K V)) := by
