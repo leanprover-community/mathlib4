3218a285f942396e759332b92b909a890b428ed5
Jon Bannon <jbannon@siena.edu>
2024-10-06 12:13:53 -0400

    Moved the proof in here, now will try to finish if time permits.

--- Mathlib/Analysis/InnerProductSpace/JointEigenspace.lean
+++ Mathlib/Analysis/InnerProductSpace/JointEigenspace.lean
@@ -162,6 +162,37 @@ theorem LinearMap.IsSymmetric.directSum_isInternal_of_commute_of_fintype [Finite
 
 end RCLike
 
+open Module End
+
+lemma iSup_iInf_maxGenEigenspace_eq_top_of_commute {ι K V : Type*}
+    [Finite ι] [Field K] [DecidableEq K] [AddCommGroup V] [Module K V] [FiniteDimensional K V]
+    (f : ι → End K V) (h : Pairwise fun i j ↦ Commute (f i) (f j))
+    (h' : ∀ i, ⨆ μ, (f i).maxGenEigenspace μ = ⊤) :
+    ⨆ χ : ι → K, ⨅ i, (f i).maxGenEigenspace (χ i) = ⊤ := by
+  have _ := Fintype.ofFinite ι
+  revert f
+  refine Fintype.induction_subsingleton_or_nontrivial ι (fun m _ hhm f hf x ↦ ?_)
+    (fun m hm hmm H f hf hC ↦ ?_)
+  · obtain (hm | hm) := isEmpty_or_nonempty m
+    · simp
+    · have := uniqueOfSubsingleton (Classical.choice hm)
+      simpa [ciInf_unique, ← (Equiv.funUnique m K).symm.iSup_comp] using x default
+  · have i := Classical.arbitrary m
+    classical
+    specialize H {x // x ≠ i} (Fintype.card_subtype_lt (x := i) (by simp))
+     (Subtype.restrict (· ≠ i) f) (fun _ _ _ ↦ hf <| by simpa [Subtype.coe_ne_coe]) (hC ·)
+    rw [← (Equiv.funSplitAt i K).symm.iSup_comp, iSup_prod, iSup_comm]
+    convert H with γ
+    rw [← iSup_eigenspace_restrict (f i) (hf i) (F := ⨅ j, eigenspace _ _)]
+    swap
+    · exact mapsTo_iInf_genEigenspace_of_forall_comm (fun j : {j // j ≠ i} ↦ hC j i) γ 1
+    congr! with μ
+    rw [← Module.End.genEigenspace_one, ← Submodule.inf_genEigenspace _ _ _ (k := 1), inf_comm,
+      iInf_split_single _ i, iInf_subtype]
+    congr! with x hx
+    · simp
+    · simp [dif_neg hx]
+
 end IsSymmetric
 
 end LinearMap
