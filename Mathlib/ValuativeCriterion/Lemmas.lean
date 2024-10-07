d1ea7a86d2875d3a572b1d3ebc27fcf4fce4da89
Christian Merten <christian@merten.dev>
2024-10-07 23:12:59 +0200

    complete valuative criterion file

--- Mathlib/ValuativeCriterion/Lemmas.lean
+++ Mathlib/ValuativeCriterion/Lemmas.lean
@@ -2,6 +2,22 @@ import Mathlib
 
 open CategoryTheory CategoryTheory.Limits
 
+section
+
+lemma _root_.RingHom.injective_stableUnderComposition :
+    RingHom.StableUnderComposition (fun f ↦ Function.Injective f) := by
+  intro R S T _ _ _ f g hf hg
+  simp only [RingHom.coe_comp]
+  exact Function.Injective.comp hg hf
+
+lemma _root_.RingHom.injective_respectsIso :
+    RingHom.RespectsIso (fun f ↦ Function.Injective f) := by
+  apply RingHom.injective_stableUnderComposition.respectsIso
+  intro R S _ _ e
+  exact e.bijective.injective
+
+end
+
 namespace AlgebraicGeometry
 
 universe u
@@ -24,5 +40,14 @@ lemma isIso_iff_of_isAffine [IsAffine Y] :
 lemma schemePreservesSpec (X Y : Scheme) (f : X ⟶ Y) (x x' : X.carrier) (h : x' ⤳ x) :
     (f.val.base x') ⤳ (f.val.base x) :=
   Specializes.map_of_continuousAt h (map_continuousAt f.val.base x)
+
+-- from #15033
+/-- If `f : A ⟶ B` is a ring homomorphism, the corresponding arrow is isomorphic
+to the arrow of the morphism induced on global sections by the map on prime spectra. -/
+noncomputable
+def Scheme.arrowIsoΓSpecOfIsAffine {A B : CommRingCat} (f : A ⟶ B) :
+    Arrow.mk f ≅ Arrow.mk ((Spec.map f).app ⊤) :=
+  Arrow.isoMk (Scheme.ΓSpecIso _).symm (Scheme.ΓSpecIso _).symm
+    (Scheme.ΓSpecIso_inv_naturality f).symm
 
 end AlgebraicGeometry
--- Mathlib/ValuativeCriterion/ValuativeCriterion.lean
+++ Mathlib/ValuativeCriterion/ValuativeCriterion.lean
@@ -53,100 +53,104 @@ https://stacks.math.columbia.edu/tag/01KE
 -/
 lemma specializingMap (H : ValuativeCriterion.Existence f) :
     SpecializingMap f.1.base := by
-      intro x' y h
-      let y' := f.val.base x'
-      let ι_stalk_y := Y.fromSpecStalk y
+  intro x' y h
+  let y' := f.val.base x'
+  let ι_stalk_y := Y.fromSpecStalk y
 
-      let stalk_y_to_residue_x' : Y.presheaf.stalk y ⟶ (X.residueField x') :=
-        (Y.presheaf.stalkSpecializes h) ≫ (f.stalkMap x') ≫ (X.toResidueField x')
+  let stalk_y_to_residue_x' : Y.presheaf.stalk y ⟶ (X.residueField x') :=
+    (Y.presheaf.stalkSpecializes h) ≫ (f.stalkMap x') ≫ (X.toResidueField x')
 
-      let f₁ := Spec.map stalk_y_to_residue_x'
-      let f₂ := X.fromSpecResidueField x'
+  let f₁ := Spec.map stalk_y_to_residue_x'
+  let f₂ := X.fromSpecResidueField x'
 
-      have comm₁ : f₁ ≫ ι_stalk_y = f₂ ≫ f := by
-        simp_all only [Spec.map_comp, Category.assoc, f₁, y', stalk_y_to_residue_x', ι_stalk_y, f₂]
-        rw [@Scheme.stalkSpecializes_fromSpecStalk]
-        rw [@Scheme.stalkMap_fromSpecStalk]
-        rfl
+  have comm₁ : f₁ ≫ ι_stalk_y = f₂ ≫ f := by
+    simp_all only [Spec.map_comp, Category.assoc, f₁, y', stalk_y_to_residue_x', ι_stalk_y, f₂]
+    rw [@Scheme.stalkSpecializes_fromSpecStalk]
+    rw [@Scheme.stalkMap_fromSpecStalk]
+    rfl
 
-      let A := (exists_factor_valuationRing stalk_y_to_residue_x').choose
-      let hA := (exists_factor_valuationRing stalk_y_to_residue_x').choose_spec.choose
-      let stalk_y_to_A_is_local := (exists_factor_valuationRing stalk_y_to_residue_x').choose_spec.choose_spec
+  let A := (exists_factor_valuationRing stalk_y_to_residue_x').choose
+  let hA := (exists_factor_valuationRing stalk_y_to_residue_x').choose_spec.choose
+  let stalk_y_to_A_is_local :=
+    (exists_factor_valuationRing stalk_y_to_residue_x').choose_spec.choose_spec
 
-      let A_to_residue_x' := CommRingCat.ofHom A.subtype
-      let stalk_y_to_A := CommRingCat.ofHom <| stalk_y_to_residue_x'.codRestrict _ hA
+  let A_to_residue_x' := CommRingCat.ofHom A.subtype
+  let stalk_y_to_A := CommRingCat.ofHom <| stalk_y_to_residue_x'.codRestrict _ hA
 
-      have obivious : stalk_y_to_A ≫ A_to_residue_x' = stalk_y_to_residue_x' := rfl
+  have obivious : stalk_y_to_A ≫ A_to_residue_x' = stalk_y_to_residue_x' := rfl
 
-      have comm₂ : f₁ = Spec.map A_to_residue_x' ≫ Spec.map stalk_y_to_A := by
-        rw [← @Spec.map_comp]
-        simp_all only [f₁]
+  have comm₂ : f₁ = Spec.map A_to_residue_x' ≫ Spec.map stalk_y_to_A := by
+    rw [← @Spec.map_comp]
+    simp_all only [f₁]
 
-      have w : f₂ ≫ f =
-          Spec.map (CommRingCat.ofHom (algebraMap A (X.residueField x'))) ≫
-            Spec.map stalk_y_to_A ≫ Y.fromSpecStalk y := by
-        rw [← comm₁, comm₂]
-        simp_all only [Spec.map_comp, Category.assoc, CommRingCat.coe_comp_of, RingHom.coe_comp, Function.comp_apply,
-          f₁, stalk_y_to_residue_x', ι_stalk_y, f₂, stalk_y_to_A, A_to_residue_x', A]
-        rfl
+  have w : f₂ ≫ f =
+      Spec.map (CommRingCat.ofHom (algebraMap A (X.residueField x'))) ≫
+        Spec.map stalk_y_to_A ≫ Y.fromSpecStalk y := by
+    rw [← comm₁, comm₂]
+    simp_all only [Spec.map_comp, Category.assoc, CommRingCat.coe_comp_of, RingHom.coe_comp,
+      Function.comp_apply,
+      f₁, stalk_y_to_residue_x', ι_stalk_y, f₂, stalk_y_to_A, A_to_residue_x', A]
+    rfl
 
-      let commSq : ValuativeCommSq f := {
-        R := A
-        commRing := inferInstance
-        domain := inferInstance
-        valuationRing := inferInstance
-        K := X.residueField x'
-        field := inferInstance
-        algebra := Algebra.ofSubring A.toSubring
-        isFractionRing := ValuationSubring.instIsFractionRingSubtypeMem A
-        i₁ := f₂
-        i₂ := Spec.map stalk_y_to_A ≫ Y.fromSpecStalk y
-        commSq := ⟨w⟩
-      }
+  let commSq : ValuativeCommSq f := {
+    R := A
+    commRing := inferInstance
+    domain := inferInstance
+    valuationRing := inferInstance
+    K := X.residueField x'
+    field := inferInstance
+    algebra := Algebra.ofSubring A.toSubring
+    isFractionRing := ValuationSubring.instIsFractionRingSubtypeMem A
+    i₁ := f₂
+    i₂ := Spec.map stalk_y_to_A ≫ Y.fromSpecStalk y
+    commSq := ⟨w⟩
+  }
 
-      let l := Classical.choice (H commSq).exists_lift
-      let x := l.l.val.base <| LocalRing.closedPoint A
+  let l := Classical.choice (H commSq).exists_lift
+  let x := l.l.val.base <| LocalRing.closedPoint A
 
-      have hx' : x' ⤳ x := by
-        have x'_eq_f₂_cls_pt : f₂.val.base (LocalRing.closedPoint <| X.residueField x') = x' :=
-          Scheme.fromSpecResidueField_apply x' (LocalRing.closedPoint (Scheme.residueField x'))
+  have hx' : x' ⤳ x := by
+    have x'_eq_f₂_cls_pt : f₂.val.base (LocalRing.closedPoint <| X.residueField x') = x' :=
+      Scheme.fromSpecResidueField_apply x' (LocalRing.closedPoint (Scheme.residueField x'))
 
-        have that : (Spec.map A_to_residue_x').val.base (LocalRing.closedPoint <| X.residueField x')
-            ⤳ LocalRing.closedPoint A := by
-          have : LocalRing (CommRingCat.of A) := ValuationSubring.localRing A
-          apply LocalRing.specializes_closedPoint
+    have that : (Spec.map A_to_residue_x').val.base (LocalRing.closedPoint <| X.residueField x')
+        ⤳ LocalRing.closedPoint A := by
+      have : LocalRing (CommRingCat.of A) := ValuationSubring.localRing A
+      apply LocalRing.specializes_closedPoint
 
-        rw [← x'_eq_f₂_cls_pt]
-        simp only [x]
+    rw [← x'_eq_f₂_cls_pt]
+    simp only [x]
 
-        have : f₂.val.base = (Spec.map (CommRingCat.ofHom (algebraMap commSq.R commSq.K)) ≫ l.l).val.base := by rw [l.fac_left]
-        rw [this]
-        exact
-          schemePreservesSpec (Spec (CommRingCat.of ↥A)) X l.l (LocalRing.closedPoint ↥A)
-            ((Spec.map (CommRingCat.ofHom (algebraMap commSq.R commSq.K))).val.base
-              (LocalRing.closedPoint ↑(Scheme.residueField x')))
-                that
+    have : f₂.val.base =
+        (Spec.map (CommRingCat.ofHom (algebraMap commSq.R commSq.K)) ≫ l.l).val.base := by
+      rw [l.fac_left]
+    rw [this]
+    exact
+      schemePreservesSpec (Spec (CommRingCat.of ↥A)) X l.l (LocalRing.closedPoint ↥A)
+        ((Spec.map (CommRingCat.ofHom (algebraMap commSq.R commSq.K))).val.base
+          (LocalRing.closedPoint ↑(Scheme.residueField x')))
+            that
 
-      have hx : f.val.base x = y := by
-        simp only [x]
-        rw [← @Scheme.comp_val_base_apply]
-        rw [l.fac_right]
-        simp only [Scheme.comp_coeBase, TopCat.coe_comp, Function.comp_apply]
-        have :
-            (Spec.map stalk_y_to_A).val.base (LocalRing.closedPoint A) =
-              LocalRing.closedPoint (Y.presheaf.stalk y) := by
-          have : LocalRing <| CommRingCat.of (Y.presheaf.stalk y) :=
-            Y.toLocallyRingedSpace.localRing y
-          have : LocalRing <| CommRingCat.of A := ValuationSubring.localRing A
-          have : IsLocalRingHom stalk_y_to_A := stalk_y_to_A_is_local
-          apply LocalRing.comap_closedPoint
-        change (Y.fromSpecStalk y).val.base
-          ((Spec.map stalk_y_to_A).val.base (LocalRing.closedPoint A)) = y
-        rw [this]
-        exact Y.fromSpecStalk_closedPoint
+  have hx : f.val.base x = y := by
+    simp only [x]
+    rw [← @Scheme.comp_val_base_apply]
+    rw [l.fac_right]
+    simp only [Scheme.comp_coeBase, TopCat.coe_comp, Function.comp_apply]
+    have :
+        (Spec.map stalk_y_to_A).val.base (LocalRing.closedPoint A) =
+          LocalRing.closedPoint (Y.presheaf.stalk y) := by
+      have : LocalRing <| CommRingCat.of (Y.presheaf.stalk y) :=
+        Y.toLocallyRingedSpace.localRing y
+      have : LocalRing <| CommRingCat.of A := ValuationSubring.localRing A
+      have : IsLocalRingHom stalk_y_to_A := stalk_y_to_A_is_local
+      apply LocalRing.comap_closedPoint
+    change (Y.fromSpecStalk y).val.base
+      ((Spec.map stalk_y_to_A).val.base (LocalRing.closedPoint A)) = y
+    rw [this]
+    exact Y.fromSpecStalk_closedPoint
 
-      use x
-      exact ⟨hx', hx⟩
+  use x
+  exact ⟨hx', hx⟩
 
 /--
 Uses `bijective_rangeRestrict_comp_of_valuationRing` and `stalkClosedPointTo`
@@ -157,7 +161,8 @@ lemma of_specializingMap
     (H : (AlgebraicGeometry.topologically @SpecializingMap).universally f) :
       ValuativeCriterion.Existence f := by
   intro c
-  rcases c with @⟨R, commRing, domain, valuationRing, K, field, algebra, isFractionRing, i₁, i₂, commSq⟩
+  rcases c with
+    @⟨R, commRing, domain, valuationRing, K, field, algebra, isFractionRing, i₁, i₂, commSq⟩
   letI : IsDomain (CommRingCat.of R) := domain
   letI : ValuationRing (CommRingCat.of R) := valuationRing
   letI : LocalRing (CommRingCat.of R) := ValuationRing.localRing (CommRingCat.of R)
@@ -206,8 +211,9 @@ lemma of_specializingMap
       comp, R_to_XR_x, XR_x_to_K,
       R_y_to_XR_x, R_y_to_R_x', R_x'_to_R]
 
-    have : Spec.map (stalk_cls_pt_to image_x) ≫ Spec.map (stalkClosedPointIso (CommRingCat.of R)).inv
-        = Spec.map (stalkClosedPointIso' image_x).inv := by
+    have : Spec.map (stalk_cls_pt_to image_x) ≫
+        Spec.map (stalkClosedPointIso (CommRingCat.of R)).inv
+          = Spec.map (stalkClosedPointIso' image_x).inv := by
       rw [← Spec.map_comp]
       apply Spec.map_inj.mpr
       rfl
@@ -233,10 +239,11 @@ lemma of_specializingMap
   let R_in_K_as_val := val_subriing_from_val_ring R K
   let XR_x_in_K := LocalSubring.range XR_x_to_K
 
+  haveI : IsLocalRingHom R_y_to_XR_x :=
+    inferInstanceAs <| IsLocalRingHom (LocallyRingedSpace.Hom.stalkMap _ _)
   letI R_to_XR_x_is_local : IsLocalRingHom R_to_XR_x :=
-    sorry
-    -- CommRingCat.isLocalRingHom_comp (stalkClosedPointIso (CommRingCat.of R)).inv
-    --   (((Spec (CommRingCat.of R)).presheaf.stalkCongr (congrArg nhds hx)).inv ≫ R_y_to_XR_x)
+    CommRingCat.isLocalRingHom_comp (stalkClosedPointIso (CommRingCat.of R)).inv
+      (((Spec (CommRingCat.of R)).presheaf.stalkCongr (congrArg nhds hx)).inv ≫ R_y_to_XR_x)
 
   have R_leq_XR_x : R_in_K_via_comp ≤ XR_x_in_K := by apply domination_preserved_by_range
 
@@ -286,7 +293,9 @@ lemma of_specializingMap
     simp only [Spec_R_to_XR, XR_to_Spec_R]
     have t : (XR.fromSpecStalk x) ≫ pullback.fst i₂ f = Spec.map R_to_XR_x := by
       simp only [R_to_XR_x, R_x'_to_R, R_y_to_R_x', R_y_to_XR_x, Spec.map_comp, Category.assoc]
-      have : Spec.map (stalk_cls_pt_to image_x) ≫ Spec.map (stalkClosedPointIso (CommRingCat.of R)).inv = Spec.map (stalkClosedPointIso' image_x).inv := by
+      have : Spec.map (stalk_cls_pt_to image_x) ≫
+          Spec.map (stalkClosedPointIso (CommRingCat.of R)).inv =
+            Spec.map (stalkClosedPointIso' image_x).inv := by
         rw [← Spec.map_comp]
         rw [stalkClosedPointIso']
       rw [this]
@@ -367,8 +376,8 @@ lemma stableUnderBaseChange :
   exact ⟨⟨⟨l, fac_left, fac_right⟩⟩⟩
 
 /-- by the three lemmas above -/
-lemma eq :
-    ValuativeCriterion.Existence = (AlgebraicGeometry.topologically @SpecializingMap).universally := by
+lemma eq : ValuativeCriterion.Existence =
+    (AlgebraicGeometry.topologically @SpecializingMap).universally := by
   ext
   constructor
   · intro _
@@ -412,7 +421,34 @@ Needs `IsImmersion (pullback.diagonal f)`, `IsClosedImmersion.of_isImmersion`,
 https://stacks.math.columbia.edu/tag/01L0
 -/
 lemma isSeparated_of_valuativeCriterion [QuasiSeparated f]
-    (hf : ValuativeCriterion.Uniqueness f) : IsSeparated f := sorry
+    (hf : ValuativeCriterion.Uniqueness f) : IsSeparated f where
+  diagonal_isClosedImmersion := by
+    suffices h : ValuativeCriterion.Existence (pullback.diagonal f) by
+      have : QuasiCompact (pullback.diagonal f) :=
+        AlgebraicGeometry.QuasiSeparated.diagonalQuasiCompact
+      apply IsClosedImmersion.of_isImmersion
+      apply IsClosedMap.isClosed_range
+      apply (topologically @IsClosedMap).universally_le
+      exact (universallyClosed_of_valuativeCriterion (pullback.diagonal f) h).out
+    intro S
+    have hc : CommSq S.i₁ (Spec.map (CommRingCat.ofHom (algebraMap S.R S.K)))
+        f (S.i₂ ≫ pullback.fst f f ≫ f) := ⟨by simp [← S.commSq.w_assoc]⟩
+    let S' : ValuativeCommSq f := ⟨S.R, S.K, S.i₁, S.i₂ ≫ pullback.fst f f ≫ f, hc⟩
+    have : Subsingleton S'.commSq.LiftStruct := hf S'
+    let S'l₁ : S'.commSq.LiftStruct := ⟨S.i₂ ≫ pullback.fst f f,
+      by simp [← S.commSq.w_assoc], by simp⟩
+    let S'l₂ : S'.commSq.LiftStruct := ⟨S.i₂ ≫ pullback.snd f f,
+      by simp [← S.commSq.w_assoc], by simp [pullback.condition]⟩
+    have h₁₂ : S'l₁ = S'l₂ := Subsingleton.elim _ _
+    constructor
+    constructor
+    refine ⟨S.i₂ ≫ pullback.fst _ _, ?_, ?_⟩
+    · simp [← S.commSq.w_assoc]
+    · simp
+      apply IsPullback.hom_ext (IsPullback.of_hasPullback _ _)
+      · simp
+      · simp only [Category.assoc, pullback.diagonal_snd, Category.comp_id]
+        exact congrArg CommSq.LiftStruct.l h₁₂
 
 /--
 https://stacks.math.columbia.edu/tag/01KZ
@@ -422,36 +458,84 @@ lemma IsSeparated.valuativeCriterion [IsSeparated f] :
   intros S
   constructor
   rintro ⟨l₁, hl₁, hl₁'⟩ ⟨l₂, hl₂, hl₂'⟩
-  ext
+  ext : 1
   dsimp at *
   have h := hl₁'.trans hl₂'.symm
   let Z := pullback (pullback.diagonal f) (pullback.lift l₁ l₂ h)
   let g : Z ⟶ Spec (.of S.R) := pullback.snd _ _
-  have : IsClosedImmersion g := sorry -- by `IsClosedImmersion.isStableUnderBaseChange`
-  have : IsAffine Z := sorry -- by `IsClosedImmersion g` and `IsClosedImmersion.iff_of_isAffine`
+  have : IsClosedImmersion g := IsClosedImmersion.isStableUnderBaseChange.snd _ _ inferInstance
+  have hZ : IsAffine Z := by
+    rw [IsClosedImmersion.iff_of_isAffine] at this
+    exact this.left
   suffices IsIso g by
-    sorry -- by category theory
-  suffices Function.Bijective (g.app ⊤) by
-    sorry -- by `isIso_iff_of_isAffine`
+    rw [← cancel_epi g]
+    conv_lhs => rw [← pullback.lift_fst l₁ l₂ h, ← pullback.condition_assoc]
+    conv_rhs => rw [← pullback.lift_snd l₁ l₂ h, ← pullback.condition_assoc]
+    simp
+  suffices h : Function.Bijective (g.app ⊤) by
+    rw [isIso_iff_of_isAffine]
+    exact ⟨hZ, h⟩
   constructor
-  · let l : Spec (.of S.K) ⟶ Z := sorry
-    have hg : l ≫ g = Spec.map (CommRingCat.ofHom (algebraMap S.R S.K)) := sorry
-    sorry -- Γ of rhs of hg is injective.
-  · sorry -- by `IsClosedImmersion g` and `IsClosedImmersion.iff_of_isAffine`
-  sorry
+  · let l : Spec (.of S.K) ⟶ Z := by
+      apply pullback.lift S.i₁ (Spec.map (CommRingCat.ofHom (algebraMap S.R S.K)))
+      apply IsPullback.hom_ext (IsPullback.of_hasPullback _ _)
+      simpa using hl₁.symm
+      simpa using hl₂.symm
+    have hg : l ≫ g = Spec.map (CommRingCat.ofHom (algebraMap S.R S.K)) :=
+      pullback.lift_snd _ _ _
+    have : Function.Injective ((l ≫ g).app ⊤) := by
+      rw [hg]
+      let e := Scheme.arrowIsoΓSpecOfIsAffine (CommRingCat.ofHom <| algebraMap S.R S.K)
+      let P : MorphismProperty CommRingCat :=
+        RingHom.toMorphismProperty <| fun f ↦ Function.Injective f
+      have : (RingHom.toMorphismProperty <| fun f ↦ Function.Injective f).RespectsIso :=
+        RingHom.toMorphismProperty_respectsIso_iff.mp RingHom.injective_respectsIso
+      show P _
+      rw [← MorphismProperty.arrow_mk_iso_iff (P := P) e]
+      exact NoZeroSMulDivisors.algebraMap_injective S.R S.K
+    rw [Scheme.comp_app _ _] at this
+    exact Function.Injective.of_comp this
+  · rw [IsClosedImmersion.iff_of_isAffine] at this
+    exact this.right
 
 -- by lemmas above
 lemma IsSeparated_eq_valuativeCriterion :
-    @IsSeparated = ValuativeCriterion.Uniqueness ⊓ @QuasiSeparated := sorry
+    @IsSeparated = ValuativeCriterion.Uniqueness ⊓ @QuasiSeparated := by
+  ext X Y f
+  constructor
+  · intro hf
+    exact ⟨IsSeparated.valuativeCriterion f, inferInstance⟩
+  · intro ⟨hu, _⟩
+    exact isSeparated_of_valuativeCriterion f hu
 
 end Uniqueness
 
 -- by definition
 lemma valuativeCriterion_eq :
-    ValuativeCriterion = ValuativeCriterion.Existence ⊓ ValuativeCriterion.Uniqueness := sorry
+    ValuativeCriterion = ValuativeCriterion.Existence ⊓ ValuativeCriterion.Uniqueness := by
+  ext X Y f
+  constructor
+  · intro hf
+    refine ⟨fun S ↦ ?_, fun S ↦ ?_⟩
+    · obtain ⟨(u : Unique (S.commSq.LiftStruct))⟩ := hf S
+      exact CommSq.HasLift.mk' default
+    · obtain ⟨(u : Unique (S.commSq.LiftStruct))⟩ := hf S
+      infer_instance
+  · intro ⟨he, hu⟩ S
+    rw [unique_iff_subsingleton_and_nonempty]
+    exact ⟨hu S, (he S).1⟩
 
 -- by lemmas above and `isProper_eq`
 lemma proper_eq_ValuativeCriterion :
-    @IsProper = ValuativeCriterion ⊓ @QuasiCompact ⊓ @QuasiSeparated ⊓ @LocallyOfFiniteType := sorry
+    @IsProper = ValuativeCriterion ⊓ @QuasiCompact ⊓ @QuasiSeparated ⊓ @LocallyOfFiniteType := by
+  rw [isProper_eq, IsSeparated_eq_valuativeCriterion, valuativeCriterion_eq,
+    universallyClosed_eq_valuativeCriterion]
+  rw [← inf_assoc]
+  ext X Y f
+  constructor
+  · intro ⟨⟨⟨⟨h0, h1⟩, h2⟩, h3⟩, h4⟩
+    exact ⟨⟨⟨⟨h2, h0⟩, h3⟩, h1⟩, h4⟩
+  · intro ⟨⟨⟨⟨h2, h0⟩, h3⟩, h1⟩, h4⟩
+    exact ⟨⟨⟨⟨h0, h1⟩, h2⟩, h3⟩, h4⟩
 
 end AlgebraicGeometry
