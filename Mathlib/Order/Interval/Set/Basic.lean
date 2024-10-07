56a60142e5ba2f3b361409fbc9274efbb66e37aa
faenuccio <filippo.nuccio@univ-st-etienne.fr>
2024-10-08 00:31:57 +0200

    addressed reviewer's comments

--- Mathlib/Order/Interval/Set/Basic.lean
+++ Mathlib/Order/Interval/Set/Basic.lean
@@ -38,35 +38,35 @@ section Preorder
 
 variable [Preorder α] {a a₁ a₂ b b₁ b₂ c x : α}
 
-/-- Left-open right-open interval $(a, b)$ -/
+/-- `Ioo a b` is the left-open right-open interval $(a, b)$ -/
 def Ioo (a b : α) :=
   { x | a < x ∧ x < b }
 
-/-- Left-closed right-open interval $[a, b)$ -/
+/-- `Ico a b` is the left-closed right-open interval $[a, b)$ -/
 def Ico (a b : α) :=
   { x | a ≤ x ∧ x < b }
 
-/-- Left-infinite right-open interval $(-\infty, a)$ -/
-def Iio (a : α) :=
-  { x | x < a }
+/-- `Iio b` is the left-infinite right-open interval $(-\infty, b)$ -/
+def Iio (b : α) :=
+  { x | x < b }
 
-/-- Left-closed right-closed interval $[a, b]$ -/
+/-- `Icc a b` is the left-closed right-closed interval $[a, b]$ -/
 def Icc (a b : α) :=
   { x | a ≤ x ∧ x ≤ b }
 
-/-- Left-infinite right-closed interval $(-\infty, a]$ -/
+/-- `Iic b` is the left-infinite right-closed interval $(-\infty, b]$ -/
 def Iic (b : α) :=
   { x | x ≤ b }
 
-/-- Left-open right-closed interval $(a, b]$ -/
+/-- `Ioc a b` is the left-open right-closed interval $(a, b]$ -/
 def Ioc (a b : α) :=
   { x | a < x ∧ x ≤ b }
 
-/-- Left-closed right-infinite interval $[a, \infty)$ -/
+/-- `Ici a` is the left-closed right-infinite interval $[a, \infty)$ -/
 def Ici (a : α) :=
   { x | a ≤ x }
 
-/-- Left-open right-infinite interval $(a, \infty)$ -/
+/-- `Ioi a` is the left-open right-infinite interval $(a, \infty)$ -/
 def Ioi (a : α) :=
   { x | a < x }
 
