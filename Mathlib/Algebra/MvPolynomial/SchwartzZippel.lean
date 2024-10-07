2260cce79bfda8455590dea9cad288b03f6352ce
Yaël Dillies <yael.dillies@gmail.com>
2024-10-07 14:57:49 +0000

    rename variables

--- Mathlib/Algebra/MvPolynomial/SchwartzZippel.lean
+++ Mathlib/Algebra/MvPolynomial/SchwartzZippel.lean
@@ -48,7 +48,7 @@ For a nonzero multivariable polynomial `p` over an integral domain, the probabil
 evaluates to zero at points drawn at random from some finite subset `S` of the integral domain is
 bounded by the degree of `p` over `|S|`. This version presents this lemma in terms of `Finset`. -/
 lemma schwartz_zippel : ∀ {n} {p : MvPolynomial (Fin n) R} (hp : p ≠ 0) (S : Fin n → Finset R),
-    #{f ∈ S ^^ n | eval f p = 0} / ∏ i, (#S i : ℚ≥0) ≤ ∑ i, (p.degreeOf i / #S i : ℚ≥0)
+    #{x ∈ S ^^ n | eval x p = 0} / ∏ i, (#S i : ℚ≥0) ≤ ∑ i, (p.degreeOf i / #S i : ℚ≥0)
   | 0, p, hp, S => by
     -- Because `p` is a polynomial over zero variables, it is constant.
     rw [p.eq_C_of_isEmpty] at *
@@ -66,11 +66,11 @@ lemma schwartz_zippel : ∀ {n} {p : MvPolynomial (Fin n) R} (hp : p ≠ 0) (S :
     have hdeg : pₖ.totalDegree + k ≤ p.totalDegree := totalDegree_coeff_finSuccEquiv_add_le _ _ hpₖ₀
     calc
       -- We split the set of possible zeros into a union of two cases.
-      #{f ∈ S ^^ (n + 1) | eval f p = 0} / ∏ i, (#S i : ℚ≥0)
+      #{x ∈ S ^^ (n + 1) | eval x p = 0} / ∏ i, (#S i : ℚ≥0)
           -- In the first case, `pₖ` evaluates to `0`.
-        = #{r ∈ S ^^ (n + 1) | eval r p = 0 ∧ eval (tail r) pₖ = 0} / ∏ i, (#S i : ℚ≥0)
+        = #{x ∈ S ^^ (n + 1) | eval x p = 0 ∧ eval (tail x) pₖ = 0} / ∏ i, (#S i : ℚ≥0)
           -- In the second case, `pₖ` does not evaluate to `0`.
-          + #{r ∈ S ^^ (n + 1) | eval r p = 0 ∧ eval (tail r) pₖ ≠ 0} / ∏ i, (#S i : ℚ≥0) := by
+          + #{x ∈ S ^^ (n + 1) | eval x p = 0 ∧ eval (tail x) pₖ ≠ 0} / ∏ i, (#S i : ℚ≥0) := by
         rw [← add_div, ← Nat.cast_add, ← card_union_add_card_inter, filter_union_right,
           ← filter_and]
         simp [← and_or_left, em, and_and_and_comm]
@@ -78,59 +78,60 @@ lemma schwartz_zippel : ∀ {n} {p : MvPolynomial (Fin n) R} (hp : p ≠ 0) (S :
         gcongr ?_ + ?_
         · -- We bound the size of the first set by induction
           calc
-            #{r ∈ S ^^ (n + 1) | eval r p = 0 ∧ eval (tail r) pₖ = 0} / ∏ i, (#S i : ℚ≥0)
-              ≤ #{r ∈ S ^^ (n + 1) | eval (tail r) pₖ = 0} / ∏ i, (#S i : ℚ≥0) := by
-              gcongr; exact fun r hr ↦ hr.2
-            _ = #S 0 * #{r ∈ tail S ^^ n | eval r pₖ = 0} / (#S 0 * (∏ i, #S (.succ i) : ℚ≥0)) := by
-              rw [card_consEquiv_filter_piFinset S fun f ↦ eval f pₖ = 0, prod_univ_succ, tail_def]
+            #{x ∈ S ^^ (n + 1) | eval x p = 0 ∧ eval (tail x) pₖ = 0} / ∏ i, (#S i : ℚ≥0)
+              ≤ #{x ∈ S ^^ (n + 1) | eval (tail x) pₖ = 0} / ∏ i, (#S i : ℚ≥0) := by
+              gcongr; exact fun x hx ↦ hx.2
+            _ = #S 0 * #{xₜ ∈ tail S ^^ n | eval xₜ pₖ = 0}
+                / (#S 0 * (∏ i, #S (.succ i) : ℚ≥0)) := by
+              rw [card_consEquiv_filter_piFinset S fun x ↦ eval x pₖ = 0, prod_univ_succ, tail_def]
               norm_cast
-            _ ≤ #{r ∈ tail S ^^ n | eval r pₖ = 0} / ∏ i, (#S (.succ i) : ℚ≥0) :=
+            _ ≤ #{xₜ ∈ tail S ^^ n | eval xₜ pₖ = 0} / ∏ i, (#S (.succ i) : ℚ≥0) :=
               mul_div_mul_left_le (by positivity)
             _ ≤ ∑ i, (pₖ.degreeOf i / #S (.succ i) : ℚ≥0) := schwartz_zippel hpₖ₀ _
             _ ≤ ∑ i, (p.degreeOf (.succ i) / #S (.succ i) : ℚ≥0) := by
               gcongr; exact degreeOf_coeff_finSuccEquiv ..
-        · -- We bound the second set by noting that if `(r, x)` is in it, then `x` is the root of
-          -- the univariate polynomial`pᵣ` obtained by evaluating each (multivariate polynomial)
-          -- coefficient at `r`. Since `pᵣ` has degree `i`, there are at most `i` such `x` for each
-          -- `r`, which gives the result.
+        · -- We bound the second set by noting that if `x` is in it, then `x₀` is the root of
+          -- the univariate polynomial`pₓ` obtained by evaluating each (multivariate polynomial)
+          -- coefficient at `xₜ`. Since `pₓ` has degree `k`, there are at most `k` such `x₀` for
+          -- each `xₜ`, which gives the result.
           calc
-            #{r ∈ S ^^ (n + 1) | eval r p = 0 ∧ eval (tail r) pₖ ≠ 0} / ∏ i, (#S i : ℚ≥0)
+            #{x ∈ S ^^ (n + 1) | eval x p = 0 ∧ eval (tail x) pₖ ≠ 0} / ∏ i, (#S i : ℚ≥0)
               ≤ ↑(p.degreeOf 0 * ∏ i, #S (.succ i)) / ∏ i, (#S i : ℚ≥0) := ?_
             _ = p.degreeOf 0 * (∏ i, #S (.succ i)) / (#S 0 * ∏ i, #S (.succ i)) := by
               norm_cast; rw [prod_univ_succ]
             _ ≤ (p.degreeOf 0 / #S 0 : ℚ≥0) := mul_div_mul_right_le (by positivity) (by positivity)
           gcongr
           calc
-            #{r ∈ S ^^ (n + 1) | eval r p = 0 ∧ eval (tail r) pₖ ≠ 0}
-              = #{r ∈ S ^^ (n + 1) | eval (tail r) pₖ ≠ 0 ∧ eval r p = 0} := by simp_rw [and_comm]
-            _ = #({r ∈ tail S ^^ n | eval r pₖ ≠ 0}.biUnion fun r ↦ image (fun x ↦ (x, r))
-                  {x ∈ S 0 | eval (cons x r) p = 0}) := by
+            #{x ∈ S ^^ (n + 1) | eval x p = 0 ∧ eval (tail x) pₖ ≠ 0}
+              = #{x ∈ S ^^ (n + 1) | eval (tail x) pₖ ≠ 0 ∧ eval x p = 0} := by simp_rw [and_comm]
+            _ = #({xₜ ∈ tail S ^^ n | eval xₜ pₖ ≠ 0}.biUnion fun xₜ ↦ image (fun x₀ ↦ (x₀, xₜ))
+                  {x₀ ∈ S 0 | eval (cons x₀ xₜ) p = 0}) := by
               rw [← filter_filter, filter_piFinset_eq_map_consEquiv S (fun r ↦ eval r pₖ ≠ 0),
                 filter_map, card_map, product_eq_biUnion_right, filter_biUnion]
               simp [Function.comp_def, Finset.filter_image, filter_filter]
               rfl
-            _ ≤ ∑ r ∈ tail S ^^ n with eval r pₖ ≠ 0,
-                  #(image (fun x ↦ (x, r)) {x ∈ S 0 | eval (cons x r) p = 0}) := card_biUnion_le
-            _ ≤ ∑ r ∈ tail S ^^ n with eval r pₖ ≠ 0, #{x ∈ S 0 | eval (cons x r) p = 0} := by
+            _ ≤ ∑ xₜ ∈ tail S ^^ n with eval xₜ pₖ ≠ 0,
+                  #image (fun x₀ ↦ (x₀, xₜ)) {x₀ ∈ S 0 | eval (cons x₀ xₜ) p = 0} := card_biUnion_le
+            _ ≤ ∑ xₜ ∈ tail S ^^ n with eval xₜ pₖ ≠ 0, #{x₀ ∈ S 0 | eval (cons x₀ xₜ) p = 0} := by
               gcongr; exact card_image_le
-            _ ≤ ∑ r ∈ tail S ^^ n with eval r pₖ ≠ 0, p.degreeOf 0 := ?_
-            _ ≤ ∑ _r ∈ tail S ^^ n, p.degreeOf 0 := by gcongr; exact filter_subset ..
+            _ ≤ ∑ xₜ ∈ tail S ^^ n with eval xₜ pₖ ≠ 0, p.degreeOf 0 := ?_
+            _ ≤ ∑ _xₜ ∈ tail S ^^ n, p.degreeOf 0 := by gcongr; exact filter_subset ..
             _ = p.degreeOf 0 * ∏ i, #S (.succ i) := by simp [mul_comm, tail]
-          gcongr with r hr
-          simp at hr
-          set pᵣ := p'.map (eval r) with hpᵣ
-          have hpᵣdeg : pᵣ.natDegree = k := by
-            rw [hpᵣ, hk, Polynomial.natDegree_map_of_leadingCoeff_ne_zero (eval r) hr.2]
-          have hpᵣ₀ : pᵣ ≠ 0 := fun h ↦ hr.2 <| by
-            rw [hpₖ, Polynomial.leadingCoeff, ← hk, ← hpᵣdeg, h, Polynomial.natDegree_zero,
-              ← Polynomial.coeff_map, ← hpᵣ, h, Polynomial.coeff_zero]
+          gcongr with xₜ hxₜ
+          simp at hxₜ
+          set pₓ := p'.map (eval xₜ) with hpₓ
+          have hpₓdeg : pₓ.natDegree = k := by
+            rw [hpₓ, hk, Polynomial.natDegree_map_of_leadingCoeff_ne_zero (eval xₜ) hxₜ.2]
+          have hpₓ₀ : pₓ ≠ 0 := fun h ↦ hxₜ.2 <| by
+            rw [hpₖ, Polynomial.leadingCoeff, ← hk, ← hpₓdeg, h, Polynomial.natDegree_zero,
+              ← Polynomial.coeff_map, ← hpₓ, h, Polynomial.coeff_zero]
           calc
-            #{x ∈ S 0 | eval (cons x r) p = 0} ≤ #pᵣ.roots.toFinset := by
+            #{x₀ ∈ S 0 | eval (cons x₀ xₜ) p = 0} ≤ #pₓ.roots.toFinset := by
               gcongr
-              simp (config := { contextual := true}) [subset_iff, eval_eq_eval_mv_eval', pᵣ, hpᵣ₀]
-            _ ≤ Multiset.card pᵣ.roots := pᵣ.roots.toFinset_card_le
-            _ ≤ pᵣ.natDegree := pᵣ.card_roots'
-            _ = k := hpᵣdeg
+              simp (config := { contextual := true}) [subset_iff, eval_eq_eval_mv_eval', pₓ, hpₓ₀]
+            _ ≤ Multiset.card pₓ.roots := pₓ.roots.toFinset_card_le
+            _ ≤ pₓ.natDegree := pₓ.card_roots'
+            _ = k := hpₓdeg
             _ ≤ p.degreeOf 0 := by
               have :
                 (ofLex (AddMonoidAlgebra.supDegree toLex p'.leadingCoeff)).cons k ∈ p.support := by
