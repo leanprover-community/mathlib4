73f494c0e84926d825dd195970222844f3d7ce3f
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2024-10-08 21:17:06 +0200

    chore: tidy various files

--- Mathlib/AlgebraicGeometry/PrimeSpectrum/Module.lean
+++ Mathlib/AlgebraicGeometry/PrimeSpectrum/Module.lean
@@ -31,8 +31,7 @@ lemma LocalizedModule.subsingleton_iff_disjoint {f : R} :
     Subsingleton (LocalizedModule (.powers f) M) ↔
       Disjoint ↑(PrimeSpectrum.basicOpen f) (Module.support R M) := by
   rw [subsingleton_iff_support_subset, PrimeSpectrum.basicOpen_eq_zeroLocus_compl,
-    disjoint_compl_left_iff]
-  rfl
+    disjoint_compl_left_iff, Set.le_iff_subset]
 
 lemma Module.stableUnderSpecialization_support :
     StableUnderSpecialization (Module.support R M) := by
