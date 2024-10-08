fb0c164cf71cd2711e67d0d7eb007269aab40df1
Yaël Dillies <yael.dillies@gmail.com>
2024-10-08 13:59:50 +0000

    golf

--- Mathlib/Data/ENNReal/Real.lean
+++ Mathlib/Data/ENNReal/Real.lean
@@ -558,72 +558,27 @@ theorem iInf_sum {α : Type*} {f : ι → α → ℝ≥0∞} {s : Finset α} [No
     rw [Finset.forall_mem_cons] at hk
     exact add_le_add hk.1.1 (Finset.sum_le_sum fun a ha => (hk.2 a ha).2)
 
-/-- If `x ≠ 0` and `x ≠ ∞`, then right multiplication by `x` maps infimum to infimum.
-See also `ENNReal.iInf_mul` that assumes `[Nonempty ι]` but does not require `x ≠ 0`. -/
-theorem iInf_mul_of_ne {ι} {f : ι → ℝ≥0∞} {x : ℝ≥0∞} (h0 : x ≠ 0) (h : x ≠ ∞) :
-    iInf f * x = ⨅ i, f i * x :=
-  le_antisymm mul_right_mono.map_iInf_le
-    ((ENNReal.div_le_iff_le_mul (Or.inl h0) <| Or.inl h).mp <|
-      le_iInf fun _ => (ENNReal.div_le_iff_le_mul (Or.inl h0) <| Or.inl h).mpr <| iInf_le _ _)
-
-/-- If `x ≠ ∞`, then right multiplication by `x` maps infimum over a nonempty type to infimum. See
-also `ENNReal.iInf_mul_of_ne` that assumes `x ≠ 0` but does not require `[Nonempty ι]`. -/
-theorem iInf_mul {ι} [Nonempty ι] {f : ι → ℝ≥0∞} {x : ℝ≥0∞} (h : x ≠ ∞) :
-    iInf f * x = ⨅ i, f i * x := by
-  by_cases h0 : x = 0
-  · simp only [h0, mul_zero, iInf_const]
-  · exact iInf_mul_of_ne h0 h
-
-/-- If `x ≠ ∞`, then left multiplication by `x` maps infimum over a nonempty type to infimum. See
-also `ENNReal.mul_iInf_of_ne` that assumes `x ≠ 0` but does not require `[Nonempty ι]`. -/
-theorem mul_iInf {ι} [Nonempty ι] {f : ι → ℝ≥0∞} {x : ℝ≥0∞} (h : x ≠ ∞) :
-    x * iInf f = ⨅ i, x * f i := by simpa only [mul_comm] using iInf_mul h
-
-/-- If `x ≠ 0` and `x ≠ ∞`, then left multiplication by `x` maps infimum to infimum.
-See also `ENNReal.mul_iInf` that assumes `[Nonempty ι]` but does not require `x ≠ 0`. -/
-theorem mul_iInf_of_ne {ι} {f : ι → ℝ≥0∞} {x : ℝ≥0∞} (h0 : x ≠ 0) (h : x ≠ ∞) :
-    x * iInf f = ⨅ i, x * f i := by simpa only [mul_comm] using iInf_mul_of_ne h0 h
-
-/-- If `f g : ι → ℝ≥0∞` take real values, and `∀ (i j : ι), a ≤ f i * g j`, then
-  `a ≤ iInf f * iInf g`. -/
-theorem le_iInf_mul_iInf {ι : Sort*} [hι : Nonempty ι] {a : ℝ≥0∞} {f g : ι → ℝ≥0∞}
-    (hf : ∀ x, f x ≠ ⊤) (hg : ∀ x, g x ≠ ⊤) (H : ∀ i j : ι, a ≤ f i * g j) :
-    a ≤ iInf f * iInf g := by
-  have hg' : iInf g ≠ ⊤ := by rw [ne_eq, iInf_eq_top, not_forall]; exact ⟨hι.some, hg hι.some⟩
-  rw [iInf_mul hg']
-  refine le_iInf ?_
-  intro i
-  rw [mul_iInf (hf i)]
-  exact le_iInf (H i)
-
+-- (Yaël) Now that we have `iInf_mul_iInf`, this theorem is too specialised
 /-- If `u v : ι → ℝ≥0∞` take real values and are antitone, then `iInf (u * v) ≤ iInf u * iInf v`. -/
 theorem iInf_mul_le_mul_iInf {u v : ℕ → ℝ≥0∞} (hu_top : ∀ x, u x ≠ ⊤) (hu : Antitone u)
-    (hv_top : ∀ x, v x ≠ ⊤) (hv : Antitone v) : iInf (u * v) ≤ iInf u * iInf v := by
-  rw [iInf_le_iff]
-  intro b hb
-  apply le_iInf_mul_iInf hu_top hv_top
-  intro m n
-  exact le_trans (hb (max m n)) (mul_le_mul' (hu (le_max_left _ _)) (hv (le_max_right _ _)))
+    (hv_top : ∀ x, v x ≠ ⊤) (hv : Antitone v) : iInf (u * v) ≤ iInf u * iInf v :=
+  (iInf_mul_iInf ⟨0, hu_top _⟩ ⟨0, hv_top _⟩ fun i j ↦
+    ⟨max i j, mul_le_mul' (hu le_sup_left) (hv le_sup_right)⟩).ge
 
-theorem le_iSup_prop (u : ℕ → ℝ≥0∞) {n k : ℕ} (hnk : n ≤ k) : u k ≤ ⨆ (k : ℕ) (_ : n ≤ k), u k := by
-  apply le_iSup_of_le k
-  rw [ciSup_pos hnk]
+-- (Yaël) This theorem is too specialised
+theorem le_iSup_prop (u : ℕ → ℝ≥0∞) {n k : ℕ} (hnk : n ≤ k) : u k ≤ ⨆ (k : ℕ) (_ : n ≤ k), u k :=
+  le_iSup₂_of_le k hnk le_rfl
 
+-- (Yaël) This theorem is too specialised
 /-- The function sending `n : ℕ` to `⨆ (k : ℕ) (x : n ≤ k), u k` is antitone. -/
 theorem Antitone.iSup {u : ℕ → ℝ≥0∞} : Antitone fun n : ℕ ↦ ⨆ (k : ℕ) (_ : n ≤ k), u k :=
-  antitone_nat_of_succ_le (fun n ↦ iSup₂_le_iff.mpr
-    fun _ hk ↦ le_iSup_prop u (le_trans (Nat.le_succ n) hk))
+  fun _m _n hmn ↦ biSup_mono fun _ ↦ hmn.trans
 
+-- (Yaël) This theorem is too specialised
 /-- If `u : ℕ → ℝ≥0∞` is bounded above by a real number, then its `iSup` is finite. -/
-theorem iSup_le_top_of_bddAbove {u : ℕ → ℝ≥0∞} {B : ℝ≥0} (hu : ∀ x, u x ≤ B) (n : ℕ) :
+theorem iSup_ne_top_of_bddAbove {u : ℕ → ℝ≥0∞} {B : ℝ≥0} (hu : ∀ x, u x ≤ B) (n : ℕ) :
     (⨆ (k : ℕ) (_ : n ≤ k), u k) ≠ ⊤ :=
-  have h_le : (⨆ (k : ℕ) (_ : n ≤ k), u k) ≤ B := by
-    have : (⨆ (k : ℕ) (_ : n ≤ k), u k) = ⨆ k : { k : ℕ // n ≤ k }, u k := by rw [iSup_subtype]
-    rw [this]
-    exact iSup_le fun m ↦ hu m
-  ne_top_of_le_ne_top coe_ne_top h_le
-
-/-! `iSup_mul`, `mul_iSup` and variants are in `Topology.Instances.ENNReal`. -/
+  ne_top_of_le_ne_top coe_ne_top <| iSup₂_le fun m _ ↦ hu m
 
 end iInf
 
