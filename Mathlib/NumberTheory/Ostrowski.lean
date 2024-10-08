c738dfa75d0e52425e263a1aff10ca373f85e8b5
Yaël Dillies <yael.dillies@gmail.com>
2024-10-08 14:37:48 +0000

    golf a bit more

--- Mathlib/NumberTheory/Ostrowski.lean
+++ Mathlib/NumberTheory/Ostrowski.lean
@@ -80,9 +80,8 @@ lemma equiv_on_nat_iff_equiv : (∃ c : ℝ, 0 < c ∧ (∀ n : ℕ , (f n) ^ c 
     f.equiv g := by
     refine ⟨fun ⟨c, hc, h⟩ ↦ ⟨c, ⟨hc, ?_⟩⟩, fun ⟨c, hc, h⟩ ↦ ⟨c, ⟨hc, fun n ↦ by rw [← h]⟩⟩⟩
     ext x
-    rw [← Rat.num_div_den x, map_div₀, map_div₀, div_rpow (apply_nonneg f _)
-      (apply_nonneg f _), h x.den, ← MulRingNorm.apply_natAbs_eq,← MulRingNorm.apply_natAbs_eq,
-      h (natAbs x.num)]
+    rw [← Rat.num_div_den x, map_div₀, map_div₀, div_rpow (by positivity) (by positivity), h x.den,
+      ← MulRingNorm.apply_natAbs_eq,← MulRingNorm.apply_natAbs_eq, h (natAbs x.num)]
 
 open Rat.MulRingNorm
 
@@ -286,10 +285,9 @@ lemma mulRingNorm_apply_le_sum_digits (n : ℕ) {m : ℕ} (hm : 1 < m) :
   replace hia := List.mem_enumFrom hia
   push_cast
   rw [map_mul, map_pow]
-  apply mul_le_mul_of_nonneg_right (le_of_lt (hcoef _))
-    (pow_nonneg (apply_nonneg f ↑m) i)
+  gcongr
   simp only [zero_le, zero_add, tsub_zero, true_and] at hia
-  exact List.mem_iff_get.mpr ⟨⟨i, hia.1⟩, hia.2.symm⟩
+  exact (hcoef (List.mem_iff_get.mpr ⟨⟨i, hia.1⟩, hia.2.symm⟩)).le
 
 /-! ## Step 1: if f is a MulRingNorm and f n > 1 for some natural n, then f n > 1 for all n ≥ 2 -/
 
@@ -307,8 +305,8 @@ lemma one_lt_of_not_bounded (notbdd : ¬ ∀ n : ℕ, f n ≤ 1) {n₀ : ℕ} (h
       apply List.sum_le_sum
       rintro ⟨i, a⟩ _
       simp only [Function.comp_apply, Function.uncurry_apply_pair]
-      exact mul_le_of_le_of_le_one' (mod_cast le_refl n₀) (pow_le_one₀ (apply_nonneg f ↑n₀) h)
-        (pow_nonneg (apply_nonneg f ↑n₀) i) (Nat.cast_nonneg n₀)
+      exact mul_le_of_le_of_le_one' (mod_cast le_refl n₀) (pow_le_one₀ (by positivity) h)
+        (by positivity) (by positivity)
     _ = n₀ * (Nat.log n₀ m + 1) := by
       rw [List.mapIdx_eq_enum_map, List.eq_replicate_of_mem (a := (n₀ : ℝ))
         (l := List.map (Function.uncurry fun _ _ ↦ n₀) (List.enum L)),
@@ -321,10 +319,7 @@ lemma one_lt_of_not_bounded (notbdd : ¬ ∀ n : ℕ, f n ≤ 1) {n₀ : ℕ} (h
   · simp only [CharP.cast_eq_zero, map_zero, zero_le_one]
   have h_ineq2 (k : ℕ) (hk : 0 < k) :
       f n ≤ (n₀ * (logb n₀ n + 1)) ^ (k : ℝ)⁻¹ * k ^ (k : ℝ)⁻¹ := by
-    have h_exp : (f n ^ (k : ℝ)) ^ (k : ℝ)⁻¹ = f n := by
-      apply rpow_rpow_inv (apply_nonneg f ↑n)
-      simp only [ne_eq, Nat.cast_eq_zero]
-      exact Nat.pos_iff_ne_zero.mp hk
+    have h_exp : (f n ^ (k : ℝ)) ^ (k : ℝ)⁻¹ = f n := rpow_rpow_inv (by positivity) (by positivity)
     have : 0 ≤ logb n₀ n := logb_nonneg (one_lt_cast.2 hn₀) (mod_cast Nat.one_le_of_lt h₀.bot_lt)
     rw [← mul_rpow (by positivity) (by positivity), ← h_exp, rpow_natCast, ← map_pow,
       ← Nat.cast_pow]
