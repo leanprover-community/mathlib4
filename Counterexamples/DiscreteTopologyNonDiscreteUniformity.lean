e63faacd41fa91662601490b6d5f1aea646facb2
faenuccio <filippo.nuccio@univ-st-etienne.fr>
2024-10-08 12:54:41 +0200

    added empty line

--- Counterexamples/DiscreteTopologyNonDiscreteUniformity.lean
+++ Counterexamples/DiscreteTopologyNonDiscreteUniformity.lean
@@ -3,6 +3,7 @@ Copyright (c) 2024 Filippo A. E. Nuccio. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Filippo A. E. Nuccio
 -/
+
 import Mathlib.Topology.UniformSpace.Cauchy
 import Mathlib.Order.Interval.Set.Basic
 
