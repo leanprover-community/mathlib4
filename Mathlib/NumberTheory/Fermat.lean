3fd31be184d1a2b0a219167d58da30998e83a8d7
Moritz Firsching <firsching@google.com>
2024-10-09 23:37:02 +0200

    review comments

--- Mathlib/NumberTheory/Fermat.lean
+++ Mathlib/NumberTheory/Fermat.lean
@@ -12,12 +12,12 @@ import Mathlib.Tactic.Ring.RingNF
 /-!
 # Fermat numbers
 
-The Fermat numbers are a sequence of natural numbers defined as $2^{2^n} + 1$, for all natural
-numbers $n$.
+The Fermat numbers are a sequence of natural numbers defined as 2^{2^n} + 1, for all natural
+numbers n.
 
 ## Main theorems
 
-- `coprime_fermat_fermat`: two distinct fermat numbers are coprime.
+- `coprime_fermat_fermat`: two distinct Fermat numbers are coprime.
 -/
 
 open Nat Finset
@@ -35,16 +35,15 @@ theorem strictMono_fermat : StrictMono fermat := by
   intro m n
   simp only [fermat, add_lt_add_iff_right, pow_lt_pow_iff_right (one_lt_two : 1 < 2), imp_self]
 
-theorem two_lt_fermat {n : ℕ} : 2 < fermat n := by
+theorem two_lt_fermat (n : ℕ) : 2 < fermat n := by
   cases n
   · simp
   · exact lt_of_succ_lt <| strictMono_fermat <| zero_lt_succ _
 
-theorem odd_fermat {n : ℕ} : Odd (fermat n) := 
+theorem odd_fermat (n : ℕ) : Odd (fermat n) :=
   (even_pow.mpr ⟨even_two, (pow_pos two_pos n).ne'⟩).add_one
 
-@[simp]
-theorem fermat_product {n : ℕ} : ∏ k in Finset.range n, fermat k = fermat n - 2 := by
+theorem fermat_product (n : ℕ) : ∏ k in Finset.range n, fermat k = fermat n - 2 := by
   induction' n with n hn
   · rfl
   rw [Finset.prod_range_succ, hn, fermat, fermat, mul_comm,
@@ -52,12 +51,12 @@ theorem fermat_product {n : ℕ} : ∏ k in Finset.range n, fermat k = fermat n 
   ring_nf
   omega
 
-theorem fermat_eq_prod_add_two {n : ℕ} : fermat n = (∏ k in Finset.range n, fermat k) + 2 := by
+theorem fermat_eq_prod_add_two (n : ℕ) : fermat n = (∏ k in Finset.range n, fermat k) + 2 := by
   rw [fermat_product, Nat.sub_add_cancel]
-  exact le_of_lt two_lt_fermat
+  exact le_of_lt <| two_lt_fermat _
 
-/-- 
-**Goldbach's theorem** : no two distinct Fermat numbers share a common factor greater than one. 
+/--
+**Goldbach's theorem** : no two distinct Fermat numbers share a common factor greater than one.
 
 From a letter to Euler, see page 37 in [juvskevivc2022].
 -/
@@ -66,7 +65,7 @@ theorem coprime_fermat_fermat  {k n : ℕ} (h : k ≠ n): Coprime (fermat n) (fe
   · simpa only [coprime_comm] using this h.symm (by omega)
   let m := (fermat n).gcd (fermat k)
   have h_n : m ∣ fermat n := (fermat n).gcd_dvd_left (fermat k)
-  have h_m : m ∣ 2 := (Nat.dvd_add_right <| (gcd_dvd_right _ _).trans <| dvd_prod_of_mem _ 
-    <| mem_range.mpr hkn).mp <| fermat_eq_prod_add_two ▸ h_n```
+  have h_m : m ∣ 2 := (Nat.dvd_add_right <| (gcd_dvd_right _ _).trans <| dvd_prod_of_mem _
+    <| mem_range.mpr hkn).mp <| fermat_eq_prod_add_two _ ▸ h_n
   refine ((dvd_prime prime_two).mp h_m).elim id (fun h_two ↦ ?_)
-  exact (odd_fermat.not_two_dvd_nat (h_two ▸ h_n)).elim
+  exact ((odd_fermat _).not_two_dvd_nat (h_two ▸ h_n)).elim
