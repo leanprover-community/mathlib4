7b81cfcee58b7ea82706d1627a8c211c909c5bf7
Yakov Pechersky <ffxen158@gmail.com>
2024-10-09 01:44:25 -0400

    better phrasing for add_pow_le

--- Mathlib/Data/Real/IsNonarchimedean.lean
+++ Mathlib/Data/Real/IsNonarchimedean.lean
@@ -137,12 +137,12 @@ theorem multiset_image_add_of_nonempty {F α β : Type*} [AddCommGroup α] [FunL
   `f ((a + b) ^ n) ≤ (f (a ^ m)) * (f (b ^ (n - m)))`. -/
 theorem add_pow_le {F α : Type*} [CommRing α] [FunLike F α ℝ]
     [RingSeminormClass F α ℝ] {f : F} (hna : IsNonarchimedean f) (n : ℕ) (a b : α) :
-    ∃ (m : ℕ) (_ : m ∈ List.range (n + 1)), f ((a + b) ^ n) ≤ f (a ^ m) * f (b ^ (n - m)) := by
+    ∃ m < n + 1, f ((a + b) ^ n) ≤ f (a ^ m) * f (b ^ (n - m)) := by
   obtain ⟨m, hm_lt, hM⟩ := finset_image_add hna
     (fun m : ℕ => a ^ m * b ^ (n - m) * ↑(n.choose m)) (Finset.range (n + 1))
   simp only [Finset.nonempty_range_iff, ne_eq, Nat.succ_ne_zero, not_false_iff, Finset.mem_range,
     if_true, forall_true_left] at hm_lt
-  refine ⟨m, List.mem_range.mpr hm_lt, ?_⟩
+  refine ⟨m, hm_lt, ?_⟩
   simp only [← add_pow] at hM
   rw [mul_comm] at hM
   exact le_trans hM (le_trans (nmul_le hna) (map_mul_le_mul _ _ _))
