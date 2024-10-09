f99040a128af97e3ea1aad97fe6a133bbe124822
Jireh Loreaux <loreaujy@gmail.com>
2024-10-09 12:22:36 -0500

    fixes in Algebra/

--- Mathlib/Algebra/Group/Subgroup/Pointwise.lean
+++ Mathlib/Algebra/Group/Subgroup/Pointwise.lean
@@ -68,9 +68,9 @@ theorem closure_toSubmonoid (S : Set G) :
     (closure S).toSubmonoid = Submonoid.closure (S ∪ S⁻¹) := by
   refine le_antisymm (fun x hx => ?_) (Submonoid.closure_le.2 ?_)
   · refine
-      closure_induction hx
+      closure_induction
         (fun x hx => Submonoid.closure_mono subset_union_left (Submonoid.subset_closure hx))
-        (Submonoid.one_mem _) (fun x y hx hy => Submonoid.mul_mem _ hx hy) fun x hx => ?_
+        (Submonoid.one_mem _) (fun x y _ _ hx hy => Submonoid.mul_mem _ hx hy) (fun x _ hx => ?_) hx
     rwa [← Submonoid.mem_closure_inv, Set.union_inv, inv_inv, Set.union_comm]
   · simp only [true_and, coe_toSubmonoid, union_subset_iff, subset_closure, inv_subset_closure]
 
