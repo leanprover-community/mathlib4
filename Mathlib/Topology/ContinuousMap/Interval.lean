61cd9e62c164bc8d5c99cdbcac7095782c0d067e
Vincent Beffara <vbeffara@gmail.com>
2024-10-10 23:35:35 +0200

    Unused import

--- Mathlib/Topology/ContinuousMap/Interval.lean
+++ Mathlib/Topology/ContinuousMap/Interval.lean
@@ -3,7 +3,6 @@ Copyright (c) 2024 Vincent Beffara. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Vincent Beffara
 -/
-import Mathlib.Topology.Algebra.Order.Compact
 import Mathlib.Topology.CompactOpen
 import Mathlib.Topology.Order.ProjIcc
 
