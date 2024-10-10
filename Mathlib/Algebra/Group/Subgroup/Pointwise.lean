5b78848b41b0db2fd112eb0d5699c5a1ecd8a41d
Yaël Dillies <yael.dillies@gmail.com>
2024-10-10 16:29:22 +0000

    right action version

--- Mathlib/Algebra/Group/Subgroup/Pointwise.lean
+++ Mathlib/Algebra/Group/Subgroup/Pointwise.lean
@@ -60,7 +60,6 @@ lemma coe_div_coe [SetLike S G] [DivisionMonoid G] [SubgroupClass S G] (H : S) :
 
 variable [Group G] [AddGroup A] {s : Set G} {a : G}
 
-
 namespace Subgroup
 
 @[to_additive (attr := simp)]
@@ -346,17 +345,61 @@ theorem conj_smul_subgroupOf {P H : Subgroup G} (hP : P ≤ H) (h : H) :
 end Monoid
 
 section Group
-variable [Group H] [MulAction G H] [IsScalarTower G H H]
+variable [Group H] [SetLike S H] [SubgroupClass S H] {s : S} {a b : G}
 
-@[to_additive]
-lemma pairwiseDisjoint_smul [SetLike S H] [SubgroupClass S H] (s : S) :
-    (Set.range fun a : G ↦ a • (s : Set H)).PairwiseDisjoint id := by
-  rintro _ ⟨a, rfl⟩ _ ⟨b, rfl⟩ hab
-  simp only [Function.onFun, id_eq, disjoint_left] at hab ⊢
+/-!
+Annoyingly, it seems like the following two pairs of lemmas cannot be unified.
+-/
+
+section Left
+variable [MulAction G H] [IsScalarTower G H H]
+
+/-- See `Subgroup.disjoint_smul_of_ne'` for a version that works for the right action of a group on
+itself. -/
+@[to_additive "See `AddSubgroup.disjoint_vadd_of_ne'` for a version that works for the right action
+of a group on itself."]
+lemma disjoint_smul_of_ne (hab : a • (s : Set H) ≠ b • s) : Disjoint (a • s : Set H) (b • s) := by
+  simp only [disjoint_left]
   rintro _ ⟨c, hc, rfl⟩ ⟨d, hd, (hcd : b • d = a • c)⟩
   refine hab ?_
   rw [← smul_coe_set hc, ← smul_assoc, ← hcd, smul_assoc, smul_coe_set hc, smul_coe_set hd]
 
+/-- See `Subgroup.pairwiseDisjoint_smul'` for a version that works for the right action of a group
+on itself. -/
+@[to_additive "See `AddSubgroup.pairwiseDisjoint_vadd'` for a version that works for the right
+action of a group on itself."]
+lemma pairwiseDisjoint_smul (s : S) :
+    (Set.range fun a : G ↦ a • (s : Set H)).PairwiseDisjoint id := by
+  rintro _ ⟨a, rfl⟩ _ ⟨b, rfl⟩; exact disjoint_smul_of_ne
+
+end Left
+
+section Right
+variable [MulAction G H] [IsScalarTower G Hᵐᵒᵖ H]
+
+open MulOpposite
+
+/-- See `Subgroup.disjoint_smul_of_ne` for a version that works for the left action of a group on
+itself. -/
+@[to_additive "See `AddSubgroup.disjoint_vadd_of_ne` for a version that works for the left action
+of a group on itself."]
+lemma disjoint_smul_of_ne' (hab : a • (s : Set H) ≠ b • s) :
+    Disjoint (a • s : Set H) (b • s) := by
+  simp only [disjoint_left]
+  rintro _ ⟨c, hc, rfl⟩ ⟨d, hd, (hcd : b • d = a • c)⟩
+  refine hab ?_
+  rw [← op_smul_coe_set hc, ← smul_assoc, ← op_smul, ← hcd, op_smul, smul_assoc, op_smul_coe_set hc,
+    op_smul_coe_set hd]
+
+/-- See `Subgroup.pairwiseDisjoint_smul` for a version that works for the left action of a group on
+itself. -/
+@[to_additive "See `AddSubgroup.pairwiseDisjoint_vadd` for a version that works for the left action
+of a group on itself."]
+lemma pairwiseDisjoint_smul' (s : S) :
+    (Set.range fun a : G ↦ a • (s : Set H)).PairwiseDisjoint id := by
+  rintro _ ⟨a, rfl⟩ _ ⟨b, rfl⟩; exact disjoint_smul_of_ne'
+
+end Right
 end Group
 
 section Group
