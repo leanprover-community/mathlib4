c82eee8c7b2bd50f418086d0cb652514e12ca8fb
Violeta Hernández <vi.hdz.p@gmail.com>
2024-10-10 05:28:38 -0600

    undeprecate

--- Mathlib/SetTheory/Ordinal/Basic.lean
+++ Mathlib/SetTheory/Ordinal/Basic.lean
@@ -487,7 +487,6 @@ theorem induction {p : Ordinal.{u} → Prop} (i : Ordinal.{u}) (h : ∀ j, (∀ 
     p i :=
   lt_wf.induction i h
 
-@[deprecated PrincipalSeg.eq (since := "2024-10-09")]
 theorem typein_apply {α β} {r : α → α → Prop} {s : β → β → Prop} [IsWellOrder α r] [IsWellOrder β s]
     (f : r ≼i s) (a : α) : typein s (f a) = typein r a := by
   rw [← f.leLT_apply _ a, (f.leLT _).eq]
