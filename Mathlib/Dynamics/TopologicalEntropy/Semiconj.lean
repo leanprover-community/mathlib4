67c4c3c3a6d091ff2f5a53f0da7be6168d3e16e7
D-Thomine <100795491+D-Thomine@users.noreply.github.com>
2024-10-11 20:05:35 +0200

    Fix documentation

--- Mathlib/Dynamics/TopologicalEntropy/Semiconj.lean
+++ Mathlib/Dynamics/TopologicalEntropy/Semiconj.lean
@@ -40,8 +40,8 @@ We use only the definition of the topological entropy using covers; the simplest
 `IsDynCoverOf.image` for nets fails.
 
 ## Main results
-- `coverEntropy_image`/`coverEntropyInf_image`: the entropy of `φ '' F` equals the entropy of `F`
-if `X` is endowed with the pullback by `φ` of the uniform structure of `Y`.
+- `coverEntropy_image_of_comap`/`coverEntropyInf_image_of_comap`: the entropy of `φ '' F` equals
+the entropy of `F` if `X` is endowed with the pullback by `φ` of the uniform structure of `Y`.
 - `coverEntropy_image_le_of_uniformContinuous`/`coverEntropyInf_image_le_of_uniformContinuous`:
 the entropy of `φ '' F` is lower than the entropy of `F` if `φ` is uniformly continuous.
 - `coverEntropy_restrict`: the entropy of the restriction of `S` to an invariant set `F` is
