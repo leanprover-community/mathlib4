cdabfb29f925dcb0fc33df2af37258b574f82bab
Yakov Pechersky <ffxen158@gmail.com>
2024-10-10 23:40:17 -0400

    generalize to division rings

--- Mathlib/Analysis/Normed/Field/Ultra.lean
+++ Mathlib/Analysis/Normed/Field/Ultra.lean
@@ -7,19 +7,20 @@ import Mathlib.Analysis.Normed.Field.Basic
 import Mathlib.Analysis.Normed.Group.Ultra
 
 /-!
-## Sufficient conditions to have an ultrametric norm on a field
+## Sufficient conditions to have an ultrametric norm on a division ring
 
 This file provides ways of constructing an instance of `IsUltrametricDist` based on
 facts about the existing norm.
 
 ## Main results
 
-* `isUltrametricDist_of_forall_norm_natCast_le_one`: a norm in a field is ultrametric if the norm
-  of the image of a natural is less than or equal to one
+* `isUltrametricDist_of_forall_norm_natCast_le_one`: a norm in a division ring is ultrametric
+  if the norm of the image of a natural is less than or equal to one
 
 ## Implementation details
 
-The proof relies on a bounded-from-above argument.
+The proof relies on a bounded-from-above argument. The main result has a longer proof
+to be able to be applied in noncommutative division rings.
 
 ## Tags
 
@@ -31,7 +32,7 @@ namespace IsUltrametricDist
 
 section sufficient
 
-variable {R F : Type*} [NormedDivisionRing R] [NormedField F]
+variable {R : Type*} [NormedDivisionRing R]
 
 lemma isUltrametricDist_of_forall_norm_add_one_le_max_norm_one
     (h : ∀ x : R, ‖x + 1‖ ≤ max ‖x‖ 1) : IsUltrametricDist R := by
@@ -62,7 +63,7 @@ lemma isUltrametricDist_of_forall_norm_sub_one_of_norm_le_one
 /-- This technical lemma is used in the proof of
 `isUltrametricDist_of_forall_norm_natCast_le_one`. -/
 lemma isUltrametricDist_of_forall_pow_norm_le_nsmul_pow_max_one_norm
-    (h : ∀ (x : R) (m : ℕ), ‖x + 1‖ ^ m ≤ (m + 1) • max 1 ‖x‖ ^ m) :
+    (h : ∀ (x : R) (m : ℕ), ‖x + 1‖ ^ m ≤ (m + 1) • max 1 (‖x‖ ^ m)) :
     IsUltrametricDist R := by
   -- it will suffice to prove that `‖x + 1‖ ≤ max 1 ‖x‖`
   refine isUltrametricDist_of_forall_norm_add_one_le_max_norm_one fun x => ?_
@@ -84,53 +85,56 @@ lemma isUltrametricDist_of_forall_pow_norm_le_nsmul_pow_max_one_norm
   rw [div_pow, gt_iff_lt, lt_div_iff₀ (by positivity), ← nsmul_eq_mul] at hm
   -- which squeezes down to get our `‖x + 1‖ ≤ a` using our to-be-proven hypothesis of
   -- `‖x + 1‖ ^ m ≤ (m + 1) • max 1 ‖x‖ ^ m`, so we're done
-  refine le_of_pow_le_pow_left ?_ (zero_lt_one.trans ha').le ((h _ _).trans hm.le)
-  rintro rfl
-  simp at hm
-
-/-- To prove that a normed field is nonarchimedean, it suffices to prove that the norm
-of the image of any natural is less than or equal to one. -/
-lemma isUltrametricDist_of_forall_norm_natCast_le_one
-    (h : ∀ n : ℕ, ‖(n : F)‖ ≤ 1) : IsUltrametricDist F := by
-  -- we first use our hypothesis about the norm of naturals to have that multiplication by
-  -- naturals keeps the norm small
-  replace h (x : F) (n : ℕ) : ‖n • x‖ ≤ ‖x‖ := by
-    rw [nsmul_eq_mul, norm_mul]
-    rcases (norm_nonneg x).eq_or_lt with hx|hx
-    · simp [← hx]
-    · rw [mul_le_iff_le_one_left hx]
-      exact h _
-  -- it will suffice to prove that `‖x + 1‖ ≤ max 1 ‖x‖`
-  -- which we will do by "complicating" the goal:
-  -- proving it for all powers `m`, `‖x + 1‖ ^ m ≤ (m + 1) • max 1 ‖x‖ ^ m`,
-  suffices ∀ (x : F) (m : ℕ), ‖x + 1‖ ^ m ≤ (m + 1) • max 1 ‖x‖ ^ m from
-    isUltrametricDist_of_forall_pow_norm_le_nsmul_pow_max_one_norm this
-  intro x m
-  -- we can distribute powers into the right term of `max` here
+  -- we can distribute powers into the right term of `max`
   have hp : max 1 ‖x‖ ^ m = max 1 (‖x‖ ^ m) := by
     rcases max_cases 1 ‖x‖ with (⟨hm, hx⟩|⟨hm, hx⟩)
     · rw [hm, one_pow, eq_comm, max_eq_left_iff]
       exact (pow_le_pow_left (norm_nonneg _) hx _).trans_eq (one_pow _)
     · rw [hm, eq_comm, max_eq_right_iff]
       exact (pow_le_pow_left zero_le_one hx.le _).trans_eq' (one_pow _).symm
-  rw [hp]; clear hp
+  rw [hp] at hm
+  refine le_of_pow_le_pow_left ?_ (zero_lt_one.trans ha').le ((h _ _).trans hm.le)
+  rintro rfl
+  simp at hm
+
+/-- To prove that a normed division ring is nonarchimedean, it suffices to prove that the norm
+of the image of any natural is less than or equal to one. -/
+lemma isUltrametricDist_of_forall_norm_natCast_le_one
+    (h : ∀ n : ℕ, ‖(n : R)‖ ≤ 1) : IsUltrametricDist R := by
+  -- it will suffice to prove that `‖x + 1‖ ≤ max 1 ‖x‖`
+  -- which we will do by "complicating" the goal:
+  -- proving it for all powers `m`, `‖x + 1‖ ^ m ≤ (m + 1) • max 1 ‖x‖ ^ m`,
+  -- modulo distribution of powers in/out of the `max` term
+  suffices ∀ (x : R) (m : ℕ), ‖x + 1‖ ^ m ≤ (m + 1) • max 1 (‖x‖ ^ m) from
+    isUltrametricDist_of_forall_pow_norm_le_nsmul_pow_max_one_norm this
+  intro x m
+  -- we first use our hypothesis about the norm of naturals to have that multiplication by
+  -- naturals keeps the norm small
+  replace h (x : R) (n : ℕ) : ‖n • x‖ ≤ ‖x‖ := by
+    rw [nsmul_eq_mul, norm_mul]
+    rcases (norm_nonneg x).eq_or_lt with hx|hx
+    · simp [← hx]
+    · rw [mul_le_iff_le_one_left hx]
+      exact h _
   -- we distribute the LHS using the binomial theorem  where all terms in the sum
   -- will be of the form `k • x ^ i` for some binomial coefficient `k : ℕ`
   -- and the terminal terms with powers `0` and `m` will have the binomial coefficient as `1`
-  have key : ∀ m : ℕ, ‖x + 1‖ ^ m ≤ ∑ k ∈ Finset.range (m + 1), ‖x‖ ^ k := by
+  have key : ‖x + 1‖ ^ m ≤ ∑ k ∈ Finset.range (m + 1), ‖x‖ ^ k := by
     -- the nature of the norm means that one of `1` and `‖x‖ ^ m` is the largest of the two,
     -- so the other terms in the binomial expansion are insignificant,
     -- since by our hypothesis, we have that `‖k • x ^ i‖ ≤ ‖x ^ i‖`.
     -- That is, either the power `0` or `m` dominates the other terms.
-    intro
-    simp_rw [← norm_pow, add_pow, one_pow, mul_one]
-    refine (norm_sum_le _ _).trans (Finset.sum_le_sum ?_)
-    intros
-    rw [mul_comm, ← nsmul_eq_mul]
-    exact h _ _
+    -- if we were over a field, we could use `add_pow`, but we prove in generality
+    -- since in any monoid, all `x` commute with `1`, thus we can expand `(x + 1) ^ m` without
+    -- assuming commutativity over the whole ring
+    simp_rw [← norm_pow, (Commute.one_right x).add_pow', one_pow, mul_one,
+      -- and this means we have to now deconstruct the antidiagonal sum
+      Finset.Nat.sum_antidiagonal_eq_sum_range_succ (fun i _ ↦ m.choose i • x ^ i) m]
+    -- and prove the inequality term by term, where the binomial coefficient keeps the norm small
+    exact (norm_sum_le _ _).trans (Finset.sum_le_sum fun _ _ ↦ h _ _)
   -- we now suffice to show that the sum of powers of norms is less than the
   -- multiple of the larger of the two
-  refine (key m).trans ?_
+  refine key.trans ?_
   -- and the number of terms in the sum is precisely `m + 1`
   rw [← Finset.card_range (m + 1), ← Finset.sum_const, Finset.card_range]
   rcases max_cases 1 (‖x‖ ^ m) with (⟨hm, hx⟩|⟨hm, hx⟩) <;> rw [hm] <;>
