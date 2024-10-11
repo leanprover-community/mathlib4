66b7ece75e6d01bd61cace1d881f04701a91802a
Yaël Dillies <yael.dillies@gmail.com>
2024-10-11 12:43:43 +0000

    generalise

--- Mathlib/MeasureTheory/Measure/NullMeasurable.lean
+++ Mathlib/MeasureTheory/Measure/NullMeasurable.lean
@@ -280,11 +280,8 @@ theorem measure_union₀' (hs : NullMeasurableSet s μ) (hd : AEDisjoint μ s t)
 theorem measure_add_measure_compl₀ {s : Set α} (hs : NullMeasurableSet s μ) :
     μ s + μ sᶜ = μ univ := by rw [← measure_union₀' hs aedisjoint_compl_right, union_compl_self]
 
-lemma measure_of_measure_compl_eq_zero₀ (hs : NullMeasurableSet s μ) (h : μ sᶜ = 0) :
-    μ s = μ univ := by simpa [h] using measure_add_measure_compl₀ hs
-
-lemma measure_of_measure_compl_eq_zero (hs : MeasurableSet s) (h : μ sᶜ = 0) : μ s = μ univ :=
-  measure_of_measure_compl_eq_zero₀ hs.nullMeasurableSet h
+lemma measure_of_measure_compl_eq_zero (hs : μ sᶜ = 0) : μ s = μ Set.univ := by
+  simpa [hs] using measure_add_measure_compl₀ <| .of_compl <| .of_null hs
 
 section MeasurableSingletonClass
 
