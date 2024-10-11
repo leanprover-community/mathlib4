bee0ae4b1c53a607fae470db584ee7a7a3cb71c9
Johan Commelin <johan@commelin.net>
2024-10-11 08:10:44 +0200

    wip

--- Mathlib/LinearAlgebra/Eigenspace/Triangularizable.lean
+++ Mathlib/LinearAlgebra/Eigenspace/Triangularizable.lean
@@ -153,6 +153,72 @@ namespace Submodule
 
 variable {p : Submodule K V} {f : Module.End K V}
 
+theorem inf_iSup_unifEigenspace [FiniteDimensional K V] (h : ∀ x ∈ p, f x ∈ p) (k : ℕ∞) :
+    p ⊓ ⨆ μ, f.unifEigenspace μ k = ⨆ μ, p ⊓ f.unifEigenspace μ k := by
+  refine le_antisymm (fun m hm ↦ ?_)
+    (le_inf_iff.mpr ⟨iSup_le fun μ ↦ inf_le_left, iSup_mono fun μ ↦ inf_le_right⟩)
+  classical
+  obtain ⟨hm₀ : m ∈ p, hm₁ : m ∈ ⨆ μ, f.unifEigenspace μ k⟩ := hm
+  obtain ⟨m, hm₂, rfl⟩ := (mem_iSup_iff_exists_finsupp _ _).mp hm₁
+  suffices ∀ μ, (m μ : V) ∈ p by
+    exact (mem_iSup_iff_exists_finsupp _ _).mpr ⟨m, fun μ ↦ mem_inf.mp ⟨this μ, hm₂ μ⟩, rfl⟩
+  intro μ
+  by_cases hμ : μ ∈ m.support; swap
+  · simp only [Finsupp.not_mem_support_iff.mp hμ, p.zero_mem]
+  have h_comm : ∀ (μ₁ μ₂ : K),
+    Commute ((f - algebraMap K (End K V) μ₁) ^ finrank K V)
+            ((f - algebraMap K (End K V) μ₂) ^ finrank K V) := fun μ₁ μ₂ ↦
+    ((Commute.sub_right rfl <| Algebra.commute_algebraMap_right _ _).sub_left
+      (Algebra.commute_algebraMap_left _ _)).pow_pow _ _
+  let g : End K V := (m.support.erase μ).noncommProd _ fun μ₁ _ μ₂ _ _ ↦ h_comm μ₁ μ₂
+  have hfg : Commute f g := Finset.noncommProd_commute _ _ _ _ fun μ' _ ↦
+    (Commute.sub_right rfl <| Algebra.commute_algebraMap_right _ _).pow_right _
+  have hg₀ : g (m.sum fun _μ mμ ↦ mμ) = g (m μ) := by
+    suffices ∀ μ' ∈ m.support, g (m μ') = if μ' = μ then g (m μ) else 0 by
+      rw [map_finsupp_sum, Finsupp.sum_congr (g2 := fun μ' _ ↦ if μ' = μ then g (m μ) else 0) this,
+        Finsupp.sum_ite_eq', if_pos hμ]
+    rintro μ' hμ'
+    split_ifs with hμμ'
+    · rw [hμμ']
+    replace hm₂ : ((f - algebraMap K (End K V) μ') ^ finrank K V) (m μ') = 0 := by
+      sorry
+      -- obtain ⟨k, hk⟩ := (mem_iSup_of_chain _ _).mp (hm₂ μ')
+      -- simpa only [End.mem_genEigenspace] using
+      --   Module.End.genEigenspace_le_genEigenspace_finrank _ _ k hk
+    have : _ = g := (m.support.erase μ).noncommProd_erase_mul (Finset.mem_erase.mpr ⟨hμμ', hμ'⟩)
+      (fun μ ↦ (f - algebraMap K (End K V) μ) ^ finrank K V) (fun μ₁ _ μ₂ _ _ ↦ h_comm μ₁ μ₂)
+    rw [← this, LinearMap.mul_apply, hm₂, _root_.map_zero]
+  have hg₁ : MapsTo g p p := Finset.noncommProd_induction _ _ _ (fun g' : End K V ↦ MapsTo g' p p)
+      (fun f₁ f₂ ↦ MapsTo.comp) (mapsTo_id _) fun μ' _ ↦ by
+    suffices MapsTo (f - algebraMap K (End K V) μ') p p by
+      simp only [LinearMap.coe_pow]; exact this.iterate (finrank K V)
+    intro x hx
+    rw [LinearMap.sub_apply, algebraMap_end_apply]
+    exact p.sub_mem (h _ hx) (smul_mem p μ' hx)
+  have hg₂ : MapsTo g ↑(f.unifEigenspace μ k) ↑(f.unifEigenspace μ k) :=
+    f.mapsTo_unifEigenspace_of_comm hfg μ k
+  have hg₃ : InjOn g ↑(f.unifEigenspace μ k) := by
+    apply LinearMap.injOn_of_disjoint_ker (subset_refl _)
+    have this := f.independent_unifEigenspace k
+    -- simp_rw [f.maxGenEigenspace_eq_genEigenspace_finrank] at this ⊢
+    rw [LinearMap.ker_noncommProd_eq_of_supIndep_ker, ← Finset.sup_eq_iSup]
+    · have := Finset.supIndep_iff_disjoint_erase.mp (this.supIndep' m.support) μ hμ
+      rw [← Finset.supIndep_iff_disjoint_erase]
+      simpa only [End.genEigenspace_def] using
+        Finset.supIndep_iff_disjoint_erase.mp (this.supIndep' m.support) μ hμ
+    · simpa only [End.genEigenspace_def] using this.supIndep' (m.support.erase μ)
+  have hg₄ : SurjOn g
+      ↑(p ⊓ f.unifEigenspace μ k) ↑(p ⊓ f.unifEigenspace μ k) := by
+    have : MapsTo g
+        ↑(p ⊓ f.unifEigenspace μ k) ↑(p ⊓ f.unifEigenspace μ k) :=
+      hg₁.inter_inter hg₂
+    rw [← LinearMap.injOn_iff_surjOn this]
+    exact hg₃.mono inter_subset_right
+  specialize hm₂ μ
+  obtain ⟨y, ⟨hy₀ : y ∈ p, hy₁ : y ∈ f.unifEigenspace μ k⟩, hy₂ : g y = g (m μ)⟩ :=
+    hg₄ ⟨(hg₀ ▸ hg₁ hm₀), hg₂ hm₂⟩
+  rwa [← hg₃ hy₁ hm₂ hy₂]
+
 theorem inf_iSup_genEigenspace [FiniteDimensional K V] (h : ∀ x ∈ p, f x ∈ p) :
     p ⊓ ⨆ μ, ⨆ k, f.genEigenspace μ k = ⨆ μ, ⨆ k, p ⊓ f.genEigenspace μ k := by
   simp_rw [← (f.genEigenspace _).mono.directed_le.inf_iSup_eq]
@@ -217,12 +283,29 @@ theorem inf_iSup_genEigenspace [FiniteDimensional K V] (h : ∀ x ∈ p, f x ∈
     hg₄ ⟨(hg₀ ▸ hg₁ hm₀), hg₂ hm₂⟩
   rwa [← hg₃ hy₁ hm₂ hy₂]
 
+theorem eq_iSup_inf_unifEigenspace [FiniteDimensional K V] (k : ℕ∞)
+    (h : ∀ x ∈ p, f x ∈ p) (h' : ⨆ μ, f.unifEigenspace μ k = ⊤) :
+    p = ⨆ μ, p ⊓ f.unifEigenspace μ k := by
+  rw [← inf_iSup_genEigenspace h, h', inf_top_eq]
+
 theorem eq_iSup_inf_genEigenspace [FiniteDimensional K V]
     (h : ∀ x ∈ p, f x ∈ p) (h' : ⨆ μ, ⨆ k, f.genEigenspace μ k = ⊤) :
     p = ⨆ μ, ⨆ k, p ⊓ f.genEigenspace μ k := by
   rw [← inf_iSup_genEigenspace h, h', inf_top_eq]
 
 end Submodule
+
+/-- In finite dimensions, if the generalized eigenspaces of a linear endomorphism span the whole
+space then the same is true of its restriction to any invariant submodule. -/
+theorem Module.End.unifEigenspace_restrict_eq_top
+    {p : Submodule K V} {f : Module.End K V} [FiniteDimensional K V] {k : ℕ∞}
+    (h : ∀ x ∈ p, f x ∈ p) (h' : ⨆ μ, f.unifEigenspace μ k = ⊤) :
+    ⨆ μ, Module.End.unifEigenspace (LinearMap.restrict f h) μ k = ⊤ := by
+  have := congr_arg (Submodule.comap p.subtype) (Submodule.eq_iSup_inf_genEigenspace h h')
+  have h_inj : Function.Injective p.subtype := Subtype.coe_injective
+  simp_rw [Submodule.inf_genEigenspace f p h, Submodule.comap_subtype_self,
+    ← Submodule.map_iSup, Submodule.comap_map_eq_of_injective h_inj] at this
+  exact this.symm
 
 /-- In finite dimensions, if the generalized eigenspaces of a linear endomorphism span the whole
 space then the same is true of its restriction to any invariant submodule. -/
