81580e3538537d659e27bfc4f51621b5f2dcd61a
Michael Rothgang <rothgang@math.uni-bonn.de>
2024-10-11 21:54:52 +0200

    Fixup.

--- Mathlib/RepresentationTheory/GroupCohomology/LowDegree.lean
+++ Mathlib/RepresentationTheory/GroupCohomology/LowDegree.lean
@@ -3,7 +3,6 @@ Copyright (c) 2023 Amelia Livingston. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Amelia Livingston, Joël Riou
 -/
-
 import Mathlib.Algebra.Homology.ShortComplex.ModuleCat
 import Mathlib.RepresentationTheory.GroupCohomology.Basic
 import Mathlib.RepresentationTheory.Invariants
