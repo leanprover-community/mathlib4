4d0e17445a0d47a7a691977c9be595fbe5a69bb8
Calle Sönne <calle.sonne@gmail.com>
2024-10-11 21:18:26 +0100

    mk_all

--- Mathlib.lean
+++ Mathlib.lean
@@ -1790,7 +1790,6 @@ import Mathlib.CategoryTheory.MorphismProperty.Concrete
 import Mathlib.CategoryTheory.MorphismProperty.Factorization
 import Mathlib.CategoryTheory.MorphismProperty.IsInvertedBy
 import Mathlib.CategoryTheory.MorphismProperty.Limits
-import Mathlib.CategoryTheory.MorphismProperty.Presheaf
 import Mathlib.CategoryTheory.MorphismProperty.Representable
 import Mathlib.CategoryTheory.NatIso
 import Mathlib.CategoryTheory.NatTrans
