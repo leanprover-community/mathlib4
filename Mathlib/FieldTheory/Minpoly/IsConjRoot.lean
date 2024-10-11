ec33fc271710838fb5f4eb88cda95504618a48ec
jjdishere <emailboxofjjd@163.com>
2024-10-12 01:22:56 +0200

    no sorry

--- Mathlib/FieldTheory/Minpoly/IsConjRoot.lean
+++ Mathlib/FieldTheory/Minpoly/IsConjRoot.lean
@@ -24,6 +24,9 @@ and `y` in `L`, `IsConjRoot K x y` is equivalent to the existence of an algebra 
 `σ : L ≃ₐ[K] L` such that `y = σ x`.
 * `not_mem_iff_exist_ne`: Let `L / K` be a normal field extension. For any element `x` in `L`,
 
+## TODO
+Add `IsConjRoot.smul` stating that if `x` and `y` are conjugate roots, then so are `r • x` and
+`r • y`.
 -/
 
 
@@ -73,25 +76,77 @@ theorem iff_exist_algEquiv [Normal K L] {x x' : L} :
     IsConjRoot K x x' ↔ ∃ σ : L ≃ₐ[K] L, σ x = x' :=
   ⟨exist_algEquiv, fun ⟨_, h⟩ => h ▸ of_algEquiv _ _⟩
 
-theorem iff_mem_aroots [CommRing A] [IsDomain A] [Algebra R A] {x x' : A} :
-    IsConjRoot R x x' ↔ x' ∈ (minpoly R x).aroots A := sorry
+theorem iff_mem_aroots {A} [CommRing A] [IsDomain A] [Nontrivial A] [Algebra K A] {x x' : A}
+    (h : IsIntegral K x) : IsConjRoot K x x' ↔ x' ∈ (minpoly K x).aroots A := by
+  rw [Polynomial.mem_aroots, iff_aeval_eq_zero h]
+  simp only [iff_and_self]
+  exact fun _ => minpoly.ne_zero h
 
-theorem iff_mem_minpoly_rootSet [CommRing A] [IsDomain A] [Algebra R A] {x x' : A} :
-    IsConjRoot R x x' ↔ x' ∈ (minpoly R x).rootSet A := iff_mem_aroots.trans (by sorry)
+theorem iff_mem_minpoly_rootSet {A} [CommRing A] [IsDomain A] [Algebra K A] {x x' : A}
+    (h : IsIntegral K x) : IsConjRoot K x x' ↔ x' ∈ (minpoly K x).rootSet A :=
+  (iff_mem_aroots h).trans (by simp [rootSet])
 
-theorem isIntegral {x x' : L} (hx : IsIntegral K x) (h : IsConjRoot K x x') :
-    IsIntegral K x' := by sorry
+theorem isIntegral {x x' : A} (hx : IsIntegral R x) (h : IsConjRoot R x x') :
+    IsIntegral R x' :=
+  ⟨minpoly R x, minpoly.monic hx, h ▸ minpoly.aeval R x'⟩
 
-theorem iff_eq_zero {x : A} : IsConjRoot R 0 x ↔ x = 0 := sorry
+/--
+A general form of `IsConjRoot.eq_of_isConjRoot_algebraMap`, only assuming `Nontrivial R`,
+`NoZeroSMulDivisors R A` and `Function.Injective (algebraMap R A)` instead of `Field R`
+-/
+theorem eq_of_isConjRoot_algebraMap' [Nontrivial R] {A} [CommRing A] [IsDomain A] [Algebra R A]
+    [NoZeroSMulDivisors R A] {r : R} {x : A} (h : IsConjRoot R x (algebraMap R A r))
+    (hf : Function.Injective (algebraMap R A)) : x = algebraMap R A r := by
+  rw [IsConjRoot, minpoly.eq_X_sub_C_of_algebraMap_inj _ hf] at h
+  have : x ∈ (X - C r).aroots A := by
+    rw [mem_aroots]
+    simp [X_sub_C_ne_zero, h ▸ minpoly.aeval R x]
+  simpa [aroots_X_sub_C] using this
 
-theorem ne_zero {x x' : A} (hx : x ≠ 0) (h : IsConjRoot R x x') : x' ≠ 0 := sorry
+theorem eq_of_isConjRoot_algebraMap {A} [CommRing A] [IsDomain A] [Nontrivial A] [Algebra K A]
+    {r : K} {x : A} (h : IsConjRoot K x (algebraMap K A r)) : x = algebraMap K A r :=
+  eq_of_isConjRoot_algebraMap' h (algebraMap K A).injective
 
--- seperable implies root number = degree
--- when degree >= 1, and split the aroot multiset and root set are not empty -- convert to Prod
--- Polynomial.nodup_aroots_iff_of_splits
--- Add `Polynomial.Splits` not `Normal`
-#check IntermediateField.splits_iff_mem
-#check Polynomial.card_rootSet_eq_natDegree
+/--
+A general form of `IsConj.eq_zero`, only assuming `Nontrivial R`,
+`NoZeroSMulDivisors R A` and `Function.Injective (algebraMap R A)` instead of `Field R`
+-/
+theorem eq_zero' [Nontrivial R] {A} [CommRing A] [IsDomain A] [Algebra R A]
+    [NoZeroSMulDivisors R A] {x : A} (h : IsConjRoot R x 0)
+    (hf : Function.Injective (algebraMap R A)) : x = 0 :=
+  (algebraMap R A).map_zero ▸ (eq_of_isConjRoot_algebraMap' ((algebraMap R A).map_zero ▸ h) hf)
+
+theorem eq_zero {A} [CommRing A] [IsDomain A] [Algebra K A] {x : A}
+    (h : IsConjRoot K x 0) : x = 0 :=
+  eq_zero' h (algebraMap K A).injective
+
+/--
+A general form of `IsConj.iff_eq_zero'`, only assuming `Nontrivial R`,
+`NoZeroSMulDivisors R A` and `Function.Injective (algebraMap R A)` instead of `Field R`
+-/
+theorem iff_eq_zero' {A} [Nontrivial R] [CommRing A] [IsDomain A] [Algebra R A]
+    [NoZeroSMulDivisors R A] {x : A} (hf : Function.Injective (algebraMap R A)) :
+    IsConjRoot R x 0 ↔ x = 0 :=
+  ⟨fun h => eq_zero' h hf, fun h => h.symm ▸ rfl⟩
+
+theorem iff_eq_zero {A} [CommRing A] [IsDomain A] [Algebra K A]
+    {x : A} :
+    IsConjRoot K x 0 ↔ x = 0 :=
+  iff_eq_zero' (algebraMap K A).injective
+
+/--
+A general form of `IsConj.ne_zero'`, only assuming `Nontrivial R`,
+`NoZeroSMulDivisors R A` and `Function.Injective (algebraMap R A)` instead of `Field R`
+-/
+theorem ne_zero' {A} [Nontrivial R] [CommRing A] [IsDomain A] [Algebra R A]
+    [NoZeroSMulDivisors R A] {x x' : A} (hx : x ≠ 0) (h : IsConjRoot R x x')
+    (hf : Function.Injective (algebraMap R A)) : x' ≠ 0 :=
+  fun h' => hx (eq_zero' (h' ▸ h) hf)
+
+theorem ne_zero {A} [CommRing A] [IsDomain A] [Algebra K A] {x x' : A}
+    (hx : x ≠ 0) (h : IsConjRoot K x x') : x' ≠ 0 :=
+  ne_zero' hx h (algebraMap K A).injective
+
 theorem not_mem_iff_exist_ne {x : L} (h : IsSeparable K x)
     (sp : (minpoly K x).Splits (algebraMap K L)) :
     x ∉ (⊥ : Subalgebra K L) ↔ ∃ x' : L, x ≠ x' ∧ IsConjRoot K x x' := by
@@ -104,8 +159,9 @@ theorem not_mem_iff_exist_ne {x : L} (h : IsSeparable K x)
       (nontrivial_iff_exists_ne ⟨x, mem_rootSet.mpr ⟨minpoly.ne_zero h.isIntegral,
           minpoly.aeval K x⟩⟩).trans ⟨fun ⟨x', hx'⟩ => ⟨x', Subtype.coe_ne_coe.mpr hx'⟩,
           fun ⟨x', hx'⟩ => ⟨x', Subtype.coe_ne_coe.mp hx'⟩⟩
-    _ ↔ _ := ⟨fun ⟨⟨x', hx'⟩, hne⟩  => ⟨x', ⟨hne.symm, iff_mem_minpoly_rootSet.mpr hx'⟩⟩,
-        fun ⟨x', hne, hx'⟩ => ⟨⟨x', iff_mem_minpoly_rootSet.mp hx'⟩, hne.symm⟩⟩
+    _ ↔ _ :=
+      ⟨fun ⟨⟨x', hx'⟩, hne⟩ => ⟨x', ⟨hne.symm, (iff_mem_minpoly_rootSet h.isIntegral).mpr hx'⟩⟩,
+          fun ⟨x', hne, hx'⟩ => ⟨⟨x', (iff_mem_minpoly_rootSet h.isIntegral).mp hx'⟩, hne.symm⟩⟩
 
 variable (R) in
 theorem of_isScalarTower {A} [CommRing A] [Algebra K A] [Algebra L A] [IsScalarTower K L A]
@@ -118,23 +174,16 @@ theorem algHom_iff {B} [Ring B] [Algebra R B] {x x' : A} (f : A →ₐ[R] B)
     IsConjRoot R (f x) (f x') ↔ IsConjRoot R x x' := by
   rw [isConjRoot_def, isConjRoot_def, algHom_eq f hf, algHom_eq f hf]
 
-theorem add_algebraMap {x x' : A} (r : R) (h : IsConjRoot R x x') :
-    IsConjRoot R (x + algebraMap R A r) (x' + algebraMap R A r) := sorry
--- minpoly.add_algebraMap
--- `should decide what is the definition when both x, x' are trancendental over R`
+theorem add_algebraMap {A} [CommRing A] [Algebra K A] {x x' : A} (r : K) (h : IsConjRoot K x x') :
+    IsConjRoot K (x + algebraMap K A r) (x' + algebraMap K A r) := by
+  rw [isConjRoot_def, minpoly.add_algebraMap x r, minpoly.add_algebraMap x' r, h]
 
-/-
+theorem sub_algebraMap {A} [CommRing A] [Algebra K A] {x x' : A} (r : K) (h : IsConjRoot K x x') :
+    IsConjRoot K (x - algebraMap K A r) (x' - algebraMap K A r) := by
+  simpa only [sub_eq_add_neg, map_neg] using add_algebraMap (-r) h
 
-theorem eq_of_isConjRoot_algebraMap {r : R} {x : A} (h : IsConjRoot R x (algebraMap R A r)) :
-    x = algebraMap R A r := sorry
+theorem neg {A} [CommRing A] [Algebra K A] {x x' : A} (h : IsConjRoot K x x') :
+    IsConjRoot K (-x) (-x') := by
+  rw [isConjRoot_def, minpoly.neg x, minpoly.neg x', h]
 
-theorem neg {x x' : A} (r : R) (h : IsConjRoot R x x') :  IsConjRoot R (-x) (-x') := sorry
-
-theorem sub_algebraMap {x x' : A} (r : R) (h : IsConjRoot R x x') :
-    IsConjRoot R (x - algebraMap R A r) (x' - algebraMap R A r) := sorry
-
-theorem smul {x x' : A} (r : R) (h : IsConjRoot R x x') :  IsConjRoot R (r • x) (r • x') := sorry -- maybe too hard?
-
-
--/
 end IsConjRoot
