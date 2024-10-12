5e0e3cc42e09435a522d71c1c74841cd4b27bda1
Violeta Hernández <vi.hdz.p@gmail.com>
2024-10-11 23:35:31 -0600

    fix

--- Mathlib.lean
+++ Mathlib.lean
@@ -3754,6 +3754,7 @@ import Mathlib.Order.Synonym
 import Mathlib.Order.TypeTags
 import Mathlib.Order.ULift
 import Mathlib.Order.UpperLower.Basic
+import Mathlib.Order.UpperLower.Defs
 import Mathlib.Order.UpperLower.Hom
 import Mathlib.Order.UpperLower.LocallyFinite
 import Mathlib.Order.WellFounded
