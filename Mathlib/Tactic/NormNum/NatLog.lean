b803652fe588601d9fef672f93a19bd37e0596f1
Kyle Miller <kmill31415@gmail.com>
2024-10-12 16:09:43 -0700

    special cases, tests

--- Mathlib/Tactic/NormNum/NatLog.lean
+++ Mathlib/Tactic/NormNum/NatLog.lean
@@ -17,6 +17,14 @@ namespace NormNum
 
 open Qq Lean Elab.Tactic Mathlib.Meta.NormNum
 
+lemma nat_log_zero (n : Nat) : Nat.log 0 n = 0 := by simp
+lemma nat_log_one (n : Nat) : Nat.log 1 n = 0 := by simp
+
+lemma nat_log_helper0 (b n : Nat) (hl : Nat.blt n b = true) :
+    Nat.log b n = 0 := by
+  rw [Nat.blt_eq] at hl
+  simp [hl]
+
 lemma nat_log_helper (b n k : Nat)
     (hl : Nat.ble (b ^ k) n = true) (hh : Nat.blt n (b ^ (k + 1)) = true) :
     Nat.log b n = k :=
@@ -26,19 +34,23 @@ theorem isNat_log : {b nb n nn k : ℕ} → IsNat b nb → IsNat n nn →
     Nat.log nb nn = k → IsNat (Nat.log b n) k
   | _, _, _, _, _, ⟨rfl⟩, ⟨rfl⟩, rfl => ⟨rfl⟩
 
-/-- Given the natural number literal `ex`, returns its square root as a natural number literal
-and an equality proof. Panics if `ex` isn't a natural number literal. -/
+/-- Given the natural number literals `eb` and `en`, returns `Nat.log eb en`
+as a natural number literal and an equality proof.
+Panics if `ex` isn't a natural number literal. -/
 def proveNatLog (eb en : Q(ℕ)) : (ek : Q(ℕ)) × Q(Nat.log $eb $en = $ek) :=
   match eb.natLit!, en.natLit! with
-  -- TODO: special cases for small values
-  -- | 0 => show (ey : Q(ℕ)) × Q(Nat.sqrt 0 = $ey) from ⟨mkRawNatLit 0, q(Nat.sqrt_zero)⟩
-  -- | 1 => show (ey : Q(ℕ)) × Q(Nat.sqrt 1 = $ey) from ⟨mkRawNatLit 1, q(Nat.sqrt_one)⟩
+  | 0, _ => show (ek : Q(ℕ)) × Q(Nat.log 0 $en = $ek) from ⟨mkRawNatLit 0, q(nat_log_zero $en)⟩
+  | 1, _ => show (ek : Q(ℕ)) × Q(Nat.log 1 $en = $ek) from ⟨mkRawNatLit 0, q(nat_log_one $en)⟩
   | b, n =>
-    let k := Nat.log b n
-    have ek : Q(ℕ) := mkRawNatLit k
-    have hl : Q(Nat.ble ($eb ^ $ek) $en = true) := (q(Eq.refl true) : Expr)
-    have hh : Q(Nat.blt $en ($eb ^ ($ek + 1)) = true):= (q(Eq.refl true) : Expr)
-    ⟨ek, q(nat_log_helper $eb $en $ek $hl $hh)⟩
+    if n < b then
+      have hh : Q(Nat.blt $en $eb = true):= (q(Eq.refl true) : Expr)
+      ⟨mkRawNatLit 0, q(nat_log_helper0 $eb $en $hh)⟩
+    else
+      let k := Nat.log b n
+      have ek : Q(ℕ) := mkRawNatLit k
+      have hl : Q(Nat.ble ($eb ^ $ek) $en = true) := (q(Eq.refl true) : Expr)
+      have hh : Q(Nat.blt $en ($eb ^ ($ek + 1)) = true):= (q(Eq.refl true) : Expr)
+      ⟨ek, q(nat_log_helper $eb $en $ek $hl $hh)⟩
 
 /-- Evaluates the `Nat.log` function. -/
 @[norm_num Nat.log _ _]
