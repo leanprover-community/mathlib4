212c7845756d30edcece6e91af02388e7057f679
Yury G. Kudryashov <urkud@urkud.name>
2024-10-12 01:44:41 -0500

    Update

--- Mathlib/Topology/Sets/Opens.lean
+++ Mathlib/Topology/Sets/Opens.lean
@@ -379,8 +379,8 @@ theorem comap_injective [T0Space β] : Injective (comap : C(α, β) → FrameHom
 /-- A homeomorphism induces an order-preserving equivalence on open sets, by taking comaps. -/
 @[simps (config := .asFn) apply]
 def _root_.Homeomorph.opensCongr (f : α ≃ₜ β) : Opens α ≃o Opens β where
-  toFun := Opens.comap f.symm.toContinuousMap
-  invFun := Opens.comap f.toContinuousMap
+  toFun := Opens.comap (f.symm : C(β, α))
+  invFun := Opens.comap (f : C(α, β))
   left_inv := fun U => ext <| f.toEquiv.preimage_symm_preimage _
   right_inv := fun U => ext <| f.toEquiv.symm_preimage_preimage _
   map_rel_iff' := by
