57a24453d3a4993d5c8a30a258d8586a9e4d9126
Violeta Hernández <vi.hdz.p@gmail.com>
2024-10-11 22:30:27 -0600

    minimize

--- Mathlib/Order/UpperLower/Defs.lean
+++ Mathlib/Order/UpperLower/Defs.lean
@@ -6,7 +6,6 @@ Authors: Yaël Dillies, Sara Rousta
 import Aesop
 import Mathlib.Data.Set.Defs
 import Mathlib.Tactic.ExtendDoc
-import Mathlib.Tactic.TypeStar
 
 /-!
 # Up-sets and down-sets
@@ -23,22 +22,22 @@ This file defines upper and lower sets in an order.
 * `LowerSet`: The type of lower sets.
 -/
 
-variable {α β : Type*} [LE α] [LE β] {s t : Set α} {a : α}
+universe u
 
 /-- An upper set in an order `α` is a set such that any element greater than one of its members is
 also a member. Also called up-set, upward-closed set. -/
 @[aesop norm unfold]
-def IsUpperSet (s : Set α) : Prop :=
+def IsUpperSet {α : Type u} [LE α] (s : Set α) : Prop :=
   ∀ ⦃a b : α⦄, a ≤ b → a ∈ s → b ∈ s
 
 /-- A lower set in an order `α` is a set such that any element less than one of its members is also
 a member. Also called down-set, downward-closed set. -/
 @[aesop norm unfold]
-def IsLowerSet (s : Set α) : Prop :=
+def IsLowerSet {α : Type u} [LE α] (s : Set α) : Prop :=
   ∀ ⦃a b : α⦄, b ≤ a → a ∈ s → b ∈ s
 
 @[inherit_doc IsUpperSet]
-structure UpperSet (α : Type*) [LE α] where
+structure UpperSet (α : Type u) [LE α] where
   /-- The carrier of an `UpperSet`. -/
   carrier : Set α
   /-- The carrier of an `UpperSet` is an upper set. -/
@@ -47,7 +46,7 @@ structure UpperSet (α : Type*) [LE α] where
 extend_docs UpperSet before "The type of upper sets of an order."
 
 @[inherit_doc IsLowerSet]
-structure LowerSet (α : Type*) [LE α] where
+structure LowerSet (α : Type u) [LE α] where
   /-- The carrier of a `LowerSet`. -/
   carrier : Set α
   /-- The carrier of a `LowerSet` is a lower set. -/
