32674ed5a1426efba9ce2e8a23c620445bd09278
Violeta Hernández <vi.hdz.p@gmail.com>
2024-10-11 19:27:05 -0600

    fix isNormal & isLimit

--- Mathlib/SetTheory/Cardinal/Aleph.lean
+++ Mathlib/SetTheory/Cardinal/Aleph.lean
@@ -396,11 +396,15 @@ theorem beth_pos (o : Ordinal) : 0 < beth o :=
 theorem beth_ne_zero (o : Ordinal) : beth o ≠ 0 :=
   (beth_pos o).ne'
 
+theorem isNormal_beth : IsNormal (ord ∘ beth) := by
+  refine (isNormal_iff_strictMono_limit _).2
+    ⟨ord_strictMono.comp beth_strictMono, fun o ho a ha ↦ ?_⟩
+  rw [comp_apply, beth_limit ho, ord_le]
+  exact ciSup_le' fun b => ord_le.1 (ha _ b.2)
+
+@[deprecated isNormal_beth (since := "2024-10-11")]
 theorem beth_normal : IsNormal.{u} fun o => (beth o).ord :=
-  (isNormal_iff_strictMono_limit _).2
-    ⟨ord_strictMono.comp beth_strictMono, fun o ho a ha => by
-      rw [beth_limit ho, ord_le]
-      exact ciSup_le' fun b => ord_le.1 (ha _ b.2)⟩
+  isNormal_beth
 
 end Cardinal
 
