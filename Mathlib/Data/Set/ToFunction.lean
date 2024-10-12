5a2aafba274ccce830c05bb82251db4fc2a90004
madvorak <martin.dvorak@matfyz.cz>
2024-10-12 16:09:44 +0200

    minimize imports

--- Mathlib/Data/Set/ToFunction.lean
+++ Mathlib/Data/Set/ToFunction.lean
@@ -3,7 +3,8 @@ Copyright (c) 2024 Martin Dvorak. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Martin Dvorak
 -/
-import Mathlib.Data.Set.Basic
+import Mathlib.Data.Set.Defs
+import Mathlib.Logic.ExistsUnique
 
 /-!
 # Set to function
@@ -44,7 +45,7 @@ section set_as_total_function
 def isFun (X : Set (α × α)) : Prop :=
   ∀ x : α, ∃! y : α, (x, y) ∈ X
 
-lemma isPartialFun_of_isFun {X : Set (α × α)} (hX : isFun X) : isPartialFun X := by
+theorem isPartialFun_of_isFun {X : Set (α × α)} (hX : isFun X) : isPartialFun X := by
   intro x y z ⟨hxy, hxz⟩
   have hy := (hX x).choose_spec.2 y hxy
   have hz := (hX x).choose_spec.2 z hxz
