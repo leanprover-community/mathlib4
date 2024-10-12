b2a0f8ead3141a22083411a23a6b86c02e60ef01
Eric Wieser <efw@google.com>
2024-10-12 14:41:55 +0100

    Update Calculus.lean

--- Mathlib/Analysis/InnerProductSpace/Calculus.lean
+++ Mathlib/Analysis/InnerProductSpace/Calculus.lean
@@ -7,6 +7,7 @@ import Mathlib.Analysis.InnerProductSpace.PiL2
 import Mathlib.Analysis.SpecialFunctions.Sqrt
 import Mathlib.Analysis.NormedSpace.HomeomorphBall
 import Mathlib.Analysis.Calculus.ContDiff.WithLp
+import Mathlib.Analysis.Calculus.FDeriv.WithLp
 
 /-!
 # Calculus in inner product spaces
