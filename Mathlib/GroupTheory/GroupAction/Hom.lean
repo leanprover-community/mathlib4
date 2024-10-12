060abc2afec9450b541d9e8bc37e4c323ab577fa
Michael Rothgang <rothgang@math.uni-bonn.de>
2024-10-12 08:58:15 +0200

    Revert "chore: uniformise more copyright headers"
    This reverts commit 8b54216e36ca43c3ca931c80e5c9a9ac260733f0.

--- Mathlib/GroupTheory/GroupAction/Hom.lean
+++ Mathlib/GroupTheory/GroupAction/Hom.lean
@@ -2,6 +2,7 @@
 Copyright (c) 2020 Kenny Lau. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kenny Lau, Antoine Chambert-Loir
+
 -/
 
 import Mathlib.Algebra.Module.Defs
