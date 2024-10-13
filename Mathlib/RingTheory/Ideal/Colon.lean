71046e259ae16763cfbb881369b4018e277e48d4
Junyan Xu <junyanxu.math@gmail.com>
2024-10-14 00:56:56 +0200

    fixes

--- Mathlib/RingTheory/Ideal/Colon.lean
+++ Mathlib/RingTheory/Ideal/Colon.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kenny Lau
 -/
 import Mathlib.LinearAlgebra.Quotient
-import Mathlib.RingTheory.Ideal.Operations
+import Mathlib.RingTheory.Ideal.Maps
 
 /-!
 # The colon ideal
