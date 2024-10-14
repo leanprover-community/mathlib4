1659e846af8e35f29f68ea84aaa66e291d430677
Anne Baanen <vierkantor@vierkantor.com>
2024-10-14 14:35:14 +0000

    chore(Group/Subsemigroup/Basic): split off Defs file (#17725)
    The criteria for including a result in `Algebra.Group.Subsemigroup.Basic`:
    
    * Can it be defined with only `Defs` imports?
    * Is it needed for a subsequent `def` or `instance`?
    
    This mostly prepares for splitting `Submonoid.Basic`.

--- Mathlib.lean
+++ Mathlib.lean
@@ -292,6 +292,7 @@ import Mathlib.Algebra.Group.Submonoid.Operations
 import Mathlib.Algebra.Group.Submonoid.Pointwise
 import Mathlib.Algebra.Group.Submonoid.Units
 import Mathlib.Algebra.Group.Subsemigroup.Basic
+import Mathlib.Algebra.Group.Subsemigroup.Defs
 import Mathlib.Algebra.Group.Subsemigroup.Membership
 import Mathlib.Algebra.Group.Subsemigroup.Operations
 import Mathlib.Algebra.Group.Support
