8b4406609154d3253078383cb2a76902b8d834f1
Riccardo Brasca <riccardo.brasca@gmail.com>
2024-10-15 16:30:34 +0200

    ops

--- Mathlib/LinearAlgebra/Dimension/Finrank.lean
+++ Mathlib/LinearAlgebra/Dimension/Finrank.lean
@@ -49,8 +49,8 @@ Defined by convention to be `0` if the space has infinite rank.
 For a vector space `M` over a field `R`, this is the same as the finite dimension
 of `M` over `R`.
 
-Not that it is possible to have `M` with `¬(Module.Finite R M)` but `finrank R M ≠ 0`, for example
-$$\mathbb{Z} \oplus (\mathbb{Z}/2\mathbb{Z})^{\mathbb{N}}$$.
+Note that it is possible to have `M` with `¬(Module.Finite R M)` but `finrank R M ≠ 0`, for example
+$$\mathbb{Z} \oplus (\mathbb{Z}/2\mathbb{Z})^{\mathbb{N}}$$ has `finrank` equal to `1`.
 -/
 noncomputable def finrank (R M : Type*) [Semiring R] [AddCommGroup M] [Module R M] : ℕ :=
   Cardinal.toNat (Module.rank R M)
