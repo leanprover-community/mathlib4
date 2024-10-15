4134b15e1becd67be909f198c9724715f773e8e2
Jireh Loreaux <loreaujy@gmail.com>
2024-10-15 10:37:05 -0500

    code review

--- Mathlib/Algebra/Notation.lean
+++ Mathlib/Algebra/Notation.lean
@@ -3,8 +3,8 @@ Copyright (c) 2024 Jireh Loreaux. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jireh Loreaux
 -/
-
 import Mathlib.Tactic.ToAdditive
+
 /-!
 # Notations for operations involving order and algebraic structure
 
@@ -16,12 +16,12 @@ import Mathlib.Tactic.ToAdditive
 * `a⁻ = (-a) ⊔ 0`: *Negative component* of an element `a` of a lattice ordered group
 -/
 
-/-- A notation class for the *positve part* function: `a⁺`. -/
+/-- A notation class for the *positive part* function: `a⁺`. -/
 class PosPart (α : Type*) where
   /-- The *positive part* of an element `a`. -/
   posPart : α → α
 
-/-- A notation class for the *positve part* function (multiplicative version): `a⁺ᵐ`. -/
+/-- A notation class for the *positive part* function (multiplicative version): `a⁺ᵐ`. -/
 @[to_additive]
 class OneLePart (α : Type*) where
   /-- The *positive part* of an element `a`. -/
