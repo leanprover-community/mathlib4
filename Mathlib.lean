86cbd062d89fd9c55592e3cd21d7d5646c0635c1
Robin Carlier <57142648+robin-carlier@users.noreply.github.com>
2024-10-15 16:07:08 +0200

    Update Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -1706,6 +1706,7 @@ import Mathlib.CategoryTheory.Limits.Shapes.WideEqualizers
 import Mathlib.CategoryTheory.Limits.Shapes.WidePullbacks
 import Mathlib.CategoryTheory.Limits.Shapes.ZeroMorphisms
 import Mathlib.CategoryTheory.Limits.Shapes.ZeroObjects
+import Mathlib.CategoryTheory.Limits.Sifted
 import Mathlib.CategoryTheory.Limits.SmallComplete
 import Mathlib.CategoryTheory.Limits.Types
 import Mathlib.CategoryTheory.Limits.TypesFiltered
