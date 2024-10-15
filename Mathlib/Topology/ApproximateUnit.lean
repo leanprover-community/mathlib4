32d8615eb9f568ebe3009e14ec2420a3c720779e
Jireh Loreaux <loreaujy@gmail.com>
2024-10-15 15:50:14 -0500

    fix imports

--- Mathlib/Topology/ApproximateUnit.lean
+++ Mathlib/Topology/ApproximateUnit.lean
@@ -3,7 +3,8 @@ Copyright (c) 2024 Jireh Loreaux. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jireh Loreaux
 -/
-import Mathlib
+import Mathlib.Topology.Bornology.Basic
+import Mathlib.Topology.Defs.Filter
 
 /-! # Approximate units
 
