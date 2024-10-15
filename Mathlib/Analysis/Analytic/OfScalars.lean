7a17f4a1c5a78094a0b3c8bab33ec4fef674bf2b
Edward Watine <02ewatine@gmail.com>
2024-10-15 16:45:04 +0100

    add an API ofScalars

---
+++ Mathlib/Analysis/Analytic/OfScalars.lean
@@ -0,0 +1,120 @@
+/-
+Copyright (c) 2024 Edward Watine. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Edward Watine
+-/
+import Mathlib.Analysis.Analytic.Basic
+
+
+/-!
+# Scalar series
+
+This file defines an API for `FormalMultilinearSeries.ofScalars`, which is a
+formal power series `∑ cᵢ • xⁱ`. It contains several convenience theorems, which are especially
+useful because Lean fails to recognise the scalar multiplication automatically. The main result of
+this file is `FormalMultilinearSeries.ofScalars_radius_of_tendsto`, which asserts the radius of
+convergence is equal to $\lim_{n \to \infty} \|c_{n+1}\|\cdot\|c_n\|^{-1}$ as long as it is not `0`.
+-/
+
+namespace FormalMultilinearSeries
+
+section Field
+
+variable {𝕜 : Type*} (E : Type*) [Field 𝕜] [Ring E] [Algebra 𝕜 E] [TopologicalSpace E]
+  [TopologicalRing E] (c : ℕ → 𝕜)
+
+/-- Formal power series of `∑ cᵢ • xⁱ` for some scalar field `𝕜` and ring algebra `E`-/
+def ofScalars : FormalMultilinearSeries 𝕜 E E :=
+  fun n ↦ c n • ContinuousMultilinearMap.mkPiAlgebraFin 𝕜 n E
+
+variable {E}
+
+/-- The sum of the formal power series. Takes the value `0` outside the radius of convergence. -/
+noncomputable def ofScalarsSum (x : E) := (ofScalars E c).sum x
+
+theorem ofScalars_apply_eq (x : E) (n : ℕ) :
+    ofScalars E c n (fun _ ↦ x) = c n • x ^ n := by
+  simp [ofScalars]
+
+/-- This naming follows the convention of `NormedSpace.expSeries_apply_eq'`. -/
+theorem ofScalars_apply_eq' (x : E) :
+    (fun n ↦ ofScalars E c n (fun _ ↦ x)) = fun n ↦ c n • x ^ n := by
+  simp [ofScalars]
+
+theorem ofScalars_sum_eq (x : E) : ofScalarsSum c x =
+    ∑' n, c n • x ^ n := tsum_congr fun n => ofScalars_apply_eq c x n
+
+theorem ofScalarsSum_eq_tsum : ofScalarsSum c =
+    fun (x : E) => ∑' n : ℕ, c n • x ^ n := funext (ofScalars_sum_eq c)
+
+@[simp]
+theorem ofScalars_op [T2Space E] (x : E) :
+    ofScalarsSum c (MulOpposite.op x) = MulOpposite.op (ofScalarsSum c x) := by
+  simp [ofScalars, ofScalars_sum_eq, ← MulOpposite.op_pow, ← MulOpposite.op_smul, tsum_op]
+
+@[simp]
+theorem ofScalars_unop [T2Space E] (x : Eᵐᵒᵖ) :
+    ofScalarsSum c (MulOpposite.unop x) = MulOpposite.unop (ofScalarsSum c x) := by
+  simp [ofScalars, ofScalars_sum_eq, ← MulOpposite.unop_pow, ← MulOpposite.unop_smul, tsum_unop]
+
+@[simp]
+theorem ofScalars_eq_zero [Nontrivial E] (n : ℕ) : ofScalars E c n = 0 ↔ c n = 0 := by
+  rw [ofScalars, smul_eq_zero (c := c n) (x := ContinuousMultilinearMap.mkPiAlgebraFin 𝕜 n E)]
+  refine or_iff_left (ContinuousMultilinearMap.ext_iff.1.mt <| not_forall_of_exists_not ?_)
+  use fun _ ↦ 1
+  simp
+
+end Field
+
+section Normed
+
+open Filter
+open scoped Topology
+
+variable {𝕜 : Type*} (E : Type*) [NontriviallyNormedField 𝕜] [NormedRing E]
+    [NormedAlgebra 𝕜 E] (c : ℕ → 𝕜) (n : ℕ)
+
+theorem ofScalars_norm_eq_mul :
+    ‖ofScalars E c n‖ = ‖c n‖ * ‖ContinuousMultilinearMap.mkPiAlgebraFin 𝕜 n E‖ := by
+  rw [ofScalars, norm_smul (c n) (ContinuousMultilinearMap.mkPiAlgebraFin 𝕜 n E)]
+
+theorem ofScalars_norm_le (hn : n > 0) : ‖ofScalars E c n‖ ≤ ‖c n‖ := by
+  simp only [ofScalars_norm_eq_mul]
+  exact (mul_le_of_le_one_right (norm_nonneg _)
+    (ContinuousMultilinearMap.norm_mkPiAlgebraFin_le_of_pos hn))
+
+@[simp]
+theorem ofScalars_norm [NormOneClass E] : ‖ofScalars E c n‖ = ‖c n‖ := by
+  simp [ofScalars_norm_eq_mul]
+
+/-- The radius of convergence of a scalar series is the inverse of the ratio of the norms of the
+coefficients. -/
+theorem ofScalars_radius_of_tendsto [NormOneClass E] {r : NNReal} (hr : r ≠ 0)
+    (hc : Tendsto (fun n ↦ ‖c n.succ‖ / ‖c n‖) atTop (𝓝 r)) :
+      (ofScalars E c).radius = ENNReal.ofNNReal r⁻¹ := by
+  have hc' {r' : NNReal} (hr' : (r' : ℝ) ≠ 0) :
+    Tendsto (fun n ↦ ‖‖ofScalars E c (n + 1)‖ * r' ^ (n + 1)‖ /
+      ‖‖ofScalars E c n‖ * r' ^ n‖) atTop (𝓝 ↑(r' * r)) := by
+    simp_rw [norm_mul, norm_norm, ofScalars_norm, mul_div_mul_comm, ← norm_div, pow_succ,
+      mul_div_right_comm, div_self (pow_ne_zero _ hr'), one_mul, norm_div, NNReal.norm_eq]
+    exact mul_comm r' r ▸ Filter.Tendsto.mul hc tendsto_const_nhds
+
+  apply le_antisymm <;> refine ENNReal.le_of_forall_nnreal_lt (fun r' hr' ↦ ?_)
+  · rw [ENNReal.coe_le_coe, NNReal.le_inv_iff_mul_le hr]
+    have := FormalMultilinearSeries.summable_norm_mul_pow _ hr'
+    contrapose! this
+    have hrz : (r' : ℝ) ≠ 0 := by aesop
+    apply not_summable_of_ratio_test_tendsto_gt_one this
+    exact hc' (by aesop)
+  · rw [ENNReal.coe_lt_coe, NNReal.lt_inv_iff_mul_lt hr] at hr'
+    by_cases hrz : r' = 0
+    · simp [hrz]
+    · apply FormalMultilinearSeries.le_radius_of_summable_norm
+      refine summable_of_ratio_test_tendsto_lt_one hr' ?_ <| hc' (NNReal.coe_ne_zero.2 hrz)
+      refine (Tendsto.eventually_ne hc (NNReal.coe_ne_zero.2 hr)).mp (Eventually.of_forall ?_)
+      simp_rw [div_ne_zero_iff, ofScalars_norm, mul_ne_zero_iff]
+      aesop
+
+end Normed
+
+end FormalMultilinearSeries
--- Mathlib/Analysis/SpecialFunctions/OrdinaryHypergeometric.lean
+++ Mathlib/Analysis/SpecialFunctions/OrdinaryHypergeometric.lean
@@ -3,7 +3,7 @@ Copyright (c) 2024 Edward Watine. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Edward Watine
 -/
-import Mathlib.Analysis.Analytic.Basic
+import Mathlib.Analysis.Analytic.OfScalars
 import Mathlib.Analysis.SpecificLimits.RCLike
 
 /-!
@@ -49,18 +49,21 @@ See <https://en.wikipedia.org/wiki/Hypergeometric_function>.
 hypergeometric, gaussian, ordinary
 -/
 
-open Nat
+open Nat FormalMultilinearSeries
 
 section Field
 
 variable {𝕂 : Type*} (𝔸 : Type*) [Field 𝕂] [Ring 𝔸] [Algebra 𝕂 𝔸] [TopologicalSpace 𝔸]
   [TopologicalRing 𝔸]
 
+/-- The coefficients in the ordinary hypergeometric sum. -/
+noncomputable abbrev ordinaryHypergeometricCoefficient (a b c : 𝕂) := fun n => ((n !⁻¹ : 𝕂) *
+    (ascPochhammer 𝕂 n).eval a * (ascPochhammer 𝕂 n).eval b * ((ascPochhammer 𝕂 n).eval c)⁻¹)
+
 /-- `ordinaryHypergeometricSeries 𝔸 (a b c : 𝕂)` is a `FormalMultilinearSeries`.
 Its sum is the `ordinaryHypergeometric` map. -/
 noncomputable def ordinaryHypergeometricSeries (a b c : 𝕂) : FormalMultilinearSeries 𝕂 𝔸 𝔸 :=
-  fun n => ((n !⁻¹ : 𝕂) * (ascPochhammer 𝕂 n).eval a * (ascPochhammer 𝕂 n).eval b *
-    ((ascPochhammer 𝕂 n).eval c)⁻¹ ) • ContinuousMultilinearMap.mkPiAlgebraFin 𝕂 n 𝔸
+  ofScalars 𝔸 (ordinaryHypergeometricCoefficient a b c)
 
 variable {𝔸} (a b c : 𝕂)
 
@@ -79,14 +82,14 @@ theorem ordinaryHypergeometricSeries_apply_eq (x : 𝔸) (n : ℕ) :
     (ordinaryHypergeometricSeries 𝔸 a b c n fun _ => x) =
       ((n !⁻¹ : 𝕂) * (ascPochhammer 𝕂 n).eval a * (ascPochhammer 𝕂 n).eval b *
         ((ascPochhammer 𝕂 n).eval c)⁻¹ ) • x ^ n := by
-  simp [ordinaryHypergeometricSeries]
+  rw [ordinaryHypergeometricSeries, ofScalars_apply_eq]
 
 /-- This naming follows the convention of `NormedSpace.expSeries_apply_eq'`. -/
 theorem ordinaryHypergeometricSeries_apply_eq' (x : 𝔸) :
     (fun n => ordinaryHypergeometricSeries 𝔸 a b c n fun _ => x) =
       fun n => ((n !⁻¹ : 𝕂) * (ascPochhammer 𝕂 n).eval a * (ascPochhammer 𝕂 n).eval b *
         ((ascPochhammer 𝕂 n).eval c)⁻¹ ) • x ^ n := by
-  simp [ordinaryHypergeometricSeries]
+  rw [ordinaryHypergeometricSeries, ofScalars_apply_eq']
 
 theorem ordinaryHypergeometric_sum_eq (x : 𝔸) : (ordinaryHypergeometricSeries 𝔸 a b c).sum x =
     ∑' n : ℕ, ((n !⁻¹ : 𝕂) * (ascPochhammer 𝕂 n).eval a * (ascPochhammer 𝕂 n).eval b *
@@ -100,7 +103,7 @@ theorem ordinaryHypergeometric_eq_tsum : ₂F₁ a b c =
 
 theorem ordinaryHypergeometricSeries_apply_zero (n : ℕ) :
     (ordinaryHypergeometricSeries 𝔸 a b c n fun _ => 0) = Pi.single (f := fun _ => 𝔸) 0 1 n := by
-  rw [ordinaryHypergeometricSeries_apply_eq]
+  rw [ordinaryHypergeometricSeries, ofScalars_apply_eq, ordinaryHypergeometricCoefficient]
   cases n <;> simp
 
 @[simp]
@@ -108,29 +111,16 @@ theorem ordinaryHypergeometric_zero : ₂F₁ a b c (0 : 𝔸) = 1 := by
   simp [ordinaryHypergeometric_eq_tsum, ← ordinaryHypergeometricSeries_apply_eq,
     ordinaryHypergeometricSeries_apply_zero]
 
-@[simp]
-theorem ordinaryHypergeometric_op [T2Space 𝔸] (x : 𝔸) :
-    ₂F₁ a b c (MulOpposite.op x) = MulOpposite.op (₂F₁ a b c x) := by
-  simp [ordinaryHypergeometric, ordinaryHypergeometric_sum_eq, ← MulOpposite.op_pow,
-    ← MulOpposite.op_smul, tsum_op]
-
-@[simp]
-theorem ordinaryHypergeometric_unop [T2Space 𝔸] (x : 𝔸ᵐᵒᵖ) :
-    ₂F₁ a b c (MulOpposite.unop x) = MulOpposite.unop (₂F₁ a b c x) := by
-  simp [ordinaryHypergeometric, ordinaryHypergeometric_sum_eq, ← MulOpposite.unop_pow,
-     ← MulOpposite.unop_smul, tsum_unop]
-
 theorem ordinaryHypergeometricSeries_symm :
     ordinaryHypergeometricSeries 𝔸 a b c = ordinaryHypergeometricSeries 𝔸 b a c := by
-  ext
-  simp [ordinaryHypergeometricSeries, mul_assoc, mul_left_comm]
+  unfold ordinaryHypergeometricSeries ordinaryHypergeometricCoefficient
+  simp [mul_assoc, mul_left_comm]
 
 /-- If any parameter to the series is a sufficiently large nonpositive integer, then the series
 term is zero. -/
-lemma ordinaryHypergeometricSeries_eq_zero_of_neg_nat {n k : ℕ}
-    (habc : k = -a ∨ k = -b ∨ k = -c) (hk : k < n) :
-      ordinaryHypergeometricSeries 𝔸 a b c n = 0 := by
-  rw [ordinaryHypergeometricSeries]
+lemma ordinaryHypergeometricSeries_eq_zero_of_neg_nat {n k : ℕ} (habc : k = -a ∨ k = -b ∨ k = -c)
+    (hk : k < n) : ordinaryHypergeometricSeries 𝔸 a b c n = 0 := by
+  rw [ordinaryHypergeometricSeries, ofScalars]
   rcases habc with h | h | h
   all_goals
     ext
@@ -164,34 +154,24 @@ theorem ordinaryHypergeometric_radius_top_of_neg_nat₃ {k : ℕ} :
 
 /-- An iff variation on `ordinaryHypergeometricSeries_eq_zero_of_nonpos_int` for `[RCLike 𝕂]`. -/
 lemma ordinaryHypergeometricSeries_eq_zero_iff (n : ℕ) :
-    ordinaryHypergeometricSeries 𝔸 a b c n = 0 ↔
-      ∃ k < n, k = -a ∨ k = -b ∨ k = -c := by
+    ordinaryHypergeometricSeries 𝔸 a b c n = 0 ↔ ∃ k < n, k = -a ∨ k = -b ∨ k = -c := by
   refine ⟨fun h ↦ ?_, fun zero ↦ ?_⟩
-  · rw [ordinaryHypergeometricSeries,
-      smul_eq_zero (c:=(_ * (Polynomial.eval c (ascPochhammer 𝕂 n))⁻¹))
-      (x:=ContinuousMultilinearMap.mkPiAlgebraFin 𝕂 n 𝔸)] at h
-    cases' h with h hm
-    · simp only [_root_.mul_eq_zero, inv_eq_zero] at h
-      rcases h with ((hn | h) | h) | h
-      · simp [Nat.factorial_ne_zero] at hn
-      all_goals
-        obtain ⟨kn, hkn, hn⟩ := (ascPochhammer_eval_eq_zero_iff _ _).1 h
-        exact ⟨kn, hkn, by tauto⟩
-    · rw [ContinuousMultilinearMap.ext_iff] at hm
-      absurd hm
-      push_neg
-      exact ⟨fun _ ↦ 1, by simp⟩
+  · rw [ordinaryHypergeometricSeries, ofScalars_eq_zero] at h
+    simp only [_root_.mul_eq_zero, inv_eq_zero] at h
+    rcases h with ((hn | h) | h) | h
+    · simp [Nat.factorial_ne_zero] at hn
+    all_goals
+      obtain ⟨kn, hkn, hn⟩ := (ascPochhammer_eval_eq_zero_iff _ _).1 h
+      exact ⟨kn, hkn, by tauto⟩
   · obtain ⟨_, h, hn⟩ := zero
     exact ordinaryHypergeometricSeries_eq_zero_of_neg_nat a b c hn h
 
 theorem ordinaryHypergeometricSeries_succ_norm_div_norm (n : ℕ)
     (habc : ∀ kn < n, (↑kn ≠ -a ∧ ↑kn ≠ -b ∧ ↑kn ≠ -c)) :
-      ‖ordinaryHypergeometricSeries 𝔸 a b c (n+1)‖ / ‖ordinaryHypergeometricSeries 𝔸 a b c n‖ =
-        ‖a + n‖ * ‖b + n‖ * ‖c + n‖⁻¹ * ‖1 + (n : 𝕂)‖⁻¹ := by
-  simp [ordinaryHypergeometricSeries, factorial_succ, ascPochhammer_succ_eval]
-  rw [norm_smul (x:=ContinuousMultilinearMap.mkPiAlgebraFin 𝕂 (n + 1) 𝔸),
-    norm_smul (x:=ContinuousMultilinearMap.mkPiAlgebraFin 𝕂 n 𝔸)]
-  simp only [norm_mul, norm_inv, ContinuousMultilinearMap.norm_mkPiAlgebraFin, mul_one]
+      ‖ordinaryHypergeometricCoefficient a b c n.succ‖ / ‖ordinaryHypergeometricCoefficient a b c n‖
+      = ‖a + n‖ * ‖b + n‖ * ‖c + n‖⁻¹ * ‖1 + (n : 𝕂)‖⁻¹ := by
+  simp only [mul_inv_rev, factorial_succ, cast_mul, cast_add,
+    cast_one, ascPochhammer_succ_eval, norm_mul, norm_inv]
   have : ‖(n ! : 𝕂)‖⁻¹ * ‖(n : 𝕂) + 1‖⁻¹ * (‖Polynomial.eval a (ascPochhammer 𝕂 n)‖ * ‖a + n‖) *
     (‖Polynomial.eval b (ascPochhammer 𝕂 n)‖ * ‖b + n‖) *
     (‖c + n‖⁻¹ * ‖Polynomial.eval c (ascPochhammer 𝕂 n)‖⁻¹) /
@@ -222,46 +202,20 @@ theorem ordinaryHypergeometricSeries_ratio_tendsto_one_atTop :
   apply (mul_one (1 : 𝕂)) ▸ Filter.Tendsto.mul <;>
   convert RCLike.tendsto_add_div_add_atTop_nhds _ _ (1 : 𝕂) one_ne_zero <;> simp
 
-/-- The ratio of successive terms of the sum `ordinaryHypergeometric a b c r` is `r`. This theorem
-is used for the ratio test in `ordinaryHypergeometricSeries_radius_eq_one`. -/
-theorem ordinaryHypergeometric_ratio_tendsto_nhds_atTop {r : ℝ} (hr : 0 < r)
-    (habc : ∀ kn : ℕ, ↑kn ≠ -a ∧ ↑kn ≠ -b ∧ ↑kn ≠ -c) : Tendsto (fun n ↦
-      ‖‖ordinaryHypergeometricSeries 𝔸 a b c (n + 1)‖ * r ^ (n + 1)‖ /
-        ‖‖ordinaryHypergeometricSeries 𝔸 a b c n‖ * r ^ n‖) atTop (𝓝 r) := by
-  simp_rw [← norm_div, mul_div_mul_comm, pow_succ, mul_div_right_comm]
-  apply Real.norm_of_nonneg (le_of_lt hr) ▸ Filter.Tendsto.norm
-  conv =>
-    enter [1, n]
-    rw [ordinaryHypergeometricSeries_succ_norm_div_norm, div_self (pow_ne_zero n hr.ne.symm),
-      one_mul, ← norm_mul, ← norm_inv, ← norm_mul, ← norm_inv, ← norm_mul]
-    rfl
-    tactic => aesop
-  apply (one_mul (_ : ℝ)) ▸ Filter.Tendsto.mul_const _ _
-  exact (norm_one (α := 𝕂)) ▸ Filter.Tendsto.norm
-    (ordinaryHypergeometricSeries_ratio_tendsto_one_atTop a b c)
-
 /-- The radius of convergence of `ordinaryHypergeometricSeries` is unity if none of the parameters
-are non-positive integers. This proof uses a similar technique to
-`formalMultilinearSeries_geometric_radius`. -/
+are non-positive integers. -/
 theorem ordinaryHypergeometricSeries_radius_eq_one
     (habc : ∀ kn : ℕ, ↑kn ≠ -a ∧ ↑kn ≠ -b ∧ ↑kn ≠ -c) :
       (ordinaryHypergeometricSeries 𝔸 a b c).radius = 1 := by
-  apply le_antisymm <;> refine ENNReal.le_of_forall_nnreal_lt (fun r hr ↦ ?_)
-  · rw [← ENNReal.coe_one, ENNReal.coe_le_coe]
-    have := FormalMultilinearSeries.summable_norm_mul_pow _ hr
-    contrapose! this
-    apply not_summable_of_ratio_test_tendsto_gt_one this
-    refine ordinaryHypergeometric_ratio_tendsto_nhds_atTop 𝔸 a b c ?_ habc
-    exact NNReal.coe_pos.2 (lt_trans zero_lt_one this)
-  · rw [← Nat.cast_one, ENNReal.coe_lt_natCast, Nat.cast_one] at hr
-    by_cases hr' : r = 0
-    · simp [hr']
-    · apply FormalMultilinearSeries.le_radius_of_summable_norm
-      apply summable_of_ratio_test_tendsto_lt_one hr
-      · refine Eventually.of_forall (fun n ↦ mul_ne_zero (norm_ne_zero_iff.2 ?_) ?_)
-        · exact (ordinaryHypergeometricSeries_eq_zero_iff 𝔸 a b c n).not.2 (by aesop)
-        · exact (pow_ne_zero n <| NNReal.coe_ne_zero.2 hr')
-      apply ordinaryHypergeometric_ratio_tendsto_nhds_atTop 𝔸 a b c ?_ habc
-      exact (Ne.intro hr').lt_of_le' (NNReal.coe_nonneg r)
+  convert ofScalars_radius_of_tendsto 𝔸 _ one_ne_zero ?_
+  · simp
+  · conv =>
+      enter [1, n]
+      rw [ordinaryHypergeometricSeries_succ_norm_div_norm,
+        ← norm_mul, ← norm_inv, ← norm_mul, ← norm_inv, ← norm_mul]
+      rfl
+      tactic => aesop
+    convert Filter.Tendsto.norm (ordinaryHypergeometricSeries_ratio_tendsto_one_atTop a b c)
+    simp
 
 end RCLike
