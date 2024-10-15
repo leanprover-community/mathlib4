b0c64104a929a7620f90d12de9386aa7e387e821
Anne C.A. Baanen <vierkantor@vierkantor.com>
2024-10-15 13:59:02 +0200

    Fix module docs and shake.
    (The declarations don't use the imports in `Order.MinMax` but the `@[deprecated]` argument does.

--- Mathlib/Deprecated/MinMax.lean
+++ Mathlib/Deprecated/MinMax.lean
@@ -1,5 +1,16 @@
+/-
+Copyright (c) 2017 Mario Carneiro. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Mario Carneiro
+-/
 import Mathlib.Deprecated.Logic
 import Mathlib.Order.MinMax
+/-!
+# Note about deprecated files
+
+This file is deprecated, and is no longer imported by anything in mathlib other than other
+deprecated files, and test files. You should not need to import it.
+-/
 
 universe u
 
