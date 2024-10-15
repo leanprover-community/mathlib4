23f702a6fcea4c12960ffaee2341bcc484498e4d
Anne Baanen <vierkantor@vierkantor.com>
2024-10-15 00:40:57 +0000

    chore(Group.Submonoid.Basic): split off `Defs` (#17726)
    The criteria for including a result in `Algebra.Group.Submonoid.Basic`:
     * Can it be defined with only `Defs` imports?
     * Is it needed for a subsequent `def` or `instance`?

--- Mathlib/RingTheory/OreLocalization/OreSet.lean
+++ Mathlib/RingTheory/OreLocalization/OreSet.lean
@@ -3,7 +3,7 @@ Copyright (c) 2022 Jakob von Raumer. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jakob von Raumer, Kevin Klinge
 -/
-import Mathlib.Algebra.Group.Submonoid.Basic
+import Mathlib.Algebra.Group.Submonoid.Defs
 import Mathlib.Algebra.GroupWithZero.Basic
 import Mathlib.Algebra.Ring.Regular
 
