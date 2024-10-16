299b0e5fd6473c6fcf29830e3331828d7ff51f06
Eric Wieser <wieser.eric@gmail.com>
2024-10-16 10:30:44 +0000

    more splits

--- Mathlib/Algebra/Category/ModuleCat/Injective.lean
+++ Mathlib/Algebra/Category/ModuleCat/Injective.lean
@@ -3,43 +3,62 @@ Copyright (c) 2023 Jujian Zhang. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jujian Zhang
 -/
-import Mathlib.Algebra.Category.ModuleCat.ChangeOfRings
-import Mathlib.Algebra.Category.Grp.EnoughInjectives
-import Mathlib.Algebra.Category.Grp.ZModuleEquivalence
+import Mathlib.Algebra.Module.Injective
+import Mathlib.GroupTheory.Divisible
+import Mathlib.Algebra.EuclideanDomain.Int
 import Mathlib.Logic.Equiv.TransferInstance
+import Mathlib.RingTheory.PrincipalIdealDomain
+import Mathlib.CategoryTheory.Preadditive.Injective
+import Mathlib.Algebra.Category.ModuleCat.EpiMono
 
 /-!
-# Category of $R$-modules has enough injectives
-
-we lift enough injectives of abelian groups to arbitrary $R$-modules by adjoint functors
-`restrictScalars ⊣ coextendScalars`
-
-## Implementation notes
-
-This file is not part of `Algebra/Module/Injective.lean` to prevent import loop: enough-injectives
-of abelian groups needs `Algebra/Module/Injective.lean` and this file needs enough-injectives of
-abelian groups.
+# Injective objects in the category of $R$-modules
 -/
 
 open CategoryTheory
 
-universe v u
-variable (R : Type u) [Ring R]
+universe u v
+variable (R : Type u) (M : Type v) [Ring R] [AddCommGroup M] [Module R M]
+namespace Module
 
-instance : EnoughInjectives (ModuleCat.{v} ℤ) :=
-  EnoughInjectives.of_equivalence (forget₂ (ModuleCat ℤ) AddCommGrp)
+theorem Baer.of_divisible [DivisibleBy M ℤ] : Module.Baer ℤ M := fun I g ↦ by
+  rcases IsPrincipalIdealRing.principal I with ⟨m, rfl⟩
+  obtain rfl | h0 := eq_or_ne m 0
+  · refine ⟨0, fun n hn ↦ ?_⟩
+    rw [Submodule.span_zero_singleton] at hn
+    subst hn
+    exact (map_zero g).symm
+  let gₘ := g ⟨m, Submodule.subset_span (Set.mem_singleton _)⟩
+  refine ⟨LinearMap.toSpanSingleton ℤ M (DivisibleBy.div gₘ m), fun n hn ↦ ?_⟩
+  rcases Submodule.mem_span_singleton.mp hn with ⟨n, rfl⟩
+  rw [map_zsmul, LinearMap.toSpanSingleton_apply, DivisibleBy.div_cancel gₘ h0, ← map_zsmul g,
+    SetLike.mk_smul_mk]
 
-lemma ModuleCat.enoughInjectives : EnoughInjectives (ModuleCat.{max v u} R) :=
-  EnoughInjectives.of_adjunction (ModuleCat.restrictCoextendScalarsAdj.{max v u} (algebraMap ℤ R))
+theorem injective_object_of_injective_module [inj : Injective R M] :
+    CategoryTheory.Injective (ModuleCat.of R M) where
+  factors g f m :=
+    have ⟨l, h⟩ := inj.out f ((ModuleCat.mono_iff_injective f).mp m) g
+    ⟨l, LinearMap.ext h⟩
 
-open ModuleCat in
-instance [UnivLE.{u,v}] : EnoughInjectives (ModuleCat.{v} R) :=
-  letI := (equivShrink.{v} R).symm.ring
-  letI := enoughInjectives.{v} (Shrink.{v} R)
-  EnoughInjectives.of_equivalence (restrictScalars (equivShrink R).symm.ringEquiv.toRingHom)
+theorem injective_module_of_injective_object
+    [inj : CategoryTheory.Injective <| ModuleCat.of R M] :
+    Module.Injective R M where
+  out X Y _ _ _ _ f hf g := by
+    have : CategoryTheory.Mono (ModuleCat.asHom f) := (ModuleCat.mono_iff_injective _).mpr hf
+    obtain ⟨l, rfl⟩ := inj.factors (ModuleCat.asHom g) (ModuleCat.asHom f)
+    exact ⟨l, fun _ ↦ rfl⟩
+
+theorem injective_iff_injective_object :
+    Module.Injective R M ↔
+    CategoryTheory.Injective (ModuleCat.of R M) :=
+  ⟨fun _ => injective_object_of_injective_module R M,
+   fun _ => injective_module_of_injective_object R M⟩
+
+end Module
+
 
 instance ModuleCat.ulift_injective_of_injective.{v'}
-    {M : Type v} [Small.{v} R] [AddCommGroup M] [Module R M]
+    [Small.{v} R] [AddCommGroup M] [Module R M]
     [CategoryTheory.Injective <| ModuleCat.of R M] :
     CategoryTheory.Injective <| ModuleCat.of R (ULift.{v'} M) :=
   Module.injective_object_of_injective_module
