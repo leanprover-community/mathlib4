4adfa8439be80cd5878aed1b341306f7e47b12fb
Jujian Zhang <jujian.zhang1998@outlook.com>
2024-10-16 08:36:38 +0100

    initial

--- Mathlib.lean
+++ Mathlib.lean
@@ -3164,6 +3164,7 @@ import Mathlib.LinearAlgebra.TensorProduct.Matrix
 import Mathlib.LinearAlgebra.TensorProduct.Opposite
 import Mathlib.LinearAlgebra.TensorProduct.Pi
 import Mathlib.LinearAlgebra.TensorProduct.Prod
+import Mathlib.LinearAlgebra.TensorProduct.Quotient
 import Mathlib.LinearAlgebra.TensorProduct.RightExactness
 import Mathlib.LinearAlgebra.TensorProduct.Subalgebra
 import Mathlib.LinearAlgebra.TensorProduct.Submodule
