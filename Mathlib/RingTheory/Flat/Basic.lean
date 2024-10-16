9b4d3b1284baae23b341fc37d9d2d4146c21128e
Jujian Zhang <jujian.zhang1998@outlook.com>
2024-10-16 22:54:52 +0100

    fucked up merge

--- Mathlib/RingTheory/Flat/Basic.lean
+++ Mathlib/RingTheory/Flat/Basic.lean
@@ -2,7 +2,6 @@
 Copyright (c) 2020 Johan Commelin. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johan Commelin, Jujian Zhang, Yongle Hu
-Authors: Johan Commelin, Jujian Zhang, Yongle Hu
 -/
 import Mathlib.Algebra.DirectSum.Finsupp
 import Mathlib.Algebra.DirectSum.Module
