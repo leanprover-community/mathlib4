10aa218e5ae483ff8177b541bccce8a4b80a5f47
David Loeffler <d.loeffler.01@cantab.net>
2024-10-17 17:53:03 +0200

    add author

--- Mathlib/NumberTheory/LSeries/DirichletContinuation.lean
+++ Mathlib/NumberTheory/LSeries/DirichletContinuation.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2024 David Loeffler. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: David Loeffler
+Authors: David Loeffler, Michael Stoll
 -/
 import Mathlib.NumberTheory.LSeries.ZMod
 import Mathlib.NumberTheory.DirichletCharacter.Basic
