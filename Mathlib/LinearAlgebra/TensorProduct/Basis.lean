17ec7022d35654ee08e26d43ae8d0f05764974bf
Jujian Zhang <jujian.zhang1998@outlook.com>
2024-10-17 07:03:56 +0100

    hmm

--- Mathlib/LinearAlgebra/TensorProduct/Basis.lean
+++ Mathlib/LinearAlgebra/TensorProduct/Basis.lean
@@ -67,11 +67,13 @@ lemma Basis.baseChange_apply (b : Basis ι R M) (i) :
     b.baseChange S i = 1 ⊗ₜ b i := by
   simp [Basis.baseChange, Basis.tensorProduct]
 
-variable {ιN ιM : Type*} (ℬ : Basis ιM R M) (𝒞 : Basis ιN R N) (x : M ⊗[R] N)
+section
+
+variable (ℬ : Basis ι R M) (𝒞 : Basis κ R N) (x : M ⊗[R] N)
 
 /-- Elements in M ⊗ N can be represented by sum of elements in M tensor elements of basis of N.-/
 lemma TensorProduct.eq_repr_basis_right :
-    ∃ (b : ιN →₀ M), (b.sum fun i m => m ⊗ₜ 𝒞 i) = x := by
+    ∃ (b : κ →₀ M), (b.sum fun i m => m ⊗ₜ 𝒞 i) = x := by
   classical
   induction x using TensorProduct.induction_on with
   | zero => exact ⟨0, by simp⟩
@@ -93,70 +95,13 @@ lemma TensorProduct.eq_repr_basis_right :
 
 /-- Elements in M ⊗ N can be represented by sum of elements of basis of M tensor elements of N.-/
 lemma TensorProduct.eq_repr_basis_left :
-    ∃ (c : ιM →₀ N), (c.sum fun i n => ℬ i ⊗ₜ n) = x := by
+    ∃ (c : ι →₀ N), (c.sum fun i n => ℬ i ⊗ₜ n) = x := by
   obtain ⟨c, hc⟩ := TensorProduct.eq_repr_basis_right ℬ (TensorProduct.comm R M N x)
   refine ⟨c, ?_⟩
   apply_fun TensorProduct.comm R M N using (TensorProduct.comm R M N).injective
   simp only [Finsupp.sum, map_sum, comm_tmul, ← hc]
 
-include ℬ in
-lemma TensorProduct.sum_tmul_basis_right_eq_zero
-    (b : ιN →₀ M) (h : (b.sum fun i m => m ⊗ₜ[R] 𝒞 i) = 0) :
-    b = 0 := by
-  classical
-  let 𝒯 := Basis.tensorProduct ℬ 𝒞
-  let I := b.support.biUnion fun i => (ℬ.repr (b i)).support
-  have eq1 := calc (0 : M ⊗[R] N)
-      _ = ∑ i ∈ b.support, b i ⊗ₜ[R] 𝒞 i := h.symm
-      _ = ∑ i ∈ b.support, (∑ k ∈ (ℬ.repr (b i)).support, (ℬ.repr (b i)) k • ℬ k) ⊗ₜ[R] 𝒞 i := by
-          refine Finset.sum_congr rfl fun z _ => ?_
-          congr
-          exact ℬ.linearCombination_repr (b z) |>.symm
-      _ = ∑ i ∈ b.support, ∑ k ∈ (ℬ.repr (b i)).support, (ℬ.repr (b i)) k • (ℬ k ⊗ₜ[R] 𝒞 i) := by
-          refine Finset.sum_congr rfl fun z _ => ?_
-          rw [TensorProduct.sum_tmul]
-          refine Finset.sum_congr rfl fun _ _ => ?_
-          rw [TensorProduct.smul_tmul']
-      _ = ∑ i ∈ b.support, ∑ k ∈ I, (ℬ.repr (b i)) k • (ℬ k ⊗ₜ[R] 𝒞 i) := by
-          refine Finset.sum_congr rfl fun j h => ?_
-          apply Finset.sum_subset
-          · intro i hi
-            simp only [Finsupp.mem_support_iff, ne_eq, Finset.mem_biUnion, I] at h hi ⊢
-            exact ⟨_, h, hi⟩
-          · intro i hi1 hi2
-            simp only [Finsupp.mem_support_iff, ne_eq, Decidable.not_not, smul_eq_zero]
-              at hi1 hi2 ⊢
-            simp only [hi2, zero_smul]
-      _ = ∑ k ∈ I, ∑ i ∈ b.support, (ℬ.repr (b i)) k • (ℬ k ⊗ₜ[R] 𝒞 i) := Finset.sum_comm
-      _ = ∑ ij ∈ I ×ˢ b.support, (ℬ.repr (b ij.2)) ij.1 • (ℬ ij.1 ⊗ₜ[R] 𝒞 ij.2) := by
-          rw [Finset.sum_product]
-      _ = ∑ ij ∈ I ×ˢ b.support, (ℬ.repr (b ij.2)) ij.1 • 𝒯 ij := by
-          refine Finset.sum_congr rfl fun ij _ => ?_
-          rw [Basis.tensorProduct_apply]
-  have LI := 𝒯.linearIndependent
-  rw [linearIndependent_iff'] at LI
-  specialize LI (I ×ˢ b.support) _ eq1.symm
-  ext i
-  by_cases hi : i ∈ b.support
-  swap
-  · simp only [Finsupp.mem_support_iff, ne_eq, Decidable.not_not] at hi; exact hi
-  rw [← ℬ.linearCombination_repr (b i)]
-  change ∑ _ ∈ _, _ = 0
-  simp only [LinearMap.coe_smulRight, LinearMap.id_coe, id_eq]
-  refine Finset.sum_eq_zero fun j hj => ?_
-  specialize LI ⟨j, i⟩ (by
-    simp only [Finset.mem_product, Finset.mem_biUnion, Finsupp.mem_support_iff, ne_eq, I] at hi hj ⊢
-    refine ⟨⟨_, hi, hj⟩, hi⟩)
-  simp [LI]
-
-include 𝒞 in
-lemma TensorProduct.sum_tmul_basis_left_eq_zero
-    (c : ιM →₀ N) (h : (c.sum fun i n => ℬ i ⊗ₜ[R] n) = 0) :
-    c = 0 := by
-  refine TensorProduct.sum_tmul_basis_right_eq_zero 𝒞 ℬ c ?_
-  apply_fun TensorProduct.comm R M N at h
-  simp only [Finsupp.sum, map_sum, comm_tmul, map_zero] at h
-  exact h
+end
 
 end CommSemiring
 
