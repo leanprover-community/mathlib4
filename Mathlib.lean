22f4ebccb240c5011416e70d83f31aa79ce824d6
Yaël Dillies <yael.dillies@gmail.com>
2024-10-17 09:45:36 +0000

    feat: family of kernels consistent with respect to a filtration From GibbsMeasure
    Co-authored-by: Kalle Kytölä <kalle.kytola@aalto.fi>

--- Mathlib.lean
+++ Mathlib.lean
@@ -3849,6 +3849,7 @@ import Mathlib.Probability.ProbabilityMassFunction.Constructions
 import Mathlib.Probability.ProbabilityMassFunction.Integrals
 import Mathlib.Probability.ProbabilityMassFunction.Monad
 import Mathlib.Probability.Process.Adapted
+import Mathlib.Probability.Process.Consistent
 import Mathlib.Probability.Process.Filtration
 import Mathlib.Probability.Process.HittingTime
 import Mathlib.Probability.Process.PartitionFiltration
---
+++ Mathlib/Probability/Process/Consistent.lean
@@ -0,0 +1,23 @@
+/-
+Copyright (c) 2024 Yaël Dillies, Kin Yau James Wong. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Yaël Dillies, Kin Yau James Wong
+-/
+import Mathlib.Probability.Kernel.Composition
+import Mathlib.Probability.Process.Filtration
+
+/-!
+# Family of kernels consistent with respect to a filtration
+-/
+
+open ProbabilityTheory Set MeasureTheory ENNReal NNReal
+
+namespace MeasureTheory.Filtration
+variable {X P E : Type*} {mX : MeasurableSpace X} {mE : MeasurableSpace E} [PartialOrder P]
+
+/-- A family of kernels `γ` on `X` indexed by a partial order `P` is consistent under conditioning
+if `γ p₂ ∘ₖ γ p₁ = γ p₁` whenever `p₁ ≤ p₂`. -/
+def IsConsistentKernel (mXs : Filtration P mX) (γ : ∀ p, Kernel[mXs p] X X) : Prop :=
+  ∀ ⦃p₁ p₂⦄, p₁ ≤ p₂ → (γ p₂).comap id (mXs.le p₂) ∘ₖ γ p₁ = γ p₁
+
+end MeasureTheory.Filtration
