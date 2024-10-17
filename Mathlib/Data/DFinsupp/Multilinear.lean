dab3add79ba7047329f534cb9813d4a3c34d2bf2
Eric Wieser <wieser.eric@gmail.com>
2024-10-17 23:27:10 +0000

    multilinearity

--- Mathlib/Data/DFinsupp/Multilinear.lean
+++ Mathlib/Data/DFinsupp/Multilinear.lean
@@ -8,6 +8,8 @@ import Mathlib.Data.DFinsupp.Basic
 
 /-!
 # Interactions between finitely-supported functions and multilinear maps
+
+This file provides `DFinsupp.piMultilinear`.
 -/
 
 universe uι uκ uR uM uN
@@ -20,6 +22,15 @@ variable [∀ p, AddCommMonoid (N p)] [∀ p, Module R (N p)]
 
 namespace DFinsupp
 
+omit [Fintype ι] in
+/-- This lemma is sufficiently impossible to name that it's easier to just keep it private. -/
+private theorem update_aux (m : (i : ι) → Π₀ (j : κ i), M i j)
+    (i : ι) (p : (i : ι) → κ i) (x : Π₀ (j : κ i), M i j) :
+    (fun k ↦ Function.update m i x k (p k)) =
+      Function.update (fun i ↦ (m i) (p i)) i (x (p i)) := by
+  ext j
+  obtain rfl | hij := eq_or_ne j i <;> simp [*]
+
 /--
 Given a family of indices `κ` and a multilinear map `f p` for each way `p` to select one index from
 each family, map a family of finitely-supported functions `x` into a finitely-supported function
@@ -28,23 +39,33 @@ from each selection.
 Strictly this doesn't need multilinearity, only the fact that `f p m = 0` whenever `m i = 0` for
 some `i`.
 -/
-@[simps toFun]
+@[simps]
 def piMultilinear
-    (f : Π (p : Π i, κ i), MultilinearMap R (fun i ↦ M i (p i)) (N p))
-    (x : ∀ i, Π₀ j : κ i, M i j) :
-    Π₀ t : Π i, κ i, N t where
-  toFun p := f p (fun i => x _ _)
-  support' := (Quotient.finChoice (S := _) fun i => (x i).support').recOnSubsingleton fun s =>
-    Trunc.mk ⟨
-      Finset.univ.val.pi (fun i ↦ (s i).val) |>.map fun f i => f i (Finset.mem_univ _),
-      fun p => by
-        simp only [Multiset.mem_map, Multiset.mem_pi, Finset.mem_val,
-          Finset.mem_univ, forall_true_left]
-        simp_rw [or_iff_not_imp_right]
-        intro h
-        push_neg at h
-        refine ⟨fun i _ => p i, fun i => (s i).prop _ |>.resolve_right ?_, rfl⟩
-        exact mt ((f p).map_coord_zero (m := fun i => x i _) i) h⟩
+    (f : Π (p : Π i, κ i), MultilinearMap R (fun i ↦ M i (p i)) (N p)) :
+    MultilinearMap R (fun i => Π₀ j : κ i, M i j) (Π₀ t : Π i, κ i, N t) where
+  toFun x :=
+  { toFun := fun p => f p (fun i => x _ _)
+    support' := (Quotient.finChoice (S := _) fun i => (x i).support').recOnSubsingleton fun s =>
+      Trunc.mk ⟨
+        Finset.univ.val.pi (fun i ↦ (s i).val) |>.map fun f i => f i (Finset.mem_univ _),
+        fun p => by
+          simp only [Multiset.mem_map, Multiset.mem_pi, Finset.mem_val,
+            Finset.mem_univ, forall_true_left]
+          simp_rw [or_iff_not_imp_right]
+          intro h
+          push_neg at h
+          refine ⟨fun i _ => p i, fun i => (s i).prop _ |>.resolve_right ?_, rfl⟩
+          exact mt ((f p).map_coord_zero (m := fun i => x i _) i) h⟩}
+  map_add' {inst} m i x y := by
+    cases Subsingleton.elim inst (by clear inst; assumption)
+    ext p
+    dsimp
+    simp_rw [update_aux, DFinsupp.add_apply, (f p).map_add]
+  map_smul' {inst} m i c x := by
+    cases Subsingleton.elim inst (by clear inst; assumption)
+    ext p
+    dsimp
+    simp_rw [update_aux, DFinsupp.smul_apply, (f p).map_smul]
 
 theorem support_piMultilinear_subset
     [∀ i, DecidableEq (κ i)]
@@ -53,10 +74,13 @@ theorem support_piMultilinear_subset
     (x : ∀ i, Π₀ j : κ i, M i j) :
     (piMultilinear f x).support ⊆ Fintype.piFinset fun i => (x i).support := by
   intro p hp
-  simp only [mem_support_toFun, piMultilinear_toFun, ne_eq, Fintype.mem_piFinset] at hp ⊢
+  simp only [mem_support_toFun, piMultilinear_apply_toFun, ne_eq, Fintype.mem_piFinset] at hp ⊢
   intro i
   exact mt ((f p).map_coord_zero (m := fun i => x i _) i) hp
 
+/-- When applied to a family of finitely-supported functions each supported on a single element,
+`piMultilinear` is itself supported on a single element, with value equal to the map `f` applied
+at that point. -/
 @[simp]
 theorem piMultilinear_single
     [∀ i, DecidableEq (κ i)]
@@ -64,13 +88,12 @@ theorem piMultilinear_single
     (p : ∀ i, κ i) (m : ∀ i, M i (p i)) :
     piMultilinear f (fun i => .single (p i) (m i)) = single p (f p m) := by
   ext q
-  simp [piMultilinear_toFun]
   obtain rfl | hpq := eq_or_ne p q
   · simp
-  · rw [dif_neg hpq]
+  · rw [single_eq_of_ne hpq]
     rw [Function.ne_iff] at hpq
-    obtain ⟨i, h⟩ := hpq
+    obtain ⟨i, hpqi⟩ := hpq
     apply (f q).map_coord_zero i
-    rw [dif_neg h]
+    simp_rw [single_eq_of_ne hpqi]
 
 end DFinsupp
