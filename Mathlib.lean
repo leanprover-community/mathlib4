a1bee4705402902a485a5f1d672c6d07a1ec3ce6
Violeta Hernández <vi.hdz.p@gmail.com>
2024-10-17 00:52:42 -0600

    fix

--- Mathlib.lean
+++ Mathlib.lean
@@ -4839,6 +4839,7 @@ import Mathlib.Topology.PartitionOfUnity
 import Mathlib.Topology.Perfect
 import Mathlib.Topology.PreorderRestrict
 import Mathlib.Topology.QuasiSeparated
+import Mathlib.Topology.Relative
 import Mathlib.Topology.RestrictGen
 import Mathlib.Topology.Semicontinuous
 import Mathlib.Topology.SeparatedMap
