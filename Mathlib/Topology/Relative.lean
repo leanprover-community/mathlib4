8a1309d1774eea29844b43dd6112894dfcdedf9f
Violeta Hernández <vi.hdz.p@gmail.com>
2024-10-17 01:14:46 -0600

    Update Relative.lean

--- Mathlib/Topology/Relative.lean
+++ Mathlib/Topology/Relative.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Violeta Hernández Palacios
 -/
 import Mathlib.Data.Set.Subset
-import Mathlib.Topology.Constructions
+import Mathlib.Topology.Order
 
 /-!
 # Relative open/closed sets
