b70b91dbe1950f2dc340235cb592176354280e55
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2024-10-17 19:36:05 +0200

    restore Index

--- Mathlib/GroupTheory/Index.lean
+++ Mathlib/GroupTheory/Index.lean
@@ -3,8 +3,9 @@ Copyright (c) 2021 Thomas Browning. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Thomas Browning
 -/
-import Mathlib.Data.Set.Card
+import Mathlib.Algebra.BigOperators.GroupWithZero.Finset
 import Mathlib.Data.Finite.Card
+import Mathlib.Data.Set.Card
 import Mathlib.GroupTheory.Coset.Card
 import Mathlib.GroupTheory.Finiteness
 import Mathlib.GroupTheory.GroupAction.Quotient
