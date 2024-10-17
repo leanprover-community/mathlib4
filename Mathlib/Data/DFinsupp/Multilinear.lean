35abe9a0da75eb5f0a44686b4db1d842889dd0e1
Eric Wieser <wieser.eric@gmail.com>
2024-10-17 23:31:24 +0000

    Sophie did the work of working out the necessary type here, and actually our helper lemmas are very similar

--- Mathlib/Data/DFinsupp/Multilinear.lean
+++ Mathlib/Data/DFinsupp/Multilinear.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2024 Eric Wieser. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Eric Wieser
+Authors: Eric Wieser, Sophie Morel
 -/
 import Mathlib.LinearAlgebra.Multilinear.Basic
 import Mathlib.Data.DFinsupp.Basic
