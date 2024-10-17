8b513aeb069b409beb9cf0e29bb494ec32204e71
jjdishere <emailboxofjjd@163.com>
2024-10-17 02:22:37 +0200

    remove all #6045

--- Mathlib/Geometry/RingedSpace/LocallyRingedSpace/ResidueField.lean
+++ Mathlib/Geometry/RingedSpace/LocallyRingedSpace/ResidueField.lean
@@ -79,14 +79,6 @@ lemma Γevaluation_ne_zero_iff_mem_basicOpen (x : X) (f : X.presheaf.obj (op ⊤
 
 variable {X Y : LocallyRingedSpace.{u}} (f : X ⟶ Y) (x : X)
 
--- TODO: This instance is found before #6045.
--- We need this strange instance for `residueFieldMap`, the type of `F` must be fixed
--- like this. The instance `IsLocalHom (f.stalkMap x)` already exists, but does not work for
--- `residueFieldMap`.
-instance : IsLocalHom (F := Y.presheaf.stalk (f.base x) →+* X.presheaf.stalk x)
-    (f.stalkMap x) :=
-  f.2 x
-
 /-- If `X ⟶ Y` is a morphism of locally ringed spaces and `x` a point of `X`, we obtain
 a morphism of residue fields in the other direction. -/
 def residueFieldMap (x : X) : Y.residueField (f.base x) ⟶ X.residueField x :=
@@ -109,8 +101,6 @@ lemma residueFieldMap_comp {Z : LocallyRingedSpace.{u}} (g : Y ⟶ Z) (x : X) :
   simp only [comp_toShHom, SheafedSpace.comp_base, Function.comp_apply, residueFieldMap]
   simp_rw [stalkMap_comp]
   haveI : IsLocalHom (g.stalkMap (f.base x)) := inferInstance
-  -- TODO: This instance is found before #6045.
-  haveI : IsLocalHom (f.stalkMap x) := inferInstance
   apply LocalRing.ResidueField.map_comp
 
 @[reassoc]
