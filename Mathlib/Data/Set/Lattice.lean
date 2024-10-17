46c079e4684d208aaa54dd648fe59a2cb9714956
Hannah Scholz <hannah@scholzbande.de>
2024-10-17 10:34:15 +0200

    fixed spacing

--- Mathlib/Data/Set/Lattice.lean
+++ Mathlib/Data/Set/Lattice.lean
@@ -1174,28 +1174,28 @@ theorem union_distrib_iInter₂_right (s : ∀ i, κ i → Set α) (t : Set α) 
     (⋂ (i) (j), s i j) ∪ t = ⋂ (i) (j), s i j ∪ t := by simp_rw [union_distrib_iInter_right]
 
 lemma biUnion_lt_eq_iUnion [LT α] [NoMaxOrder α] {s : α → Set β} :
-    ⋃ (n) (m < n), s m  = ⋃ n, s n := biSup_lt_eq_iSup
+    ⋃ (n) (m < n), s m = ⋃ n, s n := biSup_lt_eq_iSup
 
 lemma biUnion_le_eq_iUnion [Preorder α] {s : α → Set β} :
-    ⋃ (n) (m ≤ n), s m  = ⋃ n, s n := biSup_le_eq_iSup
+    ⋃ (n) (m ≤ n), s m = ⋃ n, s n := biSup_le_eq_iSup
 
 lemma biInter_lt_eq_iInter [LT α] [NoMaxOrder α] {s : α → Set β} :
-    ⋂ (n) (m < n), s m  = ⋂ (n), s n := biInf_lt_eq_iInf
+    ⋂ (n) (m < n), s m = ⋂ (n), s n := biInf_lt_eq_iInf
 
 lemma biInter_le_eq_iInter [Preorder α] {s : α → Set β} :
-    ⋂ (n) (m ≤ n), s m  = ⋂ (n), s n := biInf_le_eq_iInf
+    ⋂ (n) (m ≤ n), s m = ⋂ (n), s n := biInf_le_eq_iInf
 
 lemma biUnion_gt_eq_iUnion [LT α] [NoMinOrder α] {s : α → Set β} :
-    ⋃ (n) (m > n), s m  = ⋃ n, s n := biSup_gt_eq_iSup
+    ⋃ (n) (m > n), s m = ⋃ n, s n := biSup_gt_eq_iSup
 
 lemma biUnion_ge_eq_iUnion [Preorder α] {s : α → Set β} :
-    ⋃ (n) (m ≥ n), s m  = ⋃ n, s n := biSup_ge_eq_iSup
+    ⋃ (n) (m ≥ n), s m = ⋃ n, s n := biSup_ge_eq_iSup
 
 lemma biInter_gt_eq_iInf [LT α] [NoMinOrder α] {s : α → Set β} :
-    ⋂ (n) (m > n), s m  = ⋂ n, s n := biInf_gt_eq_iInf
+    ⋂ (n) (m > n), s m = ⋂ n, s n := biInf_gt_eq_iInf
 
 lemma biInter_ge_eq_iInf [Preorder α] {s : α → Set β} :
-    ⋂ (n) (m ≥ n), s m  = ⋂ n, s n := biInf_ge_eq_iInf
+    ⋂ (n) (m ≥ n), s m = ⋂ n, s n := biInf_ge_eq_iInf
 
 section Function
 
