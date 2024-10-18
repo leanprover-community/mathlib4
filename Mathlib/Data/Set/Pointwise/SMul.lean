757bf19dd130d71d01edc2a0d5aae4d48a8f3e33
Yaël Dillies <yael.dillies@gmail.com>
2024-10-18 12:00:47 +0000

    also generalise to Sort*

--- Mathlib/Data/Set/Pointwise/SMul.lean
+++ Mathlib/Data/Set/Pointwise/SMul.lean
@@ -330,7 +330,7 @@ theorem smul_set_inter : a • (s ∩ t) = a • s ∩ a • t :=
   image_inter <| MulAction.injective a
 
 @[to_additive]
-theorem smul_set_iInter {ι : Type*}
+theorem smul_set_iInter {ι : Sort*}
     (a : α) (t : ι → Set β) : (a • ⋂ i, t i) = ⋂ i, a • t i :=
   image_iInter (MulAction.bijective a) t
 
