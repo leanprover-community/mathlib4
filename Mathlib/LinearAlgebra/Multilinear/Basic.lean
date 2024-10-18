0f6eedf1aaec6dedcca49ca5696d20b829a7f63d
Anne C.A. Baanen <vierkantor@vierkantor.com>
2024-10-18 14:07:13 +0200

    chore(Algebra/Module): split off `NoZeroSMulDivisors`
    The definition of `NoZeroSMulDivisors` doesn't require us to know about modules. By splitting off this definition, we can streamline the downstream a bit.
    
    In the process, generalize a few instances from `Module` to `SMulWithZero`.
    
    New files:
     * `NoZeroSMulDivisors/Defs.lean`: definition and basic results that don't require `Module`
     * `NoZeroSMulDivisors/Basic.lean`: basic results that require `Module`
     * `NoZeroSMulDivisors/Pi.lean`: results on `Pi` that don't require `Module`

--- Mathlib/LinearAlgebra/Multilinear/Basic.lean
+++ Mathlib/LinearAlgebra/Multilinear/Basic.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Sébastien Gouëzel
 -/
 import Mathlib.Algebra.Algebra.Defs
+import Mathlib.Algebra.NoZeroSMulDivisors.Pi
 import Mathlib.Algebra.Order.BigOperators.Group.Finset
 import Mathlib.Data.Fintype.BigOperators
 import Mathlib.Data.Fintype.Sort
