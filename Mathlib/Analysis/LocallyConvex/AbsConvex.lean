db4e02554c0cdc839c8a96ad777f93b75e097fec
Christopher Hoskin <christopher.hoskin@gmail.com>
2024-10-18 21:02:18 +0100

    Golf

--- Mathlib/Analysis/LocallyConvex/AbsConvex.lean
+++ Mathlib/Analysis/LocallyConvex/AbsConvex.lean
@@ -196,14 +196,9 @@ variable (𝕜) [NontriviallyNormedField 𝕜]
 variable [AddCommGroup E] [Module ℝ E] [Module 𝕜 E]
 
 theorem convexHull_add_subset {s t : Set E} :
-    convexHull ℝ (s + t) ⊆ convexHull ℝ s + convexHull ℝ t := by
-  apply convexHull_min
-  apply add_subset_add
-  apply subset_convexHull
-  apply subset_convexHull
-  apply Convex.add
-  exact convex_convexHull ℝ s
-  exact convex_convexHull ℝ t
+    convexHull ℝ (s + t) ⊆ convexHull ℝ s + convexHull ℝ t :=
+  convexHull_min (add_subset_add (subset_convexHull _ _) (subset_convexHull _ _))
+    (Convex.add (convex_convexHull ℝ s) (convex_convexHull ℝ t))
 
 theorem absConvexHull_add_subset {s t : Set E} :
     absConvexHull 𝕜 (s + t) ⊆ absConvexHull 𝕜 s + absConvexHull 𝕜 t :=
