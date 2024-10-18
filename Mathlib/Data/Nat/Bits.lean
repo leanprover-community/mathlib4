4cc0f9118ce6ecfb99913cdc8d90db18ee4ba14b
Eric Wieser <wieser.eric@gmail.com>
2024-10-18 18:50:01 +0000

    one more

--- Mathlib/Data/Nat/Bits.lean
+++ Mathlib/Data/Nat/Bits.lean
@@ -400,7 +400,7 @@ theorem zero_bits : bits 0 = [] := by simp [Nat.bits]
 theorem bits_append_bit (n : ℕ) (b : Bool) (hn : n = 0 → b = true) :
     (bit b n).bits = b :: n.bits := by
   rw [Nat.bits, Nat.bits, binaryRec_eq']
-  · simpa
+  simpa
 
 @[simp]
 theorem bit0_bits (n : ℕ) (hn : n ≠ 0) : (2 * n).bits = false :: n.bits :=
