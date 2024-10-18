591e9fb8787d1f9a118a0eb6d4ea71150982308a
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2024-10-18 17:19:34 +0200

    fix

--- Mathlib/Geometry/Manifold/ContMDiff/Defs.lean
+++ Mathlib/Geometry/Manifold/ContMDiff/Defs.lean
@@ -109,7 +109,7 @@ theorem contDiffWithinAt_localInvariantProp (n : ℕ∞) :
     rw [this] at h
     have : I (e x) ∈ I.symm ⁻¹' e.target ∩ range I := by simp only [hx, mfld_simps]
     have := (mem_groupoid_of_pregroupoid.2 he).2.contDiffWithinAt this
-    convert (h.comp' _ (this.of_le le_top)).mono_of_mem _ using 1
+    convert (h.comp_inter _ (this.of_le le_top)).mono_of_mem _ using 1
     · ext y; simp only [mfld_simps]
     refine mem_nhdsWithin.mpr
       ⟨I.symm ⁻¹' e.target, e.open_target.preimage I.continuous_symm, by
