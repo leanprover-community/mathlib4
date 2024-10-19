0d932abadf1d7aad0fd9efbae434ff5e6fa24667
Daniel Weber <commandmaster2017@gmail.com>
2024-10-19 14:07:56 +0000

    feat: define rooted trees (#16272)
    This is preparation for proving Kruskal's tree theorem.
    
    
    
    Co-authored-by: Daniel Weber <55664973+Command-Master@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -3797,6 +3797,7 @@ import Mathlib.Order.SuccPred.IntervalSucc
 import Mathlib.Order.SuccPred.Limit
 import Mathlib.Order.SuccPred.LinearLocallyFinite
 import Mathlib.Order.SuccPred.Relation
+import Mathlib.Order.SuccPred.Tree
 import Mathlib.Order.SupClosed
 import Mathlib.Order.SupIndep
 import Mathlib.Order.SymmDiff
