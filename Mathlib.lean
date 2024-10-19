64791591eee9e1525c1e0be785ee2ebac080f5d7
Junyan Xu <junyanxu.math@gmail.com>
2024-10-19 23:42:21 +0000

    Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -4210,6 +4210,7 @@ import Mathlib.SetTheory.Cardinal.Continuum
 import Mathlib.SetTheory.Cardinal.CountableCover
 import Mathlib.SetTheory.Cardinal.Divisibility
 import Mathlib.SetTheory.Cardinal.ENat
+import Mathlib.SetTheory.Cardinal.FieldEmb
 import Mathlib.SetTheory.Cardinal.Finite
 import Mathlib.SetTheory.Cardinal.Finsupp
 import Mathlib.SetTheory.Cardinal.PartENat
