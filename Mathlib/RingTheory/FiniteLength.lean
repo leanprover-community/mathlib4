822520397e6f5220f10314c47a47d31d4ef417ca
Junyan Xu <junyanxu.math@gmail.com>
2024-10-19 16:49:38 -0400

    trailing space

--- Mathlib/RingTheory/FiniteLength.lean
+++ Mathlib/RingTheory/FiniteLength.lean
@@ -98,7 +98,7 @@ theorem IsSemisimpleModule.finite_tfae [IsSemisimpleModule R M] :
 instance [IsSemisimpleModule R M] [Module.Finite R M] : IsArtinian R M :=
   (IsSemisimpleModule.finite_tfae.out 0 2).mp ‹_›
 
-/- The following instances are now automatic: 
+/- The following instances are now automatic:
 example [IsSemisimpleRing R] : IsNoetherianRing R := inferInstance
 example [IsSemisimpleRing R] : IsArtinianRing R := inferInstance
 -/
