38e79332e88b5b70367d4d54fcfef4cbfcaf3d51
Andrew Yang <36414270+erdOne@users.noreply.github.com>
2024-10-19 18:24:41 +0100

    Update SnakeLemma.lean

--- Mathlib/Algebra/Module/SnakeLemma.lean
+++ Mathlib/Algebra/Module/SnakeLemma.lean
@@ -4,8 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Andrew Yang
 -/
 import Mathlib.Algebra.Exact
-import Mathlib.LinearAlgebra.TensorProduct.RightExactness
-import Mathlib.LinearAlgebra.Quotient
 
 /-!
 
