8d91e67e254b648d8c1c3c0a743b17c3db087317
FR <zhao.yu-yang@foxmail.com>
2024-10-20 05:31:59 +0800

    `Authors`

--- Mathlib/Data/Nat/BinaryRec.lean
+++ Mathlib/Data/Nat/BinaryRec.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2017 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Author: Mario Carneiro, Praneeth Kolichala, Yuyang Zhao
+Authors: Mario Carneiro, Praneeth Kolichala, Yuyang Zhao
 -/
 
 /-!
