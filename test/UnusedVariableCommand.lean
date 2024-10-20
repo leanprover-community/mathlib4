db3d1164b201cf13ead93439b6917fa2fc4864a5
adomani <adomani@gmail.com>
2024-10-21 00:40:10 +0100

    add test with declId with universe level

--- test/UnusedVariableCommand.lean
+++ test/UnusedVariableCommand.lean
@@ -1,4 +1,6 @@
 import Mathlib.Tactic.Linter.UnusedVariableCommand
+import Mathlib.adomaniLeanUtils.inspect_syntax
+import Lean.Elab.Syntax
 
 open Lean Elab Command Mathlib.Linter.UnusedVariableCommand in
 elab "mkt " cmd:command : command => do
@@ -28,3 +30,55 @@ mkt
 structure D extends Add Nat, Mul Int where mk'::
   a : Nat
   b : Int
+#check Lean.Elab.Command.elabSyntax
+section
+set_option pp.rawOnError true
+open Lean Elab Command
+universe u
+run_cmd
+  let stx ← `(command| def $(mkIdent `A).{u} := 0)
+  --let elS ← elabSyntax stx
+  let did := (stx.raw.find? (·.isOfKind ``Parser.Command.declId)).getD default
+  let prt := did.prettyPrint.pretty
+  if "A" == prt then
+    logInfo "Equal"
+  else
+    let simplStx ← stx.raw.replaceM fun s => do
+      if s.isOfKind ``Parser.Command.declId then
+        `(declId|$(mkIdentFrom s s[0].getId))
+      else return none
+    logWarning m!"Different {prt} {did[0]}\n{simplStx}"
+#check Parser.Term.explicitUniv
+--inspect
+variable {R : Type u} [Zero.{u} R]
+set_option showDefs true in
+example : (0 : R) = 0 := rfl
+
+/-
+inspect: 'variable [Zero.{u} R]'
+
+|   |   |-node Lean.Parser.Term.app, none
+|   |   |   |-node Lean.Parser.Term.explicitUniv, none
+|   |   |   |   |-ident original: ⟨⟩⟨⟩-- (Zero,Zero)
+|   |   |   |   |-atom original: ⟨⟩⟨⟩-- '.{'
+|   |   |   |   |-node null, none
+|   |   |   |   |   |-ident original: ⟨⟩⟨⟩-- (u,u)
+|   |   |   |   |-atom original: ⟨⟩⟨ ⟩-- '}'
+|   |   |   |-node null, none
+|   |   |   |   |-ident original: ⟨⟩⟨⟩-- (R,R)
+|   |   |-atom original: ⟨⟩⟨⏎⏎⏎⟩-- ']'
+-/
+/-
+inspect: 'variable [Zero R]'
+
+|   |   |-node Lean.Parser.Term.app, none
+|   |   |   |-ident original: ⟨⟩⟨ ⟩-- (Zero,Zero)
+|   |   |   |-node null, none
+|   |   |   |   |-ident original: ⟨⟩⟨⟩-- (R,R)
+|   |   |-atom original: ⟨⟩⟨⏎⏎⏎⟩-- ']'
+-/
+inspect
+variable [Zero R]
+
+
+end
