c2510f6f8b63eae11288644c0c6dee0b6c10875c
Anne C.A. Baanen <vierkantor@vierkantor.com>
2024-10-21 16:27:16 +0200

    chore(Algebra/PUnitInstances): split off `GCDMonoid PUnit` instance
    This does not seem to be used anywhere in Mathlib and splitting it off should allow us to shave quite a few imports from low down in the hierarchy.

--- Mathlib/Algebra/PUnitInstances/Algebra.lean
+++ Mathlib/Algebra/PUnitInstances/Algebra.lean
@@ -64,33 +64,4 @@ instance commRing : CommRing PUnit where
 
 instance cancelCommMonoidWithZero : CancelCommMonoidWithZero PUnit where
 
--- This is too high-powered and should be split off also
-instance normalizedGCDMonoid : NormalizedGCDMonoid PUnit where
-  gcd _ _ := unit
-  lcm _ _ := unit
-  normUnit _ := 1
-  normUnit_zero := rfl
-  normUnit_mul := by intros; rfl
-  normUnit_coe_units := by intros; rfl
-  gcd_dvd_left _ _ := ⟨unit, by subsingleton⟩
-  gcd_dvd_right _ _ := ⟨unit, by subsingleton⟩
-  dvd_gcd {_ _} _ _ _ := ⟨unit, by subsingleton⟩
-  gcd_mul_lcm _ _ := ⟨1, by subsingleton⟩
-  lcm_zero_left := by intros; rfl
-  lcm_zero_right := by intros; rfl
-  normalize_gcd := by intros; rfl
-  normalize_lcm := by intros; rfl
-
--- Porting note (#10618): simpNF lint: simp can prove this @[simp]
-theorem gcd_eq {x y : PUnit} : gcd x y = unit :=
-  rfl
-
--- Porting note (#10618): simpNF lint: simp can prove this @[simp]
-theorem lcm_eq {x y : PUnit} : lcm x y = unit :=
-  rfl
-
-@[simp]
-theorem norm_unit_eq {x : PUnit} : normUnit x = 1 :=
-  rfl
-
 end PUnit
