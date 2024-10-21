772785f0266edea45cf319fcf779c266367edd37
Junyan Xu <junyanxu.math@gmail.com>
2024-10-21 10:18:27 -0400

    shake

--- Mathlib/FieldTheory/Cardinal.lean
+++ Mathlib/FieldTheory/Cardinal.lean
@@ -3,7 +3,6 @@ Copyright (c) 2024 Junyan Xu. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Junyan Xu
 -/
-import Mathlib.FieldTheory.MvPolynomial
 import Mathlib.FieldTheory.SeparableClosure
 import Mathlib.FieldTheory.PurelyInseparable
 import Mathlib.LinearAlgebra.FreeAlgebra
