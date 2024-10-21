4182a22842432b786bdd47bc5f4db43026278b19
Salvatore Mercuri <smercuri@ed.ac.uk>
2024-10-21 11:40:51 +0100

    Correct Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -3514,7 +3514,6 @@ import Mathlib.NumberTheory.NumberField.CanonicalEmbedding.Basic
 import Mathlib.NumberTheory.NumberField.CanonicalEmbedding.ConvexBody
 import Mathlib.NumberTheory.NumberField.CanonicalEmbedding.FundamentalCone
 import Mathlib.NumberTheory.NumberField.ClassNumber
-import Mathlib.NumberTheory.NumberField.Completion
 import Mathlib.NumberTheory.NumberField.Discriminant
 import Mathlib.NumberTheory.NumberField.Embeddings
 import Mathlib.NumberTheory.NumberField.EquivReindex
