f738d2223ef0c3e25d6a5482c94afb6cf6191ffa
damiano <adomani@gmail.com>
2024-10-21 06:32:44 +0000

    feat: the multiGoal linter (#12339)
    A linter that makes sure that, when multiple goals are present, they are enclosed in `·`s.
    
    Adaptations (the order is chronological, there should be no dependency among them):
    * #12338,
    * #12361,
    * #12372,
    * #12560,
    * #12834,
    * #12381,
    * #12908,
    * #14939,
    
    plus many many more that this comment is too small to contain.
    
    
    
    Co-authored-by: Michael Rothgang <rothgami@math.hu-berlin.de>

--- Mathlib.lean
+++ Mathlib.lean
@@ -4398,6 +4398,7 @@ import Mathlib.Tactic.Linter.HaveLetLinter
 import Mathlib.Tactic.Linter.Header
 import Mathlib.Tactic.Linter.Lint
 import Mathlib.Tactic.Linter.MinImports
+import Mathlib.Tactic.Linter.Multigoal
 import Mathlib.Tactic.Linter.OldObtain
 import Mathlib.Tactic.Linter.PPRoundtrip
 import Mathlib.Tactic.Linter.RefineLinter
