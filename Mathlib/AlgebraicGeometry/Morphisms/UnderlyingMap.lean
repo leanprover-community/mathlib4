3b73b79a39d0d76df33c778733e51d4f28b29121
Andrew Yang <36414270+erdOne@users.noreply.github.com>
2024-10-21 16:25:35 +0100

    UnderlyingMap.lean を更新
    Co-authored-by: Johan Commelin <johan@commelin.net>

--- Mathlib/AlgebraicGeometry/Morphisms/UnderlyingMap.lean
+++ Mathlib/AlgebraicGeometry/Morphisms/UnderlyingMap.lean
@@ -154,7 +154,7 @@ lemma Scheme.Hom.denseRange (f : X.Hom Y) [IsDominant f] : DenseRange f.base :=
 instance (priority := 100) [Surjective f] : IsDominant f := ⟨f.surjective.denseRange⟩
 
 instance [IsDominant f] [IsDominant g] : IsDominant (f ≫ g) :=
-⟨g.denseRange.comp f.denseRange g.base.2⟩
+  ⟨g.denseRange.comp f.denseRange g.base.2⟩
 
 instance : MorphismProperty.IsMultiplicative @IsDominant where
   id_mem := fun _ ↦ inferInstance
