b3184a91682f8ec708b30c9058920bcb8d319c9d
Robin Carlier <robin.carlier@ens-lyon.fr>
2024-10-22 19:00:57 +0000

    feat(CategoryTheory/ChosenFiniteProducts): `prodComparison` and `terminalComparison` for `ChosenFiniteProducts` (#17701)
    Given a functor `F : C ⥤ D` between categories with chosen finite products and `A B : C`, introduce `terminalComparison F : F (𝟙_ C) ⟶ 𝟙_ D` and `prodComparison F A B : F (A ⊗ B) ⟶ F A ⊗ F B` the canonical maps.
    
    - Show that `terminalComparison` is an isomorphism if and only if `F` preserves terminal objects.
    - Show that the canonical map `prodComparison F A B` is natural in `A` and `B` and compatible with composition.
    - Show that `F` preserves binary products if and only if `prodComparison F A B` is an isomorphism for all `A B`.
    
    
    
    Co-authored-by: Robin Carlier <57142648+robin-carlier@users.noreply.github.com>

--- Mathlib/CategoryTheory/Limits/Shapes/BinaryProducts.lean
+++ Mathlib/CategoryTheory/Limits/Shapes/BinaryProducts.lean
@@ -196,6 +196,12 @@ theorem BinaryFan.π_app_left {X Y : C} (s : BinaryFan X Y) : s.π.app ⟨Walkin
 theorem BinaryFan.π_app_right {X Y : C} (s : BinaryFan X Y) : s.π.app ⟨WalkingPair.right⟩ = s.snd :=
   rfl
 
+/-- Constructs an isomorphism of `BinaryFan`s out of an isomorphism of the tips that commutes with
+the projections. -/
+def BinaryFan.ext {A B : C} {c c' : BinaryFan A B} (e : c.pt ≅ c'.pt)
+    (h₁ : c.fst = e.hom ≫ c'.fst) (h₂ : c.snd = e.hom ≫ c'.snd) : c ≅ c' :=
+  Cones.ext e (fun j => by rcases j with ⟨⟨⟩⟩ <;> assumption)
+
 /-- A convenient way to show that a binary fan is a limit. -/
 def BinaryFan.IsLimit.mk {X Y : C} (s : BinaryFan X Y)
     (lift : ∀ {T : C} (_ : T ⟶ X) (_ : T ⟶ Y), T ⟶ s.pt)
@@ -224,6 +230,12 @@ abbrev BinaryCofan.inl {X Y : C} (s : BinaryCofan X Y) := s.ι.app ⟨WalkingPai
 
 /-- The second inclusion of a binary cofan. -/
 abbrev BinaryCofan.inr {X Y : C} (s : BinaryCofan X Y) := s.ι.app ⟨WalkingPair.right⟩
+
+/-- Constructs an isomorphism of `BinaryCofan`s out of an isomorphism of the tips that commutes with
+the injections. -/
+def BinaryCofan.ext {A B : C} {c c' : BinaryCofan A B} (e : c.pt ≅ c'.pt)
+    (h₁ : c.inl ≫ e.hom = c'.inl) (h₂ : c.inr ≫ e.hom = c'.inr) : c ≅ c' :=
+  Cocones.ext e (fun j => by rcases j with ⟨⟨⟩⟩ <;> assumption)
 
 @[simp]
 theorem BinaryCofan.ι_app_left {X Y : C} (s : BinaryCofan X Y) :
