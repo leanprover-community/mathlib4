7618d2b9fbb893b56ca18826242a5453b4c82a17
Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>
2024-10-22 02:14:59 +0200

    Update Attr.lean

--- Mathlib/Tactic/Finiteness/Attr.lean
+++ Mathlib/Tactic/Finiteness/Attr.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2024 Floris van Doorn. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Floris van Doorn
+-/
 import Aesop
 
 declare_aesop_rule_sets [Finiteness]
