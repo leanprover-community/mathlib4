6b201622c163e4b550029e6ce101d3d303792046
Yoh Tanimoto <hoyt@jcom.home.ne.jp>
2024-10-22 11:51:46 +0200

    #min_imports

--- Mathlib/Topology/Algebra/SeparationQuotient/Basic.lean
+++ Mathlib/Topology/Algebra/SeparationQuotient/Basic.lean
@@ -3,12 +3,7 @@ Copyright (c) 2024 Yury Kudryashov. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yury Kudryashov
 -/
--- import Mathlib.Algebra.Module.Projective
--- import Mathlib.LinearAlgebra.Basis.VectorSpace
--- import Mathlib.Topology.Algebra.Module.Basic
--- import Mathlib.Topology.Algebra.ContinuousMonoidHom
--- import Mathlib.Topology.Maps.OpenQuotient
-import Mathlib.Topology.Algebra.Module.Basic
+import Mathlib.Topology.Algebra.ContinuousMonoidHom
 
 /-!
 # Algebraic operations on `SeparationQuotient`
