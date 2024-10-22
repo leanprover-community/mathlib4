963d4b923a2ed75c64b8bb542b363892b82b8df7
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2024-10-22 21:30:25 +0100

    Update Mathlib/LinearAlgebra/DFinsupp.lean
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- Mathlib/LinearAlgebra/DFinsupp.lean
+++ Mathlib/LinearAlgebra/DFinsupp.lean
@@ -11,7 +11,7 @@ import Mathlib.LinearAlgebra.LinearIndependent
 # Properties of the module `Π₀ i, M i`
 
 Given an indexed collection of `R`-modules `M i`, the `R`-module structure on `Π₀ i, M i`
-is defined in `Data.DFinsupp.Basic`.
+is defined in `Mathlib.Data.DFinsupp.Basic`.
 
 In this file we define `LinearMap` versions of various maps:
 
