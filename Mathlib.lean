1ece609f6ff5522cd5858de729b808e787a61695
Oliver Nash <github@olivernash.org>
2024-10-22 08:37:13 +0000

    feat: define finitely-semisimple linear maps (#17689)
    The motivation is to be able to apply `IsFinitelySemisimple.maxGenEigenspace_eq_eigenspace` to symmetric maps in infinite dimensions. Such maps are not in general semisimple but are semisimple on each invariant finite-dimensional subspace.

--- Mathlib.lean
+++ Mathlib.lean
@@ -502,6 +502,7 @@ import Mathlib.Algebra.Module.Submodule.Basic
 import Mathlib.Algebra.Module.Submodule.Bilinear
 import Mathlib.Algebra.Module.Submodule.EqLocus
 import Mathlib.Algebra.Module.Submodule.Equiv
+import Mathlib.Algebra.Module.Submodule.Invariant
 import Mathlib.Algebra.Module.Submodule.IterateMapComap
 import Mathlib.Algebra.Module.Submodule.Ker
 import Mathlib.Algebra.Module.Submodule.Lattice
