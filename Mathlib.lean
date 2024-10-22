63e71056d59a85802e5a4cce256976d283c566e3
tukamilano <milanotukamoto@g.ecc.u-tokyo.ac.jp>
2024-10-22 18:03:39 +0900

    feat: Hoeffding's lemma

--- Mathlib.lean
+++ Mathlib.lean
@@ -4180,6 +4180,7 @@ import Mathlib.Probability.Distributions.Geometric
 import Mathlib.Probability.Distributions.Pareto
 import Mathlib.Probability.Distributions.Poisson
 import Mathlib.Probability.Distributions.Uniform
+import Mathlib.Probability.Hoeffding
 import Mathlib.Probability.IdentDistrib
 import Mathlib.Probability.Independence.Basic
 import Mathlib.Probability.Independence.Conditional
