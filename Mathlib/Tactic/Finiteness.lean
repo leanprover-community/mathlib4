d5c387de78b0c9249ba13d62aa58004ab09d0a5e
Yaël Dillies <yael.dillies@gmail.com>
2024-10-22 20:28:41 +0000

    reduce imports to the tactic

--- Mathlib/Tactic/Finiteness.lean
+++ Mathlib/Tactic/Finiteness.lean
@@ -3,8 +3,7 @@ Copyright (c) 2023 Heather Macbeth. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Heather Macbeth
 -/
-import Mathlib.Tactic.Finiteness.Attr
-import Mathlib.MeasureTheory.Constructions.Prod.Basic
+import Mathlib.Tactic.Positivity.Core
 
 /-! # Finiteness tactic
 
@@ -18,30 +17,6 @@ set.
 a locally finite measure.
 -/
 
-open ENNReal
-
-/-! ## Lemmas -/
-
-private theorem ENNReal.ofNat_ne_top (n : ℕ) [Nat.AtLeastTwo n] : no_index (OfNat.ofNat n) ≠ ∞ :=
-  ENNReal.natCast_ne_top n
-
-private theorem ENNReal.inv_ne_top' {a} (h : a ≠ 0) : a⁻¹ ≠ ∞ := ENNReal.inv_ne_top.2 h
-
-private theorem ENNReal.add_ne_top' {a b : ℝ≥0∞} (ha : a ≠ ∞) (hb : b ≠ ∞) : a + b ≠ ∞ :=
-  ENNReal.add_ne_top.2 ⟨ha, hb⟩
-
-/-! ## Tactic implementation -/
-
-attribute [aesop (rule_sets := [Finiteness]) unsafe 20%] ne_top_of_lt
--- would have been better to implement this as a "safe" "forward" rule, why doesn't this work?
--- attribute [aesop (rule_sets [Finiteness]) safe forward] ne_top_of_lt
-
-attribute [aesop (rule_sets := [Finiteness]) safe apply]
-  Ne.lt_top
-  ENNReal.ofReal_ne_top ENNReal.coe_ne_top ENNReal.natCast_ne_top
-  ENNReal.zero_ne_top ENNReal.one_ne_top ENNReal.ofNat_ne_top
-  ENNReal.mul_ne_top ENNReal.add_ne_top' ENNReal.sub_ne_top ENNReal.inv_ne_top'
-  MeasureTheory.measure_ne_top
 
 open Aesop.BuiltinRules in
 attribute [aesop (rule_sets := [Finiteness]) safe -50] assumption intros
