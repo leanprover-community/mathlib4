599db0f58573aa5ef1cda8b37b75ff5977dc81fe
Violeta Hernández <vi.hdz.p@gmail.com>
2024-10-22 22:22:36 -0600

    fix

--- Mathlib/SetTheory/Cardinal/UnivLE.lean
+++ Mathlib/SetTheory/Cardinal/UnivLE.lean
@@ -37,6 +37,3 @@ theorem Ordinal.univLE_of_injective {f : Ordinal.{u} → Ordinal.{v}} (h : f.Inj
 /-- Together with transitivity, this shows UnivLE "IsTotalPreorder". -/
 theorem univLE_total : UnivLE.{u, v} ∨ UnivLE.{v, u} := by
   simp_rw [univLE_iff_cardinal_le]; apply le_total
-
-theorem Ordinal.univLE_of_injective {f : Ordinal.{u} → Ordinal.{v}} (h : f.Injective) :
-    UnivLE.{u, v} := univLE_iff_cardinal_le.mpr <| lift_mk_le'.mpr ⟨f, h⟩
