66e475793e8e5289bc865d09995e0b2edeecbb8b
Yaël Dillies <yael.dillies@gmail.com>
2024-10-23 09:48:25 +0000

    test file

---
+++ test/finiteness.lean
@@ -0,0 +1,15 @@
+import Mathlib.Data.ENNReal.Operations
+
+/-! ## Tests -/
+
+example : (1:ℝ≥0∞) < ∞ := by finiteness
+example : (3:ℝ≥0∞) ≠ ∞ := by finiteness
+
+example (a : ℝ) (b : ℕ) : ENNReal.ofReal a + b < ∞ := by finiteness
+
+example {a : ℝ≥0∞} (ha : a ≠ ∞) : a + 3 < ∞ := by finiteness
+example {a : ℝ≥0∞} (ha : a < ∞) : a + 3 < ∞ := by finiteness
+
+example (a : ℝ) : (ENNReal.ofReal (1 + a ^ 2))⁻¹ < ∞ := by finiteness
+
+example {α : Type*} (f : α → ℕ) : ∀ i, (f i : ℝ≥0∞) ≠ ∞ := by finiteness
