5455d6462d550d83fae43df511d7bd707e7fe80c
Hannah Fechtner <hfechtne@andrew.cmu.edu>
2024-10-23 13:34:04 -0400

    instead get rid of implicits to eq

--- Mathlib/GroupTheory/Congruence/Basic.lean
+++ Mathlib/GroupTheory/Congruence/Basic.lean
@@ -297,9 +297,10 @@ lemma comapQuotientEquivOfSurj_symm_mk (c : Con M) {f : N →* M} (hf : Function
 
 /-- This version infers the surjectivity of the function from a MulEquiv function -/
 @[simp]
-lemma comapQuotientEquivOfSurj_symm_mk' (c : Con M) (f : N ≃* M) (x : N) : @Eq
-    (Con.Quotient (comap ⇑f _ c))
-    ((comapQuotientEquivOfSurj c ↑f f.surjective).symm ⟦f x⟧) ↑x :=
+lemma comapQuotientEquivOfSurj_symm_mk' (c : Con M) (f : N ≃* M) (x : N) :
+    ((@MulEquiv.symm (Con.Quotient (comap ⇑f _ c)) _ (hasMul (comap ⇑f _ c))
+    _ (comapQuotientEquivOfSurj c ↑f f.surjective))
+    ⟦f x⟧) = ↑x :=
   (MulEquiv.symm_apply_eq (@comapQuotientEquivOfSurj M N _ _ c f _)).mpr rfl
 
 /-- The **second isomorphism theorem for monoids**. -/
