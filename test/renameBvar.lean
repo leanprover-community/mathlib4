376c500c4a9df2f04dc321c7de8ecc6982ed6c7e
Arend Mellendijk <arend.mellendijk@gmail.com>
2024-10-23 18:41:22 +0200

    add test

---
+++ test/renameBvar.lean
@@ -0,0 +1,34 @@
+import Mathlib.Tactic.RenameBvar
+
+/- This test is somewhat flaky since it depends on the pretty printer. -/
+/--
+info: ℕ : Sort u_1
+P : ℕ → ℕ → Prop
+h : ∀ (n : ℕ), ∃ m, P n m
+⊢ ∀ (l : ℕ), ∃ m, P l m
+---
+info: ℕ : Sort u_1
+P : ℕ → ℕ → Prop
+h : ∀ (q : ℕ), ∃ m, P q m
+⊢ ∀ (l : ℕ), ∃ m, P l m
+---
+info: ℕ : Sort u_1
+P : ℕ → ℕ → Prop
+h : ∀ (q : ℕ), ∃ m, P q m
+⊢ ∀ (l : ℕ), ∃ n, P l n
+---
+info: ℕ : Sort u_1
+P : ℕ → ℕ → Prop
+h : ∀ (q : ℕ), ∃ m, P q m
+⊢ ∀ (m : ℕ), ∃ n, P m n
+-/
+#guard_msgs in
+example (P : ℕ → ℕ → Prop) (h : ∀ n, ∃ m, P n m) : ∀ l, ∃ m, P l m := by
+  trace_state
+  rename_bvar n → q at h -- h is now ∀ (q : ℕ), ∃ (m : ℕ), P q m,
+  trace_state
+  rename_bvar m → n -- target is now ∀ (l : ℕ), ∃ (n : ℕ), P k n,
+  trace_state
+  rename_bvar l → m
+  trace_state
+  exact h -- Lean does not care about those bound variable names
