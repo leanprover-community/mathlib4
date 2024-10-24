3a05b6bd303975a62bec1d4c13aa012fee93fd56
Yoh Tanimoto <57562556+yoh-tanimoto@users.noreply.github.com>
2024-10-24 15:57:53 +0200

    #min_imports

--- Mathlib/Analysis/Normed/Group/SeparationQuotient.lean
+++ Mathlib/Analysis/Normed/Group/SeparationQuotient.lean
@@ -3,8 +3,7 @@ Copyright (c) 2024 Yoh Tanimoto. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yoh Tanimoto
 -/
-import Mathlib.Analysis.Normed.Field.Basic
-import Mathlib.Analysis.Normed.Group.Hom
+Mathlib.Analysis.Normed.Group.Uniform
 
 /-!
 # The null subgroup in a seminormed commutative group
