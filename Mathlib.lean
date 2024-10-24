0b4580e8d4b90716be319eac40c062d51104ca73
Joël Riou <joel.riou@universite-paris-saclay.fr>
2024-10-24 15:49:00 +0200

    feat(CategoryTheory): enriched ordinary categories

--- Mathlib.lean
+++ Mathlib.lean
@@ -1553,6 +1553,7 @@ import Mathlib.CategoryTheory.Elementwise
 import Mathlib.CategoryTheory.Endofunctor.Algebra
 import Mathlib.CategoryTheory.Endomorphism
 import Mathlib.CategoryTheory.Enriched.Basic
+import Mathlib.CategoryTheory.Enriched.Ordinary
 import Mathlib.CategoryTheory.EpiMono
 import Mathlib.CategoryTheory.EqToHom
 import Mathlib.CategoryTheory.Equivalence
