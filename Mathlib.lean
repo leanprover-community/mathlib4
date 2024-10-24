0c7d6d1e4e6f2754c2c05426ee5849a8f4711b26
Joël Riou <joel.riou@universite-paris-saclay.fr>
2024-10-24 11:10:55 +0200

    feat(Algebra/Category/ModuleCat/Presheaf): generators of presheaves of modules

--- Mathlib.lean
+++ Mathlib.lean
@@ -116,6 +116,7 @@ import Mathlib.Algebra.Category.ModuleCat.Presheaf.ChangeOfRings
 import Mathlib.Algebra.Category.ModuleCat.Presheaf.Colimits
 import Mathlib.Algebra.Category.ModuleCat.Presheaf.EpiMono
 import Mathlib.Algebra.Category.ModuleCat.Presheaf.Free
+import Mathlib.Algebra.Category.ModuleCat.Presheaf.Generator
 import Mathlib.Algebra.Category.ModuleCat.Presheaf.Limits
 import Mathlib.Algebra.Category.ModuleCat.Presheaf.Pushforward
 import Mathlib.Algebra.Category.ModuleCat.Presheaf.Sheafification
