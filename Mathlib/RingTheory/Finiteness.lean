8af14ea305a0775e2200e8b84266875cfa250737
Andrew Yang <the.erd.one@gmail.com>
2024-10-24 16:34:58 +0100

    more fix

--- Mathlib/RingTheory/Finiteness.lean
+++ Mathlib/RingTheory/Finiteness.lean
@@ -493,7 +493,7 @@ theorem exists_radical_pow_le_of_fg {R : Type*} [CommSemiring R] (I : Ideal R) (
       rw [add_comm, Nat.add_sub_assoc h.le]
       apply Nat.le_add_right
 
-theorem exists_pow_le_of_le_radical_of_fG {R : Type*} [CommSemiring R] {I J : Ideal R}
+theorem exists_pow_le_of_le_radical_of_fg {R : Type*} [CommSemiring R] {I J : Ideal R}
     (hIJ : I ≤ J.radical) (hJ : J.radical.FG) :
     ∃ k : ℕ, I ^ k ≤ J := by
   obtain ⟨k, hk⟩ := J.exists_radical_pow_le_of_fg hJ
@@ -501,6 +501,9 @@ theorem exists_pow_le_of_le_radical_of_fG {R : Type*} [CommSemiring R] {I J : Id
   calc
     I ^ k ≤ J.radical ^ k := Ideal.pow_right_mono hIJ _
     _ ≤ J := hk
+
+@[deprecated (since := "2024-10-24")]
+alias exists_pow_le_of_le_radical_of_fG := exists_pow_le_of_le_radical_of_fg
 
 end Ideal
 
