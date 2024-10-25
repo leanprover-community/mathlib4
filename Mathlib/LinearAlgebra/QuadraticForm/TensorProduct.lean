de320350ce372d914c2f8037733aebbf4628da23
Christopher Hoskin <christopher.hoskin@gmail.com>
2024-10-25 22:36:04 +0100

    Update comment

--- Mathlib/LinearAlgebra/QuadraticForm/TensorProduct.lean
+++ Mathlib/LinearAlgebra/QuadraticForm/TensorProduct.lean
@@ -76,8 +76,9 @@ variable (R A) in
 Note this is heterobasic; the quadratic form on the left can take values in a larger ring than
 the one on the right. -/
 -- `noncomputable` is a performance workaround for mathlib4#7103
--- Should be `(congr₂ (AlgebraTensorModule.rid R A A)).toLinearMap ∘ₗ (tensorDistrib' R A)` but then
--- `associated_tmul` breaks
+-- Should be
+-- `(AlgebraTensorModule.rid R A A).congrQuadraticMap.toLinearMap ∘ₗ QuadraticMap.tensorDistrib R A`
+-- but then `associated_tmul` breaks
 noncomputable def tensorDistrib :
     QuadraticForm A M₁ ⊗[R] QuadraticForm R M₂ →ₗ[A] QuadraticForm A (M₁ ⊗[R] M₂) :=
   letI : Invertible (2 : A) := (Invertible.map (algebraMap R A) 2).copy 2 (map_ofNat _ _).symm
