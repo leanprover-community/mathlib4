086c16c9e1afb5cf1671a508c821c4717fe9050e
Kim Morrison <kim@tqft.net>
2024-10-25 11:44:08 +0000

    chore: split Topology.GDelta (#18215)

--- Mathlib.lean
+++ Mathlib.lean
@@ -4779,7 +4779,8 @@ import Mathlib.Topology.FiberBundle.IsHomeomorphicTrivialBundle
 import Mathlib.Topology.FiberBundle.Trivialization
 import Mathlib.Topology.FiberPartition
 import Mathlib.Topology.Filter
-import Mathlib.Topology.GDelta
+import Mathlib.Topology.GDelta.Basic
+import Mathlib.Topology.GDelta.UniformSpace
 import Mathlib.Topology.Germ
 import Mathlib.Topology.Gluing
 import Mathlib.Topology.Hom.ContinuousEval
