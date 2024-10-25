64d33e6a660745f545b6d2e90e8303313cdac80f
Kim Morrison <kim@tqft.net>
2024-10-25 22:06:40 +1100

    fix shake

--- Mathlib/Topology/Separation/GDelta.lean
+++ Mathlib/Topology/Separation/GDelta.lean
@@ -8,6 +8,7 @@ import Mathlib.Topology.Compactness.SigmaCompact
 import Mathlib.Topology.Connected.TotallyDisconnected
 import Mathlib.Topology.Inseparable
 import Mathlib.Topology.Separation.Basic
+import Mathlib.Topology.GDelta.Basic
 
 /-!
 # Separation properties of topological spaces.
