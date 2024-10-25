6f32cbf354d14b0df23359fb0b863565cba22c65
Riccardo Brasca <riccardo.brasca@gmail.com>
2024-10-25 17:18:09 +0200

    fix name and use new lemma

--- Mathlib/Algebra/CharP/Lemmas.lean
+++ Mathlib/Algebra/CharP/Lemmas.lean
@@ -239,8 +239,8 @@ lemma sub_pow_eq_mul_pow_sub_pow_div_char :
 
 end CharP
 
-lemma Prime.dvd_add_pow_sub_pow_of_dvd (hpri : p.Prime) {r : R} (h₁ : r ∣ x ^ p) (h₂ : r ∣ p * x) :
-    r ∣ (x + y) ^ p - y ^ p := by
+lemma Nat.Prime.dvd_add_pow_sub_pow_of_dvd (hpri : p.Prime) {r : R} (h₁ : r ∣ x ^ p)
+    (h₂ : r ∣ p * x) : r ∣ (x + y) ^ p - y ^ p := by
   rw [add_pow_prime_eq hpri, add_right_comm, add_assoc, add_sub_assoc, add_sub_cancel_right]
   apply dvd_add h₁ (h₂.trans <| mul_dvd_mul_left _ <| Finset.dvd_sum _)
   simp only [Finset.mem_Ioo, and_imp, mul_assoc]
