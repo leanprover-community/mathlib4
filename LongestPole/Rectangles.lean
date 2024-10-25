410fe9b3ff617e0512d408c10deb02c61eb7775f
Kim Morrison <kim@tqft.net>
2024-10-25 12:58:09 +1100

    module doc/copyright

--- LongestPole/Rectangles.lean
+++ LongestPole/Rectangles.lean
@@ -1,3 +1,13 @@
+/-
+Copyright (c) 2024 Lean FRO. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Kim Morrison
+-/
+
+/-!
+# Finding the maximal rectangles in a list of points.
+-/
+
 structure Rectangle where
   left : Nat
   right : Nat
