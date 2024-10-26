295d8dfd36eeeab2df30f24c50ba937a5e7a180e
Joël Riou <joel.riou@universite-paris-saclay.fr>
2024-10-26 12:04:26 +0200

    fixes, using interval notation

--- Mathlib/CategoryTheory/SmallObject/Iteration/Basic.lean
+++ Mathlib/CategoryTheory/SmallObject/Iteration/Basic.lean
@@ -15,7 +15,7 @@ In this file, given a functor `Φ : C ⥤ C` and a natural transformation
 
 Given `j : J` where `J` is a well ordered set, we first introduce
 a category `Iteration ε j`. An object in this category consists of
-a functor `F : { i // i ≤ j } ⥤ C ⥤ C` equipped with the data
+a functor `F : Set.Iic j ⥤ C ⥤ C` equipped with the data
 which makes it the `i`th-iteration of `Φ` for all `i` such that `i ≤ j`.
 Under suitable assumptions on `C`, we shall show that this category
 `Iteration ε j` is equivalent to the punctual category (TODO).
@@ -37,16 +37,15 @@ namespace CategoryTheory
 
 open Category Limits
 
-variable {C : Type*} [Category C] {Φ : C ⥤ C} (ε : 𝟭 C ⟶ Φ)
-  {J : Type u} [Preorder J]
+variable {C : Type*} [Category C] {Φ : C ⥤ C} (ε : 𝟭 C ⟶ Φ) {J : Type u}
 
 namespace Functor
 
 namespace Iteration
 
-variable {j : J} (F : { i // i ≤ j } ⥤ C)
+variable [Preorder J] {j : J} (F : Set.Iic j ⥤ C)
 
-/-- The map `F.obj ⟨i, _⟩ ⟶ F.obj ⟨Order.succ i, _⟩` when `F : { i // i ≤ j } ⥤ C`
+/-- The map `F.obj ⟨i, _⟩ ⟶ F.obj ⟨Order.succ i, _⟩` when `F : Set.Iic j ⥤ C`
 and `i : J` is such that `i < j`. -/
 noncomputable abbrev mapSucc' [SuccOrder J] (i : J) (hi : i < j) :
     F.obj ⟨i, hi.le⟩ ⟶ F.obj ⟨Order.succ i, Order.succ_le_of_lt hi⟩ :=
@@ -54,9 +53,9 @@ noncomputable abbrev mapSucc' [SuccOrder J] (i : J) (hi : i < j) :
 
 variable {i : J} (hi : i ≤ j)
 
-/-- The functor `{ k // k < i } ⥤ C` obtained by "restriction" of `F : { i // i ≤ j } ⥤ C`
+/-- The functor `Set.Iio i ⥤ C` obtained by "restriction" of `F : Set.Iic j ⥤ C`
 when `i ≤ j`. -/
-def restrictionLT : { k // k < i } ⥤ C :=
+def restrictionLT : Set.Iio i ⥤ C :=
   (monotone_inclusion_lt_le_of_le hi).functor ⋙ F
 
 @[simp]
@@ -64,10 +63,10 @@ lemma restrictionLT_obj (k : J) (hk : k < i) :
     (restrictionLT F hi).obj ⟨k, hk⟩ = F.obj ⟨k, hk.le.trans hi⟩ := rfl
 
 @[simp]
-lemma restrictionLT_map {k₁ k₂ : { k // k < i }} (φ : k₁ ⟶ k₂) :
+lemma restrictionLT_map {k₁ k₂ : Set.Iio i} (φ : k₁ ⟶ k₂) :
     (restrictionLT F hi).map φ = F.map (homOfLE (by simpa using leOfHom φ)) := rfl
 
-/-- Given `F : { i // i ≤ j } ⥤ C`, `i : J` such that `hi : i ≤ j`, this is the
+/-- Given `F : Set.Iic j ⥤ C`, `i : J` such that `hi : i ≤ j`, this is the
 cocone consisting of all maps `F.obj ⟨k, hk⟩ ⟶ F.obj ⟨i, hi⟩` for `k : J` such that `k < i`. -/
 @[simps]
 def coconeOfLE : Cocone (restrictionLT F hi) where
@@ -77,9 +76,9 @@ def coconeOfLE : Cocone (restrictionLT F hi) where
       naturality := fun ⟨k₁, hk₁⟩ ⟨k₂, hk₂⟩ _ => by
         simp [comp_id, ← Functor.map_comp, homOfLE_comp] }
 
-/-- The functor `{ k // k ≤ i } ⥤ C` obtained by "restriction" of `F : { i // i ≤ j } ⥤ C`
+/-- The functor `Set.Iic i ⥤ C` obtained by "restriction" of `F : Set.Iic j ⥤ C`
 when `i ≤ j`. -/
-def restrictionLE : { k | k ≤ i } ⥤ C :=
+def restrictionLE : Set.Iic i ⥤ C :=
   (monotone_inclusion_le_le_of_le hi).functor ⋙ F
 
 @[simp]
@@ -87,7 +86,7 @@ lemma restrictionLE_obj (k : J) (hk : k ≤ i) :
     (restrictionLE F hi).obj ⟨k, hk⟩ = F.obj ⟨k, hk.trans hi⟩ := rfl
 
 @[simp]
-lemma restrictionLE_map {k₁ k₂ : { k | k ≤ i }} (φ : k₁ ⟶ k₂) :
+lemma restrictionLE_map {k₁ k₂ : Set.Iic i} (φ : k₁ ⟶ k₂) :
     (restrictionLE F hi).map φ = F.map (homOfLE (by simpa using leOfHom φ)) := rfl
 
 end Iteration
@@ -99,7 +98,7 @@ equipped with data and properties which characterizes the iterations up to a uni
 isomorphism for the three types of elements: `⊥`, successors, limit elements. -/
 structure Iteration [Preorder J] [OrderBot J] [SuccOrder J] (j : J) where
   /-- The data of all `i`th iterations for `i : J` such that `i ≤ j`. -/
-  F : { i // i ≤ j } ⥤ C ⥤ C
+  F : Set.Iic j ⥤ C ⥤ C
   /-- The zeroth iteration is the identity functor. -/
   isoZero : F.obj ⟨⊥, bot_le⟩ ≅ 𝟭 C
   /-- The iteration on a successor element is obtained by composition of
@@ -120,7 +119,7 @@ variable {j : J}
 
 section
 
-variable [OrderBot J] [SuccOrder J] (iter : Φ.Iteration ε j)
+variable [Preorder J] [OrderBot J] [SuccOrder J] (iter : Φ.Iteration ε j)
 
 /-- For `iter : Φ.Iteration.ε j`, this is the map
 `iter.F.obj ⟨i, _⟩ ⟶ iter.F.obj ⟨Order.succ i, _⟩` if `i : J` is such that `i < j`. -/
@@ -134,7 +133,9 @@ lemma mapSucc_eq (i : J) (hi : i < j) :
 
 end
 
-variable [OrderBot J] [SuccOrder J] (iter₁ iter₂ : Φ.Iteration ε j)
+section
+
+variable [Preorder J] [OrderBot J] [SuccOrder J] (iter₁ iter₂ : Φ.Iteration ε j)
 
 /-- A morphism between two objects `iter₁` and `iter₂` in the
 category `Φ.Iteration ε j` of `j`th iterations of a functor `Φ`
@@ -194,14 +195,13 @@ instance {J} {j : J} [PartialOrder J] [OrderBot J] [WellFoundedLT J] [SuccOrder 
     intro i
     induction i using SuccOrder.limitRecOn with
     | hm j H =>
-      have := H.eq_bot
-      subst this
+      obtain rfl := H.eq_bot
       simp [natTrans_app_zero]
     | hs j H IH =>
       intro hj
       simp [Hom.natTrans_app_succ, IH, (Order.lt_succ_of_not_isMax H).trans_le hj]
     | hl j H IH =>
-      apply fun hj ↦ (iter₁.isColimit j H hj).hom_ext ?_
+      refine fun hj ↦ (iter₁.isColimit j H hj).hom_ext ?_
       rintro ⟨k, hk⟩
       simp [IH k hk]
 
@@ -237,7 +237,7 @@ def trunc (iter : Iteration ε j) {i : J} (hi : i ≤ j) : Iteration ε i where
   isoZero := iter.isoZero
   isoSucc k hk := iter.isoSucc k (lt_of_lt_of_le hk hi)
   mapSucc'_eq k hk := iter.mapSucc'_eq k (lt_of_lt_of_le hk hi)
-  isColimit k _ hk := iter.isColimit k (hk.trans hi)
+  isColimit k hk' hk := iter.isColimit k hk' (hk.trans hi)
 
 variable (ε) in
 /-- The truncation functor `Iteration ε j ⥤ Iteration ε i` when `i ≤ j`. -/
@@ -254,7 +254,12 @@ lemma truncFunctor_map_natTrans_app
     ((truncFunctor ε hi).map φ).natTrans.app ⟨k, hk⟩ =
       φ.natTrans.app ⟨k, hk.trans hi⟩ := rfl
 
+end
+
 namespace Hom
+
+variable [PartialOrder J] [OrderBot J] [SuccOrder J] [WellFoundedLT J]
+  (iter₁ iter₂ : Φ.Iteration ε j)
 
 lemma congr_app (φ φ' : iter₁ ⟶ iter₂) (i : J) (hi : i ≤ j) :
     φ.natTrans.app ⟨i, hi⟩ = φ'.natTrans.app ⟨i, hi⟩ := by
