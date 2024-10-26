489f0fdc9112208986e9160f55fdfbb7d6dcbfb0
Calle Sönne <calle.sonne@gmail.com>
2024-10-26 13:48:38 +0100

    fix comments

--- Mathlib/CategoryTheory/Bicategory/Modification/Oplax.lean
+++ Mathlib/CategoryTheory/Bicategory/Modification/Oplax.lean
@@ -9,10 +9,9 @@ import Mathlib.CategoryTheory.Bicategory.NaturalTransformation.Oplax
 /-!
 # Modifications between oplax transformations
 
-A modification `Γ` between oplax transformations `η` and `θ` consists of a family of
-2-morphisms `Γ.app a : η.app a ⟶ θ.app a`, which satisfies the equation
-`(F.map f ◁ app b) ≫ θ.naturality f = η.naturality f ≫ (app a ▷ G.map f)`
-for each 1-morphism `f : a ⟶ b`.
+A modification `Γ` between oplax transformations `η` and `θ` consists of a family of 2-morphisms
+`Γ.app a : η.app a ⟶ θ.app a`, which for all 1-morphisms `f : a ⟶ b` satisfies the equation
+`(F.map f ◁ app b) ≫ θ.naturality f = η.naturality f ≫ app a ▷ G.map f`.
 
 ## Main definitions
 
