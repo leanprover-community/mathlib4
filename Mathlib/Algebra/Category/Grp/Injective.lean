bc0ce4928c99c8204ba1eb15c7f0d88f05e43fb6
Eric Wieser <wieser.eric@gmail.com>
2024-10-26 12:12:41 +0000

    revert move

--- Mathlib/Algebra/Category/Grp/Injective.lean
+++ Mathlib/Algebra/Category/Grp/Injective.lean
@@ -42,6 +42,19 @@ universe u
 
 variable (A : Type u) [AddCommGroup A]
 
+theorem Module.Baer.of_divisible [DivisibleBy A ℤ] : Module.Baer ℤ A := fun I g ↦ by
+  rcases IsPrincipalIdealRing.principal I with ⟨m, rfl⟩
+  obtain rfl | h0 := eq_or_ne m 0
+  · refine ⟨0, fun n hn ↦ ?_⟩
+    rw [Submodule.span_zero_singleton] at hn
+    subst hn
+    exact (map_zero g).symm
+  let gₘ := g ⟨m, Submodule.subset_span (Set.mem_singleton _)⟩
+  refine ⟨LinearMap.toSpanSingleton ℤ A (DivisibleBy.div gₘ m), fun n hn ↦ ?_⟩
+  rcases Submodule.mem_span_singleton.mp hn with ⟨n, rfl⟩
+  rw [map_zsmul, LinearMap.toSpanSingleton_apply, DivisibleBy.div_cancel gₘ h0, ← map_zsmul g,
+    SetLike.mk_smul_mk]
+
 namespace AddCommGrp
 
 theorem injective_as_module_iff : Injective (⟨A⟩ : ModuleCat ℤ) ↔
--- Mathlib/Algebra/Category/ModuleCat/Injective.lean
+++ Mathlib/Algebra/Category/ModuleCat/Injective.lean
@@ -4,10 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jujian Zhang
 -/
 import Mathlib.Algebra.Module.Injective
-import Mathlib.GroupTheory.Divisible
-import Mathlib.Algebra.EuclideanDomain.Int
-import Mathlib.Logic.Equiv.TransferInstance
-import Mathlib.RingTheory.PrincipalIdealDomain
 import Mathlib.CategoryTheory.Preadditive.Injective
 import Mathlib.Algebra.Category.ModuleCat.EpiMono
 
@@ -20,19 +16,6 @@ open CategoryTheory
 universe u v
 variable (R : Type u) (M : Type v) [Ring R] [AddCommGroup M] [Module R M]
 namespace Module
-
-theorem Baer.of_divisible [DivisibleBy M ℤ] : Module.Baer ℤ M := fun I g ↦ by
-  rcases IsPrincipalIdealRing.principal I with ⟨m, rfl⟩
-  obtain rfl | h0 := eq_or_ne m 0
-  · refine ⟨0, fun n hn ↦ ?_⟩
-    rw [Submodule.span_zero_singleton] at hn
-    subst hn
-    exact (map_zero g).symm
-  let gₘ := g ⟨m, Submodule.subset_span (Set.mem_singleton _)⟩
-  refine ⟨LinearMap.toSpanSingleton ℤ M (DivisibleBy.div gₘ m), fun n hn ↦ ?_⟩
-  rcases Submodule.mem_span_singleton.mp hn with ⟨n, rfl⟩
-  rw [map_zsmul, LinearMap.toSpanSingleton_apply, DivisibleBy.div_cancel gₘ h0, ← map_zsmul g,
-    SetLike.mk_smul_mk]
 
 theorem injective_object_of_injective_module [inj : Injective R M] :
     CategoryTheory.Injective (ModuleCat.of R M) where
