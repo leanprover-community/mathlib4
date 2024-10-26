be475dd05cf0230a0f3d5cc8c419966406777ce1
Yaël Dillies <yael.dillies@gmail.com>
2024-10-26 20:32:43 +0000

    docstring star

--- Mathlib/Data/ZMod/Basic.lean
+++ Mathlib/Data/ZMod/Basic.lean
@@ -30,6 +30,22 @@ Definition of the integers mod n, and the field structure on the integers mod p.
 * A coercion `cast` is defined from `ZMod n` into any ring.
 This is a ring hom if the ring has characteristic dividing `n`
 
+## Implementations notes
+
+To talk about
+
+ PFR had an ad hoc definition of "elementary `p`-groups" which is strictly less general and practical than `Module (ZMod p)` in that
+* it only works for `p` a prime
+* it doesn't contain the data of the `ZMod p` action
+
+I deleted that definition in favor of the `Module (ZMod p)` spelling in https://github.com/teorth/pfr/commit/813d4991dd85950ba9bd0fcaeae4940614a4adbf. My reasoning is that this spelling is already extensively used in `Algebra.Module.ZMod` and that generalising to an arbitrary ring of characteristic `p`  would result in pain when all we want to state is that our group has characteristic dividing `p`. Furthermore, `CharP` is slightly too strong in that it requires the characteristic to be `p` on the nose, rather than merely dividing it.
+
+Concretely, in your suggested lemma
+```
+lemma char_nsmul_eq_zero (R : Type*) (n : ℕ) [CharP n R] [Module R G] (x : G) : n • x = 0 := by
+```
+there is no way for Lean to figure `R` out from the resulting equality. And it would get boring to write `char_nsmul_eq_zero (ZMod n) n` every time.
+
 -/
 
 assert_not_exists Submodule
