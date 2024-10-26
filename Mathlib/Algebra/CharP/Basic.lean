a57fd3b21d16affde534bfe05558a773fb919f60
Yury G. Kudryashov <urkud@urkud.name>
2024-10-26 13:49:16 -0500

    Shake imports

--- Mathlib/Algebra/CharP/Basic.lean
+++ Mathlib/Algebra/CharP/Basic.lean
@@ -13,7 +13,7 @@ import Mathlib.Data.Nat.Find
 import Mathlib.Data.Nat.Prime.Defs
 import Mathlib.Data.ULift
 import Mathlib.Tactic.NormNum.Basic
-import Mathlib.Order.Interval.Set.Basic
+import Mathlib.Order.Interval.Set.Defs
 
 /-!
 # Characteristic of semirings
