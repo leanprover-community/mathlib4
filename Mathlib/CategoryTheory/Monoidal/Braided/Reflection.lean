31d1a20c2ebb52a14c9576e77ca0347df7e98d66
Dagur Asgeirsson <dagurtomas@gmail.com>
2024-10-26 20:58:42 +0000

    Update Mathlib/CategoryTheory/Monoidal/Braided/Reflection.lean
    Co-authored-by: Johan Commelin <johan@commelin.net>

--- Mathlib/CategoryTheory/Monoidal/Braided/Reflection.lean
+++ Mathlib/CategoryTheory/Monoidal/Braided/Reflection.lean
@@ -13,7 +13,7 @@ import Mathlib.Tactic.TFAE
 # Day's reflection theorem
 
 Let `D` be a symmetric monoidal closed category and let `C` be a reflective subcategory. Day's
-reflection theorem gives four equivalent conditions, which are all of the form that a map obtained
+reflection theorem proves the equivalence of four conditions, which are all of the form that a map obtained
 by acting on the unit of the reflective adjunction, with the internal hom and tensor functors, is
 an isomorphism.
 
