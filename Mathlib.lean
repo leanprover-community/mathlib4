8332a3801308aded6d4909cee2d6c887babc6394
Andrew Yang <the.erd.one@gmail.com>
2024-10-26 04:58:16 +0100

    update mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -4826,6 +4826,7 @@ import Mathlib.Topology.Instances.TrivSqZeroExt
 import Mathlib.Topology.Instances.ZMod
 import Mathlib.Topology.Irreducible
 import Mathlib.Topology.IsLocalHomeomorph
+import Mathlib.Topology.JacobsonSpace
 import Mathlib.Topology.KrullDimension
 import Mathlib.Topology.List
 import Mathlib.Topology.LocalAtTarget
