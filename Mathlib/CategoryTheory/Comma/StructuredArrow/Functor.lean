3a50fe9f577b0d3b7ec79963d96dfd9d7cffc4ac
Jakob von Raumer <jakob@von-raumer.de>
2024-10-26 22:44:04 +0200

    define CostructuredArrow.functor

---
+++ Mathlib/CategoryTheory/Comma/StructuredArrow/Functor.lean
@@ -0,0 +1,36 @@
+/-
+Copyright (c) 2024 Jakob von Raumer. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Jakob von Raumer
+-/
+import Mathlib.CategoryTheory.Comma.StructuredArrow.Basic
+import Mathlib.CategoryTheory.Grothendieck
+
+/-!
+# Structured Arrow Categories as strict functor to Cat
+
+Forming a structured arrow category `StructuredArrow S T` with `S : D` and `T : C ⥤ D` is stictly
+functorial in `S`, inducing a functor `Dᵒᵖ ⥤ Cat`. This file constructs said functor and proves
+that we can precompose it with another functor `L : E ⥤ D` to obtain a category equivalent to
+`Comma L T`.
+-/
+
+namespace CategoryTheory
+
+namespace CostructuredArrow
+
+universe v₁ v₂ v₃ v₄ u₁ u₂ u₃ u₄
+
+variable {C : Type u₁} [Category.{v₁} C] {D : Type u₂} [Category.{v₂} D]
+
+@[simps]
+def functor (T : C ⥤ D) : D ⥤ Cat where
+  obj d := .of <| CostructuredArrow T d
+  map f := CostructuredArrow.map f
+  map_id d := Functor.ext (fun _ => by simp only [map, Comma.mapRight, Functor.const_obj_obj,
+    Functor.const_map_app, right_eq_id, Cat.of_α, Category.comp_id, Cat.id_obj]; rfl)
+  map_comp f g := Functor.ext (fun _ => by simp [CostructuredArrow.map, Comma.mapRight])
+
+end CostructuredArrow
+
+end CategoryTheory
