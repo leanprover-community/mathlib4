d8c54b3907e358e21453b76334282c9d94d0e713
Blizzard_inc <thrashy4inbox@gmail.com>
2024-10-27 18:46:11 +0100

    replace macro with run_cmd

--- test/format_table.lean
+++ test/format_table.lean
@@ -1,16 +1,5 @@
 import Mathlib.Util.FormatTable
 
-open Lean
-
-/--
-The `#logAsInfo foo` command logs a message `foo` as info directly.
-This circumvents calling repr on strings, meaning instead of `"\\"`,
-the message becomes `\`. This makes it easier to use `#guard_msgs` to verify
-pure string-producing functions.
--/
-macro "#logAsInfo" t:term : command =>
-  `(command|#eval (logInfo <| $t : Lean.Elab.Command.CommandElabM Unit))
-
 /--
 info:
 | first           | second header   | third \| header |
@@ -20,9 +9,9 @@ info:
 | align left      |  align center   |     align right |
 -/
 #guard_msgs in
-#logAsInfo "\n" ++ formatTable
+run_cmd Lean.logInfo ("\n" ++ formatTable
   #["first", "second header", "third | header"]
   #[#["item number one", "item two", "item c"],
     #["the fourth item", "the fourth item", "escape | this"],
     #["align left", "align center", "align right"]]
-  (.some #[.left,.center,.right])
+  (.some #[.left,.center,.right]))
