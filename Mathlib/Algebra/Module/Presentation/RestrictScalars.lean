26efbd883d84ea931d21e3e1c6ee6569b8fcbd6d
Joël Riou <joel.riou@universite-paris-saclay.fr>
2024-10-27 21:17:15 +0100

    wip

--- Mathlib/Algebra/Module/Presentation/RestrictScalars.lean
+++ Mathlib/Algebra/Module/Presentation/RestrictScalars.lean
@@ -8,28 +8,55 @@ import Mathlib.Algebra.Algebra.Defs
 import Mathlib.Algebra.Module.Presentation.Basic
 
 /-!
+# Presentation of the restriction of scalars
 
 -/
 
 
 namespace Module
 
-namespace Presentation
-
 variable {A B : Type*} [CommRing A] [CommRing B] [Algebra A B] (M : Type*)
   [AddCommGroup M] [Module B M] [Module A M] [IsScalarTower A B M]
-  {rB : Relations A} (pA : Presentation B rB)
-  {rM : Relations B} (pM : Presentation M rM)
+  (rM : Relations B) (pM : Presentation M rM)
+  {rB : Relations A} (pB : Presentation B rB)
+
+namespace Relation
+
+structure RestrictScalarsData where
+  lift (g : rB.G) (r : rM.R) : rB.G × rM.G →₀ A
+  linearCombination_lift (g : rB.G) (r : rM.R) :
+    Finsupp.linearCombination A (fun (⟨b, m⟩ : rB.G × rM.G) ↦
+      Finsupp.single m (pB.solution.var b)) (lift g r) = rM.relation r
 
 namespace RestrictScalarsData
 
+variable {rM} {pB} (data : RestrictScalarsData rM pB)
+
+@[simps]
+noncomputable def restrictsScalars : Relations A where
+  G := rB.G × rM.G
+  R := Sum (rB.R × rM.G) (rB.G × rM.R)
+  relation := fun x ↦ match x with
+    | Sum.inl ⟨r, g⟩ => Finsupp.embDomain (Function.Embedding.sectl rB.G g) (rB.relation r)
+    | Sum.inr ⟨g, r⟩ => data.lift g r
+
 end RestrictScalarsData
 
-def restrictsScalars : Relations A where
-  G := rM.G × rB.G
-  R := Sum (rM.G × rB.R) (rM.R × rB.G)
-  relation := sorry
-  -- some extra data is needed for the second part
+end Relation
+
+namespace Presentation
+
+variable {rM}
+variable (data : Relation.RestrictScalarsData rM pB)
+
+def restrictScalarsSolution : data.restrictsScalars.Solution M where
+  var := fun ⟨b, m⟩ ↦ pB.solution.var b • pM.solution.var m
+  relation := by
+    rintro (⟨r, g⟩ | ⟨g, r⟩)
+    · dsimp
+      sorry
+    · dsimp
+      sorry
 
 end Presentation
 
