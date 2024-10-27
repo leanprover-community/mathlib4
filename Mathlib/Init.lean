885562fd604a46ee12c5df3df5d3a3a5231a41f2
Michael Rothgang <rothgang@math.uni-bonn.de>
2024-10-27 18:35:18 +0100

    Tweak comment in Mathlib.Init.

--- Mathlib/Init.lean
+++ Mathlib/Init.lean
@@ -1,6 +1,6 @@
 import Mathlib.Tactic.Linter.DocPrime
-import Mathlib.Tactic.Linter.HashCommandLinter
 import Mathlib.Tactic.Linter.GlobalAttributeIn
+import Mathlib.Tactic.Linter.HashCommandLinter
 import Mathlib.Tactic.Linter.Header
 -- This file imports Batteries.Tactic.Lint, where the `env_linter` attribute is defined.
 import Mathlib.Tactic.Linter.Lint
@@ -11,7 +11,7 @@ import Mathlib.Tactic.Linter.UnusedTactic
 import Mathlib.Tactic.Linter.Style
 
 /-!
-This is the root file in Mathlib: it is imported by virtually *all* Mathlib files.
+This is the root file in Mathlib: it is imported by *all* Mathlib files.
 For this reason, the imports of this files are carefully curated.
 Any modification involving a change in the imports of this file should be discussed beforehand.
 
@@ -20,6 +20,9 @@ Here are some general guidelines:
 * every import needs to have a comment explaining why the import is there;
 * strong preference for avoiding files that themselves have imports beyond `Lean`, and
   any exception to this rule should by accompanied by a comment explaining the transitive imports.
+
+A (text-based) linter verified that every file in Mathlib imports `Mathlib.Init`
+(perhaps indirectly) --- except for the imports in this file, of course.
 
 ## Linters
 
@@ -30,7 +33,7 @@ as early as possible.
 All linters imported here have no bulk imports;
 **Not** imported in this file are
 - the text-based linters in `Linters/TextBased.lean`, as they can be imported later
-- the `minImports` linter, as that is still disabled by default.
+- the `minImports` linter, as that is still disabled by default
 - the `haveLet` linter, as it is currently disabled by default
 
 -/
