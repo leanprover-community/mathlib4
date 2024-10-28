4559f84a5f85c45ccfbc43c4ca4f1549c8033f79
Kim Morrison <kim@tqft.net>
2024-10-28 15:00:33 +1100

    unshake

--- Mathlib/Data/Set/Image.lean
+++ Mathlib/Data/Set/Image.lean
@@ -8,7 +8,7 @@ import Mathlib.Tactic.Use
 import Batteries.Tactic.Congr
 import Mathlib.Order.TypeTags
 import Mathlib.Data.Option.Basic
-import Mathlib.Order.SymmDiff
+import Mathlib.Data.Set.SymmDiff
 
 /-!
 # Images and preimages of sets
