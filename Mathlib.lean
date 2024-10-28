ed81cec6edb7997e2d21e33c5d7685ba800a80a3
Joël Riou <joel.riou@universite-paris-saclay.fr>
2024-10-28 13:33:21 +0000

    feat(Algebra/Module): presentation of modules (#18295)
    This PR defines a basic API for the description of modules in terms of generators and relations.
    
    
    
    Co-authored-by: Joël Riou <37772949+joelriou@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -509,6 +509,7 @@ import Mathlib.Algebra.Module.Opposite
 import Mathlib.Algebra.Module.PID
 import Mathlib.Algebra.Module.Pi
 import Mathlib.Algebra.Module.PointwisePi
+import Mathlib.Algebra.Module.Presentation.Basic
 import Mathlib.Algebra.Module.Prod
 import Mathlib.Algebra.Module.Projective
 import Mathlib.Algebra.Module.Rat
