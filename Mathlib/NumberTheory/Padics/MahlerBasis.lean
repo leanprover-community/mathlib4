36bb81b9a36436fe4f84f89d756db52dc339cbcc
David Loeffler <d.loeffler.01@cantab.net>
2024-10-29 23:06:08 +0100

    better approach

--- Mathlib/NumberTheory/Padics/MahlerBasis.lean
+++ Mathlib/NumberTheory/Padics/MahlerBasis.lean
@@ -4,8 +4,9 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Giulio Caflisch, David Loeffler
 -/
 import Mathlib.Analysis.Normed.Group.Ultra
-import Mathlib.Data.Nat.Factorial.BigOperators
 import Mathlib.NumberTheory.Padics.ProperSpace
+import Mathlib.RingTheory.Binomial
+import Mathlib.Topology.Algebra.Polynomial
 import Mathlib.Topology.ContinuousMap.Compact
 
 /-!
@@ -26,48 +27,66 @@ open Finset
 
 variable {p : ℕ} [hp : Fact p.Prime]
 
+namespace PadicInt
+
+/-- Bound for norms of ascending Pochhammer symbols. -/
+lemma norm_ascPochhammer_le (k : ℕ) (x : ℤ_[p]) :
+    ‖(ascPochhammer ℤ_[p] k).eval x‖ ≤ ‖(k.factorial : ℤ_[p])‖ := by
+  let f := (ascPochhammer ℤ_[p] k).eval
+  change ‖f x‖ ≤ ‖_‖
+  have hC : (k.factorial : ℤ_[p]) ≠ 0 := Nat.cast_ne_zero.mpr k.factorial_ne_zero
+  have hf : ContinuousAt f x := Polynomial.continuousAt _
+  -- find `n : ℕ` such that `‖mahler k x - mahler k n‖ ≤ 1`
+  obtain ⟨n, hn⟩ : ∃ n : ℕ, ‖f x - f n‖ ≤ ‖(k.factorial : ℤ_[p])‖ := by
+    obtain ⟨δ, hδp, hδ⟩ := Metric.continuousAt_iff.mp hf _ (norm_pos_iff.mpr hC)
+    obtain ⟨n, hn'⟩ := PadicInt.denseRange_natCast.exists_dist_lt x hδp
+    simpa only [← dist_eq_norm_sub'] using ⟨n, (hδ (dist_comm x n ▸ hn')).le⟩
+  -- use ultrametric property to show that `‖mahler k n‖ ≤ 1` implies `‖mahler k x‖ ≤ 1`
+  refine sub_add_cancel (f x) _ ▸ (IsUltrametricDist.norm_add_le_max _ (f n)).trans (max_le hn ?_)
+  -- finish using the fact that `mahler k n ∈ ℕ`
+  simp_rw [f, ← ascPochhammer_eval_cast, Polynomial.eval_eq_smeval,
+    ← Ring.factorial_nsmul_multichoose_eq_ascPochhammer, smul_eq_mul, Nat.cast_mul, norm_mul]
+  exact mul_le_of_le_one_right (norm_nonneg _) (norm_le_one _)
+
+/-- The p-adic integers are a binomial ring, i.e. a ring where binomial coefficients make sense. -/
+noncomputable instance instBinomialRing : BinomialRing ℤ_[p] where
+  nsmul_right_injective n hn := smul_right_injective ℤ_[p] hn
+  -- We define `multichoose` as a fraction in `ℚ_[p]` together with a proof that its norm is `≤ 1`.
+  multichoose x k := ⟨(ascPochhammer ℤ_[p] k).eval x / (k.factorial : ℚ_[p]), by
+    rw [norm_div, div_le_one (by simpa using k.factorial_ne_zero)]
+    exact x.norm_ascPochhammer_le k⟩
+  factorial_nsmul_multichoose x k := by rw [← Subtype.coe_inj, nsmul_eq_mul, PadicInt.coe_mul,
+    PadicInt.coe_natCast, mul_div_cancel₀ _ (mod_cast k.factorial_ne_zero), Subtype.coe_inj,
+    Polynomial.eval_eq_smeval, Polynomial.ascPochhammer_smeval_cast]
+
+lemma continuous_choose (k : ℕ) : Continuous (fun x : ℤ_[p] ↦ Ring.choose x k) := by
+  simp only [Ring.choose, Ring.multichoose, BinomialRing.multichoose, continuous_induced_rng]
+  exact (continuous_induced_rng.mp <| (Polynomial.continuous _).comp
+    <| (continuous_sub_right _).add continuous_const).div_const _
+
+end PadicInt
+
 /--
 The `k`-th Mahler basis function, i.e. the unique continuous function `ℤ_[p] → ℚ_[p]`
 agreeing with `n ↦ n.choose k` for `n ∈ ℕ`. See [colmez2010], §1.2.1.
 -/
 noncomputable def mahler (k : ℕ) : C(ℤ_[p], ℚ_[p]) where
-  toFun x := (∏ i in range k, (x.1 - i)) / k.factorial
-  continuous_toFun := (continuous_finset_prod _ fun _ _ ↦
-    (continuous_sub_right _).comp' continuous_subtype_val).div_const _
+  toFun x := ↑(Ring.choose x k)
+  continuous_toFun := continuous_induced_rng.mp (continuous_choose k)
+
+lemma mahler_apply (k : ℕ) (x : ℤ_[p]) : mahler k x = Ring.choose x k := rfl
 
 /-- The function `mahler k` extends `n ↦ n.choose k` on `ℕ`. -/
 lemma mahler_natCast_eq (k n : ℕ) : mahler k (n : ℤ_[p]) = n.choose k := by
-  rw [mahler, ContinuousMap.coe_mk, div_eq_iff (mod_cast Nat.factorial_ne_zero k),
-    mul_comm, ← Nat.cast_mul, ← Nat.descFactorial_eq_factorial_mul_choose n k,
-    Nat.descFactorial_eq_prod_range]
-  by_cases hkn : k ≤ n
-  · simp only [Nat.cast_prod]
-    apply prod_congr rfl (fun i hi ↦ (Nat.cast_sub ?_).symm)
-    exact (Finset.mem_range.mp hi).le.trans hkn
-  · -- show there's a zero term in both products
-    simp only [Nat.cast_prod]
-    rw [prod_eq_zero (mem_range.mpr (not_le.mp hkn)) (sub_self (n : ℚ_[p]))]
-    rw [prod_eq_zero (mem_range.mpr (not_le.mp hkn)) (mod_cast (Nat.sub_self n))]
+  simp only [mahler_apply, Ring.choose_natCast, PadicInt.coe_natCast]
 
 /--
 The uniform norm of the `k`-th Mahler basis function is 1, for every `k`.
 -/
 @[simp] lemma norm_mahler_eq (k : ℕ) : ‖(mahler k : C(ℤ_[p], ℚ_[p]))‖ = 1 := by
   apply le_antisymm
-  · -- Hard direction: show all values have norm ≤ 1
-    refine (mahler k).norm_le_of_nonempty.mpr fun x ↦ ?_
-    -- find `n : ℕ` such that `‖mahler k x - mahler k n‖ ≤ 1`
-    obtain ⟨n, hn⟩ : ∃ n : ℕ, ‖mahler k x - mahler k n‖ ≤ 1 := by
-      simp only [← dist_eq_norm_sub']
-      obtain ⟨δ, hδp, hδ⟩ := Metric.continuousAt_iff.mp ((mahler k).continuousAt x) 1 one_pos
-      obtain ⟨n, hn'⟩ := PadicInt.denseRange_natCast.exists_dist_lt x hδp
-      exact ⟨n, (hδ (dist_comm x n ▸ hn')).le⟩
-    -- use ultrametric property to show that `‖mahler k n‖ ≤ 1` implies `‖mahler k x‖ ≤ 1`
-    rw [← sub_add_cancel (mahler k x) (mahler k n)]
-    refine (IsUltrametricDist.norm_add_le_max _ (mahler k (n : ℤ_[p]))).trans (max_le hn ?_)
-    -- finish using the fact that `mahler k n ∈ ℕ`
-    simpa only [mahler_natCast_eq, ← PadicInt.coe_natCast, ← PadicInt.norm_def]
-      using PadicInt.norm_le_one _
-  · -- Easy direction: show norm 1 is attained at `x = k`
+  · -- Show all values have norm ≤ 1
+    exact (mahler k).norm_le_of_nonempty.mpr (fun _ ↦ PadicInt.norm_le_one _)
+  · -- Show norm 1 is attained at `x = k`
     refine (le_of_eq ?_).trans ((mahler k).norm_coe_le_norm k)
     rw [mahler_natCast_eq, Nat.choose_self, Nat.cast_one, norm_one]
