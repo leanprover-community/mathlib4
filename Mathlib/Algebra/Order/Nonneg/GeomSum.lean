9b562c6e7a60cadbd2cc45cb080d9d96104a0116
Daniel Weber <commandmaster2017@gmail.com>
2024-10-29 07:14:34 +0200

    Generalize to canonically linear ordered semifield

--- Mathlib/Algebra/Order/Nonneg/GeomSum.lean
+++ Mathlib/Algebra/Order/Nonneg/GeomSum.lean
@@ -8,14 +8,14 @@ import Mathlib.Algebra.Order.Nonneg.Field
 import Mathlib.Algebra.GeomSum
 import Mathlib.Data.Fintype.BigOperators
 
-variable {α : Type*} [LinearOrderedField α]
+variable {α : Type*} [CanonicallyLinearOrderedSemifield α] [Sub α] [OrderedSub α]
 
-lemma Nonneg.geom_sum_of_one_lt {a : {x : α // 0 ≤ x}} (h : 1 < a) (n : ℕ) :
+lemma Nonneg.geom_sum_of_one_lt {a : α} (h : 1 < a) (n : ℕ) :
     ∑ i ∈ Finset.range n, a ^ i = (a ^ n - 1) / (a - 1) := by
   apply eq_div_of_mul_eq (tsub_pos_of_lt h).ne' (eq_tsub_of_add_eq _)
   simpa only [tsub_add_cancel_of_le h.le] using geom_sum_mul_add (a - 1) n
 
-lemma Nonneg.geom_sum_of_lt_one {a : {x : α // 0 ≤ x}} (h : a < 1) (n : ℕ) :
+lemma Nonneg.geom_sum_of_lt_one {a : α} (h : a < 1) (n : ℕ) :
     ∑ i ∈ Finset.range n, a ^ i = (1 - a ^ n) / (1 - a) := by
   apply eq_div_of_mul_eq (tsub_pos_of_lt h).ne' (eq_tsub_of_add_eq _)
   suffices ∑ i ∈ Finset.range n, a ^ i = ∑ i ∈ Finset.range n, a ^ (n - 1 - i) by
@@ -25,4 +25,3 @@ lemma Nonneg.geom_sum_of_lt_one {a : {x : α // 0 ≤ x}} (h : a < 1) (n : ℕ) 
   rw [← Fin.sum_univ_eq_sum_range, ← Fin.sum_univ_eq_sum_range, ← e.sum_comp]
   have (i : Fin n) : e i = n - 1 - i := by dsimp [e]; omega
   simp [this]
-
