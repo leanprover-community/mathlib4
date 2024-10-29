5d386560f3b69e7434e9c74106c45ce55b434150
Jireh Loreaux <loreaujy@gmail.com>
2024-10-29 00:06:47 +0000

    feat: `cfc_tsub` for `ℝ≥0` functions (#18360)

--- Mathlib/Analysis/CStarAlgebra/ContinuousFunctionalCalculus/Order.lean
+++ Mathlib/Analysis/CStarAlgebra/ContinuousFunctionalCalculus/Order.lean
@@ -35,7 +35,45 @@ the spectral order.
 continuous functional calculus, normal, selfadjoint
 -/
 
-open scoped NNReal
+open scoped NNReal CStarAlgebra
+
+local notation "σₙ" => quasispectrum
+
+theorem cfc_tsub {A : Type*} [TopologicalSpace A] [Ring A] [PartialOrder A] [StarRing A]
+    [StarOrderedRing A] [Algebra ℝ A] [TopologicalRing A]
+    [ContinuousFunctionalCalculus ℝ (IsSelfAdjoint : A → Prop)]
+    [UniqueContinuousFunctionalCalculus ℝ A] [NonnegSpectrumClass ℝ A] (f g : ℝ≥0 → ℝ≥0)
+    (a : A) (hfg : ∀ x ∈ spectrum ℝ≥0 a, g x ≤ f x) (ha : 0 ≤ a := by cfc_tac)
+    (hf : ContinuousOn f (spectrum ℝ≥0 a) := by cfc_cont_tac)
+    (hg : ContinuousOn g (spectrum ℝ≥0 a) := by cfc_cont_tac) :
+    cfc (fun x ↦ f x - g x) a = cfc f a - cfc g a := by
+  have ha' := SpectrumRestricts.nnreal_of_nonneg ha
+  have : (spectrum ℝ a).EqOn (fun x ↦ ((f x.toNNReal - g x.toNNReal : ℝ≥0) : ℝ))
+      (fun x ↦ f x.toNNReal - g x.toNNReal) :=
+    fun x hx ↦ NNReal.coe_sub <| hfg _ <| ha'.apply_mem hx
+  rw [cfc_nnreal_eq_real, cfc_nnreal_eq_real, cfc_nnreal_eq_real, cfc_congr this]
+  refine cfc_sub _ _ a ?_ ?_
+  all_goals
+    exact continuous_subtype_val.comp_continuousOn <|
+      ContinuousOn.comp ‹_› continuous_real_toNNReal.continuousOn <| ha'.image ▸ Set.mapsTo_image ..
+
+theorem cfcₙ_tsub {A : Type*} [TopologicalSpace A] [NonUnitalRing A] [PartialOrder A] [StarRing A]
+    [StarOrderedRing A] [Module ℝ A] [IsScalarTower ℝ A A] [SMulCommClass ℝ A A] [TopologicalRing A]
+    [NonUnitalContinuousFunctionalCalculus ℝ (IsSelfAdjoint : A → Prop)]
+    [UniqueNonUnitalContinuousFunctionalCalculus ℝ A] [NonnegSpectrumClass ℝ A] (f g : ℝ≥0 → ℝ≥0)
+    (a : A) (hfg : ∀ x ∈ σₙ ℝ≥0 a, g x ≤ f x) (ha : 0 ≤ a := by cfc_tac)
+    (hf : ContinuousOn f (σₙ ℝ≥0 a) := by cfc_cont_tac) (hf0 : f 0 = 0 := by cfc_zero_tac)
+    (hg : ContinuousOn g (σₙ ℝ≥0 a) := by cfc_cont_tac) (hg0 : g 0 = 0 := by cfc_zero_tac) :
+    cfcₙ (fun x ↦ f x - g x) a = cfcₙ f a - cfcₙ g a := by
+  have ha' := QuasispectrumRestricts.nnreal_of_nonneg ha
+  have : (σₙ ℝ a).EqOn (fun x ↦ ((f x.toNNReal - g x.toNNReal : ℝ≥0) : ℝ))
+      (fun x ↦ f x.toNNReal - g x.toNNReal) :=
+    fun x hx ↦ NNReal.coe_sub <| hfg _ <| ha'.apply_mem hx
+  rw [cfcₙ_nnreal_eq_real, cfcₙ_nnreal_eq_real, cfcₙ_nnreal_eq_real, cfcₙ_congr this]
+  refine cfcₙ_sub _ _ a ?_ (by simpa) ?_
+  all_goals
+    exact continuous_subtype_val.comp_continuousOn <|
+      ContinuousOn.comp ‹_› continuous_real_toNNReal.continuousOn <| ha'.image ▸ Set.mapsTo_image ..
 
 namespace Unitization
 
