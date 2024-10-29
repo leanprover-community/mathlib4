a673241d98389a73914116bf2bd1d2aefa59a6c4
PieterCuijpers <p.j.l.cuijpers@tue.nl>
2024-10-29 11:01:32 +0100

    Update Mathlib/Order/Quantale.lean
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- Mathlib/Order/Quantale.lean
+++ Mathlib/Order/Quantale.lean
@@ -3,8 +3,8 @@ Copyright (c) 2024 Pieter Cuijpers. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Pieter Cuijpers
 -/
-import Mathlib.Order.CompleteLattice
 import Mathlib.Algebra.Group.Defs
+import Mathlib.Order.CompleteLattice
 
 /-!
 # Theory of quantales
