17d5955a8d20feb1770676b854f118e8c40b7d41
Eric Wieser <wieser.eric@gmail.com>
2024-10-29 15:17:23 +0000

    feat: cardinalities of free constructions and `Nonempty (CommRing α)` (#18364)
    [Zulip thread](https://leanprover.zulipchat.com/#narrow/channel/116395-maths/topic/Does.20every.20type.20permit.20a.20semiring.3F/near/479347521)
    
    Co-authored-by: Daniel Weber <55664973+Command-Master@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -4300,6 +4300,7 @@ import Mathlib.SetTheory.Cardinal.Divisibility
 import Mathlib.SetTheory.Cardinal.ENat
 import Mathlib.SetTheory.Cardinal.Finite
 import Mathlib.SetTheory.Cardinal.Finsupp
+import Mathlib.SetTheory.Cardinal.Free
 import Mathlib.SetTheory.Cardinal.PartENat
 import Mathlib.SetTheory.Cardinal.SchroederBernstein
 import Mathlib.SetTheory.Cardinal.Subfield
