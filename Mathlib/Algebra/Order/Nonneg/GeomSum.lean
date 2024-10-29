99ba69f7cf856732b6a7c0665b1b718c5a7b87ca
Daniel Weber <commandmaster2017@gmail.com>
2024-10-29 07:30:34 +0200

    Add geom₂ and mul versions

--- Mathlib/Algebra/Order/Nonneg/GeomSum.lean
+++ Mathlib/Algebra/Order/Nonneg/GeomSum.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2024 Johan Commelin. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Johan Commelin
+Authors: Johan Commelin, Daniel Weber
 -/
 
 import Mathlib.Algebra.Order.Nonneg.Field
@@ -10,18 +10,40 @@ import Mathlib.Data.Fintype.BigOperators
 
 variable {α : Type*} [CanonicallyLinearOrderedSemifield α] [Sub α] [OrderedSub α]
 
+lemma Nonneg.geom₂_sum_mul {x y : α} (h : x < y) (n : ℕ) :
+    (∑ i ∈ Finset.range n, y ^ i * x ^ (n - 1 - i)) * (y - x) = y ^ n - x ^ n := by
+  apply eq_tsub_of_add_eq
+  simpa only [tsub_add_cancel_of_le h.le] using geom_sum₂_mul_add (y - x) x n
+
+lemma Nonneg.geom_sum_mul_of_one_lt {x : α} (h : 1 < x) (n : ℕ) :
+    (∑ i ∈ Finset.range n, x ^ i) * (x - 1) = x ^ n - 1 := by
+  convert geom₂_sum_mul h n <;> simp
+
+lemma Nonneg.geom₂_sum_mul' {x y : α} (h : x < y) (n : ℕ) :
+    (∑ i ∈ Finset.range n, x ^ i * y ^ (n - 1 - i)) * (y - x) = y ^ n - x ^ n := by
+  rw [← Finset.sum_range_reflect]
+  convert geom₂_sum_mul h n using 3
+  simp_all only [Finset.mem_range]
+  rw [mul_comm]
+  congr
+  omega
+
+lemma Nonneg.geom_sum_mul_of_lt_one {x : α} (h : x < 1) (n : ℕ) :
+    (∑ i ∈ Finset.range n, x ^ i) * (1 - x) = 1 - x ^ n := by
+  convert geom₂_sum_mul' h n <;> simp
+
+lemma Nonneg.geom₂_sum_of_lt {x y : α} (h : x < y) (n : ℕ) :
+    ∑ i ∈ Finset.range n, y ^ i * x ^ (n - 1 - i) = (y ^ n - x ^ n) / (y - x) :=
+  eq_div_of_mul_eq (tsub_pos_of_lt h).ne' (geom₂_sum_mul h n)
+
+lemma Nonneg.geom₂_sum_of_lt' {x y : α} (h : x < y) (n : ℕ) :
+    ∑ i ∈ Finset.range n, x ^ i * y ^ (n - 1 - i) = (y ^ n - x ^ n) / (y - x) :=
+  eq_div_of_mul_eq (tsub_pos_of_lt h).ne' (geom₂_sum_mul' h n)
+
 lemma Nonneg.geom_sum_of_one_lt {a : α} (h : 1 < a) (n : ℕ) :
-    ∑ i ∈ Finset.range n, a ^ i = (a ^ n - 1) / (a - 1) := by
-  apply eq_div_of_mul_eq (tsub_pos_of_lt h).ne' (eq_tsub_of_add_eq _)
-  simpa only [tsub_add_cancel_of_le h.le] using geom_sum_mul_add (a - 1) n
+    ∑ i ∈ Finset.range n, a ^ i = (a ^ n - 1) / (a - 1) :=
+  eq_div_of_mul_eq (tsub_pos_of_lt h).ne' (geom_sum_mul_of_one_lt h n)
 
 lemma Nonneg.geom_sum_of_lt_one {a : α} (h : a < 1) (n : ℕ) :
-    ∑ i ∈ Finset.range n, a ^ i = (1 - a ^ n) / (1 - a) := by
-  apply eq_div_of_mul_eq (tsub_pos_of_lt h).ne' (eq_tsub_of_add_eq _)
-  suffices ∑ i ∈ Finset.range n, a ^ i = ∑ i ∈ Finset.range n, a ^ (n - 1 - i) by
-    simpa only [tsub_add_cancel_of_le h.le, one_pow, one_mul, this] using
-      geom_sum₂_mul_add (1 - a) a n
-  let e : Fin n ≃ Fin n := Fin.rev_involutive.toPerm
-  rw [← Fin.sum_univ_eq_sum_range, ← Fin.sum_univ_eq_sum_range, ← e.sum_comp]
-  have (i : Fin n) : e i = n - 1 - i := by dsimp [e]; omega
-  simp [this]
+    ∑ i ∈ Finset.range n, a ^ i = (1 - a ^ n) / (1 - a) :=
+  eq_div_of_mul_eq (tsub_pos_of_lt h).ne' (geom_sum_mul_of_lt_one h n)
