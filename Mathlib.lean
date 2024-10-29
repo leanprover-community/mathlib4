7b6dc4d0c93bf4ff58a684c3959a121715afbd8e
Anne C.A. Baanen <vierkantor@vierkantor.com>
2024-10-29 14:30:01 +0100

    chore(RingTheory/Congruence): split off `Defs.lean`
    The goal of this PR is to split off enough to define the ideal quotient and its ring structure without too much theory in the way.

--- Mathlib.lean
+++ Mathlib.lean
@@ -3997,6 +3997,7 @@ import Mathlib.RingTheory.Coalgebra.TensorProduct
 import Mathlib.RingTheory.Complex
 import Mathlib.RingTheory.Congruence.Basic
 import Mathlib.RingTheory.Congruence.BigOperators
+import Mathlib.RingTheory.Congruence.Defs
 import Mathlib.RingTheory.Congruence.Opposite
 import Mathlib.RingTheory.Coprime.Basic
 import Mathlib.RingTheory.Coprime.Ideal
