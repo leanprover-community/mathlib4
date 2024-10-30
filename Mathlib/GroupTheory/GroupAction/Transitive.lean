0b05136f2845314fc055fc55dbf5d29a04b80633
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2024-10-30 22:21:04 +0100

    adjust import

--- Mathlib/GroupTheory/GroupAction/Transitive.lean
+++ Mathlib/GroupTheory/GroupAction/Transitive.lean
@@ -3,8 +3,8 @@ Copyright (c) 2022 Antoine Chambert-Loir. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Antoine Chambert-Loir
 -/
+import Mathlib.GroupTheory.GroupAction.Defs
 import Mathlib.GroupTheory.GroupAction.Hom
-import Mathlib.GroupTheory.GroupAction.Basic
 
 /-! # Complements to pretransitive actions
 
