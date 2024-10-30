33c34d64964ad8b39a7d0837d9d1b03c5477f559
Quang Dao <quangdao@umich.edu>
2024-10-30 15:56:14 -0400

    fix deprecated `in` notation

--- Mathlib/Algebra/MvPolynomial/Degrees.lean
+++ Mathlib/Algebra/MvPolynomial/Degrees.lean
@@ -269,12 +269,12 @@ theorem degreeOf_pow_le (i : σ) (p : MvPolynomial σ R) (n : ℕ) :
   rw [Multiset.count_nsmul]
 
 theorem degreeOf_sum_le {ι : Type*} (i : σ) (s : Finset ι) (f : ι → MvPolynomial σ R) :
-    degreeOf i (∑ j in s, f j) ≤ s.sup fun j => degreeOf i (f j) := by
+    degreeOf i (∑ j ∈ s, f j) ≤ s.sup fun j => degreeOf i (f j) := by
   simp_rw [degreeOf_eq_sup]
   exact supDegree_sum_le
 
 theorem degreeOf_prod_le {ι : Type*} (i : σ) (s : Finset ι) (f : ι → MvPolynomial σ R) :
-    degreeOf i (∏ j in s, f j) ≤ ∑ j in s, (f j).degreeOf i := by
+    degreeOf i (∏ j ∈ s, f j) ≤ ∑ j ∈ s, (f j).degreeOf i := by
   simp_rw [degreeOf_eq_sup]
   exact supDegree_prod_le (by simp only [coe_zero, Pi.zero_apply])
     (fun _ _ => by simp only [coe_add, Pi.add_apply])
