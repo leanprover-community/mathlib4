ad5d052bd6e2fd406400078ae964e9f2283c4eec
Joël Riou <joel.riou@universite-paris-saclay.fr>
2024-10-30 22:35:33 +0100

    fixed imports

--- Mathlib/Algebra/Module/Presentation/Tensor.lean
+++ Mathlib/Algebra/Module/Presentation/Tensor.lean
@@ -3,7 +3,7 @@ Copyright (c) 2024 Joël Riou. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joël Riou
 -/
-import Mathlib.Algebra.Module.Presentation.Free
+import Mathlib.Algebra.Module.Presentation.Basic
 import Mathlib.LinearAlgebra.TensorProduct.Basic
 
 /-!
