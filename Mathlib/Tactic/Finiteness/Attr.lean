69bd919dec934f9b2512abbf83f842f39fc361f5
Michael Rothgang <rothgang@math.uni-bonn.de>
2024-10-31 10:28:17 +0100

    Two more

--- Mathlib/Tactic/Finiteness/Attr.lean
+++ Mathlib/Tactic/Finiteness/Attr.lean
@@ -4,6 +4,9 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Floris van Doorn
 -/
 import Aesop
+-- Import this linter explicitly to ensure that
+-- this file has a valid copyright header and module docstring.
+import Mathlib.Tactic.Linter.Header
 
 /-! # Finiteness tactic attribute -/
 
