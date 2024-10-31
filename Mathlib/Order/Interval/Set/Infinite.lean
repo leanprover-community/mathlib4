760f27d781270cbd5062f74a16431f2bd2677127
Violeta Hernández <vi.hdz.p@gmail.com>
2024-10-31 12:51:46 -0600

    Update Mathlib/Order/Interval/Set/Infinite.lean
    Co-authored-by: Anne Baanen <Vierkantor@users.noreply.github.com>

--- Mathlib/Order/Interval/Set/Infinite.lean
+++ Mathlib/Order/Interval/Set/Infinite.lean
@@ -23,8 +23,10 @@ instance (priority := 100) NoMaxOrder.infinite [Nonempty α] [NoMaxOrder α] : I
   let ⟨f, hf⟩ := Nat.exists_strictMono α
   Infinite.of_injective f hf.injective
 
-/-- A nonempty preorder with no minimal element is infinite. -/
-instance NoMinOrder.infinite [Nonempty α] [NoMinOrder α] : Infinite α :=
+/-- A nonempty preorder with no minimal element is infinite.
+This instance has lower priority to avoid the cycle `Infinite α → Nontrivial α → Nonempty α` if possible.
+-/
+instance (priority := 100) NoMinOrder.infinite [Nonempty α] [NoMinOrder α] : Infinite α :=
   @NoMaxOrder.infinite αᵒᵈ _ _ _
 
 namespace Set
