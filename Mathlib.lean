c89d846221240c16351b00a2e0688bf262359e2a
Dagur Asgeirsson <dagurtomas@gmail.com>
2024-11-01 22:15:11 +0000

    feat(CategoryTheory/Sites): some API for the Yoneda embedding into sheaf categories (#18185)

--- Mathlib.lean
+++ Mathlib.lean
@@ -2001,6 +2001,7 @@ import Mathlib.CategoryTheory.Sites.SheafOfTypes
 import Mathlib.CategoryTheory.Sites.Sheafification
 import Mathlib.CategoryTheory.Sites.Sieves
 import Mathlib.CategoryTheory.Sites.Spaces
+import Mathlib.CategoryTheory.Sites.Subcanonical
 import Mathlib.CategoryTheory.Sites.Subsheaf
 import Mathlib.CategoryTheory.Sites.Types
 import Mathlib.CategoryTheory.Sites.Whiskering
