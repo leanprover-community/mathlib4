c013810789fea1d3da4e7fc7ed352f825ea004c2
Mark Santa Clara Munro <santacm@clarkson.edu>
2024-11-01 09:08:59 -0400

    Changed all swapRow theorem names, variables, and types to keep consistency, except for swapImpliesSwapMat and swapMatImpliesSwap

--- Mathlib/Data/Matrix/ElementaryRowOperations.lean
+++ Mathlib/Data/Matrix/ElementaryRowOperations.lean
@@ -44,7 +44,8 @@ variable {m n α R : Type*}
 -- α is for entries
 -- R is used for scalar multiplication on matrices (should it be a different type?)
 
--- i, j, k, l are used as variable name for row : m
+-- i, j, k are used as variable name for row : m
+-- l is used as a variable name for column : n
 -- x is used as variable name for multiplication : R
 
 
@@ -57,7 +58,6 @@ variable [DecidableEq m]
 section swapRow
 
 -- Copies Row j to Row i, used in SwapRow
-
 def dupRow (M : Matrix m n α) (i : m) (j : m) : Matrix m n α :=
   updateRow M i (M j)
 
@@ -65,118 +65,105 @@ def dupRow (M : Matrix m n α) (i : m) (j : m) : Matrix m n α :=
 def swapRow (M : Matrix m n α) (i : m) (j : m) : Matrix m n α :=
   updateRow (dupRow M i j) j (M i)
 
--- TO BE CHANGED
--- -- If you swap Row i and j then Row i will be the previous Row j
--- lemma swapRowi [DecidableEq m](M : Matrix m n α) (i : m) (j : m)
---   : swapRow M i j i = M j := by
---   rw [swapRow, dupRow]
---   by_cases sameRow : i = j
---   rw [sameRow, Matrix.updateRow_self]
---   rw [Matrix.updateRow_ne, Matrix.updateRow_self]
---   exact sameRow
+-- MAYBE MAKE SECTION WITH THE NEXT 3 LEMMAS
+-- If you swap Row i and j then Row i will be the previous Row j
+lemma swapRow_eq_left (M : Matrix m n α) (i : m) (j : m) :
+    swapRow M i j i = M j := by
+  rw [swapRow, dupRow]
+  by_cases sameRow : i = j
+  rw [sameRow, Matrix.updateRow_self]
+  rw [Matrix.updateRow_ne, Matrix.updateRow_self]
+  exact sameRow
 
--- -- If you swap Row i and j then Row j will be the previous Row i
--- lemma swapRowj [DecidableEq m](M : Matrix m n α) (i : m) (j : m)
---   : swapRow M i j j = M i := by
---   rw [swapRow, dupRow]
---   by_cases sameRow : j = i
---   rw [sameRow, Matrix.updateRow_self]
---   rw [Matrix.updateRow_self]
+-- If you swap Row i and j then Row j will be the previous Row i
+lemma swapRow_eq_right (M : Matrix m n α) (i : m) (j : m) :
+    swapRow M i j j = M i := by
+  rw [swapRow, dupRow]
+  by_cases sameRow : j = i
+  rw [sameRow, Matrix.updateRow_self]
+  rw [Matrix.updateRow_self]
 
--- --If you swap Rows i and j then all other rows stay the same
--- lemma swapRownotij [DecidableEq m](M : Matrix m n α) (i : m) (j : m) (p : m)
- --(h1 : p ≠ i) (h2 : p ≠ j)
---   : swapRow M i j p = M p := by
---   rw [swapRow, dupRow]
---   rw [Matrix.updateRow_ne, Matrix.updateRow_ne]
---   repeat assumption
+--If you swap Rows i and j then all other rows stay the same
+lemma swapRow_other_rows_same (M : Matrix m n α) (i : m) (j : m) (k : m) (h1 : k ≠ i) (h2 : k ≠ j) :
+    swapRow M i j k = M k := by
+  rw [swapRow, dupRow]
+  rw [Matrix.updateRow_ne, Matrix.updateRow_ne]
+  repeat assumption
 
--- -- The order of the parameters in swapRow does not matter
--- lemma swapRowijEqji [DecidableEq m](M : Matrix m n α) (i : m) (j : m)
---   : swapRow M i j = swapRow M j i := by
---   rw [← Matrix.ext_iff]
---   intro p q
---   by_cases peqi : p = i
---   rw [peqi, swapRowj, swapRowi]
---   by_cases peqj : p = j
---   rw [peqj, swapRowi, swapRowj]
---   rw [swapRownotij, swapRownotij]
---   repeat assumption
+-- The order of the parameters in swapRow does not matter
+lemma swapRow_comm (M : Matrix m n α) (i : m) (j : m) :
+    swapRow M i j = swapRow M j i := by
+  rw [← Matrix.ext_iff]
+  intro p q
+  by_cases peqi : p = i
+  rw [peqi, swapRow_eq_right, swapRow_eq_left]
+  by_cases peqj : p = j
+  rw [peqj, swapRow_eq_left, swapRow_eq_right]
+  rw [swapRow_other_rows_same, swapRow_other_rows_same]
+  repeat assumption
 
--- -- Calling swapRow twice gives you the original matrix back
--- theorem swapSwapEq [DecidableEq m] (M : Matrix m n α) (i : m) (j : m)
---   : swapRow (swapRow M i j) i j = M := by
---   rw [← Matrix.ext_iff]
---   intro p q
---   by_cases peqi : p = i
---   rw [peqi, swapRowi, swapRowj]
---   by_cases peqj : p = j
---   rw [peqj, swapRowj, swapRowi]
---   rw [swapRownotij, swapRownotij]
---   repeat assumption
+-- Calling swapRow twice gives you the original matrix back
+theorem swapRow_involutive (M : Matrix m n α) (i : m) (j : m)
+  : swapRow (swapRow M i j) i j = M := by
+  rw [← Matrix.ext_iff]
+  intro p q
+  by_cases peqi : p = i
+  rw [peqi, swapRow_eq_left, swapRow_eq_right]
+  by_cases peqj : p = j
+  rw [peqj, swapRow_eq_right, swapRow_eq_left]
+  rw [swapRow_other_rows_same, swapRow_other_rows_same]
+  repeat assumption
+
+--   Multiplying elemMatSwap i j by M is the same as swapping rows i and j of M
+theorem swapRow_id_mul_mat_eq_swapRow
+  [Fintype m] [Fintype n] [DivisionRing α]
+  (M : Matrix m m α) (i : m) (j : m)
+  : swapRow (1 : Matrix m m α) i j * M = swapRow M i j := by
+  ext p q
+  by_cases peqi : p = i
+  rw [peqi, swapRow_eq_left]
+  rw [Matrix.mul_apply, swapRow_eq_left]
+  simp_rw [Matrix.one_apply]
+  simp
+  by_cases peqj : p = j
+  rw [peqj, swapRow_eq_right, Matrix.mul_apply, swapRow_eq_right]
+  simp_rw [Matrix.one_apply]
+  simp
+  rw [swapRow_other_rows_same, Matrix.mul_apply, swapRow_other_rows_same]
+  simp_rw [Matrix.one_apply]
+  simp
+  exact peqi
+  exact peqj
+  exact peqi
+  exact peqj
 
 -- -- elemMatSwap i j is the elementary matrix formed by swapping rows i and j in the identity
 -- def elemMatSwap [DecidableEq n] [One α] [Zero α] (i : n) (j : n) : Matrix n n α :=
 --   swapRow (1 : Matrix n n α) i j
 
--- --   Multiplying elemMatSwap i j by M is the same as swapping rows i and j of M
--- theorem swapMatEqSwap [DecidableEq n]
---   [Fintype n] [Fintype m]
---   (M : Matrix n m γ) (i : n) (j : n)
---   : (elemMatSwap i j : Matrix n n γ) * M = swapRow M i j := by
---   rw [elemMatSwap]
---   ext p q
---   by_cases peqi : p = i
---   rw [peqi, swapRowi]
---   rw [Matrix.mul_apply, swapRowi]
---   simp_rw [Matrix.one_apply]
---   simp
---   by_cases peqj : p = j
---   rw [peqj, swapRowj, Matrix.mul_apply, swapRowj]
---   simp_rw [Matrix.one_apply]
---   simp
---   rw [swapRownotij, Matrix.mul_apply, swapRownotij]
---   simp_rw [Matrix.one_apply]
---   simp
---   exact peqi
---   exact peqj
---   exact peqi
---   exact peqj
-
--- -- THE FOLLOWING 3 DO NOT EXIST FOR THE OTHER ONES
 -- -- elemMatSwap is its own inverse
 -- theorem swapMatOwnInv [DecidableEq n]
 --   [Fintype n]
 --   (i : n) (j : n)
---   : (elemMatSwap i j : Matrix n n γ) * (elemMatSwap i j : Matrix n n γ) =
--- (1 : Matrix n n γ) := by
+--   : (elemMatSwap i j : Matrix n n γ) * (elemMatSwap i j : Matrix n n γ)
+-- = (1 : Matrix n n γ) := by
 --   rw [swapMatEqSwap, elemMatSwap, swapSwapEq]
 
 -- -- if M * N = P
 -- --   then this will still be true if the rows of M and P are swapped consistently
 -- -- This theorem will be used to show that solutions are preserved by swapping rows
 -- -- When we use it, N and P will only have one column
--- theorem swapImpliesSwapMat [DecidableEq m]
---   [Fintype n] [Fintype m] [Fintype p]
---   (M : Matrix m n γ) (N : Matrix n p γ) (P : Matrix m p γ) (i : m) (j : m) (h : M * N = P)
+-- theorem swapImpliesSwapMat [Fintype n] [Fintype m] [DivisionRing α]
+--   (M : Matrix m n α) (N : Matrix m n α) (P : Matrix m n α) (i : m) (j : m) (h : M * N = P)
 --   : swapRow M i j * N = swapRow P i j := by
---   rw [← swapMatEqSwap, ← swapMatEqSwap, Matrix.mul_assoc, h]
+--   rw [← swapRow_id_mul_mat_eq_swapRow, ← swapRow_id_mul_mat_eq_swapRow, Matrix.mul_assoc, h]
 
 -- -- This is the companion to the above theorem
 -- -- The above theorem shows that all are still there after the row operation
 -- -- The below theorem shows that no new solutions are added
 -- theorem swapMatImpliesSwap [DecidableEq m]
 --   [Fintype m] [Fintype n] [Fintype p]
---   (M : Matre m] [Fintype n] [Fintype p]
 --   (M : Matrix m n γ) (N : Matrix n p γ) (P : Matrix m p γ) (i : m) (j : m)
---   (h : swapRow M i j * N = swapRow P i j)
---   : M * N = P := by
---   rw [← swapMatEqSwapOld P i j] at h
---   rw [← swapSwapEq M i j]
---   rw [← swapMatEqSwapOld]
---   rw [Matrix.mul_assoc, h]
---   rw [← Matrix.mul_assoc, swapMatOwnInvOld]
---   simpix m n γ) (N : Matrix n p γ) (P : Matrix m p γ) (i : m) (j : m)
 --   (h : swapRow M i j * N = swapRow P i j)
 --   : M * N = P := by
 --   rw [← swapMatEqSwap P i j] at h
@@ -240,11 +227,10 @@ theorem mulRow_mulRow_inv_cancel_left [GroupWithZero R] [MulAction R α] (M : Ma
     exact hx
   · repeat rw [updateRow_ne h]
 
--- CHANGE VARIABLE NAMES MAYBE (j is a column here i think?)
 @[simp]
 theorem mulRow_inv_cancel [DecidableEq α] [DivisionRing α] {y : ℕ} {z : ℕ}
-    (M : Matrix (Fin y) (Fin z) α) (i : Fin y) (j : Fin z) (h : M i j ≠ 0) :
-    (mulRow M i (1/(M i j))) i j = 1 := by
+    (M : Matrix (Fin y) (Fin z) α) (i : Fin y) (l : Fin z) (h : M i l ≠ 0) :
+    (mulRow M i (1/(M i l))) i l = 1 := by
   unfold mulRow
   simp
   rw [inv_mul_cancel₀]
@@ -253,7 +239,7 @@ theorem mulRow_inv_cancel [DecidableEq α] [DivisionRing α] {y : ℕ} {z : ℕ}
 -- Let Eix by the elementary matrix formed by multiplying Row i of the identity matrix by x
 -- Here we show that multiplying Eix by M is the same as multiplying Row i of M by x
 @[simp]
-theorem mulRow_id_mul_mat_eq_mulRow_mat [Fintype m] [Fintype n] (M : Matrix m n ℝ)
+theorem mulRow_id_mul_mat_eq_mulRow [Fintype m] [Fintype n] (M : Matrix m n ℝ)
     (i : m) (x : ℝ) :
     mulRow (1 : Matrix m m ℝ) i x * M = mulRow M i x := by
   ext k l
@@ -278,8 +264,7 @@ section addMulRow
 def addMulRow [SMul R α] [Add α] (M : Matrix m n α) (i : m) (j : m) (x : R): Matrix m n α :=
   updateRow M i (M i + x • M j)
 
--- If you add a multiple of Row j into Row i, then it will be the original i plus the
--- multiple of Row j
+-- If you add a multiple of Row j into Row i, then it will be the original i + the multiple of Row j
 @[simp]
 lemma addMulRow_eq_add_mul_row [SMul R α] [Add α] (M : Matrix m n α) (i : m) (j : m) (x : R) :
     addMulRow M i j x i = M i + x • M j := by
@@ -296,8 +281,8 @@ lemma addMulRow_other_rows_same [SMul R α] [Add α] (M : Matrix m n α) (i : m)
   rw [addMulRow]
   rw [updateRow_ne h1]
 
--- If you add a multiple of Row j into Row i, then substract the same multiple of Row j
--- from Row will get the original matrix back
+-- If you add a multiple of Row j into Row i, then substract the same multiple
+-- of Row j from Row will get the original matrix back
 @[simp]
 theorem addMulRow_addMulRow_neg_cancel [Ring R] [AddCommGroup α] [Module R α] (M : Matrix m n α)
     (i : m) (j : m) (h1 : j ≠ i) (x : R) :
@@ -313,11 +298,22 @@ theorem addMulRow_addMulRow_neg_cancel [Ring R] [AddCommGroup α] [Module R α] 
   · rw [updateRow_ne h]
     rw [updateRow_ne h]
 
--- Let Eijx by the elementary matrix formed by adding a multiple of Row j to
--- Row i of the identity matrix
+-- k is the row where the value will become
+@[simp]
+theorem addMulRow_inv_cancel [DecidableEq α] [DivisionRing α] {y : ℕ} {z : ℕ}
+    (M : Matrix (Fin y) (Fin z) α) (i : Fin y) (j : Fin y) (l : Fin z) (h : M j l ≠ 0) :
+    (addMulRow M i j (-(M i l)*(1/(M j l)))) i l = 0 := by
+  unfold addMulRow
+  simp
+  rw [mul_assoc]
+  rw [inv_mul_cancel₀]
+  · simp
+  · exact h
+
+-- Let Eijx by the elementary matrix formed by adding a multiple of Row j to Row i of id
 -- Here we show that multiplying Eijx by M is the same as adding a multiple of Row j into Row i of M
 @[simp]
-theorem addMulRow_id_mul_eq_addMulRow_mat [Fintype m] [Fintype n]
+theorem addMulRow_id_mul_eq_addMulRow [Fintype m] [Fintype n]
     (M : Matrix m n ℝ) (i : m) (j : m) (x : ℝ) :
     addMulRow (1 : Matrix m m ℝ) i j x * M = addMulRow M i j x := by
   ext k l
@@ -338,28 +334,6 @@ theorem addMulRow_id_mul_eq_addMulRow_mat [Fintype m] [Fintype n]
     exact h
     exact h
 
--- k is the row where the value will become
-@[simp]
-theorem addMulRow_inv_cancel [DecidableEq α] [DivisionRing α] {y : ℕ} {z : ℕ}
-    (M : Matrix (Fin y) (Fin z) α) (i : Fin y) (j : Fin z) (k : Fin y) (h : M i j ≠ 0) :
-    (addMulRow M k i (-(M k j)*(1/(M i j)))) k j = 0 := by
-  unfold addMulRow
-  simp
-  rw [mul_assoc]
-  rw [inv_mul_cancel₀]
-  · simp
-  · exact h
-
 end addMulRow
 
 end Matrix
-
--- -- If you replace a row by the same row, the matrix is unchanged
--- lemma replaceSame [DecidableEq m](M : Matrix m n α) (i : m) :
- -- Matrix.updateRow M i (M i) = M := by
---   rw [← Matrix.ext_iff]
---   intros p q
---   by_cases peqi : p = i
---   rw [peqi, Matrix.updateRow_self]
---   rw [Matrix.updateRow_ne]
---   exact peqi
