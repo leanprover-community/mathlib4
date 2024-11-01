c80d8e2893bb8b7a905bc154657c4344bfadd228
Anne C.A. Baanen <vierkantor@vierkantor.com>
2024-11-01 14:04:10 +0100

    chore(RingTheory/NonUnitalSubsemiring): split `Basic.lean`
    This PR splits off a `Defs.lean` file from `NonUnitalSubsemiring/Basic.lean` containing on the definition and non unital subsemiring structure of `NonUnitalSubSemiring`(`Class`).

--- Mathlib.lean
+++ Mathlib.lean
@@ -4182,6 +4182,7 @@ import Mathlib.RingTheory.Nilpotent.Lemmas
 import Mathlib.RingTheory.Noetherian
 import Mathlib.RingTheory.NonUnitalSubring.Basic
 import Mathlib.RingTheory.NonUnitalSubsemiring.Basic
+import Mathlib.RingTheory.NonUnitalSubsemiring.Defs
 import Mathlib.RingTheory.Norm.Basic
 import Mathlib.RingTheory.Norm.Defs
 import Mathlib.RingTheory.NormTrace
