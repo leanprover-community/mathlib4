ed4fcb29ec6cb69642ecb366c37759eb6b675f29
Violeta Hernández <vi.hdz.p@gmail.com>
2024-11-02 16:09:28 -0600

    rename to IsCofinal

--- Mathlib/Order/Cofinal.lean
+++ Mathlib/Order/Cofinal.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Violeta Hernández Palacios
 -/
 import Mathlib.Data.Set.Basic
+import Mathlib.Tactic.Contrapose
 import Mathlib.Order.Bounds.Defs
 
 /-!
@@ -28,7 +29,7 @@ theorem isCofinal_of_isEmpty [IsEmpty α] (s : Set α) : IsCofinal s :=
   fun a ↦ isEmptyElim a
 
 theorem isCofinal_empty_iff : IsCofinal (∅ : Set α) ↔ IsEmpty α := by
-  refine ⟨fun h ↦ ⟨fun a ↦ ?_⟩, fun h ↦ cofinal_of_isEmpty _⟩
+  refine ⟨fun h ↦ ⟨fun a ↦ ?_⟩, fun h ↦ isCofinal_of_isEmpty _⟩
   simpa using h a
 
 theorem isCofinal_top [OrderTop α] : IsCofinal {(⊤ : α)} :=
@@ -64,40 +65,40 @@ theorem IsCofinal.isMax_mem {s : Set α} {a : α} (ha : IsMax a) (hs : IsCofinal
   obtain ⟨b, hb, hb'⟩ := hs a
   rwa [ha.eq_of_ge hb'] at hb
 
-theorem IsCofinal.top_mem [OrderTop α] {s : Set α} (hs : Cofinal s) : ⊤ ∈ s :=
-  hs.mem_isMax isMax_top
+theorem IsCofinal.top_mem [OrderTop α] {s : Set α} (hs : IsCofinal s) : ⊤ ∈ s :=
+  hs.isMax_mem isMax_top
 
 @[simp]
-theorem isCofinal_iff_of_orderTop [OrderTop α] {s : Set α} : Cofinal s ↔ ⊤ ∈ s :=
-  ⟨top_mem_cofinal, fun hs _ ↦ ⟨⊤, hs, le_top⟩⟩
+theorem isCofinal_iff_of_orderTop [OrderTop α] {s : Set α} : IsCofinal s ↔ ⊤ ∈ s :=
+  ⟨IsCofinal.top_mem, fun hs _ ↦ ⟨⊤, hs, le_top⟩⟩
 
 end PartialOrder
 
 section LinearOrder
 variable [LinearOrder α]
 
-theorem not_cofinal_iff {s : Set α} : ¬ Cofinal s ↔ ∃ x, ∀ y ∈ s, y < x := by
-  simp [Cofinal]
+theorem not_isCofinal_iff {s : Set α} : ¬ IsCofinal s ↔ ∃ x, ∀ y ∈ s, y < x := by
+  simp [IsCofinal]
 
-theorem bddAbove_of_not_cofinal {s : Set α} (h : ¬ Cofinal s) : BddAbove s := by
-  rw [not_cofinal_iff] at h
+theorem bddAbove_of_not_isCofinal {s : Set α} (h : ¬ IsCofinal s) : BddAbove s := by
+  rw [not_isCofinal_iff] at h
   obtain ⟨x, h⟩ := h
   exact ⟨x, fun y hy ↦ (h y hy).le⟩
 
-theorem cofinal_of_not_bddAbove {s : Set α} (h : ¬ BddAbove s) : Cofinal s := by
+theorem isCofinal_of_not_bddAbove {s : Set α} (h : ¬ BddAbove s) : IsCofinal s := by
   contrapose! h
-  exact bddAbove_of_not_cofinal h
+  exact bddAbove_of_not_isCofinal h
 
 /-- In a linear order with no maximum, cofinal sets are the same as unbounded sets. -/
-theorem not_cofinal_iff_bddAbove [NoMaxOrder α] {s : Set α} : ¬ Cofinal s ↔ BddAbove s := by
-  use bddAbove_of_not_cofinal
-  rw [not_cofinal_iff]
+theorem not_isCofinal_iff_bddAbove [NoMaxOrder α] {s : Set α} : ¬ IsCofinal s ↔ BddAbove s := by
+  use bddAbove_of_not_isCofinal
+  rw [not_isCofinal_iff]
   rintro ⟨x, h⟩
   obtain ⟨z, hz⟩ := exists_gt x
   exact ⟨z, fun y hy ↦ (h hy).trans_lt hz⟩
 
 /-- In a linear order with no maximum, cofinal sets are the same as unbounded sets. -/
-theorem not_bddAbove_iff_cofinal [NoMaxOrder α] {s : Set α} : ¬ BddAbove s ↔ Cofinal s :=
-  not_iff_comm.1 not_cofinal_iff_bddAbove
+theorem not_bddAbove_iff_isCofinal [NoMaxOrder α] {s : Set α} : ¬ BddAbove s ↔ IsCofinal s :=
+  not_iff_comm.1 not_isCofinal_iff_bddAbove
 
 end LinearOrder
