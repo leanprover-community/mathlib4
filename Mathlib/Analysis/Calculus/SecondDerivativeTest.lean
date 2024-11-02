598edec0c3431cae109b6830966005cd16753e28
Bjørn Kjos-Hanssen <bjoernkh@hawaii.edu>
2024-11-02 12:27:42 -1000

    remove uninteresting lemmas

--- Mathlib/Analysis/Calculus/SecondDerivativeTest.lean
+++ Mathlib/Analysis/Calculus/SecondDerivativeTest.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Bjørn Kjos-Hanssen
 -/
 import Mathlib.Analysis.Calculus.FirstDerivativeTest
-import Mathlib.Algebra.Order.GroupWithZero.Unbundled
+
 /-!
 # The Second-Derivative Test
 
@@ -22,52 +22,76 @@ derivative test, calculus
 
 open Set Filter Topology
 
-/-- Add to Mathlib/Algebra/Order/GroupWithZero/Unbundled.lean -/
-lemma neg_of_div_pos_left (a b : ℝ) (h : 0 < a/b) (h₁ : b < 0) : a < 0 := by
-  contrapose h
-  rw [not_lt] at h ⊢
-  exact div_nonpos_of_nonneg_of_nonpos h (le_of_lt h₁)
+/-- Insert a missing point between two adjacent open intervals. -/
+theorem insert_Ioo {x ε₀ ε₁ : ℝ} (hε₀ : ε₀ > 0) (hε₁ : ε₁ > 0):
+    insert x (Ioo (x - ε₀) x ∪ Ioo x (x + ε₁)) = Ioo (x - ε₀) (x + ε₁) := by
+  rw [← insert_union, Ioo_insert_right (by linarith)]
+  exact Ioc_union_Ioo_eq_Ioo (by linarith) (by linarith)
+
+
+section SecondDeriv
+
+variable {f : ℝ → ℝ} {x₀ : ℝ}
+
+/-- If the slope from a critical point `x₀` to `b > x₀` is positive then so is the derivative
+ at `b`. -/
+lemma slopeSimpPos {b : ℝ} (hb : x₀ < b) (hbf : b ∈ slope (deriv f) x₀ ⁻¹' Ioi 0)
+    (hf : deriv f x₀ = 0) : 0 < deriv f b := by
+  unfold slope at hbf
+  rw [hf] at hbf
+  simp_all
+
+/-- If the slope from a critical point `x₀` to `b < x₀` is positive then the derivative
+ at `b` is negative. -/
+lemma slopeSimpNeg {b : ℝ} (hb : b < x₀) (hbf : b ∈ slope (deriv f) x₀ ⁻¹' Ioi 0)
+    (hf : deriv f x₀ = 0) : deriv f b < 0 := by
+  unfold slope at hbf
+  simp_rw [smul_eq_mul, mem_preimage, hf] at hbf
+  rw [mul_comm] at hbf
+  have : 0 < deriv f b / (b - x₀) := sub_zero (deriv f b) ▸ hbf
+  contrapose this
+  exact not_lt.mpr <| div_nonpos_of_nonneg_of_nonpos (not_lt.mp this) (by linarith)
+
+/-- If the derivative is negative (positive) to the left (right) then the
+function is differentiable in a punctured neighborhood. -/
+theorem eventually_differentiable_of_deriv_nonzero {ε : ℝ}
+    (hε : ε > 0)
+    (hε₀ : ∀ b ∈ Ioo (x₀ - ε) x₀, deriv f b < 0)
+    (hε₁ : ∀ b ∈ Ioo x₀ (x₀ + ε), 0 < deriv f b) :
+    ∀ᶠ x in 𝓝[≠] x₀, DifferentiableAt ℝ f x :=
+  Eventually.mono
+    (eventually_mem_set.mpr <| insert_mem_nhds_iff.mp <| insert_Ioo hε hε ▸
+    Ioo_mem_nhds (by linarith) (by linarith))
+    fun _ hb => differentiableAt_of_deriv_ne_zero <| hb.elim
+      (fun h => ne_of_lt <| hε₀ _ h)
+      (fun h => ne_of_gt <| hε₁ _ h)
+
 
 /-- If `f''(x) > 0` then `f' < 0` on an interval to the left of `x`. -/
-lemma deriv_neg_of_deriv_deriv_pos {f : ℝ → ℝ}  {x₀ : ℝ} (hf : deriv (deriv f) x₀ > 0)
+lemma deriv_neg_of_deriv_deriv_pos (hf : deriv (deriv f) x₀ > 0)
     (hd : deriv f x₀ = 0) : ∃ u < x₀, ∀ b ∈ Ioo u x₀, deriv f b < 0 := by
-    obtain ⟨u,hu⟩ := (mem_nhdsWithin_Iio_iff_exists_mem_Ico_Ioo_subset
-      (show x₀ - 1 < x₀ by simp)).mp
-        <| nhds_left'_le_nhds_ne x₀
-        <| (tendsto_nhds.mp
-          <| hasDerivAt_iff_tendsto_slope.mp
-          <| hasDerivAt_deriv_iff.mpr (differentiableAt_of_deriv_ne_zero <| ne_of_gt hf))
-        (Set.Ioi 0) isOpen_Ioi hf
-    unfold slope at hu
-    rw [hd] at hu
-    have G₁ : ∀ b ∈ Ioo u x₀, deriv f b < 0 := fun b hb => neg_of_div_pos_left _ _ (by
-        have hub := hu.2 hb
-        field_simp at hub
-        exact hub
-      ) (by aesop)
-    exact ⟨u, hu.1.2, G₁⟩
+  obtain ⟨u,hu⟩ := (mem_nhdsWithin_Iio_iff_exists_mem_Ico_Ioo_subset
+    (show x₀ - 1 < x₀ by simp)).mp
+      <| nhds_left'_le_nhds_ne x₀ <| (tendsto_nhds.mp <| hasDerivAt_iff_tendsto_slope.mp
+        <| hasDerivAt_deriv_iff.mpr (differentiableAt_of_deriv_ne_zero <| ne_of_gt hf))
+      (Set.Ioi 0) isOpen_Ioi hf
+  exact ⟨u, hu.1.2, fun b hb => slopeSimpNeg hb.2 (hu.2 hb) hd⟩
+
 
 /-- If `f''(x) > 0` then `f' > 0` on an interval to the right of `x`. -/
-lemma deriv_pos_of_deriv_deriv_pos {f : ℝ → ℝ}  {x₀ : ℝ} (hf : deriv (deriv f) x₀ > 0)
+lemma deriv_pos_of_deriv_deriv_pos (hf : deriv (deriv f) x₀ > 0)
     (hd : deriv f x₀ = 0) : ∃ u > x₀, ∀ b ∈ Ioo x₀ u, deriv f b > 0 := by
   obtain ⟨u,hu⟩ := (mem_nhdsWithin_Ioi_iff_exists_mem_Ioc_Ioo_subset (show x₀ < x₀ + 1 by simp)).mp
-      <| nhds_right'_le_nhds_ne x₀
-      <|(tendsto_nhds.mp <| hasDerivAt_iff_tendsto_slope.mp
-        <| hasDerivAt_deriv_iff.mpr (differentiableAt_of_deriv_ne_zero <| ne_of_gt hf))
-        (Set.Ioi 0) isOpen_Ioi hf
-  unfold slope at hu
-  rw [hd] at hu
-  have G₀ : ∀ b ∈ Ioo x₀ u, deriv f b > 0 := fun b hb => by
-    have := hu.2 hb
-    simp only [vsub_eq_sub, sub_zero, smul_eq_mul, mem_preimage, mem_Ioi] at this
-    exact pos_of_mul_pos_right this <|le_of_lt (by aesop)
-  exact ⟨u, hu.1.1, G₀⟩
+    <| nhds_right'_le_nhds_ne x₀ <|(tendsto_nhds.mp <| hasDerivAt_iff_tendsto_slope.mp
+    <| hasDerivAt_deriv_iff.mpr (differentiableAt_of_deriv_ne_zero <| ne_of_gt hf))
+    (Set.Ioi 0) isOpen_Ioi hf
+  exact ⟨u, hu.1.1, fun b hb => slopeSimpPos hb.1 (hu.2 hb) hd⟩
 
 /-- If `f''(x) > 0` then `f'` changes sign at `x`.
 This lemma applies to functions like `x^2 + 1[x ≥ 0]` as well as twice differentiable
 functions.
 -/
-lemma deriv_neg_pos_of_deriv_deriv_pos {f : ℝ → ℝ}  {x₀ : ℝ}
+lemma deriv_neg_pos_of_deriv_deriv_pos
     (hf : deriv (deriv f) x₀ > 0) (hd : deriv f x₀ = 0) :
     ∃ ε > 0, (∀ b ∈ Ioo (x₀-ε) x₀, deriv f b < 0) ∧
               ∀ b ∈ Ioo x₀ (x₀ + ε), 0 < deriv f b := by
@@ -84,75 +108,37 @@ lemma deriv_neg_pos_of_deriv_deriv_pos {f : ℝ → ℝ}  {x₀ : ℝ}
   · aesop
   · constructor
     · exact fun b hb => hu₁.2 _ <| by
-        simp_all only [mem_Ioo, and_imp, sub_sub_cancel, Nat.ofNat_pos, mul_lt_mul_left, and_true]
+        simp_all only [mem_Ioo, sub_sub_cancel, and_true]
         calc u₁ < _ := h₁
              _  ≤ _ := tsub_le_tsub_left ((inv_mul_le_iff₀ zero_lt_two).mpr (by simp)) x₀
              _  < _ := hb.1
-    · exact fun b hb => hu₀.2 _ <| ⟨hb.1,
+    · exact fun b hb => hu₀.2 _ ⟨hb.1,
         calc _ < _                    := hb.2
              _ ≤ x₀ + 2⁻¹ * (u₀ - x₀) := by simp
              _ < _                    := by rw[← mul_lt_mul_left zero_lt_two]; exact h₂⟩
 
-theorem differentiableOn_right {f : ℝ → ℝ} {x₀ : ℝ}
-    {p : Set ℝ} (hp2 : ∃ b ∈ 𝓟 {x₀}ᶜ, {x | DifferentiableAt ℝ f x} = p ∩ b)
-    {l u : ℝ} (hlu1 : x₀ ∈ Ioo l u) (hlu2: Ioo l u ⊆ p) :
-    DifferentiableOn ℝ f (Ioo x₀ u) := fun x hx => DifferentiableAt.differentiableWithinAt <| by
-  obtain ⟨b,hb⟩ := hp2
-  have : x ∈ p ∩ b := ⟨hlu2 <|by
-      simp_all only [mem_Ioo, mem_principal, and_true]
-      exact lt_trans hlu1.1 hx.1, hb.1 <| ne_of_gt hx.1⟩
-  rw [← hb.2] at this
-  exact this
-
-theorem differentiableOn_left {f : ℝ → ℝ} {x₀ : ℝ}
-    {p : Set ℝ} (hp2 : ∃ b ∈ 𝓟 {x₀}ᶜ, {x | DifferentiableAt ℝ f x} = p ∩ b)
-    {l u : ℝ} (hlu1 : x₀ ∈ Ioo l u) (hlu2: Ioo l u ⊆ p) :
-    DifferentiableOn ℝ f (Ioo l x₀) := fun x hx => DifferentiableAt.differentiableWithinAt <| by
-  obtain ⟨b,hb⟩ := hp2
-  have : x ∈ p ∩ b := ⟨hlu2 <|by
-      simp_all only [mem_Ioo, mem_principal, true_and]
-      exact lt_trans hx.2 hlu1.2, hb.1 <| ne_of_lt hx.2⟩
-  rw [← hb.2] at this
-  exact this
-
-/-- Insert a missing point between two adjacent open intervals. -/
-theorem insert_Ioo {x ε₀ ε₁ : ℝ} (hε₀ : ε₀ > 0) (hε₁ : ε₁ > 0):
-    insert x (Ioo (x - ε₀) x ∪ Ioo x (x + ε₁)) = Ioo (x - ε₀) (x + ε₁) := by
-  rw [← insert_union, Ioo_insert_right (by linarith)]
-  exact Ioc_union_Ioo_eq_Ioo (by linarith) (by linarith)
-
-/-- Already in FirstDerivativeTest? -/
-theorem eventually_differentiable_of_deriv_nonzero {f : ℝ → ℝ} {x₀ ε : ℝ}
-    (hε : ε > 0 ∧ (∀ b ∈ Ioo (x₀ - ε) x₀, deriv f b < 0)
-                ∧ ∀ b ∈ Ioo x₀ (x₀ + ε), 0 < deriv f b) :
-    ∀ᶠ x in 𝓝[≠] x₀, DifferentiableAt ℝ f x :=
-  Eventually.mono
-    (eventually_mem_set.mpr <| insert_mem_nhds_iff.mp <| insert_Ioo hε.1 hε.1 ▸
-    Ioo_mem_nhds (by linarith) (by linarith))
-    fun _ hb => differentiableAt_of_deriv_ne_zero <| hb.elim
-      (fun h => ne_of_lt <| hε.2.1 _ h)
-      (fun h => ne_of_gt <| hε.2.2 _ h)
 
 /-- The Second-Derivative Test from calculus. -/
-theorem isLocalMin_of_deriv_deriv_pos {f : ℝ → ℝ}  {x₀ : ℝ}
+theorem isLocalMin_of_deriv_deriv_pos
     (hf : deriv (deriv f) x₀ > 0) (hd : deriv f x₀ = 0)
     (hc : ContinuousAt f x₀) : IsLocalMin f x₀ := by
   obtain ⟨ε,hε⟩    := deriv_neg_pos_of_deriv_deriv_pos hf hd
-  obtain ⟨p,hp⟩    := eventually_differentiable_of_deriv_nonzero hε
+  obtain ⟨p,hp⟩    := eventually_differentiable_of_deriv_nonzero hε.1 hε.2.1 hε.2.2
   obtain ⟨l,u,hlu⟩ := mem_nhds_iff_exists_Ioo_subset.mp hp.1
   let δ := min (x₀ - l) (u - x₀)
   have hζ : (1/2) * min δ ε > 0 := by aesop
   have hζ₀ : x₀ - (1/2) * min δ ε < x₀ := by linarith
   have hζ₁ : x₀ < x₀ + (1/2) * min δ ε := by linarith
-  have hεζ : x₀ - ε ≤ x₀ - (1/2) * min δ ε := by
-    suffices x₀ ≤ x₀ + (1/2) * (ε - min δ ε) by linarith
-    aesop
-  have hu :  x₀ + (1/2) * min δ ε ≤ u := by
-    linarith[min_le_left δ ε, min_le_right (x₀ - l) (u - x₀), hlu.1.2]
-  have hl : l ≤ x₀ - (1/2) * min δ ε := by
-    linarith[min_le_left δ ε, min_le_left (x₀ - l) (u - x₀), hlu.1.1]
-  exact isLocalMin_of_deriv_Ioo hζ₀ hζ₁ hc
-    ((differentiableOn_left hp.2 hlu.1 hlu.2).mono <| Ioo_subset_Ioo_left <| by linarith)
-    ((differentiableOn_right hp.2 hlu.1 hlu.2).mono <| Ioo_subset_Ioo_right <| by linarith)
-    (fun x hx => le_of_lt <| hε.2.1 _ ⟨by simp only [mem_Ioo] at hx;linarith, hx.2⟩)
-    (fun x hx => le_of_lt <| hε.2.2 _ ⟨hx.1, by simp only [mem_Ioo] at hx;linarith⟩)
+  have : x₀ ≤ x₀ + (1/2) * (ε - min δ ε) := by aesop
+  obtain ⟨b,hb⟩ := hp.2
+  apply isLocalMin_of_deriv_Ioo hζ₀ hζ₁ hc
+  · exact fun x hx => DifferentiableAt.differentiableWithinAt <| hb.2.symm.subset
+      ⟨hlu.2 ⟨lt_trans (by linarith[min_le_left δ ε, min_le_left (x₀ - l) (u - x₀)]) hx.1,
+        lt_trans hx.2 hlu.1.2⟩, hb.1 <| ne_of_lt hx.2⟩
+  · exact fun x hx => DifferentiableAt.differentiableWithinAt <| hb.2.symm.subset
+      ⟨hlu.2 ⟨lt_trans hlu.1.1 hx.1, lt_trans hx.2 (by
+        linarith[min_le_left δ ε, min_le_right (x₀ - l) (u - x₀)])⟩, hb.1 <| ne_of_gt hx.1⟩
+  · exact (fun x hx => le_of_lt <| hε.2.1 _ ⟨by simp only [mem_Ioo] at hx;linarith, hx.2⟩)
+  · exact (fun x hx => le_of_lt <| hε.2.2 _ ⟨hx.1, by simp only [mem_Ioo] at hx;linarith⟩)
+
+end SecondDeriv
