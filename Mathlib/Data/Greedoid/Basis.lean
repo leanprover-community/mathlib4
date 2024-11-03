650a74a3349615849a25a0d7b628d01d71396886
qawbecrdtey <qawbecrdtey@kaist.ac.kr>
2024-11-03 10:57:21 +0900

    Fix due to renaming.

--- Mathlib/Data/Greedoid/Basis.lean
+++ Mathlib/Data/Greedoid/Basis.lean
@@ -7,10 +7,10 @@ namespace Greedoid
 variable {α : Type*}
 
 def Bases (G : Greedoid α) : Finset α → Prop := fun s ↦
-  Maximal (fun t ↦ t ∈ G ∧ t ⊆ G.ground_set) s
+  Maximal (fun t ↦ t ∈ G ∧ t ⊆ G.groundSet) s
 
 def Basis (G : Greedoid α) (s : Finset α) : Finset α → Prop := fun b ↦
-  s ⊆ G.ground_set ∧ Maximal (fun t ↦ t ∈ G ∧ t ⊆ s) b
+  s ⊆ G.groundSet ∧ Maximal (fun t ↦ t ∈ G ∧ t ⊆ s) b
 
 def Basis' (G : Greedoid α) (s : Finset α) : Finset α → Prop := fun b ↦
   Maximal (fun t ↦ t ∈ G ∧ t ⊆ s) b
@@ -19,10 +19,10 @@ section Basis
 
 variable {G : Greedoid α} {s b : Finset α}
 
-theorem bases_eq_basis'_ground : G.Bases = G.Basis' G.ground_set := by
+theorem bases_eq_basis'_ground : G.Bases = G.Basis' G.groundSet := by
   funext x; simp only [Bases, Basis']
 
-theorem bases_eq_basis_ground : G.Bases = G.Basis G.ground_set := by
+theorem bases_eq_basis_ground : G.Bases = G.Basis G.groundSet := by
   funext x; simp [Bases, Basis]
 
 theorem bases_feasible (hb : G.Bases b) : b ∈ G :=
@@ -34,10 +34,10 @@ theorem basis'_feasible (hb : G.Basis s b) : b ∈ G :=
 theorem basis_feasible (hb : G.Basis s b) : b ∈ G :=
   hb.2.prop.1
 
-theorem bases_subset_ground (hb : G.Bases b) : b ⊆ G.ground_set :=
+theorem bases_subset_ground (hb : G.Bases b) : b ⊆ G.groundSet :=
   hb.prop.2
 
-theorem basis'_subset_ground (hb : G.Basis' s b) : b ⊆ G.ground_set :=
+theorem basis'_subset_ground (hb : G.Basis' s b) : b ⊆ G.groundSet :=
   hb.2.prop.1
 
 end Basis
