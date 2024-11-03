18710e6881da1389fc5e6fb00a8effe30ac964e9
Thomas Browning <tb65536@uw.edu>
2024-11-03 09:39:10 -0800

    use mk_iff

--- Mathlib/GroupTheory/SpecificGroups/ZGroup.lean
+++ Mathlib/GroupTheory/SpecificGroups/ZGroup.lean
@@ -23,13 +23,10 @@ and `G/G'` are cyclic of coprime orders.
 variable (G G' : Type*) [Group G] [Group G'] (f : G →* G')
 
 /-- A Z-group is a group whose Sylow subgroups are all cyclic. -/
-class IsZGroup : Prop where
+@[mk_iff] class IsZGroup : Prop where
   isZGroup : ∀ p : ℕ, p.Prime → ∀ P : Sylow p G, IsCyclic P
 
 variable {G G' f}
-
-theorem IsZGroup_iff : IsZGroup G ↔ ∀ p : ℕ, p.Prime → ∀ P : Sylow p G, IsCyclic P :=
-  ⟨fun h ↦ h.1, fun h ↦ ⟨h⟩⟩
 
 namespace IsZGroup
 
@@ -41,7 +38,7 @@ theorem of_squarefree (hG : Squarefree (Nat.card G)) : IsZGroup G := by
   exact isCyclic_of_card_dvd_prime ((hk ▸ hG.pow_dvd_of_pow_dvd) P.card_subgroup_dvd_card)
 
 theorem of_injective [hG' : IsZGroup G'] (hf : Function.Injective f) : IsZGroup G := by
-  rw [IsZGroup_iff] at hG' ⊢
+  rw [isZGroup_iff] at hG' ⊢
   intro p hp P
   obtain ⟨Q, hQ⟩ := P.exists_comap_eq_of_injective hf
   specialize hG' p hp Q
