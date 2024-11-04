5182df6ecaa770e67a9fe00082a58eaa700dfea4
Christian Merten <christian@merten.dev>
2024-11-04 15:06:32 +0100

    fix

--- Mathlib/Algebra/Category/Ring/Constructions.lean
+++ Mathlib/Algebra/Category/Ring/Constructions.lean
@@ -102,17 +102,6 @@ def pushoutCoconeIsColimit : Limits.IsColimit (pushoutCocone R A B) :=
     rw [← h.map_mul, Algebra.TensorProduct.tmul_mul_tmul, mul_one, one_mul]
     rfl
 
-lemma nontrivial_of_isPushout_of_isField {A B C D : CommRingCat.{u}}
-    (hA : IsField A) {f : A ⟶ B} {g : A ⟶ C} {inl : B ⟶ D} {inr : C ⟶ D}
-    [Nontrivial B] [Nontrivial C]
-    (h : IsPushout f g inl inr) : Nontrivial D := by
-  letI : Field A := hA.toField
-  algebraize [RingHomClass.toRingHom f, RingHomClass.toRingHom g]
-  let e : D ≅ .of (B ⊗[A] C) :=
-    IsColimit.coconePointUniqueUpToIso h.isColimit (CommRingCat.pushoutCoconeIsColimit A B C)
-  let e' : D ≃ B ⊗[A] C := e.commRingCatIsoToRingEquiv.toEquiv
-  apply e'.nontrivial
-
 end Pushout
 
 section Terminal
