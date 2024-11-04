e22a07c0246317e0c8dec3516a5f1b070b8d2f1e
Anne C.A. Baanen <vierkantor@vierkantor.com>
2024-11-04 16:29:10 +0100

    chore(LinearAlgebra): split `Countable` / `Encodable` from `Finsupp.lean`

--- Mathlib/LinearAlgebra/Finsupp.lean
+++ Mathlib/LinearAlgebra/Finsupp.lean
@@ -3,10 +3,9 @@ Copyright (c) 2019 Johannes Hölzl. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl
 -/
-import Mathlib.Data.Finsupp.Encodable
+import Mathlib.Data.Finsupp.Basic
 import Mathlib.LinearAlgebra.Pi
 import Mathlib.LinearAlgebra.Span
-import Mathlib.Data.Set.Countable
 
 /-!
 # Properties of the module `α →₀ M`
@@ -1094,14 +1093,6 @@ theorem finsuppProdLEquiv_symm_apply {α β R M : Type*} [Semiring R] [AddCommMo
     rw [← (finsuppProdLEquiv R).apply_symm_apply f, finsuppProdLEquiv_apply]
 
 end Prod
-
-/-- If `R` is countable, then any `R`-submodule spanned by a countable family of vectors is
-countable. -/
-instance {ι : Type*} [Countable R] [Countable ι] (v : ι → M) :
-    Countable (Submodule.span R (Set.range v)) := by
-  refine Set.countable_coe_iff.mpr (Set.Countable.mono ?_ (Set.countable_range
-      (fun c : (ι →₀ R) => c.sum fun i _ => (c i) • v i)))
-  exact fun _ h => Finsupp.mem_span_range_iff_exists_finsupp.mp (SetLike.mem_coe.mp h)
 
 end Finsupp
 
