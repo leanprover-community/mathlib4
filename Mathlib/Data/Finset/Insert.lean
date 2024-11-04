8ad44cce641fd2c9a28617ce5fab7c7e295e1729
Anne C.A. Baanen <vierkantor@vierkantor.com>
2024-11-04 16:13:19 +0100

    Fix overly shaken import

--- Mathlib/Data/Finset/Insert.lean
+++ Mathlib/Data/Finset/Insert.lean
@@ -3,6 +3,7 @@ Copyright (c) 2015 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Leonardo de Moura, Jeremy Avigad, Minchao Wu, Mario Carneiro
 -/
+import Mathlib.Data.Finset.Attr
 import Mathlib.Data.Finset.Dedup
 import Mathlib.Data.Finset.Empty
 import Mathlib.Data.Multiset.FinsetOps
