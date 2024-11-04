5b41402879766e0aa95920daf4bd5687584905d3
Thomas Browning <tb65536@uw.edu>
2024-11-03 19:14:00 -0800

    fix

--- Mathlib/NumberTheory/ClassNumber/Finite.lean
+++ Mathlib/NumberTheory/ClassNumber/Finite.lean
@@ -235,6 +235,7 @@ theorem exists_mem_finsetApprox (a : S) {b} (hb : b ≠ (0 : R)) :
   · exact mod_cast ε_le
 
 /-- We can approximate `a / b : L` with `q / r`, where `r` has finitely many options for `L`. -/
+include L in
 theorem exists_mem_finset_approx' (a : S) {b : S} (hb : b ≠ 0) :
     ∃ q : S,
       ∃ r ∈ finsetApprox bS adm, abv (Algebra.norm R (r • a - q * b)) < abv (Algebra.norm R b) := by
