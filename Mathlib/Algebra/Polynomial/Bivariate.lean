7151d49c72dbf8c6b9864b7bb23b1d71ea8e4d56
Kim Morrison <kim@tqft.net>
2024-11-04 22:50:20 +1100

    shake

--- Mathlib/Algebra/Polynomial/Bivariate.lean
+++ Mathlib/Algebra/Polynomial/Bivariate.lean
@@ -3,7 +3,8 @@ Copyright (c) 2024 Junyan Xu. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Junyan Xu
 -/
-import Mathlib.RingTheory.AdjoinRoot.Basic
+import Mathlib.Algebra.Polynomial.AlgebraMap
+import Mathlib.RingTheory.AdjoinRoot.Defs
 
 /-!
 # Bivariate polynomials
