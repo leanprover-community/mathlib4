f6dc6abbea0c84b5db11d57466ce223a1f2406ed
tomaz1502 <tomgm1502@gmail.com>
2024-11-04 21:31:35 +0100

    Add import to insertion sort complexity

--- Mathlib.lean
+++ Mathlib.lean
@@ -2167,6 +2167,7 @@ import Mathlib.Computability.Reduce
 import Mathlib.Computability.RegularExpressions
 import Mathlib.Computability.TMComputable
 import Mathlib.Computability.TMToPartrec
+import Mathlib.Computability.Timed.InsertionSort
 import Mathlib.Computability.TuringMachine
 import Mathlib.Condensed.Basic
 import Mathlib.Condensed.CartesianClosed
