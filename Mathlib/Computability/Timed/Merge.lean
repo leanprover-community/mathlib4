5616ca3e153a2a973026f9267cd24a9455bd469c
tomaz1502 <tomgm1502@gmail.com>
2024-11-05 13:29:59 +0100

    Remove unused import

--- Mathlib/Computability/Timed/Merge.lean
+++ Mathlib/Computability/Timed/Merge.lean
@@ -3,7 +3,6 @@ Copyright (c) 2024 Tomaz Mascarenhas. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Tomaz Mascarenhas
 -/
-import Mathlib.Data.List.Sort
 import Mathlib.Tactic.Linarith
 /-!
 # Timed Merge
