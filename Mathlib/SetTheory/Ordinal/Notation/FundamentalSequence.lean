184f1cfb7b2addd9d54d708361ceb69e833821a1
Violeta Hernández <vi.hdz.p@gmail.com>
2024-11-05 00:19:53 -0600

    fix

--- Mathlib/SetTheory/Ordinal/Notation/FundamentalSequence.lean
+++ Mathlib/SetTheory/Ordinal/Notation/FundamentalSequence.lean
@@ -3,7 +3,6 @@ Copyright (c) 2024 Violeta Hernández Palacios. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Violeta Hernández Palacios
 -/
-import Batteries.Data.Sum.Lemmas
 import Mathlib.Data.Set.Operations
 
 /-!
