5665f025f9d95a10a0916c0d2e5ba2ca9cd11fbe
adomani <adomani@gmail.com>
2024-11-05 22:49:31 +0000

    add late_importers shell script

---
+++ scripts/late_importers.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+# `root` is the module name that the script builds
+root=${1:-Mathlib}
+
+# `lineLimit` is the number of module names of output that the script returns
+# a value of `0` means that all files are returned
+lineLimit=${2:-0}
+
+# `significantDifference` is the threshold difference in imports above which
+# the script reports the module
+significantDifference=${3:-0}
+
+baseURL='https://github.com/leanprover-community/mathlib4/commit'
+refCommit=${{ github.sha }}
+
+# build the selected target and collapse all `linter.minImports` warning to a single line per warning
+lake build "${root}" | sed -z 's=\n\n*\([^⚠w]\)= \1=g' |
+  # the `gsub`s clear all unnecessary text, leaving lines of the form
+  # `warning: [filename]:[line]:[column]:[importIncrease]: [newImports]`
+  # in awk-speak, $2=[filename], $3=[line], $5=[importIncrease], $6=[newImports]
+  awk -F: 'BEGIN{max=0}
+    # only print `currMax` when we reach the report for the next file (are we missing the last report?)
+    /^⚠/{ print currMax; max=0 }
+    (max < $3) {
+      gsub(/ *Now redun.*/, "")
+      gsub(/ to \[[^]]*\]/, "")
+      gsub(/ *note: this linter.*/, "")
+      gsub(/\.\//, "")
+      gsub(/ *Imports increased by */, "")
+      gsub(/ *New imports */, "")
+      currMax=$0
+      max=$3+0
+  }' |
+  # sort by decreasing line with the last import increase
+  sort --field-separator=: -k3 -nr |
+  # prepare the table in markdown format
+  awk -F: -v lineLimit="${lineLimit}" -v significantDifference="${significantDifference}" '
+    BEGIN{
+      lineLimit=lineLimit+0
+      significantDifference=significantDifference+0
+      printf("| File | Line | Import increase | New imports |\n| :- | -: | -: | :- |\n")
+      con=0
+  }
+    ((con <= lineLimit) && (significantDifference <= $5+0)) {
+      if (!(lineLimit == 0)) { con++ }
+      fileHtml=$2
+      gsub(/\.lean$/, ".html", fileHtml)
+      gsub(/ /, "", fileHtml)
+      printf("| [%s](https://leanprover-community.github.io/mathlib4_docs/%s) | %s | %s | %s |\n", $2, fileHtml, $3, $5, $6)
+  }'
+printf '\n\n---\n\nReference commit [%s](%s)\n' "${refCommit:0:10}"  "${baseURL}/${refCommit}"
+printf '[Full report](https://github.com/%s/actions/runs/%s)\n' "${{ github.repository }}" "${{ github.run_id }}"
