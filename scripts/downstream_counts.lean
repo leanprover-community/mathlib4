a19434c75e1a8a47d348d628764f5cb3a14fb3d9
adomani <adomani@gmail.com>
2024-11-05 22:49:55 +0000

    add downstream_counts lean script

---
+++ scripts/downstream_counts.lean
@@ -0,0 +1,16 @@
+import Mathlib
+
+open Lean
+
+def downstreamCount (importGraph : NameMap (Array Name)) (fileNames : Array Name) : Array Nat :=
+  fileNames.map fun fileName ↦
+    let ds := importGraph.downstreamOf {fileName}
+    let downDeps : NameSet := ds.fold (init := {}) fun a _ c => a.append (.ofArray c)
+    (downDeps.erase fileName).size
+
+abbrev fils : Array Name := #[
+  -- insert module names here
+]
+
+run_cmd
+  dbg_trace downstreamCount (← getEnv).importGraph fils
