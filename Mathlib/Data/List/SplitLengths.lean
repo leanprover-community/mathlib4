0b643bbc3a8bc6384ea367b2449f0e6b81d0c472
Daniel Weber <55664973+Command-Master@users.noreply.github.com>
2024-11-05 13:23:47 +0200

    Update SplitLengths.lean

--- Mathlib/Data/List/SplitLengths.lean
+++ Mathlib/Data/List/SplitLengths.lean
@@ -3,9 +3,8 @@ Copyright (c) 2024 Daniel Weber. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Daniel Weber
 -/
-import Mathlib.Data.List.Join
 import Mathlib.Order.MinMax
-import Mathlib.Algebra.BigOperators.Group.List
+import Mathlib.Algebra.Group.Nat
 
 /-!
 # Splitting a list to chunks of specified lengths
