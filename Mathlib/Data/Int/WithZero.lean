0fca88d5564a33decfa86e8b3ff90152d28aea02
Fabrizio Barroero <fbarroero@gmail.com>
2024-11-05 19:23:03 +0100

    feat(Data/Int/WithZero): add API
    ---
    <!-- The text above the `---` will become the commit message when your
    PR is merged. Please leave a blank newline before the `---`, otherwise
    GitHub will format the text above it as a title.
    
    For details on the "pull request lifecycle" in mathlib, please see:
    https://leanprover-community.github.io/contribute/index.html
    
    In particular, note that most reviewers will only notice your PR
    if it passes the continuous integration checks.
    Please ask for help on https://leanprover.zulipchat.com if needed.
    
    To indicate co-authors, include lines at the bottom of the commit message
    (that is, before the `---`) using the following format:
    
    Co-authored-by: Author Name <author@email.com>
    
    If you are moving or deleting declarations, please include these lines at the bottom of the commit message
    (that is, before the `---`) using the following format:
    
    Moves:
    - Vector.* -> Mathlib.Vector.*
    - ...
    
    Deletions:
    - Nat.bit1_add_bit1
    - ...
    
    Any other comments you want to keep out of the PR commit should go
    below the `---`, and placed outside this HTML comment, or else they
    will be invisible to reviewers.
    
    If this PR depends on other PRs, please list them below this comment,
    using the following format:
    - [ ] depends on: #abc [optional extra text]
    - [ ] depends on: #xyz [optional extra text]
    
    -->
    
    [![Open in Gitpod](https://gitpod.io/button/open-in-gitpod.svg)](https://gitpod.io/from-referrer/)

--- Mathlib/Data/Int/WithZero.lean
+++ Mathlib/Data/Int/WithZero.lean
@@ -88,4 +88,38 @@ theorem toNNReal_strictMono {e : NNReal} (he : 1 < e) :
       WithZero.coe_unzero hy]
     exact hxy
 
+theorem toNNReal_eq_one_iff {e : NNReal} (m : ℤₘ₀) (he0 : e ≠ 0) (he1 : e ≠ 1) :
+    toNNReal he0 m = 1 ↔ m = 1 := by
+  by_cases hm : m = 0
+  · subst hm
+    simp_all only [ne_eq, map_zero, zero_ne_one]
+  · constructor
+    · rw [toNNReal_neg_apply he0 hm]
+      intro h
+      rw [zpow_eq_one_iff_right₀ (zero_le e) he1] at h
+      rw [← WithZero.coe_unzero hm]
+      simp_all only [ne_eq, toAdd_eq_zero, coe_one]
+    · intro a
+      subst a
+      simp_all only [ne_eq, one_ne_zero, not_false_eq_true, map_one]
+
+theorem toNNReal_lt_one_iff {e : NNReal} {m : ℤₘ₀} (he : 1 < e) :
+    toNNReal (ne_zero_of_lt he) m < 1 ↔ m < 1 := by
+  have mono := toNNReal_strictMono he
+  have : 1 = (toNNReal (ne_zero_of_lt he)) 1 := rfl
+  simp_rw [this]
+  constructor
+  · contrapose!
+    exact fun h ↦ (StrictMono.le_iff_le mono).mpr h
+  · exact fun h ↦ mono h
+
+theorem toNNReal_le_one_iff {e : NNReal} {m : ℤₘ₀} (he : 1 < e) :
+    toNNReal (ne_zero_of_lt he) m ≤ 1 ↔ m ≤ 1 := by
+  have mono := toNNReal_strictMono he
+  constructor
+  · apply le_imp_le_of_lt_imp_lt
+    exact fun a ↦ LT.lt.trans_eq' (mono a) rfl
+  · apply le_imp_le_of_lt_imp_lt
+    exact fun h ↦ (StrictMono.lt_iff_lt mono).mp h
+
 end WithZeroMulInt
