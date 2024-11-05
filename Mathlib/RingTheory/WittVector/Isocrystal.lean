e438dd15fec71c0401f6b95b88f0076f7e69550d
acmepjz <acme_pjz@hotmail.com>
2024-11-05 22:30:08 +0800

    fix

--- Mathlib/RingTheory/WittVector/Isocrystal.lean
+++ Mathlib/RingTheory/WittVector/Isocrystal.lean
@@ -74,7 +74,7 @@ variable [IsDomain k] [CharP k p] [PerfectRing k p]
 
 /-- The Frobenius automorphism of `k` induces an automorphism of `K`. -/
 def FractionRing.frobenius : K(p, k) ≃+* K(p, k) :=
-  IsFractionRing.fieldEquivOfRingEquiv (frobeniusEquiv p k)
+  IsFractionRing.ringEquivOfRingEquiv (frobeniusEquiv p k)
 
 /-- The Frobenius automorphism of `k` induces an endomorphism of `K`. For notation purposes. -/
 def FractionRing.frobeniusRingHom : K(p, k) →+* K(p, k) :=
