3f60e83f739ae54465c1e56b99ddc5389bea8167
adomani <adomani@gmail.com>
2024-11-05 22:54:36 +0000

    summarize inputs

--- scripts/late_importers.sh
+++ scripts/late_importers.sh
@@ -11,6 +11,10 @@ lineLimit=${2:-0}
 # the script reports the module
 significantDifference=${3:-0}
 
+>&2 printf 'Building %s\n' "${root}"
+>&2 printf 'Report only the top %s exceptions\n' "${lineLimit}"
+>&2 printf 'Consider a file an exception if the last import increase exceeds %s imports\n\n' "${significantDifference}"
+
 baseURL='https://github.com/leanprover-community/mathlib4/commit'
 refCommit=${{ github.sha }}
 
