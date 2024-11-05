ea39db5375776350dd421a2247d43e7704efb719
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2024-11-06 00:24:07 +0100

    feat(RingTheory/RootsOfUnity/EnoughRootsOfUnity): new file

--- Mathlib.lean
+++ Mathlib.lean
@@ -4280,6 +4280,7 @@ import Mathlib.RingTheory.RingHomProperties
 import Mathlib.RingTheory.RingInvo
 import Mathlib.RingTheory.RootsOfUnity.Basic
 import Mathlib.RingTheory.RootsOfUnity.Complex
+import Mathlib.RingTheory.RootsOfUnity.EnoughRootsOfUnity
 import Mathlib.RingTheory.RootsOfUnity.Lemmas
 import Mathlib.RingTheory.RootsOfUnity.Minpoly
 import Mathlib.RingTheory.RootsOfUnity.PrimitiveRoots
