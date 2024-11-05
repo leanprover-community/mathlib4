014c59889e7e0d99331634008d1cb7fa74402e57
Daniel Weber <commandmaster2017@gmail.com>
2024-11-05 07:13:40 +0200

    Minor tweak

--- Mathlib/FieldTheory/Differential/Basic.lean
+++ Mathlib/FieldTheory/Differential/Basic.lean
@@ -121,7 +121,7 @@ noncomputable instance (p : F[X]) [Fact (Irreducible p)] [Fact p.Monic] :
       · intro nh
         simp [natDegree_eq_zero_of_derivative_eq_zero nh] at this
       apply natDegree_derivative_lt
-      omega
+      exact Nat.not_eq_zero_of_lt this
     )
 
 instance (p : F[X]) [Fact (Irreducible p)] [Fact p.Monic] :
