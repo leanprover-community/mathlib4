6b7a20b975acd1fd0ac8c55c288a8db984f8c429
adomani <adomani@gmail.com>
2024-11-06 01:12:36 +0000

    Mathlib.Data.Nat.Defs

--- scripts/downstream_counts.lean
+++ scripts/downstream_counts.lean
@@ -1,13 +1,15 @@
 import Lean
 import ImportGraph.Imports
-import Mathlib.Logic.Function.Defs
+import Mathlib.Data.Nat.Defs
 
 open Lean
 
 def downstreamCount (importGraph : NameMap (Array Name)) (fileNames : Array Name) : Array Nat :=
   fileNames.map fun fileName ↦
     let ds := importGraph.downstreamOf {fileName}
-    let downDeps : NameSet := ds.fold (init := {}) fun a _ c => a.append (.ofArray c)
+    let downDeps : NameSet := ds.fold (init := {}) fun a b _c => a.insert b --append (.ofArray c)
+    --dbg_trace fileName
+    --dbg_trace (downDeps.erase fileName).toArray
     (downDeps.erase fileName).size
 
 abbrev fils : Array Name := #[
@@ -15,4 +17,6 @@ abbrev fils : Array Name := #[
 ]
 
 run_cmd
-  dbg_trace downstreamCount (← getEnv).importGraph fils
+  let ig := (← getEnv).importGraph
+  for f in fils do
+    dbg_trace (downstreamCount ig #[f]).getD 0 0
