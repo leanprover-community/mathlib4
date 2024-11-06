2616001b19cc650bcfd001e8aa982dc31b95052d
damiano <adomani@gmail.com>
2024-11-06 01:21:04 +0000

    Update scripts/late_importers.sh
    Co-authored-by: Bryan Gin-ge Chen <bryangingechen@gmail.com>

--- scripts/late_importers.sh
+++ scripts/late_importers.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# Make this script robust against unintentional errors.
+# See e.g. http://redsymbol.net/articles/unofficial-bash-strict-mode/ for explanation.
+set -euo pipefail
+IFS=$'\n\t'
+
  : <<'BASH_MODULE_DOCS'
 
 This script formats the `linter.minImports` output, returning a table of the form
