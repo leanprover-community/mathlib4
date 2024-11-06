3acb1eb22c9bffda43c5183a0faca7b08e455cc7
Junyan Xu <junyanxu.math@gmail.com>
2024-11-06 11:22:46 +0000

    feat: localization is flat (#18683)

--- Mathlib.lean
+++ Mathlib.lean
@@ -4080,6 +4080,7 @@ import Mathlib.RingTheory.Flat.Basic
 import Mathlib.RingTheory.Flat.CategoryTheory
 import Mathlib.RingTheory.Flat.EquationalCriterion
 import Mathlib.RingTheory.Flat.FaithfullyFlat
+import Mathlib.RingTheory.Flat.Localization
 import Mathlib.RingTheory.Flat.Stability
 import Mathlib.RingTheory.FractionalIdeal.Basic
 import Mathlib.RingTheory.FractionalIdeal.Extended
