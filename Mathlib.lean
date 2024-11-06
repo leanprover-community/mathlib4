987d851a3f2900a81cf8f470d8fe17c9558032a5
acmepjz <acme_pjz@hotmail.com>
2024-11-07 00:58:16 +0800

    add a new file

--- Mathlib.lean
+++ Mathlib.lean
@@ -4168,6 +4168,7 @@ import Mathlib.RingTheory.Localization.BaseChange
 import Mathlib.RingTheory.Localization.Basic
 import Mathlib.RingTheory.Localization.Cardinality
 import Mathlib.RingTheory.Localization.Defs
+import Mathlib.RingTheory.Localization.FieldRange
 import Mathlib.RingTheory.Localization.Finiteness
 import Mathlib.RingTheory.Localization.FractionRing
 import Mathlib.RingTheory.Localization.Free
