569e4c928fd2c33cf5b9d482679a2ade9fbd7a83
Jujian Zhang <jujian.zhang1998@outlook.com>
2024-11-06 22:24:25 +0000

    apply suggestions

--- Mathlib/LinearAlgebra/TensorProduct/Basis.lean
+++ Mathlib/LinearAlgebra/TensorProduct/Basis.lean
@@ -68,6 +68,7 @@ lemma Basis.baseChange_apply (b : Basis ι R M) (i) :
 
 section
 
+variable [DecidableEq ι] [DecidableEq κ]
 variable (ℬ : Basis ι R M) (𝒞 : Basis κ R N) (x : M ⊗[R] N)
 
 /--
@@ -75,34 +76,38 @@ If `{𝒞ᵢ}` is a basis for the module `N`, then every elements of `x ∈ M �
 as `∑ᵢ mᵢ ⊗ 𝒞ᵢ` for some `mᵢ ∈ M`.
 -/
 def TensorProduct.equivFinsuppOfBasisRight : M ⊗[R] N ≃ₗ[R] κ →₀ M :=
-  LinearEquiv.symm <|
-    LinearEquiv.ofBijective (Finsupp.lsum R fun i ↦ (TensorProduct.mk R M N).flip (𝒞 i)) <| by
-      classical
-      fconstructor
-      · intro b b' h
-        refine (TensorProduct.congr (LinearEquiv.refl R M) 𝒞.repr ≪≫ₗ
-          (finsuppScalarRight R M κ)).symm.injective ?_
-        rw [← b.sum_single, ← b'.sum_single, Finsupp.sum, map_sum, Finsupp.sum, map_sum]
-        simpa
-      · intro x
-        induction x using TensorProduct.induction_on with
-        | zero => exact ⟨0, by simp⟩
-        | tmul m n =>
-          use (𝒞.repr n).mapRange (· • m) (by simp)
-          simp only [Finsupp.mapRange, Finsupp.coe_lsum, map_zero, implies_true,
-            Finsupp.onFinset_sum, Function.comp_apply, map_smul, flip_apply, mk_apply, smul_tmul',
-            smul_tmul]
-          rw [← tmul_sum]
-          congr
-          conv_rhs => rw [← Basis.linearCombination_repr 𝒞 n]
-          rfl
-        | add x y hx hy =>
-          rcases hx with ⟨x, rfl⟩
-          rcases hy with ⟨y, rfl⟩
-          exact ⟨x + y, Finsupp.sum_add_index (by simp) fun _ _ => by simp [add_tmul]⟩
+  LinearEquiv.lTensor M 𝒞.repr ≪≫ₗ TensorProduct.finsuppScalarRight R M κ
 
+@[simp]
+lemma TensorProduct.equivFinsuppOfBasisRight_apply_tmul (m : M) (n : N) :
+    (TensorProduct.equivFinsuppOfBasisRight 𝒞) (m ⊗ₜ n) =
+    (𝒞.repr n).mapRange (· • m) (zero_smul _ _) := by
+  ext; simp [equivFinsuppOfBasisRight]
+
+lemma TensorProduct.equivFinsuppOfBasisRight_apply_tmul_apply
+    (m : M) (n : N) (i : κ) :
+    (TensorProduct.equivFinsuppOfBasisRight 𝒞) (m ⊗ₜ n) i =
+    𝒞.repr n i • m := by
+  simp only [equivFinsuppOfBasisRight_apply_tmul, Finsupp.mapRange_apply]
+
+lemma TensorProduct.equivFinsuppOfBasisRight_symm :
+    (TensorProduct.equivFinsuppOfBasisRight 𝒞).symm.toLinearMap =
+    Finsupp.lsum R fun i ↦ (TensorProduct.mk R M N).flip (𝒞 i) := by
+  ext; simp [equivFinsuppOfBasisRight]
+
+@[simp]
 lemma TensorProduct.equivFinsuppOfBasisRight_symm_apply (b : κ →₀ M) :
-    (TensorProduct.equivFinsuppOfBasisRight 𝒞).symm b = b.sum fun i m ↦ m ⊗ₜ 𝒞 i := rfl
+    (TensorProduct.equivFinsuppOfBasisRight 𝒞).symm b = b.sum fun i m ↦ m ⊗ₜ 𝒞 i :=
+  congr($(TensorProduct.equivFinsuppOfBasisRight_symm 𝒞) b)
+
+lemma TensorProduct.sum_tmul_basis_right_injective :
+    Function.Injective (Finsupp.lsum R fun i ↦ (TensorProduct.mk R M N).flip (𝒞 i)) :=
+  (equivFinsuppOfBasisRight_symm (M := M) 𝒞).symm ▸
+    (TensorProduct.equivFinsuppOfBasisRight 𝒞).symm.injective
+
+lemma TensorProduct.sum_tmul_basis_right_eq_zero
+    (b : κ →₀ M) (h : (b.sum fun i m ↦ m ⊗ₜ[R] 𝒞 i) = 0) : b = 0 :=
+  (TensorProduct.equivFinsuppOfBasisRight 𝒞).symm.injective (a₂ := 0) <| by simpa
 
 /--
 If `{ℬᵢ}` is a basis for the module `M`, then every elements of `x ∈ M ⊗ N` can be uniquely written
@@ -111,33 +116,49 @@ as `∑ᵢ ℬᵢ ⊗ nᵢ` for some `nᵢ ∈ N`.
 def TensorProduct.equivFinsuppOfBasisLeft : M ⊗[R] N ≃ₗ[R] ι →₀ N :=
   TensorProduct.comm R M N ≪≫ₗ TensorProduct.equivFinsuppOfBasisRight ℬ
 
+@[simp]
+lemma TensorProduct.equivFinsuppOfBasisLeft_apply_tmul (m : M) (n : N) :
+    (TensorProduct.equivFinsuppOfBasisLeft ℬ) (m ⊗ₜ n) =
+    (ℬ.repr m).mapRange (· • n) (zero_smul _ _) := by
+  ext; simp [equivFinsuppOfBasisLeft]
+
+lemma TensorProduct.equivFinsuppOfBasisLeft_apply_tmul_apply
+    (m : M) (n : N) (i : ι) :
+    (TensorProduct.equivFinsuppOfBasisLeft ℬ) (m ⊗ₜ n) i =
+    ℬ.repr m i • n := by
+  simp only [equivFinsuppOfBasisLeft_apply_tmul, Finsupp.mapRange_apply]
+
+lemma TensorProduct.equivFinsuppOfBasisLeft_symm :
+    (TensorProduct.equivFinsuppOfBasisLeft ℬ).symm.toLinearMap =
+    Finsupp.lsum R fun i ↦ (TensorProduct.mk R M N) (ℬ i) := by
+  ext; simp [equivFinsuppOfBasisLeft]
+
+@[simp]
 lemma TensorProduct.equivFinsuppOfBasisLeft_symm_apply (b : ι →₀ N) :
-    (TensorProduct.equivFinsuppOfBasisLeft ℬ).symm b = b.sum fun i n ↦ ℬ i ⊗ₜ n := by
-  simp only [equivFinsuppOfBasisLeft, LinearEquiv.trans_symm, LinearEquiv.trans_apply,
-    equivFinsuppOfBasisRight_symm_apply, Finsupp.sum, map_sum, comm_symm_tmul]
+    (TensorProduct.equivFinsuppOfBasisLeft ℬ).symm b = b.sum fun i n ↦ ℬ i ⊗ₜ n :=
+  congr($(TensorProduct.equivFinsuppOfBasisLeft_symm ℬ) b)
 
 /-- Elements in `M ⊗ N` can be represented by sum of elements in `M` tensor elements of basis of
 `N`. -/
 lemma TensorProduct.eq_repr_basis_right :
-    ∃ b : κ →₀ M, b.sum (fun i m ↦ m ⊗ₜ 𝒞 i) = x :=
-  (TensorProduct.equivFinsuppOfBasisRight 𝒞).symm.surjective x
+    ∃ b : κ →₀ M, b.sum (fun i m ↦ m ⊗ₜ 𝒞 i) = x := by
+  simpa using (TensorProduct.equivFinsuppOfBasisRight 𝒞).symm.surjective x
 
 /-- Elements in `M ⊗ N` can be represented by sum of elements of basis of `M` tensor elements of
   `N`.-/
 lemma TensorProduct.eq_repr_basis_left :
     ∃ (c : ι →₀ N), (c.sum fun i n ↦ ℬ i ⊗ₜ n) = x := by
   obtain ⟨c, rfl⟩ := (TensorProduct.equivFinsuppOfBasisLeft ℬ).symm.surjective x
-  exact ⟨c, (TensorProduct.comm R M N).injective <| by
-    simp [equivFinsuppOfBasisLeft, equivFinsuppOfBasisRight, Finsupp.sum]⟩
+  exact ⟨c, (TensorProduct.comm R M N).injective <| by simp [Finsupp.sum]⟩
 
-lemma TensorProduct.sum_tmul_basis_right_eq_zero
-    (b : κ →₀ M) (h : (b.sum fun i m ↦ m ⊗ₜ[R] 𝒞 i) = 0) : b = 0 :=
-  (TensorProduct.equivFinsuppOfBasisRight 𝒞).symm.injective (a₂ := 0) (h.trans (by simp))
+lemma TensorProduct.sum_tmul_basis_left_injective :
+    Function.Injective (Finsupp.lsum R fun i ↦ (TensorProduct.mk R M N) (ℬ i)) :=
+  (equivFinsuppOfBasisLeft_symm (N := N) ℬ).symm ▸
+    (TensorProduct.equivFinsuppOfBasisLeft ℬ).symm.injective
 
 lemma TensorProduct.sum_tmul_basis_left_eq_zero
     (b : ι →₀ N) (h : (b.sum fun i n ↦ ℬ i ⊗ₜ[R] n) = 0) : b = 0 :=
-  (TensorProduct.equivFinsuppOfBasisLeft ℬ).symm.injective (a₂ := 0) <| by
-    simpa [TensorProduct.equivFinsuppOfBasisLeft_symm_apply]
+  (TensorProduct.equivFinsuppOfBasisLeft ℬ).symm.injective (a₂ := 0) <| by simpa
 
 end
 
