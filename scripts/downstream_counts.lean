e7628d7ee20a1d53082343aeac14f912de536b61
adomani <adomani@gmail.com>
2024-11-06 02:05:32 +0000

    docs and imports in lean file

--- scripts/downstream_counts.lean
+++ scripts/downstream_counts.lean
@@ -1,15 +1,24 @@
-import Lean
-import ImportGraph.Imports
-import Mathlib.Data.Nat.Defs
+/-
+Copyright (c) 2024 Damiano Testa. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Damiano Testa
+-/
+
+import Mathlib
+
+/-!
+# Count the number of files that import a given file
+
+This file is part of the `latest_import.yml` workflow.  More precisely, `scripts/late_importers.sh`
+uses it to determine the number of downstream imports for the report that it prints to Zulip.
+-/
 
 open Lean
 
 def downstreamCount (importGraph : NameMap (Array Name)) (fileNames : Array Name) : Array Nat :=
   fileNames.map fun fileName ↦
     let ds := importGraph.downstreamOf {fileName}
-    let downDeps : NameSet := ds.fold (init := {}) fun a b _c => a.insert b --append (.ofArray c)
-    --dbg_trace fileName
-    --dbg_trace (downDeps.erase fileName).toArray
+    let downDeps : NameSet := ds.fold (init := {}) fun a b _c => a.insert b
     (downDeps.erase fileName).size
 
 abbrev fils : Array Name := #[
