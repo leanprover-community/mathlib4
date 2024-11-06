d3f9aa65f329385ca20c2173b3b1c0458de842da
Junyan Xu <junyanxu.math@gmail.com>
2024-11-06 06:13:20 -0500

    fix after #18657

--- Mathlib/RingTheory/Flat/Localization.lean
+++ Mathlib/RingTheory/Flat/Localization.lean
@@ -3,7 +3,7 @@ Copyright (c) 2024 Junyan Xu. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Junyan Xu
 -/
-import Mathlib.Algebra.Module.Submodule.Localization
+import Mathlib.Algebra.Module.LocalizedModule.Submodule
 import Mathlib.RingTheory.Flat.Basic
 import Mathlib.RingTheory.Localization.BaseChange
 
