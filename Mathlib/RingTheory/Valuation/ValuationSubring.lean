543c823f689f8752c8585c5b921720059aa17920
Sven Manthe <sven.manthe@gmerek-manthe.de>
2024-11-07 16:35:27 +0100

    fix

--- Mathlib/RingTheory/Valuation/ValuationSubring.lean
+++ Mathlib/RingTheory/Valuation/ValuationSubring.lean
@@ -348,10 +348,11 @@ def primeSpectrumOrderEquiv : (PrimeSpectrum A)ᵒᵈ ≃o {S // A ≤ S} :=
 
 instance le_total_ideal : IsTotal {S // A ≤ S} LE.le :=
   let _ : IsTotal (PrimeSpectrum A) (· ≤ ·) := ⟨fun ⟨x, _⟩ ⟨y, _⟩ => LE.isTotal.total x y⟩
-  (primeSpectrumOrderEquiv A).symm.toRelEmbedding.isTotal.total α β
+  ⟨(primeSpectrumOrderEquiv A).symm.toRelEmbedding.isTotal.total⟩
 
-instance linearOrderOverring : LinearOrder {S // A ≤ S} :=
-  Lattice.toLinearOrder {S // A ≤ S}
+instance linearOrderOverring : LinearOrder {S // A ≤ S} where
+  le_total := (le_total_ideal A).1
+  max_def a b := congr_fun₂ sup_eq_maxDefault a b
 
 end Order
 
