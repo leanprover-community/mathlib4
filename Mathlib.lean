c3d0b1e07c6ab6f839ee742b30ce4c58c4d56582
Anne C.A. Baanen <vierkantor@vierkantor.com>
2024-11-07 16:36:46 +0100

    chore(RingTheory/PrincipalIdealDomain): split off `PrincipalIdealRing.of_prime`
    This declaration pulls in a lot of ideal constructions which we don't use in e.g. the LinearAlgebra bits of the library.

--- Mathlib.lean
+++ Mathlib.lean
@@ -4271,6 +4271,7 @@ import Mathlib.RingTheory.Presentation
 import Mathlib.RingTheory.Prime
 import Mathlib.RingTheory.PrimeSpectrum
 import Mathlib.RingTheory.PrincipalIdealDomain
+import Mathlib.RingTheory.PrincipalIdealDomainOfPrime
 import Mathlib.RingTheory.QuotSMulTop
 import Mathlib.RingTheory.Radical
 import Mathlib.RingTheory.ReesAlgebra
