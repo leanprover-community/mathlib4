6d67e9bf891229893c24e682c884cdea74db61e1
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2024-11-07 14:28:27 -0300

    Fix imports

--- Mathlib/Data/Fin/Parity.lean
+++ Mathlib/Data/Fin/Parity.lean
@@ -1,9 +1,9 @@
 /-
 Copyright (c) 2023 Yury Kudryashov. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Yury Kudryashov
+Authors: Yury Kudryashov, Iván Renison
 -/
-import Mathlib.Algebra.Group.Even
+import Mathlib.Algebra.Ring.Parity
 import Mathlib.Data.Fin.Basic
 import Mathlib.Data.ZMod.Defs
 
