6c4ada0f35541ff383b924d09d3c1ed0e26170f7
Pim Otte <otte.pim@gmail.com>
2024-11-07 21:22:42 +0000

    feat(Combinatorics/SimpleGraph/Matching): graph isomorphisms preserve matchings (#17436)
    added lemma to prove a subgraph is a matching iff the subgraph mapped by an isomorphism is a matching.
    Added in order to support a proof of Tutte's theorem, and generalize results to be introduced in #15666
    
    
    
    Co-authored-by: Pim Otte <p.j.otte@uu.nl>

--- Mathlib/Combinatorics/SimpleGraph/Maps.lean
+++ Mathlib/Combinatorics/SimpleGraph/Maps.lean
@@ -471,6 +471,18 @@ theorem map_mem_edgeSet_iff {e : Sym2 V} : e.map f ∈ G'.edgeSet ↔ e ∈ G.ed
 theorem apply_mem_neighborSet_iff {v w : V} : f w ∈ G'.neighborSet (f v) ↔ w ∈ G.neighborSet v :=
   map_adj_iff f
 
+@[simp]
+theorem symm_toHom_comp_toHom : f.symm.toHom.comp f.toHom = Hom.id := by
+  ext v
+  simp only [RelHom.comp_apply, RelEmbedding.coe_toRelHom, RelIso.coe_toRelEmbedding,
+    RelIso.symm_apply_apply, RelHom.id_apply]
+
+@[simp]
+theorem toHom_comp_symm_toHom : f.toHom.comp f.symm.toHom = Hom.id := by
+  ext v
+  simp only [RelHom.comp_apply, RelEmbedding.coe_toRelHom, RelIso.coe_toRelEmbedding,
+    RelIso.apply_symm_apply, RelHom.id_apply]
+
 /-- An isomorphism of graphs induces an equivalence of edge sets. -/
 @[simps]
 def mapEdgeSet : G.edgeSet ≃ G'.edgeSet where
