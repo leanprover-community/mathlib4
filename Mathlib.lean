185be8f63b8bcb7f2fbfe71aef6de15361685b2d
Junyan Xu <junyanxu.math@gmail.com>
2024-11-07 18:54:03 +0000

    feat: cardinalities in specific inverse systems (#18067)
    We compute the cardinality of each node in an inverse system `F i` indexed by a well-order in which every map between successive nodes has constant fiber `X i`, and every limit node is the `limit` of the inverse subsystem formed by all previous nodes. (To avoid importing `Cardinal`, we in fact construct a bijection rather than stating the result in terms of `Cardinal.mk`.)
    
    
    
    Co-authored-by: Junyan Xu <junyanxumath@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -3784,6 +3784,7 @@ import Mathlib.Order.CountableDenseLinearOrder
 import Mathlib.Order.Cover
 import Mathlib.Order.Defs
 import Mathlib.Order.Directed
+import Mathlib.Order.DirectedInverseSystem
 import Mathlib.Order.Disjoint
 import Mathlib.Order.Disjointed
 import Mathlib.Order.Estimator
