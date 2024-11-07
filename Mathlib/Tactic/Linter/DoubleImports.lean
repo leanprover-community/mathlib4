0852e930b97e00faa846eb00dfa20c52146a08f7
Anne C.A. Baanen <vierkantor@vierkantor.com>
2024-11-07 13:57:19 +0100

    Do not warn on declarations that depend on a local definition.
    This helps for files such as `Algebra/Polynomial/Roots.lean` which start off with a definition and then prove many facts about this definition.

--- Mathlib/Tactic/Linter/DoubleImports.lean
+++ Mathlib/Tactic/Linter/DoubleImports.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Damiano Testa
 -/
 import ImportGraph.Imports
+import Mathlib.Lean.Expr.Basic
 import Mathlib.Tactic.MinImports
 
 /-! # The `doubleImports` linter
@@ -26,12 +27,31 @@ open Lean Elab Command
 The "doubleImports" linter tracks information about minimal imports over several commands.
 -/
 
+/-- Does this declaration come from the current file? -/
+def Lean.Name.isLocal (env : Environment) (decl : Name) : Bool :=
+  -- I am not sure if this is at all the right way to go about it, but it seems to work in practice.
+  (env.getModuleIdxFor? decl).isNone
+
+/-- Does the declaration with this name depend on `def`s in the current file? -/
+def Lean.Name.localDefDependencies (env : Environment) (decl : Name) : CoreM Bool := do
+  let deps ← decl.transitivelyUsedConstants
+  let constInfos := deps.toList.filterMap env.find?
+  let defs := constInfos.filter (·.isDef)
+  return defs.any fun constInfo => constInfo.name != decl && constInfo.name.isLocal env
+
+/-- Does the declaration with this name depend on `def`s in the current file? -/
+def Lean.Syntax.localDefDependencies (env : Environment) (decl : Syntax) : CoreM Bool := do
+  let options ← resolveGlobalConst decl
+  options.anyM (fun name => name.localDefDependencies env)
+
 namespace Mathlib.Linter
 
 /--
-The `doubleImports` linter incrementally computes the minimal imports needed for each file to build.
-Whenever it detects that a new command requires an increase in the (transitive) imports that it
-computed so far, it emits a warning mentioning the bigger minimal imports.
+The `doubleImports` linter determines for each declaration if it can be moved higher up the import
+hierarchy; if this is the case it will display a warning.
+
+The linter does not place a warning on any declaration depending on a definition in the current file
+(while it does place a warning on the definition itself).
 
 Unlike the related `#min_imports` command, the linter takes into account notation and tactic
 information.
@@ -58,7 +78,7 @@ def doubleImportsLinter : Linter where run := withSetOptionIn fun stx ↦ do
     let id ← getId stx
     if id != .missing then
       let newImports := getIrredundantImports env (← getAllImports stx id)
-      if newImports.size == 1 then
+      if newImports.size == 1 && !(← liftCoreM <| id.localDefDependencies env) then
         dbg_trace "imports for '{id}': {newImports.toArray.qsort (·.toString < ·.toString)}"
 
 initialize addLinter doubleImportsLinter
