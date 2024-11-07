40c1cf487bc957a888656d3e2a03bae8bd17d96b
Anne C.A. Baanen <vierkantor@vierkantor.com>
2024-11-07 16:35:16 +0100

    cleanup

--- Mathlib/LinearAlgebra/Dimension/Torsion.lean
+++ Mathlib/LinearAlgebra/Dimension/Torsion.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2018 Mario Carneiro. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Mario Carneiro, Johannes Hölzl, Sander Dahmen, Kim Morrison, Chris Hughes, Anne Baanen
+Authors: Andrew Yang
 -/
 import Mathlib.Algebra.Module.Torsion
 import Mathlib.LinearAlgebra.Dimension.Constructions
