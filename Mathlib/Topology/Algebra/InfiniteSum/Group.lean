9e559e8254c23b19d9bb39ab07aadea9e6d89177
Bhavik Mehta <bhavikmehta8@gmail.com>
2024-11-08 21:45:30 +0000

    dedup

--- Mathlib/Topology/Algebra/InfiniteSum/Group.lean
+++ Mathlib/Topology/Algebra/InfiniteSum/Group.lean
@@ -366,19 +366,3 @@ theorem tprod_const [T2Space G] (a : G) : ∏' _ : β, a = a ^ (Nat.card β) := 
       simpa [multipliable_const_iff] using ha
 
 end TopologicalGroup
-
-section NormedField
-
-variable {α E : Type*} [NormedField E] {f : α → E} {x : E}
-
-nonrec theorem HasProd.norm (hfx : HasProd f x) : HasProd (‖f ·‖) ‖x‖ := by
-  simp only [HasProd, ← norm_prod]
-  exact hfx.norm
-
-theorem Multipliable.norm (hf : Multipliable f) : Multipliable (‖f ·‖) :=
-  let ⟨x, hx⟩ := hf; ⟨‖x‖, hx.norm⟩
-
-theorem norm_tprod (hf : Multipliable f) : ‖∏' i, f i‖ = ∏' i, ‖f i‖ :=
-  hf.hasProd.norm.tprod_eq.symm
-
-end NormedField
