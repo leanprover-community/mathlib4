602a5fb5bbc1d739f8b0c3d7ef19da701e9f6b80
FR <zhao.yu-yang@foxmail.com>
2024-11-08 16:09:20 +0800

    deprecate

--- Mathlib/Algebra/Module/LinearMap/Defs.lean
+++ Mathlib/Algebra/Module/LinearMap/Defs.lean
@@ -583,7 +583,17 @@ variable [AddCommMonoid M] [AddCommMonoid M₂] [AddCommMonoid M₃]
 variable [Semiring R] [Module R M] [Semiring S] [Module S M₂] [Module R M₃]
 variable {σ : R →+* S}
 
+@[deprecated (since := "2024-11-08")]
+/-- A `DistribMulActionHom` between two modules is a linear map. -/
+def toSemilinearMap (fₗ : M →ₑ+[σ.toMonoidHom] M₂) : M →ₛₗ[σ] M₂ :=
+  { fₗ with }
+
 instance : SemilinearMapClass (M →ₑ+[σ.toMonoidHom] M₂) σ M M₂ where
+
+@[deprecated (since := "2024-11-08")]
+/-- A `DistribMulActionHom` between two modules is a linear map. -/
+def toLinearMap (fₗ : M →+[R] M₃) : M →ₗ[R] M₃ :=
+  { fₗ with }
 
 /-- A `DistribMulActionHom` between two modules is a linear map. -/
 instance : LinearMapClass (M →+[R] M₃) R M M₃ where
