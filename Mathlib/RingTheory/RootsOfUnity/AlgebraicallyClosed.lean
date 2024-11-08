c23b668f541eaf7717ef6e9ead7aabacb9d03524
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2024-11-08 12:37:13 +0100

    minigolf

--- Mathlib/RingTheory/RootsOfUnity/AlgebraicallyClosed.lean
+++ Mathlib/RingTheory/RootsOfUnity/AlgebraicallyClosed.lean
@@ -27,11 +27,11 @@ that are not divisible by the characteristic of `F`. -/
 instance hasEnoughRootsOfUnity (F : Type*) [Field F] [IsAlgClosed F] (n : ℕ) [i : NeZero (n : F)] :
     HasEnoughRootsOfUnity F n where
   prim := by
-    have : NeZero n := NeZero.of_neZero_natCast F
+    have : NeZero n := .of_neZero_natCast F
     have := isCyclotomicExtension {⟨n, NeZero.pos n⟩} F fun _ h ↦ Set.mem_singleton_iff.mp h ▸ i
     exact IsCyclotomicExtension.exists_prim_root (S := {(⟨n, NeZero.pos n⟩ : ℕ+)}) F rfl
   cyc :=
-    have : NeZero n := NeZero.of_neZero_natCast F
+    have : NeZero n := .of_neZero_natCast F
     rootsOfUnity.isCyclic F n
 
 end IsAlgClosed
