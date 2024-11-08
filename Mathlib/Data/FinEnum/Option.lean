661743681041b9c8a4d62c783ec2bca3eb574bff
Tom Kranz <TpmKranz@users.noreply.github.com>
2024-11-08 11:23:06 +0100

    fixup! fixup! fixup! FinEnum recursor

--- Mathlib/Data/FinEnum/Option.lean
+++ Mathlib/Data/FinEnum/Option.lean
@@ -45,17 +45,17 @@ def recEmptyOption {P : (α : Type u) → Sort v}
     (fin_choice : (n : ℕ) → Fin (n + 1))
     (of_equiv : {α β : Type u} → (_ : FinEnum α) → (_ : FinEnum β) → card β = card α → P α → P β )
     (h_empty : P PEmpty.{u + 1})
-    (h_option : {α : Type u} → [FinEnum α] → P α → P (Option α))
+    (h_option : {α : Type u} → FinEnum α → P α → P (Option α))
     (α : Type u) [FinEnum α] :
     P α :=
   match cardeq : card α with
   | 0 => of_equiv _ _ cardeq h_empty
   | n + 1 =>
-    have := uliftId (α := Fin n)
+    let fN := uliftId (α := Fin n)
     have : card (ULift.{u} <| Fin n) = n := card_ulift.trans card_fin
     of_equiv (insertNone _ <| fin_choice n) _
       (cardeq.trans <| congrArg Nat.succ this.symm) <|
-        h_option (recEmptyOption fin_choice of_equiv h_empty h_option _)
+        h_option fN (recEmptyOption fin_choice of_equiv h_empty h_option _)
 termination_by card α
 
 /--
@@ -66,10 +66,10 @@ theorem recEmptyOption_of_card_eq_zero {P : (α : Type u) → Sort v}
     (fin_choice : (n : ℕ) → Fin (n + 1))
     (of_equiv : {α β : Type u} → (_ : FinEnum α) → (_ : FinEnum β) → card β = card α → P α → P β )
     (h_empty : P PEmpty.{u + 1})
-    (h_option : {α : Type u} → [FinEnum α] → P α → P (Option α))
-    (α : Type u) [FinEnum α] [fe : FinEnum PEmpty.{u + 1}] (h : card α = 0) :
+    (h_option : {α : Type u} → FinEnum α → P α → P (Option α))
+    (α : Type u) [FinEnum α] (h : card α = 0) (_ : FinEnum PEmpty.{u + 1}) :
     recEmptyOption fin_choice of_equiv h_empty h_option α =
-      of_equiv _ _ (h.trans <| card_eq_zero_of_IsEmpty _ |>.symm) h_empty := by
+      of_equiv _ _ (h.trans <| card_eq_zero_of_IsEmpty |>.symm) h_empty := by
   unfold recEmptyOption
   split
   · congr 1; exact Subsingleton.allEq _ _
@@ -84,17 +84,17 @@ theorem recEmptyOption_of_card_eq_succ {P : (α : Type u) → Sort v}
     (fin_choice : (n : ℕ) → Fin (n + 1))
     (of_equiv : {α β : Type u} → (_ : FinEnum α) → (_ : FinEnum β) → card β = card α → P α → P β )
     (h_empty : P PEmpty.{u + 1})
-    (h_option : {α : Type u} → [FinEnum α] → P α → P (Option α))
+    (h_option : {α : Type u} → FinEnum α → P α → P (Option α))
     (α : Type u) [FinEnum α] (n : {n : ℕ // card α = n + 1}) :
     recEmptyOption fin_choice of_equiv h_empty h_option α =
       of_equiv (insertNone _ <| fin_choice n) _
         (n.prop.trans <| congrArg Nat.succ (card_ulift.trans card_fin).symm)
-        (h_option
-          (recEmptyOption fin_choice of_equiv h_empty h_option (ULift.{u} <| Fin n))) := by
+        (h_option uliftId <|
+          recEmptyOption fin_choice of_equiv h_empty h_option (ULift.{u} <| Fin n)) := by
   conv => lhs; unfold recEmptyOption
   split
   · exact Nat.noConfusion <| n.prop.symm.trans ‹_›
-  · rcases Nat.succ.inj (n.prop.symm.trans ‹_›) with ⟨rfl⟩; rfl
+  · rcases Nat.succ.inj <| n.prop.symm.trans ‹_› with ⟨rfl⟩; rfl
 
 /-- A recursor principle for finite-and-enumerable types, analogous to `Nat.recOn`.
 It effectively says that every `FinEnum` is either `Empty` or `Option α`, up to an `Equiv` mediated
@@ -108,7 +108,7 @@ abbrev recOnEmptyOption {P : (α : Type u) → Sort v}
     (fin_choice : (n : ℕ) → Fin (n + 1))
     (of_equiv : {α β : Type u} → (_ : FinEnum α) → (_ : FinEnum β) → card β = card α → P α → P β )
     (h_empty : P PEmpty.{u + 1})
-    (h_option : {α : Type u} → [FinEnum α] → P α → P (Option α)) :
+    (h_option : {α : Type u} → FinEnum α → P α → P (Option α)) :
     P α :=
   @recEmptyOption P fin_choice of_equiv h_empty h_option α aenum
 
