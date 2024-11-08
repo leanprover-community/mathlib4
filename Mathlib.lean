c8c36ff8533703a651fa698fda8fdf6dfe6ffb7c
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2024-11-08 11:28:14 +0100

    feat(NumberTheory/MulChar/Duality): new file

--- Mathlib.lean
+++ Mathlib.lean
@@ -3688,6 +3688,7 @@ import Mathlib.NumberTheory.ModularForms.JacobiTheta.TwoVariable
 import Mathlib.NumberTheory.ModularForms.SlashActions
 import Mathlib.NumberTheory.ModularForms.SlashInvariantForms
 import Mathlib.NumberTheory.MulChar.Basic
+import Mathlib.NumberTheory.MulChar.Duality
 import Mathlib.NumberTheory.MulChar.Lemmas
 import Mathlib.NumberTheory.Multiplicity
 import Mathlib.NumberTheory.NumberField.Basic
