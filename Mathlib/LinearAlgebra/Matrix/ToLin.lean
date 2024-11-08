4c25191ded0948ae4d597d0e5858327af7859e24
Jujian Zhang <jujian.zhang1998@outlook.com>
2024-11-08 11:17:30 +0000

    Update Mathlib/LinearAlgebra/Matrix/ToLin.lean
    Co-authored-by: Junyan Xu <junyanxu.math@gmail.com>

--- Mathlib/LinearAlgebra/Matrix/ToLin.lean
+++ Mathlib/LinearAlgebra/Matrix/ToLin.lean
@@ -1028,7 +1028,7 @@ def endVecRingEquivMatrixEnd :
 /--
 Let `M` be an `A`-module. Every `A`-linear map `Mⁿ → Mⁿ` corresponds to a `n×n`-matrix whose entries
 are `R`-linear maps `M → M`. In another word, we have`End(Mⁿ) ≅ Matₙₓₙ(End(M))` defined by:
-`(f : Mⁿ → Mⁿ) ↦ (x ↦ f (0, ...,x at j-th position, ..., 0) i)ᵢⱼ` and
+`(f : Mⁿ → Mⁿ) ↦ (x ↦ f (0, ..., x at j-th position, ..., 0) i)ᵢⱼ` and
 `m : Matₙₓₙ(End(M)) ↦ (v ↦ ∑ⱼ mᵢⱼ(vⱼ))`.
 
 See also `LinearMap.toMatrix'`
