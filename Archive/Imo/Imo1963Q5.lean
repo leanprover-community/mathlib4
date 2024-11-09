695ebd4d602b5b0a16a5a71e89188eff3d91acc5
Rida Hamadani <mridahamadani@gmail.com>
2024-11-09 12:47:16 +0200

    another mistake in docs

--- Archive/Imo/Imo1963Q5.lean
+++ Archive/Imo/Imo1963Q5.lean
@@ -8,7 +8,7 @@ import Mathlib.Analysis.SpecialFunctions.Trigonometric.Complex
 /-!
 # IMO 1963 Q5
 
-Solve the equation `cos (π / 7) - cos (2 * π / 7) + cos (3 * π / 7) = 1`.
+Solve the equation `cos (π / 7) - cos (2 * π / 7) + cos (3 * π / 7) = 1 / 2`.
 
 The main idea of the proof is multiply both sides by `2 * sin (π / 7)`, then the result is reached
 through basic algebraic manipulations with the use of some trigonometric identities.
