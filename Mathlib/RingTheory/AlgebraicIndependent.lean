8487a6298d49aeb6f473bc239e603a4cd3af2860
Junyan Xu <junyanxu.math@gmail.com>
2024-11-09 07:08:33 -0500

    removed too much

--- Mathlib/RingTheory/AlgebraicIndependent.lean
+++ Mathlib/RingTheory/AlgebraicIndependent.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Chris Hughes
 -/
 import Mathlib.RingTheory.Algebraic
+import Mathlib.RingTheory.MvPolynomial.Basic
 import Mathlib.Data.Fin.Tuple.Reflection
 
 /-!
