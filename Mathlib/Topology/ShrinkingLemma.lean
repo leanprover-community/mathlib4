cb42dcfbdad945aa0e355ffecedaa6e343839f1a
Yoh Tanimoto <57562556+yoh-tanimoto@users.noreply.github.com>
2024-11-10 12:25:05 +0100

    Update Mathlib/Topology/ShrinkingLemma.lean
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- Mathlib/Topology/ShrinkingLemma.lean
+++ Mathlib/Topology/ShrinkingLemma.lean
@@ -289,7 +289,7 @@ theorem exists_gt_t2space (v : PartialRefinement u s (fun X => IsCompact (closur
       rw [Set.mem_iInter₂] at hsi'
       exact hsi' j hj
     obtain ⟨j, hj⟩ := Set.mem_iUnion.mp
-      (Set.mem_of_subset_of_mem v.subset_iUnion (Set.mem_of_mem_inter_left hx))
+      (v.subset_iUnion (Set.mem_of_mem_inter_left hx))
     obtain rfl : j = i := by
       by_contra! h
       exact this j h hj
