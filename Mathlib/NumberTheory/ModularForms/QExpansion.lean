90546591a4cbcad85980dd2b3f9a3e2216f3f8ed
David Loeffler <d.loeffler.01@cantab.net>
2024-11-10 14:45:12 +0100

    allow general levels

--- Mathlib/NumberTheory/ModularForms/QExpansion.lean
+++ Mathlib/NumberTheory/ModularForms/QExpansion.lean
@@ -4,115 +4,125 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: David Loeffler
 -/
 import Mathlib.Analysis.Complex.Periodic
-import Mathlib.Analysis.Complex.UpperHalfPlane.FunctionsBoundedAtInfty
 import Mathlib.NumberTheory.ModularForms.Basic
 import Mathlib.NumberTheory.ModularForms.Identities
 
 /-!
 # Q-expansions of modular forms
+
+We show that a modular form of level `Γ(n)` can be written as `τ ↦ F (𝕢 n τ)` where `F` is
+analytic on the open unit disc, and `𝕢 n` is the parameter `τ ↦ exp (2 * I * π * τ / n)`. As an
+application, we show that cusp forms decay exponentially to 0 as `im τ → ∞`.
+
+## TO DO:
+
+* generalise to handle arbitrary finite-index subgroups (not just `Γ(n)` for some `n`)
+* define the `q`-expansion as a formal power series
 -/
 
-open ModularForm Complex Filter Asymptotics UpperHalfPlane
+open ModularForm Complex Filter Asymptotics UpperHalfPlane Function
 
-open scoped Real Topology Manifold
+open scoped Real Topology Manifold MatrixGroups CongruenceSubgroup
 
 noncomputable section
 
-/-!
-## Extending functions from `ℍ` to `ℂ`
--/
+variable {k : ℤ} {F : Type*} [FunLike F ℍ ℂ] {Γ : Subgroup SL(2, ℤ)} (n : ℕ) (f : F)
 
 local notation "I∞" => comap Complex.im atTop
+local notation "𝕢" => qParam
 
-namespace UpperHalfPlane
+theorem im_invQParam_pos_of_abs_lt_one
+    {h : ℝ} (hh : 0 < h) {q : ℂ} (hq : q.abs < 1) (hq_ne : q ≠ 0) :
+    0 < im (invQParam h q) :=
+  im_invQParam .. ▸ mul_pos_of_neg_of_neg
+    (div_neg_of_neg_of_pos (neg_lt_zero.mpr hh) Real.two_pi_pos)
+    ((Real.log_neg_iff (Complex.abs.pos hq_ne)).mpr hq)
 
-end UpperHalfPlane
+namespace SlashInvariantFormClass
 
-local notation "SL(" n ", " R ")" => Matrix.SpecialLinearGroup (Fin n) R
+theorem periodic_comp_ofComplex [SlashInvariantFormClass F Γ(n) k] :
+    Periodic (f ∘ ofComplex) n := by
+  intro w
+  by_cases hw : 0 < im w
+  · have : 0 < im (w + n) := by simp only [add_im, natCast_im, add_zero, hw]
+    simp only [comp_apply, ofComplex_apply_of_im_pos this, ofComplex_apply_of_im_pos hw]
+    convert SlashInvariantForm.vAdd_width_periodic n k 1 f ⟨w, hw⟩ using 2
+    simp only [Int.cast_one, mul_one, UpperHalfPlane.ext_iff, coe_mk_subtype, coe_vadd,
+      ofReal_natCast, add_comm]
+  · have : im (w + n) ≤ 0 := by simpa only [add_im, natCast_im, add_zero, not_lt] using hw
+    simp only [comp_apply, ofComplex_apply_of_im_nonpos this,
+      ofComplex_apply_of_im_nonpos (not_lt.mp hw)]
 
-section ModformEquivs
+/--
+The analytic function `F` such that `f τ = F (exp (2 * π * I * τ / n))`, extended by a choice of
+limit at `0`.
+-/
+nonrec def cuspFunction : ℂ → ℂ := cuspFunction n (f ∘ ofComplex)
 
-variable {k : ℤ}
+nonrec theorem eq_cuspFunction [NeZero n] [SlashInvariantFormClass F Γ(n) k] (τ : ℍ) :
+    cuspFunction n f (𝕢 n τ) = f τ := by
+  simpa only [comp_apply, ofComplex_apply]
+    using eq_cuspFunction (NeZero.ne _) (periodic_comp_ofComplex n f) τ
 
-theorem modform_bounded_atIInfty
-    {Γ : Subgroup SL(2, ℤ)} {F : Type*} [FunLike F ℍ ℂ] [ModularFormClass F Γ k] (f : F) :
+end SlashInvariantFormClass
+
+open SlashInvariantFormClass
+
+namespace ModularFormClass
+
+theorem differentiableAt_comp_ofComplex [ModularFormClass F Γ k] {z : ℂ} (hz : 0 < im z) :
+    DifferentiableAt ℂ (f ∘ ofComplex) z :=
+  mdifferentiableAt_iff_differentiableAt.mp ((holo f _).comp z (mdifferentiableAt_ofComplex hz))
+
+theorem bounded_at_infty_comp_ofComplex [ModularFormClass F Γ k] :
     BoundedAtFilter I∞ (f ∘ ofComplex) := by
   simpa only [SlashAction.slash_one, ModularForm.toSlashInvariantForm_coe]
-    using IsBigO.comp_tendsto (ModularFormClass.bdd_at_infty f 1) tendsto_comap_im_ofComplex
+    using (ModularFormClass.bdd_at_infty f 1).comp_tendsto tendsto_comap_im_ofComplex
 
-theorem cuspform_zero_atIInfty {Γ : Subgroup SL(2, ℤ)} {F : Type*} [FunLike F ℍ ℂ]
-    [CuspFormClass F Γ k] (f : F) : ZeroAtFilter I∞ (f ∘ ofComplex) := by
-  simpa only [SlashAction.slash_one, CuspForm.toSlashInvariantForm_coe]
-    using (CuspFormClass.zero_at_infty f 1).comp tendsto_comap_im_ofComplex
-
-theorem modform_periodic {F : Type*} [FunLike F ℍ ℂ]
-    [ModularFormClass F (CongruenceSubgroup.Gamma 1) k] (f : F) (w : ℂ) :
-    (f ∘ ofComplex) (w + 1) = (f ∘ ofComplex) w := by
-  by_cases hw : 0 < im w
-  · have : 0 < im (w + 1) := by simp only [add_im, one_im, add_zero, hw]
-    simp only [Function.comp_apply, ofComplex_apply_of_im_pos hw, ofComplex_apply_of_im_pos this]
-    convert SlashInvariantForm.vAdd_width_periodic 1 k 1 f ⟨w, hw⟩ using 2
-    simp only [Nat.cast_one, Int.cast_one, mul_one, UpperHalfPlane.ext_iff, coe_mk_subtype,
-      coe_vadd, ofReal_one, add_comm]
-  · have : ¬0 < im (w + 1) := by simp only [add_im, one_im, add_zero, hw, not_false_eq_true]
-    simp only [Function.comp_apply, ofComplex_apply_of_im_nonpos (not_lt.mp hw),
-      ofComplex_apply_of_im_nonpos (not_lt.mp this)]
-
-theorem modform_hol {Γ : Subgroup SL(2, ℤ)} {F : Type*} [FunLike F ℍ ℂ] [ModularFormClass F Γ k]
-    (f : F) {z : ℂ} (hz : 0 < im z) :
-    DifferentiableAt ℂ (f ∘ ofComplex) z := by
-  rw [← mdifferentiableAt_iff_differentiableAt]
-  exact (ModularFormClass.holo f _).comp z (mdifferentiableAt_ofComplex z hz)
-
-theorem modform_hol_infty
-    {Γ : Subgroup SL(2, ℤ)} {F : Type*} [FunLike F ℍ ℂ] [ModularFormClass F Γ k] (f : F) :
-    ∀ᶠ z : ℂ in I∞, DifferentiableAt ℂ (f ∘ ofComplex) z :=
-  eventually_of_mem (preimage_mem_comap (Ioi_mem_atTop 0)) (fun _ hz ↦ modform_hol f hz)
-
-end ModformEquivs
-
-section Modforms
-
-theorem z_in_H {q : ℂ} (hq : Complex.abs q < 1) (hq_ne : q ≠ 0) : 0 < im (invQParam 1 q) := by
-  rw [im_invQParam]
-  apply mul_pos_of_neg_of_neg
-  · exact div_neg_of_neg_of_pos (neg_lt_zero.mpr zero_lt_one) Real.two_pi_pos
-  rwa [Real.log_neg_iff (Complex.abs.pos hq_ne)]
-
-/-- The analytic function `F` such that `f τ = F (exp (2 * π * I * τ))`. -/
-def cuspFunctionH (f : ℍ → ℂ) : ℂ → ℂ := cuspFunction 1 (f ∘ ofComplex)
-
-variable {k : ℤ}
-
-local notation  "Γ(" n ")"  => CongruenceSubgroup.Gamma n
-
-theorem eq_cuspFunctionH {F : Type*} [FunLike F ℍ ℂ] [ModularFormClass F Γ(1) k] (f : F) (z : ℍ) :
-    cuspFunctionH f (qParam 1 z) = f z := by
-  convert eq_cuspFunction one_ne_zero (modform_periodic f) z
-  simp only [Function.comp_apply, ofComplex_apply]
-
-theorem cusp_fcn_diff {F : Type*} [FunLike F ℍ ℂ] [ModularFormClass F Γ(1) k] (f : F)
-    {q : ℂ} (hq : Complex.abs q < 1) :
-    DifferentiableAt ℂ (cuspFunctionH f) q := by
+nonrec theorem differentiableAt_cuspFunction [NeZero n] [ModularFormClass F Γ(n) k]
+    {q : ℂ} (hq : q.abs < 1) :
+    DifferentiableAt ℂ (cuspFunction n f) q := by
+  have npos : 0 < (n : ℝ) := mod_cast (Nat.pos_iff_ne_zero.mpr (NeZero.ne _))
   rcases eq_or_ne q 0 with rfl | hq'
-  · exact differentiableAt_cuspFunction_zero zero_lt_one (modform_periodic f) (modform_hol_infty f)
-      (modform_bounded_atIInfty f)
-  · exact qParam_right_inv one_ne_zero hq' ▸ (differentiableAt_cuspFunction one_ne_zero
-      (modform_periodic f) (modform_hol f <| z_in_H hq hq'))
+  · exact differentiableAt_cuspFunction_zero npos (periodic_comp_ofComplex n f)
+      (eventually_of_mem (preimage_mem_comap (Ioi_mem_atTop 0))
+        (fun _ ↦ differentiableAt_comp_ofComplex f))
+      (bounded_at_infty_comp_ofComplex f)
+  · exact qParam_right_inv npos.ne' hq' ▸ differentiableAt_cuspFunction npos.ne'
+      (periodic_comp_ofComplex n f) <| differentiableAt_comp_ofComplex _
+        <| im_invQParam_pos_of_abs_lt_one npos hq hq'
 
-theorem cusp_fcn_vanish {F : Type*} [FunLike F ℍ ℂ] [CuspFormClass F Γ(1) k] (f : F) :
-    cuspFunctionH f 0 = 0 :=
-  cuspFunction_zero_of_zero_at_inf zero_lt_one (cuspform_zero_atIInfty f)
+lemma analyticAt_cuspFunction_zero [NeZero n] [ModularFormClass F Γ(n) k] :
+    AnalyticAt ℂ (cuspFunction n f) 0 :=
+  DifferentiableOn.analyticAt
+    (fun q hq ↦ (differentiableAt_cuspFunction _ _ hq).differentiableWithinAt)
+    (by simpa only [ball_zero_eq] using Metric.ball_mem_nhds (0 : ℂ) zero_lt_one)
 
-theorem exp_decay_of_cuspform {F : Type*} [FunLike F ℍ ℂ] [CuspFormClass F Γ(1) k] (f : F) :
-    IsBigO atImInfty f fun z ↦ Real.exp (-2 * π * z.im) := by
-  have := exp_decay_of_zero_at_inf zero_lt_one (modform_periodic f)
-    (modform_hol_infty f) (cuspform_zero_atIInfty f)
-  simp only [div_one] at this
-  convert this.comp_tendsto tendsto_coe_atImInfty using 1
-  ext τ
-  simp only [Function.comp_apply, ofComplex_apply]
+end ModularFormClass
 
-end Modforms
+open ModularFormClass
 
-end
+namespace CuspFormClass
+
+theorem zero_at_infty_comp_ofComplex [CuspFormClass F Γ k] : ZeroAtFilter I∞ (f ∘ ofComplex) := by
+  simpa only [SlashAction.slash_one, toSlashInvariantForm_coe]
+    using (zero_at_infty f 1).comp tendsto_comap_im_ofComplex
+
+theorem cuspFunction_apply_zero [NeZero n] [CuspFormClass F Γ(n) k] :
+    cuspFunction n f 0 = 0 :=
+  cuspFunction_zero_of_zero_at_inf
+    (mod_cast (Nat.pos_iff_ne_zero.mpr (NeZero.ne _)))
+    (zero_at_infty_comp_ofComplex f)
+
+theorem exp_decay_atImInfty [NeZero n] [CuspFormClass F Γ(n) k] :
+    f =O[atImInfty] fun τ ↦ Real.exp (-2 * π * τ.im / n) := by
+  simpa only [comp_def, ofComplex_apply] using
+    (exp_decay_of_zero_at_inf
+      (mod_cast (Nat.pos_iff_ne_zero.mpr (NeZero.ne _)))
+      (periodic_comp_ofComplex n f)
+      (eventually_of_mem (preimage_mem_comap (Ioi_mem_atTop 0))
+        (fun _ ↦ differentiableAt_comp_ofComplex f))
+      (zero_at_infty_comp_ofComplex f)
+    ).comp_tendsto tendsto_coe_atImInfty
+
+end CuspFormClass
