9cda456fe897910e91947139eecd660da2319ca0
Markus Himmel <markus@lean-fro.org>
2024-11-11 18:49:21 +0100

    shake

--- Mathlib/CategoryTheory/Limits/Indization/LocallySmall.lean
+++ Mathlib/CategoryTheory/Limits/Indization/LocallySmall.lean
@@ -3,7 +3,7 @@ Copyright (c) 2024 Markus Himmel. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Markus Himmel
 -/
-import Mathlib.CategoryTheory.Limits.Preserves.Yoneda
+import Mathlib.CategoryTheory.Limits.Preserves.Ulift
 import Mathlib.CategoryTheory.Limits.IndYoneda
 import Mathlib.CategoryTheory.Limits.Indization.IndObject
 
