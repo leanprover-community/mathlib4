b12eb4107118d895284a98bb1bc47349a1290d84
adomani <adomani@gmail.com>
2024-11-11 22:55:36 +0000

    align

--- scripts/maintainer_merge_message.sh
+++ scripts/maintainer_merge_message.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 AUTHOR="${1:-AUTHOR not set}"         # the author of the PR
-M_or_D="**${2:-M_or_D not set}**"         # `merge` or `delegate`
+M_or_D="**${2:-M_or_D not set}**"     # `merge` or `delegate`
 EVENT_NAME="${3:-EVENT_NAME not set}" # one of `issue_comment`, `pull_request_review` or `pull_request_review_comment`
                                       # to be converted to `comment`, `review` or `review comment`
 PR="${4:-PR not set}"                 # the number of the PR
