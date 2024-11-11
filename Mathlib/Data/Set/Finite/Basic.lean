0fe422cdb20ef0721aca1db58a6aee99d1abe73f
Anne C.A. Baanen <vierkantor@vierkantor.com>
2024-11-11 14:45:28 +0100

    Merge `Set/Finite/Defs.lean` and `Finite/Defs.lean`

--- Mathlib/Data/Set/Finite/Basic.lean
+++ Mathlib/Data/Set/Finite/Basic.lean
@@ -3,8 +3,8 @@ Copyright (c) 2017 Johannes Hölzl. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl, Mario Carneiro, Kyle Miller
 -/
+import Mathlib.Data.Finite.Defs
 import Mathlib.Data.Fintype.Card
-import Mathlib.Data.Set.Finite.Defs
 
 /-!
 # Finite sets
