0699bfc945bc927dd5f1242c6c116ff0094ce1c8
Floris van Doorn <fpvdoorn@gmail.com>
2024-11-11 15:55:14 +0100

    add a few more examples

--- MathlibTest/Deriv.lean
+++ MathlibTest/Deriv.lean
@@ -10,3 +10,16 @@ example (x : ℝ) : deriv (λ x => cos x + 2 * sin x) x = -sin x + 2 * cos x := 
 example (x : ℝ) :
     deriv (fun x ↦ cos (sin x) * exp x) x = (cos (sin x) - sin (sin x) * cos x) * exp x := by
   simp; ring
+
+/- for more complicated examples (with more nested functions) you need to increase the
+`maxDischargeDepth`. -/
+
+example (x : ℝ) :
+    deriv (fun x ↦ sin (sin (sin x)) + sin x) x =
+    cos (sin (sin x)) * (cos (sin x) * cos x) + cos x := by
+  simp (config := {maxDischargeDepth := 3})
+
+example (x : ℝ) :
+    deriv (fun x ↦ sin (sin (sin x)) ^ 10 + sin x) x =
+    10 * sin (sin (sin x)) ^ 9 * (cos (sin (sin x)) * (cos (sin x) * cos x)) + cos x := by
+  simp (config := {maxDischargeDepth := 5})
