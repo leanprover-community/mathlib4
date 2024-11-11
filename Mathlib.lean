7bcb227629e92c0cf0c45ca3ce9f15ae54e9a272
Yury G. Kudryashov <urkud@urkud.name>
2024-11-11 12:43:26 +0000

    feat(Data/Fin/Parity): add lemmas about parity of `Fin` numbers (#8960)
    Co-authored-by: Iván Renison <85908989+IvanRenison@users.noreply.github.com>
    
    
    
    Co-authored-by: Iván Renison <85908989+IvanRenison@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -2292,6 +2292,7 @@ import Mathlib.Data.FP.Basic
 import Mathlib.Data.Fin.Basic
 import Mathlib.Data.Fin.Fin2
 import Mathlib.Data.Fin.FlagRange
+import Mathlib.Data.Fin.Parity
 import Mathlib.Data.Fin.SuccPred
 import Mathlib.Data.Fin.Tuple.Basic
 import Mathlib.Data.Fin.Tuple.BubbleSortInduction
