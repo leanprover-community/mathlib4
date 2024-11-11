00a7e31688a3325fbb62ecaef25d8714cc2cf732
Christian Merten <christian@merten.dev>
2024-11-11 10:41:31 +0100

    fix?

--- Mathlib/AlgebraicGeometry/Morphisms/Etale.lean
+++ Mathlib/AlgebraicGeometry/Morphisms/Etale.lean
@@ -1,4 +1,5 @@
-/- Copyright (c) 2024 Christian Merten. All rights reserved.
+/-
+Copyright (c) 2024 Christian Merten. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Christian Merten
 -/
