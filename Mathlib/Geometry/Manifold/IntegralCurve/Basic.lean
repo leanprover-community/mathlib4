55ffaa733fe2b4f395546be149e555d0fa80a6fb
Winston Yin <winstonyin@gmail.com>
2024-11-11 13:28:29 -0800

    docstring

--- Mathlib/Geometry/Manifold/IntegralCurve/Basic.lean
+++ Mathlib/Geometry/Manifold/IntegralCurve/Basic.lean
@@ -12,6 +12,14 @@ Let `M` be a manifold and `v : (x : M) → TangentSpace I x` be a vector field o
 curve of `v` is a function `γ : ℝ → M` such that the derivative of `γ` at `t` equals `v (γ t)`. The
 integral curve may only be defined for all `t` within some subset of `ℝ`.
 
+This is the first of a series of files, organised as follows:
+* `Mathlib.Geometry.Manifold.IntegralCurve.Basic` (this file): Basic definitions and lemmas relating
+them to each other and to continuity and differentiability
+* `Mathlib.Geometry.Manifold.IntegralCurve.Transform`: Lemmas about translating or scaling the
+domain of an integral curve by a constant
+* `Mathlib.Geometry.Manifold.IntegralCurve.ExistUnique`: Local existence and uniqueness theorems for
+integral curves
+
 ## Main definitions
 
 Let `v : M → TM` be a vector field on `M`, and let `γ : ℝ → M`.
