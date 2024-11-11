1fc93cf6430147b2bca143b11ba3b10c31878675
Lorenzo Luccioli <lorenzoluccioli@gmail.com>
2024-11-11 18:21:16 +0100

    move some lemmas

--- Mathlib/Data/Real/EReal.lean
+++ Mathlib/Data/Real/EReal.lean
@@ -662,6 +662,14 @@ lemma toENNReal_coe {x : ENNReal} : (x : EReal).toENNReal = x := by
   rwa [toENNReal, if_neg _, toReal_coe_ennreal, ENNReal.ofReal_toReal_eq_iff]
   simp [h_top]
 
+@[simp] lemma real_coe_toENNReal (x : ℝ) : (x : EReal).toENNReal = ENNReal.ofReal x := rfl
+
+@[simp]
+lemma toReal_toENNReal {x : EReal} (hx : 0 ≤ x) : x.toENNReal.toReal = x.toReal := by
+  by_cases h : x = ⊤
+  · simp [h]
+  · simp [h, toReal_nonneg hx]
+
 lemma toENNReal_eq_toENNReal {x y : EReal} (hx : 0 ≤ x) (hy : 0 ≤ y) :
     x.toENNReal = y.toENNReal ↔ x = y := by
   induction x <;> induction y <;> simp_all
@@ -679,14 +687,6 @@ lemma toENNReal_lt_toENNReal {x y : EReal} (hx : 0 ≤ x) (hxy : x < y) :
     x.toENNReal < y.toENNReal :=
   lt_of_le_of_ne (toENNReal_le_toENNReal hxy.le)
     fun h ↦ hxy.ne <| (toENNReal_eq_toENNReal hx (hx.trans_lt hxy).le).mp h
-
-@[simp] lemma real_coe_toENNReal (x : ℝ) : (x : EReal).toENNReal = ENNReal.ofReal x := rfl
-
-@[simp]
-lemma toReal_toENNReal {x : EReal} (hx : 0 ≤ x) : x.toENNReal.toReal = x.toReal := by
-  by_cases h : x = ⊤
-  · simp [h]
-  · simp [h, toReal_nonneg hx]
 
 /-! ### nat coercion -/
 
