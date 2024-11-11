c0645f6dbd9b207759ef9afccf8739eb9f6cae03
Ahmad Alkhalawi <46321632+4hma4d@users.noreply.github.com>
2024-11-11 13:22:05 +0300

    Update Mathlib/RingTheory/DedekindDomain/IntegralClosure.lean
    Co-authored-by: Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>

--- Mathlib/RingTheory/DedekindDomain/IntegralClosure.lean
+++ Mathlib/RingTheory/DedekindDomain/IntegralClosure.lean
@@ -188,7 +188,7 @@ theorem IsIntegralClosure.isNoetherianRing [IsIntegrallyClosed A] [IsNoetherianR
 /-- If `L` is a finite separable extension of `K = Frac(A)`, where `A` is
 integrally closed and Noetherian, the integral closure `C` of `A` in `L` is
 finite over `A`. -/
-theorem IsIntegralClosure.isFinite [IsIntegrallyClosed A] [IsNoetherianRing A] :
+theorem IsIntegralClosure.finite [IsIntegrallyClosed A] [IsNoetherianRing A] :
     Module.Finite A C := by
   haveI := IsIntegralClosure.isNoetherian A K L C
   exact Module.IsNoetherian.finite A C
