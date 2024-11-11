b6ca153685cf77d217a0521ab38735cd2cb438b8
Kim Morrison <kim@tqft.net>
2024-11-11 11:36:33 +1100

    Update scripts/docker_push.sh

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -1,6 +1,6 @@
 # This script attempts to build all the docker images, and then push then to the repository.
 # You'll need to have run `docker login` already;
-# check with scott.morrison@gmail.com for credentials.
+# check with Mathlib maintainers on Zulip for credentials.
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 DATE=$(date +"%Y-%m-%d")
