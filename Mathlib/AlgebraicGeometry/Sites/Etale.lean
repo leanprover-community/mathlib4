f436cd4a81c0ba8b36da36e92f6f965ee5fe2ff1
Christian Merten <christian@merten.dev>
2024-11-11 10:27:23 +0100

    feat(AlgebraicGeometry): the etale site

---
+++ Mathlib/AlgebraicGeometry/Sites/Etale.lean
@@ -0,0 +1,37 @@
+/-
+Copyright (c) 2024 Christian Merten. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Christian Merten
+-/
+import Mathlib.AlgebraicGeometry.Sites.BigZariski
+import Mathlib.AlgebraicGeometry.Morphisms.Etale
+
+/-!
+
+# The étale site
+
+In this file we define the étale topology as a Grothendieck topology on the category
+of schemes.
+
+-/
+
+universe v u
+
+open CategoryTheory MorphismProperty Limits
+
+namespace AlgebraicGeometry.Scheme
+
+/-- The étale pretopology on the category of schemes. -/
+def etalePretopology : Pretopology Scheme.{u} :=
+  pretopology @IsEtale
+
+/-- The étale topology on the category of schemes. -/
+abbrev etaleTopology : GrothendieckTopology Scheme.{u} :=
+  etalePretopology.toGrothendieck
+
+lemma zariskiTopology_le_etaleTopology : zariskiTopology ≤ etaleTopology := by
+  apply grothendieckTopology_le_grothendieckTopology
+  intro X Y f hf
+  infer_instance
+
+end AlgebraicGeometry.Scheme
