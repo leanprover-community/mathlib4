d3dd1dcfbaca0cd6da431c494976c75a2503510c
Winston Yin <winstonyin@gmail.com>
2024-11-12 02:53:09 +0000

    chore: split `IntegralCurve` (#18833)
    Split `Mathlib.Geometry.Manifold.IntegralCurve` into three files:
    * `Mathlib.Geometry.Manifold.IntegralCurve.Basic`: Definitions and basic lemmas about continuity and derivatives of integral curves
    * `Mathlib.Geometry.Manifold.IntegralCurve.Transform`: Lemmas about translation and scaling of the domain of integral curves by a constant
    * `Mathlib.Geometry.Manifold.IntegralCurve.ExistUnique`: Local existence and uniqueness theorems for integral curves
    
    No content has been substantively changed from the original file. This is to prepare for #9013, which proves a sufficient condition for global existence of integral curves. That'll go in another new file.

--- Mathlib.lean
+++ Mathlib.lean
@@ -2954,7 +2954,9 @@ import Mathlib.Geometry.Manifold.Diffeomorph
 import Mathlib.Geometry.Manifold.Instances.Real
 import Mathlib.Geometry.Manifold.Instances.Sphere
 import Mathlib.Geometry.Manifold.Instances.UnitsOfNormedAlgebra
-import Mathlib.Geometry.Manifold.IntegralCurve
+import Mathlib.Geometry.Manifold.IntegralCurve.Basic
+import Mathlib.Geometry.Manifold.IntegralCurve.ExistUnique
+import Mathlib.Geometry.Manifold.IntegralCurve.Transform
 import Mathlib.Geometry.Manifold.InteriorBoundary
 import Mathlib.Geometry.Manifold.LocalDiffeomorph
 import Mathlib.Geometry.Manifold.LocalInvariantProperties
