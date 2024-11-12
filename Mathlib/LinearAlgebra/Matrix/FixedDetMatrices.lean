ed4dadd005028376ea05ae0d6aada2fc51e5b49a
Chris Birkbeck <c.birkbeck@uea.ac.uk>
2024-11-12 08:26:43 +0000

    remove line

--- Mathlib/LinearAlgebra/Matrix/FixedDetMatrices.lean
+++ Mathlib/LinearAlgebra/Matrix/FixedDetMatrices.lean
@@ -26,7 +26,6 @@ namespace FixedDetMatrix
 
 open ModularGroup Matrix SpecialLinearGroup MatrixGroups
 
-
 /--Extensionality theorem for `FixedDetMatrix` with respect to the underlying matrix, not
 entriwise. -/
 lemma ext' {m : R} {A B : FixedDetMatrix n R m} (h : A.1 = B.1) : A = B := by
