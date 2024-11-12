e1f29f33756305ffb0dbcd17b995dcb538cc3440
Violeta Hernández <vi.hdz.p@gmail.com>
2024-11-12 06:51:45 +0000

    chore: `Data/List/Join` → `Data/List/Flatten` (#18895)

--- Mathlib/Data/List/Permutation.lean
+++ Mathlib/Data/List/Permutation.lean
@@ -3,7 +3,7 @@ Copyright (c) 2014 Parikshit Khanna. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Parikshit Khanna, Jeremy Avigad, Leonardo de Moura, Floris van Doorn, Mario Carneiro
 -/
-import Mathlib.Data.List.Join
+import Mathlib.Data.List.Flatten
 import Mathlib.Data.Nat.Factorial.Basic
 import Mathlib.Data.List.Count
 import Mathlib.Data.List.Duplicate
