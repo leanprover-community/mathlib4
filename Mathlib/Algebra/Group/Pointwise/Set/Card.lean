1a9a7b4ea4fb7fcd4232e0465b210c3fc8f962d9
Yaël Dillies <yael.dillies@gmail.com>
2024-11-13 22:09:06 +0000

    chore: move pointwise set files to `Algebra._.Pointwise`

--- Mathlib/Algebra/Group/Pointwise/Set/Card.lean
+++ Mathlib/Algebra/Group/Pointwise/Set/Card.lean
@@ -3,8 +3,8 @@ Copyright (c) 2024 Yaël Dillies. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yaël Dillies
 -/
+import Mathlib.Algebra.Group.Pointwise.Set.Finite
 import Mathlib.Data.Set.Card
-import Mathlib.Data.Set.Pointwise.Finite
 import Mathlib.SetTheory.Cardinal.Finite
 
 /-!
