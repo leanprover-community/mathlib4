3597f15bb5cd3e841064f98286c5549b22818a99
Anne Baanen <vierkantor@vierkantor.com>
2024-11-13 12:14:52 +0000

    chore(LinearAlgebra/Matrix): split off `Matrix.stdBasis` to its own file (#18733)
    Previously, `LinearAlgebra.StdBasis` also defined the standard basis for matrices. We don't need matrices to define finite-dimensional spaces.
    
    It would be nice to instead swap the import order: `LinearAlgebra.StdBasis` imported by `LinearAlgebra.Matrix.Basis`, but unfortunately that would cause a cycle with `LinearAlgebra.Matrix.ToLin`. Not entirely sure if that dependency is worth the hassle of further untangling.

--- Mathlib.lean
+++ Mathlib.lean
@@ -3281,6 +3281,7 @@ import Mathlib.LinearAlgebra.Matrix.SchurComplement
 import Mathlib.LinearAlgebra.Matrix.SesquilinearForm
 import Mathlib.LinearAlgebra.Matrix.SpecialLinearGroup
 import Mathlib.LinearAlgebra.Matrix.Spectrum
+import Mathlib.LinearAlgebra.Matrix.StdBasis
 import Mathlib.LinearAlgebra.Matrix.Symmetric
 import Mathlib.LinearAlgebra.Matrix.ToLin
 import Mathlib.LinearAlgebra.Matrix.ToLinearEquiv
