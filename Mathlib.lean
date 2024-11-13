b16ef7b2bb07d0a169b69a48ed3ecc5311d1ceb3
Thomas Browning <tb65536@uw.edu>
2024-11-13 17:52:25 +0000

    feat(GroupTheory/SpecificGroups/ZGroup): Predicate for groups whose Sylow subgroups are all cyclic (#18575)
    This PR adds a predicate stating that all Sylow subgroups of `G` are cyclic (for instance, when `G` has squarefree order). Ultimately, the goal is to show that any such group is a semidirect product of two cyclic groups.

--- Mathlib.lean
+++ Mathlib.lean
@@ -3101,6 +3101,7 @@ import Mathlib.GroupTheory.SpecificGroups.Cyclic
 import Mathlib.GroupTheory.SpecificGroups.Dihedral
 import Mathlib.GroupTheory.SpecificGroups.KleinFour
 import Mathlib.GroupTheory.SpecificGroups.Quaternion
+import Mathlib.GroupTheory.SpecificGroups.ZGroup
 import Mathlib.GroupTheory.Subgroup.Center
 import Mathlib.GroupTheory.Subgroup.Centralizer
 import Mathlib.GroupTheory.Subgroup.Saturated
