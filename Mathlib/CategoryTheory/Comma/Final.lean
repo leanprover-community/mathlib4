0c615f7f050be6d36e147d238b4571a3d7a99984
Jakob von Raumer <jakob@von-raumer.de>
2024-11-14 15:18:50 +0700

    Comma.post on equivalences

--- Mathlib/CategoryTheory/Comma/Final.lean
+++ Mathlib/CategoryTheory/Comma/Final.lean
@@ -24,7 +24,7 @@ variable {B : Type v₁} [Category.{v₁} B]
 variable {T : Type v₁} [Category.{v₁} T]
 variable (L : A ⥤ T) (R : B ⥤ T)
 
-lemma final_fst [R.Final] : (fst L R).Final := by
+lemma final_fst_small [R.Final] : (fst L R).Final := by
   rw  [Functor.final_iff_isIso_colimit_pre]
   intro G
   let i : colimit G ≅ colimit (fst L R ⋙ G) :=
@@ -35,7 +35,7 @@ lemma final_fst [R.Final] : (fst L R).Final := by
   apply colimit.hom_ext
   intro ⟨l, r, f⟩
   simp only [comp_obj, fst_obj, colimit.ι_pre, grothendieckPrecompFunctorEquivalence_functor,
-    Iso.trans_inv, Iso.symm_inv, colimitIsoColimitGrothendieck_inv, Category.assoc, i]
+    Iso.trans_inv, Iso.symm_inv, Category.assoc, i]
   simp only [← Category.assoc, ← Iso.comp_inv_eq, Iso.eq_comp_inv]
   simp only [ι_colimitIsoOfIsLeftKanExtension_inv, comp_obj, Category.assoc,
     HasColimit.isoOfNatIso_ι_hom, fiberwiseColimit_obj, functor_obj, Cat.of_α,
@@ -43,8 +43,7 @@ lemma final_fst [R.Final] : (fst L R).Final := by
     leftKanExtensionUnit_leftKanExtensionObjIsoColimit_hom_assoc,
     HasColimit.isoOfNatIso_ι_inv_assoc, proj_obj, mk_left, Grothendieck.ι_obj, grothendieckProj_obj,
     isoWhiskerRight_inv, whiskerRight_app, ιCompGrothendieckProj_inv_app, Functor.map_id,
-    Category.id_comp]
-  rw [ι_colimitFiberwiseColimitIso_hom_assoc]
+    Category.id_comp, ι_colimitIsoColimitGrothendieck_hom_assoc]
   simp only [functor_obj, Cat.of_α, Grothendieck.ι_obj, comp_obj, grothendieckProj_obj, mk_left,
     Final.colimitIso, asIso_inv, asIso_hom]
   rw [← colimit.w (grothendieckProj L ⋙ G) (j' := (Grothendieck.pre _ R).obj ⟨r, .mk f⟩)
@@ -53,6 +52,31 @@ lemma final_fst [R.Final] : (fst L R).Final := by
     colimit.ι (grothendieckPrecompFunctorToComma L R ⋙ fst L R ⋙ G) :=
     rfl
   simp [this, grothendieckPrecompFunctorToComma]
+
+variable {A : Type u₁} [Category.{v₁} A]
+variable {B : Type u₂} [Category.{v₂} B]
+variable {T : Type v₃} [Category.{v₃} T]
+variable (L : A ⥤ T) (R : B ⥤ T)
+
+#check AsSmall.up
+
+lemma final_fst [R.Final] : (fst L R).Final := by
+  let sA : A ≌ AsSmall.{max u₁ u₂ u₃ v₁ v₂ v₃} A := AsSmall.equiv
+  let sB : B ≌ AsSmall.{max u₁ u₂ u₃ v₁ v₂ v₃} B := AsSmall.equiv
+  let sT : T ≌ AsSmall.{max u₁ u₂ u₃ v₁ v₂ v₃} T := AsSmall.equiv
+  let L' := sA.inverse ⋙ L ⋙ sT.functor
+  let R' := sB.inverse ⋙ R ⋙ sT.functor
+  let fC : Comma L' R' ⥤ Comma L R :=
+    preLeft sA.inverse _ _ ⋙ preRight _ sB.inverse _ ⋙ Functor.IsEquivalence.inv (post L R sT.functor)
+
+  let sC : Comma L R ≌ Comma L' R' := by
+    sorry
+  haveI : Final (fst L' R') := final_fst_small _ _
+  haveI : Final (sC.functor ⋙ fst L' R' ⋙ sA.inverse) := inferInstance
+  apply final_of_natIso (F := (sC.functor ⋙ fst L' R' ⋙ sA.inverse))
+  sorry
+
+#check IsEquivalence
 
 end Comma
 
