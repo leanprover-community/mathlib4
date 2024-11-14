e84a8f3afcb08c2de8cc42cd5387608b866dfc47
Eric Wieser <wieser.eric@gmail.com>
2024-11-14 00:30:53 +0000

    fix yaml processing

--- scripts/check-yaml.lean
+++ scripts/check-yaml.lean
@@ -43,6 +43,7 @@ def processDb (decls : ConstMap) : String × String → IO Bool
     return false
 
 unsafe def main : IO Unit := do
+  Lean.enableInitializersExecution
   CoreM.withImportModules #[`Mathlib, `Archive]
       (searchPath := compile_time_search_path%) (trustLevel := 1024) do
     let decls := (←getEnv).constants
