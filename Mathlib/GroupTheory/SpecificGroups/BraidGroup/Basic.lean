f9236b1ac82aa3b960f788254a34c38b453c5310
Hannah Fechtner <hfechtne@andrew.cmu.edu>
2024-11-13 23:37:57 -0500

    updated version

--- Mathlib/GroupTheory/SpecificGroups/BraidGroup/Basic.lean
+++ Mathlib/GroupTheory/SpecificGroups/BraidGroup/Basic.lean
@@ -16,22 +16,21 @@ Artin's braid group on infintely many strands
 * `BraidGroupInf ` is the braid group on infinitely many strands, defined as a PresentedGroup
   on the braid relations
 
-* `braid_group n` is the braid group on n+1 strands (thus n generators) - coming in the next PR
-
 ## Main results
-* none in this PR
-
+* will add in as they get PR'd
 
 ## Tags
 braid group
 
 ## TODO
+* `braid_group n` is the braid group on n+1 strands (thus n generators)
 
 -/
 
-namespace BraidGroup
 
-/-- the set of relations on FreeGroup ℕ corresponding to vaild braid moves -/
+/-- the set of relations on FreeGroup ℕ corresponding to valid braid moves. generators are indexed
+by natural numbers. commutativity holds if the generators are at least 2 apart. if two generators
+a and b are adjacent, then aba is congruent to bab -/
 def braid_rels_inf : Set (FreeGroup ℕ) :=
   { r | ∃ i j : ℕ , 1 = Nat.dist i j ∧ r = .of i * .of j * .of i *
     (.of j)⁻¹ * (.of i)⁻¹ * (.of j)⁻¹} ∪
@@ -43,17 +42,19 @@ def BraidGroupInf := PresentedGroup braid_rels_inf
 instance : Group BraidGroupInf := by
   unfold BraidGroupInf; infer_instance
 
+namespace BraidGroupInf
+
 /-- the ith generator of the infinite braid group, whose geometric interpretation is crossing
 the ith strand over the (i+1)th strand -/
-def σi (k : ℕ) : BraidGroupInf := PresentedGroup.of k
+def σ (k : ℕ) : BraidGroupInf := PresentedGroup.of k
 
-theorem BraidGroupInf.braid {i j : ℕ} (h : 1 = Nat.dist i j): σi i * σi j * σi i =
-    σi j * σi i * σi j := by
+theorem BraidGroupInf.braid {i j : ℕ} (h : 1 = Nat.dist i j): σ i * σ j * σ i =
+    σ j * σ i * σ j := by
   symm; rw [← mul_inv_eq_one]
   exact QuotientGroup.eq.mpr <| Subgroup.subset_normalClosure <| Or.inl <| Exists.intro i <|
     Exists.intro j <| ⟨h, rfl⟩
 
-theorem BraidGroupInf.comm {i j : ℕ} (h : 2 ≤ Nat.dist i j) : σi i * σi j = σi j * σi i := by
+theorem BraidGroupInf.comm {i j : ℕ} (h : 2 ≤ Nat.dist i j) : σ i * σ j = σ j * σ i := by
   symm; rw [← mul_inv_eq_one]
   apply QuotientGroup.eq.mpr
   apply Subgroup.subset_normalClosure
@@ -72,10 +73,10 @@ def toGroup (h : ∀ r ∈ braid_rels_inf, FreeGroup.lift f r = 1) : BraidGroupI
 
 @[simp]
 theorem toGroup.of (h : ∀ r ∈ braid_rels_inf, FreeGroup.lift f r = 1) {x : ℕ} :
-    toGroup h (σi x) = f x := PresentedGroup.toGroup.of h
+    toGroup h (σ x) = f x := PresentedGroup.toGroup.of h
 
 theorem toGroup.unique (h : ∀ r ∈ braid_rels_inf, FreeGroup.lift f r = 1) (g : BraidGroupInf →* G)
-    (hg : ∀ x : ℕ, g (σi x) = f x) : ∀ {x}, g x = toGroup h x :=
+    (hg : ∀ x : ℕ, g (σ x) = f x) : ∀ {x}, g x = toGroup h x :=
   PresentedGroup.toGroup.unique h g hg
 
 end toGroup
