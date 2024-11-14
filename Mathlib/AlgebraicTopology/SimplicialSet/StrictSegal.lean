bf91600b81a9057c1c36312bcb1db0cbbd1e1f26
emilyriehl <eriehl@jhu.edu>
2024-11-14 13:12:37 -0500

    preliminary version of the StrictSegal class with some commented out terms that need to be cut

--- Mathlib/AlgebraicTopology/SimplicialSet/StrictSegal.lean
+++ Mathlib/AlgebraicTopology/SimplicialSet/StrictSegal.lean
@@ -35,22 +35,32 @@ variable (X : SSet.{u})
 
 /-- A simplicial set `X` satisfies the strict Segal condition if its simplices are uniquely
 determined by their spine. -/
-class StrictSegal : Prop where
-  segal : ∀ (n : ℕ), Function.Bijective (X.spine n)
+class StrictSegal where
+  spineToSimplex {n : ℕ} : Path X n → X _[n]
+  spine_spineToSimplex {n : ℕ} (f : Path X n) : X.spine n (spineToSimplex f) = f
+  spineToSimplex_spine {n : ℕ} (Δ : X _[n]) : spineToSimplex (X.spine n Δ) = Δ
 
-/-- In the presence of the strict Segal condition, a path of length `n` extends to an `n`-simplex
-whose spine is that path. -/
-noncomputable def spineToSimplex {X : SSet.{u}} [StrictSegal X] {n : ℕ} : Path X n → X _[n] :=
-  (Equiv.ofBijective _ (StrictSegal.segal n)).invFun
-
+-- /-- In the presence of the strict Segal condition, a path of length `n` extends to an `n`-simplex
+-- whose spine is that path. -/
+-- noncomputable def spineToSimplex {X : SSet.{u}} [StrictSegal X] {n : ℕ} : Path X n → X _[n] :=
+--   (Equiv.ofBijective _ (StrictSegal.segal n)).invFun
 
 namespace StrictSegal
 variable {X : SSet.{u}} [StrictSegal X] {n : ℕ}
 
-@[simp]
-theorem spine_spineToSimplex (f : Path X n) :
-    X.spine n (spineToSimplex f) = f :=
-  (Equiv.ofBijective _ (segal n)).right_inv f
+-- @[simp]
+-- theorem spine_spineToSimplex (f : Path X n) :
+--     X.spine n (spineToSimplex f) = f :=
+--   (Equiv.ofBijective _ (segal n)).right_inv f
+
+/-- The fields of `StrictSegal` define an equivalence between `X _[n]` and `Path X n`.-/
+def spineEquiv (n : ℕ) : X _[n] ≃ Path X n where
+  toFun := spine X n
+  invFun := spineToSimplex
+  left_inv := spineToSimplex_spine
+  right_inv := spine_spineToSimplex
+
+theorem spineInjective {n : ℕ} : Function.Injective (spineEquiv (X := X) n) := Equiv.injective _
 
 @[simp]
 theorem spineToSimplex_vertex (i : Fin (n + 1)) (f : Path X n) :
@@ -64,13 +74,15 @@ theorem spineToSimplex_arrow (i : Fin n) (f : Path X n) :
 
 /-- In the presence of the strict Segal condition, a path of length `n` can be "composed" by taking
 the diagonal edge of the resulting `n`-simplex. -/
-noncomputable def spineToDiagonal (f : Path X n) : X _[1] := diagonal X (spineToSimplex f)
+def spineToDiagonal (f : Path X n) : X _[1] := diagonal X (spineToSimplex f)
 
 @[simp]
 theorem spineToSimplex_interval (f : Path X n) (j l : ℕ) (hjl : j + l ≤  n)  :
     X.map (subinterval j l hjl).op (spineToSimplex f) =
       spineToSimplex (Path.interval f j l hjl) := by
-  apply (segal _).injective
+  apply spineInjective
+  unfold spineEquiv
+  dsimp
   rw [spine_spineToSimplex]
   convert spine_map_subinterval X j l hjl (spineToSimplex f)
   exact Eq.symm (spine_spineToSimplex f)
@@ -95,24 +107,24 @@ variable {C : Type*} [Category C] {n : ℕ}
 
 /-- Simplices in the nerve of categories are uniquely determined by their spine. Indeed, this
 property describes the essential image of the nerve functor.-/
-instance strictSegal (C : Type u) [Category.{v} C] : StrictSegal (nerve C) where
-  segal n := by
-    constructor
-    · intro Δ Δ' h
-      exact ComposableArrows.ext
-        (fun i ↦ Functor.congr_obj (congr_fun (congr_arg Path.vertex h) i) 0)
-        (fun i hi ↦
-          Functor.congr_hom (congr_fun (congr_arg Path.arrow h) ⟨i, hi⟩) (show 0 ⟶ 1 by tauto))
-    · intro F
-      refine ⟨ComposableArrows.mkOfObjOfMapSucc (fun i ↦ (F.vertex i).obj 0)
-        (fun i ↦ eqToHom (Functor.congr_obj (F.arrow_src i).symm 0) ≫
-          (F.arrow i).map' 0 1 ≫ eqToHom (Functor.congr_obj (F.arrow_tgt i) 0)), ?_⟩
-      ext i
-      · exact ComposableArrows.ext₀ rfl
-      · refine ComposableArrows.ext₁ ?_ ?_ ?_
-        · exact Functor.congr_obj (F.arrow_src i).symm 0
-        · exact Functor.congr_obj (F.arrow_tgt i).symm 0
-        · dsimp
-          apply ComposableArrows.mkOfObjOfMapSucc_map_succ
+noncomputable instance strictSegal (C : Type u) [Category.{v} C] : StrictSegal (nerve C) where
+  spineToSimplex {n} F :=
+    ComposableArrows.mkOfObjOfMapSucc (fun i ↦ (F.vertex i).obj 0)
+      (fun i ↦ eqToHom (Functor.congr_obj (F.arrow_src i).symm 0) ≫
+        (F.arrow i).map' 0 1 ≫ eqToHom (Functor.congr_obj (F.arrow_tgt i) 0))
+  spine_spineToSimplex {n} F := by
+    ext i
+    · exact ComposableArrows.ext₀ rfl
+    · refine ComposableArrows.ext₁ ?_ ?_ ?_
+      · exact Functor.congr_obj (F.arrow_src i).symm 0
+      · exact Functor.congr_obj (F.arrow_tgt i).symm 0
+      · dsimp
+        apply ComposableArrows.mkOfObjOfMapSucc_map_succ
+  spineToSimplex_spine {n} F := by
+    fapply ComposableArrows.ext
+    · intro i
+      rfl
+    · intro i hi
+      apply ComposableArrows.mkOfObjOfMapSucc_map_succ
 
 end Nerve
