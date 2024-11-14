66d1c9523b1d452f29f5fe8d38e58e760fbab7a5
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2024-11-14 11:32:03 +0100

    suggestion from review

--- Mathlib/NumberTheory/EulerProduct/ExpLog.lean
+++ Mathlib/NumberTheory/EulerProduct/ExpLog.lean
@@ -39,8 +39,7 @@ theorem exp_tsum_primes_log_eq_tsum {f : ℕ →*₀ ℂ} (hsum : Summable (‖f
   have hp (p : Nat.Primes) : 1 - f p ≠ 0 :=
     fun h ↦ norm_one (α := ℂ) ▸ (sub_eq_zero.mp h) ▸ hs p.prop.one_lt |>.false
   have H := hsum.of_norm.clog_one_sub.neg.subtype {p | p.Prime} |>.hasSum.cexp.tprod_eq
-  simp only [Set.coe_setOf, Set.mem_setOf_eq, Function.comp_apply] at H
-  conv at H => enter [1, 1, p]; rw [exp_neg, exp_log (hp p)]
+  simp only [Set.coe_setOf, Set.mem_setOf_eq, Function.comp_apply, exp_neg, exp_log (hp _)] at H
   exact H.symm.trans <| eulerProduct_completely_multiplicative_tprod hsum
 
 end EulerProduct
