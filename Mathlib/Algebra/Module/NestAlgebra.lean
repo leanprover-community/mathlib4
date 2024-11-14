0094bdcbbfc6d7936fa5d9afbae620b72818941d
Christopher Hoskin <christopher.hoskin@gmail.com>
2024-11-14 08:15:28 +0000

    Use Module.End.invtSubmodule in existing def

--- Mathlib/Algebra/Module/NestAlgebra.lean
+++ Mathlib/Algebra/Module/NestAlgebra.lean
@@ -8,6 +8,7 @@ import Mathlib.Algebra.Module.Submodule.Lattice
 import Mathlib.Order.Sublattice
 import Mathlib.Algebra.Algebra.Subalgebra.Basic
 import Mathlib.LinearAlgebra.Basis.Defs
+import Mathlib.Algebra.Module.Submodule.Invariant
 
 /-!
 # Nest algebras
@@ -40,29 +41,18 @@ variable [CommSemiring R] [AddCommMonoid M] [Module R M]
 The nest algebra of a nest
 -/
 def NestAlg (N : Sublattice (Submodule R M)) : Subalgebra R (M →ₗ[R] M) where
-  carrier := { T | ∀ (n : N), T '' n ⊆ n}
+  carrier := { T | ∀ (n : N), ↑n ∈ Module.End.invtSubmodule T}
   add_mem' S T := by
     intro n x hx
-    obtain ⟨y, ⟨hy₁, hy₂⟩⟩ := hx
-    rw [← hy₂, LinearMap.add_apply]
-    exact AddMemClass.add_mem (S _ (Set.mem_image_of_mem _ hy₁))  (T _ (Set.mem_image_of_mem _ hy₁))
+    aesop
   mul_mem' S T := by
     intro n h hx
-    obtain ⟨y, hy₁, hy₂⟩ := hx
-    rw [← hy₂, LinearMap.mul_apply]
-    exact S _ (Set.mem_image_of_mem _ (T _ (Set.mem_image_of_mem _ hy₁)))
-  one_mem' := by
-    intro n
-    simp only [LinearMap.one_apply, Set.image_id', subset_refl]
-  zero_mem' := by
-    intro n
-    simp only [LinearMap.zero_apply, Set.image_subset_iff, SetLike.mem_coe, Submodule.zero_mem,
-      Set.preimage_const_of_mem, Set.subset_univ]
+    aesop
+  one_mem' := by aesop
+  zero_mem' := by aesop
   algebraMap_mem' := by
     intro r n x hx
-    obtain ⟨y, hy₁, hy₂⟩ := hx
-    rw [← hy₂, Module.algebraMap_end_apply]
-    exact SMulMemClass.smul_mem _ hy₁
+    aesop
 
 
 /-! ### Free Modules -/
