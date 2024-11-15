ca0d3152a8f2749d28597d1ad03bc387634a302e
Hannah Fechtner <hfechtne@andrew.cmu.edu>
2024-11-14 19:07:10 -0500

    reduce imports

--- Mathlib/GroupTheory/SpecificGroups/BraidGroup/Basic.lean
+++ Mathlib/GroupTheory/SpecificGroups/BraidGroup/Basic.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Hannah Fechtner
 -/
 import Mathlib.GroupTheory.PresentedGroup
-import Mathlib.Data.Nat.Dist
 
 /-!
 #  Braid Groups
