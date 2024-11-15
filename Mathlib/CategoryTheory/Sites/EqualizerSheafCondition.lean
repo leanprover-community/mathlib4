b4c2e269ca306430873e1b8ff0dd5b36b422387d
Kim Morrison <kim@tqft.net>
2024-11-15 07:09:36 +0000

    chore: replace all #NNN tags with unambiguous URLs (#19063)
    I find it perpetually frustrating that I can't click on these.
    
    Normalising all these as URLs also opens the opportunity for scripts to parse them (e.g. identifying links to closed issues).

--- Mathlib/CategoryTheory/Sites/EqualizerSheafCondition.lean
+++ Mathlib/CategoryTheory/Sites/EqualizerSheafCondition.lean
@@ -52,7 +52,7 @@ def FirstObj : Type max v u :=
 
 variable {P R}
 
--- Porting note (#10688): added to ease automation
+-- Porting note (https://github.com/leanprover-community/mathlib4/issues/10688): added to ease automation
 @[ext]
 lemma FirstObj.ext (z₁ z₂ : FirstObj P R) (h : ∀ (Y : C) (f : Y ⟶ X)
     (hf : R f), (Pi.π _ ⟨Y, f, hf⟩ : FirstObj P R ⟶ _) z₁ =
@@ -99,7 +99,7 @@ def SecondObj : Type max v u :=
 
 variable {P S}
 
--- Porting note (#10688): added to ease automation
+-- Porting note (https://github.com/leanprover-community/mathlib4/issues/10688): added to ease automation
 @[ext]
 lemma SecondObj.ext (z₁ z₂ : SecondObj P S) (h : ∀ (Y Z : C) (g : Z ⟶ Y) (f : Y ⟶ X)
     (hf : S.arrows f), (Pi.π _ ⟨Y, Z, g, f, hf⟩ : SecondObj P S ⟶ _) z₁ =
