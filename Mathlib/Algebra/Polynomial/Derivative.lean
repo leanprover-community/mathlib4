a5af4a0848becec231877d23cc23566f1d97ffb1
Seewoo Lee <seewoo5@gmail.com>
2024-11-15 16:20:59 +0000

    feat:`Polynomial.derivative_C_mul` (#19048)
    Add `Polynomial.derivative_C_mul`. Note that `Polynomial.iterative_derivative_C_mul` already exists.

--- Mathlib/Algebra/Polynomial/Derivative.lean
+++ Mathlib/Algebra/Polynomial/Derivative.lean
@@ -148,6 +148,9 @@ theorem iterate_derivative_C_mul (a : R) (p : R[X]) (k : ℕ) :
     derivative^[k] (C a * p) = C a * derivative^[k] p := by
   simp_rw [← smul_eq_C_mul, iterate_derivative_smul]
 
+theorem derivative_C_mul (a : R) (p : R[X]) :
+    derivative (C a * p) = C a * derivative p := iterate_derivative_C_mul _ _ 1
+
 theorem of_mem_support_derivative {p : R[X]} {n : ℕ} (h : n ∈ p.derivative.support) :
     n + 1 ∈ p.support :=
   mem_support_iff.2 fun h1 : p.coeff (n + 1) = 0 =>
