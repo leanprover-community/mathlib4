fdb6d17851aec13e0b1939b6e5b272e5c98ed6fd
Markus Himmel <markus@lean-fro.org>
2024-11-15 20:56:05 +0100

    imports

--- Mathlib/CategoryTheory/Limits/Preserves/Shapes/Products.lean
+++ Mathlib/CategoryTheory/Limits/Preserves/Shapes/Products.lean
@@ -3,8 +3,6 @@ Copyright (c) 2020 Kim Morrison, Bhavik Mehta. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kim Morrison, Bhavik Mehta
 -/
-import Mathlib.CategoryTheory.Limits.Shapes.Products
-import Mathlib.CategoryTheory.Limits.Preserves.Basic
 import Mathlib.CategoryTheory.Limits.Opposites
 import Mathlib.CategoryTheory.Limits.Yoneda
 
