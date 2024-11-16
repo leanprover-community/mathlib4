61e25bcec552aee2625d417cd66d6504e2c3cacd
Jireh Loreaux <loreaujy@gmail.com>
2024-11-16 14:44:57 -0600

    Switch to `NormedLatticeAddCommGroup`

--- Mathlib/Topology/UniformSpace/Dini.lean
+++ Mathlib/Topology/UniformSpace/Dini.lean
@@ -3,8 +3,7 @@ Copyright (c) 2024 Jireh Loreaux. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jireh Loreaux
 -/
-import Mathlib.Algebra.Order.Group.Instances
-import Mathlib.Topology.Algebra.UniformGroup.Basic
+import Mathlib.Analysis.Normed.Order.Lattice
 import Mathlib.Topology.ContinuousMap.Ordered
 import Mathlib.Topology.UniformSpace.CompactConvergence
 
@@ -14,38 +13,26 @@ This file proves Dini's theorem, which states that if `F n` is a monotone increa
 continuous real-valued functions on a compact set `s` converging pointwise to a continuous function
 `f`, then `F n` converges uniformly to `f`.
 
-We generalize the codomain from `ℝ` to a linearly ordered commutative group `G` with the order
-topology which is also order closed. Therefore it applies also when the codomain is `ℤ`, `ℚ`, or
-`ℝ≥0ˣ`.
--/
+We generalize the codomain from `ℝ` to a normed latttice additive commutative group `G`. -/
 
 open Filter Topology
 
 variable {ι α G : Type*} [Preorder ι] [IsDirected ι (· ≤ ·)] [Nonempty ι]
-    [TopologicalSpace α] [LinearOrderedCommGroup G]
+variable [TopologicalSpace α] [NormedLatticeAddCommGroup G]
 
 section Unbundled
 
-open TopologicalGroup
+open TopologicalAddGroup Metric
 
-variable [u : UniformSpace G] [TopologicalGroup G] [OrderTopology G]
 variable {F : ι → α → G} {f : α → G}
 
 namespace Monotone
 
 /-- **Dini's theorem** If `F n` is a monotone increasing collection of continuous functions on a
-converging pointwise to a continuous function `f`, then `F n` converges locally uniformly to `f`.
-
-This version requires the codomain to be a `Group` instead of an `AddGroup`.
- -/
-@[to_additive tendstoLocallyUniformly_of_forall_tendsto
-"**Dini's theorem** If `F n` is a monotone increasing collection of continuous
-functions on a converging pointwise to a continuous function `f`, then `F n` converges locally
-uniformly to `f`."]
-lemma tendstoLocallyUniformly_of_forall_tendsto'
+converging pointwise to a continuous function `f`, then `F n` converges locally uniformly to `f`. -/
+lemma tendstoLocallyUniformly_of_forall_tendsto
     (hF_cont : ∀ i, Continuous (F i)) (hF_mono : Monotone F) (hf : Continuous f)
-    (h_tendsto : ∀ x, Tendsto (F · x) atTop (𝓝 (f x)))
-    (hu : toUniformSpace G = u := by first | exact UniformGroup.toUniformSpace_eq G | rfl) :
+    (h_tendsto : ∀ x, Tendsto (F · x) atTop (𝓝 (f x))) :
     TendstoLocallyUniformly F f atTop := by
   refine subsingleton_or_nontrivial G |>.elim (fun _ ↦ ?subsingleton) (fun _ ↦ ?_)
   case subsingleton =>
@@ -56,106 +43,67 @@ lemma tendstoLocallyUniformly_of_forall_tendsto'
     refine _root_.ge_of_tendsto (h_tendsto x) ?_
     filter_upwards [Ici_mem_atTop n] with m hnm
     exact hF_mono hnm x
-  rw [tendstoLocallyUniformly_iff F f atTop hu]
+  rw [tendstoLocallyUniformly_iff F f atTop UniformAddGroup.toUniformSpace_eq]
   intro v hv x
-  simp_rw +singlePass [← tendsto_div_nhds_one_iff] at h_tendsto
-  obtain ⟨y, hy⟩ := exists_one_lt' (α := G)
-  obtain ⟨l, u, h₀, h⟩ := mem_nhds_iff_exists_Ioo_subset' ⟨y⁻¹, inv_lt_one'.mpr hy⟩ ⟨y, hy⟩ |>.mp hv
-  obtain ⟨n, (hn : (F n / f) x ∈ Set.Ioo l u)⟩ :=
-    Eventually.exists <| (h_tendsto x).eventually <| isOpen_Ioo.mem_nhds h₀
-  refine ⟨(F n / f) ⁻¹' Set.Ioo l u, (isOpen_Ioo.preimage <| (hF_cont n).div' hf).mem_nhds hn, ?_⟩
-  have mono_preimage_Ioo : Monotone (fun i => (F i / f) ⁻¹' Set.Ioo l u) := by
+  obtain ⟨ε, ε_pos, hε⟩ := Metric.nhds_basis_ball.mem_iff.mp hv
+  simp_rw +singlePass [← tendsto_sub_nhds_zero_iff] at h_tendsto
+  obtain ⟨n, hn⟩ := h_tendsto x |>.basis_right nhds_basis_ball ε ε_pos |>.exists
+  refine ⟨_, (hF_cont n).sub hf |>.continuousAt.preimage_mem_nhds <| isOpen_ball.mem_nhds hn, ?_⟩
+  have mono_preimage_Ioo : Monotone (fun i => (F i - f) ⁻¹' Metric.ball 0 ε) := by
     intro n m hnm x hx
-    simp only [Set.mem_preimage, Set.mem_Ioo] at hx ⊢
-    refine ⟨hx.1.trans_le ?_, div_le_one'.2 (F_le_f _ _) |>.trans_lt h₀.2⟩
-    simpa only [Pi.div_apply, div_le_iff_le_mul, div_mul_cancel] using hF_mono hnm x
+    simp only [Set.mem_preimage, Pi.sub_apply, mem_ball, dist_zero_right] at hx ⊢
+    refine norm_le_norm_of_abs_le_abs ?_ |>.trans_lt hx
+    simp only [abs_of_nonpos (sub_nonpos.mpr (F_le_f _ _)), neg_sub]
+    gcongr
+    exact hF_mono hnm x
   filter_upwards [Ici_mem_atTop n] with m (hnm : n ≤ m) z hz
-  exact h <| mono_preimage_Ioo hnm hz
+  exact hε <| mono_preimage_Ioo hnm hz
 
 /-- **Dini's theorem** If `F n` is a monotone increasing collection of continuous functions on a
-compact space converging pointwise to a continuous `f`, then `F n` converges uniformly to `f`.
-
-This version requires the codomain to be a `Group` instead of an `AddGroup`.
- -/
-@[to_additive tendstoUniformly_of_forall_tendsto
-"**Dini's theorem** If `F n` is a monotone increasing collection of continuous
-functions on a compact space converging pointwise to a continuous `f`, then `F n` converges
-uniformly to `f`."]
-lemma tendstoUniformly_of_forall_tendsto' [CompactSpace α] (hF_cont : ∀ i, Continuous (F i))
-    (hF_mono : Monotone F) (hf : Continuous f) (h_tendsto : ∀ x, Tendsto (F · x) atTop (𝓝 (f x)))
-    (hu : toUniformSpace G = u := by first | exact UniformGroup.toUniformSpace_eq G | rfl) :
+compact space converging pointwise to a continuous `f`, then `F n` converges uniformly to `f`. -/
+lemma tendstoUniformly_of_forall_tendsto [CompactSpace α] (hF_cont : ∀ i, Continuous (F i))
+    (hF_mono : Monotone F) (hf : Continuous f) (h_tendsto : ∀ x, Tendsto (F · x) atTop (𝓝 (f x))) :
     TendstoUniformly F f atTop :=
   tendstoLocallyUniformly_iff_tendstoUniformly_of_compactSpace.mp <|
-    tendstoLocallyUniformly_of_forall_tendsto' hF_cont hF_mono hf h_tendsto hu
+    tendstoLocallyUniformly_of_forall_tendsto hF_cont hF_mono hf h_tendsto
 
 /-- **Dini's theorem** If `F n` is a monotone increasing collection of continuous functions on a
-compact set `s` converging pointwise to a continuous `f`, then `F n` converges uniformly to `f`.
-
-This version requires the codomain to be a `Group` instead of an `AddGroup`.
--/
-@[to_additive tendstoUniformlyOn_of_forall_tendsto
-"**Dini's theorem** If `F n` is a monotone increasing collection of continuous
-functions on a compact set `s` converging pointwise to a continuous `f`, then `F n` converges
-uniformly to `f`."]
-lemma tendstoUniformlyOn_of_forall_tendsto' {s : Set α} (hs : IsCompact s)
+compact set `s` converging pointwise to a continuous `f`, then `F n` converges uniformly to `f`. -/
+lemma tendstoUniformlyOn_of_forall_tendsto {s : Set α} (hs : IsCompact s)
     (hF_cont : ∀ i, ContinuousOn (F i) s) (hF_mono : ∀ x ∈ s, Monotone (F · x))
-    (hf : ContinuousOn f s) (h_tendsto : ∀ x ∈ s, Tendsto (F · x) atTop (𝓝 (f x)))
-    (hu : toUniformSpace G = u := by first | exact UniformGroup.toUniformSpace_eq G | rfl) :
+    (hf : ContinuousOn f s) (h_tendsto : ∀ x ∈ s, Tendsto (F · x) atTop (𝓝 (f x))) :
     TendstoUniformlyOn F f atTop s := by
   rw [tendstoUniformlyOn_iff_tendstoUniformly_comp_coe]
   have := isCompact_iff_compactSpace.mp hs
-  exact tendstoUniformly_of_forall_tendsto' (hF_cont · |>.restrict) (fun _ _ h x ↦ hF_mono _ x.2 h)
-    hf.restrict (fun x ↦ h_tendsto x x.2) hu
+  exact tendstoUniformly_of_forall_tendsto (hF_cont · |>.restrict) (fun _ _ h x ↦ hF_mono _ x.2 h)
+    hf.restrict (fun x ↦ h_tendsto x x.2)
 
 end Monotone
+
 namespace Antitone
 
 /-- **Dini's theorem** If `F n` is a monotone decreasing collection of continuous functions on a
-converging pointwise to a continuous function `f`, then `F n` converges locally uniformly to `f`.
-
-This version requires the codomain to be a `Group` instead of an `AddGroup`.
- -/
-@[to_additive tendstoLocallyUniformly_of_forall_tendsto
-"**Dini's theorem** If `F n` is a monotone decreasing collection of continuous
-functions on a converging pointwise to a continuous function `f`, then `F n` converges locally
-uniformly to `f`."]
-lemma tendstoLocallyUniformly_of_forall_tendsto'
+converging pointwise to a continuous function `f`, then `F n` converges locally uniformly to `f`. -/
+lemma tendstoLocallyUniformly_of_forall_tendsto
     (hF_cont : ∀ i, Continuous (F i)) (hF_anti : Antitone F) (hf : Continuous f)
-    (h_tendsto : ∀ x, Tendsto (F · x) atTop (𝓝 (f x)))
-    (hu : toUniformSpace G = u := by first | exact UniformGroup.toUniformSpace_eq G | rfl) :
+    (h_tendsto : ∀ x, Tendsto (F · x) atTop (𝓝 (f x))) :
     TendstoLocallyUniformly F f atTop :=
-  Monotone.tendstoLocallyUniformly_of_forall_tendsto' (G := Gᵒᵈ) hF_cont hF_anti hf h_tendsto hu
+  Monotone.tendstoLocallyUniformly_of_forall_tendsto (G := Gᵒᵈ) hF_cont hF_anti hf h_tendsto
 
 /-- **Dini's theorem** If `F n` is a monotone decreasing collection of continuous functions on a
-compact space converging pointwise to a continuous `f`, then `F n` converges uniformly to `f`.
-
-This version requires the codomain to be a `Group` instead of an `AddGroup`.
--/
-@[to_additive tendstoUniformly_of_forall_tendsto
-"**Dini's theorem** If `F n` is a monotone decreasing collection of continuous
-functions on a compact space converging pointwise to a continuous `f`, then `F n` converges
-uniformly to `f`."]
-lemma tendstoUniformly_of_forall_tendsto' [CompactSpace α] (hF_cont : ∀ i, Continuous (F i))
-    (hF_anti : Antitone F) (hf : Continuous f) (h_tendsto : ∀ x, Tendsto (F · x) atTop (𝓝 (f x)))
-    (hu : toUniformSpace G = u := by first | exact UniformGroup.toUniformSpace_eq G | rfl) :
+compact space converging pointwise to a continuous `f`, then `F n` converges uniformly to `f`. -/
+lemma tendstoUniformly_of_forall_tendsto [CompactSpace α] (hF_cont : ∀ i, Continuous (F i))
+    (hF_anti : Antitone F) (hf : Continuous f) (h_tendsto : ∀ x, Tendsto (F · x) atTop (𝓝 (f x))) :
     TendstoUniformly F f atTop :=
-  Monotone.tendstoUniformly_of_forall_tendsto' (G := Gᵒᵈ) hF_cont hF_anti hf h_tendsto hu
+  Monotone.tendstoUniformly_of_forall_tendsto (G := Gᵒᵈ) hF_cont hF_anti hf h_tendsto
 
 /-- **Dini's theorem** If `F n` is a monotone decreasing collection of continuous functions on a
-compact set `s` converging pointwise to a continuous `f`, then `F n` converges uniformly to `f`.
-
-This version requires the codomain to be a `Group` instead of an `AddGroup`.
--/
-@[to_additive tendstoUniformlyOn_of_forall_tendsto
-"**Dini's theorem** If `F n` is a monotone decreasing collection of continuous
-functions on a compact set `s` converging pointwise to a continuous `f`, then `F n` converges
-uniformly to `f`."]
-lemma tendstoUniformlyOn_of_forall_tendsto' {s : Set α} (hs : IsCompact s)
+compact set `s` converging pointwise to a continuous `f`, then `F n` converges uniformly to `f`. -/
+lemma tendstoUniformlyOn_of_forall_tendsto {s : Set α} (hs : IsCompact s)
     (hF_cont : ∀ i, ContinuousOn (F i) s) (hF_anti : ∀ x ∈ s, Antitone (F · x))
-    (hf : ContinuousOn f s) (h_tendsto : ∀ x ∈ s, Tendsto (F · x) atTop (𝓝 (f x)))
-    (hu : toUniformSpace G = u := by first | exact UniformGroup.toUniformSpace_eq G | rfl) :
+    (hf : ContinuousOn f s) (h_tendsto : ∀ x ∈ s, Tendsto (F · x) atTop (𝓝 (f x))) :
     TendstoUniformlyOn F f atTop s :=
-  Monotone.tendstoUniformlyOn_of_forall_tendsto' (G := Gᵒᵈ) hs hF_cont hF_anti hf h_tendsto hu
+  Monotone.tendstoUniformlyOn_of_forall_tendsto (G := Gᵒᵈ) hs hF_cont hF_anti hf h_tendsto
 
 end Antitone
 
@@ -163,39 +111,23 @@ end Unbundled
 
 namespace ContinuousMap
 
-variable [TopologicalSpace G] [TopologicalGroup G] [OrderTopology G]
 variable {F : ι → C(α, G)} {f : C(α, G)}
 
 /-- **Dini's theorem** If `F n` is a monotone increasing collection of continuous functions on
 converging pointwise to a continuous `f`, then `F n` converges to `f` in the
-compact-open topology.
-
-This version requires the codomain to be a `Group` instead of an `AddGroup`.
--/
-@[to_additive tendsto_of_monotone_of_pointwise
-"**Dini's theorem** If `F n` is a monotone increasing collection
-of continuous functions on converging pointwise to a continuous `f`, then `F n` converges to `f` in
-the compact-open topology."]
-lemma tendsto_of_monotone_of_pointwise' (hF_mono : Monotone F)
+compact-open topology. -/
+lemma tendsto_of_monotone_of_pointwise (hF_mono : Monotone F)
     (h_tendsto : ∀ x, Tendsto (F · x) atTop (𝓝 (f x))) :
     Tendsto F atTop (𝓝 f) :=
-  let _ := TopologicalGroup.toUniformSpace G
   tendsto_of_tendstoLocallyUniformly <|
-    hF_mono.tendstoLocallyUniformly_of_forall_tendsto' (F · |>.continuous) f.continuous h_tendsto
+    hF_mono.tendstoLocallyUniformly_of_forall_tendsto (F · |>.continuous) f.continuous h_tendsto
 
 /-- **Dini's theorem** If `F n` is a monotone decreasing collection of continuous functions on
 converging pointwise to a continuous `f`, then `F n` converges to `f` in the
-compact-open topology.
-
-This version requires the codomain to be a `Group` instead of an `AddGroup`.
--/
-@[to_additive tendsto_of_antitone_of_pointwise
-"**Dini's theorem** If `F n` is a monotone decreasing collection
-of continuous functions on converging pointwise to a continuous `f`, then `F n` converges to `f` in
-the compact-open topology."]
-lemma tendsto_of_antitone_of_pointwise' (hF_anti : Antitone F)
+compact-open topology. -/
+lemma tendsto_of_antitone_of_pointwise (hF_anti : Antitone F)
     (h_tendsto : ∀ x, Tendsto (F · x) atTop (𝓝 (f x))) :
     Tendsto F atTop (𝓝 f) :=
-  tendsto_of_monotone_of_pointwise' (G := Gᵒᵈ) hF_anti h_tendsto
+  tendsto_of_monotone_of_pointwise (G := Gᵒᵈ) hF_anti h_tendsto
 
 end ContinuousMap
