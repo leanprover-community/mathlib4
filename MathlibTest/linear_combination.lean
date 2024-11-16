ef007bb188ac312dadecd03a7070fe895acb1af8
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2024-11-16 12:18:02 -0500

    bump heartbeats

--- MathlibTest/linear_combination.lean
+++ MathlibTest/linear_combination.lean
@@ -503,7 +503,7 @@ typeclass inference is demanded by the lemmas it orchestrates.  This example too
 (and 73 ms on a good laptop) on an implementation with "minimal" typeclasses everywhere, e.g. lots of
 `CovariantClass`/`ContravariantClass`, and takes 206 heartbeats (10 ms on a good laptop) on the
 implementation at the time of joining Mathlib (November 2024). -/
-set_option maxHeartbeats 1000 in
+set_option maxHeartbeats 1100 in
 example {a b : ℝ} (h : a < b) : 0 < b - a := by
   linear_combination (norm := skip) h
   exact test_sorry
