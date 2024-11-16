11a1e4805556aaf7bb5b1968dc3f05b653c0012b
su00000 <ssh@stu.pku.edu.cn>
2024-11-16 09:17:54 +0800

    Update Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -4256,8 +4256,8 @@ import Mathlib.RingTheory.LaurentSeries
 import Mathlib.RingTheory.LinearDisjoint
 import Mathlib.RingTheory.LittleWedderburn
 import Mathlib.RingTheory.LocalProperties.Basic
+import Mathlib.RingTheory.LocalProperties.Exactness
 import Mathlib.RingTheory.LocalProperties.IntegrallyClosed
-import Mathlib.RingTheory.LocalProperties.LinearMap
 import Mathlib.RingTheory.LocalProperties.Projective
 import Mathlib.RingTheory.LocalProperties.Reduced
 import Mathlib.RingTheory.LocalProperties.Submodule
