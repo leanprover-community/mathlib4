45b83aaa834a2ad7c71382b2463546132d4aa2e6
Yoh Tanimoto <hoyt@jcom.home.ne.jp>
2024-11-17 19:59:41 +0100

    add lemmas

--- Mathlib/Algebra/Order/Field/Basic.lean
+++ Mathlib/Algebra/Order/Field/Basic.lean
@@ -435,6 +435,16 @@ theorem inv_pow_anti (a1 : 1 ≤ a) : Antitone fun n : ℕ => (a ^ n)⁻¹ := fu
 theorem inv_pow_strictAnti (a1 : 1 < a) : StrictAnti fun n : ℕ => (a ^ n)⁻¹ := fun _ _ =>
   inv_pow_lt_inv_pow_of_lt a1
 
+theorem le_iff_forall_one_lt_le_mul₀ {α : Type*} [LinearOrderedField α]
+    {a b : α} (hb : 0 ≤ b) : a ≤ b ↔ ∀ ε, 1 < ε → a ≤ b * ε := by
+  refine ⟨fun h _ hε ↦ h.trans <| le_mul_of_one_le_right hb hε.le, fun h ↦ ?_⟩
+  obtain rfl|hb := hb.eq_or_lt
+  · simp_rw [zero_mul] at h
+    exact h 2 one_lt_two
+  refine le_of_forall_le_of_dense fun x hbx => ?_
+  convert h (x / b) ((one_lt_div hb).mpr hbx)
+  rw [mul_div_cancel₀ _ hb.ne']
+
 /-! ### Results about `IsGLB` -/
 
 
