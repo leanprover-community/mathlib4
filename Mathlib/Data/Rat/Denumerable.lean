0e3b2132eb05e3d2c74ca8316ee8e2e94d2da6f3
Kim Morrison <kim@tqft.net>
2024-11-17 21:22:26 +1100

    shake

--- Mathlib/Data/Rat/Denumerable.lean
+++ Mathlib/Data/Rat/Denumerable.lean
@@ -4,10 +4,9 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Chris Hughes
 -/
 import Mathlib.Algebra.Order.Ring.Rat
-import Mathlib.SetTheory.Cardinal.Basic
-import Mathlib.Algebra.Order.Ring.Nat
 import Mathlib.Data.Rat.Encodable
 import Mathlib.Algebra.CharZero.Infinite
+import Mathlib.Logic.Denumerable
 
 /-!
 # Denumerability of ℚ
