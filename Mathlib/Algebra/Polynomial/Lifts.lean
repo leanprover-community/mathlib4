482bd5f885e1875d33d1721bd07df9870f196a56
Thomas Browning <tb65536@users.noreply.github.com>
2024-11-17 09:19:10 -0800

    Update Mathlib/Algebra/Polynomial/Lifts.lean
    Co-authored-by: Michael Stoll <99838730+MichaelStollBayreuth@users.noreply.github.com>

--- Mathlib/Algebra/Polynomial/Lifts.lean
+++ Mathlib/Algebra/Polynomial/Lifts.lean
@@ -155,8 +155,9 @@ theorem lifts_and_degree_eq_and_monic [Nontrivial S] {p : S[X]} (hlifts : p ∈ 
   let g : ℕ → R := fun k ↦ (hlifts k).choose
   have hg : ∀ k, f (g k) = p.coeff k := fun k ↦ (hlifts k).choose_spec
   let q : R[X] := X ^ p.natDegree + ∑ k ∈ Finset.range p.natDegree, C (g k) * X ^ k
-  have hq : map f q = p := by simp_rw [q, Polynomial.map_add, Polynomial.map_sum,
-    Polynomial.map_mul, Polynomial.map_pow, map_X, map_C, hg, ← hp.as_sum]
+  have hq : map f q = p := by
+    simp_rw [q, Polynomial.map_add, Polynomial.map_sum, Polynomial.map_mul, Polynomial.map_pow,
+      map_X, map_C, hg, ← hp.as_sum]
   have h : q.Monic := monic_X_pow_add (by simp_rw [← Fin.sum_univ_eq_sum_range, degree_sum_fin_lt])
   exact ⟨q, hq, hq ▸ (h.degree_map f).symm, h⟩
 
