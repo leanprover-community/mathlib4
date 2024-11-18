31061c293de9e116fb6fb2563fb3c5f845ac7c4c
Yaël Dillies <yael.dillies@gmail.com>
2024-11-18 11:54:36 +0000

    missing docstring

--- Mathlib/Algebra/GroupWithZero/Units/Equiv.lean
+++ Mathlib/Algebra/GroupWithZero/Units/Equiv.lean
@@ -12,34 +12,36 @@ import Mathlib.Algebra.GroupWithZero.Units.Basic
 
 assert_not_exists DenselyOrdered
 
-variable {G : Type*}
+variable {G₀ : Type*}
 
 namespace Equiv
 
 section GroupWithZero
 
-variable [GroupWithZero G]
+variable [GroupWithZero G₀]
 
 /-- Left multiplication by a nonzero element in a `GroupWithZero` is a permutation of the
 underlying type. -/
 @[simps! (config := .asFn)]
-protected def mulLeft₀ (a : G) (ha : a ≠ 0) : Perm G :=
+protected def mulLeft₀ (a : G₀) (ha : a ≠ 0) : Perm G₀ :=
   (Units.mk0 a ha).mulLeft
 
-theorem _root_.mulLeft_bijective₀ (a : G) (ha : a ≠ 0) : Function.Bijective (a * · : G → G) :=
+theorem _root_.mulLeft_bijective₀ (a : G₀) (ha : a ≠ 0) : Function.Bijective (a * · : G₀ → G₀) :=
   (Equiv.mulLeft₀ a ha).bijective
 
 /-- Right multiplication by a nonzero element in a `GroupWithZero` is a permutation of the
 underlying type. -/
 @[simps! (config := .asFn)]
-protected def mulRight₀ (a : G) (ha : a ≠ 0) : Perm G :=
+protected def mulRight₀ (a : G₀) (ha : a ≠ 0) : Perm G₀ :=
   (Units.mk0 a ha).mulRight
 
-theorem _root_.mulRight_bijective₀ (a : G) (ha : a ≠ 0) : Function.Bijective ((· * a) : G → G) :=
+theorem _root_.mulRight_bijective₀ (a : G₀) (ha : a ≠ 0) : Function.Bijective ((· * a) : G₀ → G₀) :=
   (Equiv.mulRight₀ a ha).bijective
 
+/-- Right division by a nonzero element in a `GroupWithZero` is a permutation of the
+underlying type. -/
 @[simps! (config := { simpRhs := true })]
-def divRight₀ {G₀} [GroupWithZero G₀] (a : G₀) (ha : a ≠ 0) : G₀ ≃ G₀ where
+def divRight₀ (a : G₀) (ha : a ≠ 0) : G₀ ≃ G₀ where
   toFun := (· / a)
   invFun := (· * a)
   left_inv _ := by simp [ha]
