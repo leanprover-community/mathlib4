22565fa45c59441db63dcb8a6a9363021f646ff1
Eric Wieser <wieser.eric@gmail.com>
2024-11-18 11:19:55 +0000

    chore: use `Pairwise (_ on _)` where possible (#9256)
    Co-authored-by: Jireh Loreaux <loreaujy@gmail.com>
    Co-authored-by: Yury G. Kudryashov <urkud@urkud.name>

--- Mathlib/Data/List/Nodup.lean
+++ Mathlib/Data/List/Nodup.lean
@@ -259,7 +259,7 @@ theorem nodup_flatten {L : List (List α)} :
 
 theorem nodup_flatMap {l₁ : List α} {f : α → List β} :
     Nodup (l₁.flatMap f) ↔
-      (∀ x ∈ l₁, Nodup (f x)) ∧ Pairwise (fun a b : α => Disjoint (f a) (f b)) l₁ := by
+      (∀ x ∈ l₁, Nodup (f x)) ∧ Pairwise (Disjoint on f) l₁ := by
   simp only [List.flatMap, nodup_flatten, pairwise_map, and_comm, and_left_comm, mem_map,
     exists_imp, and_imp]
   rw [show (∀ (l : List β) (x : α), f x = l → x ∈ l₁ → Nodup l) ↔ ∀ x : α, x ∈ l₁ → Nodup (f x)
