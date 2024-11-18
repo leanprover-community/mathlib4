05f1e73bb7973a4077f858df19e3106187be5177
yhtq <1414672068@qq.com>
2024-11-18 10:48:57 +0800

    Update Mathlib/Algebra/Algebra/Defs.lean
    Co-authored-by: Thomas Browning <tb65536@users.noreply.github.com>

--- Mathlib/Algebra/Algebra/Defs.lean
+++ Mathlib/Algebra/Algebra/Defs.lean
@@ -178,7 +178,7 @@ def RingHom.toAlgebra' {R S} [CommSemiring R] [Semiring S] (i : R →+* S)
 -- just simple lemmas for a declaration that is itself primed, no need for docstrings
 set_option linter.docPrime false in
 theorem RingHom.smul_toAlgebra' {R S} [CommSemiring R] [Semiring S] (i : R →+* S)
-    (h : ∀ c x, i c * x = x * i c) (r: R) (s: S) :
+    (h : ∀ c x, i c * x = x * i c) (r : R) (s : S) :
     let _ := RingHom.toAlgebra' i h
     r • s = i r * s := rfl
 
