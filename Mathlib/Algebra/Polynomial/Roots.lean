2602ce41756dc6579d4cefb5e9a741d949e0a212
Riccardo Brasca <riccardo.brasca@gmail.com>
2024-11-18 12:46:04 +0100

    already in scope

--- Mathlib/Algebra/Polynomial/Roots.lean
+++ Mathlib/Algebra/Polynomial/Roots.lean
@@ -311,7 +311,7 @@ theorem mem_nthRootsFinset {n : ℕ} (h : 0 < n) {x : R} :
 theorem nthRootsFinset_zero : nthRootsFinset 0 R = ∅ := by classical simp [nthRootsFinset_def]
 
 theorem mem_nthRootsFinset_map {S F : Type*} [CommRing S] [IsDomain S] [FunLike F R S]
-    [RingHomClass F R S] {n : ℕ} {x : R} (hx : x ∈ nthRootsFinset n R) (f : F) :
+    [RingHomClass F R S] {x : R} (hx : x ∈ nthRootsFinset n R) (f : F) :
     f x ∈ nthRootsFinset n S := by
   by_cases hn : n = 0
   · simp [hn] at hx
