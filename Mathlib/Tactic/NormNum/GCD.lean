e98ec1ea1359bc3d25705e94b4c20ae13f8dfb37
Eric Wieser <wieser.eric@gmail.com>
2024-11-18 15:49:38 +0000

    author

--- Mathlib/Tactic/NormNum/GCD.lean
+++ Mathlib/Tactic/NormNum/GCD.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2021 Mario Carneiro. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Mario Carneiro, Kyle Miller
+Authors: Mario Carneiro, Kyle Miller, Eric Wieser
 -/
 import Mathlib.Data.Int.GCD
 import Mathlib.Tactic.NormNum
