789eb41cf8f02ae0623fbe25f18839b4729996d7
Yaël Dillies <yael.dillies@gmail.com>
2024-11-18 09:20:37 +0000

    missing space
    Co-authored-by: Yury G. Kudryashov <urkud@urkud.name>

--- Mathlib/MeasureTheory/Measure/MeasureSpace.lean
+++ Mathlib/MeasureTheory/Measure/MeasureSpace.lean
@@ -678,7 +678,7 @@ section
 /- Porting note: These variables are wrapped by an anonymous section because they interrupt
 synthesizing instances in `MeasureSpace` section. -/
 
-variable {m0 : MeasurableSpace α}{mβ : MeasurableSpace β} [MeasurableSpace γ]
+variable {m0 : MeasurableSpace α} {mβ : MeasurableSpace β} [MeasurableSpace γ]
 variable {μ μ₁ μ₂ μ₃ ν ν' ν₁ ν₂ : Measure α} {s s' t : Set α}
 namespace Measure
 
