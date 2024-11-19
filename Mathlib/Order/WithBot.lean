da6ea132dbd6fe39c173030bcb3d60989c7b8674
Daniel Weber <55664973+Command-Master@users.noreply.github.com>
2024-11-19 06:19:31 +0200

    Update WithBot.lean

--- Mathlib/Order/WithBot.lean
+++ Mathlib/Order/WithBot.lean
@@ -102,8 +102,7 @@ lemma map_eq_bot_iff {f : α → β} {a : WithBot α} :
     map f a = ⊥ ↔ a = ⊥ := Option.map_eq_none'
 
 theorem map_eq_some_iff {α β : Type*} {f : α → β} {y : β} {v : WithBot α} :
-    WithBot.map f v = .some y ↔ ∃ x, v = .some x ∧ f x = y := by
-  cases v <;> simp
+    WithBot.map f v = .some y ↔ ∃ x, v = .some x ∧ f x = y := Option.map_eq_some'
 
 theorem some_eq_map_iff {α β : Type*} {f : α → β} {y : β} {v : WithBot α} :
     .some y = WithBot.map f v ↔ ∃ x, v = .some x ∧ f x = y := by
@@ -671,8 +670,7 @@ lemma map_eq_top_iff {f : α → β} {a : WithTop α} :
     map f a = ⊤ ↔ a = ⊤ := Option.map_eq_none'
 
 theorem map_eq_some_iff {α β : Type*} {f : α → β} {y : β} {v : WithTop α} :
-    WithTop.map f v = .some y ↔ ∃ x, v = .some x ∧ f x = y := by
-  cases v <;> simp
+    WithTop.map f v = .some y ↔ ∃ x, v = .some x ∧ f x = y := Option.map_eq_some'
 
 theorem some_eq_map_iff {α β : Type*} {f : α → β} {y : β} {v : WithTop α} :
     .some y = WithTop.map f v ↔ ∃ x, v = .some x ∧ f x = y := by
