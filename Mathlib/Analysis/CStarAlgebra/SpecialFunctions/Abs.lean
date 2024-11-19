761541bd6a3f73f674b69deca079985a80e8d38f
Jon Bannon <jbannon@siena.edu>
2024-11-19 09:19:14 -0500

    With trashy lemma...needs expert correction!

--- Mathlib/Analysis/CStarAlgebra/SpecialFunctions/Abs.lean
+++ Mathlib/Analysis/CStarAlgebra/SpecialFunctions/Abs.lean
@@ -45,19 +45,20 @@ lemma abs_eq_zero_iff {a : A} : abs a = 0 ↔ a = 0 := by
 theorem IsSelfAdjoint.mul_self_nonneg {a : A} (ha : IsSelfAdjoint a) : 0 ≤ a * a := by
   simpa [ha.star_eq] using star_mul_self_nonneg a
 
-lemma sqrt_eq_cfcₙ_real_sqrt (a : A) (ha : 0 ≤ a := by cfc_tac) :
-    CFC.sqrt a = cfcₙ Real.sqrt a := by
-  rw [sqrt_eq_iff (ha := ha) (hb := cfcₙ_nonneg (A := A) (fun x _ ↦ Real.sqrt_nonneg x))]
-  simp only [← cfcₙ_mul (Real.sqrt) (Real.sqrt) _]
-  have dango : Set.EqOn (fun (x : ℝ) ↦ √x * √x) (fun (x : ℝ) ↦ x) (quasispectrum ℝ a)
-      := by
-    intro x hx
-    simp only
-    rw [← Real.sqrt_mul, Real.sqrt_mul_self]
-    <;>
+lemma quasispectrum_sqrt_mul_self_eq_id {a : A} (ha : 0 ≤ a := by cfc_tac) :
+    Set.EqOn (fun (x : ℝ) ↦ √x * √x) (fun (x : ℝ) ↦ x) (quasispectrum _ a) :=
+  fun y hy ↦ (by
+     simp only
+     rw [← Real.sqrt_mul, Real.sqrt_mul_self]
+      <;>
     exact StarOrderedRing.nonneg_iff_quasispectrum_nonneg (R := ℝ) (A := A)
-      (ha := IsSelfAdjoint.of_nonneg ha).mp ha x hx
-  have := cfcₙ_congr (f := fun x ↦ √x * √x) (g := id) (hfg := dango)
+      (ha := IsSelfAdjoint.of_nonneg ha).mp ha y hy)
+
+lemma sqrt_eq_cfcₙ_real_sqrt {a : A} (ha : 0 ≤ a := by cfc_tac) :
+    CFC.sqrt a = cfcₙ Real.sqrt a := by
+  rw [sqrt_eq_iff _ (hb := cfcₙ_nonneg (A := A) (fun x _ ↦ Real.sqrt_nonneg x)),
+    ← cfcₙ_mul ..]
+  have := cfcₙ_congr (f := fun x ↦ √x * √x) (g := id) (hfg := quasispectrum_sqrt_mul_self_eq_id ha)
   simpa only [cfcₙ_id (R := ℝ) a]
 
 lemma abs_eq_cfcₙ_norm (a : A) (ha : IsSelfAdjoint a) :
