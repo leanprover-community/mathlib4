8216c000955dae07690da38b81b12886f3a59a70
Yaël Dillies <yael.dillies@gmail.com>
2024-11-19 18:15:26 +0000

    feat: `S ^ n = S` where `S` is a submonoid From GrowthInGroups

--- Mathlib/Algebra/Group/Subgroup/Pointwise.lean
+++ Mathlib/Algebra/Group/Subgroup/Pointwise.lean
@@ -51,14 +51,28 @@ lemma op_smul_coe_set [Group G] [SetLike S G] [SubgroupClass S G] {s : S} {a : G
   ext; simp [Set.mem_smul_set_iff_inv_smul_mem, mul_mem_cancel_right, ha]
 
 @[to_additive (attr := simp, norm_cast)]
-lemma coe_mul_coe [SetLike S G] [DivInvMonoid G] [SubgroupClass S G] (H : S) :
-    H * H = (H : Set G) := by aesop (add simp mem_mul)
-
-@[to_additive (attr := simp, norm_cast)]
 lemma coe_div_coe [SetLike S G] [DivisionMonoid G] [SubgroupClass S G] (H : S) :
     H / H = (H : Set G) := by simp [div_eq_mul_inv]
 
 variable [Group G] [AddGroup A] {s : Set G}
+
+namespace Set
+
+open Subgroup
+
+@[to_additive (attr := simp)]
+lemma mul_subgroupClosure (hs : s.Nonempty) : s * closure s = closure s := by
+  rw [← smul_eq_mul, ← Set.iUnion_smul_set]
+  have h a (ha : a ∈ s) : a • (closure s : Set G) = closure s :=
+    smul_coe_set <| subset_closure ha
+  simp (config := {contextual := true}) [h, hs]
+
+@[to_additive (attr := simp)]
+lemma mul_subgroupClosure_pow (hs : s.Nonempty) : ∀ n, s ^ n * closure s = closure s
+  | 0 => by simp
+  | n + 1 => by rw [pow_add, pow_one, mul_assoc, mul_subgroupClosure hs, mul_subgroupClosure_pow hs]
+
+end Set
 
 namespace Subgroup
 
