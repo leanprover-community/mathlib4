af8db5230ff980a2cc51bab8502136b77480b551
Christian Merten <christian@merten.dev>
2024-11-19 14:29:49 +0100

    add synth max heartbeats and fix assumptions

--- MathlibTest/CategoryTheory/ConcreteCategory/AlgebraCat.lean
+++ MathlibTest/CategoryTheory/ConcreteCategory/AlgebraCat.lean
@@ -5,33 +5,34 @@ universe v u
 open CategoryTheory AlgebraCat
 
 set_option maxHeartbeats 10000
+set_option synthInstance.maxHeartbeats 2000
 
 variable (R : Type u) [CommRing R]
 
 /- We test if all the coercions and `map_add` lemmas trigger correctly. -/
 
-example (X : Type u) [CommRing X] [Algebra R X] : ⇑(𝟙 (of R X)) = id := by simp
+example (X : Type u) [Ring X] [Algebra R X] : ⇑(𝟙 (of R X)) = id := by simp
 
-example {X Y : Type v} [CommRing X] [Algebra R X] [CommRing Y] [Algebra R Y] (f : X →ₐ[R] Y) :
+example {X Y : Type v} [Ring X] [Algebra R X] [Ring Y] [Algebra R Y] (f : X →ₐ[R] Y) :
     ⇑(ofHom f) = ⇑f := by simp
 
-example {X Y : Type v} [CommRing X] [Algebra R X] [CommRing Y] [Algebra R Y] (f : X →ₐ[R] Y)
+example {X Y : Type v} [Ring X] [Algebra R X] [Ring Y] [Algebra R Y] (f : X →ₐ[R] Y)
     (x : X) : (ofHom f) x = f x := by simp
 
 example {X Y Z : AlgebraCat R} (f : X ⟶ Y) (g : Y ⟶ Z) : ⇑(f ≫ g) = ⇑g ∘ ⇑f := by simp
 
-example {X Y Z : Type v} [CommRing X] [Algebra R X] [CommRing Y] [Algebra R Y] [CommRing Z]
+example {X Y Z : Type v} [Ring X] [Algebra R X] [Ring Y] [Algebra R Y] [Ring Z]
     [Algebra R Z] (f : X →ₐ[R] Y) (g : Y →ₐ[R] Z) :
     ⇑(ofHom f ≫ ofHom g) = g ∘ f := by simp
 
-example {X Y : Type v} [CommRing X] [Algebra R X] [CommRing Y] [Algebra R Y] {Z : AlgebraCat R}
+example {X Y : Type v} [Ring X] [Algebra R X] [Ring Y] [Algebra R Y] {Z : AlgebraCat R}
     (f : X →ₐ[R] Y) (g : of R Y ⟶ Z) :
     ⇑(ofHom f ≫ g) = g ∘ f := by simp
 
-example {X Y : AlgebraCat R} {Z : Type v} [CommRing Z] [Algebra R Z] (f : X ⟶ Y) (g : Y ⟶ of R Z) :
+example {X Y : AlgebraCat R} {Z : Type v} [Ring Z] [Algebra R Z] (f : X ⟶ Y) (g : Y ⟶ of R Z) :
     ⇑(f ≫ g) = g ∘ f := by simp
 
-example {Y Z : AlgebraCat R} {X : Type v} [CommRing X] [Algebra R X] (f : of R X ⟶ Y) (g : Y ⟶ Z) :
+example {Y Z : AlgebraCat R} {X : Type v} [Ring X] [Algebra R X] (f : of R X ⟶ Y) (g : Y ⟶ Z) :
     ⇑(f ≫ g) = g ∘ f := by simp
 
 example {X Y Z : AlgebraCat R} (f : X ⟶ Y) (g : Y ⟶ Z) (x : X) : (f ≫ g) x = g (f x) := by simp
