ad04df3876a303393c2a7a9e067bcecefce8a678
Chris Birkbeck <c.birkbeck@uea.ac.uk>
2024-11-20 10:44:46 +0000

    feat: bounds for modular forms of non-positive weights (#18192)
    We use the Maximum modulus principle to show that modular forms of non-positive weights are bounded by its value of some element of the upper half plane imaginary part at least 1/2.
    
    
    
    
    Co-authored-by: Chris Birkbeck <c.birkbeck@ucl.ac.uk>
    Co-authored-by: David Loeffler <d.loeffler.01@cantab.net>

--- Mathlib.lean
+++ Mathlib.lean
@@ -3788,6 +3788,7 @@ import Mathlib.NumberTheory.ModularForms.JacobiTheta.Bounds
 import Mathlib.NumberTheory.ModularForms.JacobiTheta.Manifold
 import Mathlib.NumberTheory.ModularForms.JacobiTheta.OneVariable
 import Mathlib.NumberTheory.ModularForms.JacobiTheta.TwoVariable
+import Mathlib.NumberTheory.ModularForms.LevelOne
 import Mathlib.NumberTheory.ModularForms.QExpansion
 import Mathlib.NumberTheory.ModularForms.SlashActions
 import Mathlib.NumberTheory.ModularForms.SlashInvariantForms
