715f9362aa6b154a6c7a813cfd304884859eb230
Violeta Hernández <vi.hdz.p@gmail.com>
2024-11-20 17:37:02 -0600

    merge

--- Mathlib/SetTheory/Ordinal/Arithmetic.lean
+++ Mathlib/SetTheory/Ordinal/Arithmetic.lean
@@ -3,10 +3,11 @@ Copyright (c) 2017 Johannes Hölzl. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro, Floris van Doorn, Violeta Hernández Palacios
 -/
-import Mathlib.SetTheory.Ordinal.Basic
-import Mathlib.Data.Nat.SuccPred
 import Mathlib.Algebra.GroupWithZero.Divisibility
+import Mathlib.Data.Nat.SuccPred
+import Mathlib.Order.SuccPred.InitialSeg
 import Mathlib.SetTheory.Cardinal.UnivLE
+import Mathlib.SetTheory.Ordinal.Basic
 
 /-!
 # Ordinal arithmetic
