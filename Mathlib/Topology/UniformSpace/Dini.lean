d783a1ea2835dbf3a57b2790f875a2b999f8a76f
Jireh Loreaux <loreaujy@gmail.com>
2024-11-19 20:58:09 -0600

    Update Mathlib/Topology/UniformSpace/Dini.lean
    Co-authored-by: Anatole Dedecker <anatolededecker@gmail.com>

--- Mathlib/Topology/UniformSpace/Dini.lean
+++ Mathlib/Topology/UniformSpace/Dini.lean
@@ -44,7 +44,7 @@ lemma tendstoLocallyUniformly_of_forall_tendsto
   refine (atTop : Filter ι).eq_or_neBot.elim (fun h ↦ ?eq_bot) (fun _ ↦ ?_)
   case eq_bot => simp [h, tendstoLocallyUniformly_iff_forall_tendsto]
   have F_le_f (x : α) (n : ι) : F n x ≤ f x := by
-    refine _root_.ge_of_tendsto (h_tendsto x) ?_
+    refine ge_of_tendsto (h_tendsto x) ?_
     filter_upwards [Ici_mem_atTop n] with m hnm
     exact hF_mono hnm x
   simp_rw [Metric.tendstoLocallyUniformly_iff, dist_eq_norm']
