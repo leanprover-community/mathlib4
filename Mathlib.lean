079595bc46a9145b66cb653ee422cb6099282868
Joël Riou <joel.riou@universite-paris-saclay.fr>
2024-11-20 13:18:31 +0000

    feat: the monoidal category structure on homological complexes (#15695)
    Let `c : ComplexShape I` with `I` an additive monoid. If `c` is equipped with the data and axioms `c.TensorSigns`, then the category `HomologicalComplex C c` can be eqquiped with a monoidal category structure if `C` is a monoidal category such that `C` has certain coproducts and both left/right tensoring commute with these.
    
    
    In particular, we obtain a monoidal category structure on `ChainComplex C ℕ` when `C` is an additive monoidal category.
    
    
    Co-authored-by: Kim Morrison <kim@tqft.net>
    
    
    
    Co-authored-by: Joël Riou <37772949+joelriou@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -419,6 +419,7 @@ import Mathlib.Algebra.Homology.ImageToKernel
 import Mathlib.Algebra.Homology.Linear
 import Mathlib.Algebra.Homology.LocalCohomology
 import Mathlib.Algebra.Homology.Localization
+import Mathlib.Algebra.Homology.Monoidal
 import Mathlib.Algebra.Homology.Opposite
 import Mathlib.Algebra.Homology.QuasiIso
 import Mathlib.Algebra.Homology.Refinements
