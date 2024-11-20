da192ea2add0ed5a720520907afb772f2a088030
Cuijpers, Pieter <p.j.l.cuijpers@tue.nl>
2024-11-20 12:20:26 +0100

    Small edit (spaces)

--- Mathlib/Algebra/Order/Quantale.lean
+++ Mathlib/Algebra/Order/Quantale.lean
@@ -87,7 +87,7 @@ structure Quantale (α : Type*)
 section
 
 variable {α : Type*} {ι : Type*} {x y z : α} {s : Set α} {f : ι → α}
-variable [Semigroup α][CompleteLattice α][IsQuantale α]
+variable [Semigroup α] [CompleteLattice α] [IsQuantale α]
 
 @[to_additive]
 theorem mul_sSup_distrib : x * sSup s = ⨆ y ∈ s, x * y := IsQuantale.mul_sSup_distrib _ _
