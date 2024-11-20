a7eb6cccb4b46ce0d9420521b629545fb2243a82
Cuijpers, Pieter <p.j.l.cuijpers@tue.nl>
2024-11-20 21:13:10 +0100

    Added `standard?` coercion theorems - not sure which ones need to be there...

--- Mathlib/Algebra/Order/Hom/Quantale.lean
+++ Mathlib/Algebra/Order/Hom/Quantale.lean
@@ -82,7 +82,7 @@ you should parametrize over
 `(F : Type*) [FunLike F M N] [AddMonoidHomClass F M N] [CompleteLatticeHomClass F M N] (f : F)`. -/
 structure ZeroAddQuantaleHom (α β : Type*)
   [AddMonoid α] [CompleteLattice α] [AddMonoid β] [CompleteLattice β]
-  extends α →+ β, sSupHom α β
+  extends α →ₙ+q β, ZeroHom α β
 
 /-- Infix notation for `ZeroAddQuantaleHom`. -/
 infixr:25 " →+q " => ZeroAddQuantaleHom
@@ -99,6 +99,7 @@ structure.
 When possible, instead of parametrizing results over `(f : α →ₙ*q β)`,
 you should parametrize over
 `(F : Type*) [FunLike F M N] [MulHomClass F M N] [CompleteLatticeHomClass F M N] (f : F)`. -/
+@[to_additive]
 structure QuantaleHom (α β : Type*)
   [Semigroup α] [CompleteLattice α] [Semigroup β] [CompleteLattice β]
   extends α →ₙ* β, sSupHom α β
@@ -112,11 +113,116 @@ structure in case of a unital quantale.
 When possible, instead of parametrizing results over `(f : α →*q β)`,
 you should parametrize over
 `(F : Type*) [FunLike F M N] [MonoidHomClass F M N] [CompleteLatticeHomClass F M N] (f : F)`. -/
+@[to_additive]
 structure OneQuantaleHom (α β : Type*)
   [Monoid α] [CompleteLattice α] [Monoid β] [CompleteLattice β]
-  extends α →* β, sSupHom α β
+  extends α →ₙ*q β, OneHom α β
 
 /-- Infix notation for `ZeroAddQuantaleHom`. -/
 infixr:25 " →*q " => OneQuantaleHom
 
+variable [Semigroup α] [Semigroup β] [CompleteLattice α] [CompleteLattice β] [FunLike F α β]
+
+/-- Turn an element of a type `F` satisfying `MulHomClass F α β` and `sSupHomClass F α β`
+into an actual `QuantaleHom`. This is declared as the default coercion from `F` to `α →ₙ*q β`. -/
+@[to_additive (attr := coe)
+  "Turn an element of a type `F` satisfying `AddHomClass F α β` and `sSupHomClass F α β`
+into an actual `AddQuantaleHom`. This is declared as the default coercion from `F` to `α →ₙ+q β`."]
+def QuantaleHomClass.toQuantaleHom [MulHomClass F α β] [sSupHomClass F α β] (f : F) :
+    α →ₙ*q β := { (f : α →ₙ* β) with map_sSup' := sSupHomClass.map_sSup f }
+
+/-- Any type satisfying `QuantaleHomClass` can be cast into `QuantaleHom` via
+  `QuantaleHomClass.toQuantaleHom`. -/
+@[to_additive "Any type satisfying `AddQuantaleHomClass` can be cast into `AddQuantaleHom` via
+  `AddQuantaleHomClass.toAddQuantaleHom`."]
+instance [MulHomClass F α β] [sSupHomClass F α β] : CoeTC F (α →ₙ*q β) :=
+  ⟨QuantaleHomClass.toQuantaleHom⟩
+
 end Quantale
+
+section OneQuantale
+
+variable [Monoid α] [Monoid β] [CompleteLattice α] [CompleteLattice β] [FunLike F α β]
+
+/-- Turn an element of a type `F` satisfying `MonoidHomClass F α β` and `sSupHomClass F α β`
+into an actual `OneQuantaleHom`. This is declared as the default coercion from `F` to `α →*q β`. -/
+@[to_additive (attr := coe)
+  "Turn an element of a type `F` satisfying `AddMonoidHomClass F α β` and `sSupHomClass F α β`
+into an actual `ZeroAddQuantaleHom`. This is declared as the default coercion from `F` to
+`α →+q β`."]
+def OneQuantaleHomClass.toOneQuantaleHom [MonoidHomClass F α β] [sSupHomClass F α β] (f : F) :
+    α →*q β := { (f : α →* β) with map_sSup' := sSupHomClass.map_sSup f }
+
+/-- Any type satisfying `OneQuantaleHomClass` can be cast into `OneQuantaleHom` via
+  `OneQuantaleHomClass.toOneQuantaleHom`. -/
+@[to_additive "Any type satisfying `ZeroAddQuantaleHomClass` can be cast into `ZeroAddQuantaleHom`
+  via `ZeroAddQuantaleHomClass.toZeroAddQuantaleHom`."]
+instance [MonoidHomClass F α β] [sSupHomClass F α β] : CoeTC F (α →*q β) :=
+  ⟨OneQuantaleHomClass.toOneQuantaleHom⟩
+
+end OneQuantale
+
+namespace QuantaleHom
+
+variable [Semigroup α] [Semigroup β] [CompleteLattice α] [CompleteLattice β]
+  [FunLike F α β] {f g : α →ₙ*q β}
+
+@[to_additive]
+instance : FunLike (α →ₙ*q β) α β where
+  coe f := f.toFun
+  coe_injective' f g h := by
+    obtain ⟨⟨ _ ⟩, _⟩ := f
+    obtain ⟨⟨ _ ⟩, _⟩ := g
+    congr
+
+@[to_additive]
+instance : MulHomClass (α →ₙ*q β) α β where
+  map_mul f _ _ := f.map_mul' _ _
+
+@[to_additive]
+instance : sSupHomClass (α →ₙ*q β) α β where
+  map_sSup f := f.map_sSup'
+
+-- Other lemmas should be accessed through the `FunLike` API
+@[to_additive (attr := ext)]
+theorem ext (h : ∀ a, f a = g a) : f = g :=
+  DFunLike.ext f g h
+
+@[to_additive]
+theorem toFun_eq_coe (f : α →ₙ*q β) : f.toFun = (f : α → β) :=
+  rfl
+
+end QuantaleHom
+
+namespace OneQuantaleHom
+
+variable [Monoid α] [Monoid β] [CompleteLattice α] [CompleteLattice β]
+  [FunLike F α β] {f g : α →*q β}
+
+@[to_additive]
+instance : FunLike (α →*q β) α β where
+  coe f := f.toFun
+  coe_injective' f g h := by
+    obtain ⟨⟨⟨_⟩,_⟩,_⟩ := f
+    obtain ⟨⟨⟨_⟩,_⟩,_⟩ := g
+    congr
+
+@[to_additive]
+instance : MonoidHomClass (α →*q β) α β where
+  map_mul f _ _ := f.map_mul' _ _
+  map_one f := f.map_one'
+
+@[to_additive]
+instance : sSupHomClass (α →*q β) α β where
+  map_sSup f := f.map_sSup'
+
+-- Other lemmas should be accessed through the `FunLike` API
+@[to_additive (attr := ext)]
+theorem ext (h : ∀ a, f a = g a) : f = g :=
+  DFunLike.ext f g h
+
+@[to_additive]
+theorem toFun_eq_coe (f : α →ₙ*q β) : f.toFun = (f : α → β) :=
+  rfl
+
+end OneQuantaleHom
