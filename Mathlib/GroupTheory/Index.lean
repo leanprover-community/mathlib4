6f3c07dfd861ccfa8f23d8b39076808f7b36174a
Riccardo Brasca <riccardo.brasca@gmail.com>
2024-11-21 10:36:07 +0100

    Update Mathlib/GroupTheory/Index.lean
    Co-authored-by: Thomas Browning <tb65536@users.noreply.github.com>

--- Mathlib/GroupTheory/Index.lean
+++ Mathlib/GroupTheory/Index.lean
@@ -563,7 +563,7 @@ lemma index_antitone (h : H ≤ K) [H.FiniteIndex] : K.index ≤ H.index :=
 
 @[to_additive (attr := gcongr)]
 lemma index_strictAnti (h : H < K) [hf : H.FiniteIndex] : K.index < H.index := by
-  have h0 : K.index ≠ 0 := fun H ↦ hf.1 <| by simpa [H] using (index_dvd_of_le h.le)
+  have h0 : K.index ≠ 0 := (finiteIndex_of_le h.le).finiteIndex
   apply lt_of_le_of_ne (index_antitone h.le)
   rw [← relindex_mul_index h.le, Ne, eq_comm, mul_eq_right₀ h0, relindex_eq_one]
   exact h.not_le
