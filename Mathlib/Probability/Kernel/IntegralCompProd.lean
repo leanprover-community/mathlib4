c15d47c22c76b9cdfea13eb6210bad1630aede22
RemyDegenne <Remydegenne@gmail.com>
2024-11-21 20:31:48 +0100

    fix

--- Mathlib/Probability/Kernel/IntegralCompProd.lean
+++ Mathlib/Probability/Kernel/IntegralCompProd.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Rémy Degenne
 -/
 import Mathlib.Probability.Kernel.Composition
+import Mathlib.Probability.Kernel.MeasurableIntegral
 import Mathlib.MeasureTheory.Integral.SetIntegral
 
 /-!
