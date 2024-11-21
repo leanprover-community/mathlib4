7422120b48170afcc5f3b043f2a8c644dc24c330
Kim Morrison <kim@tqft.net>
2024-11-22 09:53:48 +1100

    +zetaDelta

--- Mathlib/Data/Nat/Prime/Basic.lean
+++ Mathlib/Data/Nat/Prime/Basic.lean
@@ -217,7 +217,7 @@ theorem ne_one_iff_exists_prime_dvd : ∀ {n}, n ≠ 1 ↔ ∃ p : ℕ, p.Prime 
   | n + 2 => by
     let a := n + 2
     let ha : a ≠ 1 := Nat.succ_succ_ne_one n
-    simp only [true_iff, Ne, not_false_iff, ha]
+    simp +zetaDelta only [true_iff, Ne, not_false_iff, ha]
     exact ⟨a.minFac, Nat.minFac_prime ha, a.minFac_dvd⟩
 
 theorem eq_one_iff_not_exists_prime_dvd {n : ℕ} : n = 1 ↔ ∀ p : ℕ, p.Prime → ¬p ∣ n := by
