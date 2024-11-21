d4b4a0901144936d7a334d25b089adcbcc4cd886
Jack McKoen <mckoen@ualberta.ca>
2024-11-21 11:52:55 -0700

    fix imports

--- Mathlib/CategoryTheory/MorphismProperty/Retract.lean
+++ Mathlib/CategoryTheory/MorphismProperty/Retract.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jack McKoen
 -/
 import Mathlib.CategoryTheory.Retract
+import Mathlib.CategoryTheory.MorphismProperty.Basic
 
 /-!
 # Stability under retracts
--- Mathlib/CategoryTheory/Retract.lean
+++ Mathlib/CategoryTheory/Retract.lean
@@ -3,7 +3,7 @@ Copyright (c) 2024 Jack McKoen. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jack McKoen
 -/
-import Mathlib.CategoryTheory.MorphismProperty.Basic
+import Mathlib.CategoryTheory.Comma.Arrow
 import Mathlib.CategoryTheory.EpiMono
 
 /-!
