bded1a9eb3fc57b04f6ff80b7449c55ed2a1baa4
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2024-11-21 12:36:28 +0000

    feat(RingTheory/DividedPowers/Basic): definition of divided powers (#15657)
    Define the notion of a divided power structure on an ideal of a ring.
    
    Prove basic lemmas:
    * the variant of the multinomial theorem in this context 
    * a formula for products of divided powers
    
    Co-authored with: [María Inés de Frutos-Fernández](https://github.com/mariainesdff)
    
    
    
    Co-authored-by: leanprover-community-mathlib4-bot <leanprover-community-mathlib4-bot@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -4197,6 +4197,7 @@ import Mathlib.RingTheory.Derivation.ToSquareZero
 import Mathlib.RingTheory.DiscreteValuationRing.Basic
 import Mathlib.RingTheory.DiscreteValuationRing.TFAE
 import Mathlib.RingTheory.Discriminant
+import Mathlib.RingTheory.DividedPowers.Basic
 import Mathlib.RingTheory.DualNumber
 import Mathlib.RingTheory.EisensteinCriterion
 import Mathlib.RingTheory.EssentialFiniteness
