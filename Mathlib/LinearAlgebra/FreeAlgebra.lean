e322e6f8f9381828f246366d2382fc37b04deb25
acmepjz <acme_pjz@hotmail.com>
2024-11-21 16:52:56 +0800

    add `Algebra.lift_cardinalMk_adjoin_le`

--- Mathlib/LinearAlgebra/FreeAlgebra.lean
+++ Mathlib/LinearAlgebra/FreeAlgebra.lean
@@ -9,6 +9,7 @@ import Mathlib.LinearAlgebra.Dimension.StrongRankCondition
 import Mathlib.LinearAlgebra.Finsupp.VectorSpace
 import Mathlib.LinearAlgebra.FreeModule.Basic
 import Mathlib.LinearAlgebra.FreeModule.StrongRankCondition
+import Mathlib.SetTheory.Cardinal.Free
 
 /-!
 # Linear algebra properties of `FreeAlgebra R X`
@@ -46,10 +47,14 @@ theorem rank_eq [CommRing R] [Nontrivial R] :
 
 end FreeAlgebra
 
+namespace Algebra
+
 open Cardinal
 
-theorem Algebra.rank_adjoin_le {R : Type u} {S : Type v} [CommRing R] [Ring S] [Algebra R S]
-    (s : Set S) : Module.rank R (adjoin R s) ≤ max #s ℵ₀ := by
+variable (R : Type u) {S : Type v} (s : Set S)
+
+theorem rank_adjoin_le [CommRing R] [Ring S] [Algebra R S] :
+    Module.rank R (adjoin R s) ≤ max #s ℵ₀ := by
   rw [adjoin_eq_range_freeAlgebra_lift]
   cases subsingleton_or_nontrivial R
   · rw [rank_subsingleton]; exact one_le_aleph0.trans (le_max_right _ _)
@@ -57,3 +62,24 @@ theorem Algebra.rank_adjoin_le {R : Type u} {S : Type v} [CommRing R] [Ring S] [
   refine (lift_rank_range_le (FreeAlgebra.lift R ((↑) : s → S)).toLinearMap).trans ?_
   rw [FreeAlgebra.rank_eq, lift_id'.{v,u}, lift_umax.{v,u}, lift_le, max_comm]
   exact mk_list_le_max _
+
+theorem lift_cardinalMk_adjoin_le [CommSemiring R] [Semiring S] [Algebra R S] :
+    lift.{u} #(adjoin R s) ≤ max (max (lift.{v} #R) (lift.{u} #s)) ℵ₀ := by
+  by_cases hs : s = ∅
+  · simp_rw [hs, adjoin_empty, mk_eq_zero, lift_zero]
+    simp only [zero_le, sup_of_le_left]
+    exact (lift_mk_le_lift_mk_of_surjective (f := algebraMap R (⊥ : Subalgebra R S)) <| by
+      rintro ⟨_, x, rfl⟩; exact ⟨x, rfl⟩).trans le_sup_left
+  rcases subsingleton_or_nontrivial R with _ | _
+  · haveI := (algebraMap R (adjoin R s)).codomain_trivial
+    exact (lift_le_aleph0.2 mk_le_aleph0).trans le_sup_right
+  rw [← Ne, ← Set.nonempty_iff_ne_empty'] at hs
+  rw [adjoin_eq_range_freeAlgebra_lift]
+  have hs' := mk_freeMonoid s
+  haveI : Infinite (FreeMonoid s) := infinite_iff.2 (by simp [hs'])
+  have H := mk_range_le_lift (f := FreeAlgebra.lift R ((↑) : s → S))
+  rwa [lift_umax, lift_id'.{v, u}, FreeAlgebra.equivMonoidAlgebraFreeMonoid.toEquiv.cardinal_eq,
+    MonoidAlgebra, mk_finsupp_lift_of_infinite, hs', lift_max, lift_aleph0, sup_comm,
+    ← sup_assoc] at H
+
+end Algebra
