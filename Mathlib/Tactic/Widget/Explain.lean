021913cd64daffa5045af69a1207bc3f38643540
adamtopaz <github@adamtopaz.com>
2024-11-22 10:18:03 -0700

    lint

--- Mathlib/Tactic/Widget/Explain.lean
+++ Mathlib/Tactic/Widget/Explain.lean
@@ -3,6 +3,7 @@ Copyright (c) 2024 Adam Topaz. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Adam Topaz
 -/
+import Mathlib.Init
 import ProofWidgets
 
 /-!
