bd768a6ee27232a33497194d697adcc572bbb5d8
adomani <adomani@gmail.com>
2024-11-22 12:31:54 +0100

    test: try emoji action

--- scripts/zulip_emoji_merge_delegate.py
+++ scripts/zulip_emoji_merge_delegate.py
@@ -11,7 +11,10 @@ import re
 ZULIP_API_KEY = sys.argv[1]
 ZULIP_EMAIL = sys.argv[2]
 ZULIP_SITE = sys.argv[3]
-GITHUB_TOKEN = sys.argv[4]
+LABEL = sys.argv[4]
+#GITHUB_TOKEN = sys.argv[5]
+
+print(f"LABEL: '{LABEL}'")
 
 # Initialize Zulip client
 client = zulip.Client(
@@ -28,6 +31,8 @@ response = client.get_messages({
     "narrow": [{"operator": "channel", "operand": "PR reviews"}],
 })
 
+print(f"response: '{response}'")
+
 messages = response['messages']
 pr_pattern = re.compile(r'https://github\.com/leanprover-community/mathlib4/pull/(\d+)')
 
@@ -35,6 +40,7 @@ for message in messages:
     content = message['content']
     match = pr_pattern.search(content)
     if match:
+        print(f"matched: '{message}'")
         pr_number = match.group(1)
         # Check for emoji reactions
         reactions = message['reactions']
@@ -43,9 +49,9 @@ for message in messages:
         has_merge = any(reaction['emoji_name'] == 'merge' for reaction in reactions)
 
         pr_url = f"https://api.github.com/repos/leanprover-community/mathlib4/pulls/{pr_number}"
-        pr_response = requests.get(pr_url, headers={"Authorization": GITHUB_TOKEN})
-        pr_data = pr_response.json()
-        labels = [label['name'] for label in pr_data['labels']]
+        #pr_response = requests.get(pr_url, headers={"Authorization": GITHUB_TOKEN})
+        #pr_data = pr_response.json()
+        labels = [LABEL]
 
         if 'delegated' in labels:
             client.add_reaction({
@@ -62,7 +68,7 @@ for message in messages:
                 "message_id": message['id'],
                 "emoji_name": "bors"
             })
-        elif pr_data['title'].startswith("[Merged by Bors]"):
+        elif LABEL.startswith("[Merged by Bors]"):
             if has_peace_sign:
                 client.remove_reaction({
                     "message_id": message['id'],
