be959a9ade921f874584eb5761e7c4f6c82f6651
blizzard_inc <thrashy4inbox@gmail.com>
2024-11-22 13:33:56 +0000

    feat(LinearAlgebra/Matrix/Determinant/TotallyUnimodular): iff_fintype (#19366)
    add a lemma relating the definition of `Matrix.IsTotallyUnimodular` to `Fintype` rather than `Fin`.
    
    
    
    Co-authored-by: blizzard_inc <edegeltje@gmail.com>

--- Mathlib/LinearAlgebra/Matrix/Determinant/TotallyUnimodular.lean
+++ Mathlib/LinearAlgebra/Matrix/Determinant/TotallyUnimodular.lean
@@ -54,6 +54,18 @@ lemma isTotallyUnimodular_iff (A : Matrix m n R) : A.IsTotallyUnimodular ↔
   · intro _ _ _ _ _
     apply hA
 
+lemma isTotallyUnimodular_iff_fintype.{w} (A : Matrix m n R) : A.IsTotallyUnimodular ↔
+    ∀ (ι : Type w) [Fintype ι] [DecidableEq ι], ∀ f : ι → m, ∀ g : ι → n,
+      (A.submatrix f g).det ∈ Set.range SignType.cast := by
+  rw [isTotallyUnimodular_iff]
+  constructor
+  · intro hA ι _ _ f g
+    specialize hA (Fintype.card ι) (f ∘ (Fintype.equivFin ι).symm) (g ∘ (Fintype.equivFin ι).symm)
+    rwa [←submatrix_submatrix, det_submatrix_equiv_self] at hA
+  · intro hA k f g
+    specialize hA (ULift (Fin k)) (f ∘ Equiv.ulift) (g ∘ Equiv.ulift)
+    rwa [←submatrix_submatrix, det_submatrix_equiv_self] at hA
+
 lemma IsTotallyUnimodular.apply {A : Matrix m n R} (hA : A.IsTotallyUnimodular)
     (i : m) (j : n) :
     A i j ∈ Set.range SignType.cast := by
