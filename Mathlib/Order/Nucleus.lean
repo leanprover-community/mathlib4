c3f4d08fc7ca5b8cf525aef7768191dcbd48fbaa
Bergschaf <christian.k@netcom-mail.de>
2024-11-24 19:05:21 +0100

    nucleus montone

--- Mathlib/Order/Nucleus.lean
+++ Mathlib/Order/Nucleus.lean
@@ -10,3 +10,11 @@ structure Nucleus (X : Type*) [Order.Frame X] where
 
 lemma nucleus_preserves_top (n : Nucleus X) : n.toFun ⊤ = ⊤ :=
    top_le_iff.mp (n.increasing ⊤)
+
+lemma nucleus_monotone (n : Nucleus X) : Monotone n.toFun := by
+  rw [Monotone]
+  intro a b h
+  have h1 : a ⊓ b = a := inf_eq_left.mpr h
+  have h2 := n.preserves_inf a b
+  rw [h1] at h2
+  exact left_eq_inf.mp h2
