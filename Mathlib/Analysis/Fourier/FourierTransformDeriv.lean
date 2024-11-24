00e301532fdc433a8c9bfdc3b7f0fc64ad2f85d4
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2024-11-24 18:01:41 +0100

    fix

--- Mathlib/Analysis/Fourier/FourierTransformDeriv.lean
+++ Mathlib/Analysis/Fourier/FourierTransformDeriv.lean
@@ -9,6 +9,7 @@ import Mathlib.Analysis.Fourier.FourierTransform
 import Mathlib.Analysis.Calculus.FDeriv.Analytic
 import Mathlib.Analysis.Calculus.LineDeriv.IntegrationByParts
 import Mathlib.Analysis.Calculus.ContDiff.Bounds
+import Mathlib.Analysis.Calculus.ContDiff.CPolynomial
 
 /-!
 # Derivatives of the Fourier transform
