ce1696e3e3e6adf82aba68001b494d68b1d2a8d5
Violeta Hernández <vi.hdz.p@gmail.com>
2024-11-23 22:49:23 -0600

    Update Finsupp.lean

--- Mathlib/Algebra/BigOperators/Finsupp.lean
+++ Mathlib/Algebra/BigOperators/Finsupp.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kenny Lau
 -/
 import Mathlib.Algebra.BigOperators.Pi
-import Mathlib.Algebra.BigOperators.Fin
+import Mathlib.Algebra.BigOperators.Ring
 import Mathlib.Algebra.Group.Submonoid.Membership
 import Mathlib.Data.Finsupp.Fin
 import Mathlib.Data.Finsupp.Indicator
