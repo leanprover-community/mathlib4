f7e97c6b7ba25630b48d0bf1f4cad5522b7676ac
Ben Eltschig <b.eltschig@protonmail.com>
2024-11-24 05:51:51 +0100

    remove redundant import

--- Mathlib/Topology/Compactness/DeltaGeneratedSpace.lean
+++ Mathlib/Topology/Compactness/DeltaGeneratedSpace.lean
@@ -3,7 +3,6 @@ Copyright (c) 2022 Ben Eltschig. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Ben Eltschig
 -/
-import Mathlib.Topology.Compactness.CompactlyGeneratedSpace
 import Mathlib.Analysis.Convex.Normed
 
 /-!
