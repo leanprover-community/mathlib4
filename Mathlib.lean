65e2e49479602a8ec450d34181ab360ead947b02
Yaël Dillies <yael.dillies@gmail.com>
2024-11-25 18:36:41 +0000

    feat: group markings From LeanCamCombi

--- Mathlib.lean
+++ Mathlib.lean
@@ -4044,6 +4044,7 @@ public import Mathlib.Geometry.Euclidean.Sphere.Tangent
 public import Mathlib.Geometry.Euclidean.Triangle
 public import Mathlib.Geometry.Group.Growth.LinearLowerBound
 public import Mathlib.Geometry.Group.Growth.QuotientInter
+public import Mathlib.Geometry.Group.Marking
 public import Mathlib.Geometry.Manifold.Algebra.LeftInvariantDerivation
 public import Mathlib.Geometry.Manifold.Algebra.LieGroup
 public import Mathlib.Geometry.Manifold.Algebra.Monoid
