f309c1ccf0d815624e5c278fc050c0931f733ee0
Anne C.A. Baanen <vierkantor@vierkantor.com>
2024-11-25 14:18:54 +0100

    shake

--- Mathlib/Data/Finsupp/Ext.lean
+++ Mathlib/Data/Finsupp/Ext.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl, Kim Morrison
 -/
 import Mathlib.Algebra.Group.Submonoid.Basic
+import Mathlib.Algebra.Group.TypeTags.Hom
 import Mathlib.Data.Finsupp.Defs
 
 /-!
