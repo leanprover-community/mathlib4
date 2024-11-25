ffdb4bf7ddbeefae54499ebb7eb026480b411883
Yaël Dillies <yael.dillies@gmail.com>
2024-11-25 18:11:42 +0000

    golf

--- Mathlib/Algebra/Order/Ring/Finset.lean
+++ Mathlib/Algebra/Order/Ring/Finset.lean
@@ -4,8 +4,8 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Eric Wieser, Yaël Dillies, Andrew Yang
 -/
 import Mathlib.Algebra.Order.Field.Canonical.Defs
-import Mathlib.Algebra.Order.Floor
 import Mathlib.Data.Finset.Lattice.Fold
+import Mathlib.Data.Nat.Cast.Order.Ring
 
 /-!
 # `Finset.sup` of `Nat.cast`
@@ -21,19 +21,19 @@ variable [LinearOrderedSemiring R] {s : Finset ι}
 
 set_option linter.docPrime false in
 @[simp, norm_cast]
-lemma cast_finsetSup' (f : ι → ℕ) (hs) : ((s.sup' hs f : ℕ) : R) = s.sup' hs fun i ↦ (f i : R) := by
-  induction hs using Finset.Nonempty.cons_induction <;> simp [*]
+lemma cast_finsetSup' (f : ι → ℕ) (hs) : ((s.sup' hs f : ℕ) : R) = s.sup' hs fun i ↦ (f i : R) :=
+  comp_sup'_eq_sup'_comp _ _ cast_max
 
 set_option linter.docPrime false in
 @[simp, norm_cast]
-lemma cast_finsetInf' (f : ι → ℕ) (hs) : (↑(s.inf' hs f) : R) = s.inf' hs fun i ↦ (f i : R) := by
-  induction hs using Finset.Nonempty.cons_induction <;> simp [*]
+lemma cast_finsetInf' (f : ι → ℕ) (hs) : (↑(s.inf' hs f) : R) = s.inf' hs fun i ↦ (f i : R) :=
+  comp_inf'_eq_inf'_comp _ _ cast_min
 
 end LinearOrderedSemiring
 
 @[simp, norm_cast]
-lemma cast_finsetSup [CanonicallyLinearOrderedSemifield R] [FloorSemiring R]
-    (s : Finset ι) (f : ι → ℕ) : (↑(s.sup f) : R) = s.sup fun i ↦ (f i : R) := by
-  comp_sup_eq_sup_comp _ (by simp) (by simp)
+lemma cast_finsetSup [CanonicallyLinearOrderedSemifield R] (s : Finset ι) (f : ι → ℕ) :
+    (↑(s.sup f) : R) = s.sup fun i ↦ (f i : R) :=
+  comp_sup_eq_sup_comp _ cast_max (by simp)
 
 end Nat
