9184f7f11d3170e67bf835d0fc876b59b4c0c7db
Yaël Dillies <yael.dillies@gmail.com>
2024-11-25 17:50:30 +0000

    mk_all

--- Mathlib.lean
+++ Mathlib.lean
@@ -740,6 +740,7 @@ import Mathlib.Algebra.Order.Ring.Canonical
 import Mathlib.Algebra.Order.Ring.Cast
 import Mathlib.Algebra.Order.Ring.Cone
 import Mathlib.Algebra.Order.Ring.Defs
+import Mathlib.Algebra.Order.Ring.Finset
 import Mathlib.Algebra.Order.Ring.InjSurj
 import Mathlib.Algebra.Order.Ring.Int
 import Mathlib.Algebra.Order.Ring.Nat
