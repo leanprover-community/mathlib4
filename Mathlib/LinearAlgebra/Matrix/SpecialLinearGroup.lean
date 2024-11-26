722cc9f5b6241db5e860dbbfb2b5fd829a1dc6e5
judithludwig <judith.ludwig@iwr.uni-heidelberg.de>
2024-11-26 20:24:40 +0100

    removed import

--- Mathlib/LinearAlgebra/Matrix/SpecialLinearGroup.lean
+++ Mathlib/LinearAlgebra/Matrix/SpecialLinearGroup.lean
@@ -3,7 +3,6 @@ Copyright (c) 2020 Anne Baanen. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Anne Baanen, Wen Yang
 -/
-import Mathlib.LinearAlgebra.GeneralLinearGroup
 import Mathlib.LinearAlgebra.Matrix.Adjugate
 import Mathlib.LinearAlgebra.Matrix.Transvection
 import Mathlib.RingTheory.RootsOfUnity.Basic
