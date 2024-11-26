da8f7d0d7c24aad747f0126e0ff98f55f0cb4383
Kim Morrison <kim@tqft.net>
2024-11-27 09:31:50 +1100

    add Subgroup / integer analogue

--- Mathlib/Algebra/BigOperators/Finsupp.lean
+++ Mathlib/Algebra/BigOperators/Finsupp.lean
@@ -7,6 +7,7 @@ import Mathlib.Algebra.BigOperators.Pi
 import Mathlib.Algebra.BigOperators.Ring
 import Mathlib.Algebra.BigOperators.Fin
 import Mathlib.Algebra.Group.Submonoid.Membership
+import Mathlib.Algebra.Group.Subgroup.Lattice
 import Mathlib.Data.Finsupp.Fin
 import Mathlib.Data.Finsupp.Indicator
 
@@ -119,6 +120,11 @@ theorem sum_ite_self_eq' [DecidableEq α] {N : Type*} [AddCommMonoid N] (f : α 
 theorem prod_pow [Fintype α] (f : α →₀ ℕ) (g : α → N) :
     (f.prod fun a b => g a ^ b) = ∏ a, g a ^ f a :=
   f.prod_fintype _ fun _ ↦ pow_zero _
+
+@[to_additive (attr := simp)]
+theorem prod_zpow {N} [CommGroup N] [Fintype α] (f : α →₀ ℤ) (g : α → N) :
+    (f.prod fun a b => g a ^ b) = ∏ a, g a ^ f a :=
+  f.prod_fintype _ fun _ ↦ zpow_zero _
 
 /-- If `g` maps a second argument of 0 to 1, then multiplying it over the
 result of `onFinset` is the same as multiplying it over the original `Finset`. -/
@@ -624,3 +630,50 @@ theorem mem_closure_range_iff_of_fintype [Fintype ι] :
   simp
 
 end Submonoid
+
+namespace Subgroup
+
+variable {M : Type*} [CommGroup M] {ι : Type*} (f : ι → M) (x : M)
+
+@[to_additive]
+theorem exists_finsupp_of_mem_closure_range (hx : x ∈ closure (Set.range f)) :
+    ∃ a : ι →₀ ℤ, x = a.prod (f · ^ ·) := by
+  classical
+  induction hx using closure_induction with
+  | mem x h => obtain ⟨i, rfl⟩ := h; exact ⟨Finsupp.single i 1, by simp⟩
+  | one => use 0; simp
+  | mul x y hx hy hx' hy' =>
+    obtain ⟨⟨v, rfl⟩, w, rfl⟩ := And.intro hx' hy'
+    use v + w
+    rw [Finsupp.prod_add_index]
+    · simp
+    · simp [zpow_add]
+  | inv x hx hx' =>
+    obtain ⟨a, rfl⟩ := hx'
+    use -a
+    rw [Finsupp.prod_neg_index]
+    · simp
+    · simp [zpow_neg]
+
+variable {f x} in
+@[to_additive]
+theorem mem_closure_range_iff :
+    x ∈ closure (Set.range f) ↔ ∃ a : ι →₀ ℤ, x = a.prod (f · ^ ·) := by
+  refine ⟨exists_finsupp_of_mem_closure_range f x, ?_⟩
+  rintro ⟨a, rfl⟩
+  exact Submonoid.prod_mem _ fun i hi ↦ zpow_mem (subset_closure (Set.mem_range_self i)) _
+
+@[to_additive]
+theorem exists_of_mem_closure_range [Fintype ι] (hx : x ∈ closure (Set.range f)) :
+    ∃ a : ι → ℤ, x = ∏ i, f i ^ a i := by
+  obtain ⟨a, rfl⟩ := exists_finsupp_of_mem_closure_range f x hx
+  exact ⟨a, by simp⟩
+
+variable {f x} in
+@[to_additive]
+theorem mem_closure_range_iff_of_fintype [Fintype ι] :
+    x ∈ closure (Set.range f) ↔ ∃ a : ι → ℤ, x = ∏ i, f i ^ a i := by
+  rw [Finsupp.equivFunOnFinite.symm.exists_congr_left, mem_closure_range_iff]
+  simp
+
+end Subgroup
