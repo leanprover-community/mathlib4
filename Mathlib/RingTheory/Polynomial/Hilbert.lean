0c99f07f60452ab5b7ab2163f9022fde748c020b
FMLJohn <417963616@qq.com>
2024-11-26 15:51:16 +0000

    added `natDegree_hilbert`

--- Mathlib/RingTheory/Polynomial/Hilbert.lean
+++ Mathlib/RingTheory/Polynomial/Hilbert.lean
@@ -6,7 +6,10 @@ Authors: Fangming Li, Jujian Zhang
 import Mathlib.Algebra.Polynomial.AlgebraMap
 import Mathlib.Algebra.Polynomial.Div
 import Mathlib.Algebra.Polynomial.Eval.SMul
+import Mathlib.Algebra.Polynomial.Roots
 import Mathlib.Data.Nat.Factorial.BigOperators
+import Mathlib.Order.Interval.Set.Infinite
+import Mathlib.RingTheory.Polynomial.Pochhammer
 import Mathlib.RingTheory.PowerSeries.WellKnown
 
 /-!
@@ -107,25 +110,21 @@ A polynomial which makes it easier to define the Hilbert polynomial. See also th
 `(Polynomial.preHilbert d k).eval (n : ℚ) = (n - k + d).choose d`.
 -/
 noncomputable def preHilbert (d k : ℕ) : ℚ[X] :=
-  (d.factorial : ℚ)⁻¹ • (∏ i : Finset.range d, (X - (C (k : ℚ)) + (C (i : ℚ)) + 1))
+  (d.factorial : ℚ)⁻¹ • ((ascPochhammer ℚ d).comp (X - (C (k : ℚ)) + 1))
 
 local notation "gFOSND" => greatestFactorOneSubNotDvd
 
 theorem preHilbert_eq_choose_sub_add (d k n : ℕ) (hkn : k ≤ n):
     (preHilbert d k).eval (n : ℚ) = (n - k + d).choose d := by
-  delta preHilbert; simp only [Finset.univ_eq_attach, map_natCast, eval_smul, smul_eq_mul]
-  rw [eval_prod, Finset.prod_attach _ (fun _ => eval _ (_ - _ + _ + _)), add_choose,
-    cast_div (factorial_mul_factorial_dvd_factorial_add ..) (cast_ne_zero.2 <| mul_ne_zero
-    (n - k).factorial_ne_zero d.factorial_ne_zero), cast_mul, div_mul_eq_div_div, div_eq_inv_mul,
-    mul_eq_mul_left_iff, ← cast_div (factorial_dvd_factorial <| Nat.le_add_right (n - k) d)
-    (cast_ne_zero.2 <| factorial_ne_zero (n - k)), ← ascFactorial_eq_div]
-  simp_rw [eval_add, eval_sub, eval_X, eval_natCast, eval_one, ascFactorial_eq_prod_range,
-    cast_prod, cast_add, cast_one, Nat.cast_sub hkn, add_assoc, add_comm, true_or]
+  rw [preHilbert, eval_smul, eval_comp, map_natCast, eval_add, eval_sub, eval_X, eval_natCast,
+    eval_one, smul_eq_mul, ← cast_sub hkn, ← cast_add_one, ← ascPochhammer_eval_cast,
+    ascPochhammer_nat_eq_ascFactorial, ascFactorial_eq_factorial_mul_choose, cast_mul,
+    ← mul_assoc, ← div_eq_inv_mul, div_self (cast_ne_zero.2 (factorial_ne_zero d)), one_mul]
 
 /--
 Given `p : ℤ[X]` and `d : ℕ`, the Hilbert polynomial of `p` and `d`.
 See also `Polynomial.coeff_mul_invOneSubPow_eq_hilbert_eval`, which says
-that `PowerSeries.coeff ℤ n (p * (@invOneSubPow ℤ _ d))` is equal to
+that `PowerSeries.coeff ℤ n (p * (invOneSubPow ℤ d))` is equal to
 `(Polynomial.hilbert p d).eval (n : ℚ)` for any large enough `n : ℕ`.
 -/
 noncomputable def hilbert (p : ℤ[X]) (d : ℕ) : ℚ[X] :=
@@ -136,7 +135,7 @@ noncomputable def hilbert (p : ℤ[X]) (d : ℕ) : ℚ[X] :=
 
 /--
 Given `p : ℤ[X]` and `d : ℕ`. The key property of the Hilbert polynomial with respect to
-`p` and `d`, which says that for any term of `p * (@invOneSubPow ℤ _ d)` whose degree is
+`p` and `d`, which says that for any term of `p * (invOneSubPow ℤ d)` whose degree is
 large enough, its coefficient can be obtained by evaluating the Hilbert polynomial.
 -/
 theorem coeff_mul_invOneSubPow_eq_hilbert_eval (p : ℤ[X]) (d n : ℕ) (hn : p.natDegree < n) :
@@ -184,5 +183,73 @@ theorem coeff_mul_invOneSubPow_eq_hilbert_eval (p : ℤ[X]) (d n : ℕ) (hn : p.
         rw [Int.cast_natCast, eval_intCast, add_comm])).2 (fun i hi => by
         rw [Finset.mem_range] at hi; simp only [hi, reduceIte]; rw [add_comm];
         simp only [Int.cast_natCast, eval_intCast])
+
+/--
+The Hilbert polynomial is unique. In other words, for any `h : ℚ[X]`, if `h`
+satisfies the key property of the Hilbert polynomial (i.e. for any large enough
+`n : ℕ`, `PowerSeries.coeff ℤ n (p * (invOneSubPow ℤ d)) = h.eval (n : ℚ)`),
+then `h` is the Hilbert polynomial.
+-/
+theorem exists_unique_hilbert (p : Polynomial ℤ) (d : ℕ) :
+    ∃! (h : ℚ[X]), (∃ (N : ℕ), (∀ (n : ℕ) (_ : N < n),
+    PowerSeries.coeff ℤ n (p * (invOneSubPow ℤ d)) = h.eval (n : ℚ))) :=
+  ⟨hilbert p d, ⟨p.natDegree, fun n hn => coeff_mul_invOneSubPow_eq_hilbert_eval p d n hn⟩,
+  fun q ⟨N, hqN⟩ => eq_of_infinite_eval_eq q (hilbert p d) <| fun hfin => Set.Infinite.image
+  (Set.injOn_of_injective Nat.cast_injective) (Set.Ioi_infinite (max N p.natDegree)) <|
+  Set.Finite.subset hfin <| show @Nat.cast ℚ _ '' (Set.Ioi (max N p.natDegree)) ⊆
+  (@setOf ℚ fun x => q.eval x = (hilbert p d).eval x) by
+  intro x hx; simp only [Set.mem_image, Set.mem_Ioi, max_lt_iff, Set.mem_setOf_eq] at hx ⊢;
+  rcases hx with ⟨n, ⟨h1, h2⟩, h3⟩; rw [← h3, ← coeff_mul_invOneSubPow_eq_hilbert_eval p d n h2];
+  exact (Rat.ext (congrArg Rat.num (hqN n h1)) (congrArg Rat.den (hqN n h1))).symm⟩
+
+theorem leadingCoeff_ascPochhammer (R : Type*) [Semiring R] [Nontrivial R] (n : ℕ) :
+    leadingCoeff (ascPochhammer R n) = 1 := by
+  induction n with
+  | zero => simp only [ascPochhammer_zero, monic_one, Monic.leadingCoeff]
+  | succ n hn =>
+      rw [ascPochhammer_succ_right, leadingCoeff_mul']
+      · rw [hn, one_mul, ← C_eq_natCast, leadingCoeff_X_add_C]
+      · rw [hn, one_mul, ← C_eq_natCast, leadingCoeff_X_add_C]; exact one_ne_zero
+
+/--
+This theorem tells us the specific degree of any non-zero Hilbert polynomial.
+-/
+theorem natDegree_hilbert (p : ℤ[X]) (d : ℕ) (hh : hilbert p d ≠ 0) :
+    (hilbert p d).natDegree = d - p.rootMultiplicity 1 - 1 := by
+  by_cases h : p = 0
+  · exfalso; rw [hilbert] at hh; simp only [h, reduceDIte, ne_eq, not_true_eq_false] at hh
+  · by_cases h1 : d ≤ p.rootMultiplicity 1
+    · rw [hilbert] at hh
+      simp only [h1, reduceIte, dite_eq_ite, ite_self, ne_eq, not_true_eq_false] at hh
+    · rw [hilbert]; apply natDegree_eq_of_le_of_coeff_ne_zero _ _
+      · simp only [h, reduceDIte, h1, reduceIte]
+        apply natDegree_sum_le_of_forall_le _ _ _
+        · intro i _; apply le_trans natDegree_mul_le _
+          · simp only [natDegree_intCast, zero_add]
+            apply le_trans (natDegree_smul_le _ _) _
+            · rw [natDegree_comp, ascPochhammer_natDegree, add_comm_sub, show 1 - _ = C (1 - i : ℚ)
+                by simp only [map_sub, map_one], natDegree_add_C, natDegree_X, mul_one]
+      · simp only [h, reduceDIte, h1, reduceIte, finset_sum_coeff, coeff_intCast_mul, preHilbert,
+          coeff_smul, map_natCast, smul_eq_mul]
+        have (x : ℕ) : ((ascPochhammer ℚ (d - rootMultiplicity 1 p - 1)).comp
+            (X - (x : ℚ[X]) + 1)).coeff (d - rootMultiplicity 1 p - 1) = 1 := by
+          rw [sub_add, show x - 1 = C (x - 1 : ℚ) by simp only [map_natCast, map_sub, map_one]]
+          have : ((ascPochhammer ℚ (d - rootMultiplicity 1 p - 1)).comp
+              (X - C ((x : ℚ) - 1))).coeff (d - rootMultiplicity 1 p - 1) = ((ascPochhammer ℚ
+              (d - rootMultiplicity 1 p - 1)).comp (X - C ((x : ℚ) - 1))).coeff ((ascPochhammer ℚ
+              (d - rootMultiplicity 1 p - 1)).comp (X - C ((x : ℚ) - 1))).natDegree := by
+            rw [natDegree_comp, ascPochhammer_natDegree, natDegree_X_sub_C, mul_one]
+          rw [this, coeff_natDegree, leadingCoeff_comp <| ne_of_eq_of_ne (natDegree_X_sub_C _)
+            one_ne_zero, leadingCoeff_ascPochhammer, one_mul, leadingCoeff_X_sub_C, one_pow]
+        simp_rw [this]
+        rw [mul_one, ← Finset.sum_mul, show ∑ x in _, @Int.cast ℚ _ _ = (gFOSND p h).eval 1 by
+          rw [eval_eq_sum_range]; simp only [one_pow, mul_one, Int.cast_sum], ne_eq,
+          _root_.mul_eq_zero, inv_eq_zero, cast_eq_zero, not_or, greatestFactorOneSubNotDvd]
+        constructor
+        · simp only [eval_mul, eval_pow, eval_neg, eval_one, _root_.mul_eq_zero, pow_eq_zero_iff',
+            neg_eq_zero, one_ne_zero, false_and, Int.cast_eq_zero, false_or]
+          exact (not_iff_not.2 dvd_iff_isRoot).1
+            (exists_eq_pow_rootMultiplicity_mul_and_not_dvd p h 1).choose_spec.2
+        · exact Nat.factorial_ne_zero _
 
 end Polynomial
