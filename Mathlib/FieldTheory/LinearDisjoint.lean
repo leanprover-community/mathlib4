a6a6d6201596b38dc16ceec4c42bede6245d34e2
acmepjz <acme_pjz@hotmail.com>
2024-11-27 01:10:35 +0800

    apply suggestions

--- Mathlib/FieldTheory/LinearDisjoint.lean
+++ Mathlib/FieldTheory/LinearDisjoint.lean
@@ -29,7 +29,7 @@ See the file `Mathlib/LinearAlgebra/LinearDisjoint.lean` for details.
 
 ### Main results
 
-Equivalent characterization of linearly disjointness:
+Equivalent characterization of linear disjointness:
 
 - `IntermediateField.LinearDisjoint.linearIndependent_left`:
   if `A` and `L` are linearly disjoint, then any `F`-linearly independent family on `A` remains
@@ -65,7 +65,7 @@ Equivalent characterization of linearly disjointness:
 Other main results:
 
 - `IntermediateField.LinearDisjoint.symm`, `IntermediateField.linearDisjoint_symm`:
-  linearly disjoint is symmetric.
+  linear disjointness is symmetric.
 
 - `IntermediateField.LinearDisjoint.rank_sup_of_isAlgebraic`,
   `IntermediateField.LinearDisjoint.finrank_sup`:
@@ -152,19 +152,18 @@ theorem linearDisjoint_iff' :
   congr!
   ext; simp
 
-/-- Linearly disjoint is symmetric. -/
+/-- Linear disjointness is symmetric. -/
 theorem LinearDisjoint.symm (H : A.LinearDisjoint B) : B.LinearDisjoint A :=
   linearDisjoint_iff'.2 (linearDisjoint_iff'.1 H).symm
 
-/-- Linearly disjoint is symmetric. -/
-theorem linearDisjoint_symm : A.LinearDisjoint B ↔ B.LinearDisjoint A :=
+/-- Linear disjointness is symmetric. -/
+theorem linearDisjoint_comm : A.LinearDisjoint B ↔ B.LinearDisjoint A :=
   ⟨LinearDisjoint.symm, LinearDisjoint.symm⟩
 
 namespace LinearDisjoint
 
 variable (A) in
-theorem self_right : A.LinearDisjoint F :=
-  Subalgebra.LinearDisjoint.bot_right _
+theorem self_right : A.LinearDisjoint F := Subalgebra.LinearDisjoint.bot_right _
 
 variable (A) in
 theorem bot_right : A.LinearDisjoint (⊥ : IntermediateField F E) :=
@@ -177,8 +176,7 @@ theorem bot_left : (⊥ : IntermediateField F E).LinearDisjoint L :=
 /-- If `A` and `L` are linearly disjoint, then any `F`-linearly independent family on `A` remains
 linearly independent over `L`. -/
 theorem linearIndependent_left (H : A.LinearDisjoint L)
-    {ι : Type*} {a : ι → A} (ha : LinearIndependent F a) :
-    LinearIndependent L (A.val ∘ a) :=
+    {ι : Type*} {a : ι → A} (ha : LinearIndependent F a) : LinearIndependent L (A.val ∘ a) :=
   (Subalgebra.LinearDisjoint.linearIndependent_left_of_flat H ha).map_of_injective_injective
     (AlgEquiv.ofInjectiveField (IsScalarTower.toAlgHom F L E)) (AddMonoidHom.id E)
     (by simp) (by simp) (fun _ _ ↦ by simp_rw [Algebra.smul_def]; rfl)
@@ -186,64 +184,53 @@ theorem linearIndependent_left (H : A.LinearDisjoint L)
 /-- If there exists an `F`-basis of `A` which remains linearly independent over `L`, then
 `A` and `L` are linearly disjoint. -/
 theorem of_basis_left {ι : Type*} (a : Basis ι F A)
-    (H : LinearIndependent L (A.val ∘ a)) :
-    A.LinearDisjoint L := by
-  rw [linearDisjoint_iff]
-  refine Subalgebra.LinearDisjoint.of_basis_left _ _ a ?_
-  exact H.map_of_surjective_injective
+    (H : LinearIndependent L (A.val ∘ a)) : A.LinearDisjoint L :=
+  Subalgebra.LinearDisjoint.of_basis_left _ _ a <| H.map_of_surjective_injective
     (AlgEquiv.ofInjectiveField (IsScalarTower.toAlgHom F L E)) (AddMonoidHom.id E)
     (AlgEquiv.surjective _) (by simp) (fun _ _ ↦ by simp_rw [Algebra.smul_def]; rfl)
 
 /-- If `A` and `B` are linearly disjoint, then any `F`-linearly independent family on `B` remains
 linearly independent over `A`. -/
 theorem linearIndependent_right (H : A.LinearDisjoint B)
-    {ι : Type*} {b : ι → B} (hb : LinearIndependent F b) :
-    LinearIndependent A (B.val ∘ b) :=
+    {ι : Type*} {b : ι → B} (hb : LinearIndependent F b) : LinearIndependent A (B.val ∘ b) :=
   (linearDisjoint_iff'.1 H).linearIndependent_right_of_flat hb
 
 /-- If there exists an `F`-basis of `B` which remains linearly independent over `A`, then
 `A` and `B` are linearly disjoint. -/
 theorem of_basis_right {ι : Type*} (b : Basis ι F B)
-    (H : LinearIndependent A (B.val ∘ b)) :
-    A.LinearDisjoint B :=
+    (H : LinearIndependent A (B.val ∘ b)) : A.LinearDisjoint B :=
   linearDisjoint_iff'.2 (.of_basis_right _ _ b H)
 
 /-- If `A` and `L` are linearly disjoint, then any `F`-linearly independent family on `L` remains
 linearly independent over `A`. -/
-theorem linearIndependent_right' (H : A.LinearDisjoint L)
-    {ι : Type*} {b : ι → L} (hb : LinearIndependent F b) :
-    LinearIndependent A ((algebraMap L E) ∘ b) := by
-  have := Subalgebra.LinearDisjoint.linearIndependent_right_of_flat H <| hb.map' _
+theorem linearIndependent_right' (H : A.LinearDisjoint L) {ι : Type*} {b : ι → L}
+    (hb : LinearIndependent F b) : LinearIndependent A (algebraMap L E ∘ b) := by
+  apply Subalgebra.LinearDisjoint.linearIndependent_right_of_flat H <| hb.map' _
     (AlgEquiv.ofInjectiveField (IsScalarTower.toAlgHom F L E)).toLinearEquiv.ker
-  exact this
 
 /-- If there exists an `F`-basis of `L` which remains linearly independent over `A`, then
 `A` and `L` are linearly disjoint. -/
 theorem of_basis_right' {ι : Type*} (b : Basis ι F L)
-    (H : LinearIndependent A ((algebraMap L E) ∘ b)) :
-    A.LinearDisjoint L := by
-  rw [linearDisjoint_iff]
-  exact .of_basis_right _ _
+    (H : LinearIndependent A (algebraMap L E ∘ b)) : A.LinearDisjoint L :=
+  Subalgebra.LinearDisjoint.of_basis_right _ _
     (b.map (AlgEquiv.ofInjectiveField (IsScalarTower.toAlgHom F L E)).toLinearEquiv) H
 
 /-- If `A` and `B` are linearly disjoint, then for any `F`-linearly independent families
 `{ u_i }`, `{ v_j }` of `A`, `B`, the products `{ u_i * v_j }`
 are linearly independent over `F`. -/
-theorem linearIndependent_mul (H : A.LinearDisjoint B)
-    {κ ι : Type*} {a : κ → A} {b : ι → B} (ha : LinearIndependent F a)
-    (hb : LinearIndependent F b) : LinearIndependent F fun (i : κ × ι) ↦ (a i.1).1 * (b i.2).1 :=
+theorem linearIndependent_mul (H : A.LinearDisjoint B) {κ ι : Type*} {a : κ → A} {b : ι → B}
+    (ha : LinearIndependent F a) (hb : LinearIndependent F b) :
+    LinearIndependent F fun (i : κ × ι) ↦ (a i.1).1 * (b i.2).1 :=
   (linearDisjoint_iff'.1 H).linearIndependent_mul_of_flat_left ha hb
 
 /-- If `A` and `L` are linearly disjoint, then for any `F`-linearly independent families
 `{ u_i }`, `{ v_j }` of `A`, `L`, the products `{ u_i * v_j }`
 are linearly independent over `F`. -/
-theorem linearIndependent_mul' (H : A.LinearDisjoint L)
-    {κ ι : Type*} {a : κ → A} {b : ι → L} (ha : LinearIndependent F a)
-    (hb : LinearIndependent F b) :
+theorem linearIndependent_mul' (H : A.LinearDisjoint L) {κ ι : Type*} {a : κ → A} {b : ι → L}
+    (ha : LinearIndependent F a) (hb : LinearIndependent F b) :
     LinearIndependent F fun (i : κ × ι) ↦ (a i.1).1 * algebraMap L E (b i.2) := by
-  have := Subalgebra.LinearDisjoint.linearIndependent_mul_of_flat_left H ha <| hb.map' _
+  apply Subalgebra.LinearDisjoint.linearIndependent_mul_of_flat_left H ha <| hb.map' _
     (AlgEquiv.ofInjectiveField (IsScalarTower.toAlgHom F L E)).toLinearEquiv.ker
-  exact this
 
 /-- If there are `F`-bases `{ u_i }`, `{ v_j }` of `A`, `B`, such that the products
 `{ u_i * v_j }` are linearly independent over `F`, then `A` and `B` are linearly disjoint. -/
@@ -255,9 +242,8 @@ theorem of_basis_mul {κ ι : Type*} (a : Basis κ F A) (b : Basis ι F B)
 `{ u_i * v_j }` are linearly independent over `F`, then `A` and `L` are linearly disjoint. -/
 theorem of_basis_mul' {κ ι : Type*} (a : Basis κ F A) (b : Basis ι F L)
     (H : LinearIndependent F fun (i : κ × ι) ↦ (a i.1).1 * algebraMap L E (b i.2)) :
-    A.LinearDisjoint L := by
-  rw [linearDisjoint_iff]
-  exact .of_basis_mul _ _ a
+    A.LinearDisjoint L :=
+  Subalgebra.LinearDisjoint.of_basis_mul _ _ a
     (b.map (AlgEquiv.ofInjectiveField (IsScalarTower.toAlgHom F L E)).toLinearEquiv) H
 
 theorem of_le_left {A' : IntermediateField F E} (H : A.LinearDisjoint L)
@@ -274,8 +260,7 @@ theorem of_le_right' (H : A.LinearDisjoint L) (L' : Type*) [Field L']
     [Algebra L' E] [IsScalarTower F L' E] [IsScalarTower L' L E] : A.LinearDisjoint L' := by
   refine Subalgebra.LinearDisjoint.of_le_right_of_flat H ?_
   convert AlgHom.range_comp_le_range (IsScalarTower.toAlgHom F L' L) (IsScalarTower.toAlgHom F L E)
-  ext x
-  exact IsScalarTower.algebraMap_apply L' L E x
+  ext; exact IsScalarTower.algebraMap_apply L' L E _
 
 /-- If `A` and `B` are linearly disjoint, `A'` and `B'` are contained in `A` and `B`,
 respectively, then `A'` and `B'` are also linearly disjoint. -/
@@ -296,23 +281,22 @@ theorem inf_eq_bot (H : A.LinearDisjoint B) :
 
 /-- If `A` and `A` itself are linearly disjoint over `F`, then it is equal to `F`. -/
 theorem eq_bot_of_self (H : A.LinearDisjoint A) : A = ⊥ :=
-  inf_of_le_left (le_refl A) ▸ H.inf_eq_bot
+  inf_idem A ▸ H.inf_eq_bot
 
 /-- If `A` and `B` are linearly disjoint over `F`, and at least one them are algebraic, then the
 rank of `A ⊔ B` is equal to the product of that of `A` and `B`. Note that this result is
 also true without algebraic assumption, but the proof becomes very complicated. -/
 theorem rank_sup_of_isAlgebraic (H : A.LinearDisjoint B)
     (halg : Algebra.IsAlgebraic F A ∨ Algebra.IsAlgebraic F B) :
-    Module.rank F ↥(A ⊔ B) = Module.rank F A * Module.rank F B := by
+    Module.rank F ↥(A ⊔ B) = Module.rank F A * Module.rank F B :=
   have h := le_sup_toSubalgebra A B
-  exact (rank_sup_le_of_isAlgebraic A B halg).antisymm <|
+  (rank_sup_le_of_isAlgebraic A B halg).antisymm <|
     (linearDisjoint_iff'.1 H).rank_sup_of_free.ge.trans <|
       (Subalgebra.inclusion h).toLinearMap.rank_le_of_injective (Subalgebra.inclusion_injective h)
 
 /-- If `A` and `B` are linearly disjoint over `F`, then the `Module.finrank` of
 `A ⊔ B` is equal to the product of that of `A` and `B`. -/
-theorem finrank_sup (H : A.LinearDisjoint B) :
-    finrank F ↥(A ⊔ B) = finrank F A * finrank F B := by
+theorem finrank_sup (H : A.LinearDisjoint B) : finrank F ↥(A ⊔ B) = finrank F A * finrank F B := by
   by_cases h : FiniteDimensional F A
   · simpa only [map_mul] using
       congr(Cardinal.toNat $(H.rank_sup_of_isAlgebraic (.inl inferInstance)))
@@ -325,22 +309,17 @@ theorem finrank_sup (H : A.LinearDisjoint B) :
 /-- If `A` and `B` are finite extensions of `F`,
 such that rank of `A ⊔ B` is equal to the product of the rank of `A` and `B`,
 then `A` and `B` are linearly disjoint. -/
-theorem of_finrank_sup
-    [FiniteDimensional F A] [FiniteDimensional F B]
-    (H : finrank F ↥(A ⊔ B) = finrank F A * finrank F B) :
-    A.LinearDisjoint B := by
-  rw [linearDisjoint_iff']
-  exact .of_finrank_sup_of_free (by rwa [← sup_toSubalgebra_of_left])
+theorem of_finrank_sup [FiniteDimensional F A] [FiniteDimensional F B]
+    (H : finrank F ↥(A ⊔ B) = finrank F A * finrank F B) : A.LinearDisjoint B :=
+  linearDisjoint_iff'.2 <| .of_finrank_sup_of_free (by rwa [← sup_toSubalgebra_of_left])
 
 /-- If `A` and `L` have coprime degree over `F`, then they are linearly disjoint. -/
-theorem of_finrank_coprime (H : (finrank F A).Coprime (finrank F L)) :
-    A.LinearDisjoint L := by
-  rw [linearDisjoint_iff]
+theorem of_finrank_coprime (H : (finrank F A).Coprime (finrank F L)) : A.LinearDisjoint L :=
   letI : Field (AlgHom.range (IsScalarTower.toAlgHom F L E)) :=
     inferInstanceAs <| Field (AlgHom.fieldRange (IsScalarTower.toAlgHom F L E))
   letI : Field A.toSubalgebra := inferInstanceAs <| Field A
-  refine .of_finrank_coprime_of_free ?_
-  rwa [(AlgEquiv.ofInjectiveField (IsScalarTower.toAlgHom F L E)).toLinearEquiv.finrank_eq] at H
+  Subalgebra.LinearDisjoint.of_finrank_coprime_of_free <| by
+    rwa [(AlgEquiv.ofInjectiveField (IsScalarTower.toAlgHom F L E)).toLinearEquiv.finrank_eq] at H
 
 end LinearDisjoint
 
@@ -354,8 +333,6 @@ variable (K : Type w) [Field K] [Algebra F K]
 
 /-- Two abstract fields `E` and `K` over `F` are called linearly disjoint, if their
 tensor product over `F` is a field. -/
-@[mk_iff]
-protected structure LinearDisjoint : Prop where
-  is_field : IsField (E ⊗[F] K)
+protected abbrev LinearDisjoint := IsField (E ⊗[F] K)
 
 end Field
