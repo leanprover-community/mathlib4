61bf6784372ebf80f31b02ec3fba0c9fc948766a
Matthew Ballard <matt@mrb.email>
2024-11-26 12:03:42 -0500

    remove whitespace

--- lakefile.lean
+++ lakefile.lean
@@ -7,7 +7,6 @@ open Lake DSL
 ## Mathlib dependencies on upstream projects
 -/
 
-
 require "leanprover-community" / "batteries" @ git "main"
 require "leanprover-community" / "Qq" @ git "master"
 require "leanprover-community" / "aesop" @ git "master"
