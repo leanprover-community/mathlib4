240b39aea45fe9781c8b03446ee16c515f5b3459
Kim Morrison <kim@tqft.net>
2024-11-26 13:21:44 +1100

    lint

--- Mathlib/Deprecated/Order.lean
+++ Mathlib/Deprecated/Order.lean
@@ -5,6 +5,12 @@ Authors: Leonardo de Moura, Jeremy Avigad, Floris van Doorn
 -/
 import Mathlib.Order.Defs
 
+/-!
+# Deprecated instances about order structures.
+-/
+
+variable {α : Type*}
+
 @[deprecated (since := "2024-11-26")] -- unused in Mathlib
 instance isStrictTotalOrder_of_linearOrder [LinearOrder α] : IsStrictTotalOrder α (· < ·) where
   irrefl := lt_irrefl
