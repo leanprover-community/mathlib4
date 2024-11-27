00200113ae7e6500fe14d089684b57daef691d0e
Johan Commelin <johan@commelin.net>
2024-11-27 09:56:44 +0000

    chore(GroupTheory/FreeGroup): split off `Reduce.lean` (#19501)

--- Mathlib.lean
+++ Mathlib.lean
@@ -3115,6 +3115,7 @@ import Mathlib.GroupTheory.FreeAbelianGroupFinsupp
 import Mathlib.GroupTheory.FreeGroup.Basic
 import Mathlib.GroupTheory.FreeGroup.IsFreeGroup
 import Mathlib.GroupTheory.FreeGroup.NielsenSchreier
+import Mathlib.GroupTheory.FreeGroup.Reduce
 import Mathlib.GroupTheory.GroupAction.Basic
 import Mathlib.GroupTheory.GroupAction.Blocks
 import Mathlib.GroupTheory.GroupAction.CardCommute
