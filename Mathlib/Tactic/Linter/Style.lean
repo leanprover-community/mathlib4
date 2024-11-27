45179d8554b5842db54e047d99c5edaea0b11d95
Michael Rothgang <rothgang@math.uni-bonn.de>
2024-11-27 22:41:48 +0100

    perf: disable five small syntax style linters
    DO NOT MERGE: just to see what the performance effect is
    does not touch the longLine or longFile linters, a benchmarking including them
    was conducted in #19551.

--- Mathlib/Tactic/Linter/Style.lean
+++ Mathlib/Tactic/Linter/Style.lean
@@ -86,7 +86,7 @@ def setOptionLinter : Linter where run := withSetOptionIn fun stx => do
                If you intend to submit this contribution to the Mathlib project, \
                please remove 'set_option {name}'."
 
-initialize addLinter setOptionLinter
+--initialize addLinter setOptionLinter
 
 end Style.setOption
 
@@ -129,7 +129,7 @@ def missingEndLinter : Linter where run := withSetOptionIn fun stx ↦ do
         Linter.logLint linter.style.missingEnd stx
          m!"unclosed sections or namespaces; expected: '{ending}'"
 
-initialize addLinter missingEndLinter
+--initialize addLinter missingEndLinter
 
 end Style.missingEnd
 
@@ -197,7 +197,7 @@ def cdotLinter : Linter where run := withSetOptionIn fun stx ↦ do
             m!"This central dot `·` is isolated; please merge it with the next line."
       | _ => return
 
-initialize addLinter cdotLinter
+--initialize addLinter cdotLinter
 
 end Style
 
@@ -237,7 +237,7 @@ def dollarSyntaxLinter : Linter where run := withSetOptionIn fun stx ↦ do
       Linter.logLint linter.style.dollarSyntax s
         m!"Please use '<|' instead of '$' for the pipe operator."
 
-initialize addLinter dollarSyntaxLinter
+--initialize addLinter dollarSyntaxLinter
 
 end Style.dollarSyntax
 
@@ -283,7 +283,7 @@ def lambdaSyntaxLinter : Linter where run := withSetOptionIn fun stx ↦ do
         Please use 'fun' and not 'λ' to define anonymous functions.\n\
         The 'λ' syntax is deprecated in mathlib4."
 
-initialize addLinter lambdaSyntaxLinter
+--initialize addLinter lambdaSyntaxLinter
 
 end Style.lambdaSyntax
 
