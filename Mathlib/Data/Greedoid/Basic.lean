38e95ffc6ee84c95cf21aa142f549ba69b474bb9
qawbecrdtey <qawbecrdtey@kaist.ac.kr>
2024-11-28 01:33:11 +0900

    reduced proof length.

--- Mathlib/Data/Greedoid/Basic.lean
+++ Mathlib/Data/Greedoid/Basic.lean
@@ -5,6 +5,7 @@ Authors: Jihoon Hyun
 -/
 import Mathlib.Data.Greedoid.Accessible
 import Mathlib.Data.Greedoid.Exchange
+-- import Mathlib.Order.Defs.PartialOrder
 
 /-!
 # Greedoid
@@ -20,7 +21,7 @@ exchange and accessible properties.
 structure Greedoid (α : Type*) where
   /-- The ground set which every element lies on. -/
   ground : Finset α
-  /-- The feasible set of the greedoid. -/
+  /-- The feasible property of the greedoid. -/
   Feasible : Finset α → Prop
   feasible_empty : Feasible ∅
   exchange_property : Greedoid.ExchangeProperty Feasible
@@ -29,13 +30,6 @@ structure Greedoid (α : Type*) where
 section Greedoid
 
 variable {α : Type*}
-
-/-- Definition of `Finset` in `Greedoid`.
-    This is often called 'feasible'· -/
-protected def Greedoid.mem (G : Greedoid α) (s : Finset α) := G.Feasible s
-
-instance : Membership (Finset α) (Greedoid α) :=
-  ⟨Greedoid.mem⟩
 
 end Greedoid
 
@@ -61,68 +55,33 @@ theorem feasible_inj {G₁ G₂ : Greedoid α} :
 
 variable {G : Greedoid α}
 variable {s : Finset α}
-variable {s₁ : Finset α} (hs₁ : s₁ ∈ G)
-variable {s₂ : Finset α} (hs₂ : s₂ ∈ G)
+variable {s₁ : Finset α} (hs₁ : G.Feasible s₁)
+variable {s₂ : Finset α} (hs₂ : G.Feasible s₂)
 
 protected theorem accessible_property :
     AccessibleProperty G.Feasible := by
-  intro s hs₁ hs₂
-  by_contra h'; simp at h'
-  let F : Set (Finset α) :=
-    {t | G.Feasible t ∧ t.card < s.card}
-  have hF : ∅ ∈ F := by simp [F, G.feasible_empty, hs₂]
-  let F' : Finset α → Prop := fun t ↦
-    t ∈ F ∧ ∀ t', G.Feasible t' → t'.card < s.card → t'.card ≤ t.card
-  have hF' : ∃ x, F' x := by
-    by_contra h''; simp [F', F] at h''
-    have h₁ : ∀ n, ∃ t, t ∈ F ∧ t.card = n := by
+  intro s h₁ h₂; by_contra h'; simp only [not_exists, not_and] at h'
+  have ⟨u, hu₁, hu₂⟩ : ∃ t : Finset α, #t < #s ∧ ∀ t', G.Feasible t' → #t' < #s → #t' ≤ #t := by
+    by_contra h''; simp only [not_exists, not_and, not_forall, not_le] at h''
+    have h₁ : ∀ n, ∃ t, G.Feasible t ∧ #t < #s ∧ #t = n := by
       intro n; induction n with
-      | zero => use ∅; simp [hF]
+      | zero => exact ⟨∅, G.feasible_empty, card_pos.mpr h₂, rfl⟩
       | succ n ih =>
-        rcases ih with ⟨t, ht₁, ht₂⟩
-        rcases ht₁ with ⟨ht₁, ht₃⟩
-        rcases h'' t ht₁ ht₃ with ⟨u, hu₁, hu₂, hu₃⟩
-        rcases G.exchange_property hu₁ ht₁ hu₃ with ⟨_, _, hx₁, hx₂⟩
-        use t.cons _ hx₁
-        simp [F, ht₂, hx₂]; omega
-    rcases h₁ s.card with ⟨t, ht₁, ht₂⟩
-    simp [F, ht₂] at ht₁
-  rcases hF' with ⟨u, hu₁, hu₂⟩
-  rcases hu₁ with ⟨_, hu₁⟩
+        rcases ih with ⟨t, ht₁, ht₂, _⟩
+        rcases h'' t ht₂ with ⟨_, hu₁, _, hu₂⟩
+        rcases G.exchange_property hu₁ ht₁ hu₂ with ⟨_, _, h₁, h₂⟩
+        exact ⟨t.cons _ h₁, h₂, card_cons _ ▸ by omega, card_cons _ ▸ by omega⟩
+    rcases h₁ #s with ⟨_, _, _⟩; omega
   rcases ExchangeProperty.exists_superset_of_card_le
-    G.exchange_property hs₁ G.feasible_empty (le_of_lt hu₁) (zero_le _)
+    G.exchange_property h₁ G.feasible_empty (le_of_lt hu₁) (zero_le _)
     with ⟨t, ht₁, _, ht₂, ht₃⟩
-  simp at ht₂
-  rw [← ht₃] at hu₁
-  rcases G.exchange_property hs₁ ht₁ hu₁ with ⟨_, _, hx₁, hx₂⟩
-  have h : (t.cons _ hx₁).card < s.card := by
-    by_contra h''; simp at h''; apply h' _ ht₂ _ ht₁; omega
+  simp only [not_mem_empty, or_false] at ht₂; rw [← ht₃] at hu₁
+  rcases G.exchange_property h₁ ht₁ hu₁ with ⟨_, _, hx₁, hx₂⟩
+  have h : #(t.cons _ hx₁) < #s := by by_contra h''; simp at h''; apply h' _ ht₂ _ ht₁; omega
   have := card_cons _ ▸ ht₃ ▸ hu₂ _ hx₂ h; omega
 
 instance : Accessible G.Feasible :=
   ⟨G.accessible_property⟩
-
-section Membership
-
-@[simp]
-theorem system_feasible_mem_mem : G.Feasible s ↔ s ∈ G := by
-  rfl
-
-theorem mem_accessible
-    (hs₁ : s ∈ G) (hs₂ : s.Nonempty) :
-    ∃ t, t ⊆ s ∧ t.card + 1 = s.card ∧ t ∈ G :=
-  G.accessible_property hs₁ hs₂
-
-theorem mem_exchange
-    (hs₁ : G.Feasible s₁) (hs₂ : G.Feasible s₂) (hs : s₂.card < s₁.card) :
-    ∃ x ∈ s₁, ∃ h : x ∉ s₂, cons x s₂ h ∈ G :=
-  G.exchange_property hs₁ hs₂ hs
-
-end Membership
-
-@[simp]
-theorem empty_mem : ∅ ∈ G :=
-  G.feasible_empty
 
 end Greedoid
 
