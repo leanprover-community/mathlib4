f7599845b841a8fc2f50cef240b056b15fd5d667
Andrew Yang <the.erd.one@gmail.com>
2024-11-28 08:48:40 +0000

    feat(AlgebraicGeometry): The small affine Zariski site

--- Mathlib.lean
+++ Mathlib.lean
@@ -1002,6 +1002,7 @@ import Mathlib.AlgebraicGeometry.Scheme
 import Mathlib.AlgebraicGeometry.Sites.BigZariski
 import Mathlib.AlgebraicGeometry.Sites.Etale
 import Mathlib.AlgebraicGeometry.Sites.MorphismProperty
+import Mathlib.AlgebraicGeometry.Sites.SmallAffineZariski
 import Mathlib.AlgebraicGeometry.Spec
 import Mathlib.AlgebraicGeometry.SpreadingOut
 import Mathlib.AlgebraicGeometry.Stalk
