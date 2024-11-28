f2d6e73825651fb8f00dab4dc930844bb64e821c
Violeta Hernández <vi.hdz.p@gmail.com>
2024-11-28 14:47:00 -0600

    finish

--- Mathlib/Order/Incomparable.lean
+++ Mathlib/Order/Incomparable.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Violeta Hernández Palacios
 -/
 import Mathlib.Order.Antisymmetrization
+import Mathlib.Tactic.Tauto
 
 /-!
 # Incomparability relation
@@ -18,7 +19,7 @@ Two values in a preorder are incomparable whenever `¬ a ≤ b` and `¬ b ≤ a`
 
 open Function OrderDual
 
-variable {α : Type*} {a b c : α}
+variable {α : Type*} {a b c d : α}
 
 section Relation
 
@@ -35,8 +36,7 @@ theorem incompRel_iff {r} : IncompRel r a b ↔ ¬ r a b ∧ ¬ r b a := Iff.rfl
 
 theorem IncompRel.not_le [LE α] (h : a ‖ b) : ¬ a ≤ b := h.1
 theorem IncompRel.not_ge [LE α] (h : a ‖ b) : ¬ b ≤ a := h.2
-theorem IncompRel.not_lt [Preorder α] (h : a ‖ b) : ¬ a < b := mt le_of_lt h.not_le
-theorem IncompRel.not_gt [Preorder α] (h : a ‖ b) : ¬ b < a := mt le_of_lt h.not_ge
+theorem LE.le.not_incompRel [LE α] (h : a ≤ b) : ¬ a ‖ b := fun h' ↦ h'.not_le h
 
 @[simp]
 theorem antisymmRel_compl : AntisymmRel rᶜ = IncompRel r :=
@@ -53,14 +53,26 @@ theorem incompRel_swap : IncompRel (swap r) = IncompRel r :=
 theorem incompRel_refl [IsIrrefl α r] (a : α) : IncompRel r a a :=
   antisymmRel_refl _ _
 
+instance [IsIrrefl α r] : IsRefl α (IncompRel r) where
+  refl := incompRel_refl r
+
 variable {r}
 
 @[symm]
 theorem IncompRel.symm : IncompRel r a b → IncompRel r b a :=
   And.symm
 
+instance : IsSymm α (IncompRel r) where
+  symm _ _ := IncompRel.symm
+
 instance IncompRel.decidableRel [DecidableRel r] : DecidableRel (IncompRel r) :=
   fun _ _ ↦ inferInstanceAs (Decidable (¬ _ ∧ ¬ _))
+
+theorem IncompRel.not_antisymmRel (h : IncompRel r a b) : ¬ AntisymmRel r a b :=
+  fun h' ↦ h.1 h'.1
+
+theorem AntisymmRel.not_incompRel (h : AntisymmRel r a b) : ¬ IncompRel r a b :=
+  fun h' ↦ h'.1 h.1
 
 @[simp]
 theorem not_incompRel [IsTotal α r] : ¬ IncompRel r a b := by
@@ -68,3 +80,50 @@ theorem not_incompRel [IsTotal α r] : ¬ IncompRel r a b := by
   exact IsTotal.total a b
 
 end Relation
+
+section Preorder
+
+variable [Preorder α]
+
+theorem IncompRel.not_lt (h : a ‖ b) : ¬ a < b := mt le_of_lt h.not_le
+theorem IncompRel.not_gt (h : a ‖ b) : ¬ b < a := mt le_of_lt h.not_ge
+theorem LT.lt.not_incompRel (h : a < b) : ¬ a ‖ b := fun h' ↦ h'.not_lt h
+
+/-- Exactly one of the following is true. -/
+theorem lt_or_antisymmRel_or_gt_or_incompRel (a b : α) : a < b ∨ a ⋚ b ∨ b < a ∨ a ‖ b := by
+  simp_rw [lt_iff_le_not_le]
+  tauto
+
+@[trans]
+theorem incompRel_of_incompRel_of_antisymmRel (h₁ : a ‖ b) (h₂ : b ⋚ c) : a ‖ c :=
+  ⟨fun h ↦ h₁.not_le (h.trans h₂.ge), fun h ↦ h₁.not_ge (h₂.le.trans h)⟩
+
+alias IncompRel.trans_antisymmRel := incompRel_of_incompRel_of_antisymmRel
+
+instance : @Trans α α α (· ‖ ·) (· ⋚ ·) (· ‖ ·) where
+  trans := incompRel_of_incompRel_of_antisymmRel
+
+@[trans]
+theorem incompRel_of_antisymmRel_of_incompRel (h₁ : a ⋚ b) (h₂ : b ‖ c) : a ‖ c :=
+  (h₂.symm.trans_antisymmRel h₁.symm).symm
+
+alias AntisymmRel.trans_incompRel := incompRel_of_antisymmRel_of_incompRel
+
+instance : @Trans α α α (· ⋚ ·) (· ‖ ·) (· ‖ ·) where
+  trans := incompRel_of_antisymmRel_of_incompRel
+
+theorem AntisymmRel.incompRel_congr (h₁ : a ⋚ b) (h₂ : c ⋚ d) : a ‖ c ↔ b ‖ d where
+  mp h := (h₁.symm.trans_incompRel h).trans_antisymmRel h₂
+  mpr h := (h₁.trans_incompRel h).trans_antisymmRel h₂.symm
+
+theorem AntisymmRel.incompRel_congr_left (h : a ⋚ b) : a ‖ c ↔ b ‖ c :=
+  h.incompRel_congr (antisymmRel_refl _ c)
+
+theorem AntisymmRel.incompRel_congr_right (h : b ⋚ c) : a ‖ b ↔ a ‖ c :=
+  (antisymmRel_refl _ a).incompRel_congr h
+
+end Preorder
+
+/-- Exactly one of the following is true. -/
+theorem lt_or_eq_or_gt_or_incompRel [PartialOrder α] (a b : α) : a < b ∨ a = b ∨ b < a ∨ a ‖ b := by
+  simpa using lt_or_antisymmRel_or_gt_or_incompRel a b
