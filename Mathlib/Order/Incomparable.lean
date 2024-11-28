35d27baeffe7756abc982808351ad1d5d746dc7c
Violeta Hernández <vi.hdz.p@gmail.com>
2024-11-28 11:49:03 -0600

    progress

--- Mathlib/Order/Incomparable.lean
+++ Mathlib/Order/Incomparable.lean
@@ -18,7 +18,7 @@ Two values in a preorder are incomparable whenever `¬ a ≤ b` and `¬ b ≤ a`
 
 open Function OrderDual
 
-variable {α : Type*}
+variable {α : Type*} {a b c : α}
 
 section Relation
 
@@ -27,6 +27,16 @@ variable (r : α → α → Prop)
 /-- The incomparability relation `IncompRel r a b` means `¬ r a b` and `¬ r b c`. -/
 def IncompRel : α → α → Prop :=
   AntisymmRel rᶜ
+
+/-- The incomparability relation `a ‖ b` means that both `¬ a ≤ b` and `¬ b ≤ a`. -/
+infix:50 " ‖ "  => IncompRel (· ≤ ·)
+
+theorem incompRel_iff {r} : IncompRel r a b ↔ ¬ r a b ∧ ¬ r b a := Iff.rfl
+
+theorem IncompRel.not_le [LE α] (h : a ‖ b) : ¬ a ≤ b := h.1
+theorem IncompRel.not_ge [LE α] (h : a ‖ b) : ¬ b ≤ a := h.2
+theorem IncompRel.not_lt [Preorder α] (h : a ‖ b) : ¬ a < b := mt le_of_lt h.not_le
+theorem IncompRel.not_gt [Preorder α] (h : a ‖ b) : ¬ b < a := mt le_of_lt h.not_ge
 
 @[simp]
 theorem antisymmRel_compl : AntisymmRel rᶜ = IncompRel r :=
@@ -46,21 +56,15 @@ theorem incompRel_refl [IsIrrefl α r] (a : α) : IncompRel r a a :=
 variable {r}
 
 @[symm]
-theorem IncompRel.symm {a b : α} : IncompRel r a b → IncompRel r b a :=
+theorem IncompRel.symm : IncompRel r a b → IncompRel r b a :=
   And.symm
 
-@[trans]
-theorem AntisymmRel.trans [IsTrans α r] {a b c : α} (hab : AntisymmRel r a b)
-    (hbc : AntisymmRel r b c) : AntisymmRel r a c :=
-  ⟨_root_.trans hab.1 hbc.1, _root_.trans hbc.2 hab.2⟩
-
-instance AntisymmRel.decidableRel [DecidableRel r] : DecidableRel (AntisymmRel r) := fun _ _ =>
-  instDecidableAnd
+instance IncompRel.decidableRel [DecidableRel r] : DecidableRel (IncompRel r) :=
+  fun _ _ ↦ inferInstanceAs (Decidable (¬ _ ∧ ¬ _))
 
 @[simp]
-theorem antisymmRel_iff_eq [IsRefl α r] [IsAntisymm α r] {a b : α} : AntisymmRel r a b ↔ a = b :=
-  antisymm_iff
-
-alias ⟨AntisymmRel.eq, _⟩ := antisymmRel_iff_eq
+theorem not_incompRel [IsTotal α r] : ¬ IncompRel r a b := by
+  rw [incompRel_iff, not_and_or, not_not, not_not]
+  exact IsTotal.total a b
 
 end Relation
