4e2fadc5e5a18974da8500af2d321e62503231ea
Pim Otte <p.j.otte@uu.nl>
2024-11-29 17:50:25 +0100

    Remove simp

--- Mathlib/Data/List/Nodup.lean
+++ Mathlib/Data/List/Nodup.lean
@@ -227,7 +227,6 @@ theorem Nodup.filter (p : α → Bool) {l} : Nodup l → Nodup (filter p l) := b
 theorem nodup_reverse {l : List α} : Nodup (reverse l) ↔ Nodup l :=
   pairwise_reverse.trans <| by simp only [Nodup, Ne, eq_comm]
 
-@[simp]
 lemma nodup_tail_reverse (l : List α) (h : l[0]? = l.getLast?) :
     Nodup l.reverse.tail ↔ Nodup l.tail := by
   induction l with
