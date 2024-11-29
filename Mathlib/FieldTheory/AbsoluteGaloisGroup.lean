f7f1ffbd186ce7dc0e200c7e2ea33921cc4fd64c
adamtopaz <github@adamtopaz.com>
2024-11-29 13:06:39 -0700

    fix

--- Mathlib/FieldTheory/AbsoluteGaloisGroup.lean
+++ Mathlib/FieldTheory/AbsoluteGaloisGroup.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: María Inés de Frutos-Fernández
 -/
 import Mathlib.FieldTheory.KrullTopology
-import Mathlib.FieldTheory.IsAlgClosed.AlgebraicClosure
+import Mathlib.FieldTheory.SeparableClosure
 import Mathlib.Topology.Algebra.Group.TopologicalAbelianization
 
 /-!
@@ -13,8 +13,8 @@ import Mathlib.Topology.Algebra.Group.TopologicalAbelianization
 We define the absolute Galois group of a field `K` and its topological abelianization.
 
 ## Main definitions
-- `Field.absoluteGaloisGroup` : The Galois group of the field extension `K^al/K`, where `K^al` is an
-  algebraic closure of `K`.
+- `Field.absoluteGaloisGroup` : The Galois group of the field extension `K^al/K`, where `K^sep` is
+  "the" separable closure of `K`.
 - `Field.absoluteGaloisGroupAbelianization` : The topological abelianization of
   `Field.absoluteGaloisGroup K`, that is, the quotient of `Field.absoluteGaloisGroup K` by the
   topological closure of its commutator subgroup.
@@ -24,7 +24,7 @@ We define the absolute Galois group of a field `K` and its topological abelianiz
   commutator of `absoluteGaloisGroup` is a normal subgroup.
 
 ## Tags
-field, algebraic closure, galois group, abelianization
+field, separable closure, Galois group, absolute Galois group, abelianization
 
 -/
 
@@ -36,14 +36,23 @@ variable (K : Type*) [Field K]
 
 /-- The absolute Galois group of `K`, defined as the Galois group of the field extension `K^al/K`,
   where `K^al` is an algebraic closure of `K`. -/
-def absoluteGaloisGroup := AlgebraicClosure K ≃ₐ[K] AlgebraicClosure K
+def absoluteGaloisGroup := SeparableClosure K ≃ₐ[K] SeparableClosure K
 
 local notation "G_K" => absoluteGaloisGroup
 
 noncomputable instance : Group (G_K K) := AlgEquiv.aut
 
 /-- `absoluteGaloisGroup` is a topological space with the Krull topology. -/
-noncomputable instance : TopologicalSpace (G_K K) := krullTopology K (AlgebraicClosure K)
+noncomputable instance : TopologicalSpace (G_K K) := krullTopology K (SeparableClosure K)
+
+/-- `absoluteGaloisGroup` is T2 with the Krull topology. -/
+instance : T2Space (G_K K) := krullTopology_t2
+
+/-- `absoluteGaloisGroup` is totally disconnected with the Krull topology. -/
+instance : TotallyDisconnectedSpace (G_K K) := krullTopology_totallyDisconnectedSpace
+
+/-- `absoluteGaloisGroup` is a compact space. -/
+proof_wanted instCompactSpaceAbsoluteGaloisGroup : CompactSpace (G_K K)
 
 /-! ### The topological abelianization of the absolute Galois group -/
 
