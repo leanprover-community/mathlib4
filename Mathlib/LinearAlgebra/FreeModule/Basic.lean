410427320ddcd93aa9093b39ed1d19a088240e98
Kim Morrison <kim@tqft.net>
2024-11-30 19:39:42 +1100

    shake

--- Mathlib/LinearAlgebra/FreeModule/Basic.lean
+++ Mathlib/LinearAlgebra/FreeModule/Basic.lean
@@ -5,9 +5,9 @@ Authors: Riccardo Brasca
 -/
 import Mathlib.Data.Finsupp.Fintype
 import Mathlib.Data.Matrix.Defs
-import Mathlib.Logic.Small.Set
 import Mathlib.LinearAlgebra.Basis.Basic
 import Mathlib.LinearAlgebra.TensorProduct.Basis
+import Mathlib.Logic.Small.Basic
 
 /-!
 # Free modules
