ec4796e0776351c45812172006a439ffc56c6979
Kim Morrison <kim@tqft.net>
2024-11-30 20:17:39 +1100

    shake, and more

--- Mathlib/Algebra/Algebra/Hom.lean
+++ Mathlib/Algebra/Algebra/Hom.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kenny Lau, Yury Kudryashov
 -/
 import Mathlib.Algebra.Algebra.Basic
-import Mathlib.Algebra.BigOperators.Finsupp
 
 /-!
 # Homomorphisms of `R`-algebras
