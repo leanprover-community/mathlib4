fcd66726559328402ee9306b52850ff4ba875e17
Daniel Weber <commandmaster2017@gmail.com>
2024-11-30 09:58:59 +0200

    Update `Mathlib.lean`

--- Mathlib.lean
+++ Mathlib.lean
@@ -4247,6 +4247,7 @@ import Mathlib.RingTheory.Derivation.Basic
 import Mathlib.RingTheory.Derivation.DifferentialRing
 import Mathlib.RingTheory.Derivation.Lie
 import Mathlib.RingTheory.Derivation.MapCoeffs
+import Mathlib.RingTheory.Derivation.Multiplicity
 import Mathlib.RingTheory.Derivation.ToSquareZero
 import Mathlib.RingTheory.DiscreteValuationRing.Basic
 import Mathlib.RingTheory.DiscreteValuationRing.TFAE
