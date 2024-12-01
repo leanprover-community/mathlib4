e163d4e43e27334e65387fde1b49315df7b21f8b
Óscar <bolito2hd@gmail.com>
2024-12-01 17:16:25 +0100

    added authorship

--- Mathlib/GroupTheory/Coxeter/Basic.lean
+++ Mathlib/GroupTheory/Coxeter/Basic.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2024 Newell Jensen. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Newell Jensen, Mitchell Lee
+Authors: Newell Jensen, Mitchell Lee, Óscar Álvarez
 -/
 import Mathlib.Algebra.Group.Subgroup.Pointwise
 import Mathlib.Algebra.Ring.Int.Parity
--- Mathlib/GroupTheory/Coxeter/Inversion.lean
+++ Mathlib/GroupTheory/Coxeter/Inversion.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2024 Mitchell Lee. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Mitchell Lee
+Authors: Mitchell Lee, Óscar Álvarez
 -/
 import Mathlib.GroupTheory.Coxeter.Length
 import Mathlib.Data.List.GetD
