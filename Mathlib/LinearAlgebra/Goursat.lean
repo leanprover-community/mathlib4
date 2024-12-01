19a86376b7ee389ed3f1acddaa1af4f0f3b743bb
David Loeffler <d.loeffler.01@cantab.net>
2024-12-02 00:08:21 +0100

    general version

--- Mathlib/LinearAlgebra/Goursat.lean
+++ Mathlib/LinearAlgebra/Goursat.lean
@@ -51,6 +51,7 @@ lemma goursatSnd_toAddSubgroup :
   ext x
   simp [mem_toAddSubgroup, goursatSnd, AddSubgroup.mem_goursatSnd]
 
+variable (I) in
 lemma goursatFst_prod_goursatSnd_le : I.goursatFst.prod I.goursatSnd ≤ I := by
   simpa only [← toAddSubgroup_le, goursatFst_toAddSubgroup, goursatSnd_toAddSubgroup]
     using I.toAddSubgroup.goursatFst_prod_goursatSnd_le
@@ -83,7 +84,51 @@ lemma goursat_surjective : ∃ e : (G ⧸ I.goursatFst) ≃ₗ[R] H ⧸ I.goursa
   rw [← toAddSubgroup_injective.eq_iff]
   convert he using 1
   ext v
-  rw [mem_toAddSubgroup, LinearMap.mem_graph_iff, Eq.comm]
+  rw [mem_toAddSubgroup, mem_graph_iff, Eq.comm]
   rfl
+
+/-- **Goursat's lemma** for an arbitrary submodule of a product.
+
+If `I` is a submodule of `G × H`, then there exist submodules `M ≤ G' ≤ G` and `N ≤ H' ≤ H` such
+that `I ≤ G' × H'`, and `I` is (the image in `G × H` of) the preimage of the graph of an `R`-linear
+isomorphism `G' ⧸ M ≃ H' ⧸ N`. -/
+lemma goursat : ∃ (G' : Submodule R G) (H' : Submodule R H) (M : Submodule R G')
+    (N : Submodule R H') (e : (G' ⧸ M) ≃ₗ[R] H' ⧸ N),
+    I = (e.graph.comap <| M.mkQ.prodMap N.mkQ).map (G'.subtype.prodMap H'.subtype) := by
+  let G' := I.map (LinearMap.fst ..)
+  let H' := I.map (LinearMap.snd ..)
+  let P : I →ₗ[R] G' := (LinearMap.fst ..).submoduleMap I
+  let Q : I →ₗ[R] H' := (LinearMap.snd ..).submoduleMap I
+  let I' : Submodule R (G' × H') := LinearMap.range (P.prod Q)
+  have hI₁' : Surjective (Prod.fst ∘ I'.subtype) := by
+    simp only [← coe_fst (R := R), ← coe_comp, ← range_eq_top, LinearMap.range_comp, range_subtype]
+    simpa only [I', ← LinearMap.range_comp, fst_prod, range_eq_top] using
+      (LinearMap.fst ..).submoduleMap_surjective I
+  have hI₂' : Surjective (Prod.snd ∘ I'.subtype) := by
+    simp only [← coe_snd (R := R), ← coe_comp, ← range_eq_top, LinearMap.range_comp, range_subtype]
+    simpa only [I', ← LinearMap.range_comp, snd_prod, range_eq_top] using
+      (LinearMap.snd ..).submoduleMap_surjective I
+  obtain ⟨e, he⟩ := goursat_surjective hI₁' hI₂'
+  use G', H', I'.goursatFst, I'.goursatSnd, e
+  rw [← he]
+  simp only [LinearMap.range_comp, Submodule.range_subtype, I']
+  rw [comap_map_eq_self]
+  · ext ⟨g, h⟩
+    constructor
+    · intro hgh
+      simp only [mem_map, LinearMap.mem_range, prod_apply, Subtype.exists, Prod.exists, coe_prodMap,
+        coe_subtype, Prod.map_apply, Prod.mk.injEq, exists_and_right, exists_eq_right_right,
+        exists_eq_right, G', H', fst_apply, snd_apply]
+      exact ⟨⟨h, hgh⟩, ⟨g, hgh⟩, ⟨g, h, hgh, rfl⟩⟩
+    · simp only [mem_map, LinearMap.mem_range, prod_apply, Subtype.exists, Prod.exists,
+        coe_prodMap, coe_subtype, Prod.map_apply, Prod.mk.injEq, exists_and_right,
+        exists_eq_right_right, exists_eq_right, forall_exists_index, Pi.prod]
+      rintro hg hh g₁ h₁ hg₁h₁ ⟨hP, hQ⟩
+      simp only [Subtype.ext_iff] at hP hQ
+      rwa [← hP, ← hQ]
+  · convert goursatFst_prod_goursatSnd_le (range <| P.prod Q)
+    ext ⟨g, h⟩
+    simp_rw [mem_ker, coe_prodMap, Prod.map_apply, Submodule.mem_prod, Prod.zero_eq_mk,
+      Prod.ext_iff, ← mem_ker, ker_mkQ]
 
 end Submodule
