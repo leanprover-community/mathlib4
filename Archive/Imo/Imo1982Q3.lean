d70a44066568a6ddd1af9a6eb9e41c8ba0af9f14
Violeta Hernández <vi.hdz.p@gmail.com>
2024-12-02 13:57:05 -0600

    Update Imo1982Q3.lean

--- Archive/Imo/Imo1982Q3.lean
+++ Archive/Imo/Imo1982Q3.lean
@@ -10,7 +10,7 @@ import Mathlib.Data.NNReal.Basic
 /-!
 # IMO 1982 Q3
 
-Consider infinite sequences $\{x_n\}$ of positive reals such that $x_0 = 0$ and
+Consider infinite sequences $\{x_n\}$ of positive reals such that $x_0 = 1$ and
 $x_0 \ge x_1 \ge x_2 \ge \ldots$
 
 a) Prove that for every such sequence there is an $n \ge 1$ such that:
@@ -36,55 +36,50 @@ namespace Imo1982Q3
 include hn hx
 
 /-- `x (n + 1)` is at most the average of `x 1`, `x 2`, ..., `x n`. -/
-lemma ineq₁ : ∑ k ∈ range (n + 1), x k ≤ (∑ k ∈ range n, x k) * (1 + 1 / n) := by
+lemma le_avg : ∑ k ∈ range (n + 1), x k ≤ (∑ k ∈ range n, x k) * (1 + 1 / n) := by
   rw [sum_range_succ, mul_one_add, add_le_add_iff_left, mul_one_div,
     le_div_iff₀ (mod_cast hn.bot_lt), mul_comm, ← nsmul_eq_mul]
   conv_lhs => rw [← card_range n, ← sum_const]
   refine sum_le_sum fun k hk ↦ hx (le_of_lt ?_)
   simpa using hk
 
-variable (h0 : x 0 = 1) (hp : ∀ k, 0 < x k)
-include h0 hp
-
-/-- We combine Sedrakyan and the previous inequality. -/
-lemma ineq₂ : (∑ k ∈ range n, x (k + 1) + 1) ^ 2 / ((∑ k ∈ range n, x (k + 1)) * (1 + 1 / n))
-    ≤ ∑ k ∈ range (n + 1), x k ^ 2 / x (k + 1) := by
-  apply le_trans _ (sq_sum_div_le_sum_sq_div _ x (fun k _ ↦ hp (k + 1)))
-  gcongr
-  · exact sum_pos (fun k _ ↦ hp _) nonempty_range_succ
-  · exact add_nonneg (sum_nonneg fun k _ ↦ (hp _).le) zero_le_one
-  · rw [sum_range_succ', h0]
-  · exact ineq₁ hn (hx.comp_monotone @Nat.succ_le_succ)
-
-/-- We move `1 + 1 / n` out of the fraction. -/
-lemma ineq₃ : (∑ k ∈ range n, x (k + 1) + 1) ^ 2 / (∑ k ∈ range n, x (k + 1)) * n / (n + 1)
-    ≤ ∑ k ∈ range (n + 1), x k ^ 2 / x (k + 1) := by
-  convert ineq₂ hn hx h0 hp using 1
-  field_simp
-
-/-- Finally, AM-GM gives us the main lower bound for the RHS. -/
-lemma ineq₄ : 4 * n / (n + 1) ≤ ∑ k ∈ range (n + 1), x k ^ 2 / x (k + 1) := by
-  apply le_trans _ (ineq₃ hn hx h0 hp)
-  gcongr
-  rw [le_div_iff₀]
-  · simpa using four_mul_le_sq_add (∑ k ∈ range n, x (k + 1)) 1
-  · exact sum_pos (fun k _ ↦ hp _) (nonempty_range_iff.2 hn)
+/-- The main inequality used for part a. -/
+lemma ineq (h0 : x 0 = 1) (hp : ∀ k, 0 < x k) :
+    4 * n / (n + 1) ≤ ∑ k ∈ range (n + 1), x k ^ 2 / x (k + 1) := by
+  calc
+    -- We first use AM-GM.
+    _ ≤ (∑ k ∈ range n, x (k + 1) + 1) ^ 2 / (∑ k ∈ range n, x (k + 1)) * n / (n + 1) := by
+      gcongr
+      rw [le_div_iff₀]
+      · simpa using four_mul_le_sq_add (∑ k ∈ range n, x (k + 1)) 1
+      · exact sum_pos (fun k _ ↦ hp _) (nonempty_range_iff.2 hn)
+    -- We move the fraction into the denominator.
+    _ = (∑ k ∈ range n, x (k + 1) + 1) ^ 2 / ((∑ k ∈ range n, x (k + 1)) * (1 + 1 / n)) := by
+      field_simp
+    -- We use that `x (n + 1)` is at most the average of `x 1`, `x 2`, ..., `x n`.
+    _ ≤ (∑ k ∈ range (n + 1), x k) ^ 2 / ∑ k ∈ range (n + 1), x (k + 1) := by
+      gcongr
+      · exact sum_pos (fun k _ ↦ hp _) nonempty_range_succ
+      · exact add_nonneg (sum_nonneg fun k _ ↦ (hp _).le) zero_le_one
+      · rw [sum_range_succ', h0]
+      · exact le_avg hn (hx.comp_monotone @Nat.succ_le_succ)
+    -- We conclude by Sedrakyan.
+    _ ≤ _ := sq_sum_div_le_sum_sq_div _ x fun k _ ↦ hp (k + 1)
 
 end Imo1982Q3
 
 /-- Part a of the problem is solved by `n = 4000`. -/
-theorem imo1982Q3_a (hx : Antitone x) (h0 : x 0 = 1) (hp : ∀ k, 0 < x k) :
+theorem imo1982_q3a (hx : Antitone x) (h0 : x 0 = 1) (hp : ∀ k, 0 < x k) :
     ∃ n : ℕ, 3.999 ≤ ∑ k ∈ range n, (x k) ^ 2 / x (k + 1) := by
   use 4000
-  convert Imo1982Q3.ineq₄ (Nat.succ_ne_zero 3998) hx h0 hp
+  convert Imo1982Q3.ineq (Nat.succ_ne_zero 3998) hx h0 hp
   norm_num
 
 /-- Part b of the problem is solved by `x k = (1 / 2) ^ k`. -/
-theorem imo1982Q3_b : ∃ x : ℕ → ℝ, Antitone x ∧ x 0 = 1 ∧ (∀ k, 0 < x k)
+theorem imo1982_q3b : ∃ x : ℕ → ℝ, Antitone x ∧ x 0 = 1 ∧ (∀ k, 0 < x k)
     ∧ ∀ n, ∑ k ∈ range n, x k ^ 2 / x (k + 1) < 4 := by
-  refine ⟨fun k ↦ 2⁻¹ ^ k, ?_, ?_, ?_, fun n ↦ ?_⟩
+  refine ⟨fun k ↦ 2⁻¹ ^ k, ?_, pow_zero _, ?_, fun n ↦ ?_⟩
   · apply (pow_right_strictAnti₀ _ _).antitone <;> norm_num
-  · exact pow_zero _
   · simp
   · have {k : ℕ} : (2 : ℝ)⁻¹ ^ (k * 2) * ((2 : ℝ)⁻¹ ^ k)⁻¹ = (2 : ℝ)⁻¹ ^ k := by
       rw [← pow_sub₀] <;> simp [mul_two]
