5028f653d4d0374481d09f70af25a933fedf4020
Jakob von Raumer <jakob@von-raumer.de>
2024-12-02 17:30:35 +0700

    better naming

--- Mathlib/CategoryTheory/Limits/Preserves/Grothendieck.lean
+++ Mathlib/CategoryTheory/Limits/Preserves/Grothendieck.lean
@@ -13,19 +13,11 @@ variable {H : Type u₂} [Category.{v₂} H]
 variable {J : Type u₁} [Category.{v₁} J]
 variable {F : C ⥤ Cat}
 
-def fiberwiseColimitLimitIso (K : J ⥤ Grothendieck F ⥤ H) [HasLimit K]
-    [∀ {X Y : C} (f : X ⟶ Y), HasColimit (F.map f ⋙ Grothendieck.ι F Y ⋙ limit K)]
-    [∀ (G : Grothendieck F ⥤ H) {X Y : C} (f : X ⟶ Y),
-       HasColimit (F.map f ⋙ Grothendieck.ι F Y ⋙ G)]
-    [HasLimit (K ⋙ fiberwiseColimit')]
-    [∀ c, HasColimit (Grothendieck.ι F c ⋙ limit K)]
-    [∀ c, HasLimit ((K ⋙ fiberwiseColimit') ⋙ (evaluation C H).obj c)]
-    [HasLimitsOfShape J H]
-    [HasColimitsOfShape C H]
-    [∀ c, HasColimitsOfShape (↑(F.obj c)) H]
+def fiberwiseColimitLimitIso (K : J ⥤ Grothendieck F ⥤ H)
+    [∀ (c : C), HasColimitsOfShape (↑(F.obj c)) H] [HasLimitsOfShape J H] [HasColimitsOfShape C H]
     [hC : PreservesLimitsOfShape J (colim (J := C) (C := H))]
     [∀ c, PreservesLimitsOfShape J (colim (J := F.obj c) (C := H))] :
-    fiberwiseColimit (limit K) ≅ limit (K ⋙ fiberwiseColimit') :=
+    fiberwiseColimit (limit K) ≅ limit (K ⋙ fiberwiseColim) :=
   NatIso.ofComponents
     (fun c => HasColimit.isoOfNatIso
        (limitCompWhiskeringLeftIsoCompLimit K (Grothendieck.ι F c)).symm ≪≫
@@ -54,10 +46,10 @@ instance preservesLimitsOfShape_colim_Grothendieck [HasColimitsOfShape C H]
     (whiskeringLeft (↑(F.obj c)) (Grothendieck F) H).obj (Grothendieck.ι F c)) := sorry
   let i₂ := calc colimit (limit K)
     _ ≅ colimit (fiberwiseColimit (limit K)) := (colimitFiberwiseColimitIso _).symm
-    _ ≅ colimit (limit (K ⋙ fiberwiseColimit')) :=
+    _ ≅ colimit (limit (K ⋙ fiberwiseColim)) :=
           HasColimit.isoOfNatIso (fiberwiseColimitLimitIso _)
-    _ ≅ limit ((K ⋙ fiberwiseColimit') ⋙ colim) :=
-          preservesLimitIso colim (K ⋙ fiberwiseColimit')
+    _ ≅ limit ((K ⋙ fiberwiseColim) ⋙ colim) :=
+          preservesLimitIso colim (K ⋙ fiberwiseColim)
     _ ≅ limit (K ⋙ colim) := by sorry  -- TODO functorialisation of `colimitFiberwiseColimitIso`
   haveI : IsIso (limit.post K colim) := by
     convert Iso.isIso_hom i₂
