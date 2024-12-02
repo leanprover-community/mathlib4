b13e5a02f8995f45cbf1a9919173e6d999e7dda1
Jakob von Raumer <jakob@von-raumer.de>
2024-12-02 16:10:02 +0700

    more shambles

--- Mathlib/CategoryTheory/Limits/Preserves/Grothendieck.lean
+++ Mathlib/CategoryTheory/Limits/Preserves/Grothendieck.lean
@@ -6,10 +6,36 @@ namespace CategoryTheory
 
 namespace Limits
 
+noncomputable section
+
 variable {C : Type u₁} [Category.{v₁} C]
 variable {H : Type u₂} [Category.{v₂} H]
 variable {J : Type u₁} [Category.{v₁} J]
 variable {F : C ⥤ Cat}
+
+def fiberwiseColimitLimitIso (K : J ⥤ Grothendieck F ⥤ H) [HasLimit K]
+    [∀ {X Y : C} (f : X ⟶ Y), HasColimit (F.map f ⋙ Grothendieck.ι F Y ⋙ limit K)]
+    [∀ (G : Grothendieck F ⥤ H) {X Y : C} (f : X ⟶ Y),
+       HasColimit (F.map f ⋙ Grothendieck.ι F Y ⋙ G)]
+    [HasLimit (K ⋙ fiberwiseColimit')]
+    [∀ c, HasColimit (Grothendieck.ι F c ⋙ limit K)]
+    [∀ c, HasLimit ((K ⋙ fiberwiseColimit') ⋙ (evaluation C H).obj c)]
+    [HasLimitsOfShape J H]
+    [HasColimitsOfShape C H]
+    [∀ c, HasColimitsOfShape (↑(F.obj c)) H]
+    [hC : PreservesLimitsOfShape J (colim (J := C) (C := H))]
+    [∀ c, PreservesLimitsOfShape J (colim (J := F.obj c) (C := H))] :
+    fiberwiseColimit (limit K) ≅ limit (K ⋙ fiberwiseColimit') :=
+  NatIso.ofComponents
+    (fun c => HasColimit.isoOfNatIso
+       (limitCompWhiskeringLeftIsoCompLimit K (Grothendieck.ι F c)).symm ≪≫
+      preservesLimitIso colim _ ≪≫
+      HasLimit.isoOfNatIso
+        (Functor.associator _ _ _ ≪≫
+        isoWhiskerLeft _ sorry ≪≫
+        (Functor.associator _ _ _).symm) ≪≫
+      (limitObjIsoLimitCompEvaluation _ c).symm)
+    sorry
 
 variable (C) (F) in
 instance preservesLimitsOfShape_colim_Grothendieck [HasColimitsOfShape C H]
@@ -19,27 +45,17 @@ instance preservesLimitsOfShape_colim_Grothendieck [HasColimitsOfShape C H]
     PreservesLimitsOfShape J (colim (J := Grothendieck F) (C := H)) := by
   haveI : HasLimitsOfShape J (Grothendieck F ⥤ H) := sorry
   haveI : HasLimitsOfShape J (C ⥤ H) := sorry
-  haveI : HasLimitsOfShape J H := sorry
   haveI : HasLimitsOfShape C H := sorry
   haveI : HasColimitsOfShape J (C ⥤ H) := sorry
+  haveI : HasLimitsOfShape J H := sorry
   constructor
   intro K
-  haveI : ∀ c, HasLimit (K ⋙ (whiskeringLeft (↑(F.obj c)) (Grothendieck F) H).obj (Grothendieck.ι F c)) := sorry
-  let i₀ : ∀ c, Grothendieck.ι F c ⋙ limit K ≅
-    limit (K ⋙ (whiskeringLeft _ _ _).obj (Grothendieck.ι F c)) := sorry
-  let i₁ : fiberwiseColimit (limit K) ≅ limit (K ⋙ fiberwiseColimit') :=
-    NatIso.ofComponents
-      (fun c => HasColimit.isoOfNatIso (i₀ c) ≪≫
-        preservesLimitIso colim _ ≪≫
-        HasLimit.isoOfNatIso
-          (Functor.associator _ _ _ ≪≫
-          isoWhiskerLeft _ _ ≪≫
-          (Functor.associator _ _ _).symm) ≪≫
-        (limitObjIsoLimitCompEvaluation _ c).symm)
-      _
+  haveI : ∀ c, HasLimit (K ⋙
+    (whiskeringLeft (↑(F.obj c)) (Grothendieck F) H).obj (Grothendieck.ι F c)) := sorry
   let i₂ := calc colimit (limit K)
     _ ≅ colimit (fiberwiseColimit (limit K)) := (colimitFiberwiseColimitIso _).symm
-    _ ≅ colimit (limit (K ⋙ fiberwiseColimit')) := HasColimit.isoOfNatIso i₁
+    _ ≅ colimit (limit (K ⋙ fiberwiseColimit')) :=
+          HasColimit.isoOfNatIso (fiberwiseColimitLimitIso _)
     _ ≅ limit ((K ⋙ fiberwiseColimit') ⋙ colim) :=
           preservesLimitIso colim (K ⋙ fiberwiseColimit')
     _ ≅ limit (K ⋙ colim) := by sorry  -- TODO functorialisation of `colimitFiberwiseColimitIso`
@@ -51,9 +67,7 @@ instance preservesLimitsOfShape_colim_Grothendieck [HasColimitsOfShape C H]
     sorry
   apply preservesLimit_of_isIso_post
 
-#check limitCompWhiskeringRightIsoLimitComp
-#check limitObjIsoLimitCompEvaluation
-#check Functor.associator
+end
 
 end Limits
 
