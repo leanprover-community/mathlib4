cc6e1d2df56bd456194db345c98698eff0794974
Anne C.A. Baanen <vierkantor@vierkantor.com>
2024-12-02 11:04:13 +0100

    Readd downstream imports

--- Mathlib/RingTheory/Polynomial/IntegralNormalization.lean
+++ Mathlib/RingTheory/Polynomial/IntegralNormalization.lean
@@ -3,6 +3,7 @@ Copyright (c) 2018 Chris Hughes. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Chris Hughes, Johannes Hölzl, Kim Morrison, Jens Wagemaker, Andrew Yang, Yuyang Zhao
 -/
+import Mathlib.Algebra.Polynomial.Div
 import Mathlib.Algebra.Polynomial.Monic
 import Mathlib.RingTheory.Polynomial.ScaleRoots
 
