7c528b86eafc372878af3cdc87790ecad6d57f8a
acmepjz <acme_pjz@hotmail.com>
2024-12-02 23:24:48 +0800

    adapt master changes

--- Mathlib/FieldTheory/LinearDisjoint.lean
+++ Mathlib/FieldTheory/LinearDisjoint.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jz Pan
 -/
 import Mathlib.FieldTheory.Adjoin
-import Mathlib.RingTheory.AlgebraicIndependent
+import Mathlib.RingTheory.AlgebraicIndependent.RankAndCardinality
 import Mathlib.RingTheory.LinearDisjoint
 
 /-!
