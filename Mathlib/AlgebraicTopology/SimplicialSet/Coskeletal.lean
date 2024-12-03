ec8e6c3b143812407d28f5b01458b61489917d86
Emily Riehl <eriehl@jhu.edu>
2024-12-03 11:45:58 -0500

    Update Mathlib/AlgebraicTopology/SimplicialSet/Coskeletal.lean
    Co-authored-by: Joël Riou <37772949+joelriou@users.noreply.github.com>

--- Mathlib/AlgebraicTopology/SimplicialSet/Coskeletal.lean
+++ Mathlib/AlgebraicTopology/SimplicialSet/Coskeletal.lean
@@ -246,7 +246,7 @@ recorded by the composite functor `nerveFunctor₂`.-/
 def nerveFunctor₂ : Cat.{v, u} ⥤ SSet.Truncated 2 := nerveFunctor ⋙ truncation 2
 
 /-- The natural isomorphism between `nerveFunctor` and `nerveFunctor₂ ⋙ Truncated.cosk 2` whose
-components `nerve C ≅  (Truncated.cosk 2).obj (nerve₂ C)` shows that nerves of categories are
+components `nerve C ≅ (Truncated.cosk 2).obj (nerve₂ C)` shows that nerves of categories are
 2-coskeletal.-/
 noncomputable def cosk₂Iso : nerveFunctor.{v, u} ≅ nerveFunctor₂.{v, u} ⋙ Truncated.cosk 2 :=
   NatIso.ofComponents (fun C ↦ (nerve C).isoCoskOfIsCoskeletal 2)
