b10b73014f9af59ab87454a466bd9b1a631f7ffe
Joël Riou <joel.riou@universite-paris-saclay.fr>
2024-12-03 14:22:20 +0000

    feat(Algebra/Module): presentation of the restriction of scalars of a module (#18389)
    Given a morphism of rings `A → B` and a `B`-module `M`, we obtain a presentation of `M` as a `A`-module from a presentation of `M` as `B`-module, a presentation of `B` as a `A`-module (and some additional data).
    
    
    
    Co-authored-by: Joël Riou <37772949+joelriou@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -554,6 +554,7 @@ import Mathlib.Algebra.Module.Presentation.Differentials
 import Mathlib.Algebra.Module.Presentation.DirectSum
 import Mathlib.Algebra.Module.Presentation.Finite
 import Mathlib.Algebra.Module.Presentation.Free
+import Mathlib.Algebra.Module.Presentation.RestrictScalars
 import Mathlib.Algebra.Module.Presentation.Tautological
 import Mathlib.Algebra.Module.Prod
 import Mathlib.Algebra.Module.Projective
