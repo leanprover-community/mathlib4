3ab12ad600ef8364cbb1e1412d1d0b9b6307dc54
Kim Morrison <kim@tqft.net>
2024-12-03 20:06:17 +1100

    remove upstreamed

--- Mathlib/Algebra/BigOperators/Group/List.lean
+++ Mathlib/Algebra/BigOperators/Group/List.lean
@@ -661,22 +661,8 @@ lemma mem_mem_ranges_iff_lt_sum (l : List ℕ) {n : ℕ} :
     (∃ s ∈ l.ranges, n ∈ s) ↔ n < l.sum := by
   rw [← mem_range, ← ranges_flatten, mem_flatten]
 
-@[simp]
-theorem length_flatMap (l : List α) (f : α → List β) :
-    length (List.flatMap l f) = sum (map (length ∘ f) l) := by
-  rw [List.flatMap, length_flatten, map_map]
-
 @[deprecated (since := "2024-10-16")] alias length_bind := length_flatMap
-
-lemma countP_flatMap (p : β → Bool) (l : List α) (f : α → List β) :
-    countP p (l.flatMap f) = sum (map (countP p ∘ f) l) := by
-  rw [List.flatMap, countP_flatten, map_map]
-
 @[deprecated (since := "2024-10-16")] alias countP_bind := countP_flatMap
-
-lemma count_flatMap [BEq β] (l : List α) (f : α → List β) (x : β) :
-    count x (l.flatMap f) = sum (map (count x ∘ f) l) := countP_flatMap _ _ _
-
 @[deprecated (since := "2024-10-16")] alias count_bind := count_flatMap
 
 /-- In a flatten, taking the first elements up to an index which is the sum of the lengths of the
