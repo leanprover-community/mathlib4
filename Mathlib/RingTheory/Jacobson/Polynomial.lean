ef08ac9819c1d56c5a7eb93b7244b0edec6dee89
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2024-12-05 21:32:20 +0100

    chore: address some porting notes for Jacobson ideals

--- Mathlib/RingTheory/Jacobson/Polynomial.lean
+++ Mathlib/RingTheory/Jacobson/Polynomial.lean
@@ -29,17 +29,8 @@ theorem jacobson_bot_polynomial_le_sInf_map_maximal :
     rwa [map_jacobson_of_bijective _, map_bot] at t
     exact RingEquiv.bijective (polynomialQuotientEquivQuotientPolynomial j)
   refine eq_bot_iff.2 fun f hf => ?_
-  have r1 : (X : (R ⧸ j)[X]) ≠ 0 := fun hX => by
-    replace hX := congr_arg (fun f => coeff f 1) hX
-    simp only [coeff_X_one, coeff_zero] at hX
-    exact zero_ne_one hX.symm
-  have r2 := eq_C_of_degree_eq_zero (degree_eq_zero_of_isUnit ((mem_jacobson_bot.1 hf) X))
-  simp only [coeff_add, mul_coeff_zero, coeff_X_zero, mul_zero, coeff_one_zero, zero_add] at r2
-  erw [add_left_eq_self] at r2
-  simpa using (mul_eq_zero.mp r2).resolve_right r1
-  -- Porting note: this is golfed to much
-  -- simpa [(fun hX => by simpa using congr_arg (fun f => coeff f 1) hX : (X : (R ⧸ j)[X]) ≠ 0)]
-  --   using eq_C_of_degree_eq_zero (degree_eq_zero_of_is_unit ((mem_jacobson_bot.1 hf) X))
+  have r1 : (X : (R ⧸ j)[X]) ≠ 0 := ne_of_apply_ne (coeff · 1) <| by simp
+  simpa [r1] using eq_C_of_degree_eq_zero (degree_eq_zero_of_isUnit ((mem_jacobson_bot.1 hf) X))
 
 theorem jacobson_bot_polynomial_of_jacobson_bot (h : jacobson (⊥ : Ideal R) = ⊥) :
     jacobson (⊥ : Ideal R[X]) = ⊥ := by
