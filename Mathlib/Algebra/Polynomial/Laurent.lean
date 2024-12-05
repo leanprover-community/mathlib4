10861b4e9a1db6594508d5fc61f6c897ce0ab1d4
Justus Springer <justusspringer@gmx.de>
2024-12-05 13:47:45 +0100

    change name from lift to eval2

--- Mathlib/Algebra/Polynomial/Laurent.lean
+++ Mathlib/Algebra/Polynomial/Laurent.lean
@@ -531,56 +531,56 @@ theorem mk'_one_X : IsLocalization.mk' R[T;T⁻¹] 1
 
 /-- Given a ring homomorphism `f : R →+* S` and a unit `x` in `S`, the induced homomorphism
 `R[T;T⁻¹] →+* S` sending `T` to `x` and `T⁻¹` to `x⁻¹`. -/
-def lift : R[T;T⁻¹] →+* S :=
+def eval₂ : R[T;T⁻¹] →+* S :=
   IsLocalization.lift (M := Submonoid.powers (X : R[X])) (g := Polynomial.eval₂RingHom f x) (by
     rintro ⟨y, n, rfl⟩
     simpa only [coe_eval₂RingHom, eval₂_X_pow] using IsUnit.pow n hx
   )
 
 @[simp]
-theorem lift_toLaurent (p : R[X]) : lift f x hx (toLaurent p) = eval₂ f x p := by
-  unfold lift
+theorem eval₂_toLaurent (p : R[X]) : eval₂ f x hx (toLaurent p) = Polynomial.eval₂ f x p := by
+  unfold eval₂
   rw [←algebraMap_eq_toLaurent, IsLocalization.lift_eq]
   rfl
 
 @[simp]
-theorem lift_T_n (n : ℕ) : lift f x hx (T n) = x ^ n := by
-  rw [←Polynomial.toLaurent_X_pow, lift_toLaurent, eval₂_X_pow f x]
+theorem eval₂_T_n (n : ℕ) : eval₂ f x hx (T n) = x ^ n := by
+  rw [←Polynomial.toLaurent_X_pow, eval₂_toLaurent, eval₂_X_pow f x]
 
 @[simp]
-theorem lift_T_neg_n (n : ℕ) : lift f x hx (T (-n)) = hx.unit⁻¹ ^ n := by
+theorem eval₂_T_neg_n (n : ℕ) : eval₂ f x hx (T (-n)) = hx.unit⁻¹ ^ n := by
   rw [←mk'_one_X_pow]
-  unfold lift
+  unfold eval₂
   rw [IsLocalization.lift_mk'_spec, map_one, coe_eval₂RingHom, eval₂_X_pow, ←mul_pow,
     IsUnit.mul_val_inv, one_pow]
 
-theorem lift_T (n : ℤ) : lift f x hx (T n) =
+theorem eval₂_T (n : ℤ) : eval₂ f x hx (T n) =
   if (0 ≤ n) then (x ^ n.toNat) else (hx.unit⁻¹ ^ (-n).toNat) := by
   by_cases hn : 0 ≤ n
   · lift n to ℕ using hn
-    rw [if_pos (Int.ofNat_zero_le n), Int.toNat_ofNat, lift_T_n]
+    rw [if_pos (Int.ofNat_zero_le n), Int.toNat_ofNat, eval₂_T_n]
   · obtain ⟨m, rfl⟩ := Int.exists_eq_neg_ofNat (Int.le_of_not_le hn)
-    rw [if_neg hn, lift_T_neg_n, neg_neg, Int.toNat_ofNat]
+    rw [if_neg hn, eval₂_T_neg_n, neg_neg, Int.toNat_ofNat]
 
 @[simp]
-theorem lift_C (r : R) : lift f x hx (C r) = f r := by
-  rw [← toLaurent_C, lift_toLaurent, eval₂_C]
+theorem eval₂_C (r : R) : eval₂ f x hx (C r) = f r := by
+  rw [← toLaurent_C, eval₂_toLaurent, Polynomial.eval₂_C]
 
 @[simp]
-theorem lift_C_mul_T_n (r : R) (n : ℕ) : lift f x hx (C r * T n) = f r * x ^ n := by
-  rw [←Polynomial.toLaurent_C_mul_T, lift_toLaurent, eval₂_monomial]
+theorem eval₂_C_mul_T_n (r : R) (n : ℕ) : eval₂ f x hx (C r * T n) = f r * x ^ n := by
+  rw [←Polynomial.toLaurent_C_mul_T, eval₂_toLaurent, eval₂_monomial]
 
 @[simp]
-theorem lift_C_mul_T_neg_n (r : R) (n : ℕ) : lift f x hx (C r * T (-n)) =
-  f r * hx.unit⁻¹ ^ n := by rw [map_mul, lift_T_neg_n, lift_C]
+theorem eval₂_C_mul_T_neg_n (r : R) (n : ℕ) : eval₂ f x hx (C r * T (-n)) =
+  f r * hx.unit⁻¹ ^ n := by rw [map_mul, eval₂_T_neg_n, eval₂_C]
 
-theorem lift_C_mul_T (r : R) (n : ℤ) : lift f x hx (C r * T n) =
+theorem eval₂_C_mul_T (r : R) (n : ℤ) : eval₂ f x hx (C r * T n) =
   if (0 ≤ n) then f r * x ^ n.toNat else f r * hx.unit⁻¹ ^ (-n).toNat := by
   by_cases hn : 0 ≤ n
   · lift n to ℕ using hn
-    rw [map_mul, lift_C, lift_T_n, if_pos (Int.ofNat_zero_le n), Int.toNat_ofNat]
+    rw [map_mul, eval₂_C, eval₂_T_n, if_pos (Int.ofNat_zero_le n), Int.toNat_ofNat]
   · obtain ⟨m, rfl⟩ := Int.exists_eq_neg_ofNat (Int.le_of_not_le hn)
-    rw [map_mul, lift_C, lift_T_neg_n, if_neg hn, neg_neg, Int.toNat_ofNat]
+    rw [map_mul, eval₂_C, eval₂_T_neg_n, if_neg hn, neg_neg, Int.toNat_ofNat]
 
 end CommSemiring
 
