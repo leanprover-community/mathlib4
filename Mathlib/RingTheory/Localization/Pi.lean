c48d19a812daba2665d6ee89901f077aca9601df
maddycrim <maddycrim84@gmail.com>
2024-12-05 16:48:57 -0500

    fix imports

--- Mathlib/RingTheory/Localization/Pi.lean
+++ Mathlib/RingTheory/Localization/Pi.lean
@@ -3,7 +3,8 @@ Copyright (c) 2024 Madison Crim. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Madison Crim
 -/
-import Mathlib.RingTheory.Localization.Basic
+import Mathlib.Algebra.Group.Submonoid.BigOperators
+import Mathlib.RingTheory.Localization.Defs
 import Mathlib.Algebra.Ring.Pi
 
 /-!
