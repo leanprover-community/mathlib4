8813e601e3345f4a8fd50f7addea44f181b5c65c
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2024-12-07 01:13:11 +0000

    remove import

--- Mathlib/Lean/Meta/RefinedDiscrTree/Basic.lean
+++ Mathlib/Lean/Meta/RefinedDiscrTree/Basic.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jovan Gerbscheid
 -/
 import Mathlib.Init
-import Lean.Meta.WHNF
 
 /-!
 # Basic Definitions for `RefinedDiscrTree`
