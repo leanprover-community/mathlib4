9907a2235d8ca6c12ce4db42512d69e07635cac3
David Loeffler <d.loeffler.01@cantab.net>
2024-12-07 15:50:23 +0100

    Update Mathlib/LinearAlgebra/Goursat.lean
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- Mathlib/LinearAlgebra/Goursat.lean
+++ Mathlib/LinearAlgebra/Goursat.lean
@@ -3,9 +3,9 @@ Copyright (c) 2024 David Loeffler. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: David Loeffler
 -/
+import Mathlib.GroupTheory.Goursat
 import Mathlib.LinearAlgebra.Prod
 import Mathlib.LinearAlgebra.Quotient.Basic
-import Mathlib.GroupTheory.Goursat
 
 /-!
 # Goursat's lemma for submodules
