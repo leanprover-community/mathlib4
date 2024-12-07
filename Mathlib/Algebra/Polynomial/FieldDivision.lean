a535d75cf0441377c51d62569a6c312d5940e9fa
Yaël Dillies <yael.dillies@gmail.com>
2024-12-07 14:38:39 +0000

    fix

--- Mathlib/Algebra/Polynomial/FieldDivision.lean
+++ Mathlib/Algebra/Polynomial/FieldDivision.lean
@@ -3,6 +3,7 @@ Copyright (c) 2018 Chris Hughes. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Chris Hughes, Johannes Hölzl, Kim Morrison, Jens Wagemaker
 -/
+import Mathlib.Algebra.Order.Group.Finset
 import Mathlib.Algebra.Polynomial.Derivative
 import Mathlib.Algebra.Polynomial.Eval.SMul
 import Mathlib.Algebra.Polynomial.Roots
