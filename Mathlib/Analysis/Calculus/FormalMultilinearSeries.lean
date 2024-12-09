a7433b0c40052a3dfcb60776df19c553995bde5e
Vasily Nesterov <vas.nesterov63@gmail.com>
2024-12-10 00:35:54 +0300

    rename

--- Mathlib/Analysis/Calculus/FormalMultilinearSeries.lean
+++ Mathlib/Analysis/Calculus/FormalMultilinearSeries.lean
@@ -169,7 +169,7 @@ def unshift (q : FormalMultilinearSeries 𝕜 E (E →L[𝕜] F)) (z : F) : Form
   | 0 => (continuousMultilinearCurryFin0 𝕜 E F).symm z
   | n + 1 => (continuousMultilinearCurryRightEquiv' 𝕜 n E F).symm (q n)
 
-theorem shift_unshift {p : FormalMultilinearSeries 𝕜 E (E →L[𝕜] F)} {z : F} :
+theorem unshift_shift {p : FormalMultilinearSeries 𝕜 E (E →L[𝕜] F)} {z : F} :
     (p.unshift z).shift = p := by
   ext1 n
   simp [shift, unshift]
