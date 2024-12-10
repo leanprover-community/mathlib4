a8908583bb9fbc1edc293acb29839ce84f89ed28
Arend Mellendijk <arend.mellendijk@gmail.com>
2024-12-10 17:55:10 +0100

    temp cleanup

--- Mathlib/NumberTheory/SelbergSieve/Temp.lean
+++ Mathlib/NumberTheory/SelbergSieve/Temp.lean
@@ -14,25 +14,26 @@ import Mathlib.Analysis.SumIntegralComparisons
 import Mathlib.Data.Nat.Prime.Basic
 import Mathlib.MeasureTheory.Integral.IntervalIntegral
 import Mathlib.NumberTheory.ArithmeticFunction
+import Mathlib.NumberTheory.Harmonic.Bounds
 
 noncomputable section
 
-open scoped BigOperators ArithmeticFunction
+open scoped BigOperators
 
 open Nat ArithmeticFunction Finset
-
 
 namespace ArithmeticFunction.IsMultiplicative
 
 variable {R : Type*}
 
-theorem mult_lcm_eq_of_ne_zero [CommGroupWithZero R] (f : ArithmeticFunction R) (h_mult : f.IsMultiplicative) (x y : ℕ)
-    (hf : f (x.gcd y) ≠ 0) :
+theorem mult_lcm_eq_of_ne_zero [CommGroupWithZero R] (f : ArithmeticFunction R)
+    (h_mult : f.IsMultiplicative) (x y : ℕ) (hf : f (x.gcd y) ≠ 0) :
     f (x.lcm y) = f x * f y / f (x.gcd y) := by
   rw [←h_mult.lcm_apply_mul_gcd_apply]
   field_simp
 
-theorem prod_factors_of_mult (f : ArithmeticFunction ℝ) (h_mult : ArithmeticFunction.IsMultiplicative f) {l : ℕ} (hl : Squarefree l) :
+theorem prod_factors_of_mult (f : ArithmeticFunction ℝ) (h_mult : f.IsMultiplicative)
+    {l : ℕ} (hl : Squarefree l) :
     ∏ a : ℕ in l.primeFactors, f a = f l := by
   rw [←IsMultiplicative.map_prod_of_subset_primeFactors h_mult l _ Finset.Subset.rfl,
     Nat.prod_primeFactors_of_squarefree hl]
@@ -49,8 +50,7 @@ theorem sum_intro {α M: Type _} [AddCommMonoid M] [DecidableEq α] (s : Finset 
      (hd : d ∈ s) :
     f d = ∑ k in s, if k = d then f k else 0 := by
   trans (∑ k in s, if k = d then f d else 0)
-  · rw [sum_eq_single_of_mem d hd]
-    rw [if_pos rfl]
+  · rw [sum_eq_single_of_mem d hd, if_pos rfl]
     intro _ _ h; rw [if_neg h]
   apply sum_congr rfl; intro k _; apply if_ctx_congr Iff.rfl _ (fun _ => rfl)
   intro h; rw [h]
@@ -79,10 +79,11 @@ theorem conv_lambda_sq_larger_sum (f : ℕ → ℕ → ℕ → ℝ) (n : ℕ) :
 
 theorem moebius_inv_dvd_lower_bound (l m : ℕ) (hm : Squarefree m) :
     (∑ d in m.divisors, if l ∣ d then (μ d:ℤ) else 0) = if l = m then (μ l:ℤ) else 0 := by
-  have hm_pos : 0 < m := Nat.pos_of_ne_zero $ Squarefree.ne_zero hm
+  have hm_pos : 0 < m := Nat.pos_of_ne_zero <| Squarefree.ne_zero hm
   revert hm
   revert m
-  apply (ArithmeticFunction.sum_eq_iff_sum_smul_moebius_eq_on {n | Squarefree n} (fun _ _ => Squarefree.squarefree_of_dvd)).mpr
+  apply (ArithmeticFunction.sum_eq_iff_sum_smul_moebius_eq_on {n | Squarefree n}
+    (fun _ _ => Squarefree.squarefree_of_dvd)).mpr
   intro m hm_pos hm
   rw [sum_divisorsAntidiagonal' (f:= fun x y => μ x • if l=y then μ l else 0)]--
   by_cases hl : l ∣ m
@@ -92,7 +93,7 @@ theorem moebius_inv_dvd_lower_bound (l m : ℕ) (hm : Squarefree m) :
         hmul]
 
       apply coprime_of_squarefree_mul; rw [hmul]; exact hm
-    · intro d _ hdl; rw[if_neg $ hdl.symm, smul_zero]
+    · intro d _ hdl; rw[if_neg hdl.symm, smul_zero]
     · intro h; rw[mem_divisors] at h; exfalso; exact h ⟨hl, (Nat.ne_of_lt hm_pos).symm⟩
   · rw [if_neg hl, sum_eq_zero]; intro d hd
     rw [if_neg, smul_zero]
@@ -106,7 +107,8 @@ theorem moebius_inv_dvd_lower_bound' {P : ℕ} (hP : Squarefree P) (l m : ℕ) (
   simp_rw[ite_and, ←sum_filter, filter_comm]
 
 theorem moebius_inv_dvd_lower_bound_real {P : ℕ} (hP : Squarefree P) (l m : ℕ) (hm : m ∣ P) :
-    (∑ d in P.divisors, if l ∣ d ∧ d ∣ m then (μ d : ℝ) else 0) = if l = m then (μ l : ℝ) else 0 := by
+    (∑ d in P.divisors, if l ∣ d ∧ d ∣ m then (μ d : ℝ) else 0) = if l = m then (μ l : ℝ) else 0
+    := by
   norm_cast
   apply moebius_inv_dvd_lower_bound' hP l m hm
 
@@ -115,7 +117,8 @@ theorem gcd_dvd_mul (m n : ℕ) : m.gcd n ∣ m * n := by
     m.gcd n ∣ m := Nat.gcd_dvd_left m n
     _ ∣ m * n := ⟨n, rfl⟩
 
-theorem multiplicative_zero_of_zero_dvd (f : ArithmeticFunction ℝ) (h_mult : IsMultiplicative f) {m n : ℕ}
+theorem multiplicative_zero_of_zero_dvd (f : ArithmeticFunction ℝ) (h_mult : IsMultiplicative f)
+    {m n : ℕ}
     (h_sq : Squarefree n) (hmn : m ∣ n) (h_zero : f m = 0) : f n = 0 := by
   rcases hmn with ⟨k, rfl⟩
   simp only [MulZeroClass.zero_mul, eq_self_iff_true, h_mult.map_mul_of_coprime
@@ -126,88 +129,12 @@ theorem primeDivisors_nonempty (n : ℕ) (hn : 2 ≤ n) : n.primeFactors.Nonempt
   simp_rw[Nat.mem_primeFactors_of_ne_zero (by positivity)]
   apply Nat.exists_prime_and_dvd (by linarith)
 
-theorem div_mult_of_dvd_squarefree (f : ArithmeticFunction ℝ) (h_mult : IsMultiplicative f) (l d : ℕ) (hdl : d ∣ l)
-    (hl : Squarefree l) (hd : f d ≠ 0) : f l / f d = f (l / d) := by
+theorem div_mult_of_dvd_squarefree (f : ArithmeticFunction ℝ) (h_mult : IsMultiplicative f)
+    (l d : ℕ) (hdl : d ∣ l) (hl : Squarefree l) (hd : f d ≠ 0) : f l / f d = f (l / d) := by
   apply div_eq_of_eq_mul hd
   rw [← h_mult.right, Nat.div_mul_cancel hdl]
   apply coprime_of_squarefree_mul
   convert hl
   exact Nat.div_mul_cancel hdl
-
-theorem inv_sub_antitoneOn_gt {R : Type*} [LinearOrderedField R] (c : R) :
-    AntitoneOn (fun x:R ↦ (x-c)⁻¹) (Set.Ioi c) := by
-  refine antitoneOn_iff_forall_lt.mpr ?_
-  intro a ha b hb hab
-  rw [Set.mem_Ioi] at ha hb
-  gcongr; linarith
-
-theorem inv_sub_antitoneOn_Icc {R : Type*} [LinearOrderedField R]  (a b c: R) (ha : c < a) :
-    AntitoneOn (fun x ↦ (x-c)⁻¹) (Set.Icc a b) := by
-  by_cases hab : a ≤ b
-  · exact inv_sub_antitoneOn_gt c |>.mono <| (Set.Icc_subset_Ioi_iff hab).mpr ha
-  · simp [hab, Set.Subsingleton.antitoneOn]
-
-theorem inv_antitoneOn_pos {R : Type*} [LinearOrderedField R] :
-    AntitoneOn (fun x:R ↦ x⁻¹) (Set.Ioi 0) := by
-  convert inv_sub_antitoneOn_gt (R:=R) 0; ring
-
-theorem inv_antitoneOn_Icc {R : Type*} [LinearOrderedField R] (a b : R) (ha : 0 < a) :
-    AntitoneOn (fun x ↦ x⁻¹) (Set.Icc a b) := by
-  convert inv_sub_antitoneOn_Icc a b 0 ha; ring
-
-theorem log_add_one_le_sum_inv (n : ℕ) :
-    Real.log ↑(n+1) ≤ ∑ d in Finset.Icc 1 n, (d:ℝ)⁻¹ := by
-  calc _ = ∫ x in (1)..↑(n+1), x⁻¹ := ?_
-       _ = ∫ x in (1:ℕ)..↑(n+1), x⁻¹ := ?_
-       _ ≤ _ := ?_
-  · rw[integral_inv (by simp[(show ¬ (1:ℝ) ≤ 0 by norm_num)] )]; congr; ring
-  · congr; norm_num
-  · apply AntitoneOn.integral_le_sum_Ico (by norm_num)
-    apply inv_antitoneOn_Icc
-    norm_num
-
-theorem log_le_sum_inv (y : ℝ) (hy : 1 ≤ y) :
-    Real.log y ≤ ∑ d in Finset.Icc 1 (⌊y⌋₊), (d:ℝ)⁻¹ := by
-  calc _ ≤ Real.log ↑(Nat.floor y + 1) := ?_
-       _ ≤ _ := ?_
-  · gcongr
-    apply (le_ceil y).trans
-    norm_cast
-    exact ceil_le_floor_add_one y
-  · apply log_add_one_le_sum_inv
-
-theorem sum_inv_le_log (n : ℕ) (hn : 1 ≤ n) :
-    ∑ d in Finset.Icc 1 n, (d : ℝ)⁻¹ ≤ 1 + Real.log ↑n :=
-  by
-  rw [← Finset.sum_erase_add (Icc 1 n) _ (by simp [hn] : 1 ∈ Icc 1 n), add_comm]
-  gcongr
-  · norm_num
-  simp only [gt_iff_lt, lt_one_iff, mem_Icc, true_and, not_le, Icc_erase_left]
-  calc
-    ∑ d : ℕ in Ico 2 (n + 1), (d : ℝ)⁻¹ = ∑ d in Ico 2 (n + 1), (↑(d + 1) - 1)⁻¹ := ?_
-    _ ≤ ∫ x in (2).. ↑(n + 1), (x - 1)⁻¹  := ?_
-    _ = Real.log ↑n := ?_
-  · congr; norm_num;
-  · apply @AntitoneOn.sum_le_integral_Ico 2 (n + 1) fun x : ℝ => (x - 1)⁻¹
-    · linarith [hn]
-    apply inv_sub_antitoneOn_Icc; norm_num
-  rw [intervalIntegral.integral_comp_sub_right _ 1, integral_inv]
-  · norm_num
-  norm_num; simp[hn, show (0:ℝ) < 1 by norm_num]
-
-theorem sum_inv_le_log_real (y : ℝ) (hy : 1 ≤ y) :
-    ∑ d in Finset.Icc 1 (⌊y⌋₊), (d:ℝ)⁻¹ ≤ 1 + Real.log y := by
-  trans (1 + Real.log (⌊y⌋₊))
-  · apply sum_inv_le_log (⌊y⌋₊)
-    apply le_floor; norm_cast
-  gcongr
-  · norm_cast; apply Nat.lt_of_succ_le; apply le_floor; norm_cast
-  · apply floor_le; linarith
-
-theorem Nat.le_prod {ι : Type*} [DecidableEq ι] {f : ι → ℕ} {s : Finset ι} {i : ι} (hi : i ∈ s) (hf : ∀ i ∈ s, f i ≠ 0):
-    f i ≤ ∏ j in s, f j := by
-  rw [←prod_erase_mul (a:=i) (h:= hi)]
-  exact Nat.le_mul_of_pos_left _ (prod_pos fun j hj => Nat.pos_of_ne_zero (hf j (mem_of_mem_erase hj)))
-
 
 end Aux
