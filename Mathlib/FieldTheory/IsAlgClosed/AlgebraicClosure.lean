b924b9e33af24ed9355982fd20fe9d2735bb517a
Junyan Xu <junyanxu.math@gmail.com>
2024-12-10 15:30:37 -0500

    shake

--- Mathlib/FieldTheory/IsAlgClosed/AlgebraicClosure.lean
+++ Mathlib/FieldTheory/IsAlgClosed/AlgebraicClosure.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kenny Lau
 -/
 import Mathlib.Algebra.CharP.Algebra
-import Mathlib.Algebra.Polynomial.Eval.Irreducible
 import Mathlib.FieldTheory.Isaacs
 import Mathlib.FieldTheory.SplittingField.Construction
 
