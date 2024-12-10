57bf40112b5f3e0f11f619d4d67fe2de66f9e650
Yaël Dillies <yael.dillies@gmail.com>
2024-12-10 17:35:30 +0000

    feat: lower bound on the Ruzsa-Szemerédi problem (#19000)
    From LeanCamCombi

--- Mathlib.lean
+++ Mathlib.lean
@@ -2199,6 +2199,7 @@ import Mathlib.Combinatorics.Enumerative.DyckWord
 import Mathlib.Combinatorics.Enumerative.IncidenceAlgebra
 import Mathlib.Combinatorics.Enumerative.InclusionExclusion
 import Mathlib.Combinatorics.Enumerative.Partition
+import Mathlib.Combinatorics.Extremal.RuzsaSzemeredi
 import Mathlib.Combinatorics.HalesJewett
 import Mathlib.Combinatorics.Hall.Basic
 import Mathlib.Combinatorics.Hall.Finite
