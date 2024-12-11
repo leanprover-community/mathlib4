3afdc36af13ecd61dc06182733142724da2cd9fd
Jireh Loreaux <loreaujy@gmail.com>
2024-12-11 11:35:25 -0600

    refactor: change `⟪x, y⟫_𝕜 := conj x * y` to `y * conj x`.

--- Mathlib/Analysis/InnerProductSpace/Basic.lean
+++ Mathlib/Analysis/InnerProductSpace/Basic.lean
@@ -1939,15 +1939,18 @@ local notation "⟪" x ", " y "⟫" => @inner 𝕜 _ _ x y
 
 /-- A field `𝕜` satisfying `RCLike` is itself a `𝕜`-inner product space. -/
 instance RCLike.innerProductSpace : InnerProductSpace 𝕜 𝕜 where
-  inner x y := conj x * y
-  norm_sq_eq_inner x := by simp only [inner, conj_mul, ← ofReal_pow, ofReal_re]
+  inner x y := y * conj x
+  norm_sq_eq_inner x := by simp only [inner, mul_conj, ← ofReal_pow, ofReal_re]
   conj_symm x y := by simp only [mul_comm, map_mul, starRingEnd_self_apply]
-  add_left x y z := by simp only [add_mul, map_add]
-  smul_left x y z := by simp only [mul_assoc, smul_eq_mul, map_mul]
+  add_left x y z := by simp only [mul_add, map_add]
+  smul_left x y z := by simp only [mul_comm (conj z), mul_assoc, smul_eq_mul, map_mul]
 
 @[simp]
-theorem RCLike.inner_apply (x y : 𝕜) : ⟪x, y⟫ = conj x * y :=
+theorem RCLike.inner_apply (x y : 𝕜) : ⟪x, y⟫ = y * conj x :=
   rfl
+
+/-- A version of `RCLike.inner_apply` that swaps the order of multiplication. -/
+theorem RCLike.inner_apply' (x y : 𝕜) : ⟪x, y⟫ = conj x * y := mul_comm _ _
 
 end RCLike
 
@@ -2272,13 +2275,13 @@ def InnerProductSpace.complexToReal [SeminormedAddCommGroup G] [InnerProductSpac
 instance : InnerProductSpace ℝ ℂ := InnerProductSpace.complexToReal
 
 @[simp]
-protected theorem Complex.inner (w z : ℂ) : ⟪w, z⟫_ℝ = (conj w * z).re :=
+protected theorem Complex.inner (w z : ℂ) : ⟪w, z⟫_ℝ = (z * conj w).re :=
   rfl
 
 /-- The inner product on an inner product space of dimension 2 can be evaluated in terms
 of a complex-number representation of the space. -/
 theorem inner_map_complex [SeminormedAddCommGroup G] [InnerProductSpace ℝ G] (f : G ≃ₗᵢ[ℝ] ℂ)
-    (x y : G) : ⟪x, y⟫_ℝ = (conj (f x) * f y).re := by rw [← Complex.inner, f.inner_map_map]
+    (x y : G) : ⟪x, y⟫_ℝ = (f y * conj (f x)).re := by rw [← Complex.inner, f.inner_map_map]
 
 instance : InnerProductSpace ℝ ℂ := InnerProductSpace.complexToReal
 
