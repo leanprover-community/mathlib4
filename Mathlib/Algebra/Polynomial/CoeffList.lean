544af456f3a84fbe920ed52059b18d34423dad17
Alex Meiburg <timeroot.alex@gmail.com>
2024-12-10 21:38:01 -0500

    Update to current Mathlib

--- Mathlib/Algebra/Polynomial/CoeffList.lean
+++ Mathlib/Algebra/Polynomial/CoeffList.lean
@@ -3,10 +3,11 @@ Copyright (c) 2023 Alex Meiburg. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Alex Meiburg
 -/
-import Mathlib.Data.Polynomial.Degree.Definitions
-import Mathlib.Data.Polynomial.RingDivision
-import Mathlib.Data.Polynomial.EraseLead
+import Mathlib.Algebra.Polynomial.Degree.Definitions
+import Mathlib.Algebra.Polynomial.RingDivision
+import Mathlib.Algebra.Polynomial.EraseLead
 import Mathlib.Data.List.Range
+import Mathlib.Data.List.GetD
 
 /-!
 # A list of coefficients of a polynomial
@@ -73,42 +74,31 @@ theorem length_coeffList (P : α[X]) :
 theorem coeffList_eraseLead_nz (h : P.nextCoeff ≠ 0) :
     coeffList P = P.leadingCoeff::P.eraseLead.coeffList := by
   have hd : P.natDegree = P.eraseLead.natDegree + 1 := eraseLead_natDegree_of_nextCoeff h
-  have hpz : P ≠ 0 := ne_zero_of_nz_nextCoeff h
-  simp [coeffList, hd, hpz, ne_zero_eraseLead_of_nz_nextCoeff h, List.range_succ]
+  simp only [coeffList, hd, ne_zero_eraseLead_of_nz_nextCoeff h,
+    ne_zero_of_natDegree_gt (natDegree_pos_of_nextCoeff_ne_zero h),
+    support_eq_empty, ↓reduceIte, List.range_succ, List.reverse_append, List.reverse_cons,
+    List.reverse_nil, List.nil_append, List.cons_append, List.map_cons, List.map_reverse,
+    List.cons.injEq, List.reverse_inj, List.map_inj_left, List.mem_range]
   constructor
-  { rw [← hd]; exact coeff_natDegree }
-  constructor
-  { rw [leadingCoeff]
-    apply Eq.symm
-    apply Polynomial.eraseLead_coeff_of_ne
-    linarith }
-  rw [List.map_reverse, List.map_reverse];
-  congr 1
-  rw [List.map_eq_map_iff]
-  intro n hn
-  rw [List.mem_range] at hn
-  apply Eq.symm
-  apply Polynomial.eraseLead_coeff_of_ne
-  linarith
+  · exact hd ▸ coeff_natDegree
+  · constructor <;> {
+      intros
+      symm
+      apply Polynomial.eraseLead_coeff_of_ne
+      linarith
+    }
 
 /- Coefficients of P are always the leading coefficient, some number of zeros, and then
   `coeffList P.eraseLead`. -/
 theorem coeffList_eraseLead (h : P≠0) : ∃(n:ℕ), coeffList P =
     P.leadingCoeff::((List.replicate n 0)++P.eraseLead.coeffList) := by
   by_cases hdp : P.natDegree = 0
-  case pos =>
-    use 0
+  · use 0
     rw [eq_C_of_natDegree_eq_zero hdp] at h ⊢
-    have hcnz := coeffList_C (C_ne_zero.mp h)
-    rw [hcnz]
-    simp
-  replace hdp := Nat.ne_zero_iff_zero_lt.mp hdp
+    simp [coeffList_C (C_ne_zero.mp h)]
   have hd : P.natDegree ≥ P.eraseLead.natDegree + 1 := by
-    have := eraseLead_natDegree_le P
-    have : P.eraseLead.natDegree + 1 ≤ (P.natDegree - 1) + 1 := (add_le_add_iff_right 1).mpr this
-    rwa [Nat.sub_add_cancel hdp] at this
-  obtain ⟨dd, hd⟩ := exists_add_of_le hd
-  rw [Nat.add_comm] at hd
+    linarith [eraseLead_natDegree_le P, Nat.sub_add_cancel (Nat.ne_zero_iff_zero_lt.mp hdp)]
+  replace ⟨dd, hd⟩ := exists_add_of_le hd
   use if P.eraseLead.support = ∅ then P.natDegree else dd
   --need α to be Inhabited to use get!, 0 is an accessible default
   inhabit α
@@ -123,129 +113,83 @@ theorem coeffList_eraseLead (h : P≠0) : ∃(n:ℕ), coeffList P =
     exact Nat.add_comm dd _ ▸ hd
   case pos => --contents are equal, P.eraseLead = 0
     simp only [if_pos hep, nonpos_iff_eq_zero, tsub_zero, List.get!_eq_getD]
-    intro n
-    cases n
-    case zero => --0th element is the same
-      obtain ⟨ls, hls⟩ := coeffList_eq_cons_leadingCoeff h
+    rintro (_|n1)
+    · --0th element is the same
+      rcases coeffList_eq_cons_leadingCoeff h
       simp_all
-    case succ n1 => --1st element on is the same
+    · --1st element is the same
       simp_rw [coeffList_nz h, coeffList, if_pos hep]
-      simp_all only [natDegree_zero, Fin.cast_mk, List.get_map,
-        List.append_nil, List.length_cons, Nat.sub_zero]
-      rw [List.map_reverse]
+      simp_all only [natDegree_zero, Fin.cast_mk, List.getElem_map,
+        List.append_nil, List.length_cons, Nat.sub_zero, List.map_reverse]
       by_cases hnp : n1 + 1 < P.natDegree + 1
       case pos =>
-        rw [List.getD_reverse]; swap
-        · rw [List.length_map, List.length_range]
-          exact hd ▸ hnp
-        rw [List.getD_cons_succ, List.getD, List.length_map, List.length_range, List.get?_map,
-            List.get?_range, Option.map_some', Option.getD_some, add_tsub_cancel_right,
-            List.getD_replicate_elem_eq]; swap
-        · exact (add_lt_add_iff_right 1).mp (hd ▸ hnp)
-        · obtain ⟨np, hnp⟩ := exists_add_of_le (Nat.le_of_lt_succ hnp)
-          have hnp2 : np = P.natDegree - (n1 + 1) :=
-            (Nat.sub_eq_of_eq_add (Nat.add_comm _ np ▸ hnp)).symm
-          have : coeff P.eraseLead np = coeff P np := by
-            apply eraseLead_coeff_of_ne np
-            linarith
-          rw [← hd, ← hnp2, ← this, support_eq_empty.mp hep, coeff_zero]
-        · rw [support_eq_empty.mp hep]
-          calc dd + 1 - (n1 + 1)
-            _ ≤ dd + 1 := Nat.sub_le (dd + 1) (n1 + 1)
-            _ < dd + 2 := (add_lt_add_iff_left dd).2 Nat.one_lt_two
+        rw [List.getD_reverse _ (by simpa [hd] using hnp ), List.getD_cons_succ,
+          List.getD_eq_getElem?_getD, List.length_map, List.length_range, List.getElem?_map,
+          List.getElem?_range (by omega), Option.map_some', Option.getD_some, add_tsub_cancel_right,
+          List.getD_replicate  _ (by omega)]
+        obtain ⟨np, hnp⟩ := exists_add_of_le (Nat.le_of_lt_succ hnp)
+        have h2 : np = P.natDegree - (n1 + 1) := by omega
+        rw [← hd, ← h2, ← eraseLead_coeff_of_ne np (by omega), support_eq_empty.mp hep, coeff_zero]
       case neg =>
         replace hnp := Nat.ge_of_not_lt hnp
         rw [List.getD_cons_succ, List.getD_eq_default, List.getD_eq_default]
         · simp_all
         · simp_all only [List.length_reverse, List.length_map, List.length_range]
   case neg => --contents are equal, P.eraseLead ≠ 0
-    simp [if_neg hep]
-    intro n
-    cases n
-    case zero => --0th element is the same
-      obtain ⟨ls, hls⟩ := coeffList_eq_cons_leadingCoeff h
-      simp_all [List.get]
-    case succ n1 => --1st element on is the same
-      simp_rw [coeffList_nz h, coeffList, if_neg hep, List.map_reverse]
-      by_cases hnp : n1 + 1 < P.natDegree + 1
+    simp only [List.get!_eq_getElem!, List.getElem!_eq_getElem?_getD, support_eq_empty, if_neg hep]
+    rintro (_|n)
+    · cases coeffList_eq_cons_leadingCoeff h
+      simp_all
+    · simp_rw [coeffList_nz h, coeffList, if_neg hep, List.map_reverse]
+      by_cases hnp : n + 1 < P.natDegree + 1
       case pos =>
-        obtain ⟨dp, hdp⟩ := exists_add_of_le (Nat.le_of_lt_succ hnp)
-        rw [List.getD_reverse, List.getD_cons_succ, List.getD, List.length_map,
-            List.length_range, List.get?_map, List.get?_range,  Option.map_some', Option.getD_some,
-            add_tsub_cancel_right]
-        case h => simpa
-        have : coeff P.eraseLead dp = coeff P dp := by
-          apply eraseLead_coeff_of_ne dp
-          linarith
-        rw [hdp, add_tsub_cancel_left, ← this]
-        by_cases hnp2 : n1 < dd
+        obtain ⟨dp, _⟩ := exists_add_of_le (Nat.le_of_lt_succ hnp)
+        rw [List.getElem?_reverse (by simpa using hnp), List.getElem?_cons_succ, List.length_map,
+          List.length_range, List.getElem?_map, List.getElem?_range (by omega), Option.map_some',
+          Option.getD_some]
+        conv_lhs => equals P.eraseLead.coeff dp =>
+          rw [eraseLead_coeff_of_ne (f := P) dp (by omega)]
+          congr
+          omega
+        by_cases hnp2 : n < dd
         case pos => --goes into the 0's chunk
-          rw [List.getD_append]
-          case h =>
-            rw [List.length_replicate]
-            exact hnp2
-          rw [List.getD_replicate_elem_eq]
-          case h => exact hnp2
-          apply coeff_eq_zero_of_natDegree_lt
-          linarith
+          simpa [List.getElem?_append, hnp2] using coeff_eq_zero_of_natDegree_lt (by linarith)
         case neg => --goes into the coeffList eraseLead chunk
-          obtain ⟨d3, hd3⟩ := exists_add_of_le (Nat.ge_of_not_lt hnp2)
-          rw [List.getD_append_right]
-          case h =>
-            rw [List.length_replicate]
-            exact Nat.le_of_not_gt hnp2
-          rw [List.length_replicate, List.getD_reverse, List.getD, List.get?_map]
-          case h =>
-            rw [List.length_map, List.length_range, hd3, add_tsub_cancel_left]
-            linarith
-          rw [List.length_map, List.length_range]
-          rw [List.get?_range, Option.map_some', Option.getD_some, add_tsub_cancel_right]
-          congr 1
-          rw [hd3, add_tsub_cancel_left]
-          rw [hd3] at hdp
-          rw [hdp] at hd
-          rw [← Nat.add_assoc, Nat.add_assoc, Nat.add_comm 1 _, ← Nat.add_assoc] at hd
-          replace hd := Nat.add_right_cancel hd
-          rw [Nat.add_assoc, Nat.add_comm d3 _] at hd
-          replace hd := Nat.add_left_cancel hd
-          exact (Nat.sub_eq_of_eq_add hd.symm).symm
-          rw [Nat.sub_sub]
-          apply Nat.sub_lt <;> simp
-        rw [add_tsub_cancel_right]
-        calc
-          P.natDegree - Nat.succ n1 ≤ P.natDegree := Nat.sub_le _ _
-          P.natDegree < P.natDegree + 1 := Nat.lt_succ_self _
+          obtain ⟨_, hd3⟩ := exists_add_of_le (Nat.ge_of_not_lt hnp2)
+          rw [List.getElem?_append_right (List.length_replicate _ _ ▸ Nat.le_of_not_gt hnp2),
+            List.length_replicate, List.getElem?_reverse, List.getElem?_map]
+          · rw [List.length_map, List.length_range, List.getElem?_range (by omega), hd3,
+              Option.map_some', Option.getD_some, add_tsub_cancel_right]
+            congr 1
+            omega
+          · rw [List.length_map, List.length_range, hd3]
+            omega
       case neg =>
-        replace hnp := Nat.ge_of_not_lt hnp
-        simp only [List.getD_cons_succ]
-        rw [List.getD_eq_default, List.getD_eq_default]
-        simp_all
-        simp_all only [List.length_reverse, List.length_map, List.length_range]
+        rw [List.getElem?_cons_succ, ← List.getD_eq_getElem?_getD, ← List.getD_eq_getElem?_getD,
+          List.getD_eq_default, List.getD_eq_default]
+        · simpa using by omega
+        · simpa using hnp
 
 variable {α : Type*} [Ring α] (P : Polynomial α)
 
 /-- The coefficient list is negated if the polynomial is negated. --/
-theorem coeffList_neg : (-P).coeffList = P.coeffList.map (λx↦-x) := by
+theorem coeffList_neg : (-P).coeffList = P.coeffList.map (-·) := by
   by_cases hp : P = 0
   · rw [coeffList_of_zero hp, coeffList_of_zero (neg_eq_zero.mpr hp), List.map_nil]
   · rw [coeffList_nz hp, coeffList_nz (mt neg_eq_zero.mp hp : ¬(-P = 0)),
       natDegree_neg, List.map_map]
-    congr; funext; simp
+    simp
 
 variable {α : Type*} [DivisionSemiring α] (P : Polynomial α)
 
 /-- Over a division semiring, multiplying a polynomial by a nonzero constant multiplies
   the coefficient list. -/
 theorem coeffList_mul_C {η : α} (hη : η ≠ 0) :
-    coeffList (C η * P) = P.coeffList.map (λx↦η*x) := by
+    coeffList (C η * P) = P.coeffList.map (η * ·) := by
   by_cases hp : P = 0
-  case pos => simp only [hp, mul_zero, coeffList_zero, List.map_nil]
-  have hcη : C η * P ≠ 0 := mul_ne_zero (mt (map_eq_zero _).mp hη) hp
-  rw [coeffList_nz hcη, coeffList_nz hp]
-  rw [natDegree_mul_of_nonzero, List.map_map]
-  congr
-  funext n
-  simp
-  exact hη
+  · simp only [hp, mul_zero, coeffList_zero, List.map_nil]
+  · have hcη : C η * P ≠ 0 := mul_ne_zero (mt (map_eq_zero _).mp hη) hp
+    rw [coeffList_nz hcη, coeffList_nz hp, natDegree_mul_of_nonzero hη, List.map_map]
+    simp
 
 end Polynomial
