910487bf5fbaf26d169cf7096b5e1136b5ab49c4
Christopher Hoskin <christopher.hoskin@gmail.com>
2024-12-12 19:30:17 +0000

    Fix

--- Mathlib/LinearAlgebra/RootSystem/OfBilinear.lean
+++ Mathlib/LinearAlgebra/RootSystem/OfBilinear.lean
@@ -172,8 +172,8 @@ def ofBilinear [IsReflexive R M] (B : M →ₗ[R] M →ₗ[R] R) (hNB : LinearMa
     simp only [mem_setOf_eq, PerfectPairing.flip_apply_apply, mul_sub,
       apply_self_mul_coroot_apply B y.2, ← mul_assoc]
     rw [← isOrthogonal_reflection B x.2 hSB y y, apply_self_mul_coroot_apply, ← hSB z, ← hSB z,
-      RingHom.id_apply, RingHom.id_apply, Module.reflection_apply, map_sub,
-      mul_sub, sub_eq_sub_iff_comm, sub_left_inj]
+      RingHom.coe_addMonoidHom_id, AddMonoidHom.id_apply, AddMonoidHom.id_apply,
+      Module.reflection_apply, map_sub, mul_sub, sub_eq_sub_iff_comm, sub_left_inj]
     refine x.2.1.1 ?_
     simp only [mem_setOf_eq, map_smul, smul_eq_mul]
     rw [← mul_assoc _ _ (B z x), ← mul_assoc _ _ (B z x), mul_left_comm,
