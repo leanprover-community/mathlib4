e45308db64305989f58a8c5059cbdd3596631489
Joël Riou <joel.riou@universite-paris-saclay.fr>
2024-12-13 16:09:05 +0100

    cleaning up

--- Mathlib/CategoryTheory/Presentable/Basic.lean
+++ Mathlib/CategoryTheory/Presentable/Basic.lean
@@ -4,10 +4,21 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joël Riou
 -/
 import Mathlib.CategoryTheory.Limits.Filtered
+import Mathlib.CategoryTheory.Limits.Preserves.Basic
 import Mathlib.SetTheory.Cardinal.Cofinality
 import Mathlib.Data.Set.Finite.Basic
 
 /-! # Presentable objects
+
+If `κ` is a regular cardinal, we shall say that a preordered type `J`
+is `κ`-directed (`IsCardinalDirected`) if any subset of `J` of
+cardinality `< κ` has an upper bound.
+
+A functor `F : C ⥤ D` is `κ`-accessible (`Functor.IsAccessible`)
+if it commutes with colimits of shape `J` where `J` is any `κ`-directed preordered type.
+
+An object `X` of a category is `κ`-presentable (`IsPresentable`)
+if the functor `Hom(X, _)` (i.e. `coyoneda.obj (op X)`) is `κ`-accessible.
 
 -/
 
@@ -19,9 +30,10 @@ open Limits Opposite
 
 section
 
-variable (J : Type w) [Preorder J]
-  (κ : Cardinal.{w})
+variable (J : Type w) [Preorder J] (κ : Cardinal.{w})
 
+/-- A preorder `J` is `κ`-directed (when `κ` is regular cardinal),
+if any subset of `J` of cardinality `< κ` has an upper bound. -/
 class IsCardinalDirected [Fact κ.IsRegular] : Prop where
   exists_upper_bound (S : Set J) (hS : Cardinal.mk S < κ) :
     ∃ (j : J), ∀ (s : S), s.1 ≤ j
@@ -34,6 +46,8 @@ section
 
 variable {J κ} (S : Set J) (hS : Cardinal.mk S < κ)
 
+/-- A choice of upper bound for a subset `S : Set J`
+of cardinality `< κ`, when `J` is `κ`-directed. -/
 noncomputable def upperBound : J :=
   (IsCardinalDirected.exists_upper_bound S hS).choose
 
@@ -65,22 +79,35 @@ end IsCardinalDirected
 
 end
 
-variable {C : Type u} [Category.{v} C]
+variable {C : Type u} [Category.{v} C] {D : Type u'} [Category.{v'} D]
 
-def coyonedaColimitComparison (X : C) {J : Type u'} [Category.{v'} J] {F : J ⥤ C}
-    (c : Cocone F) :
-    Types.Quot (F ⋙ coyoneda.obj (op X)) → (X ⟶ c.pt) :=
-  Quot.lift (fun ⟨j, f⟩ ↦ f ≫ c.ι.app j) (by
-    rintro ⟨j, f⟩ ⟨j', f'⟩ ⟨g, fac⟩
-    dsimp at f f' g fac
-    subst fac
-    simp)
+namespace Functor
+
+variable (F : C ⥤ D) (κ : Cardinal.{w}) [Fact κ.IsRegular]
+
+/-- A functor is `κ`-accessible (with `κ` a regular cardinal)
+if it preserves colimits of shape `J` where `J` is any `κ`-directed preorder. -/
+class IsAccessible : Prop where
+  preservesColimitOfShape {J : Type w} [Preorder J] [IsCardinalDirected J κ] :
+    PreservesColimitsOfShape J F
+
+lemma preservesColimitsOfShape_of_isAccessible [F.IsAccessible κ]
+    (J : Type w) [Preorder J] [IsCardinalDirected J κ] :
+    PreservesColimitsOfShape J F :=
+  IsAccessible.preservesColimitOfShape κ
+
+end Functor
 
 variable (X : C) (κ : Cardinal.{w}) [Fact κ.IsRegular]
 
-class IsPresentable : Prop where
-  commutes {J : Type w} [Preorder J] [IsCardinalDirected J κ]
-    {F : J ⥤ C} (c : Cocone F) (hc : IsColimit c) :
-    Function.Bijective (coyonedaColimitComparison X c)
+/-- An object `X` in a category is `κ`-presentable (for `κ` a regular cardinal)
+when the functor `Hom(X, _)` preserves colimits indexed by
+`κ`-directed preordered sets. -/
+abbrev IsPresentable : Prop := (coyoneda.obj (op X)).IsAccessible κ
+
+lemma preservesColimitsOfShape_of_isPresentable [IsPresentable X κ]
+    (J : Type w) [Preorder J] [IsCardinalDirected J κ] :
+    PreservesColimitsOfShape J (coyoneda.obj (op X)) :=
+  (coyoneda.obj (op X)).preservesColimitsOfShape_of_isAccessible κ J
 
 end CategoryTheory
