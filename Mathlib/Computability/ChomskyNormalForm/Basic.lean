3417eaf630683c479cbbcf2d51279867115058d8
AlexLoitzl <alexander.loitzl@gmail.com>
2024-12-13 12:53:57 +0100

    Add DerivesIn to CFG and CNF and toCFG

--- Mathlib/Computability/ChomskyNormalForm/Basic.lean
+++ Mathlib/Computability/ChomskyNormalForm/Basic.lean
@@ -101,6 +101,24 @@ lemma Rewrites.append_right {r : ChomskyNormalFormRule T N} {u v : List (Symbol 
     (huv : r.Rewrites u v) (p : List (Symbol T N)) : r.Rewrites (u ++ p) (v ++ p) := by
   induction huv <;> tauto
 
+/-- Translation of `ChomskyNormalFormRule` to `ContextFreeRule` -/
+def toCFGRule (r : ChomskyNormalFormRule T N) : ContextFreeRule T N :=
+  match r with
+  | leaf n t => ⟨n, [Symbol.terminal t]⟩
+  | node nᵢ n₁ n₂ => ⟨nᵢ, [Symbol.nonterminal n₁, Symbol.nonterminal n₂]⟩
+
+lemma Rewrites.toCFGRule_match {u v : List (Symbol T N)} {r : ChomskyNormalFormRule T N}
+    (huv : r.Rewrites u v) :
+    r.toCFGRule.Rewrites u v := by
+  induction huv <;> tauto
+
+lemma Rewrites.match_toCFGRule {u v : List (Symbol T N)} {r : ChomskyNormalFormRule T N}
+    (huv : r.toCFGRule.Rewrites u v) :
+    r.Rewrites u v := by
+  induction huv with
+  | head => cases r <;> tauto
+  | cons s _ ih => exact Rewrites.cons r s ih
+
 end ChomskyNormalFormRule
 
 namespace ChomskyNormalFormGrammar
@@ -201,5 +219,259 @@ theorem Derives.head_induction_on {v : List (Symbol T g.NT)} {P : ∀ u, g.Deriv
     (head : ∀ {u w} (huw : g.Produces u w) (hwv : g.Derives w v), P w hwv → P u (hwv.head huw)) :
     P u huv :=
   Relation.ReflTransGen.head_induction_on huv refl head
+
+/-! `ChomskyNormalFormGrammar` to `ContextFreeGrammar` translation and related properties -/
+section toCFG
+
+variable [DecidableEq T]
+
+/-- Translation of `ChomskyNormalFormGrammar` to `ContextFreeGrammar` -/
+noncomputable def toCFG (g : ChomskyNormalFormGrammar T) [DecidableEq g.NT] :
+    ContextFreeGrammar T where
+  NT := g.NT
+  initial := g.initial
+  rules := (g.rules.toList.map ChomskyNormalFormRule.toCFGRule).toFinset
+
+variable {g : ChomskyNormalFormGrammar T} [DecidableEq g.NT]
+
+lemma Produces.toCFG_match {u v : List (Symbol T g.NT)} (huv : g.Produces u v) :
+    g.toCFG.Produces u v := by
+  rcases huv with ⟨r, rin, hrw⟩
+  use r.toCFGRule
+  constructor
+  · simp only [toCFG, List.mem_toFinset, List.mem_map, Finset.mem_toList]
+    use r
+  · exact ChomskyNormalFormRule.Rewrites.toCFGRule_match hrw
+
+lemma Derives.toCFG_match {u v : List (Symbol T g.NT)} (huv : g.Derives u v) :
+    g.toCFG.Derives u v := by
+  induction huv with
+  | refl => rfl
+  | tail _ hp ih =>
+    apply ih.trans_produces
+    exact Produces.toCFG_match hp
+
+lemma Generates.toCFG_match {u : List (Symbol T g.NT)} (hu : g.Generates u) : g.toCFG.Generates u :=
+  Derives.toCFG_match hu
+
+lemma Produces.match_toCFG {u v : List (Symbol T g.NT)} (huv : g.toCFG.Produces u v) :
+    g.Produces u v := by
+  rcases huv with ⟨r, hrg, huv⟩
+  simp only [toCFG, List.mem_map] at hrg
+  rw [List.mem_toFinset] at hrg
+  obtain ⟨r', hrg', rfl⟩ := List.mem_map.1 hrg
+  exact ⟨r', Finset.mem_toList.1 hrg', ChomskyNormalFormRule.Rewrites.match_toCFGRule huv⟩
+
+lemma Derives.match_toCFG {u v : List (Symbol T g.NT)} (huv : g.toCFG.Derives u v) :
+    g.Derives u v := by
+  induction huv with
+  | refl => rfl
+  | tail _ hp ih => exact ih.trans_produces (Produces.match_toCFG hp)
+
+lemma Generates.match_toCFG {u : List (Symbol T g.NT)} (hu : g.toCFG.Generates u) : g.Generates u :=
+  Derives.match_toCFG hu
+
+theorem toCFG_correct {u : List (Symbol T g.NT)} : g.Generates u ↔ g.toCFG.Generates u :=
+  ⟨Generates.toCFG_match, Generates.match_toCFG⟩
+
+end toCFG
+
+/-! Alternative definition of `Derives` which allows to use well-founded induction on derivations,
+by explicitely counting the number of steps of the transformation -/
+section derivesIn
+/-- Given a context-free grammar `g`, strings `u` and `v`, and number `n`
+`g.DerivesIn u v n` means that `g` can transform `u` to `v` in `n` rewriting steps. -/
+inductive DerivesIn (g : ChomskyNormalFormGrammar.{uN} T) :
+    List (Symbol T g.NT) → List (Symbol T g.NT) → ℕ → Prop
+  /-- 0 steps entail no transformation -/
+  | refl (w : List (Symbol T g.NT)) : g.DerivesIn w w 0
+  /-- n + 1 steps, if transforms `u` to `v` in n steps, and `v` to `w` in 1 step  -/
+  | tail (u v w : List (Symbol T g.NT)) (n : ℕ) :
+    g.DerivesIn u v n → g.Produces v w → g.DerivesIn u w n.succ
+
+lemma derives_iff_derivesIn (g : ChomskyNormalFormGrammar.{uN} T) (v w : List (Symbol T g.NT)) :
+    g.Derives v w ↔ ∃ n : ℕ, g.DerivesIn v w n := by
+  constructor
+  · intro hgvw
+    induction hgvw with
+    | refl =>
+      use 0
+      left
+    | tail _ last ih =>
+      obtain ⟨n, ihn⟩ := ih
+      use n.succ
+      right
+      · exact ihn
+      · exact last
+  · intro ⟨n, hgvwn⟩
+    induction hgvwn with
+    | refl => rfl
+    | tail _ _ _ _ last ih => exact ih.trans_produces last
+
+lemma mem_language_iff_derivesIn (g : ChomskyNormalFormGrammar.{uN} T) (w : List T) :
+    w ∈ g.language ↔ ∃ n, g.DerivesIn [Symbol.nonterminal g.initial] (w.map Symbol.terminal) n := by
+  rw [mem_language_iff]
+  exact derives_iff_derivesIn g _ _
+
+variable {g : ChomskyNormalFormGrammar.{uN} T}
+
+lemma DerivesIn.zero_steps (w : List (Symbol T g.NT)) : g.DerivesIn w w 0 := by
+  left
+
+lemma DerivesIn.zero_steps_eq {u v : List (Symbol T g.NT)} (huv: g.DerivesIn u v 0) :
+    u = v:= by
+  cases huv
+  rfl
+
+lemma Produces.single_step {v w : List (Symbol T g.NT)} (hvw : g.Produces v w) :
+    g.DerivesIn v w 1 := by
+  right
+  · left
+  · exact hvw
+
+variable {n : ℕ}
+
+lemma DerivesIn.trans_produces {u v w : List (Symbol T g.NT)}
+    (huv : g.DerivesIn u v n) (hvw : g.Produces v w) :
+    g.DerivesIn u w n.succ :=
+  DerivesIn.tail u v w n huv hvw
+
+@[trans]
+lemma DerivesIn.trans {u v w : List (Symbol T g.NT)} {m : ℕ}
+    (huv : g.DerivesIn u v n) (hvw : g.DerivesIn v w m) :
+    g.DerivesIn u w (n + m) := by
+  induction hvw with
+  | refl => exact huv
+  | tail _ _ _ _ last ih => exact trans_produces ih last
+
+lemma Produces.trans_derivesIn {u v w : List (Symbol T g.NT)}
+    (huv : g.Produces u v) (hvw : g.DerivesIn v w n) :
+    g.DerivesIn u w n.succ :=
+  n.succ_eq_one_add ▸ huv.single_step.trans hvw
+
+lemma DerivesIn.tail_of_succ {u w : List (Symbol T g.NT)} (huw : g.DerivesIn u w n.succ) :
+    ∃ v : List (Symbol T g.NT), g.DerivesIn u v n ∧ g.Produces v w := by
+  cases huw with
+  | tail v w n huv hvw =>
+    use v
+
+lemma DerivesIn.head_of_succ {u w : List (Symbol T g.NT)} (huw : g.DerivesIn u w n.succ) :
+    ∃ v : List (Symbol T g.NT), g.Produces u v ∧ g.DerivesIn v w n := by
+  induction n generalizing w with
+  | zero =>
+    cases huw with
+    | tail v w n huv hvw =>
+      cases huv with
+      | refl => exact ⟨w, hvw, zero_steps w⟩
+  | succ m ih =>
+    cases huw with
+    | tail v w n huv hvw =>
+      obtain ⟨x, hux, hxv⟩ := ih huv
+      exact ⟨x, hux, hxv.trans_produces hvw⟩
+
+/-- Add extra prefix to context-free deriving (number of steps unchanged). -/
+lemma DerivesIn.append_left {v w : List (Symbol T g.NT)}
+    (hvw : g.DerivesIn v w n) (p : List (Symbol T g.NT)) :
+    g.DerivesIn (p ++ v) (p ++ w) n := by
+  induction hvw with
+  | refl => left
+  | tail _ _ _ _ last ih => exact ih.trans_produces <| last.append_left p
+
+/-- Add extra postfix to context-free deriving (number of steps unchanged). -/
+lemma DerivesIn.append_right {v w : List (Symbol T g.NT)}
+    (hvw : g.DerivesIn v w n) (p : List (Symbol T g.NT)) :
+    g.DerivesIn (v ++ p) (w ++ p) n := by
+  induction hvw with
+  | refl => left
+  | tail _ _ _ _ last ih => exact ih.trans_produces <| last.append_right p
+
+lemma DerivesIn.append_split {p q w : List (Symbol T g.NT)} {n : ℕ}
+    (hpqw : g.DerivesIn (p ++ q) w n) :
+    ∃ x y m₁ m₂, w = x ++ y ∧ g.DerivesIn p x m₁ ∧ g.DerivesIn q y m₂ ∧ n = m₁ + m₂ := by
+  cases n with
+  | zero =>
+    cases hpqw
+    exact ⟨p, q, 0, 0, rfl, DerivesIn.refl p, DerivesIn.refl q, rfl⟩
+  | succ n =>
+    obtain ⟨v, hp, hd⟩ := hpqw.head_of_succ
+    obtain ⟨r, hrg, hr⟩ := hp
+    obtain ⟨p', q', heq, hv⟩ := hr.exists_parts
+    rw [List.append_assoc, List.singleton_append] at heq
+    have append_eq_append_cons {p q x y : List (Symbol T g.NT)} {v : Symbol T g.NT}
+        (hpqxvy : p ++ q = x ++ v :: y) :
+        (∃ w, y = w ++ q ∧ p = x ++ v :: w) ∨ (∃ w, x = p ++ w ∧ q = w ++ v :: y) := by
+      rw [List.append_eq_append_iff] at hpqxvy
+      cases hpqxvy with
+      | inl hxq =>
+        obtain ⟨a, _, hq⟩ := hxq
+        right
+        use a
+      | inr hpy =>
+        obtain ⟨a, rfl, hq⟩ := hpy
+        cases a with
+        | nil =>
+          right
+          use []
+          rw [hq]
+          constructor
+          · repeat rw [List.append_nil]
+          · rfl
+        | cons d l =>
+          left
+          use l
+          rw [List.cons_append, List.cons.injEq] at hq
+          rw [hq.1]
+          exact ⟨hq.2, rfl⟩
+    rcases append_eq_append_cons heq with ⟨a, hq', hp⟩ | ⟨a, hp', hq⟩
+    · rw [hv, hq', ← List.append_assoc] at hd
+      obtain ⟨x, y, m₁, m₂, hw, hd₁, hd₂, hn⟩ := hd.append_split
+      use x, y, (m₁ + 1), m₂
+      constructor
+      · exact hw
+      · constructor
+        · apply Produces.trans_derivesIn
+          · use r
+            constructor
+            · exact hrg
+            · rw [hp, ← List.singleton_append, ← List.append_assoc]
+              apply r.rewrites_of_exists_parts
+          · exact hd₁
+        · exact ⟨hd₂, by omega⟩
+    · rw [hv, hp', List.append_assoc, List.append_assoc] at hd
+      obtain ⟨x, y, m₁, m₂, hw, hd₁, hd₂, hn⟩ := hd.append_split
+      use x, y, m₁, m₂ + 1, hw, hd₁
+      constructor
+      · apply Produces.trans_derivesIn
+        · use r, hrg
+          rw [hq, ← List.singleton_append, ← List.append_assoc]
+          apply r.rewrites_of_exists_parts
+        · rwa [List.append_assoc]
+      · omega
+
+lemma DerivesIn.three_split {p q r w : List (Symbol T g.NT)} {n : ℕ}
+    (hg : g.DerivesIn (p ++ q ++ r) w n) :
+  ∃ x y z m₁ m₂ m₃, w = x ++ y ++ z ∧ g.DerivesIn p x m₁ ∧ g.DerivesIn q y m₂
+    ∧ g.DerivesIn r z m₃ ∧ n = m₁ + m₂ + m₃ := by
+  obtain ⟨x', z, m₁', m₃, hw₂, hd₁', hd₃, hn₂⟩ := hg.append_split
+  obtain ⟨x, y, m₁, m₂, hw₁, hd₁, hd₂, hn₁⟩ := hd₁'.append_split
+  exact ⟨x, y, z, m₁, m₂, m₃, hw₁ ▸ hw₂, hd₁, hd₂, hd₃, hn₁ ▸ hn₂⟩
+
+@[elab_as_elim]
+lemma DerivesIn.head_induction_on {b : List (Symbol T g.NT)}
+    {P : ∀ n : ℕ, ∀ a : List (Symbol T g.NT), g.DerivesIn a b n → Prop}
+    (refl : P 0 b (DerivesIn.zero_steps b))
+    (head : ∀ {n a c} (hac : g.Produces a c) (hcb : g.DerivesIn c b n),
+      P n c hcb → P n.succ a (hac.trans_derivesIn hcb))
+    {a : List (Symbol T g.NT)} (hab : g.DerivesIn a b n) :
+    P n a hab := by
+  induction hab with
+  | refl => exact refl
+  | tail _ _ _ _ last ih =>
+    apply ih
+    · exact head last _ refl
+    · intro _ _ _ produc deriv
+      exact head produc (deriv.tail _ _ _ _ last)
+
+end derivesIn
 
 end ChomskyNormalFormGrammar
--- Mathlib/Computability/ContextFreeGrammar.lean
+++ Mathlib/Computability/ContextFreeGrammar.lean
@@ -29,6 +29,7 @@ structure ContextFreeRule (T : Type uT) (N : Type uN) where
   input : N
   /-- Output string a.k.a. right-hand side. -/
   output : List (Symbol T N)
+deriving DecidableEq
 
 /-- Context-free grammar that generates words over the alphabet `T` (a type of terminals). -/
 structure ContextFreeGrammar.{uN,uT} (T : Type uT) where
@@ -299,3 +300,200 @@ theorem Language.IsContextFree.reverse (L : Language T) :
     L.IsContextFree → L.reverse.IsContextFree := by rintro ⟨g, rfl⟩; exact ⟨g.reverse, by simp⟩
 
 end closure_reversal
+
+/-! Alternative definition of `Derives` which allows to use well-founded induction on derivations,
+by explicitely counting the number of steps of the transformation -/
+section derivesIn
+
+namespace ContextFreeGrammar
+
+/-- Given a context-free grammar `g`, strings `u` and `v`, and number `n`
+`g.DerivesIn u v n` means that `g` can transform `u` to `v` in `n` rewriting steps. -/
+inductive DerivesIn (g : ContextFreeGrammar.{uN} T) :
+    List (Symbol T g.NT) → List (Symbol T g.NT) → ℕ → Prop
+  /-- 0 steps entail no transformation -/
+  | refl (w : List (Symbol T g.NT)) : g.DerivesIn w w 0
+  /-- n + 1 steps, if transforms `u` to `v` in n steps, and `v` to `w` in 1 step  -/
+  | tail (u v w : List (Symbol T g.NT)) (n : ℕ) :
+    g.DerivesIn u v n → g.Produces v w → g.DerivesIn u w n.succ
+
+lemma derives_iff_derivesIn (g : ContextFreeGrammar.{uN} T) (v w : List (Symbol T g.NT)) :
+    g.Derives v w ↔ ∃ n : ℕ, g.DerivesIn v w n := by
+  constructor
+  · intro hgvw
+    induction hgvw with
+    | refl =>
+      use 0
+      left
+    | tail _ last ih =>
+      obtain ⟨n, ihn⟩ := ih
+      use n.succ
+      right
+      · exact ihn
+      · exact last
+  · intro ⟨n, hgvwn⟩
+    induction hgvwn with
+    | refl => rfl
+    | tail _ _ _ _ last ih => exact ih.trans_produces last
+
+lemma mem_language_iff_derivesIn (g : ContextFreeGrammar.{uN} T) (w : List T) :
+    w ∈ g.language ↔ ∃ n, g.DerivesIn [Symbol.nonterminal g.initial] (w.map Symbol.terminal) n := by
+  rw [mem_language_iff, derives_iff_derivesIn]
+
+variable {g : ContextFreeGrammar.{uN} T}
+
+lemma DerivesIn.zero_steps (w : List (Symbol T g.NT)) : g.DerivesIn w w 0 := by
+  left
+
+lemma Produces.single_step {v w : List (Symbol T g.NT)} (hvw : g.Produces v w) :
+    g.DerivesIn v w 1 := by
+  right
+  · left
+  · exact hvw
+
+variable {n : ℕ}
+
+lemma DerivesIn.trans_produces {u v w : List (Symbol T g.NT)}
+    (huv : g.DerivesIn u v n) (hvw : g.Produces v w) :
+    g.DerivesIn u w n.succ :=
+  DerivesIn.tail u v w n huv hvw
+
+@[trans]
+lemma DerivesIn.trans {u v w : List (Symbol T g.NT)} {m : ℕ}
+    (huv : g.DerivesIn u v n) (hvw : g.DerivesIn v w m) :
+    g.DerivesIn u w (n + m) := by
+  induction hvw with
+  | refl => exact huv
+  | tail _ _ _ _ last ih => exact trans_produces ih last
+
+lemma Produces.trans_derivesIn {u v w : List (Symbol T g.NT)}
+    (huv : g.Produces u v) (hvw : g.DerivesIn v w n) :
+    g.DerivesIn u w n.succ :=
+  n.succ_eq_one_add ▸ huv.single_step.trans hvw
+
+lemma DerivesIn.tail_of_succ {u w : List (Symbol T g.NT)} (huw : g.DerivesIn u w n.succ) :
+    ∃ v : List (Symbol T g.NT), g.DerivesIn u v n ∧ g.Produces v w := by
+  cases huw with
+  | tail v w n huv hvw =>
+    use v
+
+lemma DerivesIn.head_of_succ {u w : List (Symbol T g.NT)} (huw : g.DerivesIn u w n.succ) :
+    ∃ v : List (Symbol T g.NT), g.Produces u v ∧ g.DerivesIn v w n := by
+  induction n generalizing w with
+  | zero =>
+    cases huw with
+    | tail v w n huv hvw =>
+      cases huv with
+      | refl => exact ⟨w, hvw, zero_steps w⟩
+  | succ m ih =>
+    cases huw with
+    | tail v w n huv hvw =>
+      obtain ⟨x, hux, hxv⟩ := ih huv
+      exact ⟨x, hux, hxv.trans_produces hvw⟩
+
+/-- Add extra prefix to context-free deriving (number of steps unchanged). -/
+lemma DerivesIn.append_left {v w : List (Symbol T g.NT)}
+    (hvw : g.DerivesIn v w n) (p : List (Symbol T g.NT)) :
+    g.DerivesIn (p ++ v) (p ++ w) n := by
+  induction hvw with
+  | refl => left
+  | tail _ _ _ _ last ih => exact ih.trans_produces <| last.append_left p
+
+/-- Add extra postfix to context-free deriving (number of steps unchanged). -/
+lemma DerivesIn.append_right {v w : List (Symbol T g.NT)}
+    (hvw : g.DerivesIn v w n) (p : List (Symbol T g.NT)) :
+    g.DerivesIn (v ++ p) (w ++ p) n := by
+  induction hvw with
+  | refl => left
+  | tail _ _ _ _ last ih => exact ih.trans_produces <| last.append_right p
+
+lemma DerivesIn.append_split {p q w : List (Symbol T g.NT)} {n : ℕ}
+    (hpqw : g.DerivesIn (p ++ q) w n) :
+    ∃ x y m₁ m₂, w = x ++ y ∧ g.DerivesIn p x m₁ ∧ g.DerivesIn q y m₂ ∧ n = m₁ + m₂ := by
+  cases n with
+  | zero =>
+    cases hpqw
+    exact ⟨p, q, 0, 0, rfl, DerivesIn.refl p, DerivesIn.refl q, rfl⟩
+  | succ n =>
+    obtain ⟨v, hp, hd⟩ := hpqw.head_of_succ
+    obtain ⟨r, hrg, hr⟩ := hp
+    obtain ⟨p', q', heq, hv⟩ := hr.exists_parts
+    rw [List.append_assoc, List.singleton_append] at heq
+    have append_eq_append_cons {p q x y : List (Symbol T g.NT)} {v : Symbol T g.NT}
+        (hpqxvy : p ++ q = x ++ v :: y) :
+        (∃ w, y = w ++ q ∧ p = x ++ v :: w) ∨ (∃ w, x = p ++ w ∧ q = w ++ v :: y) := by
+      rw [List.append_eq_append_iff] at hpqxvy
+      cases hpqxvy with
+      | inl hxq =>
+        obtain ⟨a, _, hq⟩ := hxq
+        right
+        use a
+      | inr hpy =>
+        obtain ⟨a, rfl, hq⟩ := hpy
+        cases a with
+        | nil =>
+          right
+          use []
+          rw [hq, List.append_nil, List.append_nil]
+          exact ⟨rfl, rfl⟩
+        | cons d l =>
+          left
+          use l
+          rw [List.cons_append, List.cons.injEq] at hq
+          rw [hq.1]
+          exact ⟨hq.2, rfl⟩
+    rcases append_eq_append_cons heq with ⟨a, hq', hp⟩ | ⟨a, hp', hq⟩
+    · rw [hv, hq', ← List.append_assoc] at hd
+      obtain ⟨x, y, m₁, m₂, hw, hd₁, hd₂, hn⟩ := hd.append_split
+      use x, y, (m₁ + 1), m₂
+      constructor
+      · exact hw
+      · constructor
+        · apply Produces.trans_derivesIn
+          · use r
+            constructor
+            · exact hrg
+            · rw [hp, ← List.singleton_append, ← List.append_assoc]
+              apply r.rewrites_of_exists_parts
+          · exact hd₁
+        · exact ⟨hd₂, by omega⟩
+    · rw [hv, hp', List.append_assoc, List.append_assoc] at hd
+      obtain ⟨x, y, m₁, m₂, hw, hd₁, hd₂, hn⟩ := hd.append_split
+      use x, y, m₁, m₂ + 1, hw, hd₁
+      constructor
+      · apply Produces.trans_derivesIn
+        · use r
+          constructor
+          · exact hrg
+          · rw [hq, ← List.singleton_append, ← List.append_assoc]
+            apply r.rewrites_of_exists_parts
+        · rwa [List.append_assoc]
+      · omega
+
+lemma DerivesIn.three_split {p q r w : List (Symbol T g.NT)} {n : ℕ}
+    (hg : g.DerivesIn (p ++ q ++ r) w n) :
+  ∃ x y z m₁ m₂ m₃, w = x ++ y ++ z ∧ g.DerivesIn p x m₁ ∧ g.DerivesIn q y m₂
+    ∧ g.DerivesIn r z m₃ ∧ n = m₁ + m₂ + m₃ := by
+  obtain ⟨x', z, m₁', m₃, hw₂, hd₁', hd₃, hn₂⟩ := hg.append_split
+  obtain ⟨x, y, m₁, m₂, hw₁, hd₁, hd₂, hn₁⟩ := hd₁'.append_split
+  exact ⟨x, y, z, m₁, m₂, m₃, hw₁ ▸ hw₂, hd₁, hd₂, hd₃, hn₁ ▸ hn₂⟩
+
+@[elab_as_elim]
+lemma DerivesIn.head_induction_on {b : List (Symbol T g.NT)}
+    {P : ∀ n : ℕ, ∀ a : List (Symbol T g.NT), g.DerivesIn a b n → Prop}
+    (refl : P 0 b (DerivesIn.zero_steps b))
+    (head : ∀ {n a c} (hac : g.Produces a c) (hcb : g.DerivesIn c b n),
+      P n c hcb → P n.succ a (hac.trans_derivesIn hcb))
+    {a : List (Symbol T g.NT)} (hab : g.DerivesIn a b n) :
+    P n a hab := by
+  induction hab with
+  | refl => exact refl
+  | tail _ _ _ _ last ih =>
+    apply ih
+    · exact head last _ refl
+    · intro _ _ _ produc deriv
+      exact head produc (deriv.tail _ _ _ _ last)
+
+end ContextFreeGrammar
+
+end derivesIn
