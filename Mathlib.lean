d48b4f3a36b9eea1d8e60f969a3a9ea719ad7c28
Joël Riou <joel.riou@universite-paris-saclay.fr>
2024-12-13 19:11:43 +0100

    cardinality of Arrow

--- Mathlib.lean
+++ Mathlib.lean
@@ -1669,6 +1669,7 @@ import Mathlib.CategoryTheory.CofilteredSystem
 import Mathlib.CategoryTheory.CommSq
 import Mathlib.CategoryTheory.Comma.Arrow
 import Mathlib.CategoryTheory.Comma.Basic
+import Mathlib.CategoryTheory.Comma.CardinalArrow
 import Mathlib.CategoryTheory.Comma.Final
 import Mathlib.CategoryTheory.Comma.Over
 import Mathlib.CategoryTheory.Comma.OverClass
