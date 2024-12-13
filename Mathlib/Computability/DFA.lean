6561cc515cf043acc130a09ed1320515c9d488b9
Chris Wong <lambda.fairy@gmail.com>
2024-12-13 15:46:17 +1100

    Delete duplicate author

--- Mathlib/Computability/DFA.lean
+++ Mathlib/Computability/DFA.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2020 Fox Thomson. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Fox Thomson, Chris Wong, Chris Wong
+Authors: Fox Thomson, Chris Wong
 -/
 import Mathlib.Computability.Language
 import Mathlib.Data.Countable.Small
