bb23c4bfdcf90bb957b1a97de1d08f607e305112
Seewoo Lee <seewoo5@gmail.com>
2024-12-12 17:11:45 -0800

    init

---
+++ Counterexamples/IrrationalPowerOfIrrational.lean
@@ -0,0 +1,34 @@
+/-
+Copyright (c) 2024 Seewoo Lee. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Seewoo Lee
+-/
+import Mathlib.Data.Real.Irrational
+import Mathlib.Analysis.SpecialFunctions.Pow.NNReal
+
+/-
+# Irrational power of irrational numbers are not necessarily irrational.
+
+Prove that there exist irrational numbers `a`, `b` such that `a^b` is rational.
+
+We use the following famous argument (based on the law of excluded middle and irrationality of √2).
+Consider `c = √2^√2`. If `c` is rational, we are done.
+If `c` is irrational, then `c^√2 = 2` is rational, so we are done.
+-/
+
+open Classical Real
+
+/--
+There exist irrational `a`, `b` with rational `a^b`.
+Note that the positivity assumption on `a` is required because of the definition of `rpow` for
+negative bases. See `Real.rpow_def_of_neg` for more details.
+-/
+theorem not_irrational_rpow : ¬ ∀ a b : ℝ, Irrational a → Irrational b → 0 < a → Irrational (a ^ b)
+    := by
+  intro h
+  by_cases hc : Irrational (√2 ^ √2)
+  · have ht := h (√2 ^ √2) √2 hc irrational_sqrt_two (NNReal.rpow_pos (by norm_num))
+    rw [← rpow_mul (by norm_num), mul_self_sqrt (by norm_num), rpow_two, sq_sqrt (by norm_num)]
+      at ht; simp at ht
+  · have ht := h √2 √2 irrational_sqrt_two irrational_sqrt_two (by norm_num)
+    exact hc ht
