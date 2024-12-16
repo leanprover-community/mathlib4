8adce59337b9ab4886e62e6c7da7cfdcfc62e42e
Joël Riou <joel.riou@universite-paris-saclay.fr>
2024-12-16 13:56:07 +0100

    fixing imports

--- Mathlib/SetTheory/Cardinal/HasCardinalLT.lean
+++ Mathlib/SetTheory/Cardinal/HasCardinalLT.lean
@@ -3,7 +3,7 @@ Copyright (c) 2024 Joël Riou. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joël Riou
 -/
-import Mathlib.SetTheory.Cardinal.UnivLE
+import Mathlib.SetTheory.Ordinal.Basic
 
 /-!
 # The property of being of cardinality less than a cardinal
