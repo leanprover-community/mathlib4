42bee373ab9657d1e114b7d5ba09a88ca305286d
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2024-12-18 16:49:10 +0100

    adjust linear topologies

--- Mathlib/RingTheory/MvPowerSeries/LinearTopology.lean
+++ Mathlib/RingTheory/MvPowerSeries/LinearTopology.lean
@@ -13,8 +13,6 @@ import Mathlib.Topology.Algebra.Nonarchimedean.Bases
 - `MvPowerSeries.basis`: the ideals of the ring of multivariate power series
 all coefficients the exponent of which is smaller than some bound vanish.
 
-- `MvPowerSeries.idealIsBasis`: it defines an `Ideal.IsBasis`.
-
 ## Instances :
 
 - `MvPowerSeries.linearTopology`.
@@ -26,30 +24,15 @@ in the general case, the issue is solely about the definition of a linear topolo
 -/
 namespace MvPowerSeries
 
+namespace LinearTopology
+
 open scoped Topology
 
 open Set SetLike
 
-variable (σ : Type*) (α : Type*) [Ring α]
-
-section Ideal.IsBasis
-
-/-- The underlying family for the `Ideal.IsBasis` in a multivariate power series ring. -/
-def basis : (σ →₀ ℕ) → Ideal (MvPowerSeries σ α) := fun d =>
-  { carrier   := {f | ∀ e ≤ d, coeff α e f = 0} -- monomial e 1 ∣ f
-    zero_mem' := fun _ _ => by rw [coeff_zero]
-    add_mem'  := fun hf hg e he => by rw [map_add, hf e he, hg e he, add_zero]
-    smul_mem' := fun f g hg e he => by
-      classical
-      rw [smul_eq_mul, coeff_mul]
-      apply Finset.sum_eq_zero
-      rintro uv huv
-      convert MulZeroClass.mul_zero (coeff α uv.fst f)
-      exact hg  _ (le_trans (le_iff_exists_add'.mpr
-        ⟨uv.fst, (Finset.mem_antidiagonal.mp huv).symm⟩) he) }
-
-/-- The underlying family for the `Ideal.IsBasis` in a multivariate power series ring. -/
-def basis₂ : (σ →₀ ℕ) → TwoSidedIdeal (MvPowerSeries σ α) := fun d ↦ by
+/-- The underlying family for the basis of ideals in a multivariate power series ring. -/
+def basis (σ : Type*) (α : Type*) [Ring α] :
+    (σ →₀ ℕ) → TwoSidedIdeal (MvPowerSeries σ α) := fun d ↦ by
   apply TwoSidedIdeal.mk' {f | ∀ e ≤ d, coeff α e f = 0}
   · simp [coeff_zero]
   · exact fun hf hg e he ↦ by rw [map_add, hf e he, hg e he, add_zero]
@@ -70,52 +53,23 @@ def basis₂ : (σ →₀ ℕ) → TwoSidedIdeal (MvPowerSeries σ α) := fun d 
       convert MulZeroClass.zero_mul (coeff α uv.snd g)
       exact hf _ (le_trans (le_iff_exists_add.mpr ⟨uv.2, (Finset.mem_antidiagonal.mp huv).symm⟩) he)
 
-/-- A power series `f` belongs to the ideal `basis σ α d` if and only if `coeff α e f = 0` for all
-`e ≤ d`. -/
-theorem mem_basis (f : MvPowerSeries σ α) (d : σ →₀ ℕ) :
-    f ∈ basis σ α d ↔ ∀ e ≤ d, coeff α e f = 0 := by
-  simp [basis]
+variable {σ : Type*} {α : Type*} [Ring α]
 
 /-- A power series `f` belongs to the ideal `basis σ α d` if and only if `coeff α e f = 0` for all
 `e ≤ d`. -/
-theorem mem_basis₂ (f : MvPowerSeries σ α) (d : σ →₀ ℕ) :
-    f ∈ basis₂ σ α d ↔ ∀ e ≤ d, coeff α e f = 0 := by
-  simp [basis₂]
+theorem mem_basis_iff {f : MvPowerSeries σ α} {d : σ →₀ ℕ} :
+    f ∈ basis σ α d ↔ ∀ e ≤ d, coeff α e f = 0 := by
+  simp [basis]
 
 /-- If `e ≤ d`, then we have the inclusion of ideals `basis σ α d ≤ basis σ α e`. -/
 theorem basis_le {e d : σ →₀ ℕ} (hed : e ≤ d) : basis σ α d ≤ basis σ α e :=
   fun _ => forall_imp (fun _ h ha => h (le_trans ha hed))
 
-/-- If `e ≤ d`, then we have the inclusion of ideals `basis σ α d ≤ basis σ α e`. -/
-theorem basis₂_le {e d : σ →₀ ℕ} (hed : e ≤ d) : basis₂ σ α d ≤ basis₂ σ α e :=
-  fun _ => forall_imp (fun _ h ha => h (le_trans ha hed))
-
 /-- `basis σ α d ≤ basis σ α e` if and only if `e ≤ d`. -/
 theorem basis_le_iff [Nontrivial α] {d e : σ →₀ ℕ} :
     basis σ α d ≤ basis σ α e ↔ e ≤ d := by
-  refine ⟨?_, basis_le _ _⟩
-  simp only [basis, Submodule.mk_le_mk, AddSubmonoid.mk_le_mk, setOf_subset_setOf]
-  intro h
-  rw [← inf_eq_right]
-  apply le_antisymm inf_le_right
-  by_contra h'
-  simp only [AddSubsemigroup.mk_le_mk, setOf_subset_setOf] at h
-  specialize h (monomial α e 1) _
-  · intro e' he'
-    apply coeff_monomial_ne
-    intro hee'
-    rw [hee'] at he'
-    apply h'
-    exact le_inf_iff.mpr ⟨he', le_rfl⟩
-  · apply one_ne_zero' α
-    convert h e le_rfl
-    rw [coeff_monomial_same]
-
-/-- `basis σ α d ≤ basis σ α e` if and only if `e ≤ d`. -/
-theorem basis₂_le_iff [Nontrivial α] {d e : σ →₀ ℕ} :
-    basis₂ σ α d ≤ basis₂ σ α e ↔ e ≤ d := by
-  refine ⟨?_, basis₂_le _ _⟩
-  simp only [basis₂]
+  refine ⟨?_, basis_le⟩
+  simp only [basis]
   intro h
   rw [← inf_eq_right]
   apply le_antisymm inf_le_right
@@ -133,74 +87,31 @@ theorem basis₂_le_iff [Nontrivial α] {d e : σ →₀ ℕ} :
     rw [coeff_monomial_same]
 
 /-- The function `basis σ α` is antitone. -/
-theorem basis_antitone : Antitone (basis σ α) := fun _ _ h => basis_le σ α h
+theorem basis_antitone : Antitone (basis σ α) := fun _ _ h => basis_le h
 
 /-- The function `basis σ α` is strictly antitone. -/
 theorem basis_strict_anti [Nontrivial α] : StrictAnti (basis σ α) :=
-  strictAnti_of_le_iff_le (fun _ _ ↦ (basis_le_iff σ α).symm)
+  strictAnti_of_le_iff_le (fun _ _ ↦ basis_le_iff.symm)
 
-/-- The function `basis₂ σ α` is antitone. -/
-theorem basis₂_antitone : Antitone (basis₂ σ α) := fun _ _ h => basis₂_le σ α h
-
-/-- The function `basis₂ σ α` is strictly antitone. -/
-theorem basis₂_strict_anti [Nontrivial α] : StrictAnti (basis₂ σ α) :=
-  strictAnti_of_le_iff_le (fun _ _ ↦ (basis₂_le_iff σ α).symm)
-
-theorem ringSubgroupsBasis : RingSubgroupsBasis (fun d ↦ (basis σ α d).toAddSubgroup) where
-  inter d e := ⟨d ⊔ e, (basis_antitone σ α).map_sup_le d e⟩
+variable (σ α) in
+theorem ringSubgroupsBasis :
+    RingSubgroupsBasis (fun d ↦ (basis σ α d).asIdeal.toAddSubgroup) where
+  inter d e := ⟨d ⊔ e, basis_antitone.map_sup_le d e⟩
   mul d := ⟨d, fun f ↦ by
     simp only [Submodule.coe_toAddSubgroup, mem_mul]
     rintro ⟨x, hx, y, hy, rfl⟩
     exact Ideal.mul_mem_left _ _ hy⟩
-  leftMul f d := ⟨d, fun g hg ↦ (basis σ α d).mul_mem_left f hg⟩
-  rightMul f d := ⟨d, fun g hg ↦ by
-    simp only [Submodule.coe_toAddSubgroup, mem_preimage, mem_coe, mem_basis] at hg ⊢
-    intro e he
-    classical
-    rw [coeff_mul]
-    apply Finset.sum_eq_zero
-    rintro ⟨i, j⟩ h
-    rw [hg i (le_trans ?_ he), zero_mul]
-    simp only [← Finset.mem_antidiagonal.mp h, le_self_add]⟩
-
-theorem ringSubgroupsBasis₂ :
-    RingSubgroupsBasis (fun d ↦ (basis₂ σ α d).asIdeal.toAddSubgroup) where
-  inter d e := ⟨d ⊔ e, (basis₂_antitone σ α).map_sup_le d e⟩
-  mul d := ⟨d, fun f ↦ by
-    simp only [Submodule.coe_toAddSubgroup, mem_mul]
-    rintro ⟨x, hx, y, hy, rfl⟩
-    exact Ideal.mul_mem_left _ _ hy⟩
-  leftMul f d := ⟨d, fun g hg ↦ (basis₂ σ α d).mul_mem_left f g hg⟩
+  leftMul f d := ⟨d, fun g hg ↦ (basis σ α d).mul_mem_left f g hg⟩
   rightMul f d := ⟨d, fun g hg ↦ by
     intro e he
     simp only [Submodule.coe_toAddSubgroup, TwoSidedIdeal.coe_asIdeal,
-      mem_coe, sub_zero, mem_basis₂] at hg ⊢
+      mem_coe, sub_zero, mem_basis_iff] at hg ⊢
     classical
     rw [coeff_mul]
     apply Finset.sum_eq_zero
     rintro ⟨i, j⟩ h
     rw [hg i (le_trans ?_ he), zero_mul]
     simp only [← Finset.mem_antidiagonal.mp h, le_self_add]⟩
-
-/- /-- `MvPowerSeries.basis` is an `Ideal.IsBasis`. -/
-theorem idealIsBasis : Ideal.IsBasis (basis σ α) where
-  nonempty := inferInstance
-  inter := fun d e ↦ ⟨d ⊔ e, Antitone.map_sup_le (basis_antitone σ α) d e ⟩
-  mul_right := fun d f g ↦ by
-    simp only [mem_basis]
-    intro hf e he
-    classical
-    rw [coeff_mul]
-    apply Finset.sum_eq_zero
-    rintro ⟨i, j⟩ h
-    rw [hf i (le_trans ?_ he), zero_mul]
-    simp only [← Finset.mem_antidiagonal.mp h, le_self_add]
-
-/-- `MvPowerSeries.basis` is a `RingSubgroupsBasis`. -/
-theorem ringSubgroupsBasis : RingSubgroupsBasis fun d => (basis σ α d).toAddSubgroup :=
-  (idealIsBasis σ α).toRingSubgroupsBasis -/
-
-end Ideal.IsBasis
 
 section DiscreteTopology
 
@@ -224,64 +135,12 @@ theorem basis_mem_nhds_zero (d : σ →₀ ℕ) :
     · simp only [Filter.univ_mem]
   · intro f
     simp only [Finset.coe_Iic, mem_pi, mem_Iic, mem_ite_univ_right, mem_singleton_iff, mem_coe]
+    rw [mem_basis_iff]
     exact forall_imp (fun e h he => h he he)
 
 lemma mem_nhds_zero_iff {U : Set (MvPowerSeries σ α)} :
-    U ∈ nhds 0 ↔ ∃ d, {b | b ∈ basis σ α d} ⊆ U := by
-  refine ⟨?_ , fun ⟨d, hd⟩ ↦ Filter.sets_of_superset _ (basis_mem_nhds_zero σ α d) hd⟩
-  · rw [nhds_pi, Filter.mem_pi]
-    rintro ⟨D, hD, t, ht, ht'⟩
-    use Finset.sup hD.toFinset id
-    apply subset_trans _ ht'
-    intros f hf e he
-    rw [← coeff_apply α f e, hf e (id_eq e ▸ Finset.le_sup (hD.mem_toFinset.mpr he))]
-    exact mem_of_mem_nhds (ht e)
-
-/-- If the coefficient ring `α` is endowed with the discrete topology, then the pointwise
-topology on `MvPowerSeries σ α)` agrees with the topology generated by `MvPowerSeries.basis`. -/
-theorem topology_eq_ideals_basis_topology :
-    MvPowerSeries.WithPiTopology.instTopologicalSpace α =
-      (ringSubgroupsBasis σ α).toRingFilterBasis.topology := by
-  rw [TopologicalAddGroup.ext_iff inferInstance inferInstance]
-  ext s
-  rw [mem_nhds_zero_iff, ((ringSubgroupsBasis σ α).hasBasis_nhds  0).mem_iff]
-  simp only [sub_zero, Submodule.mem_toAddSubgroup, true_and]
-
-example : (𝓝 (0 : MvPowerSeries σ α)).HasBasis (fun _ ↦ True) (fun d ↦ (basis σ α d)) := by
-  apply Filter.HasBasis.mk
-  intro s
-  rw [mem_nhds_iff]
-  constructor
-  · rintro ⟨t, hts, hopen, hmem⟩
-    obtain ⟨d, hd⟩ := (mem_nhds_zero_iff σ α).mp
-      (Filter.mem_of_superset  (IsOpen.mem_nhds hopen hmem) hts)
-    refine ⟨d, ⟨by trivial, hd⟩⟩
-  · rintro ⟨d, _, hd⟩
-    use basis σ α d
-    simp only [mem_coe, Submodule.zero_mem, and_true]
-    exact ⟨hd, (basis σ α d).toAddSubgroup.isOpen_of_mem_nhds (basis_mem_nhds_zero σ α d)⟩
-
-/-- If the coefficient ring `α` is endowed with the discrete topology, then for every `d : σ →₀ ℕ`,
-`↑(basis₂ σ α d) ∈ 𝓝 (0 : MvPowerSeries σ α)`. -/
-theorem basis₂_mem_nhds_zero (d : σ →₀ ℕ) :
-    (basis₂ σ α d : Set (MvPowerSeries σ α)) ∈ 𝓝 0 := by
-  classical
-  rw [nhds_pi, Filter.mem_pi]
-  use Finset.Iic d, Finset.finite_toSet _, (fun e => if e ≤ d then {0} else univ)
-  constructor
-  · intro e
-    split_ifs
-    · simp only [nhds_discrete, Filter.mem_pure, mem_singleton_iff]
-      rfl
-    · simp only [Filter.univ_mem]
-  · intro f
-    simp only [Finset.coe_Iic, mem_pi, mem_Iic, mem_ite_univ_right, mem_singleton_iff, mem_coe]
-    rw [mem_basis₂]
-    exact forall_imp (fun e h he => h he he)
-
-lemma mem_nhds_zero_iff₂ {U : Set (MvPowerSeries σ α)} :
-    U ∈ 𝓝 0 ↔ ∃ d, {b | b ∈ basis₂ σ α d} ⊆ U := by
-  refine ⟨?_ , fun ⟨d, hd⟩ ↦ Filter.sets_of_superset _ (basis₂_mem_nhds_zero σ α d) hd⟩
+    U ∈ 𝓝 0 ↔ ∃ d, {b | b ∈ basis σ α d} ⊆ U := by
+  refine ⟨?_ , fun ⟨d, hd⟩ ↦ Filter.sets_of_superset _ (basis_mem_nhds_zero d) hd⟩
   · rw [nhds_pi, Filter.mem_pi]
     rintro ⟨D, hD, t, ht, ht'⟩
     use Finset.sup hD.toFinset id
@@ -293,38 +152,21 @@ lemma mem_nhds_zero_iff₂ {U : Set (MvPowerSeries σ α)} :
 
 /-- If the coefficient ring `α` is endowed with the discrete topology, then the pointwise
 topology on `MvPowerSeries σ α)` agrees with the topology generated by `MvPowerSeries.basis`. -/
-theorem topology_eq_ideals_basis_topology₂ :
+theorem topology_eq_ideals_basis_topology :
     MvPowerSeries.WithPiTopology.instTopologicalSpace α =
-      (ringSubgroupsBasis₂ σ α).toRingFilterBasis.topology := by
+      (ringSubgroupsBasis σ α).toRingFilterBasis.topology := by
   rw [TopologicalAddGroup.ext_iff inferInstance inferInstance]
   ext s
-  simp [mem_nhds_zero_iff₂, ((ringSubgroupsBasis₂ σ α).hasBasis_nhds  0).mem_iff]
-
-example : (𝓝 (0 : MvPowerSeries σ α)).HasBasis (fun _ ↦ True) (fun d ↦ (basis σ α d)) := by
-  apply Filter.HasBasis.mk
-  intro s
-  rw [mem_nhds_iff]
-  constructor
-  · rintro ⟨t, hts, hopen, hmem⟩
-    obtain ⟨d, hd⟩ := (mem_nhds_zero_iff σ α).mp
-      (Filter.mem_of_superset  (IsOpen.mem_nhds hopen hmem) hts)
-    refine ⟨d, ⟨by trivial, hd⟩⟩
-  · rintro ⟨d, _, hd⟩
-    use basis σ α d
-    simp only [mem_coe, Submodule.zero_mem, and_true]
-    exact ⟨hd, (basis σ α d).toAddSubgroup.isOpen_of_mem_nhds (basis_mem_nhds_zero σ α d)⟩
-
-theorem basis₂_hasBasis :
-    (𝓝 (0 : MvPowerSeries σ α)).HasBasis (fun _ ↦ True) (fun d ↦ (basis₂ σ α d)) := by
-  apply Filter.HasBasis.mk
-  intro s
-  simp [mem_nhds_zero_iff₂]
+  simp [mem_nhds_zero_iff, ((ringSubgroupsBasis σ α).hasBasis_nhds 0).mem_iff]
 
 /-- The topology on `MvPowerSeries` is a linear topology when the ring of coefficients has
 the discrete topology. -/
-instance : LinearTopology (MvPowerSeries σ α) :=
-  LinearTopology.mk_of_twoSidedIdeal (basis₂_hasBasis σ α)
+instance : IsLinearTopology (MvPowerSeries σ α) :=
+  IsLinearTopology.mk_of_twoSidedIdeal (p := fun _ ↦ True) (s := fun d ↦ basis σ α d)
+    (Filter.HasBasis.mk fun s ↦ by simp [mem_nhds_zero_iff])
 
 end DiscreteTopology
+
+end LinearTopology
 
 end MvPowerSeries
--- Mathlib/Topology/Algebra/LinearTopology.lean
+++ Mathlib/Topology/Algebra/LinearTopology.lean
@@ -14,7 +14,7 @@ Following Bourbaki, *Algebra II*, chapter 4, §2, n° 3, a topology on a ring `R
 it is invariant by translation and there admits a basis of neighborhoods of 0 consisting of
 two-sided ideals.
 
-- `LinearTopology.tendsto_zero_mul`: for `f, g : ι → R` such that `f i` converges to `0`,
+- `IsLinearTopology.tendsto_zero_mul`: for `f, g : ι → R` such that `f i` converges to `0`,
 `f i * g i` converges to `0`.
 
 -/
@@ -27,59 +27,59 @@ section Ring
 variable (α : Type*) [Ring α]
 
 /-- A topology on a ring is linear if its topology is defined by a family of ideals. -/
-class LinearTopology [TopologicalSpace α] [TopologicalRing α] where
+class IsLinearTopology [TopologicalSpace α] [TopologicalRing α] where
   hasBasis_twoSidedIdeal : (𝓝 (0 : α)).HasBasis
     (fun I : TwoSidedIdeal α ↦ (I : Set α) ∈ 𝓝 0) (fun I : TwoSidedIdeal α ↦ (I : Set α))
 
 variable {α} [TopologicalSpace α] [TopologicalRing α]
 
-lemma LinearTopology.hasBasis_open_twoSidedIdeal [LinearTopology α] :
+lemma IsLinearTopology.hasBasis_open_twoSidedIdeal [IsLinearTopology α] :
     (𝓝 (0 : α)).HasBasis
       (fun I : TwoSidedIdeal α ↦ IsOpen (I : Set α)) (fun I : TwoSidedIdeal α ↦ (I : Set α)) :=
-  LinearTopology.hasBasis_twoSidedIdeal.congr
+  IsLinearTopology.hasBasis_twoSidedIdeal.congr
     (fun I ↦ ⟨I.asIdeal.toAddSubgroup.isOpen_of_mem_nhds, fun hI ↦ hI.mem_nhds (zero_mem I)⟩)
     (fun _ _ ↦ rfl)
 
-theorem LinearTopology.hasBasis_ideal [LinearTopology α] :
+theorem IsLinearTopology.hasBasis_ideal [IsLinearTopology α] :
     (𝓝 0).HasBasis (fun I : Ideal α ↦ (I : Set α) ∈ 𝓝 0) (fun I : Ideal α ↦ (I : Set α)) :=
-  LinearTopology.hasBasis_twoSidedIdeal.to_hasBasis
+  IsLinearTopology.hasBasis_twoSidedIdeal.to_hasBasis
     (fun I hI ↦ ⟨I.asIdeal, hI, subset_rfl⟩)
-    (fun _ ↦ LinearTopology.hasBasis_twoSidedIdeal.mem_iff.mp)
+    (fun _ ↦ IsLinearTopology.hasBasis_twoSidedIdeal.mem_iff.mp)
 
-theorem LinearTopology.hasBasis_open_ideal [LinearTopology α] :
+theorem IsLinearTopology.hasBasis_open_ideal [IsLinearTopology α] :
     (𝓝 0).HasBasis (fun I : Ideal α ↦ IsOpen (I : Set α)) (fun I : Ideal α ↦ (I : Set α)) :=
-  LinearTopology.hasBasis_ideal.congr
+  IsLinearTopology.hasBasis_ideal.congr
     (fun I ↦ ⟨I.toAddSubgroup.isOpen_of_mem_nhds, fun hI ↦ hI.mem_nhds (zero_mem I)⟩)
     (fun _ _ ↦ rfl)
 
-lemma LinearTopology.mk_of_twoSidedIdeal {ι : Sort*} {p : ι → Prop} {s : ι → TwoSidedIdeal α}
+lemma IsLinearTopology.mk_of_twoSidedIdeal {ι : Sort*} {p : ι → Prop} {s : ι → TwoSidedIdeal α}
     (h : (𝓝 0).HasBasis p (fun i ↦ (s i : Set α))) :
-    LinearTopology α where
+    IsLinearTopology α where
   hasBasis_twoSidedIdeal := h.to_hasBasis (fun i hi ↦ ⟨s i, h.mem_of_mem hi, subset_rfl⟩)
     (fun _ ↦ h.mem_iff.mp)
 
-theorem linearTopology_iff_hasBasis_twoSidedIdeal :
-    LinearTopology α ↔ (𝓝 0).HasBasis
+theorem isLinearTopology_iff_hasBasis_twoSidedIdeal :
+    IsLinearTopology α ↔ (𝓝 0).HasBasis
       (fun I : TwoSidedIdeal α ↦ (I : Set α) ∈ 𝓝 0) (fun I : TwoSidedIdeal α ↦ (I : Set α)) :=
-  ⟨fun _ ↦ LinearTopology.hasBasis_twoSidedIdeal, fun h ↦ .mk_of_twoSidedIdeal h⟩
+  ⟨fun _ ↦ IsLinearTopology.hasBasis_twoSidedIdeal, fun h ↦ .mk_of_twoSidedIdeal h⟩
 
-theorem linearTopology_iff_hasBasis_open_twoSidedIdeal :
-    LinearTopology α ↔ (𝓝 0).HasBasis
+theorem isLinearTopology_iff_hasBasis_open_twoSidedIdeal :
+    IsLinearTopology α ↔ (𝓝 0).HasBasis
       (fun I : TwoSidedIdeal α ↦ IsOpen (I : Set α)) (fun I : TwoSidedIdeal α ↦ (I : Set α)) :=
-  ⟨fun _ ↦ LinearTopology.hasBasis_open_twoSidedIdeal, fun h ↦ .mk_of_twoSidedIdeal h⟩
+  ⟨fun _ ↦ IsLinearTopology.hasBasis_open_twoSidedIdeal, fun h ↦ .mk_of_twoSidedIdeal h⟩
 
-theorem LinearTopology.tendsto_mul_zero_of_left [LinearTopology α] {ι : Type*} {f : Filter ι}
+theorem IsLinearTopology.tendsto_mul_zero_of_left [IsLinearTopology α] {ι : Type*} {f : Filter ι}
     (a b : ι → α) (ha : Tendsto a f (𝓝 0)) :
     Tendsto (a * b) f (𝓝 0) := by
-  rw [LinearTopology.hasBasis_twoSidedIdeal.tendsto_right_iff] at ha ⊢
+  rw [IsLinearTopology.hasBasis_twoSidedIdeal.tendsto_right_iff] at ha ⊢
   intro I hI
   filter_upwards [ha I hI] with i ai_mem
   exact I.mul_mem_right _ _ ai_mem
 
-theorem LinearTopology.tendsto_mul_zero_of_right_[LinearTopology α] {ι : Type*} {f : Filter ι}
+theorem IsLinearTopology.tendsto_mul_zero_of_right [IsLinearTopology α] {ι : Type*} {f : Filter ι}
     (a b : ι → α) (hb : Tendsto b f (𝓝 0)) :
     Tendsto (a * b) f (𝓝 0) := by
-  rw [LinearTopology.hasBasis_twoSidedIdeal.tendsto_right_iff] at hb ⊢
+  rw [IsLinearTopology.hasBasis_twoSidedIdeal.tendsto_right_iff] at hb ⊢
   intro I hI
   filter_upwards [hb I hI] with i bi_mem
   exact I.mul_mem_left _ _ bi_mem
@@ -90,56 +90,21 @@ section CommRing
 
 variable {α} [CommRing α] [TopologicalSpace α] [TopologicalRing α]
 
-lemma LinearTopology.mk_of_ideal {ι : Sort*} {p : ι → Prop} {s : ι → Ideal α}
+lemma IsLinearTopology.mk_of_ideal {ι : Sort*} {p : ι → Prop} {s : ι → Ideal α}
     (h : (𝓝 0).HasBasis p (fun i ↦ (s i : Set α))) :
-    LinearTopology α where
+    IsLinearTopology α where
   hasBasis_twoSidedIdeal := h.to_hasBasis
     (fun i hi ↦ ⟨(s i).toTwoSided ((s i).mul_mem_right _), by simpa using h.mem_of_mem hi, by simp⟩)
     (fun _ ↦ h.mem_iff.mp)
 
-theorem linearTopology_iff_hasBasis_ideal :
-    LinearTopology α ↔ (𝓝 0).HasBasis
+theorem isLinearTopology_iff_hasBasis_ideal :
+    IsLinearTopology α ↔ (𝓝 0).HasBasis
       (fun I : Ideal α ↦ (I : Set α) ∈ 𝓝 0) (fun I : Ideal α ↦ (I : Set α)) :=
-  ⟨fun _ ↦ LinearTopology.hasBasis_ideal, fun h ↦ .mk_of_ideal h⟩
+  ⟨fun _ ↦ IsLinearTopology.hasBasis_ideal, fun h ↦ .mk_of_ideal h⟩
 
-theorem linearTopology_iff_hasBasis_open_ideal :
-    LinearTopology α ↔ (𝓝 0).HasBasis
+theorem isLinearTopology_iff_hasBasis_open_ideal :
+    IsLinearTopology α ↔ (𝓝 0).HasBasis
       (fun I : Ideal α ↦ IsOpen (I : Set α)) (fun I : Ideal α ↦ (I : Set α)) :=
-  ⟨fun _ ↦ LinearTopology.hasBasis_open_ideal, fun h ↦ .mk_of_ideal h⟩
+  ⟨fun _ ↦ IsLinearTopology.hasBasis_open_ideal, fun h ↦ .mk_of_ideal h⟩
 
 end CommRing
-
-#exit
-variable {α : Type*} [CommRing α] [TopologicalSpace α] [TopologicalRing α] [LinearTopology α]
-
-theorem mem_nhds_zero_iff (s : Set α) :
-    (s ∈ nhds 0) ↔
-    (∃ I : Ideal α, ((I : Set α) ∈ nhds 0) ∧ (I : Set α) ⊆ s) := by
-  rw [isLinearTopology.mem_iff]
-  apply exists_congr
-  intro I
-  apply and_congr_left
-  intro hI
-  rw [isOpen_iff_nhds]
-  constructor
-  · exact fun hI' ↦ hI' 0 I.zero_mem fun ⦃_⦄ a ↦ a
-  · intro hI' x hx s hs
-    rw [Filter.mem_principal] at hs
-    rw [← vadd_mem_nhds_vadd_iff (-x)]
-    simp only [vadd_eq_add, neg_add_cancel]
-    apply Filter.mem_of_superset hI'
-    intro y hy
-    rw [Set.mem_neg_vadd_set_iff]
-    exact hs (add_mem hx hy)
-
-theorem tendsto_zero_mul {ι : Type*} {f : Filter ι}
-    (a b : ι → α) (hb : Filter.Tendsto b f (nhds 0)) :
-    Filter.Tendsto (a * b) f (nhds 0) := by
-  intro v hv
-  obtain ⟨I, I_mem, I_le⟩ := (LinearTopology.mem_nhds_zero_iff _).mp hv
-  apply Filter.sets_of_superset _ _ I_le
-  simp only [Filter.mem_sets, Filter.mem_map]
-  rw [Filter.tendsto_def] at hb
-  exact Filter.sets_of_superset _ (hb _ I_mem) (fun x hx ↦ Ideal.mul_mem_left _ _ hx)
-
-end LinearTopology
