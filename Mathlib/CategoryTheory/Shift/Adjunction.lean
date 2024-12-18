d1069d7d3b013d1e040ce775714e00ff1523e214
morel <smorel@math.princeton.edu>
2024-12-18 11:44:08 +0100

    replace import

--- Mathlib/CategoryTheory/Shift/Adjunction.lean
+++ Mathlib/CategoryTheory/Shift/Adjunction.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Sophie Morel, Joël Riou
 -/
 import Mathlib.CategoryTheory.Shift.CommShift
-import Mathlib.CategoryTheory.Adjunction.Unique
+import Mathlib.CategoryTheory.Adjunction.Mates
 
 /-!
 # Adjoints commute with shifts
