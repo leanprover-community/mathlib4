d896066323fe7acd9b654aca9553e7c89da2e6f3
Alex Meiburg <timeroot.alex@gmail.com>
2024-12-17 23:12:25 -0500

    progress, cleanup

--- Mathlib/Algebra/Operad/Clone.lean
+++ Mathlib/Algebra/Operad/Clone.lean
@@ -67,15 +67,6 @@ theorem clone_id_left {n : ℕ} (a : Fin 1 → A n) : 1 ∘∈ a = a 0 := by
 theorem cloneCompose_id {n : ℕ} (a : A n) (b : Fin n) : cloneCompose a b (1 : A 1) = a := by
   simp [cloneCompose, clonePadTo]
 
-lemma add_add_sub_one_comm_of_Fin {x b : ℕ} (a c : ℕ) (h : x < b) :
-  a + (b + c - 1) = a + b - 1 + c := by
-    omega
-
-lemma add_sub_one_comm_of_Fin {a : ℕ} (b c : ℕ) (x : Fin a) :
-  a + b - 1 + c - 1 = a + c - 1 + b - 1 := by
-    repeat rw [tsub_add_eq_add_tsub (le_add_right (Nat.one_le_of_lt x.isLt))]
-    rw [Nat.add_right_comm]
-
 /- The `cloneCompose` one-argument composition induced by a clone's
   superpose operation is associative. -/
 theorem cloneCompose_assoc (a b c : Sigma A) (p1 : Fin a.fst) (p2 : Fin b.fst) :
@@ -83,8 +74,8 @@ theorem cloneCompose_assoc (a b c : Sigma A) (p1 : Fin a.fst) (p2 : Fin b.fst) :
   (cloneCompose (cloneCompose a.snd p1 b.snd) (p1.hAdd p2) c.snd) := by
     dsimp [cloneCompose]
     rw [superpose_assoc]
-    have := add_add_sub_one_comm_of_Fin a.fst c.fst p2.2
     congr! 2 with _ x y hxy
+    · have := p2.2; omega
     subst hxy
     simp only [Fin.val_inj, Fin.val_fin_lt]
     split
@@ -123,7 +114,6 @@ theorem cloneCompose_assoc (a b c : Sigma A) (p1 : Fin a.fst) (p2 : Fin b.fst) :
         · omega
         · omega
 
-
 /- The `cloneCompose` one-argument composition induced by a clone's superpose operation
   is commutative: it doesn't matter whether the p1'th or p2'th argument is provided first. -/
 theorem cloneCompse_comm (a b c : Sigma A) (p1 p2 : Fin a.fst) (hp: p1 < p2)
@@ -131,17 +121,17 @@ theorem cloneCompse_comm (a b c : Sigma A) (p1 p2 : Fin a.fst) (hp: p1 < p2)
   (hp1' : p1.val = p1'.val) (hp2' : p2'.val = p2.val + b.fst - 1) :
   HEq (cloneCompose (cloneCompose a.snd p1 b.snd) p2' c.snd)
       (cloneCompose (cloneCompose a.snd p2 c.snd) p1' b.snd) := by
-  have h := add_sub_one_comm_of_Fin b.fst c.fst p2
   rcases p1 with ⟨p1, hp1⟩
   rcases p2 with ⟨p2, hp2⟩
   rcases p1' with ⟨p1', _⟩
   rcases p2' with ⟨p2', _⟩
   subst hp2'
   subst hp1'
-  simp only
   dsimp [cloneCompose]
   rw [superpose_assoc, superpose_assoc]
-  congr! 2 with ⟨k, hk⟩ ⟨k2, hk2⟩ hkk2
+  congr! 1
+  · omega
+  congr! 1 with ⟨k, hk⟩ ⟨k2, hk2⟩ hkk2
   rw [Fin.mk.injEq] at hkk2
   subst hkk2
   dsimp [Fin.val, Fin.addNat, Fin.hAdd]
@@ -153,7 +143,7 @@ theorem cloneCompse_comm (a b c : Sigma A) (p1 p2 : Fin a.fst) (hp: p1 < p2)
     simp only [dif_neg hp1p2, dif_pos hp, clonePadTo, superpose_assoc]
     simp only [clone_proj_left, lt_self_iff_false, dite_false, dite_true]
     congr! with ⟨k2, hk2⟩ ⟨k3, hk3⟩ hk2k3
-    have h₁ : p1 + k2 < p2 + b.fst - 1 := Nat.lt_sub_of_add_lt (by omega)
+    have h₁ : p1 + k2 < p2 + b.fst - 1 := by omega
     simp only [h₁, h₁.ne, dite_true, dite_false]
     congr!
     rwa [Fin.mk.injEq] at hk2k3
@@ -161,54 +151,43 @@ theorem cloneCompse_comm (a b c : Sigma A) (p1 p2 : Fin a.fst) (hp: p1 < p2)
     split
     next hkp1 =>
       have h₁ : k < p2 := by omega
-      have h₂ : k < p2 + b.fst - 1 := Nat.lt_sub_of_add_lt (by omega)
+      have h₂ : k < p2 + b.fst - 1 := by omega
       simp only [clone_proj_left, hkp1, h₁, h₂, hkp1.ne, h₁.ne, h₂.ne, dite_true, dite_false]
       congr!
     next =>
       have hkp1 : k > p1 := by omega
       split
       next hkp2 =>
-        simp only [clone_proj_left]
-        have hkbp2 : k + b.fst - 1 = p2 + b.fst - 1 := by congr
-        rw [dif_pos hkbp2]
-        dsimp [clonePadTo]
-        rw [superpose_assoc]
+        rw [clone_proj_left, dif_pos, clonePadTo, clonePadTo, superpose_assoc]
+        swap
+        · dsimp
+          omega
         congr! with ⟨k2, hk2⟩ ⟨k3, hk3⟩ hk2k3
         rw [Fin.mk.injEq] at hk2k3
         subst hk2k3
         rw [clone_proj_left]
-        have hkk2p1 : k + k2 > p1 := Nat.lt_add_right k2 hkp1
         dsimp
-        rw [dif_neg hkk2p1.ne.symm, dif_neg (Nat.lt_asymm hkk2p1)]
-        congr! 2
-        rw [tsub_add_eq_add_tsub (le_add_right (Nat.one_le_of_lt hkp1)),
-          Nat.add_right_comm]
+        rw [dif_neg, dif_neg]
+        · congr! 2
+          omega
+        · omega
+        · omega
       next hkp2 =>
         split
         next hkp2 =>
           simp only [clone_proj_left]
-          have hkbp2 : k + b.fst - 1 < p2 + b.fst - 1 := by
-            apply tsub_lt_tsub_right_of_le
-            · exact le_add_right (Nat.one_le_of_lt hkp1)
-            · apply Nat.add_lt_add_right hkp2
+          have hkbp2 : k + b.fst - 1 < p2 + b.fst - 1 := by omega
           rw [dif_neg hkbp2.ne, dif_pos hkbp2, dif_neg hkp1.ne.symm, dif_neg (Nat.lt_asymm hkp1)]
           congr!
         next =>
           simp only [clone_proj_left]
           replace hkp2 : k > p2 := by omega
-          have hkbp2 : k + b.fst - 1 > p2 + b.fst - 1 := by
-            apply tsub_lt_tsub_right_of_le
-            · exact le_add_right (Nat.one_le_of_lt hp)
-            · apply Nat.add_lt_add_right hkp2
-          have hkcp1 : k + c.fst - 1 > p1 := Nat.lt_sub_of_add_lt (by omega)
+          have hkbp2 : k + b.fst - 1 > p2 + b.fst - 1 := by omega
+          have hkcp1 : k + c.fst - 1 > p1 := by omega
           rw [dif_neg hkbp2.ne.symm, dif_neg (Nat.lt_asymm hkbp2)]
           rw [dif_neg hkcp1.ne.symm, dif_neg (Nat.lt_asymm hkcp1)]
-          congr! 3
-          rw [tsub_add_eq_add_tsub (le_add_right (Nat.one_le_of_lt hkp1)),
-            tsub_add_eq_add_tsub (le_add_right (Nat.one_le_of_lt hkp1)),
-            Nat.add_right_comm]
-
-set_option maxHeartbeats 400000
+          congr! 2
+          omega
 
 /- Every abstract clone naturally induces a (symmetric) operad. Currently this only shows the
  (nonsymmetric) `Operad` instance; extending it to `SymmOperad` requires some annoying lemmas with
@@ -230,14 +209,13 @@ instance clone_toSymmOperad [Clone A] : Operad A where
   assoc := fun a b c p1 p2 ↦ by
     dsimp [composeAt]
     congr 1
-    · congr 1
-      exact add_add_sub_one_comm_of_Fin _ _ p2.2
+    · have := p2.2; omega
     · exact cloneCompose_assoc a b c p1 p2
 
   comm := fun {a} b c p1 p2 hp ↦ by
     dsimp [composeAt]
     congr 1
-    · exact add_sub_one_comm_of_Fin _ _ p2
+    · omega
     · exact cloneCompse_comm a b c p1 p2 hp _ _ rfl rfl
 
   -- The following would be needed to improve this from `extends Operad` to `extends SymmOperad`
@@ -249,7 +227,7 @@ instance clone_toSymmOperad [Clone A] : Operad A where
   --     simp_rw [superpose_assoc, proj_left]
   --   }
   -- perm_left := fun s k x y ↦ by
-  --   dsimp [SigmaMul_smul, MultiComposable.compose]
+  --   dsimp [SigmaMul_smul, MultiComposable.compose, cloneCompose]
   --   rw [superpose_assoc]
   --   congr! with z
   --   rw [proj_left]
