99142590464317874ca2a6d1033ba2d83dda02a0
ChrisHughes24 <chrishughes24@gmail.com>
2024-12-19 16:51:11 +0000

    some progress

--- Mathlib/ModelTheory/DefinableFunction.lean
+++ Mathlib/ModelTheory/DefinableFunction.lean
@@ -16,15 +16,42 @@ namespace Theory
 variable {L : Language.{u, v}} (T : L.Theory) {α : Type w} {β : Type x}
 
 def IsDefFunc (φ : L.Formula (α ⊕ β)) : Prop :=
-  ∀ (M : Type (max u v)) [L.Structure M] [T.Model M],
+  ∀ (M : Theory.ModelType.{_, _, max u v} T),
       ∃ f : (α → M) → (β → M), ∀ x : α → M, ∀ y : β → M,
         (f x = y) ↔ φ.Realize (Sum.elim x y)
-#print Formula.or
-theorem isDefFunc_iff_of_finite [Fintype α] [Fintype β] {φ : L.Formula (α ⊕ β)} :
-    T.IsDefFunc φ ↔ T ⊨ᵇ
-      (Formula.iAlls (β := Empty) Sum.inr
-        (Formula.iExs Sum.swap φ)) ⊓
-      BoundedFormula.iInf (Finset.univ : Finset α) _ := by sorry
+#print ModelsBoundedFormula
+theorem isDefFunc_iff [DecidableEq (α ⊕ β)] {φ : L.Formula (α ⊕ β)} :
+    T.IsDefFunc φ ↔
+      (∃ (h : Finite β),
+        T ⊨ᵇ (Formula.iExsUnique (γ := β) id φ)) ∨
+    (T ⊨ᵇ (∀' ∀' ((Term.var (Sum.inr 0)) =' (Term.var (Sum.inr 1))) : L.Formula Empty) ∧
+      T ⊨ᵇ φ) := by
+  by_cases hM : T ⊨ᵇ ∀' ∀' ((var (Sum.inr 0) : L.Term (Empty ⊕ Fin 2)) =' var (Sum.inr 1)) ∧ T ⊨ᵇ φ
+  · refine iff_of_true ?_ (Or.inr hM)
+    intro M
+    let m : M := Classical.choice inferInstance
+    use fun _ _ => m
+    intro x y
+    have h1 : ∀ a b : M, a = b :=
+      by simpa [Sentence.Realize, Formula.Realize] using hM.1.realize_sentence M
+    exact iff_of_true (funext (fun _ => h1 _ _)) (hM.2.realize_formula M)
+  · rw [or_iff_left hM]
+    simp only [IsDefFunc, ModelsBoundedFormula, BoundedFormula.realize_iExsUnique, id_eq,
+      forall_const, exists_prop]
+    refine ⟨?_, ?_⟩
+    · intro h
+      admit
+    · intro h M
+      simp only [Classical.skolem, ExistsUnique] at h
+      rcases h.2 with ⟨f, hf⟩
+      use f M
+      intro x y
+
+
+
+
+
+
 
 /-- A definable  -/
 def DefFunc (α : Type w) (β : Type x) : Type _ :=
