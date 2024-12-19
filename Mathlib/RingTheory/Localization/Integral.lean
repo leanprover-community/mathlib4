a28d7b366b92bf6526887b22e77c36457c310c17
Yaël Dillies <yael.dillies@gmail.com>
2024-12-19 11:47:21 +0000

    feat: the localization of `R[X]` at `S` is the localization of `R` at `S` From GrowthInGroups (LeanCamCombi)
    Co-authored-by: Andrew Yang

--- Mathlib/RingTheory/Localization/Integral.lean
+++ Mathlib/RingTheory/Localization/Integral.lean
@@ -466,3 +466,35 @@ lemma isAlgebraic_of_isFractionRing {R S} (K L) [CommRing R] [CommRing S] [Field
     apply IsIntegral.tower_top (R := R)
     apply IsIntegral.map (IsScalarTower.toAlgHom R S L)
     exact Algebra.IsIntegral.isIntegral (s : S)
+
+namespace Polynomial
+
+lemma isLocalization {R} [CommRing R] (S : Submonoid R) (A) [CommRing A] [Algebra R A]
+    [IsLocalization S A] :
+    letI := (mapRingHom (algebraMap R A)).toAlgebra; IsLocalization (S.map C) A[X] := by
+  letI := (mapRingHom (algebraMap R A)).toAlgebra
+  constructor
+  · rintro ⟨_, r, hr, rfl⟩
+    simpa [RingHom.algebraMap_toAlgebra] using (IsLocalization.map_units A ⟨r, hr⟩).map C
+  · intro z
+    obtain ⟨b, hb⟩ := IsLocalization.integerNormalization_spec S z
+    refine ⟨⟨IsLocalization.integerNormalization S z, _, b, b.2, rfl⟩, ?_⟩
+    ext i
+    simp only [RingHom.algebraMap_toAlgebra, coe_mapRingHom, map_C, coeff_mul_C, coeff_map, hb,
+      mul_comm, Algebra.smul_def]
+  · intros x y e
+    rw [← sub_eq_zero, ← map_sub, RingHom.algebraMap_toAlgebra, ← RingHom.mem_ker,
+      Polynomial.ker_mapRingHom, Ideal.mem_map_C_iff] at e
+    simp only [coeff_sub, RingHom.mem_ker, map_sub, sub_eq_zero,
+      IsLocalization.eq_iff_exists S] at e
+    choose c hc using e
+    refine ⟨⟨_, _, ((x.support ∪ y.support).prod c).2, rfl⟩, ?_⟩
+    ext i
+    simp only [coeff_C_mul]
+    by_cases hi : i ∈ x.support ∪ y.support
+    · obtain ⟨k, e⟩ := Finset.dvd_prod_of_mem c hi
+      simp only [e, mul_comm _ k, Submonoid.coe_mul _ k, mul_assoc, hc]
+    · simp only [Finset.mem_union, mem_support_iff, ne_eq, not_or, not_not] at hi
+      simp [hi.1, hi.2]
+
+end Polynomial
