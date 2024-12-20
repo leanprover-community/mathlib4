39a5bdf908850bb748b8a774c257bde876450ac7
Christian Merten <christian@merten.dev>
2024-12-20 16:24:24 +0100

    fix

--- Mathlib/Data/Matrix/PEquiv.lean
+++ Mathlib/Data/Matrix/PEquiv.lean
@@ -173,16 +173,13 @@ variable {α m n : Type*} [DecidableEq m] [DecidableEq n]
 lemma toPEquiv_toMatrix_mulVec_single [Fintype n] [NonAssocSemiring α] (σ : m ≃ n) (i : n) (x : α) :
     σ.toPEquiv.toMatrix *ᵥ Pi.single i x = Pi.single (σ.symm i) x := by
   ext j
-  have : σ.toPEquiv j = some i ↔ j = σ.symm i := by
-    simp only [Equiv.toPEquiv, PEquiv.coe_mk, Function.comp_apply, Option.some.injEq]
-    exact Equiv.apply_eq_iff_eq_symm_apply σ
-  simp [Pi.single_apply, this]
+  simp [Pi.single_apply, Equiv.apply_eq_iff_eq_symm_apply σ]
 
 lemma single_vecMul_toPEquiv_toMatrix [Fintype m] [NonAssocSemiring α] (σ : m ≃ n) (i : m) (x : α) :
     Pi.single i x ᵥ* σ.toPEquiv.toMatrix = Pi.single (σ i) x := by
   ext j
-  simp only [single_vecMul, PEquiv.toMatrix_apply, Option.mem_def, Equiv.toPEquiv_eq_some_iff,
-    mul_ite, mul_one, mul_zero, Pi.single_apply]
+  simp only [single_vecMul, PEquiv.toMatrix_apply, toPEquiv_apply, Option.mem_def,
+    Option.some.injEq, mul_ite, mul_one, mul_zero, Pi.single_apply]
   simp_rw [eq_comm]
 
 @[simp]
