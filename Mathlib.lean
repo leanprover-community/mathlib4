9cc15b97160f18892ea807dc9c484898ec5a1fcc
Dagur Asgeirsson <dagurtomas@gmail.com>
2024-12-20 16:26:49 +0000

    feat(CategoryTheory/Sites): colimits in categories of extensive sheaves are computed objectwise (#19913)
    … for the shapes for which the colimit functor preserves finite products. This means all shapes when the target is a preadditive category, sifted shapes when the target is the category of sets.

--- Mathlib.lean
+++ Mathlib.lean
@@ -2106,6 +2106,7 @@ import Mathlib.CategoryTheory.Sites.Coherent.CoherentSheaves
 import Mathlib.CategoryTheory.Sites.Coherent.CoherentTopology
 import Mathlib.CategoryTheory.Sites.Coherent.Comparison
 import Mathlib.CategoryTheory.Sites.Coherent.Equivalence
+import Mathlib.CategoryTheory.Sites.Coherent.ExtensiveColimits
 import Mathlib.CategoryTheory.Sites.Coherent.ExtensiveSheaves
 import Mathlib.CategoryTheory.Sites.Coherent.ExtensiveTopology
 import Mathlib.CategoryTheory.Sites.Coherent.LocallySurjective
