24e10cde8489a351d7827536d5b01a2469df6668
Michael Rothgang <rothgang@math.uni-bonn.de>
2024-12-20 14:46:50 +0100

    Fix declaration names, and module doc-string.

--- Mathlib/GroupTheory/Sylow.lean
+++ Mathlib/GroupTheory/Sylow.lean
@@ -24,16 +24,16 @@ The Sylow theorems are the following results for every finite group `G` and ever
 
 ## Main statements
 
-* `exists_subgroup_card_pow_prime`: A generalization of Sylow's first theorem:
+* `Sylow.exists_subgroup_card_pow_prime`: A generalization of Sylow's first theorem:
   For every prime power `pⁿ` dividing the cardinality of `G`,
   there exists a subgroup of `G` of order `pⁿ`.
 * `IsPGroup.exists_le_sylow`: A generalization of Sylow's first theorem:
   Every `p`-subgroup is contained in a Sylow `p`-subgroup.
 * `Sylow.card_eq_multiplicity`: The cardinality of a Sylow subgroup is `p ^ n`
  where `n` is the multiplicity of `p` in the group order.
-* `sylow_conjugate`: A generalization of Sylow's second theorem:
+* `instIsPretransitiveSylowOfFactPrimeOfFinite`: a generalization of Sylow's second theorem:
   If the number of Sylow `p`-subgroups is finite, then all Sylow `p`-subgroups are conjugate.
-* `card_sylow_modEq_one`: A generalization of Sylow's third theorem:
+* `card_sylow_modEq_one`: a generalization of Sylow's third theorem:
   If the number of Sylow `p`-subgroups is finite, then it is congruent to `1` modulo `p`.
 -/
 
