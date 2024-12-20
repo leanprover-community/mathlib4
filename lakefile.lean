ca2e2af9d9fd8a1f67dea4266c5bff4e7f9c42e0
Kim Morrison <kim@tqft.net>
2024-12-20 11:19:33 +1100

    bump batteries

--- lakefile.lean
+++ lakefile.lean
@@ -7,8 +7,7 @@ open Lake DSL
 ## Mathlib dependencies on upstream projects
 -/
 
-require "jcommelin" / "batteries"
-  from git "https://github.com/jcommelin/batteries" @ "lean-pr-testing-6397"
+require "leanprover-community" / "batteries" @ git "lean-pr-testing-6397"
 require "leanprover-community" / "Qq" @ git "v4.14.0"
 require "leanprover-community" / "aesop" @ git "nightly-testing"
 require "leanprover-community" / "proofwidgets" @ git "v0.0.48"
