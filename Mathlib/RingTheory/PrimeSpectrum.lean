5f4179d3869c7685b3978026ad114cf8062f592d
Yaël Dillies <yael.dillies@gmail.com>
2024-12-20 13:55:10 +0000

    simp nf

--- Mathlib/RingTheory/PrimeSpectrum.lean
+++ Mathlib/RingTheory/PrimeSpectrum.lean
@@ -374,12 +374,12 @@ theorem mem_compl_zeroLocus_iff_not_mem {f : R} {I : PrimeSpectrum R} :
   rw [Set.mem_compl_iff, mem_zeroLocus, Set.singleton_subset_iff]; rfl
 
 @[simp]
-lemma zeroLocus_union_singleton_zero (s : Set R) : zeroLocus (s ∪ {0}) = zeroLocus s := by
-  rw [zeroLocus_union, zeroLocus_singleton_zero, Set.inter_univ]
+lemma zeroLocus_insert_zero (s : Set R) : zeroLocus (insert 0 s) = zeroLocus s := by
+  rw [← Set.union_singleton, zeroLocus_union, zeroLocus_singleton_zero, Set.inter_univ]
 
 @[simp]
 lemma zeroLocus_diff_singleton_zero (s : Set R) : zeroLocus (s \ {0}) = zeroLocus s := by
-  rw [← zeroLocus_union_singleton_zero, ← zeroLocus_union_singleton_zero (s := s)]; simp
+  rw [← zeroLocus_insert_zero, ← zeroLocus_insert_zero (s := s)]; simp
 
 lemma zeroLocus_smul_of_isUnit {r : R} (hr : IsUnit r) (s : Set R) :
     zeroLocus (r • s) = zeroLocus s := by
