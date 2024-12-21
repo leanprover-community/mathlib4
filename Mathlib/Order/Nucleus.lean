6b2fa0d21f1c4c4fbc30a79b60e97459e8f009e1
Bergschaf <christian.k@netcom-mail.de>
2024-12-21 11:11:35 +0100

    updates

--- Mathlib/Order/Nucleus.lean
+++ Mathlib/Order/Nucleus.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Chriara Cimino, Christian Krause
 -/
 import Mathlib.Order.CompleteBooleanAlgebra
+import Mathlib.Tactic.ApplyFun
 
 /-!
 # Nucleus
@@ -11,20 +12,61 @@ Locales are the dual concept to Frames.
 A Nucleus is a function between Frames which corresponds to a sublocale.
 https://ncatlab.org/nlab/show/nucleus
 -/
-variable {X : Type*} [Order.Frame X]
+variable {X : Type*} [CompleteLattice X]
 
 /--
 The Type of Nuclei on a Frame.
 -/
-structure Nucleus (X : Type*) [Order.Frame X] where
+structure Nucleus (X : Type*) [SemilatticeInf X] where
   /-- The function of the nucleus.-/
   toFun : X → X
   /-- A Nucleus is idempotent.-/
-  idempotent (x : X) : toFun (toFun x) = toFun x
+  idempotent (x : X) : toFun (toFun x) ≤ toFun x
   /-- A Nucleus is increasing.-/
   increasing (x : X) : x ≤ toFun x
   /-- A Nucleus preserves infima.-/
   preserves_inf (x y : X) : toFun (x ⊓ y) = toFun x ⊓ toFun y
+
+instance : FunLike (Nucleus X) X X where
+  coe := Nucleus.toFun
+  coe_injective' f g h := by cases f; cases g; congr
+
+/--
+`NucleusClass F X` states that F is a type of Nuclei.
+-/
+class NucleusClass (F X : Type*) [SemilatticeInf X] [FunLike F X X] : Prop where
+  /-- A Nucleus is idempotent.-/
+  idempotent (x : X) (f : F) : f (f x) ≤ f x
+  /-- A Nucleus is increasing.-/
+  increasing (x : X) (f : F) : x ≤ f x
+  /-- A Nucleus preserves infima.-/
+  preserves_inf (x y : X) (f : F) : f (x ⊓ y) = f x ⊓ f y
+
+
+instance (F X : Type*) [SemilatticeInf X] [FunLike F X X] [n : NucleusClass F X]
+  : OrderHomClass F X X where
+  map_rel := fun f a b h => by
+    have h1 : a ⊓ b = a := inf_eq_left.mpr h
+    have h2 := n.preserves_inf a b
+    rw [h1] at h2
+    exact left_eq_inf.mp (h2 f)
+
+lemma Nucleus.coe_eq_toFun (n : Nucleus X) {x : X} : n x = n.toFun x := by rfl
+
+
+instance : NucleusClass (Nucleus X) X where
+  idempotent := (by simp[Nucleus.coe_eq_toFun];exact fun x f ↦ f.idempotent x)
+  increasing := (by simp[Nucleus.coe_eq_toFun];exact fun x f ↦ f.increasing x)
+  preserves_inf := (by simp[Nucleus.coe_eq_toFun]; exact fun x y f ↦ f.preserves_inf x y)
+
+
+/--
+We can proove that two Nuclei are equal by showing that their functions are the same.
+-/
+@[ext]
+lemma Nucleus.ext {n m : Nucleus X} (h: ∀ a, n.toFun a = m.toFun a) : n = m :=
+  DFunLike.ext n m h
+
 
 /--
 A Nucleus preserves ⊤
@@ -32,13 +74,34 @@ A Nucleus preserves ⊤
 lemma nucleus_preserves_top (n : Nucleus X) : n.toFun ⊤ = ⊤ :=
    top_le_iff.mp (n.increasing ⊤)
 
+
+instance : LE (Nucleus X) where
+  le x y := ∀ v : X, y.toFun v ≤ x.toFun v
+
+@[simp]
+lemma Nucleus.le_iff {n m : Nucleus X} : n ≤ m ↔ ∀ v : X, m.toFun v ≤ n.toFun v := by rfl
+
+instance : Preorder (Nucleus X) where
+  le_refl := (by simp only [Nucleus.le_iff, le_refl, implies_true])
+  le_trans := (by simp only [Nucleus.le_iff]; exact fun a b c a_1 a_2 v ↦
+      Preorder.le_trans (c.toFun v) (b.toFun v) (a.toFun v) (a_2 v) (a_1 v))
+
 /--
-A Nucleus is montone
+The identity Nucleus is the biggest sublocale.
 -/
-lemma nucleus_monotone (n : Nucleus X) : Monotone n.toFun := by
-  rw [Monotone]
-  intro a b h
-  have h1 : a ⊓ b = a := inf_eq_left.mpr h
-  have h2 := n.preserves_inf a b
-  rw [h1] at h2
-  exact left_eq_inf.mp h2
+instance Nucleus.top : Top (Nucleus X) where
+  top := ⟨fun x ↦ x, Preorder.le_refl,Preorder.le_refl, fun _ _ ↦ rfl⟩
+
+instance : OrderTop (Nucleus X) where
+  le_top := (by simp[Nucleus.top];exact fun a v ↦ a.increasing v)
+
+/--
+The nucleus which sends everything to ⊤ is the ⊥ sublocale.
+-/
+instance Nucleus.bot : Bot (Nucleus X) where
+  bot := ⟨fun _ ↦ ⊤,(by simp only [le_refl, implies_true]), OrderTop.le_top,
+    fun _ _ ↦ Eq.symm (top_inf_eq _)⟩
+-- Question for the reviewer: Should these small proofs be simp's or written out statements?
+
+instance : OrderBot (Nucleus X) where
+  bot_le := (by simp[Nucleus.bot])
