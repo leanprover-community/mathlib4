e4f1c5268e9e496d2e9976dcaaff3007cef4af4d
Bergschaf <christian.k@netcom-mail.de>
2024-12-21 11:29:18 +0100

    imports

--- Mathlib/Order/Nucleus.lean
+++ Mathlib/Order/Nucleus.lean
@@ -3,7 +3,7 @@ Copyright (c) 2024 Christian Krause. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Chriara Cimino, Christian Krause
 -/
-import Mathlib.Order.CompleteBooleanAlgebra
+import Mathlib.Order.CompleteLattice
 import Mathlib.Tactic.ApplyFun
 
 /-!
