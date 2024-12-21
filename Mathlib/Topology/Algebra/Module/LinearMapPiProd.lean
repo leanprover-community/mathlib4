67156a8e400bbcb78d9483610af21f284d3f5594
Yaël Dillies <yael.dillies@gmail.com>
2024-12-21 10:17:35 +0000

    whitespace
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Mathlib/Topology/Algebra/Module/LinearMapPiProd.lean
+++ Mathlib/Topology/Algebra/Module/LinearMapPiProd.lean
@@ -301,9 +301,9 @@ lemma coprod_inl_inr : ContinuousLinearMap.coprod (.inl R M N) (.inr R M N) = .i
 
 /-- Taking the product of two maps with the same codomain is equivalent to taking the product of
 their domains.
-See note [bundled maps over different rings] for why separate `R` and `S` semirings are used. 
+See note [bundled maps over different rings] for why separate `R` and `S` semirings are used.
 
-TODO: Upgrade this to a `ContinuousLinearEquiv`. This should be true for any topological 
+TODO: Upgrade this to a `ContinuousLinearEquiv`. This should be true for any topological
 vector space over a normed field thanks to `ContinuousLinearMap.precomp` and
 `ContinuousLinearMap.postcomp`. -/
 @[simps]
