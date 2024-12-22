2c782f431c6ca4fd29e443393088189f3270b0bb
ChrisHughes24 <chrishughes24@gmail.com>
2024-12-22 14:18:47 +0000

    remove unnecessary import

--- Mathlib/Data/Rat/Denumerable.lean
+++ Mathlib/Data/Rat/Denumerable.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Chris Hughes
 -/
 import Mathlib.Algebra.ContinuedFractions.Computation.RatEquiv
-import Mathlib.SetTheory.Cardinal.Basic
 
 /-!
 # Denumerability of ℚ
