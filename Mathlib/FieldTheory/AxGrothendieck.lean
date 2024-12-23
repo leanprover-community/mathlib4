1626128e3753df6878b26d00807da38218580de5
Chris Hughes <33847686+ChrisHughes24@users.noreply.github.com>
2024-12-23 16:36:17 +0000

    Update Mathlib/FieldTheory/AxGrothendieck.lean
    Co-authored-by: Johan Commelin <johan@commelin.net>

--- Mathlib/FieldTheory/AxGrothendieck.lean
+++ Mathlib/FieldTheory/AxGrothendieck.lean
@@ -109,7 +109,7 @@ noncomputable def genericPolyMapSurjOnOfInjOn [Finite ι]
     fun i => .var (Sum.inl (Sum.inr (0, i))) =' .var (Sum.inl (Sum.inr (1, i)))
   -- x = y as a formula
   let f2 : Language.ring.Formula ((Σ i : ι, mons i) ⊕ (Fin 2 × ι)) :=
-    iInf  l2
+    iInf l2
   let injOn : Language.ring.Formula (α ⊕ Σ i : ι, mons i) :=
     Formula.iAlls (γ := Fin 2 × ι) id
       (φ.relabel (Sum.map Sum.inl (fun i => (0, i))) ⟹
