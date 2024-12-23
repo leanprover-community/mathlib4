02d6a9d4f2d99049c683a403e5db7e3b91f1d9e1
Seewoo Lee <seewoo5@gmail.com>
2024-12-22 16:25:20 -0800

    fix import

--- Mathlib/Algebra/Polynomial/Derivative.lean
+++ Mathlib/Algebra/Polynomial/Derivative.lean
@@ -3,8 +3,6 @@ Copyright (c) 2018 Chris Hughes. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Chris Hughes, Johannes Hölzl, Kim Morrison, Jens Wagemaker
 -/
-import Mathlib.Algebra.CharP.Defs
-import Mathlib.Algebra.GroupPower.IterateHom
 import Mathlib.Algebra.Polynomial.Degree.Domain
 import Mathlib.Algebra.Polynomial.Degree.Support
 import Mathlib.Algebra.Polynomial.Eval.Coeff
