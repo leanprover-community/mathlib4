6f7c232692d983db18317d86789dd770c2f0ae52
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2024-12-23 05:28:58 +0100

    last two

--- Mathlib/NumberTheory/DirichletCharacter/Basic.lean
+++ Mathlib/NumberTheory/DirichletCharacter/Basic.lean
@@ -91,9 +91,9 @@ lemma changeLevel_trans {m d : ℕ} (hm : n ∣ m) (hd : m ∣ d) :
 
 lemma changeLevel_eq_cast_of_dvd {m : ℕ} (hm : n ∣ m) (a : Units (ZMod m)) :
     (changeLevel hm χ) a = χ (ZMod.cast (a : ZMod m)) := by
-  set_option tactic.skipAssignedInstances false in
-  simpa [changeLevel_def, Function.comp_apply, MonoidHom.coe_comp] using
-      toUnitHom_eq_char' _ <| ZMod.isUnit_cast_of_dvd hm a
+  simp only [changeLevel_def, toUnitHom_eq, ofUnitHom_eq, equivToUnitHom_symm_coe,
+    MonoidHom.coe_comp, Function.comp_apply, coe_equivToUnitHom]
+  rfl
 
 /-- `χ` of level `n` factors through a Dirichlet character `χ₀` of level `d` if `d ∣ n` and
 `χ₀ = χ ∘ (ZMod n → ZMod d)`. -/
