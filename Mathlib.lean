c06a93f81886f129d03fab41f3e479c1de7cfd65
Dagur Asgeirsson <dagurtomas@gmail.com>
2024-12-23 10:59:04 +0000

    feat(CategoryTheory): AB5 and AB4* for condensed modules (#19892)

--- Mathlib.lean
+++ Mathlib.lean
@@ -2340,6 +2340,7 @@ import Mathlib.Computability.RegularExpressions
 import Mathlib.Computability.TMComputable
 import Mathlib.Computability.TMToPartrec
 import Mathlib.Computability.TuringMachine
+import Mathlib.Condensed.AB
 import Mathlib.Condensed.Basic
 import Mathlib.Condensed.CartesianClosed
 import Mathlib.Condensed.Discrete.Basic
