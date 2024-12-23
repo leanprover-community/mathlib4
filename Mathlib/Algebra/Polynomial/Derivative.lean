874638058bb3e7285077c30fb2d03d47bea053af
Seewoo Lee <seewoo5@gmail.com>
2024-12-23 09:34:13 -0800

    parantheses

--- Mathlib/Algebra/Polynomial/Derivative.lean
+++ Mathlib/Algebra/Polynomial/Derivative.lean
@@ -668,7 +668,7 @@ theorem derivative_pow_eq_zero_iff {n : ℕ} (chn : (n : k) ≠ 0) {a : k[X]} :
     derivative (a ^ n) = 0 ↔ derivative a = 0 where
   mp apd := by
     rw [derivative_pow, C_eq_natCast, mul_eq_zero, mul_eq_zero] at apd
-    rcases apd with nz | powz | goal
+    rcases apd with (nz | powz) | goal
     · rw [← C_eq_natCast, C_eq_zero] at nz
       tauto
     · have az : a = 0 := pow_eq_zero powz
