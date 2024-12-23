8c640eebc7f9036afa464f31256e071a0596fd78
Scott Carnahan <128885296+ScottCarnahan@users.noreply.github.com>
2024-12-23 12:40:38 +0900

    clean up

--- Mathlib/RingTheory/PowerSeries/Binomial.lean
+++ Mathlib/RingTheory/PowerSeries/Binomial.lean
@@ -12,26 +12,24 @@ We introduce formal power series of the form `(1 + X) ^ r`, where `r` is an elem
 commutative binomial ring `R`.
 
 ## Main Definitions
- * `PowerSeries.binomialSeries`: A power series expansion of
+ * `PowerSeries.binomialSeries`: A power series expansion of `(1 + X) ^ r`, where `r` is an element
+  of a commutative binomial ring `R`.
 
-## Main results
+## Main Results
  * `PowerSeries.binomial_add`: Adding exponents yields multiplication of series.
 
-## To do
- * Comparison with `PowerSeries.invOneSubPow`
+## TODO
+ * Comparison with `PowerSeries.invOneSubPow`.
  * When `A` is a commutative `R`-algebra, exponentiation makes the group `1 + XA[[X]]` into an
    `R`-module.
 
 -/
 
-open Finset Function
-
-open scoped Classical
-open BigOperators Pointwise
+open Finset BigOperators
 
 suppress_compilation
 
-variable {Γ R A : Type*}
+variable {R A : Type*}
 
 namespace PowerSeries
 
@@ -42,16 +40,15 @@ def binomialSeries (A) [One A] [SMul R A] (r : R) : PowerSeries A :=
   mk fun n => Ring.choose r n • 1
 
 @[simp]
-lemma binomial_coeff [Semiring A] [SMul R A] (r : R) (n : ℕ) :
-    (coeff A n) (binomialSeries A r) = Ring.choose r n • 1 := by
-  dsimp only [binomialSeries]
-  exact coeff_mk n fun n ↦ Ring.choose r n • 1
+lemma binomialSeries_coeff [Semiring A] [SMul R A] (r : R) (n : ℕ) :
+    (coeff A n) (binomialSeries A r) = Ring.choose r n • 1 :=
+  coeff_mk n fun n ↦ Ring.choose r n • 1
 
 @[simp]
-lemma binomial_add [Semiring A] [Algebra R A] (r s : R) :
+lemma binomialSeries_add [Semiring A] [Algebra R A] (r s : R) :
     binomialSeries A (r + s) = binomialSeries A r * binomialSeries A s := by
   ext n
-  simp only [binomial_coeff, Ring.add_choose_eq n (Commute.all r s), coeff_mul,
+  simp only [binomialSeries_coeff, Ring.add_choose_eq n (Commute.all r s), coeff_mul,
     Algebra.mul_smul_comm, mul_one, sum_smul]
   refine sum_congr rfl fun ab hab => ?_
   rw [mul_comm, mul_smul]
