90e9d178632161f360c89b7acd240468a2a69a41
tannerduve <tannerduve@gmail.com>
2024-12-24 12:16:31 -0500

    Definition changes

--- Mathlib/Computability/TuringDegrees.lean
+++ Mathlib/Computability/TuringDegrees.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2024 Tanner Duve, Elan Roth
+Copyright (c) 2024 Tanner Duve. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Tanner Duve, Elan Roth
 -/
@@ -7,7 +7,7 @@ import Mathlib.Computability.Primrec
 import Mathlib.Computability.Partrec
 import Mathlib.Computability.Reduce
 import Mathlib.Data.Part
-
+import Mathlib.Order.Antisymmetrization
 /-!
 # Oracle Computability and Turing Degrees
 
@@ -23,13 +23,14 @@ quotient under this relation.
 - `turing_equivalent`: A relation defining Turing equivalence between partial functions.
 - `TuringDegree`:
   The type of Turing degrees, defined as equivalence classes under `turing_equivalent`.
-- `join`: Combines two partial functions into one by mapping even and odd numbers to respective functions.
+- `join`: Combines two partial functions into one by =
+  mapping even and odd numbers to respective functions.
 
 ## Notation
 
 - `f ≤ᵀ g` : `f` is Turing reducible to `g`.
 - `f ≡ᵀ g` : `f` is Turing equivalent to `g`.
-- `f ⊔ g` : The join of partial functions `f` and `g`.
+- `f ⊕ g` : The join of partial functions `f` and `g`.
 
 ## Implementation Notes
 
@@ -83,7 +84,8 @@ inductive RecursiveIn (g : ℕ →. ℕ) : (ℕ →. ℕ) → Prop
         Nat.rfind (fun n => (fun m => m = 0) <$> f (Nat.pair a n)))
 
 /--
-`f` is Turing reducible to `g` if `f` is recursive in `g`.
+`f` is Turing reducible to `g` if `f` is recursive in `g`. This is a more convenient shorthand
+for `RecursiveIn g f`.
 -/
 def turing_reducible (f g : ℕ →. ℕ) : Prop :=
   RecursiveIn g f
@@ -97,7 +99,7 @@ infix:50 " ≤ᵀ " => turing_reducible
 `f` is Turing equivalent to `g` if `f` is reducible to `g` and `g` is reducible to `f`.
 -/
 def turing_equivalent (f g : ℕ →. ℕ) : Prop :=
-  f ≤ᵀ g ∧ g ≤ᵀ f
+  AntisymmRel turing_reducible f g
 
 /--
 Custom infix notation for `turing_equivalent`.
@@ -105,59 +107,11 @@ Custom infix notation for `turing_equivalent`.
 infix:50 " ≡ᵀ " => turing_equivalent
 
 /--
-A partial function `f` is partial recursive if and only if
-it is recursive in the constant zero function.
+If a function is partial recursive, then it is recursive in every partial function.
 -/
-lemma partrec_iff_partrec_in_zero
-  (f : ℕ →. ℕ) : Nat.Partrec f ↔ RecursiveIn (fun _ => pure 0) f := by
-  constructor
-  { intro pF
-    induction' pF
-    case mp.zero =>
-      apply RecursiveIn.zero
-    case mp.succ =>
-      apply RecursiveIn.succ
-    case mp.left =>
-      apply RecursiveIn.left
-    case mp.right =>
-      apply RecursiveIn.right
-    case mp.pair _ _ _ _ ih1 ih2 =>
-      apply RecursiveIn.pair ih1 ih2
-    case mp.comp _ _ _ _ ih1 ih2 =>
-      apply RecursiveIn.comp ih1 ih2
-    case mp.prec _ _ _ _ ih1 ih2 =>
-      apply RecursiveIn.prec ih1 ih2
-    case mp.rfind _ _ ih =>
-      apply RecursiveIn.rfind ih }
-  { intro fRecInNone
-    induction' fRecInNone
-    case mpr.zero =>
-      apply Nat.Partrec.zero
-    case mpr.succ =>
-      apply Nat.Partrec.succ
-    case mpr.left =>
-      apply Nat.Partrec.left
-    case mpr.right =>
-      apply Nat.Partrec.right
-    case mpr.oracle =>
-      apply Nat.Partrec.zero
-    case mpr.pair _ _ _ _ ih1 ih2 =>
-      apply Nat.Partrec.pair ih1 ih2
-    case mpr.comp _ _ _ _ ih1 ih2 =>
-      apply Nat.Partrec.comp ih1 ih2
-    case mpr.prec _ _ _ _ ih1 ih2 =>
-      apply Nat.Partrec.prec ih1 ih2
-    case mpr.rfind _ _ ih =>
-      apply Nat.Partrec.rfind ih }
-
-/--
-A partial function `f` is partial recursive if and only if it is recursive in
-every partial function `g`.
--/
-theorem partrec_iff_partrec_in_everything
-  (f : ℕ →. ℕ) : Nat.Partrec f ↔ (∀ g, RecursiveIn g f) := by
-  constructor
-  { intro pF
+lemma partrec_implies_recursive_in_everything
+  (f : ℕ →. ℕ) : Nat.Partrec f → (∀ g, RecursiveIn g f) := by
+    intro pF
     intro g
     induction pF
     case zero =>
@@ -175,11 +129,69 @@ theorem partrec_iff_partrec_in_everything
     case prec _ _ _ _ ih1 ih2 =>
       apply RecursiveIn.prec ih1 ih2
     case rfind _ _ ih =>
-      apply RecursiveIn.rfind ih }
-  { intro H
+      apply RecursiveIn.rfind ih
+
+/--
+If a function is recursive in the constant zero function,
+then it is partial recursive.
+-/
+lemma partrec_in_zero_implies_partrec
+(f : ℕ →. ℕ) : RecursiveIn (fun _ => pure 0) f → Nat.Partrec f := by
+  intro fRecInZero
+  induction fRecInZero
+  case zero =>
+    apply Nat.Partrec.zero
+  case succ =>
+    apply Nat.Partrec.succ
+  case left =>
+    apply Nat.Partrec.left
+  case right =>
+    apply Nat.Partrec.right
+  case oracle =>
+    apply Nat.Partrec.zero
+  case pair _ _ _ _ ih1 ih2 =>
+    apply Nat.Partrec.pair ih1 ih2
+  case comp _ _ _ _ ih1 ih2 =>
+    apply Nat.Partrec.comp ih1 ih2
+  case prec _ _ _ _ ih1 ih2 =>
+    apply Nat.Partrec.prec ih1 ih2
+  case rfind _ _ ih =>
+    apply Nat.Partrec.rfind ih
+
+lemma partrec_iff_partrec_in_zero
+  (f : ℕ →. ℕ) : Nat.Partrec f ↔ RecursiveIn (fun _ => pure 0) f := by
+  constructor
+  · intro pF
+    apply partrec_implies_recursive_in_everything
+    assumption
+  · intro h
+    apply partrec_in_zero_implies_partrec
+    assumption
+
+/-
+Alternative definition of partial recursive using the above lemma.
+-/
+def Partrec₀ (f : ℕ →. ℕ) : Prop :=
+  RecursiveIn (fun _ => pure 0) f
+
+/--
+A partial function `f` is partial recursive if and only if it is recursive in
+every partial function `g`.
+-/
+theorem partrec_iff_partrec_in_everything
+  (f : ℕ →. ℕ) : Nat.Partrec f ↔ (∀ g, RecursiveIn g f) := by
+  constructor
+  · exact partrec_implies_recursive_in_everything f
+  · intro H
     have lem : RecursiveIn (fun _ => pure 0) f := H (fun _ => pure 0)
     rw [← partrec_iff_partrec_in_zero] at lem
-    exact lem }
+    exact lem
+
+/--
+ Alternative definition of partial recursive using the above lemma.
+-/
+def Partrec₁ (f : ℕ →. ℕ) : Prop :=
+  ∀ g, RecursiveIn g f
 
 /--
 Proof that `turing_reducible` is reflexive.
@@ -190,18 +202,21 @@ theorem turing_reducible_refl (f : ℕ →. ℕ) : f ≤ᵀ f :=
 /--
 Proof that `turing_equivalent` is reflexive.
 -/
+@[refl]
 theorem turing_equivalent_refl (f : ℕ →. ℕ) : f ≡ᵀ f :=
   ⟨turing_reducible_refl f, turing_reducible_refl f⟩
 
 /--
 Proof that `turing_equivalent` is symmetric.
 -/
+@[symm]
 theorem turing_equivalent_symm {f g : ℕ →. ℕ} (h : f ≡ᵀ g) : g ≡ᵀ f :=
   ⟨h.2, h.1⟩
 
 /--
 Proof that `turing_reducible` is transitive.
 -/
+@[trans]
 theorem turing_reducible_trans {f g h : ℕ →. ℕ} :
   f ≤ᵀ g → g ≤ᵀ h → f ≤ᵀ h := by
     intro hg hh
@@ -218,19 +233,19 @@ theorem turing_reducible_trans {f g h : ℕ →. ℕ} :
       exact hh
     case pair f' h' _ _ hf_ih hh_ih =>
       apply RecursiveIn.pair
-      { apply hf_ih }
-      { apply hh_ih }
+      · apply hf_ih
+      · apply hh_ih
     case comp f' h' _ _ hf_ih hh_ih =>
       apply RecursiveIn.comp
-      { apply hf_ih }
-      { apply hh_ih }
+      · apply hf_ih
+      · apply hh_ih
     case prec f' h' _ _ hf_ih hh_ih =>
       apply RecursiveIn.prec
-      { apply hf_ih }
-      { apply hh_ih }
+      · apply hf_ih
+      · apply hh_ih
     case rfind f' _ hf_ih =>
       apply RecursiveIn.rfind
-      { apply hf_ih }
+      · apply hf_ih
 
 /--
 Proof that `turing_equivalent` is transitive.
@@ -251,15 +266,29 @@ instance : Equivalence turing_equivalent :=
   }
 
 /--
+Instance declaring that `RecursiveIn` is a preorder.
+-/
+instance : IsPreorder (ℕ →. ℕ) turing_reducible where
+  refl := turing_reducible_refl
+  trans := @turing_reducible_trans
+
+/--
 The Turing degrees as the set of equivalence classes under Turing equivalence.
 -/
 def TuringDegree :=
-  Quot turing_equivalent
+  Antisymmetrization _ turing_reducible
 
 /--
 The `join` function combines two partial functions `f` and `g` into a single partial function.
-It maps even numbers to the result of `f` applied to half the number,
-and odd numbers to the result of `g` applied to half the number.
+For a given input `n`:
+
+- **If `n` is even**:
+  - It checks if `f` is defined at `n / 2`.
+  - If so, `join f g` is defined at `n` with the value `2 * f(n / 2)`.
+
+- **If `n` is odd**:
+  - It checks if `g` is defined at `n / 2`.
+  - If so, `join f g` is defined at `n` with the value `2 * g(n / 2) + 1`.
 -/
 def join (f g : ℕ →. ℕ) : ℕ →. ℕ :=
   fun n =>
@@ -268,8 +297,8 @@ def join (f g : ℕ →. ℕ) : ℕ →. ℕ :=
     else
       (g (n / 2)).map (fun y => 2 * y + 1)
 
-/-- Join notation `f ⊔ g` is the "join" of partial functions `f` and `g`. -/
-infix:99 "⊔" => join
+/-- Join notation `f ⊕ g` is the "join" of partial functions `f` and `g`. -/
+infix:99 "⊕" => join
 
 /--
 For any partial functions `a`, `b₁`, and `b₂`, if `b₁` is Turing equivalent to `b₂`,
@@ -300,7 +329,7 @@ f ≡ᵀ g → (turing_reducible f h = turing_reducible g h) := by
 
 /--
 Here we show how to lift the Turing reducibility relation from
-partial functions to theit Turing degrees, using the above lemmas.
+partial functions to their Turing degrees, using the above lemmas.
 -/
 def TuringDegree.turing_red (d₁ d₂ : TuringDegree) : Prop :=
   @Quot.lift₂ _ _ Prop (turing_equivalent)
