77b71d51a1190bd9de4acbc956373aa79a9c1b38
ChrisHughes24 <chrishughes24@gmail.com>
2024-12-25 18:20:56 +0000

    remove import

--- Mathlib/Algebra/ContinuedFractions/Computation/Basic.lean
+++ Mathlib/Algebra/ContinuedFractions/Computation/Basic.lean
@@ -6,7 +6,6 @@ Authors: Kevin Kappelmann
 import Mathlib.Algebra.Order.Floor
 import Mathlib.Algebra.ContinuedFractions.Basic
 import Mathlib.Tactic.ApplyFun
-import Mathlib.Data.PNat.Basic
 
 /-!
 # Computable Continued Fractions
