f7801af8bd452c30087dc82a71e2c27b2e3b3d7f
morel <smorel@math.princeton.edu>
2024-12-25 17:09:04 +0100

    more fiddling

--- Mathlib/CategoryTheory/Adjunction/Additive.lean
+++ Mathlib/CategoryTheory/Adjunction/Additive.lean
@@ -39,13 +39,14 @@ lemma right_adjoint_additive [F.Additive] : G.Additive where
 lemma left_adjoint_additive [G.Additive] : F.Additive where
   map_add {X Y} f g := (adj.homEquiv _ _).injective (by simp [homEquiv_unit])
 
+variable [F.Additive]
+
 /-- If we have an adjunction `adj : F ⊣ G` of functors between preadditive categories,
 and if `F` is additive, then the hom set equivalence upgrades to an `AddEquiv`.
 Note that `F` is additive if and only if `G` is, by `Adjunction.right_adjoint_additive` and
 `Adjunction.left_adjoint_additive`.
 -/
-def homAddEquiv [F.Additive] (X : C) (Y : D) :
-    AddEquiv (F.obj X ⟶ Y) (X ⟶ G.obj Y) :=
+def homAddEquiv (X : C) (Y : D) : AddEquiv (F.obj X ⟶ Y) (X ⟶ G.obj Y) :=
   {
     adj.homEquiv _ _ with
     map_add' _ _ := by
@@ -53,48 +54,59 @@ def homAddEquiv [F.Additive] (X : C) (Y : D) :
       simp [homEquiv_apply] }
 
 @[simp]
-lemma homAddEquiv_apply [F.Additive] (X : C) (Y : D) (f : F.obj X ⟶ Y) :
+lemma homAddEquiv_apply (X : C) (Y : D) (f : F.obj X ⟶ Y) :
     adj.homAddEquiv X Y f = adj.homEquiv X Y f := rfl
 
 @[simp]
-lemma homAddEquiv_symm_apply [F.Additive] (X : C) (Y : D) (f : X ⟶ G.obj Y) :
+lemma homAddEquiv_symm_apply (X : C) (Y : D) (f : X ⟶ G.obj Y) :
     (adj.homAddEquiv X Y).symm f = (adj.homEquiv X Y).symm f := rfl
 
 @[simp]
-lemma homAddEquiv_zero [F.Additive] (X : C) (Y : D) :
-    adj.homAddEquiv X Y 0 = 0 := by rw [map_zero]
+lemma homAddEquiv_zero (X : C) (Y : D) : adj.homEquiv X Y 0 = 0 := by
+  change adj.homAddEquiv X Y 0 = 0
+  rw [map_zero]
 
 @[simp]
-lemma homAddEquiv_add [F.Additive] (X : C) (Y : D) (f f' : F.obj X ⟶ Y) :
-    adj.homAddEquiv X Y (f + f') = adj.homAddEquiv X Y f + adj.homAddEquiv X Y f' := by
-  rw [AddEquivClass.map_add]
+lemma homAddEquiv_add (X : C) (Y : D) (f f' : F.obj X ⟶ Y) :
+    adj.homEquiv X Y (f + f') = adj.homEquiv X Y f + adj.homEquiv X Y f' := by
+  change adj.homAddEquiv X Y (f + f') = _
+  simp [AddEquivClass.map_add]
 
 @[simp]
-lemma homAddEquiv_sub [F.Additive] (X : C) (Y : D) (f f' : F.obj X ⟶ Y) :
-    adj.homAddEquiv X Y (f - f') = adj.homAddEquiv X Y f - adj.homAddEquiv X Y f' := by
-  rw [AddEquiv.map_sub]
+lemma homAddEquiv_sub (X : C) (Y : D) (f f' : F.obj X ⟶ Y) :
+    adj.homEquiv X Y (f - f') = adj.homEquiv X Y f - adj.homEquiv X Y f' := by
+  change adj.homAddEquiv X Y (f - f') = _
+  simp [AddEquiv.map_sub]
 
 @[simp]
-lemma homAddEquiv_neg [F.Additive] (X : C) (Y : D) (f : F.obj X ⟶ Y) :
-    adj.homAddEquiv X Y (- f) = - adj.homAddEquiv X Y f := by rw [AddEquiv.map_neg]
+lemma homAddEquiv_neg (X : C) (Y : D) (f : F.obj X ⟶ Y) :
+    adj.homEquiv X Y (- f) = - adj.homEquiv X Y f := by
+  change adj.homAddEquiv X Y (- f) = _
+  simp [AddEquiv.map_neg]
 
 @[simp]
-lemma homAddEquiv_symm_zero [F.Additive] (X : C) (Y : D) :
-    (adj.homAddEquiv X Y).symm 0 = 0 := by rw [map_zero]
+lemma homAddEquiv_symm_zero (X : C) (Y : D) :
+    (adj.homEquiv X Y).symm 0 = 0 := by
+  change (adj.homAddEquiv X Y).symm 0 = 0
+  rw [map_zero]
 
 @[simp]
-lemma homAddEquiv_symm_add [F.Additive] (X : C) (Y : D) (f f' : X ⟶ G.obj Y) :
-    (adj.homAddEquiv X Y).symm (f + f') = (adj.homAddEquiv X Y).symm f +
-      (adj.homAddEquiv X Y).symm f' := by rw [AddEquivClass.map_add]
+lemma homAddEquiv_symm_add (X : C) (Y : D) (f f' : X ⟶ G.obj Y) :
+    (adj.homEquiv X Y).symm (f + f') = (adj.homEquiv X Y).symm f + (adj.homEquiv X Y).symm f' := by
+  change (adj.homAddEquiv X Y).symm (f + f') = _
+  simp [AddEquivClass.map_add]
 
 @[simp]
-lemma homAddEquiv_symm_sub [F.Additive] (X : C) (Y : D) (f f' : X ⟶ G.obj Y) :
-    (adj.homAddEquiv X Y).symm (f - f') = (adj.homAddEquiv X Y).symm f
-      - (adj.homAddEquiv X Y).symm f' := by rw [AddEquiv.map_sub]
+lemma homAddEquiv_symm_sub (X : C) (Y : D) (f f' : X ⟶ G.obj Y) :
+    (adj.homEquiv X Y).symm (f - f') = (adj.homEquiv X Y).symm f - (adj.homEquiv X Y).symm f' := by
+  change (adj.homAddEquiv X Y).symm (f - f') = _
+  simp [AddEquiv.map_sub]
 
 @[simp]
-lemma homAddEquiv_symm_neg [F.Additive] (X : C) (Y : D) (f : X ⟶ G.obj Y) :
-    (adj.homAddEquiv X Y).symm (- f) = - (adj.homAddEquiv X Y).symm f := by rw [AddEquiv.map_neg]
+lemma homAddEquiv_symm_neg (X : C) (Y : D) (f : X ⟶ G.obj Y) :
+    (adj.homEquiv X Y).symm (- f) = - (adj.homEquiv X Y).symm f := by
+  change (adj.homAddEquiv X Y).symm (- f) = _
+  simp [AddEquiv.map_neg]
 
 open Opposite in
 /-- If we have an adjunction `adj : F ⊣ G` of functors between preadditive categories,
@@ -104,8 +116,8 @@ universe lifting functors.
 Note that `F` is additive if and only if `G` is, by `Adjunction.right_adjoint_additive` and
 `Adjunction.left_adjoint_additive`.
 -/
-@[simps!? (config := .lemmasOnly)]
-def compPreadditiveYonedaIsoOfLeftAdjoint [F.Additive] :
+@[simps!?]
+def compPreadditiveYonedaIso :
     G ⋙ preadditiveYoneda ⋙ (whiskeringRight _ _ _).obj AddCommGrp.uliftFunctor.{max v₁ v₂} ≅
       preadditiveYoneda ⋙ (whiskeringLeft _ _ _).obj F.op ⋙
         (whiskeringRight _ _ _).obj AddCommGrp.uliftFunctor.{max v₁ v₂} := by
@@ -127,6 +139,12 @@ def compPreadditiveYonedaIsoOfLeftAdjoint [F.Additive] :
       AddEquiv.symm_symm, AddEquiv.apply_symm_apply, whiskeringLeft_obj_map, whiskerLeft_app]
     erw [adj.homEquiv_naturality_right_symm]
     rfl
+
+lemma compPreadditiveYonedaIso_hom_app_app_apply_down' (X : Cᵒᵖ) (Y : D)
+    (a : ↑(((G ⋙ preadditiveYoneda ⋙ (whiskeringRight Cᵒᵖ AddCommGrp AddCommGrp).obj
+    AddCommGrp.uliftFunctor).obj Y).obj X)) :
+        (((adj.compPreadditiveYonedaIso.hom.app Y).app X) a).down =
+          (adj.homEquiv (Opposite.unop X) Y).symm (AddEquiv.ulift a) := sorry
 
 end Adjunction
 
