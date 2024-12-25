86d70a291587054c83cf8141f201ef69cbb3775c
Marshall Abrams <marshall@logical.net>
2024-12-25 11:35:45 -0600

    Modified lakefile.lean to point to "https://github.com/mars0i/ProofWidgets4.git" @ "RechartsAxisType"
    The RechartsAxisType branch of ProofWidgets4 at github.com/mars0i
    adds `AxisType` to ProofWidgets/Components/Recharts.lean.
    
    It also modifies the `Plot` function in
    ProofWidgets/Demos/Plot.lean to make it easy to see the
    difference between using `<XAxis ...  type={.number}>` (the new
    default) and `<XAxis ...  type={.category}>` (the old default
    implicitly inherited from the Recharts).

--- lakefile.lean
+++ lakefile.lean
@@ -10,7 +10,10 @@ open Lake DSL
 require "leanprover-community" / "batteries" @ git "main"
 require "leanprover-community" / "Qq" @ git "v4.14.0"
 require "leanprover-community" / "aesop" @ git "master"
-require "leanprover-community" / "proofwidgets" @ git "v0.0.48"
+
+-- require "leanprover-community" / "proofwidgets" @ git "v0.0.48"
+require proofwidgets from git "https://github.com/mars0i/ProofWidgets4.git" @ "RechartsAxisType"
+
 require "leanprover-community" / "importGraph" @ git "v4.15.0-rc1"
 require "leanprover-community" / "LeanSearchClient" @ git "main"
 require "leanprover-community" / "plausible" @ git "v4.15.0-rc1"
