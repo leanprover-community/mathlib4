e3b9a385e1db26e45697de0f8e16d3e0edc4a624
artie2000 <artem.khovanov@gmail.com>
2024-12-25 19:16:08 +0000

    make use of existing machinery

--- Mathlib/Algebra/Ring/SumsOfSquares.lean
+++ Mathlib/Algebra/Ring/SumsOfSquares.lean
@@ -24,6 +24,62 @@ of `R` whose carrier is the subset `{S : R | IsSumSq S}`.
 
 -/
 
+/- TODO : move to right place -/
+namespace Subsemigroup
+variable {S : Type*} [CommSemigroup S] {a : S}
+
+variable (S) in
+/--
+In a commutative semigroup `S`, the type `Subsemigroup.sqIn S`
+is the subsemigroup of squares in `S`.
+-/
+@[to_additive evenIn
+"In a commutative additive semigroup `S`, the type `AddSubsemigroup.evenIn S`
+is the subsemigroup of even elements of `S`."]
+def squareIn : Subsemigroup S where
+  carrier := {s : S | IsSquare s}
+  mul_mem' := IsSquare.mul
+
+@[to_additive (attr := simp)]
+theorem mem_squareIn : a ∈ squareIn S ↔ IsSquare a := Iff.rfl
+
+@[to_additive (attr := simp, norm_cast)]
+theorem coe_squareIn : squareIn S = {s : S | IsSquare s} := rfl
+
+end Subsemigroup
+
+/- TODO : move to right place -/
+namespace Submonoid
+variable {M : Type*} [CommMonoid M] {a : M}
+
+variable (M) in
+/--
+In a commutative monoid `M`, the type `Submonoid.sqIn M`
+is the submonoid of squares in `M`.
+-/
+@[to_additive evenIn
+"In a commutative additive monoid `M`, the type `AddSubmonoid.evenIn M`
+is the submonoid of even elements of `M`."]
+def squareIn : Submonoid M where
+  carrier := {s : M | IsSquare s}
+  one_mem' := IsSquare.one
+  mul_mem' := IsSquare.mul
+
+@[to_additive (attr := simp)]
+theorem mem_squareIn : a ∈ squareIn M ↔ IsSquare a := Iff.rfl
+
+@[to_additive (attr := simp, norm_cast)]
+theorem coe_squareIn : squareIn M = {s : M | IsSquare s} := rfl
+
+end Submonoid
+
+/- TODO : move to right place -/
+theorem IsSquare.nonneg {R : Type*} [Semiring R] [LinearOrder R] [IsRightCancelAdd R]
+    [ZeroLEOneClass R] [ExistsAddOfLE R] [PosMulMono R] [AddLeftStrictMono R]
+    {x : R} (h : IsSquare x) : 0 ≤ x := by
+  rcases h with ⟨y, rfl⟩
+  exact mul_self_nonneg y
+
 universe u
 variable {R : Type*}
 
@@ -118,21 +174,17 @@ theorem AddSubmonoid.closure_isSquare [AddMonoid R] [Mul R] :
 
 @[deprecated (since := "2024-08-09")] alias SquaresAddClosure := AddSubmonoid.closure_isSquare
 
+/- TODO : remove? -/
+theorem AddSubmonoid.closure_squareIn [AddMonoid R] [CommSemiring R] :
+    (closure <| Submonoid.squareIn R) = sumSqIn R := by simp
+
 private theorem IsSumSq.isSquare_mul [NonUnitalCommSemiring R] {x S : R}
     (hx : IsSquare x) (hS : IsSumSq S) : IsSumSq (x * S) := by
   induction hS using rec'
   case zero   => aesop
-  case sq_add => rw [left_distrib]; aesop (add unsafe apply IsSquare.mul)
+  case sq_add a S ha hS ih => rw [left_distrib]; aesop (add unsafe apply IsSquare.mul)
 
-/-- In a (not necessarily unital) commutative semiring,
-if `S₁` and `S₂` are sums of squares, then `S₁ * S₂` is a sum of squares. -/
-@[aesop unsafe 50% apply]
-theorem IsSumSq.mul [NonUnitalCommSemiring R] {S₁ S₂ : R}
-    (h₁ : IsSumSq S₁) (h₂ : IsSumSq S₂) : IsSumSq (S₁ * S₂) := by
-  induction h₁ using rec'
-  case zero => aesop
-  case sq_add x hx S₁ hS₁ h_sum => rw [right_distrib]; aesop (add unsafe apply isSquare_mul)
-
+/- TODO : Do non unital version? -/
 namespace Subsemiring
 variable {T : Type*} [CommSemiring T] {a : T}
 
@@ -141,43 +193,37 @@ variable (T) in
 In a commutative semiring `R`, the type `Subsemiring.sumSqIn R`
 is the subsemiring of sums of squares in `R`.
 -/
-def sumSqIn : Subsemiring T where
-  __ := AddSubmonoid.sumSqIn T
-  mul_mem' := IsSumSq.mul
-  one_mem' := IsSquare.isSumSq isSquare_one
+def sumSqIn : Subsemiring T := (Submonoid.squareIn T).subsemiringClosure
 
-@[simp] theorem sumSqIn_toAddSubmonoid : (sumSqIn T).toAddSubmonoid = .sumSqIn T := rfl
-@[simp] theorem mem_sumSqIn : a ∈ sumSqIn T ↔ IsSumSq a := Iff.rfl
-@[simp, norm_cast] theorem coe_sumSqIn : sumSqIn T = {x : T | IsSumSq x} := rfl
+@[simp] theorem sumSqIn_toAddSubmonoid : (sumSqIn T).toAddSubmonoid = .sumSqIn T := by
+  rw [sumSqIn, ← AddSubmonoid.closure_isSquare, Submonoid.subsemiringClosure_toAddSubmonoid]
+  simp
+
+@[simp]
+theorem mem_sumSqIn : a ∈ sumSqIn T ↔ IsSumSq a := by rw [← Subsemiring.mem_toAddSubmonoid]; simp
+
+@[simp, norm_cast] theorem coe_sumSqIn : sumSqIn T = {x : T | IsSumSq x} := by ext; simp
+
+@[simp] theorem closure_isSquare : closure {x : T | IsSquare x} = sumSqIn T := by
+  rw [sumSqIn, Submonoid.subsemiringClosure_eq_closure]
+  simp
 
 end Subsemiring
+
+/- TODO : Do non unital version? -/
+/-- In a commutative semiring,
+if `S₁` and `S₂` are sums of squares, then `S₁ * S₂` is a sum of squares. -/
+@[aesop unsafe 50% apply]
+theorem IsSumSq.mul [CommSemiring R] {s₁ s₂ : R}
+    (h₁ : IsSumSq s₁) (h₂ : IsSumSq s₂) : IsSumSq (s₁ * s₂) := by
+  simpa using mul_mem (by simpa : _ ∈ Subsemiring.sumSqIn R) (by simpa)
 
 /-- In a commutative semiring, a finite product of sums of squares
 is a sum of squares. -/
 @[aesop unsafe 50% apply]
 theorem IsSumSq.prod [CommSemiring R] {ι : Type*} {I : Finset ι} {f : ι → R}
     (hf : ∀ i ∈ I, IsSumSq <| f i) : IsSumSq (∏ i ∈ I, f i) := by
-  simpa using prod_mem (S := Subsemiring.sumSqIn R) hf
-
-/--
-In a commutative semiring `R`, the subsemiring generated by the squares is the set of
-sums of squares in `R`.
--/
-@[simp]
-theorem Subsemiring.closure_isSquare [CommSemiring R] :
-    closure {x : R | IsSquare x} = sumSqIn R := by
-  refine closure_eq_of_le (fun x hx => IsSquare.isSumSq hx) (fun x hx ↦ ?_)
-  induction (show IsSumSq x by simpa) using IsSumSq.rec'
-  case zero => aesop
-  case sq_add a S ha hS h_sum => exact add_mem (subset_closure ha) (h_sum hS)
-  /- TODO : automate properly -/
-
-/- TODO : move to Group.Even and make multiplicative -/
-lemma IsSquare.nonneg {R : Type*} [Semiring R] [LinearOrder R] [IsRightCancelAdd R]
-    [ZeroLEOneClass R] [ExistsAddOfLE R] [PosMulMono R] [AddLeftStrictMono R]
-    {x : R} (h : IsSquare x) : 0 ≤ x := by
-  rcases h with ⟨y, rfl⟩
-  exact mul_self_nonneg y
+  simpa using prod_mem (S := Subsemiring.sumSqIn R) (by simpa)
 
 /--
 Let `R` be a linearly ordered semiring in which the property `a ≤ b → ∃ c, a + c = b` holds
