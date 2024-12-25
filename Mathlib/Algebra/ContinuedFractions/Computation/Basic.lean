c750167aeab0a3f05e7fff877fbbf91fe4893fbc
ChrisHughes24 <chrishughes24@gmail.com>
2024-12-25 17:59:17 +0000

    simplify

--- Mathlib/Algebra/ContinuedFractions/Computation/Basic.lean
+++ Mathlib/Algebra/ContinuedFractions/Computation/Basic.lean
@@ -145,55 +145,6 @@ protected def stream (v : K) : Stream' <| Option (IntFractPair K)
     (IntFractPair.stream v n).bind fun ap_n =>
       if ap_n.fr = 0 then none else some (IntFractPair.of ap_n.fr⁻¹)
 
-private theorem stream_fr_nonneg_and_lt_one {v : K} : ∀ {n : ℕ} (p : IntFractPair K),
-    p ∈ IntFractPair.stream v n → 0 ≤ p.fr ∧ p.fr < 1
-  | 0, p => by simp +contextual
-    [IntFractPair.stream, IntFractPair.of, Int.fract_lt_one, @eq_comm _ _ p]
-  | n + 1, p => by
-    simp only [IntFractPair.stream]
-    cases IntFractPair.stream v n with
-    | none => simp
-    | some q =>
-      simp +contextual only [IntFractPair.of, Option.some_bind, Option.ite_none_left_eq_some,
-        Option.some.injEq, @eq_comm _ _ p, Int.fract_nonneg, Int.fract_lt_one, and_self,
-        implies_true, Option.mem_def]
-
-theorem stream_fr_nonneg {v : K} {n : ℕ} {p : IntFractPair K}
-    (hp : p ∈ IntFractPair.stream v n) : 0 ≤ p.fr :=
-  (stream_fr_nonneg_and_lt_one p hp).1
-
-theorem stream_fr_lt_one {v : K} {n : ℕ} {p : IntFractPair K}
-    (hp : p ∈ IntFractPair.stream v n) : p.fr < 1 :=
-  (stream_fr_nonneg_and_lt_one p hp).2
-
-theorem stream_b_pos {v : K} : ∀ {n : ℕ}, 0 < n → ∀ (p : IntFractPair K),
-    p ∈ IntFractPair.stream v n → 0 < p.b
-  | 0, h, p => (lt_irrefl _ h).elim
-  | n + 1, _, p => by
-    simp only [IntFractPair.stream]
-    cases h : IntFractPair.stream v n with
-    | none => simp
-    | some q =>
-      simp +contextual only [IntFractPair.of, Option.some_bind, Option.mem_def,
-        Option.ite_none_left_eq_some, Option.some.injEq, @eq_comm _ _ p, Int.floor_pos, and_imp]
-      rintro h0 rfl
-      exact (one_le_inv₀ (lt_of_le_of_ne (stream_fr_nonneg_and_lt_one _ h).1 (Ne.symm h0))).2
-        (le_of_lt (stream_fr_nonneg_and_lt_one _ h).2)
-
-theorem last_stream_ne_one (v : K) (n : ℕ) : ∀ (p : IntFractPair K),
-    p ∈ IntFractPair.stream v (n + 1) → IntFractPair.stream v (n + 2) = none →
-    p.b ≠ 1 := by
-  simp only [IntFractPair.stream]
-  cases hq : IntFractPair.stream v n with
-  | none => simp
-  | some q =>
-    simp +contextual only [IntFractPair.of, Option.some_bind, Option.mem_def,
-      Option.ite_none_left_eq_some, Option.some.injEq, ↓reduceIte, ite_eq_left_iff, reduceCtorEq,
-      imp_false, Decidable.not_not, ne_eq, and_imp, forall_apply_eq_imp_iff, Int.fract_eq_iff,
-      le_refl, zero_lt_one, sub_zero, true_and, forall_exists_index, Int.floor_intCast]
-    rintro h₁ x hx rfl
-    simp [ne_of_lt (stream_fr_lt_one hq)] at hx
-
 /-- Shows that `IntFractPair.stream` has the sequence property, that is once we return `none` at
 position `n`, we also return `none` at `n + 1`.
 -/
@@ -217,17 +168,6 @@ protected def seq1 (v : K) : Stream'.Seq1 <| IntFractPair K :=
       -- create a sequence from `IntFractPair.stream`
       ⟨IntFractPair.stream v, -- the underlying stream
         @stream_isSeq _ _ _ v⟩⟩ -- the proof that the stream is a sequence
-
-theorem one_not_mem_getLast_seq1 (v : K) (ht : (IntFractPair.seq1 v).2.Terminates) (a : K) :
-    ⟨1, a⟩ ∉ (((IntFractPair.seq1 v).2).toList ht).getLast? := by
-  cases h : (IntFractPair.seq1 v).2.length ht with
-  | zero => simp [Stream'.Seq.length_eq_zero.1 h]
-  | succ n =>
-    rw [Stream'.Seq.getLast?_toList]
-    intro hmem
-    refine last_stream_ne_one v _ _ hmem ?_ rfl
-    apply (Stream'.Seq.length_le_iff (s := (IntFractPair.seq1 v).2) (h := ht)).1
-    simp [h]
 
 end IntFractPair
 
@@ -272,33 +212,5 @@ protected def of (v : K) : ContFract :=
   ⟨s.1.b, -- the head is just the first integer part
     s.2.map fun p => p.b.toNat.toPNat'⟩
     /- the sequence consists of the remaining integer parts as the partial denominators -/
-
-theorem one_not_mem_getLast?_of (v : K) (ht : (ContFract.of v).s.Terminates) :
-    1 ∉ ((ContFract.of v).s.toList ht).getLast? := by
-  have := IntFractPair.one_not_mem_getLast_seq1 v (Stream'.Seq.terminates_map_iff.1 ht)
-  simp only [Stream'.Seq.getLast?_toList, Option.mem_def, ContFract.of, Stream'.Seq.length_map,
-    Stream'.Seq.map_get?, Option.map_eq_some', not_exists, not_and] at *
-  rintro ⟨xb, xfr⟩ hx hx1
-  apply this xfr
-  simp only [hx, Option.some.injEq, IntFractPair.mk.injEq, and_true]
-  have h0xb : 0 < xb := IntFractPair.stream_b_pos (Nat.succ_pos _) _ hx
-  apply_fun ((↑) : ℕ+ → ℤ) at hx1
-  simp only [Nat.toPNat'_coe, Int.lt_toNat, Nat.cast_zero, Nat.cast_ite, Int.ofNat_toNat,
-    Nat.cast_one, PNat.val_ofNat, ite_eq_right_iff, max_eq_left (le_of_lt h0xb)] at hx1
-  exact hx1 h0xb
-
-@[simp]
-theorem coe_of (v : K) : ((ContFract.of v) : GenContFract K) = GenContFract.of v := by
-  simp only [toGenContFract, ContFract.of, IntFractPair.seq1, Stream'.Seq.map_tail, GenContFract.of,
-    GenContFract.mk.injEq, true_and] at *
-  ext n s
-  simp only [Stream'.Seq.get?_tail, Stream'.Seq.map_get?, Stream'.Seq.get?_mk, Option.map_map,
-    Option.mem_def, Option.map_eq_some', Function.comp_apply, Nat.toPNat'_coe, Int.lt_toNat,
-    Nat.cast_zero, Nat.cast_ite, Nat.cast_one]
-  refine exists_congr fun p => ?_
-  simp only [and_congr_right_iff]
-  intro h
-  rw [if_pos (IntFractPair.stream_b_pos n.succ_pos _ h), ← Int.cast_natCast,
-     Int.toNat_of_nonneg (le_of_lt (IntFractPair.stream_b_pos n.succ_pos _ h))]
 
 end ContFract
