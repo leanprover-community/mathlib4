77a24822f069d79008702d73fb80d701ed58a53f
morel <smorel@math.princeton.edu>
2024-12-25 16:22:11 +0100

    remove half the lemmas but add more lemmas

--- Mathlib/CategoryTheory/Adjunction/Additive.lean
+++ Mathlib/CategoryTheory/Adjunction/Additive.lean
@@ -40,9 +40,12 @@ lemma left_adjoint_additive [G.Additive] : F.Additive where
   map_add {X Y} f g := (adj.homEquiv _ _).injective (by simp [homEquiv_unit])
 
 /-- If we have an adjunction `adj : F ⊣ G` of functors between preadditive categories,
-and if `F` is additive, then the hom set equivalence upgrades to an `AddEquiv`.-/
-@[simps! (config := .lemmasOnly)]
-def homAddEquivOfLeftAdjoint [F.Additive] (X : C) (Y : D) :
+and if `F` is additive, then the hom set equivalence upgrades to an `AddEquiv`.
+Note that `F` is additive if and only if `G` is, by `Adjunction.right_adjoint_additive` and
+`Adjunction.left_adjoint_additive`.
+-/
+@[simps!]
+def homAddEquiv [F.Additive] (X : C) (Y : D) :
     AddEquiv (F.obj X ⟶ Y) (X ⟶ G.obj Y) :=
   {
     adj.homEquiv _ _ with
@@ -50,20 +53,50 @@ def homAddEquivOfLeftAdjoint [F.Additive] (X : C) (Y : D) :
       have := adj.right_adjoint_additive
       simp [homEquiv_apply] }
 
-/-- If we have an adjunction `adj : F ⊣ G` of functors between preadditive categories,
-and if `G` is additive, then the hom set equivalence upgrades to an `AddEquiv`.-/
-@[simps! (config := .lemmasOnly)]
-def homAddEquivOfRightAdjoint [G.Additive] (X : C) (Y : D) :
-    AddEquiv (F.obj X ⟶ Y) (X ⟶ G.obj Y) :=
-  {
-    adj.homEquiv _ _ with
-    map_add' _ _ := by simp [homEquiv_apply] }
+@[simp]
+def homAddEquiv_zero [F.Additive] (X : C) (Y : D) :
+    adj.homAddEquiv X Y 0 = 0 := by rw [map_zero]
+
+@[simp]
+def homAddEquiv_add [F.Additive] (X : C) (Y : D) (f f' : F.obj X ⟶ Y) :
+    adj.homAddEquiv X Y (f + f') = adj.homAddEquiv X Y f + adj.homAddEquiv X Y f' := by
+  rw [AddEquivClass.map_add]
+
+@[simp]
+def homAddEquiv_sub [F.Additive] (X : C) (Y : D) (f f' : F.obj X ⟶ Y) :
+    adj.homAddEquiv X Y (f - f') = adj.homAddEquiv X Y f - adj.homAddEquiv X Y f' := by
+  rw [AddEquiv.map_sub]
+
+@[simp]
+def homAddEquiv_neg [F.Additive] (X : C) (Y : D) (f : F.obj X ⟶ Y) :
+    adj.homAddEquiv X Y (- f) = - adj.homAddEquiv X Y f := by rw [AddEquiv.map_neg]
+
+@[simp]
+def homAddEquiv_symm_zero [F.Additive] (X : C) (Y : D) :
+    (adj.homAddEquiv X Y).symm 0 = 0 := by simp
+
+@[simp]
+def homAddEquiv_symm_add [F.Additive] (X : C) (Y : D) (f f' : X ⟶ G.obj Y) :
+    (adj.homAddEquiv X Y).symm (f + f') = (adj.homAddEquiv X Y).symm f +
+      (adj.homAddEquiv X Y).symm f' := by simp
+
+@[simp]
+def homAddEquiv_symm_sub [F.Additive] (X : C) (Y : D) (f f' : X ⟶ G.obj Y) :
+    (adj.homAddEquiv X Y).symm (f - f') = (adj.homAddEquiv X Y).symm f
+      - (adj.homAddEquiv X Y).symm f' := by simp
+
+@[simp]
+def homAddEquiv_symm_neg [F.Additive] (X : C) (Y : D) (f : X ⟶ G.obj Y) :
+    (adj.homAddEquiv X Y).symm (- f) = - (adj.homAddEquiv X Y).symm f := by simp
 
 open Opposite in
 /-- If we have an adjunction `adj : F ⊣ G` of functors between preadditive categories,
 and if `F` is additive, then the hom set equivalence upgrades to an isomorphism between
-`G ⋙ preadditiveYoneda` and `preadditiveYoneda ⋙ F`, once we throw in the ncessary
-universe lifting functors.-/
+`G ⋙ preadditiveYoneda` and `preadditiveYoneda ⋙ F`, once we throw in the necessary
+universe lifting functors.
+Note that `F` is additive if and only if `G` is, by `Adjunction.right_adjoint_additive` and
+`Adjunction.left_adjoint_additive`.
+-/
 @[simps! (config := .lemmasOnly)]
 def compPreadditiveYonedaIsoOfLeftAdjoint [F.Additive] :
     G ⋙ preadditiveYoneda ⋙ (whiskeringRight _ _ _).obj AddCommGrp.uliftFunctor.{max v₁ v₂} ≅
@@ -71,39 +104,10 @@ def compPreadditiveYonedaIsoOfLeftAdjoint [F.Additive] :
         (whiskeringRight _ _ _).obj AddCommGrp.uliftFunctor.{max v₁ v₂} := by
   refine NatIso.ofComponents (fun Y ↦ ?_) (fun _ ↦ ?_)
   · refine NatIso.ofComponents
-      (fun X ↦ ((AddEquiv.ulift.trans (adj.homAddEquivOfLeftAdjoint (unop X) Y)).trans
+      (fun X ↦ ((AddEquiv.ulift.trans (adj.homAddEquiv (unop X) Y)).trans
                AddEquiv.ulift.symm).toAddCommGrpIso.symm) (fun _ ↦ ?_)
     ext
-    simp [homAddEquivOfLeftAdjoint, adj.homEquiv_naturality_left_symm]
-    rfl
-  · ext
-    simp only [comp_obj, preadditiveYoneda_obj, whiskeringLeft_obj_obj, whiskeringRight_obj_obj,
-      op_obj, ModuleCat.forget₂_obj, preadditiveYonedaObj_obj_carrier,
-      preadditiveYonedaObj_obj_isAddCommGroup, AddCommGrp.uliftFunctor_obj, AddCommGrp.coe_of,
-      Functor.comp_map, whiskeringRight_obj_map, NatTrans.comp_app, whiskerRight_app,
-      AddCommGrp.uliftFunctor_map, AddEquiv.toAddMonoidHom_eq_coe, NatIso.ofComponents_hom_app,
-      Iso.symm_hom, AddEquiv.toAddCommGrpIso_inv, AddCommGrp.coe_comp', AddMonoidHom.coe_coe,
-      Function.comp_apply, AddCommGrp.ofHom_apply, AddMonoidHom.coe_comp, AddEquiv.symm_trans_apply,
-      AddEquiv.symm_symm, AddEquiv.apply_symm_apply, whiskeringLeft_obj_map, whiskerLeft_app]
-    erw [adj.homEquiv_naturality_right_symm]
-    rfl
-
-open Opposite in
-/-- If we have an adjunction `adj : F ⊣ G` of functors between preadditive categories,
-and if `G` is additive, then the hom set equivalence upgrades to an isomorphism between
-`G ⋙ preadditiveYoneda` and `preadditiveYoneda ⋙ F`, once we throw in the ncessary
-universe lifting functors.-/
-@[simps! (config := .lemmasOnly)]
-def compPreadditiveYonedaIsoOfRightAdjoint [G.Additive] :
-    G ⋙ preadditiveYoneda ⋙ (whiskeringRight _ _ _).obj AddCommGrp.uliftFunctor.{max v₁ v₂} ≅
-      preadditiveYoneda ⋙ (whiskeringLeft _ _ _).obj F.op ⋙
-      (whiskeringRight _ _ _).obj AddCommGrp.uliftFunctor.{max v₁ v₂} := by
-  refine NatIso.ofComponents (fun Y ↦ ?_) (fun _ ↦ ?_)
-  · refine NatIso.ofComponents
-      (fun X ↦ ((AddEquiv.ulift.trans (adj.homAddEquivOfRightAdjoint (unop X) Y)).trans
-               AddEquiv.ulift.symm).toAddCommGrpIso.symm) (fun _ ↦ ?_)
-    ext
-    simp [homAddEquivOfRightAdjoint, adj.homEquiv_naturality_left_symm]
+    simp [homAddEquiv, adj.homEquiv_naturality_left_symm]
     rfl
   · ext
     simp only [comp_obj, preadditiveYoneda_obj, whiskeringLeft_obj_obj, whiskeringRight_obj_obj,
