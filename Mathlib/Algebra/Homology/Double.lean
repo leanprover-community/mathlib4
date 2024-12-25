b84608cba7cc8b4d67c8be5c12a6c5b44ef68a75
Joël Riou <joel.riou@universite-paris-saclay.fr>
2024-12-25 02:42:35 +0100

    wip

--- Mathlib/Algebra/Homology/Double.lean
+++ Mathlib/Algebra/Homology/Double.lean
@@ -192,33 +192,4 @@ noncomputable def evalCompCoyonedaCorepresentableBySingle (i : ι) [DecidableEq 
       right_inv f := by simp }
   homEquiv_comp := by simp
 
-variable [c.HasNoLoop] [DecidableEq ι]
-
-open Classical in
-/-- Given a complex shape `c : ComplexShape ι` (with no loop), `X : C` and `j : ι`,
-this is a quite explicit choice of corepresentative of the functor which sends
-`K : HomologicalComplex C c` to `X ⟶ K.X j`. -/
-noncomputable def evalCompCoyonedaCorepresentative (X : C) (j : ι) :
-    HomologicalComplex C c :=
-  if hj : ∃ (k : ι), c.Rel j k ∧ j ≠ k then
-    double (𝟙 X) (hj.choose_spec.1)
-  else (single C c j).obj X
-
-/-- If a complex shape `c : ComplexShape ι` has no loop,
-then for any `X : C` and `j : ι`, the functor which sends `K : HomologicalComplex C c`
-to `X ⟶ K.X j` is corepresentable. -/
-noncomputable def evalCompCoyonedaCorepresentable (X : C) (j : ι) :
-    (eval C c j ⋙ coyoneda.obj (op X)).CorepresentableBy
-      (evalCompCoyonedaCorepresentative c X j) := by
-  dsimp [evalCompCoyonedaCorepresentative]
-  by_cases h : ∃ (k : ι), c.Rel j k ∧ j ≠ k
-  · rw [dif_pos h]
-    exact evalCompCoyonedaCorepresentableByDoubleId _ h.choose_spec.2 _
-  · rw [dif_neg h]
-    apply evalCompCoyonedaCorepresentableBySingle
-    obtain _ | _ := c.exists_distinct_prev_or j <;> tauto
-
-instance (X : C) (j : ι) : (eval C c j ⋙ coyoneda.obj (op X)).IsCorepresentable where
-  has_corepresentation := ⟨_, ⟨evalCompCoyonedaCorepresentable c X j⟩⟩
-
 end HomologicalComplex
