25d6529ff5e11a56d9fa557209d2b6f1f226dc20
Winston Yin <winstonyin@gmail.com>
2024-12-26 15:08:01 -0800

    SpaceOfCurves

--- Mathlib/Analysis/ODE/Solution.lean
+++ Mathlib/Analysis/ODE/Solution.lean
@@ -171,21 +171,61 @@ f : J × U → E continuous, K-lipschitz
   `f : ℝ → E → E` with `ContinuousOn (uncurry f) (J × U)`,
   `∀ t ∈ J, LipschitzOnWith (f t) u K`
 a : ℝ so that `closedBall x₀ (3 * a) ⊆ u`
-b : ℝ so that eventually we get integral curve α : Ioo (- b)
+b : ℝ so that eventually we get integral curve α : Ioo (-b) b → E
+  `b = (tmax - tmin) / 2`
 α : α_x (t) is the integral curve starting at x
   `α : E → ℝ → E` with `α x t` meaning `α x` is an integral curve starting at `x`
 -/
 
+
+-- [MetricSpace α] → ContractingWith K f → [inst_1 : Nonempty α] → [inst : CompleteSpace α] → α
+
+
+
+/-- The type of continuous maps  -/
+structure SpaceOfCurves (x₀ : E) (t₀ tmin tmax a : ℝ) where
+  toFun : Icc tmin tmax → closedBall x₀ (2 * a) -- use `MapsTo`?
+  mem_domain : t₀ ∈ Icc tmin tmax
+  initial : toFun ⟨t₀, mem_domain⟩ = x₀
+  continuous : Continuous toFun
+
+namespace SpaceOfCurves
+
+variable (x₀ : E) (t₀ tmin tmax a : ℝ)
+
+instance : CoeFun (SpaceOfCurves x₀ t₀ tmin tmax a)
+    fun _ => Icc tmin tmax → closedBall x₀ (2 * a) := ⟨toFun⟩
+
+
+
+end SpaceOfCurves
+
+
+
+
 -- K is NNReal because of LipschitzOnWith
 -- prop 1.1 is stated strangely at the end
-theorem exist_localFlow {tmin tmax L a b : ℝ} {u : Set E} {x₀ : E} (hu : closedBall x₀ (3 * a) ⊆ u)
+-- theorem exist_localFlow {tmin tmax L a b : ℝ} {u : Set E} {x₀ : E} (hu : closedBall x₀ (3 * a) ⊆ u)
+--     {f : ℝ → E → E} {K : ℝ≥0} (hb : 0 < b)
+--     (hcont₁ : ∀ x ∈ u, ContinuousOn (f · x) (Ioo tmin tmax))
+--     (hcont₂ : ∀ t ∈ Ioo tmin tmax, ContinuousOn (f t) u)
+--     (hle : ∀ t ∈ Ioo tmin tmax, ∀ x ∈ u, ‖f t x‖ ≤ L)
+--     (hlip : ∀ t ∈ Ioo tmin tmax, LipschitzOnWith K (f t) u)
+--     (hlt : b * L * K < a) :
+--   ∃ α : E → ℝ → E, ∀ x ∈ closedBall x₀ a, α x 0 = x ∧
+--     ∀ t ∈ Ioo (-b) b, α x t ∈ u ∧ HasDerivAt (α x) (f t (α x t)) t := sorry
+
+-- corollary: existence of a single integral curve
+
+theorem exist_localIntegralCurve {tmin tmax L a b : ℝ} {u : Set E} {x₀ : E}
+    (hu : closedBall x₀ (3 * a) ⊆ u)
     {f : ℝ → E → E} {K : ℝ≥0} (hb : 0 < b)
     (hcont₁ : ∀ x ∈ u, ContinuousOn (f · x) (Ioo tmin tmax))
     (hcont₂ : ∀ t ∈ Ioo tmin tmax, ContinuousOn (f t) u)
     (hle : ∀ t ∈ Ioo tmin tmax, ∀ x ∈ u, ‖f t x‖ ≤ L)
     (hlip : ∀ t ∈ Ioo tmin tmax, LipschitzOnWith K (f t) u)
-    (hlt : b * L * K < a) :
-  ∃ α : E → ℝ → E, ∀ x ∈ closedBall x₀ a, α x 0 = x ∧
-    ∀ t ∈ Ioo (-b) b, α x t ∈ u ∧ HasDerivAt (α x) (f t (α x t)) t := sorry
+    (hlt : b * L * K < a) {x : E} (hx : x ∈ closedBall x₀ a) :
+  ∃ α : ℝ → E, α 0 = x ∧
+    ∀ t ∈ Ioo (-b) b, α t ∈ u ∧ HasDerivAt α (f t (α t)) t := sorry
 
--- regularity
+-- provide the unique fixed point using `fixedPoint`
