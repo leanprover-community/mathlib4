ac50f1f7d00595ff66e87a997e9db68adfb6b381
Bergschaf <christian.k@netcom-mail.de>
2024-12-27 18:24:15 +0100

    refactor

--- Mathlib/Order/Subframe.lean
+++ Mathlib/Order/Subframe.lean
@@ -4,17 +4,15 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Christian Krause
 -/
 import Mathlib.Order.CompleteSublattice
+/-!
+A Subframe is a CompleteSublattice where the parent type is a frame.
+-/
 
 variable (α : Type*) [Order.Frame α]
 
 variable (S : CompleteSublattice α)
 
-def minAx2 : Order.Frame.MinimalAxioms α where
-  inf_sSup_le_iSup_inf := Order.Frame.inf_sSup_le_iSup_inf
-
-def minAx : Order.Frame.MinimalAxioms S :=
-  Subtype.coe_injective.frameMinimalAxioms ⟨Order.Frame.inf_sSup_le_iSup_inf⟩ _ (fun _ _ ↦ rfl)
-  (fun _ _ ↦ rfl) CompleteSublattice.coe_sSup' CompleteSublattice.coe_sInf' (rfl) (rfl)
-
 instance instFrame : Order.Frame S :=
-  Order.Frame.ofMinimalAxioms (minAx α S)
+  Order.Frame.ofMinimalAxioms ( Subtype.coe_injective.frameMinimalAxioms
+    ⟨Order.Frame.inf_sSup_le_iSup_inf⟩ _ (fun _ _ ↦ rfl) (fun _ _ ↦ rfl)
+     CompleteSublattice.coe_sSup' CompleteSublattice.coe_sInf' (rfl) (rfl))
