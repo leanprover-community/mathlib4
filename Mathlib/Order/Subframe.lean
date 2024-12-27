f89f8472c343411f68878bf21c232d3806f382ce
Bergschaf <christian.k@netcom-mail.de>
2024-12-27 12:59:17 +0100

    imports

--- Mathlib/Order/Subframe.lean
+++ Mathlib/Order/Subframe.lean
@@ -4,9 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Christian Krause
 -/
 import Mathlib.Order.CompleteSublattice
-import Mathlib.Order.CompleteLattice
-import Mathlib.Tactic
-
 
 variable (α : Type*) [Order.Frame α]
 
