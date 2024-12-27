9eead60e10bbfda052bbe84c36c0df964294c536
Bergschaf <christian.k@netcom-mail.de>
2024-12-27 12:11:22 +0100

    Subframe

--- Mathlib/Order/Subframe.lean
+++ Mathlib/Order/Subframe.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2024 Christian Krause. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Christian Krause
+-/
 import Mathlib.Order.CompleteSublattice
 import Mathlib.Order.CompleteLattice
 import Mathlib.Tactic
@@ -5,19 +10,14 @@ import Mathlib.Tactic
 
 variable (α : Type*) [Order.Frame α]
 
-abbrev Subframe := @CompleteSublattice α _
-
-
-variable (S : Subframe α)
+variable (S : CompleteSublattice α)
 
 def minAx2 : Order.Frame.MinimalAxioms α where
   inf_sSup_le_iSup_inf := Order.Frame.inf_sSup_le_iSup_inf
 
 def minAx : Order.Frame.MinimalAxioms S :=
-  Subtype.coe_injective.frameMinimalAxioms (minAx2 α) _ (fun _ _ ↦ rfl)
-  (fun _ _ ↦ rfl) CompleteSublattice.coe_sSup'
-  CompleteSublattice.coe_sInf' (rfl) (rfl)
-
+  Subtype.coe_injective.frameMinimalAxioms ⟨Order.Frame.inf_sSup_le_iSup_inf⟩ _ (fun _ _ ↦ rfl)
+  (fun _ _ ↦ rfl) CompleteSublattice.coe_sSup' CompleteSublattice.coe_sInf' (rfl) (rfl)
 
 instance instFrame : Order.Frame S :=
   Order.Frame.ofMinimalAxioms (minAx α S)
