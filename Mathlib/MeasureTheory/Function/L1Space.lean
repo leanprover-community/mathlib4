cce3892c2301b32d811ef54a2fc95ad2c6d084a5
tukamilano <milanotukamoto@g.ecc.u-tokyo.ac.jp>
2024-12-27 19:00:32 +0900

    add hasFiniteIntegral.of_mem_Icc

--- Mathlib/MeasureTheory/Function/L1Space.lean
+++ Mathlib/MeasureTheory/Function/L1Space.lean
@@ -154,6 +154,12 @@ theorem hasFiniteIntegral_const [IsFiniteMeasure μ] (c : β) :
     HasFiniteIntegral (fun _ : α => c) μ :=
   hasFiniteIntegral_const_iff.2 (Or.inr <| measure_lt_top _ _)
 
+theorem hasFiniteIntegral.of_mem_Icc [IsFiniteMeasure μ] (a b : ℝ) {X : α → ℝ}
+    (h : ∀ᵐ ω ∂μ, X ω ∈ Set.Icc a b) :
+    HasFiniteIntegral X μ := by
+  apply (hasFiniteIntegral_const (max ‖a‖ ‖b‖)).mono'
+  filter_upwards [h.mono fun ω h ↦ h.1, h.mono fun ω h ↦ h.2] with ω using abs_le_max_abs_abs
+
 theorem hasFiniteIntegral_of_bounded [IsFiniteMeasure μ] {f : α → β} {C : ℝ}
     (hC : ∀ᵐ a ∂μ, ‖f a‖ ≤ C) : HasFiniteIntegral f μ :=
   (hasFiniteIntegral_const C).mono' hC
@@ -446,11 +452,7 @@ theorem integrable_const_iff {c : β} : Integrable (fun _ : α => c) μ ↔ c = 
 
 theorem Integrable.of_mem_Icc [IsFiniteMeasure μ] (a b : ℝ) {X : α → ℝ} (hX : AEMeasurable X μ)
     (h : ∀ᵐ ω ∂μ, X ω ∈ Set.Icc a b) :
-     Integrable X μ := by
-  have m1 : HasFiniteIntegral X μ := by
-      apply (hasFiniteIntegral_const (max ‖a‖ ‖b‖)).mono'
-      filter_upwards [h.mono fun ω h ↦ h.1, h.mono fun ω h ↦ h.2] with ω using abs_le_max_abs_abs
-  exact ⟨hX.aestronglyMeasurable, m1⟩
+    Integrable X μ := ⟨hX.aestronglyMeasurable, hasFiniteIntegral.of_mem_Icc a b h⟩
 
 @[simp]
 theorem integrable_const [IsFiniteMeasure μ] (c : β) : Integrable (fun _ : α => c) μ :=
