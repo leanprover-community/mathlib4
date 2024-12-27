cdf32a02ac52bf67f2540f3aa9a1c74ff34e5ab4
smorel394 <smorel@math.princeton.edu>
2024-12-27 13:01:13 +0000

    feat(CategoryTheory/Adjunction/Additive): adjunctions between additive functors  (#20083)
    This provides some results and constructions for adjunctions between functors on preadditive categories:
    * If one of the adjoint functors is additive, so is the other.
    * If one of the adjoint functors is additive, the equivalence `Adjunction.homEquiv` lifts to
    an additive equivalence `Adjunction.homAddEquiv`.
    * We also give a version of this additive equivalence as an isomorphism of `preadditiveYoneda`
    functors (analogous to `Adjunction.compYonedaIso`), in
    `Adjunction.compPreadditiveYonedaIso`.
    
    
    
    Co-authored-by: smorel394 <67864981+smorel394@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1612,6 +1612,7 @@ import Mathlib.CategoryTheory.Action.Continuous
 import Mathlib.CategoryTheory.Action.Limits
 import Mathlib.CategoryTheory.Action.Monoidal
 import Mathlib.CategoryTheory.Adhesive
+import Mathlib.CategoryTheory.Adjunction.Additive
 import Mathlib.CategoryTheory.Adjunction.AdjointFunctorTheorems
 import Mathlib.CategoryTheory.Adjunction.Basic
 import Mathlib.CategoryTheory.Adjunction.Comma
