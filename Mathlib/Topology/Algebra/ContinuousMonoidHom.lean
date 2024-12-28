b3b26c80d018fb4f6b4d0e7fb445ff2f17613ebe
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2024-12-28 19:50:47 +0800

    fix naming of continuousMulEquivOfUnique

--- Mathlib/Topology/Algebra/ContinuousMonoidHom.lean
+++ Mathlib/Topology/Algebra/ContinuousMonoidHom.lean
@@ -673,7 +673,7 @@ section unique
 
 /-- The `MulEquiv` between two monoids with a unique element. -/
 @[to_additive "The `AddEquiv` between two `AddMonoid`s with a unique element."]
-def continuousMulEquivOfUnique {M N} [Unique M] [Unique N] [Mul M] [Mul N]
+def ofUnique {M N} [Unique M] [Unique N] [Mul M] [Mul N]
     [TopologicalSpace M] [TopologicalSpace N] : M ≃ₜ* N :=
   { MulEquiv.mulEquivOfUnique with
   continuous_toFun := by continuity
@@ -684,7 +684,7 @@ def continuousMulEquivOfUnique {M N} [Unique M] [Unique N] [Mul M] [Mul N]
   a unique element."]
 instance {M N} [Unique M] [Unique N] [Mul M] [Mul N]
     [TopologicalSpace M] [TopologicalSpace N] : Unique (M ≃ₜ* N) where
-  default := continuousMulEquivOfUnique
+  default := ofUnique
   uniq _ := ext fun _ ↦ Subsingleton.elim _ _
 
 end unique
