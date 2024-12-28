d0c627f444ab73436dc587b0ce2be6d903820ecb
Yaël Dillies <yael.dillies@gmail.com>
2024-12-28 21:36:41 +0000

    fix

--- Mathlib/Algebra/Group/Submonoid/Pointwise.lean
+++ Mathlib/Algebra/Group/Submonoid/Pointwise.lean
@@ -392,7 +392,7 @@ theorem pointwise_smul_le_pointwise_smul_iff {a : α} {S T : AddSubmonoid A} :
   smul_set_subset_smul_set_iff
 
 theorem pointwise_smul_le_iff {a : α} {S T : AddSubmonoid A} : a • S ≤ T ↔ S ≤ a⁻¹ • T :=
-  smul_set_subset_iff
+  smul_set_subset_iff_subset_inv_smul_set
 
 theorem le_pointwise_smul_iff {a : α} {S T : AddSubmonoid A} : S ≤ a • T ↔ a⁻¹ • S ≤ T :=
   subset_smul_set_iff
