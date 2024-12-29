9df630d747439ffec1e6601554d58adef3ec94c7
Andrew Yang <the.erd.one@gmail.com>
2024-12-29 15:13:11 +0000

    feat(RingTheory): Jacobian criterion for smoothness of local algebras

--- Mathlib.lean
+++ Mathlib.lean
@@ -4631,6 +4631,7 @@ import Mathlib.RingTheory.SimpleRing.Field
 import Mathlib.RingTheory.SimpleRing.Matrix
 import Mathlib.RingTheory.Smooth.Basic
 import Mathlib.RingTheory.Smooth.Kaehler
+import Mathlib.RingTheory.Smooth.Local
 import Mathlib.RingTheory.Smooth.Pi
 import Mathlib.RingTheory.Smooth.StandardSmooth
 import Mathlib.RingTheory.Support
