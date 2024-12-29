8c33a15f2d9def8ccfca217a8ae0bda206ada774
Sidharth Hariharan <fundamental.theor3m@gmail.com>
2024-12-29 11:22:53 +0530

    feat: add abs_exp_i and one_sub_rexp_re_le_abs_one_sub_cexp

--- Mathlib/Data/Complex/Exponential.lean
+++ Mathlib/Data/Complex/Exponential.lean
@@ -1494,4 +1494,15 @@ theorem abs_exp (z : ℂ) : abs (exp z) = Real.exp z.re := by
 theorem abs_exp_eq_iff_re_eq {x y : ℂ} : abs (exp x) = abs (exp y) ↔ x.re = y.re := by
   rw [abs_exp, abs_exp, Real.exp_eq_exp]
 
+theorem abs_exp_i (x : ℂ) : abs (Complex.exp (I * x)) = Real.exp (-x.im) := by
+  have h₁ : I * (↑x.im * I) = -x.im := by rw [mul_comm, mul_assoc, Complex.I_mul_I, mul_neg_one]
+  rw [← x.re_add_im, mul_add, h₁, Complex.abs_exp]
+  simp
+
+theorem one_sub_rexp_re_le_abs_one_sub_cexp (x : ℂ) :
+    1 - Real.exp x.re ≤ Complex.abs (1 - Complex.exp x) := calc
+  abs (1 - Complex.exp x) ≥ |Complex.abs 1 - abs (Complex.exp x)| := abs.abs_abv_sub_le_abv_sub _ _
+  _ = |1 - Real.exp x.re| := by simp [Complex.abs_exp]
+  _ ≥ _ := le_abs_self _
+
 end Complex
