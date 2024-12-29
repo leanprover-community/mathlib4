e5ef02716bfa52f382c18165362432d47cd4765e
Joël Riou <joel.riou@universite-paris-saclay.fr>
2024-12-30 00:36:39 +0100

    split

--- Mathlib.lean
+++ Mathlib.lean
@@ -1927,7 +1927,8 @@ import Mathlib.CategoryTheory.Limits.Shapes.Multiequalizer
 import Mathlib.CategoryTheory.Limits.Shapes.NormalMono.Basic
 import Mathlib.CategoryTheory.Limits.Shapes.NormalMono.Equalizers
 import Mathlib.CategoryTheory.Limits.Shapes.PiProd
-import Mathlib.CategoryTheory.Limits.Shapes.Preorder
+import Mathlib.CategoryTheory.Limits.Shapes.Preorder.Basic
+import Mathlib.CategoryTheory.Limits.Shapes.Preorder.HasIterationOfShape
 import Mathlib.CategoryTheory.Limits.Shapes.Products
 import Mathlib.CategoryTheory.Limits.Shapes.Pullback.Assoc
 import Mathlib.CategoryTheory.Limits.Shapes.Pullback.CommSq
