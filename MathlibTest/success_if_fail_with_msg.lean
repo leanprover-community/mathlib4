2c2995bcc2fcddbe8650a3e6f38848e3f37c8248
Blizzard_inc <thrashy4inbox@gmail.com>
2025-01-01 14:07:54 +0100

    fix small typo in test

--- MathlibTest/success_if_fail_with_msg.lean
+++ MathlibTest/success_if_fail_with_msg.lean
@@ -52,7 +52,7 @@ open Lean Meta Mathlib Tactic
 def alwaysFails : MetaM Unit := do throwError "I failed!"
 
 def doesntFail : MetaM Unit := do
-  try successIfFailWithMessage "I failed!" alwaysFails (← `("I failed"))
+  try successIfFailWithMessage "I failed!" alwaysFails (← `("I failed!"))
   catch _ => throwError "I *really* failed."
 
 #guard_msgs in
