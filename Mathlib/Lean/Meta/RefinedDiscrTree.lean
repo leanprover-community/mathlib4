fdba472d3df533084aa8a4fcb70967a27c9df81d
Eric Wieser <wieser.eric@gmail.com>
2025-01-01 21:39:56 +0000

    fix

--- Mathlib/Lean/Meta/RefinedDiscrTree.lean
+++ Mathlib/Lean/Meta/RefinedDiscrTree.lean
@@ -3,8 +3,6 @@ Copyright (c) 2023 J. W. Gerbscheid. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: J. W. Gerbscheid
 -/
-import Mathlib.Tactic.FunProp.StateList
-import Mathlib.Algebra.Group.Pi.Basic
 import Mathlib.Lean.Meta.RefinedDiscrTree.Pi
 import Mathlib.Lean.Meta.RefinedDiscrTree.Basic
 import Mathlib.Lean.Meta.RefinedDiscrTree.Encode
@@ -107,8 +105,6 @@ TODO:
 -/
 
 open Lean Meta
-
-open Mathlib.Meta.FunProp (StateListT StateListM)
 
 namespace Lean.Meta.RefinedDiscrTree
 variable {α}
--- Mathlib/Lean/Meta/RefinedDiscrTree/Pi.lean
+++ Mathlib/Lean/Meta/RefinedDiscrTree/Pi.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jovan Gerbscheid
 -/
 import Mathlib.Init
-import Lean.Meta.WHNF
 import Mathlib.Algebra.Group.Pi.Basic
 
 /-!
