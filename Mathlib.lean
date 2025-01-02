590fc523c1df8909efd079bc96e64489a59c05cd
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2025-01-02 13:34:41 +0100

    move Algebra.Order.EuclideanAbsoluteValue --> Algebra.Order.AbsoluteValue.Euclidean

--- Mathlib.lean
+++ Mathlib.lean
@@ -651,7 +651,7 @@ import Mathlib.Algebra.Order.CauSeq.BigOperators
 import Mathlib.Algebra.Order.CauSeq.Completion
 import Mathlib.Algebra.Order.Chebyshev
 import Mathlib.Algebra.Order.CompleteField
-import Mathlib.Algebra.Order.EuclideanAbsoluteValue
+import Mathlib.Algebra.Order.AbsoluteValue.Euclidean
 import Mathlib.Algebra.Order.Field.Basic
 import Mathlib.Algebra.Order.Field.Canonical.Basic
 import Mathlib.Algebra.Order.Field.Canonical.Defs
