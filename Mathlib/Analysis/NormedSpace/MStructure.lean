da8f096bc731530e78a7a278977da13538b01459
Christopher Hoskin <christopher.hoskin@gmail.com>
2025-01-02 08:26:49 +0000

    Use better names

--- Mathlib/Analysis/NormedSpace/MStructure.lean
+++ Mathlib/Analysis/NormedSpace/MStructure.lean
@@ -322,38 +322,44 @@ variable (p : ℝ≥0∞) (α β : Type*)
 variable {p α β}
 variable [NormedAddCommGroup α] [NormedAddCommGroup β]
 
+namespace WithLp
+
 /-- Projection on `WithLp p (α × β)` with range `α` and kernel `β` -/
-def P1 : AddMonoid.End (WithLp p (α × β)) := (AddMonoidHom.inl α β).comp (AddMonoidHom.fst α β)
+def idemFst : AddMonoid.End (WithLp p (α × β)) := (AddMonoidHom.inl α β).comp (AddMonoidHom.fst α β)
 
 /-- Projection on `WithLp p (α × β)` with range `β` and kernel `α` -/
-def P2 : AddMonoid.End (WithLp p (α × β)) := (AddMonoidHom.inr α β).comp (AddMonoidHom.snd α β)
+def idemSnd : AddMonoid.End (WithLp p (α × β)) := (AddMonoidHom.inr α β).comp (AddMonoidHom.snd α β)
 
-lemma P1_apply (x : WithLp p (α × β)) : P1 x = (x.1, 0) := rfl
+lemma idemFst_apply (x : WithLp p (α × β)) : idemFst x = (x.1, 0) := rfl
 
-lemma P2_apply (x : WithLp p (α × β)) : P2 x = (0, x.2) := rfl
+lemma idemSnd_apply (x : WithLp p (α × β)) : idemSnd x = (0, x.2) := rfl
 
-lemma P1_compl : (1 : AddMonoid.End (WithLp p (α × β))) - P1 = P2 := AddMonoidHom.ext
+lemma idemFst_compl : (1 : AddMonoid.End (WithLp p (α × β))) - idemFst = idemSnd := AddMonoidHom.ext
   fun x => by
-    rw [AddMonoidHom.sub_apply, P1_apply, P2_apply, AddMonoid.End.coe_one, id_eq,
+    rw [AddMonoidHom.sub_apply, idemFst_apply, idemSnd_apply, AddMonoid.End.coe_one, id_eq,
       sub_eq_iff_eq_add, add_comm, WithLp.mk_add_mk, zero_add, add_zero]; rfl
 
-theorem prod_norm_eq_sup (x : WithLp ∞ (α × β)) : ‖x‖ = ‖P1 x‖ ⊔ ‖(1 - P1) x‖ := by
+theorem prod_norm_eq_sup_idemFst (x : WithLp ∞ (α × β)) :
+    ‖x‖ = ‖idemFst x‖ ⊔ ‖(1 - idemFst) x‖ := by
   rw [WithLp.prod_norm_eq_sup, ← WithLp.norm_equiv_symm_fst ∞ α β x.1,
-    ← WithLp.norm_equiv_symm_snd ∞ α β x.2, P1_compl]
+    ← WithLp.norm_equiv_symm_snd ∞ α β x.2, idemFst_compl]
   rfl
 
-lemma prod_norm_eq_add_P1 [Fact (1 ≤ p)] (hp : 0 < p.toReal) (x : WithLp p (α × β)) :
-    ‖x‖ = (‖P1 x‖ ^ p.toReal + ‖(1 - P1) x‖ ^ p.toReal) ^ (1 / p.toReal) := by
+lemma prod_norm_eq_add_idemFst [Fact (1 ≤ p)] (hp : 0 < p.toReal) (x : WithLp p (α × β)) :
+    ‖x‖ = (‖idemFst x‖ ^ p.toReal + ‖(1 - idemFst) x‖ ^ p.toReal) ^ (1 / p.toReal) := by
   rw [WithLp.prod_norm_eq_add hp, ← WithLp.norm_equiv_symm_fst p α β x.1,
-    ← WithLp.norm_equiv_symm_snd p α β x.2, P1_compl]
+    ← WithLp.norm_equiv_symm_snd p α β x.2, idemFst_compl]
   rfl
 
-lemma P1_Lprojection [Fact (1 ≤ p)] :
-  IsLprojection (WithLp 1 (α × β)) (M := (AddMonoid.End (WithLp 1 (α × β)))) (P1 (p := 1)) where
+lemma idemFst_Lprojection [Fact (1 ≤ p)] :
+  IsLprojection (WithLp 1 (α × β)) (M := (AddMonoid.End (WithLp 1 (α × β))))
+    (idemFst (p := 1)) where
   proj := rfl
   Lnorm x := by
-    rw [prod_norm_eq_add_P1 (by simp only [one_toReal, zero_lt_one])]
+    rw [prod_norm_eq_add_idemFst (by simp only [one_toReal, zero_lt_one])]
     simp only [one_toReal, Real.rpow_one, ne_eq, one_ne_zero, not_false_eq_true, div_self,
       AddMonoid.End.smul_def]
+
+end WithLp
 
 end WithL1
