4cfafb74955922d2ca116ab117341c50ec485d10
RemyDegenne <Remydegenne@gmail.com>
2025-01-02 17:01:55 +0100

    change import

--- Mathlib/MeasureTheory/Function/AEEqOfLIntegral.lean
+++ Mathlib/MeasureTheory/Function/AEEqOfLIntegral.lean
@@ -3,7 +3,7 @@ Copyright (c) 2021 Rémy Degenne. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Rémy Degenne
 -/
-import Mathlib.MeasureTheory.Function.StronglyMeasurable.Lemmas
+import Mathlib.MeasureTheory.Function.StronglyMeasurable.ENNReal
 import Mathlib.MeasureTheory.Measure.WithDensity
 
 /-! # From equality of integrals to equality of functions
