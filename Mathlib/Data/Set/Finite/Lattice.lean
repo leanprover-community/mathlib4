4b6864fbd2c9b7c2973d531c60dcebddf2d36ab3
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2025-01-03 22:59:37 +0100

    cherry pick

--- Mathlib/Data/Set/Finite/Lattice.lean
+++ Mathlib/Data/Set/Finite/Lattice.lean
@@ -152,6 +152,47 @@ theorem Finite.iUnion {ι : Type*} {s : ι → Set α} {t : Set ι} (ht : t.Fini
   · rw [he i hi, mem_empty_iff_false] at hx
     contradiction
 
+/-- An indexed union of pairwise disjoint sets is finite iff all sets are finite, and all but
+finitely many are empty. -/
+lemma finite_biUnion_iff_of_pairwiseDisjoint {f : β → Set α} {s : Set β}
+    (hs : s.PairwiseDisjoint f) :
+    Set.Finite (⋃ i ∈ s, f i)
+      ↔ (∀ i ∈ s, Set.Finite (f i)) ∧ Set.Finite {i ∈ s | (f i).Nonempty} := by
+  constructor
+  · intro h
+    refine ⟨fun i hi ↦ ?_, ?_⟩
+    · have : f i ⊆ ⋃ i ∈ s, f i := subset_biUnion_of_mem hi
+      exact Finite.subset h this
+    · let u : {i ∈ s | (f i).Nonempty} → ⋃ i ∈ s, f i := fun i ↦ ⟨i.2.2.choose, by
+        have : i.2.2.choose ∈ f i := i.2.2.choose_spec
+        simp only [mem_setOf_eq, mem_iUnion, exists_prop]
+        exact ⟨i, i.2.1, this⟩⟩
+      have u_inj : Function.Injective u := by
+        rintro ⟨i, hi⟩ ⟨j, hj⟩ hij
+        simp only [coe_setOf, mem_setOf_eq, Subtype.mk.injEq]
+        by_contra h'ij
+        have : Disjoint (f i) (f j) := hs hi.1 hj.1 h'ij
+        have ui : (u ⟨i, hi⟩ : α) ∈ f i := hi.2.choose_spec
+        rw [hij] at ui
+        have uj : (u ⟨j, hj⟩ : α) ∈ f j := hj.2.choose_spec
+        exact disjoint_left.1 this ui uj
+      have : Finite (⋃ i ∈ s, f i) := h
+      exact Finite.of_injective u u_inj
+  · rintro ⟨h, h'⟩
+    have : ⋃ i ∈ s, f i = ⋃ i ∈ {i ∈ s | (f i).Nonempty}, f i := by
+      ext; simp; tauto
+    rw [this]
+    apply Set.Finite.biUnion h'
+    intro i hi
+    exact h i hi.1
+
+/-- An indexed union of pairwise disjoint sets is finite iff all sets are finite, and all but
+finitely many are empty. -/
+lemma finite_iUnion_iff_of_pairwiseDisjoint {f : β → Set α} (hs : univ.PairwiseDisjoint f) :
+    Set.Finite (⋃ i, f i) ↔ (∀ i, Set.Finite (f i)) ∧ Set.Finite {i | (f i).Nonempty} := by
+  rw [← biUnion_univ, Set.finite_biUnion_iff_of_pairwiseDisjoint hs]
+  simp
+
 section preimage
 variable {f : α → β} {s : Set β}
 
