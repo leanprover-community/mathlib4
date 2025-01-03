132c36b0e305d170d3f2bd44b9d73c28ad23e73c
Andrew Yang <the.erd.one@gmail.com>
2025-01-03 11:31:02 +0000

    address comments

--- Mathlib/Algebra/Group/Pi/Units.lean
+++ Mathlib/Algebra/Group/Pi/Units.lean
@@ -12,10 +12,13 @@ import Mathlib.Util.Delaborators
 
 variable {ι} {M : ι → Type*} [∀ i, Monoid (M i)] {x : Π i, M i}
 
-/-- The units of a product is isomorpic to the product of the units. -/
+open Units in
+/-- The monoid equivalence between units of a product,
+  and the product of the units of each monoid. -/
 @[to_additive (attr := simps)
-  "The (additive) units of a product is isomorpic to the product of the (additive) units."]
-def Units.piEquiv : (Π i, M i)ˣ ≃* Π i, (M i)ˣ where
+  "The additive-monoid equivalence between (additive) units of a product,
+  and the product of the (additive) units of each monoid."]
+def MulEquiv.piUnits : (Π i, M i)ˣ ≃* Π i, (M i)ˣ where
   toFun f i := ⟨f.val i, f.inv i, congr_fun f.val_inv i, congr_fun f.inv_val i⟩
   invFun f := ⟨(val <| f ·), (inv <| f ·), funext (val_inv <| f ·), funext (inv_val <| f ·)⟩
   left_inv _ := rfl
@@ -23,14 +26,14 @@ def Units.piEquiv : (Π i, M i)ˣ ≃* Π i, (M i)ˣ where
   map_mul' _ _ := rfl
 
 @[to_additive]
-lemma IsUnit.pi_iff :
+lemma Pi.isUnit_iff :
     IsUnit x ↔ ∀ i, IsUnit (x i) := by
   simp_rw [isUnit_iff_exists, funext_iff, ← forall_and]
   exact Classical.skolem (p := fun i y ↦ x i * y = 1 ∧ y * x i = 1).symm
 
 @[to_additive]
-alias ⟨IsUnit.apply, _⟩ := IsUnit.pi_iff
+alias ⟨IsUnit.apply, _⟩ := Pi.isUnit_iff
 
 @[to_additive]
 lemma IsUnit.val_inv_apply (hx : IsUnit x) (i) : (hx.unit⁻¹).1 i = (hx.apply i).unit⁻¹ := by
-  rw [← Units.inv_eq_val_inv, ← Units.val_inv_piEquiv_apply]; congr; ext; rfl
+  rw [← Units.inv_eq_val_inv, ← MulEquiv.val_inv_piUnits_apply]; congr; ext; rfl
