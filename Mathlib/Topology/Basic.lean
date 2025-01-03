e245801fc3f16f2d7120559d2873d7ed47c3f77a
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2025-01-03 08:30:18 +0000

    Update Mathlib/Topology/Basic.lean
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- Mathlib/Topology/Basic.lean
+++ Mathlib/Topology/Basic.lean
@@ -1359,9 +1359,11 @@ theorem IsOpen.preimage (hf : Continuous f) {t : Set Y} (h : IsOpen t) :
     IsOpen (f ⁻¹' t) :=
   hf.isOpen_preimage t h
 
-lemma Equiv.continuous_symm_of_isOpenMap (e : X ≃ Y) (h : IsOpenMap e) : Continuous e.symm := by
-  simp_rw [continuous_def, ← Set.image_equiv_eq_preimage_symm]
-  exact h
+lemma Equiv.continuous_symm_iff (e : X ≃ Y) : Continuous e.symm ↔ IsOpenMap e := by
+  simp_rw [continuous_def, ← Set.image_equiv_eq_preimage_symm, IsOpenMap]
+
+lemma Equiv.isOpenMap_symm_iff (e : X ≃ Y) : IsOpenMap e.symm ↔ Continuous e := by
+  simp_rw [← Equiv.continuous_symm_iff, Equiv.symm_symm]
 
 theorem continuous_congr {g : X → Y} (h : ∀ x, f x = g x) :
     Continuous f ↔ Continuous g :=
