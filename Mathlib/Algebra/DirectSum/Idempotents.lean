641a9b7fb9b98527ac074dcba6a6e51659ab154e
Whysoserioushah <yx3021@ic.ac.uk>
2025-01-04 20:08:29 +0800

    change names and add documentation

--- Mathlib/Algebra/DirectSum/Idempotents.lean
+++ Mathlib/Algebra/DirectSum/Idempotents.lean
@@ -6,12 +6,21 @@ Authors: Yunzhou Xie, Jujian Zhang, Andrew Yang
 import Mathlib.RingTheory.Idempotents
 import Mathlib.Algebra.DirectSum.Decomposition
 
+/-!
+# Decomposition of the identity of a ring into orthogonal idempotents
+In this file we show that if a ring `R` can be decompose into direct sum
+of (left) ideals `R = V1 ⊕ V2 ⊕ ⬝ ⬝ ⬝ ⊕ Vn` where `1 = e₁ + e₂ + ⬝ ⬝ ⬝ + eₙ`,
+then each `eᵢ` is an idempotent and they are orthogonal to each other.
+-/
+
 variable {R I : Type*} [Ring R]
 
 namespace DirectSum
 
 section OrthogonalIdempotents
 
+/-- The decomposition of `(1 : R)` into the direct sum of ideals which is also the identity
+  of the ring induced by the directsum.-/
 def idempotent [DecidableEq I] (V : I → Ideal R) [Decomposition V] (i : I) : R :=
   decompose V 1 i
 
@@ -20,22 +29,21 @@ lemma decompose_eq_mul_idempotent [DecidableEq I] (V : I → Ideal R) [Decomposi
   rw [← smul_eq_mul (a := x), idempotent, ← Submodule.coe_smul, ← smul_apply, ← decompose_smul,
     smul_eq_mul, mul_one]
 
-
-lemma decomp_ring_ortho_idem_is_idem [DecidableEq I] (V : I → Ideal R)
-    [Decomposition V] (i) : IsIdempotentElem (decompose V 1 i : R) := by
-  rw [IsIdempotentElem, ← smul_eq_mul, ← Submodule.coe_smul, ← smul_apply,
+lemma isIdempotentElem_idempotent [DecidableEq I] (V : I → Ideal R)
+    [Decomposition V] (i) : IsIdempotentElem (idempotent V i : R) := by
+  rw [IsIdempotentElem, idempotent, ← smul_eq_mul, ← Submodule.coe_smul, ← smul_apply,
     ← decompose_smul, smul_eq_mul, mul_one, decompose_coe, of_eq_same]
 
 /-- If a ring can be decomposed into direct sum of left ideals `Vᵢ`
   where `1 = e₁ + ... + eₙ` and `eᵢ ∈ Vᵢ`, then `eᵢ` is a family of orthogonal
   idempotents.-/
-theorem decomp_ring_ortho_idem [DecidableEq I] (V : I → Ideal R)
+theorem isOrthoIdempotent_idempotent [DecidableEq I] (V : I → Ideal R)
     [Decomposition V] :
-    OrthogonalIdempotents (R := R) fun i ↦ decompose V 1 i where
-  idem := decomp_ring_ortho_idem_is_idem V
+    OrthogonalIdempotents (R := R) fun i ↦ idempotent V i where
+  idem := isIdempotentElem_idempotent V
   ortho := fun i j hij ↦ by
     simp only
-    rw [← smul_eq_mul, ← Submodule.coe_smul, ← smul_apply,
+    rw [← smul_eq_mul, idempotent, idempotent, ← Submodule.coe_smul, ← smul_apply,
       ← decompose_smul, smul_eq_mul, mul_one, decompose_coe, of_eq_of_ne (h := hij),
       Submodule.coe_zero]
 
