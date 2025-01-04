2ae75559b6c75ee85c4848606c9ddca55cae5e3f
Johan Commelin <johan@commelin.net>
2025-01-04 18:27:38 +0100

    wip

--- Mathlib/Algebra/Lie/CartanCriterion.lean
+++ Mathlib/Algebra/Lie/CartanCriterion.lean
@@ -24,17 +24,29 @@ instance [Subsingleton L] : IsSolvable k L := by
 
 -- move this
 /-- The Killing form is symmetric. -/
-lemma killingForm_symm (X Y : L) : killingForm k L X Y = killingForm k L Y X := by
-  rw [killingForm_apply_apply, killingForm_apply_apply]
-  apply LinearMap.trace_mul_comm
+lemma killingForm_symm (X Y : L) : killingForm k L X Y = killingForm k L Y X :=
+  LieModule.traceForm_comm _ _ _ _ _
 
 /-- The Killing form is symmetric. -/
-lemma killingForm_isSymm : (killingForm k L).IsSymm := by
-  intro X Y
-  apply killingForm_symm
+lemma killingForm_isSymm : (killingForm k L).IsSymm :=
+  LieModule.traceForm_isSymm _ _ _
 
-theorem solvable_iff [Module.Finite k L] : IsSolvable k L ↔
-   ∀ X Y : L, Y ∈ LieAlgebra.derivedSeries k L 1 → killingForm k L X Y = 0 := by
+private theorem isSolvable_iff_of_algClosed [Module.Finite k L] [IsAlgClosed k] :
+    IsSolvable k L ↔ ∀ X Y : L, Y ∈ LieAlgebra.derivedSeries k L 1 → killingForm k L X Y = 0 := by
+  obtain _ | _ := subsingleton_or_nontrivial L
+  · simp; infer_instance
+  constructor
+  · intro h
+    obtain ⟨χ, hχ⟩ := LieModule.exists_forall_lie_eq_smul_of_isSolvable k L L
+    obtain ⟨⟨Z, hZ⟩, hZ0⟩ := exists_ne (0 : LieModule.weightSpace L χ) 
+    simp only [ne_eq, LieSubmodule.mk_eq_zero] at hZ0
+    simp only [LieModule.mem_weightSpace] at hZ
+    sorry
+  · intro h
+    sorry
+
+theorem isSolvable_iff [Module.Finite k L] :
+    IsSolvable k L ↔ ∀ X Y : L, Y ∈ LieAlgebra.derivedSeries k L 1 → killingForm k L X Y = 0 := by
   obtain _ | _ := subsingleton_or_nontrivial L
   · simp; infer_instance
   constructor
@@ -42,12 +54,13 @@ theorem solvable_iff [Module.Finite k L] : IsSolvable k L ↔
     -- WLOG: k is algebraically closed
     -- have := LieModule.exists_forall_lie_eq_smul_of_isSolvable k L L
     sorry
-  · sorry
+  · intro h
+    sorry
 
 instance [Module.Finite k L] [HasTrivialRadical k L] : IsKilling k L := by
   constructor
   suffices IsSolvable k (LieIdeal.killingCompl k L ⊤) from HasTrivialRadical.eq_bot_of_isSolvable _
-  rw [solvable_iff]
+  rw [isSolvable_iff]
   intro X Y hY
   rw [LieIdeal.killingForm_eq]
   show killingForm k L X Y = 0
