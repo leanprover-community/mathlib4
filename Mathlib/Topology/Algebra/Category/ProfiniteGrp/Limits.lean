6aeeb4cc718787b218fd7419e0ba4ae580cb0b39
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2025-01-05 13:58:05 +0800

    update head docstring and naming

--- Mathlib/Topology/Algebra/Category/ProfiniteGrp/Limits.lean
+++ Mathlib/Topology/Algebra/Category/ProfiniteGrp/Limits.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2024 Jujian Zhang. All rights reserved.
+Copyright (c) 2024 Nailin Guan. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Jujian Zhang, Nailin Guan, Youle Fang, Yuyang Zhao
+Authors: Nailin Guan, Youle Fang, Jujian Zhang, Yuyang Zhao
 -/
 import Mathlib.Topology.Algebra.Category.ProfiniteGrp.Basic
 import Mathlib.Topology.Algebra.ClopenNhdofOne
@@ -9,30 +9,24 @@ import Mathlib.Topology.Algebra.ClopenNhdofOne
 /-!
 # A profinite group is the projective limit of finite groups
 
-We give the topological group isomorphism between a profinite group and the projective limit of its quotients by open normal subgroups.
+We give the topological group isomorphism between a profinite group and the projective limit of
+its quotients by open normal subgroups.
 
 ## Main definitions
 
-In a profinite group `P` :
-* `QuotientOpenNormalSubgroup` : The functor that maps open normal subgroup of `P` to
-  the quotient group of it (which is finite, as shown by previous lemmas).
+* `QuotientOpenNormalSubgroup` : The functor that maps open normal subgroup of a profinite group
+  `P` to the quotient group of it (which is finite, as shown by previous lemmas).
 
-* `CanonicalQuotientMap` : The continuous homomorphism from `P` to the limit of the quotient group
-  of open normal subgroup ordered by inclusion of the open normal subgroup.
+* `toLimitQuotientOpenNormalSubgroup` : The continuous homomorphism from a profinite group `P` to
+  projective limit of its quotients by open normal subgroups ordered by inclusion.
 
-* `canonicalQuotientMap_surjective` : The `CanonicalQuotientMap` is surjective, proven by
-  demonstrating that its range is dense and closed.
+* `ContinuousMulEquivLimitQuotientOpenNormalSubgroup` : The `toLimitQuotientOpenNormalSubgroup` is a
+`ContinuousMulEquiv`
+
+## Main Statements
 
 * `OpenNormalSubgroupSubClopenNhdsOfOne` : For any open neighborhood of `1` there is an
   open normal subgroup contained within it.
-
-* `canonicalQuotientMap_injective` : The `CanonicalQuotientMap` is injective. This is proven by
-  showing that for any element not equal to one, the image of it on the coordinate of
-  the open normal subgroup that doesn't contain it is not equal to 1, thus not in the kernel.
-
-* `ContinuousMulEquivLimitQuotientOpenNormalSubgroup` : The `CanonicalQuotientMap` can serve as a
-  `ContinuousMulEquiv`, with the continuity of other side given by
-  `Continuous.homeoOfEquivCompactToT2`.
 
 -/
 
@@ -55,8 +49,8 @@ section
 instance (P : ProfiniteGrp) : SmallCategory (OpenNormalSubgroup P) :=
   Preorder.smallCategory (OpenNormalSubgroup ↑P.toProfinite.toTop)
 
-/-- Define the functor from OpenNormalSubgroup of a profinite group to the quotient group of it as
-  a `FiniteGrp` -/
+/-- The functor from `OpenNormalSubgroup P` to `FiniteGrp` sending `U` to `P ⧸ U`,
+where `P : ProfiniteGrp`. -/
 def QuotientOpenNormalSubgroup (P : ProfiniteGrp) :
     OpenNormalSubgroup P ⥤ FiniteGrp := {
     obj := fun H => FiniteGrp.of (P ⧸ H.toSubgroup)
@@ -70,9 +64,9 @@ def QuotientOpenNormalSubgroup (P : ProfiniteGrp) :
       (Subgroup.comap_id Y.toSubgroup ▸ leOfHom f)
       (Subgroup.comap_id Z.toSubgroup ▸ leOfHom g)).symm }
 
-/-- Defining the canonical projection from a profinite group to the limit of the quotient groups
-as a subgroup of the product space -/
-def CanonicalQuotientMap (P : ProfiniteGrp.{u}) : P ⟶
+/-- The continuous homomorphism from a profinite group `P` to the projective limit of
+its quotients by open normal subgroups ordered by inclusion.-/
+def toLimitQuotientOpenNormalSubgroup (P : ProfiniteGrp.{u}) : P ⟶
     limit ((QuotientOpenNormalSubgroup P) ⋙ (forget₂ FiniteGrp ProfiniteGrp)) where
   toFun := fun p => { val := fun H => QuotientGroup.mk p
                       property := fun _ => rfl }
@@ -88,11 +82,10 @@ def CanonicalQuotientMap (P : ProfiniteGrp.{u}) : P ⟶
     ext x
     simp only [Set.mem_preimage, Set.mem_singleton_iff]
     nth_rw 1 [← QuotientGroup.out_eq' i, eq_comm, QuotientGroup.eq]
-    symm
-    apply Set.mem_smul_set_iff_inv_smul_mem )
+    exact Iff.symm (Set.mem_smul_set_iff_inv_smul_mem) )
 
-theorem canonicalQuotientMap_dense (P : ProfiniteGrp.{u}) : Dense <|
-    Set.range (CanonicalQuotientMap P) :=
+theorem toLimitQuotientOpenNormalSubgroup_dense (P : ProfiniteGrp.{u}) : Dense <|
+    Set.range (toLimitQuotientOpenNormalSubgroup P) :=
   dense_iff_inter_open.mpr fun U ⟨s, hsO, hsv⟩ ⟨⟨spc, hspc⟩, uDefaultSpec⟩ => (by
     simp_rw [← hsv, Set.mem_preimage] at uDefaultSpec
     rcases (isOpen_pi_iff.mp hsO) _ uDefaultSpec with ⟨J, fJ, hJ1, hJ2⟩
@@ -103,27 +96,27 @@ theorem canonicalQuotientMap_dense (P : ProfiniteGrp.{u}) : Dense <|
       exact isOpen_iInter_of_finite fun i => i.1.1.isOpen'
     let m : OpenNormalSubgroup P := { M with isOpen' := hMOpen }
     rcases QuotientGroup.mk'_surjective M (spc m) with ⟨origin, horigin⟩
-    use (CanonicalQuotientMap P).toFun origin
+    use (toLimitQuotientOpenNormalSubgroup P).toFun origin
     refine ⟨?_, ⟨origin, rfl⟩⟩
     rw [← hsv]
     apply hJ2
     intro a a_in_J
     let M_to_Na : m ⟶ a := (iInf_le (fun (j : J) => j.1.1.1) ⟨a, a_in_J⟩).hom
-    rw [← (P.CanonicalQuotientMap.toFun origin).property M_to_Na]
+    rw [← (P.toLimitQuotientOpenNormalSubgroup.toFun origin).property M_to_Na]
     show (P.QuotientOpenNormalSubgroup.map M_to_Na) (QuotientGroup.mk' M origin) ∈ _
     rw [horigin]
     exact Set.mem_of_eq_of_mem (hspc M_to_Na) (hJ1 a a_in_J).2 )
 
-theorem canonicalQuotientMap_surjective (P : ProfiniteGrp.{u}) :
-    Function.Surjective (CanonicalQuotientMap P) := by
-  have : IsClosed (Set.range P.CanonicalQuotientMap) :=
-    P.CanonicalQuotientMap.continuous_toFun.isClosedMap.isClosed_range
+theorem toLimitQuotientOpenNormalSubgroup_surjective (P : ProfiniteGrp.{u}) :
+    Function.Surjective (toLimitQuotientOpenNormalSubgroup P) := by
+  have : IsClosed (Set.range P.toLimitQuotientOpenNormalSubgroup) :=
+    P.toLimitQuotientOpenNormalSubgroup.continuous_toFun.isClosedMap.isClosed_range
   rw [← Set.range_eq_univ, ← closure_eq_iff_isClosed.mpr this,
-    Dense.closure_eq <| canonicalQuotientMap_dense P]
+    Dense.closure_eq <| toLimitQuotientOpenNormalSubgroup_dense P]
 
-theorem canonicalQuotientMap_injective (P : ProfiniteGrp.{u}) :
-    Function.Injective (CanonicalQuotientMap P) := by
-  show Function.Injective (CanonicalQuotientMap P).toMonoidHom
+theorem toLimitQuotientOpenNormalSubgroup_injective (P : ProfiniteGrp.{u}) :
+    Function.Injective (toLimitQuotientOpenNormalSubgroup P) := by
+  show Function.Injective (toLimitQuotientOpenNormalSubgroup P).toMonoidHom
   rw [← MonoidHom.ker_eq_bot_iff, Subgroup.eq_bot_iff_forall]
   intro x h
   by_contra xne1
@@ -131,14 +124,15 @@ theorem canonicalQuotientMap_injective (P : ProfiniteGrp.{u}) :
     (Set.mem_compl_singleton_iff.mpr fun a => xne1 a.symm) with ⟨H, hH⟩
   exact hH ((QuotientGroup.eq_one_iff x).mp (congrFun (Subtype.val_inj.mpr h) H)) rfl
 
-/-- Make the equivilence into a ContinuousMulEquiv -/
+/-- The topological group isomorphism between a profinite group and the projective limit of
+its quotients by open normal subgroups -/
 noncomputable def ContinuousMulEquivLimitQuotientOpenNormalSubgroup (P : ProfiniteGrp.{u}) :
     P ≃ₜ* (limit ((QuotientOpenNormalSubgroup P) ⋙ (forget₂ FiniteGrp ProfiniteGrp))) := {
-  (Continuous.homeoOfEquivCompactToT2
-    (f := Equiv.ofBijective _ ⟨canonicalQuotientMap_injective P, canonicalQuotientMap_surjective P⟩)
-    P.CanonicalQuotientMap.continuous_toFun)
+  (Continuous.homeoOfEquivCompactToT2 (f := Equiv.ofBijective _
+  ⟨toLimitQuotientOpenNormalSubgroup_injective P, toLimitQuotientOpenNormalSubgroup_surjective P⟩)
+    P.toLimitQuotientOpenNormalSubgroup.continuous_toFun)
   with
-  map_mul' := (CanonicalQuotientMap P).map_mul' }
+  map_mul' := (toLimitQuotientOpenNormalSubgroup P).map_mul' }
 
 end
 
