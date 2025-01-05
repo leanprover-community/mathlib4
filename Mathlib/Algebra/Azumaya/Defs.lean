b5c67974b4ee98f5b442d35d836f10de064f9e19
Whysoserioushah <yx3021@ic.ac.uk>
2025-01-05 22:26:28 +0800

    apply suggestion and add documentation

--- Mathlib/Algebra/Azumaya/Defs.lean
+++ Mathlib/Algebra/Azumaya/Defs.lean
@@ -1,15 +1,18 @@
 /-
 Copyright (c) 2025 Yunzhou Xie. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Yunzhou Xie
+Authors: Yunzhou Xie, Jujian Zhang
 -/
 import Mathlib.Algebra.Module.Projective
 import Mathlib.RingTheory.TensorProduct.Basic
-import Mathlib.RingTheory.Finiteness.Basic
+import Mathlib.RingTheory.Finiteness.Defs
 
 /-!
 # Definition of Azumaya Algebras
-
+The definition of Azumaya algebra is a finitely generated, projective and faithful R-module
+where the tensorproduct `A ⊗[R] Aᵐᵒᵖ` is isomorphic to the endomorphism ring of A `End R A`
+via the map `f : a ⊗ b ↦ (x ↦ a * x * b.unop)`.
+TODO : Add the rest three definitions and prove they are equivalent.
 reference : Wikipedia, Noncommutative Algebra
 -/
 
@@ -17,43 +20,16 @@ variable (R A : Type*) [CommRing R] [Ring A] [Algebra R A]
 
 open TensorProduct MulOpposite
 
-@[simps]
-private abbrev moptoendo : A → Aᵐᵒᵖ →ₗ[R] Module.End R A :=
-  fun a ↦ {
-    toFun := fun b ↦ {
-      toFun := fun x ↦ a * x * unop b,
-      map_add' := fun _ _ ↦ by simp [mul_add, add_mul],
-      map_smul' := fun _ _ ↦ by simp
-    }
-    map_add' := fun _ _ ↦ by ext; simp [mul_add]
-    map_smul' := fun _ _ ↦ by ext; simp
-  }
+/-- The canonical map from `A ⊗[R] Aᵐᵒᵖ` to `Module.End R A` where
+  `a ⊗ b` maps to `f : x ↦ a * x * b`-/
+noncomputable abbrev endo : (A ⊗[R] Aᵐᵒᵖ) →ₐ[R] Module.End R A :=
+Algebra.TensorProduct.lift
+  (Algebra.lsmul R R A) (Algebra.lsmul R R A)
+  (fun _ _ ↦ by ext; simp [commute_iff_eq, mul_assoc])
 
-noncomputable abbrev endo : (A ⊗[R] Aᵐᵒᵖ) →ₐ[R] Module.End R A where
-  toFun := TensorProduct.lift (R := R) (M := A) (N := Aᵐᵒᵖ) (P := Module.End R A)
-    { toFun := moptoendo R A,
-      map_add' := fun _ _ ↦ by ext; simp [add_mul]
-      map_smul' := fun _ _ ↦ by ext; simp }
-  map_one' := by ext; simp [Algebra.TensorProduct.one_def]
-  map_mul' := fun x y ↦ by
-    induction x using TensorProduct.induction_on with
-    | zero =>
-      simp
-    | tmul x1 x2 =>
-      induction y using TensorProduct.induction_on with
-      | zero => simp
-      | tmul y1 y2 =>
-        ext; simp [mul_assoc]
-      | add y11 y22 h11 h22 =>
-        simp_all [mul_add]
-    | add x11 x22 h11 h22 =>
-      simp_all [add_mul]
-  map_zero' := rfl
-  map_add' := map_add _
-  commutes' := fun r ↦ by
-    dsimp
-    ext a
-    simp [Algebra.algebraMap_eq_smul_one]
+@[simp]
+lemma endo_apply (a : A) (b : Aᵐᵒᵖ) (x : A) : endo R A (a ⊗ₜ b) x = a * x * b.unop := by
+  simp [mul_assoc]
 
 class IsAzumaya extends Module.Projective R A, FaithfulSMul R A : Prop where
     fg : Module.Finite R A
