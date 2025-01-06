4f15e645eb686f794d076546124149a0c0ac6ad8
FM22 <artem.khovanov@gmail.com>
2025-01-06 20:22:37 +0000

    more fixes

--- Mathlib/Algebra/Ring/SumsOfSquares.lean
+++ Mathlib/Algebra/Ring/SumsOfSquares.lean
@@ -33,7 +33,7 @@ then for all `a : R`, `a * a + s` is a sum of squares in `R`.
 @[mk_iff]
 inductive IsSumSq [Mul R] [Add R] [Zero R] : R → Prop
   | zero                              : IsSumSq 0
-  | sq_add {a s : R} (hs : IsSumSq s) : IsSumSq (a * a + s)
+  | sq_add (a : R) {s : R} (hs : IsSumSq s) : IsSumSq (a * a + s)
 
 @[deprecated (since := "2024-08-09")] alias isSumSq := IsSumSq
 
@@ -41,9 +41,7 @@ inductive IsSumSq [Mul R] [Add R] [Zero R] : R → Prop
 if `s₁` and `s₂` are sums of squares, then `s₁ + s₂` is a sum of squares. -/
 theorem IsSumSq.add [AddMonoid R] [Mul R] {s₁ s₂ : R}
     (h₁ : IsSumSq s₁) (h₂ : IsSumSq s₂) : IsSumSq (s₁ + s₂) := by
-  induction h₁ with
-  | zero        => simp_all
-  | sq_add _ ih => simp_all [add_assoc, sq_add]
+  induction h₁ <;> simp_all [add_assoc, sq_add]
 
 @[deprecated (since := "2024-08-09")] alias isSumSq.add := IsSumSq.add
 
@@ -70,11 +68,12 @@ end AddSubmonoid
 @[deprecated (since := "2025-01-06")] alias sumSq := AddSubmonoid.sumSq
 
 /-- In an additive monoid with multiplication, squares are sums of squares
-(see Mathlib.Algebra.Group.Even.Basic). -/
-theorem mem_sumSq_of_isSquare [AddMonoid R] [Mul R] {x : R} (hx : IsSquare x) : x ∈ sumSq R := by
+(see Mathlib.Algebra.Group.Even). -/
+theorem mem_sumSq_of_isSquare [AddMonoid R] [Mul R] {x : R} (hx : IsSquare x) :
+    x ∈ AddSubmonoid.sumSq R := by
   rcases hx with ⟨y, hy⟩
   rw [hy, ← AddMonoid.add_zero (y * y)]
-  exact IsSumSq.sq_add _ _ IsSumSq.zero
+  exact IsSumSq.sq_add _ IsSumSq.zero
 
 @[deprecated (since := "2024-08-09")] alias SquaresInSumSq := mem_sumSq_of_isSquare
 @[deprecated (since := "2025-01-03")] alias mem_sumSqIn_of_isSquare := mem_sumSq_of_isSquare
@@ -85,7 +84,10 @@ sums of squares in `R`.
 -/
 theorem AddSubmonoid.closure_isSquare [AddMonoid R] [Mul R] :
     closure {x : R | IsSquare x} = sumSq R := by
-  refine closure_eq_of_le (fun x hx ↦ IsSquare.isSumSq hx) (fun x hx ↦ ?_)
+  refine le_antisymm (closure_le.2 (fun x hx ↦ mem_sumSq_of_isSquare hx)) (fun x hx ↦ ?_)
+  induction hx with
+  | zero           => exact zero_mem _
+  | sq_add _ _ ih  => exact add_mem (subset_closure ⟨_, rfl⟩) ih
 
 @[deprecated (since := "2024-08-09")] alias SquaresAddClosure := AddSubmonoid.closure_isSquare
 
@@ -93,11 +95,9 @@ theorem AddSubmonoid.closure_isSquare [AddMonoid R] [Mul R] :
 a term of the form `∑ i ∈ I, a i * a i` is a sum of squares. -/
 theorem IsSumSq.sum_mul_self [AddCommMonoid R] [Mul R] {ι : Type*} (I : Finset ι) (a : ι → R) :
     IsSumSq (∑ i ∈ I, a i * a i) := by
-  induction s using Finset.cons_induction with
-  | empty =>
-    simpa only [Finset.sum_empty] using IsSumSq.zero
-  | cons i s his h =>
-    exact (Finset.sum_cons (β := R) his) ▸ IsSumSq.sq_add (a i) (∑ i ∈ s, a i * a i) h
+  induction I using Finset.cons_induction with
+  | empty         => simpa using IsSumSq.zero
+  | cons i _ hi h => exact (Finset.sum_cons (β := R) hi) ▸ IsSumSq.sq_add (a i) h
 
 @[deprecated (since := "2024-12-27")] alias isSumSq_sum_mul_self := IsSumSq.sum_mul_self
 
@@ -108,7 +108,7 @@ sums of squares are non-negative.
 theorem IsSumSq.nonneg {R : Type*} [LinearOrderedSemiring R] [ExistsAddOfLE R] {s : R}
     (hs : IsSumSq s) : 0 ≤ s := by
   induction hs with
-  | zero             => simp only [le_refl]
-  | sq_add x s _ ih  => apply add_nonneg ?_ ih; simp only [← pow_two x, sq_nonneg]
+  | zero           => simp only [le_refl]
+  | sq_add x _ ih  => apply add_nonneg ?_ ih; simp only [← pow_two x, sq_nonneg]
 
 @[deprecated (since := "2024-08-09")] alias isSumSq.nonneg := IsSumSq.nonneg
