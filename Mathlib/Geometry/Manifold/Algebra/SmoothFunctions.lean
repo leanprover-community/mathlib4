ab7339920ef02e747a177630227ed0753d218667
Blizzard_inc <thrashy4inbox@gmail.com>
2025-01-06 16:12:16 +0100

    fix part 5

--- Mathlib/Geometry/Manifold/Algebra/SmoothFunctions.lean
+++ Mathlib/Geometry/Manifold/Algebra/SmoothFunctions.lean
@@ -281,12 +281,11 @@ def C : 𝕜 →+* C^∞⟮I, N; 𝓘(𝕜, A), A⟯ where
   map_zero' := by ext; exact (algebraMap 𝕜 A).map_zero
   map_add' c₁ c₂ := by ext; exact (algebraMap 𝕜 A).map_add _ _
 
-instance algebra : Algebra 𝕜 C^∞⟮I, N; 𝓘(𝕜, A), A⟯ :=
-  { --SmoothMap.semiring with -- Porting note: Commented this out.
-    smul := fun r f => ⟨r • f, contMDiff_const.smul f.contMDiff⟩
-    toRingHom := SmoothMap.C
-    commutes' := fun c f => by ext x; exact Algebra.commutes' _ _
-    smul_def' := fun c f => by ext x; exact Algebra.smul_def' _ _ }
+instance algebra : Algebra 𝕜 C^∞⟮I, N; 𝓘(𝕜, A), A⟯ where
+  smul := fun r f => ⟨r • f, contMDiff_const.smul f.contMDiff⟩
+  algebraMap := SmoothMap.C
+  commutes' := fun c f => by ext x; exact Algebra.commutes' _ _
+  smul_def' := fun c f => by ext x; exact Algebra.smul_def' _ _
 
 /-- Coercion to a function as an `AlgHom`. -/
 @[simps]
