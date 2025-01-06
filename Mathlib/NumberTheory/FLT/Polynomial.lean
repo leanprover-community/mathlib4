31b1f512b3f667b1a01cadf33e4165d7a2a7a436
Seewoo Lee <49933279+seewoo5@users.noreply.github.com>
2025-01-07 00:49:17 +0900

    Update docs
    Co-authored-by: Kevin Buzzard <k.buzzard@imperial.ac.uk>

--- Mathlib/NumberTheory/FLT/Polynomial.lean
+++ Mathlib/NumberTheory/FLT/Polynomial.lean
@@ -18,8 +18,8 @@ coprime polynomials `a, b, c` (over a field) with `a ^ n + b ^ n = c ^ n`,
 all polynomials must be constants.
 
 More generally, we can prove non-solvability of the Fermat-Catalan equation: there are no
-non-constant polynomial solution of the equation `u * a ^ p + v * b ^ q + w * c ^ r = 0`, where
-`p, q, r ≥ 3` with `p * q + q * r + r * p ≤ p * q * r` and not divisible by `char k`
+non-constant polynomial solutions to the equation `u * a ^ p + v * b ^ q + w * c ^ r = 0`, where
+`p, q, r ≥ 3` with `p * q + q * r + r * p ≤ p * q * r` , `p, q, r` not divisible by `char k`,
 and `u, v, w` are nonzero elements in `k`.
 FLT is the special case where `p = q = r = n`, `u = v = 1`, and `w = -1`.
 
