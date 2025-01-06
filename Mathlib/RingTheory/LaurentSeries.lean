23013433132611859a735c9ed68137d5595e6111
Kent Van Vels <kvanvels@protonmail.com>
2025-01-05 20:56:55 -0500

    fixed remaining classical in RingTheory.LaurentSeries

--- Mathlib/RingTheory/LaurentSeries.lean
+++ Mathlib/RingTheory/LaurentSeries.lean
@@ -540,6 +540,7 @@ theorem intValuation_eq_of_coe (P : K[X]) :
     (Ideal.span {↑P} : Ideal K⟦X⟧) ≠ 0 ∧ ((idealX K).asIdeal : Ideal K⟦X⟧) ≠ 0 := by
     simp only [Ideal.zero_eq_bot, ne_eq, Ideal.span_singleton_eq_bot, coe_eq_zero_iff, hP,
       not_false_eq_true, true_and, (idealX K).3]
+  classical
   rw [count_associates_factors_eq  (span_ne_zero).1
     (Ideal.span_singleton_prime Polynomial.X_ne_zero|>.mpr prime_X) (span_ne_zero).2,
     count_associates_factors_eq]
