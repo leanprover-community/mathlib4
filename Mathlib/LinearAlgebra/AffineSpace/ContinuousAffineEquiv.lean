eb9b33c15a9740457f71c22ae3f59d1091d986f6
Floris van Doorn <fpvdoorn@gmail.com>
2025-01-06 16:43:18 +0100

    fix

--- Mathlib/LinearAlgebra/AffineSpace/ContinuousAffineEquiv.lean
+++ Mathlib/LinearAlgebra/AffineSpace/ContinuousAffineEquiv.lean
@@ -45,13 +45,10 @@ structure ContinuousAffineEquiv (k P₁ P₂ : Type*) {V₁ V₂ : Type*} [Ring 
 notation:25 P₁ " ≃ᵃL[" k:25 "] " P₂:0 => ContinuousAffineEquiv k P₁ P₂
 
 variable {k P₁ P₂ P₃ P₄ V₁ V₂ V₃ V₄ : Type*} [Ring k]
-  [AddCommGroup V₁] [Module k V₁] [AddTorsor V₁ P₁]
-  [AddCommGroup V₂] [Module k V₂] [AddTorsor V₂ P₂]
-  [AddCommGroup V₃] [Module k V₃] [AddTorsor V₃ P₃]
-  [AddCommGroup V₄] [Module k V₄] [AddTorsor V₄ P₄]
-  [TopologicalSpace P₁]
-  [TopologicalSpace P₂]
-  [TopologicalSpace P₃] [TopologicalSpace P₄]
+  [AddCommGroup V₁] [Module k V₁] [AddTorsor V₁ P₁] [TopologicalSpace P₁]
+  [AddCommGroup V₂] [Module k V₂] [AddTorsor V₂ P₂] [TopologicalSpace P₂]
+  [AddCommGroup V₃] [Module k V₃] [AddTorsor V₃ P₃] [TopologicalSpace P₃]
+  [AddCommGroup V₄] [Module k V₄] [AddTorsor V₄ P₄] [TopologicalSpace P₄]
 
 namespace ContinuousAffineEquiv
 
@@ -102,10 +99,10 @@ def Simps.apply (e : P₁ ≃ᵃL[k] P₂) : P₁ → P₂ :=
   e
 
 /-- See Note [custom simps projection]. -/
-def Simps.coe (e : P₁ ≃ᵃL[k] P₂) : P₁ ≃ᵃ[k] P₂ :=
-  e
+def Simps.symm_apply (e : P₁ ≃ᵃL[k] P₂) : P₂ → P₁ :=
+  e.symm
 
-initialize_simps_projections ContinuousLinearMap (toFun → apply, toAffineEquiv → coe)
+initialize_simps_projections ContinuousAffineEquiv (toFun → apply, invFun → symm_apply)
 
 @[ext]
 theorem ext {e e' : P₁ ≃ᵃL[k] P₂} (h : ∀ x, e x = e' x) : e = e' :=
