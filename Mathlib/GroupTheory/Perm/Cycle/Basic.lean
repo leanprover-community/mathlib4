9e818d67f3cbdeda71c356727962f8a9e233a850
Komyyy <pol_tta@outlook.jp>
2025-01-07 16:43:13 +0900

    shake

--- Mathlib/GroupTheory/Perm/Cycle/Basic.lean
+++ Mathlib/GroupTheory/Perm/Cycle/Basic.lean
@@ -5,7 +5,6 @@ Authors: Chris Hughes, Yaël Dillies
 -/
 
 import Mathlib.Algebra.Module.BigOperators
-import Mathlib.Data.List.Iterate
 import Mathlib.GroupTheory.Perm.Finite
 import Mathlib.GroupTheory.Perm.List
 
