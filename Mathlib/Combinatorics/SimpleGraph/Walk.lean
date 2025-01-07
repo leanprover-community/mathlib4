2647207846119728be3530fa97955e3a1e3bfe2b
Daniel Weber <55664973+Command-Master@users.noreply.github.com>
2025-01-07 05:28:33 +0200

    Update Mathlib/Combinatorics/SimpleGraph/Walk.lean
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- Mathlib/Combinatorics/SimpleGraph/Walk.lean
+++ Mathlib/Combinatorics/SimpleGraph/Walk.lean
@@ -963,7 +963,7 @@ lemma cons_tail_eq (p : G.Walk x y) (hp : ¬ p.Nil) :
     simp only [getVert_cons_succ, tail_cons_eq, cons_copy, copy_rfl_rfl]
 
 @[simp]
-lemma concat_dropLast_eq (p : G.Walk x y) (hp : G.Adj p.penultimate y) :
+lemma concat_dropLast (p : G.Walk x y) (hp : G.Adj p.penultimate y) :
     p.dropLast.concat hp = p := by
   induction p with
   | nil => simp at hp
