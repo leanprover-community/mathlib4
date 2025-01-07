57f77c7b378336c2cb7def0e1960d6c789df2c9c
mariainesdff <mariaines.dff@gmail.com>
2025-01-07 15:32:55 +0100

    suggested changes

--- Mathlib/Order/Filter/ENNReal.lean
+++ Mathlib/Order/Filter/ENNReal.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Rémy Degenne
 -/
 import Mathlib.Topology.Instances.ENNReal
+
 /-!
 # Order properties of extended non-negative reals
 
