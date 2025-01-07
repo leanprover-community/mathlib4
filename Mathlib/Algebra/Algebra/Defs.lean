6fb5948c8371e9bc06978b646872a70bdad905cf
Blizzard_inc <thrashy4inbox@gmail.com>
2025-01-07 11:13:17 +0100

    adapt doc

--- Mathlib/Algebra/Algebra/Defs.lean
+++ Mathlib/Algebra/Algebra/Defs.lean
@@ -99,7 +99,8 @@ See the implementation notes in this file for discussion of the details of this 
 -/
 -- Porting note (https://github.com/leanprover-community/mathlib4/issues/5171): unsupported @[nolint has_nonempty_instance]
 class Algebra (R : Type u) (A : Type v) [CommSemiring R] [Semiring A] extends SMul R A where
-  /-- Embedding `R →+* A` given by `Algebra` structure. -/
+  /-- Embedding `R →+* A` given by `Algebra` structure.
+  Use `algebraMap` from the root namespace instead.-/
   protected algebraMap : R →+* A
   commutes' : ∀ r x, algebraMap r * x = x * algebraMap r
   smul_def' : ∀ r x, r • x = algebraMap r * x
