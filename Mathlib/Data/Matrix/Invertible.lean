19ef566422b5933b72a10c68781f4eee154f66e7
Eric Wieser <wieser.eric@gmail.com>
2025-01-07 00:20:37 +0000

    golf and style

--- Mathlib/Data/Matrix/Invertible.lean
+++ Mathlib/Data/Matrix/Invertible.lean
@@ -177,51 +177,35 @@ variable (u v : n → α)
 
 theorem add_col_mul_row_mul_invOf_eq_one
     [Invertible (1 + row ι v * ⅟A * col ι u)] :
-    (⅟A - (⅟A * (col ι u) * ⅟(1 + row ι v * ⅟A * col ι u) *
-      (row ι v) * ⅟A) ) * (A + col ι u * row ι v) = 1 := by
+    (⅟A - (⅟A * col ι u * ⅟(1 + row ι v * ⅟A * col ι u) * row ι v * ⅟A)) *
+      (A + col ι u * row ι v) = 1 := by
   haveI : Invertible (1 : Matrix ι ι α) := invertibleOne
-  haveI : Invertible (⅟ 1 + row ι v * ⅟A * col ι u) := by simpa [invOf_one']
-  have h1 : ⅟ (1 : Matrix ι ι α) = 1 := by exact invOf_one
-  have h2 : ⅟ (⅟1 + row ι v * ⅟A * col ι u) =
-    ⅟ (1 + row ι v * ⅟A * col ι u):= by congr!
-  have h3 :  col ι u * (1: Matrix ι ι α) * row ι v =
-    col ι u * row ι v := by rw [Matrix.mul_one]
-  rw [← h2, ← h3]
-  apply add_mul_mul_invOf_mul_eq_one'
+  haveI : Invertible (⅟ 1 + row ι v * ⅟A * col ι u) := by simpa
+  simpa using add_mul_mul_invOf_mul_eq_one' A (col ι u) 1 (row ι v)
 
-/-- Like `add_col_mul_row_mul_invOf_eq_one`, but with multiplication reversed. -/
+/-- Like `Matrix.add_col_mul_row_mul_invOf_eq_one`, but with multiplication reversed. -/
 theorem add_col_mul_row_mul_invOf_eq_one'
     [Invertible (1 + row ι v * ⅟A * col ι u)] :
     (A + col ι u * row ι v) *
-    (⅟A - (⅟A * (col ι u) * ⅟(1 + row ι v * ⅟A * col ι u) *
-      (row ι v) * ⅟A) ) = 1 := by
-  haveI : Invertible (1 : Matrix ι ι α) := by
-    exact invertibleOne
-  haveI : Invertible (⅟ 1 + row ι v * ⅟A * col ι u) := by simpa [invOf_one']
-  have h1 : ⅟ (1 : Matrix ι ι α) = 1 := by exact invOf_one
-  have h2 : ⅟ (⅟1 + row ι v * ⅟A * col ι u) =
-    ⅟(1 + row ι v * ⅟A * col ι u) := by congr!
-  have h3 :  col ι u * (1: Matrix ι ι α) * row ι v =
-    col ι u * row ι v := by rw [Matrix.mul_one]
-  rw [← h2, ← h3]
-  apply add_mul_mul_invOf_mul_eq_one
+      (⅟A - (⅟A * col ι u * ⅟(1 + row ι v * ⅟A * col ι u) * row ι v * ⅟A)) = 1 := by
+  haveI : Invertible (1 : Matrix ι ι α) := invertibleOne
+  haveI : Invertible (⅟ 1 + row ι v * ⅟A * col ι u) := by simpa
+  simpa using add_mul_mul_invOf_mul_eq_one A (col ι u) 1 (row ι v)
 
 variable (ι) in
 /-- If matrix `A` and the scalar `(1 + row ι v * ⅟A * col ι u)` are invertible,
-  then so is (A + col ι u * row ι v) -/
+then so is (A + col ι u * row ι v) -/
 def invertibleAddColMulRow [Invertible (1 + row ι v * ⅟A * col ι u)] :
     Invertible (A + col ι u * row ι v) where
-  invOf := (⅟A - (⅟A * (col ι u) * ⅟(1 + row ι v * ⅟A *
-    col ι u) * (row ι v) * ⅟A))
+  invOf := (⅟A - (⅟A * col ι u * ⅟(1 + row ι v * ⅟A * col ι u) * row ι v * ⅟A))
   invOf_mul_self := add_col_mul_row_mul_invOf_eq_one _ _ _
   mul_invOf_self := add_col_mul_row_mul_invOf_eq_one' _ _ _
 
--- /-- The **Sherman Morrison Rank-1 Update** (`⅟` version). -/
+/-- The **Sherman Morrison Rank-1 Update** (`⅟` version). -/
 theorem invOf_add_col_mul_row_mul [Invertible (1 + row ι v * ⅟A * col ι u)]
-  [Invertible (A + col ι u * row ι v)] :
+    [Invertible (A + col ι u * row ι v)] :
     ⅟(A + col ι u * row ι v) =
-    (⅟A - (⅟A * (col ι u) * ⅟(1 + row ι v * ⅟A * col ι u) *
-      (row ι v) * ⅟A) ) := by
+      (⅟A - (⅟A * col ι u * ⅟(1 + row ι v * ⅟A * col ι u) * row ι v * ⅟A)) := by
   letI := invertibleAddColMulRow A ι u v
   convert (rfl : ⅟(A + col ι u * row ι v) = _)
 
