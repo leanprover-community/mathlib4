df377dce55bc9155cb6e26de7a5bf1d4b044909d
emilyriehl <eriehl@jhu.edu>
2025-01-07 01:19:56 -0500

    minor extension

--- Mathlib/AlgebraicTopology/SimplicialSet/Elephant.lean
+++ Mathlib/AlgebraicTopology/SimplicialSet/Elephant.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2025 Nick Ward. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Nick Ward
+-/
 import Mathlib.AlgebraicTopology.SimplicialSet.Basic
 import Mathlib.Util.Superscript
 
@@ -68,13 +73,14 @@ structure Path₁ (X : Truncated.{u} 1) (n : ℕ) where
 
 variable {n : ℕ} (X : SSet.Truncated.{u} (n + 1))
 
-/-- A path in any `n + 1`-truncated simplicial set `X` is defined by further 1-truncating `X`, then taking the 1-truncated path. -/
+/-- A path in any `n + 1`-truncated simplicial set `X` is defined by further 1-truncating `X`, then
+taking the 1-truncated path. -/
 abbrev Path : ℕ → Type u := trunc (by omega) |>.obj X |>.Path₁
 
 /-- The spine of an `n + 1`-simplex in an `n + 1`-truncated simplicial set `X` is the path of edges
 of length `n + 1` formed by traversing through its vertices in order. -/
-def spine (Δ : X _[n + 1]ₙ₊₁) : Path X (n + 1) where
-  vertex i := X.map (SimplexCategory.const [0] [n + 1] i).op Δ
+def spine {m} (hmn : m ≤ n + 1) (Δ : X _[m]ₙ₊₁) : Path X m where
+  vertex i := X.map (SimplexCategory.const [0] [m] i).op Δ
   arrow i := X.map (SimplexCategory.mkOfSucc i).op Δ
   arrow_src i := by
     erw [← FunctorToTypes.map_comp_apply, ← op_comp (f := (δ 1).op.unop)]
@@ -86,9 +92,9 @@ def spine (Δ : X _[n + 1]ₙ₊₁) : Path X (n + 1) where
 /-- An `n + 1`-truncated simplicial set satisfies the strict Segal condition if its
 `n + 1`-simplices are uniquely determined by their spine. -/
 class StrictSegal where
-  spineToSimplex {m : ℕ} {hmn : m ≤ n + 1} : Path X m → X _[m]ₙ₊₁
-  spine_spineToSimplex {m : ℕ} {hmn : m ≤ n + 1} : X.spine ∘ spineToSimplex = id
-  spineToSimplex_spine {m : ℕ} {hmn : m ≤ n + 1} : spineToSimplex ∘ X.spine = id
+  spineToSimplex {m : ℕ} (hmn : m ≤ n + 1) : Path X m → X _[m]ₙ₊₁
+  spine_spineToSimplex {m : ℕ} (hmn : m ≤ n + 1) : X.spine hmn ∘ spineToSimplex hmn = id
+  spineToSimplex_spine {m : ℕ} (hmn : m ≤ n + 1) : spineToSimplex hmn ∘ X.spine hmn = id
 
 end SSet.Truncated
 
@@ -105,7 +111,7 @@ abbrev Path : ℕ → Type u := truncation 1 |>.obj X |>.Path₁
 `n + 1` formed by traversing in order through the vertices of the `n + 1`
 truncation. -/
 abbrev spine {n : ℕ} : X _[n + 1] → X.Path (n + 1) :=
-  truncation (n + 1) |>.obj X |>.spine
+  truncation (n + 1) |>.obj X |>.spine (Nat.le_refl _)
 
 /-- A simplicial set `X` satisfies the strict Segal condition if its simplices
 are uniquely determined by their spine. -/
@@ -115,16 +121,29 @@ class StrictSegal where
   spine_spineToSimplex {n : ℕ} : X.spine (n := n) ∘ spineToSimplex = id
   spineToSimplex_spine {n : ℕ} : spineToSimplex ∘ X.spine (n := n) = id
 
-def test (n : ℕ) : n ≤ n := by exact Nat.le_refl n
-
 instance strictSegal_of_strictSegal
     [∀ n : ℕ, Truncated.StrictSegal ((truncation (n + 1)).obj X)] :
     StrictSegal X where
   spineToSimplex {n} :=
-    Truncated.StrictSegal.spineToSimplex (X := (truncation (n + 1)).obj X)
+    Truncated.StrictSegal.spineToSimplex (X := (truncation (n + 1)).obj X) (Nat.le_refl _)
   spine_spineToSimplex {n} :=
-    Truncated.StrictSegal.spine_spineToSimplex (m := n + 1) (hmn := Nat.le_refl _)
+    Truncated.StrictSegal.spine_spineToSimplex (Nat.le_refl _)
   spineToSimplex_spine {n} :=
-    Truncated.StrictSegal.spineToSimplex_spine (m := n + 1) (hmn := Nat.le_refl _)
+    Truncated.StrictSegal.spineToSimplex_spine (Nat.le_refl _)
 
 end SSet
+
+namespace SSet.Truncated.StrictSegal
+
+variable {n} {X : SSet.Truncated.{u} (n + 1)} [StrictSegal X]
+
+/-- The fields of `StrictSegal` define an equivalence between `X [m]ₙ₊₁` and `Path X m`.-/
+def spineEquiv {m : ℕ} (hmn : m ≤ n + 1) : X _[m]ₙ₊₁ ≃ Path X m where
+  toFun := spine X hmn
+  invFun := spineToSimplex hmn
+  left_inv := by
+    exact congrFun (spineToSimplex_spine (X := X) hmn)
+  right_inv := by
+    exact congrFun (spine_spineToSimplex (X := X) hmn)
+
+end SSet.Truncated.StrictSegal
