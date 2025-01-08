c9090ee3c1ed6404c2304c0d8e963eb5750bfc00
Christopher Hoskin <christopher.hoskin@gmail.com>
2025-01-08 19:12:02 +0000

    Use Module.toAddMonoidEnd

--- Mathlib/Algebra/Group/Action/Ring.lean
+++ Mathlib/Algebra/Group/Action/Ring.lean
@@ -5,20 +5,24 @@ Authors: Christopher Hoskin
 -/
 
 import Mathlib.Algebra.Ring.Idempotents
-import Mathlib.Algebra.Module.Defs
+import Mathlib.Algebra.Module.End
 import Mathlib.Algebra.Group.Subgroup.Ker
+
+/-!
+# Action of a Ring on a Module
+
+-/
 
 variable (X : Type*) [AddCommGroup X]
 variable {M : Type*} [Ring M] [Module M X]
 
-variable (p : M)
-
 open AddMonoidHom in
-open DistribMulAction in
+open Module in
 lemma ker_id_sub_eq_range  {p : M} (h : IsIdempotentElem p) :
     ker (toAddMonoidEnd M X (1 - p)) = range (toAddMonoidEnd M X p) := by
   ext x
-  rw [mem_ker, mem_range, toAddMonoidEnd_apply, toAddMonoidHom_apply,  toAddMonoidEnd_apply,
-    sub_smul, one_smul, sub_eq_zero]
-  simp_rw [toAddMonoidHom_apply]
-  exact ⟨fun h => ⟨x,h.symm⟩, fun ⟨y,hy⟩ => by rw [← hy, ← smul_assoc, smul_eq_mul, h]⟩
+  rw [mem_ker, mem_range, map_sub, map_one, sub_apply, toAddMonoidEnd_apply_apply, sub_eq_zero,
+    AddMonoid.End.coe_one, id_eq]
+  simp_rw [toAddMonoidEnd_apply_apply]
+  exact ⟨fun h => ⟨x,h.symm⟩, fun ⟨y,hy⟩ => by
+    rw [← hy, ← smul_assoc, smul_eq_mul, h]⟩
