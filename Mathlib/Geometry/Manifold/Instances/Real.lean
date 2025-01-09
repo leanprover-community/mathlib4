dc1986e491cd5d52e9dd66a32306e01c5e188c48
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-01-09 12:15:36 +0100

    Clean up.

--- Mathlib/Geometry/Manifold/Instances/Real.lean
+++ Mathlib/Geometry/Manifold/Instances/Real.lean
@@ -308,7 +308,7 @@ lemma IccLeftChart_extend_left_eq :
     ((IccLeftChart x y).extend (𝓡∂ 1)) (Icc.instOrderBotElem.bot) = 0 := by
   calc ((IccLeftChart x y).extend (𝓡∂ 1)) (Icc.instOrderBotElem.bot)
     _ = (𝓡∂ 1) ⟨fun _ ↦ 0, by norm_num⟩ := by
-      norm_num [IccLeftChart, show (⊥ : Set.Icc x y) = x by rfl]
+      norm_num [IccLeftChart, Icc.coe_bot]
     _ = 0 := rfl
 
 lemma IccLeftChart_extend_interior_pos {p : Set.Icc x y} (hp : x < p.val ∧ p.val < y) :
@@ -375,7 +375,7 @@ lemma IccRightChart_extend_right_eq :
     (IccRightChart x y).extend (𝓡∂ 1) (Icc.instOrderTopElem.top) = 0 := by
   calc ((IccRightChart x y).extend (𝓡∂ 1)) (Icc.instOrderTopElem.top)
     _ = (𝓡∂ 1) ⟨fun _ ↦ 0, by norm_num⟩ := by
-      norm_num [IccRightChart, show (⊤ : Set.Icc x y) = y by rfl]
+      norm_num [IccRightChart, Icc.coe_top]
     _ = 0 := rfl
 
 lemma IccRightChart_extend_right_mem_frontier :
