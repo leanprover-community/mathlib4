951903e72473452c1ba96d3a102845541f6203ec
WilliamCoram <williamecoram@gmail.com>
2025-01-09 14:38:28 +0000

    Removing unneccesary definitions

--- Mathlib/RingTheory/DedekindDomain/FiniteAdeleRing.lean
+++ Mathlib/RingTheory/DedekindDomain/FiniteAdeleRing.lean
@@ -336,10 +336,6 @@ instance : Algebra R (FiniteAdeleRing R K) :=
 instance : IsScalarTower R K (FiniteAdeleRing R K) :=
   IsScalarTower.of_algebraMap_eq' rfl
 
-/-- Coercion `FiniteAdeleRing R K → K_hat R K`. -/
-@[coe]
-def coe (x : FiniteAdeleRing R K) : K_hat R K := x.1
-
 instance : Coe (FiniteAdeleRing R K) (K_hat R K) where
   coe x := x.1
 
@@ -382,17 +378,6 @@ instance : Algebra (R_hat R K) (FiniteAdeleRing R K) where
 instance : CoeFun (FiniteAdeleRing R K)
     (fun _ ↦ ∀ (v : HeightOneSpectrum R), adicCompletion K v) where
   coe a v := a.1 v
-
-section Algebra
-
-variable (L : Type*) [Field L] [Algebra L K] [Algebra L (FiniteAdeleRing R K)]
-  [IsScalarTower L K (FiniteAdeleRing R K)]
-
-theorem algebraMap_extension (r : L) :
-  algebraMap L (FiniteAdeleRing R K) r = algebraMap K (FiniteAdeleRing R K) (algebraMap L K r) :=
-  IsScalarTower.algebraMap_apply L K (FiniteAdeleRing R K) r
-
-end Algebra
 
 open scoped algebraMap -- coercion from R to `FiniteAdeleRing R K`
 
