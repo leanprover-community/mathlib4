a0ac0cb28ddb393895de2c9e522493fb3f668380
Yaël Dillies <yael.dillies@gmail.com>
2025-01-09 22:28:42 +0000

    antigolf
    Co-authored-by: Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>

--- Mathlib/Algebra/CharP/Defs.lean
+++ Mathlib/Algebra/CharP/Defs.lean
@@ -191,7 +191,7 @@ end ringChar
 lemma CharP.neg_one_ne_one [Ring R] (p : ℕ) [CharP R p] [Fact (2 < p)] : (-1 : R) ≠ (1 : R) := by
   rw [ne_comm, ← sub_ne_zero, sub_neg_eq_add, one_add_one_eq_two, ← Nat.cast_two, Ne,
     CharP.cast_eq_zero_iff R p 2]
-  exact fun h ↦ (Fact.out : 2 < p).not_le <| Nat.le_of_dvd (by decide) h
+  exact fun h ↦ (Fact.out : 2 < p).not_le <| Nat.le_of_dvd Nat.zero_lt_two h
 
 namespace CharP
 
