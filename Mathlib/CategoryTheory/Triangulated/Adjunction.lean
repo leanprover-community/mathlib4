81be01280fb3294704c0e2a3203fde08f65f7968
smorel394 <67864981+smorel394@users.noreply.github.com>
2025-01-10 21:39:03 +0100

    Update Mathlib/CategoryTheory/Triangulated/Adjunction.lean
    Co-authored-by: Joël Riou <37772949+joelriou@users.noreply.github.com>

--- Mathlib/CategoryTheory/Triangulated/Adjunction.lean
+++ Mathlib/CategoryTheory/Triangulated/Adjunction.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2024 Joël Riou. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Sophie Morel, Joël Riou
+Authors: Joël Riou, Sophie Morel
 -/
 import Mathlib.CategoryTheory.Triangulated.Functor
 import Mathlib.CategoryTheory.Shift.Adjunction
