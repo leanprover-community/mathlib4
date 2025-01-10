accf41dfb2a8e9b712828d918a16df5c082bc523
Kim Morrison <kim@tqft.net>
2025-01-10 13:01:34 +1100

    fixes after shake

--- Mathlib/Data/One/Lemmas.lean
+++ Mathlib/Data/One/Lemmas.lean
@@ -3,6 +3,7 @@ Copyright (c) 2023 Yael Dillies. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yael Dillies
 -/
+import Batteries.Tactic.Init
 import Mathlib.Data.One.Defs
 import Mathlib.Tactic.ToAdditive
 import Mathlib.Tactic.Lemma
