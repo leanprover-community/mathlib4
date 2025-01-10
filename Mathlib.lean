ad4b7f451e34fc84c4669890678e8fea56395c58
JakobStiefel <jakobstiefel@hotmail.de>
2025-01-10 16:29:31 +0100

    update Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -1563,6 +1563,7 @@ import Mathlib.Analysis.SpecialFunctions.Log.ERealExp
 import Mathlib.Analysis.SpecialFunctions.Log.Monotone
 import Mathlib.Analysis.SpecialFunctions.Log.NegMulLog
 import Mathlib.Analysis.SpecialFunctions.MulExpNegSq
+import Mathlib.Analysis.SpecialFunctions.MulExpNegSqIntegral
 import Mathlib.Analysis.SpecialFunctions.NonIntegrable
 import Mathlib.Analysis.SpecialFunctions.OrdinaryHypergeometric
 import Mathlib.Analysis.SpecialFunctions.PolarCoord
