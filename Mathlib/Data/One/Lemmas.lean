d42123befada1d0a3f91f881a34d573bc95b0b52
Kim Morrison <kim@tqft.net>
2025-01-10 13:00:14 +1100

    shake

--- Mathlib/Data/One/Lemmas.lean
+++ Mathlib/Data/One/Lemmas.lean
@@ -3,7 +3,6 @@ Copyright (c) 2023 Yael Dillies. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yael Dillies
 -/
-import Batteries.Tactic.Init
 import Mathlib.Data.One.Defs
 import Mathlib.Tactic.ToAdditive
 import Mathlib.Tactic.Lemma
--- Mathlib/Tactic/Bound/Attribute.lean
+++ Mathlib/Tactic/Bound/Attribute.lean
@@ -3,7 +3,6 @@ Copyright (c) 2024 Geoffrey Irving. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Geoffrey Irving
 -/
-import Mathlib.Data.One.Defs
 import Mathlib.Tactic.Bound.Init
 import Qq
 import Aesop
