d277e12df34c8119a608d319ed1bcbd78399c141
Kim Morrison <kim@tqft.net>
2025-01-10 11:57:56 +1100

    chore split Mathlib/Algebra/Group/Int

---
+++ Mathlib/Algebra/Group/Int/TypeTags.lean
@@ -0,0 +1,33 @@
+/-
+Copyright (c) 2016 Jeremy Avigad. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Jeremy Avigad
+-/
+import Mathlib.Algebra.Group.Int.Defs
+import Mathlib.Algebra.Group.Nat.Even
+import Mathlib.Algebra.Group.Nat.Units
+import Mathlib.Algebra.Group.Units.Basic
+import Mathlib.Data.Int.Sqrt
+
+/-!
+# Lemmas about `Multiplicative ℤ`.
+-/
+
+
+open Nat
+
+namespace Int
+
+section Multiplicative
+
+open Multiplicative
+
+lemma toAdd_pow (a : Multiplicative ℤ) (b : ℕ) : (a ^ b).toAdd = a.toAdd * b := mul_comm _ _
+
+lemma toAdd_zpow (a : Multiplicative ℤ) (b : ℤ) : (a ^ b).toAdd = a.toAdd * b := mul_comm _ _
+
+@[simp] lemma ofAdd_mul (a b : ℤ) : ofAdd (a * b) = ofAdd a ^ b := (toAdd_zpow ..).symm
+
+end Multiplicative
+
+end Int
