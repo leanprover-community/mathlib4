f44693687cb6b68a9c18c3663c01e8d89eeb6f4f
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-01-12 00:05:00 +0100

    feat(CategoryTheory/Localization): HasLocalization holds iff the localized category is locally small

--- Mathlib.lean
+++ Mathlib.lean
@@ -2000,6 +2000,7 @@ import Mathlib.CategoryTheory.Localization.FiniteProducts
 import Mathlib.CategoryTheory.Localization.HasLocalization
 import Mathlib.CategoryTheory.Localization.HomEquiv
 import Mathlib.CategoryTheory.Localization.LocalizerMorphism
+import Mathlib.CategoryTheory.Localization.LocallySmall
 import Mathlib.CategoryTheory.Localization.Opposite
 import Mathlib.CategoryTheory.Localization.Pi
 import Mathlib.CategoryTheory.Localization.Predicate
