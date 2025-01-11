3f468d39e5495c0523afb68143932af88b54155b
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-01-11 22:30:35 +0100

    the retract argument

--- Mathlib.lean
+++ Mathlib.lean
@@ -2076,6 +2076,7 @@ import Mathlib.CategoryTheory.MorphismProperty.Limits
 import Mathlib.CategoryTheory.MorphismProperty.OverAdjunction
 import Mathlib.CategoryTheory.MorphismProperty.Representable
 import Mathlib.CategoryTheory.MorphismProperty.Retract
+import Mathlib.CategoryTheory.MorphismProperty.RetractArgument
 import Mathlib.CategoryTheory.MorphismProperty.TransfiniteComposition
 import Mathlib.CategoryTheory.NatIso
 import Mathlib.CategoryTheory.NatTrans
