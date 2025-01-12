0634717cdd24285ab4685a52cbf27141f2be1455
damiano <adomani@gmail.com>
2025-01-12 10:49:23 +0000

    feat: allow several identifiers in assert_not_exists (#15994)
    Align the `assert_not_exists` syntax to `assert_not_imported`: they now both allow several identifiers.
    
    
    
    Co-authored-by: Jon Eugster <eugster.jon@gmail.com>

--- Mathlib/Algebra/Order/Ring/Unbundled/Basic.lean
+++ Mathlib/Algebra/Order/Ring/Unbundled/Basic.lean
@@ -113,8 +113,7 @@ TODO: the mixin assumptiosn can be relaxed in most cases
 
 -/
 
-assert_not_exists OrderedCommMonoid
-assert_not_exists MonoidHom
+assert_not_exists OrderedCommMonoid MonoidHom
 
 open Function
 
