0f35d84cf87b1a38718e23d69bf7b38b130f8d9c
mitchell-horner <29882987+mitchell-horner@users.noreply.github.com>
2025-01-12 20:42:32 +1100

    Update imports

--- Mathlib/Combinatorics/SimpleGraph/DeleteEdges.lean
+++ Mathlib/Combinatorics/SimpleGraph/DeleteEdges.lean
@@ -3,6 +3,7 @@ Copyright (c) 2025 Mitchell Horner. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mitchell Horner
 -/
+import Mathlib.Algebra.Order.Ring.Defs
 import Mathlib.Combinatorics.SimpleGraph.Finite
 import Mathlib.Combinatorics.SimpleGraph.Maps
 
