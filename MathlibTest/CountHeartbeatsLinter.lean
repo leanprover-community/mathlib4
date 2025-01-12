07375c4b5185baaed449547fba9e0e14e56238d7
adomani <adomani@gmail.com>
2025-01-12 13:48:35 +0100

    rename to `#`

--- MathlibTest/CountHeartbeatsLinter.lean
+++ MathlibTest/CountHeartbeatsLinter.lean
@@ -1,23 +1,23 @@
 import Mathlib.Util.CountHeartbeats
 
 -- sets the `countHeartbeats` linter option to `true`
-count_heartbeats
+#count_heartbeats
 
 mutual -- mutual declarations get ignored
 theorem XY : True := trivial
 end
 
-/-- info: Used 3 heartbeats, which is less than the current maximum of 200000. -/
+/-- info: used 3 heartbeats, which is less than the current maximum of 200000. -/
 #guard_msgs in
 set_option linter.unusedTactic false in
 set_option linter.unusedTactic false in
 example : True := trivial
 
-/-- info: Used 3 heartbeats, which is less than the current maximum of 200000. -/
+/-- info: used 3 heartbeats, which is less than the current maximum of 200000. -/
 #guard_msgs in
 example : True := trivial
 
-/-- info: 'YX' Used 2 heartbeats, which is less than the current maximum of 200000. -/
+/-- info: 'YX' used 2 heartbeats, which is less than the current maximum of 200000. -/
 #guard_msgs in
 set_option linter.unusedTactic false in
 set_option linter.unusedTactic false in
