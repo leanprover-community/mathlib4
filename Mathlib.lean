4f2a1292fb83476bbef2f35037dfd06bf1e792ba
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-01-13 10:02:39 +0000

    feat(SetTheory): the predicate HasCardinalLT (#19959)
    We introduce the universe-heterogeneous predicate `HasCardinalLT X κ` for `X : Type u` and `κ : Cardinal.{v}` saying that the cardinal of `X` is (strictly) less than `κ`. (This shall be used in the formalization of `κ`-filtered categories, which generalize filtered categories.)

--- Mathlib.lean
+++ Mathlib.lean
@@ -4771,6 +4771,7 @@ import Mathlib.SetTheory.Cardinal.ENat
 import Mathlib.SetTheory.Cardinal.Finite
 import Mathlib.SetTheory.Cardinal.Finsupp
 import Mathlib.SetTheory.Cardinal.Free
+import Mathlib.SetTheory.Cardinal.HasCardinalLT
 import Mathlib.SetTheory.Cardinal.SchroederBernstein
 import Mathlib.SetTheory.Cardinal.Subfield
 import Mathlib.SetTheory.Cardinal.ToNat
