1ae77709cf32be9f213ae0ae0d143352d31f743d
grunweg <rothgami@math.hu-berlin.de>
2025-01-13 21:56:28 +0100

    Update Mathlib/Data/Matrix/Kronecker.lean
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Mathlib/Data/Matrix/Kronecker.lean
+++ Mathlib/Data/Matrix/Kronecker.lean
@@ -421,7 +421,7 @@ def kroneckerTMul : Matrix l m α → Matrix n p β → Matrix (l × n) (m × p)
 @[inherit_doc kroneckerTMul]
 scoped[Kronecker] infixl:100 " ⊗ₖₜ " => Matrix.kroneckerMap (· ⊗ₜ ·)
 
-@[inherit_doc kroneckerTMul] 
+@[inherit_doc kroneckerTMul]
 scoped[Kronecker] notation:100 x " ⊗ₖₜ[" R "] " y:100 => Matrix.kroneckerMap (TensorProduct.tmul R) x y
 
 open Kronecker
