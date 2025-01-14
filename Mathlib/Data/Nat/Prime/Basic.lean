f1d43ab4625111d2e06339da456aab52a0d128d2
Yaël Dillies <yael.dillies@gmail.com>
2025-01-14 11:19:23 +0000

    chore(Associated): split out `Ring` results

--- Mathlib/Data/Nat/Prime/Basic.lean
+++ Mathlib/Data/Nat/Prime/Basic.lean
@@ -3,7 +3,7 @@ Copyright (c) 2015 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Leonardo de Moura, Jeremy Avigad, Mario Carneiro
 -/
-import Mathlib.Algebra.Associated.Basic
+import Mathlib.Algebra.GroupWithZero.Associated
 import Mathlib.Algebra.Ring.Parity
 import Mathlib.Data.Nat.Prime.Defs
 
