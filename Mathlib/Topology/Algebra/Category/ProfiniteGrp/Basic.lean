4c64d91ad04576aadd224da7be759fad051b3099
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2025-01-14 22:30:52 +0800

    delete unused import

--- Mathlib/Topology/Algebra/Category/ProfiniteGrp/Basic.lean
+++ Mathlib/Topology/Algebra/Category/ProfiniteGrp/Basic.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jujian Zhang, Nailin Guan, Yuyang Zhao
 -/
 import Mathlib.Algebra.Category.Grp.FiniteGrp
-import Mathlib.CategoryTheory.ConcreteCategory.EpiMono
 import Mathlib.Topology.Algebra.ClosedSubgroup
 import Mathlib.Topology.Algebra.ContinuousMonoidHom
 import Mathlib.Topology.Category.Profinite.Basic
