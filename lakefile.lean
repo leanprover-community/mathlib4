f93b15270610d8ce65205b24fe058183bf1fe51b
Kim Morrison <kim@tqft.net>
2025-01-14 14:12:27 +1100

    chore: bump to v4.16.0-rc2

--- lakefile.lean
+++ lakefile.lean
@@ -9,11 +9,11 @@ open Lake DSL
 
 require "leanprover-community" / "batteries" @ git "main"
 require "leanprover-community" / "Qq" @ git "v4.15.0"
-require "leanprover-community" / "aesop" @ git "v4.16.0-rc1"
+require "leanprover-community" / "aesop" @ git "master"
 require "leanprover-community" / "proofwidgets" @ git "v0.0.50"
 require "leanprover-community" / "importGraph" @ git "main"
 require "leanprover-community" / "LeanSearchClient" @ git "main"
-require "leanprover-community" / "plausible" @ git "v4.16.0-rc1"
+require "leanprover-community" / "plausible" @ git "main"
 
 /-!
 ## Options for building mathlib
