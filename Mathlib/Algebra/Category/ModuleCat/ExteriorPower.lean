e213b71bbc47ec60a0cec27cf7269041fd686483
Joël Riou <37772949+joelriou@users.noreply.github.com>
2025-01-15 21:22:23 +0100

    Update Mathlib/Algebra/Category/ModuleCat/ExteriorPower.lean

--- Mathlib/Algebra/Category/ModuleCat/ExteriorPower.lean
+++ Mathlib/Algebra/Category/ModuleCat/ExteriorPower.lean
@@ -9,7 +9,7 @@ import Mathlib.Algebra.Category.ModuleCat.Basic
 /-!
 # The exterior powers as functors on the category of modules
 
-In this file, given `M : ModuleCat R` and `n : ℕ`, we define `M.exterior n : ModuleCat R`,
+In this file, given `M : ModuleCat R` and `n : ℕ`, we define `M.exteriorPower n : ModuleCat R`,
 and this extends to a functor `ModuleCat.exteriorPower.functor : ModuleCat R ⥤ ModuleCat R`.
 
 -/
