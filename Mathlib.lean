c52d45710d18eeeb6a04d6c1a3a9c95c26ba9852
Oliver Nash <github@olivernash.org>
2025-01-15 19:31:20 +0000

    Split file `LinearAlgebra.PerfectPairing`

--- Mathlib.lean
+++ Mathlib.lean
@@ -3548,7 +3548,8 @@ import Mathlib.LinearAlgebra.Multilinear.Pi
 import Mathlib.LinearAlgebra.Multilinear.TensorProduct
 import Mathlib.LinearAlgebra.Orientation
 import Mathlib.LinearAlgebra.PID
-import Mathlib.LinearAlgebra.PerfectPairing
+import Mathlib.LinearAlgebra.PerfectPairing.Basic
+import Mathlib.LinearAlgebra.PerfectPairing.Restrict
 import Mathlib.LinearAlgebra.Pi
 import Mathlib.LinearAlgebra.PiTensorProduct
 import Mathlib.LinearAlgebra.Prod
