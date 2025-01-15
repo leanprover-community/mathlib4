0c82aa8fcc382d16b6a8b7a33f806906fb0c8a6b
Michał Staromiejski <michal.staromiejski@gmail.com>
2025-01-15 23:14:29 +0000

    refactor: move files from `AlgebraicGeometry/PrimeSpectrum` to `RingTheory/Spectrum/Prime` (#20778)
    Move files from `AlgebraicGeometry/PrimeSpectrum` to `RingTheory/Spectrum/Prime` with renaming `Basic.lean` to `Topology.lean` as discussed [here](https://leanprover.zulipchat.com/#narrow/channel/287929-mathlib4/topic/Maximal.20spectrum.20of.20a.20ring).
    
    Move `RingTheory/Spectrum/Prime/Maximal.lean` to `RingTheory/Spectrum/Maximal/Topology.lean` for consistency.

--- Mathlib/RingTheory/Spectrum/Prime/Jacobson.lean
+++ Mathlib/RingTheory/Spectrum/Prime/Jacobson.lean
@@ -3,8 +3,8 @@ Copyright (c) 2024 Andrew Yang. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Andrew Yang
 -/
-import Mathlib.AlgebraicGeometry.PrimeSpectrum.Noetherian
 import Mathlib.RingTheory.Jacobson.Ring
+import Mathlib.RingTheory.Spectrum.Prime.Noetherian
 import Mathlib.Topology.JacobsonSpace
 
 /-!
