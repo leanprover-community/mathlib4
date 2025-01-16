156b51f61b5bf611af9f62fb6f12ffa7b1d00403
Christopher Hoskin <christopher.hoskin@gmail.com>
2025-01-16 07:48:54 +0000

    shake

--- Mathlib/Analysis/Normed/Algebra/NumericalRange.lean
+++ Mathlib/Analysis/Normed/Algebra/NumericalRange.lean
@@ -3,8 +3,8 @@ Copyright (c) 2025 Christopher Hoskin. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Christopher Hoskin
 -/
-import Mathlib.Analysis.Normed.Algebra.Basic
 import Mathlib.Analysis.Normed.Module.Dual
+import Mathlib.Analysis.InnerProductSpace.Basic
 
 /-!
 # Numerical ranges
