a90b44616ca11777bf68a0934f06811d2111e60d
Violeta Hernández <vi.hdz.p@gmail.com>
2025-01-15 22:08:19 -0600

    Update Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -4274,6 +4274,7 @@ import Mathlib.Order.UpperLower.Hom
 import Mathlib.Order.UpperLower.LocallyFinite
 import Mathlib.Order.WellFounded
 import Mathlib.Order.WellFoundedSet
+import Mathlib.Order.WellQuasiOrder
 import Mathlib.Order.WithBot
 import Mathlib.Order.Zorn
 import Mathlib.Order.ZornAtoms
