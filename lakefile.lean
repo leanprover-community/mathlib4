ea5f56afeb1b5a3c3d893b783d15bed0c84c5ade
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-01-16 21:59:20 +0100

    chore: set Elab.async to false globally

--- lakefile.lean
+++ lakefile.lean
@@ -24,6 +24,7 @@ require "leanprover-community" / "plausible" @ git "v4.16.0-rc1"
 * as `moreServerArgs`, to set their default value in mathlib
   (as well as `Archive`, `Counterexamples` and `test`). -/
 abbrev mathlibOnlyLinters : Array LeanOption := #[
+  ⟨`Elab.async, false⟩, -- temporary: for the unusedVariable linter
   -- The `docPrime` linter is disabled: https://github.com/leanprover-community/mathlib4/issues/20560
   ⟨`linter.docPrime, false⟩,
   ⟨`linter.hashCommand, true⟩,
