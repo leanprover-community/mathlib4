e7721771e5dcfef13461f57a802c5da5921f2ff8
Violeta Hernández <vi.hdz.p@gmail.com>
2025-01-16 00:56:48 -0600

    like this?

--- Mathlib/Order/WellFounded.lean
+++ Mathlib/Order/WellFounded.lean
@@ -215,42 +215,45 @@ variable (f : α → β)
 
 section LT
 
-variable [LT β] [h : WellFoundedLT β]
+variable [LT β] (h : WellFounded ((· < ·) : β → β → Prop))
 
 /-- Given a function `f : α → β` where `β` carries a well-founded `<`, this is an element of `α`
 whose image under `f` is minimal in the sense of `Function.not_lt_argmin`. -/
 noncomputable def argmin [Nonempty α] : α :=
-  WellFounded.min (InvImage.wf f h.wf) Set.univ Set.univ_nonempty
+  WellFounded.min (InvImage.wf f h) Set.univ Set.univ_nonempty
 
-theorem not_lt_argmin [Nonempty α] (a : α) : ¬f a < f (argmin f) :=
-  WellFounded.not_lt_min (InvImage.wf f h.wf) _ _ (Set.mem_univ a)
+theorem not_lt_argmin [Nonempty α] (a : α) : ¬f a < f (argmin f h) :=
+  WellFounded.not_lt_min (InvImage.wf f h) _ _ (Set.mem_univ a)
 
 /-- Given a function `f : α → β` where `β` carries a well-founded `<`, and a non-empty subset `s`
 of `α`, this is an element of `s` whose image under `f` is minimal in the sense of
 `Function.not_lt_argminOn`. -/
 noncomputable def argminOn (s : Set α) (hs : s.Nonempty) : α :=
-  WellFounded.min (InvImage.wf f h.wf) s hs
+  WellFounded.min (InvImage.wf f h) s hs
 
 @[simp]
-theorem argminOn_mem (s : Set α) (hs : s.Nonempty) : argminOn f s hs ∈ s :=
+theorem argminOn_mem (s : Set α) (hs : s.Nonempty) : argminOn f h s hs ∈ s :=
   WellFounded.min_mem _ _ _
 
+-- Porting note (https://github.com/leanprover-community/mathlib4/issues/11119): @[simp] removed as it will never apply
 theorem not_lt_argminOn (s : Set α) {a : α} (ha : a ∈ s)
-    (hs : s.Nonempty := Set.nonempty_of_mem ha) : ¬f a < f (argminOn f s hs) :=
-  WellFounded.not_lt_min (InvImage.wf f h.wf) s hs ha
+    (hs : s.Nonempty := Set.nonempty_of_mem ha) : ¬f a < f (argminOn f h s hs) :=
+  WellFounded.not_lt_min (InvImage.wf f h) s hs ha
 
 end LT
 
 section LinearOrder
 
-variable [LinearOrder β] [WellFoundedLT β]
+variable [LinearOrder β] (h : WellFounded ((· < ·) : β → β → Prop))
 
-theorem argmin_le (a : α) [Nonempty α] : f (argmin f) ≤ f a :=
-  not_lt.mp <| not_lt_argmin f a
+-- Porting note (https://github.com/leanprover-community/mathlib4/issues/11119): @[simp] removed as it will never apply
+theorem argmin_le (a : α) [Nonempty α] : f (argmin f h) ≤ f a :=
+  not_lt.mp <| not_lt_argmin f h a
 
+-- Porting note (https://github.com/leanprover-community/mathlib4/issues/11119): @[simp] removed as it will never apply
 theorem argminOn_le (s : Set α) {a : α} (ha : a ∈ s) (hs : s.Nonempty := Set.nonempty_of_mem ha) :
-    f (argminOn f s hs) ≤ f a :=
-  not_lt.mp <| not_lt_argminOn f s ha hs
+    f (argminOn f h s hs) ≤ f a :=
+  not_lt.mp <| not_lt_argminOn f h s ha hs
 
 end LinearOrder
 
