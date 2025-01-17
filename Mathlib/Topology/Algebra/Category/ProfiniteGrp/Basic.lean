13f49d516441abcb288614a1e3d952f3a5de4e17
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2025-01-17 20:30:14 +0800

    remove resolved TODO

--- Mathlib/Topology/Algebra/Category/ProfiniteGrp/Basic.lean
+++ Mathlib/Topology/Algebra/Category/ProfiniteGrp/Basic.lean
@@ -30,8 +30,6 @@ disconnected.
 As discussion in `https://leanprover.zulipchat.com/#narrow/channel/287929-mathlib4/topic/Refactor.20
 Category.20of.20ProfiniteGrp.20and.20ContinuousMulEquiv/near/493290115`
 
-* Refactor the category of `ProfiniteGrp` into one-field structure as in `AlgebraCat`
-
 * Prove `(forget ProfiniteGrp.{u}).ReflectsIsomorphisms` using `profiniteGrpToProfinite`
 -/
 
