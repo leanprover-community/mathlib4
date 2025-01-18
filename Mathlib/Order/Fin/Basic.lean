6aa04f4f554975faff159636d1be0608471f2058
Bolton Bailey <bolton.bailey@gmail.com>
2025-01-18 12:35:43 -0800

    add imports of new file

--- Mathlib/Order/Fin/Basic.lean
+++ Mathlib/Order/Fin/Basic.lean
@@ -3,7 +3,7 @@ Copyright (c) 2017 Robert Y. Lewis. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Robert Y. Lewis, Keeley Hoek
 -/
-import Mathlib.Data.Fin.Basic
+import Mathlib.Data.Fin.Rev
 import Mathlib.Order.Hom.Set
 
 /-!
