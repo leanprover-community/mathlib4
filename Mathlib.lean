2a6fec5a7689cf074750d373f07487260797cc5d
Jack Valmadre <jack.valmadre@gmail.com>
2025-01-19 09:24:36 +1030

    C_c^∞ functions are dense in L^p for 1 ≤ p < ∞

--- Mathlib.lean
+++ Mathlib.lean
@@ -3889,6 +3889,7 @@ import Mathlib.MeasureTheory.Function.AEEqOfIntegral
 import Mathlib.MeasureTheory.Function.AEEqOfLIntegral
 import Mathlib.MeasureTheory.Function.AEMeasurableOrder
 import Mathlib.MeasureTheory.Function.AEMeasurableSequence
+import Mathlib.MeasureTheory.Function.CompactDense
 import Mathlib.MeasureTheory.Function.ConditionalExpectation.AEMeasurable
 import Mathlib.MeasureTheory.Function.ConditionalExpectation.Basic
 import Mathlib.MeasureTheory.Function.ConditionalExpectation.CondexpL1
