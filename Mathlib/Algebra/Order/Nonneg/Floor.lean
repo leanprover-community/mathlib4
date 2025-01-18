accc29f40cb975e8a87f0342afb72e67148c4c58
Kim Morrison <kim@tqft.net>
2025-01-18 16:26:43 +1100

    fix

--- Mathlib/Algebra/Order/Nonneg/Floor.lean
+++ Mathlib/Algebra/Order/Nonneg/Floor.lean
@@ -3,10 +3,8 @@ Copyright (c) 2021 Floris van Doorn. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Floris van Doorn
 -/
-import Mathlib.Algebra.Order.Ring.Unbundled.Nonneg
 import Mathlib.Algebra.Order.Floor.Defs
 import Mathlib.Algebra.Order.Nonneg.Ring
-import Mathlib.Algebra.Order.Floor
 import Mathlib.Algebra.Order.Nonneg.Basic
 import Mathlib.Algebra.Order.Nonneg.Lattice
 
