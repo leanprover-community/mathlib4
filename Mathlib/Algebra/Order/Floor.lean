d498dc25b9569974d4b46cdc12a01f27c1e450d0
Bolton Bailey <bolton.bailey@gmail.com>
2025-01-18 11:15:01 -0800

    remove lint suppression

--- Mathlib/Algebra/Order/Floor.lean
+++ Mathlib/Algebra/Order/Floor.lean
@@ -1487,5 +1487,3 @@ def evalIntCeil : PositivityExt where eval {u α} _zα _pα e := do
   | _, _, _ => throwError "failed to match on Int.ceil application"
 
 end Mathlib.Meta.Positivity
-
-set_option linter.style.longFile 1800
