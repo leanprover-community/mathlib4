73cba47f436db2f49d0b27e97654381ba4d0ef5c
Bolton Bailey <bolton.bailey@gmail.com>
2025-01-18 11:14:18 -0800

    split file

--- Mathlib/Algebra/ContinuedFractions/Computation/Basic.lean
+++ Mathlib/Algebra/ContinuedFractions/Computation/Basic.lean
@@ -3,7 +3,7 @@ Copyright (c) 2020 Kevin Kappelmann. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kevin Kappelmann
 -/
-import Mathlib.Algebra.Order.Floor
+import Mathlib.Algebra.Order.Round
 import Mathlib.Algebra.ContinuedFractions.Basic
 
 /-!
