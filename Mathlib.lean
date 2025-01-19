a58a89a28f740bc7878e82806f3ddf2f67bfd65a
Rémy Degenne <remydegenne@gmail.com>
2025-01-19 09:13:31 +0000

    feat: describe the interval of reals `t` where `exp (t * X)` is integrable (#20418)
    The set of reals `t` where `exp (t * X)` is integrable is an interval containing 0. Also prove integrability of other function for a parameter in that interval.
    
    Rename Probability/Moments to Probability/Moments/Basic
    
    
    
    Co-authored-by: RemyDegenne <Remydegenne@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -4332,7 +4332,8 @@ import Mathlib.Probability.Martingale.Convergence
 import Mathlib.Probability.Martingale.OptionalSampling
 import Mathlib.Probability.Martingale.OptionalStopping
 import Mathlib.Probability.Martingale.Upcrossing
-import Mathlib.Probability.Moments
+import Mathlib.Probability.Moments.Basic
+import Mathlib.Probability.Moments.IntegrableExpMul
 import Mathlib.Probability.Notation
 import Mathlib.Probability.ProbabilityMassFunction.Basic
 import Mathlib.Probability.ProbabilityMassFunction.Binomial
