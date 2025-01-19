165731857a079f3712426d4d1da2a5ac32c6be56
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2025-01-19 14:13:17 +0000

    feat: start building an API for vector fields on manifolds (#20639)
    Notably, define the pullback of vector fields, and show that the pullback of a differentiable vector field under a C^2 map is differentiable.
    
    More API to come in further PRs. Once all the API is there, it will be easy to define the Lie algebra of a Lie group using left-invariant vector fields, in #18396.

--- Mathlib.lean
+++ Mathlib.lean
@@ -3214,6 +3214,7 @@ import Mathlib.Geometry.Manifold.VectorBundle.MDifferentiable
 import Mathlib.Geometry.Manifold.VectorBundle.Pullback
 import Mathlib.Geometry.Manifold.VectorBundle.SmoothSection
 import Mathlib.Geometry.Manifold.VectorBundle.Tangent
+import Mathlib.Geometry.Manifold.VectorField
 import Mathlib.Geometry.Manifold.WhitneyEmbedding
 import Mathlib.Geometry.RingedSpace.Basic
 import Mathlib.Geometry.RingedSpace.LocallyRingedSpace
