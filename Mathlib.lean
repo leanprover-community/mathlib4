51f922375a0868802d56c5e420cf14fbd3f56798
Blizzard_inc <thrashy4inbox@gmail.com>
2025-01-20 13:28:06 +0100

    mk_all

--- Mathlib.lean
+++ Mathlib.lean
@@ -3013,6 +3013,7 @@ import Mathlib.Data.Sym.Sym2.Order
 import Mathlib.Data.Tree.Basic
 import Mathlib.Data.Tree.Get
 import Mathlib.Data.Tree.RBMap
+import Mathlib.Data.Tree.Traversable
 import Mathlib.Data.TwoPointing
 import Mathlib.Data.TypeMax
 import Mathlib.Data.TypeVec
