0616872f7238ad871d9619a9fda6cea2fc10ae1d
Jakob von Raumer <jakob@von-raumer.de>
2025-01-20 16:40:09 +0100

    shake

--- Mathlib/CategoryTheory/Filtered/Basic.lean
+++ Mathlib/CategoryTheory/Filtered/Basic.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Reid Barton, Kim Morrison
 -/
 import Mathlib.CategoryTheory.Limits.Shapes.FiniteLimits
-import Mathlib.CategoryTheory.IsConnected
 
 /-!
 # Filtered categories
