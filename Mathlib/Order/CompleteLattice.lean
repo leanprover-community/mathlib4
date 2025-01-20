6277c7866b41c235803ae46cb1c7b3c611b57243
artie2000 <artem.khovanov@gmail.com>
2025-01-20 17:49:02 +0000

    feat(Order/CompleteLattice): add missing simp lemmas

--- Mathlib/Order/CompleteLattice.lean
+++ Mathlib/Order/CompleteLattice.lean
@@ -113,6 +113,7 @@ theorem sSup_le_sSup_of_forall_exists_le (h : ∀ x ∈ s, ∃ y ∈ t, x ≤ y)
       hac.trans (hb hct)
 
 -- We will generalize this to conditionally complete lattices in `csSup_singleton`.
+@[simp]
 theorem sSup_singleton {a : α} : sSup {a} = a :=
   isLUB_singleton.sSup_eq
 
@@ -168,6 +169,7 @@ theorem sInf_le_sInf_of_forall_exists_le (h : ∀ x ∈ s, ∃ y ∈ t, y ≤ x)
   le_sInf fun x hx ↦ let ⟨_y, hyt, hyx⟩ := h x hx; sInf_le_of_le hyt hyx
 
 -- We will generalize this to conditionally complete lattices in `csInf_singleton`.
+@[simp]
 theorem sInf_singleton {a : α} : sInf {a} = a :=
   isGLB_singleton.sInf_eq
 
@@ -427,9 +429,11 @@ theorem sSup_diff_singleton_bot (s : Set α) : sSup (s \ {⊥}) = sSup s :=
 theorem sInf_diff_singleton_top (s : Set α) : sInf (s \ {⊤}) = sInf s :=
   @sSup_diff_singleton_bot αᵒᵈ _ s
 
+@[simp]
 theorem sSup_pair {a b : α} : sSup {a, b} = a ⊔ b :=
   (@isLUB_pair α _ a b).sSup_eq
 
+@[simp]
 theorem sInf_pair {a b : α} : sInf {a, b} = a ⊓ b :=
   (@isGLB_pair α _ a b).sInf_eq
 
