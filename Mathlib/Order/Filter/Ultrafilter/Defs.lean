b615fe3d5e199ae706d698b4dd5200733895a5bc
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2025-01-21 12:15:29 +0100

    Update Mathlib/Order/Filter/Ultrafilter/Defs.lean
    Co-authored-by: Kim Morrison <kim@tqft.net>

--- Mathlib/Order/Filter/Ultrafilter/Defs.lean
+++ Mathlib/Order/Filter/Ultrafilter/Defs.lean
@@ -18,6 +18,8 @@ In this file we define
 * `Ultrafilter.map`, `Ultrafilter.bind`, `Ultrafilter.comap` : operations on ultrafilters;
 -/
 
+assert_not_exists Set.Finite
+
 universe u v
 
 variable {α : Type u} {β : Type v} {γ : Type*}
