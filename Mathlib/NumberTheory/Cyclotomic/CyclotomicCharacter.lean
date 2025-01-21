df1c8f16525ec66b98b5b5e1bea89b4d6735a1d8
Thomas Browning <tb65536@uw.edu>
2025-01-20 16:53:08 -0800

    fix

--- Mathlib/NumberTheory/Cyclotomic/CyclotomicCharacter.lean
+++ Mathlib/NumberTheory/Cyclotomic/CyclotomicCharacter.lean
@@ -3,6 +3,7 @@ Copyright (c) 2023 Hanneke Wiersema. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kevin Buzzard, Hanneke Wiersema
 -/
+import Mathlib.Data.ZMod.QuotientRing
 import Mathlib.RingTheory.RootsOfUnity.PrimitiveRoots
 
 /-!
