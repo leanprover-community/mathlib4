272da688a7154e961a6ae3f3cde9a051e3ff5ee1
emilyriehl <eriehl@jhu.edu>
2025-01-21 15:16:42 -0500

    update Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -1640,6 +1640,7 @@ import Mathlib.CategoryTheory.Category.Bipointed
 import Mathlib.CategoryTheory.Category.Cat
 import Mathlib.CategoryTheory.Category.Cat.Adjunction
 import Mathlib.CategoryTheory.Category.Cat.AsSmall
+import Mathlib.CategoryTheory.Category.Cat.Colimit
 import Mathlib.CategoryTheory.Category.Cat.Limit
 import Mathlib.CategoryTheory.Category.Factorisation
 import Mathlib.CategoryTheory.Category.GaloisConnection
