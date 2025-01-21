481da8cf5a43e0b4967a99fc667448b5acecf4a6
Thomas Browning <tb65536@uw.edu>
2025-01-21 12:02:08 -0800

    implicit variables

--- Mathlib/GroupTheory/SpecificGroups/Dihedral.lean
+++ Mathlib/GroupTheory/SpecificGroups/Dihedral.lean
@@ -219,17 +219,16 @@ lemma not_commutative : ∀ {n : ℕ}, n ≠ 1 → n ≠ 2 →
       one_add_one_eq_two, ← ZMod.val_eq_zero, ZMod.val_two_eq_two_mod] at h'
     simpa using Nat.le_of_dvd Nat.zero_lt_two <| Nat.dvd_of_mod_eq_zero h'
 
-lemma commutative_iff {n : ℕ} :
-    Std.Commutative (fun x y : DihedralGroup n ↦ x * y) ↔ n = 1 ∨ n = 2 where
+lemma commutative_iff : Std.Commutative (fun x y : DihedralGroup n ↦ x * y) ↔ n = 1 ∨ n = 2 where
   mp := by contrapose!; rintro ⟨h1, h2⟩; exact not_commutative h1 h2
   mpr := by rintro (rfl | rfl) <;> exact ⟨by decide⟩
 
-lemma not_isCyclic {n : ℕ} (h1 : n ≠ 1) : ¬ IsCyclic (DihedralGroup n) := fun h => by
+lemma not_isCyclic (h1 : n ≠ 1) : ¬ IsCyclic (DihedralGroup n) := fun h => by
   by_cases h2 : n = 2
   · simpa [exponent, card, h2] using h.exponent_eq_card
   · exact not_commutative h1 h2 h.commutative
 
-lemma isCyclic_iff {n : ℕ} : IsCyclic (DihedralGroup n) ↔ n = 1 where
+lemma isCyclic_iff : IsCyclic (DihedralGroup n) ↔ n = 1 where
   mp := not_imp_not.mp not_isCyclic
   mpr h := h ▸ isCyclic_of_prime_card (p := 2) nat_card
 
