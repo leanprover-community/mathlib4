f38ffcf777898aa51c16d7bdaab6251f65192058
adomani <adomani@gmail.com>
2025-01-21 23:09:00 +0000

    rephrase

--- Shake/Main.lean
+++ Shake/Main.lean
@@ -457,7 +457,8 @@ def main (args : List String) : IO UInt32 := do
     pure (some (path.parent.get! / "scripts" / "noshake.json"))
   else pure none
 
-  -- Read the config file, `validCfgFile? = false` only if the config file is present and invalid
+  -- Read the config file
+  -- Set `validCfgFile?` to `false` only if the config file is present and invalid
   let (cfg, validCfgFile?) ← if let some file := cfgFile then
     try
       pure (← IO.ofExcept (Json.parse (← IO.FS.readFile file) >>= fromJson? (α := ShakeCfg)), true)
