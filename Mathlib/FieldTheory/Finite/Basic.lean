f335d383d5b5ecb29154b79622d72a8122ddfe3e
Thomas Browning <tb65536@uw.edu>
2025-01-21 13:42:54 -0800

    fix

--- Mathlib/FieldTheory/Finite/Basic.lean
+++ Mathlib/FieldTheory/Finite/Basic.lean
@@ -3,9 +3,10 @@ Copyright (c) 2018 Chris Hughes. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Chris Hughes, Joey van Langen, Casper Putz
 -/
+import Mathlib.Algebra.CharP.Reduced
+import Mathlib.Data.Nat.Prime.Int
 import Mathlib.FieldTheory.Separable
 import Mathlib.RingTheory.IntegralDomain
-import Mathlib.Algebra.CharP.Reduced
 import Mathlib.Tactic.ApplyFun
 
 /-!
