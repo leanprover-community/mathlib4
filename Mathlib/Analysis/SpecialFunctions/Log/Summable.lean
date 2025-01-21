ddd660e2d1415f9c1da0bc03507b7d91cf67cff0
Chris Birkbeck <c.birkbeck@uea.ac.uk>
2025-01-21 21:32:54 +0000

    rev updates

--- Mathlib/Analysis/SpecialFunctions/Log/Summable.lean
+++ Mathlib/Analysis/SpecialFunctions/Log/Summable.lean
@@ -14,7 +14,9 @@ We give conditions under which the logarithms of a summble sequence is summable.
 results about when the sums converge uniformly.
 
 TODO: Generalise the indexing set from ℕ to some arbitrary type, but this needs
-Summable.tendsto_atTop_zero to first be done.
+Summable.tendsto_atTop_zero to first be done. Also remove hff from
+`Complex.multipliable_one_add_of_summable` once we have vanishing/non-vanishing results for infinite
+products.
 
 -/
 
@@ -22,7 +24,7 @@ open Filter Function Complex Real
 
 open scoped Interval Topology BigOperators Nat Classical Complex
 
-lemma Complex.log_of_summable {f : ℕ → ℂ} (hf : Summable f) :
+lemma Complex.summable_log_one_add_of_summable  {f : ℕ → ℂ} (hf : Summable f) :
     Summable (fun n : ℕ => Complex.log (1 + f n)) := by
   have hff := Summable.const_smul ((3 : ℝ) / 2) (summable_norm_iff.mpr hf)
   have := Metric.tendsto_atTop.mp (Summable.tendsto_atTop_zero ((summable_norm_iff.mpr hf)))
@@ -32,23 +34,23 @@ lemma Complex.log_of_summable {f : ℕ → ℂ} (hf : Summable f) :
   obtain ⟨n, hn⟩ := this (1/2) (one_half_pos)
   exact Exists.intro n fun m hm ↦ norm_log_one_add_half_le_self (LT.lt.le (hn m hm))
 
-lemma Real.log_of_summable {f : ℕ → ℝ} (hf : Summable f) :
+lemma Real.summable_log_one_add_of_summable  {f : ℕ → ℝ} (hf : Summable f) :
      Summable (fun n : ℕ => log (1 + |f n|)) := by
   have : Summable (fun n ↦ Complex.ofRealCLM (log (1 + |f n|))) := by
-    convert Complex.log_of_summable (Complex.ofRealCLM.summable hf.norm) with x
+    convert Complex.summable_log_one_add_of_summable  (Complex.ofRealCLM.summable hf.norm) with x
     rw [ofRealCLM_apply, ofReal_log (by positivity)]
     simp only [ofReal_add, ofReal_one, norm_eq_abs, ofRealCLM_apply]
   convert Complex.reCLM.summable this
 
-lemma Complex.summable_nat_multipliable_one_add (f : ℕ → ℂ) (hf : Summable f)
+lemma Complex.multipliable_one_add_of_summable (f : ℕ → ℂ) (hf : Summable f)
     (hff : ∀ n : ℕ, 1 + f n ≠ 0) : Multipliable (fun n : ℕ => 1 + f n) := by
   refine Complex.multipliable_of_summable_log  (fun n => 1 + f n) (by simpa) ?_
-  simpa only [forall_const] using Complex.log_of_summable hf
+  simpa only [forall_const] using Complex.summable_log_one_add_of_summable hf
 
-lemma Real.summable_nat_multipliable_one_add (f : ℕ → ℝ) (hf : Summable f) :
+lemma Real.multipliable_one_add_of_summable (f : ℕ → ℝ) (hf : Summable f) :
     Multipliable (fun n : ℕ => 1 + |f n|) := by
   refine Real.multipliable_of_summable_log  (fun n => 1 + |f n|) (fun _ ↦ by positivity) ?_
-  simpa only [forall_const] using Real.log_of_summable hf
+  simpa only [forall_const] using Real.summable_log_one_add_of_summable  hf
 
 lemma Complex.tendstoUniformlyOn_tsum_nat_log_one_add {α : Type*} {f : ℕ → α → ℂ} (K : Set α)
     {u : ℕ → ℝ} (hu : Summable u) (h : ∀ᶠ n in atTop, ∀ x ∈ K, ‖f n x‖ ≤ u n) :
