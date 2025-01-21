7ab8ce541a5626350d50653215221565e6cae8af
pfaffelh <pfaffelh@gmail.com>
2025-01-21 17:34:33 +0100

    repair of antidiagonal

--- Mathlib/Algebra/Order/Antidiag/Prod.lean
+++ Mathlib/Algebra/Order/Antidiag/Prod.lean
@@ -163,8 +163,10 @@ theorem filter_fst_eq_antidiagonal (n m : A) [DecidablePred (· = m)] [Decidable
 
 theorem filter_snd_eq_antidiagonal (n m : A) [DecidablePred (· = m)] [Decidable (m ≤ n)] :
     filter (fun x : A × A ↦ x.snd = m) (antidiagonal n) = if m ≤ n then {(n - m, m)} else ∅ := by
+  have : (fun x : A × A ↦ (x.snd = m)) ∘ Prod.swap = fun x : A × A ↦ x.fst = m := by
+    ext; simp
   rw [← map_swap_antidiagonal, filter_map]
-  simp [filter_fst_eq_antidiagonal, apply_ite (Finset.map _)]
+  simp [this, filter_fst_eq_antidiagonal, apply_ite (Finset.map _)]
 
 end OrderedSub
 
