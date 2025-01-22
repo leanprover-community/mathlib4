7057975073d457351965d50ab0cd1c574e3c4cb4
Jon Eugster <eugster.jon@gmail.com>
2025-01-22 21:11:00 +0100

    fix import

--- Mathlib/CategoryTheory/Enriched/Limits/IsConicalTerminal.lean
+++ Mathlib/CategoryTheory/Enriched/Limits/IsConicalTerminal.lean
@@ -3,7 +3,7 @@ Copyright (c) 2025 Jon Eugster. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Dagur Asgeirsson, Jon Eugster, Emily Riehl
 -/
-import Mathlib.CategoryTheory.Enriched.Ordinary.Limits.IsConicalLimit
+import Mathlib.CategoryTheory.Enriched.Limits.IsConicalLimit
 
 /-!
 # Conical terminal objects in enriched ordinary categories
