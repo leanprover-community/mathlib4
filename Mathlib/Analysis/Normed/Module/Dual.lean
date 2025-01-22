4f0eae3e62d620c5095febdd2e0da833eaa620fe
Christopher Hoskin <mans0954@users.noreply.github.com>
2025-01-22 18:12:56 +0000

    Update Mathlib/Analysis/Normed/Module/Dual.lean
    Co-authored-by: Johan Commelin <johan@commelin.net>

--- Mathlib/Analysis/Normed/Module/Dual.lean
+++ Mathlib/Analysis/Normed/Module/Dual.lean
@@ -163,7 +163,7 @@ variable (𝕜 : Type*) [NontriviallyNormedField 𝕜]
 variable {E : Type*} [SeminormedAddCommGroup E] [NormedSpace 𝕜 E]
 
 lemma polarSubmodule_eq_polar (m : SubMulAction 𝕜 E) :
-    SetLike.coe (polarSubmodule 𝕜 m) = polar 𝕜 m := rfl
+    (polarSubmodule 𝕜 m : Set (Submodule 𝕜 (Dual 𝕜 E))) = polar 𝕜 m := rfl
 
 theorem mem_polar_iff {x' : Dual 𝕜 E} (s : Set E) : x' ∈ polar 𝕜 s ↔ ∀ z ∈ s, ‖x' z‖ ≤ 1 :=
   Iff.rfl
