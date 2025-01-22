f5634c12ba976b94be8d4294d742138e33052b08
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2025-01-22 19:28:35 +0100

    add docstring

--- Mathlib/Topology/Algebra/TopologicallyNilpotent.lean
+++ Mathlib/Topology/Algebra/TopologicallyNilpotent.lean
@@ -6,7 +6,31 @@ Authors: Antoine Chambert-Loir, María Inés de Frutos-Fernández
 import Mathlib.Topology.Algebra.LinearTopology
 import Mathlib.RingTheory.Ideal.Basic
 
-/-! # Topologicall nilpotent elements -/
+/-! # Topologically nilpotent elements
+
+Let `M` be a monoid with zero `M`, endowed with a topology.
+
+* `IsTopologicallyNilpotent a` says that `a : M` is *topologically nilpotent*,
+ie, its powers converge to zero.
+
+* `IsTopologicallyNilpotent.map`:
+The image of a topologically nilpotent element under a continuous morphism of
+monoids with zero endowed with a topology is topologically nilpotent.
+
+* `IsTopologicallyNilpotent.zero`: `0` is topologically nilpotent.
+
+Let `R` be a commutative ring with a linear topology.
+
+* `IsTopologicallyNilpotent.mul_left`: if `a : R` is topologically nilpotent,
+then `a*b` is topologically nilpotent.
+
+* `IsTopologicallyNilpotent.mul_right`: if `a : R` is topologically nilpotent,
+then `a * b` is topologically nilpotent.
+
+* `IsTopologicallyNilpotent.add`: if `a b : R` are topologically nilpotent,
+then `a + b` is topologically nilpotent.
+
+-/
 
 open Filter
 
@@ -14,26 +38,39 @@ open scoped Topology
 
 /-- An element is topologically nilpotent if its powers converge to `0`. -/
 def IsTopologicallyNilpotent
-    {R : Type*} [Semiring R] [TopologicalSpace R] (a : R) : Prop :=
+    {R : Type*} [MonoidWithZero R] [TopologicalSpace R] (a : R) : Prop :=
   Tendsto (fun n : ℕ => a ^ n) atTop (𝓝 0)
 
 namespace IsTopologicallyNilpotent
 
-variable {R S : Type*} [TopologicalSpace R] [TopologicalSpace S]
+section MonoidWithZero
 
-theorem map [Semiring R] [Semiring S]
-    {φ : R →+* S} (hφ : Continuous φ) {a : R} (ha : IsTopologicallyNilpotent a) :
+variable {R S : Type*} [TopologicalSpace R] [MonoidWithZero R]
+  [MonoidWithZero S] [TopologicalSpace S]
+
+/-- The image of a topologically nilpotent element under a continuous morphism
+  is topologically nilpotent -/
+theorem map {F : Type*} [FunLike F R S] [MonoidWithZeroHomClass F R S]
+    {φ : F} (hφ : Continuous φ) {a : R} (ha : IsTopologicallyNilpotent a) :
     IsTopologicallyNilpotent (φ a) := by
   unfold IsTopologicallyNilpotent at ha ⊢
   simp_rw [← map_pow]
   exact (map_zero φ ▸  hφ.tendsto 0).comp ha
 
-theorem zero [Semiring R] :
+/-- `0` is topologically nilpotent -/
+theorem zero :
     IsTopologicallyNilpotent (0 : R) := tendsto_atTop_of_eventually_const (i₀ := 1)
     (fun _ hi => by rw [zero_pow (Nat.ne_zero_iff_zero_lt.mpr hi)])
 
-variable [CommRing R] [IsLinearTopology R R]
+end MonoidWithZero
 
+-- TODO : Ring, under commutation property
+
+section CommRing
+
+variable {R : Type*} [TopologicalSpace R] [CommRing R] [IsLinearTopology R R]
+
+/-- If `a` is topologically nilpotent, then `a * b` is topologically nilpotent. -/
 theorem mul_right {a : R} (ha : IsTopologicallyNilpotent a) (b : R) :
     IsTopologicallyNilpotent (a * b) := by
   intro v hv
@@ -47,10 +84,12 @@ theorem mul_right {a : R} (ha : IsTopologicallyNilpotent a) (b : R) :
   rw [mul_pow]
   exact  I_subset (I.mul_mem_right _ (ha m hm))
 
+/-- If `b` is topologically nilpotent, then `a * b` is topologically nilpotent. -/
  theorem mul_left (a : R) {b : R} (hb : IsTopologicallyNilpotent b) :
     IsTopologicallyNilpotent (a * b) :=
   mul_comm a b ▸ mul_right hb a
 
+/-- If `a` and `b` are topologically nilpotent, then `a + b` is topologically nilpotent. -/
 theorem add {a b : R} (ha : IsTopologicallyNilpotent a) (hb : IsTopologicallyNilpotent b) :
     IsTopologicallyNilpotent (a + b) := by
   intro v hv
@@ -64,5 +103,7 @@ theorem add {a b : R} (ha : IsTopologicallyNilpotent a) (hb : IsTopologicallyNil
   exact ⟨na + nb, fun m hm ↦
     I_subset (I.add_pow_mem_of_pow_mem_of_le (ha na le_rfl) (hb nb le_rfl)
       (le_trans hm (Nat.le_add_right _ _)))⟩
+
+end CommRing
 
 end IsTopologicallyNilpotent
