3b54732407a2b5e4978c26737ae8dc0a5fe7a1a6
Whysoserioushah <yx3021@ic.ac.uk>
2025-01-22 18:34:38 +0000

    apply suggestions

--- Mathlib/Algebra/Azumaya/Defs.lean
+++ Mathlib/Algebra/Azumaya/Defs.lean
@@ -31,28 +31,26 @@ variable (R A : Type*) [CommSemiring R] [Semiring A] [Algebra R A]
 
 open TensorProduct MulOpposite
 
-noncomputable instance : Module (A ⊗[R] Aᵐᵒᵖ) A := TensorProduct.Algebra.module
-
-instance : IsScalarTower R (A ⊗[R] Aᵐᵒᵖ) A where
-  smul_assoc r aa' x := by
-    induction aa' using TensorProduct.induction_on with
-    | zero => simp
-    | tmul a a' => exact LinearMap.map_smul₂ _ _ _ _
-    | add aa' bb' h1 h2 => simp_all [add_smul]
+noncomputable abbrev instModuleTensorProduct :
+  Module (A ⊗[R] Aᵐᵒᵖ) A := TensorProduct.Algebra.module
 
 /-- The canonical map from `A ⊗[R] Aᵐᵒᵖ` to `Module.End R A` where
   `a ⊗ b` maps to `f : x ↦ a * x * b`-/
 noncomputable abbrev AlgHom.tensorMopToEnd : (A ⊗[R] Aᵐᵒᵖ) →ₐ[R] Module.End R A :=
-  Algebra.Module.toModuleEndAlgHom R A
+  letI := instModuleTensorProduct R A
+  letI : IsScalarTower R (A ⊗[R] Aᵐᵒᵖ) A := {
+    smul_assoc := fun r ab a ↦ by
+      change TensorProduct.Algebra.moduleAux _ _ = _ • TensorProduct.Algebra.moduleAux _ _
+      simp }
+  Algebra.lsmul R (A := A ⊗[R] Aᵐᵒᵖ) R A
 
 lemma AlgHom.tensorMopToEnd_apply (a : A) (b : Aᵐᵒᵖ) (x : A) :
     AlgHom.tensorMopToEnd R A (a ⊗ₜ b) x = a * x * b.unop := by
-  simp only [tensorMopToEnd, Algebra.Module.toModuleEndAlgHom, coe_mk, Module.toModuleEnd_apply,
-    DistribMulAction.toLinearMap_apply]
+  simp only [Algebra.lsmul_coe]
   change TensorProduct.Algebra.moduleAux _ _ = _
-  simp [TensorProduct.Algebra.moduleAux_apply, ← mul_assoc]
+  simp [TensorProduct.Algebra.moduleAux, ← mul_assoc]
 
 /-- An azumaya algebra is a finitely generated, projective and faithful R-algebra where
   `AlgHom.tensorMopToEnd` is an isomorphism. -/
-class IsAzumaya extends Module.Projective R A, FaithfulSMul R A, Module.Finite R A: Prop where
+class IsAzumaya extends Module.Projective R A, FaithfulSMul R A, Module.Finite R A : Prop where
     bij : Function.Bijective <| AlgHom.tensorMopToEnd R A
