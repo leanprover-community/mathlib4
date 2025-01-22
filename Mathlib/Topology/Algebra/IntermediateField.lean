418a01de3361084f8fd60890c75dda9c33fb8305
jjdishere <emailboxofjjd@163.com>
2025-01-22 21:52:57 +0800

    Update IntermediateField.lean

--- Mathlib/Topology/Algebra/IntermediateField.lean
+++ Mathlib/Topology/Algebra/IntermediateField.lean
@@ -3,14 +3,14 @@ Copyright (c) 2024 Jiedong Jiang. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jiedong Jiang
 -/
-import Mathlib.FieldTheory.Adjoin
+import Mathlib.FieldTheory.IntermediateField.Adjoin.Defs
 import Mathlib.Topology.Algebra.Field
 
 /-!
 # Topology on intermediate fields
 
-In this file we define the instances related topology on intermediate fields. The topology is
-already defined in earlier files as the subspace topology.
+In this file we define the instances related to topology and continuous actions on
+intermediate fields. The topology is already defined in earlier files as the subspace topology.
 -/
 
 variable {K L : Type*} [Field K] [Field L] [Algebra K L]
