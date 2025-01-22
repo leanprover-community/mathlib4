65e8fba8921321f9d6c9465ae7f3e8475e174a2d
Peter Nelson <71660771+apnelson1@users.noreply.github.com>
2025-01-22 15:01:21 -0500

    noncommutative

--- Mathlib/Data/Matrix/Mul.lean
+++ Mathlib/Data/Matrix/Mul.lean
@@ -645,8 +645,7 @@ theorem mul_apply_eq_vecMul [Fintype n] (A : Matrix m n α) (B : Matrix n o α) 
 theorem vecMul_eq_sum [Fintype m] (v : m → α) (M : Matrix m n α) : v ᵥ* M = ∑ i, v i • M i :=
   (Finset.sum_fn ..).symm
 
-/-- The noncommutative version of `Matrix.mulVec_eq_sum`.  -/
-theorem mulVec_eq_sum' [Fintype n] (v : n → α) (M : Matrix m n α) :
+theorem mulVec_eq_sum [Fintype n] (v : n → α) (M : Matrix m n α) :
     M *ᵥ v = ∑ i, MulOpposite.op (v i) • Mᵀ i := by
   ext i
   simp [show (M *ᵥ v) i = ∑ x, M i x * v x from rfl]
@@ -887,10 +886,6 @@ theorem mulVec_transpose [Fintype m] (A : Matrix m n α) (x : m → α) : Aᵀ *
 theorem vecMul_transpose [Fintype n] (A : Matrix m n α) (x : n → α) : x ᵥ* Aᵀ = A *ᵥ x := by
   ext
   apply dotProduct_comm
-
-/-- The commutative version of `Matrix.mulVec_eq_sum'` -/
-theorem mulVec_eq_sum [Fintype n] (v : n → α) (M : Matrix m n α) : M *ᵥ v = ∑ i, v i • Mᵀ i := by
-  rw [← vecMul_transpose, vecMul_eq_sum]
 
 theorem mulVec_vecMul [Fintype n] [Fintype o] (A : Matrix m n α) (B : Matrix o n α) (x : o → α) :
     A *ᵥ (x ᵥ* B) = (A * Bᵀ) *ᵥ x := by rw [← mulVec_mulVec, mulVec_transpose]
