4b1e14e784a41012ff4204bb53b9ca13366ef9ef
Artie Khovanov <artem.khovanov@gmail.com>
2025-01-22 23:40:02 +0000

    Update Mathlib/Algebra/Order/Field/Power.lean
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- Mathlib/Algebra/Order/Field/Power.lean
+++ Mathlib/Algebra/Order/Field/Power.lean
@@ -116,7 +116,7 @@ protected lemma Odd.zpow_nonneg_iff (hn : Odd n) : 0 ≤ a ^ n ↔ 0 ≤ a :=
 theorem Odd.zpow_nonpos_iff (hn : Odd n) : a ^ n ≤ 0 ↔ a ≤ 0 := by
   rw [le_iff_lt_or_eq, le_iff_lt_or_eq, hn.zpow_neg_iff, zpow_eq_zero_iff]
   rintro rfl
-  exact Int.not_even_iff_odd.2 hn Even.zero
+  exact Int.not_even_iff_odd.2 hn .zero
 
 lemma Odd.zpow_pos_iff (hn : Odd n) : 0 < a ^ n ↔ 0 < a := lt_iff_lt_of_le_iff_le hn.zpow_nonpos_iff
 
