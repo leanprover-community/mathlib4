4bf912822d26c6802f24fc950be3ea137c827b96
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2025-01-22 20:04:28 +0100

    delete properties of topologically nilpotent elements in linear topologies

--- Mathlib/Topology/Algebra/TopologicallyNilpotent.lean
+++ Mathlib/Topology/Algebra/TopologicallyNilpotent.lean
@@ -64,46 +64,4 @@ theorem zero :
 
 end MonoidWithZero
 
--- TODO : Ring, under commutation property
-
-section CommRing
-
-variable {R : Type*} [TopologicalSpace R] [CommRing R] [IsLinearTopology R R]
-
-/-- If `a` is topologically nilpotent, then `a * b` is topologically nilpotent. -/
-theorem mul_right {a : R} (ha : IsTopologicallyNilpotent a) (b : R) :
-    IsTopologicallyNilpotent (a * b) := by
-  intro v hv
-  rw [IsLinearTopology.hasBasis_ideal.mem_iff] at hv
-  rcases hv with ⟨I, I_mem_nhds, I_subset⟩
-  specialize ha I_mem_nhds
-  simp only [mem_map, mem_atTop_sets, ge_iff_le, Set.mem_preimage, SetLike.mem_coe] at ha ⊢
-  rcases ha with ⟨n, ha⟩
-  use n
-  intro m hm
-  rw [mul_pow]
-  exact  I_subset (I.mul_mem_right _ (ha m hm))
-
-/-- If `b` is topologically nilpotent, then `a * b` is topologically nilpotent. -/
- theorem mul_left (a : R) {b : R} (hb : IsTopologicallyNilpotent b) :
-    IsTopologicallyNilpotent (a * b) :=
-  mul_comm a b ▸ mul_right hb a
-
-/-- If `a` and `b` are topologically nilpotent, then `a + b` is topologically nilpotent. -/
-theorem add {a b : R} (ha : IsTopologicallyNilpotent a) (hb : IsTopologicallyNilpotent b) :
-    IsTopologicallyNilpotent (a + b) := by
-  intro v hv
-  rw [IsLinearTopology.hasBasis_ideal.mem_iff] at hv
-  rcases hv with ⟨I, I_mem_nhds, I_subset⟩
-  specialize ha I_mem_nhds
-  specialize hb I_mem_nhds
-  simp only [mem_map, mem_atTop_sets, ge_iff_le, Set.mem_preimage, SetLike.mem_coe] at ha hb ⊢
-  rcases ha with ⟨na, ha⟩
-  rcases hb with ⟨nb, hb⟩
-  exact ⟨na + nb, fun m hm ↦
-    I_subset (I.add_pow_mem_of_pow_mem_of_le (ha na le_rfl) (hb nb le_rfl)
-      (le_trans hm (Nat.le_add_right _ _)))⟩
-
-end CommRing
-
 end IsTopologicallyNilpotent
