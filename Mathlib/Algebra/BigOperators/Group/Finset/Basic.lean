c064bfb27f24624ecb34f861e8dc30483fce9554
Eric Wieser <wieser.eric@gmail.com>
2025-01-22 00:57:08 +0000

    tidy up

--- Mathlib/Algebra/BigOperators/Group/Finset/Basic.lean
+++ Mathlib/Algebra/BigOperators/Group/Finset/Basic.lean
@@ -1438,14 +1438,14 @@ section Image_Overlap
 
 variable {α β ι : Type*} [DecidableEq ι] [DecidableEq α]
 
-/-- If the images of `f` only overlap where `g (f i) = c` , then `g (f j) = c` whenever
+/-- If the images of `f` only overlap where `g (f i) = c`, then `g (f j) = c` whenever
 `g (f j) = g (f n)` for some `n ≠ j`.-/
 lemma filter_erase_of_pairwise {f : ι → α} {g : α → β}
     {n : ι} {I : Finset ι} (hn : n ∈ I) (c : β)
     (hf : (I.toSet).Pairwise fun i j => f i = f j → g (f i) = c) :
     ∀ j ∈ (filter (fun i => f i = f n) I).erase n, g (f j) = c := by
   intro x hx
-  simp only [mem_erase, ne_eq, mem_filter] at hx
+  simp only [mem_erase, mem_filter] at hx
   exact hf hx.2.1 hn hx.1 hx.2.2
 
 @[to_additive]
@@ -1453,9 +1453,8 @@ lemma prod_filter_of_pairwiseOne [CommMonoid β] {f : ι → α} {g : α → β}
     {n : ι} {I : Finset ι} (hn : n ∈ I)
     (hf : (I.toSet).Pairwise fun i j => f i = f j → g (f i) = 1) :
     ∏ j ∈ filter (fun j => f j = f n) I, g (f j) = g (f n) := by
-  rw [← mul_one (g (f n)), ← (prod_eq_one (filter_erase_of_pairwise hn 1 hf)),
-    ← (mul_prod_erase (filter (fun j => f j = f n) I) (fun i => g (f i))
-    <| mem_filter.mpr ⟨hn , by rfl⟩)]
+  rw [← mul_one (g (f n)), ← prod_eq_one (filter_erase_of_pairwise hn 1 hf),
+    ← mul_prod_erase (filter (f · = f n) I) (fun i => g (f i)) <| mem_filter.mpr ⟨hn, by rfl⟩]
 
 /-- A version of `Finset.prod_map` and `Finset.prod_image`, but we do nto assume that `f` is
 injective. Rather, we assume that the image of `f`  on `I` only overlaps where `g (f i) = 1`.
@@ -1463,7 +1462,7 @@ The conclusion is the same as in `prod_image`.-/
 @[to_additive (attr := simp)
 "A version of `Finset.sum_map` and `Finset.sum_image`, but we do not assume that `f` is
 injective. Rather, we assume that the image of `f`  on `I` only overlaps where `g (f i) = 1`.
-The conclusion is the same as in `prod_image`."]
+The conclusion is the same as in `sum_image`."]
 lemma prod_image_of_pairwiseOne [CommMonoid β] {f : ι → α} {g : α → β} {I : Finset ι}
     (hf : (I.toSet).Pairwise fun i j => f i = f j → g (f i) = 1) :
     ∏ s in I.image f, g s = ∏ i in I, g (f i) := by
@@ -1476,10 +1475,9 @@ conclusion is the same as in `prod_image`.-/
 @[to_additive (attr := simp)
 "A version of `Finset.sum_map` and `Finset.sum_image`, but we do not assume that `f` is
 injective. Rather, we assume that the images of `f` are disjoint on `I`, and `g ⊥ = 0`. The
-conclusion is the same as in `prod_image`."
+conclusion is the same as in `sum_image`."
 ]
-lemma prod_image_of_disjoint {α β : Type*} [CommMonoid β] [PartialOrder α]
-    [OrderBot α] [DecidableEq α] {f : ι → α} {g : α → β}
+lemma prod_image_of_disjoint [CommMonoid β] [PartialOrder α] [OrderBot α] {f : ι → α} {g : α → β}
     (hg_bot : g ⊥ = 1) {I : Finset ι} (hf_disj : (I : Set ι).PairwiseDisjoint f) :
     ∏ s in I.image f, g s = ∏ i in I, g (f i) := by
   refine prod_image_of_pairwiseOne <| hf_disj.imp fun i j (hdisj : Disjoint _ _) hfij => ?_
