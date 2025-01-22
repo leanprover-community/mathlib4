338b4da1fafc6e49fa34e45fec0af173df81f84c
Bolton Bailey <bolton.bailey@gmail.com>
2025-01-22 02:01:06 +0000

    chore(Data/Fin/Basic): split off `rev` lemmas (#20833)
    This PR splits out lemmas about the `Fin.rev` function from `Data.Fin.Basic`. This addresses an instance of a long file linter trigger.

--- Mathlib.lean
+++ Mathlib.lean
@@ -2515,6 +2515,7 @@ import Mathlib.Data.Fin.Basic
 import Mathlib.Data.Fin.Fin2
 import Mathlib.Data.Fin.FlagRange
 import Mathlib.Data.Fin.Parity
+import Mathlib.Data.Fin.Rev
 import Mathlib.Data.Fin.SuccPred
 import Mathlib.Data.Fin.Tuple.Basic
 import Mathlib.Data.Fin.Tuple.BubbleSortInduction
