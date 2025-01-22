1ac616f1572bc57e572d1a9569608b16aa0c8e46
Stefan Kebekus <kebekus@users.noreply.github.com>
2025-01-22 15:04:15 +0100

    Update following reviewer's suggestion

--- Mathlib/MeasureTheory/Integral/Periodic.lean
+++ Mathlib/MeasureTheory/Integral/Periodic.lean
@@ -241,32 +241,31 @@ over one period. -/
 theorem intervalIntegrable {t : ℝ} (h₁f : Function.Periodic f T) (hT : 0 < T)
   (h₂f : IntervalIntegrable f MeasureTheory.volume t (t + T)) (a₁ a₂ : ℝ) :
     IntervalIntegrable f MeasureTheory.volume a₁ a₂ := by
-  -- Replace [a₁, a₂] by [t - n₁ * T, t + n₂ * T], where n₁ and n₂ are integers
+  -- Replace [a₁, a₂] by [t - n₁ * T, t + n₂ * T], where n₁ and n₂ are natural numbers
   obtain ⟨n₁, hn₁⟩ := exists_nat_ge ((t - min a₁ a₂) / T)
   obtain ⟨n₂, hn₂⟩ := exists_nat_ge ((max a₁ a₂ - t) / T)
   have : Set.uIcc a₁ a₂ ⊆ Set.uIcc (t - n₁ * T) (t + n₂ * T) := by
     rw [Set.uIcc_subset_uIcc_iff_le]
     constructor
-    · calc min (t - ↑n₁ * T) (t + ↑n₂ * T)
-      _ ≤ (t - ↑n₁ * T) := by apply min_le_left
+    · calc min (t - n₁ * T) (t + n₂ * T)
+      _ ≤ (t - n₁ * T) := by apply min_le_left
       _ ≤ min a₁ a₂ := by linarith [(div_le_iff₀ hT).1 hn₁]
     · calc max a₁ a₂
       _ ≤ t + n₂ * T := by linarith [(div_le_iff₀ hT).1 hn₂]
-      _ ≤ max (t - ↑n₁ * T) (t + ↑n₂ * T) := by apply le_max_right
+      _ ≤ max (t - n₁ * T) (t + n₂ * T) := by apply le_max_right
   apply IntervalIntegrable.mono_set _ this
 
   -- Suffices to show integrability over shifted periods
   let a : ℕ → ℝ := fun n ↦ t + (n - n₁) * T
   rw [(by ring : t - n₁ * T = a 0), (by simp [a] : t + n₂ * T = a (n₁ + n₂))]
-  apply IntervalIntegrable.trans_iterate (a := fun n ↦ t + (n - n₁) * T)
+  apply IntervalIntegrable.trans_iterate
 
   -- Show integrability over a shifted period
   intro k hk
-  convert (IntervalIntegrable.comp_sub_right h₂f ((k - ↑n₁) * T)) using 1
+  convert (IntervalIntegrable.comp_sub_right h₂f ((k - n₁) * T)) using 1
   · funext x
-    rw [← h₁f.sub_int_mul_eq (k - n₁)]
-    simp
-  · rw [Nat.cast_add]
+    simpa using (h₁f.sub_int_mul_eq (k - n₁)).symm
+  · simp [a, Nat.cast_add]
     ring
 
 /-!
