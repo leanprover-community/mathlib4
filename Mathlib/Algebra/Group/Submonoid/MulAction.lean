d99998cc9764229d21ef2d45a00a93d47707ab6b
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2025-01-23 14:44:09 +0000

    remove hack and add old named instance which was being used

--- Mathlib/Algebra/Group/Submonoid/MulAction.lean
+++ Mathlib/Algebra/Group/Submonoid/MulAction.lean
@@ -13,6 +13,10 @@ import Mathlib.Algebra.Group.Action.Defs
 
 variable {M α : Type*} [Monoid M] {S : Type*} [SetLike S M] (s : S) [SubmonoidClass S M]
 
+@[to_additive]
+instance Submonoid.smul {M' : Type} [MulOneClass M'] [SMul M' α]
+    (S : Submonoid M') : SMul (↥S) α := inferInstance
+
 /-- The action by a submonoid is the action by the underlying monoid. -/
 @[to_additive
       "The additive action by an `AddSubmonoid` is the action by the underlying `AddMonoid`. "]
