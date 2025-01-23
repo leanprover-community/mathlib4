2eedf6d62cdecab8db296d76dc67f4634adcd53c
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2025-01-23 14:52:17 +0000

    re-add docstrings to make diff nicer

--- Mathlib/Algebra/Group/Submonoid/DistribMulAction.lean
+++ Mathlib/Algebra/Group/Submonoid/DistribMulAction.lean
@@ -21,6 +21,7 @@ instance [AddMonoid α] [DistribMulAction M α] : DistribMulAction s α where
   smul_zero r := smul_zero (r : M)
   smul_add r := smul_add (r : M)
 
+/-- The action by a submonoid is the action by the underlying monoid. -/
 instance distribMulAction [AddMonoid α] [DistribMulAction M α] (S : Submonoid M) :
     DistribMulAction S α :=
   inferInstance
@@ -29,6 +30,7 @@ instance [Monoid α] [MulDistribMulAction M α] : MulDistribMulAction s α where
   smul_mul r := smul_mul' (r : M)
   smul_one r := smul_one (r : M)
 
+/-- The action by a submonoid is the action by the underlying monoid. -/
 instance mulDistribMulAction [Monoid α] [MulDistribMulAction M α] (S : Submonoid M) :
     MulDistribMulAction S α :=
   inferInstance
