ad61f3ce8da8ea21e2d9e5a15e53b44e3e4c5217
yu-yama <yu-yama@users.noreply.github.com>
2025-01-23 18:44:13 +0000

    chore: import `Mathlib.GroupTheory.GroupExtension.Basic`

--- Mathlib.lean
+++ Mathlib.lean
@@ -3312,6 +3312,7 @@ import Mathlib.GroupTheory.GroupAction.Ring
 import Mathlib.GroupTheory.GroupAction.SubMulAction
 import Mathlib.GroupTheory.GroupAction.SubMulAction.Pointwise
 import Mathlib.GroupTheory.GroupAction.Support
+import Mathlib.GroupTheory.GroupExtension.Basic
 import Mathlib.GroupTheory.GroupExtension.Defs
 import Mathlib.GroupTheory.HNNExtension
 import Mathlib.GroupTheory.Index
