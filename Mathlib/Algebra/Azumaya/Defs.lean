42eebf7daa7f12acb7142082f8e6e58e71816b5b
Whysoserioushah <yx3021@ic.ac.uk>
2025-01-23 15:41:50 +0000

    apply eric suggestion

--- Mathlib/Algebra/Azumaya/Defs.lean
+++ Mathlib/Algebra/Azumaya/Defs.lean
@@ -37,7 +37,7 @@ noncomputable abbrev instModuleTensorProductMop :
 
 /-- The canonical map from `A ⊗[R] Aᵐᵒᵖ` to `Module.End R A` where
   `a ⊗ b` maps to `f : x ↦ a * x * b`. -/
-noncomputable abbrev AlgHom.tensorMopToEnd : (A ⊗[R] Aᵐᵒᵖ) →ₐ[R] Module.End R A :=
+noncomputable def AlgHom.mulLeftRight : (A ⊗[R] Aᵐᵒᵖ) →ₐ[R] Module.End R A :=
   letI : Module (A ⊗[R] Aᵐᵒᵖ) A := TensorProduct.Algebra.module
   letI : IsScalarTower R (A ⊗[R] Aᵐᵒᵖ) A := {
     smul_assoc := fun r ab a ↦ by
@@ -45,13 +45,14 @@ noncomputable abbrev AlgHom.tensorMopToEnd : (A ⊗[R] Aᵐᵒᵖ) →ₐ[R] Mod
       simp }
   Algebra.lsmul R (A := A ⊗[R] Aᵐᵒᵖ) R A
 
-lemma AlgHom.tensorMopToEnd_apply (a : A) (b : Aᵐᵒᵖ) (x : A) :
-    AlgHom.tensorMopToEnd R A (a ⊗ₜ b) x = a * x * b.unop := by
-  simp only [Algebra.lsmul_coe]
+@[simp]
+lemma AlgHom.mulLeftRight_apply (a : A) (b : Aᵐᵒᵖ) (x : A) :
+    AlgHom.mulLeftRight R A (a ⊗ₜ b) x = a * x * b.unop := by
+  simp only [AlgHom.mulLeftRight, Algebra.lsmul_coe]
   change TensorProduct.Algebra.moduleAux _ _ = _
   simp [TensorProduct.Algebra.moduleAux, ← mul_assoc]
 
 /-- An Azumaya algebra is a finitely generated, projective and faithful R-algebra where
-  `AlgHom.tensorMopToEnd` is an isomorphism. -/
+  `AlgHom.mulLeftRight` is an isomorphism. -/
 class IsAzumaya extends Module.Projective R A, FaithfulSMul R A, Module.Finite R A : Prop where
-    bij : Function.Bijective <| AlgHom.tensorMopToEnd R A
+    bij : Function.Bijective <| AlgHom.mulLeftRight R A
