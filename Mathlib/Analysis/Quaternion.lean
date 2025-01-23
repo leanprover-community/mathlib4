02b578ab07538275b6f609d2fb6baf1f8c6fcff4
Whysoserioushah <yx3021@ic.ac.uk>
2025-01-23 17:38:04 +0000

    fix proof

--- Mathlib/Analysis/Quaternion.lean
+++ Mathlib/Analysis/Quaternion.lean
@@ -225,8 +225,7 @@ theorem summable_coe {f : α → ℝ} : (Summable fun a => (f a : ℍ)) ↔ Summ
 theorem tsum_coe (f : α → ℝ) : (∑' a, (f a : ℍ)) = ↑(∑' a, f a) := by
   by_cases hf : Summable f
   · exact (hasSum_coe.mpr hf.hasSum).tsum_eq
-  · rw [tsum_eq_zero_of_not_summable hf, tsum_eq_zero_of_not_summable (summable_coe.not.mpr hf)]
-    rfl
+  · simp [tsum_eq_zero_of_not_summable hf, tsum_eq_zero_of_not_summable (summable_coe.not.mpr hf)]
 
 end infinite_sum
 
