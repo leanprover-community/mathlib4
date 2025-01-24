7dc591fdfbb78bae678699343d012fe541c2e21d
Yaël Dillies <yael.dillies@gmail.com>
2025-01-25 00:14:40 +0100

    compact
    Co-authored-by: Junyan Xu <junyanxu.math@gmail.com>

--- Mathlib/Algebra/MvPolynomial/Degrees.lean
+++ Mathlib/Algebra/MvPolynomial/Degrees.lean
@@ -545,17 +545,15 @@ variable (s t) in
 lemma degreesLE_add : degreesLE R σ (s + t) = degreesLE R σ s * degreesLE R σ t := by
   classical
   rw [le_antisymm_iff, Submodule.mul_le]
-  refine ⟨fun x hx ↦ ?_, fun x hx y hy ↦ degrees_mul_le.trans (add_le_add hx hy)⟩
-  rw [x.as_sum]
-  refine sum_mem fun i hi ↦ ?_
+  refine ⟨fun x hx ↦ x.as_sum ▸ sum_mem fun i hi ↦ ?_,
+    fun x hx y hy ↦ degrees_mul_le.trans (add_le_add hx hy)⟩
   replace hi : i.toMultiset ≤ s + t := (Finset.le_sup hi).trans hx
   let a := (i.toMultiset - t).toFinsupp
   let b := (i.toMultiset ⊓ t).toFinsupp
   have : a + b = i := Multiset.toFinsupp.symm.injective (by simp [a, b, Multiset.sub_add_inter])
   have ha : a.toMultiset ≤ s := by simpa [a, add_comm (a := t)] using hi
   have hb : b.toMultiset ≤ t := by simp [b, Multiset.inter_le_right]
-  have : monomial i (x.coeff i) = monomial a (x.coeff i) * monomial b 1 := by simp [this]
-  rw [this]
+  rw [show monomial i (x.coeff i) = monomial a (x.coeff i) * monomial b 1 by simp [this]]
   exact Submodule.mul_mem_mul ((degrees_monomial _ _).trans ha) ((degrees_monomial _ _).trans hb)
 
 @[simp] lemma degreesLE_zero : degreesLE R σ 0 = 1 := by
