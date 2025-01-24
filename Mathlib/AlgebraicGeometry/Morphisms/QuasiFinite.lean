c867f5160dfa99cd308fc06ebea8b229264794b0
Matej Penciak <matej.penciak@gmail.com>
2025-01-24 00:15:59 -0500

    wip

--- Mathlib/AlgebraicGeometry/Morphisms/QuasiFinite.lean
+++ Mathlib/AlgebraicGeometry/Morphisms/QuasiFinite.lean
@@ -14,10 +14,34 @@ open scoped AlgebraicGeometry
 
 namespace AlgebraicGeometry
 
-variable {X Y : Scheme.{u}} (f : X ⟶ Y)
+variable {X S : Scheme.{u}} (f : X ⟶ S)
 
-@[mk_iff]
-class QuasiFinite (f : X ⟶ Y) : Prop where
-  asdf : ∀ (y : Y),  IsFiniteLength (f.fiber y)
+-- #check Scheme.Hom
+
+-- example {X Y : Type} {x : X} {S : Set X} {h : x ∈ S} {T : Set Y} {f : S → T} : Prop := by
+--   have t := Set.preimage f {f x}
+--   sorry
+
+#check f.fiber
+
+-- example {x : X} : f.fiber (f.base x) := by
+
+#check IsOpen
+
+example {s : S} : S.fromSpecResidueField s := sorry
+
+example {x : X} : f.base x = (S.fromSpecResidueField (f.base x)).base
+
+#check Scheme.fromSpecResidueField
+
+def singletonx {x} : Set (f.fiber (f.base x)) := by
+  exact fun y => y = x
+
+
+-- def isquasifinite (x : X) : Prop := IsOpen (X := f.fiber (f.base x)) {x}
+
+-- @[mk_iff]
+-- class QuasiFinite (f : X ⟶ S) : Prop where
+--   asdf : ∀ (y : Y),  IsFiniteLength (f.fiber y)
 
 end AlgebraicGeometry
