cf6f2d34f2e0e560c4045bb76ca52c76485ac3ba
Christian Merten <christian@merten.dev>
2025-01-25 00:45:44 +0100

    update mathlib

--- Mathlib.lean
+++ Mathlib.lean
@@ -4552,6 +4552,7 @@ import Mathlib.RingTheory.Jacobson.Ring
 import Mathlib.RingTheory.Kaehler.Basic
 import Mathlib.RingTheory.Kaehler.CotangentComplex
 import Mathlib.RingTheory.Kaehler.JacobiZariski
+import Mathlib.RingTheory.Kaehler.Localization
 import Mathlib.RingTheory.Kaehler.Polynomial
 import Mathlib.RingTheory.Kaehler.TensorProduct
 import Mathlib.RingTheory.KrullDimension.Basic
