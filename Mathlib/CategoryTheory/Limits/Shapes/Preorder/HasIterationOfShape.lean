735af22c8051f1085f94ca4cc5fc1203cf7ac774
Joël Riou <37772949+joelriou@users.noreply.github.com>
2025-01-24 09:27:32 +0100

    Update Mathlib/CategoryTheory/Limits/Shapes/Preorder/HasIterationOfShape.lean
    Co-authored-by: Markus Himmel <markus@himmel-villmar.de>

--- Mathlib/CategoryTheory/Limits/Shapes/Preorder/HasIterationOfShape.lean
+++ Mathlib/CategoryTheory/Limits/Shapes/Preorder/HasIterationOfShape.lean
@@ -3,7 +3,6 @@ Copyright (c) 2024 Joël Riou. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joël Riou
 -/
-
 import Mathlib.CategoryTheory.Limits.FunctorCategory.Basic
 import Mathlib.CategoryTheory.Limits.Comma
 import Mathlib.Order.SuccPred.Limit
