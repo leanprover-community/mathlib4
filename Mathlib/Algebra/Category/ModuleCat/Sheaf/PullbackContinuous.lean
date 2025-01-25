b4207bb80f4208d1dc004410c3c38cc0f6424975
Joël Riou <37772949+joelriou@users.noreply.github.com>
2025-01-25 09:19:33 +0100

    Update Mathlib/Algebra/Category/ModuleCat/Sheaf/PullbackContinuous.lean

--- Mathlib/Algebra/Category/ModuleCat/Sheaf/PullbackContinuous.lean
+++ Mathlib/Algebra/Category/ModuleCat/Sheaf/PullbackContinuous.lean
@@ -3,7 +3,6 @@ Copyright (c) 2024 Joël Riou. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joël Riou
 -/
-
 import Mathlib.Algebra.Category.ModuleCat.Presheaf.Pullback
 import Mathlib.Algebra.Category.ModuleCat.Presheaf.Sheafification
 import Mathlib.Algebra.Category.ModuleCat.Sheaf.PushforwardContinuous
