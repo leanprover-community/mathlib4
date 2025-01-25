1671db5b2af86dd86df2e633d09d699c1a30dfb6
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2025-01-25 23:30:13 +0000

    namespace issue

--- Mathlib/Algebra/Group/Submonoid/Operations.lean
+++ Mathlib/Algebra/Group/Submonoid/Operations.lean
@@ -974,7 +974,7 @@ theorem Submonoid.equivMapOfInjective_coe_mulEquiv (e : M ≃* N) :
   ext
   rfl
 
-instance faithfulSMul {M' α : Type*} [MulOneClass M'] [SMul M' α] {S : Submonoid M'}
+instance Submonoid.faithfulSMul {M' α : Type*} [MulOneClass M'] [SMul M' α] {S : Submonoid M'}
     [FaithfulSMul M' α] : FaithfulSMul S α :=
   ⟨fun h => Subtype.ext <| eq_of_smul_eq_smul h⟩
 
