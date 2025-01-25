44acbfcc75cbe09f2912f1196294dbfb523bf65e
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2025-01-25 19:01:38 +0100

    update Top Nilpotent, before merge

--- Mathlib/Topology/Algebra/TopologicallyNilpotent.lean
+++ Mathlib/Topology/Algebra/TopologicallyNilpotent.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Antoine Chambert-Loir, María Inés de Frutos-Fernández
 -/
 import Mathlib.Topology.Algebra.LinearTopology
-import Mathlib.RingTheory.TwoSidedIdeal.Commute
+import Mathlib.RingTheory.Ideal.Basic
 
 /-! # Topologically nilpotent elements
 
@@ -30,6 +30,8 @@ then `a * b` is topologically nilpotent.
 * `IsTopologicallyNilpotent.add`: if `a b : R` are topologically nilpotent,
 then `a + b` is topologically nilpotent.
 
+These lemmas are actually deduced from their analogues for commuting elements of rings.
+
 -/
 
 open Filter
@@ -39,7 +41,7 @@ open scoped Topology
 /-- An element is topologically nilpotent if its powers converge to `0`. -/
 def IsTopologicallyNilpotent
     {R : Type*} [MonoidWithZero R] [TopologicalSpace R] (a : R) : Prop :=
-  Tendsto (fun n : ℕ => a ^ n) atTop (𝓝 0)
+  Tendsto (a ^ ·) atTop (𝓝 0)
 
 namespace IsTopologicallyNilpotent
 
@@ -55,12 +57,17 @@ theorem map {F : Type*} [FunLike F R S] [MonoidWithZeroHomClass F R S]
     IsTopologicallyNilpotent (φ a) := by
   unfold IsTopologicallyNilpotent at ha ⊢
   simp_rw [← map_pow]
-  exact (map_zero φ ▸  hφ.tendsto 0).comp ha
+  exact (map_zero φ ▸ hφ.tendsto 0).comp ha
 
 /-- `0` is topologically nilpotent -/
-theorem zero :
-    IsTopologicallyNilpotent (0 : R) := tendsto_atTop_of_eventually_const (i₀ := 1)
+theorem zero : IsTopologicallyNilpotent (0 : R) :=
+  tendsto_atTop_of_eventually_const (i₀ := 1)
     (fun _ hi => by rw [zero_pow (Nat.ne_zero_iff_zero_lt.mpr hi)])
+
+theorem exists_pow_mem_of_mem_nhds {a : R} (ha : IsTopologicallyNilpotent a)
+    {v : Set R} (hv : v ∈ 𝓝 0) :
+    ∃ n, a ^ n ∈ v :=
+  (ha.eventually_mem hv).exists
 
 end MonoidWithZero
 
@@ -73,52 +80,29 @@ variable {R : Type*} [TopologicalSpace R] [Ring R]
 theorem mul_right_of_commute [IsLinearTopology Rᵐᵒᵖ R]
     {a b : R} (ha : IsTopologicallyNilpotent a) (hab : Commute a b) :
     IsTopologicallyNilpotent (a * b) := by
-  intro v hv
-  rw [(IsLinearTopology.hasBasis_submodule Rᵐᵒᵖ).mem_iff] at hv
-  rcases hv with ⟨I, I_mem_nhds, I_subset⟩
-  specialize ha I_mem_nhds
-  simp only [mem_map, mem_atTop_sets, ge_iff_le, Set.mem_preimage, SetLike.mem_coe] at ha ⊢
-  rcases ha with ⟨n, ha⟩
-  use n
-  intro m hm
-  rw [hab.mul_pow]
-  exact I_subset (I.smul_mem _ (ha m hm))
+  simp_rw [IsTopologicallyNilpotent, hab.mul_pow]
+  exact IsLinearTopology.tendsto_mul_zero_of_left _ _ ha
 
 /-- If `a` and `b` commute and `b` is topologically nilpotent,
   then `a * b` is topologically nilpotent. -/
  theorem mul_left_of_commute [IsLinearTopology R R] {a b : R}
     (hb : IsTopologicallyNilpotent b) (hab : Commute a b) :
     IsTopologicallyNilpotent (a * b) := by
-  intro v hv
-  rw [IsLinearTopology.hasBasis_ideal.mem_iff] at hv
-  rcases hv with ⟨I, I_mem_nhds, I_subset⟩
-  specialize hb I_mem_nhds
-  simp only [mem_map, mem_atTop_sets, ge_iff_le, Set.mem_preimage, SetLike.mem_coe] at hb ⊢
-  rcases hb with ⟨n, hb⟩
-  use n
-  intro m hm
-  rw [hab.mul_pow]
-  exact I_subset (I.mul_mem_left _ (hb m hm))
+  simp_rw [IsTopologicallyNilpotent, hab.mul_pow]
+  exact IsLinearTopology.tendsto_mul_zero_of_right _ _ hb
 
 /-- If `a` and `b` are topologically nilpotent and commute,
   then `a + b` is topologically nilpotent. -/
-theorem add_of_commute [IsLinearTopology R R] [IsLinearTopology Rᵐᵒᵖ R]
-    {a b : R} (ha : IsTopologicallyNilpotent a) (hb : IsTopologicallyNilpotent b)
-    (h : Commute a b) :
-    IsTopologicallyNilpotent (a + b) := fun v ↦ by
-  rw [IsLinearTopology.hasBasis_twoSidedIdeal.mem_iff]
-  rintro ⟨I, I_mem_nhds, I_subset⟩
-  specialize ha I_mem_nhds
-  specialize hb I_mem_nhds
-  simp only [mem_map, mem_atTop_sets, ge_iff_le, Set.mem_preimage, SetLike.mem_coe] at ha hb ⊢
-  rcases ha with ⟨na, ha⟩
-  rcases hb with ⟨nb, hb⟩
-  use na + nb
-  intro m hm
-  apply I_subset
-  simp only [SetLike.mem_coe]
-  exact I.add_pow_mem_of_pow_mem_of_le (ha na le_rfl) (hb nb le_rfl)
-      (le_trans hm (Nat.le_add_right _ _)) h
+theorem add_of_commute [IsLinearTopology R R] {a b : R}
+    (ha : IsTopologicallyNilpotent a) (hb : IsTopologicallyNilpotent b) (h : Commute a b) :
+    IsTopologicallyNilpotent (a + b) := by
+  simp only [IsTopologicallyNilpotent, atTop_basis.tendsto_iff IsLinearTopology.hasBasis_ideal,
+    true_and]
+  intro I I_mem_nhds
+  obtain ⟨na, ha⟩ := ha.exists_pow_mem_of_mem_nhds I_mem_nhds
+  obtain ⟨nb, hb⟩ := hb.exists_pow_mem_of_mem_nhds I_mem_nhds
+  exact ⟨na + nb, fun m hm ↦
+    I.add_pow_mem_of_pow_mem_of_le_of_commute ha hb (le_trans hm (Nat.le_add_right _ _)) h⟩
 
 end Ring
 
@@ -129,17 +113,17 @@ variable {R : Type*} [TopologicalSpace R] [CommRing R] [IsLinearTopology R R]
 /-- If `a` is topologically nilpotent, then `a * b` is topologically nilpotent. -/
 theorem mul_right {a : R} (ha : IsTopologicallyNilpotent a) (b : R) :
     IsTopologicallyNilpotent (a * b) :=
-  ha.mul_right_of_commute (Commute.all a b)
+  ha.mul_right_of_commute (Commute.all ..)
 
 /-- If `b` is topologically nilpotent, then `a * b` is topologically nilpotent. -/
  theorem mul_left (a : R) {b : R} (hb : IsTopologicallyNilpotent b) :
     IsTopologicallyNilpotent (a * b) :=
-  hb.mul_left_of_commute (Commute.all a b)
+  hb.mul_left_of_commute (Commute.all ..)
 
 /-- If `a` and `b` are topologically nilpotent, then `a + b` is topologically nilpotent. -/
 theorem add {a b : R} (ha : IsTopologicallyNilpotent a) (hb : IsTopologicallyNilpotent b) :
     IsTopologicallyNilpotent (a + b) :=
-  ha.add_of_commute hb (Commute.all a b)
+  ha.add_of_commute hb (Commute.all ..)
 
 end CommRing
 
