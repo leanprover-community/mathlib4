cfc06ee211beaf95676df27c2221ded13d1443ad
sven-manthe <sven.manthe@uni-bonn.de>
2025-01-25 19:35:07 +0100

    minor

--- Mathlib/Order/CompleteLattice/SetLike.lean
+++ Mathlib/Order/CompleteLattice/SetLike.lean
@@ -19,21 +19,21 @@ variable {X : Type*} {L : Sublattice (Set X)}
 
 variable {S T : L} {x : X}
 
-@[simp] theorem mem_inf : x ∈ S ⊓ T ↔ x ∈ S ∧ x ∈ T := by simp [← mem_subtype]
-@[simp] theorem mem_sup : x ∈ S ⊔ T ↔ x ∈ S ∨ x ∈ T := by simp [← mem_subtype]
+@[ext] theorem ext (h : ∀ x, x ∈ S ↔ x ∈ T) : S = T := SetLike.coe_injective <| Set.ext h
 
-@[simp] lemma mem_coe : x ∈ T.val ↔ x ∈ T := Iff.rfl
-@[simp] lemma mem_mk (U : Set X) (h : U ∈ L) : x ∈ (⟨U, h⟩ : L) ↔ x ∈ U := Iff.rfl
+theorem mem_subtype : x ∈ L.subtype T ↔ x ∈ T := Iff.rfl
+
+@[simp] theorem setLike_mem_inf : x ∈ S ⊓ T ↔ x ∈ S ∧ x ∈ T := by simp [← mem_subtype]
+@[simp] theorem setLike_mem_sup : x ∈ S ⊔ T ↔ x ∈ S ∨ x ∈ T := by simp [← mem_subtype]
+
+@[simp] lemma setLike_mem_coe : x ∈ T.val ↔ x ∈ T := Iff.rfl
+@[simp] lemma setLike_mem_mk (U : Set X) (h : U ∈ L) : x ∈ (⟨U, h⟩ : L) ↔ x ∈ U := Iff.rfl
 
 namespace CompleteSublattice
 
 variable {X : Type*} {L : CompleteSublattice (Set X)}
 
 variable {S T : L} {𝒮 : Set L} {I : Sort*} {f : I → L} {x : X}
-
-@[ext] theorem ext (h : ∀ x, x ∈ S ↔ x ∈ T) : S = T := SetLike.coe_injective <| Set.ext h
-
-@[local simp] theorem mem_subtype : x ∈ L.subtype T ↔ x ∈ T := Iff.rfl
 
 @[simp] theorem mem_sInf : x ∈ sInf 𝒮 ↔ ∀ T ∈ 𝒮, x ∈ T := by simp [← mem_subtype]
 @[simp] theorem mem_iInf : x ∈ ⨅ i : I, f i ↔ ∀ i : I, x ∈ f i := by simp [← mem_subtype]
