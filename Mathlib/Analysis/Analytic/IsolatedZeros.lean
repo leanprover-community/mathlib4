24e055ffab661c2c82b4fe71b191fe8ae02def9b
Stefan Kebekus <kebekus@users.noreply.github.com>
2025-01-26 07:04:56 +0000

    feat: mark AnalyticAt / MeromorphicAt for use with fun_prop (#21057)
    Mark theorems pertaining to `AnalyticAt` / `MeromorphicAt` for use with `fun_prop`. As discussed yesterday in the FLUG meeting with @TwoFX and others, I duplicated a several theorems because tactics such as `fun_prop` and `rw` cannot see that `f + g` and `fun x => f x + g x` are the same thing. I made the formulations a little more consistent, in that there is now always one theorem `AnalyticAt.add` that speaks about `f + g` and one version `AnalyticAt.add'` that speaks about `fun x => f x + g x`.
    
    This new functionality is used in [Project VD](https://github.com/kebekus/ProjectVD), which aims to formalize Value Distribution Theory for meromorphic functions on the complex plane.

--- Mathlib/Analysis/Analytic/IsolatedZeros.lean
+++ Mathlib/Analysis/Analytic/IsolatedZeros.lean
@@ -241,6 +241,44 @@ lemma apply_eq_zero_of_order_toNat_ne_zero (hf : AnalyticAt 𝕜 f z₀) :
   simp [hf.order_eq_zero_iff]
   tauto
 
+/- Helper lemma for `AnalyticAt.order_mul` -/
+lemma order_mul_of_order_eq_top {f g : 𝕜 → 𝕜} (hf : AnalyticAt 𝕜 f z₀)
+    (hg : AnalyticAt 𝕜 g z₀) (h'f : hf.order = ⊤) :
+    (hf.mul hg).order = ⊤ := by
+  rw [AnalyticAt.order_eq_top_iff, eventually_nhds_iff] at *
+  obtain ⟨t, h₁t, h₂t, h₃t⟩ := h'f
+  exact ⟨t, fun y hy ↦ (by simp [h₁t y hy]), h₂t, h₃t⟩
+
+/-- The order is additive when multiplying analytic functions. -/
+theorem order_mul {f g : 𝕜 → 𝕜} (hf : AnalyticAt 𝕜 f z₀) (hg : AnalyticAt 𝕜 g z₀) :
+    (hf.mul hg).order = hf.order + hg.order := by
+  -- Trivial cases: one of the functions vanishes around z₀
+  by_cases h₂f : hf.order = ⊤
+  · simp [hf.order_mul_of_order_eq_top hg h₂f, h₂f]
+  by_cases h₂g : hg.order = ⊤
+  · simp [mul_comm f g, hg.order_mul_of_order_eq_top hf h₂g, h₂g]
+  -- Non-trivial case: both functions do not vanish around z₀
+  obtain ⟨g₁, h₁g₁, h₂g₁, h₃g₁⟩ := hf.order_neq_top_iff.1 h₂f
+  obtain ⟨g₂, h₁g₂, h₂g₂, h₃g₂⟩ := hg.order_neq_top_iff.1 h₂g
+  rw [← ENat.coe_toNat h₂f, ← ENat.coe_toNat h₂g, ← ENat.coe_add, (hf.mul hg).order_eq_nat_iff]
+  use g₁ * g₂, by exact h₁g₁.mul h₁g₂
+  constructor
+  · simp
+    tauto
+  · obtain ⟨t, h₁t, h₂t, h₃t⟩ := eventually_nhds_iff.1 h₃g₁
+    obtain ⟨s, h₁s, h₂s, h₃s⟩ := eventually_nhds_iff.1 h₃g₂
+    exact eventually_nhds_iff.2
+      ⟨t ∩ s, fun y hy ↦ (by simp [h₁t y hy.1, h₁s y hy.2]; ring), h₂t.inter h₂s, h₃t, h₃s⟩
+
+/-- The order multiplies by `n` when taking an analytic function to its `n`th power. -/
+theorem order_pow {f : 𝕜 → 𝕜} (hf : AnalyticAt 𝕜 f z₀) {n : ℕ} :
+    (hf.pow n).order = n • hf.order := by
+  induction n
+  case zero =>
+    simp [AnalyticAt.order_eq_zero_iff]
+  case succ n hn =>
+    simp [add_mul, pow_add, (hf.pow n).order_mul hf, hn]
+
 end AnalyticAt
 
 namespace AnalyticOnNhd
