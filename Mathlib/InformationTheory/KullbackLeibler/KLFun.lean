444359cd09096fd52535966d77c91a965bff3fe2
Remy Degenne <remydegenne@gmail.com>
2025-01-26 16:33:34 +0100

    imports

--- Mathlib/InformationTheory/KullbackLeibler/KLFun.lean
+++ Mathlib/InformationTheory/KullbackLeibler/KLFun.lean
@@ -3,9 +3,7 @@ Copyright (c) 2025 Rémy Degenne. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Rémy Degenne, Lorenzo Luccioli
 -/
-import Mathlib.Analysis.Convex.Integral
 import Mathlib.Analysis.SpecialFunctions.Log.NegMulLog
-import Mathlib.Analysis.Convex.Deriv
 import Mathlib.MeasureTheory.Measure.LogLikelihoodRatio
 
 /-!
