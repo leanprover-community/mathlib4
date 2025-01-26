9a6e23ba386a24a1c37064fc395c03a9adaa2f24
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-01-26 18:40:45 +0100

    feat(AlgebraicTopology/SimplicialSet): degenerate simplices

--- Mathlib.lean
+++ Mathlib.lean
@@ -1118,6 +1118,7 @@ import Mathlib.AlgebraicTopology.SimplicialObject.Split
 import Mathlib.AlgebraicTopology.SimplicialSet.Basic
 import Mathlib.AlgebraicTopology.SimplicialSet.Boundary
 import Mathlib.AlgebraicTopology.SimplicialSet.Coskeletal
+import Mathlib.AlgebraicTopology.SimplicialSet.Degenerate
 import Mathlib.AlgebraicTopology.SimplicialSet.HomotopyCat
 import Mathlib.AlgebraicTopology.SimplicialSet.Horn
 import Mathlib.AlgebraicTopology.SimplicialSet.KanComplex
