aa76e4d97cda9a3d1467dd2b5a4a3878a0d63921
Joseph Myers <jsm@polyomino.org.uk>
2025-01-26 17:15:52 +0000

    Minimize imports

--- Mathlib/Topology/MetricSpace/Congruence.lean
+++ Mathlib/Topology/MetricSpace/Congruence.lean
@@ -3,7 +3,7 @@ Copyright (c) 2024 Jovan Gerbscheid. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jovan Gerbscheid, Newell Jensen
 -/
-import Mathlib.Topology.MetricSpace.Isometry
+import Mathlib.Topology.MetricSpace.Pseudo.Defs
 
 /-!
 # Congruences
