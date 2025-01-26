d63f727f235ad0176574cb51f819e31c377ad01b
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-01-26 21:22:58 +0100

    alphabetic order

--- Mathlib/CategoryTheory/Presentable/Basic.lean
+++ Mathlib/CategoryTheory/Presentable/Basic.lean
@@ -3,8 +3,8 @@ Copyright (c) 2024 Joël Riou. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joël Riou
 -/
-import Mathlib.CategoryTheory.Limits.Preserves.Ulift
 import Mathlib.CategoryTheory.Adjunction.Limits
+import Mathlib.CategoryTheory.Limits.Preserves.Ulift
 import Mathlib.CategoryTheory.Presentable.IsCardinalFiltered
 import Mathlib.SetTheory.Cardinal.HasCardinalLT
 
