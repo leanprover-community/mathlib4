fb522c47c8f786a3c87a6b82dfe5615f184acab4
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-01-26 16:44:22 +0100

    fixing imports

--- Mathlib/CategoryTheory/Subpresheaf/Equalizer.lean
+++ Mathlib/CategoryTheory/Subpresheaf/Equalizer.lean
@@ -3,7 +3,7 @@ Copyright (c) 2024 Joël Riou. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joël Riou
 -/
-import Mathlib.CategoryTheory.Subpresheaf.Image
+import Mathlib.CategoryTheory.Subpresheaf.Basic
 
 /-!
 # The equalizer of two morphisms of presheaves, as a subpresheaf
