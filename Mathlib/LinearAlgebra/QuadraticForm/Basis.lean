05e83f64fb501bfc76bd9db9564e7be61b66ecb6
Christopher Hoskin <mans0954@users.noreply.github.com>
2025-01-27 17:50:27 +0000

    Update Mathlib/LinearAlgebra/QuadraticForm/Basis.lean
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- Mathlib/LinearAlgebra/QuadraticForm/Basis.lean
+++ Mathlib/LinearAlgebra/QuadraticForm/Basis.lean
@@ -165,7 +165,7 @@ theorem toBilin_toQuadraticMap (B : BilinMap R M N) (bm : Basis ι R M) (x y : M
     B.toQuadraticMap.toBilin bm x y =
       (∑ i ∈ s,
         ((bm.repr x) i) •((bm.repr y) i) • B (bm i) (bm i)) +
-      ∑ p ∈ Finset.filter (fun p ↦ p.1 < p.2) s.offDiag,
+      ∑ p ∈ s.offDiag with p.1 < p.2,
         ((bm.repr x) p.1) • ((bm.repr y) p.2) • (B + B.flip) (bm p.1) (bm p.2) := by
   simp_rw [toBilin, polar_toQuadraticMap, BilinMap.toQuadraticMap_apply]
   let s := (bm.repr x).support ∪ (bm.repr y).support
