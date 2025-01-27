ef38dc94a5cd04fca5836969fcccf07a54dae0d3
101damnations <101damnations@github.com>
2025-01-27 08:04:38 +0000

    move things

--- Mathlib/RepresentationTheory/Homological/GroupCohomology/LowDegree.lean
+++ Mathlib/RepresentationTheory/Homological/GroupCohomology/LowDegree.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Amelia Livingston, Joël Riou
 -/
 import Mathlib.Algebra.Homology.ShortComplex.ModuleCat
-import Mathlib.RepresentationTheory.GroupCohomology.Basic
+import Mathlib.RepresentationTheory.Homological.GroupCohomology.Basic
 import Mathlib.RepresentationTheory.Invariants
 
 /-!
