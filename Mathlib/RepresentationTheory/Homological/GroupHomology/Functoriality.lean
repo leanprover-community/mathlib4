c6e810168f23b4bb9f6bae6663720076cd26a54c
101damnations <101damnations@github.com>
2025-01-27 22:23:09 +0000

    change import

--- Mathlib/RepresentationTheory/Homological/GroupHomology/Functoriality.lean
+++ Mathlib/RepresentationTheory/Homological/GroupHomology/Functoriality.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Amelia Livingston
 -/
 import Mathlib.Algebra.Homology.HomologicalComplexAbelian
-import Mathlib.RepresentationTheory.Homological.GroupHomology.Basic
+import Mathlib.RepresentationTheory.Homological.GroupHomology.LowDegree
 
 /-!
 # Functoriality of group homology
