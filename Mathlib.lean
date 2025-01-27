df516754fa9f83fbca6a29cbdf09eaf25d2a258b
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-01-27 08:57:53 +0100

    feat(AlgebraicTopology): fibrant objects

--- Mathlib.lean
+++ Mathlib.lean
@@ -1105,6 +1105,7 @@ import Mathlib.AlgebraicTopology.FundamentalGroupoid.Product
 import Mathlib.AlgebraicTopology.FundamentalGroupoid.SimplyConnected
 import Mathlib.AlgebraicTopology.ModelCategory.Basic
 import Mathlib.AlgebraicTopology.ModelCategory.CategoryWithCofibrations
+import Mathlib.AlgebraicTopology.ModelCategory.IsCofibrant
 import Mathlib.AlgebraicTopology.MooreComplex
 import Mathlib.AlgebraicTopology.Quasicategory.Basic
 import Mathlib.AlgebraicTopology.Quasicategory.Nerve
