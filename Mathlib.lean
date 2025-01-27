ced2ced7be6d3b0981360e2e16539cf5167b7401
101damnations <101damnations@github.com>
2025-01-27 09:28:11 +0000

    add to Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -4383,6 +4383,7 @@ import Mathlib.RepresentationTheory.Homological.GroupCohomology.Basic
 import Mathlib.RepresentationTheory.Homological.GroupCohomology.Hilbert90
 import Mathlib.RepresentationTheory.Homological.GroupCohomology.LowDegree
 import Mathlib.RepresentationTheory.Homological.GroupHomology.Basic
+import Mathlib.RepresentationTheory.Homological.GroupHomology.LowDegree
 import Mathlib.RepresentationTheory.Homological.Resolution
 import Mathlib.RepresentationTheory.Invariants
 import Mathlib.RepresentationTheory.Maschke
