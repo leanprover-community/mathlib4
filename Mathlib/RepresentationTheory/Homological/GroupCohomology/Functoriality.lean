68aaeed68cd577bdf6dcac6a9e3ebae862d6f09f
101damnations <101damnations@github.com>
2025-01-27 20:30:45 +0000

    remove import

--- Mathlib/RepresentationTheory/Homological/GroupCohomology/Functoriality.lean
+++ Mathlib/RepresentationTheory/Homological/GroupCohomology/Functoriality.lean
@@ -3,7 +3,6 @@ Copyright (c) 2023 Amelia Livingston. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Amelia Livingston
 -/
-import Mathlib.RepresentationTheory.Homological.GroupCohomology.Basic
 import Mathlib.RepresentationTheory.Homological.GroupCohomology.LowDegree
 
 /-!
