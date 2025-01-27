b81670f7114e75e2fea377270a81586277bc4cb2
Anne C.A. Baanen <vierkantor@vierkantor.com>
2025-01-27 14:14:58 +0100

    Use type ascriptions to fix `simpNF` errors (sadly we can't yet remove the `erw`s...)

--- Mathlib/Algebra/Category/ModuleCat/Differentials/Presheaf.lean
+++ Mathlib/Algebra/Category/ModuleCat/Differentials/Presheaf.lean
@@ -192,7 +192,8 @@ attribute [simp] relativeDifferentials'_obj
 lemma relativeDifferentials'_map_d {X Y : Dᵒᵖ} (f : X ⟶ Y) (x : R.obj X) :
     DFunLike.coe (α := CommRingCat.KaehlerDifferential (φ'.app X))
       (β := fun _ ↦ CommRingCat.KaehlerDifferential (φ'.app Y))
-      ((relativeDifferentials' φ').map f).hom (CommRingCat.KaehlerDifferential.d x) =
+      (ModuleCat.Hom.hom (R := ↑(R.obj X)) ((relativeDifferentials' φ').map f))
+        (CommRingCat.KaehlerDifferential.d x) =
         CommRingCat.KaehlerDifferential.d (R.map f x) :=
   CommRingCat.KaehlerDifferential.map_d (φ'.naturality f) _
 
--- Mathlib/Algebra/Category/ModuleCat/Presheaf/Monoidal.lean
+++ Mathlib/Algebra/Category/ModuleCat/Presheaf/Monoidal.lean
@@ -66,7 +66,8 @@ variable {M₁ M₂ M₃ M₄}
 lemma tensorObj_map_tmul {X Y : Cᵒᵖ} (f : X ⟶ Y) (m₁ : M₁.obj X) (m₂ : M₂.obj X) :
     DFunLike.coe (α := (M₁.obj X ⊗ M₂.obj X :))
       (β := fun _ ↦ (ModuleCat.restrictScalars (R.map f).hom).obj (M₁.obj Y ⊗ M₂.obj Y))
-      ((tensorObj M₁ M₂).map f).hom (m₁ ⊗ₜ[R.obj X] m₂) = M₁.map f m₁ ⊗ₜ[R.obj Y] M₂.map f m₂ := rfl
+      (ModuleCat.Hom.hom (R := ↑(R.obj X)) ((tensorObj M₁ M₂).map f)) (m₁ ⊗ₜ[R.obj X] m₂) =
+    M₁.map f m₁ ⊗ₜ[R.obj Y] M₂.map f m₂ := rfl
 
 /-- The tensor product of two morphisms of presheaves of modules. -/
 @[simps]
