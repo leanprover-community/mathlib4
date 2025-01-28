31657f1e21e826854ac69690d02dcad0c2371973
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2025-01-28 19:01:54 +0100

    adjust import

--- Mathlib/GroupTheory/Index.lean
+++ Mathlib/GroupTheory/Index.lean
@@ -11,9 +11,6 @@ import Mathlib.GroupTheory.Finiteness
 import Mathlib.GroupTheory.GroupAction.Quotient
 import Mathlib.GroupTheory.QuotientGroup.Basic
 
-import Mathlib.Data.Nat.Prime.Factorial
-import Mathlib.Data.Fintype.Perm
-
 /-!
 # Index of a Subgroup
 
--- Mathlib/GroupTheory/IndexNormal.lean
+++ Mathlib/GroupTheory/IndexNormal.lean
@@ -3,9 +3,9 @@ Copyright (c) 2025 Antoine Chambert-Loir. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Antoine Chambert-Loir
 -/
-import Mathlib.GroupTheory.Index
-import Mathlib.Data.Fintype.Basic
+import Mathlib.Data.Fintype.Perm
 import Mathlib.Data.Nat.Prime.Factorial
+import Mathlib.GroupTheory.Index
 
 /-! # Subgroups of small index are normal
 
