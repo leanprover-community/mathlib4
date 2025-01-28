27cca525ff5f6debb6c5cc8275c0d1e4104ca992
Pim Otte <p.j.otte@uu.nl>
2025-01-28 17:09:37 +0100

    Review comments

--- Mathlib/Data/Set/Quot.lean
+++ Mathlib/Data/Set/Quot.lean
@@ -39,13 +39,13 @@ structure Represents (s : Set α) (C : Set (Quot r)) where
 lemma out_image_represents (C : Set (Quot r)) : (Quot.out '' C).Represents C where
   unique_rep {c} h := by
     use c.out
-    simp only [Set.mem_inter_iff, and_imp]
+    simp only [mem_inter_iff, and_imp]
     refine ⟨⟨by aesop, Quot.out_eq _⟩, fun y hy hyc ↦ ?_⟩
     obtain ⟨c', ⟨_, rfl⟩⟩ := hy
     rw [← hyc, c'.out_eq]
   exact {c} h := by
     ext v
-    simp only [Set.mem_inter_iff, Set.mem_image, Set.mem_empty_iff_false, iff_false,
+    simp only [mem_inter_iff, mem_image, mem_empty_iff_false, iff_false,
       not_and, forall_exists_index, and_imp]
     intro c' hc' hv hvc
     rw [← hvc, ← hv, c'.out_eq] at h
@@ -53,31 +53,26 @@ lemma out_image_represents (C : Set (Quot r)) : (Quot.out '' C).Represents C whe
 
 lemma ncard_represents_inter {C : Set (Quot r)} {s : Set α} {c : (Quot r)} (hrep : s.Represents C)
     (h : c ∈ C) : (s ∩ c).ncard = 1 := by
-  rw [Set.ncard_eq_one]
+  rw [ncard_eq_one]
   obtain ⟨a, ha⟩ := hrep.unique_rep h
   aesop
 
 lemma disjoint_represents {C : Set (Quot r)} {s : Set α} {c : Quot r} (hrep : s.Represents C)
     (h : c ∉ C) : Disjoint s c := by
-  rw [Set.disjoint_right]
+  rw [disjoint_right]
   intro v hv hvr
   have := hrep.exact h
-  rw [Set.eq_empty_iff_forall_not_mem] at this
+  rw [eq_empty_iff_forall_not_mem] at this
   simp_all
-
-lemma disjoint_of_represents {c : Quot r} {s : Set α} {p : Quot r → Prop}
-    (hrep : s.Represents {c | p c}) (h : ¬ p c) : Disjoint s c := by
-  apply disjoint_represents hrep
-  simpa
 
 lemma ncard_sdiff_represents_of_mem [Fintype α] {c : Quot r} {s : Set α} {C : Set (Quot r)}
     (hrep : s.Represents C) (h : c ∈ C) : ((c : Set α) \ s).ncard = (c : Set α).ncard - 1 := by
-  simp [← Set.ncard_inter_add_ncard_diff_eq_ncard c s (Set.toFinite _),
-    Set.inter_comm, ncard_represents_inter hrep h]
+  simp [← ncard_inter_add_ncard_diff_eq_ncard c s (toFinite _),
+    inter_comm, ncard_represents_inter hrep h]
 
 lemma ncard_sdiff_represents_of_not_mem [Fintype α] {c : Quot r} {s : Set α} {C : Set (Quot r)}
     (hrep : s.Represents C) (h : c ∉ C) : ((c : Set α) \ s).ncard = (c : Set α).ncard := by
-  simp [← Set.ncard_inter_add_ncard_diff_eq_ncard c s (Set.toFinite _),
-    Set.inter_comm, hrep.exact h]
+  simp [← ncard_inter_add_ncard_diff_eq_ncard c s (toFinite _),
+    inter_comm, hrep.exact h]
 
 end Set
