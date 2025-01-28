caa689f7d2bba7f7c2cc41f3d89e3561c17ae034
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-01-28 14:17:31 +0100

    feat(CategoryTheory): the subpresheaf of types generated by a section

--- Mathlib.lean
+++ Mathlib.lean
@@ -2266,6 +2266,7 @@ import Mathlib.CategoryTheory.Subobject.Types
 import Mathlib.CategoryTheory.Subobject.WellPowered
 import Mathlib.CategoryTheory.Subpresheaf.Basic
 import Mathlib.CategoryTheory.Subpresheaf.Image
+import Mathlib.CategoryTheory.Subpresheaf.OfSection
 import Mathlib.CategoryTheory.Subpresheaf.Sieves
 import Mathlib.CategoryTheory.Subterminal
 import Mathlib.CategoryTheory.Sums.Associator
