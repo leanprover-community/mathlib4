5c26fd2ca4f51a577309fca27959e1ee27f821ab
Jakob von Raumer <jakob@von-raumer.de>
2025-01-28 14:53:02 +0100

    ofFullyFaithful_map_add

--- Mathlib/CategoryTheory/Preadditive/Transfer.lean
+++ Mathlib/CategoryTheory/Preadditive/Transfer.lean
@@ -6,26 +6,33 @@ Authors: Jakob von Raumer
 import Mathlib.CategoryTheory.Preadditive.Basic
 import Mathlib.Algebra.Equiv.TransferInstance
 
-namespace CategoryTheory
+/-!
+# Pulling back a preadditive structure along a fully faithful functor
+
+A preadditive structure on a category `D` transfers to a preadditive structure on `C` for a given
+fully faithful functor `F : C ⥤ D`.
+-/
+namespace CategoryTheory.Preadditive
 
 open Limits
 
 universe v₁ v₂ u₁ u₂
 
 variable {C : Type u₁} [Category.{v₁} C]
-variable {D : Type u₂} [Category.{v₂} D]
+variable {D : Type u₂} [Category.{v₂} D] [Preadditive D]
 variable {F : C ⥤ D} (hF : F.FullyFaithful)
 
-@[simp]
-lemma Functor.FullyFaithful.preimage_comp {P Q R : C} (f : F.obj P ⟶ F.obj Q)
-    (g : F.obj Q ⟶ F.obj R) :  hF.preimage (f ≫ g) = hF.preimage f ≫ hF.preimage g := by
-  apply hF.map_injective
-  rw [F.map_comp]
-  simp
-
-def Functor.FullyFaithful.pullback_preadditive [Preadditive D] : Preadditive C where
+/-- If `D` is a preadditive category, any fully faithful functor `F : C ⥤ D` induces a preadditive
+structure on `C`. -/
+def ofFullyFaithful : Preadditive C where
   homGroup P Q := hF.homEquiv.addCommGroup
   add_comp P Q R f f' g := hF.map_injective (by simp [Equiv.add_def])
   comp_add P Q R f g g' := hF.map_injective (by simp [Equiv.add_def])
 
-end CategoryTheory
+@[simp low]
+lemma ofFullyFaithful_map_add {P Q : C} (f g : P ⟶ Q) :
+    have : Preadditive C := ofFullyFaithful hF
+    F.map (f + g) = F.map f + F.map g := by
+  simp [Equiv.add_def]
+
+end CategoryTheory.Preadditive
