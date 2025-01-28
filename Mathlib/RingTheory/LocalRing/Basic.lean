a2ee1854d3ddc7ff7809c5eaab885ca9875c185b
Michał Staromiejski <michal.staromiejski@gmail.com>
2025-01-28 10:58:42 +0100

    feat(RingTheory/LocalRing): `IsLocalRing` for subring

--- Mathlib/RingTheory/LocalRing/Basic.lean
+++ Mathlib/RingTheory/LocalRing/Basic.lean
@@ -3,6 +3,8 @@ Copyright (c) 2018 Kenny Lau. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kenny Lau, Chris Hughes, Mario Carneiro
 -/
+import Mathlib.Algebra.Ring.Subsemiring.Basic
+import Mathlib.Algebra.GroupWithZero.NonZeroDivisors
 import Mathlib.RingTheory.LocalRing.Defs
 import Mathlib.RingTheory.Ideal.Nonunits
 
@@ -31,6 +33,30 @@ theorem of_nonunits_add [Nontrivial R]
     (h : ∀ a b : R, a ∈ nonunits R → b ∈ nonunits R → a + b ∈ nonunits R) : IsLocalRing R where
   isUnit_or_isUnit_of_add_one {a b} hab :=
     or_iff_not_and_not.2 fun H => h a b H.1 H.2 <| hab.symm ▸ isUnit_one
+
+open nonZeroDivisors
+
+/-- If a (semi)ring `R` in which every element is either invertible or a zero divisor
+embeds in a local (semi)ring `S`, then `R` is local. -/
+lemma of_injective {R S} [Semiring R] [Semiring S] [IsLocalRing S]
+    {f : R →+* S} (hf : Function.Injective f) (h : ∀ a, a ∈ R⁰ → IsUnit a) : IsLocalRing R := by
+  haveI : Nontrivial R := f.domain_nontrivial
+  refine .of_is_unit_or_is_unit_of_add_one fun {a b} hab ↦
+    (IsLocalRing.isUnit_or_isUnit_of_add_one (map_add f .. ▸ map_one f ▸ congrArg f hab)).imp ?_ ?_
+  <;> exact h _ ∘ mem_nonZeroDivisor_of_injective hf ∘ IsUnit.mem_nonZeroDivisors
+
+/-- If in a sub(semi)ring `R` of a local (semi)ring `S` every element is either
+invertible or a zero divisor, then `R` is local. -/
+lemma of_subring {S} [Semiring S] [IsLocalRing S] {R : Subsemiring S}
+    (h : ∀ a, a ∈ R⁰ → IsUnit a) : IsLocalRing R :=
+  of_injective R.subtype_injective h
+
+/-- If in a sub(semi)ring `R` of a local (semi)ring `R'` every element is either
+invertible or a zero divisor, then `R` is local.
+This version is for `R` and `R'` that are both sub(semi)rings of a (semi)ring `S`. -/
+lemma of_subring' {S} [Semiring S] {R R' : Subsemiring S} [IsLocalRing R'] (inc : R ≤ R')
+    (h : ∀ a, a ∈ R⁰ → IsUnit a) : IsLocalRing R :=
+  of_injective (Subsemiring.inclusion_injective inc) h
 
 end Semiring
 
