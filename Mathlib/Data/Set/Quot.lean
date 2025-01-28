f2f07263229524829f57411b81d8afe30151f384
Pim Otte <p.j.otte@uu.nl>
2025-01-28 07:47:01 +0100

    Naming

--- Mathlib/Data/Set/Quot.lean
+++ Mathlib/Data/Set/Quot.lean
@@ -19,9 +19,9 @@ import Mathlib.Data.Set.Card
 variable {α : Type*} {r : α → α → Prop}
 
 /-- Convert an equivalence class to a `Set`. -/
-def Quot.toSet (c : Quot r) : Set V := {v | Quot.mk r v = c}
+def Quot.toSet (c : Quot r) : Set α := {v | Quot.mk r v = c}
 
-instance : SetLike (Quot r) V where
+instance : SetLike (Quot r) α where
   coe := Quot.toSet
   coe_injective' x y h := by
     simp only [Quot.toSet] at h
@@ -32,11 +32,11 @@ instance : SetLike (Quot r) V where
 namespace Set
 
 /-- A set represents a set of quotients if it contains exactly one element from each quotient. -/
-structure Represents (s : Set V) (C : Set (Quot r)) where
+structure Represents (s : Set α) (C : Set (Quot r)) where
   unique_rep {c : Quot r} (h : c ∈ C) : ∃! v, v ∈ s ∩ c
   exact {c : Quot r} (h : c ∉ C) : s ∩ c = ∅
 
-lemma represents_of_image_exists_rep_choose (C : Set (Quot r)) :
+lemma out_image_represents (C : Set (Quot r)) :
     (Quot.out '' C).Represents C where
   unique_rep {c} h := by
     use c.out
@@ -52,13 +52,13 @@ lemma represents_of_image_exists_rep_choose (C : Set (Quot r)) :
     rw [← hvc, ← hv, c'.out_eq] at h
     exact h hc'
 
-lemma ncard_represents_inter {C : Set (Quot r)} {s : Set V}
+lemma ncard_represents_inter {C : Set (Quot r)} {s : Set α}
     {c : (Quot r)} (hrep : s.Represents C) (h : c ∈ C) : (s ∩ c).ncard = 1 := by
   rw [Set.ncard_eq_one]
   obtain ⟨a, ha⟩ := hrep.unique_rep h
   aesop
 
-lemma disjoint_represents {C : Set (Quot r)} {s : Set V}
+lemma disjoint_represents {C : Set (Quot r)} {s : Set α}
     {c : Quot r} (hrep : s.Represents C) (h : c ∉ C) : Disjoint s c := by
   rw [Set.disjoint_right]
   intro v hv hvr
@@ -66,20 +66,20 @@ lemma disjoint_represents {C : Set (Quot r)} {s : Set V}
   rw [Set.eq_empty_iff_forall_not_mem] at this
   simp_all
 
-lemma disjoint_of_represents {c : Quot r} {s : Set V} {p : Quot r → Prop}
+lemma disjoint_of_represents {c : Quot r} {s : Set α} {p : Quot r → Prop}
     (hrep : s.Represents {c | p c}) (h : ¬ p c) : Disjoint s c := by
   apply disjoint_represents hrep
   simpa
 
-lemma ncard_sdiff_represents_of_mem [Fintype V] {c : Quot r}
-    {s : Set V} {C : Set (Quot r)}
-    (hrep : s.Represents C) (h : c ∈ C) : ((c : Set V) \ s).ncard = (c : Set V).ncard - 1 := by
+lemma ncard_sdiff_represents_of_mem [Fintype α] {c : Quot r}
+    {s : Set α} {C : Set (Quot r)}
+    (hrep : s.Represents C) (h : c ∈ C) : ((c : Set α) \ s).ncard = (c : Set α).ncard - 1 := by
   simp [← Set.ncard_inter_add_ncard_diff_eq_ncard c s (Set.toFinite _),
     Set.inter_comm, ncard_represents_inter hrep h]
 
-lemma ncard_sdiff_represents_of_not_mem [Fintype V] {c : Quot r}
-    {s : Set V} {C : Set (Quot r)}
-    (hrep : s.Represents C) (h : c ∉ C) : ((c : Set V) \ s).ncard = (c : Set V).ncard := by
+lemma ncard_sdiff_represents_of_not_mem [Fintype α] {c : Quot r}
+    {s : Set α} {C : Set (Quot r)}
+    (hrep : s.Represents C) (h : c ∉ C) : ((c : Set α) \ s).ncard = (c : Set α).ncard := by
   simp [← Set.ncard_inter_add_ncard_diff_eq_ncard c s (Set.toFinite _),
     Set.inter_comm, hrep.exact h]
 
