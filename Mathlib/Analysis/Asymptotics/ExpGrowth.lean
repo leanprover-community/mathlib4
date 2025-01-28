243f410afafd950a57aaa42d1a230d7ff7914da3
D-Thomine <100795491+D-Thomine@users.noreply.github.com>
2025-01-28 15:30:45 +0100

    Docstrings

--- Mathlib/Analysis/Asymptotics/ExpGrowth.lean
+++ Mathlib/Analysis/Asymptotics/ExpGrowth.lean
@@ -29,8 +29,10 @@ open ENNReal EReal Filter Function
 
 /-! ### Definition -/
 
+-- Lower exponential growth of a sequence of extended nonnegative real numbers.
 noncomputable def expGrowthInf (u : ℕ → ℝ≥0∞) : EReal := atTop.liminf fun n ↦ log (u n) / n
 
+-- Upper exponential growth of a sequence of extended nonnegative real numbers.
 noncomputable def expGrowthSup (u : ℕ → ℝ≥0∞) : EReal := atTop.limsup fun n ↦ log (u n) / n
 
 /-! ### Basic properties -/
@@ -252,6 +254,7 @@ lemma expGrowthInf_inf {u v : ℕ → ℝ≥0∞} :
   rw [Pi.inf_apply, log_monotone.map_min]
   exact (monotone_div_right_of_nonneg (Nat.cast_nonneg' n)).map_min
 
+-- Lower exponential growth as an `InfTopHom`.
 noncomputable def expGrowthInf_infTopHom : InfTopHom (ℕ → ℝ≥0∞) EReal where
   toFun := expGrowthInf
   map_inf' := fun u v ↦ @expGrowthInf_inf u v
@@ -275,6 +278,7 @@ lemma expGrowthSup_sup {u v : ℕ → ℝ≥0∞} :
   rw [Pi.sup_apply, log_monotone.map_max]
   exact (monotone_div_right_of_nonneg (Nat.cast_nonneg' n)).map_max
 
+-- Upper exponential growth as a `SupBotHom`.
 noncomputable def expGrowthSup_supBotHom : SupBotHom (ℕ → ℝ≥0∞) EReal where
   toFun := expGrowthSup
   map_sup' := fun u v ↦ @expGrowthSup_sup u v
