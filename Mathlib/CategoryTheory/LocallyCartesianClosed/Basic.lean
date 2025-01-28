c4c0c12a5f8084c024dde276ce5c0104d86f3826
Sina Hazratpour <sinahazratpour@gmail.com>
2025-01-28 14:46:56 +0100

    Create Basic.lean

---
+++ Mathlib/CategoryTheory/LocallyCartesianClosed/Basic.lean
@@ -0,0 +1,57 @@
+/-
+Copyright (c) 2025 Sina Hazratpour. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Sina Hazratpour and Emily Riehl
+-/
+
+import Mathlib.CategoryTheory.Closed.Cartesian
+import Mathlib.CategoryTheory.Limits.Constructions.Over.Products
+import Mathlib.CategoryTheory.Monoidal.OfHasFiniteProducts
+import Mathlib.CategoryTheory.Adjunction.Over
+
+/-!
+# Locally cartesian closed categories
+
+There are several equivalent definitions of locally
+cartesian closed categories.
+
+1. A locally cartesian closed category is a category C such that all
+the slices `Over I` are cartesian closed categories.
+
+2. Equivalently, a locally cartesian closed category `C` is a category with pullbacks such that
+each base change functor has a right adjoint, called the pushforward functor.
+
+In this file we prove the equivalence of these conditions.
+
+We also show that a locally cartesian closed category with a terminal object is cartesian closed.
+
+-/
+
+noncomputable section
+
+namespace CategoryTheory
+
+open CategoryTheory Category Limits Functor Adjunction Over
+
+attribute [local instance] monoidalOfChosenFiniteProducts
+
+universe v u
+
+section
+
+variable (C : Type u) [Category.{v} C]
+
+attribute [local instance] ConstructProducts.over_finiteProducts_of_finiteWidePullbacks
+attribute [local instance] ChosenFiniteProducts.ofFiniteProducts
+
+class LCC (C : Type u) [Category.{v} C] [HasFiniteWidePullbacks C] where
+  over_cc : Π (I : C), CartesianClosed (Over I)
+  pushforward {X Y : C} (f : X ⟶ Y) : Over X ⥤ Over Y
+  adj {X Y : C} (f : X ⟶ Y) : Over.pullback f ⊣ pushforward f := by infer_instance
+
+end
+
+
+
+
+end CategoryTheory
