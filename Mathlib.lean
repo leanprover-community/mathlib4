c6e9ee396fc501cdc740b21627b2179e04f1c35a
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-01-29 11:17:56 +0000

    feat(Algebra/Homology/Embedding): the homology of truncLE (#19574)
    Given an embedding `e` of complex shapes that satisfies `IsTruncLE`, we show that the morphism `K.ιTruncLE e : K.truncLE e ⟶ K` induces a quasi-isomorphism in degree `e.f i` for all `i`.
    
    This is the dual result of #19572.
    
    
    
    Co-authored-by: Joël Riou <37772949+joelriou@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -451,6 +451,7 @@ import Mathlib.Algebra.Homology.Embedding.StupidTrunc
 import Mathlib.Algebra.Homology.Embedding.TruncGE
 import Mathlib.Algebra.Homology.Embedding.TruncGEHomology
 import Mathlib.Algebra.Homology.Embedding.TruncLE
+import Mathlib.Algebra.Homology.Embedding.TruncLEHomology
 import Mathlib.Algebra.Homology.ExactSequence
 import Mathlib.Algebra.Homology.Factorizations.Basic
 import Mathlib.Algebra.Homology.Functor
