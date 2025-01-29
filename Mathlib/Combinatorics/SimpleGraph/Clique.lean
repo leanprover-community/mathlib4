0458e41a46832af1eb094820cd8d4181f8ed12d6
jt496 <john.talbot@gmail.com>
2025-01-29 20:27:02 +0000

    incorporated suggested changes

--- Mathlib/Combinatorics/SimpleGraph/Clique.lean
+++ Mathlib/Combinatorics/SimpleGraph/Clique.lean
@@ -368,11 +368,8 @@ protected theorem CliqueFree.replaceVertex [DecidableEq α] (h : G.CliqueFree n)
     exact hφ
 
 @[simp]
-lemma cliqueFree_one : G.CliqueFree 1 ↔ IsEmpty α :=by
-  constructor
-  · simp only [CliqueFree, isNClique_one, not_exists, forall_eq_apply_imp_iff]
-    exact fun a => { false := a }
-  · intro h t hf; simp_all
+lemma cliqueFree_one : G.CliqueFree 1 ↔ IsEmpty α := by
+  simp [CliqueFree, isEmpty_iff]
 
 @[simp]
 theorem cliqueFree_two : G.CliqueFree 2 ↔ G = ⊥ := by
