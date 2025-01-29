5818e47b974337a0a26cb2574de5401021e10044
Jakob von Raumer <jakob@von-raumer.de>
2025-01-29 14:41:01 +0000

    feat(CategoryTheory) : pull back preadditivity along fully faithful functors (#21170)

--- Mathlib.lean
+++ Mathlib.lean
@@ -2149,6 +2149,7 @@ import Mathlib.CategoryTheory.Preadditive.Projective
 import Mathlib.CategoryTheory.Preadditive.ProjectiveResolution
 import Mathlib.CategoryTheory.Preadditive.Schur
 import Mathlib.CategoryTheory.Preadditive.SingleObj
+import Mathlib.CategoryTheory.Preadditive.Transfer
 import Mathlib.CategoryTheory.Preadditive.Yoneda.Basic
 import Mathlib.CategoryTheory.Preadditive.Yoneda.Injective
 import Mathlib.CategoryTheory.Preadditive.Yoneda.Limits
