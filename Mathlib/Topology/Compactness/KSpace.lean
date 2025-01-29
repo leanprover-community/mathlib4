3e19e135c796d481d5b3b47758edab3b5d8394e7
Hannah Scholz <hannah@scholzbande.de>
2025-01-29 21:08:18 +0100

    removed unused inmport

--- Mathlib/Topology/Compactness/KSpace.lean
+++ Mathlib/Topology/Compactness/KSpace.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Floris van Doorn, Hannah Scholz
 -/
 
-import Mathlib.Topology.Sets.Compacts
 import Mathlib.Topology.Defs.Induced
 import Mathlib.Topology.RestrictGen
 import Mathlib.Topology.Compactness.CompactlyGeneratedSpace
