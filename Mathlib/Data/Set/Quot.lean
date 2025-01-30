e321c07f0ebbde342db0bb8b387ed2be5d633360
Pim Otte <p.j.otte@uu.nl>
2025-01-30 17:36:15 +0100

    Switch to bijOn

--- Mathlib/Data/Set/Quot.lean
+++ Mathlib/Data/Set/Quot.lean
@@ -29,54 +29,52 @@ instance : SetLike (Quot r) α where
       rw [← h]
       exact x.out_eq
 
-@[simp] lemma Quot.mem_toSet {x : α} {c : Quot r} : x ∈ c.toSet ↔ Quot.mk r x = c := Iff.rfl
+@[simp] lemma Quot.mem_toSet {x : α} {c : Quot r} : x ∈ c ↔ Quot.mk r x = c := Iff.rfl
 
 @[ext] theorem Quot.ext (c d : Quot r) (h : ∀ x, x ∈ c ↔ x ∈ d) : c = d := SetLike.ext h
 
 namespace Set
 
 /-- A set represents a set of quotients if it contains exactly one element from each quotient. -/
-structure Represents (s : Set α) (C : Set (Quot r)) where
-  unique_rep {c : Quot r} (h : c ∈ C) : ∃! v, v ∈ s ∩ c
-  exact {c : Quot r} (h : c ∉ C) : s ∩ c = ∅
+def Represents (s : Set α) (C : Set (Quot r)) := Set.BijOn (Quot.mk r) s C
 
-lemma out_image_represents (C : Set (Quot r)) : (Quot.out '' C).Represents C where
-  unique_rep {c} h := by
-    use c.out
-    simp only [mem_inter_iff, and_imp]
-    refine ⟨⟨by aesop, Quot.out_eq _⟩, fun y hy hyc ↦ ?_⟩
-    obtain ⟨c', ⟨_, rfl⟩⟩ := hy
-    rw [← hyc, c'.out_eq]
-  exact {c} h := by
-    ext v
-    simp only [mem_inter_iff, mem_image, mem_empty_iff_false, iff_false,
-      not_and, forall_exists_index, and_imp]
-    intro c' hc' hv hvc
-    rw [← hvc, ← hv, c'.out_eq] at h
-    exact h hc'
+lemma out_image_represents (C : Set (Quot r)) : (Quot.out '' C).Represents C :=
+  Set.BijOn.mk (by rintro c ⟨x, ⟨hx, rfl⟩⟩; simp_all) (by
+    rintro x ⟨c, ⟨hc, rfl⟩⟩ y ⟨d, ⟨hd, rfl⟩⟩ hxy
+    aesop) (fun _ _ ↦ by simpa)
+
+namespace Represents
 
 variable {C : Set (Quot r)} {s : Set α} {c : Quot r}
 
-lemma ncard_represents_inter (hrep : s.Represents C)(h : c ∈ C) : (s ∩ c).ncard = 1 := by
+lemma unique_rep (hrep : s.Represents C) (h : c ∈ C) : ∃! x, x ∈ s ∩ c := by
+  obtain ⟨x, ⟨hx, rfl⟩⟩ := hrep.2.2 h
+  use x
+  simp only [mem_inter_iff, hx, SetLike.mem_coe, Quot.mem_toSet, and_self, and_imp, true_and]
+  exact fun y hy hyx ↦ hrep.2.1 hy hx hyx
+
+lemma exact (hrep : s.Represents C) (h : c ∉ C) : Disjoint s c := by
+  rw [disjoint_left]
+  intro a ha hc
+  simp only [SetLike.mem_coe, Quot.mem_toSet] at hc
+  subst hc
+  exact h (hrep.1 ha)
+
+lemma ncard_inter (hrep : s.Represents C) (h : c ∈ C) : (s ∩ c).ncard = 1 := by
   rw [ncard_eq_one]
   obtain ⟨a, ha⟩ := hrep.unique_rep h
   aesop
 
-lemma disjoint_represents (hrep : s.Represents C) (h : c ∉ C) : Disjoint s c := by
-  rw [disjoint_right]
-  intro v hv hvr
-  have := hrep.exact h
-  rw [eq_empty_iff_forall_not_mem] at this
-  simp_all
-
-lemma ncard_sdiff_represents_of_mem [Fintype α] (hrep : s.Represents C) (h : c ∈ C) :
+lemma ncard_sdiff_of_mem [Fintype α] (hrep : s.Represents C) (h : c ∈ C) :
     ((c : Set α) \ s).ncard = (c : Set α).ncard - 1 := by
   simp [← ncard_inter_add_ncard_diff_eq_ncard c s (toFinite _),
-    inter_comm, ncard_represents_inter hrep h]
+    inter_comm, ncard_inter hrep h]
 
-lemma ncard_sdiff_represents_of_not_mem [Fintype α] (hrep : s.Represents C) (h : c ∉ C) :
+lemma ncard_sdiff_of_not_mem [Fintype α] (hrep : s.Represents C) (h : c ∉ C) :
     ((c : Set α) \ s).ncard = (c : Set α).ncard := by
   simp [← ncard_inter_add_ncard_diff_eq_ncard c s (toFinite _),
-    inter_comm, hrep.exact h]
+    inter_comm, disjoint_iff_inter_eq_empty.mp (hrep.exact h)]
+
+end Represents
 
 end Set
