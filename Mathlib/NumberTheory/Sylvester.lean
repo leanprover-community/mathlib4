6300ca3c871bb6503cb6a3590a8fb780ea1b27c7
Walter Moreira <walter@waltermoreira.net>
2025-01-29 18:48:51 -0600

    Rename sylvester -> sylvesterSequence

--- Mathlib/NumberTheory/Sylvester.lean
+++ Mathlib/NumberTheory/Sylvester.lean
@@ -33,26 +33,26 @@ We follow the presentantion from [Wikipedia](https://en.wikipedia.org/wiki/Sylve
 open Nat
 
 /-- Sylvester's sequence: https://oeis.org/A000058. -/
-def sylvester : ℕ → ℕ
+def sylvesterSequence : ℕ → ℕ
   | 0 => 2
-  | n + 1 => (sylvester n) * (sylvester n - 1) + 1
+  | n + 1 => (sylvesterSequence n) * (sylvesterSequence n - 1) + 1
 
 @[simp]
-theorem sylvester_zero : sylvester 0 = 2 := rfl
+theorem sylvester_zero : sylvesterSequence 0 = 2 := rfl
 
 @[simp]
-theorem sylvester_one : sylvester 1 = 3 := rfl
+theorem sylvester_one : sylvesterSequence 1 = 3 := rfl
 
 @[simp]
-theorem sylvester_two : sylvester 2 = 7 := rfl
+theorem sylvester_two : sylvesterSequence 2 = 7 := rfl
 
 @[simp]
-theorem sylvester_three : sylvester 3 = 43 := rfl
+theorem sylvester_three : sylvesterSequence 3 = 43 := rfl
 
-theorem two_le_sylvester (n : ℕ) : 2 ≤ sylvester n := by
+theorem two_le_sylvester (n : ℕ) : 2 ≤ sylvesterSequence n := by
   induction' n with n ih
   · simp
-  · simp only [sylvester, reduceLeDiff]
+  · simp only [sylvesterSequence, reduceLeDiff]
     exact one_le_mul_of_one_le_of_one_le (by linarith) (by omega)
 
 /--
@@ -63,35 +63,37 @@ $$
 assuming the product over the empty set to be 1.
 -/
 theorem sylvester_prod_finset_add_one {n : ℕ} :
-    sylvester n = ∏ i ∈ Finset.range n, sylvester i + 1 := by
-  rw [Finset.prod_range_induction _ (fun n => sylvester n - 1)]
+    sylvesterSequence n = ∏ i ∈ Finset.range n, sylvesterSequence i + 1 := by
+  rw [Finset.prod_range_induction _ (fun n => sylvesterSequence n - 1)]
   · exact (Nat.sub_one_add_one (by linarith [two_le_sylvester n])).symm
   · norm_num
-  · simp [sylvester, mul_comm]
+  · simp [sylvesterSequence, mul_comm]
 
-theorem sylvester_strictMono : StrictMono sylvester := by
+theorem sylvester_strictMono : StrictMono sylvesterSequence := by
   apply strictMono_nat_of_lt_succ
   intro n
-  simp only [sylvester]
+  simp only [sylvesterSequence]
   calc
-    sylvester n * (sylvester n - 1) + 1 > sylvester n * (sylvester n - 1) := by linarith
-    _ ≥ sylvester n := Nat.le_mul_of_pos_right _ <| le_sub_one_of_lt <| two_le_sylvester n
+    sylvesterSequence n * (sylvesterSequence n - 1) + 1 >
+      sylvesterSequence n * (sylvesterSequence n - 1) := by linarith
+    _ ≥ sylvesterSequence n := Nat.le_mul_of_pos_right _ <| le_sub_one_of_lt <| two_le_sylvester n
 
 /-!
 ### Coprimality
 -/
 
 theorem sylvester_mod_eq_one {m n : ℕ} (h : m < n) :
-    sylvester n % sylvester m = 1 := by
+    sylvesterSequence n % sylvesterSequence m = 1 := by
   rw [sylvester_prod_finset_add_one, ← mod_add_mod,
     dvd_iff_mod_eq_zero.mp (Finset.dvd_prod_of_mem _ <| Finset.mem_range.mpr h)]
   exact Nat.mod_eq_of_lt <| two_le_sylvester _
 
 private theorem sylvester_coprime_of_lt {m n : ℕ} (h : m < n) :
-    Coprime (sylvester m) (sylvester n) := by
+    Coprime (sylvesterSequence m) (sylvesterSequence n) := by
   rw [Coprime, Nat.gcd_rec, sylvester_mod_eq_one h, gcd_one_left]
 
-theorem sylvester_coprime {m n : ℕ} (h : m ≠ n) : Coprime (sylvester m) (sylvester n) := by
+theorem sylvester_coprime {m n : ℕ} (h : m ≠ n) :
+    Coprime (sylvesterSequence m) (sylvesterSequence n) := by
   rcases h.lt_or_lt with c | c
   · exact sylvester_coprime_of_lt c
   · exact (sylvester_coprime_of_lt c).symm
