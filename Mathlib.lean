7659e1cdcb7ee7a9dfae5ab4bf51b38961fd27f0
jt496 <john.talbot@gmail.com>
2025-01-31 14:42:34 +0000

    add file

--- Mathlib.lean
+++ Mathlib.lean
@@ -2367,6 +2367,7 @@ import Mathlib.Combinatorics.SimpleGraph.Basic
 import Mathlib.Combinatorics.SimpleGraph.Circulant
 import Mathlib.Combinatorics.SimpleGraph.Clique
 import Mathlib.Combinatorics.SimpleGraph.Coloring
+import Mathlib.Combinatorics.SimpleGraph.CompletePartite
 import Mathlib.Combinatorics.SimpleGraph.ConcreteColorings
 import Mathlib.Combinatorics.SimpleGraph.Connectivity.Subgraph
 import Mathlib.Combinatorics.SimpleGraph.Connectivity.WalkCounting
