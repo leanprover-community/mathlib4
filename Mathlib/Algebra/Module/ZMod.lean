14ee94ab9ccffb807de3fa8db7bd75e78b8e659b
Yaël Dillies <yael.dillies@gmail.com>
2025-01-31 10:21:34 +0100

    nits
    Co-authored-by: Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>

--- Mathlib/Algebra/Module/ZMod.lean
+++ Mathlib/Algebra/Module/ZMod.lean
@@ -120,10 +120,10 @@ namespace ZModModule
 variable {p : ℕ} {G : Type*} [AddCommGroup G]
 
 /-- In an elementary abelian `p`-group, every finite subgroup `H` contains a further subgroup of
-cardinality between `k` and `pk`, if `k ≤ |H|`. -/
-lemma exists_submodule_subset_card_le (hp : p.Prime) [Module (ZMod p) G] {k : ℕ}
-    (H : Submodule (ZMod p) G) (hk : k ≤ Nat.card H) (h'k : k ≠ 0) :
-    ∃ (H' : Submodule (ZMod p) G), Nat.card H' ≤ k ∧ k < p * Nat.card H' ∧ H' ≤ H := by
+cardinality between `k` and `p * k`, if `k ≤ |H|`. -/
+lemma exists_submodule_subset_card_le (hp : p.Prime) [Module (ZMod p) G]
+    (H : Submodule (ZMod p) G) {k : ℕ} (hk : k ≤ Nat.card H) (h'k : k ≠ 0) :
+    ∃ H' : Submodule (ZMod p) G, Nat.card H' ≤ k ∧ k < p * Nat.card H' ∧ H' ≤ H := by
   obtain ⟨H'm, H'mHm, H'mk, kH'm⟩ := Sylow.exists_subgroup_le_card_le
     (H := AddSubgroup.toSubgroup ((AddSubgroup.toZModSubmodule _).symm H)) hp
       isPGroup_multiplicative hk h'k
