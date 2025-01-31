88d798569aae7e04713e49492605d9229adb525d
Stefan Kebekus <kebekus@users.noreply.github.com>
2025-01-31 16:10:31 +0100

    Update Meromorphic.lean

--- Mathlib/Analysis/Analytic/Meromorphic.lean
+++ Mathlib/Analysis/Analytic/Meromorphic.lean
@@ -42,27 +42,15 @@ theorem MeromorphicAt.eventually_eq_zero_or_eventually_ne_zero {f : 𝕜 → E} 
     (hf : MeromorphicAt f z₀) :
     (∀ᶠ z in 𝓝[≠] z₀, f z = 0) ∨ (∀ᶠ z in 𝓝[≠] z₀, f z ≠ 0) := by
   obtain ⟨n, h⟩ := hf
-  repeat rw [eventually_nhdsWithin_iff, eventually_nhds_iff]
   rcases h.eventually_eq_zero_or_eventually_ne_zero with h₁ | h₂
-  · obtain ⟨N, h₁N, h₂N, h₃N⟩ := eventually_nhds_iff.1 h₁
-    left
-    use N
-    simp [h₂N, h₃N]
-    intro y h₁y h₂y
-    have A := h₁N y h₁y
-    simp at A
-    rcases A with h₃ | h₄
-    · have C := sub_eq_zero.1 h₃.1
-      tauto
+  · left
+    filter_upwards [nhdsWithin_le_nhds h₁, self_mem_nhdsWithin] with y h₁y h₂y
+    rcases (smul_eq_zero.1 h₁y) with h₃ | h₄
+    · exact False.elim (h₂y (sub_eq_zero.1 (pow_eq_zero_iff'.1 h₃).1))
     · assumption
-  · obtain ⟨N, h₁N, h₂N, h₃N⟩ := eventually_nhds_iff.1 (eventually_nhdsWithin_iff.1 h₂)
-    right
-    use N
-    simp [h₂N, h₃N]
-    intro y h₁y h₂y
-    by_contra h
-    have A := h₁N y h₁y h₂y
-    simp [h] at A
+  · right
+    filter_upwards [h₂, self_mem_nhdsWithin] with y h₁y h₂y
+    exact (smul_ne_zero_iff.1 h₁y).2
 
 namespace MeromorphicAt
 
