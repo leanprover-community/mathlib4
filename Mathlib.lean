1bcccade189f51095252c8dcbb195111597586a1
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-01-31 12:11:32 +0000

    feat(Order): order isomorphisms from `Fin n` for small `n` (#21120)
    We define order isomorphisms like `Fin.orderIsoTriple` from `Fin 3` to the finset `{a, b, c}` when `a < b` and `b < c`.
    
    
    
    Co-authored-by: Joël Riou <37772949+joelriou@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -4240,6 +4240,7 @@ import Mathlib.Order.Filter.Ultrafilter.Basic
 import Mathlib.Order.Filter.Ultrafilter.Defs
 import Mathlib.Order.Filter.ZeroAndBoundedAtFilter
 import Mathlib.Order.Fin.Basic
+import Mathlib.Order.Fin.Finset
 import Mathlib.Order.Fin.Tuple
 import Mathlib.Order.FixedPoints
 import Mathlib.Order.GaloisConnection.Basic
