bebbbcb65a383b6345a1381bc97d4e267a193c09
Gabin Kolly <gabin1999@hotmail.com>
2025-02-01 22:55:46 +0100

    remove unnecessary parentheses

--- Mathlib/ModelTheory/PartialEquiv.lean
+++ Mathlib/ModelTheory/PartialEquiv.lean
@@ -462,6 +462,8 @@ def definedAtRight
 
 end IsExtensionPair
 
+#check Nat.unpair
+
 /-- For a countably generated structure `M` and a structure `N`, if any partial equivalence
 between finitely generated substructures can be extended to any element in the domain,
 then there exists an embedding of `M` in `N`. -/
