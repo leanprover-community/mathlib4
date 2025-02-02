3bf80f88ec665916bcc7031ab0adfa353c496877
Yoh Tanimoto <57562556+yoh-tanimoto@users.noreply.github.com>
2025-02-02 13:11:34 +0100

    Update Mathlib/Topology/ContinuousMap/CompactlySupported.lean
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- Mathlib/Topology/ContinuousMap/CompactlySupported.lean
+++ Mathlib/Topology/ContinuousMap/CompactlySupported.lean
@@ -3,8 +3,8 @@ Copyright (c) 2024 Yoh Tanimoto. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yoh Tanimoto
 -/
-import Mathlib.Topology.ContinuousMap.ZeroAtInfty
 import Mathlib.Topology.Algebra.Order.Support
+import Mathlib.Topology.ContinuousMap.ZeroAtInfty
 
 /-!
 # Compactly supported continuous functions
