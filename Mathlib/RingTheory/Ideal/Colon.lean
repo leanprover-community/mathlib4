af18c39dba109f8c64cca1b32fa30f3d97c0e94d
Junyan Xu <junyanxu.math@gmail.com>
2025-02-04 15:14:15 -0500

    Discard changes to Mathlib/RingTheory/Ideal/Colon.lean

--- Mathlib/RingTheory/Ideal/Colon.lean
+++ Mathlib/RingTheory/Ideal/Colon.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kenny Lau
 -/
 import Mathlib.LinearAlgebra.Quotient
-import Mathlib.RingTheory.Ideal.Maps
+import Mathlib.RingTheory.Ideal.Operations
 
 /-!
 # The colon ideal
@@ -18,7 +18,7 @@ namespace Submodule
 
 open Pointwise
 
-variable {R M M' F G : Type*} [Ring R] [AddCommGroup M] [Module R M]
+variable {R M M' F G : Type*} [CommRing R] [AddCommGroup M] [Module R M]
 variable {N N₁ N₂ P P₁ P₂ : Submodule R M}
 
 /-- `N.colon P` is the ideal of all elements `r : R` such that `r • P ⊆ N`. -/
@@ -30,8 +30,11 @@ theorem mem_colon {r} : r ∈ N.colon P ↔ ∀ p ∈ P, r • p ∈ N :=
      ⟨fun H p hp => (Quotient.mk_eq_zero N).1 (H (Quotient.mk p) (mem_map_of_mem hp)),
        fun H _ ⟨p, hp, hpm⟩ => hpm ▸ ((Quotient.mk_eq_zero N).2 <| H p hp)⟩
 
+theorem mem_colon' {r} : r ∈ N.colon P ↔ P ≤ comap (r • (LinearMap.id : M →ₗ[R] M)) N :=
+  mem_colon
+
 @[simp]
-theorem colon_top {I : Ideal R} [I.IsTwoSided] : I.colon ⊤ = I := by
+theorem colon_top {I : Ideal R} : I.colon ⊤ = I := by
   simp_rw [SetLike.ext_iff, mem_colon, smul_eq_mul]
   exact fun x ↦ ⟨fun h ↦ mul_one x ▸ h 1 trivial, fun h _ _ ↦ I.mul_mem_right _ h⟩
 
@@ -41,24 +44,6 @@ theorem colon_bot : colon ⊥ N = N.annihilator := by
 
 theorem colon_mono (hn : N₁ ≤ N₂) (hp : P₁ ≤ P₂) : N₁.colon P₂ ≤ N₂.colon P₁ := fun _ hrnp =>
   mem_colon.2 fun p₁ hp₁ => hn <| mem_colon.1 hrnp p₁ <| hp hp₁
-
-theorem annihilator_quotient {N : Submodule R M} :
-    Module.annihilator R (M ⧸ N) = N.colon ⊤ := by
-  simp_rw [SetLike.ext_iff, Module.mem_annihilator, colon, mem_annihilator, map_top,
-    LinearMap.range_eq_top.mpr (mkQ_surjective N), mem_top, forall_true_left, forall_const]
-
-theorem _root_.Ideal.annihilator_quotient {I : Ideal R} [I.IsTwoSided] :
-    Module.annihilator R (R ⧸ I) = I := by
-  rw [Submodule.annihilator_quotient, colon_top]
-
-end Submodule
-
-namespace Submodule
-
-variable {R M : Type*} [CommRing R] [AddCommGroup M] [Module R M] {N P : Submodule R M}
-
-theorem mem_colon' {r} : r ∈ N.colon P ↔ P ≤ comap (r • (LinearMap.id : M →ₗ[R] M)) N :=
-  mem_colon
 
 theorem iInf_colon_iSup (ι₁ : Sort*) (f : ι₁ → Submodule R M) (ι₂ : Sort*)
     (g : ι₂ → Submodule R M) : (⨅ i, f i).colon (⨆ j, g j) = ⨅ (i) (j), (f i).colon (g j) :=
@@ -85,5 +70,13 @@ theorem mem_colon_singleton {N : Submodule R M} {x : M} {r : R} :
 theorem _root_.Ideal.mem_colon_singleton {I : Ideal R} {x r : R} :
     r ∈ I.colon (Ideal.span {x}) ↔ r * x ∈ I := by
   simp only [← Ideal.submodule_span_eq, Submodule.mem_colon_singleton, smul_eq_mul]
+
+theorem annihilator_quotient {N : Submodule R M} :
+    Module.annihilator R (M ⧸ N) = N.colon ⊤ := by
+  simp_rw [SetLike.ext_iff, Module.mem_annihilator, colon, mem_annihilator, map_top,
+    LinearMap.range_eq_top.mpr (mkQ_surjective N), mem_top, forall_true_left, forall_const]
+
+theorem _root_.Ideal.annihilator_quotient {I : Ideal R} : Module.annihilator R (R ⧸ I) = I := by
+  rw [Submodule.annihilator_quotient, colon_top]
 
 end Submodule
