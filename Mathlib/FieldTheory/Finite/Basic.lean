7f5e4f1db4330e66c6d0c74d8ba721bf0d47a2d1
Thomas Browning <tb65536@uw.edu>
2025-02-03 22:36:45 -0800

    fix

--- Mathlib/FieldTheory/Finite/Basic.lean
+++ Mathlib/FieldTheory/Finite/Basic.lean
@@ -5,9 +5,9 @@ Authors: Chris Hughes, Joey van Langen, Casper Putz
 -/
 import Mathlib.Algebra.CharP.Reduced
 import Mathlib.Data.Nat.Prime.Int
+import Mathlib.Data.ZMod.ValMinAbs
 import Mathlib.FieldTheory.Separable
 import Mathlib.RingTheory.IntegralDomain
-import Mathlib.Tactic.ApplyFun
 
 /-!
 # Finite fields
