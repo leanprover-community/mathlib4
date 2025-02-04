694312309f7b74f0a6323340e012e3111b84c192
Junyan Xu <junyanxu.math@gmail.com>
2025-02-04 14:53:13 -0500

    Discard changes to Mathlib/Algebra/GroupWithZero/Basic.lean

--- Mathlib/Algebra/GroupWithZero/Basic.lean
+++ Mathlib/Algebra/GroupWithZero/Basic.lean
@@ -177,15 +177,6 @@ lemma sq_eq_zero_iff : a ^ 2 = 0 ↔ a = 0 := pow_eq_zero_iff two_ne_zero
 @[simp] lemma pow_eq_zero_iff' [Nontrivial M₀] : a ^ n = 0 ↔ a = 0 ∧ n ≠ 0 := by
   obtain rfl | hn := eq_or_ne n 0 <;> simp [*]
 
-theorem exists_right_inv_of_exists_left_inv {α} [MonoidWithZero α]
-    (h : ∀ a : α, a ≠ 0 → ∃ b : α, b * a = 1) {a : α} (ha : a ≠ 0) : ∃ b : α, a * b = 1 := by
-  obtain _ | _ := subsingleton_or_nontrivial α
-  · exact ⟨a, Subsingleton.elim _ _⟩
-  obtain ⟨b, hb⟩ := h a ha
-  obtain ⟨c, hc⟩ := h b (left_ne_zero_of_mul <| hb.trans_ne one_ne_zero)
-  refine ⟨b, ?_⟩
-  conv_lhs => rw [← one_mul (a * b), ← hc, mul_assoc, ← mul_assoc b, hb, one_mul, hc]
-
 end MonoidWithZero
 
 section CancelMonoidWithZero
