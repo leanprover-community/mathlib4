5e46375b0c6e34460df41068d4e7b30d7b48a157
Matej Penciak <matej.penciak@gmail.com>
2025-02-05 00:08:09 -0500

    wip

--- Mathlib/AlgebraicGeometry/Morphisms/QuasiFinite.lean
+++ Mathlib/AlgebraicGeometry/Morphisms/QuasiFinite.lean
@@ -16,32 +16,43 @@ namespace AlgebraicGeometry
 
 variable {X S : Scheme.{u}} (f : X ⟶ S)
 
--- #check Scheme.Hom
+#check Type
 
--- example {X Y : Type} {x : X} {S : Set X} {h : x ∈ S} {T : Set Y} {f : S → T} : Prop := by
---   have t := Set.preimage f {f x}
---   sorry
+#check Scheme.Hom
+#check pullback.lift
 
-#check f.fiber
+#check re
+-- set_option trace.Meta.Tactic.simp true
 
--- example {x : X} : f.fiber (f.base x) := by
+example {x : X} : f.fiber (f.base x) := by
+  let l := X.fromSpecResidueField x
+  let ll : Spec (X.residueField x) ⟶ Spec (S.residueField (f.base x)) := Spec.map <| f.residueFieldMap x
+  let lll : Spec (X.residueField x) ⟶ f.fiber (f.base x) := pullback.lift l ll (by
+    ext1
+    · simp only [Scheme.comp_coeBase, Scheme.Hom.Spec_map_residueFieldMap_fromSpecResidueField, l, ll]
+    · ext
+      simp? [l, ll]
+      sorry
+  )
 
-#check IsOpen
-
-example {s : S} : S.fromSpecResidueField s := sorry
-
-example {x : X} : f.base x = (S.fromSpecResidueField (f.base x)).base
-
-#check Scheme.fromSpecResidueField
-
-def singletonx {x} : Set (f.fiber (f.base x)) := by
-  exact fun y => y = x
-
-
+#check Scheme.Hom.app
+--
+-- #check IsOpen
+--
+-- example {s : S} : S.fromSpecResidueField s := sorry
+--
+-- example {x : X} : f.base x = (S.fromSpecResidueField (f.base x)).base
+--
+-- #check Scheme.fromSpecResidueField
+--
+-- def singletonx {x} : Set (f.fiber (f.base x)) := by
+--   exact fun y => y = x
+--
+--
 -- def isquasifinite (x : X) : Prop := IsOpen (X := f.fiber (f.base x)) {x}
-
+--
 -- @[mk_iff]
 -- class QuasiFinite (f : X ⟶ S) : Prop where
 --   asdf : ∀ (y : Y),  IsFiniteLength (f.fiber y)
-
-end AlgebraicGeometry
+--
+-- end AlgebraicGeometry
