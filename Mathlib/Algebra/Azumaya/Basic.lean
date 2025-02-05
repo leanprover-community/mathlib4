aaf3f39198f6f1000676e54e6300ac7bc8894708
Whysoserioushah <yx3021@ic.ac.uk>
2025-02-05 01:59:50 +0000

    more on azumaya

---
+++ Mathlib/Algebra/Azumaya/Basic.lean
@@ -0,0 +1,70 @@
+import Mathlib.Algebra.Azumaya.Defs
+import Mathlib.LinearAlgebra.Contraction
+import Mathlib.LinearAlgebra.TensorProduct.Opposite
+import Mathlib
+
+open scoped TensorProduct
+
+open MulOpposite
+
+namespace IsAzumaya
+
+variable (R A B : Type*) [CommRing R] [Ring A] [Ring B] [Algebra R A] [Algebra R B]
+
+/-- The "canonical" isomorphism between `R ⊗ Rᵒᵖ` and `End R R` which is equal
+  to `AlgHom.mulLeftRight R R`. -/
+noncomputable abbrev tensorEquivEnd: R ⊗[R] Rᵐᵒᵖ ≃ₐ[R] Module.End R R :=
+  Algebra.TensorProduct.lid R Rᵐᵒᵖ|>.trans <|
+  AlgEquiv.ofRingEquiv (f := Module.moduleEndSelf R) fun r ↦ by ext; simp
+
+lemma tensorEquivEnd_eq_mulLeftRight: tensorEquivEnd R = AlgHom.mulLeftRight R R := by
+  ext; simp
+
+theorem Self: IsAzumaya R R where
+  bij := by rw [← tensorEquivEnd_eq_mulLeftRight]; exact tensorEquivEnd R|>.bijective
+
+/--
+The following diagram commutes:
+```
+          e ⊗ eᵒᵖ
+A ⊗ Aᵐᵒᵖ  ------------> B ⊗ Bᵐᵒᵖ
+  |                        |
+  |                        |
+  | mulLeftRight R A       | mulLeftRight R B
+  |                        |
+  V                        V
+End R A   ------------> End R B
+          e.conj
+```
+-/
+lemma small_comm_square (e : A ≃ₐ[R] B):
+    (AlgHom.mulLeftRight R B).comp (Algebra.TensorProduct.congr e e.op).toAlgHom =
+    (e.toLinearEquiv.algConj).toAlgHom.comp (AlgHom.mulLeftRight R A) := by
+  apply AlgHom.ext
+  intro a
+  induction a using TensorProduct.induction_on with
+  | zero => simp
+  | tmul a a' =>
+    ext; simp [AlgHom.mulLeftRight_apply, LinearEquiv.algConj, LinearEquiv.conj]
+  | add _ _ _ _ => simp_all [map_add]
+
+variable {R A B} in
+lemma _root_.FaithfulSMul.ofAlgEquiv [FaithfulSMul R A] (e : A ≃ₐ[R] B) :
+    FaithfulSMul R B where
+  eq_of_smul_eq_smul {r1 r2} h12 := by
+    specialize h12 1
+    rw [← e.apply_symm_apply 1, ← map_smul, map_one e.symm, ← map_smul] at h12
+    have : ∀ (a : A), r1 • a = r2 • a := fun a ↦ by
+      rw [← one_mul a, ← smul_mul_assoc, e.injective h12, smul_mul_assoc]
+    exact eq_of_smul_eq_smul this
+
+theorem ofAlgEquiv (e : A ≃ₐ[R] B) (hA : IsAzumaya R A) : IsAzumaya R B :=
+  let _ : Module.Projective R B := .of_equiv e.toLinearEquiv
+  let _ : FaithfulSMul R B := .ofAlgEquiv e
+  let _ : Module.Finite R B := .equiv e.toLinearEquiv
+  ⟨Function.Bijective.of_comp_iff (AlgHom.mulLeftRight R B)
+    (Algebra.TensorProduct.congr e e.op).bijective |>.1 <| by
+    erw [← AlgHom.coe_comp, small_comm_square]
+    simp [hA.bij]⟩
+
+end IsAzumaya
---
+++ Mathlib/Algebra/Azumaya/Matrix.lean
@@ -0,0 +1,61 @@
+import Mathlib.Algebra.Azumaya.Defs
+import Mathlib.LinearAlgebra.Contraction
+
+open scoped TensorProduct
+
+variable (R n : Type*) [CommRing R] [Fintype n] [DecidableEq n]
+
+noncomputable section
+
+open Matrix MulOpposite
+
+/-- `AlgHom.mulLeftRight` for matrix algebra sends basis Eᵢⱼ⊗Eₖₗ to
+  the map `f : Eₛₜ ↦ Eᵢⱼ * Eₛₜ * Eₖₗ = δⱼₛδₜₖEᵢₗ`, therefore we construct the inverse
+  by sending `f` to `f(Eⱼₖ)ᵢₗ • Eᵢⱼ⊗Eₖₗ`. -/
+abbrev AlgHom.mulLeftRightMatrix_inv: Module.End R (Matrix n n R) →ₗ[R]
+    Matrix n n R ⊗[R] (Matrix n n R)ᵐᵒᵖ where
+  toFun := fun f ↦ ∑ ⟨⟨i, j⟩, k, l⟩ : (n × n) × n × n,
+    f (stdBasisMatrix j k 1) i l • (stdBasisMatrix i j 1) ⊗ₜ[R] op (stdBasisMatrix k l 1)
+  map_add' := fun f1 f2 ↦ by simp [add_smul, Finset.sum_add_distrib]
+  map_smul' := fun r f ↦ by simp [MulAction.mul_smul, Finset.smul_sum]
+
+lemma AlgHom.mulLeftRightMatrix.inv_comp:
+    (AlgHom.mulLeftRightMatrix_inv R n).comp
+    (AlgHom.mulLeftRight R (Matrix n n R)).toLinearMap = .id :=
+  Basis.ext (Basis.tensorProduct (Matrix.stdBasis _ _ _)
+    ((Matrix.stdBasis _ _ _).map (opLinearEquiv ..)))
+  fun ⟨⟨i0, j0⟩, k0, l0⟩ ↦ by
+    simp [stdBasis_eq_stdBasisMatrix, ite_and, Fintype.sum_prod_type,
+      mulLeftRight_apply, stdBasisMatrix, Matrix.mul_apply]
+
+lemma AlgHom.mulLeftRightMatrix.comp_inv:
+    (AlgHom.mulLeftRight R (Matrix n n R)).toLinearMap.comp
+    (AlgHom.mulLeftRightMatrix_inv R n) = .id := by
+  ext f : 1
+  apply Basis.ext (Matrix.stdBasis _ _ _)
+  intro ⟨i, j⟩
+  simp [AlgHom.mulLeftRight_apply, stdBasis_eq_stdBasisMatrix]
+  ext k l
+  simp [sum_apply, Matrix.mul_apply, Finset.sum_mul, Finset.mul_sum, stdBasisMatrix,
+    Fintype.sum_prod_type, ite_and]
+
+instance [Inhabited n]: FaithfulSMul R (Matrix n n R) where
+  eq_of_smul_eq_smul {r1 r2} h := by
+    specialize h 1
+    rw [← Matrix.ext_iff] at h
+    specialize h default default
+    simp only [Matrix.smul_apply, Matrix.one_apply_eq, smul_eq_mul, mul_one] at h
+    exact h
+
+namespace IsAzumaya
+
+/-- Matrix ring over `R` is an azumaya algebra over `R`. -/
+theorem Matrix [Inhabited n]: IsAzumaya R (Matrix n n R) where
+  bij := ⟨Function.HasLeftInverse.injective ⟨AlgHom.mulLeftRightMatrix_inv R n,
+    DFunLike.congr_fun (AlgHom.mulLeftRightMatrix.inv_comp R n)⟩,
+    Function.HasRightInverse.surjective ⟨AlgHom.mulLeftRightMatrix_inv R n,
+    DFunLike.congr_fun (AlgHom.mulLeftRightMatrix.comp_inv R n)⟩⟩
+
+end IsAzumaya
+
+end
