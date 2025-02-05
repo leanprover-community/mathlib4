c21c46d8f0198298eaa09fb4df62db62cdd6ab75
Whysoserioushah <yx3021@ic.ac.uk>
2025-02-05 11:12:59 +0000

    apply andrew suggestion

--- Mathlib/Algebra/Azumaya/Basic.lean
+++ Mathlib/Algebra/Azumaya/Basic.lean
@@ -33,6 +33,10 @@ namespace IsAzumaya
 
 variable (R A B : Type*) [CommRing R] [Ring A] [Ring B] [Algebra R A] [Algebra R B]
 
+
+lemma AlgHom.mulLeftRight_bij (h : IsAzumaya R A) :
+    Function.Bijective (AlgHom.mulLeftRight R A) := h.bij
+
 /-- The "canonical" isomorphism between `R ⊗ Rᵒᵖ` and `End R R` which is equal
   to `AlgHom.mulLeftRight R R`. -/
 noncomputable abbrev tensorEquivEnd : R ⊗[R] Rᵐᵒᵖ ≃ₐ[R] Module.End R R :=
@@ -59,7 +63,7 @@ End R A   ------------> End R B
           e.conj
 ```
 -/
-lemma small_comm_square (e : A ≃ₐ[R] B):
+lemma mulLeftRight_comp_congr (e : A ≃ₐ[R] B):
     (AlgHom.mulLeftRight R B).comp (Algebra.TensorProduct.congr e e.op).toAlgHom =
     (e.toLinearEquiv.algConj).toAlgHom.comp (AlgHom.mulLeftRight R A) := by
   apply AlgHom.ext
@@ -80,13 +84,13 @@ lemma _root_.FaithfulSMul.ofAlgEquiv [FaithfulSMul R A] (e : A ≃ₐ[R] B) :
       rw [← one_mul a, ← smul_mul_assoc, e.injective h12, smul_mul_assoc]
     exact eq_of_smul_eq_smul this
 
-theorem ofAlgEquiv (e : A ≃ₐ[R] B) (hA : IsAzumaya R A) : IsAzumaya R B :=
+theorem of_AlgEquiv (e : A ≃ₐ[R] B) [IsAzumaya R A] : IsAzumaya R B :=
   let _ : Module.Projective R B := .of_equiv e.toLinearEquiv
   let _ : FaithfulSMul R B := .ofAlgEquiv e
   let _ : Module.Finite R B := .equiv e.toLinearEquiv
   ⟨Function.Bijective.of_comp_iff (AlgHom.mulLeftRight R B)
     (Algebra.TensorProduct.congr e e.op).bijective |>.1 <| by
-    erw [← AlgHom.coe_comp, small_comm_square]
-    simp [hA.bij]⟩
+    erw [← AlgHom.coe_comp, mulLeftRight_comp_congr]
+    simp [AlgHom.mulLeftRight_bij]⟩
 
 end IsAzumaya
