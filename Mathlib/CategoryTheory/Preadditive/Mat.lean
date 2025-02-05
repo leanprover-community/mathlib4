6aadd7f3ee513f64143679175b6131b36822e1af
Anne Baanen <vierkantor@vierkantor.com>
2025-02-05 17:15:11 +0000

    refactor(CategoryTheory): `ConcreteCategory` instance for `FintypeCat` (#21466)
    Upgrade the `HasForget` instance on `FintypeCat` to a `ConcreteCategory`.

--- Mathlib/CategoryTheory/Preadditive/Mat.lean
+++ Mathlib/CategoryTheory/Preadditive/Mat.lean
@@ -497,7 +497,7 @@ instance (R : Type u) : Inhabited (Mat R) := by
   infer_instance
 
 instance (R : Type u) : CoeSort (Mat R) (Type u) :=
-  Bundled.coeSort
+  FintypeCat.instCoeSort
 
 open Matrix
 
@@ -580,7 +580,7 @@ instance : (equivalenceSingleObjInverse R).Full where
 instance : (equivalenceSingleObjInverse R).EssSurj where
   mem_essImage X :=
     ⟨{  ι := X
-        X := fun _ => PUnit.unit }, ⟨eqToIso (by dsimp; cases X; congr)⟩⟩
+        X := fun _ => PUnit.unit }, ⟨eqToIso (by cases X; congr)⟩⟩
 
 instance : (equivalenceSingleObjInverse R).IsEquivalence where
 
