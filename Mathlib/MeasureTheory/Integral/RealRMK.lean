375fd5ec0a2290dc25c7ffbb2ab94c557ae9976b
Yoh Tanimoto <hoyt@jcom.home.ne.jp>
2025-02-06 01:07:38 +0100

    alphabetical

--- Mathlib/MeasureTheory/Integral/RealRMK.lean
+++ Mathlib/MeasureTheory/Integral/RealRMK.lean
@@ -3,9 +3,8 @@ Copyright (c) 2024 Yoh Tanimioto. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yoh Tanimoto
 -/
-
-import Mathlib.MeasureTheory.Integral.SetIntegral
 import Mathlib.MeasureTheory.Integral.RieszMarkovKakutani
+import Mathlib.MeasureTheory.Integral.SetIntegral
 
 /-!
 #  Riesz–Markov–Kakutani representation theorem for `Real`
