5dc2910844a85a6b16f86ae7d5b647e5502695e1
Markus Himmel <markus@himmel-villmar.de>
2025-02-06 11:52:21 +0000

    feat: the category of ind-objects satisfies the AB5 axiom (#21350)
    Co-authored-by: Markus Himmel <markus@lean-fro.org>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1660,7 +1660,9 @@ import Mathlib.CategoryTheory.Abelian.Ext
 import Mathlib.CategoryTheory.Abelian.FunctorCategory
 import Mathlib.CategoryTheory.Abelian.GrothendieckAxioms.Basic
 import Mathlib.CategoryTheory.Abelian.GrothendieckAxioms.FunctorCategory
+import Mathlib.CategoryTheory.Abelian.GrothendieckAxioms.Indization
 import Mathlib.CategoryTheory.Abelian.GrothendieckAxioms.Sheaf
+import Mathlib.CategoryTheory.Abelian.GrothendieckAxioms.Types
 import Mathlib.CategoryTheory.Abelian.GrothendieckCategory
 import Mathlib.CategoryTheory.Abelian.Images
 import Mathlib.CategoryTheory.Abelian.Injective
