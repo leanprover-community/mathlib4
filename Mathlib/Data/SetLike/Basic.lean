30f740d46060045e26dc61b2a0b87ac3b07c9a2d
Junyan Xu <junyanxu.math@gmail.com>
2025-02-06 06:55:10 -0500

    experiment: make SetLike.coe reducible

--- Mathlib/Data/SetLike/Basic.lean
+++ Mathlib/Data/SetLike/Basic.lean
@@ -98,7 +98,7 @@ class SetLike (A : Type*) (B : outParam Type*) where
   /-- The coercion from a term of a `SetLike` to its corresponding `Set` is injective. -/
   protected coe_injective' : Function.Injective coe
 
-attribute [coe] SetLike.coe
+attribute [coe, reducible, inline] SetLike.coe
 namespace SetLike
 
 variable {A : Type*} {B : Type*} [i : SetLike A B]
