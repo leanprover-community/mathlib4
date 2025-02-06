54fcfc3466c29fcc319719c137beefd095280b79
Pim Otte <p.j.otte@uu.nl>
2025-02-06 17:25:48 +0100

    Fixes

--- Mathlib/Combinatorics/SimpleGraph/Connectivity/Represents.lean
+++ Mathlib/Combinatorics/SimpleGraph/Connectivity/Represents.lean
@@ -30,16 +30,16 @@ each component. -/
 def Represents (s : Set V) (C : Set G.ConnectedComponent) : Prop :=
     Set.BijOn G.connectedComponentMk s C
 
-lemma out_image_represents {G : SimpleGraph V} (C : Set G.ConnectedComponent) :
+namespace Represents
+
+variable {C : Set G.ConnectedComponent} {s : Set V} {c : G.ConnectedComponent}
+
+lemma image_out (C : Set G.ConnectedComponent) :
     Represents (Quot.out '' C) C :=
   Set.BijOn.mk (by rintro c ⟨x, ⟨hx, rfl⟩⟩; simp_all [connectedComponentMk]) (by
     rintro x ⟨c, ⟨hc, rfl⟩⟩ y ⟨d, ⟨hd, rfl⟩⟩ hxy
     simp only [connectedComponentMk] at hxy
     aesop) (fun _ _ ↦ by simpa [connectedComponentMk])
-
-namespace Represents
-
-variable {C : Set G.ConnectedComponent} {s : Set V} {c : G.ConnectedComponent}
 
 lemma existsUnique_rep (hrep : Represents s C) (h : c ∈ C) : ∃! x, x ∈ s ∩ c.supp := by
   obtain ⟨x, ⟨hx, rfl⟩⟩ := hrep.2.2 h
@@ -56,7 +56,7 @@ lemma disjoint_supp_of_not_mem (hrep : Represents s C) (h : c ∉ C) : Disjoint 
 
 lemma ncard_inter (hrep : Represents s C) (h : c ∈ C) : (s ∩ c.supp).ncard = 1 := by
   rw [Set.ncard_eq_one]
-  obtain ⟨a, ha⟩ := hrep.unique_rep h
+  obtain ⟨a, ha⟩ := hrep.existsUnique_rep h
   aesop
 
 lemma ncard_sdiff_of_mem [Fintype V] (hrep : Represents s C) (h : c ∈ C) :
@@ -67,7 +67,7 @@ lemma ncard_sdiff_of_mem [Fintype V] (hrep : Represents s C) (h : c ∈ C) :
 lemma ncard_sdiff_of_not_mem [Fintype V] (hrep : Represents s C) (h : c ∉ C) :
     (c.supp \ s).ncard = c.supp.ncard := by
   simp [← Set.ncard_inter_add_ncard_diff_eq_ncard c.supp s (Set.toFinite _), Set.inter_comm,
-    Set.disjoint_iff_inter_eq_empty.mp (hrep.exact h)]
+    Set.disjoint_iff_inter_eq_empty.mp (hrep.disjoint_supp_of_not_mem h)]
 
 end Represents
 
