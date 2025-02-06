8ca0c3169fd1ff0340b32f20880570a8826e64fb
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-02-06 14:54:09 +0100

    fixing imports

--- Mathlib/Data/Fin/SuccPred.lean
+++ Mathlib/Data/Fin/SuccPred.lean
@@ -3,7 +3,6 @@ Copyright (c) 2022 Eric Rodriguez. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Eric Rodriguez
 -/
-import Mathlib.Algebra.Group.Fin.Basic
 import Mathlib.Order.Fin.Basic
 import Mathlib.Order.SuccPred.Basic
 
