f017b0afef37bd1adddf55cb73927f764c840ea4
jt496 <john.talbot@gmail.com>
2025-02-06 09:47:56 +0000

    add length_rotate

--- Mathlib/Combinatorics/SimpleGraph/Walk.lean
+++ Mathlib/Combinatorics/SimpleGraph/Walk.lean
@@ -1107,6 +1107,10 @@ theorem rotate_edges {u v : V} (c : G.Walk v v) (h : u ∈ c.support) :
     (c.rotate h).edges ~r c.edges :=
   (rotate_darts c h).map _
 
+theorem length_rotate {u v : V} (c : G.Walk v v) (h : u ∈ c.support) :
+    (c.rotate h).length = c.length :=by
+  rw [rotate, length_append,add_comm, ← length_append, take_spec]
+
 end WalkDecomp
 
 /-- Given a set `S` and a walk `w` from `u` to `v` such that `u ∈ S` but `v ∉ S`,
