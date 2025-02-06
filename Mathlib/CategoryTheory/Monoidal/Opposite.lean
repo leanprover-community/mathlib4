202227e90a964d383620a4942bb38cb08c645d9a
Jad Ghalayini <jad.ghalayini@protonmail.com>
2025-02-06 18:53:46 +0000

    Unused dependency

--- Mathlib/CategoryTheory/Monoidal/Opposite.lean
+++ Mathlib/CategoryTheory/Monoidal/Opposite.lean
@@ -3,7 +3,7 @@ Copyright (c) 2020 Kim Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kim Morrison
 -/
-import Mathlib.Tactic.CategoryTheory.Monoidal.PureCoherence
+import Mathlib.CategoryTheory.Monoidal.Category
 
 /-!
 # Monoidal opposites
