e98fdb54ec00120141834522e634da00f2b2a1f2
Christian Merten <christian@merten.dev>
2025-02-06 13:44:35 +0100

    shake

--- Mathlib/RingTheory/Spectrum/Prime/RingHom.lean
+++ Mathlib/RingTheory/Spectrum/Prime/RingHom.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johan Commelin, Filippo A. E. Nuccio, Andrew Yang
 -/
 import Mathlib.RingTheory.Spectrum.Prime.Basic
-import Mathlib.RingTheory.Localization.AtPrime
 
 /-!
 # Functoriality of the prime spectrum
