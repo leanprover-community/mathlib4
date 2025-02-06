5b88059d3d2c905c108b45f533a65c92a7dbb667
101damnations <101damnations@github.com>
2025-02-06 15:27:11 +0000

    tidy

--- Mathlib/RepresentationTheory/Homological/GroupCohomology/Functoriality.lean
+++ Mathlib/RepresentationTheory/Homological/GroupCohomology/Functoriality.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2023 Amelia Livingston. All rights reserved.
+Copyright (c) 2025 Amelia Livingston. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Amelia Livingston
 -/
@@ -8,69 +8,32 @@ import Mathlib.RepresentationTheory.Homological.GroupCohomology.LowDegree
 
 /-!
 # Functoriality of group cohomology
+
 Given a commutative ring `k`, a group homomorphism `f : G →* H`, a `k`-linear `H`-representation
 `A`, a `k`-linear `G`-representation `B`, and a representation morphism `Res(f)(A) ⟶ B`, we get
 a cochain map `inhomogeneousCochains A ⟶ inhomogeneousCochains B` and hence maps on
-cohomology `Hⁿ(H, A) ⟶ Hⁿ(G, B)`. We use this to show a short exact sequence of representations
-induces a short exact sequence of complexes of inhomogeneous cochains, allowing us to specialize
-API for long exact sequences to group cohomology.
-We also provide extra API for these functoriality maps in degrees 0, 1, 2.
+cohomology `Hⁿ(H, A) ⟶ Hⁿ(G, B)`.
+We also provide extra API for these maps in degrees 0, 1, 2.
+
 ## Main definitions
-* `groupCohomology.cochainsMap f φ` is the map
-`inhomogeneousCochains A ⟶ inhomogeneousCochains B`
+
+* `groupCohomology.cochainsMap f φ` is the map `inhomogeneousCochains A ⟶ inhomogeneousCochains B`
 induced by a group homomorphism `f : G →* H` and a representation morphism `φ : Res(f)(A) ⟶ B`.
 * `groupCohomology.map f φ n` is the map `Hⁿ(H, A) ⟶ Hⁿ(G, B)` induced by a group
 homomorphism `f : G →* H` and a representation morphism `φ : Res(f)(A) ⟶ B`.
+
 -/
 
 universe v u
-variable (n : ℕ)
-
-open CategoryTheory
-
-lemma Fin.comp_contractNth {G H : Type*} [MulOneClass G] [MulOneClass H] (f : G →* H)
-    (j : Fin (n + 1)) (g : Fin (n + 1) → G) :
-    f ∘ Fin.contractNth j (· * ·) g = Fin.contractNth j (· * ·) (f ∘ g) := by
-  ext x
-  rcases lt_trichotomy (x : ℕ) j with (h|h|h)
-  · simp only [Function.comp_apply, Fin.contractNth_apply_of_lt, h]
-  · simp only [Function.comp_apply, Fin.contractNth_apply_of_eq, h, f.map_mul]
-  · simp only [Function.comp_apply, Fin.contractNth_apply_of_gt, h]
-
-namespace LinearMap
-
-lemma ker_compLeft
-    {R M N : Type*} [Semiring R] [AddCommMonoid M] [AddCommMonoid N]
-    [Module R M] [Module R N] (f : M →ₗ[R] N) (I : Type*) :
-    LinearMap.ker (f.compLeft I) = Submodule.pi (Set.univ : Set I) (fun _ => LinearMap.ker f) :=
-  Submodule.ext fun _ => ⟨fun (hx : _ = _) i _ => congr_fun hx i,
-    fun hx => funext fun i => hx i trivial⟩
-
-lemma range_compLeft
-    {R M N : Type*} [Semiring R] [AddCommMonoid M] [AddCommMonoid N]
-    [Module R M] [Module R N] (f : M →ₗ[R] N) (I : Type*) :
-    LinearMap.range (f.compLeft I) = Submodule.pi (Set.univ : Set I) (fun _ => LinearMap.range f) :=
-  Submodule.ext fun _ => ⟨fun ⟨y, hy⟩ i _ => ⟨y i, congr_fun hy i⟩, fun hx => by
-    choose y hy using hx
-    exact ⟨fun i => y i trivial, funext fun i => hy i trivial⟩⟩
-
-end LinearMap
-namespace ModuleCat
-
-variable (R : Type u) [Ring R]
-
-end ModuleCat
 
 namespace groupCohomology
-open Rep
+open Rep CategoryTheory Representation
 
 variable {k G H : Type u} [CommRing k] [Group G] [Group H]
   {A : Rep k H} {B : Rep k G} (f : G →* H) (φ : (Action.res _ f).obj A ⟶ B) (n : ℕ)
 
-open Representation
-
 /-- Given a group homomorphism `f : G →* H` and a representation morphism `φ : Res(f)(A) ⟶ B`,
-this is the chain map sending `x : (Fin n → H) → A)` to `(g : Fin n → G) ↦ φ (x (f ∘ g))`. -/
+this is the chain map sending `x : Hⁿ → A)` to `(g : Gⁿ) ↦ φ (x (f ∘ g))`. -/
 @[simps! (config := .lemmasOnly) f f_hom]
 noncomputable def cochainsMap :
     inhomogeneousCochains A ⟶ inhomogeneousCochains B where
@@ -88,12 +51,11 @@ lemma cochainsMap_id :
   rfl
 
 @[simp]
-lemma cochainsMap_id_eq_compLeft {A B : Rep k G} (f : A ⟶ B) (i : ℕ) :
+lemma cochainsMap_id_f_eq_compLeft {A B : Rep k G} (f : A ⟶ B) (i : ℕ) :
     (cochainsMap (MonoidHom.id G) f).f i = ModuleCat.ofHom (f.hom.hom.compLeft _) := by
   ext
   rfl
 
-@[simp]
 lemma cochainsMap_comp {G H K : Type u} [Group G] [Group H] [Group K]
     {A : Rep k K} {B : Rep k H} {C : Rep k G} (f : H →* K) (g : G →* H)
     (φ : (Action.res _ f).obj A ⟶ B) (ψ : (Action.res _ g).obj B ⟶ C) :
@@ -101,7 +63,6 @@ lemma cochainsMap_comp {G H K : Type u} [Group G] [Group H] [Group K]
       cochainsMap f φ ≫ cochainsMap g ψ := by
   rfl
 
-@[simp]
 lemma cochainsMap_id_comp {A B C : Rep k G} (φ : A ⟶ B) (ψ : B ⟶ C) :
     cochainsMap (MonoidHom.id G) (φ ≫ ψ) =
       cochainsMap (MonoidHom.id G) φ ≫ cochainsMap (MonoidHom.id G) ψ := by
@@ -131,26 +92,24 @@ instance cochainsMap_id_f_map_epi {A B : Rep k G} (φ : A ⟶ B) [Epi φ] (i : �
   cochainsMap_f_map_epi (MonoidHom.id G) φ (fun _ _ h => h) i
 
 /-- Given a group homomorphism `f : G →* H` and a representation morphism `φ : Res(f)(A) ⟶ B`,
-this is the induced map `Zⁿ(H, A) ⟶ Zⁿ(G, B)` sending `x : (Fin n → H) → A)` to
-`(g : Fin n → G) ↦ φ (x (f ∘ g))`. -/
+this is the induced map `Zⁿ(H, A) ⟶ Zⁿ(G, B)` sending `x : Hⁿ → A` to
+`(g : Gⁿ) ↦ φ (x (f ∘ g))`. -/
 noncomputable abbrev cocyclesMap (n : ℕ) :
     groupCohomology.cocycles A n ⟶ groupCohomology.cocycles B n :=
   HomologicalComplex.cyclesMap (cochainsMap f φ) n
 
-@[simp]
 theorem cocyclesMap_id_comp {A B C : Rep k G} (φ : A ⟶ B) (ψ : B ⟶ C) (n : ℕ) :
     cocyclesMap (MonoidHom.id G) (φ ≫ ψ) n =
       cocyclesMap (MonoidHom.id G) φ n ≫ cocyclesMap (MonoidHom.id G) ψ n := by
   simp [cocyclesMap, cochainsMap_id_comp, HomologicalComplex.cyclesMap_comp]
 
 /-- Given a group homomorphism `f : G →* H` and a representation morphism `φ : Res(f)(A) ⟶ B`,
-this is the induced map `Hⁿ(H, A) ⟶ Hⁿ(G, B)` sending `x : (Fin n → H) → A)` to
-`(g : Fin n → G) ↦ φ (x (f ∘ g))`. -/
+this is the induced map `Hⁿ(H, A) ⟶ Hⁿ(G, B)` sending `x : Hⁿ → A` to
+`(g : Gⁿ) ↦ φ (x (f ∘ g))`. -/
 noncomputable abbrev map (n : ℕ) :
     groupCohomology A n ⟶ groupCohomology B n :=
   HomologicalComplex.homologyMap (cochainsMap f φ) n
 
-@[simp]
 theorem map_id_comp {A B C : Rep k G} (φ : A ⟶ B) (ψ : B ⟶ C) (n : ℕ) :
     map (MonoidHom.id G) (φ ≫ ψ) n =
       map (MonoidHom.id G) φ n ≫ map (MonoidHom.id G) ψ n := by
@@ -158,20 +117,23 @@ theorem map_id_comp {A B C : Rep k G} (φ : A ⟶ B) (ψ : B ⟶ C) (n : ℕ) :
 
 /-- Given a group homomorphism `f : G →* H` and a representation morphism `φ : Res(f)(A) ⟶ B`,
 this is the induced map sending `x : H → A` to `(g : G) ↦ φ (x (f g))`. -/
-abbrev fOne := φ.hom.hom.compLeft G ∘ₗ LinearMap.funLeft k A f
+abbrev fOne : (H → A) →ₗ[k] (G → B) := φ.hom.hom.compLeft G ∘ₗ LinearMap.funLeft k A f
 
 /-- Given a group homomorphism `f : G →* H` and a representation morphism `φ : Res(f)(A) ⟶ B`,
 this is the induced map sending `x : H × H → A` to `(g₁, g₂ : G × G) ↦ φ (x (f g₁, f g₂))`. -/
-abbrev fTwo := φ.hom.hom.compLeft (G × G) ∘ₗ LinearMap.funLeft k A (Prod.map f f)
+abbrev fTwo : (H × H → A) →ₗ[k] (G × G → B) :=
+  φ.hom.hom.compLeft (G × G) ∘ₗ LinearMap.funLeft k A (Prod.map f f)
 
 /-- Given a group homomorphism `f : G →* H` and a representation morphism `φ : Res(f)(A) ⟶ B`,
 this is the induced map sending `x : H × H × H → A` to
 `(g₁, g₂, g₃ : G × G × G) ↦ φ (x (f g₁, f g₂, f g₃))`. -/
-abbrev fThree := φ.hom.hom.compLeft (G × G × G) ∘ₗ LinearMap.funLeft k A (Prod.map f (Prod.map f f))
+abbrev fThree : (H × H × H → A) →ₗ[k] (G × G × G → B) :=
+  φ.hom.hom.compLeft (G × G × G) ∘ₗ LinearMap.funLeft k A (Prod.map f (Prod.map f f))
 
 @[reassoc (attr := simp)]
 lemma cochainsMap_f_0_comp_zeroCochainsLequiv :
-    (cochainsMap f φ).f 0 ≫ (zeroCochainsLequiv B).toModuleIso.hom =
+    (cochainsMap f φ).f 0 ≫
+      ModuleCat.ofHom (X := (inhomogeneousCochains B).X 0) (Y := B) (zeroCochainsLequiv B) =
       (zeroCochainsLequiv A).toModuleIso.hom ≫ φ.hom := by
   ext x
   simp only [cochainsMap_f, Unique.eq_default (f ∘ _)]
@@ -179,7 +141,8 @@ lemma cochainsMap_f_0_comp_zeroCochainsLequiv :
 
 @[reassoc (attr := simp)]
 lemma cochainsMap_f_1_comp_oneCochainsLequiv :
-    (cochainsMap f φ).f 1 ≫ (oneCochainsLequiv B).toModuleIso.hom =
+    (cochainsMap f φ).f 1 ≫
+      ModuleCat.ofHom (X := (inhomogeneousCochains B).X 1) (Y := G → B) (oneCochainsLequiv B) =
       (oneCochainsLequiv A).toModuleIso.hom ≫ ModuleCat.ofHom (fOne f φ) := by
   ext x
   simp only [cochainsMap_f, Unique.eq_default (f ∘ _)]
@@ -187,7 +150,8 @@ lemma cochainsMap_f_1_comp_oneCochainsLequiv :
 
 @[reassoc (attr := simp)]
 lemma cochainsMap_f_2_comp_twoCochainsLequiv :
-    (cochainsMap f φ).f 2 ≫ (twoCochainsLequiv B).toModuleIso.hom =
+    (cochainsMap f φ).f 2 ≫
+      ModuleCat.ofHom (X := (inhomogeneousCochains B).X 2) (Y := G × G → B) (twoCochainsLequiv B) =
       (twoCochainsLequiv A).toModuleIso.hom ≫ ModuleCat.ofHom (fTwo f φ) := by
   ext x g
   show φ.hom (x _) = φ.hom (x _)
@@ -196,7 +160,8 @@ lemma cochainsMap_f_2_comp_twoCochainsLequiv :
 
 @[reassoc (attr := simp)]
 lemma cochainsMap_f_3_comp_threeCochainsLequiv :
-    (cochainsMap f φ).f 3 ≫ (threeCochainsLequiv B).toModuleIso.hom =
+    (cochainsMap f φ).f 3 ≫ ModuleCat.ofHom (X := (inhomogeneousCochains B).X 3)
+      (Y := G × G × G → B) (threeCochainsLequiv B) =
       (threeCochainsLequiv A).toModuleIso.hom ≫ ModuleCat.ofHom (fThree f φ) := by
   ext x g
   show φ.hom (x _) = φ.hom (x _)
