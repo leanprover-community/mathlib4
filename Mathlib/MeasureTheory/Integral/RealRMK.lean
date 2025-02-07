fbcae93a183f61e280006660f63e7a00f618a26b
Yoh Tanimoto <57562556+yoh-tanimoto@users.noreply.github.com>
2025-02-07 15:23:02 +0100

    Update Mathlib/MeasureTheory/Integral/RealRMK.lean
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- Mathlib/MeasureTheory/Integral/RealRMK.lean
+++ Mathlib/MeasureTheory/Integral/RealRMK.lean
@@ -14,7 +14,7 @@ T2 space `X` for `Real`-linear functionals `Λ`.
 
 The measure is first defined through `rieszContent` for `toNNRealLinear`-version of `Λ`.
 The result is first proved for `Real`-linear `Λ` because in a standard proof one has to prove the
-inequalities by `le_antisymm`, yet for `C_c(X, ℝ≥0)` there is no `Neg`.
+inequalities by considering `Λ f` and `Λ (-f)` for all functions `f`, yet on `C_c(X, ℝ≥0)` there is no negation.
 
 ## References
 
