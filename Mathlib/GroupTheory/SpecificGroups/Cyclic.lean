b417cc88af9dfacd51257a6ac003496f9c893e19
mariainesdff <mariaines.dff@gmail.com>
2025-02-07 13:44:21 +0100

    suggested change

--- Mathlib/GroupTheory/SpecificGroups/Cyclic.lean
+++ Mathlib/GroupTheory/SpecificGroups/Cyclic.lean
@@ -56,7 +56,7 @@ theorem isCyclic_iff_exists_zpowers_eq_top [Group α] : IsCyclic α ↔ ∃ g : 
 @[to_additive]
 protected theorem Subgroup.isCyclic_iff_exists_zpowers_eq_top [Group α] (H : Subgroup α) :
     IsCyclic H ↔ ∃ g : α, Subgroup.zpowers g = H := by
-  rw [_root_.isCyclic_iff_exists_zpowers_eq_top]
+  rw [isCyclic_iff_exists_zpowers_eq_top]
   simp_rw [← (map_injective H.subtype_injective).eq_iff, ← MonoidHom.range_eq_map,
     H.range_subtype, MonoidHom.map_zpowers, Subtype.exists, coeSubtype, exists_prop]
   exact exists_congr fun g ↦ and_iff_right_of_imp fun h ↦ h ▸ mem_zpowers g
