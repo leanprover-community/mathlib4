8bc669b6a0aae61772f48b4e35963cb9c584b0a3
Jiang Jiedong <107380768+jjdishere@users.noreply.github.com>
2025-02-08 04:11:39 +0800

    Update Mathlib.lean
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -4746,6 +4746,8 @@ import Mathlib.RingTheory.OreLocalization.OreSet
 import Mathlib.RingTheory.OreLocalization.Ring
 import Mathlib.RingTheory.OrzechProperty
 import Mathlib.RingTheory.Perfection
+import Mathlib.RingTheory.Perfectoid.FontaineTheta
+import Mathlib.RingTheory.Perfectoid.IntergalPerfectoid
 import Mathlib.RingTheory.Perfectoid.Untilt
 import Mathlib.RingTheory.PiTensorProduct
 import Mathlib.RingTheory.Polynomial.Basic
