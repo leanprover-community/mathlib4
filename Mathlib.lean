46f70fddb851e8c7b5aa46082d1a70f3c7efc6eb
Andrew Yang <36414270+erdOne@users.noreply.github.com>
2025-02-07 14:40:49 +0000

    Update Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -1031,7 +1031,6 @@ import Mathlib.AlgebraicGeometry.Gluing
 import Mathlib.AlgebraicGeometry.GluingOneHypercover
 import Mathlib.AlgebraicGeometry.IdealSheaf
 import Mathlib.AlgebraicGeometry.Limits
-import Mathlib.AlgebraicGeometry.Liouville
 import Mathlib.AlgebraicGeometry.Modules.Presheaf
 import Mathlib.AlgebraicGeometry.Modules.Sheaf
 import Mathlib.AlgebraicGeometry.Modules.Tilde
