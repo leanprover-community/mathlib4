bd47360eef6d2c1917cc252d1bca7fafd946d0b2
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-02-07 15:17:37 +0000

    feat(Order): instances on Shrink (#21429)
    If `α : Type v` is `u`-small, we transport various order related instances on `α` to `Shrink.{u} α`.

--- Mathlib.lean
+++ Mathlib.lean
@@ -4355,6 +4355,7 @@ import Mathlib.Order.Restriction
 import Mathlib.Order.ScottContinuity
 import Mathlib.Order.SemiconjSup
 import Mathlib.Order.SetNotation
+import Mathlib.Order.Shrink
 import Mathlib.Order.Sublattice
 import Mathlib.Order.SuccPred.Archimedean
 import Mathlib.Order.SuccPred.Basic
