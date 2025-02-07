816cfddd3c6d3f44a330fd6c4aeb155fe1080c6b
Johan Commelin <johan@commelin.net>
2025-02-07 15:59:51 +0100

    chore(Algebra/Polynomial): process porting notes

--- Mathlib/Algebra/Polynomial/PartialFractions.lean
+++ Mathlib/Algebra/Polynomial/PartialFractions.lean
@@ -49,7 +49,6 @@ variable (K : Type) [Field K] [Algebra R[X] K] [IsFractionRing R[X] K]
 
 section TwoDenominators
 
--- Porting note: added for scoped `Algebra.cast` instance
 open algebraMap
 
 /-- Let R be an integral domain and f, g₁, g₂ ∈ R[X]. Let g₁ and g₂ be monic and coprime.
@@ -81,7 +80,6 @@ end TwoDenominators
 
 section NDenominators
 
--- Porting note: added for scoped `Algebra.cast` instance
 open algebraMap
 
 /-- Let R be an integral domain and f ∈ R[X]. Let s be a finite index set.
