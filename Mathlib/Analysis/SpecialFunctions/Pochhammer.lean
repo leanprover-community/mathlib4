a2cb3b77085a659543f3266d997404725a7c36e9
mitchell-horner <29882987+mitchell-horner@users.noreply.github.com>
2025-02-07 21:03:32 +1100

    Import Piecewise

--- Mathlib/Analysis/SpecialFunctions/Pochhammer.lean
+++ Mathlib/Analysis/SpecialFunctions/Pochhammer.lean
@@ -3,7 +3,7 @@ Copyright (c) 2025 Mitchell Horner. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mitchell Horner
 -/
-import Mathlib.Analysis.Convex.Deriv
+import Mathlib.Analysis.Convex.Piecewise
 import Mathlib.Data.Nat.Choose.Cast
 
 /-!
@@ -73,58 +73,36 @@ theorem convexOn_descPochhammer_eval (n : ℕ) :
     rw [interior_Ici]
     exact monotoneOn_deriv_descPochhammer_eval n
 
-/-- The function defined to be `0` for `(-∞, n-1)` and `descPochhammer ℝ n` for `[n-1, ∞)` is
-convex on the real line. -/
-lemma convexOn_ite_descPochhammer_eval (hn : 1 ≤ n) :
-    ConvexOn ℝ Set.univ (fun (x : ℝ) ↦ if n - 1 ≤ x then (descPochhammer ℝ n).eval x else 0) := by
-  refine ⟨convex_univ, fun x _ y _ a b ha hb hab ↦ ?_⟩
-  by_cases hx : n - 1 ≤ x <;> by_cases hy : n - 1 ≤ y
-  · have hcombo : n - 1 ≤ a*x + b*y := (le_min hx hy).trans (Convex.min_le_combo x y ha hb hab)
-    simpa [hcombo, hx, hy] using (convexOn_descPochhammer_eval n).2 hx hy ha hb hab
-  · have hcombo' : a*x + b*y ≤ a*x + b*(n - 1) :=
-      add_le_add_left (mul_le_mul_of_nonneg_left (le_of_not_le hy) hb) (a*x)
-    by_cases hcombo : n - 1 ≤ a*x + b*y
-    · trans (descPochhammer ℝ n).eval (a*x + b*(n - 1))
-      · simpa [hcombo] using monotoneOn_descPochhammer_eval n hcombo (hcombo.trans hcombo') hcombo'
-      · simpa [← descPochhammer_eval_coe_nat_of_lt (Nat.sub_one_lt_of_lt hn), Nat.cast_pred hn,
-          hx, hy] using (convexOn_descPochhammer_eval n).2 hx (le_refl (n - 1 : ℝ)) ha hb hab
-    · simpa [hcombo, hx, hy] using mul_nonneg ha (descPochhammer_nonneg hx)
-  · have hcombo' : a*x + b*y ≤ a*(n - 1) + b*y :=
-      add_le_add_right (mul_le_mul_of_nonneg_left (le_of_not_le hx) ha) (b*y)
-    by_cases hcombo : n - 1 ≤ a*x + b*y
-    · trans (descPochhammer ℝ n).eval (a*(n - 1) + b*y)
-      · simpa [hcombo] using monotoneOn_descPochhammer_eval n hcombo (hcombo.trans hcombo') hcombo'
-      · simpa [← descPochhammer_eval_coe_nat_of_lt (Nat.sub_one_lt_of_lt hn), Nat.cast_pred hn,
-          hx, hy] using (convexOn_descPochhammer_eval n).2 (le_refl (n - 1 : ℝ)) hy ha hb hab
-    · simpa [hcombo, hx, hy] using mul_nonneg hb (descPochhammer_nonneg hy)
-  · have hcombo : a*x + b*y ≤ n - 1 :=
-      (Convex.combo_le_max x y ha hb hab).trans (max_le (le_of_not_le hx) (le_of_not_le hy))
-    rcases lt_or_eq_of_le hcombo with hlt | heq
-    · simp [not_le_of_lt hlt, hx, hy]
-    · simpa [heq, hx, hy, Nat.cast_pred hn] using
-        (descPochhammer_eval_coe_nat_of_lt (Nat.sub_one_lt_of_lt hn)).le
-
-private lemma ite_descPochhammer_eval_eq_descFactorial (k n : ℕ) :
-    (if (n - 1 : ℝ) ≤ (k : ℝ) then (descPochhammer ℝ n).eval (k : ℝ) else 0)
+private lemma piecewise_Ici_descPochhammer_eval_zero_eq_descFactorial (k n : ℕ) :
+    (Set.Ici (n - 1 : ℝ)).piecewise (descPochhammer ℝ n).eval 0 k
       = k.descFactorial n := by
-  rw [descPochhammer_eval_eq_descFactorial ℝ k n, ite_eq_left_iff, not_le, eq_comm,
-    Nat.cast_eq_zero, Nat.descFactorial_eq_zero_iff_lt, ← @Nat.cast_lt ℝ]
+  rw [Set.piecewise, descPochhammer_eval_eq_descFactorial, ite_eq_left_iff, Set.mem_Ici, not_le,
+    eq_comm, Pi.zero_apply, Nat.cast_eq_zero, Nat.descFactorial_eq_zero_iff_lt, ← @Nat.cast_lt ℝ]
   exact (sub_lt_self (n : ℝ) zero_lt_one).trans'
+
+private lemma convexOn_piecewise_Ici_descPochhammer_eval_zero (hn : n ≠ 0) :
+    ConvexOn ℝ Set.univ ((Set.Ici (n - 1 : ℝ)).piecewise (descPochhammer ℝ n).eval 0) := by
+  rw [← Nat.pos_iff_ne_zero] at hn
+  apply convexOn_univ_piecewise_Ici_of_monotoneOn_Ici_antitoneOn_Iic
+    (convexOn_descPochhammer_eval n) (convexOn_const 0 (convex_Iic (n - 1 : ℝ)))
+    (monotoneOn_descPochhammer_eval n) antitoneOn_const
+  simpa [← Nat.cast_pred hn] using descPochhammer_eval_coe_nat_of_lt (Nat.sub_one_lt_of_lt hn)
 
 /-- Special case of **Jensen's inequality** for `Nat.descFactorial`. -/
 theorem descPochhammer_eval_le_sum_descFactorial
-    (hn : 1 ≤ n) {ι : Type*} {t : Finset ι} (p : ι → ℕ) (w : ι → ℝ)
+    (hn : n ≠ 0) {ι : Type*} {t : Finset ι} (p : ι → ℕ) (w : ι → ℝ)
     (h₀ : ∀ i ∈ t, 0 ≤ w i) (h₁ : ∑ i ∈ t, w i = 1) (h_avg : n - 1 ≤ ∑ i ∈ t, w i * p i) :
     (descPochhammer ℝ n).eval (∑ i ∈ t, w i * p i)
       ≤ ∑ i ∈ t, w i * (p i).descFactorial n := by
-  let f : ℝ → ℝ := fun (x : ℝ) ↦ if n - 1 ≤ x then (descPochhammer ℝ n).eval x else 0
+  let f : ℝ → ℝ := (Set.Ici (n - 1 : ℝ)).piecewise (descPochhammer ℝ n).eval 0
   suffices h_jensen : f (∑ i ∈ t, w i • p i) ≤ ∑ i ∈ t, w i • f (p i) by
-    simpa only [smul_eq_mul, f, h_avg, ite_descPochhammer_eval_eq_descFactorial] using h_jensen
-  exact ConvexOn.map_sum_le (convexOn_ite_descPochhammer_eval hn) h₀ h₁ (by simp)
+    simpa only [smul_eq_mul, f, Set.piecewise_eq_of_mem (Set.Ici (n - 1 : ℝ)) _ _ h_avg,
+      piecewise_Ici_descPochhammer_eval_zero_eq_descFactorial] using h_jensen
+  exact ConvexOn.map_sum_le (convexOn_piecewise_Ici_descPochhammer_eval_zero hn) h₀ h₁ (by simp)
 
 /-- Special case of **Jensen's inequality** for `Nat.choose`. -/
 theorem descPochhammer_eval_div_factorial_le_sum_choose
-    (hn : 1 ≤ n) {ι : Type*} {t : Finset ι} (p : ι → ℕ) (w : ι → ℝ)
+    (hn : n ≠ 0) {ι : Type*} {t : Finset ι} (p : ι → ℕ) (w : ι → ℝ)
     (h₀ : ∀ i ∈ t, 0 ≤ w i) (h₁ : ∑ i ∈ t, w i = 1) (h_avg : n - 1 ≤ ∑ i ∈ t, w i * p i) :
     (descPochhammer ℝ n).eval (∑ i ∈ t, w i * p i) / n.factorial
       ≤ ∑ i ∈ t, w i * (p i).choose n := by
