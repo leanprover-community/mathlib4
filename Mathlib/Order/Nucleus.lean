f45f5d8f6214e985f1ee1a902613e778795b35f6
Bergschaf <christian.k@netcom-mail.de>
2025-02-07 19:31:34 +0100

    style

--- Mathlib/Order/Nucleus.lean
+++ Mathlib/Order/Nucleus.lean
@@ -136,31 +136,31 @@ instance : CompleteSemilatticeInf (Nucleus X) where
 
 instance : CompleteLattice (Nucleus X) := completeLatticeOfCompleteSemilatticeInf (Nucleus X)
 
-@[simp] theorem Min_apply (a b : Nucleus X) (x : X) : (a ⊓ b) x = a x ⊓ b x := by
+@[simp] theorem inf_aply (m n : Nucleus X) (x : X) : (m ⊓ n) x = m x ⊓ n x := by
   rw [← sInf_pair, sInf_apply, iInf_pair]
 
 instance : HImp (Nucleus X) where
-  himp a b :=
-  { toFun x :=   ⨅ y ≥ x, a y ⇨ b y
+  himp m n :=
+  { toFun x := ⨅ y ≥ x, m y ⇨ n y
     idempotent' i := by
       apply le_iInf₂
       intro j hj
-      have h : (a (a j ⇨ b j)) ⇨ (b (a j ⇨ b j)) = a j ⇨ b j := by
-        have h_fix : b (a j ⇨ b j) = a j ⇨ b j := by
+      have h : (m (m j ⇨ n j)) ⇨ (n (m j ⇨ n j)) = m j ⇨ n j := by
+        have h_fix : n (m j ⇨ n j) = m j ⇨ n j := by
           refine le_antisymm ?_ le_apply
           rw [le_himp_iff, himp_eq_sSup]
-          have h1 : b (sSup {w | w ⊓ a j ≤ b j}) ⊓ a j ≤
-              b (sSup {w | w ⊓ a j ≤ b j}) ⊓  b (a j) := by
+          have h1 : n (sSup {w | w ⊓ m j ≤ n j}) ⊓ m j ≤
+              n (sSup {w | w ⊓ m j ≤ n j}) ⊓  n (m j) := by
             simp only [le_inf_iff, inf_le_left, true_and]
             exact inf_le_of_right_le le_apply
           apply le_trans h1
-          rw [← map_inf, sSup_inf_eq, ← @idempotent _ _ b j]
+          rw [← map_inf, sSup_inf_eq, ← @idempotent _ _ n j]
           gcongr
           apply iSup₂_le
           simp [idempotent]
-        rw [h_fix, himp_himp, ← map_inf, inf_of_le_right (le_trans b.le_apply le_himp)]
+        rw [h_fix, himp_himp, ← map_inf, inf_of_le_right (le_trans n.le_apply le_himp)]
       rw [← h]
-      refine iInf₂_le (a j ⇨ b j) ?_
+      refine iInf₂_le (m j ⇨ n j) ?_
       simp only [ge_iff_le, le_himp_iff, iInf_inf, iInf_le_iff, le_inf_iff, le_iInf_iff]
       intro b1 h2
       rcases h2 j with ⟨h3, h4⟩
@@ -187,17 +187,17 @@ instance : HImp (Nucleus X) where
           exact Preorder.le_trans _ _ _ h7 (by gcongr; simp)
     le_apply' := by
       simp only [ge_iff_le, le_iInf_iff, le_himp_iff]
-      refine fun _ _ h ↦ inf_le_of_left_le (le_trans h b.le_apply)}
+      refine fun _ _ h ↦ inf_le_of_left_le (le_trans h n.le_apply)}
 
-@[simp] theorem himp_apply (a b : Nucleus X) (x : X) : (a ⇨ b) x = ⨅ y ≥ x, a y ⇨ b y := rfl
+@[simp] theorem himp_apply (m n : Nucleus X) (x : X) : (m ⇨ n) x = ⨅ y ≥ x, m y ⇨ n y := rfl
 
 instance : HeytingAlgebra (Nucleus X) where
-  le_himp_iff a b c := by
+  le_himp_iff _ n _ := by
     simp [← coe_le_coe, Pi.le_def]
     exact ⟨fun h i ↦ h i i le_rfl,
-      fun h1 i j _ ↦ le_trans (inf_le_inf_right (b j) (by gcongr)) (h1 j)⟩
-  compl a := a ⇨ ⊥
-  himp_bot a := rfl
+      fun h1 i j _ ↦ le_trans (inf_le_inf_right (n j) (by gcongr)) (h1 j)⟩
+  compl m := m ⇨ ⊥
+  himp_bot m := rfl
 
 instance : Order.Frame (Nucleus X) where
    __ := Nucleus.instHeytingAlgebra
