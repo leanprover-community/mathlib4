e210aec50ddd29e31d3039f19716288bb95b1686
Markus Himmel <markus@lean-fro.org>
2025-02-07 17:12:45 +0100

    chore: delete `CategoryTheory.Monoidal.Types.Symmetric`

--- Mathlib.lean
+++ Mathlib.lean
@@ -2134,7 +2134,6 @@ import Mathlib.CategoryTheory.Monoidal.Tor
 import Mathlib.CategoryTheory.Monoidal.Transport
 import Mathlib.CategoryTheory.Monoidal.Types.Basic
 import Mathlib.CategoryTheory.Monoidal.Types.Coyoneda
-import Mathlib.CategoryTheory.Monoidal.Types.Symmetric
 import Mathlib.CategoryTheory.MorphismProperty.Basic
 import Mathlib.CategoryTheory.MorphismProperty.Comma
 import Mathlib.CategoryTheory.MorphismProperty.Composition
