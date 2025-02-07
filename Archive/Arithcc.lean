b9c1364433ca5a801cbf5e3c121de858187eebd3
Johan Commelin <johan@commelin.net>
2025-02-07 10:15:30 +0100

    wip

--- Archive/Arithcc.lean
+++ Archive/Arithcc.lean
@@ -220,12 +220,20 @@ protected theorem StateEq.trans {t : Register} (ζ₁ ζ₂ ζ₃ : State) :
   · simp_all only
   · trans ζ₂ <;> assumption
 
+-- Porting note (https://github.com/leanprover-community/mathlib4/issues/10754): added instance
+instance (t : Register) : Trans (StateEq (t + 1)) (StateEq (t + 1)) (StateEq (t + 1)) :=
+  ⟨@StateEq.trans _⟩
+
 /-- Transitivity of chaining `≃[t]` and `≃[t]/ac`. -/
 @[trans]
 protected theorem StateEqStateEqRs.trans (t : Register) (ζ₁ ζ₂ ζ₃ : State) :
     ζ₁ ≃[t] ζ₂ → ζ₂ ≃[t]/ac ζ₃ → ζ₁ ≃[t]/ac ζ₃ := by
   simp [StateEq]; intros
   trans ζ₂ <;> assumption
+
+-- Porting note (https://github.com/leanprover-community/mathlib4/issues/10754): added instance
+instance (t : Register) : Trans (StateEq (t + 1)) (StateEqRs (t + 1)) (StateEqRs (t + 1)) :=
+  ⟨@StateEqStateEqRs.trans _⟩
 
 /-- Writing the same value to register `t` gives `≃[t + 1]` from `≃[t]`. -/
 theorem stateEq_implies_write_eq {t : Register} {ζ₁ ζ₂ : State} (h : ζ₁ ≃[t] ζ₂) (v : Word) :
