2f6db4c237591473fdb044649766a6550a316f90
Tristan F. <LeoDog896@hotmail.com>
2025-02-08 15:28:50 -0800

    Revert "feat(CFG): lemmas about nonterminal requirements"
    This reverts commit 2f51cd579bf6d8ad89bd9b223b055612a9ce5fab.

--- Mathlib/Computability/ContextFreeGrammar.lean
+++ Mathlib/Computability/ContextFreeGrammar.lean
@@ -85,10 +85,6 @@ theorem rewrites_iff :
       u = p ++ [Symbol.nonterminal r.input] ++ q ∧ v = p ++ r.output ++ q :=
   ⟨Rewrites.exists_parts, by rintro ⟨p, q, rfl, rfl⟩; apply rewrites_of_exists_parts⟩
 
-lemma nonterminal_rule_if_rewrite : r.Rewrites u v → .nonterminal r.input ∈ u := by
-  simp_rw [rewrites_iff, List.append_assoc]
-  exact fun _ ↦ List.mem_iff_append.mpr (by tauto)
-
 /-- Add extra prefix to context-free rewriting. -/
 lemma Rewrites.append_left (hvw : r.Rewrites u v) (p : List (Symbol T N)) :
     r.Rewrites (p ++ u) (p ++ v) := by
@@ -167,17 +163,9 @@ lemma Derives.eq_or_head {u w : List (Symbol T g.NT)} (huw : g.Derives u w) :
     u = w ∨ ∃ v : List (Symbol T g.NT), g.Produces u v ∧ g.Derives v w :=
   Relation.ReflTransGen.cases_head huw
 
-lemma Derives.iff_eq_or_head {u w : List (Symbol T g.NT)} :
-    g.Derives u w ↔ u = w ∨ ∃ v : List (Symbol T g.NT), g.Produces u v ∧ g.Derives v w :=
-  Relation.ReflTransGen.cases_head_iff
-
 lemma Derives.eq_or_tail {u w : List (Symbol T g.NT)} (huw : g.Derives u w) :
-    w = u ∨ ∃ v : List (Symbol T g.NT), g.Derives u v ∧ g.Produces v w :=
-  Relation.ReflTransGen.cases_tail huw
-
-lemma Derives.iff_eq_or_tail {u w : List (Symbol T g.NT)} :
-    g.Derives u w ↔ w = u ∨ ∃ v : List (Symbol T g.NT), g.Derives u v ∧ g.Produces v w :=
-  Relation.ReflTransGen.cases_tail_iff g.Produces u w
+    u = w ∨ ∃ v : List (Symbol T g.NT), g.Derives u v ∧ g.Produces v w :=
+  (Relation.ReflTransGen.cases_tail huw).casesOn (Or.inl ∘ Eq.symm) Or.inr
 
 /-- Add extra prefix to context-free producing. -/
 lemma Produces.append_left {v w : List (Symbol T g.NT)}
@@ -206,24 +194,6 @@ lemma Derives.append_right {v w : List (Symbol T g.NT)}
   induction hvw with
   | refl => rfl
   | tail _ last ih => exact ih.trans_produces <| last.append_right p
-
-lemma produces_empty (g : ContextFreeGrammar T) (u v : List (Symbol T g.NT)) (h : g.Produces u v) :
-    ∃ r ∈ g.rules, .nonterminal r.input ∈ u := by
-  obtain ⟨w, ⟨l, r⟩⟩ := h
-  exact ⟨w, ⟨l, ContextFreeRule.nonterminal_rule_if_rewrite r⟩⟩
-
-lemma derives_empty (g : ContextFreeGrammar T) (t : g.NT) (h : ∀ r ∈ g.rules, r.input ≠ t) :
-    ∀ s ≠ [.nonterminal t], ¬g.Derives [.nonterminal t] s := fun _ hs ↦ by
-  rw [Derives.iff_eq_or_head]
-  push_neg
-  refine ⟨hs.symm, fun _ hx ↦ ?_⟩
-  have hxr := produces_empty g _ _ hx
-  simp_rw [List.mem_singleton, Symbol.nonterminal.injEq] at hxr
-  tauto
-
-lemma noninitial_empty (g : ContextFreeGrammar T) (h : ∀ r ∈ g.rules, r.input ≠ g.initial) :
-    g.language = 0 :=
-  Language.ext fun _ ↦ ⟨(absurd · (derives_empty g _ h _ (by simp))), fun _ ↦ by contradiction⟩
 
 end ContextFreeGrammar
 
