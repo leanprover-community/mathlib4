30e8e597912c667a3ad1694293e5a2d001a4d7e4
Etienne Marion <etienne.marion@ens-lyon.fr>
2025-02-08 20:17:08 +0100

    fix imports

--- Mathlib/MeasureTheory/Integral/Bochner.lean
+++ Mathlib/MeasureTheory/Integral/Bochner.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Zhouhang Zhou, Yury Kudryashov, Sébastien Gouëzel, Rémy Degenne
 -/
 import Mathlib.MeasureTheory.Integral.BochnerL1
+import Mathlib.MeasureTheory.Integral.IntegrableOn
 
 /-!
 # Bochner integral
--- Mathlib/MeasureTheory/Integral/BochnerL1.lean
+++ Mathlib/MeasureTheory/Integral/BochnerL1.lean
@@ -3,7 +3,6 @@ Copyright (c) 2019 Zhouhang Zhou. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Zhouhang Zhou, Yury Kudryashov, Sébastien Gouëzel, Rémy Degenne
 -/
-import Mathlib.MeasureTheory.Integral.IntegrableOn
 import Mathlib.MeasureTheory.Integral.SetToL1
 
 /-!
