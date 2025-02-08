9f605b030c939a6471777a3b6a86cc8f3a486ee2
Kyle Miller <kmill31415@gmail.com>
2025-02-07 16:20:34 -0800

    change namespace

--- Mathlib/Tactic/NormNum/NatLog.lean
+++ Mathlib/Tactic/NormNum/NatLog.lean
@@ -11,11 +11,9 @@ import Mathlib.Tactic.NormNum
 This module defines a `norm_num` extension for `Nat.log`.
 -/
 
-namespace Tactic
+namespace Mathlib.Meta.NormNum
 
-namespace NormNum
-
-open Qq Lean Elab.Tactic Mathlib.Meta.NormNum
+open Qq Lean Elab.Tactic
 
 lemma nat_log_zero (n : Nat) : Nat.log 0 n = 0 := by simp
 lemma nat_log_one (n : Nat) : Nat.log 1 n = 0 := by simp
@@ -34,9 +32,11 @@ theorem isNat_log : {b nb n nn k : ℕ} → IsNat b nb → IsNat n nn →
     Nat.log nb nn = k → IsNat (Nat.log b n) k
   | _, _, _, _, _, ⟨rfl⟩, ⟨rfl⟩, rfl => ⟨rfl⟩
 
-/-- Given the natural number literals `eb` and `en`, returns `Nat.log eb en`
+/--
+Given the natural number literals `eb` and `en`, returns `Nat.log eb en`
 as a natural number literal and an equality proof.
-Panics if `ex` isn't a natural number literal. -/
+Panics if `ex` isn't a natural number literal.
+-/
 def proveNatLog (eb en : Q(ℕ)) : (ek : Q(ℕ)) × Q(Nat.log $eb $en = $ek) :=
   match eb.natLit!, en.natLit! with
   | 0, _ => show (ek : Q(ℕ)) × Q(Nat.log 0 $en = $ek) from ⟨mkRawNatLit 0, q(nat_log_zero $en)⟩
@@ -52,7 +52,9 @@ def proveNatLog (eb en : Q(ℕ)) : (ek : Q(ℕ)) × Q(Nat.log $eb $en = $ek) :=
       have hh : Q(Nat.blt $en ($eb ^ ($ek + 1)) = true):= (q(Eq.refl true) : Expr)
       ⟨ek, q(nat_log_helper $eb $en $ek $hl $hh)⟩
 
-/-- Evaluates the `Nat.log` function. -/
+/--
+Evaluates the `Nat.log` function.
+-/
 @[norm_num Nat.log _ _]
 def evalNatLog : NormNumExt where eval {u α} e := do
   let mkApp2 _ (b : Q(ℕ)) (n : Q(ℕ)) ← Meta.whnfR e | failure
@@ -63,6 +65,4 @@ def evalNatLog : NormNumExt where eval {u α} e := do
   let pf' : Q(IsNat (Nat.log $b $n) $ek) := q(isNat_log $pb $pn $pf)
   return .isNat sℕ ek pf'
 
-end NormNum
-
-end Tactic
+end Mathlib.Meta.NormNum
