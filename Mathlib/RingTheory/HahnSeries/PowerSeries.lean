52643fbaf911cc5600e172725b54e2eb6c464ed6
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2025-02-09 21:33:07 +0100

    chore: tidy various files

--- Mathlib/RingTheory/HahnSeries/PowerSeries.lean
+++ Mathlib/RingTheory/HahnSeries/PowerSeries.lean
@@ -141,7 +141,8 @@ theorem ofPowerSeries_X_pow {R} [Semiring R] (n : ℕ) :
 /-- The ring `HahnSeries (σ →₀ ℕ) R` is isomorphic to `MvPowerSeries σ R` for a `Finite` `σ`.
 We take the index set of the hahn series to be `Finsupp` rather than `pi`,
 even though we assume `Finite σ` as this is more natural for alignment with `MvPowerSeries`.
-After importing `Algebra.Order.Pi` the ring `HahnSeries (σ → ℕ) R` could be constructed instead.
+After importing `Mathlib.Algebra.Order.Pi` the ring `HahnSeries (σ → ℕ) R` could be constructed
+instead.
 -/
 @[simps]
 def toMvPowerSeries {σ : Type*} [Finite σ] : HahnSeries (σ →₀ ℕ) R ≃+* MvPowerSeries σ R where
