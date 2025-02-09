a92836ffa3d33ebcf4c75f02b0b63080955de74f
Niklas Mohrin <dev@niklasmohrin.de>
2025-02-09 15:46:14 +0100

    `shake`

--- Mathlib/Combinatorics/Flow/Basic.lean
+++ Mathlib/Combinatorics/Flow/Basic.lean
@@ -3,8 +3,8 @@ Copyright (c) 2025 Niklas Mohrin. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Niklas Mohrin
 -/
+import Mathlib.Algebra.BigOperators.Group.Finset.Basic
 import Mathlib.Combinatorics.Digraph.Basic
-import Mathlib.Data.Fintype.BigOperators
 
 /-!
 # Network Flows
