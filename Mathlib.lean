887fb97b49590f76ff8acf434b4d2eadde6bd356
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-02-09 18:09:11 +0100

    feat(AlgebraicTopology): attaching cells

--- Mathlib.lean
+++ Mathlib.lean
@@ -1125,6 +1125,7 @@ import Mathlib.AlgebraicTopology.MooreComplex
 import Mathlib.AlgebraicTopology.Quasicategory.Basic
 import Mathlib.AlgebraicTopology.Quasicategory.Nerve
 import Mathlib.AlgebraicTopology.Quasicategory.StrictSegal
+import Mathlib.AlgebraicTopology.RelativeCellComplex.AttachCells
 import Mathlib.AlgebraicTopology.SimplexCategory
 import Mathlib.AlgebraicTopology.SimplicialCategory.Basic
 import Mathlib.AlgebraicTopology.SimplicialCategory.SimplicialObject
