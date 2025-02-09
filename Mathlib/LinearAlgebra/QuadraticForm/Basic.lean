ddaf5b0eb542d20fa9069e82a2326008d6956cc2
Christopher Hoskin <mans0954@users.noreply.github.com>
2025-02-09 15:25:32 +0000

    Update Mathlib/LinearAlgebra/QuadraticForm/Basic.lean
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- Mathlib/LinearAlgebra/QuadraticForm/Basic.lean
+++ Mathlib/LinearAlgebra/QuadraticForm/Basic.lean
@@ -135,7 +135,7 @@ def polarSym2 (f : M → N) : Sym2 M → N :=
   Sym2.lift ⟨polar f, polar_comm _⟩
 
 @[simp]
-lemma polarSym2_sym2Mk (f : M → N) (x y : M ) : polarSym2 f (Sym2.mk (x, y)) = polar f x y := rfl
+lemma polarSym2_sym2Mk (f : M → N) (x y : M ) : polarSym2 f (.mk (x, y)) = polar f x y := rfl
 
 end QuadraticMap
 
