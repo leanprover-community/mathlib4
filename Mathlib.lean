3fcf3529ede6d9ff335f5172f3810479f4a11ab0
Ben Eltschig <b.eltschig@protonmail.com>
2025-02-09 07:22:01 +0100

    fix `Mathlib.lean`, again

--- Mathlib.lean
+++ Mathlib.lean
@@ -5427,8 +5427,8 @@ import Mathlib.Topology.Compactness.PseudometrizableLindelof
 import Mathlib.Topology.Compactness.SigmaCompact
 import Mathlib.Topology.Connected.Basic
 import Mathlib.Topology.Connected.Clopen
-import Mathlib.Topology.Connected.LocallyConnected
 import Mathlib.Topology.Connected.LocPathConnected
+import Mathlib.Topology.Connected.LocallyConnected
 import Mathlib.Topology.Connected.PathConnected
 import Mathlib.Topology.Connected.Separation
 import Mathlib.Topology.Connected.TotallyDisconnected
