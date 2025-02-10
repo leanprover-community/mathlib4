3f247dc88d93cffb4e00bdf9c1bf373ebe2b80f4
Markus Himmel <markus@lean-fro.org>
2025-02-10 06:57:24 +0100

    Apply suggestion from code review

--- Mathlib/CategoryTheory/Monoidal/Internal/Types/Basic.lean
+++ Mathlib/CategoryTheory/Monoidal/Internal/Types/Basic.lean
@@ -61,24 +61,10 @@ is equivalent to the category of "native" bundled monoids.
 noncomputable def monTypeEquivalenceMon : Mon_ (Type u) ≌ MonCat.{u} where
   functor := functor
   inverse := inverse
-  unitIso :=
-    NatIso.ofComponents
-      (fun A =>
-        { hom := { hom := 𝟙 _ }
-          inv := { hom := 𝟙 _ } })
-      (by aesop_cat)
-  counitIso :=
-    NatIso.ofComponents
-      (fun A =>
-        { hom := MonCat.ofHom
-            { toFun := id
-              map_one' := rfl
-              map_mul' := fun _ _ => rfl }
-          inv := MonCat.ofHom
-            { toFun := id
-              map_one' := rfl
-              map_mul' := fun _ _ => rfl } })
-      (by aesop_cat)
+  unitIso := Iso.refl _
+  counitIso := NatIso.ofComponents
+    (fun A => MulEquiv.toMonCatIso { Equiv.refl _ with map_mul' := fun _ _ => rfl })
+    (by aesop_cat)
 
 /-- The equivalence `Mon_ (Type u) ≌ MonCat.{u}`
 is naturally compatible with the forgetful functors to `Type u`.
@@ -122,24 +108,10 @@ is equivalent to the category of "native" bundled commutative monoids.
 noncomputable def commMonTypeEquivalenceCommMon : CommMon_ (Type u) ≌ CommMonCat.{u} where
   functor := functor
   inverse := inverse
-  unitIso :=
-    NatIso.ofComponents
-      (fun A =>
-        { hom := { hom := 𝟙 _ }
-          inv := { hom := 𝟙 _ } })
-      (by aesop_cat)
-  counitIso :=
-    NatIso.ofComponents
-      (fun A =>
-        { hom := CommMonCat.ofHom
-            { toFun := id
-              map_one' := rfl
-              map_mul' := fun _ _ => rfl }
-          inv := CommMonCat.ofHom
-            { toFun := id
-              map_one' := rfl
-              map_mul' := fun _ _ => rfl } })
-      (by aesop_cat)
+  unitIso := Iso.refl _
+  counitIso := NatIso.ofComponents
+    (fun A => MulEquiv.toCommMonCatIso { Equiv.refl _ with map_mul' := fun _ _ => rfl })
+    (by aesop_cat)
 
 /-- The equivalences `Mon_ (Type u) ≌ MonCat.{u}` and `CommMon_ (Type u) ≌ CommMonCat.{u}`
 are naturally compatible with the forgetful functors to `MonCat` and `Mon_ (Type u)`.
