01e4458336fc91e8f5c3de24c29ca491cfbd589b
Anne Baanen <vierkantor@vierkantor.com>
2025-02-10 19:51:42 +0000

    chore(Topology/UniformSpace): split into `Defs` and `Basic` (#21664)
    Split `Defs.lean` off of `Basic.lean`, including the definition of `UniformSpace` and `UniformContinuous`, and all lemmas that can be moved over without increasing imports in `Defs.lean`.
    
    This is motivated by reducing imports for metric spaces, although it seems that `EMetricSpace/Defs.lean` still needs some results that ended up in `Basic.lean`. I think the conclusion is that `EMetricSpace/Defs.lean` should be split further.
    
    
    
    Co-authored-by: Anne Baanen <Vierkantor@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -5731,6 +5731,7 @@ import Mathlib.Topology.UniformSpace.CompactConvergence
 import Mathlib.Topology.UniformSpace.CompareReals
 import Mathlib.Topology.UniformSpace.CompleteSeparated
 import Mathlib.Topology.UniformSpace.Completion
+import Mathlib.Topology.UniformSpace.Defs
 import Mathlib.Topology.UniformSpace.Dini
 import Mathlib.Topology.UniformSpace.Equicontinuity
 import Mathlib.Topology.UniformSpace.Equiv
