22dc423666c128c2b4d8e03ffdb2396747f66eb1
tydeu <tydeu@hatpress.net>
2025-02-10 10:08:44 -0500

    feat: improve error message on out-of-date ProofWidgets

--- lakefile.lean
+++ lakefile.lean
@@ -10,7 +10,12 @@ open Lake DSL
 require "leanprover-community" / "batteries" @ git "main"
 require "leanprover-community" / "Qq" @ git "master"
 require "leanprover-community" / "aesop" @ git "master"
-require "leanprover-community" / "proofwidgets" @ git "v0.0.52-pre" -- ProofWidgets should always be pinned to a specific version
+require "leanprover-community" / "proofwidgets" -- ProofWidgets should always be pinned to a specific version
+  from git "https://github.com/tydeu/ProofWidgets4" @ "v0.0.52-pre"
+  with NameMap.empty.insert `errorOnBuild
+    "ProofWidgets not up-to-date. \
+    Please run `lake exe cache get` to fetch the latest ProofWidgets. \
+    If this does not work, report your issue on the Lean Zulip."
 require "leanprover-community" / "importGraph" @ git "main"
 require "leanprover-community" / "LeanSearchClient" @ git "main"
 require "leanprover-community" / "plausible" @ git "main"
