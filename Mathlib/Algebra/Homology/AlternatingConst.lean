ad210642ad4dd88191205de83ad04d6e411514dc
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-02-10 09:51:04 +0100

    fixing imports

--- Mathlib/Algebra/Homology/AlternatingConst.lean
+++ Mathlib/Algebra/Homology/AlternatingConst.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Andrew Yang
 -/
 import Mathlib.Algebra.Homology.ShortComplex.HomologicalComplex
+import Mathlib.Algebra.Module.BigOperators
 import Mathlib.AlgebraicTopology.ExtraDegeneracy
 
 /-!
