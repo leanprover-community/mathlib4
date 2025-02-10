0666c4b8ed8641c8ebaf1b02d9b34083b1c5ca2a
xyzw12345 <wangjt2020@163.com>
2025-02-10 10:04:11 +0800

    Update Basic.lean

--- Mathlib/Data/ENat/Basic.lean
+++ Mathlib/Data/ENat/Basic.lean
@@ -325,6 +325,9 @@ lemma add_left_injective_of_ne_top {n : ℕ∞} (hn : n ≠ ⊤) : Function.Inje
     ((WithTop.add_le_add_iff_right hn).mp e.le)
     ((WithTop.add_le_add_iff_right hn).mp e.ge)
 
+lemma add_right_injective_of_ne_top {n : ℕ∞} (hn : n ≠ ⊤) : Function.Injective (n + ·) := by
+  simp_rw [add_comm n _]; exact add_left_injective_of_ne_top hn
+
 section withTop_enat
 
 lemma add_one_natCast_le_withTop_of_lt {m : ℕ} {n : WithTop ℕ∞} (h : m < n) : (m + 1 : ℕ) ≤ n := by
