a0042be3379d91cbec8a26fa19a8017bd00d6621
Violeta Hernández <vi.hdz.p@gmail.com>
2025-02-09 23:04:56 -0600

    fix

--- Mathlib/SetTheory/Cardinal/Arithmetic.lean
+++ Mathlib/SetTheory/Cardinal/Arithmetic.lean
@@ -893,7 +893,7 @@ theorem card_opow_le_of_omega0_le_left {a : Ordinal} (ha : ω ≤ a) (b : Ordina
       intro c
       exact (IH c.1 c.2).trans (max_le_max_left _ (card_le_card c.2.le))
     · simpa using hb.ne_bot
-    · refine le_ciSup_of_le ?_ ⟨1, one_lt_omega0.trans_le <| omega0_le_of_isLimit hb⟩ ?_
+    · refine le_ciSup_of_le ?_ ⟨1, one_lt_omega0.trans_le <| omega0_le_of_isSuccLimit hb⟩ ?_
       · exact Cardinal.bddAbove_of_small _
       · simpa
 
