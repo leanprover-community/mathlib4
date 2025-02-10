c38165c0b366aa3450e93094566d36a9ec08b6cf
JakobStiefel <jakobstiefel@hotmail.de>
2025-02-10 14:34:52 +0000

    feat: basic properties of `MulExpNegMulSq` (#20604)
    Define `MulExpNegMulSq` and show basic properties. The function is an important tool in the proof of separation of finite measures by subalgebras of functions
    
    
    
    Co-authored-by: JakobStiefel <73285902+JakobStiefel@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1629,6 +1629,7 @@ import Mathlib.Analysis.SpecialFunctions.Log.Monotone
 import Mathlib.Analysis.SpecialFunctions.Log.NegMulLog
 import Mathlib.Analysis.SpecialFunctions.Log.PosLog
 import Mathlib.Analysis.SpecialFunctions.Log.Summable
+import Mathlib.Analysis.SpecialFunctions.MulExpNegMulSq
 import Mathlib.Analysis.SpecialFunctions.NonIntegrable
 import Mathlib.Analysis.SpecialFunctions.OrdinaryHypergeometric
 import Mathlib.Analysis.SpecialFunctions.PolarCoord
