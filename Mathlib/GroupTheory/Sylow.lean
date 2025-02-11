2e48786a10980907212d0c54139571afe8afe215
Yaël Dillies <yael.dillies@gmail.com>
2025-02-11 12:53:24 +0000

    chore(Periodic): don't import `Field`

--- Mathlib/GroupTheory/Sylow.lean
+++ Mathlib/GroupTheory/Sylow.lean
@@ -3,6 +3,7 @@ Copyright (c) 2018 Chris Hughes. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Chris Hughes, Thomas Browning
 -/
+import Mathlib.Algebra.Order.Archimedean.Basic
 import Mathlib.Data.SetLike.Fintype
 import Mathlib.GroupTheory.PGroup
 import Mathlib.GroupTheory.NoncommPiCoprod
