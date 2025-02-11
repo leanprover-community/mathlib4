1b4dcdbb08eab24f3f9a9f6856df6ba9be8dbd26
Christian Merten <christian@merten.dev>
2025-02-11 21:29:14 +0100

    fix?

--- Mathlib/LinearAlgebra/Matrix/Swap.lean
+++ Mathlib/LinearAlgebra/Matrix/Swap.lean
@@ -21,46 +21,6 @@ Swap matrices are a special case of *elementary matrices*. For transvections see
 This is a thin wrapper around `(Equiv.swap i j).toPEquiv.toMatrix`.
 -/
 
-namespace Equiv
-
-open Matrix
-
-variable {α n m : Type*}
-
-lemma toPEquiv_toMatrix_mulVec_apply [Fintype n] [DecidableEq n]
-    [NonAssocSemiring α] (σ : m ≃ n) (i : m) (a : n → α) :
-    (σ.toPEquiv.toMatrix *ᵥ a) i = a (σ i) := by
-  induction a using Pi.induction_add with
-  | zero => simp
-  | add f g hf hg => simp [hf, hg, Matrix.mulVec_add]
-  | single => simp [Pi.single_apply]
-
-lemma toPEquiv_toMatrix_mulVec [Fintype n] [DecidableEq n]
-    [NonAssocSemiring α] (σ : m ≃ n) (a : n → α) :
-    σ.toPEquiv.toMatrix *ᵥ a = a ∘ σ := by
-  ext i
-  simp [toPEquiv_toMatrix_mulVec_apply]
-
-lemma vecMul_toPEquiv_toMatrix_apply [Fintype m] [DecidableEq n]
-    [NonAssocSemiring α] (σ : m ≃ n) (i : n) (a : m → α) :
-    (a ᵥ* σ.toPEquiv.toMatrix) i = a (σ.symm i) := by
-  classical
-  induction a using Pi.induction_add with
-  | zero => simp
-  | add f g hf hg => simp [hf, hg, Matrix.add_vecMul]
-  | single =>
-      simp only [single_vecMul, PEquiv.toMatrix_apply, toPEquiv_apply, Option.mem_def,
-        Option.some.injEq, mul_ite, mul_one, mul_zero, Pi.single_apply, symm_apply_eq]
-      simp_rw [eq_comm]
-
-lemma vecMul_toPEquiv_toMatrix [Fintype m] [DecidableEq n]
-    [NonAssocSemiring α] (σ : m ≃ n) (a : m → α) :
-    a ᵥ* σ.toPEquiv.toMatrix = a ∘ σ.symm := by
-  ext
-  simp [vecMul_toPEquiv_toMatrix_apply]
-
-end Equiv
-
 namespace Matrix
 
 section Def
@@ -102,10 +62,21 @@ lemma map_swap {S : Type*} [Semiring S] (f : R →+* S) (i j : n) :
 
 variable [Fintype n]
 
+lemma swap_mulVec (i j : n) (a : n → R) :
+    swap R i j *ᵥ a = a ∘ Equiv.swap i j := by
+  simp [swap, PEquiv.toMatrix_toPEquiv_mulVec]
+
+lemma vecMul_swap (i j : n) (a : n → R) :
+    a ᵥ* swap R i j = a ∘ Equiv.swap i j := by
+  simp [swap, PEquiv.vecMul_toMatrix_toPEquiv]
+
 lemma swap_mulVec_single_left (i j : n) (r : R) :
     swap R i j *ᵥ Pi.single i r = Pi.single j r := by
-  simp only [swap, Equiv.toMatrix_toPEquiv_mulVec_single, Equiv.symm_swap]
-  rw [Equiv.swap_apply_left]
+  ext k
+  rw [swap_mulVec]
+  simp
+  simp [Pi.single_apply, Equiv.swap, Equiv.swapCore]
+  aesop
 
 lemma swap_mulVec_single_right (i j : n) (r : R) :
     swap R i j *ᵥ Pi.single j r = Pi.single i r := by
@@ -113,12 +84,12 @@ lemma swap_mulVec_single_right (i j : n) (r : R) :
 
 lemma swap_mulVec_single_of_ne {i j k : n} (hik : k ≠ i) (hjk : k ≠ j) (r : R) :
     swap R i j *ᵥ Pi.single k r = Pi.single k r := by
-  simp only [swap, Equiv.toMatrix_toPEquiv_mulVec_single, Equiv.symm_swap,
+  simp only [swap, PEquiv.toMatrix_toPEquiv_mulVec_single, Equiv.symm_swap,
     Equiv.swap_apply_of_ne_of_ne hik hjk]
 
 lemma single_vecMul_swap_left (i j : n) (r : R) :
     Pi.single i r ᵥ* swap R i j = Pi.single j r := by
-  simp only [swap, Equiv.single_vecMul_toMatrix_toPEquiv, Equiv.symm_swap]
+  simp only [swap, PEquiv.single_vecMul_toMatrix_toPEquiv, Equiv.symm_swap]
   rw [Equiv.swap_apply_left]
 
 lemma single_vecMul_swap_right (i j : n) (r : R) :
@@ -127,24 +98,25 @@ lemma single_vecMul_swap_right (i j : n) (r : R) :
 
 lemma single_vecMul_swap_of_ne {i j k : n} (hik : k ≠ i) (hjk : k ≠ j) (r : R) :
     Pi.single k r ᵥ* swap R i j = Pi.single k r := by
-  simp only [swap, Equiv.single_vecMul_toMatrix_toPEquiv, Equiv.symm_swap,
+  ext
+  simp [swap, PEquiv.single_vecMul_toMatrix_toPEquiv, Equiv.symm_swap, Pi.single_apply,
     Equiv.swap_apply_of_ne_of_ne hik hjk]
 
 @[simp]
 lemma swap_mulVec_apply (i j : n) (a : n → R) :
     (swap R i j *ᵥ a) i = a j := by
-  simp [swap, Equiv.toPEquiv_toMatrix_mulVec_apply]
+  simp [swap, PEquiv.toMatrix_toPEquiv_mulVec]
 
 @[simp]
 lemma vecMul_swap_apply (i j : n) (a : n → R) :
     (a ᵥ* swap R i j) i = a j := by
-  simp [swap, Equiv.vecMul_toPEquiv_toMatrix_apply]
+  simp [swap, PEquiv.vecMul_toMatrix_toPEquiv]
 
 /-- Multiplying with `swap R i j` on the left swaps the `i`-th row with the `j`-th row. -/
 @[simp]
 lemma swap_mul_apply_left (a i j : n) (g : Matrix n n R) :
     (swap R i j * g) i a = g j a := by
-  simp [swap, PEquiv.toPEquiv_mul_matrix]
+  simp [swap, PEquiv.toMatrix_toPEquiv_mul]
 
 /-- Multiplying with `swap R i j` on the left swaps the `j`-th row with the `i`-th row. -/
 @[simp]
@@ -154,13 +126,13 @@ lemma swap_mul_apply_right (a i j : n) (g : Matrix n n R) :
 
 lemma swap_mul_of_ne {a b i j : n} (hai : a ≠ i) (haj : a ≠ j) (g : Matrix n n R) :
     (swap R i j * g) a b = g a b := by
-  simp [swap, PEquiv.toPEquiv_mul_matrix, Equiv.swap_apply_of_ne_of_ne hai haj]
+  simp [swap, PEquiv.toMatrix_toPEquiv_mul, Equiv.swap_apply_of_ne_of_ne hai haj]
 
 /-- Multiplying with `swap R i j` on the right swaps the `i`-th column with the `j`-th column. -/
 @[simp]
 lemma mul_swap_apply_left (a i j : n) (g : Matrix n n R) :
     (g * swap R i j) a i = g a j := by
-  simp [swap, PEquiv.mul_toPEquiv_toMatrix]
+  simp [swap, PEquiv.mul_toMatrix_toPEquiv]
 
 /-- Multiplying with `swap R i j` on the right swaps the `j`-th column with the `i`-th column. -/
 @[simp]
@@ -170,13 +142,13 @@ lemma mul_swap_apply_right (a i j : n) (g : Matrix n n R) :
 
 lemma mul_swap_of_ne {a b : n} {i j : n} (hbi : b ≠ i) (hbj : b ≠ j) (g : Matrix n n R) :
     (g * swap R i j) a b = g a b := by
-  simp [swap, PEquiv.mul_toPEquiv_toMatrix, Equiv.swap_apply_of_ne_of_ne hbi hbj]
+  simp [swap, PEquiv.mul_toMatrix_toPEquiv, Equiv.swap_apply_of_ne_of_ne hbi hbj]
 
 /-- Swap matrices are self inverse. -/
 lemma swap_mul_self (i j : n) : swap R i j * swap R i j = 1 := by
   simp only [swap]
   rw [← Equiv.swap_inv, Equiv.Perm.inv_def]
-  apply Equiv.toMatrix_toPEquiv_mul_toMatrix_toPEquiv_symm
+  simp [← PEquiv.toMatrix_trans, ← Equiv.toPEquiv_trans]
 
 end
 
