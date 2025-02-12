ed2ae17e64d10da34d41a31cba5eda4e8f542ab4
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2025-02-12 03:04:04 +0000

    fix

--- Mathlib/Algebra/Ring/BooleanRing.lean
+++ Mathlib/Algebra/Ring/BooleanRing.lean
@@ -267,7 +267,7 @@ theorem ofBoolAlg_sdiff (a b : AsBoolAlg α) : ofBoolAlg (a \ b) = ofBoolAlg a *
 private theorem of_boolalg_symmDiff_aux (a b : α) : (a + b + a * b) * (1 + a * b) = a + b :=
   calc (a + b + a * b) * (1 + a * b)
     _ = a + b + (a * b + a * b * (a * b)) + (a * (b * b) + a * a * b) := by ring
-    _ = a + b := by simp only [mul_self, add_self, add_zero]
+    _ = a + b := by simp only [BooleanRing.mul_self, BooleanRing.add_self, add_zero]
 
 @[simp]
 theorem ofBoolAlg_symmDiff (a b : AsBoolAlg α) : ofBoolAlg (a ∆ b) = ofBoolAlg a + ofBoolAlg b := by
