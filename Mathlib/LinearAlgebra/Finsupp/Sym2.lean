53a8e5bfaa30875cbae1121b0e485a00f878badc
Christopher Hoskin <christopher.hoskin@gmail.com>
2025-02-12 19:36:30 +0000

    Add LinearAlgebra/Finsupp/Sym2

---
+++ Mathlib/LinearAlgebra/Finsupp/Sym2.lean
@@ -0,0 +1,52 @@
+/-
+Copyright (c) 2025 Christopher Hoskin. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Christopher Hoskin
+-/
+
+import Mathlib.Data.Sym.Sym2
+import Mathlib.Algebra.Ring.Defs
+import Mathlib.LinearAlgebra.Finsupp.Defs
+import Mathlib.Data.Finset.Sym
+
+/-!
+# Finitely Supported Commutative multiplication
+
+-/
+
+section
+
+variable {α R}
+
+/-- A commutative magma with multiplicative zero. -/
+class CommMagmaMulZeroClass (R : Type*) extends CommMagma R, MulZeroClass R
+
+instance [CommSemiring R] : CommMagmaMulZeroClass R where
+  zero_mul _ := zero_mul _
+  mul_zero _ := mul_zero _
+
+variable [CommMagmaMulZeroClass R]
+
+lemma Sym2.testB (f : α →₀ R) : ∀ (a : Sym2 α), mul (⇑f) a ≠ 0 → a ∈ f.support.sym2 :=
+    fun p hp => by
+  obtain ⟨a,b⟩ := p
+  simp only [Finset.mem_sym2_iff, mem_iff, Finsupp.mem_support_iff, ne_eq, forall_eq_or_imp,
+    forall_eq]
+  simp only [mul_sym2Mk, ne_eq] at hp
+  aesop
+
+/--
+`Sym2.mul` as a `Finsupp`
+-/
+noncomputable def Sym2.mul_finsupp (f : α →₀ R) :
+    Sym2 α →₀ R := Finsupp.onFinset
+      f.support.sym2
+    (Sym2.mul f) (Sym2.testB f)
+
+lemma Sym2.mul_finsupp_support (f : α →₀ R) :
+    (Sym2.mul_finsupp f).support ⊆ f.support.sym2 := fun p hp => by
+  apply Sym2.testB
+  simp_all only [Finsupp.mem_support_iff, ne_eq]
+  exact hp
+
+end
