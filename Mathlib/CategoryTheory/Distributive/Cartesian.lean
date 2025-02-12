52f2a9f78cdfc11af5c2e914a0fd5e674cf3c78f
Sina Hazratpour <sinahazratpour@gmail.com>
2025-02-12 22:00:13 +0100

    Update Mathlib/CategoryTheory/Distributive/Cartesian.lean
    Co-authored-by: Joël Riou <37772949+joelriou@users.noreply.github.com>

--- Mathlib/CategoryTheory/Distributive/Cartesian.lean
+++ Mathlib/CategoryTheory/Distributive/Cartesian.lean
@@ -3,7 +3,6 @@ Copyright (c) 2024 Sina Hazratpour. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Sina Hazratpour
 -/
-
 import Mathlib.CategoryTheory.ChosenFiniteProducts
 import Mathlib.CategoryTheory.Distributive.Monoidal
 import Mathlib.CategoryTheory.Limits.MonoCoprod
