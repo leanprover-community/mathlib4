8acf2db587d9e0dab4d9e25240656843d54e3389
Andrew Yang <the.erd.one@gmail.com>
2025-02-12 19:57:01 +0000

    fix

--- Mathlib.lean
+++ Mathlib.lean
@@ -4617,7 +4617,6 @@ import Mathlib.RingTheory.FreeCommRing
 import Mathlib.RingTheory.FreeRing
 import Mathlib.RingTheory.Generators
 import Mathlib.RingTheory.GradedAlgebra.Basic
-import Mathlib.RingTheory.GradedAlgebra.FiniteType
 import Mathlib.RingTheory.GradedAlgebra.Homogeneous.Ideal
 import Mathlib.RingTheory.GradedAlgebra.Homogeneous.Submodule
 import Mathlib.RingTheory.GradedAlgebra.HomogeneousLocalization
