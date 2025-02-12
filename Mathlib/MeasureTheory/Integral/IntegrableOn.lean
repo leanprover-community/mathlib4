53767deb25069eab7e1c9b4831c558a59d39e9fa
Arend Mellendijk <arend.mellendijk@gmail.com>
2025-02-12 15:37:18 +0100

    fix date

--- Mathlib/MeasureTheory/Integral/IntegrableOn.lean
+++ Mathlib/MeasureTheory/Integral/IntegrableOn.lean
@@ -57,7 +57,7 @@ theorem AEStronglyMeasurable.stronglyMeasurableAtFilter_of_mem {s}
     (h : AEStronglyMeasurable f (μ.restrict s)) (hl : s ∈ l) : StronglyMeasurableAtFilter f l μ :=
   ⟨s, hl, h⟩
 
-@[deprecated (since := "2025-12-02")]
+@[deprecated (since := "2025-02-12")]
 alias AeStronglyMeasurable.stronglyMeasurableAtFilter_of_mem :=
     AEStronglyMeasurable.stronglyMeasurableAtFilter_of_mem
 
