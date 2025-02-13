1a3657e0f163c3feae818c685b3e34529c1eb977
Anne C.A. Baanen <vierkantor@vierkantor.com>
2025-02-13 12:36:46 +0100

    chore(Data/Finset): avoid importing `Group` in `Finset.Image`
    This PR is a step on the way to completely removing algebra from the dependencies of measure theory. It reverses the import direction `Finset.Image -> Finset.Union` and splits off some naturals-specific lemmas (that don't seem to be used downstream anyway) from `Finset.Image`.

--- Mathlib/Data/Finset/Card.lean
+++ Mathlib/Data/Finset/Card.lean
@@ -3,7 +3,9 @@ Copyright (c) 2015 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Leonardo de Moura, Jeremy Avigad
 -/
+import Mathlib.Algebra.Group.Nat.Defs
 import Mathlib.Data.Finset.Image
+import Mathlib.Data.Int.Cast.Basic
 
 /-!
 # Cardinality of a finite set
