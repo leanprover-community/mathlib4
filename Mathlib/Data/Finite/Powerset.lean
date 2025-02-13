64cdeb433db6c5596db39d635e43cbff4ec45d74
Blizzard_inc <thrashy4inbox@gmail.com>
2025-02-13 12:25:17 +0100

    this instance is redundant

--- Mathlib/Data/Finite/Powerset.lean
+++ Mathlib/Data/Finite/Powerset.lean
@@ -13,8 +13,6 @@ variable {α : Type*}
 
 namespace Finite
 
-instance [Finite α] : Finite (Set α) := by
-  cases nonempty_fintype α
-  infer_instance
+instance [Finite α] : Finite (Set α) := inferInstance
 
 end Finite
