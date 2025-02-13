f96a161c287cff687fce8542099af6d97e0807e9
Markus Himmel <markus@lean-fro.org>
2025-02-13 13:20:02 +0100

    lake exe shake --fix

--- Mathlib/Algebra/Order/Floor.lean
+++ Mathlib/Algebra/Order/Floor.lean
@@ -6,7 +6,6 @@ Authors: Mario Carneiro, Kevin Kappelmann
 import Mathlib.Algebra.CharZero.Lemmas
 import Mathlib.Algebra.Group.Int.Even
 import Mathlib.Algebra.Group.Int.Units
-import Mathlib.Data.Int.Lemmas
 import Mathlib.Data.Nat.Cast.Order.Field
 import Mathlib.Data.Set.Subsingleton
 import Mathlib.Tactic.Abel
