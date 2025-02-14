4dcc3583d12762867152dedb366698a0c4f8f5bf
Nick Ward <102917377+gio256@users.noreply.github.com>
2025-02-14 08:16:34 +0000

    chore(AlgebraicTopology): split SimplexCategory.lean (#21625)
    We split `Mathlib/AlgebraicTopology/SimplexCategory.lean` into two files, `SimplexCategory/Defs.lean` and `SimplexCategory/Basic.lean`.
    
    - `Mathlib/AlgebraicTopology/SimplexCategory/Defs.lean` contains the core definitions and notations needed to define the simplex category, the truncated simplex category, and their respective `SmallCategory` instances.
    - `Mathlib/AlgebraicTopology/SimplexCategory/Basic.lean` contains everything else.

--- Mathlib.lean
+++ Mathlib.lean
@@ -1135,7 +1135,8 @@ import Mathlib.AlgebraicTopology.Quasicategory.Nerve
 import Mathlib.AlgebraicTopology.Quasicategory.StrictSegal
 import Mathlib.AlgebraicTopology.RelativeCellComplex.AttachCells
 import Mathlib.AlgebraicTopology.RelativeCellComplex.Basic
-import Mathlib.AlgebraicTopology.SimplexCategory
+import Mathlib.AlgebraicTopology.SimplexCategory.Basic
+import Mathlib.AlgebraicTopology.SimplexCategory.Defs
 import Mathlib.AlgebraicTopology.SimplicialCategory.Basic
 import Mathlib.AlgebraicTopology.SimplicialCategory.SimplicialObject
 import Mathlib.AlgebraicTopology.SimplicialNerve
