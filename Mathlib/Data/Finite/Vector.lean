fc2bdc868abea028d2e6bd8b11fcd599b6c3be84
Anne C.A. Baanen <vierkantor@vierkantor.com>
2025-02-14 11:29:50 +0100

    More shaking

--- Mathlib/Data/Finite/Vector.lean
+++ Mathlib/Data/Finite/Vector.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kyle Miller
 -/
 import Mathlib.Data.Fintype.Vector
-import Mathlib.Data.Fintype.Pigeonhole
 
 /-!
 # Finiteness of vector types
