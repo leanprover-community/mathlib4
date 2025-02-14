9745c14a34aa810f01635ff747bb411c61ff8a7d
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-02-14 11:01:44 +0100

    fix

--- Mathlib/AlgebraicTopology/SimplexCategory/MorphismProperty.lean
+++ Mathlib/AlgebraicTopology/SimplexCategory/MorphismProperty.lean
@@ -3,7 +3,7 @@ Copyright (c) 2025 Joël Riou. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joël Riou
 -/
-import Mathlib.AlgebraicTopology.SimplexCategory
+import Mathlib.AlgebraicTopology.SimplexCategory.Basic
 import Mathlib.CategoryTheory.MorphismProperty.Composition
 
 /-!
