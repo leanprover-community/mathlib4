5e40c460112910f5fd777997f5eb249eff0a04ca
Violeta Hernández <vi.hdz.p@gmail.com>
2025-02-14 12:22:50 -0600

    fix (?)

--- Mathlib/Data/String/Basic.lean
+++ Mathlib/Data/String/Basic.lean
@@ -152,7 +152,7 @@ instance : LinearOrder String where
   compare_eq_compareOfLessAndEq a b := by
     simp only [compare, compareOfLessAndEq, instLT, List.instLT, lt_iff_toList_lt, toList]
     split_ifs <;>
-    simp only [List.lt_iff_lex_lt] at * <;>
+    simp only [List.lt_iff_lex_lt] at *
     contradiction
 
 end String
