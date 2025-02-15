7e1575e0f18c860a1d9da2f5ef18917f5a6e2b0c
adomani <adomani@gmail.com>
2025-02-15 21:47:24 +0100

    remove comment

--- Mathlib/Tactic/FindSyntax.lean
+++ Mathlib/Tactic/FindSyntax.lean
@@ -83,7 +83,6 @@ elab "#find_syntax " id:str d:(&" approx")? : command => do
       let mod := (← findModuleOf? nm).getD (← getMainModule)
       match_results := match_results.insert mod <| (match_results.findD mod #[]).push (nm, rem.trim)
   -- We sort the messages to produce a more stable output.
-  -- let msgsToString ← msgs.mapM (·.toString)
   let sorted_results := match_results.toArray.qsort fun (mod1, _) (mod2, _) =>
     (mod1.toString < mod2.toString)
   let sorted_results := sorted_results.map fun (mod, msgs) =>
