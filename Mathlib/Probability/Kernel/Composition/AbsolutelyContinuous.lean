29d6c67730506bbfee4c7512a23ca93b5ae2f370
Remy Degenne <remydegenne@gmail.com>
2025-02-15 09:07:57 +0100

    add author

--- Mathlib/Probability/Kernel/Composition/AbsolutelyContinuous.lean
+++ Mathlib/Probability/Kernel/Composition/AbsolutelyContinuous.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2025 Rémy Degenne. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Rémy Degenne
+Authors: Rémy Degenne, Lorenzo Luccioli
 -/
 import Mathlib.Probability.Kernel.Composition.MeasureCompProd
 import Mathlib.Probability.Kernel.RadonNikodym
