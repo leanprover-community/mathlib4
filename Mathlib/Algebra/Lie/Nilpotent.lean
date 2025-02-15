bc41803a8123ed538b7180d564ef9c7d1f4d3d22
Janos Wolosz <janos.wolosz@gmail.com>
2025-02-15 16:07:12 +0100

    Fix

--- Mathlib/Algebra/Lie/Nilpotent.lean
+++ Mathlib/Algebra/Lie/Nilpotent.lean
@@ -266,8 +266,8 @@ lemma IsNilpotent.nilpotent [IsNilpotent L M] : ∃ k, lowerCentralSeries R L M 
   (isNilpotent_iff R L M).mp ‹_›
 
 variable {R L} in
-lemma IsNilpotent.mk {k : ℕ} (h : lowerCentralSeries R L M k = ⊥) : IsNilpotent L M := by
-  have h := isNilpotent_iff R L M
+lemma IsNilpotent.mk {k : ℕ} (h : lowerCentralSeries R L M k = ⊥) : IsNilpotent L M :=
+  (isNilpotent_iff R L M).mpr ⟨k, h⟩
 
 @[deprecated IsNilpotent.nilpotent (since := "2025-01-07")]
 theorem exists_lowerCentralSeries_eq_bot_of_isNilpotent [IsNilpotent L M] :
