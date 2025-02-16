c307240222130c305885be04872b3a3628e4f104
Jakob von Raumer <jakob@von-raumer.de>
2025-02-16 20:36:30 +0000

    feat(CategoryTheory): the strict functor assigning to each category its opposite category (#21954)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1762,6 +1762,7 @@ import Mathlib.CategoryTheory.Category.Cat
 import Mathlib.CategoryTheory.Category.Cat.Adjunction
 import Mathlib.CategoryTheory.Category.Cat.AsSmall
 import Mathlib.CategoryTheory.Category.Cat.Limit
+import Mathlib.CategoryTheory.Category.Cat.Op
 import Mathlib.CategoryTheory.Category.Factorisation
 import Mathlib.CategoryTheory.Category.GaloisConnection
 import Mathlib.CategoryTheory.Category.Grpd
