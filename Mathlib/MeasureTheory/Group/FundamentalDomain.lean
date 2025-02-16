7bb135272caa35409ee5cc0bafb19f1ad9974117
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2025-02-16 05:38:58 +0000

    fix

--- Mathlib/MeasureTheory/Group/FundamentalDomain.lean
+++ Mathlib/MeasureTheory/Group/FundamentalDomain.lean
@@ -138,7 +138,7 @@ theorem measure_ne_zero [Countable G] [SMulInvariantMeasure G α μ]
     (hμ : μ ≠ 0) (h : IsFundamentalDomain G s μ) : μ s ≠ 0 := by
   have hc := measure_univ_pos.mpr hμ
   contrapose! hc
-  rw [← measure_congr h.iUnion_smul_ae_eq]
+  rw [← measure_congr h.iUnion_smul_ae_eq, ← nonpos_iff_eq_zero]
   refine le_trans (measure_iUnion_le _) ?_
   simp_rw [measure_smul, hc, tsum_zero, le_refl]
 
