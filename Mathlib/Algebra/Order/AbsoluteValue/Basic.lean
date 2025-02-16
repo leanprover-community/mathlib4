3017ce5e29b0364c4296b2e5458bda2f3259a219
Fabrizio Barroero <fbarroero@gmail.com>
2025-02-16 22:02:34 +0100

    WIP WIP
    ---
    <!-- The text above the `---` will become the commit message when your
    PR is merged. Please leave a blank newline before the `---`, otherwise
    GitHub will format the text above it as a title.
    
    For details on the "pull request lifecycle" in mathlib, please see:
    https://leanprover-community.github.io/contribute/index.html
    
    In particular, note that most reviewers will only notice your PR
    if it passes the continuous integration checks.
    Please ask for help on https://leanprover.zulipchat.com if needed.
    
    To indicate co-authors, include lines at the bottom of the commit message
    (that is, before the `---`) using the following format:
    
    Co-authored-by: Author Name <author@email.com>
    
    If you are moving or deleting declarations, please include these lines at the bottom of the commit message
    (that is, before the `---`) using the following format:
    
    Moves:
    - Vector.* -> List.Vector.*
    - ...
    
    Deletions:
    - Nat.bit1_add_bit1
    - ...
    
    Any other comments you want to keep out of the PR commit should go
    below the `---`, and placed outside this HTML comment, or else they
    will be invisible to reviewers.
    
    If this PR depends on other PRs, please list them below this comment,
    using the following format:
    - [ ] depends on: #abc [optional extra text]
    - [ ] depends on: #xyz [optional extra text]
    
    -->
    
    [![Open in Gitpod](https://gitpod.io/button/open-in-gitpod.svg)](https://gitpod.io/from-referrer/)

--- Mathlib/Algebra/Order/AbsoluteValue/Basic.lean
+++ Mathlib/Algebra/Order/AbsoluteValue/Basic.lean
@@ -3,6 +3,7 @@ Copyright (c) 2021 Anne Baanen. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro, Anne Baanen
 -/
+import Mathlib.Algebra.BigOperators.Group.Finset.Basic
 import Mathlib.Algebra.GroupWithZero.Units.Lemmas
 import Mathlib.Algebra.Order.Field.Defs
 import Mathlib.Algebra.Order.Hom.Basic
@@ -253,6 +254,30 @@ lemma eq_on_nat_iff_eq_on_int {f g : AbsoluteValue R S} :
 
 end OrderedCommRing
 
+section LinearOrderedSemiring
+
+variable [Semiring R] [LinearOrderedSemiring S]
+
+/--An `AbsoluteValue` `f` is nonarchimean if it satisfies `f (x + y) ≤ max (f x) (f y)` for all
+`x` and `y` in `R`-/
+def IsNonarchimedean  (f : AbsoluteValue R S) : Prop := ∀ x y : R, f (x + y) ≤ max (f x) (f y)
+
+open Finset in
+/--Ultrametric inequality with Finset.Sum-/
+lemma sum_le_sup_of_isNonarchimedean {f : AbsoluteValue R S} (nonarch : IsNonarchimedean f)
+    {α : Type*} {s : Finset α} (hnonempty : s.Nonempty) {l : α → R} :
+    f (∑ i ∈ s, l i) ≤ s.sup' hnonempty fun i => f (l i) := by
+  apply Nonempty.cons_induction (p := fun a hn ↦ f (∑ i ∈ a, l i) ≤ a.sup' hn fun i ↦ f (l i))
+  · simp
+  · intro a s _ hs hind
+    simp only [le_sup'_iff, mem_cons, sum_cons, exists_eq_or_imp]
+    rw [← le_sup'_iff hs]
+    rcases le_max_iff.mp <| nonarch (l a) (∑ i ∈ s, l i) with h₁ | h₂
+    · exact .inl h₁
+    · exact .inr <| le_trans h₂ hind
+
+end LinearOrderedSemiring
+
 section LinearOrderedRing
 
 variable {R S : Type*} [Semiring R] [LinearOrderedRing S] (abv : AbsoluteValue R S)
@@ -269,6 +294,14 @@ protected def abs : AbsoluteValue S S where
 instance : Inhabited (AbsoluteValue S S) :=
   ⟨AbsoluteValue.abs⟩
 
+lemma apply_nat_le_one_of_isNonarchimedean [Nontrivial R] {abv : AbsoluteValue R S}
+    (nonarch : AbsoluteValue.IsNonarchimedean abv) (n : ℕ) : abv n ≤ 1 := by
+  induction n with
+  | zero => simp
+  | succ n hn =>
+    push_cast
+    exact le_trans (nonarch n 1) (max_le hn <| le_of_eq abv.map_one)
+
 end LinearOrderedRing
 
 section LinearOrderedCommRing
@@ -278,6 +311,11 @@ variable {R S : Type*} [Ring R] [LinearOrderedCommRing S] (abv : AbsoluteValue R
 @[bound]
 theorem abs_abv_sub_le_abv_sub (a b : R) : abs (abv a - abv b) ≤ abv (a - b) :=
   abs_sub_le_iff.2 ⟨abv.le_sub _ _, by rw [abv.map_sub]; apply abv.le_sub⟩
+
+lemma apply_int_le_one_of_isNonarchimedean [Nontrivial R] {abv : AbsoluteValue R S}
+    (nonarch : IsNonarchimedean abv) (x : ℤ) : abv x ≤ 1 := by
+  rw [← AbsoluteValue.apply_natAbs_eq]
+  exact apply_nat_le_one_of_isNonarchimedean nonarch x.natAbs
 
 end LinearOrderedCommRing
 
