3873c401ebb78f607a0e5966594cd3621a975314
Eric Wieser <wieser.eric@gmail.com>
2025-02-17 23:37:15 +0000

    Use √ notation

--- Mathlib/Tactic/NormNum/RealSqrt.lean
+++ Mathlib/Tactic/NormNum/RealSqrt.lean
@@ -21,20 +21,20 @@ namespace Tactic.NormNum
 open Qq Lean Lean.Meta Elab.Tactic Mathlib.Meta.NormNum NNReal
 
 lemma isNat_realSqrt {x : ℝ} {nx ny : ℕ} (h : IsNat x nx) (hy : ny * ny = nx) :
-    IsNat (Real.sqrt x) ny := ⟨by simp [h.out, ← hy]⟩
+    IsNat √x ny := ⟨by simp [h.out, ← hy]⟩
 
 lemma isNat_nnrealSqrt {x : ℝ≥0} {nx ny : ℕ} (h : IsNat x nx) (hy : ny * ny = nx) :
     IsNat (NNReal.sqrt x) ny := ⟨by simp [h.out, ← hy]⟩
 
 lemma isNat_realSqrt_neg {x : ℝ} {nx : ℕ} (h : IsInt x (Int.negOfNat nx)) :
-    IsNat (Real.sqrt x) 0 := by
+    IsNat √x 0 := by
   refine ⟨?_⟩
   simp only [Nat.cast_zero]
   refine Real.sqrt_eq_zero_of_nonpos ?_
   simp [h.out]
 
 lemma isNat_realSqrt_neg_of_isRat {x : ℝ} {num : ℤ} {denom : ℕ} (hnum : num ≤ 0)
-    (h : IsRat x num denom) : IsNat (Real.sqrt x) 0 := by
+    (h : IsRat x num denom) : IsNat √x 0 := by
   refine ⟨?_⟩
   simp only [Nat.cast_zero]
   refine Real.sqrt_eq_zero_of_nonpos ?_
@@ -46,7 +46,7 @@ lemma isNat_realSqrt_neg_of_isRat {x : ℝ} {num : ℤ} {denom : ℕ} (hnum : nu
 
 lemma isRat_realSqrt {x : ℝ} {n sn : ℤ} {d sd : ℕ} (hn : sn * sn = n)
     (hn₂ : 0 ≤ sn) (hd : sd * sd = d) (hd₂ : sd ≠ 0) (h : IsRat x n d) :
-    IsRat (Real.sqrt x) sn sd := by
+    IsRat √x sn sd := by
   refine .mk (invertibleOfNonzero (by exact_mod_cast hd₂)) ?out
   let .mk inv eq := h
   rw [eq, ← hn, invOf_eq_inv, ← hd]
@@ -55,11 +55,11 @@ lemma isRat_realSqrt {x : ℝ} {n sn : ℤ} {d sd : ℕ} (hn : sn * sn = n)
   aesop
 
 /-- `norm_num` extension that evaluates the function `Real.sqrt`. -/
-@[norm_num Real.sqrt _]
+@[norm_num √_]
 def evalRealSqrt : NormNumExt where eval {u α} e := do
   let .app _ (x : Q(ℝ)) ← whnfR e | failure
-  have : u =QL 0 := ⟨⟩; have : $α =Q ℝ := ⟨⟩; have : $e =Q Real.sqrt $x := ⟨⟩
-  let res ← derive (α := q(ℝ)) x
+  have : u =QL 0 := ⟨⟩; have : $α =Q ℝ := ⟨⟩; have : $e =Q √$x := ⟨⟩
+  let res ← derive x
   match res with
   | .isBool _ _ => failure
   | .isNat sℝ ex pf =>
@@ -69,13 +69,13 @@ def evalRealSqrt : NormNumExt where eval {u α} e := do
         have ey : Q(ℕ) := mkRawNatLit y
         have pf₁ : Q($ey * $ey = $ex) := (q(Eq.refl $ex) : Expr)
         assumeInstancesCommute
-        have pf_final : Q(IsNat (Real.sqrt $x) $ey) := q(isNat_realSqrt $pf $pf₁)
+        have pf_final : Q(IsNat √$x $ey) := q(isNat_realSqrt $pf $pf₁)
         return .isNat sℝ ey pf_final
       else failure
   | .isNegNat _ ex pf =>
       -- Recall that `Real.sqrt` returns 0 for negative inputs
       assumeInstancesCommute
-      have pf_final : Q(IsNat (Real.sqrt $x) 0) := q(isNat_realSqrt_neg $pf)
+      have pf_final : Q(IsNat (√$x) 0) := q(isNat_realSqrt_neg $pf)
       return .isNat q(inferInstance) (mkRawNatLit 0) pf_final
   | .isRat sℝ eq en ed pf =>
       let n' : ℤ := en.intLit!
@@ -84,7 +84,7 @@ def evalRealSqrt : NormNumExt where eval {u α} e := do
         -- Square root of a negative number, defined to be zero
         let hnum : Q($en ≤ 0) ← mkDecideProof q($en ≤ 0)
         assumeInstancesCommute
-        have pf_final : Q(IsNat (Real.sqrt $x) 0) := q(isNat_realSqrt_neg_of_isRat $hnum $pf)
+        have pf_final : Q(IsNat (√$x) 0) := q(isNat_realSqrt_neg_of_isRat $hnum $pf)
         return .isNat q(inferInstance) (mkRawNatLit 0) pf_final
       let n : ℕ := n'.toNat
       let sn : ℤ := Nat.sqrt n
@@ -97,7 +97,7 @@ def evalRealSqrt : NormNumExt where eval {u α} e := do
         let hd : Q($esd * $esd = $ed) ← mkDecideProof q($esd * $esd = $ed)
         let hd₂ : Q($esd ≠ 0) ← mkDecideProof q($esd ≠ 0)
         assumeInstancesCommute
-        have pf_final : Q(IsRat (Real.sqrt $x) $esn $esd) :=
+        have pf_final : Q(IsRat √$x $esn $esd) :=
           q(isRat_realSqrt $hn $hn₂ $hd $hd₂ $pf)
         return .isRat sℝ (sn / sd) esn esd pf_final
       else failure
@@ -108,7 +108,7 @@ def evalNNRealSqrt : NormNumExt where eval {u α} e := do
   let e' : Q($α) ← whnfR e
   match u, α, e' with
   | 0, ~q(NNReal), ~q(NNReal.sqrt $x) =>
-    let res ← derive (α := q(ℝ≥0)) x
+    let res ← derive x
     match res with
     | .isBool _ _ => failure
     | .isNat sℝ ex pf =>
