aa81c9ede9542052bc05a79ab1a8483129273db1
Anne C.A. Baanen <vierkantor@vierkantor.com>
2025-02-17 16:24:08 +0100

    Readd missing import

--- Mathlib/GroupTheory/OrderOfElement.lean
+++ Mathlib/GroupTheory/OrderOfElement.lean
@@ -5,6 +5,7 @@ Authors: Johannes Hölzl, Julian Kuelshammer
 -/
 import Mathlib.Algebra.CharP.Defs
 import Mathlib.Algebra.Group.Pointwise.Set.Finite
+import Mathlib.Algebra.Group.Semiconj.Basic
 import Mathlib.Algebra.Group.Subgroup.Finite
 import Mathlib.Algebra.Module.NatInt
 import Mathlib.Algebra.Order.Group.Action
