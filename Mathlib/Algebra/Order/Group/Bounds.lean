2a91feb61a8f8c1c40300ad8eac473cada8ecdfd
Anne C.A. Baanen <vierkantor@vierkantor.com>
2025-02-17 21:36:32 +0100

    Shake

--- Mathlib/Algebra/Order/Group/Bounds.lean
+++ Mathlib/Algebra/Order/Group/Bounds.lean
@@ -3,7 +3,8 @@ Copyright (c) 2017 Johannes Hölzl. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl, Yury Kudryashov
 -/
-import Mathlib.Algebra.Order.Group.Basic
+import Mathlib.Algebra.Order.Group.Defs
+import Mathlib.Algebra.Order.Group.Unbundled.Basic
 import Mathlib.Order.Bounds.Basic
 
 /-!
