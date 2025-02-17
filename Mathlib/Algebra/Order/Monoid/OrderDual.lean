63fee78ce317424d3794cd55246c53aabcab8d91
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2025-02-17 14:20:54 +0100

    todo

--- Mathlib/Algebra/Order/Monoid/OrderDual.lean
+++ Mathlib/Algebra/Order/Monoid/OrderDual.lean
@@ -17,24 +17,29 @@ open Function
 
 namespace OrderDual
 
+-- TODO rename inst
 @[to_additive]
 instance orderedCommMonoid [OrderedCommMonoid α] : OrderedCommMonoid αᵒᵈ :=
   { mul_le_mul_left := fun _ _ h c => mul_le_mul_left' h c }
 
+-- TODO rename inst
 @[to_additive OrderDual.OrderedCancelAddCommMonoid.to_mulLeftReflectLE]
 instance OrderedCancelCommMonoid.to_mulLeftReflectLE [OrderedCancelCommMonoid α] :
     MulLeftReflectLE αᵒᵈ where
     elim a b c := OrderedCancelCommMonoid.le_of_mul_le_mul_left (α := α) a c b
 
+-- TODO rename inst
 @[to_additive]
 instance orderedCancelCommMonoid [OrderedCancelCommMonoid α] : OrderedCancelCommMonoid αᵒᵈ :=
   { le_of_mul_le_mul_left := fun _ _ _ : α => le_of_mul_le_mul_left' }
 
+-- TODO rename inst
 @[to_additive]
 instance linearOrderedCancelCommMonoid [LinearOrderedCancelCommMonoid α] :
     LinearOrderedCancelCommMonoid αᵒᵈ :=
   { OrderDual.instLinearOrder α, OrderDual.orderedCancelCommMonoid with }
 
+-- TODO rename inst
 @[to_additive]
 instance linearOrderedCommMonoid [LinearOrderedCommMonoid α] : LinearOrderedCommMonoid αᵒᵈ :=
   { OrderDual.instLinearOrder α, OrderDual.orderedCommMonoid with }
