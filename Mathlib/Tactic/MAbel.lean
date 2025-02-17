978e07f8fd2d0b8d57fd6065f6e775716181f0f7
Sam Ezeh <sam.z.ezeh@gmail.com>
2025-02-17 08:55:12 +0000

    Implement the mabel tactic

--- Mathlib/Tactic/MAbel.lean
+++ Mathlib/Tactic/MAbel.lean
@@ -25,9 +25,21 @@ initialize registerTraceClass `mabel.detail
 
 variable {α : Type*}
 
-lemma Additive_identity : Additive α = α := rfl
-
-lemma ofMul_identity (x : α) : Additive.ofMul x = x := rfl
+abbrev ofMul' (α : Type*) := Additive.ofMul (α := α)
+lemma ofMul_mul' [CommMonoid α] (x y : α) :
+    Additive.ofMul.toFun (x * y) = Additive.ofMul.toFun x + Additive.ofMul.toFun y := rfl
+lemma ofMul_pow' [CommMonoid α] (x : α) (n : ℕ) :
+    Additive.ofMul.toFun (x^n) = n • Additive.ofMul.toFun x := rfl
+lemma ofMul_one' [CommMonoid α] :
+    Additive.ofMul.toFun (1 : α) = 0 := rfl
+lemma ofMul_inv' [Inv α] (x : α) :
+    Additive.ofMul.toFun x⁻¹ = -Additive.ofMul.toFun x := rfl
+lemma ofMul_div' [Div α] (x y : α) :
+    Additive.ofMul.toFun (x / y) = Additive.ofMul.toFun x - Additive.ofMul.toFun y := rfl
+lemma ofMul_zpow' [CommGroup α] (x : α) (z : ℤ) :
+    Additive.ofMul.toFun (x^z) = z • Additive.ofMul.toFun x := rfl
+lemma ofMul'_eq_ofMul (x : α)  : (ofMul' _).toFun x = Additive.ofMul.toFun x := rfl
+lemma ofMul_eq_zero' [CommMonoid α] (x : α) : ofMul' _ x = 0 ↔ x = 1 := ofMul_eq_zero
 
 /-- Tactic for converting equations in the language of
 multiplicative, commutative monoids and groups to those in the language
@@ -35,42 +47,66 @@ of additive, commutative monoids and groups.
 -/
 syntax (name := to_additive) "to_additive"  (location)? : tactic
 
+/-- The core of `to_additive` which converts multiplicative structures
+to additive structures. -/
+partial def toAdditiveCore (e : Expr) : MetaM Simp.Result := do
+ let thms := [
+    ``ofMul'_eq_ofMul,
+    ``ofMul_mul',  ``ofMul_pow',
+    ``ofMul_one', ``ofMul_inv', ``ofMul_div',
+    ``ofMul_zpow', ``ofMul_eq_zero', ``ofMul_div'
+  ]
+  let ctx ← Simp.mkContext
+    (simpTheorems := #[← thms.foldlM (·.addConst ·) {:_}])
+    (congrTheorems := ← getSimpCongrTheorems)
 
-/-- The core of `to_additive` which converts an equalites over
-multiplicative structures to equalities over additive structures. -/
-def toAdditiveCore (e₁ : Expr) (e₂ : Expr) : TacticM Unit := do
-  -- TODO: Rewrite using Simp.Result
-  let lhs ← delab e₁
-  let rhs ← delab e₂
-  evalTactic <| ← `(tactic|
-    conv => {
-      lhs
-      rw [← ofMul_identity $lhs];
-    } <;>
-    conv => {
-      rhs
-      rw [ ← ofMul_identity $rhs]
-    } <;>
-    simp only [
-      ofMul_mul, ofMul_pow, ofMul_one,
-      ofMul_inv, ofMul_div, ofMul_zpow,
-      ofMul_eq_zero, ofMul_div
-    ])
+  let rec
+    to_additive (e : Expr) := do
+      let is_multiplicative_operation : Name → Bool
+        | ``HMul.hMul => True
+        | ``HDiv.hDiv => True
+        | _ => False
+      let rec should_convert : Expr → Bool
+        | .app (.const name _) _ => is_multiplicative_operation name 
+        | _ => False
+      let rec go : Expr → MetaM Expr
+        | .app fn arg
+            => if should_convert fn then
+                return ← to_additive (.app fn arg)
+              else
+                return .app (← (to_additive fn)) (← (to_additive arg))
+        | exp@(.bvar _) => return ← to_additive exp
+        | exp => return exp
+      let α ← inferType e
+      let e' ← match (← synthInstance? (← mkAppM ``CommMonoid #[α])) with
+        | .some _ => mkAppM ``Equiv.toFun #[← mkAppM ``ofMul' #[α], e]
+        | .none => go e
+      return e'
+  let e' ← to_additive e
+  return (← (Meta.simp e' ctx)).1
 
 /-- Use `to_additive` to rewrite the main goal. -/
 def toAdditiveTarget : TacticM Unit := withMainContext do
-  let some (_, e₁, e₂) := (← whnfR <| ← getMainTarget).eq?
-    | throwError "to_additive requires an equality goal"
-  try toAdditiveCore e₁ e₂
-  catch | _ => throwError "to_additive made no progress"
+  let goal ← getMainGoal
+  let tgt ← withReducible goal.getType'
+  let result ← toAdditiveCore tgt
+  if result.expr.isConstOf ``True then
+    goal.assign (← mkOfEqTrue (← result.getProof))
+    replaceMainGoal []
+  else
+    if result.expr == tgt then throwError "to_additive made no progress"
+    replaceMainGoal [← applySimpResultToTarget goal tgt result]
+
 
 /-- Use `to_additive` to rewrite hypothesis `h`. -/
 def toAdditiveLocalDecl (fvarId : FVarId) : TacticM Unit := withMainContext do
   let tgt ← instantiateMVars (← fvarId.getType)
-  let some (_, e₁, e₂) := (← whnfR <| tgt).eq?
-    | throwError "to_additive requires an equality goal"
-  try toAdditiveCore e₁ e₂
-  catch | _ => throwError "to_additive made no progress"
+  let goal ← getMainGoal
+  let result ← toAdditiveCore tgt
+  if result.expr == tgt then throwError "to_additive made no progress"
+  match ← applySimpResultToLocalDecl goal fvarId result false with
+    | none => replaceMainGoal []
+    | some (_, newGoal) => replaceMainGoal [newGoal]
 
 /-- The `to_additive` tactic, which solves converts equations in the language of
 multiplicative commutative additive groups (or monoids) to those in additive structures. -/
@@ -87,25 +123,30 @@ open Lean Elab Meta Tactic
 multiplicative commutative additive groups (or monoids). -/
 syntax (name := mabel1) "mabel1" "!"? : tactic
 elab_rules : tactic | `(tactic| mabel1 $[!%$tk]?) => withMainContext do
+  try toAdditiveTarget
+  catch | e => throwError "mabel1 made no progress: {e.toMessageData}"
+  let tm := if tk.isSome then .default else .reducible
   let some (_, e₁, e₂) := (← whnfR <| ← getMainTarget).eq?
-    | throwError "mabel1 requires an equality goal"
-  try toAdditiveCore e₁ e₂
-  catch | _ => throwError "mabel1 made no progress"
-  try
-    if tk.isSome then do
-      evalTactic (← `(tactic| abel1!))
-    else do
-      evalTactic (← `(tactic| abel1))
-  catch
-    | e => throwError "mabel1 made no progress: {e.toMessageData}"
+    | throwError "mabel1 made no progress"
+  trace[mabel] "running on an equality `{e₁} = {e₂}`."
+  let c ← Abel.mkContext e₁
+  closeMainGoal `mabel1 <| ← AtomM.run tm <| ReaderT.run (r := c) do
+    let (e₁', p₁) ← Abel.eval e₁
+    trace[mabel] "found `{p₁}`, a proof that `{e₁} = {e₁'.e}`"
+    let (e₂', p₂) ← Abel.eval e₂
+    trace[mabel] "found `{p₂}`, a proof that `{e₂} = {e₂'.e}`"
+    unless ← isDefEq e₁' e₂' do
+      throwError "mabel1 found that the two sides were not equal"
+    trace[mabel] "verified that the simplified forms are identical"
+    mkEqTrans p₁ (← mkEqSymm p₂)
 
 @[inherit_doc mabel1] macro (name := mabel1!) "mabel1!" : tactic => `(tactic| mabel1 !)
 
 /--
 Tactic for evaluating expressions in multiplicative abelian groups.
+Fails if the target is not an equality.
 
 * `mabel!` will use a more aggressive reducibility setting to determine equality of atoms.
-* `mabel1` fails if the target is not an equality.
 
 For example:
 
@@ -136,10 +177,13 @@ elab (name := mabelNF) "mabel_nf" tk:"!"? cfg:optConfig loc:(location)? : tactic
   if tk.isSome then cfg := { cfg with red := .default }
   let loc := (loc.map expandLocation).getD (.targets #[] true)
   let s ← IO.mkRef {}
-  withLocation loc (toAdditiveLocalDecl) (toAdditiveTarget)
-    fun _ ↦ throwError "mabel_nf made no progress"
+  try
+    withLocation loc (toAdditiveLocalDecl) (toAdditiveTarget)
+      fun _ ↦ throwError "mabel_nf made no progress"
+  catch
+    | _ => throwError "mabel_nf made no progress"
   withLocation loc (Abel.abelNFLocalDecl s cfg) (Abel.abelNFTarget s cfg)
-    fun _ ↦ throwError "mabel_nf made no progress"
+     fun _ ↦ throwError "mabel_nf made no progress"
 
 /--
 The tactic `mabel` evaluates expressions in multiplicative abelian groups.
@@ -151,5 +195,5 @@ macro (name := abelConv) "mabel" : conv =>
   `(conv| first | discharge => mabel1)
 @[inherit_doc abelConv] macro "mabel!" : conv =>
   `(conv| first | discharge => mabel1!)
-
+example [CommMonoid α] (a b : α) : a * (b * a) = a * a * b := by mabel
 end Mathlib.Tactic.MAbel
