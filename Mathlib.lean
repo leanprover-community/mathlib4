f3066cd9bf830e27c8106abfc749954585b7d419
Amelia Livingston <101damnations@github.com>
2025-02-17 16:56:21 +0000

    feat: Hopf algebra structure on monoid algebras  (#13609)
    Given a commutative semiring `R`, an `R`-bialgebra (resp. `R`-Hopf algebra) `A`, and a(n) (additive) monoid (resp. (additive) group) `G`, this PR defines the `R`-bialgebra (resp. `R`-Hopf algebra) structure on `(Add)MonoidAlgebra A G`. 
    
    
    
    Co-authored-by: Amelia Livingston <al3717@ic.ac.uk>

--- Mathlib.lean
+++ Mathlib.lean
@@ -4577,6 +4577,7 @@ import Mathlib.RingTheory.Bezout
 import Mathlib.RingTheory.Bialgebra.Basic
 import Mathlib.RingTheory.Bialgebra.Equiv
 import Mathlib.RingTheory.Bialgebra.Hom
+import Mathlib.RingTheory.Bialgebra.MonoidAlgebra
 import Mathlib.RingTheory.Bialgebra.TensorProduct
 import Mathlib.RingTheory.Binomial
 import Mathlib.RingTheory.ChainOfDivisors
@@ -4584,6 +4585,7 @@ import Mathlib.RingTheory.ClassGroup
 import Mathlib.RingTheory.Coalgebra.Basic
 import Mathlib.RingTheory.Coalgebra.Equiv
 import Mathlib.RingTheory.Coalgebra.Hom
+import Mathlib.RingTheory.Coalgebra.MonoidAlgebra
 import Mathlib.RingTheory.Coalgebra.TensorProduct
 import Mathlib.RingTheory.Complex
 import Mathlib.RingTheory.Congruence.Basic
@@ -4673,6 +4675,7 @@ import Mathlib.RingTheory.HahnSeries.Summable
 import Mathlib.RingTheory.HahnSeries.Valuation
 import Mathlib.RingTheory.Henselian
 import Mathlib.RingTheory.HopfAlgebra.Basic
+import Mathlib.RingTheory.HopfAlgebra.MonoidAlgebra
 import Mathlib.RingTheory.HopfAlgebra.TensorProduct
 import Mathlib.RingTheory.HopkinsLevitzki
 import Mathlib.RingTheory.Ideal.AssociatedPrime
