b82205803ccd781c4343327effbc499009a4d3e9
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-02-17 15:29:49 +0100

    moved to GrothendieckAxioms/Colim.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -1695,7 +1695,6 @@ import Mathlib.CategoryTheory.Abelian.GrothendieckAxioms.Indization
 import Mathlib.CategoryTheory.Abelian.GrothendieckAxioms.Sheaf
 import Mathlib.CategoryTheory.Abelian.GrothendieckAxioms.Types
 import Mathlib.CategoryTheory.Abelian.GrothendieckCategory.Basic
-import Mathlib.CategoryTheory.Abelian.GrothendieckCategory.Colim
 import Mathlib.CategoryTheory.Abelian.GrothendieckCategory.Subobject
 import Mathlib.CategoryTheory.Abelian.Images
 import Mathlib.CategoryTheory.Abelian.Injective
