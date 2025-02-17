8c170401165688308615e01dc4fc496ea1e5db90
101damnations <101damnations@github.com>
2025-02-17 20:25:13 +0000

    fix

--- Mathlib/Algebra/Category/HopfAlgebraCat/Monoidal.lean
+++ Mathlib/Algebra/Category/HopfAlgebraCat/Monoidal.lean
@@ -10,8 +10,8 @@ import Mathlib.RingTheory.HopfAlgebra.TensorProduct
 /-!
 # The monoidal structure on the category of Hopf algebras
 
-In `Mathlib.RingTheory.HopfAlgebra.TensorProduct`, given two Hopf `R`-algebras `A, B`, we define a
-Hopf `R`-algebra instance on `A ⊗[R] B`.
+In `Mathlib/RingTheory/HopfAlgebra/TensorProduct.lean`, given two Hopf `R`-algebras `A, B`, we
+define a Hopf `R`-algebra instance on `A ⊗[R] B`.
 
 Here, we use this to declare a `MonoidalCategory` instance on the category of Hopf algebras, via
 the existing monoidal structure on `BialgebraCat`.
@@ -26,29 +26,29 @@ variable (R : Type u) [CommRing R]
 
 @[simps] noncomputable instance instMonoidalCategoryStruct :
     MonoidalCategoryStruct.{u} (HopfAlgebraCat R) where
-  tensorObj := fun X Y => HopfAlgebraCat.of R (X ⊗[R] Y)
-  whiskerLeft := fun X _ _ f => HopfAlgebraCat.ofHom (f.1.lTensor X)
-  whiskerRight := fun f X => HopfAlgebraCat.ofHom (f.1.rTensor X)
-  tensorHom := fun f g => HopfAlgebraCat.ofHom (Bialgebra.TensorProduct.map f.1 g.1)
-  tensorUnit := HopfAlgebraCat.of R R
-  associator := fun X Y Z => (Bialgebra.TensorProduct.assoc R X Y Z).toHopfAlgebraCatIso
-  leftUnitor := fun X => (Bialgebra.TensorProduct.lid R X).toHopfAlgebraCatIso
-  rightUnitor := fun X => (Bialgebra.TensorProduct.rid R X).toHopfAlgebraCatIso
+  tensorObj X Y := of R (X ⊗[R] Y)
+  whiskerLeft X _ _ f := ofHom (f.1.lTensor X)
+  whiskerRight f X := ofHom (f.1.rTensor X)
+  tensorHom f g := ofHom (Bialgebra.TensorProduct.map f.1 g.1)
+  tensorUnit := of R R
+  associator X Y Z := (Bialgebra.TensorProduct.assoc R X Y Z).toHopfAlgebraCatIso
+  leftUnitor X := (Bialgebra.TensorProduct.lid R X).toHopfAlgebraCatIso
+  rightUnitor X := (Bialgebra.TensorProduct.rid R X).toHopfAlgebraCatIso
 
 /-- The data needed to induce a `MonoidalCategory` structure via
 `HopfAlgebraCat.instMonoidalCategoryStruct` and the forgetful functor to bialgebras. -/
 noncomputable def MonoidalCategory.inducingFunctorData :
     Monoidal.InducingFunctorData (forget₂ (HopfAlgebraCat R) (BialgebraCat R)) where
-  μIso := fun X Y => Iso.refl _
-  whiskerLeft_eq := fun X Y Z f => by ext; rfl
-  whiskerRight_eq := fun X f => by ext; rfl
-  tensorHom_eq := fun f g => by ext; rfl
+  μIso _ _ := Iso.refl _
+  whiskerLeft_eq _ _ _ _ := by ext; rfl
+  whiskerRight_eq _ _ := by ext; rfl
+  tensorHom_eq _ _ := by ext; rfl
   εIso := Iso.refl _
-  associator_eq := fun X Y Z => BialgebraCat.Hom.ext _ _ <| BialgHom.coe_linearMap_injective <|
+  associator_eq _ _ _ := BialgebraCat.Hom.ext <| BialgHom.coe_linearMap_injective <|
     TensorProduct.ext <| TensorProduct.ext (by ext; rfl)
-  leftUnitor_eq := fun X => BialgebraCat.Hom.ext _ _ <| BialgHom.coe_linearMap_injective <|
+  leftUnitor_eq _ := BialgebraCat.Hom.ext <| BialgHom.coe_linearMap_injective <|
     TensorProduct.ext (by ext; rfl)
-  rightUnitor_eq := fun X => BialgebraCat.Hom.ext _ _ <| BialgHom.coe_linearMap_injective <|
+  rightUnitor_eq _ := BialgebraCat.Hom.ext <| BialgHom.coe_linearMap_injective <|
     TensorProduct.ext (by ext; rfl)
 
 noncomputable instance instMonoidalCategory : MonoidalCategory (HopfAlgebraCat R) :=
