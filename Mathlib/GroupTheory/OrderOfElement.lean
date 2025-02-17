5fd6ccede3ac1fc5e9b05ea345fdcf0515c7281e
Anne C.A. Baanen <vierkantor@vierkantor.com>
2025-02-17 15:28:23 +0100

    chore(Algebra/CharP): move modeq lemmas from Defs to Basic
    These lemmas are useful but they bring in a lot of dependencies such as GCDs and order structures, so I think we should rather have them in the `Basic` files to keep the definition minimal.

--- Mathlib/GroupTheory/OrderOfElement.lean
+++ Mathlib/GroupTheory/OrderOfElement.lean
@@ -5,11 +5,11 @@ Authors: Johannes Hölzl, Julian Kuelshammer
 -/
 import Mathlib.Algebra.CharP.Defs
 import Mathlib.Algebra.Group.Pointwise.Set.Finite
-import Mathlib.Algebra.Group.Semiconj.Basic
 import Mathlib.Algebra.Group.Subgroup.Finite
 import Mathlib.Algebra.Module.NatInt
 import Mathlib.Algebra.Order.Group.Action
 import Mathlib.Algebra.Order.Ring.Abs
+import Mathlib.Data.Int.ModEq
 import Mathlib.Dynamics.PeriodicPts.Lemmas
 import Mathlib.GroupTheory.Index
 import Mathlib.NumberTheory.Divisors
