b7c03e98594fd466df7104ccf36da243572dc081
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-02-17 12:54:51 +0100

    feat(CategoryTheory): subobjects in Grothendieck abelian categories

--- Mathlib.lean
+++ Mathlib.lean
@@ -1694,6 +1694,7 @@ import Mathlib.CategoryTheory.Abelian.GrothendieckAxioms.Indization
 import Mathlib.CategoryTheory.Abelian.GrothendieckAxioms.Sheaf
 import Mathlib.CategoryTheory.Abelian.GrothendieckAxioms.Types
 import Mathlib.CategoryTheory.Abelian.GrothendieckCategory.Basic
+import Mathlib.CategoryTheory.Abelian.GrothendieckCategory.Subobject
 import Mathlib.CategoryTheory.Abelian.Images
 import Mathlib.CategoryTheory.Abelian.Injective
 import Mathlib.CategoryTheory.Abelian.InjectiveResolution
