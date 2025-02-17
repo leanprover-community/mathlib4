0882ad57c0f666e2d76958f879b18960810efce2
Markus Himmel <markus@lean-fro.org>
2025-02-17 07:01:07 +0100

    Fix

--- Mathlib/Algebra/Category/Grp/ChosenFiniteProducts.lean
+++ Mathlib/Algebra/Category/Grp/ChosenFiniteProducts.lean
@@ -3,7 +3,6 @@ Copyright (c) 2025 Markus Himmel. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Markus Himmel
 -/
-import Mathlib.CategoryTheory.ChosenFiniteProducts
 import Mathlib.Algebra.Category.Grp.Biproducts
 import Mathlib.Algebra.Category.Grp.Zero
 import Mathlib.CategoryTheory.Monoidal.Types.Basic
