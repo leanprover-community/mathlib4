13affdb21a4238d2e5e19d618a95c231559285a0
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2025-02-18 02:45:56 +0000

    .

--- Mathlib/Tactic/Push.lean
+++ Mathlib/Tactic/Push.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2019 Jovan Gerbscheid. All rights reserved.
+Copyright (c) 2019 Patrick Massot. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jovan Gerbscheid, Patrick Massot, Simon Hudon, Alice Laroche, Frédéric Dupuis,
 Jireh Loreaux
