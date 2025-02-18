fd6aee2edd3cfff409c8140d93a1b544cea64f9c
Robin Carlier <57142648+robin-carlier@users.noreply.github.com>
2025-02-18 20:45:19 +0000

    feat(AlgebraicTopology/SimplexCategory/GeneratorsRelations/EpiMono): Morphism properties in `SimplexCategoryGenRel` (#21742)
    Introduce two predicates `P_σ` and `P_δ` on morphisms in `SimplexCategoryGenRel`:
     - `P_σ` is defined as the `multiplicativeClosure` of degeneracies
     - `P_δ` is defined as the `multiplicativeClosure` of faces
    
    A subsequent pull request will show that every morphism in `SimplexCategoryGenRel` factors as a `P_σ` followed by a `P_δ`.
    
    Part of a series of PR formalising that `SimplexCategoryGenRel` is equivalent to `SimplexCategory`.

--- Mathlib.lean
+++ Mathlib.lean
@@ -1142,6 +1142,7 @@ import Mathlib.AlgebraicTopology.RelativeCellComplex.Basic
 import Mathlib.AlgebraicTopology.SimplexCategory.Basic
 import Mathlib.AlgebraicTopology.SimplexCategory.Defs
 import Mathlib.AlgebraicTopology.SimplexCategory.GeneratorsRelations.Basic
+import Mathlib.AlgebraicTopology.SimplexCategory.GeneratorsRelations.EpiMono
 import Mathlib.AlgebraicTopology.SimplexCategory.MorphismProperty
 import Mathlib.AlgebraicTopology.SimplicialCategory.Basic
 import Mathlib.AlgebraicTopology.SimplicialCategory.SimplicialObject
