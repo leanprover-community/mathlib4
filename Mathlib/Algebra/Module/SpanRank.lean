c2ad97b82dfe11b268f1b8a36127d27e0984aeb8
xyzw12345 <wangjt2020@163.com>
2025-02-18 11:39:34 +0800

    Update SpanRank.lean

--- Mathlib/Algebra/Module/SpanRank.lean
+++ Mathlib/Algebra/Module/SpanRank.lean
@@ -30,7 +30,7 @@ The difference between these two definitions is only that when no finite generat
   family of cardinality equal to `spanFinrank`.
 
 ## Tags
-submodule, generating set, span rank
+submodule, generating subset, span rank
 -/
 
 variable {R M : Type*} [Semiring R] [AddCommMonoid M] [Module R M]
@@ -39,8 +39,7 @@ namespace Submodule
 
 /-- The minimum cardinality of a generating set of a submodule, possibly infinite, with type
   `ℕ∞`. If no finite generating set exists, the span rank is defined to be `⊤`. -/
-noncomputable def spanRank (p : Submodule R M) : ℕ∞ :=
-  ⨅ (s : Set M) (_ : span R s = p), s.encard
+noncomputable def spanRank (p : Submodule R M) : ℕ∞ := ⨅ (s : Set M) (_ : span R s = p), s.encard
 
 /-- The minimum cardinality of a generating set of a submodule as a natural number. If no finite
   generating set exists, the span rank is defined to be `0`. -/
@@ -70,135 +69,14 @@ lemma spanFinrank_eq_iInf (p : Submodule R M) :
 
 /-- A submodule's `spanRank` is not top if and only if it is finitely generated. -/
 @[simp]
-lemma spanRank_ne_top_iff_fg {p : Submodule R M} :
-    p.spanRank ≠ ⊤ ↔ p.FG := by
+lemma spanRank_ne_top_iff_fg {p : Submodule R M} : p.spanRank ≠ ⊤ ↔ p.FG := by
   simp [spanRank, Submodule.fg_def, and_comm]
 
 /-- A submodule is finitely generated if and only if its `spanRank` is equal to its `spanFinrank`.-/
-lemma fg_iff_spanRank_eq_spanFinrank {p : Submodule R M} :
-    p.spanRank = p.spanFinrank ↔ p.FG := by
+lemma fg_iff_spanRank_eq_spanFinrank {p : Submodule R M} : p.spanRank = p.spanFinrank ↔ p.FG := by
   rw [spanFinrank, ← spanRank_ne_top_iff_fg, ← ENat.coe_toNat_eq_self, eq_comm]
 
-/-- Constructs indexed generating elements whose cardinality equals `spanFinrank` for a finitely
-  generated submodule.-/
-theorem FG.exists_fun_spanFinrank_span_range_eq {p : Submodule R M} (h : p.FG) :
-    ∃ f : Fin p.spanFinrank → M, span R (Set.range f) = p := by
-  haveI : Nonempty { s // s.Finite ∧ span R s = p } := by
-    rcases h with ⟨s, hs⟩
-    exact ⟨s, ⟨Finset.finite_toSet s, hs⟩⟩
-  obtain ⟨⟨s, h₁, h₂⟩, h₃ : h₁.toFinset.card = _⟩ :
-    p.spanFinrank ∈ _ := by rw [spanFinrank_eq_iInf]; exact Nat.sInf_mem (Set.range_nonempty _)
-  rw [← h₃]
-  letI : Fintype (@Set.Elem M s) := h₁.fintype
-  have temp : h₁.toFinset.card = @Fintype.card (@Set.Elem M s)
-    (Fintype.ofFinite (@Set.Elem M s)) := by rw [Set.Finite.card_toFinset h₁]
-  let f' :=  Fintype.equivFinOfCardEq temp.symm
-  use Subtype.val ∘ f'.symm
-  rw [Set.range_comp, Set.range_eq_univ.mpr]
-  · simpa using h₂
-  · exact f'.symm.surjective
-
-/-- For a finitely generated submodule, its spanRank is less than or equal to n
-  if and only if there are elements indexed by (Fin n) that generates the submodule. -/
-lemma FG.spanRank_le_iff_exists_span_range_eq {p : Submodule R M} {n : ℕ} :
-    p.spanRank ≤ n ↔ ∃ f : Fin n → M, span R (Set.range f) = p := by
-  classical
-  constructor
-  · intro e
-    have h := spanRank_ne_top_iff_fg.mp (e.trans_lt (WithTop.coe_lt_top n)).ne
-    obtain ⟨f, hf⟩ := FG.exists_fun_spanFinrank_span_range_eq h
-    use fun i => if h : i.1 < p.spanFinrank then f (Fin.castLT i h) else 0
-    simp_rw [← hf]
-    apply le_antisymm
-    · rw [Submodule.span_le]
-      rintro _ ⟨x, rfl⟩
-      dsimp only
-      split_ifs
-      · apply Submodule.subset_span
-        exact Set.mem_range_self _
-      · exact (span R (Set.range f)).zero_mem
-    · rw [Submodule.span_le]
-      rintro _ ⟨x, rfl⟩
-      simp only [SetLike.mem_coe]
-      apply Submodule.subset_span
-      rw [← fg_iff_spanRank_eq_spanFinrank] at h
-      have he : (p.spanFinrank : WithTop Nat) ≤ n := by rwa [h] at e
-      use Fin.castLE (ENat.coe_le_coe.mp he) x
-      simp_all only [Fin.coe_castLE, Fin.is_lt, dite_true]
-      rfl
-  · rintro ⟨f, hf⟩
-    let s : { s : Set M // s.Finite ∧ span R s = p} := ⟨Set.range f, Set.finite_range f, hf⟩
-    calc
-      p.spanRank
-      ≤ s.2.1.toFinset.card := by
-        rw [spanRank_eq_iInf]
-        exact csInf_le (OrderBot.bddBelow _) (Set.mem_range_self _)
-      _ = (Finset.univ.image f).card := by
-        congr 2; ext
-        simp only [Set.toFinite_toFinset, Set.mem_toFinset, Finset.mem_image, Finset.mem_univ,
-          true_and]
-        exact Set.mem_range
-      _ ≤ n := by
-        simp only [Nat.cast_le]
-        convert Finset.card_image_le
-        rw [Finset.card_univ, Fintype.card_fin]
-
-/-- Generating elements for the submodule of minimum cardinality. -/
-noncomputable def FG.generators {p : Submodule R M} (h : p.FG) : Fin p.spanFinrank → M :=
-  Classical.choose (exists_fun_spanFinrank_span_range_eq h)
-
-/-- The span of the generators equals the submodule. -/
-lemma FG.span_range_generators {p : Submodule R M} (h : p.FG) :
-    span R (Set.range (generators h)) = p :=
-  Classical.choose_spec (exists_fun_spanFinrank_span_range_eq h)
-
-/-- The elements of the generators are in the submodule. -/
-lemma FG.generators_mem {p : Submodule R M} (h : p.FG) (i : Fin p.spanFinrank) :
-    generators h i ∈ p := by
-  simp_rw [← span_range_generators h]
-  exact subset_span (Set.mem_range_self i)
-
-@[simp]
-lemma spanRank_eq_zero_iff_eq_bot {I : Submodule R M} : I.spanRank = 0 ↔ I = ⊥ := by
-  constructor
-  · intro e
-    have H : Submodule.FG I := by
-      rw [← Submodule.spanRank_ne_top_iff_fg, e]
-      trivial
-    rw [fg_iff_spanRank_eq_spanFinrank.mpr H, ← WithTop.coe_zero, WithTop.coe_zero,
-        Nat.cast_eq_zero] at e
-    have := H.exists_fun_spanFinrank_span_range_eq
-    rw [e] at this
-    obtain ⟨f, hf⟩ := this
-    rwa [show Set.range f = ∅ by simp, Submodule.span_empty, eq_comm] at hf
-  · rintro rfl
-    rw [← bot_eq_zero, eq_bot_iff, bot_eq_zero, ← WithTop.coe_zero]
-    apply Submodule.FG.spanRank_le_iff_exists_span_range_eq.mpr
-    exact ⟨fun _ ↦ 0, by simp⟩
-
-lemma spanRank_sup_le_sum_spanRank {p q : Submodule R M} :
-    (p ⊔ q).spanRank ≤ p.spanRank + q.spanRank := by
-  by_cases hp : Submodule.FG p
-  swap
-  · rw [← Submodule.spanRank_ne_top_iff_fg, not_ne_iff] at hp
-    rw [hp, top_add]
-    exact le_top
-  by_cases hq : Submodule.FG q
-  swap
-  · rw [← Submodule.spanRank_ne_top_iff_fg, not_ne_iff] at hq
-    rw [hq, add_top]
-    exact le_top
-  obtain ⟨f, hf⟩ := hp.exists_fun_spanFinrank_span_range_eq
-  obtain ⟨g, hg⟩ := hq.exists_fun_spanFinrank_span_range_eq
-  rw [← Submodule.fg_iff_spanRank_eq_spanFinrank] at hp hq
-  rw [hp, hq, ← Nat.cast_add]
-  rw [FG.spanRank_le_iff_exists_span_range_eq]
-  refine ⟨(Sum.elim f g) ∘ finSumFinEquiv.symm, ?_⟩
-  rw [Set.range_comp, Set.range_eq_univ.mpr (Equiv.surjective _), Set.image_univ,
-    Set.Sum.elim_range, Submodule.span_union, hf, hg]
-
-lemma spanRank_span_le_encard (s : Set M) :
-    (Submodule.span R s).spanRank ≤ s.encard :=
+lemma spanRank_span_le_encard (s : Set M) : (Submodule.span R s).spanRank ≤ s.encard :=
   biInf_le _ (by constructor)
 
 lemma spanFinrank_span_le_ncard_of_finite {s : Set M} (hs : s.Finite) :
@@ -208,8 +86,78 @@ lemma spanFinrank_span_le_ncard_of_finite {s : Set M} (hs : s.Finite) :
   · simpa [spanFinrank] using (⟨hs.toFinset, by simp⟩ : (span R s).FG)
   · exact hs.cast_ncard_eq
 
+/-- Constructs a generating set with cardinality equal to the `spanFinrank` of the submodule when
+  the submodule is finitely generated. -/
+theorem FG.exists_span_set_encard_eq_spanFinrank {p : Submodule R M} (h : p.FG) :
+    ∃ s : Set M, s.encard = p.spanFinrank ∧ span R s = p := by
+  haveI : Nonempty { s // s.Finite ∧ span R s = p } := by
+    rcases h with ⟨s, hs⟩
+    exact ⟨s, ⟨Finset.finite_toSet s, hs⟩⟩
+  obtain ⟨⟨s, h₁, h₂⟩, h₃ : h₁.toFinset.card = _⟩ :
+    p.spanFinrank ∈ _ := by rw [spanFinrank_eq_iInf]; exact Nat.sInf_mem (Set.range_nonempty _)
+  exact ⟨s, ⟨h₃.symm ▸ (Set.Finite.encard_eq_coe_toFinset_card h₁), h₂⟩⟩
+
+theorem exists_span_set_encard_eq_spanRank (p : Submodule R M) :
+    ∃ s : Set M, s.encard = p.spanRank ∧ span R s = p := by
+  by_cases h : p.FG
+  · rw [fg_iff_spanRank_eq_spanFinrank.mpr h]; exact FG.exists_span_set_encard_eq_spanFinrank h
+  · have hp : span R p = p := by simp
+    refine ⟨p, ⟨?_, hp⟩⟩
+    rw [← spanRank_ne_top_iff_fg, ne_eq, Decidable.not_not] at h
+    nth_rw 1 1 1 1 [h, eq_top_iff, ← h, ← hp]
+    exact spanRank_span_le_encard (p : Set M)
+
+/-- For a finitely generated submodule, its spanRank is less than or equal to `n`
+  if and only if there is a generating subset with cardinality less than or equal to `n`. -/
+lemma FG.spanRank_le_enat_iff_exists_span_set_encard_le (p : Submodule R M) {n : ℕ∞} :
+    p.spanRank ≤ n ↔ ∃ s : Set M, s.encard ≤ n ∧ span R s = p := by
+  constructor
+  · intro h
+    obtain ⟨s, ⟨hs₁, hs₂⟩⟩ := exists_span_set_encard_eq_spanRank p
+    exact ⟨s, ⟨hs₁ ▸ h, hs₂⟩⟩
+  · exact (fun ⟨s, ⟨hs₁, hs₂⟩⟩ ↦ hs₂.symm ▸ (le_trans (spanRank_span_le_encard s) hs₁))
+
 @[simp]
-lemma spanRank_bot : (⊥ : Ideal R).spanRank = 0 :=
-  Submodule.spanRank_eq_zero_iff_eq_bot.mpr rfl
+lemma spanRank_eq_zero_iff_eq_bot {I : Submodule R M} : I.spanRank = 0 ↔ I = ⊥ := by
+  constructor
+  · intro h
+    obtain ⟨s, ⟨hs₁, hs₂⟩⟩ :=
+      (FG.spanRank_le_enat_iff_exists_span_set_encard_le I (n := 0)).mp (by rw [h])
+    simp_all
+  · rintro rfl
+    rw [← nonpos_iff_eq_zero, ← ENat.coe_zero, FG.spanRank_le_enat_iff_exists_span_set_encard_le]
+    refine ⟨∅, by simp⟩
+
+@[simp]
+lemma spanRank_bot : (⊥ : Ideal R).spanRank = 0 := Submodule.spanRank_eq_zero_iff_eq_bot.mpr rfl
+
+/-- Generating elements for the submodule of minimum cardinality. -/
+noncomputable def generators (p : Submodule R M) : Set M :=
+  Classical.choose (exists_span_set_encard_eq_spanRank p)
+
+lemma generators_encard (p : Submodule R M) : (generators p).encard = spanRank p :=
+  (Classical.choose_spec (exists_span_set_encard_eq_spanRank p)).1
+
+lemma FG.generators_ncard {p : Submodule R M} (h : p.FG) :
+    (generators p).ncard = spanFinrank p := by
+  rw [← ENat.coe_inj, ← fg_iff_spanRank_eq_spanFinrank.mpr h, Set.ncard, generators_encard,
+      ← spanFinrank]
+  exact (fg_iff_spanRank_eq_spanFinrank.mpr h).symm
+
+/-- The span of the generators equals the submodule. -/
+lemma span_generators (p : Submodule R M) : span R (generators p) = p :=
+  (Classical.choose_spec (exists_span_set_encard_eq_spanRank p)).2
+
+/-- The elements of the generators are in the submodule. -/
+lemma FG.generators_mem (p : Submodule R M) : generators p ⊆ p := by
+  nth_rw 2 [← span_generators p]
+  exact subset_span (s := generators p)
+
+lemma spanRank_sup_le_sum_spanRank {p q : Submodule R M} :
+    (p ⊔ q).spanRank ≤ p.spanRank + q.spanRank := by
+  apply (FG.spanRank_le_enat_iff_exists_span_set_encard_le (p ⊔ q)).mpr
+  obtain ⟨sp, ⟨hp₁, rfl⟩⟩ := exists_span_set_encard_eq_spanRank p
+  obtain ⟨sq, ⟨hq₁, rfl⟩⟩ := exists_span_set_encard_eq_spanRank q
+  exact ⟨sp ∪ sq, ⟨hp₁ ▸ hq₁ ▸ (Set.encard_union_le sp sq), span_union sp sq⟩⟩
 
 end Submodule
