831a962532df8f0c13a2e9e12a1db8c48e3a5ea3
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-02-18 18:52:42 +0100

    SingularNManifold.map compiles

--- Mathlib/Geometry/Manifold/Bordisms.lean
+++ Mathlib/Geometry/Manifold/Bordisms.lean
@@ -61,95 +61,67 @@ noncomputable section
 
 /-- A **singular `n`-manifold** on a topological space `X`, for `n ∈ ℕ`, is a pair `(M, f)`
 of a closed `n`-dimensional `C^k` manifold `M` together with a continuous map `M → X`.
+We assume that `M` is a manifold over the pair `(E, H)` with model `I`.
 
 In practice, one commonly wants to take `k=∞` (as then e.g. the intersection form is a powerful tool
 to compute bordism groups; for the definition, this makes no difference.) -/
-structure SingularNManifold.{u, v, w} (X : Type w) [TopologicalSpace X] (n : ℕ) (k : ℕ∞)
-  {E H : Type v} [NormedAddCommGroup E] [NormedSpace ℝ E] [findim: FiniteDimensional ℝ E]
+structure SingularNManifold (X : Type*) [TopologicalSpace X] (n : ℕ) (k : ℕ∞)
+  {E H : Type*} [NormedAddCommGroup E] [NormedSpace ℝ E] [FiniteDimensional ℝ E]
   [TopologicalSpace H] (I : ModelWithCorners ℝ E H)
-  (M : Type u) [TopologicalSpace M] [ChartedSpace H M] [IsManifold I k M]
-  [CompactSpace M] [BoundarylessManifold I M] where
-  -- /-- The normed space on which the manifold `M` is modeled. -/
-  -- E : Type v
-  -- /-- `E` is normed (additive) abelian group -/
-  -- [normedAddCommGroup : NormedAddCommGroup E]
-  -- /-- `E` is a real normed space -/
-  -- [normedSpace: NormedSpace ℝ E]
-  /-- The smooth manifold `M` of a singular `n`-manifold `(M, f)` -/
-  -- M : Type u
-  -- /-- The smooth manifold `M` is a topological space -/
-  -- [topSpaceM : TopologicalSpace M]
-  -- /-- The topological space on which the manifold `M` is modeled -/
-  -- H : Type v
-  -- /-- `H` is a topological space -/
-  -- [topSpaceH : TopologicalSpace H]
-  -- /-- The smooth manifold `M` is a charted space over `H` -/
-  -- [chartedSpace : ChartedSpace H M]
-  -- /-- A homeomorphism `H ≃ₜ ℝ^n`: this is used to define disjoint unions of singular n-manifolds -/
-  -- modelSpace_homeo_euclideanSpace : H ≃ₜ EuclideanSpace ℝ (Fin n)
-  -- /-- The model with corners for the manifold `M` -/
-  -- I : ModelWithCorners ℝ E H
-  -- /-- `M` is a smooth manifold with corners -/
-  -- [smoothMfd : IsManifold I k M]
-  -- /-- `M` is compact -/
-  -- [compactSpace : CompactSpace M]
-  -- /-- `M` is boundaryless -/
-  -- [boundaryless: BoundarylessManifold I M]
-  -- /-- `M` is finite-dimensional, as its model space `E` is -/
-  -- [findim: FiniteDimensional ℝ E]
+  where
+  M : Type*
+  [topSpaceM : TopologicalSpace M]
+  [chartedSpace: ChartedSpace H M]
+  [isManifold: IsManifold I k M]
+  [compactSpace: CompactSpace M]
+  [boundaryless: BoundarylessManifold I M]
   /-- `M` is `n`-dimensional, as its model space `E` is -/
   dimension : finrank ℝ E = n
   /-- The underlying map `M → X` of a singular `n`-manifold `(M, f)` on `X` -/
   f : M → X
   hf : Continuous f
 
-variable {X Y Z : Type*} [TopologicalSpace X] [TopologicalSpace Y] [TopologicalSpace Z]
-
-instance {n : ℕ} {k : ℕ∞} {s : SingularNManifold X n k} : NormedAddCommGroup s.E :=
-  s.normedAddCommGroup
-
-instance {n : ℕ} {k : ℕ∞} {s : SingularNManifold X n k} : NormedSpace ℝ s.E := s.normedSpace
-
-instance {n : ℕ} {k : ℕ∞} {s : SingularNManifold X n k} : TopologicalSpace s.M := s.topSpaceM
-
-instance {n : ℕ} {k : ℕ∞} {s : SingularNManifold X n k} : TopologicalSpace s.H := s.topSpaceH
-
-instance {n : ℕ} {k : ℕ∞} {s : SingularNManifold X n k} : ChartedSpace s.H s.M := s.chartedSpace
-
-instance {n : ℕ} {k : ℕ∞} {s : SingularNManifold X n k} : IsManifold s.I k s.M := s.smoothMfd
-
-instance {n : ℕ} {k : ℕ∞} {s : SingularNManifold X n k} : CompactSpace s.M := s.compactSpace
-
-instance {n : ℕ} {k : ℕ∞} {s : SingularNManifold X n k} : BoundarylessManifold s.I s.M :=
-  s.boundaryless
-
-instance {n : ℕ} {k : ℕ∞} {s : SingularNManifold X n k} : FiniteDimensional ℝ s.E := s.findim
+-- XXX: can I use Type* above? try when the file compiles!
 
 namespace SingularNManifold
 
-variable {n : ℕ} {k : ℕ∞}
+variable {X Y Z : Type*} [TopologicalSpace X] [TopologicalSpace Y] [TopologicalSpace Z]
+  {n : ℕ} {k : ℕ∞}
+  {E H M : Type*} [NormedAddCommGroup E] [NormedSpace ℝ E] [FiniteDimensional ℝ E]
+  [TopologicalSpace H] {I : ModelWithCorners ℝ E H} [TopologicalSpace M] [ChartedSpace H M]
+  [IsManifold I k M] [CompactSpace M] [BoundarylessManifold I M]
+
+instance {n : ℕ} {k : ℕ∞} {s : SingularNManifold X n k I} : TopologicalSpace s.M := s.topSpaceM
+
+instance {n : ℕ} {k : ℕ∞} {s : SingularNManifold X n k I} : ChartedSpace H s.M := s.chartedSpace
+
+instance {n : ℕ} {k : ℕ∞} {s : SingularNManifold X n k I} : IsManifold I k s.M := s.isManifold
+
+instance {n : ℕ} {k : ℕ∞} {s : SingularNManifold X n k I} : CompactSpace s.M := s.compactSpace
+
+instance {n : ℕ} {k : ℕ∞} {s : SingularNManifold X n k I} : BoundarylessManifold I s.M :=
+  s.boundaryless
 
 /-- A map of topological spaces induces a corresponding map of singular n-manifolds. -/
 -- This is part of proving functoriality of the bordism groups.
-noncomputable def map (s : SingularNManifold X n k)
-    {φ : X → Y} (hφ : Continuous φ) : SingularNManifold Y n k where
-  modelSpace_homeo_euclideanSpace := s.modelSpace_homeo_euclideanSpace
-  I := s.I
+noncomputable def map (s : SingularNManifold X n k I)
+    {φ : X → Y} (hφ : Continuous φ) : SingularNManifold Y n k I where
   f := φ ∘ s.f
   hf := hφ.comp s.hf
   dimension := s.dimension
 
 @[simp]
-lemma map_f (s : SingularNManifold X n k) {φ : X → Y} (hφ : Continuous φ) :
+lemma map_f (s : SingularNManifold X n k I) {φ : X → Y} (hφ : Continuous φ) :
     (s.map hφ).f = φ ∘ s.f :=
   rfl
 
-lemma map_comp (s : SingularNManifold X n k)
+lemma map_comp (s : SingularNManifold X n k I)
     {φ : X → Y} {ψ : Y → Z} (hφ : Continuous φ) (hψ : Continuous ψ) :
     ((s.map hφ).map hψ).f = (ψ ∘ φ) ∘ s.f := by
   simp [Function.comp_def]
   rfl
 
+#exit
 -- Let M, M' and W be smooth manifolds.
 universe u
 variable {E E' E'' E''' H H' H'' H''' : Type u} [NormedAddCommGroup E] [NormedSpace ℝ E]
