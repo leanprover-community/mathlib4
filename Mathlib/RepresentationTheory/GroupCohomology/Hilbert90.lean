f88c7691523460ce2443fa72fd60f5fd1bd9fe5f
Amelia Livingston <al3717@ic.ac.uk>
2025-02-18 17:20:31 +0000

    refactor(RepresentationTheory/GroupCohomology/LowDegree): define `Hn` using `ShortComplex.moduleCatHomology` for `n = 0, 1, 2` (#22046)

--- Mathlib/RepresentationTheory/GroupCohomology/Hilbert90.lean
+++ Mathlib/RepresentationTheory/GroupCohomology/Hilbert90.lean
@@ -102,7 +102,7 @@ variable (K L : Type) [Field K] [Field L] [Algebra K L] [FiniteDimensional K L]
 first group cohomology `H¹(Aut_K(L), Lˣ)` is trivial. -/
 noncomputable instance H1ofAutOnUnitsUnique : Unique (H1 (Rep.ofAlgebraAutOnUnits K L)) where
   default := 0
-  uniq := fun a => Quotient.inductionOn' a fun x => (Submodule.Quotient.mk_eq_zero _).2 <| by
+  uniq := fun a => Quotient.inductionOn' a fun x => (H1π_eq_zero_iff _).2 <| by
     refine (oneCoboundariesOfIsMulOneCoboundary ?_).2
     rcases isMulOneCoboundary_of_isMulOneCocycle_of_aut_to_units x.1
       (isMulOneCocycle_of_oneCocycles x) with ⟨β, hβ⟩
