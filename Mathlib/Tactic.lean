adececb431da09521a15841ca42e4f75160f7dfa
Yaël Dillies <yael.dillies@gmail.com>
2025-02-18 12:34:20 +0000

    mk all

--- Mathlib/Tactic.lean
+++ Mathlib/Tactic.lean
@@ -232,6 +232,7 @@ import Mathlib.Tactic.Says
 import Mathlib.Tactic.ScopedNS
 import Mathlib.Tactic.Set
 import Mathlib.Tactic.SetLike
+import Mathlib.Tactic.Simp.FinsetNat
 import Mathlib.Tactic.SimpIntro
 import Mathlib.Tactic.SimpRw
 import Mathlib.Tactic.Simps.Basic
