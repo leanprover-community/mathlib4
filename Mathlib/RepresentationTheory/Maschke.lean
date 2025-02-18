2c76b4b59657e6e112c50af743604429a8228338
Anne C.A. Baanen <vierkantor@vierkantor.com>
2025-02-18 16:34:18 +0100

    Fix imports

--- Mathlib/RepresentationTheory/Maschke.lean
+++ Mathlib/RepresentationTheory/Maschke.lean
@@ -3,8 +3,8 @@ Copyright (c) 2020 Kim Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kim Morrison
 -/
+import Mathlib.Algebra.Group.TypeTags.Fintype
 import Mathlib.Algebra.MonoidAlgebra.Basic
-import Mathlib.Algebra.Group.TypeTags.Finite
 import Mathlib.LinearAlgebra.Basis.VectorSpace
 import Mathlib.RingTheory.SimpleModule.Basic
 
