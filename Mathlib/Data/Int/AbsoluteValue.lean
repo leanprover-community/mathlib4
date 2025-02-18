281c5eb1571fb23be39b199491f703684135c454
Yaël Dillies <yael.dillies@gmail.com>
2025-02-18 13:36:08 +0000

    chore: shortcut instance for `Neg ℤˣ` This lets us avoid importing `Ring` in downstream files (most of the effect is to come).

--- Mathlib/Data/Int/AbsoluteValue.lean
+++ Mathlib/Data/Int/AbsoluteValue.lean
@@ -3,11 +3,8 @@ Copyright (c) 2021 Anne Baanen. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Anne Baanen
 -/
-import Mathlib.Algebra.GroupWithZero.Action.Units
 import Mathlib.Algebra.Module.Basic
 import Mathlib.Algebra.Order.AbsoluteValue.Basic
-import Mathlib.Algebra.Ring.Int.Units
-import Mathlib.Data.Int.Cast.Lemmas
 
 /-!
 # Absolute values and the integers
