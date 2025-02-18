191be4774a67317f70320d662cc51dcef3878a4a
su00000 <ssh@stu.pku.edu.cn>
2025-02-18 14:59:10 +0000

    feat(RingTheory/NoetherNormalization): Add Noether normalization lemma (#18247)
    This PR contains a proof of Noether normalization lemma:
    Let `A` be a finitely generated algebra over a field `k`. Then there exists natural number $r$ and an injective homomorphism from `k[X_1, X_2, ..., X_r]` to `A` such that `A` is integral over `k[X_1, X_2, ..., X_r]`.
    
    Co-authored-by: Riccardo Brasca <riccardo.brasca@gmail.com>
    Co-authored-by: Wan Lin <3438488340@qq.com>
    Co-authored-by: Xiaoyang Su <12210823@mail.sustech.edu.cn>
    
    
    
    Co-authored-by: Riccardo Brasca <riccardo.brasca@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -4824,6 +4824,7 @@ import Mathlib.RingTheory.Nakayama
 import Mathlib.RingTheory.Nilpotent.Basic
 import Mathlib.RingTheory.Nilpotent.Defs
 import Mathlib.RingTheory.Nilpotent.Lemmas
+import Mathlib.RingTheory.NoetherNormalization
 import Mathlib.RingTheory.Noetherian.Basic
 import Mathlib.RingTheory.Noetherian.Defs
 import Mathlib.RingTheory.Noetherian.Filter
