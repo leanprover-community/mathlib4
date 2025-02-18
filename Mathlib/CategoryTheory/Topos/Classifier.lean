bcd5e3f2ce18271cc893e6a6897e3f86608b65d8
CharredLee <gcc2hq@virginia.edu>
2025-02-18 13:21:33 -0500

    various minor renames and doc update

--- Mathlib/CategoryTheory/Topos/Classifier.lean
+++ Mathlib/CategoryTheory/Topos/Classifier.lean
@@ -28,21 +28,17 @@ Let `C` refer to a category with a terminal object.
   in `C`.
 
 * `CategoryTheory.HasClassifier C` says that there is at least one
-  subobject classifier.
+  subobject classifier. `Ω C` denotes a choice of subobject classifier.
 
 ## Main results
 
 * It is a theorem that the truth morphism `⊤_ C ⟶ Ω C` is a (split, and
   therefore regular) monomorphism.
 
-* `MonoClassifier.balanced` shows that any category with a subobject classifier
+* `HasClassifier.balanced` shows that any category with a subobject classifier
   is balanced. This follows from the fact that every monomorphism is the
   pullback of a regular monomorphism (the truth morphism).
 
-## Notation
-
-* if `m` is a monomorphism, `χ_ m` denotes characteristic map of `m`,
-  which is the corresponding map to the subobject classifier.
 
 ## References
 
@@ -58,7 +54,7 @@ variable (C : Type u) [Category.{v} C] [HasTerminal C]
 
 namespace CategoryTheory
 
-/-- A morphism `t : ⊤_ C ⟶ Ω` from the terminal object of a category `C`
+/-- A morphism `truth : ⊤_ C ⟶ Ω` from the terminal object of a category `C`
 is a subobject classifier if, for every monomorphism `m : U ⟶ X` in `C`,
 there is a unique map `χ : X ⟶ Ω` such that the following square is a pullback square:
 ```
@@ -67,21 +63,22 @@ there is a unique map `χ : X ⟶ Ω` such that the following square is a pullba
 terminal.from U               χ
       |                       |
       v                       v
-    ⊤_ C --------t----------> Ω
+    ⊤_ C ------truth--------> Ω
 ```
 -/
 structure Classifier where
   /-- The target of the truth morphism -/
-  {Ω : C}
+  Ω : C
   /-- the truth morphism for a subobject classifier -/
-  t : ⊤_ C ⟶ Ω
+  truth : ⊤_ C ⟶ Ω
   /-- For any monomorphism `U ⟶ X`, there is an associated characteristic map `X ⟶ Ω`. -/
-  char {U X : C} (m : U ⟶ X) [Mono m] : X ⟶ Ω
-  /-- `char m` forms the appropriate pullback square. -/
-  isPullback {U X : C} (m : U ⟶ X) [Mono m] : IsPullback m (terminal.from U) (char m) t
-  /-- `char m` is the only map `X ⟶ Ω` which forms the appropriate pullback square. -/
-  uniq {U X : C} (m : U ⟶ X) [Mono m] (χ : X ⟶ Ω) (hχ : IsPullback m (terminal.from U) χ t) :
-    χ = char m
+  χ {U X : C} (m : U ⟶ X) [Mono m] : X ⟶ Ω
+  /-- `χ m` forms the appropriate pullback square. -/
+  isPullback {U X : C} (m : U ⟶ X) [Mono m] : IsPullback m (terminal.from U) (χ m) truth
+  /-- `χ m` is the only map `X ⟶ Ω` which forms the appropriate pullback square. -/
+  uniq {U X : C} (m : U ⟶ X) [Mono m] (χ' : X ⟶ Ω)
+      (hχ' : IsPullback m (terminal.from U) χ' truth) :
+    χ' = χ m
 
 
 /-- A category `C` has a subobject classifier if there is at least one subobject classifier. -/
@@ -99,17 +96,14 @@ noncomputable section
 abbrev Ω : C := HasClassifier.exists_classifier.some.Ω
 
 /-- Notation for the "truth arrow" in an arbitrary choice of a subobject classifier -/
-abbrev t : ⊤_ C ⟶ Ω C := HasClassifier.exists_classifier.some.t
+abbrev truth : ⊤_ C ⟶ Ω C := HasClassifier.exists_classifier.some.truth
 
 variable {C}
 variable {U X : C} (m : U ⟶ X) [Mono m]
 
 /-- returns the characteristic morphism of the subobject `(m : U ⟶ X) [Mono m]` -/
-def characteristicMap : X ⟶ Ω C :=
-  HasClassifier.exists_classifier.some.char m
-
-/-- shorthand for the characteristic morphism, `ClassifierOf m` -/
-abbrev χ_ := characteristicMap m
+def χ : X ⟶ Ω C :=
+  HasClassifier.exists_classifier.some.χ m
 
 /-- The diagram
 ```
@@ -118,11 +112,11 @@ abbrev χ_ := characteristicMap m
 terminal.from U              χ_ m
       |                       |
       v                       v
-    ⊤_ C -------t C---------> Ω
+    ⊤_ C -----truth C-------> Ω
 ```
 is a pullback square.
 -/
-lemma isPullback : IsPullback m (terminal.from U) (χ_ m) (t C) :=
+lemma isPullback_χ : IsPullback m (terminal.from U) (χ m) (truth C) :=
   HasClassifier.exists_classifier.some.isPullback m
 
 /-- The diagram
@@ -132,22 +126,22 @@ lemma isPullback : IsPullback m (terminal.from U) (χ_ m) (t C) :=
 terminal.from U              χ_ m
       |                       |
       v                       v
-    ⊤_ C -------t C---------> Ω
+    ⊤_ C -----truth C-------> Ω
 ```
 commutes.
 -/
 @[reassoc]
-lemma comm : m ≫ (χ_ m) = terminal.from _ ≫ t C := (isPullback m).w
+lemma comm : m ≫ χ m = terminal.from _ ≫ truth C := (isPullback_χ m).w
 
-/-- `characteristicMap m` is the only map for which the associated square
+/-- `χ m` is the only map for which the associated square
 is a pullback square.
 -/
-lemma unique (χ : X ⟶ Ω C) (hχ : IsPullback m (terminal.from _) χ (t C)) : χ = χ_ m :=
-  HasClassifier.exists_classifier.some.uniq m χ hχ
+lemma unique (χ' : X ⟶ Ω C) (hχ' : IsPullback m (terminal.from _) χ' (truth C)) : χ' = χ m :=
+  HasClassifier.exists_classifier.some.uniq m χ' hχ'
 
 /-- `t C` is a regular monomorphism (because it is split). -/
-noncomputable instance truthIsRegularMono : RegularMono (t C) :=
-  RegularMono.ofIsSplitMono (t C)
+noncomputable instance truthIsRegularMono : RegularMono (truth C) :=
+  RegularMono.ofIsSplitMono (truth C)
 
 /-- The following diagram
 ```
@@ -156,18 +150,17 @@ noncomputable instance truthIsRegularMono : RegularMono (t C) :=
 terminal.from U              χ_ m
       |                       |
       v                       v
-    ⊤_ C -------t C---------> Ω
+    ⊤_ C -----truth C-------> Ω
 ```
 being a pullback for any monic `m` means that every monomorphism
 in `C` is the pullback of a regular monomorphism; since regularity
 is stable under base change, every monomorphism is regular.
+Hence, `C` is a regular mono category.
+It also follows that `C` is a balanced category.
 -/
-noncomputable instance monoIsRegularMono {A B : C} (m : A ⟶ B) [Mono m] : RegularMono m :=
-  regularOfIsPullbackFstOfRegular (isPullback m).w (isPullback m).isLimit
-
-/-- `C` is a balanced category.  -/
-instance balanced : Balanced C where
-  isIso_of_mono_of_epi := fun f => isIso_of_epi_of_strongMono f
+instance isRegularMonoCategory : IsRegularMonoCategory C where
+  regularMonoOfMono :=
+    fun m => ⟨regularOfIsPullbackFstOfRegular (isPullback_χ m).w (isPullback_χ m).isLimit⟩
 
 /-- If the source of a faithful functor has a subobject classifier, the functor reflects
   isomorphisms. This holds for any balanced category.
@@ -180,7 +173,7 @@ instance reflectsIsomorphisms (D : Type u₀) [Category.{v₀} D] (F : C ⥤ D) 
   the same holds -- the functor reflects isomorphisms.
 -/
 instance reflectsIsomorphismsOp (D : Type u₀) [Category.{v₀} D]
-(F : Cᵒᵖ ⥤ D) [Functor.Faithful F] :
+    (F : Cᵒᵖ ⥤ D) [Functor.Faithful F] :
     Functor.ReflectsIsomorphisms F :=
   reflectsIsomorphisms_of_reflectsMonomorphisms_of_reflectsEpimorphisms F
 
