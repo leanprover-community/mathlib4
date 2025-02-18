fd08ef88caffcd3eb969940da6ae60c92c143203
Anne C.A. Baanen <vierkantor@vierkantor.com>
2025-02-18 23:31:18 +0100

    mk_all!

--- Mathlib.lean
+++ Mathlib.lean
@@ -3670,7 +3670,6 @@ import Mathlib.LinearAlgebra.Isomorphisms
 import Mathlib.LinearAlgebra.JordanChevalley
 import Mathlib.LinearAlgebra.Lagrange
 import Mathlib.LinearAlgebra.LinearDisjoint
-import Mathlib.LinearAlgebra.LinearIndependent
 import Mathlib.LinearAlgebra.LinearIndependent.Basic
 import Mathlib.LinearAlgebra.LinearIndependent.Defs
 import Mathlib.LinearAlgebra.LinearIndependent.Lemmas
