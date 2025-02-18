b342e647655dbf8bfcafc4dfeec2a774e81940a0
Pim Otte <p.j.otte@uu.nl>
2025-02-18 10:53:57 +0100

    Undo adding line

--- Mathlib/Data/Set/Card.lean
+++ Mathlib/Data/Set/Card.lean
@@ -3,7 +3,6 @@ Copyright (c) 2023 Peter Nelson. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Peter Nelson
 -/
-
 import Mathlib.SetTheory.Cardinal.Finite
 
 /-!
