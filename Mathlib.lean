f8f1ef3634dc0c87655924f2913d611a202720e9
Sebastien Gouezel <sebastien.gouezel@univ-rennes1.fr>
2025-02-19 21:13:26 +0100

    split file

--- Mathlib.lean
+++ Mathlib.lean
@@ -1167,6 +1167,7 @@ import Mathlib.AlgebraicTopology.TopologicalSimplex
 import Mathlib.Analysis.AbsoluteValue.Equivalence
 import Mathlib.Analysis.Analytic.Basic
 import Mathlib.Analysis.Analytic.CPolynomial
+import Mathlib.Analysis.Analytic.CPolynomialDef
 import Mathlib.Analysis.Analytic.ChangeOrigin
 import Mathlib.Analysis.Analytic.Composition
 import Mathlib.Analysis.Analytic.Constructions
