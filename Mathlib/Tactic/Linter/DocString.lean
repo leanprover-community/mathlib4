3b1fdf39b3c800e9439d6d07a9089fa60b0af383
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-02-19 19:20:27 +0100

    Add rough functionality

--- Mathlib/Tactic/Linter/DocString.lean
+++ Mathlib/Tactic/Linter/DocString.lean
@@ -25,6 +25,41 @@ register_option linter.style.docString : Bool := {
 
 namespace Style
 
+/-- Checks whether a declaration docstring `input` conforms to mathlib's style guidelines
+(or, at least the syntactically checkable parts). -/
+def isGoodDocstring (initialWhitespace input : String) : IO Bool := do
+  match initialWhitespace with
+  | "\n" | " " => {let n := 42}
+  | "" =>
+    IO.eprintln s!"error: doc-string \"{input}\" should start with a space or newline"
+    return false
+  | _ =>
+    -- In any other cases, we have extraneous whitespace.
+    IO.eprintln s!"error: doc-string \"{input}\" should start with a single space"
+    return false
+
+  -- Check the ending of the doc-string: a new line or exactly one space.
+  if !(input.endsWith "\n" || input.endsWith " ") then
+    IO.eprintln s!"error: doc-string \"{input}\" end start with a space or newline"
+    return false
+  else if (input.endsWith "  ") then
+    IO.eprintln s!"error: doc-string \"{input}\" should start resp. end with at most a single space"
+    return false
+  -- Catch misleading indentation.
+  let lines := (input.split (· == '\n')).drop 0
+  if lines.any (fun l ↦ l.startsWith " ") then
+    IO.eprintln s!"error: subsequent lines in the doc-string \"{input}\" should not be indented"
+    return false
+  if input.endsWith "\"" then
+    IO.eprintln s!"error: docstring \"{input}\" ends with a single quote"
+    return false
+  else if input.trimRight.endsWith "," then
+    IO.eprintln s!"error: docstring \"{input}\" ends with a comma"
+    return false
+  -- This list of checks is not exhaustive, but a good start.
+  return true
+
+
 @[inherit_doc Mathlib.Linter.linter.style.docString]
 def docStringLinter : Linter where run := withSetOptionIn fun stx ↦ do
   unless Linter.getLinterValue linter.style.docString (← getOptions) do
@@ -32,6 +67,8 @@ def docStringLinter : Linter where run := withSetOptionIn fun stx ↦ do
   if (← get).messages.hasErrors then
     return
   let some docStx := stx.find? (·.isOfKind `Lean.Parser.Command.docComment) | return
+  -- `docString` contains e.g. trailing spaces before the '-/', but does not contain
+  -- any leading whitespace before the actual string starts.
   let docString ← getDocStringText ⟨docStx⟩
   -- `startSubstring` is only needed if you want to keep track of the whitespace between
   -- `/--` and the doc-string
@@ -40,7 +77,9 @@ def docStringLinter : Linter where run := withSetOptionIn fun stx ↦ do
     | _ => default
   let start := startSubstring.toString
   dbg_trace "'{start}{docString}'"
-  --Linter.logLint linter.style.docString docStx m!"Something is unformatted here?"
+  dbg_trace "just the string is '{docString}'"
+  if !(← isGoodDocstring start docString) then
+    Linter.logLint linter.style.docString docStx m!"Something is unformatted here?"
 
 initialize addLinter docStringLinter
 
