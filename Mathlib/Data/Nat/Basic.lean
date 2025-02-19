c7b0f34d7968938b2da3ca6ca24f4e93e8927c24
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2025-02-19 23:07:23 +0000

    .

--- Mathlib/Data/Nat/Basic.lean
+++ Mathlib/Data/Nat/Basic.lean
@@ -7,8 +7,8 @@ import Mathlib.Data.Nat.Init
 import Mathlib.Logic.Function.Basic
 import Mathlib.Logic.Nontrivial.Defs
 import Mathlib.Order.Defs.LinearOrder
+import Mathlib.Tactic.Contrapose
 import Mathlib.Tactic.GCongr.CoreAttrs
-import Mathlib.Tactic.Push
 import Mathlib.Util.AssertExists
 
 /-!
