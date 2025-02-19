d4469ef9fb294dc83142930763ccbfa9b59edba9
maddycrim <maddycrim84@gmail.com>
2025-02-19 09:46:12 -0500

    get rid of instance

--- Mathlib/RingTheory/Localization/Pi.lean
+++ Mathlib/RingTheory/Localization/Pi.lean
@@ -97,7 +97,7 @@ If each `R i` has maximal nilradical then the direct product `∏ R i` surjects 
 localization of `∏ R i` at `M`.
 -/
 lemma algebraMap_pi_surjective_of_nilradical_isMaximal (h : ∀ i, (nilradical (R i)).IsMaximal)
-  [IsLocalization M S'] [Fintype ι] [Nonempty ι] : Surjective (algebraMap (Π i, R i) (S')) := by
+  [IsLocalization M S'] [Fintype ι] : Surjective (algebraMap (Π i, R i) (S')) := by
   intro s
   set S := fun (i : ι) => Localization (M.map (Pi.evalRingHom R i))
   obtain ⟨r, hr⟩ := surjective_piRingHom_algebraMap_comp_piEvalRingHom_of_nilradical_isMaximal
