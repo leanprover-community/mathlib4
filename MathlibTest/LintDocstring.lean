314ca76abfbcc4b6f3a6e231e5992bb914afb479
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-02-19 19:30:40 +0100

    Add basic tests

---
+++ MathlibTest/LintDocstring.lean
@@ -0,0 +1,75 @@
+import Mathlib.Tactic.Linter.DocString
+
+/-! Tests for the `docstring` linter -/
+
+/--
+warning: error: doc-string "Missing space " should start with a space or newline
+note: this linter can be disabled with `set_option linter.style.docString false`
+-/
+#guard_msgs in
+/--Missing space -/
+example : Nat := 1
+
+/--
+warning: error: doc-string "Missing ending space" end start with a space or newline
+note: this linter can be disabled with `set_option linter.style.docString false`
+-/
+#guard_msgs in
+/-- Missing ending space-/
+example : Nat := 1
+
+/--
+warning: error: doc-string "Trailing newline " should start with a single space
+note: this linter can be disabled with `set_option linter.style.docString false`
+-/
+#guard_msgs in
+/--  Trailing newline -/
+example : Nat := 1
+
+/--
+warning: error: doc-string "Trailing newline  " should start resp. end with at most a single space
+note: this linter can be disabled with `set_option linter.style.docString false`
+-/
+#guard_msgs in
+/--
+Trailing newline  -/
+example : Nat := 1
+
+/--
+warning: error: subsequent lines in the doc-string "Bad indentation
+  on the second line " should not be indented
+note: this linter can be disabled with `set_option linter.style.docString false`
+-/
+#guard_msgs in
+/-- Bad indentation
+  on the second line -/
+example : Nat := 1
+
+/--
+warning: error: subsequent lines in the doc-string "Bad indentation
+not on the second, but
+   the second line " should not be indented
+note: this linter can be disabled with `set_option linter.style.docString false`
+-/
+#guard_msgs in
+/-- Bad indentation
+not on the second, but
+   the second line -/
+example : Nat := 1
+
+/--
+warning: error: docstring "ends with a comma, " ends with a comma
+note: this linter can be disabled with `set_option linter.style.docString false`
+-/
+#guard_msgs in
+/-- ends with a comma, -/
+example : Nat := 1
+
+/--
+warning: error: subsequent lines in the doc-string "With a trailing
+  quote" " should not be indented note: this linter can be disabled with `set_option linter.style.docString false`
+-/
+#guard_msgs in
+/-- With a trailing
+  quote" -/
+example : Nat := 1
