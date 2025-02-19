23fb8583ad65f6a02d3e90f506237c29c6080272
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2025-02-19 22:09:13 +0000

    undo changes to help merge

--- Mathlib/Data/Nat/Defs.lean
+++ Mathlib/Data/Nat/Defs.lean
@@ -5,9 +5,9 @@ Authors: Floris van Doorn, Leonardo de Moura, Jeremy Avigad, Mario Carneiro
 -/
 import Mathlib.Logic.Function.Basic
 import Mathlib.Logic.Nontrivial.Defs
-import Mathlib.Order.Defs.LinearOrder
+import Mathlib.Tactic.Contrapose
 import Mathlib.Tactic.GCongr.CoreAttrs
-import Mathlib.Tactic.Push
+import Mathlib.Tactic.PushNeg
 import Mathlib.Util.AssertExists
 
 /-!
