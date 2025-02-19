0124499b96e2f098582ba928b55677c795d9b022
Anne C.A. Baanen <vierkantor@vierkantor.com>
2025-02-19 16:47:43 +0100

    Merge Reindex back into Defs

--- Mathlib/LinearAlgebra/Basis/Defs.lean
+++ Mathlib/LinearAlgebra/Basis/Defs.lean
@@ -373,6 +373,137 @@ theorem eq_of_apply_eq {b₁ b₂ : Basis ι R M} : (∀ i, b₁ i = b₂ i) →
 
 end Ext
 
+section MapCoeffs
+
+variable {R' : Type*} [Semiring R'] [Module R' M] (f : R ≃+* R')
+
+attribute [local instance] SMul.comp.isScalarTower
+
+/-- If `R` and `R'` are isomorphic rings that act identically on a module `M`,
+then a basis for `M` as `R`-module is also a basis for `M` as `R'`-module.
+
+See also `Basis.algebraMapCoeffs` for the case where `f` is equal to `algebraMap`.
+-/
+@[simps (config := { simpRhs := true })]
+def mapCoeffs (h : ∀ (c) (x : M), f c • x = c • x) : Basis ι R' M := by
+  letI : Module R' R := Module.compHom R (↑f.symm : R' →+* R)
+  haveI : IsScalarTower R' R M :=
+    { smul_assoc := fun x y z => by
+        change (f.symm x * y) • z = x • (y • z)
+        rw [mul_smul, ← h, f.apply_symm_apply] }
+  exact ofRepr <| (b.repr.restrictScalars R').trans <|
+    Finsupp.mapRange.linearEquiv (Module.compHom.toLinearEquiv f.symm).symm
+
+variable (h : ∀ (c) (x : M), f c • x = c • x)
+
+theorem mapCoeffs_apply (i : ι) : b.mapCoeffs f h i = b i :=
+  apply_eq_iff.mpr <| by
+    -- Porting note: in Lean 3, these were automatically inferred from the definition of
+    -- `mapCoeffs`.
+    letI : Module R' R := Module.compHom R (↑f.symm : R' →+* R)
+    haveI : IsScalarTower R' R M :=
+    { smul_assoc := fun x y z => by
+        change (f.symm x * y) • z = x • (y • z)
+        rw [mul_smul, ← h, f.apply_symm_apply] }
+    simp
+
+@[simp]
+theorem coe_mapCoeffs : (b.mapCoeffs f h : ι → M) = b :=
+  funext <| b.mapCoeffs_apply f h
+
+end MapCoeffs
+
+section ReindexRange
+
+/-- `b.reindexRange` is a basis indexed by `range b`, the basis vectors themselves. -/
+def reindexRange : Basis (range b) R M :=
+  haveI := Classical.dec (Nontrivial R)
+  if h : Nontrivial R then
+    letI := h
+    b.reindex (Equiv.ofInjective b (Basis.injective b))
+  else
+    letI : Subsingleton R := not_nontrivial_iff_subsingleton.mp h
+    .ofRepr (Module.subsingletonEquiv R M (range b))
+
+theorem reindexRange_self (i : ι) (h := Set.mem_range_self i) : b.reindexRange ⟨b i, h⟩ = b i := by
+  by_cases htr : Nontrivial R
+  · letI := htr
+    simp [htr, reindexRange, reindex_apply, Equiv.apply_ofInjective_symm b.injective,
+      Subtype.coe_mk]
+  · letI : Subsingleton R := not_nontrivial_iff_subsingleton.mp htr
+    letI := Module.subsingleton R M
+    simp [reindexRange, eq_iff_true_of_subsingleton]
+
+theorem reindexRange_repr_self (i : ι) :
+    b.reindexRange.repr (b i) = Finsupp.single ⟨b i, mem_range_self i⟩ 1 :=
+  calc
+    b.reindexRange.repr (b i) = b.reindexRange.repr (b.reindexRange ⟨b i, mem_range_self i⟩) :=
+      congr_arg _ (b.reindexRange_self _ _).symm
+    _ = Finsupp.single ⟨b i, mem_range_self i⟩ 1 := b.reindexRange.repr_self _
+
+@[simp]
+theorem reindexRange_apply (x : range b) : b.reindexRange x = x := by
+  rcases x with ⟨bi, ⟨i, rfl⟩⟩
+  exact b.reindexRange_self i
+
+theorem reindexRange_repr' (x : M) {bi : M} {i : ι} (h : b i = bi) :
+    b.reindexRange.repr x ⟨bi, ⟨i, h⟩⟩ = b.repr x i := by
+  nontriviality
+  subst h
+  apply (b.repr_apply_eq (fun x i => b.reindexRange.repr x ⟨b i, _⟩) _ _ _ x i).symm
+  · intro x y
+    ext i
+    simp only [Pi.add_apply, LinearEquiv.map_add, Finsupp.coe_add]
+  · intro c x
+    ext i
+    simp only [Pi.smul_apply, LinearEquiv.map_smul, Finsupp.coe_smul]
+  · intro i
+    ext j
+    simp only [reindexRange_repr_self]
+    apply Finsupp.single_apply_left (f := fun i => (⟨b i, _⟩ : Set.range b))
+    exact fun i j h => b.injective (Subtype.mk.inj h)
+
+@[simp]
+theorem reindexRange_repr (x : M) (i : ι) (h := Set.mem_range_self i) :
+    b.reindexRange.repr x ⟨b i, h⟩ = b.repr x i :=
+  b.reindexRange_repr' _ rfl
+
+section Fintype
+
+variable [Fintype ι] [DecidableEq M]
+
+/-- `b.reindexFinsetRange` is a basis indexed by `Finset.univ.image b`,
+the finite set of basis vectors themselves. -/
+def reindexFinsetRange : Basis (Finset.univ.image b) R M :=
+  b.reindexRange.reindex ((Equiv.refl M).subtypeEquiv (by simp))
+
+theorem reindexFinsetRange_self (i : ι) (h := Finset.mem_image_of_mem b (Finset.mem_univ i)) :
+    b.reindexFinsetRange ⟨b i, h⟩ = b i := by
+  rw [reindexFinsetRange, reindex_apply, reindexRange_apply]
+  rfl
+
+@[simp]
+theorem reindexFinsetRange_apply (x : Finset.univ.image b) : b.reindexFinsetRange x = x := by
+  rcases x with ⟨bi, hbi⟩
+  rcases Finset.mem_image.mp hbi with ⟨i, -, rfl⟩
+  exact b.reindexFinsetRange_self i
+
+theorem reindexFinsetRange_repr_self (i : ι) :
+    b.reindexFinsetRange.repr (b i) =
+      Finsupp.single ⟨b i, Finset.mem_image_of_mem b (Finset.mem_univ i)⟩ 1 := by
+  ext ⟨bi, hbi⟩
+  rw [reindexFinsetRange, repr_reindex, Finsupp.mapDomain_equiv_apply, reindexRange_repr_self]
+  simp [Finsupp.single_apply]
+
+@[simp]
+theorem reindexFinsetRange_repr (x : M) (i : ι)
+    (h := Finset.mem_image_of_mem b (Finset.mem_univ i)) :
+    b.reindexFinsetRange.repr x ⟨b i, h⟩ = b.repr x i := by simp [reindexFinsetRange]
+
+end Fintype
+
+end ReindexRange
+
 variable [Module R M']
 
 section Constr
