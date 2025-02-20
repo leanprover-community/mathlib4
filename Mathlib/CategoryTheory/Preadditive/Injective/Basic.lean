22784363f30ff700211b39ad2b6ef9007c9cc4c8
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-02-20 09:49:50 +0100

    fix

--- Mathlib/CategoryTheory/Preadditive/Injective/Basic.lean
+++ Mathlib/CategoryTheory/Preadditive/Injective/Basic.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2022 Jujian Zhang. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Jujian Zhang, Kevin Buzzard, Joël Riou
+Authors: Jujian Zhang, Kevin Buzzard
 -/
 import Mathlib.CategoryTheory.Preadditive.Projective
 
