861d249d5eb89f1f9081901040161504232b6c12
Salvatore Mercuri <smercuri@ed.ac.uk>
2025-02-20 19:43:54 +0000

    limits of absolute values

--- Mathlib/Analysis/AbsoluteValue/Equivalence.lean
+++ Mathlib/Analysis/AbsoluteValue/Equivalence.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Michael Stoll
 -/
 import Mathlib.Analysis.SpecialFunctions.Pow.Real
+import Mathlib.Analysis.Normed.Ring.WithAbs
 
 /-!
 # Equivalence of real-valued absolute values
@@ -11,6 +12,8 @@ import Mathlib.Analysis.SpecialFunctions.Pow.Real
 Two absolute values `v₁, v₂ : AbsoluteValue R ℝ` are *equivalent* if there exists a
 positive real number `c` such that `v₁ x ^ c = v₂ x` for all `x : R`.
 -/
+
+open Filter
 
 open scoped Topology
 
@@ -61,9 +64,10 @@ lemma eq_trivial_of_isEquiv_trivial [DecidablePred fun x : R ↦ x = 0] [NoZeroD
   · simp only [ne_eq, hx, not_false_eq_true, trivial_apply] at hc ⊢
     exact (Real.rpow_left_inj (f.nonneg x) zero_le_one hc₀.ne').mp <| (Real.one_rpow c).symm ▸ hc
 
-variable {F S : Type*} [Field F] [LinearOrderedField S] {v w : AbsoluteValue F S}
+variable {F S : Type*} [Field F]
 
-theorem lt_one_iff_of_lt_one_imp [Archimedean S] [TopologicalSpace S] [OrderTopology S]
+theorem lt_one_iff_of_lt_one_imp [LinearOrderedField S]
+    {v w : AbsoluteValue F S} [Archimedean S] [TopologicalSpace S] [OrderTopology S]
     (hv : v.IsNontrivial) (h : ∀ x, v x < 1 → w x < 1) {a : F} :
     v a < 1 ↔ w a < 1:= by
   let ⟨x₀, hx₀⟩ := hv.exists_abv_lt_one
@@ -172,7 +176,7 @@ theorem exists_lt_one_one_le_of_not_isEquiv {v w : AbsoluteValue F ℝ} (hv : v.
 If `v` and `w` are two non-trivial and inequivalent absolute values then
 we can find an `a ∈ K` such that `1 < v a` while `w a  < 1`.
 -/
-theorem exists_one_lt_lt_one_of_ne_rpow {v w : AbsoluteValue F ℝ} (hv : v.IsNontrivial)
+theorem exists_one_lt_lt_one_of_not_isEquiv {v w : AbsoluteValue F ℝ} (hv : v.IsNontrivial)
     (hw : w.IsNontrivial) (h : ¬w.IsEquiv v) :
     ∃ a : F, 1 < v a ∧ w a < 1 := by
   let ⟨a, ha⟩ := exists_lt_one_one_le_of_not_isEquiv hv h
@@ -180,5 +184,181 @@ theorem exists_one_lt_lt_one_of_ne_rpow {v w : AbsoluteValue F ℝ} (hv : v.IsNo
   refine ⟨b / a, ?_⟩
   simpa using ⟨one_lt_div (pos_of_pos v (by linarith)) |>.2 (by linarith),
     div_lt_one (by linarith) |>.2 (by linarith)⟩
+
+/--
+`v (1 / (1 + a ^ n)) → 1` if `v a < 1`.
+-/
+theorem tendsto_div_one_add_pow_nhds_one {v : AbsoluteValue F ℝ} {a : F} (ha : v a < 1) :
+    Filter.Tendsto (fun (n : ℕ) => v (1 / (1 + a ^ n))) Filter.atTop (𝓝 1) := by
+  simp_rw [v.isAbsoluteValue.abv_div, v.map_one]
+  nth_rw 2 [show (1 : ℝ) = 1 / 1 by norm_num]
+  apply Tendsto.div tendsto_const_nhds _ one_ne_zero
+  have h_add := Tendsto.const_add 1 <| tendsto_pow_atTop_nhds_zero_of_lt_one (v.nonneg _) ha
+  have h_sub := Tendsto.const_sub 1 <| tendsto_pow_atTop_nhds_zero_of_lt_one (v.nonneg _) ha
+  simp only [add_zero, sub_zero] at h_add h_sub
+  exact tendsto_of_tendsto_of_tendsto_of_le_of_le h_sub h_add (v.one_sub_pow_le _)
+    (v.one_add_pow_le _)
+
+/--
+`v (1 / (1 + a ^ n)) → 0` if `1 < v a`.
+-/
+theorem tendsto_pow_div_one_add_pow_zero {v : AbsoluteValue F ℝ} {a : F} (ha : 1 < v a) :
+    Filter.Tendsto (fun (n : ℕ) => v (1 / (1 + a ^ n))) Filter.atTop (𝓝 0) := by
+  simp_rw [div_eq_mul_inv, one_mul, map_inv₀, fun n => add_comm 1 (a ^ n)]
+  apply Filter.Tendsto.inv_tendsto_atTop
+  apply Filter.tendsto_atTop_mono (fun n => v.le_add _ _)
+  simp_rw [map_one, map_pow v]
+  apply Filter.tendsto_atTop_add_right_of_le _ _ _ (fun _ => le_rfl)
+  refine tendsto_atTop_of_geom_le (by simp only [pow_zero, inv_one, zero_lt_one]) ha fun n => ?_
+  rw [← map_pow, ← map_pow, ← map_mul, pow_succ']
+
+open Filter in
+/--
+Let `a, b ∈ K`, and let `v₁, ..., vₖ` be absolute values with some `1 < vᵢ a` while all other
+`vⱼ a < 1`. Suppose `1 < vᵢ b`. Let `w` be another absolute value on `K` such that `w a = 1`,
+while `w b < 1`. Then we can find a sequence of values in `K` that tends to `∞` under `vᵢ`,
+tends to `0` under `vⱼ`, and is always `< 1` under `w`.
+
+Such a sequence is given by `a ^ n * b`.
+-/
+theorem exists_tendsto_zero_tendsto_atTop_tendsto_const
+    {ι : Type*} {v : ι → AbsoluteValue F ℝ} {w : AbsoluteValue F ℝ} {a b : F} {i : ι}
+    (ha : 1 < v i a) (haj : ∀ j ≠ i, v j a < 1) (haw : w a = 1) (hb : 1 < v i b) (hbw : w b < 1) :
+    ∃ c : ℕ → F,
+      Tendsto (fun n => (v i) (c n)) atTop atTop ∧
+        (∀ j ≠ i, Tendsto (fun n => (v j) (c n)) atTop (𝓝 0)) ∧
+          (∀ n, w (c n) < 1) := by
+  refine ⟨fun n => a ^ n * b, ?_⟩; simp_rw [map_mul, map_pow, haw, one_pow, one_mul]
+  refine ⟨Tendsto.atTop_mul_const (by linarith) (tendsto_pow_atTop_atTop_of_one_lt ha),
+    fun j hj => ?_, fun _ => hbw⟩
+  rw [← zero_mul <| v j b]
+  exact Tendsto.mul_const _ <| tendsto_pow_atTop_nhds_zero_of_lt_one ((v j).nonneg _) (haj j hj)
+
+open scoped Classical in
+/--
+Let `a, b ∈ K`, and let `v₁, ..., vₖ` be absolute values with some `1 < vᵢ a` while all other
+`vⱼ a < 1`. Suppose `1 < vᵢ b`. Let `w` be another absolute value on `K` such that `w a = 1`,
+while `w b < 1`. Then there is an element `k ∈ K` such that `1 < vᵢ k` while `vⱼ k < 1` for all
+`j ≠ i` and `w k < 1`.
+
+This is given by taking large enough values of a witness sequence to
+`exists_tendsto_zero_tendsto_atTop_tendsto_const` (for example `a ^ n * b` works).
+-/
+theorem exists_one_lt_lt_one_lt_one_of_eq_one
+    {ι : Type*} [Fintype ι] {v : ι → AbsoluteValue F ℝ} {w : AbsoluteValue F ℝ} {a b : F} {i : ι}
+    (ha : 1 < v i a) (haj : ∀ j ≠ i, v j a < 1) (haw : w a = 1) (hb : 1 < v i b) (hbw : w b < 1) :
+    ∃ k : F, 1 < v i k ∧ (∀ j ≠ i, v j k < 1) ∧ w k < 1 := by
+  let ⟨c, hc⟩ := exists_tendsto_zero_tendsto_atTop_tendsto_const ha haj haw hb hbw
+  simp_rw [Metric.tendsto_nhds, Filter.tendsto_atTop_atTop, Filter.eventually_atTop,
+    dist_zero_right, ← WithAbs.norm_eq_abv, norm_norm] at hc
+  choose r₁ hr₁ using hc.1 2
+  choose rₙ hrₙ using fun j hj => hc.2.1 j hj 1 (by linarith)
+  let r := Finset.univ.sup fun j => if h : j = i then r₁ else rₙ j h
+  refine ⟨c r, lt_of_lt_of_le (by linarith) (hr₁ r ?_), fun j hj => ?_, hc.2.2 r⟩
+  · exact Finset.le_sup_dite_pos (p := fun j => j = i) (f := fun _ _ => r₁) (Finset.mem_univ _) rfl
+  · convert hrₙ j hj _ <| Finset.le_sup_dite_neg (fun j => j = i) (Finset.mem_univ j) _
+
+open Filter in
+/--
+Let `a, b ∈ K`, and let `v₁, ..., vₖ` be absolute values with some `1 < vᵢ a` while all other
+`vⱼ a < 1`. Let `w` be another absolute value on `K` such that `1 < w a`. Then there is a
+sequence of elements in `K` that tendsto `vᵢ b` under `vᵢ`, tends to `0` under `vⱼ` for `j ≠ i`,
+and tends to `w b` under `w`.
+
+Such a sequence is given by `1 / (1 + a ^ (- n))`.
+-/
+theorem exists_tendsto_const_tendsto_zero_tendsto_const
+    {ι : Type*} {v : ι → AbsoluteValue F ℝ} {w : AbsoluteValue F ℝ} {a : F} {i : ι}
+    (b : F) (ha : 1 < v i a) (haj : ∀ j ≠ i, v j a < 1) (haw : 1 < w a) :
+    ∃ c : ℕ → F,
+      Tendsto (fun n => (v i) (c n)) atTop (𝓝 ((v i) b)) ∧
+        (∀ j ≠ i, Tendsto (fun n => v j (c n)) atTop (𝓝 0)) ∧
+          Tendsto (fun n => w (c n)) atTop (𝓝 (w b)) := by
+  refine ⟨fun n => (1 / (1 + a⁻¹ ^ n) * b), ?_⟩; simp_rw [map_mul]
+  nth_rw 2 [← one_mul (v i b), ← one_mul (w b)]
+  let hai := map_inv₀ (v i) _ ▸ inv_lt_one_of_one_lt₀ ha
+  replace haw := (map_inv₀ w _ ▸ inv_lt_one_of_one_lt₀ haw)
+  refine ⟨Tendsto.mul_const _ (tendsto_div_one_add_pow_nhds_one hai), fun j hj => ?_,
+      Tendsto.mul_const _ (tendsto_div_one_add_pow_nhds_one haw)⟩
+  replace haj := map_inv₀ (v j) _ ▸ (one_lt_inv₀ (pos_of_pos (v j) (by linarith))).2 (haj j hj)
+  exact zero_mul (v j b) ▸ Tendsto.mul_const _ (tendsto_pow_div_one_add_pow_zero haj)
+
+open scoped Classical in
+/--
+Let `a, b ∈ K`, and let `v₁, ..., vₖ` be absolute values with some `1 < vᵢ a` while all other
+`vⱼ a < 1`. Suppose `1 < vᵢ b`. Let `w` be another absolute value on `K` such that `1 < w a`,
+while `w b < 1`. Then there is an element `k ∈ K` such that `1 < vᵢ k` while `vⱼ k < 1` for all
+`j ≠ i` and `w k < 1`.
+
+This is given by taking large enough values of a witness sequence to
+`exists_tendsto_const_tendsto_zero_tendsto_const` (for example `1 / (1 + a ^ (-n))` works).
+
+Note that this is the result `exists_one_lt_lt_one_lt_one_of_eq_one` replacing the condition
+that `w a = 1` with `1 < w a`.
+-/
+theorem exists_one_lt_lt_one_lt_one_of_one_lt
+    {ι : Type*} [Fintype ι] {v : ι → AbsoluteValue F ℝ} {w : AbsoluteValue F ℝ} {a b : F} {i : ι}
+    (ha : 1 < v i a) (haj : ∀ j ≠ i, v j a < 1) (haw : 1 < w a) (hb : 1 < v i b) (hbw : w b < 1) :
+    ∃ k : F, 1 < v i k ∧ (∀ j ≠ i, v j k < 1) ∧ w k < 1 := by
+  let ⟨c, hc⟩ := exists_tendsto_const_tendsto_zero_tendsto_const b ha haj haw
+  have hₙ := fun j hj => Metric.tendsto_nhds.1 <| hc.2.1 j hj
+  simp_rw [Filter.eventually_atTop, dist_zero_right] at hₙ
+  choose r₁ hr₁ using Filter.eventually_atTop.1 <| Filter.Tendsto.eventually_const_lt hb hc.1
+  choose rₙ hrₙ using fun j hj => hₙ j hj 1 (by linarith)
+  choose rN hrN using Filter.eventually_atTop.1 <| Filter.Tendsto.eventually_lt_const hbw hc.2.2
+  let r := max (Finset.univ.sup fun j => if h : j = i then r₁ else rₙ j h) rN
+  refine ⟨c r, hr₁ r ?_, fun j hj => ?_, ?_⟩
+  · exact le_max_iff.2 <| Or.inl <|
+      Finset.le_sup_dite_pos (p := fun j => j = i) (f := fun _ _ => r₁) (Finset.mem_univ _) rfl
+  · simp only [← WithAbs.norm_eq_abv, norm_norm] at hrₙ
+    exact hrₙ j hj _ <| le_max_iff.2 <| Or.inl <|
+      Finset.le_sup_dite_neg (fun j => j = i) (Finset.mem_univ j) _
+  · exact hrN _ <| le_max_iff.2 (Or.inr le_rfl)
+
+/--
+Let `v₁, ..., vₖ` be a collection of at least two non-trivial and pairwise inequivalent
+absolute values. Then there is `a ∈ K` such that `1 < v₁ a` while `vⱼ a < 1` for
+all other `j ≠ 0`.
+-/
+theorem exists_one_lt_lt_one {n : ℕ} {v : Fin (n + 2) → AbsoluteValue F ℝ}
+    (h : ∀ i, (v i).IsNontrivial)
+    (hv : Pairwise fun i j => ¬(v i).IsEquiv (v j)) :
+    ∃ (a : F), 1 < v 0 a ∧ ∀ j ≠ 0, v j a < 1 := by
+  induction n using Nat.case_strong_induction_on with
+  | hz =>
+    let ⟨a, ha⟩ := (v 0).exists_one_lt_lt_one_of_not_isEquiv (h 0) (h 1) (hv zero_ne_one.symm)
+    exact ⟨a, ha.1, by simp [Fin.forall_fin_two]; exact ha.2⟩
+  | hi n ih =>
+    -- Assume the result is true for all smaller collections of absolute values
+    -- Let `a : K` be the value from the collection with the last absolute value removed
+    let ⟨a, ha⟩ := ih n le_rfl (fun _ => h _) (hv.comp_of_injective <| Fin.castSucc_injective _)
+    -- Let `b : K` be the value using then first and last absolute value
+    let ⟨b, hb⟩ := ih 0 (by linarith) (fun _ => h _) <| Pairwise.forall_fin_two hv
+    simp [Fin.forall_fin_two] at hb
+    -- If `v last < 1` then `a` works.
+    by_cases ha₀ : v (Fin.last _) a < 1
+    · refine ⟨a, ha.1, fun j hj => ?_⟩
+      by_cases hj' : j = Fin.last (n + 2)
+      · exact hj' ▸ ha₀
+      · exact ha.2 (Fin.castPred _ (ne_eq _ _ ▸  hj')) <| Fin.castPred_ne_zero _ hj
+    -- If `v last = 1` then this is given by `exists_one_lt_lt_one_lt_one_of_eq_one` with
+    -- `w = v last`.
+    · by_cases ha₁ : v (Fin.last _) a = 1
+      · let ⟨k, hk⟩ := exists_one_lt_lt_one_lt_one_of_eq_one
+          (v := fun i : Fin (n + 2) => v i.castSucc) ha.1 ha.2 ha₁ hb.1 hb.2
+        refine ⟨k, hk.1, fun j hj => ?_⟩
+        by_cases h : j ≠ Fin.last (n + 2)
+        · exact ne_eq _ _ ▸ hk.2.1 (j.castPred h) <| Fin.castPred_ne_zero _ hj
+        · exact not_ne_iff.1 h ▸ hk.2.2
+      -- The last cast `1 < v last` is given by `exists_one_lt_lt_one_lt_one_of_one_lt` with
+      -- `w = v last`.
+      · let ⟨k, hk⟩ := exists_one_lt_lt_one_lt_one_of_one_lt
+          (v := fun i : Fin (n + 2) => v i.castSucc) ha.1 ha.2
+            (lt_of_le_of_ne (not_lt.1 ha₀) (ne_eq _ _ ▸ ha₁).symm) hb.1 hb.2
+        refine ⟨k, hk.1, fun j hj => ?_⟩
+        by_cases h : j ≠ Fin.last _
+        · apply ne_eq _ _ ▸ hk.2.1 (j.castPred h)
+          rwa [← Fin.castPred_zero, Fin.castPred_inj]
+        · exact not_ne_iff.1 h ▸ hk.2.2
 
 end AbsoluteValue
