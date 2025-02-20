df8a24446149a0d2d83d213f988f9a138f5a5f3c
Floris van Doorn <fpvdoorn@gmail.com>
2025-02-20 18:03:12 +0100

    fix

--- Mathlib/Data/Finset/Update.lean
+++ Mathlib/Data/Finset/Update.lean
@@ -3,7 +3,8 @@ Copyright (c) 2023 Floris van Doorn. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Floris van Doorn
 -/
-import Mathlib.Data.Fintype.Basic
+import Mathlib.Data.Finset.Basic
+import Mathlib.Data.Fintype.Defs
 
 /-!
 # Update a function on a set of values
