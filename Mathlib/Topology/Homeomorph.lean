b2b26f195057a046dbc6dced29313c6988c6f084
grunweg <rothgang@math.uni-bonn.de>
2025-02-20 18:07:36 +0000

    chore: fix naming oversight from #22070 (#22128)
    Per [zulip discussion](https://leanprover.zulipchat.com/#narrow/channel/116395-maths/topic/.7BIsInducing.2CIsEmbedding.7D.2Esum_elim/near/500877183): these were merged a few hours ago, hence need no deprecations yet.

--- Mathlib/Topology/Homeomorph.lean
+++ Mathlib/Topology/Homeomorph.lean
@@ -575,11 +575,11 @@ theorem coe_sumComm : ⇑(sumComm X Y) = Sum.swap :=
 
 @[continuity, fun_prop]
 lemma continuous_sumAssoc : Continuous (Equiv.sumAssoc X Y Z) :=
-  Continuous.sum_elim (by fun_prop) (by fun_prop)
+  Continuous.sumElim (by fun_prop) (by fun_prop)
 
 @[continuity, fun_prop]
 lemma continuous_sumAssoc_symm : Continuous (Equiv.sumAssoc X Y Z).symm :=
-  Continuous.sum_elim (by fun_prop) (by fun_prop)
+  Continuous.sumElim (by fun_prop) (by fun_prop)
 
 /-- `(X ⊕ Y) ⊕ Z` is homeomorphic to `X ⊕ (Y ⊕ Z)`. -/
 def sumAssoc : (X ⊕ Y) ⊕ Z ≃ₜ X ⊕ Y ⊕ Z where
@@ -614,7 +614,7 @@ lemma sumSumSumComm_symm : (sumSumSumComm X Y W Z).symm = (sumSumSumComm X W Y Z
 @[simps! (config := .asFn) apply]
 def sumEmpty [IsEmpty Y] : X ⊕ Y ≃ₜ X where
   toEquiv := Equiv.sumEmpty X Y
-  continuous_toFun := Continuous.sum_elim continuous_id (by fun_prop)
+  continuous_toFun := Continuous.sumElim continuous_id (by fun_prop)
   continuous_invFun := continuous_inl
 
 /-- The sum of `X` with any empty topological space is homeomorphic to `X`. -/
@@ -803,9 +803,9 @@ section Distrib
 def sumProdDistrib : (X ⊕ Y) × Z ≃ₜ (X × Z) ⊕ (Y × Z) :=
   Homeomorph.symm <|
     homeomorphOfContinuousOpen (Equiv.sumProdDistrib X Y Z).symm
-        ((continuous_inl.prodMap continuous_id).sum_elim
+        ((continuous_inl.prodMap continuous_id).sumElim
           (continuous_inr.prodMap continuous_id)) <|
-      (isOpenMap_inl.prodMap IsOpenMap.id).sum_elim (isOpenMap_inr.prodMap IsOpenMap.id)
+      (isOpenMap_inl.prodMap IsOpenMap.id).sumElim (isOpenMap_inr.prodMap IsOpenMap.id)
 
 /-- `X × (Y ⊕ Z)` is homeomorphic to `X × Y ⊕ X × Z`. -/
 def prodSumDistrib : X × (Y ⊕ Z) ≃ₜ (X × Y) ⊕ (X × Z) :=
