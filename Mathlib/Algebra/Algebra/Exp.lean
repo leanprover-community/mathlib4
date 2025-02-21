16f699dcb5960ea1bd086ad945999a72caca8f7c
Janos Wolosz <janos.wolosz@gmail.com>
2025-02-21 18:12:24 +0100

    Improve imports

--- Mathlib/Algebra/Algebra/Exp.lean
+++ Mathlib/Algebra/Algebra/Exp.lean
@@ -1,25 +1,10 @@
-/-
-Copyright (c) 2018 Kenny Lau. All rights reserved.
-Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Kenny Lau, Yury Kudryashov
--/
 import Mathlib.Algebra.Algebra.Defs
-import Mathlib.Algebra.Module.Equiv.Basic
-import Mathlib.Algebra.Module.Submodule.Ker
-import Mathlib.Algebra.Module.Submodule.RestrictScalars
-import Mathlib.Algebra.Module.ULift
-import Mathlib.Algebra.Ring.CharZero
-import Mathlib.Algebra.Ring.Subring.Basic
-import Mathlib.Data.Nat.Cast.Order.Basic
-import Mathlib.Data.Int.CharZero
-import Mathlib.RingTheory.Nilpotent.Defs
+import Mathlib.Algebra.BigOperators.Group.Finset.Basic
+import Mathlib.Algebra.Field.Defs
+import Mathlib.Data.Finset.Basic
 import Mathlib.Data.Nat.Factorial.Basic
+import Mathlib.RingTheory.Nilpotent.Defs
 
-/-!
-# Further basic results about `Algebra`.
-
-This file could usefully be split further.
--/
 namespace Algebra
 
 section Exp
@@ -34,7 +19,9 @@ theorem well_def {k : ℕ} (a : A) (h : a ^ k = 0) :
     ∑ n ∈ Finset.range k, (Nat.factorial n : R)⁻¹ • (a ^ n) = exp R A a := by
   sorry
 
--- theorem add_pow (h : Commute x y) (n : ℕ) :
+-- useful: add_pow_eq_zero_of_add_le_succ_of_pow_eq_zero
+-- useful: add_pow (h : Commute x y) (n : ℕ) : ...
+-- useful: isNilpotent_add (h_comm : Commute x y) ...
 theorem exp_add_mul (a b : A) (h₁ : Commute a b) (h₂ : IsNilpotent a) (h₃ : IsNilpotent b) :
     exp R A (a + b) = exp R A a * exp R A b := by
   sorry
