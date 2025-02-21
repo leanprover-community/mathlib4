86909dea9565d3f8314d1065ec2290b282373323
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-02-21 05:19:14 +0100

    fixed imports

--- Mathlib/CategoryTheory/Abelian/GrothendieckCategory/EnoughInjectives.lean
+++ Mathlib/CategoryTheory/Abelian/GrothendieckCategory/EnoughInjectives.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joël Riou
 -/
 import Mathlib.CategoryTheory.Abelian.CommSq
-import Mathlib.CategoryTheory.Abelian.GrothendieckCategory.Basic
+import Mathlib.CategoryTheory.Generator.Basic
 import Mathlib.CategoryTheory.MorphismProperty.IsSmall
 import Mathlib.CategoryTheory.MorphismProperty.Limits
 
