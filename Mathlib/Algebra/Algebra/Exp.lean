b5c54a8d027d8c1dd3b319b4ffbd52f1ffa7498a
Janos Wolosz <janos.wolosz@gmail.com>
2025-02-21 19:05:16 +0100

    Improve naming

--- Mathlib/Algebra/Algebra/Exp.lean
+++ Mathlib/Algebra/Algebra/Exp.lean
@@ -5,28 +5,24 @@ import Mathlib.RingTheory.Nilpotent.Defs
 
 namespace Algebra
 
-section Exp
-
 variable (R : Type*) [Field R] [CharZero R]
 variable (A : Type*) [Semiring A] [Algebra R A]
 
 noncomputable def exp (a : A) : A :=
   ∑ n ∈ Finset.range (nilpotencyClass a), (Nat.factorial n : R)⁻¹ • (a ^ n)
 
-theorem well_def {k : ℕ} (a : A) (h : a ^ k = 0) :
+theorem exp_eq_truncated {k : ℕ} (a : A) (h : a ^ k = 0) :
     ∑ n ∈ Finset.range k, (Nat.factorial n : R)⁻¹ • (a ^ n) = exp R A a := by
   sorry
 
 -- useful: add_pow_eq_zero_of_add_le_succ_of_pow_eq_zero
 -- useful: add_pow (h : Commute x y) (n : ℕ) : ...
 -- useful: isNilpotent_add (h_comm : Commute x y) ...
-theorem exp_add_mul (a b : A) (h₁ : Commute a b) (h₂ : IsNilpotent a) (h₃ : IsNilpotent b) :
+theorem exp_add_of_commute (a b : A) (h₁ : Commute a b) (h₂ : IsNilpotent a) (h₃ : IsNilpotent b) :
     exp R A (a + b) = exp R A a * exp R A b := by
   sorry
 
-theorem exp_inv (a : A) (h : IsNilpotent a) : IsUnit (exp R A a) := by
+theorem exp_is_unit_of_nilpotent (a : A) (h : IsNilpotent a) : IsUnit (exp R A a) := by
   sorry
-
-end Exp
 
 end Algebra
