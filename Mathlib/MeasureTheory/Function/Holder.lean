789bc8cf0e69e2640c7c2a7713daef2f799481bf
Jireh Loreaux <loreaujy@gmail.com>
2025-02-21 13:57:06 -0600

    shake

--- Mathlib/MeasureTheory/Function/Holder.lean
+++ Mathlib/MeasureTheory/Function/Holder.lean
@@ -3,7 +3,6 @@ Copyright (c) 2025 Jireh Loreaux. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jireh Loreaux
 -/
-import Mathlib.Analysis.Normed.Module.Dual
 import Mathlib.MeasureTheory.Integral.Bochner
 
 /-! # Continuous bilinear maps on `MeasureTheory.Lp` spaces
