342de069f6fa89eef6c48a0211c01df7966d2a58
Violeta Hernández <vi.hdz.p@gmail.com>
2025-02-21 22:45:02 -0600

    fix

--- Mathlib/Order/Normal.lean
+++ Mathlib/Order/Normal.lean
@@ -3,7 +3,6 @@ Copyright (c) 2025 Violeta Hernández Palacios. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Violeta Hernández Palacios
 -/
-import Mathlib.Order.SuccPred.CompleteLinearOrder
 import Mathlib.Order.SuccPred.InitialSeg
 
 /-!
