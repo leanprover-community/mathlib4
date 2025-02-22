a76f9e8c8aa508ca604baae7695ba737dfe905df
Janos Wolosz <janos.wolosz@gmail.com>
2025-02-22 12:39:29 +0100

    Steps forward

--- Mathlib.lean
+++ Mathlib.lean
@@ -7,6 +7,7 @@ import Mathlib.Algebra.Algebra.Basic
 import Mathlib.Algebra.Algebra.Bilinear
 import Mathlib.Algebra.Algebra.Defs
 import Mathlib.Algebra.Algebra.Equiv
+import Mathlib.Algebra.Algebra.Exp
 import Mathlib.Algebra.Algebra.Field
 import Mathlib.Algebra.Algebra.Hom
 import Mathlib.Algebra.Algebra.Hom.Rat
--- Mathlib/Algebra/Algebra/Exp.lean
+++ Mathlib/Algebra/Algebra/Exp.lean
@@ -1,7 +1,9 @@
 import Mathlib.Algebra.Algebra.Defs
 import Mathlib.Algebra.BigOperators.Group.Finset.Basic
+import Mathlib.Algebra.BigOperators.Intervals
 import Mathlib.Algebra.Field.Defs
 import Mathlib.RingTheory.Nilpotent.Defs
+import LeanCopilot
 
 namespace Algebra
 
@@ -13,6 +15,15 @@ noncomputable def exp (a : A) : A :=
 
 theorem exp_eq_truncated {k : ℕ} (a : A) (h : a ^ k = 0) :
     ∑ n ∈ Finset.range k, (Nat.factorial n : R)⁻¹ • (a ^ n) = exp R A a := by
+  have h₁ : nilpotencyClass a ≤ k := by
+    exact csInf_le' h
+  have h₃ : ∑ n ∈ Finset.range k, (Nat.factorial n : R)⁻¹ • (a ^ n) =
+      ∑ n ∈ Finset.range (nilpotencyClass a), (Nat.factorial n : R)⁻¹ • (a ^ n) +
+        ∑ n ∈ Finset.Ico (nilpotencyClass a) k, (Nat.factorial n : R)⁻¹ • (a ^ n) :=
+    (Finset.sum_range_add_sum_Ico _ h₁).symm
+  suffices h₄ : ∑ n ∈ Finset.Ico (nilpotencyClass a) k, (Nat.factorial n : R)⁻¹ • (a ^ n) = 0 by
+    dsimp [exp]
+    rw [h₃, h₄, add_zero]
   sorry
 
 -- useful: add_pow_eq_zero_of_add_le_succ_of_pow_eq_zero
