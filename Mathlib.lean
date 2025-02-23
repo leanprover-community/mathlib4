ff3750e28bbaf122529f009ab56f2057347473c0
Markus Himmel <markus@lean-fro.org>
2025-02-23 16:26:42 +0100

    feat(CategoryTheory): pullbacks in functor categories

--- Mathlib.lean
+++ Mathlib.lean
@@ -1992,6 +1992,7 @@ import Mathlib.CategoryTheory.Limits.FunctorCategory.EpiMono
 import Mathlib.CategoryTheory.Limits.FunctorCategory.Filtered
 import Mathlib.CategoryTheory.Limits.FunctorCategory.Finite
 import Mathlib.CategoryTheory.Limits.FunctorCategory.Shapes.Products
+import Mathlib.CategoryTheory.Limits.FunctorCategory.Shapes.Pullbacks
 import Mathlib.CategoryTheory.Limits.FunctorToTypes
 import Mathlib.CategoryTheory.Limits.HasLimits
 import Mathlib.CategoryTheory.Limits.IndYoneda
