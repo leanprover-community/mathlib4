55e2d9d46dc914e963ef4db3ed28ffcb9cae3ada
Markus Himmel <markus@lean-fro.org>
2025-02-23 21:04:09 +0100

    add overview

--- Mathlib/CategoryTheory/Abelian/FreydMitchell.lean
+++ Mathlib/CategoryTheory/Abelian/FreydMitchell.lean
@@ -13,6 +13,50 @@ Let `C` be an abelian category. We construct a ring `FreydMitchell.EmbeddingRing
 `FreydMitchell.embedding : C ⥤ ModuleCat.{max u v} (EmbeddingRing C)` which is full, faithful and
 exact.
 
+## Overview over the proof
+
+The usual stategy to prove the Freyd-Mitchell embedding theorem is as follows:
+
+1. Prove that if `D` is a Grothendieck abelian category and `F : C ⥤ Dᵒᵖ` is a functor from a
+small category, then there is a functor `G : Dᵒᵖ ⥤ ModuleCat R` for a suitable `R` such that `G`
+is faithful and exact and `F ⋙ G` is full.
+2. Find a suitable Grothendieck abelian category `D` and a full, faithful and exact functor
+`F : C ⥤ Dᵒᵖ`.
+
+To prove (1), we proceed as follows:
+
+1. Using the Special Adjoint Functor Theorem and the duality between subobjects and quotients in
+abelian categories, we have that Grothendieck abelian categories have all limits (this is shown in
+`Abelian.GrothendieckCategory.Basic`).
+2. Using the small object argument, it is shown that Grothendieck abelian categories have enough
+injectives (see `Abelian.GrothendieckCategory.EnoughInjectives`).
+3. Putting these two together, it follows that Grothendieck abelian categories have an injective
+cogenerator (see `Generator.Abelian`).
+4. By taking a coproduct of copies of the injective cogenerator, we find a projective separator `G`
+in `Dᵒᵖ` such that every object in the image of `F` is a quotient of `G`. Then the additive Hom
+functor `Hom(G, ·) : Dᵒᵖ ⥤ Module (End G)ᵐᵒᵖ` is faithful (because `G` is a separator), left exact
+(because it is a hom functor), right exact (because `G` is projective) and full (because of a
+combination of the aforementioned properties, see `Abelian.Yoneda`). We put this all together in
+the file `Abelian.GrothendieckCategory.ModuleEmbedding.Opposite`.
+
+To prove (2), there are multiple options.
+
+* Some sources (for example Freyd's "Abelian Categories") choose `D := LeftExactFunctor C Ab`. The
+main difficulty with this approach is that it is not obvious that `D` is abelian. This approach has
+a very algebraic flavor and requires a relatively large armount of ad-hoc reasoning.
+* In the Stacks project, it is suggested to choose `D := Sheaf J Ab` for a suitable Grothendieck
+topology on `Cᵒᵖ` and there are reasons to believe that this `D` is in fact equivalent to
+`LeftExactFunctor C Ab`. This approach translates many of the interesting properties along the
+sheafification adjunction from a category of `Ab`-valued presheaves, which in turn inherits many
+interesting properties from the category of abelian groups.
+* Kashiwara and Schapira choose `D := Ind Cᵒᵖ`, which can be shown to be equivalent to
+`LeftExactFunctor C Ab` (see the file `CategoryTheory.Preadditive.Indization`). This approach
+deduces most interesting properties from the category of types.
+
+When work on this theorem commenced in early 2022, all three apporaches were quite out of reach.
+By the time the theorem was proved in early 2025, both the `Sheaf` approach and the `Ind` approach
+were available in mathlib. The code below uses `D := Ind Cᵒᵖ`.
+
 ## Implementation notes
 
 In the literature you will generally only find this theorem stated for small categories `C`. In
@@ -20,6 +64,11 @@ Lean, we can work around this limitation by passing from `C` to `AsSmall.{max u 
 enlarging the category of modules that we land in (which should be inconsequential in most
 applications) so that our embedding theorem applies to all abelian categories. If `C` was already a
 small category, then this does not change anything.
+
+## References
+
+* https://stacks.math.columbia.edu/tag/05PL
+* [M. Kashiwara, P. Schapira, *Categories and Sheaves*][Kashiwara2006], Section 9.6
 -/
 
 universe v u
