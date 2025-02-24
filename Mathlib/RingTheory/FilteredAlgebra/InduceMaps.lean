7043754dc5502498602d9e982868feba5fcc3548
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2025-02-24 11:13:00 +0800

    fix with new definition

--- Mathlib/RingTheory/FilteredAlgebra/InduceMaps.lean
+++ Mathlib/RingTheory/FilteredAlgebra/InduceMaps.lean
@@ -47,8 +47,6 @@ class SetLikeHom (σA : Type*) [SetLike σA A] {B : Type*} (σB : Type*) [SetLik
   different conditions in the similar structures such as `AddSubroup`, `module`, `Ideal`. -/
   galois : ∃ comap : σB → σA, GaloisConnection map comap
 
-
-
 open SetLikeHom Set
 /-- It is `F` part of the filtration induced by f: A → B -/
 def FB (FA : ι → σA) (f : A → B) [SetLikeHom σA σB f]: ι → σB := fun i ↦ map f (FA i)
@@ -61,22 +59,20 @@ def FB_lt (FA_lt : ι → σA) (f : A → B) [SetLikeHom σA σB f] : outParam <
 instance HomtoFil (FA FA_lt : ι → σA) (f : A → B) [fil : IsFiltration FA FA_lt]
 [SetLikeHom σA σB f] : IsFiltration (FB σA σB FA f) (FB_lt σA σB FA_lt f) (ι := ι) where
   mono {i j i_le_j}:= by
-    show (((map f <| FA i) : σB) : Set B) ≤ (((map f <| FA j) : σB) : Set B)
-    rw[← coe_map <| FA i, ← coe_map <| FA j]
-    exact le_iff_subset.mpr <| image_mono <| IsFiltration.mono i_le_j
+    show (((map f (FA i)) : σB) : Set B) ≤ (((map f (FA j)) : σB) : Set B)
+    rw[← coe_map (FA i), ← coe_map (FA j)]
+    exact image_mono (IsFiltration.mono i_le_j)
   is_le {i j i_lt_j}:= by
-    show (((map f <| FA i) : σB) : Set B) ≤ (((map f <| FA_lt j) : σB) : Set B)
-    rw[← coe_map <| FA i, ← coe_map <| FA_lt j]
-    exact le_iff_subset.mpr <| image_mono <| IsFiltration.is_le i_lt_j
+    show (((map f (FA i)) : σB) : Set B) ≤ (((map f (FA_lt j)) : σB) : Set B)
+    rw[← coe_map (FA i), ← coe_map (FA_lt j)]
+    exact image_mono (IsFiltration.is_le i_lt_j)
   is_sup {Sup j h}:= by
     obtain⟨comap, galois⟩ := galois (f := f) (σA := σA) (σB := σB)
     apply galois.l_le
-    have h : ∀ i < j, FA i ≤ comap Sup := fun i i_lt_j ↦ galois.le_u <| h i i_lt_j
+    have h : ∀ i < j, FA i ≤ comap Sup := fun i i_lt_j ↦ galois.le_u (h i i_lt_j)
     exact IsFiltration.is_sup (comap Sup) j h
 
 end HomtoFil
-
-
 
 section RingHomtoFil
 
@@ -100,17 +96,17 @@ instance RingHomtoFil (FR FR_lt: ι → σR) [fil : IsRingFiltration FR FR_lt] :
     IsRingFiltration (FS σR σS FR f) (FS_lt σR σS FR_lt f) where
   __ := HomtoFil σR σS FR FR_lt f
   one_mem := by
-    show 1 ∈ ((map f <| FR 0 : σS) : Set S)
-    rw[← coe_map <| FR 0]
+    show 1 ∈ ((map f (FR 0) : σS) : Set S)
+    rw[← coe_map (FR 0)]
     use 1
     simp only [SetLike.mem_coe, map_one, and_true, SetLike.GradedOne.one_mem]
   mul_mem {i j x y x_in_i y_in_j}:= by
-    show x * y ∈ ((map f <| FR <| i + j : σS) : Set S)
-    rw[← coe_map <| FR <| i + j]
+    show x * y ∈ ((map f (FR (i + j)) : σS) : Set S)
+    rw[← coe_map (FR (i + j))]
     have x_in_i : x ∈ ((map f (FR i) : σS) : Set S) := x_in_i
-    rw[← coe_map <| FR i] at x_in_i
+    rw[← coe_map (FR i)] at x_in_i
     have y_in_j : y ∈ ((map f (FR j) : σS) : Set S) := y_in_j
-    rw[← coe_map <| FR j] at y_in_j
+    rw[← coe_map (FR j)] at y_in_j
     obtain ⟨x₁, x_in, x_eq⟩ := x_in_i
     obtain ⟨y₁, y_in, y_eq⟩ := y_in_j
     use x₁ * y₁
@@ -147,14 +143,12 @@ instance ModuleHomtoFil [SetLikeHom σM σN f] :
     IsModuleFiltration FR FR_lt (FN σM σN FM f) (FN_lt σM σN FM_lt f) where
   __ := HomtoFil σM σN (f := f.toFun) (ι := ι) (FA := FM) (FA_lt := FM_lt)
   smul_mem {i j r n hr hn}:= by
-    have hn : n ∈ ((map f <| FM j : σN) : Set N) := hn
-    rw[← coe_map <| FM j] at hn
+    have hn : n ∈ ((map f (FM j) : σN) : Set N) := hn
+    rw[← coe_map (FM j)] at hn
     obtain⟨m, hm, heq⟩ := hn
-    show r • n ∈ ((map f (FM <| i + j) : σN) : Set N)
-    rw[← coe_map <| FM (i + j), ← heq, ← (LinearMap.CompatibleSMul.map_smul f r m)]
+    show r • n ∈ ((map f (FM (i + j)) : σN) : Set N)
+    rw[← coe_map (FM (i + j)), ← heq, ← LinearMap.CompatibleSMul.map_smul f r m]
     use r • m
-    have := IsModuleFiltration.smul_mem hr hm
-    rw[vadd_eq_add] at this
-    simp only [SetLike.mem_coe, this, map_smul, and_self]
+    simpa [SetLike.mem_coe, map_smul] using IsModuleFiltration.toGradedSMul.smul_mem hr hm
 
 end ModuleHomtoFil
