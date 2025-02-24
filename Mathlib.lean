90af39306ee4585d263844e95ea7479e8822ec1e
Peter Nelson <71660771+apnelson1@users.noreply.github.com>
2025-02-24 09:31:41 +0000

    feat(Data/Matroid/Loop): matroid loops (#22045)
    We add a definition of a loop element of a matroid, with basic API.

--- Mathlib.lean
+++ Mathlib.lean
@@ -2913,6 +2913,7 @@ import Mathlib.Data.Matroid.Constructions
 import Mathlib.Data.Matroid.Dual
 import Mathlib.Data.Matroid.IndepAxioms
 import Mathlib.Data.Matroid.Init
+import Mathlib.Data.Matroid.Loop
 import Mathlib.Data.Matroid.Map
 import Mathlib.Data.Matroid.Rank.Cardinal
 import Mathlib.Data.Matroid.Rank.Finite
