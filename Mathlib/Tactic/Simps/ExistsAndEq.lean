d3b7c20cecba5f739fb9a7e0a3139decd4988a0b
Vasily Nesterov <vas.nesterov63@gmail.com>
2025-02-25 13:06:51 +0300

    fix

--- Mathlib/Tactic/Simps/ExistsAndEq.lean
+++ Mathlib/Tactic/Simps/ExistsAndEq.lean
@@ -1,9 +1,26 @@
+/-
+Copyright (c) 2025 Vasilii Nesterov. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Vasilii Nesterov
+-/
+import Mathlib.Init
 import Qq
+
+/-!
+# simproc for `∃ a', ... ∧ a' = a ∧ ...`
+
+This module implements the `existsAndEq` simproc that checks whether `P a'` has
+the form `... ∧ a' = a ∧ ...` for the goal `∃ a', P a'`. If so, it rewrites the latter as `P a`.
+-/
 
 open Lean Meta Qq
 
+namespace existsAndEq
+
 universe u in
-theorem exists_of_imp_eq {α : Sort u} {p : α → Prop} {a : α} (h : ∀ b, p b → a = b) : (∃ b, p b) ↔ p a := by
+theorem exists_of_imp_eq {α : Sort u} {p : α → Prop} {a : α} (h : ∀ b, p b → a = b) :
+    (∃ b, p b) = p a := by
+  apply propext
   constructor
   · intro h'
     obtain ⟨b, hb⟩ := h'
@@ -11,19 +28,24 @@ theorem exists_of_imp_eq {α : Sort u} {p : α → Prop} {a : α} (h : ∀ b, p 
   · intro h'
     exact ⟨a, h'⟩
 
+set_option linter.unusedVariables false in
+/-- For an expression `p` of the form `fun (x : α) ↦ (body : Prop)`, checks whether
+`body` implies `x = a` for some `a`, and constructs a proof of `(∃ x, p x) = p a` using
+`exists_of_imp_eq`. -/
 partial def fingImpEqProof (p : Expr) : MetaM <| Option Expr := do
   lambdaTelescope p fun xs body => do
     let #[x] := xs | return .none
     let ⟨_, _, x⟩ ← inferTypeQ x
     let ⟨u, _, body⟩ ← inferTypeQ body
-    have : u =QL 0 := ⟨⟩
+    let _ : u =QL 0 := ⟨⟩
     withLocalDeclQ (u := 0) .anonymous .default body fun h => do
       let .some proof ← go x h | return .none
       let pf1 ← mkLambdaFVars #[x, h] proof
       let pf2 ← mkAppM ``exists_of_imp_eq #[pf1]
-      let pf3 ← mkAppM ``propext #[pf2]
-      return .some pf3
+      return .some pf2
 where
+  /-- Traverses the expression `h`, branching at each `And`, to find a proof of `x = a`
+  for some `a`. -/
   go {u : Level} {α : Q(Sort u)} (x : Q($α)) {e : Q(Prop)} (h : Q($e)): MetaM <| Option Expr := do
     match e with
     | ~q(@Eq $α $a $b) =>
@@ -42,8 +64,12 @@ where
         | .none => return .none
     | _ => return .none
 
+end existsAndEq
+
+/-- Checks whether `P a'` has the form `... ∧ a' = a ∧ ...` in the goal `∃ a', P a'`.
+If so, rewrites the goal as `P a`. -/
 simproc existsAndEq (Exists (fun _ => And _ _)) := fun e => do
   let_expr Exists _ p := e | return .continue
-  let .some kek := ← fingImpEqProof p | return .continue
-  let some (_, _, rhs) := (← inferType kek).eq? | return .continue
-  return .visit {expr := rhs, proof? := kek}
+  let .some pf := ← existsAndEq.fingImpEqProof p | return .continue
+  let some (_, _, rhs) := (← inferType pf).eq? | return .continue
+  return .visit {expr := rhs, proof? := pf}
