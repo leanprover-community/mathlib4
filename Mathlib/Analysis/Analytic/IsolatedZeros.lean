528bc7c5957b6831070bc9359ce45068516adea6
Stefan Kebekus <kebekus@users.noreply.github.com>
2025-02-25 13:58:58 +0100

    Implement reviewer's suggestion

--- Mathlib/Analysis/Analytic/IsolatedZeros.lean
+++ Mathlib/Analysis/Analytic/IsolatedZeros.lean
@@ -317,22 +317,21 @@ section PreimgCodiscrete
 
 /-- Preimages of codiscrete sets, local version: if `f` is analytic at `x` and not locally constant,
 then the preimage of any punctured neighbourhood of `f x` is a punctured neighbourhood of `x`. -/
-theorem AnalyticAt.preimg_of_nhdsNE {x : 𝕜} {f : 𝕜 → E} {s : Set E} (hfx : AnalyticAt 𝕜 f x)
+theorem AnalyticAt.preimage_of_nhdsNE {x : 𝕜} {f : 𝕜 → E} {s : Set E} (hfx : AnalyticAt 𝕜 f x)
     (h₂f : ¬EventuallyConst f (𝓝 x)) (hs : s ∈ 𝓝[≠] f x) :
     f ⁻¹' s ∈ 𝓝[≠] x := by
   have : ∀ᶠ (z : 𝕜) in 𝓝 x, f z ∈ insert (f x) s := by
     filter_upwards [hfx.continuousAt.preimage_mem_nhds (insert_mem_nhds_iff.2 hs)]
     tauto
   by_contra h
-  have : EventuallyConst f (𝓝 x) := by
-    rw [eventuallyConst_iff_exists_eventuallyEq]
-    use f x
-    rw [EventuallyEq, ← hfx.frequently_eq_iff_eventually_eq analyticAt_const]
-    apply ((frequently_imp_distrib_right.2 h).and_eventually
-      (eventually_nhdsWithin_of_eventually_nhds this)).mono
-    intro z ⟨h₁z, h₂z⟩
-    rw [Set.mem_insert_iff] at h₂z
-    tauto
+  absurd h₂f
+  rw [eventuallyConst_iff_exists_eventuallyEq]
+  use f x
+  rw [EventuallyEq, ← hfx.frequently_eq_iff_eventually_eq analyticAt_const]
+  apply ((frequently_imp_distrib_right.2 h).and_eventually
+    (eventually_nhdsWithin_of_eventually_nhds this)).mono
+  intro z ⟨h₁z, h₂z⟩
+  rw [Set.mem_insert_iff] at h₂z
   tauto
 
 /-- Preimages of codiscrete sets, local filter version: if `f` is analytic at `x` and not locally
@@ -340,20 +339,20 @@ constant, then the push-forward of the punctured neighbourhood filter `𝓝[≠]
 equal to the punctured neighbourhood filter `𝓝[≠] f x`. -/
 theorem AnalyticAt.map_nhdsNE {x : 𝕜} {f : 𝕜 → E} (hfx : AnalyticAt 𝕜 f x)
     (h₂f : ¬EventuallyConst f (𝓝 x)) :
-    (𝓝[≠] x).map f ≤ (𝓝[≠] f x) := fun _ hs ↦ mem_map.1 (preimg_of_nhdsNE hfx h₂f hs)
+    (𝓝[≠] x).map f ≤ (𝓝[≠] f x) := fun _ hs ↦ mem_map.1 (preimage_of_nhdsNE hfx h₂f hs)
 
 /-- Preimages of codiscrete sets: if `f` is analytic on a neighbourhood of `U` and not locally
 constant, then the preimage of any subset codiscrete within `f '' U` is codiscrete within `U`.
 
 Applications might want to use the theorem `Filter.codiscreteWithin.mono`.
 -/
-theorem AnalyticOnNhd.preimg_mem_codiscreteWithin {U : Set 𝕜} {s : Set E} {f : 𝕜 → E}
+theorem AnalyticOnNhd.preimage_mem_codiscreteWithin {U : Set 𝕜} {s : Set E} {f : 𝕜 → E}
     (hfU : AnalyticOnNhd 𝕜 f U) (h₂f : ∀ x ∈ U, ¬EventuallyConst f (𝓝 x))
     (hs : s ∈ codiscreteWithin (f '' U)) :
     f ⁻¹' s ∈ codiscreteWithin U := by
   simp_rw [mem_codiscreteWithin, disjoint_principal_right, Set.compl_diff] at *
   intro x hx
-  apply mem_of_superset ((hfU x hx).preimg_of_nhdsNE (h₂f x hx) (hs (f x) (by tauto)))
+  apply mem_of_superset ((hfU x hx).preimage_of_nhdsNE (h₂f x hx) (hs (f x) (by tauto)))
   rw [preimage_union, preimage_compl]
   apply union_subset_union_right (f ⁻¹' s)
   intro x hx
@@ -369,6 +368,6 @@ Applications might want to use the theorem `Filter.codiscreteWithin.mono`.
 theorem AnalyticOnNhd.map_codiscreteWithin {U : Set 𝕜} {f : 𝕜 → E}
     (hfU : AnalyticOnNhd 𝕜 f U) (h₂f : ∀ x ∈ U, ¬EventuallyConst f (𝓝 x)) :
     map f (codiscreteWithin U) ≤ (codiscreteWithin (f '' U)) :=
-  fun _ hs ↦ mem_map.1 (preimg_mem_codiscreteWithin hfU h₂f hs)
+  fun _ hs ↦ mem_map.1 (preimage_mem_codiscreteWithin hfU h₂f hs)
 
 end PreimgCodiscrete
