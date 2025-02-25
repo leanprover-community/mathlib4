eaea37f18a8734c362a1fef271a82fd375a11247
Vasily Nesterov <vas.nesterov63@gmail.com>
2025-02-25 13:28:10 +0300

    import it in Init

--- Mathlib/Init.lean
+++ Mathlib/Init.lean
@@ -12,6 +12,9 @@ import Mathlib.Tactic.Linter.UnusedTacticExtension
 import Mathlib.Tactic.Linter.UnusedTactic
 import Mathlib.Tactic.Linter.Style
 
+-- To test that the new simproc doesn't break anything
+import Mathlib.Tactic.Simps.ExistsAndEq
+
 /-!
 This is the root file in Mathlib: it is imported by virtually *all* Mathlib files.
 For this reason, the imports of this files are carefully curated.
