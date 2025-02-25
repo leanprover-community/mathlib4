64ee5a381044970b5634cde690e95bf64e2a7dab
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-02-25 21:39:47 +0100

    wip

--- Mathlib/CategoryTheory/Shift/Predicate.lean
+++ Mathlib/CategoryTheory/Shift/Predicate.lean
@@ -7,10 +7,10 @@ import Mathlib.CategoryTheory.ObjectProperty.ClosedUnderIsomorphisms
 import Mathlib.CategoryTheory.Shift.Basic
 
 /-!
-# Predicates on categories equipped with shift
+# Properties of objects on categories equipped with shift
 
-Given a predicate `P : C → Prop` on objects of a category equipped with a shift
-by `A`, we define shifted predicates `PredicateShift P a` for all `a : A`.
+Given a predicate `P : ObjectProperty C` on objects of a category equipped with a shift
+by `A`, we define shifted properties of objects `P.shift a` for all `a : A`.
 
 -/
 
@@ -21,28 +21,38 @@ namespace CategoryTheory
 variable {C : Type*} [Category C] (P : ObjectProperty C)
   {A : Type*} [AddMonoid A] [HasShift C A]
 
+namespace ObjectProperty
+
 /-- Given a predicate `P : C → Prop` on objects of a category equipped with a shift by `A`,
 this is the predicate which is satisfied by `X` if `P (X⟦a⟧)`. -/
-def PredicateShift (a : A) : ObjectProperty C := fun X => P (X⟦a⟧)
+def shift (a : A) : ObjectProperty C := fun X => P (X⟦a⟧)
 
-lemma predicateShift_iff (a : A) (X : C) : PredicateShift P a X ↔ P (X⟦a⟧) := Iff.rfl
+lemma prop_shift_iff (a : A) (X : C) : P.shift a X ↔ P (X⟦a⟧) := Iff.rfl
 
-instance predicateShift_closedUnderIsomorphisms (a : A) [P.IsClosedUnderIsomorphisms] :
-    (PredicateShift P a).IsClosedUnderIsomorphisms  where
+instance (a : A) [P.IsClosedUnderIsomorphisms] :
+    (P.shift a).IsClosedUnderIsomorphisms  where
   of_iso e hX := P.prop_of_iso ((shiftFunctor C a).mapIso e) hX
 
 variable (A)
 
 @[simp]
-lemma predicateShift_zero [P.IsClosedUnderIsomorphisms] : PredicateShift P (0 : A) = P := by
+lemma shift_zero [P.IsClosedUnderIsomorphisms] : P.shift (0 : A) = P := by
   ext X
   exact P.prop_iff_of_iso ((shiftFunctorZero C A).app X)
 
 variable {A}
 
-lemma predicateShift_predicateShift (a b c : A) (h : a + b = c) [P.IsClosedUnderIsomorphisms] :
-    PredicateShift (PredicateShift P b) a = PredicateShift P c := by
+lemma shift_shift (a b c : A) (h : a + b = c) [P.IsClosedUnderIsomorphisms] :
+    (P.shift b).shift a = P.shift c := by
   ext X
   exact P.prop_iff_of_iso ((shiftFunctorAdd' C a b c h).symm.app X)
+
+end ObjectProperty
+
+@[deprecated (since := "2025-02-25")] alias PredicateShift := ObjectProperty.shift
+@[deprecated (since := "2025-02-25")] alias predicateShift_iff := ObjectProperty.prop_shift_iff
+@[deprecated (since := "2025-02-25")] alias predicateShift_zero := ObjectProperty.shift_zero
+@[deprecated (since := "2025-02-25")] alias predicateShift_predicateShift :=
+  ObjectProperty.shift_shift
 
 end CategoryTheory
