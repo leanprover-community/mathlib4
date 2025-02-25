6afef28b3d770b57b6a0589593947ea546d1ab4f
xyzw12345 <wangjt2020@163.com>
2025-02-25 18:53:24 +0800

    split PR

--- Mathlib.lean
+++ Mathlib.lean
@@ -4588,8 +4588,6 @@ import Mathlib.RingTheory.Etale.Kaehler
 import Mathlib.RingTheory.Etale.Pi
 import Mathlib.RingTheory.EuclideanDomain
 import Mathlib.RingTheory.Extension
-import Mathlib.RingTheory.FilteredAlgebra.AssociatedGraded
-import Mathlib.RingTheory.FilteredAlgebra.Basic
 import Mathlib.RingTheory.Filtration
 import Mathlib.RingTheory.FiniteLength
 import Mathlib.RingTheory.FinitePresentation
