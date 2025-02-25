62e379cb518434384977ac63054ce9e927cae71c
Vasily Nesterov <vas.nesterov63@gmail.com>
2025-02-25 14:01:11 +0300

    import it in Tactic.Basic

--- Mathlib/Tactic/Basic.lean
+++ Mathlib/Tactic/Basic.lean
@@ -9,6 +9,8 @@ import Mathlib.Tactic.ExtendDoc
 import Mathlib.Tactic.Lemma
 import Mathlib.Tactic.TypeStar
 import Mathlib.Tactic.Linter.OldObtain
+-- To test that the new simproc doesn't break anything
+import Mathlib.Tactic.Simps.ExistsAndEq
 
 /-!
 # Basic tactics and utilities for tactic writing
