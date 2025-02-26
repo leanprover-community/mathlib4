713a397b6e31035490e92c8402e04b8151b7afd8
Mario Carneiro <marioc@chalmers.se>
2025-02-26 16:56:25 +0000

    feat(AlgebraicTopology/NerveAdjunction): nerve adjunction infrastructure (#21885)
    This develops preliminaries for the construction of the adjunction between the 2-truncated nerve and the 2-truncated homotopy category functor. The key technical result is `toNerve₂.mk` and `toNerve₂.ext`, which demonstrates that a map of 2-truncated simplicial sets valued in a 2-truncated nerve is determined uniquely by the underlying refl prefunctor provided a composition hypothesis is met. 
    
    
    
    Co-authored-by: Mario Carneiro <di.gama@gmail.com>
    Co-authored-by: emilyriehl <eriehl@jhu.edu>
    Co-authored-by: Emily Riehl <eriehl@jhu.edu>
    Co-authored-by: Joël Riou <joel.riou@universite-paris-saclay.fr>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1166,6 +1166,7 @@ import Mathlib.AlgebraicTopology.SimplicialSet.Horn
 import Mathlib.AlgebraicTopology.SimplicialSet.KanComplex
 import Mathlib.AlgebraicTopology.SimplicialSet.Monoidal
 import Mathlib.AlgebraicTopology.SimplicialSet.Nerve
+import Mathlib.AlgebraicTopology.SimplicialSet.NerveAdjunction
 import Mathlib.AlgebraicTopology.SimplicialSet.Path
 import Mathlib.AlgebraicTopology.SimplicialSet.StdSimplex
 import Mathlib.AlgebraicTopology.SimplicialSet.StrictSegal
