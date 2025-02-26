d3d29cc28c9d90b37bf23f8eb49918cc5d2eb10e
Kim Morrison <kim@tqft.net>
2025-02-26 20:16:37 +1100

    shake

--- Mathlib/Algebra/Field/Defs.lean
+++ Mathlib/Algebra/Field/Defs.lean
@@ -47,10 +47,10 @@ assert_not_imported Mathlib.Tactic.Common
 
 -- `NeZero` theory should not be needed in the basic algebraic hierarchy
 assert_not_imported Mathlib.Algebra.NeZero
-
 assert_not_exists MonoidHom
+assert_not_exists Set
 
-open Function Set
+open Function
 
 universe u
 
