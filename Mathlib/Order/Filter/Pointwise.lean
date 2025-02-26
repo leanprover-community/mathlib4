eb34b87b2ca65c215b235a588b777d2bb0b39fc4
Yaël Dillies <yael.dillies@gmail.com>
2025-02-26 16:41:11 +0000

    chore: split off the action part of pointwise files

--- Mathlib/Order/Filter/Pointwise.lean
+++ Mathlib/Order/Filter/Pointwise.lean
@@ -3,7 +3,7 @@ Copyright (c) 2019 Zhouhang Zhou. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Zhouhang Zhou, Yaël Dillies
 -/
-import Mathlib.Algebra.Group.Action.Pointwise.Set
+import Mathlib.Algebra.Group.Action.Pointwise.Set.Basic
 import Mathlib.Algebra.Order.Group.Defs
 import Mathlib.Algebra.Order.Group.OrderIso
 import Mathlib.Algebra.Ring.Defs
