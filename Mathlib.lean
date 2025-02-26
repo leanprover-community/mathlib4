c73be83d9904ecf3c033fc71d0af1b23a9ce7eef
sinhp <sinahazratpour@gmail.com>
2025-02-26 12:40:27 +0100

    sections as the right adjoint of star

--- Mathlib.lean
+++ Mathlib.lean
@@ -1862,6 +1862,7 @@ import Mathlib.CategoryTheory.Comma.LocallySmall
 import Mathlib.CategoryTheory.Comma.Over.Basic
 import Mathlib.CategoryTheory.Comma.Over.OverClass
 import Mathlib.CategoryTheory.Comma.Over.Pullback
+import Mathlib.CategoryTheory.Comma.Over.Sections
 import Mathlib.CategoryTheory.Comma.Presheaf.Basic
 import Mathlib.CategoryTheory.Comma.Presheaf.Colimit
 import Mathlib.CategoryTheory.Comma.StructuredArrow.Basic
