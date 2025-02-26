b021f1744788ef43c9d00746095b091e2a1fb884
101damnations <101damnations@github.com>
2025-02-26 18:06:41 +0000

    tidy

--- Mathlib/RepresentationTheory/Rep.lean
+++ Mathlib/RepresentationTheory/Rep.lean
@@ -131,12 +131,12 @@ noncomputable instance : PreservesColimits (forget₂ (Rep k G) (ModuleCat.{u} k
 theorem epi_iff_surjective {A B : Rep k G} (f : A ⟶ B) : Epi f ↔ Function.Surjective f.hom :=
   ⟨fun _ => (ModuleCat.epi_iff_surjective ((forget₂ _ _).map f)).1 inferInstance,
   fun h => (forget₂ _ _).epi_of_epi_map ((ModuleCat.epi_iff_surjective <|
-    ((forget₂ _ _).map f)).2 h)⟩
+    (forget₂ _ _).map f).2 h)⟩
 
 theorem mono_iff_injective {A B : Rep k G} (f : A ⟶ B) : Mono f ↔ Function.Injective f.hom :=
   ⟨fun _ => (ModuleCat.mono_iff_injective ((forget₂ _ _).map f)).1 inferInstance,
   fun h => (forget₂ _ _).mono_of_mono_map ((ModuleCat.mono_iff_injective <|
-    ((forget₂ _ _).map f)).2 h)⟩
+    (forget₂ _ _).map f).2 h)⟩
 
 /- Porting note: linter complains `simp` unfolds some types in the LHS, so
 have removed `@[simp]`. -/
