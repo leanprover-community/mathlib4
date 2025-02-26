7070f70f7f1eab780c7c6a6263720c606bd2397d
sinhp <sinahazratpour@gmail.com>
2025-02-26 13:33:38 +0100

    definition of lccc

--- Mathlib.lean
+++ Mathlib.lean
@@ -2173,6 +2173,7 @@ import Mathlib.CategoryTheory.Localization.SmallShiftedHom
 import Mathlib.CategoryTheory.Localization.StructuredArrow
 import Mathlib.CategoryTheory.Localization.Triangulated
 import Mathlib.CategoryTheory.Localization.Trifunctor
+import Mathlib.CategoryTheory.LocallyCartesianClosed.Basic
 import Mathlib.CategoryTheory.Monad.Adjunction
 import Mathlib.CategoryTheory.Monad.Algebra
 import Mathlib.CategoryTheory.Monad.Basic
