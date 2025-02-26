7207399bd983dba8339e94105611bf6a5a1904b3
Eric Wieser <wieser.eric@gmail.com>
2025-02-26 21:35:34 +0000

    stricter tests

--- MathlibTest/Simproc/ExistsAndEq.lean
+++ MathlibTest/Simproc/ExistsAndEq.lean
@@ -4,14 +4,23 @@ universe u
 variable (α : Type u) (p q : α → Prop)
 
 example (a : α) (hp : p a) (hq : q a) : ∃ b : α, (p b ∧ b = a) ∧ q b := by
-  simp only [existsAndEq, and_true]
-  guard_target = p a ∧ q a
-  exact ⟨hp, hq⟩
+  simp only [existsAndEq]
+  guard_target = (p a ∧ True) ∧ q a
+  exact ⟨⟨hp, trivial⟩, hq⟩
 
 example (a : α) : ∃ b : α, b = a := by
   simp only [existsAndEq]
 
+open Lean Meta Simp
+
+set_option linter.unusedTactic false in
 example (a : α) (hp : p a) (hq : q a) : (∃ b : α, p b ∧ (∃ c : α, b = a ∧ q c)) := by
-  simp only [exists_and_left, existsAndEq, hp, true_and]
-  guard_target = ∃ x, q x
-  exact ⟨a, hq⟩
+  -- the simproc doesn't handle nested `Exists`
+  simp -failIfUnchanged only [existsAndEq]
+  guard_target = ∃ b : α, p b ∧ (∃ c : α, b = a ∧ q c)
+  simp only [exists_and_left]
+  guard_target = ∃ b, p b ∧ b = a ∧ ∃ x, q x
+  -- but can clean up the rest
+  simp only [existsAndEq]
+  guard_target = p a ∧ True ∧ ∃ x, q x
+  exact ⟨hp, trivial, a, hq⟩
