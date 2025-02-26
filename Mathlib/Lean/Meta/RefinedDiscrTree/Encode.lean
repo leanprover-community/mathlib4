5ed7f601ba2d8c1cbf5391b67294615d4824c084
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2025-02-26 14:43:28 +0000

    remove outdated comment

--- Mathlib/Lean/Meta/RefinedDiscrTree/Encode.lean
+++ Mathlib/Lean/Meta/RefinedDiscrTree/Encode.lean
@@ -119,8 +119,6 @@ private def encodingStepAux (e : Expr) (lambdas : List FVarId) (root : Bool) : L
   match e.getAppFn with
   | .const n _ =>
     unless root do
-      /- since `(fun _ => 0) = 0` and `(fun _ => 1) = 1`,
-      we don't index lambdas before literals -/
       if let some v := toNatLit? e then
         return ← withLams lambdas <| .lit v
     unless e.getAppNumArgs == 0 do
