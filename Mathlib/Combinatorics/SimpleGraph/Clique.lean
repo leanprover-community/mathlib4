b9096725d38e0e516091d1f200a1357e30351d41
jt496 <john.talbot@gmail.com>
2025-02-26 16:58:44 +0000

    removed extra lemma

--- Mathlib/Combinatorics/SimpleGraph/Clique.lean
+++ Mathlib/Combinatorics/SimpleGraph/Clique.lean
@@ -427,16 +427,11 @@ lemma CliqueFree.mem_of_sup_edge_isNClique {x y : α} {t : Finset α} {n : ℕ} 
   by_contra! hf
   have ht : (t : Set α) \ {x} = t := sdiff_eq_left.mpr <| Set.disjoint_singleton_right.mpr hf
   exact h t ⟨ht ▸ hc.1.sdiff_of_sup_edge, hc.2⟩
-  
+
 /-- Adding an edge increases the clique number by at most one. -/
 protected theorem CliqueFree.sup_edge (h : G.CliqueFree n) (v w : α) :
     (G ⊔ edge v w).CliqueFree (n + 1) := fun _ hs ↦ (hs.erase_of_sup_edge_of_mem <|
-        (h.mono (Nat.le_succ n)).mem_of_sup_edge_isNClique hs).not_cliqueFree h
-
-lemma IsNClique.exists_not_adj_of_cliqueFree_succ (hc : G.IsNClique n s)
-    (h : G.CliqueFree (n + 1)) (x : α) :  ∃ y, y ∈ s ∧ ¬G.Adj x y := by
-  by_contra! hf
-  exact (hc.insert hf).not_cliqueFree h
+        (h.mono <| Nat.le_succ n).mem_of_sup_edge_isNClique hs).not_cliqueFree h
 
 end CliqueFree
 
