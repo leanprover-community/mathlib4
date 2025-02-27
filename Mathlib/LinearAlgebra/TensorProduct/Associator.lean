2b4e23e14052a88b3c7efb6bdc03ea18377ae034
Kim Morrison <kim@tqft.net>
2025-02-27 15:00:52 +1100

    cleanup imports

--- Mathlib/LinearAlgebra/TensorProduct/Associator.lean
+++ Mathlib/LinearAlgebra/TensorProduct/Associator.lean
@@ -3,11 +3,6 @@ Copyright (c) 2018 Kenny Lau. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kenny Lau, Mario Carneiro
 -/
-import Mathlib.Algebra.Module.Submodule.Bilinear
-import Mathlib.Algebra.Module.Equiv.Basic
-import Mathlib.GroupTheory.Congruence.Hom
-import Mathlib.Tactic.Abel
-import Mathlib.Tactic.SuppressCompilation
 import Mathlib.LinearAlgebra.TensorProduct.Basic
 
 /-!
