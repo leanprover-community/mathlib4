5499ca09c794a811ba64465fad657f4bcde7f3bb
Remy Degenne <remydegenne@gmail.com>
2025-02-27 09:20:37 +0100

    feat: a σ-additive content on a set ring is σ-subadditive

--- Mathlib/MeasureTheory/Measure/AddContent.lean
+++ Mathlib/MeasureTheory/Measure/AddContent.lean
@@ -398,6 +398,68 @@ theorem addContent_iUnion_eq_sum_of_tendsto_zero (hC : IsSetRing C) (m : AddCont
   exact addContent_mono hC.isSetSemiring (hC.accumulate_mem hf n) hUf
     (Set.accumulate_subset_iUnion _)
 
+/-- If an additive content is σ-additive on a set ring, then the content of a monotone sequence of
+sets tends to the content of the union. -/
+theorem tendsto_atTop_addContent_iUnion_of_addContent_iUnion_eq_tsum (hC : IsSetRing C)
+    (m_iUnion : ∀ (f : ℕ → Set α) (_ : ∀ i, f i ∈ C) (_ : (⋃ i, f i) ∈ C)
+      (_hf_disj : Pairwise (Disjoint on f)), m (⋃ i, f i) = ∑' i, m (f i))
+    ⦃f : ℕ → Set α⦄ (hf_mono : Monotone f) (hf : ∀ i, f i ∈ C) (hf_Union : ⋃ i, f i ∈ C) :
+    Tendsto (fun n ↦ m (f n)) atTop (𝓝 (m (⋃ i, f i))) := by
+  let g := disjointed f
+  have hg_Union : (⋃ i, g i) = ⋃ i, f i := iUnion_disjointed
+  simp_rw [← hg_Union,
+    m_iUnion g (hC.disjointed_mem hf) (by rwa [hg_Union]) (disjoint_disjointed f)]
+  have h : ∀ n, m (f n) = ∑ i ∈ range (n + 1), m (g i) := by
+    intro n
+    classical
+    have h1 : f n = ⋃₀ image g (range (n + 1)) := by
+      rw [← Monotone.partialSups_eq hf_mono, ← partialSups_disjointed, ←
+        partialSups_eq_sUnion_image g]
+    rw [h1, addContent_sUnion]
+    · rw [sum_image_of_disjoint addContent_empty ((disjoint_disjointed f).pairwiseDisjoint _)]
+    · intro s
+      rw [mem_coe, Finset.mem_image]
+      rintro ⟨i, _, rfl⟩
+      exact hC.disjointed_mem hf i
+    · intro s hs t ht hst
+      rw [mem_coe, Finset.mem_image] at hs ht
+      obtain ⟨i, _, rfl⟩ := hs
+      obtain ⟨j, _, rfl⟩ := ht
+      have hij : i ≠ j := fun h_eq ↦ hst (h_eq ▸ rfl)
+      exact disjoint_disjointed f hij
+    · rw [← h1]
+      exact hf n
+  simp_rw [h]
+  change Tendsto (fun n ↦ (fun k ↦ ∑ i ∈ range k, m (g i)) (n + 1)) atTop (𝓝 (∑' i, m (g i)))
+  rw [tendsto_add_atTop_iff_nat (f := (fun k ↦ ∑ i ∈ range k, m (g i))) 1]
+  exact ENNReal.tendsto_nat_tsum _
+
+/-- If an additive content is σ-additive on a set ring, then it is σ-subadditive. -/
+theorem addContent_iUnion_le_of_addContent_iUnion_eq_tsum (hC : IsSetRing C)
+    (m_iUnion : ∀ (f : ℕ → Set α) (_ : ∀ i, f i ∈ C) (_ : (⋃ i, f i) ∈ C)
+      (_hf_disj : Pairwise (Function.onFun Disjoint f)), m (⋃ i, f i) = ∑' i, m (f i)) :
+    m.IsSigmaSubadditive := by
+  intro f hf hf_Union
+  have h_tendsto : Tendsto (fun n ↦ m (partialSups f n)) atTop (𝓝 (m (⋃ i, f i))) := by
+    rw [← iSup_eq_iUnion, ← iSup_partialSups_eq]
+    refine tendsto_atTop_addContent_iUnion_of_addContent_iUnion_eq_tsum hC m_iUnion
+      (partialSups_monotone f) (hC.partialSups_mem hf) ?_
+    rwa [← iSup_eq_iUnion, iSup_partialSups_eq]
+  have h_tendsto' : Tendsto (fun n ↦ ∑ i ∈ range (n + 1), m (f i)) atTop (𝓝 (∑' i, m (f i))) := by
+    rw [tendsto_add_atTop_iff_nat (f := (fun k ↦ ∑ i ∈ range k, m (f i))) 1]
+    exact ENNReal.tendsto_nat_tsum _
+  refine le_of_tendsto_of_tendsto' h_tendsto h_tendsto' fun n ↦ ?_
+  classical
+  rw [partialSups_eq_sUnion_image]
+  refine (addContent_le_sum_of_subset_sUnion hC.isSetSemiring
+    (J := (range (n + 1)).image f) (fun s ↦ ?_) ?_ subset_rfl).trans ?_
+  · rw [mem_coe, Finset.mem_image]
+    rintro ⟨i, _, rfl⟩
+    exact hf i
+  · rw [← partialSups_eq_sUnion_image]
+    exact hC.partialSups_mem hf n
+  · exact sum_image_le_of_nonneg fun _ _ ↦ zero_le _
+
 end IsSetRing
 
 end MeasureTheory
