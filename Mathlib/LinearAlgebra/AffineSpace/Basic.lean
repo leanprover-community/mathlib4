93097774c1c161b4db8567f36406dec65a051a08
Kim Morrison <kim@tqft.net>
2025-02-27 15:03:52 +1100

    shake --fix

--- Mathlib/LinearAlgebra/AffineSpace/Basic.lean
+++ Mathlib/LinearAlgebra/AffineSpace/Basic.lean
@@ -3,7 +3,6 @@ Copyright (c) 2020 Joseph Myers. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joseph Myers
 -/
-import Mathlib.Algebra.AddTorsor.Basic
 
 /-!
 # Affine space
