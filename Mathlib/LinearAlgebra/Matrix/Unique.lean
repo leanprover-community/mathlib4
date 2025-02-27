661db0e5e79ae148d6af41ce62d3368edbfd4126
Whysoserioushah <yx3021@ic.ac.uk>
2025-02-27 15:52:11 +0000

    create

---
+++ Mathlib/LinearAlgebra/Matrix/Unique.lean
@@ -0,0 +1,52 @@
+/-
+Copyright (c) 2025 Yunzhou Xie. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Yunzhou Xie
+-/
+import Mathlib.Data.Matrix.Basic
+
+/-!
+This file prooves Matrix of dimension one is equivalent to the base itself under the
+canonical maps that sends every element `a` to `[a]` as dimension one matrix.
+
+# Main results
+- `Matrix.uniqueRingEquiv`
+- `Matrix.uniqueAlgEquiv`
+
+## Tags
+Matrix, Unique, AlgEquiv
+-/
+
+namespace Matrix
+
+variable {ι1 ι2 A R : Type*} [Unique ι1] [Unique ι2]
+
+/-- Set of all dimension one matrix is in bijection with the base set under the
+  canonical maps. -/
+abbrev uniqueEquiv : Matrix ι1 ι2 A ≃ A where
+  toFun M := M default default
+  invFun a := .of fun _ _ => a
+  left_inv M := by ext i j; simp [Subsingleton.elim i default, Subsingleton.elim j default]
+  right_inv a := by simp
+
+/-- AddEquiv version of `uniqueEquiv`. -/
+abbrev uniqueAddEquiv [Add A]: Matrix ι1 ι2 A ≃+ A where
+  __ := uniqueEquiv
+  map_add' := by simp
+
+/-- `M₁(A)` is linearly equivalent to `A` as `R`-modules where `R` is a semiring. -/
+abbrev uniqueLinearEquiv [Semiring R] [AddCommMonoid A] [Module R A] : Matrix ι1 ι2 A ≃ₗ[R] A where
+  __ := uniqueAddEquiv
+  map_smul' := by simp
+
+/-- `M₁(A)` is equivalent to `A` as rings. -/
+abbrev uniqueRingEquiv [NonUnitalNonAssocSemiring A] : Matrix ι1 ι1 A ≃+* A where
+  __ := uniqueAddEquiv
+  map_mul' := by simp [mul_apply]
+
+/-- `M₁(A)` is equivalent to `A` as `R`-algebras. -/
+abbrev uniqueAlgEquiv [Semiring A] [CommSemiring R] [Algebra R A]: Matrix ι1 ι1 A ≃ₐ[R] A where
+  __ := uniqueRingEquiv
+  commutes' r := by aesop
+
+end Matrix
