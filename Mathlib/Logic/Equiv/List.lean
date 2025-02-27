380e6327d7909a8c9920281f1e5e7d7056715df8
Anne C.A. Baanen <vierkantor@vierkantor.com>
2025-02-27 15:45:00 +0100

    Fix import duplication

--- Mathlib/Logic/Equiv/List.lean
+++ Mathlib/Logic/Equiv/List.lean
@@ -3,7 +3,6 @@ Copyright (c) 2018 Mario Carneiro. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro
 -/
-import Mathlib.Data.List.Chain
 import Mathlib.Data.Finset.Sort
 import Mathlib.Data.List.Chain
 import Mathlib.Logic.Denumerable
