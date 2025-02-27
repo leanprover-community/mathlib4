ec379f2903505cbcb00f8abd39b31439c55e1310
Whysoserioushah <109107491+Whysoserioushah@users.noreply.github.com>
2025-02-27 17:22:29 +0000

    Update Mathlib/LinearAlgebra/Matrix/Unique.lean
    Co-authored-by: Kevin Buzzard <k.buzzard@imperial.ac.uk>

--- Mathlib/LinearAlgebra/Matrix/Unique.lean
+++ Mathlib/LinearAlgebra/Matrix/Unique.lean
@@ -8,7 +8,7 @@ import Mathlib.Data.Matrix.Basic
 /-!
 # One by one matrices
 This file proves that one by one matrices over a base are equivalent to the base itself under the
-canonical maps that sends every element `a` to `[a]` as dimension one matrix.
+canonical map that sends a one by one matrix `[a]` to `a`.
 
 ## Main results
 - `Matrix.uniqueRingEquiv`
