cdaf8dac364d73998a206b502ea121a37ac36ea2
Remy Degenne <remydegenne@gmail.com>
2025-02-28 14:38:46 +0100

    author

--- Mathlib/Probability/Kernel/Composition/Comp.lean
+++ Mathlib/Probability/Kernel/Composition/Comp.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2023 Rémy Degenne. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Rémy Degenne
+Authors: Rémy Degenne, Etienne Marion
 -/
 import Mathlib.Probability.Kernel.MeasurableLIntegral
 
