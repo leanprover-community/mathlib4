c6620c83f7ac7c68d66dda9f1d16703d1eea480b
EtienneC30 <baguettes_casino0c@icloud.com>
2025-02-28 09:46:44 +0000

    feat: integral against the composition of kernels (#22363)
    Prove the formula for the integral against the composition of kernels. This is a simplified version of the proof for the composition-product which is in the same file. However we need to do all the proofs again because the results about composition-product require s-finiteness while composition do not.
    
    
    
    Co-authored-by: EtienneC30 <66847262+EtienneC30@users.noreply.github.com>

--- Mathlib/Probability/Kernel/Invariance.lean
+++ Mathlib/Probability/Kernel/Invariance.lean
@@ -36,15 +36,14 @@ namespace Kernel
 
 /-! ### Push-forward of measures along a kernel -/
 
-
-@[simp]
+@[deprecated "Use comp_add in Composition/MeasureComp" (since := "2025-02-28")]
 theorem bind_add (μ ν : Measure α) (κ : Kernel α β) : (μ + ν).bind κ = μ.bind κ + ν.bind κ := by
   ext1 s hs
   rw [Measure.bind_apply hs (Kernel.measurable _), lintegral_add_measure, Measure.coe_add,
     Pi.add_apply, Measure.bind_apply hs (Kernel.measurable _),
     Measure.bind_apply hs (Kernel.measurable _)]
 
-@[simp]
+@[deprecated "Use comp_smul in Composition/MeasureComp" (since := "2025-02-28")]
 theorem bind_smul (κ : Kernel α β) (μ : Measure α) (r : ℝ≥0∞) : (r • μ).bind κ = r • μ.bind κ := by
   ext1 s hs
   rw [Measure.bind_apply hs (Kernel.measurable _), lintegral_smul_measure, Measure.coe_smul,
