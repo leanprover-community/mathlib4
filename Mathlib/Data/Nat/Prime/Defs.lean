af159eb7fecd4fb10238dcf24cee19cc1440f95d
Jannis Limperg <jannis@limperg.de>
2025-02-28 13:08:16 +0100

    Document new lemmas

--- Mathlib/Data/Nat/Prime/Defs.lean
+++ Mathlib/Data/Nat/Prime/Defs.lean
@@ -46,12 +46,14 @@ theorem irreducible_iff_nat_prime (a : ℕ) : Irreducible a ↔ Nat.Prime a :=
 theorem not_prime_zero : ¬ Prime 0
   | h => h.ne_zero rfl
 
+/-- A copy of `not_prime_zero` stated in a way that works for `aesop`. -/
 @[aesop safe destruct] theorem prime_zero_false : Prime 0 → False :=
   not_prime_zero
 
 theorem not_prime_one : ¬ Prime 1
   | h => h.ne_one rfl
 
+/-- A copy of `not_prime_one` stated in a way that works for `aesop`. -/
 @[aesop safe destruct] theorem prime_one_false : Prime 1 → False :=
   not_prime_one
 
