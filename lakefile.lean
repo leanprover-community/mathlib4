d97935917b5034afc08910149685cf4318e3f780
Jannis Limperg <jannis@limperg.de>
2025-02-28 17:31:26 +0100

    Update Aesop
    This version of Aesop precompiles the RPINF computation.

--- lakefile.lean
+++ lakefile.lean
@@ -9,7 +9,7 @@ open Lake DSL
 
 require "leanprover-community" / "batteries" @ git "main"
 require "leanprover-community" / "Qq" @ git "master"
-require "leanprover-community" / "aesop" @ git "master"
+require "leanprover-community" / "aesop" @ git "rpinf-precomp"
 require "leanprover-community" / "proofwidgets" @ git "v0.0.52-pre2" -- ProofWidgets should always be pinned to a specific version
   with NameMap.empty.insert `errorOnBuild
     "ProofWidgets not up-to-date. \
