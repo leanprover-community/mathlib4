275e8267a2837e5266b7ca29c0b74876c9075a5d
Vasilii Nesterov <vas.nesterov63@gmail.com>
2025-02-28 12:45:33 +0000

    feat(Simproc): simproc for `∃ a', ... ∧ a' = a ∧ ...` (#22273)
    Add a simproc that checks whether `P a'` has the form `... ∧ a' = a ∧ ...` for the goal `∃ a', P a'`. If so, it rewrites the latter as `P a`. 
    
    
    
    Co-authored-by: Eric Wieser <efw@google.com>
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -5396,6 +5396,7 @@ import Mathlib.Tactic.Set
 import Mathlib.Tactic.SetLike
 import Mathlib.Tactic.SimpIntro
 import Mathlib.Tactic.SimpRw
+import Mathlib.Tactic.Simproc.ExistsAndEq
 import Mathlib.Tactic.Simps.Basic
 import Mathlib.Tactic.Simps.NotationClass
 import Mathlib.Tactic.SplitIfs
