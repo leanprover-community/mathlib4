4b838aaf1796f68f1c3200ca629c6b2e7a71740a
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2025-03-01 12:55:13 +0100

    chore: tidy various files

--- Mathlib/AlgebraicTopology/SimplicialSet/NerveAdjunction.lean
+++ Mathlib/AlgebraicTopology/SimplicialSet/NerveAdjunction.lean
@@ -25,7 +25,7 @@ exists because nerves of categories are 2-coskeletal.
 
 We also prove that `nerveFunctor` is fully faithful, demonstrating that `nerveAdjunction` is
 reflective. Since the category of simplicial sets is cocomplete, we conclude in
-`CategoryTheory.Category.Cat.Colimit` that the category of categories has colimits.
+`Mathlib.CategoryTheory.Category.Cat.Colimit` that the category of categories has colimits.
 
 -/
 
