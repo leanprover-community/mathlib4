24d09933bc0005a9a1d8e7aaf23a4e49835e8b58
xyzw12345 <wangjt2020@163.com>
2025-03-01 22:15:33 +0800

    Reapply "added CanonAtomM"
    This reverts commit 8d44c01f6d0bbe9c6b5fd5c34d1babc17664e6df.

---
+++ Mathlib/Util/CanonAtomM.lean
@@ -0,0 +1,43 @@
+import Mathlib.Init
+import Lean.Meta.Tactic.Simp.Types
+import Qq
+
+namespace Mathlib.Tactic
+
+open Lean Meta
+
+structure CanonAtomM.Context where
+  /-- The reducibility setting for definitional equality of atoms -/
+  red : TransparencyMode
+  /-- A simplification to apply to atomic expressions when they are encountered,
+  before interning them in the atom list. -/
+  evalAtom : Expr → MetaM Simp.Result := fun e ↦ pure { expr := e }
+  deriving Inhabited
+
+structure CanonAtomM.State where
+  currentIndex : Nat := 0
+  index : Std.HashMap Expr Nat := {}
+
+abbrev CanonAtomM := ReaderT CanonAtomM.Context <| StateRefT CanonAtomM.State CanonM
+
+def CanonAtomM.run {α : Type} (red : TransparencyMode) (m : CanonAtomM α)
+    (evalAtom : Expr → MetaM Simp.Result := fun e ↦ pure { expr := e }) :
+    MetaM α :=
+  ((m { red, evalAtom }).run' {}).run' red
+
+def CanonAtomM.addAtom (e : Expr) : CanonAtomM (Nat × Expr) := do
+  let e' ← canon e
+  if let some es := (← get).index.get? e' then
+    return (es, e')
+  else
+    modifyGet fun c ↦ ((c.currentIndex, e'),
+      {currentIndex := c.currentIndex + 1, index := (c.index.insert e' c.currentIndex)})
+
+open Qq
+
+def CanonAtomM.addAtomQ {u : Level} {α : Q(Type u)} (e : Q($α)) :
+    CanonAtomM (Nat × {e' : Q($α) // $e =Q $e'}) := do
+  let (n, e') ← CanonAtomM.addAtom e
+  return (n, ⟨e', ⟨⟩⟩)
+
+end Mathlib.Tactic
