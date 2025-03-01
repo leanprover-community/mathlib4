0a9cdeaccc4470873e086d0e9a16b263203372d9
xyzw12345 <wangjt2020@163.com>
2025-03-01 22:32:05 +0800

    update comment

--- Mathlib/Util/CanonAtomM.lean
+++ Mathlib/Util/CanonAtomM.lean
@@ -17,6 +17,7 @@ namespace Mathlib.Tactic
 
 open Lean Meta
 
+/-- The context (read-only state) of the `CanonAtomM` monad. -/
 structure CanonAtomM.Context where
   /-- The reducibility setting for definitional equality of atoms -/
   red : TransparencyMode
@@ -25,17 +26,26 @@ structure CanonAtomM.Context where
   evalAtom : Expr → MetaM Simp.Result := fun e ↦ pure { expr := e }
   deriving Inhabited
 
+/-- The mutable state of the `AtomM` monad. -/
 structure CanonAtomM.State where
+  /-- The index for the new atom -/
   currentIndex : Nat := 0
+  /-- A hashmap to keep track of the indices of canonicalized `Expr`s -/
   index : Std.HashMap Expr Nat := {}
 
+/-- The monad used for collecting atoms. -/
 abbrev CanonAtomM := ReaderT CanonAtomM.Context <| StateRefT CanonAtomM.State CanonM
 
+/-- Run a computation in the `CanonAtomM` monad. -/
 def CanonAtomM.run {α : Type} (red : TransparencyMode) (m : CanonAtomM α)
     (evalAtom : Expr → MetaM Simp.Result := fun e ↦ pure { expr := e }) :
     MetaM α :=
   ((m { red, evalAtom }).run' {}).run' red
 
+/-- If an atomic expression has already been encountered, get the index and the stored form of the
+atom (which will be defeq at the specified transparency, but not necessarily syntactically equal).
+If the atomic expression has *not* already been encountered, store it in the list of atoms, and
+return the new index (and the stored form of the atom, which will be the canonicalized version). -/
 def CanonAtomM.addAtom (e : Expr) : CanonAtomM (Nat × Expr) := do
   let e' ← canon e
   if let some es := (← get).index.get? e' then
@@ -46,6 +56,7 @@ def CanonAtomM.addAtom (e : Expr) : CanonAtomM (Nat × Expr) := do
 
 open Qq
 
+/-- A strongly typed version of `CanonAtomM.addAtom` using `Qq`. -/
 def CanonAtomM.addAtomQ {u : Level} {α : Q(Type u)} (e : Q($α)) :
     CanonAtomM (Nat × {e' : Q($α) // $e =Q $e'}) := do
   let (n, e') ← CanonAtomM.addAtom e
