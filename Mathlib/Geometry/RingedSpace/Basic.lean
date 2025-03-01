969fa0cf00dba1feae69cecf00a115926070e9bc
Kim Morrison <kim@tqft.net>
2025-03-01 12:12:02 +1100

    nolint

--- Mathlib/Geometry/RingedSpace/Basic.lean
+++ Mathlib/Geometry/RingedSpace/Basic.lean
@@ -35,6 +35,7 @@ open TopCat.Presheaf
 namespace AlgebraicGeometry
 
 /-- The type of Ringed spaces, as an abbreviation for `SheafedSpace CommRingCat`. -/
+@[nolint checkUnivs] -- The universes appear together in the type, but separately in the value.
 abbrev RingedSpace : Type max (u+1) (v+1) :=
   SheafedSpace.{v+1, v, u} CommRingCat.{v}
 
