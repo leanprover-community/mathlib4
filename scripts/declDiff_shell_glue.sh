cb680cdf7699f587f3e881c43e82353436c8dcf4
adomani <adomani@gmail.com>
2025-03-02 19:25:07 +0100

    feat(CI): declarations diff in Lean

---
+++ scripts/declDiff_shell_glue.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash env
+
+script_file=scripts/declarations_diff.lean
+
+echo '#all_declarations' >> "${script_file}"
+cat "${script_file}"
+
+getDeclarations () {
+  # download the cache
+  lake exe cache get Archive.lean Counterexamples.lean Mathlib.lean
+  lake build "${script_file}"
+}
+
+printf 'Save the declarations in the PR branch\n'
+
+getDeclarations > decls_in_PR.txt
+git checkout master...HEAD
+
+printf 'Save the declarations in the master branch\n'
+getDeclarations > decls_in_master.txt
+
+printf 'Diff the declarations\n'
+# diff the declarations
+diff decls_in_PR.txt decls_in_master.txt
---
+++ scripts/declarations_diff.lean
@@ -0,0 +1,22 @@
+import Lean
+import Batteries -- technically not needed, since `Mathlib` already imports it
+import Mathlib
+--import Archive
+--import Counterexamples
+import Lean.Elab.Command
+
+open Lean
+
+/-- Prints each declaration in the environment that is not an internal detail. -/
+elab "#all_declarations" : command => do
+  let sorted : Array String := (← getEnv).constants.map₁.fold (init := ∅) fun tot nm _ =>
+    if nm.isInternalDetail then
+      tot
+    else
+      tot.binInsert (· < ·) nm.toString
+  for n in sorted do
+    dbg_trace n
+
+-- CI removes the initial `--` from the following line:
+-- if you change it, make sure to update the CI configuration.
+--#all_declarations
