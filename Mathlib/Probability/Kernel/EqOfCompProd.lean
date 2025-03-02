9fcdf096eaeaefc831f4124f525257b697dc4947
Remy Degenne <remydegenne@gmail.com>
2025-03-02 21:26:57 +0100

    put things together

--- Mathlib/Probability/Kernel/EqOfCompProd.lean
+++ Mathlib/Probability/Kernel/EqOfCompProd.lean
@@ -3,11 +3,12 @@ Copyright (c) 2025 Rémy Degenne. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Rémy Degenne
 -/
-import Mathlib.Probability.Kernel.Composition.MeasureCompProd
-import Mathlib.Probability.Kernel.WithDensity
+import Mathlib.Probability.Kernel.Composition.AbsolutelyContinuous
 
 /-!
-# Results about the interaction of `Kernel.withDensity` and the composition and product of kernels
+# TODO
+
+`μ ⊗ₘ κ = μ ⊗ₘ η ↔ κ =ᵐ[μ] η`
 
 ## Main statements
 
@@ -15,7 +16,7 @@ import Mathlib.Probability.Kernel.WithDensity
 
 -/
 
-open ProbabilityTheory
+open ProbabilityTheory MeasureTheory
 
 open scoped ENNReal
 
@@ -71,3 +72,35 @@ lemma compProd_withDensity [SFinite μ] [IsSFiniteKernel κ] [IsFiniteKernel (κ
   exact compProd_withDensity_of_isFiniteMeasure hf
 
 end MeasureTheory.Measure
+
+namespace ProbabilityTheory.Kernel
+
+variable {α β : Type*} {mα : MeasurableSpace α} {mβ : MeasurableSpace β}
+  {μ : Measure α} {κ η : Kernel α β}
+  [MeasurableSpace.CountableOrCountablyGenerated α β]
+
+lemma ae_eq_of_compProd_eq [IsFiniteMeasure μ] [IsFiniteKernel κ] [IsFiniteKernel η]
+    (h : μ ⊗ₘ κ = μ ⊗ₘ η) :
+    κ =ᵐ[μ] η := by
+  have h_ac : ∀ᵐ a ∂μ, κ a ≪ η a := (Measure.absolutelyContinuous_of_eq h).kernel_of_compProd
+  have hκ_eq : ∀ᵐ a ∂μ, κ a = η.withDensity (κ.rnDeriv η) a := by
+    filter_upwards [h_ac] with a ha using (Kernel.withDensity_rnDeriv_eq ha).symm
+  suffices ∀ᵐ a ∂μ, ∀ᵐ b ∂(η a), κ.rnDeriv η a b = 1 by
+    filter_upwards [h_ac, this] with a h_ac h using (rnDeriv_eq_one_iff_eq h_ac).mp h
+  refine Measure.ae_ae_of_ae_compProd (p := fun x ↦ κ.rnDeriv η x.1 x.2 = 1) ?_
+  refine ae_eq_of_forall_setLIntegral_eq_of_sigmaFinite (by fun_prop) (by fun_prop) fun s hs _ ↦ ?_
+  simp only [MeasureTheory.lintegral_const, MeasurableSet.univ, Measure.restrict_apply,
+    Set.univ_inter, one_mul]
+  calc ∫⁻ x in s, κ.rnDeriv η x.1 x.2 ∂μ ⊗ₘ η
+  _ = (μ ⊗ₘ κ) s := by
+    rw [Measure.compProd_congr hκ_eq, Measure.compProd_withDensity, withDensity_apply _ hs]
+    fun_prop
+  _ = (μ ⊗ₘ η) s := by rw [h]
+
+/-- Two finite kernels `κ` and `η` are `μ`-a.e. equal iff the composition-products `μ ⊗ₘ κ`
+and `μ ⊗ₘ η` are equal. -/
+lemma ae_eq_iff_compProd_eq [IsFiniteMeasure μ] [IsFiniteKernel κ] [IsFiniteKernel η] :
+    κ =ᵐ[μ] η ↔ μ ⊗ₘ κ = μ ⊗ₘ η :=
+  ⟨Measure.compProd_congr, Kernel.ae_eq_of_compProd_eq⟩
+
+end ProbabilityTheory.Kernel
