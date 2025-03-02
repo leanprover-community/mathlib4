1ac65eaa1f3b68387f2d60a8840e91ef8d775da1
xyzw12345 <wangjt2020@163.com>
2025-03-02 13:30:25 +0800

    lake exe mk_all

--- Mathlib.lean
+++ Mathlib.lean
@@ -5860,6 +5860,7 @@ import Mathlib.Util.AssertExists
 import Mathlib.Util.AssertExistsExt
 import Mathlib.Util.AssertNoSorry
 import Mathlib.Util.AtomM
+import Mathlib.Util.CanonAtomM
 import Mathlib.Util.CompileInductive
 import Mathlib.Util.CountHeartbeats
 import Mathlib.Util.Delaborators
