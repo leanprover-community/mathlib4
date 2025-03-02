a103a5fc6c65fd1ac4a67309edc8b12880f92ba5
Mehmet Emre <memre@usfca.edu>
2025-03-01 23:58:43 -0800

    Add set operation notation to languages

--- Mathlib/Computability/Language.lean
+++ Mathlib/Computability/Language.lean
@@ -58,6 +58,14 @@ instance : Inhabited (Language α) := ⟨(∅ : Set _)⟩
 instance : Add (Language α) :=
   ⟨((· ∪ ·) : Set (List α) → Set (List α) → Set (List α))⟩
 
+/-- Language union is just set union. -/
+instance : Union (Language α) :=
+  ⟨((· ∪ ·) : Set (List α) → Set (List α) → Set (List α))⟩
+
+/-- Language intersection is just set intersection. -/
+instance : Inter (Language α) :=
+  ⟨((· ∩ ·) : Set (List α) → Set (List α) → Set (List α))⟩
+
 /-- The product of two languages `l` and `m` is the language made of the strings `x ++ y` where
 `x ∈ l` and `y ∈ m`. -/
 instance : Mul (Language α) :=
