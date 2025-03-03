9e6cc0dbe6a21a7d993093ac154b6e68967d3737
Bernhard Reinke <b-reinke@users.noreply.github.com>
2025-03-03 18:29:24 +0100

    feat(Data/List/Chain): chain'_iff_all_append_cons_cons

--- Mathlib/Data/List/Chain.lean
+++ Mathlib/Data/List/Chain.lean
@@ -194,6 +194,26 @@ theorem chain'_append_cons_cons {b c : α} {l₁ l₂ : List α} :
     Chain' R (l₁ ++ b :: c :: l₂) ↔ Chain' R (l₁ ++ [b]) ∧ R b c ∧ Chain' R (c :: l₂) := by
   rw [chain'_split, chain'_cons]
 
+theorem chain'_iff_all_append_cons_cons {l : List α} : Chain' R l ↔
+∀ {a b} {l₁ l₂}, l = l₁ ++ (a :: b :: l₂) → R a b := by
+  constructor
+  · exact fun h _ _ _ _ eq => (chain'_append_cons_cons.mp (eq ▸ h)).2.1
+  · induction l with
+  | nil => exact fun _ ↦ chain'_nil
+  | cons head tail ih =>
+    intro h
+    cases tail with
+    | nil => exact chain'_singleton head
+    | cons head' tail =>
+      rw [chain'_cons]
+      constructor
+      · apply h (nil_append _).symm
+      · apply ih
+        intro a b l₁ l₂ eq
+        apply h (l₁ := head :: l₁) (l₂ := l₂)
+        rw [eq]
+        simp
+
 theorem chain'_map (f : β → α) {l : List β} :
     Chain' R (map f l) ↔ Chain' (fun a b : β => R (f a) (f b)) l := by
   cases l <;> [rfl; exact chain_map _]
