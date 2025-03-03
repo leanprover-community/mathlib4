95c78f355f66ad3bdc174500d7b9824dd73cf14b
Kim Morrison <kim@tqft.net>
2025-03-03 07:17:45 +0000

    feat: split long file Algebra.BigOperators.Group.Finset.Basic (#22476)

--- Mathlib.lean
+++ Mathlib.lean
@@ -51,6 +51,7 @@ import Mathlib.Algebra.BigOperators.Finsupp
 import Mathlib.Algebra.BigOperators.Group.Finset.Basic
 import Mathlib.Algebra.BigOperators.Group.Finset.Defs
 import Mathlib.Algebra.BigOperators.Group.Finset.Pi
+import Mathlib.Algebra.BigOperators.Group.Finset.Powerset
 import Mathlib.Algebra.BigOperators.Group.Finset.Preimage
 import Mathlib.Algebra.BigOperators.Group.Finset.Sigma
 import Mathlib.Algebra.BigOperators.Group.List.Basic
