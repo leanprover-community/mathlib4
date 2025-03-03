563e1b98ccf5245055c63d07f373f5459d86b2cc
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-03-03 18:01:25 +0100

    feat: existence of right derived functors

--- Mathlib/CategoryTheory/Localization/DerivabilityStructure/PointwiseRightDerived.lean
+++ Mathlib/CategoryTheory/Localization/DerivabilityStructure/PointwiseRightDerived.lean
@@ -10,7 +10,19 @@ import Mathlib.CategoryTheory.Limits.Final
 /-!
 # Existence of pointwise right derived functors via derivability structures
 
+In this file, we show how right derivability structure can be used in
+order to construct (pointwise) right derived functors.
+Let `Φ` be a right derivability structure from `W₁ : MorphismProperty C₁`
+to `W₂ : MorphismProperty C₂`. Let `F : C₂ ⥤ H` be a functor.
+Then, the lemma `hasPointwiseRightDerivedFunctor_iff_of_isRightDerivabilityStructure`
+says that `F` has a pointwise right derived functor with respect to `W₂`
+if and only if `Φ.functor ⋙ F` has a pointwise right derived functor
+with respect to `W₁`. This is essentially the Proposition 5.5 from the article
+*Structures de dérivabilité* by Bruno Kahn and Georges Maltsiniotis (there,
+it was stated in terms of absolute derived functors).
 
+In particular, if `Φ.functor ⋙ F` inverts `W₁`, it follows that the
+right derived functor of `F` with respect to `W₂` exists.
 
 ## References
 * [Bruno Kahn and Georges Maltsiniotis, *Structures de dérivabilité*][KahnMaltsiniotis2008]
@@ -45,12 +57,20 @@ variable (Φ : LocalizerMorphism W₁ W₂) (L₁ : C₁ ⥤ D₁) (L₂ : C₂ 
   (F₂ : D₂ ⥤ H) (α₂ : F ⟶ L₂ ⋙ F₂)
   [F₁.IsRightDerivedFunctor α₁ W₁]
 
+/-- If `Φ` is a localizer morphism from `W₁ : MorphismProperty C₁` to
+`W₂ : MorphismProperty C₂`, if `L₁ : C₁ ⥤ D₁` and `L₂ : C₂ ⥤ D₂` are
+localization functors for `W₁` and `W₂`, if `F : C₂ ⥤ H` is a functor,
+if `F₁ : D₁ ⥤ H` is a right derived functor of `Φ.functor ⋙ F`,
+and if `F₂ : D₂ ⥤ H` is a functor equipped with a
+natural transformation `α₂ : F ⟶ L₂ ⋙ F₂`, this is the canonical
+morphism `F₁ ⟶ Φ.localizedFunctor L₁ L₂ ⋙ F₂`. -/
 noncomputable def rightDerivedFunctorComparison :
     F₁ ⟶ Φ.localizedFunctor L₁ L₂ ⋙ F₂ :=
   F₁.rightDerivedDesc α₁ W₁ (Φ.localizedFunctor L₁ L₂ ⋙ F₂)
     (whiskerLeft _ α₂ ≫ (Functor.associator _ _ _).inv ≫
       whiskerRight ((Φ.catCommSq L₁ L₂).iso).hom F₂ ≫ (Functor.associator _ _ _).hom)
 
+@[reassoc]
 lemma rightDerivedFunctorComparison_fac :
     α₁ ≫ whiskerLeft _ (Φ.rightDerivedFunctorComparison L₁ L₂ F F₁ α₁ F₂ α₂) =
       whiskerLeft Φ.functor α₂ ≫ ((Functor.associator _ _ _).inv ≫
@@ -66,7 +86,6 @@ lemma rightDerivedFunctorComparison_fac_app (X : C₁) :
 
 variable [Φ.IsRightDerivabilityStructure]
 
--- similar to Kahn-Maltsiniotis Lemme 5.5
 lemma hasPointwiseRightDerivedFunctorAt_iff_of_isRightDerivabilityStructure (X : C₁) :
     (Φ.functor ⋙ F).HasPointwiseRightDerivedFunctorAt W₁ X ↔
       F.HasPointwiseRightDerivedFunctorAt W₂ (Φ.functor.obj X) := by
@@ -92,9 +111,12 @@ lemma hasPointwiseRightDerivedFunctor_iff_of_isRightDerivabilityStructure :
     simpa only [hasPointwiseRightDerivedFunctorAt_iff_of_isRightDerivabilityStructure,
       ← F.hasPointwiseRightDerivedFunctorAt_iff_of_mem W₂ R.w R.hw ] using hF R.X₁
 
-variable [F₂.IsRightDerivedFunctor α₂ W₂]
+section
 
-instance [(Φ.functor ⋙ F).HasPointwiseRightDerivedFunctor W₁] :
+variable [(Φ.functor ⋙ F).HasPointwiseRightDerivedFunctor W₁]
+  [F₂.IsRightDerivedFunctor α₂ W₂]
+
+instance  :
     IsIso (Φ.rightDerivedFunctorComparison L₁ L₂ F F₁ α₁ F₂ α₂) := by
   suffices ∀ Y, IsIso ((rightDerivedFunctorComparison Φ L₁ L₂ F F₁ α₁ F₂ α₂).app Y) from
     NatIso.isIso_of_isIso_app _
@@ -117,21 +139,14 @@ instance [(Φ.functor ⋙ F).HasPointwiseRightDerivedFunctor W₁] :
   rw [this]
   infer_instance
 
-lemma isIso_α_iff_of_isRightDerivabilityStructure
-    [(Φ.functor ⋙ F).HasPointwiseRightDerivedFunctor W₁] (X : C₁) :
+lemma isIso_α_iff_of_isRightDerivabilityStructure (X : C₁) :
     IsIso (α₁.app X) ↔ IsIso (α₂.app (Φ.functor.obj X)) := by
   rw [← isIso_comp_right_iff (α₁.app X)
     ((Φ.rightDerivedFunctorComparison L₁ L₂ F F₁ α₁ F₂ α₂).app (L₁.obj X)),
     rightDerivedFunctorComparison_fac_app, isIso_comp_right_iff]
-/-
-#check hasPointwiseRightDerivedFunctorAt_iff_of_isRightDerivabilityStructure
-CategoryTheory.LocalizerMorphism.hasPointwiseRightDerivedFunctorAt_iff_of_isRightDerivabilityStructure.{v₁, v₂, v₃, u₁,
-    u₂, u₃}
-  {C₁ : Type u₁} {C₂ : Type u₂} {H : Type u₃} [Category.{v₁, u₁} C₁] [Category.{v₂, u₂} C₂] [Category.{v₃, u₃} H]
-  {W₁ : MorphismProperty C₁} {W₂ : MorphismProperty C₂} (Φ : LocalizerMorphism W₁ W₂) (F : C₂ ⥤ H)
-  [Φ.IsRightDerivabilityStructure] (X : C₁) :
-  (Φ.functor ⋙ F).HasPointwiseRightDerivedFunctorAt W₁ X ↔ F.HasPointwiseRightDerivedFunctorAt W₂ (Φ.functor.obj X)
-  -/
+
+end
+
 end LocalizerMorphism
 
 end CategoryTheory
