e66eda01a9b7b4e259a17aaf9c3cdd552fa4dd75
adomani <adomani@gmail.com>
2025-03-03 20:26:04 +0000

    use for loop

--- scripts/declarations_diff.lean
+++ scripts/declarations_diff.lean
@@ -1,5 +1,5 @@
 import Lean
-import Batteries -- technically not needed, since `Mathlib` already imports it
+--import Batteries -- technically not needed, since `Mathlib` already imports it
 --import Mathlib
 --import Archive
 --import Counterexamples
@@ -13,11 +13,9 @@ Prints each declaration in the environment that is not an internal detail.
 CI uses the name of this command: if you change it, make sure to update the CI configuration.
 -/
 elab "#all_declarations " branch:str : command => do
-  let sorted : Array String := (← getEnv).constants.map₁.fold (init := ∅) fun tot nm _ =>
-    if nm.isInternalDetail then
-      tot
-    else
-      tot.binInsert (· < ·) nm.toString
-  --for n in sorted do
+  let mut sorted : Array String := ∅
+  for (nm, _) in (← getEnv).constants do
+    if !nm.isInternalDetail then
+      sorted := sorted.binInsert (· < ·) nm.toString
   dbg_trace "* Writing {sorted.size} declarations to '{branch.getString}'"
   IO.FS.writeFile branch.getString <| .intercalate "\n" sorted.toList
