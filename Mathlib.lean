97244cb33c137215a22a4c7b1c802bc707b543d5
Yaël Dillies <yael.dillies@gmail.com>
2025-03-03 18:04:27 +0000

    chore: move `SMulWithZero` with `SMulZeroClass`

--- Mathlib.lean
+++ Mathlib.lean
@@ -1014,7 +1014,6 @@ import Mathlib.Algebra.Ring.ULift
 import Mathlib.Algebra.Ring.Units
 import Mathlib.Algebra.Ring.WithZero
 import Mathlib.Algebra.RingQuot
-import Mathlib.Algebra.SMulWithZero
 import Mathlib.Algebra.SkewMonoidAlgebra.Basic
 import Mathlib.Algebra.Small.Group
 import Mathlib.Algebra.Small.Module
