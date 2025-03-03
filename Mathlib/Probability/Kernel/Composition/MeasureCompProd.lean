c515f5290db0f4ca65d605513112fc631fbfa3b8
Rémy Degenne <remydegenne@gmail.com>
2025-03-03 10:07:13 +0000

    feat(Probability): add compProd_sum_(left/right) (#22138)

--- Mathlib/Probability/Kernel/Composition/MeasureCompProd.lean
+++ Mathlib/Probability/Kernel/Composition/MeasureCompProd.lean
@@ -41,11 +41,13 @@ def compProd (μ : Measure α) (κ : Kernel α β) : Measure (α × β) :=
 @[inherit_doc]
 scoped[ProbabilityTheory] infixl:100 " ⊗ₘ " => MeasureTheory.Measure.compProd
 
+@[simp]
 lemma compProd_of_not_sfinite (μ : Measure α) (κ : Kernel α β) (h : ¬ SFinite μ) :
     μ ⊗ₘ κ = 0 := by
   rw [compProd, Kernel.compProd_of_not_isSFiniteKernel_left, Kernel.zero_apply]
   rwa [Kernel.isSFiniteKernel_const]
 
+@[simp]
 lemma compProd_of_not_isSFiniteKernel (μ : Measure α) (κ : Kernel α β) (h : ¬ IsSFiniteKernel κ) :
     μ ⊗ₘ κ = 0 := by
   rw [compProd, Kernel.compProd_of_not_isSFiniteKernel_right, Kernel.zero_apply]
@@ -132,14 +134,25 @@ lemma compProd_add_left (μ ν : Measure α) [SFinite μ] [SFinite ν] (κ : Ker
     (μ + ν) ⊗ₘ κ = μ ⊗ₘ κ + ν ⊗ₘ κ := by
   by_cases hκ : IsSFiniteKernel κ
   · simp_rw [Measure.compProd, Kernel.const_add, Kernel.compProd_add_left, Kernel.add_apply]
-  · simp [compProd_of_not_isSFiniteKernel _ _ hκ]
+  · simp [hκ]
 
 lemma compProd_add_right (μ : Measure α) (κ η : Kernel α β)
     [IsSFiniteKernel κ] [IsSFiniteKernel η] :
     μ ⊗ₘ (κ + η) = μ ⊗ₘ κ + μ ⊗ₘ η := by
   by_cases hμ : SFinite μ
   · simp_rw [Measure.compProd, Kernel.prodMkLeft_add, Kernel.compProd_add_right, Kernel.add_apply]
-  · simp [compProd_of_not_sfinite _ _ hμ]
+  · simp [hμ]
+
+lemma compProd_sum_left {ι : Type*} [Countable ι] {μ : ι → Measure α} [∀ i, SFinite (μ i)] :
+    (sum μ) ⊗ₘ κ = sum (fun i ↦ (μ i) ⊗ₘ κ) := by
+  rw [compProd, ← Kernel.sum_const, Kernel.compProd_sum_left]
+  rfl
+
+lemma compProd_sum_right {ι : Type*} [Countable ι] {κ : ι → Kernel α β}
+    [h : ∀ i, IsSFiniteKernel (κ i)] :
+    μ ⊗ₘ (Kernel.sum κ) = sum (fun i ↦ μ ⊗ₘ (κ i)) := by
+  rw [compProd, ← Kernel.sum_prodMkLeft, Kernel.compProd_sum_right]
+  rfl
 
 @[simp]
 lemma fst_compProd (μ : Measure α) [SFinite μ] (κ : Kernel α β) [IsMarkovKernel κ] :
