69aafdb4c7d2e7af0633120a09b0895cdf7dadc9
adomani <adomani@gmail.com>
2025-03-03 15:23:32 +0000

    some docs

--- scripts/declDiff_shell_glue.sh
+++ scripts/declDiff_shell_glue.sh
@@ -5,15 +5,21 @@ script_file=scripts/declarations_diff.lean
 git checkout master
 git checkout -
 
+# Running `getDeclarations <file_name>`
+# 1. downloads the cache for `Mathlib`, `Archive` and `Counterexamples`;
+# 2. adds the line `#all_declarations <file_name>` to the declarations diff Lean script;
+# 3. builds the declarations diff script, which in turn
+# 4. saves to `<file_name>` all the declarations in the environment;
+# 5. removes the line added to the declarations diff Lean script.
 getDeclarations () {
   printf '* Download the cache\n'
   lake exe cache get Archive.lean Counterexamples.lean Mathlib.lean &&
 
   printf $'* Save the declarations to \'%s\'\n' "${1}"
   printf '#all_declarations "%s"\n' "${1}" >> "${script_file}"
-  #cat "${script_file}"
 
   lake env lean "${script_file}"
+
   # undo the local changes
   git restore .
 }
