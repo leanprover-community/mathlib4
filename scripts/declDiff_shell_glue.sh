fbd7933a48ce5d8ff9def324a7bf48a6da63697d
adomani <adomani@gmail.com>
2025-03-03 14:16:39 +0000

    format as `diff`

--- scripts/declDiff_shell_glue.sh
+++ scripts/declDiff_shell_glue.sh
@@ -9,7 +9,7 @@ getDeclarations () {
   # download the cache
   lake exe cache get Archive.lean Counterexamples.lean Mathlib.lean &&
   printf '#all_declarations "%s"\n' "${1}" >> "${script_file}"
-  cat "${script_file}"
+  #cat "${script_file}"
 
   lake env lean "${script_file}"
   # undo the local changes
@@ -27,5 +27,5 @@ printf 'Save the declarations in the master branch\n'
 getDeclarations decls_in_master.txt
 
 printf 'Diff the declarations\n'
-# diff the declarations
-diff decls_in_PR.txt decls_in_master.txt
+
+printf '```diff\n%s\n```\n' "$(diff decls_in_PR.txt decls_in_master.txt)"
