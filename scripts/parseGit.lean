ef67f3b1de589397f0b7a28c8a5923cd6db720f6
adomani <adomani@gmail.com>
2025-03-04 23:48:51 +0000

    gitDiffMaster

--- scripts/parseGit.lean
+++ scripts/parseGit.lean
@@ -184,3 +184,9 @@ run_cmd
   let (totA, totB) := diffToGitDiff diffString
   logInfo m!"{totA}"
   logInfo m!"{totB}"
+
+/-- Get the `GitDiff`s with respect to `origin/master...HEAD`. -/
+def gitDiffMaster : IO (Array GitDiff × Array GitDiff) := do
+  let diffString ← IO.Process.run
+    {cmd := "git", args := #["diff", "--unified=0", "origin/master...HEAD"]}
+  pure <| diffToGitDiff diffString
