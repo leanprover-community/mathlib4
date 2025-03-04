05f4abf0f63a50496f5be4ec8b738c4fee05affd
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-03-04 14:12:22 +0100

    mk_all

--- Mathlib.lean
+++ Mathlib.lean
@@ -5733,6 +5733,7 @@ import Mathlib.Topology.Instances.AddCircle
 import Mathlib.Topology.Instances.CantorSet
 import Mathlib.Topology.Instances.Complex
 import Mathlib.Topology.Instances.Discrete
+import Mathlib.Topology.Instances.ENNReal.Basic
 import Mathlib.Topology.Instances.ENNReal.Defs
 import Mathlib.Topology.Instances.ENNReal.Lemmas
 import Mathlib.Topology.Instances.ENat
