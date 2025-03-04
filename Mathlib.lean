a1a22d7b9e45ffd45c5c97b8fde0eaa2e33a0eb3
ADedecker <anatolededecker@gmail.com>
2025-03-04 17:55:26 +0100

    import all

--- Mathlib.lean
+++ Mathlib.lean
@@ -5519,6 +5519,7 @@ import Mathlib.Topology.Algebra.PontryaginDual
 import Mathlib.Topology.Algebra.ProperAction.Basic
 import Mathlib.Topology.Algebra.ProperAction.ProperlyDiscontinuous
 import Mathlib.Topology.Algebra.ProperConstSMul
+import Mathlib.Topology.Algebra.RestrictedProduct
 import Mathlib.Topology.Algebra.Ring.Basic
 import Mathlib.Topology.Algebra.Ring.Ideal
 import Mathlib.Topology.Algebra.Semigroup
