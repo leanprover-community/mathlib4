7f6fb52e067a8951da22eb277ae832e21d7d50ae
Parcly Taxel <reddeloostw@gmail.com>
2025-03-04 08:02:18 +0800

    compress

--- Mathlib/Tactic/Linter/CasesLinter.lean
+++ Mathlib/Tactic/Linter/CasesLinter.lean
@@ -4,8 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jeremy Tan, Damiano Testa
 -/
 import Lean.Elab.Command
--- Import this linter explicitly to ensure that
--- this file has a valid copyright header and module docstring.
 import Mathlib.Tactic.Linter.Header
 
 /-!
