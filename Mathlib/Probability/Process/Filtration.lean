cc75ba08202cd367784cf2f0e92ca480ae719d49
Etienne Marion <baguettes_casino0c@icloud.com>
2025-03-04 14:03:35 +0100

    fix

--- Mathlib/Probability/Process/Filtration.lean
+++ Mathlib/Probability/Process/Filtration.lean
@@ -388,10 +388,11 @@ def piFinset : @Filtration (Π i, X i) (Finset ι) _ pi where
 lemma piFinset_eq_comap_restrict (s : Finset ι) :
     piFinset (X := X) s = pi.comap (s.toSet.restrict) := by
   apply le_antisymm
-  · simp_rw [piFinset, ← piCongrLeft_comp_frestrictLe, ← MeasurableEquiv.coe_piCongrLeft, ← comap_comp]
-    exact MeasurableSpace.comap_mono <| Measurable.comap_le (by fun_prop)
-  · rw [← piCongrLeft_comp_restrictLe, ← MeasurableEquiv.coe_piCongrLeft, ← comap_comp]
-    exact MeasurableSpace.comap_mono <| Measurable.comap_le (by fun_prop)
+  · simp_rw [piFinset, ← Set.piCongrLeft_comp_restrict, ← MeasurableEquiv.coe_piCongrLeft,
+      ← comap_comp]
+    exact MeasurableSpace.comap_mono <| (MeasurableEquiv.measurable _).comap_le
+  · rw [← piCongrLeft_comp_restrict, ← MeasurableEquiv.coe_piCongrLeft, ← comap_comp]
+    exact MeasurableSpace.comap_mono <| (MeasurableEquiv.measurable _).comap_le
 
 end piFinset
 
