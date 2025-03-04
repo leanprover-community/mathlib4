3d55d0edbc1cf5679c41a16e8d134792a2401aa7
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-03-04 15:37:17 +0100

    wip

--- Mathlib.lean
+++ Mathlib.lean
@@ -132,6 +132,7 @@ import Mathlib.Algebra.Category.ModuleCat.Free
 import Mathlib.Algebra.Category.ModuleCat.Images
 import Mathlib.Algebra.Category.ModuleCat.Injective
 import Mathlib.Algebra.Category.ModuleCat.Kernels
+import Mathlib.Algebra.Category.ModuleCat.LeftResolutions
 import Mathlib.Algebra.Category.ModuleCat.Limits
 import Mathlib.Algebra.Category.ModuleCat.Monoidal.Basic
 import Mathlib.Algebra.Category.ModuleCat.Monoidal.Closed
---
+++ Mathlib/Algebra/Category/ModuleCat/LeftResolutions.lean
@@ -0,0 +1,43 @@
+/-
+Copyright (c) 2025 Joël Riou. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Joël Riou
+-/
+import Mathlib.Algebra.Category.ModuleCat.Adjunctions
+import Mathlib.Algebra.Category.ModuleCat.Free
+import Mathlib.Algebra.Homology.LeftResolutions.Basic
+
+/-!
+# Functorial projective resolutions of modules
+
+The fact that a `R`-module `M` can be functorially written as a quotient of a
+projective `R`-module is expressed as the definition `ModuleCat.projectiveResolutions`.
+Using the construction in the file `Algebra.Homology.LeftResolutions.Basic`,
+we may obtain a functor `(projectiveResolutions R).chainComplexFunctor` which
+sends `M : ModuleCat R` to a projective resolution.
+
+-/
+
+universe u
+
+variable (R : Type u) [Ring R]
+
+namespace ModuleCat
+
+open CategoryTheory Abelian
+
+instance (X : Type u) : Projective ((free R).obj X) where
+  factors {M N} f p hp := by
+    rw [epi_iff_surjective] at hp
+    obtain ⟨s, hs⟩ := hp.hasRightInverse
+    exact ⟨freeDesc (fun x ↦ s (f (freeMk x))), by aesop⟩
+
+/-- A `R`-module `M` can be functorially written as a quotient of a
+projective `R`-module. -/
+noncomputable def projectiveResolutions :
+    LeftResolutions
+      (fullSubcategoryInclusion (fun (M : ModuleCat.{u} R) ↦ Projective M)) where
+  F := FullSubcategory.lift _ (forget _ ⋙ free R) (by dsimp; infer_instance)
+  π := (adj R).counit
+
+end ModuleCat
