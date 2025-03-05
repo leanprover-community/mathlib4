974ed61b4692761d559008825ea55cdf31f3c6d5
Yakov Pechersky <pechersky@users.noreply.github.com>
2025-03-05 09:18:57 -0500

    shake

--- Mathlib/Topology/UniformSpace/Ultra.lean
+++ Mathlib/Topology/UniformSpace/Ultra.lean
@@ -3,7 +3,7 @@ Copyright (c) 2017 Yakov Pechersky. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yakov Pechersky
 -/
-import Mathlib.Topology.UniformSpace.Basic
+import Mathlib.Topology.UniformSpace.Defs
 import Mathlib.Topology.Bases
 
 /-!
