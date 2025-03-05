78b2823ebdca6ea6339957cda06c0e35cfddae86
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-03-05 14:37:38 +0100

    chore(Analysis/Normed/Group): merge Continuity.lean back into Basic.lean
    Splitting it out in #21554 was motivated by avoiding imports of ENNReal.
    This motivation has become moot now:
    - with #22562, just the basic properties of ENNReal do not add that many
      further imports
    - after #21670, Normed/Group/Basic.lean will import ENNReal anyway.
    
    Adding to this, the split into Basic and Continuity was never really clear
    in terms of imports: Basic.lean should be split further, but this does not seem
    like a good splitting boundary.
    
    While at it, replace one 'open' that has been likely mis-ported by 'open scoped'.

--- Mathlib/Analysis/Normed/Group/Uniform.lean
+++ Mathlib/Analysis/Normed/Group/Uniform.lean
@@ -3,7 +3,7 @@ Copyright (c) 2018 Patrick Massot. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Patrick Massot, Johannes Hölzl, Yaël Dillies
 -/
-import Mathlib.Analysis.Normed.Group.Continuity
+import Mathlib.Analysis.Normed.Group.Basic
 import Mathlib.Topology.Algebra.UniformGroup.Basic
 import Mathlib.Topology.MetricSpace.Algebra
 import Mathlib.Topology.MetricSpace.IsometricSMul
