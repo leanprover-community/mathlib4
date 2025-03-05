f3c0e5c5e20a5f917bd2d6f45ff1aa4e9a5a8ee4
adomani <adomani@gmail.com>
2025-03-05 22:24:15 +0000

    copyrights

--- Mathlib/Tactic/Linter/Local.lean
+++ Mathlib/Tactic/Linter/Local.lean
@@ -1,4 +1,8 @@
-import Lean.Elab.Command
+/-
+Copyright (c) 2025 Damiano Testa. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Damiano Testa
+-/
 import Mathlib.Util.ParseGit
 
 /-!
--- Mathlib/Util/ParseGit.lean
+++ Mathlib/Util/ParseGit.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2025 Damiano Testa. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Damiano Testa
+-/
 import Lean
 
 open Lean
