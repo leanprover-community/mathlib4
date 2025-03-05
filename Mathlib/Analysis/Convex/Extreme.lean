fd71c87894ea5ec99ab6252c247570f2d2c2d89b
Yakov Pechersky <ffxen158@gmail.com>
2025-03-05 13:30:53 -0500

    reset back to master

--- Mathlib/Analysis/Convex/Extreme.lean
+++ Mathlib/Analysis/Convex/Extreme.lean
@@ -41,7 +41,7 @@ Prove lemmas relating extreme sets and points to the intrinsic frontier.
 
 open Function Set Affine
 
-variable {𝕜 E F ι : Type*} {X : ι → Type*}
+variable {𝕜 E F ι : Type*} {π : ι → Type*}
 
 section SMul
 
@@ -152,8 +152,8 @@ end SMul
 
 section OrderedSemiring
 
-variable [OrderedSemiring 𝕜] [AddCommGroup E] [AddCommGroup F] [∀ i, AddCommGroup (X i)]
-  [Module 𝕜 E] [Module 𝕜 F] [∀ i, Module 𝕜 (X i)] {A B : Set E}
+variable [OrderedSemiring 𝕜] [AddCommGroup E] [AddCommGroup F] [∀ i, AddCommGroup (π i)]
+  [Module 𝕜 E] [Module 𝕜 F] [∀ i, Module 𝕜 (π i)] {A B : Set E}
 
 theorem IsExtreme.convex_diff (hA : Convex 𝕜 A) (hAB : IsExtreme 𝕜 A B) : Convex 𝕜 (A \ B) :=
   convex_iff_openSegment_subset.2 fun _ ⟨hx₁A, hx₁B⟩ _ ⟨hx₂A, _⟩ _ hx ↦
@@ -182,7 +182,7 @@ theorem extremePoints_prod (s : Set E) (t : Set F) :
           h.2 hx₁.2 hx₂.2 ⟨a, b, ha, hb, hab, congr_arg Prod.snd hx'⟩⟩
 
 @[simp]
-theorem extremePoints_pi (s : ∀ i, Set (X i)) :
+theorem extremePoints_pi (s : ∀ i, Set (π i)) :
     (univ.pi s).extremePoints 𝕜 = univ.pi fun i ↦ (s i).extremePoints 𝕜 := by
   classical
   ext x
