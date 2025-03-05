c99770c2ace2017bd65ea93ce24163f16842bbbc
Sabbir Rahman <33892003+upobir@users.noreply.github.com>
2025-03-05 21:30:51 +0600

    update style

--- Mathlib/Analysis/SpecialFunctions/Pow/Continuity.lean
+++ Mathlib/Analysis/SpecialFunctions/Pow/Continuity.lean
@@ -227,7 +227,7 @@ theorem continuous_rpow_const {q : ℝ} (h : 0 ≤ q) : Continuous (fun x : ℝ 
   continuous_iff_continuousAt.mpr fun x ↦ continuousAt_rpow_const x q (.inr h)
 
 @[fun_prop]
-lemma continuous_const_rpow {a : ℝ} (h: a ≠ 0) : Continuous fun x : ℝ ↦ a ^ x :=
+lemma continuous_const_rpow {a : ℝ} (h : a ≠ 0) : Continuous (fun x : ℝ ↦ a ^ x) :=
   continuous_iff_continuousAt.mpr fun _ ↦ continuousAt_const_rpow h
 
 end Real
