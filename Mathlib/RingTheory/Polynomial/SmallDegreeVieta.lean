371be1b3b8040416ef6adaa73d327eb50c58d9b0
Eric Wieser <wieser.eric@gmail.com>
2025-03-05 17:53:52 +0000

    move variable

--- Mathlib/RingTheory/Polynomial/SmallDegreeVieta.lean
+++ Mathlib/RingTheory/Polynomial/SmallDegreeVieta.lean
@@ -13,9 +13,11 @@ import Mathlib.RingTheory.Polynomial.Vieta
 
 namespace Polynomial
 
+variable {R T S : Type*}
+
 /-- **Vieta's formula** for quadratic in term of `Polynomial.roots`.
 This is a consequence of `Polynomial.coeff_eq_esymm_roots_of_card`. -/
-lemma quadratic_vieta {R : Type*} [CommRing R] [IsDomain R] {a b c x1 x2 : R}
+lemma quadratic_vieta [CommRing R] [IsDomain R] {a b c x1 x2 : R}
     (hroots : (C a * X ^ 2 + C b * X + C c).roots = {x1, x2}) :
     b = -a * (x1 + x2) ∧ c = a * x1 * x2 := by
   let p : R[X] := C a * X ^ 2 + C b * X + C c
@@ -28,7 +30,7 @@ lemma quadratic_vieta {R : Type*} [CommRing R] [IsDomain R] {a b c x1 x2 : R}
       (coeff_eq_esymm_roots_of_card hp_roots_card (k := 0) (by norm_num [hp_natDegree]))
 
 /-- **Vieta's formula** for quadratic, `aroots` version. -/
-lemma quadratic_vieta_aroots {T S : Type*} [CommRing T] [CommRing S] [IsDomain S] [Algebra T S]
+lemma quadratic_vieta_aroots [CommRing T] [CommRing S] [IsDomain S] [Algebra T S]
     {a b c : T} {x1 x2 : S} (haroots : (C a * X ^ 2 + C b * X + C c).aroots S = {x1, x2}) :
     algebraMap T S b = -algebraMap T S a * (x1 + x2) ∧
     algebraMap T S c = algebraMap T S a * x1 * x2 := by
@@ -36,7 +38,7 @@ lemma quadratic_vieta_aroots {T S : Type*} [CommRing T] [CommRing S] [IsDomain S
     X ^ 2 + C ((algebraMap T S) b) * X + C ((algebraMap T S) c) by simp] at haroots
   exact quadratic_vieta haroots
 
-private lemma roots_of_ne_zero_of_vieta {R : Type*} [CommRing R] [IsDomain R] {a b c x1 x2 : R}
+private lemma roots_of_ne_zero_of_vieta [CommRing R] [IsDomain R] {a b c x1 x2 : R}
     (ha : a ≠ 0) (hvieta : b = -a * (x1 + x2) ∧ c = a * x1 * x2) :
     (C a * X ^ 2 + C b * X + C c).roots = {x1, x2} := by
   suffices C a * X ^ 2 + C b * X + C c = C a * (X - C x1) * (X - C x2) by
@@ -46,12 +48,12 @@ private lemma roots_of_ne_zero_of_vieta {R : Type*} [CommRing R] [IsDomain R] {a
   simpa [hvieta.1, hvieta.2] using by ring
 
 /-- **Vieta's formula** for quadratic in term of `Polynomial.roots`. -/
-lemma quadratic_vieta_iff_of_ne_zero {R : Type*} [CommRing R] [IsDomain R] {a b c x1 x2 : R}
+lemma quadratic_vieta_iff_of_ne_zero [CommRing R] [IsDomain R] {a b c x1 x2 : R}
     (ha : a ≠ 0) : (C a * X ^ 2 + C b * X + C c).roots = {x1, x2} ↔
-      b = -a * (x1 + x2) ∧ c = a * x1 * x2 := by
-  refine ⟨quadratic_vieta, roots_of_ne_zero_of_vieta ha⟩
+      b = -a * (x1 + x2) ∧ c = a * x1 * x2 :=
+  ⟨quadratic_vieta, roots_of_ne_zero_of_vieta ha⟩
 
-lemma quadratic_vieta_aroots_iff_of_ne_zero {T S : Type*} [CommRing T] [CommRing S] [IsDomain S]
+lemma quadratic_vieta_aroots_iff_of_ne_zero [CommRing T] [CommRing S] [IsDomain S]
     [Algebra T S] {a b c : T} {x1 x2 : S} (ha : algebraMap T S a ≠ 0) :
       (C a * X ^ 2 + C b * X + C c).aroots S = {x1, x2} ↔
         algebraMap T S b = -algebraMap T S a * (x1 + x2) ∧
@@ -63,7 +65,7 @@ lemma quadratic_vieta_aroots_iff_of_ne_zero {T S : Type*} [CommRing T] [CommRing
   exact roots_of_ne_zero_of_vieta ha ⟨hb, hc⟩
 
 /-- **Vieta's formula** for quadratic in term of `Polynomial.roots`. -/
-lemma quadratic_vieta_iff_of_ne_zero' {R : Type*} [Field R] {a b c x1 x2 : R} (ha : a ≠ 0) :
+lemma quadratic_vieta_iff_of_ne_zero' [Field R] {a b c x1 x2 : R} (ha : a ≠ 0) :
     (C a * X ^ 2 + C b * X + C c).roots = {x1, x2} ↔
       x1 + x2 = -b / a ∧ x1 * x2 = c / a := by
   rw [quadratic_vieta_iff_of_ne_zero ha]
