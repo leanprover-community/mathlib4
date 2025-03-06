401a0ea3d6e624c14aaa0cbbe4fde13df5ca82d6
Dagur Asgeirsson <dagurtomas@gmail.com>
2025-03-06 20:33:32 +0100

    .

--- Mathlib/Condensed/Slides.lean
+++ Mathlib/Condensed/Slides.lean
@@ -68,9 +68,9 @@ PhD defence, University of Copenhagen, March 7th 2025
 * It strives to formalize everything in the highest generality
   possible.
 
-* My PhD research is about formalizing condensed mathematics
-  *for mathlib* with the goal to establish the theory of solid
-  abelian groups / modules.
+* The goal of my research has been to formalize condensed
+  mathematics *for mathlib*, to eventually establish a formalized
+  theory of solid abelian groups / modules.
 
 -/
 
@@ -136,17 +136,21 @@ has not been implemented in mathlib).
 
 -/
 
-example : Abelian CondensedAb.{u} := inferInstance
+#check CondensedAb.{u}
 
-example : HasColimits CondensedAb.{u} := inferInstance
+#synth Category CondensedAb.{u}
 
-example : HasLimits CondensedAb.{u} := inferInstance
+#synth Abelian CondensedAb.{u}
 
-example : AB4 CondensedAb.{u} := inferInstance
+#synth HasColimits CondensedAb.{u}
 
-example : AB4Star CondensedAb.{u} := inferInstance
+#synth HasLimits CondensedAb.{u}
 
-example : AB5 CondensedAb.{u} := inferInstance
+#synth AB4 CondensedAb.{u}
+
+#synth AB4Star CondensedAb.{u}
+
+#synth AB5 CondensedAb.{u}
 
 end CondAb
 
@@ -212,6 +216,8 @@ section Paper2
 /-
 
 # Paper 2: Categorical foundations of formalized condensed mathematics
+
+Joint with: R. Brasca, N. Kuhn, F. A. E. Nuccio, A. Topaz
 
 Let's first back up a bit and see how to implement condensed objects in
 full mathlib generality:
@@ -325,6 +331,25 @@ example : EqualizerCondition X ↔
 #check Fork.ofι
 
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 variable (Y : Stonean.{u}ᵒᵖ ⥤ Type (u+1))
 
@@ -501,9 +526,49 @@ various ways, important for the development of the solid theory.
 
 
 
-variable (M : CondensedAb) (S : Profinite)
 
-def discreteCocone (S : Profinite) :
+
+#check CondensedMod.isDiscrete_tfae
+
+variable (M : CondensedAb)
+
+/-
+Notation for the functor mapping an abelian
+group to the corresponding discrete abelian group
+-/
+notation3 "δ" => discrete (ModuleCat (ULift.{1} ℤ))
+
+/-
+Being discrete is equivalent to the counit inducing an
+isomorphism `δ M(*) ⟶ M`.
+-/
+def isoUnderlyingOfDiscrete [M.IsDiscrete] :
+    M ≅ (δ).obj (M.val.obj (*)) :=
+  haveI : IsIso ((discreteUnderlyingAdj _).counit.app M) := by
+    rwa [((CondensedMod.isDiscrete_tfae _ M).out 1 0:)]
+  (asIso ((discreteUnderlyingAdj _).counit.app _)).symm
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+variable (S : Profinite)
+
+def discreteCocone :
     Cocone (S.diagram.op ⋙ profiniteToCompHaus.op ⋙ M.val) :=
   (profiniteToCompHaus.op ⋙ M.val).mapCocone S.asLimitCone.op
 
@@ -521,18 +586,20 @@ lemma discreteness_characterization : M.IsDiscrete ↔
     ∀ S, Nonempty (IsColimit (discreteCocone M S)) :=
   (CondensedMod.isDiscrete_tfae _ M).out 0 6
 
-/-
-Being discrete is equivalent to the counit inducing an
-isomorphism `cst M(*) ⟶ M` where `cst` denotes the
-constant sheaf functor (`Condensed.discrete`)
--/
-instance [M.IsDiscrete] :
-    IsIso ((discreteUnderlyingAdj _).counit.app M) := by
-  rwa [((CondensedMod.isDiscrete_tfae _ M).out 1 0:)]
+section
 
-def isoUnderlyingOfDiscrete [M.IsDiscrete] :
-    M ≅ (discrete _).obj (M.val.obj (*)) :=
-  (asIso ((discreteUnderlyingAdj _).counit.app _)).symm
+/-
+In fact, the discreteness characterization has an internal version.
+-/
+
+def discreteInternalCocone :
+    Cocone (S.diagram.op ⋙ (profiniteFree _).op ⋙ (internalHom.flip.obj M)) :=
+  ((profiniteFree _).op ⋙ (internalHom.flip.obj M)).mapCocone S.asLimitCone.op
+
+def isColimitDiscreteInternal [M.IsDiscrete] :
+  IsColimit (discreteInternalCocone M S) := sorry
+
+end
 
 end Paper3
 
@@ -554,11 +621,10 @@ end Paper3
 
 
 
-section Future
+
+section Application
 
 /-
-
-# Future work
 
 We give an outline of the promised isomorphism `ℤ[S]◾ ≅ ∏ ℤ`.
 
@@ -570,38 +636,45 @@ Nöbeling's theorem and the discreteness characterization is done.
 
 -/
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 open CondensedAb
+
+variable  (S : Profinite.{0})
 
 section
 
-#check of
+variable (M : CondensedAb)
 
-variable (S : Profinite.{0})
-
-instance : (ℤ[S] ⟶[CondensedAb] of ℤ).IsDiscrete := by sorry
-
-def isoUnderlyingInternal : (ℤ[S] ⟶[CondensedAb] of ℤ).val.obj (*) ≅
-    ModuleCat.of (ULift ℤ) (ULift (LocallyConstant S ℤ)) := sorry
-
-def isoInternalLocConst :
-    (ℤ[S] ⟶[CondensedAb] of ℤ) ≅ of (LocallyConstant S ℤ) :=
-  (isoUnderlyingOfDiscrete _) ≪≫
-    (discrete _).mapIso (isoUnderlyingInternal S)
-
-variable (M : CondensedAb.{u})
-
-example : M.IsSolid ↔ ∀ S : Profinite.{u},
+example : M.IsSolid ↔ ∀ S : Profinite,
     IsIso ((yoneda.obj M).map (ε S : ℤ[S] ⟶ ℤ[S]◾).op) :=
   ⟨fun h ↦ h.1, fun h ↦ ⟨h⟩⟩
+
+variable (S : Profinite.{0})
 
 example : ℤ[S]◾ ≅
   (rightKanExtension
     FintypeCat.toProfinite
     (finFree (ULift.{1} ℤ))).obj S := Iso.refl _
 
-def iso1 :
-    ℤ[S]◾ ≅ ((ℤ[S] ⟶[CondensedAb] (of ℤ)) ⟶[CondensedAb] of ℤ) :=
-  sorry
+def solidIso : ℤ[S]◾ ≅ limit (S.diagram ⋙ profiniteFree (ULift.{1} ℤ)) :=
+  haveI : Initial <| Profinite.Extend.functor S.asLimitCone :=
+    Profinite.Extend.functor_initial S.asLimitCone S.asLimit
+  (profiniteSolidIsPointwiseRightKanExtension _ _).isoLimit ≪≫
+    (Initial.limitIso (Profinite.Extend.functor S.asLimitCone)
+      (S.fintypeDiagram' ⋙ finFree (ULift ℤ))).symm
 
 end
 
@@ -622,9 +695,45 @@ end
 
 
 
+
 section
 
-variable (S : Profinite.{0})
+/-
+This is the key observation, and can be proved using the
+discreteness characterization from paper 3, and a bit
+of abstract nonsense.
+-/
+instance : (ℤ[S] ⟶[CondensedAb] of ℤ).IsDiscrete := by sorry
+
+def isoInternalLocConst : (ℤ[S] ⟶[CondensedAb] of ℤ) ≅ of (LocallyConstant S ℤ) :=
+  let i : (ℤ[S] ⟶[CondensedAb] of ℤ).val.obj (*) ≅
+    ModuleCat.of (ULift ℤ) (ULift (LocallyConstant S ℤ)) := sorry
+  (isoUnderlyingOfDiscrete _) ≪≫ (δ).mapIso i
+
+
+def internalHomIntIso : (of ℤ ⟶[CondensedAb] of ℤ) ≅ of ℤ := sorry
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+section
 
 /-
 `internalHom.flip.obj X` is the functor `(_ ⟶[CondensedAb] X)`
@@ -637,19 +746,121 @@ instance (X : CondensedAb) :
     PreservesLimits (internalHom.flip.obj X) :=
   sorry -- missing API
 
-def internalHomIntIso : (of ℤ ⟶[CondensedAb] of ℤ) ≅ of ℤ := sorry
+instance (X : CondensedAb) :
+    PreservesLimitsOfSize.{0, 0} (internalHom.flip.obj X) :=
+  sorry -- missing API
 
-def iso : ℤ[S]◾ ≅ ∏ᶜ (fun i : index S ↦ of ℤ) := by
-  refine
-    iso1 S ≪≫
-    (internalHom.mapIso (isoInternalLocConst S).op).symm.app _ ≪≫
-    ?_
-  change (((discrete _).mapCocone (freeCofan S)).pt ⟶[CondensedAb] _) ≅ _
+def locallyConstantCocone :
+    Cocone (S.diagram.op ⋙ profiniteToCompHaus.op ⋙
+      ((CondensedMod.LocallyConstant.functor (ULift.{1} ℤ)).obj
+        (ModuleCat.of _ (ULift.{1} ℤ))).val) := discreteCocone
+          ((CondensedMod.LocallyConstant.functor (ULift.{1} ℤ)).obj
+            (ModuleCat.of _ (ULift.{1} ℤ))) S
+
+def isColimitLocallyConstantCocone : IsColimit (locallyConstantCocone S) := by
+  have : ((CondensedMod.LocallyConstant.functor (ULift.{1} ℤ)).obj
+      (ModuleCat.of _ (ULift.{1} ℤ))).IsDiscrete := by
+    rw [((CondensedMod.isDiscrete_tfae (ULift.{1} ℤ) _).out 0 3:)]
+    exact
+      obj_mem_essImage (CondensedMod.LocallyConstant.functor (ULift ℤ))
+        (ModuleCat.of (ULift ℤ) (ULift ℤ))
+  rw [discreteness_characterization] at this
+  exact (this S).some
+
+def isoAux : (δ).obj (locallyConstantCocone S).pt ≅ of (LocallyConstant S ℤ) := by
+  refine (δ).mapIso ?_
+  refine LinearEquiv.toModuleIso ?_
+  refine {
+    toLinearMap := ⟨⟨fun x ↦ ⟨fun s ↦ (x s).down, ?_⟩, fun _ _ ↦ rfl⟩, fun _ _ ↦ rfl⟩
+    invFun := fun x ↦ ⟨fun s ↦ x.down s, ?_⟩
+    left_inv := congrFun rfl
+    right_inv := congrFun rfl }
+  · intro U
+    let U' : Set (ULift.{1} ℤ) := ULift.up '' U
+    convert x.2 U' using 1
+    ext s
+    simp only [Cone.op_pt, Set.mem_preimage, LocallyConstant.toFun_eq_coe, Set.mem_image, U']
+    refine ⟨fun h ↦ ⟨(x s).down, h, rfl⟩, fun h ↦ ?_⟩
+    obtain ⟨y, hy⟩ := h
+    rw [← hy.2]
+    exact hy.1
+  · intro U
+    let U' : Set ℤ := ULift.down '' U
+    convert x.down.2 U' using 1
+    ext s
+    simp [U']
+    rfl
+
+example : of (LocallyConstant S ℤ) = (δ).obj (ModuleCat.of (ULift.{1} ℤ)
+    (ULift.{1} (LocallyConstant S ℤ))) := rfl
+
+end
+
+
+
+section
+
+/-
+The strategy is to prove that `ℤ[S]◾` and `∏ ℤ` are both
+isomorphic to `(ℤ[S] ⟶[CondensedAb] ℤ) ⟶[CondensedAb] ℤ`
+(where `⟶[CondensedAb]` denotes the internal hom in
+condensed abelian groups).
+-/
+
+def iso₁ : ℤ[S]◾ ≅ ((ℤ[S] ⟶[CondensedAb] (of ℤ)) ⟶[CondensedAb] of ℤ) := by
+  refine solidIso _ ≪≫ ?_
+  change _ ≅ (internalHom.flip.obj _).obj _
+  refine ?_ ≪≫ (internalHom.flip.obj _).mapIso (isoInternalLocConst _).op
+  refine ?_ ≪≫ (internalHom.flip.obj _).mapIso (isoAux S).symm.op
+  have : (δ).IsLeftAdjoint := (discreteUnderlyingAdj _).isLeftAdjoint
+  let h : IsColimit ((δ).mapCocone (locallyConstantCocone S)) := by
+    apply isColimitOfPreserves
+    exact isColimitLocallyConstantCocone S
+  let hh : IsLimit ((internalHom.flip.obj (of ℤ)).mapCone
+      ((δ).mapCocone (locallyConstantCocone S)).op) := by
+    apply isLimitOfPreserves
+    exact h.op
+  refine ?_ ≪≫ (limit.isLimit _).conePointUniqueUpToIso hh
+  dsimp
+  let e : ((DiscreteQuotient S)ᵒᵖ)ᵒᵖ ≌ DiscreteQuotient S :=
+    opOpEquivalence (DiscreteQuotient _)
+  refine ?_ ≪≫ (Functor.Initial.limitIso e.inverse _)
+  refine lim.mapIso ?_
+  let F := ((CondensedMod.LocallyConstant.functor (ULift.{1} ℤ)).obj
+        (ModuleCat.of _ (ULift.{1} ℤ))).val
+  let i : e.inverse ⋙ (S.diagram.op ⋙ profiniteToCompHaus.op ⋙ F ⋙ (δ)).op ⋙
+      internalHom.flip.obj (of ℤ) ≅
+      S.diagram ⋙ profiniteToCompHaus ⋙ F.rightOp ⋙ (δ).op
+        ⋙ internalHom.flip.obj (of ℤ) := Iso.refl _
+  refine ?_ ≪≫ i.symm
+  change (S.fintypeDiagram ⋙ _) ⋙ _ ≅ (S.fintypeDiagram ⋙ _) ⋙ _
+  refine Functor.associator _ _ _ ≪≫ ?_ ≪≫ (Functor.associator _ _ _).symm
+  refine isoWhiskerLeft S.fintypeDiagram ?_
+  refine NatIso.ofComponents (fun X ↦ ?_) sorry
+  dsimp [e]
+  change ℤ[FintypeCat.toProfinite.obj X] ≅
+    (_ ⟶[CondensedAb] of ℤ)
+  refine ?_ ≪≫ (internalHom.flip.obj _).mapIso (isoAux _).op
+  change _ ≅ (of (LocallyConstant (FintypeCat.toProfinite.obj X) _))
+      ⟶[CondensedAb] (of ℤ)
+  sorry
+/-!
+We're reduced to showing that the free condensed abelian group on a
+finite set `X` is isomorphic to the internal hom
+`(LocallyConstant X ℤ) ⟶[CondensedAb] ℤ`,
+naturally in `X`.
+-/
+
+
+def iso₂ : ((ℤ[S] ⟶[CondensedAb] (of ℤ)) ⟶[CondensedAb] of ℤ) ≅
+    ∏ᶜ (fun i : index S ↦ of ℤ) := by
+  refine (internalHom.mapIso (isoInternalLocConst S).op).symm.app _ ≪≫ ?_
+  change (((δ).mapCocone (freeCofan S)).pt ⟶[CondensedAb] _) ≅ _
   change ((internalHom.flip.obj _).mapCone
-    ((discrete _).mapCocone (freeCofan S)).op).pt ≅ _
-  have : (discrete (ModuleCat (ULift.{1} ℤ))).IsLeftAdjoint :=
+    ((δ).mapCocone (freeCofan S)).op).pt ≅ _
+  have : (δ).IsLeftAdjoint :=
     (discreteUnderlyingAdj _).isLeftAdjoint
-  let h : IsColimit ((discrete _).mapCocone (freeCofan S)) := by
+  let h : IsColimit ((δ).mapCocone (freeCofan S)) := by
     apply isColimitOfPreserves
     exact isColimitFreeCofan S
   let hh : IsLimit ((internalHom.flip.obj (of ℤ)).mapCone
@@ -663,9 +874,12 @@ def iso : ℤ[S]◾ ≅ ∏ᶜ (fun i : index S ↦ of ℤ) := by
   refine lim.mapIso ?_
   exact NatIso.ofComponents (fun _ ↦ internalHomIntIso) (fun _ ↦ rfl)
 
+def iso : ℤ[S]◾ ≅ ∏ᶜ (fun i : index S ↦ of ℤ) :=
+  iso₁ S ≪≫ iso₂ S
+
 end
 
-end Future
+end Application
 
 
 
