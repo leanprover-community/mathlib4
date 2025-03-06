bcfd135b7c32a479a86bc6e987ab6a063bde365c
EtienneC30 <66847262+EtienneC30@users.noreply.github.com>
2025-03-06 12:46:55 +0100

    useless parentheses
    Co-authored-by: Rémy Degenne <remydegenne@gmail.com>

--- Mathlib/Probability/Process/Filtration.lean
+++ Mathlib/Probability/Process/Filtration.lean
@@ -386,7 +386,7 @@ def piFinset : @Filtration (Π i, X i) (Finset ι) _ pi where
   le' s := s.measurable_restrict.comap_le
 
 lemma piFinset_eq_comap_restrict (s : Finset ι) :
-    piFinset (X := X) s = pi.comap (s.toSet.restrict) := by
+    piFinset (X := X) s = pi.comap s.toSet.restrict := by
   apply le_antisymm
   · simp_rw [piFinset, ← Set.piCongrLeft_comp_restrict, ← MeasurableEquiv.coe_piCongrLeft,
       ← comap_comp]
