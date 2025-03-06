ad724df7cc6b0138f478c0616234e5cca3375fc3
Sina Hazratpour <sinahazratpour@gmail.com>
2025-03-06 18:48:30 +0100

    Update Mathlib/CategoryTheory/Pi/Monoidal.lean
    Co-authored-by: Markus Himmel <markus@himmel-villmar.de>

--- Mathlib/CategoryTheory/Pi/Monoidal.lean
+++ Mathlib/CategoryTheory/Pi/Monoidal.lean
@@ -3,7 +3,6 @@ Copyright (c) 2025 Sina Hazratpour. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Sina Hazratpour
 -/
-
 import Mathlib.CategoryTheory.Pi.Basic
 import Mathlib.CategoryTheory.Monoidal.Category
 
