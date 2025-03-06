dbbea5a3af4e4602ee1f996afc0b0b0e12cf9591
Sina Hazratpour <sinahazratpour@gmail.com>
2025-03-06 18:48:40 +0100

    Update Mathlib/CategoryTheory/Pi/MonoidalClosed.lean
    Co-authored-by: Markus Himmel <markus@himmel-villmar.de>

--- Mathlib/CategoryTheory/Pi/MonoidalClosed.lean
+++ Mathlib/CategoryTheory/Pi/MonoidalClosed.lean
@@ -3,7 +3,6 @@ Copyright (c) 2025 Sina Hazratpour. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Sina Hazratpour
 -/
-
 import Mathlib.CategoryTheory.Closed.Monoidal
 import Mathlib.CategoryTheory.Pi.Monoidal
 
