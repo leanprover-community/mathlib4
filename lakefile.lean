8c2df8be59a24511a7c88ef309fe46f3643d4e68
Sebastian Ullrich <sebasti@nullri.ch>
2025-03-06 15:25:52 +0100

    use fixed deps

--- lakefile.lean
+++ lakefile.lean
@@ -7,9 +7,9 @@ open Lake DSL
 ## Mathlib dependencies on upstream projects
 -/
 
-require "leanprover-community" / "batteries" @ git "main"
+require "leanprover-community" / "batteries" @ git "lean-pr-testing-7334"
 require "leanprover-community" / "Qq" @ git "master"
-require "leanprover-community" / "aesop" @ git "nightly-testing"
+require "Kha" / "aesop" @ git "lean-pr-testing-7334"
 require "leanprover-community" / "proofwidgets" @ git "v0.0.53" -- ProofWidgets should always be pinned to a specific version
   with NameMap.empty.insert `errorOnBuild
     "ProofWidgets not up-to-date. \
