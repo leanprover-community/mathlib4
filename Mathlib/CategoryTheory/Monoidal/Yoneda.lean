8ba05b6d5367b6aedacfa71e5dd0de256803f16f
Kiolt <38689041+Kiolt@users.noreply.github.com>
2025-03-07 19:01:48 +0000

    feat: objects representing functors to `CommMon` satisfy `IsCommMon`
    From Toric

--- Mathlib/CategoryTheory/Monoidal/Yoneda.lean
+++ Mathlib/CategoryTheory/Monoidal/Yoneda.lean
@@ -200,3 +200,25 @@ lemma essImage_yonedaMon :
     exact ⟨Mon_.mk' X, ⟨yonedaMonObjMon_Class.ofRepresentableBy X F e⟩⟩
 
 end Mon_
+
+section CommMon_
+
+variable [ChosenFiniteProducts C]
+
+/-- If `X` represents a presheaf of commutative groups, then `X` is a commutative group object. -/
+def IsCommMon.ofRepresentableBy (F : Cᵒᵖ ⥤ CommMonCat)
+    (α : (F ⋙ forget _).RepresentableBy X) :
+    letI := Mon_Class.ofRepresentableBy X (F ⋙ (forget₂ CommMonCat MonCat)) α
+    IsCommMon X := by
+  letI := Mon_Class.ofRepresentableBy X (F ⋙ (forget₂ CommMonCat MonCat)) α
+  refine ⟨?_⟩
+  change (β_ X X).hom ≫ α.homEquiv.symm (α.homEquiv (fst X X) * α.homEquiv (snd X X))
+      = α.homEquiv.symm (α.homEquiv (fst X X) * α.homEquiv (snd X X))
+  apply α.homEquiv.injective
+  simp only [α.homEquiv_comp, Equiv.apply_symm_apply]
+  simp only [Functor.comp_map, ConcreteCategory.forget_map_eq_coe, map_mul]
+  simp only [← ConcreteCategory.forget_map_eq_coe, ← Functor.comp_map, ← α.homEquiv_comp]
+  rw [_root_.mul_comm]
+  simp
+
+end CommMon_
