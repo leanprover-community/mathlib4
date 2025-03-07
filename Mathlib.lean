a16c365809987e70b6634e2b1564ded4afc6c111
Xavier Roblot <roblot@math.univ-lyon1.fr>
2025-03-07 10:40:53 +0000

    feat(NumberField/MixedSpace): add polar coordinates change of variables (part 1) (#22120)
    We define two polar coordinate changes of variables for the mixed space `ℝ^r₁ × ℂ^r₂` associated
    to a number field `K` of signature `(r₁, r₂)`.
    
    - The first one is `mixedEmbedding.polarCoord` and has value in `ℝ^r₁ × (ℝ ⨯ ℝ)^r₂`.
    
    - The second is`mixedEmbedding.polarSpaceCoord` and has value in `ℝ^(r₁+r₂) × ℝ^r₂`.
    
    This second change of variable is of interest for computing the volume of a set with enough symmetry. This part will be developed in a following PR. 
    
    This PR is part of the proof of the Analytic Class Number Formula.

--- Mathlib.lean
+++ Mathlib.lean
@@ -4277,6 +4277,7 @@ import Mathlib.NumberTheory.NumberField.Basic
 import Mathlib.NumberTheory.NumberField.CanonicalEmbedding.Basic
 import Mathlib.NumberTheory.NumberField.CanonicalEmbedding.ConvexBody
 import Mathlib.NumberTheory.NumberField.CanonicalEmbedding.FundamentalCone
+import Mathlib.NumberTheory.NumberField.CanonicalEmbedding.PolarCoord
 import Mathlib.NumberTheory.NumberField.ClassNumber
 import Mathlib.NumberTheory.NumberField.Completion
 import Mathlib.NumberTheory.NumberField.Discriminant.Basic
