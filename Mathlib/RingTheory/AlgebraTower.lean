4f726687dc2ef3b58282e83d3d01c92bb2acdb1c
Anne C.A. Baanen <vierkantor@vierkantor.com>
2025-03-07 14:57:14 +0100

    Upstream a lemma that got downstreamed by accident

--- Mathlib/RingTheory/AlgebraTower.lean
+++ Mathlib/RingTheory/AlgebraTower.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kenny Lau
 -/
 import Mathlib.Algebra.Algebra.Tower
-import Mathlib.LinearAlgebra.Basis.Submodule
+import Mathlib.LinearAlgebra.Basis.Basic
 
 /-!
 # Towers of algebras
