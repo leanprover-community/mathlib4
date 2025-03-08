08983e9e095a8a1f402ae4ecf3779217af9b504b
ybenmeur <180416181+ybenmeur@users.noreply.github.com>
2025-03-08 14:25:24 +0100

    Update Tannaka.lean

--- Mathlib/RepresentationTheory/Tannaka.lean
+++ Mathlib/RepresentationTheory/Tannaka.lean
@@ -42,6 +42,10 @@ variable (k G) in
 /-- The monoidal forgetful functor from `FDRep k G` to `FGModuleCat k`. -/
 def forget := LaxMonoidalFunctor.of (forget₂ (FDRep k G) (FGModuleCat k))
 
+@[simp] lemma forget_obj (X : FDRep k G) : (forget k G).obj X = X.V := rfl
+
+@[simp] lemma forget_map (X Y : FDRep k G) (f : X ⟶ Y) : (forget k G).map f = f.hom := rfl
+
 /-- Definition of `equivHom g : Aut (forget k G)` by its components. -/
 @[simps]
 def equivApp (g : G) (X : FDRep k G) : X.V ≅ X.V where
@@ -49,15 +53,14 @@ def equivApp (g : G) (X : FDRep k G) : X.V ≅ X.V where
   inv := ofHom (X.ρ g⁻¹)
   hom_inv_id := by
     ext x
-    change (X.ρ g⁻¹ * X.ρ g) x = x
-    simp [← map_mul]
+    simp
   inv_hom_id := by
     ext x
-    change (X.ρ g * X.ρ g⁻¹) x = x
-    simp [← map_mul]
+    simp
 
 variable (k G) in
 /-- The group homomorphism `G →* Aut (forget k G)` shown to be an isomorphism. -/
+@[simps]
 def equivHom : G →* Aut (forget k G) where
   toFun g :=
     LaxMonoidalFunctor.isoOfComponents (equivApp g) (fun f ↦ (f.comm g).symm) rfl (by intros; rfl)
@@ -65,13 +68,11 @@ def equivHom : G →* Aut (forget k G) where
   map_mul' _ _ := by ext; simp; rfl
 
 /-- The representation on `G → k` induced by multiplication on the right in `G`. -/
-@[simps]
 def rightRegular : Representation k G (G → k) where
-  toFun s := {
-    toFun f t := f (t * s)
+  toFun s :=
+  { toFun f t := f (t * s)
     map_add' _ _ := rfl
-    map_smul' _ _ := rfl
-  }
+    map_smul' _ _ := rfl }
   map_one' := by
     ext
     simp
@@ -79,28 +80,34 @@ def rightRegular : Representation k G (G → k) where
     ext
     simp [mul_assoc]
 
+@[simp]
+lemma rightRegular_apply (s t : G) (f : G → k) : rightRegular s f t = f (t * s) := rfl
+
 /-- The representation on `G → k` induced by multiplication on the left in `G`. -/
-@[simps]
 def leftRegular : Representation k G (G → k) where
-  toFun s := {
-    toFun f t := f (s⁻¹ * t)
+  toFun s :=
+  { toFun f t := f (s⁻¹ * t)
     map_add' _ _ := rfl
-    map_smul' _ _ := rfl
-  }
+    map_smul' _ _ := rfl }
   map_one' := by
     ext
     simp
   map_mul' _ _ := by
     ext
     simp [mul_assoc]
+
+@[simp]
+lemma leftRegular_apply (s t : G) (f : G → k) : leftRegular s f t = f (s⁻¹ * t) := rfl
 
 variable [Fintype G]
 
 variable (k G) in
 /-- The right regular representation `rightRegular` on `G → k` as a `FDRep k G`. -/
+@[simp]
 def rightFDRep : FDRep k G := FDRep.of rightRegular
 
 /-- Map sending `η : Aut (forget k G)` to its component at `rightFDRep k G` as a linear map. -/
+@[simp]
 def toRightFDRepComp (η : Aut (forget k G)) : (G → k) →ₗ[k] (G → k) :=
   (η.hom.hom.app (rightFDRep k G)).hom
 
@@ -109,10 +116,8 @@ end definitions
 variable [Fintype G]
 
 lemma equivHom_inj [Nontrivial k] [DecidableEq G] : Function.Injective (equivHom k G) := by
-  rw [injective_iff_map_eq_one]
-  intro s h
-  apply_fun (fun x ↦ (toRightFDRepComp x) (single 1 1) 1) at h
-  change (single 1 1 : G → k) (1 * s) = (single 1 1 : G → k) 1 at h
+  intro s t h
+  apply_fun (fun x ↦ (toRightFDRepComp x) (single t 1) 1) at h
   simp_all [single_apply]
 
 /-- An algebra morphism `φ : (G → k) →ₐ[k] k` is an evaluation map. -/
@@ -141,7 +146,7 @@ def mulRepHom : rightFDRep k G ⊗ rightFDRep k G ⟶ rightFDRep k G where
   hom := ofHom (LinearMap.mul' k (G → k))
   comm := by
     intro
-    ext (u : TensorProduct k (G → k) (G → k))
+    ext u
     refine TensorProduct.induction_on u rfl (fun _ _ ↦ rfl) (fun _ _ hx hy ↦ ?_)
     simp only [map_add, hx, hy]
 
@@ -160,11 +165,12 @@ an algebra morphism `(G → k) →ₐ[k] (G → k)`. -/
 def algHomOfRightFDRepComp (η : Aut (forget k G)) : (G → k) →ₐ[k] (G → k) := by
   refine AlgHom.ofLinearMap (toRightFDRepComp η) ?_ (map_mul_toRightFDRepComp η)
   let α_inv : (G → k) → (G → k) := (η.inv.hom.app (rightFDRep k G)).hom
+  suffices (toRightFDRepComp η) (α_inv 1) = (1 : G → k) by
+    have h := this
+    rwa [← one_mul (α_inv 1), map_mul_toRightFDRepComp, h, mul_one] at this
   have := η.inv_hom_id
   apply_fun (fun x ↦ (x.hom.app (rightFDRep k G)).hom (1 : G → k)) at this
-  change (toRightFDRepComp η) (α_inv 1) = (1 : G → k) at this
-  have h := this
-  rwa [← one_mul (α_inv 1), map_mul_toRightFDRepComp, h, mul_one] at this
+  exact this
 
 end FiniteGroup
 
