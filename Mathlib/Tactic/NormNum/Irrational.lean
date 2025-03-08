ef129431d2c485d6452eb469430e6e09d2e204dc
Vasilii Nesterov <vas.nesterov63@gmail.com>
2025-03-08 10:13:40 +0300

    some

--- Mathlib/Tactic/NormNum/Irrational.lean
+++ Mathlib/Tactic/NormNum/Irrational.lean
@@ -5,6 +5,8 @@ Authors: Vasilii Nesterov
 -/
 import Mathlib.Data.Real.Irrational
 import Mathlib.Tactic.Rify
+import Mathlib.Analysis.SpecialFunctions.Pow.Real
+
 
 /-! # `norm_num` extension for `Nat.sqrt`
 
@@ -61,6 +63,26 @@ private theorem irrational_of_certificate {x : ℝ} {m n k : ℕ}
   · rwa [h_even.neg_pow]
   · linarith [h_odd.pow_neg (show x < 0 by linarith)]
 
+private theorem irrational_rpow_rat_rat {x y : ℝ} {xn xd yn yd kn kd : ℕ}
+    (hx_isRat : IsRat x (Int.ofNat xn) xd)
+    (hy_isRat : IsRat y (Int.ofNat yn) yd)
+    -- (h1 : Irrational (x^((yd : ℝ)⁻¹))
+    (hx_coprime : IsCoprime xn xd)
+    (hn1 : kn^yd < xn^yn)
+    (hn2 : xn^yn < (kn + 1)^yd)
+    (hd1 : kd^yd < xd^yn)
+    (hd2 : xd^yn < (kd + 1)^yd)
+    :
+    Irrational (x^y) := by
+  rcases hx_isRat with ⟨hx_inv, hx_eq⟩
+  rcases hy_isRat with ⟨hy_inv, hy_eq⟩
+  rw [hy_eq]
+  rw [Real.rpow_intCast_mul sorry]
+  simp
+  apply irrational_nrt_of_notint_nrt
+
+
+
 private theorem irrational_sqrt_of_certificate {x : ℝ} {m k : ℕ}
     (h_isNat : IsNat x m)
     (h1 : k^2 < m)
@@ -71,6 +93,22 @@ private theorem irrational_sqrt_of_certificate {x : ℝ} {m k : ℕ}
   · simp
   · simp
   all_goals assumption
+
+private theorem irrational_rat {x : ℝ} (num denom : ℕ)
+    (h_isRat : IsRat x (Int.ofNat num) denom)
+    (h1 : Irrational denom)
+    (h2 : Irrational num) :
+    Irrational (Real.sqrt x) := by
+  obtain ⟨_, h⟩ := h_isRat
+  rw [h]
+
+  apply Irrational.int_mul
+  · refine Irrational.of_inv ?_
+
+  · intro h
+    simp at h
+    rw [h] at h2
+    simp at h2
 
 def findNotPowerCertificateCore (m n : ℕ) : Option ℕ := Id.run do
   let mut left := 0
@@ -93,17 +131,37 @@ def findNotPowerCertificate (m n : Q(ℕ)) : MetaM (NotPowerCertificate m n) := 
   let .isBool true pf_right := ← derive q($m < ($k + 1)^$n) | failure
   return ⟨q($k), pf_left, pf_right⟩
 
+-- @[norm_num Irrational (Real.rpow _ _)]
+-- def evalIrrationalRpow : NormNumExt where eval {u α} e := do
+--   match u, α, e with
+--   | 0, ~q(Prop), ~q(Irrational (Real.rpow $x $y)) =>
+--     match ← derive y with
+--     | .isNat (sℝ : Q(AddMonoidWithOne ℝ)) yn ypf =>
+--       match ← derive x with
+--       | .isNat (sℝ : Q(AddMonoidWithOne ℝ)) xn xpf =>
+--         let cert ← findNotPowerCertificate xn yn
+--         assumeInstancesCommute
+--         return .isTrue q(irrational_sqrt_of_certificate $pf $cert.pf_left $cert.pf_right)
+--       | _ => failure
+--     | _ => failure
+--   | _ => failure
+
 @[norm_num Irrational (Real.sqrt _)]
 def evalIrrationalSqrt : NormNumExt where eval {u α} e := do
-  have inst : AddMonoidWithOne ℝ := by infer_instance
   match u, α, e with
   | 0, ~q(Prop), ~q(Irrational (Real.sqrt $x)) =>
     match ← derive x with
-    | .isBool _ _ => failure
     | .isNat (sℝ : Q(AddMonoidWithOne ℝ)) ex pf =>
       let cert ← findNotPowerCertificate ex q(nat_lit 2)
       assumeInstancesCommute
       return .isTrue q(irrational_sqrt_of_certificate $pf $cert.pf_left $cert.pf_right)
+    | .isRat sℝ eq en ed pf =>
+      match en with
+      | .app (.const ``Int.negOfNat []) (n : Q(ℕ)) =>
+        sorry
+      | .app (.const ``Int.ofNat []) (n' : Q(ℕ)) =>
+        sorry
+      | _ => failure
     | _ => failure
   | _ => failure
 
