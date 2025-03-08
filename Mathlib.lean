b0c49432a53b9345e22c6fa4dbda3448e63d7217
mitchell-horner <29882987+mitchell-horner@users.noreply.github.com>
2025-03-08 20:21:48 +1100

    Add `...Extremal.Basic`

--- Mathlib.lean
+++ Mathlib.lean
@@ -2474,6 +2474,7 @@ import Mathlib.Combinatorics.SimpleGraph.Density
 import Mathlib.Combinatorics.SimpleGraph.Diam
 import Mathlib.Combinatorics.SimpleGraph.Ends.Defs
 import Mathlib.Combinatorics.SimpleGraph.Ends.Properties
+import Mathlib.Combinatorics.SimpleGraph.Extremal.Basic
 import Mathlib.Combinatorics.SimpleGraph.Finite
 import Mathlib.Combinatorics.SimpleGraph.Finsubgraph
 import Mathlib.Combinatorics.SimpleGraph.Girth
