a24007683513a31ecc76dd0d79e71601b91362da
Junyan Xu <junyanxu.math@gmail.com>
2025-03-08 05:28:03 +0000

    unused import

--- Mathlib/Topology/Homotopy/Lifting.lean
+++ Mathlib/Topology/Homotopy/Lifting.lean
@@ -3,8 +3,8 @@ Copyright (c) 2023 Junyan Xu. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Junyan Xu
 -/
-import Mathlib.AlgebraicTopology.FundamentalGroupoid.FundamentalGroup
 import Mathlib.Topology.Covering
+import Mathlib.Topology.Homotopy.Path
 import Mathlib.Topology.UnitInterval
 /-!
 
