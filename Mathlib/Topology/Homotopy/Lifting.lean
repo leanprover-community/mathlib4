9854ebc6bc11eda642ceafd7998bb7211097222f
Junyan Xu <junyanxu.math@gmail.com>
2025-03-09 15:46:05 +0800

    remove import FundamentalGroup for now

--- Mathlib/Topology/Homotopy/Lifting.lean
+++ Mathlib/Topology/Homotopy/Lifting.lean
@@ -3,7 +3,6 @@ Copyright (c) 2025 Junyan Xu. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Junyan Xu
 -/
-import Mathlib.AlgebraicTopology.FundamentalGroupoid.FundamentalGroup
 import Mathlib.AlgebraicTopology.FundamentalGroupoid.SimplyConnected
 import Mathlib.Topology.Connected.LocPathConnected
 import Mathlib.Topology.Covering
