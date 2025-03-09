0983da0e97bdc1f305cde4ec431bf9d12d27af2a
Markus Himmel <markus@lean-fro.org>
2025-03-09 11:17:01 +0100

    Cleanup, part one

---
+++ Mathlib/CategoryTheory/Abelian/GrothendieckCategory/ModuleEmbedding/GabrielPopescu.lean
@@ -0,0 +1,171 @@
+/-
+Copyright (c) 2025 Markus Himmel. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Markus Himmel
+-/
+import Mathlib.CategoryTheory.Abelian.GrothendieckAxioms.Connected
+import Mathlib.CategoryTheory.Abelian.GrothendieckCategory.Basic
+import Mathlib.CategoryTheory.Abelian.Yoneda
+import Mathlib.CategoryTheory.Preadditive.Injective.Preserves
+import Mathlib.Algebra.Category.ModuleCat.Injective
+import Mathlib.CategoryTheory.Filtered.Connected
+
+/-!
+# The Gabriel-Popescu theorem
+
+We prove the following Gabriel-Popescu theorem: if `C` is a Grothendieck abelian category and
+`G` is a separator, then the functor `preadditiveCoyonedaObj G : C ⥤ ModuleCat (End R)ᵐᵒᵖ` sending
+`X` to `Hom(G, X)` is fully faithful and has an exact left adjoint.
+
+## References
+
+* [Barry Mitchell, *A quick proof of the Gabriel-Popesco theorem*][mitchell1981]
+-/
+
+universe v u
+
+open CategoryTheory Limits
+
+namespace CategoryTheory.Limits
+
+variable {C : Type u} [Category.{v} C] [HasZeroMorphisms C] {X Y : C}
+
+section
+
+variable (f g : X ⟶ Y) (h : f = g) [HasKernel f] [HasKernel g]
+
+/-- Equal maps have isomorphic kernels. -/
+@[simps] noncomputable def kernel.congr : kernel f ≅ kernel g where
+  hom := kernel.lift _ (kernel.ι f) (by simp [← h])
+  inv := kernel.lift _ (kernel.ι g) (by simp [h])
+
+end
+
+section
+
+variable (f : X ⟶ Y) [HasKernel f] [HasImage f] [HasKernel (factorThruImage f)]
+
+/-- The kernel of the morphism `X ⟶ image f` is just the kernel of `f`. -/
+noncomputable def kernelFactorThruImage : kernel (factorThruImage f) ≅ kernel f :=
+  (kernelCompMono (factorThruImage f) (image.ι f)).symm ≪≫ (kernel.congr _ _ (by simp))
+
+@[reassoc (attr := simp)]
+theorem kernelFactorThruImage_hom_comp_ι :
+    (kernelFactorThruImage f).hom ≫ kernel.ι f = kernel.ι (factorThruImage f) := by
+  simp [kernelFactorThruImage]
+
+@[reassoc (attr := simp)]
+theorem kernelFactorThruImage_inv_comp_ι :
+    (kernelFactorThruImage f).inv ≫ kernel.ι (factorThruImage f) = kernel.ι f := by
+  simp [kernelFactorThruImage]
+
+end
+
+
+end CategoryTheory.Limits
+
+namespace CategoryTheory.Abelian
+
+variable {C : Type u} [Category.{v} C] [Abelian C] [IsGrothendieckAbelian.{v} C]
+
+namespace GrothendieckPopescuAux
+
+open CoproductsFromFiniteFiltered
+
+/-- This is the map `⨁ₘ G ⟶ A` induced by `M ⟶ Hom(G, A)`. -/
+noncomputable def d {G A : C} {M : ModuleCat (End G)ᵐᵒᵖ}
+    (g : M ⟶ ModuleCat.of (End G)ᵐᵒᵖ (G ⟶ A)) : ∐ (fun (_ : M) => G) ⟶ A :=
+  Sigma.desc fun (m : M) => g m
+
+@[reassoc]
+theorem ι_d {G A : C} {M : ModuleCat (End G)ᵐᵒᵖ} (g : M ⟶ ModuleCat.of (End G)ᵐᵒᵖ (G ⟶ A)) (m : M) :
+    Sigma.ι _ m ≫ d g = g.hom m := by
+  simp [d]
+
+theorem finiteSubcoproductsCocone_ι_app_preadditive {α : Type v} [DecidableEq α] (f : α → C)
+    [HasCoproduct f]
+    (S : Finset (Discrete α)) :
+    (finiteSubcoproductsCocone f).ι.app S = ∑ a ∈ S.attach, Sigma.π _ a ≫ Sigma.ι _ a.1.as := by
+  dsimp
+  ext v
+  simp only [colimit.ι_desc, Cofan.mk_pt, Cofan.mk_ι_app, Preadditive.comp_sum]
+  rw [Finset.sum_eq_single v]
+  · simp
+  · intro b hb hb₁
+    rw [Sigma.ι_π_of_ne_assoc _ (Ne.symm hb₁), zero_comp]
+  · simp
+
+attribute [local instance] IsFiltered.isConnected in
+/-- This is the "Lemma" in [mitchell1981]. -/
+theorem ι_d_comp_d {G : C} (hG : IsSeparator G) {A B : C} {M : ModuleCat (End G)ᵐᵒᵖ}
+    (g : M ⟶ ModuleCat.of (End G)ᵐᵒᵖ (G ⟶ A)) (hg : Mono g)
+    (f : M ⟶ ModuleCat.of (End G)ᵐᵒᵖ (G ⟶ B)) :
+    kernel.ι (d g) ≫ d f = 0 := by
+  refine (isColimitFiniteSubproductsCocone (fun (_ : M) => G)).pullback_zero_ext (fun F => ?_)
+  dsimp only [liftToFinsetObj_obj, Discrete.functor_obj_eq_as, finiteSubcoproductsCocone_pt,
+    Functor.const_obj_obj]
+  classical
+  rw [finiteSubcoproductsCocone_ι_app_preadditive, ← pullback.condition_assoc]
+  refine (Preadditive.isSeparator_iff G).1 hG _ (fun h => ?_)
+  rw [Preadditive.comp_sum_assoc, Preadditive.comp_sum_assoc, Preadditive.sum_comp]
+  simp only [Category.assoc, ι_d]
+  let r (x : F) : (End G)ᵐᵒᵖ := MulOpposite.op (h ≫ pullback.fst _ _ ≫ Sigma.π _ x)
+  suffices ∑ x ∈ F.attach, r x • f.hom x.1.as = 0 by simpa [End.smul_left, r] using this
+  simp only [← LinearMap.map_smul, ← map_sum]
+  suffices ∑ x ∈ F.attach, r x • x.1.as = 0 by simp [this]
+  simp only [← g.hom.map_eq_zero_iff ((ModuleCat.mono_iff_injective _).1 hg), map_sum, map_smul]
+  simp only [← ι_d g, End.smul_left, MulOpposite.unop_op, Category.assoc, r]
+  simp [← Preadditive.comp_sum, ← Preadditive.sum_comp', pullback.condition_assoc]
+
+theorem exists_d_comp_eq_d {G : C} (hG : IsSeparator G) {A} (B : C) [Injective B]
+    {M : ModuleCat (End G)ᵐᵒᵖ} (g : M ⟶ ModuleCat.of (End G)ᵐᵒᵖ (G ⟶ A)) (hg : Mono g)
+    (f : M ⟶ ModuleCat.of (End G)ᵐᵒᵖ (G ⟶ B)) : ∃ (l : A ⟶ B), d g ≫ l = d f := by
+  let l₁ : image (d g) ⟶ B := epiDesc (factorThruImage (d g)) (d f) (by
+    rw [← kernelFactorThruImage_hom_comp_ι, Category.assoc, ι_d_comp_d hG _ hg, comp_zero])
+  let l₂ : A ⟶ B := Injective.factorThru l₁ (Limits.image.ι (d g))
+  refine ⟨l₂, ?_⟩
+  simp only [l₂, l₁]
+  conv_lhs => congr; rw [← Limits.image.fac (d g)]
+  simp [-Limits.image.fac]
+
+end GrothendieckPopescuAux
+
+open GrothendieckPopescuAux
+
+theorem full (G : C) (hG : IsSeparator G) : (preadditiveCoyonedaObj G).Full where
+  map_surjective {A B} f := by
+    have := (isSeparator_iff_epi G).1 hG A
+    have := ι_d_comp_d hG (𝟙 _) inferInstance f
+    simp only [ModuleCat.hom_id, LinearMap.id_coe, id_eq, d] at this
+    refine ⟨Abelian.epiDesc _ _ this, ?_⟩
+    ext q
+    simpa [-Abelian.comp_epiDesc] using Sigma.ι _ q ≫= comp_epiDesc _ _ this
+
+/-- `G ⟶ G` and `(End G)ᵐᵒᵖ` are isomorphic as `(End G)ᵐᵒᵖ`-modules. -/
+@[simps]
+def linearEquiv (G : C) : (G ⟶ G) ≃ₗ[(End G)ᵐᵒᵖ] (End G)ᵐᵒᵖ where
+  toFun f := ⟨f⟩
+  map_add' := by aesop_cat
+  map_smul' := by aesop_cat
+  invFun := fun ⟨f⟩ => f
+  left_inv := by aesop_cat
+  right_inv := by aesop_cat
+
+theorem preserves_injectives (G : C) (hG : IsSeparator G) :
+    (preadditiveCoyonedaObj G).PreservesInjectiveObjects where
+  injective_obj {B} hB := by
+    rw [← Module.injective_iff_injective_object]
+    simp only [preadditiveCoyonedaObj_obj_carrier, preadditiveCoyonedaObj_obj_isAddCommGroup,
+      preadditiveCoyonedaObj_obj_isModule]
+    apply Module.Baer.injective
+    intro M g
+    have := exists_d_comp_eq_d hG B (ModuleCat.ofHom
+      ⟨⟨fun i => i.1.unop, by aesop_cat⟩, by aesop_cat⟩) ?_ (ModuleCat.ofHom g)
+    · obtain ⟨l, hl⟩ := this
+      refine ⟨((preadditiveCoyonedaObj G).map l).hom ∘ₗ (linearEquiv G).symm.toLinearMap, ?_⟩
+      intro f hf
+      simpa [d] using Sigma.ι _ ⟨f, hf⟩ ≫= hl
+    · rw [ModuleCat.mono_iff_injective]
+      aesop_cat
+
+end CategoryTheory.Abelian
