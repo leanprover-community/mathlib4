e1db90fb79bda77ed1d395156a5cca3289eddd7f
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-03-09 21:03:36 +0100

    fix

--- Mathlib/CategoryTheory/Localization/CalculusOfFractions/OfAdjunction.lean
+++ Mathlib/CategoryTheory/Localization/CalculusOfFractions/OfAdjunction.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joël Riou
 -/
 import Mathlib.CategoryTheory.Adjunction.Opposites
-import Mathlib.CategoryTheory.Localization.Adjunction
+import Mathlib.CategoryTheory.Adjunction.FullyFaithful
 import Mathlib.CategoryTheory.Localization.CalculusOfFractions
 
 /-!
