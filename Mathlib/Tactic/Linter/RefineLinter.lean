77ffd776b7fb2a9fd4e4d96c32245eeed92e6924
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-03-09 09:08:12 +0100

    Re-instate comment about importing the header linter.

--- Mathlib/Tactic/Linter/RefineLinter.lean
+++ Mathlib/Tactic/Linter/RefineLinter.lean
@@ -4,6 +4,8 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Damiano Testa
 -/
 import Lean.Elab.Command
+-- Import this linter explicitly to ensure that
+-- this file has a valid copyright header and module docstring.
 import Mathlib.Tactic.Linter.Header
 
 /-!
