93d9eb0e28211f391d44b0669390f08b198df846
adomani <adomani@gmail.com>
2025-03-10 13:00:52 +0100

    add tests

--- MathlibTest/PPRoundtrip.lean
+++ MathlibTest/PPRoundtrip.lean
@@ -83,3 +83,42 @@ note: this linter can be disabled with `set_option linter.style.commandStart fal
 -/
 #guard_msgs in
 example {a :Nat} : a = a := rfl
+
+/--
+warning: Current syntax:  'mple  {a :'
+Expected syntax: 'mple {a : '
+
+note: this linter can be disabled with `set_option linter.style.commandStart false`
+-/
+#guard_msgs in
+example  {a :Nat} : a = a := rfl
+
+/--
+warning: unused variable `b`
+note: this linter can be disabled with `set_option linter.unusedVariables false`
+---
+warning: Current syntax:  ' Nat}{b : '
+Expected syntax: ' Nat} {b :'
+
+note: this linter can be disabled with `set_option linter.style.commandStart false`
+-/
+#guard_msgs in
+example {a : Nat}{b : Nat} : a = a := rfl
+
+/--
+warning: Current syntax:  'Nat}  '
+Expected syntax: 'Nat} : a ='
+
+note: this linter can be disabled with `set_option linter.style.commandStart false`
+-/
+#guard_msgs in
+example {a : Nat}  : a = a := rfl
+
+/--
+warning: Current syntax:  'alpha   ] '
+Expected syntax: 'alpha] {a '
+
+note: this linter can be disabled with `set_option linter.style.commandStart false`
+-/
+#guard_msgs in
+example {alpha} [Neg alpha   ] {a : Nat} : a = a := rfl
