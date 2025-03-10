521ce5b11364dbc18590b4dfae5a6c475ea87b50
tukamilano <107251402+tukamilano@users.noreply.github.com>
2025-03-10 18:58:41 +0900

    Update Hoeffding.lean

--- Mathlib/Probability/Hoeffding.lean
+++ Mathlib/Probability/Hoeffding.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2024 Kei Tsukamoto. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Kei Tsukamoto
+Authors: Kei Tsukamoto, Kazumi Kasaura, Yuma Mizuno, Naoto Onda, Sho Sonoda
 -/
 
 import Mathlib.Probability.Moments.Basic
