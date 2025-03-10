046487689130713af7fdceffd0e878eca50bf115
mitchell-horner <29882987+mitchell-horner@users.noreply.github.com>
2025-03-11 01:03:01 +1100

    Remove private lemma

--- Mathlib/Analysis/Convex/Piecewise.lean
+++ Mathlib/Analysis/Convex/Piecewise.lean
@@ -84,12 +84,6 @@ theorem convexOn_univ_piecewise_Iic_of_antitoneOn_Iic_monotoneOn_Ici
       (Set.Iic e).piecewise_eq_of_not_mem f g (Set.not_mem_Iic.mpr hc)]
     exact hg.2 hx.le hy.le ha hb hab
 
-omit [OrderedAddCommGroup β] in
-private lemma piecewise_Ici_eq_piecewise_Iic_of_eq (h_eq : f e = g e) :
-    (Set.Ici e).piecewise f g = (Set.Iic e).piecewise g f := by
-  ext x; by_cases hx : x = e
-  all_goals simp [Set.piecewise, @le_iff_lt_or_eq _ _ _ e, ← @ite_not _ (e ≤ _), hx, h_eq]
-
 /-- The piecewise function `(Set.Ici e).piecewise f g` of a function `f` increasing and convex on
 `Set.Ici e` and a function `g` decreasing and convex on `Set.Iic e`, such that `f e = g e`, is
 convex on the universal set. -/
@@ -97,7 +91,11 @@ theorem convexOn_univ_piecewise_Ici_of_monotoneOn_Ici_antitoneOn_Iic
     (hf : ConvexOn 𝕜 (Set.Ici e) f) (hg : ConvexOn 𝕜 (Set.Iic e) g)
     (h_mono : MonotoneOn f (Set.Ici e)) (h_anti : AntitoneOn g (Set.Iic e)) (h_eq : f e = g e) :
     ConvexOn 𝕜 Set.univ ((Set.Ici e).piecewise f g) := by
-  rw [piecewise_Ici_eq_piecewise_Iic_of_eq h_eq]
+  have h_piecewise_Ici_eq_piecewise_Iic :
+      (Set.Ici e).piecewise f g = (Set.Iic e).piecewise g f := by
+    ext x; by_cases hx : x = e
+      <;> simp [Set.piecewise, @le_iff_lt_or_eq _ _ x e, ← @ite_not _ (e ≤ _), hx, h_eq]
+  rw [h_piecewise_Ici_eq_piecewise_Iic]
   exact convexOn_univ_piecewise_Iic_of_antitoneOn_Iic_monotoneOn_Ici hg hf h_anti h_mono h_eq.symm
 
 /-- The piecewise function `(Set.Iic e).piecewise f g` of a function `f` increasing and concave on
