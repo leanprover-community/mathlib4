8f3bec869f4a8efc9293d2d4e3cd989734a55652
acmepjz <acme_pjz@hotmail.com>
2025-03-10 15:30:00 +0800

    add scripts to test if there are keys in references.bib with non-ASCII characters

--- scripts/lint-bib.sh
+++ scripts/lint-bib.sh
@@ -6,6 +6,21 @@ set -euo pipefail
 IFS=$'\n\t'
 
 set -x
+
+# test if there are keys with non-ASCII characters
+bibtool --pass.comments=on -- 'select{$key "[^-.:A-Za-z0-9_]+"}' \
+  docs/references.bib -o docs/non-ascii.bib
+
+if [ ! -s docs/non-ascii.bib ]; then
+    echo "ERROR: There are items in references.bib with keys containing non-ASCII characters:"
+    echo
+    cat docs/non-ascii.bib
+    rm docs/non-ascii.bib
+    exit 1
+else
+    rm docs/non-ascii.bib
+fi
+
 # https://leanprover-community.github.io/contribute/doc.html#citing-other-works
 cp docs/references.bib docs/references.bib.old
 bibtool --preserve.key.case=on --preserve.keys=on --pass.comments=on --print.use.tab=off -s \
