44b4f1272191eae5ca1c6bae53da7fc85d127a64
Eric Wieser <wieser.eric@gmail.com>
2025-03-11 01:03:21 +0000

    chore(RingTheory/PolynomialAlgebra): split
    This moves the Matrix / Polynomial interaction to a new file.

--- Mathlib.lean
+++ Mathlib.lean
@@ -4941,6 +4941,7 @@ import Mathlib.RingTheory.Localization.NumDen
 import Mathlib.RingTheory.Localization.Pi
 import Mathlib.RingTheory.Localization.Submodule
 import Mathlib.RingTheory.MatrixAlgebra
+import Mathlib.RingTheory.MatrixPolynomialAlgebra
 import Mathlib.RingTheory.Morita.Basic
 import Mathlib.RingTheory.Multiplicity
 import Mathlib.RingTheory.MvPolynomial
