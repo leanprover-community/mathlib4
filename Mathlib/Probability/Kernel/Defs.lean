625b7e8b8af9f78c88d55cd7353b738190787ba9
adomani <adomani@gmail.com>
2025-03-11 19:35:01 +0000

    another pp space

--- Mathlib/Probability/Kernel/Defs.lean
+++ Mathlib/Probability/Kernel/Defs.lean
@@ -60,7 +60,7 @@ structure Kernel (α β : Type*) [MeasurableSpace α] [MeasurableSpace β] where
   measurable' : Measurable toFun
 
 /-- Notation for `Kernel` with respect to a non-standard σ-algebra in the domain. -/
-scoped notation "Kernel[" mα "]" α:arg β:arg => @Kernel α β mα _
+scoped notation "Kernel[" mα "] " α:arg β:arg => @Kernel α β mα _
 
 /-- Notation for `Kernel` with respect to a non-standard σ-algebra in the domain and codomain. -/
 scoped notation "Kernel[" mα ", " mβ "] " α:arg β:arg => @Kernel α β mα mβ
