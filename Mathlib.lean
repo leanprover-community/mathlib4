35530f08b1a997d49149dfab7f10c9bef43ebc9d
mitchell-horner <29882987+mitchell-horner@users.noreply.github.com>
2025-03-11 01:14:52 +0000

    feat(Analysis/SpecialFunctions): prove special case of Jensen's inequality for binomial coefficients (#20738)
    Prove special case of Jensen's inequality for binomial coefficients using the descending Pochhammer polynomials.
    
    Also prove
    
    - special case of Jensen's inequality for the descending factorial using the descending Pochhammer polynomials,
    - the descending Pochhammer polynomials `descPochhamer · n` are monotone/convex on $$[n-1, \infty)$$,
    - misc. analysis/ring theory lemmas for the descending Pochhammer polynomials.
    
    
    
    Co-authored-by: Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1425,6 +1425,7 @@ import Mathlib.Analysis.Convex.KreinMilman
 import Mathlib.Analysis.Convex.Measure
 import Mathlib.Analysis.Convex.Mul
 import Mathlib.Analysis.Convex.PartitionOfUnity
+import Mathlib.Analysis.Convex.Piecewise
 import Mathlib.Analysis.Convex.Quasiconvex
 import Mathlib.Analysis.Convex.Radon
 import Mathlib.Analysis.Convex.Segment
@@ -1697,6 +1698,7 @@ import Mathlib.Analysis.SpecialFunctions.MulExpNegMulSq
 import Mathlib.Analysis.SpecialFunctions.MulExpNegMulSqIntegral
 import Mathlib.Analysis.SpecialFunctions.NonIntegrable
 import Mathlib.Analysis.SpecialFunctions.OrdinaryHypergeometric
+import Mathlib.Analysis.SpecialFunctions.Pochhammer
 import Mathlib.Analysis.SpecialFunctions.PolarCoord
 import Mathlib.Analysis.SpecialFunctions.PolynomialExp
 import Mathlib.Analysis.SpecialFunctions.Pow.Asymptotics
