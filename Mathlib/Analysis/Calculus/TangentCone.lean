810e075c98ea5dacd95c109cccfbd8c6751f472b
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2025-03-12 12:02:10 +0100

    start splitting files

--- Mathlib/Analysis/Calculus/TangentCone.lean
+++ Mathlib/Analysis/Calculus/TangentCone.lean
@@ -4,9 +4,9 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Sébastien Gouëzel
 -/
 import Mathlib.Analysis.Convex.Topology
-import Mathlib.Analysis.Normed.Module.Basic
 import Mathlib.Analysis.Seminorm
 import Mathlib.Analysis.SpecificLimits.Basic
+import Mathlib.Analysis.Calculus.TangentConeDef
 
 /-!
 # Tangent cone
@@ -39,37 +39,6 @@ open Filter Set
 
 open Topology
 
-section TangentCone
-
-variable {E : Type*} [AddCommMonoid E] [Module 𝕜 E] [TopologicalSpace E]
-
-/-- The set of all tangent directions to the set `s` at the point `x`. -/
-def tangentConeAt (s : Set E) (x : E) : Set E :=
-  { y : E | ∃ (c : ℕ → 𝕜) (d : ℕ → E),
-    (∀ᶠ n in atTop, x + d n ∈ s) ∧
-    Tendsto (fun n => ‖c n‖) atTop atTop ∧
-    Tendsto (fun n => c n • d n) atTop (𝓝 y) }
-
-/-- A property ensuring that the tangent cone to `s` at `x` spans a dense subset of the whole space.
-The main role of this property is to ensure that the differential within `s` at `x` is unique,
-hence this name. The uniqueness it asserts is proved in `UniqueDiffWithinAt.eq` in
-`Mathlib.Analysis.Calculus.FDeriv.Basic`.
-To avoid pathologies in dimension 0, we also require that `x` belongs to the closure of `s` (which
-is automatic when `E` is not `0`-dimensional). -/
-@[mk_iff]
-structure UniqueDiffWithinAt (s : Set E) (x : E) : Prop where
-  dense_tangentCone : Dense (Submodule.span 𝕜 (tangentConeAt 𝕜 s x) : Set E)
-  mem_closure : x ∈ closure s
-
-/-- A property ensuring that the tangent cone to `s` at any of its points spans a dense subset of
-the whole space. The main role of this property is to ensure that the differential along `s` is
-unique, hence this name. The uniqueness it asserts is proved in `UniqueDiffOn.eq` in
-`Mathlib.Analysis.Calculus.FDeriv.Basic`. -/
-def UniqueDiffOn (s : Set E) : Prop :=
-  ∀ x ∈ s, UniqueDiffWithinAt 𝕜 s x
-
-end TangentCone
-
 variable {𝕜}
 variable {E F G : Type*}
 
@@ -78,27 +47,6 @@ section TangentCone
 -- This section is devoted to the properties of the tangent cone.
 
 open NormedField
-section TVS
-variable [AddCommGroup E] [Module 𝕜 E] [TopologicalSpace E]
-variable {x y : E} {s t : Set E}
-
-theorem mem_tangentConeAt_of_pow_smul {r : 𝕜} (hr₀ : r ≠ 0) (hr : ‖r‖ < 1)
-    (hs : ∀ᶠ n : ℕ in atTop, x + r ^ n • y ∈ s) : y ∈ tangentConeAt 𝕜 s x := by
-  refine ⟨fun n ↦ (r ^ n)⁻¹, fun n ↦ r ^ n • y, hs, ?_, ?_⟩
-  · simp only [norm_inv, norm_pow, ← inv_pow]
-    exact tendsto_pow_atTop_atTop_of_one_lt <| (one_lt_inv₀ (norm_pos_iff.2 hr₀)).2 hr
-  · simp only [inv_smul_smul₀ (pow_ne_zero _ hr₀), tendsto_const_nhds]
-
-theorem tangentCone_univ : tangentConeAt 𝕜 univ x = univ :=
-  let ⟨_r, hr₀, hr⟩ := exists_norm_lt_one 𝕜
-  eq_univ_of_forall fun _ ↦ mem_tangentConeAt_of_pow_smul (norm_pos_iff.1 hr₀) hr <|
-    Eventually.of_forall fun _ ↦ mem_univ _
-
-theorem tangentCone_mono (h : s ⊆ t) : tangentConeAt 𝕜 s x ⊆ tangentConeAt 𝕜 t x := by
-  rintro y ⟨c, d, ds, ctop, clim⟩
-  exact ⟨c, d, mem_of_superset ds fun n hn => h hn, ctop, clim⟩
-
-end TVS
 
 section Normed
 variable [NormedAddCommGroup E] [NormedSpace 𝕜 E]
@@ -357,29 +305,6 @@ section UniqueDiff
 ### Properties of `UniqueDiffWithinAt` and `UniqueDiffOn`
 
 This section is devoted to properties of the predicates `UniqueDiffWithinAt` and `UniqueDiffOn`. -/
-
-section TVS
-variable [AddCommGroup E] [Module 𝕜 E] [TopologicalSpace E]
-variable {x y : E} {s t : Set E}
-
-theorem UniqueDiffOn.uniqueDiffWithinAt {s : Set E} {x} (hs : UniqueDiffOn 𝕜 s) (h : x ∈ s) :
-    UniqueDiffWithinAt 𝕜 s x :=
-  hs x h
-
-theorem uniqueDiffWithinAt_univ : UniqueDiffWithinAt 𝕜 univ x := by
-  rw [uniqueDiffWithinAt_iff, tangentCone_univ]
-  simp
-
-theorem uniqueDiffOn_univ : UniqueDiffOn 𝕜 (univ : Set E) :=
-  fun _ _ => uniqueDiffWithinAt_univ
-
-theorem uniqueDiffOn_empty : UniqueDiffOn 𝕜 (∅ : Set E) :=
-  fun _ hx => hx.elim
-
-theorem UniqueDiffWithinAt.congr_pt (h : UniqueDiffWithinAt 𝕜 s x) (hy : x = y) :
-    UniqueDiffWithinAt 𝕜 s y := hy ▸ h
-
-end TVS
 
 section Normed
 variable [NormedAddCommGroup E] [NormedSpace 𝕜 E]
---
+++ Mathlib/Analysis/Calculus/TangentConeDef.lean
@@ -0,0 +1,109 @@
+/-
+Copyright (c) 2019 Sébastien Gouëzel. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Sébastien Gouëzel
+-/
+import Mathlib.Analysis.SpecificLimits.Basic
+import Mathlib.Analysis.Normed.Field.Lemmas
+
+/-!
+# Tangent cone
+
+In this file, we define two predicates `UniqueDiffWithinAt 𝕜 s x` and `UniqueDiffOn 𝕜 s`
+ensuring that, if a function has two derivatives, then they have to coincide. As a direct
+definition of this fact (quantifying on all target types and all functions) would depend on
+universes, we use a more intrinsic definition: if all the possible tangent directions to the set
+`s` at the point `x` span a dense subset of the whole subset, it is easy to check that the
+derivative has to be unique.
+
+Therefore, we introduce the set of all tangent directions, named `tangentConeAt`,
+and express `UniqueDiffWithinAt` and `UniqueDiffOn` in terms of it.
+One should however think of this definition as an implementation detail: the only reason to
+introduce the predicates `UniqueDiffWithinAt` and `UniqueDiffOn` is to ensure the uniqueness
+of the derivative. This is why their names reflect their uses, and not how they are defined.
+
+## Implementation details
+
+Note that this file is imported by `Mathlib.Analysis.Calculus.FDeriv.Basic`. Hence, derivatives are
+not defined yet. The property of uniqueness of the derivative is therefore proved in
+`Mathlib.Analysis.Calculus.FDeriv.Basic`, but based on the properties of the tangent cone we prove
+here.
+-/
+
+assert_not_exists NormedSpace
+
+variable (𝕜 : Type*) [NontriviallyNormedField 𝕜]
+
+open Filter Set
+
+open Topology
+
+variable {E : Type*} [AddCommMonoid E] [Module 𝕜 E] [TopologicalSpace E]
+
+/-- The set of all tangent directions to the set `s` at the point `x`. -/
+def tangentConeAt (s : Set E) (x : E) : Set E :=
+  { y : E | ∃ (c : ℕ → 𝕜) (d : ℕ → E),
+    (∀ᶠ n in atTop, x + d n ∈ s) ∧
+    Tendsto (fun n => ‖c n‖) atTop atTop ∧
+    Tendsto (fun n => c n • d n) atTop (𝓝 y) }
+
+/-- A property ensuring that the tangent cone to `s` at `x` spans a dense subset of the whole space.
+The main role of this property is to ensure that the differential within `s` at `x` is unique,
+hence this name. The uniqueness it asserts is proved in `UniqueDiffWithinAt.eq` in
+`Mathlib.Analysis.Calculus.FDeriv.Basic`.
+To avoid pathologies in dimension 0, we also require that `x` belongs to the closure of `s` (which
+is automatic when `E` is not `0`-dimensional). -/
+@[mk_iff]
+structure UniqueDiffWithinAt (s : Set E) (x : E) : Prop where
+  dense_tangentCone : Dense (Submodule.span 𝕜 (tangentConeAt 𝕜 s x) : Set E)
+  mem_closure : x ∈ closure s
+
+/-- A property ensuring that the tangent cone to `s` at any of its points spans a dense subset of
+the whole space. The main role of this property is to ensure that the differential along `s` is
+unique, hence this name. The uniqueness it asserts is proved in `UniqueDiffOn.eq` in
+`Mathlib.Analysis.Calculus.FDeriv.Basic`. -/
+def UniqueDiffOn (s : Set E) : Prop :=
+  ∀ x ∈ s, UniqueDiffWithinAt 𝕜 s x
+
+variable {𝕜}
+variable {E F G : Type*}
+
+-- This section is devoted to the properties of the tangent cone.
+
+open NormedField
+
+variable [AddCommGroup E] [Module 𝕜 E] [TopologicalSpace E]
+variable {x y : E} {s t : Set E}
+
+theorem mem_tangentConeAt_of_pow_smul {r : 𝕜} (hr₀ : r ≠ 0) (hr : ‖r‖ < 1)
+    (hs : ∀ᶠ n : ℕ in atTop, x + r ^ n • y ∈ s) : y ∈ tangentConeAt 𝕜 s x := by
+  refine ⟨fun n ↦ (r ^ n)⁻¹, fun n ↦ r ^ n • y, hs, ?_, ?_⟩
+  · simp only [norm_inv, norm_pow, ← inv_pow]
+    exact tendsto_pow_atTop_atTop_of_one_lt <| (one_lt_inv₀ (norm_pos_iff.2 hr₀)).2 hr
+  · simp only [inv_smul_smul₀ (pow_ne_zero _ hr₀), tendsto_const_nhds]
+
+theorem tangentCone_univ : tangentConeAt 𝕜 univ x = univ :=
+  let ⟨_r, hr₀, hr⟩ := exists_norm_lt_one 𝕜
+  eq_univ_of_forall fun _ ↦ mem_tangentConeAt_of_pow_smul (norm_pos_iff.1 hr₀) hr <|
+    Eventually.of_forall fun _ ↦ mem_univ _
+
+theorem tangentCone_mono (h : s ⊆ t) : tangentConeAt 𝕜 s x ⊆ tangentConeAt 𝕜 t x := by
+  rintro y ⟨c, d, ds, ctop, clim⟩
+  exact ⟨c, d, mem_of_superset ds fun n hn => h hn, ctop, clim⟩
+
+theorem UniqueDiffOn.uniqueDiffWithinAt {s : Set E} {x} (hs : UniqueDiffOn 𝕜 s) (h : x ∈ s) :
+    UniqueDiffWithinAt 𝕜 s x :=
+  hs x h
+
+theorem uniqueDiffWithinAt_univ : UniqueDiffWithinAt 𝕜 univ x := by
+  rw [uniqueDiffWithinAt_iff, tangentCone_univ]
+  simp
+
+theorem uniqueDiffOn_univ : UniqueDiffOn 𝕜 (univ : Set E) :=
+  fun _ _ => uniqueDiffWithinAt_univ
+
+theorem uniqueDiffOn_empty : UniqueDiffOn 𝕜 (∅ : Set E) :=
+  fun _ hx => hx.elim
+
+theorem UniqueDiffWithinAt.congr_pt (h : UniqueDiffWithinAt 𝕜 s x) (hy : x = y) :
+    UniqueDiffWithinAt 𝕜 s y := hy ▸ h
