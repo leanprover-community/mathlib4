f9548a83a669a6647b9b15e4dfef46fea8454f00
Jireh Loreaux <loreaujy@gmail.com>
2025-03-13 10:00:00 -0500

    fix imports

--- Mathlib/Topology/ContinuousMap/StarOrdered.lean
+++ Mathlib/Topology/ContinuousMap/StarOrdered.lean
@@ -3,8 +3,9 @@ Copyright (c) 2024 Jireh Loreaux. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jireh Loreaux
 -/
+import Mathlib.Algebra.Order.Star.Basic
 import Mathlib.Topology.ContinuousMap.ContinuousMapZero
-import Mathlib.Topology.ContinuousMap.Lattice
+import Mathlib.Topology.ContinuousMap.Ordered
 
 /-! # Continuous functions as a star-ordered ring
 
