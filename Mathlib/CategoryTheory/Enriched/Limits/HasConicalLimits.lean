f0162273b8f7ab96131cc89246de20900ff4d492
Jon Eugster <eugster.jon@gmail.com>
2025-03-14 11:06:27 +0100

    fix style

--- Mathlib/CategoryTheory/Enriched/Limits/HasConicalLimits.lean
+++ Mathlib/CategoryTheory/Enriched/Limits/HasConicalLimits.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jon Eugster, Dagur Asgeirsson, Emily Riehl
 -/
 import Mathlib.CategoryTheory.Enriched.Ordinary.Basic
-import Mathlib.CategoryTheory.Limits.Preserves.Limits
 
 /-!
 # HasConicalLimits
