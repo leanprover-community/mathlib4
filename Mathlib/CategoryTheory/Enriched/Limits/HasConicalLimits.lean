86fccca6542ba66e6e0d04fa0bf6c54eb17307e9
Jon Eugster <eugster.jon@gmail.com>
2025-03-14 19:17:37 +0100

    remove comment

--- Mathlib/CategoryTheory/Enriched/Limits/HasConicalLimits.lean
+++ Mathlib/CategoryTheory/Enriched/Limits/HasConicalLimits.lean
@@ -124,7 +124,6 @@ end HasConicalLimitsOfSize
 end Results
 namespace HasConicalLimits
 
--- Note that `Category.{v, v} J` is deliberately chosen this way, see `HasConicalLimits`.
 variable (J : Type v) [SmallCategory J]
 variable (V : Type u') [Category.{v'} V] [MonoidalCategory V]
 variable (C : Type u) [Category.{v} C] [EnrichedOrdinaryCategory V C]
