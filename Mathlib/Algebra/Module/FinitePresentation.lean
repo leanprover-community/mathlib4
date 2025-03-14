8e4f0365f89875c39615b65b3ef7f1925b8c9ba1
Christian Merten <christian@merten.dev>
2025-03-14 11:16:19 +0100

    pi finite presentation

--- Mathlib/Algebra/Module/FinitePresentation.lean
+++ Mathlib/Algebra/Module/FinitePresentation.lean
@@ -53,6 +53,8 @@ For finitely presented algebras, see `Algebra.FinitePresentation`
 in file `Mathlib.RingTheory.FinitePresentation`.
 -/
 
+universe u v
+
 open Finsupp
 
 section Semiring
@@ -76,7 +78,6 @@ section Ring
 
 section
 
-universe u v
 variable (R : Type u) (M : Type*) [Ring R] [AddCommGroup M] [Module R M]
 
 theorem Module.FinitePresentation.exists_fin [fp : Module.FinitePresentation R M] :
@@ -270,12 +271,68 @@ lemma Module.finitePresentation_of_projective_of_exact
   have ⟨l, hl⟩ := Module.projective_lifting_property g .id hg
   Module.finitePresentation_of_split_exact f g l hl hf H
 
+namespace Module.FinitePresentation
+
+lemma of_equiv (e : M ≃ₗ[R] N) [Module.FinitePresentation R M] :
+    Module.FinitePresentation R N := by
+  simp [← Module.FinitePresentation.fg_ker_iff e.toLinearMap e.surjective, Submodule.fg_bot]
+
+variable (M) in
+instance (priority := 900) of_subsingleton [Subsingleton M] :
+    Module.FinitePresentation R M :=
+  .of_equiv (default : (Fin 0 → R) ≃ₗ[R] M)
+
+variable (M N) in
+instance prod [Module.FinitePresentation R M] [Module.FinitePresentation R N] :
+    Module.FinitePresentation R (M × N) := by
+  have hf : Function.Surjective (LinearMap.fst R M N) := LinearMap.fst_surjective
+  have : FinitePresentation R ↥(LinearMap.ker (LinearMap.fst R M N)) := by
+    rw [LinearMap.ker_fst]
+    exact .of_equiv (LinearEquiv.ofInjective (LinearMap.inr R M N) LinearMap.inr_injective)
+  apply Module.finitePresentation_of_ker (.fst R M N) hf
+
+private lemma pi_aux {ι : Type} [Finite ι] (M : ι → Type*) [∀ i, AddCommGroup (M i)]
+    [∀ i, Module R (M i)] [∀ i, Module.FinitePresentation R (M i)] :
+    Module.FinitePresentation R (∀ i, M i) := by
+  refine Module.pi_induction' (motive := fun N _ _ ↦ Module.FinitePresentation R N) R ?_ ?_ ?_ M
+      inferInstance
+  · intro N N' _ _ _ _ e hN
+    exact Module.FinitePresentation.of_equiv e
+  · infer_instance
+  · introv hN hN'
+    infer_instance
+
+instance pi {ι : Type*} (M : ι → Type*)
+    [∀ i, AddCommGroup (M i)] [∀ i, Module R (M i)] [∀ i, Module.FinitePresentation R (M i)]
+    [Finite ι] : Module.FinitePresentation R (∀ i, M i) := by
+  cases nonempty_fintype ι
+  convert Module.FinitePresentation.of_equiv (LinearEquiv.piCongrLeft R M (Fintype.equivFin ι).symm)
+  apply Module.FinitePresentation.pi_aux
+
+end Module.FinitePresentation
+
 end Ring
 
 section CommRing
 
 variable {R M N N'} [CommRing R] [AddCommGroup M] [Module R M] [AddCommGroup N] [Module R N]
 variable [AddCommGroup N'] [Module R N'] (S : Submonoid R) (f : N →ₗ[R] N') [IsLocalizedModule S f]
+
+variable (R M) in
+lemma Module.FinitePresentation.trans (S : Type*) [CommRing S] [Algebra R S]
+    [Module S M] [IsScalarTower R S M] [Module.FinitePresentation R S]
+    [Module.FinitePresentation S M] : Module.FinitePresentation R M := by
+  obtain ⟨n, K, e, hK⟩ := Module.FinitePresentation.exists_fin S M
+  let f : (Fin n → S) →ₗ[R] M := (e.symm ∘ₗ K.mkQ).restrictScalars R
+  refine Module.finitePresentation_of_surjective f (fun m ↦ ?_) ?_
+  · obtain ⟨a, ha⟩ := K.mkQ_surjective (e m)
+    exact ⟨a, by simp [f, ha]⟩
+  · have : Module.Finite S
+        (Submodule.restrictScalars R (LinearMap.ker (e.symm.toLinearMap ∘ₗ K.mkQ))) := by
+      show Module.Finite S (LinearMap.ker (e.symm.toLinearMap ∘ₗ K.mkQ))
+      simpa [Finite.iff_fg]
+    simp only [f, LinearMap.ker_restrictScalars, ← Module.Finite.iff_fg]
+    exact Module.Finite.trans S _
 
 open TensorProduct in
 instance {A} [CommRing A] [Algebra R A] [Module.FinitePresentation R M] :
--- Mathlib/LinearAlgebra/Pi.lean
+++ Mathlib/LinearAlgebra/Pi.lean
@@ -12,6 +12,7 @@ import Mathlib.Algebra.Module.Submodule.Range
 import Mathlib.Algebra.Module.Equiv.Basic
 import Mathlib.Logic.Equiv.Fin
 import Mathlib.LinearAlgebra.Prod
+import Mathlib.Data.Fintype.Option
 
 /-!
 # Pi types of modules
@@ -636,6 +637,33 @@ theorem LinearMap.vecCons_apply {n} (f : M →ₗ[R] M₂) (g : M →ₗ[R] Fin 
     f.vecCons g m = Matrix.vecCons (f m) (g m) :=
   rfl
 
+variable (R) in
+/--
+To show a property `P` of modules holds for arbitrary finite products of modules, it suffices
+to show
+1. `P` is stable under isomorphism.
+2. `P` holds for the zero module.
+3. `P` holds for `M × N` if it holds for both `M` and `N`.
+
+See `Module.pi_induction'` for a version where `motive` assumes `AddCommGroup` instead.
+-/
+@[elab_as_elim]
+lemma Module.pi_induction {ι : Type} [Finite ι]
+    (motive : ∀ (N : Type u) [AddCommMonoid N] [Module R N], Prop)
+    (equiv : ∀ {N N' : Type u} [AddCommMonoid N] [AddCommMonoid N']
+      [Module R N] [Module R N'], (N ≃ₗ[R] N') → motive N → motive N')
+    (unit : motive PUnit) (prod : ∀ {N N' : Type u} [AddCommMonoid N] [AddCommMonoid N']
+      [Module R N] [Module R N'], motive N → motive N' → motive (N × N'))
+    (M : ι → Type u) [∀ i, AddCommMonoid (M i)] [∀ i, Module R (M i)]
+    (h : ∀ i, motive (M i)) : motive (∀ i, M i) := by
+  classical
+  cases nonempty_fintype ι
+  revert M
+  refine Fintype.induction_empty_option
+    (fun α β _ e h M _ _ hM ↦ equiv (LinearEquiv.piCongrLeft R M e) <| h _ fun i ↦ hM _)
+    (fun M _ _ _ ↦ equiv default unit) (fun α _ h M _ _ hn ↦ ?_) ι
+  exact equiv (LinearEquiv.piOptionEquivProd R).symm <| prod (hn _) (h _ fun i ↦ hn i)
+
 end Semiring
 
 section CommSemiring
@@ -663,5 +691,23 @@ def LinearMap.vecCons₂ {n} (f : M →ₗ[R] M₂ →ₗ[R] M₃) (g : M →ₗ
   map_smul' r x := LinearMap.ext fun z => by simp [Matrix.smul_cons r (f x z)]
 
 end CommSemiring
+
+/-- A variant of `Module.pi_induction` that assumes `AddCommGroup` instead of `AddCommMonoid`. -/
+@[elab_as_elim]
+lemma Module.pi_induction' {ι : Type} [Finite ι] (R : Type*) [Ring R]
+    (motive : ∀ (N : Type u) [AddCommGroup N] [Module R N], Prop)
+    (equiv : ∀ {N N' : Type u} [AddCommGroup N] [AddCommGroup N']
+      [Module R N] [Module R N'], (N ≃ₗ[R] N') → motive N → motive N')
+    (unit : motive PUnit) (prod : ∀ {N N' : Type u} [AddCommGroup N] [AddCommGroup N']
+      [Module R N] [Module R N'], motive N → motive N' → motive (N × N'))
+    (M : ι → Type u) [∀ i, AddCommGroup (M i)] [∀ i, Module R (M i)]
+    (h : ∀ i, motive (M i)) : motive (∀ i, M i) := by
+  classical
+  cases nonempty_fintype ι
+  revert M
+  refine Fintype.induction_empty_option
+    (fun α β _ e h M _ _ hM ↦ equiv (LinearEquiv.piCongrLeft R M e) <| h _ fun i ↦ hM _)
+    (fun M _ _ _ ↦ equiv default unit) (fun α _ h M _ _ hn ↦ ?_) ι
+  exact equiv (LinearEquiv.piOptionEquivProd R).symm <| prod (hn _) (h _ fun i ↦ hn i)
 
 end Fin
