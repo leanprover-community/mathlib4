e758a71728556bcd43b957808254482be9dad327
Ben Eltschig <b.eltschig@protonmail.com>
2025-03-14 06:00:17 +0100

    redefine in terms of `HasGlobalSectionsFunctor`

--- Mathlib/CategoryTheory/Sites/GlobalSections.lean
+++ Mathlib/CategoryTheory/Sites/GlobalSections.lean
@@ -13,11 +13,15 @@ is isomorphic to several other constructions when they exist, most notably evalu
 on a terminal object and `Functor.sectionsFunctor`.
 
 ## Main definitions / results
-* `Sheaf.Γ J A`: the global sections functor `Sheaf J A ⥤ A`, defined as taking each sheaf to
-  the limit of its underlying presheaf in `A`.
+* `HasGlobalSectionsFunctor J A`: typeclass stating that the constant sheaf functor `A ⥤ Sheaf J A`
+  has a right-adjoint.
+* `Sheaf.Γ J A`: the global sections functor `Sheaf J A ⥤ A`, defined as the right-adjoint of the
+  constant sheaf functor, whenever that exists.
 * `constantSheafΓAdj J A`: the adjunction between the constant sheaf functor and `Sheaf.Γ J A`.
-* `Sheaf.ΓNatIsoSheafSections J A hT`: on sites with a terminal object `T`, `Sheaf.Γ J A` is
-  isomorphic to the functor evaluating sheaves at `T`.
+* `Sheaf.ΓNatIsoSheafSections J A hT`: on sites with a terminal object `T`, `Sheaf.Γ J A` exists and
+  is isomorphic to the functor evaluating sheaves at `T`.
+* `Sheaf.ΓNatIsoLim J A`: when `A` has limits of shape `Cᵒᵖ`, `Sheaf.Γ J A` exists and is isomorphic
+  to the functor taking each sheaf to the limit of its underlying presheaf.
 * `Sheaf.ΓNatIsoSectionsFunctor J`: for sheaves of types, `Sheaf.Γ J A` is isomorphic to the
   functor taking each sheaf to the type of sections of its underlying presheaf in the sense of
   `Functor.sections`.
@@ -33,25 +37,51 @@ open CategoryTheory Limits Sheaf Opposite GrothendieckTopology
 namespace CategoryTheory
 
 variable {C : Type u} [Category.{v} C] (J : GrothendieckTopology C)
-  (A : Type u₂) [Category.{v₂} A]
+  (A : Type u₂) [Category.{v₂} A] [HasWeakSheafify J A]
 
-/-- The global sections functor `Γ`, taking sheaves valued in `A` to its global sections in `A`.
-It is given here as a limit instead of evaluation on a terminal object of `C`; this has the
-advantage of not requiring `C` to have a terminal object, but the disadvantage of requiring `A`
-to have limits of shape `Cᵒᵖ`. -/
-noncomputable def Sheaf.Γ [HasLimitsOfShape Cᵒᵖ A] : Sheaf J A ⥤ A :=
-  sheafToPresheaf J A ⋙ lim
+/-- Typeclass stating that the constant sheaf functor has a right adjoint. This right adjoint will
+then be called the global sections functor and written `Sheaf.Γ`. -/
+abbrev HasGlobalSectionsFunctor := (constantSheaf J A).IsLeftAdjoint
 
-/-- The constant sheaf functor is left-adjoint to the global sections functor when both exist. -/
-noncomputable def constantSheafΓAdj [HasWeakSheafify J A] [HasLimitsOfShape Cᵒᵖ A] :
+/-- We define the global sections functor as the right-adjoint of the constant sheaf functor
+whenever it exists. -/
+noncomputable def Sheaf.Γ [HasGlobalSectionsFunctor J A] : Sheaf J A ⥤ A :=
+  (constantSheaf J A).rightAdjoint
+
+/-- The constant sheaf functor is by definition left-adjoint to the global sections functor. -/
+noncomputable def constantSheafΓAdj [HasGlobalSectionsFunctor J A] :
     constantSheaf J A ⊣ Γ J A :=
-  constLimAdj.comp (sheafificationAdjunction J A)
+  Adjunction.ofIsLeftAdjoint (constantSheaf J A)
 
-instance [HasWeakSheafify J A] [HasLimitsOfShape Cᵒᵖ A] : (constantSheaf J A).IsLeftAdjoint :=
-  ⟨Γ J A, ⟨constantSheafΓAdj J A⟩⟩
+instance [HasGlobalSectionsFunctor J A] : (Γ J A).IsRightAdjoint := by
+  unfold Γ; infer_instance
 
-instance [HasWeakSheafify J A] [HasLimitsOfShape Cᵒᵖ A] : (Γ J A).IsRightAdjoint :=
-  ⟨constantSheaf J A, ⟨constantSheafΓAdj J A⟩⟩
+/-- Sites with a terminal object admit a global sections functor. -/
+instance hasGlobalSectionsFunctor_of_hasTerminal [HasTerminal C] :
+    HasGlobalSectionsFunctor J A :=
+  ⟨_, ⟨constantSheafAdj J A terminalIsTerminal⟩⟩
+
+/-- On sites with a terminal object, the global sections functor is isomorphic to the functor
+of sections on that object. -/
+noncomputable def Sheaf.ΓNatIsoSheafSections [HasTerminal C] {T : C} (hT : IsTerminal T) :
+    Γ J A ≅ (sheafSections J A).obj (op T) :=
+  (constantSheafΓAdj J A).rightAdjointUniq (constantSheafAdj J A hT)
+
+/-- Every site `C` admits a global sections functor for `A`-valued sheaves when `A` has limits of
+shape `Cᵒᵖ`. -/
+instance hasGlobalSectionsFunctor_of_hasLimitsOfShape [HasLimitsOfShape Cᵒᵖ A] :
+    HasGlobalSectionsFunctor J A :=
+  ⟨sheafToPresheaf J A ⋙ lim, ⟨constLimAdj.comp (sheafificationAdjunction J A)⟩⟩
+
+/-- Global sections of sheaves are naturally isomorphic to the limits of the underlying presheaves,
+provided those exist. -/
+noncomputable def Sheaf.ΓNatIsoLim [HasLimitsOfShape Cᵒᵖ A] :
+    Γ J A ≅ sheafToPresheaf J A ⋙ lim :=
+  (constantSheafΓAdj J A).rightAdjointUniq (constLimAdj.comp (sheafificationAdjunction J A))
+
+-- this is currently needed to obtain the instance `HasSheafify J (Type max u v)`.
+attribute [local instance] CategoryTheory.Types.instConcreteCategory
+attribute [local instance] CategoryTheory.Types.instFunLike
 
 /-- For functors `F : C ⥤ Type _`, `F.sections` is naturally isomorphic to the type `⊤_ _ ⟶ F`
 of natural transformations from the terminal functor to `F`. -/
@@ -66,28 +96,19 @@ noncomputable def sectionsFunctorNatIsoCoyoneda :
         dsimp; ext X x; simp [Unique.eq_default x]
   }
 
-/-- When `C` has a terminal object, the global sections functor is isomorphic to the functor
-of sections on that object. -/
-noncomputable def Sheaf.ΓNatIsoSheafSections [HasLimitsOfShape Cᵒᵖ A] {T : C}
-    (hT : IsTerminal T) : Γ J A ≅ (sheafSections J A).obj (op T) := by
-  refine (isoWhiskerLeft _ ?_).trans <| (sheafSectionsNatIsoEvaluation J A).symm
-  refine @asIso _ _ _ _ { app := fun F ↦ limit.π _ _ } ?_
-  have hT' := initialOpOfTerminal hT
-  exact (NatTrans.isIso_iff_isIso_app _).2 <| fun F ↦ isIso_π_of_isInitial hT' F
-
 /-- For sheaves of types, the global sections functor is isomorphic to the sections functor
 on presheaves. -/
 noncomputable def Sheaf.ΓNatIsoSectionsFunctor :
-    Γ J (Type max u w) ≅ sheafToPresheaf J _ ⋙ Functor.sectionsFunctor _ :=
-  isoWhiskerLeft _ <| Types.limNatIsoSectionsFunctor
+    Γ J (Type max u v) ≅ sheafToPresheaf J _ ⋙ Functor.sectionsFunctor _ :=
+  (ΓNatIsoLim J _).trans (isoWhiskerLeft _ Types.limNatIsoSectionsFunctor)
 
 /-- For sheaves of types, the global sections functor is isomorphic to the covariant hom
 functor of the terminal sheaf. -/
-noncomputable def Sheaf.ΓNatIsoCoyonedaObj :
-    Γ J (Type max u w) ≅ coyoneda.obj (op (⊤_ _)) := by
+noncomputable def Sheaf.ΓNatIsoCoyoneda :
+    Γ J (Type max u v) ≅ coyoneda.obj (op (⊤_ _)) := by
   refine (ΓNatIsoSectionsFunctor J).trans ?_
   refine (isoWhiskerLeft _ sectionsFunctorNatIsoCoyoneda).trans ?_
-  let e : (⊤_ Sheaf J _).val ≅ ⊤_ Cᵒᵖ ⥤ Type max u w :=
+  let e : (⊤_ Sheaf J _).val ≅ ⊤_ Cᵒᵖ ⥤ Type max u v :=
     (terminalIsoIsTerminal (terminalIsTerminal.isTerminalObj (sheafToPresheaf J _) _)).symm
   refine (isoWhiskerLeft _ <| coyoneda.mapIso e.op).trans ?_
   exact NatIso.ofComponents fun _ ↦ Sheaf.homEquiv.toIso.symm
