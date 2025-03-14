4a709c3ec5a8911147813b5db7458ac02f4c7dae
Yakov Pechersky <ffxen158@gmail.com>
2025-03-14 10:34:28 +0000

    feat(Topology/UniformSpace): define nonarchimedean uniformity (#22580)
    prove basic result that such a space is topologically zero dimensional
    
    leave todos for future directions
    
    
    
    
    Co-authored-by: leanprover-community-mathlib4-bot <leanprover-community-mathlib4-bot@users.noreply.github.com>
    Co-authored-by: Yakov Pechersky <pechersky@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -6000,6 +6000,7 @@ import Mathlib.Topology.UniformSpace.OfFun
 import Mathlib.Topology.UniformSpace.Pi
 import Mathlib.Topology.UniformSpace.Real
 import Mathlib.Topology.UniformSpace.Separation
+import Mathlib.Topology.UniformSpace.Ultra
 import Mathlib.Topology.UniformSpace.UniformConvergence
 import Mathlib.Topology.UniformSpace.UniformConvergenceTopology
 import Mathlib.Topology.UniformSpace.UniformEmbedding
