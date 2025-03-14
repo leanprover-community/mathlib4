2f3a3f04e92dc724f114f784465d0d6a77804144
Anne C.A. Baanen <vierkantor@vierkantor.com>
2025-03-14 12:19:13 +0100

    feat(Tactic/Linter): a linter for directory dependencies
    This is a linter that enfores that modules in certain directories should not import modules in certain others. For example, `Mathlib.Algebra.Notation` can only import `Mathlib.Algebra` files that are also in `Mathlib.Algebra.Notation`.
    
    The current implementation seems to be quite slow and needs optimization.

---
+++ Mathlib/Tactic/Linter/DirectoryDependency.lean
@@ -0,0 +1,126 @@
+/-
+Copyright (c) 2025 Lean FRO, LLC. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Anne Baanen
+-/
+import Lean.Elab.Command
+import Lean.Elab.ParseImportsFast
+
+/-! # The `directoryDependency` linter
+
+The `directoryDependency` linter detects imports between directories that are supposed to be
+independent. By specifying that one directory does not import from another, we can improve the
+modularity of Mathlib.
+-/
+
+open Lean Elab Command
+
+/-- Find the longest prefix of `n` such that `f` returns `some` (or return `none` otherwise). -/
+def Lean.Name.findPrefix {α} (f : Name → Option α) (n : Name) : Option α := do
+  f n <|> match n with
+    | anonymous => none
+    | str n' _ => n'.findPrefix f
+    | num n' _ => n'.findPrefix f
+
+namespace Mathlib.Linter
+
+/--
+The `directoryDependency` linter detects detects imports between directories that are supposed to be
+independent. If this is the case, it emits a warning.
+-/
+register_option linter.directoryDependency : Bool := {
+  defValue := true
+  descr := "enable the directoryDependency linter"
+}
+
+namespace DirectoryDependency
+
+/-- `NamePrefixRel` is a datatype for storing relations between name prefixes.
+
+That is, `R : NamePrefixRel` is supposed to answer given names `(n₁, n₂)` whether there are any
+prefixes `n₁'` of `n₁` and `n₂'` of `n₂` such that `n₁' R n₂'`.
+
+The current implementation is a `NameMap` of `NameSet`s, testing each prefix of `n₁` and `n₂` in
+turn. This can probably be optimized.
+-/
+def NamePrefixRel := NameMap NameSet
+
+namespace NamePrefixRel
+
+instance : EmptyCollection NamePrefixRel := inferInstanceAs (EmptyCollection (NameMap _))
+
+/-- Make all names with prefix `n₁` related to names with prefix `n₂`. -/
+def insert (r : NamePrefixRel) (n₁ n₂ : Name) : NamePrefixRel :=
+  match r.find? n₁ with
+    | some ns => NameMap.insert r n₁ (ns.insert n₂)
+    | none => NameMap.insert r n₁ (.insert ∅ n₂)
+
+/-- Convert an array of prefix pairs to a `NamePrefixRel`. -/
+def ofArray (xs : Array (Name × Name)) : NamePrefixRel :=
+  xs.foldl (init := ∅)
+    fun r (n₁, n₂) => r.insert n₁ n₂
+
+/-- Get a prefix of `n₁` that is related to a prefix of `n₂`. -/
+def find (r : NamePrefixRel) (n₁ n₂ : Name) : Option (Name × Name) :=
+  n₁.findPrefix fun n₁' => do
+    let ns ← r.find? n₁'
+    n₂.findPrefix fun n₂' =>
+      if ns.contains n₂'
+      then (n₁', n₂')
+      else none
+
+/-- Is a prefix of `n₁` related to a prefix of `n₂`? -/
+def contains (r : NamePrefixRel) (n₁ n₂ : Name) : Bool := (r.find n₁ n₂).isSome
+
+end NamePrefixRel
+
+/-- `forbiddenImportDirs` relates module prefixes, specifying that modules with the first prefix
+should not import modules with the second prefix (except if specifically allowed in
+`overrideAllowedImportDirs`).
+
+For example, ``(`Mathlib.Algebra.Notation, `Mathlib.Algebra)`` is in `forbiddenImportDirs` and
+``(`Mathlib.Algebra.Notation, `Mathlib.Algebra.Notation)`` is in `overrideAllowedImportDirs`
+because modules in `Mathlib.Algebra.Notation` cannot import modules in `Mathlib.Algebra` that are
+outside `Mathlib.Algebra.Notation`.
+-/
+def forbiddenImportDirs : NamePrefixRel := .ofArray #[
+  (`Mathlib.Data, `Mathlib.Dynamics),
+  (`Mathlib.Algebra.Notation, `Mathlib.Algebra),
+  -- (`Mathlib.Topology, `Mathlib.Algebra),
+]
+
+/-- `overrideAllowedImportDirs` relates module prefixes, specifying that modules with the first
+prefix are allowed to import modules with the second prefix, even if disallowed in
+`forbiddenImportDirs`.
+
+For example, ``(`Mathlib.Algebra.Notation, `Mathlib.Algebra)`` is in `forbiddenImportDirs` and
+``(`Mathlib.Algebra.Notation, `Mathlib.Algebra.Notation)`` is in `overrideAllowedImportDirs`
+because modules in `Mathlib.Algebra.Notation` cannot import modules in `Mathlib.Algebra` that are
+outside `Mathlib.Algebra.Notation`.
+-/
+def overrideAllowedImportDirs : NamePrefixRel := .ofArray #[
+  (`Mathlib.Algebra.Notation, `Mathlib.Algebra.Notation),
+  (`Mathlib.Topology.Algebra, `Mathlib.Algebra),
+]
+
+@[inherit_doc Mathlib.Linter.linter.directoryDependency]
+def directoryDependencyLinter : Linter where run := withSetOptionIn fun stx ↦ do
+  unless Linter.getLinterValue linter.directoryDependency (← getOptions) do
+    return
+  if (← get).messages.hasErrors then
+    return
+  let mainModule ← getMainModule
+  let env ← getEnv
+  for imported in env.allImportedModuleNames do
+    match forbiddenImportDirs.find mainModule imported with
+    | some (n₁, n₂) => do
+      if !overrideAllowedImportDirs.contains mainModule imported then
+        Linter.logLint linter.directoryDependency stx
+          m!"This module depends on {imported}, but modules starting with {n₁} are not allowed to import modules starting with {n₂} (exceptions can be added to `overrideAllowedImportDirs`)."
+    | none => pure ()
+
+initialize addLinter directoryDependencyLinter
+
+end DirectoryDependency
+
+end Mathlib.Linter
