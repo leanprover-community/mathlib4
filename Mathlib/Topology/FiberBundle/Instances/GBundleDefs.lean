7ce09e53e589d5246dcd62a07906522d12c80ff9
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-03-14 21:38:26 +0100

    Tweak GBundleDefs.lean

--- Mathlib/Topology/FiberBundle/Instances/GBundleDefs.lean
+++ Mathlib/Topology/FiberBundle/Instances/GBundleDefs.lean
@@ -6,9 +6,7 @@ Authors: Dominic Steinitz
 import Mathlib.Analysis.Normed.Ring.Basic
 import Mathlib.LinearAlgebra.UnitaryGroup
 
-set_option linter.style.longLine false
-
-open Set
+open Matrix Set
 
 structure GBundleCore (ι : Type*) (B : Type*) [TopologicalSpace B] (F : Type*)
     [TopologicalSpace F] (G : Type*) [Group G] [MulAction G F] where
@@ -22,13 +20,10 @@ structure GBundleCore (ι : Type*) (B : Type*) [TopologicalSpace B] (F : Type*)
     ContinuousOn (fun p : B × F => coordChange i j p.1 p.2) ((baseSet i ∩ baseSet j) ×ˢ univ)
   coordChange_comp : ∀ i j k, ∀ x ∈ baseSet i ∩ baseSet j ∩ baseSet k, ∀ v,
     (coordChange j k x) (coordChange i j x v) = coordChange i k x v
-  coordChange_structure_group : ∀ i j, ∀ x ∈ baseSet i ∩ baseSet j, ∃ g : G, ∀ v : F, coordChange i j x v = g • v
+  coordChange_structure_group :
+    ∀ i j, ∀ x ∈ baseSet i ∩ baseSet j, ∃ g : G, ∀ v : F, coordChange i j x v = g • v
 
-open MulAction Set
-
-open LinearMap Matrix UnitaryGroup
-
-instance {n : ℕ}: MulAction (orthogonalGroup (Fin n) ℝ) (Fin n -> ℝ) where
+instance {n : ℕ} : MulAction (orthogonalGroup (Fin n) ℝ) (Fin n -> ℝ) where
   smul g x := g.1.mulVec x
   one_smul x := by
     change (1 : orthogonalGroup (Fin n) ℝ).1.mulVec x = x
