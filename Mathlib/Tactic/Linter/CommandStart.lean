ec94e70f110bf8baa254d8f8369ef958ccf7cae2
adomani <adomani@gmail.com>
2025-03-15 10:48:59 +0100

    process strings

--- Mathlib/Tactic/Linter/CommandStart.lean
+++ Mathlib/Tactic/Linter/CommandStart.lean
@@ -65,43 +65,45 @@ structure FormatError where
   fmtPos : Nat
   msg : String
 
-def mkFormatError (ls ms : List Char) (msg : String) : FormatError where
+def mkFormatError (ls ms : String) (msg : String) : FormatError where
   srcNat := ls.length
-  srcPos := (String.mk ls).endPos
+  srcPos := ls.endPos
   fmtPos := ms.length
   msg := msg
 
 partial
-def parallelScanAux : Array FormatError → List Char → List Char → Array FormatError
-  | as, L@(' '::ls), M@(m::ms) =>
+def parallelScanAux (as : Array FormatError) (L M : String) : Array FormatError :=
+  if M.isEmpty then as else
+  if M.trim.isEmpty then
+    as
+  else
+  let ls := L.drop 1
+  let ms := M.drop 1
+  match L.get 0, M.get 0 with
+  | ' ', m =>
     if m.isWhitespace then
-      parallelScanAux as ls (ms.dropWhile (·.isWhitespace))
+      parallelScanAux as (ls) (ms.dropWhile (·.isWhitespace))
     else
       parallelScanAux (as.push (mkFormatError L M "extra space")) ls M
-  | as, L@('\n'::ls), M@(m::ms) =>
+  | '\n', m =>
     let lth := ls.takeWhile (·.isWhitespace) |>.length
     if m.isWhitespace then
       parallelScanAux as (ls.drop lth) (ms.dropWhile (·.isWhitespace))
     else
       parallelScanAux (as.push (mkFormatError L M "remove line break")) (ls.drop lth) M
-  | as, L@(l::ls), M@(m::ms) => -- `l` is not whitespace
+  | l, m => -- `l` is not whitespace
     if l == m then
       parallelScanAux as ls ms
     else
       if m.isWhitespace then
         parallelScanAux
-          (as.push (mkFormatError L M "missing space")) (l::ls) (ms.dropWhile (·.isWhitespace))
+          (as.push (mkFormatError L M "missing space")) L (ms.dropWhile (·.isWhitespace))
     else
       as.push (mkFormatError ls ms "Oh no! (Unreachable?)")
-  | as, _, [] => as
-  | as, [], M =>
-    if M.all (·.isWhitespace) then
-      as
-    else
-      as.push (mkFormatError [] M "The formatted string finished early! (Unreachable?)")
+
 
 def parallelScan (src fmt : String) : Array FormatError :=
-  parallelScanAux ∅ src.toList fmt.toList
+  parallelScanAux ∅ src fmt
 
 /--
 Returns the pair consisting of
