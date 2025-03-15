7f1e67f51934520b2697b317ef83b792007fa0b9
Jireh Loreaux <loreaujy@gmail.com>
2025-03-15 00:55:33 -0500

    remove import

--- Mathlib/Data/Real/ConjExponents.lean
+++ Mathlib/Data/Real/ConjExponents.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Sébastien Gouëzel, Yury Kudryashov
 -/
 import Mathlib.Data.ENNReal.Holder
-import Mathlib.Tactic.Finiteness
 
 /-!
 # Real conjugate exponents
