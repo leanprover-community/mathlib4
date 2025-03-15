leanprover-community/mathlib4

commit f033aca78f32c0bef4d264d5576a083d0534330d

chore(Data): go through remaining porting notes (#22907)
This PR goes through all porting notes in `Mathlib/Data` and reviews them. Most of the notes can simply be applied or dropped.

Mathlib/Data/PFun.lean
@@ -99,6 +99,7 @@ theorem ext' {f g : α →. β} (H1 : ∀ a, a ∈ Dom f ↔ a ∈ Dom g) (H2 : 
     f = g :=
   funext fun a => Part.ext' (H1 a) (H2 a)
 
+@[ext]
 theorem ext {f g : α →. β} (H : ∀ a b, b ∈ f a ↔ b ∈ g a) : f = g :=
   funext fun a => Part.ext (H a)
 
@@ -318,7 +319,7 @@ theorem fixInduction'_stop {C : α → Sort*} {f : α →. β ⊕ α} {b : β} {
     @fixInduction' _ _ C _ _ _ h hbase hind = hbase a fa := by
   unfold fixInduction'
   rw [fixInduction_spec]
-  -- Porting note: the explicit motive required because `simp` behaves differently
+  -- Porting note: the explicit motive required because `simp` does not apply `Part.get_eq_of_mem`
   refine Eq.rec (motive := fun x e ↦
       Sum.casesOn x ?_ ?_ (Eq.trans (Part.get_eq_of_mem fa (dom_of_mem_fix h)) e) = hbase a fa) ?_
     (Part.get_eq_of_mem fa (dom_of_mem_fix h)).symm
@@ -331,7 +332,7 @@ theorem fixInduction'_fwd {C : α → Sort*} {f : α →. β ⊕ α} {b : β} {a
     @fixInduction' _ _ C _ _ _ h hbase hind = hind a a' h' fa (fixInduction' h' hbase hind) := by
   unfold fixInduction'
   rw [fixInduction_spec]
-  -- Porting note: the explicit motive required because `simp` behaves differently
+  -- Porting note: the explicit motive required because `simp` does not apply `Part.get_eq_of_mem`
   refine Eq.rec (motive := fun x e =>
       Sum.casesOn (motive := fun y => (f a).get (dom_of_mem_fix h) = y → C a) x ?_ ?_
       (Eq.trans (Part.get_eq_of_mem fa (dom_of_mem_fix h)) e) = _) ?_
@@ -544,7 +545,6 @@ theorem mem_prodLift {f : α →. β} {g : α →. γ} {x : α} {y : β × γ} :
     y ∈ f.prodLift g x ↔ y.1 ∈ f x ∧ y.2 ∈ g x := by
   trans ∃ hp hq, (f x).get hp = y.1 ∧ (g x).get hq = y.2
   · simp only [prodLift, Part.mem_mk_iff, And.exists, Prod.ext_iff]
-  -- Porting note: was just `[exists_and_left, exists_and_right]`
   · simp only [exists_and_left, exists_and_right, Membership.mem, Part.Mem]
 
 /-- Product of partial functions. -/
@@ -575,19 +575,16 @@ theorem mem_prodMap {f : α →. γ} {g : β →. δ} {x : α × β} {y : γ × 
 theorem prodLift_fst_comp_snd_comp (f : α →. γ) (g : β →. δ) :
     prodLift (f.comp ((Prod.fst : α × β → α) : α × β →. α))
         (g.comp ((Prod.snd : α × β → β) : α × β →. β)) =
-      prodMap f g :=
-  ext fun a => by simp
+      prodMap f g := by
+  aesop
 
 @[simp]
-theorem prodMap_id_id : (PFun.id α).prodMap (PFun.id β) = PFun.id _ :=
-  ext fun _ _ ↦ by simp [eq_comm]
+theorem prodMap_id_id : (PFun.id α).prodMap (PFun.id β) = PFun.id _ := by
+  aesop
 
 @[simp]
 theorem prodMap_comp_comp (f₁ : α →. β) (f₂ : β →. γ) (g₁ : δ →. ε) (g₂ : ε →. ι) :
-    (f₂.comp f₁).prodMap (g₂.comp g₁) = (f₂.prodMap g₂).comp (f₁.prodMap g₁) := -- by
-  -- Porting note: was `by tidy`, below is a golfed version of the `tidy?` proof
-  ext <| fun ⟨_, _⟩ ⟨_, _⟩ ↦
-  ⟨fun ⟨⟨⟨h1l1, h1l2⟩, ⟨h1r1, h1r2⟩⟩, h2⟩ ↦ ⟨⟨⟨h1l1, h1r1⟩, ⟨h1l2, h1r2⟩⟩, h2⟩,
-   fun ⟨⟨⟨h1l1, h1r1⟩, ⟨h1l2, h1r2⟩⟩, h2⟩ ↦ ⟨⟨⟨h1l1, h1l2⟩, ⟨h1r1, h1r2⟩⟩, h2⟩⟩
+    (f₂.comp f₁).prodMap (g₂.comp g₁) = (f₂.prodMap g₂).comp (f₁.prodMap g₁) := by
+  aesop
 
 end PFun

Mathlib/Data/Sign.lean
@@ -205,16 +205,15 @@ section cast
 
 variable {α : Type*} [Zero α] [One α] [Neg α]
 
-/-- Turn a `SignType` into zero, one, or minus one. This is a coercion instance, but note it is
-only a `CoeTC` instance: see note [use has_coe_t]. -/
+/-- Turn a `SignType` into zero, one, or minus one. This is a coercion instance. -/
 @[coe]
 def cast : SignType → α
   | zero => 0
   | pos => 1
   | neg => -1
 
--- Porting note: Translated has_coe_t to CoeTC
-instance : CoeTC SignType α :=
+/-- This is a `Coe` since the type on the right (trivially) determines the type on the left. -/
+instance : Coe SignType α :=
   ⟨cast⟩
 
 /-- Casting out of `SignType` respects composition with functions preserving `0, 1, -1`. -/

commit 3826fc6f2c73ffb2a78d5fd70c72c9ad1bdee197

chore(Data): fix performance regressions of #22907
This fixes two major performance regressions I accidentally introduced in #22907:

* in `Data.PFun`, `aesop` was very slow to prove a goal, restore manual proof
* in `Data.Sign`, I turned a `CoeTC SignType ɑ` instance into `Coe SignType ɑ` (since `CoeTC` is an implementation detail), but this would mean that for any coercion to `ɑ` we'd try `SignType` as an intermediate step. Instead make it a `CoeTail` so it would only be applied as a fallback.

Mathlib/Data/PFun.lean
@@ -584,7 +584,10 @@ theorem prodMap_id_id : (PFun.id α).prodMap (PFun.id β) = PFun.id _ := by
 
 @[simp]
 theorem prodMap_comp_comp (f₁ : α →. β) (f₂ : β →. γ) (g₁ : δ →. ε) (g₂ : ε →. ι) :
-    (f₂.comp f₁).prodMap (g₂.comp g₁) = (f₂.prodMap g₂).comp (f₁.prodMap g₁) := by
-  aesop
+    (f₂.comp f₁).prodMap (g₂.comp g₁) = (f₂.prodMap g₂).comp (f₁.prodMap g₁) :=
+  -- `aesop` can prove this but takes over a second, so we do it manually
+  ext <| fun ⟨_, _⟩ ⟨_, _⟩ ↦
+  ⟨fun ⟨⟨⟨h1l1, h1l2⟩, ⟨h1r1, h1r2⟩⟩, h2⟩ ↦ ⟨⟨⟨h1l1, h1r1⟩, ⟨h1l2, h1r2⟩⟩, h2⟩,
+   fun ⟨⟨⟨h1l1, h1r1⟩, ⟨h1l2, h1r2⟩⟩, h2⟩ ↦ ⟨⟨⟨h1l1, h1l2⟩, ⟨h1r1, h1r2⟩⟩, h2⟩⟩
 
 end PFun

Mathlib/Data/Sign.lean
@@ -212,8 +212,8 @@ def cast : SignType → α
   | pos => 1
   | neg => -1
 
-/-- This is a `Coe` since the type on the right (trivially) determines the type on the left. -/
-instance : Coe SignType α :=
+/-- This is a `CoeTail` since the type on the right (trivially) determines the type on the left. -/
+instance : CoeTail SignType α :=
   ⟨cast⟩
 
 /-- Casting out of `SignType` respects composition with functions preserving `0, 1, -1`. -/