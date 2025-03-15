c5a1102fdf8e872510593ca70c68ee0c86fb624a
yuanyi-350 <kysyy1@126.com>
2025-03-15 16:03:10 +0800

    Update Exactness.lean

--- Mathlib/RingTheory/FilteredAlgebra/Exactness.lean
+++ Mathlib/RingTheory/FilteredAlgebra/Exactness.lean
@@ -176,9 +176,30 @@ variable (f : FilteredAddGroupHom FR (fun n ↦ FR (n - 1)) FS (fun n ↦ FS (n 
 
 variable (g : FilteredAddGroupHom FS (fun n ↦ FS (n - 1)) FT (fun n ↦ FT (n - 1)))
 
-lemma shrinking_lemma (S' : AddSubgroup S) {p : ℤ} (y : (ofClass (FS p) ⊓ S' : AddSubgroup S))
-    (h : ∀ i : ℤ, S' ⊓ ofClass (FS i) ≤ AddSubgroup.map f (ofClass (FR i))) :
-  ∀ s : ℕ, ∃ x : FR p, y - (f.toAddMonoidHom x) ∈ FS (p - s - 1) := sorry
+lemma shrinking_lemma {monoR : Monotone FR} (S' : AddSubgroup S) {p : ℤ} (y : (ofClass (FS p) ⊓ S' : AddSubgroup S))
+    (h : ∀ i : ℤ, (ofClass (FS i) ⊓ S' : AddSubgroup S) ≤ AddSubgroup.map f (ofClass (FR i))) :
+    ∀ s : ℕ, ∃ x : FR p, y - (f.toAddMonoidHom x) ∈ (ofClass (FS (p - s)) ⊓ S' : AddSubgroup S):= by
+  intro s
+  induction' s with s ih
+  · use 0
+    simp[AddMonoidHom.map_zero f.toAddMonoidHom]
+  · obtain⟨x₀, h₀⟩ := ih
+    obtain⟨z, zin, eq⟩ : y - f.toAddMonoidHom x₀ ∈ (map f.toAddMonoidHom (ofClass (FR (p - s)))) :=
+      h (p - s) h₀
+
+    have : ((⟨z, zin⟩ : FR (p - s)) : R) ∈ (FR p) := by
+      have : FR (p - s) ≤ FR p := by
+        apply monoR
+        simp
+      exact this zin
+
+    use (x₀ + ⟨((⟨z, zin⟩ : FR (p - s)) : R) , this⟩)
+
+    simp only [Nat.cast_add, Nat.cast_one, AddMemClass.coe_add, AddMonoidHom.map_add]
+    simp[eq]
+    constructor
+    · exact AddSubgroup.zero_mem (ofClass (FS (p - (↑s + 1))))
+    · exact AddSubgroup.zero_mem S'
 
 
 
@@ -186,6 +207,7 @@ theorem exact_of_graded_exact
     (exhaustive : letI := (mk_int FS monoS); IsExhaustiveFiltration FS (fun n ↦ FS (n - 1)))
     (discrete : letI := (mk_int FS monoS); IsDiscreteFiltration FS (fun n ↦ FS (n - 1)))
     (exact : Function.Exact Gr+[f] Gr+[g]) : Function.Exact f.toAddMonoidHom g.toAddMonoidHom := by
+  sorry
   -- refine Function.Exact.of_comp_of_mem_range ?_ ?_
   -- · sorry
   -- · intro y yto0
