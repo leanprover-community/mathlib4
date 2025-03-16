3d8d050c64d1def62525b2f88de15099980fdea0
Stefan Kebekus <kebekus@users.noreply.github.com>
2025-03-16 07:24:53 +0100

    Update Mathlib/Algebra/Order/WithTop/Untop0.lean
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- Mathlib/Algebra/Order/WithTop/Untop0.lean
+++ Mathlib/Algebra/Order/WithTop/Untop0.lean
@@ -32,7 +32,7 @@ def untop₀ [Zero α] (a : WithTop α) : α := a.untopD 0
 -/
 
 @[simp]
-lemma untop₀_eq_zero (a : WithTop α) :
+lemma untop₀_eq_zero {a : WithTop α} :
     a.untop₀ = 0 ↔ a = 0 ∨ a = ⊤ := by simp [untop₀]
 
 @[simp]
