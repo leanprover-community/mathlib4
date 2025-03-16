272c0614abcb7ddb8108c21f2c63199e026c9295
pfaffelh <pfaffelh@gmail.com>
2025-03-16 10:10:53 +0100

    restored imports

--- Mathlib/MeasureTheory/SetSemiring.lean
+++ Mathlib/MeasureTheory/SetSemiring.lean
@@ -3,6 +3,11 @@ Copyright (c) 2023 Rémy Degenne. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Rémy Degenne, Peter Pfaffelhuber
 -/
+import Mathlib.Data.Nat.Lattice
+import Mathlib.Data.Set.Accumulate
+import Mathlib.Data.Set.Pairwise.Lattice
+import Mathlib.Order.CompleteLattice
+import Mathlib.MeasureTheory.PiSystem
 import Mathlib.MeasureTheory.MeasurableSpace.Pi
 
 /-! # Semirings of sets
