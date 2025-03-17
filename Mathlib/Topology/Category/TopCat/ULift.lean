527f882072ee80d605ba369a9e52a83e7cb3683d
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-03-17 19:01:42 +0100

    Shake

--- Mathlib/Topology/Category/TopCat/ULift.lean
+++ Mathlib/Topology/Category/TopCat/ULift.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joël Riou
 -/
 import Mathlib.Topology.Category.TopCat.Basic
-import Mathlib.Topology.Homeomorph.Lemmas
+import Mathlib.Topology.Homeomorph.Constructions
 
 /-!
 # Lifting topological spaces to a higher universe
