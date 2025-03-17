daf35dc82959a98fb8550a2137e029f38c8b09fa
plp127 <aaronliu2008@outlook.com>
2025-03-17 17:27:28 -0400

    add ite check first

--- Mathlib/Tactic/FunProp/ToBatteries.lean
+++ Mathlib/Tactic/FunProp/ToBatteries.lean
@@ -51,7 +51,7 @@ Swaps bvars indices `i` and `j`
 NOTE: the indices `i` and `j` do not correspond to the `n` in `bvar n`. Rather
 they behave like indices in `Expr.lowerLooseBVars`, `Expr.liftLooseBVars`, etc.
 -/
-def _root_.Lean.Expr.swapBVars (e : Expr) (i j : Nat) : Expr := match e with
+def _root_.Lean.Expr.swapBVars (e : Expr) (i j : Nat) : Expr := if i = j then e else match e with
 | .bvar n => if n = i then .bvar j else if n = j then .bvar i else e
 | .forallE _ t b _ => e.updateForallE! (t.swapBVars i j) (b.swapBVars (i + 1) (j + 1))
 | .letE _ t v b _ => e.updateLet! (t.swapBVars i j) (v.swapBVars i j) (b.swapBVars (i + 1) (j + 1))
