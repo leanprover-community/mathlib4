5fbe9347fa8b8dbf83b8e2a51ec99a8458038919
ADedecker <anatolededecker@gmail.com>
2025-03-17 22:49:34 +0100

    Add import

--- Mathlib/Topology/Algebra/RestrictedProduct.lean
+++ Mathlib/Topology/Algebra/RestrictedProduct.lean
@@ -3,6 +3,7 @@ Copyright (c) 2025 Anatole Dedecker. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Anatole Dedecker
 -/
+import Mathlib.Topology.Algebra.Group.Pointwise
 import Mathlib.Topology.Algebra.Ring.Basic
 
 /-!
