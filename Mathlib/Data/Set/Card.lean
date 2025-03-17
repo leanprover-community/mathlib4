cc6da6c075cb8d90a510c2a94e14c68f5073a810
Yury G. Kudryashov <urkud@urkud.name>
2025-03-17 00:20:32 -0500

    Apply suggestions from code review

--- Mathlib/Data/Set/Card.lean
+++ Mathlib/Data/Set/Card.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2023 Peter Nelson. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Peter Nelson, Yury G. Kudryashov
+Authors: Peter Nelson
 -/
 import Mathlib.SetTheory.Cardinal.Finite
 
