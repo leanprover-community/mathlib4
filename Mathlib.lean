247d7f31b828321da7adb6241cc64a513bb7f428
Jon Eugster <eugster.jon@gmail.com>
2025-03-18 18:04:37 +0000

    feat(CategoryTheory/Enriched/Limits): add conical pullbacks (#22924)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1919,6 +1919,7 @@ import Mathlib.CategoryTheory.Enriched.Basic
 import Mathlib.CategoryTheory.Enriched.FunctorCategory
 import Mathlib.CategoryTheory.Enriched.HomCongr
 import Mathlib.CategoryTheory.Enriched.Limits.HasConicalLimits
+import Mathlib.CategoryTheory.Enriched.Limits.HasConicalPullbacks
 import Mathlib.CategoryTheory.Enriched.Opposite
 import Mathlib.CategoryTheory.Enriched.Ordinary.Basic
 import Mathlib.CategoryTheory.EpiMono
