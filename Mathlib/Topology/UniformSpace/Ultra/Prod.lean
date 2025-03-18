159a07b11e9b22cbddfcabaaa2e28a3dac4196f2
Yakov Pechersky <ffxen158@gmail.com>
2025-03-17 22:25:53 -0400

    golf thanks to `mk_of_hasBasis`
    Co-authored-by: Anatole Dedecker <anatolededecker@gmail.com>

--- Mathlib/Topology/UniformSpace/Ultra/Prod.lean
+++ Mathlib/Topology/UniformSpace/Ultra/Prod.lean
@@ -34,26 +34,17 @@ lemma IsTransitiveRel.entourageProd {s : Set (X × X)} {t : Set (Y × Y)}
 lemma IsUltraUniformity.comap {u : UniformSpace Y} (h : IsUltraUniformity Y) (f : X → Y) :
     @IsUltraUniformity _ (u.comap f) := by
   letI := u.comap f
-  constructor
-  refine (h.hasBasis.comap (Prod.map f f)).to_hasBasis' ?_ ?_
-  · simp only [uniformity_comap, Filter.mem_comap, id_eq, and_imp]
-    intro s hs hss hst
-    exact ⟨_, ⟨⟨s, hs, Set.preimage_mono (subset_refl _)⟩, hss.preimage_map _, hst.preimage_map _⟩,
-      subset_refl _⟩
-  · aesop
+  refine .mk_of_hasBasis (h.hasBasis.comap (Prod.map f f)) ?_ ?_
+  · exact fun _ ⟨_, hU, _⟩ ↦ hU.preimage_map f
+  · exact fun _ ⟨_, _, hU⟩ ↦ hU.preimage_map f
 
 lemma IsUltraUniformity.inf {u u' : UniformSpace X} (h : @IsUltraUniformity _ u)
     (h' : @IsUltraUniformity _ u') :
     @IsUltraUniformity _ (u ⊓ u') := by
   letI := u ⊓ u'
-  constructor
-  refine (h.hasBasis.inf h'.hasBasis).to_hasBasis' ?_ ?_
-  · simp only [id_eq, Set.subset_inter_iff, and_imp, Prod.forall]
-    intros
-    exact ⟨_, ⟨Filter.mem_inf_of_inter ‹_› ‹_› (subset_refl _),
-      IsSymmetricRel.inter ‹_› ‹_›, IsTransitiveRel.inter ‹_› ‹_›⟩,
-      Set.inter_subset_left, Set.inter_subset_right⟩
-  · aesop
+  refine .mk_of_hasBasis (h.hasBasis.inf h'.hasBasis) ?_ ?_
+  · exact fun _ ⟨⟨_, hU, _⟩, _, hU', _⟩ ↦ hU.inter hU'
+  · exact fun _ ⟨⟨_, _, hU⟩, _, _, hU'⟩ ↦ hU.inter hU'
 
 /-- The product of uniform spaces with nonarchimedean uniformities has a
 nonarchimedean uniformity. -/
@@ -66,20 +57,9 @@ lemma IsUltraUniformity.iInf {ι : Type*} {U : (i : ι) → UniformSpace X}
     (hU : ∀ i, @IsUltraUniformity X (U i)) :
     @IsUltraUniformity _ (⨅ i, U i : UniformSpace X) := by
   letI : UniformSpace X := ⨅ i, U i
-  constructor
-  rw [iInf_uniformity]
-  refine (Filter.hasBasis_iInf (fun i ↦ (hU i).hasBasis)).to_hasBasis' ?_ ?_
-  · simp only [id_eq, Set.iInter_coe_set, Set.subset_iInter_iff, and_imp]
-    intro ⟨s, f⟩ hsf ih
-    simp only at ih ⊢
-    refine ⟨⋂ i : s, f i, ⟨?_, ?_, ?_⟩, ?_⟩
-    · rw [Filter.mem_iInf]
-      exact ⟨s, hsf, f, fun _ ↦ (ih _).left, rfl⟩
-    · exact IsSymmetricRel.iInter (fun i ↦ (ih i).right.left)
-    · exact IsTransitiveRel.iInter (fun i ↦ (ih i).right.right)
-    · intros
-      exact Set.iInter_subset _ _
-  · aesop
+  refine .mk_of_hasBasis (iInf_uniformity ▸ (Filter.hasBasis_iInf (fun i ↦ (hU i).hasBasis))) ?_ ?_
+  · exact fun _ ⟨_, h⟩ ↦ IsSymmetricRel.iInter fun i ↦ (h i).right.left
+  · exact fun _ ⟨_, h⟩ ↦ IsTransitiveRel.iInter fun i ↦ (h i).right.right
 
 /-- The indexed product of uniform spaces with nonarchimedean uniformities has a
 nonarchimedean uniformity. -/
