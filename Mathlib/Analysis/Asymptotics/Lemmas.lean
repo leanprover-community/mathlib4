6cc5c7c4ba93576f127253e7453138af5b631c86
Arend Mellendijk <arend.mellendijk@gmail.com>
2025-03-18 16:02:41 +0100

    generalise

--- Mathlib/Analysis/Asymptotics/Lemmas.lean
+++ Mathlib/Analysis/Asymptotics/Lemmas.lean
@@ -603,8 +603,8 @@ theorem isBigO_one_nat_atTop_iff {f : ℕ → E''} :
   Iff.trans (isBigO_nat_atTop_iff fun _ h => (one_ne_zero h).elim) <| by
     simp only [norm_one, mul_one]
 
-theorem isBigO_nat_of_isBigO_atTop {f g : ℕ → E''} (hfg : f =O[atTop] g) {l : Filter ℕ}
-    (h : ∀ᶠ n in l, g n = 0 → f n = 0) : f =O[l] g := by
+theorem isBigO_nat_of_isBigO_atTop {f : ℕ → E''} {g : ℕ → F''} (hfg : f =O[atTop] g)
+    {l : Filter ℕ} (h : ∀ᶠ n in l, g n = 0 → f n = 0) : f =O[l] g := by
   obtain ⟨C, hC_pos, hC⟩ := Asymptotics.bound_of_isBigO_nat_atTop hfg
   refine isBigO_iff.mpr ⟨C, ?_⟩
   filter_upwards [h] with x h
