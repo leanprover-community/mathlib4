c58b874b980b855bb183382c82b8b7d37c0517d4
EtienneC30 <baguettes_casino0c@icloud.com>
2025-03-18 14:37:17 +0000

    feat: completely metrizable spaces (#22926)
    Introduce the notion of completely metrizable spaces and refactor `PolishSpace` using it.

--- Mathlib.lean
+++ Mathlib.lean
@@ -5910,6 +5910,7 @@ import Mathlib.Topology.MetricSpace.Ultra.ContinuousMaps
 import Mathlib.Topology.MetricSpace.Ultra.Pi
 import Mathlib.Topology.MetricSpace.Ultra.TotallySeparated
 import Mathlib.Topology.Metrizable.Basic
+import Mathlib.Topology.Metrizable.CompletelyMetrizable
 import Mathlib.Topology.Metrizable.ContinuousMap
 import Mathlib.Topology.Metrizable.Real
 import Mathlib.Topology.Metrizable.Uniformity
