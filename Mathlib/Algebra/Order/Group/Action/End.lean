967c0cb3087c95a8c5782225eab36a8f60adf6ad
Yaël Dillies <yael.dillies@gmail.com>
2025-03-19 10:42:25 +0000

    shake

--- Mathlib/Algebra/Order/Group/Action/End.lean
+++ Mathlib/Algebra/Order/Group/Action/End.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yaël Dillies
 -/
 import Mathlib.Algebra.Group.Action.Faithful
-import Mathlib.Algebra.Group.Opposite
 import Mathlib.Algebra.Order.Group.End
 import Mathlib.Order.RelIso.Basic
 
