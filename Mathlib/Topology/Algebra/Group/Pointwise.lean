e722197cafd41a0c5c6a6ab34b80ebcbd53fedf6
Junyan Xu <junyanxu.math@gmail.com>
2025-03-19 11:01:37 +0000

    fix cyclic import

--- Mathlib/Topology/Algebra/Group/Pointwise.lean
+++ Mathlib/Topology/Algebra/Group/Pointwise.lean
@@ -313,6 +313,17 @@ theorem exists_closed_nhds_one_inv_eq_mul_subset {U : Set G} (hU : U ∈ 𝓝 1)
   _ ⊆ V * V := mul_subset_mul hW hW
   _ ⊆ U := hV
 
+@[to_additive] lemma Subgroup.disjoint_nhds_of_discrete (S : Subgroup G) [d : DiscreteTopology S] :
+    ∃ U ∈ 𝓝 (1 : G), ∀ g ∈ S, (g * ·) '' U ∩ U ≠ ∅ ∨ (· * g) '' U ∩ U ≠ ∅ → g = 1 := by
+  simp_rw [← Set.nonempty_iff_ne_empty]
+  obtain ⟨V, hV⟩ := nhds_inter_eq_singleton_of_mem_discrete S.one_mem
+  obtain ⟨U, hU, -, hUinv, hUV⟩ := exists_closed_nhds_one_inv_eq_mul_subset hV.1
+  refine ⟨U, hU, fun g hgS ↦ ?_⟩
+  rintro (⟨_, ⟨x, hx, rfl⟩, hgx⟩|⟨_, ⟨x, hx, rfl⟩, hxg⟩) <;>
+    refine hV.2.subset ⟨hUV ?_, hgS⟩ <;> rw [← hUinv] at hx
+  · exact ⟨_, _, hgx, hx, by simp⟩
+  · exact ⟨_, _, hx, hxg, by simp⟩
+
 end
 
 section
