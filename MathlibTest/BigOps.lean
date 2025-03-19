d22e35676a1dad7a5dc0f83a4b1050bd9baa41cf
Yaël Dillies <yael.dillies@gmail.com>
2025-03-19 20:13:26 +0000

    #guard_msgs

--- MathlibTest/BigOps.lean
+++ MathlibTest/BigOps.lean
@@ -4,13 +4,37 @@ section
 variable {ι M : Type*} [CommMonoid M] [AddCommMonoid M] [Fintype ι] {s : Finset ι} {p : ι → Prop}
   [DecidablePred p] {f : ι → M}
 
-#guard_expr ∑ i, f i = Finset.sum Finset.univ fun i ↦ f i
-#guard_expr ∏ i, f i = Finset.prod Finset.univ fun i ↦ f i
-#guard_expr ∑ i ∈ s, f i = Finset.sum s fun i ↦ f i
-#guard_expr ∏ i ∈ s, f i = Finset.prod s fun i ↦ f i
-#guard_expr ∑ i with p i, f i = Finset.sum {i | p i} fun i ↦ f i
-#guard_expr ∏ i with p i, f i = Finset.prod {i | p i} fun i ↦ f i
-#guard_expr ∑ i ∈ s with p i, f i = Finset.sum {i ∈ s | p i} fun i ↦ f i
-#guard_expr ∏ i ∈ s with p i, f i = Finset.prod {i ∈ s | p i} fun i ↦ f i
+
+/-- info: ∑ i : ι, f i : M -/
+#guard_msgs in
+#check Finset.sum Finset.univ fun i ↦ f i
+
+/-- info: ∏ i : ι, f i : M -/
+#guard_msgs in
+#check Finset.prod Finset.univ fun i ↦ f i
+
+/-- info: ∑ i ∈ s, f i : M -/
+#guard_msgs in
+#check Finset.sum s fun i ↦ f i
+
+/-- info: ∏ i ∈ s, f i : M -/
+#guard_msgs in
+#check Finset.prod s fun i ↦ f i
+
+/-- info: ∑ i with p i, f i : M -/
+#guard_msgs in
+#check Finset.sum {i | p i} fun i ↦ f i
+
+/-- info: ∏ i with p i, f i : M -/
+#guard_msgs in 
+#check Finset.prod {i | p i} fun i ↦ f i
+
+/-- info: ∑ i ∈ s with p i, f i : M -/
+#guard_msgs in
+#check Finset.sum {i ∈ s | p i} fun i ↦ f i
+
+/-- info: ∏ i ∈ s with p i, f i : M -/
+#guard_msgs in
+#check Finset.prod {i ∈ s | p i} fun i ↦ f i
 
 end
