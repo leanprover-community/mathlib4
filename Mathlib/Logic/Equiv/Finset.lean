96ea62b47aadc699e8f5bb1a3cd7ba985900505d
Eric Wieser <wieser.eric@gmail.com>
2025-03-19 11:50:01 +0000

    tweak imports

--- Mathlib/Logic/Equiv/Finset.lean
+++ Mathlib/Logic/Equiv/Finset.lean
@@ -3,8 +3,8 @@ Copyright (c) 2018 Mario Carneiro. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro
 -/
-import Mathlib.Logic.Equiv.Multiset
 import Mathlib.Data.Finset.Sort
+import Mathlib.Logic.Equiv.Multiset
 
 /-!
 # `Encodable` and `Denumerable` instances for `Finset`
--- Mathlib/Logic/Equiv/Multiset.lean
+++ Mathlib/Logic/Equiv/Multiset.lean
@@ -3,8 +3,9 @@ Copyright (c) 2018 Mario Carneiro. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro
 -/
-import Mathlib.Logic.Equiv.List
+import Mathlib.Data.List.Chain
 import Mathlib.Data.Multiset.Sort
+import Mathlib.Logic.Equiv.List
 
 /-!
 # `Encodable` and `Denumerable` instances for `Multiset`
