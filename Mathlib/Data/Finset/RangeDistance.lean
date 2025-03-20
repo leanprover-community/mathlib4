105422aafd9c9fea7ca7356c26eab492c8f234d8
Christopher Hoskin <christopher.hoskin@gmail.com>
2025-03-20 19:12:15 +0000

    Tidy up

--- Mathlib/Data/Finset/RangeDistance.lean
+++ Mathlib/Data/Finset/RangeDistance.lean
@@ -19,20 +19,12 @@ open Finset
 
 lemma abs_sub_lt_of_mem_finset_range (N n m : ℕ) (hn : n ∈ range N) (hm : m ∈ range N) :
     |n - (m : ℤ)| < N := by
-  simp_all only [mem_range]
   rcases (Nat.lt_or_ge n m) with (h1 | h2)
-  · have e1 : 0 < m-(n : ℤ) := by
-      simp_all only [sub_pos, Int.ofNat_lt]
-    rw [abs_sub_comm]
-    rw [abs_of_pos e1]
-    rw [← Int.add_lt_add_iff_right n]
-    rw [sub_add_cancel]
+  · rw [← Int.ofNat_lt, ← sub_pos] at h1
+    rw [abs_sub_comm, abs_of_pos h1, ← Int.add_lt_add_iff_right n, sub_add_cancel]
     norm_cast
-    exact Nat.lt_add_right n hm
-  · have e1 : 0 ≤ n-(m : ℤ) := by
-      simp_all only [sub_nonneg, Int.ofNat_le]
-    rw [abs_of_nonneg e1]
-    rw [← Int.add_lt_add_iff_right m]
-    rw [sub_add_cancel]
+    exact Nat.lt_add_right n (mem_range.mp hm)
+  · rw [ge_iff_le, ← Int.ofNat_le, ← sub_nonneg] at h2
+    rw [abs_of_nonneg h2, ← Int.add_lt_add_iff_right m, sub_add_cancel]
     norm_cast
-    exact Nat.lt_add_right m hn
+    exact Nat.lt_add_right m (mem_range.mp hn)
