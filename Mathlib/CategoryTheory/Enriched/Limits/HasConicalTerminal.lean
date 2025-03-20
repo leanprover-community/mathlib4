58223f16caa2bae3da905c0da21c8045cc305d40
Jon Eugster <eugster.jon@gmail.com>
2025-03-20 14:45:39 +0100

    cleanup

--- Mathlib/CategoryTheory/Enriched/Limits/HasConicalTerminal.lean
+++ Mathlib/CategoryTheory/Enriched/Limits/HasConicalTerminal.lean
@@ -7,24 +7,20 @@ import Mathlib.CategoryTheory.Enriched.Limits.HasConicalProducts
 
 /-!
 ## existence of conical terminal objects
-gi-/
+-/
 
-universe v₁ u₁ v₂ u₂ w v' v u u'
+universe w v' v u u'
 
 namespace CategoryTheory.Enriched
 
 open Limits HasConicalLimit
 
-variable {J : Type u₁} [Category.{v₁} J] {K : Type u₂} [Category.{v₂} K]
-variable (V : Type u') [Category.{v'} V] [MonoidalCategory V]
-variable {C : Type u} [Category.{v} C] [EnrichedOrdinaryCategory V C]
-variable (F : J ⥤ C)
-
-variable [HasConicalLimit V F]
-
 /-- A category has a conical terminal object
 if it has a conical limit over the empty diagram. -/
 abbrev HasConicalTerminal := HasConicalLimitsOfShape (Discrete.{0} PEmpty)
+
+variable (V : Type u') [Category.{v'} V] [MonoidalCategory V]
+variable (C : Type u) [Category.{v} C] [EnrichedOrdinaryCategory V C]
 
 example [HasConicalTerminal V C] : HasTerminal C := inferInstance
 
