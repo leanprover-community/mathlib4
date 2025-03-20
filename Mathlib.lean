de8381daf95c688723f0ce249a75ff5ca2ce7fa7
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-03-20 22:37:03 +0000

    feat(AlgebraicTopology): endow `SSet` with notions of cofibrations and fibrations (#21118)
    We define cofibrations and fibrations in the category of simplicial sets. Kan complexes are redefined (through an abbreviation) as fibrant objects in this category.
    
    
    
    Co-authored-by: Joël Riou <37772949+joelriou@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1188,6 +1188,7 @@ import Mathlib.AlgebraicTopology.SimplicialObject.Coskeletal
 import Mathlib.AlgebraicTopology.SimplicialObject.Split
 import Mathlib.AlgebraicTopology.SimplicialSet.Basic
 import Mathlib.AlgebraicTopology.SimplicialSet.Boundary
+import Mathlib.AlgebraicTopology.SimplicialSet.CategoryWithFibrations
 import Mathlib.AlgebraicTopology.SimplicialSet.Coskeletal
 import Mathlib.AlgebraicTopology.SimplicialSet.Degenerate
 import Mathlib.AlgebraicTopology.SimplicialSet.HomotopyCat
