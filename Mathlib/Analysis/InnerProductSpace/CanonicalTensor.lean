1b6d0e7b616d14b7f90fcff0054b915368f5ab2a
Stefan Kebekus <kebekus@users.noreply.github.com>
2025-03-21 07:21:45 +0100

    Update Mathlib/Analysis/InnerProductSpace/CanonicalTensor.lean
    Co-authored-by: David Loeffler <d.loeffler.01@cantab.net>

--- Mathlib/Analysis/InnerProductSpace/CanonicalTensor.lean
+++ Mathlib/Analysis/InnerProductSpace/CanonicalTensor.lean
@@ -42,21 +42,20 @@ theorem InnerProductSpace.canonicalCovariantTensorRepresentation [FiniteDimensio
     {ι : Type*} [Fintype ι] (v : OrthonormalBasis ι ℝ E) :
     InnerProductSpace.canonicalCovariantTensor E = ∑ i, (v i) ⊗ₜ[ℝ] (v i) := by
   let w := stdOrthonormalBasis ℝ E
-  conv =>
-    right; arg 2; intro i; rw [← w.sum_repr' (v i)]
-  simp_rw [sum_tmul, tmul_sum, smul_tmul_smul]
-  conv =>
-    right; rw [Finset.sum_comm]
-    arg 2; intro y; rw [Finset.sum_comm]
-    arg 2; intro x; rw [← Finset.sum_smul]
-    arg 1; arg 2; intro i; rw [← real_inner_comm (w x)]
-  simp_rw [OrthonormalBasis.sum_inner_mul_inner v]
-  have {i} : ∑ j, ⟪w i, w j⟫_ℝ • w i ⊗ₜ[ℝ] w j = w i ⊗ₜ[ℝ] w i := by
-    rw [Fintype.sum_eq_single i, orthonormal_iff_ite.1 w.orthonormal]
-    simp only [↓reduceIte, one_smul]
-    intro _ _
+  calc ∑ m, w m ⊗ₜ[ℝ] w m
+  _ = ∑ m, ∑ n, ⟪w m, w n⟫_ℝ • w m ⊗ₜ[ℝ] w n := by
+    congr 1 with m
+    rw [Fintype.sum_eq_single m _, orthonormal_iff_ite.1 w.orthonormal]
+    · simp only [↓reduceIte, one_smul]
     simp only [orthonormal_iff_ite.1 w.orthonormal, ite_smul, one_smul, zero_smul,
       ite_eq_right_iff]
     tauto
-  simp_rw [this]
-  rfl
+  _ = ∑ m, ∑ n, (∑ i, ⟪w m, v i⟫_ℝ * ⟪v i, w n⟫_ℝ) • w m ⊗ₜ[ℝ] w n := by
+    simp_rw [OrthonormalBasis.sum_inner_mul_inner v]
+  _ = ∑ m, ∑ n, (∑ i, ⟪w m, v i⟫_ℝ * ⟪w n, v i⟫_ℝ) • w m ⊗ₜ[ℝ] w n := by
+    simp only [real_inner_comm (w _)]
+  _ = ∑ i, (∑ m, ⟪w m, v i⟫_ℝ • w m) ⊗ₜ[ℝ] ∑ n, ⟪w n, v i⟫_ℝ • w n := by
+    simp only [sum_tmul, tmul_sum, smul_tmul_smul, Finset.sum_comm (γ := ι), Finset.sum_smul]
+    rw [Finset.sum_comm]
+  _ = ∑ i, v i ⊗ₜ[ℝ] v i := by
+    simp only [w.sum_repr' (v _)]
