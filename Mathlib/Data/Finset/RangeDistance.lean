c5a5314b9d7ec655b1a439fa69322c5ea38c9749
Christopher Hoskin <christopher.hoskin@gmail.com>
2025-03-21 08:26:12 +0000

    Use max_sub_min_eq_abs' instead

--- Mathlib/Data/Finset/RangeDistance.lean
+++ Mathlib/Data/Finset/RangeDistance.lean
@@ -19,12 +19,7 @@ open Finset
 
 lemma abs_sub_lt_of_mem_finset_range {N n m : ℕ} (hn : n ∈ range N) (hm : m ∈ range N) :
     |n - (m : ℤ)| < N := by
-  rcases (Nat.lt_or_ge n m) with (h1 | h2)
-  · rw [← Int.ofNat_lt, ← sub_pos] at h1
-    rw [abs_sub_comm, abs_of_pos h1, ← Int.add_lt_add_iff_right n, sub_add_cancel]
-    norm_cast
-    exact Nat.lt_add_right n (mem_range.mp hm)
-  · rw [ge_iff_le, ← Int.ofNat_le, ← sub_nonneg] at h2
-    rw [abs_of_nonneg h2, ← Int.add_lt_add_iff_right m, sub_add_cancel]
-    norm_cast
-    exact Nat.lt_add_right m (mem_range.mp hn)
+  rw [← max_sub_min_eq_abs', sub_lt_iff_lt_add, ← Lean.Omega.Int.ofNat_max,
+    ← Lean.Omega.Int.ofNat_min]
+  norm_cast
+  exact Nat.lt_add_right (n ⊓ m) (sup_lt_iff.mpr ⟨List.mem_range.mp hn, List.mem_range.mp hm⟩)
