7f68a5b7866599a5473faf0bda0695815cfb85e4
Janos Wolosz <janos.wolosz@gmail.com>
2025-03-21 18:48:31 +0100

    Proove theta_h

--- Mathlib/Algebra/Lie/Weights/Theta.lean
+++ Mathlib/Algebra/Lie/Weights/Theta.lean
@@ -38,8 +38,8 @@ variable {K L : Type*} [Field K] [CharZero K] [LieRing L] [LieAlgebra K L]
 automorphism. -/
 noncomputable def theta {α : Weight K H L} {h e f : L} (hα : α.IsNonZero) (ht : IsSl2Triple h e f)
     (he : e ∈ rootSpace H α) (hf : f ∈ rootSpace H (- α)) (t : Kˣ) : L ≃ₗ⁅K⁆ L := by
-  let D1 := LieDerivation.instDerivation K L (t • e)
-  let D2 := LieDerivation.instDerivation K L (t⁻¹ • f)
+  let D1 := LieDerivation.instDerivation K L ((t : K) • e)
+  let D2 := LieDerivation.instDerivation K L ((t⁻¹ : K) • f)
   have he' : t • e ∈ rootSpace H α := by
     apply Submodule.smul_mem
     exact he
@@ -51,6 +51,46 @@ noncomputable def theta {α : Weight K H L} {h e f : L} (hα : α.IsNonZero) (ht
   have n₂ : IsNilpotent D2.toLinearMap := LieAlgebra.isNilpotent_ad_of_mem_rootSpace H n₀ hf'
   exact LieEquiv.trans (LieEquiv.trans (LieDerivation.exp D1 n₁) (LieDerivation.exp D2 n₂))
     (LieDerivation.exp D1 n₁)
+
+theorem theta_e {α : Weight K H L} {h e f : L} (hα : α.IsNonZero) (ht : IsSl2Triple h e f)
+    (he : e ∈ rootSpace H α) (hf : f ∈ rootSpace H (- α)) (t : Kˣ) :
+      theta H hα ht he hf t e = ((t⁻¹ ^ 2) : K) • f := by
+  sorry
+
+theorem theta_f {α : Weight K H L} {h e f : L} (hα : α.IsNonZero) (ht : IsSl2Triple h e f)
+    (he : e ∈ rootSpace H α) (hf : f ∈ rootSpace H (- α)) (t : Kˣ) :
+      theta H hα ht he hf t f = ((t ^ 2) : K) • e := by
+  sorry
+
+theorem theta_h {α : Weight K H L} {h e f : L} (hα : α.IsNonZero) (ht : IsSl2Triple h e f)
+    (he : e ∈ rootSpace H α) (hf : f ∈ rootSpace H (- α)) (t : Kˣ) :
+      theta H hα ht he hf t h = - h := by
+  calc
+    theta H hα ht he hf t h = theta H hα ht he hf t ⁅e, f⁆ := by
+      rw [ht.lie_e_f]
+    _ = ⁅theta H hα ht he hf t e, theta H hα ht he hf t f⁆ := by
+      apply LieHom.map_lie
+    _ = ⁅((t⁻¹) ^ 2 : K) • f, (t ^ 2 : K) • e⁆ := by
+      rw [theta_e H hα ht he hf t, theta_f H hα ht he hf t]
+    _ = ((t⁻¹) ^ 2 : K) • ⁅f, (t ^ 2 : K) • e⁆ := by
+      rw [smul_lie]
+    _ = ((t⁻¹) ^ 2 : K) • (t ^ 2 : K) • ⁅f, e⁆ := by
+      rw [lie_smul]
+    _ = (((t⁻¹) ^ 2 : K) * (t ^ 2 : K)) • ⁅f, e⁆ := by
+      simp
+    _ = ⁅f, e⁆ := by
+      simp_all only [Units.val_inv_eq_inv_val, inv_pow, isUnit_iff_ne_zero, ne_eq, OfNat.ofNat_ne_zero,
+        not_false_eq_true, pow_eq_zero_iff, Units.ne_zero, IsUnit.inv_mul_cancel, one_smul]
+    _ = - ⁅e, f⁆ := by
+      rw [lie_skew]
+    _ = - h := by
+      rw [ht.lie_e_f]
+
+
+
+
+
+
 
 /-
 lemma theta_apply {α : Weight K H L} {h e f : L} (hα : α.IsNonZero) (ht : IsSl2Triple h e f)
