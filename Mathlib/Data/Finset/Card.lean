ed230862f957df80ceafd5c3187fe6c9d07eb5ce
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2025-03-22 15:12:47 +0100

    chore: tidy various files

--- Mathlib/Data/Finset/Card.lean
+++ Mathlib/Data/Finset/Card.lean
@@ -527,7 +527,7 @@ theorem sdiff_nonempty_of_card_lt_card (h : #s < #t) : (t \ s).Nonempty := by
   rw [nonempty_iff_ne_empty, Ne, sdiff_eq_empty_iff_subset]
   exact fun h' ↦ h.not_le (card_le_card h')
 
-theorem exists_mem_not_mem_of_card_lt_card (h : #s < #t) :  ∃ e, e ∈ t ∧ e ∉ s := by
+theorem exists_mem_not_mem_of_card_lt_card (h : #s < #t) : ∃ e, e ∈ t ∧ e ∉ s := by
   simpa [Finset.Nonempty] using sdiff_nonempty_of_card_lt_card h
 
 @[simp]
