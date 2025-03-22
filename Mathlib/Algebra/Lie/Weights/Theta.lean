96a799f4870f67e63de3bc89c7f6d08c9f0854aa
Janos Wolosz <janos.wolosz@gmail.com>
2025-03-22 19:30:26 +0100

    Steps toward theta

--- Mathlib/Algebra/Lie/Weights/Theta.lean
+++ Mathlib/Algebra/Lie/Weights/Theta.lean
@@ -29,28 +29,26 @@ variable {K L : Type*} [Field K] [CharZero K] [LieRing L] [LieAlgebra K L]
   (H : LieSubalgebra K L) [LieRing.IsNilpotent H]
   [IsTriangularizable K H L] [FiniteDimensional K L]
 
+variable {α : Weight K H L} {h e f : L}
 
---lemma exists_isSl2Triple_of_weight_isNonZero {α : Weight K H L} (hα : α.IsNonZero) :
---    ∃ h e f : L, IsSl2Triple h e f ∧ e ∈ rootSpace H α ∧ f ∈ rootSpace H (- α) :=
-
-
-/-- In characteristic zero, the exponential of a nilpotent derivation is a Lie algebra
-automorphism. -/
-noncomputable def theta {α : Weight K H L} {h e f : L} (hα : α.IsNonZero) (ht : IsSl2Triple h e f)
-    (he : e ∈ rootSpace H α) (hf : f ∈ rootSpace H (- α)) (t : Kˣ) : L ≃ₗ⁅K⁆ L := by
-  let D1 := LieDerivation.instDerivation K L ((t : K) • e)
-  let D2 := LieDerivation.instDerivation K L ((t⁻¹ : K) • f)
+noncomputable def exp_ad_e (hα : α.IsNonZero) (he : e ∈ rootSpace H α) (t : Kˣ) : L ≃ₗ⁅K⁆ L := by
+  let D := LieDerivation.instDerivation K L ((t : K) • e)
   have he' : (t : K) • e ∈ rootSpace H α := by
     apply Submodule.smul_mem
     exact he
-  have hf' : (t⁻¹ : K) • f ∈ rootSpace H (- α) := by
+  exact LieDerivation.exp D (LieAlgebra.isNilpotent_ad_of_mem_rootSpace H hα he')
+
+noncomputable def exp_ad_f (hα : α.IsNonZero) (hf : f ∈ rootSpace H (-α)) (t : Kˣ) : L ≃ₗ⁅K⁆ L := by
+  let D := LieDerivation.instDerivation K L (-(t⁻¹ : K) • f)
+  have hf' : -(t⁻¹ : K) • f ∈ rootSpace H (- α) := by
     apply Submodule.smul_mem
     exact hf
   have n₀ : ((- α) : H → K) ≠ 0 := neg_ne_zero.mpr hα
-  have n₁ : IsNilpotent D1.toLinearMap := LieAlgebra.isNilpotent_ad_of_mem_rootSpace H hα he'
-  have n₂ : IsNilpotent D2.toLinearMap := LieAlgebra.isNilpotent_ad_of_mem_rootSpace H n₀ hf'
-  exact LieEquiv.trans (LieEquiv.trans (LieDerivation.exp D1 n₁) (LieDerivation.exp D2 n₂))
-    (LieDerivation.exp D1 n₁)
+  exact LieDerivation.exp D (LieAlgebra.isNilpotent_ad_of_mem_rootSpace H n₀ hf')
+
+noncomputable def theta (hα : α.IsNonZero) (he : e ∈ rootSpace H α) (hf : f ∈ rootSpace H (- α))
+    (t : Kˣ) : L ≃ₗ⁅K⁆ L := by
+  exact ((exp_ad_e H hα he t).trans (exp_ad_f H hα hf t)).trans (exp_ad_e H hα he t)
 
 theorem theta_e {α : Weight K H L} {h e f : L} (hα : α.IsNonZero) (ht : IsSl2Triple h e f)
     (he : e ∈ rootSpace H α) (hf : f ∈ rootSpace H (- α)) (t : Kˣ) :
