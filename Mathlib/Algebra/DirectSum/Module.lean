f5357b909c59cf3cd86276cf34bcccd522fb8e94
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2025-03-22 10:59:26 +0800

    Revert "fix changed name"
    This reverts commit 17c4031c0deac8b363e00f638eb20f2bb5df64ed.

--- Mathlib/Algebra/DirectSum/Module.lean
+++ Mathlib/Algebra/DirectSum/Module.lean
@@ -313,12 +313,12 @@ variable [∀ i, AddCommGroup (β i)] (f : ∀ i, α i →+ β i)
 
 lemma ker_map : (map f).ker =
     (AddSubgroup.pi Set.univ (f · |>.ker)).comap (DirectSum.coeFnAddMonoidHom α) :=
-  DFinsupp.AddCommGroup.ker_mapRangeAddMonoidHom f
+  DFinsupp.ker_mapRangeAddMonoidHom f
 
 lemma range_map : (map f).range =
     (AddSubgroup.pi Set.univ (f · |>.range)).comap (DirectSum.coeFnAddMonoidHom β) := by
   classical
-  exact DFinsupp.AddCommGroup.range_mapRangeAddMonoidHom f
+  exact DFinsupp.range_mapRangeAddMonoidHom f
 
 end
 
