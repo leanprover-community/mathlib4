86ba570fb25d45093e95719cc4cc150e069f00ab
Xavier Roblot <roblot@math.univ-lyon1.fr>
2025-03-23 10:07:18 +0100

    fix typing

--- Mathlib/Algebra/Group/Subgroup/Lattice.lean
+++ Mathlib/Algebra/Group/Subgroup/Lattice.lean
@@ -481,13 +481,13 @@ theorem _root_.AddSubgroup.toSubgroup_closure {A : Type*} [AddGroup A] (S : Set 
   Subgroup.toAddSubgroup.injective (Subgroup.toAddSubgroup_closure _ ).symm
 
 theorem toAddSubgroup'_closure {A : Type*} [AddGroup A] (S : Set (Multiplicative A)) :
-    (closure S).toAddSubgroup' = AddSubgroup.closure (Additive.ofMul ⁻¹' S) :=
+    (closure S).toAddSubgroup' = AddSubgroup.closure (Multiplicative.ofAdd ⁻¹' S) :=
   le_antisymm (toAddSubgroup'.to_galoisConnection.l_le <|
       (closure_le _).mpr <| AddSubgroup.subset_closure (G := A))
     ((AddSubgroup.closure_le _).mpr <| Subgroup.subset_closure (G := Multiplicative A))
 
 theorem _root_.AddSubgroup.toSubgroup'_closure (S : Set (Additive G)) :
-    (AddSubgroup.closure S).toSubgroup' = Subgroup.closure (Multiplicative.ofAdd ⁻¹' S) :=
+    (AddSubgroup.closure S).toSubgroup' = Subgroup.closure (Additive.ofMul ⁻¹' S) :=
   congr_arg AddSubgroup.toSubgroup' (toAddSubgroup'_closure _).symm
 
 @[to_additive]
