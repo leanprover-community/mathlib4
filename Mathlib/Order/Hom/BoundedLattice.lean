0554c2aa2e3e2d9e56eefb2d9ec9fb5126e3f55a
Parcly Taxel <reddeloostw@gmail.com>
2025-03-24 13:19:33 +0800

    shake

--- Mathlib/Order/Hom/BoundedLattice.lean
+++ Mathlib/Order/Hom/BoundedLattice.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yaël Dillies
 -/
 import Mathlib.Order.Hom.Lattice
+import Mathlib.Order.SymmDiff
 
 /-!
 # Bounded lattice homomorphisms
