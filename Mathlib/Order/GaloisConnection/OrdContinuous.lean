2a4c08b8bab695d51012c662a08a48951c0cfec7
Kalle Kytölä <kalle.kytola@aalto.fi>
2025-03-25 18:24:55 +0200

    Include the new file and add Yaël as an author.

--- Mathlib/Order/GaloisConnection/OrdContinuous.lean
+++ Mathlib/Order/GaloisConnection/OrdContinuous.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2025 Kalle Kytölä. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Kalle Kytölä
+Authors: Kalle Kytölä, Yaël Dillies
 -/
 import Mathlib.CategoryTheory.Adjunction.Basic
 import Mathlib.CategoryTheory.Category.Preorder
