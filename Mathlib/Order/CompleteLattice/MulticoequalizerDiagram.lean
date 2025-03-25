f816d2aac7e4ba072d493b3aa440f987cdbc2be6
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-03-25 16:39:22 +0100

    fix

--- Mathlib/Order/CompleteLattice/MulticoequalizerDiagram.lean
+++ Mathlib/Order/CompleteLattice/MulticoequalizerDiagram.lean
@@ -3,7 +3,7 @@ Copyright (c) 2025 Joël Riou. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joël Riou
 -/
-import Mathlib.Order.CompleteLattice
+import Mathlib.Order.CompleteLattice.Lemmas
 import Mathlib.CategoryTheory.Category.Preorder
 import Mathlib.CategoryTheory.Limits.Shapes.Multiequalizer
 import Mathlib.CategoryTheory.CommSq
