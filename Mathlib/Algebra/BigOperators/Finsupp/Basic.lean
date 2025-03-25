c64485589675d9fa1b37f8b40e4d445c245c683b
Johan Commelin <johan@commelin.net>
2025-03-25 16:13:37 +0100

    chore(Algebra/BigOperators/Finsupp): add Finsupp.sum_apply''
    Part of an attempt to factor some reusable bits out of
    `Mathlib/Topology/Category/Profinite/Nobeling.lean`
    
    This lemma applies in situations where `Finsupp.sum_apply'` does not,
    because coercions are getting in the way.
    
    We currently don't have a typeclass to express
    "addition on this FunLike is pointwise"
    so I have expressed that with explicit arguments.

--- Mathlib/Algebra/BigOperators/Finsupp/Basic.lean
+++ Mathlib/Algebra/BigOperators/Finsupp/Basic.lean
@@ -543,6 +543,21 @@ theorem Finset.sum_apply' : (∑ k ∈ s, f k) i = ∑ k ∈ s, f k i :=
 theorem Finsupp.sum_apply' : g.sum k x = g.sum fun i b => k i b x :=
   Finset.sum_apply _ _ _
 
+theorem Finsupp.sum_apply'' {A F : Type*} [AddZeroClass A] [AddCommMonoid F] [FunLike F γ B]
+    (g : ι →₀ A) (k : ι → A → F) (x : γ)
+    (hg0 : ∀ (a : ι), k a 0 = 0) (hgadd : ∀ (a : ι) (b₁ b₂ : A), k a (b₁ + b₂) = k a b₁ + k a b₂)
+    (h0 : (0 : F) x = 0) (hadd : ∀ (f g : F), (f + g : F) x = f x + g x) :
+    g.sum k x = g.sum (fun i a ↦ k i a x) := by
+  induction g using Finsupp.induction with
+  | h0 => simp [h0]
+  | ha i a f hf ha ih =>
+    rw [Finsupp.sum_add_index' hg0 hgadd, Finsupp.sum_add_index', hadd, ih]
+    · congr 1
+      rw [Finsupp.sum_single_index (hg0 i), Finsupp.sum_single_index]
+      simp [hg0, h0]
+    · simp [hg0, h0]
+    · simp [hgadd, hadd]
+
 theorem Finsupp.sum_sum_index' (h0 : ∀ i, t i 0 = 0) (h1 : ∀ i x y, t i (x + y) = t i x + t i y) :
     (∑ x ∈ s, f x).sum t = ∑ x ∈ s, (f x).sum t := by
   classical
