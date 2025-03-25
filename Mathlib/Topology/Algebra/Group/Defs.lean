4206148ebb604da9db3537d9d23ae96f39b99c57
Yury G. Kudryashov <urkud@urkud.name>
2025-03-25 00:53:01 -0500

    Move defs to a new file
    TODO: update module docstrings

---
+++ Mathlib/Topology/Algebra/Group/Defs.lean
@@ -0,0 +1,80 @@
+/-
+Copyright (c) 2017 Johannes Hölzl. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Johannes Hölzl, Mario Carneiro, Patrick Massot
+-/
+import Mathlib.Topology.Algebra.Monoid
+
+/-!
+# Definitions about topological groups
+-/
+
+universe u
+
+/-- Basic hypothesis to talk about a topological additive group. A topological additive group
+over `M`, for example, is obtained by requiring the instances `AddGroup M` and
+`ContinuousAdd M` and `ContinuousNeg M`. -/
+class ContinuousNeg (G : Type u) [TopologicalSpace G] [Neg G] : Prop where
+  continuous_neg : Continuous fun a : G => -a
+
+attribute [continuity, fun_prop] ContinuousNeg.continuous_neg
+
+/-- Basic hypothesis to talk about a topological group. A topological group over `M`, for example,
+is obtained by requiring the instances `Group M` and `ContinuousMul M` and
+`ContinuousInv M`. -/
+@[to_additive (attr := continuity)]
+class ContinuousInv (G : Type u) [TopologicalSpace G] [Inv G] : Prop where
+  continuous_inv : Continuous fun a : G => a⁻¹
+
+attribute [continuity, fun_prop] ContinuousInv.continuous_inv
+
+export ContinuousInv (continuous_inv)
+export ContinuousNeg (continuous_neg)
+
+/-- A topological (additive) group is a group in which the addition and negation operations are
+continuous.
+
+When you declare an instance that does not already have a `UniformSpace` instance,
+you should also provide an instance of `UniformSpace` and `UniformAddGroup` using
+`IsTopologicalAddGroup.toUniformSpace` and `uniformAddGroup_of_addCommGroup`. -/
+class IsTopologicalAddGroup (G : Type u) [TopologicalSpace G] [AddGroup G] : Prop
+    extends ContinuousAdd G, ContinuousNeg G
+
+@[deprecated (since := "2025-02-14")] alias TopologicalAddGroup :=
+  IsTopologicalAddGroup
+
+/-- A topological group is a group in which the multiplication and inversion operations are
+continuous.
+
+When you declare an instance that does not already have a `UniformSpace` instance,
+you should also provide an instance of `UniformSpace` and `UniformGroup` using
+`IsTopologicalGroup.toUniformSpace` and `uniformGroup_of_commGroup`. -/
+@[to_additive]
+class IsTopologicalGroup (G : Type*) [TopologicalSpace G] [Group G] : Prop
+    extends ContinuousMul G, ContinuousInv G
+
+@[deprecated (since := "2025-02-14")] alias TopologicalGroup :=
+  IsTopologicalGroup
+
+/-- A typeclass saying that `p : G × G ↦ p.1 - p.2` is a continuous function. This property
+automatically holds for topological additive groups but it also holds, e.g., for `ℝ≥0`. -/
+class ContinuousSub (G : Type*) [TopologicalSpace G] [Sub G] : Prop where
+  continuous_sub : Continuous fun p : G × G => p.1 - p.2
+
+/-- A typeclass saying that `p : G × G ↦ p.1 / p.2` is a continuous function. This property
+automatically holds for topological groups. Lemmas using this class have primes.
+The unprimed version is for `GroupWithZero`. -/
+@[to_additive existing]
+class ContinuousDiv (G : Type*) [TopologicalSpace G] [Div G] : Prop where
+  continuous_div' : Continuous fun p : G × G => p.1 / p.2
+
+-- see Note [lower instance priority]
+@[to_additive]
+instance (priority := 100) IsTopologicalGroup.to_continuousDiv
+    {G : Type u} [TopologicalSpace G] [Group G] [IsTopologicalGroup G] : ContinuousDiv G where
+  continuous_div' := by
+    simp only [div_eq_mul_inv]
+    exact continuous_fst.mul <| continuous_inv.comp continuous_snd
+
+export ContinuousSub (continuous_sub)
+export ContinuousDiv (continuous_div')
