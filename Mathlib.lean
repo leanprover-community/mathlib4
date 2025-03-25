870fd7fa922e2991688de0096104494d99fcfc69
Oliver Butterley <oliver.butterley@gmail.com>
2025-03-25 08:58:10 +0100

    rename file

--- Mathlib.lean
+++ Mathlib.lean
@@ -3881,7 +3881,7 @@ import Mathlib.MeasureTheory.Integral.Periodic
 import Mathlib.MeasureTheory.Integral.Pi
 import Mathlib.MeasureTheory.Integral.Prod
 import Mathlib.MeasureTheory.Integral.RieszMarkovKakutani.Basic
-import Mathlib.MeasureTheory.Integral.RieszMarkovKakutani.RealRMK
+import Mathlib.MeasureTheory.Integral.RieszMarkovKakutani.Real
 import Mathlib.MeasureTheory.Integral.SetIntegral
 import Mathlib.MeasureTheory.Integral.SetToL1
 import Mathlib.MeasureTheory.Integral.TorusIntegral
