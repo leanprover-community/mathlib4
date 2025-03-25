71e47772c9fecdbb16cc3a4ba20778f4c705bf08
Yury G. Kudryashov <urkud@urkud.name>
2025-03-25 02:28:07 -0500

    Fix lint (not really)

--- Mathlib/Topology/Algebra/Monoid/Defs.lean
+++ Mathlib/Topology/Algebra/Monoid/Defs.lean
@@ -1,4 +1,12 @@
+/-
+Copyright (c) 2017 Johannes Hölzl. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Johannes Hölzl, Mario Carneiro, Mitchell Lee
+-/
 import Mathlib.Topology.Constructions.SumProd
+
+/-!
+-/
 
 universe u v
 
