aa4f8f8b270daf99de0d15261dde7521cb84f2e8
Kim Morrison <kim@tqft.net>
2025-03-26 14:58:36 +1100

    lint

--- Mathlib/Algebra/Field/Subfield/Defs.lean
+++ Mathlib/Algebra/Field/Subfield/Defs.lean
@@ -201,8 +201,6 @@ theorem coe_toSubring (s : Subfield K) : (s.toSubring : Set K) = s :=
 theorem mem_toSubring (s : Subfield K) (x : K) : x ∈ s.toSubring ↔ x ∈ s :=
   Iff.rfl
 
-theorem subtype_toSubring (s : Subfield K) : s.toSubring.subtype = s.subtype := rfl
-
 end Subfield
 
 /-- A `Subring` containing inverses is a `Subfield`. -/
