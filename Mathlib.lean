8623dd13e0888f31ae832588640c86587d905aff
Eric Wieser <efw@google.com>
2025-03-26 13:12:44 +0000

    fix(Tactic/Use): do not allow unprotected cdot (#23257)
    Zulip thread: [#mathlib4 > &#96;use ·&#96; @ 💬](https://leanprover.zulipchat.com/#narrow/channel/287929-mathlib4/topic/.60use.20.C2.B7.60/near/448279748)
    
    
    
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -5572,6 +5572,7 @@ import Mathlib.Tactic.Widget.InteractiveUnfold
 import Mathlib.Tactic.Widget.SelectInsertParamsClass
 import Mathlib.Tactic.Widget.SelectPanelUtils
 import Mathlib.Tactic.Widget.StringDiagram
+import Mathlib.Tactic.WithoutCDot
 import Mathlib.Tactic.Zify
 import Mathlib.Testing.Plausible.Functions
 import Mathlib.Testing.Plausible.Sampleable
