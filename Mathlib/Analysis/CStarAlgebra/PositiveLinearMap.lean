6ca318f58b2852eaed39d9778b9d53b4fe6c6073
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2025-03-26 15:28:45 +0100

    todo

--- Mathlib/Analysis/CStarAlgebra/PositiveLinearMap.lean
+++ Mathlib/Analysis/CStarAlgebra/PositiveLinearMap.lean
@@ -132,6 +132,7 @@ instance {F : Type*} [FunLike F A₁ A₂] [LinearMapClass F ℂ A₁ A₂] [Ord
   map_continuous f := by
     have hbound : ∃ C : ℝ, ∀ a, ‖f a‖ ≤ C * ‖a‖ := by
       obtain ⟨C, h⟩ := exists_norm_apply_le (f : A₁ →ₚ[ℂ] A₂)
+      -- TODO missing simps lemmas
       exact ⟨C, h⟩
     exact (LinearMap.mkContinuousOfExistsBound (f : A₁ →ₗ[ℂ] A₂) hbound).continuous
 
