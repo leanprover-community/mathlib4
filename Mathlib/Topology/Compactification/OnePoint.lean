9d8b2887a520afbf41550aad0344bba996a14129
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-03-26 17:16:27 +0100

    Simp can prove this

--- Mathlib/Topology/Compactification/OnePoint.lean
+++ Mathlib/Topology/Compactification/OnePoint.lean
@@ -130,9 +130,7 @@ theorem range_coe_union_infty : range ((↑) : X → OnePoint X) ∪ {∞} = uni
 theorem insert_infty_range_coe : insert ∞ (range (@some X)) = univ :=
   insert_none_range_some _
 
-@[simp]
-theorem range_coe_inter_infty : range ((↑) : X → OnePoint X) ∩ {∞} = ∅ :=
-  range_some_inter_none X
+theorem range_coe_inter_infty : range ((↑) : X → OnePoint X) ∩ {∞} = ∅ := by simp
 
 @[simp]
 theorem compl_range_coe : (range ((↑) : X → OnePoint X))ᶜ = {∞} :=
