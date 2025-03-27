52bfbccc1578258555b8e7c60c0c698f4e29a1e7
Pablo Donato <pablo.donato@tuta.io>
2025-03-27 15:16:55 +0100

    Put back missing import

--- Mathlib/CategoryTheory/Topos/Classifier.lean
+++ Mathlib/CategoryTheory/Topos/Classifier.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Charlie Conneen, Pablo Donato
 -/
 import Mathlib.CategoryTheory.Limits.Shapes.RegularMono
+import Mathlib.CategoryTheory.Functor.ReflectsIso.Balanced
 import Mathlib.CategoryTheory.Subobject.Presheaf
 
 /-!
