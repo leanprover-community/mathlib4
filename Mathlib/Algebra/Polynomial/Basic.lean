07c40e2dd282f17685f3b3faf062483c72e0b134
mariainesdff <mariaines.dff@gmail.com>
2025-03-27 18:14:41 +0100

    delete lemma

--- Mathlib/Algebra/Polynomial/Basic.lean
+++ Mathlib/Algebra/Polynomial/Basic.lean
@@ -491,14 +491,6 @@ theorem C_mul_monomial : C a * monomial n b = monomial n (a * b) := by
 theorem monomial_mul_C : monomial n a * C b = monomial n (a * b) := by
   simp only [← monomial_zero_left, monomial_mul_monomial, add_zero]
 
-theorem C_finset_sum {α : Type*} (s : Finset α) (b : α → R) :
-    (s.sum fun x : α ↦ C (b x)) = C (s.sum b) := by
-  classical
-  refine s.induction_on ?_ ?_
-  · simp [Finset.sum_empty, _root_.map_zero]
-  · intro a s ha hs
-    rw [Finset.sum_insert ha, Finset.sum_insert ha, hs, C_add]
-
 /-- `X` is the polynomial variable (aka indeterminate). -/
 def X : R[X] :=
   monomial 1 1
