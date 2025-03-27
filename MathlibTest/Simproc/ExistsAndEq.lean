7fa13d1fd36cb6899fe9b977e9854052b66afb17
Vasilii Nesterov <vas.nesterov63@gmail.com>
2025-03-27 16:48:25 +0300

    fix

--- MathlibTest/Simproc/ExistsAndEq.lean
+++ MathlibTest/Simproc/ExistsAndEq.lean
@@ -11,6 +11,14 @@ example (a : α) (hp : p a) (hq : q a) : ∃ b : α, (p b ∧ b = a) ∧ q b := 
 example (a : α) : ∃ b : α, b = a := by
   simp only [existsAndEq]
 
+/--
+error: simp made no progress
+-/
+#guard_msgs in
+example (a : α) (f : α → α) (hp : p a) (hq : q a) : ∃ b : α, (p b ∧ b = f b) ∧ q b := by
+  simp only [existsAndEq]
+  sorry
+
 open Lean Meta Simp
 
 set_option linter.unusedTactic false in
