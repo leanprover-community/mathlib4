00cc9f41f87846e980b39ba11c54042ceca818dd
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-03-27 14:30:03 +0100

    Change

--- Mathlib/Data/Set/Disjoint.lean
+++ Mathlib/Data/Set/Disjoint.lean
@@ -30,6 +30,7 @@ protected theorem disjoint_iff : Disjoint s t ↔ s ∩ t ⊆ ∅ :=
 theorem disjoint_iff_inter_eq_empty : Disjoint s t ↔ s ∩ t = ∅ :=
   disjoint_iff
 
+@[simp]
 theorem _root_.Disjoint.inter_eq : Disjoint s t → s ∩ t = ∅ :=
   Disjoint.eq_bot
 
