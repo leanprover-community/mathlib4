bed8052a7be614511c86f7c96863cc05cdfd2cce
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2025-03-28 22:38:32 +0000

    refactor

--- Archive/Imo/Imo2020Q6.lean
+++ Archive/Imo/Imo2020Q6.lean
@@ -1,7 +1,8 @@
 import Mathlib
 
 
-section
+section lineMap
+
 variable {k E} [LinearOrderedField k] [OrderedAddCommGroup E]
 variable [Module k E] [OrderedSMul k E]
 
@@ -19,14 +20,13 @@ theorem left_le_lineMap_iff_le' (h : a < b) : a ≤ lineMap a b r ↔ 0 ≤ r :=
 theorem lineMap_le_right_iff_le' (h : a < b) : lineMap a b r ≤ b ↔ r ≤ 1 :=
   Iff.trans (by rw [lineMap_apply_one]) (lineMap_le_lineMap_iff_of_lt' h)
 
-end
+end lineMap
 
 open Finset
 
-
-theorem exists_between_and_le_dist (S : Finset ℝ) (n : Nat) (a b : ℝ) (hab : a < b)
-    (hS : #(S.filter (· ∈ Set.Ioo a b)) < n) :
-    ∃ x : ℝ, a < x ∧ x < b ∧ ∀ y ∈ S, (b - a) / (2 * n) ≤ |x - y| := by
+theorem exists_between_and_separated {ι : Type*} (S : Finset ι) (f : ι → ℝ) (n : Nat)
+    (a b : ℝ) (hab : a < b) (hS : #{p ∈ S | f p ∈ Set.Ioo a b} < n) :
+    ∃ x ∈ Set.Ioo a b, ∀ p ∈ S, (b - a) / (2 * n) ≤ |x - f p| := by
   have : n > 0 := by omega
   -- make `n` defEq to `_ + 1`
   cases' n with n; · contradiction
@@ -35,65 +35,59 @@ theorem exists_between_and_le_dist (S : Finset ℝ) (n : Nat) (a b : ℝ) (hab :
   let interval (i : Fin n) : Set ℝ :=
     Set.Ioo (AffineMap.lineMap a b ((i : ℝ) / n)) (AffineMap.lineMap a b (((i : ℝ) + 1) / n))
 
-  let rel (y : ℝ) (i : Fin n) : Prop :=
-    y ∈ interval i
+  let rel (p : ι) (k : Fin n) : Prop :=
+    f p ∈ interval k
 
-  by_cases h : ∀ i ∈ Finset.univ, ∃ y ∈ S.filter (· ∈ Set.Ioo a b), rel y i
-  · -- show that the intervals are disjoint
+  by_cases h : ∀ k ∈ Finset.univ, ∃ p ∈ ({p ∈ S | f p ∈ Set.Ioo a b} : Finset _), rel p k
+  · -- show that the `n` intervals are disjoint
     have disjoint : Pairwise fun i j => Disjoint (interval i) (interval j) := by
       rw [pairwise_disjoint_on]
       intro i j hlt
       unfold interval
-      rw [Set.Ioo_disjoint_Ioo]
-      apply le_sup_of_le_right; apply inf_le_of_left_le
-      replace hlt : (i : ℝ) + 1 ≤ j := by norm_cast
+      rw [Set.Ioo_disjoint_Ioo]; apply le_sup_of_le_right; apply inf_le_of_left_le
       rw [lineMap_le_lineMap_iff_of_lt' hab]
       gcongr
+      norm_cast
     -- use the pigeon hole principle on the disjoint intervals
     have := card_le_card_of_forall_subsingleton' rel h <| by
-      intro y _
+      simp only [mem_univ, true_and]
+      intro p _
       unfold rel
-      intro x ⟨_, hx⟩ y ⟨_, hy⟩
+      intro i hi j hj
       by_contra h
-      have : Disjoint (interval x) (interval y) := disjoint h
+      have : Disjoint (interval i) (interval j) := disjoint h
       rw [Set.disjoint_left] at this
-      exact this hx hy
+      exact this hi hj
     rw [card_univ, Fintype.card_fin] at this
     omega
-
-  push_neg at h
+  push_neg at h; simp at h
   -- the `i`th interval in disjoint with `S`
-  rcases h with ⟨i, _, h⟩
-  unfold rel at h
+  obtain ⟨i, h⟩ := h; unfold rel at h
   -- use the midpoint of the `i`th interval
-  use AffineMap.lineMap a b ((i : ℝ) / n + 1 / (2 * n))
-  have ineq₁: (i : ℝ) / n ≤ 1 - 1 / n := by
-    field_simp [n]; gcongr; exact Fin.is_le _
-  have : b - a > 0 := by linarith
-  -- check that the point is in between `0` and `1`
-  constructor
-  · simp [AffineMap.lineMap_apply_module']
+  use AffineMap.lineMap a b (i / n + 1 / (2 * n) : ℝ)
+  have ineq₁: (i / n : ℝ) ≤ 1 - 1 / n := by field_simp [n]; gcongr; apply Fin.is_le
+  have : b - a > 0 := sub_pos.mpr hab
+  -- check that the point is in between `a` and `b`
+  constructor; constructor
+  · simp [AffineMap.lineMap_apply_ring']
     positivity
-  constructor
-  · simp [AffineMap.lineMap_apply_module']
-    have ineq₂: (1 : ℝ) / (2 * n) < 1 / n := by
+  · rw [AffineMap.lineMap_apply_ring']
+    have ineq₂: (1 / (2 * n) : ℝ) < 1 / n := by
       gcongr; norm_cast; omega
     linear_combination (ineq₁ + ineq₂) * (b - a)
-  intro y hy
-  -- the `i`th interval is disjoint with `S`
-  have : y ∉ interval i := by
-    by_cases ha : a < y
-    · by_cases hb : y < b
-      · apply h
-        simp [ha, hb, hy]
-      · apply Set.not_mem_Ioo_of_ge
-        push_neg at hb
-        simp [AffineMap.lineMap_apply_module']
-        linear_combination ineq₁ * (b - a) + hb
+  intro p hp
+  -- the `i`th interval is disjoint with `f '' S`
+  have : f p ∉ interval i := by
+    by_cases ha : a < f p; by_cases hb : f p < b
+    · exact h p hp ha hb
+    · apply Set.not_mem_Ioo_of_ge
+      push_neg at hb
+      rw [AffineMap.lineMap_apply_ring']
+      linear_combination ineq₁ * (b - a) + hb
     · apply Set.not_mem_Ioo_of_le
-      have ineq₃ : (0:ℝ) ≤ i / n := by positivity
       push_neg at ha
-      simp [AffineMap.lineMap_apply_module']
+      rw [AffineMap.lineMap_apply_ring']
+      have ineq₃ : (0:ℝ) ≤ i / n := by positivity
       linear_combination ineq₃ * (b - a) + ha
   simp only [interval, Set.mem_Ioo, not_and_or, not_lt] at this
   -- `y ∈ S` is either above or below the interval, either way we get the bound
@@ -101,13 +95,13 @@ theorem exists_between_and_le_dist (S : Finset ℝ) (n : Nat) (a b : ℝ) (hab :
   · trans; swap; · apply le_abs_self
     rw [le_sub_iff_add_le, ← le_sub_iff_add_le']
     trans; · exact h
-    simp [AffineMap.lineMap_apply_module']
+    rw [AffineMap.lineMap_apply_ring', AffineMap.lineMap_apply_ring']
     ring_nf; rfl
   · rw [abs_sub_comm]
     trans; swap; · apply le_abs_self
     rw [le_sub_iff_add_le]
     trans; swap; · exact h
-    simp [AffineMap.lineMap_apply_module']
+    rw [AffineMap.lineMap_apply_ring', AffineMap.lineMap_apply_ring']
     ring_nf; rfl
 
 
@@ -116,7 +110,7 @@ open Module
 open scoped RealInnerProductSpace
 
 variable {V P : Type*} [NormedAddCommGroup V] [InnerProductSpace ℝ V] [MetricSpace P]
-variable [NormedAddTorsor V P] (k : Nat) [Fact (finrank ℝ V = k+1)]
+variable [NormedAddTorsor V P] (dim : Nat) [Fact (finrank ℝ V = dim+1)]
 
 
 noncomputable def project (a b p : P) : ℝ := innerSL ℝ (a -ᵥ b) (a -ᵥ p) / (‖a -ᵥ b‖ ^ 2)
@@ -130,25 +124,14 @@ noncomputable def project (a b p : P) : ℝ := innerSL ℝ (a -ᵥ b) (a -ᵥ p)
 
 open scoped Classical
 
-theorem exists_affine_between_and_le_dist {S : Finset P} (n : Nat) (a b : P)
-    (i j : ℝ) (hi : 0 ≤ i) (hij : i < j) (hj : j ≤ 1)
-    (hS : #((S.filter (project a b · ∈ Set.Ioo i j))) < n)
+theorem exists_affine_between_and_separated {ι : Type*} (S : Finset ι) (f : ι → P) (n : Nat)
+    (a b : P) (i j : ℝ) (hi : 0 ≤ i) (hij : i < j) (hj : j ≤ 1)
+    (hS : #{p ∈ S | project a b (f p) ∈ Set.Ioo i j} < n)
     (hab : a ≠ b) :
-    ∃ l : AffineSubspace ℝ P, finrank ℝ l.direction = k ∧ l.SOppSide a b ∧
-    ∀ p ∈ S, (j - i) * dist a b / (2 * n) ≤ Metric.infDist p l := by
+    ∃ l : AffineSubspace ℝ P, finrank ℝ l.direction = dim ∧ l.SOppSide a b ∧
+    ∀ p ∈ S, dist a b * ((j - i) / (2 * n)) ≤ Metric.infDist (f p) l := by
 
-  replace hS : #(filter (· ∈ Set.Ioo i j) (image (project a b) S)) < n := by
-    refine lt_of_le_of_lt ?_ hS
-    rw [filter_image]
-    exact card_image_le
-
-  have ha' : project a b a = 0 := by simp [project]
-  have hb' : project a b b = 1 := by
-    simp [project]
-    rw [real_inner_self_eq_norm_sq, div_self]
-    apply pow_ne_zero 2; rwa [norm_ne_zero_iff, vsub_ne_zero]
-
-  obtain ⟨x, lt_x, x_lt, hx⟩ := exists_between_and_le_dist (image (project a b) S) n i j hij hS
+  obtain ⟨x, x_ioo, hx⟩ := exists_between_and_separated S (project a b <| f ·) n i j hij hS
 
   use .mk' (AffineMap.lineMap a b x) (LinearMap.ker (innerₛₗ ℝ (a -ᵥ b)))
 
@@ -169,34 +152,29 @@ theorem exists_affine_between_and_le_dist {S : Finset P} (n : Nat) (a b : P)
     rwa [norm_pos_iff, vsub_ne_zero]
   constructor
   · refine Sbtw.sOppSide_of_not_mem_of_mem ?_ ?_ (AffineSubspace.self_mem_mk' _ _)
-    · simp [hab, lt_of_le_of_lt hi lt_x, lt_of_lt_of_le x_lt hj]
-    · simp [hab, (lt_of_le_of_lt hi lt_x).ne.symm]
+    · simp [hab, lt_of_le_of_lt hi x_ioo.1, lt_of_lt_of_le x_ioo.2 hj]
+    · simp [hab, (lt_of_le_of_lt hi x_ioo.1).ne.symm]
 
   intro p hp
   -- we show that the distance between `p` and the plane corresponds to
   -- the distance between `project p` and `x`.
-  specialize hx (project a b p) (Finset.mem_image_of_mem (project a b) hp)
+  specialize hx p hp
+  rw [project, sub_div' (by positivity), abs_div, le_div_iff₀' (by positivity),
+    abs_of_pos (by positivity)] at hx
   rw [Metric.infDist_eq_iInf]
   apply le_ciInf
-  intro ⟨y, hy⟩; simp only [dist_eq_norm_vsub]
-  simp at hy
-  have h : |inner (a -ᵥ b) (p -ᵥ (AffineMap.lineMap a b) x)| ≤ ‖a -ᵥ b‖ * ‖p -ᵥ y‖ := by
-    convert abs_real_inner_le_norm (a -ᵥ b) (p -ᵥ y) using 2
-    rw [← sub_eq_zero, ← inner_sub_right]; simpa
+  simp [dist_eq_norm_vsub]
+  intro y hy
   rw [← mul_le_mul_left this]
-  trans; swap; · exact h
-  rw [project, sub_div' (by positivity), abs_div, le_div_iff₀ (by positivity),
-    abs_eq_self.mpr (by positivity)] at hx
-  ring_nf at hx ⊢
-  trans; · exact hx
-  apply le_of_eq
-  rw [abs_eq_abs]; left
-  simp
-  rw [sub_eq_iff_eq_add, ← inner_add_right]
-  rw [add_comm]; simp
-  rw [inner_smul_right, real_inner_self_eq_norm_sq, mul_comm]
-
-
+  calc
+    _ = ‖a -ᵥ b‖ ^ 2 * ((j - i) / (2 * ↑n)) := by ring
+    _ ≤ |x * ‖a -ᵥ b‖ ^ 2 - ⟪a -ᵥ b, a -ᵥ f p⟫| := hx
+    _ = |⟪a -ᵥ b, f p -ᵥ (AffineMap.lineMap a b) x⟫| := by
+      congr 1
+      rw [sub_eq_iff_eq_add', ← inner_add_right]
+      simp [inner_smul_right, real_inner_self_eq_norm_sq]
+    _ = |⟪a -ᵥ b, f p -ᵥ y⟫| := by congr 1; rw [← sub_eq_zero, ← inner_sub_right]; simp; exact hy
+    _ ≤ ‖a -ᵥ b‖ * ‖f p -ᵥ y‖ := abs_real_inner_le_norm _ _
 
 variable [Fact (Module.finrank ℝ V = 2)]
 
@@ -216,29 +194,30 @@ theorem result : ∃ c : ℝ, 0 < c ∧ ∀ {n : ℕ} (hn : 1 < n) {S : Finset P
     obtain ⟨a, ha, b, hb, hab⟩ := h
     have : 0 < dist a b := lt_of_lt_of_le (by positivity) hab
     rw [dist_pos] at this
-    obtain ⟨l, rank, sOpp, h⟩ := exists_affine_between_and_le_dist 1 (n*2) a b 0 1
+    obtain ⟨l, rank, sOpp, h⟩ := exists_affine_between_and_separated 1 S (·) (n*2) a b 0 1
       le_rfl zero_lt_one le_rfl (lt_of_le_of_lt (card_filter_le S _) (by omega)) this
-    simp at h
+    norm_num at h
     use l, rank
     constructor; · use a, b, ha, hb
     intro p hp
     specialize h p hp
     trans; swap; · exact h
-    rw [le_div_iff₀ (by positivity)]
+    rw [← mul_inv_le_iff₀ (by positivity)]
     trans; swap; · exact hab
     rw [neg_div, Real.rpow_neg (by positivity)]
-    field_simp; rw [div_le_iff₀ (by positivity), ← Real.rpow_add (by positivity)]
-    rw [show (2/3+1/3 : ℝ) = 1 by norm_num, Real.rpow_one]
-    field_simp [c, div_le_iff₀]; linarith only
+    field_simp [div_le_iff₀]
+    rw [← Real.rpow_add (by positivity)]
+    norm_num; linarith only
 
   · push_neg at h
     -- If the points are closer than `n^(2/3)` together, then we can solve the problem by
     -- picking the furthest such points `a` and `b`, and choosing the best perpendiculer line
-    -- through the segment of with `1/2` at the edge.
+    -- through the segment of width `1/2` at the edge.
     obtain ⟨a, ha, b, hb, hab⟩ : ∃ᵉ (a ∈ S) (b ∈ S), ∀ᵉ (x ∈ S) (y ∈ S), dist x y ≤ dist a b := by
       -- sorry
       have : Nonempty S := Nonempty.to_subtype (Finset.card_pos.mp (by omega))
-      obtain ⟨⟨⟨a, ha⟩, ⟨b, hb⟩⟩, _, hab⟩ := Set.Finite.exists_maximal_wrt (fun xy : S × S => dist xy.1.val xy.2.val)
+      obtain ⟨⟨⟨a, ha⟩, ⟨b, hb⟩⟩, _, hab⟩ :=
+        Set.Finite.exists_maximal_wrt (fun xy : S × S => dist xy.1.val xy.2.val)
           Set.univ Set.finite_univ Set.univ_nonempty
       use a, ha, b, hb
       intro x hx y hy
@@ -248,5 +227,3 @@ theorem result : ∃ c : ℝ, 0 < c ∧ ∀ {n : ℕ} (hn : 1 < n) {S : Finset P
       constructor <;> linarith only [hab]
 
     sorry
-
-#check Submodule.mem_orthogonal_singleton_iff_inner_right
