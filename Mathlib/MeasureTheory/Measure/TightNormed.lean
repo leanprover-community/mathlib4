49fc34d59f8f6778a5332e7ec17a7d90c44ad758
Remy Degenne <remydegenne@gmail.com>
2025-03-28 10:25:39 +0100

    use ProperSpace

--- Mathlib/MeasureTheory/Measure/TightNormed.lean
+++ Mathlib/MeasureTheory/Measure/TightNormed.lean
@@ -3,8 +3,7 @@ Copyright (c) 2025 Rémy Degenne. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Rémy Degenne
 -/
-import Mathlib.Analysis.Normed.Module.FiniteDimension
-import Mathlib.Analysis.Normed.Order.Lattice
+import Mathlib.Analysis.Normed.Group.Basic
 import Mathlib.MeasureTheory.Measure.Tight
 import Mathlib.Order.CompletePartialOrder
 
@@ -15,9 +14,8 @@ Criteria for tightness of sets of measures in normed and inner product spaces.
 
 ## Main statements
 
-* `isTightMeasureSet_iff_tendsto_measure_norm_gt`: in a finite dimensional real normed space,
-  a set of measures `S` is tight if and only if the function `r ↦ ⨆ μ ∈ S, μ {x | r < ‖x‖}`
-  tends to `0` at infinity.
+* `isTightMeasureSet_iff_tendsto_measure_norm_gt`: in a proper normed group, a set of measures `S`
+  is tight if and only if the function `r ↦ ⨆ μ ∈ S, μ {x | r < ‖x‖}` tends to `0` at infinity.
 
 -/
 
@@ -27,44 +25,62 @@ open scoped Topology
 
 namespace MeasureTheory
 
-variable {E : Type*} [NormedAddCommGroup E] {mE : MeasurableSpace E} {S : Set (Measure E)}
+variable {E : Type*} {mE : MeasurableSpace E} {S : Set (Measure E)}
 
-section NormedSpace
+section PseudoMetricSpace
 
-lemma tendsto_measure_norm_gt_of_isTightMeasureSet (hS : IsTightMeasureSet S) :
-    Tendsto (fun r : ℝ ↦ ⨆ μ ∈ S, μ {x | r < ‖x‖}) atTop (𝓝 0) := by
-  suffices Tendsto ((⨆ μ ∈ S, μ) ∘ (fun r ↦ {x | r < ‖x‖})) atTop (𝓝 0) by
+variable [PseudoMetricSpace E]
+
+lemma tendsto_measure_compl_closedBall_of_isTightMeasureSet (hS : IsTightMeasureSet S) (x : E) :
+    Tendsto (fun r : ℝ ↦ ⨆ μ ∈ S, μ (Metric.closedBall x r)ᶜ) atTop (𝓝 0) := by
+  suffices Tendsto ((⨆ μ ∈ S, μ) ∘ (fun r ↦ (Metric.closedBall x r)ᶜ)) atTop (𝓝 0) by
     convert this with r
     simp
   refine hS.comp <| .mono_right ?_ <| monotone_smallSets Metric.cobounded_le_cocompact
-  refine HasAntitoneBasis.tendsto_smallSets ?_
-  exact ⟨Filter.atTop_basis_Ioi.cobounded_of_norm, fun _ _ hr x ↦ hr.trans_lt⟩
+  exact (Metric.hasAntitoneBasis_cobounded_compl_closedBall _).tendsto_smallSets
 
-section FiniteDimensional
-
-variable [NormedSpace ℝ E] [FiniteDimensional ℝ E]
-
-lemma isTightMeasureSet_of_tendsto_measure_norm_gt
-    (h : Tendsto (fun r : ℝ ↦ ⨆ μ ∈ S, μ {x | r < ‖x‖}) atTop (𝓝 0)) :
+lemma isTightMeasureSet_of_tendsto_measure_compl_closedBall [ProperSpace E] {x : E}
+    (h : Tendsto (fun r : ℝ ↦ ⨆ μ ∈ S, μ (Metric.closedBall x r)ᶜ) atTop (𝓝 0)) :
     IsTightMeasureSet S := by
   refine IsTightMeasureSet_iff_exists_isCompact_measure_compl_le.mpr fun ε hε ↦ ?_
   rw [ENNReal.tendsto_atTop_zero] at h
   obtain ⟨r, h⟩ := h ε hε
-  refine ⟨Metric.closedBall 0 r, isCompact_closedBall 0 r, ?_⟩
-  specialize h r le_rfl
-  simp only [iSup_le_iff] at h
-  convert h using 4 with μ hμ
+  exact ⟨Metric.closedBall x r, isCompact_closedBall x r, by simpa using h r le_rfl⟩
+
+/-- In a proper pseudo-metric space, a set of measures `S` is tight if and only if
+the function `r ↦ ⨆ μ ∈ S, μ (Metric.closedBall x r)ᶜ` tends to `0` at infinity. -/
+lemma isTightMeasureSet_iff_tendsto_measure_compl_closedBall [ProperSpace E] (x : E) :
+    IsTightMeasureSet S ↔ Tendsto (fun r : ℝ ↦ ⨆ μ ∈ S, μ (Metric.closedBall x r)ᶜ) atTop (𝓝 0) :=
+  ⟨fun hS ↦ tendsto_measure_compl_closedBall_of_isTightMeasureSet hS x,
+    isTightMeasureSet_of_tendsto_measure_compl_closedBall⟩
+
+end PseudoMetricSpace
+
+section NormedAddCommGroup
+
+variable [NormedAddCommGroup E]
+
+lemma tendsto_measure_norm_gt_of_isTightMeasureSet (hS : IsTightMeasureSet S) :
+    Tendsto (fun r : ℝ ↦ ⨆ μ ∈ S, μ {x | r < ‖x‖}) atTop (𝓝 0) := by
+  have h := tendsto_measure_compl_closedBall_of_isTightMeasureSet hS 0
+  convert h using 6 with r
   ext
   simp
 
-/-- In a finite dimensional real normed space, a set of measures `S` is tight if and only if
+lemma isTightMeasureSet_of_tendsto_measure_norm_gt [ProperSpace E]
+    (h : Tendsto (fun r : ℝ ↦ ⨆ μ ∈ S, μ {x | r < ‖x‖}) atTop (𝓝 0)) :
+    IsTightMeasureSet S := by
+  refine isTightMeasureSet_of_tendsto_measure_compl_closedBall (x := 0) ?_
+  convert h using 6 with r
+  ext
+  simp
+
+/-- In a proper normed group, a set of measures `S` is tight if and only if
 the function `r ↦ ⨆ μ ∈ S, μ {x | r < ‖x‖}` tends to `0` at infinity. -/
-lemma isTightMeasureSet_iff_tendsto_measure_norm_gt :
+lemma isTightMeasureSet_iff_tendsto_measure_norm_gt [ProperSpace E] :
     IsTightMeasureSet S ↔ Tendsto (fun r : ℝ ↦ ⨆ μ ∈ S, μ {x | r < ‖x‖}) atTop (𝓝 0) :=
   ⟨tendsto_measure_norm_gt_of_isTightMeasureSet, isTightMeasureSet_of_tendsto_measure_norm_gt⟩
 
-end FiniteDimensional
-
-end NormedSpace
+end NormedAddCommGroup
 
 end MeasureTheory
