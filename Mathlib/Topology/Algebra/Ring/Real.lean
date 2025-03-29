fdefaa4094939eb8e72ad6ffab24db760aee2bb9
Parcly Taxel <reddeloostw@gmail.com>
2025-03-30 01:07:09 +0800

    fix

--- Mathlib/Topology/Algebra/Ring/Real.lean
+++ Mathlib/Topology/Algebra/Ring/Real.lean
@@ -3,7 +3,7 @@ Copyright (c) 2017 Johannes Hölzl. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl, Mario Carneiro
 -/
-import Mathlib.Data.NNReal.Defs
+import Mathlib.Data.EReal.Order
 import Mathlib.Topology.Algebra.Order.Field
 import Mathlib.Topology.Algebra.UniformGroup.Defs
 import Mathlib.Topology.Bornology.Real
