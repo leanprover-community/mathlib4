206815a1b362554881765712d333d6654ea8e90e
Janos Wolosz <janos.wolosz@gmail.com>
2025-03-29 08:27:14 +0100

    Fix import

--- Mathlib/Algebra/Lie/Derivation/Basic.lean
+++ Mathlib/Algebra/Lie/Derivation/Basic.lean
@@ -3,7 +3,6 @@ Copyright (c) 2024 Frédéric Marbach. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Frédéric Marbach
 -/
-import Mathlib.Algebra.Algebra.Rat
 import Mathlib.Algebra.Lie.NonUnitalNonAssocAlgebra
 import Mathlib.Algebra.Lie.OfAssociative
 import Mathlib.Algebra.Lie.Subalgebra
