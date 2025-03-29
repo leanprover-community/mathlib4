8644d7df44d64b13952d1d5dad643c3c178cfc02
Miguel Marco <mmarco@unizar.es>
2025-03-29 15:40:19 +0100

    eliminate wrongly added variables

--- Mathlib/Data/Int/Order/Lemmas.lean
+++ Mathlib/Data/Int/Order/Lemmas.lean
@@ -19,15 +19,15 @@ namespace Int
 
 /-! ### nat abs -/
 
-theorem natAbs_eq_iff_mul_self_eq {a b c : ℤ} : a.natAbs = b.natAbs ↔ a * a = b * b := by
+theorem natAbs_eq_iff_mul_self_eq {a b : ℤ} : a.natAbs = b.natAbs ↔ a * a = b * b := by
   rw [← abs_eq_iff_mul_self_eq, abs_eq_natAbs, abs_eq_natAbs]
   exact Int.natCast_inj.symm
 
-theorem natAbs_lt_iff_mul_self_lt {a b c : ℤ} : a.natAbs < b.natAbs ↔ a * a < b * b := by
+theorem natAbs_lt_iff_mul_self_lt {a b : ℤ} : a.natAbs < b.natAbs ↔ a * a < b * b := by
   rw [← abs_lt_iff_mul_self_lt, abs_eq_natAbs, abs_eq_natAbs]
   exact Int.ofNat_lt.symm
 
-theorem natAbs_le_iff_mul_self_le {a b c : ℤ} : a.natAbs ≤ b.natAbs ↔ a * a ≤ b * b := by
+theorem natAbs_le_iff_mul_self_le {a b : ℤ} : a.natAbs ≤ b.natAbs ↔ a * a ≤ b * b := by
   rw [← abs_le_iff_mul_self_le, abs_eq_natAbs, abs_eq_natAbs]
   exact Int.ofNat_le.symm
 
