a54b3de86d29e867ff15a978fa2ac2cab1b30c6e
Paul Lezeau <paul.lezeau@gmail.com>
2025-03-29 18:05:07 +0000

    Update Mathlib/Util/Qq.lean
    Co-authored-by: Eric Wieser <efw@google.com>

--- Mathlib/Util/Qq.lean
+++ Mathlib/Util/Qq.lean
@@ -43,7 +43,6 @@ def mkDecideProofQ (p : Q(Prop)) : MetaM Q($p) := mkDecideProof p
 /-- Join a list of elements of type `α` into a container `m α`.
 
 Usually `m` is `q(Multiset)` or `q(Finset)` or `q(Set0`. -/
-
 def mkSetLiteralQ {u v : Level} {α : Q(Type u)} (m : Q(Type u → Type v))
     (elems : List Q($α))
     (_ : Q(EmptyCollection ($m $α)) := by exact q(inferInstance))
