6acf9ef6be7720b118c1e6ffe6ed16aa076c9678
Miguel Marco <mmarco@unizar.es>
2025-03-29 17:13:36 +0100

    Remove extra spaces

--- Mathlib/Data/Int/Order/Lemmas.lean
+++ Mathlib/Data/Int/Order/Lemmas.lean
@@ -80,35 +80,35 @@ theorem lt_div_iff_of_dvd_of_neg {a b c : ℤ} (hc : c < 0) (hcb : c ∣ b) :
     mul_lt_mul_left_of_neg hc]
 
 lemma div_le_div_iff_of_dvd_of_pos_of_pos {a b c d : ℤ} (hb : 0 < b) (hd : 0 < d) (hba : b ∣ a)
-    (hdc : d ∣ c) : a / b ≤  c / d ↔ d * a ≤ c * b := by
+    (hdc : d ∣ c) : a / b ≤ c / d ↔ d * a ≤ c * b := by
   obtain ⟨_, hx⟩ := hba
   obtain ⟨y, hy⟩ := hdc
   rw [hx, hy, mul_ediv_cancel_left _ (Int.ne_of_gt hd), mul_ediv_cancel_left _ (Int.ne_of_gt hb),
     mul_assoc,mul_comm y b,_root_.mul_le_mul_left hd,_root_.mul_le_mul_left hb]
 
 lemma div_le_div_iff_of_dvd_of_pos_of_neg {a b c d : ℤ} (hb : 0 < b) (hd : d < 0) (hba : b ∣ a)
-    (hdc : d ∣ c) : a / b ≤  c / d ↔ c * b ≤ d * a := by
+    (hdc : d ∣ c) : a / b ≤ c / d ↔ c * b ≤ d * a := by
   obtain ⟨_, hx⟩ := hba
   obtain ⟨y, hy⟩ := hdc
   rw [hx, hy, mul_ediv_cancel_left _ (Int.ne_of_lt hd), mul_ediv_cancel_left _ (Int.ne_of_gt hb),
     mul_assoc,mul_comm y b,mul_le_mul_left_of_neg hd,mul_le_mul_iff_of_pos_left hb]
 
 lemma div_le_div_iff_of_dvd_of_neg_of_pos {a b c d : ℤ} (hb : b < 0) (hd : 0 < d) (hba : b ∣ a)
-    (hdc : d ∣ c) : a / b ≤  c / d ↔ c * b ≤ d * a := by
+    (hdc : d ∣ c) : a / b ≤ c / d ↔ c * b ≤ d * a := by
   obtain ⟨_, hx⟩ := hba
   obtain ⟨y, hy⟩ := hdc
   rw [hx, hy, mul_ediv_cancel_left _ (Int.ne_of_lt hb), mul_ediv_cancel_left _ (Int.ne_of_gt hd),
     mul_assoc, mul_le_mul_iff_of_pos_left hd, mul_comm y b,mul_le_mul_left_of_neg hb]
 
 lemma div_le_div_iff_of_dvd_of_neg_of_neg {a b c d : ℤ} (hb : b < 0) (hd : d < 0) (hba : b ∣ a)
-    (hdc : d ∣ c) : a / b ≤  c / d ↔ d * a ≤ c * b := by
+    (hdc : d ∣ c) : a / b ≤ c / d ↔ d * a ≤ c * b := by
   obtain ⟨_, hx⟩ := hba
   obtain ⟨y, hy⟩ := hdc
   rw [hx, hy, mul_ediv_cancel_left _ (Int.ne_of_lt hd), mul_ediv_cancel_left _ (Int.ne_of_lt hb),
     mul_assoc,mul_comm y b,mul_le_mul_left_of_neg hd,mul_le_mul_left_of_neg hb]
 
 lemma div_lt_div_iff_of_dvd_of_pos {a b c d : ℤ} (hb : 0 < b) (hd : 0 < d) (hba : b ∣ a)
-    (hdc : d ∣ c) : a / b <  c / d ↔ d * a < c * b := by
+    (hdc : d ∣ c) : a / b < c / d ↔ d * a < c * b := by
   obtain ⟨_, hx⟩ := hba
   obtain ⟨y, hy⟩ := hdc
   rw [hx, hy, mul_ediv_cancel_left _ (Int.ne_of_gt hd), mul_ediv_cancel_left _ (Int.ne_of_gt hb),
@@ -129,7 +129,7 @@ lemma div_lt_div_iff_of_dvd_of_neg_of_pos {a b c d : ℤ} (hb : b < 0) (hd : 0 <
     mul_assoc, mul_lt_mul_iff_of_pos_left hd, mul_comm y b,mul_lt_mul_left_of_neg hb]
 
 lemma div_lt_div_iff_of_dvd_of_neg_of_neg {a b c d : ℤ} (hb : b < 0) (hd : d < 0) (hba : b ∣ a)
-    (hdc : d ∣ c) : a / b <  c / d ↔ d * a < c * b := by
+    (hdc : d ∣ c) : a / b < c / d ↔ d * a < c * b := by
   obtain ⟨_, hx⟩ := hba
   obtain ⟨y, hy⟩ := hdc
   rw [hx, hy, mul_ediv_cancel_left _ (Int.ne_of_lt hd), mul_ediv_cancel_left _ (Int.ne_of_lt hb),
