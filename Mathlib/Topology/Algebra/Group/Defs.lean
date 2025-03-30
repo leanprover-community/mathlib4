6e409d16b210926d2f805c0ddfc239eba85cf659
Riccardo Brasca <riccardo.brasca@gmail.com>
2025-03-30 21:01:06 +0200

    doc

--- Mathlib/Topology/Algebra/Group/Defs.lean
+++ Mathlib/Topology/Algebra/Group/Defs.lean
@@ -91,7 +91,7 @@ end ContinuousInv
 continuous.
 
 When you declare an instance that does not already have a `UniformSpace` instance,
-you should also provide an instance of `UniformSpace` and `UniformAddGroup` using
+you should also provide an instance of `UniformSpace` and `IsUniformAddGroup` using
 `IsTopologicalAddGroup.toUniformSpace` and `isUniformAddGroup_of_addCommGroup`. -/
 class IsTopologicalAddGroup (G : Type u) [TopologicalSpace G] [AddGroup G] : Prop
     extends ContinuousAdd G, ContinuousNeg G
@@ -103,7 +103,7 @@ class IsTopologicalAddGroup (G : Type u) [TopologicalSpace G] [AddGroup G] : Pro
 continuous.
 
 When you declare an instance that does not already have a `UniformSpace` instance,
-you should also provide an instance of `UniformSpace` and `UniformGroup` using
+you should also provide an instance of `UniformSpace` and `IsUniformGroup` using
 `IsTopologicalGroup.toUniformSpace` and `isUniformGroup_of_commGroup`. -/
 @[to_additive]
 class IsTopologicalGroup (G : Type*) [TopologicalSpace G] [Group G] : Prop
