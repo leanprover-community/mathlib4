afb87ef1e3efe38af647182b995cb3531346e731
Alex Meiburg <timeroot.alex@gmail.com>
2025-03-30 15:11:21 -0400

    rename

--- Mathlib/Algebra/Clone/Basic.lean
+++ Mathlib/Algebra/Clone/Basic.lean
@@ -3,7 +3,7 @@ Copyright (c) 2024 Alex Meiburg. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Alex Meiburg
 -/
-import Mathlib.Algebra.Clone.Notation
+import Mathlib.Algebra.Clone.Defs
 import Mathlib.Algebra.Order.Ring.Nat
 import Mathlib.Data.Fin.Basic
 
