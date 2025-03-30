4cf46a7090424413da2a6d5600305673b3a98ea4
Scott Carnahan <128885296+ScottCarnahan@users.noreply.github.com>
2025-03-31 00:53:08 +0900

    fix induction

--- Mathlib/Algebra/Polynomial/Laurent.lean
+++ Mathlib/Algebra/Polynomial/Laurent.lean
@@ -628,18 +628,18 @@ variable [Semiring R] [Semiring S] [Module R S] [SMulCommClass R S S]
 
 theorem smeval_T_pow_mul (n : ℤ) : (T n * f).smeval x = (x ^ n).val * f.smeval x := by
   induction f using LaurentPolynomial.induction_on' with
-  | h_add p q hp hq =>
+  | add p q hp hq =>
     rw [mul_add, smeval_add, hp, hq, ← mul_add, smeval_add]
-  | h_C_mul_T m r =>
+  | C_mul_T m r =>
     rw [T_mul, mul_T_assoc, smeval_C_mul_T_n, smeval_C_mul_T_n, add_comm, zpow_add, Units.val_mul,
       ← smul_eq_mul, smul_comm, smul_eq_mul]
 
 @[simp]
 theorem smeval_mul [IsScalarTower R S S] : (f * g).smeval x = f.smeval x * g.smeval x := by
   induction f using LaurentPolynomial.induction_on' with
-  | h_add _ _ hp hq=>
+  | add _ _ hp hq=>
     rw [add_mul, smeval_add, smeval_add, hp, hq, add_mul]
-  | h_C_mul_T n r =>
+  | C_mul_T n r =>
     rw [smeval_C_mul, mul_assoc, smeval_C_mul, smeval_T_pow, smeval_T_pow_mul, smul_mul_assoc]
 
 end Algebra
