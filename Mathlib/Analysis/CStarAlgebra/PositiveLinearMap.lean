47c3c2f3801a6c93ee6669c8f40756d44fbdb665
Frédéric Dupuis <31101893+dupuisf@users.noreply.github.com>
2025-03-29 21:44:46 -0400

    Update Mathlib/Analysis/CStarAlgebra/PositiveLinearMap.lean
    Co-authored-by: Jireh Loreaux <loreaujy@gmail.com>

--- Mathlib/Analysis/CStarAlgebra/PositiveLinearMap.lean
+++ Mathlib/Analysis/CStarAlgebra/PositiveLinearMap.lean
@@ -92,67 +92,38 @@ lemma exists_norm_apply_le (f : A₁ →ₚ[ℂ] A₂) : ∃ C : ℝ≥0, ∀ a,
     gcongr
     exact hy_norm i
   -- Let's proceed by contradiction
-  by_contra hcontra
-  push_neg at hcontra
-  let x' n := Classical.choose (hcontra (2 ^ (2 * n)))
-  have hx' n := Classical.choose_spec (hcontra ((2 : ℝ≥0) ^ (2 * n)))
-  -- Let `x n` be a sequence such that `‖x n‖ = 1` and `‖f (x n)‖ ≥ 4^n`.
-  let x n := ‖x' n‖⁻¹ • x' n
-  have hnz : ∀ n, x' n ≠ 0 := by
-    by_contra H
-    push_neg at H
-    obtain ⟨m, hm⟩ := H
-    unfold x' at hm
-    specialize hx' m
-    simp [hm] at hx'
-  have hx : ∀ n, 0 ≤ x n ∧ 2 ^ (2 * n) < ‖f (x n)‖ := fun n => by
-    refine ⟨smul_nonneg (by positivity) (hx' n).1, ?_⟩
-    unfold x
-    simp only [f.map_smul_of_tower, norm_smul, norm_inv, Complex.norm_real,
-      norm_norm, gt_iff_lt]
-    rw [lt_inv_mul_iff₀ (norm_pos_iff.mpr (hnz n)), mul_comm]
-    exact (hx' n).2
-  have hx_norm : ∀ n, ‖x n‖ = 1 := by
-    intro n
-    unfold x
-    have : ‖x' n‖ ≠ 0 := by aesop
-    simp [norm_smul, inv_mul_cancel₀ this]
-  -- Consider the sum `∑ n, 2^{-n} • x n`.
-  let xsum := ∑' (n : ℕ), (2 : ℝ) ^ (-(n : ℤ)) • x n
-  -- This sum converges to some element
+by_contra! hcontra
+  -- Given `n : ℕ`, we can always choose a positive element of norm one with `2 ^ (2 * n) < ‖f x‖`
+  have (n : ℕ) : ∃ x, 0 ≤ x ∧ ‖x‖ = 1 ∧ 2 ^ (2 * n) < ‖f x‖ := by
+    obtain ⟨hx₁, hx₂⟩ := Classical.choose_spec (hcontra (2 ^ (2 * n)))
+    set x := Classical.choose (hcontra (2 ^ (2 * n)))
+    have hx := (eq_zero_or_norm_pos x).resolve_left (fun hx ↦ by simp_all)
+    refine ⟨‖x‖⁻¹ • x, smul_nonneg (by positivity) hx₁, ?_, ?_⟩
+    · simp [norm_smul, inv_mul_cancel₀ hx.ne']
+    · simpa [norm_smul] using (lt_inv_mul_iff₀' hx).mpr hx₂
+  -- Let `x n` be a sequence of nonnegative elements such that `‖x n‖ = 1` and `‖f (x n)‖ ≥ 4 ^ n`.
+  choose x hx using this
+  simp only [forall_and] at hx
+  obtain ⟨hx_nonneg, hx_norm, hx⟩ := hx
+  -- `∑ n, 2 ^ (-n) • x n` converges
   have x_summable : Summable fun n : ℕ => (2 : ℝ) ^ (-(n : ℤ)) • x n := by
     refine Summable.of_norm ?_
     have : (2 : ℝ)⁻¹ < 1 := by norm_num
     simp [norm_smul, hx_norm, ← inv_pow, this]
-  -- Then `f` applied to a single term of the sum is upper-bounded by `f xsum`,
-  -- and, taking norms, `‖f xsum‖ ≥ 2 ^ n` for any `n`, which is a contradiction.
-  have h₁ : ∀ n : ℕ, f ((2 : ℝ) ^ (-(n : ℤ)) • x n) ≤ f xsum := by
-    intro n
+  -- There is some `n` such that `‖f (∑' m, 2 ^ (-m) • x m)‖ < 2 ^ n`
+  obtain ⟨n, hn⟩ : ∃ n : ℕ, ‖f (∑' (n : ℕ), (2 : ℝ) ^ (-(n : ℤ)) • x n)‖ < (2 : ℝ) ^ n :=
+    tendsto_pow_atTop_atTop_of_one_lt (show (1 : ℝ) < 2 by norm_num) |>.eventually_gt_atTop _
+      |>.exists
+  -- But `2 ^ n ≤ ‖f (2 ^ (-n) • x n)‖ ≤ ‖f (∑' m, 2 ^ (-m) • x m)‖`, which is a contradiction.
+  apply hn.not_le
+  trans ‖f ((2 : ℝ) ^ (-n : ℤ) • x n)‖
+  · have := hx n |>.le
+    rw [pow_mul', sq] at this
+    simpa [norm_smul] using (le_inv_mul_iff₀' (show 0 < (2 : ℝ) ^ n by positivity)).mpr this
+  · have (m : ℕ) : 0 ≤ ((2 : ℝ) ^ (-(m : ℤ)) • x m) := smul_nonneg (by positivity) (hx_nonneg m)
+    refine CStarAlgebra.norm_le_norm_of_nonneg_of_le (f.map_nonneg (this n)) ?_
     gcongr
-    exact le_tsum x_summable _ fun j hj => smul_nonneg (by positivity) (hx j).1
-  have h₂ : ∀ n : ℕ, 2 ^ n ≤ ‖f xsum‖ := fun n => by
-    calc _ = (2 : ℝ) ^ (n : ℤ) := rfl
-      _ = (2 : ℝ) ^ (-(n : ℤ)) * 2 ^ (2 * (n : ℤ)) := by
-          rw [← zpow_add₀ (a := 2) (by norm_num) (-n) (2 * n)]
-          congr
-          ring
-      _ ≤ ‖f ((2 : ℝ) ^ (-(n : ℤ)) • x n)‖ := by
-          simp only [f.map_smul_of_tower, zpow_neg, zpow_natCast, norm_smul,
-            norm_inv, norm_pow, Real.norm_ofNat, inv_pos, Nat.ofNat_pos, pow_pos, mul_le_mul_left]
-          exact le_of_lt (hx n).2
-      _ ≤ ‖f xsum‖ := by
-          refine CStarAlgebra.norm_le_norm_of_nonneg_of_le ?_ (h₁ n)
-          exact f.map_nonneg <| smul_nonneg (by positivity) (hx n).1
-  have h₃ : ∃ n : ℕ, ⌈‖f xsum‖⌉₊ < (2 : ℝ) ^ n := by
-    refine Eventually.exists (f := atTop) ?_
-    refine Tendsto.eventually_gt_atTop ?_ _
-    exact tendsto_pow_atTop_atTop_of_one_lt (by norm_num)
-  obtain ⟨n, hn⟩ := h₃
-  have hfinal : (2 : ℝ) ^ n < 2 ^ n := calc
-    _ ≤ ‖f xsum‖ := h₂ n
-    _ ≤ ⌈‖f xsum‖⌉₊ := Nat.le_ceil ‖f xsum‖
-    _ < 2 ^ n := hn
-  linarith
+    exact le_tsum x_summable n fun m _ ↦ this m
 
 instance {F : Type*} [FunLike F A₁ A₂] [PositiveLinearMapClass F ℂ A₁ A₂] :
     ContinuousLinearMapClass F ℂ A₁ A₂ where
