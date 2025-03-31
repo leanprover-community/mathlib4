7d1ff5a3cdc141c5f09c5deae52aabd52d94ef60
adomani <adomani@gmail.com>
2025-03-31 09:48:14 +0200

    fix(test): add missing `approximately` flag to count_heartbeats

--- MathlibTest/CountHeartbeats.lean
+++ MathlibTest/CountHeartbeats.lean
@@ -3,9 +3,9 @@ import Mathlib.Util.SleepHeartbeats
 
 set_option linter.style.header false
 
-/-- info: Used 7 heartbeats, which is less than the current maximum of 200000. -/
+/-- info: Used approximately 0 heartbeats, which is less than the current maximum of 200000. -/
 #guard_msgs in
-#count_heartbeats in
+#count_heartbeats approximately in
 example (a : Nat) : a = a := rfl
 
 /-- info: Used 7 heartbeats, which is less than the minimum of 200000. -/
