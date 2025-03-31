538f503e9cab84e526fda9ef5882df5ef2b251a2
Oliver Butterley <oliver.butterley@gmail.com>
2025-03-31 11:36:56 +0200

    trim

--- Mathlib/MeasureTheory/Integral/RieszMarkovKakutani/iUnion_Ixx_Ixx.lean
+++ Mathlib/MeasureTheory/Integral/RieszMarkovKakutani/iUnion_Ixx_Ixx.lean
@@ -11,12 +11,10 @@ import Mathlib.Algebra.CharP.Defs
 
 /-!
 TO DO:
-- Tidy this result find a good location for it `#find_home`
-- Tidy the use of the result in `Real.lean`.
-- Minimize imports `#min_imports`
+- Tidy this result, add natural variants, find a good location for it.
 
 Similar to lemmas in `Mathlib/Order/Interval/Set/Disjoint.lean` but adding this there is probably
-not good because it would require more imports
+not good because it would require more imports.
 -/
 
 open Set
@@ -44,9 +42,3 @@ lemma iUnion_Ioc_Ioc {X : Type*} [LinearOrderedSemiring X]
 --     exact ⟨i, i.2, hi⟩
 --   · rintro ⟨i, hiN, hi⟩
 --     exact ⟨⟨i, hiN⟩, hi⟩
-
--- lemma iUnion_Ioc_Ioc' {N : ℕ} (c : ℝ) {δ : ℝ} (hδ : 0 < δ) :
---     ⋃ n : Fin N, Ioc (c + n * δ) (c + n * δ + δ) = Ioc (c) (c + N * δ) := by
---   rw [Fin_to_Nat N (fun n => Ioc (c + n * δ) (c + n * δ + δ))]
---   -- Use the above to prove this.
---   exact iUnion_Ioc_Ioc N c (show 0 ≤ δ by linarith)
