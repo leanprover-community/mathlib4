39afc3014c384201f029afb43fdb538326a3bc36
Anne Baanen <vierkantor@vierkantor.com>
2025-03-31 11:38:34 +0000

    refactor(Data): split off material on `Set.Pairwise` from `List.Nodup` (#23407)
    Importing `Data.Set.Pairwise` in `Data.List.Nodup` comes with quite a bit of transitive dependencies, including on the long pole. I tried reversing the import direction, but going the other way lead to a lot more transitive imports. So let's split the files.

--- Mathlib.lean
+++ Mathlib.lean
@@ -3252,6 +3252,7 @@ import Mathlib.Data.Set.Opposite
 import Mathlib.Data.Set.Order
 import Mathlib.Data.Set.Pairwise.Basic
 import Mathlib.Data.Set.Pairwise.Lattice
+import Mathlib.Data.Set.Pairwise.List
 import Mathlib.Data.Set.Piecewise
 import Mathlib.Data.Set.Pointwise.Support
 import Mathlib.Data.Set.Prod
