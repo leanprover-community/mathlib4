d7fa9dbefca2c74003f483db2dfb7279308f86cb
Whysoserioushah <yx3021@ic.ac.uk>
2025-03-31 17:57:48 +0100

    remove span'

--- Mathlib/RingTheory/TwoSidedIdeal/SpanAsSum.lean
+++ Mathlib/RingTheory/TwoSidedIdeal/SpanAsSum.lean
@@ -27,57 +27,25 @@ variable {R : Type*} [Ring R]
 
 namespace TwoSidedIdeal
 
-private def span' (s : Set R) : TwoSidedIdeal R := .mk'
-  {x | ∃ (ι : Type) (fin : Fintype ι) (xL : ι → R) (xR : ι → R) (y : ι → s),
-    x = ∑ i : ι, xL i * y i * xR i}
-  ⟨Empty, Fintype.instEmpty, Empty.elim, Empty.elim, Empty.elim, by simp⟩
-  (by
-    rintro _ _ ⟨na, fina, xLa, xRa, ya, rfl⟩ ⟨nb, finb, xLb, xRb, yb, rfl⟩
-    refine ⟨na ⊕ nb, inferInstance, Sum.elim xLa xLb, Sum.elim xRa xRb, Sum.elim ya yb, by
-      simp⟩)
-  (by
-    rintro _ ⟨n, finn, xL, xR, y, rfl⟩
-    exact ⟨n, finn, -xL, xR, y, by simp⟩)
-  (by
-    rintro a _ ⟨n, finn, xL, xR, y, rfl⟩
-    exact ⟨n, finn, a • xL, xR, y, by
-      rw [Finset.mul_sum]; simp only [mul_assoc, Pi.smul_apply, smul_eq_mul]⟩)
-  (by
-    rintro _ b ⟨n, finn, xL, xR, y, rfl⟩
-    exact ⟨n, finn, xL, fun x ↦ xR x * b, y, by simp [Finset.sum_mul, mul_assoc]⟩)
-
-private lemma mem_span'_iff_exists_fin (s : Set R) (x : R) :
-    x ∈ span' s ↔
-    ∃ (ι : Type) (_ : Fintype ι) (xL : ι → R) (xR : ι → R) (y : ι → s),
-    x = ∑ i : ι, xL i * (y i : R) * xR i := by
-  simp only [span', mem_mk', Set.mem_setOf_eq]
-
 lemma mem_span_iff_exists_fin (s : Set R) (x : R) :
     x ∈ span s ↔
     ∃ (ι : Type) (_ : Fintype ι) (xL : ι → R) (xR : ι → R) (y : ι → s),
     x = ∑ i : ι, xL i * (y i : R) * xR i := by
-  suffices eq1 : span s = span' s by
-    rw [eq1]
-    simp only [span', Set.mem_setOf_eq]
-    generalize_proofs h1 h2 h3 h4 h5
-    simp_all only [mem_mk', Set.mem_setOf_eq]
-
-  rw [span, RingCon.ringConGen_eq]
-  apply ringCon_injective
-  refine sInf_eq_of_forall_ge_of_forall_gt_exists_lt ?_ ?_
-  · rintro I (hI : ∀ a b, _ → _)
-    suffices span' s ≤ .mk I by
-      rw [ringCon_le_iff] at this
-      exact this
-    intro x h
-    rw [mem_span'_iff_exists_fin] at h
-    obtain ⟨n, finn, xL, xR, y, rfl⟩ := h
-    rw [mem_iff]
-    refine TwoSidedIdeal.finsetSum_mem _ _ _ fun i _ ↦ TwoSidedIdeal.mul_mem_right _ _ _
-      (TwoSidedIdeal.mul_mem_left _ _ _ <| hI (y i) 0 (by simp))
-  · rintro I hI
-    exact ⟨(span' s).ringCon, fun a b H ↦ ⟨PUnit, inferInstance, fun _ ↦ 1, fun _ ↦ 1,
-      fun _ ↦ ⟨a - b, by simp [H]⟩, by simp⟩, hI⟩
+  constructor
+  · intro hx
+    refine span_induction
+      (fun y hy ↦ ⟨PUnit, inferInstance, fun _ ↦ 1, fun _ ↦ 1, fun _ ↦ ⟨y, hy⟩, by simp⟩)
+      ⟨Empty, Fintype.instEmpty, Empty.elim, Empty.elim, Empty.elim, by simp⟩
+      (fun x y hx hy ⟨ι1, _, xL1, xR1, y1, eq1⟩ ⟨ι2, _, xL2, xR2, y2, eq2⟩ ↦ ?_)
+      (fun x hx ⟨ι, _, xL, xR, y, _⟩ ↦ ?_) (fun a b x hx ⟨ι, _, xL, xR, y, _⟩ ↦ ?_) hx
+    · exact ⟨(ι1 ⊕ ι2), inferInstance, Sum.elim xL1 xL2, Sum.elim xR1 xR2, Sum.elim y1 y2,
+        by simp [eq1, eq2]⟩
+    · exact ⟨ι, inferInstance, (fun i ↦ - (xL i)), xR, y, by simp [*]⟩
+    · exact ⟨ι, inferInstance, (fun i ↦ a * xL i), (fun i ↦ xR i * b), y, by
+        simp [*, Finset.mul_sum, Finset.sum_mul, ← mul_assoc]⟩
+  · rintro ⟨_, _, _, _, _, rfl⟩
+    exact finsetSum_mem _ _ _ <| fun _ _ ↦ mul_mem_right _ _ _ <| mul_mem_left _ _ _ <|
+      subset_span <| Subtype.coe_prop _
 
 lemma mem_span_ideal_iff_exists_fin (s : Ideal R) (x : R) :
     x ∈ span s ↔
