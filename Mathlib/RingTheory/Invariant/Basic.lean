ab9f8df2d0b767357cb322bff7cff7f6b82367aa
Andrew Yang <the.erd.one@gmail.com>
2025-03-31 02:27:12 +0100

    remove

--- Mathlib/RingTheory/Invariant/Basic.lean
+++ Mathlib/RingTheory/Invariant/Basic.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2024 Thomas Browning. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Thomas Browning, Andrew Yang
+Authors: Thomas Browning
 -/
 import Mathlib.RingTheory.IntegralClosure.IntegralRestrict
 
