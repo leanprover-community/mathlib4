0d557a8e36a193e96d1342af6cb025c85a3c4c62
Joseph Myers <jsm@polyomino.org.uk>
2025-03-31 11:38:31 +0000

    feat(Geometry/Euclidean/Simplex): simplex angle properties (#21960)
    Show that the angles of an equilateral simplex in a Euclidean space are `π / 3`, and define the property of a simplex (typically a triangle) being acute-angled.  (This property is more common and more useful than the alternatives of "has a right angle" and "has an obtuse angle", since when it's given that a triangle has one of the latter properties, normally it's stated *which* angle is a right angle or an obtuse angle, in which case you just have an equation or inequality for a given angle.  So while those other properties will be wanted eventually - occasionally a problem does involve such properties without stating which angle is involved - they are lower priority.)

--- Mathlib.lean
+++ Mathlib.lean
@@ -3435,6 +3435,7 @@ import Mathlib.Geometry.Euclidean.Inversion.Calculus
 import Mathlib.Geometry.Euclidean.Inversion.ImageHyperplane
 import Mathlib.Geometry.Euclidean.MongePoint
 import Mathlib.Geometry.Euclidean.PerpBisector
+import Mathlib.Geometry.Euclidean.Simplex
 import Mathlib.Geometry.Euclidean.Sphere.Basic
 import Mathlib.Geometry.Euclidean.Sphere.Power
 import Mathlib.Geometry.Euclidean.Sphere.Ptolemy
