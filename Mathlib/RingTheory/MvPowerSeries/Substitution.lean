7a96da236f8f0ed02903edb7782e13596ae3bebf
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2025-03-31 19:01:35 +0200

    change UniformAddGroup to Is…

--- Mathlib/RingTheory/MvPowerSeries/Substitution.lean
+++ Mathlib/RingTheory/MvPowerSeries/Substitution.lean
@@ -286,7 +286,7 @@ theorem map_algebraMap_eq_subst_X (f : MvPowerSeries σ R) :
 variable
     {T : Type*} [CommRing T]
     [UniformSpace T] [T2Space T] [CompleteSpace T]
-    [UniformAddGroup T] [IsTopologicalRing T] [IsLinearTopology T T]
+    [IsUniformAddGroup T] [IsTopologicalRing T] [IsLinearTopology T T]
     [Algebra R T] -- [Algebra S T] [IsScalarTower R S T]
     {ε : MvPowerSeries τ S →ₐ[R] T}
 
