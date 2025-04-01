96b832f6d419da4424144ac9c133878a82df1edc
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2025-04-01 20:24:47 +0100

    add test

---
+++ MathlibTest/Delab/SupInf.lean
@@ -0,0 +1,25 @@
+import Mathlib
+
+/-- info: max 1 2 : ℕ -/
+#guard_msgs in
+#check max (1 : ℕ) 2
+
+/-- info: max 1 2 : ℝ -/
+#guard_msgs in
+#check max (1 : ℝ) 2
+
+/-- info: {1} ⊔ {2} : Set ℕ -/
+#guard_msgs in
+#check max ({1} : Set Nat) {2}
+
+variable {α : Type} (a b : α) [Lattice α]
+
+/-- info: a ⊔ b : α -/
+#guard_msgs in
+#check max a b
+
+variable [LinearOrder α]
+
+/-- info: max a b : α -/
+#guard_msgs in
+#check max a b
