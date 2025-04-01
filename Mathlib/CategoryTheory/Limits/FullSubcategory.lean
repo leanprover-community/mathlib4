e6a1f798cc2e70d22fd915b460fc0ae9ac404a8a
Yaël Dillies <yael.dillies@gmail.com>
2025-04-01 09:53:52 +0100

    Joël's golf

--- Mathlib/CategoryTheory/Limits/FullSubcategory.lean
+++ Mathlib/CategoryTheory/Limits/FullSubcategory.lean
@@ -148,35 +148,18 @@ variable (F : C ⥤ D)
 
 /-- The essential image of a functor is closed under the limits it preserves. -/
 protected lemma ClosedUnderLimitsOfShape.essImage [HasLimitsOfShape J C]
-    [PreservesLimitsOfShape J F] [F.Full] [F.Faithful] : ClosedUnderLimitsOfShape J F.essImage := by
-  rintro G c hc hG
-  choose Hobj e using hG
-  replace e i := (e i).some
-  have hF : F.FullyFaithful := .ofFullyFaithful _
-  let H : J ⥤ C := {
-    obj := Hobj
-    map {i j} f := hF.preimage <| (e i).hom ≫ G.map f ≫ (e j).inv
-    map_id i := F.map_injective <| by simp
-    map_comp {i j k} f g := F.map_injective <| by simp
-  }
-  exact ⟨Limits.limit H, ⟨IsLimit.conePointsIsoOfNatIso (s := F.mapCone <| limit.cone H)
-    (isLimitOfPreserves _ (limit.isLimit H)) hc <| NatIso.ofComponents e⟩⟩
+    [PreservesLimitsOfShape J F] [F.Full] [F.Faithful] :
+    ClosedUnderLimitsOfShape J F.essImage := fun G c hc hG ↦
+  ⟨limit (essImage.liftFunctor G F hG),
+    ⟨IsLimit.conePointsIsoOfNatIso (isLimitOfPreserves F (limit.isLimit _)) hc
+      (essImage.liftFunctorCompIso _ _ _)⟩⟩
 
 /-- The essential image of a functor is closed under the colimits it preserves. -/
 protected lemma ClosedUnderColimitsOfShape.essImage [HasColimitsOfShape J C]
     [PreservesColimitsOfShape J F] [F.Full] [F.Faithful] :
-    ClosedUnderColimitsOfShape J F.essImage := by
-  rintro G c hc hG
-  choose Hobj e using hG
-  replace e i := (e i).some
-  have hF : F.FullyFaithful := .ofFullyFaithful _
-  let H : J ⥤ C := {
-    obj := Hobj
-    map {i j} f := hF.preimage <| (e i).hom ≫ G.map f ≫ (e j).inv
-    map_id i := F.map_injective <| by simp
-    map_comp {i j k} f g := F.map_injective <| by simp
-  }
-  exact ⟨Limits.colimit H, ⟨IsColimit.coconePointsIsoOfNatIso (s := F.mapCocone <| colimit.cocone H)
-    (isColimitOfPreserves _ (colimit.isColimit H)) hc <| NatIso.ofComponents e⟩⟩
+    ClosedUnderColimitsOfShape J F.essImage := fun G _c hc hG ↦
+  ⟨colimit (essImage.liftFunctor G F hG),
+    ⟨IsColimit.coconePointsIsoOfNatIso (isColimitOfPreserves F (colimit.isColimit _)) hc
+      (essImage.liftFunctorCompIso _ _ _)⟩⟩
 
 end CategoryTheory.Limits
