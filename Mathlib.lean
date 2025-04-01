1ea501d64f86542692f4d8acf4d9f3d7e9674c75
Parcly Taxel <reddeloostw@gmail.com>
2025-04-01 23:48:16 +0800

    chore: split `Order.Interval.Set.Basic`

--- Mathlib.lean
+++ Mathlib.lean
@@ -4586,6 +4586,7 @@ import Mathlib.Order.Interval.Set.Infinite
 import Mathlib.Order.Interval.Set.InitialSeg
 import Mathlib.Order.Interval.Set.IsoIoo
 import Mathlib.Order.Interval.Set.Limit
+import Mathlib.Order.Interval.Set.LinearOrder
 import Mathlib.Order.Interval.Set.Monotone
 import Mathlib.Order.Interval.Set.OrdConnected
 import Mathlib.Order.Interval.Set.OrdConnectedComponent
