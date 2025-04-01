a97558970876d0eafd7fa65f8cd5dd8d98f039aa
Robin Carlier <57142648+robin-carlier@users.noreply.github.com>
2025-04-01 16:42:13 +0200

    Update Mathlib/CategoryTheory/Sums/Products.lean
    Co-authored-by: Joël Riou <37772949+joelriou@users.noreply.github.com>

--- Mathlib/CategoryTheory/Sums/Products.lean
+++ Mathlib/CategoryTheory/Sums/Products.lean
@@ -3,7 +3,6 @@ Copyright (c) 2025 Robin Carlier. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Robin Carlier
 -/
-
 import Mathlib.CategoryTheory.Sums.Basic
 import Mathlib.CategoryTheory.Products.Basic
 
