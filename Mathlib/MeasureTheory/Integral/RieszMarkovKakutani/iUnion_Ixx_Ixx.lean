863128a721c6f82d1dcfd28e5c08bb7c3b1399bd
Oliver Butterley <oliver.butterley@gmail.com>
2025-04-02 00:40:08 +0200

    first draft of `iUnion_Ioc_subset_Ioc`

--- Mathlib/MeasureTheory/Integral/RieszMarkovKakutani/iUnion_Ixx_Ixx.lean
+++ Mathlib/MeasureTheory/Integral/RieszMarkovKakutani/iUnion_Ixx_Ixx.lean
@@ -11,14 +11,43 @@ import Mathlib.Algebra.CharP.Defs
 
 /-!
 TO DO:
-- Tidy this result, add natural variants, find a good location for it.
-
-Similar to lemmas in `Mathlib/Order/Interval/Set/Disjoint.lean` but adding this there is probably
-not good because it would require more imports.
+- Condense `iUnion_Ioc_subset_Ioc`
+- Reorganise `Real.lean` to use this result
 -/
 
 open Set
-variable {α : Type*}
+
+lemma iUnion_Ioc_subset_Ioc {X : Type*} [LinearOrder X] (N : ℕ) (a : ℕ → X) :
+    Ioc (a 0) (a N) ⊆ ⋃ i ∈ Finset.range N, Ioc (a i) (a (i + 1)) := by
+  induction N with
+  | zero => simp
+  | succ N ih =>
+    by_cases hc : a N ≤ a (N + 1)
+    · by_cases hc' : a 0 ≤ a N
+      · calc
+          _ = Ioc (a 0) (a N) ∪ Ioc (a N) (a (N + 1)) := Eq.symm <| Ioc_union_Ioc_eq_Ioc hc' hc
+          _ ⊆ (⋃ i ∈ Finset.range N, Ioc (a i) (a (i + 1))) ∪ Ioc (a N) (a (N + 1)) :=
+            union_subset_union ih (by rfl)
+          _ ⊆ _ := by simp [Finset.mem_insert, Finset.range_succ]
+      · push_neg at hc'
+        calc
+          _ ⊆ Ioc (a N) (a (N + 1)) := by
+            refine Ioc_subset_Ioc (le_of_lt hc') (by rfl)
+          _ ⊆ _ := by simp [Finset.mem_insert, Finset.range_succ]
+    · push_neg at hc
+      by_cases hc' : a 0 ≤ a (N + 1)
+      · calc
+          _ ⊆ Ioc (a 0) (a N) := by simp [← Ioc_union_Ioc_eq_Ioc hc' (le_of_lt hc)]
+          _ ⊆ ⋃ i ∈ Finset.range N, Ioc (a i) (a (i + 1)) := ih
+          _ ⊆ _ := by simp [Finset.mem_insert, Finset.range_succ]
+      · push_neg at hc'
+        intro x hx
+        have := calc a (N + 1)
+          _ < a 0 := hc'
+          _ < x := hx.1
+          _ ≤ a (N + 1) := hx.2
+        exact False.elim <| (lt_self_iff_false _).mp this
+
 
 lemma iUnion_Ioc_Ioc {X : Type*} [LinearOrderedSemiring X]
     (N : ℕ) (c : X) {δ : X} (hδ : 0 ≤ δ) :
