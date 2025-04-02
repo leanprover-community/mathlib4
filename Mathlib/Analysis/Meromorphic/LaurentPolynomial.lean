7a0a082827437f0a0cd6edf3a6d1f8110f07a27f
Stefan Kebekus <kebekus@users.noreply.github.com>
2025-04-02 12:51:47 +0200

    Update

--- Mathlib/Analysis/Meromorphic/LaurentPolynomial.lean
+++ Mathlib/Analysis/Meromorphic/LaurentPolynomial.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Stefan Kebekus
 -/
 import Mathlib.Analysis.Meromorphic.Divisor
-import Mathlib.Analysis.Meromorphic.NormalFormAt
+import Mathlib.Analysis.Meromorphic.NormalForm
 
 /-!
 # Laurent polynomials
