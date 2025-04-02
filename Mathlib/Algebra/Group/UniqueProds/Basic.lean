23bb6e2f660d3ee1332ac7e3d572777624982daf
Kim Morrison <kim@tqft.net>
2025-04-02 21:12:34 +1100

    fixes

--- Mathlib/Algebra/Group/UniqueProds/Basic.lean
+++ Mathlib/Algebra/Group/UniqueProds/Basic.lean
@@ -623,12 +623,3 @@ instance {ι} (G : ι → Type*) [∀ i, AddZeroClass (G i)] [∀ i, TwoUniqueSu
 instance {ι G} [AddZeroClass G] [TwoUniqueSums G] : TwoUniqueSums (ι →₀ G) :=
   TwoUniqueSums.of_injective_addHom
     Finsupp.coeFnAddHom.toAddHom DFunLike.coe_injective inferInstance
-
-/-- Any `FreeMonoid` has the `TwoUniqueProds` property. -/
-instance FreeMonoid.instTwoUniqueProds {κ : Type*} : TwoUniqueProds (FreeMonoid κ) :=
-  .of_mulHom ⟨Multiplicative.ofAdd ∘ List.length, fun _ _ ↦ congr_arg _ List.length_append⟩
-    (fun _ _ _ _ h h' ↦ List.append_inj h <| Equiv.injective Multiplicative.ofAdd h'.1)
-
-/-- Any `FreeAddMonoid` has the `TwoUniqueSums` property. -/
-instance FreeAddMonoid.instTwoUniqueSums {κ : Type*} : TwoUniqueSums (FreeAddMonoid κ) :=
-  .of_addHom ⟨_, fun _ _ => List.length_append⟩ (fun _ _ _ _ h h' ↦ List.append_inj h h'.1)
