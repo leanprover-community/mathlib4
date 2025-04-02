3058685a7a453f942945b844d4db222b41604811
jt496 <john.talbot@gmail.com>
2025-04-02 19:47:26 +0100

    simplified

--- Mathlib/Combinatorics/SimpleGraph/Brooks/Brooks.lean
+++ Mathlib/Combinatorics/SimpleGraph/Brooks/Brooks.lean
@@ -86,7 +86,7 @@ theorem BrooksPartial (hk : 3 ≤ k) (hc : G.CliqueFree (k + 1)) (hbdd : ∀ v, 
         ((degreeOn_le_degree ..).trans (hbdd v)) <| hd _ hv
   have hbdd' : ∀ v, v ∈ s → G.degree v = k := fun v hv ↦ le_antisymm (hbdd v)
         <| hd v hv ▸ degreeOn_le_degree ..
-  have hins : ∀ v ∈ s, ∀ w, G.Adj v w → w ∈ s := by
+  have hin : ∀ v ∈ s, ∀ w, G.Adj v w → w ∈ s := by
     by_contra! hf
     obtain ⟨v, hv, w, ha, hns⟩ := hf
     have := degreeOn_lt_degree ⟨by rwa [← mem_neighborFinset] at ha, hns⟩
@@ -123,7 +123,7 @@ theorem BrooksPartial (hk : 3 ≤ k) (hc : G.CliqueFree (k + 1)) (hbdd : ∀ v, 
     have v41s : ∀ y, y ∈ v41.support → y ∈ s := by
       intro y hy
       cases mv41 y hy with
-      | inl h => exact h ▸ (hins _ h3.2 _ h34)
+      | inl h => exact h ▸ (hin _ h3.2 _ h34)
       | inr h => cases h with
                 | inl h => exact h ▸ h3.2
                 | inr h => cases h with
@@ -145,22 +145,15 @@ theorem BrooksPartial (hk : 3 ≤ k) (hc : G.CliqueFree (k + 1)) (hbdd : ∀ v, 
     · --Main Case 1 the path is all of s
       simp_rw [support_append, v41sup, List.tail, toFinset_append, toFinset_cons, toFinset_nil,
                 insert_emptyc_eq] at hr
-      rw [v41sup, List.tail] at hdisj2
-      obtain ⟨vⱼ, hj⟩ : ∃ vⱼ, G.Adj v₂ vⱼ ∧ vⱼ ≠ v₁ ∧ vⱼ ≠ v₃ ∧ vⱼ ∈ s := by
-        obtain ⟨vⱼ, hj⟩ := G.exists_adj_ne_of_two_lt_degree v₂
-          (hk.trans (hbdd' _ hv₂).symm.le) v₁ v₃
-        exact ⟨vⱼ,hj.1,hj.2.1,hj.2.2,hins _ hv₂ _ hj.1⟩
-      have h1q := fun hf ↦ hdisj2 hf ((mem_cons_of_mem _ <|mem_cons_of_mem _ <| mem_cons_self ..))
+      obtain ⟨vⱼ, hj⟩ := G.exists_adj_ne_of_two_lt_degree v₂ (hk.trans (hbdd' _ hv₂).symm.le) v₁ v₃
+      have hj : G.Adj v₂ vⱼ ∧ vⱼ ≠ v₁ ∧ vⱼ ≠ v₃ ∧ vⱼ ∈ s := ⟨hj.1, hj.2.1, hj.2.2, hin _ hv₂ _ hj.1⟩
+      have h1q := fun hf ↦ hdisj2 hf ((mem_cons_of_mem _ <| mem_cons_of_mem _ <| mem_cons_self ..))
       have h2q := fun hf ↦ hdisj2 hf (mem_cons_of_mem _ <| mem_cons_self ..)
       have h3q := fun hf ↦ hdisj2 hf (mem_cons_self ..)
-      have hj123: vⱼ ∉ ({v₃ , v₂, v₁} : Finset α) := by
-        simp_rw [mem_insert, Finset.mem_singleton, not_or]
-        exact ⟨hj.2.2.1, hj.1.symm.ne, hj.2.1⟩
+      have hj123: vⱼ ∉ ({v₃ , v₂, v₁} : Finset α) := by simpa using ⟨hj.2.2.1, hj.1.symm.ne, hj.2.1⟩
       have hvjq : vⱼ ∈ q.support := by
-        subst hr
-        apply mem_toFinset.1
-        cases (mem_union.1 hj.2.2.2) with
-        | inl h => exact h
+        cases (mem_union.1 (hr ▸ hj.2.2.2)) with
+        | inl h => exact mem_toFinset.1 h
         | inr h => exact (hj123 h).elim
       obtain ⟨C, hC⟩ := Brooks1_exists hk hbdd hq.of_append_left hvjq hj.1.symm h1.1 h3.1.symm hne
                              hnadj h1q h2q h3q
@@ -171,14 +164,13 @@ theorem BrooksPartial (hk : 3 ≤ k) (hc : G.CliqueFree (k + 1)) (hbdd : ∀ v, 
       have hssf := Finset.ssubset_iff_subset_ne.2 ⟨fun y hy ↦ hss _ <| mem_toFinset.1 hy, hr⟩
       have h1 := Nat.lt_of_succ_lt <| hk.trans (hbdd' _ <| hss _ <| start_mem_support ..).symm.le
       let p := (q.append v41).reverse
-      have hp : p.IsPath := hq.reverse
-      let  S := {x | G.Adj vᵣ x ∧ x ≠ p.penultimate}
+      let S := {x | G.Adj vᵣ x ∧ x ≠ p.penultimate}
       obtain ⟨y, hy⟩ : ∃ y, y ∈ S := G.exists_adj_ne_of_one_lt_degree vᵣ h1 p.penultimate
       have hmaxp : ∀ x, G.Adj vᵣ x → x ∈ p.support := by
         simp_rw [p, support_reverse, mem_reverse]; exact hmax
       obtain ⟨m, hm⟩ := exists_getVert_first p hy (fun x hx ↦ hmaxp x hx.1)
       let c := (p.drop m).cons (hm.1.1)
-      have hcy := hp.cons_drop_isCycle hm.1.1 hm.1.2
+      have hcy := hq.reverse.cons_drop_isCycle hm.1.1 hm.1.2
       have hmlt : m < p.length := by
         by_contra!
         exact G.loopless _ <| (p.getVert_of_length_le this) ▸ hm.1.1
@@ -192,39 +184,36 @@ theorem BrooksPartial (hk : 3 ≤ k) (hc : G.CliqueFree (k + 1)) (hbdd : ∀ v, 
         intro y hy
         rw [mem_toFinset, support_cons] at *
         cases hy with
-        | head as => exact start_mem_support ..
-        | tail b hy =>
+        | head _ => exact start_mem_support ..
+        | tail _ hy =>
           have := (support_drop_subset _ _) hy
           rwa [support_reverse, mem_reverse] at this
       -- `c` is not empty
-      have hnemp := card_pos.2 ⟨_, mem_toFinset.2 c.start_mem_support⟩
       -- so we will be able to color `c` and `s \ c` by induction if needed
-      have hccard := (card_lt_card hsub).trans_le hn.le
       have hsdcard : #(s \ c.support.toFinset) < n := by
         rw [card_sdiff hsub.1]
         apply hn.le.trans_lt'
         rw [Nat.sub_lt_iff_lt_add (card_le_card hsub.1)]
-        exact Nat.lt_add_of_pos_left hnemp
+        exact Nat.lt_add_of_pos_left <| card_pos.2 ⟨_, mem_toFinset.2 c.start_mem_support⟩
       -- Two subcases either `c` has a neighbor in `s \ c` or not
       by_cases hnbc : ∃ x, x ∈ c.support ∧ ∃ y, y ∈ s \ c.support.toFinset ∧ G.Adj x y
       · obtain ⟨x, hx, y, hy, had⟩ := hnbc
         --`x ∈ c` has a neighbor `y ∈ s \ c` (but `vᵣ ∈ c` has no neighbors in `s \ c`)
         -- so there is a first dart `d = (d₁, d₂)` in `c` such that `d₁` is adjacent
         -- to something `s \ c` and `d₂` is not
-        let S : Set α := {a | ∃ b ∈ s \ c.support.toFinset, G.Adj a b}
-        have rS : vᵣ ∉ S := fun ⟨ _, hy , had⟩ ↦
-            (mem_sdiff.1 hy).2 <| mem_toFinset.2 <| hcym _ had
+        have rS : vᵣ ∉ {a | ∃ b ∈ s \ c.support.toFinset, G.Adj a b} :=
+          fun ⟨ _, hy , had⟩ ↦ (mem_sdiff.1 hy).2 <| mem_toFinset.2 <| hcym _ had
         obtain ⟨d, hd, ⟨y, hy1, hd1⟩, hd2⟩ := exists_boundary_dart_of_closed c _
                       (Set.mem_setOf.2 ⟨_, hy, had⟩) rS hx (start_mem_support ..)
-        replace hd2 : ∀ b ∈ s \ c.support.toFinset, ¬ G.Adj d.toProd.2 b := by
+        have d2 : ∀ b ∈ s \ c.support.toFinset, ¬ G.Adj d.toProd.2 b := by
           contrapose! hd2; exact hd2
         -- We color `s \ c` by induction
         obtain ⟨C₁, hC₁⟩ := ih _ hsdcard _ rfl
         -- we then extend this coloring to `d₂` by coloring `d₂` with the color of `y`
         -- the neighbor of `d₁` (so now `d₁` is a vertex on `c` that has two neigbors
         -- colored with the same color)
-        let C₂ := C₁.insertNotAdj hd2 y
-        have hC₂ := C₁.lt_of_insertNotAdj_lt hd2 y hC₁
+        let C₂ := C₁.insertNotAdj d2 y
+        have hC₂ := C₁.lt_of_insertNotAdj_lt d2 y hC₁
         let hr := (c.dart_fst_mem_support_of_mem_darts hd)
         -- we take the path given by removing `d₂` from `c` (we do this by rotating
         -- `c` to start at `d₁` and then removing its last two darts)
@@ -233,21 +222,19 @@ theorem BrooksPartial (hk : 3 ≤ k) (hc : G.CliqueFree (k + 1)) (hbdd : ∀ v, 
         have hne : d.toProd.2 ≠ y := by
           intro hf; subst_vars
           exact (mem_sdiff.1 hy1).2 <| mem_toFinset.2 (c.dart_snd_mem_support_of_mem_darts hd)
-        have hc2eq : C₂ d.toProd.2 = C₂ y := C₁.eq_ofinsertNotAdj hd2
         -- We will color the vertices in `p` greedily in reverse order, so ending with `d₁`
         have hd2 := mem_toFinset.2 (c.dart_snd_mem_support_of_mem_darts hd)
         have hsdc := sdiff_union_of_subset hsub.1
         have heq : (insert d.toProd.2 (s \ c.support.toFinset)
                       ∪ p.reverse.support.toFinset) = s := by
-            rwa [support_reverse, toFinset_reverse, hcy.support_rotate_tail_tail_eq hd,
-                insert_union, ← erase_eq_of_not_mem (not_mem_sdiff_of_mem_right hd2),
-                ← erase_union_distrib, insert_erase (hsdc.symm ▸ (hsub.1 hd2))]
+          rwa [support_reverse, toFinset_reverse, hcy.support_rotate_tail_tail_eq hd, insert_union,
+           ← erase_eq_of_not_mem (not_mem_sdiff_of_mem_right hd2), ← erase_union_distrib,
+            insert_erase (hsdc.symm ▸ (hsub.1 hd2))]
         have hps : p.support.toFinset ⊆ c.support.toFinset := by
           intro x hx
           rw [mem_toFinset] at *
-          apply (c.mem_support_rotate_iff hr).1
-          apply support_drop_subset _ _ (support_drop_subset _ _ hx)
-        have hdisj2 : Disjoint (insert d.toProd.2 (s \ c.support.toFinset))
+          exact (mem_support_rotate_iff hr).1 (support_drop_subset _ _ (support_drop_subset _ _ hx))
+        have hdisj : Disjoint (insert d.toProd.2 (s \ c.support.toFinset))
           p.reverse.support.toFinset := by
           rw [support_reverse, toFinset_reverse]
           apply disjoint_insert_left.2
@@ -258,12 +245,11 @@ theorem BrooksPartial (hk : 3 ≤ k) (hc : G.CliqueFree (k + 1)) (hbdd : ∀ v, 
         -- already has two neighbors colored with the same color we never need to use
         -- more that `k` colors along the path.
         exact ⟨(C₂.Greedy p.reverse.support).copy heq, C₂.Greedy_of_path_notInj hbdd hp.reverse hC₂
-                  (mem_insert_self ..) (mem_insert_of_mem hy1) d.adj hd1 hne hc2eq hdisj2⟩
+         (mem_insert_self ..) (mem_insert_of_mem hy1) d.adj hd1 hne (C₁.eq_ofinsertNotAdj d2) hdisj⟩
       · -- The cycle `c` has no edges into `s \ c` and so we can now color
-        -- `c` and `s \ c` by induction
-        obtain ⟨C₁, hC₁⟩ := ih _ hccard  _ rfl
+        -- `c` and `s \ c` by induction and then join the colorings together
+        obtain ⟨C₁, hC₁⟩ := ih _ ((card_lt_card hsub).trans_le hn.le)  _ rfl
         obtain ⟨C₂, hC₂⟩ := ih _ hsdcard _ rfl
-        push_neg at hnbc
         exact ⟨(C₁.join C₂ (by simpa using hnbc)).copy (union_sdiff_of_subset hsub.1),
           copy_isK (C₁.join_isK_of_isK hC₁ hC₂)⟩
   · -- `s` is empty so easy to `k`-color
