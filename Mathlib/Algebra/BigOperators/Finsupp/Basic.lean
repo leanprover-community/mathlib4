leanprover-community/mathlib4

commit 03a6268b5f31e961b2d671efacc4235869263ac7

chore(Algebra/BigOperators/Finsupp): add Finsupp.sum_apply'' (#23305)
Part of an attempt to factor some reusable bits out of
`Mathlib/Topology/Category/Profinite/Nobeling.lean`

This lemma applies in situations where `Finsupp.sum_apply'` does not,
because coercions are getting in the way.

We currently don't have a typeclass to express
"addition on this FunLike is pointwise"
so I have expressed that with explicit arguments.

Mathlib/Algebra/BigOperators/Finsupp/Basic.lean
@@ -543,6 +543,26 @@ theorem Finset.sum_apply' : (∑ k ∈ s, f k) i = ∑ k ∈ s, f k i :=
 theorem Finsupp.sum_apply' : g.sum k x = g.sum fun i b => k i b x :=
   Finset.sum_apply _ _ _
 
+/-- Version of `Finsupp.apply'` that applies in large generality to linear combinations
+of functions in any `FunLike` type on which addition is defined pointwise.
+
+At the time of writing Mathlib does not have a typeclass to express the condition
+that addition on a `FunLike` type is pointwise; hence this is asserted via explicit hypotheses. -/
+theorem Finsupp.sum_apply'' {A F : Type*} [AddZeroClass A] [AddCommMonoid F] [FunLike F γ B]
+    (g : ι →₀ A) (k : ι → A → F) (x : γ)
+    (hg0 : ∀ (i : ι), k i 0 = 0) (hgadd : ∀ (i : ι) (a₁ a₂ : A), k i (a₁ + a₂) = k i a₁ + k i a₂)
+    (h0 : (0 : F) x = 0) (hadd : ∀ (f g : F), (f + g : F) x = f x + g x) :
+    g.sum k x = g.sum (fun i a ↦ k i a x) := by
+  induction g using Finsupp.induction with
+  | h0 => simp [h0]
+  | ha i a f hf ha ih =>
+    rw [Finsupp.sum_add_index' hg0 hgadd, Finsupp.sum_add_index', hadd, ih]
+    · congr 1
+      rw [Finsupp.sum_single_index (hg0 i), Finsupp.sum_single_index]
+      simp [hg0, h0]
+    · simp [hg0, h0]
+    · simp [hgadd, hadd]
+
 theorem Finsupp.sum_sum_index' (h0 : ∀ i, t i 0 = 0) (h1 : ∀ i x y, t i (x + y) = t i x + t i y) :
     (∑ x ∈ s, f x).sum t = ∑ x ∈ s, (f x).sum t := by
   classical

commit 09a9feb6e82350fa55d79ce64cfa7c6ec643a5db

chore(Algebra/BigOperators/Finsupp): fix typo (#23570)
introduced in #23305

Mathlib/Algebra/BigOperators/Finsupp/Basic.lean
@@ -543,7 +543,7 @@ theorem Finset.sum_apply' : (∑ k ∈ s, f k) i = ∑ k ∈ s, f k i :=
 theorem Finsupp.sum_apply' : g.sum k x = g.sum fun i b => k i b x :=
   Finset.sum_apply _ _ _
 
-/-- Version of `Finsupp.apply'` that applies in large generality to linear combinations
+/-- Version of `Finsupp.sum_apply'` that applies in large generality to linear combinations
 of functions in any `FunLike` type on which addition is defined pointwise.
 
 At the time of writing Mathlib does not have a typeclass to express the condition