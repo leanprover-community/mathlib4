d3e3f45e35c9cb4fdc1b831eb212118cbe3f983f
Yaël Dillies <yael.dillies@gmail.com>
2025-04-02 08:12:24 +0000

    make `mem_span_finset` match

--- Mathlib/Algebra/Group/Submonoid/BigOperators.lean
+++ Mathlib/Algebra/Group/Submonoid/BigOperators.lean
@@ -149,34 +149,36 @@ variable [CommMonoid M] {x : M}
 @[to_additive]
 lemma mem_closure_iff_exists_finset_subset {s : Set M} :
     x ∈ closure s ↔
-      ∃ (n : M → ℕ) (t : Finset M), ↑t ⊆ s ∧ n.support ⊆ t ∧ x = ∏ a ∈ t, a ^ n a where
+      ∃ (f : M → ℕ) (t : Finset M), ↑t ⊆ s ∧ f.support ⊆ t ∧ ∏ a ∈ t, a ^ f a = x where
   mp hx := by
     classical
-    induction' hx using closure_induction with x hx x y _ _ hx hy
-    · simp only [Finset.mem_coe] at hx
+    induction hx using closure_induction with
+    | one => exact ⟨0, ∅, by simp⟩
+    | mem x hx =>
+      simp only [Finset.mem_coe] at hx
       exact ⟨Pi.single x 1, {x}, by simp [hx, Pi.single_apply]⟩
-    · exact ⟨0, ∅, by simp⟩
-    · obtain ⟨m, t, hts, hm, rfl⟩ := hx
-      obtain ⟨n, u, hus, hn, rfl⟩ := hy
-      refine ⟨m + n, t ∪ u, mod_cast Set.union_subset hts hus,
-        (Function.support_add _ _).trans <| mod_cast Set.union_subset_union hm hn, ?_⟩
-      simp [pow_add, Finset.prod_mul_distrib]
-      congr 1
-      · refine Finset.prod_subset Finset.subset_union_left ?_
-        simp +contextual [Function.support_subset_iff'.1 hm]
-      · refine Finset.prod_subset Finset.subset_union_right ?_
-        simp +contextual [Function.support_subset_iff'.1 hn]
+    | mul x y _ _ hx hy =>
+    obtain ⟨f, t, hts, hf, rfl⟩ := hx
+    obtain ⟨g, u, hus, hg, rfl⟩ := hy
+    refine ⟨f + g, t ∪ u, mod_cast Set.union_subset hts hus,
+      (Function.support_add _ _).trans <| mod_cast Set.union_subset_union hf hg, ?_⟩
+    simp only [Pi.add_apply, pow_add, Finset.prod_mul_distrib]
+    congr 1 <;> symm
+    · refine Finset.prod_subset Finset.subset_union_left ?_
+      simp +contextual [Function.support_subset_iff'.1 hf]
+    · refine Finset.prod_subset Finset.subset_union_right ?_
+      simp +contextual [Function.support_subset_iff'.1 hg]
   mpr := by
     rintro ⟨n, t, hts, -, rfl⟩; exact prod_mem _ fun x hx ↦ pow_mem (subset_closure <| hts hx) _
 
 @[to_additive]
 lemma mem_closure_finset {s : Finset M} :
-    x ∈ closure s ↔ ∃ n : M → ℕ, n.support ⊆ s ∧ x = ∏ a ∈ s, a ^ n a where
+    x ∈ closure s ↔ ∃ f : M → ℕ, f.support ⊆ s ∧ ∏ a ∈ s, a ^ f a = x where
   mp := by
     rw [mem_closure_iff_exists_finset_subset]
-    rintro ⟨n, t, hts, hn, rfl⟩
-    refine ⟨n, hn.trans hts, Finset.prod_subset hts ?_⟩
-    simp +contextual [Function.support_subset_iff'.1 hn]
+    rintro ⟨f, t, hts, hf, rfl⟩
+    refine ⟨f, hf.trans hts, .symm <| Finset.prod_subset hts ?_⟩
+    simp +contextual [Function.support_subset_iff'.1 hf]
   mpr := by rintro ⟨n, -, rfl⟩; exact prod_mem _ fun x hx ↦ pow_mem (subset_closure hx) _
 
 end CommMonoid
