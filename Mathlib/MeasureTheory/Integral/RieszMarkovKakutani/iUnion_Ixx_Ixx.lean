1c31aa27c18958bbea8e22b8440f676ecdeb3f13
Oliver Butterley <oliver.butterley@gmail.com>
2025-04-02 14:41:31 +0200

    clean

--- Mathlib/MeasureTheory/Integral/RieszMarkovKakutani/iUnion_Ixx_Ixx.lean
+++ Mathlib/MeasureTheory/Integral/RieszMarkovKakutani/iUnion_Ixx_Ixx.lean
@@ -32,24 +32,3 @@ lemma iUnion_Ico_subset_Ico {X : Type*} [LinearOrder X] (N : ℕ) (a : ℕ → X
     _ ⊆ _ := by
       simp_rw [Finset.range_succ, Finset.mem_insert, iUnion_iUnion_eq_or_left, union_comm]
       exact union_subset_union_right (Ico (a N) (a (N + 1))) ih
-
--- lemma iUnion_Ioc_Ioc {X : Type*} [LinearOrderedSemiring X]
---     (N : ℕ) (c : X) {δ : X} (hδ : 0 ≤ δ) :
---     ⋃ n ∈ Finset.range N, Ioc (c + n * δ) (c + n * δ + δ) = Ioc c (c + N * δ) := by
---   induction N with
---   | zero => simp
---   | succ N ih =>
---     simp only [Finset.mem_insert, iUnion_iUnion_eq_or_left, Nat.cast_add,
---       Nat.cast_one, ih, Finset.range_succ]
---     rw [union_comm, Ioc_union_Ioc_eq_Ioc]
---     · simp [add_mul, add_assoc]
---     · simpa [le_add_iff_nonneg_right] using mul_nonneg (Nat.cast_nonneg' N) hδ
---     · simp [hδ]
-
--- lemma Fin_to_Nat {X : Type*} (N : ℕ) (s : ℕ → Set X) :
---     ⋃ (n : Fin N), s n = ⋃ n ∈ Finset.range N, s n := by
---   ext x
---   simp only [mem_iUnion, Finset.mem_range, exists_prop]
---   constructor
---   · exact fun ⟨i, hi⟩ ↦ ⟨i, i.2, hi⟩
---   · exact fun ⟨i, hiN, hi⟩ ↦ ⟨⟨i, hiN⟩, hi⟩
