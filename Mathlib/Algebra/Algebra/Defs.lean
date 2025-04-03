be0857d250d85113d253322dbde43487ae885c9e
Riccardo Brasca <riccardo.brasca@gmail.com>
2025-04-03 13:50:58 +0200

    generalize

--- Mathlib/Algebra/Algebra/Defs.lean
+++ Mathlib/Algebra/Algebra/Defs.lean
@@ -90,7 +90,7 @@ section Prio
 
 See the implementation notes in this file for discussion of the details of this definition.
 -/
-class Algebra (R : Type u) (A : Type v) [CommSemiring R] [Semiring A] extends SMul R A where
+class Algebra (R : Type u) (A : Type v) [Semiring R] [Semiring A] extends SMul R A where
   /-- Embedding `R →+* A` given by `Algebra` structure.
   Use `algebraMap` from the root namespace instead. -/
   protected algebraMap : R →+* A
