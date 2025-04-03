10640180e66b063598c2683221c9a2eeca695bec
Paul-Lez <paul.lezeau@gmail.com>
2025-04-03 22:35:11 +0100

    Fix name in docstring

--- Mathlib/Util/Simp.lean
+++ Mathlib/Util/Simp.lean
@@ -18,17 +18,17 @@ namespace Simp
 
 open Simp
 
-/-- `Qq` version of `Simp.Methods.discharge?`, which avoids having to use `~q` matching
-on the proof expression returned by `Methods.discharge?`
+/-- `Qq` version of `Lean.Meta.Simp.Methods.discharge?`, which avoids having to use `~q` matching
+on the proof expression returned by `discharge?`
 
-`Methods.dischargeQ? (a : Q(Prop))` attempts to prove `a` using the discharger, returning
+`dischargeQ? (a : Q(Prop))` attempts to prove `a` using the discharger, returning
 `some (pf : Q(a))` if a proof is found and `none` otherwise.
 -/
 def Methods.dischargeQ? (M : Methods) (a : Q(Prop)) : SimpM <| Option Q($a) := do
   match ← M.discharge? a with
   | some pf =>
-    let ⟨0, ~q($a), pf⟩ ← inferTypeQ pf | return none
-    return some pf
+    let pf : Q($a) := pf
+    pf.check
   | none => return none
 
 --TODO(Paul-Lez): add tests
