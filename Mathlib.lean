74242d0858a6b227b51fb4fb7debe1a2ee13fd1c
Yury G. Kudryashov <urkud@urkud.name>
2025-04-03 00:14:23 -0500

    mk_all

--- Mathlib.lean
+++ Mathlib.lean
@@ -4230,6 +4230,7 @@ import Mathlib.MeasureTheory.VectorMeasure.Basic
 import Mathlib.MeasureTheory.VectorMeasure.Decomposition.Hahn
 import Mathlib.MeasureTheory.VectorMeasure.Decomposition.Jordan
 import Mathlib.MeasureTheory.VectorMeasure.Decomposition.Lebesgue
+import Mathlib.MeasureTheory.VectorMeasure.Decomposition.RadonNikodym
 import Mathlib.MeasureTheory.VectorMeasure.WithDensity
 import Mathlib.ModelTheory.Algebra.Field.Basic
 import Mathlib.ModelTheory.Algebra.Field.CharP
