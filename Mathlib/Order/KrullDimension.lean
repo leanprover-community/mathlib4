bd683d591f5788b40d494a35aca807cd46331252
Junyan Xu <junyanxu.math@gmail.com>
2025-04-04 18:38:20 -0400

    generalize

--- Mathlib/Order/KrullDimension.lean
+++ Mathlib/Order/KrullDimension.lean
@@ -860,7 +860,8 @@ lemma KrullDimLE.mono {n m : ℕ} (e : n ≤ m) (α : Type*) [Preorder α] [Krul
     KrullDimLE m α :=
   ⟨KrullDimLE.krullDim_le (n := n).trans (Nat.cast_le.mpr e)⟩
 
-instance {α} [Preorder α] [Subsingleton α] : KrullDimLE 0 α := ⟨krullDim_nonpos_of_subsingleton⟩
+instance (n α) [Preorder α] [Subsingleton α] : KrullDimLE n α :=
+  ⟨krullDim_nonpos_of_subsingleton.trans n.zero_le⟩
 
 end typeclass
 
