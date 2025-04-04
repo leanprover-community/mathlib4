d973c5345ed64def5bca82dc537162aa2c9191e9
Rida Hamadani <mridahamadani@gmail.com>
2025-04-04 10:35:32 +0300

    Update Mathlib/Combinatorics/SimpleGraph/StronglyRegular.lean
    Co-authored-by: Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>

--- Mathlib/Combinatorics/SimpleGraph/StronglyRegular.lean
+++ Mathlib/Combinatorics/SimpleGraph/StronglyRegular.lean
@@ -55,8 +55,8 @@ for empty graphs, since there are no pairs of adjacent vertices. -/
 theorem bot_strongly_regular : (⊥ : SimpleGraph V).IsSRGWith (Fintype.card V) 0 ℓ 0 where
   card := rfl
   regular := bot_degree
-  of_adj := fun _ _ h ↦ h.elim
-  of_not_adj := fun v w _ ↦ by
+  of_adj _ _ h := h.elim
+  of_not_adj v w _ := by
     simp only [card_eq_zero, Fintype.card_ofFinset, forall_true_left, not_false_iff, bot_adj]
     ext
     simp [mem_commonNeighbors]
