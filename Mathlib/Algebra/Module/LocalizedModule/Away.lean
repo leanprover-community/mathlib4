effdcda14f84d4d98a3e24cdc3ea13791d09c80a
Yongle Hu <140475041+mbkybky@users.noreply.github.com>
2025-04-04 17:13:42 +0800

    Update Away.lean

--- Mathlib/Algebra/Module/LocalizedModule/Away.lean
+++ Mathlib/Algebra/Module/LocalizedModule/Away.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yongle Hu
 -/
 import Mathlib.Algebra.Module.LocalizedModule.Basic
-import Mathlib.RingTheory.Ideal.Defs
 
 /-!
 # Localizations of modules away from an alement
