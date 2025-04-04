b6e4cbfcff07db829d7c7220e1c05295922f41e7
Parcly Taxel <reddeloostw@gmail.com>
2025-04-05 01:44:32 +0800

    fix 4

--- Mathlib/Algebra/Order/Floor/Ring.lean
+++ Mathlib/Algebra/Order/Floor/Ring.lean
@@ -3,8 +3,8 @@ Copyright (c) 2018 Mario Carneiro. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro, Kevin Kappelmann
 -/
+import Mathlib.Algebra.Order.Field.Basic
 import Mathlib.Algebra.Order.Floor.Defs
-import Mathlib.Data.Nat.Cast.Order.Field
 import Mathlib.Tactic.Abel
 import Mathlib.Tactic.FieldSimp
 import Mathlib.Tactic.Linarith
