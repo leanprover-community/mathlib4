768441974f898c9ff20370ba3b7d7c475dc02d98
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-04-05 22:04:29 +0000

    feat(CategoryTheory): weak factorization systems (#20680)
    Co-authored-by: Joël Riou <37772949+joelriou@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -2300,6 +2300,7 @@ import Mathlib.CategoryTheory.MorphismProperty.Representable
 import Mathlib.CategoryTheory.MorphismProperty.Retract
 import Mathlib.CategoryTheory.MorphismProperty.RetractArgument
 import Mathlib.CategoryTheory.MorphismProperty.TransfiniteComposition
+import Mathlib.CategoryTheory.MorphismProperty.WeakFactorizationSystem
 import Mathlib.CategoryTheory.NatIso
 import Mathlib.CategoryTheory.NatTrans
 import Mathlib.CategoryTheory.Noetherian
