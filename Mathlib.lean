ab33df0d526dcec42e8947b55788ae504e99f889
Christopher Hoskin <wieser.eric@gmail.com>
2025-04-06 22:22:18 +0000

    feat(LinearAlgebra/QuadraticForm/Basis): basis expansion of a quadratic map (#18578)
    Three lemmas about Quadratic maps applied to:
    
    - A finite sum
    - A `linearCombination`
    - The representation of an element wrt a basis
    
    
    
    Co-authored-by: Christopher Hoskin <mans0954@users.noreply.github.com>
    Co-authored-by: Christopher Hoskin <christopher.hoskin@gmail.com>
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -3309,6 +3309,7 @@ import Mathlib.Data.Sum.Order
 import Mathlib.Data.Sym.Basic
 import Mathlib.Data.Sym.Card
 import Mathlib.Data.Sym.Sym2
+import Mathlib.Data.Sym.Sym2.Finsupp
 import Mathlib.Data.Sym.Sym2.Init
 import Mathlib.Data.Sym.Sym2.Order
 import Mathlib.Data.Tree.Basic
