c24780bf4de9e70766eb44f48a2bac7aa73d90ee
Yury G. Kudryashov <urkud@urkud.name>
2025-04-06 16:55:51 -0500

    Update Mathlib/Algebra/GroupWithZero/Action/Pointwise/Set.lean
    Co-authored-by: Eric Wieser <efw@google.com>

--- Mathlib/Algebra/GroupWithZero/Action/Pointwise/Set.lean
+++ Mathlib/Algebra/GroupWithZero/Action/Pointwise/Set.lean
@@ -33,6 +33,7 @@ lemma smul_set_pi₀ {M ι : Type*} {α : ι → Type*} [GroupWithZero M] [∀ i
     {c : M} (hc : c ≠ 0) (I : Set ι) (s : ∀ i, Set (α i)) : c • I.pi s = I.pi (c • s) :=
   smul_set_pi_of_isUnit (.mk0 _ hc) I s
 
+/-- A slightly more general version of `Set.smul_set_pi₀`. -/
 lemma smul_set_pi₀' {M ι : Type*} {α : ι → Type*} [GroupWithZero M] [∀ i, MulAction M (α i)]
     {c : M} {I : Set ι} (h : c ≠ 0 ∨ I = univ) (s : ∀ i, Set (α i)) : c • I.pi s = I.pi (c • s) :=
   h.elim (fun hc ↦ smul_set_pi_of_isUnit (.mk0 _ hc) I s) (fun hI ↦ hI ▸ smul_set_univ_pi ..)
