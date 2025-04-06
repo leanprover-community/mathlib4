c8bd8e196d7a673f5fb8dc35db2cb83d05215997
Ben Eltschig <b.eltschig@protonmail.com>
2025-04-06 09:24:17 +0200

    update comments

--- Mathlib/Geometry/Diffeology/Basic.lean
+++ Mathlib/Geometry/Diffeology/Basic.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2025 Ben Eltschig. All rights reserved.
+Copyright (c) 2024 Ben Eltschig. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Ben Eltschig
 -/
@@ -10,33 +10,90 @@ import Mathlib.Tactic.FunProp.ContDiff
 # Diffeological spaces
 
 A diffeological space is a concrete sheaf on the site of cartesian spaces ℝⁿ and smooth maps
-between them, or equivalently a set `X` with a well-behaved notion of smoothness for functions from
-ℝⁿ to X - see https://ncatlab.org/nlab/show/diffeological+space.
+between them, or equivalently a type `X` with a well-behaved notion of smoothness for functions from
+the spaces ℝⁿ to X - see https://ncatlab.org/nlab/show/diffeological+space. In this file we focus on
+the latter viewpoint and define some of the basic notions of diffeology, like diffeological spaces
+and smooth maps between them.
 
-In this file we define some of the basic notions of diffeology, in particular a typeclass
-`DiffeologicalSpace X`, the notions `IsPlot p` and `DSmooth f` for maps ℝⁿ → X resp. X → Y, and the
-D-topology, the finest topology on a diffeological space for which all plots are continuous.
+Concretely, this means that for our purposes a diffeological space is a type `X` together with a set
+`plots n` of maps ℝⁿ → X for each n (called plots), such that the following three properties hold:
+* Every constant map is a plot.
+* For every plot p : ℝⁿ → X and smooth map f : ℝᵐ → ℝⁿ, p ∘ f is a plot.
+* Every map p : ℝⁿ → X that is locally smooth is a plot, where by locally smooth we mean that ℝⁿ can
+  be covered by open sets U such that p ∘ f is a plot for every smooth f : ℝᵐ → U.
+Every normed space, smooth manifold etc. is then naturally a diffeological space by simply taking
+the plots to be those maps ℝⁿ → X that are smooth in the traditional sense. A map `f : X → Y`
+between diffeological spaces is furthermore called smooth if postcomposition with it sends plots of
+`X` to plots of `Y`. This is equivalent to the usual definition of smoothness for maps between e.g.
+manifolds, and equivalent to being a plot for maps p : ℝⁿ → X.
+
+In addition to this notion of smoothness, every diffeological space `X` also comes equipped with a
+natural diffeology, called the D-topology; it is the finest topology on `X` that makes all plots
+continuous, and consists precisely of those sets whose preimages under plots are all open. This
+recovers the standard topology of e.g. normed spaces and manifolds, and makes all smooth maps
+continuous. We provide this as a definition but not as an instance, for reasons outlined in the
+implementation details below.
+
+## Main definitions / results
+
+* `DiffeologicalSpace X`: the type of diffeologies on a type `X`.
+* `IsPlot p`: predicate stating that a map p : ℝⁿ → X is a plot, i.e. belongs to the diffeology
+  on `X`.
+* `DSmooth f`: predicate stating that a map `f : X → Y` between diffeological spaces is smooth in
+  the sense that it sends plots to plots. This is the correct notion of morphisms between
+  diffeological spaces.
+* `DTop`: the D-topology on a diffeological space, consisting of all sets `U` whose preimage
+  `p ⁻¹' u` is open for all plots `p`. This is a definition rather than an instance to avoid
+  typeclass diamonds (see below), and meant for use with notation such as `Continuous[_,DTop]`.
+* `IsDTopCompatible X`: typeclass stating that `X` is equipped with a topology that is equal (but
+  not necessarily defeq) to the D-topology.
+* `euclideanDiffeology`: the standard diffeology on any finite-dimensional normed space `X`,
+  consisting of all conventionally smooth maps ℝⁿ → X. This is again a definition rather than a
+  instance for typeclass diamond reasons; however, we do put this as an instance on `ℝ` and
+  `EuclideanSpace ℝ ι` for finite `ι`.
+* `IsContDiffCompatible X`: typeclass stating that the diffeology on a normed space `X` is equal to
+  the diffeology whose plots are precisely the smooth maps.
+* `isPlot_iff_dsmooth`: a map ℝⁿ → X is a plot iff it is smooth.
+* `dsmooth_iff_contDiff`: a map between finite-dimensional normed spaces is smooth in the sense of
+  `DSmooth` iff it is smooth in the sense of `ContDiff ℝ ∞`.
 
 ## Implementation notes
 
-Instead of defining diffeologies as collections of plots whose domains are the spaces ℝⁿ, we could
-have also equivalently allowed the domains to be open subsets of ℝⁿ, or arbitrary smooth manifolds.
-We chose to go with the first option because the latter two can both be somewhat inconvenient to
-work with in Lean.
+### Choice of test spaces
 
-A downside of working only with the spaces ℝⁿ instead of also their subsets is that it makes the
-sheaf condition / locality condition of diffeologies somewhat awkward to state and prove; to
-mitigate this, we provide `DiffeologicalSpace.mkOfPlotsOn` as a way to construct a diffeology from
-plots whose domains are subsets of ℝⁿ.
+Instead of defining diffeologies as collections of plots ℝⁿ → X whose domains are the spaces ℝⁿ, we
+could have also defined them in terms of maps from some other collection of test spaces; for
+example:
+* all open balls in the spaces ℝⁿ
+* all open subsets of the spaces ℝⁿ
+* all finite-dimensional normed spaces, or open balls therein / open subsets thereof
+* all finite-dimensional smooth manifolds.
+All of these result in equivalent notions of diffeologies and smooth maps; the abstract way to see
+this is that the corresponding sites are all dense subsites of the site of finite-dimensional smooth
+manifolds, and hence give rise to equivalent sheaf topoi. Which of those sites / collections of test
+spaces to use is hence mainly a matter of convenience; we have gone with the cartesian spaces ℝⁿ
+mainly for two reasons:
+* They are the simplest to work with for practical purposes: maps between subsets are more annoying
+  to deal with formally than maps between types, and e.g. smooth manifolds are extremely annoying
+  to quantify over, while the cartesian spaces ℝⁿ are indexed simply by the natural numbers ℕ.
+* Working with e.g. all finite-dimensional normed spaces instead of this particular choice of
+  representatives would lead to an unnecessary universe bump.
+
+One downside of this choice compared to that of all open subsets of ℝⁿ is however that it makes the
+sheaf condition / locality condition of diffeologies ("any map that is locally a plot is also
+globally a plot") somewhat awkward to state and prove. To mitigate this, we provide
+`DiffeologicalSpace.ofPlotsOn` as a way to construct a diffeology from plots whose domains are
+subsets of ℝⁿ. See the definition of `euclideanDiffeology` for an example where this is used.
 
 ### D-Topology
 
-Since the D-topology does not always commute with e.g. products, it can not be registered as an
-instance - otherwise, there would be two `TopologicalSpace`-instances on binary products, the
-product topology of the D-topologies on the factors and the D-topology of the product diffeology.
-We thus instead define a typeclass `IsDTopCompatible X` to express when the topology on `X` does
-match the D-topology, and also give the D-topology the short name `DTop` to enable use of notations
-such as `Continuous[_, DTop]` for continuity with respect to the D-topology.
+While the D-topology is quite well-behaved in some regards, it does unfortunately not always commute
+with e.g. products. This means that it can not be registered as an instance - otherwise, there would
+be two `TopologicalSpace`-instances on binary products, the product topology of the D-topologies on
+the factors and the D-topology of the product diffeology. We thus instead define a typeclass
+`IsDTopCompatible X` to express when the topology on `X` does match the D-topology, and also give
+the D-topology the short name `DTop` to enable use of notations such as `Continuous[_,DTop]`
+for continuity with respect to the D-topology.
 
 In order to make it easier to work with diffeological spaces whose natural diffeology does match
 the D-topology, we also include the D-topology as part of the data of `DiffeologicalSpace X`.
@@ -44,6 +101,7 @@ This allows the diffeologies on e.g. ℝⁿ, manifolds and quotients of diffeolo
 defined in such a way that the D-topology is defeq to the topology that the space already carries.
 
 ### Diffeologies on normed spaces
+
 Every normed spaces carries a natural diffeology consisting of all smooth maps from ℝⁿ. While this
 "normed space diffeology" does commute with arbitrary products, we can't register it as an instance
 because it wouldn't be defeq to the product diffeology on products of normed spaces. We thus only
@@ -55,6 +113,25 @@ To make working with finite-dimensional spaces easier, `euclideanDiffeology` is 
 way that its D-topology is defeq to the topology induced by the norm - however, this currently comes
 at the cost of making the definition work only on finite-dimensional spaces. It should be possible
 to extend this to all Banach or Fréchet spaces though in the future.
+
+## TODO
+
+Much of the basic theory of diffeological spaces has already been formalised at
+https://github.com/peabrainiac/lean-orbifolds and just needs to be upstreamed. However, some TODOs
+that haven't been formalised at all yet and only depend on the material here are:
+* Define a `normedSpaceDiffeology` analogous to `euclideanDiffeology` for all normed spaces (as
+  opposed to just finite-dimensional ones) and show that `IsContDiffCompatible` holds precisely for
+  this diffeology.
+* Show that the D-topology of the normed space diffeology is simply the topology induced by the
+  norm. On paper, this is relatively straightforward: for a set U ⊆ X that is not open under the
+  standard normed space topology, take a sequence x_i outside of U that converges to a point in U,
+  a smooth map ℝ → X under which a convergent sequence in ℝ maps to this sequence (x_i), and use it
+  to conclude that U is not D-open either. However, constructing the needed smooth map explicitly is
+  probably a lot of work. If this can be done without too much effort, change
+  `normedSpaceDiffeology` to make this a definitional equality, and remove `euclideanDiffeology` in
+  favour of `normedSpaceDiffeology` completely.
+* Generalise `dsmooth_iff_contDiff` to infinite-dimensional normed spaces if possible. There should
+  be some references at least for the case of Banach spaces in the literature.
 
 ## References
 
@@ -187,14 +264,14 @@ theorem dsmooth_const {y : Y} : DSmooth fun _ : X ↦ y :=
 
 /-- Replaces the D-topology of a diffeology with another topology equal to it. Useful
 to construct diffeologies with better definitional equalities. -/
-def DiffeologicalSpace.withDTopology {X : Type*} (d : DiffeologicalSpace X)
+def DiffeologicalSpace.replaceDTopology {X : Type*} (d : DiffeologicalSpace X)
     (t : TopologicalSpace X) (h : DTop[d] = t) : DiffeologicalSpace X where
   dTopology := t
   isOpen_iff_preimages_plots := by intro _; rw [← d.isOpen_iff_preimages_plots, ← h]
   __ := d
 
-lemma DiffeologicalSpace.withDTopology_eq {X : Type*} {d : DiffeologicalSpace X}
-    {t : TopologicalSpace X} {h : DTop[d] = t} : d.withDTopology t h = d := by
+lemma DiffeologicalSpace.replaceDTopology_eq {X : Type*} {d : DiffeologicalSpace X}
+    {t : TopologicalSpace X} {h : DTop[d] = t} : d.replaceDTopology t h = d := by
   ext; rfl
 
 /-- A structure with plots specified on open subsets of ℝⁿ rather than ℝⁿ itself. Useful
@@ -232,7 +309,7 @@ structure DiffeologicalSpace.CorePlotsOn (X : Type*) where
 organised in the form of the auxiliary `CorePlotsOn` structure.
 This is more involved in most regards, but also often makes it quite a lot easier to prove
 the locality condition. -/
-def DiffeologicalSpace.mkOfPlotsOn {X : Type*} (d : CorePlotsOn X) : DiffeologicalSpace X where
+def DiffeologicalSpace.ofCorePlotsOn {X : Type*} (d : CorePlotsOn X) : DiffeologicalSpace X where
   plots _ := {p | d.isPlot p}
   constant_plots _ := d.isPlot_const _
   plot_reparam hp hf := d.isPlotOn_univ.mp <|
@@ -262,7 +339,7 @@ instance because we also want to have product diffeologies as an instance, and h
 cause instance diamonds on spaces like `Fin n → ℝ`. -/
 def euclideanDiffeology {X : Type*} [NormedAddCommGroup X] [NormedSpace ℝ X]
     [FiniteDimensional ℝ X] : DiffeologicalSpace X :=
-  DiffeologicalSpace.mkOfPlotsOn {
+  DiffeologicalSpace.ofCorePlotsOn {
     isPlotOn := fun {_ u} _ p ↦ ContDiffOn ℝ ∞ p u
     isPlotOn_congr := fun _ _ _ h ↦ contDiffOn_congr h
     isPlot := fun p ↦ ContDiff ℝ ∞ p
@@ -373,7 +450,7 @@ protected theorem DSmooth.contDiff [FiniteDimensional ℝ X] {f : X → Y} (hf :
 theorem dsmooth_iff_contDiff [FiniteDimensional ℝ X] {f : X → Y} : DSmooth f ↔ ContDiff ℝ ∞ f :=
   ⟨DSmooth.contDiff, ContDiff.dsmooth⟩
 
-/-- Verifies that `fun_prop` is set up correctly. -/
+/-- Verify that `fun_prop` is set up correctly. -/
 example {X Y : Type*} [DiffeologicalSpace X] [DiffeologicalSpace Y] {n m : ℕ} {f : Eucl n → Eucl m}
     {p : Eucl m → X} {g : X → Y} (hf : ContDiff ℝ ∞ f) (hp : IsPlot p) (hg : DSmooth g) :
     IsPlot (g ∘ p ∘ f) := by
