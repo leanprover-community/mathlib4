635eeccd2ab562d49b026ed309e69e2ee4799553
Aaron Liu <aaronliu2008@outlook.com>
2025-04-06 08:35:44 -0400

    patch

--- Mathlib/Algebra/Order/Rearrangement.lean
+++ Mathlib/Algebra/Order/Rearrangement.lean
@@ -69,7 +69,7 @@ theorem MonovaryOn.sum_smul_comp_perm_le_sum_smul (hfg : MonovaryOn f g s)
   classical
   revert hσ σ hfg
   apply Finset.induction_on_max_value (fun i ↦ toLex (g i, f i))
-    (p := fun t ↦ ∀ {σ : Perm ι}, MonovaryOn f g t → {x | σ x ≠ x} ⊆ t →
+    (motive := fun t ↦ ∀ {σ : Perm ι}, MonovaryOn f g t → {x | σ x ≠ x} ⊆ t →
       ∑ i ∈ t, f i • g (σ i) ≤ ∑ i ∈ t, f i • g i) s
   · simp only [le_rfl, Finset.sum_empty, imp_true_iff]
   intro a s has hamax hind σ hfg hσ
