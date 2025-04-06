3040c5165151943d855501d19f22a8e19f730c2b
Joël Riou <37772949+joelriou@users.noreply.github.com>
2025-04-06 12:03:46 +0200

    Update Mathlib/AlgebraicTopology/ModelCategory/Instances.lean

--- Mathlib/AlgebraicTopology/ModelCategory/Instances.lean
+++ Mathlib/AlgebraicTopology/ModelCategory/Instances.lean
@@ -3,7 +3,6 @@ Copyright (c) 2024 Joël Riou. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joël Riou
 -/
-
 import Mathlib.CategoryTheory.MorphismProperty.WeakFactorizationSystem
 import Mathlib.AlgebraicTopology.ModelCategory.CategoryWithCofibrations
 
