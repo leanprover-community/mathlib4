f0683546e4bc153c0c018474209ee0f1a82ea0c8
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-04-08 00:28:22 +0200

    Fix syntax error; add doc-string

--- Mathlib/Tactic/Basic.lean
+++ Mathlib/Tactic/Basic.lean
@@ -164,5 +164,9 @@ end Mathlib.Tactic
 /-- A mathlib library note: the note's content should be contained in its doc-string. -/
 def LibraryNote := Unit
 
-open Lean in macro "library_note2 " name:ident ppSpace dc:docComment : command =>
+
+open Lean in
+/-- `library_note2 myNote /-- documentation -/` creates a library note named `myNote`
+in the `Mathlib.LibraryNote` namespace, whose content is `/-- documentation -/`. -/
+macro "library_note2 " name:ident ppSpace dc:docComment : command =>
   `($dc:docComment def $(mkIdent (Name.append `Mathlib.LibraryNote name.getId)) : LibraryNote := ())
