105a4d54e9a5c60515c829d35e2ce65787955091
SEU-Prime <79910080@qq.com>
2025-04-07 23:56:43 +0800

    Create AmiceTrans.lean

---
+++ Mathlib/NumberTheory/Padics/AmiceTrans.lean
@@ -0,0 +1,29 @@
+/-
+Copyright (c) 2025 Hanliu Jiang. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Shanwen Wang, Hanliu Jiang
+-/
+
+import Mathlib.NumberTheory.Padics.MahlerBasis
+import Mathlib.RingTheory.PowerSeries.Basic
+/-!
+# The Amice Transform of p-adic measure
+
+
+## References
+
+* [P. Colmez, *Fonctions d'une variable p-adique*][colmez2010]
+
+## Tags
+
+Bojanic
+-/
+
+open Finset IsUltrametricDist NNReal Filter PowerSeries
+
+open scoped fwdDiff ZeroAtInfty Topology
+
+variable {p : ℕ} [hp : Fact p.Prime]
+
+namespace PadicInt
+
