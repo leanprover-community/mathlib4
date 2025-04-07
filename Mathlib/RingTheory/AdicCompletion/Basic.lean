edd5d9c3a805fb74d29174d762b9e7bc87d27982
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2025-04-07 23:41:44 +0200

    Update Mathlib/RingTheory/AdicCompletion/Basic.lean

--- Mathlib/RingTheory/AdicCompletion/Basic.lean
+++ Mathlib/RingTheory/AdicCompletion/Basic.lean
@@ -85,9 +85,9 @@ abbrev Hausdorffification : Type _ :=
 to define `AdicCompletion`. -/
 abbrev AdicCompletion.transitionMap {m n : ℕ} (hmn : m ≤ n) := factorPow I M hmn
 
-/-- The completion of a module with respect to an ideal. This is Hausdorff but not
-necessarily complete: a typical sufficient condition for completeness is for the ideal to be
-finitely generated. -/
+/-- The completion of a module with respect to an ideal. 
+
+This is Hausdorff but not necessarily complete: a classical sufficient condition for completeness is  that `M` be finitely generated [Stacks, 0G1Q]. -/
 def AdicCompletion : Type _ :=
   { f : ∀ n : ℕ, M ⧸ (I ^ n • ⊤ : Submodule R M) //
     ∀ {m n} (hmn : m ≤ n), AdicCompletion.transitionMap I M hmn (f n) = f m }
