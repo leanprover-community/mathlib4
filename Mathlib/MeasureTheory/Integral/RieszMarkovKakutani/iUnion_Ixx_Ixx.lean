44384a0b48863b805bafd732300639f4f391839c
Oliver Butterley <oliver.butterley@gmail.com>
2025-04-07 09:58:38 +0200

    more concise argument

--- Mathlib/MeasureTheory/Integral/RieszMarkovKakutani/iUnion_Ixx_Ixx.lean
+++ Mathlib/MeasureTheory/Integral/RieszMarkovKakutani/iUnion_Ixx_Ixx.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2025 . All rights reserved.
+Copyright (c) 2025 Oliver Butterley. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: _
+Authors: Oliver Butterley
 -/
 import Mathlib.Data.Set.Lattice
 import Mathlib.Data.Finset.Range
@@ -19,9 +19,7 @@ lemma iUnion_Ioc_subset_Ioc {X : Type*} [LinearOrder X] (N : ℕ) (a : ℕ → X
   | zero => simp
   | succ N ih => calc
     _ ⊆ Ioc (a 0) (a N) ∪ Ioc (a N) (a (N + 1)) := Ioc_subset_Ioc_union_Ioc
-    _ ⊆ _ := by
-      simp_rw [Finset.range_succ, Finset.mem_insert, iUnion_iUnion_eq_or_left, union_comm]
-      exact union_subset_union_right (Ioc (a N) (a (N + 1))) ih
+    _ ⊆ _ := by simpa [Finset.range_succ] using union_subset_union_right (Ioc (a N) (a (N + 1))) ih
 
 lemma iUnion_Ico_subset_Ico {X : Type*} [LinearOrder X] (N : ℕ) (a : ℕ → X) :
     Ico (a 0) (a N) ⊆ ⋃ i ∈ Finset.range N, Ico (a i) (a (i + 1)) := by
@@ -29,6 +27,4 @@ lemma iUnion_Ico_subset_Ico {X : Type*} [LinearOrder X] (N : ℕ) (a : ℕ → X
   | zero => simp
   | succ N ih => calc
     _ ⊆ Ico (a 0) (a N) ∪ Ico (a N) (a (N + 1)) := Ico_subset_Ico_union_Ico
-    _ ⊆ _ := by
-      simp_rw [Finset.range_succ, Finset.mem_insert, iUnion_iUnion_eq_or_left, union_comm]
-      exact union_subset_union_right (Ico (a N) (a (N + 1))) ih
+    _ ⊆ _ := by simpa [Finset.range_succ] using union_subset_union_right (Ico (a N) (a (N + 1))) ih
