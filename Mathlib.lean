f62f2aa9e31b67c76a9813ac76114db459dd5727
Andrew Yang <the.erd.one@gmail.com>
2025-04-07 16:44:48 +0000

    feat(RingTheory): `dim R + 1 ≤ dim R[X]` (#21088)
    We add the following results
    - `ringKrullDim_quotient_succ_le_of_nonZeroDivisor`: If `r` is not a zero divisor, then
      `dim R/r + 1 ≤ dim R`.
    - `ringKrullDim_succ_le_ringKrullDim_polynomial`: `dim R + 1 ≤ dim R[X]`.
    - `ringKrullDim_add_enatCard_le_ringKrullDim_mvPolynomial`: `dim R + #σ ≤ dim R[σ]`.
    
    
    
    Co-authored-by: Andrew Yang <36414270+erdOne@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -5019,6 +5019,7 @@ import Mathlib.RingTheory.Kaehler.Polynomial
 import Mathlib.RingTheory.Kaehler.TensorProduct
 import Mathlib.RingTheory.KrullDimension.Basic
 import Mathlib.RingTheory.KrullDimension.Field
+import Mathlib.RingTheory.KrullDimension.NonZeroDivisors
 import Mathlib.RingTheory.KrullDimension.Zero
 import Mathlib.RingTheory.Lasker
 import Mathlib.RingTheory.LaurentSeries
