7e647c48cc19a48a73b5c8541d18892d90959bbc
xyzw12345 <wangjt2020@163.com>
2025-04-08 08:23:50 +0800

    Update Mathlib/LinearAlgebra/SymmetricAlgebra/MvPolynomial.lean
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- Mathlib/LinearAlgebra/SymmetricAlgebra/MvPolynomial.lean
+++ Mathlib/LinearAlgebra/SymmetricAlgebra/MvPolynomial.lean
@@ -19,7 +19,7 @@ variable {R L : Type*} [CommSemiring R] [AddCommMonoid L] [Module R L]
   free module and multivariate polynomial over the basis. -/
 noncomputable def SymmetricAlgebra.isoMvPolynomial {I : Type*} (h : Basis I R L) :
     SymmetricAlgebra R L ≃ₐ[R] MvPolynomial I R := .ofAlgHom
-  (SymmetricAlgebra.lift (Basis.constr h R (fun i ↦ ((MvPolynomial.X i) : (MvPolynomial I R)))))
+  (SymmetricAlgebra.lift (Basis.constr h R fun i ↦ (.X i : MvPolynomial I R))
   (MvPolynomial.aeval (R := R) (fun i ↦ SymmetricAlgebra.ι R L (h i)))
   (MvPolynomial.algHom_ext fun i ↦ (by simp))
   (SymmetricAlgebra.algHom_ext <| h.ext fun i ↦ by simp)
