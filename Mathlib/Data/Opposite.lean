0d6f094f0ab77dc98087a2f259d38c9c11b081ac
Yury G. Kudryashov <urkud@urkud.name>
2025-04-08 17:48:19 -0500

    Update Mathlib/Data/Opposite.lean

--- Mathlib/Data/Opposite.lean
+++ Mathlib/Data/Opposite.lean
@@ -101,8 +101,8 @@ instance [Nonempty α] : Nonempty αᵒᵖ := Nonempty.map op ‹_›
 
 instance [Subsingleton α] : Subsingleton αᵒᵖ := unop_injective.subsingleton
 
-@[deprecated Opposite.rec (since := "2025-04-04")]
-protected def rec' {F : αᵒᵖ → Sort v} (h : ∀ X, F (op X)) : ∀ X, F X := fun X => h (unop X)
+@[deprecated (since := "2025-04-04")]
+alias rec' := Opposite.rec
 
 /-- If `X` is `u`-small, also `Xᵒᵖ` is `u`-small.
 Note: This is not an instance, because it tends to mislead typeclass search. -/
