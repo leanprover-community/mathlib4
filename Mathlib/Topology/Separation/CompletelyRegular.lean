3a9da69cb3064a4b90c35359d659e8ea2e6f8354
Yury G. Kudryashov <urkud@urkud.name>
2025-04-08 14:59:54 -0500

    Apply suggestions from code review

--- Mathlib/Topology/Separation/CompletelyRegular.lean
+++ Mathlib/Topology/Separation/CompletelyRegular.lean
@@ -92,22 +92,20 @@ instance T35Space.instT3space [T35Space X] : T3Space X := {}
 instance T4Space.instT35Space [T4Space X] : T35Space X := {}
 
 lemma separatesPoints_continuous_of_completelyRegularSpace [CompletelyRegularSpace X] :
-    SeparatesPoints (Continuous : Set (X → ℝ)) :=
-  by
+    SeparatesPoints (Continuous : Set (X → ℝ)) := by
   intro x y x_ne_y
   obtain ⟨f, f_cont, f_zero, f_one⟩ :=
-    CompletelyRegularSpace.completely_regular x { y } isClosed_singleton x_ne_y
+    CompletelyRegularSpace.completely_regular x {y} isClosed_singleton x_ne_y
   exact ⟨fun x ↦ f x, continuous_subtype_val.comp f_cont, by aesop⟩
 
 @[deprecated (since := "2025-03-21")]
 alias separatesPoints_continuous_of_t35Space := separatesPoints_continuous_of_completelyRegularSpace
 
 lemma separatesPoints_continuous_of_completelyRegularSpace_Icc [CompletelyRegularSpace X] :
-    SeparatesPoints (Continuous : Set (X → I)) :=
-  by
+    SeparatesPoints (Continuous : Set (X → I)) := by
   intro x y x_ne_y
   obtain ⟨f, f_cont, f_zero, f_one⟩ :=
-    CompletelyRegularSpace.completely_regular x { y } isClosed_singleton x_ne_y
+    CompletelyRegularSpace.completely_regular x {y} isClosed_singleton x_ne_y
   exact ⟨f, f_cont, by aesop⟩
 
 @[deprecated (since := "2025-03-21")]
@@ -115,8 +113,7 @@ alias separatesPoints_continuous_of_t35Space_Icc :=
   separatesPoints_continuous_of_completelyRegularSpace_Icc
 
 lemma injective_stoneCechUnit_of_completelyRegularSpace [CompletelyRegularSpace X] :
-    Function.Injective (stoneCechUnit : X → StoneCech X) :=
-  by
+    Function.Injective (stoneCechUnit : X → StoneCech X) := by
   intros a b hab
   contrapose hab
   obtain ⟨f, fc, fab⟩ := separatesPoints_continuous_of_completelyRegularSpace_Icc hab
