d08233d90012a65b808b2dbc00bdc9da201197be
xyzw12345 <wangjt2020@163.com>
2025-04-08 23:07:36 +0800

    Update Mathlib/LinearAlgebra/SymmetricAlgebra/MvPolynomial.lean
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- Mathlib/LinearAlgebra/SymmetricAlgebra/MvPolynomial.lean
+++ Mathlib/LinearAlgebra/SymmetricAlgebra/MvPolynomial.lean
@@ -18,9 +18,12 @@ variable {R M : Type*} [CommSemiring R] [AddCommMonoid M] [Module R M]
 /-- `SymmetricAlgebra.isoMvPolynomial` give an algebra isomorphism between symmetric algebra over a
 free module and multivariate polynomial over the basis. -/
 noncomputable def SymmetricAlgebra.isoMvPolynomial {I : Type*} (h : Basis I R M) :
-    SymmetricAlgebra R M ≃ₐ[R] MvPolynomial I R := .ofAlgHom
-  (SymmetricAlgebra.lift <| Basis.constr h R .X) (MvPolynomial.aeval fun i ↦ ι R M (h i))
-  (MvPolynomial.algHom_ext fun i ↦ by simp) (algHom_ext <| h.ext fun i ↦ by simp)
+    SymmetricAlgebra R M ≃ₐ[R] MvPolynomial I R :=
+  .ofAlgHom
+    (SymmetricAlgebra.lift <| Basis.constr h R .X)
+    (MvPolynomial.aeval fun i ↦ ι R M (h i))
+    (MvPolynomial.algHom_ext fun i ↦ by simp) 
+    (algHom_ext <| h.ext fun i ↦ by simp)
 
 theorem IsSymmetricAlgebra.mvPolynomial (I : Type*) (h : Basis I R M) :
     IsSymmetricAlgebra (Basis.constr h R (.X : I → MvPolynomial I R)) :=
