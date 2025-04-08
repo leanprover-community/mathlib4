4ccb106bb583f8fdb0b2cde77ea2c098976bc1d5
xyzw12345 <wangjt2020@163.com>
2025-04-08 08:24:04 +0800

    Update Mathlib/LinearAlgebra/SymmetricAlgebra/MvPolynomial.lean
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- Mathlib/LinearAlgebra/SymmetricAlgebra/MvPolynomial.lean
+++ Mathlib/LinearAlgebra/SymmetricAlgebra/MvPolynomial.lean
@@ -21,7 +21,7 @@ noncomputable def SymmetricAlgebra.isoMvPolynomial {I : Type*} (h : Basis I R L)
     SymmetricAlgebra R L ≃ₐ[R] MvPolynomial I R := .ofAlgHom
   (SymmetricAlgebra.lift (Basis.constr h R fun i ↦ (.X i : MvPolynomial I R))
   (MvPolynomial.aeval (R := R) (fun i ↦ SymmetricAlgebra.ι R L (h i)))
-  (MvPolynomial.algHom_ext fun i ↦ (by simp))
+  (MvPolynomial.algHom_ext fun i ↦ by simp)
   (SymmetricAlgebra.algHom_ext <| h.ext fun i ↦ by simp)
 
 theorem IsSymmetricAlgebra.mvPolynomial (I : Type*) (h : Basis I R L) :
