42ce50c1a17498bb14c5f18158df3e3edc97370d
emilyriehl <eriehl@jhu.edu>
2025-04-09 14:23:15 -0400

    argument error

--- Mathlib/AlgebraicTopology/SimplicialSet/StrictSegal.lean
+++ Mathlib/AlgebraicTopology/SimplicialSet/StrictSegal.lean
@@ -222,7 +222,7 @@ in paths. -/
 noncomputable def segalSpine : Z _⦋2⦌₂ ⟶ Z _⦋1⦌₂ ⨯ Z _⦋1⦌₂ :=
   prod.lift (Z.map (δ 2).op) (Z.map (δ 0).op)
 
-instance (sz : StrictSegal Z) : Mono (Y := Path Z 2) (Z.spine 2) :=
+instance (sz : StrictSegal Z) : Mono (Y := Path Z 2) (Z.spine 2 (by omega)) :=
   (CategoryTheory.mono_iff_injective _).mpr (StrictSegal.spineInjective sz 2 _)
 
 /-- Paths of length two in a 2-truncated simplicial set include into pairs of 1-simplices. -/
