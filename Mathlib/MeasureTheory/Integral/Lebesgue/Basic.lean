32a3b186002de5f7c70b9afbc9e8e1137f061961
Parcly Taxel <reddeloostw@gmail.com>
2025-04-09 14:05:42 +0800

    `Sub`

--- Mathlib/MeasureTheory/Integral/Lebesgue/Basic.lean
+++ Mathlib/MeasureTheory/Integral/Lebesgue/Basic.lean
@@ -907,30 +907,6 @@ theorem lintegral_iSup_ae {f : ℕ → α → ℝ≥0∞} (hf : ∀ n, Measurabl
     simp only [not_forall, not_le, mem_setOf_eq, not_exists, not_lt] at this
     exact this n
 
-theorem lintegral_sub' {f g : α → ℝ≥0∞} (hg : AEMeasurable g μ) (hg_fin : ∫⁻ a, g a ∂μ ≠ ∞)
-    (h_le : g ≤ᵐ[μ] f) : ∫⁻ a, f a - g a ∂μ = ∫⁻ a, f a ∂μ - ∫⁻ a, g a ∂μ := by
-  refine ENNReal.eq_sub_of_add_eq hg_fin ?_
-  rw [← lintegral_add_right' _ hg]
-  exact lintegral_congr_ae (h_le.mono fun x hx => tsub_add_cancel_of_le hx)
-
-theorem lintegral_sub {f g : α → ℝ≥0∞} (hg : Measurable g) (hg_fin : ∫⁻ a, g a ∂μ ≠ ∞)
-    (h_le : g ≤ᵐ[μ] f) : ∫⁻ a, f a - g a ∂μ = ∫⁻ a, f a ∂μ - ∫⁻ a, g a ∂μ :=
-  lintegral_sub' hg.aemeasurable hg_fin h_le
-
-theorem lintegral_sub_le' (f g : α → ℝ≥0∞) (hf : AEMeasurable f μ) :
-    ∫⁻ x, g x ∂μ - ∫⁻ x, f x ∂μ ≤ ∫⁻ x, g x - f x ∂μ := by
-  rw [tsub_le_iff_right]
-  by_cases hfi : ∫⁻ x, f x ∂μ = ∞
-  · rw [hfi, add_top]
-    exact le_top
-  · rw [← lintegral_add_right' _ hf]
-    gcongr
-    exact le_tsub_add
-
-theorem lintegral_sub_le (f g : α → ℝ≥0∞) (hf : Measurable f) :
-    ∫⁻ x, g x ∂μ - ∫⁻ x, f x ∂μ ≤ ∫⁻ x, g x - f x ∂μ :=
-  lintegral_sub_le' f g hf.aemeasurable
-
 theorem lintegral_strict_mono_of_ae_le_of_frequently_ae_lt {f g : α → ℝ≥0∞} (hg : AEMeasurable g μ)
     (hfi : ∫⁻ x, f x ∂μ ≠ ∞) (h_le : f ≤ᵐ[μ] g) (h : ∃ᵐ x ∂μ, f x ≠ g x) :
     ∫⁻ x, f x ∂μ < ∫⁻ x, g x ∂μ := by
@@ -955,88 +931,6 @@ theorem setLIntegral_strict_mono {f g : α → ℝ≥0∞} {s : Set α} (hsm : M
     (h : ∀ᵐ x ∂μ, x ∈ s → f x < g x) : ∫⁻ x in s, f x ∂μ < ∫⁻ x in s, g x ∂μ :=
   lintegral_strict_mono (by simp [hs]) hg.aemeasurable hfi ((ae_restrict_iff' hsm).mpr h)
 
-/-- Monotone convergence theorem for nonincreasing sequences of functions -/
-theorem lintegral_iInf_ae {f : ℕ → α → ℝ≥0∞} (h_meas : ∀ n, Measurable (f n))
-    (h_mono : ∀ n : ℕ, f n.succ ≤ᵐ[μ] f n) (h_fin : ∫⁻ a, f 0 a ∂μ ≠ ∞) :
-    ∫⁻ a, ⨅ n, f n a ∂μ = ⨅ n, ∫⁻ a, f n a ∂μ :=
-  have fn_le_f0 : ∫⁻ a, ⨅ n, f n a ∂μ ≤ ∫⁻ a, f 0 a ∂μ :=
-    lintegral_mono fun _ => iInf_le_of_le 0 le_rfl
-  have fn_le_f0' : ⨅ n, ∫⁻ a, f n a ∂μ ≤ ∫⁻ a, f 0 a ∂μ := iInf_le_of_le 0 le_rfl
-  (ENNReal.sub_right_inj h_fin fn_le_f0 fn_le_f0').1 <|
-    show ∫⁻ a, f 0 a ∂μ - ∫⁻ a, ⨅ n, f n a ∂μ = ∫⁻ a, f 0 a ∂μ - ⨅ n, ∫⁻ a, f n a ∂μ from
-      calc
-        ∫⁻ a, f 0 a ∂μ - ∫⁻ a, ⨅ n, f n a ∂μ = ∫⁻ a, f 0 a - ⨅ n, f n a ∂μ :=
-          (lintegral_sub (.iInf h_meas)
-              (ne_top_of_le_ne_top h_fin <| lintegral_mono fun _ => iInf_le _ _)
-              (ae_of_all _ fun _ => iInf_le _ _)).symm
-        _ = ∫⁻ a, ⨆ n, f 0 a - f n a ∂μ := congr rfl (funext fun _ => ENNReal.sub_iInf)
-        _ = ⨆ n, ∫⁻ a, f 0 a - f n a ∂μ :=
-          (lintegral_iSup_ae (fun n => (h_meas 0).sub (h_meas n)) fun n =>
-            (h_mono n).mono fun _ ha => tsub_le_tsub le_rfl ha)
-        _ = ⨆ n, ∫⁻ a, f 0 a ∂μ - ∫⁻ a, f n a ∂μ :=
-          (have h_mono : ∀ᵐ a ∂μ, ∀ n : ℕ, f n.succ a ≤ f n a := ae_all_iff.2 h_mono
-          have h_mono : ∀ n, ∀ᵐ a ∂μ, f n a ≤ f 0 a := fun n =>
-            h_mono.mono fun a h => by
-              induction' n with n ih
-              · exact le_rfl
-              · exact le_trans (h n) ih
-          congr_arg iSup <|
-            funext fun n =>
-              lintegral_sub (h_meas _) (ne_top_of_le_ne_top h_fin <| lintegral_mono_ae <| h_mono n)
-                (h_mono n))
-        _ = ∫⁻ a, f 0 a ∂μ - ⨅ n, ∫⁻ a, f n a ∂μ := ENNReal.sub_iInf.symm
-
-/-- Monotone convergence theorem for nonincreasing sequences of functions -/
-theorem lintegral_iInf {f : ℕ → α → ℝ≥0∞} (h_meas : ∀ n, Measurable (f n)) (h_anti : Antitone f)
-    (h_fin : ∫⁻ a, f 0 a ∂μ ≠ ∞) : ∫⁻ a, ⨅ n, f n a ∂μ = ⨅ n, ∫⁻ a, f n a ∂μ :=
-  lintegral_iInf_ae h_meas (fun n => ae_of_all _ <| h_anti n.le_succ) h_fin
-
-theorem lintegral_iInf' {f : ℕ → α → ℝ≥0∞} (h_meas : ∀ n, AEMeasurable (f n) μ)
-    (h_anti : ∀ᵐ a ∂μ, Antitone (fun i ↦ f i a)) (h_fin : ∫⁻ a, f 0 a ∂μ ≠ ∞) :
-    ∫⁻ a, ⨅ n, f n a ∂μ = ⨅ n, ∫⁻ a, f n a ∂μ := by
-  simp_rw [← iInf_apply]
-  let p : α → (ℕ → ℝ≥0∞) → Prop := fun _ f' => Antitone f'
-  have hp : ∀ᵐ x ∂μ, p x fun i => f i x := h_anti
-  have h_ae_seq_mono : Antitone (aeSeq h_meas p) := by
-    intro n m hnm x
-    by_cases hx : x ∈ aeSeqSet h_meas p
-    · exact aeSeq.prop_of_mem_aeSeqSet h_meas hx hnm
-    · simp only [aeSeq, hx, if_false]
-      exact le_rfl
-  rw [lintegral_congr_ae (aeSeq.iInf h_meas hp).symm]
-  simp_rw [iInf_apply]
-  rw [lintegral_iInf (aeSeq.measurable h_meas p) h_ae_seq_mono]
-  · congr
-    exact funext fun n ↦ lintegral_congr_ae (aeSeq.aeSeq_n_eq_fun_n_ae h_meas hp n)
-  · rwa [lintegral_congr_ae (aeSeq.aeSeq_n_eq_fun_n_ae h_meas hp 0)]
-
-/-- Monotone convergence for an infimum over a directed family and indexed by a countable type -/
-theorem lintegral_iInf_directed_of_measurable {mα : MeasurableSpace α} [Countable β]
-    {f : β → α → ℝ≥0∞} {μ : Measure α} (hμ : μ ≠ 0) (hf : ∀ b, Measurable (f b))
-    (hf_int : ∀ b, ∫⁻ a, f b a ∂μ ≠ ∞) (h_directed : Directed (· ≥ ·) f) :
-    ∫⁻ a, ⨅ b, f b a ∂μ = ⨅ b, ∫⁻ a, f b a ∂μ := by
-  cases nonempty_encodable β
-  cases isEmpty_or_nonempty β
-  · simp only [iInf_of_empty, lintegral_const,
-      ENNReal.top_mul (Measure.measure_univ_ne_zero.mpr hμ)]
-  inhabit β
-  have : ∀ a, ⨅ b, f b a = ⨅ n, f (h_directed.sequence f n) a := by
-    refine fun a =>
-      le_antisymm (le_iInf fun n => iInf_le _ _)
-        (le_iInf fun b => iInf_le_of_le (Encodable.encode b + 1) ?_)
-    exact h_directed.sequence_le b a
-  calc
-    ∫⁻ a, ⨅ b, f b a ∂μ
-    _ = ∫⁻ a, ⨅ n, (f ∘ h_directed.sequence f) n a ∂μ := by simp only [this, Function.comp_apply]
-    _ = ⨅ n, ∫⁻ a, (f ∘ h_directed.sequence f) n a ∂μ := by
-      rw [lintegral_iInf ?_ h_directed.sequence_anti]
-      · exact hf_int _
-      · exact fun n => hf _
-    _ = ⨅ b, ∫⁻ a, f b a ∂μ := by
-      refine le_antisymm (le_iInf fun b => ?_) (le_iInf fun n => ?_)
-      · exact iInf_le_of_le (Encodable.encode b + 1) (lintegral_mono <| h_directed.sequence_le b)
-      · exact iInf_le (fun b => ∫⁻ a, f b a ∂μ) _
-
 /-- Known as Fatou's lemma, version with `AEMeasurable` functions -/
 theorem lintegral_liminf_le' {f : ℕ → α → ℝ≥0∞} (h_meas : ∀ n, AEMeasurable (f n) μ) :
     ∫⁻ a, liminf (fun n => f n a) atTop ∂μ ≤ liminf (fun n => ∫⁻ a, f n a ∂μ) atTop :=
@@ -1053,21 +947,6 @@ theorem lintegral_liminf_le' {f : ℕ → α → ℝ≥0∞} (h_meas : ∀ n, AE
 theorem lintegral_liminf_le {f : ℕ → α → ℝ≥0∞} (h_meas : ∀ n, Measurable (f n)) :
     ∫⁻ a, liminf (fun n => f n a) atTop ∂μ ≤ liminf (fun n => ∫⁻ a, f n a ∂μ) atTop :=
   lintegral_liminf_le' fun n => (h_meas n).aemeasurable
-
-theorem lintegral_tendsto_of_tendsto_of_antitone {f : ℕ → α → ℝ≥0∞} {F : α → ℝ≥0∞}
-    (hf : ∀ n, AEMeasurable (f n) μ) (h_anti : ∀ᵐ x ∂μ, Antitone fun n ↦ f n x)
-    (h0 : ∫⁻ a, f 0 a ∂μ ≠ ∞)
-    (h_tendsto : ∀ᵐ x ∂μ, Tendsto (fun n ↦ f n x) atTop (𝓝 (F x))) :
-    Tendsto (fun n ↦ ∫⁻ x, f n x ∂μ) atTop (𝓝 (∫⁻ x, F x ∂μ)) := by
-  have : Antitone fun n ↦ ∫⁻ x, f n x ∂μ := fun i j hij ↦
-    lintegral_mono_ae (h_anti.mono fun x hx ↦ hx hij)
-  suffices key : ∫⁻ x, F x ∂μ = ⨅ n, ∫⁻ x, f n x ∂μ by
-    rw [key]
-    exact tendsto_atTop_iInf this
-  rw [← lintegral_iInf' hf h_anti h0]
-  refine lintegral_congr_ae ?_
-  filter_upwards [h_anti, h_tendsto] with _ hx_anti hx_tendsto
-    using tendsto_nhds_unique hx_tendsto (tendsto_atTop_iInf hx_anti)
 
 open Encodable in
 /-- Monotone convergence for a supremum over a directed family and indexed by a countable type -/
@@ -1300,52 +1179,6 @@ theorem setLIntegral_subtype {s : Set α} (hs : MeasurableSet s) (t : Set s) (f 
     ∫⁻ x in t, f x ∂(μ.comap (↑)) = ∫⁻ x in (↑) '' t, f x ∂μ := by
   rw [(MeasurableEmbedding.subtype_coe hs).restrict_comap, lintegral_subtype_comap hs,
     restrict_restrict hs, inter_eq_right.2 (Subtype.coe_image_subset _ _)]
-
-section UnifTight
-
-/-- If `f : α → ℝ≥0∞` has finite integral, then there exists a measurable set `s` of finite measure
-such that the integral of `f` over `sᶜ` is less than a given positive number.
-
-Also used to prove an `Lᵖ`-norm version in
-`MeasureTheory.MemLp.exists_eLpNorm_indicator_compl_le`. -/
-theorem exists_setLintegral_compl_lt {f : α → ℝ≥0∞} (hf : ∫⁻ a, f a ∂μ ≠ ∞)
-    {ε : ℝ≥0∞} (hε : ε ≠ 0) :
-    ∃ s : Set α, MeasurableSet s ∧ μ s < ∞ ∧ ∫⁻ a in sᶜ, f a ∂μ < ε := by
-  by_cases hf₀ : ∫⁻ a, f a ∂μ = 0
-  · exact ⟨∅, .empty, by simp, by simpa [hf₀, pos_iff_ne_zero]⟩
-  obtain ⟨g, hgf, hg_meas, hgsupp, hgε⟩ :
-      ∃ g ≤ f, Measurable g ∧ μ (support g) < ∞ ∧ ∫⁻ a, f a ∂μ - ε < ∫⁻ a, g a ∂μ := by
-    obtain ⟨g, hgf, hgε⟩ : ∃ (g : α →ₛ ℝ≥0∞) (_ : g ≤ f), ∫⁻ a, f a ∂μ - ε < g.lintegral μ := by
-      simpa only [← lt_iSup_iff, ← lintegral_def] using ENNReal.sub_lt_self hf hf₀ hε
-    refine ⟨g, hgf, g.measurable, ?_, by rwa [g.lintegral_eq_lintegral]⟩
-    exact SimpleFunc.FinMeasSupp.of_lintegral_ne_top <| ne_top_of_le_ne_top hf <|
-      g.lintegral_eq_lintegral μ ▸ lintegral_mono hgf
-  refine ⟨_, measurableSet_support hg_meas, hgsupp, ?_⟩
-  calc
-    ∫⁻ a in (support g)ᶜ, f a ∂μ
-      = ∫⁻ a in (support g)ᶜ, f a - g a ∂μ := setLIntegral_congr_fun
-      (measurableSet_support hg_meas).compl <| ae_of_all _ <| by intro; simp_all
-    _ ≤ ∫⁻ a, f a - g a ∂μ := setLIntegral_le_lintegral _ _
-    _ = ∫⁻ a, f a ∂μ - ∫⁻ a, g a ∂μ :=
-      lintegral_sub hg_meas (ne_top_of_le_ne_top hf <| lintegral_mono hgf) (ae_of_all _ hgf)
-    _ < ε := ENNReal.sub_lt_of_lt_add (lintegral_mono hgf) <|
-      ENNReal.lt_add_of_sub_lt_left (.inl hf) hgε
-
-/-- For any function `f : α → ℝ≥0∞`, there exists a measurable function `g ≤ f` with the same
-integral over any measurable set. -/
-theorem exists_measurable_le_setLintegral_eq_of_integrable {f : α → ℝ≥0∞} (hf : ∫⁻ a, f a ∂μ ≠ ∞) :
-    ∃ (g : α → ℝ≥0∞), Measurable g ∧ g ≤ f ∧ ∀ s : Set α, MeasurableSet s →
-      ∫⁻ a in s, f a ∂μ = ∫⁻ a in s, g a ∂μ := by
-  obtain ⟨g, hmg, hgf, hifg⟩ := exists_measurable_le_lintegral_eq (μ := μ) f
-  use g, hmg, hgf
-  refine fun s hms ↦ le_antisymm ?_ (lintegral_mono hgf)
-  rw [← compl_compl s, setLintegral_compl hms.compl, setLintegral_compl hms.compl, hifg]
-  · gcongr; apply hgf
-  · rw [hifg] at hf
-    exact ne_top_of_le_ne_top hf (setLIntegral_le_lintegral _ _)
-  · exact ne_top_of_le_ne_top hf (setLIntegral_le_lintegral _ _)
-
-end UnifTight
 
 theorem ae_lt_top' {f : α → ℝ≥0∞} (hf : AEMeasurable f μ) (h2f : ∫⁻ x, f x ∂μ ≠ ∞) :
     ∀ᵐ x ∂μ, f x < ∞ := by
