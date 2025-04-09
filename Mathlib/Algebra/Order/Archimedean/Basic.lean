c88eed0bd74514442937694bb9b3473515e19cd2
Parcly Taxel <reddeloostw@gmail.com>
2025-04-09 22:20:18 +0800

    shake

--- Mathlib/Algebra/Order/Archimedean/Basic.lean
+++ Mathlib/Algebra/Order/Archimedean/Basic.lean
@@ -3,7 +3,6 @@ Copyright (c) 2018 Mario Carneiro. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro
 -/
-import Mathlib.Algebra.Group.Action.Defs
 import Mathlib.Algebra.Order.Floor.Semiring
 import Mathlib.Algebra.Order.Monoid.Units
 import Mathlib.Algebra.Order.Ring.Pow
