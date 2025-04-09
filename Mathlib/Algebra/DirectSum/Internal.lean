55eb2f87478d302354ab7b47808ed93a9ed66509
Jireh Loreaux <loreaujy@gmail.com>
2025-04-09 13:16:02 -0500

    feat(Algebra/DirectSum/Internal): adds a few properties of `ℕ`-indexed direct sums over graded monoids

--- Mathlib/Algebra/DirectSum/Internal.lean
+++ Mathlib/Algebra/DirectSum/Internal.lean
@@ -6,6 +6,7 @@ Authors: Eric Wieser, Kevin Buzzard, Jujian Zhang, Fangming Li
 import Mathlib.Algebra.Algebra.Operations
 import Mathlib.Algebra.Algebra.Subalgebra.Basic
 import Mathlib.Algebra.DirectSum.Algebra
+import Mathlib.Data.Finset.NatAntidiagonal
 
 /-!
 # Internally graded rings and algebras
@@ -167,6 +168,15 @@ theorem coe_mul_apply_eq_dfinsuppSum [AddMonoid ι] [SetLike.GradedMonoid A]
 
 @[deprecated (since := "2025-04-06")]
 alias coe_mul_apply_eq_dfinsupp_sum := coe_mul_apply_eq_dfinsuppSum
+
+open Finset in
+theorem coe_mul_apply_eq_sum_antidiagonal [AddMonoid ι] [HasAntidiagonal ι]
+    [SetLike.GradedMonoid A] (r r' : ⨁ i, A i) (n : ι) :
+    (r * r') n = ∑ ij ∈ antidiagonal n, (r ij.1 : R) * r' ij.2 := by
+  classical
+  simp_rw [coe_mul_apply_eq_dfinsuppSum, DFinsupp.sum, ← sum_product']
+  refine sum_congr_of_eq_on_inter (by aesop) ?_ ?_
+  all_goals aesop (erase simp not_and) (add simp not_and_or)
 
 theorem coe_of_mul_apply_aux [AddMonoid ι] [SetLike.GradedMonoid A] {i : ι} (r : A i)
     (r' : ⨁ i, A i) {j n : ι} (H : ∀ x : ι, i + x = n ↔ x = j) :
@@ -428,3 +438,78 @@ theorem SetLike.Homogeneous.smul [CommSemiring S] [Semiring R] [Algebra S R] {A 
   ⟨i, Submodule.smul_mem _ _ hi⟩
 
 end HomogeneousElement
+
+/-! ### ℕ-gradings -/
+
+section Semiring
+
+variable [Semiring R] [SetLike σ R] [AddSubmonoidClass σ R]
+variable {A : ℕ → σ} [SetLike.GradedMonoid A]
+
+theorem mul_apply_eq_zero {r r' : ⨁ i, A i} {m n : ℕ}
+    (hr : ∀ i < m, r i = 0) (hr' : ∀ i < n, r' i = 0) ⦃k : ℕ⦄ (hk : k < m + n) :
+    (r * r') k = 0 := by
+  rw [Subtype.ext_iff, ZeroMemClass.coe_zero, coe_mul_apply_eq_sum_antidiagonal]
+  apply Finset.sum_eq_zero fun x hx ↦ ?_
+  obtain (hx | hx) : x.1 < m ∨ x.2 < n := by simp at hx; omega
+  all_goals simp [hr, hr', hx]
+
+/-- The difference with `DirectSum.listProd_apply_eq_zero` is that the indices at which
+the terms of the list are zero is allowed to vary. -/
+theorem listProd_apply_eq_zero' {l : List ((⨁ i, A i) × ℕ)}
+    (hl : ∀ xn ∈ l, ∀ k < xn.2, xn.1 k = 0) ⦃n : ℕ⦄ (hn : n < (l.map Prod.snd).sum)  :
+    (l.map Prod.fst).prod n = 0 := by
+  induction l generalizing n with
+  | nil => simp [(zero_le n).not_lt] at hn
+  | cons head tail ih =>
+    simp only [List.mem_cons, forall_eq_or_imp, List.map_cons, List.sum_cons,
+      List.prod_cons] at hl hn ⊢
+    exact mul_apply_eq_zero hl.1 (ih hl.2) hn
+
+theorem listProd_apply_eq_zero {l : List (⨁ i, A i)} {m : ℕ}
+    (hl : ∀ x ∈ l, ∀ k < m, x k = 0) ⦃n : ℕ⦄ (hn : n < l.length * m) :
+    l.prod n = 0 := by
+  -- a proof which uses `DirectSum.listProd_apply_eq_zero'` is actually more work
+  induction l generalizing n with
+  | nil => simp [(zero_le n).not_lt] at hn
+  | cons head tail ih =>
+    simp only [List.mem_cons, forall_eq_or_imp, List.length_cons, List.prod_cons] at hl hn ⊢
+    refine mul_apply_eq_zero hl.1 (ih hl.2) ?_
+    simpa [add_mul, add_comm m] using hn
+
+end Semiring
+
+section CommSemiring
+
+variable [CommSemiring R] [SetLike σ R] [AddSubmonoidClass σ R]
+variable {A : ℕ → σ} [SetLike.GradedMonoid A]
+
+/-- The difference with `DirectSum.multisetProd_apply_eq_zero` is that the indices at which
+the terms of the multiset are zero is allowed to vary. -/
+theorem multisetProd_apply_eq_zero' {s : Multiset ((⨁ i, A i) × ℕ)}
+    (hs : ∀ xn ∈ s, ∀ k < xn.2, xn.1 k = 0) ⦃n : ℕ⦄ (hn : n < (s.map Prod.snd).sum)  :
+    (s.map Prod.fst).prod n = 0 := by
+  have := listProd_apply_eq_zero' (l := s.toList) (by simpa using hs)
+    (by simpa [← Multiset.sum_coe, ← Multiset.map_coe])
+  simpa [← Multiset.prod_coe, ← Multiset.map_coe]
+
+theorem multiProd_apply_eq_zero {s : Multiset (⨁ i, A i)} {m : ℕ}
+    (hs : ∀ x ∈ s, ∀ k < m, x k = 0) {n : ℕ} (hn : n < s.card * m) :
+    s.prod n = 0 := by
+  have := listProd_apply_eq_zero (l := s.toList) (by simpa using hs)
+    (by simpa [← Multiset.sum_coe, ← Multiset.map_coe])
+  simpa [← Multiset.prod_coe, ← Multiset.map_coe]
+
+/-- The difference with `DirectSum.finsetProd_apply_eq_zero` is that the indices at which
+the terms of the multiset are zero is allowed to vary. -/
+theorem finsetProd_apply_eq_zero' {s : Finset ((⨁ i, A i) × ℕ)}
+    (hs : ∀ xn ∈ s, ∀ k < xn.2, xn.1 k = 0) ⦃n : ℕ⦄ (hn : n < ∑ xn ∈ s, xn.2)  :
+    (∏ xn ∈ s, xn.1) n = 0 := by
+  simpa using listProd_apply_eq_zero' (l := s.toList) (by simpa using hs) (by simpa)
+
+theorem finsetProd_apply_eq_zero {s : Finset (⨁ i, A i)} {m : ℕ}
+    (hs : ∀ x ∈ s, ∀ k < m, x k = 0) {n : ℕ} (hn : n < s.card * m) :
+    (∏ x ∈ s, x) n = 0 := by
+  simpa using listProd_apply_eq_zero (l := s.toList) (by simpa using hs) (by simpa)
+
+end CommSemiring
