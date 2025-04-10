a929e963f540ff68b50a96c111d98f594ff70c54
morel <smorel@math.princeton.edu>
2025-04-10 11:09:59 +0200

    the statement (modulo boundedness)

--- Mathlib/CategoryTheory/Triangulated/Filtered/Derived.lean
+++ Mathlib/CategoryTheory/Triangulated/Filtered/Derived.lean
@@ -6,7 +6,7 @@ import Mathlib.CategoryTheory.Localization.Triangulated
 noncomputable section
 
 open CategoryTheory Preadditive Limits Triangulated CategoryTheory.FilteredTriangulated
-  TStructure
+  TStructure Pretriangulated
 
 open scoped ZeroObject
 
@@ -68,10 +68,6 @@ noncomputable local instance : t₂.homology₀.ShiftSequence ℤ :=
 noncomputable local instance : tF₂.homology₀.ShiftSequence ℤ :=
   Functor.ShiftSequence.tautological _ _
 
-
-variable [t₂.HasHeart] [tF₂.HasHeart] [t₂.HasHomology₀] [tF₂.HasHomology₀]
-  [t₂.homology₀.ShiftSequence ℤ] [tF₂.homology₀.ShiftSequence ℤ]
-
 local instance : L₂.functor.CommShift ℤ := L₂.commShift
 
 local instance : L₂.functor.IsTriangulated := L₂.triangulated
@@ -101,13 +97,86 @@ def AcyclicComplexAcyclic : Triangulated.Subcategory
   (ComplexShape.up ℤ) ⋙ HomotopyCategory.homologyFunctor
   t₁.Heart (ComplexShape.up ℤ) 0).homologicalKernel
 
-#synth IsTriangulated (AcyclicComplexAcyclic t₁ t₂ T).W.Localization
+-- A complex in the homotopy category of `AcyclicCategory T t₁ t₂` is acyclic if and only
+-- if its image in the homotopy category of `t₁.Heart` ia acyclic.
 
-#synth Abelian t₁.Heart
+instance : ObjectProperty.IsClosedUnderIsomorphisms (AcyclicComplexAcyclic t₁ t₂ T).P :=
+  Functor.instIsClosedUnderIsomorphismsPHomologicalKernel
+  (Functor.mapHomotopyCategory (T.AcyclicToHeart t₁ t₂)
+  (ComplexShape.up ℤ) ⋙ HomotopyCategory.homologyFunctor
+  t₁.Heart (ComplexShape.up ℤ) 0)
 
-#check Localization.Construction.lift (Functor.mapHomotopyCategory (T.AcyclicToHeart t₁ t₂)
+lemma AcyclicComplexAcyclic_iff (K : HomotopyCategory (AcyclicCategory T t₁ t₂)
+    (ComplexShape.up ℤ)) :
+    (AcyclicComplexAcyclic t₁ t₂ T).P K ↔ (HomotopyCategory.subcategoryAcyclic t₁.Heart).P
+    (((T.AcyclicToHeart t₁ t₂).mapHomotopyCategory (ComplexShape.up ℤ)).obj K) := sorry
+
+-- A morphism in the homotopy category of `AcyclicCategory T t₁ t₂` has an acyclic
+-- cone if and only if then its image in the homotopy category of `t₁.Heart`
+-- is a quasi-isomorphism.
+lemma AcyclicComplexAcyclic_W {K L : HomotopyCategory (AcyclicCategory T t₁ t₂)
+    (ComplexShape.up ℤ)} (f : K ⟶ L) : (AcyclicComplexAcyclic t₁ t₂ T).W f ↔
+    HomotopyCategory.quasiIso _ _ (((T.AcyclicToHeart t₁ t₂).mapHomotopyCategory
+    (ComplexShape.up ℤ)).map f) := by
+  obtain ⟨X, g, h, dT⟩ := distinguished_cocone_triangle f
+  erw [Subcategory.mem_W_iff_of_distinguished (AcyclicComplexAcyclic t₁ t₂ T) _ dT]
+  rw [AcyclicComplexAcyclic_iff]
+  erw [← Subcategory.mem_W_iff_of_distinguished (HomotopyCategory.subcategoryAcyclic t₁.Heart)
+    _ (((T.AcyclicToHeart t₁ t₂).mapHomotopyCategory
+    (ComplexShape.up ℤ)).map_distinguished _ dT)]
+  rw [HomotopyCategory.quasiIso_eq_subcategoryAcyclic_W]
+  rfl
+
+-- Condition (b) of Proposition A.3.2.
+instance Acyclic_equivalence :
+    (Localization.Construction.lift (Functor.mapHomotopyCategory (T.AcyclicToHeart t₁ t₂)
+    (ComplexShape.up ℤ) ⋙ DerivedCategory.Qh) (W := (AcyclicComplexAcyclic t₁ t₂ T).W)
+    (fun _ _ f hf ↦ (Localization.inverts DerivedCategory.Qh (HomotopyCategory.quasiIso t₁.Heart
+                    (ComplexShape.up ℤ))) _ ((AcyclicComplexAcyclic_W t₁ t₂ T f).mp hf))
+    ).IsEquivalence := sorry
+
+-- First conclusion of Proposition A.3.2:
+-- The functor from `HomotopyCategory (AcyclicCategory T t₁ t₂)` to `HomotopyCategory t₂.Heart`
+-- induced by `T` sends acyclic complexes to acyclic complexes, hence quasi-isomorphisms to
+-- quasi-isomorphisms.
+
+lemma AcyclicComplexAcyclic_image {K : HomotopyCategory (AcyclicCategory T t₁ t₂)
+    (ComplexShape.up ℤ)} (hK : (AcyclicComplexAcyclic t₁ t₂ T).P K) :
+    (HomotopyCategory.subcategoryAcyclic t₂.Heart).P
+    (((T.FromAcyclic t₁ t₂).mapHomotopyCategory (ComplexShape.up ℤ)).obj K) := sorry
+-- This follows from the calculations in the file `Acyclic.lean`, modulo boundedness problems.
+
+lemma AcyclicComplexAcyclic_W_image {K L : HomotopyCategory (AcyclicCategory T t₁ t₂)
+    (ComplexShape.up ℤ)} {f : K ⟶ L} (hf : (AcyclicComplexAcyclic t₁ t₂ T).W f) :
+    HomotopyCategory.quasiIso _ _ (((T.FromAcyclic t₁ t₂).mapHomotopyCategory
+    (ComplexShape.up ℤ)).map f) := by
+  obtain ⟨X, g, h, dT⟩ := distinguished_cocone_triangle f
+  replace hf := (Subcategory.mem_W_iff_of_distinguished (AcyclicComplexAcyclic t₁ t₂ T) _ dT).mp hf
+  replace hf := AcyclicComplexAcyclic_image t₁ t₂ T hf
+  rw [HomotopyCategory.quasiIso_eq_subcategoryAcyclic_W]
+  exact (Subcategory.mem_W_iff_of_distinguished (HomotopyCategory.subcategoryAcyclic t₂.Heart)
+    _ (((T.FromAcyclic t₁ t₂).mapHomotopyCategory
+    (ComplexShape.up ℤ)).map_distinguished _ dT)).mpr hf
+
+-- By the first conclusion and condition (b), we get a functor from `DerivedCategory t₁.Heart`
+-- to `DerivedCategory t₂.Heart`.
+
+def DerivedFunctor : DerivedCategory t₁.Heart ⥤ DerivedCategory t₂.Heart :=
+  (Localization.Construction.lift (Functor.mapHomotopyCategory (T.AcyclicToHeart t₁ t₂)
   (ComplexShape.up ℤ) ⋙ DerivedCategory.Qh) (W := (AcyclicComplexAcyclic t₁ t₂ T).W)
-  (fun X Y f hf ↦ by )
+  (fun _ _ f hf ↦ (Localization.inverts DerivedCategory.Qh (HomotopyCategory.quasiIso t₁.Heart
+                    (ComplexShape.up ℤ))) _ ((AcyclicComplexAcyclic_W t₁ t₂ T f).mp hf))).inv ⋙
+  (Localization.Construction.lift (Functor.mapHomotopyCategory (T.FromAcyclic t₁ t₂)
+  (ComplexShape.up ℤ) ⋙ DerivedCategory.Qh) (W := (AcyclicComplexAcyclic t₁ t₂ T).W)
+  (fun _ _ _ hf ↦ (Localization.inverts DerivedCategory.Qh (HomotopyCategory.quasiIso t₂.Heart
+                    (ComplexShape.up ℤ))) _ (AcyclicComplexAcyclic_W_image t₁ t₂ T hf)))
+
+-- Second statement of Proposition A.3.2: the "commutative" diagram.
+-- This is an existence statement.
+
+def DerivedFunctor_comp :
+    Realization L₁ t₁ tF₁ ⋙ T ≅ DerivedFunctor t₁ t₂ T ⋙ Realization L₂ t₂ tF₂ := sorry
+
 
 end Triangulated.Filtered
 
