e1dbc5f206428cee36bd09f710178ad75432fb39
Bernhard Reinke <b-reinke@users.noreply.github.com>
2025-04-10 23:36:43 +0200

    feat(Algebra/BigOperators/Finprod): add powerset projection lemmas

--- Mathlib/Algebra/BigOperators/Finprod.lean
+++ Mathlib/Algebra/BigOperators/Finprod.lean
@@ -950,6 +950,19 @@ lemma finprod_option {f : Option α → M} (hf : (mulSupport (f ∘ some)).Finit
     exact Option.some_injective _
 
 @[to_additive]
+lemma finprod_mem_powerset_insert {f : Set α → M} {s : Set α} {a : α} (hs : s.Finite)
+    (has : a ∉ s) : ∏ᶠ t ∈ 𝒫 insert a s, f t = ∏ᶠ t ∈ 𝒫 s, f t * f (insert a t) := by
+  rw [Set.powerset_insert, finprod_mem_union (powerset_insert_disjoint has) hs.powerset
+  (hs.powerset.image (insert a)), finprod_mem_image (powerset_insert_injOn has),
+  finprod_mem_mul_distrib hs.powerset]
+
+@[to_additive]
+lemma finprod_mem_powerset_diff_elem {f : Set α → M} {s : Set α} {a : α} (hs : s.Finite)
+    (has : a ∈ s) : ∏ᶠ t ∈ 𝒫 s, f t = ∏ᶠ t ∈ 𝒫 (s \ {a}), f t * f (insert a t) := by
+  nth_rw 1 2 [← Set.insert_diff_self_of_mem has] -- second appearence hidden by notation
+  exact finprod_mem_powerset_insert (hs.subset Set.diff_subset) (not_mem_diff_of_mem rfl)
+
+@[to_additive]
 theorem mul_finprod_cond_ne (a : α) (hf : (mulSupport f).Finite) :
     (f a * ∏ᶠ (i) (_ : i ≠ a), f i) = ∏ᶠ i, f i := by
   classical
--- Mathlib/Data/Set/Image.lean
+++ Mathlib/Data/Set/Image.lean
@@ -564,6 +564,23 @@ theorem powerset_insert (s : Set α) (a : α) : 𝒫 insert a s = 𝒫 s ∪ ins
     · exact subset_trans h (subset_insert a s)
     · exact insert_subset_insert h₁
 
+theorem powerset_insert_disjoint {s : Set α} {a : α} (h : a ∉ s) :
+    Disjoint (𝒫 s) (insert a '' 𝒫 s) := by
+  rw [Set.disjoint_iff_forall_ne]
+  intros u u_mem v v_mem uv
+  have au : a ∉ u := Set.not_mem_subset (Set.subset_of_mem_powerset u_mem) h
+  have av : a ∈ v := by
+    simp only [mem_powerset_iff, mem_image] at v_mem
+    obtain ⟨_, _, eq⟩ := v_mem
+    simp [← eq]
+  exact au (uv ▸ av)
+
+theorem powerset_insert_injOn {s : Set α} {a : α} (h : a ∉ s) :
+    Set.InjOn (insert a) (𝒫 s) := fun u u_mem v v_mem eq ↦ by
+  rw [Subset.antisymm_iff] at eq ⊢
+  rwa [Set.insert_subset_insert_iff <| Set.not_mem_subset ((mem_powerset_iff _ _).mp v_mem) h,
+  Set.insert_subset_insert_iff <| Set.not_mem_subset ((mem_powerset_iff _ _).mp u_mem) h] at eq
+
 /-! ### Lemmas about range of a function. -/
 
 
