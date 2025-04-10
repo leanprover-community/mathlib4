30238a3c4754650af44f7a43f508ed5c12a1bb38
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2025-04-10 19:14:23 +0200

    fixup! wip step 1

--- Mathlib/Analysis/Normed/Group/Seminorm.lean
+++ Mathlib/Analysis/Normed/Group/Seminorm.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: María Inés de Frutos-Fernández, Yaël Dillies
 -/
 import Mathlib.Data.NNReal.Defs
+import Mathlib.Data.NNReal.Archimedean
 import Mathlib.Order.ConditionallyCompleteLattice.Group
 import Mathlib.Tactic.GCongr.CoreAttrs
 
