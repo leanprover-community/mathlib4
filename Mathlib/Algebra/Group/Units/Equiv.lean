a8c3499b1a2f30f4f250668452022ae747693541
Yaël Dillies <yael.dillies@gmail.com>
2025-04-11 17:31:30 +0100

    simp high

--- Mathlib/Algebra/Group/Units/Equiv.lean
+++ Mathlib/Algebra/Group/Units/Equiv.lean
@@ -187,7 +187,8 @@ theorem MulEquiv.inv_symm (G : Type*) [DivisionCommMonoid G] :
 section EquivLike
 variable [Monoid M] [Monoid N] [EquivLike F M N] [MulEquivClass F M N] (f : F) {x : M}
 
-@[to_additive (attr := simp)]
+-- Higher priority to take over the non-additivisable `isUnit_map_iff`
+@[to_additive (attr := simp high)]
 lemma MulEquiv.isUnit_map : IsUnit (f x) ↔ IsUnit x where
   mp hx := by
     simpa using hx.map <| MonoidHom.mk ⟨EquivLike.inv f, EquivLike.injective f <| by simp⟩
