6bfd6c3e4fc10e64d4a56a554f59a3f18736e938
Xavier Genereux <xaviergenereux@hotmail.com>
2025-04-11 17:30:58 +0200

    feat: Ideal.count_associates_eq

--- Mathlib/RingTheory/DedekindDomain/Ideal.lean
+++ Mathlib/RingTheory/DedekindDomain/Ideal.lean
@@ -1196,6 +1196,39 @@ theorem count_associates_factors_eq [DecidableEq (Ideal R)] [DecidableEq <| Asso
     rw [Associates.prime_pow_dvd_iff_le hI hJ']
   omega
 
+/-- Variant of `UniqueFactorizationMonoid.count_normalizedFactors_eq` for associated Ideals. -/
+theorem Ideal.count_associates_eq [DecidableEq (Associates (Ideal R))]
+    [(p : Associates (Ideal R)) → Decidable (Irreducible p)]
+    {a a₀ x : R} {n : ℕ} (hx : Prime x) (ha : ¬x ∣ a) (heq : a₀ = x ^ n * a) :
+    (Associates.mk (span {x})).count (Associates.mk (span {a₀})).factors = n := by
+  have hx0 : x ≠ 0 := Prime.ne_zero hx
+  classical
+  rw [count_associates_factors_eq, UniqueFactorizationMonoid.count_normalizedFactors_eq]
+  · exact (prime_span_singleton_iff.mpr hx).irreducible
+  · exact normalize_eq _
+  · simp only [span_singleton_pow, heq, dvd_span_singleton]
+    exact Ideal.mul_mem_right _ _ (mem_span_singleton_self (x ^ n))
+  · simp only [span_singleton_pow, heq, dvd_span_singleton, mem_span_singleton]
+    rw [pow_add, pow_one, mul_dvd_mul_iff_left (pow_ne_zero n hx0)]
+    exact ha
+  · simp only [Submodule.zero_eq_bot, ne_eq, span_singleton_eq_bot, not_false_eq_true]
+    aesop
+  · exact (span_singleton_prime hx0).mpr hx
+  · simp only [ne_eq, span_singleton_eq_bot]; exact hx0
+
+/-- Variant of `UniqueFactorizationMonoid.count_normalizedFactors_eq` for associated Ideals. -/
+theorem Ideal.count_associates_eq' [DecidableEq (Associates (Ideal R))]
+    [(p : Associates (Ideal R)) → Decidable (Irreducible p)]
+    {a x : R} (hx : Prime x) {n : ℕ} (hle : x ^ n ∣ a) (hlt : ¬x ^ (n + 1) ∣ a) :
+    (Associates.mk (span {x})).count (Associates.mk (span {a})).factors = n := by
+  obtain ⟨q, hq⟩ := hle
+  apply Ideal.count_associates_eq hx _ hq
+  contrapose! hlt with hdvd
+  obtain ⟨q', hq'⟩ := hdvd
+  use q'
+  rw [hq, hq']
+  ring
+
 end
 
 theorem Ideal.le_mul_of_no_prime_factors {I J K : Ideal R}
