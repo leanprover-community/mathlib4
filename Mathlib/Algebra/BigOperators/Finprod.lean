c642f05b06e782333255c93f7e45f0d4abbd05a0
Bernhard Reinke <b-reinke@users.noreply.github.com>
2025-04-11 06:50:59 +0200

    Harmonize with Finset/Powerset

--- Mathlib/Algebra/BigOperators/Finprod.lean
+++ Mathlib/Algebra/BigOperators/Finprod.lean
@@ -949,16 +949,14 @@ lemma finprod_option {f : Option α → M} (hf : (mulSupport (f ∘ some)).Finit
   · rw [finprod_mem_range]
     exact Option.some_injective _
 
-@[to_additive]
 lemma finprod_mem_powerset_insert {f : Set α → M} {s : Set α} {a : α} (hs : s.Finite)
-    (has : a ∉ s) : ∏ᶠ t ∈ 𝒫 insert a s, f t = ∏ᶠ t ∈ 𝒫 s, f t * f (insert a t) := by
+    (has : a ∉ s) : ∏ᶠ t ∈ 𝒫 insert a s, f t = (∏ᶠ t ∈ 𝒫 s, f t) * ∏ᶠ t ∈ 𝒫 s, f (insert a t) := by
   rw [Set.powerset_insert, finprod_mem_union (powerset_insert_disjoint has) hs.powerset
-  (hs.powerset.image (insert a)), finprod_mem_image (powerset_insert_injOn has),
-  finprod_mem_mul_distrib hs.powerset]
+  (hs.powerset.image (insert a)), finprod_mem_image (powerset_insert_injOn has)]
 
-@[to_additive]
 lemma finprod_mem_powerset_diff_elem {f : Set α → M} {s : Set α} {a : α} (hs : s.Finite)
-    (has : a ∈ s) : ∏ᶠ t ∈ 𝒫 s, f t = ∏ᶠ t ∈ 𝒫 (s \ {a}), f t * f (insert a t) := by
+    (has : a ∈ s) : ∏ᶠ t ∈ 𝒫 s, f t = (∏ᶠ t ∈ 𝒫 (s \ {a}), f t)
+    * ∏ᶠ t ∈ 𝒫 (s \ {a}), f (insert a t) := by
   nth_rw 1 2 [← Set.insert_diff_self_of_mem has] -- second appearence hidden by notation
   exact finprod_mem_powerset_insert (hs.subset Set.diff_subset) (not_mem_diff_of_mem rfl)
 
