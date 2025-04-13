bdcac60132135521779b7c5f4415615881c4399d
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2025-04-13 19:20:02 +0800

    fix import

--- Mathlib/RingTheory/Regular/Category.lean
+++ Mathlib/RingTheory/Regular/Category.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jingting Wang, Wanyi He, Nailin Guan
 -/
 import Mathlib.Algebra.Homology.ShortComplex.ModuleCat
-import Mathlib.CategoryTheory.EffectiveEpi.RegularEpi
 import Mathlib.RingTheory.QuotSMulTop
 
 /-!
