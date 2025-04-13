badeb57fc25c08fdde04af87bf32445d303f7a2e
Sebastien Gouezel <sebastien.gouezel@univ-rennes1.fr>
2025-04-13 22:24:22 +0200

    lint again

--- Mathlib/Geometry/Manifold/AnalyticManifold.lean
+++ Mathlib/Geometry/Manifold/AnalyticManifold.lean
@@ -1,6 +1,11 @@
+/-
+Copyright (c) 2023 Michael Lee. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Michael Lee, Geoffrey Irving
+-/
 import Mathlib.Deprecated.AnalyticManifold
 
-set_option linter.directoryDependency false
 set_option linter.style.header false
+set_option linter.directoryDependency false
 
-deprecated_module since 2025-04-13
+deprecated_module (since "2025-04-13")
