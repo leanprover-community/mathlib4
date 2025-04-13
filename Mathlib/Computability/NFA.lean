81401459f7cba4d502f7c53840e520fcc495badf
Rudy Peterson <47577010+rudynicolop@users.noreply.github.com>
2025-04-13 11:42:18 +0200

    Update Mathlib/Computability/NFA.lean
    code style
    
    Co-authored-by: EtienneC30 <66847262+EtienneC30@users.noreply.github.com>

--- Mathlib/Computability/NFA.lean
+++ Mathlib/Computability/NFA.lean
@@ -134,9 +134,7 @@ section Auxilary
 /-- `M.unstep` is the reverse of `M.step`. -/
 def unstep (s : σ) (a : α) : Set σ := {s' | s ∈ M.step s' a}
 
-theorem mem_unstep (s t : σ) (a : α) :
-  s ∈ M.unstep t a ↔ t ∈ M.step s a := by
-  rfl
+theorem mem_unstep {s t : σ} {a : α} : s ∈ M.unstep t a ↔ t ∈ M.step s a := Iff.rfl
 
 /-- `M.unstepSet S a` is the union of `unstep M s a` for all `s ∈ S`. -/
 def unstepSet (S : Set σ) (a : α) : Set σ :=
