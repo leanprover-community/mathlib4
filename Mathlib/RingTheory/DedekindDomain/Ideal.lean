fa21384955e1b79b40bc567f8ed50e9858ee33a5
Xavier Genereux <xaviergenereux@hotmail.com>
2025-04-14 13:49:12 +0200

    doc: add description

--- Mathlib/RingTheory/DedekindDomain/Ideal.lean
+++ Mathlib/RingTheory/DedekindDomain/Ideal.lean
@@ -48,6 +48,7 @@ to add a `(h : ¬ IsField A)` assumption whenever this is explicitly needed.
 dedekind domain, dedekind ring
 -/
 
+set_option linter.style.longFile 1700
 
 variable (R A K : Type*) [CommRing R] [CommRing A] [Field K]
 
@@ -970,6 +971,11 @@ instance isMaximal : v.asIdeal.IsMaximal := v.isPrime.isMaximal v.ne_bot
 
 theorem prime : Prime v.asIdeal := Ideal.prime_of_isPrime v.ne_bot v.isPrime
 
+/--
+The (nonzero) prime elements of the monoid with zero `Ideal R` correspond
+to an element of type `HeightOneSpectrum R`.
+
+See `prime` for the inverse direction. -/
 @[simps]
 def ofPrime (p : Ideal R) (hp : Prime p) : HeightOneSpectrum R :=
   ⟨p, Ideal.isPrime_of_prime hp, hp.ne_zero⟩
