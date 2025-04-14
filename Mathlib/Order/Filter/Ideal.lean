ec0d694bd99f252b0b07d651378ee8aa4c0fdac7
Aaron Liu <aaronliu2008@outlook.com>
2025-04-14 18:20:26 -0400

    restate in term of filters

--- Mathlib/Order/Filter/Ideal.lean
+++ Mathlib/Order/Filter/Ideal.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Aaron Liu
 -/
 import Mathlib.Order.Filter.Basic
-import Mathlib.Order.Ideal
+import Mathlib.Order.PFilter
 
 /-!
 # Filters and Ideals
@@ -31,49 +31,52 @@ open Order OrderDual Set
 /--
 The complement of a filter, as an order ideal of sets.
 -/
-def Filter.complIdeal (f : Filter α) : Ideal (Set α) where
-  carrier := compl ⁻¹' f.sets
-  lower' a b hab ha := f.mem_of_superset ha (compl_anti hab)
-  nonempty' := ⟨∅, by simp⟩
-  directed' a ha b hb := ⟨a ∪ b, by simp_all, subset_union_left, subset_union_right⟩
+def Filter.toPFilter (f : Filter α) : PFilter (Set α) where
+  dual := {
+    carrier := ofDual ⁻¹' f.sets
+    lower' a b hab ha := f.mem_of_superset ha (monotone_id.dual_left hab)
+    nonempty' := ⟨toDual univ, by simp⟩
+    directed' a ha b hb :=
+      ⟨toDual (ofDual a ∩ ofDual b), by simp_all, inter_subset_left, inter_subset_right⟩
+  }
 
 @[simp]
 theorem Filter.mem_complIdeal (f : Filter α) (s : Set α) :
-    s ∈ f.complIdeal ↔ sᶜ ∈ f := Iff.rfl
+    s ∈ f.toPFilter ↔ s ∈ f := Iff.rfl
 
 /--
 The complement of an order ideal of sets, as a filter.
 -/
-def Order.Ideal.complFilter (i : Ideal (Set α)) : Filter α where
-  sets := compl ⁻¹' i
-  univ_sets := by simpa using i.bot_mem
-  sets_of_superset hx hxy := i.lower (compl_anti hxy) hx
-  inter_sets hx hy := by simpa [compl_inter] using i.sup_mem hx hy
+def Order.PFilter.toFilter (i : PFilter (Set α)) : Filter α where
+  sets := i
+  univ_sets := by simpa using i.top_mem
+  sets_of_superset hx hxy := i.mem_of_le (monotone_id.dual_left hxy) hx
+  inter_sets hx hy := by simpa [compl_inter] using i.inf_mem hx hy
 
 @[simp]
-theorem Order.Ideal.mem_complFilter (i : Ideal (Set α)) (s : Set α) :
-    s ∈ i.complFilter ↔ sᶜ ∈ i := Iff.rfl
+theorem Order.PFilter.mem_complFilter (i : PFilter (Set α)) (s : Set α) :
+    s ∈ i.toFilter ↔ s ∈ i := Iff.rfl
 
 @[simp]
-theorem Filter.complFilter_complIdeal (f : Filter α) : f.complIdeal.complFilter = f := by
+theorem Filter.complFilter_complIdeal (f : Filter α) : f.toPFilter.toFilter = f := by
   ext; simp
 @[simp]
-theorem Order.Ideal.complFilter_complIdeal (i : Ideal (Set α)) : i.complFilter.complIdeal = i := by
+theorem Order.Ideal.complFilter_complIdeal (i : PFilter (Set α)) : i.toFilter.toPFilter = i := by
   ext; simp
 
-theorem Filter.complIdeal_anti : Antitone (@Filter.complIdeal α) :=
+theorem Filter.toPFilter_anti : Antitone (@Filter.toPFilter α) :=
   fun _ _ hab _ hb ↦ hab hb
-theorem Order.Ideal.complFilter_anti : Antitone (@Ideal.complFilter α) :=
+theorem Order.PFilter.toFilter_anti : Antitone (@Order.PFilter.toFilter α) :=
   fun _ _ hab _ hb ↦ hab hb
 
 /--
 Filters and ideals are complement to each other.
 -/
 @[simps! apply symm_apply]
-def Filter.complIdealIso : Filter α ≃o (Ideal (Set α))ᵒᵈ :=
+def Filter.complIdealIso : Filter α ≃o (PFilter (Set α))ᵒᵈ :=
   OrderIso.ofHomInv
-    (OrderHom.mk _ Filter.complIdeal_anti.dual_right)
-    (OrderHom.mk _ Ideal.complFilter_anti.dual_left)
+    (OrderHom.mk _ Filter.toPFilter_anti.dual_right)
+    (OrderHom.mk _ PFilter.toFilter_anti.dual_left)
     (by ext; simp) (by ext; simp)
 
 end
