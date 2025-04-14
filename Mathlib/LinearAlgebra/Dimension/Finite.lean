8ee0df3e70f3f6c2142baa457d42817165575843
Junyan Xu <junyanxu.math@gmail.com>
2025-04-14 11:57:59 +0000

    extract lemmas + golfs

--- Mathlib/LinearAlgebra/Dimension/Finite.lean
+++ Mathlib/LinearAlgebra/Dimension/Finite.lean
@@ -553,24 +553,9 @@ variable {ι : Type*}
 @[simp] lemma finite_finsupp :
     Module.Finite R (ι →₀ M) ↔ IsEmpty ι ∨ Subsingleton M ∨ Module.Finite R M ∧ Finite ι where
   mp h := by
-    classical
-    simp only [or_iff_not_imp_left, not_subsingleton_iff_nontrivial, not_isEmpty_iff,
-      Nonempty.forall]
-    refine fun i _ ↦ ⟨.of_surjective _ (Finsupp.lapply_surjective (R := R) (M := M) i), ?_⟩
-    obtain ⟨s, hs⟩ := h
-    suffices (s.sup fun f ↦ f.support).toSet = .univ by
-      exact .of_finite_univ <| this ▸ Finset.finite_toSet _
-    obtain ⟨x, hx⟩ := exists_ne (0 : M)
-    ext j
-    simp only [SetLike.ext_iff, mem_span_iff_exists_finset_subset, Finset.coe_subset,
-      support_subset_iff, ne_eq, Finset.mem_coe, Finsupp.ext_iff, Finsupp.coe_finset_sum,
-      Finsupp.coe_smul, Finset.sum_apply, Pi.smul_apply, mem_top, iff_true, Finset.mem_sup,
-      Finsupp.mem_support_iff, mem_univ] at hs ⊢
-    contrapose! hx
-    obtain ⟨n, t, hts, -, hn⟩ := hs (.single j x)
-    specialize hn j
-    rw [Finset.sum_eq_zero fun k hk ↦ by rw [hx _ <| hts hk, smul_zero]] at hn
-    simpa [eq_comm] using hn
+    simp only [or_iff_not_imp_left, not_subsingleton_iff_nontrivial, not_isEmpty_iff]
+    exact fun ⟨i⟩ _ ↦ ⟨.of_surjective _ (Finsupp.lapply_surjective i),
+      finite_of_span_finite_eq_top_finsupp _ (Finset.finite_toSet _) h.1.choose_spec⟩
   mpr
   | .inl _ => inferInstance
   | .inr <| .inl h => inferInstance
