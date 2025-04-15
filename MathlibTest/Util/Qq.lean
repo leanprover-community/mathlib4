3def1d53cb6e9ce38e7e70bfc4a27b587408f1d6
Paul-Lez <paul.lezeau@gmail.com>
2025-04-15 22:35:38 +0000

    Add extra test

--- MathlibTest/Util/Qq.lean
+++ MathlibTest/Util/Qq.lean
@@ -1,5 +1,6 @@
 import Mathlib.Util.Qq
 import Mathlib.Data.Finset.Basic
+import Mathlib.Data.Real.Basic
 
 
 open Qq Lean Elab Meta
@@ -31,4 +32,8 @@ info: {1, 2, 3} : List ℕ
 #guard_msgs in
 #check by_elab return mkSetLiteralQ q(List ℕ) [q(1), q(2), q(3)]
 
+
+/-- info: {0 • -1, 1 • -1, 2 • -1, 3 • -1} : Finset ℤ -/
+#guard_msgs in
+#check by_elab return mkSetLiteralQ q(Finset ℤ) (List.range 4 |>.map fun n : ℕ ↦ q($n • (-1 : ℤ)))
 end mkSetLiteralQ
