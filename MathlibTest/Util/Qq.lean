8ad18901620c490d2b4e8f51d6f08167f11eeb47
Eric Wieser <efw@google.com>
2025-04-16 00:24:34 +0100

    Update MathlibTest/Util/Qq.lean
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- MathlibTest/Util/Qq.lean
+++ MathlibTest/Util/Qq.lean
@@ -30,7 +30,6 @@ info: {1, 2, 3} : List ℕ
 #guard_msgs in
 #check by_elab return mkSetLiteralQ q(List ℕ) [q(1), q(2), q(3)]
 
-
 /-- info: {0 • -1, 1 • -1, 2 • -1, 3 • -1} : Finset ℤ -/
 #guard_msgs in
 #check by_elab return mkSetLiteralQ q(Finset ℤ) (List.range 4 |>.map fun n : ℕ ↦ q($n • (-1 : ℤ)))
