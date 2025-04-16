0a4acb2b6eefeeb38891fa272c081a84f32f3e31
Paul Lezeau <paul.lezeau@gmail.com>
2025-04-16 08:32:51 +0100

    Update Qq.lean
    Co-authored-by: Eric Wieser <efw@google.com>

--- MathlibTest/Util/Qq.lean
+++ MathlibTest/Util/Qq.lean
@@ -30,8 +30,8 @@ info: {1, 2, 3} : List ℕ
 #guard_msgs in
 #check by_elab return mkSetLiteralQ q(List ℕ) [q(1), q(2), q(3)]
 
-/-- info: {0 • -1, 1 • -1, 2 • -1, 3 • -1} : Finset ℤ -/
+/-- info: {0 ^ 2, 1 ^ 2, 2 ^ 2, 3 ^ 2} : Finset ℕ -/
 #guard_msgs in
-#check by_elab return mkSetLiteralQ q(Finset ℤ) (List.range 4 |>.map fun n : ℕ ↦ q($n • (-1 : ℤ)))
+#check by_elab return mkSetLiteralQ q(Finset ℤ) (List.range 4 |>.map fun n : ℕ ↦ q($n^2))
 
 end mkSetLiteralQ
