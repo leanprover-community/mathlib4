4b68c69c83f966d3f896614bdb43f860bd7199bb
Scott Carnahan <128885296+ScottCarnahan@users.noreply.github.com>
2025-04-16 10:13:49 +0900

    Update Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -4997,6 +4997,7 @@ import Mathlib.RingTheory.GradedAlgebra.Noetherian
 import Mathlib.RingTheory.GradedAlgebra.Radical
 import Mathlib.RingTheory.HahnSeries.Addition
 import Mathlib.RingTheory.HahnSeries.Basic
+import Mathlib.RingTheory.HahnSeries.Binomial
 import Mathlib.RingTheory.HahnSeries.HEval
 import Mathlib.RingTheory.HahnSeries.Multiplication
 import Mathlib.RingTheory.HahnSeries.PowerSeries
