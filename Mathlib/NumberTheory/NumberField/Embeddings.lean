68c23936d31e1b118bb43417bcc8eacb7d1076e2
Xavier Roblot <roblot@math.univ-lyon1.fr>
2025-04-16 18:03:38 +0200

    add instances

--- Mathlib/NumberTheory/NumberField/Embeddings.lean
+++ Mathlib/NumberTheory/NumberField/Embeddings.lean
@@ -1166,11 +1166,24 @@ is a subset of `ℝ`. -/
 @[mk_iff] class IsTotallyReal (K : Type*) [Field K] [NumberField K] where
   isReal : ∀ v : InfinitePlace K, v.IsReal
 
-variable {K : Type*} [Field K] [NumberField K]
+variable (F : Type*) {K : Type*} [Field K] [NumberField K]
 
 theorem nrComplexPlaces_eq_zero_iff :
     nrComplexPlaces K = 0 ↔ IsTotallyReal K := by
   simp [Fintype.card_eq_zero_iff, isEmpty_subtype, isTotallyReal_iff]
+
+variable (K) in
+theorem IsTotally.of_algebra [IsTotallyReal K] [Field F] [NumberField F] [Algebra F K]  :
+    IsTotallyReal F where
+  isReal w := by
+    obtain ⟨W, rfl⟩ : ∃ W : InfinitePlace K, W.comap (algebraMap F K) = w := comap_surjective w
+    exact IsReal.comap _ (IsTotallyReal.isReal W)
+
+instance [IsTotallyReal K] (F : IntermediateField ℚ K)  :
+    IsTotallyReal F := IsTotally.of_algebra F K
+
+ instance [IsTotallyReal K] (F : Subfield K) :
+    IsTotallyReal F := IsTotally.of_algebra F K
 
 variable (K)
 
