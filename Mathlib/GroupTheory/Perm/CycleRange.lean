3395dbfa75048920b938e4bab62ff1802a97cbfc
yuanyi-350 <kysyy1@126.com>
2025-04-17 21:43:41 +0800

    Update CycleRange.lean

--- Mathlib/GroupTheory/Perm/CycleRange.lean
+++ Mathlib/GroupTheory/Perm/CycleRange.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2025 Yi.Yuan. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Yi.Yuan
+-/
 import Mathlib.GroupTheory.Perm.Fin
 
 open Equiv Fin Equiv.Perm
