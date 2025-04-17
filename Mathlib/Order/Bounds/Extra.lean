236656f9ac2267d07bf00cda6349c6116f139881
Christopher Hoskin <christopher.hoskin@gmail.com>
2025-04-17 07:14:34 +0100

    Stop using lowerClosure

--- Mathlib/Order/Bounds/Extra.lean
+++ Mathlib/Order/Bounds/Extra.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Christopher Hoskin
 -/
 import Mathlib.Order.Bounds.Basic
-import Mathlib.Order.UpperLower.Closure
 
 /-!
 # Extra results
@@ -20,8 +19,8 @@ universe u v
 variable {α : Type u} {β : Type v}
 variable [Preorder α] [Preorder β]
 
-lemma DirectedOn.fst_image_times_snd_image_subset_lowerClosure {d : Set (α × β)}
-    (hd : DirectedOn (· ≤ ·) d) : (Prod.fst '' d) ×ˢ (Prod.snd '' d) ⊆ lowerClosure d :=
+lemma DirectedOn.dominated_fst_image_times_snd_image {d : Set (α × β)}
+    (hd : DirectedOn (· ≤ ·) d) : Dominated (· ≤ ·) ((Prod.fst '' d) ×ˢ (Prod.snd '' d)) d :=
   fun ⟨p₁, p₂⟩ hp => by
     simp at hp
     obtain ⟨⟨r₁, hr₁⟩, ⟨r₂, hr₂⟩⟩ := hp
@@ -32,5 +31,5 @@ lemma Monotone.upperBounds_image_of_directedOn_prod {γ : Type*} [Preorder γ] {
     (Hg : Monotone g) {d : Set (α × β)} (hd : DirectedOn (· ≤ ·) d) :
     upperBounds (g '' d) = upperBounds (g '' (Prod.fst '' d) ×ˢ (Prod.snd '' d)) := le_antisymm
   (Hg.upperBounds_image_subset_of_dominated
-    (fun _ ha => hd.fst_image_times_snd_image_subset_lowerClosure ha))
+    (fun _ ha => hd.dominated_fst_image_times_snd_image _ ha))
   (upperBounds_mono_set (image_mono subset_fst_image_prod_snd_image))
