4a5de7fc5426e55d80378ef3520c5132fcc963be
Yury G. Kudryashov <urkud@urkud.name>
2025-04-18 14:33:07 -0500

    mk_all

--- Mathlib.lean
+++ Mathlib.lean
@@ -1316,6 +1316,7 @@ import Mathlib.Analysis.Calculus.BumpFunction.Convolution
 import Mathlib.Analysis.Calculus.BumpFunction.FiniteDimension
 import Mathlib.Analysis.Calculus.BumpFunction.InnerProduct
 import Mathlib.Analysis.Calculus.BumpFunction.Normed
+import Mathlib.Analysis.Calculus.BumpFunction.SmoothApprox
 import Mathlib.Analysis.Calculus.Conformal.InnerProduct
 import Mathlib.Analysis.Calculus.Conformal.NormedSpace
 import Mathlib.Analysis.Calculus.ContDiff.Basic
