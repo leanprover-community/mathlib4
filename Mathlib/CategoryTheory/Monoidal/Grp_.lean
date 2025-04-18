cdcbc269ce46a64337ec4a6a99f00d82d4b747b3
mm2420 <kiolterino@gmail.com>
2025-04-18 12:49:45 +0000

    shake

--- Mathlib/CategoryTheory/Monoidal/Grp_.lean
+++ Mathlib/CategoryTheory/Monoidal/Grp_.lean
@@ -3,7 +3,6 @@ Copyright (c) 2025 Markus Himmel. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Markus Himmel
 -/
-import Mathlib.CategoryTheory.Adjunction.Limits
 import Mathlib.CategoryTheory.Monoidal.Cartesian.Mon_
 import Mathlib.CategoryTheory.Limits.Shapes.Pullback.CommSq
 import Mathlib.CategoryTheory.Limits.ExactFunctor
