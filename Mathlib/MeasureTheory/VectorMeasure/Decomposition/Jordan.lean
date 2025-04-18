39d4754da8bdf5b08fc0621c9c8db3a3f788d0ca
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2025-04-18 15:28:59 +0200

    fix

--- Mathlib/MeasureTheory/VectorMeasure/Decomposition/Jordan.lean
+++ Mathlib/MeasureTheory/VectorMeasure/Decomposition/Jordan.lean
@@ -349,37 +349,39 @@ theorem toSignedMeasure_injective : Injective <| @JordanDecomposition.toSignedMe
   ext1 i hi
   -- we see that the positive parts of the two Jordan decompositions are equal to their
   -- associated signed measures restricted on their associated Hahn decompositions
-  have hμ₁ : (j₁.posPart i).toReal = j₁.toSignedMeasure (i ∩ Sᶜ) := by
+  have hμ₁ : j₁.posPart.real i = j₁.toSignedMeasure (i ∩ Sᶜ) := by
     rw [toSignedMeasure, toSignedMeasure_sub_apply (hi.inter hS₁.compl),
+      measureReal_def (μ := j₁.negPart),
       show j₁.negPart (i ∩ Sᶜ) = 0 from
         nonpos_iff_eq_zero.1 (hS₅ ▸ measure_mono Set.inter_subset_right),
       ENNReal.toReal_zero, sub_zero]
     conv_lhs => rw [← Set.inter_union_compl i S]
-    rw [measure_union,
+    rw [measureReal_union, measureReal_def,
       show j₁.posPart (i ∩ S) = 0 from
         nonpos_iff_eq_zero.1 (hS₄ ▸ measure_mono Set.inter_subset_right),
-      zero_add]
+      ENNReal.toReal_zero, zero_add]
     · refine
         Set.disjoint_of_subset_left Set.inter_subset_right
           (Set.disjoint_of_subset_right Set.inter_subset_right disjoint_compl_right)
     · exact hi.inter hS₁.compl
-  have hμ₂ : (j₂.posPart i).toReal = j₂.toSignedMeasure (i ∩ Tᶜ) := by
+  have hμ₂ : j₂.posPart.real i = j₂.toSignedMeasure (i ∩ Tᶜ) := by
     rw [toSignedMeasure, toSignedMeasure_sub_apply (hi.inter hT₁.compl),
+      measureReal_def (μ := j₂.negPart),
       show j₂.negPart (i ∩ Tᶜ) = 0 from
         nonpos_iff_eq_zero.1 (hT₅ ▸ measure_mono Set.inter_subset_right),
       ENNReal.toReal_zero, sub_zero]
     conv_lhs => rw [← Set.inter_union_compl i T]
-    rw [measure_union,
+    rw [measureReal_union, measureReal_def,
       show j₂.posPart (i ∩ T) = 0 from
         nonpos_iff_eq_zero.1 (hT₄ ▸ measure_mono Set.inter_subset_right),
-      zero_add]
+      ENNReal.toReal_zero, zero_add]
     · exact
         Set.disjoint_of_subset_left Set.inter_subset_right
           (Set.disjoint_of_subset_right Set.inter_subset_right disjoint_compl_right)
     · exact hi.inter hT₁.compl
   -- since the two signed measures associated with the Jordan decompositions are the same,
   -- and the symmetric difference of the Hahn decompositions have measure zero, the result follows
-  rw [← ENNReal.toReal_eq_toReal (measure_ne_top _ _) (measure_ne_top _ _), hμ₁, hμ₂, ← hj]
+  rw [← measureReal_eq_measureReal_iff, hμ₁, hμ₂, ← hj]
   exact of_inter_eq_of_symmDiff_eq_zero_positive hS₁.compl hT₁.compl hi hS₃ hT₃ hST₁
 
 @[simp]
@@ -457,7 +459,7 @@ theorem null_of_totalVariation_zero (s : SignedMeasure α) {i : Set α}
   rw [← toSignedMeasure_toJordanDecomposition s, toSignedMeasure, VectorMeasure.coe_sub,
     Pi.sub_apply, Measure.toSignedMeasure_apply, Measure.toSignedMeasure_apply]
   by_cases hi : MeasurableSet i
-  · rw [if_pos hi, if_pos hi]; simp [hs.1, hs.2]
+  · rw [if_pos hi, if_pos hi]; simp [hs.1, hs.2, measureReal_def]
   · simp [if_neg hi]
 
 theorem absolutelyContinuous_ennreal_iff (s : SignedMeasure α) (μ : VectorMeasure α ℝ≥0∞) :
