2df9c673eeb306e2291874896e9ba6f3d4ba4849
Aaron Liu <aaronliu2008@outlook.com>
2025-04-18 19:11:05 -0400

    shake

--- Mathlib/Topology/UniformSpace/Uniformizable.lean
+++ Mathlib/Topology/UniformSpace/Uniformizable.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Aaron Liu
 -/
 import Mathlib.Topology.Separation.CompletelyRegular
-import Mathlib.Topology.UrysohnsLemma
 
 /-!
 # Uniformizable Spaces
