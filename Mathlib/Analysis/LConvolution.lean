99ba8c1870bbd39e0e23ff0e39bd546933798d85
DavidLedvinka <50644608+DavidLedvinka@users.noreply.github.com>
2025-04-19 06:59:04 -0400

    Update Mathlib/Analysis/LConvolution.lean
    Co-authored-by: EtienneC30 <66847262+EtienneC30@users.noreply.github.com>

--- Mathlib/Analysis/LConvolution.lean
+++ Mathlib/Analysis/LConvolution.lean
@@ -18,7 +18,7 @@ by the formula `(f ⋆ₗ[μ] g) x = ∫⁻ y, (f y) * (g (-y + x)) ∂μ`. This
 formula used by MeasureTheory.convolution for convolution of two functions, however it does agree
 when the domain of `f` and `g` is a commutative group. The main reason for this is so that
 (under sufficient conditions) if `{μ ν π : Measure G} {f g : G → ℝ≥0∞}` are such that
-`μ = π.withDensity f`, `ν = π.withDensity g` where `π` is left multiplication invariant then
+`μ = π.withDensity f`, `ν = π.withDensity g` where `π` is left-invariant then
 `(μ ∗ ν) = π.withDensity (f ⋆ₗ[π] g)`. If the formula in MeasureTheory.convolution was used
 the order of the densities would be flipped.
 
