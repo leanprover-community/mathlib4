8c985fbd53468b3f4e7def4ecdceff29bb41b5f2
DavidLedvinka <50644608+DavidLedvinka@users.noreply.github.com>
2025-04-19 08:44:40 -0400

    Update Mathlib/Analysis/LConvolution.lean
    Co-authored-by: EtienneC30 <66847262+EtienneC30@users.noreply.github.com>

--- Mathlib/Analysis/LConvolution.lean
+++ Mathlib/Analysis/LConvolution.lean
@@ -15,7 +15,7 @@ using the Lebesgue integral.
 
 We define the convolution of two functions using the Lebesgue integral (in the additive case)
 by the formula `(f ⋆ₗ[μ] g) x = ∫⁻ y, (f y) * (g (-y + x)) ∂μ`. This does not agree with the
-formula used by MeasureTheory.convolution for convolution of two functions, however it does agree
+formula used by `MeasureTheory.convolution` for convolution of two functions, however it does agree
 when the domain of `f` and `g` is a commutative group. The main reason for this is so that
 (under sufficient conditions) if `{μ ν π : Measure G} {f g : G → ℝ≥0∞}` are such that
 `μ = π.withDensity f`, `ν = π.withDensity g` where `π` is left-invariant then
