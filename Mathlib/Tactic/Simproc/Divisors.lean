3ac6e558c54d39124500690c665bf30228ae9437
Paul-Lez <paul.lezeau@gmail.com>
2025-04-19 12:47:02 +0000

    Move test and and some new ones

--- Mathlib/Tactic/Simproc/Divisors.lean
+++ Mathlib/Tactic/Simproc/Divisors.lean
@@ -38,29 +38,3 @@ simproc_decl Nat.properDivisorsEq (Nat.properDivisors _) := fun e => do
   let rhs := mkSetLiteralQ q(Finset ℕ) rhsListQ
   let pf ← Meta.mkDecideProof (← mkEq e rhs)
   return .done {expr := rhs, proof? := pf }
-
-example : Nat.divisors 1710 = {1, 2, 3, 5, 6, 9, 10, 15, 18, 19, 30, 38, 45, 57,
-      90, 95, 114, 171, 190, 285, 342, 570, 855, 1710} := by
-  simp only [Nat.divisorsEq]
-
-example : Nat.divisors 57 = {1, 3, 19, 57} := by
-  simp only [Nat.divisorsEq]
-
-example : 2 ≤ Finset.card (Nat.divisors 3) := by
-  simp [Nat.divisorsEq]
-
-/-- error: simp made no progress -/
-#guard_msgs in
-example (n : ℕ) (hn : n ≠ 0) : 1 ≤ Finset.card (Nat.divisors n) := by
-  simp only [Nat.divisorsEq]
-
-example :
-    Nat.properDivisors 1710 = {1, 2, 3, 5, 6, 9, 10, 15, 18, 19, 30, 38, 45, 57,
-      90, 95, 114, 171, 190, 285, 342, 570, 855} := by
-  simp only [Nat.properDivisorsEq]
-
-example : Nat.properDivisors 57 = {1, 3, 19} := by
-  simp only [Nat.properDivisorsEq]
-
-example : 2 ≤ Finset.card (Nat.divisors 3) := by
-  simp [Nat.divisorsEq]
