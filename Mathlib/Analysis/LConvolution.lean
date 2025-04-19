c144b1e3163c828a3862f27477b082a7c3b2b470
DavidLedvinka <david.ledvinka@mail.utoronto.ca>
2025-04-19 07:13:21 -0400

    delete whitespace

--- Mathlib/Analysis/LConvolution.lean
+++ Mathlib/Analysis/LConvolution.lean
@@ -43,16 +43,16 @@ noncomputable def mlconvolution (f g : G → ℝ≥0∞) (μ : Measure G) :
 
 /-- Scoped notation for the multiplicative convolution of functions with respect to a measure `μ`.
 -/
-scoped[MeasureTheory] notation:67 f " ⋆ₗ["μ:67"] " g:66  => MeasureTheory.mlconvolution f g μ
+scoped[MeasureTheory] notation:67 f " ⋆ₗ["μ:67"] " g:66 => MeasureTheory.mlconvolution f g μ
 
 /-- Scoped notation for the multiplicative convolution of functions with respect to volume. -/
-scoped[MeasureTheory] notation:67 f " ⋆ₗ " g:66  => MeasureTheory.mlconvolution f g volume
+scoped[MeasureTheory] notation:67 f " ⋆ₗ " g:66 => MeasureTheory.mlconvolution f g volume
 
 /-- Scoped notation for the additive convolution of functions with respect to a measure `μ`. -/
-scoped[MeasureTheory] notation:67 f " ⋆ₗ["μ:67"] " g:66  => MeasureTheory.lconvolution f g μ
+scoped[MeasureTheory] notation:67 f " ⋆ₗ["μ:67"] " g:66 => MeasureTheory.lconvolution f g μ
 
 /-- Scoped notation for the additive convolution of functions with respect to volume. -/
-scoped[MeasureTheory] notation:67 f " ⋆ₗ " g:66  => MeasureTheory.lconvolution f g volume
+scoped[MeasureTheory] notation:67 f " ⋆ₗ " g:66 => MeasureTheory.lconvolution f g volume
 
 /- The definition of multiplicative convolution of functions. -/
 @[to_additive "The definition of additive convolution of functions"]
