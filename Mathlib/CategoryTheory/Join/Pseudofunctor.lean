6750a67794ec2a9b04ccafce672cab68f5bd4718
Robin Carlier <57142648+robin-carlier@users.noreply.github.com>
2025-04-21 21:37:57 +0200

    make everything univ polymorphic + implement @joelriou's suggestion

--- Mathlib/CategoryTheory/Join/Pseudofunctor.lean
+++ Mathlib/CategoryTheory/Join/Pseudofunctor.lean
@@ -52,199 +52,131 @@ def prelaxFunctorLeft (D : Type u₂) [Category.{v₂} D] :
 -- The proof gets too slow if we put it in a single `pseudofunctor` constructor,
 -- so we break down the component proofs for the pseudofunctors over several lemmas.
 
+section
+variable {A B C D : Type*} [Category A] [Category B] [Category C] [Category D]
+
+
+variable (A) in
 /-- The structural isomorphism for composition of `pseudoFunctorRight`. -/
-def prelaxFunctorRight.mapCompRight (C : Type u₁) [Category.{v₁} C] {x y z : Cat.{v₂, u₂}}
-    (f : x ⟶ y) (g : y ⟶ z) :
-    (prelaxFunctorRight C).map (f ≫ g) ≅
-    (prelaxFunctorRight C).map f ≫ (prelaxFunctorRight C).map g :=
-  mapIsoWhiskerRight (Functor.leftUnitor _).symm _ ≪≫ mapPairComp (𝟭 C) f (𝟭 C) g
+def mapCompRight (F : B ⥤ C) (G : C ⥤ D) :
+    mapPair (𝟭 A) (F ⋙ G) ≅ mapPair (𝟭 A) F ⋙ mapPair (𝟭 A) G :=
+  mapIsoWhiskerRight (Functor.leftUnitor _).symm _ ≪≫ mapPairComp (𝟭 A) F (𝟭 A) G
 
+variable (D) in
 /-- The structural isomorphism for composition of `pseudoFunctorLeft`. -/
-def prelaxFunctorLeft.mapCompLeft (D : Type u₂) [Category.{v₂} D] {x y z : Cat.{v₁, u₁}}
-    (f : x ⟶ y) (g : y ⟶ z) :
-    (prelaxFunctorLeft D).map (f ≫ g) ≅
-    (prelaxFunctorLeft D).map f ≫ (prelaxFunctorLeft D).map g :=
-  mapIsoWhiskerLeft _ (Functor.leftUnitor _).symm ≪≫ mapPairComp f (𝟭 D) g (𝟭 D)
+def mapCompLeft (F : A ⥤ B) (G : B ⥤ C) :
+    mapPair (F ⋙ G) (𝟭 D) ≅ mapPair F (𝟭 D) ⋙ mapPair G (𝟭 D) :=
+  mapIsoWhiskerLeft _ (Functor.leftUnitor _).symm ≪≫ mapPairComp F (𝟭 D) G (𝟭 D)
 
-lemma prelaxFunctorRight.map₂_whisker_left (C : Type u₁) [Category.{v₁} C]
-    {a b c : Cat.{v₂, u₂}} (f : a ⟶ b) {g h : b ⟶ c} (η : g ⟶ h) :
-    (prelaxFunctorRight C).map₂ (f ◁ η) =
-      (mapCompRight C f g).hom ≫ (prelaxFunctorRight C).map f ◁ (prelaxFunctorRight C).map₂ η ≫
-      (mapCompRight C f h).inv := by
-  apply natTrans_ext <;>
-  ext <;>
-  ( simp only [prelaxFunctorRight_toPrelaxFunctorStruct_toPrefunctor_obj, Cat.of_α,
-    prelaxFunctorRight_toPrelaxFunctorStruct_toPrefunctor_map, Functor.comp_obj, inclLeft_obj,
-    mapPair_obj_left, Functor.id_obj, Bicategory.whiskerLeft,
-    prelaxFunctorRight_toPrelaxFunctorStruct_map₂, whiskerLeft_app, mapWhiskerLeft_app,
-    mapCompRight, Iso.trans_hom, Iso.trans_inv, Category.assoc]
-    repeat rw [NatTrans.comp_app]
-    simp )
+variable (A) in
+@[reassoc]
+lemma mapWhiskerLeft_whiskerLeft (F : B ⥤ C) {G H : C ⥤ D} (η : G ⟶ H) :
+    mapWhiskerLeft _ (whiskerLeft F η) =
+      (mapCompRight A F G).hom ≫ whiskerLeft (mapPair (𝟭 A) F) (mapWhiskerLeft _ η) ≫
+      (mapCompRight A F H).inv := by
+  apply natTrans_ext <;> ext <;> simp [mapCompRight]
 
-lemma prelaxFunctorLeft.map₂_whisker_left (D : Type u₂) [Category.{v₂} D]
-    {a b c : Cat.{v₁, u₁}} (f : a ⟶ b) {g h : b ⟶ c} (η : g ⟶ h) :
-    (prelaxFunctorLeft D).map₂ (f ◁ η) =
-      (mapCompLeft D f g).hom ≫ (prelaxFunctorLeft D).map f ◁ (prelaxFunctorLeft D).map₂ η ≫
-      (mapCompLeft D f h).inv := by
-  apply natTrans_ext <;>
-  ext <;>
-  ( simp only [prelaxFunctorLeft_toPrelaxFunctorStruct_toPrefunctor_obj, Cat.of_α,
-    prelaxFunctorLeft_toPrelaxFunctorStruct_toPrefunctor_map, Functor.comp_obj, inclRight_obj,
-    mapPair_obj_left, Functor.id_obj, Bicategory.whiskerLeft,
-    prelaxFunctorRight_toPrelaxFunctorStruct_map₂, whiskerLeft_app, mapWhiskerLeft_app,
-    mapCompLeft, Iso.trans_hom, Iso.trans_inv, Category.assoc]
-    repeat rw [NatTrans.comp_app]
-    simp )
+variable (D) in
+@[reassoc]
+lemma mapWhiskerRight_whiskerLeft (F : A ⥤ B) {G H : B ⥤ C} (η : G ⟶ H) :
+    mapWhiskerRight (whiskerLeft F η) (𝟭 D) =
+      (mapCompLeft D F G).hom ≫ whiskerLeft (mapPair F (𝟭 D)) (mapWhiskerRight η _) ≫
+      (mapCompLeft D F H).inv := by
+  apply natTrans_ext <;> ext <;> simp [mapCompLeft]
 
-lemma prelaxFunctorRight.map₂_whisker_right (C : Type u₁) [Category.{v₁} C]
-    {a b c : Cat.{v₂, u₂}} {f g: a ⟶ b} (η : f ⟶ g) (h : b ⟶ c) :
-    (prelaxFunctorRight C).map₂ (η ▷ h) =
-      (mapCompRight C f h).hom ≫ (prelaxFunctorRight C).map₂ η ▷ (prelaxFunctorRight C).map h ≫
-      (mapCompRight C g h).inv := by
-  apply natTrans_ext <;>
-  ext <;>
-  ( simp only [prelaxFunctorRight_toPrelaxFunctorStruct_toPrefunctor_obj, Cat.of_α,
-    prelaxFunctorRight_toPrelaxFunctorStruct_toPrefunctor_map, Functor.comp_obj, inclLeft_obj,
-    mapPair_obj_left, Functor.id_obj, Bicategory.whiskerLeft,
-    prelaxFunctorRight_toPrelaxFunctorStruct_map₂, whiskerLeft_app, mapWhiskerLeft_app,
-    mapCompRight, Iso.trans_hom, Iso.trans_inv, Category.assoc]
-    repeat rw [NatTrans.comp_app]
-    simp )
+variable (A) in
+@[reassoc]
+lemma mapWhiskerLeft_whiskerRight {F G: B ⥤ C} (η : F ⟶ G) (H : C ⥤ D) :
+    mapWhiskerLeft _ (whiskerRight η H) =
+      (mapCompRight A F H).hom ≫ whiskerRight (mapWhiskerLeft _ η) (mapPair (𝟭 A) H) ≫
+      (mapCompRight A G H).inv := by
+  apply natTrans_ext <;> ext <;> simp [mapCompRight]
 
-lemma prelaxFunctorLeft.map₂_whisker_right (D : Type u₂) [Category.{v₂} D]
-    {a b c : Cat.{v₁, u₁}} {f g: a ⟶ b} (η : f ⟶ g) (h : b ⟶ c) :
-    (prelaxFunctorLeft D).map₂ (η ▷ h) =
-      (mapCompLeft D f h).hom ≫ (prelaxFunctorLeft D).map₂ η ▷ (prelaxFunctorLeft D).map h ≫
-      (mapCompLeft D g h).inv := by
-  apply natTrans_ext <;>
-  ext <;>
-  ( simp only [prelaxFunctorLeft_toPrelaxFunctorStruct_toPrefunctor_obj, Cat.of_α,
-    prelaxFunctorLeft_toPrelaxFunctorStruct_toPrefunctor_map, Functor.comp_obj, inclRight_obj,
-    mapPair_obj_left, Functor.id_obj, Bicategory.whiskerLeft,
-    prelaxFunctorRight_toPrelaxFunctorStruct_map₂, whiskerLeft_app, mapWhiskerLeft_app,
-    mapCompLeft, Iso.trans_hom, Iso.trans_inv, Category.assoc]
-    repeat rw [NatTrans.comp_app]
-    simp )
+variable (D) in
+@[reassoc]
+lemma mapWhiskerRight_whiskerRight {F G: A ⥤ B} (η : F ⟶ G) (H : B ⥤ C) :
+    mapWhiskerRight (whiskerRight η H) _ =
+      (mapCompLeft D F H).hom ≫ whiskerRight (mapWhiskerRight η _) (mapPair H (𝟭 D)) ≫
+      (mapCompLeft D G H).inv := by
+  apply natTrans_ext <;> ext <;> simp [mapCompLeft]
 
-lemma prelaxFunctorRight.map₂_associator (C : Type u₁) [Category.{v₁} C]
-    {a b c d : Cat.{v₂, u₂}} (f : a ⟶ b) (g : b ⟶ c) (h : c ⟶ d) :
-    (prelaxFunctorRight C).map₂ (α_ f g h).hom =
-      (mapCompRight C (f ≫ g) h).hom ≫ (mapCompRight C f g).hom ▷ (prelaxFunctorRight C).map h ≫
-      (α_ ((prelaxFunctorRight C).map f) ((prelaxFunctorRight C).map g)
-        ((prelaxFunctorRight C).map h)).hom ≫
-      (prelaxFunctorRight C).map f ◁ (mapCompRight C g h).inv ≫ (mapCompRight C f (g ≫ h)).inv := by
-  apply natTrans_ext <;>
-  ext <;>
-  ( simp only [prelaxFunctorRight_toPrelaxFunctorStruct_toPrefunctor_obj, Cat.of_α,
-    prelaxFunctorRight_toPrelaxFunctorStruct_toPrefunctor_map, Functor.comp_obj, inclRight_obj,
-    mapPair_obj_left, Cat.comp_obj, associator, prelaxFunctorRight_toPrelaxFunctorStruct_map₂,
-    whiskerLeft_app, mapWhiskerRight_app, Functor.associator_hom_app, Functor.map_id, mapCompRight,
-    Iso.trans_hom, mapIsoWhiskerLeft_hom, Iso.symm_hom, comp_whiskerRight, Bicategory.whiskerLeft,
-    Iso.trans_inv, Category.assoc]
-    repeat rw [NatTrans.comp_app]
-    simp only [mapPair_obj_left, Functor.id_obj, Functor.comp_obj, mapWhiskerLeft_app,
-      Functor.leftUnitor_inv_app, Functor.map_id, inclLeft_obj, Cat.comp_obj, Cat.of_α,
-      mapPairComp_hom_app_left, whiskerLeft_app, Functor.associator_hom_app, whiskerLeft_app,
-      Cat.comp_app, mapPairComp_inv_app_left, mapIsoWhiskerLeft_inv_app, Iso.symm_inv,
-      Functor.leftUnitor_hom_app, Category.comp_id, Category.id_comp]
-    simp )
+variable {E : Type*} [Category E]
 
+variable (A) in
+@[reassoc]
+lemma mapWhiskerLeft_associator (F : B ⥤ C) (G : C ⥤ D) (H : D ⥤ E) :
+    mapWhiskerLeft _ (F.associator G H).hom =
+      (mapCompRight A (F ⋙ G) H).hom ≫ whiskerRight (mapCompRight A F G).hom (mapPair (𝟭 A) H) ≫
+        ((mapPair (𝟭 A) F).associator (mapPair (𝟭 A) G) (mapPair (𝟭 A) H)).hom ≫
+      whiskerLeft (mapPair (𝟭 A) F) (mapCompRight A G H).inv ≫ (mapCompRight A F (G ⋙ H)).inv := by
+  apply natTrans_ext <;> ext <;> simp [mapCompRight]
 
-lemma prelaxFunctorLeft.map₂_associator (D : Type u₂) [Category.{v₂} D]
-    {a b c d: Cat.{v₁, u₁}} (f : a ⟶ b) (g : b ⟶ c) (h : c ⟶ d) :
-    (prelaxFunctorLeft D).map₂ (α_ f g h).hom =
-      (mapCompLeft D (f ≫ g) h).hom ≫ (mapCompLeft D f g).hom ▷ (prelaxFunctorLeft D).map h ≫
-      (α_ ((prelaxFunctorLeft D).map f) ((prelaxFunctorLeft D).map g)
-        ((prelaxFunctorLeft D).map h)).hom ≫
-      (prelaxFunctorLeft D).map f ◁ (mapCompLeft D g h).inv ≫ (mapCompLeft D f (g ≫ h)).inv := by
-  apply natTrans_ext <;>
-  ext <;>
-  ( simp only [prelaxFunctorLeft_toPrelaxFunctorStruct_toPrefunctor_obj, Cat.of_α,
-    prelaxFunctorLeft_toPrelaxFunctorStruct_toPrefunctor_map, Functor.comp_obj, inclLeft_obj,
-    mapPair_obj_left, Cat.comp_obj, associator, prelaxFunctorLeft_toPrelaxFunctorStruct_map₂,
-    whiskerLeft_app, mapWhiskerRight_app, Functor.associator_hom_app, Functor.map_id, mapCompLeft,
-    Iso.trans_hom, mapIsoWhiskerLeft_hom, Iso.symm_hom, comp_whiskerRight, Bicategory.whiskerLeft,
-    Iso.trans_inv, Category.assoc]
-    repeat rw [NatTrans.comp_app]
-    simp only [mapPair_obj_left, Functor.id_obj, Functor.comp_obj, mapWhiskerLeft_app,
-      Functor.leftUnitor_inv_app, Functor.map_id, inclLeft_obj, Cat.comp_obj, Cat.of_α,
-      mapPairComp_hom_app_left, whiskerLeft_app, Functor.associator_hom_app, whiskerLeft_app,
-      Cat.comp_app, mapPairComp_inv_app_left, mapIsoWhiskerLeft_inv_app, Iso.symm_inv,
-      Functor.leftUnitor_hom_app, Category.comp_id, Category.id_comp]
-    simp )
+variable (E) in
+lemma mapWhiskerRight_associator (F : A ⥤ B) (G : B ⥤ C) (H : C ⥤ D) :
+    mapWhiskerRight (F.associator G H).hom _ =
+      (mapCompLeft E (F ⋙ G) H).hom ≫ whiskerRight (mapCompLeft E F G).hom (mapPair H (𝟭 E)) ≫
+        ((mapPair F (𝟭 E)).associator (mapPair G (𝟭 E)) (mapPair H (𝟭 E))).hom ≫
+      whiskerLeft (mapPair F (𝟭 E)) (mapCompLeft E G H).inv ≫ (mapCompLeft E F (G ⋙ H)).inv := by
+  apply natTrans_ext <;> ext <;> simp [mapCompLeft]
 
-open prelaxFunctorRight in
+end
+
 /-- The pseudofunctor sending `D` to `C ⋆ D`. -/
+@[simps!]
 def pseudofunctorRight (C : Type u₁) [Category.{v₁} C] :
     Pseudofunctor Cat.{v₂, u₂} Cat.{max v₁ v₂, max u₁ u₂} where
   toPrelaxFunctor := prelaxFunctorRight C
   mapId D := mapPairId
   mapComp := mapCompRight C
-  map₂_whisker_left := map₂_whisker_left C
-  map₂_whisker_right := map₂_whisker_right C
-  map₂_associator := map₂_associator C
+  map₂_whisker_left := mapWhiskerLeft_whiskerLeft C
+  map₂_whisker_right := mapWhiskerLeft_whiskerRight C
+  map₂_associator := mapWhiskerLeft_associator C
   map₂_left_unitor {_ _} f := by
     apply NatTrans.ext
     ext x
-    cases x <;>
-      ( repeat rw [NatTrans.comp_app]
-        simp only [prelaxFunctorRight_toPrelaxFunctorStruct_toPrefunctor_obj, Cat.of_α,
-          prelaxFunctorRight_toPrelaxFunctorStruct_toPrefunctor_map, mapPair_obj_left,
-          Functor.id_obj, leftUnitor, prelaxFunctorRight_toPrelaxFunctorStruct_map₂,
-          mapWhiskerLeft_app, Cat.comp_obj, mapCompRight, Iso.trans_hom, Cat.id_obj,
-          Cat.whiskerRight_app, mapPairId_hom_app, Functor.map_id, Functor.leftUnitor_hom_app,
-          Category.comp_id]
-        repeat rw [NatTrans.comp_app]
-        simp )
+    cases x
+    all_goals
+      dsimp [mapCompRight, mapCompLeft, leftUnitor]
+      simp only [mapWhiskerLeft_app, Functor.id_obj, Category.assoc]
+      repeat rw [NatTrans.comp_app]
+      simp
   map₂_right_unitor {_ _} g := by
     apply NatTrans.ext
     ext x
-    cases x <;>
-      ( repeat rw [NatTrans.comp_app]
-        simp only [prelaxFunctorRight_toPrelaxFunctorStruct_toPrefunctor_obj, Cat.of_α,
-          prelaxFunctorRight_toPrelaxFunctorStruct_toPrefunctor_map, mapPair_obj_left,
-          Functor.id_obj, rightUnitor, prelaxFunctorRight_toPrelaxFunctorStruct_map₂,
-          mapWhiskerLeft_app, Cat.comp_obj, mapCompRight, Iso.trans_hom, Cat.id_obj,
-          Cat.whiskerRight_app, mapPairId_hom_app, Functor.map_id, Functor.rightUnitor_hom_app,
-          Category.comp_id]
-        repeat rw [NatTrans.comp_app]
-        simp )
+    cases x
+    all_goals
+      dsimp [mapCompRight, mapCompLeft, rightUnitor]
+      simp only [mapWhiskerLeft_app, Functor.id_obj, Category.assoc]
+      repeat rw [NatTrans.comp_app]
+      simp
 
-open prelaxFunctorLeft in
 /-- The pseudofunctor sending `C` to `C ⋆ D`. -/
+@[simps!]
 def pseudofunctorLeft (D : Type u₂) [Category.{v₂} D] :
     Pseudofunctor Cat.{v₁, u₁} Cat.{max v₁ v₂, max u₁ u₂} where
   toPrelaxFunctor := prelaxFunctorLeft D
   mapId D := mapPairId
   mapComp := mapCompLeft D
-  map₂_whisker_left := map₂_whisker_left D
-  map₂_whisker_right := map₂_whisker_right D
-  map₂_associator := map₂_associator D
+  map₂_whisker_left := mapWhiskerRight_whiskerLeft D
+  map₂_whisker_right := mapWhiskerRight_whiskerRight D
+  map₂_associator := mapWhiskerRight_associator D
   map₂_left_unitor {_ _} f := by
     apply NatTrans.ext
     ext x
-    cases x <;>
-      ( repeat rw [NatTrans.comp_app]
-        simp only [prelaxFunctorLeft_toPrelaxFunctorStruct_toPrefunctor_obj, Cat.of_α,
-          prelaxFunctorLeft_toPrelaxFunctorStruct_toPrefunctor_map, mapPair_obj_left,
-          Functor.id_obj, leftUnitor, prelaxFunctorLeft_toPrelaxFunctorStruct_map₂,
-          mapWhiskerLeft_app, Cat.comp_obj, mapCompLeft, Iso.trans_hom, Cat.id_obj,
-          Cat.whiskerRight_app, mapPairId_hom_app, Functor.map_id, Functor.leftUnitor_hom_app,
-          Category.comp_id]
-        repeat rw [NatTrans.comp_app]
-        simp )
+    cases x
+    all_goals
+      dsimp [mapCompRight, mapCompLeft, leftUnitor]
+      simp only [mapWhiskerLeft_app, Functor.id_obj, Category.assoc]
+      repeat rw [NatTrans.comp_app]
+      simp
   map₂_right_unitor {_ _} g := by
     apply NatTrans.ext
     ext x
-    cases x <;>
-      ( repeat rw [NatTrans.comp_app]
-        simp only [prelaxFunctorLeft_toPrelaxFunctorStruct_toPrefunctor_obj, Cat.of_α,
-          prelaxFunctorLeft_toPrelaxFunctorStruct_toPrefunctor_map, mapPair_obj_left,
-          Functor.id_obj, rightUnitor, prelaxFunctorLeft_toPrelaxFunctorStruct_map₂,
-          mapWhiskerLeft_app, Cat.comp_obj, mapCompLeft, Iso.trans_hom, Cat.id_obj,
-          Cat.whiskerRight_app, mapPairId_hom_app, Functor.map_id, Functor.rightUnitor_hom_app,
-          Category.comp_id]
-        repeat rw [NatTrans.comp_app]
-        simp )
+    cases x
+    all_goals
+      dsimp [mapCompRight, mapCompLeft, rightUnitor]
+      simp only [mapWhiskerLeft_app, Functor.id_obj, Category.assoc]
+      repeat rw [NatTrans.comp_app]
+      simp
 
 end CategoryTheory.Join
