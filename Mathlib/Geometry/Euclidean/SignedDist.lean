17c4555c5aba88207a81166680c6db7a5ac60e36
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2025-04-21 05:23:26 +0200

    fix `norm_inner_eq_norm_tfae`

--- Mathlib/Geometry/Euclidean/SignedDist.lean
+++ Mathlib/Geometry/Euclidean/SignedDist.lean
@@ -168,7 +168,7 @@ lemma abs_signedDist_eq_dist_iff_vsub_mem_span :
   by_cases h : v = 0
   · simp [h, eq_comm (a := (0:ℝ)), eq_comm (a := (0:V))]
   rw [inv_mul_eq_iff_eq_mul₀ (by positivity)]
-  have := (@norm_inner_eq_norm_tfae ℝ _ _ _ _ v (q -ᵥ p)).out 0 2
+  have := (norm_inner_eq_norm_tfae ℝ v (q -ᵥ p)).out 0 2
   rw [Real.norm_eq_abs] at this
   rw [this]
   simp [h, eq_comm]
