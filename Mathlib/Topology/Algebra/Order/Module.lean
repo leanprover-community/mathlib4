dd669070269df4db0b4041ac54b49f351dcb5345
Yaël Dillies <yael.dillies@gmail.com>
2025-04-22 17:29:50 +0000

    `ContinuousSMul` instance

--- Mathlib/Topology/Algebra/Order/Module.lean
+++ Mathlib/Topology/Algebra/Order/Module.lean
@@ -5,13 +5,16 @@ Authors: Yaël Dillies
 -/
 import Mathlib.Algebra.Order.Nonneg.Module
 import Mathlib.Topology.Algebra.ConstMulAction
+import Mathlib.Topology.Algebra.MulAction
 
 /-!
 # Continuous nonnegative scalar multiplication
 -/
 
 variable {R α : Type*} [Semiring R] [PartialOrder R] [SMul R α] [TopologicalSpace α]
-  [ContinuousConstSMul R α]
 
-instance : ContinuousConstSMul {r : R // 0 ≤ r} α where
+instance [ContinuousConstSMul R α] : ContinuousConstSMul {r : R // 0 ≤ r} α where
   continuous_const_smul r := continuous_const_smul r.1
+
+instance [TopologicalSpace R] [ContinuousSMul R α] : ContinuousSMul {r : R // 0 ≤ r} α where
+  continuous_smul := continuous_smul (M := R).comp <| continuous_subtype_val.prodMap continuous_id
