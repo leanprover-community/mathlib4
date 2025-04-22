ccd25bfa713f6219f0214faa3cdd55acca27ca7a
Bernhard Reinke <b-reinke@users.noreply.github.com>
2025-04-22 11:06:16 +0200

    rename Red.reduced to IsReduced

--- Mathlib/GroupTheory/FreeGroup/ReducedWords.lean
+++ Mathlib/GroupTheory/FreeGroup/ReducedWords.lean
@@ -11,7 +11,7 @@ This file defines some extra lemmas for free groups, in particular about (cyclic
 
 ## Main declarations
 
-* `FreeGroup.Red.reduced`: the predicate for reduced words
+* `FreeGroup.IsReduced`: the predicate for reduced words
 
 -/
 open List
@@ -23,40 +23,38 @@ namespace FreeGroup
 
 variable {L L₁ L₂ : List (α × Bool)}
 
-namespace Red
-
 /-- Predicate asserting that the word `L` admits no reduction steps, i.e., no two neighboring
 elements of the word cancel. -/
 @[to_additive "Predicate asserting the word `L` admits no reduction steps, i.e., no two neighboring
 elements of the word cancel."]
-def reduced (L : List (α × Bool)) : Prop := List.Chain' (fun a b => ¬(a.1 = b.1 ∧ (!a.2) = b.2)) L
+def IsReduced (L : List (α × Bool)) : Prop := List.Chain' (fun a b => ¬(a.1 = b.1 ∧ (!a.2) = b.2)) L
 
 @[to_additive (attr := simp)]
-theorem reduced_nil : reduced ([] : List (α × Bool)) := List.chain'_nil
+theorem isReduced_nil : IsReduced ([] : List (α × Bool)) := List.chain'_nil
 
 @[to_additive (attr := simp)]
-theorem reduced_singleton {a : (α × Bool)} : reduced [a] := List.chain'_singleton a
+theorem isReduced_singleton {a : (α × Bool)} : IsReduced [a] := List.chain'_singleton a
 
 @[to_additive (attr := simp)]
-theorem reduced_cons {a b: (α × Bool)} :
-    reduced (a :: b :: L) ↔ ¬(a.1 = b.1 ∧ (!a.2) = b.2) ∧ reduced (b :: L) :=
+theorem isReduced_cons {a b: (α × Bool)} :
+    IsReduced (a :: b :: L) ↔ ¬(a.1 = b.1 ∧ (!a.2) = b.2) ∧ IsReduced (b :: L) :=
   List.chain'_cons
 
 @[to_additive]
-theorem not_step_reduced : reduced L₁ → ¬ Step L₁ L₂
-| red, step => by induction step; simp [reduced] at red
+theorem IsReduced.not_step : IsReduced L₁ → ¬ Red.Step L₁ L₂
+| red, step => by induction step; simp [IsReduced] at red
 
 @[to_additive]
-theorem not_step_reduced_iff : reduced L₁ ↔ ∀ L₂, ¬ Step L₁ L₂ where
-  mp h _ := not_step_reduced h
+theorem isReduced_not_step_iff : IsReduced L₁ ↔ ∀ L₂, ¬ Red.Step L₁ L₂ where
+  mp h _ := h.not_step
   mpr hL := by
     induction L₁ with
-    | nil => exact reduced_nil
+    | nil => exact isReduced_nil
     | cons x l ih =>
       cases l with
-      | nil => exact reduced_singleton
+      | nil => exact isReduced_singleton
       | cons y l' =>
-        rw [reduced_cons]
+        rw [isReduced_cons]
         constructor
         · intro ⟨eq1, eq2⟩
           obtain ⟨x1, x2⟩ := x
@@ -64,48 +62,48 @@ theorem not_step_reduced_iff : reduced L₁ ↔ ∀ L₂, ¬ Step L₁ L₂ wher
           simp only at eq1 eq2
           apply hL l'
           rw [eq1, ← eq2]
-          apply Step.cons_not
+          apply Red.Step.cons_not
         · apply ih
           intro L₂ step
           apply hL (x :: L₂)
-          exact Step.cons step
+          exact Red.Step.cons step
 
 @[to_additive]
-theorem reduced_infix : reduced L₂ → L₁ <:+: L₂ → reduced L₁ := Chain'.infix
+theorem IsReduced.infix : IsReduced L₂ → L₁ <:+: L₂ → IsReduced L₁ := Chain'.infix
 
 @[to_additive]
-theorem reduced_min (h : reduced L₁) : Red L₁ L₂ ↔ L₂ = L₁ :=
-  Relation.reflTransGen_iff_eq fun _ => not_step_reduced h
+theorem IsReduced.min (h : IsReduced L₁) : Red L₁ L₂ ↔ L₂ = L₁ :=
+  Relation.reflTransGen_iff_eq fun _ => h.not_step
 
 @[to_additive]
-theorem reduced_cons_append_chain {x : α × Bool} {L₁ L₂ : List (α × Bool)} (h : L₁ ≠ []) :
-    reduced (x :: L₁) → reduced (L₁ ++ L₂) → reduced (x :: L₁ ++ L₂) := by
+theorem IsReduced.cons_append_chain {x : α × Bool} {L₁ L₂ : List (α × Bool)} (h : L₁ ≠ []) :
+    IsReduced (x :: L₁) → IsReduced (L₁ ++ L₂) → IsReduced (x :: L₁ ++ L₂) := by
   intro h1 h2
   induction L₁
   · contradiction
-  · apply reduced_cons.mp at h1
-    apply reduced_cons.mpr
+  · apply isReduced_cons.mp at h1
+    apply isReduced_cons.mpr
     tauto
 
 @[to_additive]
-theorem reduced_append_chain {L₁ L₂ L₃ : List (α × Bool)} (h : L₂ ≠ []) :
-    reduced (L₁ ++ L₂) → reduced (L₂ ++ L₃) → reduced (L₁ ++ L₂ ++ L₃) := by
+theorem IsReduced.append_chain {L₁ L₂ L₃ : List (α × Bool)} (h : L₂ ≠ []) :
+    IsReduced (L₁ ++ L₂) → IsReduced (L₂ ++ L₃) → IsReduced (L₁ ++ L₂ ++ L₃) := by
   intro h1 h2
   induction L₁
   case nil => simp [h2]
   case cons head tail ih =>
-    refine reduced_cons_append_chain (by simp [h]) h1 (ih (reduced_infix h1 ⟨[head], [], by simp⟩))
+    refine h1.cons_append_chain (by simp [h]) (ih (h1.infix ⟨[head], [], by simp⟩))
 
 variable [DecidableEq α]
 
 @[to_additive]
-theorem reduced_iff_eq_reduce : reduced L ↔ reduce L = L := by
+theorem isReduced_iff_reduce_eq : IsReduced L ↔ reduce L = L := by
   constructor
   · intro h
-    rw [← reduced_min h]
+    rw [← h.min]
     exact reduce.red
   · intro h
-    unfold reduced
+    unfold IsReduced
     rw [List.chain'_iff_forall_rel_of_append_cons_cons]
     intro ⟨x₁, x₂⟩ ⟨y₁, y₂⟩ l₁ l₂ hl hx
     simp only at hl hx
@@ -114,10 +112,8 @@ theorem reduced_iff_eq_reduce : reduced L ↔ reduce L = L := by
     apply reduce.not h
 
 @[to_additive]
-theorem reduced_toWord {x : FreeGroup α} : Red.reduced (x.toWord) := by
-  rw [Red.reduced_iff_eq_reduce]
+theorem isReduced_toWord {x : FreeGroup α} : IsReduced (x.toWord) := by
+  rw [isReduced_iff_reduce_eq]
   simp
-
-end Red
 
 end FreeGroup
