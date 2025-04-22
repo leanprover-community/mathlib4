5323f11599bd612e5cf79ed6649f3d9ebea3d689
Robin Carlier <57142648+robin-carlier@users.noreply.github.com>
2025-04-22 17:41:22 +0200

    implement suggestions

--- Mathlib/CategoryTheory/Join/Pseudofunctor.lean
+++ Mathlib/CategoryTheory/Join/Pseudofunctor.lean
@@ -21,34 +21,6 @@ namespace CategoryTheory.Join
 
 open Bicategory
 
-/-- The `PrelaxFunctor` structure underlying `Join.pseudofunctorRight`. -/
-@[simps]
-def prelaxFunctorRight (C : Type u₁) [Category.{v₁} C] :
-    PrelaxFunctor Cat.{v₂, u₂} Cat.{max v₁ v₂, max u₁ u₂} where
-  obj D := Cat.of (C ⋆ D)
-  map F := mapPair (𝟭 C) F
-  map₂ := mapWhiskerLeft _
-  map₂_id {x y} f := by
-    apply natTrans_ext <;> aesop_cat
-  map₂_comp η θ := by
-    apply natTrans_ext <;>
-    ( dsimp
-      rw [← mapWhiskerLeft_comp] )
-
-/-- The `PrelaxFunctor` structure underlying `Join.pseudofunctorLeft`. -/
-@[simps]
-def prelaxFunctorLeft (D : Type u₂) [Category.{v₂} D] :
-    PrelaxFunctor Cat.{v₁, u₁} Cat.{max v₁ v₂, max u₁ u₂} where
-  obj C := Cat.of (C ⋆ D)
-  map F := mapPair F (𝟭 D)
-  map₂ := (mapWhiskerRight · _)
-  map₂_id {x y} f := by
-    apply natTrans_ext <;> aesop_cat
-  map₂_comp η θ := by
-    apply natTrans_ext <;>
-    ( dsimp
-      rw [← mapWhiskerRight_comp] )
-
 -- The proof gets too slow if we put it in a single `pseudofunctor` constructor,
 -- so we break down the component proofs for the pseudofunctors over several lemmas.
 
@@ -72,7 +44,7 @@ variable (A) in
 @[reassoc]
 lemma mapWhiskerLeft_whiskerLeft (F : B ⥤ C) {G H : C ⥤ D} (η : G ⟶ H) :
     mapWhiskerLeft _ (whiskerLeft F η) =
-      (mapCompRight A F G).hom ≫ whiskerLeft (mapPair (𝟭 A) F) (mapWhiskerLeft _ η) ≫
+    (mapCompRight A F G).hom ≫ whiskerLeft (mapPair (𝟭 A) F) (mapWhiskerLeft _ η) ≫
       (mapCompRight A F H).inv := by
   apply natTrans_ext <;> ext <;> simp [mapCompRight]
 
@@ -80,7 +52,7 @@ variable (D) in
 @[reassoc]
 lemma mapWhiskerRight_whiskerLeft (F : A ⥤ B) {G H : B ⥤ C} (η : G ⟶ H) :
     mapWhiskerRight (whiskerLeft F η) (𝟭 D) =
-      (mapCompLeft D F G).hom ≫ whiskerLeft (mapPair F (𝟭 D)) (mapWhiskerRight η _) ≫
+    (mapCompLeft D F G).hom ≫ whiskerLeft (mapPair F (𝟭 D)) (mapWhiskerRight η _) ≫
       (mapCompLeft D F H).inv := by
   apply natTrans_ext <;> ext <;> simp [mapCompLeft]
 
@@ -88,7 +60,7 @@ variable (A) in
 @[reassoc]
 lemma mapWhiskerLeft_whiskerRight {F G: B ⥤ C} (η : F ⟶ G) (H : C ⥤ D) :
     mapWhiskerLeft _ (whiskerRight η H) =
-      (mapCompRight A F H).hom ≫ whiskerRight (mapWhiskerLeft _ η) (mapPair (𝟭 A) H) ≫
+    (mapCompRight A F H).hom ≫ whiskerRight (mapWhiskerLeft _ η) (mapPair (𝟭 A) H) ≫
       (mapCompRight A G H).inv := by
   apply natTrans_ext <;> ext <;> simp [mapCompRight]
 
@@ -96,7 +68,7 @@ variable (D) in
 @[reassoc]
 lemma mapWhiskerRight_whiskerRight {F G: A ⥤ B} (η : F ⟶ G) (H : B ⥤ C) :
     mapWhiskerRight (whiskerRight η H) _ =
-      (mapCompLeft D F H).hom ≫ whiskerRight (mapWhiskerRight η _) (mapPair H (𝟭 D)) ≫
+    (mapCompLeft D F H).hom ≫ whiskerRight (mapWhiskerRight η _) (mapPair H (𝟭 D)) ≫
       (mapCompLeft D G H).inv := by
   apply natTrans_ext <;> ext <;> simp [mapCompLeft]
 
@@ -106,17 +78,45 @@ variable (A) in
 @[reassoc]
 lemma mapWhiskerLeft_associator (F : B ⥤ C) (G : C ⥤ D) (H : D ⥤ E) :
     mapWhiskerLeft _ (F.associator G H).hom =
-      (mapCompRight A (F ⋙ G) H).hom ≫ whiskerRight (mapCompRight A F G).hom (mapPair (𝟭 A) H) ≫
-        ((mapPair (𝟭 A) F).associator (mapPair (𝟭 A) G) (mapPair (𝟭 A) H)).hom ≫
-      whiskerLeft (mapPair (𝟭 A) F) (mapCompRight A G H).inv ≫ (mapCompRight A F (G ⋙ H)).inv := by
+    (mapCompRight A (F ⋙ G) H).hom ≫ whiskerRight (mapCompRight A F G).hom (mapPair (𝟭 A) H) ≫
+      ((mapPair (𝟭 A) F).associator (mapPair (𝟭 A) G) (mapPair (𝟭 A) H)).hom ≫
+    whiskerLeft (mapPair (𝟭 A) F) (mapCompRight A G H).inv ≫ (mapCompRight A F (G ⋙ H)).inv := by
   apply natTrans_ext <;> ext <;> simp [mapCompRight]
 
 variable (E) in
 lemma mapWhiskerRight_associator (F : A ⥤ B) (G : B ⥤ C) (H : C ⥤ D) :
     mapWhiskerRight (F.associator G H).hom _ =
-      (mapCompLeft E (F ⋙ G) H).hom ≫ whiskerRight (mapCompLeft E F G).hom (mapPair H (𝟭 E)) ≫
-        ((mapPair F (𝟭 E)).associator (mapPair G (𝟭 E)) (mapPair H (𝟭 E))).hom ≫
-      whiskerLeft (mapPair F (𝟭 E)) (mapCompLeft E G H).inv ≫ (mapCompLeft E F (G ⋙ H)).inv := by
+    (mapCompLeft E (F ⋙ G) H).hom ≫ whiskerRight (mapCompLeft E F G).hom (mapPair H (𝟭 E)) ≫
+      ((mapPair F (𝟭 E)).associator (mapPair G (𝟭 E)) (mapPair H (𝟭 E))).hom ≫
+    whiskerLeft (mapPair F (𝟭 E)) (mapCompLeft E G H).inv ≫ (mapCompLeft E F (G ⋙ H)).inv := by
+  apply natTrans_ext <;> ext <;> simp [mapCompLeft]
+
+variable (A) in
+lemma mapWhiskerLeft_left_unitor (F : B ⥤ C) :
+    mapWhiskerLeft _ F.leftUnitor.hom =
+    (mapCompRight A (𝟭 _) F).hom ≫ whiskerRight mapPairId.hom (mapPair _ F) ≫
+      (mapPair _ F).leftUnitor.hom := by
+  apply natTrans_ext <;> ext <;> simp [mapCompRight]
+
+variable (C) in
+lemma mapWhiskerRight_left_unitor (F : A ⥤ B) :
+    mapWhiskerRight F.leftUnitor.hom (𝟭 C) =
+    (mapCompLeft C (𝟭 A) F).hom ≫ whiskerRight mapPairId.hom (mapPair F (𝟭 C)) ≫
+      (mapPair F (𝟭 C)).leftUnitor.hom := by
+  apply natTrans_ext <;> ext <;> simp [mapCompLeft]
+
+variable (A) in
+lemma mapWhiskerLeft_right_unitor (F : B ⥤ C) :
+    mapWhiskerLeft _ F.rightUnitor.hom =
+    (mapCompRight A F (𝟭 C)).hom ≫ whiskerLeft (mapPair _ F) mapPairId.hom ≫
+      (mapPair (𝟭 A) _).rightUnitor.hom := by
+  apply natTrans_ext <;> ext <;> simp [mapCompRight]
+
+variable (C) in
+lemma mapWhiskerRight_right_unitor (F : A ⥤ B) :
+    mapWhiskerRight F.rightUnitor.hom _ =
+    (mapCompLeft C F (𝟭 B)).hom ≫ whiskerLeft (mapPair F _) mapPairId.hom ≫
+      (mapPair _ (𝟭 C)).rightUnitor.hom := by
   apply natTrans_ext <;> ext <;> simp [mapCompLeft]
 
 end
@@ -125,58 +125,34 @@ end
 @[simps!]
 def pseudofunctorRight (C : Type u₁) [Category.{v₁} C] :
     Pseudofunctor Cat.{v₂, u₂} Cat.{max v₁ v₂, max u₁ u₂} where
-  toPrelaxFunctor := prelaxFunctorRight C
+  obj D := Cat.of (C ⋆ D)
+  map F := mapPair (𝟭 C) F
+  map₂ := mapWhiskerLeft _
+  map₂_id {x y} f := by apply mapWhiskerLeft_id
+  map₂_comp η θ := by apply mapWhiskerLeft_comp
   mapId D := mapPairId
   mapComp := mapCompRight C
   map₂_whisker_left := mapWhiskerLeft_whiskerLeft C
   map₂_whisker_right := mapWhiskerLeft_whiskerRight C
   map₂_associator := mapWhiskerLeft_associator C
-  map₂_left_unitor {_ _} f := by
-    apply NatTrans.ext
-    ext x
-    cases x
-    all_goals
-      dsimp [mapCompRight, leftUnitor]
-      simp only [mapWhiskerLeft_app, Functor.id_obj, Category.assoc]
-      repeat rw [NatTrans.comp_app]
-      simp
-  map₂_right_unitor {_ _} g := by
-    apply NatTrans.ext
-    ext x
-    cases x
-    all_goals
-      dsimp [mapCompRight, rightUnitor]
-      simp only [mapWhiskerLeft_app, Functor.id_obj, Category.assoc]
-      repeat rw [NatTrans.comp_app]
-      simp
+  map₂_left_unitor := mapWhiskerLeft_left_unitor C
+  map₂_right_unitor := mapWhiskerLeft_right_unitor C
 
 /-- The pseudofunctor sending `C` to `C ⋆ D`. -/
 @[simps!]
 def pseudofunctorLeft (D : Type u₂) [Category.{v₂} D] :
     Pseudofunctor Cat.{v₁, u₁} Cat.{max v₁ v₂, max u₁ u₂} where
-  toPrelaxFunctor := prelaxFunctorLeft D
+  obj C := Cat.of (C ⋆ D)
+  map F := mapPair F (𝟭 D)
+  map₂ := (mapWhiskerRight · _)
+  map₂_id {x y} f := by apply mapWhiskerRight_id
+  map₂_comp η θ := by apply mapWhiskerRight_comp
   mapId D := mapPairId
   mapComp := mapCompLeft D
   map₂_whisker_left := mapWhiskerRight_whiskerLeft D
   map₂_whisker_right := mapWhiskerRight_whiskerRight D
   map₂_associator := mapWhiskerRight_associator D
-  map₂_left_unitor {_ _} f := by
-    apply NatTrans.ext
-    ext x
-    cases x
-    all_goals
-      dsimp [mapCompLeft, leftUnitor]
-      simp only [mapWhiskerLeft_app, Functor.id_obj, Category.assoc]
-      repeat rw [NatTrans.comp_app]
-      simp
-  map₂_right_unitor {_ _} g := by
-    apply NatTrans.ext
-    ext x
-    cases x
-    all_goals
-      dsimp [mapCompLeft, rightUnitor]
-      simp only [mapWhiskerLeft_app, Functor.id_obj, Category.assoc]
-      repeat rw [NatTrans.comp_app]
-      simp
+  map₂_left_unitor := mapWhiskerRight_left_unitor D
+  map₂_right_unitor := mapWhiskerRight_right_unitor D
 
 end CategoryTheory.Join
