1988522a3404ea796d7956cba01d56298c5c8d24
Yaël Dillies <yael.dillies@gmail.com>
2025-04-23 16:29:53 +0000

    shake

--- Mathlib/Order/Bounds/Image.lean
+++ Mathlib/Order/Bounds/Image.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl, Yury Kudryashov, Paul Lezeau
 -/
 import Mathlib.Data.Set.NAry
-import Mathlib.Order.Bounds.Basic
+import Mathlib.Order.Bounds.Defs
 
 /-!
 
