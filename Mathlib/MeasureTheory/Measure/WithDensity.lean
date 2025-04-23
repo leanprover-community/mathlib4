6628a177aef50db3ea60d4e650a6dab747b95395
DavidLedvinka <50644608+DavidLedvinka@users.noreply.github.com>
2025-04-23 07:18:11 -0400

    Update Mathlib/MeasureTheory/Measure/WithDensity.lean
    Co-authored-by: Thomas Zhu <29544653+hanwenzhu@users.noreply.github.com>

--- Mathlib/MeasureTheory/Measure/WithDensity.lean
+++ Mathlib/MeasureTheory/Measure/WithDensity.lean
@@ -667,7 +667,7 @@ theorem prod_withDensity₀ {f : α → ℝ≥0∞} {g : β → ℝ≥0∞}
     (μ.withDensity f).prod (ν.withDensity g) = (μ.prod ν).withDensity (fun z ↦ f z.1 * g z.2) := by
   rw [prod_withDensity_left₀ hf, prod_withDensity_right₀ hg, ← withDensity_mul₀, mul_comm]
   · rfl
-  all_goals fun_prop (disch:= intro _ hs; simp [hs])
+  all_goals fun_prop (disch := intro _ hs; simp [hs])
 
 theorem prod_withDensity {f : α → ℝ≥0∞} {g : β → ℝ≥0∞} (hf : Measurable f) (hg : Measurable g) :
     (μ.withDensity f).prod (ν.withDensity g) = (μ.prod ν).withDensity (fun z ↦ f z.1 * g z.2) :=
