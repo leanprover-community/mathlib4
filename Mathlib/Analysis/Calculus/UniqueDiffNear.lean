bc631097156bee25bf873a165e82c955e80d986d
Yury G. Kudryashov <urkud@urkud.name>
2025-04-23 22:06:20 -0500

    Update

--- Mathlib/Analysis/Calculus/UniqueDiffNear.lean
+++ Mathlib/Analysis/Calculus/UniqueDiffNear.lean
@@ -27,3 +27,27 @@ theorem UniqueDiffNear.eventually (h : UniqueDiffNear 𝕜 s x) :
 theorem UniqueDiffNear.of_uniqueDiffWithinAt_of_eventually (h₁ : UniqueDiffWithinAt 𝕜 s x)
     (h₂ : ∀ᶠ y in 𝓝[s] x, UniqueDiffWithinAt 𝕜 s y) : UniqueDiffNear 𝕜 s x :=
     iff_uniqueDiffWithinAt_and_eventually.mpr ⟨h₁, h₂⟩
+
+theorem UniqueDiffOn.uniqueDiffNear (h : UniqueDiffOn 𝕜 s) (hx : x ∈ s) :
+    UniqueDiffNear 𝕜 s x :=
+  .of_uniqueDiffWithinAt_of_eventually (h _ hx) <| eventually_mem_nhdsWithin.mono h
+
+theorem eventually_insert_uniqueDiffNear :
+    (∀ᶠ y in 𝓝[insert x s] x, UniqueDiffNear 𝕜 s y) ↔ UniqueDiffNear 𝕜 s x := by
+  simp [uniqueDiffNear_iff_eventually_insert]
+
+alias ⟨_, UniqueDiffNear.eventually_insert'⟩ := eventually_insert_uniqueDiffNear
+
+theorem UniqueDiffNear.eventually' (h : UniqueDiffNear 𝕜 s x) :
+    ∀ᶠ y in 𝓝[s] x, UniqueDiffNear 𝕜 s y :=
+  h.eventually_insert'.filter_mono <| by gcongr; apply subset_insert
+
+theorem UniqueDiffNear.exists_uniqueDiffOn_subset (h : UniqueDiffNear 𝕜 s x) (ht : t ∈ 𝓝[s] x) :
+    ∃ u ∈ 𝓝[s] x, UniqueDiffOn 𝕜 (insert x u) ∧ u ⊆ t := by
+  rcases mem_nhdsWithin.mp (inter_mem h.eventually ht) with ⟨U, hUo, hxU, hU⟩
+  rw [subset_inter_iff] at hU
+  refine ⟨U ∩ s, mem_nhdsWithin.mpr ⟨U, hUo, hxU, Subset.rfl⟩, ?_, hU.2⟩
+  rintro y (rfl | hy)
+  · have := h.uniqueDiffWithinAt.inter (hUo.mem_nhds hxU)
+  
+    
