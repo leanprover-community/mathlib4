f41b7f7ff6732e57fa5daf0a8eed3ec9520889af
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2025-04-25 01:28:52 +0200

    add missing import

--- Mathlib/Data/Nat/Basic.lean
+++ Mathlib/Data/Nat/Basic.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Floris van Doorn, Leonardo de Moura, Jeremy Avigad, Mario Carneiro
 -/
 import Mathlib.Data.Nat.Init
+import Mathlib.Data.Set.Defs
 import Mathlib.Logic.Nontrivial.Defs
 import Mathlib.Order.Defs.LinearOrder
 import Mathlib.Tactic.Contrapose
