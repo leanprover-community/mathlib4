be9a99514a9685d23155f7e72507d1912e4d6e11
Bolton Bailey <bolton.bailey@gmail.com>
2025-04-23 18:42:41 -0700

    convert into a proof-wanted

--- Mathlib/Computability/TMComputable.lean
+++ Mathlib/Computability/TMComputable.lean
@@ -97,28 +97,6 @@ instance inhabitedCfg : Inhabited (Cfg tm) :=
 def step : tm.Cfg → Option tm.Cfg :=
   Turing.TM2.step tm.m
 
-/--
-A (TM2) Multi-tape Turing Machine composing the operations of two other Multi-tape Turing Machines.
-This machine should work by first carrying out the oppertions of the first machine on the subset of
-tapes associated with the first machine, then copying the output tape of the first machine to the
-input tape of the second machine, then running the second machine.
--/
-def comp (m1 m2 : FinTM2) : FinTM2 where
-  K := m1.K ⊕ m2.K
-  kDecidableEq := sorry
-  kFin := sorry
-  k₀ := Sum.inl m1.k₀
-  k₁ := Sum.inr m2.k₁
-  Γ := Sum.elim m1.Γ m2.Γ
-  Λ := Unit ⊕ m1.Λ ⊕ m2.Λ
-  main := Sum.inl ()
-  ΛFin := sorry
-  σ := m1.σ ⊕ m2.σ
-  initialState := Sum.inl m1.initialState
-  σFin := sorry
-  Γk₀Fin := sorry
-  m := sorry
-
 end
 
 end FinTM2
@@ -321,10 +299,19 @@ instance inhabitedTM2Computable :
 instance inhabitedTM2ComputableAux : Inhabited (TM2ComputableAux Bool Bool) :=
   ⟨(default : TM2Computable finEncodingBoolBool finEncodingBoolBool id).toTM2ComputableAux⟩
 
-def TM2ComputableInPolyTime.comp {α β γ : Type} {eα : FinEncoding α} {eβ : FinEncoding β}
+/--
+For any two polynomial time Multi-tape Turing Machines,
+There exists another polynomial time Multi-tape Turing Machine that composes their operations.
+This machine can work by simply having one tape for each tape in both of the composed TMs.
+It first carries out the operations of the first TM on the tapes associated with the first TM,
+then copies the output tape of the first TM to the input tape of the second TM,
+then runs the second TM.
+-/
+proof_wanted TM2ComputableInPolyTime.comp
+    {α β γ : Type} {eα : FinEncoding α} {eβ : FinEncoding β}
     {eγ : FinEncoding γ} {f : α → β} {g : β → γ} (h1 : TM2ComputableInPolyTime eα eβ f)
     (h2 : TM2ComputableInPolyTime eβ eγ g) :
-    TM2ComputableInPolyTime eα eγ (g ∘ f) := sorry
+  ∃ t : TM2ComputableInPolyTime eα eγ (g ∘ f), true
 
 
 end
