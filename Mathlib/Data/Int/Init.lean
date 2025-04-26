430834e196c27682a2085714363c4daad85315a8
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2025-04-26 13:13:21 +0200

    Add Int.natAbs_sign_of_ne_zero

--- Mathlib/Data/Int/Init.lean
+++ Mathlib/Data/Int/Init.lean
@@ -28,6 +28,7 @@ namespace Int
 -- Pending rename in core.
 alias add_mul_emod_self_right := add_mul_emod_self
 alias natAbs_natCast := natAbs_ofNat
+alias natAbs_sign_of_ne_zero := natAbs_sign_of_nonzero
 
 variable {a b c d m n : ℤ}
 
