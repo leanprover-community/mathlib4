917a12bf6e194e27cbb4b40830ba908f072a4678
Andrew Yang <36414270+erdOne@users.noreply.github.com>
2025-04-26 16:13:58 +0100

    Update Domain.lean

--- Mathlib/RingTheory/Flat/Domain.lean
+++ Mathlib/RingTheory/Flat/Domain.lean
@@ -7,20 +7,19 @@ import Mathlib.LinearAlgebra.Basis.VectorSpace
 import Mathlib.RingTheory.Flat.Localization
 
 /-!
-
 # Flat modules in domains
 
 We show that the tensor product of two injective linear maps is injective if the sources are flat
 and the ring is an integral domain.
-
 -/
+
 universe u
 
 variable {R M N : Type*} [CommRing R] [AddCommGroup M] [Module R M]
 variable [AddCommGroup N] [Module R N]
 variable {P Q : Type*} [AddCommGroup P] [Module R P] [AddCommGroup Q] [Module R Q]
 
-open TensorProduct
+open TensorProduct Function
 
 attribute [local instance 1100] Module.Free.of_divisionRing Module.Flat.of_free in
 /-- Tensor product of injective maps over domains are injective under some flatness conditions.
@@ -28,8 +27,7 @@ Also see `TensorProduct.map_injective_of_flat_flat`
 for different flatness conditions but without the domain assumption. -/
 lemma TensorProduct.map_injective_of_flat_flat_of_isDomain [IsDomain R]
     (f : P →ₗ[R] M) (g : Q →ₗ[R] N) [H : Module.Flat R P] [Module.Flat R Q]
-    (hf : Function.Injective f) (hg : Function.Injective g) :
-    Function.Injective (TensorProduct.map f g) := by
+    (hf : Injective f) (hg : Injective g) : Injective (TensorProduct.map f g) := by
   let K := FractionRing R
   refine .of_comp (f := TensorProduct.mk R K _ 1) ?_
   have H₁ := TensorProduct.map_injective_of_flat_flat (f.baseChange K) (g.baseChange K)
@@ -54,8 +52,8 @@ lemma TensorProduct.map_injective_of_flat_flat_of_isDomain [IsDomain R]
 
 /-- Tensor product of linearly independent families is linearly independent over domains.
 This is true over non-domains if one of the modules is flat.
-See `LinearIndependent.tensorProduct_of_flat_left`. -/
-lemma LinearIndependent.tensorProduct_of_isDomain [IsDomain R]
+See `LinearIndependent.tmul_of_flat_left`. -/
+lemma LinearIndependent.tmul_of_isDomain [IsDomain R]
     {ι ι' : Type*} {v : ι → M} (hv : LinearIndependent R v)
     {w : ι' → N} (hw : LinearIndependent R w) :
     LinearIndependent R fun i : ι × ι' ↦ v i.1 ⊗ₜ[R] w i.2 := by
