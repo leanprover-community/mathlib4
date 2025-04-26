b692f2473514d8a56416c1868dfe33a21dc25c59
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2025-04-27 00:16:59 +0200

    .

--- Mathlib/Analysis/SpecialFunctions/Pow/Continuity.lean
+++ Mathlib/Analysis/SpecialFunctions/Pow/Continuity.lean
@@ -191,7 +191,7 @@ theorem continuousAt_rpow_of_ne (p : ℝ × ℝ) (hp : p.1 ≠ 0) :
     rw [continuousAt_congr (rpow_eq_nhds_of_pos hp)]
     refine continuous_exp.continuousAt.comp (ContinuousAt.mul ?_ continuous_snd.continuousAt)
     refine (continuousAt_log ?_).comp continuous_fst.continuousAt
-    exact hp.lt.ne.symm
+    exact hp.ne'
 
 theorem continuousAt_rpow_of_pos (p : ℝ × ℝ) (hp : 0 < p.2) :
     ContinuousAt (fun p : ℝ × ℝ => p.1 ^ p.2) p := by
