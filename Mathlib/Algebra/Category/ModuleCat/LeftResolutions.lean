aa8a4776e745d0dbbc79b813cd3d95f2967c1a84
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-04-27 21:14:49 +0200

    fix

--- Mathlib/Algebra/Category/ModuleCat/LeftResolutions.lean
+++ Mathlib/Algebra/Category/ModuleCat/LeftResolutions.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joël Riou
 -/
 import Mathlib.Algebra.Category.ModuleCat.Adjunctions
-import Mathlib.Algebra.Category.ModuleCat.Free
+import Mathlib.Algebra.Category.ModuleCat.EpiMono
 import Mathlib.Algebra.Homology.LeftResolutions.Basic
 
 /-!
