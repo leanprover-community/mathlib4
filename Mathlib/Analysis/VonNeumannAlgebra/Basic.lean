51d31100dec1e9f3ee1c97ff30eac1839dd18c7a
Moritz Doll <moritz.doll@googlemail.com>
2025-04-28 09:21:00 +1000

    shake

--- Mathlib/Analysis/VonNeumannAlgebra/Basic.lean
+++ Mathlib/Analysis/VonNeumannAlgebra/Basic.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kim Morrison
 -/
 import Mathlib.Analysis.CStarAlgebra.ContinuousLinearMap
-import Mathlib.Analysis.Normed.Module.Dual
 
 /-!
 # Von Neumann algebras
