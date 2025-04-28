19205e2e4465d75ac914f09e440fb395d4b2aecc
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2025-04-28 22:28:55 +0200

    shake

--- Mathlib/Algebra/Ring/GrindInstances.lean
+++ Mathlib/Algebra/Ring/GrindInstances.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kim Morrison
 -/
 import Mathlib.Algebra.Ring.Defs
-import Mathlib.Data.Int.Cast.Lemmas
+import Mathlib.Data.Int.Cast.Basic
 
 /-!
 # Instances for `grind`.
