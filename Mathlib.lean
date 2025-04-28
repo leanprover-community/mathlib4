41e6352362cf90aee27b124a3a90f6473b95fcb0
mariainesdff <mariaines.dff@gmail.com>
2025-04-28 14:48:26 +0200

    rename file

--- Mathlib.lean
+++ Mathlib.lean
@@ -1663,7 +1663,7 @@ import Mathlib.Analysis.Normed.Ring.Lemmas
 import Mathlib.Analysis.Normed.Ring.Ultra
 import Mathlib.Analysis.Normed.Ring.Units
 import Mathlib.Analysis.Normed.Ring.WithAbs
-import Mathlib.Analysis.Normed.Unbundled.AlgNormOfGalois
+import Mathlib.Analysis.Normed.Unbundled.AlgNormOfAut
 import Mathlib.Analysis.Normed.Unbundled.AlgebraNorm
 import Mathlib.Analysis.Normed.Unbundled.FiniteExtension
 import Mathlib.Analysis.Normed.Unbundled.IsPowMulFaithful
