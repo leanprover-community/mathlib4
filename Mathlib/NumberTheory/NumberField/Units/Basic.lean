0df109ca1f3866c959c9f2e18ef50eb21e772b70
Xavier Roblot <roblot@math.univ-lyon1.fr>
2025-04-28 10:01:55 +0200

    change to def + add instance

--- Mathlib/NumberTheory/NumberField/Units/Basic.lean
+++ Mathlib/NumberTheory/NumberField/Units/Basic.lean
@@ -136,7 +136,11 @@ instance : Nonempty (torsion K) := One.instNonempty
 instance [NumberField K] : IsCyclic (torsion K) := subgroup_units_cyclic _
 
 /-- The order of the torsion subgroup. -/
-abbrev torsionOrder [NumberField K] : ℕ := Fintype.card (torsion K)
+def torsionOrder [NumberField K] : ℕ := Fintype.card (torsion K)
+
+instance [NumberField K] : NeZero (torsionOrder K) := by
+  unfold torsionOrder
+  infer_instance
 
 theorem torsionOrder_pos [NumberField K] :
     0 < torsionOrder K := Nat.pos_of_neZero (torsionOrder K)
