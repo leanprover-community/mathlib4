c7cf6b6ebcea2efcee0ee9c477096814aae44e5c
David Loeffler <d.loeffler.01@cantab.net>
2025-04-28 07:37:29 +0000

    refactor(Topology/UniformSpace): clarify def of TendstoLocallyUniformly (#24342)
    Split `Mathlib.Topology.UniformSpace.UniformConvergence` into 3 files, and add a detailed explanation of how our
    definition of `TendstoLocallyUniformly` differs from the one usually found in the literature.
    
    See discussion on Zulip: https://leanprover.zulipchat.com/#narrow/channel/116395-maths/topic/Locally.20uniform.20convergence/with/514097469

--- Mathlib.lean
+++ Mathlib.lean
@@ -6247,6 +6247,7 @@ import Mathlib.Topology.UniformSpace.DiscreteUniformity
 import Mathlib.Topology.UniformSpace.Equicontinuity
 import Mathlib.Topology.UniformSpace.Equiv
 import Mathlib.Topology.UniformSpace.HeineCantor
+import Mathlib.Topology.UniformSpace.LocallyUniformConvergence
 import Mathlib.Topology.UniformSpace.Matrix
 import Mathlib.Topology.UniformSpace.OfCompactT2
 import Mathlib.Topology.UniformSpace.OfFun
@@ -6256,6 +6257,7 @@ import Mathlib.Topology.UniformSpace.Real
 import Mathlib.Topology.UniformSpace.Separation
 import Mathlib.Topology.UniformSpace.Ultra.Basic
 import Mathlib.Topology.UniformSpace.Ultra.Constructions
+import Mathlib.Topology.UniformSpace.UniformApproximation
 import Mathlib.Topology.UniformSpace.UniformConvergence
 import Mathlib.Topology.UniformSpace.UniformConvergenceTopology
 import Mathlib.Topology.UniformSpace.UniformEmbedding
