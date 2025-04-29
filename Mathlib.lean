6824591625404102784504287f26afd4ee638439
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-04-29 06:40:50 +0000

    feat(CategoryTheory/Center): linear structures and ring morphisms (#23815)
    If `C` is a category and `R` a ring, we relate `R`-linear structures on `C` and ring morphisms `R →+* CatCenter C`.

--- Mathlib.lean
+++ Mathlib.lean
@@ -1912,6 +1912,7 @@ import Mathlib.CategoryTheory.Category.RelCat
 import Mathlib.CategoryTheory.Category.TwoP
 import Mathlib.CategoryTheory.Category.ULift
 import Mathlib.CategoryTheory.Center.Basic
+import Mathlib.CategoryTheory.Center.Linear
 import Mathlib.CategoryTheory.ChosenFiniteProducts
 import Mathlib.CategoryTheory.ChosenFiniteProducts.Cat
 import Mathlib.CategoryTheory.ChosenFiniteProducts.FunctorCategory
