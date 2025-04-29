0c3840a9932c2181910a12dec2c5b4bd277f87f0
Dagur Asgeirsson <dagurtomas@gmail.com>
2025-04-29 11:44:25 +0000

    feat(Condensed): light condensed modules form a Grothendieck abelian category

--- Mathlib/Algebra/Category/ModuleCat/AB.lean
+++ Mathlib/Algebra/Category/ModuleCat/AB.lean
@@ -5,12 +5,14 @@ Authors: Dagur Asgeirsson
 -/
 import Mathlib.Algebra.Category.Grp.AB
 import Mathlib.Algebra.Category.ModuleCat.Colimits
+import Mathlib.CategoryTheory.Abelian.GrothendieckCategory.Basic
 /-!
 
 # AB axioms in module categories
 
 This file proves that the category of modules over a ring satisfies Grothendieck's axioms AB5, AB4,
-and AB4*.
+and AB4*. Further, it proves that `R` is a separator in the category of modules over `R`, and
+concludes that this category is Grothendieck abelian.
 -/
 
 universe u
@@ -30,3 +32,13 @@ instance : AB4 (ModuleCat.{u} R) := AB4.of_AB5 _
 instance : AB4Star (ModuleCat.{u} R) where
   ofShape J :=
     HasExactLimitsOfShape.domain_of_functor (Discrete J) (forget₂ (ModuleCat R) AddCommGrp.{u})
+
+lemma ModuleCat.isSeparator : IsSeparator (ModuleCat.of R R) := fun X Y f g h ↦ by
+  simp only [Set.mem_singleton_iff, forall_eq, ModuleCat.hom_ext_iff, LinearMap.ext_iff] at h
+  ext x
+  simpa using h (ModuleCat.ofHom (LinearMap.toSpanSingleton R X x)) 1
+
+instance : HasSeparator (ModuleCat.{u} R) where
+  hasSeparator := ⟨ModuleCat.of R R, ModuleCat.isSeparator R⟩
+
+instance : IsGrothendieckAbelian.{u} (ModuleCat.{u} R) where
