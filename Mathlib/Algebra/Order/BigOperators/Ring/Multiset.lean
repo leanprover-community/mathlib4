3468f7a8b5dced24f67f09f1d669de8dbd7c5303
mariainesdff <mariaines.dff@gmail.com>
2025-04-30 17:39:04 +0200

    delete import

--- Mathlib/Algebra/Order/BigOperators/Ring/Multiset.lean
+++ Mathlib/Algebra/Order/BigOperators/Ring/Multiset.lean
@@ -3,9 +3,8 @@ Copyright (c) 2021 Ruben Van de Velde. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Ruben Van de Velde
 -/
-import Mathlib.Algebra.Order.BigOperators.Group.Multiset
-import Mathlib.Algebra.Order.BigOperators.Ring.List
 import Mathlib.Algebra.BigOperators.Group.Multiset.Defs
+import Mathlib.Algebra.Order.BigOperators.Ring.List
 
 /-!
 # Big operators on a multiset in ordered rings
