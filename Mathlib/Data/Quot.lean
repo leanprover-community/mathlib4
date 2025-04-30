652a2ba6990bb13194e8140cf0a98e5c08f570fb
Xavier Roblot <roblot@math.univ-lyon1.fr>
2025-04-30 10:45:46 +0200

    make it a simp?

--- Mathlib/Data/Quot.lean
+++ Mathlib/Data/Quot.lean
@@ -297,6 +297,7 @@ theorem Quotient.lift_comp_mk {_ : Setoid α} (f : α → β) (h : ∀ a b : α,
     Quotient.lift f h ∘ Quotient.mk _ = f :=
   rfl
 
+@[simp]
 theorem Quotient.lift_surjective_iff {α β : Sort*} {s : Setoid α} (f : α → β)
     (h : ∀ (a b : α), a ≈ b → f a = f b) :
     Function.Surjective (Quotient.lift f h : Quotient s → β) ↔ Function.Surjective f :=
