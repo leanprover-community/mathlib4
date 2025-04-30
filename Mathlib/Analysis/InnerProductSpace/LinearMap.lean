83dcb81a42044035e14aa08e0151d4d5376ed0af
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2025-04-30 12:08:07 +0200

    Define IsPositive for LinearMap

--- Mathlib/Analysis/InnerProductSpace/LinearMap.lean
+++ Mathlib/Analysis/InnerProductSpace/LinearMap.lean
@@ -281,6 +281,15 @@ variable [SeminormedAddCommGroup E] [InnerProductSpace 𝕜 E]
 
 local notation "⟪" x ", " y "⟫" => @inner 𝕜 _ _ x y
 
+/-- Extract a real bilinear form from an linear map `T`,
+by taking the pairing `fun x ↦ re ⟪T x, x⟫`. -/
+def LinearMap.reApplyInnerSelf (T : E →ₗ[𝕜] E) (x : E) : ℝ :=
+  re ⟪T x, x⟫
+
+theorem LinearMap.reApplyInnerSelf_apply (T : E →ₗ[𝕜] E) (x : E) :
+    T.reApplyInnerSelf x = re ⟪T x, x⟫ :=
+  rfl
+
 /-- Extract a real bilinear form from an operator `T`,
 by taking the pairing `fun x ↦ re ⟪T x, x⟫`. -/
 def ContinuousLinearMap.reApplyInnerSelf (T : E →L[𝕜] E) (x : E) : ℝ :=
