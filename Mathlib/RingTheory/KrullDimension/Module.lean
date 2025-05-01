3be5b95c2db357a11b4540c03e565d4385720116
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2025-05-01 20:52:54 +0800

    add lemma about eq bot

--- Mathlib/RingTheory/KrullDimension/Module.lean
+++ Mathlib/RingTheory/KrullDimension/Module.lean
@@ -26,12 +26,21 @@ variable (R : Type*) [CommRing R] (M : Type*) [AddCommGroup M] [Module R M]
 noncomputable def supportDim : WithBot ℕ∞ :=
   krullDim (Module.support R M)
 
+lemma supportDim_eq_bot_of_subsingleton [Subsingleton M] : supportDim R M = ⊥ := by
+  simpa [supportDim, support_eq_empty_iff]
+
 lemma supportDim_ne_bot_of_nontrivial [Nontrivial M] : supportDim R M ≠ ⊥ := by
-  simpa [supportDim, Module.support_eq_empty_iff, not_subsingleton_iff_nontrivial]
+  simpa [supportDim, support_eq_empty_iff, not_subsingleton_iff_nontrivial]
+
+lemma supportDim_eq_bot_iff_subsingleton : supportDim R M = ⊥ ↔ Subsingleton M := by
+  simp [supportDim, krullDim_eq_bot_iff, support_eq_empty_iff]
+
+lemma supportDim_ne_bot_iff_nontrivial : supportDim R M ≠ ⊥ ↔ Nontrivial M := by
+  simp [supportDim, krullDim_eq_bot_iff, support_eq_empty_iff, not_subsingleton_iff_nontrivial]
 
 lemma supportDim_eq_ringKrullDim_quotient_ann [Module.Finite R M] :
     supportDim R M = ringKrullDim (R ⧸ (Module.annihilator R M)) := by
   simp only [supportDim]
-  rw [Module.support_eq_zeroLocus, ringKrullDim_quotient]
+  rw [support_eq_zeroLocus, ringKrullDim_quotient]
 
 end Module
