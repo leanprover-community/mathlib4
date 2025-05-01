ac858281cab3a40764cd1cc787a885d37e145fe8
Dagur Asgeirsson <dagurtomas@gmail.com>
2025-05-01 10:37:39 +0000

    fix the build

--- Mathlib/CategoryTheory/Localization/Monoidal/Braided.lean
+++ Mathlib/CategoryTheory/Localization/Monoidal/Braided.lean
@@ -20,7 +20,7 @@ braided, in such a way that the localization functor is braided. If `C` is symme
 the monoidal structure on `D` is also symmetric.
 -/
 
-open CategoryTheory Category MonoidalCategory Monoidal BraidedCategory
+open CategoryTheory Category MonoidalCategory BraidedCategory
 
 namespace CategoryTheory.Localization.Monoidal
 
