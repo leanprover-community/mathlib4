05b595ef49920646eee2ea38c81aaaf583c8741d
Xavier Roblot <roblot@math.univ-lyon1.fr>
2025-05-01 16:40:03 +0000

    chore(Algebra): modify the class `IsQuadraticExtension` (#24369)
    The class `IsQuadraticExtension` that was added in a recent PR is modified in the following way: 
    - Moved it to `Algebra` namespace
    - Add the conditions `Module.Free R S` and `StrongRankCondition R` (for `IsQuadraticExtension R S`); these conditions ensure that `Module.Finite R S` (added as an instance)
    - Unbundle the hypothesis `Algebra R S`, thus it is truly a `Prop` as the `Is` indicates. 
    
    Note. since the class  `IsQuadraticExtension`  was just added, no deprecation aliases have been added. 
    
    
    
    Co-authored-by: Xavier Roblot <46200072+xroblot@users.noreply.github.com>

--- Mathlib/FieldTheory/Normal/Basic.lean
+++ Mathlib/FieldTheory/Normal/Basic.lean
@@ -17,8 +17,8 @@ is the same as being a splitting field (`Normal.of_isSplittingField` and
 
 ## Additional Results
 
-* `IsQuadraticExtension.Normal`: a quadratic extension, given as a class
-  `IsQuadraticExtension`, is normal.
+* `Algebra.IsQuadraticExtension.normal`: the instance that a quadratic extension, given as a class
+  `Algebra.IsQuadraticExtension`, is normal.
 
 -/
 
@@ -283,14 +283,14 @@ end minpoly
 /--
 A quadratic extension is normal.
 -/
-instance IsQuadraticExtension.Normal (F K : Type*) [Field F] [Field K]
-    [h : IsQuadraticExtension F K] :
+instance Algebra.IsQuadraticExtension.normal (F K : Type*) [Field F] [Field K] [Algebra F K]
+    [IsQuadraticExtension F K] :
     Normal F K where
   splits' := by
     intro x
-    obtain h | h := le_iff_lt_or_eq.mp (h.finrank_eq_two ▸ minpoly.natDegree_le x)
+    obtain h | h := le_iff_lt_or_eq.mp (finrank_eq_two F K ▸ minpoly.natDegree_le x)
     · exact splits_of_natDegree_le_one _ (by rwa [Nat.le_iff_lt_add_one])
     · exact splits_of_natDegree_eq_two _ h (minpoly.aeval F x)
 
 @[deprecated (since := "2025-04-17")] alias normal_of_finrank_eq_two :=
-  IsQuadraticExtension.Normal
+  Algebra.IsQuadraticExtension.normal
