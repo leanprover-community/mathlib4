7760da847a2b4e19be649c202aabaa5905cb4f09
Joseph Myers <jsm@polyomino.org.uk>
2025-05-01 14:04:24 +0000

    refactor(Geometry/Euclidean): split out altitudes (#24504)
    Move the definition of `Affine.Simplex.altitude` and associated API from `MongePoint.lean` to a separate file, in preparation for setting up other associated definitions (as discussed in #23752) that are of use without involving the Monge point / orthocenter.
    
    There are no changes to definitions, statements or proofs; this is just moving content from one file to another.

--- Mathlib.lean
+++ Mathlib.lean
@@ -3504,6 +3504,7 @@ import Mathlib.FieldTheory.SeparableDegree
 import Mathlib.FieldTheory.SplittingField.Construction
 import Mathlib.FieldTheory.SplittingField.IsSplittingField
 import Mathlib.FieldTheory.Tower
+import Mathlib.Geometry.Euclidean.Altitude
 import Mathlib.Geometry.Euclidean.Angle.Oriented.Affine
 import Mathlib.Geometry.Euclidean.Angle.Oriented.Basic
 import Mathlib.Geometry.Euclidean.Angle.Oriented.RightAngle
