7886b64460f2dbec55a6175b54a7c2e836c25239
Damiano Testa <adomani@gmail.com>
2025-05-01 13:54:38 +0200

    shake

--- Mathlib/Tactic/Linter/CommandStart.lean
+++ Mathlib/Tactic/Linter/CommandStart.lean
@@ -4,7 +4,8 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Damiano Testa
 -/
 
-import Mathlib.Tactic.Linter.PPRoundtrip
+import Lean.Elab.Command
+import Lean.Parser.Syntax
 
 /-!
 #  The `commandStart` linter
