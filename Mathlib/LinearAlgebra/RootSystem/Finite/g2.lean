e392f72659c68e89ea6e75da4198e898cf786a1a
Fabrizio Barroero <fbarroero@gmail.com>
2025-05-02 14:39:33 +0200

    Delete Mathlib/LinearAlgebra/RootSystem/Finite/g2.lean

--- Mathlib/LinearAlgebra/RootSystem/Finite/g2.lean
+++
@@ -1,8 +0,0 @@
-/-
-Copyright (c) 2025 Oliver Nash. All rights reserved.
-Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Oliver Nash
--/
-import Mathlib.LinearAlgebra.RootSystem.Finite.Lemmas
-
-deprecated_module (since := "2025-04-19")
