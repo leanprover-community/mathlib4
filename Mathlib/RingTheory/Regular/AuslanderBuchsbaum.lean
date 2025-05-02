fd177d14dae0e0a2372e910ad71bad36483e549e
Yongle Hu <140475041+mbkybky@users.noreply.github.com>
2025-05-02 14:39:31 +0800

    stament of dim shifting

--- Mathlib/RingTheory/Regular/AuslanderBuchsbaum.lean
+++ Mathlib/RingTheory/Regular/AuslanderBuchsbaum.lean
@@ -1,6 +1,20 @@
 import Mathlib.CategoryTheory.Abelian.Projective.Dimension
 import Mathlib.RingTheory.Regular.Depth
 
+namespace CategoryTheory
+
+universe w v u
+
+open Abelian Limits ZeroObject
+
+variable {C : Type u} [Category.{v} C] [Abelian C] [HasExt.{w} C]
+  {X : C} {S : ShortComplex C} (hS : S.ShortExact) [Projective S.X₂]
+  (n₀ n₁ : ℕ) (h : n₀ + 1 = n₁) (h0 : n₀ ≠ 0)
+
+def dim_shifting (n : ℕ) : Ext X S.X₃ n₀ ≃ Ext X S.X₁ n₁ := by sorry
+
+end CategoryTheory
+
 universe v u
 
 open IsLocalRing
