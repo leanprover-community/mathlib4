8abbe1a3b17712fcb875bca7b69a2f4bedcdd9e7
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-05-03 11:03:04 +0200

    wip

--- Mathlib/CategoryTheory/Monoidal/KFlat.lean
+++ Mathlib/CategoryTheory/Monoidal/KFlat.lean
@@ -6,6 +6,7 @@ Authors: Joël Riou
 import Mathlib.CategoryTheory.Localization.DerivabilityStructure.Basic
 import Mathlib.CategoryTheory.Monoidal.Derived
 import Mathlib.CategoryTheory.Monoidal.Subcategory
+import Mathlib.CategoryTheory.MorphismProperty.Composition
 
 /-!
 # K-flat objects
@@ -24,15 +25,29 @@ def kFlat : ObjectProperty C := fun X ↦
   W ≤ W.inverseImage (tensorLeft X) ∧
     W ≤ W.inverseImage (tensorRight X)
 
+section
+
+variable {W}
+
+lemma kFlat.whiskerLeft {X Y₁ Y₂ : C} (hX : W.kFlat X)
+    {f : Y₁ ⟶ Y₂} (hf : W f) : W (X ◁ f) :=
+  hX.1 _ hf
+
+lemma kFlat.whiskerRight {Y X₁ X₂ : C} (hY : W.kFlat Y)
+    {f : X₁ ⟶ X₂} (hf : W f) : W (f ▷ Y) :=
+  hY.2 _ hf
+
+end
+
 instance [W.RespectsIso] : W.kFlat.IsMonoidal where
   prop_unit :=
     ⟨fun _ _ _ hf ↦ (W.arrow_mk_iso_iff (Arrow.isoMk (λ_ _) (λ_ _))).2 hf,
       fun _ _ _ hf ↦ (W.arrow_mk_iso_iff (Arrow.isoMk (ρ_ _) (ρ_ _))).2 hf⟩
   prop_tensor X₁ X₂ hX₁ hX₂ :=
     ⟨fun _ _ _ hf ↦ (W.arrow_mk_iso_iff
-        (Arrow.isoMk (α_ _ _ _) (α_ _ _ _))).2 (hX₁.1 _ (hX₂.1 _ hf)),
+        (Arrow.isoMk (α_ _ _ _) (α_ _ _ _))).2 (hX₁.whiskerLeft (hX₂.whiskerLeft hf)),
       fun _ _ _ hf ↦ (W.arrow_mk_iso_iff
-        (Arrow.isoMk (α_ _ _ _) (α_ _ _ _))).1 (hX₂.2 _ (hX₁.2 _ hf))⟩
+        (Arrow.isoMk (α_ _ _ _) (α_ _ _ _))).1 (hX₂.whiskerRight (hX₁.whiskerRight hf))⟩
 
 abbrev KFlat := W.kFlat.FullSubcategory
 
@@ -48,6 +63,26 @@ def localizerMorphismKFlat :
 instance [W.RespectsIso] :
     W.localizerMorphismKFlat.functor.Monoidal :=
   inferInstanceAs W.ιKFlat.Monoidal
+
+lemma whiskerRight_of_kFlat [W.HasTwoOutOfThreeProperty]
+    [W.localizerMorphismKFlat.HasLeftResolutions]
+    {X₁ X₂ : C} (f : X₁ ⟶ X₂) (hf : W f) (hX₁ : W.kFlat X₁) (hX₂ : W.kFlat X₂) (Y : C) :
+    W (f ▷ Y) := by
+  obtain ⟨Y', hY', g, hg⟩ : ∃ (Y' : C) (hY : W.kFlat Y') (g : Y' ⟶ Y), W g := by
+    have R : W.localizerMorphismKFlat.LeftResolution Y := Classical.arbitrary _
+    exact ⟨R.X₁.obj, R.X₁.property, R.w, R.hw⟩
+  rw [← W.precomp_iff _ _ (hX₁.whiskerLeft hg), whisker_exchange f g]
+  exact W.comp_mem _ _ (hY'.whiskerRight hf) (hX₂.whiskerLeft hg)
+
+lemma whiskerLeft_of_kFlat [W.HasTwoOutOfThreeProperty]
+    [W.localizerMorphismKFlat.HasLeftResolutions]
+    {Y₁ Y₂ : C} (g : Y₁ ⟶ Y₂) (hg : W g) (hY₁ : W.kFlat Y₁) (hY₂ : W.kFlat Y₂) (X : C) :
+    W (X ◁ g) := by
+  obtain ⟨X', hX', f, hf⟩ : ∃ (X' : C) (hX : W.kFlat X') (f : X' ⟶ X), W f := by
+    have R : W.localizerMorphismKFlat.LeftResolution X := Classical.arbitrary _
+    exact ⟨R.X₁.obj, R.X₁.property, R.w, R.hw⟩
+  rw [← W.precomp_iff _ _ (hY₁.whiskerRight hf), ← whisker_exchange f g]
+  exact W.comp_mem _ _ (hX'.whiskerLeft hg) (hY₂.whiskerRight hf)
 
 /-instance [W.localizerMorphismKFlat.IsLeftDerivabilityStructure]
     [L.IsLocalization W] [W.ContainsIdentities] :
