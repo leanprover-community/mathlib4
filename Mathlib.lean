f39b9d30037200dfc99959aba431f8c8158dfd05
Frédéric Dupuis <dupuisf@iro.umontreal.ca>
2025-05-03 14:10:05 +0000

    feat(CStarAlgebra): define completely positive maps (#24359)
    This PR defines completely positive maps. A linear map `φ : A₁ →ₗ[ℂ] A₂` (where `A₁` and `A₂` are C⋆-algebras) is called completely positive (CP) if applying `φ` to all entries of a k × k matrix is also positive for every `k ∈ ℕ`.
    
    Here we provide the definition and a few basic properties: CP maps are positive, and non-unital star algebra homomorphisms are CP. A fair amount of supporting material was also added to `CStarMatrix`. The API is still very barebones and we leave e.g. composition for subsequent PRs.
    
    
    
    Co-authored-by: Frédéric Dupuis <31101893+dupuisf@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1293,6 +1293,7 @@ import Mathlib.Analysis.CStarAlgebra.ApproximateUnit
 import Mathlib.Analysis.CStarAlgebra.Basic
 import Mathlib.Analysis.CStarAlgebra.CStarMatrix
 import Mathlib.Analysis.CStarAlgebra.Classes
+import Mathlib.Analysis.CStarAlgebra.CompletelyPositiveMap
 import Mathlib.Analysis.CStarAlgebra.ContinuousFunctionalCalculus.Basic
 import Mathlib.Analysis.CStarAlgebra.ContinuousFunctionalCalculus.Instances
 import Mathlib.Analysis.CStarAlgebra.ContinuousFunctionalCalculus.Integral
