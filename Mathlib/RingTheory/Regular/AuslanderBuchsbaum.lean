5d6745da1440fd155d7c6cfed34614f69240fc17
Yijun Yuan <941201yuan@gmail.com>
2025-05-03 14:40:36 +0800

    add some sorry

--- Mathlib/RingTheory/Regular/AuslanderBuchsbaum.lean
+++ Mathlib/RingTheory/Regular/AuslanderBuchsbaum.lean
@@ -25,9 +25,26 @@ open RingTheory.Sequence Ideal CategoryTheory CategoryTheory.Abelian
 
 variable {R : Type u} [CommRing R] [Small.{v} R]
 
+lemma proj_mod_over_local_ring_is_free [IsLocalRing R] (M : ModuleCat.{v} R) [Module.Finite R M] [Module.Projective R M]: Module.Free R M:= by
+  -- Add your proof here
+  sorry
+
+local instance : CategoryTheory.HasExt.{max u v} (ModuleCat.{v} R) :=
+  --CategoryTheory.HasExt.standard (ModuleCat.{v} R)
+  CategoryTheory.hasExt_of_enoughProjectives.{max u v} (ModuleCat.{v} R)
+
 open scoped Classical in
 theorem AuslanderBuchsbaum [IsNoetherianRing R] [IsLocalRing R]
     (M : ModuleCat.{v} R) [Nontrivial M] [Module.Finite R M]
     [Small.{v} (R ⧸ (IsLocalRing.maximalIdeal R))]
-    (h : ∃ i : ℕ, CategoryTheory.HasProjectiveDimensionLE M i) :
-    Nat.find h + IsLocalRing.depth M = IsLocalRing.depth (ModuleCat.of R R) := by sorry
+    (hfinprojdim : ∃ i : ℕ, CategoryTheory.HasProjectiveDimensionLE M i) :
+    Nat.find hfinprojdim + IsLocalRing.depth M = IsLocalRing.depth (ModuleCat.of R R) := by
+    generalize h: Nat.find hfinprojdim = n
+    induction' n
+    · simp
+      have pdz: HasProjectiveDimensionLE M (Nat.find hfinprojdim) := Nat.find_spec hfinprojdim
+      simp [h, HasProjectiveDimensionLE, hasProjectiveDimensionLT_iff M  1] at pdz
+      have pm: Module.Projective R M := by
+        sorry
+      sorry
+    · sorry
