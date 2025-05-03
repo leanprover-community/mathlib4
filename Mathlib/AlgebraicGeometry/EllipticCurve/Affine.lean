fddfbc96016cc29500e727d51bbd3c91aff8e6c5
David Kurniadi Angdinata <dka31@cantab.ac.uk>
2025-05-03 19:01:08 +0100

    Add deprecated module

---
+++ Mathlib/AlgebraicGeometry/EllipticCurve/Affine.lean
@@ -0,0 +1,7 @@
+/-
+Copyright (c) 2023 David Kurniadi Angdinata. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: David Kurniadi Angdinata
+-/
+
+deprecated_module (since := "2025-05-03")
