151ce28695cb6699c3ae73f7caa4b303431c3b25
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-05-03 11:34:34 +0200

    fix

--- Mathlib/CategoryTheory/Shift/Linear.lean
+++ Mathlib/CategoryTheory/Shift/Linear.lean
@@ -3,7 +3,7 @@ Copyright (c) 2024 Joël Riou. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joël Riou
 -/
-import Mathlib.CategoryTheory.Shift.Localization
+import Mathlib.CategoryTheory.Shift.CommShift
 import Mathlib.CategoryTheory.Localization.Linear
 
 /-!
