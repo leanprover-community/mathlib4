79673fc218e2b93d7273dc9cf7c604d642f4b095
pfaffelh <p.p@stochastik.uni-freiburg.de>
2025-05-03 23:10:17 +0200

    add Mathlib

--- Mathlib.lean
+++ Mathlib.lean
@@ -3916,6 +3916,7 @@ import Mathlib.LinearAlgebra.Matrix.GeneralLinearGroup.Basic
 import Mathlib.LinearAlgebra.Matrix.GeneralLinearGroup.Card
 import Mathlib.LinearAlgebra.Matrix.GeneralLinearGroup.Defs
 import Mathlib.LinearAlgebra.Matrix.Gershgorin
+import Mathlib.LinearAlgebra.Matrix.Gram
 import Mathlib.LinearAlgebra.Matrix.Hermitian
 import Mathlib.LinearAlgebra.Matrix.HermitianFunctionalCalculus
 import Mathlib.LinearAlgebra.Matrix.Ideal
