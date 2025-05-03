ba3e92b03cacbb786913091cdb5dbd25e03e7314
David Kurniadi Angdinata <dka31@cantab.ac.uk>
2025-05-03 19:11:37 +0100

    Import Mathlib.Init

--- Mathlib/AlgebraicGeometry/EllipticCurve/Affine.lean
+++ Mathlib/AlgebraicGeometry/EllipticCurve/Affine.lean
@@ -3,5 +3,6 @@ Copyright (c) 2023 David Kurniadi Angdinata. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: David Kurniadi Angdinata
 -/
+import Mathlib.Init
 
 deprecated_module (since := "2025-05-03")
