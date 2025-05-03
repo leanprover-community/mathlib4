2c4fce7701ed4a3cb49db81a46700d413042a470
Nailin Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2025-05-03 07:23:29 +0000

    feat(Algebra): finiteness of associated primes (#24242)
    In this PR we proved the finiteness of associated primes for finitely generated modules over notherian ring. Mainly from https://github.com/acmepjz/lean-iwasawa/blob/master/Iwasawalib/RingTheory/CharacteristicIdeal/Basic.lean authored by @acmepjz 
    
    Co-authored-by: Jz Pan <acme_pjz@hotmail.com>
    
    
    
    Co-authored-by: acmepjz <acme_pjz@hotmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -5053,6 +5053,7 @@ import Mathlib.RingTheory.HopfAlgebra.TensorProduct
 import Mathlib.RingTheory.HopkinsLevitzki
 import Mathlib.RingTheory.Ideal.AssociatedPrime
 import Mathlib.RingTheory.Ideal.AssociatedPrime.Basic
+import Mathlib.RingTheory.Ideal.AssociatedPrime.Finiteness
 import Mathlib.RingTheory.Ideal.Basic
 import Mathlib.RingTheory.Ideal.Basis
 import Mathlib.RingTheory.Ideal.BigOperators
