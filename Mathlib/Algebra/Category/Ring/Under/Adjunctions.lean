7cb0a90c6d42c9a4c78c85eb07f804ff964f10e8
Ruiqi Chen <ruiqi_chen@stu.pku.edu.cn>
2025-05-04 15:20:13 +0800

    tensorProd_freeAbs

--- Mathlib/Algebra/Category/Ring/Under/Adjunctions.lean
+++ Mathlib/Algebra/Category/Ring/Under/Adjunctions.lean
@@ -70,8 +70,7 @@ lemma tensorProd_freeAbs_tauto : freeAbs R ⋙ R.tensorProd S = {
     have (ninstσ : Algebra R (MvPolynomial σ R)) (ninstτ : Algebra R (MvPolynomial τ R))
         (eqσ : ninstσ = MvPolynomial.algebra) (eqτ : ninstτ = MvPolynomial.algebra) :
         (@Algebra.TensorProduct.map _ _ _ _ _ _ _ _ _ _ _ _ _ _
-        (ninstσ) _ _ _ _ _
-        (ninstτ) (AlgHom.id S S)
+        (ninstσ) _ _ _ _ _ (ninstτ) (AlgHom.id S S)
         ((cast <| congr_arg₂ (fun instσ instτ => @AlgHom R (MvPolynomial σ R)
             (MvPolynomial τ R) _ _ _ instσ instτ)
         eqσ.symm eqτ.symm) (MvPolynomial.rename f))).toUnder =
@@ -89,81 +88,31 @@ lemma tensorProd_freeAbs_tauto : freeAbs R ⋙ R.tensorProd S = {
       (mkUnder_eq (MvPolynomial σ R)) (mkUnder_eq (MvPolynomial τ R))
 
 
--- noncomputable def tensorProd_freeAbs : freeAbs R ⋙ R.tensorProd S ≅ freeAbs S := by
---   -- To get rid of algebra_eq
---   let left : Type u ⥤ Under S := {
---     obj σ := S.mkUnder <| S ⊗[R] (MvPolynomial σ R)
---     map f := (Algebra.TensorProduct.map (AlgHom.id S S) (MvPolynomial.rename f)).toUnder
---     map_id σ := by
---       simp only
---       have : MvPolynomial.rename (𝟙 σ) = AlgHom.id R (MvPolynomial σ R) :=
---         MvPolynomial.rename_id (R := R) (σ := σ)
---       rw [this, Algebra.TensorProduct.map_id]
---       rfl
---     map_comp f g := by
---       simp only
---       have : MvPolynomial.rename (R := R) (f ≫ g) =
---         (MvPolynomial.rename g).comp (MvPolynomial.rename f) :=
---         (MvPolynomial.rename_comp_rename f g).symm
---       rw [this, Algebra.TensorProduct.map_id_comp, AlgHom.toUnder_comp]
---   }
---   have : freeAbs R ⋙ R.tensorProd S = left := by
---     have obj (σ : Type u) : (freeAbs R ⋙ R.tensorProd S).obj σ = left.obj σ := by
---       unfold freeAbs left tensorProd
---       simp only [Functor.comp_obj]
---       rw [algebra_eq]
---       rfl
---     #check eqToHom
---     have map (σ τ : Type u) (f : σ ⟶ τ) : (freeAbs R ⋙ R.tensorProd S).map f
---       = eqToHom (obj σ) ≫ left.map f ≫ eqToHom (obj τ).symm := by
---       unfold freeAbs left tensorProd
---       simp only [Functor.comp_obj, Functor.comp_map, id_eq, eq_mpr_eq_cast, AlgHom.toUnder_comp,
---         cast_eq]
---       ext x
---       -- simp only [AlgHom.toUnder_right,
---       --   Under.comp_right, Under.eqToHom_right, hom_comp, RingHom.coe_comp, Function.comp_apply]
-
---       #check AlgHom.toUnder_eq_core
---       sorry
---     exact CategoryTheory.Functor.ext obj map
---   rw [this]
---   -- let pointwise (σ : Type u) : left.obj σ ≅ (freeAbs S).obj σ := (MvPolynomial.algebraTensorAlgEquiv (σ := σ) R S).toUnder
---   -- unfold left
---   exact NatIso.ofComponents (fun σ => (MvPolynomial.algebraTensorAlgEquiv (σ := σ) R S).toUnder) (fun {σ τ} f => by
---     unfold freeAbs left
---     simp only
---     -- exact?
---     sorry
---   )
-
-
-
-  -- let pointwise (σ : Type u) : (freeAbs R ⋙ R.tensorProd S).obj σ ≅ (freeAbs S).obj σ := by
-  --   unfold freeAbs tensorProd
-  --   simp only [Functor.comp_obj]
-  --   rw [algebra_eq]
-  --   exact (MvPolynomial.algebraTensorAlgEquiv (σ := σ) R S).toUnder
-  -- exact NatIso.ofComponents pointwise (fun {σ τ} f => by
-  --   unfold freeAbs tensorProd pointwise
-  --   simp only [Functor.comp_obj, Functor.comp_map, eq_mpr_eq_cast, id_eq]
-  --   #check MvPolynomial.aeval_one_tmul
-  --   -- S ⊗[R] (MvPolynomial σ R) ≃ₐ[S] MvPolynomial σ S
-  --   -- S ⊗[R] (MvPolynomial τ R) ≃ₐ[S] MvPolynomial τ S
-  --   have : MvPolynomial σ R →ₐ[R] MvPolynomial τ R := MvPolynomial.rename f
-  --   -- have : MvPolynomial σ R ⟶ MvPolynomial τ R := (MvPolynomial.rename f).toUnder
-
-  --   -- have : S ⊗[R] (MvPolynomial σ R) ⟶ S ⊗[R] (MvPolynomial τ R) := by sorry
-  --   -- The algebra_eq leads to some difficulties
-  --   -- rw [algebra_eq]
-  --   ext x
-  --   simp only [AlgHom.toUnder_eq, eq_mpr_eq_cast, cast_cast, Under.comp_right, hom_comp,
-  --     RingHom.coe_comp, Function.comp_apply, AlgHom.toUnder_right]
-
-  --   -- erw [← AlgHom.toUnder_eq]
-
-  --   sorry
-  -- )
-
+noncomputable def tensorProd_freeAbs : freeAbs R ⋙ R.tensorProd S ≅ freeAbs S := by
+  -- To get rid of algebra_eq
+  rw [tensorProd_freeAbs_tauto]
+  exact (NatIso.ofComponents
+    (fun σ => (MvPolynomial.algebraTensorAlgEquiv (σ := σ) R S).symm.toUnder)
+    (fun {σ τ} f => by
+      show (MvPolynomial.rename f).toUnder ≫
+          (MvPolynomial.algebraTensorAlgEquiv R S).symm.toAlgHom.toUnder
+          = (MvPolynomial.algebraTensorAlgEquiv R S).symm.toAlgHom.toUnder ≫
+          (Algebra.TensorProduct.map (AlgHom.id S S) (MvPolynomial.rename f)).toUnder
+      suffices (MvPolynomial.algebraTensorAlgEquiv R S).symm.toAlgHom.comp (MvPolynomial.rename f)
+          = (Algebra.TensorProduct.map (AlgHom.id S S) (MvPolynomial.rename f)).comp
+            (MvPolynomial.algebraTensorAlgEquiv R S).symm.toAlgHom from
+        congr_arg (fun f => f.toUnder) this
+      suffices (e : σ) →
+          (MvPolynomial.algebraTensorAlgEquiv R S).symm.toAlgHom
+          ((MvPolynomial.rename f) (MvPolynomial.X e))
+          = (Algebra.TensorProduct.map (AlgHom.id S S) (MvPolynomial.rename f))
+          ((MvPolynomial.algebraTensorAlgEquiv R S).symm.toAlgHom (MvPolynomial.X e)) from by
+        exact MvPolynomial.algHom_ext this
+      unfold MvPolynomial.algebraTensorAlgEquiv
+      simp only [AlgEquiv.toAlgHom_eq_coe, MvPolynomial.rename_X, AlgHom.coe_coe,
+        AlgEquiv.ofAlgHom_symm_apply, MvPolynomial.aeval_X, Algebra.TensorProduct.map_tmul,
+        AlgHom.coe_id, id_eq, implies_true]
+    )).symm
 
 -- /-- The freeAbs is given by `R ⊗[ℤ] ℤ [σ]`. -/
 
