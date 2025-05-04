9598c4d53d41a541e093e6f374d249e47d454132
Christopher Hoskin <christopher.hoskin@gmail.com>
2025-05-04 10:17:18 +0100

    rootsOfUnityCircleEquiv

--- Mathlib/RingTheory/RootsOfUnity/AlgebraicallyClosed.lean
+++ Mathlib/RingTheory/RootsOfUnity/AlgebraicallyClosed.lean
@@ -39,21 +39,6 @@ end IsAlgClosed
 
 variable (n : ℕ) [NeZero n]
 
-#check ((rootsOfUnityUnitsMulEquiv ℂ n).toMonoidHom.comp (restrictRootsOfUnity Circle.toUnits n) :
-  ((rootsOfUnity n Circle) →* (rootsOfUnity n ℂ)))
-
-#check ((rootsOfUnityUnitsMulEquiv ℂ n) : rootsOfUnity n ℂˣ ≃* rootsOfUnity n ℂ)
-
-#check ((restrictRootsOfUnity Circle.toUnits n) : ((rootsOfUnity n Circle) →* (rootsOfUnity n ℂˣ)))
-
-variable (z : rootsOfUnity n ℂ)
-
-#check z.val.val
-
-#check Complex.norm_eq_one_of_mem_rootsOfUnity z.prop
-
-lemma n1 : ‖z.val.val‖=1 := Complex.norm_eq_one_of_mem_rootsOfUnity z.prop
-
 @[simps]
 noncomputable def rootsOfUnitytoCircle : (rootsOfUnity n ℂ) →* Circle where
   toFun := fun z => ⟨z.val.val,
@@ -61,34 +46,14 @@ noncomputable def rootsOfUnitytoCircle : (rootsOfUnity n ℂ) →* Circle where
   map_one' := rfl
   map_mul' _ _ := rfl
 
-#check (rootsOfUnitytoCircle n).toHomUnits
-
-
 noncomputable def rootsOfUnityCircleUnitsEquiv : rootsOfUnity n Circle ≃* rootsOfUnity n ℂ where
   __ := (rootsOfUnityUnitsMulEquiv ℂ n).toMonoidHom.comp (restrictRootsOfUnity Circle.toUnits n)
-  invFun z := ⟨MonoidHom.toHomUnits (rootsOfUnitytoCircle n) z, by
+  invFun z := ⟨(rootsOfUnitytoCircle n).toHomUnits z, by
     rw [mem_rootsOfUnity']
     rw [MonoidHom.coe_toHomUnits]
     rw [← MonoidHom.map_pow]
     rw [← (rootsOfUnitytoCircle n).map_one]
     congr
     aesop⟩
-  left_inv := sorry
-  right_inv := sorry
-
-variable (w : rootsOfUnity n ℂˣ)
-
-#check w.2
-
-#check Units.liftRight
-
-#check (toUnits (G := ℂˣ) : ℂˣ ≃* ℂˣˣ)
-
-#check (MulEquiv.restrictRootsOfUnity (toUnits (G := ℂˣ)) n :
-   (rootsOfUnity n ℂˣ) ≃* (rootsOfUnity n ℂˣˣ))
-
-noncomputable def rootsOfUnityCircleUnitsEquiv2 : rootsOfUnity n Circle ≃* rootsOfUnity n ℂˣ where
-  __ := restrictRootsOfUnity Circle.toUnits n
-  invFun := fun z => ⟨z.val,sorry⟩
-  left_inv := sorry
-  right_inv := sorry
+  left_inv _ := by aesop
+  right_inv _ := by aesop
