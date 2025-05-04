c501ba7387ee2ffb9bc6088853ff47a036477251
Ruiqi Chen <ruiqi_chen@stu.pku.edu.cn>
2025-05-04 22:06:23 +0800

    Slight Changes

--- Mathlib/Algebra/Category/Ring/Under/Adjunctions.lean
+++ Mathlib/Algebra/Category/Ring/Under/Adjunctions.lean
@@ -9,7 +9,19 @@ import Mathlib.RingTheory.TensorProduct.MvPolynomial
 import Mathlib.Algebra.MvPolynomial.CommRing
 import Mathlib.Algebra.Category.Ring.Adjunctions
 
+/-!
+# Adjunctions related to `Under (R : CommRingCat)`
+
+In this file we provide functors `forgetRelative R S : Under S ⥤ Under R` for `S` an `R`-algebra,
+`freeAbsolute R : Type u ⥤ Under R` the free functor given by polynomials over `R` and
+`forget : Under R ⥤ Type u` the forgetful functor. And prove some basic facts including adjunctions
+`tensorProd R S ⊣ forgetRelative R S` and `freeAbsolute R ⊣ forget`.
+-/
+
+noncomputable section
+
 open TensorProduct CategoryTheory
+
 universe u
 variable {R S : CommRingCat.{u}}
 variable [Algebra R S]
@@ -23,25 +35,25 @@ def forgetRelative : Under S ⥤ Under R := Under.map <| ofHom Algebra.algebraMa
 
 /-- The adjunction between `tensorProd R S` and `forgetRelative R S`. -/
 @[simps! unit_app counit_app]
-noncomputable def adjTensorForget : tensorProd R S ⊣ forgetRelative R S :=
+def adjTensorForget : tensorProd R S ⊣ forgetRelative R S :=
   (Under.mapPushoutAdj (ofHom <| algebraMap R S)).ofNatIsoLeft ((R.tensorProdIsoPushout S).symm)
 
 variable (R) in
 /-- The free functor given by polynomials. -/
 @[simps! map_right]
-noncomputable def freeAbs : Type u ⥤ Under R where
+def freeAbsolute : Type u ⥤ Under R where
   obj σ := R.mkUnder <| MvPolynomial σ R
   map f := (MvPolynomial.rename f).toUnder
   map_id σ := congr_arg (fun x => x.toUnder) <| MvPolynomial.rename_id (σ := σ) (R := R)
   map_comp f g := congr_arg (fun x => x.toUnder) (MvPolynomial.rename_comp_rename (R := R) f g).symm
 
 @[simp]
-lemma freeAbs_obj (σ : Type u) : algebra ((freeAbs R).obj σ) = MvPolynomial.algebra :=
+lemma freeAbsolute_obj (σ : Type u) : algebra ((freeAbsolute R).obj σ) = MvPolynomial.algebra :=
   mkUnder_eq <| MvPolynomial σ R
 
 @[simp]
-lemma freeAbs_map {σ τ : Type u} (f : σ ⟶ τ) :
-    toAlgHom ((freeAbs R).map f) =
+lemma freeAbsolute_map {σ τ : Type u} (f : σ ⟶ τ) :
+    toAlgHom ((freeAbsolute R).map f) =
     (cast <| congr_arg₂
     (fun instA instB => @AlgHom R (MvPolynomial σ R) (MvPolynomial τ R) _ _ _ instA instB)
     (mkUnder_eq (MvPolynomial σ R)).symm
@@ -51,7 +63,7 @@ lemma freeAbs_map {σ τ : Type u} (f : σ ⟶ τ) :
 /-- The forgetful functor `Under R ⥤ CommRingCat ⥤ Type`. -/
 def forget : Under R ⥤ Type u := Under.forget R ⋙ HasForget.forget
 
-lemma tensorProd_freeAbs_tauto : freeAbs R ⋙ R.tensorProd S = {
+lemma tensorProd_freeAbsolute_tauto : freeAbsolute R ⋙ R.tensorProd S = {
     obj σ := S.mkUnder <| S ⊗[R] (MvPolynomial σ R)
     map f := (Algebra.TensorProduct.map (AlgHom.id S S) (MvPolynomial.rename f)).toUnder
     map_id σ := by
@@ -69,7 +81,7 @@ lemma tensorProd_freeAbs_tauto : freeAbs R ⋙ R.tensorProd S = {
     } := by
   apply CategoryTheory.Functor.ext
   · intro σ τ f
-    unfold freeAbs tensorProd
+    unfold freeAbsolute tensorProd
     dsimp
     rw [AlgHom.toUnder_eq]
     -- find out the path induction
@@ -93,10 +105,10 @@ lemma tensorProd_freeAbs_tauto : freeAbs R ⋙ R.tensorProd S = {
     exact this (algebra (R.mkUnder (MvPolynomial σ R))) (algebra (R.mkUnder (MvPolynomial τ R)))
       (mkUnder_eq (MvPolynomial σ R)) (mkUnder_eq (MvPolynomial τ R))
 
-/-- We obtain `freeAbs S` by base changing `freeAbs R` using `⊗[R] S`.  -/
-noncomputable def tensorProd_freeAbs : freeAbs R ⋙ R.tensorProd S ≅ freeAbs S := by
+/-- We obtain `freeAbsolute S` by base changing `freeAbsolute R` using `⊗[R] S`. -/
+def tensorProd_freeAbsolute : freeAbsolute R ⋙ R.tensorProd S ≅ freeAbsolute S := by
   -- To get rid of algebra_eq
-  rw [tensorProd_freeAbs_tauto]
+  rw [tensorProd_freeAbsolute_tauto]
   exact (NatIso.ofComponents
     (fun σ => (MvPolynomial.algebraTensorAlgEquiv (σ := σ) R S).symm.toUnder)
     (fun {σ τ} f => by
@@ -121,11 +133,11 @@ noncomputable def tensorProd_freeAbs : freeAbs R ⋙ R.tensorProd S ≅ freeAbs 
     )).symm
 
 /-- A commutative ring is an algebra over `ℤ` which is commutative. -/
-noncomputable def Under_ℤ : Under (of (ULift.{u, 0} ℤ)) ≌ CommRingCat.{u} :=
+def Under_ℤ : Under (of (ULift.{u, 0} ℤ)) ≌ CommRingCat.{u} :=
   Under.equivalenceOfIsInitial isInitial
 
-/-- The defined `freeAbs ℤ` is isomorphic to `free` -/
-noncomputable def freeAbs_ℤ_tauto : free ⋙ Under_ℤ.inverse ≅ freeAbs (of (ULift.{u, 0} ℤ)) where
+/-- The defined `freeAbsolute ℤ` is isomorphic to `free` -/
+def freeAbsolute_ℤ_tauto : free ⋙ Under_ℤ.inverse ≅ freeAbsolute (of (ULift.{u, 0} ℤ)) where
   hom := {
     app σ := Under.homMk
       (CommRingCat.ofHom <| MvPolynomial.map <| Int.castRingHom (ULift.{u, 0} ℤ))
@@ -163,8 +175,9 @@ instance (R : CommRingCat.{u}) : Algebra (of (ULift.{u, 0} ℤ)) R
   := RingHom.toAlgebra RingHom.smulOneHom
 
 /-- The free forgetful adjunction of `Under R`. -/
-noncomputable def adjFreeForget : freeAbs R ⊣ forget :=
+def adjFreeForget : freeAbsolute R ⊣ forget :=
   (adj.comp (Under_ℤ.symm.toAdjunction.comp adjTensorForget)).ofNatIsoLeft
-  (isoWhiskerRight freeAbs_ℤ_tauto ((of (ULift.{u, 0} ℤ)).tensorProd R) ≪≫ tensorProd_freeAbs)
+  (isoWhiskerRight freeAbsolute_ℤ_tauto ((of (ULift.{u, 0} ℤ)).tensorProd R)
+      ≪≫ tensorProd_freeAbsolute)
 
 end CommRingCat
