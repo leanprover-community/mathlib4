ba6653a2bbf6c24b1cd6fee8cde6afb54c6e0c9a
Paul-Lez <paul.lezeau@gmail.com>
2025-05-05 18:20:16 +0100

    More fixing and added a todo

--- Mathlib/Tactic/ImportDiff.lean
+++ Mathlib/Tactic/ImportDiff.lean
@@ -3,8 +3,15 @@ Copyright (c) 2025 Paul Lezeau. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Paul Lezeau
 -/
-import Lean
 import Batteries.Data.List.Basic
+import Mathlib.Init
+
+/-! # The `#import_diff` command
+
+This file implements the `#import_diff` commmand, which is useful for debugging large import changes
+by determining the transitive imports added by a given import.
+
+-/
 
 open Lean Elab Meta
 
@@ -13,6 +20,9 @@ module `foo` is added to the set of imports of the file. -/
 elab "#import_diff" n:ident : command => do
   let name := n.getId
   let env ← MonadEnv.getEnv
+  --TODO(Paul-Lez): perhaps here it would be more sensible to compute the transitive imports
+  --once we have removed `n` from the imports (in order to cover the case where the user
+  --is already importing the `n`).
   let current_all_imports := env.allImportedModuleNames
   let current_imports := env.imports
   let new_imports := current_imports.push { module := name }
