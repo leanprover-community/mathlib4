9ee83c2334845d06ecd94327b1abff38b66524e7
xyzw12345 <wangjt2020@163.com>
2025-05-05 11:12:30 +0800

    add lemmas according to review comments

--- Mathlib/LinearAlgebra/SymmetricAlgebra/MvPolynomial.lean
+++ Mathlib/LinearAlgebra/SymmetricAlgebra/MvPolynomial.lean
@@ -4,27 +4,46 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Raphael Douglas Giles, Zhixuan Dai, Zhenyan Fu, Yiming Fu, Jingting Wang
 -/
 import Mathlib.LinearAlgebra.SymmetricAlgebra.Basic
-import Mathlib.Algebra.MvPolynomial.Eval
+import Mathlib.RingTheory.MvPolynomial.Basic
 
 /-!
 # Main Definitions
 
-1. `SymmetricAlgebra.isoMvPolynomial` give an algebra isomorphism between symmetric algebra over a
+1. `SymmetricAlgebra.equivMvPolynomial` give an algebra isomorphism between symmetric algebra over a
   free module and multivariate polynomial over the basis.
 -/
 
 variable {R M : Type*} [CommSemiring R] [AddCommMonoid M] [Module R M]
 
-/-- `SymmetricAlgebra.isoMvPolynomial` give an algebra isomorphism between symmetric algebra over a
-free module and multivariate polynomial over the basis. -/
-noncomputable def SymmetricAlgebra.isoMvPolynomial {I : Type*} (h : Basis I R M) :
+/-- `SymmetricAlgebra.equivMvPolynomial` give an algebra isomorphism between symmetric algebra over
+a free module and multivariate polynomial over the basis. -/
+noncomputable def SymmetricAlgebra.equivMvPolynomial {I : Type*} (b : Basis I R M) :
     SymmetricAlgebra R M ≃ₐ[R] MvPolynomial I R :=
   .ofAlgHom
-    (SymmetricAlgebra.lift <| Basis.constr h R .X)
-    (MvPolynomial.aeval fun i ↦ ι R M (h i))
+    (SymmetricAlgebra.lift <| Basis.constr b R .X)
+    (MvPolynomial.aeval fun i ↦ ι R M (b i))
     (MvPolynomial.algHom_ext fun i ↦ by simp)
-    (algHom_ext <| h.ext fun i ↦ by simp)
+    (algHom_ext <| b.ext fun i ↦ by simp)
 
-theorem IsSymmetricAlgebra.mvPolynomial (I : Type*) (h : Basis I R M) :
-    IsSymmetricAlgebra (Basis.constr h R (.X : I → MvPolynomial I R)) :=
-  (SymmetricAlgebra.isoMvPolynomial h).bijective
+@[simp]
+lemma SymmetricAlgebra.equivMvPolynomial_ι_apply {I : Type*} (b : Basis I R M) (i : I) :
+    equivMvPolynomial b (ι R M (b i)) = .X (R := R) i :=
+  (SymmetricAlgebra.lift_ι_apply _ _).trans <| by simp
+
+@[simp]
+lemma SymmetricAlgebra.equivMvPolynomial_symm_ι {I : Type*} (b : Basis I R M) (i : I) :
+    (equivMvPolynomial b).symm (MvPolynomial.X i) = ι R M (b i) :=
+  (equivMvPolynomial b).toEquiv.symm_apply_eq.mpr <| equivMvPolynomial_ι_apply b i |>.symm
+
+@[simps! repr_apply]
+noncomputable def _root_.Basis.symmetricAlgebra {I : Type*} (b : Basis I R M) :
+    Basis (I →₀ ℕ) R (SymmetricAlgebra R M) :=
+  (MvPolynomial.basisMonomials I R).map <| (SymmetricAlgebra.equivMvPolynomial b).symm.toLinearEquiv
+
+instance instModuleFree [Module.Free R M] : Module.Free R (SymmetricAlgebra R M) :=
+  let ⟨⟨_κ, b⟩⟩ := Module.Free.exists_basis (R := R) (M := M)
+  .of_basis b.symmetricAlgebra
+
+theorem IsSymmetricAlgebra.mvPolynomial (I : Type*) (b : Basis I R M) :
+    IsSymmetricAlgebra (Basis.constr b R (.X : I → MvPolynomial I R)) :=
+  (SymmetricAlgebra.equivMvPolynomial b).bijective
