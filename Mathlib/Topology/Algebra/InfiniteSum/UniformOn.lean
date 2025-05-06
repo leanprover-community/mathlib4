35e3411577d05682719806e16c5402b64de14d7b
Chris Birkbeck <c.birkbeck@uea.ac.uk>
2025-05-06 17:27:56 +0100

    add David as author

--- Mathlib/Topology/Algebra/InfiniteSum/UniformOn.lean
+++ Mathlib/Topology/Algebra/InfiniteSum/UniformOn.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2025 Chris Birkbeck. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Chris Birkbeck
+Authors: Chris Birkbeck, David Loeffler
 -/
 import Mathlib.Topology.Algebra.InfiniteSum.Defs
 import Mathlib.Topology.Algebra.UniformConvergence
