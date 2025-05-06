3e758bf14cc7399f18059027471a8b40b7a4e711
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-05-06 22:32:40 +0200

    chore: copy one lemma for ENNReal-value function

--- Mathlib/Analysis/SpecialFunctions/Pow/Integral.lean
+++ Mathlib/Analysis/SpecialFunctions/Pow/Integral.lean
@@ -30,12 +30,45 @@ layer cake representation, Cavalieri's principle, tail probability formula
 -/
 
 open Set
+open scoped ENNReal
 
 namespace MeasureTheory
 
 variable {α : Type*} [MeasurableSpace α] (μ : Measure α)
 
 section Layercake
+
+/-- An application of the layer cake formula / Cavalieri's principle / tail probability formula:
+
+For a nonnegative function `f` on a measure space, the Lebesgue integral of `f` can
+be written (roughly speaking) as: `∫⁻ f^p ∂μ = p * ∫⁻ t in 0..∞, t^(p-1) * μ {ω | f(ω) ≥ t}`.
+
+See `MeasureTheory.lintegral_rpow_eq_lintegral_meas_lt_mul_enorm` for a version with sets of the
+form `{ω | f(ω) > t}` instead. -/
+theorem lintegral_rpow_eq_lintegral_meas_le_mul_enorm
+    {f : α → ℝ≥0∞} (hf : AEMeasurable f μ) {p : ℝ} (p_pos : 0 < p) :
+    ∫⁻ ω, f ω ^ p ∂μ =
+      ENNReal.ofReal p *
+        ∫⁻ t in Ioi (0 : ℝ), μ {a : α | ENNReal.ofReal t ≤ f a} * ENNReal.ofReal (t ^ (p - 1)) := by
+  have one_lt_p : -1 < p - 1 := by linarith
+  have obs : ∀ x : ℝ, ∫ t : ℝ in (0)..x, t ^ (p - 1) = x ^ p / p := by
+    intro x
+    rw [integral_rpow (Or.inl one_lt_p)]
+    simp [Real.zero_rpow p_pos.ne.symm]
+  set g := fun t : ℝ => t ^ (p - 1)
+  have g_nn : ∀ᵐ t ∂volume.restrict (Ioi (0 : ℝ)), 0 ≤ g t := by
+    filter_upwards [self_mem_ae_restrict (measurableSet_Ioi : MeasurableSet (Ioi (0 : ℝ)))]
+    intro t t_pos
+    exact Real.rpow_nonneg (mem_Ioi.mp t_pos).le (p - 1)
+  have g_intble (t) (ht : 0 < t) : IntervalIntegrable g volume 0 t :=
+    intervalIntegral.intervalIntegrable_rpow' one_lt_p
+  sorry /- TODO: generalise the lemma below!
+  have key := lintegral_comp_eq_lintegral_meas_le_mul μ hf g_intble g_nn
+  rw [← key, ← lintegral_const_mul'' (ENNReal.ofReal p)] <;> simp_rw [obs]
+  · congr with ω
+    rw [← ENNReal.ofReal_mul p_pos.le, mul_div_cancel₀ (f ω ^ p) p_pos.ne.symm]
+  · have aux := (measurable_const (a := p)).aemeasurable (μ := μ)
+    exact measurable_id.ennreal_ofReal.comp_aemeasurable <| (f_mble.pow aux).div_const p -/
 
 /-- An application of the layer cake formula / Cavalieri's principle / tail probability formula:
 
