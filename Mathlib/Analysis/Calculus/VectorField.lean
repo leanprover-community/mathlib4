e0130f2f51a8c3c44b27e5eec9a122dd00677315
Jens Lindekamp <jens.lindekamp@ista.com>
2025-05-06 16:07:29 +0200

    Added product rule to Lie brackets on vector spaces

--- Mathlib/Analysis/Calculus/VectorField.lean
+++ Mathlib/Analysis/Calculus/VectorField.lean
@@ -107,6 +107,32 @@ lemma lieBracket_smul_right {c : 𝕜} (hW : DifferentiableAt 𝕜 W x) :
   simp only [← differentiableWithinAt_univ, ← lieBracketWithin_univ] at hW ⊢
   exact lieBracketWithin_smul_right hW uniqueDiffWithinAt_univ
 
+/--
+Product rule for Lie Brackets: given two vector fields `V W : E → E` and a function `f : E → 𝕜`,
+we have `[V, f • W] = (df V) • W + f • [V, W]`
+-/
+lemma lieBracketWithin_fmul_right {f : E → 𝕜}
+    (hf : DifferentiableWithinAt 𝕜 f s x)
+    (hW : DifferentiableWithinAt 𝕜 W s x)
+    (hs: UniqueDiffWithinAt 𝕜 s x) :
+    lieBracketWithin 𝕜 V (fun y => f y • W y) s x =
+      (fderivWithin 𝕜 f s x) (V x) • (W x)  + (f • lieBracketWithin 𝕜 V W s) x := by
+  simp [lieBracketWithin, fderivWithin_smul hs hf hW, smul_sub, add_comm, add_sub_assoc]
+
+/--
+Product rule for Lie Brackets: given two vector fields `V W : E → E` and a function `f : E → 𝕜`,
+we have `[V, f • W] = (df V) • W + f • [V, W]`
+-/
+lemma lieBracket_fmul_right {f : E → 𝕜}
+    (hf : DifferentiableAt 𝕜 f x)
+    (hW : DifferentiableAt 𝕜 W x) :
+    lieBracket 𝕜 V (fun y => f y • W y) x =
+      (fderiv 𝕜 f x) (V x) • (W x)  + (f • lieBracket 𝕜 V W) x := by
+  simp only [← differentiableWithinAt_univ, ← lieBracketWithin_univ] at hW ⊢
+  simp only [← differentiableWithinAt_univ, ← lieBracketWithin_univ] at hf ⊢
+  rw [fderiv]
+  exact lieBracketWithin_fmul_right hf hW uniqueDiffWithinAt_univ
+
 lemma lieBracketWithin_add_left (hV : DifferentiableWithinAt 𝕜 V s x)
     (hV₁ : DifferentiableWithinAt 𝕜 V₁ s x) (hs : UniqueDiffWithinAt 𝕜 s x) :
     lieBracketWithin 𝕜 (V + V₁) W s x =
