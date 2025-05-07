ddda61a200631dd851f8186092eb6daff4c92e7c
Yaël Dillies <yael.dillies@gmail.com>
2025-05-07 08:33:08 +0000

    fix

--- Mathlib/Analysis/Calculus/FDeriv/WithLp.lean
+++ Mathlib/Analysis/Calculus/FDeriv/WithLp.lean
@@ -50,25 +50,25 @@ theorem hasFDerivWithinAt_piLp :
 
 namespace PiLp
 
-theorem hasStrictFDerivAt_equiv (f : PiLp p E) :
+theorem hasStrictFDerivAt_ofLp (f : PiLp p E) :
     HasStrictFDerivAt ofLp (ofLpContinuousLinearEquiv p 𝕜 _).toContinuousLinearMap f :=
   .of_isLittleO <| (Asymptotics.isLittleO_zero _ _).congr_left fun _ => (sub_self _).symm
 
-theorem hasStrictFDerivAt_equiv_symm (f : ∀ i, E i) :
+theorem hasStrictFDerivAt_toLp (f : ∀ i, E i) :
     HasStrictFDerivAt (toLp p) (ofLpContinuousLinearEquiv p 𝕜 _).symm.toContinuousLinearMap f :=
   .of_isLittleO <| (Asymptotics.isLittleO_zero _ _).congr_left fun _ => (sub_self _).symm
 
 nonrec theorem hasStrictFDerivAt_apply (f : PiLp p E) (i : ι) :
     HasStrictFDerivAt (𝕜 := 𝕜) (fun f : PiLp p E => f i) (proj p E i) f :=
-  (hasStrictFDerivAt_apply i f).comp f (hasStrictFDerivAt_equiv (𝕜 := 𝕜) p f)
+  (hasStrictFDerivAt_apply i f).comp f (hasStrictFDerivAt_ofLp (𝕜 := 𝕜) p f)
 
-theorem hasFDerivAt_equiv (f : PiLp p E) :
+theorem hasFDerivAt_ofLp (f : PiLp p E) :
     HasFDerivAt ofLp (ofLpContinuousLinearEquiv p 𝕜 _).toContinuousLinearMap f :=
-  (hasStrictFDerivAt_equiv p f).hasFDerivAt
+  (hasStrictFDerivAt_ofLp p f).hasFDerivAt
 
 theorem hasFDerivAt_toLp (f : ∀ i, E i) :
     HasFDerivAt (toLp p) (ofLpContinuousLinearEquiv p 𝕜 _).symm.toContinuousLinearMap f :=
-  (hasStrictFDerivAt_equiv_symm p f).hasFDerivAt
+  (hasStrictFDerivAt_toLp p f).hasFDerivAt
 
 nonrec theorem hasFDerivAt_apply (f : PiLp p E) (i : ι) :
     HasFDerivAt (𝕜 := 𝕜) (fun f : PiLp p E => f i) (proj p E i) f :=
