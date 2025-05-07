2b69436a981b9bc1c0bfb59e5c6586e1c89586f5
Yaël Dillies <yael.dillies@gmail.com>
2025-05-07 08:39:25 +0000

    shake

--- Mathlib/Topology/Algebra/Module/ClosedSubmodule.lean
+++ Mathlib/Topology/Algebra/Module/ClosedSubmodule.lean
@@ -3,7 +3,6 @@ Copyright (c) 2025 Yaël Dillies. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yaël Dillies
 -/
-import Mathlib.Algebra.Module.Submodule.Pointwise
 import Mathlib.Topology.Algebra.Module.LinearMap
 import Mathlib.Topology.Sets.Closeds
 
