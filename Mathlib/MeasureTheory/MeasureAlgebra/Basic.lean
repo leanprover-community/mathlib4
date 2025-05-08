521a9e4aaf54a212b876e4156e59bcf5cd733005
Will Du <duwill1412@gmail.com>
2025-05-08 00:03:35 -0400

    clean up

--- Mathlib/MeasureTheory/MeasureAlgebra/Basic.lean
+++ Mathlib/MeasureTheory/MeasureAlgebra/Basic.lean
@@ -28,33 +28,43 @@ lemma prop321Ba (hd : a ⊓ b = ⊥) : μ (a ⊔ b) = (μ a) + (μ b) := by
     unfold Pairwise Disjoint
     intro i j hi_ne_j x hx_le_si hx_le_sj
     rcases i with _ | _ | i
-    · rcases j with _ | _ | j
-      · exfalso
-        exact hi_ne_j rfl
-      · rw [← hd]
-        exact le_inf hx_le_si hx_le_sj
-      · exact hx_le_sj
-    · rcases j with _ | _ | j
-      · rw [← hd]
-        exact le_inf hx_le_sj hx_le_si
-      · exfalso
-        exact hi_ne_j rfl
-      · exact hx_le_sj
-    · exact hx_le_si
+    repeat {
+      rcases j with _ | _ | j
+      repeat {
+        try { exfalso; exact hi_ne_j rfl }
+        try { rw [← hd]; exact le_inf hx_le_si hx_le_sj }
+        try { rw [← hd]; exact le_inf hx_le_sj hx_le_si }
+        try { exact hx_le_sj }
+      }
+    }
+    exact hx_le_si
+  -- how to coerce
   have hdisjoint : μ (⨆i s) = ∑' (i : ℕ), μ (s i) := by
     exact MeasureAlgebraMeasure.disjoint μ s hpd
+  have ne_0_ne_1_eq_bot {x : ℕ} (h0 : x ≠ 0) (h1 : x ≠ 1) : s x = ⊥ := by
+    unfold s
+    -- how?
+    simp_all [↓reduceIte]
   have hsum : ∑' i, μ (s i) = (μ a) + (μ b) := by
-    let d : Finset ℕ := ({0, 1} : Set ℕ).toFinset
+    let d : Finset ℕ := {0, 1}
     have hf : ∀ x ∉ d, μ (s x) = 0 := by
       intro x hx_not_mem_d
-      have temp := Finset.forall_mem_not_eq.mpr hx_not_mem_d
-      have temp2 := (temp 0)
-      -- what does this do?
-      simp_all [↓reduceIte, s, d]
+      have h0_in_d : 0 ∈ d := by
+        rw [Finset.mem_insert]
+        left
+        rfl
+      have h1_in_d : 1 ∈ d := by
+        rw [Finset.mem_insert]
+        right
+        exact Finset.mem_singleton.mpr rfl
+      have hmem_d_not_eq := (Finset.forall_mem_not_eq.mpr hx_not_mem_d)
+      rw [ne_0_ne_1_eq_bot ((hmem_d_not_eq 0) h0_in_d) ((hmem_d_not_eq 1) h1_in_d)]
       exact μ.zero
     rw [tsum_eq_sum hf]
-    -- what
-    simp_all only [Set.toFinset_insert, Set.toFinset_singleton, Finset.mem_insert, Finset.mem_singleton, not_or, ↓reduceIte, and_imp, zero_ne_one, not_false_eq_true, Finset.sum_insert, Finset.sum_singleton, one_ne_zero, d, s]
+    have h0_not_mem_1 : 0 ∉ ({1} : Finset ℕ) := by
+      rw [Finset.mem_singleton]; exact zero_ne_one
+    rw [Finset.sum_insert h0_not_mem_1, Finset.sum_singleton]
+    rfl
   rw [hsup, hdisjoint, hsum]
 
 lemma prop321Bb (hab : a ≤ b) : μ a ≤ μ b := by
