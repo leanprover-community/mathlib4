de5e8ab233b05f2f10c7f8d8ea59b0fa26f8f390
Martin Dvorak <martin.dvorak@matfyz.cz>
2025-05-09 16:58:18 +0200

    feat(TODO where?): inverting `Matrix` inverts its `LinearEquiv`

--- Mathlib.lean
+++ Mathlib.lean
@@ -6327,3 +6327,8 @@ import Mathlib.Util.TermReduce
 import Mathlib.Util.TransImports
 import Mathlib.Util.WhatsNew
 import Mathlib.Util.WithWeakNamespace
+
+lemma LinearEquiv.toMatrix'_inv {X R : Type*} [Fintype X] [DecidableEq X] [CommRing R]
+    (f : (X → R) ≃ₗ[R] (X → R)) :
+    f.toMatrix'⁻¹ = f⁻¹.toMatrix' :=
+  Matrix.inv_eq_right_inv (by simpa using (f.toLinearMap.toMatrix'_comp f.symm.toLinearMap).symm)
