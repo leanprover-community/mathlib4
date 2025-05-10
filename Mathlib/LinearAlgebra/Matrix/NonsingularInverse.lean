8c3f12e4c53cf3764822e46150bf61011ce51ec3
Martin Dvorak <martin.dvorak@matfyz.cz>
2025-05-10 16:14:02 +0200

    found home

--- Mathlib/LinearAlgebra/Matrix/NonsingularInverse.lean
+++ Mathlib/LinearAlgebra/Matrix/NonsingularInverse.lean
@@ -737,6 +737,10 @@ theorem inv_kronecker [Fintype m] [DecidableEq m]
   · apply inv_eq_right_inv
     rw [← mul_kronecker_mul, ← one_kronecker_one, mul_nonsing_inv _ hA, mul_nonsing_inv _ hB]
 
+lemma _root_.LinearEquiv.toMatrix'_inv [Fintype m] [DecidableEq m] (f : (m → α) ≃ₗ[α] (m → α)) :
+    f.toMatrix'⁻¹ = f⁻¹.toMatrix' :=
+  inv_eq_right_inv (by simpa using (f.toLinearMap.toMatrix'_comp f.symm.toLinearMap).symm)
+
 
 /-! ### More results about determinants -/
 
