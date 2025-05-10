5c8783e3b9ecc071e97f1a26a60b64ec1fe7a128
101damnations <101damnations@github.com>
2025-05-10 01:46:00 +0100

    im confused...

--- Mathlib/RepresentationTheory/Coinduced.lean
+++ Mathlib/RepresentationTheory/Coinduced.lean
@@ -3,7 +3,6 @@ Copyright (c) 2025 Amelia Livingston. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Amelia Livingston
 -/
-import Mathlib.GroupTheory.GroupAction.Ring
 import Mathlib.RepresentationTheory.Rep
 
 /-!
