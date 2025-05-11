ccee4e2b3011840b26b1d0c8ac699e12483e2149
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-05-11 16:56:15 +0200

    cleaning up

--- Mathlib/CategoryTheory/Types/Monomorphisms.lean
+++ Mathlib/CategoryTheory/Types/Monomorphisms.lean
@@ -20,11 +20,9 @@ in the file `CategoryTheory.MorphismProperty.Retract`.)
 
 universe v' u' u
 
-namespace CategoryTheory
+namespace CategoryTheory.Types
 
-open MorphismProperty Limits Types
-
-namespace Types
+open MorphismProperty Limits
 
 instance : (monomorphisms (Type u)).IsStableUnderCobaseChange where
   of_isPushout {X₁ X₂ X₃ X₄ t l r b} sq ht := by
@@ -39,17 +37,15 @@ instance : MorphismProperty.IsStableUnderFilteredColimits.{v', u'} (monomorphism
     intro x₁ y₁ h
     obtain ⟨j, x₁, y₁, rfl, rfl⟩ : ∃ (j : J) (x₁' y₁' : F₁.obj j),
         x₁ = c₁.ι.app j x₁' ∧ y₁ = c₁.ι.app j y₁' := by
-      obtain ⟨j, x₁, rfl⟩ := jointly_surjective_of_isColimit hc₁ x₁
-      obtain ⟨l, y₁, rfl⟩ := jointly_surjective_of_isColimit hc₁ y₁
+      obtain ⟨j, x₁, rfl⟩ := Types.jointly_surjective_of_isColimit hc₁ x₁
+      obtain ⟨l, y₁, rfl⟩ := Types.jointly_surjective_of_isColimit hc₁ y₁
       exact ⟨_,  _, _, congr_fun (c₁.w (IsFiltered.leftToMax j l)).symm _,
         congr_fun (c₁.w (IsFiltered.rightToMax j l)).symm _⟩
     rw [hφ, hφ] at h
-    obtain ⟨k, α, hk⟩ := (FilteredColimit.isColimit_eq_iff' hc₂ _ _).1 h
+    obtain ⟨k, α, hk⟩ := (Types.FilteredColimit.isColimit_eq_iff' hc₂ _ _).1 h
     simp only [← FunctorToTypes.naturality] at hk
     rw [← c₁.w α, types_comp_apply, types_comp_apply, hf _ hk]⟩
 
 example : IsStableUnderTransfiniteComposition.{u'} (monomorphisms (Type u)) where
 
-end Types
-
-end CategoryTheory
+end CategoryTheory.Types
