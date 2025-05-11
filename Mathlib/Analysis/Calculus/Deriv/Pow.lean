448df181f458518da673046deee6ed9507bfab18
Eric Wieser <wieser.eric@gmail.com>
2025-05-11 00:29:04 +0000

    shake more

--- Mathlib/Analysis/Calculus/Deriv/Pow.lean
+++ Mathlib/Analysis/Calculus/Deriv/Pow.lean
@@ -3,7 +3,6 @@ Copyright (c) 2019 Sébastien Gouëzel. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Sébastien Gouëzel
 -/
-import Mathlib.Analysis.Calculus.Deriv.Mul
 import Mathlib.Analysis.Calculus.FDeriv.Pow
 
 /-!
