7df5f46872823c48749ce35966a18995dc17c355
Yaël Dillies <yael.dillies@gmail.com>
2025-05-11 07:23:33 +0000

    golf from Toric

--- Mathlib/Algebra/Order/Nonneg/Module.lean
+++ Mathlib/Algebra/Order/Nonneg/Module.lean
@@ -17,11 +17,12 @@ These instances are useful for working with `ConvexCone`.
 -/
 
 variable {𝕜 𝕜' E : Type*}
-variable [Semiring 𝕜] [PartialOrder 𝕜]
 
 local notation3 "𝕜≥0" => {c : 𝕜 // 0 ≤ c}
 
 namespace Nonneg
+section Semiring
+variable [Semiring 𝕜] [PartialOrder 𝕜]
 
 section SMul
 
@@ -78,29 +79,23 @@ instance instModule : Module 𝕜≥0 E :=
   Module.compHom E Nonneg.coeRingHom
 
 end Module
+end Semiring
 
 section Ring
+variable [Ring 𝕜] [LinearOrder 𝕜] [IsOrderedRing 𝕜] [AddCommMonoid E] [Module 𝕜 E]
 
-variable (𝕜 : Type*) [Ring 𝕜] [LinearOrder 𝕜] [IsOrderedRing 𝕜] [AddCommMonoid E] [Module 𝕜 E]
-
-private instance isFiniteOver : Module.Finite {c : 𝕜 // 0 ≤ c} 𝕜 := by
-  rw [Module.finite_def, Submodule.fg_def]
-  refine ⟨{1, -1}, by simp, ?_⟩
-  rw [Submodule.eq_top_iff']
-  intro x
-  by_cases hx : 0 ≤ x
-  · have x_eq_smul : x = (⟨x, hx⟩ : {c : 𝕜 // 0 ≤ c}) • 1 := by simp
-    rw [x_eq_smul]
-    exact Submodule.smul_mem _ _ (Submodule.subset_span (Set.mem_insert 1 {-1}))
-  · have x_eq_smul : x = (⟨-x, neg_nonneg.mpr (not_le.mp hx).le⟩ : {c : 𝕜 // 0 ≤ c}) • -1 := by simp
-    rw [x_eq_smul]
-    exact Submodule.smul_mem _ _ (Submodule.subset_span (Set.mem_insert_of_mem 1 rfl))
+private instance instModuleFiniteAux : Module.Finite 𝕜≥0 𝕜 := by
+  simp_rw [Module.finite_def, Submodule.fg_def, Submodule.eq_top_iff']
+  refine ⟨{1, -1}, by simp, fun x ↦ ?_⟩
+  obtain hx | hx := le_total 0 x
+  · simpa using Submodule.smul_mem (M := 𝕜) (.span 𝕜≥0 {1, -1}) ⟨x, hx⟩ (x := 1)
+      (Submodule.subset_span <| by simp)
+  · simpa using Submodule.smul_mem (M := 𝕜) (.span 𝕜≥0 {1, -1}) ⟨-x, neg_nonneg.2 hx⟩ (x := -1)
+      (Submodule.subset_span <| by simp)
 
 /-- If a module is finite over a linearly ordered ring, then it is also finite over the non-negative
 scalars. -/
-instance instModuleFinite [h : Module.Finite 𝕜 E] : Module.Finite {c : 𝕜 // 0 ≤ c} E :=
-  .trans 𝕜 E
+instance instModuleFinite [Module.Finite 𝕜 E] : Module.Finite 𝕜≥0 E := .trans 𝕜 E
 
 end Ring
-
 end Nonneg
