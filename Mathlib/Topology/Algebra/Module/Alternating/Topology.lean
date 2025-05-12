19e78bc64ab7642c88f3299b7af11e6315749955
Yury G. Kudryashov <urkud@urkud.name>
2025-05-11 23:52:42 -0500

    Fix

--- Mathlib/Topology/Algebra/Module/Alternating/Topology.lean
+++ Mathlib/Topology/Algebra/Module/Alternating/Topology.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yury Kudryashov
 -/
 import Mathlib.Topology.Algebra.Module.Multilinear.Topology
-import Mathlib.Topology.Algebra.Module.Alternating.Curry
+import Mathlib.Topology.Algebra.Module.Alternating.Basic
 
 /-!
 # Topology on continuous alternating maps
