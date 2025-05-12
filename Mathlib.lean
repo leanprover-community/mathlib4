9e281937e84dcbfa09be06057a9b3a1e36a6893f
Yury G. Kudryashov <urkud@urkud.name>
2025-05-12 15:03:36 +0000

    feat: define `Path.segment` (#24761)
    Extract the definition of `Path.segment` from `JoinedIn.of_segment_subset`
    and prove basic properties of this definition.

--- Mathlib.lean
+++ Mathlib.lean
@@ -1491,6 +1491,7 @@ import Mathlib.Analysis.Convex.KreinMilman
 import Mathlib.Analysis.Convex.Measure
 import Mathlib.Analysis.Convex.Mul
 import Mathlib.Analysis.Convex.PartitionOfUnity
+import Mathlib.Analysis.Convex.PathConnected
 import Mathlib.Analysis.Convex.Piecewise
 import Mathlib.Analysis.Convex.Quasiconvex
 import Mathlib.Analysis.Convex.Radon
