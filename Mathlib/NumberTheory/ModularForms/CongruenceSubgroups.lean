6106ba13cd3e4d48afe0737ae675f202320d83c8
David Loeffler <d.loeffler.01@cantab.net>
2025-05-13 00:10:17 +0200

    shake

--- Mathlib/NumberTheory/ModularForms/CongruenceSubgroups.lean
+++ Mathlib/NumberTheory/ModularForms/CongruenceSubgroups.lean
@@ -3,7 +3,8 @@ Copyright (c) 2022 Chris Birkbeck. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Chris Birkbeck
 -/
-import Mathlib.Analysis.Complex.UpperHalfPlane.MoebiusAction
+import Mathlib.Data.Real.Basic
+import Mathlib.LinearAlgebra.Matrix.GeneralLinearGroup.Defs
 
 /-!
 # Congruence subgroups
