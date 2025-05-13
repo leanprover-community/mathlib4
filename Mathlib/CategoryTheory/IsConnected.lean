0f13cc8031cb8f522d46e408ccf0dd1a1e514053
Robin Carlier <57142648+robin-carlier@users.noreply.github.com>
2025-05-13 21:11:51 +0200

    use ambient var

--- Mathlib/CategoryTheory/IsConnected.lean
+++ Mathlib/CategoryTheory/IsConnected.lean
@@ -457,10 +457,10 @@ theorem nonempty_hom_of_preconnected_groupoid {G} [Groupoid G] [IsPreconnected G
 
 attribute [instance] nonempty_hom_of_preconnected_groupoid
 
-instance PreconnectedOfSubsingleton {J : Type*} [Category J] [Subsingleton J] :
+instance PreconnectedOfSubsingleton [Subsingleton J] :
     IsPreconnected J where
   iso_constant {α} F j := ⟨NatIso.ofComponents (fun x ↦ eqToIso (by simp [Subsingleton.allEq x j]))⟩
 
-instance {J : Type*} [Category J] [Nonempty J] [Subsingleton J] : IsConnected J where
+instance [Nonempty J] [Subsingleton J] : IsConnected J where
 
 end CategoryTheory
