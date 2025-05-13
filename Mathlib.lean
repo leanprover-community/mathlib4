e47fe09759da42731ed25a9355b4c47a0fb992d9
pfaffelh <pfaffelh@gmail.com>
2025-05-13 09:54:48 +0200

    new file BM2

--- Mathlib.lean
+++ Mathlib.lean
@@ -5199,6 +5199,7 @@ import Mathlib.Probability.ProbabilityMassFunction.Constructions
 import Mathlib.Probability.ProbabilityMassFunction.Integrals
 import Mathlib.Probability.ProbabilityMassFunction.Monad
 import Mathlib.Probability.Process.Adapted
+import Mathlib.Probability.Process.BrownianMotion
 import Mathlib.Probability.Process.Filtration
 import Mathlib.Probability.Process.FiniteDimensionalLaws
 import Mathlib.Probability.Process.HittingTime
