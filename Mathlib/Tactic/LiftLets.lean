52d7a8d4c1c4d8481ec702bb2035b8c003631d9d
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-05-14 08:52:14 +0200

    Line length

--- Mathlib/Tactic/LiftLets.lean
+++ Mathlib/Tactic/LiftLets.lean
@@ -6,5 +6,5 @@ Authors: Kyle Miller
 import Mathlib.Tactic.Basic
 import Mathlib.Tactic.Linter.DeprecatedModule
 
-deprecated_module "The lift_lets tactic was moved to Lean core; you can probably just remove this import"
-  (since := "2025-05-02")
+deprecated_module "The lift_lets tactic was moved to Lean core; \
+  you can probably just remove this import" (since := "2025-05-02")
