0d2d1b5b5b5b24ce1df38a7e9a143ea4cf0e3cec
mariainesdff <mariaines.dff@gmail.com>
2025-05-14 15:40:08 +0200

    suggested changes

--- Mathlib.lean
+++ Mathlib.lean
@@ -1663,9 +1663,9 @@ import Mathlib.Analysis.Normed.Ring.Lemmas
 import Mathlib.Analysis.Normed.Ring.Ultra
 import Mathlib.Analysis.Normed.Ring.Units
 import Mathlib.Analysis.Normed.Ring.WithAbs
-import Mathlib.Analysis.Normed.Unbundled.AlgNormOfAut
 import Mathlib.Analysis.Normed.Unbundled.AlgebraNorm
 import Mathlib.Analysis.Normed.Unbundled.FiniteExtension
+import Mathlib.Analysis.Normed.Unbundled.InvariantExtension
 import Mathlib.Analysis.Normed.Unbundled.IsPowMulFaithful
 import Mathlib.Analysis.Normed.Unbundled.RingSeminorm
 import Mathlib.Analysis.Normed.Unbundled.SeminormFromBounded
