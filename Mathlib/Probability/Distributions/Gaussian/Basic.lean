29676a32a4c30013d7d8c3d4c6ed03a9bafdb236
Remy Degenne <remydegenne@gmail.com>
2025-05-14 13:04:19 +0200

    feat: define Gaussian measures in normed spaces

---
+++ Mathlib/Probability/Distributions/Gaussian/Basic.lean
@@ -0,0 +1,138 @@
+/-
+Copyright (c) 2025 Rémy Degenne. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Rémy Degenne
+-/
+import Mathlib.Probability.Distributions.Gaussian
+
+/-!
+# Gaussian distributions in Banach spaces
+
+We introduce a predicate `IsGaussian` for measures on a Banach space `E` such that the map by
+any continuous linear form is a Gaussian measure on `ℝ`.
+
+For Gaussian distributions in `ℝ`, see the file `Mathlib.Probability.Distributions.Gaussian`.
+
+## Main definitions
+
+* `IsGaussian`: a measure `μ` is Gaussian if its map by every continuous linear form
+  `L : E →L[ℝ] ℝ` is a real Gaussian measure.
+  That is, `μ.map L = gaussianReal (μ[L]) (Var[L; μ]).toNNReal`.
+
+## Main statements
+
+* `isGaussian_iff_charFunCLM_eq`: a finite measure `μ` is Gaussian if and only if
+  its characteristic function has value `exp (μ[L] * I - Var[L; μ] / 2)` for every
+  continuous linear form `L : E →L[ℝ] ℝ`.
+
+## References
+
+* [Martin Hairer, *An introduction to stochastic PDEs*][hairer2009introduction]
+
+-/
+
+open MeasureTheory Complex
+open scoped ENNReal NNReal
+
+namespace ProbabilityTheory
+
+/-- A measure is Gaussian if its map by every continuous linear form is a real Gaussian measure. -/
+class IsGaussian {E : Type*} [TopologicalSpace E] [AddCommMonoid E] [Module ℝ E]
+  {mE : MeasurableSpace E} (μ : Measure E) : Prop where
+  map_eq_gaussianReal (L : E →L[ℝ] ℝ) : μ.map L = gaussianReal (μ[L]) (Var[L; μ]).toNNReal
+
+/-- A real Gaussian measure is Gaussian. -/
+instance isGaussian_gaussianReal (m : ℝ) (v : ℝ≥0) : IsGaussian (gaussianReal m v) where
+  map_eq_gaussianReal L := by
+    rw [gaussianReal_map_continuousLinearMap]
+    simp only [integral_continuousLinearMap_gaussianReal, variance_continuousLinearMap_gaussianReal,
+      Real.coe_toNNReal']
+    congr
+    rw [Real.toNNReal_mul (by positivity), Real.toNNReal_coe]
+    congr
+    simp only [left_eq_sup]
+    positivity
+
+variable {E F : Type*} [NormedAddCommGroup E] [NormedSpace ℝ E] [MeasurableSpace E] [BorelSpace E]
+  [NormedAddCommGroup F] [NormedSpace ℝ F] [MeasurableSpace F] [BorelSpace F]
+  {μ : Measure E} [IsGaussian μ]
+
+/-- Dirac measures are Gaussian. -/
+instance {x : E} : IsGaussian (Measure.dirac x) where
+  map_eq_gaussianReal L := by rw [Measure.map_dirac (by fun_prop)]; simp
+
+/-- A Gaussian measure is a probability measure. -/
+instance : IsProbabilityMeasure μ where
+  measure_univ := by
+    let L : E →L[ℝ] ℝ := Nonempty.some inferInstance
+    have : μ.map L Set.univ = 1 := by simp [IsGaussian.map_eq_gaussianReal L]
+    simpa [Measure.map_apply (by fun_prop : Measurable L) .univ] using this
+
+lemma IsGaussian.memLp_continuousLinearMap (μ : Measure E) [IsGaussian μ] (L : E →L[ℝ] ℝ)
+    (p : ℝ≥0∞) (hp : p ≠ ∞) :
+    MemLp L p μ := by
+  suffices MemLp (id ∘ L) p μ from this
+  rw [← memLp_map_measure_iff (by fun_prop) (by fun_prop), IsGaussian.map_eq_gaussianReal L]
+  convert memLp_id_gaussianReal p.toNNReal
+  simp [hp]
+
+@[fun_prop]
+lemma IsGaussian.integrable_continuousLinearMap (μ : Measure E) [IsGaussian μ] (L : E →L[ℝ] ℝ) :
+    Integrable L μ := by
+  rw [← memLp_one_iff_integrable]
+  exact IsGaussian.memLp_continuousLinearMap μ L 1 (by simp)
+
+/-- The map of a Gaussian measure by a continuous linear map is Gaussian. -/
+instance isGaussian_map (L : E →L[ℝ] F) : IsGaussian (μ.map L) where
+  map_eq_gaussianReal L' := by
+    rw [Measure.map_map (by fun_prop) (by fun_prop)]
+    change Measure.map (L'.comp L) μ = _
+    rw [IsGaussian.map_eq_gaussianReal (L'.comp L)]
+    congr
+    · rw [integral_map (by fun_prop) (by fun_prop)]
+      simp
+    · rw [← variance_id_map (by fun_prop)]
+      conv_rhs => rw [← variance_id_map (by fun_prop)]
+      rw [Measure.map_map (by fun_prop) (by fun_prop)]
+      simp
+
+instance isGaussian_map_equiv (L : E ≃L[ℝ] F) : IsGaussian (μ.map L) :=
+  isGaussian_map (L : E →L[ℝ] F)
+
+section CharFunCLM
+
+/-- The characteristic function of a Gaussian measure `μ` has value
+`exp (μ[L] * I - Var[L; μ] / 2)` on `L : E →L[ℝ] ℝ`. -/
+lemma IsGaussian.charFunCLM_eq {μ : Measure E} [IsGaussian μ] (L : E →L[ℝ] ℝ) :
+    charFunCLM μ L = exp (μ[L] * I - Var[L; μ] / 2) := by
+  calc charFunCLM μ L
+  _ = charFun (μ.map L) 1 := by rw [charFunCLM_eq_charFun_map_one]
+  _ = charFun (gaussianReal (μ[L]) (Var[L; μ]).toNNReal) 1 := by
+    rw [IsGaussian.map_eq_gaussianReal L]
+  _ = exp (μ[L] * I - Var[L; μ] / 2) := by
+    rw [charFun_gaussianReal]
+    simp only [ofReal_one, one_mul, Real.coe_toNNReal', one_pow, mul_one]
+    congr
+    · rw [integral_complex_ofReal]
+    · simp only [sup_eq_left]
+      exact variance_nonneg _ _
+
+/-- A finite measure is Gaussian iff its characteristic function has value
+`exp (μ[L] * I - Var[L; μ] / 2)` for every `L : E →L[ℝ] ℝ`. -/
+theorem isGaussian_iff_charFunCLM_eq {μ : Measure E} [IsFiniteMeasure μ] :
+    IsGaussian μ ↔ ∀ L : E →L[ℝ] ℝ, charFunCLM μ L = exp (μ[L] * I - Var[L; μ] / 2) := by
+  refine ⟨fun h ↦ h.charFunCLM_eq, fun h ↦ ⟨fun L ↦ Measure.ext_of_charFun ?_⟩⟩
+  ext u
+  rw [charFun_map_eq_charFunCLM_smul L u, h (u • L), charFun_gaussianReal]
+  simp only [ContinuousLinearMap.coe_smul', Pi.smul_apply, smul_eq_mul, ofReal_mul,
+    Real.coe_toNNReal']
+  congr
+  · rw [integral_const_mul, integral_complex_ofReal]
+  · rw [max_eq_left (variance_nonneg _ _), mul_comm, ← ofReal_pow, ← ofReal_mul, ← variance_mul]
+    congr
+
+alias ⟨_, isGaussian_of_charFunCLM_eq⟩ := isGaussian_iff_charFunCLM_eq
+
+end CharFunCLM
+
+end ProbabilityTheory
