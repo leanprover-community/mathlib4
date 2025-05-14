f558b649f8126d230c7e24b69723626aca8209d4
Christopher Hoskin <christopher.hoskin@gmail.com>
2025-05-14 19:46:09 +0100

    rootsOfUnityAddChar

--- Mathlib/Analysis/SpecialFunctions/Complex/CircleAddChar.lean
+++ Mathlib/Analysis/SpecialFunctions/Complex/CircleAddChar.lean
@@ -91,9 +91,14 @@ lemma isPrimitive_stdAddChar (N : ℕ) [NeZero N] :
   rwa [← (stdAddChar (N := N)).map_zero_eq_one, injective_stdAddChar.eq_iff] at ht
 
 /-- `ZMod.toCircle` as an `AddChar` into `rootsOfUnity n Circle`. -/
-noncomputable def expHom (n : ℕ) [NeZero n] : AddChar (ZMod n) (rootsOfUnity n Circle) where
+noncomputable def rootsOfUnityAddChar (n : ℕ) [NeZero n] :
+    AddChar (ZMod n) (rootsOfUnity n Circle) where
   toFun x := ⟨toUnits (ZMod.toCircle x), by ext; simp [← AddChar.map_nsmul_eq_pow]⟩
   map_zero_eq_one' := by simp
   map_add_eq_mul' _ _:= by ext; simp [AddChar.map_add_eq_mul]
+
+@[simp] lemma rootsOfUnityAddChar_val (n : ℕ) [NeZero n] (x : ZMod n) :
+    (rootsOfUnityAddChar n x).val = toCircle x := by
+  rfl
 
 end ZMod
