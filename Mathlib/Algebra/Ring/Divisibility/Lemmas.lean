e4f0822700db086ab323b557dbe81f35f362bec1
Xavier Roblot <46200072+xroblot@users.noreply.github.com>
2025-05-15 12:12:20 +0200

    Update Lemmas.lean

--- Mathlib/Algebra/Ring/Divisibility/Lemmas.lean
+++ Mathlib/Algebra/Ring/Divisibility/Lemmas.lean
@@ -89,7 +89,6 @@ lemma add_pow_dvd_pow_of_pow_eq_zero_left (hp : n + m ≤ p + 1) (h_comm : Commu
     (hy : y ^ n = 0) : (x + y) ^ m ∣ x ^ p :=
   add_comm x y ▸ h_comm.symm.add_pow_dvd_pow_of_pow_eq_zero_right hp hy
 
-
 end Ring
 
 end Commute
