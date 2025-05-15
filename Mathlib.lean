a5aeeabbc5a034e2ad3cad5c413a9d496ded3324
Dagur Asgeirsson <dagurtomas@gmail.com>
2025-05-15 09:40:06 +0000

    mk_all

--- Mathlib.lean
+++ Mathlib.lean
@@ -2281,6 +2281,7 @@ import Mathlib.CategoryTheory.Localization.HomEquiv
 import Mathlib.CategoryTheory.Localization.Linear
 import Mathlib.CategoryTheory.Localization.LocalizerMorphism
 import Mathlib.CategoryTheory.Localization.LocallySmall
+import Mathlib.CategoryTheory.Localization.Monoidal
 import Mathlib.CategoryTheory.Localization.Monoidal.Basic
 import Mathlib.CategoryTheory.Localization.Monoidal.Braided
 import Mathlib.CategoryTheory.Localization.Opposite
