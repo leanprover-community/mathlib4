9e7d035eaa9148e5aa81d80aabcf12c1c1b84004
Johan Commelin <johan@commelin.net>
2025-05-15 09:40:37 +0000

    chore: remove `@[simp]` from lemmas that `simp [*]` can prove (#24926)

--- Mathlib/Algebra/GroupWithZero/Defs.lean
+++ Mathlib/Algebra/GroupWithZero/Defs.lean
@@ -205,7 +205,8 @@ variable [GroupWithZero G₀] {a : G₀}
 
 @[simp] lemma inv_zero : (0 : G₀)⁻¹ = 0 := GroupWithZero.inv_zero
 
-@[simp] lemma mul_inv_cancel₀ (h : a ≠ 0) : a * a⁻¹ = 1 := GroupWithZero.mul_inv_cancel a h
+@[simp]
+lemma mul_inv_cancel₀ (h : a ≠ 0) : a * a⁻¹ = 1 := GroupWithZero.mul_inv_cancel a h
 
 -- See note [lower instance priority]
 instance (priority := 100) GroupWithZero.toMulDivCancelClass : MulDivCancelClass G₀ where
