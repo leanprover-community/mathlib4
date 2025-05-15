d8a7ea472385ecf15226a2484a54b90a0a4bd7c0
Salvatore Mercuri <smercuri@ed.ac.uk>
2025-05-15 09:30:48 +0100

    Fix indentation

--- Mathlib/NumberTheory/NumberField/InfinitePlace/Basic.lean
+++ Mathlib/NumberTheory/NumberField/InfinitePlace/Basic.lean
@@ -95,18 +95,18 @@ theorem norm_embedding_eq (w : InfinitePlace K) (x : K) :
 
 variable (K) in
 theorem embedding_injective : (embedding (K := K)).Injective := by
-   intro v₁ v₂ h
-   rw [← mk_embedding v₁, h, mk_embedding]
+  intro v₁ v₂ h
+  rw [← mk_embedding v₁, h, mk_embedding]
 
 variable (K) in
 theorem conjugate_embedding_injective :
-     (fun (v : InfinitePlace K) => ComplexEmbedding.conjugate v.embedding).Injective :=
-   star_injective.comp <| embedding_injective K
+    (fun (v : InfinitePlace K) => ComplexEmbedding.conjugate v.embedding).Injective :=
+  star_injective.comp <| embedding_injective K
 
 variable (K) in
 theorem eq_of_embedding_eq_conjugate {v₁ v₂ : InfinitePlace K}
-     (h : v₁.embedding = ComplexEmbedding.conjugate v₂.embedding) : v₁ = v₂ := by
-   rw [← mk_embedding v₁, h, mk_conjugate_eq, mk_embedding]
+    (h : v₁.embedding = ComplexEmbedding.conjugate v₂.embedding) : v₁ = v₂ := by
+  rw [← mk_embedding v₁, h, mk_conjugate_eq, mk_embedding]
 
 theorem eq_iff_eq (x : K) (r : ℝ) : (∀ w : InfinitePlace K, w x = r) ↔ ∀ φ : K →+* ℂ, ‖φ x‖ = r :=
   ⟨fun hw φ => hw (mk φ), by rintro hφ ⟨w, ⟨φ, rfl⟩⟩; exact hφ φ⟩
