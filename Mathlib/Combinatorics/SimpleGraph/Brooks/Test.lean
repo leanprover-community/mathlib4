55eba4793e5706ca8ce2cc3ff8e7d0f1d05208ed
jt496 <john.talbot@gmail.com>
2025-05-15 13:20:02 +0100

    with trace

--- Mathlib/Combinatorics/SimpleGraph/Brooks/Test.lean
+++ Mathlib/Combinatorics/SimpleGraph/Brooks/Test.lean
@@ -2,13 +2,21 @@ import Mathlib.Data.Finset.Basic
 
 variable {α : Type*} {a : α}
 
-def Null (f : Set α → Set α) (s : Set α) [Nonempty (f s)] : ℕ := 0
+def null (f : Set α → ℕ) (s : Set α) [NeZero (f s)] : ℕ := 0
 
-lemma Null_set (f : Set α → Set α) (s : Set α) (_h : a ∈ s) [Nonempty (f s)] : Null f s = 0 := rfl
-
-lemma Null_finset (f : Set α → Set α) (s : Finset α) (h : a ∈ s) [Nonempty (f s)] :
-    Null f s = 0 := by
-  exact Null_set f _ h  -- failed to synthesize Nonempty ↑(f (Membership.mem s.val))
--- exact Null_set f s h            -- works
--- exact Null_set f _ (by exact h) -- works
--- rfl                             -- works
+lemma null_set (f : Set α → ℕ) (s : Set α) (_h : a ∈ s) [NeZero (f s)] : null f s = 0 := rfl
+-- set_option trace.Meta.synthInstance true
+-- set_option trace.Meta.isDefEq true
+-- set_option trace.Elab.step true
+-- set_option trace.Elab.app.args true
+-- set_option trace.Elab.app.propagateExpectedType true
+-- set_option trace.Elab.step.result true
+set_option trace.Elab.app true
+lemma null_finset (f : Set α → ℕ) (s : Finset α) (h : a ∈ s) [NeZero (f s)] :
+    null f s = 0 := by
+sorry
+--  exact null_set f _ h  -- failed to synthesize NeZero (f (Membership.mem s.val))
+-- exact null_set f s h            -- Goals accomplished!
+-- exact null_set _ _ (by exact h) -- Goals accomplished!
+-- exact @null_set _ _ _ _ h ‹_›   -- Goals accomplished!
+-- rfl                             -- Goals accomplished!
