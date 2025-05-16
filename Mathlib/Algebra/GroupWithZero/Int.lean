aaafbe2d8f56b80e94fa8f909de99953484c90c1
faenuccio <filippo.nuccio@univ-st-etienne.fr>
2025-05-16 16:43:48 +0200

    trying to move lemmas on Zm0

--- Mathlib/Algebra/GroupWithZero/Int.lean
+++ Mathlib/Algebra/GroupWithZero/Int.lean
@@ -5,6 +5,7 @@ Authors: María Inés de Frutos-Fernández, Filippo A. E. Nuccio
 -/
 import Mathlib.Algebra.Group.Int.TypeTags
 import Mathlib.Algebra.GroupWithZero.WithZero
+import Mathlib.GroupTheory.SpecificGroups.Cyclic
 
 /-!
 # Lemmas about `ℤₘ₀`.
@@ -28,3 +29,37 @@ theorem ofAdd_neg_one_pow_comm (a : ℤ) (n : ℕ) :
   rw [← zpow_natCast, zpow_comm, ← ofAdd_zpow]
 
 end WithZero
+
+#min_imports
+
+#exit
+
+-- open scoped Multiplicative
+
+-- **CHECK FROM HERE ON**
+open Multiplicative
+
+instance : IsCyclic ℤₘ₀ˣ :=
+  isCyclic_of_surjective WithZero.unitsWithZeroEquiv.symm (MulEquiv.surjective _)
+
+instance : Nontrivial ℤₘ₀ˣ :=
+  Function.Surjective.nontrivial (f := WithZero.unitsWithZeroEquiv) (MulEquiv.surjective _)
+
+open Subgroup
+lemma top_eq_zpowers_neg_one :
+    zpowers (ofAdd (-1 : ℤ)) = (⊤ : Subgroup (Multiplicative ℤ)) := by
+  rw [← coe_eq_univ, ← ofAdd_image_zmultiples_eq_zpowers_ofAdd]
+  simp
+
+open LinearOrderedCommGroup WithZero in
+lemma genLTOne_eq_neg_one : unitsWithZeroEquiv.symm (ofAdd (-1 : ℤ)) = (genLTOne (ℤₘ₀ˣ)) :=  by
+  let e := (unitsWithZeroEquiv (α := Multiplicative ℤ)).symm
+  refine genLTOne_unique (e (ofAdd (-1 : ℤ))) ⟨?_, ?_⟩
+  · simpa only [Int.reduceNeg, ofAdd_neg, map_inv, Left.inv_lt_one_iff] using
+      compareOfLessAndEq_eq_lt.mp rfl
+  rw [← map_top_of_surjective e.toMonoidHom (MulEquiv.surjective _), ← top_eq_zpowers_neg_one,
+    MonoidHom.map_zpowers]
+  rfl
+
+
+end Multiplicative
