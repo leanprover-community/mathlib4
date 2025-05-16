1fba76c11b0a39187345e4038e3c88bedcd1e41a
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2025-05-16 20:06:54 +0800

    rearrange lemmas

--- Mathlib/RingTheory/CohenMacaulay/Basic.lean
+++ Mathlib/RingTheory/CohenMacaulay/Basic.lean
@@ -36,41 +36,16 @@ section IsLocalization
 variable [IsLocalRing R] [IsNoetherianRing R] (p : Ideal R) [p.IsPrime]
   [Small.{v} R] [Small.{v} (R ⧸ (maximalIdeal R))]
 
-variable {Rₚ : Type u'} [CommRing Rₚ] [Algebra R Rₚ] [IsLocalization.AtPrime Rₚ p]
-  [IsLocalRing Rₚ]
-  -- This can be deduced from `IsLocalization.AtPrime Rₚ p`, but cannot be an
-  -- `instance`, so we need to manually add this condition.
-  [Small.{v'} Rₚ] [Small.{v'} (Rₚ ⧸ (maximalIdeal Rₚ))]
-
-variable (M : ModuleCat.{v} R) (Mₚ : ModuleCat.{v'} Rₚ)
-  [Module R Mₚ] (f : M →ₗ[R] Mₚ) [IsLocalizedModule.AtPrime p f]
-
-include p f
-
-lemma isLocalize_at_prime_isCohenMacaulay_of_isCohenMacaulay [M.IsCohenMacaulay] :
-    Mₚ.IsCohenMacaulay := by
+lemma depth_eq_dim_quotient_associated_prime_of_isCohenMacaulay (M : ModuleCat.{v} R)
+    [M.IsCohenMacaulay] (mem : p ∈ associatedPrimes R M) :
+    IsLocalRing.depth M = ringKrullDim (R ⧸ p) := by
   sorry
 
-lemma isLocalize_at_prime_depth_eq_of_isCohenMacaulay [Small.{v} (R ⧸ p)] [M.IsCohenMacaulay] :
-    p.depth M = IsLocalRing.depth Mₚ := sorry
-
-end IsLocalization
-
---have some universe problem may have better statement using `IsLocalizedModule`
-lemma localize_at_prime_isCohenMacaulay_of_isCohenMacaulay [IsLocalRing R] [IsNoetherianRing R]
-    (p : Ideal R) [p.IsPrime] [Small.{v} R] [Small.{v} (R ⧸ (maximalIdeal R))]
-    (M : ModuleCat.{v} R) [M.IsCohenMacaulay] :
-    (ModuleCat.of (Localization.AtPrime p) (LocalizedModule.AtPrime p M)).IsCohenMacaulay :=
-  isLocalize_at_prime_isCohenMacaulay_of_isCohenMacaulay p M _
-    (LocalizedModule.mkLinearMap p.primeCompl M)
-
---have some universe problem may have better statement using `IsLocalizedModule`
-lemma localize_at_prime_depth_eq_of_isCohenMacaulay [IsLocalRing R] [IsNoetherianRing R]
-    (p : Ideal R) [p.IsPrime] [Small.{v} R] [Small.{v} (R ⧸ (maximalIdeal R))] [Small.{v} (R ⧸ p)]
-    (M : ModuleCat.{v} R) [M.IsCohenMacaulay] : p.depth M =
-    IsLocalRing.depth (ModuleCat.of (Localization.AtPrime p) (LocalizedModule.AtPrime p M)) :=
-  isLocalize_at_prime_depth_eq_of_isCohenMacaulay p M _
-    (LocalizedModule.mkLinearMap p.primeCompl M)
+--will update the two sets are equal when done with works about `Ass(M)`
+lemma associated_prime_minimal_of_isCohenMacaulay (M : ModuleCat.{v} R)
+    [M.IsCohenMacaulay] (mem : p ∈ associatedPrimes R M) :
+    p ∈ (Module.annihilator R M).minimalPrimes := by
+  sorry
 
 lemma ENat.add_right_cancel_iff (a b c : ℕ∞) (netop : c ≠ ⊤) : a + c = b + c ↔ a = b :=
   ⟨fun h ↦ ENat.add_left_injective_of_ne_top netop h, fun h ↦ by rw [h]⟩
@@ -104,6 +79,54 @@ lemma quotient_regular_isCohenMacaulay_iff_isCohenMacaulay [IsLocalRing R] [IsNo
   rw [← Module.supportDim_regular_sequence_add_length_eq_supportDim rs reg,
     ← depth_quotient_regular_sequence_add_length_eq_depth M rs reg, WithBot.coe_add]
   exact withBotENat_add_coe_cancel _ _ rs.length
+
+variable {Rₚ : Type u'} [CommRing Rₚ] [Algebra R Rₚ] [IsLocalization.AtPrime Rₚ p]
+  [IsLocalRing Rₚ]
+  -- This can be deduced from `IsLocalization.AtPrime Rₚ p`, but cannot be an
+  -- `instance`, so we need to manually add this condition.
+  [Small.{v'} Rₚ] [Small.{v'} (Rₚ ⧸ (maximalIdeal Rₚ))]
+
+variable (M : ModuleCat.{v} R) (Mₚ : ModuleCat.{v'} Rₚ)
+  [Module R Mₚ] (f : M →ₗ[R] Mₚ) [IsLocalizedModule.AtPrime p f]
+
+include p f
+
+lemma isLocalize_at_prime_prime_depth_le_depth [Small.{v} (R ⧸ p)] :
+    p.depth M ≤ IsLocalRing.depth Mₚ := by
+  sorry
+
+lemma isLocalize_at_prime_dim_eq_prime_depth_of_isCohenMacaulay [Small.{v} (R ⧸ p)]
+    [M.IsCohenMacaulay] : Module.supportDim Rₚ Mₚ = p.depth M := by
+  have : p.depth M ≠ ⊤ := sorry
+  rcases ENat.ne_top_iff_exists.mp this with ⟨n, hn⟩
+  induction' n with n ih generalizing M Mₚ
+  · sorry
+  · sorry
+
+lemma isLocalize_at_prime_isCohenMacaulay_of_isCohenMacaulay [M.IsCohenMacaulay] :
+    Mₚ.IsCohenMacaulay := by
+  sorry
+
+lemma isLocalize_at_prime_depth_eq_of_isCohenMacaulay [Small.{v} (R ⧸ p)] [M.IsCohenMacaulay] :
+    p.depth M = IsLocalRing.depth Mₚ := sorry
+
+end IsLocalization
+
+--have some universe problem may have better statement using `IsLocalizedModule`
+lemma localize_at_prime_isCohenMacaulay_of_isCohenMacaulay [IsLocalRing R] [IsNoetherianRing R]
+    (p : Ideal R) [p.IsPrime] [Small.{v} R] [Small.{v} (R ⧸ (maximalIdeal R))]
+    (M : ModuleCat.{v} R) [M.IsCohenMacaulay] :
+    (ModuleCat.of (Localization.AtPrime p) (LocalizedModule.AtPrime p M)).IsCohenMacaulay :=
+  isLocalize_at_prime_isCohenMacaulay_of_isCohenMacaulay p M _
+    (LocalizedModule.mkLinearMap p.primeCompl M)
+
+--have some universe problem may have better statement using `IsLocalizedModule`
+lemma localize_at_prime_depth_eq_of_isCohenMacaulay [IsLocalRing R] [IsNoetherianRing R]
+    (p : Ideal R) [p.IsPrime] [Small.{v} R] [Small.{v} (R ⧸ (maximalIdeal R))] [Small.{v} (R ⧸ p)]
+    (M : ModuleCat.{v} R) [M.IsCohenMacaulay] : p.depth M =
+    IsLocalRing.depth (ModuleCat.of (Localization.AtPrime p) (LocalizedModule.AtPrime p M)) :=
+  isLocalize_at_prime_depth_eq_of_isCohenMacaulay p M _
+    (LocalizedModule.mkLinearMap p.primeCompl M)
 
 variable (R)
 
