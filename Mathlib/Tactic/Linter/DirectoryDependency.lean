05d0a12a078af8c7551b772b12b3eae108fdc01b
Anne C.A. Baanen <vierkantor@vierkantor.com>
2025-05-16 15:43:09 +0200

    Typo fix

--- Mathlib/Tactic/Linter/DirectoryDependency.lean
+++ Mathlib/Tactic/Linter/DirectoryDependency.lean
@@ -16,7 +16,7 @@ modularity of Mathlib.
 -- XXX: is there a better long-time place for this
 /-- Parse all imports in a text file at `path` and return just their names:
 this is just a thin wrapper around `Lean.parseImports'`.
-Omit `Init (which is part of the prelude). -/
+Omit `Init` (which is part of the prelude). -/
 def findImports (path : System.FilePath) : IO (Array Lean.Name) := do
   return (← Lean.parseImports' (← IO.FS.readFile path) path.toString).imports
     |>.map (fun imp ↦ imp.module) |>.erase `Init
