5decab907fcd36590b3e2e0c5532a3af78cd011a
David Loeffler <d.loeffler.01@cantab.net>
2025-05-17 15:42:06 +0000

    feat(LinearAlgebra/Matrix): num and denom of a rational matrix (#24951)
    Write a rational matrix as an integer matrix `Matrix.num` divided by a nonzero natural `Matrix.den`.

--- Mathlib.lean
+++ Mathlib.lean
@@ -3970,6 +3970,7 @@ import Mathlib.LinearAlgebra.Matrix.Gershgorin
 import Mathlib.LinearAlgebra.Matrix.Hermitian
 import Mathlib.LinearAlgebra.Matrix.HermitianFunctionalCalculus
 import Mathlib.LinearAlgebra.Matrix.Ideal
+import Mathlib.LinearAlgebra.Matrix.Integer
 import Mathlib.LinearAlgebra.Matrix.InvariantBasisNumber
 import Mathlib.LinearAlgebra.Matrix.IsDiag
 import Mathlib.LinearAlgebra.Matrix.LDL
