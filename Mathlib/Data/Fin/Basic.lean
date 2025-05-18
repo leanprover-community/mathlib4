a9677b456f7b88c2ef0b2be3a351365e7ef5a84d
Eric Wieser <wieser.eric@gmail.com>
2025-05-18 22:47:02 +0000

    shake

--- Mathlib/Data/Fin/Basic.lean
+++ Mathlib/Data/Fin/Basic.lean
@@ -3,7 +3,6 @@ Copyright (c) 2017 Robert Y. Lewis. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Robert Y. Lewis, Keeley Hoek
 -/
-import Mathlib.Algebra.NeZero
 import Mathlib.Data.Int.DivMod
 import Mathlib.Logic.Embedding.Basic
 import Mathlib.Logic.Equiv.Set
