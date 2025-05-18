8f461b06ad7e1fa97c4f6b135af2cb87d0250e6e
Yaël Dillies <yael.dillies@gmail.com>
2025-05-18 18:57:42 +0000

    fix

--- Mathlib/Algebra/GroupWithZero/Indicator.lean
+++ Mathlib/Algebra/GroupWithZero/Indicator.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Zhouhang Zhou, Yury Kudryashov
 -/
 import Mathlib.Algebra.Group.Indicator
+import Mathlib.Algebra.Group.Support
 import Mathlib.Algebra.GroupWithZero.Basic
 
 /-!
