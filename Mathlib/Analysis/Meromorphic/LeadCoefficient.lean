28bf0b0ed6855699d53d9f4501a7fd32323f98c2
Stefan Kebekus <kebekus@users.noreply.github.com>
2025-05-19 10:01:28 +0200

    Implement referee's suggestion

--- Mathlib/Analysis/Meromorphic/LeadCoefficient.lean
+++ Mathlib/Analysis/Meromorphic/LeadCoefficient.lean
@@ -12,7 +12,7 @@ This file defines the leading coefficient of a meromorphic function. If `f` is m
 point `x`, the leading coefficient is defined as the (unique!) value `g x` for a presentation of `f`
 in the form `(z - x) ^ order • g z` with `g` analytic at `x`.
 
-The lemma `leadCoefficient_eq_limit` expresses the leading coefficient as a limit.
+The lemma `leadCoeff_eq_limit` expresses the leading coefficient as a limit.
 -/
 
 variable
@@ -30,25 +30,24 @@ If `f` is meromorphic of finite order at a point `x`, the leading coefficient is
 (unique!) value `g x` for a presentation of `f` in the form `(z - x) ^ order • g z` with `g`
 analytic at `x`. In all other cases, the leading coefficient is defined to be zero.
 -/
-noncomputable def leadCoefficient : E := by
-  by_cases h₁ : ¬MeromorphicAt f x
+noncomputable def leadCoeff : E := by
+  by_cases h₁ : MeromorphicAt f x
+  · by_cases h₂ : h₁.order = ⊤
+    · exact 0
+    · exact (h₁.order_ne_top_iff.1 h₂).choose x
   · exact 0
-  rw [not_not] at h₁
-  by_cases h₂ : h₁.order = ⊤
-  · exact 0
-  exact (h₁.order_ne_top_iff.1 h₂).choose x
 
 /--
 If `f` is not meromorphic at `x`, the leading coefficient is zero by definition.
 -/
-@[simp] lemma leadCoefficient_of_not_MeromorphicAt (h : ¬MeromorphicAt f x) :
-    leadCoefficient f x = 0 := by simp_all [leadCoefficient]
+@[simp] lemma leadCoeff_of_not_MeromorphicAt (h : ¬MeromorphicAt f x) :
+    leadCoeff f x = 0 := by simp_all [leadCoeff]
 
 /--
 If `f` is meromorphic of infinite order at `x`, the leading coefficient is zero by definition.
 -/
-@[simp] lemma leadCoefficient_of_order_eq_top (h₁ : MeromorphicAt f x) (h₂ : h₁.order = ⊤) :
-    leadCoefficient f x = 0 := by simp_all [leadCoefficient]
+@[simp] lemma leadCoeff_of_order_eq_top (h₁ : MeromorphicAt f x) (h₂ : h₁.order = ⊤) :
+    leadCoeff f x = 0 := by simp_all [leadCoeff]
 
 /-!
 ## Characterization of the Leading Coefficient
@@ -59,10 +58,10 @@ Definition of the leading coefficient in case where `f` is meromorphic of finite
 presentation is given.
 -/
 @[simp]
-lemma leadCoefficient_of_order_eq_finite (h₁ : MeromorphicAt f x) (h₂ : AnalyticAt 𝕜 g x)
+lemma leadCoeff_of_order_eq_finite (h₁ : MeromorphicAt f x) (h₂ : AnalyticAt 𝕜 g x)
     (h₃ : h₁.order ≠ ⊤) (h₄ : f =ᶠ[𝓝[≠] x] fun z ↦ (z - x) ^ h₁.order.untop₀ • g z) :
-    leadCoefficient f x = g x := by
-  unfold leadCoefficient
+    leadCoeff f x = g x := by
+  unfold leadCoeff
   simp only [h₁, not_true_eq_false, reduceDIte, h₃, ne_eq]
   obtain ⟨h'₁, h'₂, h'₃⟩ := (h₁.order_ne_top_iff.1 h₃).choose_spec
   apply Filter.EventuallyEq.eq_of_nhds
@@ -73,13 +72,13 @@ lemma leadCoefficient_of_order_eq_finite (h₁ : MeromorphicAt f x) (h₂ : Anal
   simp_all [zpow_ne_zero, sub_ne_zero]
 
 /--
-Variant of `leadCoefficient_of_order_eq_finite`: Definition of the leading coefficient in case where
+Variant of `leadCoeff_of_order_eq_finite`: Definition of the leading coefficient in case where
 `f` is meromorphic of finite order and a presentation is given.
 -/
 @[simp]
-lemma _root_.AnalyticAt.leadCoefficient_of_order_eq_finite₁ (h₁ : AnalyticAt 𝕜 g x) (h₂ : g x ≠ 0)
+lemma _root_.AnalyticAt.leadCoeff_of_order_eq_finite₁ (h₁ : AnalyticAt 𝕜 g x) (h₂ : g x ≠ 0)
     (h₃ : f =ᶠ[𝓝[≠] x] fun z ↦ (z - x) ^ n • g z) :
-    leadCoefficient f x = g x := by
+    leadCoeff f x = g x := by
   have h₄ : MeromorphicAt f x := by
     rw [MeromorphicAt.meromorphicAt_congr h₃]
     fun_prop
@@ -87,15 +86,15 @@ lemma _root_.AnalyticAt.leadCoefficient_of_order_eq_finite₁ (h₁ : AnalyticAt
     simp only [h₄.order_eq_int_iff, ne_eq, zpow_natCast]
     use g, h₁, h₂
     exact h₃
-  simp_all [leadCoefficient_of_order_eq_finite h₄ h₁, this]
+  simp_all [leadCoeff_of_order_eq_finite h₄ h₁, this]
 
 /--
 If `f` is analytic and does not vanish at `x`, then the leading coefficient of `f` at `x` is `f x`.
 -/
 @[simp]
-lemma _root_.AnalyticAt.leadCoefficient_of_nonvanish (h₁ : AnalyticAt 𝕜 f x) (h₂ : f x ≠ 0) :
-    leadCoefficient f x = f x := by
-  rw [h₁.leadCoefficient_of_order_eq_finite₁ (n := 0) h₂]
+lemma _root_.AnalyticAt.leadCoeff_of_nonvanish (h₁ : AnalyticAt 𝕜 f x) (h₂ : f x ≠ 0) :
+    leadCoeff f x = f x := by
+  rw [h₁.leadCoeff_of_order_eq_finite₁ (n := 0) h₂]
   filter_upwards
   simp
 
@@ -103,11 +102,11 @@ lemma _root_.AnalyticAt.leadCoefficient_of_nonvanish (h₁ : AnalyticAt 𝕜 f x
 If `f` is meromorphic at `x`, then the leading coefficient of `f` at `x` is the limit of the
 function `(· - x) ^ (-h₁.order.untop₀) • f`.
 -/
-lemma leadCoefficient_eq_limit (h : MeromorphicAt f x) :
-    Tendsto ((· - x) ^ (-h.order.untop₀) • f) (𝓝[≠] x) (𝓝 (leadCoefficient f x)) := by
+lemma leadCoeff_eq_limit (h : MeromorphicAt f x) :
+    Tendsto ((· - x) ^ (-h.order.untop₀) • f) (𝓝[≠] x) (𝓝 (leadCoeff f x)) := by
   by_cases h₂ : h.order = ⊤
   · simp_all only [WithTop.untop₀_top, neg_zero, zpow_zero, one_smul,
-      leadCoefficient_of_order_eq_top]
+      leadCoeff_of_order_eq_top]
     apply Tendsto.congr' (f₁ := 0)
     · filter_upwards [h.order_eq_top_iff.1 h₂] with y hy
       simp_all
@@ -119,7 +118,7 @@ lemma leadCoefficient_eq_limit (h : MeromorphicAt f x) :
       ← zpow_neg, ← zpow_add', neg_add_cancel, zpow_zero, one_smul]
     left
     simp_all [sub_ne_zero]
-  · rw [leadCoefficient_of_order_eq_finite h h₁g h₂ h₃g]
+  · rw [leadCoeff_of_order_eq_finite h h₁g h₂ h₃g]
     apply h₁g.continuousAt.continuousWithinAt
 
 /-!
@@ -129,10 +128,10 @@ lemma leadCoefficient_eq_limit (h : MeromorphicAt f x) :
 /--
 If `f` is meromorphic of finite order at `x`, then the leading coefficient is not zero.
 -/
-lemma zero_ne_leadCoefficient (h₁ : MeromorphicAt f x) (h₂ : h₁.order ≠ ⊤) :
-    0 ≠ leadCoefficient f x := by
+lemma zero_ne_leadCoeff (h₁ : MeromorphicAt f x) (h₂ : h₁.order ≠ ⊤) :
+    0 ≠ leadCoeff f x := by
   obtain ⟨g, h₁g, h₂g, h₃g⟩ := h₁.order_ne_top_iff.1 h₂
-  simpa [h₁g.leadCoefficient_of_order_eq_finite₁ h₂g h₃g] using h₂g.symm
+  simpa [h₁g.leadCoeff_of_order_eq_finite₁ h₂g h₃g] using h₂g.symm
 
 /-!
 ## Congruence Lemma
@@ -141,16 +140,16 @@ lemma zero_ne_leadCoefficient (h₁ : MeromorphicAt f x) (h₂ : h₁.order ≠ 
 /--
 If two functions agree in a punctured neighborhood, then their leading coefficients agree.
 -/
-lemma leadCoefficient_congr_nhdNE {f₁ f₂ : 𝕜 → E} (h : f₁ =ᶠ[𝓝[≠] x] f₂) :
-    leadCoefficient f₁ x = leadCoefficient f₂ x := by
+lemma leadCoeff_congr_nhdNE {f₁ f₂ : 𝕜 → E} (h : f₁ =ᶠ[𝓝[≠] x] f₂) :
+    leadCoeff f₁ x = leadCoeff f₂ x := by
   by_cases h₁ : ¬MeromorphicAt f₁ x
   · simp [h₁, (MeromorphicAt.meromorphicAt_congr h).not.1 h₁]
   rw [not_not] at h₁
   by_cases h₂ : h₁.order = ⊤
   · simp_all [h₁.congr h, h₁.order_congr h]
   obtain ⟨g, h₁g, h₂g, h₃g⟩ := h₁.order_ne_top_iff.1 h₂
-  rw [h₁g.leadCoefficient_of_order_eq_finite₁ h₂g h₃g,
-    h₁g.leadCoefficient_of_order_eq_finite₁ h₂g (h.symm.trans h₃g)]
+  rw [h₁g.leadCoeff_of_order_eq_finite₁ h₂g h₃g,
+    h₁g.leadCoeff_of_order_eq_finite₁ h₂g (h.symm.trans h₃g)]
 
 /-!
 ## Behavior under Arithmetic Operations
@@ -159,9 +158,9 @@ lemma leadCoefficient_congr_nhdNE {f₁ f₂ : 𝕜 → E} (h : f₁ =ᶠ[𝓝[�
 /--
 The leading coefficient of a scalar product is the scalar product of the leading coefficients.
 -/
-lemma leadCoefficient_smul {f₁ : 𝕜 → 𝕜} {f₂ : 𝕜 → E} (hf₁ : MeromorphicAt f₁ x)
+lemma leadCoeff_smul {f₁ : 𝕜 → 𝕜} {f₂ : 𝕜 → E} (hf₁ : MeromorphicAt f₁ x)
     (hf₂ : MeromorphicAt f₂ x) :
-    leadCoefficient (f₁ • f₂) x = (leadCoefficient f₁ x) • (leadCoefficient f₂ x) := by
+    leadCoeff (f₁ • f₂) x = (leadCoeff f₁ x) • (leadCoeff f₂ x) := by
   by_cases h₁f₁ : hf₁.order = ⊤
   · simp_all [hf₁, hf₁.smul hf₂, hf₁.order_smul hf₂, h₁f₁]
   by_cases h₁f₂ : hf₂.order = ⊤
@@ -173,34 +172,34 @@ lemma leadCoefficient_smul {f₁ : 𝕜 → 𝕜} {f₂ : 𝕜 → E} (hf₁ : M
     simp_all [hf₁.order_smul hf₂]
     rw [← smul_assoc, ← smul_assoc, smul_eq_mul, smul_eq_mul, zpow_add₀ (sub_ne_zero.2 h₃y)]
     ring_nf
-  rw [h₁g₁.leadCoefficient_of_order_eq_finite₁ h₂g₁ h₃g₁,
-    h₁g₂.leadCoefficient_of_order_eq_finite₁ h₂g₂ h₃g₂,
-    leadCoefficient_of_order_eq_finite (hf₁.smul hf₂) (h₁g₁.smul h₁g₂)
+  rw [h₁g₁.leadCoeff_of_order_eq_finite₁ h₂g₁ h₃g₁,
+    h₁g₂.leadCoeff_of_order_eq_finite₁ h₂g₂ h₃g₂,
+    leadCoeff_of_order_eq_finite (hf₁.smul hf₂) (h₁g₁.smul h₁g₂)
       (by simp_all [hf₁.order_smul hf₂]) this]
   simp
 
 /--
 The leading coefficient of a product is the product of the leading coefficients.
 -/
-lemma leadCoefficient_mul {f₁ f₂ : 𝕜 → 𝕜} (hf₁ : MeromorphicAt f₁ x)
+lemma leadCoeff_mul {f₁ f₂ : 𝕜 → 𝕜} (hf₁ : MeromorphicAt f₁ x)
     (hf₂ : MeromorphicAt f₂ x) :
-    leadCoefficient (f₁ * f₂) x = (leadCoefficient f₁ x) * (leadCoefficient f₂ x) := by
-  exact leadCoefficient_smul hf₁ hf₂
+    leadCoeff (f₁ * f₂) x = (leadCoeff f₁ x) * (leadCoeff f₂ x) := by
+  exact leadCoeff_smul hf₁ hf₂
 
 /--
 The leading coefficient of the inverse function is the inverse of the leading coefficient.
 -/
-lemma leadCoefficient_inv {f : 𝕜 → 𝕜} :
-    leadCoefficient f⁻¹ x = (leadCoefficient f x)⁻¹ := by
+lemma leadCoeff_inv {f : 𝕜 → 𝕜} :
+    leadCoeff f⁻¹ x = (leadCoeff f x)⁻¹ := by
   by_cases h₁ : MeromorphicAt f x
   · by_cases h₂ : h₁.order = ⊤
     · simp_all [h₁.order_inv]
     have : f⁻¹ * f =ᶠ[𝓝[≠] x] 1 := by
       filter_upwards [h₁.order_ne_top_iff_eventually_ne_zero.1 h₂]
       simp_all
-    rw [← mul_eq_one_iff_eq_inv₀ (h₁.zero_ne_leadCoefficient h₂).symm,
-      ← leadCoefficient_mul h₁.inv h₁, leadCoefficient_congr_nhdNE this,
-      analyticAt_const.leadCoefficient_of_order_eq_finite₁ (n := 0)]
+    rw [← mul_eq_one_iff_eq_inv₀ (h₁.zero_ne_leadCoeff h₂).symm,
+      ← leadCoeff_mul h₁.inv h₁, leadCoeff_congr_nhdNE this,
+      analyticAt_const.leadCoeff_of_order_eq_finite₁ (n := 0)]
     <;> simp_all
     exact eventuallyEq_nhdsWithin_of_eqOn fun _ ↦ congrFun rfl
   · simp_all
@@ -209,11 +208,11 @@ lemma leadCoefficient_inv {f : 𝕜 → 𝕜} :
 Except for edge cases, the leading coefficient of the power of a function is the power of the
 leading coefficient.
 -/
-lemma leadCoefficient_zpow₁ {f : 𝕜 → 𝕜} (h₁ : MeromorphicAt f x) (h₂ : h₁.order ≠ ⊤) :
-    leadCoefficient (f ^ n) x = (leadCoefficient f x) ^ n := by
+lemma leadCoeff_zpow₁ {f : 𝕜 → 𝕜} (h₁ : MeromorphicAt f x) (h₂ : h₁.order ≠ ⊤) :
+    leadCoeff (f ^ n) x = (leadCoeff f x) ^ n := by
   obtain ⟨g, h₁g, h₂g, h₃g⟩ := h₁.order_ne_top_iff.1 h₂
-  rw [h₁g.leadCoefficient_of_order_eq_finite₁ (n := h₁.order.untop₀) h₂g h₃g,
-    (h₁g.zpow h₂g (n := n)).leadCoefficient_of_order_eq_finite₁ (n := (h₁.zpow n).order.untop₀)
+  rw [h₁g.leadCoeff_of_order_eq_finite₁ (n := h₁.order.untop₀) h₂g h₃g,
+    (h₁g.zpow h₂g (n := n)).leadCoeff_of_order_eq_finite₁ (n := (h₁.zpow n).order.untop₀)
       (by simp_all [h₂g, zpow_ne_zero])]
   · simp only [Pi.pow_apply]
   · filter_upwards [h₃g] with a ha
@@ -223,28 +222,28 @@ lemma leadCoefficient_zpow₁ {f : 𝕜 → 𝕜} (h₁ : MeromorphicAt f x) (h�
 Except for edge cases, the leading coefficient of the power of a function is the power of the
 leading coefficient.
 -/
-lemma leadCoefficient_zpow₂ {f : 𝕜 → 𝕜} (h : MeromorphicAt f x) (hn : n ≠ 0):
-    leadCoefficient (f ^ n) x = (leadCoefficient f x) ^ n := by
+lemma leadCoeff_zpow₂ {f : 𝕜 → 𝕜} (h : MeromorphicAt f x) (hn : n ≠ 0):
+    leadCoeff (f ^ n) x = (leadCoeff f x) ^ n := by
   by_cases h₁ : h.order = ⊤
   · simp_all [h.order_zpow, h₁, h.zpow n, zero_zpow n hn]
-  apply leadCoefficient_zpow₁ h h₁
+  apply leadCoeff_zpow₁ h h₁
 
 /--
 Except for edge cases, the leading coefficient of the power of a function is the power of the
 leading coefficient.
 -/
-lemma leadCoefficient_pow₁ {n : ℕ} {f : 𝕜 → 𝕜} (h₁ : MeromorphicAt f x) (h₂ : h₁.order ≠ ⊤) :
-    leadCoefficient (f ^ n) x = (leadCoefficient f x) ^ n := by
-  convert leadCoefficient_zpow₁ h₁ h₂ (n := n)
+lemma leadCoeff_pow₁ {n : ℕ} {f : 𝕜 → 𝕜} (h₁ : MeromorphicAt f x) (h₂ : h₁.order ≠ ⊤) :
+    leadCoeff (f ^ n) x = (leadCoeff f x) ^ n := by
+  convert leadCoeff_zpow₁ h₁ h₂ (n := n)
   <;> simp
 
 /--
 Except for edge cases, the leading coefficient of the power of a function is the power of the
 leading coefficient.
 -/
-lemma leadCoefficient_pow₂ {n : ℕ} {f : 𝕜 → 𝕜} (h : MeromorphicAt f x) (hn : n ≠ 0):
-    leadCoefficient (f ^ n) x = (leadCoefficient f x) ^ n := by
-  convert leadCoefficient_zpow₂ h (n := n) (Int.ofNat_ne_zero.mpr hn)
+lemma leadCoeff_pow₂ {n : ℕ} {f : 𝕜 → 𝕜} (h : MeromorphicAt f x) (hn : n ≠ 0):
+    leadCoeff (f ^ n) x = (leadCoeff f x) ^ n := by
+  convert leadCoeff_zpow₂ h (n := n) (Int.ofNat_ne_zero.mpr hn)
   <;> simp
 
 end MeromorphicAt
