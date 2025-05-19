ef13e1de84a42d2c7528b2f8133b75064bb38698
David Loeffler <d.loeffler.01@cantab.net>
2025-05-19 22:19:50 +0200

    WIP

--- Mathlib/NumberTheory/ModularForms/Basic.lean
+++ Mathlib/NumberTheory/ModularForms/Basic.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Chris Birkbeck
 -/
 import Mathlib.Algebra.DirectSum.Algebra
+import Mathlib.Analysis.Calculus.Deriv.Star
 import Mathlib.Analysis.Complex.UpperHalfPlane.FunctionsBoundedAtInfty
 import Mathlib.Analysis.Complex.UpperHalfPlane.Manifold
 import Mathlib.Geometry.Manifold.MFDeriv.SpecificFunctions
@@ -22,7 +23,7 @@ modular form.
 
 open Complex UpperHalfPlane
 
-open scoped Topology Manifold MatrixGroups
+open scoped Topology Manifold MatrixGroups ComplexConjugate
 
 noncomputable section
 
@@ -30,17 +31,62 @@ section ModularForm
 
 open ModularForm
 
-/-- The weight `k` slash action of `GL(2, ℝ)⁺` preserves holomorphic functions.
-
-TO DO: Actually this holds for `GL(2, ℝ)` (without the positivity assumption), but this is
-somewhat more annoying to prove: we have to argue that the composite of two anti-holomorphic
-functions is holomorphic. -/
-lemma MDifferentiable.slash {f : ℍ → ℂ} (hf : MDifferentiable 𝓘(ℂ) 𝓘(ℂ) f)
+/-- The weight `k` slash action of `GL(2, ℝ)⁺` preserves holomorphic functions. -/
+private lemma MDifferentiable.slash_of_pos {f : ℍ → ℂ} (hf : MDifferentiable 𝓘(ℂ) 𝓘(ℂ) f)
     (k : ℤ) {g : GL (Fin 2) ℝ} (hg : 0 < g.det.val) :
     MDifferentiable 𝓘(ℂ) 𝓘(ℂ) (f ∣[k] g) := by
   refine .mul (.mul ?_ mdifferentiable_const) (UpperHalfPlane.mdifferentiable_denom_zpow g _)
   simp only [σ, hg, ↓reduceIte]
   exact hf.comp (UpperHalfPlane.mdifferentiable_smul hg)
+
+private abbrev J : GL (Fin 2) ℝ :=
+  ⟨!![1, 0; 0, -1], !![1, 0; 0, -1],
+    by simp [Matrix.one_fin_two], by simp [Matrix.one_fin_two]⟩
+
+private lemma J_smul (τ : ℍ) :
+    J • τ = ofComplex (-(conj ↑τ)) := by
+  ext
+  simp [J, UpperHalfPlane.coe_smul, σ, if_neg (show ¬(1 : ℝ) < 0 by norm_num), num, denom,
+    div_neg, ofComplex_apply_of_im_pos (by simpa using τ.im_pos : 0 < (-(starRingEnd ℂ) ↑τ).im)]
+
+private lemma slash_J (f : ℍ → ℂ) (k : ℤ) :
+    (f ∣[k] J) = fun τ : ℍ ↦ -conj (f <| ofComplex <| -(conj ↑τ)) := by
+  ext τ
+  simp only [slash_def]
+  have detj : J.det = -1 := by ext; simp [J]
+  have sigj : σ J = starRingEnd ℂ := by simp [σ, J]
+  have denj : denom J τ = -1 := by simp [denom]
+  rw [detj, sigj, denj, Units.val_neg, Units.val_one, ofReal_neg, ofReal_one, mul_assoc,
+    ← zpow_add₀ (by norm_num : (-1 : ℂ) ≠ 0), (by ring : k - 1 + -k = -1),
+    zpow_neg_one, inv_neg_one, mul_neg_one, J_smul]
+
+/-- The weight `k` slash action of `GL(2, ℝ)⁺` preserves holomorphic functions. -/
+private lemma MDifferentiable.slashJ {f : ℍ → ℂ} (hf : MDifferentiable 𝓘(ℂ) 𝓘(ℂ) f) (k : ℤ) :
+    MDifferentiable 𝓘(ℂ) 𝓘(ℂ) (f ∣[k] J) := by
+  simp only [mdifferentiable_iff, slash_J, Function.comp_def] at hf ⊢
+  have : {z | 0 < z.im}.EqOn (fun x ↦ -conj (f <| ofComplex <| -conj ↑(ofComplex x)))
+      (fun x ↦ -conj (f <| ofComplex <| -conj x)) := fun z hz ↦ by
+    simp [ofComplex_apply_of_im_pos hz]
+  refine .congr (fun z hz ↦ DifferentiableAt.differentiableWithinAt ?_) this
+  have : 0 < (-conj z).im := by simpa using hz
+  have := hf.differentiableAt ((Complex.continuous_im.isOpen_preimage _ isOpen_Ioi).mem_nhds this)
+  simpa using (this.comp _ differentiable_neg.differentiableAt).star_star.neg
+
+/-- The weight `k` slash action of `GL(2, ℝ)` preserves holomorphic functions. -/
+lemma MDifferentiable.slash {f : ℍ → ℂ} (hf : MDifferentiable 𝓘(ℂ) 𝓘(ℂ) f)
+    (k : ℤ) (g : GL (Fin 2) ℝ) : MDifferentiable 𝓘(ℂ) 𝓘(ℂ) (f ∣[k] g) := by
+  rcases g.det_ne_zero.lt_or_lt with hg | hg
+  · have : g = J * (J * g) := by
+      rw [← mul_assoc]
+      conv_lhs => rw [← one_mul g]
+      congr 1
+      ext; simp [Matrix.one_fin_two]
+    rw [this, SlashAction.slash_mul]
+    apply (hf.slashJ k).slash_of_pos
+    rw [map_mul, Units.val_mul, g.val_det_apply]
+    refine mul_pos_of_neg_of_neg ?_ hg
+    simp [J, Matrix.det_fin_two]
+  · exact hf.slash_of_pos k hg
 
 variable (F : Type*) (Γ : Subgroup SL(2, ℤ)) (k : ℤ)
 
@@ -456,14 +502,14 @@ noncomputable def ModularForm.translate [ModularFormClass F Γ k] :
     ModularForm (Γ.map <| MulAut.conj g⁻¹) k where
   __ := SlashInvariantForm.translate f g
   bdd_at_infty' h := by simpa [SlashAction.slash_mul] using ModularFormClass.bdd_at_infty f (g * h)
-  holo' := (ModularFormClass.holo f).slash k (by simp)
+  holo' := (ModularFormClass.holo f).slash k g
 
 @[simp]
 lemma ModularForm.coe_translate [ModularFormClass F Γ k] : translate f g = ⇑f ∣[k] g := rfl
 
 /-- Translating a `CuspForm` by `SL(2, ℤ)`, to obtain a new `CuspForm`.
 
-(TODO : Define this more generally for `GL(2, ℚ)⁺`.) -/
+(TODO : Define this more generally for `GL(2, ℚ)`.) -/
 noncomputable def CuspForm.translate [CuspFormClass F Γ k] :
     CuspForm (Γ.map <| MulAut.conj g⁻¹) k where
   __ := ModularForm.translate f g
