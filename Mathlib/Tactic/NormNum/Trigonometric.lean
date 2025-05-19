b427ba9013c1e1aebf8f721cd1f6eced8a904c17
Christopher Hoskin <christopher.hoskin@gmail.com>
2025-05-19 07:15:47 +0100

    Manual shake

--- Mathlib/Tactic/NormNum/Trigonometric.lean
+++ Mathlib/Tactic/NormNum/Trigonometric.lean
@@ -3,8 +3,7 @@ Copyright (c) 2025 Eric Wieser. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Eric Wieser
 -/
-import Mathlib.Tactic.NormNum.Basic
-import Mathlib.Analysis.Normed.Field.Lemmas
+import Mathlib.Analysis.SpecialFunctions.Trigonometric.Basic
 
 /-!
 # Draft
