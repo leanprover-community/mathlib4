leanprover-community/mathlib4

commit 82c80d8c2debafc2e2c0d9facf35325d5a3745d6

feat(NumberField): define the `maximalRealSubfield` of a number field (#23760)
Define the `maximalRealSubfield` of a number field. It is a totally real subfield that contains all other totally real subfields. 

Add also a couple of instances about the fact that subfields of a totally real field are totally real. 



Co-authored-by: Xavier Roblot <46200072+xroblot@users.noreply.github.com>

Mathlib/NumberTheory/NumberField/InfinitePlace/TotallyRealComplex.lean
@@ -3,7 +3,7 @@ Copyright (c) 2022 Xavier Roblot. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kevin Buzzard, Xavier Roblot
 -/
-import Mathlib.NumberTheory.NumberField.InfinitePlace.Basic
+import Mathlib.NumberTheory.NumberField.InfinitePlace.Ramification
 
 /-!
 # Totally real and totally complex number fields
@@ -16,6 +16,9 @@ This file defines the type of totally real and totally complex number fields.
   are real. In other words, the image of every ring homomorphism `K → ℂ` is a subset of `ℝ`.
 * `NumberField.IsTotallyComplex`: a number field `K` is totally complex if all of its infinite
   places are complex.
+* `NumberField.maximalRealSubfield`: the maximal real subfield of `K`. It is totally real,
+  see `NumberField.isTotallyReal_maximalRealSubfield`, and contains all the other totally real
+  subfields of `K`, see `NumberField.IsTotallyReal.le_maximalRealSubfield`
 
 ## Tags
 
@@ -40,11 +43,26 @@ is a subset of `ℝ`. -/
 @[mk_iff] class IsTotallyReal (K : Type*) [Field K] [NumberField K] where
   isReal : ∀ v : InfinitePlace K, v.IsReal
 
-variable {K : Type*} [Field K] [NumberField K]
+variable {F : Type*} [Field F] [NumberField F] {K : Type*} [Field K] [NumberField K]
 
 theorem nrComplexPlaces_eq_zero_iff :
     nrComplexPlaces K = 0 ↔ IsTotallyReal K := by
   simp [Fintype.card_eq_zero_iff, isEmpty_subtype, isTotallyReal_iff]
+
+theorem IsTotallyReal.ofRingEquiv [IsTotallyReal F] (f : F ≃+* K) : IsTotallyReal K where
+  isReal _ := (isReal_comap_iff f).mp <| IsTotallyReal.isReal _
+
+variable (F K) in
+theorem IsTotally.of_algebra [IsTotallyReal K] [Algebra F K] : IsTotallyReal F where
+  isReal w := by
+    obtain ⟨W, rfl⟩ : ∃ W : InfinitePlace K, W.comap (algebraMap F K) = w := comap_surjective w
+    exact IsReal.comap _ (IsTotallyReal.isReal W)
+
+instance [IsTotallyReal K] (F : IntermediateField ℚ K) : IsTotallyReal F :=
+  IsTotally.of_algebra F K
+
+instance [IsTotallyReal K] (F : Subfield K) : IsTotallyReal F :=
+  IsTotally.of_algebra F K
 
 variable (K)
 
@@ -61,6 +79,56 @@ instance : IsTotallyReal ℚ where
   isReal v := by
     rw [Subsingleton.elim v Rat.infinitePlace]
     exact Rat.isReal_infinitePlace
+
+section maximalRealSubfield
+
+open ComplexEmbedding
+
+/--
+The maximal real subfield of `K`. It is totally real,
+see `NumberField.isTotallyReal_maximalRealSubfield`, and contains all the other totally real
+subfields of `K`, see `NumberField.IsTotallyReal.le_maximalRealSubfield`.
+-/
+def maximalRealSubfield : Subfield K where
+  carrier := {x | ∀ φ : K →+* ℂ, star (φ x) = φ x}
+  mul_mem' hx hy _ := by rw [map_mul, star_mul, hx, hy, mul_comm]
+  one_mem' := by simp
+  add_mem' hx hy _ := by rw [map_add, star_add, hx, hy]
+  zero_mem' := by simp
+  neg_mem' := by simp
+  inv_mem' := by simp
+
+instance isTotallyReal_maximalRealSubfield :
+    IsTotallyReal (maximalRealSubfield K) where
+  isReal w := by
+    rw [InfinitePlace.isReal_iff, ComplexEmbedding.isReal_iff]
+    ext x
+    rw [RingHom.star_apply, ← lift_algebraMap_apply K w.embedding]
+    exact x.prop _
+
+variable {K}
+
+theorem IsTotallyReal.le_maximalRealSubfield (E : Subfield K) [IsTotallyReal E] :
+    E ≤ maximalRealSubfield K := by
+  intro x hx φ
+  rw [show φ x = (φ.comp E.subtype) ⟨x, hx⟩ by rfl, RCLike.star_def, ← conjugate_coe_eq]
+  refine RingHom.congr_fun ?_ _
+  exact ComplexEmbedding.isReal_iff.mp  <| isReal_mk_iff.mp <| isReal _
+
+theorem isTotallyReal_iff_le_maximalRealSubfield {E : Subfield K} :
+    IsTotallyReal E ↔ E ≤ maximalRealSubfield K :=
+  ⟨fun h ↦ h.le_maximalRealSubfield, fun h ↦ IsTotallyReal.ofRingEquiv
+    (RingEquiv.ofBijective _ (Subfield.inclusion h).rangeRestrictField_bijective).symm⟩
+
+instance isTotallyReal_sup {E F : Subfield K} [IsTotallyReal E] [IsTotallyReal F] :
+    IsTotallyReal (E ⊔ F : Subfield K) := by
+  simp_all [isTotallyReal_iff_le_maximalRealSubfield]
+
+instance isTotallyReal_iSup {ι : Type*} {k : ι → Subfield K} [∀ i, IsTotallyReal (k i)] :
+    IsTotallyReal (⨆ i, k i : Subfield K) := by
+  simp_all [isTotallyReal_iff_le_maximalRealSubfield]
+
+ end maximalRealSubfield
 
 end TotallyRealField
 

Mathlib/NumberTheory/NumberField/Basic.lean
@@ -73,6 +73,10 @@ instance of_intermediateField [NumberField K] [NumberField L] [Algebra K L]
     (E : IntermediateField K L) : NumberField E :=
   of_module_finite K E
 
+variable {K} in
+instance of_subfield [NumberField K] (E : Subfield K) : NumberField E where
+  to_finiteDimensional := FiniteDimensional.left ℚ E K
+
 theorem of_tower [NumberField K] [NumberField L] [Algebra K L] (E : Type*) [Field E]
     [Algebra K E] [Algebra E L] [IsScalarTower K E L] : NumberField E :=
   letI := Module.Finite.left K E L

Mathlib/NumberTheory/NumberField/InfinitePlace/Embeddings.lean
@@ -142,9 +142,8 @@ open Complex NumberField
 
 open scoped ComplexConjugate
 
-variable {K : Type*} [Field K] {k : Type*} [Field k]
+variable (K : Type*) [Field K] {k : Type*} [Field k]
 
-variable (K) in
 /--
 A (random) lift of the complex embedding `φ : k →+* ℂ` to an extension `K` of `k`.
 -/
@@ -162,7 +161,9 @@ theorem lift_comp_algebraMap [Algebra k K] [Algebra.IsAlgebraic k K] (φ : k →
 @[simp]
 theorem lift_algebraMap_apply [Algebra k K] [Algebra.IsAlgebraic k K] (φ : k →+* ℂ) (x : k) :
     lift K φ (algebraMap k K x) = φ x :=
-  RingHom.congr_fun (lift_comp_algebraMap φ) x
+  RingHom.congr_fun (lift_comp_algebraMap K φ) x
+
+variable {K}
 
 /-- The conjugate of a complex embedding as a complex embedding. -/
 abbrev conjugate (φ : K →+* ℂ) : K →+* ℂ := star φ

commit ce3f8ce795e5d2f62cf7d91f72d340d9c123c570

chore(NumberField/IsTotallyReal): fixed a typo introduced in #23760 (#25016)

Mathlib/NumberTheory/NumberField/InfinitePlace/TotallyRealComplex.lean
@@ -53,16 +53,16 @@ theorem IsTotallyReal.ofRingEquiv [IsTotallyReal F] (f : F ≃+* K) : IsTotallyR
   isReal _ := (isReal_comap_iff f).mp <| IsTotallyReal.isReal _
 
 variable (F K) in
-theorem IsTotally.of_algebra [IsTotallyReal K] [Algebra F K] : IsTotallyReal F where
+theorem IsTotallyReal.of_algebra [IsTotallyReal K] [Algebra F K] : IsTotallyReal F where
   isReal w := by
     obtain ⟨W, rfl⟩ : ∃ W : InfinitePlace K, W.comap (algebraMap F K) = w := comap_surjective w
     exact IsReal.comap _ (IsTotallyReal.isReal W)
 
 instance [IsTotallyReal K] (F : IntermediateField ℚ K) : IsTotallyReal F :=
-  IsTotally.of_algebra F K
+  IsTotallyReal.of_algebra F K
 
 instance [IsTotallyReal K] (F : Subfield K) : IsTotallyReal F :=
-  IsTotally.of_algebra F K
+  IsTotallyReal.of_algebra F K
 
 variable (K)
 