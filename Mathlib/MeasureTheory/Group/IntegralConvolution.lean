021f95da546c42d1197bb663bbe5e06cbf82e699
Rémy Degenne <remydegenne@gmail.com>
2025-05-19 08:45:52 +0200

    Apply suggestions from code review
    Co-authored-by: Sebastien Gouezel <sebastien.gouezel@univ-rennes1.fr>

--- Mathlib/MeasureTheory/Group/IntegralConvolution.lean
+++ Mathlib/MeasureTheory/Group/IntegralConvolution.lean
@@ -16,7 +16,7 @@ integrals over there.
 
 ## Main statements
 
-* `integrable_conv_iff` A function is integrable with respect to the convolution `μ ∗ ν` iff
+* `integrable_conv_iff`: A function is integrable with respect to the convolution `μ ∗ ν` iff
   the function `y ↦ f (x + y)` is integrable with respect to `ν` for `μ`-almost every `x` and
   the function `x ↦ ∫ y, ‖f (x + y)‖ ∂ν` is integrable with respect to `μ`.
 * `integral_conv`: if `f` is integrable with respect to the convolution `μ ∗ ν`, then
