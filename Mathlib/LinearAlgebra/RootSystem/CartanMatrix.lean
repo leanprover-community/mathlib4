55c6d7a94cfac78c681ddcc2cbdf1d20440e40c3
Oliver Nash <github@olivernash.org>
2025-05-19 19:31:35 +0100

    feat: define the Lie algebra associated to a root system

--- Mathlib/LinearAlgebra/RootSystem/CartanMatrix.lean
+++ Mathlib/LinearAlgebra/RootSystem/CartanMatrix.lean
@@ -114,6 +114,14 @@ lemma cartanMatrix_mem_of_ne [Finite ι] [IsDomain R] {i j : b.support} (hij : i
   refine ⟨⟨{i, j}, by simpa⟩, Finsupp.single i (1 : R) + Finsupp.single j (2 : R), ?_⟩
   simp [contra, hij, hij.symm]
 
+@[simp]
+lemma cartanMatrix_map_abs [DecidableEq b.support] [Finite ι] [IsDomain R] :
+    b.cartanMatrix.map abs = 4 • 1 - b.cartanMatrix := by
+  ext i j
+  rcases eq_or_ne i j with rfl | h <;> rw [Matrix.sub_apply]
+  · simp
+  · simpa [h] using b.cartanMatrix_le_zero_of_ne i j h
+
 lemma cartanMatrix_nondegenerate [Finite ι] [IsDomain R]
     {P : RootSystem ι R M N} [P.IsCrystallographic] (b : P.Base) :
     b.cartanMatrix.Nondegenerate :=
---
+++ Mathlib/LinearAlgebra/RootSystem/LieAlgebra.lean
@@ -0,0 +1,84 @@
+/-
+Copyright (c) 2025 Oliver Nash. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Oliver Nash
+-/
+import Mathlib.Algebra.Lie.OfAssociative
+import Mathlib.LinearAlgebra.RootSystem.CartanMatrix
+import Mathlib.LinearAlgebra.RootSystem.Chain
+
+/-!
+# The Lie algebra of a root system
+-/
+
+noncomputable section
+
+open Set
+
+namespace RootPairing.Base
+
+variable {ι R M N : Type*} [Finite ι] [CommRing R] [IsDomain R] [CharZero R]
+  [AddCommGroup M] [Module R M] [AddCommGroup N] [Module R N]
+  {P : RootSystem ι R M N} [P.IsReduced] [P.IsCrystallographic] [P.IsIrreducible]
+  {b : P.Base}
+
+def e₁₂ (i : b.support) : Matrix b.support ι R :=
+  Matrix.of fun j ↦ open scoped Classical in
+    Pi.single i ↑|b.cartanMatrix i j|
+
+def e₂₁ (i : b.support) : Matrix ι b.support R :=
+  Matrix.of fun j k ↦ open scoped Classical in
+    if P.root j + P.root i = P.root k then P.chainBotCoeff i j else 0
+
+def e₂₂ (i : b.support) : Matrix ι ι R :=
+  Matrix.of fun j ↦ open scoped Classical in
+    if P.root j = - P.root i then Pi.single i 1 else 0
+
+def e (i : b.support) :
+    Matrix (b.support ⊕ ι) (b.support ⊕ ι) R :=
+  Matrix.fromBlocks 0 (e₁₂ i) (e₂₁ i) (e₂₂ i)
+
+def f₁₂ (i : b.support) : Matrix b.support ι R :=
+  Matrix.of fun j ↦ open scoped Classical in
+    Pi.single (P.reflection_perm i i) ↑|b.cartanMatrix i j|
+
+def f₂₁ (i : b.support) : Matrix ι b.support R :=
+  Matrix.of fun j k ↦ open scoped Classical in
+    if P.root j - P.root i = P.root k then P.chainTopCoeff i j else 0
+
+def f₂₂ (i : b.support) : Matrix ι ι R :=
+  Matrix.of fun j ↦ open scoped Classical in
+    if P.root j = P.root i then Pi.single i 1 else 0
+
+def f (i : b.support) :
+    Matrix (b.support ⊕ ι) (b.support ⊕ ι) R :=
+  Matrix.fromBlocks 0 (f₁₂ i) (f₂₁ i) (f₂₂ i)
+
+def h (i : b.support) :
+    Matrix (b.support ⊕ ι) (b.support ⊕ ι) R :=
+  Matrix.fromBlocks 0 0 0 <| Matrix.of fun j _ ↦ P.pairingIn R i j
+
+/-- Lemma 3.3 (a) from [Geck](Geck2017).
+
+TODO Add part (b) as well as Lemmas 3.4, 3.5. -/
+lemma lie_h_e [Fintype b.support] [Fintype ι] (i j : b.support) :
+    ⁅h j, e i⁆ = b.cartanMatrix i j • e i :=
+  sorry
+
+variable (b)
+variable [Fintype b.support] [Fintype ι] [DecidableEq ι]
+
+/-- The Lie algebra associated to a root system with distinguished base. -/
+def lieAlgebra :
+    LieSubalgebra R (Matrix (b.support ⊕ ι) (b.support ⊕ ι) R) :=
+  LieSubalgebra.lieSpan R _ (range e ∪ range f)
+
+instance : LieRingModule b.lieAlgebra (b.support ⊕ ι → R) := sorry
+
+instance : LieModule R b.lieAlgebra (b.support ⊕ ι → R) := sorry
+
+instance : LieModule.IsFaithful R b.lieAlgebra (b.support ⊕ ι → R) := sorry
+
+instance : Module.Finite R b.lieAlgebra := sorry
+
+end RootPairing.Base
