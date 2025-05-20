9b47672fb62b43660a348e51fc0475cd1a75abb4
acmepjz <acme_pjz@hotmail.com>
2025-05-20 20:09:08 +0800

    apply suggestions

--- Mathlib/RingTheory/Polynomial/Eisenstein/Distinguished.lean
+++ Mathlib/RingTheory/Polynomial/Eisenstein/Distinguished.lean
@@ -45,19 +45,21 @@ alias _root_.IsDistinguishedAt.map_eq_X_pow := map_eq_X_pow
 
 section degree_eq_order_map
 
-variable {I : Ideal R} (f h : R⟦X⟧) {g : R[X]} (distinguish : g.IsDistinguishedAt I)
-  (nmem : PowerSeries.constantCoeff R h ∉ I) (eq : f = g * h)
-include distinguish nmem eq
+variable {I : Ideal R} (f h : R⟦X⟧) {g : R[X]}
 
-lemma map_ne_zero_of_eq_mul : f.map (Ideal.Quotient.mk I) ≠ 0 := fun H ↦ by
+lemma map_ne_zero_of_eq_mul (distinguish : g.IsDistinguishedAt I)
+    (nmem : PowerSeries.constantCoeff R h ∉ I) (eq : f = g * h) :
+    f.map (Ideal.Quotient.mk I) ≠ 0 := fun H ↦ by
   have mapf : f.map (Ideal.Quotient.mk I) = (Polynomial.X ^ g.natDegree : (R ⧸ I)[X]) *
       h.map (Ideal.Quotient.mk I) := by
     simp [← map_eq_X_pow distinguish, eq]
   apply_fun PowerSeries.coeff _ g.natDegree at H
   simp [mapf, PowerSeries.coeff_X_pow_mul', eq_zero_iff_mem, nmem] at H
 
-lemma degree_eq_coe_lift_order_map : g.degree = (f.map (Ideal.Quotient.mk I)).order.lift
-    (order_finite_iff_ne_zero.2 (distinguish.map_ne_zero_of_eq_mul f h nmem eq)) := by
+lemma degree_eq_coe_lift_order_map (distinguish : g.IsDistinguishedAt I)
+    (nmem : PowerSeries.constantCoeff R h ∉ I) (eq : f = g * h) :
+    g.degree = (f.map (Ideal.Quotient.mk I)).order.lift
+      (order_finite_iff_ne_zero.2 (distinguish.map_ne_zero_of_eq_mul f h nmem eq)) := by
   have : Nontrivial R := _root_.nontrivial_iff.mpr
     ⟨0, PowerSeries.constantCoeff R h, ne_of_mem_of_not_mem I.zero_mem nmem⟩
   rw [Polynomial.degree_eq_natDegree distinguish.monic.ne_zero, Nat.cast_inj, ← ENat.coe_inj,
@@ -76,7 +78,9 @@ alias _root_.IsDistinguishedAt.degree_eq_order_map := degree_eq_coe_lift_order_m
 @[deprecated (since := "2025-05-19")]
 alias degree_eq_order_map := degree_eq_coe_lift_order_map
 
-lemma coe_natDegree_eq_order_map : g.natDegree = (f.map (Ideal.Quotient.mk I)).order := by
+lemma coe_natDegree_eq_order_map (distinguish : g.IsDistinguishedAt I)
+    (nmem : PowerSeries.constantCoeff R h ∉ I) (eq : f = g * h) :
+    g.natDegree = (f.map (Ideal.Quotient.mk I)).order := by
   rw [natDegree, distinguish.degree_eq_coe_lift_order_map f h nmem eq]
   exact ENat.coe_lift _ (order_finite_iff_ne_zero.2 (distinguish.map_ne_zero_of_eq_mul f h nmem eq))
 
