a97856565630d55845ee83ce5f58feed3882275c
Yaël Dillies <yael.dillies@gmail.com>
2025-05-20 10:00:19 +0000

    reduce diff

--- Mathlib/Algebra/Group/TypeTags/Basic.lean
+++ Mathlib/Algebra/Group/TypeTags/Basic.lean
@@ -454,8 +454,7 @@ instance Multiplicative.coeToFun {α : Type*} {β : α → Sort*} [CoeFun α β]
 
 lemma Pi.mulSingle_multiplicativeOfAdd_eq {ι : Type*} [DecidableEq ι] {M : ι → Type*}
     [(i : ι) → AddMonoid (M i)] (i : ι) (a : M i) (j : ι) :
-    Pi.mulSingle (M := fun i ↦ Multiplicative (M i)) i (.ofAdd a) j =
-      Multiplicative.ofAdd ((Pi.single i a) j) := by
+    Pi.mulSingle (M := fun i ↦ Multiplicative (M i)) i (.ofAdd a) j = .ofAdd ((Pi.single i a) j) := by
   rcases eq_or_ne j i with rfl | h
   · simp only [mulSingle_eq_same, single_eq_same]
   · simp only [mulSingle, ne_eq, h, not_false_eq_true, Function.update_of_ne, one_apply, single,
