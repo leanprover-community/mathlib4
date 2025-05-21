0596e6d39673fb6f30bf01f0228f75f6ff198530
Christopher Hoskin <mans0954@users.noreply.github.com>
2025-05-21 17:21:34 +0100

    Update Mathlib/Analysis/SpecialFunctions/Complex/CircleAddChar.lean
    Co-authored-by: David Loeffler <d.loeffler.01@cantab.net>

--- Mathlib/Analysis/SpecialFunctions/Complex/CircleAddChar.lean
+++ Mathlib/Analysis/SpecialFunctions/Complex/CircleAddChar.lean
@@ -126,6 +126,9 @@ noncomputable def rootsOfUnityCircleEquiv : rootsOfUnity n Circle ≃* rootsOfUn
   right_inv _ := by aesop
 
 instance : HasEnoughRootsOfUnity Circle n := (rootsOfUnityCircleEquiv n).symm.hasEnoughRootsOfUnity
+@[simp] lemma rootsOfUnityCircleEquiv_apply (w : rootsOfUnity n Circle) : 
+    ((rootsOfUnityCircleEquiv n w).val : ℂ) = ((w.val : Circle) : ℂ) :=
+  rfl
 
 open Real in
 lemma rootsOfUnityCircleEquiv_comp_rootsOfUnityAddChar_val (j : ZMod n) :
