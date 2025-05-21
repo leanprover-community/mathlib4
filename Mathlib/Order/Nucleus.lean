de8553bc773a3952df1c53269415e782248555ba
Bergschaf <christian.k@netcom-mail.de>
2025-05-21 19:52:48 +0200

    fix

--- Mathlib/Order/Nucleus.lean
+++ Mathlib/Order/Nucleus.lean
@@ -239,7 +239,8 @@ lemma comp_eq_right_iff_le : n ∘ m = m ↔ n ≤ m where
   mp h x := by
     rw [← mem_range.mp (Set.range_subset_iff.mp h x)]
     exact n.monotone (m.le_apply)
-  mpr h := range_subset_range_iff_exists_comp.mpr (Exists.intro ↑m ((factorizes_iff_le.mpr) h).symm)
+  mpr h :=
+    range_subset_range_iff_exists_comp.mpr (Exists.intro ↑m ((comp_eq_right_iff_le.mpr) h).symm)
 
 end Frame
 end Nucleus
