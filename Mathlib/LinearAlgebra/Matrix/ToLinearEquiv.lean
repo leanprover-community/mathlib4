8d898f2bb46d387c0a9959bfd0abed89cd2f442b
Yury G. Kudryashov <urkud@urkud.name>
2025-05-20 21:22:20 -0500

    Revert

--- Mathlib/LinearAlgebra/Matrix/ToLinearEquiv.lean
+++ Mathlib/LinearAlgebra/Matrix/ToLinearEquiv.lean
@@ -110,11 +110,16 @@ theorem exists_mulVec_eq_zero_iff_aux {K : Type*} [DecidableEq n] [Field K] {M :
     exact eq_zero_of_mulVec_eq_zero hv mul_eq
   · contrapose!
     intro h
-    have : Function.Injective M.mulVecLin := by
-      simpa only [← LinearMap.ker_eq_bot, ker_mulVecLin_eq_bot_iff, not_imp_not] using h
-    have : M * (LinearEquiv.ofInjectiveEndo M.mulVecLin this).symm.toMatrix' = 1 := by
-      rw [← toMatrix'_one, ← LinearEquiv.ofInjectiveEndo_right_inv, toMatrix'_mul,
-        toMatrix'_mulVecLin]
+    have : Function.Injective (Matrix.toLin' M) := by
+      simpa only [← LinearMap.ker_eq_bot, ker_toLin'_eq_bot_iff, not_imp_not] using h
+    have :
+      M *
+          LinearMap.toMatrix'
+            ((LinearEquiv.ofInjectiveEndo (Matrix.toLin' M) this).symm : (n → K) →ₗ[K] n → K) =
+        1 := by
+      refine Matrix.toLin'.injective (LinearMap.ext fun v => ?_)
+      rw [Matrix.toLin'_mul, Matrix.toLin'_one, Matrix.toLin'_toMatrix', LinearMap.comp_apply]
+      exact (LinearEquiv.ofInjectiveEndo (Matrix.toLin' M) this).apply_symm_apply v
     exact Matrix.det_ne_zero_of_right_inverse this
 
 theorem exists_mulVec_eq_zero_iff' {A : Type*} (K : Type*) [DecidableEq n] [CommRing A]
