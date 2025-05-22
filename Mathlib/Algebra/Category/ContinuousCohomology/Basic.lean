4af472775589d78a59079345ab02eb50e4467c91
Andrew Yang <the.erd.one@gmail.com>
2025-05-22 15:40:24 +0100

    fixy fix

--- Mathlib/Algebra/Category/ContinuousCohomology/Basic.lean
+++ Mathlib/Algebra/Category/ContinuousCohomology/Basic.lean
@@ -5,6 +5,7 @@ Authors: Richard Hill, Andrew Yang
 -/
 import Mathlib.Algebra.Category.ModuleCat.Topology.Homology
 import Mathlib.Algebra.Homology.Embedding.Restriction
+import Mathlib.Algebra.Homology.Functor
 import Mathlib.Algebra.Homology.ShortComplex.HomologicalComplex
 import Mathlib.CategoryTheory.Action.Limits
 import Mathlib.Topology.ContinuousMap.Algebra
@@ -14,11 +15,19 @@ import Mathlib.Topology.ContinuousMap.Algebra
 # Continuous cohomology
 
 We define continuous cohomology as the homology of homogeneous cochains.
+
+## Impleemntation details
+
 We define homogenous cochains as `g`-invariant continuous function in `C(G, C(G,...,C(G, M)))`
 instead of the usual `C(Gⁿ, M)` to allow more general topological groups other than locally compact
 ones. For this to work, we also work in `Action (TopModuleCat R) G`, where the `G` action on `M`
 is only continuous on `M`, and not necessarily continuous in both variables, because the `G` action
 on `C(G, M)` might not be continuous on both variables even if it is on `M`.
+
+For the differential map, instead of a finite sum we use the inductive definition
+`d₋₁ : M → C(G, M) := const : m ↦ g ↦ m` and
+`dₙ₊₁ : C(G, _) → C(G, C(G, _)) := const - C(G, dₙ) : f ↦ g ↦ f - dₙ (f (g))`
+See `ContinuousCohomology.MultiInd.d`.
 
 ## Main definition
 - `ContinuousCohomology.homogeneousCochains`:
@@ -117,18 +126,13 @@ lemma d_comp_d (n : ℕ) :
     rw [Preadditive.sub_comp, ← whiskerRight_comp, ih, whiskerRight_zero, sub_zero, sub_eq_zero]
     rfl
 
-/-- The complex of functors taking `M` to `C(G, C(G,...,C(G, M)))`. -/
+/-- The complex of functors whose behaviour pointwise takes an `R`-linear `G`-representation `M`
+to the complex `M → C(G, M) → ⋯ → C(G, C(G,...,C(G, M))) → ⋯`
+The `G`-invariant submodules of it is the homogeneous cochains (shifted by one). -/
 def complex : CochainComplex (Action (TopModuleCat R) G ⥤ Action (TopModuleCat R) G) ℕ :=
   CochainComplex.of (functor R G) (d R G) (d_comp_d R G)
 
 end MultiInd
-
-/-- The functor taking an `R`-linear `G`-representation `M` to complex of representations
-whose n-th component is `C(G, C(G,...,C(G, M)))` (with n `G`s).
-The `G`-invariant submodule of it is the homogeneous cochains. -/
-def multiInd : Action (TopModuleCat R) G ⥤ CochainComplex (Action (TopModuleCat R) G) ℕ where
-  obj M := (((evaluation _ _).obj M).mapHomologicalComplex _).obj (MultiInd.complex R G)
-  map {M N} f := (NatTrans.mapHomologicalComplex ((evaluation _ _).map f) _).app _
 
 /-- The functor taking an `R`-linear `G`-representation to its `G`-invariant submodule. -/
 def invariants : Action (TopModuleCat R) G ⥤ TopModuleCat R where
@@ -148,7 +152,7 @@ instance : (invariants R G).Additive where
 /-- `homogeneousCochains R G` is the functor taking
 an `R`-linear `G`-representation to the complex of homogeneous cochains. -/
 def homogeneousCochains : Action (TopModuleCat R) G ⥤ CochainComplex (TopModuleCat R) ℕ :=
-  multiInd R G ⋙ (invariants R G).mapHomologicalComplex _ ⋙
+  (MultiInd.complex R G).asFunctor ⋙ (invariants R G).mapHomologicalComplex _ ⋙
     (ComplexShape.embeddingUp'Add 1 1).restrictionFunctor _
 
 /-- `continuousCohomology R G n` is the functor taking
@@ -156,5 +160,53 @@ an `R`-linear `G`-representation to its `n`-th continuous homology. -/
 noncomputable
 def _root_.continuousCohomology (n : ℕ) : Action (TopModuleCat R) G ⥤ TopModuleCat R :=
   homogeneousCochains R G ⋙ HomologicalComplex.homologyFunctor _ _ n
+
+/-- The `0`-homogeneous cochains are isomorphic to `Xᴳ`. -/
+def kerHomogeneousCochainsZeroEquiv
+    (X : Action (TopModuleCat R) G) (n : ℕ) (hn : n = 1) :
+    LinearMap.ker (((homogeneousCochains R G).obj X).d 0 n).hom ≃L[R] (invariants R G).obj X where
+  toFun x :=
+  { val := DFunLike.coe (F := C(G, _)) x.1.1 1
+    property g := by
+      subst hn
+      obtain ⟨⟨x : C(G, _), hx⟩, hx'⟩ := x
+      have : (X.ρ g).hom (x (g⁻¹ * 1)) = x 1 := congr(DFunLike.coe (F := C(G, _)) $(hx g) 1)
+      have hx' : x (g⁻¹ * 1) + (- x 1) = 0 :=
+        congr(DFunLike.coe (F := C(G, _)) (DFunLike.coe (F := C(G, _)) ($hx').1 1) (g⁻¹ * 1))
+      rw [← sub_eq_add_neg, sub_eq_zero] at hx'
+      exact congr((X.ρ g).hom $hx').symm.trans this }
+  map_add' _ _ := rfl
+  map_smul' _ _ := rfl
+  invFun x := by
+    refine ⟨⟨ContinuousLinearMap.const R _ x.1, fun g ↦ ContinuousMap.ext fun a ↦
+      by subst hn; exact x.2 g⟩, ?_⟩
+    subst hn
+    exact Subtype.ext (ContinuousMap.ext fun a ↦
+      ContinuousMap.ext fun b ↦ show x.1 + (- x.1) = (0 : X.V) by simp)
+  left_inv x := by
+    subst hn
+    obtain ⟨⟨x : C(G, _), hx⟩, hx'⟩ := x
+    refine Subtype.ext (Subtype.ext <| ContinuousMap.ext fun a ↦ ?_)
+    have hx' : x 1 + (- x a) = 0 :=
+      congr(DFunLike.coe (F := C(G, _)) (DFunLike.coe (F := C(G, _)) ($hx').1 a) 1)
+    rwa [← sub_eq_add_neg, sub_eq_zero] at hx'
+  right_inv _ := rfl
+  continuous_toFun := continuous_induced_rng.mpr ((continuous_eval_const (F := C(G, _)) 1).comp
+      (continuous_subtype_val.comp continuous_subtype_val))
+  continuous_invFun := continuous_induced_rng.mpr
+    (continuous_induced_rng.mpr ((ContinuousLinearMap.const R G).cont.comp continuous_subtype_val))
+
+open ShortComplex HomologyData in
+/-- `H⁰_cont(G, X) ≅ Xᴳ`. -/
+noncomputable
+def continuousCohomologyZeroIso : (continuousCohomology R G 0) ≅ invariants R G :=
+  NatIso.ofComponents (fun X ↦ (ofIsLimitKernelFork _ (by simp) _
+    (TopModuleCat.isLimitKer _)).left.homologyIso ≪≫ TopModuleCat.ofIso
+      (kerHomogeneousCochainsZeroEquiv R G X _ (by simp))) fun {X Y} f ↦ by
+  dsimp [continuousCohomology, HomologicalComplex.homologyMap]
+  rw [Category.assoc, ← Iso.inv_comp_eq]
+  rw [LeftHomologyData.leftHomologyIso_inv_naturality_assoc, Iso.inv_hom_id_assoc,
+    ← cancel_epi (LeftHomologyData.π _), leftHomologyπ_naturality'_assoc]
+  rfl
 
 end ContinuousCohomology
