db5aeeebcdc9f83c6aba19f648f6634e2a8f72bc
Parcly Taxel <reddeloostw@gmail.com>
2025-05-23 10:03:33 +0800

    at least four

--- Mathlib/Tactic/Cases.lean
+++ Mathlib/Tactic/Cases.lean
@@ -129,7 +129,7 @@ elab (name := induction') "induction' " tgts:(Parser.Tactic.elimTarget,+)
           (genArg.1.getArgs[1]!).getArgs.map Syntax.prettyPrint else Array.empty
         let inductor : Format := if usingArg.1.getArgs.size > 1 then
           Syntax.prettyPrint usingArg.1.getArgs[1]! else "~"
-        if subgoals.toList.length ≤ 1 then
+        if subgoals.toList.length ≥ 4 then
           logInfoAt tgts m!"{body.getAppFn.setPPExplicit true} {inductor} {gens} {names} \
             {subgoals.toList.length}"
         setGoals <| (subgoals ++ result.others).toList ++ gs
