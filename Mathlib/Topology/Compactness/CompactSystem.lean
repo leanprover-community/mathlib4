3eecb42e1e7e7eae648e5f6ebba2fd310270351f
pfaffelh <pfaffelh@gmail.com>
2025-05-23 23:50:20 +0200

    fininsh?

--- Mathlib/Topology/Compactness/CompactSystem.lean
+++ Mathlib/Topology/Compactness/CompactSystem.lean
@@ -18,6 +18,8 @@ This files defines compact systems of sets.
 
 ## Main results
 
+* `IsCompactSystemiff_isCompactSystem_of_or_univ`: A set system is a compact
+system iff inserting `univ` gives a compact system.
 * `IsClosedCompact.isCompactSystem`: The set of closed and compact sets is a compact system.
 * `IsClosedCompact.isCompactSystem_of_T2Space`: In a `T2Space α`, the set of compact sets
   is a compact system in a `T2Space`.
@@ -108,7 +110,7 @@ lemma of_IsEmpty (h : IsEmpty α) (p : Set α → Prop) : IsCompactSystem p :=
 
 /-- A set system is a compact system iff adding `univ` gives a compact system. -/
 lemma iff_isCompactSystem_of_or_univ : IsCompactSystem p ↔
-    IsCompactSystem (fun s ↦ (p s ∨ (s = univ))) := by
+    IsCompactSystem (fun s ↦ (p s ∨ s = univ)) := by
   refine ⟨fun h ↦ ?_, fun h ↦ mono h (fun s ↦ fun a ↦ Or.symm (Or.inr a))⟩
   wlog ht : Nonempty α
   · rw [not_nonempty_iff] at ht
@@ -117,42 +119,34 @@ lemma iff_isCompactSystem_of_or_univ : IsCompactSystem p ↔
     intro s h' hd
     classical
     by_cases h₀ : ∀ n, ¬p (s n)
-    · have h₁ : ∀ n, s n = univ := by
-        intro n
-        simp only [h₀, false_or] at h'
-        exact h' n
-      simp_rw [h₁, iInter_univ, Set.univ_nonempty]
+    · simp only [h₀, false_or] at h'
+      simp_rw [h', iInter_univ, Set.univ_nonempty]
     · push_neg at h₀
       let n := Nat.find h₀
       let s' := fun i ↦ if p (s i) then s i else s n
       have h₁ : ∀ i, p (s' i) := by
         intro i
         by_cases h₁ : p (s i)
-        · have h₂ : s' i = s i := if_pos h₁
-          exact h₂ ▸ h₁
-        · have h₂ : s' i = s n := if_neg h₁
-          exact h₂ ▸ (Nat.find_spec h₀)
+        · simp only [h₁, ↓reduceIte, s']
+        · simp only [h₁, ↓reduceIte, Nat.find_spec h₀, s', n]
       have h₃ : ∀ i, (p (s i) → s' i = s i) := fun i h ↦ if_pos h
       have h₄ : ∀ i, (¬p (s i) → s' i = s n) := fun i h ↦ if_neg h
       have h₂ : ⋂ i, s i = ⋂ i, s' i := by
+        simp only [s'] at *
         ext x
         simp only [mem_iInter]
         refine ⟨fun h i ↦ ?_, fun h i ↦ ?_⟩
-        · by_cases h₅ : p (s i)
-          · exact (h₃ i h₅) ▸ h i
-          · exact (h₄ i h₅) ▸ h n
-        · have h₄ : s i = s' i ∨ s i = univ := by
-            rcases h' i with a | b
-            · exact Or.inl <| Eq.symm (if_pos a)
-            · exact Or.inr b
-          rcases h₄ with a | b
-          · exact a ▸ h i
-          · simp [b]
+        · by_cases h' : p (s i) <;> simp only [h', ↓reduceIte, h, s', n]
+        · specialize h' i
+          specialize h i
+          rcases h' with a | b
+          · simp only [a, ↓reduceIte, s', n] at h
+            exact h
+          · simp only [b, Set.mem_univ]
       apply h₂ ▸ h s' h₁
       by_contra! a
       obtain ⟨j, hj⟩ := a
-      have h₂ : ∀ (v : ℕ) (hv : n ≤ v), Dissipate s v = Dissipate s' v:= by
-        intro v hv
+      have h₂ (v : ℕ) (hv : n ≤ v) : Dissipate s v = Dissipate s' v:= by
         ext x
         refine ⟨fun h ↦ ?_, fun h ↦ ?_⟩ <;> simp only [dissipate_def, mem_iInter] at h ⊢ <;>
           intro i hi
@@ -173,39 +167,6 @@ lemma iff_isCompactSystem_of_or_univ : IsCompactSystem p ↔
       specialize hd (max j n)
       rw [h₂, Set.nonempty_iff_ne_empty, h₇] at hd
       exact hd rfl
-
-example (a b : Set α) (ha : a = ∅ ) : a ∩ b = ∅ := by
-  subst ha
-  simp_all only [Set.empty_inter]
-
-example (P Q : Prop) (hP : ¬P) (hPQ : P ∨ Q) : Q := by
-  simp_all only [false_or]
-
-
-theorem isPiSystem.iff_isPiSystem_or_empty : IsPiSystem p ↔ IsPiSystem (fun s ↦ p s ∨ s = ∅) := by
-  refine ⟨fun h ↦ ?_, fun h ↦ ?_⟩
-  · intro a ha b hb hab
-    rcases ha with ha₁ | ha₂
-    · rcases hb with hb₁ | hb₂
-      · left
-        exact hpi a ha₁ b hb₁ hab
-      · right
-        exact hb₂ ▸ (Set.inter_empty a)
-    · simp only [ha₂, Set.empty_inter]
-      right
-      rfl
-  · intro a ha b hb hab
-    simp [IsPiSystem] at h
-    specialize h a
-    by_cases ha : a = ∅
-    · sorry
-    ·
-    specialize h a (Or.inl ha) b (Or.inl hb) hab
-    by_cases ha : a = ∅
-    · sorry
-    ·
-    apply h a ha b hb hab
-    sorry
 
 theorem iff_directed (hpi : IsPiSystem p) :
     IsCompactSystem p ↔
@@ -248,7 +209,6 @@ theorem iff_directed (hpi : IsPiSystem p) :
         exact Set.not_nonempty_iff_eq_empty.mp g
     apply h₀ h₁ h2
 
-
 theorem iff_directed' (hpi : IsPiSystem p) :
     IsCompactSystem p ↔
     ∀ (C : ℕ → Set α), ∀ (_ : Directed (fun (x1 x2 : Set α) => x1 ⊇ x2) C), (∀ i, p (C i)) →
@@ -260,7 +220,7 @@ theorem iff_directed' (hpi : IsPiSystem p) :
 
 section IsCompactIsClosed
 
-variable (α : Type*) [TopologicalSpace α]
+variable {α : Type*} [TopologicalSpace α]
 
 /-- The set of compact and closed sets is a compact system. -/
 theorem of_isCompact_isClosed :
@@ -278,9 +238,9 @@ theorem of_isCompact_isClosed :
     have hz : s z ≠ univ := fun h ↦ a.prop <| eq_univ_of_subset hz1 h
     use ⟨z, hz⟩
   have htcl : ∀ (i : { j : ℕ | s j ≠ univ}), IsClosed (s i) :=
-    fun i ↦ (Or.resolve_right (h1 i.val) i.prop).2
+    fun i ↦ (h1 i).2
   have htco : ∀ (i : { j : ℕ | s j ≠ univ}), IsCompact (s i) :=
-    fun i ↦ (Or.resolve_right (h1 i.val) i.prop).1
+    fun i ↦ (h1 i).1
   haveI f : Nonempty α := by
     apply nonempty_of_exists _
     · exact fun x ↦ x ∈ s 0
@@ -300,59 +260,12 @@ theorem of_isCompact_isClosed :
       (fun j ↦ h2 j) htco htcl
   · simp only [ne_eq, coe_setOf, nonempty_subtype, not_exists, not_not, s'] at h
     simp [s', h]
-
 
 /-- The set of sets which are either compact and closed, or `univ`, is a compact system. -/
 theorem of_isCompact_isClosed_or_univ :
     IsCompactSystem (fun s : Set α ↦ (IsCompact s ∧ IsClosed s) ∨ (s = univ)) := by
-  let p := fun (s : Set α) ↦ (IsCompact s ∧ IsClosed s) ∨ (s = univ)
-  have h2 : ∀ (s t : Set α), p s → p t → p (s ∩ t) := by
-    intro s t hs ht
-    by_cases hs' : s = univ
-    · rw [hs', univ_inter]
-      exact ht
-    · by_cases ht' : t = univ
-      · rw [ht', inter_comm, univ_inter]
-        exact hs
-      · exact Or.inl <| ⟨IsCompact.inter_right (Or.resolve_right hs hs').1
-        (Or.resolve_right ht ht').2, IsClosed.inter (Or.resolve_right hs hs').2
-          (Or.resolve_right ht ht').2⟩
-  rw [IsCompactSystem.iff_directed' h2]
-  intro s hs h1 h2
-  let s' := fun (i : { j : ℕ | s j ≠ univ}) ↦ s i
-  have hs' : Directed (fun x1 x2 ↦ x1 ⊇ x2) s' := by
-    intro a b
-    obtain ⟨z, hz1, hz2⟩ := hs a.val b.val
-    have hz : s z ≠ univ := fun h ↦ a.prop <| eq_univ_of_subset hz1 h
-    use ⟨z, hz⟩
-  have htcl : ∀ (i : { j : ℕ | s j ≠ univ}), IsClosed (s i) :=
-    fun i ↦ (Or.resolve_right (h1 i.val) i.prop).2
-  have htco : ∀ (i : { j : ℕ | s j ≠ univ}), IsCompact (s i) :=
-    fun i ↦ (Or.resolve_right (h1 i.val) i.prop).1
-  haveI f : Nonempty α := by
-    apply nonempty_of_exists _
-    · exact fun x ↦ x ∈ s 0
-    · exact h2 0
-  by_cases h : Nonempty ↑{j | s j ≠ Set.univ}
-  · have g :  (⋂ i, s' i).Nonempty → (⋂ i, s i).Nonempty := by
-      rw [Set.nonempty_iInter, Set.nonempty_iInter]
-      rintro ⟨x, hx⟩
-      use x
-      intro i
-      by_cases g : s i ≠ univ
-      · exact hx ⟨i, g⟩
-      · simp only [ne_eq, not_not, s'] at g
-        rw [g]
-        simp only [Set.mem_univ]
-    apply g <| IsCompact.nonempty_iInter_of_directed_nonempty_isCompact_isClosed s' hs'
-      (fun j ↦ h2 j) htco htcl
-  · simp only [ne_eq, coe_setOf, nonempty_subtype, not_exists, not_not, s'] at h
-    simp [s', h]
-
-/-- The set of compact and closed sets is a compact system. -/
-theorem of_isCompact_isClosed :
-    IsCompactSystem (fun s : Set α ↦ IsCompact s ∧ IsClosed s) :=
-  IsCompactSystem.mono (of_isCompact_isClosed_or_univ α) (fun _ hs ↦ Or.inl hs)
+  rw [← iff_isCompactSystem_of_or_univ]
+  exact of_isCompact_isClosed
 
 /-- In a `T2Space` the set of compact sets is a compact system. -/
 theorem of_isCompact [T2Space α] :
@@ -360,7 +273,7 @@ theorem of_isCompact [T2Space α] :
   have h : (fun s : Set α ↦ IsCompact s) = (fun s : Set α ↦ IsCompact s ∧ IsClosed s) := by
     ext s
     refine ⟨fun h' ↦ ⟨h', h'.isClosed⟩, fun h ↦ h.1⟩
-  exact h ▸ (of_isCompact_isClosed α)
+  exact h ▸ (of_isCompact_isClosed)
 
 end IsCompactIsClosed
 
