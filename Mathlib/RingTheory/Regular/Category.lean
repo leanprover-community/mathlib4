a40273ecb59bdfbfb97ae78ca8689336ba8be570
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2025-05-23 18:49:51 +0800

    fix lemma naming
    and docstring

--- Mathlib/RingTheory/Regular/Category.lean
+++ Mathlib/RingTheory/Regular/Category.lean
@@ -16,15 +16,15 @@ variable {R : Type u} [CommRing R] (M : ModuleCat.{v} R)
 
 open CategoryTheory Ideal Pointwise
 
-namespace ModuleCat
-
-lemma smulShortComplex_function_exact (M : Type v) [AddCommGroup M] [Module R M] (r : R) :
+lemma LinearMap.exact_smul_id_smul_top_mkQ (M : Type v) [AddCommGroup M] [Module R M] (r : R) :
     Function.Exact (r • LinearMap.id : M →ₗ[R] M) (r • (⊤ : Submodule R M)).mkQ := by
   intro x
   simp [Submodule.mem_smul_pointwise_iff_exists, Submodule.ideal_span_singleton_smul r ⊤,
     Submodule.mem_smul_pointwise_iff_exists]
 
-/-- The short complex `M → M → M⧸xM` given by an element `x : R`. -/
+namespace ModuleCat
+
+/-- The short (exact) complex `M → M → M⧸xM` obtain from the scalar multiple of `x : R` on `M`. -/
 @[simps]
 def smulShortComplex (r : R) :
     ShortComplex (ModuleCat R) where
@@ -35,11 +35,11 @@ def smulShortComplex (r : R) :
   g := ModuleCat.ofHom (r • (⊤ : Submodule R M)).mkQ
   zero := by
     ext x
-    exact (smulShortComplex_function_exact M r).apply_apply_eq_zero x
+    exact (LinearMap.exact_smul_id_smul_top_mkQ M r).apply_apply_eq_zero x
 
 lemma smulShortComplex_exact (r : R) : (smulShortComplex M r).Exact := by
   simp [smulShortComplex, ShortComplex.ShortExact.moduleCat_exact_iff_function_exact,
-    smulShortComplex_function_exact]
+    LinearMap.exact_smul_id_smul_top_mkQ]
 
 instance smulShortComplex_g_epi (r : R) : Epi (smulShortComplex M r).g := by
   simpa [smulShortComplex, ModuleCat.epi_iff_surjective] using Submodule.mkQ_surjective _
