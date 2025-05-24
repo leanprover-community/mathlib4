8429d2a2fa695fac2b34c62a2e41d07af396c2bb
qawbecrdtey <qawbecrdtey@kaist.ac.kr>
2025-05-25 02:17:51 +0900

    Added lemma 'det_lapMatrix_eq_zero'.

--- Mathlib/Combinatorics/SimpleGraph/LapMatrix.lean
+++ Mathlib/Combinatorics/SimpleGraph/LapMatrix.lean
@@ -135,6 +135,14 @@ theorem lapMatrix_toLin'_apply_eq_zero_iff_forall_reachable (x : V → ℝ) :
     Matrix.toLin' (G.lapMatrix ℝ) x = 0 ↔ ∀ i j : V, G.Reachable i j → x i = x j :=
   G.lapMatrix_mulVec_eq_zero_iff_forall_reachable
 
+@[simp]
+theorem det_lapMatrix_eq_zero [h : Nonempty V] : (G.lapMatrix ℝ).det = 0 := by
+  rw [← Matrix.exists_mulVec_eq_zero_iff]
+  use fun _ ↦ 1
+  refine ⟨?_, (lapMatrix_mulVec_eq_zero_iff_forall_adj G).mpr fun _ _ _ ↦ rfl⟩
+  rw [← Function.support_nonempty_iff]
+  use Classical.choice h; simp
+
 section
 
 variable [DecidableEq G.ConnectedComponent]
