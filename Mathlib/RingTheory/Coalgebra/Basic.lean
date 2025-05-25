a9a05f8df33a7822bef6f68cf5efa07dd85f11fe
Andrew Yang <the.erd.one@gmail.com>
2025-05-25 18:53:33 +0100

    shake

--- Mathlib/RingTheory/Coalgebra/Basic.lean
+++ Mathlib/RingTheory/Coalgebra/Basic.lean
@@ -3,9 +3,6 @@ Copyright (c) 2023 Ali Ramsey. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Ali Ramsey, Eric Wieser
 -/
-import Mathlib.Algebra.Algebra.Bilinear
-import Mathlib.LinearAlgebra.DFinsupp
-import Mathlib.LinearAlgebra.Prod
 import Mathlib.LinearAlgebra.TensorProduct.Finiteness
 import Mathlib.LinearAlgebra.TensorProduct.Associator
 
