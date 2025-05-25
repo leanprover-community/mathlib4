173332e4be2e2fc853f51171ebd894916332c673
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2025-05-25 19:43:04 +0200

    revert theorems for shifted balls

--- Mathlib/Topology/Algebra/Valued/ValuationTopology.lean
+++ Mathlib/Topology/Algebra/Valued/ValuationTopology.lean
@@ -171,6 +171,7 @@ theorem cauchy_iff {F : Filter R} : Cauchy F ↔
 
 variable (R)
 
+-- Unfortunately, the converse only holds when the range of the valuation is dense.
 theorem ball_subset_ball {a b : R} {r s : Γ₀}
     (hrs : r ≤ s) (hab : v (a - b) < s) :
     { x | v (x - a) < r } ⊆ { x | v (x - b) < s } := by
@@ -180,9 +181,10 @@ theorem ball_subset_ball {a b : R} {r s : Γ₀}
   rw [← sub_add_sub_cancel x a b]
   apply map_add_lt _ (lt_of_lt_of_le hx hrs) hab
 
-/-- An open ball in a valued ring is open if its radius is a unit of `rangeGroup₀` -/
-theorem isOpen_ball (a : R) (r : _i.v.rangeGroup₀ˣ) :
-    IsOpen (X := R) {x | v (x - a) < r } := by
+/-- An open ball in a valued ring centered at the origin is open
+if its radius is a unit of `rangeGroup₀`. -/
+theorem isOpen_ball (r : _i.v.rangeGroup₀ˣ) :
+    IsOpen (X := R) {x | v x < r } := by
   rw [isOpen_iff_mem_nhds]
   intro x hx
   rw [mem_nhds]
@@ -191,41 +193,76 @@ theorem isOpen_ball (a : R) (r : _i.v.rangeGroup₀ˣ) :
   use r
   intro y hy
   simp only [Units.coe_map, Submonoid.subtype_apply] at hy
-  rw [← sub_add_sub_cancel y x a]
+  rw [← sub_add_cancel y x]
   apply v.map_add_lt hy hx
 
+open Pointwise
+
+theorem ball_eq_center_vadd_ball (a : R) (r : Γ₀) :
+    {x | v (x - a) < r} = a +ᵥ {x : R | v x < r} := by
+  ext
+  simp only [mem_setOf_eq, mem_vadd_set_iff_neg_vadd_mem, vadd_eq_add,sub_eq_neg_add]
+
+theorem closedBall_eq_center_vadd_closedBall (a : R) (r : Γ₀) :
+    {x | v (x - a) ≤ r} = a +ᵥ {x : R | v x ≤ r} := by
+  ext
+  simp only [mem_setOf_eq, mem_vadd_set_iff_neg_vadd_mem, vadd_eq_add,sub_eq_neg_add]
+
+theorem sphere_eq_center_vadd_sphere (a : R) (r : Γ₀) :
+    {x | v (x - a) = r} = a +ᵥ {x : R | v x = r} := by
+  ext
+  simp only [mem_setOf_eq, mem_vadd_set_iff_neg_vadd_mem, vadd_eq_add,sub_eq_neg_add]
+
+/-- An open ball in a valued ring is open if its radius is a unit of `rangeGroup₀`. -/
+theorem isOpen_ball' (a : R) (r : _i.v.rangeGroup₀ˣ) :
+    IsOpen (X := R) {x | v (x - a) < r } := by
+  rw [ball_eq_center_vadd_ball]
+  exact (isOpen_ball R r).vadd a
+
+-- Compare with `isOpen_ball` which has a stronger assumption on the radius.
 /-- An open ball centered at the origin in a valued ring is closed. -/
-theorem isClosed_ball (a : R) (r : Γ₀) :
-    IsClosed (X := R) {x | v (x - a) < r} := by
+theorem isClosed_ball (r : Γ₀) :
+    IsClosed (X := R) {x | v x < r} := by
   rcases eq_or_ne r 0 with rfl|hr
   · simp
   rw [← isOpen_compl_iff, isOpen_iff_mem_nhds]
   intro b hb
   simp only [mem_compl_iff, mem_setOf_eq, not_lt] at hb
   simp only [mem_nhds_iff]
-  use {x | v (x - b) < v (b - a) }
+  use {x | v (x - b) < v b }
   constructor
   · intro x hx
     simp only [mem_setOf_eq, mem_compl_iff, not_lt] at ⊢ hx
-    rwa [← sub_add_sub_cancel x b a, map_add_eq_of_lt_right v hx]
+    rwa [← sub_add_cancel x b, map_add_eq_of_lt_right v hx]
   constructor
-  · have ha' : IsUnit (⟨v (b - a) , v.mem_rangeGroup₀⟩ : v.rangeGroup₀) := by
+  · have ha' : IsUnit (⟨v b, v.mem_rangeGroup₀⟩ : v.rangeGroup₀) := by
       simp only [isUnit_iff_ne_zero, ne_eq, ← Subtype.coe_inj, MonoidHomWithZero.range₀_coe_zero]
       intro h
-      apply hr
-      rwa [h, le_zero_iff] at hb
-    exact isOpen_ball R b ha'.unit
+      rw [h, le_zero_iff] at hb
+      exact hr hb
+    exact isOpen_ball' R b ha'.unit
   · simp only [mem_setOf_eq, sub_self, map_zero]
     exact lt_of_lt_of_le (zero_lt_iff.mpr hr) hb
 
+/-- An open ball centered at the origin in a valued ring is closed. -/
+theorem isClosed_ball' (a : R) (r : Γ₀) :
+    IsClosed (X := R) {x | v (x - a) < r} := by
+  rw [ball_eq_center_vadd_ball]
+  apply IsClosed.vadd (isClosed_ball R r)
+
 /-- In a valued ring, an open ball whose radius is unit in `rangeGroup₀` is clopen. -/
-theorem isClopen_ball (a : R) (r : _i.v.rangeGroup₀ˣ) :
+theorem isClopen_ball (r : _i.v.rangeGroup₀ˣ) :
+    IsClopen (X := R) {x | v x  < r } :=
+  ⟨isClosed_ball _ _, isOpen_ball _ _⟩
+
+/-- In a valued ring, an open ball whose radius is unit in `rangeGroup₀` is clopen. -/
+theorem isClopen_ball' (a : R) (r : _i.v.rangeGroup₀ˣ) :
     IsClopen (X := R) {x | v (x - a) < r } :=
-  ⟨isClosed_ball _ _ _, isOpen_ball _ _ _⟩
+  ⟨isClosed_ball' _ _ _, isOpen_ball' _ _ _⟩
 
 /-- In a valued ring, a closed ball whose radius is a unit of `rangeGroup₀` is open. -/
-theorem isOpen_closedball {a : R} {r : _i.v.rangeGroup₀ˣ} :
-    IsOpen (X := R) {x | v (x - a) ≤ r} := by
+theorem isOpen_closedball {r : _i.v.rangeGroup₀ˣ} :
+    IsOpen (X := R) {x | v x ≤ r} := by
   rw [isOpen_iff_mem_nhds]
   intro x hx
   rw [mem_nhds]
@@ -234,17 +271,30 @@ theorem isOpen_closedball {a : R} {r : _i.v.rangeGroup₀ˣ} :
   intro b hb
   simp only [mem_setOf_eq] at hx
   simp only [Units.coe_map, Submonoid.subtype_apply] at hb
+  rw [← sub_add_cancel b x]
+  exact v.map_add_le (le_of_lt hb) hx
+
+/-- In a valued ring, a closed ball whose radius is a unit of `rangeGroup₀` is open. -/
+theorem isOpen_closedball' {a : R} {r : _i.v.rangeGroup₀ˣ} :
+    IsOpen (X := R) {x | v (x - a) ≤ r} := by
+  rw [isOpen_iff_mem_nhds]
+  intro x hx
+  rw [mem_nhds]
+  simp only [Units.coe_map, Submonoid.subtype_apply]
+  use r
+  intro b hb
+  simp only [mem_setOf_eq] at hx hb ⊢
   rw [← sub_add_sub_cancel b x a]
   exact v.map_add_le (le_of_lt hb) hx
 
 /-- A closed ball in a valued ring is closed. -/
-theorem isClosed_closedBall (a : R) (r : Γ₀) :
-    IsClosed (X := R) {x | v (x - a) ≤ r} := by
+theorem isClosed_closedBall (r : Γ₀) :
+    IsClosed (X := R) {x | v x ≤ r} := by
   rw [← isOpen_compl_iff, isOpen_iff_mem_nhds]
   intro x hx
   rw [mem_nhds]
   simp only [mem_compl_iff, mem_setOf_eq, not_le] at hx
-  have : IsUnit (⟨v (x - a), v.mem_rangeGroup₀⟩ : v.rangeGroup₀) := by
+  have : IsUnit (⟨v x, v.mem_rangeGroup₀⟩ : v.rangeGroup₀) := by
     simp only [isUnit_iff_ne_zero, ne_eq]
     simp only [← Subtype.coe_inj, MonoidHomWithZero.range₀_coe_zero,
       ← zero_lt_iff]
@@ -254,26 +304,37 @@ theorem isClosed_closedBall (a : R) (r : Γ₀) :
   simp only [Units.coe_map, IsUnit.unit_spec, Submonoid.subtype_apply, mem_setOf_eq, mem_compl_iff,
     not_le]
   intro hy
-  rw [← sub_add_sub_cancel y x a, add_comm, map_add_eq_of_lt_left v hy]
+  rw [← sub_add_cancel y x, add_comm, map_add_eq_of_lt_left v hy]
   exact hx
 
-/-- In a valued ring, a closed ball whose radius is a unit of `v.rangeGroup₀` is clopen. -/
-theorem isClopen_closedBall {a : R} {r : _i.v.rangeGroup₀ˣ} :
-    IsClopen (X := R) {x | v (x - a) ≤ r} :=
-  ⟨isClosed_closedBall _ _ _, isOpen_closedball _⟩
+/-- A closed ball in a valued ring is closed. -/
+theorem isClosed_closedBall' (a : R) (r : Γ₀) :
+    IsClosed (X := R) {x | v (x - a) ≤ r} := by
+  rw [closedBall_eq_center_vadd_closedBall]
+  exact (isClosed_closedBall R r).vadd a
 
-/-- A sphere centred at the origin in a valued ring is clopen. -/
-theorem isClopen_sphere {a : R} {r : Γ₀} (hr : r ≠ 0) :
-    IsClopen (X := R) {x | v (x - a) = r} := by
-  by_cases h : ∃ x, v (x - a) = r
-  · have H : {x : R | v (x - a) = r} = {x | v (x - a) ≤ r} \ {x | v (x - a) < r} := by
+/-- In a valued ring, a closed ball whose radius is a unit of `v.rangeGroup₀` is clopen. -/
+theorem isClopen_closedBall {r : _i.v.rangeGroup₀ˣ} :
+    IsClopen (X := R) {x | v x ≤ r} :=
+  ⟨isClosed_closedBall _ _, isOpen_closedball _⟩
+
+/-- In a valued ring, a closed ball whose radius is a unit of `v.rangeGroup₀` is clopen. -/
+theorem isClopen_closedBall' {a : R} {r : _i.v.rangeGroup₀ˣ} :
+    IsClopen (X := R) {x | v (x - a) ≤ r} :=
+  ⟨isClosed_closedBall' _ _ _, isOpen_closedball' _⟩
+
+/-- A sphere centered at the origin in a valued ring is clopen. -/
+theorem isClopen_sphere {r : Γ₀} (hr : r ≠ 0) :
+    IsClopen (X := R) {x | v x = r} := by
+  by_cases h : ∃ x : R, v x = r
+  · have H : {x : R | v x = r} = {x | v x ≤ r} \ {x | v x < r} := by
       ext x; simp [← le_antisymm_iff]
     rw [H]
     suffices ∃ γ : _i.v.rangeGroup₀ˣ, γ = r by
       obtain ⟨γ, rfl⟩ := this
-      exact IsClopen.diff (isClopen_closedBall R) (isClopen_ball R a γ)
+      exact IsClopen.diff (isClopen_closedBall R) (isClopen_ball R γ)
     obtain ⟨x, h⟩ := h
-    have h' : IsUnit (⟨v (x-a), v.mem_rangeGroup₀⟩ : v.rangeGroup₀) := by
+    have h' : IsUnit (⟨v x, v.mem_rangeGroup₀⟩ : v.rangeGroup₀) := by
       simpa [isUnit_iff_ne_zero, ne_eq, ← Subtype.coe_inj, h] using hr
     use h'.unit
     simp [← h]
@@ -282,29 +343,47 @@ theorem isClopen_sphere {a : R} {r : Γ₀} (hr : r ≠ 0) :
     push_neg at h
     exact h
 
-/-- A sphere centred at the origin in a valued ring is open. -/
-theorem isOpen_sphere {a : R} {r : Γ₀} (hr : r ≠ 0) :
-    IsOpen (X := R) {x | v (x - a) = r} :=
-  isClopen_sphere _ hr |>.isOpen
+/-- A sphere centered at the origin in a valued ring is open. -/
+theorem isOpen_sphere {r : Γ₀} (hr : r ≠ 0) :
+    IsOpen (X := R) {x | v x = r} :=
+  (isClopen_sphere _ hr).isOpen
 
 /-- A sphere centred at the origin in a valued ring is closed. -/
-theorem isClosed_sphere (a : R) (r : Γ₀) :
-    IsClosed (X := R) {x | v (x - a) = r} := by
+theorem isClosed_sphere (r : Γ₀) :
+    IsClosed (X := R) {x | v x = r} := by
   rcases eq_or_ne r 0 with rfl|hr
-  · simpa using isClosed_closedBall R a 0
+  · simpa using isClosed_closedBall R 0
   exact isClopen_sphere _ hr |>.isClosed
+
+-- TODO: prove `IsClopen.smul`
+/-- A sphere in a valued ring is clopen. -/
+theorem isClopen_sphere' {a : R} {r : Γ₀} (hr : r ≠ 0) :
+    IsClopen (X := R) {x | v (x - a) = r} := by
+  rw [sphere_eq_center_vadd_sphere]
+  exact ⟨(isClosed_sphere R r).vadd a, (isClopen_sphere R hr).isOpen.vadd a⟩
+
+/-- A sphere of nonzero radius in a valued ring is open. -/
+theorem isOpen_sphere' {a : R} {r : Γ₀} (hr : r ≠ 0) :
+    IsOpen (X := R) {x | v (x - a) = r} :=
+  (isClopen_sphere' _ hr).isOpen
+
+/-- A sphere in a valued ring is closed. -/
+theorem isClosed_sphere' (a : R) (r : Γ₀) :
+    IsClosed (X := R) {x | v (x - a) = r} := by
+  rw [sphere_eq_center_vadd_sphere]
+  exact (isClosed_sphere R r).vadd a
 
 /-- The closed unit ball in a valued ring is open. -/
 theorem isOpen_integer : IsOpen (_i.v.integer : Set R) := by
   simpa only [sub_zero, Units.val_one, OneMemClass.coe_one] using
-    isOpen_closedball R (a := 0) (r := 1)
+    isOpen_closedball R (r := 1)
 
 @[deprecated (since := "2025-04-25")]
 alias integer_isOpen := isOpen_integer
 
 /-- The closed unit ball of a valued ring is closed. -/
 theorem isClosed_integer : IsClosed (_i.v.integer : Set R) := by
-  simpa only [sub_zero] using isClosed_closedBall R 0 1
+  simpa only [sub_zero] using isClosed_closedBall R 1
 
 /-- The closed unit ball of a valued ring is clopen. -/
 theorem isClopen_integer : IsClopen (_i.v.integer : Set R) :=
