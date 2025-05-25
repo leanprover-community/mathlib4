ba4515270d1fd15ea2a42c01d33bf48c19c34536
Yaël Dillies <yael.dillies@gmail.com>
2025-05-25 08:37:32 +0000

    feat(Coalgebra): mixin for cocommutative coalgebras A coalgebra `A` is cocommutative if its comultiplication `δ : A → A ⊗ A` commutes with the swapping `β : A ⊗ A ≃ A ⊗ A` of the factors in the tensor product.
    From Toric
    
    Co-authored-by: Michał Mrugała <kiolterino@gmail.com>

--- Mathlib/RingTheory/Coalgebra/TensorProduct.lean
+++ Mathlib/RingTheory/Coalgebra/TensorProduct.lean
@@ -140,6 +140,15 @@ instance instCoalgebra : Coalgebra S (A ⊗[R] B) where
     · dsimp
       simp only [one_smul]
 
+instance [IsCocomm R A] [IsCocomm R B] : IsCocomm R (A ⊗[R] B) where
+  comm_comp_comul := by
+    ext x y
+    dsimp
+    conv_rhs => rw [← comm_comul _ x, ← comm_comul _ y]
+    hopf_tensor_induction comul (R := R) x with x₁ x₂
+    hopf_tensor_induction comul (R := R) y with y₁ y₂
+    simp
+
 end TensorProduct
 
 namespace Coalgebra
