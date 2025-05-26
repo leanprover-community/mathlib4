c997513a9262d4f851fcdd723cbe1a5b173ff1ee
xyzw12345 <wangjt2020@163.com>
2025-05-26 14:59:59 +0800

    add proof of lemma

--- Mathlib/RingTheory/KrullDimension/Polynomial.lean
+++ Mathlib/RingTheory/KrullDimension/Polynomial.lean
@@ -3,23 +3,16 @@ Copyright (c) 2025 Jingting Wang. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jingting Wang
 -/
-import Mathlib.Data.Set.Card
-import Mathlib.Algebra.Order.Ring.WithTop
-import Mathlib.RingTheory.KrullDimension.Basic
-import Mathlib.RingTheory.Spectrum.Prime.RingHom
-import Mathlib.RingTheory.LocalRing.ResidueField.Ideal
-import Mathlib.RingTheory.TensorProduct.Basic
-import Mathlib.Algebra.Polynomial.AlgebraMap
-import Mathlib.RingTheory.PolynomialAlgebra
-import Mathlib.RingTheory.PrincipalIdealDomain
 import Mathlib.Algebra.Polynomial.FieldDivision
+import Mathlib.RingTheory.KrullDimension.Basic
+import Mathlib.RingTheory.LocalRing.ResidueField.Ideal
+import Mathlib.RingTheory.PolynomialAlgebra
+import Mathlib.RingTheory.Spectrum.Prime.RingHom
 
 /-!
 # Krull dimension of polynomial ring
 
-This file proves the basic properties of the krull dimension of the polynomial ring over a
-commutative ring
-
+This file proves properties of the krull dimension of the polynomial ring over a commutative ring
 
 ## Main results
 
@@ -27,8 +20,8 @@ commutative ring
   `R` is less than `2 * (ringKrullDim R) + 1`.
 -/
 
-lemma Order.krullDim_le_of_krullDim_preimage_le {α β : Type*} [Preorder α] [Preorder β] (f : α →o β)
-    {m : ℕ} (h : ∀ (x : β), Order.krullDim (f ⁻¹' {x}) ≤ m) :
+lemma Order.krullDim_le_of_krullDim_preimage_le {α β : Type*} [Preorder α] [PartialOrder β]
+    (f : α →o β) {m : ℕ} (h : ∀ (x : β), Order.krullDim (f ⁻¹' {x}) ≤ m) :
     Order.krullDim α ≤ (m + 1) * Order.krullDim β + m := by
   classical
   rw [Order.krullDim, Order.krullDim]
@@ -41,7 +34,12 @@ lemma Order.krullDim_le_of_krullDim_preimage_le {α β : Type*} [Preorder α] [P
     · exact Ne.symm (not_eq_of_beq_eq_false rfl)
     · exact le_iSup (fun p ↦ (p.length : WithBot ℕ∞)) q
   let q : List β := List.dedup (List.map f p.toList)
-  have hq_lt : List.Chain' (· < ·) q := sorry
+  have hq_lt : List.Chain' (· < ·) q := by
+    apply List.Pairwise.chain'
+    simp_rw [lt_iff_le_and_ne, List.pairwise_and_iff]
+    exact ⟨List.Pairwise.sublist (List.dedup_sublist _) (List.Pairwise.map _ f.monotone
+      (List.chain'_iff_pairwise.mp (show p.toList.Chain' (· ≤ ·) from
+        List.Chain'.imp (fun _ _ ↦ le_of_lt) (RelSeries.toList_chain' p)))), List.nodup_dedup _⟩
   have hq_ne_nil : q ≠ [] := by simp [q, RelSeries.toList_ne_nil p]
   let q' : LTSeries β := RelSeries.Equiv.symm ⟨q, ⟨hq_ne_nil, hq_lt⟩⟩
   have : q'.toList = q := congr($((RelSeries.Equiv.right_inv ⟨q, ⟨hq_ne_nil, hq_lt⟩⟩)))
@@ -56,15 +54,31 @@ lemma Order.krullDim_le_of_krullDim_preimage_le {α β : Type*} [Preorder α] [P
       List.length_map, List.mem_map, List.mem_dedup, RelSeries.mem_toList, exists_exists_and_eq_and,
       forall_exists_index, and_imp, forall_apply_eq_imp_iff₂, q]
       intro a ha
-      let p' : List (f ⁻¹' {f a}) := List.map (fun x ↦ ⟨x.val, sorry⟩)
-        (List.filter ((fun x ↦ x == f a) ∘ ⇑f) (RelSeries.toList p)).attach
-
-      sorry
+      set p' := (List.filter ((fun x ↦ x == f a) ∘ f) (RelSeries.toList p))
+      have hp'_ne_nil : p' ≠ [] := List.ne_nil_of_mem <| List.mem_filter.mpr
+        ⟨RelSeries.mem_toList.mpr ha, by simp⟩
+      have hp'_lt : List.Chain' (· < ·) p' := List.Pairwise.chain' <| List.Pairwise.sublist
+        List.filter_sublist (List.chain'_iff_pairwise.mp <| RelSeries.toList_chain' p)
+      let p'' : List (f ⁻¹' {f a}) := List.map (fun x ↦ ⟨x.val, by
+        simpa using (List.mem_filter.mp x.prop).2⟩) p'.attach
+      have hp''_ne_nil : p'' ≠ [] := by simp [p'', hp'_ne_nil]
+      have hp''_lt : List.Chain' (· < ·) p'' := by
+        apply List.chain'_map_of_chain' (R := (· < ·))
+        · simp
+        · apply List.chain'_attach.mpr
+          simp only [Subtype.mk_lt_mk, exists_prop, exists_and_left, p'', q]
+          apply (List.Chain'.iff ?_).mp (List.Chain'.iff_mem.mp hp'_lt)
+          tauto
+      have : p'.length = p''.length := by simp [p'']
+      have := le_trans (Order.LTSeries.length_le_krullDim
+        (RelSeries.fromListChain' p'' hp''_ne_nil hp''_lt)) (h (f a))
+      simp only [RelSeries.fromListChain'_length, Nat.cast_le, tsub_le_iff_right, q] at this
+      omega
   exact ⟨q', h_length⟩
 
 /-- Another version when the `OrderHom` is unbundled -/
-lemma Order.krullDim_le_of_krullDim_preimage_le' {α β : Type*} [Preorder α] [Preorder β] (f : α → β)
-    (h_mono : Monotone f) {m : ℕ} (h : ∀ (x : β), Order.krullDim (f ⁻¹' {x}) ≤ m) :
+lemma Order.krullDim_le_of_krullDim_preimage_le' {α β : Type*} [Preorder α] [PartialOrder β]
+    (f : α → β) (h_mono : Monotone f) {m : ℕ} (h : ∀ (x : β), Order.krullDim (f ⁻¹' {x}) ≤ m) :
     Order.krullDim α ≤ (m + 1) * Order.krullDim β + m :=
   Order.krullDim_le_of_krullDim_preimage_le ⟨f, h_mono⟩ h
 
@@ -95,3 +109,5 @@ theorem Polynomial.ringKrullDim_le {R : Type*} [CommRing R] :
       ← ringKrullDim_eq_of_ringEquiv (polyEquivTensor R (p.asIdeal.ResidueField)).toRingEquiv,
       ← Ring.KrullDimLE_iff_ringKrullDim_le]
     infer_instance
+
+#min_imports
