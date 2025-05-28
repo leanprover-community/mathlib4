809c0025d3321b20507ceb0ca684bdf5ad309f0c
Brian-Nugent <bnugent@uw.edu>
2025-05-28 12:49:18 -0700

    Removed IsLocalRingDimension.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -5228,7 +5228,6 @@ import Mathlib.RingTheory.LocalRing.ResidueField.Defs
 import Mathlib.RingTheory.LocalRing.ResidueField.Ideal
 import Mathlib.RingTheory.LocalRing.RingHom.Basic
 import Mathlib.RingTheory.LocalRing.Subring
-import Mathlib.RingTheory.LocalRingDimension
 import Mathlib.RingTheory.Localization.Algebra
 import Mathlib.RingTheory.Localization.AsSubring
 import Mathlib.RingTheory.Localization.AtPrime
