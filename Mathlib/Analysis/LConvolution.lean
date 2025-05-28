6da6d8b5d61bcdc35edb43e7c9316c9ab458d77b
DavidLedvinka <50644608+DavidLedvinka@users.noreply.github.com>
2025-05-28 12:46:48 -0400

    Update LConvolution.lean
    More fix

--- Mathlib/Analysis/LConvolution.lean
+++ Mathlib/Analysis/LConvolution.lean
@@ -98,9 +98,9 @@ variable [Group G] [MeasurableMul₂ G] [MeasurableInv G]
 
 variable {μ : Measure G} [IsMulLeftInvariant μ] [SFinite μ]
 
-/-- The convolution of AEMeasurable functions is `AEMeasurable`. -/
+/-- The convolution of `AEMeasurable` functions is `AEMeasurable`. -/
 @[to_additive (attr := measurability, fun_prop)
-"The convolution of AEMeasurable functions is AEMeasurable"]
+"The convolution of `AEMeasurable` functions is `AEMeasurable`"]
 theorem aemeasurable_mlconvolution {f g : G → ℝ≥0∞}
     (hf : AEMeasurable f μ) (hg : AEMeasurable g μ) :
     AEMeasurable (f ⋆ₗ[μ] g) μ := by
