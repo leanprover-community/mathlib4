e3a9c5813096d55076eebdd5d34b347f2a47bfb7
Bhavik Mehta <bhavikmehta8@gmail.com>
2025-05-29 18:50:20 +0000

    feat(Order): add gcongr lemmas for Prod.mk (#25301)
    Add three gcongr lemmas to help decompose explicit elements of a product type.
    
    From the [ABC-Exceptions project](https://github.com/b-mehta/ABC-Exceptions)

--- Mathlib/Order/Basic.lean
+++ Mathlib/Order/Basic.lean
@@ -1067,6 +1067,8 @@ lemma le_def : x ≤ y ↔ x.1 ≤ y.1 ∧ x.2 ≤ y.2 := .rfl
 
 @[simp] lemma mk_le_mk : (a₁, b₁) ≤ (a₂, b₂) ↔ a₁ ≤ a₂ ∧ b₁ ≤ b₂ := .rfl
 
+@[gcongr] lemma GCongr.mk_le_mk (ha : a₁ ≤ a₂) (hb : b₁ ≤ b₂) : (a₁, b₁) ≤ (a₂, b₂) := ⟨ha, hb⟩
+
 @[simp] lemma swap_le_swap : x.swap ≤ y.swap ↔ x ≤ y := and_comm
 @[simp] lemma swap_le_mk : x.swap ≤ (b, a) ↔ x ≤ (a, b) := and_comm
 @[simp] lemma mk_le_swap : (b, a) ≤ x.swap ↔ (a, b) ≤ x := and_comm
@@ -1094,6 +1096,9 @@ theorem mk_le_mk_iff_left : (a₁, b) ≤ (a₂, b) ↔ a₁ ≤ a₂ :=
 
 theorem mk_le_mk_iff_right : (a, b₁) ≤ (a, b₂) ↔ b₁ ≤ b₂ :=
   and_iff_right le_rfl
+
+@[gcongr] alias ⟨_, GCongr.mk_le_mk_left⟩ := mk_le_mk_iff_left
+@[gcongr] alias ⟨_, GCongr.mk_le_mk_right⟩ := mk_le_mk_iff_right
 
 theorem mk_lt_mk_iff_left : (a₁, b) < (a₂, b) ↔ a₁ < a₂ :=
   lt_iff_lt_of_le_iff_le' mk_le_mk_iff_left mk_le_mk_iff_left
