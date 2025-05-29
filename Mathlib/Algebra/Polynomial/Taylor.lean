2329990656f40d4fba0d5cebd99eaadec05ef63d
Kenny Lau <kc_kennylau@yahoo.com.hk>
2025-05-29 19:54:02 +0100

    3 new lemmas, 2 lemmas generalised

--- Mathlib/Algebra/Polynomial/Taylor.lean
+++ Mathlib/Algebra/Polynomial/Taylor.lean
@@ -43,6 +43,9 @@ theorem taylor_apply : taylor r f = f.comp (X + C r) :=
 theorem taylor_X : taylor r X = X + C r := X_comp
 
 @[simp]
+theorem taylor_X_pow (n) : taylor r (X ^ n) = (X + C r) ^ n := X_pow_comp
+
+@[simp]
 theorem taylor_C (x : R) : taylor r (C x) = C x := C_comp
 
 theorem taylor_zero (f : R[X]) : taylor 0 f = f := by rw [taylor_apply, C_0, add_zero, comp_X]
@@ -105,7 +108,25 @@ theorem degree_taylor (p : R[X]) (r : R) : degree (taylor r p) = degree p := by
   · rw [degree_eq_natDegree hp, degree_eq_iff_natDegree_eq ((taylor_eq_zero r p).not.2 hp),
       natDegree_taylor]
 
+theorem eq_zero_of_hasseDeriv_eq_zero (f : R[X]) (r : R)
+    (h : ∀ k, (hasseDeriv k f).eval r = 0) : f = 0 := by
+  rw [← taylor_eq_zero r]
+  ext k
+  rw [taylor_coeff, h, coeff_zero]
+
 end Semiring
+
+section Ring
+
+variable {R : Type*} [Ring R]
+
+theorem taylor_injective (r : R) : Function.Injective (taylor r) :=
+  (injective_iff_map_eq_zero' _).2 (taylor_eq_zero r)
+
+@[simp] lemma taylor_inj {r : R} (p q : R[X]) :
+  taylor r p = taylor r q ↔ p = q := (taylor_injective r).eq_iff
+
+end Ring
 
 section CommSemiring
 
@@ -118,6 +139,10 @@ theorem taylor_mul (p q : R[X]) : taylor r (p * q) = taylor r p * taylor r q := 
 @[simps!]
 def taylorAlgHom (r : R) : R[X] →ₐ[R] R[X] :=
   AlgHom.ofLinearMap (taylor r) (taylor_one r) (taylor_mul r)
+
+@[simp]
+theorem taylor_pow (n : ℕ) : taylor r (f ^ n) = taylor r f ^ n :=
+  (taylorAlgHom r).map_pow ..
 
 @[simp, norm_cast] lemma coe_taylorAlgHom : taylorAlgHom r = taylor r :=
   rfl
@@ -156,19 +181,6 @@ noncomputable def taylorEquiv (r : R) : R[X] ≃ₐ[R] R[X] where
 
 theorem taylor_eval_sub (s : R) :
     (taylor r f).eval (s - r) = f.eval s := by rw [taylor_eval, sub_add_cancel]
-
-theorem taylor_injective (r : R) : Function.Injective (taylor r) := by
-  intro f g h
-  apply_fun taylor (-r) at h
-  simpa only [taylor_apply, comp_assoc, add_comp, X_comp, C_comp, C_neg, neg_add_cancel_right,
-    comp_X] using h
-
-theorem eq_zero_of_hasseDeriv_eq_zero (f : R[X]) (r : R)
-    (h : ∀ k, (hasseDeriv k f).eval r = 0) : f = 0 := by
-  apply taylor_injective r
-  rw [LinearMap.map_zero]
-  ext k
-  simp only [taylor_coeff, h, coeff_zero]
 
 /-- Taylor's formula. -/
 theorem sum_taylor_eq (f : R[X]) (r : R) :
