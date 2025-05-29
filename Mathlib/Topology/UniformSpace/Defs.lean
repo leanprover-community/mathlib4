052a3ec844fdd7ac9211192624c363f4dc291aff
Yakov Pechersky <ffxen158@gmail.com>
2025-05-28 21:23:57 -0400

    clean up simp

--- Mathlib/Topology/UniformSpace/Defs.lean
+++ Mathlib/Topology/UniformSpace/Defs.lean
@@ -260,7 +260,7 @@ lemma isSymmetricRel_idRel : IsSymmetricRel (idRel : Set (α × α)) := by
   simp [IsSymmetricRel, idRel, eq_comm]
 
 lemma isSymmetricRel_univ : IsSymmetricRel (Set.univ : Set (α × α)) := by
-  simp [IsSymmetricRel, idRel, eq_comm]
+  simp [IsSymmetricRel]
 
 lemma IsSymmetricRel.preimage_prodMap {U : Set (β × β)} (ht : IsSymmetricRel U) (f : α → β) :
     IsSymmetricRel (Prod.map f f ⁻¹' U) :=
