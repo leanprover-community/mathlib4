e009f64a9393f03a3892f574d86256c61e8b478b
Oliver Butterley <oliver.butterley@gmail.com>
2025-05-30 19:43:12 +0200

    Revert "trim imports"
    This reverts commit e2b20d94d2be844cd2be6ac00af94bafe73db841.

--- Mathlib/MeasureTheory/VectorMeasure/Variation.lean
+++ Mathlib/MeasureTheory/VectorMeasure/Variation.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Oliver Butterley, Yoh Tanimoto
 -/
 import Mathlib.MeasureTheory.VectorMeasure.Basic
+import Mathlib.Analysis.Normed.Module.Basic
 import Mathlib.Analysis.Normed.Group.InfiniteSum
 
 /-!
