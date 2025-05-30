bd69a905416ad34b9b637d016cf61f38a0e64fc6
Christopher Hoskin <mans0954@users.noreply.github.com>
2025-05-30 06:00:32 +0100

    Update Mathlib/Analysis/InnerProductSpace/Basic.lean
    Co-authored-by: Frédéric Dupuis <31101893+dupuisf@users.noreply.github.com>

--- Mathlib/Analysis/InnerProductSpace/Basic.lean
+++ Mathlib/Analysis/InnerProductSpace/Basic.lean
@@ -813,7 +813,7 @@ theorem eq_of_norm_le_re_inner_eq_norm_sq {x y : E} (hle : ‖x‖ ≤ ‖y‖) 
   have H₂ : re ⟪y, x⟫ = ‖y‖ ^ 2 := by rwa [← inner_conj_symm, conj_re]
   simpa [inner_sub_left, inner_sub_right, ← norm_sq_eq_re_inner, h, H₂] using H₁
 
--- Sufficient and necessary condition for equality in the triangle inequality
+/-- Equality is achieved in the triangle inequality iff the two vectors are collinear. -/
 theorem norm_add_eq_iff_real {x y : F} : ‖x + y‖ = ‖x‖ + ‖y‖ ↔ ‖y‖ • x = ‖x‖ • y := by
   rw [← pow_left_inj₀ (norm_nonneg _) (Left.add_nonneg (norm_nonneg _) (norm_nonneg _)) two_ne_zero,
     norm_add_sq (𝕜 := ℝ), add_pow_two, add_left_inj, add_right_inj, re_to_real, mul_assoc,
