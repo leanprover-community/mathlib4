ff2db862e2df92a405162413d94d39ff67bfb02a
DavidLedvinka <david.ledvinka@mail.utoronto.ca>
2025-05-30 03:28:47 -0400

    fix unused imports

--- Mathlib/MeasureTheory/Measure/Regular.lean
+++ Mathlib/MeasureTheory/Measure/Regular.lean
@@ -3,7 +3,7 @@ Copyright (c) 2021 Sébastien Gouëzel. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Sébastien Gouëzel, Floris van Doorn, Yury Kudryashov
 -/
-import Mathlib.MeasureTheory.Constructions.BorelSpace.Order
+import Mathlib.MeasureTheory.Constructions.BorelSpace.Basic
 import Mathlib.MeasureTheory.Group.MeasurableEquiv
 import Mathlib.Topology.MetricSpace.HausdorffDistance
 
