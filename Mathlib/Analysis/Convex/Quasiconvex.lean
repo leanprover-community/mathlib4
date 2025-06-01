a35b8b95bf9e31ee8dcfa84f3579ab241ecf5032
Frédéric Dupuis <dupuisf@iro.umontreal.ca>
2025-05-31 20:29:20 -0400

    quasiconvex

---
+++ Mathlib/Analysis/Convex/Quasiconvex.lean
@@ -0,0 +1,9 @@
+/-
+Copyright (c) 2021 Yaël Dillies. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Yaël Dillies
+-/
+import Mathlib.Analysis.Convex.Function.Quasiconvex
+import Mathlib.Tactic.Linter.DeprecatedModule
+
+deprecated_module (since := "2025-05-31")
