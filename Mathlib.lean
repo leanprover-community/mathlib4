ce1e4bec59d64f9226e379b10151a29524a5dfc4
Oliver Nash <github@olivernash.org>
2025-06-02 15:20:28 +0000

    feat: implement Geck's construction of a Lie algebra associated to a root system with distinguished base (#25285)

--- Mathlib.lean
+++ Mathlib.lean
@@ -4069,6 +4069,7 @@ import Mathlib.LinearAlgebra.RootSystem.Finite.CanonicalBilinear
 import Mathlib.LinearAlgebra.RootSystem.Finite.G2
 import Mathlib.LinearAlgebra.RootSystem.Finite.Lemmas
 import Mathlib.LinearAlgebra.RootSystem.Finite.Nondegenerate
+import Mathlib.LinearAlgebra.RootSystem.GeckConstruction
 import Mathlib.LinearAlgebra.RootSystem.Hom
 import Mathlib.LinearAlgebra.RootSystem.Irreducible
 import Mathlib.LinearAlgebra.RootSystem.IsValuedIn
