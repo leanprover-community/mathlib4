615b2907618a6323e81178cdbd3f74f9064ceb84
Komyyy <pol_tta@outlook.jp>
2025-06-03 00:28:03 +0900

    `Subgroup ↥H ≃o { H' : Subgroup G // H' ≤ H }`

--- Mathlib/Algebra/Group/Subgroup/Ker.lean
+++ Mathlib/Algebra/Group/Subgroup/Ker.lean
@@ -471,6 +471,21 @@ theorem map_subtype_le_map_subtype {G' : Subgroup G} {H K : Subgroup G'} :
     H.map G'.subtype ≤ K.map G'.subtype ↔ H ≤ K :=
   map_le_map_iff_of_injective G'.subtype_injective
 
+/-- Subgroups of the subgroup `H` is considered as a subgroup which is less or equal than `H`. -/
+@[to_additive (attr := simps apply_coe) "Additive subgroups of the additive subgroup `H` is
+considered as an additive subgroup which is less or equal than `H`."]
+def MapSubtype.orderIso (H : Subgroup G) : Subgroup ↥H ≃o { H' : Subgroup G // H' ≤ H } where
+  toFun H' := ⟨H'.map H.subtype, map_subtype_le H'⟩
+  invFun sH' := sH'.1.subgroupOf H
+  left_inv H' := comap_map_eq_self_of_injective H.subtype_injective H'
+  right_inv sH' := Subtype.ext (map_subgroupOf_eq_of_le sH'.2)
+  map_rel_iff' := by simp
+
+@[to_additive (attr := simp)]
+lemma MapSubtype.orderIso_symm_apply (H : Subgroup G) (sH' : { H' : Subgroup G // H' ≤ H }) :
+    (MapSubtype.orderIso H).symm sH' = sH'.1.subgroupOf H :=
+  rfl
+
 @[to_additive]
 theorem map_lt_map_iff_of_injective {f : G →* N} (hf : Function.Injective f) {H K : Subgroup G} :
     H.map f < K.map f ↔ H < K :=
