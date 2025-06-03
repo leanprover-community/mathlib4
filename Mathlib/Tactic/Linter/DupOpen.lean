2a0c9664b106efadb29a2198c3cadabb0d0f7bb5
Damiano Testa <adomani@gmail.com>
2025-06-03 14:15:55 +0200

    docs from review

--- Mathlib/Tactic/Linter/DupOpen.lean
+++ Mathlib/Tactic/Linter/DupOpen.lean
@@ -15,8 +15,14 @@ open Lean Elab Command Linter
 
 namespace Mathlib.Linter
 
-/-- Returns the array of repetitions in the input `l`. -/
-def getReps {α} [BEq α] [Hashable α] (l : List α) (init := (∅ : Std.HashSet α)) :
+/--
+Returns the array of repetitions in the input `l`, where multiple repetitions all get reported.
+For instance
+```lean
+#guard  getRepetitions [1, 2, 3, 2, 1, 2] = #[2, 1, 2]
+```
+-/
+def getRepetitions {α} [BEq α] [Hashable α] (l : List α) (init := (∅ : Std.HashSet α)) :
     Array α := Id.run do
   let mut seen := init
   let mut reps := #[]
@@ -27,7 +33,15 @@ def getReps {α} [BEq α] [Hashable α] (l : List α) (init := (∅ : Std.HashSe
       seen := seen.insert a
   return reps
 
-/-- The "dupOpen" linter emits a warning when an `open` command opens an already open namespace. -/
+/--
+The "dupOpen" linter emits a warning when an `open` command opens an already open namespace.
+
+The linter does *not* check whether the declarations accessible with an `open` are actually used
+with the shorter name, but simply whether an `open` namespace is repeated.
+
+The check is almost purely syntactical: the linter reads the namespaces in `open` and `namespace`
+commands and compares them with the `openDecls` in scope.
+-/
 register_option linter.dupOpen : Bool := {
   defValue := false
   descr := "enable the dupOpen linter"
@@ -42,19 +56,22 @@ def dupOpenLinter : Linter where run := withSetOptionIn fun stx ↦ do
   if (← get).messages.hasErrors then
     return
   let s ← getScope
+  -- `reps` are the repeated namespaces, `os` is the array of identifiers that could be the
+  -- culprits of the repetitions and `isNamespace?` is a boolean that is `true` if the command is
+  -- `namespace`, rather than `open`.
   let (reps, os, isNamespace?) :=
     match stx with
     | `(namespace $ns) =>
       let stringSegments := s.openDecls.flatMap (s!"{·}".splitOn ".")
       let os := stringSegments.map (mkIdentFrom ns <| .str .anonymous ·)
       let names := ns.getId.components.map (·.toString)
-      let reps := getReps names (.ofList stringSegments)
+      let reps := getRepetitions names (.ofList stringSegments)
       (reps, os.toArray, true)
     | `(open $os*) =>
-      let reps := getReps (s.openDecls.map (s!"{·}"))
+      let reps := getRepetitions (s.openDecls.map (s!"{·}"))
       (reps, os, false)
     | _ => default
-  let mut toReport : Std.HashSet (String.Range × Name × String):= ∅
+  let mut toReport : Std.HashSet (String.Range × Name × String) := ∅
   for rep in reps do
     for o in os do
       if rep.endsWith o.getId.toString then
