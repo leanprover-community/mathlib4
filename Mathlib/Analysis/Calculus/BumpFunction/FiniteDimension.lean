1afce00c7cf0292050509b1079fca2fdb9eabd17
grunweg <oliver.butterley@gmail.com>
2025-06-03 10:13:00 +0000

    chore(Analysis/Normed/Group/InfiniteSum): make argument implicit (#25375)
    Make an argument `g` which is followed by an explicit hypothesis `hg` about `g` implicit.
    And make one minor tweak to the file, tidying after #25371.
    
    
    
    Co-authored-by: Michael Rothgang <rothgang@math.uni-bonn.de>

--- Mathlib/Analysis/Calculus/BumpFunction/FiniteDimension.lean
+++ Mathlib/Analysis/Calculus/BumpFunction/FiniteDimension.lean
@@ -155,7 +155,7 @@ theorem IsOpen.exists_smooth_support_eq {s : Set E} (hs : IsOpen s) :
       _ = δ n := by field_simp
   choose r rpos hr using this
   have S : ∀ x, Summable fun n => (r n • g n) x := fun x ↦ by
-    refine .of_nnnorm_bounded _ δc.summable fun n => ?_
+    refine .of_nnnorm_bounded δc.summable fun n => ?_
     rw [← NNReal.coe_le_coe, coe_nnnorm]
     simpa only [norm_iteratedFDeriv_zero] using hr n 0 (zero_le n) x
   refine ⟨fun x => ∑' n, (r n • g n) x, ?_, ?_, ?_⟩
