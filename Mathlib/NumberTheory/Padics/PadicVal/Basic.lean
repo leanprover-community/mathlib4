c357f2abe5bba8eda639d1994bc257b419de666b
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2025-06-04 23:05:44 +0200

    restore comment

--- Mathlib/NumberTheory/Padics/PadicVal/Basic.lean
+++ Mathlib/NumberTheory/Padics/PadicVal/Basic.lean
@@ -508,6 +508,7 @@ lemma Nat.log_ne_padicValNat_succ {n : ℕ} (hn : n ≠ 0) : log 2 n ≠ padicVa
   rw [← Nat.lt_add_one_iff, ← mul_one (2 ^ _)] at h1
   rw [← add_one_le_iff, Nat.pow_succ] at h2
   refine not_dvd_of_between_consec_multiples h1 (lt_of_le_of_ne' h2 ?_) pow_padicValNat_dvd
+  -- TODO(kmill): Why is this `p := 2` necessary?
   exact pow_succ_padicValNat_not_dvd (p := 2) n.succ_ne_zero ∘ dvd_of_eq
 
 lemma Nat.max_log_padicValNat_succ_eq_log_succ (n : ℕ) [hp : Fact (p.Prime)] :
