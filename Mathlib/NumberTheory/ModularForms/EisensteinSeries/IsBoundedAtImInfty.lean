e6a57669371527345fdc21ed16bb7ee28a8b8ea5
Chris Birkbeck <c.birkbeck@uea.ac.uk>
2025-06-04 16:57:08 +0100

    fix2

--- Mathlib/NumberTheory/ModularForms/EisensteinSeries/IsBoundedAtImInfty.lean
+++ Mathlib/NumberTheory/ModularForms/EisensteinSeries/IsBoundedAtImInfty.lean
@@ -27,7 +27,7 @@ we then use our bounds for Eisenstein series in these vertical strips to get the
 
 noncomputable section
 
-open ModularForm UpperHalfPlane Matrix SlashInvariantForm CongruenceSubgroup EisensteinSeries
+open ModularForm UpperHalfPlane  Matrix SlashInvariantForm CongruenceSubgroup
 open scoped MatrixGroups
 
 namespace EisensteinSeries
