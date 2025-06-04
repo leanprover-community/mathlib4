52af25149e613accdc10320bac00c3580d539678
Bhavik Mehta <bhavikmehta8@gmail.com>
2025-06-04 16:18:14 +0100

    fix(Linter): mark `linear_combination` as allowed after a flexible tactic

--- Mathlib/Tactic/Linter/FlexibleLinter.lean
+++ Mathlib/Tactic/Linter/FlexibleLinter.lean
@@ -295,6 +295,7 @@ def flexible : Std.HashSet Name :=
     `Mathlib.Tactic.normNum,
     `linarith,
     `nlinarith,
+    `linear_combination,
     ``Lean.Parser.Tactic.tacticNorm_cast__,
     `Aesop.Frontend.Parser.aesopTactic,
     `Mathlib.Tactic.Tauto.tauto,
