c82ac287d5a3de09cdc37d378acbf053f7f47643
mitchell-horner <29882987+mitchell-horner@users.noreply.github.com>
2025-06-04 23:46:03 +1000

    Refactor TuranDensity.lean

--- Mathlib/Combinatorics/SimpleGraph/Extremal/TuranDensity.lean
+++ Mathlib/Combinatorics/SimpleGraph/Extremal/TuranDensity.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mitchell Horner
 -/
 import Mathlib.Analysis.Asymptotics.AsymptoticEquivalent
+import Mathlib.Combinatorics.Enumerative.DoubleCounting
 import Mathlib.Combinatorics.SimpleGraph.DeleteEdges
 import Mathlib.Combinatorics.SimpleGraph.Extremal.Basic
 import Mathlib.Data.Nat.Choose.Cast
@@ -31,89 +32,62 @@ namespace SimpleGraph
 
 variable {V W : Type*} {G : SimpleGraph V} {H : SimpleGraph W}
 
-/-- If `G` is `H.Free`, then `G.deleteIncidenceSet v` is also `H.Free` and has at most
-`extremalNumber (card V-1) H` many edges. -/
-theorem card_edgeFinset_deleteIncidenceSet_le_extremalNumber
-    [Fintype V] [DecidableRel G.Adj] [DecidableEq V] (h : H.Free G) (v : V) :
-    #(G.deleteIncidenceSet v).edgeFinset ≤ extremalNumber (card V-1) H := by
-  rw [← card_edgeFinset_induce_compl_singleton, ← @card_unique ({v} : Set V), ← card_compl_set]
-  apply card_edgeFinset_le_extremalNumber
-  contrapose! h
-  rw [not_free] at h ⊢
-  exact h.trans ⟨Copy.induce G {v}ᶜ⟩
-
-lemma extremalNumber_div_choose_two_succ_le {n : ℕ} (hn : 2 ≤ n) (H : SimpleGraph W) :
-    (extremalNumber (n+1) H / (n+1).choose 2 : ℝ) ≤ (extremalNumber n H / n.choose 2 : ℝ) := by
+lemma antitoneOn_extremalNumber_div_choose_two (H : SimpleGraph W) :
+    AntitoneOn (fun n ↦ (extremalNumber n H / n.choose 2 : ℝ)) (Set.Ici 2) := by
+  apply antitoneOn_nat_Ici_of_succ_le
+  intro n hn
   conv_lhs =>
-    enter [1, 1, 1]
+    enter [1, 1]
     rw [← Fintype.card_fin (n+1)]
   rw [div_le_iff₀ (mod_cast Nat.choose_pos (by linarith)),
     extremalNumber_le_iff_of_nonneg H (by positivity)]
   intro G _ h
   rw [mul_comm, ← mul_div_assoc, le_div_iff₀' (mod_cast Nat.choose_pos hn), Nat.cast_choose_two,
     Nat.cast_choose_two, Nat.cast_add_one, add_sub_cancel_right (n : ℝ) 1,
-    mul_comm _ (n-1 : ℝ), ← mul_div (n-1 : ℝ), mul_comm _ (n/2 : ℝ), mul_assoc,
+    mul_comm _ (n-1 : ℝ), ← mul_div (n-1 : ℝ), mul_comm _ (n/2 : ℝ), mul_assoc, mul_comm (n-1 : ℝ),
     ← mul_div (n+1 : ℝ), mul_comm _ (n/2 : ℝ), mul_assoc, mul_le_mul_left (by positivity),
     ← Nat.cast_pred (by positivity), ←Nat.cast_mul, ←Nat.cast_add_one, ←Nat.cast_mul, Nat.cast_le]
+  conv_rhs =>
+    rw [← Fintype.card_fin (n+1), ← card_univ]
   -- double counting `(v, e) ↦ v ∉ e`
-  let s := (univ ×ˢ G.edgeFinset).filter fun (v, e) ↦ v ∉ e
-  trans #s
+  apply card_nsmul_le_card_nsmul' (r := fun v e ↦ v ∉ e)
   -- counting `e`
-  · conv_rhs =>
-      rw [card_filter, sum_product_right]
-      enter [2, x]
-      rw [← card_filter]
-    simp_rw [Sym2.mem_toFinset, filter_not, filter_mem_eq_inter, ← sum_attach G.edgeFinset,
-      univ_inter, ← compl_eq_univ_sdiff, card_compl, Fintype.card_fin, card_toFinset_mem_edgeFinset,
-      sum_const, card_attach, smul_eq_mul, Nat.succ_sub_succ_eq_sub, mul_comm]
-    rfl
+  · intro e he
+    simp_rw [← Sym2.mem_toFinset, bipartiteBelow, filter_not, filter_mem_eq_inter, univ_inter,
+      ← compl_eq_univ_sdiff, card_compl, Fintype.card_fin, card_toFinset_mem_edgeFinset ⟨e, he⟩,
+      Nat.cast_id, Nat.reduceSubDiff, le_refl]
   -- counting `v`
-  · conv_rhs =>
-      rw [← Fintype.card_fin (n+1), ← card_univ, ← smul_eq_mul, ← sum_const]
-    conv_lhs =>
-      rw [card_filter, sum_product]
-      enter [2, x]
-      rw [← card_filter]
-    apply sum_le_sum
-    intro i _
+  · intro v hv
     simpa [edgeFinset_deleteIncidenceSet_eq_filter]
-      using card_edgeFinset_deleteIncidenceSet_le_extremalNumber h i
-
-/-- The limit `extremalNumber (Fin n) H / n.choose 2` as `n` approaches `∞` exists. -/
-lemma exists_tendsto_extremalNumber_div_choose_two (H : SimpleGraph W) :
-    ∃ x, Tendsto (fun (n : ℕ) ↦ (extremalNumber n H / n.choose 2 : ℝ)) atTop (𝓝 x) := by
-  let f := fun (n : ℕ) ↦ (extremalNumber n H / n.choose 2 : ℝ)
-  suffices h : ∃ x, Tendsto (fun (n : ℕ) ↦ f (n + 2)) atTop (𝓝 x) by
-    simpa [tendsto_add_atTop_iff_nat 2] using h
-  use ⨅ n, f (n + 2)
-  apply tendsto_atTop_ciInf
-  · apply antitone_nat_of_succ_le
-    intro n
-    apply extremalNumber_div_choose_two_succ_le
-    exact le_add_of_nonneg_left n.zero_le
-  · use 0
-    intro n ⟨_, hn⟩
-    rw [← hn]
-    positivity
+      using card_edgeFinset_deleteIncidenceSet_le_extremalNumber h v
 
 /-- The **Turán density** of a simple graph `H` is the limit of `extremalNumber n H / n.choose 2`
 as `n` approaches `∞`.
 
 See `SimpleGraph.tendsto_turanDensity` for proof of existence. -/
 noncomputable def turanDensity (H : SimpleGraph W) :=
-  limUnder atTop fun (n : ℕ) ↦ (extremalNumber n H / n.choose 2 : ℝ)
+  limUnder atTop fun n ↦ (extremalNumber n H / n.choose 2 : ℝ)
 
 /-- The **Turán density** of a simple graph `H` is well-defined. -/
 theorem tendsto_turanDensity (H : SimpleGraph W) :
-    Tendsto (fun (n : ℕ) ↦ (extremalNumber n H / n.choose 2 : ℝ)) atTop (𝓝 (turanDensity H)) := by
-  have ⟨_, h⟩ := exists_tendsto_extremalNumber_div_choose_two H
-  rwa [← Tendsto.limUnder_eq h] at h
+    Tendsto (fun n ↦ (extremalNumber n H / n.choose 2 : ℝ)) atTop (𝓝 (turanDensity H)) := by
+  let f := fun n ↦ (extremalNumber n H / n.choose 2 : ℝ)
+  suffices h : ∃ x, Tendsto (fun n ↦ f (n + 2)) atTop (𝓝 x) by
+    obtain ⟨_, h⟩ := by simpa [tendsto_add_atTop_iff_nat 2] using h
+    simpa [← Tendsto.limUnder_eq h] using h
+  use ⨅ n, f (n + 2)
+  apply tendsto_atTop_ciInf
+  · rw [antitone_add_nat_iff_antitoneOn_nat_Ici]
+    exact antitoneOn_extremalNumber_div_choose_two H
+  · use 0
+    intro n ⟨_, hn⟩
+    rw [← hn]
+    positivity
 
 /-- `extremalNumber n H` is asymptotically equivalent to `turanDensity H * n.choose 2` as `n`
 approaches `∞`. -/
 theorem isEquivalent_extremalNumber (h : turanDensity H ≠ 0) :
-    (fun (n : ℕ) ↦ (extremalNumber n H : ℝ))
-      ~[atTop] (fun (n : ℕ) ↦ (turanDensity H * n.choose 2 : ℝ)) := by
+    (fun n ↦ (extremalNumber n H : ℝ)) ~[atTop] (fun n ↦ (turanDensity H * n.choose 2 : ℝ)) := by
   have hπ := tendsto_turanDensity H
   apply Tendsto.const_mul (1/turanDensity H : ℝ) at hπ
   simp_rw [one_div_mul_cancel h, div_mul_div_comm, one_mul] at hπ
--- Mathlib/Order/Monotone/Basic.lean
+++ Mathlib/Order/Monotone/Basic.lean
@@ -555,8 +555,42 @@ theorem Nat.rel_of_forall_rel_succ_of_le (r : β → β → Prop) [IsRefl β r] 
 theorem monotone_nat_of_le_succ {f : ℕ → α} (hf : ∀ n, f n ≤ f (n + 1)) : Monotone f :=
   Nat.rel_of_forall_rel_succ_of_le (· ≤ ·) hf
 
+theorem monotone_add_nat_of_le_succ {f : ℕ → α} {k : ℕ} (hf : ∀ n ≥ k, f n ≤ f (n + 1)) :
+    Monotone (fun n ↦ f (n + k)) :=
+  fun _ _ hle ↦ Nat.rel_of_forall_rel_succ_of_le_of_le (· ≤ ·) hf
+    (Nat.le_add_left k _) (Nat.add_le_add_iff_right.mpr hle)
+
+-- TODO replace `{ x | k ≤ x }` with `Set.Ici k`
+theorem monotoneOn_nat_Ici_of_le_succ {f : ℕ → α} {k : ℕ} (hf : ∀ n ≥ k, f n ≤ f (n + 1)) :
+    MonotoneOn f { x | k ≤ x } :=
+  fun _ hab _ _ hle ↦ Nat.rel_of_forall_rel_succ_of_le_of_le (· ≤ ·) hf hab hle
+
+-- TODO replace `{ x | k ≤ x }` with `Set.Ici k`
+theorem monotone_add_nat_iff_monotoneOn_nat_Ici {f : ℕ → α} {k : ℕ} :
+    Monotone (fun n ↦ f (n + k)) ↔ MonotoneOn f { x | k ≤ x } := by
+  refine ⟨fun h x hx y hy hle ↦ ?_, fun h x y hle ↦ ?_⟩
+  · rw [← Nat.sub_add_cancel hx, ← Nat.sub_add_cancel hy]
+    rw [← Nat.sub_le_sub_iff_right hy] at hle
+    exact h hle
+  · rw [← Nat.add_le_add_iff_right] at hle
+    exact h (Nat.le_add_left k x) (Nat.le_add_left k y) hle
+
 theorem antitone_nat_of_succ_le {f : ℕ → α} (hf : ∀ n, f (n + 1) ≤ f n) : Antitone f :=
   @monotone_nat_of_le_succ αᵒᵈ _ _ hf
+
+theorem antitone_add_nat_of_succ_le {f : ℕ → α} {k : ℕ} (hf : ∀ n ≥ k, f (n + 1) ≤ f n) :
+    Antitone (fun n ↦ f (n + k)) :=
+  @monotone_add_nat_of_le_succ αᵒᵈ _ f k hf
+
+-- TODO replace `{ x | k ≤ x }` with `Set.Ici k`
+theorem antitoneOn_nat_Ici_of_succ_le {f : ℕ → α} {k : ℕ} (hf : ∀ n ≥ k, f (n + 1) ≤ f n) :
+    AntitoneOn f { x | k ≤ x } :=
+  @monotoneOn_nat_Ici_of_le_succ αᵒᵈ _ f k hf
+
+-- TODO replace `{ x | k ≤ x }` with `Set.Ici k`
+theorem antitone_add_nat_iff_antitoneOn_nat_Ici {f : ℕ → α} {k : ℕ} :
+    Antitone (fun n ↦ f (n + k)) ↔ AntitoneOn f { x | k ≤ x } :=
+  @monotone_add_nat_iff_monotoneOn_nat_Ici αᵒᵈ _ f k
 
 theorem strictMono_nat_of_lt_succ {f : ℕ → α} (hf : ∀ n, f n < f (n + 1)) : StrictMono f :=
   Nat.rel_of_forall_rel_succ_of_lt (· < ·) hf
