78693c95b2875d626553bd3f69b701a08c5f37da
Xavier Roblot <roblot@math.univ-lyon1.fr>
2025-06-04 11:03:39 +0000

    chore(NumberField): deprecate the file `Ideal` (#25388)
    Move the content of this file to the new file `Ideal.Asymptotics` to make room for more files about ideals of a number field.

--- Mathlib.lean
+++ Mathlib.lean
@@ -4579,7 +4579,7 @@ import Mathlib.NumberTheory.NumberField.EquivReindex
 import Mathlib.NumberTheory.NumberField.FinitePlaces
 import Mathlib.NumberTheory.NumberField.FractionalIdeal
 import Mathlib.NumberTheory.NumberField.House
-import Mathlib.NumberTheory.NumberField.Ideal
+import Mathlib.NumberTheory.NumberField.Ideal.Asymptotics
 import Mathlib.NumberTheory.NumberField.InfinitePlace.Basic
 import Mathlib.NumberTheory.NumberField.InfinitePlace.Embeddings
 import Mathlib.NumberTheory.NumberField.InfinitePlace.Ramification
