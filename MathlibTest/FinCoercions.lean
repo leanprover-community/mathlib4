9f590b68a5b3f6a10af8ac2a9c09a4dee08d0cda
Kim Morrison <kim@tqft.net>
2025-06-04 02:54:44 +0200

    test

---
+++ MathlibTest/FinCoercions.lean
@@ -0,0 +1,31 @@
+-- We verify that after importing Mathlib,
+-- we have not introduced a global coercion from `Nat` to `Fin n`.
+-- Such coercions introduce unexpected invisible wrap-around arithmetic.
+-- `open Fin.CommRing ...` *does* introduce such a coercion.
+
+import Mathlib
+
+set_option pp.mvars false
+
+-- We first verify that there is no global coercion from `Nat` to `Fin n`.
+-- Such a coercion would frequently introduce unexpected modular arithmetic.
+
+#check Fin.instNatCast
+#synth NatCast (Fin 3)
+
+/-- info: fun n => ↑n : ℕ → Fin 3 -/
+#guard_msgs in #check fun (n : Nat) => (n : Fin 3)
+
+-- This coercion is available via `open Fin.CommRing in ...`
+
+section
+
+open Fin.CommRing
+
+variable (m : Nat) (n : Fin 3)
+/-- info: n < ↑m : Prop -/
+#guard_msgs in #check n < m
+
+end
+
+example (x : Fin (n + 1)) (h : x < n) : Fin (n + 1) := x.succ.castLT (by simp [h])
