92c172658165c3d227d990149156cad7debf2348
Peter Pfaffelhuber <41927422+pfaffelh@users.noreply.github.com>
2025-06-05 00:12:09 +0200

    Update Mathlib.lean
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -6387,6 +6387,7 @@ import Mathlib.Topology.Compactness.CompactSystem
 import Mathlib.Topology.Compactness.CompactlyGeneratedSpace
 import Mathlib.Topology.Compactness.DeltaGeneratedSpace
 import Mathlib.Topology.Compactness.CompactlyCoherentSpace
+import Mathlib.Topology.Compactness.DeltaGeneratedSpace
 import Mathlib.Topology.Compactness.Compact
 import Mathlib.Topology.Compactness.Exterior
 import Mathlib.Topology.Compactness.LocallyCompact
