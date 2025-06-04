c49c47a28961c497d75c0311dad6ac57978c19a5
Peter Pfaffelhuber <41927422+pfaffelh@users.noreply.github.com>
2025-06-05 00:09:16 +0200

    Update Mathlib.lean
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -3286,7 +3286,8 @@ import Mathlib.Data.Matrix.RowCol
 import Mathlib.Data.Matrix.Vec
 import Mathlib.Data.Matroid.Basic
 import Mathlib.Data.Matroid.Circuit
-import Mathlib.Data.Matroid.Closure
+import Mathlib.Data.Matrix.Vec
+import Mathlib.Data.Matrix.Reflection
 import Mathlib.Data.Matroid.Constructions
 import Mathlib.Data.Matroid.Dual
 import Mathlib.Data.Matroid.IndepAxioms
