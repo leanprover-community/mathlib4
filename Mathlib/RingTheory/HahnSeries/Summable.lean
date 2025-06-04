7741e3c7209dafc68e9c3bff6fdb06fef305c897
Eric Wieser <wieser.eric@gmail.com>
2025-06-04 00:15:57 +0000

    shake

--- Mathlib/RingTheory/HahnSeries/Summable.lean
+++ Mathlib/RingTheory/HahnSeries/Summable.lean
@@ -3,7 +3,6 @@ Copyright (c) 2021 Aaron Anderson. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Aaron Anderson
 -/
-import Mathlib.Algebra.Ring.Action.Rat
 import Mathlib.RingTheory.HahnSeries.Multiplication
 
 /-!
