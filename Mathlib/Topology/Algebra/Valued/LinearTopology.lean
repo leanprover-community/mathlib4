d07a94effa0d34a710b3cc10446f6d0f7df734c9
Yakov Pechersky <ffxen158@gmail.com>
2025-06-04 15:25:59 -0400

    big golf

--- Mathlib/Topology/Algebra/Valued/LinearTopology.lean
+++ Mathlib/Topology/Algebra/Valued/LinearTopology.lean
@@ -20,49 +20,11 @@ variable {K Γ₀ : Type*} [Field K] [LinearOrderedCommGroupWithZero Γ₀] [Val
 
 instance IsLinearTopology.of_valued :
     IsLinearTopology 𝒪[K] K := by
-  -- a linear topology in this case means a basis of submodules, which our valued field has
-  -- thanks to `Valued.hasBasis_nhds_zero`
-  -- later, we will need to deal with the case of the trivial submodule,
-  -- which is open iff the topology is discrete, so we will claim that the topology is not discrete
-  -- because the discrete topology case is trivially `IsLinearTopology`
-  by_cases hd : DiscreteTopology K
-  · infer_instance
   rw [isLinearTopology_iff_hasBasis_open_submodule]
-  simp_rw [hasBasis_iff, mem_nhds_zero]
-  intro t
-  constructor
-  · rintro ⟨r, hr⟩
-    -- easy implication: given a `r : Γ₀ˣ`, we have an open submodule that is an open ball
-    have : (algebraMap 𝒪[K] K : 𝒪[K] → K) = Subtype.val := rfl
-    -- this is `algebraMap (Ideal K) (Submodule 𝒪[K] K)` but does not have lemmas about it
-    refine ⟨Submodule.map (Algebra.linearMap 𝒪[K] K) (idealBall K r), ?_, ?_⟩
-    · simp only [Submodule.map_coe, Algebra.linearMap_apply, coe_idealBall]
-      rw [← (isOpenEmbedding_algebraMap_integer _).isOpen_iff_image_isOpen]
-      exact isOpen_idealBall K r
-    · refine hr.trans' ?_
-      intro
-      simp +contextual [this]
-  · rintro ⟨I, hI, hIt⟩
-   -- harder implication: given an open submodule `I`, find an open ball in it.
-   -- We need to get some `r : Γ₀ˣ` (really, `r ≠ 0`).
-   -- We will construct such an `r` from a nontrivial element of `I`,
-   -- since `I` must not be trivial itself.
-   -- If it was trivial, then it could not have been open, since then a singleton would be open,
-   -- which contradicts the assumption that the topology is not discrete.
-    have hI' : I ≠ ⊥ := by
-      rintro rfl
-      have : DiscreteTopology K := by
-        rw [discreteTopology_iff_singleton_mem_nhds]
-        intro y
-        simpa using singleton_add_mem_nhds_of_nhds_zero y (hI.mem_nhds rfl)
-      contradiction
-    -- We now extract a nontrivial `x`, which will have a nonzero valuation `r := v x`,
-    -- which we lift into the units `Γ₀ˣ`. The submodule ball associated to that `r` is necessarily
-    -- contained in `I`.
-    obtain ⟨x, hx, hx0⟩ : ∃ y ∈ I, y ≠ 0 := Submodule.exists_mem_ne_zero_of_ne_bot hI'
-    replace hx0 : Valued.v (x : K) ≠ 0 := by simp [hx0]
-    refine ⟨Units.mk0 _ hx0, hIt.trans' ?_⟩
-    exact submoduleBall_v_le_of_mem hx hx0
+  apply (hasBasis_nhds_zero K Γ₀).to_hasBasis
+  · exact fun r _ ↦ ⟨ltSubmodule K r, isOpen_ltSubmodule _ _, subset_rfl⟩
+  · intro I hI
+    simpa [mem_nhds_zero] using hI.mem_nhds I.zero_mem
 
 instance IsLinearTopology.of_valued' :
     IsLinearTopology 𝒪[K] 𝒪[K] := by
