1f004f5d6cda28df99f47b11cba08f376ac03fef
Peter Pfaffelhuber <41927422+pfaffelh@users.noreply.github.com>
2025-06-05 00:09:05 +0200

    Update Mathlib.lean
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -2468,7 +2468,8 @@ import Mathlib.CategoryTheory.Shift.Induced
 import Mathlib.CategoryTheory.Shift.InducedShiftSequence
 import Mathlib.CategoryTheory.Shift.Localization
 import Mathlib.CategoryTheory.Shift.Opposite
-import Mathlib.CategoryTheory.Shift.Pullback
+import Mathlib.CategoryTheory.Shift.Linear
+import Mathlib.CategoryTheory.Shift.Induced
 import Mathlib.CategoryTheory.Shift.Quotient
 import Mathlib.CategoryTheory.Shift.ShiftSequence
 import Mathlib.CategoryTheory.Shift.ShiftedHom
