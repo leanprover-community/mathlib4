5843b425a7b42592c84685fd9654e03240aad358
Yongle Hu <140475041+mbkybky@users.noreply.github.com>
2025-06-04 14:23:59 +0800

    Update Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -5387,6 +5387,7 @@ import Mathlib.RingTheory.QuotSMulTop
 import Mathlib.RingTheory.Radical
 import Mathlib.RingTheory.ReesAlgebra
 import Mathlib.RingTheory.Regular.Depth
+import Mathlib.RingTheory.Regular.Flat
 import Mathlib.RingTheory.Regular.IsSMulRegular
 import Mathlib.RingTheory.Regular.RegularSequence
 import Mathlib.RingTheory.RingHom.Finite
