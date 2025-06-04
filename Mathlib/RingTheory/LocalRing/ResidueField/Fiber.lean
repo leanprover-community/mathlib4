cdaf6d343da9b1a5d76f452bfa1af6df670b00bf
xyzw12345 <wangjt2020@163.com>
2025-06-04 16:51:59 +0800

    fix import

--- Mathlib/RingTheory/LocalRing/ResidueField/Fiber.lean
+++ Mathlib/RingTheory/LocalRing/ResidueField/Fiber.lean
@@ -3,6 +3,7 @@ Copyright (c) 2025 Jingting Wang, Junyan Xu. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jingting Wang, Junyan Xu
 -/
+import Mathlib.RingTheory.Spectrum.Prime.RingHom
 import Mathlib.RingTheory.LocalRing.ResidueField.Ideal
 import Mathlib.RingTheory.Localization.BaseChange
 import Mathlib.RingTheory.TensorProduct.Quotient
