94849e0642094d80b5596d1715e979021387b397
Kim Morrison <kim@tqft.net>
2025-06-05 21:52:47 +1000

    restore test

--- MathlibTest/FinCoercions.lean
+++ MathlibTest/FinCoercions.lean
@@ -10,10 +10,16 @@ set_option pp.mvars false
 -- We first verify that there is no global coercion from `Nat` to `Fin n`.
 -- Such a coercion would frequently introduce unexpected modular arithmetic.
 
-#check Fin.instAddMonoidWithOne
-#synth NatCast (Fin 3)
-
-/-- info: fun n => ↑n : ℕ → Fin 3 -/
+/--
+error: type mismatch
+  n
+has type
+  ℕ : Type
+but is expected to have type
+  Fin 3 : Type
+---
+info: fun n => sorry : (n : ℕ) → ?_ n
+-/
 #guard_msgs in #check fun (n : Nat) => (n : Fin 3)
 
 -- This coercion is available via `open Fin.CommRing in ...`
