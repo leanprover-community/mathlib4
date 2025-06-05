a1acedc5aba4f8252c85bd60804f9dc40141e053
emilyriehl <eriehl@jhu.edu>
2025-06-05 09:40:41 -0400

    removed and

--- Mathlib/CategoryTheory/Category/Cat/Terminal.lean
+++ Mathlib/CategoryTheory/Category/Cat/Terminal.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2025 Emily Riehl. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Robin Carlier and Emily Riehl
+Authors: Robin Carlier, Emily Riehl
 -/
 import Mathlib.CategoryTheory.Category.Cat.Limit
 import Mathlib.CategoryTheory.Limits.Shapes.Terminal
