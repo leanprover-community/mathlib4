84cef20d32ef3a0f5e02905e9633c8931ae90320
Damiano Testa <adomani@gmail.com>
2025-06-06 16:44:11 +0200

    test: changes for PR summary

--- Mathlib/Init.lean
+++ Mathlib/Init.lean
@@ -19,6 +19,7 @@ import Mathlib.Tactic.Linter.UnusedTactic
 import Mathlib.Tactic.Linter.Style
 -- This import makes the `#min_imports` command available globally.
 import Mathlib.Tactic.MinImports
+import Mathlib.Tactic
 
 /-!
 This is the root file in Mathlib: it is imported by virtually *all* Mathlib files.
@@ -78,3 +79,5 @@ register_linter_set linter.mathlibStandardSet :=
   linter.style.setOption
   linter.style.maxHeartbeats
   -- The `docPrime` linter is disabled: https://github.com/leanprover-community/mathlib4/issues/20560
+
+def New := 0
