4e056f05e969dc47abf7acb88b37b1b489970b36
igorkhavkine <igor@tichy>
2025-06-07 16:01:59 +0200

    Added ContinuousLinearMap.coprod_comp_prodComm.

--- Mathlib/Topology/Algebra/Module/Equiv.lean
+++ Mathlib/Topology/Algebra/Module/Equiv.lean
@@ -1082,6 +1082,19 @@ alias ring_inverse_eq_map_inverse := ringInverse_eq_inverse
   rw [← ringInverse_eq_inverse]
   exact Ring.inverse_one _
 
+/-- Composition of a map on a product with the exchange of the product factors. -/
+theorem coprod_comp_prodComm
+  (R : Type*) [Semiring R]
+  (M₁ : Type*) [TopologicalSpace M₁] [AddCommMonoid M₁] [Module R M₁]
+  (M₂ : Type*) [TopologicalSpace M₂] [AddCommMonoid M₂] [Module R M₂]
+  (M : Type*) [TopologicalSpace M] [AddCommMonoid M] [Module R M] [ContinuousAdd M]
+  (f : M₁ →L[R] M) (g : M₂ →L[R] M) :
+    (f.coprod g) ∘L (ContinuousLinearEquiv.prodComm R M₂ M₁) = (g.coprod f) := by
+  ext; all_goals
+  simp only [coe_comp', ContinuousLinearEquiv.coe_coe, Function.comp_apply, inl_apply, inr_apply,
+    ContinuousLinearEquiv.prodComm_apply, Prod.swap_prod_mk, coprod_apply, map_zero,
+    zero_add, add_zero, coprod_comp_inl, coprod_comp_inr]
+
 end ContinuousLinearMap
 
 namespace Submodule
