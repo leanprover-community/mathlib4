6b29ed7d4b2d138c6ce94ea95b92f50e72439113
Calle Sönne <calle.sonne@gmail.com>
2025-06-07 15:02:05 +0100

    import fix

--- Mathlib/CategoryTheory/Category/Grpd.lean
+++ Mathlib/CategoryTheory/Category/Grpd.lean
@@ -3,9 +3,9 @@ Copyright (c) 2019 Yury Kudryashov. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yury Kudryashov, Calle Sönne
 -/
+
 import Mathlib.CategoryTheory.SingleObj
 import Mathlib.CategoryTheory.Limits.Shapes.Products
-import Mathlib.CategoryTheory.Bicategory.Functor.Pseudofunctor
 
 /-!
 # Category of groupoids
