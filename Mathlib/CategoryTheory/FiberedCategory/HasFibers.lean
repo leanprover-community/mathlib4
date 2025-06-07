a45c8d70fb1aeeffe4388e2092c6c9673b8aab6d
Calle Sönne <calle.sonne@gmail.com>
2025-06-07 15:06:09 +0100

    Apply suggestions from code review
    Co-authored-by: Joël Riou <37772949+joelriou@users.noreply.github.com>

--- Mathlib/CategoryTheory/FiberedCategory/HasFibers.lean
+++ Mathlib/CategoryTheory/FiberedCategory/HasFibers.lean
@@ -3,7 +3,6 @@ Copyright (c) 2024 Calle Sönne. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Calle Sönne, Paul Lezeau
 -/
-
 import Mathlib.CategoryTheory.FiberedCategory.Fiber
 import Mathlib.CategoryTheory.FiberedCategory.Fibered
 
