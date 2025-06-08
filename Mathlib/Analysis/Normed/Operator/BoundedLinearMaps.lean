2400dc6817e929b1c25a7a29c5ce26173ee7ca01
igorkhavkine <133780155+igorkhavkine@users.noreply.github.com>
2025-06-08 22:30:33 +0200

    Incorporated suggestion from review.

--- Mathlib/Analysis/Normed/Operator/BoundedLinearMaps.lean
+++ Mathlib/Analysis/Normed/Operator/BoundedLinearMaps.lean
@@ -466,7 +466,7 @@ theorem ContinuousOn.clm_apply {X} [TopologicalSpace X] {f : X → (E →L[𝕜]
     ContinuousOn (fun x ↦ f x (g x)) s :=
   isBoundedBilinearMap_apply.continuous.comp_continuousOn (hf.prodMk hg)
 
-theorem ContinuousOn.clm_coprod {X : Type*} [TopologicalSpace X]
+theorem ContinuousOn.continuousLinearMapCoprod {X : Type*} [TopologicalSpace X]
   {G : Type*} [SeminormedAddCommGroup G] [NormedSpace 𝕜 G]
     {f : X → E →L[𝕜] G} {g : X → F →L[𝕜] G} {s : Set X}
     (hf : ContinuousOn f s) (hg : ContinuousOn g s) :
@@ -474,13 +474,13 @@ theorem ContinuousOn.clm_coprod {X : Type*} [TopologicalSpace X]
   simp only [← comp_fst_add_comp_snd]
   exact (hf.clm_comp continuousOn_const).add (hg.clm_comp continuousOn_const)
 
-theorem Continuous.clm_coprod {X : Type*} [TopologicalSpace X]
+theorem Continuous.continuousLinearMapCoprod {X : Type*} [TopologicalSpace X]
   {G : Type*} [SeminormedAddCommGroup G] [NormedSpace 𝕜 G]
     {f : X → E →L[𝕜] G} {g : X → F →L[𝕜] G}
     (hf : Continuous f) (hg : Continuous g) :
     Continuous (fun x => (f x).coprod (g x)) := by
   apply continuous_iff_continuousOn_univ.mpr
-  exact hf.continuousOn.clm_coprod hg.continuousOn
+  exact hf.continuousOn.continuousLinearMapCoprod hg.continuousOn
 
 end
 
