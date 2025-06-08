14ef6a1ddb0586d9450dd0e4c9a58347819b88a7
Andrew Yang <the.erd.one@gmail.com>
2025-06-08 14:42:39 +0100

    shake

--- Mathlib/RingTheory/DedekindDomain/Different.lean
+++ Mathlib/RingTheory/DedekindDomain/Different.lean
@@ -3,13 +3,9 @@ Copyright (c) 2023 Andrew Yang. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Andrew Yang
 -/
-import Mathlib.RingTheory.DedekindDomain.Ideal
-import Mathlib.RingTheory.Discriminant
-import Mathlib.RingTheory.DedekindDomain.IntegralClosure
 import Mathlib.NumberTheory.KummerDedekind
-import Mathlib.RingTheory.IntegralClosure.IntegralRestrict
-import Mathlib.RingTheory.Trace.Quotient
 import Mathlib.RingTheory.Finiteness.Quotient
+import Mathlib.RingTheory.Trace.Quotient
 
 /-!
 # The different ideal
