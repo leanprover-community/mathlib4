3b22d36207a43f9a4ddca0c6ede8a8c7cc97da53
Andrew Yang <36414270+erdOne@users.noreply.github.com>
2025-06-09 07:54:34 +0100

    Update FractionRingAlgebra.lean

--- Mathlib/RingTheory/Localization/FractionRingAlgebra.lean
+++ Mathlib/RingTheory/Localization/FractionRingAlgebra.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2025 Andrew Yang. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Andrew Yang
+-/
 import Mathlib.RingTheory.Localization.Integral
 import Mathlib.RingTheory.RingHom.Finite
 
