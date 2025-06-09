25e97a2724ede29a2012053a78f98f052ddc56a9
Vlad Tsyrklevich <vlad@tsyrklevi.ch>
2025-06-09 14:26:12 +0100

    feat(data): List.Chain' helper lemmas
    Add two helpers lemmas to go from a List.Chain' hypothesis to a concrete
    predicate about two consecutive elements, one in the positive and one in
    the negative.

--- Mathlib/Data/List/Chain.lean
+++ Mathlib/Data/List/Chain.lean
@@ -304,6 +304,28 @@ theorem Chain'.imp_head {x y} (h : ∀ {z}, R x z → R y z) {l} (hl : Chain' R 
     Chain' R (y :: l) :=
   hl.tail.cons' fun _ hz => h <| hl.rel_head? hz
 
+theorem chain'_getElem (h : List.Chain' R l) (n : ℕ) (h' : n + 1 < l.length) :
+    R l[n] l[n+1] :=
+  chain'_pair.mp <| Chain'.infix h ⟨l.take n, l.drop (n + 2), by simp⟩
+
+theorem chain'_of_not (h : ¬List.Chain' R l) : ∃ n : ℕ, ∃ h : n + 1 < l.length, ¬R l[n] l[n+1] := by
+  contrapose! h
+  induction l with
+  | nil => simp
+  | cons head tail ih =>
+      rw [chain'_cons']
+      constructor
+      · by_cases h' : tail.length = 0
+        · simp [eq_nil_iff_length_eq_zero.mpr h']
+        · intro y yh
+          simp only [head?_eq_getElem?, Option.mem_def] at yh
+          obtain ⟨_, rfl⟩ := getElem?_eq_some_iff.mp yh
+          have := h 0 (by rw [length_cons]; omega)
+          rwa [getElem_cons_zero] at this
+      · refine ih (fun n h' => ?_)
+        have := h (n + 1) (by rw [length_cons]; omega)
+        simpa using this
+
 theorem chain'_reverse : ∀ {l}, Chain' R (reverse l) ↔ Chain' (flip R) l
   | [] => Iff.rfl
   | [a] => by simp only [chain'_singleton, reverse_singleton]
