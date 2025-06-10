afab88bbdd36c473cbe27bee5b9246795da24ab6
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2025-06-10 23:04:21 +0100

    add `Jovan Gerbscheid` to author list

--- Mathlib/Tactic/GCongr/Core.lean
+++ Mathlib/Tactic/GCongr/Core.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2023 Mario Carneiro, Heather Macbeth. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Mario Carneiro, Heather Macbeth
+Authors: Mario Carneiro, Heather Macbeth, Jovan Gerbscheid
 -/
 import Lean
 import Batteries.Lean.Except
