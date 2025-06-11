8899bc662f73b2d08bf263b9cdc3977083eb8327
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2025-06-11 03:07:20 +0100

    finish (hopefully) implementation

--- Mathlib/Tactic/GRW.lean
+++ Mathlib/Tactic/GRW.lean
@@ -4,11 +4,12 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Sebastian Zimmer, Mario Carneiro, Heather Macbeth
 -/
 import Mathlib.Tactic.GRW.Core
-
+import Mathlib.Tactic.GCongr
 /-!
-# `grw` tactic
+# the `grewrite` tactic
 
-A generalization of the `rw` tactic to work with relations other than equality.
+The `grw`/`grewrite` tactic is a generalization of the `rewrite` tactic that works with relations
+other than equality.
 
 -/
 
@@ -16,52 +17,58 @@ namespace Mathlib.Tactic
 
 open Lean Meta Elab Parser Tactic
 
-def grewriteTarget (stx : Syntax) (symm : Bool) (config : Rewrite.Config := {}) : TacticM Unit := do
-  Term.withSynthesize <| withMainContext do
+def grewriteTarget (stx : Syntax) (symm : Bool) (config : GRewrite.Config) : TacticM Unit := do
+  let goal ← getMainGoal
+  Term.withSynthesize <| goal.withContext do
     let e ← elabTerm stx none true
     if e.hasSyntheticSorry then
       throwAbortTactic
-    let r ← (← getMainGoal).rewrite (← getMainTarget) e symm (config := config)
-    let mvarId' ← (← getMainGoal).replaceTargetEq r.eNew r.eqProof
-    replaceMainGoal (mvarId' :: r.mvarIds)
+    let goal ← getMainGoal
+    let target ← goal.getType
+    let r ← goal.grewrite target e (forwardImp := false) (symm := symm) (config := config)
+    let mvarNew ← mkFreshExprSyntheticOpaqueMVar r.eNew (← goal.getTag)
+    goal.assign (mkApp r.impProof mvarNew)
+    replaceMainGoal (mvarNew.mvarId! :: r.mvarIds)
 
-def grewriteLocalDecl (stx : Syntax) (symm : Bool) (fvarId : FVarId) (config : Rewrite.Config := {}) :
+def grewriteLocalDecl (stx : Syntax) (symm : Bool) (fvarId : FVarId) (config : GRewrite.Config) :
     TacticM Unit := withMainContext do
-  -- Note: we cannot execute `replaceLocalDecl` inside `Term.withSynthesize`.
+  -- Note: we cannot execute `replace` inside `Term.withSynthesize`.
   -- See issues #2711 and #2727.
-  let rwResult ← Term.withSynthesize <| withMainContext do
+  let goal ← getMainGoal
+  let r ← Term.withSynthesize <| withMainContext do
     let e ← elabTerm stx none true
     if e.hasSyntheticSorry then
       throwAbortTactic
     let localDecl ← fvarId.getDecl
-    (← getMainGoal).rewrite localDecl.type e symm (config := config)
-  let replaceResult ← (← getMainGoal).replaceLocalDecl fvarId rwResult.eNew rwResult.eqProof
-  replaceMainGoal (replaceResult.mvarId :: rwResult.mvarIds)
+    goal.grewrite localDecl.type e (forwardImp := true) (symm := symm) (config := config)
+  let proof := .app (r.impProof) (.mvar goal)
+  let { mvarId, .. } ← goal.replace fvarId proof r.eNew
+  replaceMainGoal (mvarId :: r.mvarIds)
 
-declare_config_elab elabGRewriteConfig Rewrite.Config
+declare_config_elab elabGRewriteConfig GRewrite.Config
 
 /--
-`grw [e]` works just like `rw [e]`, but `e` can be a relation other than `=` or `↔`.
-
+`grewrite [e]` works just like `rewerite [e]`, but `e` can be a relation other than `=` or `↔`.
 
 For example,
 ```lean
 example (h₁ : a < b) (h₂ : b ≤ c) : a + d ≤ c + d := by
-  grw [h₁, h₂]
+  grewrite [h₁, h₂]; rfl
 
-example
+example (h : a ≡ b [ZMOD n]) : a ^ 2 ≡ b ^ 2 [ZMOD n] := by
+  grewrite [h]; rfl
 
 example : (h₁ : a ∣ b) (h₂ : c ∣ a * d) : a ∣ b * d := by
-  grw [h₁]
+  grewrite [h₁]
   exact h₂
 
 ```
-To be able to use `grewrite`, the relevant lemmas need to be tagged with `grw`
+To be able to use `grewrite`, the relevant lemmas need to be tagged with `@[gcongr]`.
+To rewrite inside a transitive relation, you can also give it an `IsTrans` instance.
 -/
-
 syntax (name := grewriteSeq) "grewrite" optConfig rwRuleSeq (location)? : tactic
 
-@[tactic grewriteSeq] def evalGRewriteSeq : Tactic := fun stx => do
+@[tactic grewriteSeq, inherit_doc grewriteSeq] def evalGRewriteSeq : Tactic := fun stx => do
   let cfg ← elabGRewriteConfig stx[1]
   let loc := expandOptLocation stx[3]
   withRWRulesSeq stx[0] stx[2] fun symm term => do
@@ -69,5 +76,68 @@ syntax (name := grewriteSeq) "grewrite" optConfig rwRuleSeq (location)? : tactic
       (grewriteLocalDecl term symm · cfg)
       (grewriteTarget term symm cfg)
       (throwTacticEx `grewrite · "did not find instance of the pattern in the current goal")
+
+/--
+`grw [e]` works just like `rw [e]`, but `e` can be a relation other than `=` or `↔`.
+
+For example,
+```lean
+example (h₁ : a < b) (h₂ : b ≤ c) : a + d ≤ c + d := by
+  grw [h₁, h₂]
+
+example (h : a ≡ b [ZMOD n]) : a ^ 2 ≡ b ^ 2 [ZMOD n] := by
+  grw [h]
+
+example : (h₁ : a ∣ b) (h₂ : c ∣ a * d) : a ∣ b * d := by
+  grw [h₁]
+  exact h₂
+
+```
+To be able to use `grw`, the relevant lemmas need to be tagged with `@[gcongr]`.
+To rewrite inside a transitive relation, you can also give it an `IsTrans` instance.
+-/
+macro (name := rwSeq) "grw " c:optConfig s:rwRuleSeq l:(location)? : tactic =>
+  match s with
+  | `(rwRuleSeq| [$rs,*]%$rbrak) =>
+    -- We show the `rfl` state on `]`
+    `(tactic| (grewrite $c [$rs,*] $(l)?; with_annotate_state $rbrak (try (with_reducible rfl))))
+  | _ => Macro.throwUnsupported
+
+
+/-- `apply_rw [rules]` is a shorthand for `grw +implicationHyp [rules]`.
+This does rewriting up to unfolding of regular definitions (by comparison to regular `rw`
+which only unfolds `@[reducible]` definitions). -/
+macro "apply_rw" c:optConfig s:rwRuleSeq loc:(location)? : tactic => do
+  `(tactic| grw $[$(getConfigItems c)]* +implicationHyp $s:rwRuleSeq $(loc)?)
+
+
+/-- `nth_grewrite` is just like `nth_rewrite`, but for `grewrite`. -/
+syntax (name := nthGRewriteSeq) "nth_grewrite" optConfig ppSpace num+ rwRuleSeq (location)? : tactic
+
+@[inherit_doc nthGRewriteSeq, tactic nthGRewriteSeq]
+def evalNthGRewriteSeq : Tactic := fun stx => do
+  match stx with
+  | `(tactic| nth_grewrite $cfg:optConfig $[$n]* $_rules:rwRuleSeq $[$loc]?) =>
+    let cfg ← elabGRewriteConfig cfg
+    let loc := expandOptLocation (mkOptionalNode loc)
+    let occ := Occurrences.pos (n.map TSyntax.getNat).toList
+    let cfg := { cfg with occs := occ }
+    withRWRulesSeq stx[0] stx[3] fun symm term => do
+      withLocation loc
+        (grewriteLocalDecl term symm · cfg)
+        (grewriteTarget term symm cfg)
+        (throwTacticEx `nth_grewrite · "did not find instance of the pattern in the current goal")
+  | _ => throwUnsupportedSyntax
+
+/-- `nth_grw` is just like `nth_rw`, but for `grw`. -/
+macro (name := nthGRwSeq) "nth_grw" c:optConfig ppSpace n:num+ s:rwRuleSeq l:(location)? : tactic =>
+  -- Note: This is a direct copy of `nth_rw` from core.
+  match s with
+  | `(rwRuleSeq| [$rs,*]%$rbrak) =>
+    -- We show the `rfl` state on `]`
+    `(tactic| (nth_grewrite $c:optConfig $[$n]* [$rs,*] $(l)?; with_annotate_state $rbrak
+      (try (with_reducible rfl))))
+  | _ => Macro.throwUnsupported
+
 
 end Mathlib.Tactic
--- Mathlib/Tactic/GRW/Core.lean
+++ Mathlib/Tactic/GRW/Core.lean
@@ -14,9 +14,16 @@ This module defines the core of the `grw` tactic.
 
 -/
 
-open Lean Meta Elab Tactic
+open Lean Meta
 
 namespace Mathlib.Tactic
+
+def GRewrite.dischargeMain (hrel : Expr) (goal : MVarId) : MetaM Unit := do
+  try
+    goal.gcongrForward #[hrel]
+  catch _ =>
+    throwTacticEx `grewrite goal m!"could not discharge {← goal.getType} using {← inferType hrel}"
+
 
 structure GRewriteResult where
   /-- The rewritten expression -/
@@ -26,11 +33,10 @@ structure GRewriteResult where
   /-- The new side goals -/
   mvarIds  : List MVarId -- new goals
 
-axiom getRel (e : Expr) : Option (Name × Expr × Expr)
-
 /-- Configures the behavior of the `rewrite` and `rw` tactics. -/
 structure GRewrite.Config extends Rewrite.Config where
   useRewrite : Bool := true
+  implicationHyp : Bool := false
 
 declare_config_elab elabRewriteConfig Rewrite.Config
 
@@ -44,58 +50,66 @@ If `symm = false`, we rewrite `e` to `eNew := e[x/y]`; otherwise `eNew := e[y/x]
 
 The code aligns with `Lean.MVarId.rewrite` as much as possible.
 -/
-noncomputable
-def _root_.Lean.MVarId.grewrite (mvarId : MVarId) (e : Expr) (hrel : Expr) (forwardImp : Bool)
-    (symm : Bool := false) (config := { : GRewrite.Config }) : MetaM GRewriteResult :=
-  mvarId.withContext do
-    mvarId.checkNotAssigned `grewrite
+def _root_.Lean.MVarId.grewrite (goal : MVarId) (e : Expr) (hrel : Expr)
+    (forwardImp symm : Bool) (config : GRewrite.Config) : MetaM GRewriteResult :=
+  goal.withContext do
+    goal.checkNotAssigned `grewrite
     let hrelType ← instantiateMVars (← inferType hrel)
-    let (newMVars, binderInfos, hrelType) ← forallMetaTelescopeReducing hrelType
+    let maxMVars? := if config.implicationHyp then some (hrelType.getForallArity - 1) else none
+    let (newMVars, binderInfos, hrelType) ← forallMetaTelescopeReducing hrelType maxMVars?
+
     -- If we can use the normal `rewrite` tactic, we default to using that.
     if (hrelType.isAppOfArity ``Iff 2 || hrelType.isAppOfArity ``Eq 3) && config.useRewrite then
-      let x ← mvarId.rewrite e hrel symm config.toConfig
-      sorry
-    else
+      let ⟨eNew, eqProof, subgoals⟩ ← goal.rewrite e hrel symm config.toConfig
+      let proof ← if forwardImp then
+        mkAppOptM ``cast #[e, eNew, eqProof]
+      else
+        mkAppOptM ``cast #[eNew, e, ← mkEqSymm eqProof]
+      return ⟨eNew, proof, subgoals⟩
+
     let hrelIn := hrel
+    -- check that `hrel` proves a relation
     let hrel := mkAppN hrel newMVars
-    let some (relName, lhs, rhs) := getRel hrelType |
-      throwTacticEx `grewrite mvarId m!"{hrelType} is not a relation"
+    let some (_, lhs, rhs) := GCongr.getRel hrelType |
+      throwTacticEx `grewrite goal m!"{hrelType} is not a relation"
     let (lhs, rhs) := if symm then (rhs, lhs) else (lhs, rhs)
     if lhs.getAppFn.isMVar then
-      throwTacticEx `grewrite mvarId
+      throwTacticEx `grewrite goal
         m!"pattern is a metavariable{indentExpr lhs}\nfrom equation{indentExpr hrelType}"
+    -- abstract the occurrences of `lhs` from `e` to get `eAbst`
     let e ← instantiateMVars e
     let eAbst ←
       withConfig (fun oldConfig => { config, oldConfig with }) <| kabstract e lhs config.occs
     unless eAbst.hasLooseBVars do
-      throwTacticEx `grewrite mvarId
+      throwTacticEx `grewrite goal
         m!"did not find instance of the pattern in the target expression{indentExpr lhs}"
-    -- construct grewrite proof
+    -- construct `eNew` by instantiating `eAbst` with `rhs`.
     let eNew := eAbst.instantiate1 rhs
     let eNew ← instantiateMVars eNew
+    -- check that `eNew` is well typed
     try
       check eNew
     catch ex =>
-      throwTacticEx `grewrite mvarId m!"\
-        new expression is not type correct:{indentD eNew}\nError: {ex.toMessageData}\
+      throwTacticEx `grewrite goal m!"\
+        rewritten expression is not type correct:{indentD eNew}\nError: {ex.toMessageData}\
         \n\n\
         Possible solutions: use grewrite's 'occs' configuration option to limit which occurrences \
-        are rewritten, or use 'gcongr'."
+        are rewritten, or specify what the rewritten expression should be and use 'gcongr'."
     let eNew ← if rhs.hasBinderNameHint then eNew.resolveBinderNameHint else pure eNew
+    -- construct the implication proof using `gcongr`
     let template := eAbst.instantiate1 (← mkFreshExprSyntheticOpaqueMVar default)
-
     let mkImp (e₁ e₂ : Expr) : Expr := .forallE `_a e₁ e₂ .default
     let imp := if forwardImp then mkImp e eNew else mkImp eNew e
     let gcongrGoal ← mkFreshExprMVar imp
-    let (_, _, _) ← gcongrGoal.mvarId!.gcongr template [] (inGRewrite := true)
-
-    postprocessAppMVars `grewrite mvarId newMVars binderInfos
+    let (_, _, sideGoals) ← gcongrGoal.mvarId!.gcongr template [] (inGRewrite := true)
+      (mainGoalDischarger := GRewrite.dischargeMain hrel)
+    -- post-process the metavariables
+    postprocessAppMVars `grewrite goal newMVars binderInfos
       (synthAssignedInstances := !tactic.skipAssignedInstances.get (← getOptions))
-    let newMVarIds ← newMVars.map Expr.mvarId! |>.filterM fun mvarId => not <$> mvarId.isAssigned
+    let newMVarIds ← (sideGoals ++ newMVars.map Expr.mvarId!).filterM (not <$> ·.isAssigned)
     let otherMVarIds ← getMVarsNoDelayed hrelIn
     let otherMVarIds := otherMVarIds.filter (!newMVarIds.contains ·)
     let newMVarIds := newMVarIds ++ otherMVarIds
     pure { eNew, impProof := ← instantiateMVars gcongrGoal, mvarIds := newMVarIds.toList }
-
 
 end Mathlib.Tactic
