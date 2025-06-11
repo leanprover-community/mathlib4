fbaf610c8d9dc58bc3f111b57d9647990dab015a
Sebastien Gouezel <sebastien.gouezel@univ-rennes1.fr>
2025-06-11 18:28:23 +0200

    Update Mathlib/LinearAlgebra/AffineSpace/AffineEquiv.lean
    Co-authored-by: Jireh Loreaux <loreaujy@gmail.com>

--- Mathlib/LinearAlgebra/AffineSpace/AffineEquiv.lean
+++ Mathlib/LinearAlgebra/AffineSpace/AffineEquiv.lean
@@ -163,7 +163,7 @@ def symm (e : P₁ ≃ᵃ[k] P₂) : P₂ ≃ᵃ[k] P₁ where
         LinearEquiv.apply_symm_apply, Equiv.apply_symm_apply]
 
 @[simp]
-theorem symm_toEquiv (e : P₁ ≃ᵃ[k] P₂) : e.symm.toEquiv = e.toEquiv.symm :=
+theorem toEquiv_symm (e : P₁ ≃ᵃ[k] P₂) : e.symm.toEquiv = e.toEquiv.symm :=
   rfl
 
 @[simp]
