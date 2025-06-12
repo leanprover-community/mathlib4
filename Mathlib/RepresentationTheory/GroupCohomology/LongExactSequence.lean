c4a17b91af57d77ae531e5bcdb80eefe73825f9f
101damnations <101damnations@github.com>
2025-06-12 16:04:27 +0100

    whoops

--- Mathlib/RepresentationTheory/GroupCohomology/LongExactSequence.lean
+++ Mathlib/RepresentationTheory/GroupCohomology/LongExactSequence.lean
@@ -28,46 +28,6 @@ universe u v
 
 namespace CategoryTheory.ShortComplex.ShortExact
 
-variable {C : Type u} [Category.{v} C] {FC : C → C → Type*} {CC : C → Type v}
-  [∀ X Y, FunLike (FC X Y) (CC X) (CC Y)] [ConcreteCategory.{v} C FC] [HasForget₂ C Ab.{v}]
-  [Abelian C] [(forget₂ C Ab).Additive] [(forget₂ C Ab).PreservesHomology]
-  {ι : Type*} {c : ComplexShape ι}
-
-variable {S : ShortComplex (HomologicalComplex C c)} (hS : S.ShortExact) (i j : ι) (hij : c.Rel i j)
-
-include hS in
-theorem d_eq_zero_of_f_eq_d_apply
-    (x₂ : ((forget₂ C Ab).obj (S.X₂.X i))) (x₁ : ((forget₂ C Ab).obj (S.X₁.X j)))
-    (hx₁ : ((forget₂ C Ab).map (S.f.f j)) x₁ = ((forget₂ C Ab).map (S.X₂.d i j)) x₂) (k : ι) :
-    ((forget₂ C Ab).map (S.X₁.d j k)) x₁ = 0 := by
-  have := hS.mono_f
-  apply (Preadditive.mono_iff_injective (S.f.f k)).1 inferInstance
-  rw [← ConcreteCategory.forget₂_comp_apply, ← HomologicalComplex.Hom.comm,
-    ConcreteCategory.forget₂_comp_apply, hx₁, ← ConcreteCategory.forget₂_comp_apply,
-    HomologicalComplex.d_comp_d, Functor.map_zero, map_zero]
-  rfl
-
-end CategoryTheory.ShortComplex.ShortExact
-namespace LinearMap
-
-variable {R M M₂ : Type*} [Semiring R]
-
-lemma ker_compLeft [AddCommMonoid M] [AddCommMonoid M₂]
-    [Module R M] [Module R M₂] (f : M →ₗ[R] M₂) (I : Type*) :
-    LinearMap.ker (f.compLeft I) = Submodule.pi (Set.univ : Set I) (fun _ => LinearMap.ker f) :=
-  Submodule.ext fun _ => ⟨fun (hx : _ = _) i _ => congr_fun hx i,
-    fun hx => funext fun i => hx i trivial⟩
-
-lemma range_compLeft [AddCommMonoid M] [AddCommMonoid M₂]
-    [Module R M] [Module R M₂] (f : M →ₗ[R] M₂) (I : Type*) :
-    LinearMap.range (f.compLeft I) =
-      Submodule.pi (Set.univ : Set I) (fun _ => LinearMap.range f) :=
-  Submodule.ext fun _ => ⟨fun ⟨y, hy⟩ i _ => ⟨y i, congr_fun hy i⟩, fun hx => by
-    choose y hy using hx
-    exact ⟨fun i => y i trivial, funext fun i => hy i trivial⟩⟩
-
-end LinearMap
-
 namespace groupCohomology
 
 open CategoryTheory ShortComplex
