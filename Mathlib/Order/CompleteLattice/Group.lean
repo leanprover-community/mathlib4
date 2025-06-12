6640b95923d64ce84e27e2f16bb1e4c219cbc4b8
Jireh Loreaux <loreaujy@gmail.com>
2025-06-12 11:14:43 -0500

    Apply suggestions from code review

--- Mathlib/Order/CompleteLattice/Group.lean
+++ Mathlib/Order/CompleteLattice/Group.lean
@@ -15,21 +15,21 @@ variable {α : Type*} {ι : Sort*} {κ : ι → Sort*}
 @[to_additive]
 lemma iSup_mul_le (u v : ι → α) :
     ⨆ i, u i * v i ≤ (⨆ i, u i) * ⨆ i, v i :=
-  iSup_le fun _ ↦ mul_le_mul' (le_iSup _ _) (le_iSup _ _)
+  iSup_le fun _ ↦ mul_le_mul' (le_iSup ..) (le_iSup ..)
 
 @[to_additive]
 lemma le_iInf_mul (u v : ι → α) :
     (⨅ i, u i) * ⨅ i, v i ≤ ⨅ i, u i * v i :=
-  iSup_mul_le (α := αᵒᵈ) _ _
+  iSup_mul_le (α := αᵒᵈ) ..
 
 @[to_additive]
 lemma iSup₂_mul_le (u v : (i : ι) → κ i → α) :
     ⨆ (i) (j), u i j * v i j ≤ (⨆ (i) (j), u i j) * ⨆ (i) (j), v i j := by
-  refine le_trans ?_ (iSup_mul_le _ _)
+  refine le_trans ?_ (iSup_mul_le ..)
   gcongr
-  exact iSup_mul_le _ _
+  exact iSup_mul_le ..
 
 @[to_additive]
 lemma le_iInf₂_mul (u v : (i : ι) → κ i → α) :
     (⨅ (i) (j), u i j) * ⨅ (i) (j), v i j ≤ ⨅ (i) (j), u i j * v i j :=
-  iSup₂_mul_le (α := αᵒᵈ) _ _
+  iSup₂_mul_le (α := αᵒᵈ) ..
