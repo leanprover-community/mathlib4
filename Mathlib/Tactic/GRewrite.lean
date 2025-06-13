b0c117d7a8670d3f80704901b830ba8fe6703652
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2025-06-13 11:01:43 +0100

    remove import

--- Mathlib/Tactic/GRewrite.lean
+++ Mathlib/Tactic/GRewrite.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Sebastian Zimmer, Mario Carneiro, Heather Macbeth, Jovan Gerbscheid
 -/
 import Mathlib.Tactic.GRewrite.Elab
-import Mathlib.Tactic.GCongr
 
 /-!
 
