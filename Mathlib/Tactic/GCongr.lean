db71a5f112bb2e7ca96d4c52dedacd8cc1e9763e
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2025-06-13 11:47:07 +0100

    import `register_hint`

--- Mathlib/Tactic/GCongr.lean
+++ Mathlib/Tactic/GCongr.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro, Heather Macbeth
 -/
 import Mathlib.Tactic.GCongr.CoreAttrs
+import Mathlib.Tactic.Hint
 
 /-! # Setup for the `gcongr` tactic
 
