5f330d2c3789a64567496ac1d3a3b61b47f21682
faenuccio <filippo.nuccio@univ-st-etienne.fr>
2025-06-13 22:53:48 +0200

    seems to work...

--- Mathlib/Algebra/GroupWithZero/Range.lean
+++ Mathlib/Algebra/GroupWithZero/Range.lean
@@ -12,6 +12,8 @@ Authors: Antoine Chambert-Loir, Filippo A. E. Nuccio
 -- import Mathlib.Algebra.Group.Submonoid.Basic
 -- import Mathlib.Algebra.Group.Subgroup.Lattice
 import Mathlib.Algebra.GroupWithZero.WithZero
+import Mathlib.Algebra.Order.GroupWithZero.Canonical
+import Mathlib.Algebra.Order.Group.Units
 import Mathlib.Algebra.Group.Subgroup.Lattice
 import Mathlib.Algebra.Group.Subgroup.Pointwise
 import Mathlib.Algebra.GroupWithZero.NonZeroDivisors
@@ -56,107 +58,94 @@ section GroupWithZero
 
 variable  [GroupWithZero B]
 
+variable [MonoidWithZero A] /- [Nontrivial A]  -/[MonoidWithZeroHomClass F A B]
+
 /-- For a map with codomain a `MonoidWithZero`, this is a smallest
 `GroupWithZero`, that contains the invertible elements in the image. See
 `MonoidHomWithZero.mem_range₀_iff_of_comm` for another characterization of `range₀ f` when `B` is
 commutative. -/
-
-def range₀ : Submonoid B where
-  carrier := (↑)''(Subgroup.closure (G := Bˣ) ((↑)⁻¹' (range f))).carrier ∪ {0}
+def Valuation.valueMonoid :
+  Submonoid Bˣ where
+  carrier := ((↑)⁻¹' (range f))
   mul_mem' {b b'} hb hb' := by
-    simp only [union_singleton, mem_insert_iff, mul_eq_zero, mem_image, Subsemigroup.mem_carrier,
-      Submonoid.mem_toSubsemigroup, Subgroup.mem_toSubmonoid] at hb hb' ⊢
-    rcases hb with hb | ⟨a, ha, rfl⟩ <;> rcases hb' with hb' | ⟨a', ha', rfl⟩
-    rotate_right
-    · exact Or.inr ⟨a * a', by simpa only [Units.val_mul, and_true] using Subgroup.mul_mem _ ha ha'⟩
-    all_goals tauto
-  one_mem' := by simpa using Subgroup.one_mem ..
+    simp at hb hb' ⊢
+    obtain ⟨y, hy⟩ := hb
+    obtain ⟨y', hy'⟩ := hb'
+    use y * y'
+    rw [map_mul]
+    rw [hy, hy']
+  one_mem' := ⟨1, by simp⟩
 
-instance : CancelMonoidWithZero (range₀ f) where
-  zero := ⟨0, by simp [range₀]⟩
-  zero_mul a := by
-    rw [← Subtype.coe_inj, Submonoid.coe_mul]
-    exact zero_mul ..
-  mul_zero a := by
-    rw [← Subtype.coe_inj, Submonoid.coe_mul]
-    exact mul_zero ..
-  mul_left_cancel_of_ne_zero := by
-    rintro ⟨x, _⟩ ⟨_, _⟩ ⟨_, _⟩ ha H
-    have : x ≠ 0 := Subtype.coe_ne_coe.mpr ha
-    simpa [this] using H
-  mul_right_cancel_of_ne_zero {a b c} ha H := by
-    rcases b with ⟨x, _⟩; rcases a; rcases c
-    have : x ≠ 0 := Subtype.coe_ne_coe.mpr ha
-    simpa [this] using H
+def Valuation.valueGroup : Subgroup Bˣ := Subgroup.closure (Valuation.valueMonoid f)
 
-theorem zero_mem_range₀ : 0 ∈ range₀ f := by
-  simp [range₀]
+abbrev Valuation.valueMonoid₀ := WithZero (Valuation.valueMonoid f)
 
-@[simp]
-theorem range₀_coe_zero : ((0 : range₀ f) : B) = 0 := rfl
+abbrev Valuation.valueGroup₀ := WithZero (Valuation.valueGroup f)
 
-theorem mem_range₀ {a : A} : f a ∈ range₀ f := by
-  by_cases h : f a = 0 <;>
-  simp [range₀, h]
-  refine ⟨Units.mk0 (f a) h, ?_, rfl⟩
-  apply Subgroup.subset_closure
-  simp
+open Valuation
 
-@[simp]
-theorem range₀_coe_one : ((1 : range₀ f) : B) = 1 := rfl
+lemma foo {b : Bˣ} {H : Set Bˣ} (hb : b ∈ H) : b ∈ Subgroup.closure H := by
+  exact Subgroup.mem_closure.mpr fun K a ↦ a hb
 
-theorem inv_mem_range₀ {b : B} (hb : b ∈ range₀ f) : b⁻¹ ∈ range₀ f := by
-  simp only [range₀, union_singleton, inv_zero, Submonoid.mem_mk, Subsemigroup.mem_mk,
-    mem_insert_iff, mem_image, Units.ne_zero, and_false, exists_const, or_false] at hb ⊢
-  rcases hb with hb | ⟨b, hb, rfl⟩
-  · simp [hb]
-  exact Or.inr ⟨b⁻¹, by simpa⟩
+lemma mem_valueMonoid
+    {b : Bˣ} (hb : b.1 ∈ range f) : b ∈ valueMonoid f := by
+  rcases hb with ⟨c, _⟩
+  simp only [valueMonoid, Submonoid.mem_mk, Subsemigroup.mem_mk, mem_preimage, mem_range]
+  use c
+--
+-- theorem inv_mem_valueGroup {b : Bˣ} (hb : b ∈ (valueGroup f)) : b⁻¹ ∈ valueGroup f := by
+--   simp only [range₀, union_singleton, inv_zero, Submonoid.mem_mk, Subsemigroup.mem_mk,
+--     mem_insert_iff, mem_image, Units.ne_zero, and_false, exists_const, or_false] at hb ⊢
+--   rcases hb with hb | ⟨b, hb, rfl⟩
+--   · simp [hb]
+--   exact Or.inr ⟨b⁻¹, by simpa⟩
 
-instance : GroupWithZero (range₀ f) where
-  toMonoidWithZero := inferInstance
-  inv a := by
-    rcases a with ⟨a, ha⟩
-    exact ⟨a⁻¹, inv_mem_range₀ _ ha⟩
-  exists_pair_ne := by
-    use 1, ⟨0, by simp [range₀]⟩
-    simp [← Subtype.coe_inj]
-  inv_zero := Subtype.coe_inj.symm.mpr inv_zero
-  mul_inv_cancel a ha₀ := by
-    rw [Submonoid.mk_mul_mk, Submonoid.mk_eq_one, mul_inv_cancel₀]
-    rwa [← Subtype.coe_ne_coe] at ha₀
+instance : MonoidWithZero (valueMonoid₀ f) := inferInstance
 
-theorem inv_mem_range₀_iff {b : B} : b⁻¹ ∈ range₀ f ↔ b ∈ range₀ f := by
-  by_cases h : b = 0
-  · simp only [h, inv_zero, zero_mem_range₀]
-  exact ⟨fun h ↦ ((inv_inv b).symm ▸ (inv_mem_range₀ f)) h, inv_mem_range₀ _⟩
+
+instance : GroupWithZero (valueGroup₀ f) := inferInstance
+  -- toMonoidWithZero := inferInstance
+  -- inv a := by
+  --   rcases a with ⟨a, ha⟩
+  --   exact ⟨a⁻¹, inv_mem_range₀ _ ha⟩
+  -- exists_pair_ne := by
+  --   use 1, ⟨0, by simp [range₀]⟩
+  --   simp [← Subtype.coe_inj]
+  -- inv_zero := Subtype.coe_inj.symm.mpr inv_zero
+  -- mul_inv_cancel a ha₀ := by
+  --   rw [Submonoid.mk_mul_mk, Submonoid.mk_eq_one, mul_inv_cancel₀]
+  --   rwa [← Subtype.coe_ne_coe] at ha₀
+
+-- theorem inv_mem_range₀_iff {b : B} : b⁻¹ ∈ range₀ f ↔ b ∈ range₀ f := by
+--   by_cases h : b = 0
+--   · simp only [h, inv_zero, zero_mem_range₀]
+--   exact ⟨fun h ↦ ((inv_inv b).symm ▸ (inv_mem_range₀ f)) h, inv_mem_range₀ _⟩
 
 -- **START HERE**
 
-variable [MonoidWithZero A] [Nontrivial A] [MonoidWithZeroHomClass F A B]
-
-variable{f}
-variable (h : ∀ {a}, f a ≠ 0 ↔ a ≠ 0) -- IS THIS A TYPECLASS?
-
-def nonZeroDivisors_map : (nonZeroDivisors A) →* (nonZeroDivisors B) where
-  toFun := fun a ↦ ⟨f a, by simp [h]⟩
-  map_one' := by simp
-  map_mul' x y := by simp
-
-@[simp]
-lemma nonZeroDivisors_map_apply (a : nonZeroDivisors A) : nonZeroDivisors_map h a = f a := rfl
-
-noncomputable
-def nonZeroDivisors_mrange : Submonoid Bˣ := MonoidHom.mrange
-  (nonZeroDivisorsEquivUnits.toMonoidHom.comp (nonZeroDivisors_map h))
-
-lemma mem_nonZeroDivisors_mrange [NoZeroDivisors A]
-    {b : Bˣ} (hb : b.1 ∈ range f) : b ∈ nonZeroDivisors_mrange h := by
-  simp only [nonZeroDivisors_mrange, MonoidHom.mem_mrange, MulEquiv.toMonoidHom_eq_coe,
-    MonoidHom.coe_comp, MonoidHom.coe_coe, Function.comp_apply, nonZeroDivisorsEquivUnits_apply,
-    nonZeroDivisors_map_apply, Subtype.exists, mem_nonZeroDivisors_iff_ne_zero]
-  rcases hb with ⟨c, hc⟩
-  exact ⟨c, h.mp <| hc ▸ b.ne_zero, by simp [hc, Units.mk0_val]⟩
-
+-- variable{f}
+-- variable (h : ∀ {a}, f a ≠ 0 ↔ a ≠ 0) -- IS THIS A TYPECLASS?
+--
+-- def nonZeroDivisors_map : (nonZeroDivisors A) →* (nonZeroDivisors B) where
+--   toFun := fun a ↦ ⟨f a, by simp [h]⟩
+--   map_one' := by simp
+--   map_mul' x y := by simp
+--
+-- @[simp]
+-- lemma nonZeroDivisors_map_apply (a : nonZeroDivisors A) : nonZeroDivisors_map h a = f a := rfl
+--
+-- noncomputable
+-- def nonZeroDivisors_mrange : Submonoid Bˣ := MonoidHom.mrange
+--   (nonZeroDivisorsEquivUnits.toMonoidHom.comp (nonZeroDivisors_map h))
+--
+-- lemma mem_nonZeroDivisors_mrange [NoZeroDivisors A]
+--     {b : Bˣ} (hb : b.1 ∈ range f) : b ∈ nonZeroDivisors_mrange h := by
+--   simp only [nonZeroDivisors_mrange, MonoidHom.mem_mrange, MulEquiv.toMonoidHom_eq_coe,
+--     MonoidHom.coe_comp, MonoidHom.coe_coe, Function.comp_apply, nonZeroDivisorsEquivUnits_apply,
+--     nonZeroDivisors_map_apply, Subtype.exists, mem_nonZeroDivisors_iff_ne_zero]
+--   rcases hb with ⟨c, hc⟩
+--   exact ⟨c, h.mp <| hc ▸ b.ne_zero, by simp [hc, Units.mk0_val]⟩
+--
 -- lemma mem_nonZeroDivisors_mrange_old [NoZeroDivisors A]
 --     {b : Bˣ} (hb : b.1 ∈ range f) : b ∈ nonZeroDivisors_mrange h := by
 --   simp only [nonZeroDivisors_mrange, MulEquiv.toMonoidHom_eq_coe, MonoidHom.mem_mrange,
@@ -171,89 +160,145 @@ lemma mem_nonZeroDivisors_mrange [NoZeroDivisors A]
 --   rcases hx with _ | hx
 --   · assumption
 --   · simp [hx, Eq.comm] at ha
-
-def nonZeroDivisors_range : Subgroup Bˣ := Subgroup.closure (nonZeroDivisors_mrange h)
+--
+-- def nonZeroDivisors_range : Subgroup Bˣ := Subgroup.closure (nonZeroDivisors_mrange h)
+--
+-- lemma nonZeroDivisors_mrange_eq_nonZeroDivisors_range {X W : Type*} [GroupWithZero X]
+--     [FunLike W X B] [MonoidWithZeroHomClass W X B] {φ : W} :
+--     (nonZeroDivisors_mrange (map_ne_zero φ)) =
+--       (nonZeroDivisors_range (map_ne_zero φ)).toSubmonoid := by
+--   simp [nonZeroDivisors_range]
+--   rw [Subgroup.closure_toSubmonoid]
+--   rw [Eq.comm]
+--   apply Submonoid.closure_eq_of_le
+--   · simp only [union_subset_iff, subset_refl, true_and]
+--     intro x hx
+--     rcases hx with ⟨y, hy⟩
+--     simp at hy
+--     set z : nonZeroDivisors X := by
+--       use y⁻¹
+--       simp with hz
+--     use z
+--     rw [hz]
+--     simp only [MulEquiv.toMonoidHom_eq_coe, MonoidHom.coe_comp, MonoidHom.coe_coe,
+--       Function.comp_apply, nonZeroDivisorsEquivUnits_apply, nonZeroDivisors_map_apply, map_inv₀]
+--     rw [← inv_inv (a := Units.mk0 _ _)] at hy
+--     simp at hy
+--     apply_fun (fun b : Bˣ ↦ b⁻¹)
+--     · simp only
+--       rw [← hy]
+--       sorry
+--       -- rw [Units.mk0_inv_eq_inv]
+--       -- simp
+--     exact inv_injective
+--   · rw [Submonoid.closure_union]
+--     simp
 
 lemma Units.mk0_inv_eq_inv {b : B} (hb : b ≠ 0) :
     (Units.mk0 b hb)⁻¹ = Units.mk0 (b⁻¹) (inv_ne_zero hb) := Units.eq_iff.mp rfl
 
-lemma nonZeroDivisors_mrange_eq_nonZeroDivisors_range {X W : Type*} [GroupWithZero X]
-    [FunLike W X B] [MonoidWithZeroHomClass W X B] {φ : W} :
-    (nonZeroDivisors_mrange (map_ne_zero φ)) =
-      (nonZeroDivisors_range (map_ne_zero φ)).toSubmonoid := by
-  simp [nonZeroDivisors_range]
+-- theorem inv_mem_valueMonoid {b : Bˣ} : b⁻¹ ∈ valueMonoid f ↔ b ∈ valueMonoid f := by
+  -- by_cases h : b = 0
+  -- · simp only [h, inv_zero, zero_mem_range₀]
+
+  -- exact ⟨fun h ↦ ((inv_inv b).symm ▸ (inv_mem_range₀ f)) h, inv_mem_range₀ _⟩
+
+lemma valueMonoid_eq_valueGroup {X W : Type*} [GroupWithZero X] [FunLike W X B]
+    [MonoidWithZeroHomClass W X B] {w : W} : (valueMonoid w) = (valueGroup w).toSubmonoid := by
+  simp [valueGroup]
   rw [Subgroup.closure_toSubmonoid]
   rw [Eq.comm]
   apply Submonoid.closure_eq_of_le
-  · simp
-    rintro x ⟨y, hy⟩
+  · simp [union_subset_iff, subset_refl, true_and, valueMonoid]
+    intro x ⟨y, hy⟩
     simp at hy
-    set z : nonZeroDivisors X := by
-      use y⁻¹
-      simp with hz
-    use z
-    rw [hz]
+    use y⁻¹
     simp only [MulEquiv.toMonoidHom_eq_coe, MonoidHom.coe_comp, MonoidHom.coe_coe,
-      Function.comp_apply, nonZeroDivisorsEquivUnits_apply, nonZeroDivisors_map_apply, map_inv₀]
-    rw [← inv_inv (a := Units.mk0 _ _)] at hy
-    simp at hy
-    apply_fun (fun b : Bˣ ↦ b⁻¹)
-    · simp only
-      rw [← hy]
-      rw [Units.mk0_inv_eq_inv]
-      simp
-    exact inv_injective
+      Function.comp_apply, nonZeroDivisorsEquivUnits_apply, map_inv₀]
+    rw [hy]
+    simp
   · rw [Submonoid.closure_union]
     simp
 
-lemma nonZeroDivisors_mrange_eq_nonZeroDivisors_range' {X W : Type*} [GroupWithZero X]
-    [FunLike W X B] [MonoidWithZeroHomClass W X B] {φ : W} :
-    ((nonZeroDivisors_mrange (map_ne_zero φ)) : Set Bˣ) =
-      (nonZeroDivisors_range (map_ne_zero φ)) := by
-  rw [nonZeroDivisors_mrange_eq_nonZeroDivisors_range]
+lemma valueMonoid_eq_valueGroup' {X W : Type*} [GroupWithZero X] [FunLike W X B]
+    [MonoidWithZeroHomClass W X B] {w : W} : ((valueMonoid w) : Set Bˣ)= (valueGroup w) := by
+  rw [valueMonoid_eq_valueGroup]
   rfl
 
-
-lemma nonZeroDivisors_range_eq_range {X W : Type*} [GroupWithZero X] [FunLike W X B]
-    [MonoidWithZeroHomClass W X B] {φ : W} :
-    Units.val '' (nonZeroDivisors_range (map_ne_zero φ)) = (range φ \ {0}) := by
+lemma valueGroup_eq_range {X W : Type*} [GroupWithZero X] [FunLike W X B]
+    [MonoidWithZeroHomClass W X B] {w : W} :
+    Units.val '' (valueGroup w) = (range w \ {0}) := by
   ext x
   refine ⟨fun h ↦ ?_, fun h ↦ ?_⟩
-  · rw [← nonZeroDivisors_mrange_eq_nonZeroDivisors_range'] at h
+  · rw [← valueMonoid_eq_valueGroup'] at h
     obtain ⟨y, ⟨hy, rfl⟩⟩  := h
     simp only [Subgroup.coe_toSubmonoid, Subgroup.closure_eq, SetLike.mem_coe] at hy
     simp
-    simp [nonZeroDivisors_mrange] at hy
-    obtain ⟨a, ha0, ha⟩ := hy
+    simp [valueMonoid] at hy
+    obtain ⟨a, ha⟩ := hy
     use a
-    rw [← ha]
-    rfl
-  · rw [← nonZeroDivisors_mrange_eq_nonZeroDivisors_range']
+  · rw [← valueMonoid_eq_valueGroup']
     simp only [mem_diff, mem_range, mem_singleton_iff] at h
     obtain ⟨⟨y, hy⟩, hx₀⟩ := h
     set u : Bˣ := Units.mk0 x hx₀ with hu
     simp
     use u
     constructor
-    · apply mem_nonZeroDivisors_mrange
+    · apply mem_valueMonoid
       rw [hu]
       simp
       use y
     · rfl
+--
+-- lemma nonZeroDivisors_mrange_eq_nonZeroDivisors_range' {X W : Type*} [GroupWithZero X]
+--     [FunLike W X B] [MonoidWithZeroHomClass W X B] {φ : W} :
+--     ((nonZeroDivisors_mrange (map_ne_zero φ)) : Set Bˣ) =
+--       (nonZeroDivisors_range (map_ne_zero φ)) := by
+--   rw [nonZeroDivisors_mrange_eq_nonZeroDivisors_range]
+--   rfl
 
-abbrev NZDRange₀ := WithZero (nonZeroDivisors_range h)
 
-instance : GroupWithZero (NZDRange₀ h) where
-  toMonoidWithZero := inferInstance
-  inv_zero := sorry
-  mul_inv_cancel := sorry
-  div_eq_mul_inv := sorry
+-- lemma nonZeroDivisors_range_eq_range {X W : Type*} [GroupWithZero X] [FunLike W X B]
+--     [MonoidWithZeroHomClass W X B] {φ : W} :
+--     Units.val '' (nonZeroDivisors_range (map_ne_zero φ)) = (range φ \ {0}) := by
+--   ext x
+--   refine ⟨fun h ↦ ?_, fun h ↦ ?_⟩
+--   · rw [← nonZeroDivisors_mrange_eq_nonZeroDivisors_range'] at h
+--     obtain ⟨y, ⟨hy, rfl⟩⟩  := h
+--     simp only [Subgroup.coe_toSubmonoid, Subgroup.closure_eq, SetLike.mem_coe] at hy
+--     simp
+--     simp [nonZeroDivisors_mrange] at hy
+--     obtain ⟨a, ha0, ha⟩ := hy
+--     use a
+--     rw [← ha]
+--     rfl
+--   · rw [← nonZeroDivisors_mrange_eq_nonZeroDivisors_range']
+--     simp only [mem_diff, mem_range, mem_singleton_iff] at h
+--     obtain ⟨⟨y, hy⟩, hx₀⟩ := h
+--     set u : Bˣ := Units.mk0 x hx₀ with hu
+--     simp
+--     use u
+--     constructor
+--     · apply mem_nonZeroDivisors_mrange
+--       rw [hu]
+--       simp
+--       use y
+--     · rfl
+
+-- abbrev NZDRange₀ := WithZero (nonZeroDivisors_range h)
+
+-- instance : GroupWithZero (NZDRange₀ h) where
+--   toMonoidWithZero := inferInstance
+--   inv_zero := sorry
+--   mul_inv_cancel := sorry
+--   div_eq_mul_inv := sorry
 
 open Subgroup in
-theorem mem_range₀_iff_of_comm (y : B) :
-    y ∈ range₀ f ↔ ∃ a, f a ≠ 0 ∧ ∃ x, f a * y = f x := by sorry
+theorem mem_valueMonoid_iff_of_comm (y : Bˣ) :
+    y ∈ valueMonoid f ↔ ∃ a, f a ≠ 0 ∧ ∃ x, f a * y = f x := by sorry
 
 
+#exit
 end GroupWithZero
 section CommGroupWithZero
 
@@ -261,7 +306,6 @@ variable [MonoidWithZero A] [Nontrivial A] [CommGroupWithZero B] [MonoidWithZero
 variable{f}
 variable (h : ∀ {a}, f a ≠ 0 ↔ a ≠ 0) -- IS THIS A TYPECLASS?
 
-#exit
 open Subgroup in
 theorem mem_NZDRange₀_iff_of_comm (y : Bˣ) :
     y ∈ (nonZeroDivisors_range h) ↔ ∃ a, f a ≠ 0 ∧ ∃ x, f a * y = f x := by
--- Mathlib/RingTheory/Valuation/Discrete/Basic.lean
+++ Mathlib/RingTheory/Valuation/Discrete/Basic.lean
@@ -32,44 +32,43 @@ equivalent to asking that `ofAdd (-1 : ℤ)` belongs to the image, in turn equiv
 
 namespace Valuation
 
-open Function Set LinearOrderedCommGroup MonoidHomWithZero
+open Function Set LinearOrderedCommGroup MonoidHomWithZero Valuation
 
 variable {Γ : Type*} [LinearOrderedCommGroupWithZero Γ]
 
-variable {A : Type*} [CommRing A] [IsDomain A] (v : Valuation A Γ)
+variable {A : Type*} [Ring A] /- [IsDomain A] -/ (v : Valuation A Γ)
 
-variable {v} in
-lemma ne_zero_iff' (hv : v.supp.primeCompl = nonZeroDivisors A) {a : A} : v a ≠ 0 ↔ a ≠ 0 := by
-  refine ⟨fun h ha ↦ (ha ▸ h) (Valuation.map_zero ..), fun h hva ↦ ?_⟩
-  · replace hva : a ∈ (v.supp : Set A) := hva
-    rw [← compl_compl (x := (v.supp : Set A)), mem_compl_iff] at hva
-    replace hva : a ∉ (v.supp).primeCompl := hva
-    simp only [hv, mem_nonZeroDivisors_iff_ne_zero, ne_eq, not_not] at hva
-    exact h hva
+-- variable {v} in
+-- lemma ne_zero_iff' (hv : v.supp.primeCompl = nonZeroDivisors A) {a : A} : v a ≠ 0 ↔ a ≠ 0 := by
+--   refine ⟨fun h ha ↦ (ha ▸ h) (Valuation.map_zero ..), fun h hva ↦ ?_⟩
+--   · replace hva : a ∈ (v.supp : Set A) := hva
+--     rw [← compl_compl (x := (v.supp : Set A)), mem_compl_iff] at hva
+--     replace hva : a ∉ (v.supp).primeCompl := hva
+--     simp only [hv, mem_nonZeroDivisors_iff_ne_zero, ne_eq, not_not] at hva
+--     exact h hva
 
 /-- Given a linearly ordered commutative group with zero `Γ` such that `Γˣ` is
 nontrivial cyclic, a valuation `v : A → Γ` on a ring `A` is *discrete*, if
 `genLTOne Γˣ` belongs to the image. Note that the latter is equivalent to
 asking that `1 : ℤ` belongs to the image of the corresponding additive valuation. -/
 class IsDiscrete : Prop where
-  supp_v' : v.supp.primeCompl = nonZeroDivisors A
-  exists_generator_lt_one' : ∃ (γ : Γˣ), Subgroup.zpowers γ =
-    (nonZeroDivisors_range (ne_zero_iff' supp_v')) ∧ γ < 1
+  -- supp_v' : v.supp.primeCompl = nonZeroDivisors A
+  exists_generator_lt_one' : ∃ (γ : Γˣ), Subgroup.zpowers γ = (valueGroup v) ∧ γ < 1
 
 namespace IsDiscrete
 
-lemma supp_v [IsDiscrete v] : v.supp.primeCompl = nonZeroDivisors A := supp_v'
+-- lemma supp_v [IsDiscrete v] : v.supp.primeCompl = nonZeroDivisors A := supp_v'
 
 lemma exists_generator_lt_one [IsDiscrete v] :
-    ∃ (γ : Γˣ), Subgroup.zpowers γ = (nonZeroDivisors_range (ne_zero_iff' (supp_v v))) ∧ γ < 1 :=
+    ∃ (γ : Γˣ), Subgroup.zpowers γ = valueGroup v ∧ γ < 1 :=
   exists_generator_lt_one'
 
 /-- Given a discrete valuation `v`, `Valuation.IsDiscrete.generator` is a generator of the value
 group that is `< 1`. -/
 noncomputable def generator [IsDiscrete v] : Γˣ := (exists_generator_lt_one v).choose
 
-lemma generator_zpowers_eq_nonZeroDivisors_range [IsDiscrete v] :
-    (Subgroup.zpowers (generator v)) = (nonZeroDivisors_range (ne_zero_iff' (supp_v v))) :=
+lemma generator_zpowers_eq_valueGroup [IsDiscrete v] :
+    (Subgroup.zpowers (generator v)) = valueGroup v :=
   (exists_generator_lt_one v).choose_spec.1
 
 lemma generator_lt_one [IsDiscrete v] : (generator v) < 1 :=
@@ -77,7 +76,7 @@ lemma generator_lt_one [IsDiscrete v] : (generator v) < 1 :=
 
 lemma generator_zpowers_eq_range (K : Type*) [Field K] (w : Valuation K Γ) [IsDiscrete w] :
     Units.val '' (Subgroup.zpowers (generator w)) = range w \ {0} := by
-  rw [generator_zpowers_eq_nonZeroDivisors_range, nonZeroDivisors_range_eq_range]
+  rw [generator_zpowers_eq_valueGroup, valueGroup_eq_range]
 
 lemma generator_mem_range (K : Type*) [Field K] (w : Valuation K Γ) [IsDiscrete w] :
     ↑(generator w) ∈ range w := by
@@ -87,10 +86,10 @@ lemma generator_mem_range (K : Type*) [Field K] (w : Valuation K Γ) [IsDiscrete
 
 lemma generator_ne_zero [IsDiscrete v] : (generator v : Γ) ≠ 0 := by simp
 
-lemma cyclic_value_group [IsDiscrete v] :
-    IsCyclic <| nonZeroDivisors_range (ne_zero_iff' (supp_v v)) := by
+lemma valueGroup_IsCyclic [IsDiscrete v] :
+    IsCyclic <| valueGroup v := by
   rw [isCyclic_iff_exists_zpowers_eq_top]
-  rw [← generator_zpowers_eq_nonZeroDivisors_range]
+  rw [← generator_zpowers_eq_valueGroup]
   let γ := generator v
   have hγ : γ ∈ Subgroup.zpowers γ := by
     simp
