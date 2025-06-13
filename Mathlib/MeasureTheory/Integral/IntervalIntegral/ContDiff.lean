824caa00c68cb4d73cd6dc7dde2fd9816e684a12
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2025-06-13 17:04:10 +0200

    review

--- Mathlib/MeasureTheory/Integral/IntervalIntegral/ContDiff.lean
+++ Mathlib/MeasureTheory/Integral/IntervalIntegral/ContDiff.lean
@@ -3,8 +3,8 @@ Copyright (c) 2025 Sébastien Gouëzel. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Sébastien Gouëzel
 -/
-import Mathlib.MeasureTheory.Integral.IntervalIntegral.FundThmCalculus
 import Mathlib.Analysis.Calculus.ContDiff.Basic
+import Mathlib.MeasureTheory.Integral.IntervalIntegral.FundThmCalculus
 
 /-! # Fundamental theorem of calculus for `C^1` functions
 
@@ -31,12 +31,11 @@ theorem integral_deriv_of_contDiffOn_Icc (h : ContDiffOn ℝ 1 f (Icc a b)) (hab
     ∫ x in a..b, deriv f x = f b - f a := by
   rcases hab.eq_or_lt with rfl | h'ab
   · simp
-  apply integral_eq_sub_of_hasDerivAt_of_le hab
-  · apply h.continuousOn
+  apply integral_eq_sub_of_hasDerivAt_of_le hab h.continuousOn
   · intro x hx
     apply DifferentiableAt.hasDerivAt
     apply ((h x ⟨hx.1.le, hx.2.le⟩).differentiableWithinAt le_rfl).differentiableAt
-    apply Icc_mem_nhds hx.1 hx.2
+    exact Icc_mem_nhds hx.1 hx.2
   · have := (h.derivWithin (m := 0) (uniqueDiffOn_Icc h'ab) (by simp)).continuousOn
     apply (this.intervalIntegrable_of_Icc (μ := volume) hab).congr
     simp only [hab, uIoc_of_le]
@@ -61,7 +60,7 @@ theorem integral_deriv_of_contDiffOn_uIcc (h : ContDiffOn ℝ 1 f (uIcc a b)) :
     ∫ x in a..b, deriv f x = f b - f a := by
   rcases le_or_gt a b with hab | hab
   · simp only [uIcc_of_le hab] at h
-    apply integral_deriv_of_contDiffOn_Icc h hab
+    exact integral_deriv_of_contDiffOn_Icc h hab
   · simp only [uIcc_of_ge hab.le] at h
     rw [integral_symm, integral_deriv_of_contDiffOn_Icc h hab.le]
     abel
@@ -72,12 +71,14 @@ theorem integral_derivWithin_uIcc_of_contDiffOn_uIcc (h : ContDiffOn ℝ 1 f (uI
     ∫ x in a..b, derivWithin f (uIcc a b) x = f b - f a := by
   rcases le_or_gt a b with hab | hab
   · simp only [uIcc_of_le hab] at h ⊢
-    apply integral_derivWithin_Icc_of_contDiffOn_Icc h hab
+    exact integral_derivWithin_Icc_of_contDiffOn_Icc h hab
   · simp only [uIcc_of_ge hab.le] at h ⊢
     rw [integral_symm, integral_derivWithin_Icc_of_contDiffOn_Icc h hab.le]
     abel
 
 end intervalIntegral
+
+open intervalIntegral
 
 theorem enorm_sub_le_lintegral_deriv_of_contDiffOn_Icc (h : ContDiffOn ℝ 1 f (Icc a b))
     (hab : a ≤ b) :
@@ -88,9 +89,8 @@ theorem enorm_sub_le_lintegral_deriv_of_contDiffOn_Icc (h : ContDiffOn ℝ 1 f (
   let g := UniformSpace.Completion.toComplₗᵢ (𝕜 := ℝ) (E := E)
   have : ‖(g ∘ f) b - (g ∘ f) a‖ₑ = ‖f b - f a‖ₑ := by
     rw [← edist_eq_enorm_sub, Function.comp_def, g.isometry.edist_eq, edist_eq_enorm_sub]
-  rw [← this,
-    ← intervalIntegral.integral_deriv_of_contDiffOn_Icc (g.contDiff.comp_contDiffOn h) hab,
-    intervalIntegral.integral_of_le hab, restrict_Ioc_eq_restrict_Icc]
+  rw [← this, ← integral_deriv_of_contDiffOn_Icc (g.contDiff.comp_contDiffOn h) hab,
+    integral_of_le hab, restrict_Ioc_eq_restrict_Icc]
   apply (enorm_integral_le_lintegral_enorm _).trans
   apply lintegral_mono_ae
   rw [← restrict_Ioo_eq_restrict_Icc]
