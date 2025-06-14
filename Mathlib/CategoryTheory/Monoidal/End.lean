a1235f86beca6bdbe06cef4df9484def48b3f640
Robin Carlier <57142648+robin-carlier@users.noreply.github.com>
2025-06-14 19:51:21 +0200

    phrasing

--- Mathlib/CategoryTheory/Monoidal/End.lean
+++ Mathlib/CategoryTheory/Monoidal/End.lean
@@ -30,7 +30,7 @@ variable (C : Type u) [Category.{v} C]
 with tensor product given by composition of functors
 (and horizontal composition of natural transformations).
 
-Note: due to composition of functor in mathlib being reversed compared to the
+Note: due to the fact that composition of functors in mathlib is reversed compared to the
 one usually found in the literature, this monoidal structure is in fact the monoidal
 opposite of the one usually considered in the literature.
 -/
