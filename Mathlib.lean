b64dedcadade7ed9a58a547aa570fbb8b6195757
JovanGerb <jovan.gerbscheid@gmail.com>
2025-06-14 09:56:06 +0000

    feat: improvements to RefinedDiscrTree (#11968)
    This PR defines `RefinedDiscrTree`, which is an improved version of `DiscrTree` with many new features, such as
    - indexing lambda terms, dependent forall terms, and bound variables.
    - giving a score to matches, in order to sort them
    - indexing star patterns so that `a+b` and `a+a` are indexed differently
    - taking into account eta reductions, so that `exp` can still be matched with the library pattern `fun x => exp (f x)`.
    
    This PR makes `RefinedDiscrTree` into a lazy data structure, meaning that it can be used without a cache, just like `LazyDiscrTree`.
    
    This PR also removes these features:
    - indexing `fun x => x` as `id`
    - indexing `fun x => f x + g x` as `f + g`, and similarly for `-`, `*`, `/`, `⁻¹`, `^`.
    - indexing `fun _ => 42` as `42`
    
    These equivalent indexings do not hold definitionally in the `reducible` transparency, which is the transparency that is used for unification when using a discrimination tree. Therefore, indexing these different expressions the same is actually inefficient rather than helpful.
    
    This is part of the bigger #11768, which uses this discrimination tree for a library rewriting tactic.
    
    This replaces an older version of `RefinedDiscrTree`.
    
    
    
    Co-authored-by: JovanGerb <56355248+JovanGerb@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -3844,8 +3844,8 @@ import Mathlib.Lean.Meta.KAbstractPositions
 import Mathlib.Lean.Meta.RefinedDiscrTree
 import Mathlib.Lean.Meta.RefinedDiscrTree.Basic
 import Mathlib.Lean.Meta.RefinedDiscrTree.Encode
+import Mathlib.Lean.Meta.RefinedDiscrTree.Initialize
 import Mathlib.Lean.Meta.RefinedDiscrTree.Lookup
-import Mathlib.Lean.Meta.RefinedDiscrTree.Pi
 import Mathlib.Lean.Meta.Simp
 import Mathlib.Lean.Name
 import Mathlib.Lean.PrettyPrinter.Delaborator
@@ -5720,7 +5720,6 @@ import Mathlib.Tactic.FunProp.Differentiable
 import Mathlib.Tactic.FunProp.Elab
 import Mathlib.Tactic.FunProp.FunctionData
 import Mathlib.Tactic.FunProp.Mor
-import Mathlib.Tactic.FunProp.StateList
 import Mathlib.Tactic.FunProp.Theorems
 import Mathlib.Tactic.FunProp.ToBatteries
 import Mathlib.Tactic.FunProp.Types
