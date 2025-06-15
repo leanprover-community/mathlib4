ad471b97c82e08c37643732a2fddaf94548e540d
Aaron Liu <aaronliu2008@outlook.com>
2025-06-15 07:10:37 -0400

    author

--- Mathlib/Tactic/Widget/Conv.lean
+++ Mathlib/Tactic/Widget/Conv.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2023 Robin Böhne. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Robin Böhne, Wojciech Nawrocki, Patrick Massot
+Authors: Robin Böhne, Wojciech Nawrocki, Patrick Massot, Aaron Liu
 -/
 import Mathlib.Tactic.Widget.SelectPanelUtils
 import Mathlib.Data.String.Defs
