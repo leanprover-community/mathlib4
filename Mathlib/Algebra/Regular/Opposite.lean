1efa24f989d1c3b041698fc22d3739db1a26cf85
Eric Wieser <wieser.eric@gmail.com>
2025-06-15 11:45:45 +0000

    shake

--- Mathlib/Algebra/Regular/Opposite.lean
+++ Mathlib/Algebra/Regular/Opposite.lean
@@ -3,8 +3,8 @@ Copyright (c) 2025 Eric Wieser. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Eric Wieser
 -/
-import Mathlib.Algebra.Group.Opposite
 import Mathlib.Algebra.Regular.Basic
+import Mathlib.Algebra.Opposites
 
 /-!
 # Results about `IsRegular` and `MulOpposite`
