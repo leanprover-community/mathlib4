e2476c62355216d06d3516eab3ca1721ca0c4a1b
Eric Wieser <wieser.eric@gmail.com>
2025-06-15 09:34:07 +0000

    fix

--- Mathlib/Algebra/Regular/ULift.lean
+++ Mathlib/Algebra/Regular/ULift.lean
@@ -1,5 +1,5 @@
 /-
-Cupyright (c) 2025 Eric Wieser. All rights reserved.
+Copyright (c) 2025 Eric Wieser. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Eric Wieser
 -/
