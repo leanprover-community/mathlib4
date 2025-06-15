2447ed3ad193636deacc1cc3bc53af0c5f0472b1
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2025-06-15 17:48:17 +0100

    feat(gcongr): let `gcongr` take a depth parameter, analogous to `congr`

--- MathlibTest/GCongr/implications.lean
+++ MathlibTest/GCongr/implications.lean
@@ -14,3 +14,8 @@ example (h : a → b) : ¬ ¬ ¬ b → ¬ ¬ ¬ a := by gcongr ¬ ¬ ¬ ?_
 example (h : a → b) : (∃ i, ∀ j, i ∧ b → j) → (∃ i, ∀ j, i ∧ a → j) := by gcongr
 example (h : a → b) : (∃ i, ∀ j, i ∧ b → j) → (∃ i, ∀ j, i ∧ a → j) := by
   gcongr ∃ i, ∀ j, i ∧ ?_ → j
+
+example (h : c → b) : (a → b → c) → (a → b → b) := by
+  gcongr 1
+  guard_target =ₛ (b → c) → (b → b)
+  gcongr 1
