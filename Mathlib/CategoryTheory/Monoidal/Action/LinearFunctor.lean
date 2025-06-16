06818d8be3d6278f1aaea9b340f8f78850f30918
Robin Carlier <57142648+robin-carlier@users.noreply.github.com>
2025-06-16 18:28:13 +0200

    left linearity ok

--- Mathlib/CategoryTheory/Monoidal/Action/LinearFunctor.lean
+++ Mathlib/CategoryTheory/Monoidal/Action/LinearFunctor.lean
@@ -10,9 +10,9 @@ import Mathlib.CategoryTheory.Monoidal.Action.Basic
 Given a monoidal category `C` acting on the left or on the right on categories
 `D` and `D'`, we introduce the following typeclasses on functors `F : D ⥤ D` to
 express compatibility of `F` with the action of `C`:
-* `F.LaxLeftLinear C` bundles the "lineator" as a morphism
+* `F.LaxLeftLinear C` bundles the "μₗ" as a morphism
   `c ⊙ₗ F.obj d ⟶ F.obj (c ⊙ₗ d)`.
-* `F.LaxRightLinear` bundles the "lineator" as a morphism
+* `F.LaxRightLinear` bundles the "μₗ" as a morphism
   `F.obj d ᵣ⊙ c ⟶ F.obj (d ᵣ⊙ c)`.
 * `F.OplaxLeftLinear`.
 * `F.OplaxRightLinear`.
@@ -30,33 +30,33 @@ section leftAction
 open MonoidalLeftAction
 
 /-- `F.LaxLinear C` equips `F : D ⥤ D'` with a family of morphisms
-`lineator F : ∀ (c : C) (d : D), c ⊙ₗ F.obj d ⟶ F.obj (c ⊙ₗ d)`
+`μₗ F : ∀ (c : C) (d : D), c ⊙ₗ F.obj d ⟶ F.obj (c ⊙ₗ d)`
 that is natural in each variable and coherent with respect to left actions
 of `C` on `D` and `D'`. -/
 class LaxLeftLinear
     (F : D ⥤ D') (C : Type*) [Category C] [MonoidalCategory C]
     [MonoidalLeftAction C D] [MonoidalLeftAction C D'] where
-  /-- The "lineator" morphism. -/
-  lineator (F) (c : C) (d : D) : c ⊙ₗ F.obj d ⟶ F.obj (c ⊙ₗ d)
-  lineator_naturality_left (F) {c c': C} (f : c ⟶ c') (d : D) :
-    f ⊵ₗ F.obj d ≫ lineator c' d = lineator c d ≫ F.map (f ⊵ₗ d) := by aesop_cat
-  lineator_naturality_right (F) (c : C) {d d' : D} (f : d ⟶ d') :
-    c ⊴ₗ F.map f ≫ lineator c d' = lineator c d ≫ F.map (c ⊴ₗ f) := by aesop_cat
-  lineator_associativity (F) (c c' : C) (d : D) :
-    lineator (c ⊗ c') d ≫ F.map (σ_ₗ _ _ _).hom =
-    (σ_ₗ c c' (F.obj d)).hom ≫ c ⊴ₗ lineator c' d ≫
-      lineator c (c' ⊙ₗ d) := by aesop_cat
-  lineator_unitality (F) (d : D) :
-    (υ_ₗ (F.obj d)).hom = lineator (𝟙_ C) d ≫ F.map (υ_ₗ d).hom := by aesop_cat
+  /-- The "μₗ" morphism. -/
+  μₗ (F) (c : C) (d : D) : c ⊙ₗ F.obj d ⟶ F.obj (c ⊙ₗ d)
+  μₗ_naturality_left (F) {c c': C} (f : c ⟶ c') (d : D) :
+    f ⊵ₗ F.obj d ≫ μₗ c' d = μₗ c d ≫ F.map (f ⊵ₗ d) := by aesop_cat
+  μₗ_naturality_right (F) (c : C) {d d' : D} (f : d ⟶ d') :
+    c ⊴ₗ F.map f ≫ μₗ c d' = μₗ c d ≫ F.map (c ⊴ₗ f) := by aesop_cat
+  μₗ_associativity (F) (c c' : C) (d : D) :
+    μₗ (c ⊗ c') d ≫ F.map (αₗ _ _ _).hom =
+    (αₗ c c' (F.obj d)).hom ≫ c ⊴ₗ μₗ c' d ≫
+      μₗ c (c' ⊙ₗ d) := by aesop_cat
+  μₗ_unitality (F) (d : D) :
+    (λₗ (F.obj d)).hom = μₗ (𝟙_ C) d ≫ F.map (λₗ d).hom := by aesop_cat
 
 namespace LaxLeftLinear
 
 -- These are [reassoc (attr := simp)] on the basis that analog lemmas for
 -- lax monoidal functors are also [reassoc (attr := simp)].
-attribute [reassoc (attr := simp)] lineator_naturality_right
-attribute [reassoc (attr := simp)] lineator_naturality_left
-attribute [reassoc (attr := simp)] lineator_associativity
-attribute [reassoc (attr := simp)] lineator_unitality
+attribute [reassoc (attr := simp)] μₗ_naturality_right
+attribute [reassoc (attr := simp)] μₗ_naturality_left
+attribute [reassoc (attr := simp)] μₗ_associativity
+attribute [simp, reassoc] μₗ_unitality
 
 variable
   (F : D ⥤ D') {C : Type*} [Category C] [MonoidalCategory C]
@@ -64,48 +64,55 @@ variable
   [F.LaxLeftLinear C]
 
 @[reassoc (attr := simp)]
-lemma lineator_associativity_inv (c c' : C) (d : D) :
-    lineator F c (c' ⊙ₗ d) ≫
-      c ⊴ₗ oplaxLineator F c' d ≫ (σ_ₗ _ _ _).inv =
-    F.map (σ_ₗ _ _ _).inv ≫ oplaxLineator F (c ⊗ c' : C) d := by
-  simpa [-oplaxLineator_associativity, -oplaxLineator_associativity_assoc] using
-    F.map (σ_ₗ _ _ _).inv ≫=
-      (oplaxLineator_associativity F c c' d).symm =≫
-      (σ_ₗ _ _ _).inv
+lemma μₗ_associativity_inv (c c' : C) (d : D) :
+    c ⊴ₗ μₗ F c' d ≫ μₗ F c (c' ⊙ₗ d) ≫ F.map (αₗ _ _ _).inv =
+    (αₗ c c' (F.obj d)).inv ≫ μₗ F (c ⊗ c') d := by
+  simpa [-μₗ_associativity, -μₗ_associativity_assoc] using
+    (αₗ _ _ _).inv ≫=
+      (μₗ_associativity F c c' d).symm =≫
+      F.map (αₗ _ _ _).inv
+
+@[reassoc (attr := simp)]
+lemma μₗ_unitality_inv (d : D) :
+     (λₗ (F.obj d)).inv ≫ μₗ F (𝟙_ C) d = F.map (λₗ d).inv := by
+  simpa [-μₗ_unitality] using
+    (λₗ[C] (F.obj d)).inv ≫=
+      (μₗ_unitality F d).symm =≫
+      F.map (λₗ[C] d).inv
 
 end LaxLeftLinear
 
 /-- `F.OplaxLinear C` equips `F : D ⥤ D'` with a family of morphisms
-`oplaxLineator F : ∀ (c : C) (d : D), c ⊙ₗ F.obj d ⟶ F.obj (c ⊙ₗ d)`
+`δₗ F : ∀ (c : C) (d : D), c ⊙ₗ F.obj d ⟶ F.obj (c ⊙ₗ d)`
 that is natural in each variable and coherent with respect to left actions
 of `C` on `D` and `D'`. -/
 class OplaxLeftLinear
     (F : D ⥤ D') (C : Type*) [Category C] [MonoidalCategory C]
     [MonoidalLeftAction C D] [MonoidalLeftAction C D'] where
-  /-- The "lineator" morphism. -/
-  oplaxLineator (F) (c : C) (d : D) : F.obj (c ⊙ₗ d) ⟶ c ⊙ₗ F.obj d
-  oplaxLineator_naturality_left (F) {c c': C} (f : c ⟶ c') (d : D) :
-    F.map (f ⊵ₗ d) ≫ oplaxLineator c' d =
-    oplaxLineator c d ≫ f ⊵ₗ F.obj d := by aesop_cat
-  oplaxLineator_naturality_right (F) (c : C) {d d' : D} (f : d ⟶ d') :
-    F.map (c ⊴ₗ f) ≫ oplaxLineator c d' =
-    oplaxLineator c d ≫ c ⊴ₗ F.map f := by aesop_cat
-  oplaxLineator_associativity (F) (c c' : C) (d : D) :
-    oplaxLineator (c ⊗ c') d ≫ (σ_ₗ _ _ _).hom =
-    F.map (σ_ₗ _ _ _).hom ≫ oplaxLineator c (c' ⊙ₗ d) ≫
-      c ⊴ₗ oplaxLineator c' d := by aesop_cat
-  oplaxLineator_unitality (F) (d : D) :
-    (υ_ₗ (F.obj d)).inv =
-    F.map (υ_ₗ d).inv ≫ oplaxLineator (𝟙_ C) d := by aesop_cat
+  /-- The oplax lineator morphism morphism. -/
+  δₗ (F) (c : C) (d : D) : F.obj (c ⊙ₗ d) ⟶ c ⊙ₗ F.obj d
+  δₗ_naturality_left (F) {c c': C} (f : c ⟶ c') (d : D) :
+    F.map (f ⊵ₗ d) ≫ δₗ c' d =
+    δₗ c d ≫ f ⊵ₗ F.obj d := by aesop_cat
+  δₗ_naturality_right (F) (c : C) {d d' : D} (f : d ⟶ d') :
+    F.map (c ⊴ₗ f) ≫ δₗ c d' =
+    δₗ c d ≫ c ⊴ₗ F.map f := by aesop_cat
+  δₗ_associativity (F) (c c' : C) (d : D) :
+    δₗ (c ⊗ c') d ≫ (αₗ _ _ _).hom =
+    F.map (αₗ _ _ _).hom ≫ δₗ c (c' ⊙ₗ d) ≫
+      c ⊴ₗ δₗ c' d := by aesop_cat
+  δₗ_unitality_inv (F) (d : D) :
+    (λₗ (F.obj d)).inv =
+    F.map (λₗ d).inv ≫ δₗ (𝟙_ C) d := by aesop_cat
 
 namespace OplaxLeftLinear
 
 -- These are [reassoc (attr := simp)] on the basis that analog lemmas for
 -- oplax monoidal functors are also [reassoc (attr := simp)].
-attribute [reassoc (attr := simp)] oplaxLineator_naturality_right
-attribute [reassoc (attr := simp)] oplaxLineator_naturality_left
-attribute [reassoc (attr := simp)] oplaxLineator_associativity
-attribute [reassoc (attr := simp)] oplaxLineator_unitality
+attribute [reassoc (attr := simp)] δₗ_naturality_right
+attribute [reassoc (attr := simp)] δₗ_naturality_left
+attribute [reassoc (attr := simp)] δₗ_associativity
+attribute [simp, reassoc] δₗ_unitality_inv
 
 variable
   (F : D ⥤ D') {C : Type*} [Category C] [MonoidalCategory C]
@@ -113,27 +120,35 @@ variable
   [F.OplaxLeftLinear C]
 
 @[reassoc (attr := simp)]
-lemma oplaxLineator_associativity_inv (c c' : C) (d : D) :
-    oplaxLineator F c (c' ⊙ₗ d) ≫
-      c ⊴ₗ oplaxLineator F c' d ≫ (σ_ₗ _ _ _).inv =
-    F.map (σ_ₗ _ _ _).inv ≫ oplaxLineator F (c ⊗ c' : C) d := by
-  simpa [-oplaxLineator_associativity, -oplaxLineator_associativity_assoc] using
-    F.map (σ_ₗ _ _ _).inv ≫=
-      (oplaxLineator_associativity F c c' d).symm =≫
-      (σ_ₗ _ _ _).inv
+lemma δₗ_associativity_inv (c c' : C) (d : D) :
+    δₗ F c (c' ⊙ₗ d) ≫
+      c ⊴ₗ δₗ F c' d ≫ (αₗ _ _ _).inv =
+    F.map (αₗ _ _ _).inv ≫ δₗ F (c ⊗ c' : C) d := by
+  simpa [-δₗ_associativity, -δₗ_associativity_assoc] using
+    F.map (αₗ _ _ _).inv ≫=
+      (δₗ_associativity F c c' d).symm =≫
+      (αₗ _ _ _).inv
+
+@[reassoc (attr := simp)]
+lemma δₗ_unitality_hom (d : D) :
+    δₗ F (𝟙_ C) d ≫ (λₗ (F.obj d)).hom = F.map (λₗ d).hom := by
+  simpa [-δₗ_unitality_inv] using
+    F.map (λₗ[C] d).hom ≫=
+      (δₗ_unitality_inv F d).symm =≫
+      (λₗ[C] (F.obj d)).hom
 
 end OplaxLeftLinear
 
 /-- `F.LeftLinear C` asserts that `F` is both lax and oplax left-linear,
-in a compatible way, i.e that `lineator` is inverse to `oplaxLineator`. -/
+in a compatible way, i.e that `μₗ` is inverse to `δₗ`. -/
 class LeftLinear
     (F : D ⥤ D') (C : Type*) [Category C] [MonoidalCategory C]
     [MonoidalLeftAction C D] [MonoidalLeftAction C D'] extends
     F.LaxLeftLinear C, F.OplaxLeftLinear C where
-  lineator_comp_oplaxLineator (F) (c : C) (d : D) :
-    LaxLeftLinear.lineator F c d ≫ OplaxLeftLinear.oplaxLineator F c d = 𝟙 _
-  oplaxLineator_comp_lineator (F) (c : C) (d : D) :
-    OplaxLeftLinear.oplaxLineator F c d ≫ LaxLeftLinear.lineator F c d = 𝟙 _
+  μₗ_comp_δₗ (F) (c : C) (d : D) :
+    LaxLeftLinear.μₗ F c d ≫ OplaxLeftLinear.δₗ F c d = 𝟙 _
+  δₗ_comp_μₗ (F) (c : C) (d : D) :
+    OplaxLeftLinear.δₗ F c d ≫ LaxLeftLinear.μₗ F c d = 𝟙 _
 
 namespace LeftLinear
 
@@ -144,41 +159,33 @@ variable
   [MonoidalLeftAction C D] [MonoidalLeftAction C D']
   [F.LeftLinear C]
 
-attribute [reassoc (attr := simp)] lineator_comp_oplaxLineator
-attribute [reassoc (attr := simp)] oplaxLineator_comp_lineator
+attribute [reassoc (attr := simp)] μₗ_comp_δₗ
+attribute [reassoc (attr := simp)] δₗ_comp_μₗ
 
-/-- A shorthand to bundle the lineator as an isomorphism -/
+/-- A shorthand to bundle the μₗ as an isomorphism -/
 abbrev ℓ_ (c : C) (d : D) : c ⊙ₗ F.obj d ≅ F.obj (c ⊙ₗ d) where
-  hom := LaxLeftLinear.lineator F c d
-  inv := OplaxLeftLinear.oplaxLineator F c d
+  hom := LaxLeftLinear.μₗ F c d
+  inv := OplaxLeftLinear.δₗ F c d
 
 variable (c : C) (d : D)
 
-instance : IsIso (lineator F c d) := Iso.isIso_hom (ℓ_ F c d)
+instance : IsIso (μₗ F c d) := Iso.isIso_hom (ℓ_ F c d)
 
-instance : IsIso (oplaxLineator F c d) := Iso.isIso_inv (ℓ_ F c d)
-
-@[simp]
-lemma inv_lineator :
-    CategoryTheory.inv (lineator F c d) = oplaxLineator F c d :=
-  Eq.symm <| IsIso.eq_inv_of_hom_inv_id <| lineator_comp_oplaxLineator F c d
+instance : IsIso (δₗ F c d) := Iso.isIso_inv (ℓ_ F c d)
 
 @[simp]
-lemma inv_oplaxLineator :
-    CategoryTheory.inv (oplaxLineator F c d) = lineator F c d :=
-  Eq.symm <| IsIso.eq_inv_of_hom_inv_id <| oplaxLineator_comp_lineator F c d
+lemma inv_μₗ :
+    CategoryTheory.inv (μₗ F c d) = δₗ F c d :=
+  Eq.symm <| IsIso.eq_inv_of_hom_inv_id <| μₗ_comp_δₗ F c d
+
+@[simp]
+lemma inv_δₗ :
+    CategoryTheory.inv (δₗ F c d) = μₗ F c d :=
+  Eq.symm <| IsIso.eq_inv_of_hom_inv_id <| δₗ_comp_μₗ F c d
 
 end LeftLinear
 
-section rightAction
+end leftAction
 
-open MonoidalRightAction
-
--- variable [MonoidalRightAction C D] [MonoidalRightAction C D']
-
--- class respectsRightAction (F : D ⥤ D') where
---   τ_ (d : D) (c : C) : F.obj (d ᵣ⊙ c) ≅ F.obj d ᵣ⊙ c
-
-end rightAction
 
 end CategoryTheory.Functor
