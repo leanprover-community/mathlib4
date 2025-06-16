cdcb17b436949d1a4eeb754754846c4ed80998ea
Raph-DG <raphaeldouglasgiles@gmail.com>
2025-06-16 08:23:50 +0000

    feat(RingTheory): Order of vanishing in a ring (#25255)
    In this PR we define the order of vanishing of a ring and its extension to elements of the function field (as in stacks project 02MD) as well as some basic API.
    
    
    
    Co-authored-by: Raph-DG <77658801+Raph-DG@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -5362,6 +5362,7 @@ import Mathlib.RingTheory.Norm.Defs
 import Mathlib.RingTheory.Norm.Transitivity
 import Mathlib.RingTheory.NormTrace
 import Mathlib.RingTheory.Nullstellensatz
+import Mathlib.RingTheory.OrderOfVanishing
 import Mathlib.RingTheory.OreLocalization.Basic
 import Mathlib.RingTheory.OreLocalization.Cardinality
 import Mathlib.RingTheory.OreLocalization.OreSet
