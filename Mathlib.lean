1b50e472bb7f1b80f444ae2d60842d5bfb6b02b3
Vasilii Nesterov <vas.nesterov63@gmail.com>
2025-06-17 14:29:21 +0300

    mk_all

--- Mathlib.lean
+++ Mathlib.lean
@@ -3365,6 +3365,7 @@ import Mathlib.Data.Real.EReal
 import Mathlib.Data.Real.GoldenRatio
 import Mathlib.Data.Real.Hyperreal
 import Mathlib.Data.Real.Irrational
+import Mathlib.Data.Real.OfDigits
 import Mathlib.Data.Real.Pi.Bounds
 import Mathlib.Data.Real.Pi.Irrational
 import Mathlib.Data.Real.Pi.Leibniz
