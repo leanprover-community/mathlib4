6bde5e37e250fe5b54130b79a3fb152d768e0fb6
Weiyi Wang <wwylele@gmail.com>
2025-06-16 20:55:02 -0400

    clean up

--- Mathlib/Data/Real/Embedding.lean
+++ Mathlib/Data/Real/Embedding.lean
@@ -18,24 +18,19 @@ import Mathlib.Tactic.Qify
 This file provides embedding of any archimedean groups into reals.
 
 ## Main declarations
-* `orderAddMonoidHom_real_of_pos` defines a `M →+o ℝ` for archimedean group `M`
+* `orderAddMonoidHom_real` defines a `M →+o ℝ` for archimedean group `M`
   that maps a given positive element to the real number 1.
 * `exists_orderAddMonoidHom_real_injective` states there exists an injective `M →+o ℝ`
   for any archimedean group `M`.
 -/
-
-
-theorem Rat.mkRat_add_mkRat_of_den (n₁ n₂ : Int) {d : Nat} (z : d ≠ 0)  :
-    mkRat n₁ d + mkRat n₂ d = mkRat (n₁ + n₂) d := by
-  rw [Rat.mkRat_add_mkRat _ _ z z, ← add_mul, Rat.mkRat_mul_right z]
 
 namespace Archimedean
 
 variable {M : Type*}
 variable [AddCommGroup M] [LinearOrder M] [IsOrderedAddMonoid M]
 
-/-- Set of rational numbers that are less than the "number" `x / one`. Formally,
- these are numbers `p / q` such that `p • one < q • x`. -/
+/-- Set of rational numbers that are less than the "number" `x / one`.
+ Formally, these are numbers `p / q` such that `p • one < q • x`. -/
 abbrev ratLt (one x : M) : Set ℚ := {r | r.num • one < r.den • x}
 
 theorem mkRat_mem_ratLt {num : ℤ} {den : ℕ} (hden: den ≠ 0) {one x : M} :
@@ -74,8 +69,7 @@ theorem ratLt_nonempty {one : M} (hpos : 0 < one) (x : M) : (ratLt one x).Nonemp
   · obtain ⟨n, hn⟩ := Archimedean.arch (-x - x) hpos
     use Rat.ofInt (-n)
     suffices -(n • one) < x by simpa using this
-    refine neg_lt.mpr (lt_of_lt_of_le ?_ hn)
-    simpa using hneg
+    exact neg_lt.mpr (lt_of_lt_of_le (by simpa using hneg) hn)
   · exact ⟨Rat.ofInt (-1), by simpa using hpos⟩
   · obtain ⟨n, hn⟩ := Archimedean.arch one hxpos
     use Rat.mk' 1 (n + 1) (by simp) (by simp)
@@ -88,12 +82,12 @@ theorem ratLt_add {one : M} (hpos : 0 < one) (x y : M) :
   rw [Set.mem_add]
   constructor
   · /- Given `a ∈ ratLt one (x + y)`, find `u ∈ ratLt one x`, `v ∈ ratLt one y`
-       such that `u + v = a`.
-       In a naive attempt, one can take the denominator `d` of `a`,
-       and find the largest `u = p / d < x / one`
-       However, `d` could be too "coarse", and `v = a - u` could be 1/d too large than `y / one`
-       To ensure a large enough denominator, we take `d * k`, where
-       `one + one ≤ k • (d • (x + y) - a.num • one)` -/
+      such that `u + v = a`.
+      In a naive attempt, one can take the denominator `d` of `a`,
+      and find the largest `u = p / d < x / one`
+      However, `d` could be too "coarse", and `v = a - u` could be 1/d too large than `y / one`
+      To ensure a large enough denominator, we take `d * k`, where
+      `one + one ≤ k • (d • (x + y) - a.num • one)` -/
     intro h
     rw [Set.mem_setOf_eq] at h
     have : 0 < a.den • (x + y) - a.num • one := sub_pos.mpr h
@@ -108,49 +102,44 @@ theorem ratLt_add {one : M} (hpos : 0 < one) (x y : M) :
     · rw [mkRat_mem_ratLt hka0, ← smul_smul]
       simpa using hm2
     · have hk' : one + (k • a.num • one - k • a.den • y) ≤ k • a.den • x - one := by
-        rw [smul_add, smul_sub, smul_add] at hk
-        rw [le_sub_iff_add_le, ← sub_le_iff_le_add] at hk
+        rw [smul_add, smul_sub, smul_add, le_sub_iff_add_le, ← sub_le_iff_le_add] at hk
         rw [le_sub_iff_add_le]
         convert hk using 1
         abel
       have h : one + (k • a.num • one - k • a.den • y) ≤ m • one := by
         simpa using hk'.trans hm1
-      have : k • a.num • one - k • a.den • y < m • one := by
-        exact lt_of_lt_of_le (lt_add_of_pos_left _ hpos) h
+      have : k • a.num • one - k • a.den • y < m • one :=
+        lt_of_lt_of_le (lt_add_of_pos_left _ hpos) h
       rw [mkRat_mem_ratLt hka0, sub_smul, sub_lt_comm, ← smul_smul, ← smul_smul, natCast_zsmul]
       exact this
     · rw [Rat.mkRat_add_mkRat_of_den _ _ hka0]
       rw [add_sub_cancel, Rat.mkRat_mul_left hk0, Rat.mkRat_num_den']
-  · intro ⟨u, hu, v, hv, huv⟩
+  · -- `u ∈ ratLt one x`, `v ∈ ratLt one y` → `u + v ∈ ratLt one (x + y)`
+    intro ⟨u, hu, v, hv, huv⟩
     rw [← huv]
     rw [Set.mem_setOf_eq] at hu hv ⊢
-    have hu' : (u.num * v.den) • one < (u.den * v.den: ℤ) • x := by
-      rw [mul_comm u.num, mul_comm (u.den : ℤ)]
-      rw [← smul_smul, ← smul_smul]
+    have hu' : (u.num * v.den) • one < (u.den * v.den : ℤ) • x := by
+      rw [mul_comm u.num, mul_comm (u.den : ℤ), ← smul_smul, ← smul_smul]
       exact zsmul_lt_zsmul_right (by simpa using v.den_pos) (by simpa using hu)
-    have hv' : (v.num * u.den) • one < (u.den * v.den: ℤ) • y := by
-      rw [mul_comm]
-      rw [← smul_smul, ← smul_smul]
-      apply zsmul_lt_zsmul_right (by simpa using u.den_pos) (by simpa using hv)
+    have hv' : (v.num * u.den) • one < (u.den * v.den : ℤ) • y := by
+      rw [mul_comm, ← smul_smul, ← smul_smul]
+      exact zsmul_lt_zsmul_right (by simpa using u.den_pos) (by simpa using hv)
     suffices ((u + v).num * u.den * v.den) • one <
         ((u + v).den : ℤ) • (u.den * v.den : ℤ) • (x + y) by
       rw [mul_assoc, mul_comm, zsmul_comm, ← smul_smul] at this
       rw [smul_lt_smul_iff_of_pos_left
         (mul_pos (by simpa using u.den_pos) (by simpa using v.den_pos))] at this
       simpa using this
-    rw [Rat.add_num_den']
-    rw [mul_comm, ←smul_smul]
+    rw [Rat.add_num_den', mul_comm, ← smul_smul]
     rw [smul_lt_smul_iff_of_pos_left (by simpa using (u + v).den_pos)]
     rw [add_smul, smul_add]
-    apply add_lt_add hu' hv'
+    exact add_lt_add hu' hv'
 
-theorem ratLt'_bddAbove {one : M} (hpos : 0 < one) (x : M) : BddAbove (ratLt' one x) := by
-  refine Monotone.map_bddAbove ?_ ?_
-  · exact Rat.cast_mono
-  · exact ratLt_bddAbove hpos x
+theorem ratLt'_bddAbove {one : M} (hpos : 0 < one) (x : M) : BddAbove (ratLt' one x) :=
+   Monotone.map_bddAbove Rat.cast_mono <| ratLt_bddAbove hpos x
 
-theorem ratLt'_nonempty {one : M} (hpos : 0 < one) (x : M): (ratLt' one x).Nonempty := by
-  apply Set.image_nonempty.mpr (ratLt_nonempty hpos x)
+theorem ratLt'_nonempty {one : M} (hpos : 0 < one) (x : M): (ratLt' one x).Nonempty :=
+   Set.image_nonempty.mpr (ratLt_nonempty hpos x)
 
 open Pointwise in
 theorem ratLt'_add {one : M} (hpos : 0 < one) (x y : M) :
@@ -204,21 +193,21 @@ theorem embed_real_strictMono {one : M} (hpos: 0 < one) : StrictMono (embed_real
 /-- The bundled `M →+o ℝ` for archimedean `M`.
 The given element `one` is mapped to the real number 1. -/
 noncomputable
-def orderAddMonoidHom_real_of_pos {one : M} (hpos: 0 < one) : M →+o ℝ where
+def orderAddMonoidHom_real {one : M} (hpos: 0 < one) : M →+o ℝ where
   toFun := embed_real one
   map_zero' := embed_real_zero hpos
   map_add' := embed_real_add hpos
   monotone' := (embed_real_strictMono hpos).monotone
 
 theorem orderAddMonoidHom_real_apply {one : M} (hpos: 0 < one) (a : M):
-    (orderAddMonoidHom_real_of_pos hpos) a = embed_real one a := by rfl
+    (orderAddMonoidHom_real hpos) a = embed_real one a := by rfl
 
-theorem orderAddMonoidHom_real_injective_of_pos {one : M} (hpos: 0 < one) :
-    Function.Injective (orderAddMonoidHom_real_of_pos hpos) :=
+theorem orderAddMonoidHom_real_injective {one : M} (hpos: 0 < one) :
+    Function.Injective (orderAddMonoidHom_real hpos) :=
   (embed_real_strictMono hpos).injective
 
 theorem orderAddMonoidHom_real_one {one : M} (hpos: 0 < one) :
-    (orderAddMonoidHom_real_of_pos hpos) one = 1 := by
+    (orderAddMonoidHom_real hpos) one = 1 := by
   rw [orderAddMonoidHom_real_apply]
   apply le_antisymm
   · apply csSup_le (ratLt'_nonempty hpos one)
@@ -238,7 +227,7 @@ theorem orderAddMonoidHom_real_one {one : M} (hpos: 0 < one) :
     contrapose! h'
     obtain ⟨y, hxy, hy⟩ := exists_rat_btwn h'
     norm_cast at hy
-    refine ⟨y, hy, hxy⟩
+    exact ⟨y, hy, hxy⟩
 
 variable (M) in
 theorem exists_orderAddMonoidHom_real_injective :
@@ -248,6 +237,6 @@ theorem exists_orderAddMonoidHom_real_injective :
   · have : Nontrivial M := not_subsingleton_iff_nontrivial.mp h
     obtain ⟨a, ha⟩ := exists_ne (0 : M)
     have ha : 0 < |a| := by simpa using ha
-    exact ⟨orderAddMonoidHom_real_of_pos ha, orderAddMonoidHom_real_injective_of_pos ha⟩
+    exact ⟨orderAddMonoidHom_real ha, orderAddMonoidHom_real_injective ha⟩
 
 end Archimedean
