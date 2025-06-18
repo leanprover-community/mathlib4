1de3175c278c8fecfc395531599db7a817d6e808
Oliver Butterley <oliver.butterley@gmail.com>
2025-06-18 11:22:38 +0200

    QuasiMeasurePreserving

--- Mathlib/Dynamics/BirkhoffSum/MeasurePreserving.lean
+++ Mathlib/Dynamics/BirkhoffSum/MeasurePreserving.lean
@@ -4,39 +4,36 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Oliver Butterley
 -/
 import Mathlib.Dynamics.BirkhoffSum.Average
-import Mathlib.Dynamics.Ergodic.MeasurePreserving
-import Mathlib.MeasureTheory.Function.StronglyMeasurable.AEStronglyMeasurable
-
+import Mathlib.MeasureTheory.Measure.QuasiMeasurePreserving
 /-!
 # Birkhoff sum for measure preserving maps
 
 This file contains some lemmas about the `birkhoffSum` and `birkhoffAverage` of a map which is
-`MeasurePreserving`.
+`QuasiMeasurePreserving`.
 -/
 
 section MeasurePreserving
 
-open MeasureTheory Filter
+open MeasureTheory Measure Filter
 
 variable {α M : Type*} [AddCommMonoid M] [MeasurableSpace α]
 variable {R : Type*} [DivisionSemiring R] [Module R M]
 variable {f : α → α} {μ : Measure α} {φ φ' : α → M}
 
 /-- If `φ` and `φ'` are `ae μ` equal then the corresponding `birkhoffSum` are `ae μ` equal. -/
-theorem birkhoffSum_ae_eq_of_ae_eq (hf : MeasurePreserving f μ μ) (hφ : φ =ᵐ[μ] φ') (n : ℕ) :
+theorem birkhoffSum_ae_eq_of_ae_eq (hf : QuasiMeasurePreserving f μ μ) (hφ : φ =ᵐ[μ] φ') n :
     birkhoffSum f φ n =ᵐ[μ] birkhoffSum f φ' n := by
   obtain ⟨s, hs, hs'⟩ := eventuallyEq_iff_exists_mem.mp hφ
   let t := {x | ∀ n, f^[n] x ∈ s}
   have ht : t ∈ ae μ := by
     refine mem_ae_iff.mpr ?_
     rw [show tᶜ = ⋃ n, (f^[n])⁻¹' sᶜ by ext x; simp [t]]
-    refine measure_iUnion_null_iff.mpr fun m ↦ nonpos_iff_eq_zero.mp ?_
-    exact le_of_le_of_eq ((hf.iterate m).measure_preimage_le sᶜ) hs
+    exact measure_iUnion_null_iff.mpr fun n ↦ ((hf.iterate n).preimage_null hs)
   filter_upwards [ht] with x hx
   exact Finset.sum_congr rfl fun x _ => hs' (hx x)
 
 /-- If `φ` and `φ'` are `ae μ` equal then the corresponding `birkhoffAverage` are `ae μ` equal. -/
-theorem birkhoffAverage_ae_eq_of_ae_eq (hf : MeasurePreserving f μ μ) (hφ : φ =ᵐ[μ] φ') (n : ℕ) :
+theorem birkhoffAverage_ae_eq_of_ae_eq (hf : QuasiMeasurePreserving f μ μ) (hφ : φ =ᵐ[μ] φ') n :
     birkhoffAverage R f φ n =ᵐ[μ] birkhoffAverage R f φ' n :=
   EventuallyEq.const_smul (birkhoffSum_ae_eq_of_ae_eq hf hφ n) (n : R)⁻¹
 
