43948978f8d72c2c19041cb37dbf65c778196da6
Robin Carlier <57142648+robin-carlier@users.noreply.github.com>
2025-06-18 16:35:49 +0200

    ...

--- Mathlib/CategoryTheory/Monoidal/Mod_.lean
+++ Mathlib/CategoryTheory/Monoidal/Mod_.lean
@@ -15,119 +15,7 @@ universe v₁ v₂ u₁ u₂
 open CategoryTheory MonoidalCategory Mon_Class
 
 variable {C : Type u₁} [Category.{v₁} C] [MonoidalCategory.{v₁} C]
-
-/-- A module object for a monoid object, all internal to some monoidal category. -/
-structure Mod_ (A : Mon_ C) where
-  /-- The underlying object in the ambient monoidal category -/
-  X : C
-  /-- The action morphism of the module object -/
-  smul : A.X ⊗ X ⟶ X
-  one_smul : η ▷ X ≫ smul = (λ_ X).hom := by aesop_cat
-  assoc : μ ▷ X ≫ smul = (α_ A.X A.X X).hom ≫ A.X ◁ smul ≫ smul := by aesop_cat
-
-attribute [reassoc (attr := simp)] Mod_.one_smul Mod_.assoc
-
-namespace Mod_
-
-variable {A : Mon_ C} (M : Mod_ A)
-
-theorem assoc_flip :
-    A.X ◁ M.smul ≫ M.smul = (α_ A.X A.X M.X).inv ≫ μ ▷ M.X ≫ M.smul := by simp
-
-/-- A morphism of module objects. -/
-@[ext]
-structure Hom (M N : Mod_ A) where
-  /-- The underlying morphism -/
-  hom : M.X ⟶ N.X
-  smul_hom : M.smul ≫ hom = A.X ◁ hom ≫ N.smul := by aesop_cat
-
-attribute [reassoc (attr := simp)] Hom.smul_hom
-
-/-- The identity morphism on a module object. -/
-@[simps]
-def id (M : Mod_ A) : Hom M M where hom := 𝟙 M.X
-
-instance homInhabited (M : Mod_ A) : Inhabited (Hom M M) :=
-  ⟨id M⟩
-
-/-- Composition of module object morphisms. -/
-@[simps]
-def comp {M N O : Mod_ A} (f : Hom M N) (g : Hom N O) : Hom M O where hom := f.hom ≫ g.hom
-
-instance : Category (Mod_ A) where
-  Hom M N := Hom M N
-  id := id
-  comp f g := comp f g
-
-@[ext]
-lemma hom_ext {M N : Mod_ A} (f₁ f₂ : M ⟶ N) (h : f₁.hom = f₂.hom) : f₁ = f₂ :=
-  Hom.ext h
-
-@[simp]
-theorem id_hom' (M : Mod_ A) : (𝟙 M : M ⟶ M).hom = 𝟙 M.X := by
-  rfl
-
-@[simp]
-theorem comp_hom' {M N K : Mod_ A} (f : M ⟶ N) (g : N ⟶ K) :
-    (f ≫ g).hom = f.hom ≫ g.hom :=
-  rfl
-
-variable (A)
-
-/-- A monoid object as a module over itself. -/
-@[simps]
-def regular : Mod_ A where
-  X := A.X
-  smul := μ
-
-instance : Inhabited (Mod_ A) :=
-  ⟨regular A⟩
-
-/-- The forgetful functor from module objects to the ambient category. -/
-def forget : Mod_ A ⥤ C where
-  obj A := A.X
-  map f := f.hom
-
-#adaptation_note /-- https://github.com/leanprover/lean4/pull/6053
-we needed to increase the `maxHeartbeats` limit if we didn't write an explicit proof for
-`map_id` and `map_comp`.
-
-This may indicate a configuration problem in Aesop. -/
-/-- A morphism of monoid objects induces a "restriction" or "comap" functor
-between the categories of module objects.
--/
-@[simps]
-def comap {A B : Mon_ C} (f : A ⟶ B) : Mod_ B ⥤ Mod_ A where
-  obj M :=
-    { X := M.X
-      smul := f.hom ▷ M.X ≫ M.smul
-      one_smul := by
-        slice_lhs 1 2 => rw [← comp_whiskerRight]
-        rw [IsMon_Hom.one_hom, one_smul]
-      assoc := by
-        -- oh, for homotopy.io in a widget!
-        slice_rhs 2 3 => rw [whisker_exchange]
-        simp only [whiskerRight_tensor, MonoidalCategory.whiskerLeft_comp, Category.assoc,
-          Iso.hom_inv_id_assoc]
-        slice_rhs 4 5 => rw [Mod_.assoc_flip]
-        slice_rhs 3 4 => rw [associator_inv_naturality_middle]
-        slice_rhs 2 4 => rw [Iso.hom_inv_id_assoc]
-        slice_rhs 1 2 => rw [← MonoidalCategory.comp_whiskerRight, ← whisker_exchange]
-        slice_rhs 1 2 => rw [← MonoidalCategory.comp_whiskerRight, ← tensorHom_def',
-          ← IsMon_Hom.mul_hom]
-        rw [comp_whiskerRight, Category.assoc] }
-  map g :=
-    { hom := g.hom
-      smul_hom := by
-        dsimp
-        slice_rhs 1 2 => rw [whisker_exchange]
-        slice_rhs 2 3 => rw [← g.smul_hom]
-        rw [Category.assoc] }
-
--- Lots more could be said about `comap`, e.g. how it interacts with
--- identities, compositions, and equalities of monoid object morphisms.
-end Mod_
-
+  {D : Type u₂} [Category.{v₂} D] [MonoidalLeftAction C D]
 
 section Mod_Class
 
@@ -184,7 +72,7 @@ abbrev regular : Mod_Class M M where
 module over the trivial monoid. -/
 @[simps]
 instance (X : D) : Mod_Class (𝟙_ C) X where
-  smul := λₗ _|>.hom
+  smul := (λₗ _).hom
 
 @[ext]
 theorem ext {X : C} (h₁ h₂ : Mod_Class M X) (H : h₁.smul = h₂.smul) :
@@ -220,8 +108,8 @@ instance (f : M ≅ N) [IsMod_Hom A f.hom] :
   smul_hom := by simp [Iso.comp_inv_eq]
 
 variable (D) in
-/-- A (bundled) module object for a monoid object in a monoidal category acting
-on the ambient category. -/
+/-- A module object for a monoid object in a monoidal category acting on the
+ambient category. -/
 structure Mod_ (A : C) [Mon_Class A] where
   /-- The underlying object in the ambient category -/
   X : D
@@ -344,7 +232,7 @@ open MonoidalLeftAction in
 `A`-linear morphism when `M` and `N` have an `A`-module structure obtained
 by restricting scalars along a monoid morphism `A ⟶ B`. -/
 @[simps!]
-lemma scalarRestriction_isMod_Hom
+lemma scalarRestriction_hom
     (M N : D) [Mod_Class B M] [Mod_Class B N] (g : M ⟶ N) [IsMod_Hom B g] :
     letI := scalarRestriction f M
     letI := scalarRestriction f N
@@ -367,7 +255,7 @@ def comap {A B : C} [Mon_Class A] [Mon_Class B] (f : A ⟶ B) [IsMon_Hom f] :
     letI := scalarRestriction f M.X
     ⟨M.X⟩
   map {M N} g :=
-    letI := scalarRestriction_isMod_Hom f M.X N.X g.hom
+    letI := scalarRestriction_hom f M.X N.X g.hom
     ⟨g.hom⟩
 
 -- Lots more could be said about `comap`, e.g. how it interacts with
