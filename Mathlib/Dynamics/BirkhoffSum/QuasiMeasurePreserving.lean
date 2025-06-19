ecf636913b1f419965e208b8a15a0ad3517be3d9
Oliver Butterley <oliver.butterley@gmail.com>
2025-06-19 09:18:06 +0200

    explicit R

--- Mathlib/Dynamics/BirkhoffSum/QuasiMeasurePreserving.lean
+++ Mathlib/Dynamics/BirkhoffSum/QuasiMeasurePreserving.lean
@@ -17,7 +17,6 @@ section MeasurePreserving
 open MeasureTheory Measure Filter
 
 variable {α M : Type*} [AddCommMonoid M] [MeasurableSpace α]
-variable {R : Type*} [DivisionSemiring R] [Module R M]
 variable {f : α → α} {μ : Measure α} {φ φ' : α → M}
 
 /-- If `φ` and `φ'` are `ae μ` equal then the corresponding `birkhoffSum` are `=ᵐ[μ]` equal. -/
@@ -33,7 +32,8 @@ theorem birkhoffSum_ae_eq_of_ae_eq (hf : QuasiMeasurePreserving f μ μ) (hφ : 
   exact Finset.sum_congr rfl fun x _ => hs' (hx x)
 
 /-- If `φ` and `φ'` are `ae μ` equal then the corresponding `birkhoffAverage` are `=ᵐ[μ]` equal. -/
-theorem birkhoffAverage_ae_eq_of_ae_eq (hf : QuasiMeasurePreserving f μ μ) (hφ : φ =ᵐ[μ] φ') n :
+theorem birkhoffAverage_ae_eq_of_ae_eq (R : Type*) [DivisionSemiring R] [Module R M]
+    (hf : QuasiMeasurePreserving f μ μ) (hφ : φ =ᵐ[μ] φ') n :
     birkhoffAverage R f φ n =ᵐ[μ] birkhoffAverage R f φ' n :=
   EventuallyEq.const_smul (birkhoffSum_ae_eq_of_ae_eq hf hφ n) (n : R)⁻¹
 
