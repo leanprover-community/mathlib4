93cce6bfd48354cb44cb49c741a7a7c9253cc6a8
adomani <adomani@gmail.com>
2025-06-19 21:53:16 +0100

    update

--- Mathlib/Test.lean
+++ Mathlib/Test.lean
@@ -1,3 +1,7 @@
 import Lean
 
-run_cmd IO.FS.writeFile "OhNo.txt" s!"I am a new file"
+run_cmd
+  let file := "OhNo.txt"
+  IO.FS.writeFile file s!"I am a new file"
+  let cat ← IO.Process.run {cmd := "cat", args := #[file]}
+  IO.println cat
