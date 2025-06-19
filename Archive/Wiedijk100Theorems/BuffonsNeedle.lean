862c7f6a163d2683c253b35a448835803c919c37
Stefan Kebekus <kebekus@users.noreply.github.com>
2025-06-19 14:15:08 +0000

    chore: rename file (#26135)
    In preparation for an upcoming PR, rename "Analysis/SpecialFunctions/Integrals.lean" to "Analysis/SpecialFunctions/Integrals/Basic.lean." Rationale: I plan to upload code that computes the integral of `log(sin z)`. The code should probably not be included in "Analysis/SpecialFunctions/Integrals.lean" because the methods are different, and my code requires massive new imports. In this setting, I feel it makes sense to open a new directory for code where integrals are computed.
    
    This material is used in [Project VD](https://github.com/kebekus/ProjectVD), which aims to formalize Value Distribution Theory for meromorphic functions on the complex plane.

--- Archive/Wiedijk100Theorems/BuffonsNeedle.lean
+++ Archive/Wiedijk100Theorems/BuffonsNeedle.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Enrico Z. Borba
 -/
 
-import Mathlib.Analysis.SpecialFunctions.Integrals
+import Mathlib.Analysis.SpecialFunctions.Integrals.Basic
 import Mathlib.MeasureTheory.Integral.Prod
 import Mathlib.Probability.Density
 import Mathlib.Probability.Distributions.Uniform
