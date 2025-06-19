8d5ce6005e1692aac8e7da46921fd8d3feebae42
Christopher Hoskin <mans0954@users.noreply.github.com>
2025-06-19 19:00:06 +0100

    Extract lemmas

--- Mathlib/RingTheory/Polynomial/SmallDegreeVieta.lean
+++ Mathlib/RingTheory/Polynomial/SmallDegreeVieta.lean
@@ -57,20 +57,27 @@ lemma eq_mul_mul_of_aroots_quadratic_eq_pair [CommRing T] [CommRing S] [IsDomain
     X ^ 2 + C ((algebraMap T S) b) * X + C ((algebraMap T S) c) by simp] at haroots
   exact eq_mul_mul_of_roots_quadratic_eq_pair haroots
 
+lemma factorize_of_vieta [CommRing R] [IsDomain R] {a b c x1 x2 : R}
+    (hvieta : b = -a * (x1 + x2) ∧ c = a * x1 * x2) :
+    C a * X ^ 2 + C b * X + C c = C a * (X - C x1) * (X - C x2) := by
+  simpa [hvieta.1, hvieta.2] using by ring
+
+lemma roots_of_ne_zero_of_vieta [CommRing R] [IsDomain R] {a b c x1 x2 : R} (ha : a ≠ 0)
+    (hvieta : b = -a * (x1 + x2) ∧ c = a * x1 * x2) :
+    (C a * X ^ 2 + C b * X + C c).roots = {x1, x2} := by
+    suffices C a * X ^ 2 + C b * X + C c = C a * (X - C x1) * (X - C x2) by
+      have h1 : C a * (X - C x1) ≠ 0 := mul_ne_zero (by simpa) (Polynomial.X_sub_C_ne_zero _)
+      have h2 : C a * (X - C x1) * (X - C x2) ≠ 0 := mul_ne_zero h1 (Polynomial.X_sub_C_ne_zero _)
+      simp [this, Polynomial.roots_mul h2, Polynomial.roots_mul h1]
+    exact factorize_of_vieta hvieta
+
 /-- **Vieta's formula** for quadratics as an iff. -/
 lemma roots_quadratic_eq_pair_iff_of_ne_zero [CommRing R] [IsDomain R] {a b c x1 x2 : R}
     (ha : a ≠ 0) :
     (C a * X ^ 2 + C b * X + C c).roots = {x1, x2} ↔
       b = -a * (x1 + x2) ∧ c = a * x1 * x2 :=
-  have roots_of_ne_zero_of_vieta (hvieta : b = -a * (x1 + x2) ∧ c = a * x1 * x2) :
-      (C a * X ^ 2 + C b * X + C c).roots = {x1, x2} := by
-    suffices C a * X ^ 2 + C b * X + C c = C a * (X - C x1) * (X - C x2) by
-      have h1 : C a * (X - C x1) ≠ 0 := mul_ne_zero (by simpa) (Polynomial.X_sub_C_ne_zero _)
-      have h2 : C a * (X - C x1) * (X - C x2) ≠ 0 := mul_ne_zero h1 (Polynomial.X_sub_C_ne_zero _)
-      simp [this, Polynomial.roots_mul h2, Polynomial.roots_mul h1]
-    simpa [hvieta.1, hvieta.2] using by ring
   ⟨fun h => ⟨eq_neg_mul_add_of_roots_quadratic_eq_pair h, eq_mul_mul_of_roots_quadratic_eq_pair h⟩,
-    roots_of_ne_zero_of_vieta⟩
+    roots_of_ne_zero_of_vieta ha⟩
 
 /-- **Vieta's formula** for quadratics as an iff (`aroots` version). -/
 lemma aroots_quadratic_eq_pair_iff_of_ne_zero [CommRing T] [CommRing S] [IsDomain S]
