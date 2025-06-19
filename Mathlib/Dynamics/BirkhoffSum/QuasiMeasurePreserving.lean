9e325bb7a8434227771889d805c648d2cf8e625b
Oliver Butterley <oliver.butterley@gmail.com>
2025-06-19 11:39:59 +0200

    section name and refine docstrings

--- Mathlib/Dynamics/BirkhoffSum/QuasiMeasurePreserving.lean
+++ Mathlib/Dynamics/BirkhoffSum/QuasiMeasurePreserving.lean
@@ -5,21 +5,23 @@ Authors: Oliver Butterley
 -/
 import Mathlib.Dynamics.BirkhoffSum.Average
 import Mathlib.MeasureTheory.Measure.QuasiMeasurePreserving
+
 /-!
-# Birkhoff sum for measure preserving maps
+# Birkhoff sum and average for measure preserving maps
 
 This file contains some lemmas about the `birkhoffSum` and `birkhoffAverage` of a map which is
 `QuasiMeasurePreserving`.
 -/
 
-section MeasurePreserving
+section QuasiMeasurePreserving
 
 open MeasureTheory Measure Filter
 
 variable {α M : Type*} [AddCommMonoid M] [MeasurableSpace α]
 variable {f : α → α} {μ : Measure α} {φ φ' : α → M}
 
-/-- If `φ` and `φ'` are `ae μ` equal then the corresponding `birkhoffSum` are `=ᵐ[μ]` equal. -/
+/-- If observables  `φ` and `φ'` are `=ᵐ[μ]` equal then the corresponding `birkhoffSum` are `=ᵐ[μ]`
+equal. -/
 theorem birkhoffSum_ae_eq_of_ae_eq (hf : QuasiMeasurePreserving f μ μ) (hφ : φ =ᵐ[μ] φ') n :
     birkhoffSum f φ n =ᵐ[μ] birkhoffSum f φ' n := by
   obtain ⟨s, hs, hs'⟩ := eventuallyEq_iff_exists_mem.mp hφ
@@ -31,10 +33,11 @@ theorem birkhoffSum_ae_eq_of_ae_eq (hf : QuasiMeasurePreserving f μ μ) (hφ : 
   filter_upwards [ht] with x hx
   exact Finset.sum_congr rfl fun x _ => hs' (hx x)
 
-/-- If `φ` and `φ'` are `ae μ` equal then the corresponding `birkhoffAverage` are `=ᵐ[μ]` equal. -/
+/-- If observables `φ` and `φ'` are `=ᵐ[μ]` equal then the corresponding `birkhoffAverage` are
+`=ᵐ[μ]` equal. -/
 theorem birkhoffAverage_ae_eq_of_ae_eq (R : Type*) [DivisionSemiring R] [Module R M]
     (hf : QuasiMeasurePreserving f μ μ) (hφ : φ =ᵐ[μ] φ') n :
     birkhoffAverage R f φ n =ᵐ[μ] birkhoffAverage R f φ' n :=
   EventuallyEq.const_smul (birkhoffSum_ae_eq_of_ae_eq hf hφ n) (n : R)⁻¹
 
-end MeasurePreserving
+end QuasiMeasurePreserving
