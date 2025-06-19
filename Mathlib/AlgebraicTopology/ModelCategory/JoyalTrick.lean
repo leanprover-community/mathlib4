56d7667e16db0340b56d46915f201e07b6a85311
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-06-19 17:21:52 +0200

    fix year

--- Mathlib/AlgebraicTopology/ModelCategory/JoyalTrick.lean
+++ Mathlib/AlgebraicTopology/ModelCategory/JoyalTrick.lean
@@ -1,9 +1,8 @@
 /-
-Copyright (c) 2024 Joël Riou. All rights reserved.
+Copyright (c) 2025 Joël Riou. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joël Riou
 -/
-
 import Mathlib.AlgebraicTopology.ModelCategory.CategoryWithCofibrations
 import Mathlib.CategoryTheory.MorphismProperty.Limits
 
