53d06c5f987fcd75dad74bb18b2c7e8d8ec5a51e
Pierre Quinton <pierre.quinton@gmail.com>
2025-06-20 09:04:14 +0200

    remove double imports

--- Mathlib/Order/SymmDiff.lean
+++ Mathlib/Order/SymmDiff.lean
@@ -3,7 +3,6 @@ Copyright (c) 2021 Bryan Gin-ge Chen. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Adam Topaz, Bryan Gin-ge Chen, Yaël Dillies
 -/
-import Mathlib.Order.BooleanAlgebra.Defs
 import Mathlib.Order.BooleanAlgebra.Basic
 import Mathlib.Logic.Equiv.Basic
 
