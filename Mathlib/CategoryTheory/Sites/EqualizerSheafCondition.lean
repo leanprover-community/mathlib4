8a1f991e86298526534ec830f94d7e1945ee11d1
Christian Merten <christian@merten.dev>
2025-06-20 18:55:27 +0200

    move

--- Mathlib/CategoryTheory/Sites/EqualizerSheafCondition.lean
+++ Mathlib/CategoryTheory/Sites/EqualizerSheafCondition.lean
@@ -3,10 +3,8 @@ Copyright (c) 2020 Bhavik Mehta. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Bhavik Mehta
 -/
-import Mathlib.CategoryTheory.Limits.Opposites
-import Mathlib.CategoryTheory.Limits.Preserves.Shapes.Products
-import Mathlib.CategoryTheory.Limits.Types.Shapes
 import Mathlib.CategoryTheory.Sites.IsSheafFor
+import Mathlib.CategoryTheory.Limits.Types.Shapes
 import Mathlib.Tactic.ApplyFun
 
 /-!
@@ -400,63 +398,5 @@ end Presieve
 end
 
 end Equalizer
-
-variable {C : Type*} [Category C]
-
-/--
-If
-- `F` contravariantly maps (suitable) coproducts to products,
-- (suitable) coproducts exist in `C`, and
-- (suitable) coproducts distribute over pullbacks, then:
-
-`F` is a sheaf for the single object covering `{ ∐ᵢ Yᵢ ⟶ X }`
-if and only if it is a presieve for `{ fᵢ : Yᵢ ⟶ X }ᵢ`.
-
-Note: The second two conditions are satisfied if `C` is (finitary) extensive.
--/
-lemma Presieve.isSheafFor_sigmaDesc_iff {F : Cᵒᵖ ⥤ Type v} {X : C} {ι : Type*} [Small.{v} ι]
-    {Y : ι → C}
-    (f : ∀ i, Y i ⟶ X) [(ofArrows Y f).hasPullbacks]
-    [HasCoproduct Y] [HasCoproduct fun (ij : ι × ι) ↦ pullback (f ij.1) (f ij.2)]
-    [HasPullback (Limits.Sigma.desc f) (Limits.Sigma.desc f)]
-    [PreservesLimit (Discrete.functor <| fun i ↦ op (Y i)) F]
-    [PreservesLimit (Discrete.functor fun (ij : ι × ι) ↦ op (pullback (f ij.1) (f ij.2))) F]
-    [IsIso (Limits.Sigma.desc fun (ij : ι × ι) ↦ Limits.pullback.map (f ij.fst) (f ij.snd)
-      (Limits.Sigma.desc f) (Limits.Sigma.desc f) (Limits.Sigma.ι _ _) (Limits.Sigma.ι _ _) (𝟙 X)
-      (by simp) (by simp))] :
-    Presieve.IsSheafFor F (.singleton <| Limits.Sigma.desc f) ↔
-      Presieve.IsSheafFor F (.ofArrows Y f) := by
-  let e : (∐ fun (ij : ι × ι) ↦ pullback (f ij.1) (f ij.2)) ⟶
-      pullback (Limits.Sigma.desc f) (Limits.Sigma.desc f) :=
-    Limits.Sigma.desc fun ij ↦
-    pullback.map _ _ _ _ (Limits.Sigma.ι _ _) (Limits.Sigma.ι _ _) (𝟙 X) (by simp) (by simp)
-  rw [Equalizer.Presieve.isSheafFor_singleton_iff (pullback.cone _ _) (pullback.isLimit _ _),
-    Equalizer.Presieve.Arrows.sheaf_condition]
-  refine (Fork.isLimitEquivOfIsos _ _ ?_ ?_ ?_ ?_ ?_ ?_).nonempty_congr
-  · exact F.mapIso (opCoproductIsoProduct Y) ≪≫ PreservesProduct.iso F _
-  · exact F.mapIso (.op <| asIso e) ≪≫ F.mapIso (opCoproductIsoProduct _) ≪≫
-      PreservesProduct.iso F _
-  · exact Iso.refl _
-  · refine Pi.hom_ext _ _ fun ij ↦ ?_
-    simp only [Iso.trans_hom, Functor.mapIso_hom, PreservesProduct.iso_hom, Category.assoc,
-      limit.cone_x, PullbackCone.fst_limit_cone, Iso.op_hom, asIso_hom, e, piComparison_comp_π,
-      Equalizer.Presieve.Arrows.firstMap]
-    rw [← F.map_comp, opCoproductIsoProduct_hom_comp_π, ← F.map_comp, ← op_comp, Sigma.ι_desc,
-      ← F.map_comp, ← op_comp, pullback.lift_fst, Pi.lift_π, piComparison_comp_π_assoc,
-      ← F.map_comp, ← F.map_comp]
-    simp
-  · refine Pi.hom_ext _ _ fun ij ↦ ?_
-    simp only [Iso.trans_hom, Functor.mapIso_hom, PreservesProduct.iso_hom, Category.assoc,
-      limit.cone_x, PullbackCone.snd_limit_cone, Iso.op_hom, asIso_hom, e, piComparison_comp_π,
-      Equalizer.Presieve.Arrows.secondMap]
-    rw [← F.map_comp, opCoproductIsoProduct_hom_comp_π, ← F.map_comp, ← op_comp, Sigma.ι_desc,
-      ← F.map_comp, ← op_comp, pullback.lift_snd, Pi.lift_π, piComparison_comp_π_assoc,
-      ← F.map_comp, ← F.map_comp]
-    simp
-  · refine Pi.hom_ext _ _ fun i ↦ ?_
-    simp only [Fork.ofι_pt, Fork.ι_ofι, Iso.trans_hom, Functor.mapIso_hom, PreservesProduct.iso_hom,
-      Category.assoc, piComparison_comp_π]
-    rw [← F.map_comp, ← F.map_comp, opCoproductIsoProduct_hom_comp_π, ← op_comp, Sigma.ι_desc]
-    simp [Equalizer.Presieve.Arrows.forkMap]
 
 end CategoryTheory
