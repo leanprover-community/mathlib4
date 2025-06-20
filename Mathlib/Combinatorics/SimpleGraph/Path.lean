261c43441e69e5f5c6a1f48b1a17a12ea11655d6
Bolton Bailey <bolton.bailey@gmail.com>
2025-06-20 15:14:32 -0400

    grunweg's comment

--- Mathlib/Combinatorics/SimpleGraph/Path.lean
+++
@@ -1,5 +0,0 @@
-import Mathlib.Combinatorics.SimpleGraph.Connectivity.WalkDecomp
-import Mathlib.Combinatorics.SimpleGraph.Walk.Trail
-import Mathlib.Combinatorics.SimpleGraph.Subgraph
-
-deprecated_module (since := "2025-06-20")
--- Mathlib/Combinatorics/SimpleGraph/Walk.lean
+++
@@ -1,3 +0,0 @@
-import Mathlib.Combinatorics.SimpleGraph.DeleteEdges
-
-deprecated_module (since := "2025-06-20")
