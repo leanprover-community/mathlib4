61f20aee7e9250aa8489e1f7b1cb28913bf7b44e
Robin Carlier <57142648+robin-carlier@users.noreply.github.com>
2025-06-20 13:47:53 +0200

    Update Mathlib/CategoryTheory/Products/Basic.lean
    Co-authored-by: Joël Riou <37772949+joelriou@users.noreply.github.com>

--- Mathlib/CategoryTheory/Products/Basic.lean
+++ Mathlib/CategoryTheory/Products/Basic.lean
@@ -62,7 +62,7 @@ section
 variable {C D}
 
 /-- Construct a morphism in a product category by giving its constituent components.
-This constructor should be preffered over `Prod.mk`, because lean infers better the
+This constructor should be preferred over `Prod.mk`, because lean infers better the
 source and target of the resulting morphism. -/
 @[simps]
 def Prod.mkHom {X₁ X₂ : C} {Y₁ Y₂ : D} (f : X₁ ⟶ X₂) (g : Y₁ ⟶ Y₂) : (X₁, Y₁) ⟶ (X₂, Y₂) :=
