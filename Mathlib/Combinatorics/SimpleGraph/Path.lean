c65f4c33d1720bde491a9dcd5bcbe9521aefeaf4
Bolton Bailey <bolton.bailey@gmail.com>
2025-06-20 17:14:44 -0400

    add deprecations

---
+++ Mathlib/Combinatorics/SimpleGraph/Path.lean
@@ -0,0 +1,6 @@
+import Mathlib.Combinatorics.SimpleGraph.Walk.Circuit
+import Mathlib.Combinatorics.SimpleGraph.Walk.Cycle
+import Mathlib.Combinatorics.SimpleGraph.Walk.Path
+import Mathlib.Combinatorics.SimpleGraph.Walk.Trail
+
+deprecated_module (since := "2025-06-20")
