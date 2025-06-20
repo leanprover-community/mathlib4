ce3f3f6685cb2bc99511a9e1e1c37cc0ffe124a5
Patrick Massot <patrickmassot@free.fr>
2025-06-20 15:53:47 +0200

    Start covariant derivatives

---
+++ Mathlib/Geometry/Manifold/VectorBundle/CovariantDerivative.lean
@@ -0,0 +1,65 @@
+import Mathlib.Geometry.Manifold.VectorBundle.SmoothSection
+import Mathlib.Geometry.Manifold.VectorBundle.Tangent
+
+open Bundle Filter Function
+
+open scoped Bundle Manifold ContDiff
+
+variable {𝕜 : Type*} [NontriviallyNormedField 𝕜]
+
+
+section
+
+variable {E : Type*} [NormedAddCommGroup E]
+  [NormedSpace 𝕜 E] {H : Type*} [TopologicalSpace H] (I : ModelWithCorners 𝕜 E H)
+  {M : Type*} [TopologicalSpace M] [ChartedSpace H M] [IsManifold I 2 M]
+
+variable (F : Type*) [NormedAddCommGroup F] [NormedSpace 𝕜 F]
+  -- `F` model fiber
+  (n : WithTop ℕ∞)
+  (V : M → Type*) [TopologicalSpace (TotalSpace F V)]
+  [∀ x, AddCommGroup (V x)] [∀ x, Module 𝕜 (V x)]
+  [∀ x : M, TopologicalSpace (V x)] [∀ x, IsTopologicalAddGroup (V x)]
+  [∀ x, ContinuousSMul 𝕜 (V x)]
+  [FiberBundle F V] [VectorBundle 𝕜 F V]
+  -- `V` vector bundle
+
+
+def bar (a : 𝕜) : TangentSpace 𝓘(𝕜, 𝕜) a ≃L[𝕜] 𝕜 := sorry
+
+variable (x : M)
+-- set_option diagnostics true
+-- set_option trace.Meta.synthInstance.instances true in
+-- #synth AddCommMonoid (V x  →L[𝕜] V x)
+
+structure CovariantDerivative where
+  toFun : (Π x : M, TangentSpace I x) → (Π x : M, V x) → (Π x : M, V x)
+  addX : ∀ (X X' : Π x : M, TangentSpace I x) (σ : Π x : M, V x),
+    toFun (X + X') σ = toFun X σ + toFun X' σ
+  smulX : ∀ (X : Π x : M, TangentSpace I x) (σ : Π x : M, V x) (f : M → 𝕜),
+    toFun (f • X) σ = f • toFun X σ
+  addσ : ∀ (X : Π x : M, TangentSpace I x) (σ σ' : Π x : M, V x)(x : M),
+    MDifferentiableAt I (I.prod 𝓘(𝕜, F)) (fun x ↦ TotalSpace.mk' F x (σ x)) x
+    → MDifferentiableAt I (I.prod 𝓘(𝕜, F)) (fun x ↦ TotalSpace.mk' F x (σ' x)) x
+    → toFun X (σ + σ') x = toFun X σ x + toFun X σ' x
+  -- smul_const_σ : ∀ (X : Π x : M, TangentSpace I x) (σ : Π x : M, V x) (a : 𝕜),
+  --   toFun X (a • σ) = a • toFun X σ
+  leibniz : ∀ (X : Π x : M, TangentSpace I x) (σ : Π x : M, V x) (f : M → 𝕜) (x : M),
+    MDifferentiableAt I I.tangent (fun x ↦ (X x : TangentBundle I M)) x
+    → MDifferentiableAt I (I.prod 𝓘(𝕜, F)) (fun x ↦ TotalSpace.mk' F x (σ x)) x
+    → MDifferentiableAt I 𝓘(𝕜, 𝕜) f x
+    → toFun X (f • σ) x = (f • toFun X σ) x + (bar _ <| mfderiv I 𝓘(𝕜, 𝕜) f x (X x)) • σ x
+
+end
+
+
+
+
+section
+
+variable {E : Type*} [NormedAddCommGroup E] [NormedSpace 𝕜 E]
+variable {E' : Type*} [NormedAddCommGroup E'] [NormedSpace 𝕜 E']
+
+-- def trivial_covariant_derivative : CovariantDerivative 𝓘(𝕜, E) E' (fun _ ↦ E') 𝓘(𝕜, E')
+
+end
