44a6749cd77a81e4c0416b76a02038326e8d9956
Bolton Bailey <bolton.bailey@gmail.com>
2025-06-20 15:44:49 -0400

    add file

---
+++ Mathlib/Combinatorics/SimpleGraph/Ends/Example.lean
@@ -0,0 +1,23 @@
+/-
+Copyright (c) 2022 Bolton Bailey. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Bolton Bailey
+-/
+
+import Mathlib.Data.Finite.Set
+
+/-!
+# Git Hooks experiment
+
+I have added
+
+```bash
+#!/bin/bash
+lake exe mk_all
+```
+
+To `.git/hooks/pre-push`
+
+I will now push this as a PR to see if it works.
+
+-/
