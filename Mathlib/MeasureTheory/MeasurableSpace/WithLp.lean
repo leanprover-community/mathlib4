632e270bf4c90589e724cab9d0e749c7df2eefa0
Etienne Marion <baguettes_casino0c@icloud.com>
2025-06-21 13:48:37 +0200

    fun_prop and measurability

--- Mathlib/MeasureTheory/MeasurableSpace/WithLp.lean
+++ Mathlib/MeasureTheory/MeasurableSpace/WithLp.lean
@@ -12,9 +12,11 @@ variable {X : Type*} [MeasurableSpace X]
 instance measurableSpace : MeasurableSpace (WithLp p X) :=
   MeasurableSpace.map (WithLp.equiv p X).symm inferInstance
 
+@[fun_prop, measurability]
 lemma measurable_equiv : Measurable (WithLp.equiv p X) :=
   Measurable.of_comap_le MeasurableSpace.comap_map_le
 
+@[fun_prop, measurability]
 lemma measurable_equiv_symm : Measurable (WithLp.equiv p X).symm :=
   Measurable.of_le_map le_rfl
 
