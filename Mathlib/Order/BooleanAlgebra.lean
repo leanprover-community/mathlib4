8695aa6d15755aecce4bbcdae2149f3345a18a48
Pierre Quinton <pierre.quinton@gmail.com>
2025-06-21 23:11:59 +0200

    Update Mathlib/Order/BooleanAlgebra.lean
    Co-authored-by: Aaron Liu <aaronliu2008@outlook.com>

--- Mathlib/Order/BooleanAlgebra.lean
+++ Mathlib/Order/BooleanAlgebra.lean
@@ -4,5 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl, Bryan Gin-ge Chen
 -/
 import Mathlib.Order.BooleanAlgebra.Basic
+import Mathlib.Tactic.Linter.DeprecatedModule
 
 deprecated_module (since := "2025-06-19")
