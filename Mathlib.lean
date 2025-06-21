e1a882f63cbc2def2a5ae23fcd15353aeab5658b
LessnessRandomness <lessnessr@gmail.com>
2025-06-21 07:09:11 +0300

    Update Mathlib.lean
    Forgot to run `lake exe mk_all`

--- Mathlib.lean
+++ Mathlib.lean
@@ -1539,7 +1539,6 @@ import Mathlib.Analysis.InnerProductSpace.Spectrum
 import Mathlib.Analysis.InnerProductSpace.StarOrder
 import Mathlib.Analysis.InnerProductSpace.Subspace
 import Mathlib.Analysis.InnerProductSpace.Symmetric
-import Mathlib.Analysis.InnerProductSpace.TemporaryName
 import Mathlib.Analysis.InnerProductSpace.TwoDim
 import Mathlib.Analysis.InnerProductSpace.WeakOperatorTopology
 import Mathlib.Analysis.InnerProductSpace.l2Space
