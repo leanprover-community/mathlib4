7e60f69dc502b9c2f2b63e2ad4d09362e4019751
Robin Carlier <57142648+robin-carlier@users.noreply.github.com>
2025-06-21 19:24:40 +0200

    Update Mathlib/CategoryTheory/Limits/FilteredColimitCommutesFiniteLimit.lean
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- Mathlib/CategoryTheory/Limits/FilteredColimitCommutesFiniteLimit.lean
+++ Mathlib/CategoryTheory/Limits/FilteredColimitCommutesFiniteLimit.lean
@@ -27,7 +27,7 @@ colimit (over `K`) of the limits (over `J`) with the limit of the colimits is an
 -/
 
 -- Various pieces of algebra that have previously been spuriously imported here:
-assert_not_exists map_ne_zero Field GroupWithZero
+assert_not_exists map_ne_zero MonoidWithZero
 
 universe w v₁ v₂ v u₁ u₂ u
 
