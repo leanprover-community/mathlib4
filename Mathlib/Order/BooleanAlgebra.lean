2803a0085b2da536cddbab63de9ce2778218acd5
Pierre Quinton <pierre.quinton@gmail.com>
2025-06-21 22:39:33 +0200

    fix import

--- Mathlib/Order/BooleanAlgebra.lean
+++ Mathlib/Order/BooleanAlgebra.lean
@@ -3,7 +3,6 @@ Copyright (c) 2017 Johannes Hölzl. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl, Bryan Gin-ge Chen
 -/
-import Mathlib.Order.BooleanAlgebra.Defs
 import Mathlib.Order.BooleanAlgebra.Basic
 
 deprecated_module (since := "2025-06-19")
