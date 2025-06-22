04d19b6614f01cf58adec0359f2fddfdda8bc0f7
John Talbot <john.talbot@gmail.com>
2025-06-22 12:57:06 +0000

    chore(SimpleGraph): split `Combinatorics.SimpleGraph.Path` (#25844)
    Split the long file `SimpleGraph.Path.lean` into two roughly equal parts. 
    
    1. Material on Paths, Cycles, Circuits and Trails is in `Paths.lean`
    2. Material on Reachable/Preconnected/Connected/ConnectedComponents is in `Connectivity.Connected`

--- Mathlib.lean
+++ Mathlib.lean
@@ -2718,6 +2718,7 @@ import Mathlib.Combinatorics.SimpleGraph.Clique
 import Mathlib.Combinatorics.SimpleGraph.Coloring
 import Mathlib.Combinatorics.SimpleGraph.CompleteMultipartite
 import Mathlib.Combinatorics.SimpleGraph.ConcreteColorings
+import Mathlib.Combinatorics.SimpleGraph.Connectivity.Connected
 import Mathlib.Combinatorics.SimpleGraph.Connectivity.Represents
 import Mathlib.Combinatorics.SimpleGraph.Connectivity.Subgraph
 import Mathlib.Combinatorics.SimpleGraph.Connectivity.WalkCounting
@@ -2747,6 +2748,7 @@ import Mathlib.Combinatorics.SimpleGraph.Metric
 import Mathlib.Combinatorics.SimpleGraph.Operations
 import Mathlib.Combinatorics.SimpleGraph.Partition
 import Mathlib.Combinatorics.SimpleGraph.Path
+import Mathlib.Combinatorics.SimpleGraph.Paths
 import Mathlib.Combinatorics.SimpleGraph.Prod
 import Mathlib.Combinatorics.SimpleGraph.Regularity.Bound
 import Mathlib.Combinatorics.SimpleGraph.Regularity.Chunk
--- Mathlib/Combinatorics/SimpleGraph/Trails.lean
+++ Mathlib/Combinatorics/SimpleGraph/Trails.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kyle Miller
 -/
 import Mathlib.Algebra.Ring.Parity
-import Mathlib.Combinatorics.SimpleGraph.Path
+import Mathlib.Combinatorics.SimpleGraph.Paths
 
 /-!
 
