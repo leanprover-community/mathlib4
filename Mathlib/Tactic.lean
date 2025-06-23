0158cf28c143222e953c5a7581bffbeab0f4a48f
adomani <adomani@gmail.com>
2025-06-23 12:49:12 +0100

    remove unused files

--- Mathlib/Tactic.lean
+++ Mathlib/Tactic.lean
@@ -157,7 +157,6 @@ import Mathlib.Tactic.Linter.HashCommandLinter
 import Mathlib.Tactic.Linter.HaveLetLinter
 import Mathlib.Tactic.Linter.Header
 import Mathlib.Tactic.Linter.Lint
-import Mathlib.Tactic.Linter.Local
 import Mathlib.Tactic.Linter.MinImports
 import Mathlib.Tactic.Linter.Multigoal
 import Mathlib.Tactic.Linter.OldObtain
