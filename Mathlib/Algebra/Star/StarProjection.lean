2b71e2e0e790af13132747ee1f11f0a86fbda11c
Eric Wieser <wieser.eric@gmail.com>
2025-06-23 16:03:11 +0000

    style golf

--- Mathlib/Algebra/Star/StarProjection.lean
+++ Mathlib/Algebra/Star/StarProjection.lean
@@ -78,8 +78,7 @@ theorem add [NonUnitalNonAssocSemiring R] [StarRing R]
     (hp : IsStarProjection p) (hq : IsStarProjection q) (hpq : p * q = 0) :
     IsStarProjection (p + q) where
   isSelfAdjoint := hp.isSelfAdjoint.add hq.isSelfAdjoint
-  isIdempotentElem := by
-    apply IsIdempotentElem.add hp.isIdempotentElem hq.isIdempotentElem
+  isIdempotentElem := hp.isIdempotentElem.add hq.isIdempotentElem <| by
     rw [hpq, zero_add]
     simpa [hp.isSelfAdjoint.star_eq, hq.isSelfAdjoint.star_eq] using congr(star $(hpq))
 
