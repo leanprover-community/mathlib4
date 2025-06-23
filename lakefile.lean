9bb19c991ac039c5efcd04309b4d6cbd088a1d30
Anne C.A. Baanen <vierkantor@vierkantor.com>
2025-06-23 18:07:45 +0200

    Bump dependencies and silence linter.

--- lakefile.lean
+++ lakefile.lean
@@ -35,6 +35,7 @@ abbrev mathlibOnlyLinters : Array LeanOption := #[
 abbrev mathlibLeanOptions := #[
     ⟨`pp.unicode.fun, true⟩, -- pretty-prints `fun a ↦ b`
     ⟨`autoImplicit, false⟩,
+    ⟨`linter.unusedSimpArgs, false⟩, -- Temporarily disable this linter while we work on adaptations.
     ⟨`maxSynthPendingDepth, .ofNat 3⟩
   ] ++ -- options that are used in `lake build`
     mathlibOnlyLinters.map fun s ↦ { s with name := `weak ++ s.name }
