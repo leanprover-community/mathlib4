7556b8de455f4a41a611621dd694787183c053fb
Jeremy Tan Jie Rui <reddeloostw@gmail.com>
2025-06-23 14:05:09 +0000

    feat: IMO 1985 Q2 (#25783)

--- Mathlib.lean
+++ Mathlib.lean
@@ -3073,6 +3073,7 @@ import Mathlib.Data.Int.LeastGreatest
 import Mathlib.Data.Int.Lemmas
 import Mathlib.Data.Int.Log
 import Mathlib.Data.Int.ModEq
+import Mathlib.Data.Int.NatAbs
 import Mathlib.Data.Int.NatPrime
 import Mathlib.Data.Int.Notation
 import Mathlib.Data.Int.Order.Basic
