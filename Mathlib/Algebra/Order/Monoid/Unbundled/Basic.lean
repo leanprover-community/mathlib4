35e730a95c55448b091200c1ec12defc04d9bf69
Eric Wieser <wieser.eric@gmail.com>
2025-06-23 20:20:02 +0000

    whitespace

--- Mathlib/Algebra/Order/Monoid/Unbundled/Basic.lean
+++ Mathlib/Algebra/Order/Monoid/Unbundled/Basic.lean
@@ -1367,7 +1367,6 @@ protected theorem Injective [Mul α] [PartialOrder α] {a : α} (ha : MulLECance
     Injective (a * ·) :=
   fun _ _ h => le_antisymm (ha h.le) (ha h.ge)
 
-
 @[to_additive]
 protected theorem isLeftRegular [Mul α] [PartialOrder α] {a : α}
     (ha : MulLECancellable a) : IsLeftRegular a :=
