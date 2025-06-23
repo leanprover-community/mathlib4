f12d0291f7e0c68544b6b4701d1633dcbc2f4a9a
adomani <adomani@gmail.com>
2025-06-23 13:21:33 +0100

    fix New

--- Mathlib/New.lean
+++ Mathlib/New.lean
@@ -1,4 +1,13 @@
-import MathlibTest.CommandStart
+/-
+Copyright (c) 2025 Damiano Testa. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Damiano Testa
+-/
+import Mathlib.Init
+
+/-!
+# A useless module doc-string
+-/
 
 -- Testing that the `commandStart` is active on new files.
 variable  (a : Nat)
