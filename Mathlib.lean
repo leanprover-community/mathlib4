367d0c614f01fd54d3f573584e0f58fe8833264d
Eric Wieser <wieser.eric@gmail.com>
2025-06-24 10:56:32 +0000

    refactor: move the definition of `IsRegular` earlier (#26321)
    The motivation here is to be able to define things like `IsCancelMulZero` in terms of `IsRegular`, which would allow API for the latter to be used to provide instances for the former.
    
    As a first step, this moves the `MulZeroClass` lemmas abut `IsRegular` into the `GroupWithZero` folder, which is consistent with the ring lemmas living inside the `Ring` folder.

--- Mathlib.lean
+++ Mathlib.lean
@@ -469,6 +469,7 @@ import Mathlib.Algebra.GroupWithZero.Pointwise.Set.Basic
 import Mathlib.Algebra.GroupWithZero.Pointwise.Set.Card
 import Mathlib.Algebra.GroupWithZero.Prod
 import Mathlib.Algebra.GroupWithZero.ProdHom
+import Mathlib.Algebra.GroupWithZero.Regular
 import Mathlib.Algebra.GroupWithZero.Semiconj
 import Mathlib.Algebra.GroupWithZero.Subgroup
 import Mathlib.Algebra.GroupWithZero.Submonoid.Pointwise
