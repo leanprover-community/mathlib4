1670f386e223a35a284c86dfef09cfb8f2622ad1
adamtopaz <github@adamtopaz.com>
2025-06-24 14:52:18 -0600

    Add author

--- Mathlib/RingTheory/Valuation/ValuativeRel.lean
+++ Mathlib/RingTheory/Valuation/ValuativeRel.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2025 Adam Topaz. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Adam Topaz
+Authors: Aaron Liu, Adam Topaz
 -/
 import Mathlib.RingTheory.Valuation.Basic
 import Mathlib.Topology.Basic
