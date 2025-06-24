61819d78441a2b7cda41b3d376ab9d3b7fd45651
Yaël Dillies <yael.dillies@gmail.com>
2025-06-24 21:30:21 +0000

    deprecated aliases

--- Mathlib/CategoryTheory/Monoidal/CoherenceLemmas.lean
+++ Mathlib/CategoryTheory/Monoidal/CoherenceLemmas.lean
@@ -28,10 +28,14 @@ theorem leftUnitor_tensor_hom'' (X Y : C) :
     (α_ (𝟙_ C) X Y).hom ≫ (λ_ (X ⊗ Y)).hom = (λ_ X).hom ⊗ₘ 𝟙 Y := by
   monoidal_coherence
 
+@[deprecated (since := "2025-06-24")] alias leftUnitor_tensor'' := leftUnitor_tensor_hom''
+
 @[reassoc]
 theorem leftUnitor_tensor_hom' (X Y : C) :
     (λ_ (X ⊗ Y)).hom = (α_ (𝟙_ C) X Y).inv ≫ ((λ_ X).hom ⊗ₘ 𝟙 Y) := by
   monoidal_coherence
+
+@[deprecated (since := "2025-06-24")] alias leftUnitor_tensor' := leftUnitor_tensor_hom'
 
 @[reassoc]
 theorem leftUnitor_tensor_inv' (X Y : C) :
