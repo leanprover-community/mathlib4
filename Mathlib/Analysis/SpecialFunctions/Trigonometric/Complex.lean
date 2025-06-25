6fde5f514d7206f33169a47582fb5c1608fbd9cc
Eric Wieser <wieser.eric@gmail.com>
2025-06-25 23:01:41 +0000

    import woes

--- Mathlib/Analysis/SpecialFunctions/Trigonometric/Complex.lean
+++ Mathlib/Analysis/SpecialFunctions/Trigonometric/Complex.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Chris Hughes, Abhimanyu Pallavi Sudhir, Jean Lo, Calle Sönne, Benjamin Davidson
 -/
 import Mathlib.Algebra.QuadraticDiscriminant
-import Mathlib.Analysis.SpecialFunctions.Pow.Complex
+import Mathlib.Analysis.SpecialFunctions.Complex.Log
 
 /-!
 # Complex trigonometric functions
