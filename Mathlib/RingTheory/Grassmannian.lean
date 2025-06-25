0e51cb2210ee7536b26dcd80f634925082a4a6f2
Kenny Lau <kc_kennylau@yahoo.com.hk>
2025-06-25 10:34:30 +0100

    update docstring, rename to Module.Grassmannian

--- Mathlib/RingTheory/Grassmannian.lean
+++ Mathlib/RingTheory/Grassmannian.lean
@@ -11,43 +11,53 @@ import Mathlib.RingTheory.Spectrum.Prime.FreeLocus
 
 ## Main definitions
 
-- `Grassmannian`: `G(k, M; R)` is the `k`ᵗʰ Grassmannian of the `R`-module `M`. It is defined to be
-  the set of submodules of `M` whose quotient is locally free of rank `k`. Note that this indexing
-  is the opposite of some indexing in literature, where this rank would be `n - k` instead, where
-  `M = R ^ n`.
+- `Module.Grassmannian`: `G(k, M; R)` is the `k`ᵗʰ Grassmannian of the `R`-module `M`. It is defined
+  to be the set of submodules of `M` whose **quotient** is locally free of rank `k`. Note that there
+  is another convention in literature where the `k`ᵗʰ Grassmannian would instead be `k`-dimensional
+  subspaces of a given vector space over a field. See implementation notes below.
 
 ## Implementation notes
 
-Convention for a finite dimensional vector space `V` over a field `F`, is that `G(k, V; F)` should
-parametrise `k`-dimensional subspaces of `V`, but this only works for finite dimensional spaces, and
-over fields. This is why Grothendieck in EGA V.11 (unpublished) and
-[Grothendieck, EGA I.9.7.3][grothendieck-1971] (Springer edition only)
-defines the Grassmannians to parametrise locally free rank-`k` quotients instead.
+In the literature, two conventions exist:
 
-Then the conventional `k`-dimensional subspaces of `V` with dimension `n`, can be recovered by
-`G(n-k, V; F)`.
+1. The `k`ᵗʰ Grassmannian parametrises `k`-dimensional **subspaces** of a given finite dimensional
+   vector space over a field.
+2. The `k`ᵗʰ Grassmannian parametrises **quotients** that are locally free of rank `k`, of a given
+   module over a ring.
+
+For the purposes of Algebraic Geometry, the first definition here cannot be generalised to obtain
+a scheme to represent the functor, which is why the second definition is the one chosen by
+[Grothendieck, EGA I.9.7.3][grothendieck-1971] (Springer edition only), and in EGA V.11
+(unpublished).
+
+The first definition in the stated generality (i.e. over a field `F`, and finite dimensional vector
+space `V`) can be recovered from the second definition by noting that `k`-dimensional subspaces of
+`V` are canonically equivalent to `(n-k)`-dimensional quotients of `V`, and also to `k`-dimensional
+quotients of `V*`, the dual of `V`. In symbols, this means that the first definition is equivalent
+to `G(n - k, V; F)` and also to `G(k, V →ₗ[F] F; F)`, where `n` is the dimension of `V`.
 
 ## TODO
-- Define the functor `Grassmannian.functor R M k` that sends an `R`-algebra `A` to the set
+- Define and recover the subspace-definition (i.e. the first definition above).
+- Define the functor `Module.Grassmannian.functor R M k` that sends an `R`-algebra `A` to the set
   `G(k, A ⊗[R] M; A)`.
 - Define `chart x` indexed by `x : Fin k → M` as a subtype consisting of those
   `N ∈ G(k, A ⊗[R] M; A)` such that the composition `R^k → M → M⧸N` is an isomorphism.
-- Define `chartFunctor x` to turn `chart x` into a subfunctor of `Grassmannian.functor`. This will
-  correspond to an affine open chart in the Grassmannian.
+- Define `chartFunctor x` to turn `chart x` into a subfunctor of `Module.Grassmannian.functor`. This
+  will correspond to an affine open chart in the Grassmannian.
 - Grassmannians for schemes and quasi-coherent sheaf of modules.
-- Representability of `Grassmannian.functor R M k`.
+- Representability of `Module.Grassmannian.functor R M k`.
 -/
 
 universe u v w
 
-open Module
+namespace Module
 
 variable (R : Type u) [CommRing R] (M : Type v) [AddCommGroup M] [Module R M] (k : ℕ)
 
 /-- `G(k, M; R)` is the `k`ᵗʰ Grassmannian of the `R`-module `M`. It is defined to be the set of
-submodules of `M` whose quotient is locally free of rank `k`. Note that this indexing is the
-opposite of some indexing in literature, where this rank would be `n - k` instead, where
-`M = R ^ n`. -/
+submodules of `M` whose quotient is locally free of rank `k`. Note that there is another convention
+in literature where instead the submodule is required to have rank `k`. See the implementation
+notes in the beginning of this file. -/
 @[stacks 089R] structure Grassmannian extends Submodule R M where
   finite_quotient : Module.Finite R (M ⧸ toSubmodule)
   projective_quotient : Projective R (M ⧸ toSubmodule)
@@ -68,3 +78,5 @@ instance : CoeOut G(k, M; R) (Submodule R M) :=
   cases N₁; cases N₂; congr 1
 
 end Grassmannian
+
+end Module
