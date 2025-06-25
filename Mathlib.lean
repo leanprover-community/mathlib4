0765e3327a0f2e494539662f6c5355e92234ef42
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-06-25 10:15:56 +0000

    feat: the bicategory of adjunctions in a bicategory (#25977)
    Given a bicategory `B`, we construct a bicategory `Adj B` that has the same objects but whose `1`-morphisms are adjunctions (in the same direction as the left adjoints), and `2`-morphisms are tuples of mate maps between the left and right adjoints (where the map between right adjoints is in the opposite direction).
    
    
    
    Co-authored-by: Joël Riou <37772949+joelriou@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1928,6 +1928,7 @@ import Mathlib.CategoryTheory.Adjunction.Triple
 import Mathlib.CategoryTheory.Adjunction.Unique
 import Mathlib.CategoryTheory.Adjunction.Whiskering
 import Mathlib.CategoryTheory.Balanced
+import Mathlib.CategoryTheory.Bicategory.Adjunction.Adj
 import Mathlib.CategoryTheory.Bicategory.Adjunction.Basic
 import Mathlib.CategoryTheory.Bicategory.Adjunction.Mate
 import Mathlib.CategoryTheory.Bicategory.Basic
