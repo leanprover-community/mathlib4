8c96b53863371c4db6404021375e90403827c745
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-06-25 13:32:02 +0200

    wip

--- Mathlib/AlgebraicTopology/ModelCategory/DerivabilityStructure.lean
+++ Mathlib/AlgebraicTopology/ModelCategory/DerivabilityStructure.lean
@@ -3,7 +3,7 @@ Copyright (c) 2025 Joël Riou. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joël Riou
 -/
-import Mathlib.AlgebraicTopology.ModelCategory.FundamentalLemma3
+import Mathlib.AlgebraicTopology.ModelCategory.FundamentalLemma1
 import Mathlib.CategoryTheory.Localization.DerivabilityStructure.Constructor
 
 /-!
