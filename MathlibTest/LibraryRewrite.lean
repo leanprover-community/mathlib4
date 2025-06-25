f26627b9fcb2eff24a421231a86f7a24fc73e300
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2025-06-25 19:32:48 +0100

    reduce test file

--- MathlibTest/LibraryRewrite.lean
+++ MathlibTest/LibraryRewrite.lean
@@ -1,5 +1,7 @@
 import Mathlib.Tactic.Widget.LibraryRewrite
-import Mathlib
+import Mathlib.Algebra.Group.Nat.Defs
+import Mathlib.Data.Subtype
+import Batteries.Data.Nat.Gcd
 
 -- set_option trace.profiler true
 -- set_option trace.rw?? true
@@ -15,12 +17,8 @@ Pattern a → b → c
 · p ∧ p → q
   and_imp
 
-Pattern ∀ (a : α) (b : β), p a b
-· ∀ (x : p ×' p), q
-  PProd.forall'
-
 Pattern ∀ (x : α), p x → b
-· (∃ (_ : p), p) → q
+· (∃ x, p) → q
   exists_imp
 
 Pattern ∀ (x : α) (h : p x), q x h
@@ -38,8 +36,6 @@ Pattern a → b
   iff_or_self
 · p → q ↔ (p → q) ∨ p
   iff_self_or
-· p ⇨ (p → q)
-  himp_iff_imp
 · p ↔ (p → q) ∧ p
   iff_and_self
 · p ↔ p ∧ (p → q)
@@ -63,12 +59,10 @@ Pattern a → b
   ⊢ p
   imp_iff_right
 
-Pattern ∀ (i : ι), p i
-· ⨅ (_ : p), (p → q)
-  iInf_Prop_eq
+Pattern ∀ (p : P), Q p
 · p → p → p → q
   forall_self_imp
-· ¬∃ (_ : p), ¬(p → q)
+· ¬∃ x, ¬(p → q)
   Classical.not_exists_not
 · True
   ⊢ p → (p → q ↔ True)
@@ -84,32 +78,14 @@ Pattern ∀ (i : ι), p i
 info: Pattern n + 1
 · n.succ
   Nat.add_one
-· ack 0 n
-  ack_zero
-· (2 ^ n).size
-  Nat.size_pow
-· (Finset.Iic n).card
-  Nat.card_Iic
-· Nat.multichoose 2 n
-  Nat.multichoose_two
 · (↑n + 1).toNat
   Int.toNat_natCast_add_one
-· (n + 1).choose n
-  Nat.choose_succ_self_right
-· (Finset.antidiagonal n).card
-  Finset.Nat.card_antidiagonal
-· (List.Nat.antidiagonal n).length
-  List.Nat.length_antidiagonal
-· (Multiset.Nat.antidiagonal n).card
-  Multiset.Nat.card_antidiagonal
 
 Pattern n + m
 · 1 + n
   Nat.add_comm
 · n.add 1
   Nat.add_eq
-· Nat.succ^[1] n
-  Nat.succ_iterate
 · n.succ + 1 - 1
   Nat.succ_add_sub_one
 · n + Nat.succ 1 - 1
@@ -123,142 +99,15 @@ Pattern n + m
 · max n (n + 1)
   Nat.max_add_right_self
 
-Pattern x + 1
-· ∑ i ∈ Finset.range 2, n ^ i
-  geom_sum_two
-· (ComplexShape.down ℕ).prev n
-  ChainComplex.prev
-· (ComplexShape.up ℕ).next n
-  CochainComplex.next
-· Order.succ n
-  Order.succ_eq_add_one
-· SuccOrder.succ n
-  SuccAddOrder.succ_eq_add_one
-
 Pattern a + b
 · 1 + n
   add_comm
-· n +ᵥ 1
-  vadd_eq_add
-· max n 1 + min n 1
-  max_add_min
-· min n 1 + max n 1
-  min_add_max
-· AddOpposite.op 1 +ᵥ n
-  op_vadd_eq_add
-· {n, 1}.sum
-  Multiset.sum_pair
-· (addLeftEmbedding n) 1
-  addLeftEmbedding_apply
-· (n ::ₘ {1}).esymm 1
-  Multiset.esymm_pair_one
-· (addRightEmbedding 1) n
-  addRightEmbedding_apply
-· (OrderEmbedding.addLeft n) 1
-  OrderEmbedding.addLeft_apply
-· (OrderEmbedding.addRight 1) n
-  OrderEmbedding.addRight_apply
 -/
 #guard_msgs in
 #rw?? n+1
 
 /--
-info: Pattern p = 2
-· Even 5
-  ⊢ Nat.Prime 5
-  Nat.Prime.even_iff
-
-Pattern a = b
-· 5 ≤ 2 ∧ 2 ≤ 5
-  Nat.le_antisymm_iff
-· Nat.beq 5 2 = true
-  Nat.beq_eq
-· ↑5 = ↑2
-  ENat.coe_inj
-· Nat.succ 5 = Nat.succ 2
-  Nat.succ_inj
-· ↑5 = ↑2
-  Int.ofNat_inj
-· ↑5 = ↑2
-  Num.of_nat_inj
-· ↑5 = ↑2
-  Rat.natCast_inj
-· 5 ^^^ 2 = 0
-  Nat.xor_eq_zero
-· 5 + 1 = 2 + 1
-  Nat.add_one_inj
-· Int.negSucc 5 = Int.negSucc 2
-  Int.negSucc_inj
-· 5 * 5 = 2 * 2
-  Nat.mul_self_inj
-· Nonempty (Fin 5 ≃ Fin 2)
-  Fin.equiv_iff_eq
-· Nat.divisors 5 = Nat.divisors 2
-  Nat.divisors_inj
-· Nat.succPNat 5 = Nat.succPNat 2
-  Nat.succPNat_inj
-· compare 5 2 = Ordering.eq
-  Nat.compare_eq_eq
-· 5 ≡ 2 [MOD 0]
-  Nat.modEq_zero_iff
-· (5 == 2) = true
-  Nat.beq_eq_true_eq
-· ∀ (a : ℕ), a ∣ 5 ↔ a ∣ 2
-  Nat.dvd_left_iff_eq
-· ↑5 = ↑2
-  EReal.natCast_eq_iff
-· ↑5 = ↑2
-  PartENat.natCast_inj
-· ∀ (a : ℕ), 5 ∣ a ↔ 2 ∣ a
-  Nat.dvd_right_iff_eq
-· Int.subNatNat 5 2 = 0
-  Int.subNat_eq_zero_iff
-· Batteries.UnionFind.empty.Equiv 5 2
-  Batteries.UnionFind.equiv_empty
-· Int.negSucc 5 + 1 = -↑2
-  Int.negSucc_add_one_eq_neg_ofNat_iff
-· -↑5 = Int.negSucc 2 + 1
-  Int.neg_ofNat_eq_negSucc_add_one_iff
-· Nat.factorial 5 = Nat.factorial 2
-  ⊢ 1 < 5
-  Nat.factorial_inj
-· ∀ (p : ℕ), Nat.Prime p → (p ∣ 5 ↔ p ∣ 2)
-  ⊢ Squarefree 5
-  ⊢ Squarefree 2
-  Nat.Squarefree.ext_iff
-· ∀ (p : ℕ), Nat.Prime p → padicValNat p 5 = padicValNat p 2
-  ⊢ 5 ≠ 0
-  ⊢ 2 ≠ 0
-  Nat.eq_iff_prime_padicValNat_eq
-· 5 ∣ 2
-  ⊢ Nat.Prime 2
-  ⊢ 2 ≤ 5
-  Nat.dvd_prime_two_le
-· 2 ∣ 5
-  ⊢ Nat.Prime 5
-  ⊢ 2 ≠ 1
-  Nat.Prime.dvd_iff_eq
-
-Pattern m = OfNat.ofNat n
-· ↑5 = 2
-  WithBot.coe_eq_ofNat
-· ↑5 = 2
-  WithTop.coe_eq_ofNat
-
-Pattern OfNat.ofNat n = m
-· 5 = ↑2
-  WithBot.ofNat_eq_coe
-· 5 = ↑2
-  WithTop.ofNat_eq_coe
--/
-#guard_msgs in
-#rw?? 5=2
-
--- showing all rewrites:
-/--
 info: Pattern n / 2
-· n.div2
-  Nat.div2_val
 · n >>> 1
   Nat.shiftRight_one
 
@@ -267,242 +116,99 @@ Pattern x / y
   Nat.div_eq
 · (n - n % 2) / 2
   Nat.div_eq_sub_mod_div
-· {e ∈ Finset.range n | 2 ∣ e + 1}.card
-  Nat.card_multiples
-· n ⌊/⌋ 2
-  Nat.floorDiv_eq_div
-· {k ∈ Finset.range n.succ | k ≠ 0 ∧ 2 ∣ k}.card
-  Nat.card_multiples'
-· {x ∈ Finset.Ioc 0 n | 2 ∣ x}.card
-  Nat.Ioc_filter_dvd_card_eq_div
-· ⌊↑n / ↑2⌋₊
-  NNRat.floor_natCast_div_natCast
-· ⌊↑n / ↑2⌋₊
-  Rat.natFloor_natCast_div_natCast
 · 0
   ⊢ n < 2
   Nat.div_eq_of_lt
-· n.divExact 2 ?h
-  ⊢ 2 ∣ n
-  Nat.divExact_eq_div
 · (n + 1) / 2
   ⊢ ¬2 ∣ n + 1
   Nat.succ_div_of_not_dvd
-· (n + 1) / 2
-  ⊢ (n + 1) % 2 ≠ 0
-  Nat.succ_div_of_mod_ne_zero
 · (n - 2) / 2 + 1
   ⊢ 0 < 2
   ⊢ 2 ≤ n
   Nat.div_eq_sub_div
-· n / 2 % ?c
-  ⊢ n < ?c
-  Nat.div_mod_eq_div
-· ?m * n / (?m * 2)
-  ⊢ 0 < ?m
-  Nat.mul_div_mul_left
-· n * ?m / (2 * ?m)
-  ⊢ 0 < ?m
-  Nat.mul_div_mul_right
-· ?k
-  ⊢ ?k * 2 ≤ n
-  ⊢ n < (?k + 1) * 2
-  Nat.div_eq_of_lt_le
-· {x ∈ Finset.Ico 1 (Nat.succ ?c) | x * 2 ≤ n}.card
-  ⊢ 0 < 2
-  ⊢ n / 2 ≤ ?c
-  ZMod.div_eq_filter_card
-· ?k
-  ⊢ 0 < 2
-  ⊢ n = ?k * 2
-  Nat.div_eq_of_eq_mul_left
-· ?k
-  ⊢ 0 < 2
-  ⊢ n = 2 * ?k
-  Nat.div_eq_of_eq_mul_right
-· n / (2 / ?b) / ?b
-  ⊢ ?b ∣ 2
-  ⊢ 2 ∣ n
-  Nat.div_div_div_eq_div
-· ?a
-  ⊢ 2 ≠ 0
-  ⊢ ?a * 2 = n
-  Nat.eq_div_of_mul_eq_left
-· ?a
-  ⊢ 2 ≠ 0
-  ⊢ 2 * ?a = n
-  Nat.eq_div_of_mul_eq_right
 -/
 #guard_msgs in
-#rw?? all n/2
+#rw?? n/2
 
 /--
-info: Pattern ∑ a ∈ s, (f a + b)
-· ∑ a ∈ Finset.range n, a + (Finset.range n).card • 1
-  Finset.sum_add_card_nsmul
+info: Pattern n.gcd n
+· n
+  Nat.gcd_self
 
-Pattern ∑ x ∈ s, (f x + g x)
-· ∑ x ∈ Finset.range n, x + ∑ x ∈ Finset.range n, 1
-  Finset.sum_add_distrib
-
-Pattern ∑ i ∈ Finset.range n, f i
-· ∑ i, (↑i + 1)
-  Finset.sum_range
-· ∑ j ∈ Finset.range n, (n - 1 - j + 1)
-  Finset.sum_range_reflect
-· 0 + 1 + ∑ x ∈ Finset.Ico 1 n, (x + 1)
-  ⊢ 0 < n
-  Finset.sum_range_eq_add_Ico
-
-Pattern ∑ x ∈ s, h x (f x)
-· ∑ x ∈ (Finset.range n).attach, (↑x + 1)
-  Finsupp.sum_attach_index
-· (Finsupp.indicator (Finset.range n) fun x x => 1).sum HAdd.hAdd
-  ⊢ ∀ a ∈ Finset.range n, a + 0 = 0
-  Finsupp.sum_indicator_index
-
-Pattern ∑ c ∈ s, g c a
-· (∑ c ∈ Finset.range n, HAdd.hAdd c) 1
-  Finset.sum_apply
-
-Pattern ∑ i ∈ s, f i
-· Finset.fold (fun x1 x2 => x1 + x2) 0 (fun i => i + 1) (Finset.range n)
-  Finset.sum_eq_fold
-· ∑' (x : ℕ), (↑(Finset.range n)).indicator (fun x => x + 1) x
-  sum_eq_tsum_indicator
-· (Multiset.map (fun x => x + 1) (Finset.range n).val).sum
-  Finset.sum_eq_multiset_sum
-· ∑ x ∈ (Finset.range n).attach, (↑x + 1)
-  Finset.sum_attach
-· ∑ i, (↑i + 1)
-  Finset.sum_coe_sort
-· ∑' (x : { x // x ∈ Finset.range n }), (↑x + 1)
-  Finset.tsum_subtype
-· ∑' (x : ↑↑(Finset.range n)), (↑x + 1)
-  Finset.tsum_subtype'
-· ∑ i, (↑i + 1)
-  Finset.sum_finset_coe
-· ∑ᶠ (i : ℕ) (_ : i ∈ ↑(Finset.range n)), (i + 1)
-  finsum_mem_coe_finset
-· (List.map (fun n => n + 1) (Finset.range n).toList).sum
-  Finset.sum_map_toList
-· ∑ᶠ (i : ℕ) (_ : i ∈ Finset.range n), (i + 1)
-  finsum_mem_finset_eq_sum
-· (Finset.range n).noncommSum (fun n => n + 1) ⋯
-  Finset.noncommSum_eq_sum
-· ∑ x ∈ Finset.range n with x + 1 ≠ 0, (x + 1)
-  Finset.sum_filter_ne_zero
-· ∑ i ∈ Finset.range n, if i ∈ Finset.range n then i + 1 else 0
-  Finset.sum_extend_by_zero
-· 0
-  ⊢ ∀ x ∈ Finset.range n, x + 1 = 0
-  Finset.sum_eq_zero
-· ∑' (b : ℕ), (b + 1)
-  ⊢ ∀ b ∉ Finset.range n, b + 1 = 0
-  tsum_eq_sum
-· ∑ᶠ (i : ℕ), (i + 1)
-  ⊢ (Function.support fun i => i + 1) ⊆ ↑(Finset.range n)
-  finsum_eq_sum_of_support_subset
+Pattern m.gcd n
+· (n % n).gcd n
+  Nat.gcd_rec
+· if n = 0 then n else (n % n).gcd n
+  Nat.gcd_def
+· (n + n).gcd n
+  Nat.gcd_add_self_left
+· n.gcd (n + n)
+  Nat.gcd_add_self_right
+· (↑n).gcd ↑n
+  Int.gcd_natCast_natCast
+· (n.gcd n).gcd n
+  Nat.gcd_gcd_self_left_left
+· n.gcd (n.gcd n)
+  Nat.gcd_gcd_self_right_left
+· n
+  ⊢ n ∣ n
+  Nat.gcd_eq_left
+· 1
+  ⊢ n.Coprime n
+  Nat.Coprime.gcd_eq_one
+· (n - n).gcd n
+  ⊢ n ≤ n
+  Nat.gcd_self_sub_left
+· n.gcd (n - n)
+  ⊢ n ≤ n
+  Nat.gcd_self_sub_right
 -/
 #guard_msgs in
-#rw?? ∑ n ∈ Finset.range n, (n + 1)
+#rw?? Nat.gcd n n
 
--- `Inv.inv` is indexed as `id⁻¹`, and `fun x => (f x)⁻¹` is indexed as `f⁻¹`.
-axiom foo (f : Rat → Rat) : Continuous (fun x => (f x)⁻¹) = True
+def AtZero (f : Int → Int) : Int := f 0
+
+theorem atZero_neg (f : Int → Int) : AtZero (fun x => - f x) = - AtZero f := rfl
+theorem neg_atZero_neg (f : Int → Int) : - AtZero (fun x => - f x) = AtZero f := Int.neg_neg (f 0)
+
+theorem atZero_add (f g : Int → Int) : AtZero (fun x => f x + g x) = AtZero f + AtZero g := rfl
+theorem atZero_add_const (f : Int → Int) (c : Int) : AtZero (fun x => f x + c) = AtZero f + c := rfl
 
 /--
-info: Pattern Continuous fun x => (f x)⁻¹
-· True
-  foo
+info: Pattern AtZero fun x => f x + c
+· (AtZero fun x => x) + 1
+  atZero_add_const
 
-Pattern Continuous f
-· ∀ (s : Set ℚ), IsOpen s → IsOpen (Inv.inv ⁻¹' s)
-  continuous_def
-· ∀ (b : ℚ), ∀ ε > 0, ∃ δ > 0, ∀ (a : ℚ), dist a b < δ → dist a⁻¹ b⁻¹ < ε
-  Metric.continuous_iff
-· Continuous (Rat.cast ∘ Inv.inv)
-  continuous_induced_rng
-· ∀ (a : ℚ), ∀ ε > 0, ∀ᶠ (x : ℚ) in nhds a, dist x⁻¹ a⁻¹ < ε
-  Metric.continuous_iff'
-· ∀ (s : Set ℚ), IsClosed s → IsClosed (Inv.inv ⁻¹' s)
-  continuous_iff_isClosed
-· PseudoMetricSpace.toUniformSpace.toTopologicalSpace ≤
-    TopologicalSpace.induced Inv.inv PseudoMetricSpace.toUniformSpace.toTopologicalSpace
-  continuous_iff_le_induced
-· ∀ (x : ℚ) (g : Ultrafilter ℚ), ↑g ≤ nhds x → Filter.Tendsto Inv.inv (↑g) (nhds x⁻¹)
-  continuous_iff_ultrafilter
-· ∀ (x : ℚ), ContinuousAt Inv.inv x
-  continuous_iff_continuousAt
-· TopologicalSpace.coinduced Inv.inv PseudoMetricSpace.toUniformSpace.toTopologicalSpace ≤
-    PseudoMetricSpace.toUniformSpace.toTopologicalSpace
-  continuous_iff_coinduced_le
-· SeqContinuous Inv.inv
-  continuous_iff_seqContinuous
-· ∀ (b : ℚ), Filter.Tendsto (fun x => (x⁻¹, b⁻¹)) (nhds b) (uniformity ℚ)
-  Uniform.continuous_iff'_left
-· ∀ (b : ℚ), Filter.Tendsto (fun x => (b⁻¹, x⁻¹)) (nhds b) (uniformity ℚ)
-  Uniform.continuous_iff'_right
-· Continuous fun x => dist x.1⁻¹ x.2⁻¹
-  continuous_iff_continuous_dist
-· ContinuousOn Inv.inv Set.univ
-  continuous_iff_continuousOn_univ
-· LowerSemicontinuous Inv.inv ∧ UpperSemicontinuous Inv.inv
-  continuous_iff_lower_upperSemicontinuous
-· Continuous (-Inv.inv)
-  continuous_neg_iff
+Pattern AtZero fun x => f x + g x
+· (AtZero fun x => x) + AtZero fun x => 1
+  atZero_add
+
+Pattern AtZero f
+· -AtZero fun x => -(x + 1)
+  neg_atZero_neg
 -/
 #guard_msgs in
-#rw?? Continuous (Inv.inv : Rat → Rat)
-
--- Nat.Coprime is reducible, so we would get matches with the pattern `n = 1`.
--- But this wouldn't work with the `rw` tactic, so we make sure to avoid this.
+#rw?? AtZero fun x => x + 1
 /--
-info: Pattern Nat.Coprime 2 n
-· Odd 3
-  Nat.coprime_two_left
+info: Pattern AtZero fun x => -f x
+· -AtZero fun x => x
+  atZero_neg
 
-Pattern n.Coprime m
+Pattern AtZero f
+· -AtZero fun x => - -x
+  neg_atZero_neg
+-/
+#guard_msgs in
+#rw?? AtZero (Neg.neg : Int → Int)
+
+-- `Nat.Coprime` is reducible, so we might get matches with the pattern `n = 1`.
+-- This doesn't work with the `rw` tactic though, so we make sure to avoid them.
+/--
+info: Pattern n.Coprime m
 · Nat.Coprime 3 2
   Nat.coprime_comm
-· IsRelPrime 2 3
-  Nat.coprime_iff_isRelPrime
 · Nat.gcd 2 3 = 1
   Nat.coprime_iff_gcd_eq_one
-· IsUnit ↑2
-  ZMod.isUnit_iff_coprime
-· IsCoprime ↑2 ↑3
-  Nat.isCoprime_iff_coprime
-· (2 + 3).Coprime 3
-  Nat.coprime_add_self_left
-· (3 + 2).Coprime 3
-  Nat.coprime_self_add_left
-· Nat.Coprime 2 (3 + 2)
-  Nat.coprime_add_self_right
-· Nat.Coprime 2 (2 + 3)
-  Nat.coprime_self_add_right
-· ¬2 ∣ 3
-  ⊢ Nat.Prime 2
-  Nat.Prime.coprime_iff_not_dvd
-· (3 - 2).Coprime 3
-  ⊢ 2 ≤ 3
-  Nat.coprime_self_sub_left
-· (2 - 3).Coprime 3
-  ⊢ 3 ≤ 2
-  Nat.coprime_sub_self_left
-· Nat.Coprime 2 (2 - 3)
-  ⊢ 3 ≤ 2
-  Nat.coprime_self_sub_right
-· Nat.Coprime 2 (3 - 2)
-  ⊢ 2 ≤ 3
-  Nat.coprime_sub_self_right
-· 2 ≠ 3
-  ⊢ Nat.Prime 2
-  ⊢ Nat.Prime 3
-  Nat.coprime_primes
-· Disjoint (Nat.primeFactors 2) (Nat.primeFactors 3)
-  ⊢ 2 ≠ 0
-  ⊢ 3 ≠ 0
-  Nat.disjoint_primeFactors
 -/
 #guard_msgs in
 #rw?? Nat.Coprime 2 3
