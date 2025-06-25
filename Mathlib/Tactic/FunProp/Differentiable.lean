c5c177bc8d9989d5e39fcbd8e8886c13f6215553
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-06-25 14:48:11 +0200

    chore: move three results to their proper place, and name using the fun_comp convention

--- Mathlib/Tactic/FunProp/Differentiable.lean
+++ Mathlib/Tactic/FunProp/Differentiable.lean
@@ -31,22 +31,6 @@ variable {F : Type*} [NormedAddCommGroup F] [NormedSpace K F]
 variable {G : Type*} [NormedAddCommGroup G] [NormedSpace K G]
 variable {f : E → F} {x} {s}
 
-@[fun_prop]
-theorem Differentiable.comp' {g : F → G} (hg : Differentiable K g) (hf : Differentiable K f) :
-    Differentiable K (fun x => g (f x)) :=
-  fun x => DifferentiableAt.comp x (hg (f x)) (hf x)
-
-@[fun_prop]
-theorem DifferentiableAt.comp' {f : E → F} {g : F → G} (hg : DifferentiableAt K g (f x))
-    (hf : DifferentiableAt K f x) : DifferentiableAt K (fun x => g (f x)) x :=
-  (hg.hasFDerivAt.comp x hf.hasFDerivAt).differentiableAt
-
-@[fun_prop]
-theorem DifferentiableOn.comp' {g : F → G} {t : Set F} (hg : DifferentiableOn K g t)
-    (hf : DifferentiableOn K f s) (st : Set.MapsTo f s t) :
-    DifferentiableOn K (fun x => g (f x)) s :=
-  fun x hx => DifferentiableWithinAt.comp x (hg (f x) (st hx)) (hf x hx) st
-
 end lambda_rules
 
 end Missing
