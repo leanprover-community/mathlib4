214a94bb40626935fdca5cef6e3b5f519a275c59
Vasilii Nesterov <vas.nesterov63@gmail.com>
2025-06-26 16:55:16 +0300

    shake

--- Mathlib/Algebra/Order/Floor/Semiring.lean
+++ Mathlib/Algebra/Order/Floor/Semiring.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro, Kevin Kappelmann
 -/
 import Mathlib.Algebra.Order.Floor.Defs
-import Mathlib.Data.Nat.Cast.Order.Field
+import Mathlib.Algebra.Order.Field.Basic
 import Mathlib.Tactic.Linarith
 
 /-!
