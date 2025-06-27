e1c56935ffc505637c9172c48611131ad355847e
artie2000 <artem.khovanov@gmail.com>
2025-06-27 01:26:53 +0100

    update docs

--- Mathlib/Tactic/SetLike.lean
+++ Mathlib/Tactic/SetLike.lean
@@ -30,23 +30,24 @@ are given the `aesop` attribute according to the following principles:
 - Apply-style rules with nontrivial hypotheses are marked `unsafe`. This is because applying them
   might not be provability-preserving in the context of more complex membership rules.
   For instance, `mul_mem` is marked `unsafe`.
-- Unsafe rules are not given a priority higher than 90%. This is the same probability
-  Aesop gives to safe rules when they generate metavariables. If the priority is too high, loops
-  generated in the presence of metavariables will time out Aesop.
-- Apply-style rules with simple hypotheses which are refuted quickly if they aren't provable
-  are given probability 90%. An example is `mul_mem`.
-- Rules that cause loops (even in the absence of metavariables) are given a priority of 5%.
-  For performance reasons, these rules are placed in the `SetLike!` ruleset instead of the
-  `SetLike` ruleset. An example is `SetLike.mem_of_subset`.
-- All other `unsafe` rules are given a probability between 5% and 90% based on how likely they are
-  to progress the proof state towards a solution.
-- Simplifying the left hand side of a membership goal (e.g., `mul_mem`) is prioritised over
-  simplifying the right hand side (e.g., `Subgroup.mem_closure_of_mem`).
+  - Unsafe rules are given a probability no higher than 90%. This is the same probability
+    Aesop gives to safe rules when they generate metavariables. If the priority is too high, loops
+    generated in the presence of metavariables will time out Aesop.
+  - Rules that cause loops (even in the absence of metavariables) are given a low priority of 5%.
+    These rules are placed in the `SetLike!` ruleset instead of the `SetLike` ruleset so that
+    they are not invoked by default. An example is `SetLike.mem_of_subset`.
+  - Simplifying the left hand side of a membership goal is prioritised over simplifying the
+    right hand side. By default, rules simplifying the LHS (e.g., `mul_mem`) are given
+    probability 90% and rules simplifying the RHS are given probability 80%
+    (e.g., `Subgroup.mem_closure_of_mem`).
+  - These default probabilities are for rules with simple hypotheses that fail quickly when
+    not satisfied, such as `mul_mem`. Rules with more complicated hypotheses, or rules that are
+    less likely to progress the proof state towards a solution, are given a lower priority.
 - To optimise performance and avoid timeouts, Aesop should not be invoking low-priority rules
   unless it can make no other progress. If common usage patterns cause Aesop to invoke such rules,
   additional lemmas are added at a higher priority to cover that pattern.
   For example, `Subgroup.mem_closure_of_mem` covers a common use case of `SetLike.mem_of_subset`.
 
-Some examples of membership-related goals which Aesop this ruleset is designed to close
+Some examples of membership-related goals which Aesop with this ruleset is designed to close
 can be found in the file MathlibTest/set_like.lean.
 -/
