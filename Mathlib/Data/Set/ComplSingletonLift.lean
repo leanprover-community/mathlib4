90465826b74c48bd2f7efb637e0ad070eb32b572
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-06-27 10:43:09 +0200

    fixing imports

--- Mathlib/Data/Set/ComplSingletonLift.lean
+++ Mathlib/Data/Set/ComplSingletonLift.lean
@@ -3,7 +3,8 @@ Copyright (c) 2025 Joël Riou. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joël Riou
 -/
-import Mathlib.Data.Set.Basic
+import Mathlib.Data.Set.Operations
+import Mathlib.Logic.Equiv.Defs
 
 /-!
 # Extending a function from the complement of a singleton
