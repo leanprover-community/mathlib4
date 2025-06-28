3dbdaec85ab45f208ad67e98b1d6782f0db59762
Paul Schwahn <schwahn@ime.unicamp.br>
2025-06-28 18:49:58 -0300

    refactor `prodEquivOfIsCompl_symm_apply`
    Co-authored-by: Kenny Lau <kc_kennylau@yahoo.com.hk>

--- Mathlib/LinearAlgebra/Projection.lean
+++ Mathlib/LinearAlgebra/Projection.lean
@@ -128,9 +128,9 @@ theorem prodEquivOfIsCompl_symm_apply_snd_eq_zero (h : IsCompl p q) {x : E} :
     mem_left_iff_eq_zero_of_disjoint h.disjoint]
 
 theorem prodEquivOfIsCompl_symm_apply (h : IsCompl p q)
-      (x y z : E) (hy : y ∈ p) (hz : z ∈ q) (hx : x = y + z) :
-    (prodEquivOfIsCompl p q h).symm x = ⟨⟨y, hy⟩, ⟨z, hz⟩⟩ := by
-  rw [LinearEquiv.symm_apply_eq, coe_prodEquivOfIsCompl', hx]
+      (x y z : E) (hy : y ∈ p) (hz : z ∈ q) (hx : y + z = x) :
+    (prodEquivOfIsCompl p q h).symm x = ⟨⟨y, hy⟩, ⟨z, hz⟩⟩ :=
+  (LinearEquiv.symm_apply_eq _).2 hx.symm
 
 theorem prodEquivOfIsCompl_symm_add (h : IsCompl p q) (x : E) :
     x = ((prodEquivOfIsCompl p q h).symm x).1.val + ((prodEquivOfIsCompl p q h).symm x).2.val :=
