0b9f88fa4bae7b54b8f645fcbec6696caa40a205
Emily Riehl <eriehl@jhu.edu>
2025-06-29 15:12:35 +0100

    Update Mathlib/CategoryTheory/EqToHom.lean
    Co-authored-by: Robin Carlier <57142648+robin-carlier@users.noreply.github.com>

--- Mathlib/CategoryTheory/EqToHom.lean
+++ Mathlib/CategoryTheory/EqToHom.lean
@@ -228,7 +228,6 @@ instance {X Y : C} (h : X = Y) : IsIso (eqToHom h) :=
 theorem inv_eqToHom {X Y : C} (h : X = Y) : inv (eqToHom h) = eqToHom h.symm := by
   aesop_cat
 
-@[simp]
 theorem eqToHom_quiverHomOfEq {X Y : C} (f : X ⟶ Y) {X' Y' : C} (hX : X = X') (hY : Y = Y') :
     Quiver.homOfEq f hX hY = eqToHom hX.symm ≫ f ≫ eqToHom hY := by
   subst hX hY; simp
