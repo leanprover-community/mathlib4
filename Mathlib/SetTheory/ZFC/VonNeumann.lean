e6ce3f405b299fd59825aac6d1474d5a7b1a9e82
Violeta Hernández <vi.hdz.p@gmail.com>
2025-06-30 15:36:42 -0600

    Update Mathlib/SetTheory/ZFC/VonNeumann.lean

--- Mathlib/SetTheory/ZFC/VonNeumann.lean
+++ Mathlib/SetTheory/ZFC/VonNeumann.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2024 Violeta Hernández Palacios. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Violeta Hernández Palacios, Knala Solomon
+Authors: Violeta Hernández Palacios
 -/
 import Mathlib.SetTheory.ZFC.Class
 import Mathlib.SetTheory.ZFC.Ordinal
