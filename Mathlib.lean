02d1f1a6c061c06977996b8c674166a354c1a3d6
María Inés de Frutos-Fernández <mariaines.dff@gmail.com>
2025-06-30 16:02:15 +0000

    feat(RingTheory/DividedPowers/SubPDIdeal): add SubDPIdeal (#24480)
    Let `A` be a commutative (semi)ring and let `I` be an ideal of `A` with a divided power
    structure `hI`. A subideal `J` of `I` is a *sub-dp-ideal* of `(I, hI)` if, for all `n ∈ ℕ > 0` and
    all `x ∈ J`, `hI.dpow n x ∈ J`.
    
    The "TODOs" listed on the file docstring have been formalized by the authors and will be added in subsequent PRs.
    
    Co-authored-by: @AntoineChambert-Loir 
    
    
    
    Co-authored-by: Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>

--- Mathlib.lean
+++ Mathlib.lean
@@ -5164,6 +5164,7 @@ import Mathlib.RingTheory.DiscreteValuationRing.TFAE
 import Mathlib.RingTheory.Discriminant
 import Mathlib.RingTheory.DividedPowers.Basic
 import Mathlib.RingTheory.DividedPowers.DPMorphism
+import Mathlib.RingTheory.DividedPowers.SubDPIdeal
 import Mathlib.RingTheory.DualNumber
 import Mathlib.RingTheory.EisensteinCriterion
 import Mathlib.RingTheory.EssentialFiniteness
