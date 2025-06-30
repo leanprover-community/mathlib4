dd3d6417d01876c5535ab14cc5899a05be57b176
Monica Omar <2497250a@research.gla.ac.uk>
2025-06-30 18:17:45 +0000

    fix

--- Mathlib/Analysis/InnerProductSpace/Positive.lean
+++ Mathlib/Analysis/InnerProductSpace/Positive.lean
@@ -130,9 +130,8 @@ theorem IsPositive.adjoint_conj {T : E →L[𝕜] E} (hT : T.IsPositive) (S : F 
 
 theorem IsPositive.conj_orthogonalProjection (U : Submodule 𝕜 E) {T : E →L[𝕜] E} (hT : T.IsPositive)
     [CompleteSpace U] :
-    (U.subtypeL ∘L
-        U.orthogonalProjection ∘L T ∘L U.subtypeL ∘L U.orthogonalProjection).IsPositive := by
-  have := hT.conj_adjoint (U.subtypeL ∘L U.orthogonalProjection)
+    (U.starProjection ∘L T ∘L U.starProjection).IsPositive := by
+  have := hT.conj_adjoint (U.starProjection)
   rwa [(orthogonalProjection_isSelfAdjoint U).adjoint_eq] at this
 
 theorem IsPositive.orthogonalProjection_comp {T : E →L[𝕜] E} (hT : T.IsPositive) (U : Submodule 𝕜 E)
