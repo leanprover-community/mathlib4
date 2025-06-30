df074b280d5fbbc2c877d082650e2c281deb5239
Amelia Livingston <101damnations@github.com>
2025-06-30 14:03:56 +0000

    feat(RepresentationTheory/Homological/GroupHomology/LowDegree): simpler expressions for differentials (#25873)
    The first part of a file giving simpler expressions for group homology in low degree, following the structure of `RepresentationTheory/Homological/GroupCohomology/LowDegree.lean`.
    
    
    
    Co-authored-by: Amelia Livingston <al3717@ic.ac.uk>

--- Mathlib.lean
+++ Mathlib.lean
@@ -5077,6 +5077,7 @@ import Mathlib.RepresentationTheory.Homological.GroupCohomology.Hilbert90
 import Mathlib.RepresentationTheory.Homological.GroupCohomology.LowDegree
 import Mathlib.RepresentationTheory.Homological.GroupHomology.Basic
 import Mathlib.RepresentationTheory.Homological.GroupHomology.Functoriality
+import Mathlib.RepresentationTheory.Homological.GroupHomology.LowDegree
 import Mathlib.RepresentationTheory.Homological.Resolution
 import Mathlib.RepresentationTheory.Induced
 import Mathlib.RepresentationTheory.Invariants
