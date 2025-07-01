799fb449fe197898617a7b515cfec28633af4a50
David Loeffler <d.loeffler.01@cantab.net>
2025-07-01 15:39:56 +0200

    use mkOfDetNeZero, de-privatize

--- Mathlib/NumberTheory/ModularForms/Basic.lean
+++ Mathlib/NumberTheory/ModularForms/Basic.lean
@@ -27,6 +27,31 @@ open scoped Topology Manifold MatrixGroups ComplexConjugate
 
 noncomputable section
 
+namespace UpperHalfPlane
+
+/-- The matrix `[1, 0; 0, -1]`, which defines an anti-holomorphic involution of `ℍ` via
+`τ ↦ -conj τ`. -/
+def J : GL (Fin 2) ℝ := .mkOfDetNeZero !![1, 0; 0, -1] (by simp)
+
+lemma coe_J_smul (τ : ℍ) : (↑(J • τ) : ℂ) = -conj ↑τ := by
+  simp [UpperHalfPlane.coe_smul, σ, J, if_neg (show ¬(1 : ℝ) < 0 by norm_num), num, denom, div_neg]
+
+lemma J_smul (τ : ℍ) : J • τ = ofComplex (-(conj ↑τ)) := by
+  ext
+  rw [coe_J_smul, ofComplex_apply_of_im_pos (by simpa using τ.im_pos), coe_mk_subtype]
+
+@[simp] lemma val_J : J.val = !![1, 0; 0, -1] := rfl
+
+@[simp] lemma J_sq : J ^ 2 = 1 := by ext; simp [J, sq, Matrix.one_fin_two]
+
+@[simp] lemma det_J : J.det = -1 := by ext; simp [J]
+
+@[simp] lemma sigma_J : σ J = starRingEnd ℂ := by simp [σ, J]
+
+@[simp] lemma denom_J (τ : ℍ) : denom J τ = -1 := by simp [J, denom]
+
+end UpperHalfPlane
+
 section ModularForm
 
 open ModularForm
@@ -39,29 +64,11 @@ private lemma MDifferentiable.slash_of_pos {f : ℍ → ℂ} (hf : MDifferentiab
   refine .mul (.mul ?_ mdifferentiable_const) (mdifferentiable_denom_zpow g _)
   simpa only [σ, hg, ↓reduceIte] using hf.comp (mdifferentiable_smul hg)
 
-private abbrev J : GL (Fin 2) ℝ :=
-  ⟨!![1, 0; 0, -1], !![1, 0; 0, -1], by simp [Matrix.one_fin_two], by simp [Matrix.one_fin_two]⟩
-
-private lemma J_sq : J ^2 = 1 := by
-  ext : 1
-  simp [sq, Matrix.mul_fin_two, Matrix.one_fin_two]
-
-private lemma J_smul (τ : ℍ) :
-    J • τ = ofComplex (-(conj ↑τ)) := by
-  ext
-  simp [UpperHalfPlane.coe_smul, σ, if_neg (show ¬(1 : ℝ) < 0 by norm_num), num, denom,
-    div_neg, ofComplex_apply_of_im_pos (by simpa using τ.im_pos : 0 < (-(starRingEnd ℂ) ↑τ).im)]
-
 private lemma slash_J (f : ℍ → ℂ) (k : ℤ) :
     f ∣[k] J = fun τ : ℍ ↦ -conj (f <| ofComplex <| -(conj ↑τ)) := by
   ext τ
-  simp only [slash_def]
-  have detj : J.det = -1 := by ext; simp [J]
-  have sigj : σ J = starRingEnd ℂ := by simp [σ, J]
-  have denj : denom J τ = -1 := by simp [denom]
-  rw [detj, sigj, denj, Units.val_neg, Units.val_one, ofReal_neg, ofReal_one, mul_assoc,
-    ← zpow_add₀ (by norm_num : (-1 : ℂ) ≠ 0), (by ring : k - 1 + -k = -1),
-    zpow_neg_one, inv_neg_one, mul_neg_one, J_smul]
+  simp [slash_def, J_smul, mul_assoc, ← zpow_add₀ (by norm_num : (-1 : ℂ) ≠ 0),
+    (by ring : k - 1 + -k = -1), -zpow_neg, zpow_neg_one]
 
 /-- The weight `k` slash action of the negative-determinant matrix `J` preserves holomorphic
 functions. -/
@@ -79,12 +86,9 @@ private lemma MDifferentiable.slashJ {f : ℍ → ℂ} (hf : MDifferentiable �
 /-- The weight `k` slash action of `GL(2, ℝ)` preserves holomorphic functions. -/
 lemma MDifferentiable.slash {f : ℍ → ℂ} (hf : MDifferentiable 𝓘(ℂ) 𝓘(ℂ) f)
     (k : ℤ) (g : GL (Fin 2) ℝ) : MDifferentiable 𝓘(ℂ) 𝓘(ℂ) (f ∣[k] g) := by
-  rcases g.det_ne_zero.lt_or_gt with hg | hg
-  · have : g = J * (J * g) := by rw [← mul_assoc, ← sq, J_sq, one_mul]
-    rw [this, SlashAction.slash_mul]
-    apply (hf.slashJ k).slash_of_pos
-    simpa only [map_mul, Units.val_mul, g.val_det_apply] using mul_pos_of_neg_of_neg (by simp) hg
-  · exact hf.slash_of_pos k hg
+  refine g.det_ne_zero.lt_or_gt.elim (fun hg ↦ ?_) (hf.slash_of_pos k)
+  rw [show g = J * (J * g) by simp [← mul_assoc, ← sq], SlashAction.slash_mul]
+  exact (hf.slashJ k).slash_of_pos _ (by simpa using hg)
 
 variable (F : Type*) (Γ : Subgroup SL(2, ℤ)) (k : ℤ)
 
