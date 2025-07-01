baf7cd26532c87bf533710bec1da3a33e4eb0ac2
Eric Wieser <wieser.eric@gmail.com>
2025-07-01 09:56:11 +0000

    feat: more lemmas on lattices within intervals

--- Mathlib/Order/LatticeIntervals.lean
+++ Mathlib/Order/LatticeIntervals.lean
@@ -35,9 +35,20 @@ namespace Ico
 instance semilatticeInf [SemilatticeInf α] {a b : α} : SemilatticeInf (Ico a b) :=
   Subtype.semilatticeInf fun _ _ hx hy => ⟨le_inf hx.1 hy.1, lt_of_le_of_lt inf_le_left hx.2⟩
 
+@[simp, norm_cast]
+protected lemma coe_inf [SemilatticeInf α] {a b : α} {x y : Ico a b} :
+    (↑(x ⊓ y) : α) = (x : α) ⊓ (y : α) :=
+  rfl
 /-- `Ico a b` has a bottom element whenever `a < b`. -/
-protected abbrev orderBot [PartialOrder α] {a b : α} (h : a < b) : OrderBot (Ico a b) :=
-  (isLeast_Ico h).orderBot
+instance orderBot [PartialOrder α] {a b : α} [Fact (a < b)] : OrderBot (Ico a b) :=
+  (isLeast_Ico Fact.out).orderBot
+
+@[simp, norm_cast]
+protected lemma coe_bot [PartialOrder α] {a b : α} [Fact (a < b)] : ↑(⊥ : Ico a b) = a := rfl
+
+protected lemma disjoint_iff [SemilatticeInf α] {a b : α} [Fact (a < b)] {x y : Ico a b} :
+    Disjoint x y ↔ (x : α) ⊓ (y : α) = a := by
+  simp [_root_.disjoint_iff, Subtype.ext_iff]
 
 end Ico
 
@@ -46,6 +57,11 @@ namespace Iio
 instance semilatticeInf [SemilatticeInf α] {a : α} : SemilatticeInf (Iio a) :=
   Subtype.semilatticeInf fun _ _ hx _ => lt_of_le_of_lt inf_le_left hx
 
+@[simp, norm_cast]
+protected lemma coe_inf [SemilatticeInf α] {a : α} {x y : Iio a} :
+    (↑(x ⊓ y) : α) = (x : α) ⊓ (y : α) :=
+  rfl
+
 end Iio
 
 namespace Ioc
@@ -53,9 +69,21 @@ namespace Ioc
 instance semilatticeSup [SemilatticeSup α] {a b : α} : SemilatticeSup (Ioc a b) :=
   Subtype.semilatticeSup fun _ _ hx hy => ⟨lt_of_lt_of_le hx.1 le_sup_left, sup_le hx.2 hy.2⟩
 
+@[simp, norm_cast]
+protected lemma coe_sup [SemilatticeSup α] {a b : α} {x y : Ioc a b} :
+    (↑(x ⊔ y) : α) = (x : α) ⊔ (y : α) :=
+  rfl
+
 /-- `Ioc a b` has a top element whenever `a < b`. -/
-protected abbrev orderTop [PartialOrder α] {a b : α} (h : a < b) : OrderTop (Ioc a b) :=
-  (isGreatest_Ioc h).orderTop
+instance orderTop [PartialOrder α] {a b : α} [Fact (a < b)] : OrderTop (Ioc a b) :=
+  (isGreatest_Ioc Fact.out).orderTop
+
+@[simp, norm_cast]
+protected lemma coe_top [PartialOrder α] {a b : α} [Fact (a < b)] : ↑(⊤ : Ioc a b) = b := rfl
+
+protected lemma codisjoint_iff [SemilatticeSup α] {a b : α} [Fact (a < b)] {x y : Ioc a b} :
+    Codisjoint x y ↔ (x : α) ⊔ (y : α) = b := by
+  simp [_root_.codisjoint_iff, Subtype.ext_iff]
 
 end Ioc
 
@@ -63,6 +91,11 @@ namespace Ioi
 
 instance semilatticeSup [SemilatticeSup α] {a : α} : SemilatticeSup (Ioi a) :=
   Subtype.semilatticeSup fun _ _ hx _ => lt_of_lt_of_le hx le_sup_left
+
+@[simp, norm_cast]
+protected lemma coe_sup [SemilatticeSup α] {a : α} {x y : Ioi a} :
+    (↑(x ⊔ y) : α) = (x : α) ⊔ (y : α) :=
+  rfl
 
 end Ioi
 
@@ -128,13 +161,8 @@ protected lemma isCompl_iff [Lattice α] [OrderBot α] {x y : Iic a} :
 
 protected lemma complementedLattice_iff [Lattice α] [OrderBot α] :
     ComplementedLattice (Iic a) ↔ ∀ b, b ≤ a → ∃ c ≤ a, b ⊓ c = ⊥ ∧ b ⊔ c = a := by
-  refine ⟨fun h b hb ↦ ?_, fun h ↦ ⟨fun ⟨x, hx⟩ ↦ ?_⟩⟩
-  · obtain ⟨⟨c, hc₁⟩, hc⟩ := exists_isCompl (⟨b, hb⟩ : Iic a)
-    obtain ⟨hc₂, hc₃⟩ := Set.Iic.isCompl_iff.mp hc
-    exact ⟨c, hc₁, disjoint_iff.mp hc₂, hc₃⟩
-  · simp_rw [Set.Iic.isCompl_iff]
-    obtain ⟨c, hc₁, hc₂, hc₃⟩ := h x hx
-    exact ⟨⟨c, hc₁⟩, disjoint_iff.mpr hc₂, hc₃⟩
+  simp_rw [complementedLattice_iff, Iic.isCompl_iff, Subtype.forall, Subtype.exists, disjoint_iff,
+    exists_prop, Set.mem_Iic]
 
 end Iic
 
@@ -143,8 +171,18 @@ namespace Ici
 instance semilatticeInf [SemilatticeInf α] {a : α} : SemilatticeInf (Ici a) :=
   Subtype.semilatticeInf fun _ _ hx hy => le_inf hx hy
 
+@[simp, norm_cast]
+protected lemma coe_inf [SemilatticeInf α] {a : α} {x y : Ici a} :
+    (↑(x ⊓ y) : α) = (x : α) ⊓ (y : α) :=
+  rfl
+
 instance semilatticeSup [SemilatticeSup α] {a : α} : SemilatticeSup (Ici a) :=
   Subtype.semilatticeSup fun _ _ hx _ => le_trans hx le_sup_left
+
+@[simp, norm_cast]
+protected lemma coe_sup [SemilatticeSup α] {a : α} {x y : Ici a} :
+    (↑(x ⊔ y) : α) = (x : α) ⊔ (y : α) :=
+  rfl
 
 instance lattice [Lattice α] {a : α} : Lattice (Ici a) :=
   { Ici.semilatticeInf, Ici.semilatticeSup with }
@@ -173,6 +211,19 @@ theorem coe_top [Preorder α] [OrderTop α] {a : α} : ↑(⊤ : Ici a) = (⊤ :
 instance boundedOrder [Preorder α] [OrderTop α] {a : α} : BoundedOrder (Ici a) :=
   { Ici.orderTop, Ici.orderBot with }
 
+
+protected lemma disjoint_iff [SemilatticeInf α] {a : α} {x y : Ici a} :
+    Disjoint x y ↔ ↑x ⊓ ↑y = a := by
+  simp [_root_.disjoint_iff, Subtype.ext_iff]
+
+protected lemma codisjoint_iff [SemilatticeSup α] [OrderTop α] {a : α} {x y : Ici a} :
+    Codisjoint x y ↔ Codisjoint (x : α) (y : α) := by
+  simp [_root_.codisjoint_iff, Subtype.ext_iff]
+
+protected lemma isCompl_iff [Lattice α] [OrderTop α] {a : α} {x y : Ici a} :
+    IsCompl x y ↔ (x : α) ⊓ (y : α) = a ∧ Codisjoint (x : α) (y : α) := by
+  rw [_root_.isCompl_iff, Ici.disjoint_iff, Ici.codisjoint_iff]
+
 end Ici
 
 namespace Icc
@@ -182,30 +233,50 @@ variable {a b : α}
 instance semilatticeInf [SemilatticeInf α] : SemilatticeInf (Icc a b) :=
   Subtype.semilatticeInf fun _ _ hx hy => ⟨le_inf hx.1 hy.1, le_trans inf_le_left hx.2⟩
 
+@[simp, norm_cast]
+protected lemma coe_inf [SemilatticeInf α] {x y : Icc a b} :
+    (↑(x ⊓ y) : α) = (x : α) ⊓ (y : α) :=
+  rfl
+
 instance semilatticeSup [SemilatticeSup α] : SemilatticeSup (Icc a b) :=
   Subtype.semilatticeSup fun _ _ hx hy => ⟨le_trans hx.1 le_sup_left, sup_le hx.2 hy.2⟩
+
+@[simp, norm_cast]
+protected lemma coe_sup [SemilatticeSup α] {x y : Icc a b} :
+    (↑(x ⊔ y) : α) = (x : α) ⊔ (y : α) :=
+  rfl
 
 instance lattice [Lattice α] : Lattice (Icc a b) :=
   { Icc.semilatticeInf, Icc.semilatticeSup with }
 
-variable [Preorder α] [Fact (a ≤ b)]
-
 /-- `Icc a b` has a bottom element whenever `a ≤ b`. -/
-instance : OrderBot (Icc a b) :=
+instance [Preorder α] [Fact (a ≤ b)] : OrderBot (Icc a b) :=
   (isLeast_Icc Fact.out).orderBot
 
 @[simp, norm_cast]
-theorem coe_bot : ↑(⊥ : Icc a b) = a := rfl
+theorem coe_bot [Preorder α] [Fact (a ≤ b)] : ↑(⊥ : Icc a b) = a := rfl
 
 /-- `Icc a b` has a top element whenever `a ≤ b`. -/
-instance : OrderTop (Icc a b) :=
+instance [Preorder α] [Fact (a ≤ b)] : OrderTop (Icc a b) :=
   (isGreatest_Icc Fact.out).orderTop
 
 @[simp, norm_cast]
-theorem coe_top : ↑(⊤ : Icc a b) = b := rfl
+theorem coe_top [Preorder α] [Fact (a ≤ b)] : ↑(⊤ : Icc a b) = b := rfl
 
 /-- `Icc a b` is a `BoundedOrder` whenever `a ≤ b`. -/
-instance : BoundedOrder (Icc a b) where
+instance [Preorder α] [Fact (a ≤ b)] : BoundedOrder (Icc a b) where
+
+protected lemma disjoint_iff [SemilatticeInf α] [Fact (a ≤ b)] {x y : Icc a b} :
+    Disjoint x y ↔ ↑x ⊓ ↑y = a := by
+  simp [_root_.disjoint_iff, Subtype.ext_iff]
+
+protected lemma codisjoint_iff [SemilatticeSup α] [Fact (a ≤ b)] {x y : Icc a b} :
+    Codisjoint x y ↔ ↑x ⊔ (y : α) = b := by
+  simp [_root_.codisjoint_iff, Subtype.ext_iff]
+
+protected lemma isCompl_iff [Lattice α] [Fact (a ≤ b)] {x y : Icc a b} :
+    IsCompl x y ↔ (x : α) ⊓ (y : α) = a ∧ (x : α) ⊔ (y : α) = b := by
+  rw [_root_.isCompl_iff, Icc.disjoint_iff, Icc.codisjoint_iff]
 
 end Icc
 
