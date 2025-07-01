1c2eea8f1d756dec364d7dd18fad2096c3578ce6
Jakob von Raumer <jakob@von-raumer.de>
2025-07-01 18:01:55 +0200

    address @JoelRiou's suggestions

--- Mathlib/AlgebraicTopology/SimplexCategory/Truncated.lean
+++ Mathlib/AlgebraicTopology/SimplexCategory/Truncated.lean
@@ -41,26 +41,10 @@ theorem initial_inclusion {n : ℕ} (hn : 0 < n) : (inclusion n).Initial := by
 
 /-- For `0 < n ≤ m`, the inclusion functor from the `n`-truncated simplex category to the
 `m`-truncated simplex category is initial. -/
-theorem initial_incl {n m : ℕ} (hm : 0 < n) (hn : n ≤ m) : (incl n m).Initial := by
-  constructor
-  rintro ⟨Δ, hΔ⟩
-  have : Nonempty (CostructuredArrow (incl n m) ⟨Δ, hΔ⟩) := ⟨⟨⦋0⦌ₙ, ⟨⟨⟩⟩, ⦋0⦌.const _ 0 ⟩⟩
-  apply zigzag_isConnected
-  rintro ⟨⟨Δ₁, hΔ₁⟩, ⟨⟨⟩⟩, f⟩ ⟨⟨Δ₂, hΔ₂⟩, ⟨⟨⟩⟩, f'⟩
-  change Δ₁ ⟶ Δ at f
-  change Δ₂ ⟶ Δ at f'
-  apply Zigzag.trans (j₂ := ⟨⦋0⦌ₙ, ⟨⟨⟩⟩, ⦋0⦌.const _ (f 0)⟩)
-    (.of_inv <| CostructuredArrow.homMk <| Hom.tr <| ⦋0⦌.const _ 0)
-  by_cases hff' : f 0 ≤ f' 0
-  · trans ⟨⦋1⦌ₙ, ⟨⟨⟩⟩, mkOfLe (n := Δ.len) (f 0) (f' 0) hff'⟩
-    · apply Zigzag.of_hom <| CostructuredArrow.homMk <| Hom.tr <| ⦋0⦌.const _ 0
-    · trans ⟨⦋0⦌ₙ, ⟨⟨⟩⟩, ⦋0⦌.const _ (f' 0)⟩
-      · apply Zigzag.of_inv <| CostructuredArrow.homMk <| Hom.tr <| ⦋0⦌.const _ 1
-      · apply Zigzag.of_hom <| CostructuredArrow.homMk <| Hom.tr <| ⦋0⦌.const _ 0
-  · trans ⟨⦋1⦌ₙ, ⟨⟨⟩⟩, mkOfLe (n := Δ.len) (f' 0) (f 0) (le_of_not_ge hff')⟩
-    · apply Zigzag.of_hom <| CostructuredArrow.homMk <| Hom.tr <| ⦋0⦌.const _ 1
-    · trans ⟨⦋0⦌ₙ, ⟨⟨⟩⟩, ⦋0⦌.const _ (f' 0)⟩
-      · apply Zigzag.of_inv <| CostructuredArrow.homMk <| Hom.tr <| ⦋0⦌.const _ 0
-      · apply Zigzag.of_hom <| CostructuredArrow.homMk <| Hom.tr <| ⦋0⦌.const _ 0
+theorem initial_incl {n m : ℕ} (hn : 0 < n) (hm : n ≤ m) : (incl n m).Initial := by
+  have := initial_inclusion hn
+  have : (incl n m hm ⋙ inclusion m).Initial :=
+    Functor.initial_of_natIso (inclCompInclusion _).symm
+  apply Functor.initial_of_comp_full_faithful _ (inclusion m)
 
 end SimplexCategory.Truncated
