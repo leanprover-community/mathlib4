60c4e194ce3d6f80aa7801c2b10edbc8ff296384
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2025-07-01 10:35:14 +0200

    imports

--- Mathlib/Algebra/Ring/Hom/Basic.lean
+++ Mathlib/Algebra/Ring/Hom/Basic.lean
@@ -4,6 +4,8 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Amelia Livingston, Jireh Loreaux
 -/
 
-import Mathlib.Algebra.Ring.Hom.Defs
+import Mathlib.Algebra.Ring.Hom.InjSurj
+import Mathlib.Deprecated.RingHom
+import Mathlib.Tactic.Linter.DeprecatedModule
 
 deprecated_module (since := "2025-06-09")
