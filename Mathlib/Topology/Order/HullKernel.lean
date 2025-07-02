1e0bf75995794bfa45fb9a42ad04efc839cfdc6d
Christopher Hoskin <mans0954@users.noreply.github.com>
2025-07-02 08:48:14 +0100

    Make gc_closureOperator a simp lemma

--- Mathlib/Topology/Order/HullKernel.lean
+++ Mathlib/Topology/Order/HullKernel.lean
@@ -180,6 +180,7 @@ theorem gc : GaloisConnection (α := Set T) (β := αᵒᵈ)
       exists_eq_right, ← ofDual_le_ofDual, forall_exists_index, OrderDual.forall, ofDual_toDual]
   exact ⟨fun h b hbS => h _ (Subtype.coe_prop b) hbS, fun h b _ hbS => h hbS⟩
 
+@[simp]
 lemma gc_closureOperator (S : Set T) : gc.closureOperator S = hull T (kernel S) := by
   simp only [toDual_sInf, GaloisConnection.closureOperator_apply, ofDual_sSup]
   rw [← preimage_comp, ← OrderDual.toDual_symm_eq, Equiv.symm_comp_self, preimage_id_eq, id_eq]
