4c1fedd36978e1f82f5d766e9c69eeb7bc544812
Robin Carlier <57142648+robin-carlier@users.noreply.github.com>
2025-07-03 11:43:28 +0000

    feat(CategoryTheory/Monoidal/Action): preservation of left/right actions by functors (#25976)
    We introduce typeclasses `F.LaxLeftLinear C`, `F.OplaxLeftLinear C` and `F.LeftLinear C` on functors `F :  D ⥤ D'` where `D` and `D` have a monoidal left action of `C`. These typeclasses bundle "compatibility" of the actions, and are analogue to `LaxMonoidal` and `OplaxMonoidal` structures on functors.
    
    More precisely, `F.LaxLeftLinear C` bundles a"lineator" as a morphism `μₗ : c ⊙ₗ F.obj d ⟶ F.obj (c ⊙ₗ d)`, subject to naturality, associativity and unitality conditions while `F.OplaxLeftLinear C` bundles the "lineator" as a morphism `δₗ : F.obj (c ⊙ₗ d) ⟶ c ⊙ₗ F.obj d ` subject to similar conditions. `F.LeftLinear` bundles both a `LaxLeftLinear` and an `OplaxLeftLinear` structure, as well as the fact that `μₗ` is left and right inverse to `δₗ`.
    
    The analogue typeclasses for right actions are also introduced.

--- Mathlib.lean
+++ Mathlib.lean
@@ -2375,6 +2375,7 @@ import Mathlib.CategoryTheory.Monad.Monadicity
 import Mathlib.CategoryTheory.Monad.Products
 import Mathlib.CategoryTheory.Monad.Types
 import Mathlib.CategoryTheory.Monoidal.Action.Basic
+import Mathlib.CategoryTheory.Monoidal.Action.LinearFunctor
 import Mathlib.CategoryTheory.Monoidal.Bimod
 import Mathlib.CategoryTheory.Monoidal.Bimon_
 import Mathlib.CategoryTheory.Monoidal.Braided.Basic
