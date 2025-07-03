b7b2ff78362e56a6bb2dbf9a044ff21ef1062b04
yu-yama <yu-yama@users.noreply.github.com>
2025-07-03 16:24:56 +0100

    fix: rewrite `conjClassesEquivH1` following re-definition of `H1`
    chore: use existing `groupCohomology.cocyclesOfIsMulCocycle₁` def
    lint: suppress `linter.directoryDependency` error
    chore: add simps lemmas
    chore: add ext lemmas to `GroupExtension.{Section,Splitting}`

--- Mathlib/GroupTheory/GroupExtension/Abelian.lean
+++ Mathlib/GroupTheory/GroupExtension/Abelian.lean
@@ -5,7 +5,10 @@ Authors: Yudai Yamazaki
 -/
 
 import Mathlib.GroupTheory.GroupExtension.Basic
-import Mathlib.RepresentationTheory.GroupCohomology.LowDegree
+import Mathlib.RepresentationTheory.Homological.GroupCohomology.LowDegree
+
+set_option linter.style.header false in
+set_option linter.directoryDependency false in
 
 /-!
 # Lemmas about extensions by Abelian groups
@@ -38,77 +41,96 @@ noncomputable def toRep : Rep ℤ G :=
 theorem toRep_ρ_apply_apply (g : G) (n : Additive N) :
     ((toRep φ).ρ g) n = Additive.ofMul (φ g (Additive.toMul n)) := rfl
 
-/-- Returns the 1-cocycle corresponding to a splitting. -/
-def splittingToOneCocycle (s : (toGroupExtension φ).Splitting) :
-    groupCohomology.oneCocycles (toRep φ) where
-  val g := Additive.ofMul (α := N) (s g).left
-  property := by
-    rw [groupCohomology.mem_oneCocycles_iff]
-    intro g₁ g₂
-    rw [toRep_ρ_apply_apply, toMul_ofMul, map_mul, mul_left, mul_comm, ← ofMul_mul, right_splitting]
+end SemidirectProduct
 
-/-- Returns the splitting corresponding to a 1-cocycle. -/
-def splittingOfOneCocycle (f : groupCohomology.oneCocycles (toRep φ)) :
-    (toGroupExtension φ).Splitting where
+namespace GroupExtension
+
+section SemidirectProduct
+
+variable {N G : Type} [CommGroup N] [Group G] {φ : G →* MulAut N}
+
+namespace Splitting
+
+/-- The 1-cocycle corresponding to a splitting. -/
+@[simps!]
+def toOneCocycle (s : (SemidirectProduct.toGroupExtension φ).Splitting) :
+    groupCohomology.cocycles₁ (SemidirectProduct.toRep φ) :=
+  @groupCohomology.cocyclesOfIsMulCocycle₁ G N _ _ (MulDistribMulAction.compHom N φ)
+    (fun g ↦ (s g).left)
+    fun g₁ g₂ ↦ by
+      simp only [map_mul, SemidirectProduct.mul_left, SemidirectProduct.right_splitting]
+      rw [mul_comm, MulAction.compHom_smul_def, MulAut.smul_def]
+
+theorem toOneCocycle_apply (s : (SemidirectProduct.toGroupExtension φ).Splitting) (g : G) :
+    s.toOneCocycle g = Additive.ofMul (s g).left := toOneCocycle_coe s g
+
+/-- The splitting corresponding to a 1-cocycle. -/
+@[simps!]
+noncomputable def ofOneCocycle (f : groupCohomology.cocycles₁ (SemidirectProduct.toRep φ)) :
+    (SemidirectProduct.toGroupExtension φ).Splitting where
   toFun g := ⟨Additive.toMul (f g), g⟩
   map_one' := by
     ext
-    · simp only [one_left, groupCohomology.oneCocycles_map_one, toMul_zero]
-    · simp only [one_right]
+    · simp only [SemidirectProduct.one_left, groupCohomology.cocycles₁_map_one, toMul_zero]
+    · simp only [SemidirectProduct.one_right]
   map_mul' g₁ g₂ := by
     ext
-    · simp only [mul_left]
-      rw [(groupCohomology.mem_oneCocycles_iff f).mp f.property g₁ g₂, toMul_add, mul_comm,
-        toRep_ρ_apply_apply, toMul_ofMul]
-    · simp only [mul_right]
-  rightInverse_rightHom g := by simp only [toGroupExtension_rightHom, rightHom_eq_right]
+    · simp only [SemidirectProduct.mul_left]
+      rw [(groupCohomology.mem_cocycles₁_iff f).mp f.property g₁ g₂, toMul_add, mul_comm,
+        SemidirectProduct.toRep_ρ_apply_apply, toMul_ofMul]
+    · simp only [SemidirectProduct.mul_right]
+  rightInverse_rightHom g :=  by
+    simp only [SemidirectProduct.toGroupExtension_rightHom, SemidirectProduct.rightHom_eq_right]
 
 /-- The bijection between the splittings of the group extension associated to a semidirect product
   and the 1-cocycles -/
-def splittingEquivOneCocycles :
-    (toGroupExtension φ).Splitting ≃ groupCohomology.oneCocycles (toRep φ) where
-  toFun := splittingToOneCocycle φ
-  invFun := splittingOfOneCocycle φ
+@[simps!]
+noncomputable def equivOneCocycles : (SemidirectProduct.toGroupExtension φ).Splitting ≃
+    groupCohomology.cocycles₁ (SemidirectProduct.toRep φ) where
+  toFun := toOneCocycle
+  invFun := ofOneCocycle
   left_inv s := by
-    unfold splittingToOneCocycle splittingOfOneCocycle
-    congr
     ext g
-    <;> congr
-    <;> exact (s.rightHom_splitting g).symm
+    · rw [ofOneCocycle_apply_left, toOneCocycle_apply, toMul_ofMul]
+    · rw [ofOneCocycle_apply_right, ← SemidirectProduct.rightHom_eq_right,
+      ← SemidirectProduct.toGroupExtension_rightHom, rightHom_splitting]
   right_inv f := by
-    unfold splittingToOneCocycle splittingOfOneCocycle
     ext g
-    simp only [mk_eq_inl_mul_inr, GroupExtension.Splitting.coe_mk, MonoidHom.coe_mk, OneHom.coe_mk,
-      mul_left, left_inl, right_inl, map_one, left_inr, MulAut.one_apply, mul_one, ofMul_toMul,
-      groupCohomology.oneCocycles.coe_mk]
+    simp only [toOneCocycle_apply, ofOneCocycle_apply_left, ofMul_toMul]
 
-/-- Two splittings are `N`-conjugates iff the difference of the corresponding 1-cocycles is a
-  1-coboundary. -/
-theorem isConj_iff_sub_mem_oneCoboundaries (s₁ s₂ : (toGroupExtension φ).Splitting) :
-    (toGroupExtension φ).IsConj s₁ s₂ ↔
-    splittingToOneCocycle φ s₁ - splittingToOneCocycle φ s₂ ∈
-    groupCohomology.oneCoboundaries (toRep φ) := by
-  rw [sub_mem_comm_iff, groupCohomology.mem_oneCoboundaries_iff]
-  apply Additive.ofMul.exists_congr
-  intro n
-  rw [funext_iff]
-  apply forall_congr'
-  intro g
-  simp only [toGroupExtension_inl, SemidirectProduct.ext_iff, mul_left, mul_right, inv_left,
-    inv_right, right_splitting, left_inl, right_inl, inv_one, map_one, map_inv, MulAut.one_apply,
-    one_mul, mul_one, and_true]
-  rw [eq_mul_inv_iff_mul_eq, mul_comm, ← eq_mul_inv_iff_mul_eq, mul_assoc, mul_comm,
-    ← mul_inv_eq_iff_eq_mul, ← groupCohomology.oneCocycles.val_eq_coe, AddSubgroupClass.coe_sub,
-    Pi.sub_apply]
-  simp only [splittingToOneCocycle, toRep_ρ_apply_apply, toMul_ofMul, ← div_eq_mul_inv]
-  rfl
+end Splitting
 
 /-- The bijection between the `N`-conjugacy classes of splittings and the first cohomology group -/
-def conjClassesEquivH1 : (toGroupExtension φ).ConjClasses ≃ groupCohomology.H1 (toRep φ) :=
-  Quotient.congr (splittingEquivOneCocycles φ) (by
+noncomputable def conjClassesEquivH1 : (SemidirectProduct.toGroupExtension φ).ConjClasses ≃
+    (groupCohomology.shortComplexH1 (SemidirectProduct.toRep φ)).moduleCatLeftHomologyData.H :=
+  Quotient.congr Splitting.equivOneCocycles (by
     intro s₁ s₂
-    rw [Submodule.quotientRel_def]
-    exact isConj_iff_sub_mem_oneCoboundaries φ s₁ s₂
+    rw [Submodule.quotientRel_def, sub_mem_comm_iff]
+    simp only [groupCohomology.shortComplexH1, groupCohomology.d₀₁, groupCohomology.d₁₂,
+      ModuleCat.hom_ofHom, CategoryTheory.ShortComplex.moduleCatToCycles,
+      LinearMap.range_codRestrict, Submodule.mem_comap, Submodule.subtype_apply,
+      groupCohomology.cocycles₁.val_eq_coe, LinearMap.mem_range, LinearMap.coe_mk, AddHom.coe_mk,
+      SemidirectProduct.toRep_ρ_apply_apply]
+    change (SemidirectProduct.toGroupExtension φ).IsConj s₁ s₂ ↔
+      ∃ y, (fun g ↦ Additive.ofMul ((φ g) (Additive.toMul y)) - y) =
+        Splitting.equivOneCocycles s₂ - Splitting.equivOneCocycles s₁
+    apply Additive.ofMul.exists_congr
+    intro n
+    simp only [funext_iff]
+    apply forall_congr'
+    intro g
+    simp only [SemidirectProduct.toGroupExtension_inl, SemidirectProduct.ext_iff,
+      SemidirectProduct.mul_left, SemidirectProduct.left_inl, SemidirectProduct.right_inl, map_one,
+      MulAut.one_apply, SemidirectProduct.mul_right, one_mul, SemidirectProduct.inv_left, inv_one,
+      map_inv, SemidirectProduct.inv_right, mul_one, toMul_ofMul,
+      ← groupCohomology.cocycles₁.val_eq_coe, AddSubgroupClass.coe_sub, Pi.sub_apply,
+      Splitting.equivOneCocycles_apply_coe, SemidirectProduct.right_splitting, and_true]
+    rw [eq_mul_inv_iff_mul_eq, eq_sub_iff_add_eq', ← add_sub_assoc, sub_eq_iff_eq_add',
+      ← Additive.ofMul.apply_eq_iff_eq]
+    simp only [ofMul_mul]
+    rfl
   )
 
 end SemidirectProduct
+
+end GroupExtension
--- Mathlib/GroupTheory/GroupExtension/Defs.lean
+++ Mathlib/GroupTheory/GroupExtension/Defs.lean
@@ -249,7 +249,10 @@ variable {S}
 @[to_additive (attr := simp)]
 theorem coe_mk (σ : G → E) (hσ : Function.RightInverse σ S.rightHom) : (mk σ hσ : G → E) = σ := rfl
 
-variable (σ : S.Section)
+variable (σ σ' : S.Section)
+
+@[to_additive (attr := ext)]
+theorem ext (h : ∀ g : G, σ g = σ' g) : σ = σ' := DFunLike.ext σ σ' h
 
 @[to_additive (attr := simp)]
 theorem rightHom_section (g : G) : S.rightHom (σ g) = g := σ.rightInverse_rightHom g
@@ -290,7 +293,10 @@ theorem coe_mk (s : G →* E) (hs : Function.RightInverse s S.rightHom) : (mk s 
 theorem coe_monoidHom_mk (s : G →* E) (hs : Function.RightInverse s S.rightHom) :
     (mk s hs : G →* E) = s := rfl
 
-variable (s : S.Splitting)
+variable (s s' : S.Splitting)
+
+@[to_additive (attr := ext)]
+theorem ext (h : ∀ g : G, s g = s' g) : s = s' := DFunLike.ext s s' h
 
 @[to_additive (attr := simp)]
 theorem rightHom_splitting (g : G) : S.rightHom (s g) = g := s.rightInverse_rightHom g
