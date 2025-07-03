a8d449435736f8b0eb1eaedcd744734770aecc3f
adamtopaz <github@adamtopaz.com>
2025-07-03 00:08:06 -0600

    MVP

--- Mathlib/Tactic/Ring/VariableName.lean
+++ Mathlib/Tactic/Ring/VariableName.lean
@@ -1,13 +1,48 @@
+/-
+Copyright (c) 2025 Adam Topaz. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Adam Topaz
+-/
 import Mathlib.Algebra.MvPolynomial.Basic
+
+/-!
+The command `name_variables` names variables in
+`MvPolynomial (Fin n) R` for the appropriate value of `n`.
+The notation introduced by this command is local.
+
+Usage:
+
+```lean
+variable (R : Type) [CommRing R]
+
+name_variables X, Y, Z over R
+
+#check Y -- Y : MvPolynomial (Fin 3) R
+```
+-/
 
 open Lean Elab Command
 
 namespace Mathlib.Tactic
 
-syntax (name := nameVariablesOver)
-  "name_variables " ident,+ " over " term : command
+/--
+The command `name_variables` names variables in
+`MvPolynomial (Fin n) R` for the appropriate value of `n`.
+The notation introduced by this command is local.
 
-@[command_elab nameVariablesOver]
+Usage:
+
+```lean
+variable (R : Type) [CommRing R]
+
+name_variables X, Y, Z over R
+
+#check Y -- Y : MvPolynomial (Fin 3) R
+```
+-/
+syntax (name := nameVariablesOver) "name_variables " ident,+ " over " term : command
+
+@[command_elab nameVariablesOver, inherit_doc nameVariablesOver]
 def elabNameVariablesOver : CommandElab
 | `(command|name_variables $vars:ident,* over $R:term) => do
   let vars := vars.getElems
