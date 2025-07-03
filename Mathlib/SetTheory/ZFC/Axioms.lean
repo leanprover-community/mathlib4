a19283ac278d44693608e29165e75e0bb1f22fdc
Kenny Lau <kc_kennylau@yahoo.com.hk>
2025-07-03 01:54:56 +0100

    fix

--- Mathlib/SetTheory/ZFC/Axioms.lean
+++ Mathlib/SetTheory/ZFC/Axioms.lean
@@ -4,8 +4,8 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kenny Lau
 -/
 
-set_option linter.directoryDependency false
 import Mathlib.ModelTheory.Syntax
+set_option linter.directoryDependency false
 
 /-!
 # The Axioms of ZFC
