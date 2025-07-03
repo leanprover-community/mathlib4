8a1d2a6920e523cb5256aca13484ab0e30310f49
FernandoChu <17756312+FernandoChu@users.noreply.github.com>
2025-07-03 14:14:51 +0200

    remove import

--- Mathlib/CategoryTheory/Functor/Trifunctor.lean
+++ Mathlib/CategoryTheory/Functor/Trifunctor.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joël Riou
 -/
 import Mathlib.CategoryTheory.Functor.Category
-import Mathlib.CategoryTheory.Functor.Bifunctor
 
 /-!
 # Trifunctors obtained by composition of bifunctors
