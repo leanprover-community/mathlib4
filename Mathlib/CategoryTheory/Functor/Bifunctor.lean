fcff7d9ffa7e80fc5c673184599da55e86537480
FernandoChu <17756312+FernandoChu@users.noreply.github.com>
2025-07-03 11:51:50 +0200

    Fix authorship

--- Mathlib/CategoryTheory/Functor/Bifunctor.lean
+++ Mathlib/CategoryTheory/Functor/Bifunctor.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2025 Fernando Chu. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Fernando Chu
+Authors: Andrea Laretto, Fernando Chu
 -/
 import Mathlib.CategoryTheory.Functor.Category
 import Mathlib.CategoryTheory.Opposites
