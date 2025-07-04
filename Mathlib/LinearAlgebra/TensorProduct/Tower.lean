a74a6baea3fdc2617a9bdfdc679d1f9b84f09f9a
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2025-07-04 00:20:45 -0400

    initial commit

--- Mathlib/LinearAlgebra/TensorProduct/Tower.lean
+++ Mathlib/LinearAlgebra/TensorProduct/Tower.lean
@@ -662,6 +662,12 @@ lemma baseChange_pow (f : Module.End R M) (n : ℕ) :
     (f ^ n).baseChange A = f.baseChange A ^ n :=
   map_pow (Module.End.baseChangeHom _ _ _) f n
 
+variable {R A M N} in
+theorem rTensor_comp_baseChange_comm_apply (φ : A →ₐ[R] B) (t : A ⊗[R] M) (f : M →ₗ[R] N) :
+    (φ.toLinearMap.rTensor N) (f.baseChange A t)  =
+      (f.baseChange B) (φ.toLinearMap.rTensor M t) := by
+  simp [LinearMap.baseChange_eq_ltensor, ← LinearMap.comp_apply]
+
 end Semiring
 
 section Ring
@@ -728,3 +734,4 @@ lemma baseChange_span (s : Set M) :
   rw [baseChange_eq_span, map_span, span_span_of_tower]
 
 end Submodule
+
