169afb19489460e3d8e81d27450a435cae244f08
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2025-07-04 00:23:43 -0400

    update Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -5563,6 +5563,7 @@ import Mathlib.RingTheory.Support
 import Mathlib.RingTheory.SurjectiveOnStalks
 import Mathlib.RingTheory.TensorProduct.Basic
 import Mathlib.RingTheory.TensorProduct.Finite
+import Mathlib.RingTheory.TensorProduct.FG
 import Mathlib.RingTheory.TensorProduct.Free
 import Mathlib.RingTheory.TensorProduct.IsBaseChangePi
 import Mathlib.RingTheory.TensorProduct.MvPolynomial
