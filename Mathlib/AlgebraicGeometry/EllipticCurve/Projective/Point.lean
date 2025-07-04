7f311bbe7ca9dc8ce0f042f8791b91fd4531f446
Matthew Ballard <matt@mrb.email>
2025-07-04 09:15:19 -0400

    delete whitespace

--- Mathlib/AlgebraicGeometry/EllipticCurve/Projective/Point.lean
+++ Mathlib/AlgebraicGeometry/EllipticCurve/Projective/Point.lean
@@ -6,7 +6,6 @@ Authors: David Kurniadi Angdinata
 import Mathlib.AlgebraicGeometry.EllipticCurve.Affine.Point
 import Mathlib.AlgebraicGeometry.EllipticCurve.Projective.Formula
 
-
 /-!
 # Nonsingular points and the group law in projective coordinates
 
