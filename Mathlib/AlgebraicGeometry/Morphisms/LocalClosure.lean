ff12324350fe54fc6a480d78956e3f837ec0d74d
Christian Merten <christian@merten.dev>
2025-07-04 16:30:10 +0900

    generalise

--- Mathlib/AlgebraicGeometry/Morphisms/LocalClosure.lean
+++ Mathlib/AlgebraicGeometry/Morphisms/LocalClosure.lean
@@ -9,8 +9,8 @@ import Mathlib.AlgebraicGeometry.Morphisms.Basic
 /-!
 # Local closure of morphism properties
 
-We define the source local closure of a property `P` and show it inherits stability properties
-from `P`.
+We define the source local closure of a property `P` wrt. a morphism property `W` and show it
+inherits stability properties from `P`.
 -/
 
 universe u
@@ -19,10 +19,12 @@ open CategoryTheory Limits MorphismProperty
 
 namespace AlgebraicGeometry
 
+variable (W : MorphismProperty Scheme.{u})
+
 /-- The source (Zariski-)local closure of `P` is satisfied if there exists
 an open cover of the source on which `P` is satisfied. -/
 def SourceLocalClosure (P : MorphismProperty Scheme.{u}) : MorphismProperty Scheme.{u} :=
-  fun X _ f ↦ ∃ (𝒰 : Scheme.OpenCover.{u} X), ∀ (i : 𝒰.J), P (𝒰.map i ≫ f)
+  fun X _ f ↦ ∃ (𝒰 : Scheme.Cover.{u} W X), ∀ (i : 𝒰.J), P (𝒰.map i ≫ f)
 
 namespace SourceLocalClosure
 
@@ -30,18 +32,18 @@ variable (P Q : MorphismProperty Scheme.{u}) {X Y : Scheme.{u}}
 
 /-- A choice of open cover on which `P` is satisfied if `f` satisfies the source local closure
 of `P`. -/
-noncomputable def openCover {f : X ⟶ Y} (hf : SourceLocalClosure P f) : Scheme.OpenCover.{u} X :=
+noncomputable def openCover {f : X ⟶ Y} (hf : SourceLocalClosure W P f) : Scheme.Cover.{u} W X :=
   hf.choose
 
-lemma property_openCoverMap_comp (f : X ⟶ Y) (hf : SourceLocalClosure P f) (i : hf.openCover.J) :
+lemma property_openCoverMap_comp (f : X ⟶ Y) (hf : SourceLocalClosure W P f) (i : hf.openCover.J) :
     P (hf.openCover.map i ≫ f) :=
   hf.choose_spec i
 
-lemma le : P ≤ SourceLocalClosure P :=
+lemma le [W.ContainsIdentities] [W.RespectsIso] : P ≤ SourceLocalClosure W P :=
   fun X Y f hf ↦ ⟨X.coverOfIsIso (𝟙 X), by simpa⟩
 
 lemma iff_forall_exists [P.RespectsIso] {f : X ⟶ Y} :
-    SourceLocalClosure P f ↔ ∀ (x : X), ∃ (U : X.Opens), x ∈ U ∧ P (U.ι ≫ f) := by
+    SourceLocalClosure IsOpenImmersion P f ↔ ∀ (x : X), ∃ (U : X.Opens), x ∈ U ∧ P (U.ι ≫ f) := by
   refine ⟨fun ⟨𝒰, hf⟩ x ↦ ?_, fun H ↦ ?_⟩
   · refine ⟨(𝒰.map (𝒰.f x)).opensRange, 𝒰.covers x, ?_⟩
     rw [← Scheme.Hom.isoOpensRange_inv_comp, Category.assoc, P.cancel_left_of_respectsIso]
@@ -50,21 +52,23 @@ lemma iff_forall_exists [P.RespectsIso] {f : X ⟶ Y} :
     exact ⟨.mkOfCovers X (fun x ↦ U x) (fun _ ↦ (U _).ι) (fun x ↦ ⟨x, ⟨x, hx x⟩, rfl⟩)
       fun _ ↦ inferInstance, hf⟩
 
+variable [W.IsStableUnderBaseChange] [Scheme.IsJointlySurjectivePreserving W]
+
 instance [P.RespectsLeft Q] [Q.IsStableUnderBaseChange] :
-    (SourceLocalClosure P).RespectsLeft Q := by
+    (SourceLocalClosure W P).RespectsLeft Q := by
   refine ⟨fun {X Y} Z f hf g ⟨𝒰, hg⟩ ↦ ⟨𝒰.pullbackCover f, fun i ↦ ?_⟩⟩
   simpa [pullback.condition_assoc] using
     RespectsLeft.precomp (Q := Q) _ (Q.pullback_snd _ _ hf) _ (hg i)
 
-instance [P.RespectsRight Q] : (SourceLocalClosure P).RespectsRight Q := by
+instance [P.RespectsRight Q] : (SourceLocalClosure W P).RespectsRight Q := by
   refine ⟨fun {X Y} Z f hf g ⟨𝒰, hg⟩ ↦ ⟨𝒰, fun i ↦ ?_⟩⟩
   rw [← Category.assoc]
   exact RespectsRight.postcomp _ hf _ (hg i)
 
-instance [P.RespectsIso] : (SourceLocalClosure P).RespectsIso where
+instance [P.RespectsIso] : (SourceLocalClosure W P).RespectsIso where
 
 instance [P.RespectsIso] [P.RespectsLeft @IsOpenImmersion] :
-    IsLocalAtSource (SourceLocalClosure P) where
+    IsLocalAtSource (SourceLocalClosure IsOpenImmersion P) where
   iff_of_openCover' {X Y} f 𝒰 := by
     refine ⟨fun ⟨𝒱, h⟩ ↦ fun i ↦ ⟨𝒱.pullbackCover (𝒰.map i), fun j ↦ ?_⟩, fun h ↦ ?_⟩
     · simpa [pullback.condition_assoc] using
@@ -72,29 +76,24 @@ instance [P.RespectsIso] [P.RespectsLeft @IsOpenImmersion] :
     · choose 𝒱 h𝒱 using h
       exact ⟨𝒰.bind 𝒱, fun i ↦ h𝒱 _ _⟩
 
-instance [P.IsStableUnderBaseChange] : (SourceLocalClosure P).IsStableUnderBaseChange := by
+instance [P.IsStableUnderBaseChange] : (SourceLocalClosure W P).IsStableUnderBaseChange := by
   refine .mk' fun X Y S f g _ ⟨𝒰, hg⟩ ↦ ⟨𝒰.pullbackCover (pullback.snd f g), fun i ↦ ?_⟩
   simpa [← pullbackLeftPullbackSndIso_hom_fst, P.cancel_left_of_respectsIso] using
     P.pullback_fst _ _ (hg i)
 
-instance [P.ContainsIdentities] : (SourceLocalClosure P).ContainsIdentities :=
+instance [W.ContainsIdentities] [P.ContainsIdentities] :
+    (SourceLocalClosure W P).ContainsIdentities :=
   ⟨fun X ↦ ⟨X.coverOfIsIso (𝟙 X), fun _ ↦ P.id_mem _⟩⟩
 
-instance [P.IsStableUnderBaseChange] [P.IsMultiplicative] [P.RespectsLeft @IsOpenImmersion] :
-    (SourceLocalClosure P).IsStableUnderComposition := by
-  refine ⟨fun {X Y Z} f g hf hg ↦ ?_⟩
-  wlog h : P f generalizing X Y f g
-  · obtain ⟨𝒰, hf⟩ := hf
-    rw [IsLocalAtSource.iff_of_openCover 𝒰 (P := SourceLocalClosure P)]
-    intro i
-    rw [← Category.assoc]
-    exact this _ _ (le _ _ (hf _)) hg (hf _)
-  obtain ⟨𝒰, hg⟩ := hg
-  refine ⟨𝒰.pullbackCover f, fun i ↦ ?_⟩
-  simpa [pullback.condition_assoc] using P.comp_mem _ _ (P.pullback_snd _ _ h) (hg _)
+instance [W.IsStableUnderComposition] [P.IsStableUnderBaseChange] [P.IsStableUnderComposition] :
+    (SourceLocalClosure W P).IsStableUnderComposition := by
+  refine ⟨fun {X Y Z} f g ⟨𝒰, hf⟩ ⟨𝒱, hg⟩ ↦ ?_⟩
+  refine ⟨𝒰.bind fun i ↦ (𝒱.pullbackCover (𝒰.map i ≫ f)), fun i ↦ ?_⟩
+  simpa [← pullbackRightPullbackFstIso_inv_snd_fst_assoc, pullback.condition_assoc] using
+    P.comp_mem _ _ (P.pullback_snd _ _ (hf _)) (hg i.snd)
 
-instance [P.IsStableUnderBaseChange] [P.IsMultiplicative] [P.RespectsLeft @IsOpenImmersion] :
-    (SourceLocalClosure P).IsMultiplicative where
+instance [W.IsMultiplicative] [P.IsStableUnderBaseChange] [P.IsMultiplicative] :
+    (SourceLocalClosure W P).IsMultiplicative where
 
 end SourceLocalClosure
 
@@ -111,7 +110,7 @@ namespace IsLocalIso
 variable (f : X ⟶ Y)
 
 lemma eq_sourceLocalClosure_isOpenImmersion :
-    @IsLocalIso = SourceLocalClosure @IsOpenImmersion := by
+    @IsLocalIso = SourceLocalClosure IsOpenImmersion IsOpenImmersion := by
   ext
   rw [isLocalIso_iff, SourceLocalClosure.iff_forall_exists]
 
