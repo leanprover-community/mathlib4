c9ecc8ee2ce685eb484b6d274a6423753f831827
Total Noob <klausgy@gmail.com>
2025-07-04 19:24:07 +0200

    chore: lake exe mk_all

--- Mathlib.lean
+++ Mathlib.lean
@@ -2626,6 +2626,7 @@ import Mathlib.CategoryTheory.Sums.Associator
 import Mathlib.CategoryTheory.Sums.Basic
 import Mathlib.CategoryTheory.Sums.Products
 import Mathlib.CategoryTheory.Thin
+import Mathlib.CategoryTheory.Topos.Basic
 import Mathlib.CategoryTheory.Topos.Classifier
 import Mathlib.CategoryTheory.Triangulated.Adjunction
 import Mathlib.CategoryTheory.Triangulated.Basic
