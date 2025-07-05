d6935158d4472678ffae2cac8651343ff0f3900f
Yi-Jing Tseng <mjesjingtw@gmail.com>
2025-07-05 16:42:11 +0800

    feat(MeasureTheory): add SigmaAlgebra definition and related lemma

---
+++ Mathlib/MeasureTheory/MeasurableSpace/SigmaAlgebra.lean
@@ -0,0 +1,32 @@
+/-
+Copyright (c) 2025 Yi-Jing Tseng. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Yi-Jing Tseng
+-/
+import Mathlib.MeasureTheory.MeasurableSpace.Defs
+
+/-!
+Some doc
+-/
+
+universe u
+
+variable {α : Type u}
+
+namespace MeasurableSpace
+
+variable {m : MeasurableSpace α}
+
+/--
+`m.SigmaAlgebra` is *the* σ-algebra of `m`-measurable sets, packaged as
+a plain `Set (Set α)`, so you can write `A ∈ m.SigmaAlgebra`.
+-/
+def SigmaAlgebra : Set (Set α) := m.MeasurableSet'
+
+@[simp]
+lemma mem_SigmaAlgebra {s : Set α} :
+    s ∈ m.SigmaAlgebra ↔ (MeasurableSet s) :=
+  Iff.rfl
+
+
+end MeasurableSpace
