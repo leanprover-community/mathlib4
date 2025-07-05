6ecfee52cf19be23bef4190f1ef14a4e858f1e44
Paul Lezeau <paul.lezeau@gmail.com>
2025-07-05 07:48:06 +0000

    chore(Data/Nat/Factorization/Basic): minimize dependencies (#24330)

--- Mathlib.lean
+++ Mathlib.lean
@@ -3296,6 +3296,7 @@ import Mathlib.Data.Nat.Factorial.SuperFactorial
 import Mathlib.Data.Nat.Factorization.Basic
 import Mathlib.Data.Nat.Factorization.Defs
 import Mathlib.Data.Nat.Factorization.Induction
+import Mathlib.Data.Nat.Factorization.LCM
 import Mathlib.Data.Nat.Factorization.PrimePow
 import Mathlib.Data.Nat.Factorization.Root
 import Mathlib.Data.Nat.Factors
