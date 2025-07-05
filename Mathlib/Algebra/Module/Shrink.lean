62a3345e5d7f622c43114ff2f829ed35960db9da
Yaël Dillies <yael.dillies@gmail.com>
2025-07-05 07:38:08 +0000

    chore(Algebra/Equiv/TransferInstance): split off group results on `Shrink` Move group results about `Shrink` from `Algebra.Equiv.TransferInstance` to a new file `Algebra.Group.Shrink`. Almost all this material was duplicated between `Algebra.Equiv.TransferInstance` and `Algebra.Small.Group`.
    This is the second part of #26732, split off for Eric's convenience.

--- Mathlib/Algebra/Module/Shrink.lean
+++ Mathlib/Algebra/Module/Shrink.lean
@@ -3,11 +3,11 @@ Copyright (c) 2021 Kim Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kim Morrison
 -/
-import Mathlib.Algebra.Group.Shrink
+import Mathlib.Algebra.Equiv.TransferInstance
 import Mathlib.Algebra.Ring.Shrink
 
 /-!
-# Transfer module and algebra structures from `α` to `Shrink α`.
+# Transfer module and algebra structures from `α` to `Shrink α`
 -/
 
 noncomputable section
