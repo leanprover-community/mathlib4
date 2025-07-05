f24e4606dc7b3e959217833a9b61f491d20659bb
Yi-Jing Tseng <mjesjingtw@gmail.com>
2025-07-06 00:59:06 +0800

    refactor(SigmaAlgebra): improve documentation and clarity of σ-algebra helpers

--- Mathlib/MeasureTheory/MeasurableSpace/SigmaAlgebra.lean
+++ Mathlib/MeasureTheory/MeasurableSpace/SigmaAlgebra.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yi-Jing Tseng
 -/
 
-import Mathlib.MeasureTheory.MeasurableSpace.Defs
 import Mathlib.MeasureTheory.MeasurableSpace.MeasurablyGenerated
 
 /-!
