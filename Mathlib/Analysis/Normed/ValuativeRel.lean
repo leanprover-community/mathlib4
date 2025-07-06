cbb67659b4c1c51502302d3e52852459eb2a4ace
Yakov Pechersky <ffxen158@gmail.com>
2025-07-06 18:35:07 -0400

    single space

--- Mathlib/Analysis/Normed/ValuativeRel.lean
+++ Mathlib/Analysis/Normed/ValuativeRel.lean
@@ -40,7 +40,7 @@ def valuation' : Valuation R ℝ≥0 where
 theorem valuation'_apply (x : R) : valuation' x = ‖x‖₊ := rfl
 
 /-- A nonarchimedean normed ring carries a valuative relation induced by the norm.
-This is a scoped instance.  -/
+This is a scoped instance. -/
 def toValuativeRel : ValuativeRel R := .ofValuation valuation'
 
 open Filter Topology
