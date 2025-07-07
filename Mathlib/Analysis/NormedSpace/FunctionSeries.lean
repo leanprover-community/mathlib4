2f47769987d8995bde6117804e7ba5f8fcd59a76
Chris Birkbeck <c.birkbeck@uea.ac.uk>
2025-07-07 14:55:22 +0100

    imports

--- Mathlib/Analysis/NormedSpace/FunctionSeries.lean
+++ Mathlib/Analysis/NormedSpace/FunctionSeries.lean
@@ -3,8 +3,8 @@ Copyright (c) 2022 Sébastien Gouëzel. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Sébastien Gouëzel
 -/
-import Mathlib.Analysis.Calculus.UniformLimitsDeriv
-import Mathlib.Topology.Algebra.InfiniteSum.UniformOn
+import Mathlib.Analysis.Normed.Group.InfiniteSum
+import Mathlib.Topology.Instances.ENNReal.Lemmas
 
 /-!
 # Continuity of series of functions
--- Mathlib/Topology/Algebra/InfiniteSum/tsumUniformOn.lean
+++ Mathlib/Topology/Algebra/InfiniteSum/tsumUniformOn.lean
@@ -3,6 +3,8 @@ Copyright (c) 2022 Sébastien Gouëzel. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Sébastien Gouëzel
 -/
+import Mathlib.Algebra.Lie.OfAssociative
+import Mathlib.Analysis.Calculus.UniformLimitsDeriv
 import Mathlib.Analysis.NormedSpace.FunctionSeries
 import Mathlib.Topology.Algebra.InfiniteSum.UniformOn
 
