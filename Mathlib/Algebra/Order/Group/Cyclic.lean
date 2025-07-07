984faeda41f5df700940d527ff7298ca24b1a011
mariainesdff <mariaines.dff@gmail.com>
2025-07-07 11:10:13 +0200

    feat(Algebra/Order/Group/Cyclic): add genLTOne_unique

--- Mathlib/Algebra/Order/Group/Cyclic.lean
+++ Mathlib/Algebra/Order/Group/Cyclic.lean
@@ -3,6 +3,7 @@ Copyright (c) 2025 María Inés de Frutos-Fernández, Filippo A. E. Nuccio. All 
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: María Inés de Frutos-Fernández, Filippo A. E. Nuccio
 -/
+import Mathlib.Algebra.Order.Group.Basic
 import Mathlib.GroupTheory.SpecificGroups.Cyclic
 
 /-!
@@ -26,6 +27,30 @@ open LinearOrderedCommGroup
 variable {G : Type*} [CommGroup G] [LinearOrder G] [IsOrderedMonoid G]
 
 namespace Subgroup
+
+@[to_additive]
+lemma zpowers_eq_zpowers_iff {x y : G} :
+    Subgroup.zpowers x = Subgroup.zpowers y ↔ x = y ∨ x⁻¹ = y := by
+  rw [iff_comm]
+  constructor
+  · rintro (rfl|rfl) <;> simp
+  intro h
+  have hx : x ∈ Subgroup.zpowers y := by simp [← h]
+  have hy : y ∈ Subgroup.zpowers x := by simp [h]
+  rw [Subgroup.mem_zpowers_iff] at hx hy
+  obtain ⟨k, rfl⟩ := hy
+  obtain ⟨l, hl⟩ := hx
+  wlog hx1 : 1 < x
+  · push_neg at hx1
+    rcases hx1.eq_or_lt with rfl|hx1
+    · simp
+    · specialize this (x := x⁻¹) (-k) (by simp [h]) (-l) (by simp [hl]) (by simp [hx1])
+      simpa [or_comm] using this
+  simp only [← zpow_mul] at hl
+  replace hl : x ^ (k * l) = x ^ (1 : ℤ) := by simp [hl]
+  rw [zpow_right_inj hx1, Int.mul_eq_one_iff_eq_one_or_neg_one] at hl
+  refine hl.imp ?_ ?_ <;>
+  simp +contextual
 
 variable (H : Subgroup G) [Nontrivial H] [hH : IsCyclic H]
 
@@ -53,6 +78,14 @@ lemma genLTOne_lt_one : H.genLTOne < 1 :=
 lemma genLTOne_zpowers_eq_top : Subgroup.zpowers H.genLTOne = H :=
   H.exists_generator_lt_one.choose_spec.2
 
+lemma genLTOne_unique (g : G) : g < 1 ∧ Subgroup.zpowers g = H → g = H.genLTOne := by
+  rintro ⟨hg_lt, hg_top⟩
+  rw [← H.genLTOne_zpowers_eq_top] at hg_top
+  rcases Subgroup.zpowers_eq_zpowers_iff.mp hg_top with _ | h
+  · assumption
+  rw [← one_lt_inv', h] at hg_lt
+  exact (not_lt_of_gt hg_lt <| Subgroup.genLTOne_lt_one _).elim
+
 end Subgroup
 
 section IsCyclic
@@ -66,6 +99,9 @@ noncomputable def genLTOne : G := (⊤ : Subgroup G).genLTOne
 
 @[to_additive (attr := simp) negGen_eq_of_top]
 lemma genLTOne_eq_of_top : genLTOne G = (⊤ : Subgroup G).genLTOne := rfl
+
+lemma genLTOne_unique (g : G) : g < 1 ∧ Subgroup.zpowers g = ⊤ → g = genLTOne G :=
+  (⊤ : Subgroup G).genLTOne_unique g
 
 end IsCyclic
 
