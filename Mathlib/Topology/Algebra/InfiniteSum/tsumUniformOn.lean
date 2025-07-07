880215019265990e1f7c9051dc8cdaf5e573529d
Chris Birkbeck <c.birkbeck@uea.ac.uk>
2025-07-07 14:59:54 +0100

    fix name

--- Mathlib/Topology/Algebra/InfiniteSum/tsumUniformOn.lean
+++ Mathlib/Topology/Algebra/InfiniteSum/tsumUniformOn.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2022 Sébastien Gouëzel. All rights reserved.
+Copyright (c) 2025 Chris Birkbeck. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Sébastien Gouëzel
+Authors: Chris Birkbeck
 -/
 import Mathlib.Algebra.Lie.OfAssociative
 import Mathlib.Analysis.Calculus.UniformLimitsDeriv
