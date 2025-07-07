35a28a05230d5fca6e63661b25226a82110196cc
Stefan Kebekus <kebekus@users.noreply.github.com>
2025-07-07 10:37:12 +0200

    Create Constructions.lean

---
+++ Mathlib/Analysis/InnerProductSpace/Harmonic/Constructions.lean
@@ -0,0 +1,133 @@
+/-
+Copyright (c) 2025 Stefan Kebekus. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Stefan Kebekus
+-/
+import Mathlib.Analysis.InnerProductSpace.Harmonic.Basic
+import Mathlib.Analysis.Calculus.ContDiff.RestrictScalars
+import Mathlib.Analysis.SpecialFunctions.Complex.Analytic
+
+/-!
+# Construction of Harmonic Functions
+
+This file constructs examples of harmonic functions.
+
+- If `f : ℂ → F` is complex-differentiable, then `f` is harmonic. If `F = ℂ`,
+  then so is its real part, imaginary part, and complex conjugate.
+
+- If `f : ℂ → ℂ` is complex-differentiable without zero, then `log ‖f‖` is
+  harmonic.
+-/
+
+open Complex InnerProductSpace Topology
+
+variable
+  {F : Type*} [NormedAddCommGroup F] [NormedSpace ℂ F]
+  {f : ℂ → F} {x : ℂ}
+
+/-!
+## Harmonicity of Analytic Functions on the Complex Plane
+-/
+
+/--
+Continuously complex-differentiable functions on ℂ are harmonic.
+-/
+theorem ContDiffAt.harmonicAt (h : ContDiffAt ℂ 2 f x) : HarmonicAt f x := by
+  constructor
+  · exact h.restrict_scalars ℝ
+  · filter_upwards [h.restrictScalars_iteratedFDeriv_eventuallyEq (𝕜 := ℝ)] with a ha
+    have : (iteratedFDeriv ℂ 2 f a) (I • ![1, 1])
+        = (∏ i, I) • ((iteratedFDeriv ℂ 2 f a) ![1, 1]) :=
+      (iteratedFDeriv ℂ 2 f a).map_smul_univ (fun _ ↦ I) ![1, 1]
+    simp_all [laplacian_eq_iteratedFDeriv_complexPlane f, ← ha,
+      ContinuousMultilinearMap.coe_restrictScalars]
+
+/--
+Analytic functions on ℂ are harmonic.
+-/
+theorem AnalyticAt.harmonicAt [CompleteSpace F] (h : AnalyticAt ℂ f x) :
+    HarmonicAt f x := h.contDiffAt.harmonicAt
+
+/--
+If `f : ℂ → ℂ` is complex-analytic, then its real part is harmonic.
+-/
+theorem ContDiffAt.harmonicAt_realPart {f : ℂ → ℂ} (h : AnalyticAt ℂ f x) :
+    HarmonicAt (reCLM ∘ f) x := h.harmonicAt.comp_CLM reCLM
+
+/--
+If `f : ℂ → ℂ` is complex-analytic, then its imaginary part is harmonic.
+-/
+theorem ContDiffAt.harmonicAt_imaginaryPart {f : ℂ → ℂ} (h : AnalyticAt ℂ f x) :
+  HarmonicAt (imCLM ∘ f) x := h.harmonicAt.comp_CLM imCLM
+
+/--
+If `f : ℂ → ℂ` is complex-analytic, then its complex conjugate is harmonic.
+-/
+theorem ContDiffAt.harmonicAt_conjugate {f : ℂ → ℂ} (h : AnalyticAt ℂ f x) :
+  HarmonicAt (conjCLE ∘ f) x := (harmonicAt_comp_CLE_iff conjCLE).2 h.harmonicAt
+
+/-!
+## Harmonicity of `log ‖analytic‖`
+-/
+
+/- Helper lemma for AnalyticAt.harmonicAt_log_norm -/
+private lemma slitPlaneLemma {z : ℂ} (hz : z ≠ 0) : z ∈ slitPlane ∨ -z ∈ slitPlane := by
+  rw [mem_slitPlane_iff, mem_slitPlane_iff]
+  rw [ne_eq, Complex.ext_iff] at hz
+  push_neg at hz
+  simp_all only [ne_eq, zero_re, zero_im, neg_re, Left.neg_pos_iff, neg_im, neg_eq_zero]
+  by_contra contra
+  push_neg at contra
+  exact hz (le_antisymm contra.1.1 contra.2.1) contra.1.2
+
+/- Helper lemma for AnalyticAt.harmonicAt_log_norm -/
+private lemma analyticAt_harmonicAt_log_normSq {z : ℂ} {g : ℂ → ℂ} (h₁g : AnalyticAt ℂ g z)
+    (h₂g : g z ≠ 0) (h₃g : g z ∈ slitPlane) :
+    HarmonicAt (Real.log ∘ normSq ∘ g) z := by
+  rw [harmonicAt_congr_nhds (f₂ := reCLM ∘ (conjCLE ∘ log ∘ g + log ∘ g))]
+  · exact (((harmonicAt_comp_CLE_iff conjCLE).2 ((analyticAt_clog h₃g).comp h₁g).harmonicAt).add
+      ((analyticAt_clog h₃g).comp h₁g).harmonicAt).comp_CLM reCLM
+  · have t₀ := h₁g.differentiableAt.continuousAt.preimage_mem_nhds
+      ((isOpen_slitPlane.inter isOpen_ne).mem_nhds ⟨h₃g, h₂g⟩)
+    calc Real.log ∘ normSq ∘ g
+    _ =ᶠ[𝓝 z] reCLM ∘ ofRealCLM ∘ Real.log ∘ normSq ∘ g:= by
+      aesop
+    _ =ᶠ[𝓝 z] reCLM ∘ log ∘ ((conjCLE ∘ g) * g) := by
+      filter_upwards with x
+      simp only [Function.comp_apply, ofRealCLM_apply, Pi.mul_apply, conjCLE_apply]
+      rw [ofReal_log, normSq_eq_conj_mul_self]
+      exact normSq_nonneg (g x)
+    _ =ᶠ[𝓝 z] reCLM ∘ (log ∘ conjCLE ∘ g + log ∘ g) := by
+      filter_upwards [t₀] with x hx
+      simp only [Function.comp_apply, Pi.mul_apply, conjCLE_apply, Pi.add_apply]
+      congr
+      rw [Complex.log_mul_eq_add_log_iff _ hx.2, Complex.arg_conj]
+      simp only [Complex.slitPlane_arg_ne_pi hx.1, ↓reduceIte, neg_add_cancel, Set.mem_Ioc,
+        Left.neg_neg_iff, Real.pi_pos, Real.pi_nonneg, and_self]
+      simpa [ne_eq, map_eq_zero] using hx.2
+    _ =ᶠ[𝓝 z] ⇑reCLM ∘ (⇑conjCLE ∘ log ∘ g + log ∘ g) := by
+      apply Filter.eventuallyEq_iff_exists_mem.2
+      use g⁻¹' (Complex.slitPlane ∩ {0}ᶜ), t₀
+      intro x hx
+      simp only [Function.comp_apply, Pi.add_apply, conjCLE_apply]
+      congr 1
+      rw [← Complex.log_conj]
+      simp [Complex.slitPlane_arg_ne_pi hx.1]
+
+/--
+If `f : ℂ → ℂ` is complex-analytic without zero, then `log ‖f‖` is harmonic.
+-/
+theorem AnalyticAt.harmonicAt_log_norm {f : ℂ → ℂ} {z : ℂ} (h₁f : AnalyticAt ℂ f z) (h₂f : f z ≠ 0) :
+    HarmonicAt (Real.log ‖f ·‖) z := by
+  have : (Real.log ‖f ·‖) = (2 : ℝ)⁻¹ • (Real.log ∘ Complex.normSq ∘ f) := by
+    funext z
+    simp only [Pi.smul_apply, Function.comp_apply, smul_eq_mul]
+    rw [Complex.norm_def, Real.log_sqrt]
+    linarith
+    exact (f z).normSq_nonneg
+  rw [this]
+  apply HarmonicAt.const_smul
+  by_cases h₃f : f z ∈ Complex.slitPlane
+  · exact analyticAt_harmonicAt_log_normSq h₁f h₂f h₃f
+  · rw [(by aesop : Complex.normSq ∘ f = Complex.normSq ∘ (-f))]
+    exact analyticAt_harmonicAt_log_normSq h₁f.neg (by simpa) ((slitPlaneLemma h₂f).resolve_left h₃f)
