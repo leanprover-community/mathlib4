0ef8c70545acf60ae595ab636f8228936dce0372
Xavier Roblot <roblot@math.univ-lyon1.fr>
2025-07-07 08:10:58 +0000

    feat(NumberField): Image of torsion modulo an ideal (#26072)
    Let `I` be an integral ideal of a number field `K`. We define the morphism from the torsion of `K` to `(𝓞 K ⧸ I)ˣ` and prove that it is injective if the norm of `I` is coprime with the order of the torsion of `K`.
    
    As a consequence, we prove that for a prime ideal `P` coprime with the order of the torsion of `K`, the norm of `P` is congruent to `1` modulo `torsionOrder K`.
    
    
    Co-authored-by: Xavier Roblot <46200072+xroblot@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -4669,6 +4669,7 @@ import Mathlib.NumberTheory.NumberField.FractionalIdeal
 import Mathlib.NumberTheory.NumberField.House
 import Mathlib.NumberTheory.NumberField.Ideal
 import Mathlib.NumberTheory.NumberField.Ideal.Asymptotics
+import Mathlib.NumberTheory.NumberField.Ideal.Basic
 import Mathlib.NumberTheory.NumberField.InfinitePlace.Basic
 import Mathlib.NumberTheory.NumberField.InfinitePlace.Embeddings
 import Mathlib.NumberTheory.NumberField.InfinitePlace.Ramification
