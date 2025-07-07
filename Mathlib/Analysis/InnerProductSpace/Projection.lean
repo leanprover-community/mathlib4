a3202d0e459de6e057245111e4560eaf3a869880
Monica Omar <2497250a@research.gla.ac.uk>
2025-07-07 17:01:26 +0000

    update

--- Mathlib/Analysis/InnerProductSpace/Projection.lean
+++ Mathlib/Analysis/InnerProductSpace/Projection.lean
@@ -562,11 +562,9 @@ theorem orthogonalProjection_eq_self_iff {v : E} : (K.orthogonalProjection v : E
 lemma range_starProjection (U : Submodule 𝕜 E) [U.HasOrthogonalProjection] :
     LinearMap.range U.starProjection = U := by
   ext x
-  simp only [LinearMap.mem_range, starProjection, ContinuousLinearMap.comp_apply,
-    subtypeL_apply]
-  refine ⟨fun ⟨y, hy⟩ ↦ hy ▸ coe_mem (U.orthogonalProjection y), fun h ↦ ?_⟩
-  use x
-  simp only [orthogonalProjection_eq_self_iff, h]
+  simp only [LinearMap.mem_range, starProjection, ContinuousLinearMap.comp_apply, subtypeL_apply]
+  exact ⟨fun ⟨y, hy⟩ ↦ hy ▸ coe_mem (U.orthogonalProjection y),
+    fun h ↦ ⟨x, orthogonalProjection_eq_self_iff.mpr h⟩⟩
 
 @[simp]
 lemma starProjection_apply_mem (U : Submodule 𝕜 E) [U.HasOrthogonalProjection] (x : E) :
