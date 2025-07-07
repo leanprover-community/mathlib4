032bd78a80e278f46266cfa19be5384e36322ce0
Filippo A. E. Nuccio <filippo.nuccio@univ-st-etienne.fr>
2025-07-07 16:42:23 +0200

    removed  imports

--- Mathlib/Algebra/Order/GroupWithZero/WithZero.lean
+++ Mathlib/Algebra/Order/GroupWithZero/WithZero.lean
@@ -1,11 +1,10 @@
 /-
 Copyright (c) 2024 Kevin Buzzard. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Kevin Buzzard
+Authors: Kevin Buzzard, Filippo A. E. Nuccio
 -/
 import Mathlib.Algebra.GroupWithZero.Range
 import Mathlib.Algebra.Order.GroupWithZero.Canonical
-import Mathlib.Algebra.Order.Monoid.Units
 /-!
 
 # Covariant instances on `WithZero`
