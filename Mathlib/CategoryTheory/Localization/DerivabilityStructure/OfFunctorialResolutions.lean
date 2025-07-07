bc9a25f3be169624a7f1f74d0815bd5d66edc960
Joël Riou <37772949+joelriou@users.noreply.github.com>
2025-07-07 10:13:01 +0200

    Update Mathlib/CategoryTheory/Localization/DerivabilityStructure/OfFunctorialResolutions.lean
    Co-authored-by: Robin Carlier <57142648+robin-carlier@users.noreply.github.com>

--- Mathlib/CategoryTheory/Localization/DerivabilityStructure/OfFunctorialResolutions.lean
+++ Mathlib/CategoryTheory/Localization/DerivabilityStructure/OfFunctorialResolutions.lean
@@ -32,10 +32,10 @@ variable (Φ : LocalizerMorphism W₁ W₂)
 include hi in
 lemma hasRightResolutions_arrow_of_functorial_resolutions :
     Φ.arrow.HasRightResolutions :=
-  fun f ↦ ⟨{
-    X₁ := Arrow.mk (ρ.map f.hom)
-    w := Arrow.homMk (i.app _) (i.app _) (i.naturality f.hom).symm
-    hw := ⟨hi _, hi _⟩ }⟩
+  fun f ↦
+    ⟨{ X₁ := Arrow.mk (ρ.map f.hom)
+       w := Arrow.homMk (i.app _) (i.app _) (i.naturality f.hom).symm
+       hw := ⟨hi _, hi _⟩ }⟩
 
 namespace functorialRightResolutions
 
