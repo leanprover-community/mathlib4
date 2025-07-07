7a41712f2b19cc33e71614c3b1c4d3d62d9ee484
Oliver Butterley <oliver.butterley@gmail.com>
2025-07-07 11:54:30 +0200

    partialSups_add_one'

--- Mathlib/Algebra/Order/PartialSups.lean
+++ Mathlib/Algebra/Order/PartialSups.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kim Morrison
 -/
 import Mathlib.Algebra.Order.SuccPred
+import Mathlib.Data.Nat.SuccPred
 import Mathlib.Order.PartialSups
 
 /-!
@@ -18,3 +19,7 @@ variable {α ι : Type*} [SemilatticeSup α]
 lemma partialSups_add_one [Add ι] [One ι] [LinearOrder ι] [LocallyFiniteOrderBot ι] [SuccAddOrder ι]
     (f : ι → α) (i : ι) : partialSups f (i + 1) = partialSups f i ⊔ f (i + 1) :=
   Order.succ_eq_add_one i ▸ partialSups_succ f i
+
+lemma partialSups_add_one' (f : ℕ → α) (n : ℕ) :
+    partialSups f (n + 1) = f 0 ⊔ partialSups (f ∘ (fun k ↦ k + 1)) n := by
+  exact Order.succ_eq_add_one n ▸ partialSups_succ' f n
