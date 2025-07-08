b6b1725e19173e65a84ffc488208751e135ccf1e
Junyan Xu <junyanxu.math@gmail.com>
2025-07-08 23:40:57 +0200

    start Primitive

---
+++ Mathlib/Analysis/Complex/Primitive.lean
@@ -0,0 +1,7 @@
+/-
+Copyright (c) 2025 Junyan Xu. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Junyan Xu
+-/
+import Mathlib.Analysis.Calculus.MeanValue
+import Mathlib.Topology.EtaleSpace
