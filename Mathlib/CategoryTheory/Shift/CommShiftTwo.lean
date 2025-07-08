38c59168147bda57774a873122785522482b5ad3
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-07-08 07:54:27 +0200

    wip

--- Mathlib/CategoryTheory/Shift/CommShiftTwo.lean
+++ Mathlib/CategoryTheory/Shift/CommShiftTwo.lean
@@ -31,8 +31,12 @@ section
 
 variable (D) (M : Type*) [AddCommMonoid M] [HasShift D M]
 
-abbrev Shift₂Data : Type _ :=
+abbrev Shift₂Data' : Type _ :=
   TwistShiftData (PullbackShift D (.sum M)) (M × M)
+
+structure Shift₂Data where
+  z (m₁ m₂ : M) : (CatCenter D)ˣ
+  -- TODO: add axioms...
 
 end
 
@@ -40,11 +44,26 @@ namespace Functor
 
 variable (F : C₁ ⥤ C₂ ⥤ D) {M : Type*} [AddCommMonoid M]
   [HasShift C₁ M] [HasShift C₂ M] [HasShift D M]
-  (t : Shift₂Data D M)
+  (t' : Shift₂Data' D M)
 
-abbrev uncurryToTwistShift : C₁ × C₂ ⥤ TwistShift t := uncurry.obj F
+abbrev uncurryToTwistShift : C₁ × C₂ ⥤ TwistShift t' := uncurry.obj F
 
-abbrev CommShift₂' := (F.uncurryToTwistShift t).CommShift (M × M)
+abbrev CommShift₂' := (F.uncurryToTwistShift t').CommShift (M × M)
+
+variable (t : Shift₂Data D M)
+
+class CommShift₂ where
+  commShiftObj (X₁ : C₁) : (F.obj X₁).CommShift M
+  commShiftFlipObj (X₂ : C₂) : (F.flip.obj X₂).CommShift M
+  commShift_map {X₁ Y₁ : C₁} (f : X₁ ⟶ Y₁) : NatTrans.CommShift (F.map f) M
+  commShift_flip_map {X₂ Y₂ : C₂} (f : X₂ ⟶ Y₂) : NatTrans.CommShift (F.flip.map f) M
+  compatibility (X₁ : C₁) (X₂ : C₂) (m₁ m₂ : M) :
+    ((F.flip.obj (X₂⟦m₂⟧)).commShiftIso m₁).hom.app X₁ ≫
+      (((F.obj X₁).commShiftIso m₂).hom.app X₂)⟦m₁⟧' =
+        t.z m₁ m₂ •
+          (((F.obj (X₁⟦m₁⟧)).commShiftIso m₂).hom.app X₂) ≫
+            (((F.flip.obj X₂).commShiftIso m₁).hom.app (X₁))⟦m₂⟧' ≫
+            (shiftFunctorComm D m₁ m₂).hom.app ((F.obj X₁).obj X₂)
 
 end Functor
 
