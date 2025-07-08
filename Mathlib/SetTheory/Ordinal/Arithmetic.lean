6d5561ca6d00c68423a68bb977ecbcb56acdfc7b
Violeta Hernández <vi.hdz.p@gmail.com>
2025-07-08 04:19:40 -0600

    trim more PR

--- Mathlib/SetTheory/Ordinal/Arithmetic.lean
+++ Mathlib/SetTheory/Ordinal/Arithmetic.lean
@@ -529,16 +529,13 @@ theorem sub_lt_of_le {a b c : Ordinal} (h : b ≤ a) : a - b < c ↔ a < b + c :
   lt_iff_lt_of_le_iff_le (le_sub_of_le h)
 
 @[simp]
-theorem sub_zero (a : Ordinal) : a - 0 = a := by
-  simpa only [zero_add] using add_sub_cancel 0 a
+theorem sub_zero (a : Ordinal) : a - 0 = a := by simpa only [zero_add] using add_sub_cancel 0 a
 
 @[simp]
-theorem zero_sub (a : Ordinal) : 0 - a = 0 := by
-  simpa only [Ordinal.le_zero] using (sub_le_self 0 a)
+theorem zero_sub (a : Ordinal) : 0 - a = 0 := by rw [← Ordinal.le_zero]; apply sub_le_self
 
 @[simp]
-theorem sub_self (a : Ordinal) : a - a = 0 := by
-  simpa only [add_zero] using add_sub_cancel a 0
+theorem sub_self (a : Ordinal) : a - a = 0 := by simpa only [add_zero] using add_sub_cancel a 0
 
 protected theorem sub_eq_zero_iff_le {a b : Ordinal} : a - b = 0 ↔ a ≤ b :=
   ⟨fun h => by simpa only [h, add_zero] using le_add_sub a b, fun h => by
@@ -591,16 +588,10 @@ theorem add_le_iff_of_isLimit {a b c : Ordinal} (hb : IsLimit b) :
 theorem isNormal_add_right (a : Ordinal) : IsNormal (a + ·) :=
   ⟨fun b => (add_lt_add_iff_left a).2 (lt_succ b), fun _b l _c => add_le_iff_of_isLimit l⟩
 
-@[deprecated isNormal_add_right (since := "2024-10-11")]
-alias add_isNormal := isNormal_add_right
-
 theorem isLimit_add (a) {b} : IsLimit b → IsLimit (a + b) :=
   (isNormal_add_right a).isLimit
 
-@[deprecated isLimit_add (since := "2024-10-11")]
-alias add_isLimit := isLimit_add
-
-alias IsLimit.add := add_isLimit
+alias IsLimit.add := isLimit_add
 
 theorem isLimit_sub {a b} (ha : IsLimit a) (h : b < a) : IsLimit (a - b) := by
   rw [isLimit_iff, Ordinal.sub_ne_zero_iff_lt, isSuccPrelimit_iff_succ_lt]
