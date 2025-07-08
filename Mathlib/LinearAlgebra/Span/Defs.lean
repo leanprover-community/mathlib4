766ae705efcca586e2e490b5d37f6a792a9c7bf1
agjftucker <agjftucker@gmail.com>
2025-07-08 14:09:22 +0100

    extraneous line
    Co-authored-by: themathqueen <23701951+themathqueen@users.noreply.github.com>

--- Mathlib/LinearAlgebra/Span/Defs.lean
+++ Mathlib/LinearAlgebra/Span/Defs.lean
@@ -355,7 +355,6 @@ theorem codisjoint_iff_exists_add_eq :
   exact forall_congr' (fun z => mem_sup.trans <| by simp)
 
 @[deprecated (since := "2025-07-05")] alias ⟨exists_add_eq_of_codisjoint, _⟩ := codisjoint_iff_exists_add_eq
-  simpa only [h.eq_top] using Submodule.mem_top
 
 variable (p p')
 
