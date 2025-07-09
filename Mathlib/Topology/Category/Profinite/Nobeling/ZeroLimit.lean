bab0713d7fb6d3c888042140fbbf0c3b635edecf
Violeta Hernández <vi.hdz.p@gmail.com>
2025-07-09 07:56:32 -0600

    Update ZeroLimit.lean

--- Mathlib/Topology/Category/Profinite/Nobeling/ZeroLimit.lean
+++ Mathlib/Topology/Category/Profinite/Nobeling/ZeroLimit.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Dagur Asgeirsson
 -/
 import Mathlib.LinearAlgebra.LinearIndependent.Basic
-import Mathlib.SetTheory.Ordinal.Arithmetic
 import Mathlib.Topology.Category.Profinite.Nobeling.Basic
 
 /-!
