6c5c3a9e202da6e24059c8bc1a13560a7d520024
Damiano Testa <adomani@gmail.com>
2025-07-09 14:18:17 +0200

    chore: space following `Σ`

--- Mathlib/Data/Finite/Sigma.lean
+++ Mathlib/Data/Finite/Sigma.lean
@@ -14,7 +14,7 @@ variable {α : Type*}
 
 namespace Finite
 
-instance {β : α → Type*} [Finite α] [∀ a, Finite (β a)] : Finite (Σa, β a) := by
+instance {β : α → Type*} [Finite α] [∀ a, Finite (β a)] : Finite (Σ a, β a) := by
   letI := Fintype.ofFinite α
   letI := fun a => Fintype.ofFinite (β a)
   infer_instance
