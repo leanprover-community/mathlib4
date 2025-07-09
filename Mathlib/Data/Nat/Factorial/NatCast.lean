30f2c53d910e55a4b92464fc463cfd4938790f95
mariainesdff <mariaines.dff@gmail.com>
2025-07-09 10:27:23 +0200

    imports

--- Mathlib/Data/Nat/Factorial/NatCast.lean
+++ Mathlib/Data/Nat/Factorial/NatCast.lean
@@ -6,8 +6,7 @@ Authors: Antoine Chambert-Loir, María Inés de Frutos-Fernández
 
 import Mathlib.Algebra.Algebra.Defs
 import Mathlib.Algebra.CharP.Invertible
-import Mathlib.Algebra.Order.Group.Nat
-import Mathlib.Data.Nat.Factorial.Basic
+import Mathlib.Data.Finset.NatAntidiagonal
 import Mathlib.RingTheory.Nilpotent.Defs
 
 /-!
