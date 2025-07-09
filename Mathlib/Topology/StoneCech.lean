6e9ef653ff1c52827999ff9dfce68c292baf0692
Komyyy <pol_tta@outlook.jp>
2025-07-09 13:29:42 +0900

    better notion

--- Mathlib/Topology/StoneCech.lean
+++ Mathlib/Topology/StoneCech.lean
@@ -1,11 +1,3 @@
-/-
-Copyright (c) 2018 Reid Barton. All rights reserved.
-Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Reid Barton
--/
-import Mathlib.Topology.Bases
-import Mathlib.Topology.DenseEmbedding
-import Mathlib.Topology.Connected.TotallyDisconnected
+import Mathlib.Topology.Compactification.StoneCech
 
-deprecated_module "This file was moved to `Mathlib.Topology.Compactification.StoneCech`."
-  (since := "2025-06-07")
+deprecated_module (since := "2025-06-07")
