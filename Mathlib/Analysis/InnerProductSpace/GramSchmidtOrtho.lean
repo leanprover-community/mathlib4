66ebef5f17a42d395139672a237da38e65916e4c
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-07-10 17:15:16 +0200

    chore(GramSchmidtOrtho): namespace the remaining file

--- Mathlib/Analysis/InnerProductSpace/GramSchmidtOrtho.lean
+++ Mathlib/Analysis/InnerProductSpace/GramSchmidtOrtho.lean
@@ -217,12 +217,6 @@ theorem gramSchmidt_linearIndependent {f : ι → E} (h₀ : LinearIndependent �
   linearIndependent_of_ne_zero_of_inner_eq_zero (fun _ => gramSchmidt_ne_zero _ h₀) fun _ _ =>
     gramSchmidt_orthogonal 𝕜 f
 
-end InnerProductSpace
-
-open InnerProductSpace
-
-variable {𝕜}
-
 /-- When given a basis, `gramSchmidt` produces a basis. -/
 noncomputable def gramSchmidtBasis (b : Basis ι 𝕜 E) : Basis ι 𝕜 E :=
   Basis.mk (gramSchmidt_linearIndependent b.linearIndependent)
@@ -360,3 +354,5 @@ theorem gramSchmidtOrthonormalBasis_det [DecidableEq ι] :
   exact ((gramSchmidtOrthonormalBasis h f).repr_apply_apply (f _) _).symm
 
 end OrthonormalBasis
+
+end InnerProductSpace
