cdbdfc7ff1d61466adb014aca7d70f81e8c43cc9
Violeta Hernández <vi.hdz.p@gmail.com>
2025-07-10 01:22:37 -0600

    more progress

--- Mathlib/Order/Dedekind.lean
+++ Mathlib/Order/Dedekind.lean
@@ -58,8 +58,7 @@ def upperCuts (α : Type*) [Preorder α] : Set (Set α) :=
 theorem mem_upperCuts_iff_eq : s ∈ upperCuts α ↔ upperBounds (lowerBounds s) = s :=
   mem_lowerCuts_iff_eq (α := αᵒᵈ)
 
-theorem inter_mem_upperCuts (hs : s ∈ upperCuts α) (ht : t ∈ upperCuts α) :
-    s ∩ t ∈ upperCuts α :=
+theorem inter_mem_upperCuts (hs : s ∈ upperCuts α) (ht : t ∈ upperCuts α) : s ∩ t ∈ upperCuts α :=
   inter_mem_lowerCuts (α := αᵒᵈ) hs ht
 
 theorem sInter_mem_upperCuts {S : Set (Set α)} (hS : S ⊆ upperCuts α) : ⋂₀ S ∈ upperCuts α :=
@@ -77,3 +76,98 @@ theorem upperBounds_mem_upperCuts_of_mem_lowerCuts (H : s ∈ lowerCuts α) :
 theorem lowerBounds_mem_lowerCuts_of_mem_upperCuts (H : s ∈ upperCuts α) :
     lowerBounds s ∈ lowerCuts α := by
   simp_all [mem_upperCuts_iff_eq, mem_lowerCuts_iff_eq]
+
+/-! ### Dedekind cuts -/
+
+/-- A Dedekind cut (in the Dedekind-MacNeille completion) is a defined as a member of `lowerCuts α`.
+
+Use `Dedekind.of_lowerCuts` to define a member of this structure. For the dual definition through
+`upperCuts`, use `DedekindCut.of_upperCuts`. -/
+structure DedekindCut (α : Type*) [Preorder α] where
+  carrier : Set α
+  carrier_mem_lowerCuts : carrier ∈ lowerCuts α
+
+namespace DedekindCut
+
+variable (A B : DedekindCut α)
+
+/- The lower set in a Dedekind cut. -/
+def lowerCut : Set α := A.carrier
+
+theorem lowerCut_mem_lowerCuts : A.lowerCut ∈ lowerCuts α :=
+  A.carrier_mem_lowerCuts
+
+@[simp]
+theorem lowerCut_eq : lowerBounds (upperBounds A.lowerCut) = A.lowerCut :=
+  mem_lowerCuts_iff_eq.mp A.lowerCut_mem_lowerCuts
+
+/- The upper set in a Dedekind cut. -/
+def upperCut : Set α := upperBounds A.lowerCut
+
+@[simp]
+theorem upperCut_eq : upperBounds (lowerBounds A.upperCut) = A.upperCut := by
+  rw [upperCut, lowerCut_eq]
+
+theorem upperCut_mem_upperCuts : A.upperCut ∈ upperCuts α :=
+  mem_upperCuts_iff_eq.mpr A.upperCut_eq
+
+@[simp]
+theorem upperBounds_lowerCut : upperBounds A.lowerCut = A.upperCut :=
+  rfl
+
+@[simp]
+theorem lowerBounds_upperCut : lowerBounds A.upperCut = A.lowerCut := by
+  rw [← upperBounds_lowerCut, lowerCut_eq]
+
+/-- Build a Dedekind cut from its lower set. -/
+def of_lowerCuts (s : Set α) (hs : s ∈ lowerCuts α) : DedekindCut α :=
+  ⟨s, hs⟩
+
+@[simp]
+theorem lowerCut_of_lowerCuts (hs : s ∈ lowerCuts α) : lowerCut (of_lowerCuts s hs) = s :=
+  rfl
+
+@[simp]
+theorem upperCut_of_lowerCuts (hs : s ∈ lowerCuts α) :
+    upperCut (of_lowerCuts s hs) = upperBounds s :=
+  rfl
+
+/-- Build a Dedekind cut from its upper set. -/
+def of_upperCuts (s : Set α) (hs : s ∈ upperCuts α) : DedekindCut α :=
+  ⟨_, lowerBounds_mem_lowerCuts_of_mem_upperCuts hs⟩
+
+@[simp]
+theorem lowerCut_of_upperCuts (hs : s ∈ upperCuts α) :
+    lowerCut (of_upperCuts s hs) = lowerBounds s :=
+  rfl
+
+@[simp]
+theorem upperCut_of_upperCuts (hs : s ∈ upperCuts α) :
+    upperCut (of_upperCuts s hs) = s :=
+  mem_upperCuts_iff_eq.mp hs
+
+variable {A B}
+
+theorem lowerCut_subset_iff_subset_upperCut :
+    A.lowerCut ⊆ B.lowerCut ↔ B.upperCut ⊆ A.upperCut where
+  mp  H := by simpa using fun a ha ↦ upperBounds_mono_set H ha
+  mpr H := by simpa using fun a ha ↦ lowerBounds_mono_set H ha
+
+theorem lowerCut_ssubset_iff_ssubset_upperCut :
+    A.lowerCut ⊂ B.lowerCut ↔ B.upperCut ⊂ A.upperCut := by
+  simp [ssubset_def, lowerCut_subset_iff_subset_upperCut]
+
+theorem lowerCut_eq_iff_upperCut_eq : A.lowerCut = B.lowerCut ↔ A.upperCut = B.upperCut := by
+  simp [subset_antisymm_iff, lowerCut_subset_iff_subset_upperCut, and_comm]
+
+@[ext]
+theorem ext_lowerCut (h : A.lowerCut = B.lowerCut) : A = B := by
+  cases A; cases B; simpa
+
+theorem ext_upperCut (h : A.upperCut = B.upperCut) : A = B :=
+  ext_lowerCut (lowerCut_eq_iff_upperCut_eq.mpr h)
+
+theorem ext_upperCut_iff : A = B ↔ A.upperCut = B.upperCut :=
+  ⟨congrArg _, ext_upperCut⟩
+
+end DedekindCut
