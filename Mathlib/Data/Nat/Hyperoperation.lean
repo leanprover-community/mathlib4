68083cd2b3c34d7b37b0a824d42a775c84bc2cb4
Rob23oba <robin.arnez@web.de>
2025-07-10 13:41:34 +0200

    fix tiny

--- Mathlib/Data/Nat/Hyperoperation.lean
+++ Mathlib/Data/Nat/Hyperoperation.lean
@@ -80,4 +80,5 @@ theorem hyperoperation_ge_three_one (n k : ℕ) : hyperoperation (n + 3) 1 k = 1
 @[grind =]
 theorem hyperoperation_ge_four_zero (n k : ℕ) :
     hyperoperation (n + 4) 0 k = if Even k then 1 else 0 := by
-  induction k with grind [Nat.even_add_one]
+  -- TODO: Even.zero shouldn't be necessary here, it is marked `@[grind]`
+  induction k with grind [Nat.even_add_one, Even.zero]
