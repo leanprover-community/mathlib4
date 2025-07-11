dce6d067a9d7347c88ddaa16c821d08403122862
Yaël Dillies <yael.dillies@gmail.com>
2025-07-11 20:22:19 +0000

    fix

--- Mathlib/Data/Rel/Cover.lean
+++ Mathlib/Data/Rel/Cover.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yaël Dillies
 -/
 import Mathlib.Data.Rel.Separated
+import Mathlib.Topology.UniformSpace.Defs
 
 /-!
 # Covers in a uniform space
@@ -23,10 +24,10 @@ The concept of uniform covers is used to define two further notions of covering:
 [R. Vershynin, *High Dimensional Probability*][vershynin2018high], Section 4.2.
 -/
 
-open Set
+open Set UniformSpace
 
-namespace UniformSpace
-variable {X : Type*} {U V : Set (X × X)} {s t N N₁ N₂ : Set X} {x : X}
+namespace Rel
+variable {X : Type*} {U V : Rel X X} {s t N N₁ N₂ : Set X} {x : X}
 
 /-- For an entourage `U`, a set `N` is a *`U`-cover* of a set `s` if every point of `s` is `U`-close
 to some point of `N`.
@@ -34,12 +35,12 @@ to some point of `N`.
 This is also called a *`U`-net* in the literature.
 
 [R. Vershynin, *High Dimensional Probability*][vershynin2018high], 4.2.1. -/
-def IsCover (U : Set (X × X)) (s N : Set X) : Prop := ∀ ⦃x⦄, x ∈ s → ∃ y ∈ N, (x, y) ∈ U
+def IsCover (U : Rel X X) (s N : Set X) : Prop := ∀ ⦃x⦄, x ∈ s → ∃ y ∈ N, (x, y) ∈ U
 
 @[simp] lemma IsCover.empty : IsCover U ∅ N := by simp [IsCover]
 
 @[simp] lemma isCover_empty_right : IsCover U s ∅ ↔ s = ∅ := by
-  simp [IsCover, eq_empty_iff_forall_not_mem]
+  simp [IsCover, eq_empty_iff_forall_notMem]
 
 @[simp] protected nonrec lemma IsCover.nonempty (hsN : IsCover U s N) (hs : s.Nonempty) :
     N.Nonempty := let ⟨_x, hx⟩ := hs; let ⟨y, hy, _⟩ := hsN hx; ⟨y, hy⟩
@@ -55,21 +56,21 @@ lemma IsCover.anti (hst : s ⊆ t) (ht : IsCover U t N) : IsCover U s N := fun _
 lemma IsCover.mono_entourage (hUV : U ⊆ V) (hU : IsCover U s N) : IsCover V s N :=
   fun _x hx ↦ let ⟨y, hy, hxy⟩ := hU hx; ⟨y, hy, hUV hxy⟩
 
-lemma isCover_iff_subset_iUnion_ball (hU : IsSymmetricRel U) :
-    IsCover U s N ↔ s ⊆ ⋃ y ∈ N, ball y U := by simp [IsCover, subset_def, ball, hU.mk_mem_comm]
+lemma isCover_iff_subset_iUnion_ball [U.IsSymm] : IsCover U s N ↔ s ⊆ ⋃ y ∈ N, ball y U := by
+  simp [IsCover, subset_def, ball, U.comm]
 
 alias ⟨IsCover.subset_iUnion_ball, IsCover.of_subset_iUnion_ball⟩ := isCover_iff_subset_iUnion_ball
 
 /-- A maximal `U`-separated subset of a set `s` is a `U`-cover of `s`.
 
 [R. Vershynin, *High Dimensional Probability*][vershynin2018high], 4.2.6. -/
-lemma IsCover.of_maximal_isSeparated (hUrefl : ∀ x, (x, x) ∈ U) (hUsymm : IsSymmetricRel U)
+lemma IsCover.of_maximal_isSeparated [U.IsRefl] [U.IsSymm]
     (hN : Maximal (fun N ↦ N ⊆ s ∧ IsSeparated U N) N) : IsCover U s N := by
   rintro x hx
   by_contra! h
-  simpa [hUrefl] using h _ <| hN.2 (y := insert x N) ⟨by simp [insert_subset_iff, hx, hN.1.1],
-    hN.1.2.insert hUsymm fun y hy _ ↦ h y hy⟩ (subset_insert _ _) (mem_insert _ _)
+  simpa [U.rfl] using h _ <| hN.2 (y := insert x N) ⟨by simp [insert_subset_iff, hx, hN.1.1],
+    hN.1.2.insert fun y hy hxy ↦ (h y hy hxy).elim⟩ (subset_insert _ _) (mem_insert _ _)
 
-@[simp] lemma isCover_idRel : IsCover idRel s N ↔ s ⊆ N := by simp [IsCover, subset_def]
+@[simp] lemma isCover_relId : IsCover Rel.id s N ↔ s ⊆ N := by simp [IsCover, subset_def]
 
-end UniformSpace
+end Rel
