98caa7d2c6b4e4b92f8907d46e6a4b2297aa654c
Emily Riehl <eriehl@jhu.edu>
2025-07-11 09:12:11 -0400

    Update Mathlib/AlgebraicTopology/SimplicialSet/HomotopyCat.lean
    Co-authored-by: Joël Riou <37772949+joelriou@users.noreply.github.com>

--- Mathlib/AlgebraicTopology/SimplicialSet/HomotopyCat.lean
+++ Mathlib/AlgebraicTopology/SimplicialSet/HomotopyCat.lean
@@ -383,7 +383,7 @@ def hoFunctorDeltaZeroIsTerminal : IsTerminal (hoFunctor.obj (Δ[0])) := by
   apply Cat.isTerminalOfUniqueOfIsDiscrete
 
 /-- The homotopy category functor preserves generic terminal objects. -/
-noncomputable def hoFunctor.terminalIso : (hoFunctor.obj (⊤_ SSet)) ≅ (⊤_ Cat) :=
+noncomputable def hoFunctor.terminalIso : hoFunctor.obj (⊤_ SSet) ≅ ⊤_ Cat :=
   hoFunctor.mapIso (terminalIsoIsTerminal stdSimplex.isTerminalObj₀) ≪≫
     (terminalIsoIsTerminal hoFunctorDeltaZeroIsTerminal).symm
 
