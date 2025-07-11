ea1338ae2957e373830202a7f8192a40f3d31794
Christopher Hoskin <mans0954@users.noreply.github.com>
2025-07-11 23:38:00 +0100

    Fix

--- Mathlib/Order/Neutral.lean
+++ Mathlib/Order/Neutral.lean
@@ -1,4 +1,10 @@
-import Mathlib
+/-
+Copyright (c) 2025 Christopher Hoskin. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Christopher Hoskin
+-/
+
+import Mathlib.Order.Lattice
 
 --import Mathlib.Order.Lattice
 
@@ -13,124 +19,6 @@ def IsStandard [Lattice α] (a : α) : Prop :=
 
 def IsNeutral [Lattice α] (a : α) : Prop :=
   ∀ (x y : α), (a ⊓ x) ⊔ (a ⊓ y) ⊔ (x ⊓ y) = (a ⊔ x) ⊓ (a ⊔ y) ⊓ (x ⊔ y)
-
-structure IsLatticeCon [Lattice α] (r : α → α → Prop) : Prop extends Equivalence r where
-  inf : ∀ {w x y z}, r w x → r y z → r (w ⊓ y) (x ⊓ z)
-  sup : ∀ {w x y z}, r w x → r y z → r (w ⊔ y) (x ⊔ z)
-
-lemma isLatticCon_iff [Lattice α] (r : α → α → Prop) (h : IsRefl _ r) : IsLatticeCon r ↔
-    (∀ x y : α, r x y ↔ r (x ⊓ y) (x ⊔ y)) ∧
-    (∀ x y z : α, x ≤ y → y ≤ z → r x y → r y z → r x z) ∧
-    (∀ x y t : α, x ≤ y → r x y → r (x ⊓ t) (y ⊓ t) ∧ r (x ⊔ t) (y ⊔ t)) := by
-  constructor
-  · intro hlc
-    constructor
-    · intro x y
-      constructor
-      · intro h
-        exact hlc.trans (y := y) (by
-          conv_rhs => rw [← inf_idem y]
-          exact hlc.inf h (hlc.refl y)) (by
-          conv_lhs => rw [← sup_idem y]
-          exact hlc.sup (hlc.symm h) (hlc.refl y))
-      · intro h
-        have e1 : r x (x ⊓ y)  := by
-          conv_lhs => rw [← inf_sup_self (a := x) (b := y)]
-          conv_rhs => rw [← inf_idem x, inf_assoc]
-          exact hlc.inf (hlc.refl x) (hlc.symm h)
-        have e2 : r (x ⊓ y) y  := by
-          conv_rhs => rw [← inf_sup_self (a := y) (b := x)]
-          conv_lhs => rw [← inf_idem y, inf_comm, inf_assoc]
-          apply hlc.inf (hlc.refl y)
-          rw [inf_comm, sup_comm]
-          exact h
-        exact hlc.trans e1 e2
-    · exact ⟨fun _ _ _ _ _ => hlc.trans, fun _ _ t _ h2 =>
-        ⟨hlc.inf h2 (hlc.refl t), hlc.sup h2 (hlc.refl t)⟩⟩
-  · intro ⟨h1,h2,h3⟩
-    have e1 (a b c d : α) (hb : b ∈ Set.Icc a d) (hc : c ∈ Set.Icc a d) (h : r a d) : r b c := by
-      rw [h1]
-      conv_lhs => rw [← inf_eq_left.mpr inf_le_sup]
-      conv_rhs => rw [← inf_eq_right.mpr (sup_le hb.2 hc.2)]
-      apply (h3 _ _ _ (inf_le_of_left_le hb.2) _).1
-      rw [← sup_eq_right.mpr (le_inf hb.1 hc.1), ← sup_eq_left.mpr (inf_le_of_left_le hb.2)]
-      exact (h3 _ _ _ (le_trans hb.1 hb.2) h).2
-    have ee1 (x y t : α) (hh : r x y) : r (x ⊔ t) (y ⊔ t) := e1 ((x ⊓ y) ⊔ t) _ _ ((x ⊔ y) ⊔ t) (by
-        simp
-        constructor
-        · exact le_trans inf_le_left le_sup_left
-        · rw [sup_assoc]
-          exact le_sup_left) (by
-        simp
-        constructor
-        · exact le_trans inf_le_right le_sup_left
-        · rw [sup_comm, ← sup_assoc]
-          exact le_sup_right) (h3 _ _ _ inf_le_sup ((h1 _ _).mp hh)).2
-    have ee2 (x y t : α) (hh : r x y) : r (x ⊓ t) (y ⊓ t) := by
-      apply e1 ((x ⊓ y) ⊓ t) _ _ ((x ⊔ y) ⊓ t) (by
-        simp
-        constructor
-        · rw [inf_assoc]
-          exact inf_le_left
-        · exact le_trans inf_le_left le_sup_left) (by
-        simp
-        constructor
-        · rw [inf_comm, ← inf_assoc]
-          exact inf_le_right
-        · exact le_trans inf_le_left le_sup_right) (h3 _ _ _ inf_le_sup ((h1 _ _).mp hh)).1
-    have transitive: ∀ {x y z : α}, r x y → r y z → r x z := by
-      intro x y z hxy hyz
-      apply e1 (x ⊓ y ⊓ z) _ _ (x ⊔ y ⊔ z) (by
-          rw [Set.mem_Icc]
-          constructor
-          · rw [inf_assoc]
-            exact inf_le_left
-          · rw [sup_assoc]
-            exact le_sup_left) (by simp only [Set.mem_Icc, inf_le_right, le_sup_right, and_self])
-      have e2 : r ((x ⊓ y) ⊔ (y ⊔ z)) ((x ⊔ y) ⊔ (y ⊔ z)) :=
-        (h3 _ _ _ inf_le_sup ((h1 x y).mp hxy)).2
-      have e3 : (x ⊔ y) ⊔ (y ⊔ z) = x ⊔ y ⊔ z := by
-        rw [sup_comm x y, ← sup_sup_distrib_left, sup_assoc]
-      have e4 : (x ⊓ y) ⊔ (y ⊔ z) = (y ⊔ z) :=
-        sup_eq_right.mpr (le_trans inf_le_right le_sup_left)
-      rw [e3, e4] at e2
-      have e2' : r ((x ⊓ y) ⊓ (y ⊓ z)) ((x ⊔ y) ⊓ (y ⊓ z))  :=
-        (h3 _ _ _ inf_le_sup ((h1 x y).mp hxy)).1
-      have e3' : (x ⊓ y) ⊓ (y ⊓ z) = x ⊓ y ⊓ z := by
-        rw [inf_comm x y, ← inf_inf_distrib_left, inf_assoc]
-      have e4' : (x ⊔ y) ⊓ (y ⊓ z) = (y ⊓ z) :=
-        inf_eq_right.mpr (le_trans inf_le_left le_sup_right)
-      rw [e3', e4'] at e2'
-      have e5 : r (x ⊓ y ⊓ z) (y ⊔ z) := h2 (y := y ⊓ z) _ _
-        (by rw [inf_assoc]; exact inf_le_right) inf_le_sup e2' ((h1 _ _).mp hyz)
-      apply h2 (y := y ⊔ z) _ _ (by rw [inf_assoc]; exact inf_le_of_right_le inf_le_sup)
-        (by rw [sup_assoc]; exact le_sup_right) e5 e2
-    constructor
-    · constructor
-      · intro x
-        exact h.refl _
-      · intro x y hxy
-        rw [h1, inf_comm, sup_comm, ← h1]
-        exact hxy
-      · exact transitive
-    · intro x₀ y₀ x₁ y₁ h₀ h₁
-      have s1 : r (x₀ ⊓ x₁) (x₀ ⊓ y₁) := by
-        conv_lhs => rw [inf_comm]
-        conv_rhs => rw [inf_comm]
-        apply ee2
-        exact h₁
-      have s2 : r (x₀ ⊓ y₁) (y₀ ⊓ y₁) := ee2 _  _ _ h₀
-      exact transitive s1 s2
-    · intro x₀ y₀ x₁ y₁ h₀ h₁
-      have s1 : r (x₀ ⊔ x₁) (x₀ ⊔ y₁) := by
-        conv_lhs => rw [sup_comm]
-        conv_rhs => rw [sup_comm]
-        apply ee1
-        exact h₁
-      have s2 : r (x₀ ⊔ y₁) (y₀ ⊔ y₁) := ee1 _  _ _ h₀
-      exact transitive s1 s2
-
-
 
 def ker (f : α → β) : α → α → Prop := fun a b => f a = f b
 
