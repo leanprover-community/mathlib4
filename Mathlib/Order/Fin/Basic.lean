6308050cff7d7cc164bc4384e3f5be3354bfff66
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2025-07-11 15:04:26 +0100

    reomve `@[gcongr]` tags

--- Mathlib/Order/Fin/Basic.lean
+++ Mathlib/Order/Fin/Basic.lean
@@ -266,11 +266,6 @@ lemma predAbove_right_monotone (p : Fin n) : Monotone p.predAbove := fun a b H =
   · exact le_pred_of_lt ((not_lt.mp ha).trans_lt hb)
   · exact H
 
-@[gcongr]
-theorem _root_.GCongr.Fin.predAbove_le_predAbove_right (p : Fin n) {i j : Fin (n + 1)} (h : i ≤ j) :
-    p.predAbove i ≤ p.predAbove j :=
-  predAbove_right_monotone p h
-
 lemma predAbove_left_monotone (i : Fin (n + 1)) : Monotone fun p ↦ predAbove p i := fun a b H ↦ by
   dsimp [predAbove]
   split_ifs with ha hb hb
@@ -279,11 +274,6 @@ lemma predAbove_left_monotone (i : Fin (n + 1)) : Monotone fun p ↦ predAbove p
   · have : b < a := castSucc_lt_castSucc_iff.mpr (hb.trans_le (le_of_not_gt ha))
     exact absurd H this.not_ge
   · rfl
-
-@[gcongr]
-lemma _root_.GCongr.predAbove_le_predAbove_left {p q : Fin n} (h : p ≤ q) (i : Fin (n + 1)) :
-    p.predAbove i ≤ q.predAbove i :=
-  predAbove_left_monotone i h
 
 @[gcongr]
 lemma predAbove_le_predAbove {p q : Fin n} (hpq : p ≤ q) {i j : Fin (n + 1)} (hij : i ≤ j) :
