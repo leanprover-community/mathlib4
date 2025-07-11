bf9e55d2244865868316a8a2d76afb2bd31d888a
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2025-07-11 22:14:26 +0800

    Update Basic.lean

--- Mathlib/RingTheory/CohenMacaulay/Basic.lean
+++ Mathlib/RingTheory/CohenMacaulay/Basic.lean
@@ -604,6 +604,11 @@ section Polynomial
 
 open Polynomial
 
+lemma Polynomial.localization_at_comap_maximal_isCM_isCM [IsNoetherianRing R]
+    [IsCohenMacaulayLocalRing R] (p : Ideal R[X]) [p.IsPrime] (max : p.comap C = maximalIdeal R) :
+    IsCohenMacaulayLocalRing (Localization.AtPrime p) := by
+  sorry
+
 theorem Polynomial.isCM_of_isCM [IsNoetherianRing R] [IsCohenMacaulayRing R] :
     IsCohenMacaulayRing R[X] := by
   apply (isCohenMacaulayRing_def _).mpr (fun p hp ↦ ?_)
@@ -611,16 +616,26 @@ theorem Polynomial.isCM_of_isCM [IsNoetherianRing R] [IsCohenMacaulayRing R] :
   let S := (Localization.AtPrime q)[X]
   let pc := Submonoid.map Polynomial.C.toMonoidHom q.primeCompl
   let _ : Algebra R[X] S := algebra R (Localization.AtPrime q)
-  let _ : IsLocalization pc S := {
+  have _ : IsLocalization pc S := {
     map_units' x := by
       rcases x.2 with ⟨y, mem, eq⟩
       apply isUnit_of_mul_eq_one _ (C (Localization.mk 1 ⟨y, mem⟩))
       simp [← eq, S, ← map_mul, ← Localization.mk_one_eq_algebraMap, Localization.mk_mul]
     surj' z := by
-      let f : ℕ → q.primeCompl := fun n ↦
-        (Classical.choose (Localization.mkHom_surjective (Polynomial.coeff z n))).2
-      #check ∏ n ∈ z.1.1, f n
-      sorry
+      induction' z using Polynomial.induction_on' with f g hf hg n a
+      · rcases hf with ⟨⟨x1, y1⟩, h1⟩
+        rcases hg with ⟨⟨x2, y2⟩, h2⟩
+        use (x2 * y1.1 + x1 * y2.1, y1 * y2)
+        simp only [Submonoid.coe_mul, map_mul, add_mul, map_add]
+        nth_rw 4 [mul_comm]
+        simp [← mul_assoc, h1, h2, add_comm]
+      · rcases Localization.mkHom_surjective a with ⟨⟨x, y⟩, h⟩
+        use ((monomial n) x, ⟨C y.1, by simp [pc]⟩)
+        simp only [← h, Localization.mkHom_apply, algebraMap_def, coe_mapRingHom, map_C, ←
+          Localization.mk_one_eq_algebraMap, monomial_mul_C, map_monomial, S, Localization.mk_mul]
+        congr 1
+        apply Localization.mk_eq_mk_iff.mpr (Localization.r_of_eq ?_)
+        simp [mul_comm]
     exists_of_eq {x y} eq := by
       have eq' (n : ℕ) : (algebraMap R (Localization.AtPrime q)) (Polynomial.coeff x n) =
         (algebraMap R (Localization.AtPrime q)) (Polynomial.coeff y n) := by
@@ -631,10 +646,16 @@ theorem Polynomial.isCM_of_isCM [IsNoetherianRing R] [IsCohenMacaulayRing R] :
       let g : ℕ → q.primeCompl := fun n ↦ Classical.choose (IsLocalization.exists_of_eq (eq' n))
       have g_spec (n : ℕ) := Classical.choose_spec
         (IsLocalization.exists_of_eq (M := q.primeCompl) (eq' n))
-
-
-      sorry
-  }
+      let s := ∏ n ∈ x.1.1 ∪ y.1.1, g n
+      use ⟨C s.1, by simp [pc]⟩
+      ext n
+      simp only [coeff_C_mul, s]
+      by_cases mem : n ∈ x.1.1 ∪ y.1.1
+      · rcases Finset.dvd_prod_of_mem g mem with ⟨t, ht⟩
+        simp only [ht, Submonoid.coe_mul, mul_comm _ t.1, mul_assoc]
+        rw [g_spec n]
+      · simp only [Finset.mem_union, Finsupp.mem_support_iff, ne_eq, not_or, not_not] at mem
+        simp [← Polynomial.toFinsupp_apply, mem] }
   let pS := p.map (algebraMap R[X] S)
   have disj : Disjoint (pc : Set R[X]) (p : Set R[X]) := by
     simpa [pc, q] using Set.disjoint_image_left.mpr
@@ -644,8 +665,20 @@ theorem Polynomial.isCM_of_isCM [IsNoetherianRing R] [IsCohenMacaulayRing R] :
     convert IsLocalization.isLocalization_isLocalization_atPrime_isLocalization pc
       (Localization.AtPrime pS) pS
     exact (IsLocalization.comap_map_of_isPrime_disjoint pc _ _ ‹_› disj).symm
-
-  sorry
+  let _ := (isCohenMacaulayRing_def R).mp ‹_› q (comap_isPrime C p)
+  have : comap C pS = maximalIdeal (Localization.AtPrime q) := by
+    rw [← IsLocalization.map_comap q.primeCompl _ (comap C pS),
+      ← IsLocalization.map_comap q.primeCompl _ (maximalIdeal (Localization.AtPrime q))]
+    simp only [comap_comap, S, pS]
+    rw [← Polynomial.algebraMap_eq (R := Localization.AtPrime q),
+      ← IsScalarTower.algebraMap_eq R (Localization.AtPrime q) (Localization.AtPrime q)[X],
+      IsScalarTower.algebraMap_eq R R[X] (Localization.AtPrime q)[X], ← comap_comap,
+      IsLocalization.comap_map_of_isPrime_disjoint pc _ _ ‹_› disj,
+      IsLocalization.AtPrime.comap_maximalIdeal (Localization.AtPrime q) q]
+    rfl
+  let _ := localization_at_comap_maximal_isCM_isCM (Localization.AtPrime q) pS this
+  exact isCohenMacaulayLocalRing_of_ringEquiv (IsLocalization.algEquiv p.primeCompl
+    (Localization.AtPrime pS) (Localization.AtPrime p)).toRingEquiv
 
 theorem MvPolynomial.isCM_of_isCM [IsNoetherianRing R] [IsCohenMacaulayRing R] (n : ℕ):
     IsCohenMacaulayRing (MvPolynomial (Fin n) R) := by
