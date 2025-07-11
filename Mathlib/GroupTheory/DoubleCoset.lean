1002529b01092877c99886915a9de8ec4e822d42
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2025-07-11 17:02:12 +0100

    lint

--- Mathlib/GroupTheory/DoubleCoset.lean
+++ Mathlib/GroupTheory/DoubleCoset.lean
@@ -33,10 +33,12 @@ namespace DoubleCoset
 def doubleCoset (a : α) (s t : Set α) : Set α :=
   s * {a} * t
 
-lemma doubleCoset_eq_image2 (a : α) (s t : Set α) : doubleCoset a s t = Set.image2 (· * a * ·) s t := by
+lemma doubleCoset_eq_image2 (a : α) (s t : Set α) :
+    doubleCoset a s t = Set.image2 (· * a * ·) s t := by
   simp_rw [doubleCoset, Set.mul_singleton, ← Set.image2_mul, Set.image2_image_left]
 
-theorem mem_doubleCoset {s t : Set α} {a b : α} : b ∈ doubleCoset a s t ↔ ∃ x ∈ s, ∃ y ∈ t, b = x * a * y := by
+theorem mem_doubleCoset {s t : Set α} {a b : α} :
+    b ∈ doubleCoset a s t ↔ ∃ x ∈ s, ∃ y ∈ t, b = x * a * y := by
   simp only [doubleCoset_eq_image2, Set.mem_image2, eq_comm]
 
 theorem mem_doubleCoset_self (H K : Subgroup G) (a : G) : a ∈ doubleCoset a H K :=
@@ -45,8 +47,8 @@ theorem mem_doubleCoset_self (H K : Subgroup G) (a : G) : a ∈ doubleCoset a H 
 theorem doubleCoset_eq_of_mem {H K : Subgroup G} {a b : G} (hb : b ∈ doubleCoset a H K) :
     doubleCoset b H K = doubleCoset a H K := by
   obtain ⟨h, hh, k, hk, rfl⟩ := mem_doubleCoset.1 hb
-  rw [doubleCoset, doubleCoset, ← Set.singleton_mul_singleton, ← Set.singleton_mul_singleton, mul_assoc,
-    mul_assoc, Subgroup.singleton_mul_subgroup hk, ← mul_assoc, ← mul_assoc,
+  rw [doubleCoset, doubleCoset, ← Set.singleton_mul_singleton, ← Set.singleton_mul_singleton,
+    mul_assoc, mul_assoc, Subgroup.singleton_mul_subgroup hk, ← mul_assoc, ← mul_assoc,
     Subgroup.subgroup_mul_singleton hh]
 
 theorem mem_doubleCoset_of_not_disjoint {H K : Subgroup G} {a b : G}
@@ -58,7 +60,8 @@ theorem mem_doubleCoset_of_not_disjoint {H K : Subgroup G} {a b : G}
   rwa [mul_assoc, mul_assoc, eq_inv_mul_iff_mul_eq, ← mul_assoc, ← mul_assoc, eq_mul_inv_iff_mul_eq]
 
 theorem eq_of_not_disjoint {H K : Subgroup G} {a b : G}
-    (h : ¬Disjoint (doubleCoset a H K) (doubleCoset b H K)) : doubleCoset a H K = doubleCoset b H K := by
+    (h : ¬Disjoint (doubleCoset a H K) (doubleCoset b H K)) :
+    doubleCoset a H K = doubleCoset b H K := by
   rw [disjoint_comm] at h
   have ha : a ∈ doubleCoset b H K := mem_doubleCoset_of_not_disjoint h
   apply doubleCoset_eq_of_mem ha
@@ -124,8 +127,8 @@ theorem mk_out_eq_mul (H K : Subgroup G) (g : G) :
   refine ⟨h⁻¹, k⁻¹, H.inv_mem h_h, K.inv_mem hk, eq_mul_inv_of_mul_eq (eq_inv_mul_of_mul_eq ?_)⟩
   rw [← mul_assoc, ← T]
 
-theorem mk_eq_of_doubleCoset_eq {H K : Subgroup G} {a b : G} (h : doubleCoset a H K = doubleCoset b H K) :
-    mk H K a = mk H K b := by
+theorem mk_eq_of_doubleCoset_eq {H K : Subgroup G} {a b : G}
+    (h : doubleCoset a H K = doubleCoset b H K) : mk H K a = mk H K b := by
   rw [eq]
   exact mem_doubleCoset.mp (h.symm ▸ mem_doubleCoset_self H K b)
 
