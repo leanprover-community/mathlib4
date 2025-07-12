a428f1b8ec8984bd7d5c9848de89469df9262ca3
Komyyy <pol_tta@outlook.jp>
2025-07-12 19:07:51 +0900

    feat: Linear upper or lower sets topologies are completely normal

---
+++ Mathlib/Topology/Separation/LinearUpperLowerSetTopology.lean
@@ -0,0 +1,30 @@
+/-
+Copyright (c) 2025 Miyahara Kō. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Miyahara Kō
+-/
+
+import Mathlib.Topology.Order.UpperLowerSetTopology
+import Mathlib.Topology.Separation.Regular
+
+/-!
+# Linear upper or lower sets topologies are completely normal
+-/
+
+open Set Topology.IsUpperSet
+
+instance {α : Type*} [TopologicalSpace α] [LinearOrder α] [Topology.IsUpperSet α] :
+    CompletelyNormalSpace α where
+  completely_normal s t hcst hsct := by
+    obtain (rfl | ⟨a, ha⟩) := s.eq_empty_or_nonempty
+    case inl => simp
+    obtain (rfl | ⟨b, hb⟩) := t.eq_empty_or_nonempty
+    case inl => simp
+    exfalso
+    -- issue #27058
+    fail_if_success grw [← singleton_subset_iff.mpr ha, ← singleton_subset_iff.mpr hb] at hcst hsct
+    grw [Set.disjoint_iff, ← singleton_subset_iff.mpr ha, ← singleton_subset_iff.mpr hb,
+      ← Set.disjoint_iff,] at hcst hsct
+    conv at hcst => equals a < b => simp
+    conv at hsct => equals b < a => simp
+    exact lt_asymm hcst hsct
