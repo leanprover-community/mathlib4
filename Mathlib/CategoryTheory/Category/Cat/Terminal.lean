885ec14cbf66a5bfa300cbf369ce49562f317be1
emilyriehl <eriehl@jhu.edu>
2025-07-12 18:29:02 -0400

    unnecessary import

--- Mathlib/CategoryTheory/Category/Cat/Terminal.lean
+++ Mathlib/CategoryTheory/Category/Cat/Terminal.lean
@@ -3,7 +3,6 @@ Copyright (c) 2025 Emily Riehl. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Robin Carlier, Emily Riehl
 -/
-import Mathlib.CategoryTheory.Category.Cat.Limit
 import Mathlib.CategoryTheory.Limits.Shapes.Terminal
 
 /-!
