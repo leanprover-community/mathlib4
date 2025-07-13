85f45518fdbab8e4922d15de4261c92ca06f34f5
Eric Wieser <wieser.eric@gmail.com>
2025-07-13 15:18:29 +0000

    feat: `rank (vecMulVec _ _) ≤ 1`

--- Mathlib/LinearAlgebra/FreeModule/Finite/Matrix.lean
+++ Mathlib/LinearAlgebra/FreeModule/Finite/Matrix.lean
@@ -106,11 +106,3 @@ instance Module.Free.addMonoidHom [Module.Free ℤ N] : Module.Free ℤ (M →+ 
   Module.Free.of_equiv (addMonoidHomLequivInt ℤ).symm
 
 end Integer
-
-theorem Matrix.rank_vecMulVec {K m n : Type u} [CommRing K] [Fintype n]
-    [DecidableEq n] (w : m → K) (v : n → K) : (Matrix.vecMulVec w v).toLin'.rank ≤ 1 := by
-  nontriviality K
-  rw [Matrix.vecMulVec_eq (Fin 1), Matrix.toLin'_mul]
-  refine le_trans (LinearMap.rank_comp_le_left _ _) ?_
-  refine (LinearMap.rank_le_domain _).trans_eq ?_
-  rw [rank_fun', Fintype.card_ofSubsingleton, Nat.cast_one]
