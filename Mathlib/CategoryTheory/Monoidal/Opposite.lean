92d1f0895bc7fa6fbaabf68c1a7146deb461b3db
Robin Carlier <57142648+robin-carlier@users.noreply.github.com>
2025-07-13 18:45:03 +0200

    fix unused simp arg

--- Mathlib/CategoryTheory/Monoidal/Opposite.lean
+++ Mathlib/CategoryTheory/Monoidal/Opposite.lean
@@ -346,7 +346,7 @@ instance : (mopMopEquivalence C).IsMonoidal where
   leftAdjoint_ε := by
     simp [ε, η, Adjunction.homEquiv, mopMopEquivalence, Equivalence.trans, unmopEquiv, ε]
   leftAdjoint_μ X Y := by
-    simp [μ, η, δ, Adjunction.homEquiv, mopMopEquivalence, Equivalence.trans, unmopEquiv, μ]
+    simp [μ, δ, Adjunction.homEquiv, mopMopEquivalence, Equivalence.trans, unmopEquiv, μ]
 
 /-- The identification `mop X ⊗ mop Y = mop (Y ⊗ X)` as a natural isomorphism. -/
 @[simps!]
