64bfb6e287e42cfec1ae3c65eb2f9bfbf873895a
Paul-Lez <paul.lezeau@gmail.com>
2025-07-13 23:09:09 +0100

    feat: define vector bundles

--- Mathlib.lean
+++ Mathlib.lean
@@ -172,6 +172,7 @@ import Mathlib.Algebra.Category.ModuleCat.Sheaf.Limits
 import Mathlib.Algebra.Category.ModuleCat.Sheaf.PullbackContinuous
 import Mathlib.Algebra.Category.ModuleCat.Sheaf.PushforwardContinuous
 import Mathlib.Algebra.Category.ModuleCat.Sheaf.Quasicoherent
+import Mathlib.Algebra.Category.ModuleCat.Sheaf.VectorBundle
 import Mathlib.Algebra.Category.ModuleCat.Simple
 import Mathlib.Algebra.Category.ModuleCat.Subobject
 import Mathlib.Algebra.Category.ModuleCat.Tannaka
