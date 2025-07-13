3b96c01d507bff382b8d479037c15d5349c9c4db
staroperator <staroperator92@gmail.com>
2025-07-13 19:55:40 -0400

    replace (0,S,+) with (0,1,+)

--- Mathlib/ModelTheory/Arithmetic/Presburger/Basic.lean
+++ Mathlib/ModelTheory/Arithmetic/Presburger/Basic.lean
@@ -9,7 +9,7 @@ import Mathlib.ModelTheory.Semantics
 /-!
 # Presburger arithmetic
 
-This file defines the first-order language of Presburger arithmetic as (0,S,+).
+This file defines the first-order language of Presburger arithmetic as (0,1,+).
 
 ## Main Definitions
 
@@ -17,8 +17,8 @@ This file defines the first-order language of Presburger arithmetic as (0,S,+).
 
 ## TODO
 
-- Generalize `presburger.finsum` (maybe also `presburger.natCast` and `presburger.nsmul`) for
-  classes like `FirstOrder.Language.IsOrdered`.
+- Generalize `presburger.finsum` (maybe also `NatCast` and `SMul`) for classes like
+  `FirstOrder.Language.IsOrdered`.
 - Define the theory of Presburger arithmetic and prove its properties (quantifier elimination,
   completeness, etc).
 
@@ -28,14 +28,14 @@ variable {α : Type*}
 
 namespace FirstOrder
 
-/-- The type of Presburger arithmetic functions, defined as (0,S,+) -/
+/-- The type of Presburger arithmetic functions, defined as (0,1,+) -/
 inductive presburgerFunc : ℕ → Type
   | zero : presburgerFunc 0
-  | succ : presburgerFunc 1
+  | one : presburgerFunc 0
   | add : presburgerFunc 2
   deriving DecidableEq
 
-/-- The language of Presburger arithmetic, defined as (0,S,+). -/
+/-- The language of Presburger arithmetic, defined as (0,1,+). -/
 def Language.presburger : Language :=
   { Functions := presburgerFunc
     Relations := fun _ => Empty }
@@ -48,37 +48,32 @@ variable {t t₁ t₂ : presburger.Term α}
 instance : Zero (presburger.Term α) where
   zero := Constants.term .zero
 
-/-- `presburger.succ t` is the successor of `t`, applying `presburgerFunc.succ` on `t`. -/
-def succ (t : presburger.Term α) := Functions.apply₁ presburgerFunc.succ t
+instance : One (presburger.Term α) where
+  one := Constants.term .one
 
 instance : Add (presburger.Term α) where
   add := Functions.apply₂ .add
 
-protected def natCast : ℕ → presburger.Term α
-| 0 => 0
-| n + 1 => succ (presburger.natCast n)
-
 instance : NatCast (presburger.Term α) where
-  natCast := presburger.natCast
+  natCast := Nat.unaryCast
 
 @[simp] theorem natCast_zero : NatCast.natCast 0 = (0 : presburger.Term α) := rfl
 
-@[simp] theorem natCast_succ {n : ℕ} : NatCast.natCast (n + 1) = succ (n : presburger.Term α) := rfl
-
-protected def nsmul : ℕ → presburger.Term α → presburger.Term α
-| 0, _ => 0
-| n + 1, t => presburger.nsmul n t + t
+@[simp] theorem natCast_succ {n : ℕ} : NatCast.natCast (n + 1) = (n : presburger.Term α) + 1 := rfl
 
 instance : SMul ℕ (presburger.Term α) where
-  smul := presburger.nsmul
+  smul := nsmulRec
 
 @[simp] theorem zero_nsmul : 0 • t = 0 := rfl
 
 @[simp] theorem succ_nsmul {n : ℕ} : (n + 1) • t = n • t + t := rfl
 
+/-- `presburger.succ t` is `t + 1`, the successor of `t`. -/
+def succ (t : presburger.Term α) := t + 1
+
 /-- Summation over a finite set of terms in Presburger arithmetic.
 
-  It is defined via choice, and therefore the result only makes sense when the structure satisfies
+  It is defined via choice, so the result only makes sense when the structure satisfies
   commutativity (see `realize_finsum`). -/
 noncomputable def finsum {β : Type*} [Fintype β] (f : β → presburger.Term α) : presburger.Term α :=
   ((Finset.univ : Finset β).toList.map f).foldr (· + ·) 0
@@ -92,19 +87,21 @@ variable [Zero M] [One M] [Add M]
 instance : presburger.Structure M where
   funMap
   | .zero, _ => 0
-  | .succ, v => v 0 + 1
+  | .one, v => 1
   | .add, v => v 0 + v 1
 
 @[simp] theorem funMap_zero {v} :
     @Structure.funMap presburger M _ _ presburgerFunc.zero v = 0 := rfl
 
-@[simp] theorem funMap_succ {v} :
-    @Structure.funMap presburger M _ _ presburgerFunc.succ v = v 0 + 1 := rfl
+@[simp] theorem funMap_one {v} :
+    @Structure.funMap presburger M _ _ presburgerFunc.one v = 1 := rfl
 
 @[simp] theorem funMap_add {v} :
     @Structure.funMap presburger M _ _ presburgerFunc.add v = v 0 + v 1 := rfl
 
 @[simp] theorem realize_zero : Term.realize v (0 : presburger.Term α) = 0 := rfl
+
+@[simp] theorem realize_one : Term.realize v (1 : presburger.Term α) = 1 := rfl
 
 @[simp] theorem realize_succ : Term.realize v (succ t) = Term.realize v t + 1 := rfl
 
@@ -119,7 +116,7 @@ end
 
 @[simp] theorem realize_nsmul [AddMonoidWithOne M] {n : ℕ} :
     Term.realize v (n • t) = n • Term.realize v t := by
-  induction n with simp [*, _root_.zero_nsmul, add_nsmul]
+  induction n with simp [*, add_nsmul]
 
 @[simp] theorem realize_finsum [AddCommMonoidWithOne M]
     {β : Type*} [Fintype β] {f : β → presburger.Term α} :
