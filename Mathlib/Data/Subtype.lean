79f4fb4e05b793aea3dfe9a9a5dc3d1f368f751f
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2025-07-14 15:14:51 +0200

    Update Mathlib/Data/Subtype.lean
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- Mathlib/Data/Subtype.lean
+++ Mathlib/Data/Subtype.lean
@@ -183,7 +183,7 @@ theorem map_eq {p : α → Prop} {q : β → Prop} {f g : α → β}
     (h₁ : ∀ a : α, p a → q (f a)) (h₂ : ∀ a : α, p a → q (g a))
     (x y : Subtype p) :
     map f h₁ x = map g h₂ y ↔ f x = g y :=
-  ⟨congr_arg Subtype.val, (Subtype.ext ·)⟩
+  Subtype.ext_iff
 
 theorem map_ne {p : α → Prop} {q : β → Prop} {f g : α → β}
     (h₁ : ∀ a : α, p a → q (f a)) (h₂ : ∀ a : α, p a → q (g a))
