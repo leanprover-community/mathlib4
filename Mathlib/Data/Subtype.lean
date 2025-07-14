02f4bc91c3e021cb17d3332552aa8629b3b671be
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2025-07-14 21:33:09 +0200

    Update Mathlib/Data/Subtype.lean

--- Mathlib/Data/Subtype.lean
+++ Mathlib/Data/Subtype.lean
@@ -181,7 +181,7 @@ theorem map_involutive {p : α → Prop} {f : α → α} (h : ∀ a, p a → p (
 
 theorem map_eq {p : α → Prop} {q : β → Prop} {f g : α → β}
     (h₁ : ∀ a : α, p a → q (f a)) (h₂ : ∀ a : α, p a → q (g a))
-    (x y : Subtype p) :
+    {x y : Subtype p} :
     map f h₁ x = map g h₂ y ↔ f x = g y :=
   Subtype.ext_iff
 
