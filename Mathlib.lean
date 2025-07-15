8b8ed9acff1e95aacf5e99d7dabbb83a8f2bdcf9
ADedecker <anatolededecker@gmail.com>
2025-07-15 21:09:43 +0200

    mk_all

--- Mathlib.lean
+++ Mathlib.lean
@@ -5465,6 +5465,7 @@ import Mathlib.RingTheory.Nullstellensatz
 import Mathlib.RingTheory.OrderOfVanishing
 import Mathlib.RingTheory.OreLocalization.Basic
 import Mathlib.RingTheory.OreLocalization.Cardinality
+import Mathlib.RingTheory.OreLocalization.NonZeroDivisors
 import Mathlib.RingTheory.OreLocalization.OreSet
 import Mathlib.RingTheory.OreLocalization.Ring
 import Mathlib.RingTheory.OrzechProperty
