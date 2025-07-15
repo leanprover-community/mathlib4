6e030b00e6f44c844fdd7999c6065bf92672c1bc
smorel394 <67864981+smorel394@users.noreply.github.com>
2025-07-15 17:50:43 +0200

    Update Mathlib/CategoryTheory/Preadditive/Injective/Basic.lean
    Co-authored-by: Joël Riou <37772949+joelriou@users.noreply.github.com>

--- Mathlib/CategoryTheory/Preadditive/Injective/Basic.lean
+++ Mathlib/CategoryTheory/Preadditive/Injective/Basic.lean
@@ -301,7 +301,7 @@ namespace Functor
 variable {D : Type*} [Category D] (F : C ⥤ D)
 
 theorem injective_of_map_injective [F.Full] [F.Faithful]
-    [F.PreservesMonomorphisms] (I : C) (hI : Injective (F.obj I)) : Injective I where
+    [F.PreservesMonomorphisms] {I : C} (hI : Injective (F.obj I)) : Injective I where
   factors g f _ := by
     obtain ⟨h, fac⟩ := hI.factors (F.map g) (F.map f)
     exact ⟨F.preimage h, F.map_injective (by simp [fac])⟩
