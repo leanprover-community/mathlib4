6c0e85296a036ecdff55478dce1fb2bfdc9f5145
damiano <adomani@gmail.com>
2025-07-15 09:22:23 +0000

    chore: remove whitespace (#26963)
    Found by #26926.

--- Mathlib/LinearAlgebra/Matrix/ToLin.lean
+++ Mathlib/LinearAlgebra/Matrix/ToLin.lean
@@ -793,7 +793,7 @@ lemma LinearMap.toMatrix_prodMap [DecidableEq m] [DecidableEq (n ⊕ m)]
     (φ₁ : Module.End R M₁) (φ₂ : Module.End R M₂) :
     toMatrix (v₁.prod v₂) (v₁.prod v₂) (φ₁.prodMap φ₂) =
       Matrix.fromBlocks (toMatrix v₁ v₁ φ₁) 0 0 (toMatrix v₂ v₂ φ₂) := by
-  ext (i|i) (j|j) <;> simp [toMatrix]
+  ext (i | i) (j | j) <;> simp [toMatrix]
 
 end ToMatrix
 
