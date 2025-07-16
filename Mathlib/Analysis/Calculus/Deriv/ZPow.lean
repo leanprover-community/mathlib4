8f1c4a31f9e737878fb31db66f1c5759d11b9310
Chris Birkbeck <c.birkbeck@uea.ac.uk>
2025-07-16 14:53:28 +0100

    Add itereatedDerivWithin version of zpow lemmas

--- Mathlib/Analysis/Calculus/Deriv/ZPow.lean
+++ Mathlib/Analysis/Calculus/Deriv/ZPow.lean
@@ -5,6 +5,7 @@ Authors: Sébastien Gouëzel, Yury Kudryashov
 -/
 import Mathlib.Analysis.Calculus.Deriv.Pow
 import Mathlib.Analysis.Calculus.Deriv.Inv
+import Mathlib.Analysis.Calculus.Deriv.Shift
 
 /-!
 # Derivatives of `x ^ m`, `m : ℤ`
@@ -132,6 +133,33 @@ theorem iter_deriv_inv (k : ℕ) (x : 𝕜) :
 theorem iter_deriv_inv' (k : ℕ) :
     deriv^[k] Inv.inv = fun x : 𝕜 => (-1) ^ k * k ! * x ^ (-1 - k : ℤ) :=
   funext (iter_deriv_inv k)
+
+open Function in
+theorem iter_deriv_inv_linear (k : ℕ) (c d : 𝕜) :
+    deriv^[k] (fun x ↦ (c * x + d)⁻¹) =
+    (fun x : 𝕜 ↦ (-1) ^ k * k ! * c ^ k * (c * x + d)^ (-1 - k : ℤ)) := by
+  induction' k with k ihk
+  · simp
+  · rw [Nat.factorial_succ, show  k + 1 = 1 + k by ring, iterate_add_apply, ihk]
+    ext z
+    simp only [Int.reduceNeg, iterate_one, deriv_const_mul_field',
+      Nat.cast_add, Nat.cast_one]
+    by_cases hd : c = 0
+    · simp [hd]
+    · have := deriv_comp_add_const (fun x ↦ (c * x) ^ (-1 - k : ℤ)) (d / c) z
+      have h0 : (fun x ↦ (c * (x + d / c)) ^ (-1 - (k : ℤ))) =
+        (fun x ↦ (c * x + d) ^ (-1 - (k : ℤ))) := by
+        ext y
+        field_simp
+        ring_nf
+      rw [h0, deriv_comp_mul_left c (fun x ↦ (x) ^ (-1 - k : ℤ)) (z + d / c)] at this
+      field_simp [this]
+      ring_nf
+
+theorem iter_deriv_inv_linear_sub (k : ℕ) (c d : 𝕜) :
+    deriv^[k] (fun x ↦ (c * x - d)⁻¹) =
+    (fun x : 𝕜 ↦ (-1) ^ k * k ! * c ^ k * (c * x - d)^ (-1 - k : ℤ)) := by
+  simpa [sub_eq_add_neg] using iter_deriv_inv_linear k c (-d)
 
 variable {f : E → 𝕜} {t : Set E} {a : E}
 
