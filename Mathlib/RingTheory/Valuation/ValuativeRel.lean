61fe3d33197634db600a74955fb20f2e4111537b
Yakov Pechersky <ffxen158@gmail.com>
2025-07-16 09:39:04 -0400

    simp linting

--- Mathlib/RingTheory/Valuation/ValuativeRel.lean
+++ Mathlib/RingTheory/Valuation/ValuativeRel.lean
@@ -505,7 +505,6 @@ lemma valuation_eq_zero_iff {x : R} :
     valuation R x = 0 ↔ x ≤ᵥ 0 :=
   ValueGroupWithZero.mk_eq_zero _ _
 
-@[simp]
 lemma valuation_posSubmonoid_ne_zero (x : posSubmonoid R) :
     valuation R (x : R) ≠ 0 := by
   rw [ne_eq, valuation_eq_zero_iff]
@@ -550,7 +549,7 @@ lemma isEquiv {Γ₁ Γ₂ : Type*}
 lemma valuation_posSubmonoid_ne_zero_of_compatible {Γ : Type*} [LinearOrderedCommMonoidWithZero Γ]
     (v : Valuation R Γ) [v.Compatible] (x : posSubmonoid R) :
     v (x : R) ≠ 0 := by
-  simp [(isEquiv v (valuation R)).ne_zero]
+  simp [(isEquiv v (valuation R)).ne_zero, valuation_posSubmonoid_ne_zero]
 
 variable (R) in
 /-- An alias for endowing a ring with a preorder defined as the valuative relation. -/
@@ -804,3 +803,5 @@ lemma ValuativeRel.IsRankLeOne.of_compatible_withZeroMulInt (v : Valuation R ℤ
     simp [StrictMono, «forall», e, zpow_pos, -inv_zpow', zpow_lt_zpow_iff_right₀]
   exact ⟨⟨MonoidWithZeroHom.comp e (ValuativeRel.ValueGroupWithZero.unquot v),
     he.comp (ValuativeRel.ValueGroupWithZero.unquot_strictMono v)⟩⟩
+
+#lint
