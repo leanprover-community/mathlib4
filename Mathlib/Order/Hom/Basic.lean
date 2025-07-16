0cbc759de4e38aa4e45a1e125427ee7d64ff3189
Yakov Pechersky <pechersky@users.noreply.github.com>
2025-07-16 10:48:37 -0400

    Update Mathlib/Order/Hom/Basic.lean
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- Mathlib/Order/Hom/Basic.lean
+++ Mathlib/Order/Hom/Basic.lean
@@ -1189,12 +1189,8 @@ lemma StrictMono.denselyOrdered_range {X Y : Type*} [LinearOrder X] [DenselyOrde
     {f : X → Y} (hf : StrictMono f) :
     DenselyOrdered (Set.range f) := by
   constructor
-  simp only [Subtype.exists, Set.mem_range, Subtype.forall, Subtype.mk_lt_mk, exists_and_right,
-    forall_exists_index, exists_prop, forall_apply_eq_imp_iff, hf.lt_iff_lt]
-  intro a b hab
-  obtain ⟨c, hc⟩ := exists_between hab
-  use f c
-  simp_all [hf.lt_iff_lt]
+  simpa [← exists_and_left, ← exists_and_right, exists_comm, hf.lt_iff_lt]
+    using fun _ _ ↦ exists_between
 
 lemma denselyOrdered_iff_of_orderIsoClass {X Y F : Type*} [Preorder X] [Preorder Y]
     [EquivLike F X Y] [OrderIsoClass F X Y] (f : F) :
