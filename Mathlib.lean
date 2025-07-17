ed84d14c053745f350d736d4e1b069d49bad5a0c
Sebastien Gouezel <sebastien.gouezel@univ-rennes1.fr>
2025-07-17 17:33:37 +0000

    feat: length of a path in a manifold (#26778)
    Consider a manifold in which the tangent spaces have an enormed structure. Then one defines
    `pathELength γ a b` as the length of the path `γ : ℝ → M` between `a` and `b`, i.e., the integral
    of the norm of its manifold derivative. We develop a reasonable API around this notion
    
    We also define `riemannianEDist x y` as the infimum of the length of paths from `x` to `y`. We show that it is symmetric and satisfies the triangle inequality.

--- Mathlib.lean
+++ Mathlib.lean
@@ -3733,6 +3733,7 @@ import Mathlib.Geometry.Manifold.MFDeriv.UniqueDifferential
 import Mathlib.Geometry.Manifold.Metrizable
 import Mathlib.Geometry.Manifold.PartitionOfUnity
 import Mathlib.Geometry.Manifold.PoincareConjecture
+import Mathlib.Geometry.Manifold.Riemannian.PathELength
 import Mathlib.Geometry.Manifold.Sheaf.Basic
 import Mathlib.Geometry.Manifold.Sheaf.LocallyRingedSpace
 import Mathlib.Geometry.Manifold.Sheaf.Smooth
