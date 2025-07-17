ccfcd4a84e12de57f58748cbba3714dfdb50550c
Junyan Xu <junyanxu.math@gmail.com>
2025-07-17 03:19:30 +0200

    shake

--- Mathlib/Algebra/GroupWithZero/NonZeroDivisors.lean
+++ Mathlib/Algebra/GroupWithZero/NonZeroDivisors.lean
@@ -3,10 +3,9 @@ Copyright (c) 2020 Kenny Lau. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kenny Lau, Devon Tuma, Oliver Nash
 -/
-import Mathlib.Algebra.Group.Action.Opposite
+
 import Mathlib.Algebra.Group.Submonoid.Membership
 import Mathlib.Algebra.GroupWithZero.Associated
-import Mathlib.Algebra.GroupWithZero.Opposite
 
 /-!
 # Non-zero divisors and smul-divisors
