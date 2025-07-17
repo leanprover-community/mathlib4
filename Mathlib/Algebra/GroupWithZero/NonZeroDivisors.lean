20aca35ee4655ee9dab1f7f1c75ff7aaa850ebca
Junyan Xu <junyanxu.math@gmail.com>
2025-07-17 03:20:33 +0200

    blank line

--- Mathlib/Algebra/GroupWithZero/NonZeroDivisors.lean
+++ Mathlib/Algebra/GroupWithZero/NonZeroDivisors.lean
@@ -3,7 +3,6 @@ Copyright (c) 2020 Kenny Lau. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kenny Lau, Devon Tuma, Oliver Nash
 -/
-
 import Mathlib.Algebra.Group.Submonoid.Membership
 import Mathlib.Algebra.GroupWithZero.Associated
 
