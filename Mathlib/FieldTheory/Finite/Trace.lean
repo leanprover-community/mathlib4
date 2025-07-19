0b417bf0ebca5d52939b21fce5168318fe5c9ebd
Kenny Lau <kc_kennylau@yahoo.com.hk>
2025-07-19 13:39:49 +0100

    change proofs

--- Mathlib/FieldTheory/Finite/Trace.lean
+++ Mathlib/FieldTheory/Finite/Trace.lean
@@ -42,18 +42,18 @@ theorem algebraMap_trace_eq_sum_pow (K L : Type*) [Field K] [Field L]
     [Fintype K] [Finite L] [Algebra K L] (x : L) :
     algebraMap K L (Algebra.trace K L x) =
       ∑ i ∈ Finset.range (Module.finrank K L), x ^ (Fintype.card K ^ i) := by
-  rw [trace_eq_sum_automorphisms,
-    ← Fintype.sum_bijective _ (bijective_frobeniusAlgEquivOfAlgebraic_pow K L) _ _ fun _ ↦ rfl]
-  simp_rw [Finset.sum_range, AlgEquiv.coe_pow, coe_frobeniusAlgEquivOfAlgebraic_iterate]
+  rw [trace_eq_sum_automorphisms, Finset.sum_range]
+  exact Eq.symm <| Fintype.sum_bijective _ (bijective_frobeniusAlgEquivOfAlgebraic_pow K L) _ _ <|
+    fun i ↦ by rw [AlgEquiv.coe_pow, coe_frobeniusAlgEquivOfAlgebraic_iterate]
 
 /-- An explicit formula for the norm map: `norm[L/K](x) = ∏ i < [L:K], x ^ ((#K) ^ i)`. -/
 theorem algebraMap_norm_eq_prod_pow (K L : Type*) [Field K] [Field L]
     [Fintype K] [Finite L] [Algebra K L] (x : L) :
     algebraMap K L (Algebra.norm K x) =
       ∏ i ∈ Finset.range (Module.finrank K L), x ^ (Fintype.card K ^ i) := by
-  rw [Algebra.norm_eq_prod_automorphisms,
-    ← Fintype.prod_bijective _ (bijective_frobeniusAlgEquivOfAlgebraic_pow K L) _ _ fun _ ↦ rfl]
-  simp_rw [Finset.prod_range, AlgEquiv.coe_pow, coe_frobeniusAlgEquivOfAlgebraic_iterate]
+  rw [Algebra.norm_eq_prod_automorphisms, Finset.prod_range]
+  exact Eq.symm <| Fintype.prod_bijective _ (bijective_frobeniusAlgEquivOfAlgebraic_pow K L) _ _ <|
+    fun i ↦ by rw [AlgEquiv.coe_pow, coe_frobeniusAlgEquivOfAlgebraic_iterate]
 
 /-- An explicit formula for the norm map: `norm[L/K](x) = x ^ (∑ i < [L:K], (#K) ^ i)`. -/
 theorem algebraMap_norm_eq_pow_sum (K L : Type*) [Field K] [Field L]
