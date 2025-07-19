74a643e12667a26098a48aea5d41f354b9e9de85
Paul-Lez <paul.lezeau@gmail.com>
2025-07-19 21:51:13 +0100

    Apply review suggestions

--- Mathlib/Tactic/Simproc/Divisors.lean
+++ Mathlib/Tactic/Simproc/Divisors.lean
@@ -15,20 +15,18 @@ This file implements (d)simprocs to compute various objects related to divisors:
 
 -/
 
-open Lean Meta Qq
+open Lean Meta Simp Qq
 
 /-- The `Nat.divisorsEq` computes the finset `Nat.divisors n` when `n` is a natural number
 literal. -/
 dsimproc_decl Nat.divisors_ofNat (Nat.divisors _) := fun e => do
   unless e.isAppOfArity `Nat.divisors 1 do return .continue
   let some n ← fromExpr? e.appArg! | return .continue
-  return mkSetLiteralQ q(Finset ℕ) <| (n.divisors.sort (· ≤ ·)).map fun n => (Lean.toExpr n : Q(ℕ))
+  return unsafe .done <| mkSetLiteralQ q(Finset ℕ) <| (n.divisors.val.unquot.map mkNatLit)
 
 /-- Compute the finset `Nat.properDivisorsEq n` when `n` is a numeral;
 for instance, this simplifies `Nat.properDivisorsEq 12` to `{1, 2, 3, 4, 6}`. -/
 dsimproc_decl Nat.properDivisors_ofNat (Nat.properDivisors _) := fun e => do
   unless e.isAppOfArity `Nat.properDivisors 1 do return .continue
   let some n ← fromExpr? e.appArg! | return .continue
-  let rhsListQ : List Q(ℕ) := (n.properDivisors.sort (· ≤ ·)).map fun n => (Lean.toExpr n : Q(ℕ))
-  let rhs := mkSetLiteralQ q(Finset ℕ) rhsListQ
-  return .done rhs
+  return unsafe .done <| mkSetLiteralQ q(Finset ℕ) <| (n.properDivisors.val.unquot.map mkNatLit)
