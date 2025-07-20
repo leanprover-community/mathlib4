2767882f2aa5c0f53615e30e94d2922c3f0e6de6
Tristan F. <LeoDog896@hotmail.com>
2025-07-20 10:36:49 -0700

    chore: mv variables up

--- Mathlib/Data/Fintype/Quotient.lean
+++ Mathlib/Data/Fintype/Quotient.lean
@@ -138,24 +138,17 @@ section
 
 variable {ι₁ : Type*} [Fintype ι₁] [DecidableEq ι₁] {ι₂ : Type*} [Fintype ι₂] [DecidableEq ι₂]
   {α : ι₁ → Sort*} {S₁ : ∀ i, Setoid (α i)} {β : ι₂ → Sort*} {S₂ : ∀ i, Setoid (β i)} {φ : Sort*}
-  (q₁ : ∀ i, Quotient (S₁ i)) (q₂ : ∀ i, Quotient (S₂ i))
+  (q₁ : ∀ i, Quotient (S₁ i)) (q₂ : ∀ i, Quotient (S₂ i)) (f : (∀ i, α i) → (∀ i, β i) → φ)
+  (c : ∀ (a₁ : ∀ i, α i) (b₁ : ∀ i, β i) (a₂ : ∀ i, α i) (b₂ : ∀ i, β i),
+    (∀ i, a₁ i ≈ a₂ i) → (∀ i, b₁ i ≈ b₂ i) → f a₁ b₁ = f a₂ b₂)
 
 /-- Lift a binary function from its finitely indexed types `∀ i : ι₁, α i`, `∀ i : ι₂, β i` to
 a binary function on quotients. This is analogous to the combination of `Quotient.finLiftOn`
 and `Quotient.liftOn₂`. -/
-def finLiftOn₂ (q₁ : ∀ i, Quotient (S₁ i)) (q₂ : ∀ i, Quotient (S₂ i))
-    (f : (∀ i, α i) → (∀ i, β i) → φ)
-    (c : ∀ (a₁ : ∀ i, α i) (b₁ : ∀ i, β i) (a₂ : ∀ i, α i) (b₂ : ∀ i, β i),
-        (∀ i, a₁ i ≈ a₂ i) → (∀ i, b₁ i ≈ b₂ i) → f a₁ b₁ = f a₂ b₂) :
-      φ :=
-  Quotient.liftOn₂ (finChoice q₁) (finChoice q₂) f c
+def finLiftOn₂ : φ := Quotient.liftOn₂ (finChoice q₁) (finChoice q₂) f c
 
 @[simp]
-lemma finLiftOn₂_mk (f : (∀ i, α i) → (∀ i, β i) → φ)
-    (h : ∀ (a₁ : ∀ i, α i) (b₁ : ∀ i, β i) (a₂ : ∀ i, α i) (b₂ : ∀ i, β i),
-      (∀ i, a₁ i ≈ a₂ i) → (∀ i, b₁ i ≈ b₂ i) → f a₁ b₁ = f a₂ b₂)
-    (a : ∀ i, α i) (b : ∀ i, β i) :
-    finLiftOn₂ (⟦a ·⟧) (⟦b ·⟧) f h = f a b := by
+lemma finLiftOn₂_mk (a : ∀ i, α i) (b : ∀ i, β i) : finLiftOn₂ (⟦a ·⟧) (⟦b ·⟧) f c = f a b := by
   simp_rw [finLiftOn₂, finChoice_eq, lift_mk]
 
 end
