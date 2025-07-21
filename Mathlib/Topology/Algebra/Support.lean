bd397144f777ddbfe18a4cf041a40d2ed1967e1c
Luigi Massacci <luigi.massacci.it@gmail.com>
2025-07-21 12:02:48 +0200

    HasCompactSupport.div lemma

--- Mathlib/Topology/Algebra/Support.lean
+++ Mathlib/Topology/Algebra/Support.lean
@@ -333,6 +333,13 @@ protected lemma HasCompactMulSupport.inv' {α β : Type*} [TopologicalSpace α] 
     HasCompactMulSupport (f⁻¹) := by
   simpa only [HasCompactMulSupport, mulTSupport, mulSupport_inv'] using hf
 
+@[to_additive]
+theorem HasCompactSupport.div {α β : Type*} [TopologicalSpace α] [DivisionMonoid β]
+    {f f' : α → β} (hf : HasCompactMulSupport f) (hf' : HasCompactMulSupport f') :
+    HasCompactMulSupport (f / f') :=
+  div_eq_mul_inv f f' ▸ hf.mul hf'.inv'
+
+
 end DivisionMonoid
 
 section SMulZeroClass
