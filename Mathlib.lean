36f9b7c961e25109936bae3cc0afad7afade5f35
Amelia Livingston <al3717@ic.ac.uk>
2025-07-22 22:52:58 +0100

    add file

--- Mathlib.lean
+++ Mathlib.lean
@@ -5134,6 +5134,7 @@ import Mathlib.RepresentationTheory.Homological.GroupCohomology.LongExactSequenc
 import Mathlib.RepresentationTheory.Homological.GroupCohomology.LowDegree
 import Mathlib.RepresentationTheory.Homological.GroupCohomology.Shapiro
 import Mathlib.RepresentationTheory.Homological.GroupHomology.Basic
+import Mathlib.RepresentationTheory.Homological.GroupHomology.FiniteCyclic
 import Mathlib.RepresentationTheory.Homological.GroupHomology.Functoriality
 import Mathlib.RepresentationTheory.Homological.GroupHomology.LowDegree
 import Mathlib.RepresentationTheory.Homological.GroupHomology.Shapiro
