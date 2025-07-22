80a8f4138e504a88cd8c60470b0b8d51a15c52ca
Scott Carnahan <128885296+ScottCarnahan@users.noreply.github.com>
2025-07-22 09:58:46 +0900

    split file, fix imports

--- Mathlib.lean
+++ Mathlib.lean
@@ -856,6 +856,7 @@ import Mathlib.Algebra.Order.GroupWithZero.Unbundled.OrderIso
 import Mathlib.Algebra.Order.GroupWithZero.WithZero
 import Mathlib.Algebra.Order.Hom.Basic
 import Mathlib.Algebra.Order.Hom.Monoid
+import Mathlib.Algebra.Order.Hom.MonoidWithZero
 import Mathlib.Algebra.Order.Hom.Ring
 import Mathlib.Algebra.Order.Interval.Basic
 import Mathlib.Algebra.Order.Interval.Finset.Basic
