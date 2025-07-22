c2d75579c366536fd07428295fb2749b223e7273
Bolton Bailey <bolton.bailey@gmail.com>
2025-07-22 17:57:31 -0400

    feat(Data/List/DropRight): add reverse and append lemmas for rdropWhile and rtakeWhile

--- Mathlib/Data/List/DropRight.lean
+++ Mathlib/Data/List/DropRight.lean
@@ -133,6 +133,9 @@ theorem dropWhile_idempotent : dropWhile p (dropWhile p l) = dropWhile p l := by
 theorem rdropWhile_idempotent : rdropWhile p (rdropWhile p l) = rdropWhile p l :=
   rdropWhile_eq_self_iff.mpr (rdropWhile_last_not _ _)
 
+theorem rdropWhile_reverse : l.reverse.rdropWhile p = (l.dropWhile p).reverse := by
+  simp_rw [rdropWhile, reverse_reverse]
+
 /-- Take elements from the tail end of a list that satisfy `p : α → Bool`.
 Implemented naively via `List.reverse` -/
 def rtakeWhile : List α :=
@@ -175,6 +178,14 @@ theorem mem_rtakeWhile_imp {x : α} (hx : x ∈ rtakeWhile p l) : p x := by
 theorem rtakeWhile_idempotent (p : α → Bool) (l : List α) :
     rtakeWhile p (rtakeWhile p l) = rtakeWhile p l :=
   rtakeWhile_eq_self_iff.mpr fun _ => mem_rtakeWhile_imp
+
+theorem rtakeWhile_reverse : l.reverse.rtakeWhile p = (l.takeWhile p).reverse := by
+  simp_rw [rtakeWhile, reverse_reverse]
+
+theorem rdropWhile_append_rtakeWhile :
+    l.rdropWhile p ++ l.rtakeWhile p = l := by
+  simp only [rdropWhile, rtakeWhile]
+  rw [← List.reverse_append, takeWhile_append_dropWhile, reverse_reverse]
 
 lemma rdrop_add (i j : ℕ) : (l.rdrop i).rdrop j = l.rdrop (i + j) := by
   simp_rw [rdrop_eq_reverse_drop_reverse, reverse_reverse, drop_drop]
