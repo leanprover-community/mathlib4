93cd10b5a9072ecac2dcc3065a978251458ad2b4
Komyyy <pol_tta@outlook.jp>
2025-07-22 15:31:24 +0900

    feat: `norm_num` extentions for Ordinals

--- Mathlib.lean
+++ Mathlib.lean
@@ -5945,6 +5945,7 @@ import Mathlib.Tactic.NormNum.NatFib
 import Mathlib.Tactic.NormNum.NatLog
 import Mathlib.Tactic.NormNum.NatSqrt
 import Mathlib.Tactic.NormNum.OfScientific
+import Mathlib.Tactic.NormNum.Ordinal
 import Mathlib.Tactic.NormNum.Pow
 import Mathlib.Tactic.NormNum.PowMod
 import Mathlib.Tactic.NormNum.Prime
