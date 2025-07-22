f1a92a38b2f279f32599ad03e2f51e62a5eef56b
Emily Riehl <eriehl@jhu.edu>
2025-07-22 07:13:10 +0000

    Feat(CategoryTheory): terminal categories with an application to hoFunctor (#25781)
    Some preliminary API around terminal categories is developed with an application for the infinity-cosmos project: proving that the homotopy category functor preserves terminal objects. We also include proofs that `[0]` is terminal in the simplex category and `Δ[0]` is the terminal simplicial set.
    
    Co-authored by [Robin Carlier](robin.carlier@ens-lyon.fr), [Jakob von Raumer](https://github.com/javra), [Adam Topaz](topaz@ualberta.ca), and [Zeyi Zhao](zzhao95@jh.edu).
    
    
    
    Co-authored-by: Joël Riou <37772949+joelriou@users.noreply.github.com>
    Co-authored-by: thorimur <68410468+thorimur@users.noreply.github.com>
    Co-authored-by: Robin Carlier <57142648+robin-carlier@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1992,6 +1992,7 @@ import Mathlib.CategoryTheory.Category.Cat.CartesianClosed
 import Mathlib.CategoryTheory.Category.Cat.Colimit
 import Mathlib.CategoryTheory.Category.Cat.Limit
 import Mathlib.CategoryTheory.Category.Cat.Op
+import Mathlib.CategoryTheory.Category.Cat.Terminal
 import Mathlib.CategoryTheory.Category.Factorisation
 import Mathlib.CategoryTheory.Category.GaloisConnection
 import Mathlib.CategoryTheory.Category.Grpd
