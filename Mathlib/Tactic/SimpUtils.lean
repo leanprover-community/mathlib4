89f4e102afd14f573b34e980447cfe3eae9b0d8b
Paul-Lez <paul.lezeau@gmail.com>
2025-07-23 14:10:00 +0100

    Add header

--- Mathlib/Tactic/SimpUtils.lean
+++ Mathlib/Tactic/SimpUtils.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2025 Paul Lezeau. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Paul Lezeau
+-/
 import Mathlib.Init
 import Lean.Elab.Tactic.Simproc
 
