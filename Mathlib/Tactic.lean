f25f4dc3bdf0005b1018928a2ef231830298ebfa
Paul-Lez <paul.lezeau@gmail.com>
2025-07-23 14:08:39 +0100

    feat: add simproc finding commands

--- Mathlib/Tactic.lean
+++ Mathlib/Tactic.lean
@@ -281,6 +281,7 @@ public import Mathlib.Tactic.SetLike
 public import Mathlib.Tactic.SetNotationForOrder
 public import Mathlib.Tactic.SimpIntro
 public import Mathlib.Tactic.SimpRw
+public import Mathlib.Tactic.SimpUtils
 public import Mathlib.Tactic.Simproc.Divisors
 public import Mathlib.Tactic.Simproc.ExistsAndEq
 public import Mathlib.Tactic.Simproc.Factors
