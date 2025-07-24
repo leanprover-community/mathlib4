2d70e5ec1e80b1a27a3e56655d0da4aa37c6ec72
Aaron Liu <aaronliu2008@outlook.com>
2025-07-24 14:15:41 +0000

    feat(Abelianization): `lift_symm_apply` (#27423)
    Adds a `@[simp]` theorem `lift_symm_apply` and a regular theorem `coe_lift_symm`.

--- Mathlib/GroupTheory/Abelianization/Defs.lean
+++ Mathlib/GroupTheory/Abelianization/Defs.lean
@@ -92,6 +92,11 @@ theorem lift_apply_of (x : G) : lift f (of x) = f x :=
 @[deprecated (since := "2025-07-23")]
 alias lift.of := lift_apply_of
 
+theorem coe_lift_symm : (lift.symm : (Abelianization G →* A) → (G →* A)) = (·.comp of) := rfl
+
+@[simp]
+theorem lift_symm_apply (f : Abelianization G →* A) : lift.symm f = f.comp of := rfl
+
 theorem lift_unique (φ : Abelianization G →* A)
     -- hφ : φ agrees with f on the image of G in Gᵃᵇ
     (hφ : ∀ x : G, φ (Abelianization.of x) = f x)
