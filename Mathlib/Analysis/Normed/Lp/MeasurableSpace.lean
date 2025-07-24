43f7c8def077ac97e59db0323ceb96929c88a118
Etienne Marion <baguettes_casino0c@icloud.com>
2025-07-24 21:52:07 +0200

    measurable space

--- Mathlib/Analysis/Normed/Lp/MeasurableSpace.lean
+++ Mathlib/Analysis/Normed/Lp/MeasurableSpace.lean
@@ -5,6 +5,7 @@ Authors: Etienne Marion
 -/
 import Mathlib.Analysis.Normed.Lp.PiLp
 import Mathlib.MeasureTheory.Constructions.BorelSpace.Basic
+import Mathlib.MeasureTheory.Constructions.Polish.Basic
 
 /-!
 # Measurable space structure on `WithLp`
@@ -20,25 +21,29 @@ variable (p : ℝ≥0∞) (X : Type*) [MeasurableSpace X]
 namespace WithLp
 
 instance measurableSpace : MeasurableSpace (WithLp p X) :=
-  MeasurableSpace.map (WithLp.toLp p) inferInstance
+  MeasurableSpace.comap WithLp.ofLp inferInstance
 
 @[fun_prop, measurability]
-lemma measurable_ofLp : Measurable (@WithLp.ofLp p X) := fun _ h ↦ h
+lemma measurable_ofLp : Measurable (@WithLp.ofLp p X) := comap_measurable _
 
 @[deprecated measurable_ofLp (since := "2024-04-27")]
-lemma measurable_equiv : Measurable (WithLp.equiv p X) := fun _ h ↦ h
+lemma measurable_equiv : Measurable (WithLp.equiv p X) := measurable_ofLp p X
 
 @[fun_prop, measurability]
-lemma measurable_toLp : Measurable (@WithLp.toLp p X) := fun _ h ↦ h
+lemma measurable_toLp : Measurable (@WithLp.toLp p X) := fun s hs ↦ by
+  obtain ⟨t, ht, rfl⟩ := hs
+  simpa [Set.preimage_preimage]
 
 @[deprecated measurable_toLp (since := "2024-04-27")]
-lemma measurable_equiv_symm : Measurable (WithLp.equiv p X).symm := fun _ h ↦ h
+lemma measurable_equiv_symm : Measurable (WithLp.equiv p X).symm := measurable_toLp p X
 
 variable (Y : Type*) [MeasurableSpace Y] [TopologicalSpace X] [TopologicalSpace Y]
   [BorelSpace X] [BorelSpace Y] [SecondCountableTopologyEither X Y]
 
-instance borelSpace : BorelSpace (WithLp p (X × Y)) :=
-  inferInstanceAs <| BorelSpace (X × Y)
+instance borelSpace : BorelSpace (WithLp p (X × Y)) where
+  measurable_eq := by
+    rw [instProdTopologicalSpace, borel_comap, measurableSpace,
+      BorelSpace.measurable_eq (α := X × Y)]
 
 end WithLp
 
@@ -47,8 +52,10 @@ namespace PiLp
 variable {ι : Type*} {X : ι → Type*} [Countable ι] [∀ i, MeasurableSpace (X i)]
     [∀ i, TopologicalSpace (X i)] [∀ i, BorelSpace (X i)] [∀ i, SecondCountableTopology (X i)]
 
-instance borelSpace : BorelSpace (PiLp p X) :=
-  inferInstanceAs <| BorelSpace (Π i, X i)
+instance borelSpace : BorelSpace (PiLp p X) where
+  measurable_eq := by
+    rw [topologicalSpace, borel_comap, WithLp.measurableSpace,
+      BorelSpace.measurable_eq (α := Π i, X i)]
 
 end PiLp
 
@@ -57,8 +64,8 @@ namespace MeasurableEquiv
 /-- The map from `X` to `WithLp p X` as a measurable equivalence. -/
 protected def toLp : X ≃ᵐ (WithLp p X) where
   toEquiv := (WithLp.equiv p X).symm
-  measurable_toFun := measurable_id
-  measurable_invFun := measurable_id
+  measurable_toFun := WithLp.measurable_toLp p X
+  measurable_invFun := WithLp.measurable_ofLp p X
 
 lemma coe_toLp : ⇑(MeasurableEquiv.toLp p X) = WithLp.toLp p := rfl
 
