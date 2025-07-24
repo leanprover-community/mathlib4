cade20adbfee162c94554e940d27518eebc67e0a
Scott Carnahan <128885296+ScottCarnahan@users.noreply.github.com>
2025-07-25 04:08:54 +0900

    fix docstring

--- Mathlib/Algebra/Group/Action/Units.lean
+++ Mathlib/Algebra/Group/Action/Units.lean
@@ -75,8 +75,9 @@ instance mulAction' [Group G] [Monoid M] [MulAction G M] [SMulCommClass G M M]
   one_smul _ := Units.ext <| one_smul _ _
   mul_smul _ _ _ := Units.ext <| mul_smul _ _ _
 
-/-- This is not the usual `smul_eq_mul` because `mulAction'` creates a diamond. See zulip:
-https://leanprover.zulipchat.com/#narrow/channel/113488-general/topic/units.2Emul_action'.20diamond/near/246400399 -/
+/-- This is not the usual `smul_eq_mul` because `mulAction'` creates a diamond.
+
+Discussed [on Zulip](https://leanprover.zulipchat.com/#narrow/channel/113488-general/topic/units.2Emul_action'.20diamond/near/246400399). -/
 @[simp]
 lemma smul_eq_mul {M} [CommMonoid M] (u₁ u₂ : Mˣ) :
     u₁ • u₂ = u₁ * u₂ := by
