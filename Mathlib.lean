181dceb3834a2abb1e54dd69ed0af75a1e5c793e
Christian Merten <christian@merten.dev>
2025-07-24 07:37:58 +0000

    feat(AlgebraicGeometry): codescent implies descent (#23547)
    Let `P` and `P'` be morphism properties of schemes. We show some results to deduce
    that `P` descends along `P'` from a codescent property of ring homomorphisms.
    
    
    
    Co-authored-by: Christian Merten <136261474+chrisflav@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1182,6 +1182,7 @@ import Mathlib.AlgebraicGeometry.Morphisms.AffineAnd
 import Mathlib.AlgebraicGeometry.Morphisms.Basic
 import Mathlib.AlgebraicGeometry.Morphisms.ClosedImmersion
 import Mathlib.AlgebraicGeometry.Morphisms.Constructors
+import Mathlib.AlgebraicGeometry.Morphisms.Descent
 import Mathlib.AlgebraicGeometry.Morphisms.Etale
 import Mathlib.AlgebraicGeometry.Morphisms.Finite
 import Mathlib.AlgebraicGeometry.Morphisms.FinitePresentation
