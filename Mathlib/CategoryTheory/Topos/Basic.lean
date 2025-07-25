2796598d8da42aa4534e4b9ce23ae4a742aef05c
Total Noob <klausgy@gmail.com>
2025-07-25 15:50:07 +0200

    modularized api, pointwise powerobject constructor

--- Mathlib/CategoryTheory/Topos/Basic.lean
+++ Mathlib/CategoryTheory/Topos/Basic.lean
@@ -36,74 +36,85 @@ def WhiskeredHom (B C : ℰ) : ℰᵒᵖ ⥤ Type v :=
 def IsPowerObjectOf (hc : Classifier ℰ (𝟙_ ℰ)) (B P : ℰ) :=
   (WhiskeredHom B hc.Ω).RepresentableBy P
 
-variable (ℰ)
+namespace PowerObject
 
-/-- An elementary topos is a category with a fixed subobject classifier and power objects. -/
-class ElementaryTopos [HasPullbacks ℰ] where
-  /-- A fixed choice of subobject classifier in `ℰ`. -/
-  hc : Classifier ℰ (𝟙_ ℰ)
-  /-- Every `B` has a power object `P B`. -/
-  P (B : ℰ) : ℰ
-  /-- `P B` is a power object of `B`. -/
-  hP (B : ℰ) : IsPowerObjectOf hc B (P B)
-
-namespace ElementaryTopos
-
-variable {ℰ} [HasPullbacks ℰ] [ElementaryTopos ℰ]
+variable {sc : Classifier ℰ (𝟙_ ℰ)} {B PB : ℰ} (hPB : IsPowerObjectOf sc B PB)
 
 /-- The P-transpose of a morphism `g : A ⟶ P B`. -/
-def hat {A : ℰ} (B : ℰ) (g : A ⟶ P B) : B ⊗ A ⟶ hc.Ω :=
-  (hP B).homEquiv.toFun g
+def hat {A : ℰ} (g : A ⟶ PB) : B ⊗ A ⟶ sc.Ω :=
+  hPB.homEquiv.toFun g
 
 /-- The P-transpose of a morphism `f : B × A ⟶ Ω`. -/
-def unhat {A B : ℰ} (f : B ⊗ A ⟶ hc.Ω) : (A ⟶ P B) :=
-  (hP B).homEquiv.invFun f
+def unhat {A : ℰ} (f : B ⊗ A ⟶ sc.Ω) : (A ⟶ PB) :=
+  hPB.homEquiv.invFun f
 
 @[simp]
-lemma hat_unhat {A B : ℰ} (f : B ⊗ A ⟶ hc.Ω) :
-  hat B (unhat f) = f := (hP B).homEquiv.apply_symm_apply f
+lemma hat_unhat {A : ℰ} (f : B ⊗ A ⟶ sc.Ω) :
+  hat hPB (unhat hPB f) = f := hPB.homEquiv.apply_symm_apply f
 
 @[simp]
-lemma unhat_hat {A B : ℰ} (g : A ⟶ P B) :
-  unhat (hat B g) = g := (hP B).homEquiv.symm_apply_apply g
+lemma unhat_hat {A : ℰ} (g : A ⟶ PB) :
+  unhat hPB (hat hPB g) = g := hPB.homEquiv.symm_apply_apply g
 
 /-- The element relation as a subobject of `B ⨯ (P B)`. -/
-def ε_ (B : ℰ) : B ⊗ (P B) ⟶ hc.Ω :=
-  (hP B).homEquiv.toFun (𝟙 (P B))
+def ε_ : B ⊗ (PB) ⟶ sc.Ω := hPB.homEquiv.toFun (𝟙 (PB))
 
 @[simp]
-lemma comm {A B : ℰ} (f : B ⊗ A ⟶ hc.Ω) : (B ◁ unhat f) ≫ ε_ B = f := by
-  have : (hP B).homEquiv (unhat f) = f := by unfold unhat; simp
-  simpa [this] using Eq.symm (RepresentableBy.homEquiv_eq (hP B) (unhat f))
+lemma comm {A : ℰ} (f : B ⊗ A ⟶ sc.Ω) : (B ◁ unhat hPB f) ≫ ε_ hPB = f := by
+  have : hPB.homEquiv (unhat hPB f) = f := by unfold unhat; simp
+  simpa [this] using Eq.symm (RepresentableBy.homEquiv_eq hPB (unhat hPB f))
 
-lemma uniq {A B : ℰ} (f : B ⊗ A ⟶ hc.Ω) (g : A ⟶ P B)
-    (h : f = (B ◁ g) ≫ ε_ B) : g = unhat f := by
-  have : hat B g = f := by rw [← comm (hat B g)]; simp [h]
-  simpa using congr(unhat $this)
+lemma uniq {A : ℰ} (f : B ⊗ A ⟶ sc.Ω) (g : A ⟶ PB)
+    (h : f = (B ◁ g) ≫ ε_ hPB) : g = unhat hPB f := by
+  have : hat hPB g = f := by rw [← comm hPB (hat hPB g)]; simp [h]
+  simpa using congr(unhat hPB $this)
+
+variable {C PC : ℰ} (hPC : IsPowerObjectOf sc C PC)
 
 /-- The morphism `P_morph h` is the functorial action on a morphism `h : B ⟶ C`,
     defined as the P-transpose of `ε_C ∘ (h ⨯ 𝟙)`. -/
-def P_morph {B C : ℰ} (h : B ⟶ C) : P C ⟶ P B := unhat ((h ▷ P C) ≫ ε_ C)
+def P_morph (h : B ⟶ C) : PC ⟶ PB := unhat hPB ((h ▷ PC) ≫ ε_ hPC)
 
 /-- Naturality (dinaturality) of `ε`. This corresponds to the naturality square of ε
     in MM92 diagram (5). -/
-lemma ε_dinaturality {B C : ℰ} (h : B ⟶ C) :
-  (h ▷ P C) ≫ ε_ C = (B ◁ (P_morph h)) ≫ ε_ B := Eq.symm (comm _)
+lemma ε_dinaturality (h : B ⟶ C) :
+  (h ▷ PC) ≫ ε_ hPC = (B ◁ (P_morph hPB hPC h)) ≫ ε_ hPB := Eq.symm (comm hPB _)
 
 /-- `P` covariantly preserves composition, shown by stacking dinaturality squares. -/
-private lemma P_compose {B C D : ℰ} (h : B ⟶ C) (h' : C ⟶ D) :
-    P_morph (h ≫ h') = P_morph h' ≫ P_morph h := by
-  let comm_outer : (h ▷ P D) ≫ (h' ▷ P D) ≫ ε_ D =
-      (B ◁ (P_morph h')) ≫ (B ◁ (P_morph h)) ≫ ε_ B := by
-    rw [ε_dinaturality h', ← reassoc_of% whisker_exchange h, ε_dinaturality h]
-  rw [P_morph]; simp; rw[comm_outer, ← uniq _ (P_morph h' ≫ P_morph h) (by aesop_cat)]
+private lemma P_compose {D PD : ℰ} (hPD : IsPowerObjectOf sc D PD) (h : B ⟶ C) (h' : C ⟶ D) :
+    P_morph hPB hPD (h ≫ h') = P_morph hPC hPD h' ≫ P_morph hPB hPC h := by
+  let comm_outer : (h ▷ PD) ≫ (h' ▷ PD) ≫ ε_ hPD =
+      (B ◁ (P_morph _ _ h')) ≫ (B ◁ (P_morph _ _ h)) ≫ ε_ hPB := by
+    rw [ε_dinaturality hPC hPD h', ← reassoc_of% whisker_exchange h, ε_dinaturality hPB hPC h]
+  rw [P_morph]; simp
+  rw[comm_outer, ← uniq _ _ (P_morph _ _ h' ≫ P_morph _ _ h) (by aesop_cat)]
 
-/-- The power object functor `P : ℰᵒᵖ ⥤ ℰ` defined from `P` and `P_morph`. -/
-def P_functor : ℰᵒᵖ ⥤ ℰ := {
-  obj B := P (unop B),
-  map h := P_morph h.unop,
-  map_id B := Eq.symm (uniq _ _ (by aesop_cat)),
-  map_comp {B C D : ℰᵒᵖ} (h : B ⟶ C) (h' : C ⟶ D) := P_compose h'.unop h.unop
-}
+end PowerObject
+
+variable (ℰ) [HasPullbacks ℰ]
+
+/-- An elementary topos is a category with a fixed subobject classifier and power objects. -/
+class ElementaryTopos where
+  /-- A fixed choice of subobject classifier in `ℰ`. -/
+  sc : Classifier ℰ (𝟙_ ℰ)
+  /-- Every `B` has a power object `P B`. -/
+  P : ℰᵒᵖ ⥤ ℰ
+  hP (B : ℰ) : IsPowerObjectOf sc B (P.obj (op B))
+
+namespace ElementaryTopos
+
+open PowerObject
+
+/-- Construct an elementary topos pointwise defined power objects. -/
+def mkFromPointwisePowerObjects (sc : Classifier ℰ (𝟙_ ℰ))
+    (P' : ℰ → ℰ) (hP : ∀ B : ℰ, IsPowerObjectOf sc B (P' B)) : ElementaryTopos ℰ :=
+  { sc := sc
+    P :=
+    { obj B := P' B.unop,
+      map {B C : ℰᵒᵖ} (h : B ⟶ C) := P_morph (hP C.unop) (hP B.unop) h.unop,
+      map_id B := Eq.symm (uniq (hP B.unop) _ _ (by simp)),
+      map_comp {B C D : ℰᵒᵖ} (h : B ⟶ C) (h' : C ⟶ D) :=
+        P_compose (hP D.unop) (hP C.unop) (hP B.unop) h'.unop h.unop }
+    hP B := hP B }
 
 end ElementaryTopos
