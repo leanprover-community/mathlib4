fb3c04e2ce71d5fb3a4417cbaaa24b0d9b18bced
thorimur <68410468+thorimur@users.noreply.github.com>
2025-07-25 15:19:15 -0400

    even more tweaks

--- Mathlib/CategoryTheory/Equivalence.lean
+++ Mathlib/CategoryTheory/Equivalence.lean
@@ -62,13 +62,15 @@ open CategoryTheory.Functor NatIso Category
 -- declare the `v`'s first; see `CategoryTheory.Category` for an explanation
 universe v₁ v₂ v₃ u₁ u₂ u₃
 
-/-- An equivalence of categories. We define an equivalence as a half-adjoint equivalence: a pair
-of functors `F : C ⥤ D` and `G : D ⥤ C` with a unit `η : 𝟭 C ≅ F ⋙ G` and counit
-`ε : G ⋙ F ≅ 𝟭 D`. `η` and `ε` are natural isomorphisms that satisfy the triangle law for `F`:
-`Fη ≫ εF = 𝟙 F`. Or, in other words, the composite `F ⟶ FGF ⟶ F` is the identity.
+/-- An equivalence of categories.
+
+We define an equivalence between `C` and `D` as a half-adjoint equivalence: a pair of functors
+`F : C ⥤ D` and `G : D ⥤ C` with a unit `η : 𝟭 C ≅ F ⋙ G` and counit `ε : G ⋙ F ≅ 𝟭 D` such that
+the natural isomorphisms `η` and `ε` satisfy the triangle law for `F`: namely, `Fη ≫ εF = 𝟙 F`. Or,
+in other words, the composite `F ⟶ FGF ⟶ F` is the identity.
 
 In `unit_inverse_comp`, we show that this is sufficient to establish a full adjoint
-equivalence—i.e., that the composite `G ⟶ GFG ⟶ G` must also be the identity.
+equivalence. I.e., the composite `G ⟶ GFG ⟶ G` is also the identity.
 
 The triangle equation `functor_unitIso_comp` is written as a family of equalities between
 morphisms. It is more complicated if we write it as an equality of natural transformations, because
