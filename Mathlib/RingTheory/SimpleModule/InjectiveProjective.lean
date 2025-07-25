22ceadf3c8c07943d710c0373b986e0a0f4269a4
Junyan Xu <junyanxu.math@gmail.com>
2025-07-25 17:46:22 +0100

    fix, rename, deprecate

--- Mathlib/RingTheory/SimpleModule/InjectiveProjective.lean
+++ Mathlib/RingTheory/SimpleModule/InjectiveProjective.lean
@@ -16,12 +16,18 @@ namespace Module
 
 variable (R : Type*) [Ring R] [IsSemisimpleRing R] (M : Type*) [AddCommGroup M] [Module R M]
 
-theorem injective_of_semisimple_ring : Module.Injective R M where
+theorem injective_of_isSemisimpleRing : Module.Injective R M where
   out X Y _ _ _ _ f hf g :=
     let ⟨h, comp⟩ := IsSemisimpleModule.extension_property f hf g
     ⟨h, fun _ ↦ by rw [← comp, LinearMap.comp_apply]⟩
 
-theorem projective_of_semisimple_ring : Module.Projective R M :=
-  Module.Projective.of_lifting_property'' IsSemisimpleModule.lifting_property
+theorem surjective_of_isSemisimpleRing : Module.Projective R M :=
+  Module.Projective.of_lifting_property'' (IsSemisimpleModule.lifting_property · · _)
+
+@[deprecated (since := "2025-07-25")]
+alias injective_of_semisimple_ring := injective_of_isSemisimpleRing
+
+@[deprecated (since := "2025-07-25")]
+alias projective_of_semisimple_ring := surjective_of_isSemisimpleRing
 
 end Module
