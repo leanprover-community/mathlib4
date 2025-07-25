0c63e704eff4e166b893a83a30becb9555e95456
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-07-25 23:51:19 +0200

    chore: more indentation fixes
    Found by the linter in #27473.

--- Mathlib/Data/Matrix/Basic.lean
+++ Mathlib/Data/Matrix/Basic.lean
@@ -296,7 +296,7 @@ lemma entryAddMonoidHom_eq_comp {i : m} {j : n} :
   simp [AddMonoidHom.ext_iff]
 
 @[simp] lemma entryAddMonoidHom_toAddHom {i : m} {j : n} :
-  (entryAddMonoidHom α i j : AddHom _ _) = entryAddHom α i j := rfl
+    (entryAddMonoidHom α i j : AddHom _ _) = entryAddHom α i j := rfl
 
 end AddMonoidHom
 
