e2983238a6645202fe1bfa4b72c50f912b0814bc
Jon Bannon <jbannon@siena.edu>
2025-07-26 23:35:49 +0000

    chore(Algebra/Algebra/Defs): modified `RingHom.toAlgebra'` and `RingHom.toAlgebra` docstrings to reflect the risk of diamonds (#27514)
    Modified the docstring of `RingHom.toAlgebra'` to include a warning that if `S` already has a `SMul R S` 
    instance, then using this result creates another `SMul R S` instance from the supplied `RingHom` and 
    this will likely create a diamond. Also modified `RingHom.toAlgebra` similarly (a bit shorter, since this result calls the primed version, so the warning here says that the call to the primed version may create a diamond.)
    
    
    
    Co-authored-by: Jon Bannon <59937998+JonBannon@users.noreply.github.com>

--- Mathlib/Algebra/Algebra/Defs.lean
+++ Mathlib/Algebra/Algebra/Defs.lean
@@ -162,7 +162,11 @@ end CommRingRing
 end algebraMap
 
 /-- Creating an algebra from a morphism to the center of a semiring.
-See note [reducible non-instances]. -/
+See note [reducible non-instances].
+
+*Warning:* In general this should not be used if `S` already has a `SMul R S`
+instance, since this creates another `SMul R S` instance from the supplied `RingHom` and
+this will likely create a diamond. -/
 abbrev RingHom.toAlgebra' {R S} [CommSemiring R] [Semiring S] (i : R →+* S)
     (h : ∀ c x, i c * x = x * i c) : Algebra R S where
   smul c x := i c * x
@@ -184,7 +188,11 @@ theorem RingHom.algebraMap_toAlgebra' {R S} [CommSemiring R] [Semiring S] (i : R
   rfl
 
 /-- Creating an algebra from a morphism to a commutative semiring.
-See note [reducible non-instances]. -/
+See note [reducible non-instances].
+
+*Warning:* In general this should not be used if `S` already has a `SMul R S`
+instance, since this creates another `SMul R S` instance from the supplied `RingHom` and
+this will likely create a diamond. -/
 abbrev RingHom.toAlgebra {R S} [CommSemiring R] [CommSemiring S] (i : R →+* S) : Algebra R S :=
   i.toAlgebra' fun _ => mul_comm _
 
