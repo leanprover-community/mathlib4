e31a7039bf43d70d21ac96cc1772cf7d5c515432
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2025-07-28 00:45:42 +0100

    authors

--- Mathlib/Util/AtLocation.lean
+++ Mathlib/Util/AtLocation.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2022 Mario Carneiro. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Mario Carneiro
+Authors: Mario Carneiro, Heather Macbeth
 -/
 import Lean.Elab.Tactic.Location
 import Lean.Meta.Tactic.Simp.Main
