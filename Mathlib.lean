79dc348f874f9a30eb489e19dd08abd4552e94eb
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-07-27 15:28:08 +0530

    feat(Algebra/Homology): complementary embeddings

--- Mathlib.lean
+++ Mathlib.lean
@@ -522,6 +522,7 @@ import Mathlib.Algebra.Homology.DerivedCategory.ShortExact
 import Mathlib.Algebra.Homology.DerivedCategory.SingleTriangle
 import Mathlib.Algebra.Homology.DifferentialObject
 import Mathlib.Algebra.Homology.Double
+import Mathlib.Algebra.Homology.Embedding.AreComplementary
 import Mathlib.Algebra.Homology.Embedding.Basic
 import Mathlib.Algebra.Homology.Embedding.Boundary
 import Mathlib.Algebra.Homology.Embedding.CochainComplex
