7eb0f42a618f703d6d52dced67931fe8d7176051
Pierre Quinton <pierre.quinton@gmail.com>
2025-07-27 10:07:24 +0200

    Add instances of Lawful Preorders for any Preorder.

--- Mathlib/Order/LawfulPreorder.lean
+++ Mathlib/Order/LawfulPreorder.lean
@@ -10,9 +10,31 @@ import Mathlib.Order.Defs.PartialOrder
 variable {α β : Type*}
 
 class LawfulSupPreorder (α) extends Preorder α, SupSet α where
-  isLUB_sSup_of_exists_isLUB (s : Set α) : ∃ x, IsLUB s x → IsLUB s (sSup s)
+  isLUB_sSup_of_exists_isLUB (s : Set α) : (∃ x, IsLUB s x) → IsLUB s (sSup s)
+
+open Classical in
+noncomputable instance Preorder.toLawfulSupPreorder [Preorder α] [Inhabited α] :
+    LawfulSupPreorder α where
+  sSup s := if hs : ∃ x, IsLUB s x then Classical.choose hs else default
+  isLUB_sSup_of_exists_isLUB s := by
+    intro ⟨x, hs⟩
+    rw [dif_pos]
+    exact Classical.choose_spec ⟨x, hs⟩
 
 class LawfulInfPreorder (α) extends Preorder α, InfSet α where
-  isGLB_sInf_of_exists_isGLB (s : Set α) : ∃ x, IsGLB s x → IsGLB s (sInf s)
+  isGLB_sInf_of_exists_isGLB (s : Set α) : (∃ x, IsGLB s x) → IsGLB s (sInf s)
+
+open Classical in
+noncomputable instance Preorder.toLawfulInfPreorder [Preorder α] [Inhabited α] :
+    LawfulInfPreorder α where
+  sInf s := if hs : ∃ x, IsGLB s x then Classical.choose hs else default
+  isGLB_sInf_of_exists_isGLB s := by
+    intro ⟨x, hs⟩
+    rw [dif_pos]
+    exact Classical.choose_spec ⟨x, hs⟩
 
 class LawfulPreorder (α) extends LawfulSupPreorder α, LawfulInfPreorder α
+
+open Classical in
+noncomputable instance Preorder.toLawfulPreorder [Preorder α] [Inhabited α] :
+    LawfulPreorder α where
