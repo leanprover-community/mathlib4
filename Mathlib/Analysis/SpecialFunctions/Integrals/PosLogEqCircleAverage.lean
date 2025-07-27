97e92ab7d1df6bcc690f587989a67cc5a6271323
Stefan Kebekus <kebekus@users.noreply.github.com>
2025-07-27 08:00:12 +0200

    Update PosLogEqCircleAverage.lean

--- Mathlib/Analysis/SpecialFunctions/Integrals/PosLogEqCircleAverage.lean
+++ Mathlib/Analysis/SpecialFunctions/Integrals/PosLogEqCircleAverage.lean
@@ -43,12 +43,43 @@ lemma circleIntegrability_log_norm_id_sub_const {c : ℂ} (r : ℝ) :
 ## Computing `circleAverage (log ‖· - a‖) 0 1` in case where `‖a‖ = 1`.
 -/
 
+-- Integral computation used in `circleAverage_log_norm_id_sub_const₁`
+private lemma circleAverage_log_norm_id_sub_const₁_integral :
+    ∫ x in 0..(2 * π), log (4 * sin (x / 2) ^ 2) / 2 = 0 := by
+  calc ∫ x in 0..(2 * π), log (4 * sin (x / 2) ^ 2) / 2
+  _ = ∫ (x : ℝ) in 0..π, log (4 * sin x ^ 2) := by
+    have {x : ℝ} : x / 2 = 2⁻¹ * x := by ring
+    rw [intervalIntegral.integral_div, this, inv_mul_integral_comp_div
+      (f := fun x ↦ log (4 * sin x ^ 2))]
+    simp
+  _ = ∫ (x : ℝ) in 0..π, log 4 + 2 * log (sin x) := by
+    apply integral_congr_codiscreteWithin
+    apply codiscreteWithin.mono (by tauto : Ι 0 π ⊆ Set.univ)
+    have : AnalyticOnNhd ℝ (4 * sin · ^ 2) Set.univ := fun _ _ ↦ by fun_prop
+    have := this.preimage_zero_mem_codiscrete (x := π / 2)
+    simp only [sin_pi_div_two, one_pow, mul_one, ne_eq, OfNat.ofNat_ne_zero, not_false_eq_true,
+      Set.preimage_compl, forall_const] at this
+    filter_upwards [this] with a ha
+    simp only [Set.mem_compl_iff, Set.mem_preimage, Set.mem_singleton_iff, mul_eq_zero,
+      OfNat.ofNat_ne_zero, ne_eq, not_false_eq_true, pow_eq_zero_iff, false_or] at ha
+    rw [log_mul (by norm_num) (by simp_all), log_pow, Nat.cast_ofNat]
+  _ = (∫ (x : ℝ) in 0..π, log 4) + 2 * ∫ (x : ℝ) in 0..π, log (sin x) := by
+    rw [integral_add _root_.intervalIntegrable_const
+      (by apply intervalIntegrable_log_sin.const_mul 2), intervalIntegral.integral_const_mul]
+  _ = 0 := by
+    simp only [intervalIntegral.integral_const, sub_zero, smul_eq_mul, integral_log_sin_zero_pi,
+      (by norm_num : (4 : ℝ) = 2 * 2), log_mul two_ne_zero two_ne_zero]
+    ring
+
 /--
 If `a : ℂ` has norm one, then the circle average `circleAverage (log ‖· - a‖) 0 1` vanishes.
 -/
 @[simp]
 theorem circleAverage_log_norm_id_sub_const₁ (h : ‖a‖ = 1) :
     circleAverage (log ‖· - a‖) 0 1 = 0 := by
+  -- Observing that the problem is rotation invariant, we rotate by an angle of `ζ = - arg a` and
+  -- reduce the problem to the case where `a = 1`. The integral can then be evalutated by a direct
+  -- computation.
   simp only [circleAverage, mul_inv_rev, smul_eq_mul, mul_eq_zero, inv_eq_zero, OfNat.ofNat_ne_zero,
     or_false]
   right
@@ -88,26 +119,4 @@ theorem circleAverage_log_norm_id_sub_const₁ (h : ‖a‖ = 1) :
     _ = 4 * sin (x / 2) ^ 2 := by
       nth_rw 1 [← mul_one 4, ← sin_sq_add_cos_sq (x / 2)]
       ring
-  _ = ∫ (x : ℝ) in 0..π, log (4 * sin x ^ 2) := by
-    have {x : ℝ} : x / 2 = 2⁻¹ * x := by ring
-    rw [intervalIntegral.integral_div, this, inv_mul_integral_comp_div
-      (f := fun x ↦ log (4 * sin x ^ 2))]
-    simp
-  _ = ∫ (x : ℝ) in 0..π, log 4 + 2 * log (sin x) := by
-    apply integral_congr_codiscreteWithin
-    apply codiscreteWithin.mono (by tauto : Ι 0 π ⊆ Set.univ)
-    have : AnalyticOnNhd ℝ (4 * sin · ^ 2) Set.univ := fun _ _ ↦ by fun_prop
-    have := this.preimage_zero_mem_codiscrete (x := π / 2)
-    simp only [sin_pi_div_two, one_pow, mul_one, ne_eq, OfNat.ofNat_ne_zero, not_false_eq_true,
-      Set.preimage_compl, forall_const] at this
-    filter_upwards [this] with a ha
-    simp only [Set.mem_compl_iff, Set.mem_preimage, Set.mem_singleton_iff, mul_eq_zero,
-      OfNat.ofNat_ne_zero, ne_eq, not_false_eq_true, pow_eq_zero_iff, false_or] at ha
-    rw [log_mul (by norm_num) (by simp_all), log_pow, Nat.cast_ofNat]
-  _ = (∫ (x : ℝ) in 0..π, log 4) + 2 * ∫ (x : ℝ) in 0..π, log (sin x) := by
-    rw [integral_add _root_.intervalIntegrable_const
-      (by apply intervalIntegrable_log_sin.const_mul 2), intervalIntegral.integral_const_mul]
-  _ = 0 := by
-    simp only [intervalIntegral.integral_const, sub_zero, smul_eq_mul, integral_log_sin_zero_pi,
-      (by norm_num : (4 : ℝ) = 2 * 2), log_mul two_ne_zero two_ne_zero]
-    ring
+  _ = 0 := circleAverage_log_norm_id_sub_const₁_integral
