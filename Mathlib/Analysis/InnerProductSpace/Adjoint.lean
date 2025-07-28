45ca8b18e65131b9a7cf7c0170275f8a5a9ff251
themathqueen <2497250a@research.gla.ac.uk>
2025-07-28 21:26:32 +0000

    chore(Analysis/InnerProductSpace/Adjoint): typo in docstring (#27607)

--- Mathlib/Analysis/InnerProductSpace/Adjoint.lean
+++ Mathlib/Analysis/InnerProductSpace/Adjoint.lean
@@ -238,7 +238,7 @@ theorem norm_adjoint_comp_self (A : E →L[𝕜] F) :
           Real.sqrt_mul_self (norm_nonneg x)]
 
 /-- The C⋆-algebra instance when `𝕜 := ℂ` can be found in
-`Analysis.CStarAlgebra.ContinuousLinearMap`. -/
+`Analysis/CStarAlgebra/ContinuousLinearMap`. -/
 instance : CStarRing (E →L[𝕜] E) where
   norm_mul_self_le x := le_of_eq <| Eq.symm <| norm_adjoint_comp_self x
 
