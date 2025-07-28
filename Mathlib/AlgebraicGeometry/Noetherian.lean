f8e7aff325807c7a5823567b553807c3499da511
Raph-DG <raphaeldouglasgiles@gmail.com>
2025-07-28 16:54:20 +0000

    feat(AlgebraicGeometry): Any stalk of a locally noetherian scheme is Noetherian (and ditto for integral schemes) (#26850)
    In this PR we show any stalk of a locally noetherian scheme is Noetherian (and ditto for integral schemes), and we write these results as typeclass instances.

--- Mathlib/AlgebraicGeometry/Noetherian.lean
+++ Mathlib/AlgebraicGeometry/Noetherian.lean
@@ -314,6 +314,13 @@ instance {R} [CommRing R] [IsNoetherianRing R] :
   suffices IsNoetherianRing (CommRingCat.of R) by infer_instance
   assumption
 
+instance [IsLocallyNoetherian X] {x : X} : IsNoetherianRing (X.presheaf.stalk x) := by
+  obtain ⟨U, hU, hU2, hU3⟩ := exists_isAffineOpen_mem_and_subset (U := ⊤) (x := x) (by aesop)
+  have := AlgebraicGeometry.IsAffineOpen.isLocalization_stalk hU ⟨x, hU2⟩
+  exact @IsLocalization.isNoetherianRing _ _ (hU.primeIdealOf ⟨x, hU2⟩).asIdeal.primeCompl
+        (X.presheaf.stalk x) _ (X.presheaf.algebra_section_stalk ⟨x, hU2⟩)
+        this (IsLocallyNoetherian.component_noetherian ⟨U, hU⟩)
+
 /-- `R` is a Noetherian ring if and only if `Spec R` is a Noetherian scheme. -/
 theorem isNoetherian_Spec {R : CommRingCat} :
     IsNoetherian (Spec R) ↔ IsNoetherianRing R :=
