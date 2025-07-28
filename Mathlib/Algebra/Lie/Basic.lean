f6aefa40638a0b813a5fb03a8c90867345cf46f8
Oliver Nash <github@olivernash.org>
2025-07-28 19:01:03 +0100

    Appease shake

--- Mathlib/Algebra/Lie/Basic.lean
+++ Mathlib/Algebra/Lie/Basic.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Oliver Nash
 -/
 import Mathlib.Algebra.BigOperators.Group.Finset.Sigma
-import Mathlib.Algebra.BigOperators.Ring.Finset
 import Mathlib.Algebra.Module.Submodule.Equiv
 import Mathlib.Algebra.Module.Equiv.Basic
 import Mathlib.Algebra.Module.Rat
