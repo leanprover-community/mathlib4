633c07866ef9e91d25294dea3415df9f6641efd4
Monica Omar <2497250a@research.gla.ac.uk>
2025-07-29 13:23:08 +0000

    fix

--- Mathlib/Algebra/Ring/Idempotent.lean
+++ Mathlib/Algebra/Ring/Idempotent.lean
@@ -72,9 +72,8 @@ variable [NonUnitalRing R] {a b : R}
 
 lemma add_sub_mul_of_commute (h : Commute a b) (ha : IsIdempotentElem a) (hb : IsIdempotentElem b) :
     IsIdempotentElem (a + b - a * b) := by
-  rw [IsIdempotentElem]
-  simp only [h.eq, mul_sub, mul_add, sub_mul, add_mul, ha.eq, mul_assoc, add_sub_cancel_right,
-    hb.eq, hb.mul_self_mul, add_sub_cancel_left, sub_right_inj]
+  simp only [IsIdempotentElem, h.eq, mul_sub, mul_add, sub_mul, add_mul, ha.eq,
+    mul_assoc, add_sub_cancel_right, hb.eq, hb.mul_self_mul, add_sub_cancel_left, sub_right_inj]
   rw [← h.eq, ha.mul_self_mul, h.eq, hb.mul_self_mul, add_sub_cancel_right]
 
 end NonUnitalRing
