29d161816a19aea1c00dfd1284d28e107e595a36
Jovan Gerbscheid <56355248+JovanGerb@users.noreply.github.com>
2025-07-29 01:32:43 +0100

    fix precedence

--- Mathlib/Algebra/Category/ModuleCat/ChangeOfRings.lean
+++ Mathlib/Algebra/Category/ModuleCat/ChangeOfRings.lean
@@ -292,7 +292,8 @@ variable (M : Type v) [AddCommMonoid M] [Module R M]
 
 -- This notation is necessary because we need to reason about `s ⊗ₜ m` where `s : S` and `m : M`;
 -- without this notation, one needs to work with `s : (restrictScalars f).obj ⟨S⟩`.
-scoped[ChangeOfRings] notation:100 s:101 "⊗ₜ[" R "," f "]" m:100 =>
+scoped[ChangeOfRings] notation:100 s:100 "⊗ₜ[" R "," f "]" m:101 =>
+  @TensorProduct.tmul R _ _ _ _ _ (Module.compHom _ f) _ s m
   @TensorProduct.tmul R _ _ _ _ _ (Module.compHom _ f) _ s m
 
 end Unbundled
