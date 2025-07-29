6a84bad5bece8b334f41c8c651b12df9f8974863
Aaron Liu <aaronliu2008@outlook.com>
2025-07-29 10:00:09 -0400

    explain priority

--- Mathlib/Topology/Sober.lean
+++ Mathlib/Topology/Sober.lean
@@ -234,6 +234,7 @@ theorem quasiSober_of_open_cover (S : Set (Set α)) (hS : ∀ s : S, IsOpen (s :
 Any R1 space is a quasi-sober space because any irreducible set is
 contained in the closure of a singleton.
 -/
+-- see note [lower instance priority]
 instance (priority := 100) R1Space.quasiSober [R1Space α] : QuasiSober α where
   sober h hs := by
     obtain ⟨x, hx⟩ := h.nonempty
