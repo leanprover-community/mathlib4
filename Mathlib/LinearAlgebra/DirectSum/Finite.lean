dfa1234cec26bd4d2976054e1d5729384d0f0640
smorel394 <67864981+smorel394@users.noreply.github.com>
2025-07-30 21:01:32 +0200

    Update Finite.lean

--- Mathlib/LinearAlgebra/DirectSum/Finite.lean
+++ Mathlib/LinearAlgebra/DirectSum/Finite.lean
@@ -13,19 +13,14 @@ This file defines a `Module.Finite` instance for a finite direct sum of finite m
 
 -/
 
-variable {R : Type*} [Semiring R] {ι : Type*} [_root_.Finite ι]
-variable (M : ι → Type*) [∀ i : ι, AddCommMonoid (M i)] [∀ i : ι, Module R (M i)]
+open DirectSum
 
-instance Module.Finite.dfinsupp [∀ (i : ι), Module.Finite R (M i)] :
-    Module.Finite R (Π₀ (i : ι), M i) :=
+variable {R ι : Type*} [Semiring R] [Finite ι] (M : ι → Type*)
+  [∀ i : ι, AddCommMonoid (M i)] [∀ i : ι, Module R (M i)] [∀ (i : ι), Module.Finite R (M i)]
+
+instance Module.Finite.instDFinsupp : Module.Finite R (Π₀ (i : ι), M i) :=
   letI : Fintype ι := Fintype.ofFinite _
   Module.Finite.equiv DFinsupp.linearEquivFunOnFintype.symm
 
-open DirectSum
-
-variable (R : Type*) [Semiring R] {ι : Type*} [Finite ι] (M : ι → Type*)
-  [∀ i : ι, AddCommMonoid (M i)] [∀ i : ι, Module R (M i)]
-
-instance Module.Finite.directSum [∀ i : ι, Module.Finite R (M i)] : Module.Finite R (⨁ i, M i) := by
-  dsimp [DirectSum]
-  infer_instance
+instance Module.Finite.instDirectSum : Module.Finite R (⨁ i, M i) :=
+  Module.Finite.instDFinsupp M
