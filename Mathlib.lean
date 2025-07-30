f3f73060dfb92ce7ab116924c3e8d48b55454736
Etienne Marion <baguettes_casino0c@icloud.com>
2025-07-30 12:25:45 +0200

    mk_all

--- Mathlib.lean
+++ Mathlib.lean
@@ -5123,6 +5123,7 @@ import Mathlib.Probability.Martingale.OptionalStopping
 import Mathlib.Probability.Martingale.Upcrossing
 import Mathlib.Probability.Moments.Basic
 import Mathlib.Probability.Moments.ComplexMGF
+import Mathlib.Probability.Moments.CovInnerBilin
 import Mathlib.Probability.Moments.Covariance
 import Mathlib.Probability.Moments.CovarianceBilin
 import Mathlib.Probability.Moments.IntegrableExpMul
