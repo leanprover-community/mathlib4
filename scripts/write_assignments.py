9d399b74897573c7b144b0786d5895b8a0e8865a
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-07-30 21:38:35 +0200

    Try a new approach

---
+++ scripts/write_assignments.py
@@ -0,0 +1,25 @@
+"""
+Read a file |assignments.json| containing reviewer assignments for pull requests,
+parse its contents and generate a shell script making the github API calls to add these users
+as assignees.
+"""
+import json
+
+def call(number: int, handle: str) -> str:
+    # This keeps any existing assignee on the PR.
+    # Calling this with a non-existent user does nothing (in particular, keeps existing assignees).
+    raw = f'''curl --location --request POST --header "Accept: application/vnd.github+json" \\
+        --header "authorization: Bearer ${{ secrets.ASSIGN_REVIEWERS }}" \\
+        --header "X-GitHub-Api-Version: 2022-11-28" \\
+        https://api.github.com/repos/leanprover-community/mathlib4/issues/{number}/assignees \\
+        --data \'{{"assignees":["{handle}"]}}\''''
+    return raw.replace("        ", "  ")
+
+if __name__ == '__main__':
+    with open('assignments.json', 'r') as fi:
+        data = json.load(fi)
+    output = [call(number, user_handle) for (number, user_handle) in data.items()]
+    with open('assign_users.sh', 'w') as outfile:
+        for (number, user_handle) in data.items():
+            print(f"assigning PR {number} to {user_handle}")
+        outfile.write("\n".join(output) + '\n')
