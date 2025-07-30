6db2d8cd0e79e349bceb40ccddbedf22be22112d
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2025-07-30 08:59:36 +0200

    Fix Copyright

--- Mathlib/Analysis/InnerProductSpace/OuterProduct.lean
+++ Mathlib/Analysis/InnerProductSpace/OuterProduct.lean
@@ -1,5 +1,6 @@
 /-
 Copyright (c) 2025 Iván Renison, Jam Khan. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Iván Renison, Jam Khan
 -/
 import Mathlib.Analysis.InnerProductSpace.Positive
