94c4527073770d0925ff76784b39417e0e4603f8
Yakov Pechersky <ffxen158@gmail.com>
2025-07-29 21:14:10 -0400

    choose_ne_zero

--- Mathlib/NumberTheory/Padics/PadicVal/Basic.lean
+++ Mathlib/NumberTheory/Padics/PadicVal/Basic.lean
@@ -599,7 +599,7 @@ in base `p`. This sum is expressed over the finset `Ico 1 b` where `b` is any bo
 `log p n`. -/
 theorem padicValNat_choose {n k b : ℕ} [hp : Fact p.Prime] (hkn : k ≤ n) (hnb : log p n < b) :
     padicValNat p (choose n k) = #{i ∈ Finset.Ico 1 b | p ^ i ≤ k % p ^ i + (n - k) % p ^ i} := by
-  exact_mod_cast (padicValNat_eq_emultiplicity (p := p) <| (choose_pos hkn).ne') ▸
+  exact_mod_cast (padicValNat_eq_emultiplicity (p := p) <| (choose_ne_zero hkn)) ▸
     Prime.emultiplicity_choose hp.out hkn hnb
 
 /-- **Kummer's Theorem**
@@ -609,7 +609,7 @@ in base `p`. This sum is expressed over the finset `Ico 1 b` where `b` is any bo
 `log p (n + k)`. -/
 theorem padicValNat_choose' {n k b : ℕ} [hp : Fact p.Prime] (hnb : log p (n + k) < b) :
     padicValNat p (choose (n + k) k) = #{i ∈ Finset.Ico 1 b | p ^ i ≤ k % p ^ i + n % p ^ i} := by
-  exact_mod_cast (padicValNat_eq_emultiplicity (p := p) <| Nat.ne_of_gt <| choose_pos <|
+  exact_mod_cast (padicValNat_eq_emultiplicity (p := p) <| choose_ne_zero <|
     Nat.le_add_left k n)▸ Prime.emultiplicity_choose' hp.out hnb
 
 /-- **Kummer's Theorem**
