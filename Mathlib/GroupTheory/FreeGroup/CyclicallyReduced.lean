fdf75011fd044f209e1f950446214a4041b3a8cd
Bernhard Reinke <b-reinke@users.noreply.github.com>
2025-07-30 10:54:28 +0200

    Simp normal form adjustment

--- Mathlib/GroupTheory/FreeGroup/CyclicallyReduced.lean
+++ Mathlib/GroupTheory/FreeGroup/CyclicallyReduced.lean
@@ -47,7 +47,7 @@ theorem isCyclicallyReduced_iff :
 @[to_additive]
 theorem isCyclicallyReduced_cons_append_iff {a b : α × Bool} :
     IsCyclicallyReduced (b :: L ++ [a]) ↔
-    IsReduced (b :: L ++ [a]) ∧ ¬(a.1 = b.1 ∧ (!a.2) = b.2) := by
+    IsReduced (b :: L ++ [a]) ∧ (a.1 = b.1 → a.2 = b.2) := by
   rw [isCyclicallyReduced_iff,List.getLast?_concat]
   simp
 
