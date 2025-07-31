facd8b77e0132672792784037a8d29a03b5a96e8
Yakov Pechersky <ffxen158@gmail.com>
2025-07-31 13:37:34 -0400

    shake

--- Mathlib/NumberTheory/Padics/WithVal.lean
+++ Mathlib/NumberTheory/Padics/WithVal.lean
@@ -3,11 +3,13 @@ Copyright (c) 2025 Yakov Pechersky. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yakov Pechersky
 -/
+import Mathlib.Analysis.RCLike.Basic
 import Mathlib.GroupTheory.ArchimedeanDensely
-import Mathlib.NumberTheory.NumberField.FinitePlaces
 import Mathlib.NumberTheory.Padics.PadicNumbers
 import Mathlib.Topology.Algebra.UniformRing
+import Mathlib.Topology.Algebra.Valued.ValuedField
 import Mathlib.Topology.Algebra.Valued.WithVal
+import Mathlib.Topology.GDelta.MetrizableSpace
 
 /-!
 # Equivalence between `ℚ_[p]` and `WithVal v`
