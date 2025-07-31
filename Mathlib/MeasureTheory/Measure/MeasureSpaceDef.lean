dde57e3ff4234c3c66ca1e99aa3d5efe23dc673f
Jon Bannon <jbannon@siena.edu>
2025-07-30 21:06:21 -0400

    Trying to reduce the diff on a file...

--- Mathlib/MeasureTheory/Measure/MeasureSpaceDef.lean
+++ Mathlib/MeasureTheory/Measure/MeasureSpaceDef.lean
@@ -3,7 +3,6 @@ Copyright (c) 2017 Johannes Hölzl. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl, Mario Carneiro
 -/
-
 import Mathlib.MeasureTheory.OuterMeasure.Induced
 import Mathlib.MeasureTheory.OuterMeasure.AE
 import Mathlib.Order.Filter.CountableInter
@@ -364,6 +363,7 @@ end MeasureSpace
 end
 
 end MeasureTheory
+
 section
 
 open MeasureTheory
