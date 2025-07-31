64c39c50709df3cf31bd9f63b9008d86cf272b9e
Etienne Marion <baguettes_casino0c@icloud.com>
2025-07-31 22:12:25 +0200

    fix imports

--- Mathlib/Probability/Moments/Variance.lean
+++ Mathlib/Probability/Moments/Variance.lean
@@ -3,7 +3,6 @@ Copyright (c) 2022 Sébastien Gouëzel. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Sébastien Gouëzel, Kexing Ying
 -/
-import Mathlib.Probability.Integration
 import Mathlib.Probability.Moments.Covariance
 
 /-!
