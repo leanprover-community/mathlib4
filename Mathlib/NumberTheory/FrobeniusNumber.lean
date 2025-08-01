1488bb062fb8f07f1f77a1b2cf0a7f555a18aac8
Junyan Xu <junyanxu.math@gmail.com>
2025-08-01 18:57:08 +0200

    authors

--- Mathlib/NumberTheory/FrobeniusNumber.lean
+++ Mathlib/NumberTheory/FrobeniusNumber.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2021 Alex Zhao. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Alex Zhao
+Authors: Alex Zhao, Daniel Buth, Sebastian Meier, Junyan Xu
 -/
 import Mathlib.RingTheory.Ideal.NatInt
 
