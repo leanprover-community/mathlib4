d9a582d5ce04805436671b9beb93380000861bc9
Yakov Pechersky <ffxen158@gmail.com>
2025-08-01 15:48:47 +0000

    chore(SetTheory/ZFC): scope sUnion notation (#27833)
    it causes elaboration clash with regular Set notation when type is unspecified
    
    zulip thread:
    https://leanprover.zulipchat.com/#narrow/channel/287929-mathlib4/topic/Unscoped.20.60.E2.8B.83.E2.82.80.60.20and.20.60.E2.8B.82.E2.82.80.60

--- Mathlib/SetTheory/ZFC/Basic.lean
+++ Mathlib/SetTheory/ZFC/Basic.lean
@@ -460,7 +460,9 @@ theorem sUnion_lem {α β : Type u} (A : α → PSet) (B : β → PSet) (αβ : 
     match A a, B b, ea, eb, c, d, hd with
     | _, _, rfl, rfl, _, _, hd => exact hd
 
-/-- The union operator, the collection of elements of elements of a ZFC set -/
+/-- The union operator, the collection of elements of elements of a ZFC set. Uses `⋃₀` notation,
+scoped under the `ZFSet` namespace.
+-/
 def sUnion : ZFSet → ZFSet :=
   Quotient.map PSet.sUnion
     fun ⟨_, A⟩ ⟨_, B⟩ ⟨αβ, βα⟩ =>
@@ -470,14 +472,14 @@ def sUnion : ZFSet → ZFSet :=
           fun b hb => ⟨b, PSet.Equiv.symm hb⟩⟩
 
 @[inherit_doc]
-prefix:110 "⋃₀ " => ZFSet.sUnion
+scoped prefix:110 "⋃₀ " => ZFSet.sUnion
 
 /-- The intersection operator, the collection of elements in all of the elements of a ZFC set. We
-define `⋂₀ ∅ = ∅`. -/
+define `⋂₀ ∅ = ∅`. Uses `⋂₀` notation, scoped under the `ZFSet` namespace. -/
 def sInter (x : ZFSet) : ZFSet := (⋃₀ x).sep (fun y => ∀ z ∈ x, y ∈ z)
 
 @[inherit_doc]
-prefix:110 "⋂₀ " => ZFSet.sInter
+scoped prefix:110 "⋂₀ " => ZFSet.sInter
 
 @[simp]
 theorem mem_sUnion {x y : ZFSet.{u}} : y ∈ ⋃₀ x ↔ ∃ z ∈ x, y ∈ z :=
