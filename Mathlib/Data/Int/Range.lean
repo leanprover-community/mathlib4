2456d00f8b841908ed6909c5cf326f773dc34cb7
Junyan Xu <junyanxu.math@gmail.com>
2025-08-01 09:29:15 +0000

    fix

--- Mathlib/Data/Int/Range.lean
+++ Mathlib/Data/Int/Range.lean
@@ -3,7 +3,7 @@ Copyright (c) 2018 Kenny Lau. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro, Kenny Lau
 -/
-import Mathlib.Algebra.Group.Int.Defs,
+import Mathlib.Algebra.Group.Int.Defs
 import Mathlib.Algebra.Order.Group.Unbundled.Basic
 
 /-!
