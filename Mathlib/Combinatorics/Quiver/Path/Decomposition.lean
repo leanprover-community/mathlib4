b1e73589f2139b85f1dfec6eb620bcc6d2bd29c9
Matteo Cipollina <matteo.cipollina@yahoo.it>
2025-08-01 20:13:45 +0200

    remove unused import

--- Mathlib/Combinatorics/Quiver/Path/Decomposition.lean
+++ Mathlib/Combinatorics/Quiver/Path/Decomposition.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Matteo Cipollina
 -/
 
-import Mathlib.Algebra.GroupWithZero.Nat
 import Mathlib.Algebra.Order.Group.Nat
 import Mathlib.Combinatorics.Quiver.Path
 import Mathlib.Tactic.Cases
