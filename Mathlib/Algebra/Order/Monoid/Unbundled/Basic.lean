778d8ca63530c5b7d7c32c899d3a0612206dd9b6
euprunin <euprunin@users.noreply.github.com>
2025-08-01 10:15:28 +0200

    chore(Algebra/Order): golf `mem_piAntidiag` and `trichotomy_of_mul_eq_mul` using `grind`

--- Mathlib/Algebra/Order/Monoid/Unbundled/Basic.lean
+++ Mathlib/Algebra/Order/Monoid/Unbundled/Basic.lean
@@ -329,10 +329,10 @@ theorem trichotomy_of_mul_eq_mul
     [MulLeftStrictMono α] [MulRightStrictMono α]
     (h : a * b = c * d) : (a = c ∧ b = d) ∨ a < c ∨ b < d := by
   obtain hac | rfl | hca := lt_trichotomy a c
-  · right; left; exact hac
+  · grind
   · left; simpa using mul_right_inj_of_comparable (LinearOrder.le_total d b) |>.1 h
   · obtain hbd | rfl | hdb := lt_trichotomy b d
-    · right; right; exact hbd
+    · grind
     · exact False.elim <| ne_of_lt (mul_lt_mul_right' hca b) h.symm
     · exact False.elim <| ne_of_lt (mul_lt_mul_of_lt_of_lt hca hdb) h.symm
 
