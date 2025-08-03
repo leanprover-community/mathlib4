3035b60dc7505ba19c138cc048b2432c930d71ae
Parcly Taxel <reddeloostw@gmail.com>
2025-08-03 15:14:57 +0800

    use more grind

--- Archive/Imo/Imo2001Q5.lean
+++ Archive/Imo/Imo2001Q5.lean
@@ -69,11 +69,11 @@ lemma not_collinear_BAC : ¬Collinear ℝ {s.B, s.A, s.C} := by
 
 lemma BAP_eq : ∠ s.B s.A s.P = π / 6 := by
   have := angle_add_of_ne_of_ne s.A_ne_B s.A_ne_C s.P_on_BC
-  rw [← s.AP_bisect_BAC, ← two_mul, s.BAC_eq] at this; linarith [this]
+  rw [← s.AP_bisect_BAC, ← two_mul, s.BAC_eq] at this; grind
 
 lemma PAC_eq : ∠ s.P s.A s.C = π / 6 := by
   have := angle_add_of_ne_of_ne s.A_ne_B s.A_ne_C s.P_on_BC
-  rw [s.AP_bisect_BAC, ← two_mul, s.BAC_eq] at this; linarith [this]
+  rw [s.AP_bisect_BAC, ← two_mul, s.BAC_eq] at this; grind
 
 lemma P_ne_B : s.P ≠ s.B := by
   by_contra h; have := s.BAP_eq
@@ -97,7 +97,7 @@ lemma QBC_eq : ∠ s.Q s.B s.C = s.x := by rw [← s.BQ_bisect_ABC, s.ABQ_eq]
 
 lemma ABC_eq : ∠ s.A s.B s.C = 2 * s.x := by
   have := angle_add_of_ne_of_ne s.A_ne_B.symm s.B_ne_C s.Q_on_AC
-  rw [s.ABQ_eq, s.QBC_eq, ← two_mul] at this; exact this.symm
+  rw [s.ABQ_eq, s.QBC_eq] at this; grind
 
 lemma x_pos : 0 < s.x := by
   by_contra! h
@@ -121,7 +121,7 @@ lemma AQC_eq : ∠ s.A s.Q s.C = π := by rw [angle_eq_pi_iff_sbtw]; exact s.sbt
 
 lemma ACB_eq : ∠ s.A s.C s.B = 2 * π / 3 - 2 * s.x := by
   have := angle_add_angle_add_angle_eq_pi s.A s.B_ne_C
-  rw [angle_comm, s.ABC_eq, s.BAC_eq] at this; linarith
+  rw [angle_comm, s.ABC_eq, s.BAC_eq] at this; grind
 
 lemma x_lt_pi_div_three : s.x < π / 3 := by
   by_contra! h
@@ -133,12 +133,12 @@ lemma x_lt_pi_div_three : s.x < π / 3 := by
 lemma APB_eq : ∠ s.A s.P s.B = 5 * π / 6 - 2 * s.x := by
   have := angle_add_angle_add_angle_eq_pi s.P s.A_ne_B
   rw [s.BAP_eq, angle_comm s.P, angle_eq_angle_of_angle_eq_pi _ s.BPC_eq, s.ABC_eq] at this
-  linarith
+  grind
 
 lemma AQB_eq : ∠ s.A s.Q s.B = 2 * π / 3 - s.x := by
   have := angle_add_angle_add_angle_eq_pi s.Q s.A_ne_B
   rw [angle_eq_angle_of_angle_eq_pi _ s.AQC_eq, s.BAC_eq, angle_comm s.Q, s.ABQ_eq] at this
-  linarith
+  grind
 
 /-- A macro for applying the bounds on `x`, `x_pos` and `x_lt_pi_div_three`. -/
 macro (name := bx) "bx" : tactic => `(tactic| linarith [s.x_pos, s.x_lt_pi_div_three])
@@ -186,14 +186,14 @@ lemma x_eq : s.x = 2 * π / 9 := by
   · refine mul_ne_zero two_ne_zero (Real.sin_pos_of_pos_of_lt_pi ?_ ?_).ne' <;> bx
   rw [← eq_sub_iff_add_eq', div_sub_one] at work; swap
   · refine (Real.cos_pos_of_mem_Ioo ⟨?_, ?_⟩).ne' <;> bx
-  rw [add_div, sub_div s.x, ← Real.two_mul_sin_mul_sin, show π / 3 / 2 = π / 6 by linarith,
+  rw [add_div, sub_div s.x, ← Real.two_mul_sin_mul_sin, show π / 3 / 2 = π / 6 by ring,
     Real.sin_pi_div_six, show 2 * Real.sin (s.x / 2) * (1 / 2) = Real.sin (s.x / 2) by ring] at work
   apply mul_eq_mul_of_div_eq_div at work; rotate_left
   · refine (Real.sin_pos_of_pos_of_lt_pi ?_ ?_).ne' <;> bx
   · refine (Real.cos_pos_of_mem_Ioo ⟨?_, ?_⟩).ne' <;> bx
   rw [one_div_mul_eq_div, div_eq_iff_mul_eq two_ne_zero, ← mul_rotate, Real.two_mul_sin_mul_sin,
-    show s.x / 2 - (5 * π / 6 - 2 * s.x) = 5 * (s.x / 2) + π / 6 - π by linarith, Real.cos_sub_pi,
-    show s.x / 2 + (5 * π / 6 - 2 * s.x) = π - (3 * (s.x / 2) + π / 6) by linarith, Real.cos_pi_sub,
+    show s.x / 2 - (5 * π / 6 - 2 * s.x) = 5 * (s.x / 2) + π / 6 - π by ring, Real.cos_sub_pi,
+    show s.x / 2 + (5 * π / 6 - 2 * s.x) = π - (3 * (s.x / 2) + π / 6) by ring, Real.cos_pi_sub,
     neg_sub_neg] at work
   rw [mul_div_assoc, mul_comm, ← div_eq_iff two_ne_zero]
   set y := s.x / 2
@@ -212,11 +212,11 @@ lemma x_eq : s.x = 2 * π / 9 := by
   rw [← Real.cos_pi_div_two] at iden; apply_fun Real.arccos at iden
   rw [Real.arccos_cos (by unfold y; bx) (by unfold y; bx),
     Real.arccos_cos (by positivity) (by linarith [Real.pi_pos])] at iden
-  linarith
+  grind
 
 end Setup
 
 theorem result (s : Setup X) : ∠ s.A s.B s.C = 4 * π / 9 ∧ ∠ s.A s.C s.B = 2 * π / 9 := by
-  rw [s.ABC_eq, s.ACB_eq, s.x_eq]; constructor <;> linarith
+  rw [s.ABC_eq, s.ACB_eq, s.x_eq]; constructor <;> ring
 
 end Imo2001Q5
