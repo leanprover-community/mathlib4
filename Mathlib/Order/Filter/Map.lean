69affde3d3d35761160756f629072a36418f9dc4
Attila Gáspár <58485900+gasparattila@users.noreply.github.com>
2025-08-03 23:36:26 +0200

    Update

--- Mathlib/Order/Filter/Map.lean
+++ Mathlib/Order/Filter/Map.lean
@@ -149,11 +149,13 @@ theorem eventually_pure {a : α} {p : α → Prop} : (∀ᶠ x in pure a, p x) �
 theorem principal_singleton (a : α) : 𝓟 {a} = pure a :=
   Filter.ext fun s => by simp only [mem_pure, mem_principal, singleton_subset_iff]
 
-theorem principal_eq_biSup_pure {s : Set α} : 𝓟 s = ⨆ a ∈ s, pure a :=
+@[simp]
+theorem biSup_pure_eq_principal {s : Set α} : ⨆ a ∈ s, pure a = 𝓟 s :=
   Filter.ext fun s => by simp only [mem_iSup]; rfl
 
-theorem top_eq_iSup_pure : (⊤ : Filter α) = ⨆ a, pure a := by
-  rw [← principal_univ, principal_eq_biSup_pure, iSup_univ]
+@[simp]
+theorem iSup_pure_eq_top : ⨆ a, pure a = (⊤ : Filter α) := by
+  rw [← principal_univ, ← biSup_pure_eq_principal, iSup_univ]
 
 @[simp]
 theorem map_pure (f : α → β) (a : α) : map f (pure a) = pure (f a) :=
