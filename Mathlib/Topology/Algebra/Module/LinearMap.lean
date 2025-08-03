681792faaecae5b0dadf4e3468387306719e8766
Christopher Hoskin <mans0954@users.noreply.github.com>
2025-08-03 20:18:25 +0100

    Update docstring

--- Mathlib/Topology/Algebra/Module/LinearMap.lean
+++ Mathlib/Topology/Algebra/Module/LinearMap.lean
@@ -60,9 +60,9 @@ abbrev ContinuousLinearMapClass (F : Type*) (R : outParam Type*) [Semiring R]
     [TopologicalSpace M₂] [AddCommMonoid M₂] [Module R M] [Module R M₂] [FunLike F M M₂] :=
   ContinuousSemilinearMapClass F (RingHom.id R) M M₂
 
-/-- The *strong dual* of a topological vector space space `M`. This is the space of
+/-- The *strong dual* of a topological vector space space `M` over a ring `R`. This is the space of
 continuous linear functionals and is equipped with the topology of uniform convergence
-on bounded subsets. -/
+on bounded subsets. `StrongDual R M` is an abbreviation for `M →L[R] R`. -/
 abbrev StrongDual (R : Type*) [Semiring R] [TopologicalSpace R]
   (M : Type*) [TopologicalSpace M] [AddCommMonoid M] [Module R M] : Type _ := M →L[R] R
 
