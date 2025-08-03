5a22a38979a5a94a466377895a8f410264c444ad
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2025-08-03 21:04:14 +0200

    add missing import

--- Mathlib/Combinatorics/Quiver/Basic.lean
+++ Mathlib/Combinatorics/Quiver/Basic.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: David Wärn, Kim Morrison
 -/
 import Mathlib.Data.Opposite
+import Mathlib.Tactic.ToDual
 
 /-!
 # Quivers
