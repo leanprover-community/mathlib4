74345da400659c543dd2bfce4b815989385f6ed1
Yuyang Zhao 赵雨扬 <zhao.yu-yang@foxmail.com>
2025-08-03 17:25:46 +0800

    more

--- Mathlib/RingTheory/PowerSeries/Binomial.lean
+++ Mathlib/RingTheory/PowerSeries/Binomial.lean
@@ -43,7 +43,7 @@ def binomialSeries (A) [One A] [SMul R A] (r : R) : PowerSeries A :=
 
 @[simp]
 lemma binomialSeries_coeff [Semiring A] [SMul R A] (r : R) (n : ℕ) :
-    (coeff A n) (binomialSeries A r) = Ring.choose r n • 1 :=
+    (coeff n) (binomialSeries A r) = Ring.choose r n • 1 :=
   coeff_mk n fun n ↦ Ring.choose r n • 1
 
 @[simp]
@@ -66,14 +66,14 @@ lemma binomialSeries_nat [CommRing A] (d : ℕ) :
     rw [one_pow, mul_one, coeff_X_pow_mul']
     have hkd : k ≤ d := mem_range_succ_iff.mp hk
     simp only [hkd.trans (le_of_lt h), ↓reduceIte]
-    rw [← map_natCast (C A), coeff_ne_zero_C (by omega)]
+    rw [← map_natCast C, coeff_ne_zero_C (by omega)]
   · rw [binomialSeries_coeff, add_comm, add_pow]
     simp only [zsmul_eq_mul, mul_one, one_pow, map_sum]
     rw [sum_eq_single_of_mem n (by simp only [mem_range]; omega) ?_, coeff_X_pow_mul',
       Ring.choose_eq_nat_choose]
     · simp
     · intro k hk hkn
-      rw [mul_comm, ← map_natCast (C A), coeff_C_mul_X_pow]
+      rw [mul_comm, ← map_natCast C, coeff_C_mul_X_pow]
       exact if_neg (Ne.symm hkn)
 
 lemma rescale_neg_one_invOneSubPow [CommRing A] (d : ℕ) :
