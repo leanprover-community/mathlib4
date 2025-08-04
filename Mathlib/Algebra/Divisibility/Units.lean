07ab0ae460195a52a645845bfced0453e0876333
Junyan Xu <junyanxu.math@gmail.com>
2025-08-04 22:36:32 +0200

    docBlame

--- Mathlib/Algebra/Divisibility/Units.lean
+++ Mathlib/Algebra/Divisibility/Units.lean
@@ -138,7 +138,9 @@ end CommMonoid
 section RelPrime
 
 /-- `x` and `y` are relatively prime if every common divisor is a unit. -/
-@[to_additive] def IsRelPrime [Monoid α] (x y : α) : Prop := ∀ ⦃d⦄, d ∣ x → d ∣ y → IsUnit d
+@[to_additive "`x` and `y` are additively relatively prime if every common additive divisor
+is an additive unit."]
+def IsRelPrime [Monoid α] (x y : α) : Prop := ∀ ⦃d⦄, d ∣ x → d ∣ y → IsUnit d
 
 variable [CommMonoid α] {x y z : α}
 
