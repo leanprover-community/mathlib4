ba02833962f6599c67121777ebbdbd141c599ece
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2025-08-04 22:49:17 +0200

    non terminal `simp` to `rw`

--- Mathlib/Topology/UnitInterval.lean
+++ Mathlib/Topology/UnitInterval.lean
@@ -225,19 +225,19 @@ instance : LinearOrderedCommMonoidWithZero I where
     simp [h_ij]
 
 lemma subtype_Iic_eq_Icc (x : I) : Subtype.val⁻¹' (Iic ↑x) = Icc 0 x := by
-  simp
+  rw [preimage_subtype_val_Iic]
   exact Icc_bot.symm
 
 lemma subtype_Iio_eq_Ico (x : I) : Subtype.val⁻¹' (Iio ↑x) = Ico 0 x := by
-  simp
+  rw [preimage_subtype_val_Iio]
   exact Ico_bot.symm
 
 lemma subtype_Ici_eq_Icc (x : I) : Subtype.val⁻¹' (Ici ↑x) = Icc x 1 := by
-  simp
+  rw [preimage_subtype_val_Ici]
   exact Icc_top.symm
 
 lemma subtype_Ioi_eq_Ioc (x : I) : Subtype.val⁻¹' (Ioi ↑x) = Ioc x 1 := by
-  simp
+  rw [preimage_subtype_val_Ioi]
   exact Ioc_top.symm
 
 end unitInterval
