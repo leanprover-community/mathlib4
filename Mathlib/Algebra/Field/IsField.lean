535bde5e97f5d5bc1d28e436aedf2347f35b831b
Yakov Pechersky <ffxen158@gmail.com>
2025-08-04 13:49:10 -0400

    fix proof

--- Mathlib/Algebra/Field/IsField.lean
+++ Mathlib/Algebra/Field/IsField.lean
@@ -48,10 +48,10 @@ theorem IsField.nontrivial {R : Type u} [Semiring R] (h : IsField R) : Nontrivia
   ⟨h.exists_pair_ne⟩
 
 lemma IsField.isDomain {R : Type u} [Semiring R] (h : IsField R) : IsDomain R where
-  mul_left_cancel_of_ne_zero ha hb := by
+  mul_left_cancel_of_ne_zero ha _ _ hb := by
     obtain ⟨x, hx⟩ := h.mul_inv_cancel ha
     simpa [← mul_assoc, h.mul_comm, hx] using congr_arg (x * ·) hb
-  mul_right_cancel_of_ne_zero ha hb := by
+  mul_right_cancel_of_ne_zero ha _ _ hb := by
     obtain ⟨x, hx⟩ := h.mul_inv_cancel ha
     simpa [mul_assoc, hx] using congr_arg (· * x) hb
   exists_pair_ne := h.exists_pair_ne
