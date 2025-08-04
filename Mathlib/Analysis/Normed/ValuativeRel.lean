8e0494e42918e57c13fc690c83d80033e1e7802d
Yakov Pechersky <ffxen158@gmail.com>
2025-08-04 14:14:02 -0400

    cleanup

--- Mathlib/Analysis/Normed/ValuativeRel.lean
+++ Mathlib/Analysis/Normed/ValuativeRel.lean
@@ -5,6 +5,7 @@ Authors: Yakov Pechersky
 -/
 import Mathlib.Analysis.Normed.Field.Lemmas
 import Mathlib.Analysis.Normed.Group.Ultra
+import Mathlib.RingTheory.Valuation.RankOne
 import Mathlib.Topology.Algebra.Valued.ValuativeRel
 
 /-!
@@ -49,54 +50,17 @@ scoped[NormedField] attribute [instance] NormedField.toValuativeRel
 
 lemma rel_iff_le (x y : R) : x ≤ᵥ y ↔ ‖x‖ ≤ ‖y‖ := Iff.rfl
 
-@[simp]
-lemma coe_posSubmonoid_ne_zero (x : (ValuativeRel.posSubmonoid R)) :
-    (x : R) ≠ 0 := by
-  have := x.prop
-  rw [ValuativeRel.posSubmonoid_def] at this
-  simpa [rel_iff_le] using this
-
 lemma norm_pos_posSubmonoid (x : (ValuativeRel.posSubmonoid R)) :
     0 < ‖(x : R)‖ := by
   simp
 
 instance : (valuation' (R := R)).Compatible := .ofValuation _
 
-/-- Norm lifted from the ValueGroupWithZero. -/
-noncomputable
-def ofValueGroupWithZero : ValuativeRel.ValueGroupWithZero R →*₀ ℝ≥0 :=
-  ⟨⟨ValuativeRel.ValueGroupWithZero.lift (fun r s ↦ ‖r‖₊ / ‖(s : R)‖₊) <| by
-    intro x y r s
-    simp only [rel_iff_le]
-    intro h h'
-    rw [div_eq_div_iff]
-    · simp only [norm_mul, NNReal.val_eq_coe, ← NNReal.coe_inj, NNReal.coe_mul,
-      coe_nnnorm] at h h' ⊢
-      linarith
-    · simp
-    · simp, by simp⟩, by simp, by
-      intros
-      simp only
-      apply ValuativeRel.ValueGroupWithZero.lift_mul
-      field_simp⟩
-
-lemma ofValueGroupWithZero_strictMono : StrictMono (ofValueGroupWithZero (R := R)) := by
-  intro a b h
-  simp only [ofValueGroupWithZero]
-  induction a using ValuativeRel.ValueGroupWithZero.ind with | mk a r
-  induction b using ValuativeRel.ValueGroupWithZero.ind with | mk b s
-  simp only [ValuativeRel.ValueGroupWithZero.mk_lt_mk, rel_iff_le, norm_mul, not_le,
-    MonoidWithZeroHom.coe_mk, ZeroHom.coe_mk, ValuativeRel.ValueGroupWithZero.lift_mk] at h ⊢
-  rw [div_lt_div_iff₀]
-  · exact h.right
-  · simp
-  · simp
-
-instance : ValuativeRel.IsRankLeOne R where
-  nonempty := ⟨ofValueGroupWithZero, ofValueGroupWithZero_strictMono⟩
+instance : ValuativeRel.IsRankLeOne R :=
+  .of_compatible_mulArchimedean (valuation' (R := R))
 
 instance {K : Type*} [NormedField K] [IsUltrametricDist K] :
-    ValuativeTopology K := by
+    IsValuativeTopology K := by
   have he : valuation'.IsEquiv (ValuativeRel.valuation K) := ValuativeRel.isEquiv _ _
   refine .of_hasBasis (Metric.nhds_basis_ball.to_hasBasis' ?_ ?_)
   · intro ε hε
@@ -123,10 +87,9 @@ instance {K : Type*} [NormedField K] [IsUltrametricDist K] :
     simp_rw [← hr, ← Valuation.map_div, ← he.lt_iff_lt]
     simp only [valuation'_apply, map_div₀, ← NNReal.coe_lt_coe, coe_nnnorm, NNReal.coe_div]
     simp_rw [← dist_zero_right]
+    rcases eq_or_ne r 0 with rfl | hr
+    · simp [eq_comm] at hr
     refine Metric.ball_mem_nhds _ ?_
-    simp only [dist_zero_right, norm_pos_iff, ne_eq, coe_posSubmonoid_ne_zero, not_false_eq_true,
-      div_pos_iff_of_pos_right]
-    rintro rfl
-    simp [eq_comm] at hr
+    simp [hr]
 
 end NormedField
