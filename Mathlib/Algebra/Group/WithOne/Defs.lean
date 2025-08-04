fce431f224ca2e9bcf4ab7973d1a68d3dc911ae3
Yakov Pechersky <pechersky@users.noreply.github.com>
2025-08-04 10:45:44 -0400

    increase import

--- Mathlib/Algebra/Group/WithOne/Defs.lean
+++ Mathlib/Algebra/Group/WithOne/Defs.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro, Johan Commelin
 -/
 import Mathlib.Algebra.Group.Defs
+import Mathlib.Data.Option.Basic
 import Mathlib.Logic.Nontrivial.Basic
 import Mathlib.Tactic.Common
 
