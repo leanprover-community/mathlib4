7516ecf72e961651f5bfe85148976d9fe81a4f7a
Amelia Livingston <al3717@ic.ac.uk>
2025-08-04 23:09:55 +0100

    fix

--- Mathlib/RepresentationTheory/Basic.lean
+++ Mathlib/RepresentationTheory/Basic.lean
@@ -380,8 +380,8 @@ lemma apply_sub_id_partialSum_eq (n : ℕ) (g : G) (x : V) :
   induction n with
   | zero => simp [Fin.partialSum]
   | succ n h =>
-    have : (fun (j : Fin (n + 2)) => ρ (g ^ (j : ℕ)) x) ∘ Fin.castSucc =
-      fun (j : Fin (n + 1)) => ρ (g ^ (j : ℕ)) x := by ext; simp
+    have : Fin.init (fun (j : Fin (n + 2)) => ρ (g ^ (j : ℕ)) x) =
+      fun (j : Fin (n + 1)) => ρ (g ^ (j : ℕ)) x := by ext; simp [Fin.init]
     rw [← Fin.succ_eq_last_succ.2 rfl, Fin.partialSum_succ, ← Fin.partialSum_init, map_add,
       this, h]
     simp [pow_succ']
