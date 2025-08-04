5fd534f15155d78355855d319f4b86ce6fccff11
Junyan Xu <junyanxu.math@gmail.com>
2025-08-05 00:29:23 +0200

    Update Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -1020,8 +1020,8 @@ import Mathlib.Algebra.Polynomial.Taylor
 import Mathlib.Algebra.Polynomial.UnitTrinomial
 import Mathlib.Algebra.PresentedMonoid.Basic
 import Mathlib.Algebra.Prime.Defs
-import Mathlib.Algebra.Prime.Prod
 import Mathlib.Algebra.Prime.Lemmas
+import Mathlib.Algebra.Prime.Prod
 import Mathlib.Algebra.QuadraticDiscriminant
 import Mathlib.Algebra.Quandle
 import Mathlib.Algebra.Quaternion
