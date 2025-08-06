fd9e7322fa924963234676446883164ff7000965
Kenny Lau <kc_kennylau@yahoo.com.hk>
2025-08-06 10:29:45 +0100

    Update Mathlib/LinearAlgebra/Finsupp/Defs.lean
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- Mathlib/LinearAlgebra/Finsupp/Defs.lean
+++ Mathlib/LinearAlgebra/Finsupp/Defs.lean
@@ -165,7 +165,7 @@ def mapDomain.linearEquiv (f : α ≃ α') : (α →₀ M) ≃ₗ[R] (α' →₀
     simp [← mapDomain_comp]
 
 @[simp] theorem mapDomain.linearEquiv_coe (f : α ≃ α') :
-    (linearEquiv M R f : _ → _) = mapDomain f := rfl
+    ⇑(linearEquiv M R f) = mapDomain f := rfl
 
 @[simp] theorem mapDomain.linearEquiv_toLinearMap (f : α ≃ α') :
     (linearEquiv M R f : _ →ₗ[R] _) = lmapDomain M R f := rfl
