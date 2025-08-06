a2aaea89060f1923c0b9fd2edbd604f0f5478135
Jovan Gerbscheid <56355248+JovanGerb@users.noreply.github.com>
2025-08-06 08:52:25 +0200

    Update IterateMapComap.lean

--- Mathlib/Algebra/Module/Submodule/IterateMapComap.lean
+++ Mathlib/Algebra/Module/Submodule/IterateMapComap.lean
@@ -52,7 +52,7 @@ theorem iterateMapComap_le_succ (K : Submodule R N) (h : K.map f ≤ K.map i) (n
     simp_rw [iterateMapComap, iterate_succ', Function.comp_apply]
     calc
       _ ≤ (f.iterateMapComap i n K).map i := map_comap_le _ _
-      _ ≤ (((f.iterateMapComap i n K).map f).comap f).map i := map_mono (le_comap_map _ _)
+      _ ≤ (((f.iterateMapComap i n K).map f).comap f).map i := by grw [← le_comap_map]
       _ ≤ _ := by gcongr; exact ih
 
 /-- If `f` is surjective, `i` is injective, and there exists some `m` such that
