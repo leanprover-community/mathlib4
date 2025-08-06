00d6bef9f02e1e33bf9334f4ac5e365d407add81
Yongshun Shreck Ye <ShreckYe@gmail.com>
2025-08-06 20:11:01 +0800

    Move the destructuring alias after `Prime.dvd_mul`

--- Mathlib/Data/Nat/Prime/Defs.lean
+++ Mathlib/Data/Nat/Prime/Defs.lean
@@ -422,11 +422,11 @@ theorem Prime.coprime_iff_not_dvd {p n : ℕ} (pp : Prime p) : Coprime p n ↔ �
   ⟨fun co d => pp.not_dvd_one <| co.dvd_of_dvd_mul_left (by simp [d]), fun nd =>
     coprime_of_dvd fun _ m2 mp => ((prime_dvd_prime_iff_eq m2 pp).1 mp).symm ▸ nd⟩
 
-alias ⟨Prime.dvd_or_dvd, _⟩ := Prime.dvd_mul
-
 theorem Prime.dvd_mul {p m n : ℕ} (pp : Prime p) : p ∣ m * n ↔ p ∣ m ∨ p ∣ n :=
   ⟨fun H => or_iff_not_imp_left.2 fun h => (pp.coprime_iff_not_dvd.2 h).dvd_of_dvd_mul_left H,
     Or.rec (fun h : p ∣ m => h.mul_right _) fun h : p ∣ n => h.mul_left _⟩
+
+alias ⟨Prime.dvd_or_dvd, _⟩ := Prime.dvd_mul
 
 theorem prime_iff {p : ℕ} : p.Prime ↔ _root_.Prime p :=
   ⟨fun h => ⟨h.ne_zero, h.not_isUnit, fun _ _ => h.dvd_mul.mp⟩, Prime.irreducible⟩
