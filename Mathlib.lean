f51a697fdb109ee243eba83f5bd91382dffbc4a2
Yaël Dillies <yael.dillies@gmail.com>
2025-08-06 07:02:37 +0000

    chore(Algebra/Notation): move `Pi.single` earlier The definition doesn't need anything about monoids/groups.
    This is useful to disentangle `Finsupp` results downstream.

--- Mathlib.lean
+++ Mathlib.lean
@@ -760,7 +760,8 @@ import Mathlib.Algebra.NoZeroSMulDivisors.Prod
 import Mathlib.Algebra.Notation
 import Mathlib.Algebra.Notation.Defs
 import Mathlib.Algebra.Notation.Lemmas
-import Mathlib.Algebra.Notation.Pi
+import Mathlib.Algebra.Notation.Pi.Basic
+import Mathlib.Algebra.Notation.Pi.Defs
 import Mathlib.Algebra.Notation.Prod
 import Mathlib.Algebra.Opposites
 import Mathlib.Algebra.Order.AbsoluteValue.Basic
