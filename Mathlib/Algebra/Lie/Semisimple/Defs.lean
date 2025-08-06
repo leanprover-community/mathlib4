4b44b85af9e0c69e5c88143793cd71175386ac04
Oliver Nash <github@olivernash.org>
2025-08-06 11:47:42 +0100

    Convenience constructor for `IsSimpleOrder`

--- Mathlib/Algebra/Lie/Semisimple/Defs.lean
+++ Mathlib/Algebra/Lie/Semisimple/Defs.lean
@@ -31,8 +31,8 @@ abbrev LieModule.IsIrreducible : Prop :=
 
 variable {R L M} in
 lemma LieModule.IsIrreducible.mk [Nontrivial M] (h : ∀ N : LieSubmodule R L M, N ≠ ⊥ → N = ⊤) :
-    IsIrreducible R L M where
-  eq_bot_or_eq_top N := or_iff_not_imp_left.mpr <| h N
+    IsIrreducible R L M :=
+  IsSimpleOrder.mk' h
 
 lemma LieSubmodule.eq_top_of_isIrreducible [LieModule.IsIrreducible R L M]
     (N : LieSubmodule R L M) [Nontrivial N] :
