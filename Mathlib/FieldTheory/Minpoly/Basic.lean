258069e59f0d03d3590f791ae6401bb0ae10a404
Junyan Xu <junyanxu.math@gmail.com>
2025-08-06 13:34:29 +0200

    docstring

--- Mathlib/FieldTheory/Minpoly/Basic.lean
+++ Mathlib/FieldTheory/Minpoly/Basic.lean
@@ -157,6 +157,9 @@ theorem unique' {p : A[X]} (hm : p.Monic) (hp : Polynomial.aeval x p = 0)
     mul_one]
 
 open Polynomial in
+/-- If a monic polynomial `p : A[X]` of degree `n` annihilates an element `x` in an `A`-algebra `B`,
+such that `{xⁱ | 0 ≤ i < n} is linearly independent over `A`, then `p` is the minimal polynomial
+of `x` over `A`. -/
 theorem eq_of_linearIndependent {p : A[X]} (monic : p.Monic) (hp0 : p.aeval x = 0)
     (n : ℕ) (hpn : p.degree = n) (ind : LinearIndependent A fun i : Fin n ↦ x ^ i.val) :
     minpoly A x = p :=
