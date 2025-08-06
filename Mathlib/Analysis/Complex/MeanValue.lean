b58e17d6ed56b83d08f748029ad2543f046d04b6
Stefan Kebekus <kebekus@users.noreply.github.com>
2025-08-06 09:57:37 +0200

    Apply suggestions from code review
    Co-authored-by: EtienneC30 <66847262+EtienneC30@users.noreply.github.com>

--- Mathlib/Analysis/Complex/MeanValue.lean
+++ Mathlib/Analysis/Complex/MeanValue.lean
@@ -22,16 +22,12 @@ private theorem circleAverage_of_differentiable_on_off_countable_posRadius (hR :
     (h₂f : ∀ z ∈ ball c R \ s, DifferentiableAt ℂ f z) :
     circleAverage f c R = f c := by
   calc circleAverage f c R
-  _ = (2 * π * I)⁻¹ • (∮ (z : ℂ) in C(c, R), (z - c)⁻¹ • f z) := by
-    rw [circleAverage_eq_circleIntegral]
-    exact (ne_of_lt hR).symm
+  _ = (2 * π * I)⁻¹ • (∮ (z : ℂ) in C(c, R), (z - c)⁻¹ • f z) :=
+    circleAverage_eq_circleIntegral hR.ne'
   _ = f c := by
     rw [circleIntegral_sub_center_inv_smul_of_differentiable_on_off_countable hR hs h₁f h₂f,
       ← smul_assoc]
-    nth_rw 2 [((MulAction.one_smul (f c)).symm : f c = (1 : ℂ) • f c)]
-    congr
-    rw [smul_eq_mul]
-    apply inv_mul_cancel₀ two_pi_I_ne_zero
+    field_simp [pi_ne_zero]
 
 /--
 The **Mean Value Property** of complex differentiable functions: If `f : ℂ → E` is continuous on a
@@ -41,7 +37,7 @@ points of its interior, then the circle average `circleAverage f c R` equals `f 
 theorem circleAverage_of_differentiable_on_off_countable (hs : s.Countable)
     (h₁f : ContinuousOn f (closedBall c |R|)) (h₂f : ∀ z ∈ ball c |R| \ s, DifferentiableAt ℂ f z) :
     circleAverage f c R = f c := by
-  rcases lt_trichotomy 0 R with h|h|h
+  rcases lt_trichotomy 0 R with h | rfl | h
   · rw [← abs_of_pos h]
     exact circleAverage_of_differentiable_on_off_countable_posRadius (abs_pos_of_pos h) hs h₁f h₂f
   · simp [h.symm]
@@ -54,9 +50,7 @@ differentiable at all points of a closed disc of radius `R` and center `c`, then
 `circleAverage f c R` equals `f c`.
 -/
 theorem circleAverage_of_differentiable_on (hf : ∀ z ∈ closedBall c |R|, DifferentiableAt ℂ f z) :
-    circleAverage f c R = f c := by
-  apply circleAverage_of_differentiable_on_off_countable Set.countable_empty
+    circleAverage f c R = f c :=
+  circleAverage_of_differentiable_on_off_countable Set.countable_empty
     (fun x hx ↦ (hf x hx).continuousAt.continuousWithinAt)
-  intro z hz
-  apply hf z
-  simp_all [le_of_lt]
+    fun z hz ↦ hf z (by simp_all [le_of_lt])
