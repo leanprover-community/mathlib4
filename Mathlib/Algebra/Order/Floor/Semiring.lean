a7bdd14463ad2e4043ebade65b20f3212f6f3148
Yongshun Shreck Ye <ShreckYe@gmail.com>
2025-08-07 20:45:01 +0800

    Add `ceil_lt_add_one'` which is not completed yet

--- Mathlib/Algebra/Order/Floor/Semiring.lean
+++ Mathlib/Algebra/Order/Floor/Semiring.lean
@@ -343,6 +343,19 @@ theorem ceil_lt_add_one (ha : 0 ≤ a) : (⌈a⌉₊ : R) < a + 1 :=
   lt_ceil.1 <| (Nat.lt_succ_self _).trans_le (ceil_add_one ha).ge
 
 @[bound]
+lemma ceil_lt_add_one' [Neg R] (ha : -1 < a) : ⌈a⌉₊ < a + 1 := by
+  by_cases h : a ≥ 0
+  · exact ceil_lt_add_one h
+  · have : ⌈a⌉₊ = 0 :=
+      ceil_eq_zero.mpr (le_of_not_ge h)
+    rw [this]
+    norm_cast
+    suffices -1 + 1 < a + 1 by
+      convert this
+      sorry
+    gcongr
+
+@[bound]
 theorem ceil_add_le (a b : R) : ⌈a + b⌉₊ ≤ ⌈a⌉₊ + ⌈b⌉₊ := by
   rw [ceil_le, Nat.cast_add]
   gcongr <;> apply le_ceil
