14bc2c8d9fe140a058fcec06a2dd600c2cb27ae9
artie2000 <artem.khovanov@gmail.com>
2025-08-07 21:27:00 +0100

    change instance priority to 100

--- Mathlib/Algebra/Order/Ring/Defs.lean
+++ Mathlib/Algebra/Order/Ring/Defs.lean
@@ -167,8 +167,9 @@ instance (priority := 200) IsOrderedRing.toMulPosMono : MulPosMono R where
 
 end IsOrderedRing
 
+-- see Note [lower instance priority]
 /-- Turn an ordered domain into a strict ordered ring. -/
-instance IsOrderedRing.toIsStrictOrderedRing (R : Type*)
+instance (priority := 100) IsOrderedRing.toIsStrictOrderedRing (R : Type*)
     [Ring R] [PartialOrder R] [IsOrderedRing R] [NoZeroDivisors R] [Nontrivial R] :
     IsStrictOrderedRing R :=
   .of_mul_pos fun _ _ ap bp ↦ (mul_nonneg ap.le bp.le).lt_of_ne' (mul_ne_zero ap.ne' bp.ne')
