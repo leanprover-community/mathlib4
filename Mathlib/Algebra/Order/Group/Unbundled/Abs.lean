cbcfae573181b21ef41bb455e8403af6640fda03
Yaël Dillies <yael.dillies@gmail.com>
2025-08-07 01:38:37 +0000

    chore(Algebra/Notation): move `Pi.single` earlier (#28027)
    The definition doesn't need anything about monoids/groups.
    
    This is useful to disentangle `Finsupp` results downstream.

--- Mathlib/Algebra/Order/Group/Unbundled/Abs.lean
+++ Mathlib/Algebra/Order/Group/Unbundled/Abs.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jeremy Avigad, Leonardo de Moura, Mario Carneiro, Johannes Hölzl
 -/
 import Mathlib.Algebra.Group.Even
+import Mathlib.Algebra.Group.Pi.Basic
 import Mathlib.Algebra.Order.Group.Lattice
 
 /-!
