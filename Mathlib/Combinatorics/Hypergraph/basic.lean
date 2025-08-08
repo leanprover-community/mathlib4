06a9510132aac5bc965c669d333f440ce164ff65
Evan Walter Clark Spotte-Smith, PhD <espottesmith@gmail.com>
2025-08-08 17:39:52 -0400

    More definitions

--- Mathlib/Combinatorics/Hypergraph/basic.lean
+++ Mathlib/Combinatorics/Hypergraph/basic.lean
@@ -37,6 +37,7 @@ conversions:
 - `Hypergraph α β` ⇒ `Hypergraph β α` (i.e., constructing the *dual* of a hypergraph)
 
 Other (future) aspects of interest:
+- Finiteness
 - Hyperpaths
 - Random hypergraphs
 
@@ -50,7 +51,8 @@ For `H : Hypergraph α β`, ...
     hyperedge `e : β`.
 * `H.IsHyperedge e s` means that the hyperedge `x` contains exactly the vertices contained in
     `s : Set α`.
-* `H.Adj x y` means that there exists some hyperedge containing both `x` and `y`.
+* `H.Adj x y` means that there exists some hyperedge containing both `x` and `y` (or, in other
+    words, `x` and `y` are incident on some shared hyperedge `e`).
 * `H.EAdj e f` means that there exists some vertex that is incident on both hyperedge `e` and
     hyperedge `f : β`.
 
@@ -66,6 +68,11 @@ From `Mathlib.Combinatorics.Graph.Basic`:
 "The main tradeoff is that parts of the API will need to care about whether a term
 `x : α` or `e : β` is a 'real' vertex or edge of the graph, rather than something outside
 the vertex or edge set. This is an issue, but is likely amenable to automation."
+
+TODO:
+  - Should Hypergraph carry around a "hyperlink" set, where a hyperlink is a pair of type
+  (β, Set α)?
+    - Would sort of bypass the need for IsHyperedge
 -/
 
 structure Hypergraph (α β : Type*) where
@@ -82,7 +89,8 @@ namespace Hypergraph
 
 variable {H : Hypergraph α β}
 
-/-! ## Notation-/
+
+/-! ## Notation -/
 
 /-- `V(H)` denotes the `vertexSet` of a hypergraph `H` -/
 scoped notation "V(" H ")" => Hypergraph.vertexSet H
@@ -90,36 +98,19 @@ scoped notation "V(" H ")" => Hypergraph.vertexSet H
 /-- `E(H)` denotes the `hyperedgeSet` of a hypergraph `H` -/
 scoped notation "E(" H ")" => Hypergraph.hyperedgeSet H
 
-/--
-The empty hypergraph
-
-An empty hypergraph contains an empty vertex set and an empty hyperedge set
--/
-def emptyHypergraph (α β : Type*) : Hypergraph α β := Hypergraph.mk ∅ ∅ (fun _ _ => False)
-
-/--
-Predicate to determine if a hypergraph is empty
--/
-def IsEmpty (H : Hypergraph α β) : Prop := V(H) = ∅ ∧ E(H) = ∅
-
-/--
-Predicate to determine if a hypergraph is trivial
-
-A hypergraph is trivial if it has a nonempty vertex set and an empty hyperedge set
--/
-def IsTrivial (H : Hypergraph α β) : Prop := Nonempty V(H) ∧ E(H) = ∅
-
--- * `H.IsHyperedge e s` means that the hyperedge `x` contains exactly the vertices contained in
---     `s : Set α`.
--- * `H.Adj x y` means that there exists some hyperedge containing both `x` and `y`.
--- * `H.EAdj e f` means that there exists some vertex that is incident on both hyperedge `e` and
---     hyperedge `f : β`.
+/-! ## Basic Hypergraph Definitions & Predicates-/
 
 /--
 Predicate to determine if a hyperedge `e` contains exactly the vertex subset `s : Set α`
 -/
 def IsHyperedge (H : Hypergraph α β) (e : β) (s : Set α) : Prop :=
   (∀ x ∈ s, x ∈ V(H) ∧ H.IsIncident x e) ∧ (∀ y ∈ V(H) \ s, ¬H.IsIncident y e)
+
+/--
+We define the *hyperedge vertex set* as the set of all subsets of `V(H)` that represent real
+hyperedges in `E(H)`
+-/
+def hyperedgeVertexSet (H : Hypergraph α β) := {s | ∃ e, H.IsHyperedge e s}
 
 /--
 Predicate for adjacency. Two vertices `x` and `y` are adjacent if there is some
@@ -141,6 +132,36 @@ incident on a hyperedge that is not in the hyperedge set.
 def EAdj (H : Hypergraph α β) (e : β) (f : β) : Prop :=
   ∃ x ∈ V(H), H.IsIncident x e ∧ H.IsIncident x f
 
+
+/-! ## Common Example Hypergraphs -/
+
+/--
+The empty hypergraph
+
+An empty hypergraph contains an empty vertex set and an empty hyperedge set
+-/
+def emptyHypergraph (α β : Type*) : Hypergraph α β := Hypergraph.mk ∅ ∅ (fun _ _ => False)
+
+/--
+A singleton hypergraph
+
+A singleton hypergraph contains only one vertex and an empty hyperedge set
+-/
+def singletonHypergraph (α β : Type*) (x : α) : Hypergraph α β :=
+  Hypergraph.mk {x} ∅ (fun _ _ => False)
+
+
+/-! ## Additional Predicates -/
+
+/--
+Predicate to determine if a vertex is isolated, meaning that it is not incident on any hyperedges.
+Note that this includes loops, i.e., if vertex `x` is isolated, there is no hyperedge with
+associated vertex subset `{x}`
+-/
+def IsIsolated (H : Hypergraph α β) (x : α) : Prop :=
+  ∀ e ∈ E(H), ¬H.IsIncident x e
+
+
 /--
 Predicate to determine if a hyperedge `e` is a loop, meaning that its associated vertex subset `s`
 contains only one vertex, i.e., `|s| = 1`
@@ -149,6 +170,25 @@ TODO: am I using Set.encard right?
 -/
 def IsLoop (H : Hypergraph α β) (e : β) (s : Set α) : Prop :=
   H.IsHyperedge e s ∧ (Set.encard s) = 1
+
+/--
+Predicate to determine if a hypergraph is empty
+-/
+def IsEmpty (H : Hypergraph α β) : Prop := V(H) = ∅ ∧ E(H) = ∅
+
+/--
+Predicate to determine if a hypergraph is trivial
+
+A hypergraph is trivial if it has a nonempty vertex set and an empty hyperedge set
+-/
+def IsTrivial (H : Hypergraph α β) : Prop := Nonempty V(H) ∧ E(H) = ∅
+
+/--
+Predicate to determine is a hypergraph `H` is complete, meaning that each member of the power set of
+the vertices (`𝒫 V(H)`) is represented in `E(H)`
+-/
+def IsComplete (H : Hypergraph α β) : Prop :=
+  ∀ s ∈ 𝒫 V(H), ∃ e ∈ E(H), H.IsHyperedge e s
 
 /--
 Predicate to determine if a hypergraph is simple
@@ -161,6 +201,8 @@ TODO: define this in a sane way
 -/
 def IsSimple (H : Hypergraph α β) : Prop := sorry
 
+
+/-! Cardinality -/
 
 
 end Hypergraph
