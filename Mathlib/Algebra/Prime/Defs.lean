7f02d929a950fc59eb732e7196ed19fdeb888ffb
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2025-08-09 00:08:01 +0200

    todo rename

--- Mathlib/Algebra/Prime/Defs.lean
+++ Mathlib/Algebra/Prime/Defs.lean
@@ -94,6 +94,7 @@ theorem not_prime_one : ¬Prime (1 : M) := fun h => h.not_unit isUnit_one
 
 end Prime
 
+-- TODO rename: Irreducible.not_dvd_of_isUnit
 theorem Irreducible.not_dvd_isUnit [CommMonoid M] {p u : M} (hp : Irreducible p) (hu : IsUnit u) :
     ¬p ∣ u :=
   mt (isUnit_of_dvd_unit · hu) hp.not_isUnit
