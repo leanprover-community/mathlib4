123812fed5e9dfebae86b7d6f3a102c05e0cb919
Jon Bannon <jbannon@siena.edu>
2025-08-08 14:47:33 -0400

    Still having issues placing protected theorem EventuallyEq.star {R : Type*} [Star R] {f g : α → R} {l : Filter α}     (h : f =ᶠ[l] g) : (fun x ↦ star (f x)) =ᶠ[l] fun x ↦ star (g x) :=   h.fun_comp Star.star.
    If I put this in Mathlib.Order.Filter.Basic we get import collisions with other files. (Probably some kind of circular import.) But where to put it?

--- Mathlib/MeasureTheory/Function/StronglyMeasurable/AEStronglyMeasurable.lean
+++ Mathlib/MeasureTheory/Function/StronglyMeasurable/AEStronglyMeasurable.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Rémy Degenne, Sébastien Gouëzel
 -/
 import Mathlib.MeasureTheory.Function.StronglyMeasurable.Basic
-import Mathlib.Order.Filter.Basic
 
 /-!
 # Strongly measurable and finitely strongly measurable functions
