24985c424bd405c6f6dbad4598c02693e587400e
Xiangyu Li <xiangyuu.li@mail.utoronto.ca>
2025-08-08 16:10:01 -0400

    chore(all): regen import-all after adding SimplicialComplex

--- Mathlib.lean
+++ Mathlib.lean
@@ -2869,6 +2869,7 @@ import Mathlib.Combinatorics.SimpleGraph.Turan
 import Mathlib.Combinatorics.SimpleGraph.Tutte
 import Mathlib.Combinatorics.SimpleGraph.UniversalVerts
 import Mathlib.Combinatorics.SimpleGraph.Walk
+import Mathlib.Combinatorics.SimplicialComplex.Basic
 import Mathlib.Combinatorics.Young.SemistandardTableau
 import Mathlib.Combinatorics.Young.YoungDiagram
 import Mathlib.Computability.Ackermann
