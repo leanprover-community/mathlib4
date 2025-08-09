b2a6a187ef39d1e23e39d24e35b04a2966260c8e
LLaurance <laurancelau.2022@berkeley.edu>
2025-08-10 06:52:32 +0800

    Minimize imports

--- Mathlib/Combinatorics/SimpleGraph/Connectivity/Connected.lean
+++ Mathlib/Combinatorics/SimpleGraph/Connectivity/Connected.lean
@@ -3,7 +3,6 @@ Copyright (c) 2021 Kyle Miller. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kyle Miller
 -/
-import Mathlib.Combinatorics.SimpleGraph.Connectivity.WalkDecomp
 import Mathlib.Combinatorics.SimpleGraph.Paths
 import Mathlib.Combinatorics.SimpleGraph.Subgraph
 
