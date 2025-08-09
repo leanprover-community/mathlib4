037f80281805ec8efd4b3d645c55852a922789ab
David J. Webb <david.webb@chaminade.edu>
2025-08-09 01:12:33 -1000

    modified:   Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -2870,7 +2870,6 @@ import Mathlib.Computability.Partrec
 import Mathlib.Computability.PartrecCode
 import Mathlib.Computability.PostTuringMachine
 import Mathlib.Computability.Primrec
-import Mathlib.Computability.PrimrecFilter
 import Mathlib.Computability.Reduce
 import Mathlib.Computability.RegularExpressions
 import Mathlib.Computability.TMComputable
