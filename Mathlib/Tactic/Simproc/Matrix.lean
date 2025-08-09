fca0dd4d0412334661c1a6d0e96b32dc88f44a13
Kenny Lau <kc_kennylau@yahoo.com.hk>
2025-08-09 17:06:36 +0100

    some progress

--- Mathlib/Tactic/Simproc/Matrix.lean
+++ Mathlib/Tactic/Simproc/Matrix.lean
@@ -70,15 +70,47 @@ def mkMatrixLit {u : Level} {m n : ℕ} {R : Q(Type u)} (M : Matrix (Fin m) (Fin
 
 end Qq
 
-/-- A simproc for terms of the form `Matrix.transpose (Matrix.of _)`. -/
-simproc matrix_transpose (Matrix.transpose (Matrix.of _)) := .ofQ fun u α MT? ↦ do
+/-- A simproc for terms of the form `Matrix.diagonal _`. -/
+simproc matrix_diagonal (Matrix.diagonal _) := .ofQ fun u α M? ↦ do
+  -- trace[debug] m!"{M?}"
+  -- haveI' : $α =Q Matrix (Fin (OfNat.ofNat $k?)) (Fin (OfNat.ofNat $k?)) $R := ⟨⟩
+  -- let .some _ ← trySynthInstanceQ q(Zero ($R)) | return .continue
+  -- match M? with
   let .succ _ := u | return .continue
-  let ~q(@Matrix (Fin (OfNat.ofNat $n?)) (Fin (OfNat.ofNat $m?)) $R) := α | return .continue
-  let ~q(transpose $M?) := MT? | return .continue
-  let m := m?.natLit!
-  let n := n?.natLit!
-  let .some M ← matrixLit! (m := m) (n := n) (R := R) M? | return .continue
-  return .visit { expr := mkMatrixLit (Mᵀ), proof? := .some q(Eq.symm (etaExpand_eq $MT?)) }
+  let ~q(@Matrix (Fin (OfNat.ofNat $k?)) (Fin (OfNat.ofNat $k?)) $R) := α | return .continue
+  trace[debug] m!"{R}, {α}, {M?}, {k?}"
+  let ~q(@diagonal (Fin (OfNat.ofNat $k?)) $R $dF $zR $v) := M? | return .continue
+  return .continue
+  -- | _ => return .continue
+  -- let ~q(diagonal $v) := M? | return .continue
+  -- let ~q(@Nat.cast (Matrix (Fin $k) (Fin $k) $R) (@instNatCastOfZero $_ $R $_ $_ $inst) $n) := M? | return .continue
+  -- return .continue
+  -- let ~q(transpose $M?) := MT? | return .continue
+  -- let m := m?.natLit!
+  -- let n := n?.natLit!
+  -- let .some M ← matrixLit! (m := m) (n := n) (R := R) M? | return .continue
+  -- return .visit { expr := mkMatrixLit (Mᵀ), proof? := .some q(Eq.symm (etaExpand_eq $MT?)) }
 
-example : (!![1, 2, 3; 4, 5, 6]ᵀ : Matrix (Fin (2+1)) (Fin 2) ℤ) = !![1, 4; 2, 5; 3, 6] := by
+set_option trace.debug true
+
+example (n : ℤ) : (diagonal ![37, -1] : Matrix (Fin 2) (Fin 2) ℤ) = !![37, 0; 0, -1] := by
+  simp
+
+example {α : Type*} [Zero α] :
+    (0 : Matrix (Fin 2) (Fin 2) α) = !![0, 0; 0, 0] := by
+  simp
+
+example {α : Type*} [Zero α] [One α] :
+    (1 : Matrix (Fin 2) (Fin 2) α) = !![1, 0; 0, 1] := by
+  simp
+
+example {α : Type*} [Zero α] [NatCast α] :
+    (37 : Matrix (Fin 2) (Fin 2) α) = !![37, 0; 0, 37] := by
+  simp
+
+example {α : Type*} [Zero α] [NatCast α] (n : ℕ) :
+    (n : Matrix (Fin 2) (Fin 2) α) = !![(n : α), 0; 0, n] := by
+  simp
+
+example (n : ℤ) : (n : Matrix (Fin 2) (Fin 2) ℤ) = !![-3, 0; 0, -3] := by
   simp
