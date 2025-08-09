4bbfce882b5fbad11cd2471c6500da189f6a3c7c
Sebi-Kumar <sebikumar@outlook.com>
2025-08-09 11:23:59 -0400

    Fixed typo

--- Mathlib/Topology/Subpath.lean
+++ Mathlib/Topology/Subpath.lean
@@ -72,7 +72,7 @@ lemma subpath_range_of_le (γ : Path a b) (t₀ t₁ : I) (h : t₀ ≤ t₁) :
 @[simp]
 lemma subpath_range_of_ge (γ : Path a b) (t₀ t₁ : I) (h : t₁ ≤ t₀) :
     range (γ.subpath t₀ t₁) = γ '' (Icc t₁ t₀) := by
-  rw [← subpath_symm, symm_range, subpath_range₀ _ _ _ h]
+  rw [← subpath_symm, symm_range, subpath_range_of_le _ _ _ h]
 
 /-- The range of a subpath is the image of the original path on the relevant interval. -/
 @[simp]
