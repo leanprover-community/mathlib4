766698306bcd972904a995ec5846de5baa4e3d2e
Kenny Lau <kc_kennylau@yahoo.com.hk>
2025-08-10 02:01:46 +0100

    rw proc

--- Mathlib/Tactic/Simproc/Matrix.lean
+++ Mathlib/Tactic/Simproc/Matrix.lean
@@ -20,64 +20,102 @@ attribute [-simp] cons_transpose
 namespace Qq
 
 /-- Decompose a vector expression into a vector of expressions. -/
-def piFinLit! {u : Level} {n : ℕ} {R : Q(Type u)} (e : Q(Fin $n → $R)) :
+def piFinLit? {u : Level} {n : ℕ} {R : Q(Type u)} (e : Q(Fin $n → $R)) :
     MetaM (Option (Fin n → Q($R))) := do
   match n with
   | 0 => return .some ![]
   | _+1 =>
     let ~q(vecCons $eh? $et?) := e | return .none
-    let .some et ← piFinLit! et? | return .none
+    let .some et ← piFinLit? et? | return .none
     return .some (vecCons eh? et)
 
 /-- Decompose a "double vector" expression, i.e. `Fin m → Fin n → R`, into a double vector of
 expressions. -/
-def piFinPiFinLit! {u : Level} {m n : ℕ} {R : Q(Type u)} (e : Q(Fin $m → Fin $n → $R)) :
+def piFinPiFinLit? {u : Level} {m n : ℕ} {R : Q(Type u)} (e : Q(Fin $m → Fin $n → $R)) :
     MetaM (Option (Fin m → Fin n → Q($R))) := do
   match m with
   | 0 => return .some ![]
   | _+1 =>
     let ~q(vecCons $eh? $et?) := e | return .none
-    let .some eh ← piFinLit! eh? | return .none
-    let .some et ← piFinPiFinLit! et? | return .none
+    let .some eh ← piFinLit? eh? | return .none
+    let .some et ← piFinPiFinLit? et? | return .none
     return .some (vecCons eh et)
 
 /-- Decompose a matrix expression into a matrix of expressions. -/
-def matrixLit! {u : Level} {m n : ℕ} {R : Q(Type u)} (M : Q(Matrix (Fin $m) (Fin $n) $R)) :
+def matrixLit? {u : Level} {m n : ℕ} {R : Q(Type u)} (M : Q(Matrix (Fin $m) (Fin $n) $R)) :
     MetaM (Option (Matrix (Fin m) (Fin n) Q($R))) := do
   let ~q(of $M) := M | return .none
-  let .some e ← piFinPiFinLit! M | return .none
+  let .some e ← piFinPiFinLit? M | return .none
   return of e
 
-/-- Construct a vector expression from a vector of expressions. -/
-def mkPiFinLit {u : Level} {n : ℕ} {R : Q(Type u)} (e : Fin n → Q($R)) :
-    Q(Fin $n → $R) :=
-  match n with
-  | 0 => q(![])
-  | _+1 => q(vecCons $(e 0) $(mkPiFinLit (e ∘ Fin.succ)))
+/-- Given a matrix of expressions, construct a proof of `(mkLiteralQ M)ᵀ = mkLiteralQ (Mᵀ)`. -/
+def mkTransposeProof {u : Level} {α : Q(Type u)} {m n : ℕ} (M : Matrix (Fin m) (Fin n) Q($α)) :
+    (P : Q(Prop)) × Q($P) :=
+  ⟨q($(mkLiteralQ M)ᵀ = $(mkLiteralQ Mᵀ)),
+  cast (by rfl) q((etaExpand_eq $(mkLiteralQ M)ᵀ).symm)⟩
 
-/-- Construct a double vector expression from a double vector of expressions. -/
-def mkPiFinPiFinLit {u : Level} {m n : ℕ} {R : Q(Type u)} (e : Fin m → Fin n → Q($R)) :
-    Q(Fin $m → Fin $n → $R) :=
-  match m with
-  | 0 => q(![])
-  | _+1 => q(vecCons $(mkPiFinLit (e 0)) $(mkPiFinPiFinLit (e ∘ Fin.succ)))
-
-/-- Construct a matrix expression from a matrix of expressions. -/
-def mkMatrixLit {u : Level} {m n : ℕ} {R : Q(Type u)} (M : Matrix (Fin m) (Fin n) Q($R)) :
-    Q(Matrix (Fin $m) (Fin $n) $R) :=
-  q(of $(mkPiFinPiFinLit M))
+/-- Prove a statement of the form
+```
+theorem Matrix.transpose₂₃ {α : Type*} (a₀₀ a₀₁ a₀₂ a₁₀ a₁₁ a₁₂ : α) :
+    !![a₀₀, a₀₁, a₀₂; a₁₀, a₁₁, a₁₂]ᵀ = !![a₀₀, a₁₀; a₀₁, a₁₁; a₀₂, a₁₂] :=
+  (etaExpand_eq _).symm
+```
+Returns the type of this statement and its proof. -/
+def mkTransposeProp (u : Level) (m n : ℕ) : (P : Q(Prop)) × Q($P) :=
+  let α : Q(Type u) := Expr.fvar ⟨`α⟩
+  let varName (i : Fin m) (j : Fin n) : Name :=
+    ("a_" ++ i.val.repr ++ "_" ++ j.val.repr).toName
+  let var (i : Fin m) (j : Fin n) : Q($α) :=
+    .fvar ⟨varName i j⟩
+  let ⟨P', pf'⟩ := mkTransposeProof (of var)
+  let argsE : List Expr :=
+    [α] ++ (List.finRange m).flatMap fun i : Fin m ↦ List.ofFn fun j : Fin n ↦ by exact var i j
+  let argsD : List LocalDecl :=
+    [.cdecl 0 ⟨`α⟩ `α (.sort u.succ) .implicit .default] ++
+    (List.finRange m).flatMap fun i : Fin m ↦ List.ofFn fun j : Fin n ↦
+      .cdecl (finProdFinEquiv (i, j) + 1) ⟨varName i j⟩ (varName i j) α .default .default
+  ⟨Closure.mkForall argsD.toArray (P'.abstract argsE.toArray),
+  Closure.mkLambda argsD.toArray (pf'.abstract argsE.toArray)⟩
 
 end Qq
 
-/-- A simproc for terms of the form `Matrix.transpose (Matrix.of _)`. -/
-simproc matrix_transpose (Matrix.transpose (Matrix.of _)) := .ofQ fun u α MT? ↦ do
-  let .succ _ := u | return .continue
-  let ~q(@Matrix (Fin (OfNat.ofNat $n?)) (Fin (OfNat.ofNat $m?)) $R) := α | return .continue
-  let ~q(transpose $M?) := MT? | return .continue
-  let m := m?.natLit!
-  let n := n?.natLit!
-  let .some M ← matrixLit! (m := m) (n := n) (R := R) M? | return .continue
-  return .visit { expr := mkMatrixLit (Mᵀ), proof? := .some q(Eq.symm (etaExpand_eq $MT?)) }
+open Elab Tactic
 
-example : (!![1, 2, 3; 4, 5, 6]ᵀ : Matrix (Fin (2+1)) (Fin 2) ℤ) = !![1, 4; 2, 5; 3, 6] := by
-  simp
+/-- A custom theorem for reducing transpose of explicit matrices. For example, `transpose_of% 2 3`
+is the theorem saying `!![a, b, c; d, e, f]ᵀ = !![a, d; b, e; c, f]`. -/
+elab:max (name := transpose_tac_elab)
+    "transpose_of% " mStx:term:max nStx:term:max : term => do
+  let u ← Lean.Meta.mkFreshLevelMVar
+  let m : Q(Nat) ← Term.elabTermEnsuringType mStx (mkConst ``Nat)
+  let n : Q(Nat) ← Term.elabTermEnsuringType nStx (mkConst ``Nat)
+  let some m ← (evalNat m).run
+    | throwErrorAt mStx "Expecting a natural number, have{indentD m}"
+  let some n ← (evalNat n).run
+    | throwErrorAt nStx "Expecting a natural number, have{indentD n}"
+  let ⟨P, h⟩ := mkTransposeProp u m n
+  let .some h ← checkTypeQ h P | throwError m!"Wrong proof generated for {m}, {n}."
+  mkExpectedTypeHint h P
+
+example (u : Matrix (Fin 2) (Fin 3) ℤ) (v : Matrix (Fin 3) (Fin 2) ℤ)
+    (hu : u = !![1, 2, 3; 4, 5, 6]) (hv : v = !![1, 4; 2, 5; 3, 6]) :
+    uᵀ = v := by
+  rw [hu, transpose_of% 2 3, hv]
+
+-- Here we use the convention that `e{name}` is the expression for `{name}`, e.g. `eMT` is the
+-- expression corresponding to the transpose of the matrix `M`.
+/-- A simproc for terms of the form `Matrix.transpose (Matrix.of _)`. -/
+simproc matrix_transpose (Matrix.transpose (Matrix.of _)) := .ofQ fun u α eMT ↦ do
+  let .succ _ := u | return .continue
+  let ~q(@Matrix (Fin (OfNat.ofNat $en)) (Fin (OfNat.ofNat $em)) $R) := α | return .continue
+  let ~q(transpose $eM) := eMT | return .continue
+  let .some M ← matrixLit? (m := em.natLit!) (n := en.natLit!) (R := R) eM | return .continue
+  let ⟨P, h⟩ := mkTransposeProof M
+  let ~q($lhs = $rhs) := P | return .continue
+  return .visit { expr := rhs, proof? := .some h }
+
+example (u : Matrix (Fin 2) (Fin 3) ℤ) (v : Matrix (Fin 3) (Fin 2) ℤ)
+    (hu : u = !![1, 2, 3; 4, 5, 6]) (hv : v = !![1, 4; 2, 5; 3, 6]) :
+    uᵀ = v := by
+  rw [hu]
+  simp only [matrix_transpose]
+  rw [hv]
