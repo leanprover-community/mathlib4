24014e012d68e1b34e3f447955bc5747e29afce7
Violeta Hernández <vi.hdz.p@gmail.com>
2025-08-10 07:33:59 -0600

    abs_natCast

--- Mathlib/Data/Nat/Cast/Order/Ring.lean
+++ Mathlib/Data/Nat/Cast/Order/Ring.lean
@@ -22,8 +22,7 @@ section OrderedSemiring
 we use a generic collection of instances so that it applies in other settings (e.g., in a
 `StarOrderedRing`, or the `selfAdjoint` or `StarOrderedRing.positive` parts thereof). -/
 
-variable [AddMonoidWithOne α] [PartialOrder α]
-variable [AddLeftMono α] [ZeroLEOneClass α]
+variable [AddMonoidWithOne α] [PartialOrder α] [AddLeftMono α] [ZeroLEOneClass α]
 
 /-- Specialisation of `Nat.cast_nonneg'`, which seems to be easier for Lean to use. -/
 @[simp]
@@ -45,6 +44,10 @@ theorem cast_min {α} [Semiring α] [LinearOrder α] [IsStrictOrderedRing α] (m
 theorem cast_max {α} [Semiring α] [LinearOrder α] [IsStrictOrderedRing α] (m n : ℕ) :
     (↑(max m n : ℕ) : α) = max (m : α) n :=
   (@mono_cast α _).map_max
+
+@[simp, norm_cast]
+theorem abs_natCast {α} [Ring α] [Lattice α] [IsOrderedRing α] (n : ℕ) : |(n : α)| = n :=
+  abs_of_nonneg n.cast_nonneg'
 
 section Nontrivial
 
