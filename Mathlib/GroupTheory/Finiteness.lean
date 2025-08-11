6f0bf74343f1fa69b6c9e4202f2a14f907d9ce3c
staroperator <staroperator92@gmail.com>
2025-08-11 15:12:31 +0000

    feat(GroupTheory/Finiteness): `AddMonoid.FG ℕ` and `AddGroup.FG ℤ` (#27786)
    Note: [`Module.Finite.self`](https://leanprover-community.github.io/mathlib4_docs/Mathlib/RingTheory/Finiteness/Basic.html#Module.Finite.self) gives equivalent results as `Module.Finite ℕ ℕ` and `Module.Finite ℤ ℤ`. But there is no instance from `Module.Finite` to `AddMonoid.FG` (there is one converse, [AddMonoid.FG.to_moduleFinite_nat](https://leanprover-community.github.io/mathlib4_docs/Mathlib/RingTheory/Finiteness/Defs.html#AddMonoid.FG.to_moduleFinite_nat)).

--- Mathlib/GroupTheory/Finiteness.lean
+++ Mathlib/GroupTheory/Finiteness.lean
@@ -351,3 +351,21 @@ instance instMonoidFG [FG M] [FG N] : FG (M × N) where
   fg_top := by rw [← Submonoid.top_prod_top]; exact .prod ‹FG M›.fg_top ‹FG N›.fg_top
 
 end Prod
+
+namespace AddMonoid
+
+instance : FG ℕ := by
+  rw [fg_iff, ← Nat.addSubmonoid_closure_one]
+  exact ⟨{1}, rfl, by simp⟩
+
+end AddMonoid
+
+namespace AddGroup
+
+instance : FG ℤ := by
+  rw [fg_iff]
+  refine ⟨{1}, ?_, by simp⟩
+  ext x
+  simp [AddSubgroup.mem_closure_singleton]
+
+end AddGroup
