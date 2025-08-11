79ac2d887cdecf3c1fdb601af055ac66122023b3
euprunin <euprunin@users.noreply.github.com>
2025-08-11 14:16:46 +0200

    chore(RingTheory/HahnSeries): golf `orderTop_add_orderTop_le_orderTop_mul`

--- Mathlib/RingTheory/HahnSeries/Multiplication.lean
+++ Mathlib/RingTheory/HahnSeries/Multiplication.lean
@@ -712,14 +712,7 @@ instance {Γ} [AddCommMonoid Γ] [LinearOrder Γ] [IsOrderedCancelAddMonoid Γ] 
 theorem orderTop_add_orderTop_le_orderTop_mul {Γ}
     [AddCommMonoid Γ] [LinearOrder Γ] [IsOrderedCancelAddMonoid Γ]
     [NonUnitalNonAssocSemiring R] {x y : HahnSeries Γ R} :
-    x.orderTop + y.orderTop ≤ (x * y).orderTop := by
-  by_cases hx : x = 0; · simp [hx]
-  by_cases hy : y = 0; · simp [hy]
-  by_cases hxy : x * y = 0
-  · simp [hxy]
-  rw [orderTop_of_ne hx, orderTop_of_ne hy, orderTop_of_ne hxy, ← WithTop.coe_add,
-    WithTop.coe_le_coe, ← Set.IsWF.min_add]
-  exact Set.IsWF.min_le_min_of_subset support_mul_subset_add_support
+    x.orderTop + y.orderTop ≤ (x * y).orderTop := orderTop_add_le_mul
 
 @[simp]
 theorem order_mul {Γ} [AddCommMonoid Γ] [LinearOrder Γ] [IsOrderedCancelAddMonoid Γ]
