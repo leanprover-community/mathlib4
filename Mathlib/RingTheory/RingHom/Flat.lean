dbdc097769ccc0d8d695800157d577971f9ec3cf
Kenny Lau <kc_kennylau@yahoo.com.hk>
2025-08-11 14:24:24 +0100

    Update Flat.lean

--- Mathlib/RingTheory/RingHom/Flat.lean
+++ Mathlib/RingTheory/RingHom/Flat.lean
@@ -26,9 +26,7 @@ def RingHom.Flat {R : Type u} {S : Type v} [CommRing R] [CommRing S] (f : R →+
 
 lemma flat_algebraMap_iff {R S : Type*} [CommRing R] [CommRing S] [Algebra R S] :
     (algebraMap R S).Flat ↔ Module.Flat R S := by
-  simp only [RingHom.Flat]
-  congr!
-  exact Algebra.algebra_ext _ _ fun _ ↦ rfl
+  rw [RingHom.Flat, toAlgebra_algebraMap]
 
 namespace RingHom.Flat
 
