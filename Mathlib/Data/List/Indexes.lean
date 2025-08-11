5ce0ef3f6820d568a1f349e3ee7e55d85ae1913c
Kim Morrison <kim@tqft.net>
2025-08-11 13:56:35 +1000

    fix shake

--- Mathlib/Data/List/Indexes.lean
+++ Mathlib/Data/List/Indexes.lean
@@ -3,6 +3,8 @@ Copyright (c) 2020 Jannis Limperg. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jannis Limperg
 -/
+import Mathlib.Util.AssertExists
+import Mathlib.Tactic.Cases
 import Mathlib.Data.List.Defs
 
 /-!
