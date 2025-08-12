6ef7e2b5f5f2f69d2c7389a5bb008907555505ae
Chris Birkbeck <c.birkbeck@uea.ac.uk>
2025-08-12 09:07:53 +0100

    name update

--- Mathlib/NumberTheory/Divisors.lean
+++ Mathlib/NumberTheory/Divisors.lean
@@ -748,7 +748,7 @@ section pnat
 
 /-- The map from `Nat.divisorsAntidiagonal n` to `ℕ+ × ℕ+` given by sending `n = a * b`
 to `(a , b)`. -/
-def divisorsAntidiagonal_factors (n : ℕ+) : Nat.divisorsAntidiagonal n → ℕ+ × ℕ+ :=
+def divisorsAntidiagonalFactors (n : ℕ+) : Nat.divisorsAntidiagonal n → ℕ+ × ℕ+ :=
     fun x ↦
    ⟨⟨x.1.1, Nat.pos_of_mem_divisors (Nat.fst_mem_divisors_of_mem_antidiagonal x.2)⟩,
     (⟨x.1.2, Nat.pos_of_mem_divisors (Nat.snd_mem_divisors_of_mem_antidiagonal x.2)⟩ : ℕ+),
@@ -757,13 +757,13 @@ def divisorsAntidiagonal_factors (n : ℕ+) : Nat.divisorsAntidiagonal n → ℕ
 /-- The equivalence from the union over `n` of `Nat.divisorsAntidiagonal n` to `ℕ+ × ℕ+`
 given by sending `n = a * b` to `(a , b)`. -/
 def sigmaAntidiagonalEquivProd : (Σ n : ℕ+, Nat.divisorsAntidiagonal n) ≃ ℕ+ × ℕ+ where
-  toFun x := divisorsAntidiagonal_factors x.1 x.2
+  toFun x := divisorsAntidiagonalFactors x.1 x.2
   invFun x :=
     ⟨⟨x.1.val * x.2.val, mul_pos x.1.2 x.2.2⟩, ⟨x.1, x.2⟩, by simp [Nat.mem_divisorsAntidiagonal]⟩
   left_inv := by
     rintro ⟨n, ⟨k, l⟩, h⟩
     rw [Nat.mem_divisorsAntidiagonal] at h
-    ext <;> simp [divisorsAntidiagonal_factors, ← PNat.coe_injective.eq_iff, h.1]
+    ext <;> simp [divisorsAntidiagonalFactors, ← PNat.coe_injective.eq_iff, h.1]
   right_inv _ := rfl
 
 end pnat
