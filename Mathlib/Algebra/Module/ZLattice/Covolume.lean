e7d686056b70fad4f209a50a791b2771cb7de329
Etienne Marion <baguettes_casino0c@icloud.com>
2025-08-12 11:54:55 +0200

    fix

--- Mathlib/Algebra/Module/ZLattice/Covolume.lean
+++ Mathlib/Algebra/Module/ZLattice/Covolume.lean
@@ -168,7 +168,7 @@ theorem covolume_div_covolume_eq_relindex' {E : Type*} [NormedAddCommGroup E]
   let f := (EuclideanSpace.equiv _ ℝ).symm.trans
     (stdOrthonormalBasis ℝ E).repr.toContinuousLinearEquiv.symm
   have hf : MeasurePreserving f := (stdOrthonormalBasis ℝ E).measurePreserving_repr_symm.comp
-    (EuclideanSpace.volume_preserving_measurableEquiv _).symm
+    (EuclideanSpace.volume_preserving_measurableEquiv_toLp_symm _).symm
   rw [← covolume_comap L₁ volume volume hf, ← covolume_comap L₂ volume volume hf,
     covolume_div_covolume_eq_relindex _ _ (fun _ h' ↦ h h'), ZLattice.comap_toAddSubgroup,
     ZLattice.comap_toAddSubgroup, Nat.cast_inj, LinearEquiv.toAddMonoidHom_commutes,
