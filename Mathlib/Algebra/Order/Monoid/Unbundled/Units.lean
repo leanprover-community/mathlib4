1c56d85e1e987312e509b55cd99e110bf0efdafa
Kenny Lau <kc_kennylau@yahoo.com.hk>
2025-08-12 23:13:35 +0100

    style

--- Mathlib/Algebra/Order/Monoid/Unbundled/Units.lean
+++ Mathlib/Algebra/Order/Monoid/Unbundled/Units.lean
@@ -17,17 +17,20 @@ theorem Units.mulLECancellable_val [MulLeftMono M] (a : Mˣ) :
     MulLECancellable (↑a : M) := fun _ _ h ↦ by
   simpa using mul_le_mul_left' h ↑a⁻¹
 
-theorem Units.mul_le_mul_left [MulLeftMono M] (a : Mˣ) {b c : M} : a * b ≤ a * c ↔ b ≤ c :=
+theorem Units.mul_le_mul_left [MulLeftMono M] (a : Mˣ) {b c : M} :
+    a * b ≤ a * c ↔ b ≤ c :=
   a.mulLECancellable_val.mul_le_mul_iff_left
 
-theorem IsUnit.mulLECancellable [MulLeftMono M] {a : M} (ha : IsUnit a) : MulLECancellable a :=
+theorem IsUnit.mulLECancellable [MulLeftMono M] {a : M} (ha : IsUnit a) :
+    MulLECancellable a :=
   ha.unit.mulLECancellable_val
 
 theorem IsUnit.mul_le_mul_left [MulLeftMono M] {a b c : M} (ha : IsUnit a) :
     a * b ≤ a * c ↔ b ≤ c :=
   ha.unit.mul_le_mul_left
 
-theorem Units.mul_le_mul_right [MulRightMono M] (a : Mˣ) {b c : M} : b * a ≤ c * a ↔ b ≤ c :=
+theorem Units.mul_le_mul_right [MulRightMono M] (a : Mˣ) {b c : M} :
+    b * a ≤ c * a ↔ b ≤ c :=
   ⟨(by simpa using mul_le_mul_right' · ↑a⁻¹), (mul_le_mul_right' · _)⟩
 
 theorem IsUnit.mul_le_mul_right [MulRightMono M] {a b c : M} (ha : IsUnit a) :
