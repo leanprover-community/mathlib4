89365f045afbc81ffb95c228d26f83824550c405
Kenny Lau <kc_kennylau@yahoo.com.hk>
2025-08-12 22:55:42 +0100

    shake, style

--- Mathlib/Algebra/Order/Monoid/Unbundled/Units.lean
+++ Mathlib/Algebra/Order/Monoid/Unbundled/Units.lean
@@ -4,9 +4,8 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kenny Lau
 -/
 
-import Mathlib.Algebra.Group.Units.Defs
-import Mathlib.Algebra.Order.Monoid.Canonical.Defs
-
+import Mathlib.Algebra.Group.Units.Basic
+import Mathlib.Algebra.Order.Monoid.Unbundled.Basic
 
 /-!
 # Lemmas for units in an ordered monoid
@@ -14,26 +13,23 @@ import Mathlib.Algebra.Order.Monoid.Canonical.Defs
 
 variable {M : Type*} [Monoid M] [LE M]
 
-theorem Units.mulLECancellable_val [MulLeftMono M]
-    (a : Mˣ) : MulLECancellable (↑a : M) := fun _ _ h ↦ by
+theorem Units.mulLECancellable_val [MulLeftMono M] (a : Mˣ) :
+    MulLECancellable (↑a : M) := fun _ _ h ↦ by
   simpa using mul_le_mul_left' h ↑a⁻¹
 
-theorem Units.mul_le_mul_left [MulLeftMono M]
-    (a : Mˣ) {b c : M} : a * b ≤ a * c ↔ b ≤ c :=
+theorem Units.mul_le_mul_left [MulLeftMono M] (a : Mˣ) {b c : M} : a * b ≤ a * c ↔ b ≤ c :=
   a.mulLECancellable_val.mul_le_mul_iff_left
 
-theorem IsUnit.mulLECancellable [MulLeftMono M]
-    {a : M} (ha : IsUnit a) : MulLECancellable a :=
+theorem IsUnit.mulLECancellable [MulLeftMono M] {a : M} (ha : IsUnit a) : MulLECancellable a :=
   ha.unit.mulLECancellable_val
 
-theorem IsUnit.mul_le_mul_left [MulLeftMono M]
-    {a b c : M} (ha : IsUnit a) : a * b ≤ a * c ↔ b ≤ c :=
+theorem IsUnit.mul_le_mul_left [MulLeftMono M] {a b c : M} (ha : IsUnit a) :
+    a * b ≤ a * c ↔ b ≤ c :=
   ha.unit.mul_le_mul_left
 
-theorem Units.mul_le_mul_right [MulRightMono M]
-    (a : Mˣ) {b c : M} : b * a ≤ c * a ↔ b ≤ c :=
+theorem Units.mul_le_mul_right [MulRightMono M] (a : Mˣ) {b c : M} : b * a ≤ c * a ↔ b ≤ c :=
   ⟨(by simpa using mul_le_mul_right' · ↑a⁻¹), (mul_le_mul_right' · _)⟩
 
-theorem IsUnit.mul_le_mul_right [MulRightMono M]
-    {a b c : M} (ha : IsUnit a) : b * a ≤ c * a ↔ b ≤ c :=
+theorem IsUnit.mul_le_mul_right [MulRightMono M] {a b c : M} (ha : IsUnit a) :
+    b * a ≤ c * a ↔ b ≤ c :=
   ha.unit.mul_le_mul_right
