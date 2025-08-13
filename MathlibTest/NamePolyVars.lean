dc132a7c38c4daf02d8275af6961ae425083f7fa
Kenny Lau <kc_kennylau@yahoo.com.hk>
2025-08-13 15:35:31 +0100

    add another example

--- MathlibTest/NamePolyVars.lean
+++ MathlibTest/NamePolyVars.lean
@@ -1,3 +1,4 @@
+import Mathlib.Data.ZMod.Defs
 import Mathlib.Tactic.Ring.NamePolyVars
 
 variable (R : Type) [CommRing R]
@@ -5,6 +6,7 @@ variable (R : Type) [CommRing R]
 name_poly_vars R [X,Y,Z]
 name_poly_vars Int [q]
 name_poly_vars R [S][T][U]
+name_poly_vars (ZMod 37) [d,e]
 
 noncomputable example : Vector (MvPolynomial (Fin 3) R) 3 :=
   have : X = MvPolynomial.X 0 := rfl
@@ -21,3 +23,8 @@ noncomputable example : Vector (Polynomial (Polynomial (Polynomial R))) 3 :=
   have : T = Polynomial.C Polynomial.X := rfl
   have : U = Polynomial.X := rfl
   #v[S, T, U]
+
+noncomputable example : Vector (MvPolynomial (Fin 2) (ZMod 37)) 2 :=
+  have : d = MvPolynomial.X 0 := rfl
+  have : e = MvPolynomial.X 1 := rfl
+  #v[d, e]
