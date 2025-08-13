2d3f4a78b1102a4da3f16941bcfce844cb1d00da
Kenny Lau <kc_kennylau@yahoo.com.hk>
2025-08-13 11:40:02 +0100

    allow locations

--- Mathlib/Tactic/Simproc/Apply.lean
+++ Mathlib/Tactic/Simproc/Apply.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kenny Lau
 -/
 
-import Lean.Elab.Tactic.Basic
+import Lean.Elab.Tactic.Location
 import Lean.Meta.Tactic.Simp.Main
 
 /-!
@@ -14,15 +14,37 @@ The only function `Simproc.apply` defined in this file takes a `s : Simproc` and
 goal.
 -/
 
-open Lean Elab Tactic
+namespace Lean.Meta.Simp.Simproc
 
-/-- Execute the given `Simproc` on the goal. -/
-def Lean.Meta.Simp.Simproc.apply (s : Simproc) : TacticM Unit := do
-  liftMetaTactic1 fun e ↦ do
+open Elab.Tactic
+
+set_option linter.unusedVariables false in
+/-- Process the given local hypothesis. -/
+def applyAtLocalDecl (s : Simproc) (f : FVarId) : TacticM Unit := do
+  let hyp ← instantiateMVars (← f.getType)
+  let ctx ← mkContext (simpTheorems := #[])
+  let (r, _) ← mainCore hyp ctx (methods := {post := s})
+  liftMetaTactic1 fun m ↦ do
+    let .some (f, m) ← applySimpResultToLocalDecl m f r false | return m
+    return m
+
+/-- Process the goal. -/
+def applyAtTarget (s : Simproc) : TacticM Unit := do
+  liftMetaTactic1 fun m ↦ do
     -- We cannot pass a `Simproc` object into `simp`, because `simp` only accepts names of global
     -- constants. However, the core part of `simp`, i.e. `Simp.mainCore`, actually allows `Simproc`s
     -- to be executed, so this code calls `Simp.mainCore` directly.
-    let target ← instantiateMVars (← e.getType)
-    let ctx ← Simp.mkContext (simpTheorems := #[])
-    let (r, _) ← Simp.mainCore target ctx (methods := {post := s})
-    applySimpResultToTarget e target r
+    let target ← instantiateMVars (← m.getType)
+    let ctx ← mkContext (simpTheorems := #[])
+    let (r, _) ← mainCore target ctx (methods := {post := s})
+    let i ← applySimpResultToTarget m target r
+    return i
+  evalTactic (← `(tactic| try rfl))
+
+/-- Execute the given `Simproc` on the goal. -/
+def apply (s : Simproc) (loc : Option Location) : TacticM Unit :=
+  match loc with
+  | .none => applyAtTarget s
+  | .some loc => withLocation loc (applyAtLocalDecl s) (applyAtTarget s) default
+
+end Lean.Meta.Simp.Simproc
