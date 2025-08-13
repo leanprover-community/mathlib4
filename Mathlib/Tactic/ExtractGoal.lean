73e3a3e1d1162204b0b1413dd3064e9a5a71bc5c
Anne Baanen <Vierkantor@users.noreply.github.com>
2025-08-13 17:05:41 +0200

    Remove commented-out line
    Co-authored-by: Kim Morrison <477956+kim-em@users.noreply.github.com>

--- Mathlib/Tactic/ExtractGoal.lean
+++ Mathlib/Tactic/ExtractGoal.lean
@@ -3,7 +3,6 @@ Copyright (c) 2017 Simon Hudon. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Simon Hudon, Kyle Miller, Damiano Testa
 -/
--- import Mathlib.Init
 import Lean.Elab.Term
 import Lean.Elab.Tactic.ElabTerm
 import Lean.Meta.Tactic.Cleanup
