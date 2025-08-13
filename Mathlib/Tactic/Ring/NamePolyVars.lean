8cbd60481c1e0a9872bb22ada6621ec72c089d23
Aaron Liu <aaronliu2008@outlook.com>
2025-08-13 11:35:25 -0400

    unspace

--- Mathlib/Tactic/Ring/NamePolyVars.lean
+++ Mathlib/Tactic/Ring/NamePolyVars.lean
@@ -17,8 +17,8 @@ Usage:
 ```lean
 variable (R : Type) [CommRing R]
 
-name_poly_vars R [X,Y,Z]
-name_poly_vars R [s][t][u]
+name_poly_vars R[X,Y,Z]
+name_poly_vars R[s][t][u]
 
 #check Y -- Y : MvPolynomial (Fin 3) R
 #check t -- t : Polynomial (Polynomial (Polynomial R))
@@ -30,45 +30,61 @@ open Lean Elab Command
 namespace Mathlib.Tactic
 
 /--
+A variable that can be in the head position of a `name_poly_vars` command.
+This is either an identifier or a term enclosed in parentheses.
+-/
+syntax polyVarsHead := ident <|> ("(" term ")")
+
+/--
+Convert a `polyVarsHead` to a term.
+-/
+def polyVarsHeadToTerm? (stx : TSyntax ``polyVarsHead) : Option Term :=
+  match stx with
+  | `(polyVarsHead| $k:ident) => some { raw := k.raw }
+  | `(polyVarsHead| ($u:term)) => some u
+  | _ => none
+
+/--
 The command `name_poly_vars` names variables in
 `MvPolynomial (Fin n) R` for the appropriate value of `n`.
 The notation introduced by this command is local.
 
-For `Polynomial (Polynomial (...))`, use the syntax `name_poly_vars R [X][Y][Z]`.
+For `Polynomial (Polynomial (...))`, use the syntax `name_poly_vars R[X][Y][Z]`.
 
 Usage:
 
 ```lean
 variable (R : Type) [CommRing R]
 
-name_poly_vars R [X,Y,Z]
+name_poly_vars R[X,Y,Z]
 
 #check Y -- Y : MvPolynomial (Fin 3) R
 ```
 -/
-syntax (name := nameMvPolyVars) "name_poly_vars " term:max ppSpace "[" ident,+ "]" : command
+syntax (name := nameMvPolyVars) "name_poly_vars " polyVarsHead "[" ident,+ "]" : command
 
 /--
 The command `name_poly_vars` names variables in `Polynomial (Polynomial (... R))` stacked
 appropriately many times. The notation introduced by this command is local.
 
-For `MvPolynomial (Fin n) R`, use the syntax `name_poly_vars R [X,Y,Z]`.
+For `MvPolynomial (Fin n) R`, use the syntax `name_poly_vars R[X,Y,Z]`.
 
 Usage:
 
 ```lean
 variable (R : Type) [CommRing R]
 
-name_poly_vars R [X][Y][Z]
+name_poly_vars R[X][Y][Z]
 
 #check Y -- Y : Polynomial (Polynomial (Polynomial R))
 ```
 -/
-syntax (name := namePolyVars) "name_poly_vars " term:max "[" sepBy(ident, "][") "]" : command
+syntax (name := namePolyVars) "name_poly_vars " polyVarsHead "[" sepBy(ident, "][") "]" : command
 
 @[command_elab nameMvPolyVars, inherit_doc namePolyVars]
 def elabNameMvVariables : CommandElab
-| `(command|name_poly_vars $R:term [$vars:ident,*]) => do
+| `(command|name_poly_vars $R:polyVarsHead [$vars:ident,*]) => do
+  let some R := polyVarsHeadToTerm? R | throwUnsupportedSyntax
   let vars := vars.getElems
   let size := vars.size
   let sizeStx : TSyntax `term := quote size
@@ -83,7 +99,8 @@ def elabNameMvVariables : CommandElab
 
 @[command_elab namePolyVars, inherit_doc namePolyVars]
 def elabNamePolyVariables : CommandElab
-| `(command|name_poly_vars $R:term [$vars:ident][*]) => do
+| `(command|name_poly_vars $R:polyVarsHead [$vars:ident][*]) => do
+  let some R := polyVarsHeadToTerm? R | throwUnsupportedSyntax
   let vars := vars.getElems.reverse
   let size := vars.size
   let type : Term ← size.rec (return R) fun _ S ↦ do
