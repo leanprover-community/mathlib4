752a46f1e83b2427be5c37bd75bcdecbbb737cfc
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2025-08-13 13:25:47 +0100

    move some methods to MetaM

--- Mathlib/Util/AtLocation.lean
+++ Mathlib/Util/AtLocation.lean
@@ -35,9 +35,8 @@ inductive SimprocLike
 
 variable (m : SimprocLike)
 
-/-- Use the procedure `m` to rewrite the main goal. -/
-def atTarget (proc : String) (failIfUnchanged : Bool) : TacticM Unit := withMainContext do
-  let goal ← getMainGoal
+/-- Use the procedure `m` to rewrite the provided goal. -/
+def atTarget (proc : String) (failIfUnchanged : Bool) (goal : MVarId) : MetaM (Option MVarId) := do
   let tgt ← instantiateMVars (← goal.getType)
   let m := match m with
   | .noContext m => m
@@ -49,22 +48,21 @@ def atTarget (proc : String) (failIfUnchanged : Bool) : TacticM Unit := withMain
   if failIfUnchanged && unchanged then throwError "{proc} made no progress on goal"
   if r.expr.consumeMData.isConstOf ``True then
     goal.assign (← mkOfEqTrue (← r.getProof))
-    replaceMainGoal []
+    pure none
   else
     -- this ensures that we really get the same goal as an `MVarId`,
     -- not a different `MVarId` for which `MVarId.getType` is the same
-    if unchanged then return
-    let newGoal ← applySimpResultToTarget goal tgt r
-    replaceMainGoal [newGoal]
+    if unchanged then return goal
+    applySimpResultToTarget goal tgt r
 
 /-- Use the procedure `m` to rewrite hypothesis `fvarId`. -/
-def atLocalDecl (proc : String) (failIfUnchanged : Bool) (mayCloseGoal : Bool) (fvarId : FVarId) :
-    TacticM Unit := withMainContext do
+def atLocalDecl (proc : String) (failIfUnchanged : Bool) (mayCloseGoal : Bool) (fvarId : FVarId)
+    (goal : MVarId) :
+    MetaM (Option MVarId) := do
   let ldecl ← fvarId.getDecl
   if ldecl.isImplementationDetail then
     throwError "cannot run {proc} at {ldecl.userName}, it is an implementation detail"
   let tgt ← instantiateMVars (← fvarId.getType)
-  let goal ← getMainGoal
   let m := match m with
   | .noContext m => m
   | .withContext ctx m => m <| ctx.setSimpTheorems <| ctx.simpTheorems.eraseTheorem (.fvar fvarId)
@@ -73,16 +71,14 @@ def atLocalDecl (proc : String) (failIfUnchanged : Bool) (mayCloseGoal : Bool) (
   -- `applySimpResultToLocalDeclCore`
   if failIfUnchanged && tgt.consumeMData == myres.expr.consumeMData then
     throwError "{proc} made no progress at {ldecl.userName}"
-  match ← applySimpResultToLocalDecl goal fvarId myres mayCloseGoal with
-  | none => replaceMainGoal []
-  | some (_, newGoal) => replaceMainGoal [newGoal]
+  return (← applySimpResultToLocalDecl goal fvarId myres mayCloseGoal).map Prod.snd
 
 /-- Use the procedure `m` to rewrite at specified locations. -/
 def atLocation (proc : String) (loc : Location) (failIfUnchanged : Bool := true)
     (mayCloseGoalFromHyp : Bool := false) :
     TacticM Unit :=
-  withLocation loc (atLocalDecl m proc failIfUnchanged mayCloseGoalFromHyp)
-    (atTarget m proc failIfUnchanged)
+  withLocation loc (liftMetaTactic1 ∘ atLocalDecl m proc failIfUnchanged mayCloseGoalFromHyp)
+    (liftMetaTactic1 <| atTarget m proc failIfUnchanged)
     fun _ ↦ throwError "{proc} made no progress anywhere"
 
 end Mathlib.Tactic
