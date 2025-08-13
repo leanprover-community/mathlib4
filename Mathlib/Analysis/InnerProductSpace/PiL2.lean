12bf43dcb361fed1b32f386a918a96c230fe3069
Monica Omar <2497250a@research.gla.ac.uk>
2025-08-13 19:53:13 +0000

    namespace

--- Mathlib/Analysis/InnerProductSpace/PiL2.lean
+++ Mathlib/Analysis/InnerProductSpace/PiL2.lean
@@ -1176,10 +1176,9 @@ theorem inner_matrix_col_col [Fintype m] (A B : Matrix m n 𝕜) (i j : n) :
     ⟪Aᵀ i, Bᵀ j⟫ₑ = (Aᴴ * B) i j := by
   simp [PiLp.inner_apply, dotProduct, mul_apply', mul_comm]
 
-open ContinuousLinearMap in
 /-- The matrix representation of `(lsmul 𝕜 𝕜).flip x)` given by basis `b` is equal to the
 column `b.repr x`. -/
-theorem lsmul_flip_apply_toMatrix {𝕜 E : Type*} [NontriviallyNormedField 𝕜]
+theorem ContinuousLinearMap.lsmul_flip_apply_toMatrix {𝕜 E : Type*} [NontriviallyNormedField 𝕜]
     [SeminormedAddCommGroup E] [NormedSpace 𝕜 E] (b : Basis ι 𝕜 E) (x : E) :
     ((lsmul 𝕜 𝕜).flip x).toMatrix (.singleton Unit 𝕜) b = replicateCol Unit (b.repr x) := by
   ext; simp [LinearMap.toMatrix_apply]
