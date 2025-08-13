80d3a14f2b930fc01bae952425fc0b17498c7c44
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-08-13 17:25:01 +0530

    fixing imports

--- Mathlib/AlgebraicTopology/SimplicialSet/AnodyneExtensions/IsUniquelyCodimOneFace.lean
+++ Mathlib/AlgebraicTopology/SimplicialSet/AnodyneExtensions/IsUniquelyCodimOneFace.lean
@@ -3,7 +3,7 @@ Copyright (c) 2025 Joël Riou. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joël Riou
 -/
-import Mathlib.AlgebraicTopology.SimplicialSet.NonDegenerateSimplices
+import Mathlib.AlgebraicTopology.SimplicialSet.Simplices
 
 /-!
 # Simplices that are uniquely codimensional one faces
