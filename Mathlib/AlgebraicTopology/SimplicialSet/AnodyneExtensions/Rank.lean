6bae3b849ca3163be02be1fa874ca2d0ef03b181
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-08-13 18:48:08 +0530

    cleaning up

--- Mathlib/AlgebraicTopology/SimplicialSet/AnodyneExtensions/Rank.lean
+++ Mathlib/AlgebraicTopology/SimplicialSet/AnodyneExtensions/Rank.lean
@@ -3,7 +3,7 @@ Copyright (c) 2025 Joël Riou. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joël Riou
 -/
-import Mathlib.AlgebraicTopology.SimplicialSet.AnodyneExtensions.Pairing
+import Mathlib.AlgebraicTopology.SimplicialSet.AnodyneExtensions.PairingCore
 import Mathlib.Order.OrderIsoNat
 
 /-!
@@ -11,11 +11,16 @@ import Mathlib.Order.OrderIsoNat
 
 We introduce types of (weak) rank functions for a pairing `P`
 of a subcomplex `A` of a simplicial set `X`. These are
-functions `f : P.II → ι` such that `P.AncestralRel x y` implies `f x < f y`
+functions `f : P.II → α` such that `P.AncestralRel x y` implies `f x < f y`
 (in the weak case, we require this only under the additional condition
 that `x` and `y` are of the same dimension). Such rank functions
-are used to show that the ancestral relation on `P.II` is well founded,
+are used in order to show that the ancestral relation on `P.II` is well founded,
 i.e. that `P` is regular (when we already know `P` is proper).
+Conversely, we shall show that if `P` is regular,
+then `P.RankFunction ℕ` is non empty (TODO @joelriou).
+
+(We also introduce similar definitions for the structure `PairingCore`.)
+
 
 ## References
 * [Sean Moss, *Another approach to the Kan-Quillen model structure*][moss-2020]
@@ -38,6 +43,7 @@ variable {X : SSet.{u}} {A : X.Subcomplex} (P : A.Pairing)
 /-- A rank function for a pairing is a function from the type (II) simplices
 to a partially ordered type which maps ancestrality relations to strict inequalities. -/
 structure RankFunction where
+  /-- the rank function -/
   rank : P.II → α
   lt {x y : P.II} : P.AncestralRel x y → rank x < rank y
 
@@ -61,6 +67,7 @@ end RankFunction
 to a partially ordered type which maps an ancestrality relation between
 simplices of the same dimension to a strict inequality. -/
 structure WeakRankFunction where
+  /-- the (weak) rank function -/
   rank : P.II → α
   lt {x y : P.II} : P.AncestralRel x y → x.1.dim = y.1.dim → rank x < rank y
 
@@ -104,6 +111,7 @@ variable {X : SSet.{u}} {A : X.Subcomplex} (h : A.PairingCore)
 /-- A rank function for `h : A.PairincgCore` is a function from the index type `h.ι`
 to a partially ordered type which maps the ancestrality relations to strict inequalities. -/
 structure RankFunction where
+  /-- the rank function -/
   rank : h.ι → α
   lt {x y : h.ι} : h.AncestralRel x y → rank x < rank y
 
@@ -111,20 +119,47 @@ structure RankFunction where
 to a partially ordered type which maps an ancestrality relation between
 indices of the same dimension to a strict inequality. -/
 structure WeakRankFunction where
+  /-- the (weak) rank function -/
   rank : h.ι → α
   lt {x y : h.ι} : h.AncestralRel x y → h.d x = h.d y → rank x < rank y
 
+/-- Rank functions for `h : A.PairingCore` correspond to
+rank functions for `h.pairing : A.Pairing`. -/
 noncomputable def rankFunctionEquiv :
     h.RankFunction α ≃ h.pairing.RankFunction α where
   toFun f :=
     { rank s := f.rank (h.equivII.symm s)
-      lt := sorry }
+      lt {x y} hxy := by
+        obtain ⟨x, rfl⟩ := h.equivII.surjective x
+        obtain ⟨y, rfl⟩ := h.equivII.surjective y
+        rw [← ancestralRel_iff] at hxy
+        simpa using f.lt hxy }
   invFun g :=
     { rank x := g.rank (h.equivII x)
-      lt := by
-        sorry }
-  left_inv := sorry
-  right_inv := sorry
+      lt hxy := by
+        rw [ancestralRel_iff] at hxy
+        exact g.lt hxy }
+  left_inv _ := by simp
+  right_inv _ := by simp
+
+/-- Weak rank functions for `h : A.PairingCore` correspond to
+weak rank functions for `h.pairing : A.Pairing`. -/
+noncomputable def weakRankFunctionEquiv :
+    h.WeakRankFunction α ≃ h.pairing.WeakRankFunction α where
+  toFun f :=
+    { rank s := f.rank (h.equivII.symm s)
+      lt {x y} hxy := by
+        obtain ⟨x, rfl⟩ := h.equivII.surjective x
+        obtain ⟨y, rfl⟩ := h.equivII.surjective y
+        rw [← ancestralRel_iff] at hxy
+        simpa using f.lt hxy }
+  invFun g :=
+    { rank x := g.rank (h.equivII x)
+      lt hxy := by
+        rw [ancestralRel_iff] at hxy
+        exact g.lt hxy }
+  left_inv _ := by simp
+  right_inv _ := by simp
 
 end PairingCore
 
