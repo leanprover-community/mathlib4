50c72fb0dc0b915e701839dc02b968273ab15919
Paul Lezeau <lezeau@google.com>
2025-08-14 16:08:11 +0000

    Fix header

--- Mathlib/Topology/Instances/Matrix/SpecialLinearGroup.lean
+++ Mathlib/Topology/Instances/Matrix/SpecialLinearGroup.lean
@@ -1,11 +1,18 @@
 /-
-Copyright (c) 2021 Paul Lezeau. All rights reserved.
+Copyright (c) 2025 Paul Lezeau. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Paul Lezeau
 -/
 
 import Mathlib.LinearAlgebra.Matrix.SpecialLinearGroup
 import Mathlib.Topology.Instances.Matrix.Basic.Basic
+
+/-!
+# Topological properties of SLₙ
+
+This file is a place to collect topological results about `SLₙ`
+-/
+
 
 open Matrix
 
