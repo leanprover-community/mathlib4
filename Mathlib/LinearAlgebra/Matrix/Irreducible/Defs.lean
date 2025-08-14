facf4a0e1dfcb7a021fdd667c1c6889248b0901b
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2025-08-14 23:14:03 +0200

    isIrreducible_transpose_iff - todo defeq

--- Mathlib/LinearAlgebra/Matrix/Irreducible/Defs.lean
+++ Mathlib/LinearAlgebra/Matrix/Irreducible/Defs.lean
@@ -214,21 +214,7 @@ theorem IsIrreducible.transpose (hA : IsIrreducible A) : IsIrreducible Aᵀ := b
 
 @[simp]
 theorem isIrreducible_transpose_iff :
-    Aᵀ.IsIrreducible ↔ A.IsIrreducible := by
-  by_cases hA_nonneg : ∀ i j, 0 ≤ A i j
-  · exact ⟨fun h ↦
-    let hA_T_nonneg : ∀ i j, 0 ≤ (Aᵀ) i j := fun i j => by
-      simpa [Matrix.transpose_apply] using hA_nonneg j i
-    IsIrreducible.transpose h,
-   fun h ↦ IsIrreducible.transpose h⟩
-  · have : ¬ Aᵀ.IsIrreducible := by
-      rw [isIrreducible_iff]
-      simp only [transpose_apply, isSStronglyConnected_iff, not_and, not_forall, not_exists,
-        not_lt, nonpos_iff_eq_zero]
-      intro a; simp_all only [implies_true, not_true_eq_false]
-    have : ¬ A.IsIrreducible := by
-      rw [isIrreducible_iff]; simp_all only [not_forall, not_le, isSStronglyConnected_iff,
-      not_and, not_exists, not_lt, nonpos_iff_eq_zero, isEmpty_Prop, IsEmpty.forall_iff]
-    simp_all only [not_forall, not_le]
+    Aᵀ.IsIrreducible ↔ A.IsIrreducible :=
+  ⟨IsIrreducible.transpose, IsIrreducible.transpose⟩
 
 end Matrix
