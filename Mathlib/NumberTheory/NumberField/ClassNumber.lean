c2ba49d94f56e4a8ff1a286a9fbfda205aa2b4c5
Riccardo Brasca <riccardo.brasca@gmail.com>
2025-08-14 20:13:41 +0200

    add authors

--- Mathlib/NumberTheory/NumberField/ClassNumber.lean
+++ Mathlib/NumberTheory/NumberField/ClassNumber.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2021 Anne Baanen. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Anne Baanen
+Authors: Anne Baanen, Riccardo Brasca, Xavier Roblot
 -/
 import Mathlib.NumberTheory.ClassNumber.AdmissibleAbs
 import Mathlib.NumberTheory.ClassNumber.Finite
