d8d44ceb33f00ea4ffed31417af1e389a1cf0d33
euprunin <euprunin@users.noreply.github.com>
2025-08-14 13:29:24 +0200

    deprecate `ne_zero_of_ne_zero_of_monic`

--- Mathlib/Algebra/Polynomial/Monic.lean
+++ Mathlib/Algebra/Polynomial/Monic.lean
@@ -49,8 +49,8 @@ theorem Monic.as_sum (hp : p.Monic) :
   suffices C (p.coeff p.natDegree) = 1 by rw [this, one_mul]
   exact congr_arg C hp
 
-theorem ne_zero_of_ne_zero_of_monic (hp : p ≠ 0) (hq : Monic q) : q ≠ 0 :=
-  Monic.ne_zero_of_polynomial_ne hq hp
+@[deprecated (since := "2025-08-14")] alias ne_zero_of_ne_zero_of_monic :=
+  Monic.ne_zero_of_polynomial_ne
 
 theorem Monic.map [Semiring S] (f : R →+* S) (hp : Monic p) : Monic (p.map f) := by
   unfold Monic
