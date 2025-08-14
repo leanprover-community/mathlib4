5655966cc26384d0e78b4e186fdb7344d4b9b9f0
Kim Morrison <kim@tqft.net>
2025-08-14 22:15:32 +1000

    shake --fix

--- Mathlib/MeasureTheory/Function/ConditionalExpectation/AEMeasurable.lean
+++ Mathlib/MeasureTheory/Function/ConditionalExpectation/AEMeasurable.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Rémy Degenne
 -/
 import Mathlib.MeasureTheory.Function.LpSeminorm.Trim
-import Mathlib.MeasureTheory.Function.StronglyMeasurable.Inner
 import Mathlib.MeasureTheory.Function.StronglyMeasurable.Lp
 
 /-! # Functions a.e. measurable with respect to a sub-σ-algebra
