0d802b92bcf9f2b0e8ad3b4e72b9c99a03ac595c
Paul Lezeau <lezeau@google.com>
2025-08-14 15:40:28 +0000

    namespacing

--- Mathlib/Data/Part.lean
+++ Mathlib/Data/Part.lean
@@ -218,11 +218,11 @@ theorem eq_of_get_eq_get {a b : Part α} (ha : a.Dom) (hb : b.Dom) (hab : a.get 
   rw [← eq_get_iff_mem ha, ← eq_get_iff_mem hb, hab]
 
 theorem eq_iff_of_dom {a b : Part α} (ha : a.Dom) (hb : b.Dom) : a.get ha = b.get hb ↔ a = b :=
-  ⟨fun H ↦ Part.eq_of_get_eq_get ha hb H, fun H ↦ Part.get_eq_get_of_eq a ha H⟩
+  ⟨fun H ↦ eq_of_get_eq_get ha hb H, fun H ↦ get_eq_get_of_eq a ha H⟩
 
 theorem get_eq_get {a b : Part α} (ha : a.Dom) (hb : a.get ha ∈ b) : a = b := by
   have hb' : b.Dom := Part.dom_iff_mem.mpr ⟨a.get ha, hb⟩
-  rwa [← Part.eq_get_iff_mem hb', Part.eq_iff_of_dom ha hb'] at hb
+  rwa [← eq_get_iff_mem hb', eq_iff_of_dom ha hb'] at hb
 
 @[simp]
 theorem none_toOption [Decidable (@none α).Dom] : (none : Part α).toOption = Option.none :=
