523b5906fdb6cd4c38c7c2337d45c66ca4756a1a
Weiyi Wang <wwylele@gmail.com>
2025-08-14 10:11:57 -0400

    Update Mathlib/Algebra/Order/Archimedean/Class.lean
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- Mathlib/Algebra/Order/Archimedean/Class.lean
+++ Mathlib/Algebra/Order/Archimedean/Class.lean
@@ -577,7 +577,7 @@ theorem ballSubgroup_antitone : Antitone (ballSubgroup (M := M)) := by
 
 /-- A subgroup `G` is called a grade at `c` iff
 `ballSubgroup c` and `G` are complements in the lattice under `closedBallSubgroup c`. -/
-@[to_additive /--A subgroup `G` is called a grade at `c` iff
+@[to_additive /-- A subgroup `G` is called a grade at `c` iff
 `ballAddSubgroup c` and `G` are complements in the lattice under `closedBallAddSubgroup c`. -/]
 def IsGradeSubgroup (c : MulArchimedeanClass M) (G : Subgroup M) : Prop :=
   Disjoint (ballSubgroup c) G ∧ ballSubgroup c ⊔ G = closedBallSubgroup c
