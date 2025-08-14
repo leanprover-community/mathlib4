e78ba688c85fdbce6121ba883d2baa3c46b86064
Monica Omar <2497250a@research.gla.ac.uk>
2025-08-14 01:40:21 +0000

    fix

--- Mathlib/Analysis/InnerProductSpace/Projection/FiniteDimensional.lean
+++ Mathlib/Analysis/InnerProductSpace/Projection/FiniteDimensional.lean
@@ -283,7 +283,7 @@ theorem OrthogonalFamily.projection_directSum_coeAddHom [DecidableEq ι] {V : ι
   | zero => simp
   | of j x =>
     simp_rw [DirectSum.coeAddMonoidHom_of, DirectSum.of]
-      -- Porting note: was in the previous `simp_rw`, no longer works
+    -- Porting note: was in the previous `simp_rw`, no longer works
     -- This used to be `rw`, but we need `erw` after https://github.com/leanprover/lean4/pull/2644
     erw [DFinsupp.singleAddHom_apply]
     obtain rfl | hij := Decidable.eq_or_ne i j
