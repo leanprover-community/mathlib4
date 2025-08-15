43cb87450c20761cc838e84196c7407409a52952
Kim Morrison <kim@tqft.net>
2025-08-15 07:49:15 +0200

    deprecation

--- Archive/Wiedijk100Theorems/BallotProblem.lean
+++ Archive/Wiedijk100Theorems/BallotProblem.lean
@@ -119,7 +119,7 @@ theorem counted_succ_succ (p q : ℕ) :
           Nat.add_sub_cancel]
       · rw [List.count_tail, hl₁, List.head?_eq_head, hlast, if_neg (by decide), Nat.sub_zero]
       · exact fun x hx => hl₂ x (List.mem_of_mem_tail hx)
-      · rw [← hlast, List.head_cons_tail]
+      · rw [← hlast, List.cons_head_tail]
     · refine Or.inr ⟨l.tail, ⟨?_, ?_, ?_⟩, ?_⟩
       · rw [List.count_tail, hl₀, List.head?_eq_head, hlast, if_neg (by decide), Nat.sub_zero]
       · rw [List.count_tail, hl₁, List.head?_eq_head, hlast, beq_self_eq_true, if_pos rfl,
