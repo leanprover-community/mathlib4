d856edd2b163296e0bfab949d1308e0903e0dbfd
ChrisHughes24 <chrishughes24@gmail.com>
2025-08-15 17:36:42 +0100

    merge master properly

--- Mathlib/LinearAlgebra/AffineSpace/Slope.lean
+++ Mathlib/LinearAlgebra/AffineSpace/Slope.lean
@@ -83,11 +83,6 @@ theorem slope_comm (f : k → PE) (a b : k) : slope f a b = slope f b a := by
 @[simp] lemma slope_neg_fun (f : k → E) : slope (-f) = -slope f := by
   ext x y; exact slope_neg f x y
 
-lemma slope_eq_zero_iff {f : k → E} {a b : k} : slope f a b = 0 ↔ f a = f b := by
-  rw [slope, smul_eq_zero, inv_eq_zero, sub_eq_zero, vsub_eq_zero_iff_eq, @eq_comm _ (f a),
-    or_iff_right_of_imp]
-  exact congr_arg _
-
 /-- `slope f a c` is a linear combination of `slope f a b` and `slope f b c`. This version
 explicitly provides coefficients. If `a ≠ c`, then the sum of the coefficients is `1`, so it is
 actually an affine combination, see `lineMap_slope_slope_sub_div_sub`. -/
@@ -123,33 +118,3 @@ theorem lineMap_slope_lineMap_slope_lineMap (f : k → PE) (a b r : k) :
   convert lineMap_slope_slope_sub_div_sub f b (lineMap a b r) a hab.symm using 2
   rw [lineMap_apply_ring, eq_div_iff (sub_ne_zero.2 hab), sub_mul, one_mul, mul_sub, ← sub_sub,
     sub_sub_cancel]
-
-section Order
-
-variable [LinearOrder k] [IsStrictOrderedRing k] [PartialOrder E] [IsOrderedAddMonoid E]
-  [PosSMulMono k E] {f : k → E} {x y : k}
-
-lemma slope_nonneg_iff_of_le (hxy : x ≤ y) : 0 ≤ slope f x y ↔ f x ≤ f y := by
-  by_cases hxeqy : x = y
-  · subst hxeqy; simp
-  rw [slope]
-  refine ⟨?_, ?_⟩
-  · intro h
-    have := smul_nonneg (sub_nonneg.2 hxy) h
-    rwa [← mul_smul, mul_inv_cancel₀ (mt sub_eq_zero.1 (Ne.symm hxeqy)), one_smul,
-      vsub_eq_sub, sub_nonneg] at this
-  · intro h
-    apply smul_nonneg (inv_nonneg.2 (sub_nonneg.2 hxy))
-    rwa [vsub_eq_sub, sub_nonneg]
-
-lemma slope_nonpos_iff_of_le (hxy : x ≤ y) : slope f x y ≤ 0 ↔ f y ≤ f x := by
-  simpa using slope_nonneg_iff_of_le (f := -f) hxy
-
-lemma slope_pos_iff_of_le (hxy : x ≤ y) : 0 < slope f x y ↔ f x < f y := by
-  rw [lt_iff_le_and_ne, slope_nonneg_iff_of_le hxy, lt_iff_le_and_ne, Ne,
-    Ne, @eq_comm _ 0, slope_eq_zero_iff]
-
-lemma slope_neg_iff_of_le (hxy : x ≤ y) : slope f x y < 0 ↔ f y < f x := by
-  simpa using slope_pos_iff_of_le (f := -f) hxy
-
-end Order
