331c61c888e83e055869dbcfd44a15e9e44b8c95
euprunin <euprunin@users.noreply.github.com>
2025-08-15 08:16:26 +0200

    chore(Data/Nat): deprecate `exists_lt_succ` (duplicate)

--- Mathlib/Data/Nat/Init.lean
+++ Mathlib/Data/Nat/Init.lean
@@ -70,10 +70,7 @@ alias ⟨of_le_succ, _⟩ := le_succ_iff
 lemma forall_lt_succ : (∀ m < n + 1, p m) ↔ (∀ m < n, p m) ∧ p n := by
   simp only [Nat.lt_succ_iff, Nat.le_iff_lt_or_eq, or_comm, forall_eq_or_imp, and_comm]
 
-lemma exists_lt_succ : (∃ m < n + 1, p m) ↔ (∃ m < n, p m) ∨ p n := by
-  classical
-  rw [← Decidable.not_iff_not]
-  simpa [not_exists, not_or] using forall_lt_succ
+@[deprecated (since := "2025-08-15")] alias exists_lt_succ := exists_lt_succ_right
 
 lemma two_lt_of_ne : ∀ {n}, n ≠ 0 → n ≠ 1 → n ≠ 2 → 2 < n
   | 0, h, _, _ => (h rfl).elim
