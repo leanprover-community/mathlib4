e912a7934ebd332320f65a5d30de555518e68b09
Yuyang Zhao 赵雨扬 <zhao.yu-yang@foxmail.com>
2025-08-15 21:45:14 +0800

    feat: add `ContinuousSMul` instances for `ℚ≥0`

--- Mathlib/Topology/Instances/Rat.lean
+++ Mathlib/Topology/Instances/Rat.lean
@@ -3,6 +3,7 @@ Copyright (c) 2017 Johannes Hölzl. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl, Mario Carneiro
 -/
+import Mathlib.Algebra.Algebra.Rat
 import Mathlib.Data.NNRat.Order
 import Mathlib.Topology.Algebra.Order.Archimedean
 import Mathlib.Topology.Algebra.Ring.Real
@@ -122,5 +123,18 @@ instance : ContinuousSub ℚ≥0 :=
 
 instance : OrderTopology ℚ≥0 := orderTopology_of_ordConnected (t := Set.Ici 0)
 instance : HasContinuousInv₀ ℚ≥0 := inferInstance
+
+instance : ContinuousSMul ℚ ℝ where
+  continuous_smul := continuous_induced_dom.fst'.smul (M := ℝ) (X := ℝ) continuous_snd
+
+instance {R} [Ring R] [TopologicalSpace R] [Algebra ℚ R] [Algebra ℚ≥0 R] [ContinuousSMul ℚ R] :
+    ContinuousSMul ℚ≥0 R where
+  continuous_smul := by
+    convert continuous_induced_dom.fst'.smul (M := ℚ) (X := R) continuous_snd using 2
+    rw [← cast_smul_eq_nnqsmul ℚ]
+    rfl
+
+instance : ContinuousSMul ℚ≥0 NNReal where
+  continuous_smul := by apply Continuous.subtype_mk; fun_prop
 
 end NNRat
