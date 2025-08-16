a8c33b42d8dbab09e6ec628cfe39c68717b33a4d
David Loeffler <d.loeffler.01@cantab.net>
2025-08-16 13:49:19 +0200

    redundant import

--- Mathlib/LinearAlgebra/Projectivization/GLAction.lean
+++ Mathlib/LinearAlgebra/Projectivization/GLAction.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: David Loeffler
 -/
 import Mathlib.Data.Matrix.Action
-import Mathlib.LinearAlgebra.GeneralLinearGroup
 import Mathlib.LinearAlgebra.Matrix.GeneralLinearGroup.Defs
 import Mathlib.LinearAlgebra.Projectivization.Basic
 
