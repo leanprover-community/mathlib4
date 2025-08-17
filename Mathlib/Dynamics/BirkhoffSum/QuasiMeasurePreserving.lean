4124aa75406ea32ff91f7c022d9c6ff3593fb57e
Oliver Butterley <oliver.butterley@gmail.com>
2025-08-17 07:33:06 +0200

    Add Lua

--- Mathlib/Dynamics/BirkhoffSum/QuasiMeasurePreserving.lean
+++ Mathlib/Dynamics/BirkhoffSum/QuasiMeasurePreserving.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2025 Oliver Butterley. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Oliver Butterley
+Authors: Oliver Butterley, Lua Viana Reis
 -/
 import Mathlib.Dynamics.BirkhoffSum.Average
 import Mathlib.MeasureTheory.Measure.QuasiMeasurePreserving
