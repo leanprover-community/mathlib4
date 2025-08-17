60eb9098ba3cbb9fd878a68ccd22498c5e4bd5d6
Jon Bannon <jbannon@siena.edu>
2025-08-17 11:28:46 +0000

    feat(MeasureTheory): add `Star`, `InvolutiveStar` and `TrivialStar` instances for `AEEqFun` (#28123)
    We add `Filter.EventuallyEq.star`, `StronglyMeasurable.star` and `AEStronglyMeasurable.star` in order to define `Star (α →ₘ[μ] R)`, `InvolutiveStar (α →ₘ[μ] R)`, and `TrivialStar (α →ₘ[μ] R)`.
    
    These results are on the way to a `CStarAlgebra` instance for L^\infty.
    
    
    
    Co-authored-by: Jon Bannon <59937998+JonBannon@users.noreply.github.com>

--- Mathlib/MeasureTheory/Function/StronglyMeasurable/Basic.lean
+++ Mathlib/MeasureTheory/Function/StronglyMeasurable/Basic.lean
@@ -458,6 +458,12 @@ protected theorem smul_const {𝕜} [TopologicalSpace 𝕜] [SMul 𝕜 β] [Cont
     (hf : StronglyMeasurable f) (c : β) : StronglyMeasurable fun x => f x • c :=
   continuous_smul.comp_stronglyMeasurable (hf.prodMk stronglyMeasurable_const)
 
+/-- Pointwise star on functions induced from continuous star preserves strong measurability. -/
+@[measurability]
+protected theorem star {R : Type*} [MeasurableSpace α] [Star R] [TopologicalSpace R]
+    [ContinuousStar R] (f : α → R) (hf : StronglyMeasurable f) : StronglyMeasurable (star f) :=
+  ⟨fun n => star (hf.approx n), fun x => (hf.tendsto_approx x).star⟩
+
 /-- In a normed vector space, the addition of a measurable function and a strongly measurable
 function is measurable. Note that this is not true without further second-countability assumptions
 for the addition of two measurable functions. -/
