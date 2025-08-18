0dcdf29f61b25d918b62c3ae3dc0769edb582f76
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2025-08-18 10:23:47 +0200

    Move rank_range_rankOne

--- Mathlib/Analysis/InnerProductSpace/LinearMap.lean
+++ Mathlib/Analysis/InnerProductSpace/LinearMap.lean
@@ -379,21 +379,4 @@ lemma isIdempotentElem_rankOne_self {x : E} (h : ‖x‖ = 1) :
 
 end
 
-section
-
-variable [NormedAddCommGroup E] [InnerProductSpace 𝕜 E]
-variable [NormedAddCommGroup F] [InnerProductSpace 𝕜 F]
-
-lemma range_rankOne (x : E) {y : F} (hy : y ≠ 0) :
-    LinearMap.range (rankOne 𝕜 x y) = 𝕜 ∙ x :=
-  range_smulRight_apply (map_eq_zero_iff _
-    (InnerProductSpace.toDualMap 𝕜 F).injective |>.not.mpr hy) _
-
-lemma rank_range_rankOne {x : E} {y : F} (hx : x ≠ 0) (hy : y ≠ 0) :
-    Module.rank 𝕜 (LinearMap.range (rankOne 𝕜 x y)) = 1 := by
-  rw [range_rankOne x hy, Module.rank_eq_one_iff_finrank_eq_one]
-  exact finrank_span_singleton hx
-
-end
-
 end InnerProductSpace
