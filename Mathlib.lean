ccb6bd98269658602ede7398865e34290ad14834
Silvere Gangloff <silveregangloff@Ordinateur-portable-de-Silvere.local>
2025-08-18 10:54:04 +0200

    chore: update import-all (lake exe mk_all)

--- Mathlib.lean
+++ Mathlib.lean
@@ -3857,6 +3857,7 @@ import Mathlib.Dynamics.OmegaLimit
 import Mathlib.Dynamics.PeriodicPts.Defs
 import Mathlib.Dynamics.PeriodicPts.Lemmas
 import Mathlib.Dynamics.SymbolicDynamics.Basic
+import Mathlib.Dynamics.SymbolicDynamics.EntropyFinitelyGenerated
 import Mathlib.Dynamics.TopologicalEntropy.CoverEntropy
 import Mathlib.Dynamics.TopologicalEntropy.DynamicalEntourage
 import Mathlib.Dynamics.TopologicalEntropy.NetEntropy
