3154896397b4478863178ba88b76b89fbf8ba5f1
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-08-18 07:35:41 +0000

    feat(AlgebraicTopology/SimplicialSet): uniqueness of the decomposition involving non-degenerate simplices (#26076)
    Any simplex `x : X _⦋n⦌` of a simplicial set can be written in a unique way as `X.map f.op y` for an epimorphism `f : ⦋n⦌ ⟶ ⦋m⦌` and a non-degenerate `m`-simplex `y`.
    
    
    
    Co-authored-by: Joël Riou <37772949+joelriou@users.noreply.github.com>

--- Mathlib/AlgebraicTopology/SimplexCategory/Basic.lean
+++ Mathlib/AlgebraicTopology/SimplexCategory/Basic.lean
@@ -31,6 +31,9 @@ instance {n m : ℕ} : DecidableEq (⦋n⦌ ⟶ ⦋m⦌) := fun a b =>
 
 section Init
 
+lemma congr_toOrderHom_apply {a b : SimplexCategory} {f g : a ⟶ b} (h : f = g)
+    (x : Fin (a.len + 1)) : f.toOrderHom x = g.toOrderHom x := by rw [h]
+
 /-- The constant morphism from ⦋0⦌. -/
 def const (x y : SimplexCategory) (i : Fin (y.len + 1)) : x ⟶ y :=
   Hom.mk <| ⟨fun _ => i, by tauto⟩
