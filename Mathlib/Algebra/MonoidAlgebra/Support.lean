e97ddb6f0e52b3327778706f789d4a2e4f5ff3c3
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2025-08-19 00:32:02 +0200

    .

--- Mathlib/Algebra/MonoidAlgebra/Support.lean
+++ Mathlib/Algebra/MonoidAlgebra/Support.lean
@@ -22,6 +22,7 @@ open Finset Finsupp
 
 variable {k : Type u₁} {G : Type u₂} [Semiring k]
 
+@[to_additive support_mul]
 theorem support_mul [Mul G] [DecidableEq G] (a b : MonoidAlgebra k G) :
     (a * b).support ⊆ a.support * b.support := by
   rw [MonoidAlgebra.mul_def]
@@ -29,16 +30,19 @@ theorem support_mul [Mul G] [DecidableEq G] (a b : MonoidAlgebra k G) :
     support_sum.trans <| biUnion_subset.2 fun _y hy ↦
       support_single_subset.trans <| singleton_subset_iff.2 <| mem_image₂_of_mem hx hy
 
+@[to_additive support_single_mul_subset]
 theorem support_single_mul_subset [DecidableEq G] [Mul G] (f : MonoidAlgebra k G) (r : k) (a : G) :
     (single a r * f : MonoidAlgebra k G).support ⊆ Finset.image (a * ·) f.support :=
   (support_mul _ _).trans <| (Finset.image₂_subset_right support_single_subset).trans <| by
     rw [Finset.image₂_singleton_left]
 
+@[to_additive support_mul_single_subset]
 theorem support_mul_single_subset [DecidableEq G] [Mul G] (f : MonoidAlgebra k G) (r : k) (a : G) :
     (f * single a r).support ⊆ Finset.image (· * a) f.support :=
   (support_mul _ _).trans <| (Finset.image₂_subset_left support_single_subset).trans <| by
     rw [Finset.image₂_singleton_right]
 
+@[to_additive support_single_mul_eq_image]
 theorem support_single_mul_eq_image [DecidableEq G] [Mul G] (f : MonoidAlgebra k G) {r : k}
     (hr : ∀ y, r * y = 0 ↔ y = 0) {x : G} (lx : IsLeftRegular x) :
     (single x r * f : MonoidAlgebra k G).support = Finset.image (x * ·) f.support := by
@@ -47,6 +51,7 @@ theorem support_single_mul_eq_image [DecidableEq G] [Mul G] (f : MonoidAlgebra k
   simp only [mul_apply, mem_support_iff.mp yf, hr, mem_support_iff, sum_single_index,
     Finsupp.sum_ite_eq', Ne, not_false_iff, if_true, zero_mul, ite_self, sum_zero, lx.eq_iff]
 
+@[to_additive support_mul_single_eq_image]
 theorem support_mul_single_eq_image [DecidableEq G] [Mul G] (f : MonoidAlgebra k G) {r : k}
     (hr : ∀ y, y * r = 0 ↔ y = 0) {x : G} (rx : IsRightRegular x) :
     (f * single x r).support = Finset.image (· * x) f.support := by
@@ -55,6 +60,7 @@ theorem support_mul_single_eq_image [DecidableEq G] [Mul G] (f : MonoidAlgebra k
   simp only [mul_apply, mem_support_iff.mp yf, hr, mem_support_iff, sum_single_index,
     Finsupp.sum_ite_eq', Ne, not_false_iff, if_true, mul_zero, ite_self, rx.eq_iff]
 
+@[to_additive support_mul_single]
 theorem support_mul_single [Mul G] [IsRightCancelMul G] (f : MonoidAlgebra k G) (r : k)
     (hr : ∀ y, y * r = 0 ↔ y = 0) (x : G) :
     (f * single x r).support = f.support.map (mulRightEmbedding x) := by
@@ -63,6 +69,7 @@ theorem support_mul_single [Mul G] [IsRightCancelMul G] (f : MonoidAlgebra k G) 
     simp only [support_mul_single_eq_image f hr (IsRightRegular.all x),
       mem_image, mem_map, mulRightEmbedding_apply]
 
+@[to_additive support_single_mul]
 theorem support_single_mul [Mul G] [IsLeftCancelMul G] (f : MonoidAlgebra k G) (r : k)
     (hr : ∀ y, r * y = 0 ↔ y = 0) (x : G) :
     (single x r * f : MonoidAlgebra k G).support = f.support.map (mulLeftEmbedding x) := by
@@ -71,10 +78,11 @@ theorem support_single_mul [Mul G] [IsLeftCancelMul G] (f : MonoidAlgebra k G) (
     simp only [support_single_mul_eq_image f hr (IsLeftRegular.all x), mem_image,
       mem_map, mulLeftEmbedding_apply]
 
+@[to_additive support_one_subset]
 lemma support_one_subset [One G] : (1 : MonoidAlgebra k G).support ⊆ 1 :=
   Finsupp.support_single_subset
 
-@[simp]
+@[to_additive (attr := simp) support_one]
 lemma support_one [One G] [NeZero (1 : k)] : (1 : MonoidAlgebra k G).support = 1 :=
   Finsupp.support_single_ne_zero _ one_ne_zero
 
@@ -93,29 +101,7 @@ end MonoidAlgebra
 
 namespace AddMonoidAlgebra
 
-open Finset Finsupp MulOpposite
-
 variable {k : Type u₁} {G : Type u₂} [Semiring k]
-
-theorem support_mul [DecidableEq G] [Add G] (a b : k[G]) :
-    (a * b).support ⊆ a.support + b.support :=
-  @MonoidAlgebra.support_mul k (Multiplicative G) _ _ _ _ _
-
-theorem support_mul_single [Add G] [IsRightCancelAdd G] (f : k[G]) (r : k)
-    (hr : ∀ y, y * r = 0 ↔ y = 0) (x : G) :
-    (f * single x r : k[G]).support = f.support.map (addRightEmbedding x) :=
-  MonoidAlgebra.support_mul_single (G := Multiplicative G) _ _ hr _
-
-theorem support_single_mul [Add G] [IsLeftCancelAdd G] (f : k[G]) (r : k)
-    (hr : ∀ y, r * y = 0 ↔ y = 0) (x : G) :
-    (single x r * f : k[G]).support = f.support.map (addLeftEmbedding x) :=
-  MonoidAlgebra.support_single_mul (G := Multiplicative G) _ _ hr _
-
-lemma support_one_subset [Zero G] : (1 : k[G]).support ⊆ 0 := Finsupp.support_single_subset
-
-@[simp]
-lemma support_one [Zero G] [NeZero (1 : k)] : (1 : k[G]).support = 0 :=
-  Finsupp.support_single_ne_zero _ one_ne_zero
 
 section Span
 
--- Mathlib/GroupTheory/Coset/Defs.lean
+++ Mathlib/GroupTheory/Coset/Defs.lean
@@ -46,7 +46,7 @@ open scoped Pointwise
 variable {α : Type*}
 
 -- Porting note: see https://leanprover.zulipchat.com/#narrow/stream/287929-mathlib4/topic/.E2.9C.94.20to_additive.2Emap_namespace
-run_cmd Lean.Elab.Command.liftCoreM <| ToAdditive.insertTranslation `QuotientGroup `QuotientAddGroup
+run_cmd Lean.modifyEnv (ToAdditive.translations.addEntry · (`QuotientGroup, `QuotientAddGroup))
 
 namespace QuotientGroup
 
