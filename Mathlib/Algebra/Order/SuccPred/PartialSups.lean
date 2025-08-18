ed5a3f7ce45b9bbf8c1794295db027d54c142dda
Oliver Butterley <oliver.butterley@gmail.com>
2025-08-18 15:52:36 +0200

    docstring

--- Mathlib/Algebra/Order/SuccPred/PartialSups.lean
+++ Mathlib/Algebra/Order/SuccPred/PartialSups.lean
@@ -10,22 +10,21 @@ import Mathlib.Order.SuccPred.LinearLocallyFinite
 /-!
 # `PartialSups` in a `SuccAddOrder`
 
-## TODO
-
-Explain what results to expect in them / what results should go in them in the future.
+Basic results concerning `PartialSups` which follow with minimal assumptions beyond the fact that
+the `PartialSup` is defined over a `SuccAddOrder`.
 -/
 
 open Finset
 
-variable {α ι : Type*} [SemilatticeSup α]
+variable {α ι : Type*} [SemilatticeSup α] [LinearOrder ι]
 
 @[simp]
-lemma partialSups_add_one [Add ι] [One ι] [LinearOrder ι] [LocallyFiniteOrderBot ι] [SuccAddOrder ι]
+lemma partialSups_add_one [Add ι] [One ι] [LocallyFiniteOrderBot ι] [SuccAddOrder ι]
     (f : ι → α) (i : ι) : partialSups f (i + 1) = partialSups f i ⊔ f (i + 1) :=
   Order.succ_eq_add_one i ▸ partialSups_succ f i
 
 /-- See `partialSups_succ` for another decomposition of `(partialSups f) (Order.succ i)`. -/
-lemma partialSups_succ' {α : Type*} [SemilatticeSup α] [LinearOrder ι] [LocallyFiniteOrder ι]
+lemma partialSups_succ' {α : Type*} [SemilatticeSup α] [LocallyFiniteOrder ι]
     [SuccOrder ι] [OrderBot ι] (f : ι → α) (i : ι) :
     (partialSups f) (Order.succ i) = f ⊥ ⊔ (partialSups (f ∘ Order.succ)) i := by
   refine Succ.rec (by simp) (fun j _ h ↦ ?_) (bot_le (a := i))
@@ -34,7 +33,7 @@ lemma partialSups_succ' {α : Type*} [SemilatticeSup α] [LinearOrder ι] [Local
   simp [this, h, sup_assoc]
 
 /-- See `partialSups_add_one` for another decomposition of `partialSups f (i + 1)`. -/
-lemma partialSups_add_one' [Add ι] [One ι] [LinearOrder ι] [OrderBot ι] [LocallyFiniteOrder ι]
+lemma partialSups_add_one' [Add ι] [One ι] [OrderBot ι] [LocallyFiniteOrder ι]
     [SuccAddOrder ι] (f : ι → α) (i : ι) :
     partialSups f (i + 1) = f ⊥ ⊔ partialSups (f ∘ (fun k ↦ k + 1)) i := by
   simpa [← Order.succ_eq_add_one] using partialSups_succ' f i
