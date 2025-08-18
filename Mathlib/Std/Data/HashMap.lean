003c2758e5c60da950eeebf57aad71870e372d1e
thorimur <68410468+thorimur@users.noreply.github.com>
2025-08-18 16:51:39 -0400

    deprecate module?

--- Mathlib/Std/Data/HashMap.lean
+++ Mathlib/Std/Data/HashMap.lean
@@ -4,7 +4,9 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kim Morrison
 -/
 import Std.Data.HashMap.AdditionalOperations
-import Mathlib.Init
+import Mathlib.Tactic.Linter.DeprecatedModule
+
+deprecated_module (since := "2025-08-18")
 
 /-!
 # Convenience functions for hash maps
