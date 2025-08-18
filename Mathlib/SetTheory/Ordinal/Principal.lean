6b16e974d910b1963815067bdd989ef197766b0a
Violeta Hernández <vi.hdz.p@gmail.com>
2025-08-18 01:12:46 -0600

    add

--- Mathlib/SetTheory/Ordinal/Principal.lean
+++ Mathlib/SetTheory/Ordinal/Principal.lean
@@ -68,6 +68,7 @@ theorem not_principal_iff_of_monotone
     ¬ Principal op o ↔ ∃ a < o, o ≤ op a a := by
   simp [principal_iff_of_monotone h₁ h₂]
 
+@[simp]
 theorem principal_zero : Principal op 0 := fun a _ h =>
   (Ordinal.not_lt_zero a h).elim
 
@@ -93,6 +94,24 @@ theorem op_eq_self_of_principal (hao : a < o) (H : IsNormal (op a))
 
 theorem nfp_le_of_principal (hao : a < o) (ho : Principal op o) : nfp (op a) a ≤ o :=
   nfp_le fun n => (ho.iterate_lt hao n).le
+
+protected theorem Principal.sSup {op : Ordinal → Ordinal → Ordinal} {s : Set Ordinal}
+    (H : ∀ x ∈ s, Principal op x) : Principal op (sSup s) := by
+  have : Principal op (sSup ∅) := by simp
+  by_cases hs : BddAbove s; swap; rwa [csSup_of_not_bddAbove hs]
+  obtain rfl | hs' := s.eq_empty_or_nonempty; assumption
+  refine fun x y hx hy ↦ ?_
+  rw [lt_csSup_iff hs hs'] at *
+  obtain ⟨a, has, ha⟩ := hx
+  obtain ⟨b, hbs, hb⟩ := hy
+  refine ⟨_, max_rec' _ has hbs, max_rec ?_ ?_⟩ <;> intro hab
+  · exact H a has ha (hb.trans_le hab)
+  · exact H b hbs (ha.trans_le hab) hb
+
+protected theorem Principal.iSup {op : Ordinal → Ordinal → Ordinal} {ι} {f : ι → Ordinal}
+    (H : ∀ i, Principal op (f i)) : Principal op (⨆ i, f i) := by
+  apply Principal.sSup
+  simpa
 
 end Arbitrary
 
