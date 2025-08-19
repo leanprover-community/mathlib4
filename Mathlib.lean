d2304421325680b49417e4a7b1d12a2ed078467a
Vasilii Nesterov <vas.nesterov63@gmail.com>
2025-08-19 20:13:40 +0000

    feat(Analysis): radius of convergence for binomial series (#27537)
    Introduce the binomial series as `FormalMultilinearSeries`, and prove that its radius of convergence is `1`.

--- Mathlib.lean
+++ Mathlib.lean
@@ -1336,6 +1336,7 @@ import Mathlib.AlgebraicTopology.SingularSet
 import Mathlib.AlgebraicTopology.TopologicalSimplex
 import Mathlib.Analysis.AbsoluteValue.Equivalence
 import Mathlib.Analysis.Analytic.Basic
+import Mathlib.Analysis.Analytic.Binomial
 import Mathlib.Analysis.Analytic.CPolynomial
 import Mathlib.Analysis.Analytic.CPolynomialDef
 import Mathlib.Analysis.Analytic.ChangeOrigin
