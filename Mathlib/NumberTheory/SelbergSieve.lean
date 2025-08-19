6e62219875e62f54c6bc2902f1921a0695e5d00e
Junyan Xu <junyanxu.math@gmail.com>
2025-08-19 12:41:42 +0000

    chore: import reductions discovered through #28532 (#28608)

--- Mathlib/NumberTheory/SelbergSieve.lean
+++ Mathlib/NumberTheory/SelbergSieve.lean
@@ -3,7 +3,7 @@ Copyright (c) 2024 Arend Mellendijk. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Arend Mellendijk
 -/
-import Mathlib.Analysis.Normed.Ring.Basic
+import Mathlib.Data.Real.Basic
 import Mathlib.NumberTheory.ArithmeticFunction
 
 /-!
