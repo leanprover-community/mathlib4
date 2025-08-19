ff7ffc5b723f7353b5874a61ab82f24d4f68dc15
strihanje01 <strihanje01@gmail.com>
2025-08-19 15:28:58 +0200

    feat(Combinatorics/Additive/VerySmallDoubling): weak non-commutative Kneser's theorem
    add convolution and rewrite rightCosetRepresentingFinset

--- Mathlib/Combinatorics/Additive/VerySmallDoubling.lean
+++ Mathlib/Combinatorics/Additive/VerySmallDoubling.lean
@@ -8,6 +8,7 @@ import Mathlib.Data.Real.GoldenRatio
 import Mathlib.Tactic.Linarith
 import Mathlib.Tactic.Positivity
 import Mathlib.Tactic.Qify
+import Mathlib.Combinatorics.Additive.Convolution
 
 /-!
 # Sets with very small doubling
@@ -95,23 +96,6 @@ private lemma le_card_smul_inter_smul (hA : #(B * A) ≤ K * #A) (ha : a ∈ B) 
 private lemma lt_card_smul_inter_smul (hA : #(B * A) < K * #A) (ha : a ∈ B) (hb : b ∈ B) :
     (2 - K) * #A < #((a • A) ∩ (b • A)) := by
   have : 2 * (#A : ℝ) ≤ #(a •> A ∩ b •> A) + #(B * A) := mod_cast big_intersection ha hb; linarith
-
-private lemma card_smul_inter_smul (A B : Finset G) (x y : G) :
-    #((x • A) ∩ (y • B)) = #{wz ∈ A ×ˢ B | wz.1 * wz.2⁻¹ = x⁻¹ * y} :=
-  card_nbij' (fun z ↦ (x⁻¹ * z, y⁻¹ * z)) (fun zw ↦ x • zw.1)
-    (by simp +contextual [Set.MapsTo, Set.mem_smul_set_iff_inv_smul_mem, mul_assoc])
-    (by simp +contextual [Set.MapsTo, Set.mem_smul_set_iff_inv_smul_mem]
-        simp +contextual [mul_inv_eq_iff_eq_mul, mul_assoc])
-    (by simp [Set.LeftInvOn])
-    (by simp +contextual [Set.LeftInvOn, mul_inv_eq_iff_eq_mul, mul_assoc])
-
-private lemma card_inter_smul (A B : Finset G) (x : G) :
-    #(A ∩ (x • B)) = #{yz ∈ A ×ˢ B | yz.1 * yz.2⁻¹ = x} := by
-  simpa using card_smul_inter_smul _ _ 1 x
-
-private lemma card_inv_smul_inter (A B : Finset G) (x : G) :
-    #(x⁻¹ • A ∩ B) = #{yz ∈ A ×ˢ B | yz.1 * yz.2⁻¹ = x} := by
-  simpa using card_smul_inter_smul _ _ x⁻¹ 1
 
 private lemma le_card_mul_inv_eq (hA : #(B * A) ≤ K * #A) (hx : x ∈ B⁻¹ * B) :
     (2 - K) * #A ≤ #{ab ∈ A ×ˢ A | ab.1 * ab.2⁻¹ = x} := by
@@ -361,52 +345,6 @@ private lemma op_smul_eq_iff_mem {H : Subgroup G} {c : Set G} {x : G}
 omit [DecidableEq G] in
 private lemma op_smul_eq_op_smul_iff_mem {H : Subgroup G} {x y : G} :
     x ∈ (H : Set G) <• y ↔ (H : Set G) <• x = H <• y := op_smul_eq_iff_mem (mem_orbit _ _)
-
-/-- Given finite subsets `A` and `B` of a group `G`, convolution of `A` and `B` is a map `G → ℕ`
-that for a given `x ∈ G` returns the number of representations of `x` in the form `x = ab⁻¹`, where
-`a ∈ A`, `b ∈ B`. -/
-private def convolution (A B : Finset G) : G → ℕ := fun x => #{ab ∈ A ×ˢ B | ab.1 * ab.2⁻¹ = x}
-
-private lemma convolution_nonneg (A B : Finset G) (x : G) : 0 ≤ A.convolution B x := by finiteness
-
-private lemma convolution_pos_of_mem {A B : Finset G} {x : G} (hx : x ∈ A * B⁻¹) :
-    0 < A.convolution B x := by
-  obtain ⟨a, ha, b, hb, h⟩ := mem_mul.mp hx
-  exact card_pos.mpr ⟨(a, b⁻¹),
-    by simp_all only [mem_inv', mem_filter, mem_product, and_self, inv_inv]⟩
-
-private lemma convolution_le_card_left (A B : Finset G) (x : G) : A.convolution B x ≤ #A := by
-  simp only [convolution, ← card_inter_smul]
-  gcongr
-  exact inter_subset_left
-
-private lemma convolution_le_card_right (A B : Finset G) (x : G) : A.convolution B x ≤ #B := by
-  simp only [convolution, ← card_inv_smul_inter]
-  gcongr
-  exact inter_subset_right
-
-private lemma convolution_inv (A B : Finset G) (x : G) :
-    A.convolution B x⁻¹ = B.convolution A x := by
-  rw [convolution, convolution, ← card_inter_smul, ← card_inv_smul_inter, inter_comm]
-
-private lemma convolution_op_smul_eq_op_smul_inv_convolution (A B : Finset G) (s : G) :
-    A.convolution (B <• s) = (A <• s⁻¹).convolution B := funext <| fun x => by
-  simp only [convolution, ← card_inter_smul, smul_comm x (op s), inv_mul_cancel, one_smul,
-    ← card_smul_finset (op s⁻¹) (A ∩ x •> B <• s), op_inv, smul_finset_inter, smul_smul]
-
-private lemma op_smul_convolution_eq_convolution_op_smul_inv (A B : Finset G) (s : G) :
-    (A <• s).convolution B = A.convolution (B <• s⁻¹) := by
-  simpa only [inv_inv] using Eq.symm <| convolution_op_smul_eq_op_smul_inv_convolution A B s⁻¹
-
-private lemma convolution_mul_left_eq_inv_smul_convolution (A B : Finset G) (s x : G) :
-    A.convolution B (s * x) = (s⁻¹ •> A).convolution B x := by
-  simp only [convolution, ← card_inter_smul, ← smul_smul, ← card_smul_finset s (s⁻¹ •> A ∩ x •> B),
-    smul_finset_inter, smul_inv_smul]
-
-private lemma convolution_mul_right_eq_convolution_smul (A B : Finset G) (s x : G) :
-    A.convolution B (x * s) = A.convolution (s •> B) x := by
-  rw [← convolution_inv, mul_inv_rev, convolution_mul_left_eq_inv_smul_convolution,
-    inv_inv, convolution_inv]
 
 /-- Given a finite subset `A` of group `G` and a subgroup `H ≤ G`, right coset representing set of
 `H * A` is a subset `Z` of `A` such that `H * Z = H * A` and `∀ z₁ z₂ ∈ Z → Hz₁ ≠ Hz₂`. -/
