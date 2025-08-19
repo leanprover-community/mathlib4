7932178c4cb3952bd6681e26d29c49503332cf6c
Kenny Lau <kc_kennylau@yahoo.com.hk>
2025-08-19 09:59:38 +0100

    Update OpenImmersion.lean

--- Mathlib/RingTheory/RingHom/OpenImmersion.lean
+++ Mathlib/RingTheory/RingHom/OpenImmersion.lean
@@ -15,6 +15,42 @@ localization map away from some element. We also define the equivalent
 
 universe u
 
+namespace Algebra
+
+open IsLocalization Away
+
+variable {R S T : Type*} [CommSemiring R] [CommSemiring S] [CommSemiring T]
+  [Algebra R S] [Algebra R T]
+
+variable (R S) in
+/-- A standard open immersion is one that is a localization map away from some element. -/
+@[mk_iff] class IsStandardOpenImmersion [Algebra R S] : Prop where
+  exists_away' : ∃ r : R, IsLocalization.Away r S
+
+variable (R S) in
+theorem exists_away [IsStandardOpenImmersion R S] : ∃ r : R, IsLocalization.Away r S :=
+  IsStandardOpenImmersion.exists_away'
+
+lemma IsStandardOpenImmersion.away (r : R) :
+    IsStandardOpenImmersion R (Localization.Away r) :=
+  ⟨r, inferInstance⟩
+
+variable (R S T) in
+@[trans] theorem IsStandardOpenImmersion.trans [Algebra S T] [IsScalarTower R S T]
+    [IsStandardOpenImmersion R S] [IsStandardOpenImmersion S T] :
+    IsStandardOpenImmersion R T :=
+  let ⟨r, _⟩ := exists_away R S
+  let ⟨s, _⟩ := exists_away S T
+  have : Away (algebraMap R S (sec r s).1) T :=
+    .of_associated (associated_sec_fst r s).symm
+  ⟨r * (sec r s).1, mul' S T r _⟩
+
+instance [IsStandardOpenImmersion R T] : IsStandardOpenImmersion S (TensorProduct R S T) :=
+  let ⟨r, _⟩ := exists_away R T
+  ⟨algebraMap R S r, inferInstance⟩
+
+end Algebra
+
 namespace RingHom
 
 variable {R S T : Type*} [CommRing R] [CommRing S] [CommRing T] (f : R →+* S) (g : S →+* T)
@@ -23,20 +59,11 @@ variable {R S T : Type*} [CommRing R] [CommRing S] [CommRing T] (f : R →+* S) 
 @[algebraize RingHom.IsStandardOpenImmersion.toAlgebra]
 def IsStandardOpenImmersion : Prop :=
   letI := f.toAlgebra
-  ∃ r : R, IsLocalization.Away r S
-
-variable (R S) in
-/-- A standard open immersion is one that is a localization map away from some element. -/
-@[mk_iff] class _root_.Algebra.IsStandardOpenImmersion [Algebra R S] : Prop where
-  exists_away : ∃ r : R, IsLocalization.Away r S
-
-lemma _root_.Algebra.IsStandardOpenImmersion.away (r : R) :
-    Algebra.IsStandardOpenImmersion R (Localization.Away r) :=
-  ⟨r, inferInstance⟩
+  Algebra.IsStandardOpenImmersion R S
 
 lemma isStandardOpenImmersion_algebraMap [Algebra R S] :
     (algebraMap R S).IsStandardOpenImmersion ↔ Algebra.IsStandardOpenImmersion R S := by
-  rw [IsStandardOpenImmersion, Algebra.isStandardOpenImmersion_iff, toAlgebra_algebraMap]
+  rw [IsStandardOpenImmersion, toAlgebra_algebraMap]
 
 namespace IsStandardOpenImmersion
 
@@ -46,7 +73,7 @@ lemma algebraMap' [Algebra R S] (r : R) [IsLocalization.Away r S] :
 
 lemma toAlgebra {f : R →+* S} (hf : f.IsStandardOpenImmersion) :
     @Algebra.IsStandardOpenImmersion R S _ _ f.toAlgebra :=
-  letI := f.toAlgebra; ⟨hf⟩
+  letI := f.toAlgebra; hf
 
 /-- A bijective ring map is a standard open immersion. -/
 lemma of_bijective {f : R →+* S} (hf : Function.Bijective f) : f.IsStandardOpenImmersion :=
@@ -65,15 +92,7 @@ lemma comp (hf : f.IsStandardOpenImmersion) (hg : g.IsStandardOpenImmersion) :
   algebraize [f, g, g.comp f]
   obtain ⟨r, hr⟩ := hf
   obtain ⟨s, hs⟩ := hg
-  let s' := (IsLocalization.Away.sec r s).1
-  -- factor this out?
-  have assoc : Associated (algebraMap R S s') s := by
-    unfold s'
-    rw [← IsLocalization.Away.sec_spec, map_pow]
-    exact associated_mul_unit_left _ _ (.pow _ <| IsLocalization.Away.algebraMap_isUnit _)
-  have : IsLocalization.Away (algebraMap R S s') T :=
-    IsLocalization.Away.of_associated assoc.symm
-  exact ⟨r * s', IsLocalization.Away.mul' S T r _⟩
+  exact .trans _ S _
 
 theorem containsIdentities : ContainsIdentities.{u} IsStandardOpenImmersion := id
 
@@ -86,8 +105,7 @@ theorem isStableUnderBaseChange : IsStableUnderBaseChange.{u} IsStandardOpenImme
   refine .mk respectsIso ?_
   introv h
   rw [isStandardOpenImmersion_algebraMap] at h ⊢
-  obtain ⟨r, _⟩ := h
-  exact ⟨algebraMap R S r, inferInstance⟩
+  infer_instance
 
 theorem holdsForLocalizationAway : HoldsForLocalizationAway.{u} IsStandardOpenImmersion := by
   introv R h
