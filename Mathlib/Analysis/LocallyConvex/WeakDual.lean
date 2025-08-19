e2c938aab96767bc940a23a8e8cd9bba2e7fdb7e
Christopher Hoskin <4855578+mans0954@users.noreply.github.com>
2025-08-19 05:59:13 +0100

    Update Mathlib/Analysis/LocallyConvex/WeakDual.lean
    Co-authored-by: Bryan Gin-ge Chen <bryangingechen@gmail.com>

--- Mathlib/Analysis/LocallyConvex/WeakDual.lean
+++ Mathlib/Analysis/LocallyConvex/WeakDual.lean
@@ -117,7 +117,7 @@ section NontriviallyNormedField
 
 variable [NontriviallyNormedField 𝕜] [AddCommGroup E] [Module 𝕜 E]
 
-/- A linear functional `φ` is in the span of a collection of linear functional if and only if `φ` is
+/- A linear functional `φ` is in the span of a collection of linear functionals if and only if `φ` is
 continuous with respect to the topology induced by the collection of linear functionals. See
 `LinearMap.mem_span_iff_continuous_of_finite` for a result about finite collections of linear
 functionals. -/
