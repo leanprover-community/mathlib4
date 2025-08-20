66f12ad825de47cc7dc4abb731710e5ac5bac2d8
mitchell-horner <29882987+mitchell-horner@users.noreply.github.com>
2025-08-21 08:15:22 +1000

    Add `classical`

--- Mathlib/Combinatorics/SimpleGraph/Extremal/Turan.lean
+++ Mathlib/Combinatorics/SimpleGraph/Extremal/Turan.lean
@@ -102,6 +102,7 @@ theorem not_cliqueFree_of_isTuranMaximal (hn : r ≤ card V) (hG : G.IsTuranMaxi
   obtain ⟨a, -, b, -, hab, hGab⟩ : ∃ a ∈ K, ∃ b ∈ K, a ≠ b ∧ ¬ G.Adj a b := by
     simpa only [isNClique_iff, IsClique, Set.Pairwise, mem_coe, ne_eq, and_true, not_forall,
       exists_prop, exists_and_right] using h K
+  classical
   exact hGab <| le_sup_right.trans_eq ((hG.le_iff_eq <| h.sup_edge _ _).1 le_sup_left).symm <|
     (edge_adj ..).2 ⟨Or.inl ⟨rfl, rfl⟩, hab⟩
 
