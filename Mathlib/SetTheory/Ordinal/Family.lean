02e7553a596184f4d3d52e1bd10d25abbbafe51c
Violeta Hernández <vi.hdz.p@gmail.com>
2025-08-19 23:45:18 -0600

    remove import

--- Mathlib/SetTheory/Ordinal/Family.lean
+++ Mathlib/SetTheory/Ordinal/Family.lean
@@ -3,7 +3,6 @@ Copyright (c) 2017 Johannes Hölzl. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro, Floris van Doorn, Violeta Hernández Palacios
 -/
-import Mathlib.SetTheory.Cardinal.UnivLE
 import Mathlib.SetTheory.Ordinal.Arithmetic
 
 /-!
