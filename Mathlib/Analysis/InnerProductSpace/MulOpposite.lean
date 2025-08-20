55cae1c8c6e5c767d8d28b027048c16980364427
themathqueen <23701951+themathqueen@users.noreply.github.com>
2025-08-20 06:21:33 +0000

    Update MulOpposite.lean

--- Mathlib/Analysis/InnerProductSpace/MulOpposite.lean
+++ Mathlib/Analysis/InnerProductSpace/MulOpposite.lean
@@ -3,8 +3,8 @@ Copyright (c) 2025 Monica Omar. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Monica Omar
 -/
-import Mathlib.LinearAlgebra.Basis.MulOpposite
 import Mathlib.Analysis.InnerProductSpace.PiL2
+import Mathlib.LinearAlgebra.Basis.MulOpposite
 
 /-!
 # Inner product space on `Hᵐᵒᵖ`
