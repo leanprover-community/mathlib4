af4a1dc027ae5b4c857ee25ed11b42b32bb89a17
faenuccio <filippo.nuccio@univ-st-etienne.fr>
2025-08-20 20:02:15 +0200

    wip

--- Mathlib/Analysis/Normed/Module/Milman-Pettis.lean
+++ Mathlib/Analysis/Normed/Module/Milman-Pettis.lean
@@ -1,12 +1,31 @@
 import Mathlib.Analysis.Normed.Module.WeakDual
 import Mathlib.Analysis.Normed.Module.Dual
+import Mathlib.Analysis.Normed.Module.Dual
 
 
-variable {E : Type*} [NormedAddCommGroup E] [NormedSpace ℝ E]
+variable {E F : Type*} [NormedAddCommGroup E] [NormedSpace ℝ E]
+  [NormedAddCommGroup F] [NormedSpace ℝ F]
 
-open Metric
+open Metric NormedSpace
 
 #check NormedSpace.polar_closedBall (𝕜 := ℝ) (E := E) (zero_lt_one)
 
--- lemma fromPR StronDual.polar
--- IsClosed (((↑) : StrongDual 𝕜 E → E → 𝕜) '' StrongDual.polar ℝ (ball 0 1)) =
+#check StrongDual ℝ E
+
+local notation3 "𝓤₀" => closedBall (0 : E) 1
+-- local notation3 "𝓤₁" => closedBall (0 : E) 1
+local notation3 "𝓤" => closedBall (0 : StrongDual ℝ (StrongDual ℝ E)) 1
+
+-- #check inclusionInDoubleDualLi ℝ
+lemma goldstine : closure (StrongDual.toWeakDual '' (inclusionInDoubleDual ℝ E '' 𝓤₀)) = 𝓤 := by
+  sorry
+
+
+-- theorem polar_flip_polar_eq {B : E →ₗ[ℝ] F →ₗ[ℝ] ℝ} {s : Set E} [Nonempty s] :
+--     B.flip.polar (B.polar s) = closedAbsConvexHull (E := WeakBilin B) ℝ s := by
+--
+--
+--
+--
+-- theorem Bourbaki_EVT_4_2_4_5 :
+--     (strongDualPairing ℝ E).polar ((strongDualPairing ℝ E).flip.polar 𝓑₀) = 𝓑 := by
