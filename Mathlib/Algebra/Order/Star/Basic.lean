8a56d9de2e524e13085e211fa6ca81927f7d8294
staroperator <staroperator92@gmail.com>
2025-08-20 03:43:36 +0000

    chore(Algebra/Group): rename `Nat.addSubmonoid_closure_one` and `AddSubgroup.closure_singleton_int_one_eq_top` (#28301)
    This PR renames `Nat.addSubmonoid_closure_one` to `Nat.addSubmonoidClosure_one`, and `AddSubgroup.closure_singleton_int_one_eq_top` to `Int.addSubgroupClosure_one`, to align with naming convention. It also deprecates `AddSubgroup.zmultiples_one_eq_top` because it's duplication of existing `Int.zmultiples_one`, and simplifies the proof of `AddGroup.FG ℤ` using the renamed theorem.

--- Mathlib/Algebra/Order/Star/Basic.lean
+++ Mathlib/Algebra/Order/Star/Basic.lean
@@ -395,7 +395,7 @@ instance Nat.instStarOrderedRing : StarOrderedRing ℕ where
     have : AddSubmonoid.closure (range fun x : ℕ ↦ x * x) = ⊤ :=
       eq_top_mono
         (AddSubmonoid.closure_mono <| singleton_subset_iff.2 <| mem_range.2 ⟨1, one_mul _⟩)
-        Nat.addSubmonoid_closure_one
+        Nat.addSubmonoidClosure_one
     simp [this, le_iff_exists_add]
 
 namespace IsStarProjection
