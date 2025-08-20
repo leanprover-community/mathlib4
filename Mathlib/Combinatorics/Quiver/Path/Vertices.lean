d74b66b6bec2cbbdd40faa6a9719b2f5a78bb5f5
Matteo Cipollina <matteo.cipollina@yahoo.it>
2025-08-20 13:21:12 +0200

    fix unused import

--- Mathlib/Combinatorics/Quiver/Path/Vertices.lean
+++ Mathlib/Combinatorics/Quiver/Path/Vertices.lean
@@ -5,7 +5,6 @@ Authors: Matteo Cipollina
 -/
 
 import Mathlib.Algebra.Order.Group.Nat
-import Mathlib.Algebra.Order.Sub.Basic
 import Mathlib.Combinatorics.Quiver.Path
 import Mathlib.Data.Finset.Dedup
 
