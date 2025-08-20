e2fe06913ea28320687e2a5a62f40fba870a6bdf
Violeta Hernández <vi.hdz.p@gmail.com>
2025-08-20 00:51:28 -0600

    add thms

--- Mathlib/SetTheory/Cardinal/Basic.lean
+++ Mathlib/SetTheory/Cardinal/Basic.lean
@@ -621,7 +621,10 @@ theorem mk_emptyCollection (α : Type u) : #(∅ : Set α) = 0 :=
 theorem mk_emptyCollection_iff {α : Type u} {s : Set α} : #s = 0 ↔ s = ∅ := by
   rw [mk_eq_zero_iff, isEmpty_coe_sort]
 
-lemma mk_set_ne_zero_iff {α : Type u} (s : Set α) : #s ≠ 0 ↔ s.Nonempty := by
+theorem mk_set_eq_zero_iff {s : Set α} : #s = 0 ↔ s = ∅ := by
+  rw [mk_eq_zero_iff, isEmpty_coe_sort]
+
+theorem mk_set_ne_zero_iff {α : Type u} (s : Set α) : #s ≠ 0 ↔ s.Nonempty := by
   rw [mk_ne_zero_iff, nonempty_coe_sort]
 
 @[simp]
@@ -765,6 +768,10 @@ theorem mk_eq_nat_iff_fintype {n : ℕ} : #α = n ↔ ∃ h : Fintype α, @Finty
     exact ⟨⟨t, eq_univ_iff_forall.1 ht⟩, hn⟩
   · rintro ⟨⟨t, ht⟩, hn⟩
     exact ⟨t, eq_univ_iff_forall.2 ht, hn⟩
+
+theorem mk_set_eq_one_iff {s : Set α} : #s = 1 ↔ ∃ x, s = {x} := by
+  rw [← Nat.cast_one, mk_set_eq_nat_iff_finset]
+  aesop (add simp [Finset.card_eq_one])
 
 theorem mk_union_add_mk_inter {α : Type u} {S T : Set α} :
     #(S ∪ T : Set α) + #(S ∩ T : Set α) = #S + #T := by
