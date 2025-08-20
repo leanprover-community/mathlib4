4555bbe57e4c8cb1ff83064c1c929cab2d0c4290
Robin Carlier <57142648+robin-carlier@users.noreply.github.com>
2025-08-20 05:06:03 +0000

    chore(CategoryTheory/Monoidal): deprecate `CategoryTheory.Monoidal.Limits` (#28423)
    This was renamed in #25732 to `CategoryTheory/Monoidal/Limits/Basic`.

--- Mathlib.lean
+++ Mathlib.lean
@@ -2498,6 +2498,7 @@ import Mathlib.CategoryTheory.Monoidal.Internal.Module
 import Mathlib.CategoryTheory.Monoidal.Internal.Types.Basic
 import Mathlib.CategoryTheory.Monoidal.Internal.Types.CommGrp_
 import Mathlib.CategoryTheory.Monoidal.Internal.Types.Grp_
+import Mathlib.CategoryTheory.Monoidal.Limits
 import Mathlib.CategoryTheory.Monoidal.Limits.Basic
 import Mathlib.CategoryTheory.Monoidal.Limits.Preserves
 import Mathlib.CategoryTheory.Monoidal.Linear
