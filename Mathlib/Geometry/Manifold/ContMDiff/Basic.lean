22a77acae07f5711dd0968d8fcf02cc65fa262d6
bwangpj <70694994+bwangpj@users.noreply.github.com>
2025-08-21 21:36:26 +0800

    Update Basic.lean

--- Mathlib/Geometry/Manifold/ContMDiff/Basic.lean
+++ Mathlib/Geometry/Manifold/ContMDiff/Basic.lean
@@ -162,20 +162,18 @@ end id
 
 section Iterate
 
-/-- The iterates of `C^n` functions are `C^n`. -/
-theorem ContMDiff.iterate {f : M → M} (hf : ContMDiff I I n f) (k : ℕ) :
-    ContMDiff I I n (f^[k]) := by
-  induction' k with k h
-  · simpa using contMDiff_id
-  simpa using h.comp hf
-
 /-- The iterates of `C^n` functions on domains are `C^n`. -/
 theorem ContMDiffOn.iterate {f : M → M} (hf : ContMDiffOn I I n f s)
     (hmaps : Set.MapsTo f s s) (k : ℕ) :
     ContMDiffOn I I n (f^[k]) s := by
   induction' k with k h
   · simpa using contMDiffOn_id
-  simpa using h.comp hf hmaps
+  · simpa using h.comp hf hmaps
+
+/-- The iterates of `C^n` functions are `C^n`. -/
+theorem ContMDiff.iterate {f : M → M} (hf : ContMDiff I I n f) (k : ℕ) :
+    ContMDiff I I n (f^[k]) :=
+  contMDiffOn_univ.mp ((contMDiffOn_univ.mpr hf).iterate (univ.mapsTo_univ f) k)
 
 end Iterate
 
