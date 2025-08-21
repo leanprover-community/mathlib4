7c6821b8c7d64d06ff07ae1e73abdac81042c65c
themathqueen <23701951+themathqueen@users.noreply.github.com>
2025-08-21 09:12:35 +0000

    Update Mathlib/LinearAlgebra/Matrix/Spectrum.lean
    Co-authored-by: Riccardo Brasca <riccardo.brasca@gmail.com>

--- Mathlib/LinearAlgebra/Matrix/Spectrum.lean
+++ Mathlib/LinearAlgebra/Matrix/Spectrum.lean
@@ -152,7 +152,7 @@ lemma rank_eq_card_non_zero_eigs : A.rank = Fintype.card {i // hA.eigenvalues i 
 /-- The eigenvalues of a Hermitian matrix `A` are all zero iff `A = 0`. -/
 theorem eigenvalues_eq_zero_iff :
     hA.eigenvalues = 0 ↔ A = 0 := by
-  refine ⟨fun h => ?_, fun h => by ext; simp [h, eigenvalues_eq]⟩
+  refine ⟨fun h ↦ ?_, fun h ↦ by ext; simp [h, eigenvalues_eq]⟩
   rw [hA.spectral_theorem, h, Pi.comp_zero, RCLike.ofReal_zero, Function.const_zero,
     Pi.zero_def, diagonal_zero, mul_zero, zero_mul]
 
