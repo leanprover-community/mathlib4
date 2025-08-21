ce7d12d77897dc6e5f2d995c35831b24bb8285d0
Anatole Dedecker <anatolededecker@gmail.com>
2025-08-21 15:31:24 +0000

    chore(OreLocalization): generalize some results from rings to monoids with zeros (#27174)

--- Mathlib.lean
+++ Mathlib.lean
@@ -5577,6 +5577,7 @@ import Mathlib.RingTheory.Nullstellensatz
 import Mathlib.RingTheory.OrderOfVanishing
 import Mathlib.RingTheory.OreLocalization.Basic
 import Mathlib.RingTheory.OreLocalization.Cardinality
+import Mathlib.RingTheory.OreLocalization.NonZeroDivisors
 import Mathlib.RingTheory.OreLocalization.OreSet
 import Mathlib.RingTheory.OreLocalization.Ring
 import Mathlib.RingTheory.OrzechProperty
