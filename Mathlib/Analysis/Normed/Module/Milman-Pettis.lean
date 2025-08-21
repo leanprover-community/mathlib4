2936fb29070b14fb780bf49da51a0ac457e10ba9
faenuccio <filippo.nuccio@univ-st-etienne.fr>
2025-08-21 14:50:48 +0200

    wip

--- Mathlib/Analysis/Normed/Module/Milman-Pettis.lean
+++ Mathlib/Analysis/Normed/Module/Milman-Pettis.lean
@@ -1,31 +1,44 @@
+/-
+Copyright (c) 2025 Filippo A. E. Nuccio. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Filippo A. E. Nuccio
+-/
+
+import Mathlib.Analysis.Convex.Uniform
 import Mathlib.Analysis.Normed.Module.WeakDual
 import Mathlib.Analysis.Normed.Module.Dual
 import Mathlib.Analysis.Normed.Module.Dual
 
+variable {E : Type*} [NormedAddCommGroup E] [NormedSpace ℝ E]
 
-variable {E F : Type*} [NormedAddCommGroup E] [NormedSpace ℝ E]
-  [NormedAddCommGroup F] [NormedSpace ℝ F]
-
-open Metric NormedSpace
-
-#check NormedSpace.polar_closedBall (𝕜 := ℝ) (E := E) (zero_lt_one)
-
-#check StrongDual ℝ E
+open Metric NormedSpace Function
 
 local notation3 "𝓤₀" => closedBall (0 : E) 1
--- local notation3 "𝓤₁" => closedBall (0 : E) 1
-local notation3 "𝓤" => closedBall (0 : StrongDual ℝ (StrongDual ℝ E)) 1
+local notation3 "E**" => StrongDual ℝ (StrongDual ℝ E)
 
--- #check inclusionInDoubleDualLi ℝ
-lemma goldstine : closure (StrongDual.toWeakDual '' (inclusionInDoubleDual ℝ E '' 𝓤₀)) = 𝓤 := by
+/- Goldstine lemma (see Brezis, Chapter § 3.5, Lemma 3.4) says that the unit ball in the double
+dual of a Banach space (**FAE: I suspect completeness is not needed) ** is the closure, with respect
+to the weak topology `σ(E**, E*)` induced by the canonical pairing `E** × E* → ℝ`, of the image of
+the unit ball in  `E`. Observe that, for any topological `𝕜`-module `M`, `strongDualPairing 𝕜 M` is
+the pairing whose *first* variable is in `M*` and the second is in `M`. -/
+lemma goldstine : closure (X := (WeakBilin (strongDualPairing ℝ (StrongDual ℝ E))))
+  (inclusionInDoubleDual ℝ E '' 𝓤₀) = ball (0 : E**) 1 := by sorry
+
+lemma surjective_iff_ball_le_range {F : Type*} [NormedAddCommGroup F] [NormedSpace ℝ F]
+    {f : E → F} : Surjective f ↔ ∃ s : Set F, ∃ ρ > 0, ball 0 ρ ≤ Set.range f := by
   sorry
 
+set_option linter.style.commandStart false
 
--- theorem polar_flip_polar_eq {B : E →ₗ[ℝ] F →ₗ[ℝ] ℝ} {s : Set E} [Nonempty s] :
---     B.flip.polar (B.polar s) = closedAbsConvexHull (E := WeakBilin B) ℝ s := by
---
---
---
---
--- theorem Bourbaki_EVT_4_2_4_5 :
---     (strongDualPairing ℝ E).polar ((strongDualPairing ℝ E).flip.polar 𝓑₀) = 𝓑 := by
+/- Milman-Pettis theorem: every uniformly convex Banach (**FAE: Complete Needed?**) space is\
+reflexive. For the time being, we state this property as the surjectivity of
+`inclusionInDoubleDual`,
+but it must be proven that for normed space this is equivalent to `includionInDoubleDual` being
+a homeomorphism. -/
+theorem surjective_of_uniformConvexSpace [UniformConvexSpace E] :
+    Surjective (inclusionInDoubleDual ℝ E) := by
+  rw [surjective_iff_ball_le_range]
+  refine ⟨ball 0 1, _, zero_lt_one, ?_⟩
+  intro φ hφ
+  let ε := infDist φ (ball 0 1)
+  sorry
