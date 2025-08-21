a60e32a429beae4870cdd553cee1eee58d2cff59
Xavier Roblot <46200072+xroblot@users.noreply.github.com>
2025-08-21 15:44:44 +0200

    imports

--- Mathlib/RingTheory/Ideal/Norm/RelNorm.lean
+++ Mathlib/RingTheory/Ideal/Norm/RelNorm.lean
@@ -3,9 +3,8 @@ Copyright (c) 2022 Anne Baanen. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Anne Baanen, Alex J. Best
 -/
-import Mathlib.RingTheory.IntegralClosure.IntegralRestrict
-import Mathlib.RingTheory.Localization.AtPrime.Basic
 import Mathlib.RingTheory.DedekindDomain.Instances
+import Mathlib.RingTheory.IntegralClosure.IntegralRestrict
 
 /-!
 
