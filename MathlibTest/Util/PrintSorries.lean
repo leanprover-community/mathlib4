b0cd39d3dd7325a4d87f28fcd6c31df005a52487
Yaël Dillies <yael.dillies@gmail.com>
2025-08-21 11:06:19 +0000

    fix tests

--- MathlibTest/Util/PrintSorries.lean
+++ MathlibTest/Util/PrintSorries.lean
@@ -1,37 +1,34 @@
 import Mathlib.Util.PrintSorries
 
+/-- warning: declaration uses 'sorry' -/
+#guard_msgs in
 theorem thm1 : 1 = 2 := by sorry
 
-/--
-info: thm1 has sorry
--/
+/-- info: thm1 has sorry -/
 #guard_msgs in
 #print sorries thm1
 
 theorem thm2 : 1 = 2 := thm1
 
-/--
-info: thm1 has sorry
--/
+/-- info: thm1 has sorry -/
 #guard_msgs in
 #print sorries thm2
 
-/--
-info: thm1 has sorry
--/
+/-- info: thm1 has sorry -/
 #guard_msgs in
 #print sorries
 
+/-- warning: declaration uses 'sorry' -/
+#guard_msgs in
 def f (n : Nat) : Nat := sorry
 theorem thm3 : f 1 = f 2 := rfl -- (!) This is since it's a fixed `sorry : Nat`
 
-/--
-info: f has sorry
--/
+/-- info: f has sorry -/
 #guard_msgs in
 #print sorries thm3
 
-
+/-- warning: declaration uses 'sorry' -/
+#guard_msgs in
 def f' : Nat → Nat := sorry
 /--
 error: Not a definitional equality: the left-hand side
@@ -48,6 +45,9 @@ but is expected to have type
 -/
 #guard_msgs in
 theorem thm3' : f' 1 = f' 2 := rfl -- fails as expected, it's an unknown `sorry : Nat → Nat`
+
+/-- warning: declaration uses 'sorry' -/
+#guard_msgs in
 theorem thm : True := by
   let f : Nat → Nat := sorry
   have : f 1 = f 2 := sorry
@@ -63,10 +63,10 @@ thm has sorry
 #guard_msgs in
 #print sorries thm
 
+/-- warning: declaration uses 'sorry' -/
+#guard_msgs in
 theorem thm' : True := sorryAx _ false
 
-/--
-info: thm' has sorryAx
--/
+/-- info: thm' has sorryAx -/
 #guard_msgs in
 #print sorries thm'
