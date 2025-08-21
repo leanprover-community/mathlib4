905771e5eb83ac1af88f5cb33195880c22b38353
Kenny Lau <kc_kennylau@yahoo.com.hk>
2025-08-21 10:07:58 +0100

    Apply suggestions from code review
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- Mathlib/Algebra/Order/Hom/Submonoid.lean
+++ Mathlib/Algebra/Order/Hom/Submonoid.lean
@@ -3,14 +3,14 @@ Copyright (c) 2025 Kenny Lau. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kenny Lau
 -/
-
 import Mathlib.Algebra.Group.Submonoid.Operations
 import Mathlib.Algebra.Order.Hom.Monoid
 
-/-! # Isomorphism of submonoids of ordered monoids
+/-!
+# Isomorphism of submonoids of ordered monoids
 -/
 
-/-- The top submononid is isomorphic to the whole type as ordered monoids. -/
+/-- The top submonoid is order isomorphic to the whole monoid. -/
 @[simps!]
 def Submonoid.topOrderMonoidIso {α : Type*} [Preorder α] [Monoid α] : (⊤ : Submonoid α) ≃*o α where
   __ := Submonoid.topEquiv
