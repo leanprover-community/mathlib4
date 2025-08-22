a3e995e23b2f074648bd910a0530e5855cc20b34
themathqueen <2497250a@research.gla.ac.uk>
2025-08-22 15:25:00 +0000

    feat(RingTheory/Coalgebra/MulOpposite): coalgebra instance for MulOpposite (#25858)
    Adding an instance for `Aᵐᵒᵖ` being an `R`-coalgebra when `A` is.
    
    
    
    Co-authored-by: themathqueen <23701951+themathqueen@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -5289,6 +5289,7 @@ import Mathlib.RingTheory.Coalgebra.Basic
 import Mathlib.RingTheory.Coalgebra.Equiv
 import Mathlib.RingTheory.Coalgebra.Hom
 import Mathlib.RingTheory.Coalgebra.MonoidAlgebra
+import Mathlib.RingTheory.Coalgebra.MulOpposite
 import Mathlib.RingTheory.Coalgebra.TensorProduct
 import Mathlib.RingTheory.Complex
 import Mathlib.RingTheory.Congruence.Basic
