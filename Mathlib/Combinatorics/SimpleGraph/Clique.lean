f4ec67fd5c52c2e042b0a1195b0e4d6d65464d53
mitchell-horner <29882987+mitchell-horner@users.noreply.github.com>
2025-08-23 08:44:20 +1000

    Impl `isClique_image_copy_top`, etc.

--- Mathlib/Combinatorics/SimpleGraph/Clique.lean
+++ Mathlib/Combinatorics/SimpleGraph/Clique.lean
@@ -184,6 +184,14 @@ lemma isClique_sup_edge_of_ne_iff {v w : α} {s : Set α} (h : v ≠ w) :
   ⟨fun h' ↦ ⟨h'.sdiff_of_sup_edge, (edge_comm .. ▸ h').sdiff_of_sup_edge⟩,
     fun h' ↦ isClique_sup_edge_of_ne_sdiff h h'.1 h'.2⟩
 
+/-- The vertices in a copy of `⊤` are a clique. -/
+theorem isClique_image_copy_top (f : Copy (⊤ : SimpleGraph β) G) :
+    G.IsClique (f '' Set.univ) := by
+  intro _ ⟨_, _, h⟩ _ ⟨_, _, h'⟩ heq
+  rw [← h, show f _ = f.topEmbedding _ by rfl, ← h', show f _ = f.topEmbedding _ by rfl] at heq ⊢
+  rwa [← f.topEmbedding.coe_toEmbedding, (f.topEmbedding.apply_eq_iff_eq _ _).ne,
+    ← top_adj, ← f.topEmbedding.map_adj_iff] at heq
+
 end Clique
 
 /-! ### `n`-cliques -/
@@ -306,6 +314,12 @@ lemma IsNClique.erase_of_sup_edge_of_mem [DecidableEq α] {v w : α} {s : Finset
   isClique := coe_erase v _ ▸ hc.1.sdiff_of_sup_edge
   card_eq  := by rw [card_erase_of_mem hx, hc.2]
 
+/-- The vertices in a copy of `⊤ : SimpleGraph β` are a `card β`-clique. -/
+theorem isNClique_map_copy_top [DecidableEq α] [Fintype β] (f : Copy (⊤ : SimpleGraph β) G) :
+    G.IsNClique (card β) (univ.map f.toEmbedding) := by
+  rw [isNClique_iff, card_map, card_univ, coe_map, coe_univ]
+  exact ⟨isClique_image_copy_top f, rfl⟩
+
 end NClique
 
 /-! ### Graphs without cliques -/
@@ -353,6 +367,13 @@ theorem not_cliqueFree_iff (n : ℕ) : ¬G.CliqueFree n ↔ Nonempty (completeGr
 
 theorem cliqueFree_iff {n : ℕ} : G.CliqueFree n ↔ IsEmpty (completeGraph (Fin n) ↪g G) := by
   rw [← not_iff_not, not_cliqueFree_iff, not_isEmpty_iff]
+
+/-- A simple graph has no `card β`-cliques iff it does not contain `⊤ : SimpleGraph β`. -/
+theorem cliqueFree_iff_top_free {β : Type*} [Fintype β] :
+    G.CliqueFree (card β) ↔ (⊤ : SimpleGraph β).Free G := by
+  rw [← not_iff_not, not_free, cliqueFree_iff, not_isEmpty_iff,
+    isContained_congr (Iso.completeGraph (Fintype.equivFin β)) Iso.refl]
+  exact ⟨fun ⟨f⟩ ↦ ⟨f.toCopy⟩, fun ⟨f⟩ ↦ ⟨f.topEmbedding⟩⟩
 
 theorem not_cliqueFree_card_of_top_embedding [Fintype α] (f : (⊤ : SimpleGraph α) ↪g G) :
     ¬G.CliqueFree (card α) := by
