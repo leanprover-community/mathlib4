7e7f1c2971232a6601881cca92865f60a2ee215d
euprunin <euprunin@users.noreply.github.com>
2025-08-22 18:35:39 +0000

    chore(Analysis/CStarAlgebra/Module): golf entire `map_top_submodule` (#28374)

--- Mathlib/Analysis/CStarAlgebra/Module/Synonym.lean
+++ Mathlib/Analysis/CStarAlgebra/Module/Synonym.lean
@@ -170,11 +170,7 @@ def linearEquiv [Semiring R] [AddCommGroup E] [Module R E] : C⋆ᵐᵒᵈ(A, E)
     invFun := (equiv _ _).symm }
 
 lemma map_top_submodule {R : Type*} [Semiring R] [AddCommGroup E] [Module R E] :
-    (⊤ : Submodule R E).map (linearEquiv R A E).symm = ⊤ := by
-  ext x
-  refine ⟨fun _  => trivial, fun _ => ?_⟩
-  rw [Submodule.mem_map]
-  exact ⟨linearEquiv R A E x, by simp⟩
+    (⊤ : Submodule R E).map (linearEquiv R A E).symm = ⊤ := Submodule.map_eq_top_iff.mpr rfl
 
 instance instModuleFinite [Semiring R] [AddCommGroup E] [Module R E] [Module.Finite R E] :
     Module.Finite R C⋆ᵐᵒᵈ(A, E) := ‹Module.Finite R E›
