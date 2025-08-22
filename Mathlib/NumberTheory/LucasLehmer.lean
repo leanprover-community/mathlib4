3bd7692c9b956d1b8303efe22e600a765303ad7e
Xavier Roblot <46200072+xroblot@users.noreply.github.com>
2025-08-22 13:19:06 +0000

    chore: remove unnecessary `simp` call (#28775)
    cf. [#mathlib4 > Lint error with &#96;Simp says&#96;](https://leanprover.zulipchat.com/#narrow/channel/287929-mathlib4/topic/Lint.20error.20with.20.60Simp.20says.60)

--- Mathlib/NumberTheory/LucasLehmer.lean
+++ Mathlib/NumberTheory/LucasLehmer.lean
@@ -561,11 +561,9 @@ theorem order_ω (p' : ℕ) (h : lucasLehmerResidue (p' + 2) = 0) :
   -- the order of ω divides 2^p
   · exact Nat.prime_two
   · intro o
-    have ω_pow := orderOf_dvd_iff_pow_eq_one.1 o
-    replace ω_pow :=
-      congr_arg (Units.coeHom (X (q (p' + 2))) : Units (X (q (p' + 2))) → X (q (p' + 2))) ω_pow
-    simp? at ω_pow says
-      simp only [Units.coeHom_apply, Units.val_pow_eq_pow_val, ωUnit_coe, Units.val_one] at ω_pow
+    have ω_pow :=
+      congr_arg (Units.coeHom (X (q (p' + 2))) : Units (X (q (p' + 2))) → X (q (p' + 2))) <|
+        orderOf_dvd_iff_pow_eq_one.1 o
     have h : (1 : ZMod (q (p' + 2))) = -1 :=
       congr_arg Prod.fst (ω_pow.symm.trans (ω_pow_eq_neg_one p' h))
     haveI : Fact (2 < (q (p' + 2) : ℕ)) := ⟨two_lt_q _⟩
