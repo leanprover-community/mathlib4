018de68385cf07229d754d09f614d3ea9857080f
Gaëtan Serré <56162277+gaetanserre@users.noreply.github.com>
2025-08-22 19:59:56 +0200

    Update Mathlib/Analysis/SpecialFunctions/Sigmoid.lean
    Co-authored-by: themathqueen <23701951+themathqueen@users.noreply.github.com>

--- Mathlib/Analysis/SpecialFunctions/Sigmoid.lean
+++ Mathlib/Analysis/SpecialFunctions/Sigmoid.lean
@@ -54,7 +54,6 @@ lemma range_sigmoid : range Sigmoid = Ioo 0 1 := by
     · exact inv_lt_one_of_one_lt₀ <| lt_add_of_pos_right 1 (exp_pos (-y))
   · intro hx
     use -(log (-1 + x.1⁻¹))
-    simp only [Sigmoid, RelEmbedding.coe_mk, Embedding.coeFn_mk, neg_neg]
     suffices exp (log (-1 + x.1⁻¹)) = -1 + x.1⁻¹ by
       simp only [this, add_neg_cancel_left, inv_inv]
     exact exp_log (lt_neg_add_iff_lt.mpr <| one_lt_inv_iff₀.mpr hx)
