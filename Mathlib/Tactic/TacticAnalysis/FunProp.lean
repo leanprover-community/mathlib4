b9646977ecdffc540a685738c925e9e9d21e29fe
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-08-23 13:57:49 +0200

    refactor: add a framework for a generic simple tactic replacement

--- Mathlib/Tactic/TacticAnalysis/FunProp.lean
+++ Mathlib/Tactic/TacticAnalysis/FunProp.lean
@@ -16,6 +16,35 @@ This file defines passes to run from the tactic analysis framework.
 open Lean Mathlib
 
 /--
+Define a pass that tries replacing a list of tactics with one better tactic.
+* `new` is the `TSyntax` of the tactic itself, such as ``(tactic| fun_prop)`.
+  We assume that `new` takes no custom arguments.
+* `oldTacticKinds` describes the tactics that are subsumed by `new`:
+  is an array of `SyntaxNodeKind`s for the tactics' main parsers
+  (such as `Mathlib.Tactic.linarith`).
+  Providing several kinds can be useful to cover all variants of one particular tactic in one go.
+* `newName` is how the new tactic should be printed in a failure message
+* `msg_pass` is the message shown to the user when the new replacement works.
+-/
+def tacticReplacementWith (oldTacticKinds : Array SyntaxNodeKind) (new : MetaM (TSyntax `tactic))
+    (newName : MessageData) (msg_success : MessageData) : TacticAnalysis.Config :=.ofComplex {
+  out := (List MVarId × MessageData)
+  ctx := Syntax
+  trigger _ stx := if oldTacticKinds.contains stx.getKind
+    then .accept stx else .skip
+  test _stx goal := do
+    try
+      let (goals, _) ← Lean.Elab.runTactic goal (← new)
+      return (goals, m!"")
+    catch _e =>
+      let name ← mkAuxDeclName `extracted
+      let ((sig, _, _), _) ← (Mathlib.Tactic.ExtractGoal.goalSignature name goal).run
+      return ([goal], m!"{newName} did not prove {sig}")
+  tell _stx _old new :=
+    if new.1.1.isEmpty then msg_success
+    else none }
+
+/--
 Define a pass that tries replacing a specific tactic with `fun_prop`.
 
 `tacticKind` is an array of `SyntaxNodeKind` for the tactic's main parsers,
@@ -23,23 +52,9 @@ for example `Mathlib.Tactic.linarith`.
 This can be useful to cover all variants of one particular tactic.
 -/
 def funPropReplacementWith (tacticKinds : Array SyntaxNodeKind) :
-    TacticAnalysis.Config :=.ofComplex {
-  out := (List MVarId × MessageData)
-  ctx := Syntax
-  trigger _ stx := if tacticKinds.contains stx.getKind
-    then .accept stx else .skip
-  test _stx goal := do
-    let tac ← `(tactic| fun_prop)
-    try
-      let (goals, _) ← Lean.Elab.runTactic goal tac
-      return (goals, m!"")
-    catch _e => -- `fun_prop` throws an error if it fails.
-      let name ← mkAuxDeclName `extracted
-      let ((sig, _, _), _) ← (Mathlib.Tactic.ExtractGoal.goalSignature name goal).run
-      return ([goal], m!"fun_prop did not prove {sig}")
-  tell _stx _old new :=
-    if new.1.1.isEmpty then m!"'fun_prop' can solve this, and is almost always faster"
-    else none }
+    TacticAnalysis.Config :=
+  tacticReplacementWith tacticKinds (`(tactic| fun_prop)) m!"fun_prop"
+    m!"'fun_prop' can solve this, and is almost always faster"
 
 /-- Suggest replacing the `continuity` tactic (and its `continuity?` variant)
 with `fun_prop` if that also solves the goal. -/
