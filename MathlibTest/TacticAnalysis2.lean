0ad06efe73b5b53be62fef1f6bde7b30a377f724
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-08-23 14:56:41 +0200

    wip measurability test

--- MathlibTest/TacticAnalysis2.lean
+++ MathlibTest/TacticAnalysis2.lean
@@ -1,9 +1,15 @@
 import Mathlib.Tactic.TacticAnalysis.FunProp
 import Mathlib.Topology.Continuous
 
+import Mathlib.MeasureTheory.MeasurableSpace.Basic
+--import Mathlib.MeasureTheory.Constructions.BorelSpace.Basic
+--import Mathlib.MeasureTheory.Function.SpecialFunctions.Basic
+--import Mathlib.MeasureTheory.Function.SpecialFunctions.Inner
+--import Mathlib.MeasureTheory.Function.StronglyMeasurable.Lemmas
+
 set_option linter.tacticAnalysis.continuityToFunProp true
 
-variable {X : Type*} [TopologicalSpace X]
+variable {X Y : Type*} [TopologicalSpace X]
 
 /-- warning: 'fun_prop' can solve this, and is almost always faster -/
 #guard_msgs in
@@ -21,4 +27,5 @@ warning: 'fun_prop' can solve this, and is almost always faster
 #guard_msgs in
 example : Continuous (@id X) := by continuity?
 
--- TODO: also add a test for measurability!
+-- TODO: see what imports make out of this!
+example : Measurable (@id Y) := by measurability
