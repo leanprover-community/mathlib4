584bd7f0556a20fa078f47c40b9033a6fbf5e209
Yuyang Zhao 赵雨扬 <zhao.yu-yang@foxmail.com>
2025-08-23 22:07:28 +0800

    perf

--- Mathlib/MeasureTheory/Integral/IntervalIntegral/Basic.lean
+++ Mathlib/MeasureTheory/Integral/IntervalIntegral/Basic.lean
@@ -53,8 +53,8 @@ open MeasureTheory Set Filter Function TopologicalSpace
 open scoped Topology Filter ENNReal Interval NNReal
 
 variable {ι 𝕜 ε ε' E F A : Type*} [NormedAddCommGroup E]
-  [TopologicalSpace ε] [AddCommMonoid ε] [ENormedAddMonoid ε]
-  [TopologicalSpace ε'] [AddCommMonoid ε'] [ENormedAddMonoid ε']
+  [TopologicalSpace ε] [AddMonoid ε] [ENormedAddMonoid ε]
+  [TopologicalSpace ε'] [AddMonoid ε'] [ENormedAddMonoid ε']
 
 /-!
 ### Integrability on an interval
@@ -295,14 +295,15 @@ theorem sub {f g : ℝ → E} (hf : IntervalIntegrable f μ a b) (hg : IntervalI
     IntervalIntegrable (fun x => f x - g x) μ a b :=
   ⟨hf.1.sub hg.1, hf.2.sub hg.2⟩
 
-theorem sum [ContinuousAdd ε]
+theorem sum {ε} [TopologicalSpace ε] [AddCommMonoid ε] [ENormedAddMonoid ε] [ContinuousAdd ε]
     (s : Finset ι) {f : ι → ℝ → ε} (h : ∀ i ∈ s, IntervalIntegrable (f i) μ a b) :
     IntervalIntegrable (∑ i ∈ s, f i) μ a b :=
   ⟨integrable_finset_sum' s fun i hi => (h i hi).1, integrable_finset_sum' s fun i hi => (h i hi).2⟩
 
 /-- Finite sums of interval integrable functions are interval integrable. -/
 @[simp]
-protected theorem finsum [ContinuousAdd ε] [PseudoMetrizableSpace ε] {f : ι → ℝ → ε}
+protected theorem finsum {ε} [TopologicalSpace ε] [AddCommMonoid ε] [ENormedAddMonoid ε]
+    [ContinuousAdd ε] [PseudoMetrizableSpace ε] {f : ι → ℝ → ε}
     (h : ∀ i, IntervalIntegrable (f i) μ a b) :
     IntervalIntegrable (∑ᶠ i, f i) μ a b := by
   by_cases h₁ : f.support.Finite
