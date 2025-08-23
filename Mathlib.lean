e6b0a2f4d6136a7a6d1f0ee53c111bf2f1c449d7
Xavier Roblot <46200072+xroblot@users.noreply.github.com>
2025-08-23 15:08:04 +0200

    fix mathlib

--- Mathlib.lean
+++ Mathlib.lean
@@ -5797,6 +5797,7 @@ import Mathlib.RingTheory.WittVector.Verschiebung
 import Mathlib.RingTheory.WittVector.WittPolynomial
 import Mathlib.RingTheory.ZMod
 import Mathlib.RingTheory.ZMod.UnitsCyclic
+import Mathlib.Sandbox
 import Mathlib.SetTheory.Cardinal.Aleph
 import Mathlib.SetTheory.Cardinal.Arithmetic
 import Mathlib.SetTheory.Cardinal.Basic
