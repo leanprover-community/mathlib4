1d90c9fbbb178a7d1e2e475170fbd1908f870eae
Yi.Yuan <kysyy1@126.com>
2025-08-25 06:45:01 +0800

    Update Mathlib/NumberTheory/Multiplicity.lean
    Co-authored-by: themathqueen <23701951+themathqueen@users.noreply.github.com>

--- Mathlib/NumberTheory/Multiplicity.lean
+++ Mathlib/NumberTheory/Multiplicity.lean
@@ -257,11 +257,9 @@ theorem Int.sq_mod_four_eq_one_of_odd {x : ℤ} : Odd x → x ^ 2 % 4 = 1 := by
   decide
 
 lemma Int.eight_dvd_sq_sub_one_of_odd {k : ℤ} (hk : Odd k) : 8 ∣ k ^ 2 - 1 := by
-  rcases hk with ⟨m, hm⟩
+  rcases hk with ⟨m, rfl⟩
   have eq : (2 * m + 1) ^ 2 - 1 = 4 * m * (m + 1) := by ring
-  have : 4 * 2 ∣ 4 * (m * (m + 1)) :=
-    (Int.mul_dvd_mul_iff_left (by norm_num)).mpr (two_dvd_mul_add_one m)
-  simpa [hm, eq, mul_assoc] using this
+  simpa [eq, mul_assoc] using (Int.mul_dvd_mul_iff_left (by norm_num)).mpr (two_dvd_mul_add_one m)
 
 lemma Nat.eight_dvd_sq_sub_one_of_odd {k : ℕ} (hk : Odd k) : 8 ∣ k ^ 2 - 1 := by
   rcases hk with ⟨m, hm⟩
