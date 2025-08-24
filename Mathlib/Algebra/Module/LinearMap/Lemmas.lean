ebc8fa1be0d02f5bdf2cbdeee46a2a96a79ebbde
Monica Omar <2497250a@research.gla.ac.uk>
2025-08-24 06:38:02 +0000

    new file

---
+++ Mathlib/Algebra/Module/LinearMap/Lemmas.lean
@@ -0,0 +1,40 @@
+/-
+Copyright (c) 2025 Monica Omar. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Monica Omar
+-/
+import Mathlib.Algebra.Module.Submodule.Range
+import Mathlib.LinearAlgebra.Span.Defs
+
+/-!
+# Some lemmas about linear maps
+
+-/
+
+namespace LinearMap
+variable {R M M₁ : Type*} [AddCommMonoid M] [AddCommMonoid M₁]
+
+theorem surjective_iff_ne_zero {R M : Type*} [AddCommMonoid M]
+    [DivisionSemiring R] [Module R M] {f : M →ₗ[R] R} :
+    Function.Surjective f ↔ f ≠ 0 := by
+  refine ⟨ne_zero_of_surjective, fun hf z => ?_⟩
+  obtain ⟨y, hy⟩ : ∃ y, f y ≠ 0 := by simpa [Ne, LinearMap.ext_iff] using hf
+  use (z * (f y)⁻¹) • y
+  simp [hy]
+
+protected alias ⟨_, surjective⟩ := LinearMap.surjective_iff_ne_zero
+
+theorem range_smulRight_apply_of_surjective [Semiring R] [Module R M] [Module R M₁]
+    {f : M →ₗ[R] R} (hf : Function.Surjective f) (x : M₁) :
+    range (f.smulRight x) = Submodule.span R {x} := Submodule.ext fun z => by
+  simp_rw [LinearMap.mem_range, LinearMap.smulRight_apply, Submodule.mem_span_singleton]
+  refine ⟨fun ⟨w, hw⟩ => ⟨f w, hw ▸ rfl⟩, fun ⟨w, hw⟩ => ?_⟩
+  obtain ⟨y, rfl⟩ := hf w
+  exact ⟨y, hw⟩
+
+theorem range_smulRight_apply [DivisionSemiring R] [Module R M] [Module R M₁]
+    {f : M →ₗ[R] R} (hf : f ≠ 0) (x : M₁) :
+    range (f.smulRight x) = Submodule.span R {x} :=
+  range_smulRight_apply_of_surjective (LinearMap.surjective hf) x
+
+end LinearMap
