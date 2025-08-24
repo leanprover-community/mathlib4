7e9fbaa5c86d3b4240a91673b3d7dd0ad6d38727
mitchell-horner <29882987+mitchell-horner@users.noreply.github.com>
2025-08-25 07:20:01 +1000

    Use `Set.range`

--- Mathlib/Combinatorics/SimpleGraph/Clique.lean
+++ Mathlib/Combinatorics/SimpleGraph/Clique.lean
@@ -185,12 +185,12 @@ lemma isClique_sup_edge_of_ne_iff {v w : α} {s : Set α} (h : v ≠ w) :
     fun h' ↦ isClique_sup_edge_of_ne_sdiff h h'.1 h'.2⟩
 
 /-- The vertices in a copy of `⊤` are a clique. -/
-theorem isClique_image_copy_top (f : Copy (⊤ : SimpleGraph β) G) :
-    G.IsClique (f '' Set.univ) := by
-  intro _ ⟨_, _, h⟩ _ ⟨_, _, h'⟩ heq
-  rw [← h, show f _ = f.topEmbedding _ by rfl, ← h', show f _ = f.topEmbedding _ by rfl] at heq ⊢
+theorem isClique_range_copy_top (f : Copy (⊤ : SimpleGraph β) G) :
+    G.IsClique (Set.range f) := by
+  intro _ ⟨_, h⟩ _ ⟨_, h'⟩ nh
+  rw [← h, show f _ = f.topEmbedding _ by rfl, ← h', show f _ = f.topEmbedding _ by rfl] at nh ⊢
   rwa [← f.topEmbedding.coe_toEmbedding, (f.topEmbedding.apply_eq_iff_eq _ _).ne,
-    ← top_adj, ← f.topEmbedding.map_adj_iff] at heq
+    ← top_adj, ← f.topEmbedding.map_adj_iff] at nh
 
 end Clique
 
@@ -317,8 +317,8 @@ lemma IsNClique.erase_of_sup_edge_of_mem [DecidableEq α] {v w : α} {s : Finset
 /-- The vertices in a copy of `⊤ : SimpleGraph β` are a `card β`-clique. -/
 theorem isNClique_map_copy_top [Fintype β] (f : Copy (⊤ : SimpleGraph β) G) :
     G.IsNClique (card β) (univ.map f.toEmbedding) := by
-  rw [isNClique_iff, card_map, card_univ, coe_map, coe_univ]
-  exact ⟨isClique_image_copy_top f, rfl⟩
+  rw [isNClique_iff, card_map, card_univ, coe_map, coe_univ, Set.image_univ]
+  exact ⟨isClique_range_copy_top f, rfl⟩
 
 end NClique
 
