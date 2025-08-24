007ff5fca7881cbfd429cfe9c813205743707f77
Yi.Yuan <kysyy1@126.com>
2025-08-25 06:45:12 +0800

    Update Mathlib/NumberTheory/Multiplicity.lean
    Co-authored-by: themathqueen <23701951+themathqueen@users.noreply.github.com>

--- Mathlib/NumberTheory/Multiplicity.lean
+++ Mathlib/NumberTheory/Multiplicity.lean
@@ -262,12 +262,9 @@ lemma Int.eight_dvd_sq_sub_one_of_odd {k : ℤ} (hk : Odd k) : 8 ∣ k ^ 2 - 1 :
   simpa [eq, mul_assoc] using (Int.mul_dvd_mul_iff_left (by norm_num)).mpr (two_dvd_mul_add_one m)
 
 lemma Nat.eight_dvd_sq_sub_one_of_odd {k : ℕ} (hk : Odd k) : 8 ∣ k ^ 2 - 1 := by
-  rcases hk with ⟨m, hm⟩
-  have eq : (2 * m + 1) ^ 2 = 1 + 4 * m * (m + 1) := by ring
-  have eq : (2 * m + 1) ^ 2 - 1 = 4 * m * (m + 1) := by omega
-  have : 4 * 2 ∣ 4 * (m * (m + 1)) :=
-    (Nat.mul_dvd_mul_iff_left (by norm_num)).mpr (two_dvd_mul_add_one m)
-  simpa [hm, eq, mul_assoc] using this
+  rcases hk with ⟨m, rfl⟩
+  have eq : (2 * m + 1) ^ 2 - 1 = 4 * m * (m + 1) := by ring_nf; grind
+  simpa [eq, mul_assoc] using (mul_dvd_mul_iff_left (by norm_num)).mpr (two_dvd_mul_add_one m)
 
 theorem Int.two_pow_two_pow_add_two_pow_two_pow {x y : ℤ} (hx : ¬2 ∣ x) (hxy : 4 ∣ x - y) (i : ℕ) :
     emultiplicity 2 (x ^ 2 ^ i + y ^ 2 ^ i) = ↑(1 : ℕ) := by
