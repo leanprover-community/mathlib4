9123fe886a72ad34a60dfbfa893f012837bba17e
Yongshun Ye <ShreckYe@gmail.com>
2025-08-25 17:19:25 +0800

    Add `gcd_ne_zero_iff`s

--- Mathlib/Algebra/GCDMonoid/Multiset.lean
+++ Mathlib/Algebra/GCDMonoid/Multiset.lean
@@ -151,6 +151,10 @@ theorem gcd_eq_zero_iff (s : Multiset α) : s.gcd = 0 ↔ ∀ x : α, x ∈ s �
     intro a s sgcd h
     simp [h a (mem_cons_self a s), sgcd fun x hx ↦ h x (mem_cons_of_mem hx)]
 
+theorem gcd_ne_zero_iff (s : Multiset α) : s.gcd ≠ 0 ↔ ∃ x ∈ s, x ≠ 0 := by
+  convert not_congr (gcd_eq_zero_iff s)
+  simp
+
 theorem gcd_map_mul (a : α) (s : Multiset α) : (s.map (a * ·)).gcd = normalize a * s.gcd := by
   refine s.induction_on ?_ fun b s ih ↦ ?_
   · simp_rw [map_zero, gcd_zero, mul_zero]
