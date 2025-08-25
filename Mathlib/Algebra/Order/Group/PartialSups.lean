a40a0dc874fc76525c85e4a1e7fb909ee884abff
Lua Viana Reis <me@lua.blog.br>
2025-08-25 09:30:29 -0300

    better names

--- Mathlib/Algebra/Order/Group/PartialSups.lean
+++ Mathlib/Algebra/Order/Group/PartialSups.lean
@@ -15,9 +15,9 @@ variable {α ι : Type*}
 variable [SemilatticeSup α] [Group α] [Preorder ι] [LocallyFiniteOrderBot ι]
 
 @[to_additive]
-lemma partialSups_mulLeft [MulLeftMono α] (f : ι → α) (c : α) (i : ι) :
+lemma partialSups_const_mul [MulLeftMono α] (f : ι → α) (c : α) (i : ι) :
     partialSups (c * f ·) i = c * (partialSups f i) := map_partialSups (OrderIso.mulLeft _) ..
 
 @[to_additive]
-lemma partialSups_mulRight [MulRightMono α] (f : ι → α) (c : α) (i : ι) :
+lemma partialSups_mul_const [MulRightMono α] (f : ι → α) (c : α) (i : ι) :
     partialSups (f · * c) i = (partialSups f i) * c := map_partialSups (OrderIso.mulRight _) ..
