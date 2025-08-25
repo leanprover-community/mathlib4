039362c3565ab3e652725c6fde0eea5acf72c275
Anne C.A. Baanen <vierkantor@vierkantor.com>
2025-08-25 09:50:30 +0200

    Actually import the linters we want to run.

--- Mathlib/Init.lean
+++ Mathlib/Init.lean
@@ -20,7 +20,7 @@ import Mathlib.Tactic.Linter.UnusedTactic
 import Mathlib.Tactic.Linter.Style
 -- This import makes the `#min_imports` command available globally.
 import Mathlib.Tactic.MinImports
-import Mathlib.Tactic.TacticAnalysis
+import Mathlib.Tactic.TacticAnalysis.Declarations
 
 /-!
 This is the root file in Mathlib: it is imported by virtually *all* Mathlib files.
