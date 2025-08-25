47329a22f45632e743947e70724cf77990cecb5a
Yakov Pechersky <ffxen158@gmail.com>
2025-08-24 22:59:08 -0400

    shake

--- Mathlib/NumberTheory/Padics/WithVal.lean
+++ Mathlib/NumberTheory/Padics/WithVal.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yakov Pechersky
 -/
 import Mathlib.Analysis.RCLike.Basic
-import Mathlib.GroupTheory.ArchimedeanDensely
 import Mathlib.NumberTheory.Padics.PadicNumbers
 import Mathlib.Topology.Algebra.Valued.ValuedField
 import Mathlib.Topology.Algebra.Valued.WithVal
