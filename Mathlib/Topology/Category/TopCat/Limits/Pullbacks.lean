05ffb3056348106f6e4e991828744eb652e9826a
euprunin <euprunin@users.noreply.github.com>
2025-08-27 00:05:53 +0200

    use `rfl` instead of `by rfl`

--- Mathlib/Topology/Category/TopCat/Limits/Pullbacks.lean
+++ Mathlib/Topology/Category/TopCat/Limits/Pullbacks.lean
@@ -109,8 +109,7 @@ theorem pullbackIsoProdSubtype_hom_apply {f : X ⟶ Z} {g : Y ⟶ Z}
     (x : ↑(pullback f g)) :
     (pullbackIsoProdSubtype f g).hom x =
       ⟨⟨pullback.fst f g x, pullback.snd f g x⟩, by
-        simpa using CategoryTheory.congr_fun pullback.condition x⟩ := by
-  rfl
+        simpa using CategoryTheory.congr_fun pullback.condition x⟩ := rfl
 
 theorem pullback_topology {X Y Z : TopCat.{u}} (f : X ⟶ Z) (g : Y ⟶ Z) :
     (pullback f g).str =
