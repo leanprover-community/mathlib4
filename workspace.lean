507f99a2a3a6cfca01de8ec17e2dc903491ae78c
yuanyi-350 <kysyy1@126.com>
2025-08-26 15:51:10 +0800

    add test

--- workspace.lean
+++ workspace.lean
@@ -1,10 +1,14 @@
 import Mathlib
 
+/- Reference : https://www-users.cse.umn.edu/~arnold/502.s97/functional.pdf
+-/
+
 open scoped LinearMap InnerProduct
 
-lemma adjoint_range_ker_complement0 {α β : Type*} [NormedAddCommGroup α] [NormedAddCommGroup β]
+lemma adjoint_Invertible_of_Invertible {α β : Type*} [NormedAddCommGroup α] [NormedAddCommGroup β]
     [InnerProductSpace ℝ α] [InnerProductSpace ℝ β] [CompleteSpace α] [CompleteSpace β]
     (f : α →L[ℝ] β) (h : f.IsInvertible) : (f†).IsInvertible := by
+  -- If $S=T^{-1}: Y \rightarrow X$ exists
   rcases h with ⟨g, hg⟩
   have eq1 := ContinuousLinearMap.adjoint_comp f g.symm.toContinuousLinearMap
   nth_rw 1 [← hg] at eq1
@@ -12,15 +16,19 @@ lemma adjoint_range_ker_complement0 {α β : Type*} [NormedAddCommGroup α] [Nor
   have eq2 := ContinuousLinearMap.adjoint_comp g.symm.toContinuousLinearMap f
   nth_rw 1 [← hg] at eq2
   simp at eq2
+  -- then $S T=I_X$ and $T S=I_Y$, so $T^* S^*=I_{X^*}$ and $S^* T^*=I_{Y^*}$
+  -- which shows that $T^*$ is invertible.
   exact ContinuousLinearMap.IsInvertible.of_inverse (id (Eq.symm eq2)) (id (Eq.symm eq1))
 
-lemma adjoint_range_ker_complement1 {α β : Type*} [NormedAddCommGroup α] [NormedAddCommGroup β]
+/-- Theorem. Let $T: X \rightarrow Y$ be a bounded linear operator between Banach spaces.
+Then $T$ is invertible iff $T^*$ is. -/
+lemma adjoint_Invertible_iff_Invertible {α β : Type*} [NormedAddCommGroup α] [NormedAddCommGroup β]
     [InnerProductSpace ℝ α] [InnerProductSpace ℝ β] [CompleteSpace α] [CompleteSpace β]
     (f : α →L[ℝ] β) : f.IsInvertible ↔ (f†).IsInvertible := by
   constructor
-  · exact fun a => adjoint_range_ker_complement0 f a
+  · exact fun a => adjoint_Invertible_of_Invertible f a
   · intro h
-    have := adjoint_range_ker_complement0 (f†) h
+    have := adjoint_Invertible_of_Invertible (f†) h
     simpa using this
 
 lemma adjoint_range_ker_complement {α β : Type*} [NormedAddCommGroup α] [NormedAddCommGroup β]
