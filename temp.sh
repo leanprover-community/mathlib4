7a6d491ea3ec170e28d3b6c4f9a2e84c888a850b
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-08-26 23:24:55 +0200

    chore: squeeze a few more non-terminal simps

---
+++ temp.sh
@@ -0,0 +1,5 @@
+rm /home/michael/.cache/mathlib/ce721c76a2fcaf4b.ltar
+rm /home/michael/.cache/mathlib/c54e779eea333a65.ltar
+rm /home/michael/.cache/mathlib/d80afa4d09a85cc7.ltar
+rm /home/michael/.cache/mathlib/a45aa3c646272fe1.ltar
+rm /home/michael/.cache/mathlib/6629ab97f222c228.ltar
