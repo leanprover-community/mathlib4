3e4c8f57556da3191d3b9e123f201aa299276188
euprunin <euprunin@users.noreply.github.com>
2025-08-26 01:11:36 +0000

    chore(Algebra/Order/Archimedean): remove redundant `have` in `exists_div_btwn` (#28924)

--- Mathlib/Algebra/Order/Archimedean/Basic.lean
+++ Mathlib/Algebra/Order/Archimedean/Basic.lean
@@ -412,7 +412,6 @@ theorem exists_div_btwn {x y : K} {n : ℕ} (h : x < y) (nh : (y - x)⁻¹ < n) 
   obtain ⟨z, zh⟩ := exists_floor (x * n)
   refine ⟨z + 1, ?_⟩
   have n0' := (inv_pos.2 (sub_pos.2 h)).trans nh
-  have n0 := Nat.cast_pos.1 n0'
   rw [div_lt_iff₀ n0']
   refine ⟨(lt_div_iff₀ n0').2 <| (lt_iff_lt_of_le_iff_le (zh _)).1 (lt_add_one _), ?_⟩
   rw [Int.cast_add, Int.cast_one]
