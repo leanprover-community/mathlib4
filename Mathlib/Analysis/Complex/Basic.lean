a2c9b52c4bbdac1cbc9e710a95659063513855a0
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-08-26 16:43:07 +0200

    fix

--- Mathlib/Analysis/Complex/Basic.lean
+++ Mathlib/Analysis/Complex/Basic.lean
@@ -3,7 +3,7 @@ Copyright (c) 2019 Sébastien Gouëzel. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Sébastien Gouëzel
 -/
-import Mathlib.Data.Analysis.Complex.Order
+import Mathlib.Analysis.Complex.Order
 import Mathlib.Analysis.RCLike.Basic
 import Mathlib.Data.Complex.BigOperators
 import Mathlib.Data.Complex.Module
