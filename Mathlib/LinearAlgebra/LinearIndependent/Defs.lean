b97d95a0ba48e55845c740f694776399a2b4ec0a
staroperator <staroperator92@gmail.com>
2025-08-26 19:27:11 -0400

    golf another `sum_congr`

--- Mathlib/LinearAlgebra/LinearIndependent/Defs.lean
+++ Mathlib/LinearAlgebra/LinearIndependent/Defs.lean
@@ -693,7 +693,7 @@ nonrec theorem Fintype.linearIndependent_iffₒₛ [DecidableEq ι] [Fintype ι]
     · exact h.2 i (Finset.mem_compl.2 hi)
   · specialize h t₁ (fun i => if i ∈ t₁ ∨ i ∈ t₂ then f i else 0) ?_
     · rw [← Finset.sum_subset (ht₁t₂.le_compl_left)]
-      · convert heq using 1 <;> refine Finset.sum_congr rfl fun i hi => ?_ <;> simp [hi]
+      · convert heq using 2 with i hi i hi <;> simp [hi]
       · intro i hi hi'
         simp [Finset.mem_compl.1 hi, hi']
     refine ⟨fun i hi => ?_, fun i hi => ?_⟩ <;> simpa [hi] using h i
