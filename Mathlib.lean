a7bd722e53b63a40c857087c659fbc26caf23657
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2025-08-26 21:44:31 +0000

    feat: add ConjRootClass (#18693)
    Co-authored-by: FR <zhao.yu-yang@foxmail.com>
    Co-authored-by: LessnessRandomness <lessnessr@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -3708,6 +3708,7 @@ import Mathlib.FieldTheory.KummerPolynomial
 import Mathlib.FieldTheory.Laurent
 import Mathlib.FieldTheory.LinearDisjoint
 import Mathlib.FieldTheory.Minpoly.Basic
+import Mathlib.FieldTheory.Minpoly.ConjRootClass
 import Mathlib.FieldTheory.Minpoly.Field
 import Mathlib.FieldTheory.Minpoly.IsConjRoot
 import Mathlib.FieldTheory.Minpoly.IsIntegrallyClosed
