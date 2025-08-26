ba7c31c8171e4479997771d4b1e932ce6d211282
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-08-26 22:17:54 +0200

    chore: disable the linter in a few instances

--- Mathlib/Order/Interval/Set/LinearOrder.lean
+++ Mathlib/Order/Interval/Set/LinearOrder.lean
@@ -512,6 +512,8 @@ theorem Ioc_union_Ioc' (h₁ : c ≤ b) (h₂ : a ≤ d) : Ioc a b ∪ Ioc c d =
     tauto
   · tauto
 
+-- TODO: fix a good way to fix the linter; the exact only applies to one of five goals
+set_option linter.flexible false in
 theorem Ioc_union_Ioc (h₁ : min a b ≤ max c d) (h₂ : min c d ≤ max a b) :
     Ioc a b ∪ Ioc c d = Ioc (min a c) (max b d) := by
   rcases le_total a b with hab | hab <;> rcases le_total c d with hcd | hcd <;> simp [*] at h₁ h₂
