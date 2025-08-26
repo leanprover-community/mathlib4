e4d719af9ecf6a16c1d2fe542cd151c79ed3ffc1
Wrenna Robson <wren.robson@gmail.com>
2025-08-26 10:44:11 +0100

    Fix changed name

--- Mathlib/Order/Hom/Order.lean
+++ Mathlib/Order/Hom/Order.lean
@@ -51,8 +51,8 @@ instance [SemilatticeInf β] : Min (α →o β) where
     ((f ⊓ g : α →o β) : α → β) = (f : α → β) ⊓ g := rfl
 
 instance [SemilatticeInf β] : SemilatticeInf (α →o β) :=
-  { (_ : PartialOrder (α →o β)), (dualIso α β).symm.toGaloisInsertion.liftSemilatticeInf with
-    inf := (· ⊓ ·) }
+  { (_ : PartialOrder (α →o β)),
+    (OrderIso.dualHom α β).symm.toGaloisInsertion.liftSemilatticeInf with inf := (· ⊓ ·) }
 
 instance lattice [Lattice β] : Lattice (α →o β) :=
   { (_ : SemilatticeSup (α →o β)), (_ : SemilatticeInf (α →o β)) with }
