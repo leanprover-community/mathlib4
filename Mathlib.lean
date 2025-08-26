3e9837218519c3268d78fc08b6b406624790a4a5
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-08-26 22:49:11 +0200

    chore: move Data/Real/Irrational to RingTheory
    It certainly has nothing to do with data (structure).

--- Mathlib.lean
+++ Mathlib.lean
@@ -3508,7 +3508,6 @@ import Mathlib.Data.Real.ENatENNReal
 import Mathlib.Data.Real.EReal
 import Mathlib.Data.Real.Embedding
 import Mathlib.Data.Real.GoldenRatio
-import Mathlib.Data.Real.Irrational
 import Mathlib.Data.Real.Pointwise
 import Mathlib.Data.Real.Sign
 import Mathlib.Data.Real.Sqrt
@@ -5662,6 +5661,7 @@ import Mathlib.RingTheory.PrincipalIdealDomain
 import Mathlib.RingTheory.PrincipalIdealDomainOfPrime
 import Mathlib.RingTheory.QuotSMulTop
 import Mathlib.RingTheory.Radical
+import Mathlib.RingTheory.Real.Irrational
 import Mathlib.RingTheory.ReesAlgebra
 import Mathlib.RingTheory.Regular.Category
 import Mathlib.RingTheory.Regular.Depth
