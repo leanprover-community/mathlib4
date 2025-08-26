d173d3fd56c6009850e02414e20127af87f02afb
Paul Lezeau <lezeau@google.com>
2025-08-26 13:03:45 +0000

    Use where instead of constructor

--- Mathlib/Algebra/CharZero/AddMonoidHom.lean
+++ Mathlib/Algebra/CharZero/AddMonoidHom.lean
@@ -12,7 +12,6 @@ This file exists in order to avoid adding extra imports to other files in this s
 -/
 
 theorem CharZero.of_addMonoidHom {M N : Type*} [AddCommMonoidWithOne M] [AddCommMonoidWithOne N]
-    [CharZero M] {e : M →+ N} (he : e 1 = 1) (he' : Function.Injective e) : CharZero N := by
-  constructor
-  intro n m h
-  rwa [← map_natCast' _ he, ← map_natCast' _ he, he'.eq_iff, Nat.cast_inj] at h
+    [CharZero M] {e : M →+ N} (he : e 1 = 1) (he' : Function.Injective e) : CharZero N where
+  cast_injective n m h := by
+    rwa [← map_natCast' _ he, ← map_natCast' _ he, he'.eq_iff, Nat.cast_inj] at h
