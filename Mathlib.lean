22a4a61b9be77c7794d0733097cc9a53569f8c36
Vasilii Nesterov <vas.nesterov63@gmail.com>
2025-08-26 16:47:35 +0300

    mk_all

--- Mathlib.lean
+++ Mathlib.lean
@@ -3518,6 +3518,7 @@ import Mathlib.Data.Semiquot
 import Mathlib.Data.Seq.Basic
 import Mathlib.Data.Seq.Computation
 import Mathlib.Data.Seq.Parallel
+import Mathlib.Data.Seq.Seq
 import Mathlib.Data.Seq.WSeq
 import Mathlib.Data.Set.Accumulate
 import Mathlib.Data.Set.Basic
