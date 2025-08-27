1720a76b8bfde53abbe1abe7977a2a258406d19f
Yongshun Ye <ShreckYe@gmail.com>
2025-08-27 14:42:33 +0800

    WIP

--- GetScanlEqFoldlTakeExperiments.lean
+++ GetScanlEqFoldlTakeExperiments.lean
@@ -2,4 +2,9 @@ import Mathlib
 
 theorem get_scanl_eq_foldl_take {α} {f} {a : α} (l : List α) (i : Fin (l.length + 1)) :
     (l.scanl f a).get (i.cast (l.length_scanl a).symm) = (l.take i).foldl f a := by
-  sorry
+  induction i using Fin.induction
+  case zero => simp
+  case succ i ih =>
+    simp at ih ⊢
+
+    sorry
