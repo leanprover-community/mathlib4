79bf50e229e572d1719202abf803758f95032560
euprunin <178733547+euprunin@users.noreply.github.com>
2025-08-27 18:24:52 +0000

    chore(Data/NNReal): golf entire `sSup_of_not_bddAbove` using `grind` (#28843)
    Co-authored-by: euprunin <euprunin@users.noreply.github.com>

--- Mathlib/Data/NNReal/Defs.lean
+++ Mathlib/Data/NNReal/Defs.lean
@@ -817,9 +817,7 @@ theorem le_toNNReal_of_coe_le {x : ℝ≥0} {y : ℝ} (h : ↑x ≤ y) : x ≤ y
   (le_toNNReal_iff_coe_le <| x.2.trans h).2 h
 
 nonrec theorem sSup_of_not_bddAbove {s : Set ℝ≥0} (hs : ¬BddAbove s) : SupSet.sSup s = 0 := by
-  rw [← bddAbove_coe] at hs
-  rw [← coe_inj, coe_sSup, NNReal.coe_zero]
-  exact sSup_of_not_bddAbove hs
+  grind [csSup_of_not_bddAbove, csSup_empty, bot_eq_zero']
 
 theorem iSup_of_not_bddAbove (hf : ¬BddAbove (range f)) : ⨆ i, f i = 0 :=
   sSup_of_not_bddAbove hf
