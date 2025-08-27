25d4dee93a9d90d534734b63238da477f030548f
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2025-08-27 18:56:14 +0800

    Update GlobalDimension.lean

--- Mathlib/RingTheory/RegularLocalRing/GlobalDimension.lean
+++ Mathlib/RingTheory/RegularLocalRing/GlobalDimension.lean
@@ -5,23 +5,33 @@ Authors: Nailin Guan
 -/
 import Mathlib.RingTheory.RegularLocalRing.Basic
 import Mathlib.RingTheory.GlobalDimension
-
+import Mathlib.RingTheory.CohenMacaulay.Maximal
 /-!
 
 # Global Dimension of Regular Local Ring is equal to Krull Dimension
 
 -/
 
-universe u
+--set_option pp.universes true
+
+universe u v
 
 variable (R : Type u) [CommRing R]
 
-open IsLocalRing
+open IsLocalRing CategoryTheory
 
-lemma projectiveDimension_residueField_eq_ringKrullDim [IsRegularLocalRing R] :
-    projectiveDimension (ModuleCat.of R (ResidueField R)) = ringKrullDim R := by
+lemma finite_projectiveDimension_of_isRegularLocalRing [IsRegularLocalRing R]
+    (M : ModuleCat.{v} R) [Module.Finite R M] : ∃ n, HasProjectiveDimensionLE M n := by
   sorry
 
+/- have some universe problem
+lemma projectiveDimension_residueField_eq_ringKrullDim [IsRegularLocalRing R] :
+    projectiveDimension (ModuleCat.of R (ResidueField R)) = ringKrullDim R := by
+  --follows from AB thm and above easily
+  sorry
+-/
+
 theorem globalDimension_eq_ringKrullDim [IsRegularLocalRing R] :
-    globalDimension R = ringKrullDim R := by
+    globalDimension.{u, v} R = ringKrullDim R := by
+
   sorry
