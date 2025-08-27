493f388665cd4f4933d4649dfd205cc266822bb0
P. Michael Kielstra <4313413+PMKielstra@users.noreply.github.com>
2025-08-27 15:53:58 -0700

    Remove tactic folder import

--- Mathlib/MeasureTheory/Integral/IntervalIntegral/TrapezoidalRule.lean
+++ Mathlib/MeasureTheory/Integral/IntervalIntegral/TrapezoidalRule.lean
@@ -3,7 +3,6 @@ Copyright (c) 2025 P. Michael Kielstra. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: P. Michael Kielstra
 -/
-import Mathlib.Tactic
 import Mathlib.Analysis.SpecialFunctions.Integrals.Basic
 
 /-!
