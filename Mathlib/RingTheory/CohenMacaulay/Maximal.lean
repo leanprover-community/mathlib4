c58a268a5147f6fafafaba8e4c395c17e3fb3111
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2025-08-27 18:46:15 +0800

    Update Maximal.lean

--- Mathlib/RingTheory/CohenMacaulay/Maximal.lean
+++ Mathlib/RingTheory/CohenMacaulay/Maximal.lean
@@ -4,16 +4,26 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Nailin Guan, Yongle Hu
 -/
 import Mathlib.RingTheory.CohenMacaulay.Basic
+import Mathlib.RingTheory.RegularLocalRing.Basic
 
 /-!
 
 # Maximal Cohen Macaulay Module
+
+The definition of maximal Cohen Macaulay module.
+
+# Main definition and results
+
+* `free_of_isMaximalCohenMacaulay_of_isRegularLocalRing` : maximal Cohen Macaulay module over
+  regular local ring is free.
 
 -/
 
 universe v' v u' u
 
 variable {R : Type u} [CommRing R]
+
+open IsLocalRing
 
 class ModuleCat.IsMaximalCohenMacaulay [IsLocalRing R] [Small.{v} R]
     (M : ModuleCat.{v} R) : Prop where
@@ -38,3 +48,31 @@ lemma isCohenMacaulay_of_isMaximalCohenMacaulay [IsNoetherianRing R] [IsLocalRin
     right
     apply le_antisymm _ (depth_le_supportDim M)
     simpa [(isMaximalCohenMacaulay_def M).mp ‹_›] using Module.supportDim_le_ringKrullDim R M
+
+lemma isCohenMacaulayLocalRing_of_isRegularLocalRing [IsRegularLocalRing R] :
+    IsCohenMacaulayLocalRing R := by
+  apply isCohenMacaulayLocalRing_of_ringKrullDim_le_depth
+  rw [depth_eq_sSup_length_regular]
+  let fg' : (maximalIdeal R).FG := (isNoetherianRing_iff_ideal_fg R).mp inferInstance _
+  let _ : Fintype (maximalIdeal R).generators := (Submodule.FG.finite_generators fg').fintype
+  have : ringKrullDim R = ((maximalIdeal R).generators.toFinset.card : ℕ∞) := by
+    rw [← (isRegularLocalRing_def R).mp ‹_›, ← Set.ncard_eq_toFinset_card',
+      Submodule.FG.generators_ncard fg']
+    rfl
+  simp only [this, WithBot.coe_le_coe]
+  apply le_sSup
+  use (maximalIdeal R).generators.toFinset.toList
+  simp only [Finset.length_toList, Set.toFinset_card, Set.coe_fintypeCard, Finset.mem_toList,
+    Set.mem_toFinset, exists_prop, and_true]
+  refine ⟨?_, fun r hr ↦ Submodule.FG.generators_mem (maximalIdeal R) hr⟩
+  apply isRegular_of_span_eq_maximalIdeal
+  · simpa [Ideal.ofList] using (maximalIdeal R).span_generators
+  · rw [Finset.length_toList, this]
+    rfl
+
+theorem free_of_isMaximalCohenMacaulay_of_isRegularLocalRing [IsRegularLocalRing R] [Small.{v} R]
+    (M : ModuleCat.{v} R) [Module.Finite R M] [M.IsMaximalCohenMacaulay] : Module.Free R M := by
+  rcases exist_nat_eq R with ⟨n, hn⟩
+  induction' n with n ih generalizing R M
+  · sorry
+  · sorry
