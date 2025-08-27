a7d2c5fbd342cf25d23d8aaae5e1b2ba3596a7bd
Yongshun Ye <ShreckYe@gmail.com>
2025-08-27 12:56:00 +0800

    Remove an unnecessary import
    Co-authored-by: Matthew Robert Ballard <100034030+mattrobball@users.noreply.github.com>

--- Mathlib/Algebra/Order/Floor/Ring.lean
+++ Mathlib/Algebra/Order/Floor/Ring.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro, Kevin Kappelmann
 -/
 import Mathlib.Algebra.Order.Field.Basic
-import Mathlib.Algebra.Order.Floor.Defs
 import Mathlib.Algebra.Order.Floor.Semiring
 import Mathlib.Tactic.Abel
 import Mathlib.Tactic.FieldSimp
