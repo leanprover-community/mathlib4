69f321bc06c93b9cd380d7d2b136bfe8488de86e
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2025-08-27 09:59:37 +0200

    restore necessary tags

--- Mathlib/Data/Set/Basic.lean
+++ Mathlib/Data/Set/Basic.lean
@@ -485,20 +485,34 @@ instance uniqueEmpty [IsEmpty α] : Unique (Set α) where
   uniq := eq_empty_of_isEmpty
 
 /-- See also `Set.nonempty_iff_ne_empty`. -/
-theorem not_nonempty_iff_eq_empty {s : Set α} : ¬s.Nonempty ↔ s = ∅ := by
+@[push]
+theorem not_nonempty_iff_eq_empty : ¬s.Nonempty ↔ s = ∅ := by
   simp only [Set.Nonempty, not_exists, eq_empty_iff_forall_notMem]
 
 /-- See also `Set.not_nonempty_iff_eq_empty`. -/
+@[push ←]
 theorem nonempty_iff_ne_empty : s.Nonempty ↔ s ≠ ∅ :=
   not_nonempty_iff_eq_empty.not_right
 
+/-- Variant of `nonempty_iff_ne_empty` used by `push_neg`. -/
+@[push ←]
+theorem nonempty_iff_empty_ne : s.Nonempty ↔ ∅ ≠ s :=
+  nonempty_iff_ne_empty.trans ne_comm
+
 /-- See also `nonempty_iff_ne_empty'`. -/
+@[push]
 theorem not_nonempty_iff_eq_empty' : ¬Nonempty s ↔ s = ∅ := by
   rw [nonempty_subtype, not_exists, eq_empty_iff_forall_notMem]
 
 /-- See also `not_nonempty_iff_eq_empty'`. -/
+@[push ←]
 theorem nonempty_iff_ne_empty' : Nonempty s ↔ s ≠ ∅ :=
   not_nonempty_iff_eq_empty'.not_right
+
+/-- Variant of `nonempty_iff_ne_empty'` used by `push_neg`. -/
+@[push ←]
+theorem nonempty_iff_empty_ne' : Nonempty s ↔ ∅ ≠ s :=
+  nonempty_iff_ne_empty'.trans ne_comm
 
 alias ⟨Nonempty.ne_empty, _⟩ := nonempty_iff_ne_empty
 
