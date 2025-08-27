c730baf0cc982b60a288586ca002cf3f729449e2
Chris Birkbeck <c.birkbeck@uea.ac.uk>
2025-08-27 15:46:08 +0100

    fix

--- Mathlib/NumberTheory/ModularForms/DedekindEta.lean
+++ Mathlib/NumberTheory/ModularForms/DedekindEta.lean
@@ -9,7 +9,6 @@ import Mathlib.Analysis.CStarAlgebra.Classes
 import Mathlib.Analysis.Complex.LocallyUniformLimit
 import Mathlib.Analysis.Complex.UpperHalfPlane.Exp
 import Mathlib.Analysis.NormedSpace.MultipliableUniformlyOn
-import Mathlib.Data.Complex.FiniteDimensional
 import Mathlib.Analysis.Calculus.LogDerivUniformlyOn
 import Mathlib.NumberTheory.ModularForms.EisensteinSeries.E2
 import Mathlib.NumberTheory.TsumDivsorsAntidiagonal
