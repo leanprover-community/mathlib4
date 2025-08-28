b35d7f1455c1a604a7361143021678ff0ba9da4c
Yury Kudryashov <162619279+yury-harmonic@users.noreply.github.com>
2025-08-28 15:48:10 +0000

    feat(NormNum): add extension for `Nat.ModEq`, `Int.ModEq` (#29044)
    Co-authored-by: Yury Kudryashov <yury@harmonic.fun>

--- Mathlib/Tactic.lean
+++ Mathlib/Tactic.lean
@@ -195,6 +195,7 @@ import Mathlib.Tactic.NormNum.Inv
 import Mathlib.Tactic.NormNum.Irrational
 import Mathlib.Tactic.NormNum.IsCoprime
 import Mathlib.Tactic.NormNum.LegendreSymbol
+import Mathlib.Tactic.NormNum.ModEq
 import Mathlib.Tactic.NormNum.NatFactorial
 import Mathlib.Tactic.NormNum.NatFib
 import Mathlib.Tactic.NormNum.NatLog
