55a4c42d4483b1fa7c5f8909648483b945fdc6a3
Yongshun Ye <ShreckYe@gmail.com>
2025-08-28 18:11:15 +0800

    Inline more

--- Mathlib/Data/Nat/Prime/Defs.lean
+++ Mathlib/Data/Nat/Prime/Defs.lean
@@ -125,9 +125,7 @@ theorem prime_def_le_sqrt {p : ℕ} : Prime p ↔ 2 ≤ p ∧ ∀ m, 2 ≤ m →
         rcases le_sqrt_of_eq_mul e with hm | hk
         · exact a m m2 hm mdvd
         · rw [mul_comm] at e
-          have : 1 < k := by
-            exact Nat.lt_of_mul_lt_mul_right (a := m) ((one_mul m).symm ▸ e ▸ l)
-          exact a k this hk ⟨m, e⟩⟩
+          exact a k (Nat.lt_of_mul_lt_mul_right (a := m) ((one_mul m).symm ▸ e ▸ l)) hk ⟨m, e⟩⟩
 
 theorem prime_iff_not_exists_mul_eq {p : ℕ} :
     p.Prime ↔ 2 ≤ p ∧ ¬ ∃ m n, m < p ∧ n < p ∧ m * n = p := by
