166d8e869980c350c9b21da0e86b23250990b1a2
Yongshun Ye <ShreckYe@gmail.com>
2025-08-28 16:40:05 +0800

    Inline and remove `lt_mul_of_lt_mul_right` using Copilot

--- Mathlib/Data/Nat/Sqrt.lean
+++ Mathlib/Data/Nat/Sqrt.lean
@@ -205,11 +205,9 @@ lemma le_sqrt_of_eq_mul {a b c : ℕ} (h : a = b * c) : b ≤ a.sqrt ∨ c ≤ a
   if hle : b ≤ a.sqrt then
     Or.inl hle
   else
-    have lt_mul_of_lt_mul_right {a b c d : ℕ}
-        (h : a < b * c) (hle : b ≤ d) : a < d * c := by
-      exact lt_of_lt_of_le h (mul_le_mul_right c hle)
-    Or.inr (Nat.le_of_lt_succ (Nat.lt_of_mul_lt_mul_left (lt_mul_of_lt_mul_right
-      (h ▸ Nat.lt_succ_sqrt a) (Nat.gt_of_not_le hle))))
+    Or.inr (Nat.le_of_lt_succ (Nat.lt_of_mul_lt_mul_left (
+      lt_of_lt_of_le (h ▸ Nat.lt_succ_sqrt a)
+        (mul_le_mul_right (a.sqrt + 1) (Nat.gt_of_not_le hle)))))
     -- or : `by right; nlinarith [Nat.lt_succ_sqrt' a]`
 
 end Nat
