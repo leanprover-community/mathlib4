e336914ddbd367ade52de652359e51a3b4414fff
Yongshun Ye <ShreckYe@gmail.com>
2025-08-28 15:15:07 +0800

    Follow Mathlib's naming conventions

--- Mathlib/LeSqrtOfEqMulTest.lean
+++ Mathlib/LeSqrtOfEqMulTest.lean
@@ -1,10 +1,10 @@
 import Mathlib
 
-lemma le_sqrt_of_eq_mul {a b c : ℕ} (a_eq_b_mul_c : a = b * c) : b ≤ a.sqrt ∨ c ≤ a.sqrt := by
-  by_cases w_b_le_sqrt : b ≤ a.sqrt
-  . exact Or.inl w_b_le_sqrt
-  . right
-    simp at w_b_le_sqrt
+lemma le_sqrt_of_eq_mul {a b c : ℕ} (h : a = b * c) : b ≤ a.sqrt ∨ c ≤ a.sqrt := by
+  by_cases hle : b ≤ a.sqrt
+  · exact Or.inl hle
+  · right
+    simp at hle
     have : (a.sqrt + 1) ^ 2 > a := by
       exact Nat.lt_succ_sqrt' a
     nlinarith
