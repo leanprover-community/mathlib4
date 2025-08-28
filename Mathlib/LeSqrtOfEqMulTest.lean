86ed188c5b644e6a0dc1f1223d4db6c886dba9fe
Yongshun Ye <ShreckYe@gmail.com>
2025-08-28 15:55:22 +0800

    Golf by inlining

--- Mathlib/LeSqrtOfEqMulTest.lean
+++ Mathlib/LeSqrtOfEqMulTest.lean
@@ -1,17 +1,9 @@
 import Mathlib
 
-lemma le_sqrt_of_eq_mul {a b c : ℕ} (h : a = b * c) : b ≤ a.sqrt ∨ c ≤ a.sqrt := by
-  by_cases hle : b ≤ a.sqrt
-  · exact Or.inl hle
-  · right
-    replace hle : a.sqrt < b := by
-      exact Nat.gt_of_not_le hle
-    have := Nat.lt_succ_sqrt a
-    have : b * c < a.sqrt.succ * a.sqrt.succ := by
-      exact lt_of_eq_of_lt h.symm this
-    have : b * c < b * a.sqrt.succ := by
-      exact lt_mul_of_lt_mul_right this hle
-    have : c < a.sqrt.succ := by
-      exact Nat.lt_of_mul_lt_mul_left this
-    exact Nat.le_of_lt_succ this
-    -- or : `nlinarith [Nat.lt_succ_sqrt' a]`
+lemma le_sqrt_of_eq_mul {a b c : ℕ} (h : a = b * c) : b ≤ a.sqrt ∨ c ≤ a.sqrt :=
+  if hle : b ≤ a.sqrt then
+    Or.inl hle
+  else
+    Or.inr (Nat.le_of_lt_succ (Nat.lt_of_mul_lt_mul_left (lt_mul_of_lt_mul_right
+      (lt_of_eq_of_lt h.symm (Nat.lt_succ_sqrt a)) (Nat.gt_of_not_le hle))))
+    -- or : `by right; nlinarith [Nat.lt_succ_sqrt' a]`
