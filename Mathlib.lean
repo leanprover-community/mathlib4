4dab7d05e3df18016caf68e0d857564b54a2fe04
Christian Merten <136261474+chrisflav@users.noreply.github.com>
2025-08-28 14:45:25 +0000

    feat(AlgebraicGeometry): directed covers (#24528)
    Directed covers are covers, where every intersection can be covered by a component of the cover. For open covers this is equivalent to the images forming a basis of the topology.
    
    If a cover is directed, the compatibility conditions for gluing become easier, because only compatibility with the transition maps needs to be checked. In particular, the covered scheme naturally is the colimit of the components
    of the cover.
    
    Co-authored-by: Christian Merten <christian@merten.dev>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1176,6 +1176,7 @@ import Mathlib.Algebra.Vertex.VertexOperator
 import Mathlib.AlgebraicGeometry.AffineScheme
 import Mathlib.AlgebraicGeometry.AffineSpace
 import Mathlib.AlgebraicGeometry.AffineTransitionLimit
+import Mathlib.AlgebraicGeometry.Cover.Directed
 import Mathlib.AlgebraicGeometry.Cover.MorphismProperty
 import Mathlib.AlgebraicGeometry.Cover.Open
 import Mathlib.AlgebraicGeometry.Cover.Over
