76ce0ff10cfe2de11022764c14a1443d250952ea
Paul Lezeau <paul.lezeau@gmail.com>
2025-08-28 17:17:33 +0100

    Update scripts/print-style-errors.sh
    Co-authored-by: Bryan Gin-ge Chen <bryangingechen@gmail.com>

--- scripts/print-style-errors.sh
+++ scripts/print-style-errors.sh
@@ -9,7 +9,7 @@ IFS=$'\n\t'
 # once the Python style linters have been rewritten in Lean.
 # Humans should never run this directly, but at most through `lean exe lint-style --fix`
 
-# use C scope so that sorting is the same on macOS and Linux
+# use C locale so that sorting is the same on macOS and Linux
 # see https://unix.stackexchange.com/questions/362728/why-does-gnu-sort-sort-differently-on-my-osx-machine-and-linux-machine
 find Mathlib -name '*.lean' -print0 | xargs --null ./scripts/lint-style.py "$@" | LC_ALL=C sort || true
 find Archive -name '*.lean' -print0 | xargs --null ./scripts/lint-style.py "$@" | LC_ALL=C sort || true
