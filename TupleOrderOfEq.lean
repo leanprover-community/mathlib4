3aebfbee05a9e8a98cd054df6f8d9c086d4d9d61
Yongshun Ye <ShreckYe@gmail.com>
2025-08-29 16:48:23 +0800

    Remove a redundant space

--- TupleOrderOfEq.lean
+++ TupleOrderOfEq.lean
@@ -9,6 +9,6 @@ variable [∀ i, Monoid (α i)]
 #check Prod.orderOf_mk
 
 @[to_additive]
-lemma orderOf_eq (a : (i : Fin n) → α i)  : orderOf a = Finset.univ.lcm (fun i => orderOf (a i)) :=
+lemma orderOf_eq (a : (i : Fin n) → α i) : orderOf a = Finset.univ.lcm (fun i => orderOf (a i)) :=
   sorry
 end Tuple
