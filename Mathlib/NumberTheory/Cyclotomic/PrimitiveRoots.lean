3253f79de014f7433e452917d68bdd5b133d132b
Paul Lezeau <72892199+Paul-Lez@users.noreply.github.com>
2025-08-29 02:42:26 +0000

    feat(Algebra/Squarefree/Basic): add lemma about indexed products of squarefree elements (#29051)
    This PR also adds an import to `Algebra.Squarefree.Basic`.
    
    As a drive-by, I've also simplified two proofs in `Data.Nat.Squarefree`.
    
    Upstreamed from [formal-conjectures](https://github.com/google-deepmind/formal-conjectures)
    
    Co-authored-by: Paul Lezeau <lezeau@google.com>
    Co-authored-by: Paul Lezeau <paul.lezeau@gmail.com>

--- Mathlib/NumberTheory/Cyclotomic/PrimitiveRoots.lean
+++ Mathlib/NumberTheory/Cyclotomic/PrimitiveRoots.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Alex J. Best, Riccardo Brasca, Eric Rodriguez
 -/
 import Mathlib.Data.Nat.Factorization.LCM
+import Mathlib.Data.Nat.Factorization.PrimePow
 import Mathlib.Data.PNat.Prime
 import Mathlib.NumberTheory.Cyclotomic.Basic
 import Mathlib.RingTheory.Adjoin.PowerBasis
