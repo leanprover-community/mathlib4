acc1ea851dc108c6907cc06d19987ff82ea0b886
grunweg <10105016+grunweg@users.noreply.github.com>
2025-08-29 02:10:05 +0000

    chore: minimize `fun_prop` imports (#29078)
    This is both general import hygiene with the usual benefits (parallelism, potentially fewer rebuilds, etc.),
    and has the practical benefit of enabling direct tagging of `Function.Injective` and friends with fun_prop.
    Previously, this was impossible due to import cycles.
    
    Zulip discussion: https://leanprover.zulipchat.com/#narrow/channel/144837-PR-reviews/topic/.2328191.20tag.20Injective.2C.20Surjective.2C.20and.20Bijective.20with.20fun_pro/with/536662776
    
    Co-authored-by: Michael Rothgang <rothgang@math.uni-bonn.de>

--- Mathlib/Topology/ContinuousMap/Defs.lean
+++ Mathlib/Topology/ContinuousMap/Defs.lean
@@ -3,6 +3,7 @@ Copyright (c) 2020 Nicolò Cavalleri. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Nicolò Cavalleri, Yury Kudryashov
 -/
+import Mathlib.Data.FunLike.Basic
 import Mathlib.Tactic.Continuity
 import Mathlib.Tactic.Lift
 import Mathlib.Topology.Defs.Basic
