7772f879f8fd116d35ae3ab98e6f94df8d9028b0
Yongshun Ye <ShreckYe@gmail.com>
2025-08-31 16:13:09 +0800

    Golf the proof

--- Mathlib/Dynamics/PeriodicPts/Defs.lean
+++ Mathlib/Dynamics/PeriodicPts/Defs.lean
@@ -532,10 +532,8 @@ variable {ι : Type*} {α : ι → Type*} {f : ∀ i, α i → α i} {x : ∀ i,
 
 -- TODO: make `x` explicit similar to `isFixedPt_prodMap`?
 @[simp]
-theorem isFixedPt_piMap : IsFixedPt (Pi.map f) x ↔ ∀ i, IsFixedPt (f i) (x i) := by
-  simp [IsFixedPt]
-  --unfold Pi.map
-  exact funext_iff
+theorem isFixedPt_piMap : IsFixedPt (Pi.map f) x ↔ ∀ i, IsFixedPt (f i) (x i) :=
+  funext_iff
 
 theorem IsFixedPt.piMap (h : ∀ i, IsFixedPt (f i) (x i)) : IsFixedPt (Pi.map f) x :=
   isFixedPt_piMap.mpr h
