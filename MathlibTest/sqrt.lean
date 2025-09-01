9ba3ca3a4e2ceb61c7f419967bd6f9230fa3730c
Kim Morrison <kim@tqft.net>
2025-09-01 11:05:50 +1000

    cleanup

--- MathlibTest/sqrt.lean
+++ MathlibTest/sqrt.lean
@@ -6,44 +6,75 @@ import Mathlib
 
 namespace Nat
 
+/--
+Approximate `√x` as a rational number, to within `1/prec`.
+-/
 def ratSqrt (x : ℕ) (prec : ℕ) : ℚ := ((x * prec^2).sqrt : ℚ) / prec
 
-theorem ratSqrt_sq_le {x prec : ℕ} : (ratSqrt x prec)^2 ≤ x := sorry
-theorem lt_ratSqrt_add_inv_prec_eq {x prec : ℕ} : x < (ratSqrt x prec + 1 / prec)^2 := sorry
+theorem ratSqrt_nonneg {x prec : ℕ} (h : 0 < prec) : 0 ≤ ratSqrt x prec := by
+  unfold ratSqrt
+  positivity
 
-theorem ratSqrt_le_realSqrt {x prec : ℕ} : ratSqrt x prec ≤ √x := sorry
-theorem realSqrt_lt_ratSqrt_add_inv_prec {x prec : ℕ} : √x < ratSqrt x prec + 1 / prec := sorry
+theorem ratSqrt_sq_le {x prec : ℕ} (h : 0 < prec) : (ratSqrt x prec)^2 ≤ x := by
+  unfold ratSqrt
+  rw [div_pow, div_le_iff₀ (by positivity)]
+  norm_cast
+  exact sqrt_le' (x * prec ^ 2)
+
+theorem lt_ratSqrt_add_inv_prec_eq {x prec : ℕ} (h : 0 < prec) : x < (ratSqrt x prec + 1 / prec)^2 := by
+  unfold ratSqrt
+  rw [← mul_lt_mul_iff_of_pos_right (a := (prec^2 : ℚ)) (by positivity)]
+  rw [← mul_pow, add_mul]
+  rw [div_mul_cancel₀, div_mul_cancel₀]
+  · norm_cast
+    exact lt_succ_sqrt' (x * prec ^ 2)
+  all_goals norm_cast; positivity
+
+theorem ratSqrt_le_realSqrt {x prec : ℕ} (h : 0 < prec) : ratSqrt x prec ≤ √x := by
+  have := ratSqrt_sq_le (x := x) h
+  have : (x.ratSqrt prec ^ 2 : ℝ) ≤ ↑x := by norm_cast
+  have := Real.sqrt_monotone this
+  rwa [Real.sqrt_sq] at this
+  simpa only [Rat.cast_nonneg] using ratSqrt_nonneg h
+
+theorem realSqrt_lt_ratSqrt_add_inv_prec {x prec : ℕ} (h : 0 < prec) :
+    √x < ratSqrt x prec + 1 / prec := by
+  have := lt_ratSqrt_add_inv_prec_eq (x := x) h
+  have : (x : ℝ) < ↑((x.ratSqrt prec + 1 / prec) ^ 2 : ℚ) := by norm_cast
+  have := Real.sqrt_lt_sqrt (by simp) this
+  rw [Rat.cast_pow] at this
+  rw [Real.sqrt_sq] at this
+  · push_cast at this
+    exact this
+  · push_cast
+    exact add_nonneg (by simpa using ratSqrt_nonneg h) (by simp)
+
+theorem realSqrt_mem_Ico {x prec : ℕ} (h : 0 < prec) :
+    √x ∈ Set.Ico (ratSqrt x prec : ℝ) (ratSqrt x prec + 1 / prec : ℝ) := by
+  grind [ratSqrt_le_realSqrt, realSqrt_lt_ratSqrt_add_inv_prec]
+
+theorem ratSqrt_mem_Ioc {x prec : ℕ} (h : 0 < prec) :
+    (ratSqrt x prec : ℝ) ∈ Set.Ioc (√x - 1 / prec) √x := by
+  grind [ratSqrt_le_realSqrt, realSqrt_lt_ratSqrt_add_inv_prec]
 
 end Nat
 
-def sqrtApproxLower : ℚ := ((640320 * 10^(2 * 10^6) * 10^(2 * 10^6)).sqrt : ℚ) / (10^(2 * 10^6) : ℕ)
-def sqrtApproxUpper : ℚ := ((640320 * 10^(2 * 10^6) * 10^(2 * 10^6)).sqrt + 1 : ℚ) / 10^(2 * 10^6)
-
-theorem Real.natCast_sqrt_le (n : ℕ) : (n.sqrt : ℝ) ≤ (n : ℝ)^(1/2 : ℝ) := sorry
-
-theorem sqrtApproxLower_le_sqrt : sqrtApproxLower ≤ (640320 : ℝ)^(1/2 : ℝ) := by
-  unfold sqrtApproxLower
-  simp only [Rat.cast_div, Rat.cast_pow, Nat.cast_ofNat, Rat.cast_ofNat, Rat.cast_natCast,
-    Nat.cast_pow]
-  rw [div_le_iff₀ (by norm_num)]
-  apply le_trans (Real.natCast_sqrt_le _)
-  have : ∀ x y : ℝ, x^2 ≤ y^2 → x ≤ y := sorry
-  apply this
-  rw [mul_pow]
-  rw [← Real.rpow_two, ← Real.rpow_mul (by norm_num)]
-  rw [← Real.rpow_two, ← Real.rpow_mul (by norm_num)]
-  rw [show (1 / 2 * 2 : ℝ) = 1 by norm_num, Real.rpow_one, Real.rpow_one, pow_two]
-  rw [Nat.cast_mul, Nat.cast_mul, Nat.cast_ofNat, Nat.cast_pow, Nat.cast_ofNat, mul_assoc]
-
-
-
-
+open Nat
 
 open Lean in
 instance : ToExpr ℚ where
   toExpr q := mkApp2 (.const ``mkRat []) (toExpr q.num) (toExpr q.den)
   toTypeExpr := .const ``Rat [0]
 
--- example : sqrtApproxLower = eval% sqrtApproxLower := by
---   unfold sqrtApproxLower
---   norm_num
+/-- Compute an explicit rational approximation of `√10005`, accurate to 2 million decimal places. -/
+def sqrt_10005_approx : ℚ := eval% ratSqrt 10005 (10^(2 * 10^6))
+
+theorem sqrt_10005_approx_eq : sqrt_10005_approx = ratSqrt 10005 (10^(2 * 10^6)) := by
+  norm_num [sqrt_10005_approx, ratSqrt]
+
+theorem sqrt_10005 :
+    √(10005 : ℝ) ∈
+      Set.Ico (sqrt_10005_approx : ℝ)
+              (sqrt_10005_approx + 1 / 10^(2 * 10^6) : ℝ) := by
+  rw [sqrt_10005_approx_eq]
+  exact_mod_cast realSqrt_mem_Ico (x := 10005) (by norm_num)
