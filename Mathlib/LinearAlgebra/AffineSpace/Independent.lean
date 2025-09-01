c863bce231083b287545e425c971a81533f42761
Joseph Myers <jsm@polyomino.org.uk>
2025-09-01 20:45:20 +0000

    Update Mathlib/LinearAlgebra/AffineSpace/Independent.lean
    Co-authored-by: Christian Merten <136261474+chrisflav@users.noreply.github.com>

--- Mathlib/LinearAlgebra/AffineSpace/Independent.lean
+++ Mathlib/LinearAlgebra/AffineSpace/Independent.lean
@@ -343,11 +343,11 @@ lemma AffineIndependent.eq_zero_of_affineCombination_mem_affineSpan {p : ι → 
 lemma AffineIndependent.indicator_eq_of_affineCombination_comp_embedding_eq {ι₂ : Type*} {p : ι → P}
     (ha : AffineIndependent k p) {s₁ : Finset ι} {s₂ : Finset ι₂} {w₁ : ι → k} {w₂ : ι₂ → k}
     (hw₁ : ∑ i ∈ s₁, w₁ i = 1) (hw₂ : ∑ i ∈ s₂, w₂ i = 1) (e : ι₂ ↪ ι)
-    (h : s₁.affineCombination k p w₁ = s₂.affineCombination k (p ∘ e) w₂) :
-    Set.indicator (s₂.map e) (extend e w₂ 0) = Set.indicator (↑s₁) w₁ := by
+    (h : s₂.affineCombination k (p ∘ e) w₂ = s₁.affineCombination k p w₁) :
+    Set.indicator (s₂.map e) (extend e w₂ 0) = Set.indicator s₁ w₁ := by
   have hw₂e : extend e w₂ 0 ∘ e = w₂ := extend_comp e.injective _ _
   rw [← hw₂e, ← affineCombination_map] at h
-  refine (ha.indicator_eq_of_affineCombination_eq s₁ (s₂.map e) _ _ hw₁ ?_ h).symm
+  refine (ha.indicator_eq_of_affineCombination_eq s₁ (s₂.map e) _ _ hw₁ ?_ h.symm).symm
   rw [sum_map]
   convert hw₂ with i hi
   exact e.injective.extend_apply _ _ _
