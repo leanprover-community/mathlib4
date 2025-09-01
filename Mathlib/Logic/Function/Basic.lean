13af3c44ba39363ee7fe071aa6eb037c04b3442c
Eric Wieser <425260+eric-wieser@users.noreply.github.com>
2025-09-01 07:30:58 +0000

    feat: results about `Function.{curry,uncurry}`, and `Matrix.vec_single` (#29149)

--- Mathlib/Logic/Function/Basic.lean
+++ Mathlib/Logic/Function/Basic.lean
@@ -807,8 +807,39 @@ theorem comp_right {α β γ δ : Sort*} {f : α → β} {g : α → γ} (h : Fa
 
 end FactorsThrough
 
+section CurryAndUncurry
+
 theorem uncurry_def {α β γ} (f : α → β → γ) : uncurry f = fun p ↦ f p.1 p.2 :=
   rfl
+
+theorem uncurry_injective {α β γ} : Function.Injective (uncurry : (α → β → γ) → _) :=
+  LeftInverse.injective curry_uncurry
+
+theorem curry_injective {α β γ} : Function.Injective (curry : (α × β → γ) → _) :=
+  LeftInverse.injective uncurry_curry
+
+theorem uncurry_flip {α β γ} (f : α → β → γ) : uncurry (flip f) = uncurry f ∘ Prod.swap :=
+  rfl
+
+theorem flip_curry {α β γ} (f : α × β → γ) : flip (curry f) = curry (f ∘ Prod.swap) :=
+  rfl
+
+theorem curry_update {α α' β : Type*} [DecidableEq α] [DecidableEq α']
+    (f : α × α' → β) (aa' : α × α') (b : β) :
+    curry (Function.update f aa' b) =
+      Function.update (curry f) aa'.1 (Function.update (curry f aa'.1) aa'.2 b) := by
+  ext a a'
+  let ⟨a₂, a₂'⟩ := aa'
+  obtain rfl | ha := eq_or_ne a a₂ <;> obtain rfl | ha' := eq_or_ne a' a₂' <;> simp [*]
+
+theorem uncurry_update_update {α α' β : Type*} [DecidableEq α] [DecidableEq α']
+    (f : α → α' → β) (a : α) (a' : α') (b : β) :
+    uncurry (Function.update f a (Function.update (f a) a' b)) =
+      Function.update (uncurry f) (a, a') b := by
+  apply curry_injective
+  simp [curry_update]
+
+end CurryAndUncurry
 
 section Bicomp
 
