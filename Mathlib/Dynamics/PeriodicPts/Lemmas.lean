2dbf20a57fd60c8b5f0782fc273a144d1817bba1
Yongshun Ye <ShreckYe@gmail.com>
2025-09-01 14:24:39 +0800

    Complete half of the proof of `minimalPeriod_piMap`

--- Mathlib/Dynamics/PeriodicPts/Lemmas.lean
+++ Mathlib/Dynamics/PeriodicPts/Lemmas.lean
@@ -143,11 +143,51 @@ section Pi
 
 variable {ι : Type*} {α : ι → Type*} {f : ∀ i, α i → α i} {x : ∀ i, α i}
 
+-- TODO: use this?
+#check isEmpty_or_nonempty
+
+/-- This `sInf` can be regarded as a generalized version of LCM
+for possibly infinite sets and types. -/
 -- TODO: make `f` and `x` explicit similar to `minimalPeriod_prodMap`?
 theorem minimalPeriod_piMap :
-    minimalPeriod (Pi.map f) x = sInf {n | 0 < n ∧ ∀ i, minimalPeriod (f i) (x i) ∣ n} :=
-
-  sorry
+    minimalPeriod (Pi.map f) x = sInf {n | 0 < n ∧ ∀ i, minimalPeriod (f i) (x i) ∣ n} := by
+  by_cases h : {n | 0 < n ∧ ∀ (i : ι), minimalPeriod (f i) (x i) ∣ n}.Nonempty
+  · sorry
+  · --replace h := not_nonempty_iff_eq_empty.mp h
+    simp [not_nonempty_iff_eq_empty.mp h] --simp [h]
+    apply minimalPeriod_eq_zero_of_notMem_periodicPts
+    simp [← isPeriodicPt_iff_minimalPeriod_dvd] at h
+    /- have : IsEmpty {n | 0 < n ∧ ∀ (i : ι), IsPeriodicPt (f i) n (x i)} := by
+      exact not_nonempty_iff.mp h -/
+    --simp [not_nonempty_iff.mp] at h
+    --simp [periodicPts]
+    have : ¬ Nonempty {n | 0 < n ∧ ∀ (i : ι), IsPeriodicPt (f i) n (x i)} := by
+      intro h2
+      have : {n | 0 < n ∧ ∀ (i : ι), IsPeriodicPt (f i) n (x i)}.Nonempty := by
+        exact nonempty_coe_sort.mp h2
+      exact h this
+    simp at this
+    simp [periodicPts]
+    exact this
+    --exact h
+    /-
+    intro x_mem_periodicPts
+    simp [periodicPts] at x_mem_periodicPts
+    exact h x_mem_periodicPts
+    -/
+  /- eq_of_forall_dvd <| by
+    by_cases h : {n | 0 < n ∧ ∀ (i : ι), minimalPeriod (f i) (x i) ∣ n}.Nonempty
+    · simp [← isPeriodicPt_iff_minimalPeriod_dvd]
+      --simp [IsPeriodicPt, IsFixedPt]
+      intro n
+      set inf := sInf {n | 0 < n ∧ ∀ (i : ι), IsPeriodicPt (f i) n (x i)}
+      constructor
+      ·
+        sorry
+      ·
+        sorry
+    · simp [not_nonempty_iff_eq_empty.mp h]
+      sorry -/
 
 -- TODO: make `f` and `x` explicit similar to `minimalPeriod_prodMap`?
 -- alternative name: `minimalPeriod_piMap_fintype`
