70a92c9ef299e2d5479cf5ee322d60f78e401622
bwangpj <70694994+bwangpj@users.noreply.github.com>
2025-09-02 05:25:07 +0800

    Update Countable.lean

--- Mathlib/Topology/MetricSpace/Countable.lean
+++ Mathlib/Topology/MetricSpace/Countable.lean
@@ -3,8 +3,6 @@ Copyright (c) 2025 Bryan Wang. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Geoffrey Irving, Bryan Wang
 -/
-import Mathlib.Topology.MetricSpace.Pseudo.Defs
-import Mathlib.Topology.MetricSpace.Defs
 import Mathlib.Analysis.Real.Cardinality
 
 /-!
@@ -24,35 +22,37 @@ namespace Metric
 section TotallyDisconnected
 
 open Function (uncurry) in
-/-- Countable metric spaces are totally disconnected. -/
-instance instTotallyDisconnectedSpace_of_countable
-    [Countable γ] :
-    TotallyDisconnectedSpace γ := by
-  generalize hR : {r | ∃ x y : γ, dist x y = r} = R
-  have rc : R.Countable := by
-    have e : R = range (uncurry (dist (α := γ))) := by apply Set.ext; intro r; simp [← hR]
-    rw [e]; exact countable_range _
-  refine @TotallySeparatedSpace.totallyDisconnectedSpace _ _ ?_
+/-- Countable metric spaces are totally separated. -/
+instance instTotallySeparatedSpace_of_countable [Countable γ] :
+    TotallySeparatedSpace γ := by
+  let R := {r | ∃ x y : γ, dist x y = r}
+  have hR : R.Countable := by
+    have hR₁ : R = range (uncurry (dist (α := γ))) := by apply Set.ext; intro r; simp [R]
+    rw [hR₁]; exact countable_range _
   rw [totallySeparatedSpace_iff_exists_isClopen]
-  intro x y xy
-  rw [← dist_pos] at xy
+  intro x y hxy
+  rw [← dist_pos] at hxy
   have h : ¬Ioo 0 (dist x y) ⊆ R := by
-    by_contra h; have h' := (rc.mono h)
-    rw [← Cardinal.le_aleph0_iff_set_countable, Cardinal.mk_Ioo_real xy] at h'
+    by_contra h; have h' := (hR.mono h)
+    rw [← Cardinal.le_aleph0_iff_set_countable, Cardinal.mk_Ioo_real hxy] at h'
     exact (not_le.mpr (Cardinal.cantor _)) h'
   simp only [not_subset, mem_Ioo] at h; rcases h with ⟨r, ⟨rp, rxy⟩, rr⟩
-  have e : ball x r = closedBall x r := by
+  have hball : ball x r = closedBall x r := by
     apply Set.ext; intro z; simp only [mem_ball, mem_closedBall]
-    simp only [mem_setOf, not_exists, ← hR] at rr; simp [Ne.le_iff_lt (rr z x)]
+    simp only [mem_setOf, not_exists, R] at rr; simp [Ne.le_iff_lt (rr z x)]
   refine ⟨ball x r, ⟨?_, isOpen_ball⟩, ?_⟩
-  · simpa [e] using isClosed_closedBall;
+  · simpa [hball] using isClosed_closedBall;
   use mem_ball_self rp; simpa [dist_comm] using rxy.le
+
+/-- Countable metric spaces are totally disconnected. -/
+instance instTotallyDisconnectedSpace_of_countable [Countable γ] :
+    TotallyDisconnectedSpace γ := inferInstance
 
 /-- Countable subsets of metric spaces are totally disconnected. -/
 theorem Countable.isTotallyDisconnected (hs : Countable s) :
     IsTotallyDisconnected s := by
   rw [← totallyDisconnectedSpace_subtype_iff]
-  exact @instTotallyDisconnectedSpace_of_countable _ _ (countable_coe_iff.mpr hs)
+  infer_instance
 
 end TotallyDisconnected
 
