2987540a8853a4529396dcc67dd5d342df61623a
Sebastien Gouezel <10818434+sgouezel@users.noreply.github.com>
2025-09-01 06:26:32 +0000

    feat: finite product of probability measures (#28284)
    We copy the API for the binary product of probability measures. Notably, we show that a finite product of probability measures depends continuously on the factors.
    
    Co-authored-by: sgouezel <sebastien.gouezel@univ-rennes1.fr>

--- Mathlib.lean
+++ Mathlib.lean
@@ -4569,6 +4569,7 @@ import Mathlib.MeasureTheory.Measure.Doubling
 import Mathlib.MeasureTheory.Measure.EverywherePos
 import Mathlib.MeasureTheory.Measure.FiniteMeasure
 import Mathlib.MeasureTheory.Measure.FiniteMeasureExt
+import Mathlib.MeasureTheory.Measure.FiniteMeasurePi
 import Mathlib.MeasureTheory.Measure.FiniteMeasureProd
 import Mathlib.MeasureTheory.Measure.GiryMonad
 import Mathlib.MeasureTheory.Measure.Haar.Basic
