184fd30170c3ed781857a46de38d55b97dfe5470
euprunin <euprunin@users.noreply.github.com>
2025-09-01 21:38:41 +0200

    chore: remove redundant `replace` invocations

--- Mathlib/Algebra/Lie/Submodule.lean
+++ Mathlib/Algebra/Lie/Submodule.lean
@@ -686,7 +686,6 @@ lemma isCompactElement_lieSpan_singleton (m : M) :
     CompleteLattice.IsCompactElement (lieSpan R L {m}) := by
   rw [CompleteLattice.isCompactElement_iff_le_of_directed_sSup_le]
   intro s hne hdir hsup
-  replace hsup : m ∈ (↑(sSup s) : Set M) := (SetLike.le_def.mp hsup) (subset_lieSpan rfl)
   suffices (↑(sSup s) : Set M) = ⋃ N ∈ s, ↑N by simp_all
   replace hne : Nonempty s := Set.nonempty_coe_sort.mpr hne
   have := Submodule.coe_iSup_of_directed _ hdir.directed_val
