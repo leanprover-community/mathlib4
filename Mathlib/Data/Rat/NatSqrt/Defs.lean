7f2300a8fead274d7112fad25031fbd0931bf039
Kim Morrison <kim@tqft.net>
2025-09-01 22:03:12 +1000

    explicitness

--- Mathlib/Data/Rat/NatSqrt/Defs.lean
+++ Mathlib/Data/Rat/NatSqrt/Defs.lean
@@ -19,17 +19,17 @@ Approximate the square root of a natural number as a rational number, to within 
 -/
 def ratSqrt (x : ℕ) (prec : ℕ) : ℚ := ((x * prec ^ 2).sqrt : ℚ) / prec
 
-theorem ratSqrt_nonneg {x prec : ℕ} : 0 ≤ ratSqrt x prec := by
+theorem ratSqrt_nonneg (x prec : ℕ) : 0 ≤ ratSqrt x prec := by
   unfold ratSqrt
   positivity
 
-theorem ratSqrt_sq_le {x prec : ℕ} (h : 0 < prec) : (ratSqrt x prec) ^ 2 ≤ x := by
+theorem ratSqrt_sq_le (x : ℕ) {prec : ℕ} (h : 0 < prec) : (ratSqrt x prec) ^ 2 ≤ x := by
   unfold ratSqrt
   rw [div_pow, div_le_iff₀ (by positivity)]
   norm_cast
   exact sqrt_le' (x * prec ^ 2)
 
-theorem lt_ratSqrt_add_inv_prec_sq {x prec : ℕ} (h : 0 < prec) :
+theorem lt_ratSqrt_add_inv_prec_sq (x : ℕ) {prec : ℕ} (h : 0 < prec) :
     x < (ratSqrt x prec + 1 / prec) ^ 2 := by
   unfold ratSqrt
   rw [← mul_lt_mul_iff_of_pos_right (a := (prec ^ 2 : ℚ)) (by positivity)]
--- Mathlib/Data/Rat/NatSqrt/Real.lean
+++ Mathlib/Data/Rat/NatSqrt/Real.lean
@@ -13,14 +13,14 @@ and the real square root.
 
 namespace Nat
 
-theorem ratSqrt_le_realSqrt {x prec : ℕ} (h : 0 < prec) : ratSqrt x prec ≤ √x := by
+theorem ratSqrt_le_realSqrt (x : ℕ) {prec : ℕ} (h : 0 < prec) : ratSqrt x prec ≤ √x := by
   have := ratSqrt_sq_le (x := x) h
   have : (x.ratSqrt prec ^ 2 : ℝ) ≤ ↑x := by norm_cast
   have := Real.sqrt_monotone this
   rwa [Real.sqrt_sq] at this
-  simpa only [Rat.cast_nonneg] using ratSqrt_nonneg
+  simpa only [Rat.cast_nonneg] using ratSqrt_nonneg _ _
 
-theorem realSqrt_lt_ratSqrt_add_inv_prec {x prec : ℕ} (h : 0 < prec) :
+theorem realSqrt_lt_ratSqrt_add_inv_prec (x : ℕ) {prec : ℕ} (h : 0 < prec) :
     √x < ratSqrt x prec + 1 / prec := by
   have := lt_ratSqrt_add_inv_prec_sq (x := x) h
   have : (x : ℝ) < ↑((x.ratSqrt prec + 1 / prec) ^ 2 : ℚ) := by norm_cast
@@ -30,13 +30,13 @@ theorem realSqrt_lt_ratSqrt_add_inv_prec {x prec : ℕ} (h : 0 < prec) :
   · push_cast at this
     exact this
   · push_cast
-    exact add_nonneg (by simpa using ratSqrt_nonneg) (by simp)
+    exact add_nonneg (by simpa using ratSqrt_nonneg _ _) (by simp)
 
-theorem realSqrt_mem_Ico {x prec : ℕ} (h : 0 < prec) :
+theorem realSqrt_mem_Ico (x : ℕ) {prec : ℕ} (h : 0 < prec) :
     √x ∈ Set.Ico (ratSqrt x prec : ℝ) (ratSqrt x prec + 1 / prec : ℝ) := by
   grind [ratSqrt_le_realSqrt, realSqrt_lt_ratSqrt_add_inv_prec]
 
-theorem ratSqrt_mem_Ioc {x prec : ℕ} (h : 0 < prec) :
+theorem ratSqrt_mem_Ioc (x : ℕ) {prec : ℕ} (h : 0 < prec) :
     (ratSqrt x prec : ℝ) ∈ Set.Ioc (√x - 1 / prec) √x := by
   grind [ratSqrt_le_realSqrt, realSqrt_lt_ratSqrt_add_inv_prec]
 
