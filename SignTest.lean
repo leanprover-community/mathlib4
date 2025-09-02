c3e0725519ae9b3d2a46db10e95e9b3c13a44b58
Yongshun Ye <ShreckYe@gmail.com>
2025-09-02 16:41:23 +0800

    Add code from copied from my own project

---
+++ SignTest.lean
@@ -0,0 +1,21 @@
+import Mathlib
+
+open Real
+
+--lemma sign_mul_abs
+example (x : ℝ) : sign x * |x| = x := by
+  unfold sign
+  split_ifs
+  . simp
+    suffices |x| = -x by
+      linarith
+    simp
+    linarith
+  . simp
+    linarith
+  . simp
+    linarith
+
+--lemma sign_mul
+example (x y : ℝ) : (x * y).sign = x.sign * y.sign := by
+  sorry
