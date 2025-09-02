3cbf2cc744b9faa52cc353caf1824cf9e39e07bd
Yongshun Ye <ShreckYe@gmail.com>
2025-09-02 17:19:12 +0800

    Remove the `#check`s for references

--- SignTest.lean
+++ SignTest.lean
@@ -4,15 +4,10 @@ open Real
 
 namespace Real
 
-#check sign_mul_abs
-
 @[simp]
 lemma sign_mul_abs (x : ℝ) : sign x * |x| = x := by
   rcases lt_trichotomy x 0 with hx | rfl | hx <;>
   simp [*, sign_of_pos, sign_zero, sign_of_neg, abs_of_pos, abs_of_neg]
-
-
-#check sign_mul
 
 lemma sign_mul (x y : ℝ) : (x * y).sign = x.sign * y.sign := by
   rcases lt_trichotomy x 0 with (hx | hx | hx) <;> rcases lt_trichotomy y 0 with (hy | hy | hy) <;>
