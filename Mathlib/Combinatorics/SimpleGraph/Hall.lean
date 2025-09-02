b516896c042cd395c5c6214b831cec76735c4ee4
Vlad Tsyrklevich <vlad@tsyrklevi.ch>
2025-09-02 20:07:16 +0200

    clean up

--- Mathlib/Combinatorics/SimpleGraph/Hall.lean
+++ Mathlib/Combinatorics/SimpleGraph/Hall.lean
@@ -15,8 +15,8 @@ This file derives Hall's Marriage Theorem for bipartite graphs from the combinat
 
 ## Main statements
 
-* `SimpleGraph.exists_IsMatching_of_forall_ncard_le`: Hall's marriage theorem for a matching for a
-  matching on a single partition of a bipartite graph.
+* `SimpleGraph.exists_IsMatching_of_forall_ncard_le`: Hall's marriage theorem for a matching on a
+  single partition of a bipartite graph.
 * `SimpleGraph.exists_IsPerfectMatching_of_forall_ncard_le`: Hall's marriage theorem for a perfect
   matching on a bipartite graph.
 
@@ -65,7 +65,7 @@ theorem exists_IsMatching_of_forall_ncard_le [DecidablePred (· ∈ p₁)] (h₁
     simpa [← Set.ncard_coe_finset, neighborFinset_def]
   have (x : V) (h : x ∈ p₁) : f ⟨x, h⟩ ∉ p₁ := h₁.disjoint |>.notMem_of_mem_right <|
     isBipartiteWith_neighborSet_subset h₁ h <| Set.mem_toFinset.mp <| hf₂ ⟨x, h⟩
-  use hall_subgraph f this (fun v h ↦ G.mem_neighborFinset _ _ |>.mp (hf₂ ⟨v, by assumption⟩))
+  use hall_subgraph f this (fun v _ ↦ G.mem_neighborFinset _ _ |>.mp <| hf₂ ⟨v, by assumption⟩)
   refine ⟨by simp, fun v hv ↦ ?_⟩
   simp only [Set.mem_union, Set.mem_range, Subtype.exists] at hv ⊢
   rcases hv with h' | ⟨x, hx₁, hx₂⟩
@@ -76,15 +76,13 @@ theorem exists_IsMatching_of_forall_ncard_le [DecidablePred (· ∈ p₁)] (h₁
     exact fun _ _ k ↦ Subtype.ext_iff_val.mp <| hf₁ (hx₂ ▸ k)
 
 private
-lemma union_eq_set_univ_of_forall_ncard_le (h₁ : G.IsBipartiteWith p₁ p₂)
+lemma union_eq_univ_of_forall_ncard_le (h₁ : G.IsBipartiteWith p₁ p₂)
     (h₂ : ∀ s : Set V, s.ncard ≤ (⋃ x ∈ s, G.neighborSet x).ncard) : p₁ ∪ p₂ = Set.univ := by
   obtain ⟨f, _, hf₂⟩ := Finset.all_card_le_biUnion_card_iff_exists_injective
       (fun x ↦ G.neighborFinset x) |>.mp fun s ↦ by
     have := h₂ s
     simpa [← Set.ncard_coe_finset, neighborFinset_def]
   refine Set.eq_univ_iff_forall.mpr fun x ↦ ?_
-  have := hf₂ x
-  have := G.mem_neighborFinset _ _ |>.mp (hf₂ x)
   have := h₁.mem_of_adj <| G.mem_neighborFinset _ _ |>.mp (hf₂ x)
   grind
 
@@ -101,10 +99,10 @@ lemma exists_bijective_of_forall_ncard_le [DecidablePred (· ∈ p₁)] (h₁ : 
   have (x : V) (h : x ∈ p₂) : f x ∉ p₂ := h₁.disjoint |>.notMem_of_mem_left <|
     isBipartiteWith_neighborSet_subset h₁.symm h <| Set.mem_toFinset.mp <| hf₂ x
   have (x : V) : f x ∈ p₁ ∨ f x ∈ p₂ := by
-    simp [union_eq_set_univ_of_forall_ncard_le h₁ h₂, p₁.mem_union (f x) p₂ |>.mp]
+    simp [union_eq_univ_of_forall_ncard_le h₁ h₂, p₁.mem_union (f x) p₂ |>.mp]
   let f' (x : p₁) : p₂ := ⟨f x, by grind⟩
   let g' (x : p₂) : p₁ := ⟨f x, by grind⟩
-  refine Embedding.schroeder_bernstein' (f := f') (g := g') ?_ ?_ (fun x y ↦ G.Adj x y) ?_ ?_
+  refine Embedding.schroeder_bernstein_of_rel (f := f') (g := g') ?_ ?_ (fun x y ↦ G.Adj x y) ?_ ?_
   · exact Injective.of_comp (f := Subtype.val) <| hf₁.comp <| Subtype.val_injective
   · exact Injective.of_comp (f := Subtype.val) <| hf₁.comp <| Subtype.val_injective
   · exact fun v ↦ mem_neighborFinset _ _ _ |>.mp (hf₂ v)
@@ -116,11 +114,11 @@ theorem exists_IsPerfectMatching_of_forall_ncard_le [DecidablePred (· ∈ p₁)
     (h₁ : G.IsBipartiteWith p₁ p₂) (h₂ : ∀ s : Set V, s.ncard ≤ (⋃ x ∈ s, G.neighborSet x).ncard) :
     ∃ M : Subgraph G, M.IsPerfectMatching := by
   obtain ⟨b, hb₁, hb₂⟩ := exists_bijective_of_forall_ncard_le h₁ h₂
-  use hall_subgraph (fun v ↦ (b v).1) (fun v hv ↦ h₁.disjoint |>.notMem_of_mem_right (b ⟨v, hv⟩).2)
-    (fun v h ↦ hb₂ ⟨v, by assumption⟩)
+  use hall_subgraph (fun v ↦ b v) (fun v hv ↦ h₁.disjoint.notMem_of_mem_right (b ⟨v, hv⟩).property)
+    (fun v hv ↦ hb₂ ⟨v, hv⟩)
   have : p₁ ∪ (Set.range (fun v ↦ (b v).1)) = Set.univ := by
     rw [Set.range_comp', hb₁.surjective.range_eq, Subtype.coe_image_univ]
-    exact union_eq_set_univ_of_forall_ncard_le h₁ h₂
+    exact union_eq_univ_of_forall_ncard_le h₁ h₂
   refine ⟨fun v _ ↦ ?_, by simp [Subgraph.IsSpanning, this]⟩
   simp only [dite_else_false]
   split
