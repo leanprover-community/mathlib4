7cd490ab697e9aa7c2bca6180e81fc23533eab0a
Harald Husum <harald.husum@intelecy.com>
2025-09-02 20:38:26 +0200

    attach quasi

--- Mathlib/Dynamics/Ergodic/Ergodic.lean
+++ Mathlib/Dynamics/Ergodic/Ergodic.lean
@@ -15,16 +15,16 @@ respect to `μ` (or `μ` is ergodic with respect to `f`) if the only measurable 
 
 In this file we define ergodic maps / measures together with quasi-ergodic maps / measures and
 provide some basic API. Quasi-ergodicity is a weaker condition than ergodicity for which the measure
-preserving condition is relaxed to quasi measure preserving.
+preserving condition is relaxed to quasi-measure-preserving.
 
 # Main definitions:
 
 * `PreErgodic`: the ergodicity condition without the measure preserving condition. This exists
   to share code between the `Ergodic` and `QuasiErgodic` definitions.
 * `Ergodic`: the definition of ergodic maps / measures.
-* `QuasiErgodic`: the definition of quasi ergodic maps / measures.
-* `Ergodic.quasiErgodic`: an ergodic map / measure is quasi ergodic.
-* `QuasiErgodic.ae_empty_or_univ'`: when the map is quasi measure preserving, one may relax the
+* `QuasiErgodic`: the definition of quasi-ergodic maps / measures.
+* `Ergodic.quasiErgodic`: an ergodic map / measure is quasi-ergodic.
+* `QuasiErgodic.ae_empty_or_univ'`: when the map is quasi-measure-preserving, one may relax the
   strict invariance condition to almost invariance in the ergodicity condition.
 
 -/
@@ -46,8 +46,8 @@ preserving and pre-ergodic. -/
 structure Ergodic (f : α → α) (μ : Measure α := by volume_tac) : Prop extends
   MeasurePreserving f μ μ, PreErgodic f μ
 
-/-- A map `f : α → α` is said to be quasi ergodic with respect to a measure `μ` if it is quasi
-measure preserving and pre-ergodic. -/
+/-- A map `f : α → α` is said to be quasi-ergodic with respect to a measure `μ` if it is
+quasi-measure-preserving and pre-ergodic. -/
 structure QuasiErgodic (f : α → α) (μ : Measure α := by volume_tac) : Prop extends
   QuasiMeasurePreserving f μ μ, PreErgodic f μ
 
@@ -121,14 +121,14 @@ theorem aeconst_set₀ (hf : QuasiErgodic f μ) (hsm : NullMeasurableSet s μ) (
   let ⟨_t, h₀, h₁, h₂⟩ := hf.toQuasiMeasurePreserving.exists_preimage_eq_of_preimage_ae hsm hs
   (hf.aeconst_set h₀ h₂).congr h₁
 
-/-- For a quasi ergodic map, sets that are almost invariant (rather than strictly invariant) are
+/-- For a quasi-ergodic map, sets that are almost invariant (rather than strictly invariant) are
 still either almost empty or full. -/
 theorem ae_empty_or_univ₀ (hf : QuasiErgodic f μ) (hsm : NullMeasurableSet s μ)
     (hs : f ⁻¹' s =ᵐ[μ] s) :
     s =ᵐ[μ] (∅ : Set α) ∨ s =ᵐ[μ] univ :=
   eventuallyConst_set'.mp <| hf.aeconst_set₀ hsm hs
 
-/-- For a quasi ergodic map, sets that are almost invariant (rather than strictly invariant) are
+/-- For a quasi-ergodic map, sets that are almost invariant (rather than strictly invariant) are
 still either almost empty or full. -/
 theorem ae_mem_or_ae_notMem₀ (hf : QuasiErgodic f μ) (hsm : NullMeasurableSet s μ)
     (hs : f ⁻¹' s =ᵐ[μ] s) :
@@ -150,7 +150,7 @@ end QuasiErgodic
 
 namespace Ergodic
 
-/-- An ergodic map is quasi ergodic. -/
+/-- An ergodic map is quasi-ergodic. -/
 theorem quasiErgodic (hf : Ergodic f μ) : QuasiErgodic f μ :=
   { hf.toPreErgodic, hf.toMeasurePreserving.quasiMeasurePreserving with }
 
