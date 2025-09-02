cbe7f9b45e5a62a2bba25d7aae339afebc436f59
Yongshun Ye <ShreckYe@gmail.com>
2025-09-02 17:20:19 +0800

    Move into `SignTest.lean`

--- Mathlib/Data/Real/Sign.lean
+++ Mathlib/Data/Real/Sign.lean
@@ -105,4 +105,13 @@ theorem sign_inv (r : ℝ) : sign r⁻¹ = sign r := by
   · rw [sign_zero, inv_zero, sign_zero]
   · rw [sign_of_pos hp, sign_of_pos (inv_pos.mpr hp)]
 
+lemma sign_mul_abs (x : ℝ) : sign x * |x| = x := by
+  rcases lt_trichotomy x 0 with hx | rfl | hx <;>
+  simp [*, sign_of_pos, sign_zero, sign_of_neg, abs_of_pos, abs_of_neg]
+
+lemma sign_mul (x y : ℝ) : (x * y).sign = x.sign * y.sign := by
+  rcases lt_trichotomy x 0 with (hx | hx | hx) <;> rcases lt_trichotomy y 0 with (hy | hy | hy) <;>
+    simp [hx, hy, mul_pos_of_neg_of_neg, mul_neg_of_neg_of_pos, mul_neg_of_pos_of_neg,
+      sign_of_pos, sign_zero, sign_of_neg]
+
 end Real
