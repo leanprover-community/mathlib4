2ed344e830b4223d5bec502de9896a5c50261179
Jireh Loreaux <loreaujy@gmail.com>
2025-09-02 15:28:19 -0500

    chore(Analysis/NormedSpace/DualNumber): deprecate module

--- Mathlib.lean
+++ Mathlib.lean
@@ -1831,6 +1831,7 @@ import Mathlib.Analysis.NormedSpace.Alternating.Curry
 import Mathlib.Analysis.NormedSpace.BallAction
 import Mathlib.Analysis.NormedSpace.ConformalLinearMap
 import Mathlib.Analysis.NormedSpace.Connected
+import Mathlib.Analysis.NormedSpace.DualNumber
 import Mathlib.Analysis.NormedSpace.ENormedSpace
 import Mathlib.Analysis.NormedSpace.Extend
 import Mathlib.Analysis.NormedSpace.Extr
