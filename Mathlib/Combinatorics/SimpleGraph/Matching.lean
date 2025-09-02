b9e64e06746473e36048f6d14339ce8a4fbbdf74
Vlad Tsyrklevich <vlad@tsyrklevi.ch>
2025-09-02 11:07:35 +0200

    Move to new file

--- Mathlib/Combinatorics/SimpleGraph/Matching.lean
+++ Mathlib/Combinatorics/SimpleGraph/Matching.lean
@@ -3,8 +3,6 @@ Copyright (c) 2020 Alena Gusakov. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Alena Gusakov, Arthur Paulino, Kyle Miller, Pim Otte
 -/
-import Mathlib.Combinatorics.Hall.Basic
-import Mathlib.Combinatorics.SimpleGraph.Bipartite
 import Mathlib.Combinatorics.SimpleGraph.Clique
 import Mathlib.Combinatorics.SimpleGraph.Connectivity.Subgraph
 import Mathlib.Combinatorics.SimpleGraph.Connectivity.WalkCounting
@@ -255,88 +253,6 @@ lemma IsPerfectMatching.induce_connectedComponent_isMatching (h : M.IsPerfectMat
 lemma IsPerfectMatching.toSubgraph_iff (h : M.spanningCoe ≤ G') :
     (G'.toSubgraph M.spanningCoe h).IsPerfectMatching ↔ M.IsPerfectMatching := by
   simp only [isPerfectMatching_iff, toSubgraph_adj, spanningCoe_adj]
-
-private
-abbrev hall_subgraph {p : Set V} [DecidablePred (· ∈ p)] (f : p → V)
-    (h : ∀ x : V, (h : x ∈ p) → f ⟨x, h⟩ ∉ p)
-    (h₂ : ∀ x : V, (h : x ∈ p) → G.Adj x (f ⟨x, h⟩)) : Subgraph G :=
-  {
-    verts := p ∪ Set.range f
-    Adj v w :=
-      if h : v ∈ p then f ⟨v, h⟩ = w
-      else if h : w ∈ p then f ⟨w, h⟩ = v
-      else False
-    adj_sub {v w} h := by
-      repeat' split at h
-      · have := h₂ v (by assumption)
-        rwa [← h]
-      · have := h₂ w (by assumption) |>.symm
-        rwa [← h]
-      · contradiction
-    edge_vert {v w} := by grind
-    symm {x y} := by grind
-  }
-
-/-- This is the version of **Hall's marriage theorem** for bipartite graphs that finds a matching
-for a single partition given that the neighborhood-condition only holds for elements of that
-partition. -/
-theorem exists_IsMatching_of_forall_ncard_le [DecidableEq V] [G.LocallyFinite]
-    {p₁ p₂ : Set V} [DecidablePred (· ∈ p₁)] (h₁ : G.IsBipartiteWith p₁ p₂)
-    (h₂ : ∀ s ⊆ p₁, s.ncard ≤ (⋃ x ∈ s, G.neighborSet x).ncard) :
-    ∃ M : Subgraph G, p₁ ⊆ M.verts ∧ M.IsMatching := by
-  obtain ⟨f, hf₁, hf₂⟩ := Finset.all_card_le_biUnion_card_iff_exists_injective
-      (fun (x : p₁) ↦ G.neighborFinset x) |>.mp fun s ↦ by
-    have := h₂ (s.image Subtype.val) (by simp)
-    rw [Set.ncard_coe_finset, Finset.card_image_of_injective _ Subtype.val_injective] at this
-    simpa [← Set.ncard_coe_finset, SimpleGraph.neighborFinset_def]
-  have (x : V) (h : x ∈ p₁) : f ⟨x, h⟩ ∉ p₁ := h₁.disjoint |>.notMem_of_mem_right <|
-    SimpleGraph.isBipartiteWith_neighborSet_subset h₁ h <| Set.mem_toFinset.mp <| hf₂ ⟨x, h⟩
-  use hall_subgraph f this (fun v h ↦ G.mem_neighborFinset _ _ |>.mp (hf₂ ⟨v, by assumption⟩))
-  refine ⟨by simp, fun v hv ↦ ?_⟩
-  simp only [Set.mem_union, Set.mem_range, Subtype.exists] at hv ⊢
-  rcases hv with h' | ⟨x, hx₁, hx₂⟩
-  · exact ⟨f ⟨v, h'⟩, by simp_all⟩
-  · use x
-    have := hx₂ ▸ (this x hx₁)
-    simp only [this, ↓reduceDIte, hx₁, hx₂, dite_else_false, forall_exists_index, true_and]
-    exact fun _ _ k ↦ Subtype.ext_iff_val.mp <| hf₁ (hx₂ ▸ k)
-
-/-- This is the version of **Hall's marriage theorem** for bipartite graphs that finds a perfect
-matching given that the neighborhood-condition holds globally. -/
-theorem exists_IsPerfectMatching_of_forall_ncard_le [DecidableEq V] [G.LocallyFinite]
-    {p₁ p₂ : Set V} [DecidablePred (· ∈ p₁)] (h₁ : G.IsBipartiteWith p₁ p₂)
-    (h₂ : ∀ s : Set V, s.ncard ≤ (⋃ x ∈ s, G.neighborSet x).ncard) :
-    ∃ M : Subgraph G, M.IsPerfectMatching := by
-  obtain ⟨f, hf₁, hf₂⟩ := Finset.all_card_le_biUnion_card_iff_exists_injective
-      (fun x ↦ G.neighborFinset x) |>.mp fun s ↦ by
-    have := h₂ s
-    simpa [← Set.ncard_coe_finset, SimpleGraph.neighborFinset_def]
-  have (x : V) (h : x ∈ p₁) : f x ∉ p₁ := h₁.disjoint |>.notMem_of_mem_right <|
-    SimpleGraph.isBipartiteWith_neighborSet_subset h₁ h <| Set.mem_toFinset.mp <| hf₂ x
-  have (x : V) (h : x ∈ p₂) : f x ∉ p₂ := h₁.disjoint |>.notMem_of_mem_left <|
-    SimpleGraph.isBipartiteWith_neighborSet_subset h₁.symm h <| Set.mem_toFinset.mp <| hf₂ x
-  have : p₁ ∪ p₂ = Set.univ := by
-    refine Set.eq_univ_iff_forall.mpr fun x ↦ ?_
-    have := h₁.mem_of_adj <| G.mem_neighborFinset _ _ |>.mp (hf₂ x)
-    grind
-  have (x : V) : f x ∈ p₁ ∨ f x ∈ p₂ := by simp [this, Set.mem_union (f x) p₁ p₂ |>.mp]
-  let f' (x : p₁) : p₂ := ⟨f x, by grind⟩
-  let g' (x : p₂) : p₁ := ⟨f x, by grind⟩
-  have := Function.Embedding.schroeder_bernstein' (f := f') (g := g') ?_ ?_ (fun x y ↦ G.Adj x y)
-    (fun v ↦ mem_neighborFinset _ _ _ |>.mp (hf₂ v))
-    (fun v ↦ mem_neighborFinset _ _ _ |>.mp (hf₂ v) |>.symm)
-  · obtain ⟨b, hb₁, hb₂⟩ := this
-    use hall_subgraph (fun v ↦ (b v).1) (by grind) (fun v h ↦ hb₂ ⟨v, by assumption⟩)
-    have : p₁ ∪ (Set.range (fun v ↦ (b v).1)) = Set.univ := by
-      rwa [Set.range_comp', hb₁.surjective.range_eq, Subtype.coe_image_univ]
-    refine ⟨fun v _ ↦ ?_, by simp [IsSpanning, this]⟩
-    simp only [dite_else_false]
-    split
-    · exact existsUnique_eq'
-    · obtain ⟨x, _⟩ := hb₁.existsUnique ⟨v, by grind⟩
-      exact ⟨x, by grind⟩
-  · exact Injective.of_comp (f := Subtype.val) <| hf₁.comp <| Subtype.val_injective
-  · exact Injective.of_comp (f := Subtype.val) <| hf₁.comp <| Subtype.val_injective
 
 end Subgraph
 
