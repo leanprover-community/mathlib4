196aa3d24e2a23c3273794400d65f34f92d5bba2
Yongshun Ye <ShreckYe@gmail.com>
2025-09-02 17:11:11 +0800

    Golf the proof referring to `sign_mul_abs` for `SignType`

--- SignTest.lean
+++ SignTest.lean
@@ -6,15 +6,8 @@ open Real
 
 --lemma sign_mul_abs
 example (x : ℝ) : sign x * |x| = x := by
-  unfold sign
-  split_ifs with h h
-  · suffices |x| = -x by linarith
-    simp
-    linarith
-  · simp
-    linarith
-  · simp
-    linarith
+  rcases lt_trichotomy x 0 with hx | rfl | hx <;>
+  simp [*, sign_of_pos, sign_zero, sign_of_neg, abs_of_pos, abs_of_neg]
 
 --lemma sign_mul
 example (x y : ℝ) : (x * y).sign = x.sign * y.sign := by
