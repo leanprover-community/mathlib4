1479358f114c116fdbe9c7e81057a45bbedd005c
Jireh Loreaux <loreaujy@gmail.com>
2025-09-03 20:55:46 +0000

    chore(Analysis/NormedSpace/OperatorNorm/): deprecate modules (#29329)

---
+++ Mathlib/Analysis/NormedSpace/OperatorNorm/Basic.lean
@@ -0,0 +1,3 @@
+import Mathlib.Analysis.Normed.Operator.Basic
+
+deprecated_module (since := "2025-09-03")
---
+++ Mathlib/Analysis/NormedSpace/OperatorNorm/Completeness.lean
@@ -0,0 +1,3 @@
+import Mathlib.Analysis.Normed.Operator.Completeness
+
+deprecated_module (since := "2025-09-03")
