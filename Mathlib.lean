29f6f75da8a234db2ad29b9f8a353c01dcc0b1d7
Jireh Loreaux <loreaujy@gmail.com>
2025-09-03 14:38:32 +0000

    chore(Analysis/NormedSpace/{FunctionSeries,IndicatorFunction}): deprecate modules (#29268)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1844,10 +1844,12 @@ import Mathlib.Analysis.NormedSpace.DualNumber
 import Mathlib.Analysis.NormedSpace.ENormedSpace
 import Mathlib.Analysis.NormedSpace.Extend
 import Mathlib.Analysis.NormedSpace.Extr
+import Mathlib.Analysis.NormedSpace.FunctionSeries
 import Mathlib.Analysis.NormedSpace.HahnBanach.Extension
 import Mathlib.Analysis.NormedSpace.HahnBanach.SeparatingDual
 import Mathlib.Analysis.NormedSpace.HahnBanach.Separation
 import Mathlib.Analysis.NormedSpace.HomeomorphBall
+import Mathlib.Analysis.NormedSpace.IndicatorFunction
 import Mathlib.Analysis.NormedSpace.Int
 import Mathlib.Analysis.NormedSpace.MStructure
 import Mathlib.Analysis.NormedSpace.Multilinear.Basic
