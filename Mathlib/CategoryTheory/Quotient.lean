e27cc46f778fab333cb0df51839c90101b1a769c
jlh18 <josephhua73@yahoo.com>
2025-09-02 21:31:10 -0400

    fix: finish merge

--- Mathlib/CategoryTheory/Quotient.lean
+++ Mathlib/CategoryTheory/Quotient.lean
@@ -146,10 +146,6 @@ instance groupoid : Groupoid (Quotient r) where
 
 end
 
-<<<<<<< HEAD
-=======
-
->>>>>>> FreeGroupoid1
 /-- The functor from a category to its quotient. -/
 def functor : C ⥤ Quotient r where
   obj a := { as := a }
