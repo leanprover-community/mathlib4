abc8605f46a34abe0c123a65bb9cd32e767f0095
Yaël Dillies <yael.dillies@gmail.com>
2025-09-03 08:49:46 +0000

    fix

--- Mathlib/GroupTheory/Index.lean
+++ Mathlib/GroupTheory/Index.lean
@@ -99,9 +99,9 @@ theorem relIndex_map_map_of_injective {f : G →* G'} (H K : Subgroup G) (hf : F
 alias relindex_map_map_of_injective := relIndex_map_map_of_injective
 
 @[to_additive]
-theorem relindex_map_map (f : G →* G') (H K : Subgroup G) :
-    (map f H).relindex (map f K) = (H ⊔ f.ker).relindex (K ⊔ f.ker) := by
-  rw [← comap_map_eq, ← comap_map_eq, relindex_comap, (gc_map_comap f).l_u_l_eq_l]
+theorem relIndex_map_map (f : G →* G') (H K : Subgroup G) :
+    (map f H).relIndex (map f K) = (H ⊔ f.ker).relIndex (K ⊔ f.ker) := by
+  rw [← comap_map_eq, ← comap_map_eq, relIndex_comap, (gc_map_comap f).l_u_l_eq_l]
 
 variable {H K L}
 
