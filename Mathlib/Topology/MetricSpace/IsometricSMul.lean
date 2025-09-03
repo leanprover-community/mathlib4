f34d92834e801b3576eee9d56771457ebc379517
Harald Husum <harald.husum@intelecy.com>
2025-09-03 09:09:16 +0200

    attach pseudo

--- Mathlib/Topology/MetricSpace/IsometricSMul.lean
+++ Mathlib/Topology/MetricSpace/IsometricSMul.lean
@@ -16,7 +16,7 @@ public import Mathlib.Topology.MetricSpace.Lipschitz
 
 In this file we define two typeclasses:
 
-- `IsIsometricSMul M X` says that `M` multiplicatively acts on a (pseudo extended) metric space
+- `IsIsometricSMul M X` says that `M` multiplicatively acts on a (pseudo-extended) metric space
   `X` by isometries;
 - `IsIsometricVAdd` is an additive version of `IsIsometricSMul`.
 
