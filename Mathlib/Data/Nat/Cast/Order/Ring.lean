0d158fa4054cafb7f7bb3d8d19de75e9cc20cbde
Violeta Hernández <vi.hdz.p@gmail.com>
2025-09-02 20:53:33 -0600

    make diff nicer

--- Mathlib/Data/Nat/Cast/Order/Ring.lean
+++ Mathlib/Data/Nat/Cast/Order/Ring.lean
@@ -82,17 +82,6 @@ theorem cast_tsub [CommSemiring α] [PartialOrder α] [IsOrderedRing α] [Canoni
   · rcases le_iff_exists_add'.mp h with ⟨m, rfl⟩
     rw [add_tsub_cancel_right, cast_add, add_tsub_cancel_right]
 
-section PartialOrderedRing
-variable [Ring R] [PartialOrder R] [IsStrictOrderedRing R] {m n : ℕ}
-
-@[simp, norm_cast] lemma neg_cast_eq_cast : (-m : R) = n ↔ m = 0 ∧ n = 0 := by
-  simp [neg_eq_iff_add_eq_zero, ← cast_add]
-
-@[simp, norm_cast] lemma cast_eq_neg_cast : (m : R) = -n ↔ m = 0 ∧ n = 0 := by
-  simp [eq_neg_iff_add_eq_zero, ← cast_add]
-
-end PartialOrderedRing
-
 section Lattice
 variable [Ring R] [Lattice R] [IsOrderedRing R]
 
@@ -103,6 +92,17 @@ theorem abs_cast (n : ℕ) : |(n : R)| = n := abs_of_nonneg n.cast_nonneg
 theorem abs_ofNat (n : ℕ) [n.AtLeastTwo] : |(ofNat(n) : R)| = ofNat(n) := abs_cast n
 
 end Lattice
+
+section PartialOrderedRing
+variable [Ring R] [PartialOrder R] [IsStrictOrderedRing R] {m n : ℕ}
+
+@[simp, norm_cast] lemma neg_cast_eq_cast : (-m : R) = n ↔ m = 0 ∧ n = 0 := by
+  simp [neg_eq_iff_add_eq_zero, ← cast_add]
+
+@[simp, norm_cast] lemma cast_eq_neg_cast : (m : R) = -n ↔ m = 0 ∧ n = 0 := by
+  simp [eq_neg_iff_add_eq_zero, ← cast_add]
+
+end PartialOrderedRing
 
 lemma mul_le_pow {a : ℕ} (ha : a ≠ 1) (b : ℕ) :
     a * b ≤ a ^ b := by
