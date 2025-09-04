5097061b560947faf08ef4fb7e265c1827f02f3a
Monica Omar <2497250a@research.gla.ac.uk>
2025-09-04 06:02:47 +0000

    fix import

--- Mathlib/Algebra/Central/Matrix.lean
+++ Mathlib/Algebra/Central/Matrix.lean
@@ -3,7 +3,7 @@ Copyright (c) 2024 Jujian Zhang. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jujian Zhang, Yunzhou Xie
 -/
-import Mathlib.Algebra.Central.Basic
+import Mathlib.Algebra.Central.Defs
 import Mathlib.Data.Matrix.Basis
 
 /-!
