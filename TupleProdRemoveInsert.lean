a56f3195fbe7e59b0506f05c7fcc515476137e0c
Yongshun Ye <ShreckYe@gmail.com>
2025-09-04 18:41:21 +0800

    Find some related lemmas

--- TupleProdRemoveInsert.lean
+++ TupleProdRemoveInsert.lean
@@ -7,13 +7,19 @@ open List
 #check Fin.cons
 #check Fin.snoc
 
+#check Fin.prod_cons
+
 example (p : Fin n → ℕ) : ∏ j, insertNth i x p j = x * ∏ j, p j := by
   --exact?
   sorry
 
+#check List.prod_cons
+
 example (l : List ℕ) (h : i < l.length) : (l.insertIdx i a).prod = a * l.prod := by
   --exact?
   sorry
+
+#check List.Vector.prod_cons
 
 example (v : List.Vector ℕ n) : (v.insertIdx a i).toList.prod = a * v.toList.prod := by
   --exact?
