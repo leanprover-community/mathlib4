929bfd7d3662da47493f305986703e74de5ed59e
LessnessRandomness <56280933+LessnessRandomness@users.noreply.github.com>
2025-09-04 21:15:54 +0000

    feat(Analysis.NormedSpace): add normalized vectors (#26308)
    Add normalized vectors to Mathlib (zero vector for zero vector, the corresponding unit vectors for nonzero vectors)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1863,6 +1863,7 @@ import Mathlib.Analysis.NormedSpace.MStructure
 import Mathlib.Analysis.NormedSpace.Multilinear.Basic
 import Mathlib.Analysis.NormedSpace.Multilinear.Curry
 import Mathlib.Analysis.NormedSpace.MultipliableUniformlyOn
+import Mathlib.Analysis.NormedSpace.Normalize
 import Mathlib.Analysis.NormedSpace.OperatorNorm.Asymptotics
 import Mathlib.Analysis.NormedSpace.OperatorNorm.Basic
 import Mathlib.Analysis.NormedSpace.OperatorNorm.Bilinear
