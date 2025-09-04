3ae63343d650232db300389936f17bb801168462
Geoffrey Irving <irving@naml.us>
2025-09-04 21:22:14 +0100

    feat(Trigonometric): Taylor series bounds for sin and cos
    Zulip discussion here:
    
      https://leanprover.zulipchat.com/#narrow/channel/217875-Is-there-code-for-X.3F/topic/Better.20.60Real.2Esin.60.20bounds/near/535035576
    
    We also include the full set of intervals on which sin and cos are
    monotone/antitone.

---
+++ Mathlib/Analysis/SpecialFunctions/Trigonometric/SeriesBounds.lean
@@ -0,0 +1,103 @@
+/-
+Copyright (c) 2025 Geoffrey Irving. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Geoffrey Irving
+-/
+
+import Mathlib.Algebra.BigOperators.Field
+import Mathlib.Analysis.Complex.Basic
+import Mathlib.Analysis.Complex.Trigonometric
+
+/-!
+# Taylor series bounds for trigonometric functions
+
+## Main statements
+
+We prove Taylor series bounds with optimal remainder for `sin` and `cos`, over both `ℂ` and `ℝ`.
+See `Trigonometric.Bounds` for simpler inequalities and monotonicity in various intervals, and
+`Trigonometric.Basic` for even even simpler inequalities and monotonicity results.
+
+## Tags
+
+sin, cos
+-/
+
+open Set
+open scoped Real
+
+/-- Double the range of a `Finset.sum` -/
+lemma Finset.sum_range_even {M : Type*} [AddCommMonoid M] (n : ℕ) (f : ℕ → M) :
+    ∑ k ∈ Finset.range n, f k = ∑ k ∈ Finset.range (2 * n), if Even k then f (k / 2) else 0 := by
+  induction' n with n h
+  · simp
+  · simp [Finset.sum_range_succ, h, mul_add]
+
+lemma Complex.sin_series_bound {z : ℂ} (z1 : ‖z‖ ≤ 1) (n : ℕ) :
+    ‖sin z - z * ∑ k ∈ Finset.range n, (-1) ^ k * z ^ (2 * k) / (2 * k + 1).factorial‖ ≤
+      ‖z‖ ^ (2 * n + 1) * ((2 * n + 2) / ((2 * n + 1).factorial * (2 * n + 1))) := by
+  have e : z * ∑ k ∈ Finset.range n, (-1) ^ k * z ^ (2 * k) / (2 * k + 1).factorial =
+      (∑ k ∈ Finset.range (2 * n + 1), (-z * I) ^ k / k.factorial -
+       ∑ k ∈ Finset.range (2 * n + 1), (z * I) ^ k / k.factorial) * I / 2 := by
+    simp only [← Finset.sum_sub_distrib, ← sub_div, Finset.sum_range_even n, Finset.sum_div,
+      Finset.mul_sum, Finset.sum_mul]
+    simp only [Finset.sum_range_succ', pow_zero, Nat.factorial_zero, sub_self, zero_div, add_zero,
+      zero_mul]
+    refine Finset.sum_congr rfl fun k _ ↦ ?_
+    rcases Nat.even_or_odd' k with ⟨a, e | e⟩
+    · simp only [e, even_two, Even.mul_right, ↓reduceIte, ne_eq, OfNat.ofNat_ne_zero,
+      not_false_eq_true, mul_div_cancel_left₀, pow_mul, pow_succ', pow_zero, mul_one, mul_pow,
+      neg_mul, mul_neg, neg_neg]
+      ring_nf
+      simp only [mul_comm _ 2, pow_mul, I_sq, mul_neg]
+      simp only [neg_mul, neg_neg, mul_one]
+    · simp [e, pow_mul, pow_add, mul_pow]
+  have r : ∀ a b c d : ℂ, (a - b) - (c - d) = (a - c) - (b - d) := fun _ _ _ _ ↦ by ring
+  rw [sin, e, ← sub_div, ← sub_mul, r, norm_div, Complex.norm_two, div_le_iff₀ (by norm_num),
+    norm_mul, Complex.norm_I, mul_one]
+  refine le_trans (norm_sub_le _ _) ?_
+  refine le_trans (add_le_add (Complex.exp_bound (x := -z * I) (by simpa) (by omega))
+    (Complex.exp_bound (x := z * I) (by simpa) (by omega))) (le_of_eq ?_)
+  simp only [norm_mul, Complex.norm_I, norm_neg, Nat.succ_eq_add_one,
+    Nat.cast_add, Nat.cast_mul]
+  ring_nf
+
+lemma Complex.cos_series_bound {z : ℂ} (z1 : ‖z‖ ≤ 1) {n : ℕ} (n0 : 0 < n) :
+    ‖cos z - ∑ k ∈ Finset.range n, (-1) ^ k * z ^ (2 * k) / (2 * k).factorial‖ ≤
+      ‖z‖ ^ (2 * n) * ((2 * n + 1) / ((2 * n).factorial * (2 * n))) := by
+  have e : ∑ k ∈ Finset.range n, (-1) ^ k * z ^ (2 * k) / (2 * k).factorial =
+      (∑ k ∈ Finset.range (2 * n), (z * I) ^ k / k.factorial +
+       ∑ k ∈ Finset.range (2 * n), (-z * I) ^ k / k.factorial) / 2 := by
+    simp only [← Finset.sum_add_distrib, Finset.sum_range_even n, Finset.sum_div]
+    refine Finset.sum_congr rfl fun k _ ↦ ?_
+    rcases Nat.even_or_odd' k with ⟨a, e | e⟩
+    · simp only [e, even_two, Even.mul_right, ↓reduceIte, ne_eq, OfNat.ofNat_ne_zero,
+        not_false_eq_true, mul_div_cancel_left₀, pow_mul, mul_pow, I_sq, mul_neg, mul_one, neg_mul,
+        Even.neg_pow, ← add_div, neg_pow' (z^2), mul_comm _ ((-1 : ℂ) ^ a), ← add_mul]
+      ring
+    · simp [e, pow_mul, pow_add, mul_pow, neg_div]
+  have r : ∀ a b c d : ℂ, (a + b) - (c + d) = (a - c) + (b - d) := fun _ _ _ _ ↦ by ring
+  rw [cos, e, ← sub_div, r, norm_div, Complex.norm_two, div_le_iff₀ (by norm_num)]
+  refine le_trans (norm_add_le _ _) ?_
+  refine le_trans (add_le_add (Complex.exp_bound (x := z * I) (by simpa) (by omega))
+    (Complex.exp_bound (x := -z * I) (by simpa) (by omega))) (le_of_eq ?_)
+  simp only [norm_mul, Complex.norm_I, norm_neg, Nat.succ_eq_add_one,
+    Nat.cast_add, Nat.cast_mul]
+  ring_nf
+
+lemma Real.sin_series_bound {x : ℝ} (x1 : |x| ≤ 1) (n : ℕ) :
+    |sin x - x * ∑ k ∈ Finset.range n, (-1) ^ k * x ^ (2 * k) / (2 * k + 1).factorial| ≤
+      |x| ^ (2 * n + 1) * ((2 * n + 2) / ((2 * n + 1).factorial * (2 * n + 1))) := by
+  have b := Complex.sin_series_bound (z := x) (by simpa only [Complex.norm_real]) n
+  simp only [← Complex.ofReal_sin, ← Complex.ofReal_pow, ← Complex.ofReal_one,
+    ← Complex.ofReal_neg, ← Complex.ofReal_mul, ← Complex.ofReal_natCast, ← Complex.ofReal_div,
+    ← Complex.ofReal_sum, ← Complex.ofReal_sub, Complex.norm_real] at b
+  exact b
+
+lemma Real.cos_series_bound {x : ℝ} (x1 : |x| ≤ 1) {n : ℕ} (n0 : 0 < n) :
+    |cos x - ∑ k ∈ Finset.range n, (-1) ^ k * x ^ (2 * k) / (2 * k).factorial| ≤
+      |x| ^ (2 * n) * ((2 * n + 1) / ((2 * n).factorial * (2 * n))) := by
+  have b := Complex.cos_series_bound (z := x) (by simpa only [Complex.norm_real]) n0
+  simp only [← Complex.ofReal_cos, ← Complex.ofReal_pow, ← Complex.ofReal_one,
+    ← Complex.ofReal_neg, ← Complex.ofReal_mul, ← Complex.ofReal_natCast, ← Complex.ofReal_div,
+    ← Complex.ofReal_sum, ← Complex.ofReal_sub, Complex.norm_real] at b
+  exact b
