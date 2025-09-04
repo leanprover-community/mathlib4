cfd816beeeeaec62a4df828fa757eac613f9045b
Paul Lezeau <lezeau@google.com>
2025-09-04 22:14:25 +0000

    feat: add char instances for RatFunc

--- Mathlib/Algebra/CharP/Algebra.lean
+++ Mathlib/Algebra/CharP/Algebra.lean
@@ -61,6 +61,10 @@ theorem charP_of_injective_algebraMap' (R A : Type*) [Field R] [Semiring A] [Alg
     [Nontrivial A] (p : ℕ) [CharP R p] : CharP A p :=
   charP_of_injective_algebraMap (algebraMap R A).injective p
 
+def charP_of_injective_algebraMap'' (R : Type*) {A : Type*} [CommRing R] [CommRing A] [Algebra R A]
+    [FaithfulSMul R A] (p : ℕ) [CharP R p] : CharP A p :=
+  charP_of_injective_ringHom (FaithfulSMul.algebraMap_injective R A) p
+
 /-- If a ring homomorphism `R →+* A` is injective and `R` has characteristic zero
 then so does `A`. -/
 theorem charZero_of_injective_ringHom {R A : Type*} [NonAssocSemiring R] [NonAssocSemiring A]
@@ -71,6 +75,10 @@ theorem charZero_of_injective_ringHom {R A : Type*} [NonAssocSemiring R] [NonAss
 theorem charZero_of_injective_algebraMap {R A : Type*} [CommSemiring R] [Semiring A] [Algebra R A]
     (h : Function.Injective (algebraMap R A)) [CharZero R] : CharZero A :=
   charZero_of_injective_ringHom h
+
+def charZero_of_injective_algebraMap' (R : Type*) {A : Type*} [CommRing R] [CommRing A]
+    [Algebra R A] [FaithfulSMul R A] [CharZero R] : CharZero A :=
+  charZero_of_injective_ringHom (FaithfulSMul.algebraMap_injective R A)
 
 /-- If `R →+* A` is injective, and `A` is of characteristic `p`, then `R` is also of
 characteristic `p`. Similar to `RingHom.charZero`. -/
@@ -113,6 +121,10 @@ as `R`. -/
 lemma expChar_of_injective_algebraMap {R A : Type*} [CommSemiring R] [Semiring A] [Algebra R A]
     (h : Function.Injective (algebraMap R A)) (q : ℕ) [ExpChar R q] : ExpChar A q :=
   expChar_of_injective_ringHom h q
+
+def expChar_of_injective_algebraMap'' (R : Type*) {A : Type*} [CommRing R] [CommRing A]
+    [Algebra R A] [FaithfulSMul R A] (q : ℕ) [ExpChar R q] : ExpChar A q :=
+  expChar_of_injective_ringHom (FaithfulSMul.algebraMap_injective R A) q
 
 /-!
 As an application, a `ℚ`-algebra has characteristic zero.
