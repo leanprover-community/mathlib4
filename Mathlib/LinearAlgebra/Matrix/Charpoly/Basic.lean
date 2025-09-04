50197ef0210fd3ecb4c8ad4c7b22a9adedf6399a
Lawrence Wu <lawrence.wu@harmonic.fun>
2025-09-04 14:17:25 -0700

    golf

--- Mathlib/LinearAlgebra/Matrix/Charpoly/Basic.lean
+++ Mathlib/LinearAlgebra/Matrix/Charpoly/Basic.lean
@@ -221,30 +221,30 @@ theorem aeval_self_charpoly (M : Matrix n n R) : aeval M M.charpoly = 0 := by
   -- Thus we have $χ_M(M) = 0$, which is the desired result.
   exact h
 
+
 -- This proof follows https://math.stackexchange.com/a/311362/315369
 /-- A version of `charpoly_mul_comm` for rectangular matrices. -/
 theorem charpoly_mul_comm' (A : Matrix m n R) (B : Matrix n m R) :
     X ^ Fintype.card n * (A * B).charpoly = X ^ Fintype.card m * (B * A).charpoly := by
   let M := fromBlocks (scalar m X) (A.map C) (B.map C) (1 : Matrix n n R[X])
   let N := fromBlocks (-1 : Matrix m m R[X]) 0 (B.map C) (-scalar n X)
-  have hMN : M * N = fromBlocks
-      (-scalar m X + (A * B).map C) (-(X : R[X]) • A.map C) 0 (-scalar n X) := by
+  have hMN :
+      M * N = fromBlocks (-scalar m X + (A * B).map C) (-(X : R[X]) • A.map C) 0 (-scalar n X) := by
     simp [M, N, fromBlocks_multiply, smul_eq_mul_diagonal, -diagonal_neg]
-  have hNM : N * M = fromBlocks (-scalar m X) (-A.map C)
-      0 ((B * A).map C - scalar n X) := by
+  have hNM : N * M = fromBlocks (-scalar m X) (-A.map C) 0 ((B * A).map C - scalar n X) := by
     simp [M, N, fromBlocks_multiply, -diagonal_neg, -scalar_apply,
       sub_eq_add_neg, scalar_comm, Commute.all]
   have hdet_MN : (M * N).det = (-1 : R[X]) ^ (Fintype.card m + Fintype.card n) *
-      (X ^ Fintype.card n * (scalar m X - (A * B).map C).det) := calc
-    _ = (-(scalar m X - (A * B).map C)).det * ((-1) ^ Fintype.card n * X ^ Fintype.card n) := by
-      simp [hMN, -diagonal_neg, det_neg, neg_add_eq_sub]
-    _ = _ := by simp [-neg_sub, det_neg]; ring
+      (X ^ Fintype.card n * (scalar m X - (A * B).map C).det) := by
+    rw [hMN, det_fromBlocks_zero₂₁, neg_add_eq_sub, ← neg_sub, det_neg]
+    simp
+    ring
   have hdet_NM : (N * M).det = (-1 : R[X]) ^ (Fintype.card m + Fintype.card n) *
-      (X ^ Fintype.card m * (scalar n X - (B * A).map C).det) := calc
-    _ = (-1) ^ Fintype.card m * X ^ Fintype.card m * (-(scalar n X - (B * A).map C)).det := by
-      simp [hNM, -diagonal_neg, det_neg]
-    _ = _ := by simp [-neg_sub, det_neg]; ring
-  simp only [charpoly, charmatrix, RingHom.mapMatrix_apply]
+      (X ^ Fintype.card m * (scalar n X - (B * A).map C).det) := by
+    rw [hNM, det_fromBlocks_zero₂₁, ← neg_sub, det_neg (_ - _)]
+    simp
+    ring
+  dsimp only [charpoly, charmatrix, RingHom.mapMatrix_apply]
   rw [← (isUnit_neg_one.pow _).isRegular.left.eq_iff, ← hdet_MN, ← hdet_NM, det_mul_comm]
 
 /-- A version of `charpoly_mul_comm'` for square matrices. -/
