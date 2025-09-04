a80f1af16acfc2ebca409b17d8e28309cb165ae9
damiano <adomani@gmail.com>
2025-09-04 11:48:15 +0000

    chore: add `NormedSpace` and `Deprecated/Order` module deprecations (#29293)
    Generated by the `#create_deprecated_module` command.

---
+++ Mathlib/Deprecated/Order.lean
@@ -0,0 +1,5 @@
+import Mathlib.Algebra.Order.GroupWithZero.Unbundled.Basic
+import Mathlib.Order.RelClasses
+import Mathlib.Tactic.Linter.DeprecatedModule
+
+deprecated_module (since := "2025-09-02")
