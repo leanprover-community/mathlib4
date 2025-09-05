b9f0e82aa8317fe7a97718e7b017eedc38d767ab
Javier Burroni <javier.burroni@gmail.com>
2025-09-05 11:01:31 -0400

    changed order of imports

--- Mathlib.lean
+++ Mathlib.lean
@@ -3497,8 +3497,8 @@ import Mathlib.Data.PNat.Equiv
 import Mathlib.Data.PNat.Factors
 import Mathlib.Data.PNat.Find
 import Mathlib.Data.PNat.Interval
-import Mathlib.Data.PNat.Order
 import Mathlib.Data.PNat.Notation
+import Mathlib.Data.PNat.Order
 import Mathlib.Data.PNat.Prime
 import Mathlib.Data.PNat.Xgcd
 import Mathlib.Data.PSigma.Order
