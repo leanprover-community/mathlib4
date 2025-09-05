79dc9517fe752e38670a2d936d079b7df0781ebc
staroperator <staroperator92@gmail.com>
2025-09-05 08:16:14 -0400

    update Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -6021,6 +6021,7 @@ import Mathlib.SetTheory.Surreal.Basic
 import Mathlib.SetTheory.Surreal.Dyadic
 import Mathlib.SetTheory.Surreal.Multiplication
 import Mathlib.SetTheory.ZFC.Basic
+import Mathlib.SetTheory.ZFC.Cardinal
 import Mathlib.SetTheory.ZFC.Class
 import Mathlib.SetTheory.ZFC.Ordinal
 import Mathlib.SetTheory.ZFC.PSet
