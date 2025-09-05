716f255c295057861bc052ea0e82c8f7953bf353
Nikolas Tapia <tapia@wias-berlin.de>
2025-09-05 12:47:11 +0000

    feat(Algebra/LieAdmissible/Defs): define Lie-admissible rings/algebras (#28496)
    define lie-admissible rings/algebras and prove they can be upgraded to lie-rings/algebras
    
    also show that pre-lie rings are lie-admissible

--- Mathlib.lean
+++ Mathlib.lean
@@ -771,6 +771,7 @@ import Mathlib.Algebra.NoZeroSMulDivisors.Basic
 import Mathlib.Algebra.NoZeroSMulDivisors.Defs
 import Mathlib.Algebra.NoZeroSMulDivisors.Pi
 import Mathlib.Algebra.NoZeroSMulDivisors.Prod
+import Mathlib.Algebra.NonAssoc.LieAdmissible.Defs
 import Mathlib.Algebra.NonAssoc.PreLie.Basic
 import Mathlib.Algebra.Notation
 import Mathlib.Algebra.Notation.Defs
