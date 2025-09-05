6df88517ad4237dfd8a9ef22da7d6b8bdf86df83
María Inés de Frutos-Fernández <88536493+mariainesdff@users.noreply.github.com>
2025-09-05 17:21:17 +0000

    feat(RingTheory/DividedPowers/RatAlgebra): add definitions (#22322)
    In this file we show that, for certain choices of a commutative (semi)ring `A` and an ideal `I` of
    `A`, the family of maps `ℕ → A → A` given by `fun n x ↦ x^n/n!` is a divided power structure on `I`.
    
    Co-authored-by: AntoineChambert-Loir <antoine.chambert-loir@u-paris.fr>
    Co-authored-by: mariainesdff <mariaines.dff@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -5390,6 +5390,7 @@ import Mathlib.RingTheory.DiscreteValuationRing.TFAE
 import Mathlib.RingTheory.Discriminant
 import Mathlib.RingTheory.DividedPowers.Basic
 import Mathlib.RingTheory.DividedPowers.DPMorphism
+import Mathlib.RingTheory.DividedPowers.RatAlgebra
 import Mathlib.RingTheory.DividedPowers.SubDPIdeal
 import Mathlib.RingTheory.DualNumber
 import Mathlib.RingTheory.EisensteinCriterion
