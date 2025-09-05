346c873abb14c6212f2932eaeec707ef36ed4c4c
Chris Hughes <33847686+ChrisHughes24@users.noreply.github.com>
2025-09-05 07:07:46 +0000

    feat(Nullstellensatz); Generalize nullstellensatz to use a different field for the polynomial ring (#28639)
    Co-authored-by: ChrisHughes24 <chrishughes24@gmail.com>

--- Mathlib/ModelTheory/Algebra/Ring/Definability.lean
+++ Mathlib/ModelTheory/Algebra/Ring/Definability.lean
@@ -26,7 +26,7 @@ open MvPolynomial Language BoundedFormula
 theorem mvPolynomial_zeroLocus_definable {ι K : Type*} [Field K]
     [CompatibleRing K] (S : Finset (MvPolynomial ι K)) :
     Set.Definable (⋃ p ∈ S, p.coeff '' p.support : Set K) Language.ring
-      (zeroLocus (Ideal.span (S : Set (MvPolynomial ι K)))) := by
+      (zeroLocus K (Ideal.span (S : Set (MvPolynomial ι K)))) := by
   rw [Set.definable_iff_exists_formula_sum]
   let p' := genericPolyMap (fun p : S => p.1.support)
   letI := Classical.decEq ι
@@ -39,11 +39,12 @@ theorem mvPolynomial_zeroLocus_definable {ι K : Type*} [Field K]
             simp only [Set.mem_iUnion]
             exact ⟨p.1.1, p.1.2, Set.mem_image_of_mem _ p.2.2⟩⟩) id)) 0), ?_⟩
   -- Squeezing this simp slows it down significantly. Please measure before removing.
-  simp? [Formula.Realize, Term.equal, Function.comp_def, p'] says
-    simp only [Finset.mem_coe, Formula.Realize, Term.equal, Term.relabel_relabel,
-    Function.comp_def, realize_iInf, realize_bdEqual, Term.realize_relabel, Sum.elim_inl,
-    realize_termOfFreeCommRing, lift_genericPolyMap, Sum.map_inr, id_eq, Sum.elim_inr, Sum.map_inl,
-    MvPolynomialSupportLEEquiv_symm_apply_coeff, realize_zero, Subtype.forall, p']
+  simp? [Formula.Realize, Term.equal, Function.comp_def, p', MvPolynomial.aeval_eq_eval₂Hom] says
+    simp only [Finset.mem_coe, aeval_eq_eval₂Hom, Algebra.algebraMap_self, coe_eval₂Hom, eval₂_id,
+      Formula.Realize, Term.equal, Term.relabel_relabel, Function.comp_def, realize_iInf,
+      realize_bdEqual, Term.realize_relabel, Sum.elim_inl, realize_termOfFreeCommRing,
+      lift_genericPolyMap, Sum.map_inr, id_eq, Sum.elim_inr, Sum.map_inl,
+      MvPolynomialSupportLEEquiv_symm_apply_coeff, realize_zero, Subtype.forall, p']
 
 end Ring
 
