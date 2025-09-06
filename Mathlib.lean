e132bfbdc2c01b14165b47a64e4236f3c376d778
Weiyi Wang <wwylele@gmail.com>
2025-09-05 22:37:18 -0400

    generalize

--- Mathlib.lean
+++ Mathlib.lean
@@ -3907,6 +3907,7 @@ import Mathlib.GroupTheory.Coxeter.Length
 import Mathlib.GroupTheory.Coxeter.Matrix
 import Mathlib.GroupTheory.Divisible
 import Mathlib.GroupTheory.DivisibleHull
+import Mathlib.GroupTheory.DivisibleHull2
 import Mathlib.GroupTheory.DoubleCoset
 import Mathlib.GroupTheory.EckmannHilton
 import Mathlib.GroupTheory.Exponent
