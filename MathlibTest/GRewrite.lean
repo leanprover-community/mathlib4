3de0a001561664be3c9a685363d69c05d54268e8
Yaël Dillies <yael.dillies@gmail.com>
2025-09-07 06:49:47 +0000

    refactor: make `PosMulMono` and alike custom classes In the current setup, users get confusing non-beta-reduced goals involving a subtype when trying to provide an instance of `PosMulMono`. This PR changes it to be a custom structure with the expected fields. This follows `PosSMulMono` and alike.

--- MathlibTest/GRewrite.lean
+++ MathlibTest/GRewrite.lean
@@ -215,7 +215,7 @@ the inequality goes in the wrong direction). -/
 /--
 error: Tactic `grewrite` failed: could not discharge x ≤ y using x ≥ y
 
-case h₁.h
+case h₁.hbc
 α : Type u_1
 inst✝² : CommRing α
 inst✝¹ : LinearOrder α
@@ -262,7 +262,7 @@ example {Prime : ℕ → Prop} {a a' : ℕ} (h₁ : Prime (a + 1)) (h₂ : a = a
 /--
 error: Tactic `grewrite` failed: could not discharge b ≤ a using a ≤ b
 
-case h₁.h
+case h₁.hbc
 α : Type u_1
 inst✝² : CommRing α
 inst✝¹ : LinearOrder α
