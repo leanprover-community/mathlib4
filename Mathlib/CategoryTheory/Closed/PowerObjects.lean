d8e94fe3b7e07928277d5e03ce1f2bd3964e827e
Klaus Gy <klausgy@gmail.com>
2025-09-07 12:32:13 +0200

    removed some imports

--- Mathlib/CategoryTheory/Closed/PowerObjects.lean
+++ Mathlib/CategoryTheory/Closed/PowerObjects.lean
@@ -4,8 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Klaus Gy
 -/
 import Mathlib.CategoryTheory.Limits.Shapes.Pullback.Equalizer
-import Mathlib.CategoryTheory.Monoidal.Cartesian.Basic
-import Mathlib.CategoryTheory.Subobject.Basic
 import Mathlib.CategoryTheory.Topos.Classifier
 /-!
 # Elementary Topos (in Elementary Form)
