ab161ba21d6d6a22b8d7d33897996f1e7ba7772d
Lawrence Wu <lawrence.wu@harmonic.fun>
2025-09-07 18:28:07 -0700

    chore: golf LinearMap.charpoly_toMatrix

--- Mathlib/LinearAlgebra/Charpoly/ToMatrix.lean
+++ Mathlib/LinearAlgebra/Charpoly/ToMatrix.lean
@@ -34,54 +34,26 @@ namespace LinearMap
 
 section Basic
 
-/- These attribute tweaks save ~ 2000 heartbeats in `LinearMap.charpoly_toMatrix`. -/
-attribute [-instance] instCoeOutOfCoeSort
-attribute [local instance 2000] RingHomClass.toNonUnitalRingHomClass
-attribute [local instance 2000] NonUnitalRingHomClass.toMulHomClass
-
 /-- `charpoly f` is the characteristic polynomial of the matrix of `f` in any basis. -/
 @[simp]
 theorem charpoly_toMatrix {ι : Type w} [DecidableEq ι] [Fintype ι] (b : Basis ι R M) :
     (toMatrix b b f).charpoly = f.charpoly := by
-  let A := toMatrix b b f
-  let b' := chooseBasis R M
+  unfold LinearMap.charpoly
+  set b' := chooseBasis R M
   let ι' := ChooseBasisIndex R M
-  let A' := toMatrix b' b' f
+  rw [← basis_toMatrix_mul_linearMap_toMatrix_mul_basis_toMatrix b b' b b']
+  set P := b.toMatrix b'
+  set A := toMatrix b' b' f
+  set Q := b'.toMatrix b
   let e := Basis.indexEquiv b b'
   let φ := reindexLinearEquiv R R e e
   let φ₁ := reindexLinearEquiv R R e (Equiv.refl ι')
   let φ₂ := reindexLinearEquiv R R (Equiv.refl ι') (Equiv.refl ι')
   let φ₃ := reindexLinearEquiv R R (Equiv.refl ι') e
-  let P := b.toMatrix b'
-  let Q := b'.toMatrix b
-  have hPQ : C.mapMatrix (φ₁ P) * C.mapMatrix (φ₃ Q) = 1 := by
-    rw [RingHom.mapMatrix_apply, RingHom.mapMatrix_apply, ← Matrix.map_mul,
-      reindexLinearEquiv_mul R R, Basis.toMatrix_mul_toMatrix_flip,
-      reindexLinearEquiv_one, ← RingHom.mapMatrix_apply, RingHom.map_one]
   calc
-    A.charpoly = (reindex e e A).charpoly := (charpoly_reindex _ _).symm
-    _ = det (scalar ι' X - C.mapMatrix (φ A)) := rfl
-    _ = det (scalar ι' X - C.mapMatrix (φ (P * A' * Q))) := by
-      rw [basis_toMatrix_mul_linearMap_toMatrix_mul_basis_toMatrix]
-    _ = det (scalar ι' X - C.mapMatrix (φ₁ P * φ₂ A' * φ₃ Q)) := by
-      rw [reindexLinearEquiv_mul, reindexLinearEquiv_mul]
-    _ = det (scalar ι' X - C.mapMatrix (φ₁ P) * C.mapMatrix A' * C.mapMatrix (φ₃ Q)) := by
-      simp [φ₁, φ₂, φ₃, ι']
-    _ = det (scalar ι' X * C.mapMatrix (φ₁ P) * C.mapMatrix (φ₃ Q) -
-          C.mapMatrix (φ₁ P) * C.mapMatrix A' * C.mapMatrix (φ₃ Q)) := by
-      rw [Matrix.mul_assoc ((scalar ι') X), hPQ, Matrix.mul_one]
-    _ = det (C.mapMatrix (φ₁ P) * scalar ι' X * C.mapMatrix (φ₃ Q) -
-          C.mapMatrix (φ₁ P) * C.mapMatrix A' * C.mapMatrix (φ₃ Q)) := by
-      rw [scalar_commute _ commute_X]
-    _ = det (C.mapMatrix (φ₁ P) * (scalar ι' X - C.mapMatrix A') * C.mapMatrix (φ₃ Q)) := by
-      rw [← Matrix.sub_mul, ← Matrix.mul_sub]
-    _ = det (C.mapMatrix (φ₁ P)) * det (scalar ι' X - C.mapMatrix A') * det (C.mapMatrix (φ₃ Q)) :=
-      by rw [det_mul, det_mul]
-    _ = det (C.mapMatrix (φ₁ P)) * det (C.mapMatrix (φ₃ Q)) * det (scalar ι' X - C.mapMatrix A') :=
-      by ring
-    _ = det (scalar ι' X - C.mapMatrix A') := by
-      rw [← det_mul, hPQ, det_one, one_mul]
-    _ = f.charpoly := rfl
+    (P * A * Q).charpoly = (φ (P * A * Q)).charpoly := (charpoly_reindex _ _).symm
+    _ = (φ₁ P * φ₂ A * φ₃ Q).charpoly := by rw [reindexLinearEquiv_mul, reindexLinearEquiv_mul]
+    _ = A.charpoly := by rw [charpoly_mul_comm, ← mul_assoc]; simp [P, Q, φ₁, φ₂, φ₃]
 
 lemma charpoly_prodMap (f₁ : M₁ →ₗ[R] M₁) (f₂ : M₂ →ₗ[R] M₂) :
     (f₁.prodMap f₂).charpoly = f₁.charpoly * f₂.charpoly := by
