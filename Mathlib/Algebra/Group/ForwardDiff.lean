2cf74bb84a2f1f08a86f8a4281d0ab375dd85741
jasin <219660779@qq.com>
2025-09-08 19:12:15 +0800

    update fwdDiff_iter_degree_add_one_eq_zero

--- Mathlib/Algebra/Group/ForwardDiff.lean
+++ Mathlib/Algebra/Group/ForwardDiff.lean
@@ -270,7 +270,7 @@ theorem Polynomial.fwdDiff_iter_eq_zero_of_degree_lt {P : R[X]} {n : ℕ} (hP : 
 
 theorem Polynomial.fwdDiff_iter_degree_add_one_eq_zero (P : R[X]) :
     Δ_[1]^[P.natDegree + 1] P.eval = 0 := by
-    let hP : P.natDegree < P.natDegree + 1 := Nat.lt_succ_self P.natDegree
+    have hP : P.natDegree < P.natDegree + 1 := Nat.lt_succ_self P.natDegree
     exact Polynomial.fwdDiff_iter_eq_zero_of_degree_lt hP
 
 /--
