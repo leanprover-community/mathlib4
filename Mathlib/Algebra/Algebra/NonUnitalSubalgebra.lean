23590d223a02a3f4aa4bacbb42036652d36358f5
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2025-09-08 21:59:51 +0200

    golf

--- Mathlib/Algebra/Algebra/NonUnitalSubalgebra.lean
+++ Mathlib/Algebra/Algebra/NonUnitalSubalgebra.lean
@@ -153,12 +153,7 @@ Useful to fix definitional equalities. -/
 protected def copy (S : NonUnitalSubalgebra R A) (s : Set A) (hs : s = ↑S) :
     NonUnitalSubalgebra R A :=
   { S.toNonUnitalSubsemiring.copy s hs with
-    smul_mem' := fun r a ha => by
-      simp_rw [AddSubsemigroup.mem_carrier, AddSubmonoid.mem_toSubsemigroup,
-        NonUnitalSubsemiring.mem_toAddSubmonoid, ← SetLike.mem_coe,
-        NonUnitalSubsemiring.coe_copy _ _ hs] at ha ⊢
-      rw [hs] at ha ⊢
-      exact S.smul_mem' r ha }
+    smul_mem' r a := by simpa [hs] using S.smul_mem r }
 
 @[simp]
 theorem coe_copy (S : NonUnitalSubalgebra R A) (s : Set A) (hs : s = ↑S) :
