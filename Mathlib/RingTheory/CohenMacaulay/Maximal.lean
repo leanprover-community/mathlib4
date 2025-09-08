d6f661b6e8166d752b658945d4239020394bcb0e
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2025-09-08 19:46:40 +0800

    Update Maximal.lean

--- Mathlib/RingTheory/CohenMacaulay/Maximal.lean
+++ Mathlib/RingTheory/CohenMacaulay/Maximal.lean
@@ -70,9 +70,47 @@ lemma isCohenMacaulayLocalRing_of_isRegularLocalRing [IsRegularLocalRing R] :
   · rw [Finset.length_toList, this]
     rfl
 
+lemma isField_of_isRegularLocalRing_of_dimension_zero [IsRegularLocalRing R]
+    (h : ringKrullDim R = 0) : IsField R := by
+  rw [IsLocalRing.isField_iff_maximalIdeal_eq, ← Submodule.spanRank_eq_zero_iff_eq_bot]
+  have := (isRegularLocalRing_def R).mp ‹_›
+  simp only [h, Nat.cast_eq_zero] at this
+  have fg : (maximalIdeal R).FG := (isNoetherianRing_iff_ideal_fg R).mp inferInstance _
+  simpa [Submodule.fg_iff_spanRank_eq_spanFinrank.mpr fg] using this
+
 theorem free_of_isMaximalCohenMacaulay_of_isRegularLocalRing [IsRegularLocalRing R] [Small.{v} R]
     (M : ModuleCat.{v} R) [Module.Finite R M] [M.IsMaximalCohenMacaulay] : Module.Free R M := by
   rcases exist_nat_eq R with ⟨n, hn⟩
   induction' n with n ih generalizing R M
-  · sorry
-  · sorry
+  · have : IsField R := isField_of_isRegularLocalRing_of_dimension_zero hn
+    sorry
+  · obtain ⟨x, xmem, xnmem⟩ : ∃ x ∈ maximalIdeal R, x ∉ (maximalIdeal R) ^ 2 := by
+      by_contra! ge
+      have : IsField R := by
+        simpa only [← subsingleton_cotangentSpace_iff, Ideal.cotangent_subsingleton_iff,
+          IsIdempotentElem] using le_antisymm Ideal.mul_le_right (le_of_le_of_eq ge (pow_two _))
+      rw [ringKrullDim_eq_zero_of_isField this, ← Nat.cast_zero, Nat.cast_inj] at hn
+      exact Nat.zero_ne_add_one n hn
+    let _ := (quotient_span_singleton R xmem xnmem).1
+    have dim : ringKrullDim (R ⧸ Ideal.span {x}) = n := by
+      have := (quotient_span_singleton R xmem xnmem).2
+      simp only [hn, Nat.cast_add, Nat.cast_one] at this
+      exact (withBotENat_add_coe_cancel _ _ 1).mp this
+    have reg : IsSMulRegular M x := by
+      by_contra h
+      have := (Set.ext_iff.mp (biUnion_associatedPrimes_eq_compl_regular R M) x).mpr h
+      simp only [Set.mem_iUnion, SetLike.mem_coe, exists_prop] at this
+      rcases this with ⟨p, pass, mem⟩
+      --use `depth R ≤ dim(R ⧸ p)`
+      sorry
+    have : Module.Finite (R ⧸ Ideal.span {x}) (QuotSMulTop x M) := by
+      --Module.Finite.of_surjective
+      sorry
+    have max : (ModuleCat.of (R ⧸ Ideal.span {x}) (QuotSMulTop x ↑M)).IsMaximalCohenMacaulay := by
+      rw [isMaximalCohenMacaulay_def]
+      --need to switch ring
+      --RingTheory.Sequence.isWeaklyRegular_map_algebraMap_iff
+      sorry
+    have free := ih (ModuleCat.of (R ⧸ Ideal.span {x}) (QuotSMulTop x M)) dim
+    --https://stacks.math.columbia.edu/tag/00NS
+    sorry
