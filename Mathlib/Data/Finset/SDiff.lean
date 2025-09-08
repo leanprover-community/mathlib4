a72ac31c05fe4775914a69a250b8ece6b987aa63
Kim Morrison <kim@tqft.net>
2025-09-08 18:05:54 +1000

    shake --fix

--- Mathlib/Data/Finset/SDiff.lean
+++ Mathlib/Data/Finset/SDiff.lean
@@ -3,7 +3,8 @@ Copyright (c) 2015 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Leonardo de Moura, Jeremy Avigad, Minchao Wu, Mario Carneiro
 -/
-import Mathlib.Data.Finset.Lattice.Lemmas
+import Mathlib.Data.Finset.Insert
+import Mathlib.Data.Finset.Lattice.Basic
 
 /-!
 # Difference of finite sets
