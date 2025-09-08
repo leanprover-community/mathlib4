a5a9c1d71cfe943e0b706c43573cdea9854ac6f9
Harald Husum <harald.husum@intelecy.com>
2025-09-08 09:42:14 +0200

    Fix

--- Mathlib/Computability/AkraBazzi/AkraBazzi.lean
+++ Mathlib/Computability/AkraBazzi/AkraBazzi.lean
@@ -62,7 +62,7 @@ open Finset Real Filter Asymptotics
 open scoped Topology
 
 /-!
-#### Definition of Akra–Bazzi recurrences
+### Definition of Akra–Bazzi recurrences
 
 This section defines the predicate `AkraBazziRecurrence T g a b r` which states that `T`
 satisfies the recurrence
@@ -81,7 +81,7 @@ local notation "ε" => smoothingFn
 
 
 /-!
-#### Technical lemmas
+### Technical lemmas
 
 The next several lemmas are technical results leading up to `rpow_p_mul_one_sub_smoothingFn_le` and
 `rpow_p_mul_one_add_smoothingFn_ge`, which are key steps in the main proof.
@@ -446,7 +446,7 @@ lemma rpow_p_mul_one_add_smoothingFn_ge :
   exact hn
 
 /-!
-#### Main proof
+### Main proof
 
 This final section proves the Akra–Bazzi theorem.
 -/
