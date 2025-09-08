18eb7ee4b70e63d8c6f36ac9ded101e2efc68496
Yongshun Ye <ShreckYe@gmail.com>
2025-09-08 23:05:20 +0800

    More `example`s

--- TanHalfAngleSubstitutions.lean
+++ TanHalfAngleSubstitutions.lean
@@ -2,7 +2,21 @@ import Mathlib
 
 open Real
 
-example {x : ℝ} (h : ∀ k : ℤ, x ≠ (2 * k + 1) * π / 2) :
+variable {x : ℝ}
+
+example (h : ∀ k : ℤ, x ≠ (2 * k + 1) * π / 2) :
     sin x = (2 * tan (x / 2)) / (1 + tan (x / 2) ^ 2) := by
   --apply?
   sorry
+
+example : sin x = (2 * tan (x / 2)) / (1 + tan (x / 2) ^ 2) := by
+  sorry
+
+example (h : ∀ k : ℤ, x ≠ (2 * k + 1) * π / 2) :
+    cos x = (1 - tan (x / 2) ^ 2) / (1 + tan (x / 2) ^ 2) := by
+  --apply?
+  sorry
+
+example : tan x = (2 * tan (x / 2)) / (1 - tan (x / 2) ^ 2) := by
+  conv_lhs => rw [show x = 2 * (x / 2) by group]
+  exact tan_two_mul
