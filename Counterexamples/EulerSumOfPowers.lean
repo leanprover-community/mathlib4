032c9e42532a391e910d13bdd90c206fcc4effca
Michael Stoll <99838730+MichaelStollBayreuth@users.noreply.github.com>
2025-09-09 02:14:14 +0300

    add Michael Stoll as coauthor

--- Counterexamples/EulerSumOfPowers.lean
+++ Counterexamples/EulerSumOfPowers.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2025 Snir Broshi. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Snir Broshi
+Authors: Snir Broshi, Michael Stoll
 -/
 import Mathlib.NumberTheory.FLT.Three
 
