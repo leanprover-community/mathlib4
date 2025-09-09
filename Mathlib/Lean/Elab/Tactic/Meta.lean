beb02307becde4b105774a6569f01e8ae1f41083
Kim Morrison <kim@tqft.net>
2025-09-09 17:20:02 +1000

    shake

--- Mathlib/Lean/Elab/Tactic/Meta.lean
+++ Mathlib/Lean/Elab/Tactic/Meta.lean
@@ -3,7 +3,7 @@ Copyright (c) 2025 Lean FRO, LLC. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kim Morrison
 -/
-import Lean.Elab.Tactic.Meta
+import Lean.Elab.SyntheticMVars
 
 /-!
 # Additions to `Lean.Elab.Tactic.Meta`
