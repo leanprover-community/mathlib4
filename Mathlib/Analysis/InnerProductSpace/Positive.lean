0494dc7c1db0496f803a166e5a06cf27152f283c
Monica Omar <23701951+themathqueen@users.noreply.github.com>
2025-09-09 12:38:36 +0000

    Apply suggestions from code review
    Co-authored-by: Oliver Nash <7734364+ocfnash@users.noreply.github.com>

--- Mathlib/Analysis/InnerProductSpace/Positive.lean
+++ Mathlib/Analysis/InnerProductSpace/Positive.lean
@@ -171,12 +171,13 @@ theorem IsIdempotentElem.isPositive_iff_isSymmetric {T : E →ₗ[𝕜] E} (hT :
 
 open scoped ComplexOrder in
 /-- `A.toEuclideanLin` is positive if and only if `A` is positive semi-definite. -/
-theorem _root_.Matrix.isPositive_toEuclideanLin_iff {n : Type*} [Fintype n] [DecidableEq n]
+@[simp] theorem _root_.Matrix.isPositive_toEuclideanLin_iff {n : Type*} [Fintype n] [DecidableEq n]
     {A : Matrix n n 𝕜} : A.toEuclideanLin.IsPositive ↔ A.PosSemidef := by
   simp_rw [LinearMap.IsPositive, ← Matrix.isHermitian_iff_isSymmetric, inner_re_symm,
     EuclideanSpace.inner_eq_star_dotProduct, Matrix.piLp_ofLp_toEuclideanLin, Matrix.toLin'_apply,
     dotProduct_comm (A.mulVec _), Matrix.PosSemidef, and_congr_right_iff, RCLike.nonneg_iff (K:=𝕜)]
-  intro hA; simp_rw [hA.im_star_dotProduct_mulVec_self, and_true]; rfl
+  refine fun hA ↦ (EuclideanSpace.equiv n 𝕜).forall_congr' fun x ↦ ?_
+  simp [hA.im_star_dotProduct_mulVec_self]
 
 theorem isPositive_linearIsometryEquiv_conj_iff {T : E →ₗ[𝕜] E} (f : E ≃ₗᵢ[𝕜] F) :
     IsPositive (f.toLinearMap ∘ₗ T ∘ₗ f.symm.toLinearMap) ↔ IsPositive T := by
