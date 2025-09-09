55a66e5c8a194b0c7622af5136385204bf3641e1
Izan Beltran Ferreiro <izanbf1803@gmail.com>
2025-09-09 20:21:18 +0200

    Formalize statement and prove eq_of_invOf_add_eq_invOf_add_invOf
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- Mathlib/Algebra/Ring/Invertible.lean
+++ Mathlib/Algebra/Ring/Invertible.lean
@@ -45,6 +45,35 @@ theorem invOf_sub_invOf [Ring R] (a b : R) [Invertible a] [Invertible b] :
     ⅟a - ⅟b = ⅟a * (b - a) * ⅟b := by
   rw [mul_sub, invOf_mul_self, sub_mul, one_mul, mul_assoc, mul_invOf_self, mul_one]
 
+theorem eq_of_invOf_add_eq_invOf_add_invOf [Ring R] (a b : R) [Invertible a] [Invertible b]
+    [Invertible (a + b)] : ⅟(a + b) = ⅟a + ⅟b → a * ⅟b * a = b * ⅟a * b := by
+  intro h
+  have h_neg_identity : -1 = ⅟a * b + ⅟b * a := by
+    have : 1 = 2 + ⅟a * b + ⅟b * a := by
+      rw [← invOf_mul_self (a + b), h, add_mul, mul_add, mul_add, invOf_mul_self a,
+          invOf_mul_self b, ← add_assoc, add_assoc 1 _, add_comm 1 _, add_assoc 2 _,
+          add_comm 2 _, add_assoc, one_add_one_eq_two]
+    apply (add_left_inj 2).mp
+    conv => lhs; rw [← one_add_one_eq_two, ← add_assoc, neg_add_cancel, zero_add]
+    rw [add_comm, ← add_assoc]
+    exact this
+  let helper {x y : R} [Invertible x] [Invertible y] (h' : -1 = ⅟x * y + ⅟y * x)
+      : -(y + x) = x * ⅟y * x := by
+    apply (add_left_inj y).mp
+    rw [(by simp : -(y + x) + y = -x)]
+    conv => rhs; rhs; rw [(by simp : y = x * ⅟x * y)]
+    rw [mul_assoc, mul_assoc, ← mul_add x]
+    rw [(by simp : -x = x * (-1))]
+    apply (mul_right_inj_of_invertible x).mpr
+    rw [add_comm]
+    exact h'
+  have h_a_binv_a : -(b + a) = a * ⅟b * a := by
+    exact helper h_neg_identity
+  have h_b_ainv_b : -(a + b) = b * ⅟a * b := by
+    rw [add_comm] at h_neg_identity
+    exact helper h_neg_identity
+  rw [← h_a_binv_a, ← h_b_ainv_b, add_comm]
+
 /-- A version of `inv_add_inv'` for `Ring.inverse`. -/
 theorem Ring.inverse_add_inverse [Semiring R] {a b : R} (h : IsUnit a ↔ IsUnit b) :
     Ring.inverse a + Ring.inverse b = Ring.inverse a * (a + b) * Ring.inverse b := by
