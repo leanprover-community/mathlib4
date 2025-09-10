3528bfebe95a940a9a5bcbacde0fd826e465af7e
Jingting Wang <wangjt2020@163.com>
2025-09-10 18:26:58 +0800

    Update Mathlib/RingTheory/LocalRing/MaximalIdeal/Basic.lean
    Co-authored-by: Kenny Lau <kc_kennylau@yahoo.com.hk>

--- Mathlib/RingTheory/LocalRing/MaximalIdeal/Basic.lean
+++ Mathlib/RingTheory/LocalRing/MaximalIdeal/Basic.lean
@@ -129,7 +129,7 @@ end IsLocalRing
 theorem IsLocalRing.maximalIdeal_eq_bot {R : Type*} [Field R] : IsLocalRing.maximalIdeal R = ⊥ :=
   IsLocalRing.isField_iff_maximalIdeal_eq.mp (Field.toIsField R)
 
-lemma Subring.isLocalRing_of_unit {R : Type*} [CommRing R] [IsLocalRing R] (S : Subring R)
+lemma Subring.isLocalRing_of_unit {R : Type*} [Ring R] [IsLocalRing R] (S : Subring R)
     (h_unit : ∀ (x : R) (hx : x ∈ S), IsUnit x → IsUnit (⟨x, hx⟩ : S)) :
     IsLocalRing S where
   isUnit_or_isUnit_of_add_one {x y} hxy :=
