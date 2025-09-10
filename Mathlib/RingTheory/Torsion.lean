fa2fd229d63bf2bf6c28a5df494c954fd0726a55
Yakov Pechersky <ffxen158@gmail.com>
2025-09-10 17:36:31 -0400

    feat(RingTheory/Torsion): torsion = union of roots of unity
    with additional API for Associates and rootsOfUnity sups

---
+++ Mathlib/RingTheory/Torsion.lean
@@ -0,0 +1,38 @@
+/-
+Copyright (c) 2025 Yakov Pechersky. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Yakov Pechersky
+-/
+import Mathlib.GroupTheory.Torsion
+import Mathlib.RingTheory.RootsOfUnity.Basic
+
+/-!
+# Torsion groups are the union of all subgroups of roots of unity
+
+-/
+
+variable (M : Type*) [CommMonoid M]
+
+lemma rootsOfUnity_le_torsion (k : ℕ) [NeZero k] :
+    rootsOfUnity k M ≤ CommGroup.torsion Mˣ := by
+  intro
+  simp only [mem_rootsOfUnity, CommGroup.mem_torsion, isOfFinOrder_iff_pow_eq_one]
+  intro h
+  exact ⟨_, Nat.pos_of_ne_zero NeZero.out, h⟩
+
+lemma torsion_eq_biSup_rootsOfUnity :
+    CommGroup.torsion Mˣ = ⨆ (n : ℕ) (_hn : n ≠ 0), rootsOfUnity n M := by
+  refine le_antisymm ?_ ?_
+  · intro x hx
+    rw [CommGroup.mem_torsion] at hx
+    -- without `(i := ...)`, timeout at `isDefEq`
+    rw [Subgroup.mem_biSup_of_directedOn (i := orderOf x) (orderOf_ne_zero_iff.mpr hx)]
+    · use orderOf x
+      simp [hx]
+    · intro a ha b hb
+      use a.lcm b
+      simp_all [Function.onFun, rootsOfUnity_le_of_dvd, Nat.dvd_lcm_left, Nat.dvd_lcm_right]
+  · simp only [iSup_le_iff]
+    intro n hn
+    have : NeZero n := ⟨hn⟩
+    exact rootsOfUnity_le_torsion M n
