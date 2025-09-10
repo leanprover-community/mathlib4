477c1d81a4e67b1ae2864f6931b12c29a5751cbc
Violeta Hernández <vi.hdz.p@gmail.com>
2025-09-10 07:07:05 -0600

    Update WellFounded.lean

--- Mathlib/Order/UpperLower/WellFounded.lean
+++ Mathlib/Order/UpperLower/WellFounded.lean
@@ -3,7 +3,7 @@ Copyright (c) 2025 Violeta Hernández Palacios. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Violeta Hernández Palacios
 -/
-import Mathlib.Order.UpperLower.Basic
+import Mathlib.Order.UpperLower.CompleteLattice
 import Mathlib.Order.WellQuasiOrder
 
 /-!
@@ -12,13 +12,13 @@ import Mathlib.Order.WellQuasiOrder
 We show that `LowerSet α` and `UpperSet α` are well-founded whenever `α` is a WQO.
 -/
 
-variable [Preorder α] [WellQuasiOrderedLE α]
+variable {α : Type*} [Preorder α] [WellQuasiOrderedLE α]
 
 instance : WellFoundedLT (LowerSet α) where
   wf := by
     rw [RelEmbedding.wellFounded_iff_isEmpty]
     refine ⟨fun f ↦ ?_⟩
-    have hf' (n) : ((f n).1 \ f (n + 1)).Nonempty := diff_nonempty.2 (f.2.2 n.lt_succ_self).not_ge
+    have hf' (n : ℕ) := Set.diff_nonempty.2 (f.2.2 n.lt_succ_self).not_ge
     choose g hg using hf'
     obtain ⟨m, n, h, h'⟩ := wellQuasiOrdered_le g
     apply not_not.2 <| (f n).lower' h' (hg n).1
