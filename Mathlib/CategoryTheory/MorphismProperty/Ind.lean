c7aff0f549fe34ff85f508187adda620d7551511
Christian Merten <christian@merten.dev>
2025-09-10 12:07:23 +0200

    review suggestions

--- Mathlib/CategoryTheory/MorphismProperty/Ind.lean
+++ Mathlib/CategoryTheory/MorphismProperty/Ind.lean
@@ -3,6 +3,7 @@ Copyright (c) 2025 Christian Merten. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Christian Merten
 -/
+import Mathlib.CategoryTheory.Comma.LocallySmall
 import Mathlib.CategoryTheory.Limits.Preserves.Over
 import Mathlib.CategoryTheory.MorphismProperty.Comma
 import Mathlib.CategoryTheory.MorphismProperty.Limits
@@ -20,12 +21,11 @@ We show that `ind P` inherits stability properties from `P`.
 
 - `CategoryTheory.MorphismProperty.ind`: `f` satisfies `ind P` if `f` is a filtered colimit of
   morphisms in `P`.
-- `CategoryTheory.MorphismProperty.compact`: `f : X ⟶ Y` is compact if `Y` viewed as an object of
-  `Under X` is compact.
 
 ## Main results:
 
-- `CategoryTheory.MorphismProperty.ind_ind`: If `P` implies compact, then `P.ind.ind = P.ind`.
+- `CategoryTheory.MorphismProperty.ind_ind`: If `P` implies finitely presentable, then
+  `P.ind.ind = P.ind`.
 
 ## TODOs:
 
@@ -33,7 +33,7 @@ We show that `ind P` inherits stability properties from `P`.
 - Show `ind P` is stable under composition if `P` spreads out (Christian).
 -/
 
-universe v u
+universe w v u
 
 namespace CategoryTheory.MorphismProperty
 
@@ -54,18 +54,13 @@ See `CategoryTheory.MorphismProperty.ind_iff_ind_under_mk` for an equivalent cha
 in terms of `Y` as an object of `Under X`.
 -/
 def ind (P : MorphismProperty C) : MorphismProperty C :=
-  fun X Y f ↦ ∃ (J : Type v) (_ : SmallCategory J) (_ : IsFiltered J)
+  fun X Y f ↦ ∃ (J : Type w) (_ : SmallCategory J) (_ : IsFiltered J)
     (D : J ⥤ C) (t : (Functor.const J).obj X ⟶ D) (s : D ⟶ (Functor.const J).obj Y)
     (_ : IsColimit (Cocone.mk _ s)), ∀ j, P (t.app j) ∧ t.app j ≫ s.app j = f
 
-variable (C) in
-/-- A morphism `f : X ⟶ Y` is compact if `Y` is compact viewed as an object of `Under X`. -/
-def compact : MorphismProperty C :=
-  fun _ _ f ↦ ObjectProperty.compact _ (CategoryTheory.Under.mk f)
-
-lemma exists_hom_of_compact {J : Type v} [SmallCategory J] [IsFiltered J] {D : J ⥤ C} {c : Cocone D}
-    (hc : IsColimit c) {X A : C} {p : X ⟶ A} (hp : compact C p) (s : (Functor.const J).obj X ⟶ D)
-    (f : A ⟶ c.pt) (h : ∀ (j : J), s.app j ≫ c.ι.app j = p ≫ f) :
+lemma exists_hom_of_isFinitelyPresentable {J : Type w} [SmallCategory J] [IsFiltered J] {D : J ⥤ C}
+    {c : Cocone D} (hc : IsColimit c) {X A : C} {p : X ⟶ A} (hp : isFinitelyPresentable.{w} C p)
+    (s : (Functor.const J).obj X ⟶ D) (f : A ⟶ c.pt) (h : ∀ (j : J), s.app j ≫ c.ι.app j = p ≫ f) :
     ∃ (j : J) (q : A ⟶ D.obj j), p ≫ q = s.app j ∧ q ≫ c.ι.app j = f :=
   hp.exists_hom_of_isColimit_under hc _ s _ h
 
@@ -79,7 +74,7 @@ lemma le_ind : P ≤ P.ind := by
 variable {P}
 
 lemma ind_iff_ind_under_mk {X Y : C} (f : X ⟶ Y) :
-    P.ind f ↔ P.underObj.ind (CategoryTheory.Under.mk f) := by
+    ind.{w} P f ↔ ObjectProperty.ind.{w} P.underObj (CategoryTheory.Under.mk f) := by
   refine ⟨fun ⟨J, _, _, D, t, s, hs, hst⟩ ↦ ?_, fun ⟨J, _, _, pres, hpres⟩ ↦ ?_⟩
   · refine ⟨J, ‹_›, ‹_›, ⟨Under.lift D t, ?_, ?_⟩, ?_⟩
     · exact { app j := CategoryTheory.Under.homMk (s.app j) (by simp [hst]) }
@@ -93,7 +88,8 @@ lemma ind_iff_ind_under_mk {X Y : C} (f : X ⟶ Y) :
     · exact hpres j
     · simp
 
-lemma underObj_ind_eq_ind_underObj (X : C) : P.ind.underObj (X := X) = P.underObj.ind := by
+lemma underObj_ind_eq_ind_underObj (X : C) :
+    underObj (ind.{w} P) (X := X) = ObjectProperty.ind.{w} P.underObj := by
   ext f
   simp [underObj, show f = CategoryTheory.Under.mk f.hom from rfl, ind_iff_ind_under_mk]
 
@@ -111,7 +107,8 @@ instance [P.RespectsIso] : P.ind.RespectsIso where
     · simp [reassoc_of% (hst j).2]
 
 lemma ind_underObj_pushout {X Y : C} (g : X ⟶ Y) [HasPushouts C] [P.IsStableUnderCobaseChange]
-    {f : Under X} (hf : P.underObj.ind f) : P.underObj.ind ((Under.pushout g).obj f) := by
+    {f : Under X} (hf : ObjectProperty.ind.{w} P.underObj f) :
+    ObjectProperty.ind.{w} P.underObj ((Under.pushout g).obj f) := by
   obtain ⟨J, _, _, pres, hpres⟩ := hf
   use J, inferInstance, inferInstance, pres.map (Under.pushout g)
   intro i
@@ -122,10 +119,12 @@ instance [P.IsStableUnderCobaseChange] [HasPushouts C] : P.ind.IsStableUnderCoba
   rw [ind_iff_ind_under_mk] at hf ⊢
   exact ind_underObj_pushout g hf
 
-/-- `ind` is idempotent if `P` implies compact. -/
-lemma ind_ind (hp : P ≤ compact C) : P.ind.ind = P.ind := by
+/-- `ind` is idempotent if `P` implies finitely presentable. -/
+lemma ind_ind (hp : P ≤ isFinitelyPresentable.{w} C) [LocallySmall.{w} C] :
+    ind.{w} (ind.{w} P) = ind.{w} P := by
   refine le_antisymm (fun X Y f hf ↦ ?_) P.ind.le_ind
-  have : P.underObj ≤ ObjectProperty.compact (Under X) := fun f hf ↦ hp _ hf
-  simpa [ind_iff_ind_under_mk, underObj_ind_eq_ind_underObj, ObjectProperty.ind_ind this] using hf
+  have : P.underObj ≤ ObjectProperty.isFinitelyPresentable.{w} (Under X) := fun f hf ↦ hp _ hf
+  simpa [ind_iff_ind_under_mk, underObj_ind_eq_ind_underObj,
+    ObjectProperty.ind_ind.{w} this] using hf
 
 end CategoryTheory.MorphismProperty
--- Mathlib/CategoryTheory/ObjectProperty/Ind.lean
+++ Mathlib/CategoryTheory/ObjectProperty/Ind.lean
@@ -15,18 +15,18 @@ Given an object property `P`, we define an object property `ind P` that is satis
 
 - `CategoryTheory.ObjectProperty.ind`: `X` satisfies `ind P` if `X` is a filtered colimit of `Xᵢ`
   for `Xᵢ` in `P`.
-- `CategoryTheory.ObjectProperty.compact`: `X` is compact if it is finitely presentable.
 
 ## Main results
 
-- `CategoryTheory.ObjectProperty.ind_ind`: If `P` implies compact, then `P.ind.ind = P.ind`.
+- `CategoryTheory.ObjectProperty.ind_ind`: If `P` implies finitely presentable, then
+  `P.ind.ind = P.ind`.
 
 ## TODOs:
 
 - Dualise to obtain `CategoryTheory.ObjectProperty.pro`.
 -/
 
-universe v u
+universe w v u
 
 namespace CategoryTheory.ObjectProperty
 
@@ -36,26 +36,25 @@ variable {C : Type u} [Category.{v} C] {P : ObjectProperty C}
 
 /-- `X` satisfies `ind P` if `X` is a filtered colimit of `Xᵢ` for `Xᵢ` in `P`. -/
 def ind (P : ObjectProperty C) : ObjectProperty C :=
-  fun X ↦ ∃ (J : Type v) (_ : SmallCategory J) (_ : IsFiltered J)
+  fun X ↦ ∃ (J : Type w) (_ : SmallCategory J) (_ : IsFiltered J)
     (pres : ColimitPresentation J X), ∀ i, P (pres.diag.obj i)
 
 variable (P) in
-lemma le_ind : P ≤ P.ind := by
+lemma le_ind : P ≤ ind.{w} P := by
   intro X hX
   exact ⟨PUnit, inferInstance, inferInstance, .self X, by simpa⟩
 
 instance : P.ind.IsClosedUnderIsomorphisms where
   of_iso {X Y} e := fun ⟨J, _, _, pres, h⟩ ↦ ⟨J, ‹_›, ‹_›, pres.ofIso e, h⟩
 
-variable (C) in
-/-- The compact objects of `C` are the finitely presentable ones. -/
-def compact : ObjectProperty C := fun X ↦ IsFinitelyPresentable.{v} X
-
-/-- `ind` is idempotent if `P` implies compact. -/
-lemma ind_ind (h : P ≤ compact C) : P.ind.ind = P.ind := by
+/-- `ind` is idempotent if `P` implies finitely presentable. -/
+lemma ind_ind (h : P ≤ isFinitelyPresentable.{w} C) [LocallySmall.{w} C] :
+    ind.{w} (ind.{w} P) = ind.{w} P := by
   refine le_antisymm (fun X h ↦ ?_) (le_ind P.ind)
   choose J Jc Jf pres K Kc Kf pres' hp using h
   have (j : J) (i : K j) : IsFinitelyPresentable ((pres' j).diag.obj i) := h _ (hp _ _)
-  exact ⟨_, inferInstance, inferInstance, pres.bind pres', fun k ↦ by simp [hp]⟩
+  have := IsFiltered.of_equivalence (ShrinkHoms.equivalence (ColimitPresentation.Total pres'))
+  exact ⟨_, inferInstance, inferInstance,
+    (pres.bind pres').reindex (ShrinkHoms.equivalence _).inverse, fun k ↦ by simp [hp]⟩
 
 end CategoryTheory.ObjectProperty
