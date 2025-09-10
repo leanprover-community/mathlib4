0eb6e205bb699d8b0ae33565470d853341ea3161
Vlad Tsyrklevich <vlad@tsyrklevi.ch>
2025-09-10 16:14:13 +0200

    feat(SimpleGraph): `IsHamiltonian` implies vertex type is `Fintype`/`Finite`
    From zulip: https://leanprover.zulipchat.com/#narrow/channel/217875-Is-there-code-for-X.3F/topic/Hamiltonian.20paths.20define.20a.20bijection.20with.20Fin.20n/near/538529855

--- Mathlib/Combinatorics/SimpleGraph/Hamiltonian.lean
+++ Mathlib/Combinatorics/SimpleGraph/Hamiltonian.lean
@@ -51,6 +51,13 @@ lemma IsPath.isHamiltonian_of_mem (hp : p.IsPath) (hp' : ∀ w, w ∈ p.support)
 lemma IsPath.isHamiltonian_iff (hp : p.IsPath) : p.IsHamiltonian ↔ ∀ w, w ∈ p.support :=
   ⟨(·.mem_support), hp.isHamiltonian_of_mem⟩
 
+/-- If a path `p` is Hamiltonian then its vertex set must be finite. -/
+protected def IsHamiltonian.fintype (hp : p.IsHamiltonian) : Fintype α where
+  elems := p.support.toFinset
+  complete := fun x ↦ List.mem_toFinset.mpr (mem_support hp x)
+
+protected lemma IsHamiltonian.finite (hp : p.IsHamiltonian) : Finite α := hp.fintype.finite
+
 section
 variable [Fintype α]
 
