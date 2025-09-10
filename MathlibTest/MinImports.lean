4de5e842959d478ee6a1a5c4c2e1234043afd1b4
Anne Baanen <2116570+Vierkantor@users.noreply.github.com>
2025-09-10 03:20:24 +0000

    chore(Init): import tactic analysis passes (#29305)
    We declare a few tactic analysis passes but never import them as part of Mathlib. This means we can't enable them locally or via global options.
    
    Depends
    
    Co-authored-by: Anne C.A. Baanen <vierkantor@vierkantor.com>

--- MathlibTest/MinImports.lean
+++ MathlibTest/MinImports.lean
@@ -76,8 +76,7 @@ lemma uses_norm_num : (0 + 1 : ℕ) = 1 := by norm_num
 #min_imports in uses_norm_num
 
 /--
-info: import Mathlib.Tactic.ExtractGoal
-import Mathlib.Tactic.Lemma
+info: import Mathlib.Tactic.Lemma
 import Mathlib.Data.Nat.Notation
 ---
 info: theorem hi.extracted_1_1 (n : ℕ) : n = n := sorry
