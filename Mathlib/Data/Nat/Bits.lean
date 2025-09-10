e2f5d265e8c7554a58c164d5f228ece812b6bd1c
Harald Husum <harald.husum@gmail.com>
2025-09-10 15:46:29 +0000

    doc: capitalize boolean (#29246)
    Adjectives derived from the proper names of people are almost always capitalized. Hence, "Boolean" should presumably also be capitalized. This conclusion is backed by the practice of [Wikipedia](https://en.wikipedia.org/wiki/Boolean_algebra), [Merriam-Webster](https://www.merriam-webster.com/dictionary/Boolean) and the [OED](https://www.oed.com/dictionary/boolean_adj). [Searching](https://scholar.google.com/scholar?as_ylo=2025&q=%22boolean%22&hl=en&as_sdt=0,5) for "boolean" on Google Scholar shows results heavily favoring the capitalized version. 
    
    In mathematics, there is a single exception to this rule that I know of, namely "abelian". This case is deemed weird enough that Wikipedia finds it necessary to provide the following justification in the article on [abelian groups](https://en.wikipedia.org/wiki/Abelian_group#A_note_on_typography):
    
    > Among mathematical adjectives derived from the proper name of a mathematician, the word "abelian" is rare in that it is usually spelled with a lowercase *a*, rather than an uppercase *A*, the lack of capitalization being a tacit acknowledgment not only of the degree to which Abel's name has been institutionalized but also of how ubiquitous in modern mathematics are the concepts introduced by him.
    
    In conclusion, I think mathlib would seem less idiosyncratic if we followed this established pattern to a greater extent.
    
    This PR is a one-off effort to have current mathlib align with the pattern. It does not imply that future contributors should meet friction in the PR process when opting for lowercase "boolean". I do, however, believe that the current mathlib is leading by example, and that however mathlib currently looks will influence the choices of future contributors. Hence why I think this PR will have positive downstream effects in any case.

--- Mathlib/Data/Nat/Bits.lean
+++ Mathlib/Data/Nat/Bits.lean
@@ -157,8 +157,8 @@ def bits : ℕ → List Bool :=
   binaryRec [] fun b _ IH => b :: IH
 
 /-- `ldiff a b` performs bitwise set difference. For each corresponding
-  pair of bits taken as booleans, say `aᵢ` and `bᵢ`, it applies the
-  boolean operation `aᵢ ∧ ¬bᵢ` to obtain the `iᵗʰ` bit of the result. -/
+  pair of bits taken as Booleans, say `aᵢ` and `bᵢ`, it applies the
+  Boolean operation `aᵢ ∧ ¬bᵢ` to obtain the `iᵗʰ` bit of the result. -/
 def ldiff : ℕ → ℕ → ℕ :=
   bitwise fun a b => a && not b
 
