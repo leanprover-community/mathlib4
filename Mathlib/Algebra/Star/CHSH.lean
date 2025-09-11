3f4f3369b4b7eda3d942974b5442400f9fea86b6
Kim Morrison <kim@tqft.net>
2025-09-11 20:05:35 +1000

    fix import

--- Mathlib/Algebra/Star/CHSH.lean
+++ Mathlib/Algebra/Star/CHSH.lean
@@ -3,6 +3,7 @@ Copyright (c) 2020 Kim Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kim Morrison
 -/
+import Mathlib.Tactic.LinearCombination
 import Mathlib.Algebra.CharP.Invertible
 import Mathlib.Algebra.Order.Star.Basic
 import Mathlib.Algebra.Ring.Regular
