e0ddee5421f8f0d081a9b219abcafa5a01797c7f
Yongshun Ye <ShreckYe@gmail.com>
2025-09-11 11:41:57 +0800

    Complete proving `sin_eq_two_mul_tan_half_div_one_add_tan_half_sq`

--- TanHalfAngleSubstitution.lean
+++ TanHalfAngleSubstitution.lean
@@ -10,19 +10,26 @@ lemma one_add_tan_sq_mul_cos_sq_eq_one (h : cos x ≠ 0) : (1 + tan x ^ 2) * cos
   conv_rhs => rw [← sin_sq_add_cos_sq x, ← tan_mul_cos h]
   ring
 
-/-- `tan` takes the junk value `0` when `sin x = 0` so this always holds. -/
+/-- `tan x` takes the junk value `0` when `cos x = 0` -/
+lemma tan_eq_zero_of_cos_eq_zero (h : cos x = 0) : tan x = 0 := by
+  sorry
+
+/-- `tan (x / 2)` takes the junk value `0` when `sin x = 0` so this always holds. -/
 theorem sin_eq_two_mul_tan_half_div_one_add_tan_half_sq :
     sin x = (2 * tan (x / 2)) / (1 + tan (x / 2) ^ 2) := by
+  conv_lhs => rw [show x = 2 * (x / 2) by group, sin_two_mul]
   by_cases h : cos (x / 2) = 0
-  · have : sin x = 0 := by
-      sorry
-    rw [this]
+  · simp [h]
     have : tan (x / 2) = 0 := by
-      sorry
-    rw [this]
-    simp
-  . rw [← mul_div_mul_right _ _ (pow_ne_zero 2 h), one_add_tan_sq_mul_cos_sq_eq_one (x / 2) h]
-    conv_lhs => rw [show x = 2 * (x / 2) by group, sin_two_mul, ← tan_mul_cos h]
+      rw [cos_eq_zero_iff] at h
+      obtain ⟨k, hxk⟩ := h
+      apply tan_eq_zero_iff.mpr
+      use 2 * k + 1
+      push_cast
+      exact hxk.symm
+    simp [this]
+  . conv_rhs => rw [← mul_div_mul_right _ _ (pow_ne_zero 2 h), one_add_tan_sq_mul_cos_sq_eq_one (x / 2) h]
+    rw [← tan_mul_cos h]
     group
 
 theorem cos_eq_two_mul_tan_half_div_one_sub_tan_half_sq (h : cos x ≠ -1) :
