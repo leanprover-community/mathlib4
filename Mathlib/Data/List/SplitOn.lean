ee64e26f1cb58e7b61abd34a9c629a7978ae833a
Markus Himmel <markus@lean-fro.org>
2025-09-11 16:45:52 +0200

    Fix

--- Mathlib/Data/List/SplitOn.lean
+++ Mathlib/Data/List/SplitOn.lean
@@ -51,7 +51,8 @@ theorem splitOnP_spec (as : List α) :
   | cons a as' ih =>
     rw [splitOnP_cons, filter]
     by_cases h : p a
-    · rw [if_pos h, h, map, cons_append, zipWith, nil_append, flatten, cons_append, cons_inj_right]
+    · rw [if_pos h, h, map, cons_append, zipWith, nil_append, flatten_cons, cons_append,
+        cons_inj_right]
       exact ih
     · rw [if_neg h, eq_false_of_ne_true h, flatten_zipWith (splitOnP_ne_nil _ _)
         (append_ne_nil_of_right_ne_nil _ (cons_ne_nil [] [])), cons_inj_right]
@@ -105,7 +106,7 @@ theorem splitOn_intercalate [DecidableEq α] (x : α) (hx : ∀ l ∈ ls, x ∉ 
     rw [eq_of_beq H] at hy
     refine hx hd ?_ hy
     simp
-  · simp only [intersperse_cons₂, singleton_append, flatten]
+  · simp only [intersperse_cons₂, singleton_append, flatten_cons]
     specialize ih _ _
     · intro l hl
       apply hx l
