5f0d1bc3683088d6cdefc9efabee0ba2eff35ed1
Dagur Asgeirsson <dagurtomas@gmail.com>
2025-09-12 10:56:56 -0600

    address comments from Joel

--- Mathlib/CategoryTheory/Monoidal/Braided/Multifunctor.lean
+++ Mathlib/CategoryTheory/Monoidal/Braided/Multifunctor.lean
@@ -5,6 +5,7 @@ Authors: Dagur Asgeirsson
 -/
 import Mathlib.CategoryTheory.Monoidal.Braided.Basic
 import Mathlib.CategoryTheory.Functor.CurryingThree
+
 /-!
 
 # Constructing braided categories from natural transformations between multifunctors
@@ -32,43 +33,43 @@ variable (C)
 
 /-- The trifunctor `X₁ X₂ X₃ ↦ (X₁ ⊗ X₂) ⊗ X₃ -/
 @[simps!]
-def functor₁ : C ⥤ C ⥤ C ⥤ C := bifunctorComp₁₂ (curriedTensor C) (curriedTensor C)
+def functor₁₂₃ : C ⥤ C ⥤ C ⥤ C := bifunctorComp₁₂ (curriedTensor C) (curriedTensor C)
 
 /-- The trifunctor `X₁ X₂ X₃ ↦ X₁ ⊗ (X₂ ⊗ X₃) -/
 @[simps!]
-def functor₂ : C ⥤ C ⥤ C ⥤ C := bifunctorComp₂₃ (curriedTensor C) (curriedTensor C)
+def functor₁₂₃' : C ⥤ C ⥤ C ⥤ C := bifunctorComp₂₃ (curriedTensor C) (curriedTensor C)
 
 /-- The trifunctor `X₁ X₂ X₃ ↦ (X₂ ⊗ X₃) ⊗ X₁ -/
 @[simps!]
-def functor₃ : C ⥤ C ⥤ C ⥤ C := (bifunctorComp₂₃ (curriedTensor C).flip (curriedTensor C))
+def functor₂₃₁ : C ⥤ C ⥤ C ⥤ C := (bifunctorComp₂₃ (curriedTensor C).flip (curriedTensor C))
 
 /-- The trifunctor `X₁ X₂ X₃ ↦ X₂ ⊗ (X₃ ⊗ X₁) -/
 @[simps!]
-def functor₄ : C ⥤ C ⥤ C ⥤ C := (bifunctorComp₂₃ (curriedTensor C) (curriedTensor C)).flip.flip₁₃
+def functor₂₃₁' : C ⥤ C ⥤ C ⥤ C := (bifunctorComp₂₃ (curriedTensor C) (curriedTensor C)).flip.flip₁₃
 
 /-- The trifunctor `X₁ X₂ X₃ ↦ (X₂ ⊗ X₁) ⊗ X₃ -/
 @[simps!]
-def functor₅ : C ⥤ C ⥤ C ⥤ C := bifunctorComp₁₂ (curriedTensor C).flip (curriedTensor C)
+def functor₂₁₃ : C ⥤ C ⥤ C ⥤ C := bifunctorComp₁₂ (curriedTensor C).flip (curriedTensor C)
 
 /-- The trifunctor `X₁ X₂ X₃ ↦ X₂ ⊗ (X₁ ⊗ X₃) -/
 @[simps!]
-def functor₆ : C ⥤ C ⥤ C ⥤ C := (bifunctorComp₂₃ (curriedTensor C) (curriedTensor C)).flip
+def functor₂₁₃' : C ⥤ C ⥤ C ⥤ C := (bifunctorComp₂₃ (curriedTensor C) (curriedTensor C)).flip
 
 /-- The trifunctor `X₁ X₂ X₃ ↦ X₃ ⊗ (X₁ ⊗ X₂) -/
 @[simps!]
-def functor₇ : C ⥤ C ⥤ C ⥤ C := (bifunctorComp₂₃ (curriedTensor C) (curriedTensor C)).flip.flip₂₃
+def functor₃₁₂' : C ⥤ C ⥤ C ⥤ C := (bifunctorComp₂₃ (curriedTensor C) (curriedTensor C)).flip.flip₂₃
 
 /-- The trifunctor `X₁ X₂ X₃ ↦ (X₃ ⊗ X₁) ⊗ X₂ -/
 @[simps!]
-def functor₈ : C ⥤ C ⥤ C ⥤ C := (bifunctorComp₁₂ (curriedTensor C) (curriedTensor C)).flip.flip₂₃
+def functor₃₁₂ : C ⥤ C ⥤ C ⥤ C := (bifunctorComp₁₂ (curriedTensor C) (curriedTensor C)).flip.flip₂₃
 
 /-- The trifunctor `X₁ X₂ X₃ ↦ X₁ ⊗ (X₃ ⊗ X₂) -/
 @[simps!]
-def functor₉ : C ⥤ C ⥤ C ⥤ C := (bifunctorComp₂₃ (curriedTensor C) (curriedTensor C).flip)
+def functor₁₃₂' : C ⥤ C ⥤ C ⥤ C := (bifunctorComp₂₃ (curriedTensor C) (curriedTensor C).flip)
 
 /-- The trifunctor `X₁ X₂ X₃ ↦ (X₁ ⊗ X₃) ⊗ X₂ -/
 @[simps!]
-def functor₁₀ : C ⥤ C ⥤ C ⥤ C := (bifunctorComp₁₂ (curriedTensor C) (curriedTensor C)).flip₂₃
+def functor₁₃₂ : C ⥤ C ⥤ C ⥤ C := (bifunctorComp₁₂ (curriedTensor C) (curriedTensor C)).flip₂₃
 
 end Hexagon
 
@@ -91,31 +92,55 @@ example : (bifunctorComp₁₂ (curriedTensor C) (curriedTensor C)) =
 
 namespace Forward
 
+/-!
+
+# The forward hexagon identity
+
+Given a braiding in the form of a natural isomorphism of bifunctors
+`β : curriedTensor C ≅ (curriedTensor C).flip` (i.e. `(β.app X₁).app X₂ : X₁ ⊗ X₂ ≅ X₂ ⊗ X₁`),
+we phrase the forward hexagon identity as an equality of natural transformations between trifunctors
+(the hexagon on the left is the diagram we require to commute, the hexagon on the right is the
+same on the object level on three objects `X₁ X₂ X₃`).
+
+```
+            funtor₁₂₃                         (X₁ ⊗ X₂) ⊗ X₃
+associtator/          \ secondMap₁             /           \
+          v            v                      v             v
+     functor₁₂₃'    functor₂₁₃          X₁ ⊗ (X₂ ⊗ X₃)    (X₂ ⊗ X₁) ⊗ X₃
+ firsMap₂ |            |secondMap₂            |             |
+          v            v                      v             v
+     functor₂₃₁     functor₂₁₃'         (X₂ ⊗ X₃) ⊗ X₁    X₂ ⊗ (X₁ ⊗ X₃)
+  firstMap₃\           / secondMap₃            \            /
+            v         v                         v          v
+             functor₂₃₁'                        X₂ ⊗ (X₃ ⊗ X₁)
+```
+-/
+
 /-- The middle left map in the forward hexagon identity. -/
 @[simps!]
-def firstMap₂ (β : curriedTensor C ≅ (curriedTensor C).flip) : functor₂ C ⟶ functor₃ C :=
+def firstMap₂ (β : curriedTensor C ≅ (curriedTensor C).flip) : functor₁₂₃' C ⟶ functor₂₃₁ C :=
   (bifunctorComp₂₃Functor.map β.hom).app _
 
 variable (C) in
 /-- The bottom left map in the forward hexagon identity. -/
 @[simps!]
-def firstMap₃ : functor₃ C ⟶ functor₄ C where
+def firstMap₃ : functor₂₃₁ C ⟶ functor₂₃₁' C where
   app _ := { app _ := { app _ := (α_ _ _ _).hom } }
 
 /-- The top right map in the forward hexagon identity. -/
 @[simps!]
-def secondMap₁ (β : curriedTensor C ≅ (curriedTensor C).flip) : functor₁ C ⟶ functor₅ C :=
+def secondMap₁ (β : curriedTensor C ≅ (curriedTensor C).flip) : functor₁₂₃ C ⟶ functor₂₁₃ C :=
   (bifunctorComp₁₂Functor.map β.hom).app _
 
 variable (C) in
 /-- The middle right map in the forward hexagon identity. -/
 @[simps!]
-def secondMap₂ : functor₅ C ⟶ functor₆ C where
+def secondMap₂ : functor₂₁₃ C ⟶ functor₂₁₃' C where
   app _ := { app _ := { app _ := (α_ _ _ _).hom } }
 
 /-- The bottom right map in the forward hexagon identity. -/
 @[simps!]
-def secondMap₃ (β : curriedTensor C ≅ (curriedTensor C).flip) : functor₆ C ⟶ functor₄ C :=
+def secondMap₃ (β : curriedTensor C ≅ (curriedTensor C).flip) : functor₂₁₃' C ⟶ functor₂₃₁' C :=
   flip₁₃Functor.map ((flipFunctor _ _ _).map
     ((bifunctorComp₂₃Functor.obj (curriedTensor C)).map ((flipFunctor _ _ _).map β.hom)))
 
@@ -123,32 +148,56 @@ end Forward
 
 namespace Reverse
 
+/-!
+
+# The reverse hexagon identity
+
+Given a braiding in the form of a natural isomorphism of bifunctors
+`β : curriedTensor C ≅ (curriedTensor C).flip` (i.e. `(β.app X₁).app X₂ : X₁ ⊗ X₂ ≅ X₂ ⊗ X₁`),
+we phrase the reverse hexagon identity as an equality of natural transformations between trifunctors
+(the hexagon on the left is the diagram we require to commute, the hexagon on the right is the
+same on the object level on three objects `X₁ X₂ X₃`).
+
+```
+            funtor₁₂₃'                        X₁ ⊗ (X₂ ⊗ X₃)
+associtator/          \ secondMap₁             /           \
+          v            v                      v             v
+     functor₁₂₃    functor₁₃₂'          (X₁ ⊗ X₂) ⊗ X₃    X₁ ⊗ (X₃ ⊗ X₂)
+ firsMap₂ |            |secondMap₂            |             |
+          v            v                      v             v
+     functor₃₁₂'    functor₁₃₂          X₃ ⊗ (X₁ ⊗ X₂)    (X₁ ⊗ X₃) ⊗ X₂
+  firstMap₃\           / secondMap₃            \            /
+            v         v                         v          v
+             functor₃₁₂                        (X₃ ⊗ X₁) ⊗ X₂
+```
+-/
+
 /-- The middle left map in the reverse hexagon identity. -/
 @[simps!]
-def firstMap₂ (β : curriedTensor C ≅ (curriedTensor C).flip) : functor₁ C ⟶ functor₇ C :=
+def firstMap₂ (β : curriedTensor C ≅ (curriedTensor C).flip) : functor₁₂₃ C ⟶ functor₃₁₂' C :=
   flip₂₃Functor.map ((flipFunctor _ _ _).map ((bifunctorComp₂₃Functor.map
     ((flipFunctor _ _ _).map β.hom)).app _))
 
 variable (C) in
 /-- The bottom left map in the reverse hexagon identity. -/
 @[simps!]
-def firstMap₃ : functor₇ C ⟶ functor₈ C :=
+def firstMap₃ : functor₃₁₂' C ⟶ functor₃₁₂ C :=
   flip₂₃Functor.map ((flipFunctor _ _ _).map (curriedAssociatorNatIso C).inv)
 
 /-- The top right map in the reverse hexagon identity. -/
 @[simps!]
-def secondMap₁ (β : curriedTensor C ≅ (curriedTensor C).flip) : functor₂ C ⟶ functor₉ C :=
+def secondMap₁ (β : curriedTensor C ≅ (curriedTensor C).flip) : functor₁₂₃' C ⟶ functor₁₃₂' C :=
   (bifunctorComp₂₃Functor.obj _).map β.hom
 
 variable (C) in
 /-- The middle right map in the reverse hexagon identity. -/
 @[simps!]
-def secondMap₂ : functor₉ C ⟶ functor₁₀ C where
+def secondMap₂ : functor₁₃₂' C ⟶ functor₁₃₂ C where
   app _ := { app _ := { app _ := (α_ _ _ _).inv } }
 
 /-- The bottom right map in the reverse hexagon identity. -/
 @[simps!]
-def secondMap₃ (β : curriedTensor C ≅ (curriedTensor C).flip) : functor₁₀ C ⟶ functor₈ C :=
+def secondMap₃ (β : curriedTensor C ≅ (curriedTensor C).flip) : functor₁₃₂ C ⟶ functor₃₁₂ C :=
   flip₂₃Functor.map ((bifunctorComp₁₂Functor.map β.hom).app _)
 
 end Reverse
