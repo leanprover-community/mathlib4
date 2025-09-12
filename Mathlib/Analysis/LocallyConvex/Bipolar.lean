8c270ce737cc3dd1a62a03b2c61ad25af9b7a742
Christopher Hoskin <4855578+mans0954@users.noreply.github.com>
2025-09-12 19:10:39 +0100

    Update Mathlib/Analysis/LocallyConvex/Bipolar.lean
    Co-authored-by: Filippo A. E. Nuccio <filippo.nuccio@univ-st-etienne.fr>

--- Mathlib/Analysis/LocallyConvex/Bipolar.lean
+++ Mathlib/Analysis/LocallyConvex/Bipolar.lean
@@ -110,55 +110,53 @@ variable [Module ℝ E] [IsScalarTower ℝ 𝕜 E]
 open scoped ComplexConjugate
 theorem flip_polar_polar_eq {B : E →ₗ[𝕜] F →ₗ[𝕜] 𝕜} {s : Set E} [Nonempty s] :
     B.flip.polar (B.polar s) = closedAbsConvexHull (E := WeakBilin B) 𝕜 s := by
-  apply le_antisymm
-  · simp only [Set.le_eq_subset]
-    rw [← Set.compl_subset_compl]
-    intro x hx
-    obtain ⟨f, ⟨u, ⟨hf₁, hf₂⟩⟩⟩ :=
-      RCLike.geometric_hahn_banach_closed_point (𝕜 := 𝕜) (E := WeakBilin B)
-        absConvex_convexClosedHull.2 isClosed_closedAbsConvexHull hx
-    have e3 : RCLike.re (f 0) < u :=
-      (hf₁ 0) (absConvexHull_subset_closedAbsConvexHull zero_mem_absConvexHull)
-    rw [map_zero, map_zero] at e3
-    let g := (1/u : ℝ) • f
-    have fg : g = (1/u : ℝ) • f := rfl
-    have fg2 : u • g = f := by
-      rw [fg, one_div, ← smul_assoc, smul_eq_mul, mul_inv_cancel₀ (ne_of_lt e3).symm, one_smul]
-    have hg₁ : ∀ a ∈ (closedAbsConvexHull (E := WeakBilin B) 𝕜) s, RCLike.re (g a) < 1 :=
-        fun _ ha => by
-      rw [fg, ContinuousLinearMap.coe_smul', Pi.smul_apply, RCLike.smul_re, one_div,
-        ← (inv_mul_cancel₀ (lt_iff_le_and_ne.mp e3).2.symm)]
-      exact mul_lt_mul_of_pos_left ((hf₁ _) ha) (inv_pos_of_pos e3)
-    obtain ⟨f₀, hf₀⟩ := B.dualEmbedding_surjective g
-    have hg₃ : f₀ ∈ (B.polar (E := WeakBilin B) s) := by
-      simp [← hf₀, WeakBilin.eval] at hg₁
-      intro x₂ hx₂
-      let l := conj (B x₂ f₀) / ‖B x₂ f₀‖
-      have lnorm : ‖l‖ ≤ 1 := by
-        rw [norm_div, RCLike.norm_conj, norm_algebraMap', norm_norm]
-        exact div_self_le_one _
-      have i1 : RCLike.re ((B.flip f₀) (l • x₂)) < 1 := hg₁ _
-        (balanced_iff_smul_mem.mp absConvex_convexClosedHull.1 lnorm
-          (subset_closedAbsConvexHull hx₂))
-      rw [CompatibleSMul.map_smul, smul_eq_mul, mul_comm, ← mul_div_assoc, LinearMap.flip_apply,
-        RCLike.mul_conj, sq, mul_self_div_self, RCLike.ofReal_re] at i1
-      exact le_of_lt i1
-    have one_lt_x_f₀ : 1 < RCLike.re (B x f₀) := by
-      rw [← one_lt_inv_mul₀ e3] at hf₂
-      suffices u⁻¹ * RCLike.re (f x) = RCLike.re ((B x) f₀) by exact lt_of_lt_of_eq hf₂ this
-      rw [← RCLike.re_ofReal_mul]
-      congr
-      simp [map_inv₀, ← fg2, ← hf₀, WeakBilin.eval]
-      rw [← smul_eq_mul, ← smul_assoc]
-      norm_cast
-      have unz : u ≠ 0 := (ne_of_lt e3).symm
-      aesop
-    by_contra hc
-    rw [Set.mem_compl_iff, not_not] at hc
-    exact ((lt_iff_le_not_ge.mp one_lt_x_f₀).2)
-      (Preorder.le_trans (RCLike.re ((B x) f₀)) ‖(B x) f₀‖ 1
-        (RCLike.re_le_norm ((B x) f₀)) (hc f₀ hg₃))
-  · exact closedAbsConvexHull_min (subset_bipolar B s) (polar_AbsConvex _) (polar_isClosed B.flip _)
+  refine le_antisymm ?_ <| closedAbsConvexHull_min
+    (subset_bipolar B s) (polar_AbsConvex _) (polar_isClosed B.flip _)
+  simp only [Set.le_eq_subset]
+  rw [← Set.compl_subset_compl]
+  intro x hx
+  obtain ⟨f, ⟨u, ⟨hf₁, hf₂⟩⟩⟩ :=
+    RCLike.geometric_hahn_banach_closed_point (𝕜 := 𝕜) (E := WeakBilin B)
+      absConvex_convexClosedHull.2 isClosed_closedAbsConvexHull hx
+  have e3 : RCLike.re (f 0) < u :=
+    (hf₁ 0) (absConvexHull_subset_closedAbsConvexHull zero_mem_absConvexHull)
+  rw [map_zero, map_zero] at e3
+  set g := (1/u : ℝ) • f with fg
+  have fg2 : u • g = f := by
+    rw [fg, one_div, ← smul_assoc, smul_eq_mul, mul_inv_cancel₀ (ne_of_lt e3).symm, one_smul]
+  have hg₁ {a : _} (ha : a ∈ (closedAbsConvexHull (E := WeakBilin B) 𝕜) s) :
+    RCLike.re (g a) < 1 := by
+    rw [fg, ContinuousLinearMap.coe_smul', Pi.smul_apply, RCLike.smul_re, one_div,
+      ← (inv_mul_cancel₀ (lt_iff_le_and_ne.mp e3).2.symm)]
+    exact mul_lt_mul_of_pos_left ((hf₁ _) ha) (inv_pos_of_pos e3)
+  obtain ⟨f₀, hf₀⟩ := B.dualEmbedding_surjective g
+  have hg₃ : f₀ ∈ (B.polar (E := WeakBilin B) s) := by
+    simp [← hf₀, WeakBilin.eval] at hg₁
+    intro x₂ hx₂
+    let l := conj (B x₂ f₀) / ‖B x₂ f₀‖
+    have lnorm : ‖l‖ ≤ 1 := by
+      rw [norm_div, RCLike.norm_conj, norm_algebraMap', norm_norm]
+      exact div_self_le_one _
+    have i1 : RCLike.re ((B.flip f₀) (l • x₂)) ≤ 1 := le_of_lt <| hg₁
+      <| balanced_iff_smul_mem.mp absConvex_convexClosedHull.1 lnorm
+        (subset_closedAbsConvexHull hx₂)
+    rwa [CompatibleSMul.map_smul, smul_eq_mul, mul_comm, ← mul_div_assoc, LinearMap.flip_apply,
+      RCLike.mul_conj, sq, mul_self_div_self, RCLike.ofReal_re] at i1
+  have one_lt_x_f₀ : 1 < RCLike.re (B x f₀) := by
+    rw [← one_lt_inv_mul₀ e3] at hf₂
+    suffices u⁻¹ * RCLike.re (f x) = RCLike.re ((B x) f₀) by exact lt_of_lt_of_eq hf₂ this
+    rw [← RCLike.re_ofReal_mul]
+    congr
+    simp [map_inv₀, ← fg2, ← hf₀, WeakBilin.eval]
+    rw [← smul_eq_mul, ← smul_assoc]
+    norm_cast
+    have unz : u ≠ 0 := (ne_of_lt e3).symm
+    aesop
+  by_contra hc
+  rw [Set.mem_compl_iff, not_not] at hc
+  exact ((lt_iff_le_not_ge.mp one_lt_x_f₀).2)
+    (Preorder.le_trans (RCLike.re ((B x) f₀)) ‖(B x) f₀‖ 1
+      (RCLike.re_le_norm ((B x) f₀)) (hc f₀ hg₃))
 
 /-
 This fails when `s` is empty. Indeed, `closedAbsConvexHull (E := WeakBilin B) 𝕜 s` is the empty set,
