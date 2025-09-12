8a4274f96a19091eda2f295aba8b30b4a62404e0
Christopher Hoskin <mans0954@users.noreply.github.com>
2025-09-12 20:35:08 +0100

    SeminormedRing

--- Mathlib/Analysis/LocallyConvex/WithSeminorms.lean
+++ Mathlib/Analysis/LocallyConvex/WithSeminorms.lean
@@ -57,7 +57,7 @@ variable {𝕜 𝕜₂ 𝕝 𝕝₂ E F G ι ι' : Type*}
 
 section FilterBasis
 
-variable [NormedField 𝕜] [AddCommGroup E] [Module 𝕜 E]
+variable [SeminormedRing 𝕜] [AddCommGroup E] [Module 𝕜 E]
 variable (𝕜 E ι)
 
 /-- An abbreviation for indexed families of seminorms. This is mainly to allow for dot-notation. -/
@@ -151,6 +151,31 @@ theorem basisSets_smul (U) (hU : U ∈ p.basisSets) :
   refine Set.Subset.trans (ball_smul_ball (s.sup p) √r √r) ?_
   rw [hU, Real.mul_self_sqrt (le_of_lt hr)]
 
+/-- If a family of seminorms is continuous, then their basis sets are neighborhoods of zero. -/
+lemma basisSets_mem_nhds {𝕜 E ι : Type*} [NormedField 𝕜]
+    [AddCommGroup E] [Module 𝕜 E] [TopologicalSpace E] (p : SeminormFamily 𝕜 E ι)
+    (hp : ∀ i, Continuous (p i)) (U : Set E) (hU : U ∈ p.basisSets) : U ∈ 𝓝 (0 : E) := by
+  obtain ⟨s, r, hr, rfl⟩ := p.basisSets_iff.mp hU
+  clear hU
+  refine Seminorm.ball_mem_nhds ?_ hr
+  classical
+  induction s using Finset.induction_on with
+  | empty => simpa using continuous_zero
+  | insert a s _ hs =>
+    simp only [Finset.sup_insert, coe_sup]
+    exact Continuous.max (hp a) hs
+
+end SeminormFamily
+
+end FilterBasis
+
+section
+
+namespace SeminormFamily
+
+variable [NormedField 𝕜] [AddCommGroup E] [Module 𝕜 E]
+variable (p : SeminormFamily 𝕜 E ι)
+
 theorem basisSets_smul_left (x : 𝕜) (U : Set E) (hU : U ∈ p.basisSets) :
     ∃ V ∈ p.addGroupFilterBasis.sets, V ⊆ (fun y : E => x • y) ⁻¹' U := by
   rcases p.basisSets_iff.mp hU with ⟨s, r, hr, hU⟩
@@ -189,30 +214,16 @@ theorem filter_eq_iInf (p : SeminormFamily 𝕜 E ι) :
         ⟨Metric.ball 0 r, Metric.ball_mem_nhds 0 hr,
           Eq.subset (p i).ball_zero_eq_preimage_ball.symm⟩
 
-/-- If a family of seminorms is continuous, then their basis sets are neighborhoods of zero. -/
-lemma basisSets_mem_nhds {𝕜 E ι : Type*} [NormedField 𝕜]
-    [AddCommGroup E] [Module 𝕜 E] [TopologicalSpace E] (p : SeminormFamily 𝕜 E ι)
-    (hp : ∀ i, Continuous (p i)) (U : Set E) (hU : U ∈ p.basisSets) : U ∈ 𝓝 (0 : E) := by
-  obtain ⟨s, r, hr, rfl⟩ := p.basisSets_iff.mp hU
-  clear hU
-  refine Seminorm.ball_mem_nhds ?_ hr
-  classical
-  induction s using Finset.induction_on with
-  | empty => simpa using continuous_zero
-  | insert a s _ hs =>
-    simp only [Finset.sup_insert, coe_sup]
-    exact Continuous.max (hp a) hs
-
 end SeminormFamily
 
-end FilterBasis
+end
 
 section Bounded
 
 namespace Seminorm
 
-variable [NormedField 𝕜] [AddCommGroup E] [Module 𝕜 E]
-variable [NormedField 𝕜₂] [AddCommGroup F] [Module 𝕜₂ F]
+variable [SeminormedRing 𝕜] [AddCommGroup E] [Module 𝕜 E]
+variable [SeminormedRing 𝕜₂] [AddCommGroup F] [Module 𝕜₂ F]
 variable {σ₁₂ : 𝕜 →+* 𝕜₂} [RingHomIsometric σ₁₂]
 
 -- Todo: This should be phrased entirely in terms of the von Neumann bornology.
