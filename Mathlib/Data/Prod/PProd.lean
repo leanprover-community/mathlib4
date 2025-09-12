f5fb2f97e91e716a20f145d6d39e68a8a67e08f7
Kim Morrison <kim@tqft.net>
2025-09-12 12:21:50 +1000

    fix bad shake

--- Mathlib/Data/Prod/PProd.lean
+++ Mathlib/Data/Prod/PProd.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Eric Wieser
 -/
 import Batteries.Logic
+import Mathlib.Tactic.TypeStar
 
 /-!
 # Extra facts about `PProd`
--- Mathlib/Logic/Nontrivial/Defs.lean
+++ Mathlib/Logic/Nontrivial/Defs.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Sébastien Gouëzel
 -/
 import Mathlib.Tactic.TypeStar
+import Mathlib.Tactic.Lemma
 
 /-!
 # Nontrivial types
