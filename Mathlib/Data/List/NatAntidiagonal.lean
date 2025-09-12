235fbdcf7cfa9e6da33f64b7d4fc2a290ad09aae
Rob23oba <robin.arnez@web.de>
2025-09-12 17:09:46 +0200

    Revert "oops"
    This reverts commit 517b83248bb6787b5c7a4c1c5731c68641833ddb.

--- Mathlib/Data/List/NatAntidiagonal.lean
+++ Mathlib/Data/List/NatAntidiagonal.lean
@@ -53,14 +53,7 @@ theorem antidiagonal_zero : antidiagonal 0 = [(0, 0)] :=
 
 /-- The antidiagonal of `n` does not contain duplicate entries. -/
 theorem nodup_antidiagonal (n : ℕ) : Nodup (antidiagonal n) :=
-  #adaptation_note
-  /--
-  nightly-2025-09-11
-  Unfortunately the implicit arguments of `LeftInverse.injective`
-  were re-ordered during upstreaming.
-  I'll fix this shortly.
-  -/
-  nodup_range.map ((@LeftInverse.injective (ℕ × ℕ) ℕ Prod.fst fun i ↦ (i, n - i)) fun _ ↦ rfl)
+  nodup_range.map ((@LeftInverse.injective ℕ (ℕ × ℕ) Prod.fst fun i ↦ (i, n - i)) fun _ ↦ rfl)
 
 @[simp]
 theorem antidiagonal_succ {n : ℕ} :
