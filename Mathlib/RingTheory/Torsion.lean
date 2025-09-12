8e3dc11d8e4a3289439605891106e04f5b1a4414
Yakov Pechersky <ffxen158@gmail.com>
2025-09-12 16:37:31 -0400

    other direction and names

--- Mathlib/RingTheory/Torsion.lean
+++ Mathlib/RingTheory/Torsion.lean
@@ -13,14 +13,17 @@ import Mathlib.RingTheory.RootsOfUnity.Basic
 
 variable (M : Type*) [CommMonoid M]
 
+lemma CommGroup.mem_torsion_iff_exists_mem_rootsOfUnity {x : Mˣ} :
+    x ∈ CommGroup.torsion Mˣ ↔ ∃ n ≠ 0, x ∈ rootsOfUnity n M := by
+  simp [CommGroup.mem_torsion, isOfFinOrder_iff_pow_eq_one, Nat.pos_iff_ne_zero]
+
 lemma rootsOfUnity_le_torsion (k : ℕ) [NeZero k] :
     rootsOfUnity k M ≤ CommGroup.torsion Mˣ := by
   intro
-  simp only [mem_rootsOfUnity, CommGroup.mem_torsion, isOfFinOrder_iff_pow_eq_one]
-  intro h
-  exact ⟨_, Nat.pos_of_ne_zero NeZero.out, h⟩
+  have : k ≠ 0 := NeZero.out
+  grind [CommGroup.mem_torsion_iff_exists_mem_rootsOfUnity]
 
-lemma torsion_eq_biSup_rootsOfUnity :
+lemma CommGroup.torsion_eq_biSup_rootsOfUnity :
     CommGroup.torsion Mˣ = ⨆ (n : ℕ) (_hn : n ≠ 0), rootsOfUnity n M := by
   refine le_antisymm ?_ ?_
   · intro x hx
@@ -36,3 +39,9 @@ lemma torsion_eq_biSup_rootsOfUnity :
     intro n hn
     have : NeZero n := ⟨hn⟩
     exact rootsOfUnity_le_torsion M n
+
+-- TODO:
+-- lemma CommGroup.torsion_eq_rootsOfUnity_finite_card {R : Type*} [CommRing R] [IsDomain R]
+--     [Finite (CommGroup.torsion Rˣ)] :
+--     CommGroup.torsion Rˣ = rootsOfUnity (Nat.card (CommGroup.torsion Rˣ)) R := by
+--   sorry
