0cf45538cd66854c55c0de0b664e7c53775ac513
Monica Omar <2497250a@research.gla.ac.uk>
2025-09-12 20:27:56 +0000

    3 more

--- Mathlib/RingTheory/Smooth/Basic.lean
+++ Mathlib/RingTheory/Smooth/Basic.lean
@@ -69,10 +69,9 @@ theorem exists_lift {B : Type u} [CommRing B] [_RB : Algebra R B]
   · intro B _ I hI _; exact FormallySmooth.comp_surjective I hI
   · intro B _ I J hIJ h₁ h₂ _ g
     let this : ((B ⧸ I) ⧸ J.map (Ideal.Quotient.mk I)) ≃ₐ[R] B ⧸ J :=
-      {
-        (DoubleQuot.quotQuotEquivQuotSup I J).trans
-          (Ideal.quotEquivOfEq (sup_eq_right.mpr hIJ)) with
-        commutes' := fun x => rfl }
+      .ofCommutes ((DoubleQuot.quotQuotEquivQuotSup I J).trans
+          (Ideal.quotEquivOfEq (sup_eq_right.mpr hIJ)))
+        fun _ => rfl
     obtain ⟨g', e⟩ := h₂ (this.symm.toAlgHom.comp g)
     obtain ⟨g', rfl⟩ := h₁ g'
     replace e := congr_arg this.toAlgHom.comp e
