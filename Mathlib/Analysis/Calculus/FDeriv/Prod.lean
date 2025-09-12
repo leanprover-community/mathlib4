cd1fdd40de9e1f30477e36edf0643e023a08cba8
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-09-12 14:38:56 +0200

    feat: add HasFDerivWithinAt.prodMap

--- Mathlib/Analysis/Calculus/FDeriv/Prod.lean
+++ Mathlib/Analysis/Calculus/FDeriv/Prod.lean
@@ -341,6 +341,13 @@ protected theorem HasStrictFDerivAt.prodMap (hf : HasStrictFDerivAt f f' p.1)
   (hf.comp p hasStrictFDerivAt_fst).prodMk (hf₂.comp p hasStrictFDerivAt_snd)
 
 @[fun_prop]
+protected theorem HasFDerivWithinAt.prodMap {s' : Set G}
+    (hf : HasFDerivWithinAt f f' s p.1) (hf₂ : HasFDerivWithinAt f₂ f₂' s' p.2) :
+    HasFDerivWithinAt (Prod.map f f₂) (f'.prodMap f₂') (s ×ˢ s') p :=
+  (hf.comp _ hasFDerivWithinAt_fst mapsTo_fst_prod).prodMk
+    (hf₂.comp _ hasFDerivWithinAt_snd mapsTo_snd_prod)
+
+@[fun_prop]
 protected theorem HasFDerivAt.prodMap (hf : HasFDerivAt f f' p.1) (hf₂ : HasFDerivAt f₂ f₂' p.2) :
     HasFDerivAt (Prod.map f f₂) (f'.prodMap f₂') p :=
   (hf.comp p hasFDerivAt_fst).prodMk (hf₂.comp p hasFDerivAt_snd)
