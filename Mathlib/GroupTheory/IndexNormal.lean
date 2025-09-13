a4665279e0b8ee99b24f5eb6aa334faef8e2ce6d
Yaël Dillies <yael.dillies@gmail.com>
2025-09-13 17:16:14 +0000

    chore(GroupTheory/Index): rename `relindex` to `relIndex` (#19872)
    Zulip discussion about renaming: [#PR reviews > #19872 rename relindex to relIndex @ 💬](https://leanprover.zulipchat.com/#narrow/channel/144837-PR-reviews/topic/.2319872.20rename.20relindex.20to.20relIndex/near/515908054)

--- Mathlib/GroupTheory/IndexNormal.lean
+++ Mathlib/GroupTheory/IndexNormal.lean
@@ -46,12 +46,12 @@ theorem normal_of_index_eq_minFac_card (hHp : H.index = (Nat.card G).minFac) :
   by_cases hG1 : Nat.card G = 1
   · rw [hG1, minFac_one] at hHp
     exact normal_of_index_eq_one hHp
-  suffices H.normalCore.relindex H = 1 by
+  suffices H.normalCore.relIndex H = 1 by
     convert H.normalCore_normal
-    exact le_antisymm (relindex_eq_one.mp this) (normalCore_le H)
+    exact le_antisymm (relIndex_eq_one.mp this) (normalCore_le H)
   have : Finite G := finite_of_card_ne_zero hG0
   have index_ne_zero : H.index ≠ 0 := index_ne_zero_of_finite
-  rw [← mul_left_inj' index_ne_zero, one_mul, relindex_mul_index H.normalCore_le]
+  rw [← mul_left_inj' index_ne_zero, one_mul, relIndex_mul_index H.normalCore_le]
   have hp : Nat.Prime H.index := hHp ▸ minFac_prime hG1
   have h : H.normalCore.index ∣ H.index ! := by
     rw [normalCore_eq_ker, index_ker, index_eq_card, ← Nat.card_perm]
