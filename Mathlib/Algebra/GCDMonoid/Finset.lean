67c6b09709b41827fcf9301bb6d396df7acfe79f
Yongshun Ye <ShreckYe@gmail.com>
2025-09-13 21:44:00 +0000

    feat(Algebra/GCDMonoid/Finset): change the signature of `lcm_eq_zero_iff` to its simp normal form and add `lcm_ne_zero_iff` theorems (#29208)
    This PR is similar to #28897 and I found a need for one of the added theorems in #29204.
    
    I've noticed that the existing theorem `lcm_eq_zero_iff`'s rhs term `0 ∈ f '' s` is not in simp normal form. Should this be deprecated and replaced? I have left comments about this in the code.
    
    Co-authored-by: Oliver Nash <7734364+ocfnash@users.noreply.github.com>

--- Mathlib/Algebra/GCDMonoid/Finset.lean
+++ Mathlib/Algebra/GCDMonoid/Finset.lean
@@ -101,9 +101,12 @@ theorem lcm_image [DecidableEq β] {g : γ → β} (s : Finset γ) :
 theorem lcm_eq_lcm_image [DecidableEq α] : s.lcm f = (s.image f).lcm id :=
   Eq.symm <| lcm_image _
 
-theorem lcm_eq_zero_iff [Nontrivial α] : s.lcm f = 0 ↔ 0 ∈ f '' s := by
-  simp only [Multiset.mem_map, lcm_def, Multiset.lcm_eq_zero_iff, Set.mem_image, mem_coe, ←
-    Finset.mem_def]
+@[simp]
+theorem lcm_eq_zero_iff [Nontrivial α] : s.lcm f = 0 ↔ ∃ x ∈ s, f x = 0 := by
+  simp only [lcm_def, Multiset.lcm_eq_zero_iff, Multiset.mem_map, mem_val]
+
+theorem lcm_ne_zero_iff [Nontrivial α] : s.lcm f ≠ 0 ↔ ∀ x ∈ s, f x ≠ 0 := by
+  simp [lcm_eq_zero_iff]
 
 end lcm
 
