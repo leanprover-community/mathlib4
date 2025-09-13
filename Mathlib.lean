613c841543192f63fd502d8f07adb6d109dfcad8
Christian Merten <136261474+chrisflav@users.noreply.github.com>
2025-09-13 02:46:44 +0000

    chore(RingTheory/Smooth): split `StandardSmooth` (#28725)
    We factor out the `SubmersivePresentation` part into a separate file to prepare for new content. Also the module docstrings and TODOs are updated.

--- Mathlib.lean
+++ Mathlib.lean
@@ -5425,6 +5425,7 @@ import Mathlib.RingTheory.Extension.Cotangent.Basic
 import Mathlib.RingTheory.Extension.Cotangent.LocalizationAway
 import Mathlib.RingTheory.Extension.Generators
 import Mathlib.RingTheory.Extension.Presentation.Basic
+import Mathlib.RingTheory.Extension.Presentation.Submersive
 import Mathlib.RingTheory.FilteredAlgebra.Basic
 import Mathlib.RingTheory.Filtration
 import Mathlib.RingTheory.FiniteLength
