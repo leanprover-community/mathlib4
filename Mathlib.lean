d116b80e87708ca2dca21b8dda279bef679a9899
Uni Marx <uniwuni@protonmail.com>
2025-09-13 20:04:16 +0200

    mathlib update🦔

--- Mathlib.lean
+++ Mathlib.lean
@@ -5301,6 +5301,7 @@ import Mathlib.RepresentationTheory.Basic
 import Mathlib.RepresentationTheory.Character
 import Mathlib.RepresentationTheory.Coinduced
 import Mathlib.RepresentationTheory.Coinvariants
+import Mathlib.RepresentationTheory.Commutative
 import Mathlib.RepresentationTheory.FDRep
 import Mathlib.RepresentationTheory.FiniteIndex
 import Mathlib.RepresentationTheory.GroupCohomology.Basic
