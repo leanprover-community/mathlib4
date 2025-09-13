d7e4ee3a51004aab5ffae9d0fb0e22f9491b63be
Anthony Fernandes <fernandes10.anthony@gmail.com>
2025-09-13 02:01:33 +0200

    golf

--- Mathlib/RingTheory/Noetherian/OfPrime.lean
+++ Mathlib/RingTheory/Noetherian/OfPrime.lean
@@ -61,16 +61,11 @@ open Ideal
 /-- If all prime ideals in a commutative ring are finitely generated, so are all other ideals. -/
 theorem IsNoetherianRing.of_prime (H : ∀ I : Ideal R, I.IsPrime → I.FG) :
     IsNoetherianRing R := by
-  refine ⟨isOka_fg.forall_of_forall_prime (fun h ↦ ?_) H⟩
-  refine zorn_le₀ { I : Ideal R | ¬I.FG } (fun C hC hC₂ ↦ ?_)
-  by_cases H : C.Nonempty
-  · refine ⟨sSup C, ?_, fun _ ↦ le_sSup⟩
-    intro ⟨G, hG⟩
-    obtain ⟨I, I_mem_C, G_subset_I⟩ : ∃ I ∈ C, G.toSet ⊆ I := by
-      refine hC₂.directedOn.exists_mem_subset_of_finset_subset_biUnion H (fun _ hx ↦ ?_)
-      simp only [Set.mem_iUnion, SetLike.mem_coe, exists_prop]
-      exact (Submodule.mem_sSup_of_directed H hC₂.directedOn).1 <| hG ▸ subset_span hx
-    have I_eq_sSup : I = sSup C := le_antisymm (le_sSup I_mem_C) (hG ▸ Ideal.span_le.2 G_subset_I)
-    exact hC I_mem_C ⟨G, I_eq_sSup ▸ hG⟩
-  · obtain ⟨I, hI⟩ := h
-    exact ⟨I, hI, by simp [Set.not_nonempty_iff_eq_empty.1 H]⟩
+  refine ⟨isOka_fg.forall_of_forall_prime' (fun C hC₁ hC₂ I hI h ↦ ⟨sSup C, ?_, h⟩) H⟩
+  obtain ⟨G, hG⟩ := h
+  obtain ⟨J, J_mem_C, G_subset_J⟩ : ∃ J ∈ C, G.toSet ⊆ J := by
+    refine hC₂.directedOn.exists_mem_subset_of_finset_subset_biUnion ⟨I, hI⟩ (fun _ hx ↦ ?_)
+    simp only [Set.mem_iUnion, SetLike.mem_coe, exists_prop]
+    exact (Submodule.mem_sSup_of_directed ⟨I, hI⟩ hC₂.directedOn).1 <| hG ▸ subset_span hx
+  suffices J_eq_sSup : J = sSup C from J_eq_sSup ▸ J_mem_C
+  exact le_antisymm (le_sSup J_mem_C) (hG ▸ Ideal.span_le.2 G_subset_J)
