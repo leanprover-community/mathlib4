85ee89d1ca01d09052d45ee8a0890e3c72918071
Christopher Hoskin <mans0954@users.noreply.github.com>
2025-09-13 18:34:28 +0100

    WhiteSpace

--- Mathlib/Topology/Algebra/Module/WeakBilin.lean
+++ Mathlib/Topology/Algebra/Module/WeakBilin.lean
@@ -5,7 +5,6 @@ Authors: Kalle Kytölä, Moritz Doll
 -/
 import Mathlib.Topology.Algebra.Module.LinearMap
 import Mathlib.LinearAlgebra.SesquilinearForm
-
 import Mathlib.Topology.Algebra.Module.Holding
 import Mathlib.Topology.Algebra.Module.StrongTopology
 
