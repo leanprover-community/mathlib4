029c46d7d1f0384b68053b1c7e6484312cc6a767
Bryan Wang Peng Jun <bwangpengjun@math.harvard.edu>
2025-09-14 16:21:00 +0000

    feat(AlgebraicGeometry/EllipticCurve): reduction of elliptic curves (#28115)
    Define the reduction of elliptic curves over the fraction field of a DVR, following Silverman.
    
    Co-authored-by: bwangpj <70694994+bwangpj@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1209,6 +1209,7 @@ import Mathlib.AlgebraicGeometry.EllipticCurve.NormalForms
 import Mathlib.AlgebraicGeometry.EllipticCurve.Projective.Basic
 import Mathlib.AlgebraicGeometry.EllipticCurve.Projective.Formula
 import Mathlib.AlgebraicGeometry.EllipticCurve.Projective.Point
+import Mathlib.AlgebraicGeometry.EllipticCurve.Reduction
 import Mathlib.AlgebraicGeometry.EllipticCurve.VariableChange
 import Mathlib.AlgebraicGeometry.EllipticCurve.Weierstrass
 import Mathlib.AlgebraicGeometry.Fiber
