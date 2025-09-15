fc35bc6bafc7905184001d7fc0299514a931b984
Lawrence Wu <git@llllvvuu.dev>
2025-09-14 17:39:16 -0700

    more api

--- Mathlib/junk.lean
+++ Mathlib/junk.lean
@@ -16,30 +16,31 @@ We define *diagonalization* as the special case where `α` is `Unit`.
 
 * `LinearMap.diagonalization_of_isDiag_toMatrix`:
   a basis for which the matrix representation is diagonal is a diagonalization.
-* `LinearMap.Diagonalization.μ_equiv`:
-  all diagonalizations of a linear map have the same eigenvalues up to permutation.
-* `LinearMap.exists_diagonalization_iff_iSup_eigenspace`:
-  a linear map is diagonalizable iff the eigenspaces span the whole space.
 * `LinearMap.exists_diagonalization_iff_directSum_eigenspace`:
   a linear map is diagonalizable iff the direct sum of the eigenspaces is the whole space.
+* `LinearMap.exists_diagonalization_iff_iSup_eigenspace`:
+  a linear map is diagonalizable iff the eigenspaces span the whole space.
 * `LinearMap.exists_diagonalization_iff_minpoly_splits_and_squarefree`:
   a linear map is diagonalizable iff the minimal polynomial splits and is squarefree.
 * `LinearMap.exists_simultaneousDiagonalization_iff_commute`: a family of linear maps is
   simultaneously diagonalizable iff they commute and are individually diagonalizable.
+* `LinearMap.Diagonalization.μ_equiv`:
+  all diagonalizations of a linear map have the same eigenvalues up to permutation.
 
 ## TODO
 
+* `IsIdempotentElem` implies diagonalizable (https://math.stackexchange.com/a/1017060/315369)
 * Treatment of normal endomorphisms
 -/
 
 namespace LinearMap
 
-open Module Matrix Polynomial
+open Module End Matrix Polynomial
 
-universe u
+universe u v
 
-variable {α R : Type*} {M : Type u} [CommRing R] [AddCommGroup M] [Module R M]
-variable {K : Type*} [Field K] [Module K M]
+variable {α : Type*} {R : Type u} {M : Type v} [CommRing R] [AddCommGroup M] [Module R M]
+variable {K : Type u} [Field K] [Module K M]
 
 /--
 A diagonalization of a family of linear maps $T_i : V \to V$ is a basis of $V$
@@ -79,15 +80,51 @@ noncomputable def Diagonalization.mk' {ι : Type*} {f : End R M} {b : Basis ι R
   μ := fun _ i ↦ (exists_hasEigenVector i).choose
   hasEigenVector_μ := fun _ i => (exists_hasEigenVector i).choose_spec
 
+/-- A basis for the module gives a diagonalization for the zero map. -/
+def diagonalization_zero [Nontrivial R] {ι : Type*} (b : Basis ι R M) :
+    (0 : End R M).Diagonalization ι :=
+  Diagonalization.mk (b := b) (μ := 0) (by simp [hasEigenvector_iff, b.ne_zero])
+
+/-- A basis for the module gives a diagonalization for the identity map. -/
+def diagonalization_one [Nontrivial R] {ι : Type*} (b : Basis ι R M) :
+    (1 : End R M).Diagonalization ι :=
+  Diagonalization.mk (b := b) (μ := 1) (by simp [hasEigenvector_iff, b.ne_zero])
+
+/-- A diagonalization of `f` can be scaled to give a diagonalization of `c • f`. -/
+def SimultaneousDiagonalization.smul {ι : Type*} {f : α → End R M}
+    (D : SimultaneousDiagonalization ι f) (c : R) : SimultaneousDiagonalization ι (c • f) where
+  toBasis := D.toBasis
+  μ := fun a i ↦ c * D.μ a i
+  hasEigenVector_μ a i := by
+    have := D.hasEigenVector_μ a i
+    simp_all [hasEigenvector_iff, smul_smul]
+
+def Diagonalization.smul {ι : Type*} {f : End R M} (D : f.Diagonalization ι) (c : R) :
+    (c • f).Diagonalization ι :=
+  SimultaneousDiagonalization.smul D c
+
+def SimultaneousDiagonalization.diagonalization_sum [Fintype α] {ι : Type*} {f : α → End R M}
+    (D : SimultaneousDiagonalization ι f) : (∑ a, f a).Diagonalization ι :=
+  Diagonalization.mk (b := D.toBasis) (μ := fun i ↦ ∑ a, D.μ a i) <| by
+    sorry
+
+def SimultaneousDiagonalization.diagonalization_mul [Fintype α] {ι : Type*} {f : α → End R M}
+    (D : SimultaneousDiagonalization ι f) (i j : α) : (f i * f j).Diagonalization ι :=
+  Diagonalization.mk (b := D.toBasis) (μ := fun k ↦ D.μ i k * D.μ j k) <| by
+    sorry
+
 /--
 Alternative constructor for `LinearMap.Diagonalization` from a basis of eigenvectors and
 diagonality of the matrix representation.
 -/
-noncomputable def diagonalization_of_isDiag_toMatrix {ι : Type*} [Fintype ι] [DecidableEq ι]
-    {f : End R M} {b : Basis ι R M} (h : (f.toMatrix b b).IsDiag) :
+noncomputable def diagonalization_of_isDiag_toMatrix [Nontrivial R] {ι : Type*} [Fintype ι]
+    [DecidableEq ι] {f : End R M} {b : Basis ι R M} (h : (f.toMatrix b b).IsDiag) :
     f.Diagonalization ι :=
   Diagonalization.mk (b := b) (μ := fun i ↦ f.toMatrix b b i i) <| by
-    sorry
+    intro i
+    have : f (b i) = ∑ j, f.toMatrix b b j i • b j := by simp [toMatrix_apply, b.sum_repr]
+    have (j : ι) (hj : j ≠ i) : f.toMatrix b b j i = 0 := h hj
+    simp_all [hasEigenvector_iff, Finset.sum_eq_single i, b.ne_zero]
 
 /-- The diagonalization of the endomorphism indexed by `a`. -/
 def SimultaneousDiagonalization.diagonalization {ι : Type*} {f : α → Module.End R M}
@@ -132,21 +169,39 @@ lemma Diagonalization.isRoot_charpoly {ι : Type*} [Fintype ι] [DecidableEq ι]
 
 lemma Diagonalization.iSup_eigenspace {ι : Type*} {f : End R M} (D : f.Diagonalization ι) :
     ⨆ μ, f.eigenspace μ = ⊤ := by
+  rw [eq_top_iff, ← Basis.span_eq D.toBasis, Submodule.span_le, Set.range_subset_iff]
+  exact fun i ↦ Submodule.mem_iSup_of_mem (D.μ i) (hasEigenvector_iff.mp (D.hasEigenVector_μ i)).1
+
+-- https://mathoverflow.net/questions/16953/are-submodules-of-free-modules-free#comment31868_16954
+instance {R M : Type*} [Semiring R] [AddCommMonoid M] [IsPrincipalIdealRing R] [IsDomain R] [Module R M] [Module.Free R M] (N : Submodule R M) : Module.Free R N := by
   sorry
 
-lemma exists_diagonalization_iff_iSup_eigenspace {f : End R M} [Free R M] :
-    (∃ ι : Type u, Nonempty (f.Diagonalization ι)) ↔ ⨆ μ, f.eigenspace μ = ⊤ := by
-  sorry
+lemma exists_diagonalization_iff_directSum_eigenspace [IsPrincipalIdealRing R] [IsDomain R]
+    [DecidableEq R] [NoZeroSMulDivisors R M] [Free R M] {f : End R M} :
+    (∃ ι : Type v, Nonempty (f.Diagonalization ι)) ↔ DirectSum.IsInternal f.eigenspace := by
+  constructor <;> intro h
+  · obtain ⟨ι, ⟨D⟩⟩ := h
+    simp [DirectSum.isInternal_submodule_iff_iSupIndep_and_iSup_eq_top, eigenspaces_iSupIndep f,
+      D.iSup_eigenspace]
+  · let v := fun i ↦ (Free.exists_basis R (f.eigenspace i)).some.2
+    let B' := h.collectedBasis v -- universe (max u v)
+    let e := (B'.indexEquiv (Free.exists_basis R M).some.2)
+    let B := B'.reindex e -- move to universe v
+    refine ⟨_, ⟨Diagonalization.mk' (b := B) fun i ↦ ?_⟩⟩
+    use (e.symm i).fst
+    rw [hasEigenvector_iff, B'.reindex_apply]
+    refine ⟨h.collectedBasis_mem v _, B'.ne_zero _⟩
 
-lemma exists_diagonalization_iff_directSum_eigenspace {f : End R M}
-    [Free R M] [NoZeroSMulDivisors R M] [DecidableEq R] :
-    (∃ ι : Type u, Nonempty (f.Diagonalization ι)) ↔ DirectSum.IsInternal f.eigenspace := by
-  simp [DirectSum.isInternal_submodule_iff_iSupIndep_and_iSup_eq_top,
-    Module.End.eigenspaces_iSupIndep f, exists_diagonalization_iff_iSup_eigenspace]
+lemma exists_diagonalization_iff_iSup_eigenspace [IsPrincipalIdealRing R] [IsDomain R]
+    [NoZeroSMulDivisors R M] [Free R M] {f : End R M} :
+    (∃ ι : Type v, Nonempty (f.Diagonalization ι)) ↔ ⨆ μ, f.eigenspace μ = ⊤ := by classical calc
+  _ ↔ DirectSum.IsInternal f.eigenspace := exists_diagonalization_iff_directSum_eigenspace
+  _ ↔ _ := by
+    simp [DirectSum.isInternal_submodule_iff_iSupIndep_and_iSup_eq_top, eigenspaces_iSupIndep f]
 
 -- probably follow-up work
 theorem exists_diagonalization_iff_minpoly_splits_and_squarefree {f : End K M} :
-    (∃ ι : Type u, Nonempty (f.Diagonalization ι)) ↔
+    (∃ ι : Type v, Nonempty (f.Diagonalization ι)) ↔
     (minpoly K f).Splits (RingHom.id K) ∧ Squarefree (minpoly K f) := by
   sorry
 
@@ -162,8 +217,8 @@ theorem LinearMap.Diagonalization.μ_equiv {ι ι' R M : Type*} [CommRing R] [Is
   sorry
 
 theorem exists_simultaneousDiagonalization_iff_commute {f : α → End R M} :
-    (∃ ι : Type u, Nonempty (SimultaneousDiagonalization ι f)) ↔
-    (∀ i : α, ∃ ι : Type u, Nonempty (Diagonalization ι (f i))) ∧
+    (∃ ι : Type v, Nonempty (SimultaneousDiagonalization ι f)) ↔
+    (∀ i : α, ∃ ι : Type v, Nonempty (Diagonalization ι (f i))) ∧
     ∀ i j : α, Commute (f i) (f j) := by
   sorry
 
