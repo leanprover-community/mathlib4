4305686ff77da077b2bcc4f79a21138065f7c361
DavidLedvinka <50644608+DavidLedvinka@users.noreply.github.com>
2025-09-15 03:32:50 -0400

    Update Mathlib/MeasureTheory/Measure/Decomposition/RadonNikodym.lean
    Co-authored-by: Rémy Degenne <remydegenne@gmail.com>

--- Mathlib/MeasureTheory/Measure/Decomposition/RadonNikodym.lean
+++ Mathlib/MeasureTheory/Measure/Decomposition/RadonNikodym.lean
@@ -528,14 +528,17 @@ variable {G : Type*} [Group G] [MeasureSpace G] [MeasurableMul₂ G] [Measurable
   {μ : Measure G} [IsMulLeftInvariant μ]
 
 @[to_additive]
-theorem rnDeriv_mconv [SFinite μ] (ν₁ ν₂ : Measure G) [IsFiniteMeasure ν₁] [IsFiniteMeasure ν₂]
+theorem rnDeriv_mconv [SFinite μ] {ν₁ ν₂ : Measure G} [IsFiniteMeasure ν₁] [IsFiniteMeasure ν₂]
     [ν₁.HaveLebesgueDecomposition μ] [ν₂.HaveLebesgueDecomposition μ]
-    [(ν₁ ∗ₘ ν₂).HaveLebesgueDecomposition μ] (hν₁ : ν₁ ≪ μ) (hν₂ : ν₂ ≪ μ) :
+    (hν₁ : ν₁ ≪ μ) (hν₂ : ν₂ ≪ μ) :
     (ν₁ ∗ₘ ν₂).rnDeriv μ =ᵐ[μ] (ν₁.rnDeriv μ) ⋆ₘₗ[μ] (ν₂.rnDeriv μ) := by
+  have h_eq : ν₁ ∗ₘ ν₂ = μ.withDensity (ν₁.rnDeriv μ ⋆ₘₗ[μ] ν₂.rnDeriv μ) := by
+    rw [← mconv_withDensity_eq_mlconvolution (by fun_prop) (by fun_prop),
+      withDensity_rnDeriv_eq _ _ hν₁, withDensity_rnDeriv_eq _ _ hν₂]
+  have : (ν₁ ∗ₘ ν₂).HaveLebesgueDecomposition μ :=
+    ⟨⟨0, (ν₁.rnDeriv μ) ⋆ₘₗ[μ] (ν₂.rnDeriv μ)⟩, by fun_prop, by simp, by simpa⟩
   rw [← withDensity_eq_iff (by fun_prop) (by fun_prop),
-    withDensity_rnDeriv_eq _ _ (mconv_absolutelyContinuous hν₂),
-    ← mconv_withDensity_eq_mlconvolution (by fun_prop) (by fun_prop),
-    withDensity_rnDeriv_eq _ _ hν₁, withDensity_rnDeriv_eq _ _ hν₂]
+    withDensity_rnDeriv_eq _ _ (mconv_absolutelyContinuous hν₂), h_eq]
   exact (lintegral_rnDeriv_lt_top (ν₁ ∗ₘ ν₂) μ).ne
 
 @[to_additive]
