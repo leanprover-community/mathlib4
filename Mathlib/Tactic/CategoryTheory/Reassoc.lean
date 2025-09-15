e15df763ca9283fe8dbea472c15d3b3e2264897e
leanprover-community-bot-assistant <97194829+leanprover-community-bot-assistant@users.noreply.github.com>
2025-09-15 00:32:52 +0000

    chore: adaptations for nightly-2025-09-12 (#61)
    Co-authored-by: leanprover-community-mathlib4-bot <leanprover-community-mathlib4-bot@users.noreply.github.com>
    Co-authored-by: Rob23oba <robin.arnez@web.de>
    Co-authored-by: mathlib4-bot <github-mathlib4-bot@leanprover.zulipchat.com>
    Co-authored-by: Kim Morrison <477956+kim-em@users.noreply.github.com>
    Co-authored-by: Kim Morrison <kim@tqft.net>
    Co-authored-by: github-actions <github-actions@github.com>

--- Mathlib/Tactic/CategoryTheory/Reassoc.lean
+++ Mathlib/Tactic/CategoryTheory/Reassoc.lean
@@ -3,7 +3,6 @@ Copyright (c) 2022 Kim Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kim Morrison, Robin Carlier
 -/
-import Mathlib.Tactic.CategoryTheory.Refs
 import Mathlib.CategoryTheory.Functor.Basic
 import Mathlib.Lean.Meta.Simp
 import Mathlib.Tactic.Simps.Basic
@@ -32,6 +31,66 @@ open Lean Meta Elab Tactic
 open Mathlib.Tactic
 
 namespace CategoryTheory
+
+/-- A variant of `eq_whisker` with a more convenient argument order for use in tactics. -/
+theorem eq_whisker' {C : Type*} [Category C]
+    {X Y : C} {f g : X ⟶ Y} (w : f = g) {Z : C} (h : Y ⟶ Z) :
+    f ≫ h = g ≫ h := by rw [w]
+
+/-- Simplify an expression using only the axioms of a category. -/
+def categorySimp (e : Expr) : MetaM Simp.Result :=
+  simpOnlyNames [``Category.comp_id, ``Category.id_comp, ``Category.assoc,
+    ``Functor.id_obj, ``Functor.id_map, ``Functor.comp_obj, ``Functor.comp_map] e
+    (config := { decide := false })
+
+/--
+Given an equation `f = g` between morphisms `X ⟶ Y` in a category,
+produce the equation `∀ {Z} (h : Y ⟶ Z), f ≫ h = g ≫ h`,
+but with compositions fully right associated and identities removed.
+Also returns the category `C` and any instance metavariables that need to be solved for.
+-/
+def reassocExprHom (e : Expr) : MetaM (Expr × Array MVarId) := do
+  let lem₀ ← mkConstWithFreshMVarLevels ``eq_whisker'
+  let (args, _, _) ← forallMetaBoundedTelescope (← inferType lem₀) 7
+  let inst := args[1]!
+  inst.mvarId!.setKind .synthetic
+  let w := args[6]!
+  w.mvarId!.assignIfDefEq e
+  withEnsuringLocalInstance inst.mvarId! do
+    return (← simpType categorySimp (mkAppN lem₀ args), #[inst.mvarId!])
+
+/--
+Adding `@[reassoc]` to a lemma named `F` of shape `∀ .., f = g`, where `f g : X ⟶ Y` are
+morphisms in some category, will create a new lemma named `F_assoc` of shape
+`∀ .. {Z : C} (h : Y ⟶ Z), f ≫ h = g ≫ h`
+but with the conclusions simplified using the axioms for a category
+(`Category.comp_id`, `Category.id_comp`, and `Category.assoc`).
+So, for example, if the conclusion of `F` is `a ≫ b = g` then
+the conclusion of `F_assoc` will be `a ≫ (b ≫ h) = g ≫ h` (note that `≫` reassociates
+to the right so the brackets will not appear in the statement).
+
+This attribute is useful for generating lemmas which the simplifier can use even on expressions
+that are already right associated.
+
+Note that if you want both the lemma and the reassociated lemma to be
+`simp` lemmas, you should tag the lemma `@[reassoc (attr := simp)]`.
+The variant `@[simp, reassoc]` on a lemma `F` will tag `F` with `@[simp]`,
+but not `F_assoc` (this is sometimes useful).
+
+This attribute also works for lemmas of shape `∀ .., f = g` where `f g : X ≅ Y` are
+isomorphisms, provided that `Tactic.CategoryTheory.IsoReassoc` has been imported.
+-/
+syntax (name := reassoc) "reassoc" (" (" &"attr" " := " Parser.Term.attrInstance,* ")")? : attr
+
+/--
+IO ref for reassociation handlers `reassoc` attribute, so that it can be extended
+with additional handlers. Handlers take a proof of the equation.
+
+The default handler is `reassocExprHom` for morphism reassociation.
+This will be extended in `Tactic.CategoryTheory.IsoReassoc` for isomorphism reassociation.
+-/
+private initialize reassocImplRef : IO.Ref (Array (Expr → MetaM (Expr × Array MVarId))) ←
+  IO.mkRef #[reassocExprHom]
 
 /--
 Registers a handler for `reassocExpr`. The handler takes a proof of an equation
