b97c80b6db8a692891acd8a329859fe6eae306b5
Violeta Hernández <vi.hdz.p@gmail.com>
2025-09-15 12:44:02 -0600

    continue

--- Mathlib/Data/Real/StandardPart.lean
+++ Mathlib/Data/Real/StandardPart.lean
@@ -31,7 +31,7 @@ private theorem exists_mk_sub_real_mul_gt' (f : ℝ →+* K) (hf : StrictMono f)
   obtain h | h := h.lt_or_eq
   · use 0; simpa
   · let s := {r : ℝ | f r * y ≤ x}
-    have hs₁ : Nonempty s := by
+    have hs₁ : s.Nonempty := by
       obtain ⟨r, hr, hr'⟩ := (mk_le_mk_iff_denselyOrdered f hf).1 h.ge
       rw [abs_of_pos hx, abs_of_pos hy] at hr'
       exact ⟨r, hr'⟩
@@ -45,35 +45,40 @@ private theorem exists_mk_sub_real_mul_gt' (f : ℝ →+* K) (hf : StrictMono f)
       refine fun r₁ r₂ hr hrs ↦ hrs.trans' ?_
       rw [mul_le_mul_iff_left₀ hy]
       exact hf.monotone hr
-    have hs₄ : Set.Iio (sSup s) ⊆ s := sorry
     use sSup s
     rw [mk_lt_mk]
     rintro (_ | n)
     · simpa using hy.ne'
-    have hf : 0 < f (n + 1)⁻¹ := by
+    have hfn : 0 < f (n + 1)⁻¹ := by
       rw [← f.map_zero, hf.lt_iff_lt, inv_pos]
       exact n.cast_add_one_pos
-    have hf' : f (n + 1)⁻¹ * (n + 1 :) = 1 := by
+    have hfn' : f (n + 1)⁻¹ * (n + 1 :) = 1 := by
       rw [← map_natCast f, Nat.cast_add_one, ← f.map_mul, inv_mul_cancel₀, f.map_one]
       exact n.cast_add_one_ne_zero
     by_contra! hn
     rw [abs_of_pos hy] at hn
     obtain hs | hs := le_total (x - f (sSup s) * y) 0
     · rw [abs_of_nonpos hs, nsmul_eq_mul, mul_neg, mul_sub, ← mul_assoc, le_neg, sub_le_iff_le_add,
-        ← neg_one_mul, ← add_mul, ← mul_le_mul_iff_right₀ hf, ← mul_assoc, hf', one_mul,
-        ← mul_assoc, mul_add, ← mul_assoc, hf', one_mul, mul_neg_one,
+        ← neg_one_mul, ← add_mul, ← mul_le_mul_iff_right₀ hfn, ← mul_assoc, hfn', one_mul,
+        ← mul_assoc, mul_add, ← mul_assoc, hfn', one_mul, mul_neg_one,
         ← f.map_neg, ← f.map_add] at hn
-      rw [hn.antisymm (hs₄ sorry), ← sub_mul, ← f.map_sub, add_sub_cancel_right] at hs
-      sorry
+      have hn' : -(n + 1 : ℝ)⁻¹ + sSup s < sSup s := by simpa using n.cast_add_one_pos
+      obtain ⟨a, ha, ha'⟩ := exists_lt_of_lt_csSup hs₁ hn'
+      apply ha'.not_ge
+      rw [← hf.le_iff_le, ← mul_le_mul_iff_left₀ hy]
+      exact ha.trans_eq <| hn.antisymm (hs₃ ha'.le ha)
     · rw [abs_of_nonneg hs, nsmul_eq_mul, mul_sub, ← mul_assoc, le_sub_iff_add_le,
-        ← one_add_mul] at hn
-      sorry
+        ← one_add_mul, ← mul_le_mul_iff_right₀ hfn, ← mul_assoc, mul_add, ← mul_assoc, hfn',
+        one_mul, mul_one, ← mul_assoc, hfn', one_mul, ← f.map_add] at hn
+      have hn' : sSup s < (n + 1 : ℝ)⁻¹ + sSup s := by simpa using n.cast_add_one_pos
+      apply hn.not_gt
+      simpa [s] using notMem_of_csSup_lt hn' hs₂
 
-#exit
 theorem exists_mk_sub_real_mul_gt (f : ℝ →+* K) (hf : StrictMono f)
-    (hx : x ≠ 0) (h : mk y ≤ mk x) : ∃ r : ℝ, mk y < mk (x - f r * y) := by
+    (hy : y ≠ 0) (h : mk y ≤ mk x) : ∃ r : ℝ, mk y < mk (x - f r * y) := by
+  obtain rfl | hx := eq_or_ne x 0
+  · use 0; simp_all [lt_top_iff_ne_top]
   have H (x y r) : mk (-x - f r * y) = mk (x + f r * y) := by rw [← neg_add', mk_neg]
-  have hy : y ≠ 0 := by aesop
   obtain hx | hx := hx.lt_or_gt <;> obtain hy | hy := hy.lt_or_gt
   · convert exists_mk_sub_real_mul_gt' f hf (neg_pos.2 hx) (neg_pos.2 hy) (by simpa) using 3
     · simp
@@ -85,25 +90,56 @@ theorem exists_mk_sub_real_mul_gt (f : ℝ →+* K) (hf : StrictMono f)
     convert exists_mk_sub_real_mul_gt' f hf hx (neg_pos.2 hy) (by simpa) using 3 <;> simp
   · exact exists_mk_sub_real_mul_gt' f hf hx hy h
 
-theorem exists_mk_add_real_mul_gt (f : ℝ →+* K) (hf : StrictMono f) (hx : x ≠ 0) (h : mk y ≤ mk x) :
+theorem exists_mk_add_real_mul_gt (f : ℝ →+* K) (hf : StrictMono f) (hy : y ≠ 0) (h : mk y ≤ mk x) :
     ∃ r : ℝ, mk y < mk (x + f r * y) := by
   rw [(Equiv.neg _).exists_congr_left]
-  simpa [← sub_eq_add_neg] using exists_mk_sub_real_mul_gt f hf hx h
+  simpa [← sub_eq_add_neg] using exists_mk_sub_real_mul_gt f hf hy h
 
+open Classical in
 /-- The standard part of a ratio `x / y` is the unique real `r` such that
 `ArchimedeanClass.mk y < ArchimedeanClass.mk (x - f r * y)`. -/
-noncomputable def standardPart (f : ℝ →+* K) (hf : StrictMono f) (x y : K) : ℝ :=
-  if h : x ≠ 0 ∧ mk y ≤ mk x then Classical.choose (exists_mk_add_real_mul_gt f hf h.1 h.2) else 0
+noncomputable def standardPart (f : ℝ →+* K) (x y : K) : ℝ :=
+  if H : ∃ r : ℝ, mk y < mk (x - f r * y) then Classical.choose H else 0
+
+theorem mk_lt_mk_sub_standardPart_mul (f : ℝ →+* K) (hf : StrictMono f)
+    (hy : y ≠ 0) (h : mk y ≤ mk x) : mk y < mk (x - f (standardPart f x y) * y) := by
+  have H := exists_mk_sub_real_mul_gt f hf hy h
+  rw [standardPart, dif_pos H]
+  exact Classical.choose_spec H
+
+theorem standardPart_of_mk_lt_mk (f : ℝ →+* K) (hf : StrictMono f) {r : ℝ}
+    (h : mk x < mk y) : standardPart f x y = 0 := by
+  apply dif_neg
+  intro ⟨r, hr⟩
+  apply hr.not_ge
+  rw [mk_sub_eq_mk_left]
+
+
+  #exit
+
+theorem standardPart_of_mk_sub_gt_mk (f : ℝ →+* K) (hf : StrictMono f) {r : ℝ}
+    (h : mk y < mk (x - f r * y)) : standardPart f x y = r := by
+  have hy : y ≠ 0 := by aesop
+
+  sorry
+
+  #exit
 
 @[simp]
-theorem standardPart_zero_left (f : ℝ →+* K) (hf : StrictMono f) (y : K) :
-    standardPart f hf 0 y = 0 := by
+theorem standardPart_zero_right (f : ℝ →+* K) (x : K) : standardPart f x 0 = 0 := by
   simp [standardPart]
 
-@[simp]
-theorem standardPart_zero_right (f : ℝ →+* K) (hf : StrictMono f) (x : K) :
-    standardPart f hf x 0 = 0 := by
-  simp [standardPart]
+theorem standardPart_zero_left (f : ℝ →+* K) (hf : StrictMono f) (x : K) :
+    standardPart f 0 x = 0 := by
+  obtain rfl | hx := eq_or_ne x 0
+  · exact standardPart_zero_right f 0
+  · rw [standardPart]
+    split_ifs
+    apply dif_neg
+    simp only [zero_sub, mk_neg,  not_exists, not_lt]
+
+#exit
+
 #exit
 
 theorem existsUnique_mk_sub_real_mul_gt (f : ℝ →+* K) (hf : StrictMono f)
