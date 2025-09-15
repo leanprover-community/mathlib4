35da2f2f03df4d98bea93f96d81acafa3d234bf0
Damiano Testa <adomani@gmail.com>
2025-09-15 22:23:59 +0200

    mk_all

--- Mathlib.lean
+++ Mathlib.lean
@@ -6122,6 +6122,7 @@ import Mathlib.Tactic.Linter.Multigoal
 import Mathlib.Tactic.Linter.OldObtain
 import Mathlib.Tactic.Linter.PPRoundtrip
 import Mathlib.Tactic.Linter.Style
+import Mathlib.Tactic.Linter.TechDebtLinter
 import Mathlib.Tactic.Linter.TextBased
 import Mathlib.Tactic.Linter.UnusedTactic
 import Mathlib.Tactic.Linter.UnusedTacticExtension
--- Mathlib/Tactic.lean
+++ Mathlib/Tactic.lean
@@ -165,6 +165,7 @@ import Mathlib.Tactic.Linter.Multigoal
 import Mathlib.Tactic.Linter.OldObtain
 import Mathlib.Tactic.Linter.PPRoundtrip
 import Mathlib.Tactic.Linter.Style
+import Mathlib.Tactic.Linter.TechDebtLinter
 import Mathlib.Tactic.Linter.TextBased
 import Mathlib.Tactic.Linter.UnusedTactic
 import Mathlib.Tactic.Linter.UnusedTacticExtension
