4e2c7dcf2e4a334ce822ee4b5e93041cbb8c96f0
Xavier Généreux <20421163+Louddy@users.noreply.github.com>
2025-09-16 12:06:35 +0000

    feat(SkewMonoidAlgebra): Modifying elements of skew monoid algebra at exactly one point (erase and update) (#29319)
    Basic results on updating/erasing an element of a skew monoid algebras using one point of the domain. Similar to `Finsupp.erase` and `Finsupp.update`
    
    Co-authored-by: María Inés de Frutos Fernández <[mariaines.dff@gmail.com](mailto:mariaines.dff@gmail.com)>
    Co-authored-by: Xavier Genereux <xaviergenereux@hotmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1153,6 +1153,7 @@ import Mathlib.Algebra.Ring.Units
 import Mathlib.Algebra.Ring.WithZero
 import Mathlib.Algebra.RingQuot
 import Mathlib.Algebra.SkewMonoidAlgebra.Basic
+import Mathlib.Algebra.SkewMonoidAlgebra.Single
 import Mathlib.Algebra.SkewMonoidAlgebra.Support
 import Mathlib.Algebra.Squarefree.Basic
 import Mathlib.Algebra.Star.Basic
