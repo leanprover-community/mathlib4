7f9a157f67002c81277741293e75728b53e08348
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2025-09-16 22:30:20 +0200

    feat: Ideal.sum_ramification_inertia_of_isLocalRing

--- Mathlib/NumberTheory/RamificationInertia/Basic.lean
+++ Mathlib/NumberTheory/RamificationInertia/Basic.lean
@@ -895,6 +895,27 @@ theorem sum_ramification_inertia (K L : Type*) [Field K] [Field L] [IsDedekindDo
       algebraMap_injective_of_field_isFractionRing R S K L, le_bot_iff]
   · exact finrank_quotient_map p K L
 
+/-- `Ideal.sum_ramification_inertia`, in the local (DVR) case. -/
+lemma sum_ramification_inertia_of_isLocalRing
+    (K L : Type*) [Field K] [Field L] [IsLocalRing S]
+    [IsDedekindDomain R] [Algebra R K] [IsFractionRing R K] [Algebra S L]
+    [IsFractionRing S L] [Algebra K L] [Algebra R L] [IsScalarTower R S L]
+    [IsScalarTower R K L] [Module.Finite R S] [p.IsMaximal] (hp0 : p ≠ ⊥) :
+    ramificationIdx (algebraMap R S) p (IsLocalRing.maximalIdeal S) *
+      p.inertiaDeg (IsLocalRing.maximalIdeal S) = Module.finrank K L := by
+  have := FaithfulSMul.of_field_isFractionRing R S K L
+  rw [← sum_ramification_inertia S p K L hp0]
+  symm
+  apply Finset.sum_eq_single_of_mem
+  · rw [← Finset.mem_coe, coe_primesOverFinset hp0]
+    obtain ⟨w', hmax, hover⟩ := Ideal.exists_ideal_liesOver_maximal_of_isIntegral p S
+    rw [← IsLocalRing.eq_maximalIdeal hmax]
+    exact ⟨hmax.isPrime, hover⟩
+  · intro b hb hnw
+    absurd hnw
+    rw [← Finset.mem_coe, coe_primesOverFinset hp0] at hb
+    exact IsLocalRing.eq_maximalIdeal <| Ideal.primesOver.isMaximal ⟨b, hb⟩
+
 end FactorsMap
 
 section tower
