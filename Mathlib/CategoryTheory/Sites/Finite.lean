fd6328e2896a15cc5ee93ab57e8b96437e687741
Kenny Lau <kc_kennylau@yahoo.com.hk>
2025-09-16 16:15:41 +0100

    remove newline

--- Mathlib/CategoryTheory/Sites/Finite.lean
+++ Mathlib/CategoryTheory/Sites/Finite.lean
@@ -3,7 +3,6 @@ Copyright (c) 2025 Kenny Lau. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kenny Lau
 -/
-
 import Mathlib.CategoryTheory.Sites.Pretopology
 import Mathlib.Data.Set.Finite.Lattice
 
