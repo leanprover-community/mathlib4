b9fd3788fdc3d837089e1fdeb79ef1e92de6723a
David Loeffler <d.loeffler.01@cantab.net>
2025-09-16 15:34:35 +0000

    feat(NumberTheory/ModularForms): define boundedness / vanishing at a cusp (#29560)

--- Mathlib.lean
+++ Mathlib.lean
@@ -4836,6 +4836,7 @@ import Mathlib.NumberTheory.MaricaSchoenheim
 import Mathlib.NumberTheory.Modular
 import Mathlib.NumberTheory.ModularForms.ArithmeticSubgroups
 import Mathlib.NumberTheory.ModularForms.Basic
+import Mathlib.NumberTheory.ModularForms.BoundedAtCusp
 import Mathlib.NumberTheory.ModularForms.CongruenceSubgroups
 import Mathlib.NumberTheory.ModularForms.Cusps
 import Mathlib.NumberTheory.ModularForms.DedekindEta
