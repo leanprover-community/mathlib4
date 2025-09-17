29dc1e72da0424d82f5bc66505cc6df5c824de1d
Violeta Hernández <vi.hdz.p@gmail.com>
2025-09-16 23:17:05 -0600

    sure

--- Mathlib.lean
+++ Mathlib.lean
@@ -5169,6 +5169,7 @@ import Mathlib.Order.PrimeIdeal
 import Mathlib.Order.PrimeSeparator
 import Mathlib.Order.Prod.Lex.Hom
 import Mathlib.Order.PropInstances
+import Mathlib.Order.Quotient
 import Mathlib.Order.Radical
 import Mathlib.Order.Rel.GaloisConnection
 import Mathlib.Order.RelClasses
