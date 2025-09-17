28ad8307f4b3104ef45ca67e720b30d41d4654c4
Anne Baanen <2116570+Vierkantor@users.noreply.github.com>
2025-09-17 01:02:36 +0000

    chore(*): replace `simp? says` with `simp only` (#29593)
    Replace the output of `simp? says` with a `simp only` where the proof does not become less readable, and then golf the result by trying to drop lemmas from the simp set, or even replace it outright with (`simp_`)`rw`.
    
    This is a weaker form of #29442.
    
    Co-authored-by: Anne C.A. Baanen <vierkantor@vierkantor.com>

--- Mathlib/Data/Set/Enumerate.lean
+++ Mathlib/Data/Set/Enumerate.lean
@@ -48,8 +48,7 @@ theorem enumerate_eq_none :
     · cases m with
       | zero => contradiction
       | succ m' =>
-        simp? [hs, enumerate] at h ⊢ says
-          simp only [enumerate, hs, Option.bind_eq_bind, Option.bind_some] at h ⊢
+        simp only [enumerate, hs] at h ⊢
         have hm : n ≤ m' := Nat.le_of_succ_le_succ hm
         exact enumerate_eq_none h hm
 
--- Mathlib/GroupTheory/CoprodI.lean
+++ Mathlib/GroupTheory/CoprodI.lean
@@ -758,9 +758,7 @@ theorem mulHead_prod {i j : ι} (w : NeWord M i j) (x : M i) (hnotone : x * w.he
   | singleton => simp [replaceHead]
   | append _ _ _ w_ih_w₁ w_ih_w₂ =>
     specialize w_ih_w₁ _ hnotone
-    clear w_ih_w₂
-    simp? [replaceHead, ← mul_assoc] at * says
-      simp only [replaceHead, head, append_prod, ← mul_assoc] at *
+    simp only [replaceHead, append_prod, ← mul_assoc]
     congr 1
 
 section Group
