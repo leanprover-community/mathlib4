761725cbd7ddf5f53bd8e7e6a2602ac9b03e27b0
Kim Morrison <kim@tqft.net>
2025-09-17 10:31:02 +1000

    copyright linter

--- Mathlib/Topology/Algebra/Group/Units.lean
+++ Mathlib/Topology/Algebra/Group/Units.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2025 Ruben Van de Velde and David Ledvinka. All rights reserved.
+Copyright (c) 2025 Ruben Van de Velde. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Ruben Van de Velde and David Ledvinka
+Authors: Ruben Van de Velde, David Ledvinka
 -/
 import Mathlib.Algebra.Group.Pi.Units
 import Mathlib.Algebra.Group.Submonoid.Units
