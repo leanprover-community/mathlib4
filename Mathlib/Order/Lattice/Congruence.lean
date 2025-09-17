555f78ae62c85e1dcff479cd33aa7689fbc5d9e3
Christopher Hoskin <mans0954@users.noreply.github.com>
2025-09-17 20:31:00 +0100

    Golf

--- Mathlib/Order/Lattice/Congruence.lean
+++ Mathlib/Order/Lattice/Congruence.lean
@@ -78,7 +78,7 @@ def mk' [Lattice α] (r : α → α → Prop) (h₁ : IsRefl α r)
     (h₃ : ∀ ⦃x y z : α⦄, x ≤ y → y ≤ z → r x y → r y z → r x z)
     (h₄ : ∀ ⦃x y t : α⦄, x ≤ y → r x y → r (x ⊓ t) (y ⊓ t) ∧ r (x ⊔ t) (y ⊔ t)) : LatticeCon α :=
   LatticeCon.mk (Setoid.mk r (Equivalence.mk h₁.refl
-  (fun h => by rw [h₂, inf_comm, sup_comm, ← h₂]; exact h)
+  (fun h => by simpa [h₂, inf_comm, sup_comm, ← h₂] using h)
   (fun hxy hxz => transitive h₂ h₃ h₄ hxy hxz))) (by
     intro w _ _ _ h1 h2
     have compatible_left_inf {x y t : α} (hh : r x y) : r (x ⊓ t) (y ⊓ t) :=
