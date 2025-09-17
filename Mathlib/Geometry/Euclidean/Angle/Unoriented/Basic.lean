d57006748cf2176699f01b0a5e299b10fa6ccb05
LessnessRandomness <lessnessr@gmail.com>
2025-09-17 20:09:34 +0300

    change order of import to alphabetic in Geometry/Euclidean/Unoriented/Bassic too

--- Mathlib/Geometry/Euclidean/Angle/Unoriented/Basic.lean
+++ Mathlib/Geometry/Euclidean/Angle/Unoriented/Basic.lean
@@ -3,9 +3,10 @@ Copyright (c) 2020 Joseph Myers. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joseph Myers, Manuel Candales
 -/
-import Mathlib.Analysis.NormedSpace.Normalize
 import Mathlib.Analysis.InnerProductSpace.Subspace
+import Mathlib.Analysis.NormedSpace.Normalize
 import Mathlib.Analysis.SpecialFunctions.Trigonometric.Inverse
+
 
 /-!
 # Angles between vectors
