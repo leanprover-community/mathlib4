186d62417302ec0a64890274fcebe2d24bb0cfdd
Fernando Chu <17756312+FernandoChu@users.noreply.github.com>
2025-09-17 09:22:17 +0200

    Apply suggestions from code review
    Co-authored-by: Robin Carlier <57142648+robin-carlier@users.noreply.github.com>

--- Mathlib/CategoryTheory/Limits/Skeleton.lean
+++ Mathlib/CategoryTheory/Limits/Skeleton.lean
@@ -4,9 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Fernando Chu
 -/
 import Mathlib.CategoryTheory.Adjunction.Limits
-import Mathlib.CategoryTheory.Limits.HasLimits
 import Mathlib.CategoryTheory.Skeletal
-import Mathlib.CategoryTheory.Limits.Creates
 
 /-!
 # (Co)limits of the skeleton of a category
@@ -28,9 +26,9 @@ open CategoryTheory ThinSkeleton
 namespace CategoryTheory.Limits
 
 universe v₁ u₁ v₂ u₂ v₃ u₃ w w'
-variable {J : Type u₁} [Category.{v₁} J]
-variable {C : Type u₂} [Category.{v₂} C]
-variable {D : Type u₃} [Category.{v₃} D]
+
+variable {J : Type u₁} [Category.{v₁} J] {C : Type u₂} [Category.{v₂} C]
+  {D : Type u₃} [Category.{v₃} D]
 
 instance hasLimitsOfShape_skeleton [HasLimitsOfShape J C] : HasLimitsOfShape J (Skeleton C) :=
   hasLimitsOfShape_of_hasLimitsOfShape_createsLimitsOfShape (fromSkeleton C)
@@ -39,7 +37,7 @@ instance hasLimitsOfSize_skeleton [HasLimitsOfSize.{w, w'} C] :
     HasLimitsOfSize.{w,w'} (Skeleton C) :=
   hasLimits_of_hasLimits_createsLimits (fromSkeleton C)
 
-instance hasLimits_skeleton [HasLimits C] : HasLimits (Skeleton C) := by infer_instance
+example [HasLimits C] : HasLimits (Skeleton C) := by infer_instance
 
 instance hasColimitsOfShape_skeleton [HasColimitsOfShape J C] : HasColimitsOfShape J (Skeleton C) :=
   hasColimitsOfShape_of_hasColimitsOfShape_createsColimitsOfShape (fromSkeleton C)
@@ -48,7 +46,7 @@ instance hasColimitsOfSize_skeleton [HasColimitsOfSize.{w, w'} C] :
     HasColimitsOfSize.{w,w'} (Skeleton C) :=
   hasColimits_of_hasColimits_createsColimits (fromSkeleton C)
 
-instance hasColimits_skeleton [HasColimits C] : HasColimits (Skeleton C) := by infer_instance
+example [HasColimits C] : HasColimits (Skeleton C) := by infer_instance
 
 variable [Quiver.IsThin C]
 
