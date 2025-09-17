8978e33d9e25326427e16ee8c8b50482e35a5305
mariainesdff <mariainesdff@gmail.com>
2025-09-17 14:23:22 +0200

    remove import

--- Mathlib/RingTheory/PowerSeries/Substitution.lean
+++ Mathlib/RingTheory/PowerSeries/Substitution.lean
@@ -3,7 +3,6 @@ Copyright (c) 2025 Antoine Chambert-Loir, María Inés de Frutos Fernández. All
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Antoine Chambert-Loir, María Inés de Frutos Fernández
 -/
-import Mathlib.RingTheory.MvPowerSeries.Order
 import Mathlib.RingTheory.MvPowerSeries.Substitution
 import Mathlib.RingTheory.PowerSeries.Evaluation
 
