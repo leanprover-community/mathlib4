c96505814dd056f4b1a3d6d0bf49600c59f6e6bf
Stefan Kebekus <kebekus@users.noreply.github.com>
2025-09-18 12:07:12 +0200

    Update Analytic.lean

--- Mathlib/Analysis/Complex/Harmonic/Analytic.lean
+++ Mathlib/Analysis/Complex/Harmonic/Analytic.lean
@@ -107,6 +107,5 @@ Harmonic functions are real analytic.
 theorem HarmonicAt.analyticAt (hf : HarmonicAt f x) : AnalyticAt ℝ f x := by
   obtain ⟨ε, h₁ε, h₂ε⟩ := isOpen_iff.1 (isOpen_setOf_harmonicAt (f := f)) x hf
   obtain ⟨F, h₁F, h₂F⟩ := harmonic_is_realOfHolomorphic (fun _ hy ↦ h₂ε hy)
-  rw [(by aesop : (fun z ↦ (F z).re) = Complex.reCLM ∘ F)] at h₂F
   rw [analyticAt_congr (Filter.eventually_of_mem (ball_mem_nhds x h₁ε) (fun y hy ↦ h₂F.symm hy))]
   exact (reCLM.analyticAt (F x)).comp (h₁F x (mem_ball_self h₁ε)).restrictScalars
