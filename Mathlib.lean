08f7a0dbf7c85ce68c1b81d96d340bf6a373ef8e
Jireh Loreaux <loreaujy@gmail.com>
2025-09-18 17:28:35 +0000

    chore(Analysis/NormedSpace/Extend): split file (#29445)
    This PR accomplishes a few tasks:
    
    1. splits the file according to whether the material needs the operator norm on continuous linear map. This will help us avoid importing the operator norm into `LocallyConvex/` in an upcoming transition of material from `NormedSpace/HahnBanach/` into other folders.
    2. moves the resulting files out of the `NormedSpace` folder in accordance with #28698.
    3. Cleans up the declarations / proofs / docstrings a small amount.
    4. Uses `Module.Dual` spelling for the linear maps.
     
    This is part of #28698 to migrate the material in `Analysis/NormedSpace/` to other locations.

--- Mathlib.lean
+++ Mathlib.lean
@@ -1816,6 +1816,7 @@ import Mathlib.Analysis.Normed.Module.Convex
 import Mathlib.Analysis.Normed.Module.Dual
 import Mathlib.Analysis.Normed.Module.FiniteDimension
 import Mathlib.Analysis.Normed.Module.RCLike.Basic
+import Mathlib.Analysis.Normed.Module.RCLike.Extend
 import Mathlib.Analysis.Normed.Module.RCLike.Real
 import Mathlib.Analysis.Normed.Module.Ray
 import Mathlib.Analysis.Normed.Module.Span
@@ -1864,7 +1865,6 @@ import Mathlib.Analysis.NormedSpace.BallAction
 import Mathlib.Analysis.NormedSpace.Connected
 import Mathlib.Analysis.NormedSpace.DualNumber
 import Mathlib.Analysis.NormedSpace.ENormedSpace
-import Mathlib.Analysis.NormedSpace.Extend
 import Mathlib.Analysis.NormedSpace.Extr
 import Mathlib.Analysis.NormedSpace.FunctionSeries
 import Mathlib.Analysis.NormedSpace.HahnBanach.Extension
@@ -1904,6 +1904,7 @@ import Mathlib.Analysis.Polynomial.Factorization
 import Mathlib.Analysis.Quaternion
 import Mathlib.Analysis.RCLike.Basic
 import Mathlib.Analysis.RCLike.BoundedContinuous
+import Mathlib.Analysis.RCLike.Extend
 import Mathlib.Analysis.RCLike.Inner
 import Mathlib.Analysis.RCLike.Lemmas
 import Mathlib.Analysis.RCLike.TangentCone
--- Mathlib/Analysis/NormedSpace/HahnBanach/Extension.lean
+++ Mathlib/Analysis/NormedSpace/HahnBanach/Extension.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yury Kudryashov, Heather Macbeth
 -/
 import Mathlib.Analysis.Convex.Cone.Extension
-import Mathlib.Analysis.NormedSpace.Extend
+import Mathlib.Analysis.Normed.Module.RCLike.Extend
 import Mathlib.Analysis.RCLike.Lemmas
 
 /-!
