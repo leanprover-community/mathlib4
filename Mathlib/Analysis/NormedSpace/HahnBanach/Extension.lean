93523b3e5917459fd5bfdab40132495790246147
Jireh Loreaux <loreaujy@gmail.com>
2025-09-18 10:44:35 -0500

    shake

--- Mathlib/Analysis/NormedSpace/HahnBanach/Extension.lean
+++ Mathlib/Analysis/NormedSpace/HahnBanach/Extension.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yury Kudryashov, Heather Macbeth
 -/
 import Mathlib.Analysis.Convex.Cone.Extension
-import Mathlib.Analysis.Normed.Module.RCLike.Basic
 import Mathlib.Analysis.Normed.Module.RCLike.Extend
 import Mathlib.Analysis.RCLike.Lemmas
 
