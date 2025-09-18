e888cf2890e6ee076e190f4d59c97967928441cb
bwangpj <70694994+bwangpj@users.noreply.github.com>
2025-09-18 00:31:55 -0400

    Update QuotientRing.lean

--- Mathlib/Data/ZMod/QuotientRing.lean
+++ Mathlib/Data/ZMod/QuotientRing.lean
@@ -77,23 +77,23 @@ def ZMod.prodEquivPi {ι : Type*} [Fintype ι] (a : ι → ℕ)
   quotientInfRingEquivPiQuotient _ this |>.trans <|
   RingEquiv.piCongrRight fun i ↦ Int.quotientSpanNatEquivZMod (a i)
 
-def ZMod.equivPi_aux (N : ℕ) :
-    ZMod (∏ (p : N.primeFactors), (p : ℕ) ^ (N.factorization p))
-    ≃+* Π (p : N.primeFactors), ZMod (p ^ (N.factorization p)) :=
-  prodEquivPi (fun (p : N.primeFactors) ↦ (p : ℕ) ^ (N.factorization p))
+def ZMod.equivPi_aux :
+    ZMod (∏ (p : n.primeFactors), (p : ℕ) ^ (n.factorization p))
+    ≃+* Π (p : n.primeFactors), ZMod (p ^ (n.factorization p)) :=
+  prodEquivPi (fun (p : n.primeFactors) ↦ (p : ℕ) ^ (n.factorization p))
     (by
       intro p1 p2 hp
-      refine Nat.Coprime.pow (N.factorization ↑p1) (N.factorization ↑p2) ?_
+      refine Nat.Coprime.pow (n.factorization ↑p1) (n.factorization ↑p2) ?_
       apply (Nat.coprime_primes _ _).mpr
       · exact Subtype.coe_ne_coe.mpr hp
       · exact Nat.prime_of_mem_primeFactors p1.2
       · exact Nat.prime_of_mem_primeFactors p2.2)
 
-/-- The **Chinese remainder theorem**, version for `ZMod N`. -/
-def ZMod.equivPi (N : ℕ) (hN : N ≠ 0) :
-    ZMod N ≃+* Π (p : N.primeFactors), ZMod (p ^ (N.factorization p)) := by
-  let equiv := equivPi_aux N
-  rw [← Nat.prime_factorization hN] at equiv
+/-- The **Chinese remainder theorem**, version for `ZMod n`. -/
+def ZMod.equivPi (hn : n ≠ 0) :
+    ZMod n ≃+* Π (p : n.primeFactors), ZMod (p ^ (n.factorization p)) := by
+  let equiv := equivPi_aux n
+  rw [← Nat.prime_factorization hn] at equiv
   exact equiv
 
 end ChineseRemainder
