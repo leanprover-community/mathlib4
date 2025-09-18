2bfb9247c02e1e7bbbdac3d6b7cb9c1ca2df2442
Yury G. Kudryashov <urkud@urkud.name>
2025-09-17 21:09:43 -0500

    Fix docs

--- Mathlib/Topology/UniformSpace/Path.lean
+++ Mathlib/Topology/UniformSpace/Path.lean
@@ -36,16 +36,18 @@ theorem isUniformEmbedding_coe : IsUniformEmbedding ((↑) : Path x y → C(I, X
 theorem uniformContinuous (γ : Path x y) : UniformContinuous γ :=
   CompactSpace.uniformContinuous_of_continuous <| map_continuous _
 
-/-- Extension of a path to the real line is a uniformly continuous function. -/
+/-- Given a path `γ`, it extension to the real line `γ.extend : C(ℝ, X)`
+is a uniformly continuous function. -/
 theorem uniformContinuous_extend (γ : Path x y) : UniformContinuous γ.extend :=
   γ.uniformContinuous.comp <| LipschitzWith.projIcc _ |>.uniformContinuous
 
-/-- Extension of a path to the real line is uniformly continuous in the path argument. -/
-theorem uniformContinuous_extend_left : UniformContinuous (Path.extend : Path x y → _) :=
+/-- The function sending a path `γ` to its extension `γ.extend : ℝ → X`
+is uniformly continuous in `γ`. -/
+theorem uniformContinuous_extend_left : UniformContinuous (Path.extend : Path x y → C(ℝ, X)) :=
   ContinuousMap.projIccCM.uniformContinuous_comp_left.comp isUniformEmbedding_coe.uniformContinuous
 
-/-- If `U i` bounded by `p i` form a basis of entourages of `X`,
-then the sets `{(γ₁, γ₂) | ∀ t, (γ₁ t, γ₂ t) ∈ U i}` bounded by `p i`
+/-- If `{U i | p i}` form a basis of entourages of `X`,
+then the entourages `{V i | p i}`, `V i = {(γ₁, γ₂) | ∀ t, (γ₁ t, γ₂ t) ∈ U i}`,
 form a basis of entourages of paths between `x` and `y`. -/
 theorem _root_.Filter.HasBasis.uniformityPath {ι : Sort*} {p : ι → Prop} {U : ι → Set (X × X)}
     (hU : (𝓤 X).HasBasis p U) :
@@ -56,17 +58,21 @@ theorem hasBasis_uniformity :
     (𝓤 (Path x y)).HasBasis (· ∈ 𝓤 X) ({γ | ∀ t, (γ.1 t, γ.2 t) ∈ ·}) :=
   (𝓤 X).basis_sets.uniformityPath
 
-theorem uniformContinuous_symm : UniformContinuous (Path.symm : Path x y → _) :=
+theorem uniformContinuous_symm : UniformContinuous (Path.symm : Path x y → Path y x) :=
   hasBasis_uniformity.uniformContinuous_iff hasBasis_uniformity |>.mpr fun U hU ↦
     ⟨U, hU, fun _ _ h x ↦ h (σ x)⟩
 
+/-- The function `Path.trans` that concatenates two paths `γ₁ : Path x y` and `γ₂ : Path y z`
+is uniformly continuous in `(γ₁, γ₂)`. -/
 theorem uniformContinuous_trans :
-    UniformContinuous (Path.trans : Path x y → Path y z → _).uncurry :=
+    UniformContinuous (Path.trans : Path x y → Path y z → Path x z).uncurry :=
   hasBasis_uniformity.uniformity_prod hasBasis_uniformity
     |>.uniformContinuous_iff hasBasis_uniformity |>.mpr fun U hU ↦
       ⟨(U, U), ⟨hU, hU⟩, fun ⟨_, _⟩ ⟨_, _⟩ ⟨h₁, h₂⟩ t ↦ by
         by_cases ht : (t : ℝ) ≤ 2⁻¹ <;> simp [Path.trans_apply, ht, h₁ _, h₂ _]⟩
 
+/-- The space of paths between two points in a complete uniform space
+is a complete uniform space. -/
 instance instCompleteSpace [CompleteSpace X] : CompleteSpace (Path x y) :=
   isUniformEmbedding_coe.completeSpace <| by simpa [Set.EqOn, range_coe]
     using ContinuousMap.isComplete_setOf_eqOn (Function.update (fun _ : I ↦ y) 0 x) {0, 1}
