72a0b450d41757fbb3d6cc3ae39a678f17edb01f
Dagur Asgeirsson <dagurtomas@gmail.com>
2025-09-19 12:58:41 -0600

    feat(CategoryTheory): define internally projective objects

---
+++ Mathlib/CategoryTheory/Preadditive/Projective/Internal.lean
@@ -0,0 +1,45 @@
+/-
+Copyright (c) 2025 Dagur Asgeirsson. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Dagur Asgeirsson, Jonas van der Schaaf
+-/
+import Mathlib.CategoryTheory.Retract
+import Mathlib.CategoryTheory.Closed.Monoidal
+import Mathlib.CategoryTheory.Limits.FunctorCategory.EpiMono
+/-!
+
+# Internal projectivity
+
+This file defines internal projectivity and provides explicit characterizations of
+internal projectivity in the category of light condensed modules over a commutative ring.
+
+-/
+
+noncomputable section
+
+universe u
+
+open CategoryTheory MonoidalCategory MonoidalClosed Limits Functor
+
+namespace CategoryTheory
+
+variable {C : Type*} [Category C] [MonoidalCategory C] [MonoidalClosed C]
+
+/--
+An object `P : C` is *internally projective* if the functor `P ⟶[C] -` taking internal homs
+out of `P` preserves epimorphisms.
+-/
+class InternallyProjective (P : C) : Prop where
+  /-- The functor `P ⟶[C] -` preserves epimorphisms. -/
+  preserves_epi : (ihom P).PreservesEpimorphisms
+
+attribute [instance] InternallyProjective.preserves_epi
+
+namespace InternallyProjective
+
+lemma ofRetract {X Y : C} (r : Retract Y X) [InternallyProjective X] : InternallyProjective Y where
+  preserves_epi :=
+    have : Retract (ihom Y) (ihom X) := r.op.map internalHom
+    preservesEpimorphisms.ofRetract this
+
+end CategoryTheory.InternallyProjective
