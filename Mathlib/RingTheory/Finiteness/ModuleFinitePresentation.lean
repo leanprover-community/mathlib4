2b82924fb4cee6d3632924adc11c02ec67b4dea4
Christian Merten <christian@merten.dev>
2025-09-19 19:07:20 +0200

    shake

--- Mathlib/RingTheory/Finiteness/ModuleFinitePresentation.lean
+++ Mathlib/RingTheory/Finiteness/ModuleFinitePresentation.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Christian Merten
 -/
 import Mathlib.Algebra.Module.FinitePresentation
-import Mathlib.RingTheory.IsAdjoinRoot
+import Mathlib.RingTheory.AdjoinRoot
 import Mathlib.RingTheory.MvPolynomial.Basic
 
 /-!
