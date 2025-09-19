b2718010048cf42a9282ed64bd788e2095ab7247
DavidLedvinka <david.ledvinka@mail.utoronto.ca>
2025-09-19 19:37:21 -0400

    change to theorem

--- Mathlib/MeasureTheory/Measure/WithDensity.lean
+++ Mathlib/MeasureTheory/Measure/WithDensity.lean
@@ -720,7 +720,7 @@ variable {M : Type*} [Monoid M] [MeasurableSpace M]
 -- `mconv_smul_left` is in the `Convolution` file. This lemma is here because this is the file in
 -- which we prove the instance that gives `SFinite (c • ν)`.
 @[to_additive conv_smul_right]
-lemma Measure.mconv_smul_right (μ : Measure M) (ν : Measure M) [SFinite ν] (s : ℝ≥0∞) :
+theorem Measure.mconv_smul_right (μ : Measure M) (ν : Measure M) [SFinite ν] (s : ℝ≥0∞) :
     μ ∗ₘ (s • ν) = s • (μ ∗ₘ ν) := by
   unfold mconv
   rw [Measure.prod_smul_right, Measure.map_smul]
