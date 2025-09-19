7aa5578ad32654f5f785e6e4e6dd1ca259944408
Dagur Asgeirsson <dagurtomas@gmail.com>
2025-09-19 14:50:07 -0600

    define as object property

--- Mathlib/CategoryTheory/Preadditive/Projective/Internal.lean
+++ Mathlib/CategoryTheory/Preadditive/Projective/Internal.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Dagur Asgeirsson, Jonas van der Schaaf
 -/
 import Mathlib.CategoryTheory.Closed.Monoidal
+import Mathlib.CategoryTheory.ObjectProperty.Retract
 
 /-!
 
@@ -13,6 +14,10 @@ This file defines internal projectivity of objects `P` in a category `C` as a cl
 `InternallyProjective P`. This means that the functor taking internal homs out of `P`
 preserves epimorphisms. It also proves that a retract of an internally projective object
 is internally projective (see `InternallyProjective.ofRetract`).
+
+This property is important in the setting of light condensed abelian groups, when establishing
+the solid theory (see the lecture series on analytic stacks:
+https://www.youtube.com/playlist?list=PLx5f8IelFRgGmu6gmL-Kf_Rl_6Mm7juZO).
 -/
 
 noncomputable section
@@ -29,17 +34,27 @@ variable {C : Type*} [Category C] [MonoidalCategory C] [MonoidalClosed C]
 An object `P : C` is *internally projective* if the functor `P ⟶[C] -` taking internal homs
 out of `P` preserves epimorphisms.
 -/
-class InternallyProjective (P : C) : Prop where
-  /-- The functor `P ⟶[C] -` preserves epimorphisms. -/
-  preserves_epi : (ihom P).PreservesEpimorphisms
+def isInternallyProjective : ObjectProperty C := fun P ↦ (ihom P).PreservesEpimorphisms
 
-attribute [instance] InternallyProjective.preserves_epi
+/--
+An object `P : C` is *internally projective* if the functor `P ⟶[C] -` taking internal homs
+out of `P` preserves epimorphisms.
+-/
+abbrev InternallyProjective (P : C) := isInternallyProjective.Is P
+
+instance InternallyProjective.preserves_epi (P : C) [InternallyProjective P] :
+    (ihom P).PreservesEpimorphisms :=
+  isInternallyProjective.prop_of_is P
+
+instance : (isInternallyProjective (C := C)).IsStableUnderRetracts where
+  of_retract {Y X} r h :=
+    have : InternallyProjective X := ⟨h⟩
+    have : Retract (ihom Y) (ihom X) := r.op.map internalHom
+    preservesEpimorphisms.ofRetract this
 
 namespace InternallyProjective
 
-lemma ofRetract {X Y : C} (r : Retract Y X) [InternallyProjective X] : InternallyProjective Y where
-  preserves_epi :=
-    have : Retract (ihom Y) (ihom X) := r.op.map internalHom
-    preservesEpimorphisms.ofRetract this
+lemma ofRetract {X Y : C} (r : Retract Y X) [InternallyProjective X] : InternallyProjective Y :=
+  ⟨isInternallyProjective.prop_of_retract r (isInternallyProjective.prop_of_is _)⟩
 
 end CategoryTheory.InternallyProjective
