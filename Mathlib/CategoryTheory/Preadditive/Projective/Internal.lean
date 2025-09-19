c1c45f6e851b80f3f1916c43977b3b4c42a92938
Dagur Asgeirsson <dagurtomas@gmail.com>
2025-09-19 13:10:09 -0600

    remove imports

--- Mathlib/CategoryTheory/Preadditive/Projective/Internal.lean
+++ Mathlib/CategoryTheory/Preadditive/Projective/Internal.lean
@@ -3,9 +3,7 @@ Copyright (c) 2025 Dagur Asgeirsson. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Dagur Asgeirsson, Jonas van der Schaaf
 -/
-import Mathlib.CategoryTheory.Retract
 import Mathlib.CategoryTheory.Closed.Monoidal
-import Mathlib.CategoryTheory.Limits.FunctorCategory.EpiMono
 
 /-!
 
