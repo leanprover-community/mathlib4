ef678164cba5c1bc94b75d47bbe785e91df3c34b
DavidLedvinka <david.ledvinka@mail.utoronto.ca>
2025-09-19 19:04:35 -0400

    fix wrong theorem

--- Mathlib/MeasureTheory/Group/Convolution.lean
+++ Mathlib/MeasureTheory/Group/Convolution.lean
@@ -93,11 +93,11 @@ theorem mconv_zero (μ : Measure M) : μ ∗ₘ (0 : Measure M) = (0 : Measure M
 
 -- `mconv_smul_right` needs an instance to get `SFinite (c • ν)` from `SFinite ν`,
 -- hence it is placed in the `WithDensity` file, where the instance is defined.
-@[to_additive]
-theorem mconv_smul_left (μ : Measure M) (ν : Measure M) (s : ℝ≥0∞) :
-    s • μ ∗ₘ ν = s • (μ ∗ₘ ν) := by
+@[to_additive conv_smul_left]
+theorem mconv_smul_left (μ : Measure M) (ν : Measure M) [SFinite ν] (s : ℝ≥0∞) :
+    (s • μ) ∗ₘ ν = s • (μ ∗ₘ ν) := by
   unfold mconv
-  rw [← Measure.map_smul]
+  rw [← Measure.map_smul, Measure.prod_smul_left]
 
 @[to_additive]
 theorem mconv_add [MeasurableMul₂ M] (μ : Measure M) (ν : Measure M) (ρ : Measure M) [SFinite μ]
