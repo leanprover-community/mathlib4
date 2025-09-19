e74eef822c1e0d066a8e39b6adcf456c5f3336fc
WilliamCoram <williamecoram@gmail.com>
2025-09-19 10:36:35 +0100

    Update Mathlib/GroupTheory/DoubleCoset.lean
    Co-authored-by: Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>

--- Mathlib/GroupTheory/DoubleCoset.lean
+++ Mathlib/GroupTheory/DoubleCoset.lean
@@ -345,7 +345,7 @@ theorem union_finset_rightRel_cover (H K : Subgroup G)
     obtain ⟨a, ha⟩ : ∃ a : H, a * x = q := by
       obtain ⟨a', ha'⟩ := (Quotient.eq).mp hx
       exact ⟨a', by simpa using ha'⟩
-    exact ⟨⟨ a⁻¹, by simp only [inv_mem_iff, SetLike.coe_mem]⟩, eq_inv_mul_of_mul_eq ha⟩
+    exact ⟨⟨a⁻¹, by simp only [inv_mem_iff, SetLike.coe_mem]⟩, eq_inv_mul_of_mul_eq ha⟩
   refine ⟨a.1, ?_⟩
   simp only [Subtype.coe_prop, SetLike.mem_coe, true_and]
   exact ⟨1, Subgroup.one_mem K, by simpa using ha⟩
