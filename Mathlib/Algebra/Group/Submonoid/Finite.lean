5ec26ea8de077ce0b7afb2447f9493ef42a1f68a
Monica Omar <2497250a@research.gla.ac.uk>
2025-09-19 22:08:27 +0000

    cleanup

--- Mathlib/Algebra/Group/Submonoid/Finite.lean
+++ Mathlib/Algebra/Group/Submonoid/Finite.lean
@@ -75,13 +75,13 @@ theorem pi_le_iff [DecidableEq η] [Finite η] {H : ∀ i, Submonoid (f i)} {J :
 
 @[to_additive]
 theorem closure_pi [Finite η] {s : Π i, Set (f i)} (hs : ∀ i, 1 ∈ s i) :
-    closure (Set.univ.pi fun i => s i) = pi Set.univ fun i => closure (s i) :=
+    closure (univ.pi fun i => s i) = pi univ fun i => closure (s i) :=
   le_antisymm
-    (closure_le.2 <| Set.pi_subset_pi_iff.2 <| .inl fun _ _ => subset_closure)
+    (closure_le.2 <| pi_subset_pi_iff.2 <| .inl fun _ _ => subset_closure)
     (by
       classical
       apply pi_le_iff.mpr fun i => map_le_of_le_comap _ <| closure_le.2 fun _x hx =>
-      subset_closure <| Set.mem_univ_pi.mpr fun j => by
+      subset_closure <| mem_univ_pi.mpr fun j => by
         by_cases H : j = i
         · subst H
           simpa
