41d6537c5f7e8d095328256112697ecb2bfaeedf
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-09-20 23:52:22 +0200

    fixing imports

--- Mathlib/CategoryTheory/ObjectProperty/ColimitsOfShape.lean
+++ Mathlib/CategoryTheory/ObjectProperty/ColimitsOfShape.lean
@@ -3,7 +3,7 @@ Copyright (c) 2025 Joël Riou. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joël Riou
 -/
-import Mathlib.CategoryTheory.ObjectProperty.FullSubcategory
+import Mathlib.CategoryTheory.ObjectProperty.Small
 import Mathlib.CategoryTheory.Limits.Presentation
 
 /-!
