c9d8f97ae9065840233f55bb58774c6dcdd71c16
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2025-09-20 12:01:21 +0200

    more sensible lemma order

--- Mathlib/LinearAlgebra/AffineSpace/Ordered.lean
+++ Mathlib/LinearAlgebra/AffineSpace/Ordered.lean
@@ -111,10 +111,10 @@ theorem left_le_lineMap_iff_nonneg (h : a < b) : a ≤ lineMap a b r ↔ 0 ≤ r
 theorem lineMap_le_left_iff_nonpos (h : a < b) : lineMap a b r ≤ a ↔ r ≤ 0 := by
   rw [← lineMap_le_lineMap_iff_of_lt' h, lineMap_apply_zero]
 
-theorem lineMap_le_right_iff_le_one (h : a < b) : lineMap a b r ≤ b ↔ r ≤ 1 := by
+theorem right_le_lineMap_iff_one_le (h : a < b) : b ≤ lineMap a b r ↔ 1 ≤ r := by
   rw [← lineMap_le_lineMap_iff_of_lt' h, lineMap_apply_one]
 
-theorem right_le_lineMap_iff_one_le (h : a < b) : b ≤ lineMap a b r ↔ 1 ≤ r := by
+theorem lineMap_le_right_iff_le_one (h : a < b) : lineMap a b r ≤ b ↔ r ≤ 1 := by
   rw [← lineMap_le_lineMap_iff_of_lt' h, lineMap_apply_one]
 
 theorem lineMap_lt_lineMap_iff_of_lt' (h : a < b) : lineMap a b r < lineMap a b r' ↔ r < r' := by
@@ -127,10 +127,10 @@ theorem left_lt_lineMap_iff_pos (h : a < b) : a < lineMap a b r ↔ 0 < r := by
 theorem lineMap_lt_left_iff_neg (h : a < b) : lineMap a b r < a ↔ r < 0 := by
   rw [← lineMap_lt_lineMap_iff_of_lt' h, lineMap_apply_zero]
 
-theorem lineMap_lt_right_iff_lt_one (h : a < b) : lineMap a b r < b ↔ r < 1 := by
+theorem right_lt_lineMap_iff_one_lt (h : a < b) : b < lineMap a b r ↔ 1 < r  := by
   rw [← lineMap_lt_lineMap_iff_of_lt' h, lineMap_apply_one]
 
-theorem right_lt_lineMap_iff_one_lt (h : a < b) : b < lineMap a b r ↔ 1 < r  := by
+theorem lineMap_lt_right_iff_lt_one (h : a < b) : lineMap a b r < b ↔ r < 1 := by
   rw [← lineMap_lt_lineMap_iff_of_lt' h, lineMap_apply_one]
 
 theorem midpoint_le_midpoint [Invertible (2 : k)] (ha : a ≤ a') (hb : b ≤ b') :
