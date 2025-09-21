b834b4614f5072c0d8b55bead564bca06d4099c4
dsfxcimk <134686157+dsfxcimk@users.noreply.github.com>
2025-09-21 12:07:57 +0800

    Update Mathlib/Geometry/Euclidean/Triangle.lean
    Co-authored-by: Monica Omar <23701951+themathqueen@users.noreply.github.com>

--- Mathlib/Geometry/Euclidean/Triangle.lean
+++ Mathlib/Geometry/Euclidean/Triangle.lean
@@ -277,11 +277,8 @@ theorem angle_add_angle_add_angle_eq_pi {p₁ p₂ : P} (p₃ : P) (h : p₂ ≠
 /-- **Exterior angle theorem**. -/
 theorem exterior_angle_eq_angle_add_angle {p₁ p₂ p₃ : P} (p : P) (h : Sbtw ℝ p p₁ p₂) :
     ∠ p₃ p₁ p = ∠ p₁ p₃ p₂ + ∠ p₃ p₂ p₁ := by
-  have H := (EuclideanGeometry.angle_add_angle_eq_pi_of_angle_eq_pi p₃ (Sbtw.angle₁₂₃_eq_pi h)).symm
-  rw [angle_comm p₃ p₁ p₂, add_comm, ← angle_add_angle_add_angle_eq_pi p₃ ((Sbtw.right_ne h).symm)]
-    at H
-  simp only [angle_comm] at *
-  linarith
+  linarith [angle_add_angle_eq_pi_of_angle_eq_pi p₃ h.angle₁₂₃_eq_pi,
+    angle_add_angle_add_angle_eq_pi p₃ h.right_ne.symm, angle_comm p₃ p₁ p₂]
 
 /-- The **sum of the angles of a triangle** (possibly degenerate, where the triangle is a line),
 oriented angles at point. -/
