20850e4a0b38de765926df1d147cbdb8810f227d
Vlad Tsyrklevich <vlad902@gmail.com>
2025-09-21 11:53:10 +0200

    Update Mathlib/Data/Set/Card.lean
    Co-authored-by: Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>

--- Mathlib/Data/Set/Card.lean
+++ Mathlib/Data/Set/Card.lean
@@ -596,7 +596,7 @@ theorem ncard_mono [Finite α] : @Monotone (Set α) _ _ _ ncard := fun _ _ ↦ n
 @[simp] theorem ncard_univ (α : Type*) : (univ : Set α).ncard = Nat.card α := Nat.card_univ
 
 @[simp] theorem ncard_le_card [Finite α] : s.ncard ≤ Nat.card α :=
-  ncard_univ _ ▸ ncard_le_ncard (fun _ _ ↦ trivial) finite_univ
+  ncard_univ _ ▸ ncard_le_ncard s.subset_univ finite_univ
 
 @[simp] theorem ncard_empty (α : Type*) : (∅ : Set α).ncard = 0 := by
   rw [ncard_eq_zero]
