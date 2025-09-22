75155f055a5a005d13ab5eaf96b8b17c583c3e61
WilliamCoram <177530016+WilliamCoram@users.noreply.github.com>
2025-09-22 13:29:53 +0000

    feat: restricted power series form a ring (#26089)
    We define restricted power series over a normed ring R, and show they form a ring when R has the ultrametric property
    <NEWLINE>
    This is an update of [#23338](https://github.com/leanprover-community/mathlib4/pull/23338) to meet new expectations of PRs from forks.
    
    Co-authored-by: Riccardo Brasca <riccardo.brasca@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -5758,6 +5758,7 @@ import Mathlib.RingTheory.PowerSeries.Inverse
 import Mathlib.RingTheory.PowerSeries.NoZeroDivisors
 import Mathlib.RingTheory.PowerSeries.Order
 import Mathlib.RingTheory.PowerSeries.PiTopology
+import Mathlib.RingTheory.PowerSeries.Restricted
 import Mathlib.RingTheory.PowerSeries.Substitution
 import Mathlib.RingTheory.PowerSeries.Trunc
 import Mathlib.RingTheory.PowerSeries.WeierstrassPreparation
