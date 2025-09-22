e218c42756201e0cc4c75464873fab6ef91ddfad
Joël Riou <37772949+joelriou@users.noreply.github.com>
2025-09-22 16:39:35 +0200

    Update Mathlib/Algebra/Homology/DerivedCategory/Linear.lean

--- Mathlib/Algebra/Homology/DerivedCategory/Linear.lean
+++ Mathlib/Algebra/Homology/DerivedCategory/Linear.lean
@@ -18,7 +18,7 @@ open CategoryTheory Category Limits Pretriangulated ZeroObject Preadditive
 universe w v u
 
 variable (R : Type w) [Ring R] (C : Type u) [Category.{v} C] [Abelian C] [Linear R C]
-  [HasDerivedCategory C]
+  [HasDerivedCategory.{w} C]
 
 namespace DerivedCategory
 
