295534b78dcbb5102acd2abe031794ae9de69472
Christopher Hoskin <mans0954@users.noreply.github.com>
2025-09-23 23:24:33 +0100

    Get most of it

--- Mathlib/RingTheory/Polynomial/SmallDegreeVieta.lean
+++ Mathlib/RingTheory/Polynomial/SmallDegreeVieta.lean
@@ -5,7 +5,6 @@ Authors: Qinchuan Zhang
 -/
 import Mathlib.Tactic.FieldSimp
 import Mathlib.Tactic.LinearCombination
--- import Mathlib.Algebra.Polynomial.Roots
 import Mathlib.RingTheory.Polynomial.Vieta
 import Mathlib.RingTheory.Polynomial.Resultant.Basic
 
@@ -70,10 +69,19 @@ lemma eq_mul_mul_of_aroots_quadratic_eq_pair [CommRing T] [CommRing S] [IsDomain
   · rw [← hp_roots_card']
     exact card_roots' (map (algebraMap T S) p)
 
+lemma test [CommRing R] [IsDomain R] {p : R[X]}
+    (hp : p.natDegree = 2) : (p.coeff 2) ≠ 0 := by
+  apply coeff_ne_zero_of_eq_degree
+  unfold natDegree at hp
+  rw [WithBot.unbotD_eq_iff] at hp
+  simp at hp
+  rw [hp]
+  norm_cast
+
 
 /-- **Vieta's formula** for quadratics as an iff. -/
 lemma roots_quadratic_eq_pair_iff_of_ne_zero [CommRing R] [IsDomain R] {x1 x2 : R} {p : R[X]}
-    (hp : p.natDegree = 2) (ha : p.coeff 2 ≠ 0) :
+    (hp : p.natDegree = 2) :
     p.roots = {x1, x2} ↔
       p.coeff 1 = -(p.coeff 2) * (x1 + x2) ∧ (p.coeff 0) = (p.coeff 2) * x1 * x2 :=
   let a := p.coeff 2
@@ -82,6 +90,7 @@ lemma roots_quadratic_eq_pair_iff_of_ne_zero [CommRing R] [IsDomain R] {x1 x2 : 
   have roots_of_ne_zero_of_vieta (hvieta : b = -a * (x1 + x2) ∧ c = a * x1 * x2) :
       p.roots = {x1, x2} := by
     suffices p = C a * (X - C x1) * (X - C x2) by
+      have h0 : (p.coeff 2) ≠ 0 := test hp
       have h1 : C a * (X - C x1) ≠ 0 := mul_ne_zero (by simpa) (Polynomial.X_sub_C_ne_zero _)
       have h2 : C a * (X - C x1) * (X - C x2) ≠ 0 := mul_ne_zero h1 (Polynomial.X_sub_C_ne_zero _)
       simp [this, Polynomial.roots_mul h2, Polynomial.roots_mul h1]
@@ -94,31 +103,44 @@ lemma roots_quadratic_eq_pair_iff_of_ne_zero [CommRing R] [IsDomain R] {x1 x2 : 
 
 /-- **Vieta's formula** for quadratics as an iff (`aroots` version). -/
 lemma aroots_quadratic_eq_pair_iff_of_ne_zero [CommRing T] [CommRing S] [IsDomain S]
-    [Algebra T S] {a b c : T} {x1 x2 : S} (ha : algebraMap T S a ≠ 0) :
-    (C a * X ^ 2 + C b * X + C c).aroots S = {x1, x2} ↔
-      algebraMap T S b = -algebraMap T S a * (x1 + x2) ∧
-      algebraMap T S c = algebraMap T S a * x1 * x2 := by
-  rw [aroots_def, show map (algebraMap T S) (C a * X ^ 2 + C b * X + C c) = C ((algebraMap T S) a) *
-    X ^ 2 + C ((algebraMap T S) b) * X + C ((algebraMap T S) c) by simp]
-  exact roots_quadratic_eq_pair_iff_of_ne_zero ha
+    [Algebra T S] {p : T[X]} {x1 x2 : S} (ha : (map (algebraMap T S) p).natDegree = 2) :
+    p.aroots S = {x1, x2} ↔
+      algebraMap T S (p.coeff 1) = -algebraMap T S (p.coeff 2) * (x1 + x2) ∧
+      algebraMap T S (p.coeff 0) = algebraMap T S (p.coeff 2) * x1 * x2 := by
+  let a := p.coeff 2
+  let b := p.coeff 1
+  let c := p.coeff 0
+  rw [roots_quadratic_eq_pair_iff_of_ne_zero ha, coeff_map, coeff_map, coeff_map]
 
 /-- **Vieta's formula** for quadratics as an iff (`Field` version). -/
-lemma roots_quadratic_eq_pair_iff_of_ne_zero' [Field R] {a b c x1 x2 : R} (ha : a ≠ 0) :
-    (C a * X ^ 2 + C b * X + C c).roots = {x1, x2} ↔
-      x1 + x2 = -b / a ∧ x1 * x2 = c / a := by
-  rw [roots_quadratic_eq_pair_iff_of_ne_zero ha]
+lemma roots_quadratic_eq_pair_iff_of_ne_zero' [Field R] {p : R[X]} {x1 x2 : R}
+    (hp : p.natDegree = 2) : p.roots = {x1, x2} ↔
+      x1 + x2 = -(p.coeff 1) / (p.coeff 2) ∧ x1 * x2 = (p.coeff 0) / (p.coeff 2) := by
+  rw [roots_quadratic_eq_pair_iff_of_ne_zero hp]
   field_simp
-  exact and_congr ⟨fun h => by linear_combination h, fun h => by linear_combination h⟩
-    ⟨fun h => by linear_combination -h, fun h => by linear_combination -h⟩
+  have h0 : (p.coeff 2) ≠ 0 := test hp
+  apply and_congr
+  · constructor
+    · intro h
+      rw [h, neg_div, neg_neg, mul_div_cancel_left₀ _ h0]
+    · intro h
+      rw [h, mul_neg, neg_neg, (mul_div_cancel₀ _ h0)]
+  · constructor
+    · intro h
+      rw [h, mul_assoc, mul_div_cancel_left₀ _ h0]
+    · intro h
+      rw [mul_assoc, h, mul_div_cancel₀ _ h0]
 
 /-- **Vieta's formula** for quadratics as an iff (`aroots, Field` version). -/
-lemma aroots_quadratic_eq_pair_iff_of_ne_zero' [CommRing T] [Field S] [Algebra T S] {a b c : T}
-    {x1 x2 : S} (ha : algebraMap T S a ≠ 0) :
-    (C a * X ^ 2 + C b * X + C c).aroots S = {x1, x2} ↔
-      x1 + x2 = -algebraMap T S b / algebraMap T S a ∧
-      x1 * x2 = algebraMap T S c / algebraMap T S a := by
-  rw [aroots_def, show map (algebraMap T S) (C a * X ^ 2 + C b * X + C c) = C ((algebraMap T S) a) *
-    X ^ 2 + C ((algebraMap T S) b) * X + C ((algebraMap T S) c) by simp]
-  exact roots_quadratic_eq_pair_iff_of_ne_zero' ha
+lemma aroots_quadratic_eq_pair_iff_of_ne_zero' [CommRing T] [Field S] [Algebra T S] {p : T[X]}
+    {x1 x2 : S} (ha : (map (algebraMap T S) p).natDegree = 2) :
+    p.aroots S = {x1, x2} ↔
+      x1 + x2 = -algebraMap T S (p.coeff 1) / algebraMap T S (p.coeff 2) ∧
+      x1 * x2 = algebraMap T S (p.coeff 0) / algebraMap T S (p.coeff 2) := by
+  rw [aroots_def]
+  rw [roots_quadratic_eq_pair_iff_of_ne_zero' ha]
+  apply and_congr
+  · rw [coeff_map, coeff_map]
+  · rw [coeff_map, coeff_map]
 
 end Polynomial
