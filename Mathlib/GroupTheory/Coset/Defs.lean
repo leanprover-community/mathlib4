4aca97f74f45ac0b6692567b3bbf820a53ff9786
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2025-09-24 00:44:56 +0200

    .

--- Mathlib/GroupTheory/Coset/Defs.lean
+++ Mathlib/GroupTheory/Coset/Defs.lean
@@ -46,8 +46,7 @@ open scoped Pointwise
 variable {α : Type*}
 
 -- See https://leanprover.zulipchat.com/#narrow/stream/287929-mathlib4/topic/.E2.9C.94.20to_additive.2Emap_namespace
-run_cmd Lean.Elab.Command.liftCoreM <|
-  ToAdditive.insertTranslation ToAdditive.toAdditiveBundle `QuotientGroup `QuotientAddGroup
+run_meta ToAdditive.insertTranslation ToAdditive.toAdditiveBundle `QuotientGroup `QuotientAddGroup
 
 namespace QuotientGroup
 
