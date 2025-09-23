5d40e9e5ed6c5dca6ae53007fdf0d9361bd5bd2f
Jovan Gerbscheid <56355248+JovanGerb@users.noreply.github.com>
2025-09-23 19:12:59 +0200

    Apply suggestion from @plp127
    Co-authored-by: Aaron Liu <aaronliu2008@outlook.com>

--- Mathlib/Tactic/NormNum/Result.lean
+++ Mathlib/Tactic/NormNum/Result.lean
@@ -124,7 +124,7 @@ def mkOfNat (α : Q(Type u)) (_sα : Q(AddMonoidWithOne $α)) (lit : Q(ℕ)) :
     | k+2 =>
       let k : Q(ℕ) := mkRawNatLit k
       let _x : Q(Nat.AtLeastTwo $lit) :=
-        (q(Nat.instAtLeastTwo $k) : Expr)
+        (q(instAtLeastTwo $k) : Expr)
       let a' : Q($α) := q(OfNat.ofNat $lit)
       pure ⟨a', (q(Eq.refl $a') : Expr)⟩
 
