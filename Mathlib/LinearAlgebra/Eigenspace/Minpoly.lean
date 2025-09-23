5640837b2ffbb0eec61ca10af9ffefe6c13ef4a1
Lawrence Wu <git@llllvvuu.dev>
2025-09-23 14:43:38 -0700

    shake

--- Mathlib/LinearAlgebra/Eigenspace/Minpoly.lean
+++ Mathlib/LinearAlgebra/Eigenspace/Minpoly.lean
@@ -3,8 +3,10 @@ Copyright (c) 2020 Alexander Bentkamp. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Alexander Bentkamp
 -/
+import Mathlib.Algebra.Polynomial.Roots
+import Mathlib.FieldTheory.Minpoly.Basic
 import Mathlib.LinearAlgebra.Eigenspace.Basic
-import Mathlib.FieldTheory.Minpoly.Field
+import Mathlib.RingTheory.IntegralClosure.Algebra.Basic
 
 /-!
 # Eigenvalues are the roots of the minimal polynomial.
