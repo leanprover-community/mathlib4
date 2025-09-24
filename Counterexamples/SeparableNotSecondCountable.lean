5a2204574cf264b3d1d9302d66f2e72d9707ad32
Yury G. Kudryashov <urkud@urkud.name>
2025-09-23 19:50:29 -0500

    Fix

--- Counterexamples/SeparableNotSecondCountable.lean
+++ Counterexamples/SeparableNotSecondCountable.lean
@@ -3,7 +3,7 @@ Copyright (c) 2025 Yury G. Kudryashov. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yury G. Kudryashov
 -/
-import Mathlib.Data.Real.Cardinality
+import Mathlib.Analysis.Real.Cardinality
 
 /-!
 # Example of a linear order which is a separable space but is not a second countable topology
