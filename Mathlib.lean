167f09148f5efc7daaa1e31865f5f9b09739c553
Fabrizio Barroero <23321199+fbarroero@users.noreply.github.com>
2025-09-24 14:41:19 +0000

    feat(Analysis/Polynomial/MahlerMeasure): the Mahler measure of a complex polynomial (#26035)
    We introduce the Mahler measure of a complex polynomial and prove some basic facts.
    
    Co-authored-by: fbarroero <fbarroero@gmail.com>
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1904,6 +1904,7 @@ import Mathlib.Analysis.PSeriesComplex
 import Mathlib.Analysis.Polynomial.Basic
 import Mathlib.Analysis.Polynomial.CauchyBound
 import Mathlib.Analysis.Polynomial.Factorization
+import Mathlib.Analysis.Polynomial.MahlerMeasure
 import Mathlib.Analysis.Quaternion
 import Mathlib.Analysis.RCLike.Basic
 import Mathlib.Analysis.RCLike.BoundedContinuous
