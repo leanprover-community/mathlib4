ed366587af38150ed15af3c9e913647aa01e672a
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2025-09-24 21:22:51 +0800

    fix import

--- Mathlib/RingTheory/RegularLocalRing/GlobalDimension.lean
+++ Mathlib/RingTheory/RegularLocalRing/GlobalDimension.lean
@@ -3,7 +3,6 @@ Copyright (c) 2025 Nailin Guan. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Nailin Guan
 -/
-import Mathlib.RingTheory.RegularLocalRing.Basic
 import Mathlib.RingTheory.CohenMacaulay.Maximal
 import Mathlib.RingTheory.Regular.AuslanderBuchsbaum
 /-!
