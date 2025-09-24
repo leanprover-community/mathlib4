1b681177cf6486367d88bf1570aa886032c6f789
DavidLedvinka <david.ledvinka@mail.utoronto.ca>
2025-09-23 20:07:26 -0400

    progress

--- Mathlib/MeasureTheory/Measure/Typeclasses/Probability.lean
+++ Mathlib/MeasureTheory/Measure/Typeclasses/Probability.lean
@@ -88,9 +88,20 @@ instance isProbabilityMeasureSMul [IsFiniteMeasure μ] [NeZero μ] :
 
 variable [IsProbabilityMeasure μ] {p : α → Prop} {f : β → α}
 
-theorem isProbabilityMeasure_map {f : α → β} (hf : AEMeasurable f μ) :
+theorem Measure.isProbabilityMeasure_map {f : α → β} (hf : AEMeasurable f μ) :
     IsProbabilityMeasure (map f μ) :=
   ⟨by simp [map_apply_of_aemeasurable, hf]⟩
+
+theorem Measure.isProbabilityMeasure_of_map_isProbabilityMeasure {μ : Measure α} {f : α → β}
+    (hf : AEMeasurable f μ) [IsProbabilityMeasure (μ.map f)] : IsProbabilityMeasure μ where
+  measure_univ := by
+    rw [← Set.preimage_univ (f := f), ← map_apply_of_aemeasurable hf .univ]
+    exact IsProbabilityMeasure.measure_univ
+
+theorem Measure.isProbabilityMeasure_iff_map_isProbabilityMeasure {μ : Measure α} {f : α → β}
+    (hf : AEMeasurable f μ) : IsProbabilityMeasure μ ↔ IsProbabilityMeasure (μ.map f) :=
+  ⟨fun _ ↦ isProbabilityMeasure_map hf,
+    fun _ ↦ isProbabilityMeasure_of_map_isProbabilityMeasure hf⟩
 
 instance IsProbabilityMeasure_comap_equiv (f : β ≃ᵐ α) : IsProbabilityMeasure (μ.comap f) := by
   rw [← MeasurableEquiv.map_symm]; exact isProbabilityMeasure_map f.symm.measurable.aemeasurable
