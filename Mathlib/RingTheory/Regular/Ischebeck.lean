8bea149eb170914d05459be97921a1786e74b884
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2025-09-24 17:05:31 +0800

    fix import

--- Mathlib/RingTheory/Regular/Ischebeck.lean
+++ Mathlib/RingTheory/Regular/Ischebeck.lean
@@ -3,7 +3,6 @@ Copyright (c) 2025 Nailin Guan. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Nailin Guan
 -/
-import Mathlib.Algebra.Homology.DerivedCategory.Ext.EnoughInjectives
 import Mathlib.RingTheory.Regular.Depth
 import Mathlib.RingTheory.Ideal.KrullsHeightTheorem
 import Mathlib.RingTheory.KrullDimension.Field
