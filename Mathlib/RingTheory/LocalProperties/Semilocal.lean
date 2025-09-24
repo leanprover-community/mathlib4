a367a2065b9ab5f435525db3d970e166310285f6
pelicanhere <fakegreenall@foxmail.com>
2025-09-25 00:51:12 +0800

    revise doc and fix format

--- Mathlib/RingTheory/LocalProperties/Semilocal.lean
+++ Mathlib/RingTheory/LocalProperties/Semilocal.lean
@@ -6,7 +6,6 @@ Authors: Yiming Fu
 import Mathlib.RingTheory.DedekindDomain.PID
 import Mathlib.RingTheory.KrullDimension.PID
 import Mathlib.RingTheory.Localization.Finiteness
-
 /-!
 # Local properties for semilocal rings
 
@@ -15,10 +14,10 @@ finitely many maximal ideals).
 
 ## Main results
 
-* `Module.finite.of_isLocalized_maximal`: A module `M` over a semilocal ring `R` is finite if it is
-  locally finite at every maximal ideal.
-* `IsNoetherianRing.of_isLocalization_maximal`: A semilocal ring `R` is Noetherian if it is
-  locally Noetherian at every maximal ideal.
+* `Module.Finite.of_isLocalized_maximal`: A module `M` over a semilocal ring `R` is finite if its
+  localization at every maximal ideal is finite.
+* `IsNoetherianRing.of_isLocalization_maximal`: A semilocal ring `R` is Noetherian if its
+  localization at every maximal ideal is a Noetherian ring.
 * `isPrincipalIdealRing_of_isPrincipalIdealRing_localization`: A semilocal integral domain `A` is a
   PID if its localization at every maximal ideal is a PID.
 -/
@@ -44,8 +43,8 @@ variable
 section isLocalized_maximal
 
 include f in
-/-- A module `M` over a semilocal ring `R` is finite if it is
-locally finite at every maximal ideal. -/
+/-- A module `M` over a semilocal ring `R` is finite if its
+  localization at every maximal ideal is finite. -/
 theorem Module.Finite.of_isLocalized_maximal
     (H : ∀ (P : Ideal R) [P.IsMaximal], Module.Finite (Rₚ P) (Mₚ P)) :
     Module.Finite R M := by
@@ -62,13 +61,13 @@ theorem Module.Finite.of_isLocalized_maximal
   exact Submodule.subset_span ⟨_, by simpa using ⟨_, _, x.2, rfl⟩, rfl⟩
 
 variable {M} in
-/-- A submodule `N` of a module `M` over a semilocal ring `R` is finitely generated if it is
-locally finitely generated at every maximal ideal. -/
+/-- A submodule `N` of a module `M` over a semilocal ring `R` is finitely generated if its
+localization at every maximal ideal is finitely generated. -/
 theorem Submodule.fg_of_isLocalized_maximal (N : Submodule R M)
     (H : ∀ (P : Ideal R) [P.IsMaximal], (Submodule.localized' (Rₚ P) P.primeCompl (f P) N).FG) :
     N.FG := by
   simp_rw [← Module.Finite.iff_fg] at ⊢ H
-  exact .of_isLocalized_maximal  _ _ _ (fun P ↦ N.toLocalized' (Rₚ P) P.primeCompl (f P)) H
+  exact .of_isLocalized_maximal _ _ _ (fun P ↦ N.toLocalized' (Rₚ P) P.primeCompl (f P)) H
 
 end isLocalized_maximal
 
@@ -82,19 +81,20 @@ theorem Module.Finite.of_localized_maximal
 
 variable {M} in
 theorem Submodule.fg_of_localized_maximal (N : Submodule R M)
-    (H : ∀ (P : Ideal R) [P.IsMaximal], (Submodule.localized (P.primeCompl) N).FG) :
+    (H : ∀ (P : Ideal R) [P.IsMaximal], (N.localized (P.primeCompl)).FG) :
     N.FG := N.fg_of_isLocalized_maximal _ _ _ H
 
 end localized_maximal
 
 section IsLocalization
 
-/-- A semilocal ring `R` is Noetherian if it is locally Noetherian at every maximal ideal. -/
+/-- A semilocal ring `R` is Noetherian if its
+localization at every maximal ideal is a Noetherian ring. -/
 theorem IsNoetherianRing.of_isLocalization_maximal
     (H : ∀ (P : Ideal R) [P.IsMaximal], IsNoetherianRing (Rₚ P)) :
     IsNoetherianRing R where
   noetherian N := Submodule.fg_of_isLocalized_maximal
-    Rₚ Rₚ (fun P _ => Algebra.linearMap R (Rₚ P)) N (fun _ _ => IsNoetherian.noetherian _)
+    Rₚ Rₚ (fun P _ => Algebra.linearMap R (Rₚ P)) N fun _ _ => IsNoetherian.noetherian _
 
 end IsLocalization
 
@@ -119,8 +119,8 @@ theorem isPrincipalIdealRing_of_isPrincipalIdealRing_isLocalization [IsDomain R]
   have : IsNoetherianRing R :=
     IsNoetherianRing.of_isLocalization_maximal Rₚ (fun P _ => inferInstance)
   have : IsIntegrallyClosed R := by
-    refine IsIntegrallyClosed.of_isLocalization_maximal Rₚ (fun P _ => ?_)
-    have := hpid P
+    refine IsIntegrallyClosed.of_isLocalization_maximal Rₚ (fun P hP => ?_)
+
     sorry
   have : Ring.KrullDimLE 1 R :=
     Ring.krullDimLE_of_isLocalization_maximal Rₚ (fun P _ => inferInstance)
@@ -134,3 +134,5 @@ theorem isPrincipalIdealRing_of_isPrincipalIdealRing_isLocalization [IsDomain R]
 end IsLocalization
 
 end CommRing
+
+#min_imports
