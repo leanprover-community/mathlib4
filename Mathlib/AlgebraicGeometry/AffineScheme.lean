1dc4f40faf64c7558ea5eaf4d824c0eebee39f37
bwangpj <70694994+bwangpj@users.noreply.github.com>
2025-09-24 07:49:36 -0400

    Update

--- Mathlib/AlgebraicGeometry/AffineScheme.lean
+++ Mathlib/AlgebraicGeometry/AffineScheme.lean
@@ -165,7 +165,6 @@ instance Spec_faithful : Spec.Faithful := Functor.Faithful.toEssImage _
 
 instance Spec_essSurj : Spec.EssSurj := Functor.EssSurj.toEssImage (F := _)
 
-instance Spec_isEquivalence : Spec.IsEquivalence where
 
 /-- The forgetful functor `AffineScheme ⥤ Scheme`. -/
 @[simps!]
