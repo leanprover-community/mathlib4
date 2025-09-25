1c2b4e2dd2e8444ce33861d402d1059e14877611
Salvatore Mercuri <salvatore.mercuri@imperial.ac.uk>
2025-09-25 12:01:37 +0100

    linter fixes

--- Mathlib/Analysis/Normed/Ring/WithAbs.lean
+++ Mathlib/Analysis/Normed/Ring/WithAbs.lean
@@ -51,10 +51,13 @@ instance instInhabited : Inhabited (WithAbs v) := ⟨0⟩
 /-- The canonical (semiring) equivalence between `WithAbs v` and `R`. -/
 def equiv : WithAbs v ≃+* R := RingEquiv.refl _
 
+/-- The canonical (semiring) equivalence between `WithAbs v` and `WithAbs w`, for any two
+absolute values `v` and `w` on `R`. -/
 def equivWithAbs (v w : AbsoluteValue R S) : WithAbs v ≃+* WithAbs w :=
     (WithAbs.equiv v).trans <| (WithAbs.equiv w).symm
 
-def equivWithAbs_symm (v w : AbsoluteValue R S) : (equivWithAbs v w).symm = equivWithAbs w v := rfl
+theorem equivWithAbs_symm (v w : AbsoluteValue R S) :
+    (equivWithAbs v w).symm = equivWithAbs w v := rfl
 
 @[simp]
 theorem equiv_equivWithAbs_symm_apply {v w : AbsoluteValue R S} {x : WithAbs w} :
