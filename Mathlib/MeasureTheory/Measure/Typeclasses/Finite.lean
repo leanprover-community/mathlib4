bcfb0a0bb8296a9c12dfa4f169adaee18b94e3be
DavidLedvinka <50644608+DavidLedvinka@users.noreply.github.com>
2025-09-25 04:41:12 -0400

    Update Mathlib/MeasureTheory/Measure/Typeclasses/Finite.lean
    Co-authored-by: Rémy Degenne <remydegenne@gmail.com>

--- Mathlib/MeasureTheory/Measure/Typeclasses/Finite.lean
+++ Mathlib/MeasureTheory/Measure/Typeclasses/Finite.lean
@@ -122,9 +122,9 @@ theorem Measure.isFiniteMeasure_of_map {μ : Measure α} {f : α → β}
     rw [← Set.preimage_univ (f := f), ← map_apply_of_aemeasurable hf .univ]
     exact IsFiniteMeasure.measure_univ_lt_top
 
-theorem Measure.isFiniteMeasure_iff_isFiniteMeasure_map {μ : Measure α} {f : α → β}
-    (hf : AEMeasurable f μ) : IsFiniteMeasure μ ↔ IsFiniteMeasure (μ.map f) :=
-  ⟨fun _ ↦ isFiniteMeasure_map μ f, fun _ ↦ isFiniteMeasure_of_isFiniteMeasure_map hf⟩
+theorem Measure.isFiniteMeasure_map_iff {μ : Measure α} {f : α → β}
+    (hf : AEMeasurable f μ) : IsFiniteMeasure (μ.map f) ↔ IsFiniteMeasure μ :=
+  ⟨fun _ ↦ isFiniteMeasure_of_isFiniteMeasure_map hf, fun _ ↦ isFiniteMeasure_map μ f⟩
 
 instance IsFiniteMeasure_comap (f : β → α) [IsFiniteMeasure μ] : IsFiniteMeasure (μ.comap f) where
   measure_univ_lt_top := by
