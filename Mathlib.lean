9979d98621ce4cbadf74bdeff5befa2c0a9e6d7c
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2025-09-25 14:13:43 +0800

    Update Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -5504,6 +5504,7 @@ import Mathlib.RingTheory.FreeCommRing
 import Mathlib.RingTheory.FreeRing
 import Mathlib.RingTheory.Frobenius
 import Mathlib.RingTheory.Generators
+import Mathlib.RingTheory.GlobalDimension
 import Mathlib.RingTheory.GradedAlgebra.Basic
 import Mathlib.RingTheory.GradedAlgebra.FiniteType
 import Mathlib.RingTheory.GradedAlgebra.Homogeneous.Ideal
@@ -5793,6 +5794,7 @@ import Mathlib.RingTheory.RegularLocalRing.GlobalDimension
 import Mathlib.RingTheory.RegularLocalRing.RegularRing.Basic
 import Mathlib.RingTheory.RegularLocalRing.RegularRing.GlobalDimension
 import Mathlib.RingTheory.RegularLocalRing.RegularRing.Polynomial
+import Mathlib.RingTheory.RegularLocalRing.RegularRing.Syzygy
 import Mathlib.RingTheory.RingHom.Etale
 import Mathlib.RingTheory.RingHom.FaithfullyFlat
 import Mathlib.RingTheory.RingHom.Finite
