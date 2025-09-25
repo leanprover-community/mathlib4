56eaa894b7e62704d27522a4b2b2e2e24cf82ac9
Remy Degenne <remydegenne@gmail.com>
2025-09-25 11:09:42 +0200

    fix

--- Mathlib/MeasureTheory/Measure/SubFinite.lean
+++ Mathlib/MeasureTheory/Measure/SubFinite.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2025 Rémy Degenne. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Rémy Degenne. All rights reserved.
+Authors: Rémy Degenne
 -/
 import Mathlib.MeasureTheory.Measure.Decomposition.RadonNikodym
 import Mathlib.MeasureTheory.Measure.Sub
