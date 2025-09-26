f0320cdb2a292b2c1c336abaabb99063ef320837
Joël Riou <37772949+joelriou@users.noreply.github.com>
2025-09-26 06:47:14 +0000

    feat(CategoryTheory): limit presentations (#29849)
    This PR introduces `LimitPresentation` which is the dual notion to `ColimitPresentation` introduced in #29382.

--- Mathlib/CategoryTheory/Limits/Presentation.lean
+++ Mathlib/CategoryTheory/Limits/Presentation.lean
@@ -19,10 +19,11 @@ the data of objects `Dⱼ` and natural maps `sⱼ : Dⱼ ⟶ X` that make `X` th
   `{Dᵢ}` in `C` and natural maps `sᵢ : Dᵢ ⟶ X` making `X` into the colimit of the `Dᵢ`.
 - `CategoryTheory.Limits.ColimitPresentation.bind`: Given a colimit presentation of `X` and
   colimit presentations of the components, this is the colimit presentation over the sigma type.
+- `CategoryTheory.Limits.LimitPresentation`: A limit presentation of `X` over `J` is a diagram
+  `{Dᵢ}` in `C` and natural maps `sᵢ : X ⟶ Dᵢ` making `X` into the limit of the `Dᵢ`.
 
 ## TODOs:
 
-- Dualise to obtain `LimitPresentation`.
 - Refactor `TransfiniteCompositionOfShape` so that it extends `ColimitPresentation`.
 -/
 
@@ -52,6 +53,9 @@ initialize_simps_projections ColimitPresentation (-isColimit)
 abbrev cocone (pres : ColimitPresentation J X) : Cocone pres.diag :=
   Cocone.mk _ pres.ι
 
+lemma hasColimit (pres : ColimitPresentation J X) : HasColimit pres.diag :=
+  ⟨_, pres.isColimit⟩
+
 /-- The canonical colimit presentation of any object over a point. -/
 @[simps]
 noncomputable
@@ -69,6 +73,15 @@ def map (P : ColimitPresentation J X) {D : Type*} [Category D] (F : C ⥤ D)
   diag := P.diag ⋙ F
   ι := Functor.whiskerRight P.ι F ≫ (F.constComp _ _).hom
   isColimit := (isColimitOfPreserves F P.isColimit).ofIsoColimit (Cocones.ext (.refl _) (by simp))
+
+/-- If `P` is a colimit presentation of `X`, it is possible to define another
+colimit presentation of `X` where `P.diag` is replaced by an isomorphic functor. -/
+@[simps]
+def changeDiag (P : ColimitPresentation J X) {F : J ⥤ C} (e : F ≅ P.diag) :
+    ColimitPresentation J X where
+  diag := F
+  ι := e.hom ≫ P.ι
+  isColimit := (IsColimit.precomposeHomEquiv e _).2 P.isColimit
 
 /-- Map a colimit presentation under an isomorphism. -/
 @[simps]
@@ -215,4 +228,74 @@ end Small
 
 end
 
-end CategoryTheory.Limits.ColimitPresentation
+end ColimitPresentation
+
+/-- A limit presentation of `X` over `J` is a diagram `{Dᵢ}` in `C` and natural maps
+`sᵢ : X ⟶ Dᵢ` making `X` into the limit of the `Dᵢ`. -/
+structure LimitPresentation (J : Type w) [Category.{t} J] (X : C) where
+  /-- The diagram `{Dᵢ}`. -/
+  diag : J ⥤ C
+  /-- The natural maps `sᵢ : X ⟶ Dᵢ`. -/
+  π : (Functor.const J).obj X ⟶ diag
+  /-- `X` is the limit of the `Dᵢ` via `sᵢ`. -/
+  isLimit : IsLimit (Cone.mk _ π)
+
+variable {J : Type w} [Category.{t} J] {X : C}
+
+namespace LimitPresentation
+
+initialize_simps_projections LimitPresentation (-isLimit)
+
+/-- The cone associated to a limit presentation. -/
+abbrev cone (pres : LimitPresentation J X) : Cone pres.diag :=
+  Cone.mk _ pres.π
+
+lemma hasLimit (pres : LimitPresentation J X) : HasLimit pres.diag :=
+  ⟨_, pres.isLimit⟩
+
+/-- The canonical limit presentation of any object over a point. -/
+@[simps]
+noncomputable
+def self (X : C) : LimitPresentation PUnit.{s + 1} X where
+  diag := (Functor.const _).obj X
+  π := 𝟙 _
+  isLimit := isLimitConstCone _ _
+
+/-- If `F` preserves limits of shape `J`, it maps limit presentations of `X` to
+limit presentations of `F(X)`. -/
+@[simps]
+noncomputable
+def map (P : LimitPresentation J X) {D : Type*} [Category D] (F : C ⥤ D)
+    [PreservesLimitsOfShape J F] : LimitPresentation J (F.obj X) where
+  diag := P.diag ⋙ F
+  π := (F.constComp _ _).inv ≫ Functor.whiskerRight P.π F
+  isLimit := (isLimitOfPreserves F P.isLimit).ofIsoLimit (Cones.ext (.refl _) (by simp))
+
+/-- If `P` is a limit presentation of `X`, it is possible to define another
+limit presentation of `X` where `P.diag` is replaced by an isomorphic functor. -/
+@[simps]
+def changeDiag (P : LimitPresentation J X) {F : J ⥤ C} (e : F ≅ P.diag) :
+    LimitPresentation J X where
+  diag := F
+  π := P.π ≫ e.inv
+  isLimit := (IsLimit.postcomposeHomEquiv e.symm _).2 P.isLimit
+
+/-- Map a limit presentation under an isomorphism. -/
+@[simps]
+def ofIso (P : LimitPresentation J X) {Y : C} (e : X ≅ Y) : LimitPresentation J Y where
+  diag := P.diag
+  π := (Functor.const J).map e.inv ≫ P.π
+  isLimit := P.isLimit.ofIsoLimit (Cones.ext e)
+
+/-- Change the index category of a limit presentation. -/
+@[simps]
+noncomputable
+def reindex (P : LimitPresentation J X) {J' : Type*} [Category J'] (F : J' ⥤ J) [F.Initial] :
+    LimitPresentation J' X where
+  diag := F ⋙ P.diag
+  π := F.whiskerLeft P.π
+  isLimit := (Functor.Initial.isLimitWhiskerEquiv F _).symm P.isLimit
+
+end LimitPresentation
+
+end CategoryTheory.Limits
