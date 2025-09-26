ccd3b24ec10d66505f2e86f543c8fac4db777048
Bryan Gin-ge Chen <bryangingechen@gmail.com>
2025-09-25 21:04:01 -0400

    add Archive, Counterexamples, fixes

--- scripts/remove_deprecated_decls_CI.sh
+++ scripts/remove_deprecated_decls_CI.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-# Check if there are removed deprecated declarations and file a PR if necessary.
+# Check if there are changes in the working directory
+# (which should come from the linter removing deprecated declarations)
+# and file a PR if necessary.
 # Also set an output parameter 'message' containing a Zulip message
-# DO NOT run this as a human; this is meant only for automation usage!
+# DO NOT run this as a human; this is meant only for usage in automation!
 
 # Make this script robust against unintentional errors.
 # See e.g. http://redsymbol.net/articles/unofficial-bash-strict-mode/ for explanation.
@@ -17,15 +19,15 @@ owner_name=leanprover-community
 from_date="${1:-from date not set}"
 to_date="${2:-to date not set}"
 
-# Exit if the branch already exists
+# Exit if the branch already exists (a PR is already open)
 git fetch --quiet "$remote_name"
 git rev-parse --verify --quiet "refs/remotes/${remote_name}/${branch_name}" && exit 0
 
-pr_title="chore: remove deprecated declarations from $from_date to $to_date"
-pr_body='I am happy to remove some deprecated declarations for you!'
+pr_title="chore: remove declarations deprecated between $from_date and $to_date"
+pr_body='I am happy to remove some deprecated declarations for you! Please check if there are any remaining stray comments or other issues before merging.'
 
 git checkout -b "$branch_name"
-git add -A
+git add -u
 git commit -m "$pr_title" || { echo "No changes to commit" && exit 0; }
 
 gh_api() {
@@ -47,4 +49,4 @@ pr_id="$(gh_api "repos/${owner_name}/mathlib4/pulls" -X POST -d @- <<EOF | jq -r
 EOF
 )"
 
-printf $'message<<EOF\nPlease review #%s, which removes deprecated declarations older than %s.\nEOF\n' "${pr_id}" "${to_date}" | tee -a "${GITHUB_OUTPUT}"
+printf $'message<<EOF\nPlease review #%s, which removes deprecated declarations between %s and %s. Pay special attention to whether there are stray comments that can also be deleted.\nEOF\n' "${pr_id}" "${from_date}" "${to_date}" | tee -a "${GITHUB_OUTPUT}"
