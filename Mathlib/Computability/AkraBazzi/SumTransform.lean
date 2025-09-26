4bf91607f208999970f2df614dd4a2068857aae8
Harald Husum <harald.husum@gmail.com>
2025-09-26 00:18:42 +0000

    doc: move docstring references to definitions to correct locations (#29479)
    This addresses some instances of docstrings referring to results that have been moved away form their prior locations.

--- Mathlib/Computability/AkraBazzi/SumTransform.lean
+++ Mathlib/Computability/AkraBazzi/SumTransform.lean
@@ -18,11 +18,10 @@ We develop further preliminaries required for the theorem, up to the sum transfo
 
 * `AkraBazziRecurrence T g a b r`: the predicate stating that `T : ℕ → ℝ` satisfies an Akra-Bazzi
   recurrence with parameters `g`, `a`, `b` and `r` as above.
-* `GrowsPolynomially`: The growth condition that `g` must satisfy for the theorem to apply.
-  It roughly states that
-  `c₁ g(n) ≤ g(u) ≤ c₂ g(n)`, for u between b*n and n for any constant `b ∈ (0,1)`.
 * `sumTransform`: The transformation which turns a function `g` into
   `n^p * ∑ u ∈ Finset.Ico n₀ n, g u / u^(p+1)`.
+* `asympBound`: The asymptotic bound satisfied by an Akra-Bazzi recurrence, namely
+  `n^p (1 + ∑ g(u) / u^(p+1))`
 
 
 ## References
@@ -474,9 +473,9 @@ lemma isTheta_smoothingFn_sub_self (i : α) :
 ### Akra-Bazzi exponent `p`
 
 Every Akra-Bazzi recurrence has an associated exponent, denoted by `p : ℝ`, such that
-`∑ a_i b_i^p = 1`.  This section shows the existence and uniqueness of this exponent `p` for any
-`R : AkraBazziRecurrence`, and defines `R.asympBound` to be the asymptotic bound satisfied by `R`,
-namely `n^p (1 + ∑_{u < n} g(u) / u^(p+1))`. -/
+`∑ a_i b_i^p = 1`. This section shows the existence and uniqueness of this exponent `p` for any
+`R : AkraBazziRecurrence`. These results are used in the next section to define the asymptotic
+bound expression. -/
 
 @[continuity, fun_prop]
 lemma continuous_sumCoeffsExp : Continuous (fun (p : ℝ) => ∑ i, a i * (b i) ^ p) := by
@@ -535,8 +534,9 @@ lemma sumCoeffsExp_p_eq_one : ∑ i, a i * (b i) ^ p a b = 1 := by
 ### The sum transform
 
 This section defines the "sum transform" of a function `g` as
-`∑ u ∈ Finset.Ico n₀ n, g u / u^(p+1)`,
-and uses it to define `asympBound` as the bound satisfied by an Akra-Bazzi recurrence.
+`∑ u ∈ Finset.Ico n₀ n, g u / u^(p+1)`, and uses it to define `asympBound` as the bound satisfied
+by an Akra-Bazzi recurrence, namely `n^p (1 + ∑_{u < n} g(u) / u^(p+1))`. Here, the exponent `p`
+refers to the one established in the previous section.
 
 Several properties of the sum transform are then proven.
 -/
