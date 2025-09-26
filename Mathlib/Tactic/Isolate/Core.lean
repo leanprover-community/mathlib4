564f02d8df3084e563508046334fd0fa5540c729
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2025-09-26 17:30:42 +0100

    omit import

--- Mathlib/Tactic/Isolate/Core.lean
+++ Mathlib/Tactic/Isolate/Core.lean
@@ -3,7 +3,6 @@ Copyright (c) 2025 Heather Macbeth. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Heather Macbeth
 -/
-import Mathlib.Control.Basic
 import Mathlib.Tactic.GCongr.Core
 import Mathlib.Tactic.Relation.Symm
 
