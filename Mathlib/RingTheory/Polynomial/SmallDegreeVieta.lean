0fee49b117ec946b9ecf56be6c919e2234c19626
Christopher Hoskin <mans0954@users.noreply.github.com>
2025-09-26 19:09:13 +0100

    degree roots_quadratic_eq_pair_iff_of_ne_zero

--- Mathlib/RingTheory/Polynomial/SmallDegreeVieta.lean
+++ Mathlib/RingTheory/Polynomial/SmallDegreeVieta.lean
@@ -59,7 +59,7 @@ lemma eq_mul_mul_of_aroots_quadratic_eq_pair [CommRing T] [CommRing S] [IsDomain
 
 /-- **Vieta's formula** for quadratics as an iff. -/
 lemma roots_quadratic_eq_pair_iff_of_ne_zero [CommRing R] [IsDomain R] {x1 x2 : R} {p : R[X]}
-    (hp : p.natDegree = 2) :
+    (hp : p.degree = 2) :
     p.roots = {x1, x2} ↔
       p.coeff 1 = -(p.coeff 2) * (x1 + x2) ∧ (p.coeff 0) = (p.coeff 2) * x1 * x2 :=
   let a := p.coeff 2
@@ -68,17 +68,14 @@ lemma roots_quadratic_eq_pair_iff_of_ne_zero [CommRing R] [IsDomain R] {x1 x2 : 
   have roots_of_ne_zero_of_vieta (hvieta : b = -a * (x1 + x2) ∧ c = a * x1 * x2) :
       p.roots = {x1, x2} := by
     suffices p = C a * (X - C x1) * (X - C x2) by
-      have h0 : (p.coeff 2) ≠ 0 := coeff_ne_zero_of_eq_natDegree Nat.zero_lt_two hp
+      have h0 : (p.coeff 2) ≠ 0 := coeff_ne_zero_of_eq_degree hp
       have h1 : C a * (X - C x1) ≠ 0 := mul_ne_zero (by simpa) (Polynomial.X_sub_C_ne_zero _)
       have h2 : C a * (X - C x1) * (X - C x2) ≠ 0 := mul_ne_zero h1 (Polynomial.X_sub_C_ne_zero _)
       simp [this, Polynomial.roots_mul h2, Polynomial.roots_mul h1]
-    have ep : p = C a * X ^ 2 + C b * X + C c := eq_quadratic_of_natDegree_le_two (Nat.le_of_eq hp)
+    have ep : p = C a * X ^ 2 + C b * X + C c := eq_quadratic_of_degree_le_two (le_of_eq hp)
     simpa [ep, hvieta.1, hvieta.2] using by ring_nf
-  ⟨fun h => ⟨eq_neg_mul_add_of_roots_quadratic_eq_pair
-    ((degree_eq_iff_natDegree_eq_of_pos Nat.zero_lt_two).mpr hp) h,
-    eq_mul_mul_of_roots_quadratic_eq_pair
-      ((degree_eq_iff_natDegree_eq_of_pos Nat.zero_lt_two).mpr hp) h⟩,
-    roots_of_ne_zero_of_vieta⟩
+  ⟨fun h => ⟨eq_neg_mul_add_of_roots_quadratic_eq_pair hp h,
+    eq_mul_mul_of_roots_quadratic_eq_pair hp h⟩, roots_of_ne_zero_of_vieta⟩
 
 /-- **Vieta's formula** for quadratics as an iff (`aroots` version). -/
 lemma aroots_quadratic_eq_pair_iff_of_ne_zero [CommRing T] [CommRing S] [IsDomain S]
@@ -86,13 +83,15 @@ lemma aroots_quadratic_eq_pair_iff_of_ne_zero [CommRing T] [CommRing S] [IsDomai
     p.aroots S = {x1, x2} ↔
       algebraMap T S (p.coeff 1) = -algebraMap T S (p.coeff 2) * (x1 + x2) ∧
       algebraMap T S (p.coeff 0) = algebraMap T S (p.coeff 2) * x1 * x2 := by
-  rw [roots_quadratic_eq_pair_iff_of_ne_zero ha, coeff_map, coeff_map, coeff_map]
+  rw [roots_quadratic_eq_pair_iff_of_ne_zero
+    ((degree_eq_iff_natDegree_eq_of_pos Nat.zero_lt_two).mpr ha), coeff_map, coeff_map, coeff_map]
 
 /-- **Vieta's formula** for quadratics as an iff (`Field` version). -/
 lemma roots_quadratic_eq_pair_iff_of_ne_zero' [Field R] {p : R[X]} {x1 x2 : R}
     (hp : p.natDegree = 2) : p.roots = {x1, x2} ↔
       x1 + x2 = -(p.coeff 1) / (p.coeff 2) ∧ x1 * x2 = (p.coeff 0) / (p.coeff 2) := by
-  rw [roots_quadratic_eq_pair_iff_of_ne_zero hp]
+  rw [roots_quadratic_eq_pair_iff_of_ne_zero
+    ((degree_eq_iff_natDegree_eq_of_pos Nat.zero_lt_two).mpr hp)]
   have h0 : (p.coeff 2) ≠ 0 := coeff_ne_zero_of_eq_natDegree Nat.zero_lt_two hp
   field_simp
   exact and_congr ⟨fun h => by linear_combination h, fun h => by linear_combination h⟩
