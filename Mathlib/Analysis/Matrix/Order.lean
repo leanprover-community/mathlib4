f98e1b4dafb7c96825701fa79df55006feb6e3b9
Monica Omar <2497250a@research.gla.ac.uk>
2025-09-26 14:48:44 +0000

    fix

--- Mathlib/Analysis/Matrix/Order.lean
+++ Mathlib/Analysis/Matrix/Order.lean
@@ -18,7 +18,7 @@ This allows us to use more general results from C⋆-algebras, like `CFC.sqrt`.
 
 * `Matrix.instPartialOrder`: the partial order on matrices given by `x ≤ y := (y - x).PosSemidef`.
 * `Matrix.PosSemidef.dotProduct_mulVec_zero_iff`: for a positive semi-definite matrix `A`,
-we have x⋆ A x = 0` iff `A x = 0
+we have `x⋆ A x = 0` iff `A x = 0`.
 * `Matrix.PosDef.matrixInnerProductSpace`: the inner product on matrices induced by a
 positive definite matrix `M`: `⟪x, y⟫ = (y * M * xᴴ).trace`.
 
