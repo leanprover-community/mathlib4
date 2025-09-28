866012f3935e08434f8e41497ad1f1867439c2f2
Damiano Testa <adomani@gmail.com>
2025-09-28 15:34:53 +0200

    reduce import

--- Mathlib/Tactic/Linter/FindDeprecations.lean
+++ Mathlib/Tactic/Linter/FindDeprecations.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Damiano Testa
 -/
 --import Mathlib --.Deprecated.Order
-import Lean
+--import Lean
 import ImportGraph.Imports
 --import Mathlib.mwe_deprecations
 
