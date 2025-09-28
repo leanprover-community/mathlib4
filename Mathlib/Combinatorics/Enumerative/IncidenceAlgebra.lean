7ff5ba50936a063a5a9066f40e592e736e3dcde5
Emmanuel Ferdman <emmanuelferdman@gmail.com>
2025-09-28 21:35:44 +0300

    chore: deprecate duplicate `zeta_mul_kappa` lemma
    Signed-off-by: Emmanuel Ferdman <emmanuelferdman@gmail.com>

--- Mathlib/Combinatorics/Enumerative/IncidenceAlgebra.lean
+++ Mathlib/Combinatorics/Enumerative/IncidenceAlgebra.lean
@@ -352,6 +352,8 @@ lemma zeta_mul_zeta [NonAssocSemiring 𝕜] [Preorder α] [LocallyFiniteOrder α
   rw [mem_Icc] at hx
   rw [zeta_of_le hx.1, zeta_of_le hx.2, one_mul]
 
+@[deprecated (since := "2025-09-28")] alias zeta_mul_kappa := zeta_mul_zeta
+
 section Mu
 variable (𝕜) [AddCommGroup 𝕜] [One 𝕜] [Preorder α] [LocallyFiniteOrder α] [DecidableEq α]
 
