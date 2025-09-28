17a03baa8b4acbece3706316001dbb23b8758299
Monica Omar <2497250a@research.gla.ac.uk>
2025-09-28 13:20:43 +0000

    add deprecations

--- Mathlib/Algebra/Star/SelfAdjoint.lean
+++ Mathlib/Algebra/Star/SelfAdjoint.lean
@@ -192,6 +192,11 @@ lemma _root_.IsUnit.isSelfAdjoint_conjugate_iff' {a u : R} (hu : IsUnit u) :
     IsSelfAdjoint (star u * a * u) ↔ IsSelfAdjoint a := by
   simpa using hu.star.isSelfAdjoint_conjugate_iff
 
+@[deprecated (since := "2025-09-28")] alias _root_.isSelfAdjoint_conjugate_iff_of_isUnit :=
+  IsUnit.isSelfAdjoint_conjugate_iff
+@[deprecated (since := "2025-09-28")] alias _root_.isSelfAdjoint_conjugate_iff_of_isUnit' :=
+  IsUnit.isSelfAdjoint_conjugate_iff'
+
 end Monoid
 
 section Semiring
