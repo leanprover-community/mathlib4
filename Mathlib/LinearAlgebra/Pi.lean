eb42cda806c41fea98f0a4db2d0d74044ab6838e
Etienne Marion <baguettes_casino0c@icloud.com>
2025-09-28 20:16:12 +0200

    shorter

--- Mathlib/LinearAlgebra/Pi.lean
+++ Mathlib/LinearAlgebra/Pi.lean
@@ -147,9 +147,7 @@ lemma single_apply [DecidableEq ι] {i : ι} (v : φ i) :
   rfl
 
 lemma sum_single_apply [Fintype ι] [DecidableEq ι] (v : Π i, φ i) :
-    ∑ i, Pi.single i (v i) = v := by
-  ext i
-  simp
+    ∑ i, Pi.single i (v i) = v := by ext; simp
 
 @[simp]
 theorem coe_single [DecidableEq ι] (i : ι) :
