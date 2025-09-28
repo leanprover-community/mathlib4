aa0e50c5a590997c1f12813a94b0b7f6f7ab3762
Emmanuel Ferdman <emmanuelferdman@gmail.com>
2025-09-28 21:13:39 +0300

    fix: remove duplicate `zeta_mul_zeta` lemma
    Signed-off-by: Emmanuel Ferdman <emmanuelferdman@gmail.com>

--- Mathlib/Combinatorics/Enumerative/IncidenceAlgebra.lean
+++ Mathlib/Combinatorics/Enumerative/IncidenceAlgebra.lean
@@ -352,13 +352,6 @@ lemma zeta_mul_zeta [NonAssocSemiring 𝕜] [Preorder α] [LocallyFiniteOrder α
   rw [mem_Icc] at hx
   rw [zeta_of_le hx.1, zeta_of_le hx.2, one_mul]
 
-lemma zeta_mul_kappa [NonAssocSemiring 𝕜] [Preorder α] [LocallyFiniteOrder α] [DecidableLE α]
-    (a b : α) : (zeta 𝕜 * zeta 𝕜 : IncidenceAlgebra 𝕜 α) a b = (Icc a b).card := by
-  rw [mul_apply, card_eq_sum_ones, Nat.cast_sum, Nat.cast_one]
-  refine sum_congr rfl fun x hx ↦ ?_
-  rw [mem_Icc] at hx
-  rw [zeta_of_le hx.1, zeta_of_le hx.2, one_mul]
-
 section Mu
 variable (𝕜) [AddCommGroup 𝕜] [One 𝕜] [Preorder α] [LocallyFiniteOrder α] [DecidableEq α]
 
