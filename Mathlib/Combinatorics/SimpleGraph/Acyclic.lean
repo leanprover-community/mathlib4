f3b476069cc45ed48684c90198711f6156896504
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2025-09-29 12:29:48 +0000

    Add lemma `IsTree.dist_ne_of_adj` and necessary lemmas

--- Mathlib/Combinatorics/SimpleGraph/Acyclic.lean
+++ Mathlib/Combinatorics/SimpleGraph/Acyclic.lean
@@ -5,6 +5,7 @@ Authors: Kyle Miller
 -/
 import Mathlib.Combinatorics.SimpleGraph.Connectivity.Connected
 import Mathlib.Combinatorics.SimpleGraph.DegreeSum
+import Mathlib.Combinatorics.SimpleGraph.Metric
 import Mathlib.Tactic.Linarith
 
 /-!
@@ -122,6 +123,42 @@ theorem isAcyclic_of_path_unique (h : ∀ (v w : V) (p q : G.Path v w), p = q) :
 
 theorem isAcyclic_iff_path_unique : G.IsAcyclic ↔ ∀ ⦃v w : V⦄ (p q : G.Path v w), p = q :=
   ⟨IsAcyclic.path_unique, isAcyclic_of_path_unique⟩
+
+lemma IsAcyclic.mem_support_of_ne_mem_support_of_adj_of_isPath (hG : G.IsAcyclic) {u v w : V}
+    {p : G.Walk u v} {q : G.Walk u w} (hp : p.IsPath) (hq : q.IsPath) (hadj : G.Adj v w)
+    (hv : v ∉ q.support) : w ∈ p.support := by
+  let r := q.concat hadj.symm
+  have hr : r.IsPath := hq.concat hv hadj.symm
+  have h := isAcyclic_iff_path_unique.mp hG ⟨p, hp⟩ ⟨r, hr⟩
+  simp only [Subtype.mk.injEq] at h
+  rw [h]
+  unfold r
+  apply q.support_subset_support_concat
+  exact q.end_mem_support
+
+lemma IsAcyclic.ne_mem_support_of_support_of_adj_of_isPath (hG : G.IsAcyclic) {u v w : V}
+    {p : G.Walk u v} {q : G.Walk u w} (hp : p.IsPath) (hq : q.IsPath) (hadj : G.Adj v w)
+    (hw : w ∈ p.support) : v ∉ q.support := by
+  obtain ⟨p₀, p₁, hp₀, hp₁, happend⟩ := hp.mem_support_iff_exists_append.mp hw
+  have hp₀p₁ : (p₀.append p₁).IsPath := by
+    rw [← happend]
+    exact hp
+  have hvw : v ≠ w := hadj.symm.ne'
+  have hvp₁ : v ∈ p₁.support := p₁.end_mem_support
+  have hp₀q := hG.path_unique ⟨p₀, hp₀⟩ ⟨q, hq⟩
+  simp only [Subtype.mk.injEq] at hp₀q
+  rw [← hp₀q]
+  intro hxp
+  exact hp₀p₁.ne_of_mem_support_of_append hvw hxp hvp₁ rfl
+
+lemma IsAcyclic.path_concat (hG : G.IsAcyclic) {u v w : V} {p : G.Walk u v} {q : G.Walk u w}
+    (hp : p.IsPath) (hq : q.IsPath) (hadj : G.Adj v w) (hv : v ∈ q.support) :
+    q = p.concat hadj := by
+  have hw : w ∉ p.support := hG.ne_mem_support_of_support_of_adj_of_isPath hq hp hadj.symm hv
+  have hpw : (p.concat hadj).IsPath := hp.concat hw hadj
+  have h := isAcyclic_iff_path_unique.mp hG ⟨q, hq⟩ ⟨p.concat hadj, hpw⟩
+  simp only [Subtype.mk.injEq] at h
+  exact h
 
 theorem isTree_iff_existsUnique_path :
     G.IsTree ↔ Nonempty V ∧ ∀ v w : V, ∃! p : G.Walk v w, p.IsPath := by
@@ -253,5 +290,24 @@ lemma IsTree.exists_vert_degree_one_of_nontrivial [Fintype V] [Nontrivial V] [De
   use v
   rw [← hv]
   exact h.minDegree_eq_one_of_nontrivial
+
+lemma IsTree.dist_ne_of_adj (hG : G.IsTree) (u : V) {v w : V} (hadj : SimpleGraph.Adj G v w) :
+    G.dist u v ≠ G.dist u w := by
+  obtain ⟨p, hp, hp'⟩ := hG.isConnected.exists_path_of_dist u v
+  obtain ⟨q, hq, hq'⟩ := hG.isConnected.exists_path_of_dist u w
+  rw [← hp', ← hq']
+  by_cases hw : w ∈ p.support
+  · have hp : p = q.concat hadj.symm := hG.IsAcyclic.path_concat hq hp hadj.symm hw
+    rw [hp]
+    rw [q.length_concat]
+    exact q.length.ne_add_one.symm
+  · have hv : v ∈ q.support := hG.IsAcyclic.mem_support_of_ne_mem_support_of_adj_of_isPath hq hp
+      hadj.symm hw
+    have h : (p.concat hadj).IsPath := by
+      rw [← hG.IsAcyclic.path_concat hp hq hadj hv]
+      exact hq
+    have hq : q = p.concat hadj := hG.IsAcyclic.path_concat hp hq hadj hv
+    rw [hq, p.length_concat]
+    exact p.length.ne_add_one
 
 end SimpleGraph
