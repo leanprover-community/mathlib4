e9ee1146c05682d87810405649e5350d01c015c6
Yakov Pechersky <ffxen158@gmail.com>
2025-09-29 11:08:54 -0400

    suggestions

--- Mathlib/RingTheory/ZMod/Torsion.lean
+++ Mathlib/RingTheory/ZMod/Torsion.lean
@@ -13,17 +13,19 @@ This file shows that the `ZMod p` has `p - 1` roots-of-unity.
 
 -/
 
-lemma ZMod.rootsOfUnity_eq_top {p : ℕ} [Fact p.Prime] :
+namespace ZMod
+
+lemma rootsOfUnity_eq_top {p : ℕ} [Fact p.Prime] :
     (rootsOfUnity (p - 1) (ZMod p)) = ⊤ := by
   ext
   simpa [Units.ext_iff] using pow_card_sub_one_eq_one (Units.ne_zero _)
 
 instance {p : ℕ} [Fact p.Prime] : HasEnoughRootsOfUnity (ZMod p) (p - 1) := by
-  have : NeZero (p - 1) := by
-    have : 2 ≤ p := Nat.Prime.two_le Fact.out
-    exact ⟨by grind⟩
+  have : NeZero (p - 1) := ⟨by have : 2 ≤ p := Nat.Prime.two_le Fact.out; grind⟩
   refine HasEnoughRootsOfUnity.of_card_le ?_
   have := Nat.card_congr (MulEquiv.subgroupCongr (ZMod.rootsOfUnity_eq_top (p := p))).toEquiv
   rw [Nat.card_eq_fintype_card] at this
   rw [this]
   simp [Fintype.card_units]
+
+end ZMod
