cd6188680757997029bbaa51bab2d0302c3401e5
Riccardo Brasca <riccardo.brasca@gmail.com>
2025-09-30 15:39:51 +0200

    ops

--- Mathlib/RingTheory/RootsOfUnity/Units.lean
+++ Mathlib/RingTheory/RootsOfUnity/Units.lean
@@ -2,7 +2,6 @@
 Copyright (c) 2021 Alex J. Best. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Alex J. Best, Riccardo Brasca
-
 -/
 import Mathlib.RingTheory.RootsOfUnity.PrimitiveRoots
 
