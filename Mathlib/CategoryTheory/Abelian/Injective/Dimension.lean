9852ae56db1864bc8c9d9431cfc2071c15c3e127
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2025-09-30 13:53:08 +0800

    fix copyright

--- Mathlib/CategoryTheory/Abelian/Injective/Dimension.lean
+++ Mathlib/CategoryTheory/Abelian/Injective/Dimension.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2024 Joël Riou. All rights reserved.
+Copyright (c) 2025 Nailin Guan. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Joël Riou, Nailin Guan
+Authors: Nailin Guan
 -/
 import Mathlib.Algebra.Homology.DerivedCategory.Ext.EnoughInjectives
 import Mathlib.CategoryTheory.Abelian.Exact
