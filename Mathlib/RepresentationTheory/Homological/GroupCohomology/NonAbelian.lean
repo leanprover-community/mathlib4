860086d2e1e185ab226af7e75034bde847e5af76
xyzw12345 <wangjt2020@163.com>
2025-09-30 11:07:24 +0800

    initialize branch

---
+++ Mathlib/RepresentationTheory/Homological/GroupCohomology/NonAbelian.lean
@@ -0,0 +1,17 @@
+import Mathlib.RepresentationTheory.Homological.GroupCohomology.LowDegree
+
+/-!
+# Non-abelian group cohomology
+
+Let `G` be a group acting on another (not necessarily abelian) group `A`, in this file we define
+`H⁰(G, A)` and `H¹(G, A)`, and prove some basic properties about it.
+
+## Main Results
+
+## Reference
+
+-/
+
+namespace groupCohomology
+
+end groupCohomology
