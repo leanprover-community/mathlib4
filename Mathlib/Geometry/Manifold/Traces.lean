fb21400b8e9424af37c01d9512f19e637d68be4a
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-09-30 08:37:29 +0200

    Document trace classes

--- Mathlib/Geometry/Manifold/Traces.lean
+++ Mathlib/Geometry/Manifold/Traces.lean
@@ -8,8 +8,16 @@ import Lean.Util.Trace
 /-!
 # Trace classes for differential geometry elaborators
 
-TODO: add a more complete doc-string
+This file defines custom trace classes for the differential geometry elaborators in
+`Mathlib.Geometry.Manifold.Elaborators.lean`. These need to be in a different file for technical
+reasons.
 
+The `TotalSpaceMk` trace class is associated to the `T%` elaborator, which converts a section
+of a vector bundle as a dependent function to a non-dependent function into the total space.
+
+The `MDiffElab` trace class corresponds to the elaborators `CMDiff` and friends, and provides
+tracing information about inferring a model with corners on the domain (resp. codomain) of the
+map in question.
 -/
 open Lean
 
