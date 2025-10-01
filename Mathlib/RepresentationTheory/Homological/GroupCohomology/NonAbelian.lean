8e91a30e8bfd7cd5ce8b284d805f53709ea79425
xyzw12345 <wangjt2020@163.com>
2025-10-01 11:14:50 +0800

    golf

--- Mathlib/RepresentationTheory/Homological/GroupCohomology/NonAbelian.lean
+++ Mathlib/RepresentationTheory/Homological/GroupCohomology/NonAbelian.lean
@@ -56,15 +56,9 @@ variable (G : Type u) [Monoid G]
 
 def H0 (A : Type*) [AddGroup A] [DistribMulAction G A] : AddSubgroup A where
   carrier := setOf fun v => ∀ g : G, g • v = v
-  add_mem' := by
-    intro a b ha hb g
-    simp [ha g, hb g, -Pi.add_apply]
-  zero_mem' := by
-    intro g
-    simp
-  neg_mem' := by
-    intro a ha g
-    simp [ha g]
+  add_mem' := by simp +contextual
+  zero_mem' := by simp
+  neg_mem' := by simp +contextual
 
 variable {G}
 
