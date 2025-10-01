b50506196e5944509dbb963fcafc844e1de4aa26
xyzw12345 <wangjt2020@163.com>
2025-10-01 09:53:35 +0800

    update things

--- Mathlib/RepresentationTheory/Homological/GroupCohomology/NonAbelian.lean
+++ Mathlib/RepresentationTheory/Homological/GroupCohomology/NonAbelian.lean
@@ -47,7 +47,7 @@ section H0
 
 variable (G : Type u) [Monoid G]
 
-def H0 (A : NonAbelianRep G) : AddSubgroup A where
+def H0 (A : Type*) [AddGroup A] [DistribMulAction G A] : AddSubgroup A where
   carrier := setOf fun v => ∀ g : G, g • v = v
   add_mem' := sorry
   zero_mem' := sorry
@@ -55,15 +55,20 @@ def H0 (A : NonAbelianRep G) : AddSubgroup A where
 
 variable {G}
 
-def H0.map {A B : NonAbelianRep G} (f : A →+[G] B) : H0 G A →+ H0 G B := sorry
+def H0.map {A B : Type*} [AddGroup A] [AddGroup B] [DistribMulAction G A] [DistribMulAction G B]
+    (f : A →+[G] B) : H0 G A →+ H0 G B := sorry
 
-theorem H0.map_id (A : NonAbelianRep G) : H0.map (.id _) = .id (H0 G A) := sorry
+variable (G) in
+theorem H0.map_id (A : Type*) [AddGroup A] [DistribMulAction G A] :
+    H0.map (.id _) = .id (H0 G A) := sorry
 
-theorem H0.map_comp {A B C : NonAbelianRep G} (f : A →+[G] B) (g : B →+[G] C) :
-    H0.map (g.comp f) = (H0.map g).comp (H0.map f) := sorry
+theorem H0.map_comp {A B C : Type*} [AddGroup A] [AddGroup B] [AddGroup C]
+    [DistribMulAction G A] [DistribMulAction G B] [DistribMulAction G C]
+    (f : A →+[G] B) (g : B →+[G] C) : H0.map (g.comp f) = (H0.map g).comp (H0.map f) := sorry
 
-theorem H0.map_injective_of_injective {A B : NonAbelianRep G} (f : A →+[G] B)
-    (hf : Function.Injective f) : Function.Injective (H0.map f) := sorry
+theorem H0.map_injective_of_injective {A B : Type*} [AddGroup A] [AddGroup B] [DistribMulAction G A]
+    [DistribMulAction G B] (f : A →+[G] B) (hf : Function.Injective f) :
+    Function.Injective (H0.map f) := sorry
 
 -- def H0Functor : (NonAbelianRep G) ⥤ AddGrp := sorry
 
@@ -71,7 +76,7 @@ end H0
 
 section H1
 
-variable (G : Type u) [Monoid G] (A : NonAbelianRep G)
+variable (G : Type u) [Monoid G] (A : Type*) [AddGroup A] [DistribMulAction G A]
 
 def Z1 := { f : G → A // ∀ g h : G, f (g * h) = f g + g • f h}
 
@@ -82,11 +87,11 @@ instance inhabited : Inhabited (Z1 G A) := ⟨0⟩
 
 instance coeFun : CoeFun (Z1 G A) (fun _ ↦ G → A) := ⟨fun f ↦ f.val⟩
 
-variable {G} in
-def cohomologous {A : NonAbelianRep G} (f g : Z1 G A) : Prop :=
+variable {G} {A} in
+def cohomologous (f g : Z1 G A) : Prop :=
   ∃ a : A, ∀ h : G, g h = - a + f h + (h • a)
 
-instance setoid (A : NonAbelianRep G) : Setoid (Z1 G A) where
+instance setoid : Setoid (Z1 G A) where
   r := cohomologous
   iseqv := {
     refl := fun f => ⟨0, fun h => by simp⟩,
@@ -96,26 +101,29 @@ instance setoid (A : NonAbelianRep G) : Setoid (Z1 G A) where
 
 end Z1
 
-def H1 (A : NonAbelianRep G) : Pointed where
-  X := Quotient (Z1.setoid G A)
-  point := ⟦0⟧
+def H1 := Quotient (Z1.setoid G A)
 
 instance : Zero (H1 G A) := ⟨⟦0⟧⟩
 instance : Inhabited (H1 G A) := ⟨0⟩
 
 variable {G}
 
-def H1.map {A B : NonAbelianRep G} (f : A →+[G] B) : H1 G A → H1 G B :=
+def H1.map {A B : Type*} [AddGroup A] [AddGroup B] [DistribMulAction G A]
+    [DistribMulAction G B] (f : A →+[G] B) : H1 G A → H1 G B :=
   Quotient.map (fun z : Z1 G A => ⟨f ∘ z, fun g h => by simp [z.prop, map_smul]⟩)
     (fun z1 z2 ⟨a, ha⟩ => ⟨f a, fun h => by simp [ha, map_smul]⟩)
 
-theorem H1.map_id {A : NonAbelianRep G} : H1.map (.id _) = 𝟙 (H1 G A) := sorry
+variable (G) in
+theorem H1.map_id (A : Type*) [AddGroup A] [DistribMulAction G A] :
+    H1.map (.id _) = 𝟙 (H1 G A) :=
+  sorry
 
-theorem H1.map_zero {A B : NonAbelianRep G} (f : A →+[G] B) :
-    H1.map f 0 = 0 := sorry
+theorem H1.map_zero {A B : Type*} [AddGroup A] [AddGroup B] [DistribMulAction G A]
+    [DistribMulAction G B] (f : A →+[G] B) : H1.map f 0 = 0 := sorry
 
-theorem H1.map_comp {A B C : NonAbelianRep G} (f : A →+[G] B) (g : B →+[G] C) :
-    H1.map (g.comp f) = (H1.map g).comp (H1.map f) := sorry
+theorem H1.map_comp {A B C : Type*} [AddGroup A] [AddGroup B] [AddGroup C]
+    [DistribMulAction G A] [DistribMulAction G B] [DistribMulAction G C]
+    (f : A →+[G] B) (g : B →+[G] C) : H1.map (g.comp f) = (H1.map g).comp (H1.map f) := sorry
 
 -- def H1Functor : NonAbelianRep G ⥤ Pointed := sorry
 
@@ -123,21 +131,24 @@ end H1
 
 section connectHom
 
-variable {G : Type u} [Group G] (S : ShortComplex (NonAbelianRep G)) (hS : S.Exact)
+variable {G : Type u} [Group G] {A B C : Type*} [AddGroup A] [AddGroup B] [AddGroup C]
+    [DistribMulAction G A] [DistribMulAction G B] [DistribMulAction G C]
+    {f : A →+[G] B} {g : B →+[G] C} (hf : Function.Injective f) (hg : Function.Surjective g)
+    (hfg : Function.Exact f g)
 
-def δ₀₁ : H0 G S.X₃ → H1 G S.X₁ := sorry
+def δ₀₁ : H0 G C → H1 G A := sorry
 
-def δ₀₁_zero : δ₀₁ S 0 = 0 := sorry
+-- def δ₀₁_zero : δ₀₁ 0 = 0 := sorry
 
-theorem exact₁ : Function.Exact (H0.map (S.f : S.X₁ →+[G] S.X₂)) (H0.map (S.g : S.X₂ →+[G] S.X₃)) :=
-  sorry
+-- theorem exact₁ : Function.Exact (H0.map (S.f : S.X₁ →+[G] S.X₂)) (H0.map (S.g : S.X₂ →+[G] S.X₃)) :=
+--   sorry
 
-theorem exact₂ : Function.Exact (H0.map (S.g : S.X₂ →+[G] S.X₃)) (δ₀₁ S) := sorry
+-- theorem exact₂ : Function.Exact (H0.map (S.g : S.X₂ →+[G] S.X₃)) (δ₀₁ S) := sorry
 
-theorem exact₃ : Function.Exact (δ₀₁ S) (H1.map (S.f : S.X₁ →+[G] S.X₂)) := sorry
+-- theorem exact₃ : Function.Exact (δ₀₁ S) (H1.map (S.f : S.X₁ →+[G] S.X₂)) := sorry
 
-theorem exact₄ : Function.Exact (H1.map (S.f : S.X₁ →+[G] S.X₂)) (H1.map (S.g : S.X₂ →+[G] S.X₃)) :=
-  sorry
+-- theorem exact₄ : Function.Exact (H1.map (S.f : S.X₁ →+[G] S.X₂)) (H1.map (S.g : S.X₂ →+[G] S.X₃)) :=
+--   sorry
 
 end connectHom
 
