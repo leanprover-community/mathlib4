25df95484f82e8957d7e082f302ed57894a15d42
grunweg <10105016+grunweg@users.noreply.github.com>
2025-10-01 13:25:46 +0000

    chore: remove non-terminal simps (#30106)
    Found by the flexible linter; extracted from https://github.com/leanprover-community/mathlib4/pull/28962.
    
    I consider these changes improvements as well, given they improve robustness.
    
    Co-authored-by: Michael Rothgang <rothgang@math.uni-bonn.de>

--- Mathlib/Probability/ProbabilityMassFunction/Constructions.lean
+++ Mathlib/Probability/ProbabilityMassFunction/Constructions.lean
@@ -313,7 +313,7 @@ theorem support_bernoulli : (bernoulli p h).support = { b | cond b (p ≠ 0) (p 
       ENNReal.coe_one, Bool.cond_prop, Set.mem_setOf_eq, Bool.false_eq_true, ite_false, not_iff_not]
     constructor
     · intro h'
-      simp [tsub_eq_zero_iff_le] at h'
+      simp only [tsub_eq_zero_iff_le, one_le_coe_iff] at h'
       exact eq_of_le_of_ge h h'
     · intro h'
       simp only [h', ENNReal.coe_one, tsub_self]
