f07a21497d0e8b612a9359a225f16e578a101d49
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2025-10-01 19:46:24 +0200

    Update Mathlib/GroupTheory/GroupAction/SubMulAction/OfFixingSubgroup.lean
    Co-authored-by: Thomas Browning <tb65536@users.noreply.github.com>

--- Mathlib/GroupTheory/GroupAction/SubMulAction/OfFixingSubgroup.lean
+++ Mathlib/GroupTheory/GroupAction/SubMulAction/OfFixingSubgroup.lean
@@ -468,50 +468,27 @@ theorem IsPretransitive.isPretransitive_ofFixingSubgroup_inter
     IsPretransitive (fixingSubgroup M (s ∩ g • s)) (ofFixingSubgroup M (s ∩ g • s)) := by
   rw [Ne, Set.top_eq_univ, ← Set.compl_empty_iff, ← Ne, ← Set.nonempty_iff_ne_empty] at ha
   obtain ⟨a, ha⟩ := ha
+  rw [Set.compl_union] at ha
   have ha' : a ∈ (s ∩ g • s)ᶜ := by
     rw [Set.compl_inter]
-    apply Set.mem_union_left
-    rw [Set.compl_union] at ha
-    exact Set.mem_of_mem_inter_left ha
+    exact Set.mem_union_left _ ha.1
   rw [MulAction.isPretransitive_iff_base (⟨a, ha'⟩ : ofFixingSubgroup M (s ∩ g • s))]
   rintro ⟨x, hx⟩
   rw [mem_ofFixingSubgroup_iff, Set.mem_inter_iff, not_and_or] at hx
   rcases hx with hx | hx
-  · obtain ⟨⟨k, hk⟩, hkax⟩ := hs.exists_smul_eq
-      ⟨a, (by intro ha'; apply ha; apply Set.mem_union_left _ ha')⟩
-      ⟨x, hx⟩
-    use ⟨k, (by
-      rw [mem_fixingSubgroup_iff] at hk ⊢
-      intro y  hy
-      apply hk
-      exact Set.mem_of_mem_inter_left hy)⟩
+  · obtain ⟨⟨k, hk⟩, hkax⟩ := hs.exists_smul_eq ⟨a, ha.1⟩ ⟨x, hx⟩
+    use ⟨k, fun ⟨y, hy⟩ ↦ hk ⟨y, hy.1⟩⟩
+    rwa [Subtype.ext_iff] at hkax ⊢
+  · have hg'x : g⁻¹ • x ∈ ofFixingSubgroup M s := mt Set.mem_smul_set_iff_inv_smul_mem.mpr hx
+    have hg'a : g⁻¹ • a ∈ ofFixingSubgroup M s := mt Set.mem_smul_set_iff_inv_smul_mem.mpr ha.2
+    obtain ⟨⟨k, hk⟩, hkax⟩ := hs.exists_smul_eq ⟨g⁻¹ • a, hg'a⟩ ⟨g⁻¹ • x, hg'x⟩
+    use ⟨g * k * g⁻¹, ?_⟩
     · simp only [← SetLike.coe_eq_coe] at hkax ⊢
-      exact hkax
-  · suffices hg'x : g⁻¹ • x ∈ ofFixingSubgroup M s by
-      suffices hg'a : g⁻¹ • a ∈ ofFixingSubgroup M s by
-        obtain ⟨⟨k, hk⟩, hkax⟩ := hs.exists_smul_eq ⟨g⁻¹ • a, hg'a⟩ ⟨g⁻¹ • x, hg'x⟩
-        use ⟨g * k * g⁻¹, (by
-          rw [mem_fixingSubgroup_iff] at hk ⊢
-          intro y hy
-          simp [← smul_smul, smul_eq_iff_eq_inv_smul g]
-          apply hk
-          rw [← Set.mem_smul_set_iff_inv_smul_mem]
-          exact Set.mem_of_mem_inter_right hy)⟩
-        · simp only [← SetLike.coe_eq_coe] at hkax ⊢
-          simp only [SetLike.val_smul] at hkax ⊢
-          rw [← smul_eq_iff_eq_inv_smul] at hkax
-          change (g * k * g⁻¹) • a = x
-          simp only [← smul_smul]
-          exact hkax
-      rw [mem_ofFixingSubgroup_iff]
-      rw [← Set.mem_smul_set_iff_inv_smul_mem]
-      intro h
-      apply ha
-      exact Set.mem_union_right _ h
-    rw [mem_ofFixingSubgroup_iff]
-    intro h
-    apply hx
-    rwa [Set.mem_smul_set_iff_inv_smul_mem]
+      rwa [SetLike.val_smul, Subgroup.mk_smul, eq_inv_smul_iff, smul_smul, smul_smul] at hkax
+    · rw [mem_fixingSubgroup_iff] at hk ⊢
+      intro y hy
+      rw [mul_smul, mul_smul, smul_eq_iff_eq_inv_smul g]
+      exact hk _ (Set.mem_smul_set_iff_inv_smul_mem.mp hy.2)
 
 theorem _root_.Set.ncard_range_of_injective {α β : Type*} {f : α → β} (hf : Function.Injective f) :
     (Set.range f).ncard = Nat.card α := by
