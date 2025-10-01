0b57ad43e3cf18b3f0b1010a5d44d44881360c9d
Stefan Kebekus <kebekus@users.noreply.github.com>
2025-10-01 08:42:20 +0200

    Update FirstMainTheorem.lean

--- Mathlib/Analysis/Complex/ValueDistribution/FirstMainTheorem.lean
+++ Mathlib/Analysis/Complex/ValueDistribution/FirstMainTheorem.lean
@@ -3,6 +3,7 @@ Copyright (c) 2025 Stefan Kebekus. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Stefan Kebekus
 -/
+import Mathlib.Analysis.Complex.JensenFormula
 import Mathlib.Analysis.Complex.ValueDistribution.CharacteristicFunction
 
 /-!
@@ -22,19 +23,78 @@ of the characteristic function `characteristic f ⊤` under modifications of `f`
 See Section~VI.2 of [Lang, *Introduction to Complex Hyperbolic Spaces*][MR886677] or Section~1.1 of
 [Noguchi-Winkelmann, *Nevanlinna Theory in Several Complex Variables and Diophantine
 Approximation*][MR3156076] for a detailed discussion.
-
-### TODO
-
-- Formalize the first part of the First Main Theorem, which is the more substantial part of the
-  statement.
 -/
 namespace ValueDistribution
 
+open Asymptotics Filter Function.locallyFinsuppWithin MeromorphicAt MeromorphicOn Metric Real
+
+section FirstPart
+
+variable {f : ℂ → ℂ} {R : ℝ}
+
+/-!
+## First Part of the First Main Theorem
+-/
+
+/--
+Helper lemma for the first part of the First Main Theorem: Given a meromorphic function `f`, compute
+difference between the characteristic functions of `f` and of its inverse.
+-/
+lemma characteristic_sub_characteristic_inv (h : MeromorphicOn f ⊤) :
+    characteristic f ⊤ - characteristic f⁻¹ ⊤ =
+      circleAverage (log ‖f ·‖) 0 - (divisor f Set.univ).logCounting := by
+  calc characteristic f ⊤ - characteristic f⁻¹ ⊤
+  _ = proximity f ⊤ - proximity f⁻¹ ⊤ - (logCounting f⁻¹ ⊤ - logCounting f ⊤) := by
+    unfold characteristic
+    ring
+  _ = circleAverage (log ‖f ·‖) 0 - (logCounting f⁻¹ ⊤ - logCounting f ⊤) := by
+    rw [proximity_sub_proximity_inv_eq_circleAverage h]
+  _ = circleAverage (log ‖f ·‖) 0 - (logCounting f 0 - logCounting f ⊤) := by
+    rw [logCounting_inv]
+  _ = circleAverage (log ‖f ·‖) 0 - (divisor f Set.univ).logCounting := by
+    rw [← ValueDistribution.log_counting_zero_sub_logCounting_top]
+
+/--
+First part of the First Main Theorem: Away from zero, the difference between the characteristic
+functions of `f` and `f⁻¹` equals `log ‖leadCoefficient f 0‖`.
+-/
+@[simp]
+theorem characteristic_sub_characteristic_inv_at_zero (hf : MeromorphicOn f ⊤) (hR : R ≠ 0) :
+    characteristic f ⊤ R - characteristic f⁻¹ ⊤ R = log ‖meromorphicTrailingCoeffAt f 0‖ := by
+  calc characteristic f ⊤ R - characteristic f⁻¹ ⊤ R
+  _ = (characteristic f ⊤ - characteristic f⁻¹ ⊤) R  := by simp
+  _ = circleAverage (log ‖f ·‖) 0 R - (divisor f Set.univ).logCounting R := by
+    rw [characteristic_sub_characteristic_inv hf]
+    rfl
+  _ = log ‖meromorphicTrailingCoeffAt f 0‖ := by
+    rw [MeromorphicOn.circleAverage_log_norm hR (hf.mono_set (by tauto))]
+    unfold Function.locallyFinsuppWithin.logCounting
+    have : (divisor f (closedBall 0 |R|)) = (divisor f Set.univ).toClosedBall R :=
+      (divisor_restrict hf (by tauto)).symm
+    simp [this, toClosedBall, restrictMonoidHom, restrict_apply]
+
+/--
+Complement to the first part of the First Main Theorem: At 0, the difference between the
+characteristic functions of `f`  and `f⁻¹` equals `log ‖f 0‖`.
+-/
+@[simp]
+theorem characteristic_sub_characteristic_inv_off_zero (h : MeromorphicOn f ⊤) :
+    characteristic f ⊤ 0 - characteristic f⁻¹ ⊤ 0 = log ‖f 0‖ := by
+  calc characteristic f ⊤ 0 - characteristic f⁻¹ ⊤ 0
+  _ = (characteristic f ⊤ - characteristic f⁻¹ ⊤) 0 := by simp
+  _ = circleAverage (log ‖f ·‖) 0 0 - (divisor f Set.univ).logCounting 0 := by
+    rw [ValueDistribution.characteristic_sub_characteristic_inv h]
+    rfl
+  _ = log ‖f 0‖ := by
+    simp
+
+end FirstPart
+
+section SecondPart
+
 variable
   {E : Type*} [NormedAddCommGroup E] [NormedSpace ℂ E]
-  {f : ℂ → E} {a₀ : E}
-
-open Asymptotics Filter Real
+  {a₀ : E} {f : ℂ → E}
 
 /-!
 ## Second Part of the First Main Theorem
@@ -78,5 +138,7 @@ theorem abs_characteristic_sub_characteristic_shift_eqO (h : MeromorphicOn f ⊤
   simp_rw [isBigO_iff', eventually_atTop]
   use log⁺ ‖a₀‖ + log 2, add_pos_of_nonneg_of_pos posLog_nonneg (log_pos one_lt_two), 0
   simp [abs_characteristic_sub_characteristic_shift_le h]
+
+end SecondPart
 
 end ValueDistribution
