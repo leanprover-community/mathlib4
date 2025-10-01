d9fd09175301903b0c4c7e430343ed210ee3de94
Junyan Xu <junyanxu.math@gmail.com>
2025-10-01 18:12:45 +0000

    proper fix

--- Mathlib/GroupTheory/Perm/Centralizer.lean
+++ Mathlib/GroupTheory/Perm/Centralizer.lean
@@ -470,10 +470,10 @@ theorem nat_card_range_toPermHom :
     simp only [Finset.mem_image, Finset.mem_attach,
         true_and, Subtype.exists, exists_prop, Multiset.mem_toFinset]
     simp only [cycleType_def, Function.comp_apply, Multiset.mem_map, Finset.mem_val]
-  simp only [← SetLike.coe_sort_coe, Fintype.card_eq_nat_card]
+  simp only [Fintype.card_eq_nat_card]
   congr
   ext
-  rw [SetLike.mem_coe, mem_range_toPermHom_iff', Set.mem_setOf_eq]
+  rw [mem_range_toPermHom_iff', Set.mem_setOf_eq]
 
 section Kernel
 /- Here, we describe the kernel of `g.OnCycleFactors.toPermHom` -/
