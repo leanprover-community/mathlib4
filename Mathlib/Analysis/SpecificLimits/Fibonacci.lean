9267872a0cf14cc9205f128d30979512d21129f2
Snir Broshi <26556598+NotWearingPants@users.noreply.github.com>
2025-10-02 04:09:49 +0300

    chore(Analysis/SpecificLimits/Fibonacci): cleanup proofs

--- Mathlib/Analysis/SpecificLimits/Fibonacci.lean
+++ Mathlib/Analysis/SpecificLimits/Fibonacci.lean
@@ -12,29 +12,26 @@ import Mathlib.Data.Real.GoldenRatio
 We prove that the ratio of consecutive Fibonacci numbers tends to the golden ratio.
 -/
 
+open Nat Real Filter goldenRatio
 
-open goldenRatio
-
-/-- The limit of dividing consecutive Fibonacci numbers is the golden ratio. -/
+/-- The limit of `fib (n + 1) / fib n` as `n → ∞` is the golden ratio. -/
 theorem tendsto_fib_succ_div_fib_atTop :
-    Filter.Tendsto (fun n ↦ (Nat.fib (n + 1) / Nat.fib n : ℝ)) Filter.atTop <| nhds φ := by
-  have h n (hn : n ≥ 1) : (Nat.fib n : ℝ)⁻¹ * ψ ^ n + φ = Nat.fib (n + 1) / Nat.fib n := by
-    rw [← Real.fib_succ_sub_goldenRatio_mul_fib, mul_sub]
-    nth_rw 2 [mul_comm]
-    rw [mul_assoc, Field.mul_inv_cancel, mul_one, sub_add_cancel, inv_mul_eq_div]
-    simp [Nat.fib_eq_zero]
-    bound
-  refine Filter.Tendsto.congr' (Filter.eventually_atTop.mpr ⟨_, h⟩) ?_
-  have : ∀ᶠ n in Filter.atTop, |(Nat.fib n : ℝ)⁻¹| ≤ 1 :=
-    Filter.Eventually.of_forall fun n ↦ by simp [Nat.cast_inv_le_one]
-  convert Filter.Tendsto.add_const _ <| bdd_le_mul_tendsto_zero' _ this <|
-    tendsto_pow_atTop_nhds_zero_of_abs_lt_one <|
-      abs_lt.mpr ⟨Real.neg_one_lt_goldenConj, by linarith [Real.goldenConj_neg]⟩
+    Tendsto (fun n ↦ (fib (n + 1) / fib n : ℝ)) atTop <| nhds φ := by
+  have h₁ n (hn : n ≥ 1) : (fib n : ℝ)⁻¹ * ψ ^ n + φ = fib (n + 1) / fib n := by
+    rw [← fib_succ_sub_goldenRatio_mul_fib, mul_comm, sub_mul, mul_assoc, Field.mul_inv_cancel,
+      mul_one, sub_add_cancel, mul_comm, inv_mul_eq_div]
+    simp only [ne_eq, cast_eq_zero, fib_eq_zero]
+    cutsat
+  have h₂ : ∀ᶠ n in atTop, |(fib n : ℝ)⁻¹| ≤ 1 :=
+    Eventually.of_forall fun n ↦ by simp [cast_inv_le_one]
+  convert Tendsto.congr' (eventually_atTop.mpr ⟨_, h₁⟩) <| Tendsto.add_const _ <|
+    bdd_le_mul_tendsto_zero' _ h₂ <| tendsto_pow_atTop_nhds_zero_of_abs_lt_one <|
+      abs_lt.mpr ⟨neg_one_lt_goldenConj, by linarith [goldenConj_neg]⟩
   simp
 
-/-- The limit of dividing consecutive Fibonacci numbers in reverse order
-is the negative conjugate of the golden ratio. -/
+/-- The limit of `fib n / fib (n + 1)` as `n → ∞` is the negative conjugate of the golden ratio. -/
 theorem tendsto_fib_div_fib_succ_atTop :
-    Filter.Tendsto (fun n ↦ (Nat.fib n / Nat.fib (n + 1) : ℝ)) Filter.atTop <| nhds (-ψ) := by
-  rw [← Real.inv_goldenRatio, ← funext <| fun n ↦ inv_div ..]
-  exact Filter.Tendsto.inv₀ tendsto_fib_succ_div_fib_atTop Real.goldenRatio_ne_zero
+    Tendsto (fun n ↦ (fib n / fib (n + 1) : ℝ)) atTop <| nhds <| -ψ := by
+  convert tendsto_fib_succ_div_fib_atTop.inv₀ (by positivity) using 2
+  · rw [inv_div]
+  · rw [inv_goldenRatio]
