9bd42bc642d28ad839d1dbf76748c54750641d0d
Salvatore Mercuri <salvatore.mercuri@imperial.ac.uk>
2025-10-03 16:47:04 +0100

    Golf proofs

--- Mathlib/NumberTheory/Padics/HeightOneSpectrum.lean
+++ Mathlib/NumberTheory/Padics/HeightOneSpectrum.lean
@@ -44,179 +44,110 @@ def Rat.ringOfIntegersSpectrumEquiv : HeightOneSpectrum (𝓞 ℚ) ≃ HeightOne
     HeightOneSpectrum.mapEquiv ringOfIntegersEquiv
 
 noncomputable
-def IsDedekindDomain.HeightOneSpectrum.toNatGenerator (𝔭 : HeightOneSpectrum ℤ) : ℕ :=
-    (Submodule.IsPrincipal.generator 𝔭.asIdeal).natAbs
+def IsDedekindDomain.HeightOneSpectrum.toNatGenerator {R : Type*} [CommRing R] {F : Type*}
+    [EquivLike F R ℤ] [RingEquivClass F R ℤ] (f : F) (v : HeightOneSpectrum R) : ℕ :=
+  Submodule.IsPrincipal.generator (v.asIdeal.map f) |>.natAbs
 
 namespace IsDedekindDomain.HeightOneSpectrum
 
-theorem toNatGenerator_span {𝔭 : HeightOneSpectrum ℤ} :
-    𝔭.asIdeal = Ideal.span {↑𝔭.toNatGenerator} := by
+theorem toNatGenerator_span {R : Type*} [CommRing R] {F : Type*}
+    [EquivLike F R ℤ] [RingEquivClass F R ℤ] (f : F) (v : HeightOneSpectrum R) :
+    v.asIdeal.map f = Ideal.span {↑(v.toNatGenerator f)} := by
   simp [toNatGenerator]
 
-theorem toNatGenerator_dvd_iff {𝔭 : HeightOneSpectrum ℤ} {n : ℕ} :
-    𝔭.toNatGenerator ∣ n ↔ ↑n ∈ 𝔭.asIdeal := by
+theorem toNatGenerator_dvd_iff {R : Type*} [CommRing R] {F : Type*}
+    [EquivLike F R ℤ] [RingEquivClass F R ℤ] (f : F) (v : HeightOneSpectrum R) {n : ℕ} :
+    v.toNatGenerator f ∣ n ↔ ↑n ∈ v.asIdeal.map f := by
   rw [toNatGenerator_span, Ideal.mem_span_singleton]
   exact Int.ofNat_dvd.symm
 
-instance (𝔭 : HeightOneSpectrum ℤ) : Fact 𝔭.toNatGenerator.Prime :=
-  ⟨Int.prime_iff_natAbs_prime.1 <| Submodule.IsPrincipal.prime_generator_of_isPrime _ 𝔭.ne_bot⟩
+instance {R : Type*} [CommRing R] {F : Type*} [EquivLike F R ℤ] [RingEquivClass F R ℤ] (f : F)
+    (v : HeightOneSpectrum R) : Fact (v.toNatGenerator f).Prime :=
+  ⟨Int.prime_iff_natAbs_prime.1 <| Submodule.IsPrincipal.prime_generator_of_isPrime _
+    ((Ideal.map_eq_bot_iff_of_injective (EquivLike.injective f)).not.2 v.ne_bot)⟩
 
-noncomputable def toRatpadicValuation {R : Type*} [CommRing R] (h : R ≃+* ℤ)
-    (𝔭 : HeightOneSpectrum R) : Valuation ℚ ℤᵐ⁰ :=
-  Rat.padicValuation (HeightOneSpectrum.mapEquiv h 𝔭).toNatGenerator
+noncomputable def toRatpadicValuation {R : Type*} [CommRing R] {F : Type*}
+    [EquivLike F R ℤ] [RingEquivClass F R ℤ] (f : F)
+    (v : HeightOneSpectrum R) : Valuation ℚ ℤᵐ⁰ :=
+  Rat.padicValuation (v.toNatGenerator f)
 
-@[simp]
-theorem toRatpadicValuation_rfl {𝔭 : HeightOneSpectrum ℤ} :
-    𝔭.toRatpadicValuation (RingEquiv.refl ℤ) = Rat.padicValuation 𝔭.toNatGenerator := by
-  simp [toRatpadicValuation, HeightOneSpectrum.mapEquiv, toNatGenerator, Ideal.map]
-
-theorem _root_.Rat.surjective_padicValuation (p : ℕ) [Fact (p.Prime)] :
+theorem _root_.Rat.surjective_padicValuation (p : ℕ) [hp : Fact (p.Prime)] :
     Function.Surjective (Rat.padicValuation p) := by
   intro x
   induction x with
   | zero => simp
   | coe x =>
-    simp [Rat.padicValuation, -WithZero.exp_neg]
     induction x with | ofAdd x
-    simp [WithZero.exp, -ofAdd_neg]
-    by_cases hx : 0 ≤ x
+    rcases le_or_gt 0 x with (hx | hx) <;> simp only [Rat.padicValuation, WithZero.exp]
     · use (p ^ x.natAbs)⁻¹
-      rcases eq_or_ne x 0 with (h | h)
-      · simp [h]
-      · have : ((p : ℚ) ^ x.natAbs)⁻¹  ≠ 0 := by
-          apply inv_ne_zero
-          apply pow_ne_zero
-          simp
-          exact (Fact.out : p.Prime).ne_zero
-        simp [this, hx]
+      simp [hp.out.ne_zero, padicValRat.pow, hx]
     · use p ^ x.natAbs
-      rcases eq_or_ne x 0 with (h | h)
-      · simp [h]
-      · have : ((p : ℚ) ^ x.natAbs) ≠ 0 := by
-          apply pow_ne_zero
-          simpa using (Fact.out : p.Prime).ne_zero
-        simp [this, padicValRat.pow (show (p : ℚ) ≠ 0 by simp [(Fact.out : p.Prime).ne_zero])]
-        simp at hx
-        have : |x| = -x := by
-          simp
-          linarith
-        simp [this]
-
-theorem valuation_toNatGenerator (𝔭 : HeightOneSpectrum ℤ) :
-    𝔭.valuation ℚ 𝔭.toNatGenerator = .exp (-1) := by
-  erw [valuation_of_algebraMap]
-  apply IsDedekindDomain.HeightOneSpectrum.intValuation_singleton
-  · simpa [toNatGenerator] using (Submodule.IsPrincipal.eq_bot_iff_generator_eq_zero _).not.1
-      𝔭.ne_bot
-  · simp [toNatGenerator]
+      simp [hp.out.ne_zero, padicValRat.pow, abs_eq_neg_self.2 (le_of_lt hx)]
 
 theorem intValuation_eq_one_iff {R : Type*} [CommRing R] [IsDedekindDomain R]
-    (v : HeightOneSpectrum R) (x : R) : v.intValuation x = 1 ↔ x ∉ v.asIdeal := by
+    {v : HeightOneSpectrum R} {x : R} : v.intValuation x = 1 ↔ x ∉ v.asIdeal := by
   refine ⟨fun h ↦ by simp [← (intValuation_lt_one_iff_mem _ _).not, h], fun h ↦ ?_⟩
   exact le_antisymm (v.intValuation_le_one x) <| by
     simp [← not_lt, (v.intValuation_lt_one_iff_mem _).not, h]
 
-theorem valuation_le_one_iff {R : Type*} [CommRing R] [IsDedekindDomain R] {K : Type*} [Field K]
-    [Algebra R K] [IsFractionRing R K] (v : HeightOneSpectrum R) (a : R) (b : nonZeroDivisors R)
-    (h : b.1 ∈ v.asIdeal → a ∉ v.asIdeal) :
-    v.valuation K (IsLocalization.mk' K a b) ≤ 1 ↔ ↑b ∉ v.asIdeal := by
-  constructor
-  · intro hv
-    simp at hv
-    contrapose! hv
-    have : a ∉ v.asIdeal := h hv
-    rw [valuation_of_algebraMap, valuation_of_algebraMap]
-    rw [← intValuation_lt_one_iff_mem] at hv
-    rw [← intValuation_eq_one_iff] at this
-    rw [← WithZero.log_lt_log (by norm_num)]
-    · rw [WithZero.log_div (by simp_all)]
-      · simp [this]
-        rw [← WithZero.log_lt_log (intValuation_ne_zero _ _ (by simp)) (by norm_num)] at hv
-        simpa
-      · apply intValuation_ne_zero
-        simp
-    · simp
-      refine ⟨intValuation_ne_zero _ _ (fun _ ↦ by simp_all), intValuation_ne_zero _ _ (by simp)⟩
-  · intro hb
-    simp [valuation_of_algebraMap]
-    rw [← intValuation_eq_one_iff] at hb
-    simp [hb, intValuation_le_one]
+open scoped algebraMap in
+theorem valuation_le_one_iff {R : Type*} [CommRing R] [IsDedekindDomain R] (K : Type*) [Field K]
+    [Algebra R K] [IsFractionRing R K] (v : HeightOneSpectrum R) (a : R) {b : R} (hb : b ≠ 0)
+    (h : b ∈ v.asIdeal → a ∉ v.asIdeal) :
+    v.valuation K (a / b) ≤ 1 ↔ b ∉ v.asIdeal := by
+  refine ⟨fun hv ↦ ?_, fun hb ↦ by
+    simp [valuation_of_algebraMap, intValuation_eq_one_iff.2 hb, intValuation_le_one]⟩
+  contrapose! hv
+  have ha₀ : a ≠ 0 := fun _ ↦ by simp_all
+  have hva : v.valuation K a ≠ 0 := (Valuation.ne_zero_iff _).2 (by simp [ha₀])
+  have hvb : v.valuation K b ≠ 0 := (Valuation.ne_zero_iff _).2 (by simp [hb])
+  rw [← WithZero.log_lt_log one_ne_zero ((Valuation.ne_zero_iff _).2 (by simp [ha₀, hb])),
+    map_div₀, WithZero.log_div hva hvb, WithZero.log_one, Int.sub_pos,
+    WithZero.log_lt_log hvb hva]
+  simpa [valuation_of_algebraMap, intValuation_eq_one_iff.2 <| h hv, intValuation_lt_one_iff_mem]
 
 theorem _root_.Rat.num_not_mem_ideal_of_den_mem {R : Type*} [CommRing R] [IsDedekindDomain R]
     [Algebra R ℚ] [IsFractionRing R ℚ] (f : R ≃+* ℤ) {𝔭 : Ideal R} (hp : Prime 𝔭) (x : ℚ) :
     ↑x.den ∈ 𝔭 → ↑x.num ∉ 𝔭 := by
-  have : Submodule.IsPrincipal (Ideal.map f 𝔭) := by
-    exact IsPrincipalIdealRing.principal (Ideal.map f 𝔭)
-  obtain ⟨p, hp'⟩ := Submodule.IsPrincipal.map_ringHom f.symm this
-  simp [Ideal.comap_map_of_bijective _ f.bijective] at hp'
-  rw [hp']
-  simp [Ideal.mem_span_singleton]
+  obtain ⟨p, h𝔭⟩ := IsPrincipalIdealRing.principal (Ideal.map f 𝔭) |>.map_ringHom f.symm
+  rw [Ideal.map_symm, Ideal.comap_map_of_bijective _ f.bijective, Ideal.submodule_span_eq] at h𝔭
+  simp_rw [h𝔭, Ideal.mem_span_singleton]
   intro hden
-  have := x.reduced
-  haveI : IsPrincipalIdealRing R := IsPrincipalIdealRing.of_surjective _ f.symm.surjective
-  have := Nat.Coprime.cast (R := R) x.reduced
-  rw [← isRelPrime_iff_isCoprime] at this
-  contrapose this
-  rw [IsRelPrime]
-  simp
-  use p
-  refine ⟨?_, hden, ?_⟩
-  · simp_all
-    by_cases h₀ : 0 ≤ x.num
-    · rwa [abs_eq_self.2 h₀]
-    · rw [abs_eq_neg_self.2 (le_of_lt (not_le.1 h₀))]
-      simpa
-  · rw [hp'] at hp
-    simp at hp
-    exact Prime.not_unit hp
+  have : IsPrincipalIdealRing R := IsPrincipalIdealRing.of_surjective _ f.symm.surjective
+  have := isRelPrime_iff_isCoprime.2 <| Nat.Coprime.cast (R := R) x.reduced
+  contrapose! this
+  simp only [IsRelPrime, not_forall]
+  refine ⟨p, ?_, hden, (Ideal.prime_span_singleton_iff.1 <| h𝔭 ▸ hp).not_unit⟩
+  rcases lt_or_ge x.num 0 with (h₀ | h₀)
+  · simpa [abs_eq_neg_self.2 (le_of_lt h₀)]
+  · simpa [abs_eq_self.2 h₀]
 
-theorem _root_.Rat.valuation_le_one_iff {R : Type*} [CommRing R] [IsDedekindDomain R]
+theorem _root_.Rat.valuation_le_one_iff_den {R : Type*} [CommRing R] [IsDedekindDomain R]
     [Algebra R ℚ] [IsFractionRing R ℚ] (f : R ≃+* ℤ) (𝔭 : HeightOneSpectrum R) (x : ℚ) :
     𝔭.valuation ℚ x ≤ 1 ↔ ↑x.den ∉ 𝔭.asIdeal := by
-  have : (x.den : R) ≠ 0 := by
-    apply_fun f
-    simp
-  have := 𝔭.valuation_le_one_iff (K := ℚ) x.num ⟨x.den, by simp [this]⟩
-  simp at this
-  rw [← this]
-  · nth_rw 1 [← x.num_div_den]
-    simp
-  · exact x.num_not_mem_ideal_of_den_mem f 𝔭.prime
+  have : (x.den : R) ≠ 0 := fun h ↦ by simpa using congrArg f h
+  simp [← 𝔭.valuation_le_one_iff ℚ x.num this (x.num_not_mem_ideal_of_den_mem f 𝔭.prime),
+    x.num_div_den]
 
 theorem valuation_equiv_toRatpadicValuation {R : Type*} [CommRing R] [IsDedekindDomain R]
-    [Algebra R ℚ] [IsFractionRing R ℚ] (h : R ≃+* ℤ)
+    [Algebra R ℚ] [IsFractionRing R ℚ] (f : R ≃+* ℤ)
     (𝔭 : HeightOneSpectrum R) :
-    (𝔭.valuation ℚ).IsEquiv (𝔭.toRatpadicValuation h) := by
-  simp [Valuation.isEquiv_iff_val_le_one, toRatpadicValuation, Rat.padicValuation_le_one_iff,
-    Rat.valuation_le_one_iff h, toNatGenerator_dvd_iff, HeightOneSpectrum.mapEquiv]
-  intro x
-  rw [← Ideal.apply_mem_of_equiv_iff (f := h.symm) (x := (x.den : ℤ))]
-  rw [Ideal.map_symm, Ideal.comap_map_of_bijective _ h.bijective]
-  simp
-
-variable {R : Type*} [CommRing R] [IsDedekindDomain R]
-    [Algebra R ℚ] [IsFractionRing R ℚ] [IsIntegralClosure R ℤ ℚ] (v : Ideal ℤ)
-
-instance : IsPrincipalIdealRing (𝓞 ℚ) :=
-  IsPrincipalIdealRing.of_surjective _ Rat.ringOfIntegersEquiv.symm.surjective
+    (𝔭.valuation ℚ).IsEquiv (𝔭.toRatpadicValuation f) := by
+  simp [Valuation.isEquiv_iff_val_le_one,  Rat.padicValuation_le_one_iff, Ideal.map_symm,
+    Rat.valuation_le_one_iff_den f, toNatGenerator_dvd_iff, toRatpadicValuation,
+    ← Ideal.apply_mem_of_equiv_iff (f := f.symm),  Ideal.comap_map_of_bijective _ f.bijective]
 
 noncomputable def withValEquiv {R : Type*} [CommRing R] [IsDedekindDomain R]
-    [Algebra R ℚ] [IsFractionRing R ℚ] (h : R ≃+* ℤ) (𝔭 : HeightOneSpectrum R) :
-    WithVal (𝔭.valuation ℚ) ≃ᵤ WithVal (𝔭.toRatpadicValuation h) :=
+    [Algebra R ℚ] [IsFractionRing R ℚ] (f : R ≃+* ℤ) (𝔭 : HeightOneSpectrum R) :
+    WithVal (𝔭.valuation ℚ) ≃ᵤ WithVal (𝔭.toRatpadicValuation f) :=
   Valuation.IsEquiv.uniformEquiv (𝔭.valuation_surjective ℚ) (Rat.surjective_padicValuation _)
-    (𝔭.valuation_equiv_toRatpadicValuation h)
+    (𝔭.valuation_equiv_toRatpadicValuation f)
 
 noncomputable
-def adicCompletionRatEquivInt (𝔭 : HeightOneSpectrum ℤ) :
-    𝔭.adicCompletion ℚ ≃ᵤ ℚ_[𝔭.toNatGenerator] := by
-  apply UniformSpace.Completion.mapEquiv (𝔭.withValEquiv (RingEquiv.refl _)) |>.trans
-  exact toRatpadicValuation_rfl ▸ Padic.withValUniformEquiv
-
-noncomputable
-def padicCompletionRatEquiv (v : HeightOneSpectrum (𝓞 ℚ)) :
-    let p : ℕ := (HeightOneSpectrum.mapEquiv Rat.ringOfIntegersEquiv v).toNatGenerator
-    v.adicCompletion ℚ ≃ᵤ ℚ_[p] :=
-  UniformSpace.Completion.mapEquiv (v.withValEquiv Rat.ringOfIntegersEquiv) |>.trans
-    Padic.withValUniformEquiv
+def _root_.Rat.adicCompletionEquivPadicCompletion {R : Type*} [CommRing R] [IsDedekindDomain R]
+    [Algebra R ℚ] [IsFractionRing R ℚ] {F : Type*} [EquivLike F R ℤ] [RingEquivClass F R ℤ]
+    (f : F) (𝔭 : HeightOneSpectrum R) :
+    𝔭.adicCompletion ℚ ≃ᵤ ℚ_[𝔭.toNatGenerator f] := by
+  apply UniformSpace.Completion.mapEquiv (𝔭.withValEquiv f) |>.trans Padic.withValUniformEquiv
 
 end IsDedekindDomain.HeightOneSpectrum
