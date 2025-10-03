2c5168c37a8b5b3c58a024cb45273a83ce75a577
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2025-10-03 10:32:24 +0800

    fix

--- Mathlib/RingTheory/RegularLocalRing/RegularRing/Polynomial.lean
+++ Mathlib/RingTheory/RegularLocalRing/RegularRing/Polynomial.lean
@@ -3,6 +3,7 @@ Copyright (c) 2025 Nailin Guan. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Nailin Guan
 -/
+import Mathlib.Algebra.Polynomial.FieldDivision
 import Mathlib.RingTheory.RegularLocalRing.RegularRing.Basic
 /-!
 
