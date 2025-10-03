11341f178f61e84ea0b3718ed3fb48ddc5a621ee
Rémy Degenne <4094732+RemyDegenne@users.noreply.github.com>
2025-10-03 10:42:17 +0000

    feat(Probability): Fernique's theorem for distributions that are invariant by rotation (#28343)
    Let `μ` be a finite measure on a second-countable normed space `E` such that the product measure `μ.prod μ` on `E × E` is invariant by rotation of angle `-π/4`.
    Then there exists a constant `C > 0` such that the function `x ↦ exp (C * ‖x‖ ^ 2)` is integrable with respect to `μ`.
    
    The main application of this theorem will be for Gaussian measures.
    
    Co-authored-by: Remy Degenne <remydegenne@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -5272,6 +5272,7 @@ import Mathlib.Probability.ConditionalProbability
 import Mathlib.Probability.Decision.Risk.Defs
 import Mathlib.Probability.Density
 import Mathlib.Probability.Distributions.Exponential
+import Mathlib.Probability.Distributions.Fernique
 import Mathlib.Probability.Distributions.Gamma
 import Mathlib.Probability.Distributions.Gaussian
 import Mathlib.Probability.Distributions.Gaussian.Basic
