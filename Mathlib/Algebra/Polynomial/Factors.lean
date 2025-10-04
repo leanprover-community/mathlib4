dcd8f770a42e17f2e7c17082c4faa4349e181caa
tb65536 <thomas.l.browning@gmail.com>
2025-10-04 15:10:32 -0700

    unused import

--- Mathlib/Algebra/Polynomial/Factors.lean
+++ Mathlib/Algebra/Polynomial/Factors.lean
@@ -3,7 +3,6 @@ Copyright (c) 2025 Thomas Browning. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Thomas Browning
 -/
-import Mathlib.Algebra.EuclideanDomain.Field
 import Mathlib.Algebra.Polynomial.Roots
 
 /-!
