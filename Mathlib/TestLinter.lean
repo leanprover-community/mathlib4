e9db9c1c97e925854a49d6cd3f64f490f3cbcc0c
Kenny Lau <kc_kennylau@yahoo.com.hk>
2025-10-04 17:15:46 +0100

    test linter

---
+++ Mathlib/TestLinter.lean
@@ -0,0 +1,12 @@
+/-
+Copyright (c) 2025 Nobody. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Nobody
+-/
+import Mathlib.Init
+
+/-! # A file to test linter, ignore it. -/
+
+set_option linter.unusedVariables false in
+/-- lorem ipsum -/
+def foo1234 (m n : Nat) : Nat := m
