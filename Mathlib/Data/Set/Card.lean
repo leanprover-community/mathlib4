c2a36910f86dc522a6386509bc5f4d149ae53209
Vlad Tsyrklevich <vlad@tsyrklevi.ch>
2025-10-05 00:05:42 +0200

    match Antoine's ncard_le_card

--- Mathlib/Data/Set/Card.lean
+++ Mathlib/Data/Set/Card.lean
@@ -180,7 +180,7 @@ section Lattice
 theorem encard_le_encard (h : s ⊆ t) : s.encard ≤ t.encard := by
   rw [← union_diff_cancel h, encard_union_eq disjoint_sdiff_right]; exact le_self_add
 
-@[simp] theorem encard_le_card : s.encard ≤ ENat.card α :=
+theorem encard_le_card : s.encard ≤ ENat.card α :=
   encard_univ _ ▸ encard_le_encard s.subset_univ
 
 theorem encard_mono {α : Type*} : Monotone (encard : Set α → ℕ∞) :=
