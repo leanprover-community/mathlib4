5ba8f959a8a188c337f5a845610ce7f3f8ad50c5
Michael Rothgang <10105016+grunweg@users.noreply.github.com>
2025-10-04 07:54:24 +0000

    chore(Lean/Expr/Basic): move material about rewriting to a new file (#30156)
    These methods should go into Lean.Meta.Tactic.Rewrite instead, so move them to a new file. I credit Kim, Kyle and Patrick from #3801 for this; Kim's final clean-up eventually introduced these methods.
    
    With this moved, the imports for Lean.Expr.Basic can be reduced to just Lean.Expr, which we do.
    
    Noticed during work on #26221.

--- Mathlib.lean
+++ Mathlib.lean
@@ -4126,6 +4126,7 @@ import Mathlib.Lean.Meta.RefinedDiscrTree.Encode
 import Mathlib.Lean.Meta.RefinedDiscrTree.Initialize
 import Mathlib.Lean.Meta.RefinedDiscrTree.Lookup
 import Mathlib.Lean.Meta.Simp
+import Mathlib.Lean.Meta.Tactic.Rewrite
 import Mathlib.Lean.Name
 import Mathlib.Lean.PrettyPrinter.Delaborator
 import Mathlib.Lean.Thunk
