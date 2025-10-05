ce18e3a8238e19dd2db349a2a58310f7407c6812
Yakov Pechersky <pechersky@users.noreply.github.com>
2025-10-05 15:12:33 -0400

    lint import

--- Mathlib/Topology/Algebra/Valued/WithVal.lean
+++ Mathlib/Topology/Algebra/Valued/WithVal.lean
@@ -3,7 +3,7 @@ Copyright (c) 2025 Salvatore Mercuri. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Salvatore Mercuri
 -/
-import Mathlib.RingTheory.Valuation.ValuativeRel
+import Mathlib.RingTheory.Valuation.ValuativeRel.Basic
 import Mathlib.Topology.UniformSpace.Completion
 import Mathlib.Topology.Algebra.Valued.ValuationTopology
 import Mathlib.NumberTheory.NumberField.Basic
