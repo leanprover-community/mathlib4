22c07bc7f2440d69ccf551f061d691072314e2a2
pelicanhere <fakegreenall@foxmail.com>
2025-10-05 13:49:44 +0800

    Apply suggestion from @erdOne
    Co-authored-by: Andrew Yang <36414270+erdOne@users.noreply.github.com>

--- Mathlib/RingTheory/Localization/AtPrime/Basic.lean
+++ Mathlib/RingTheory/Localization/AtPrime/Basic.lean
@@ -124,7 +124,7 @@ instance isDomain_of_local_atPrime {P : Ideal A} (_ : P.IsPrime) :
   isDomain_localization P.primeCompl_le_nonZeroDivisors
 
 /-- This is an `IsLocalization.AtPrime` version for `IsLocalization.isDomain_of_local_atPrime`. -/
-theorem isDomain_of_isLocalization_atPrime (S : Type*) [CommSemiring S] [Algebra A S]
+theorem isDomain_of_atPrime (S : Type*) [CommSemiring S] [Algebra A S]
     (P : Ideal A) [P.IsPrime] [IsLocalization.AtPrime S P] : IsDomain S :=
   isDomain_of_le_nonZeroDivisors S P.primeCompl_le_nonZeroDivisors
 
