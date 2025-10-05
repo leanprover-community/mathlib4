2d09ce9cc8bde5ea5dadfa7c8e40bdeeaeded28c
Yaël Dillies <yael.dillies@gmail.com>
2025-10-05 14:22:01 +0000

    chore: rename `mul_le_mul_right'` to `mul_le_mul_left`

--- Mathlib/NumberTheory/Zsqrtd/Basic.lean
+++ Mathlib/NumberTheory/Zsqrtd/Basic.lean
@@ -699,8 +699,8 @@ theorem le_arch (a : ℤ√d) : ∃ n : ℕ, a ≤ n := by
   rw [show (x : ℤ) + d * Nat.succ y - x = d * Nat.succ y by simp]
   exact h (y + 1)
 
-protected theorem add_le_add_left (a b : ℤ√d) (ab : a ≤ b) (c : ℤ√d) : c + a ≤ c + b :=
-  show Nonneg _ by rw [add_sub_add_left_eq_sub]; exact ab
+protected theorem add_le_add_left (a b : ℤ√d) (ab : a ≤ b) (c : ℤ√d) : a + c ≤ b + c :=
+  show Nonneg _ by rwa [add_sub_add_right_eq_sub]
 
 protected theorem le_of_add_le_add_left (a b c : ℤ√d) (h : c + a ≤ c + b) : a ≤ b := by
   simpa using Zsqrtd.add_le_add_left _ _ h (-c)
