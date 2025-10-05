d62a549b8e24ad51f84bed89505b5d45c964d62b
pelicanhere <174301426+pelicanhere@users.noreply.github.com>
2025-10-05 10:19:19 +0000

    feat(RingTheory): local properties of semilocal rings (#29924)
    Proves local properties of semilocal rings, which wiil be used in Iwasawa theory. For a natural language proof, see [lean-iwasawa](https://acmepjz.github.io/lean-iwasawa/blueprint/sect0002.html#a0000000013) (slightly different from this PR).
    > 
    > > 
    > [![Open in Gitpod](https://camo.githubusercontent.com/b04f5659467d23b5109ba935a40c00decd264eea25c22d50a118021349eea94f/68747470733a2f2f676974706f642e696f2f627574746f6e2f6f70656e2d696e2d676974706f642e737667)](https://gitpod.io/from-referrer/)

--- Mathlib/RingTheory/DedekindDomain/PID.lean
+++ Mathlib/RingTheory/DedekindDomain/PID.lean
@@ -169,15 +169,19 @@ theorem Ideal.IsPrincipal.of_finite_maximals_of_isUnit (hf : {I : Ideal R | I.Is
     (FractionalIdeal.isPrincipal.of_finite_maximals_of_inv le_rfl hf I
       (↑hI.unit⁻¹ : FractionalIdeal R⁰ (FractionRing R)) hI.unit.mul_inv)
 
-/-- A Dedekind domain is a PID if its set of primes is finite. -/
-theorem IsPrincipalIdealRing.of_finite_primes [IsDedekindDomain R]
-    (h : {I : Ideal R | I.IsPrime}.Finite) : IsPrincipalIdealRing R :=
+/-- A Dedekind domain is a PID if its set of maximal ideals is finite. -/
+theorem IsPrincipalIdealRing.of_finite_maximals [IsDedekindDomain R]
+    (h : {I : Ideal R | I.IsMaximal}.Finite) : IsPrincipalIdealRing R :=
   ⟨fun I => by
     obtain rfl | hI := eq_or_ne I ⊥
     · exact bot_isPrincipal
-    apply Ideal.IsPrincipal.of_finite_maximals_of_isUnit
-    · apply h.subset; exact @Ideal.IsMaximal.isPrime _ _
-    · exact isUnit_of_mul_eq_one _ _ (FractionalIdeal.coe_ideal_mul_inv I hI)⟩
+    apply Ideal.IsPrincipal.of_finite_maximals_of_isUnit h
+    exact isUnit_of_mul_eq_one _ _ (FractionalIdeal.coe_ideal_mul_inv I hI)⟩
+
+/-- A Dedekind domain is a PID if its set of primes is finite. -/
+theorem IsPrincipalIdealRing.of_finite_primes [IsDedekindDomain R]
+    (h : {I : Ideal R | I.IsPrime}.Finite) : IsPrincipalIdealRing R :=
+  IsPrincipalIdealRing.of_finite_maximals <| h.subset fun _ hi ↦ hi.isPrime
 
 variable [IsDedekindDomain R]
 variable (S : Type*) [CommRing S]
