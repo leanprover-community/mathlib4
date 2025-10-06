db325ee5bf05325810b7e0b100d5d9359d54d5da
Yongshun Ye <ShreckYe@gmail.com>
2025-10-06 10:59:26 +0800

    Fix, adding back a line removed by mistake

--- Mathlib/Data/Nat/Factorization/Basic.lean
+++ Mathlib/Data/Nat/Factorization/Basic.lean
@@ -479,6 +479,7 @@ lemma card_multiples' (N n : ℕ) : #{k ∈ range N.succ | k ≠ 0 ∧ n ∣ k} 
 theorem exists_eq_pow_of_exponent_coprime_of_pow_eq_pow
     {a b m n : ℕ} (ha : a ≠ 0) (hb : b ≠ 0) (hmn : m.Coprime n) (h : a ^ m = b ^ n) :
     ∃ c, a = c ^ n ∧ b = c ^ m := by
+  by_cases hn : n = 0
   · use b
     simp_all
   have factorization_pow_eq := congrArg factorization h
