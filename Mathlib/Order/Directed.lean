3117be9c4d4382ddaea2629acbb35b284d7f57da
Yakov Pechersky <5342086+pechersky@users.noreply.github.com>
2025-10-06 18:32:02 +0000

    feat(Algebra/Subgroup/Lattice): `mem_biSup_of_directedOn` (#29990)
    Generalization of existing `mem_iSup_of_directed`
    
    On the way to characterization of the torsion subgroup as the sup over all subgroups of roots of unity

--- Mathlib/Order/Directed.lean
+++ Mathlib/Order/Directed.lean
@@ -90,6 +90,13 @@ theorem DirectedOn.mono_comp {r : α → α → Prop} {rb : β → β → Prop} 
     (hg : ∀ ⦃x y⦄, r x y → rb (g x) (g y)) (hf : DirectedOn r s) : DirectedOn rb (g '' s) :=
   directedOn_image.mpr (hf.mono hg)
 
+lemma directedOn_onFun_iff {r : α → α → Prop} {f : β → α} {s : Set β} :
+    DirectedOn (r on f) s ↔ DirectedOn r (f '' s) := by
+  refine ⟨DirectedOn.mono_comp (by simp), fun h x hx y hy ↦ ?_⟩
+  obtain ⟨_, ⟨z, hz, rfl⟩, hz'⟩ := h (f x) (Set.mem_image_of_mem f hx) (f y)
+    (Set.mem_image_of_mem f hy)
+  grind
+
 /-- A set stable by supremum is `≤`-directed. -/
 theorem directedOn_of_sup_mem [SemilatticeSup α] {S : Set α}
     (H : ∀ ⦃i j⦄, i ∈ S → j ∈ S → i ⊔ j ∈ S) : DirectedOn (· ≤ ·) S := fun a ha b hb =>
