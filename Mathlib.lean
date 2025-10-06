2a0472dd495de6b5d8de813bbd009a1be54981e2
Etienne Marion <baguettes_casino0c@icloud.com>
2025-10-06 18:49:22 +0200

    mk_all

--- Mathlib.lean
+++ Mathlib.lean
@@ -4395,6 +4395,8 @@ import Mathlib.LinearAlgebra.RootSystem.WeylGroup
 import Mathlib.LinearAlgebra.SModEq
 import Mathlib.LinearAlgebra.Semisimple
 import Mathlib.LinearAlgebra.SesquilinearForm
+import Mathlib.LinearAlgebra.SesquilinearForm.Basic
+import Mathlib.LinearAlgebra.SesquilinearForm.Star
 import Mathlib.LinearAlgebra.Span.Basic
 import Mathlib.LinearAlgebra.Span.Defs
 import Mathlib.LinearAlgebra.StdBasis
