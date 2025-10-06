e09be072c1d832950f47b094ac8d7c361a3186a2
Yongshun Ye <ShreckYe@gmail.com>
2025-10-06 11:02:44 +0800

    Fix an error introduced by the new `letI`

--- Mathlib/Data/Nat/Factorization/Basic.lean
+++ Mathlib/Data/Nat/Factorization/Basic.lean
@@ -525,7 +525,7 @@ theorem exists_eq_pow_of_exponent_coprime_of_pow_eq_pow
 theorem exists_eq_pow_of_pow_eq_pow
     {a b m n : ℕ} (ha : a ≠ 0) (hb : b ≠ 0) (hmn : m ≠ 0 ∨ n ≠ 0) (h : a ^ m = b ^ n) :
     letI g := gcd m n; ∃ c, a = c ^ (n / g) ∧ b = c ^ (m / g) := by
-  intro g
+  set g := gcd m n
   let m' := m / gcd m n
   let n' := n / gcd m n
   have coprime : m'.Coprime n' := by
