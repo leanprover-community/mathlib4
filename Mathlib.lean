edf88c09b09fa891073e44539c1c256b5b4cf381
Andrew Yang <36414270+erdOne@users.noreply.github.com>
2025-10-06 20:40:10 +0000

    feat(AlgebraicGeometry): quasi-affine schemes (#30256)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1273,6 +1273,7 @@ import Mathlib.AlgebraicGeometry.ProjectiveSpectrum.Topology
 import Mathlib.AlgebraicGeometry.Properties
 import Mathlib.AlgebraicGeometry.PullbackCarrier
 import Mathlib.AlgebraicGeometry.Pullbacks
+import Mathlib.AlgebraicGeometry.QuasiAffine
 import Mathlib.AlgebraicGeometry.RationalMap
 import Mathlib.AlgebraicGeometry.ResidueField
 import Mathlib.AlgebraicGeometry.Restrict
