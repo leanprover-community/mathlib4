fe4df3d2f7eb2154fc8c2ce9855bbfc3a7038399
Snir Broshi <26556598+NotWearingPants@users.noreply.github.com>
2025-10-07 04:21:26 +0300

    feat(FieldTheory/IsAlgClosed/Basic): merge `exists_aeval_eq_zero'` into `exists_aeval_eq_zero` to generalize from `Field` to `CommSemiring` + `FaithfulSMul`

--- Mathlib/FieldTheory/IsAlgClosed/Basic.lean
+++ Mathlib/FieldTheory/IsAlgClosed/Basic.lean
@@ -177,11 +177,7 @@ theorem exists_aeval_eq_zero_of_injective {R : Type*} [CommSemiring R] [IsAlgClo
     ∃ x : k, aeval x p = 0 :=
   exists_eval₂_eq_zero_of_injective _ hinj p hp
 
-theorem exists_aeval_eq_zero {R : Type*} [Field R] [IsAlgClosed k] [Algebra R k] (p : R[X])
-    (hp : p.degree ≠ 0) : ∃ x : k, aeval x p = 0 :=
-  exists_eval₂_eq_zero _ p hp
-
-theorem exists_aeval_eq_zero' {R : Type*} [CommSemiring R] [IsAlgClosed k] [Algebra R k]
+theorem exists_aeval_eq_zero {R : Type*} [CommSemiring R] [IsAlgClosed k] [Algebra R k]
     [FaithfulSMul R k] (p : R[X]) (hp : p.degree ≠ 0) : ∃ x : k, aeval x p = 0 :=
   exists_aeval_eq_zero_of_injective _ (FaithfulSMul.algebraMap_injective _ _) p hp
 
