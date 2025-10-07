d0b40d914310a74ddee448c120fa31c3a13ecb64
or4nge19 <matteo.cipollina@yahoo.it>
2025-10-07 17:55:32 +0200

    Update ConnectedComponent.lean

--- Mathlib/Combinatorics/Quiver/ConnectedComponent.lean
+++ Mathlib/Combinatorics/Quiver/ConnectedComponent.lean
@@ -63,7 +63,11 @@ protected theorem eq (a b : V) :
 
 end WeaklyConnectedComponent
 
-variable {V}
+/-- A wide subquiver `H` of `Symmetrify V` determines a wide subquiver of `V`, containing an
+arrow `e` if either `e` or its reversal is in `H`. -/
+def wideSubquiverSymmetrify (H : WideSubquiver (Symmetrify V)) : WideSubquiver V :=
+  fun _ _ ↦ { e | H _ _ (Sum.inl e) ∨ H _ _ (Sum.inr e) }
+
 
 /-!
 ## Strongly connected components (directed connectivity)
