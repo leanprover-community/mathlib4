03da717014c10806db11755f8c2d1949b738a7f0
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-10-07 11:42:13 +0200

    feat(CategoryTheory): the opposite of a property of objects

--- Mathlib/CategoryTheory/ObjectProperty/Small.lean
+++ Mathlib/CategoryTheory/ObjectProperty/Small.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joël Riou
 -/
 import Mathlib.CategoryTheory.ObjectProperty.FullSubcategory
+import Mathlib.CategoryTheory.ObjectProperty.Opposite
 import Mathlib.Logic.Small.Basic
 
 /-!
@@ -32,5 +33,21 @@ instance (P : ObjectProperty C) [ObjectProperty.Small.{w} P] :
 lemma Small.of_le {P Q : ObjectProperty C} [ObjectProperty.Small.{w} Q] (h : P ≤ Q) :
     ObjectProperty.Small.{w} P :=
   small_of_injective (Subtype.map_injective h Function.injective_id)
+
+instance (P : ObjectProperty C) [ObjectProperty.Small.{w} P] :
+    ObjectProperty.Small.{w} P.op :=
+  small_of_injective P.subtypeOpEquiv.injective
+
+instance (P : ObjectProperty Cᵒᵖ) [ObjectProperty.Small.{w} P] :
+    ObjectProperty.Small.{w} P.unop := by
+  simpa only [← small_congr P.unop.subtypeOpEquiv]
+
+instance {ι : Type*} (X : ι → C) [Small.{w} ι] :
+    ObjectProperty.Small.{w} (ofObj X) :=
+  small_of_surjective (f := fun i ↦ ⟨X i, by simp⟩) (by rintro ⟨_, ⟨i⟩⟩;simp )
+
+instance (X Y : C) : ObjectProperty.Small.{w} (.pair X Y) := by
+  dsimp [pair]
+  infer_instance
 
 end CategoryTheory.ObjectProperty
