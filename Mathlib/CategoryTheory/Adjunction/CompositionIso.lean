8ee34f30411d3d40acd61f8f4c5dc6f22b7caf7a
Joël Riou <37772949+joelriou@users.noreply.github.com>
2025-10-08 18:12:17 +0200

    Apply suggestion from @joelriou

--- Mathlib/CategoryTheory/Adjunction/CompositionIso.lean
+++ Mathlib/CategoryTheory/Adjunction/CompositionIso.lean
@@ -12,7 +12,7 @@ In this file, given isomorphisms between compositions of right adjoint functors,
 we obtain isomorphisms between the corresponding compositions of the left adjoint functors,
 and show that the left adjoint functors satisfy properties similar to the left/right
 unitality and the associativity of pseudofunctors if the right adjoint functors
-satisfies the corresponding properties.
+satisfy the corresponding properties.
 
 This shall used be to study the behaviour with respect to composition of
 the pullback functors on presheaves of modules, by reducing these definitions and
