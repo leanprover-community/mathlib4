5c9309319266261503c02883f0c989ac98b31cf9
Izan Beltran Ferreiro <izanbf1803@gmail.com>
2025-10-08 17:33:09 +0200

    Improve implication to iff with calc proof

--- Mathlib/Algebra/Ring/Invertible.lean
+++ Mathlib/Algebra/Ring/Invertible.lean
@@ -96,21 +96,22 @@ lemma neg_add_eq_mul_invOf_mul_same_iff [Ring R] {a b : R} [Invertible a] [Inver
     _ ↔ -a = a * (⅟a * b + ⅟b * a) := by simp only [mul_add, mul_assoc]
     _ ↔ -1 = ⅟a * b + ⅟b * a := ⟨fun h ↦ by simpa using congr_arg (⅟a * ·) h, fun h ↦ by simp [← h]⟩
 
-lemma neg_one_eq_of_invOf_add_eq_add_invOf [Ring R] {a b : R} [Invertible a]
-    [Invertible b] [Invertible (a + b)] (h : ⅟(a + b) = ⅟a + ⅟b) : -1 = ⅟a * b + ⅟b * a := by
-  have : 1 = 2 + ⅟a * b + ⅟b * a := by
-    rw [← invOf_mul_self (a + b), h, add_mul, mul_add, mul_add, invOf_mul_self a,
-        invOf_mul_self b, ← add_assoc, add_assoc 1 _, add_comm 1 _, add_assoc 2 _,
-        add_comm 2 _, add_assoc, one_add_one_eq_two]
-  apply (add_left_inj 2).mp
-  conv => lhs; rw [← one_add_one_eq_two, ← add_assoc, neg_add_cancel, zero_add]
-  rw [add_comm, ← add_assoc]
-  exact this
+lemma neg_one_eq_invOf_mul_add_invOf_mul_iff [Ring R] {a b : R} [Invertible a]
+    [Invertible b] [Invertible (a + b)] : ⅟(a + b) = ⅟a + ⅟b ↔ -1 = ⅟a * b + ⅟b * a := by
+  calc ⅟(a + b) = ⅟a + ⅟b
+      ↔ ⅟(a + b) * (a + b) = (⅟a + ⅟b) * (a + b) := by rw [mul_left_inj_of_invertible]
+    _ ↔ 1 = ⅟a * a + ⅟b * a + (⅟a * b + ⅟b * b) := by rw [invOf_mul_self, mul_add, add_mul, add_mul]
+    _ ↔ 1 = 1 + ⅟b * a + (1 + ⅟a * b) := by rw [invOf_mul_self, invOf_mul_self, add_comm _ 1]
+    _ ↔ 1 = 1 + 1 + ⅟b * a + ⅟a * b := by rw [← add_assoc, add_comm _ 1, ← add_assoc]
+    _ ↔ -2 + 1 = -2 + (2 + ⅟b * a + ⅟a * b) := by rw [one_add_one_eq_two, add_right_inj]
+    _ ↔ -2 + 1 = ⅟b * a + ⅟a * b := by rw [← add_assoc, ← add_assoc, neg_add_cancel, zero_add]
+    _ ↔ -1 + 0 = ⅟b * a + ⅟a * b := by rw [← one_add_one_eq_two, neg_add, add_assoc, neg_add_cancel]
+    _ ↔ -1 = ⅟a * b + ⅟b * a := by rw [add_zero, add_comm]
 
 theorem eq_of_invOf_add_eq_invOf_add_invOf [Ring R] {a b : R} [Invertible a] [Invertible b]
     [Invertible (a + b)] (h : ⅟(a + b) = ⅟a + ⅟b) :
     a * ⅟b * a = b * ⅟a * b := by
-  have h' := neg_one_eq_of_invOf_add_eq_add_invOf h
+  have h' := neg_one_eq_invOf_mul_add_invOf_mul_iff.mp h
   have h_a_binv_a : -(b + a) = a * ⅟b * a := neg_add_eq_mul_invOf_mul_same_iff.mpr h'
   have h_b_ainv_b : -(a + b) = b * ⅟a * b := by
     rw [add_comm] at h'
