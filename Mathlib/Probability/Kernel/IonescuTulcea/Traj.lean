337e4a9578a74a3081f707cd43d6801f4419b00d
Paulo Rauber <pauloeduardorauber@gmail.com>
2025-10-08 16:00:51 +0100

    Draft documentation

--- Mathlib/Probability/Kernel/IonescuTulcea/Traj.lean
+++ Mathlib/Probability/Kernel/IonescuTulcea/Traj.lean
@@ -6,8 +6,11 @@ Authors: Etienne Marion
 import Mathlib.MeasureTheory.Constructions.ProjectiveFamilyContent
 import Mathlib.MeasureTheory.Function.FactorsThrough
 import Mathlib.MeasureTheory.Measure.ProbabilityMeasure
+import Mathlib.MeasureTheory.Measure.Typeclasses.Probability
 import Mathlib.MeasureTheory.OuterMeasure.OfAddContent
 import Mathlib.Probability.Kernel.Composition.IntegralCompProd
+import Mathlib.Probability.Kernel.Composition.Lemmas
+import Mathlib.Probability.Kernel.CondDistrib
 import Mathlib.Probability.Kernel.IonescuTulcea.PartialTraj
 import Mathlib.Probability.Kernel.SetIntegral
 
@@ -36,11 +39,13 @@ the trajectory in `X 0 × ... × X a` and outputs the distribution of the whole 
 We also provide tools to compute integrals against `traj κ a` and an expression for the conditional
 expectation.
 
-## Main definition
+## Main definitions
 
 * `traj κ a`: a kernel from `Π i : Iic a, X i` to `Π n, X n` which takes as input a trajectory
   up to time `a` and outputs the distribution of the trajectory obtained by iterating the kernels
   `κ`. Its existence is given by the Ionescu-Tulcea theorem.
+* `trajMeasure μ₀ κ`: a measure on `Π n, X n` that corresponds to the distribution of the trajectory
+  obtained by starting with the distribution `μ₀` and then iterating the kernels `κ`.
 
 ## Main statements
 
@@ -49,6 +54,8 @@ expectation.
 * `traj_comp_partialTraj`: Given the distribution up to time `a`, `partialTraj κ a b`
   gives the distribution of the trajectory up to time `b`, and composing this with
   `traj κ b` gives the distribution of the whole trajectory.
+* `condDistrib_trajMeasure_ae_eq_kernel`: a regular conditional probability distribution of the
+  point at time `a + 1` given the trajectory up to time `a` corresponds to the kernel `κ a`.
 * `condExp_traj`: If `a ≤ b`, the conditional expectation of `f` with respect to `traj κ a`
   given the information up to time `b` is obtained by integrating `f` against `traj κ b`.
 
@@ -530,6 +537,20 @@ lemma traj_map_frestrictLe_of_le {a b : ℕ} (hab : a ≤ b) :
       deterministic (frestrictLe₂ hab) (measurable_frestrictLe₂ _) := by
   rw [traj_map_frestrictLe, partialTraj_le]
 
+/-- The pushforward of `traj κ a` along the the point at time `a + 1` is the kernel `κ a`. -/
+lemma traj_map_eq_kernel {a : ℕ} : (traj κ a).map (fun x ↦ x (a + 1)) = κ a := by
+  set f : (Π n, X n) → X (a + 1) := fun x ↦ x (a + 1)
+  set g : (Π n : Iic (a + 1), X n) → X (a + 1) := fun x ↦ x ⟨a + 1, by simp⟩
+  have hf : f = g ∘ (frestrictLe (a + 1)) := by rfl
+  have hp : g ∘ IicProdIoc a (a + 1) = (piSingleton a).symm ∘ Prod.snd := by
+    ext
+    simp [g, _root_.IicProdIoc, piSingleton]
+  rw [hf, map_comp_right _ (by fun_prop) (by fun_prop), traj_map_frestrictLe, partialTraj_succ_self,
+    ← map_comp_right _ (by fun_prop) (by fun_prop), hp,
+    map_comp_right _ (by fun_prop) (by fun_prop), ← snd_eq, snd_prod,
+    ← map_comp_right _ (by fun_prop) (by fun_prop)]
+  simp
+
 variable (κ)
 
 /-- To check that `η = traj κ a` it is enough to show that the restriction of `η` to variables `≤ b`
@@ -648,6 +669,14 @@ lemma partialTraj_compProd_traj {a b : ℕ} (hab : a ≤ b) (u : Π i : Iic a, X
   any_goals exact ms.preimage (by fun_prop)
   fun_prop
 
+lemma partialTraj_compProd_kernel_eq_traj_map {a : ℕ} {x₀ : Π n : Iic 0, X n} :
+    (partialTraj κ 0 a x₀) ⊗ₘ (κ a) = (traj κ 0 x₀).map (fun x ↦ (frestrictLe a x, x (a + 1))) := by
+  set f := fun x ↦ (frestrictLe a x, x (a + 1))
+  set g := fun x ↦ (frestrictLe a x, x)
+  have hf : f = (Prod.map id (fun x ↦ x (a + 1))) ∘ g := rfl
+  rw [hf, ← Measure.map_map (by fun_prop) (by fun_prop), ← partialTraj_compProd_traj zero_le',
+    ← MeasureTheory.Measure.compProd_map (by fun_prop), traj_map_eq_kernel]
+
 theorem integral_traj_partialTraj' {a b : ℕ} (hab : a ≤ b) {x₀ : Π i : Iic a, X i}
     {f : (Π i : Iic b, X i) → (Π n : ℕ, X n) → E}
     (hf : Integrable f.uncurry ((partialTraj κ a b x₀) ⊗ₘ (traj κ b))) :
@@ -729,5 +758,53 @@ theorem condExp_traj' {a b c : ℕ} (hab : a ≤ b) (hbc : b ≤ c)
   exact (mcf.comp_measurable measurable_updateFinset).aestronglyMeasurable
 
 end integral
+
+section trajMeasure
+
+instance : Unique (Iic 0) := by simp only [mem_Iic, nonpos_iff_eq_zero]; exact Unique.subtypeEq 0
+
+lemma coe_default_Iic_zero : ((default : Iic 0) : ℕ) = 0 := by
+  calc _ = ((⟨0, by simp⟩ : Iic 0) : ℕ) := by congr; exact (Unique.eq_default _).symm
+    _ = _ := by simp
+
+/-- Measurable equivalence between `(i : Iic 0) → X i` and `X 0`. -/
+def MeasurableEquiv.piIicZero (X : ℕ → Type*) [∀ n, MeasurableSpace (X n)] :
+    ((i : Iic 0) → X i) ≃ᵐ X 0 :=
+  (MeasurableEquiv.piUnique _).trans (coe_default_Iic_zero.symm ▸ MeasurableEquiv.refl _)
+
+/-- Distribution of the trajectory given by starting with `μ₀` and iterating the kernels `κ`. -/
+noncomputable
+def trajMeasure (μ₀ : Measure (X 0)) (κ : (n : ℕ) → Kernel (Π i : Iic n, X i) (X (n + 1)))
+    [∀ n, IsMarkovKernel (κ n)] :
+    Measure (Π n, X n) :=
+  (traj κ 0) ∘ₘ (μ₀.map (MeasurableEquiv.piIicZero _).symm)
+
+variable {μ₀ : Measure (X 0)} [IsProbabilityMeasure μ₀]
+
+instance : IsProbabilityMeasure (trajMeasure μ₀ κ) := by
+  rw [trajMeasure]
+  have : IsProbabilityMeasure (μ₀.map (MeasurableEquiv.piIicZero _).symm) :=
+    Measure.isProbabilityMeasure_map <| by fun_prop
+  infer_instance
+
+lemma trajMeasure_map_frestrictLe_compProd_kernel_eq_trajMeasure_map {a : ℕ} :
+    (trajMeasure μ₀ κ).map (frestrictLe a) ⊗ₘ κ a =
+      (trajMeasure μ₀ κ).map (fun x ↦ (frestrictLe a x, x (a + 1))) := by
+  rw [Measure.compProd_eq_comp_prod, trajMeasure, Measure.map_comp _ _ (by fun_prop),
+    traj_map_frestrictLe, Measure.comp_assoc, Measure.map_comp _ _ (by fun_prop)]
+  congr with x₀
+  rw [comp_apply, ← Measure.compProd_eq_comp_prod, map_apply _ (by fun_prop),
+    partialTraj_compProd_kernel_eq_traj_map]
+
+/-- A regular conditional probability distribution of the point at time `a + 1` given the
+trajectory up to time `a` is the kernel `κ a`. -/
+lemma condDistrib_trajMeasure_ae_eq_kernel {a : ℕ}
+    [StandardBorelSpace (X (a + 1))] [Nonempty (X (a + 1))] :
+    condDistrib (fun x ↦ x (a + 1)) (frestrictLe a) (trajMeasure μ₀ κ)
+      =ᵐ[(trajMeasure μ₀ κ).map (frestrictLe a)] κ a := by
+  apply condDistrib_ae_eq_of_measure_eq_compProd_of_measurable (by measurability) (by measurability)
+  exact trajMeasure_map_frestrictLe_compProd_kernel_eq_trajMeasure_map.symm
+
+end trajMeasure
 
 end ProbabilityTheory.Kernel
