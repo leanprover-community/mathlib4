32bf9ff722b740615f79d7f4e9530184d317bd23
Christopher Hoskin <4855578+mans0954@users.noreply.github.com>
2025-10-09 17:08:11 +0000

    feat(Order/ScottContinuity): Scott continuity on product spaces (#26338)
    Some further results on Scott Continuity:
    
    - `scottContinuous_iff_map_sSup`: `f` is Scott continuous if and only if it commutes with `sSup` on directed sets
    - `ScottContinuous_prod_of_ScottContinuous`: `f` is Scott continuous on a product space if it is Scott continuous in each variable
    - `ScottContinuousOn.inf₂`: For complete linear orders, the meet operation is Scott continuous
    
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -5254,6 +5254,8 @@ import Mathlib.Order.RelIso.Set
 import Mathlib.Order.RelSeries
 import Mathlib.Order.Restriction
 import Mathlib.Order.ScottContinuity
+import Mathlib.Order.ScottContinuity.Complete
+import Mathlib.Order.ScottContinuity.Prod
 import Mathlib.Order.SemiconjSup
 import Mathlib.Order.Set
 import Mathlib.Order.SetIsMax
