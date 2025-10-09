0d83d6b344cc63080c4938aa8c5676c41f752d76
Sina Hazratpour <sinahazratpour@gmail.com>
2025-10-09 16:20:06 +0200

    The section functor as a right adjoint to the star functor

--- Mathlib.lean
+++ Mathlib.lean
@@ -2561,6 +2561,7 @@ import Mathlib.CategoryTheory.Localization.StructuredArrow
 import Mathlib.CategoryTheory.Localization.Triangulated
 import Mathlib.CategoryTheory.Localization.Trifunctor
 import Mathlib.CategoryTheory.LocallyCartesianClosed.Prelim
+import Mathlib.CategoryTheory.LocallyCartesianClosed.Sections
 import Mathlib.CategoryTheory.LocallyDirected
 import Mathlib.CategoryTheory.Monad.Adjunction
 import Mathlib.CategoryTheory.Monad.Algebra
