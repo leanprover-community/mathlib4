17b56a09d21192eb845838b4327a649f2b5330ce
Kenny Lau <kc_kennylau@yahoo.com.hk>
2025-10-09 17:40:10 +0100

    feat(RingTheory): isomorphism of graded rings

--- Mathlib/RingTheory/GradedAlgebra/RingHom.lean
+++ Mathlib/RingTheory/GradedAlgebra/RingHom.lean
@@ -3,7 +3,7 @@ Copyright (c) 2025 Kenny Lau. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kenny Lau
 -/
-import Mathlib.Data.FunLike.GradedFunLike
+import Mathlib.Data.FunLike.Graded
 import Mathlib.RingTheory.GradedAlgebra.Basic
 
 /-!
