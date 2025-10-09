b78d0d7f89ff9f4906a06ca238cee90909589a24
Joël Riou <37772949+joelriou@users.noreply.github.com>
2025-10-09 13:01:52 +0000

    feat(AlgebraicTopology/SimplicialSet): nondegenerate simplices in the standard simplex (#28395)
    In this PR, we show that `d`-dimensional nondegenerate simplices in `Δ[n]` identify to `Fin (d + 1) ↪o Fin (n + 1)`.
    
    (This PR also contains some general additional API for (non)degenerate simplices. For this reason, three definitions related to subcomplexes generated by a simplex had to be moved from `StdSimplex.lean` to `Subcomplex.lean`.)

--- Mathlib/AlgebraicTopology/SimplicialSet/Simplices.lean
+++ Mathlib/AlgebraicTopology/SimplicialSet/Simplices.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joël Riou
 -/
 import Mathlib.CategoryTheory.Elements
-import Mathlib.AlgebraicTopology.SimplicialSet.StdSimplex
+import Mathlib.AlgebraicTopology.SimplicialSet.Subcomplex
 
 /-!
 # The preordered type of simplices of a simplicial set
