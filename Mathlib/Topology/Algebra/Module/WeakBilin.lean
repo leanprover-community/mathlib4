d3ba373496ddf628ccdda2053265c20f407340e1
Christopher Hoskin <mans0954@users.noreply.github.com>
2025-10-09 21:01:00 +0100

    Fix

--- Mathlib/Topology/Algebra/Module/WeakBilin.lean
+++ Mathlib/Topology/Algebra/Module/WeakBilin.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kalle Kytölä, Moritz Doll
 -/
 import Mathlib.Topology.Algebra.Module.LinearMap
-import Mathlib.LinearAlgebra.SesquilinearForm
+import Mathlib.LinearAlgebra.SesquilinearForm.Basic
 import Mathlib.Topology.Algebra.Module.LinearSpan
 import Mathlib.Topology.Algebra.Module.StrongTopology
 
