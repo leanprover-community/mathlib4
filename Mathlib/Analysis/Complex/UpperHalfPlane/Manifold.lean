4a8acb2d487bc53d8d2fe389a1ae7b5cc34cadec
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-10-10 13:45:17 -0700

    Golf UpperHalfPlane/Manifold

--- Mathlib/Analysis/Complex/UpperHalfPlane/Manifold.lean
+++ Mathlib/Analysis/Complex/UpperHalfPlane/Manifold.lean
@@ -9,6 +9,7 @@ public import Mathlib.Analysis.Complex.CauchyIntegral
 public import Mathlib.Analysis.Complex.UpperHalfPlane.Topology
 public import Mathlib.Geometry.Manifold.Algebra.Structures
 public import Mathlib.Geometry.Manifold.ContMDiff.Atlas
+import Mathlib.Geometry.Manifold.Notation
 public import Mathlib.Geometry.Manifold.MFDeriv.FDeriv
 
 /-!
@@ -34,15 +35,14 @@ instance : IsManifold 𝓘(ℂ) ω ℍ :=
   UpperHalfPlane.isOpenEmbedding_coe.isManifold_singleton
 
 /-- The inclusion map `ℍ → ℂ` is a map of `C^n` manifolds. -/
-theorem contMDiff_coe : ContMDiff 𝓘(ℂ) 𝓘(ℂ) n ((↑) : ℍ → ℂ) :=
+theorem contMDiff_coe : CMDiff n ((↑) : ℍ → ℂ) :=
   fun _ => contMDiffAt_extChartAt
 
 /-- The inclusion map `ℍ → ℂ` is a differentiable map of manifolds. -/
-theorem mdifferentiable_coe : MDifferentiable 𝓘(ℂ) 𝓘(ℂ) ((↑) : ℍ → ℂ) :=
+theorem mdifferentiable_coe : MDiff ((↑) : ℍ → ℂ) :=
   contMDiff_coe.mdifferentiable one_ne_zero
 
-lemma contMDiffAt_ofComplex {z : ℂ} (hz : 0 < z.im) :
-    ContMDiffAt 𝓘(ℂ) 𝓘(ℂ) n ofComplex z := by
+lemma contMDiffAt_ofComplex {z : ℂ} (hz : 0 < z.im) : CMDiffAt n ofComplex z := by
   rw [contMDiffAt_iff]
   constructor
   · -- continuity at z
@@ -52,71 +52,64 @@ lemma contMDiffAt_ofComplex {z : ℂ} (hz : 0 < z.im) :
   · -- smoothness in local chart
     simpa using contDiffAt_id.congr_of_eventuallyEq (eventuallyEq_coe_comp_ofComplex hz)
 
-lemma mdifferentiableAt_ofComplex {z : ℂ} (hz : 0 < z.im) :
-    MDifferentiableAt 𝓘(ℂ) 𝓘(ℂ) ofComplex z :=
+lemma mdifferentiableAt_ofComplex {z : ℂ} (hz : 0 < z.im) : MDiffAt ofComplex z :=
   (contMDiffAt_ofComplex hz).mdifferentiableAt one_ne_zero
 
 lemma contMDiffAt_iff {f : ℍ → ℂ} {τ : ℍ} :
-    ContMDiffAt 𝓘(ℂ) 𝓘(ℂ) n f τ ↔ ContDiffAt ℂ n (f ∘ ofComplex) τ := by
+    CMDiffAt n f τ ↔ ContDiffAt ℂ n (f ∘ ofComplex) τ := by
   rw [← contMDiffAt_iff_contDiffAt]
   refine ⟨fun hf ↦ ?_, fun hf ↦ ?_⟩
   · exact (ofComplex_apply τ ▸ hf).comp _ (contMDiffAt_ofComplex τ.im_pos)
   · simpa only [Function.comp_def, ofComplex_apply] using hf.comp τ (contMDiff_coe τ)
 
 lemma mdifferentiableAt_iff {f : ℍ → ℂ} {τ : ℍ} :
-    MDifferentiableAt 𝓘(ℂ) 𝓘(ℂ) f τ ↔ DifferentiableAt ℂ (f ∘ ofComplex) ↑τ := by
+    MDiffAt f τ ↔ DifferentiableAt ℂ (f ∘ ofComplex) ↑τ := by
   rw [← mdifferentiableAt_iff_differentiableAt]
   refine ⟨fun hf ↦ ?_, fun hf ↦ ?_⟩
   · exact (ofComplex_apply τ ▸ hf).comp _ (mdifferentiableAt_ofComplex τ.im_pos)
   · simpa only [Function.comp_def, ofComplex_apply] using hf.comp τ (mdifferentiable_coe τ)
 
 lemma mdifferentiable_iff {f : ℍ → ℂ} :
-    MDifferentiable 𝓘(ℂ) 𝓘(ℂ) f ↔ DifferentiableOn ℂ (f ∘ ofComplex) {z | 0 < z.im} :=
+    MDiff f ↔ DifferentiableOn ℂ (f ∘ ofComplex) {z | 0 < z.im} :=
   ⟨fun h z hz ↦ (mdifferentiableAt_iff.mp (h ⟨z, hz⟩)).differentiableWithinAt,
     fun h ⟨z, hz⟩ ↦ mdifferentiableAt_iff.mpr <| (h z hz).differentiableAt
      <| isOpen_upperHalfPlaneSet.mem_nhds hz⟩
 
-lemma contMDiff_num (g : GL (Fin 2) ℝ) : ContMDiff 𝓘(ℂ) 𝓘(ℂ) n (fun τ : ℍ ↦ num g τ) :=
+lemma contMDiff_num (g : GL (Fin 2) ℝ) : CMDiff n (fun τ : ℍ ↦ num g τ) :=
   (contMDiff_const.smul contMDiff_coe).add contMDiff_const
 
-lemma contMDiff_denom (g : GL (Fin 2) ℝ) : ContMDiff 𝓘(ℂ) 𝓘(ℂ) n (fun τ : ℍ ↦ denom g τ) :=
+lemma contMDiff_denom (g : GL (Fin 2) ℝ) : CMDiff n (fun τ : ℍ ↦ denom g τ) :=
   (contMDiff_const.smul contMDiff_coe).add contMDiff_const
 
-lemma contMDiff_denom_zpow (g : GL (Fin 2) ℝ) (k : ℤ) :
-    ContMDiff 𝓘(ℂ) 𝓘(ℂ) n (denom g · ^ k : ℍ → ℂ) := fun τ ↦ by
+lemma contMDiff_denom_zpow (g : GL (Fin 2) ℝ) (k : ℤ) : CMDiff n (denom g · ^ k : ℍ → ℂ) := by
+  intro τ
   have : AnalyticAt ℂ (· ^ k) (denom g τ) := (differentiableOn_zpow k _ (by tauto)).analyticOnNhd
     isOpen_compl_singleton _ (denom_ne_zero g τ)
   exact this.contDiffAt.contMDiffAt.comp τ (contMDiff_denom g τ)
 
-lemma contMDiff_inv_denom (g : GL (Fin 2) ℝ) :
-    ContMDiff 𝓘(ℂ) 𝓘(ℂ) n (fun τ : ℍ ↦ (denom g τ)⁻¹) := by
+lemma contMDiff_inv_denom (g : GL (Fin 2) ℝ) : CMDiff n (fun τ : ℍ ↦ (denom g τ)⁻¹) := by
   simpa using contMDiff_denom_zpow g (-1)
 
 /-- Each element of `GL(2, ℝ)⁺` defines a map of `C ^ n` manifolds `ℍ → ℍ`. -/
-lemma contMDiff_smul {g : GL (Fin 2) ℝ} (hg : 0 < g.det.val) :
-    ContMDiff 𝓘(ℂ) 𝓘(ℂ) n (fun τ : ℍ ↦ g • τ) := fun τ ↦ by
+lemma contMDiff_smul {g : GL (Fin 2) ℝ} (hg : 0 < g.det.val) : CMDiff n (fun τ : ℍ ↦ g • τ) := by
+  intro τ
   refine contMDiffAt_iff_target.mpr ⟨(continuous_const_smul g).continuousAt, ?_⟩
   simpa [glPos_smul_def hg] using (contMDiff_num g τ).mul (contMDiff_inv_denom g τ)
 
-lemma mdifferentiable_num (g : GL (Fin 2) ℝ) :
-    MDifferentiable 𝓘(ℂ) 𝓘(ℂ) (fun τ : ℍ ↦ num g τ) :=
+lemma mdifferentiable_num (g : GL (Fin 2) ℝ) : MDiff (fun τ : ℍ ↦ num g τ) :=
   (contMDiff_num g).mdifferentiable one_ne_zero
 
-lemma mdifferentiable_denom (g : GL (Fin 2) ℝ) :
-    MDifferentiable 𝓘(ℂ) 𝓘(ℂ) (fun τ : ℍ ↦ denom g τ) :=
+lemma mdifferentiable_denom (g : GL (Fin 2) ℝ) : MDiff (fun τ : ℍ ↦ denom g τ) :=
   (contMDiff_denom g).mdifferentiable one_ne_zero
 
-lemma mdifferentiable_denom_zpow (g : GL (Fin 2) ℝ) (k : ℤ) :
-    MDifferentiable 𝓘(ℂ) 𝓘(ℂ) (denom g · ^ k : ℍ → ℂ) :=
+lemma mdifferentiable_denom_zpow (g : GL (Fin 2) ℝ) (k : ℤ) : MDiff (denom g · ^ k : ℍ → ℂ) :=
   (contMDiff_denom_zpow g k).mdifferentiable one_ne_zero
 
-lemma mdifferentiable_inv_denom (g : GL (Fin 2) ℝ) :
-    MDifferentiable 𝓘(ℂ) 𝓘(ℂ) (fun τ : ℍ ↦ (denom g τ)⁻¹) :=
+lemma mdifferentiable_inv_denom (g : GL (Fin 2) ℝ) : MDiff (fun τ : ℍ ↦ (denom g τ)⁻¹) :=
   (contMDiff_inv_denom g).mdifferentiable one_ne_zero
 
 /-- Each element of `GL(2, ℝ)⁺` defines a complex-differentiable map `ℍ → ℍ`. -/
-lemma mdifferentiable_smul {g : GL (Fin 2) ℝ} (hg : 0 < g.det.val) :
-    MDifferentiable 𝓘(ℂ) 𝓘(ℂ) (fun τ : ℍ ↦ g • τ) :=
+lemma mdifferentiable_smul {g : GL (Fin 2) ℝ} (hg : 0 < g.det.val) : MDiff (fun τ : ℍ ↦ g • τ) :=
   (contMDiff_smul hg).mdifferentiable one_ne_zero
 
 lemma eq_zero_of_frequently {f : ℍ → ℂ} (hf : MDifferentiable 𝓘(ℂ) 𝓘(ℂ) f)
