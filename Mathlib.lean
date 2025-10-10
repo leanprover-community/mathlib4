d976e01fb092e845bc6cae0a5aa93bdf0f2bb5e9
Yury Kudryashov <yury@harmonic.fun>
2025-10-10 21:54:55 +0000

    Move a file

--- Mathlib.lean
+++ Mathlib.lean
@@ -1999,6 +1999,7 @@ import Mathlib.Analysis.SpecialFunctions.Pow.Continuity
 import Mathlib.Analysis.SpecialFunctions.Pow.Deriv
 import Mathlib.Analysis.SpecialFunctions.Pow.Integral
 import Mathlib.Analysis.SpecialFunctions.Pow.NNReal
+import Mathlib.Analysis.SpecialFunctions.Pow.NthRootLemmas
 import Mathlib.Analysis.SpecialFunctions.Pow.Real
 import Mathlib.Analysis.SpecialFunctions.Sigmoid
 import Mathlib.Analysis.SpecialFunctions.SmoothTransition
@@ -3526,7 +3527,6 @@ import Mathlib.Data.Nat.Multiplicity
 import Mathlib.Data.Nat.Notation
 import Mathlib.Data.Nat.Nth
 import Mathlib.Data.Nat.NthRoot.Defs
-import Mathlib.Data.Nat.NthRoot.Lemmas
 import Mathlib.Data.Nat.Order.Lemmas
 import Mathlib.Data.Nat.PSub
 import Mathlib.Data.Nat.Pairing
