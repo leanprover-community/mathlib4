d9b679eee15fd9b07324b06750fe8eec8e6a6bf3
Snir Broshi <26556598+SnirBroshi@users.noreply.github.com>
2025-10-11 19:29:00 +0300

    feat(Algebra/Polynomial/Div): `p ∣ q → rootMultiplicity x p ≤ rootMultiplicity x q`

--- Mathlib/Algebra/Polynomial/Div.lean
+++ Mathlib/Algebra/Polynomial/Div.lean
@@ -704,6 +704,11 @@ lemma le_rootMultiplicity_mul {p q : R[X]} (x : R) (hpq : p * q ≠ 0) :
   rw [le_rootMultiplicity_iff hpq, pow_add]
   gcongr <;> apply pow_rootMultiplicity_dvd
 
+lemma rootMultiplicity_le_rootMultiplicity_of_dvd {p q : R[X]} (hq : q ≠ 0) (hpq : p ∣ q) (x : R) :
+    p.rootMultiplicity x ≤ q.rootMultiplicity x := by
+  obtain ⟨_, rfl⟩ := hpq
+  exact Nat.le_of_add_right_le <| le_rootMultiplicity_mul x hq
+
 lemma pow_rootMultiplicity_not_dvd (p0 : p ≠ 0) (a : R) :
     ¬(X - C a) ^ (rootMultiplicity a p + 1) ∣ p := by rw [← rootMultiplicity_le_iff p0]
 
