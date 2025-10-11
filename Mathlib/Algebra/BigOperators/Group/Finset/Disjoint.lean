ef71ef04465751fbf551c38c4c0c466aad4b98d0
Andrew Yang <the.erd.one@gmail.com>
2025-10-11 13:49:29 +0100

    fix

--- Mathlib/Algebra/BigOperators/Group/Finset/Disjoint.lean
+++ Mathlib/Algebra/BigOperators/Group/Finset/Disjoint.lean
@@ -12,10 +12,11 @@ import Mathlib.Order.Disjointed
 
 /-- Given a sequence of finite sets `s₀ ⊆ s₁ ⊆ s₂ ⋯`, the product of `gᵢ` over `i ∈ sₙ` is equal
 to `∏_{i ∈ s₀} gᵢ` * `∏_{j < n, i ∈ sⱼ₊₁ \ sⱼ} gᵢ`. -/
-@[to_additive]
+@[to_additive "Given a sequence of finite sets `s₀ ⊆ s₁ ⊆ s₂ ⋯`, the sum of `gᵢ` over `i ∈ sₙ` is
+equal to `∑_{i ∈ s₀} gᵢ` + `∑_{j < n, i ∈ sⱼ₊₁ \ sⱼ} gᵢ`."]
 lemma Finset.prod_eq_prod_range_sdiff
     {α β : Type*} [DecidableEq α] [CommMonoid β] (s : ℕ → Finset α) (hs : Monotone s)
-     (g : α → β) (n : ℕ) :
+    (g : α → β) (n : ℕ) :
     ∏ i ∈ s n, g i = (∏ i ∈ s 0, g i) * ∏ i ∈ range n, ∏ j ∈ s (i + 1) \ s i, g j := by
   conv_lhs => rw [← hs.partialSups_eq, ← disjiUnion_Iic_disjointed, Iic_eq_Icc,
     prod_disjiUnion, Nat.bot_eq_zero, ← Nat.range_succ_eq_Icc_zero, prod_range_succ', mul_comm]
