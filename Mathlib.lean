43dfa8cafa5c5c396701b7c8cc0a3bee1eccb5e4
Monica Omar <2497250a@research.gla.ac.uk>
2025-10-11 15:35:20 +0000

    move

--- Mathlib.lean
+++ Mathlib.lean
@@ -1162,8 +1162,8 @@ import Mathlib.Algebra.Star.CHSH
 import Mathlib.Algebra.Star.Center
 import Mathlib.Algebra.Star.CentroidHom
 import Mathlib.Algebra.Star.Conjneg
-import Mathlib.Algebra.Star.Convolution
 import Mathlib.Algebra.Star.Free
+import Mathlib.Algebra.Star.LinearMap
 import Mathlib.Algebra.Star.Module
 import Mathlib.Algebra.Star.MonoidHom
 import Mathlib.Algebra.Star.NonUnitalSubalgebra
