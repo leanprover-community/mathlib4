b8ae59388afb13fb6d3630c80abf48b364753703
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2025-10-11 20:00:54 +0000

    feat: Ideal.ramificationIdx_mul_inertiaDeg_of_isLocalRing (#29729)
    We add the special case `Ideal.ramificationIdx_mul_inertiaDeg_of_isLocalRing` of `Ideal.sum_ramification_inertia` in the local (DVR) case: the product of the ramification index and the inertia degree equals the degree of the field extension.
    
    Also make the argument `p` implicit in `Ideal.sum_ramification_inertia`.
    
    Co-authored-by: Matthew Jasper <mjjasper1@gmail.com>
    Co-authored-by: Xavier Roblot <46200072+xroblot@users.noreply.github.com>

--- Mathlib/RingTheory/DedekindDomain/Ideal/Lemmas.lean
+++ Mathlib/RingTheory/DedekindDomain/Ideal/Lemmas.lean
@@ -1011,6 +1011,12 @@ theorem coe_primesOverFinset : primesOverFinset p B = primesOver p B := by
     (fun ⟨hPp, h⟩ => ⟨hPp, ⟨hpm.eq_of_le (comap_ne_top _ hPp.ne_top) (le_comap_of_map_le h)⟩⟩)
     (fun ⟨hPp, h⟩ => ⟨hPp, map_le_of_le_comap h.1.le⟩)
 
+variable {R} (A) in
+theorem IsLocalRing.primesOverFinset_eq [IsLocalRing A] [IsDedekindDomain A]
+    [Algebra R A] [FaithfulSMul R A] [Module.Finite R A] {p : Ideal R} [p.IsMaximal] (hp0 : p ≠ ⊥) :
+    primesOverFinset p A = {IsLocalRing.maximalIdeal A} := by
+  rw [← Finset.coe_eq_singleton, coe_primesOverFinset hp0, IsLocalRing.primesOver_eq A hp0]
+
 namespace IsDedekindDomain.HeightOneSpectrum
 
 /--
