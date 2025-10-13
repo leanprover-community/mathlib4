eed9c44240db7d8bb2c7c8e23e83a7220000c31e
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2025-10-13 11:12:48 +0800

    fix

--- Mathlib/Algebra/Category/Grp/Preadditive.lean
+++ Mathlib/Algebra/Category/Grp/Preadditive.lean
@@ -64,7 +64,7 @@ def homAddEquiv : (M ⟶ N) ≃+ (M →+ N) :=
   { ConcreteCategory.homEquiv (C := AddCommGrpCat) with
     map_add' _ _ := rfl }
 
-lemma subsingleton_of_isZero {G : AddCommGrp} (h : Limits.IsZero G) :
+lemma subsingleton_of_isZero {G : AddCommGrpCat} (h : Limits.IsZero G) :
     Subsingleton G := by
   apply subsingleton_of_forall_eq 0 (fun g ↦ ?_)
   rw [← AddMonoidHom.id_apply G g, ← AddCommGrp.hom_id]
