55317ba1bdd93b76236ed7bb9db5ee873db045f7
Moritz Doll <moritz.doll@googlemail.com>
2025-10-14 11:23:39 -0700

    mk_all

--- Mathlib.lean
+++ Mathlib.lean
@@ -1681,6 +1681,7 @@ import Mathlib.Analysis.Convolution
 import Mathlib.Analysis.Distribution.AEEqOfIntegralContDiff
 import Mathlib.Analysis.Distribution.FourierSchwartz
 import Mathlib.Analysis.Distribution.SchwartzSpace
+import Mathlib.Analysis.Distribution.TemperateGrowth
 import Mathlib.Analysis.Fourier.AddCircle
 import Mathlib.Analysis.Fourier.AddCircleMulti
 import Mathlib.Analysis.Fourier.BoundedContinuousFunctionChar
