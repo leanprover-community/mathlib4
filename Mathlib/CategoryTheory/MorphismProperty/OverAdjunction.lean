a00243437963b796de3b6089c399c1d936bf7f49
jlh18 <josephhua73@yahoo.com>
2025-10-14 07:20:08 -0400

    remove author

--- Mathlib/CategoryTheory/MorphismProperty/OverAdjunction.lean
+++ Mathlib/CategoryTheory/MorphismProperty/OverAdjunction.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2024 Christian Merten. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Christian Merten, Joseph Hua
+Authors: Christian Merten
 -/
 import Mathlib.CategoryTheory.MorphismProperty.Comma
 import Mathlib.CategoryTheory.Comma.Over.Pullback
