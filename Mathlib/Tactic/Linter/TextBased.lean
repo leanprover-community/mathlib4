89070259495ad45f7f77ff7eb7245e85f4f86983
Kim Morrison <477956+kim-em@users.noreply.github.com>
2025-10-15 08:17:10 +1100

    Update Mathlib/Tactic/Linter/TextBased.lean
    Co-authored-by: Anne Baanen <Vierkantor@users.noreply.github.com>

--- Mathlib/Tactic/Linter/TextBased.lean
+++ Mathlib/Tactic/Linter/TextBased.lean
@@ -309,6 +309,9 @@ def lintFile (opts : LinterOptions) (path : FilePath) (exceptions : Array ErrorC
   return (errors, if changes_made then some changed else none)
 
 /-- Enables the old Python-based style linters. -/
+-- TODO: these linters assume they are being run in `./scripts` and do not work on
+-- downstream projects. Fix this before re-enabling them by default.
+-- Or better yet: port them to Lean 4.
 register_option linter.pythonStyle : Bool := { defValue := false }
 
 /-- Lint a collection of modules for style violations.
