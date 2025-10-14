24d243750882ec2570770bcb87b7fe8223a37714
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2025-10-14 16:29:49 +0200

    Revert "chore: rename eq_zero_of_zpow_eq_zero to zpow_eq_zero"
    This reverts commit 85a045d7be91a1eccdf48e584b75bbd4a636c3e2.

--- Mathlib/Algebra/GroupWithZero/Units/Basic.lean
+++ Mathlib/Algebra/GroupWithZero/Units/Basic.lean
@@ -356,12 +356,10 @@ lemma zpow_ne_zero {a : G₀} : ∀ n : ℤ, a ≠ 0 → a ^ n ≠ 0
   | (_ : ℕ) => by rw [zpow_natCast]; exact pow_ne_zero _
   | .negSucc n => fun ha ↦ by rw [zpow_negSucc]; exact inv_ne_zero (pow_ne_zero _ ha)
 
-lemma zpow_eq_zero {n : ℤ} : a ^ n = 0 → a = 0 := not_imp_not.1 (zpow_ne_zero _)
-
-@[deprecated (since := "2025-10-11")] alias eq_zero_of_zpow_eq_zero := zpow_eq_zero
+lemma eq_zero_of_zpow_eq_zero {n : ℤ} : a ^ n = 0 → a = 0 := not_imp_not.1 (zpow_ne_zero _)
 
 lemma zpow_eq_zero_iff {n : ℤ} (hn : n ≠ 0) : a ^ n = 0 ↔ a = 0 :=
-  ⟨zpow_eq_zero, fun ha => ha.symm ▸ zero_zpow _ hn⟩
+  ⟨eq_zero_of_zpow_eq_zero, fun ha => ha.symm ▸ zero_zpow _ hn⟩
 
 lemma zpow_ne_zero_iff {n : ℤ} (hn : n ≠ 0) : a ^ n ≠ 0 ↔ a ≠ 0 := (zpow_eq_zero_iff hn).ne
 
