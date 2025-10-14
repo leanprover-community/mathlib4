905c11e364b4aad9af7a0cad9299388893895238
Remy Degenne <remydegenne@gmail.com>
2025-10-14 13:46:31 +0200

    min_imports

--- Mathlib/MeasureTheory/Function/ConvergenceInDistribution.lean
+++ Mathlib/MeasureTheory/Function/ConvergenceInDistribution.lean
@@ -3,7 +3,7 @@ Copyright (c) 2025 Rémy Degenne. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Rémy Degenne
 -/
-import Mathlib.MeasureTheory.Measure.Portmanteau
+import Mathlib.MeasureTheory.Measure.ProbabilityMeasure
 
 /-!
 # Convergence in distribution
