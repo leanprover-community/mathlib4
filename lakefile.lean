d9429e79d107882f380af1601be400d602a186bc
Kim Morrison <kim@tqft.net>
2025-10-14 22:58:44 +1100

    turn off pythonStyle

--- lakefile.lean
+++ lakefile.lean
@@ -28,6 +28,7 @@ abbrev mathlibOnlyLinters : Array LeanOption := #[
   ⟨`linter.mathlibStandardSet, true⟩,
   ⟨`linter.checkInitImports, true⟩,
   ⟨`linter.allScriptsDocumented, true⟩,
+  ⟨`linter.pythonStyle, true⟩,
   ⟨`linter.style.longFile, .ofNat 1500⟩,
   -- ⟨`linter.nightlyRegressionSet, true⟩,
   -- `latest_import.yml` uses this comment: if you edit it, make sure that the workflow still works
