663c02c9a9d522cd6b7662d74f43c24052d8b763
yonggyuchoimath <yonggyuchoimath@gmail.com>
2025-10-15 17:30:47 +0000

    fix mistake in merging

--- Mathlib/CategoryTheory/Limits/Opposites.lean
+++ Mathlib/CategoryTheory/Limits/Opposites.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kim Morrison, Floris van Doorn
 -/
 import Mathlib.CategoryTheory.Limits.HasLimits
+import Mathlib.CategoryTheory.Limits.Shapes.FiniteLimits
 
 /-!
 # Limits in `C` give colimits in `Cᵒᵖ`.
