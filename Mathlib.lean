bc7f7b77e718aaa585d5cb725f63ce877d45b8ed
Xavier Roblot <46200072+xroblot@users.noreply.github.com>
2025-10-15 15:17:59 +0000

    chore(NumberField): follow-up to #30536 (#30583)
    Create the deprecated module files for the files moved in #30536

--- Mathlib.lean
+++ Mathlib.lean
@@ -4859,8 +4859,12 @@ import Mathlib.NumberTheory.ClassNumber.FunctionField
 import Mathlib.NumberTheory.Cyclotomic.Basic
 import Mathlib.NumberTheory.Cyclotomic.CyclotomicCharacter
 import Mathlib.NumberTheory.Cyclotomic.Discriminant
+import Mathlib.NumberTheory.Cyclotomic.Embeddings
 import Mathlib.NumberTheory.Cyclotomic.Gal
+import Mathlib.NumberTheory.Cyclotomic.PID
 import Mathlib.NumberTheory.Cyclotomic.PrimitiveRoots
+import Mathlib.NumberTheory.Cyclotomic.Rat
+import Mathlib.NumberTheory.Cyclotomic.Three
 import Mathlib.NumberTheory.Dioph
 import Mathlib.NumberTheory.DiophantineApproximation.Basic
 import Mathlib.NumberTheory.DiophantineApproximation.ContinuedFractions
---
+++ Mathlib/NumberTheory/Cyclotomic/Rat.lean
@@ -0,0 +1,3 @@
+import Mathlib.NumberTheory.NumberField.Cyclotomic.Basic
+
+deprecated_module (since := "2025-10-14")
