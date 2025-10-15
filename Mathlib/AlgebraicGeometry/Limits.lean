ff11cd51652709cdc648592c03b5933bfcb1fb7e
Yong-Gyu Choi <63726570+yonggyuchoimath@users.noreply.github.com>
2025-10-15 10:06:17 +0000

    chore(CategoryTheory/Limits/Opposites): split the file Limits.Opposites (#30517)
    This PR splits the file 
    - `Mathlib/CategoryTheory/Limits/Opposites.lean`
    
    into 
    
    - `Mathlib/CategoryTheory/Limits/Opposites.lean`
    - `Mathlib/CategoryTheory/Limits/Shapes/Opposites/`
    
    This was suggested in a review of #30200.
    
    Co-authored-by: yonggyuchoimath <yonggyuchoimath@gmail.com>

--- Mathlib/AlgebraicGeometry/Limits.lean
+++ Mathlib/AlgebraicGeometry/Limits.lean
@@ -3,6 +3,7 @@ Copyright (c) 2022 Andrew Yang. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Andrew Yang
 -/
+import Mathlib.CategoryTheory.Limits.Shapes.Opposites.Products
 import Mathlib.AlgebraicGeometry.Pullbacks
 import Mathlib.AlgebraicGeometry.AffineScheme
 
