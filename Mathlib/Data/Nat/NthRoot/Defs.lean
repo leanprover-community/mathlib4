349f2fc23f90aedf91faaff49af2270ededaafda
Yury Kudryashov <yury@harmonic.fun>
2025-10-16 18:32:31 +0000

    Import `Mathlib.Init`

--- Mathlib/Data/Nat/NthRoot/Defs.lean
+++ Mathlib/Data/Nat/NthRoot/Defs.lean
@@ -3,6 +3,7 @@ Copyright (c) 2025 Concordance Inc. dba Harmonic. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yury Kudryashov
 -/
+import Mathlib.Init
 
 /-!
 # Definition of `Nat.nthRoot`
