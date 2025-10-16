52eb14f9cce54cf1a835b471789c1b6615aea045
DavidLedvinka <50644608+DavidLedvinka@users.noreply.github.com>
2025-10-16 08:57:54 +0000

    feat(Topology): Add PairReduction.lean (#26243)
    Add file PairReduction.lean which proves the theorem `pair_reduction` which is needed for the proof of the general Kolmogorov-Chentsov theorem in the Brownian Motion project.
    
    
    - [x] depends on: #27239
    
    Co-authored-by: @RemyDegenne 
    Co-authored-by: Remy Degenne <remydegenne@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -6706,6 +6706,7 @@ import Mathlib.Topology.EMetricSpace.BoundedVariation
 import Mathlib.Topology.EMetricSpace.Defs
 import Mathlib.Topology.EMetricSpace.Diam
 import Mathlib.Topology.EMetricSpace.Lipschitz
+import Mathlib.Topology.EMetricSpace.PairReduction
 import Mathlib.Topology.EMetricSpace.Paracompact
 import Mathlib.Topology.EMetricSpace.Pi
 import Mathlib.Topology.ExtendFrom
