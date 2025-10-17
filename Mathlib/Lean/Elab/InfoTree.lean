f6aa268a2e130c3461e0f30b1244701aed553a13
Kim Morrison <kim@tqft.net>
2025-10-17 11:45:51 +1100

    add import Mathlib.Init

--- Mathlib/Lean/Elab/InfoTree.lean
+++ Mathlib/Lean/Elab/InfoTree.lean
@@ -3,6 +3,7 @@ Copyright (c) 2025 Marc Huisinga. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Marc Huisinga
 -/
+import Mathlib.Init
 import Lean.Server.InfoUtils
 import Lean.Meta.TryThis
 
