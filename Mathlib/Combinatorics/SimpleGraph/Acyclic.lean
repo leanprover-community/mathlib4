a2f6937f3253b84af50e8a99fbd8da6e51f6839f
LLaurance <laurancelau.2022@berkeley.edu>
2025-10-17 22:47:34 +0800

    Amend theorem parameters

--- Mathlib/Combinatorics/SimpleGraph/Acyclic.lean
+++ Mathlib/Combinatorics/SimpleGraph/Acyclic.lean
@@ -335,12 +335,12 @@ lemma Preconnected.connected_induce_complement_singleton_of_degree_eq_one [Decid
     use u
     aesop
 
-/-- A finite nontrivial connected graph contains a vertex that leaves the graph connected if
+/-- A finite nontrivial (pre)connected graph contains a vertex that leaves the graph connected if
 removed. -/
-lemma Connected.exists_vertex_connected_induce_complement_singleton_of_fintype_of_nontrivial
-    (hconn : G.Connected) [DecidableEq V] [Fintype V] [Nontrivial V] :
+lemma Preconnected.exists_vertex_connected_induce_complement_singleton_of_fintype_of_nontrivial
+    (hpreconn : G.Preconnected) [DecidableEq V] [Fintype V] [Nontrivial V] :
     ∃ v : V, (G.induce {v}ᶜ).Connected := by
-  obtain ⟨T, _, T_isTree⟩ := hconn.exists_isTree_le
+  obtain ⟨T, _, T_isTree⟩ := Connected.exists_isTree_le ⟨hpreconn⟩
   have ⟨hT, _⟩ := T_isTree
   have := Classical.decRel T.Adj
   obtain ⟨v, hv⟩ := T_isTree.exists_vert_degree_one_of_nontrivial
@@ -352,7 +352,7 @@ lemma Connected.exists_vertex_preconnected_induce_complement_singleton_of_fintyp
     (hconn : G.Connected) [DecidableEq V] [Fintype V] : ∃ v : V, (G.induce {v}ᶜ).Preconnected := by
   by_cases h : Nontrivial V
   · obtain ⟨v, hv⟩ :=
-      hconn.exists_vertex_connected_induce_complement_singleton_of_fintype_of_nontrivial
+      hconn.preconnected
     exact ⟨v, hv.preconnected⟩
   · use hconn.nonempty.some
     have := not_nontrivial_iff_subsingleton.mp h
