7f638fc1eeedcfd480824cf86ac09b74cc0a9d3c
Michael Rothgang <10105016+grunweg@users.noreply.github.com>
2025-10-18 20:48:21 +0000

    chore: use the `positivity [h, h']` syntax when useful (#30654)
    Since #30388, `have := foo; positivity` can be shortened to `positivity [foo]`.
    Make use of that when sensible.
    
    I searched for all occurrences of `have :.*\n\s*positi` (using VS Code, i.e. using `rg` internally) and inspected them manually. In a few cases, keeping the `have` separate seemed more readable to me.

--- Mathlib/Probability/Distributions/Geometric.lean
+++ Mathlib/Probability/Distributions/Geometric.lean
@@ -47,14 +47,12 @@ lemma geometricPMFRealSum (hp_pos : 0 < p) (hp_le_one : p ≤ 1) :
 lemma geometricPMFReal_pos {n : ℕ} (hp_pos : 0 < p) (hp_lt_one : p < 1) :
     0 < geometricPMFReal p n := by
   rw [geometricPMFReal]
-  have : 0 < 1 - p := sub_pos.mpr hp_lt_one
-  positivity
+  positivity [sub_pos.mpr hp_lt_one]
 
 lemma geometricPMFReal_nonneg {n : ℕ} (hp_pos : 0 < p) (hp_le_one : p ≤ 1) :
     0 ≤ geometricPMFReal p n := by
   rw [geometricPMFReal]
-  have : 0 ≤ 1 - p := sub_nonneg.mpr hp_le_one
-  positivity
+  positivity [sub_nonneg.mpr hp_le_one]
 
 /-- Geometric distribution with success probability `p`. -/
 noncomputable
