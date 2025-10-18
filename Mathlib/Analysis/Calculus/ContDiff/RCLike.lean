8d6ea74c732366dc99c1c4eb62ec850a7f48a2aa
Ben Eltschig <b.eltschig@protonmail.com>
2025-10-18 07:14:36 +0200

    fill in sorry

--- Mathlib/Analysis/Calculus/ContDiff/RCLike.lean
+++ Mathlib/Analysis/Calculus/ContDiff/RCLike.lean
@@ -147,30 +147,54 @@ lemma LipschitzOnWith.closure {K}
   sorry
 
 -- TODO: move to Lipschitz.lean
-/-- If `f` is Lipschitz on a collection of sets `sᵢ` with constants `Kᵢ`,
-it is also Lipschitz on `⋃ i, sᵢ` with constant `sup Kᵢ` (provided that supremum is finite). -/
-lemma LipschitzOnWith.sup {f : E → F} {ι : Type*} {s : ι → Set E} {K : ι → ℝ≥0} {K' : ℝ≥0}
-    (hf : ∀ i, LipschitzOnWith (K i) f (s i)) (hK' : ∀ i, K i ≤ K') :
-    LipschitzOnWith K' f (⋃ i, s i) := by
-  sorry
-
--- TODO: move to Lipschitz.lean
+open Metric in
 /-- If `f` is locally Lipschitz on a compact set `s`, it is Lipschitz on `s`. -/
-lemma LocallyLipschitzOn.exists_lipschitzOnWith_of_compact {f : E → F} (hs : IsCompact s)
+lemma LocallyLipschitzOn.exists_lipschitzOnWith_of_compact {α β : Type*} [PseudoMetricSpace α]
+    [PseudoMetricSpace β] {f : α → β} {s : Set α} (hs : IsCompact s)
     (hf : LocallyLipschitzOn s f) : ∃ K, LipschitzOnWith K f s := by
-  -- At each point `x`, you have a neighbourhood `tₓ`
-  -- on which `f` is Lipschitz with some constant `Kₓ`.
-  choose Kx tx htx hfx using hf
-  -- Finitely many of these cover `s` (by compactness).
-  obtain ⟨ti, hti⟩ := hs.elim_nhdsWithin_subcover' tx htx
-  let Ki' : ti → ℝ≥0 := fun ⟨i, _hi⟩ ↦ Kx i.2
-  -- Taking the maximum of the corresponding Kᵢ gives you a Lipschitz constant for `f` on `s`.
-  use iSup Ki'
-  have : s = ⋃ i ∈ ti, tx i.2 := sorry
-  simp_rw [this]
-  -- make this work
-  -- apply LipschitzOnWith.sup (f := f) (K' := iSup Ki') (ι := ti)--(K := Ki')
-  sorry
+  have hf' := hf.continuousOn
+  replace hf : ∀ x ∈ s, ∃ ε > 0, ∃ K, LipschitzOnWith K f (ball x ε ∩ s) := fun x hx ↦ by
+    let ⟨K, t, ht, hf⟩ := hf hx
+    let ⟨ε, hε, hε'⟩ := Metric.mem_nhdsWithin_iff.1 ht
+    exact ⟨ε, hε, K, hf.mono hε'⟩
+  choose ε hε K hf using hf
+  have : ∀ x (hx : x ∈ s), ∃ K' : ℝ≥0, ∀ y ∈ s.diff (ball x (ε x hx)),
+      edist (f x) (f y) ≤ K' * edist x y := fun x hx ↦ by
+    let ⟨K', hK'⟩ := (hs.diff isOpen_ball).bddAbove_image
+      (f := fun y ↦ dist (f x) (f y) / dist x y) <| .div (.mono (by fun_prop) s.diff_subset)
+        (by fun_prop) fun y hy ↦ ((hε x hx).trans_le <| not_lt.1 <| dist_comm x y ▸ hy.2).ne.symm
+    refine ⟨⟨K' ⊔ 0, le_sup_right⟩, fun y hy ↦ ?_⟩
+    rw [edist_nndist, edist_nndist, ← ENNReal.coe_mul, ENNReal.coe_le_coe]
+    refine (div_le_iff₀ ?_).1 ?_
+    · exact NNReal.coe_pos.1 <| coe_nndist x y ▸
+        ((hε x hx).trans_le <| not_lt.1 <| dist_comm x y ▸ hy.2)
+    · simp [← NNReal.coe_le_coe, (mem_upperBounds.1 hK') _ <| Set.mem_image_of_mem _ hy]
+  choose K' hK' using this
+  obtain ⟨t, ht⟩ := hs.elim_nhdsWithin_subcover' (fun x hx ↦ s ∩ ball x (ε x hx / 2))
+    (fun x hx ↦ inter_mem_nhdsWithin s <| ball_mem_nhds x <| half_pos <| hε x hx)
+  use t.sup fun i ↦ K _ i.2 + 2 * K' _ i.2
+  intro x hx y hy
+  let ⟨z, hz, hx'⟩ := mem_iUnion₂.1 <| ht hx
+  by_cases hy' : y ∈ ball z.1 (ε _ z.2)
+  · refine (hf _ z.2 ⟨hx'.2.trans <| half_lt_self <| hε _ z.2, hx⟩ ⟨hy', hy⟩).trans <|
+      mul_le_mul_of_nonneg_right ?_ <| zero_le _
+    exact ENNReal.coe_le_coe.2 <| t.le_sup_of_le hz <| le_self_add
+  · refine (edist_triangle_left _ _ (f z)).trans ?_
+    refine .trans ?_ <| mul_le_mul_of_nonneg_right (ENNReal.coe_le_coe.2 <| t.le_sup hz) (zero_le _)
+    refine (add_le_add (hf _ z.2 ⟨mem_ball_self <| hε _ z.2, z.2⟩ ⟨hx'.2.trans <| half_lt_self <|
+      hε _ z.2, hx⟩) <| hK' _ z.2 _ ⟨hy, hy'⟩).trans ?_
+    refine (add_le_add_left (mul_le_mul_of_nonneg_left (edist_triangle_left z.1 y x)
+      (zero_le _)) _).trans ?_
+    simp_rw [edist_dist, ENNReal.coe_nnreal_eq]
+    rw [← ENNReal.ofReal_mul, ← ENNReal.ofReal_mul, ← ENNReal.ofReal_add, ← ENNReal.ofReal_mul,
+      ← ENNReal.ofReal_add, NNReal.coe_add, NNReal.coe_mul, NNReal.coe_ofNat,
+      mul_add, ← add_assoc, dist_comm, ← add_mul]
+    · have h : dist x z ≤ dist x y := by
+        linarith [mem_ball.1 hx'.2, (not_lt (α := ℝ)).1 hy', dist_triangle_left y z x]
+      apply ENNReal.ofReal_le_ofReal
+      refine (add_le_add_right (mul_le_mul_of_nonneg_left h (by positivity)) _).trans ?_
+      linarith
+    all_goals positivity
 
 /-- If `f` is `C¹` on a convex compact set `s`, it is Lipschitz on `s`. -/
 theorem ContDiffOn.exists_lipschitzOnWith {s : Set E} {f : E → F} {n} (hf : ContDiffOn ℝ n f s)
