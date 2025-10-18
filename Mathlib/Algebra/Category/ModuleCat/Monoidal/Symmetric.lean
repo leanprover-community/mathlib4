0ef33b110377b41aaa72dc3ead7f8518905d5d78
Junyan Xu <junyanxu.math@gmail.com>
2025-10-18 02:28:38 +0200

    shake

--- Mathlib/Algebra/Category/ModuleCat/Monoidal/Symmetric.lean
+++ Mathlib/Algebra/Category/ModuleCat/Monoidal/Symmetric.lean
@@ -3,7 +3,7 @@ Copyright (c) 2020 Kim Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kevin Buzzard, Kim Morrison, Jakob von Raumer
 -/
-import Mathlib.CategoryTheory.Monoidal.Braided.Transport
+import Mathlib.CategoryTheory.Monoidal.Braided.Basic
 import Mathlib.Algebra.Category.ModuleCat.Monoidal.Basic
 
 /-!
