9dfc84a3399e467d4baf937f682863d510fd239e
Snir Broshi <26556598+SnirBroshi@users.noreply.github.com>
2025-10-19 08:49:01 +0300

    feat(Combinatorics/SimpleGraph/Hamiltonian): golf `getVert_surjective`

--- Mathlib/Combinatorics/SimpleGraph/Hamiltonian.lean
+++ Mathlib/Combinatorics/SimpleGraph/Hamiltonian.lean
@@ -77,9 +77,7 @@ theorem isHamiltonian_iff_support_get_bijective : p.IsHamiltonian ↔ (p.support
 theorem IsHamiltonian.getVert_surjective (hp : p.IsHamiltonian) : (p.getVert ·).Surjective := by
   intro v
   have ⟨n, h⟩ := List.mem_iff_get.mp <| p.support.one_le_count_iff.mp <| le_of_eq <| hp v |>.symm
-  refine ⟨n, p.getVert_eq_support_getElem ?_ |>.trans h⟩
-  have := p.length_support
-  cutsat
+  exact ⟨n, h ▸ p.getVert_eq_support_getElem (by grind [length_support])⟩
 
 /-- A Hamiltonian cycle is a cycle that visits every vertex once. -/
 structure IsHamiltonianCycle (p : G.Walk a a) : Prop extends p.IsCycle where
