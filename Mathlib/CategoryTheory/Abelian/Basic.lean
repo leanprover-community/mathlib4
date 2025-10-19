c05480e856804b54648536f622602d457657a518
mm2420 <38689041+Kiolt@users.noreply.github.com>
2025-10-19 15:27:01 +0000

    feat(CategoryTheory/Abelian): iso between image and coimage functors (#30673)
    From ClassFieldTheory
    
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>
    Co-authored-by: Kiolt <kiolterino@gmail.com>

--- Mathlib/CategoryTheory/Abelian/Basic.lean
+++ Mathlib/CategoryTheory/Abelian/Basic.lean
@@ -401,6 +401,32 @@ theorem factorThruImage_comp_coimageIsoImage'_inv :
   simp only [IsImage.isoExt_inv, image.isImage_lift, image.fac_lift,
     coimageStrongEpiMonoFactorisation_e]
 
+variable {Z : C} (g : Y ⟶ Z)
+
+@[simp] lemma image.ι_comp_eq_zero : image.ι f ≫ g = 0 ↔ f ≫ g = 0 := by
+  simp [← cancel_epi (Abelian.factorThruImage _)]
+
+@[simp] lemma coimage.comp_π_eq_zero : f ≫ coimage.π g = 0 ↔ f ≫ g = 0 := by
+  simp [← cancel_mono (Abelian.factorThruCoimage _)]
+
+/-- `Abelian.image` as a functor from the arrow category. -/
+@[simps]
+def imageFunctor : Arrow C ⥤ C where
+  obj f := Abelian.image f.hom
+  map {f g} u := kernel.lift _ (Abelian.image.ι f.hom ≫ u.right) <| by simp [← Arrow.w_assoc u]
+
+/-- `Abelian.coimage` as a functor from the arrow category. -/
+@[simps]
+def coimageFunctor : Arrow C ⥤ C where
+  obj f := Abelian.coimage f.hom
+  map {f g} u := cokernel.desc _ (u.left ≫ Abelian.coimage.π g.hom) <| by
+    simp [← Category.assoc, coimage.comp_π_eq_zero]; simp
+
+/-- The image and coimage of an arrow are naturally isomorphic. -/
+@[simps!]
+def coimageFunctorIsoImageFunctor : coimageFunctor (C := C) ≅ imageFunctor :=
+  NatIso.ofComponents fun _ ↦ Abelian.coimageIsoImage _
+
 /-- There is a canonical isomorphism between the abelian image and the categorical image of a
     morphism. -/
 abbrev imageIsoImage : Abelian.image f ≅ image f :=
