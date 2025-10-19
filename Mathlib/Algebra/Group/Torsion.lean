dae72e012ddd83513cd78039a975a334c96845fb
Yaël Dillies <yael.dillies@gmail.com>
2025-10-19 15:30:02 +0000

    deprecated aliases

--- Mathlib/Algebra/Group/Torsion.lean
+++ Mathlib/Algebra/Group/Torsion.lean
@@ -47,6 +47,12 @@ lemma IsMulTorsionFree.pow_eq_one_iff : a ^ n = 1 ↔ a = 1 ∨ n = 0 := by
 @[to_additive IsAddTorsionFree.nsmul_eq_zero_iff_left]
 lemma IsMulTorsionFree.pow_eq_one_iff_right (ha : a ≠ 1) : a ^ n = 1 ↔ n = 0 := by simp [*]
 
+@[deprecated (since := "2025-10-19")]
+alias IsAddTorsionFree.nsmul_eq_zero_iff' := IsAddTorsionFree.nsmul_eq_zero_iff_left
+
+@[deprecated (since := "2025-10-19")]
+alias IsMulTorsionFree.pow_eq_one_iff' := IsMulTorsionFree.pow_eq_one_iff_right
+
 /-- See `sq_eq_one_iff` for a version that holds in rings. -/
 @[to_additive two_nsmul_eq_zero]
 lemma sq_eq_one : a ^ 2 = 1 ↔ a = 1 := IsMulTorsionFree.pow_eq_one_iff_left (by cutsat)
