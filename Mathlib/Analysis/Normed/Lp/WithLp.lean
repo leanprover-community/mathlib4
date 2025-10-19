5ee3fe74b65e1b2c6e01a1af83ee911bcaee8886
Etienne Marion <baguettes_casino0c@icloud.com>
2025-10-19 16:23:37 +0200

    better phrasing

--- Mathlib/Analysis/Normed/Lp/WithLp.lean
+++ Mathlib/Analysis/Normed/Lp/WithLp.lean
@@ -55,7 +55,7 @@ section Notation
 
 open Lean.PrettyPrinter.Delaborator
 
-/-- This avoids `toLp p x` to be printed `{ ofLp := x }`. -/
+/-- This avoids `toLp p x` being printed as `{ ofLp := x }`. -/
 @[app_delab WithLp.toLp]
 def WithLp.delabToLp : Delab := delabApp
 
