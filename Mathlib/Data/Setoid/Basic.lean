1a84573dc8a75373ba7231b3f53307bb61d1dd51
Snir Broshi <26556598+SnirBroshi@users.noreply.github.com>
2025-10-19 09:00:50 +0300

    chore(Data/Setoid/Basic): remove unused import

--- Mathlib/Data/Setoid/Basic.lean
+++ Mathlib/Data/Setoid/Basic.lean
@@ -6,7 +6,6 @@ Authors: Amelia Livingston, Bryan Gin-ge Chen
 import Mathlib.Logic.Relation
 import Mathlib.Order.CompleteLattice.Basic
 import Mathlib.Order.GaloisConnection.Defs
-import Mathlib.Tactic.NthRewrite
 
 /-!
 # Equivalence relations
