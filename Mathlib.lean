1f3931b529d110e02d5231762a2f29896fce86a7
bjornsolheim <bjorn.solheim@protonmail.com>
2025-10-20 16:50:12 +0200

    Removed ConicalCombination.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -3875,7 +3875,6 @@ import Mathlib.FieldTheory.SplittingField.Construction
 import Mathlib.FieldTheory.SplittingField.IsSplittingField
 import Mathlib.FieldTheory.Tower
 import Mathlib.Geometry.Convex.Cone.Basic
-import Mathlib.Geometry.Convex.Cone.ConicalCombination
 import Mathlib.Geometry.Convex.Cone.Dual
 import Mathlib.Geometry.Convex.Cone.Pointed
 import Mathlib.Geometry.Convex.Cone.TensorProduct
