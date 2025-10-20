1ae39c080b7caff1782cf9835afa5e68552e9cb4
Arend Mellendijk <arend.mellendijk@gmail.com>
2025-10-20 07:18:16 +0200

    copyright

--- Mathlib/Tactic/Polynomial/Core.lean
+++ Mathlib/Tactic/Polynomial/Core.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2025 Arend Mellendijk. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Arend Mellendijk
+-/
 import Mathlib.Tactic.Algebra
 
 /-!
