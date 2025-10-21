4bb8bd2c1cf88ff488e4932f9a909eb83bf157a8
Kim Morrison <kim@tqft.net>
2025-10-21 05:33:04 +0200

    lake update importGraph (move to module system)

--- lakefile.lean
+++ lakefile.lean
@@ -14,7 +14,7 @@ require "leanprover-community" / "proofwidgets" @ git "v0.0.76-pre" -- ProofWidg
     "ProofWidgets not up-to-date. \
     Please run `lake exe cache get` to fetch the latest ProofWidgets. \
     If this does not work, report your issue on the Lean Zulip."
-require "leanprover-community" / "importGraph" @ git "nightly-testing"
+require "leanprover-community" / "importGraph" @ git "main"
 require "leanprover-community" / "LeanSearchClient" @ git "main"
 require "leanprover-community" / "plausible" @ git "main"
 
