da38783214281518f21542a156fc52e9edca6775
Joël Riou <37772949+joelriou@users.noreply.github.com>
2025-10-21 11:51:10 +0000

    feat(AlgebraicTopology): the type of nondegenerate simplices not in a given subcomplex (#28330)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1374,6 +1374,7 @@ import Mathlib.AlgebraicTopology.SimplicialSet.Nerve
 import Mathlib.AlgebraicTopology.SimplicialSet.NerveAdjunction
 import Mathlib.AlgebraicTopology.SimplicialSet.NerveNondegenerate
 import Mathlib.AlgebraicTopology.SimplicialSet.NonDegenerateSimplices
+import Mathlib.AlgebraicTopology.SimplicialSet.NonDegenerateSimplicesSubcomplex
 import Mathlib.AlgebraicTopology.SimplicialSet.Op
 import Mathlib.AlgebraicTopology.SimplicialSet.Path
 import Mathlib.AlgebraicTopology.SimplicialSet.Simplices
