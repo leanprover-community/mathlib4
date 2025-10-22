ad15f92b44b60478e9fcf75294ed258a9ae98ecc
Javier Burroni <jburroni@users.noreply.github.com>
2025-10-22 15:49:30 -0400

    Apply suggestions from code review
    Co-authored-by: Kenny Lau <kc_kennylau@yahoo.com.hk>

--- Mathlib/Data/PNat/Order.lean
+++ Mathlib/Data/PNat/Order.lean
@@ -3,10 +3,8 @@ Copyright (c) 2025 Javier Burroni. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Javier Burroni
 -/
-import Mathlib.Data.PNat.Defs
-import Mathlib.Data.PNat.Basic
-import Mathlib.Order.SuccPred.Basic
 import Mathlib.Algebra.Order.SuccPred
+import Mathlib.Data.PNat.Basic
 
 /-!
 # Order related instances for `ℕ+`
@@ -16,7 +14,7 @@ namespace PNat
 open Nat
 
 instance instSuccOrder : SuccOrder ℕ+ :=
-  SuccOrder.ofSuccLeIff (fun n ↦ n + 1) (by rfl)
+  .ofSuccLeIff (· + 1) Iff.rfl
 
 instance instSuccAddOrder : SuccAddOrder ℕ+ where
   succ_eq_add_one _ := rfl
