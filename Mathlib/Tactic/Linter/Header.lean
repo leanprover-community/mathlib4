05d1bef1e5c0974722ed6d3aa9154f5a167fdb75
Bryan Gin-ge Chen <bryangingechen@gmail.com>
2025-10-22 10:58:34 -0400

    add some whitespace to hopefully fix the broken cache file

--- Mathlib/Tactic/Linter/Header.lean
+++ Mathlib/Tactic/Linter/Header.lean
@@ -22,6 +22,7 @@ import statements*
 module doc-string*
 remaining file
 ```
+
 It emits a warning if
 * the copyright statement is malformed;
 * `Mathlib.Tactic` is imported;
@@ -31,6 +32,7 @@ It emits a warning if
 The linter allows `import`-only files and does not require a copyright statement in `Mathlib.Init`.
 
 ## Implementation
+
 The strategy used by the linter is as follows.
 The linter computes the end position of the first module doc-string of the file,
 resorting to the end of the file, if there is no module doc-string.
