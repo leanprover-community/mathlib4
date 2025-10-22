4f0eade01e6f8ea907c6d18fddae9a1c211cdb5b
Adam Topaz <adamtopaz@users.noreply.github.com>
2025-10-22 15:24:30 -0600

    add test; fix bug.

--- Mathlib/Tactic/CategoryTheory/CategoryStar.lean
+++ Mathlib/Tactic/CategoryTheory/CategoryStar.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Adam Topaz
 -/
 import Mathlib.CategoryTheory.Category.Basic
+import Mathlib.Lean.Level
 
 /-!
 # Support for `Category* C`.
@@ -19,13 +20,15 @@ because it makes it easier to specify explicit universes when needed.
 
 The elaborator `Category*` provides analogous behavior to `Type*` for introducing category
 instances. The term elaborator `Category* C` creates a universe parameter `v`, and places it just
-*before* any universe parameters appearing in `C`. If `C` has no parameters, `v` is becomes the
-last level parameter. The elaborated term is then `Category.{v} C`.
+*before* any universe parameters appearing in `C` and its type. If `C` and its type have no
+parameters, `v` is becomes the last level parameter. The elaborated term is then `Category.{v} C`.
 
 Basic usage:
 ```lean
 variable (C : Type*) [Category* C]
 ```
+
+Note: `Category* C` expects both `C` and its type to *not involve* any level mvars.
 -/
 
 open Lean Meta Elab Term
@@ -33,18 +36,23 @@ open CategoryTheory
 
 /--
 The syntax `Category* C` creates a new distinct implicit universe parameter `v`, placed
-just before any parameters appearing in `C`, and elaborates to `Category.{v} C`.
+just before any parameters appearing in `C` and its type, and elaborates to `Category.{v} C`.
 -/
-elab "Category*" ppSpace C:term : term => do
+elab "Category*" ppSpace C:term : term => commitIfNoEx do
   let u ← mkFreshLevelMVar
-  let v ← mkFreshLevelMVar
-  let C ← instantiateExprMVars <| ← elabTermEnsuringType C (some <| .sort <| .succ u)
-  let instTp := .app (.const ``Category [v,u]) C
+  let cExpr ← instantiateExprMVars <| ← elabTermEnsuringType C (some <| .sort <| .succ u)
+  if cExpr.hasLevelMVar then
+    throwError "The term{indentD cExpr}\nhas level mvars"
+  let tpCExpr ← instantiateExprMVars <| ← Meta.inferType cExpr
+  if tpCExpr.hasLevelMVar then
+    throwError "The type{indentD tpCExpr}\nof{indentD cExpr}\nhas level mvars"
+  let instTp ← instantiateMVars <| .app (.const ``Category [← mkFreshLevelMVar, u]) cExpr
   let levelNames ← getLevelNames
   let ⟨mctx, vs, _, out⟩ :=
     (← getMCtx).levelMVarToParam (fun n => levelNames.elem n) (fun _ => false) instTp `v 1
-  let v::[] := vs.toList | throwError "The term{indentD C}\nhas level mvars"
-  let us := (collectLevelParams {} C).params
+  let v::[] := vs.toList
+    | throwError "Unexpected Error:{indentD out}\ndoesn't have exactly one new level parameter"
+  let us := (collectLevelParams {} cExpr).params ++ (collectLevelParams {} tpCExpr).params
   match (us.filterMap fun nm => levelNames.findIdx? fun x => x == nm).max? with
   | some idx => setLevelNames <| levelNames.insertIdx (idx + 1) v
   | none => setLevelNames <| v :: levelNames
