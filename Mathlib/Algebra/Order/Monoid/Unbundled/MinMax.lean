aea0b2af57ac2a42f73f293327c27ca5d2819e31
Snir Broshi <26556598+SnirBroshi@users.noreply.github.com>
2025-10-22 13:58:44 +0300

    chore(Algebra/Order/Monoid/Unbundled/MinMax): revert golf of `mul_lt_mul_iff_of_le_of_le` until grind is stable

--- Mathlib/Algebra/Order/Monoid/Unbundled/MinMax.lean
+++ Mathlib/Algebra/Order/Monoid/Unbundled/MinMax.lean
@@ -103,7 +103,10 @@ theorem mul_lt_mul_iff_of_le_of_le [MulLeftMono α]
     [MulRightMono α] [MulLeftStrictMono α]
     [MulRightStrictMono α] {a₁ a₂ b₁ b₂ : α} (ha : a₁ ≤ a₂)
     (hb : b₁ ≤ b₂) : a₁ * b₁ < a₂ * b₂ ↔ a₁ < a₂ ∨ b₁ < b₂ := by
-  grind [lt_or_lt_of_mul_lt_mul, mul_lt_mul_of_lt_of_le, mul_lt_mul_of_le_of_lt]
+  refine ⟨lt_or_lt_of_mul_lt_mul, fun h => ?_⟩
+  rcases h with ha' | hb'
+  · exact mul_lt_mul_of_lt_of_le ha' hb
+  · exact mul_lt_mul_of_le_of_lt ha hb'
 
 end Mul
 
