1b1ef80470f9ea431cb80427742af3a769e26de0
tb65536 <thomas.l.browning@gmail.com>
2025-10-21 18:37:20 -0700

    fix imports

--- Mathlib/Topology/Algebra/Group/SubmonoidClosure.lean
+++ Mathlib/Topology/Algebra/Group/SubmonoidClosure.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yury Kudryashov
 -/
 
+import Mathlib.Algebra.Group.Subgroup.ZPowers.Basic
 import Mathlib.Order.Filter.AtTopBot.Group
 import Mathlib.Topology.Algebra.Group.Basic
 
