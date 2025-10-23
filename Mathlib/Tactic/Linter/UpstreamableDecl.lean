10b4d4c513de3258cbad6399df84c6bcc6b257e5
Bryan Gin-ge Chen <bryangingechen@gmail.com>
2025-10-23 04:05:01 -0400

    chore: revert #30788 and #30785 (#30807)

--- Mathlib/Tactic/Linter/UpstreamableDecl.lean
+++ Mathlib/Tactic/Linter/UpstreamableDecl.lean
@@ -3,7 +3,7 @@ Copyright (c) 2024 Damiano Testa. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Damiano Testa, Anne Baanen
 -/
-import ImportGraph.Imports
+import ImportGraph.Meta
 import Mathlib.Init
 
 /-! # The `upstreamableDecl` linter
