1b54977a79ce843b225d570350233afcef5b831d
Yaël Dillies <yael.dillies@gmail.com>
2025-10-23 12:43:21 +0200

    golf

--- Mathlib/Topology/LocallyFinsupp.lean
+++ Mathlib/Topology/LocallyFinsupp.lean
@@ -307,7 +307,10 @@ instance [SemilatticeInf Y] [Zero Y] : Min (locallyFinsuppWithin U Y) where
 lemma min_apply [SemilatticeInf Y] [Zero Y] {D₁ D₂ : locallyFinsuppWithin U Y} {x : X} :
     min D₁ D₂ x = min (D₁ x) (D₂ x) := rfl
 
-instance [Lattice Y] [Zero Y] : Lattice (locallyFinsuppWithin U Y) where
+section Lattice
+variable [Lattice Y]
+
+instance [Zero Y] : Lattice (locallyFinsuppWithin U Y) where
   le := (· ≤ ·)
   lt := (· < ·)
   le_refl := by simp [le_def]
@@ -324,19 +327,20 @@ instance [Lattice Y] [Zero Y] : Lattice (locallyFinsuppWithin U Y) where
   inf_le_right D₁ D₂ := fun x ↦ by simp
   le_inf D₁ D₂ D₃ h₁₃ h₂₃ := fun x ↦ by simp [h₁₃ x, h₂₃ x]
 
-@[simp]
-lemma posPart_apply [AddCommGroup Y] [LinearOrder Y] (a : locallyFinsuppWithin U Y) (x : X) :
-    a⁺ x = (a x)⁺ := rfl
+variable [AddCommGroup Y]
 
-@[simp]
-lemma negPart_apply [AddCommGroup Y] [LinearOrder Y] (a : locallyFinsuppWithin U Y) (x : X) :
-    a⁻ x = (a x)⁻ := rfl
+@[simp] lemma posPart_apply (a : locallyFinsuppWithin U Y) (x : X) : a⁺ x = (a x)⁺ := rfl
+@[simp] lemma negPart_apply (a : locallyFinsuppWithin U Y) (x : X) : a⁻ x = (a x)⁻ := rfl
+
+end Lattice
+
+section LinearOrder
+variable [AddCommGroup Y] [LinearOrder Y] [IsOrderedAddMonoid Y]
 
 /--
 Functions with locally finite support within `U` form an ordered commutative group.
 -/
-instance [AddCommGroup Y] [LinearOrder Y] [IsOrderedAddMonoid Y] :
-    IsOrderedAddMonoid (locallyFinsuppWithin U Y) where
+instance : IsOrderedAddMonoid (locallyFinsuppWithin U Y) where
   add_le_add_left := fun _ _ _ _ ↦ by simpa [le_def]
 
 /--
@@ -344,12 +348,9 @@ Taking the positive part of a function with locally finite support commutes with
 scalar multiplication by a natural number.
 -/
 @[simp]
-theorem nsmul_posPart [AddCommGroup Y] [LinearOrder Y] [IsOrderedAddMonoid Y]
-    (f : locallyFinsuppWithin U Y) (n : ℕ) :
-    (n • f)⁺ = n • f⁺ := by
-  unfold instPosPart
+theorem nsmul_posPart (f : locallyFinsuppWithin U Y) (n : ℕ) : (n • f)⁺ = n • f⁺ := by
   ext x
-  simp only [coe_nsmul, Pi.smul_apply, max_apply, coe_zero, Pi.zero_apply]
+  simp only [posPart, max_apply, coe_nsmul, Pi.smul_apply, coe_zero, Pi.zero_apply]
   by_cases h : f x < 0
   · simpa [max_eq_right_of_lt h] using nsmul_le_nsmul_right h.le n
   · simpa [not_lt.1 h] using nsmul_nonneg (not_lt.1 h) n
@@ -359,15 +360,15 @@ Taking the negative part of a function with locally finite support commutes with
 scalar multiplication by a natural number.
 -/
 @[simp]
-theorem nsmul_negPart [AddCommGroup Y] [LinearOrder Y] [IsOrderedAddMonoid Y]
-    (f : locallyFinsuppWithin U Y) (n : ℕ) :
-    (n • f)⁻ = n • f⁻ := by
-  unfold instNegPart
+theorem nsmul_negPart (f : locallyFinsuppWithin U Y) (n : ℕ) : (n • f)⁻ = n • f⁻ := by
   ext x
-  simp only [coe_nsmul, Pi.smul_apply, max_apply, coe_neg, Pi.neg_apply, coe_zero, Pi.zero_apply]
+  simp only [negPart, max_apply, coe_neg, coe_nsmul, Pi.neg_apply, Pi.smul_apply, coe_zero,
+    Pi.zero_apply]
   by_cases h : -f x < 0
   · simpa [max_eq_right_of_lt h] using nsmul_le_nsmul_right h.le n
   · simpa [not_lt.1 h] using nsmul_nonneg (not_lt.1 h) n
+
+end LinearOrder
 
 /-!
 ## Restriction
