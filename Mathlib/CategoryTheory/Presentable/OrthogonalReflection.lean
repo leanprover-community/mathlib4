bf5e3fe71967dbe90282afe4012c00385fc384a6
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-10-23 17:22:56 +0200

    better imports

--- Mathlib/CategoryTheory/Presentable/OrthogonalReflection.lean
+++ Mathlib/CategoryTheory/Presentable/OrthogonalReflection.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joël Riou
 -/
 import Mathlib.CategoryTheory.Limits.Shapes.Multiequalizer
-import Mathlib.CategoryTheory.Limits.Shapes.RegularMono
 import Mathlib.CategoryTheory.Localization.Bousfield
 import Mathlib.CategoryTheory.SmallObject.Iteration.Basic
 
