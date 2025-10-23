c650aa7fdb81085f82f8c6f3257a781bd17e4e3b
Adam Topaz <adamtopaz@users.noreply.github.com>
2025-10-22 19:13:03 -0600

    Update CategoryStar.lean

--- Mathlib/Tactic/CategoryTheory/CategoryStar.lean
+++ Mathlib/Tactic/CategoryTheory/CategoryStar.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Adam Topaz
 -/
 import Mathlib.CategoryTheory.Category.Basic
-import Mathlib.Lean.Level
 
 /-!
 # Support for `Category* C`.
