48f06957e2677f32d3fc6eb3b068abd704bccf62
Michael Rothgang <10105016+grunweg@users.noreply.github.com>
2025-10-23 00:20:02 +0000

    style(misc): fix whitespace (#30691)
    Extracted from #30658. Found by extending the commandStart linter to proof bodies.
    
    Co-authored-by: adomani <adomani@gmail.com>

--- Mathlib/LinearAlgebra/CliffordAlgebra/Prod.lean
+++ Mathlib/LinearAlgebra/CliffordAlgebra/Prod.lean
@@ -50,8 +50,7 @@ then the product of the embedding in `CliffordAlgebra Q` commutes up to a sign f
 nonrec theorem map_mul_map_of_isOrtho_of_mem_evenOdd
     {i₁ i₂ : ZMod 2} (hm₁ : m₁ ∈ evenOdd Q₁ i₁) (hm₂ : m₂ ∈ evenOdd Q₂ i₂) :
     map f₁ m₁ * map f₂ m₂ = (-1 : ℤˣ) ^ (i₂ * i₁) • (map f₂ m₂ * map f₁ m₁) := by
-  -- the strategy; for each variable, induct on powers of `ι`, then on the exponent of each
-  -- power.
+  -- for each variable, induct on powers of `ι`, then on the exponent of each power
   induction hm₁ using Submodule.iSup_induction' with
   | zero => rw [map_zero, zero_mul, mul_zero, smul_zero]
   | add _ _ _ _ ihx ihy => rw [map_add, add_mul, mul_add, ihx, ihy, smul_add]
@@ -65,7 +64,7 @@ nonrec theorem map_mul_map_of_isOrtho_of_mem_evenOdd
       rw [map_add, add_mul, mul_add, ihx, ihy, smul_add]
     | mem_mul m₁ hm₁ i x₁ _hx₁ ih₁ =>
       obtain ⟨v₁, rfl⟩ := hm₁
-      -- this is the first interesting goal
+      -- This is the first interesting goal.
       rw [map_mul, mul_assoc, ih₁, mul_smul_comm, map_apply_ι, Nat.cast_succ, mul_add_one,
         uzpow_add, mul_smul, ← mul_assoc, ← mul_assoc, ← smul_mul_assoc ((-1) ^ i₂)]
       clear ih₁
@@ -84,7 +83,7 @@ nonrec theorem map_mul_map_of_isOrtho_of_mem_evenOdd
           rw [map_add, add_mul, mul_add, ihx, ihy, smul_add]
         | mem_mul m₂ hm₂ i x₂ _hx₂ ih₂ =>
           obtain ⟨v₂, rfl⟩ := hm₂
-          -- this is the second interesting goal
+          -- This is the second interesting goal.
           rw [map_mul, map_apply_ι, Nat.cast_succ, ← mul_assoc,
             ι_mul_ι_comm_of_isOrtho (hf _ _), neg_mul, mul_assoc, ih₂, mul_smul_comm,
             ← mul_assoc, ← Units.neg_smul, uzpow_add, uzpow_one, mul_neg_one]
@@ -101,7 +100,7 @@ theorem commute_map_mul_map_of_isOrtho_of_mem_evenOdd_zero_right
 
 theorem map_mul_map_eq_neg_of_isOrtho_of_mem_evenOdd_one
     (hm₁ : m₁ ∈ evenOdd Q₁ 1) (hm₂ : m₂ ∈ evenOdd Q₂ 1) :
-    map f₁ m₁ * map f₂ m₂ = - map f₂ m₂ * map f₁ m₁ := by
+    map f₁ m₁ * map f₂ m₂ = -map f₂ m₂ * map f₁ m₁ := by
   simp [map_mul_map_of_isOrtho_of_mem_evenOdd _ _ hf _ _ hm₁ hm₂]
 
 end map_mul_map
--- Mathlib/Probability/ProbabilityMassFunction/Binomial.lean
+++ Mathlib/Probability/ProbabilityMassFunction/Binomial.lean
@@ -27,7 +27,7 @@ def binomial (p : ℝ≥0) (h : p ≤ 1) (n : ℕ) : PMF (Fin (n + 1)) :=
       ↑(p ^ (i : ℕ) * (1 - p) ^ ((Fin.last n - i) : ℕ) * (n.choose i : ℕ))) (by
     dsimp only
     norm_cast
-    convert (add_pow p (1-p) n).symm
+    convert (add_pow p (1 - p) n).symm
     · rw [Finset.sum_fin_eq_sum_range]
       apply Finset.sum_congr rfl
       intro i hi
