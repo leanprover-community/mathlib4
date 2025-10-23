bbc7a20af2ea05119411c61798be92a2ed780a9b
Yuyang Zhao 赵雨扬 <zhao.yu-yang@foxmail.com>
2025-10-23 22:14:28 +0800

    fix

--- Mathlib/Topology/Instances/Rat.lean
+++ Mathlib/Topology/Instances/Rat.lean
@@ -3,6 +3,7 @@ Copyright (c) 2017 Johannes Hölzl. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl, Mario Carneiro
 -/
+import Mathlib.Algebra.Algebra.Rat
 import Mathlib.Algebra.Module.Rat
 import Mathlib.Data.NNRat.Order
 import Mathlib.Topology.Algebra.Order.Archimedean
