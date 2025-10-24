0ceabe55798a2f695d200b3101a5472e21b21823
Thomas Browning <13339017+tb65536@users.noreply.github.com>
2025-10-24 10:20:09 +0000

    chore(GroupTheory/Index): move awkwardly placed lemmas (#30832)
    The lemmas `Subgroup.toSubmonoid_zpowers` and `Submonoid.powers_le_zpowers` were awkwardly placed at the top of `Index.lean` despite being unrelated to the contents of that file. I have moved them to directly afterward `closure_toSubmonoid` which is thematically similar and also the key ingredient in the proof.
    
    Co-authored-by: tb65536 <thomas.l.browning@gmail.com>

--- Mathlib/GroupTheory/Index.lean
+++ Mathlib/GroupTheory/Index.lean
@@ -38,18 +38,6 @@ Several theorems proved in this file are known as Lagrange's theorem.
 
 assert_not_exists Field
 
-@[to_additive]
-lemma Subgroup.toSubmonoid_zpowers {G : Type*} [Group G] (g : G) :
-    (Subgroup.zpowers g).toSubmonoid = Submonoid.powers g ⊔ Submonoid.powers g⁻¹ := by
-  rw [zpowers_eq_closure, closure_toSubmonoid, Submonoid.closure_union, Submonoid.powers_eq_closure,
-    Submonoid.powers_eq_closure, Set.inv_singleton]
-
-@[to_additive]
-lemma Submonoid.powers_le_zpowers {G : Type*} [Group G] (g : G) :
-    Submonoid.powers g ≤ (Subgroup.zpowers g).toSubmonoid := by
-  rw [Subgroup.toSubmonoid_zpowers]
-  exact le_sup_left
-
 open scoped Pointwise
 
 namespace Subgroup
