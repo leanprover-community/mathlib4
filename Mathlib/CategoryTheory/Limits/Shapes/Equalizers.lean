a4e5d9eda6911edec158e45169845b459fb1ca23
Fernando Chu <17756312+FernandoChu@users.noreply.github.com>
2025-10-24 14:53:52 +0200

    Update Equalizers.lean

--- Mathlib/CategoryTheory/Limits/Shapes/Equalizers.lean
+++ Mathlib/CategoryTheory/Limits/Shapes/Equalizers.lean
@@ -856,8 +856,8 @@ section
 variable {f g : X ⟶ Y} {Z : C} (h : Z ⟶ X)
 
 /--
-Given a fork `s` on morphisms `f, g : X ⟶ Y`, and a pullback cone `c` on `s.ι : s.pt ⟶ X` and
-`h : Z ⟶ X`, the projection `c.snd : c.pt ⟶ Z` induces a fork on `h ≫ f` and `h ≫ g`.
+Given a fork `s` on morphisms `f, g : X ⟶ Y` and a pullback cone `c` on `s.ι : s.pt ⟶ X` and a
+morphism `h : Z ⟶ X`, the projection `c.snd : c.pt ⟶ Z` induces a fork on `h ≫ f` and `h ≫ g`.
 -/
 def precompFork (s : Fork f g) (c : PullbackCone s.ι h) :
     Fork (h ≫ f) (h ≫ g) :=
