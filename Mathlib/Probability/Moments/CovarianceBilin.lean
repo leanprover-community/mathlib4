6e4386dffa82d976ac19907066a25ef7c44b41d4
Etienne Marion <baguettes_casino0c@icloud.com>
2025-10-25 00:46:39 +0200

    imports

--- Mathlib/Probability/Moments/CovarianceBilin.lean
+++ Mathlib/Probability/Moments/CovarianceBilin.lean
@@ -3,7 +3,6 @@ Copyright (c) 2025 Rémy Degenne. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Rémy Degenne, Etienne Marion
 -/
-import Mathlib.Analysis.InnerProductSpace.Dual
 import Mathlib.Analysis.InnerProductSpace.Positive
 import Mathlib.MeasureTheory.SpecificCodomains.WithLp
 import Mathlib.Probability.Moments.CovarianceBilinDual
