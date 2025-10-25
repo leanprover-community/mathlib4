d70f340e603a99d323103a394be6b3ee3b7f562c
Hagb (Junyu Guo 郭俊余) <hagbgreen@yahoo.com>
2025-10-25 14:30:45 +0800

    Apply suggestions from code review
    Co-authored-by: Christian Merten <christian@merten.dev>

--- Mathlib/RingTheory/MvPolynomial/MonomialOrder.lean
+++ Mathlib/RingTheory/MvPolynomial/MonomialOrder.lean
@@ -119,7 +119,7 @@ if its leading coefficient (for that monomial order) is 1. -/
 def Monic (f : MvPolynomial σ R) : Prop :=
   m.leadingCoeff f = 1
 
-/-- the leading term of of a multivariate polynomial with respect to a monomial ordering -/
+/-- The leading term of of a multivariate polynomial with respect to a monomial ordering. -/
 noncomputable def leadingTerm (f : MvPolynomial σ R) : MvPolynomial σ R :=
   monomial (m.degree f) (m.leadingCoeff f)
 
@@ -623,6 +623,7 @@ protected theorem Monic.prod {ι : Type*} {P : ι → MvPolynomial σ R} {s : Fi
 /--
 The leading term in a multivariate polynomial is zero if and only if this polynomial is zero.
 -/
+@[simp]
 lemma leadingTerm_eq_zero_iff (p : MvPolynomial σ R) : m.leadingTerm p = 0 ↔ p = 0 := by
   simp only [leadingTerm, monomial_eq_zero, leadingCoeff_eq_zero_iff]
 
@@ -668,6 +669,7 @@ lemma degree_leadingTerm (f : MvPolynomial σ R) :
   simp [leadingTerm, degree_monomial]
   simp_intro h
 
+@[simp]
 lemma leadingCoeff_leadingTerm (f : MvPolynomial σ R) :
     m.leadingCoeff (m.leadingTerm f) = m.leadingCoeff f := by
   simp [leadingTerm, leadingCoeff_monomial]
