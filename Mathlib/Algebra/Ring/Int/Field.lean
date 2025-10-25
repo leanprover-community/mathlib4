73e5d8757f278f3ca7d292b87db544135e0def1d
Andrew Yang <the.erd.one@gmail.com>
2025-10-25 02:00:28 +0100

    oops

--- Mathlib/Algebra/Ring/Int/Field.lean
+++ Mathlib/Algebra/Ring/Int/Field.lean
@@ -9,4 +9,4 @@ import Mathlib.Algebra.Ring.Int.Defs
 /-! # `ℤ` is not a field -/
 
 /-- `ℤ` with its usual ring structure is not a field. -/
-theorem not_isField : ¬IsField ℤ := fun ⟨_, _, h⟩ ↦ have := @h 2; by grind
+theorem Int.not_isField : ¬IsField ℤ := fun ⟨_, _, h⟩ ↦ have := @h 2; by grind
