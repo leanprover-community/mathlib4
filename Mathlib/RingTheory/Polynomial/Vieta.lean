6257a52cce1aa7d315bfad674611005f3ffaeef2
tb65536 <thomas.l.browning@gmail.com>
2025-10-25 01:26:54 -0700

    shake

--- Mathlib/RingTheory/Polynomial/Vieta.lean
+++ Mathlib/RingTheory/Polynomial/Vieta.lean
@@ -3,7 +3,7 @@ Copyright (c) 2020 Hanting Zhang. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Hanting Zhang
 -/
-import Mathlib.Algebra.Polynomial.Splits
+import Mathlib.Algebra.Polynomial.Factors
 import Mathlib.RingTheory.MvPolynomial.Symmetric.Defs
 
 /-!
