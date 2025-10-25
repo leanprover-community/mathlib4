64ee90cd740ad2ae8659843a713920971d7b351c
Monica Omar <2497250a@research.gla.ac.uk>
2025-10-25 14:27:47 +0000

    update

--- Mathlib/Data/Nat/Fib/Lemmas.lean
+++ Mathlib/Data/Nat/Fib/Lemmas.lean
@@ -23,35 +23,29 @@ lemma fib_matrix_eq : ∀ {n : ℕ}, n ≠ 0 →
     !![fib (n + 1), fib n; fib n, fib (n - 1)] = !![1, 1; 1, 0] ^ n
   | 1, _ => by simp
   | n + 2, _ => by
-    rw [pow_succ, ← fib_matrix_eq (by simp)]
-    simp
-    grind [fib_add_two]
+    rw [pow_succ, ← fib_matrix_eq (add_one_ne_zero n)]
+    simp [fib_add_two, ← add_assoc, add_rotate, add_comm (fib n)]
 
 /-- **Cassini's identity**: for nonzero `n`, `fib (n + 1) * fib (n - 1) - fib n ^ 2 = (-1) ^ n`. -/
 lemma fib_succ_mul_fib_pred_sub_fib_sq {n : ℕ} (hn : n ≠ 0) :
     (fib (n + 1) * fib (n - 1) - fib n ^ 2 : ℤ) = (-1) ^ n :=
-  calc fib (n + 1) * fib (n - 1) - fib n ^ 2
-      = ((!![fib (n + 1), fib n; fib n, fib (n - 1)] : Matrix _ _ ℤ)).det := by simp [pow_two]
-    _ = ((!![fib (n + 1), fib n; fib n, fib (n - 1)].map (castRingHom ℤ))).det := rfl
-    _ = (!![1, 1; 1, 0] ^ n).det := by
-      rw [fib_matrix_eq hn, Matrix.map_pow]
-      congr
-      simp [← Matrix.ext_iff]
+  calc _ = (!![fib (n + 1), fib n; fib n, fib (n - 1)] : Matrix _ _ ℤ).det := by simp [pow_two]
+    _ = (!![fib (n + 1), fib n; fib n, fib (n - 1)].map (castRingHom ℤ)).det := rfl
+    _ = (!![1, 1; 1, 0].map (castRingHom ℤ) ^ n).det := by rw [fib_matrix_eq hn, Matrix.map_pow]
+    _ = (!![1, 1; 1, 0] ^ n).det := by congr; simp [← Matrix.ext_iff]
     _ = (-1) ^ n := by simp
 
 /-- Cassini's identity for nonzero even `n`: `fib (n + 1) * fib (n - 1) = 1 + fib n ^ 2.` -/
 lemma fib_succ_mul_fib_pred_of_ne_zero_even {n : ℕ} (hn : n ≠ 0) (hn2 : Even n) :
     fib (n + 1) * fib (n - 1) = 1 + fib n ^ 2 := by
-  have := fib_succ_mul_fib_pred_sub_fib_sq hn
   obtain ⟨a, rfl⟩ := hn2
-  grind [pow_mul]
+  grind [fib_succ_mul_fib_pred_sub_fib_sq, pow_mul]
 
 /-- Cassini's identity for odd `n`: `fib (n + 1) * fib (n - 1) = fib n ^ 2 - 1.` -/
 lemma fib_succ_mul_fib_pred_of_odd {n : ℕ} (hn : Odd n) :
     fib (n + 1) * fib (n - 1) = fib n ^ 2 - 1 := by
-  have := fib_succ_mul_fib_pred_sub_fib_sq (n := n) (by grind)
   obtain ⟨a, rfl⟩ := hn
-  grind [pow_mul]
+  grind [fib_succ_mul_fib_pred_sub_fib_sq, pow_mul]
 
 /-- **Catalan's identity**: for nonzero `x` and `a`,
 `fib (x + a) ^ 2 - fib (x + 2 * a) * fib x = fib a ^ 2 * (-1) ^ x`. -/
@@ -64,7 +58,7 @@ lemma fib_add_sub_fib_add_two_mul_mul_fib {x a : ℕ} (hx : x ≠ 0) (ha : a ≠
         · rw [add_comm, fib_add_of_ne_zero hx]
           simp only [cast_add, cast_mul, add_comm, mul_comm]
         · rw [add_comm x, fib_add_of_ne_zero hx]
-          simp_rw [two_mul, fib_add _ _, fib_add_of_ne_zero (m := a) (n := a) ha]
+          simp_rw [two_mul, fib_add, fib_add_of_ne_zero (m := a) (n := a) ha]
           simp only [cast_add, cast_mul, sq, add_comm, mul_comm]
     _ = fib x * fib (x - 1) * fib a * (fib (a + 1) - fib (a - 1)) +
       fib a ^ 2 * (fib (x - 1) ^ 2 - fib x ^ 2) := by ring
