a149bc34d83dd42f004b366cb698103e8861e879
Monica Omar <2497250a@research.gla.ac.uk>
2025-10-25 14:45:50 +0000

    rename

--- Mathlib/Data/Nat/Fib/Lemmas.lean
+++ Mathlib/Data/Nat/Fib/Lemmas.lean
@@ -12,7 +12,7 @@ import Mathlib.LinearAlgebra.Matrix.Determinant.Basic
 
 Cassini's identity states that for nonzero `n`, `fib (n + 1) * fib (n - 1) - fib n ^ 2` is equal
 to `(-1) ^ n`. And Catalan's identity states that for nonzero `x` and `a`, we get
-`fib (x + a) ^ 2 - fib (x + 2 * a) * fib x = fib a ^ 2 * (-1) ^ x`.
+`fib (x + a) ^ 2 - fib x * fib (x + 2 * a) = (-1) ^ x * fib a ^ 2`.
 -/
 
 namespace Nat
@@ -35,9 +35,9 @@ lemma fib_succ_mul_fib_pred_sub_fib_sq {n : ℕ} (hn : n ≠ 0) :
     _ = (!![1, 1; 1, 0] ^ n).det := by congr; simp [← Matrix.ext_iff]
     _ = (-1) ^ n := by simp
 
-/-- Cassini's identity for nonzero even `n`: `fib (n + 1) * fib (n - 1) = 1 + fib n ^ 2.` -/
+/-- Cassini's identity for nonzero even `n`: `fib (n + 1) * fib (n - 1) = fib n ^ 2 + 1.` -/
 lemma fib_succ_mul_fib_pred_of_ne_zero_even {n : ℕ} (hn : n ≠ 0) (hn2 : Even n) :
-    fib (n + 1) * fib (n - 1) = 1 + fib n ^ 2 := by
+    fib (n + 1) * fib (n - 1) = fib n ^ 2 + 1 := by
   obtain ⟨a, rfl⟩ := hn2
   grind [fib_succ_mul_fib_pred_sub_fib_sq, pow_mul]
 
@@ -48,12 +48,13 @@ lemma fib_succ_mul_fib_pred_of_odd {n : ℕ} (hn : Odd n) :
   grind [fib_succ_mul_fib_pred_sub_fib_sq, pow_mul]
 
 /-- **Catalan's identity**: for nonzero `x` and `a`,
-`fib (x + a) ^ 2 - fib (x + 2 * a) * fib x = fib a ^ 2 * (-1) ^ x`. -/
-lemma fib_add_sub_fib_add_two_mul_mul_fib {x a : ℕ} (hx : x ≠ 0) (ha : a ≠ 0) :
-    (fib (x + a) ^ 2 - fib (x + 2 * a) * fib x : ℤ) = fib a ^ 2 * (-1) ^ x :=
-  calc ((fib (x + a)) ^ 2 - (fib (x + 2 * a)) * (fib x) : ℤ)
+`fib (x + a) ^ 2 - fib x * fib (x + 2 * a) = (-1) ^ x * fib a ^ 2`. -/
+lemma fib_add_sq_sub_fib_mul_fib_add_two_mul {x a : ℕ} (hx : x ≠ 0) (ha : a ≠ 0) :
+    (fib (x + a) ^ 2 - fib x * fib (x + 2 * a) : ℤ) = (-1) ^ x * fib a ^ 2 :=
+  calc (fib (x + a) ^ 2 - fib x * fib (x + 2 * a) : ℤ)
       = (fib x * fib (a + 1) + fib (x - 1) * fib a) ^ 2 - (fib x * (fib (a + 1) ^ 2 + fib a ^ 2) +
           fib (x - 1) * (fib a * fib (a + 1) + fib (a - 1) * fib a)) * fib x := by
+        rw [mul_comm]
         congr 2
         · rw [add_comm, fib_add_of_ne_zero hx]
           simp only [cast_add, cast_mul, add_comm, mul_comm]
@@ -73,7 +74,7 @@ lemma fib_add_sub_fib_add_two_mul_mul_fib {x a : ℕ} (hx : x ≠ 0) (ha : a ≠
 Also see `fib_add_sub_fib_add_two_mul_mul_fib`. -/
 theorem fib_sq_sub_fib_sub_mul_fub_add {n r : ℕ} (hr : r ≠ 0) (hrn : r < n) :
     (fib n ^ 2 - fib (n - r) * fib (n + r) : ℤ) = (-1) ^ (n - r) * fib r ^ 2 := by
-  have := fib_add_sub_fib_add_two_mul_mul_fib (x := n - r) (a := r) (by grind) hr
+  have := fib_add_sq_sub_fib_mul_fib_add_two_mul (Nat.sub_ne_zero_iff_lt.mpr hrn) hr
   grind
 
 end Nat
