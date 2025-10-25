30f0c6250ef19a40cf5b51508bf297673386bece
Monica Omar <2497250a@research.gla.ac.uk>
2025-10-25 22:14:18 +0000

    fix

--- Mathlib/Data/Nat/Fib/Lemmas.lean
+++ Mathlib/Data/Nat/Fib/Lemmas.lean
@@ -17,7 +17,8 @@ to `(-1) ^ n`. And Catalan's identity states that for nonzero `x` and `a`, we ge
 
 namespace Nat
 
-/-- Being a linear recurrence, the entries of the Fibonacci sequence can be related to matrix exponentiation. -/
+/-- Being a linear recurrence, the entries of the Fibonacci sequence can be related to
+matrix exponentiation. -/
 lemma fib_matrix_eq : ∀ {n : ℕ},
     !![fib (n + 2), fib (n + 1); fib (n + 1), fib n] = !![1, 1; 1, 0] ^ (n + 1)
   | 0 => by simp
