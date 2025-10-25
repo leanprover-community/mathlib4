fe5966b82054c7a30bf29e4f33b951a597803431
Monica Omar <2497250a@research.gla.ac.uk>
2025-10-25 18:03:00 +0000

    move

--- Mathlib/LinearAlgebra/TensorProduct/Matrix.lean
+++ Mathlib/LinearAlgebra/TensorProduct/Matrix.lean
@@ -6,7 +6,6 @@ Authors: Eric Wieser
 import Mathlib.LinearAlgebra.Matrix.Kronecker
 import Mathlib.LinearAlgebra.Matrix.ToLin
 import Mathlib.LinearAlgebra.TensorProduct.Basis
-import Mathlib.RingTheory.MatrixAlgebra
 
 /-!
 # Connections between `TensorProduct` and `Matrix`
@@ -18,7 +17,7 @@ Notably, `TensorProduct.toMatrix_map` shows that taking the tensor product of li
 equivalent to taking the Kronecker product of their matrix representations.
 -/
 
-open Matrix Module LinearMap TensorProduct
+open Matrix Module LinearMap
 open scoped Kronecker
 
 variable {R : Type*} {M N P M' N' : Type*} {ι κ τ ι' κ' : Type*}
@@ -69,43 +68,3 @@ theorem TensorProduct.toMatrix_assoc :
     Basis.tensorProduct_repr_tmul_apply, Basis.repr_self, Finsupp.single_apply, @eq_comm _ k',
     @eq_comm _ j', smul_eq_mul, mul_ite, mul_one, mul_zero, ← ite_and, @eq_comm _ i',
     submatrix_apply, Matrix.one_apply, id_eq, Equiv.prodAssoc_apply, Prod.mk.injEq]
-
-namespace Matrix
-namespace TensorProduct
-variable {R m n : Type*} [CommSemiring R]
-  [Fintype n] [Fintype m] [DecidableEq n] [DecidableEq m]
-
-theorem lift_kroneckerBilinear_apply (x : Matrix m m R ⊗[R] Matrix n n R) :
-    lift kroneckerBilinear x = fun ij kl ↦ (matrixEquivTensor n R _).symm x ij.2 kl.2 ij.1 kl.1 :=
-  x.induction_on (by simp only [map_zero, zero_apply]; rfl)
-    fun x y => by ext; simp [Algebra.algebraMap_eq_smul_one, mul_comm (x _ _), kroneckerBilinear]
-    fun _ _ h1 h2 => by simp only [map_add, h1, h2, add_apply]; rfl
-
-/-- The algebra equivalence between `⊗ₜ` and `⊗ₖ`, i.e., the equivalence between
-tensor products of matrices and their Kronecker product.
-The map is given by `x ⊗ₜ y ↦ x ⊗ₖ y` and inverse `x ⊗ₖ y ↦ x ⊗ₜ y`. -/
--- TODO: upgrade this to `≃⋆ₐ` for when `R` is a ⋆-ring (after #27290)
-noncomputable def toKronecker :
-    (Matrix m m R ⊗[R] Matrix n n R) ≃ₐ[R] Matrix (m × n) (m × n) R where
-  toFun x := lift kroneckerBilinear x
-  invFun x := matrixEquivTensor n R (Matrix m m R) fun i j k l => x (k, i) (l, j)
-  map_add' _ _ := map_add _ _ _
-  commutes' _ := by
-    simp [Algebra.algebraMap_eq_smul_one, Algebra.TensorProduct.one_def, kroneckerBilinear]
-  left_inv x := by simp [lift_kroneckerBilinear_apply]
-  right_inv x := by simp only [lift_kroneckerBilinear_apply, AlgEquiv.symm_apply_apply]
-  map_mul' x y :=
-    x.induction_on (by simp) (fun a b =>
-      y.induction_on (by simp)
-        fun c d => by simp [mul_kronecker_mul, kroneckerBilinear]
-      fun _ _ h1 h2 => by simp only [map_add, h1, h2, mul_add])
-    fun _ _ h1 h2 => by simp only [map_add, add_mul, h1, h2]
-
-@[simp] theorem toKronecker_tmul (x : Matrix m m R) (y : Matrix n n R) :
-    (x ⊗ₜ y).toKronecker = x ⊗ₖ y := rfl
-
-@[simp] theorem toKronecker_symm_kronecker (x : Matrix m m R) (y : Matrix n n R) :
-    toKronecker.symm (x ⊗ₖ y) = x ⊗ₜ y := by simp [AlgEquiv.symm_apply_eq]
-
-end TensorProduct
-end Matrix
