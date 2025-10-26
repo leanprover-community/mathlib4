d7e3b56f41f5b6bf24f4f670eb544689ba155549
Frédéric Dupuis <dupuisf@iro.umontreal.ca>
2025-10-25 22:06:01 -0400

    add module deprecation

---
+++ Mathlib/Analysis/SpecialFunctions/ContinuousFunctionalCalculus/ExpLog.lean
@@ -0,0 +1,5 @@
+import Mathlib.Analysis.SpecialFunctions.ContinuousFunctionalCalculus.ExpLog.Basic
+
+deprecated_module
+  "This file has now moved to Mathlib.Analysis.SpecialFunctions.ContinuousFunctionalCalculus.ExpLog.Basic"
+  (since := "2025-10-25")
