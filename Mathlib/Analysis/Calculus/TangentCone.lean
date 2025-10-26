6bb28350a75725c736ae907a682b1fb566cf0dd4
Yury G. Kudryashov <urkud@urkud.name>
2025-10-26 12:24:24 -0500

    chore: restore `TangentCone.lean` as a deprecated module
    The content was moved to other files in #30932

---
+++ Mathlib/Analysis/Calculus/TangentCone.lean
@@ -0,0 +1,9 @@
+import Mathlib.Analysis.Calculus.TangentCone.Basic
+import Mathlib.Analysis.Calculus.TangentCone.Defs
+import Mathlib.Analysis.Calculus.TangentCone.DimOne
+import Mathlib.Analysis.Calculus.TangentCone.Pi
+import Mathlib.Analysis.Calculus.TangentCone.Prod
+import Mathlib.Analysis.Calculus.TangentCone.ProperSpace
+import Mathlib.Analysis.Calculus.TangentCone.Real
+
+deprecated_module (since := "2025-10-26")
