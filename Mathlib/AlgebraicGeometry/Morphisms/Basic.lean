5343e8cbddcec0d6e1b9bda116d8ab5cf076f419
Andrew Yang <the.erd.one@gmail.com>
2025-10-27 22:58:00 +0000

    revert

--- Mathlib/AlgebraicGeometry/Morphisms/Basic.lean
+++ Mathlib/AlgebraicGeometry/Morphisms/Basic.lean
@@ -19,7 +19,7 @@ which we call an `AffineTargetMorphismProperty`. In this file, we provide API le
 local at the target, and special support for those properties whose `AffineTargetMorphismProperty`
 takes on a more simple form. We also provide API lemmas for properties local at the target.
 The main interfaces of the API are the typeclasses `IsLocalAtTarget`, `IsLocalAtSource` and
-`HasAffineProperty`, which we describe in detail below.
+`HasAffineProperty`, which we describle in detail below.
 
 ## `IsZariskiLocalAtTarget`
 
@@ -179,30 +179,6 @@ lemma of_range_subset_iSup [P.RespectsRight @IsOpenImmersion] {ι : Type*} (U : 
   dsimp
   rw [Scheme.Hom.image_iSup, Scheme.Hom.image_top_eq_opensRange, Scheme.Opens.opensRange_ι]
   simp [Scheme.Hom.image_preimage_eq_opensRange_inf, le_iSup U]
-
-lemma of_forall_source_exists_preimage
-    [P.RespectsRight IsOpenImmersion] [P.HasOfPostcompProperty IsOpenImmersion]
-    (f : X ⟶ Y) (hX : ∀ x, ∃ (U : Y.Opens), f x ∈ U ∧ P ((f ⁻¹ᵁ U).ι ≫ f)) :
-    P f := by
-  choose U h₁ h₂ using hX
-  apply IsZariskiLocalAtTarget.of_range_subset_iSup U
-  · rintro y ⟨x, rfl⟩
-    simp only [Opens.coe_iSup, Set.mem_iUnion, SetLike.mem_coe]
-    exact ⟨x, h₁ x⟩
-  · intro x
-    exact P.of_postcomp (f ∣_ U x) (U x).ι (inferInstanceAs <| IsOpenImmersion _) (by simp [h₂])
-
-lemma of_forall_source_exists_preimage
-    [P.RespectsRight IsOpenImmersion] [P.HasOfPostcompProperty IsOpenImmersion]
-    (f : X ⟶ Y) (hX : ∀ x, ∃ (U : Y.Opens), f.base x ∈ U ∧ P ((f ⁻¹ᵁ U).ι ≫ f)) :
-    P f := by
-  choose U h₁ h₂ using hX
-  apply IsZariskiLocalAtTarget.of_range_subset_iSup U
-  · rintro y ⟨x, rfl⟩
-    simp only [Opens.coe_iSup, Set.mem_iUnion, SetLike.mem_coe]
-    exact ⟨x, h₁ x⟩
-  · intro x
-    exact P.of_postcomp (f ∣_ U x) (U x).ι (inferInstanceAs <| IsOpenImmersion _) (by simp [h₂])
 
 end IsZariskiLocalAtTarget
 
