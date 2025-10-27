873680b83680916351fc26b549b7101a8e578bd4
WangYiran01 <jiuyoujiuyoujinzai@163.com>
2025-10-27 15:57:08 +0800

    chore: regenerate import all files

--- Mathlib.lean
+++ Mathlib.lean
@@ -2965,6 +2965,7 @@ import Mathlib.Combinatorics.Enumerative.DyckWord
 import Mathlib.Combinatorics.Enumerative.IncidenceAlgebra
 import Mathlib.Combinatorics.Enumerative.InclusionExclusion
 import Mathlib.Combinatorics.Enumerative.Partition
+import Mathlib.Combinatorics.Enumerative.RecLatticePath
 import Mathlib.Combinatorics.Enumerative.Stirling
 import Mathlib.Combinatorics.Extremal.RuzsaSzemeredi
 import Mathlib.Combinatorics.Graph.Basic
