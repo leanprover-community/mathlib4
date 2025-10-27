2b971f98423b694dbc0a8bf015987ae440f1926e
Yuyang Zhao 赵雨扬 <zhao.yu-yang@foxmail.com>
2025-10-27 08:02:58 +0800

    shake

--- Mathlib/Analysis/SpecialFunctions/OrdinaryHypergeometric.lean
+++ Mathlib/Analysis/SpecialFunctions/OrdinaryHypergeometric.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Edward Watine
 -/
 import Mathlib.Analysis.Analytic.OfScalars
-import Mathlib.Analysis.SpecificLimits.RCLike
+import Mathlib.Analysis.RCLike.Basic
 
 /-!
 # Ordinary hypergeometric function in a Banach algebra
