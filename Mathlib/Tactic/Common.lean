a7585e075ab7af391961efc8fd34afe97cfaa96c
Kim Morrison <kim@tqft.net>
2025-10-27 01:24:19 +0100

    require canonical

--- Mathlib/Tactic/Common.lean
+++ Mathlib/Tactic/Common.lean
@@ -8,6 +8,7 @@ Authors: Kim Morrison
 import Aesop
 import Qq
 import Plausible
+import Canonical
 
 -- Tools for analysing imports, like `#find_home`, `#minimize_imports`, ...
 import ImportGraph.Imports
--- Mathlib/Tactic/Linter/DirectoryDependency.lean
+++ Mathlib/Tactic/Linter/DirectoryDependency.lean
@@ -176,6 +176,7 @@ def allowedImportDirs : NamePrefixRel := .ofArray #[
   (`MathlibTest.Header, `Plausible),
   (`MathlibTest.Header, `ProofWidgets),
   (`MathlibTest.Header, `Qq),
+  (`MathlibTest.Header, `Canonical),
   -- (`MathlibTest.Header, `Mathlib.Tactic),
   -- (`MathlibTest.Header, `Mathlib.Deprecated),
   (`MathlibTest.Header, `Batteries),
@@ -649,13 +650,14 @@ def directoryDependencyCheck (mainModule : Name) : CommandElabM (Array MessageDa
   else
     -- We always allow imports in the same directory (for each matching prefix),
     -- from `Init`, `Lean` and `Std`, as well as imports in `Aesop`, `Qq`, `Plausible`,
-    -- `ImportGraph`, `ProofWidgets` or `LeanSearchClient` (as these are imported in Tactic.Common).
+    -- `ImportGraph`, `ProofWidgets`, `LeanSearchClient` and `Canonical`
+    -- (as these are imported in Tactic.Common).
     -- We also allow transitive imports of Mathlib.Init, as well as Mathlib.Init itself.
     let initImports := (← findImports ("Mathlib" / "Init.lean")).append
       #[`Mathlib.Init, `Mathlib.Tactic.DeclarationNames]
     let exclude := [
       `Init, `Std, `Lean,
-      `Aesop, `Qq, `Plausible, `ImportGraph, `ProofWidgets, `LeanSearchClient
+      `Aesop, `Qq, `Plausible, `ImportGraph, `ProofWidgets, `LeanSearchClient, `Canonical
     ]
     let importsToCheck := imports.filter (fun imp ↦ !exclude.any (·.isPrefixOf imp))
       |>.filter (fun imp ↦ !matchingPrefixes.any (·.isPrefixOf imp))
