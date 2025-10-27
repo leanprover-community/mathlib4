88d330e19d35d91b0e5b0f6415a6939d00648f20
Yaël Dillies <yael.dillies@gmail.com>
2025-10-27 02:53:45 +0000

    feat: characterise when `Set.rangeFactorization` is injective (#30126)

--- Mathlib/Data/Set/Operations.lean
+++ Mathlib/Data/Set/Operations.lean
@@ -156,6 +156,16 @@ def range (f : ι → α) : Set α := {x | ∃ y, f y = x}
 /-- Any map `f : ι → α` factors through a map `rangeFactorization f : ι → range f`. -/
 def rangeFactorization (f : ι → α) : ι → range f := fun i => ⟨f i, mem_range_self i⟩
 
+@[simp] lemma rangeFactorization_injective :
+    (Set.rangeFactorization f).Injective ↔ f.Injective := by
+  simp [Function.Injective, rangeFactorization]
+
+@[simp] lemma rangeFactorization_surjective : (rangeFactorization f).Surjective :=
+  fun ⟨_, i, rfl⟩ ↦ ⟨i, rfl⟩
+
+@[simp] lemma rangeFactorization_bijective :
+    (Set.rangeFactorization f).Bijective ↔ f.Injective := by simp [Function.Bijective]
+
 end Range
 
 /-- We can use the axiom of choice to pick a preimage for every element of `range f`. -/
