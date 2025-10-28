ec71a07435fd1e4fa84adc594ad83c355b7bdd46
Joël Riou <37772949+joelriou@users.noreply.github.com>
2025-10-28 15:18:33 +0000

    refactor(AlgebraicTopology): redefine the topological simplex using the convexity API (#28893)
    Before this PR, the n-dimensional topological simplex (in the `AlgebraicTopology` hierarchy) was defined as a subtype of `Fin (n + 1) → ℝ≥0`, which did not interact well with the convexity API which assumes the ambient type is a vector space. In this PR, we redefine the topological simplex functor (as a cosimplicial object in `TopCat`) using the already existing `stdSimplex` from `Analysis.Convex`.
    
    Co-authored-by: Stefan Kebekus <5110976+kebekus@users.noreply.github.com>
    Co-authored-by: Robin Carlier <57142648+robin-carlier@users.noreply.github.com>
    Co-authored-by: Weiyi Wang <wwylele@gmail.com>
    Co-authored-by: Sebastien Gouezel <10818434+sgouezel@users.noreply.github.com>
    Co-authored-by: Xavier Roblot <46200072+xroblot@users.noreply.github.com>
    Co-authored-by: Salvatore Mercuri <47568553+smmercuri@users.noreply.github.com>
    Co-authored-by: Junyan Xu <junyanxu.math@gmail.com>
    Co-authored-by: Jeremy Tan Jie Rui <54175463+Parcly-Taxel@users.noreply.github.com>
    Co-authored-by: Bryan Gin-ge Chen <5209952+bryangingechen@users.noreply.github.com>
    Co-authored-by: Bolton Bailey <bolton.bailey@gmail.com>
    Co-authored-by: Violeta Hernández <vi.hdz.p@gmail.com>
    Co-authored-by: staroperator <143908816+staroperator@users.noreply.github.com>
    Co-authored-by: Rémy Degenne <4094732+RemyDegenne@users.noreply.github.com>
    Co-authored-by: grunweg <10105016+grunweg@users.noreply.github.com>
    Co-authored-by: Ben Eltschig <43812953+peabrainiac@users.noreply.github.com>
    Co-authored-by: Yury Kudryashov <162619279+yury-harmonic@users.noreply.github.com>
    Co-authored-by: Christian Merten <136261474+chrisflav@users.noreply.github.com>
    Co-authored-by: euprunin <178733547+euprunin@users.noreply.github.com>
    Co-authored-by: Michael Rothgang <10105016+grunweg@users.noreply.github.com>
    Co-authored-by: Chris Birkbeck <56166236+CBirkbeck@users.noreply.github.com>
    Co-authored-by: Hannah Scholz <70071345+scholzhannah@users.noreply.github.com>
    Co-authored-by: Lawrence Wu <hi@llllvvuu.dev>
    Co-authored-by: Yizheng Zhu <zhuyizheng@gmail.com>

--- Mathlib/Analysis/Convex/StdSimplex.lean
+++ Mathlib/Analysis/Convex/StdSimplex.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Alexander Bentkamp, Yury Kudryashov, Yaël Dillies, Joël Riou
 -/
 import Mathlib.Analysis.Convex.Combination
+import Mathlib.Analysis.Convex.PathConnected
 import Mathlib.Topology.Algebra.Monoid.FunOnFinite
 import Mathlib.Topology.MetricSpace.ProperSpace.Real
 import Mathlib.Topology.UnitInterval
@@ -191,6 +192,16 @@ theorem isCompact_stdSimplex : IsCompact (stdSimplex ℝ ι) :=
 instance stdSimplex.instCompactSpace_coe : CompactSpace ↥(stdSimplex ℝ ι) :=
   isCompact_iff_compactSpace.mp <| isCompact_stdSimplex _
 
+/-- `stdSimplex ℝ ι` is path connected. -/
+theorem isPathConnected_stdSimplex [Nonempty ι] :
+    IsPathConnected (stdSimplex ℝ ι) :=
+  (convex_stdSimplex ℝ ι).isPathConnected (by
+    classical
+    exact ⟨_, single_mem_stdSimplex ℝ (Classical.arbitrary ι)⟩)
+
+instance [Nonempty ι] : PathConnectedSpace (stdSimplex ℝ ι) :=
+  isPathConnected_iff_pathConnectedSpace.1 (isPathConnected_stdSimplex _)
+
 /-- The standard one-dimensional simplex in `ℝ² = Fin 2 → ℝ`
 is homeomorphic to the unit interval. -/
 @[simps! -fullyApplied]
@@ -231,6 +242,10 @@ lemma zero_le (s : stdSimplex S X) (x : X) : 0 ≤ s x := s.2.1 x
 
 @[simp]
 lemma sum_eq_one (s : stdSimplex S X) : ∑ x, s x = 1 := s.2.2
+
+lemma add_eq_one (s : stdSimplex S (Fin 2)) :
+    s 0 + s 1 = 1 := by
+  simpa only [Fin.sum_univ_two] using sum_eq_one s
 
 section
 
@@ -285,6 +300,42 @@ lemma map_vertex [DecidableEq X] [DecidableEq Y] (f : X → Y) (x : X) :
 lemma continuous_map [TopologicalSpace S] [IsTopologicalSemiring S] (f : X → Y) :
     Continuous (map (S := S) f) :=
   Continuous.subtype_mk ((FunOnFinite.continuous_linearMap S S f).comp continuous_induced_dom) _
+
+lemma vertex_injective [Nontrivial S] [DecidableEq X] :
+    Function.Injective (vertex (S := S) (X := X)) := by
+  intro x y h
+  replace h := DFunLike.congr_fun h x
+  by_contra!
+  simp [Pi.single_eq_of_ne this] at h
+
+instance [Nonempty X] : Nonempty (stdSimplex S X) := by
+  classical
+  exact ⟨vertex (Classical.arbitrary _)⟩
+
+instance [Nontrivial S] [Nontrivial X] : Nontrivial (stdSimplex S X) where
+  exists_pair_ne := by
+    classical
+    obtain ⟨x, y, hxy⟩ := exists_pair_ne X
+    exact ⟨vertex x, vertex y, fun h ↦ hxy (vertex_injective h)⟩
+
+instance [Subsingleton X] : Subsingleton (stdSimplex S X) where
+  allEq s t := by
+    ext i
+    have (u : stdSimplex S X) : u i = 1 := by
+      rw [← sum_eq_one u, Finset.sum_eq_single i _ (by simp)]
+      intro j _ hj
+      exact (hj (Subsingleton.elim j i)).elim
+    simp [this]
+
+instance [Unique X] : Unique (stdSimplex S X) where
+  default := ⟨1, by simp, by simp⟩
+  uniq := by subsingleton
+
+@[simp]
+lemma eq_one_of_unique [Unique X] (s : stdSimplex S X) (x : X) :
+    s x = 1 := by
+  obtain rfl : s = default := by subsingleton
+  rfl
 
 end
 
