347f0731e0bc81340b04ed73f3be1a972e0048ca
ADedecker <anatolededecker@gmail.com>
2025-10-28 16:45:41 +0100

    shake

--- Mathlib/Topology/Algebra/Group/AddTorsor.lean
+++ Mathlib/Topology/Algebra/Group/AddTorsor.lean
@@ -4,7 +4,8 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Attila Gáspár
 -/
 import Mathlib.Algebra.AddTorsor.Basic
-import Mathlib.Topology.Algebra.Group.Basic
+import Mathlib.Topology.Algebra.Monoid
+import Mathlib.Topology.Algebra.Group.Defs
 
 /-!
 # Topological torsors of additive groups
