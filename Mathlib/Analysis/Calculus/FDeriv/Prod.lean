8ad5f0b4a994da61d327358decdc146af49e0653
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-10-28 16:39:01 +0100

    Fix

--- Mathlib/Analysis/Calculus/FDeriv/Prod.lean
+++ Mathlib/Analysis/Calculus/FDeriv/Prod.lean
@@ -344,9 +344,9 @@ protected theorem HasStrictFDerivAt.prodMap (hf : HasStrictFDerivAt f f' p.1)
 protected theorem HasFDerivWithinAt.prodMap {s : Set <| E × G}
     (hf : HasFDerivWithinAt f f' (Prod.fst '' s) p.1)
     (hf₂ : HasFDerivWithinAt f₂ f₂' (Prod.snd '' s) p.2) :
-    HasFDerivWithinAt (Prod.map f f₂) (f'.prodMap f₂') s p := by
+    HasFDerivWithinAt (Prod.map f f₂) (f'.prodMap f₂') s p :=
   (hf.comp _ hasFDerivWithinAt_fst mapsTo_fst_prod).prodMk
-    (hf₂.comp _ hasFDerivWithinAt_snd mapsTo_snd_prod)
+    (hf₂.comp _ hasFDerivWithinAt_snd mapsTo_snd_prod) |>.mono (by grind)
 
 @[fun_prop]
 protected theorem HasFDerivAt.prodMap (hf : HasFDerivAt f f' p.1) (hf₂ : HasFDerivAt f₂ f₂' p.2) :
