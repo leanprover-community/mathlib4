2c854e744f1c19abc264f3806a5200de374033e8
David Loeffler <d.loeffler.01@cantab.net>
2025-10-28 21:32:12 +0000

    feat(Topology/Algebra/Order/Archimedean): subgroups cyclic iff discrete (#30471)
    An add subgroup of the reals (or similar groups) is discrete iff it's cyclic, and related statements.

---
+++ Mathlib/Topology/Algebra/Order/ArchimedeanDiscrete.lean
@@ -0,0 +1,72 @@
+/-
+Copyright (c) 2025 David Loeffler. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: David Loeffler
+-/
+import Mathlib.GroupTheory.ArchimedeanDensely
+import Mathlib.GroupTheory.SpecificGroups.Cyclic
+import Mathlib.Topology.Algebra.IsUniformGroup.Basic
+import Mathlib.Topology.Algebra.Order.Archimedean
+import Mathlib.Topology.Order.DenselyOrdered
+
+/-!
+# Discreteness of subgroups in archimedean ordered groups
+
+This file contains some supplements to the results in `Mathlib.Topology.Algebra.Order.Archimedean`,
+involving discreteness of subgroups, which require heavier imports.
+-/
+
+namespace Subgroup
+
+variable {G : Type*} [CommGroup G] [LinearOrder G] [IsOrderedMonoid G]
+  [TopologicalSpace G] [OrderTopology G]
+
+/-- In a linearly ordered group with the order topology, the powers of a single element form a
+discrete subgroup. -/
+@[to_additive /-- In a linearly ordered additive group with the order topology, the multiples of a
+single element form a discrete subgroup. -/]
+instance instDiscreteTopologyZMultiples (g : G) : DiscreteTopology (zpowers g) := by
+  wlog ha : 1 ≤ g
+  · specialize this g⁻¹ (one_le_inv'.mpr (le_of_not_ge ha))
+    rwa [zpowers_inv] at this
+  rcases eq_or_lt_of_le ha with rfl | ha
+  · rw [zpowers_one_eq_bot]
+    exact Subsingleton.discreteTopology
+  rw [discreteTopology_iff_isOpen_singleton_one, isOpen_induced_iff]
+  refine ⟨Set.Ioo (g ^ (-1 : ℤ)) (g ^ (1 : ℤ)), isOpen_Ioo, ?_⟩
+  ext ⟨_, ⟨n, rfl⟩⟩
+  constructor
+  · simp only [Set.mem_preimage, Set.mem_Ioo, Set.mem_singleton_iff, and_imp]
+    intro hn hn'
+    rw [zpow_lt_zpow_iff_right ha] at hn hn'
+    simp only [Subtype.ext_iff, show n = 0 by omega, zpow_zero, coe_one]
+  · simp_all
+
+variable [MulArchimedean G]
+
+@[to_additive]
+instance [DiscreteTopology G] : IsCyclic G := by
+  nontriviality G
+  exact LinearOrderedCommGroup.isCyclic_iff_not_denselyOrdered.mpr fun h ↦
+    haveI := h.subsingleton_of_discreteTopology; false_of_nontrivial_of_subsingleton G
+
+/-- In an Archimedean densely linearly ordered group (with the order topology), a subgroup is
+discrete iff it is cyclic. -/
+@[to_additive /-- In an Archimedean linearly ordered additive group (with the order topology), a
+subgroup is discrete iff it is cyclic. -/]
+lemma discrete_iff_cyclic {H : Subgroup G} : IsCyclic H ↔ DiscreteTopology H := by
+  nontriviality G using isCyclic_of_subsingleton, Subsingleton.discreteTopology
+  rw [Subgroup.isCyclic_iff_exists_zpowers_eq_top]
+  constructor
+  · rintro ⟨g, rfl⟩
+    infer_instance
+  · have := H.dense_or_cyclic
+    simp only [← Subgroup.zpowers_eq_closure, Eq.comm (a := H)] at this
+    refine fun hA ↦ this.elim (fun h ↦ ?_) id
+    -- remains to show a contradiction assuming `H` is both dense and discrete
+    obtain rfl : H = ⊤ := by
+      rw [← coe_eq_univ, ← (dense_iff_closure_eq.mp h), H.isClosed_of_discrete.closure_eq]
+    have : DiscreteTopology G := by rwa [← (Homeomorph.Set.univ G).discreteTopology_iff]
+    exact isCyclic_iff_exists_zpowers_eq_top.mp inferInstance
+
+end Subgroup
