8a1ec9127de1f29b662ead18bbb1d6ce60f3951f
Christian Merten <christian@merten.dev>
2025-10-28 21:18:31 +0100

    improve linter

--- Mathlib/Tactic/Linter/InnerOuter.lean
+++ Mathlib/Tactic/Linter/InnerOuter.lean
@@ -5,40 +5,45 @@ import Batteries.Data.List.Matcher
 open Lean Elab Command Linter
 
 structure Config where
-  outer : Name
-  inner : Name
+  chain : List Name
   projection : String
 
 def isBadSyntax (c : Config) : Syntax → Bool
-  | `(c.outer $_*) => true
-  | stx => List.containsInfix (toString stx).toList c.projection.toList
+  | .ident _ s _ _ => List.containsInfix s.str.toList c.projection.toList
+  | _ => false
 
 def isTermApp : Syntax → Bool
   | `($_ $_*) => true
   | _ => false
 
-partial
-def containsOuter (c : Config) (e : Expr) : Bool :=
-  if e.isAppOf c.outer then
-    true
-  else
-    e.getAppArgs.any (fun e ↦ containsOuter c e)
+def isValValAux (e : Expr) : List Name → Bool
+  | [] => true
+  | n :: ns => e.isAppOf n && e.getAppArgs.any (fun e ↦ isValValAux e ns)
 
 def isValVal (c : Config) (e : Expr) : Bool :=
-  e.isAppOf c.inner && e.getAppArgs.any (fun e ↦ containsOuter c e)
+  isValValAux e c.chain
 
 partial
-def find (c : Config) : InfoTree → Array (TermInfo × Syntax × Bool)
+def find (c : Config) : InfoTree → Array (Syntax × Bool)
   | .node k args =>
-    let kargs := (args.map (find c)).foldl (· ++ ·) #[]
     if let .ofTermInfo i := k then
       if isTermApp i.stx then
-        match i.stx.find? (isBadSyntax c) with
-        | some stx => #[(i, stx, (i.expr.find? (isValVal c)).isSome)] ++ kargs
-        | none => kargs
+        if List.containsInfix (toString i.stx).toList c.projection.toList then
+          #[(i.stx, (i.expr.find? (isValVal c)).isSome)]
+        else
+          #[]
+        --match i.stx.find? (isBadSyntax c) with
+        --| some stx => #[(stx, (i.expr.find? (isValVal c)).isSome)]
+        --| none => #[(i.stx, (i.expr.find? (isValVal c)).isSome)]
       else
-        kargs
-    else kargs
+        (args.map (find c)).foldl (· ++ ·) #[]
+        -- kargs
+    --else if let .ofFieldInfo i := k then
+    --  #[(i.stx, (i.val.find? (isValVal c)).isSome)] ++ kargs
+    --else if let .ofPartialTermInfo i := k then
+    --  #[(i.stx, true)] ++ kargs
+    else
+      (args.map (find c)).foldl (· ++ ·) #[]
   | .context _ t => find c t
   | _ => default
 
@@ -53,14 +58,18 @@ def innerOuterLinter (c : Config) : Linter where
       return
     let trees ← getInfoTrees
     for tree in trees.map (find c) do
-      for (info, stx, relevant) in tree do
+      for (stx, relevant) in tree do
         unless relevant do continue
-        Linter.logLintIf linter.innerOuter info.stx
-          m!"{stx} uses explicit syntax for {c.inner} ({c.outer} ?_)"
+        Linter.logLintIf linter.innerOuter stx
+          m!"{stx} uses unwanted explicit syntax."
 
 def homBase : Config where
-  outer := `AlgebraicGeometry.PresheafedSpace.Hom.base
-  inner := `DFunLike.coe
-  projection := ".base"
+  chain :=
+    [`DFunLike.coe ,
+     `CategoryTheory.ConcreteCategory.hom ,
+     `AlgebraicGeometry.PresheafedSpace.Hom.base ,
+     `AlgebraicGeometry.LocallyRingedSpace.Hom.toHom ,
+     `AlgebraicGeometry.Scheme.Hom.toLRSHom']
+  projection := "base"
 
 initialize addLinter (innerOuterLinter homBase)
