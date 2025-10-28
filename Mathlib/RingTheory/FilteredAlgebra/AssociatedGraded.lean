b7ed05a1b5d0e7c003539e0c6c439a592ed591e8
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2025-10-28 21:14:17 +0800

    drop Heq lemmas

--- Mathlib/RingTheory/FilteredAlgebra/AssociatedGraded.lean
+++ Mathlib/RingTheory/FilteredAlgebra/AssociatedGraded.lean
@@ -128,23 +128,6 @@ lemma induction_on {i : ι} {C : GradedPiece F F_lt i → Prop} (x : GradedPiece
 
 lemma mk_eq {i : ι} (x : F i) : mk F F_lt x = ⟦x⟧ := rfl
 
-lemma HEq_rfl {i j : ι} {r : A} (h : i = j) (hi : r ∈ ofClass (F i)) (hj : r ∈ ofClass (F j)) :
-    HEq (mk F F_lt ⟨r, hi⟩) (mk F F_lt ⟨r, hj⟩) :=
-  h ▸ HEq.rfl
-
-lemma HEq_eq_mk_eq {i j : ι} {x : GradedPiece F F_lt i} {y : GradedPiece F F_lt j} {r s : A}
-    (h : i = j) (e : r = s) (hi : r ∈ ofClass (F i)) (hj : s ∈ ofClass (F j))
-    (hx : x = mk F F_lt ⟨r, hi⟩) (hy : y = mk F F_lt ⟨s, hj⟩) : HEq x y := by
-  rw [hx, hy]
-  subst e
-  exact HEq_rfl F F_lt h hi hj
-
--- Will be easier to use if HMul intances for F i is added and some other refactor is done.
-lemma HEq_eq_mk_coe_eq {i j : ι} {x : GradedPiece F F_lt i} {y : GradedPiece F F_lt j}
-    (r : ofClass (F i)) (s : ofClass (F j)) (h : i = j) (e : (r : A) = (s : A))
-    (hx : x = mk F F_lt r) (hy : y = mk F F_lt s) : HEq x y :=
-  HEq_eq_mk_eq F F_lt h e r.2 (e ▸ s.2) hx hy
-
 end
 
 end GradedPiece
