89a8e81f9eda3a8f2dab96346f8d202a410a7e77
Christian Merten <christian@merten.dev>
2025-10-28 23:16:17 +0100

    update mathlib

--- Mathlib.lean
+++ Mathlib.lean
@@ -5586,6 +5586,7 @@ import Mathlib.RingTheory.EuclideanDomain
 import Mathlib.RingTheory.Extension
 import Mathlib.RingTheory.Extension.Basic
 import Mathlib.RingTheory.Extension.Cotangent.Basic
+import Mathlib.RingTheory.Extension.Cotangent.Basis
 import Mathlib.RingTheory.Extension.Cotangent.LocalizationAway
 import Mathlib.RingTheory.Extension.Generators
 import Mathlib.RingTheory.Extension.Presentation.Basic
