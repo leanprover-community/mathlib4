352dc0059a6d73aa47b37e2a52de9f9a6ad3dabb
Yaël Dillies <yael.dillies@gmail.com>
2025-10-29 00:35:03 +0000

    feat: relation-separated sets (#23920)
    Define a notion of separation of a set relative to a relation. This will be used to unify metric and dynamical separation.
    
    From MiscYD and LeanAPAP

--- Mathlib.lean
+++ Mathlib.lean
@@ -3694,6 +3694,7 @@ import Mathlib.Data.Real.Sqrt
 import Mathlib.Data.Real.Star
 import Mathlib.Data.Real.StarOrdered
 import Mathlib.Data.Rel
+import Mathlib.Data.Rel.Separated
 import Mathlib.Data.SProd
 import Mathlib.Data.Semiquot
 import Mathlib.Data.Seq.Basic
