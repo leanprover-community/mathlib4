381f1e626b88968e9414b652566119ef34ff555b
Yury G. Kudryashov <urkud@urkud.name>
2025-10-29 00:17:52 -0500

    Fix

--- Mathlib/Topology/Algebra/GroupWithZero.lean
+++ Mathlib/Topology/Algebra/GroupWithZero.lean
@@ -148,6 +148,7 @@ end GroupWithZero
 
 section NhdsInv
 
+open scoped Pointwise
 variable [GroupWithZero G₀] [TopologicalSpace G₀] [ContinuousInv₀ G₀] {x : G₀}
 
 lemma nhds_inv₀ (hx : x ≠ 0) : 𝓝 x⁻¹ = (𝓝 x)⁻¹ := by
