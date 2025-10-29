c1f646136895e702a0926b3de0c5f5c47e29ad1b
Emily Riehl <19517483+emilyriehl@users.noreply.github.com>
2025-10-29 00:04:09 +0000

    Feat(Logic): basic properties of boolean negation (#31028)
    We show that `not : Bool -> Bool` is an involutive equivalence and derive some immediate consequences.
    
    Co-authored-by: Kenny Lau <kc_kennylau@yahoo.com.hk>
    Co-authored-by: Wrenna Robson <wren.robson@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -4533,6 +4533,7 @@ import Mathlib.Logic.Encodable.Lattice
 import Mathlib.Logic.Encodable.Pi
 import Mathlib.Logic.Equiv.Array
 import Mathlib.Logic.Equiv.Basic
+import Mathlib.Logic.Equiv.Bool
 import Mathlib.Logic.Equiv.Defs
 import Mathlib.Logic.Equiv.Embedding
 import Mathlib.Logic.Equiv.Fin.Basic
