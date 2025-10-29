6d9c7c8dedae300723011474f06fd7569f5757b8
or4nge19 <matteo.cipollina@yahoo.it>
2025-10-29 18:14:38 +0100

    address comments

--- Mathlib/Data/Matrix/Mul.lean
+++ Mathlib/Data/Matrix/Mul.lean
@@ -2,8 +2,8 @@
 Copyright (c) 2018 Ellen Arlt. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Ellen Arlt, Blair Shi, Sean Leather, Mario Carneiro, Johan Commelin, Lu-Ming Zhang,
-  Matteo Cipollina
 -/
+
 import Mathlib.Algebra.BigOperators.GroupWithZero.Action
 import Mathlib.Algebra.BigOperators.Ring.Finset
 import Mathlib.Algebra.Regular.Basic
--- Mathlib/LinearAlgebra/Matrix/Irreducible/Defs.lean
+++ Mathlib/LinearAlgebra/Matrix/Irreducible/Defs.lean
@@ -58,9 +58,9 @@ matrix, nonnegative, positive, power, quiver, graph, irreducible, primitive, per
 -/
 namespace Matrix
 
-open Finset Quiver Quiver.Path
+open Quiver Quiver.Path
 
-variable {n R : Type*} [Fintype n] [Ring R] [LinearOrder R]
+variable {n R : Type*} [Ring R] [LinearOrder R]
 
 /-- The directed graph (quiver) associated with a matrix `A`,
 with an edge `i ⟶ j` iff `0 < A i j`. -/
@@ -69,13 +69,15 @@ def toQuiver (A : Matrix n n R) : Quiver n :=
 
 /-- A matrix `A` is irreducible if it is entrywise nonnegative and
 its quiver of positive entries (`toQuiver A`) is strongly connected. -/
-def IsIrreducible (A : Matrix n n R) : Prop :=
-  (∀ i j, 0 ≤ A i j) ∧ @IsSStronglyConnected n (toQuiver A)
+@[mk_iff] structure IsIrreducible (A : Matrix n n R) : Prop where
+  nonneg (i j : n) : 0 ≤ A i j
+  connected : @IsSStronglyConnected n (toQuiver A)
 
 /-- A matrix `A` is primitive if it is entrywise nonnegative
 and some positive power has all entries strictly positive. -/
-def IsPrimitive [DecidableEq n] (A : Matrix n n R) : Prop :=
-  (∀ i j, 0 ≤ A i j) ∧ ∃ k > 0, ∀ i j, 0 < (A ^ k) i j
+@[mk_iff] structure IsPrimitive [Fintype n] [DecidableEq n] (A : Matrix n n R) : Prop where
+  nonneg (i j : n) : 0 ≤ A i j
+  exists_pos_pow : ∃ k > 0, ∀ i j, 0 < (A ^ k) i j
 
 variable {A : Matrix n n R}
 
@@ -88,44 +90,31 @@ lemma IsIrreducible.exists_pos [Nontrivial n]
   have no_out : ∀ j : n, IsEmpty (i ⟶ j) :=
     fun j => ⟨fun e => h_row ⟨j, e⟩⟩
   obtain ⟨j, hij⟩ := exists_pair_ne n
-  obtain ⟨p, hp_pos⟩ := h_irr.2 i j
+  obtain ⟨p, hp_pos⟩ := h_irr.connected i j
   have h_le : 1 ≤ p.length := Nat.succ_le_of_lt hp_pos
   have ⟨v, p₁, p₂, _hp_eq, hp₁_len⟩ := p.exists_eq_comp_of_le_length (n := 1) h_le
   have hlen_ne : p₁.length ≠ 0 := by simp [hp₁_len]
-  obtain ⟨c, p', e, rfl⟩ :=
-    (Quiver.Path.length_ne_zero_iff_eq_cons (p := p₁)).1 hlen_ne
-  have hp'_succ : p'.length + 1 = 1 := by
-    simpa [Quiver.Path.length_cons] using hp₁_len
-  have hp'0 : p'.length = 0 := by
-    exact Nat.succ.inj hp'_succ
-  have hi_c : i = c := Quiver.Path.eq_of_length_zero p' hp'0
-  subst hi_c
+  obtain ⟨c, p', e, rfl⟩ := (Quiver.Path.length_ne_zero_iff_eq_cons (p := p₁)).1 (by omega)
+  obtain ⟨rfl⟩ : i = c := Quiver.Path.eq_of_length_zero p' (by aesop)
   exact (no_out _).false e
 
 /--
 For a matrix `A` with nonnegative entries, the `(i, j)`-entry of the `k`-th power `A ^ k`
 is strictly positive if and only if there exists a path of length `k` from `i` to `j` in the
 quiver associated to `A` via `toQuiver`. -/
-theorem pow_entry_pos_iff_exists_path
-    [IsOrderedRing R] [PosMulStrictMono R] [Nontrivial R] [DecidableEq n]
+theorem pow_apply_pos_iff_nonempty_path
+    [Fintype n] [IsOrderedRing R] [PosMulStrictMono R] [Nontrivial R] [DecidableEq n]
     (hA : ∀ i j, 0 ≤ A i j) (k : ℕ) (i j : n) :
-    letI := toQuiver A; 0 < (A ^ k) i j ↔ Nonempty {p : Path i j // p.length = k} := by
-  induction k generalizing i j with
+    letI := toQuiver A
+    0 < (A ^ k) i j ↔ Nonempty {p : Path i j // p.length = k} := by
+    letI := toQuiver A
+    induction k generalizing i j with
   | zero =>
-    constructor
-    · intro h_pos
-      by_cases h_eq : i = j
-      · subst h_eq
-        letI : Quiver n := toQuiver A
-        exact ⟨⟨Quiver.Path.nil, rfl⟩⟩
-      · have : (A ^ 0) i j = 0 := by
-          simp [pow_zero, h_eq]
-        simp_all only [lt_self_iff_false]
-    · rintro ⟨p, hp⟩
-      letI : Quiver n := toQuiver A
-      have h_eq : i = j := Quiver.Path.eq_of_length_zero p hp
-      have : 0 < (1 : R) := zero_lt_one
-      simp [pow_zero, h_eq]
+    refine ⟨fun h_pos ↦ ?_, fun ⟨p, hp⟩ ↦ ?_⟩
+    · rcases eq_or_ne i j with rfl | h_eq
+      · exact ⟨⟨Quiver.Path.nil, rfl⟩⟩
+      · simp_all only [pow_zero, ne_eq, not_false_eq_true, one_apply_ne, lt_self_iff_false]
+    · simp [Quiver.Path.eq_of_length_zero p hp]
   | succ m ih =>
     rw [pow_succ, mul_apply]
     constructor
@@ -137,18 +126,10 @@ theorem pow_entry_pos_iff_exists_path
           using h_pos
       have hAm_nonneg : 0 ≤ (A ^ m) i l := pow_apply_nonneg hA m i l
       have hA_nonneg' : 0 ≤ A l j := hA l j
-      have h_Am : 0 < (A ^ m) i l := by
-        by_contra h
-        have h_eq : (A ^ m) i l = 0 := le_antisymm (le_of_not_gt h) hAm_nonneg
-        simp [h_eq] at hl_pos
-      have h_A : 0 < A l j := by
-        by_contra h
-        have h_eq : A l j = 0 := le_antisymm (le_of_not_gt h) hA_nonneg'
-        simp [h_eq] at hl_pos
-      obtain ⟨⟨p, hp_len⟩⟩ := (ih i l).mp h_Am
-      exact ⟨letI := toQuiver A; p.cons h_A, by
-          subst hp_len
-          simp only [Path.length_cons]⟩
+      have h_Am : 0 < (A ^ m) i l := by by_contra! h; simp [le_antisymm h hAm_nonneg] at hl_pos
+      have h_A : 0 < A l j := by by_contra! h; simp [le_antisymm h hA_nonneg'] at hl_pos
+      obtain ⟨⟨p, rfl⟩⟩ := (ih i l).mp h_Am
+      exact ⟨p.cons h_A, by simp⟩
     · rintro ⟨p, hp_len⟩
       cases p with
       | nil => simp [Quiver.Path.length] at hp_len
@@ -165,50 +146,47 @@ theorem pow_entry_pos_iff_exists_path
 /-- Irreducibility of a nonnegative matrix `A` is equivalent to entrywise positivity of some
 power: between any two indices `i, j` there exists a positive integer `k` such that the
 `(i, j)`-entry of `A ^ k` is strictly positive. -/
-theorem IsIrreducible.iff_exists_pow_pos
-    [IsOrderedRing R] [PosMulStrictMono R] [Nontrivial R] [DecidableEq n]
+theorem isIrreducible_iff_exists_pow_pos
+    [Fintype n] [IsOrderedRing R] [PosMulStrictMono R] [Nontrivial R] [DecidableEq n]
     (hA : ∀ i j, 0 ≤ A i j) :
     IsIrreducible A ↔ ∀ i j, ∃ k > 0, 0 < (A ^ k) i j := by
+  letI : Quiver n := toQuiver A
   constructor
   · intro h_irr i j
-    letI : Quiver n := toQuiver A
     obtain ⟨p, hp_len⟩ := h_irr.2 i j
     refine ⟨p.length, hp_len, ?_⟩
     have : Nonempty {q : Path i j // q.length = p.length} := ⟨⟨p, rfl⟩⟩
     have hpos :=
-      (pow_entry_pos_iff_exists_path (A := A) hA p.length i j).2 this
+      (pow_apply_pos_iff_nonempty_path (A := A) hA p.length i j).2 this
     simpa using hpos
   · intro h_exists
     constructor
     · exact hA
     · intro i j
       obtain ⟨k, hk_pos, hk_entry⟩ := h_exists i j
-      letI : Quiver n := toQuiver A
       obtain ⟨⟨p, hp_len⟩⟩ :=
-        (pow_entry_pos_iff_exists_path (A := A) hA k i j).mp hk_entry
+        (pow_apply_pos_iff_nonempty_path (A := A) hA k i j).mp hk_entry
       subst hp_len
       exact ⟨p, hk_pos⟩
 
 /-- If a nonnegative square matrix `A` is primitive, then `A` is irreducible. -/
-theorem IsPrimitive.to_IsIrreducible
-    [IsOrderedRing R] [PosMulStrictMono R] [Nontrivial R] [DecidableEq n]
+theorem IsPrimitive.isIrreducible
+    [Fintype n] [IsOrderedRing R] [PosMulStrictMono R] [Nontrivial R] [DecidableEq n]
     (h_prim : IsPrimitive A) : IsIrreducible A := by
   obtain ⟨h_nonneg, k, hk_pos, hk_all⟩ := h_prim
-  refine ⟨h_nonneg, fun i j ↦ ?_⟩
-  letI : Quiver n := toQuiver A
-  have ⟨⟨p, hp⟩⟩ : Nonempty {p : Path i j // p.length = k} :=
-    (pow_entry_pos_iff_exists_path h_nonneg k i j).mp (hk_all i j)
-  exact ⟨p, hp ▸ hk_pos⟩
+  rw [isIrreducible_iff_exists_pow_pos h_nonneg]
+  intro i j; simp_all only [gt_iff_lt]
+  apply Exists.intro
+  · apply And.intro
+    · exact hk_pos
+    · simp_all only
 
 /-! ## Transposition -/
 
 /-- Reverse a path in `toQuiver A` to a path in `toQuiver Aᵀ`, swapping endpoints. -/
-def transposeRev
-    {n R : Type*} [Ring R] [LinearOrder R] {A : Matrix n n R} :
-    ∀ {i j : n}, (@Quiver.Path n (toQuiver A) i j) →
-      (@Quiver.Path n (toQuiver Aᵀ) j i) := by
+def transposePath {i j : n} (p : @Quiver.Path n A.toQuiver i j) :
+    @Quiver.Path n Aᵀ.toQuiver j i := by
   letI : Quiver n := toQuiver A
-  intro i j p
   induction p with
   | nil =>
     exact (@Quiver.Path.nil _ (toQuiver Aᵀ) _)
@@ -218,14 +196,10 @@ def transposeRev
       simpa [Matrix.transpose_apply] using e
     exact (@Quiver.Path.comp n (toQuiver Aᵀ) c b i (@Quiver.Hom.toPath n (toQuiver Aᵀ) c b eT) ih)
 
-variable {n R : Type*} [Ring R] [LinearOrder R]
-variable {A : Matrix n n R}
-
 /-- Irreducibility is invariant under transpose. -/
-theorem IsIrreducible.transpose
-    (hA_nonneg : ∀ i j, 0 ≤ A i j) (hA : IsIrreducible A) : IsIrreducible Aᵀ := by
+theorem IsIrreducible.transpose (hA : IsIrreducible A) : IsIrreducible Aᵀ := by
   have hA_T_nonneg : ∀ i j, 0 ≤ (Aᵀ) i j := fun i j => by
-    simpa [Matrix.transpose_apply] using hA_nonneg j i
+    simpa [Matrix.transpose_apply] using hA.nonneg j i
   refine ⟨hA_T_nonneg, ?_⟩
   intro i j
   letI : Quiver n := toQuiver A
@@ -234,20 +208,30 @@ theorem IsIrreducible.transpose
   | nil =>
       exact False.elim ((lt_irrefl (0 : Nat)) (by simp [Quiver.Path.length] at hp_pos))
   | @cons b _ q e =>
-      let qT := transposeRev (A := A) (q.cons e)
+      let qT := transposePath (A := A) (q.cons e)
       letI : Quiver n := toQuiver Aᵀ
       have hqT_pos : 0 < qT.length := by
-        have : 0 < Nat.succ ((transposeRev (A := A) q).length) := Nat.succ_pos _
-        simp [qT, transposeRev, Quiver.Path.length_comp, Quiver.Path.length_toPath]
+        have : 0 < Nat.succ ((transposePath (A := A) q).length) := Nat.succ_pos _
+        simp [qT, transposePath, Quiver.Path.length_comp, Quiver.Path.length_toPath]
       exact ⟨qT, hqT_pos⟩
 
-theorem IsIrreducible.transpose_iff
-    (hA_nonneg : ∀ i j, 0 ≤ A i j) :
-    IsIrreducible Aᵀ ↔ IsIrreducible A :=
-  ⟨fun h ↦
+@[simp]
+theorem isIrreducible_transpose_iff :
+    Aᵀ.IsIrreducible ↔ A.IsIrreducible := by
+  by_cases hA_nonneg : ∀ i j, 0 ≤ A i j
+  · exact ⟨fun h ↦
     let hA_T_nonneg : ∀ i j, 0 ≤ (Aᵀ) i j := fun i j => by
       simpa [Matrix.transpose_apply] using hA_nonneg j i
-    IsIrreducible.transpose hA_T_nonneg h,
-   fun h ↦ IsIrreducible.transpose hA_nonneg h⟩
+    IsIrreducible.transpose h,
+   fun h ↦ IsIrreducible.transpose h⟩
+  · have : ¬ Aᵀ.IsIrreducible := by
+      rw [isIrreducible_iff]
+      simp only [transpose_apply, isSStronglyConnected_iff, not_and, not_forall, not_exists,
+        not_lt, nonpos_iff_eq_zero]
+      intro a; simp_all only [implies_true, not_true_eq_false]
+    have : ¬ A.IsIrreducible := by
+      rw [isIrreducible_iff]; simp_all only [not_forall, not_le, isSStronglyConnected_iff,
+      not_and, not_exists, not_lt, nonpos_iff_eq_zero, isEmpty_Prop, IsEmpty.forall_iff]
+    simp_all only [not_forall, not_le]
 
 end Matrix
