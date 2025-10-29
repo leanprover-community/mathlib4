3a7ff935bca633750b30754ab694ee6666f861cd
Joël Riou <37772949+joelriou@users.noreply.github.com>
2025-10-29 00:35:05 +0000

    feat(Topology/Sheaves): the equivalence `Over U ≌ Opens U` for `U : Opens X` (#30415)

--- Mathlib.lean
+++ Mathlib.lean
@@ -6983,6 +6983,7 @@ import Mathlib.Topology.Sheaves.Limits
 import Mathlib.Topology.Sheaves.LocalPredicate
 import Mathlib.Topology.Sheaves.LocallySurjective
 import Mathlib.Topology.Sheaves.MayerVietoris
+import Mathlib.Topology.Sheaves.Over
 import Mathlib.Topology.Sheaves.PUnit
 import Mathlib.Topology.Sheaves.Presheaf
 import Mathlib.Topology.Sheaves.PresheafOfFunctions
