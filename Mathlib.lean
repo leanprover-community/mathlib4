f51aaf5a2f438aaf7c0f30ab5d866e7f5829e880
Joël Riou <37772949+joelriou@users.noreply.github.com>
2025-10-29 00:03:58 +0000

    feat(CategoryTheory/ObjectProperty): closure of a property under certain limits (#29854)
    In this PR, given a property `P` of objects in a category `C` and family of categories `J : α → Type _`, we introduce the closure `P.limitsClosure J` of `P` under limits of shapes `J a` for all `a : α`, and under certain smallness circumstances, we show that its essentially small.
    
    - [x] depends on: #29851
    - [x] depends on: #29881
    - [x] depends on: #29903
    - [x] depends on: #29849
    - [x] depends on: #29843

--- Mathlib.lean
+++ Mathlib.lean
@@ -2717,11 +2717,13 @@ import Mathlib.CategoryTheory.Noetherian
 import Mathlib.CategoryTheory.ObjectProperty.Basic
 import Mathlib.CategoryTheory.ObjectProperty.ClosedUnderIsomorphisms
 import Mathlib.CategoryTheory.ObjectProperty.ColimitsOfShape
+import Mathlib.CategoryTheory.ObjectProperty.CompleteLattice
 import Mathlib.CategoryTheory.ObjectProperty.ContainsZero
 import Mathlib.CategoryTheory.ObjectProperty.EpiMono
 import Mathlib.CategoryTheory.ObjectProperty.Extensions
 import Mathlib.CategoryTheory.ObjectProperty.FullSubcategory
 import Mathlib.CategoryTheory.ObjectProperty.Ind
+import Mathlib.CategoryTheory.ObjectProperty.LimitsClosure
 import Mathlib.CategoryTheory.ObjectProperty.LimitsOfShape
 import Mathlib.CategoryTheory.ObjectProperty.Local
 import Mathlib.CategoryTheory.ObjectProperty.Opposite
