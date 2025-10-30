1574ad5b182b022210e0b7d2e680059f189a01e3
Kenny Lau <kc_kennylau@yahoo.com.hk>
2025-10-30 09:46:00 +0000

    docstring

--- Mathlib/Util/OpenUnscoped.lean
+++ Mathlib/Util/OpenUnscoped.lean
@@ -5,11 +5,52 @@ Authors: Kenny Lau
 -/
 import Mathlib.Init
 
+/-! # Open unscoped
+
+`open unscoped Foo` opens the namespace `Foo` without activating its scope,
+meaning without activating any of its scoped instances or notations.
+
+Example usage:
+```lean
+namespace Nat
+
+def pi : Nat := 3
+scoped notation "π" => pi
+
+end Nat
+
+open unscoped Nat
+
+#check pi
+#check π -- error
+```
+-/
+
 namespace Lean.Elab.Command
 
+/-- Adds a declaration or a namespace to the list of opened namespaces in the scope. -/
 def Scope.addOpenDecl (o : OpenDecl) (oldScope : Scope) : Scope :=
   { oldScope with openDecls := o :: oldScope.openDecls }
 
+/--
+`open unscoped Foo` opens the namespace `Foo` without activating its scope,
+meaning without activating any of its scoped instances or notations.
+
+Example usage:
+```lean
+namespace Nat
+
+def pi : Nat := 3
+scoped notation "π" => pi
+
+end Nat
+
+open unscoped Nat
+
+#check pi
+#check π -- error
+```
+-/
 elab "open " "unscoped " nss:ident* : command => do
   for ns in nss do
     for ns in (← resolveNamespace ns) do
