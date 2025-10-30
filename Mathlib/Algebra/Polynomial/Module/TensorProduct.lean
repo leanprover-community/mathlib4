4e85712502e17a7ee56e7fffb3789cba6aaf110e
George McNinch <gmcninch@gmail.com>
2025-10-29 21:16:48 -0400

    add and use ext-lemmas

--- Mathlib/Algebra/Polynomial/Module/TensorProduct.lean
+++ Mathlib/Algebra/Polynomial/Module/TensorProduct.lean
@@ -25,18 +25,15 @@ namespace PolynomialModule
 /-- The `R[X]`-linear equivalence `(R[X] ⊗[R] M) ≃ₗ[R[X]] (PolynomialModule R M)`.
 -/
 def polynomialTensorProductLEquivPolynomialModule :
-  R[X] ⊗[R] M ≃ₗ[R[X]] PolynomialModule R M :=
-  let e := liftBaseChange R[X] <| PolynomialModule.lsingle R (M := M) 0
-  let inv : PolynomialModule R M →ₗ[R] _ :=
-    Finsupp.lsum R fun n ↦ TensorProduct.mk R R[X] M (X ^ n)
+    R[X] ⊗[R] M ≃ₗ[R[X]] PolynomialModule R M :=
+  let e := liftBaseChange R[X] <| lsingle R (M := M) 0
+  let inv := (eval X).restrictScalars R ∘ₗ map R[X] (TensorProduct.mk R R[X] M 1)
   have left : inv ∘ₗ e = .id := by
     ext n x
-    simpa [inv, e] using (Finsupp.sum_single_index (by simp)).trans <| by
-      simp [monomial_one_right_eq_X_pow]
-
+    simp [inv, e, ← monomial_one_right_eq_X_pow, smul_tmul']
   have right : e.restrictScalars R ∘ₗ inv = .id := by
-    refine Finsupp.lhom_ext' fun n ↦ LinearMap.ext fun x ↦ ?_
-    simpa [e, inv, ← monomial_one_right_eq_X_pow] using by rfl
+    ext n x
+    simp [e, inv, ← monomial_one_right_eq_X_pow]
   { __ := e
     invFun := inv
     left_inv := (congr($left ·))
