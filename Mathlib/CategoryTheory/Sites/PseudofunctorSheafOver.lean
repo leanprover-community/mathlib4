52a08004da7239770cb7838aff3ce91c3b5536a9
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-10-30 13:18:04 +0100

    fixing imports

--- Mathlib/CategoryTheory/Sites/PseudofunctorSheafOver.lean
+++ Mathlib/CategoryTheory/Sites/PseudofunctorSheafOver.lean
@@ -3,7 +3,6 @@ Copyright (c) 2025 Joël Riou. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joël Riou
 -/
-import Mathlib.CategoryTheory.Bicategory.Functor.Cat
 import Mathlib.CategoryTheory.Bicategory.Functor.LocallyDiscrete
 import Mathlib.CategoryTheory.Sites.Over
 
