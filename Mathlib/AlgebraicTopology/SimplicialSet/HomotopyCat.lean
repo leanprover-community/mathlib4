13281c703556e946615601a1f0f8451bdd41a58c
Emily Riehl <eriehl@jhu.edu>
2025-10-30 16:31:17 -0400

    attempt to fix bad universe levels linter

--- Mathlib/AlgebraicTopology/SimplicialSet/HomotopyCat.lean
+++ Mathlib/AlgebraicTopology/SimplicialSet/HomotopyCat.lean
@@ -327,7 +327,7 @@ end
 
 /-- For a simplicial set `X`, the underlying type of `hoFunctor.obj X` is equivalent to `X _⦋0⦌`. -/
 def hoFunctor.obj.equiv (X : SSet) : hoFunctor.obj X ≃ X _⦋0⦌ :=
-  (Quotient.equiv _).trans (Quotient.equiv _)
+  (Quotient.equiv.{u,u} _).trans (Quotient.equiv _)
 section
 
 /-- Since `⦋0⦌ : SimplexCategory` is terminal, `Δ[0]` has a unique point and thus
