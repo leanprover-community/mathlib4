4e6309477f387bade18ecbf0f29377683b853a4a
Daniel Figueroa <40865765+daefigueroa@users.noreply.github.com>
2025-10-30 23:03:37 +0100

    add suggested changes from review and make docstrings uniform

--- Mathlib/Dynamics/Transitive.lean
+++ Mathlib/Dynamics/Transitive.lean
@@ -29,9 +29,9 @@ class AddAction.IsTopologicallyTransitive (M α : Type*) [AddMonoid M] [Topologi
   exists_vadd_inter : ∀ {U V : Set α}, IsOpen U → U.Nonempty → IsOpen V → V.Nonempty →
     ∃ m : M, ((m +ᵥ U) ∩ V).Nonempty
 
-/-- An action of a monoid `M` on a topological space `α` is called *topologically transitive* if
-for any pair of nonempty open sets `U` and `V` in `α` there exists an `m : M` such that
-`(m • U) ∩ V` is nonempty. -/
+/-- An action of a monoid `M` on a topological space `α` is called *topologically transitive* if for
+any pair of nonempty open sets `U` and `V` in `α` there exists an `m : M` such that `(m • U) ∩ V` is
+nonempty. -/
 @[to_additive]
 class MulAction.IsTopologicallyTransitive (M α : Type*) [Monoid M] [TopologicalSpace α]
     [MulAction M α] : Prop where
@@ -49,18 +49,24 @@ theorem MulAction.isTopologicallyTransitive_iff :
     IsTopologicallyTransitive M α ↔ ∀ {U V : Set α}, IsOpen U → U.Nonempty → IsOpen V →
     V.Nonempty → ∃ m : M, ((m • U) ∩ V).Nonempty := ⟨fun h ↦ h.1, fun h ↦ ⟨h⟩⟩
 
-/-- A monoid action on `α` by `M` is topologically transitive if and only if for any nonempty
+/-- An action of a monoid `M` on `α` is topologically transitive if and only if for any nonempty
 open subset `U` of `α` the union over the elements of `M` of images of `U` is dense in `α`. -/
-@[to_additive]
+@[to_additive /-- An action of an additive monoid `M` on `α` is topologically transitive if and only
+if for any nonempty open subset `U` of `α` the union over the elements of `M` of images of `U` is
+dense in `α`. -/]
 theorem MulAction.isTopologicallyTransitive_iff_dense_iUnion :
     IsTopologicallyTransitive M α ↔
     ∀ {U : Set α}, IsOpen U → U.Nonempty → Dense (⋃ m : M, m • U) := by
-  simp [isTopologicallyTransitive_iff, dense_iff_inter_open, inter_iUnion, inter_comm]
+  simp only [isTopologicallyTransitive_iff, inter_comm, dense_iff_inter_open, inter_iUnion,
+    nonempty_iUnion]
   exact ⟨fun h _ h₁ h₂ _ h₃ h₄ ↦ h h₁ h₂ h₃ h₄, fun h _ _ h₁ h₂ h₃ h₄ ↦ h h₁ h₂ _ h₃ h₄⟩
 
-/-- A monoid action on `α` by `M` is topologically transitive if and only if for any nonempty open
-subset `U` of `α` the union of the preimages of `U` over the elements of `M` is dense in `α`. -/
-@[to_additive]
+/-- An action of a monoid `M` on `α` is topologically transitive if and only if for any nonempty
+open subset `U` of `α` the union of the preimages of `U` over the elements of `M` is dense in `α`.
+-/
+@[to_additive /-- An action of an additive monoid `M` on `α` is topologically transitive if and only
+if for any nonempty open subset `U` of `α` the union of the preimages of `U` over the elements of
+`M` is dense in `α`. -/]
 theorem MulAction.isTopologicallyTransitive_iff_dense_iUnion_preimage :
     IsTopologicallyTransitive M α ↔
     ∀ {U : Set α}, IsOpen U → U.Nonempty → Dense (⋃ m : M, (m • ·) ⁻¹' U) := by
@@ -77,25 +83,30 @@ theorem IsOpen.dense_iUnion_preimage_smul [h : IsTopologicallyTransitive M α]
     {U : Set α} (hUo : IsOpen U) (hUne : U.Nonempty) : Dense (⋃ m : M, (m • ·) ⁻¹' U) :=
   (isTopologicallyTransitive_iff_dense_iUnion_preimage M).mp h hUo hUne
 
-/-- Let `M` be a topologically transitive monoid action on `α`. If `U : Set α` is nonempty and
-`(⋃ m : M, (m • ·) ⁻¹' U) ⊆ U` then `U` is dense in `α`. -/
-@[to_additive]
+/-- Let `M` be a monoid with a topologically transitive action on `α`. If `U` is a nonempty open
+subset of `α` and `(m • ·) ⁻¹' U ⊆ U` for all `m : M` then `U` is dense in `α`. -/
+@[to_additive /-- Let `M` be an additive monoid with a topologically transitive action on `α`. If
+`U` is a nonempty open subset of `α` and `(m +ᵥ ·) ⁻¹' U ⊆ U` for all `m : M` then `U` is dense in
+`α`. -/]
 theorem IsOpen.dense_of_preimage_smul_invariant [IsTopologicallyTransitive M α] {U : Set α}
-    (hUo : IsOpen U) (hUne : U.Nonempty) (hUinv : (⋃ m : M, (m • ·) ⁻¹' U) ⊆ U) : Dense U :=
-  .mono hUinv (hUo.dense_iUnion_preimage_smul M hUne)
+    (hUo : IsOpen U) (hUne : U.Nonempty) (hUinv : ∀ m : M, (m • ·) ⁻¹' U ⊆ U) : Dense U :=
+  .mono (by simpa only [iUnion_subset_iff]) (hUo.dense_iUnion_preimage_smul M hUne)
 
-/-- A continuous monoid action on `α` by `M` is topologically transitive if and only if any
-nonempty open subset `U` of `α` with `(⋃ m : M, (m • ·) ⁻¹' U) ⊆ U` is dense in `α`. -/
-@[to_additive]
+/-- An action of a monoid `M` on `α` that is continuous in the second argument is topologically
+transitive if and only if any nonempty open subset `U` of `α` with `(m • ·) ⁻¹' U ⊆ U` for all
+`m : M` is dense in `α`. -/
+@[to_additive /-- An action of an additive monoid `M` on `α` that is continuous in the second
+argument is topologically transitive if and only if any nonempty open subset `U` of `α` with
+`(m +ᵥ ·) ⁻¹' U ⊆ U` for all `m : M` is dense in `α`. -/]
 theorem MulAction.isTopologicallyTransitive_iff_dense_of_preimage_invariant
     [h : ContinuousConstSMul M α] : IsTopologicallyTransitive M α ↔
-    ∀ {U : Set α}, IsOpen U → U.Nonempty → ⋃ m : M, (m • ·) ⁻¹' U ⊆ U → Dense U := by
+    ∀ {U : Set α}, IsOpen U → U.Nonempty → (∀ m : M, (m • ·) ⁻¹' U ⊆ U) → Dense U := by
   refine ⟨fun _ _ h₀ h₁ h₂ ↦ h₀.dense_of_preimage_smul_invariant M h₁ h₂, fun h₄ ↦ ?_⟩
   refine (isTopologicallyTransitive_iff_dense_iUnion_preimage M).mpr ?_
-  refine fun hU _ ↦ h₄ (isOpen_iUnion fun a ↦ hU.preimage (h.1 a)) ?_ fun x hx ↦ ?_
-  · exact nonempty_iUnion.mpr ⟨1, by simpa [one_smul]⟩
-  · simp only [mem_iUnion, mem_preimage, smul_smul] at hx ⊢
-    exact match hx with | ⟨i, j, h₀⟩ => ⟨j * i, h₀⟩
+  refine fun hU _ ↦ h₄ (isOpen_iUnion fun a ↦ hU.preimage (h.1 a)) ?_ fun b _ ↦ ?_
+  · exact nonempty_iUnion.mpr ⟨1, by simpa only [one_smul]⟩
+  · simp only [preimage_iUnion, mem_iUnion, mem_preimage, smul_smul, forall_exists_index]
+    exact fun c hc ↦ ⟨c * b, hc⟩
 
 @[to_additive]
 instance MulAction.isTopologicallyTransitive_of_isMinimal [IsMinimal M α] :
