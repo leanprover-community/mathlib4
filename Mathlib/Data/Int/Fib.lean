21ef5e33d82786b6119e91be51067cdf6e8f6d94
Monica Omar <2497250a@research.gla.ac.uk>
2025-10-30 13:42:51 +0000

    fib_add

--- Mathlib/Data/Int/Fib.lean
+++ Mathlib/Data/Int/Fib.lean
@@ -33,12 +33,16 @@ def fib (n : ℤ) : ℤ :=
 @[simp] theorem fib_two : fib 2 = 1 := rfl
 @[simp] theorem fib_neg_one : fib (-1) = 1 := rfl
 @[simp] theorem fib_neg_two : fib (-2) = -1 := rfl
+
 theorem fib_of_nonneg {n : ℤ} (hn : 0 ≤ n) : fib n = n.toNat.fib := by simp [fib, hn]
+
 theorem fib_of_odd {n : ℤ} (hn : Odd n) : fib n = (natAbs n).fib := by grind [fib]
-theorem fib_two_mul_add_one {n : ℤ} : fib (2 * n + 1) = (natAbs (2 * n + 1)).fib :=
+
+theorem fib_two_mul_add_one_eq_natFib_natAbs {n : ℤ} : fib (2 * n + 1) = (natAbs (2 * n + 1)).fib :=
   fib_of_odd <| odd_two_mul_add_one n
+
 theorem fib_two_mul_add_one_nonneg {n : ℤ} : 0 ≤ fib (2 * n + 1) := by
-  simp [fib_two_mul_add_one]
+  simp [fib_two_mul_add_one_eq_natFib_natAbs]
 
 theorem fib_neg_natCast (n : ℕ) : fib (-n) = (-1) ^ (n + 1) * n.fib := by
   rcases n.even_or_odd with (hn | hn)
@@ -82,7 +86,8 @@ theorem fib_add_one (n : ℤ) : fib (n + 1) = fib (n + 2) - fib n := by
   obtain ⟨n, (rfl | rfl)⟩ := n.eq_nat_or_neg <;> simp [fib_neg_natCast]
 
 -- auxiliary for `fib_add`
-theorem fib_natCast_add : ∀ (m : ℕ) (n : ℤ), fib (m + n) = fib (m - 1) * fib n + fib m * fib (n + 1)
+private theorem fib_natCast_add :
+    ∀ (m : ℕ) (n : ℤ), fib (m + n) = fib (m - 1) * fib n + fib m * fib (n + 1)
   | 0, _ => by simp
   | 1, _ => by simp [add_comm]
   | m + 2, n => by
@@ -95,6 +100,46 @@ theorem fib_natCast_add : ∀ (m : ℕ) (n : ℤ), fib (m + n) = fib (m - 1) * f
         _ = fib (m + 1) * fib n + fib (m + 2) * fib (n + 1) := by grind [fib_add_two]
         _ = _ := by grind
 
--- TODO: `fib_add`, `fib_two_mul`, `fib_two_mul_add_one'`, `fib_two_mul_add_two`, `fib_dvd`, ...
+-- auxiliary for `fib_add`
+private theorem fib_add_natCast :
+    ∀ (m : ℤ) (n : ℕ), fib (m + n) = fib (m - 1) * fib n + fib m * fib (n + 1)
+  | _, 0 => by simp
+  | _, 1 => by
+      simp only [Nat.cast_one, fib_one, mul_one, reduceAdd, fib_two, add_comm]
+      grind [fib_add_two]
+  | n, m + 2 =>
+      calc _ = fib (m + n) + fib (m + n + 1) := by grind [fib_add_two]
+        _ = fib (m - 1) * fib n + fib m * fib (n + 1) + fib ((m + 1 : ℕ) + n) := by
+          rw [fib_natCast_add]; grind
+        _ = fib (m - 1) * fib n + fib m * fib (n + 1) + fib m * fib n +
+            fib (m + 1) * fib (n + 1) := by rw [fib_natCast_add]; grind
+        _ = (fib (m - 1) + fib m) * fib n + (fib m + fib (m + 1)) * fib (n + 1) := by grind
+        _ = fib (n - 1) * fib (m + 2) + fib n * fib (m + 3) := by grind [fib_add_two]
+        _ = _ := by grind
+
+-- auxiliary for `fib_add`
+private theorem fib_neg_natCast_add_neg_natCast :
+    ∀ (m n : ℕ), fib (-m + -n) = fib (-m - 1) * fib (-n) + fib (-m) * fib (-n + 1)
+  | _, 0 => by simp
+  | _, 1 => by simp [sub_eq_neg_add, add_comm]
+  | m, n + 2 =>
+      calc _ = fib (-m + -n) - fib (-m + -(n + 1 : ℕ)) := by grind [fib_add_two]
+        _ = fib (-m - 1) * fib (-n) + fib (-m) * fib (-n + 1) -
+            fib (-m - 1) * fib (-n - 1) - fib (-m) * fib (-n) := by
+          conv_lhs => rw [fib_neg_natCast_add_neg_natCast,
+            fib_neg_natCast_add_neg_natCast]
+          grind
+        _ = fib (-m - 1) * (fib (-n) - fib (-n - 1)) +
+            fib (-m) * (fib (-n + 1) - fib (-n)) := by grind [fib_add_two]
+        _ = _ := by grind [fib_add_two]
+
+theorem fib_add (m n : ℤ) : fib (m + n) = fib (m - 1) * fib n + fib m * fib (n + 1) := by
+  obtain ⟨m, (rfl | rfl)⟩ := m.eq_nat_or_neg
+  · exact fib_natCast_add _ _
+  · obtain ⟨n, (rfl | rfl)⟩ := n.eq_nat_or_neg
+    · exact fib_add_natCast _ _
+    · exact fib_neg_natCast_add_neg_natCast _ _
+
+-- TODO: `fib_two_mul`, `fib_two_mul_add_one`, `fib_two_mul_add_two`, `fib_dvd`, ...
 
 end Int
