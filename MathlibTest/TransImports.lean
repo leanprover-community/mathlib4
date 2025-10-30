985e8a879b3b159f02f4531edc11f5d3be60d65b
Joachim Breitner <mail@joachim-breitner.de>
2025-10-30 21:09:09 +0100

    Updating test (not sure if this is a bump-when-broken test)

--- MathlibTest/TransImports.lean
+++ MathlibTest/TransImports.lean
@@ -1,10 +1,10 @@
 import Mathlib.Util.TransImports
 
 /--
-info: 'MathlibTest.TransImports' has at most 1000 transitive imports
+info: 'MathlibTest.TransImports' has at most 2000 transitive imports
 
 2 starting with "Mathlib.Tactic.Linter.H":
 [Mathlib.Tactic.Linter.HashCommandLinter, Mathlib.Tactic.Linter.Header]
 -/
 #guard_msgs in
-#trans_imports "Mathlib.Tactic.Linter.H" at_most 1000
+#trans_imports "Mathlib.Tactic.Linter.H" at_most 2000
