659d7c1caf02b29193c1b5a7bb91cda2bc5db156
Thomas Browning <13339017+tb65536@users.noreply.github.com>
2025-10-31 08:47:57 +0000

    doc(FieldTheory/Galois/IsGaloisGroup): clarify terminological issues (#31029)
    This PR expands to docstring of `FieldTheory/Galois/IsGaloisGroup.lean` to clarify the terminological issues that have been raised in this Zulip thread: https://leanprover.zulipchat.com/#narrow/channel/144837-PR-reviews/topic/.2330791.20generalizing.20.60IsGaloisGroup.60
    
    Co-authored-by: tb65536 <thomas.l.browning@gmail.com>

--- Mathlib/FieldTheory/Galois/IsGaloisGroup.lean
+++ Mathlib/FieldTheory/Galois/IsGaloisGroup.lean
@@ -16,9 +16,20 @@ we do not assume that `L` is an algebraic extension of `K`.
 ## Implementation notes
 
 We actually define `IsGaloisGroup G A B` for extensions of rings `B/A`, with the same definition
-(faithful action on `B` with fixed field `A`). In this generality, the name `IsGaloisGroup` is a
-bit of a misnomer, since `IsGaloisGroup G A B` corresponds to the fields of fractions
-`Frac(B)/Frac(A)` being a Galois extension of fields, rather than `B/A` being étale (for instance).
+(faithful action on `B` with fixed ring `A`). This definition turns out to axiomatize a common
+setup in algebraic number theory where a Galois group `Gal(L/K)` acts on an extension of subrings
+`B/A` (e.g., rings of integers). In particular, there are theorems in algebraic number theory that
+naturally assume `[IsGaloisGroup G A B]` and whose statements would otherwise require assuming
+`(K L : Type*) [Field K] [Field L] [Algebra K L] [IsGalois K L]` (along with predicates relating
+`K` and `L` to the rings `A` and `B`) despite `K` and `L` not appearing in the conclusion.
+
+Unfortunately, this definition of `IsGaloisGroup G A B` for extensions of rings `B/A` is
+nonstandard and clashes with other notions such as the étale fundamental group. In particular, if
+`A` is integrally closed, then  `IsGaloisGroup G A B` is equivalent to `B/A` being integral and the
+fields of fractions `Frac(B)/Frac(A)` being Galois, rather than `B/A` being étale for instance.
+
+But in the absence of a more suitable name, the utility of the predicate `IsGaloisGroup G A B` for
+extensions of rings `B/A` seems to outweigh these terminological issues.
 -/
 
 section CommRing
