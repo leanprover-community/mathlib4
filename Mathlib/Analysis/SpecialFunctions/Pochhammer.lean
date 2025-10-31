ba7e908354ae18beddddf110f3d0fc0c78582925
Zhao Yuyang 赵雨扬 <zhao.yu-yang@foxmail.com>
2025-10-31 09:48:46 +0000

    chore(Data/Nat/{Factorial, Choose}/Cast): move lemmas about `Polynomial` (#30745)
    The remaining lemmas in the files are unrelated to `ascPochhammer` and `descPochhammer`. This significantly reduces the imports.

--- Mathlib/Analysis/SpecialFunctions/Pochhammer.lean
+++ Mathlib/Analysis/SpecialFunctions/Pochhammer.lean
@@ -6,7 +6,6 @@ Authors: Mitchell Horner
 import Mathlib.Algebra.BigOperators.Field
 import Mathlib.Analysis.Convex.Deriv
 import Mathlib.Analysis.Convex.Piecewise
-import Mathlib.Data.Nat.Choose.Cast
 import Mathlib.Analysis.Convex.Jensen
 
 /-!
--- Mathlib/Data/Nat/Factorial/Cast.lean
+++ Mathlib/Data/Nat/Factorial/Cast.lean
@@ -3,7 +3,8 @@ Copyright (c) 2021 Yaël Dillies. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yaël Dillies
 -/
-import Mathlib.RingTheory.Polynomial.Pochhammer
+import Mathlib.Data.Nat.Cast.Basic
+import Mathlib.Data.Nat.Factorial.Basic
 
 /-!
 # Cast of factorials
@@ -24,30 +25,6 @@ variable (S : Type*)
 
 namespace Nat
 
-section Semiring
-
-variable [Semiring S] (a b : ℕ)
-
-theorem cast_ascFactorial : a.ascFactorial b = (ascPochhammer S b).eval (a : S) := by
-  rw [← ascPochhammer_nat_eq_ascFactorial, ascPochhammer_eval_cast]
-
-theorem cast_descFactorial :
-    a.descFactorial b = (ascPochhammer S b).eval (a - (b - 1) : S) := by
-  rw [← ascPochhammer_eval_cast, ascPochhammer_nat_eq_descFactorial]
-  induction b with
-  | zero => simp
-  | succ b =>
-    simp_rw [add_succ, Nat.add_one_sub_one]
-    obtain h | h := le_total a b
-    · rw [descFactorial_of_lt (lt_succ_of_le h), descFactorial_of_lt (lt_succ_of_le _)]
-      rw [tsub_eq_zero_iff_le.mpr h, zero_add]
-    · rw [tsub_add_cancel_of_le h]
-
-theorem cast_factorial : (a ! : S) = (ascPochhammer S a).eval 1 := by
-  rw [← one_ascFactorial, cast_ascFactorial, cast_one]
-
-end Semiring
-
 section Ring
 
 variable [Ring S] (a b : ℕ)
@@ -55,12 +32,10 @@ variable [Ring S] (a b : ℕ)
 /-- Convenience lemma. The `a - 1` is not using truncated subtraction, as opposed to the definition
 of `Nat.descFactorial` as a natural. -/
 theorem cast_descFactorial_two : (a.descFactorial 2 : S) = a * (a - 1) := by
-  rw [cast_descFactorial]
+  rw [descFactorial]
   cases a
   · simp
-  · rw [succ_sub_succ, tsub_zero, cast_succ, add_sub_cancel_right, ascPochhammer_succ_right,
-      ascPochhammer_one, Polynomial.X_mul, Polynomial.eval_mul_X, Polynomial.eval_add,
-      Polynomial.eval_X, cast_one, Polynomial.eval_one]
+  · simp [mul_add, add_mul]
 
 end Ring
 
