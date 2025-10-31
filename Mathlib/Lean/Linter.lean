5c76e957fe5cbbbfdf25a4eec8f3b74adb2f6607
thorimur <thomasmurrills@gmail.com>
2025-10-31 18:06:27 -0400

    chore: minimize import

--- Mathlib/Lean/Linter.lean
+++ Mathlib/Lean/Linter.lean
@@ -3,7 +3,7 @@ Copyright (c) 2025 Thomas R. Murrills. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Thomas R. Murrills
 -/
-import Lean
+import Lean.Elab.Command
 
 /-
 # Additional utilities for the `Linter` API
