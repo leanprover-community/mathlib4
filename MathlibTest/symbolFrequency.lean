fda263bf1fd080ed06ea904e75ade1d9d4304bd7
Kim Morrison <kim@tqft.net>
2025-10-31 21:59:25 +1100

    comment out more

--- MathlibTest/symbolFrequency.lean
+++ MathlibTest/symbolFrequency.lean
@@ -1,12 +1,12 @@
-import Mathlib
+-- import Mathlib
 
-open Lean PremiseSelection
+-- open Lean PremiseSelection
 
--- Check that the symbol frequency extension is working in Mathlib.
--- There should be at least 10_000 theorems mentioning `Nat`.
+-- -- Check that the symbol frequency extension is working in Mathlib.
+-- -- There should be at least 10_000 theorems mentioning `Nat`.
 
-/-- info: true -/
-#guard_msgs in
-run_meta do
-  let n ← symbolFrequency `Nat
-  logInfo s!"{decide (10_000 ≤ n)}"
+-- /-- info: true -/
+-- #guard_msgs in
+-- run_meta do
+--   let n ← symbolFrequency `Nat
+--   logInfo s!"{decide (10_000 ≤ n)}"
