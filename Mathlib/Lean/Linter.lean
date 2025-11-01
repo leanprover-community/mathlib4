c9731fdd64acbdebaa5ea67e1b50c3d7458e73ee
thorimur <thomasmurrills@gmail.com>
2025-10-31 20:54:59 -0400

    chore: mark `@[macro_inline]`, document

--- Mathlib/Lean/Linter.lean
+++ Mathlib/Lean/Linter.lean
@@ -13,16 +13,31 @@ open Lean Elab Command Linter
 
 namespace Lean.Linter
 
-/-- Runs a `CommandElabM` action when the provided linter option is `true`. -/
+/--
+Runs a `CommandElabM` action when the provided linter option is `true`.
+
+Note: this definition is marked as `@[macro_inline]`, so it is okay to supply it with a linter option which has been registered in the same module.
+-/
+@[macro_inline]
 def whenLinterOption (opt : Lean.Option Bool) (x : CommandElabM Unit) : CommandElabM Unit := do
   if getLinterValue opt (← getLinterOptions) then x
 
-/-- Runs a `CommandElabM` action when the provided linter option is `false`. -/
+/--
+Runs a `CommandElabM` action when the provided linter option is `false`.
+
+Note: this definition is marked as `@[macro_inline]`, so it is okay to supply it with a linter option which has been registered in the same module.
+-/
+@[macro_inline]
 def whenNotLinterOption (opt : Lean.Option Bool) (x : CommandElabM Unit) : CommandElabM Unit := do
   unless getLinterValue opt (← getLinterOptions) do x
 
-/-- Processes `set_option ... in`s that wrap the input `stx`, then acts on the inner syntax with
-`x` after checking that the provided linter option is `true`. -/
+/--
+Processes `set_option ... in`s that wrap the input `stx`, then acts on the inner syntax with
+`x` after checking that the provided linter option is `true`.
+
+Note: this definition is marked as `@[macro_inline]`, so it is okay to supply it with a linter option which has been registered in the same module.
+-/
+@[macro_inline]
 def whenLinterActivated (opt : Lean.Option Bool) (x : CommandElab) : CommandElab :=
   withSetOptionIn (whenLinterOption opt ∘ x)
 
