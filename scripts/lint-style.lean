73152319156e77e3b347e8e88730d7d172dc4340
thorimur <thomasmurrills@gmail.com>
2025-10-31 20:29:10 -0400

    chore: fix `lint-style`?

--- scripts/lint-style.lean
+++ scripts/lint-style.lean
@@ -140,6 +140,8 @@ def missingInitImports (opts : LinterOptions) : IO Nat := do
   let missing := modulesWithoutMathlibImports.erase `Mathlib.Tactic.Linter.Header
     -- This file is imported by `Mathlib/Tactic/Linter/Header.lean`.
     |>.erase `Mathlib.Tactic.Linter.DirectoryDependency
+    -- This file is imported by `Mathlib/Tactic/Linter/Header.lean`.
+    |>.erase `Mathlib.Lean.Linter
   if missing.size > 0 then
     IO.eprintln s!"error: the following {missing.size} module(s) do not import Mathlib.Init: \
       {missing}"
