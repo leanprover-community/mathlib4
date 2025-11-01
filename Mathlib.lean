258c9cbd09be9884eb34749eec1454f74499b4f1
FernandoChu <17756312+FernandoChu@users.noreply.github.com>
2025-11-01 20:09:01 +0100

    lint

--- Mathlib.lean
+++ Mathlib.lean
@@ -2790,6 +2790,7 @@ import Mathlib.CategoryTheory.Products.Unitor
 import Mathlib.CategoryTheory.Quotient
 import Mathlib.CategoryTheory.Quotient.Linear
 import Mathlib.CategoryTheory.Quotient.Preadditive
+import Mathlib.CategoryTheory.RegularCategory.Basic
 import Mathlib.CategoryTheory.Retract
 import Mathlib.CategoryTheory.Shift.Adjunction
 import Mathlib.CategoryTheory.Shift.Basic
