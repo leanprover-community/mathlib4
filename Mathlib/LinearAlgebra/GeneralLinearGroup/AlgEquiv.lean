4b9c3df8aa63014861f4371822319762b180c92b
Monica Omar <23701951+themathqueen@users.noreply.github.com>
2025-11-01 15:45:29 +0000

    Update AlgEquiv.lean

--- Mathlib/LinearAlgebra/GeneralLinearGroup/AlgEquiv.lean
+++ Mathlib/LinearAlgebra/GeneralLinearGroup/AlgEquiv.lean
@@ -18,77 +18,69 @@ In other words, the map `MulSemiringAction.toAlgEquiv` from `GeneralLinearGroup 
 -/
 
 namespace Module.End
-variable {R V : Type*} [AddCommGroup V]
+variable {R K V : Type*} [CommSemiring R] [AddCommGroup V] [Module R V] [Field K] [Module K V]
 
 open Module LinearMap End
 
 /-- This takes in a linear map `f : End R V →ₗ End R V`, a dual `y : Dual R V`,
 and an element `z : V`, and constructs a linear operator on `V` such that
 `x ↦ f (smulRightₗ y x) z`. -/
-private def auxLinear [CommSemiring R] [Module R V] (f : End R V →ₗ[R] End R V)
-    (y : V →ₗ[R] R) (z : V) : End R V :=
+private def auxLinear (f : End R V →ₗ[R] End R V) (y : Dual R V) (z : V) : End R V :=
   applyₗ z ∘ₗ f ∘ₗ smulRightₗ y
 
-@[simp] private theorem auxLinear_apply [CommSemiring R] [Module R V]
-    (f : End R V →ₗ[R] End R V) (y : Dual R V) (x z : V) :
+@[simp] private theorem auxLinear_apply (f : End R V →ₗ[R] End R V) (y : Dual R V) (x z : V) :
   auxLinear f y z x = f (smulRightₗ y x) z := rfl
 
-private theorem auxLinear_map_apply [CommSemiring R] [Module R V]
-    (f : End R V →ₐ[R] End R V) (y : Dual R V) (z : V) (A : End R V) (x : V) :
+private theorem auxLinear_map_apply (f : End R V →ₐ[R] End R V) (y : Dual R V)
+    (z : V) (A : End R V) (x : V) :
     auxLinear f y z (A x) = f A (auxLinear f y z x) := by
   simp_rw [auxLinear_apply, coe_coe, ← mul_apply, ← map_mul]
   congr
   ext
   simp
 
-private theorem auxLinear_comp [CommSemiring R] [Module R V]
-    (f : End R V →ₐ[R] End R V) (y : Dual R V) (z : V) (A : End R V) :
+private theorem auxLinear_comp (f : End R V →ₐ[R] End R V) (y : Dual R V) (z : V) (A : End R V) :
     auxLinear f y z ∘ₗ A = f A ∘ₗ auxLinear f y z :=
   LinearMap.ext <| auxLinear_map_apply f y z A
 
-variable [Field R] [Module R V]
-
 section move
 
-variable (R) in
+variable (K) in
 /-- This is a linear map version of `SeparatingDual.exists_ne_zero` in a vector space. -/
-theorem dual_exists_ne_zero {x : V} (hx : x ≠ 0) :
-    ∃ f : Dual R V, f x ≠ 0 :=
-  let b := Basis.ofVectorSpace R V
+theorem dual_exists_ne_zero {x : V} (hx : x ≠ 0) : ∃ f : Dual K V, f x ≠ 0 :=
+  let b := Basis.ofVectorSpace K V
   have hb : b.repr x ≠ 0 := by simpa
   have ⟨i, hi⟩ := not_forall.mp fun h ↦ hb <| Finsupp.ext h
   ⟨b.coord i, hi⟩
 
-variable (R) in
+variable (K) in
 /-- This is a linear map version of `SeparatingDual.exists_eq_one` in a vector space. -/
-theorem dual_exists_eq_one {x : V} (hx : x ≠ 0) :
-    ∃ f : Dual R V, f x = 1 :=
-  have ⟨f, hf⟩ := dual_exists_ne_zero R hx
+theorem dual_exists_eq_one {x : V} (hx : x ≠ 0) : ∃ f : Dual K V, f x = 1 :=
+  have ⟨f, hf⟩ := dual_exists_ne_zero K hx
   ⟨(f x)⁻¹ • f, inv_mul_cancel₀ hf⟩
 
-/-- The center of linear maps on a vector space is trivial,
+/-- The center of endomorphisms on a vector space is trivial,
 in other words, it is a central algebra. -/
-instance _root_.Algebra.IsCentral.linearMap :
-    Algebra.IsCentral R (V →ₗ[R] V) where
+instance _root_.Algebra.IsCentral.end : Algebra.IsCentral K (End K V) where
   out T hT := by
-    have h' (f : Dual R V) (y v : V) : f (T v) • y = f v • T y := by
+    have h' (f : Dual K V) (y v : V) : f (T v) • y = f v • T y := by
       simpa using congr($(Subalgebra.mem_center_iff.mp hT <| f.smulRight y) v)
     nontriviality V
     obtain ⟨x, hx⟩ := exists_ne (0 : V)
-    obtain ⟨f, hf⟩ := dual_exists_eq_one (R := R) hx
+    obtain ⟨f, hf⟩ := dual_exists_eq_one K hx
     exact ⟨f (T x), ext fun _ => by simp [h', hf]⟩
 
 end move
 
-/-- Given an algebra automorphism `f` in `End R V`, there exists a linear isomorphism `T`
+/-- Given an algebra automorphism `f` in `End K V`, there exists a linear isomorphism `T`
 such that `f` is given by `x ↦ T * x * T⁻¹`. -/
-theorem AlgEquiv.coe_eq_linearEquiv_conjugate (f : End R V ≃ₐ[R] End R V) :
-    ∃ T : V ≃ₗ[R] V, ⇑f = fun x ↦ T ∘ₗ x ∘ₗ T.symm := by
+theorem AlgEquiv.coe_eq_linearEquiv_conjugate (f : End K V ≃ₐ[K] End K V) :
+    ∃ T : V ≃ₗ[K] V, ⇑f = fun x ↦ T ∘ₗ x ∘ₗ T.symm := by
   nontriviality V
   simp_rw [funext_iff, ← comp_assoc, LinearEquiv.eq_comp_toLinearMap_symm]
-  obtain ⟨u, v, huv⟩ : ∃ u : V, ∃ v : Dual R V, v u ≠ 0 := by
+  obtain ⟨u, v, huv⟩ : ∃ u : V, ∃ v : Dual K V, v u ≠ 0 := by
     obtain ⟨u, hu⟩ := nontrivial_iff_exists_ne 0 |>.mp ‹Nontrivial V›
-    obtain ⟨v, hv⟩ := dual_exists_ne_zero R hu
+    obtain ⟨v, hv⟩ := dual_exists_ne_zero K hu
     exact ⟨u, v, hv⟩
   obtain ⟨z, hz⟩ : ∃ z : V, f (smulRightₗ v u) z ≠ 0 := by
     simp_rw [ne_eq, ← not_forall]
@@ -102,7 +94,7 @@ theorem AlgEquiv.coe_eq_linearEquiv_conjugate (f : End R V ≃ₐ[R] End R V) :
   have surj : Function.Surjective T := by
     intro w
     have : T u ≠ 0 := by simpa [T]
-    obtain ⟨d, hd⟩ := dual_exists_eq_one R this
+    obtain ⟨d, hd⟩ := dual_exists_eq_one K this
     use f.symm (smulRightₗ d w) u
     have h : f (f.symm (smulRightₗ d w)) (T u) = w := by simp [hd]
     simp_rw [this', h]
@@ -118,9 +110,9 @@ theorem AlgEquiv.coe_eq_linearEquiv_conjugate (f : End R V ≃ₐ[R] End R V) :
 /-- Alternate statement of `coe_eq_linearEquiv_conjugate`. -/
 theorem mulSemiringActionToAlgEquiv_conjAct_surjective :
     Function.Surjective
-      (MulSemiringAction.toAlgEquiv (G := ConjAct (GeneralLinearGroup R V)) R (End R V)) := by
+      (MulSemiringAction.toAlgEquiv (G := ConjAct (GeneralLinearGroup K V)) K (End K V)) := by
   intro f
   obtain ⟨T, hT⟩ := f.coe_eq_linearEquiv_conjugate
-  exact ⟨LinearMap.GeneralLinearGroup.ofLinearEquiv T, (DFunLike.coe_injective hT).symm⟩
+  exact ⟨GeneralLinearGroup.ofLinearEquiv T, (DFunLike.coe_injective hT).symm⟩
 
 end Module.End
