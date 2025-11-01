dc763a77aadc699dad3dfd0459f4c582ac944397
Rmal <97214596+CoolRmal@users.noreply.github.com>
2025-11-01 17:39:07 -0400

    change import

--- Mathlib/Analysis/Complex/CauchyIntegral.lean
+++ Mathlib/Analysis/Complex/CauchyIntegral.lean
@@ -3,10 +3,7 @@ Copyright (c) 2021 Yury Kudryashov. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yury Kudryashov
 -/
-import Mathlib.Analysis.Analytic.Uniqueness
 import Mathlib.Analysis.Calculus.DiffContOnCl
-import Mathlib.Analysis.Calculus.DSlope
-import Mathlib.Analysis.Calculus.FDeriv.Analytic
 import Mathlib.Analysis.Complex.ReImTopology
 import Mathlib.Analysis.Real.Cardinality
 import Mathlib.MeasureTheory.Integral.CircleIntegral
--- Mathlib/Analysis/Complex/Liouville.lean
+++ Mathlib/Analysis/Complex/Liouville.lean
@@ -4,8 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yury Kudryashov
 -/
 import Mathlib.Analysis.Complex.CauchyIntegral
-import Mathlib.Analysis.Calculus.FDeriv.Analytic
-import Mathlib.Analysis.Normed.Module.Completion
 
 /-!
 # Liouville's theorem
