ef9b9405860a300f03e8f3e2f6f909df1a56d3f5
xbb <xbbng.0@gmail.com>
2025-11-02 00:39:54 +0800

    style: apply lint-style updates (bibliography/doc formatting)

--- Mathlib.lean
+++ Mathlib.lean
@@ -2987,6 +2987,7 @@ import Mathlib.Combinatorics.Enumerative.DyckWord
 import Mathlib.Combinatorics.Enumerative.IncidenceAlgebra
 import Mathlib.Combinatorics.Enumerative.InclusionExclusion
 import Mathlib.Combinatorics.Enumerative.Partition
+import Mathlib.Combinatorics.Enumerative.PentagonalNumbers
 import Mathlib.Combinatorics.Enumerative.Stirling
 import Mathlib.Combinatorics.Extremal.RuzsaSzemeredi
 import Mathlib.Combinatorics.Graph.Basic
