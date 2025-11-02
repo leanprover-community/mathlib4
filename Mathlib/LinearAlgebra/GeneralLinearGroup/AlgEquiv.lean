00cce21c1514f7e69583149f586ecb4b2904dd62
Monica Omar <23701951+themathqueen@users.noreply.github.com>
2025-11-02 15:06:43 +0000

    Update AlgEquiv.lean

--- Mathlib/LinearAlgebra/GeneralLinearGroup/AlgEquiv.lean
+++ Mathlib/LinearAlgebra/GeneralLinearGroup/AlgEquiv.lean
@@ -3,9 +3,11 @@ Copyright (c) 2025 Monica Omar. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Monica Omar
 -/
+import Mathlib.Algebra.Algebra.Equiv
 import Mathlib.Algebra.Ring.Action.ConjAct
-import Mathlib.LinearAlgebra.Basis.VectorSpace
 import Mathlib.LinearAlgebra.Dual.Defs
+import Mathlib.LinearAlgebra.FreeModule.Basic
+import Mathlib.LinearAlgebra.GeneralLinearGroup
 
 /-!
 # Algebra automorphisms in `End K V` are inner
