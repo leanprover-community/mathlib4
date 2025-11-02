bf194cb558be81814b0efe68ff4c45070fec5fb4
Calle Sönne <calle.sonne@gmail.com>
2025-11-02 14:50:58 +0000

    Update Mathlib/CategoryTheory/Bicategory/Functor/StrictlyUnitary.lean

--- Mathlib/CategoryTheory/Bicategory/Functor/StrictlyUnitary.lean
+++ Mathlib/CategoryTheory/Bicategory/Functor/StrictlyUnitary.lean
@@ -56,7 +56,6 @@ structure StrictlyUnitaryLaxFunctor extends LaxFunctor B C where
   map_id (X : B) : map (𝟙 X) = 𝟙 (obj X) := by cat_disch
   mapId_eq_eqToHom (X : B) : (mapId X) = eqToHom (map_id X).symm := by cat_disch
 
-attribute [simp] StrictlyUnitaryLaxFunctor.map_id
 
 /-- A helper structure that bundles the necessary data to
 construct a `StrictlyUnitaryLaxFunctor` without specifying the redundant
