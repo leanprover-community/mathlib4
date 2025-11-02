38c0af2ffda83d7203cf78a86e5b450dc15c2dc2
Riccardo Brasca <riccardo.brasca@gmail.com>
2025-11-02 15:52:06 +0100

    fix copyright

--- Mathlib/RingTheory/PowerSeries/Ideal.lean
+++ Mathlib/RingTheory/PowerSeries/Ideal.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2021 Alex J. Best. All rights reserved.
+Copyright (c) 2025 Riccardo Brasca. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Riccardo Brasca, Anthony Fernandes, Marc Robin
 -/
