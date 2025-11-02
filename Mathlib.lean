433514715c7219a38f604f54294bf7e2e2dfd428
Daniel Figueroa <40865765+daefigueroa@users.noreply.github.com>
2025-11-02 21:27:20 +0000

    feat(Dynamics): add results on topologically transitive monoid actions (#28001)
    We define topological transitivity for monoid actions on topological spaces, prove basic facts about topologically transitive actions and give some characterizations of topological transitivity.
    
    Co-authored-by: figud <40865765+figud@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -3863,6 +3863,7 @@ import Mathlib.Dynamics.TopologicalEntropy.DynamicalEntourage
 import Mathlib.Dynamics.TopologicalEntropy.NetEntropy
 import Mathlib.Dynamics.TopologicalEntropy.Semiconj
 import Mathlib.Dynamics.TopologicalEntropy.Subset
+import Mathlib.Dynamics.Transitive
 import Mathlib.FieldTheory.AbelRuffini
 import Mathlib.FieldTheory.AbsoluteGaloisGroup
 import Mathlib.FieldTheory.AlgebraicClosure
