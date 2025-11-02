465e1a67a179909b2adc9b36a1e734f0504aa2f9
blizzard_inc <edwardvdmeent@gmail.com>
2025-11-02 15:48:26 +0100

    Update Mathlib/CategoryTheory/FiberedCategory/HomLift.lean
    Co-authored-by: Calle Sönne <calle.sonne@gmail.com>

--- Mathlib/CategoryTheory/FiberedCategory/HomLift.lean
+++ Mathlib/CategoryTheory/FiberedCategory/HomLift.lean
@@ -26,7 +26,7 @@ it is used to make the code more readable.
 
 ## Implementation
 The class `IsHomLift` is defined as an inductive with the single constructor
-`.map (φ : a ⟶ b) : IsHomLiftAux p (p.map φ) φ`, similar to how `Eq a b` has the single constructor
+`.map (φ : a ⟶ b) : IsHomLift p (p.map φ) φ`, similar to how `Eq a b` has the single constructor
 `.rfl (a : α) : Eq a a`.
 
 -/
