e92ad1ea9eb486cb310b9bc748b4003f33572e60
Monica Omar <23701951+themathqueen@users.noreply.github.com>
2025-11-03 18:58:09 +0000

    chore: move Analysis/Matrix to Analysis/Matrix/Normed (#31228)
    Since we already have a folder for `Analysis/Matrix`, we should probably move this there. It looks strange on the documentation otherwise.

--- Mathlib/Analysis/CStarAlgebra/Matrix.lean
+++ Mathlib/Analysis/CStarAlgebra/Matrix.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Hans Parshall
 -/
 import Mathlib.Analysis.InnerProductSpace.Adjoint
-import Mathlib.Analysis.Matrix
+import Mathlib.Analysis.Matrix.Normed
 import Mathlib.Analysis.RCLike.Basic
 import Mathlib.LinearAlgebra.UnitaryGroup
 import Mathlib.Topology.UniformSpace.Matrix
--- Mathlib/NumberTheory/SiegelsLemma.lean
+++ Mathlib/NumberTheory/SiegelsLemma.lean
@@ -3,7 +3,7 @@ Copyright (c) 2024 Fabrizio Barroero. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Fabrizio Barroero, Laura Capuano, Amos Turchet
 -/
-import Mathlib.Analysis.Matrix
+import Mathlib.Analysis.Matrix.Normed
 import Mathlib.Data.Pi.Interval
 import Mathlib.Tactic.Rify
 
