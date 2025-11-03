769a2206e0372f86e67eedfbd5a2ef343c3f2bad
Yaël Dillies <yael.dillies@gmail.com>
2025-11-03 08:58:09 +0000

    golf

--- Mathlib/Topology/Instances/ENNReal/Lemmas.lean
+++ Mathlib/Topology/Instances/ENNReal/Lemmas.lean
@@ -613,8 +613,7 @@ protected theorem tsum_add : ∑' a, (f a + g a) = ∑' a, f a + ∑' a, g a :=
 
 protected lemma sum_add_tsum_compl {ι : Type*} (s : Finset ι) (f : ι → ℝ≥0∞) :
     ∑ i ∈ s, f i + ∑' i : ↥(s : Set ι)ᶜ, f i = ∑' i, f i := by
-  rw [_root_.tsum_subtype, sum_eq_tsum_indicator, ← ENNReal.tsum_add]
-  simp
+  simp [tsum_subtype, sum_eq_tsum_indicator, ← ENNReal.tsum_add]
 
 protected theorem tsum_le_tsum (h : ∀ a, f a ≤ g a) : ∑' a, f a ≤ ∑' a, g a :=
   ENNReal.summable.tsum_le_tsum h ENNReal.summable
