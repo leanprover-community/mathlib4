ef7508080f71c5fbd464693de09e38e6cb869776
euprunin <178733547+euprunin@users.noreply.github.com>
2025-11-03 21:18:13 +0000

    chore(RingTheory/LocalRing/ResidueField): deprecate`isLocalHom_residue` (#28497)

--- Mathlib/RingTheory/Valuation/ValuationSubring.lean
+++ Mathlib/RingTheory/Valuation/ValuationSubring.lean
@@ -642,9 +642,8 @@ theorem ker_unitGroupToResidueFieldUnits :
 
 theorem surjective_unitGroupToResidueFieldUnits :
     Function.Surjective A.unitGroupToResidueFieldUnits :=
-  (IsLocalRing.surjective_units_map_of_local_ringHom _ Ideal.Quotient.mk_surjective
-        IsLocalRing.isLocalHom_residue).comp
-    (MulEquiv.surjective _)
+  IsLocalRing.surjective_units_map_of_local_ringHom _ Ideal.Quotient.mk_surjective
+    (inferInstanceAs (IsLocalHom (IsLocalRing.residue A))) |>.comp (MulEquiv.surjective _)
 
 /-- The quotient of the unit group of `A` by the principal unit group of `A` agrees with
 the units of the residue field of `A`. -/
