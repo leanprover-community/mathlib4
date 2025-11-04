cf8ab95ecc52ea2170d964578e1f0e94610da152
Yaël Dillies <yael.dillies@gmail.com>
2025-11-04 11:38:02 +0000

    feat: relation-covered sets (#31104)
    Define a notion of cover of a set relative to a relation. This will be used to unify metric and dynamical covers.
    
    From MiscYD and LeanAPAP

--- Mathlib.lean
+++ Mathlib.lean
@@ -3711,6 +3711,7 @@ import Mathlib.Data.Real.Sqrt
 import Mathlib.Data.Real.Star
 import Mathlib.Data.Real.StarOrdered
 import Mathlib.Data.Rel
+import Mathlib.Data.Rel.Cover
 import Mathlib.Data.Rel.Separated
 import Mathlib.Data.SProd
 import Mathlib.Data.Semiquot
