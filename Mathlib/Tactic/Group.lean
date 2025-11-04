748c0495cd8de7f1e261d005007cfcadc17b2116
Michael Rothgang <10105016+grunweg@users.noreply.github.com>
2025-11-04 10:58:46 +0000

    chore: fix some copyright headers (#31255)
    In each case, the respective file has a clear initial author, who simply forgot to add themselves as a copyright holder when making this file. The individual commit messages contain the details.

--- Mathlib/Tactic/Group.lean
+++ Mathlib/Tactic/Group.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2020. All rights reserved.
+Copyright (c) 2020 Patrick Massot. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Thomas Browning, Patrick Massot
 -/
