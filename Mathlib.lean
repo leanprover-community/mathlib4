8ede7c78a84c3e3840a3588036708ea2244cc65a
Sebastian Ullrich <sebasti@nullri.ch>
2025-11-05 13:18:47 +0100

    chore: adjust mk_all

--- Mathlib.lean
+++ Mathlib.lean
@@ -7205,3 +7205,5 @@ public import Mathlib.Util.TermReduce
 public import Mathlib.Util.TransImports
 public import Mathlib.Util.WhatsNew
 public import Mathlib.Util.WithWeakNamespace
+
+set_option linter.style.longLine false
