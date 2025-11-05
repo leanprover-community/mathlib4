bb39927ee1df0d536ea3d850d424a06c633411dd
Yaël Dillies <yael.dillies@gmail.com>
2025-11-05 13:09:27 +0000

    feat(MetricSpace): covers (aka nets) (#23124)
    Define covers (aka nets), which are a quantitative notion of compactness in a metric space.
    
    From my PhD (MiscYD)

--- Mathlib.lean
+++ Mathlib.lean
@@ -6891,6 +6891,7 @@ import Mathlib.Topology.MetricSpace.Closeds
 import Mathlib.Topology.MetricSpace.Completion
 import Mathlib.Topology.MetricSpace.Congruence
 import Mathlib.Topology.MetricSpace.Contracting
+import Mathlib.Topology.MetricSpace.Cover
 import Mathlib.Topology.MetricSpace.Defs
 import Mathlib.Topology.MetricSpace.Dilation
 import Mathlib.Topology.MetricSpace.DilationEquiv
