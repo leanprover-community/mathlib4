e7d33fe0fd934b198afe03cb97498924f5c2fd7e
Jiedong Jiang <emailboxofjjd@163.com>
2025-11-06 02:01:11 +0800

    split first commit

--- Mathlib.lean
+++ Mathlib.lean
@@ -5548,7 +5548,6 @@ import Mathlib.RingTheory.AdicCompletion.Exactness
 import Mathlib.RingTheory.AdicCompletion.Functoriality
 import Mathlib.RingTheory.AdicCompletion.LocalRing
 import Mathlib.RingTheory.AdicCompletion.Noetherian
-import Mathlib.RingTheory.AdicCompletion.RingHom
 import Mathlib.RingTheory.AdicCompletion.Topology
 import Mathlib.RingTheory.Adjoin.Basic
 import Mathlib.RingTheory.Adjoin.Dimension
