9e19f428fe683c9c22c0282b41f09fdfde5f84be
Sebastian Ullrich <sebasti@nullri.ch>
2025-11-05 13:42:04 +0100

    TEMP: update aesop

--- lakefile.lean
+++ lakefile.lean
@@ -8,7 +8,7 @@ open Lake DSL
 
 require "leanprover-community" / "batteries" @ git "lean-pr-testing-11122"
 require "leanprover-community" / "Qq" @ git "master"
-require "leanprover-community" / "aesop" @ git "nightly-testing"
+require "leanprover-community" / "aesop" @ git "modulize-imports"
 require "leanprover-community" / "proofwidgets" @ git "v0.0.77" -- ProofWidgets should always be pinned to a specific version
   with NameMap.empty.insert `errorOnBuild
     "ProofWidgets not up-to-date. \
