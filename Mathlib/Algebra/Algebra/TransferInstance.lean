72c335eeda200d82f45a518baf48637260b2294e
Etienne Marion <baguettes_casino0c@icloud.com>
2025-11-05 13:47:56 +0100

    review

--- Mathlib/Algebra/Algebra/TransferInstance.lean
+++ Mathlib/Algebra/Algebra/TransferInstance.lean
@@ -22,31 +22,22 @@ variable (R) in
 /-- Transfer `Algebra` across an `Equiv` -/
 protected abbrev algebra (e : α ≃ β) [Semiring β] :
     let _ := Equiv.semiring e
-    ∀ [Algebra R β], Algebra R α := fast_instance% by
+    ∀ [Algebra R β], Algebra R α := fast_instance%
   letI := Equiv.semiring e
   letI := e.smul R
-  refine ⟨{
-    toFun r := e.symm (algebraMap R β r)
-    map_one' := by rw [map_one]; rfl
-    map_mul' x y := by
-      nth_rw 1 [map_mul, ← e.apply_symm_apply (algebraMap R β x),
-        ← e.apply_symm_apply (algebraMap R β y)]; rfl
-    map_zero' := by simp; rfl
-    map_add' x y := by
-      nth_rw 1 [map_add, ← e.apply_symm_apply (algebraMap R β x),
-        ← e.apply_symm_apply (algebraMap R β y)]; rfl
-  },
-  fun r x ↦ ?_, fun r x ↦ ?_⟩
-  · simp only [RingHom.coe_mk, MonoidHom.coe_mk, OneHom.coe_mk]
-    calc
-    _ = e.symm ((e (e.symm (algebraMap R β r)) * e x)) := rfl
-    _ = e.symm (e x * e (e.symm (algebraMap R β r))) := by simp [Algebra.commutes]
-    _ = x * e.symm (algebraMap R β r) := rfl
-  · simp only [RingHom.coe_mk, MonoidHom.coe_mk, OneHom.coe_mk]
-    calc r • x
-      = e.symm (r • e x) := rfl
-    _ = e.symm (e (e.symm (algebraMap R β r)) * e x) := by simp [Algebra.smul_def]
-    _ = e.symm (algebraMap R β r) * x := rfl
+  { algebraMap := e.ringEquiv.symm.toRingHom.comp (algebraMap R β)
+    commutes' r x := by
+      simp only [RingEquiv.toRingHom_eq_coe, RingHom.coe_comp, RingHom.coe_coe, Function.comp_apply]
+      calc
+      _ = e.symm ((e (e.symm (algebraMap R β r)) * e x)) := rfl
+      _ = e.symm (e x * e (e.symm (algebraMap R β r))) := by simp [Algebra.commutes]
+      _ = x * e.symm (algebraMap R β r) := rfl
+    smul_def' r x := by
+      simp only [RingEquiv.toRingHom_eq_coe, RingHom.coe_comp, RingHom.coe_coe, Function.comp_apply]
+      calc r • x
+        = e.symm (r • e x) := rfl
+      _ = e.symm (e (e.symm (algebraMap R β r)) * e x) := by simp [Algebra.smul_def]
+      _ = e.symm (algebraMap R β r) * x := rfl }
 
 lemma algebraMap_def (e : α ≃ β) [Semiring β] [Algebra R β] (r : R) :
     (@algebraMap R α _ (Equiv.semiring e) (Equiv.algebra R e)) r = e.symm ((algebraMap R β) r) :=
