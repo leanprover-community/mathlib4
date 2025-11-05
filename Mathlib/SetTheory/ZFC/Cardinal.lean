e4246e1d3a5a187153f7b3ee9f460821048c2053
staroperator <staroperator92@gmail.com>
2025-11-05 08:21:21 -0500

    unsimp

--- Mathlib/SetTheory/ZFC/Cardinal.lean
+++ Mathlib/SetTheory/ZFC/Cardinal.lean
@@ -28,13 +28,12 @@ def card (x : ZFSet.{u}) : Cardinal.{u} := #(Shrink x.toSet)
 variable {x y : ZFSet.{u}}
 
 /-- `ZFSet.card x` is equal to the cardinality of `x` as a set of `ZFSet`s. -/
-@[simp]
 theorem card_toSet : #x.toSet = lift.{u + 1, u} (card x) := by
   rw [card, lift_mk_shrink'']
 
 @[gcongr]
 theorem card_mono (h : x ⊆ y) : card x ≤ card y := by
-  simpa using mk_le_mk_of_subset (toSet_subset_iff.2 h)
+  simpa [card_toSet] using mk_le_mk_of_subset (toSet_subset_iff.2 h)
 
 @[simp]
 theorem card_empty : card ∅ = 0 := by
@@ -64,25 +63,25 @@ theorem card_union_le : card (x ∪ y) ≤ card x + card y := by
 @[simp]
 theorem card_powerset (x : ZFSet.{u}) : card (powerset x) = 2 ^ card x := by
   rw [← lift_inj.{u, u + 1}]
-  simpa using mk_congr (powersetEquiv x)
+  simpa [card_toSet] using mk_congr (powersetEquiv x)
 
 theorem card_image_le {f : ZFSet → ZFSet} [Definable₁ f] :
     card (image f x) ≤ card x := by
-  simpa [← toSet_image] using mk_image_le (f := f) (s := x.toSet)
+  simpa [card_toSet, ← toSet_image] using mk_image_le (f := f) (s := x.toSet)
 
 theorem lift_card_range_le {α} [Small.{v, u} α] {f : α → ZFSet.{v}} :
     lift.{u} (card (range f)) ≤ lift.{v} #α := by
   rw [← lift_le.{max u (v + 1)}, lift_lift.{v}, lift_umax.{u, v + 1}]
-  simpa [← toSet_range] using mk_range_le_lift (f := f)
+  simpa [card_toSet, ← toSet_range] using mk_range_le_lift (f := f)
 
 theorem iSup_card_le_card_iUnion {α} [Small.{v, u} α] {f : α → ZFSet.{v}} :
     ⨆ i, card (f i) ≤ card (⋃ i, f i) := by
-  simpa [← toSet_iUnion, ← lift_iSup (bddAbove_of_small _)] using
+  simpa [card_toSet, ← toSet_iUnion, ← lift_iSup (bddAbove_of_small _)] using
     iSup_mk_le_mk_iUnion (f := toSet ∘ f)
 
 theorem lift_card_iUnion_le_sum_card {α} [Small.{v, u} α] {f : α → ZFSet.{v}} :
     lift (card (⋃ i, f i)) ≤ sum fun i => card (f i) := by
   rw [← lift_le.{max u (v + 1)}, lift_umax.{max u v, v + 1}]
-  simpa [← toSet_iUnion] using mk_iUnion_le_sum_mk_lift (f := toSet ∘ f)
+  simpa [card_toSet, ← toSet_iUnion] using mk_iUnion_le_sum_mk_lift (f := toSet ∘ f)
 
 end ZFSet
