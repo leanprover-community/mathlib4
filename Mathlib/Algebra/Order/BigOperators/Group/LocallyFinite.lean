0f373cbf1a6b9f904ce696e3d9e6040506ea364f
Andrew Yang <36414270+erdOne@users.noreply.github.com>
2025-11-05 10:36:17 +0000

    feat: `∑ z ∈ L, ‖z - x‖⁻ʳ` converges for lattices `L` (#25804)

--- Mathlib/Algebra/Order/BigOperators/Group/LocallyFinite.lean
+++ Mathlib/Algebra/Order/BigOperators/Group/LocallyFinite.lean
@@ -6,6 +6,7 @@ Authors: Yaël Dillies
 import Mathlib.Algebra.BigOperators.Group.Finset.Sigma
 import Mathlib.Algebra.Order.Interval.Finset.SuccPred
 import Mathlib.Data.Nat.SuccPred
+import Mathlib.Order.Disjointed
 import Mathlib.Order.Interval.Finset.Nat
 
 /-!
@@ -160,4 +161,20 @@ lemma prod_prod_Ioi_mul_eq_prod_prod_off_diag (f : α → α → M) :
   refine prod_nbij' (fun i ↦ ⟨i.2, i.1⟩) (fun i ↦ ⟨i.2, i.1⟩) ?_ ?_ ?_ ?_ ?_ <;> simp
 
 end LinearOrder
+
+/-- Given a sequence of finite sets `s₀ ⊆ s₁ ⊆ s₂ ⋯`, the product of `gᵢ` over `i ∈ sₙ` is equal
+to `∏_{i ∈ s₀} gᵢ` * `∏_{j < n, i ∈ sⱼ₊₁ \ sⱼ} gᵢ`. -/
+@[to_additive /-- Given a sequence of finite sets `s₀ ⊆ s₁ ⊆ s₂ ⋯`, the sum of `gᵢ` over `i ∈ sₙ` is
+equal to `∑_{i ∈ s₀} gᵢ` + `∑_{j < n, i ∈ sⱼ₊₁ \ sⱼ} gᵢ`.-/]
+lemma prod_eq_prod_range_sdiff
+    {α β : Type*} [DecidableEq α] [CommMonoid β] (s : ℕ → Finset α) (hs : Monotone s)
+    (g : α → β) (n : ℕ) :
+    ∏ i ∈ s n, g i = (∏ i ∈ s 0, g i) * ∏ i ∈ range n, ∏ j ∈ s (i + 1) \ s i, g j := by
+  conv_lhs => rw [← hs.partialSups_eq, ← disjiUnion_Iic_disjointed, Iic_eq_Icc,
+    prod_disjiUnion, Nat.bot_eq_zero, ← Nat.range_succ_eq_Icc_zero, prod_range_succ', mul_comm]
+  congrm (∏ x ∈ ?_, g x) * ∏ k ∈ range n, ∏ x ∈ s (k + 1) \ ?_, g x
+  · simp
+  · change (Iic k).sup (s ∘ id) = s k
+    rw [← comp_sup_eq_sup_comp_of_nonempty hs nonempty_Iic, sup_Iic]
+
 end Finset
