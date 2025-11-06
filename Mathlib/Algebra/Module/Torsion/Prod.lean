26a4b33dffe27ed60fba360bd38d20f41c327c6f
Yaël Dillies <yael.dillies@gmail.com>
2025-11-06 12:18:15 +0000

    feat(Algebra/Module/Torsion): more instances (#30716)
    Split `Algebra.Module.Torsion` into `Algebra.Module.Torsion.Free` for torsion-free modules and `Algebra.Module.Torsion.Basic` for torsion modules. Add prod and pi instances, as well as pullback instances and the fact that a torsion-free module over a char zero domain is a torsion-free group.

---
+++ Mathlib/Algebra/Module/Torsion/Prod.lean
@@ -0,0 +1,26 @@
+/-
+Copyright (c) 2025 Yaël Dillies. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Yaël Dillies
+-/
+import Mathlib.Algebra.Module.Prod
+import Mathlib.Algebra.Module.Torsion.Free
+
+/-!
+# Product of torsion-free modules
+
+This file shows that the product of two torsion-free modules is torsion-free.
+-/
+
+open Module
+
+variable {R M N : Type*}
+
+namespace Prod
+
+instance moduleIsTorsionFree [Semiring R] [AddCommMonoid M] [AddCommMonoid N]
+    [Module R M] [Module R N] [IsTorsionFree R M] [IsTorsionFree R N] :
+    IsTorsionFree R (M × N) where
+  isSMulRegular _r hr := hr.isSMulRegular.prodMap hr.isSMulRegular
+
+end Prod
