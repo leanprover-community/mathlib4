ff2157c660cffba5ee0714f0f48ef6ca4ae473f1
Andrew Yang <the.erd.one@gmail.com>
2025-11-07 12:50:20 +0000

    oops

--- Mathlib/RingTheory/Polynomial/UniversalFactorizationRing.lean
+++ Mathlib/RingTheory/Polynomial/UniversalFactorizationRing.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2025 Andrew Yang. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Richard Hill, Andrew Yang
+Authors: Andrew Yang
 -/
 import Mathlib.Algebra.MvPolynomial.CommRing
 import Mathlib.Algebra.Polynomial.Monic
