e883b55eb2704ddfb83b909092540a820cfd9121
Dagur Asgeirsson <dagurtomas@gmail.com>
2025-11-07 09:58:00 -0700

    remove additions that are now irrelevant, and done in a separate PR

--- Mathlib/CategoryTheory/Monoidal/NaturalTransformation.lean
+++ Mathlib/CategoryTheory/Monoidal/NaturalTransformation.lean
@@ -206,15 +206,4 @@ def isoOfComponents {F G : LaxMonoidalFunctor C D} (e : ∀ X, F.obj X ≅ G.obj
 
 end LaxMonoidalFunctor
 
-namespace Functor.Monoidal
-
-instance transport_iso_isMonoidal {F G : C ⥤ D} [F.Monoidal] (e : F ≅ G) :
-    letI : G.Monoidal := transport e
-    e.hom.IsMonoidal := by
-  letI G.Monoidal := transport e
-  refine ⟨rfl, fun X Y ↦ ?_⟩
-  simp [transport_μ, tensorHom_comp_tensorHom_assoc]
-
-end Functor.Monoidal
-
 end CategoryTheory
