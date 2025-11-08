54367b1197c3bc0579d4e277fc54afa4e975133e
yuanyi-350 <kysyy1@126.com>
2025-11-08 13:27:02 +0800

    fix

--- Mathlib.lean
+++ Mathlib.lean
@@ -1901,7 +1901,6 @@ import Mathlib.Analysis.Normed.Operator.BanachSteinhaus
 import Mathlib.Analysis.Normed.Operator.Basic
 import Mathlib.Analysis.Normed.Operator.Bilinear
 import Mathlib.Analysis.Normed.Operator.BoundedLinearMaps
-import Mathlib.Analysis.Normed.Operator.ClosedRange
 import Mathlib.Analysis.Normed.Operator.Compact
 import Mathlib.Analysis.Normed.Operator.CompleteCodomain
 import Mathlib.Analysis.Normed.Operator.Completeness
