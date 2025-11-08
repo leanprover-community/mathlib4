dbf44bd73722f613bb01a7819b9aba0a6471e144
euprunin <euprunin@users.noreply.github.com>
2025-11-08 13:57:41 +0100

    shorten code

--- Mathlib/RingTheory/DedekindDomain/Ideal/Lemmas.lean
+++ Mathlib/RingTheory/DedekindDomain/Ideal/Lemmas.lean
@@ -1049,9 +1049,7 @@ theorem primesOver_finite : (primesOver p B).Finite := by
 noncomputable instance : Fintype (p.primesOver B) := Set.Finite.fintype (primesOver_finite p B)
 
 theorem primesOver_ncard_ne_zero : (primesOver p B).ncard ≠ 0 := by
-  have exists_maximal_ideal : ∃ P : Ideal B, P.IsMaximal ∧ P.LiesOver p :=
-    exists_maximal_ideal_liesOver_of_isIntegral p
-  rcases exists_maximal_ideal with ⟨P, hPm, hp⟩
+  rcases exists_maximal_ideal_liesOver_of_isIntegral (S := B) p with ⟨P, hPm, hp⟩
   exact Set.ncard_ne_zero_of_mem ⟨hPm.isPrime, hp⟩ (primesOver_finite p B)
 
 theorem one_le_primesOver_ncard : 1 ≤ (primesOver p B).ncard :=
