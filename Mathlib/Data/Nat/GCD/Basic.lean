f0db196c260295248793b4cef0c351bec26eb95c
stepanholub <holub@karlin.mff.cuni.cz>
2025-11-08 20:06:12 +0100

    lemma sub_eq_gcd_of_sub_dvd

--- Mathlib/Data/Nat/GCD/Basic.lean
+++ Mathlib/Data/Nat/GCD/Basic.lean
@@ -265,4 +265,12 @@ lemma div_lcm_eq_div_gcd (hkm : m ∣ k) (hkn : n ∣ k) : (k / m).lcm (k / n) =
   simpa [Nat.div_dvd_iff_dvd_mul (Nat.dvd_trans (Nat.gcd_dvd_left m n) hkm)
     (gcd_pos_of_pos_left n hm), Nat.gcd_mul_right m c n] using (Nat.dvd_gcd hmc hnc)
 
+lemma sub_eq_gcd_of_sub_dvd {p q : ℕ} (sub_dvd_q : p - q ∣ q) : (p - q) = p.gcd q := by
+  by_cases sub_pos : p - q = 0
+  · simp_all
+  have : p - q + q = p := by omega
+  have := (this ▸ (Nat.dvd_add_right (dvd_refl (p - q))).mpr sub_dvd_q)
+  exact Nat.dvd_antisymm (Nat.dvd_gcd this sub_dvd_q)
+   (Nat.dvd_sub (p.gcd_dvd_left q) (p.gcd_dvd_right q))
+
 end Nat
