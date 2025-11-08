1aa52f2f3b3c66cecb3bb45342d48154966b8e43
Attila Gáspár <58485900+gasparattila@users.noreply.github.com>
2025-11-08 15:59:48 +0100

    feat(Topology/Sets): injectivity of `Compacts.map`

--- Mathlib/Topology/Sets/Compacts.lean
+++ Mathlib/Topology/Sets/Compacts.lean
@@ -148,6 +148,10 @@ theorem map_comp (f : β → γ) (g : α → β) (hf : Continuous f) (hg : Conti
     K.map (f ∘ g) (hf.comp hg) = (K.map g hg).map f hf :=
   Compacts.ext <| Set.image_comp _ _ _
 
+theorem map_injective {f : α → β} (hf : Continuous f) (hf' : Function.Injective f) :
+    Function.Injective (Compacts.map f hf) :=
+  .of_comp (f := SetLike.coe) <| hf'.image_injective.comp SetLike.coe_injective
+
 @[simp]
 theorem map_singleton {f : α → β} (hf : Continuous f) (x : α) : Compacts.map f hf {x} = {f x} :=
   Compacts.ext Set.image_singleton
