5f9bf17ac2f5b20f826cc18de02f5b21e39017da
Wen Yang <yang-wen@139.com>
2025-11-09 23:39:09 +0800

    linter

--- Mathlib/RingTheory/Radical.lean
+++ Mathlib/RingTheory/Radical.lean
@@ -257,7 +257,8 @@ lemma radical_eq_iff_primeFactors_eq :
   ⟨fun h => by rw [← primeFactors_radical, h]; exact primeFactors_radical,
     fun h => by simp [radical, h]⟩
 
-@[deprecated "This lemma is deprecated in favor of using `radical_eq_iff_primeFactors_eq.mpr`. " (since := "2025-11-09")]
+@[deprecated "This lemma is deprecated in favor of using `radical_eq_iff_primeFactors_eq.mpr`. "
+   (since := "2025-11-09")]
 lemma radical_eq_of_primeFactors_eq (h : primeFactors a = primeFactors b) :
     radical a = radical b := radical_eq_iff_primeFactors_eq.mpr h
 
