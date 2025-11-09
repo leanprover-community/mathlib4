4aaebca5ac41c3b30264ec998fc86640c39ebaf5
Joël Riou <37772949+joelriou@users.noreply.github.com>
2025-11-09 13:26:26 +0000

    feat(AlgebraicTopology): the opposite of a model category structure (#31095)
    In this PR, we show that if `C` is a model category, then so is `Cᵒᵖ`.

--- Mathlib/CategoryTheory/MorphismProperty/Factorization.lean
+++ Mathlib/CategoryTheory/MorphismProperty/Factorization.lean
@@ -51,7 +51,24 @@ structure MapFactorizationData {X Y : C} (f : X ⟶ Y) where
   hi : W₁ i
   hp : W₂ p
 
-attribute [reassoc (attr := simp)] MapFactorizationData.fac
+namespace MapFactorizationData
+
+attribute [reassoc (attr := simp)] fac
+
+variable {X Y : C} (f : X ⟶ Y)
+
+/-- The opposite of a factorization. -/
+@[simps]
+def op {X Y : C} {f : X ⟶ Y} (hf : MapFactorizationData W₁ W₂ f) :
+    MapFactorizationData W₂.op W₁.op f.op where
+  Z := Opposite.op hf.Z
+  i := hf.p.op
+  p := hf.i.op
+  fac := Quiver.Hom.unop_inj (by simp)
+  hi := hf.hp
+  hp := hf.hi
+
+end MapFactorizationData
 
 /-- The data of a term in `MapFactorizationData W₁ W₂ f` for any morphism `f`. -/
 abbrev FactorizationData := ∀ {X Y : C} (f : X ⟶ Y), MapFactorizationData W₁ W₂ f
@@ -65,6 +82,9 @@ class HasFactorization : Prop where
 /-- A chosen term in `FactorizationData W₁ W₂` when `HasFactorization W₁ W₂` holds. -/
 noncomputable def factorizationData [HasFactorization W₁ W₂] : FactorizationData W₁ W₂ :=
   fun _ => Nonempty.some (HasFactorization.nonempty_mapFactorizationData _)
+
+instance [HasFactorization W₁ W₂] : HasFactorization W₂.op W₁.op where
+  nonempty_mapFactorizationData f := ⟨(factorizationData W₁ W₂ f.unop).op⟩
 
 /-- The class of morphisms that are of the form `i ≫ p` with `W₁ i` and `W₂ p`. -/
 def comp : MorphismProperty C := fun _ _ f => Nonempty (MapFactorizationData W₁ W₂ f)
