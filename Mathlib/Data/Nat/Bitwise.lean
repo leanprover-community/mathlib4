7f55cd7f635967b2def1206eac713b2e95b443f8
Snir Broshi <26556598+SnirBroshi@users.noreply.github.com>
2025-11-09 07:12:10 +0200

    feat(Data/Nat/Bitwise): formula for the xor of {0,...,n}

--- Mathlib/Data/Nat/Bitwise.lean
+++ Mathlib/Data/Nat/Bitwise.lean
@@ -9,6 +9,7 @@ import Mathlib.Algebra.Ring.Nat
 import Mathlib.Data.Bool.Basic
 import Mathlib.Data.List.GetD
 import Mathlib.Data.Nat.Bits
+import Mathlib.Data.ZMod.Basic
 import Mathlib.Order.Basic
 import Mathlib.Tactic.AdaptationNote
 import Mathlib.Tactic.Common
@@ -344,6 +345,43 @@ theorem xor_mod_two_eq {m n : ℕ} : (m ^^^ n) % 2 = (m + n) % 2 := by
 theorem even_xor {m n : ℕ} : Even (m ^^^ n) ↔ (Even m ↔ Even n) := by
   simp only [even_iff, xor_mod_two_eq]
   cutsat
+
+@[simp]
+theorem xor_one_of_even {n : ℕ} (h : Even n) : n ^^^ 1 = n + 1 := by
+  cases n with
+  | zero => rfl
+  | succ n =>
+    simp [HXor.hXor, instXorOp, xor, bitwise, even_iff.mp h, ← mul_two, div_two_mul_two_of_even h]
+
+@[simp]
+theorem xor_one_of_odd {n : ℕ} (h : Odd n) : n ^^^ 1 = n - 1 := by
+  cases n with
+  | zero =>
+    exact not_odd_zero h |>.elim
+  | succ n =>
+    apply succ_injective
+    simp [HXor.hXor, instXorOp, xor, bitwise, odd_iff.mp h, ← mul_two,
+      div_two_mul_two_add_one_of_odd h]
+
+/-- The xor of the numbers from 0 to n can be easily calculated using `n mod 4`. -/
+theorem xor_range (n : ℕ) : Finset.fold (· ^^^ ·) 0 id (.range (n + 1)) =
+    match (n : ZMod 4) with | 0 => n | 1 => 1 | 2 => n + 1 | 3 => 0 := by
+  induction n with
+  | zero => simp
+  | succ n h =>
+    rw [Finset.range_add_one, Finset.fold_insert Finset.notMem_range_self, h, id_eq, cast_add,
+      cast_one]
+    match h : (n : ZMod 4) with
+    | 0 =>
+      rw [zero_add, ← xor_one_of_even <| even_iff.mpr ?_, ← Nat.xor_comm, xor_xor_cancel_left]
+      rw [← @mod_mod_of_dvd _ 4 _ <| by simp, ZMod.natCast_eq_natCast_iff' n .. |>.mp h]
+    | 1 =>
+      refine xor_one_of_even <| even_iff.mpr ?_
+      rw [add_mod, ← @mod_mod_of_dvd _ 4 n <| by simp, ZMod.natCast_eq_natCast_iff' .. |>.mp h]
+    | 2 =>
+      apply Nat.xor_self
+    | 3 =>
+      apply xor_zero
 
 @[simp] theorem bit_lt_two_pow_succ_iff {b x n} : bit b x < 2 ^ (n + 1) ↔ x < 2 ^ n := by
   cases b <;> simp <;> omega
