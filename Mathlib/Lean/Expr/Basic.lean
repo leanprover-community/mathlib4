56c92dc5ecb47712b40cec8707de7111e42b551c
F. G. Dorais <fgdorais@gmail.com>
2025-11-09 07:54:59 -0500

    chore: adaptation for batteries#1496

--- Mathlib/Lean/Expr/Basic.lean
+++ Mathlib/Lean/Expr/Basic.lean
@@ -6,6 +6,7 @@ Floris van Doorn, Edward Ayers, Arthur Paulino
 -/
 import Mathlib.Init
 import Lean.Expr
+import Batteries.Lean.Expr
 
 /-!
 # Additional operations on Expr and related types
@@ -365,16 +366,6 @@ def getBinderName (e : Expr) : MetaM (Option Name) := do
 def mapForallBinderNames : Expr → (Name → Name) → Expr
   | .forallE n d b bi, f => .forallE (f n) d (mapForallBinderNames b f) bi
   | e, _ => e
-
-open Lean.Elab.Term
-/-- Annotates a `binderIdent` with the binder information from an `fvar`. -/
-def addLocalVarInfoForBinderIdent (fvar : Expr) (tk : TSyntax ``binderIdent) : MetaM Unit :=
-  -- the only TermElabM thing we do in `addLocalVarInfo` is check inPattern,
-  -- which we assume is always false for this function
-  discard <| TermElabM.run do
-    match tk with
-    | `(binderIdent| $n:ident) => Elab.Term.addLocalVarInfo n fvar
-    | tk => Elab.Term.addLocalVarInfo (Unhygienic.run `(_%$tk)) fvar
 
 /-- If `e` has a structure as type with field `fieldName`, `mkDirectProjection e fieldName` creates
 the projection expression `e.fieldName` -/
