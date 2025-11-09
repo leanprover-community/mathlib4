e8d61994ac7808dc2dfa8b3d5ec0cf2e25624d2d
Yongxi (Aaron) Lin <97214596+CoolRmal@users.noreply.github.com>
2025-11-09 15:27:07 -0500

    Update Mathlib/Analysis/LocallyConvex/Separation.lean
    Co-authored-by: Etienne Marion <66847262+EtienneC30@users.noreply.github.com>

--- Mathlib/Analysis/LocallyConvex/Separation.lean
+++ Mathlib/Analysis/LocallyConvex/Separation.lean
@@ -387,7 +387,7 @@ theorem iInter_nat_halfSpaces_eq_of_prod {F : Type*} {s : Set (E × F)}
     ∃ (L : ℕ → E →L[𝕜] 𝕜) (T : ℕ → F →L[𝕜] 𝕜) (c : ℕ → ℝ),
     ⋂ (i : ℕ), {(x, y) | c i ≤ re (L i x) + re (T i y)} = s
     ∧ (s.Nonempty → s ≠ univ → ∀ i, ∃ (x : E) (y : F), re (L i x) + re (T i y) ≠ 0) := by
-  have ⟨LT, c, eq1, eq2⟩ := iInter_nat_halfSpaces_eq (𝕜 := 𝕜) hs₁ hs₂ hsep
+  obtain ⟨LT, c, eq1, eq2⟩ := iInter_nat_halfSpaces_eq (𝕜 := 𝕜) hs₁ hs₂ hsep
   use fun i ↦ (LT i).comp (.inl 𝕜 E F), fun i ↦ (LT i).comp (.inr 𝕜 E F), c
   constructor
   · rw [← eq1]
