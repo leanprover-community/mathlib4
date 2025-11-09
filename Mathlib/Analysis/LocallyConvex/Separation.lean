46d86128fe9d375d505ea78c15e47f999c7f57bb
Rmal <97214596+CoolRmal@users.noreply.github.com>
2025-11-09 15:36:03 -0500

    Update Separation.lean

--- Mathlib/Analysis/LocallyConvex/Separation.lean
+++ Mathlib/Analysis/LocallyConvex/Separation.lean
@@ -324,8 +324,7 @@ section Countable
 variable [NormedAddCommGroup E] [NormedSpace ℝ E] [Module 𝕜 E] [ContinuousSMul 𝕜 E]
 
 /-- A closed convex set `s` is the intersection of countably many half spaces in a separable Banach
-space. Moreover, these halfspaces are all nontrivial if `s` is nonempty. This is Lemma 1.2.9 in
-[Hytonen_VanNeerven_Veraar_Wies_2016]. -/
+space. Moreover, these halfspaces are all nontrivial if `s` is nonempty. -/
 theorem iInter_nat_halfSpaces_eq
     (hs₁ : Convex ℝ s) (hs₂ : IsClosed s) (hsep : IsSeparable sᶜ) :
     ∃ (L : ℕ → E →L[𝕜] 𝕜) (c : ℕ → ℝ),
