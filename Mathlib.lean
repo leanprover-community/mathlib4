f4b199efb268d9cdb1fabd7af7f3a990ce631e73
Kim Morrison <kim@tqft.net>
2025-11-10 15:01:20 +1100

    mk_all

--- Mathlib.lean
+++ Mathlib.lean
@@ -1344,6 +1344,7 @@ import Mathlib.AlgebraicTopology.FundamentalGroupoid.FundamentalGroup
 import Mathlib.AlgebraicTopology.FundamentalGroupoid.InducedMaps
 import Mathlib.AlgebraicTopology.FundamentalGroupoid.PUnit
 import Mathlib.AlgebraicTopology.FundamentalGroupoid.Product
+import Mathlib.AlgebraicTopology.FundamentalGroupoid.SemilocallySimplyConnected
 import Mathlib.AlgebraicTopology.FundamentalGroupoid.SimplyConnected
 import Mathlib.AlgebraicTopology.ModelCategory.Basic
 import Mathlib.AlgebraicTopology.ModelCategory.BrownLemma
