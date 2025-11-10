e17692efa1f30540047d455724f0e5d6b4dcbd66
Oliver Nash <7734364+ocfnash@users.noreply.github.com>
2025-11-10 14:52:21 +0000

    chore: state `writtenInExtChartAt_comp` using `Filter.EventuallyEq` (#31469)

--- Mathlib/Geometry/Manifold/MFDeriv/Basic.lean
+++ Mathlib/Geometry/Manifold/MFDeriv/Basic.lean
@@ -496,9 +496,9 @@ theorem MDifferentiable.continuous (h : MDifferentiable I I' f) : Continuous f :
 /-! ### Deriving continuity from differentiability on manifolds -/
 
 theorem writtenInExtChartAt_comp (h : ContinuousWithinAt f s x) :
-    {y | writtenInExtChartAt I I'' x (g ∘ f) y =
-          (writtenInExtChartAt I' I'' (f x) g ∘ writtenInExtChartAt I I' x f) y} ∈
-      𝓝[(extChartAt I x).symm ⁻¹' s ∩ range I] (extChartAt I x) x := by
+    writtenInExtChartAt I I'' x (g ∘ f)
+      =ᶠ[𝓝[(extChartAt I x).symm ⁻¹' s ∩ range I] (extChartAt I x x)]
+      (writtenInExtChartAt I' I'' (f x) g ∘ writtenInExtChartAt I I' x f) := by
   apply
     @Filter.mem_of_superset _ _ (f ∘ (extChartAt I x).symm ⁻¹' (extChartAt I' (f x)).source) _
       (extChartAt_preimage_mem_nhdsWithin
