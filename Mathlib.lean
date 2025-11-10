138711337be427e1ef9ec35ebe447b468a037a73
Monica Omar <23701951+themathqueen@users.noreply.github.com>
2025-11-10 14:52:18 +0000

    feat(Algebra/Order/Star): add `StarOrderedRing (Π i, A i)` (#29824)

--- Mathlib.lean
+++ Mathlib.lean
@@ -999,6 +999,7 @@ import Mathlib.Algebra.Order.Ring.WithTop
 import Mathlib.Algebra.Order.Round
 import Mathlib.Algebra.Order.Star.Basic
 import Mathlib.Algebra.Order.Star.Conjneg
+import Mathlib.Algebra.Order.Star.Pi
 import Mathlib.Algebra.Order.Star.Prod
 import Mathlib.Algebra.Order.Sub.Basic
 import Mathlib.Algebra.Order.Sub.Defs
