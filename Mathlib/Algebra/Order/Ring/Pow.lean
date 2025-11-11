66eb103f78d554202bd903edb1891c5cb13c7ca4
Yury G. Kudryashov <urkud@urkud.name>
2025-11-11 01:49:40 -0600

    feat: add a version of Bernoulli's inequality

--- Mathlib/Algebra/Order/Ring/Pow.lean
+++ Mathlib/Algebra/Order/Ring/Pow.lean
@@ -5,34 +5,49 @@ Authors: Yury Kudryashov
 -/
 import Mathlib.Data.Nat.Cast.Commute
 import Mathlib.Data.Nat.Cast.Order.Ring
+import Mathlib.Tactic.Abel
 
 /-! # Bernoulli's inequality -/
 
 variable {R : Type*}
 
 section OrderedSemiring
-variable [Semiring R] [PartialOrder R] [IsOrderedRing R] {a : R}
+variable [Semiring R] [PartialOrder R] [IsOrderedRing R] {a b : R}
+
+/-- Bernoulli's inequality for `b / a`, written after multiplication by the denominators. -/
+lemma Commute.pow_add_mul_le_add_pow_of_sq_nonneg (Hcomm : Commute a b) (ha : 0 ≤ a)
+    (Hsq : 0 ≤ b * b) (Hsq' : 0 ≤ (a + b) * (a + b))
+    (H : 0 ≤ 2 * a + b) : ∀ n : ℕ, a ^ n + n * a ^ (n - 1) * b ≤ (a + b) ^ n
+  | 0 => by simp
+  | 1 => by simp
+  | 2 =>
+    calc
+      a ^ 2 + (2 : ℕ) * a ^ 1 * b ≤ a ^ 2 + (2 : ℕ) * a ^ 1 * b + b * b :=
+        le_add_of_nonneg_right Hsq
+      _ = (a + b) ^ 2 := by simp [sq, add_mul, mul_add, two_mul, Hcomm.eq, add_assoc]
+  | n + 3 => by
+    calc
+      _ ≤ a ^ (n + 3) + ↑(n + 3) * a ^ (n + 2) * b +
+            ((n + 1) * (b * b * (2 * a + b) * a ^ n) + a ^ (n + 1) * (b * b)) :=
+        le_add_of_nonneg_right <| by
+          apply_rules [add_nonneg, mul_nonneg, Nat.cast_nonneg, pow_nonneg, zero_le_one]
+      _ = (a + b) * (a + b) * (a ^ (n + 1) + ↑(n + 1) * a ^ n * b) := by
+        simp only [Nat.cast_add, Nat.cast_one, Nat.cast_ofNat, pow_succ', add_mul, mul_add, two_mul,
+          Hcomm.eq, (n.cast_commute (_ : R)).symm.left_comm, mul_assoc, (Hcomm.pow_left _).eq,
+          (Hcomm.pow_left _).left_comm, Hcomm.left_comm, ← @two_add_one_eq_three R, one_mul]
+        abel
+      _ ≤ (a + b) * (a + b) * (a + b) ^ (n + 1) := by
+        gcongr
+        · assumption
+        · apply Commute.pow_add_mul_le_add_pow_of_sq_nonneg <;> assumption
+      _ = (a + b) ^ (n + 3) := by simp only [pow_succ', mul_assoc]
 
 /-- **Bernoulli's inequality**. This version works for semirings but requires
 additional hypotheses `0 ≤ a * a` and `0 ≤ (1 + a) * (1 + a)`. -/
-lemma one_add_mul_le_pow' (Hsq : 0 ≤ a * a) (Hsq' : 0 ≤ (1 + a) * (1 + a)) (H : 0 ≤ 2 + a) :
-    ∀ n : ℕ, 1 + n * a ≤ (1 + a) ^ n
-  | 0 => by simp
-  | 1 => by simp
-  | n + 2 =>
-    have : 0 ≤ n * (a * a * (2 + a)) + a * a :=
-      add_nonneg (mul_nonneg n.cast_nonneg (mul_nonneg Hsq H)) Hsq
-    calc
-      _ ≤ 1 + ↑(n + 2) * a + (n * (a * a * (2 + a)) + a * a) := le_add_of_nonneg_right this
-      _ = (1 + a) * (1 + a) * (1 + n * a) := by
-          simp only [Nat.cast_add, add_mul, mul_add, one_mul, mul_one, ← one_add_one_eq_two,
-            Nat.cast_one, add_assoc]
-          simp only [← add_assoc, add_comm _ (↑n * a)]
-          simp only [add_assoc, (n.cast_commute (_ : R)).left_comm]
-          simp only [add_comm, add_left_comm]
-      _ ≤ (1 + a) * (1 + a) * (1 + a) ^ n :=
-        mul_le_mul_of_nonneg_left (one_add_mul_le_pow' Hsq Hsq' H _) Hsq'
-      _ = (1 + a) ^ (n + 2) := by simp only [pow_succ', mul_assoc]
+lemma one_add_mul_le_pow' (Hsq : 0 ≤ a * a) (Hsq' : 0 ≤ (1 + a) * (1 + a)) (H : 0 ≤ 2 + a) (n : ℕ) :
+    1 + n * a ≤ (1 + a) ^ n := by
+  simpa using (Commute.one_left a).pow_add_mul_le_add_pow_of_sq_nonneg zero_le_one Hsq Hsq'
+    (by simpa using H) n
 
 end OrderedSemiring
 
