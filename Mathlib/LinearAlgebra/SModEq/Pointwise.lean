7f628917fe7f6d136f66717934af13350e930ec6
Jiedong Jiang <emailboxofjjd@163.com>
2025-11-11 18:57:45 +0800

    fix

--- Mathlib/LinearAlgebra/SModEq/Pointwise.lean
+++ Mathlib/LinearAlgebra/SModEq/Pointwise.lean
@@ -13,11 +13,22 @@ In this file, we record more lemmas about `SModEq` on elements
 of modules or rings.
 -/
 
+open Submodule
+
+open Polynomial
+
+variable {R : Type*} [Ring R] {I : Ideal R}
+variable {M : Type*} [AddCommGroup M] [Module R M] {U : Submodule R M}
+
+namespace SModEq
+
 /--
 A variant of `SModEq.smul`, where the scalar belongs to an ideal.
 -/
-theorem smul' {I : Ideal R} (hxy : x ≡ y [SMOD U])
+theorem smul' (hxy : x ≡ y [SMOD U])
     {c : R} (hc : c ∈ I) : c • x ≡ c • y [SMOD (I • U)] := by
   rw [SModEq.sub_mem] at hxy ⊢
   rw [← smul_sub]
   exact smul_mem_smul hc hxy
+
+end SModEq
