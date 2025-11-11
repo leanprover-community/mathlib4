027b73dfede5136481af49cd4c4eef92b5ad9434
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-11-11 22:52:52 +0100

    Rewrite PR title validation to use parsec (more)

--- Mathlib/Tactic/Linter/ValidatePRTitle.lean
+++ Mathlib/Tactic/Linter/ValidatePRTitle.lean
@@ -14,12 +14,45 @@ Not all checks from the commit conventions are implemented: for instance, no eff
 verify whether the title or body are written in present imperative tense.
 -/
 
-/-- Split a string in two, at a given position. Assumes `pos` is valid. -/
--- TODO: this should be added to batteries, as a version of `splitOn`
-def splitAtPos (s : String) (pos : String.Pos.Raw) : String × String :=
-  let before := String.Pos.Raw.extract s 0 pos
-  let after := String.Pos.Raw.extract s (String.Pos.Raw.next s pos) s.endPos
-  (before, after)
+open Std.Internal.Parsec String
+
+-- TODO: also parse and return the main PR title
+def prTitle : Parser (String × Option String) :=
+  Prod.mk
+    <$> (["feat", "chore", "perf", "refactor", "style", "fix", "doc", "test", "ci"].firstM pstring)
+    <*> (
+      (skipString "(" *> some <$> manyChars (notFollowedBy (skipString "):") *> any) <* skipString "): ")
+      <|> (skipString ": " *> pure none)
+    )
+
+-- Some self-tests for the parser.
+/-- info: Except.ok ("feat", some "x") -/
+#guard_msgs in
+#eval Parser.run prTitle "feat(x): foo"
+/-- info: Except.ok ("feat", none) -/
+#guard_msgs in
+#eval Parser.run prTitle "feat: foo"
+/-- info: Except.error "offset 10: expected: ): " -/
+#guard_msgs in
+#eval Parser.run prTitle "feat(: foo"
+/-- info: Except.error "offset 4: expected: : " -/
+#guard_msgs in
+#eval Parser.run prTitle "feat): foo"
+/-- info: Except.error "offset 4: expected: : " -/
+#guard_msgs in
+#eval Parser.run prTitle "feat)(: foo"
+/-- info: Except.error "offset 4: expected: : " -/
+#guard_msgs in
+#eval Parser.run prTitle "feat)(sdf): foo"
+/-- info: Except.ok ("feat", some "sdf") -/
+#guard_msgs in
+#eval Parser.run prTitle "feat(sdf): foo:"
+/-- info: Except.error "offset 4: expected: : " -/
+#guard_msgs in
+#eval Parser.run prTitle "feat foo"
+/-- info: Except.ok ("chore", none) -/
+#guard_msgs in
+#eval Parser.run prTitle "chore: test"
 
 /--
 Check if `title` matches the mathlib conventions for PR titles
@@ -31,21 +64,24 @@ Return all error messages for violations found.
 -/
 def validateTitle (title : String) : Array String := Id.run do
   -- The title should be of the form "abbrev: main title" or "abbrev(scope): main title".
+  -- We use the parser above to extract abbrev, scope and main title,
+  -- but give some custom errors in some easily detectable cases.
   if !title.contains ':' then
     return #["error: the PR title does not contain a colon"]
 
-  -- Split 'title' at the first occurrence of ':'.
-  let (type, main) := splitAtPos title (title.find (· == ':'))
-  let mut errors := #[]
-  if main.endsWith "." then
-    errors := errors.push "error: the PR title should not end with a full stop"
-
-  let knownKinds := ["feat", "chore", "perf", "refactor", "style", "fix", "doc", "test", "ci"]
-  let mut isFine := false
-  for kind in knownKinds do
-    if type.startsWith kind then isFine := true
-  if isFine == false then
-    errors := errors.push s!"error: the PR title should be of the form \
-      \"kind: main title\" or \"kind(scope): main title\"\n
-      Known PR title kinds are {knownKinds}"
-  return errors
+  match Parser.run prTitle title with
+  | Except.error _ =>
+    return #[s!"error: the PR title should be of the form\n  abbrev: main title\nor\n  \
+      abbrev(scope): main title"]
+  | Except.ok (kind, _scope?) =>
+    -- Future: also check scope (and the main PR title)
+    let mut errors := #[]
+    let knownKinds := ["feat", "chore", "perf", "refactor", "style", "fix", "doc", "test", "ci"]
+    let mut isFine := false
+    for k in knownKinds do
+      if kind.startsWith k then isFine := true
+    if isFine == false then
+      errors := errors.push s!"error: the PR title should be of the form \
+        \"kind: main title\" or \"kind(scope): main title\"\n
+        Known PR title kinds are {knownKinds}"
+    return errors
