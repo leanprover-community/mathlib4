085b31512223eacc3cd77a0748679018643c3de4
thorimur <thomasmurrills@gmail.com>
2025-11-12 13:54:31 -0500

    temp

--- Mathlib/Tactic/Linter/UnusedDecidable.lean
+++ Mathlib/Tactic/Linter/UnusedDecidable.lean
@@ -127,11 +127,17 @@ def getDecls (t : InfoTree) : List Name :=
 --     | return []
 --   return decls
 
-partial def findSome? (p : Info → Option α) (t : InfoTree) : Option α :=
-  match t with
-  | .context _ t => findSome? p t
-  | .node i ts   => p i <|> ts.findSome? (findSome? p)
-  | _ => none
+partial def findSome? {α} (f : ContextInfo → Info → PersistentArray InfoTree → Option α) :
+    InfoTree → Option α :=
+  go none
+where go ctx?
+  | context ctx t => go (ctx.mergeIntoOuter? ctx?) t
+  | node i ts =>
+    let a := match ctx? with
+      | none => none
+      | some ctx => f ctx i ts
+    a <|> ts.findSome? (go <| i.updateContext? ctx?)
+  | hole _ => none
 
 /--
 Given a constant name, find the first `TermInfo` whose expression is exactly that constant. Does
@@ -146,22 +152,37 @@ def getTermInfoForConst? (t : InfoTree) (decl : Name) : Option TermInfo :=
 def getConstRef? (t : InfoTree) (decl : Name) : Option Syntax :=
   t.getTermInfoForConst? decl |>.map (·.stx)
 
-open Lean.Parser.Command in
-def getDeclSigRefByIdRange? (idRef : Syntax) : Syntax → TSyntax ``declSig
-  | `(Parser.Command.theorem| theorem $id$[.{$_}]? $sig:declSig $_:declVal)
-  | `(«lemma»| lemma $id$[.{$_}]? $sig:declSig $_:declVal) =>
-    if
 
-def withDeclSigRef {m : Type → Type} [Monad m] [MonadRef m] {α} (cmd : Syntax)
-    (t : InfoTree) (decl : Name) (x : m α) : m α := do
-  let some ref := getConstRef? t decl | x
+end Lean.Elab.InfoTree
+
+def _root_.Lean.Syntax.rangeEq? (stx₁ stx₂ : Syntax) (canonicalOnly₁ canonicalOnly₂ := true) :
+    Option Bool :=
+  match stx₁.getRange? canonicalOnly₁, stx₂.getRange? canonicalOnly₂ with
+  | some r₁, some r₂ => r₁ == r₂
+  | _, _ => none
+
+def _root_.Lean.Syntax.rangeEq (stx₁ stx₂ : Syntax) (canonicalOnly₁ canonicalOnly₂ := true) :
+    Bool :=
+  match stx₁.getRange? canonicalOnly₁, stx₂.getRange? canonicalOnly₂ with
+  | some r₁, some r₂ => r₁ == r₂
+  | _, _ => false
+
+def _root_.Lean.Syntax.hasRange (stx : Syntax) (range : String.Range) (canonicalOnly := true) : Bool :=
+  stx.getRange? canonicalOnly |>.map (· == range) |>.getD false
+
+
+
+private def Lean.Elab.InfoTree.withDeclSigRef {m : Type → Type} [Monad m] [MonadRef m] {α} (cmd : Syntax)
+    (t : InfoTree) (decl : Name) (x : m α) : m α := withRef cmd do
+  let some idRef := t.getConstRef? decl | x
   let sigRef? := cmd.findSome? fun
-      | `(Parser.Command.theorem| theorem $id$[.{$_}]? $sig:declSig $_:declVal)
-      | `(«lemma»| lemma $id$[.{$_}]? $sig:declSig $_:declVal) =>
-        if id.rangeEq ref then sig else none
-      | _ => none
-  withRef ref <| withRef? sigRef? x
-
+    | `(Parser.Command.theorem| theorem $id$[.{$_,*}]? $sig:declSig $_:declVal)
+    | `(«lemma»| lemma $id$[.{$_,*}]? $sig:declSig $_:declVal) =>
+      if id.raw.rangeEq idRef then sig else none
+    -- TODO: handle `let rec` decls (after `where`), handle defs etc.
+    | _ => none
+  -- Fall back to `idRef` if `sigRef` is not found or has no position info.
+  withRef idRef <| withRef? sigRef? x
 
 /-
 # Next up:
@@ -173,7 +194,6 @@ def withDeclSigRef {m : Type → Type} [Monad m] [MonadRef m] {α} (cmd : Syntax
 
 -/
 
-end Lean.Elab.InfoTree
 
 namespace Mathlib.Linter.UnusedDecidable
 
@@ -183,7 +203,6 @@ a of the same form (i.e. of the form `∀ (x₀ : X₀) (x₁ : X₁) ⋯, Decid
 checks if the constant is one of: `DecidableEq`, `DecidableLE`, `DecidableLT`, `DecidableRel`,
 `DecidablePred`, or `Decidable`.
 
-
 Runs `cleanupAnnotations` on `type` and `forallE` bodies, and ignores metadata in applications.
 -/
 @[inline] partial def isAppOrForallOfConstP (p : Name → Bool) (type : Expr) : Bool :=
@@ -192,16 +211,17 @@ Runs `cleanupAnnotations` on `type` and `forallE` bodies, and ignores metadata i
     | .forallE _ _ body _ => isAppOrForallOfConstP p body
     | _ => false
 
-@[inline] def isDecidableVariant (n : Name) : Bool :=
-  n == ``DecidableEq   ||
-  n == ``DecidableLE   ||
-  n == ``DecidableLT   ||
-  n == ``DecidableRel  ||
-  n == ``DecidablePred ||
-  n == ``Decidable
-
 @[inline] partial def isAppOrForallOfConst (declName : Name) (type : Expr) : Bool :=
   isAppOrForallOfConstP (· == declName) type
+
+def isDecidableVariant (type : Expr) : Bool :=
+  isAppOrForallOfConstP (type := type) fun n =>
+    n == ``DecidableEq   ||
+    n == ``DecidableLE   ||
+    n == ``DecidableLT   ||
+    n == ``DecidableRel  ||
+    n == ``DecidablePred ||
+    n == ``Decidable
 
 /--
 The `unusedDecidable` linter checks if a theorem's hypotheses include `Decidable*` instances which
@@ -218,39 +238,135 @@ register_option linter.unusedDecidable : Bool := {
     replaced with a use of `classical` in the proof."
 }
 
-def unusedDecidable : Linter where
-  run := fun stx => onTheorems fun decl
+def _root_.Nat.pluralizing (count : Nat) (singular plural : MessageData) : MessageData :=
+  if count = 1 then singular else plural
+
+def _root_.Lean.MessageData.pluralizing (count : Nat) (singular plural : MessageData) : MessageData :=
+  if count = 1 then singular else plural
+
+def _root_.String.withPlural (singular plural : String) (count : Nat) : String :=
+  if count = 1 then singular else plural
+
+def _root_.Lean.Environment.findTheoremAsync? (env : Environment) (decl : Name) :
+    Option ConstantVal := do
+  let info ← env.findAsync? decl
+  if info.kind matches .thm then info.toConstantVal else none
+
+def _root_.Lean.Elab.InfoTree.getTheorems (t : InfoTree) (env : Environment) : List ConstantVal :=
+  t.getDecls.filterMap env.findTheoremAsync?
+
+def _root_.Lean.Elab.InfoTree.getTheoremsWhere (t : InfoTree) (env : Environment)
+    (p : Name → Bool) : List ConstantVal :=
+  t.getDecls.filterMap fun decl => if p decl then env.findTheoremAsync? decl else none
+
+def onTheorems (x : ConstantVal → InfoTree → CommandElabM Unit) : CommandElabM Unit := do
+  for t in ← getInfoTrees do
+    let decls := t.getTheorems (← getEnv)
+    for decl in decls do
+      x decl t
+
+def onTheoremsWhere (p : Name → Bool) (x : ConstantVal → InfoTree → CommandElabM Unit) :
+    CommandElabM Unit := do
+  for t in ← getInfoTrees do
+    let decls := t.getTheoremsWhere (← getEnv) p
+    for decl in decls do
+      x decl t
+
+
+-- def _root_.Lean.Elab.InfoTree.getTheorems (t : InfoTree) (env : Environment) : List ConstantVal :=
+--   t.getDecls.filterMap fun decl => do
+--     let info ← env.findAsync? decl
+--     if info.kind matches .thm then info.toConstantVal else none
+
+
+structure Parameter where
+  fvar? : Option Expr
+  type? : Option Expr
+  idx : Nat
+
+instance : ToMessageData Parameter where
+  toMessageData (param : Parameter) :=
+    if let some type := param.type? then
+      m!"`[{type}]` (#{param.idx})"
+    else
+      m!"parameter #{param.idx}"
+
+/-- Given a (full, resolvable) declaration name `foo` and an array of parameters
+`#[p₁, p₂, ..., pₙ]`, constructs the message:
+> \`{foo}\` has the hypothes(is/es) {p₁}, {p₂}, ..., and {pₙ} which (is/are) not used in the
+  remainder of the type.
+-/
+def _root_.Lean.Name.unusedInstancesMsg (declName : Name)
+    (unusedInstanceBinders : Array Parameter) : MessageData :=
+  let unusedInstanceBinders := unusedInstanceBinders.map toMessageData
+  m!"`{.ofConstName declName}` has the \
+  {"hypothesis".withPlural "hypotheses" unusedInstanceBinders.size} \
+  {.andList unusedInstanceBinders.toList} which \
+  {"is".withPlural "are" unusedInstanceBinders.size} not used in the remainder of the type."
+
+
+def getLCtxOfDecl? (t : InfoTree) (decl : Name) : Option LocalContext :=
+  t.findSome? fun ctx i ch =>
+    match i with
+    | .ofCustomInfo i => do
+      guard <| i.value.typeName == ``Lean.Elab.Term.BodyInfo && ctx.parentDecl? == some decl
+      let body ← ch[0]?
+      let rec go ctx? : InfoTree → Option LocalContext
+        | .context ctx'? t => go (ctx'?.mergeIntoOuter? ctx?) t
+        | .node i _ => match i with
+          | .ofTermInfo ti => ti.lctx
+          | .ofTacticInfo ti => match ti.goalsBefore with
+            | [mvarId] => ti.mctxBefore.getDecl mvarId |>.lctx
+            | _ => none
+          | _ => none
+        | .hole _ => none
+      go ctx body
+    | _ => none
+
+/--
+Gathers instance hypotheses in the type of `decl` that are unused in the remainder of the type and
+whose types satisfy `p`. (Does not consider the body of the declaration.) Collects them into an
+`Array Parameter`, and if (and only if) this array is nonempty, feeds it to `logOnUnused`.
+
+Since `logOnUnused` will only be run if its argument is nonempty, it is allowed to be expensive.
+
+Note that `p` is non-monadic, and may encounter loose bvars in its argument. This is a performance
+optimization. However, the `Parameter`s are created in a telescope, and their fields will *not*
+have loose bound variables.
+-/
+def _root_.Lean.Elab.InfoTree.onUnusedInstancesWhere (t : InfoTree) (decl : ConstantVal)
+    (p : Expr → Bool) (logOnUnused : Array Parameter → TermElabM Unit) : CommandElabM Unit := do
+  let unusedInstances := decl.type.getUnusedForallInstanceBinderIdxsWhere p
+  if let some maxIdx := unusedInstances.back? then liftTermElabM do
+    forallBoundedTelescope decl.type (some <| maxIdx + 1)
+      (cleanupAnnotations := true) fun fvars _ => do
+        let unusedInstances : Array Parameter ← unusedInstances.mapM fun idx =>
+          return {
+              fvar? := fvars[idx]?
+              type? := ← fvars[idx]?.mapM (inferType ·)
+              idx
+            }
+        logOnUnused unusedInstances
+
+def _root_.Lean.Syntax.logUnusedInstancesInTheoremsWhere (cmd : Syntax) (linter : Lean.Option Bool)
+    (onlyOnNames : Name → Bool) (onlyOnInstanceTypes : Expr → Bool) (msg : MessageData) :
+    CommandElabM Unit := do
+  for t in ← getInfoTrees do
+    let thms := t.getTheoremsWhere (← getEnv) onlyOnNames
+    for thm in thms do
+      t.onUnusedInstancesWhere thm onlyOnInstanceTypes fun unusedDecidables _ _ =>
+        t.withDeclSigRef cmd thm.name do
+          logLint linter (← getRef)
+            m!"{thm.name.unusedInstancesMsg unusedDecidables}\n{msg}"
 
 /-- The linter for unused `Decidable*` hypotheses in proposition declarations. -/
-def unusedDecidable' : Linter where
-  run := withSetOptionIn fun stx => do
+def unusedDecidable : Linter where
+  run := withSetOptionIn fun cmd => do
     unless getLinterValue linter.unusedDecidable (← getLinterOptions) do
       return
-    for t in ← getInfoTrees do
-      let decls := t.getDecls
-      -- For now, only handle `theorem`s.
-      -- TODO: handle `def` propositions too; requires re-running `withHeaderSecVars`
-      let env ← getEnv
-      let decls := decls.filter fun decl =>
-        (`Decidable).isPrefixOf decl || wasOriginallyTheorem env decl
-      for decl in decls do
-        let some val := (← getEnv).findConstVal? decl | continue
-        let unusedDecidableHyps :=
-          val.type.getUnusedForallInstanceBinderIdxsWhere (isAppOrForallOfConstP isDecidableVariant)
-
-        unless unusedDecidableHyps.isEmpty do liftTermElabM do
-          -- TODO: get syntax from infotrees for logging and Try This suggestion
-          forallBoundedTelescope val.type (some <| unusedDecidableHyps.back! + 1)
-            (cleanupAnnotations := true) fun fvars _ => do
-              let decidables ← unusedDecidableHyps.mapM fun idx =>
-                return m!"`[{← inferType fvars[idx]!}]`"
-              logLint linter.unusedDecidable (← getRef) m!"\
-                `{.ofConstName val.name}` has the \
-                {if decidables.size = 1 then "hypothesis" else "hypotheses"} \
-                {.andList decidables.toList} which \
-                {if decidables.size = 1 then "is" else "are"} not used in the remainder of the \
-                type.\n\
-                Consider removing these hypotheses and using `classical` in the proof instead."
+    cmd.logUnusedInstancesInTheoremsWhere linter.unusedDecidable
+      (!(`Decidable).isPrefixOf ·) isDecidableVariant
+      m!"Consider removing these hypotheses and using `classical` in the proof instead."
 
 initialize addLinter unusedDecidable
 
