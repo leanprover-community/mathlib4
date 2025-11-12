fde98b27ef633418674cde7e22781104bafe3cfe
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-11-12 18:48:27 +0100

    better simp

--- Mathlib/Algebra/Homology/Factorizations/CM5b.lean
+++ Mathlib/Algebra/Homology/Factorizations/CM5b.lean
@@ -52,7 +52,7 @@ instance (n : ℤ) [K.IsStrictlyGE (n + 1)] [L.IsStrictlyGE n] :
   intro i hi
   refine IsZero.of_iso ?_ ((HomologicalComplex.eval C (ComplexShape.up ℤ) i).mapBiprod _ _)
   dsimp
-  simp only [biprod_isZero_iff, mappingCone.isZero_X_iff]
+  simp only [biprod_isZero_iff, mappingCone.isZero_X_iff, I_X]
   refine ⟨⟨?_, ?_⟩, L.isZero_of_isStrictlyGE n i hi⟩
   all_goals exact (I K).isZero_of_isStrictlyGE (n + 1) _
 
