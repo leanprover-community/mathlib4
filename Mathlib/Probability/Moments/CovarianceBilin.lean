d536a905c804b0526a469f2874078065539c3ab0
Sebastien Gouezel <sebastien.gouezel@univ-rennes1.fr>
2025-11-12 23:04:57 +0100

    Apply suggestion from @sgouezel

--- Mathlib/Probability/Moments/CovarianceBilin.lean
+++ Mathlib/Probability/Moments/CovarianceBilin.lean
@@ -3,7 +3,6 @@ Copyright (c) 2025 Rémy Degenne. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Rémy Degenne, Etienne Marion
 -/
-
 import Mathlib.Analysis.InnerProductSpace.Positive
 import Mathlib.Analysis.Normed.Lp.MeasurableSpace
 import Mathlib.MeasureTheory.SpecificCodomains.WithLp
