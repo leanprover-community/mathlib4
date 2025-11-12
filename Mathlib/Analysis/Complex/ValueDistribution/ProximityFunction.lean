ac48e6adc8b30cd28a5e3d428a25300457ae7243
Stefan Kebekus <5110976+kebekus@users.noreply.github.com>
2025-11-12 06:28:01 +0000

    feat: behaviour of Nevanlinna functions under multiplication (#30494)
    Establish the behavior of Nevanlinna functions under multiplication.
    
    This material is used in [Project VD](https://github.com/kebekus/ProjectVD), which aims to formalize Value Distribution Theory for meromorphic functions on the complex plane. The formula established here is part of a larger package discussing the behavior of the Nenvanlinna height under algebraic manipulations of the functions.

--- Mathlib/Analysis/Complex/ValueDistribution/ProximityFunction.lean
+++ Mathlib/Analysis/Complex/ValueDistribution/ProximityFunction.lean
@@ -106,4 +106,48 @@ theorem proximity_sub_proximity_inv_eq_circleAverage {f : ℂ → ℂ} (h₁f : 
   · simp_rw [← norm_inv]
     apply circleIntegrable_posLog_norm_meromorphicOn (h₁f.inv.mono_set (by tauto))
 
+/-!
+## Behaviour under Arithmetic Operations
+-/
+
+/--
+The proximity function `f * g` at `⊤` is less than or equal to the sum of the
+proximity functions of `f` and `g`, respectively.
+-/
+theorem proximity_top_mul_le {f₁ f₂ : ℂ → ℂ} (h₁f₁ : MeromorphicOn f₁ Set.univ)
+    (h₁f₂ : MeromorphicOn f₂ Set.univ) :
+    proximity (f₁ * f₂) ⊤ ≤ (proximity f₁ ⊤) + (proximity f₂ ⊤) := by
+  calc proximity (f₁ * f₂) ⊤
+  _ = circleAverage (fun x ↦ log⁺ (‖f₁ x‖ * ‖f₂ x‖)) 0 := by
+    simp [proximity]
+  _ ≤ circleAverage (fun x ↦ log⁺ ‖f₁ x‖ + log⁺ ‖f₂ x‖) 0 := by
+    intro r
+    apply circleAverage_mono
+    · simp_rw [← norm_mul]
+      apply circleIntegrable_posLog_norm_meromorphicOn
+      exact MeromorphicOn.fun_mul (fun x a ↦ h₁f₁ x trivial) fun x a ↦ h₁f₂ x trivial
+    · apply (circleIntegrable_posLog_norm_meromorphicOn (fun x a ↦ h₁f₁ x trivial)).add
+        (circleIntegrable_posLog_norm_meromorphicOn (fun x a ↦ h₁f₂ x trivial))
+    · exact fun _ _ ↦ posLog_mul
+  _ = circleAverage (log⁺ ‖f₁ ·‖) 0 + circleAverage (log⁺ ‖f₂ ·‖) 0:= by
+    ext r
+    apply circleAverage_add
+    · exact circleIntegrable_posLog_norm_meromorphicOn (fun x a ↦ h₁f₁ x trivial)
+    · exact circleIntegrable_posLog_norm_meromorphicOn (fun x a ↦ h₁f₂ x trivial)
+  _ = (proximity f₁ ⊤) + (proximity f₂ ⊤) := rfl
+
+/--
+The proximity function `f * g` at `0` is less than or equal to the sum of the
+proximity functions of `f` and `g`, respectively.
+-/
+theorem proximity_zero_mul_le {f₁ f₂ : ℂ → ℂ} (h₁f₁ : MeromorphicOn f₁ Set.univ)
+    (h₁f₂ : MeromorphicOn f₂ Set.univ) :
+    proximity (f₁ * f₂) 0 ≤ (proximity f₁ 0) + (proximity f₂ 0) := by
+  calc proximity (f₁ * f₂) 0
+  _ ≤ (proximity f₁⁻¹ ⊤) + (proximity f₂⁻¹ ⊤) := by
+    rw [← proximity_inv, mul_inv]
+    apply proximity_top_mul_le (MeromorphicOn.inv_iff.mpr h₁f₁) (MeromorphicOn.inv_iff.mpr h₁f₂)
+  _ = (proximity f₁ 0) + (proximity f₂ 0) := by
+    rw [proximity_inv, proximity_inv]
+
 end ValueDistribution
