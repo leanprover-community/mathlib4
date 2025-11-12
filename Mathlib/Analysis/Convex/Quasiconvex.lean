8b0939e45ff5ca0446b4791624755119bb753e5c
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2025-11-12 10:42:45 +0100

    properties of quasiconcave functions

--- Mathlib/Analysis/Convex/Quasiconvex.lean
+++ Mathlib/Analysis/Convex/Quasiconvex.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yaël Dillies
 -/
 import Mathlib.Analysis.Convex.Function
+import Mathlib.Analysis.Convex.Topology
 
 /-!
 # Quasiconvex and quasiconcave functions
@@ -82,6 +83,106 @@ theorem QuasiconcaveOn.convex [IsDirected β (· ≥ ·)] (hf : QuasiconcaveOn �
   hf.dual.convex
 
 end LE_β
+
+section Composition
+
+variable {𝕜 E β : Type*} [Semiring 𝕜] [PartialOrder 𝕜]
+  [AddCommMonoid E] [SMul 𝕜 E]
+
+variable {β γ : Type*} [LinearOrder β] [Preorder γ]
+
+variable {s : Set E} {f : E → β} {g : β → γ}
+
+theorem QuasiconvexOn.monotone_comp
+    (hg : Monotone g) (hf : QuasiconvexOn 𝕜 s f) :
+    QuasiconvexOn 𝕜 s (g ∘ f) := fun c x hx y hy ↦ by
+  simp only [Function.comp_apply, mem_setOf_eq] at hx hy
+  intro a b ha hb hab
+  simp only [Function.comp_apply, mem_setOf_eq]
+  cases le_total (f x) (f y) with
+  | inl h =>
+      specialize hf (f y) ⟨hx.1, h⟩ ⟨hy.1, le_rfl⟩ ha hb hab
+      simp only [mem_setOf_eq] at hf
+      exact ⟨hf.1, le_trans (hg hf.2) hy.2⟩
+  | inr h =>
+      specialize hf (f x) ⟨hx.1, le_rfl⟩ ⟨hy.1, h⟩ ha hb hab
+      simp only [mem_setOf_eq] at hf
+      exact ⟨hf.1, le_trans (hg hf.2) hx.2⟩
+
+theorem QuasiconvexOn.antitone_comp (hg : Antitone g) (hf : QuasiconvexOn 𝕜 s f) :
+  QuasiconcaveOn 𝕜 s (g ∘ f) :=
+  hf.monotone_comp (γ := γᵒᵈ) hg
+
+theorem QuasiconcaveOn.monotone_comp (hg : Monotone g) (hf : QuasiconcaveOn 𝕜 s f) :
+  QuasiconcaveOn 𝕜 s (g ∘ f) := by
+  apply QuasiconvexOn.monotone_comp (β := βᵒᵈ) (γ := γᵒᵈ) (f := f) _ hf
+  rw [← monotone_dual_iff] ; exact hg
+
+theorem QuasiconcaveOn.antitone_comp (hg : Antitone g) (hf : QuasiconcaveOn 𝕜 s f) :
+  QuasiconvexOn 𝕜 s (g ∘ f) := by
+  apply QuasiconvexOn.antitone_comp (β := βᵒᵈ) (γ := γᵒᵈ) (f := f) _ hf
+  rw [← antitone_dual_iff]; exact hg
+
+theorem QuasilinearOn.monotone_comp (hg : Monotone g) (hf : QuasilinearOn 𝕜 s f) :
+  QuasilinearOn 𝕜 s (g ∘ f) :=
+  ⟨hf.1.monotone_comp hg, hf.2.monotone_comp hg⟩
+
+theorem QuasilinearOn.antitone_comp (hg : Antitone g) (hf : QuasilinearOn 𝕜 s f) :
+  QuasilinearOn 𝕜 s (g ∘ f) :=
+  ⟨hf.2.antitone_comp hg, hf.1.antitone_comp hg⟩
+
+end Composition
+
+section Restriction
+
+variable {𝕜 E : Type*} [Semiring 𝕜] [PartialOrder 𝕜]
+  [AddCommMonoid E] [SMul 𝕜 E]
+variable {β : Type*} [Preorder β]
+variable {s : Set E} {f : E → β}
+
+theorem Convex.quasiconvexOn_restrict {t : Set E} (hf : QuasiconvexOn 𝕜 s f) (hst : t ⊆ s)
+    (ht : Convex 𝕜 t) : QuasiconvexOn 𝕜 t f :=
+  by
+  intro b
+  rw [Set.sep_of_subset hst]
+  exact Convex.inter ht (hf b)
+
+theorem Convex.quasiconcaveOn_restrict {t : Set E} (hf : QuasiconcaveOn 𝕜 s f) (hst : t ⊆ s)
+    (ht : Convex 𝕜 t) : QuasiconcaveOn 𝕜 t f :=
+  by
+  intro b
+  rw [Set.sep_of_subset hst]
+  exact Convex.inter ht (hf b)
+
+end Restriction
+
+section Preconnected
+
+variable {E : Type*} [AddCommGroup E] [Module ℝ E]
+  [TopologicalSpace E] [IsTopologicalAddGroup E] [ContinuousSMul ℝ E]
+
+variable {β : Type*} [Preorder β] {f : E → β}
+
+theorem QuasiconcaveOn.isPreconnected_preimage {s : Set E} {t : β}
+    (hfc : QuasiconcaveOn ℝ s f) :
+    IsPreconnected (f ∘ (fun x ↦ ↑x) ⁻¹' Ici t : Set s) := by
+  rw [preimage_comp,
+    ← Topology.IsInducing.subtypeVal.isPreconnected_image,
+    image_preimage_eq_inter_range,
+    Subtype.range_coe, inter_comm]
+  exact (hfc t).isPreconnected
+
+theorem QuasiconvexOn.isPreconnected_preimage {s : Set E} {t : β}
+    (hfc : QuasiconvexOn ℝ s f) :
+    IsPreconnected (f ∘ (fun x ↦ ↑x) ⁻¹' Iic t : Set s) := by
+  exact QuasiconcaveOn.isPreconnected_preimage (β := βᵒᵈ) hfc
+
+theorem QuasilinearOn.isPreconnected_preimage {s : Set E} {t : β}
+    (hfc : QuasilinearOn ℝ s f) :
+    IsPreconnected (f ∘ (fun x ↦ ↑x) ⁻¹' Iic t : Set s) :=
+  hfc.left.isPreconnected_preimage
+
+end Preconnected
 
 section Semilattice_β
 
