9865ac9d59d0cd98532a37121097ea827fa5e9a6
Julian Komaromy <julian.komaromy@gmail.com>
2025-11-12 13:38:02 +0100

    show that left and right homotopies define eqrels

--- Mathlib/AlgebraicTopology/Quasicategory/TwoTruncated.lean
+++ Mathlib/AlgebraicTopology/Quasicategory/TwoTruncated.lean
@@ -4,7 +4,7 @@ import Mathlib.AlgebraicTopology.SimplicialSet.CompStructTruncated
 open CategoryTheory SimplicialObject.Truncated
 
 namespace SSet.Truncated
-open Edge
+open Edge CompStruct
 
 class Quasicategory₂ (X : Truncated 2) where
   fill21 {x₀ x₁ x₂ : X _⦋0⦌₂}
@@ -24,5 +24,75 @@ class Quasicategory₂ (X : Truncated 2) where
       (f₀ : CompStruct e₁₂ e₂₃ e₁₃)
       (f₁ : CompStruct e₀₂ e₂₃ e₀₃) :
       Nonempty (CompStruct e₀₁ e₁₃ e₀₃)
+
+/--
+Two edges `f` and `g` are left homotopic if there is a 2-simplex with
+(0, 1)-edge `f`, (1, 2)-edge `id` and (0, 2)-edge `g`. We use `Nonempty` to
+have a `Prop` valued `HomotopicL`.
+-/
+abbrev HomotopicL {X : Truncated 2} {x y : X _⦋0⦌₂} (f g : Edge x y) :=
+  Nonempty (CompStruct f (id y) g)
+
+/--
+Two edges `f` and `g` are right homotopic if there is a 2-simplex with
+(0, 1)-edge `id`, (1, 2)-edge `f`, and (0, 2)-edge `g`. We use `Nonempty` to
+have a `Prop` valued `HomotopicR`.
+-/
+abbrev HomotopicR {X : Truncated 2} {x y : X _⦋0⦌₂} (f g : Edge x y) :=
+  Nonempty (CompStruct (id x) f g)
+
+
+section left_homotopy_eqrel
+variable {X : Truncated 2} [Quasicategory₂ X]
+
+/--
+Left homotopy relation is reflexive
+-/
+def HomotopicL.refl {x y : X _⦋0⦌₂} {f : Edge x y} : HomotopicL f f
+  := ⟨compId f⟩
+
+/--
+Left homotopy relation is symmetric
+-/
+def HomotopicL.symm {x y : X _⦋0⦌₂} {f g : Edge x y} (hfg : HomotopicL f g) : HomotopicL g f := by
+  rcases hfg with ⟨hfg⟩
+  exact Quasicategory₂.fill31 hfg (idComp (id y)) (compId f)
+
+/--
+Left homotopy relation is transitive
+-/
+def HomotopicL.trans {x y : X _⦋0⦌₂} {f g h : Edge x y} (hfg : HomotopicL f g)
+    (hgh : HomotopicL g h) : HomotopicL f h := by
+  rcases hfg with ⟨hfg⟩
+  rcases hgh with ⟨hgh⟩
+  exact Quasicategory₂.fill32 hfg (idComp (id y)) hgh
+
+end left_homotopy_eqrel
+
+section right_homotopy_eqrel
+variable {X : Truncated 2} [Quasicategory₂ X]
+
+/--
+Right homotopy relation is reflexive
+-/
+def HomotopicR.refl {x y : X _⦋0⦌₂} {f : Edge x y} : HomotopicR f f := ⟨idComp f⟩
+
+/--
+Right homotopy relation is symmetric
+-/
+def HomotopicR.symm {x y : X _⦋0⦌₂} {f g : Edge x y} (hfg : HomotopicR f g) : HomotopicR g f := by
+  rcases hfg with ⟨hfg⟩
+  exact Quasicategory₂.fill32 (idComp (id x)) hfg (idComp f)
+
+/--
+Right homotopy relation is transitive
+-/
+def HomotopicR.trans {x y : X _⦋0⦌₂} {f g h : Edge x y} (hfg : HomotopicR f g)
+    (hgh : HomotopicR g h) : HomotopicR f h := by
+  rcases hfg with ⟨hfg⟩
+  rcases hgh with ⟨hgh⟩
+  exact Quasicategory₂.fill31 (idComp (id x)) hfg hgh
+
+end right_homotopy_eqrel
 
 end SSet.Truncated
