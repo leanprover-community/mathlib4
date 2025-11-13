e1a7d1ac04d3f69a251d737bdb14485ea1704a11
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2025-11-13 19:39:11 +0100

    copy over the copyright header to `ToAdditive`

--- Mathlib/Tactic/ToAdditive/ToAdditive.lean
+++ Mathlib/Tactic/ToAdditive/ToAdditive.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2017 Mario Carneiro. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Mario Carneiro, Yury Kudryashov, Floris van Doorn, Jon Eugster, Bryan Gin-ge Chen,
+Jovan Gerbscheid
+-/
 import Mathlib.Tactic.ToAdditive.Frontend
 
 namespace Mathlib.Tactic.ToAdditive
