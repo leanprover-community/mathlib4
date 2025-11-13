6f9cf55eaa4855cd0813df9607b4ddb9be2257f4
Attila Gáspár <58485900+gasparattila@users.noreply.github.com>
2025-11-13 14:47:55 +0100

    chore(Tactic/Measurability): `fun_prop` lemmas for solving `MeasurableSet {x | ...}` goals
    Currently, lemmas of the form `MeasurableSet {x | ...}` cannot be used by `measurability`, because `simp` changes the goal to `Measurable fun x => ...`. This PR adds the necessary `fun_prop` lemmas so that they work with `measurability` (except for statements involving equality, which are done in #31512). This fixes #26620.

--- Mathlib/MeasureTheory/Constructions/BorelSpace/Order.lean
+++ Mathlib/MeasureTheory/Constructions/BorelSpace/Order.lean
@@ -157,11 +157,13 @@ section PartialOrder
 
 variable [PartialOrder α] [OrderClosedTopology α] [SecondCountableTopology α] {a b : α}
 
-@[measurability]
 theorem measurableSet_le' : MeasurableSet { p : α × α | p.1 ≤ p.2 } :=
   OrderClosedTopology.isClosed_le'.measurableSet
 
-@[measurability]
+@[fun_prop]
+theorem measurable_le : Measurable fun p : α × α => p.1 ≤ p.2 :=
+  measurableSet_setOf.mp measurableSet_le'
+
 theorem measurableSet_le {f g : δ → α} (hf : Measurable f) (hg : Measurable g) :
     MeasurableSet { a | f a ≤ g a } :=
   hf.prodMk hg measurableSet_le'
@@ -219,11 +221,13 @@ instance nhdsWithin_Iio_isMeasurablyGenerated : (𝓝[Iio b] a).IsMeasurablyGene
 instance nhdsWithin_uIcc_isMeasurablyGenerated : IsMeasurablyGenerated (𝓝[[[a, b]]] x) :=
   nhdsWithin_Icc_isMeasurablyGenerated
 
-@[measurability]
 theorem measurableSet_lt' [SecondCountableTopology α] : MeasurableSet { p : α × α | p.1 < p.2 } :=
   (isOpen_lt continuous_fst continuous_snd).measurableSet
 
-@[measurability]
+@[fun_prop]
+theorem measurable_lt [SecondCountableTopology α] : Measurable fun p : α × α => p.1 < p.2 :=
+  measurableSet_setOf.mp measurableSet_lt'
+
 theorem measurableSet_lt [SecondCountableTopology α] {f g : δ → α} (hf : Measurable f)
     (hg : Measurable g) : MeasurableSet { a | f a < g a } :=
   hf.prodMk hg measurableSet_lt'
