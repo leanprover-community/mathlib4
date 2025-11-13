4a6bef8b5dd8247febf9dfbe544ddb8d8049e7d3
jlh18 <josephhua73@yahoo.com>
2025-11-13 10:36:21 -0500

    add author

--- Mathlib/CategoryTheory/Bicategory/Grothendieck.lean
+++ Mathlib/CategoryTheory/Bicategory/Grothendieck.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2024 Calle Sönne. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Calle Sönne
+Authors: Calle Sönne, Joseph Hua
 -/
 
 import Mathlib.CategoryTheory.Bicategory.LocallyDiscrete
