40ab9329409de37f0fa2ef2df1e5f28c38b984fb
thorimur <thomasmurrills@gmail.com>
2025-11-13 19:28:43 -0500

    chore: fix imports, close section

--- Mathlib/Lean/Message.lean
+++ Mathlib/Lean/Message.lean
@@ -3,7 +3,7 @@ Copyright (c) 2025 Thomas R. Murrills. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Thomas R. Murrills
 -/
-import Lean.Message
+import Mathlib.Init
 
 /-!
 # Additional utilities for working with `MessageData`
@@ -34,3 +34,5 @@ otherwise.
 -/
 def Lean.MessageData.withPlural (singular plural : MessageData) (count : Nat) : MessageData :=
   if count = 1 then singular else plural
+
+end WithPlural
