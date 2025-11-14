11596a8cae2e6a05c3408dc9bf82285f67ea2d42
Monica Omar <23701951+themathqueen@users.noreply.github.com>
2025-11-14 11:51:54 +0000

    chore(Algebra): move `QuadraticAlgebra` to `QuadraticAlgebra/Defs` (#31490)
    Since we already have a `QuadraticAlgebra` folder, this should go in there. It looks strange on the documentation otherwise.

--- Mathlib.lean
+++ Mathlib.lean
@@ -1084,8 +1084,8 @@ import Mathlib.Algebra.Polynomial.UnitTrinomial
 import Mathlib.Algebra.PresentedMonoid.Basic
 import Mathlib.Algebra.Prime.Defs
 import Mathlib.Algebra.Prime.Lemmas
-import Mathlib.Algebra.QuadraticAlgebra
 import Mathlib.Algebra.QuadraticAlgebra.Basic
+import Mathlib.Algebra.QuadraticAlgebra.Defs
 import Mathlib.Algebra.QuadraticDiscriminant
 import Mathlib.Algebra.Quandle
 import Mathlib.Algebra.Quaternion
