3d4b36ef411520b69c56a11703e7bfbd619a5ef3
Jovan Gerbscheid <56355248+JovanGerb@users.noreply.github.com>
2025-11-14 11:52:08 +0000

    chore(Tactic/Translate): rename the `ToAdditive` folder (#31612)
    Rename the folder `ToAdditive` to `Translate`. And rename the file `Frontend` to `Core` (since that now mostly contains the backend code for the translation attributes). This is because the folder now contains both the implementation of `to_additive` and `to_dual`, so `Translate` is a better description of its content.
    
    Instead of `Translate`, we could also call it e.g. `Translation`, `TranslateAttr` or `TranslationAttr`.

--- Mathlib/Tactic/ToAdditive.lean
+++ Mathlib/Tactic/ToAdditive.lean
@@ -3,7 +3,7 @@ Copyright (c) 2024 Miyahara Kō. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Miyahara Kō
 -/
-import Mathlib.Tactic.ToAdditive.ToAdditive
+import Mathlib.Tactic.Translate.ToAdditive
 
 /-!
 ## `@[to_additive]` attributes for basic types
