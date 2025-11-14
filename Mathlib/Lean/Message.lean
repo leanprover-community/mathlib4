fe476bedc85df8d9ae82a28406ab948134ed882c
thorimur <thomasmurrills@gmail.com>
2025-11-13 19:08:58 -0500

    feat: `withPlural`

--- Mathlib/Lean/Message.lean
+++ Mathlib/Lean/Message.lean
@@ -4,6 +4,33 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Floris van Doorn
 -/
 import Lean.Message
-import Mathlib.Tactic.Linter.DeprecatedModule
 
-deprecated_module (since := "2025-08-18")
+/-!
+# Additional utilities for working with `MessageData`
+-/
+
+section WithPlural
+
+/-!
+Thin wrappers around `if count = 1 then singular else plural` for Lean's different textual
+datatypes. This helps readability of source when constructing logged messages that require dynamic
+pluralization.
+-/
+
+/-- `"foo".withPlural "foos" count` returns `"foo"` when `count` is `1`, and `"foos"` otherwise. -/
+def String.withPlural (singular plural : String) (count : Nat) : String :=
+  if count = 1 then singular else plural
+
+/--
+`f!"foo".withPlural f!"foos" count` returns `f!"foo"` when `count` is `1`, and `f!"foos"`
+otherwise.
+-/
+def Std.Format.withPlural (singular plural : Format) (count : Nat) : Format :=
+  if count = 1 then singular else plural
+
+/--
+`m!"foo".withPlural m!"foos" count` returns `m!"foo"` when `count` is `1`, and `m!"foos"`
+otherwise.
+-/
+def Lean.MessageData.withPlural (singular plural : MessageData) (count : Nat) : MessageData :=
+  if count = 1 then singular else plural
