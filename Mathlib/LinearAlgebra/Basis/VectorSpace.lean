2091ba4f308ca25b0a85de61daff9dd8c7053de6
Moritz Doll <moritz.doll@googlemail.com>
2025-11-14 16:46:15 +1100

    one more golf

--- Mathlib/LinearAlgebra/Basis/VectorSpace.lean
+++ Mathlib/LinearAlgebra/Basis/VectorSpace.lean
@@ -262,20 +262,18 @@ def LinearMap.leftInverse (f : V →ₗ[K] V') : V' →ₗ[K] V :=
   (f.exists_leftInverse_of_injective h_inj).choose
   else 0
 
-@[simp]
 theorem LinearMap.leftInverse_comp_of_inj {f : V →ₗ[K] V'} (h_inj : LinearMap.ker f = ⊥) :
     f.leftInverse ∘ₗ f = LinearMap.id := by
   simpa [leftInverse, h_inj] using (f.exists_leftInverse_of_injective h_inj).choose_spec
 
 /-- If `f` is injective, then the left inverse composed with `f` is the identity. -/
-@[simp]
 theorem LinearMap.leftInverse_apply_of_inj {f : V →ₗ[K] V'} (h_inj : LinearMap.ker f = ⊥) (x : V) :
     f.leftInverse (f x) = x :=
   LinearMap.ext_iff.mp (f.leftInverse_comp_of_inj h_inj) x
 
 theorem Submodule.exists_isCompl (p : Submodule K V) : ∃ q : Submodule K V, IsCompl p q :=
-  let ⟨f, hf⟩ := p.subtype.exists_leftInverse_of_injective p.ker_subtype
-  ⟨LinearMap.ker f, LinearMap.isCompl_of_proj <| LinearMap.ext_iff.1 hf⟩
+  ⟨LinearMap.ker p.subtype.leftInverse,
+    LinearMap.isCompl_of_proj <| LinearMap.leftInverse_apply_of_inj p.ker_subtype⟩
 
 instance Submodule.complementedLattice : ComplementedLattice (Submodule K V) :=
   ⟨Submodule.exists_isCompl⟩
