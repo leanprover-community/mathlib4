50add1a9160cf1b1241f7f0f804588698a821f05
Frédéric Dupuis <dupuisf@iro.umontreal.ca>
2025-11-15 12:49:24 -0500

    fix it again...

--- Mathlib/Analysis/SpecialFunctions/ContinuousFunctionalCalculus/ExpLog.lean
+++ Mathlib/Analysis/SpecialFunctions/ContinuousFunctionalCalculus/ExpLog.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2024 Frédéric Dupuis. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Mario Carneiro, Kevin Kappelmann
+Authors: Frédéric Dupuis
 -/
 import Mathlib.Analysis.SpecialFunctions.ContinuousFunctionalCalculus.ExpLog.Basic
 
