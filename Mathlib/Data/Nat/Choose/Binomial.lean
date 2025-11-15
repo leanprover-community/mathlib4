5ba3f63067cefbab50fc461648a494c76cc9d979
Bolton Bailey <bolton.bailey@gmail.com>
2025-11-14 22:24:51 -0800

    inductive

--- Mathlib/Data/Nat/Choose/Binomial.lean
+++ Mathlib/Data/Nat/Choose/Binomial.lean
@@ -26,26 +26,55 @@ open scoped Nat
 
 namespace Nat
 
+def binom : ℕ → ℕ → ℕ
+  | _, 0 => 1
+  | 0, _ => 1
+  | a + 1, b + 1 => choose (a + 1) b + choose a (b + 1)
+
+
 -- TODO: uncomment
 -- variable (a b : ℕ)
 
-def binom (a b : ℕ) : ℕ := Nat.choose (a + b) a
+
+lemma binom_eq_choose (a b : ℕ) : binom a b = Nat.choose (a + b) a := by
+  sorry
 
 @[simp]
-lemma binom_eq_choose (a b : ℕ) : binom a b = Nat.choose (a + b) a := rfl
+lemma binom_zero_right (a : ℕ) : binom a 0 = 1 := by
+  simp [binom_eq_choose]
+
+@[simp]
+lemma binom_zero_left (b : ℕ) : binom 0 b = 1 := by
+  simp [binom_eq_choose]
+
+@[simp]
+lemma binom_one_right (a : ℕ) : binom a 1 = a + 1 := by
+  simp [binom_eq_choose]
+
+@[simp]
+lemma binom_one_left (b : ℕ) : binom 1 b = 1 + b := by
+  simp [binom_eq_choose]
+
+lemma binom_succ_succ (a b : ℕ) :
+    binom (succ a) (succ b) = binom a (succ b) + binom (succ a) b := by
+  simp [binom_eq_choose, Nat.choose_succ_succ]
+  sorry
+
+lemma binom_succ_succ' (a b : ℕ) :
+    binom (a + 1) (b + 1) = binom a (b + 1) + binom (a + 1) b := by
+  convert binom_succ_succ a b
 
 lemma binom_symm (a b : ℕ) : binom a b = binom b a := by
-  simpa [add_comm] using choose_symm_add
+  simpa [binom_eq_choose, add_comm] using choose_symm_add
 
-theorem binom_pos (a b : ℕ) : 0 < binom a b := Nat.choose_pos (le_add_right a b)
+theorem binom_pos (a b : ℕ) : 0 < binom a b := sorry
 
 theorem binom_eq_factorial_div_factorial (a b : ℕ) : binom a b =
-    (a + b)! / (a ! * b !) := by simp [choose_eq_factorial_div_factorial]
+    (a + b)! / (a ! * b !) := by simp [binom_eq_choose, choose_eq_factorial_div_factorial]
 
 theorem binom_eq_multinomial (a b : ℕ) :
     binom a b = Nat.multinomial (Finset.univ) ![a, b] := by
-  simp [multinomial_univ_two]
-  simp [choose_eq_factorial_div_factorial]
+  simp [multinomial_univ_two, binom_eq_choose, choose_eq_factorial_div_factorial]
 
 section PositivityExtension
 
