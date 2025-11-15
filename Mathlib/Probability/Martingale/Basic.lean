c32ec626861990f15b63f6159906ac065ba7703b
Etienne Marion <baguettes_casino0c@icloud.com>
2025-11-15 18:30:42 +0100

    fix

--- Mathlib/Probability/Martingale/Basic.lean
+++ Mathlib/Probability/Martingale/Basic.lean
@@ -3,6 +3,7 @@ Copyright (c) 2021 Rémy Degenne. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Rémy Degenne, Kexing Ying
 -/
+import Mathlib.MeasureTheory.Function.ConditionalExpectation.PullOut
 import Mathlib.Probability.Notation
 import Mathlib.Probability.Process.Stopping
 import Mathlib.Probability.Process.Predictable
