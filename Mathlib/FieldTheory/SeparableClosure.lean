e572c79f2274620cae18c8be8bfe1883534c9390
Junyan Xu <junyanxu.math@gmail.com>
2025-11-15 21:23:24 +0000

    fix Finset.toSet

--- Mathlib/FieldTheory/SeparableClosure.lean
+++ Mathlib/FieldTheory/SeparableClosure.lean
@@ -372,13 +372,13 @@ lemma FG.exists_finset_maximalFor_isTranscendenceBasis_separableClosure
     (Hfg : FG (F := F) (E := E) ⊤) :
     ∃ s : Finset E, MaximalFor (fun t : Set E ↦ IsTranscendenceBasis F ((↑) : t → E))
       (fun t ↦ (separableClosure (adjoin F t) E).restrictScalars F) s := by
-  let d (s : Finset E) := Field.finInsepDegree (adjoin F s.toSet) E
+  let d (s : Finset E) := Field.finInsepDegree (adjoin F (s : Set E)) E
   have Hexists : {s : Finset E | IsTranscendenceBasis F ((↑) : s → E)}.Nonempty := by
     have ⟨s, hs⟩ := Hfg
-    have : Algebra.IsAlgebraic (Algebra.adjoin F s.toSet) E := by
+    have : Algebra.IsAlgebraic (Algebra.adjoin F (s : Set E)) E := by
       rw [← isAlgebraic_adjoin_iff_top, hs, Algebra.isAlgebraic_iff_isIntegral]
       refine Algebra.isIntegral_of_surjective topEquiv.surjective
-    have ⟨t, hts, ht⟩ := exists_isTranscendenceBasis_subset (R := F) s.toSet
+    have ⟨t, hts, ht⟩ := exists_isTranscendenceBasis_subset (R := F) (s : Set E)
     lift t to Finset E using s.finite_toSet.subset hts
     exact ⟨t, ht⟩
   let s := d.argminOn _ Hexists
@@ -387,12 +387,12 @@ lemma FG.exists_finset_maximalFor_isTranscendenceBasis_separableClosure
   have : t.Finite := by
     simp [Set.Finite, ← Cardinal.mk_lt_aleph0_iff, ht.cardinalMk_eq hs, Cardinal.nat_lt_aleph0]
   lift t to Finset E using this
-  have : Module.Finite (adjoin F s.toSet) E := by
+  have : Module.Finite (adjoin F (s : Set E)) E := by
     apply (config := { allowSynthFailures := true }) finite_of_fg_of_isAlgebraic
     · exact .of_restrictScalars (K := F) (by rwa [restrictScalars_top])
     · convert hs.isAlgebraic_field <;> simp [s]
-  have : Module.Finite ((separableClosure (adjoin F s.toSet) E).restrictScalars F) E :=
-    inferInstanceAs <| Module.Finite (separableClosure (adjoin F s.toSet) E) E
+  have : Module.Finite ((separableClosure (adjoin F (s : Set E)) E).restrictScalars F) E :=
+    inferInstanceAs <| Module.Finite (separableClosure (adjoin F (s : Set E)) E) E
   exact d.not_lt_argminOn _ ht Hexists (by apply finrank_lt_of_lt H)
 
 @[simp]
