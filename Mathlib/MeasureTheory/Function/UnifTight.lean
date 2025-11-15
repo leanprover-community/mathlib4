e3f63ff5f0948a6abcbe3abb0450fe82fd31551d
Harald Husum <harald.husum@gmail.com>
2025-11-15 01:45:29 +0000

    doc(MeasureTheory): ensure only a single H1 header per file (#31618)
    This PR ensures we only have a single H1 header per lean file in the`MeasureTheory` subdirectory. We do this for the following reasons:
    
    - Having more than one H1 header per file is likely to hamper both assistive technologies and SEO, since it introduces ambiguity about what the title of the resulting documentation webpage actually is.
    - The [documentation style guide](https://leanprover-community.github.io/contribute/doc.html) asks for the title of the file to be H1, any other header in the file-level docstring to be H2, and sectioning headers to be H3.
    
    I have used my own judgement to decide whether to demote the extra H1 headers to H2 or H3. I ask reviewers to verify that my choices makes sense to them.
    
    Co-authored-by: Michael Rothgang <rothgang@math.uni-bonn.de>

--- Mathlib/MeasureTheory/Function/UnifTight.lean
+++ Mathlib/MeasureTheory/Function/UnifTight.lean
@@ -21,7 +21,7 @@ is also proved later in the file.
   exists some measurable set `s` with finite measure such that the Lp-norm of
   `f i` restricted to `sᶜ` is smaller than `ε` for all `i`.
 
-# Main results
+## Main results
 
 * `MeasureTheory.unifTight_finite`: a finite sequence of Lp functions is uniformly
   tight.
