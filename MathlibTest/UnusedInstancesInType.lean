bdcb420da45aa8d469ac5e657cdbcc239ed985fd
thorimur <thomasmurrills@gmail.com>
2025-11-16 15:45:13 -0500

    chore: more testing

--- MathlibTest/UnusedInstancesInType.lean
+++ MathlibTest/UnusedInstancesInType.lean
@@ -1,6 +1,10 @@
 import Mathlib.Tactic.Linter.UnusedInstancesInType
 
+section decidable
+
 set_option linter.unusedDecidable true
+
+section unused
 
 /--
 warning: `foo` has the hypothesis `[DecidableEq α]` (#2) which is not used in the remainder of the type.
@@ -21,25 +25,73 @@ warning: unused variable `x`
 
 Note: This linter can be disabled with `set_option linter.unusedVariables false`
 ---
-warning: `foo₂` has the hypothesis `[(m : Nat) → Decidable (m ≠ 0)]` (#2) which is not used in the remainder of the type.
+warning: `foo₂` has the hypothesis `[(α : Type) → Decidable (Nonempty α)]` (#2) which is not used in the remainder of the type.
 Consider removing these hypotheses and using `classical` in the proof instead. For terms, consider using `open Scoped classical in` at the term level (not the command level).
 
 Note: This linter can be disabled with `set_option linter.unusedDecidable false`
 -/
 #guard_msgs in
-theorem foo₂ (a : Type) [∀ m : Nat, Decidable (m ≠ 0)] (x : Unit) [Nonempty a] : True := trivial
+theorem foo₂ (a : Type) [∀ α : Type, Decidable (Nonempty α)] (x : Unit) [Nonempty a] : True :=
+  trivial
 
 -- TODO: why the newline + indentation in the pretty-printing of the forall?
 /--
-warning: `foo₃` has the hypotheses `[(m : Nat) →
-  Decidable (m ≠ 0)]` (#2) and `[DecidableEq β]` (#3) which are not used in the remainder of the type.
+warning: `foo₃` has the hypotheses `[(α : Type) →
+  Decidable (Nonempty α)]` (#2) and `[DecidableEq β]` (#3) which are not used in the remainder of the type.
 Consider removing these hypotheses and using `classical` in the proof instead. For terms, consider using `open Scoped classical in` at the term level (not the command level).
 
 Note: This linter can be disabled with `set_option linter.unusedDecidable false`
 -/
 #guard_msgs in
-theorem foo₃ {β} [∀ m : Nat, Decidable (m ≠ 0)] [DecidableEq β] : True := trivial
+theorem foo₃ {β} [∀ α : Type, Decidable (Nonempty α)] [DecidableEq β] : True := trivial
 
+-- See through `let`, don't count it as an index
+/--
+warning: `foo₄` has the hypothesis `[DecidableEq β]` (#2) which is not used in the remainder of the type.
+Consider removing these hypotheses and using `classical` in the proof instead. For terms, consider using `open Scoped classical in` at the term level (not the command level).
+
+Note: This linter can be disabled with `set_option linter.unusedDecidable false`
+-/
+#guard_msgs in
+theorem foo₄ {β} : let _ := 2; ∀ [DecidableEq β], True := trivial
+
+-- Linter should not fire when `sorry` appears in the type, even though the instances are unused
 /-- warning: declaration uses 'sorry' -/
 #guard_msgs in
-theorem fooSorry {β} [∀ m : Nat, Decidable (m ≠ 0)] [DecidableEq β] (b : sorry) : True := trivial
+theorem fooSorry {β} [∀ α : Type, Decidable (Nonempty α)] [DecidableEq β] (b : sorry) : True :=
+  trivial
+
+end unused
+
+section used
+
+/- The linter either should not fire on these declarations because the instance hypotheses are used
+in the type, or not fire on *every* instance in these declarations. -/
+
+def Uses (α : Sort u) (_ : α := by infer_instance) : Prop := True
+
+theorem fooUsing [DecidableEq (Nat → Nat)] : Uses (DecidableEq (Nat → Nat)) := trivial
+
+theorem fooUsing₁ [DecidableEq (Nat → Nat)] : Uses (DecidableEq (Nat → Nat)) → True :=
+  fun _ =>  trivial
+
+-- Should fire on parameter #1 but not parameter #2
+/--
+warning: `fooUsing₂` has the hypothesis `[DecidablePred Nonempty]` (#1) which is not used in the remainder of the type.
+Consider removing these hypotheses and using `classical` in the proof instead. For terms, consider using `open Scoped classical in` at the term level (not the command level).
+
+Note: This linter can be disabled with `set_option linter.unusedDecidable false`
+-/
+#guard_msgs in
+theorem fooUsing₂ [DecidablePred Nonempty] [DecidableEq (Nat → Nat)] :
+    Uses (DecidableEq (Nat → Nat)) → True :=
+  fun _ =>  trivial
+
+-- Note `optParam` test
+theorem fooUsing₃ [DecidablePred Nonempty] [DecidableEq (Nat → Nat)]
+    (_ : Uses (DecidablePred Nonempty) := trivial) : Uses (DecidableEq (Nat → Nat)) → True :=
+  fun _ =>  trivial
+
+end used
+
+end decidable
