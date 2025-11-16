1461a5457dca17ea797123988a7f5ddb4dd4d970
thorimur <thomasmurrills@gmail.com>
2025-11-16 15:24:58 -0500

    chore: Mathlib.Init, API

--- Mathlib/Lean/Environment.lean
+++ Mathlib/Lean/Environment.lean
@@ -3,15 +3,46 @@ Copyright (c) 2025 Thomas R. Murrills. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Thomas R. Murrills
 -/
+import Mathlib.Init
 import Lean.Environment
+
+namespace Lean.Environment
+
+section constKind
+
+/- The following declarations account for the fact that the `ConstantKind` of a declaration is
+accessible when getting its `ConstantVal`, but is not recorded in said `ConstantVal`. -/
+
+/--
+Like `findConstVal?`, but also returns the declarations `ConstantKind`, which is known immediately.
+
+Blocks on everything but the constant's body (if any), which is not accessible through the result.
+-/
+def findConstValWithKind? (env : Environment) (decl : Name) (skipRealize := false) :
+    Option (ConstantVal × ConstantKind) := do
+  let info ← env.findAsync? decl skipRealize
+  return (info.toConstantVal, info.kind)
+
+/--
+Like `findConstVal?`, but only finds the `ConstantVal` for `decl` in `env` if its kind satisfies
+`p`. Otherwise, returns `none`.
+
+Blocks on everything but the constant's body (if any), which is not accessible through the result.
+-/
+def findConstValOfKind? (env : Environment) (p : ConstantKind → Bool) (decl : Name)
+    (skipRealize := false) : Option ConstantVal := do
+  let info ← env.findAsync? decl skipRealize
+  if p info.kind then info.toConstantVal else none
 
 /--
 Like `findConstVal?`, but only finds the `ConstantVal` for `decl` in `env` if it is a theorem.
 
 Blocks on everything but the constant's body (if any), which is not accessible through the result.
 -/
-def Lean.Environment.findTheoremConstVal? (env : Environment) (decl : Name)
+def findTheoremConstVal? (env : Environment) (decl : Name)
     (skipRealize := false) : Option ConstantVal := do
-  -- Note: if upstreamed , we could check the (private) base first, then use `findAsyncCore?`
-  let info ← env.findAsync? decl skipRealize
-  if info.kind matches .thm then info.toConstantVal else none
+  env.findConstValOfKind? (· matches .thm) decl skipRealize
+
+end constKind
+
+end Lean.Environment
