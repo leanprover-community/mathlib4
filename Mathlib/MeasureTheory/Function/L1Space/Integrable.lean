efa66e6bb430dfbed6dcb3d80cfa91324186fe64
Etienne Marion <baguettes_casino0c@icloud.com>
2025-11-16 08:49:50 +0100

    revert some changes

--- Mathlib/MeasureTheory/Function/L1Space/Integrable.lean
+++ Mathlib/MeasureTheory/Function/L1Space/Integrable.lean
@@ -683,43 +683,39 @@ lemma Integrable.measure_norm_gt_lt_top {f : α → β} (hf : Integrable f μ) {
     μ {x | ε < ‖f x‖} < ∞ :=
   lt_of_le_of_lt (measure_mono (fun _ h ↦ (Set.mem_setOf_eq ▸ h).le)) (hf.measure_norm_ge_lt_top hε)
 
-section Order
-
-variable [Lattice β] [HasSolidNorm β] [IsOrderedAddMonoid β] {f : α → β}
-
 -- TODO: try generalising all lemmas below to enorm classes
 
-/-- If `f` is integrable, then for any `c > 0` the set `{x | f x ≥ c}` has finite measure. -/
-lemma Integrable.measure_ge_lt_top (hf : Integrable f μ) {ε : β} (ε_pos : 0 < ε) :
+/-- If `f` is `ℝ`-valued and integrable, then for any `c > 0` the set `{x | f x ≥ c}` has finite
+measure. -/
+lemma Integrable.measure_ge_lt_top {f : α → ℝ} (hf : Integrable f μ) {ε : ℝ} (ε_pos : 0 < ε) :
     μ {a : α | ε ≤ f a} < ∞ := by
-  refine lt_of_le_of_lt (measure_mono ?_) (hf.measure_norm_ge_lt_top (norm_pos_iff.2 ε_pos.ne'))
+  refine lt_of_le_of_lt (measure_mono ?_) (hf.measure_norm_ge_lt_top ε_pos)
   intro x hx
-  apply norm_le_norm_of_abs_le_abs
-  rw [abs_of_nonneg ε_pos.le]
+  simp only [Real.norm_eq_abs, Set.mem_setOf_eq] at hx ⊢
   exact hx.trans (le_abs_self _)
 
-/-- If `f` is integrable, then for any `c < 0` the set `{x | f x ≤ c}` has finite measure. -/
-lemma Integrable.measure_le_lt_top (hf : Integrable f μ) {c : β} (c_neg : c < 0) :
+/-- If `f` is `ℝ`-valued and integrable, then for any `c < 0` the set `{x | f x ≤ c}` has finite
+measure. -/
+lemma Integrable.measure_le_lt_top {f : α → ℝ} (hf : Integrable f μ) {c : ℝ} (c_neg : c < 0) :
     μ {a : α | f a ≤ c} < ∞ := by
-  refine lt_of_le_of_lt (measure_mono ?_) (hf.measure_norm_ge_lt_top (norm_pos_iff.2 c_neg.ne))
+  refine lt_of_le_of_lt (measure_mono ?_) (hf.measure_norm_ge_lt_top (show 0 < -c by linarith))
   intro x hx
-  apply norm_le_norm_of_abs_le_abs
-  rw [abs_of_nonpos c_neg.le, abs_of_nonpos (hx.trans c_neg.le)]
-  exact neg_le_neg hx
+  simp only [Real.norm_eq_abs, Set.mem_setOf_eq] at hx ⊢
+  exact (show -c ≤ - f x by linarith).trans (neg_le_abs _)
 
-/-- If `f` is integrable, then for any `c > 0` the set `{x | f x > c}` has finite measure. -/
-lemma Integrable.measure_gt_lt_top (hf : Integrable f μ) {ε : β} (ε_pos : 0 < ε) :
+/-- If `f` is `ℝ`-valued and integrable, then for any `c > 0` the set `{x | f x > c}` has finite
+measure. -/
+lemma Integrable.measure_gt_lt_top {f : α → ℝ} (hf : Integrable f μ) {ε : ℝ} (ε_pos : 0 < ε) :
     μ {a : α | ε < f a} < ∞ :=
   lt_of_le_of_lt (measure_mono (fun _ hx ↦ (Set.mem_setOf_eq ▸ hx).le))
     (Integrable.measure_ge_lt_top hf ε_pos)
 
-/-- If `f` is integrable, then for any `c < 0` the set `{x | f x < c}` has finite measure. -/
-lemma Integrable.measure_lt_lt_top {f : α → β} (hf : Integrable f μ) {c : β} (c_neg : c < 0) :
+/-- If `f` is `ℝ`-valued and integrable, then for any `c < 0` the set `{x | f x < c}` has finite
+measure. -/
+lemma Integrable.measure_lt_lt_top {f : α → ℝ} (hf : Integrable f μ) {c : ℝ} (c_neg : c < 0) :
     μ {a : α | f a < c} < ∞ :=
   lt_of_le_of_lt (measure_mono (fun _ hx ↦ (Set.mem_setOf_eq ▸ hx).le))
     (Integrable.measure_le_lt_top hf c_neg)
-
-end Order
 
 theorem LipschitzWith.integrable_comp_iff_of_antilipschitz {K K'} {f : α → β} {g : β → γ}
     (hg : LipschitzWith K g) (hg' : AntilipschitzWith K' g) (g0 : g 0 = 0) :
