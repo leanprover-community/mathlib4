5316a94c04c0d3a171499e1c630d7b2eb0552e50
Vasilii Nesterov <vas.nesterov63@gmail.com>
2025-11-17 00:01:38 +0300

    swap imports

--- Counterexamples/PeanoCurve.lean
+++ Counterexamples/PeanoCurve.lean
@@ -3,8 +3,8 @@ Copyright (c) 2025 Vasilii Nesterov. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Vasilii Nesterov
 -/
-import Mathlib.Topology.MetricSpace.HausdorffAlexandroff
 import Mathlib.Analysis.Complex.Tietze
+import Mathlib.Topology.MetricSpace.HausdorffAlexandroff
 
 /-!
 # Peano curve
