05b56fc2e7636dc6c602264f6df740bd9d0b99c6
Joseph Myers <10537793+jsm28@users.noreply.github.com>
2025-11-16 17:26:30 +0000

    feat(LinearAlgebra/AffineSpace/Combination): `affineCombination_mem_affineSpan_image` (#31204)
    Add a lemma giving a condition for an affine combination to lie in the affine span of a subset of the indexed family of points (the reverse direction of `eq_affineCombination_of_mem_affineSpan_image`, which is the only current `image` lemma in this file, roughly).

--- Mathlib/LinearAlgebra/AffineSpace/Combination.lean
+++ Mathlib/LinearAlgebra/AffineSpace/Combination.lean
@@ -865,6 +865,25 @@ lemma eq_affineCombination_of_mem_affineSpan_image {p₁ : P} {p : ι → P} {s 
   simp only [Finset.affineCombination_map, Function.Embedding.coe_subtype]
   exact fs'.affineCombination_congr (by simp) (by simp)
 
+lemma affineCombination_mem_affineSpan_image [Nontrivial k] {s : Finset ι} {w : ι → k}
+    (h : ∑ i ∈ s, w i = 1) {s' : Set ι} (hs' : ∀ i ∈ s, i ∉ s' → w i = 0) (p : ι → P) :
+    s.affineCombination k p w ∈ affineSpan k (p '' s') := by
+  classical
+  rw [Set.image_eq_range]
+  let w' : s' → k := fun i ↦ w i
+  have h' : ∑ i ∈ s with i ∈ s', w i = 1 := by
+    rw [← h, ← Finset.sum_sdiff (s₁ := {x ∈ s | x ∈ s'}) (s₂ := s) (by simp), right_eq_add]
+    refine Finset.sum_eq_zero ?_
+    intro i hi
+    simp only [Finset.mem_sdiff, Finset.mem_filter, not_and] at hi
+    exact hs' i hi.1 (hi.2 hi.1)
+  rw [← Finset.sum_subtype_eq_sum_filter] at h'
+  convert affineCombination_mem_affineSpan h' (fun x ↦ p x)
+  rw [Finset.affineCombination_subtype_eq_filter, Finset.affineCombination_indicator_subset w p
+    (Finset.filter_subset _ _)]
+  refine Finset.affineCombination_congr _ (fun i hi ↦ ?_) (fun _ _ ↦ rfl)
+  simp_all [Set.indicator_apply]
+
 variable (k V)
 
 /-- A point is in the `affineSpan` of an indexed family if and only
