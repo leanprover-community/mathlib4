bf45447bd2250fdefc94c53c5050c6e635d285d8
thorimur <thomasmurrills@gmail.com>
2025-11-16 15:24:58 -0500

    chore: Mathlib.Init, API

--- Mathlib/Lean/Expr.lean
+++ Mathlib/Lean/Expr.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro, Simon Hudon, Kim Morrison, Keeley Hoek, Robert Y. Lewis, Floris van Doorn,
 Thomas R. Murrills
 -/
+import Mathlib.Init
 import Mathlib.Lean.Expr.Basic
 
 /-!
