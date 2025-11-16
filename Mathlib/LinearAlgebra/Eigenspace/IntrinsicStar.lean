0e7ad9be58e1d2de54928bec1f0999ea2ee78d01
Monica Omar <2497250a@research.gla.ac.uk>
2025-11-16 06:46:24 +0000

    update

--- Mathlib/LinearAlgebra/Eigenspace/IntrinsicStar.lean
+++ Mathlib/LinearAlgebra/Eigenspace/IntrinsicStar.lean
@@ -13,17 +13,17 @@ import Mathlib.LinearAlgebra.Eigenspace.Basic
 -/
 
 namespace Module.End
-variable {R V : Type*} [CommRing R] [InvolutiveStar R]
-  [AddCommGroup V] [StarAddMonoid V] [Module R V] [StarModule R V]
+variable {R V : Type*} [InvolutiveStar R] [AddCommGroup V] [StarAddMonoid V]
 
 open scoped IntrinsicStar
 open LinearMap
 
-theorem mem_eigenspace_intrinsicStar_iff (f : End R V) (α : R) (x : V) :
+theorem mem_eigenspace_intrinsicStar_iff [CommRing R] [Module R V] [StarModule R V]
+    (f : End R V) (α : R) (x : V) :
     x ∈ (star f).eigenspace α ↔ star x ∈ f.eigenspace (star α) := by
   simp_rw [mem_eigenspace_iff, intrinsicStar_apply, star_eq_iff_star_eq, star_smul, eq_comm]
 
-theorem spectrum_intrinsicStar (f : End R V) :
+theorem spectrum_intrinsicStar [CommSemiring R] [Module R V] [StarModule R V] (f : End R V) :
     spectrum R (star f) = star (spectrum R f) := by
   ext x
   simp_rw [Set.mem_star, spectrum.mem_iff, not_iff_not, Algebra.algebraMap_eq_smul_one]
