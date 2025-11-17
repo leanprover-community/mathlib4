84216a22e23ba14c563b6686a64a460044ae7ca9
thorimur <68410468+thorimur@users.noreply.github.com>
2025-11-17 12:02:07 -0500

    chore: revert `withPlural` dependency

--- Mathlib/Lean/Message.lean
+++ Mathlib/Lean/Message.lean
@@ -1,38 +1,9 @@
 /-
-Copyright (c) 2025 Thomas R. Murrills. All rights reserved.
+Copyright (c) 2022 Floris van Doorn. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Thomas R. Murrills
+Authors: Floris van Doorn
 -/
-import Mathlib.Init
+import Lean.Message
+import Mathlib.Tactic.Linter.DeprecatedModule
 
-/-!
-# Additional utilities for working with `MessageData`
--/
-
-section WithPlural
-
-/-!
-Thin wrappers around `if count = 1 then singular else plural` for Lean's different textual
-datatypes. This helps readability of source when constructing logged messages that require dynamic
-pluralization.
--/
-
-/-- `"foo".withPlural "foos" count` returns `"foo"` when `count` is `1`, and `"foos"` otherwise. -/
-def String.withPlural (singular plural : String) (count : Nat) : String :=
-  if count = 1 then singular else plural
-
-/--
-`f!"foo".withPlural f!"foos" count` returns `f!"foo"` when `count` is `1`, and `f!"foos"`
-otherwise.
--/
-def Std.Format.withPlural (singular plural : Format) (count : Nat) : Format :=
-  if count = 1 then singular else plural
-
-/--
-`m!"foo".withPlural m!"foos" count` returns `m!"foo"` when `count` is `1`, and `m!"foos"`
-otherwise.
--/
-def Lean.MessageData.withPlural (singular plural : MessageData) (count : Nat) : MessageData :=
-  if count = 1 then singular else plural
-
-end WithPlural
+deprecated_module (since := "2025-08-18")
