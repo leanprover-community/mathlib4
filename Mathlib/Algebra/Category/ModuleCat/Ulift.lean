bb63fc73ecac2a026f802305ffdbddd57ddcea67
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2025-11-17 13:08:12 +0800

    add preserve

--- Mathlib/Algebra/Category/ModuleCat/Ulift.lean
+++ Mathlib/Algebra/Category/ModuleCat/Ulift.lean
@@ -3,10 +3,11 @@ Copyright (c) 2025 Nailin Guan. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Nailin Guan
 -/
-import Mathlib.Algebra.Category.ModuleCat.Basic
-import Mathlib.Algebra.Homology.ShortComplex.ExactFunctor
+import Mathlib.Algebra.Category.ModuleCat.Injective
+import Mathlib.Algebra.Category.ModuleCat.Projective
 import Mathlib.Algebra.Homology.ShortComplex.ModuleCat
-import Mathlib.Algebra.Module.ULift
+import Mathlib.CategoryTheory.Preadditive.Injective.Preserves
+import Mathlib.CategoryTheory.Preadditive.Projective.Preserves
 /-!
 
 # Ulift Functor of ModuleCat
@@ -63,5 +64,16 @@ instance : Limits.PreservesFiniteColimits (uliftFunctor.{u, v, v'} R) := by
   have := ((CategoryTheory.Functor.exact_tfae (uliftFunctor.{u, v, v'} R)).out 1 3).mp
     (uliftFunctor_map_exact R)
   exact this.2
+
+instance [Small.{v} R] : (uliftFunctor.{u, v, v'} R).PreservesProjectiveObjects where
+  projective_obj {M} proj := by
+    let _ : Small.{max v v', u} R := small_lift R
+    let _ := (IsProjective.iff_projective M).mpr proj
+    rw [← IsProjective.iff_projective]
+    exact Module.Projective.of_equiv ULift.moduleEquiv.symm
+
+instance [Small.{v} R] : (uliftFunctor.{u, v, v'} R).PreservesInjectiveObjects where
+  injective_obj {M} inj := (Module.injective_iff_injective_object R _).mp
+    (Module.ulift_injective_of_injective R ((Module.injective_iff_injective_object R M).mpr inj))
 
 end ModuleCat
