21ad3a1735b496175ae22811107eaa7d39ae95af
Joël Riou <37772949+joelriou@users.noreply.github.com>
2025-11-17 00:03:43 +0000

    feat(CategoryTheory): dense functors (#29565)
    A functor `F : C ⥤ D` is dense if `𝟭 D` is a pointwise left Kan extension of `F` along itself, i.e. any `Y : D` is the
    colimit of all `F.obj X` for all morphisms `F.obj X ⟶ Y` (which is the condition `F.DenseAt Y`). When `F` is full, we show that this is equivalent to saying that the restricted Yoneda functor `D ⥤ Cᵒᵖ ⥤ Type _` is fully faithful. We also show that the range of a dense functor is a strong generator.

--- Mathlib.lean
+++ Mathlib.lean
@@ -2363,6 +2363,7 @@ import Mathlib.CategoryTheory.Functor.Functorial
 import Mathlib.CategoryTheory.Functor.Hom
 import Mathlib.CategoryTheory.Functor.KanExtension.Adjunction
 import Mathlib.CategoryTheory.Functor.KanExtension.Basic
+import Mathlib.CategoryTheory.Functor.KanExtension.Dense
 import Mathlib.CategoryTheory.Functor.KanExtension.DenseAt
 import Mathlib.CategoryTheory.Functor.KanExtension.Pointwise
 import Mathlib.CategoryTheory.Functor.KanExtension.Preserves
