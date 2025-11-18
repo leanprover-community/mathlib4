a95a2e7f635c393e497a6ef10dfbf09977e5be9f
Calle Sönne <calle.sonne@gmail.com>
2025-11-18 16:32:32 +0000

    long line fix

--- Mathlib/CategoryTheory/ConcreteCategory/UnbundledHom.lean
+++ Mathlib/CategoryTheory/ConcreteCategory/UnbundledHom.lean
@@ -22,8 +22,8 @@ namespace CategoryTheory
 /-- A class for unbundled homs used to define a category. `hom` must
 take two types `α`, `β` and instances of the corresponding structures,
 and return a predicate on `α → β`. -/
-@[deprecated "The prefered method for talking about concrete categories is to implement the category \
-manually and then provide the `ConcreteCategory` instance on top of this. See \
+@[deprecated "The prefered method for talking about concrete categories is to implement the \
+category manually and then provide the `ConcreteCategory` instance on top of this. See \
 `ConcreteCategory/Basic.lean`" (since := "2025-11-17")]
 class UnbundledHom {c : Type u → Type u} (hom : ∀ ⦃α β⦄, c α → c β → (α → β) → Prop) : Prop where
   hom_id : ∀ {α} (ia : c α), hom ia ia id
