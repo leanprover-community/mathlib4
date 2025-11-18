809bf76c7417fea3e98b70d06cc89d95f7fe4b3f
Kim Morrison <477956+kim-em@users.noreply.github.com>
2025-11-18 12:49:22 +0000

    feat: lemmas for paths-up-to-homotopy (#31574)
    We introduce `Path.Homotopic.Quotient.mk` as the normal form (rather than `_root_.Quotient.mk`) for constructing a path-up-to-homotopy from a path. This enables us to work more easily in the quotient. We add simp lemmas corresponding to the basic homotopies relating `refl` / `symm` / `trans` / `cast`.

--- Mathlib/AlgebraicTopology/FundamentalGroupoid/InducedMaps.lean
+++ Mathlib/AlgebraicTopology/FundamentalGroupoid/InducedMaps.lean
@@ -84,8 +84,10 @@ include hfg
 /-- If `f(p(t) = g(q(t))` for two paths `p` and `q`, then the induced path homotopy classes
 `f(p)` and `g(p)` are the same as well, despite having a priori different types -/
 theorem heq_path_of_eq_image :
-    (πₘ (TopCat.ofHom f)).map ⟦p⟧ ≍ (πₘ (TopCat.ofHom g)).map ⟦q⟧ := by
-  simp only [map_eq, ← Path.Homotopic.map_lift]; apply Path.Homotopic.hpath_hext; exact hfg
+    (πₘ (TopCat.ofHom f)).map ⟦p⟧ ≍ (πₘ (TopCat.ofHom g)).map (Path.Homotopic.Quotient.mk q) := by
+  simp only [map_eq, ← Path.Homotopic.Quotient.mk_map]
+  apply Path.Homotopic.hpath_hext
+  exact hfg
 
 private theorem start_path : f x₀ = g x₂ := by convert hfg 0 <;> simp only [Path.source]
 
