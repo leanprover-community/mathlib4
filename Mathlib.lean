efceda65f89a6fbd0db3380deb351160af9d5b20
Andrew Yang <36414270+erdOne@users.noreply.github.com>
2025-11-18 09:58:01 +0000

    feat(RingTheory): coeffs of a poly is integral if it divides an integral poly (#31360)

--- Mathlib.lean
+++ Mathlib.lean
@@ -5996,6 +5996,7 @@ import Mathlib.RingTheory.Polynomial.HilbertPoly
 import Mathlib.RingTheory.Polynomial.Ideal
 import Mathlib.RingTheory.Polynomial.IntegralNormalization
 import Mathlib.RingTheory.Polynomial.IrreducibleRing
+import Mathlib.RingTheory.Polynomial.IsIntegral
 import Mathlib.RingTheory.Polynomial.Nilpotent
 import Mathlib.RingTheory.Polynomial.Opposites
 import Mathlib.RingTheory.Polynomial.Pochhammer
