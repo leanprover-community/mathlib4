b1fcfba9e93d181123a65ef8c17945caab7e5ed1
Moritz Doll <moritz.doll@googlemail.com>
2025-11-19 19:47:03 +1100

    fixes

--- Mathlib/Analysis/Calculus/BumpFunction/SmoothApproxLp.lean
+++ Mathlib/Analysis/Calculus/BumpFunction/SmoothApproxLp.lean
@@ -28,7 +28,7 @@ namespace HasCompactSupport
 
 variable [NormedAddCommGroup E] [NormedSpace ℝ E] [FiniteDimensional ℝ E] [BorelSpace E]
   [NormedSpace ℝ F]
-  (μ : Measure E := by volume_tac) [IsFiniteMeasureOnCompacts μ] [μ.IsOpenPosMeasure]
+  (μ : Measure E := by volume_tac) [IsFiniteMeasureOnCompacts μ]
 
 /-- For every continuous compactly supported function `f`there exists a smooth compactly supported
 function `g` such that `f - g` is arbitrary small in the `Lp`-norm for `p < ∞`. -/
@@ -43,8 +43,8 @@ theorem exist_eLpNorm_sub_le_of_continuous {p : ℝ≥0∞} (hp : p ≠ ⊤) {ε
   have hs₁ : μ (tsupport f) ≠ ⊤ := h₁.measure_lt_top.ne
   have hs₂ : 0 < (μ <| tsupport f).toReal := by
     -- Since `f` is not the zero function `tsupport f` has positive measure
-    rw [← measure_support_eq_zero_iff _] at hf
-    exact toReal_pos (pos_mono (subset_tsupport f) (pos_of_ne_zero hf)).ne' hs₁'
+    rw [← Measure.measure_support_eq_zero_iff _] at hf
+    exact toReal_pos (pos_mono (subset_tsupport f) (pos_of_ne_zero hf)).ne' hs₁
   set ε' := ε * (μ <| tsupport f).toReal ^ (-(1 / p.toReal)) with ε'_def
   have hε' : 0 < ε' := by positivity
   have hε₂ : ENNReal.ofReal ε' * μ (tsupport f) ^ (1 / p.toReal) ≤ ENNReal.ofReal ε := by
@@ -53,7 +53,7 @@ theorem exist_eLpNorm_sub_le_of_continuous {p : ℝ≥0∞} (hp : p ≠ ⊤) {ε
         Real.rpow_zero, mul_one]
   obtain ⟨g, hg₁, hg₂, hg₃⟩ := h₂.exists_contDiff_approx ⊤ (ε := fun _ ↦ ε') (by fun_prop)
     (by intro; positivity)
-  refine ⟨g, h₁.mono hg₃, hg₁, (eLpNorm_sub_le_of_dist_bdd' μ hp hε'.le ?_ h₁.measurableSet
+  refine ⟨g, h₁.mono hg₃, hg₁, (eLpNorm_sub_le_of_dist_bdd' μ hp h₁.measurableSet hε'.le ?_
     (subset_tsupport f) (hg₃.trans (subset_tsupport f))).trans hε₂⟩
   intro x
   rw [dist_comm]
@@ -65,7 +65,7 @@ namespace MeasureTheory.MemLp
 
 variable [NormedAddCommGroup E] [NormedSpace ℝ E] [FiniteDimensional ℝ E] [BorelSpace E]
   [NormedSpace ℝ F]
-  {μ : Measure E} [IsFiniteMeasureOnCompacts μ] [μ.IsOpenPosMeasure]
+  {μ : Measure E} [IsFiniteMeasureOnCompacts μ]
 
 /-- Every `Lp` function can be approximated by a smooth compactly supported function provided that
 `p < ∞`. -/
