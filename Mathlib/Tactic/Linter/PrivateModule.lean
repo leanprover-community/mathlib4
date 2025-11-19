b0e8728bd0b552edd238261db02d6d15ec4c2eb2
thorimur <thomasmurrills@gmail.com>
2025-11-19 14:07:37 -0500

    chore: copyright header

--- Mathlib/Tactic/Linter/PrivateModule.lean
+++ Mathlib/Tactic/Linter/PrivateModule.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2025 Thomas R. Murrills. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Thomas R. Murrills
+-/
 module
 
 public import Mathlib.Init
