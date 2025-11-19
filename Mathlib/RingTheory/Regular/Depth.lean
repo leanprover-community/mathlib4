7abc97471155df7009181c914d23aec6465a1ea9
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2025-11-19 17:11:59 +0800

    fix empty line

--- Mathlib/RingTheory/Regular/Depth.lean
+++ Mathlib/RingTheory/Regular/Depth.lean
@@ -3,7 +3,6 @@ Copyright (c) 2025 Nailin Guan. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Nailin Guan, Yi Song
 -/
-
 module
 
 public import Mathlib.Algebra.Category.Grp.Zero
