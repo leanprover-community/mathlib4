cb4a701cdda65813e4a59df89f42a526a867ac1b
Dagur Asgeirsson <dagurtomas@gmail.com>
2025-11-19 09:09:53 -0700

    Apply suggestions from code review

--- Mathlib/Topology/Category/CompHausLike/Cartesian.lean
+++ Mathlib/Topology/Category/CompHausLike/Cartesian.lean
@@ -3,8 +3,10 @@ Copyright (c) 2025 Dagur Asgeirsson. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Dagur Asgeirsson
 -/
-import Mathlib.CategoryTheory.Monoidal.Cartesian.Basic
-import Mathlib.Topology.Category.CompHausLike.Limits
+module
+
+public import Mathlib.CategoryTheory.Monoidal.Cartesian.Basic
+public import Mathlib.Topology.Category.CompHausLike.Limits
 
 /-!
 # Cartesian monoidal structure on `CompHausLike`
--- Mathlib/Topology/Category/LightProfinite/Cartesian.lean
+++ Mathlib/Topology/Category/LightProfinite/Cartesian.lean
@@ -3,8 +3,10 @@ Copyright (c) 2025 Dagur Asgeirsson. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Dagur Asgeirsson
 -/
-import Mathlib.Topology.Category.CompHausLike.Cartesian
-import Mathlib.Topology.Category.LightProfinite.Basic
+module
+
+public import Mathlib.Topology.Category.CompHausLike.Cartesian
+public import Mathlib.Topology.Category.LightProfinite.Basic
 
 /-!
 # Cartesian monoidal structure on `LightProfinite`
