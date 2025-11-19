338fae5faa6f2697b1b170657ea34ed6fe705c48
Monica Omar <23701951+themathqueen@users.noreply.github.com>
2025-11-19 08:29:00 +0000

    Update Coalgebra.lean

--- Mathlib/Analysis/InnerProductSpace/Coalgebra.lean
+++ Mathlib/Analysis/InnerProductSpace/Coalgebra.lean
@@ -3,8 +3,10 @@ Copyright (c) 2025 Monica Omar. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Monica Omar
 -/
-import Mathlib.Analysis.InnerProductSpace.TensorProduct
-import Mathlib.RingTheory.Coalgebra.Basic
+module
+
+public import Mathlib.Analysis.InnerProductSpace.TensorProduct
+public import Mathlib.RingTheory.Coalgebra.Basic
 
 /-!
 # Finite-dimensional inner product space with a (co)algebra structure
