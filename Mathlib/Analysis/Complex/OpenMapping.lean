66b2a6571bcd28f2ab6cd8e15f195d394fc175bd
Jovan Gerbscheid <56355248+JovanGerb@users.noreply.github.com>
2025-11-19 09:39:00 +0000

    feat(push_neg): tag `not_frequently` and `not_eventually` (#31403)
    This PR add support for `not_frequently` and `not_eventually` in the `push_neg` tactic, and it refactors some proofs using this.

--- Mathlib/Analysis/Complex/OpenMapping.lean
+++ Mathlib/Analysis/Complex/OpenMapping.lean
@@ -127,7 +127,7 @@ theorem AnalyticAt.eventually_constant_or_nhds_le_map_nhds {z₀ : E} (hg : Anal
     · exact hgr (by simpa [ray, norm_smul, mem_sphere_zero_iff_norm.mp hz] using ht)
     · exact analyticAt_const.add
         ((ContinuousLinearMap.smulRight (ContinuousLinearMap.id ℂ ℂ) z).analyticAt t)
-  by_cases! h : ∀ z ∈ sphere (0 : E) 1, ∀ᶠ t in 𝓝 0, gray z t = gray z 0
+  by_cases h : ∀ z ∈ sphere (0 : E) 1, ∀ᶠ t in 𝓝 0, gray z t = gray z 0
   · left
     -- If g is eventually constant along every direction, then it is eventually constant
     refine eventually_of_mem (ball_mem_nhds z₀ hr) fun z hz => ?_
@@ -145,6 +145,7 @@ theorem AnalyticAt.eventually_constant_or_nhds_le_map_nhds {z₀ : E} (hg : Anal
     simpa only [ray, gray, w, smul_smul, mul_inv_cancel₀ h', one_smul, add_sub_cancel,
       Function.comp_apply, coe_smul] using h3 (↑‖z - z₀‖) h4
   · right
+    simp only [not_forall] at h
     -- Otherwise, it is open along at least one direction and that implies the result
     obtain ⟨z, hz, hrz⟩ := h
     specialize h1 z hz 0 (mem_ball_self hr)
@@ -160,10 +161,11 @@ is analytic on a connected set `U`, then either it is constant on `U`, or it is 
 sense that it maps any open set contained in `U` to an open set in `ℂ`). -/
 theorem AnalyticOnNhd.is_constant_or_isOpen (hg : AnalyticOnNhd ℂ g U) (hU : IsPreconnected U) :
     (∃ w, ∀ z ∈ U, g z = w) ∨ ∀ s ⊆ U, IsOpen s → IsOpen (g '' s) := by
-  by_cases! h : ∃ z₀ ∈ U, ∀ᶠ z in 𝓝 z₀, g z = g z₀
+  by_cases h : ∃ z₀ ∈ U, ∀ᶠ z in 𝓝 z₀, g z = g z₀
   · obtain ⟨z₀, hz₀, h⟩ := h
     exact Or.inl ⟨g z₀, hg.eqOn_of_preconnected_of_eventuallyEq analyticOnNhd_const hU hz₀ h⟩
-  · refine Or.inr fun s hs1 hs2 => isOpen_iff_mem_nhds.mpr ?_
+  · simp only [not_exists, not_and] at h
+    refine Or.inr fun s hs1 hs2 => isOpen_iff_mem_nhds.mpr ?_
     rintro z ⟨w, hw1, rfl⟩
     exact (hg w (hs1 hw1)).eventually_constant_or_nhds_le_map_nhds.resolve_left (h w (hs1 hw1))
         (image_mem_map (hs2.mem_nhds hw1))
