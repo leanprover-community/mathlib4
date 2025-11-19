ec1a35044dfa207dd27c7e2b311c50b497af69e8
Monica Omar <23701951+themathqueen@users.noreply.github.com>
2025-11-19 12:20:52 +0000

    Update IntrinsicStar.lean

--- Mathlib/LinearAlgebra/Eigenspace/IntrinsicStar.lean
+++ Mathlib/LinearAlgebra/Eigenspace/IntrinsicStar.lean
@@ -3,8 +3,10 @@ Copyright (c) 2025 Monica Omar. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Monica Omar
 -/
-import Mathlib.Algebra.Star.LinearMap
-import Mathlib.LinearAlgebra.Eigenspace.Basic
+module
+
+public import Mathlib.Algebra.Star.LinearMap
+public import Mathlib.LinearAlgebra.Eigenspace.Basic
 
 /-!
 
