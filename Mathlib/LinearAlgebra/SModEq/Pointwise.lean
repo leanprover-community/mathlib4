9aba6c4ad941daadf60e7de545064e017ac899a8
Jiedong Jiang <emailboxofjjd@163.com>
2025-11-20 00:07:28 +0800

    fix

--- Mathlib/LinearAlgebra/SModEq/Pointwise.lean
+++ Mathlib/LinearAlgebra/SModEq/Pointwise.lean
@@ -3,8 +3,10 @@ Copyright (c) 2025 Jiedong Jiang. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jiedong Jiang
 -/
-import Mathlib.Algebra.Algebra.Operations
-import Mathlib.LinearAlgebra.SModEq.Basic
+module
+
+public import Mathlib.Algebra.Algebra.Operations
+public import Mathlib.LinearAlgebra.SModEq.Basic
 
 /-!
 # Pointwise lemmas for modular equivalence
