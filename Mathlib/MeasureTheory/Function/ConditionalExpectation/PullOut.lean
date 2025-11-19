7a9528c87b9c87cd745f2c1dad802408330056df
Etienne Marion <baguettes_casino0c@icloud.com>
2025-11-19 19:29:31 +0100

    retry

--- Mathlib/MeasureTheory/Function/ConditionalExpectation/PullOut.lean
+++ Mathlib/MeasureTheory/Function/ConditionalExpectation/PullOut.lean
@@ -33,6 +33,9 @@ We specialize this statement to the cases where `B` is scalar multiplication and
 conditional expectation, pull-out, bilinear map
 -/
 
+@[expose] public section
+
+
 open TopologicalSpace MeasureTheory.Lp Filter ContinuousLinearMap
 
 open scoped NNReal ENNReal Topology MeasureTheory
