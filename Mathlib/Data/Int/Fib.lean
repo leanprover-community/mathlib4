28612a0ec583ac177f247ff41073bd5ef9dee67c
Monica Omar <23701951+themathqueen@users.noreply.github.com>
2025-11-19 08:25:19 +0000

    Update Fib.lean
    Publicly import Fibonacci functions from natural numbers.

--- Mathlib/Data/Int/Fib.lean
+++ Mathlib/Data/Int/Fib.lean
@@ -3,7 +3,9 @@ Copyright (c) 2025 Monica Omar. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Monica Omar
 -/
-import Mathlib.Data.Nat.Fib.Basic
+module
+
+public import Mathlib.Data.Nat.Fib.Basic
 
 /-!
 
