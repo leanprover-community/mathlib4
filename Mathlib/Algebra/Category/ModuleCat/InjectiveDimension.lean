6b2a2fb6ce2c2b4d91f61184651c49c860aad8d9
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2025-11-20 21:24:47 +0800

    fix

--- Mathlib/Algebra/Category/ModuleCat/InjectiveDimension.lean
+++ Mathlib/Algebra/Category/ModuleCat/InjectiveDimension.lean
@@ -3,20 +3,24 @@ Copyright (c) 2025 Nailin Guan. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Nailin Guan
 -/
-import Mathlib.Algebra.Category.Grp.Zero
-import Mathlib.Algebra.Category.ModuleCat.EnoughInjectives
-import Mathlib.Algebra.Category.ModuleCat.Ulift
-import Mathlib.Algebra.Homology.DerivedCategory.Ext.EnoughInjectives
-import Mathlib.Algebra.Homology.DerivedCategory.Ext.EnoughProjectives
-import Mathlib.Algebra.Homology.DerivedCategory.Ext.Ulift
-import Mathlib.CategoryTheory.Abelian.Injective.Dimension
-import Mathlib.RingTheory.Ideal.Quotient.Operations
+module
+
+public import Mathlib.Algebra.Category.Grp.Zero
+public import Mathlib.Algebra.Category.ModuleCat.EnoughInjectives
+public import Mathlib.Algebra.Category.ModuleCat.Ulift
+public import Mathlib.Algebra.Homology.DerivedCategory.Ext.EnoughInjectives
+public import Mathlib.Algebra.Homology.DerivedCategory.Ext.EnoughProjectives
+public import Mathlib.Algebra.Homology.DerivedCategory.Ext.Ulift
+public import Mathlib.CategoryTheory.Abelian.Injective.Dimension
+public import Mathlib.RingTheory.Ideal.Quotient.Operations
 
 /-!
 
 # Injective Dimension in ModuleCat
 
 -/
+
+@[expose] public section
 
 universe u u' v v'
 
