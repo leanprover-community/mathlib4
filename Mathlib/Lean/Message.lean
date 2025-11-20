543c73c4060422f43dab094b966f766826ab0c2a
thorimur <68410468+thorimur@users.noreply.github.com>
2025-11-19 19:11:44 -0500

    chore: modulize

--- Mathlib/Lean/Message.lean
+++ Mathlib/Lean/Message.lean
@@ -3,11 +3,15 @@ Copyright (c) 2025 Thomas R. Murrills. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Thomas R. Murrills
 -/
-import Mathlib.Init
+module
+
+public import Mathlib.Init
 
 /-!
 # Additional utilities for working with `MessageData`
 -/
+
+@[expose] public section
 
 section WithPlural
 
