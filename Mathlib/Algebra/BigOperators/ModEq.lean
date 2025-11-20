703465076aaa10bc973222e7ab0c168067da5153
Yury Kudryashov <yury@harmonic.fun>
2025-11-20 18:57:32 +0000

    Use `module .. public import`

--- Mathlib/Algebra/BigOperators/ModEq.lean
+++ Mathlib/Algebra/BigOperators/ModEq.lean
@@ -3,8 +3,10 @@ Copyright (c) 2025 Concordance Inc. dba Harmonic. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yury Kudryashov
 -/
-import Mathlib.Algebra.BigOperators.Ring.Finset
-import Mathlib.Data.ZMod.Basic
+module
+
+public import Mathlib.Algebra.BigOperators.Ring.Finset
+public import Mathlib.Data.ZMod.Basic
 
 /-!
 # Congruence modulo natural and integer numbers for big operators
