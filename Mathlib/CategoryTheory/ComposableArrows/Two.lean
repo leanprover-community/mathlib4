6b1998414d4564d76f7425ee1d9e04ee731556db
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-11-20 06:27:51 +0100

    added comment about the names

--- Mathlib/CategoryTheory/ComposableArrows/Two.lean
+++ Mathlib/CategoryTheory/ComposableArrows/Two.lean
@@ -10,10 +10,13 @@ public import Mathlib.CategoryTheory.ComposableArrows.Basic
 /-!
 # API for compositions of two arrows
 
-Given morphisms `f : i ⟶ j`, `g : j ⟶ k`, and `fg : i ⟶ k` such that `f ≫ g = fg`
-(which may arise from a term in `ComposableArrows C 2`), we define
-maps `twoδ₂Toδ₁ : mk₁ f ⟶ mk₁ fg` and `twoδ₁Toδ₀ : mk₁ fg ⟶ mk₁ g` in
-the category `ComposableArrows C 1`.
+Given morphisms `f : i ⟶ j`, `g : j ⟶ k`, and `fg : i ⟶ k` in a category `C`
+such that `f ≫ g = fg`, we define maps `twoδ₂Toδ₁ : mk₁ f ⟶ mk₁ fg` and
+`twoδ₁Toδ₀ : mk₁ fg ⟶ mk₁ g` in the category `ComposableArrows C 1`.
+The names are justified by the fact that `ComposableArrow.mk₂ f g`
+can be thought of as a `2`-simplex in the simplicial set `nerve C`,
+and its faces (numbered from `0` to `2`) are respectively `mk₁ g`,
+`mk₁ fg` and `mk₁ f`.
 
 -/
 
