ca0e0a5303981b1aee1695018fcdcda8535b3886
thorimur <68410468+thorimur@users.noreply.github.com>
2025-11-20 10:42:20 -0500

    chore: remove unnecessary `import all`

--- Mathlib/Tactic/Linter/PrivateModule.lean
+++ Mathlib/Tactic/Linter/PrivateModule.lean
@@ -8,7 +8,6 @@ module
 public meta import Lean.Elab.Command
 public import Lean.Linter.Basic
 public import Lean.Environment
-import all Lean.Environment
 -- Import this linter explicitly to ensure that
 -- this file has a valid copyright header and module docstring.
 import Mathlib.Tactic.Linter.Header
