daea74a58bce9303d3f1b0d6c39bcb71377bfcb1
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2025-11-20 17:00:20 +0800

    fix

--- Mathlib.lean
+++ Mathlib.lean
@@ -6053,6 +6053,7 @@ public import Mathlib.RingTheory.Regular.IsSMulRegular
 public import Mathlib.RingTheory.Regular.RegularSequence
 public import Mathlib.RingTheory.RegularLocalRing.Defs
 public import Mathlib.RingTheory.RegularLocalRing.RegularRing.Basic
+public import Mathlib.RingTheory.RegularLocalRing.RegularRing.Polynomial
 public import Mathlib.RingTheory.RingHom.Bijective
 public import Mathlib.RingTheory.RingHom.Etale
 public import Mathlib.RingTheory.RingHom.FaithfullyFlat
