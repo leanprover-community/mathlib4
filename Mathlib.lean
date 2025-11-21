9d4764b4f92064f39dcc14fe9486cf3c8b5bc147
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2025-11-21 18:36:30 +0800

    add basic defs

--- Mathlib.lean
+++ Mathlib.lean
@@ -5763,6 +5763,7 @@ public import Mathlib.RingTheory.FreeCommRing
 public import Mathlib.RingTheory.FreeRing
 public import Mathlib.RingTheory.Frobenius
 public import Mathlib.RingTheory.Generators
+public import Mathlib.RingTheory.Gorenstein.Defs
 public import Mathlib.RingTheory.GradedAlgebra.Basic
 public import Mathlib.RingTheory.GradedAlgebra.FiniteType
 public import Mathlib.RingTheory.GradedAlgebra.Homogeneous.Ideal
