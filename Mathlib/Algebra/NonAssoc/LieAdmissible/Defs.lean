31a410493f0580975d6c34834048d081a3a9f4e3
Nikolas Tapia <tapia@wias-berlin.de>
2025-11-21 20:54:28 +0100

    resolve conflicts and use module

--- Mathlib/Algebra/NonAssoc/LieAdmissible/Defs.lean
+++ Mathlib/Algebra/NonAssoc/LieAdmissible/Defs.lean
@@ -3,11 +3,9 @@ Copyright (c) 2025 Nikolas Tapia. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Nikolas Tapia
 -/
-import Mathlib.Algebra.Algebra.Defs
-import Mathlib.Algebra.Lie.Basic
-import Mathlib.Algebra.NonAssoc.PreLie.Basic
 module
 
+public import Mathlib.Algebra.Algebra.Defs
 public import Mathlib.Algebra.Lie.Basic
 public import Mathlib.Algebra.NonAssoc.PreLie.Basic
 /-!
