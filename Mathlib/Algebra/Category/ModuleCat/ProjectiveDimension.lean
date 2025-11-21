519eab88d3d13bf6307de7663951a9feb2ad9420
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2025-11-21 16:39:12 +0800

    finish semi linear equiv version

--- Mathlib/Algebra/Category/ModuleCat/ProjectiveDimension.lean
+++ Mathlib/Algebra/Category/ModuleCat/ProjectiveDimension.lean
@@ -21,13 +21,12 @@ universe u u' v v'
 
 variable {R : Type u} [CommRing R]
 
-open CategoryTheory Abelian
+open CategoryTheory Abelian Module
 
 namespace CategoryTheory
 
 section
 
-open Module in
 lemma hasProjectiveDimensionLE_of_linearEquiv [Small.{v} R] [Small.{v'} R]
     {M : ModuleCat.{v} R} {N : ModuleCat.{v'} R}
     (e' : M ≃ₗ[R] N) (n : ℕ) [h : HasProjectiveDimensionLE M n] :
@@ -126,6 +125,33 @@ section
 
 variable {R' : Type u'} [CommRing R'] (e : R ≃+* R')
 
+section
+
+lemma _root_.Module.projective_of_semiLinearEquiv {M N : Type*} [AddCommGroup M] [AddCommGroup N]
+    [Module R M] [Module R' N] (proj : Module.Projective R M)
+    (e' : letI : RingHomInvPair e.toRingHom e.symm.toRingHom := RingHomInvPair.of_ringEquiv e
+          letI : RingHomInvPair e.symm.toRingHom e.toRingHom := RingHomInvPair.symm _ _
+          M ≃ₛₗ[e.toRingHom] N) : Module.Projective R' N := by
+  rcases Module.projective_def'.mp proj with ⟨s, hs⟩
+  letI : RingHomInvPair e.toRingHom e.symm.toRingHom := RingHomInvPair.of_ringEquiv e
+  letI : RingHomInvPair e.symm.toRingHom e.toRingHom := RingHomInvPair.symm _ _
+  let eR : R ≃ₛₗ[e.toRingHom] R' := {
+    __ := e
+    map_smul' r x := by simp }
+  let eP : (M →₀ R) ≃ₛₗ[e.toRingHom] (M →₀ R') := Finsupp.mapRange.linearEquiv eR
+  let i : N →ₗ[R'] (M →₀ R') := eP.toLinearMap.comp (s.comp e'.symm.toLinearMap)
+  let s' : (M →₀ R') →ₗ[R'] N :=
+    e'.toLinearMap.comp ((Finsupp.linearCombination R id).comp eP.symm.toLinearMap)
+  have : s'.comp i = LinearMap.id := by
+    ext x
+    simp only [RingEquiv.toRingHom_eq_coe, LinearMap.coe_comp, LinearEquiv.coe_coe,
+      Function.comp_apply, LinearEquiv.symm_apply_apply, LinearMap.id_coe, id_eq,
+      ← e'.eq_symm_apply, s', i]
+    rw [← LinearMap.comp_apply, hs, LinearMap.id_apply]
+  exact Module.Projective.of_split i s' this
+
+end
+
 open Module in
 lemma hasProjectiveDimensionLE_of_semiLinearEquiv [Small.{v} R] [Small.{v'} R']
     {M : ModuleCat.{v} R} {N : ModuleCat.{v'} R'}
@@ -139,32 +165,81 @@ lemma hasProjectiveDimensionLE_of_semiLinearEquiv [Small.{v} R] [Small.{v'} R']
   · simp only [HasProjectiveDimensionLE, zero_add,
       ← projective_iff_hasProjectiveDimensionLT_one] at h ⊢
     rw [← IsProjective.iff_projective] at h ⊢
-
-    sorry
+    exact Module.projective_of_semiLinearEquiv e h e'
   · rename_i n ih
-    let PM := ModuleCat.of R (M →₀ Shrink.{v} R)
-    let b : Basis M R (M →₀ Shrink.{v} R) := ⟨Finsupp.mapRange.linearEquiv (Shrink.linearEquiv R R)⟩
-    let f : PM ⟶ M := ModuleCat.ofHom (b.constr ℕ _root_.id)
-    have epif : Epi f := by
-      rw [ModuleCat.epi_iff_range_eq_top, LinearMap.range_eq_top]
+    let b : Basis M R (M →₀ Shrink.{v} R) :=
+      ⟨Finsupp.mapRange.linearEquiv (Shrink.linearEquiv.{v} R R)⟩
+    let f := (b.constr ℕ _root_.id)
+    have surjf : Function.Surjective f := by
       refine fun m ↦ ⟨Finsupp.single m 1, ?_⟩
-      simp only [ModuleCat.hom_ofHom, f, b]
-      rw [Basis.constr_apply]
+      simp only [f, b]
+      rw [Module.Basis.constr_apply]
       simp [Finsupp.mapRange.linearEquiv]
-    have surjf := (ModuleCat.epi_iff_surjective f).mp epif
-    let _ : Projective PM := ModuleCat.projective_of_free b
-    let PN := ModuleCat.of R' (N →₀ Shrink.{v'} R')
     let b' : Basis N R' (N →₀ Shrink.{v'} R') :=
-      ⟨Finsupp.mapRange.linearEquiv (Shrink.linearEquiv R' R')⟩
-    let _ : Projective PN := ModuleCat.projective_of_free b'
-    let eR : Shrink.{v} R ≃ₛₗ[e.toRingHom] Shrink.{v'} R' := sorry
-    let eP : PM ≃ₛₗ[e.toRingHom] PN :=
+      ⟨Finsupp.mapRange.linearEquiv (Shrink.linearEquiv.{v'} R' R')⟩
+    let eR' : R ≃ₛₗ[e.toRingHom] R' := {
+      __ := e
+      map_smul' r x := by simp }
+    let eR : Shrink.{v} R ≃ₛₗ[e.toRingHom] Shrink.{v'} R' :=
+      ((Shrink.linearEquiv.{v} R R).trans eR').trans (Shrink.linearEquiv.{v'} R' R').symm
+    let eP : (M →₀ Shrink.{v} R) ≃ₛₗ[e.toRingHom] (N →₀ Shrink.{v'} R') :=
       (Finsupp.mapDomain.linearEquiv (Shrink.{v} R) R e'.toEquiv).trans
       (Finsupp.mapRange.linearEquiv eR)
-    let g : PN ⟶ N := ModuleCat.ofHom ((e'.toLinearMap.comp f.hom).comp eP.symm.toLinearMap)
-    have epi : Epi g := by simpa [ModuleCat.epi_iff_surjective, g] using surjf
-
-    sorry
+    let g := ((e'.toLinearMap.comp f).comp eP.symm.toLinearMap)
+    have surjg : Function.Surjective g := by
+      simp only [LinearMap.coe_comp, LinearEquiv.coe_coe, g]
+      exact (e'.surjective.comp surjf).comp eP.symm.surjective
+    let S : ShortComplex (ModuleCat.{v} R) := {
+      X₃ := M
+      f := (ModuleCat.ofHom (LinearMap.ker f).subtype)
+      g := (ModuleCat.ofHom f)
+      zero := by
+        rw [← ModuleCat.ofHom_comp, LinearMap.comp_ker_subtype]
+        rfl }
+    have S_exact : S.ShortExact := {
+      exact := (CategoryTheory.ShortComplex.ShortExact.moduleCat_exact_iff_function_exact S).mpr
+        (LinearMap.exact_subtype_ker_map _)
+      mono_f := (ModuleCat.mono_iff_injective _).mpr (Submodule.subtype_injective _)
+      epi_g := (ModuleCat.epi_iff_surjective _).mpr surjf }
+    let S' : ShortComplex (ModuleCat.{v'} R') := {
+      X₃ := N
+      f := (ModuleCat.ofHom (LinearMap.ker g).subtype)
+      g := (ModuleCat.ofHom g)
+      zero := by
+        rw [← ModuleCat.ofHom_comp, LinearMap.comp_ker_subtype]
+        rfl }
+    have S'_exact : S'.ShortExact := {
+      exact := (CategoryTheory.ShortComplex.ShortExact.moduleCat_exact_iff_function_exact S').mpr
+        (LinearMap.exact_subtype_ker_map _)
+      mono_f := (ModuleCat.mono_iff_injective _).mpr (Submodule.subtype_injective _)
+      epi_g := (ModuleCat.epi_iff_surjective _).mpr surjg }
+    have : HasProjectiveDimensionLT S.X₁ (n + 1) :=
+      (S_exact.hasProjectiveDimensionLT_X₃_iff n (ModuleCat.projective_of_free b)).mp h
+    have ker1 (x : LinearMap.ker f) : eP x.1 ∈ LinearMap.ker g := by
+      simp only [LinearMap.mem_ker, LinearMap.coe_comp, LinearEquiv.coe_coe, Function.comp_apply,
+        EmbeddingLike.map_eq_zero_iff, g]
+      rw [← LinearMap.mem_ker.mp x.2]
+      congr
+      exact eP.symm_apply_apply x.1
+    have ker2 (x : LinearMap.ker g) : eP.symm x.1 ∈ LinearMap.ker f := by
+      have := LinearMap.mem_ker.mp x.2
+      simp only [g, LinearMap.coe_comp, LinearEquiv.coe_coe, Function.comp_apply,
+        EmbeddingLike.map_eq_zero_iff] at this
+      exact this
+    let eker' : LinearMap.ker f ≃ₛₗ[e.toRingHom] LinearMap.ker g := {
+      toFun x := ⟨eP x.1, ker1 x⟩
+      map_add' x y := by simp
+      map_smul' r x := SetCoe.ext (eP.map_smulₛₗ _ _)
+      invFun x := ⟨eP.symm x.1, ker2 x⟩
+      left_inv := by
+        rw [Function.leftInverse_iff_comp]
+        exact funext (fun x ↦ SetCoe.ext (eP.symm_apply_apply x.1))
+      right_inv := by
+        rw [Function.rightInverse_iff_comp]
+        exact funext (fun x ↦ SetCoe.ext (eP.apply_symm_apply x.1)) }
+    let eker : S.X₁ ≃ₛₗ[e.toRingHom] S'.X₁ := eker'
+    apply (S'_exact.hasProjectiveDimensionLT_X₃_iff n (ModuleCat.projective_of_free b')).mpr
+    exact ih eker
 
 lemma projectiveDimension_eq_of_semiLinearEquiv [Small.{v} R] [Small.{v'} R']
     {M : ModuleCat.{v} R} {N : ModuleCat.{v'} R'}
