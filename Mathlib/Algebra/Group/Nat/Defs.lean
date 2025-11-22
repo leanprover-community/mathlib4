75ed83cf6011c9a6629f79862d3011e430885a69
Yaël Dillies <yael.dillies@gmail.com>
2025-11-22 16:50:43 +0100

    reduce diff

--- Mathlib/Algebra/Group/Nat/Defs.lean
+++ Mathlib/Algebra/Group/Nat/Defs.lean
@@ -6,7 +6,6 @@ Authors: Floris van Doorn, Leonardo de Moura, Jeremy Avigad, Mario Carneiro
 module
 
 public import Mathlib.Algebra.Group.Defs
-public import Mathlib.Data.Nat.Basic
 
 /-!
 # The natural numbers form a monoid
