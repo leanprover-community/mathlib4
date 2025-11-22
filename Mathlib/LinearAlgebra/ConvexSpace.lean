6ed11ea558a9e62832f763188f8eb951c684379b
Kim Morrison <kim@tqft.net>
2025-11-22 14:01:07 +1100

    cleanup

--- Mathlib/LinearAlgebra/ConvexSpace.lean
+++ Mathlib/LinearAlgebra/ConvexSpace.lean
@@ -1,10 +1,13 @@
-import Mathlib.Algebra.Module.Basic
-import Mathlib.Data.Finset.Basic
-import Mathlib
+import Mathlib.Data.Finsupp.SMulWithZero
+import Mathlib.Data.Finsupp.Sigma
+import Mathlib.GroupTheory.GroupAction.Ring
+import Mathlib.Algebra.Order.Ring.Defs
 
 universe u v
 
 noncomputable section
+
+open Classical
 
 structure FiniteProbability (R : Type u) [LE R] [AddCommMonoid R] [One R] (ι : Type v)
     extends weights : ι →₀ R where
@@ -13,25 +16,32 @@ structure FiniteProbability (R : Type u) [LE R] [AddCommMonoid R] [One R] (ι : 
 
 namespace FiniteProbability
 
-variable {R : Type u} [LE R] [Semiring R] {κ : Type v} {ι : κ → Type v}
+variable {R : Type u} [PartialOrder R] [Semiring R] [IsStrictOrderedRing R]
+  {κ : Type v} {ι : κ → Type v}
 
-def _root_.Finsupp.embSigma {k : κ} (f : ι k →₀ R) : (Σ k, ι k) →₀ R :=
-  Finsupp.embDomain (Function.Embedding.sigmaMk k) f
-
-def dirac {ι : Type v} (i : ι) : FiniteProbability R ι where
+open Classical in
+def single {ι : Type v} (i : ι) : FiniteProbability R ι where
   weights := Finsupp.single i 1
-  nonneg := sorry
-  total := sorry
+  nonneg m := by
+    rw [Finsupp.single_apply]
+    split
+    · exact zero_le_one' R
+    · grind
+  total := by simp
 
 def comp (f : FiniteProbability R κ) (g : (k : κ) → FiniteProbability R (ι k)) :
     FiniteProbability R (Σ k, ι k) where
   weights := f.sum (fun m r => (r • (g m).weights).embSigma)
-  nonneg := sorry
-  total := sorry
+  nonneg := by
+    intro ⟨k, i⟩
+    sorry
+  total := by
+    sorry
 
 end FiniteProbability
 
-class ConvexSpace (R : Type u) (M : Type v) [LE R] [Semiring R] where
+class ConvexSpace (R : Type u) (M : Type v)
+    [PartialOrder R] [Semiring R] [IsStrictOrderedRing R] where
   convexCombination {ι : Type v} (f : FiniteProbability R ι) (xs : ι → M) : M
   /-- Associativity of convex combination. -/
   assoc
@@ -41,7 +51,7 @@ class ConvexSpace (R : Type u) (M : Type v) [LE R] [Semiring R] where
     convexCombination f₀ (fun m => convexCombination (f₁ m) (xs m)) =
       convexCombination (f₀.comp f₁) (fun ⟨k, i⟩ => xs k i)
   /-- A convex combination of a single point is that point. -/
-  dirac {ι : Type v} (i : ι) (x : M) : convexCombination (.dirac i) (fun _ => x) = x
+  single {ι : Type v} (i : ι) (x : M) : convexCombination (.single i) (fun _ => x) = x
   /-- Convex combinations are determined by the points with non-zero weights. -/
   -- Perhaps this follows from `assoc`, but I don't see how.
   ext {ι : Type v} (f : FiniteProbability R ι) (xs xs' : ι → M)
