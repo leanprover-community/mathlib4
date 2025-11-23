422edc21b09e3d48244ff531f3d3c84f60a8c708
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2025-11-23 21:47:30 +0800

    fix

--- Mathlib/RingTheory/GlobalDimension.lean
+++ Mathlib/RingTheory/GlobalDimension.lean
@@ -7,6 +7,7 @@ module
 
 public import Mathlib.Algebra.Category.Grp.Zero
 public import Mathlib.Algebra.Category.ModuleCat.EnoughInjectives
+public import Mathlib.Algebra.Category.ModuleCat.Localization
 public import Mathlib.Algebra.Category.ModuleCat.Projective
 public import Mathlib.Algebra.Homology.DerivedCategory.Ext.EnoughInjectives
 public import Mathlib.Algebra.Homology.ShortComplex.ModuleCat
@@ -39,41 +40,7 @@ variable (R : Type u) [CommRing R]
 
 open CategoryTheory
 
-local instance [Small.{v} R] (M : Type v) [AddCommGroup M] [Module R M] (S : Submonoid R) :
-    Small.{v} (LocalizedModule S M) :=
-  small_of_surjective (IsLocalizedModule.mk'_surjective S (LocalizedModule.mkLinearMap S M))
-
 variable {R}
-
-/-- Shrink of `LocalizedModule S M` in category which `M` belongs. -/
-noncomputable def ModuleCat.localizedModule [Small.{v} R] (M : ModuleCat.{v} R) (S : Submonoid R) :
-    ModuleCat.{v} (Localization S) :=
-  ModuleCat.of.{v} _ (Shrink.{v} (LocalizedModule S M))
-
-/-- The `R` module structure on `M.localizedModule S` given by the
-`R` module structure on (Shrink.{v} (LocalizedModule S M)) -/
-noncomputable local instance [Small.{v} R] (M : ModuleCat.{v} R) (S : Submonoid R) :
-    Module R (M.localizedModule S) :=
-  inferInstanceAs (Module R (Shrink.{v} (LocalizedModule S M)))
-
-/-- The corresponding linear map to make `M.localizedModule` is localized module of `M`. -/
-noncomputable def ModuleCat.localizedModule_mkLinearMap [Small.{v} R] (M : ModuleCat.{v} R)
-    (S : Submonoid R) : M →ₗ[R] (M.localizedModule S) :=
-  (Shrink.linearEquiv.{v} R _).symm.toLinearMap.comp (LocalizedModule.mkLinearMap S M)
-
-instance ModuleCat.localizedModule_isLocalizedModule [Small.{v} R] (M : ModuleCat.{v} R)
-    (S : Submonoid R) : IsLocalizedModule S (M.localizedModule_mkLinearMap S) := by
-  simpa [ModuleCat.localizedModule_mkLinearMap] using IsLocalizedModule.of_linearEquiv _ _ _
-
-instance [Small.{v} R] (M : ModuleCat.{v} R) (S : Submonoid R) :
-    IsScalarTower R (Localization S) (M.localizedModule S) :=
-  (equivShrink (LocalizedModule S M)).symm.isScalarTower R (Localization S)
-
-/-- The category version of `IsLocalizedModule.mapExtendScalars`. -/
-noncomputable def ModuleCat.localizedModule_map [Small.{v} R] {M N : ModuleCat.{v} R}
-    (S : Submonoid R) (f : M ⟶ N) : (M.localizedModule S) ⟶ (N.localizedModule S) :=
-  ModuleCat.ofHom.{v} <| IsLocalizedModule.mapExtendScalars S (M.localizedModule_mkLinearMap S)
-    (N.localizedModule_mkLinearMap S) (Localization S) (ModuleCat.homLinearEquiv (S := R) f)
 
 lemma projectiveDimension_eq_iSup_localizedModule_prime [Small.{v, u} R] [IsNoetherianRing R]
     (M : ModuleCat.{v} R) [Module.Finite R M] : projectiveDimension M =
