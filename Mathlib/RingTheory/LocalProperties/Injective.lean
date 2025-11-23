7e80a1c131607ef8a54c046231ba91503774b20b
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2025-11-23 17:21:23 +0800

    Update Injective.lean

--- Mathlib/RingTheory/LocalProperties/Injective.lean
+++ Mathlib/RingTheory/LocalProperties/Injective.lean
@@ -36,8 +36,6 @@ lemma Module.Baer.iff_surjective : Module.Baer R M ↔
 
 section
 
-private instance [IsNoetherianRing R] (I : Ideal R) : Module.FinitePresentation R (R ⧸ I) := sorry
-
 universe u' v'
 
 theorem Module.injective_of_isLocalizedModule [Small.{v} R] [IsNoetherianRing R] {Rₛ : Type u'}
@@ -46,8 +44,24 @@ theorem Module.injective_of_isLocalizedModule [Small.{v} R] [IsNoetherianRing R]
     [IsLocalizedModule S f] [Module.Injective R M] : Module.Injective Rₛ Mₛ := by
   have MB : Baer R M := Baer.of_injective ‹_›
   rw [← Baer.iff_injective, Baer.iff_surjective]
-  intro I
-
-  sorry
+  intro Iₛ
+  let I := Iₛ.comap (algebraMap R Rₛ)
+  let _ : FinitePresentation R I := finitePresentation_of_finite R I
+  let g : I →ₗ[R] Iₛ := sorry
+  let _ : IsLocalizedModule S g :=
+    --Submodule.isLocalizedModule
+    sorry
+  let h : R →ₗ[R] Rₛ := Algebra.linearMap R Rₛ
+  let _ : IsLocalizedModule S h := inferInstance
+  let gM := IsLocalizedModule.mapExtendScalars S g f Rₛ
+  let _ : IsLocalizedModule S gM := FinitePresentation.isLocalizedModule_mapExtendScalars S g f Rₛ
+  let hM := IsLocalizedModule.mapExtendScalars S h f Rₛ
+  let _ : IsLocalizedModule S hM := FinitePresentation.isLocalizedModule_mapExtendScalars S h f Rₛ
+  have surj := Baer.iff_surjective.mp MB I
+  have eq : (LinearMap.lcomp Rₛ Mₛ (Submodule.subtype Iₛ)) =
+    IsLocalizedModule.mapExtendScalars S hM gM Rₛ (LinearMap.lcomp R M (Submodule.subtype I)) := by
+    sorry
+  rw [eq]
+  exact IsLocalizedModule.map_surjective S hM gM _ (Baer.iff_surjective.mp MB I)
 
 end
