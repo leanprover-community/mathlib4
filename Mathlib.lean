06091f66eee6ce0280f4323a09c939f05fe5af84
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2025-11-24 15:18:00 +0800

    Update Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -5764,6 +5764,7 @@ public import Mathlib.RingTheory.FreeCommRing
 public import Mathlib.RingTheory.FreeRing
 public import Mathlib.RingTheory.Frobenius
 public import Mathlib.RingTheory.Generators
+public import Mathlib.RingTheory.Gorenstein.Basic
 public import Mathlib.RingTheory.Gorenstein.Defs
 public import Mathlib.RingTheory.GradedAlgebra.Basic
 public import Mathlib.RingTheory.GradedAlgebra.FiniteType
