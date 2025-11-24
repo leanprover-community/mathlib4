e766df6c921eb3d47e9ff197076b2ee65b1f871d
Newell Jensen <newell.jensen@gmail.com>
2025-11-23 17:21:22 -0800

    Update Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -1732,6 +1732,7 @@ public import Mathlib.Analysis.Convex.TotallyBounded
 public import Mathlib.Analysis.Convex.Uniform
 public import Mathlib.Analysis.Convex.Visible
 public import Mathlib.Analysis.Convolution
+public import Mathlib.Analysis.Delone.Basic
 public import Mathlib.Analysis.Distribution.AEEqOfIntegralContDiff
 public import Mathlib.Analysis.Distribution.ContDiffMapSupportedIn
 public import Mathlib.Analysis.Distribution.FourierSchwartz
