2c48265d280a6badb9f52046f60325fe5fa0c4f3
Scott Carnahan <128885296+ScottCarnahan@users.noreply.github.com>
2025-11-24 08:57:35 +0000

    feat (RingTheory/HahnSeries): introduce binomialFamily and generalized powers. (#31215)
    We introduce a way to take generalized powers of Hahn series whose leading term is 1. The powers take values in a binomial ring.

--- Mathlib.lean
+++ Mathlib.lean
@@ -13019,6 +13019,7 @@ public import Mathlib.RingTheory.GradedAlgebra.Radical
 public import Mathlib.RingTheory.Grassmannian
 public import Mathlib.RingTheory.HahnSeries.Addition
 public import Mathlib.RingTheory.HahnSeries.Basic
+public import Mathlib.RingTheory.HahnSeries.Binomial
 public import Mathlib.RingTheory.HahnSeries.HEval
 public import Mathlib.RingTheory.HahnSeries.HahnEmbedding
 public import Mathlib.RingTheory.HahnSeries.Lex
