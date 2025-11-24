9502da2cb52e2f6884bc2058cde186a51022155e
Andrew Yang <the.erd.one@gmail.com>
2025-11-24 19:52:16 +0000

    fix

--- Mathlib/Algebra/Ring/Int/Field.lean
+++ Mathlib/Algebra/Ring/Int/Field.lean
@@ -3,10 +3,12 @@ Copyright (c) 2025 Andrew Yang. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Andrew Yang
 -/
-import Mathlib.Algebra.Field.IsField
-import Mathlib.Algebra.Ring.Int.Defs
+module
+
+public import Mathlib.Algebra.Field.IsField
+public import Mathlib.Algebra.Ring.Int.Defs
 
 /-! # `ℤ` is not a field -/
 
 /-- `ℤ` with its usual ring structure is not a field. -/
-theorem Int.not_isField : ¬IsField ℤ := fun ⟨_, _, h⟩ ↦ have := @h 2; by grind
+public theorem Int.not_isField : ¬IsField ℤ := fun ⟨_, _, h⟩ ↦ have := @h 2; by grind
