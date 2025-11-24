d3186f96152ed528ca94565cca7a1e8c0e2af40c
Snir Broshi <26556598+SnirBroshi@users.noreply.github.com>
2025-11-24 06:40:13 +0200

    feat(Combinatorics/SimpleGraph/Walks): correct authors based on git history

--- Mathlib/Combinatorics/SimpleGraph/Walks/Basic.lean
+++ Mathlib/Combinatorics/SimpleGraph/Walks/Basic.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2021 Kyle Miller. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Kyle Miller
+Authors: Kyle Miller, Peter Nelson
 -/
 module
 
--- Mathlib/Combinatorics/SimpleGraph/Walks/Subwalks.lean
+++ Mathlib/Combinatorics/SimpleGraph/Walks/Subwalks.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2021 Kyle Miller. All rights reserved.
+Copyright (c) 2025 Rida Hamadani. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Kyle Miller
+Authors: Rida Hamadani
 -/
 module
 
