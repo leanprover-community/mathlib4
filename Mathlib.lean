584323098a36b2d7fceb55ebfc148e2643bc20a5
Riccardo Brasca <riccardo.brasca@gmail.com>
2025-11-24 10:22:01 +0100

    forgot this

--- Mathlib.lean
+++ Mathlib.lean
@@ -6188,7 +6188,7 @@ public import Mathlib.RingTheory.UniqueFactorizationDomain.Finite
 public import Mathlib.RingTheory.UniqueFactorizationDomain.Finsupp
 public import Mathlib.RingTheory.UniqueFactorizationDomain.GCDMonoid
 public import Mathlib.RingTheory.UniqueFactorizationDomain.Ideal
-public import Mathlib.RingTheory.UniqueFactorizationDomain.Kaplanski
+public import Mathlib.RingTheory.UniqueFactorizationDomain.Kaplansky
 public import Mathlib.RingTheory.UniqueFactorizationDomain.Multiplicative
 public import Mathlib.RingTheory.UniqueFactorizationDomain.Multiplicity
 public import Mathlib.RingTheory.UniqueFactorizationDomain.Nat
