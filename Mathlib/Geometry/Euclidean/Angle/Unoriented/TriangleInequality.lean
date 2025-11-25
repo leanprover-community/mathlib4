2e0718e015d6bc83f6ac1749143c401c23d18398
LessnessRandomness <lessnessr@gmail.com>
2025-11-25 11:57:40 +0200

    Apply changes suggested by @JovanGerb

--- Mathlib/Geometry/Euclidean/Angle/Unoriented/TriangleInequality.lean
+++ Mathlib/Geometry/Euclidean/Angle/Unoriented/TriangleInequality.lean
@@ -46,16 +46,16 @@ lemma inner_normalized_ortho_sq_add_inner_sq_eq_one {x y : V}
   rw [NormedSpace.normalize, real_inner_smul_right, ortho, inner_sub_right,
     Submodule.starProjection_unit_singleton _ hy, real_inner_smul_right]
   by_cases h₁ : x = y
-  · simp_all
+  · simp [*]
   by_cases h₂ : x = - y
-  · simp_all
+  · simp [*]
   rw [real_inner_self_eq_norm_sq, hx]
   have H1 : ‖x - ⟪y, x⟫ • y‖ ≠ 0 := by
     simp only [ne_eq, norm_eq_zero, sub_eq_zero]
     intro H2
     rw [H2, norm_smul, hy, Real.norm_eq_abs, mul_one] at hx
     apply eq_or_eq_neg_of_abs_eq at hx
-    rcases hx with hx | hx <;> rw [hx] at H2 <;> simp_all
+    rcases hx with hx | hx <;> simp [*] at H2
   have H2 : (1 - ⟪y, x⟫ * ⟪x, y⟫) ^ 2 + ‖x - ⟪y, x⟫ • y‖ ^ 2 * ⟪x, y⟫ ^ 2 =
       ‖x - ⟪y, x⟫ • y‖ ^ 2 := by
     rw [← real_inner_self_eq_norm_sq, inner_sub_left, inner_sub_right, inner_sub_right,
@@ -94,22 +94,24 @@ lemma angle_le_angle_add_angle_aux {x y : V} (Hx : ‖x‖ = 1) (Hy : ‖y‖ = 
       simp [Hy, hxy]
   rw [← smul_assoc, smul_eq_mul]
   field_simp
-  rw [sq, ← real_inner_self_eq_norm_sq]
+  rw [← real_inner_self_eq_norm_sq]
   have H0 : ⟪x - ⟪x, y⟫ • y, x - ⟪x, y⟫ • y⟫ = 1 - ⟪x, y⟫ ^ 2 := by
     rw [inner_sub_left, inner_sub_right, inner_sub_right, inner_self_eq_one_of_norm_eq_one Hx,
       real_inner_smul_right, ← sq, real_inner_smul_left, real_inner_smul_left,
       real_inner_smul_right, inner_self_eq_one_of_norm_eq_one Hy, real_inner_comm y x]
     ring
   rw [real_inner_comm x, H0]
-  field_simp; simp
+  simp [H]
 
 lemma angle_le_angle_add_angle_of_norm_eq_one {x y z : V}
     (Hx : ‖x‖ = 1) (Hy : ‖y‖ = 1) (Hz : ‖z‖ = 1) :
     angle x z ≤ angle x y + angle y z := by
   rcases lt_or_ge Real.pi (angle x y + angle y z) with H | H
   · linarith [angle_le_pi x z]
-  have H0 : 0 ≤ angle x y + angle y z :=
-    add_nonneg (angle_nonneg x y) (angle_nonneg y z)
+  suffices Real.cos (angle x y + angle y z) ≤ Real.cos (angle x z) by
+    have H0 : 0 ≤ angle x y + angle y z :=
+      add_nonneg (angle_nonneg x y) (angle_nonneg y z)
+    rwa [Real.strictAntiOn_cos.le_iff_ge ⟨H0, H⟩ ⟨angle_nonneg x z, angle_le_pi x z⟩] at this
   have H1 : ⟪x, z⟫ = ⟪x, z⟫ := rfl
   nth_rw 2 [angle_le_angle_add_angle_aux Hx Hy, angle_le_angle_add_angle_aux Hz Hy] at H1
   simp only [inner_add_left, inner_add_right, real_inner_smul_left, real_inner_smul_right,
@@ -117,19 +119,17 @@ lemma angle_le_angle_add_angle_of_norm_eq_one {x y z : V}
     angle_comm z y, inner_normalize_ortho] at H1
   norm_num at H1
   rw [mul_comm (Real.cos (angle y z))] at H1
+  rw [Real.cos_add, ← inner_eq_cos_angle_of_norm_eq_one Hx Hz, H1]
   have H2 : -1 ≤ ⟪normalize (ortho y x), normalize (ortho y z)⟫ := by
     by_cases H3 : ortho y x = 0
-    · simp_all
+    · simp [*]
     by_cases H4 : ortho y z = 0
-    · simp_all
+    · simp [*]
     exact neg_one_le_real_inner_of_norm_eq_one
       (norm_normalize_eq_one_iff.mpr H3) (norm_normalize_eq_one_iff.mpr H4)
-  have H3 := mul_nonneg (sin_angle_nonneg x y) (sin_angle_nonneg y z)
-  have H4 : Real.cos (angle x y + angle y z) ≤ Real.cos (angle x z) := by
-    rw [neg_le_iff_add_nonneg] at H2
-    rw [Real.cos_add, ← inner_eq_cos_angle_of_norm_eq_one Hx Hz, H1]
-    linarith [mul_nonneg H3 H2]
-  rwa [Real.strictAntiOn_cos.le_iff_ge ⟨H0, H⟩ ⟨angle_nonneg x z, angle_le_pi x z⟩] at H4
+  have := sin_angle_nonneg x y; have := sin_angle_nonneg y z
+  grw [← H2]
+  ring_nf; rfl
 
 end UnitVectorAngles
 
