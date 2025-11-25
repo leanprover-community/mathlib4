f4645e4197ea481767d9d3ae529d47511643e377
François G. Dorais <3247514+fgdorais@users.noreply.github.com>
2025-11-25 00:00:11 +0000

    chore: adaptation for batteries#1500 (#32075)
    After [batteries#1500](https://github.com/leanprover-community/batteries/pull/1500) is merged:
    
    - [x] Edit the lakefile to point to leanprover-community/batteries:main
    - [x] Run lake update batteries
    - [x] Merge leanprover-community/mathlib4:master
    - [ ] Wait for CI and merge
    
    Co-authored-by: leanprover-community-mathlib4-bot <leanprover-community-mathlib4-bot@users.noreply.github.com>
    Co-authored-by: Wrenna Robson <wrenna.robson@bristol.ac.uk>
    Co-authored-by: F. G. Dorais <fgdorais@gmail.com>

--- Mathlib/Data/List/Nodup.lean
+++ Mathlib/Data/List/Nodup.lean
@@ -120,16 +120,14 @@ theorem not_nodup_of_get_eq_of_ne (xs : List α) (n m : Fin xs.length)
   rw [nodup_iff_injective_get]
   exact fun hinj => hne (hinj h)
 
-theorem idxOf_getElem [BEq α] [LawfulBEq α] {l : List α} (H : Nodup l) (i : Nat)
-    (h : i < l.length) : idxOf l[i] l = i := by
-  rw [← H.getElem_inj_iff, getElem_idxOf]
-  simp [idxOf_lt_length_iff]
+@[deprecated Nodup.idxOf_getElem (since := "2025-11-10")]
+theorem idxOf_getElem [DecidableEq α] {l : List α} : Nodup l → (i : Nat) → (h : i < l.length) →
+    idxOf l[i] l = i := Nodup.idxOf_getElem
 
 -- This is incorrectly named and should be `idxOf_get`;
 -- this already exists, so will require a deprecation dance.
-theorem get_idxOf [BEq α] [LawfulBEq α] {l : List α} (H : Nodup l) (i : Fin l.length) :
-    idxOf (get l i) l = i := by
-  simp [idxOf_getElem, H]
+theorem get_idxOf [DecidableEq α] {l : List α} (H : Nodup l) (i : Fin l.length) :
+    idxOf (get l i) l = i := by grind
 
 theorem nodup_iff_count_le_one [BEq α] [LawfulBEq α] {l : List α} : Nodup l ↔ ∀ a, count a l ≤ 1 :=
   nodup_iff_sublist.trans <|
