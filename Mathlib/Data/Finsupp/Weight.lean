5a706e6d0d2dbf7a124292bedbdefffdfab483f2
Wenrong Zou <141128015+WenrongZou@users.noreply.github.com>
2025-11-25 19:15:36 +0100

    Update Mathlib/Data/Finsupp/Weight.lean
    Co-authored-by: Jovan Gerbscheid <56355248+JovanGerb@users.noreply.github.com>

--- Mathlib/Data/Finsupp/Weight.lean
+++ Mathlib/Data/Finsupp/Weight.lean
@@ -214,7 +214,7 @@ theorem degree_eq_sum [Fintype σ] (f : σ →₀ R) : f.degree = ∑ i, f i := 
 theorem degree_add (a b : σ →₀ R) : (a + b).degree = a.degree + b.degree :=
   sum_add_index' (h := fun _ ↦ id) (congrFun rfl) fun _ _ ↦ congrFun rfl
 
-theorem degree_sum {α : Type*} [DecidableEq α] {s : Finset α} (f : α → σ →₀ ℕ) :
+theorem degree_sum {ι : Type*} [DecidableEq ι] {s : Finset ι} (f : ι → σ →₀ ℕ) :
     (∑ i ∈ s, f i).degree = ∑ i ∈ s, (f i).degree := by
   refine s.induction rfl ?_
   intro a s hs hs'
