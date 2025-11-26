449eca180f62f414d34869552facc820ef20271c
mitchell-horner <29882987+mitchell-horner@users.noreply.github.com>
2025-11-27 00:32:45 +1100

    Whitespace

--- Mathlib/Algebra/Order/Monoid/Canonical/Basic.lean
+++ Mathlib/Algebra/Order/Monoid/Canonical/Basic.lean
@@ -28,8 +28,7 @@ namespace Set
 variable {α : Type*} [AddCommMonoid α] [PartialOrder α] [CanonicallyOrderedAdd α]
   [Sub α] [OrderedSub α] {β : Type*} {f : α → β} {k : α}
 
-theorem range_add_eq_image_Ici :
-    range (fun x ↦ f (x + k)) = f '' Ici k :=
+theorem range_add_eq_image_Ici : range (fun x ↦ f (x + k)) = f '' Ici k :=
   Set.ext fun x ↦ ⟨fun ⟨y, hfy⟩ ↦ ⟨y + k, self_le_add_left k y, hfy⟩,
     fun ⟨y, hy, hfy⟩ ↦ ⟨y - k, by simpa [tsub_add_cancel_of_le hy] using hfy⟩⟩
 
