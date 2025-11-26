cc5b4e95b1b0fccb5f0e9d11d488a6ddc7ca5846
Kenny Lau <kc_kennylau@yahoo.com.hk>
2025-11-26 22:35:08 +0100

    review

--- Mathlib/RingTheory/Localization/Rat.lean
+++ Mathlib/RingTheory/Localization/Rat.lean
@@ -12,36 +12,26 @@ public import Mathlib.RingTheory.Localization.NumDen
 # Ring-theoretic fractions in ℚ
 -/
 
+@[expose] public section
+
 namespace Rat
+
+open IsFractionRing
 
 theorem isLocalizationIsInteger_iff (q : ℚ) :
     IsLocalization.IsInteger ℤ q ↔ q ∈ Set.range Int.cast := by
   simp [IsLocalization.IsInteger]
 
-theorem isFractionRingDen (q : ℚ) : (IsFractionRing.den ℤ q : ℤ).natAbs = q.den := by
-  conv_rhs => rw [← IsFractionRing.mk'_num_den ℤ q]
-  simp only [IsFractionRing.mk'_eq_div, algebraMap_int_eq, eq_intCast]
-  apply Nat.cast_injective (R := ℤ)
-  have := IsFractionRing.num_den_reduced ℤ q
-  rw [isRelPrime_iff_isCoprime, Int.isCoprime_iff_nat_coprime] at this
-  obtain h | h | h := lt_trichotomy (IsFractionRing.den ℤ q : ℤ) 0
-  · have h_pos : 0 < -(IsFractionRing.den ℤ q : ℤ) := neg_pos_of_neg h
-    rw [Int.ofNat_natAbs_of_nonpos h.le, ← neg_div_neg_eq, ← Int.cast_neg, ← Int.cast_neg]
-    rw [Rat.den_div_eq_of_coprime h_pos (by rwa [Int.natAbs_neg, Int.natAbs_neg])]
-  · simp at h
-  · rw [Rat.den_div_eq_of_coprime h this, Int.natAbs_of_nonneg h.le]
+theorem associated_num_den (q : ℚ) :
+    Associated (IsFractionRing.num ℤ q) q.num ∧ Associated (IsFractionRing.den ℤ q : ℤ) q.den :=
+  num_den_unique ℤ q q.num ⟨q.den, by simp⟩
+    (by simpa [isRelPrime_iff_isCoprime, Int.isCoprime_iff_nat_coprime] using q.reduced)
+    (by simp [Rat.num_div_den])
 
-theorem isFractionRingNum (q : ℚ) : Associated (IsFractionRing.num ℤ q : ℤ) q.num := by
-  conv_rhs => rw [← IsFractionRing.mk'_num_den ℤ q]
-  simp only [IsFractionRing.mk'_eq_div, algebraMap_int_eq, eq_intCast]
-  have := IsFractionRing.num_den_reduced ℤ q
-  rw [isRelPrime_iff_isCoprime, Int.isCoprime_iff_nat_coprime] at this
-  obtain h | h | h := lt_trichotomy (IsFractionRing.den ℤ q : ℤ) 0
-  · have h_pos : 0 < -(IsFractionRing.den ℤ q : ℤ) := neg_pos_of_neg h
-    rw [← neg_div_neg_eq, ← Int.cast_neg, ← Int.cast_neg]
-    rw [Rat.num_div_eq_of_coprime h_pos (by rwa [Int.natAbs_neg, Int.natAbs_neg]),
-      Associated.neg_right_iff]
-  · simp at h
-  · rw [Rat.num_div_eq_of_coprime h this]
+theorem isFractionRingDen (q : ℚ) : (IsFractionRing.den ℤ q : ℤ).natAbs = q.den := by
+  simpa [Int.associated_iff_natAbs] using q.associated_num_den.2
+
+theorem isFractionRingNum (q : ℚ) : Associated (IsFractionRing.num ℤ q : ℤ) q.num :=
+  q.associated_num_den.1
 
 end Rat
