6d2715238c2a2ffe0a96b2ef0c849a075aaf87fc
Etienne Marion <baguettes_casino0c@icloud.com>
2025-11-26 22:12:11 +0100

    cancel

--- Mathlib/Probability/Distributions/Gaussian/HasGaussianLaw.lean
+++ Mathlib/Probability/Distributions/Gaussian/HasGaussianLaw.lean
@@ -218,53 +218,6 @@ section Independence
 
 variable {ι : Type*} [Fintype ι] {mΩ : MeasurableSpace Ω} {P : Measure Ω}
 
-section charFun
-set_option trace.Meta.Tactic.fun_prop true in
-lemma HasGaussianLaw.charFun_toLp_pi {X : ι → Ω → ℝ} (hX : HasGaussianLaw (fun ω ↦ (X · ω)) P)
-    (ξ : EuclideanSpace ℝ ι) :
-    charFun (P.map (fun ω ↦ toLp 2 (X · ω))) ξ =
-      exp (∑ i, ξ i * P[X i] * I - ∑ i, ∑ j, (ξ i : ℂ) * ξ j * (cov[X i, X j; P] / 2)) := by
-  have := hX.isProbabilityMeasure
-  nth_rw 1 [(hX.toLp_pi 2).isGaussian_map.charFun_eq', covarianceBilin_apply_pi,
-    EuclideanSpace.real_inner_eq_sum_mul]
-  · simp_rw [ofReal_sum, Finset.sum_mul, ← mul_div_assoc, Finset.sum_div,
-      integral_complex_ofReal, ← ofReal_mul]
-    congrm exp (∑ i, Complex.ofReal (_ * ?_) * I - _)
-    rw [integral_map, eval_integral_piLp]
-    · simp
-    · simpa using fun i ↦ (hX.eval i).integrable
-    · exact (measurable_toLp 2 _).comp_aemeasurable hX.aemeasurable
-    · exact aestronglyMeasurable_id
-  · exact fun i ↦ (hX.eval i).memLp_two
-
-lemma HasGaussianLaw.charFun_toLp_prodMk {X Y : Ω → ℝ} (hXY : HasGaussianLaw (fun ω ↦ (X ω, Y ω)) P)
-    (ξ : WithLp 2 (ℝ × ℝ)) :
-    charFun (P.map (fun ω ↦ toLp 2 (X ω, Y ω))) ξ =
-      exp ((ξ.fst * P[X] + ξ.snd * P[Y]) * I -
-        (ξ.fst ^ 2 * Var[X; P] +
-          2 * ξ.fst * ξ.snd * cov[X, Y; P] + ξ.snd ^ 2 * Var[Y; P]) / 2) := by
-  have := hXY.isProbabilityMeasure
-  nth_rw 1 [(hXY.toLp_prodMk 2).isGaussian_map.charFun_eq', covarianceBilin_apply_prod]
-  · simp only [id_eq, prod_inner_apply, ofLp_fst, RCLike.inner_apply, conj_trivial, mul_comm,
-    ofLp_snd, ofReal_add, ofReal_mul, add_div, integral_complex_ofReal, pow_two]
-    rw [mul_comm (ξ.fst : ℂ), mul_comm (ξ.snd : ℂ)]
-    congrm exp (I * (?_ * _ + ?_ * _) - ?_)
-    · rw [integral_map, fst_integral_withLp]
-      · simp
-      · exact HasGaussianLaw.integrable
-      · fun_prop
-      · exact aestronglyMeasurable_id
-    · rw [integral_map, snd_integral_withLp]
-      · simp
-      · exact HasGaussianLaw.integrable
-      · fun_prop
-      · exact aestronglyMeasurable_id
-    · ring
-  · exact hXY.fst.memLp_two
-  · exact hXY.snd.memLp_two
-
-end charFun
-
 open ContinuousLinearMap in
 lemma iIndepFun.hasGaussianLaw {E : ι → Type*}
     [∀ i, NormedAddCommGroup (E i)] [∀ i, NormedSpace ℝ (E i)] [∀ i, MeasurableSpace (E i)]
