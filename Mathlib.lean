c5b06bd85a539c327d24df89a56c7346ac9a92c9
sinhp <sinahazratpour@gmail.com>
2025-11-26 16:01:05 +0100

    Update Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -2711,6 +2711,7 @@ public import Mathlib.CategoryTheory.Localization.StructuredArrow
 public import Mathlib.CategoryTheory.Localization.Triangulated
 public import Mathlib.CategoryTheory.Localization.Trifunctor
 public import Mathlib.CategoryTheory.LocallyCartesianClosed.ChosenPullbacksAlong
+public import Mathlib.CategoryTheory.LocallyCartesianClosed.ExponentiableMorphism
 public import Mathlib.CategoryTheory.LocallyDirected
 public import Mathlib.CategoryTheory.MarkovCategory.Basic
 public import Mathlib.CategoryTheory.Monad.Adjunction
