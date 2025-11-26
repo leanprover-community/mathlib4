545ad11535c91a77123172d95a08a8a052bfa2c7
Yaël Dillies <yael.dillies@gmail.com>
2025-11-26 08:25:34 +0100

    generalise

--- Mathlib/AlgebraicGeometry/Pullbacks.lean
+++ Mathlib/AlgebraicGeometry/Pullbacks.lean
@@ -10,8 +10,6 @@ public import Mathlib.AlgebraicGeometry.Gluing
 public import Mathlib.CategoryTheory.Limits.Opposites
 public import Mathlib.CategoryTheory.Limits.Shapes.Diagonal
 public import Mathlib.CategoryTheory.Monoidal.Cartesian.Over
-public import Mathlib.CategoryTheory.Monoidal.CommMon_
-public import Mathlib.CategoryTheory.Monoidal.Grp_
 
 /-!
 # Fibred products of schemes
@@ -637,6 +635,14 @@ instance Scheme.pullback_map_isOpenImmersion {X Y S X' Y' S' : Scheme}
   rw [pullback_map_eq_pullbackFstFstIso_inv]
   infer_instance
 
+section CartesianMonoidalCategory
+variable {S : Scheme}
+
+instance : CartesianMonoidalCategory (Over S) := Over.cartesianMonoidalCategory _
+instance : BraidedCategory (Over S) := .ofCartesianMonoidalCategory
+
+end CartesianMonoidalCategory
+
 section Spec
 
 variable (R S T : Type u) [CommRing R] [CommRing S] [CommRing T] [Algebra R S] [Algebra R T]
@@ -728,163 +734,23 @@ lemma diagonal_SpecMap :
 
 end Spec
 
-section CartesianMonoidalCategory
-variable {S : Scheme}
-
-instance : CartesianMonoidalCategory (Over S) := Over.cartesianMonoidalCategory _
-instance : BraidedCategory (Over S) := .ofCartesianMonoidalCategory
-
-end CartesianMonoidalCategory
-
 namespace Scheme
-variable {M S T : Scheme.{u}} [M.Over S] {f : T ⟶ S}
-
-instance : (Over.pullback f).Braided := .ofChosenFiniteProducts _
-
-@[simps]
-instance canonicallyOverPullback : (pullback (M ↘ S) f).CanonicallyOver T where
-  hom := pullback.snd (M ↘ S) f
-
-@[simps! -isSimp mul one]
-instance monObjAsOverPullback [MonObj (asOver M S)] : MonObj (asOver (pullback (M ↘ S) f) T) :=
-  ((Over.pullback f).mapMon.obj <| .mk <| asOver M S).mon
-
-instance isCommMonObj_asOver_pullback [MonObj (asOver M S)] [IsCommMonObj (asOver M S)] :
-    IsCommMonObj (asOver (pullback (M ↘ S) f) T) :=
-  ((Over.pullback f).mapCommMon.obj <| .mk <| asOver M S).comm
-
-instance GrpObjAsOverPullback [GrpObj (asOver M S)] : GrpObj (asOver (pullback (M ↘ S) f) T) :=
-  ((Over.pullback f).mapGrp.obj <| .mk <| asOver M S).grp
-
-instance : (pullback.fst (M ↘ S) (𝟙 S)).IsOver S := ⟨(pullback.condition).trans (by simp)⟩
-
-@[simp]
-lemma η_pullback_left : (OplaxMonoidal.η (Over.pullback f)).left = (pullback.snd (𝟙 _) f) := rfl
-
-@[simp]
-lemma ε_pullback_left : (LaxMonoidal.ε (Over.pullback f)).left = inv (pullback.snd (𝟙 _) f) := by
-  apply IsIso.eq_inv_of_hom_inv_id
-  rw [← η_pullback_left, ← Over.comp_left, Monoidal.η_ε, Over.id_left]
-
-lemma μ_pullback_left_fst_fst (X Y : Over S) :
-    (LaxMonoidal.μ (Over.pullback f) X Y).left ≫
-      pullback.fst _ _ ≫ pullback.fst _ _ = pullback.fst _ _ ≫ pullback.fst _ _ := by
-  rw [Monoidal.μ_of_cartesianMonoidalCategory,
-    ← cancel_epi (prodComparisonIso (Over.pullback f) X Y).hom.left, ← Over.comp_left_assoc,
-    Iso.hom_inv_id]
-  simp [CartesianMonoidalCategory.prodComparison, fst]
-
-lemma μ_pullback_left_fst_snd (X Y : Over S) :
-    (LaxMonoidal.μ (Over.pullback f) X Y).left ≫
-      pullback.fst _ _ ≫ pullback.snd _ _ = pullback.snd _ _ ≫ pullback.fst _ _ := by
-  rw [Monoidal.μ_of_cartesianMonoidalCategory,
-    ← cancel_epi (prodComparisonIso (Over.pullback f) X Y).hom.left,
-    ← Over.comp_left_assoc, Iso.hom_inv_id]
-  simp [CartesianMonoidalCategory.prodComparison, snd]
-
-lemma μ_pullback_left_snd (X Y : Over S) :
-    (LaxMonoidal.μ (Over.pullback f) X Y).left ≫ pullback.snd _ _ =
-      pullback.snd _ _ ≫ pullback.snd _ _ := by
-  rw [Monoidal.μ_of_cartesianMonoidalCategory,
-    ← cancel_epi (prodComparisonIso (Over.pullback f) X Y).hom.left,
-    ← Over.comp_left_assoc, Iso.hom_inv_id]
-  simp [CartesianMonoidalCategory.prodComparison]
-
-@[simp]
-lemma μ_pullback_left_fst_fst' {X Y : Scheme} (g₁ : X ⟶ S) (g₂ : Y ⟶ S) :
-    (LaxMonoidal.μ (Over.pullback f) (.mk g₁) (.mk g₂)).left ≫
-      pullback.fst (pullback.fst g₁ g₂ ≫ g₁) f ≫ pullback.fst g₁ g₂ =
-        pullback.fst _ _ ≫ pullback.fst _ _ :=
-  μ_pullback_left_fst_fst ..
-
-@[simp]
-lemma μ_pullback_left_fst_snd' {X Y : Scheme} (g₁ : X ⟶ S) (g₂ : Y ⟶ S) :
-    (LaxMonoidal.μ (Over.pullback f) (.mk g₁) (.mk g₂)).left ≫
-      pullback.fst (pullback.fst g₁ g₂ ≫ g₁) f ≫ pullback.snd g₁ g₂ =
-        pullback.snd _ _ ≫ pullback.fst _ _ :=
-  μ_pullback_left_fst_snd ..
-
-@[simp]
-lemma μ_pullback_left_snd' {X Y : Scheme} (g₁ : X ⟶ S) (g₂ : Y ⟶ S) :
-    (LaxMonoidal.μ (Over.pullback f) (.mk g₁) (.mk g₂)).left ≫
-      pullback.snd (pullback.fst g₁ g₂ ≫ g₁) f =
-        pullback.snd _ _ ≫ pullback.snd _ _ := μ_pullback_left_snd ..
-
-attribute [local simp] monObjAsOverPullback_one in
-instance isMonHom_fst_id_right [MonObj (asOver M S)] :
-    IsMonHom ((pullback.fst (M ↘ S) (𝟙 S)).asOver S) where
-  one_hom := by ext; simp [monObjAsOverPullback_one]
-  mul_hom := by
-    ext
-    dsimp [monObjAsOverPullback_mul]
-    simp only [Category.assoc, limit.lift_π, PullbackCone.mk_pt, PullbackCone.mk_π_app]
-    simp only [← Category.assoc]
-    congr 1
-    ext <;> simp [OverClass.asOver]
-
-@[simp]
-lemma preservesTerminalIso_pullback {R S : CommRingCat.{u}} (f : R ⟶ S) :
-    preservesTerminalIso (Over.pullback (Spec.map f)) =
-      Over.isoMk (asIso (pullback.snd (𝟙 _) (Spec.map f))) (by simp) := by
-  ext1; exact toUnit_unique _ _
-
-@[simp]
-lemma prodComparisonIso_pullback_Spec_inv_left_fst_fst {R S : CommRingCat.{u}} (f : R ⟶ S)
-    (A B : Over (Spec R)) :
-    (prodComparisonIso (Over.pullback (Spec.map f)) A B).inv.left ≫
-      pullback.fst (pullback.fst A.hom B.hom ≫ A.hom) (Spec.map f) ≫
-        pullback.fst _ _ = pullback.fst (pullback.snd A.hom (Spec.map f))
-          (pullback.snd B.hom (Spec.map f)) ≫ pullback.fst _ _ := by
-  rw [← cancel_epi (prodComparisonIso (Over.pullback (Spec.map f)) A B).hom.left,
-    Over.hom_left_inv_left_assoc]
-  simp [CartesianMonoidalCategory.prodComparison, fst]
-
-@[simp]
-lemma prodComparisonIso_pullback_Spec_inv_left_fst_fst' {R S : CommRingCat.{u}} (f : R ⟶ S)
-    {A B : Scheme.{u}} (fA : A ⟶ Spec R) (fB : B ⟶ Spec R) :
-    (prodComparisonIso (Over.pullback (Spec.map f)) (.mk fA) (.mk fB)).inv.left ≫
-      pullback.fst (pullback.fst fA fB ≫ fA) (Spec.map f) ≫
-        pullback.fst _ _ = pullback.fst (pullback.snd fA (Spec.map f))
-          (pullback.snd fB (Spec.map f)) ≫ pullback.fst _ _ :=
-  prodComparisonIso_pullback_Spec_inv_left_fst_fst ..
-
-@[simp]
-lemma prodComparisonIso_pullback_Spec_inv_left_fst_snd' {R S : CommRingCat.{u}} (f : R ⟶ S)
-    {A B : Scheme.{u}} (fA : A ⟶ Spec R) (fB : B ⟶ Spec R) :
-    (prodComparisonIso (Over.pullback (Spec.map f)) (.mk fA) (.mk fB)).inv.left ≫
-      pullback.fst (pullback.fst fA fB ≫ fA) (Spec.map f) ≫
-        pullback.snd _ _ = pullback.snd _ _ ≫ pullback.fst _ _ := by
-  rw [← cancel_epi (prodComparisonIso (Over.pullback (Spec.map f)) _ _).hom.left,
-    Over.hom_left_inv_left_assoc]
-  simp [CartesianMonoidalCategory.prodComparison, snd]
-
-@[simp]
-lemma prodComparisonIso_pullback_Spec_inv_left_snd' {R S : CommRingCat.{u}} (f : R ⟶ S)
-    {A B : Scheme.{u}} (fA : A ⟶ Spec R) (fB : B ⟶ Spec R) :
-    (by exact (prodComparisonIso (Over.pullback (Spec.map f)) (.mk fA) (.mk fB)).inv.left) ≫
-      pullback.snd (pullback.fst fA fB ≫ fA) (Spec.map f) =
-        pullback.snd _ _ ≫ pullback.snd _ _ := by
-  rw [← cancel_epi (prodComparisonIso (Over.pullback (Spec.map f)) _ _).hom.left,
-    Over.hom_left_inv_left_assoc]
-  simp [CartesianMonoidalCategory.prodComparison]
+variable (R S T : Type u) [CommRing R] [CommRing S] [CommRing T] [Algebra R S] [Algebra R T]
 
 @[reassoc (attr := simp)]
-lemma pullbackSpecIso_hom_base (R S T : Type u) [CommRing R] [CommRing S] [CommRing T] [Algebra R S]
-    [Algebra R T] :
+lemma pullbackSpecIso_hom_base :
     (pullbackSpecIso R S T).hom ≫ Spec.map (CommRingCat.ofHom (algebraMap R _)) =
       pullback.fst _ _ ≫ Spec.map (CommRingCat.ofHom (algebraMap _ _)) := by
   simp [Algebra.TensorProduct.algebraMap_def]
 
 @[reassoc (attr := simp)]
-lemma pullbackSpecIso_hom_fst' (R S T : Type u) [CommRing R] [CommRing S] [CommRing T] [Algebra R S]
-    [Algebra R T] :
+lemma pullbackSpecIso_hom_fst' :
     (pullbackSpecIso R S T).hom ≫ Spec.map (CommRingCat.ofHom (algebraMap S _)) =
       pullback.fst _ _ := by
   simp [Algebra.TensorProduct.algebraMap_def]
 
 @[reassoc]
-lemma pullbackSpecIso_inv_fst' (R S T : Type u) [CommRing R] [CommRing S] [CommRing T] [Algebra R S]
-    [Algebra R T] :
+lemma pullbackSpecIso_inv_fst' :
     (pullbackSpecIso R S T).inv ≫ pullback.fst _ _ =
       Spec.map (CommRingCat.ofHom (algebraMap S _)) := by
   simp [← cancel_epi (pullbackSpecIso R S T).hom]
--- Mathlib/CategoryTheory/Monoidal/Cartesian/Over.lean
+++ Mathlib/CategoryTheory/Monoidal/Cartesian/Over.lean
@@ -6,7 +6,12 @@ Authors: Andrew Yang
 module
 
 public import Mathlib.CategoryTheory.Monoidal.Cartesian.Basic
+public import Mathlib.CategoryTheory.Comma.Over.OverClass
+public import Mathlib.CategoryTheory.Comma.Over.Pullback
 public import Mathlib.CategoryTheory.Limits.Constructions.Over.Products
+public import Mathlib.CategoryTheory.Monoidal.CommMon_
+public import Mathlib.CategoryTheory.Monoidal.Grp_
+public import Mathlib.CategoryTheory.Adjunction.Limits
 
 /-!
 
@@ -17,28 +22,29 @@ for the induced `MonoidalCategory (Over X)` instance.
 
 -/
 
-@[expose] public section
+public noncomputable section
 
 namespace CategoryTheory.Over
 
-open Limits CartesianMonoidalCategory
+open Functor Limits CartesianMonoidalCategory OverClass
 
 variable {C : Type*} [Category C] [HasPullbacks C]
 
 /-- A choice of finite products of `Over X` given by `Limits.pullback`. -/
-noncomputable abbrev cartesianMonoidalCategory (X : C) : CartesianMonoidalCategory (Over X) :=
+abbrev cartesianMonoidalCategory (X : C) : CartesianMonoidalCategory (Over X) :=
   .ofChosenFiniteProducts
     ⟨asEmptyCone (Over.mk (𝟙 X)), IsTerminal.ofUniqueHom (fun Y ↦ Over.homMk Y.hom)
       fun Y m ↦ Over.OverMorphism.ext (by simpa using m.w)⟩
     fun Y Z ↦ ⟨pullbackConeEquivBinaryFan.functor.obj (pullback.cone Y.hom Z.hom),
     (pullback.isLimit _ _).pullbackConeEquivBinaryFanFunctor⟩
 
-@[deprecated (since := "2025-05-15")] alias chosenFiniteProducts := cartesianMonoidalCategory
+@[deprecated (since := "2025-05-15")]
+noncomputable alias chosenFiniteProducts := cartesianMonoidalCategory
 
 attribute [local instance] cartesianMonoidalCategory
 
 /-- `Over X` is braided w.r.t. the Cartesian monoidal structure given by `Limits.pullback`. -/
-noncomputable abbrev braidedCategory (X : C) : BraidedCategory (Over X) :=
+abbrev braidedCategory (X : C) : BraidedCategory (Over X) :=
   .ofCartesianMonoidalCategory
 
 attribute [local instance] braidedCategory
@@ -54,7 +60,7 @@ lemma tensorObj_ext {R : C} {S T : Over X} (f₁ f₂ : R ⟶ (S ⊗ T).left)
   pullback.hom_ext e₁ e₂
 
 @[simp]
-lemma tensorObj_left (R S : Over X) : (R ⊗ S).left = pullback R.hom S.hom := rfl
+lemma tensorObj_left (R S : Over X) : (R ⊗ S).left = Limits.pullback R.hom S.hom := rfl
 
 @[simp]
 lemma tensorObj_hom (R S : Over X) : (R ⊗ S).hom = pullback.fst R.hom S.hom ≫ R.hom := rfl
@@ -184,5 +190,131 @@ lemma braiding_hom_left {R S : Over X} :
 @[simp]
 lemma braiding_inv_left {R S : Over X} :
     (β_ R S).inv.left = (pullbackSymmetry _ _).hom := rfl
+
+variable {A B R S Y Z : C} [OverClass R X] {f : S ⟶ X}
+
+instance : (Over.pullback f).Braided := .ofChosenFiniteProducts _
+
+@[simps]
+instance canonicallyOverPullback : CanonicallyOverClass (Limits.pullback (R ↘ X) f) S where
+  hom := pullback.snd (R ↘ X) f
+
+@[simps! -isSimp mul one]
+instance monObjAsOverPullback [MonObj (asOver R X)] :
+    MonObj (asOver (Limits.pullback (R ↘ X) f) S) :=
+  ((Over.pullback f).mapMon.obj <| .mk <| asOver R X).mon
+
+instance isCommMonObj_asOver_pullback [MonObj (asOver R X)] [IsCommMonObj (asOver R X)] :
+    IsCommMonObj (asOver (Limits.pullback (R ↘ X) f) S) :=
+  ((Over.pullback f).mapCommMon.obj <| .mk <| asOver R X).comm
+
+instance GrpObjAsOverPullback [GrpObj (asOver R X)] :
+    GrpObj (asOver (Limits.pullback (R ↘ X) f) S) :=
+  ((Over.pullback f).mapGrp.obj <| .mk <| asOver R X).grp
+
+instance : HomIsOver (pullback.fst (R ↘ X) (𝟙 X)) X := ⟨pullback.condition.trans <| by simp⟩
+
+@[simp]
+lemma η_pullback_left : (OplaxMonoidal.η (Over.pullback f)).left = (pullback.snd (𝟙 _) f) := rfl
+
+@[simp]
+lemma ε_pullback_left : (LaxMonoidal.ε (Over.pullback f)).left = inv (pullback.snd (𝟙 _) f) := by
+  apply IsIso.eq_inv_of_hom_inv_id
+  rw [← η_pullback_left, ← Over.comp_left, Monoidal.η_ε, Over.id_left]
+
+lemma μ_pullback_left_fst_fst (R S : Over X) :
+    (LaxMonoidal.μ (Over.pullback f) R S).left ≫
+      pullback.fst _ _ ≫ pullback.fst _ _ = pullback.fst _ _ ≫ pullback.fst _ _ := by
+  rw [Monoidal.μ_of_cartesianMonoidalCategory,
+    ← cancel_epi (prodComparisonIso (Over.pullback f) R S).hom.left, ← Over.comp_left_assoc,
+    Iso.hom_inv_id]
+  simp [CartesianMonoidalCategory.prodComparison, fst]
+
+lemma μ_pullback_left_fst_snd (R S : Over X) :
+    (LaxMonoidal.μ (Over.pullback f) R S).left ≫
+      pullback.fst _ _ ≫ pullback.snd _ _ = pullback.snd _ _ ≫ pullback.fst _ _ := by
+  rw [Monoidal.μ_of_cartesianMonoidalCategory,
+    ← cancel_epi (prodComparisonIso (Over.pullback f) R S).hom.left,
+    ← Over.comp_left_assoc, Iso.hom_inv_id]
+  simp [CartesianMonoidalCategory.prodComparison, snd]
+
+lemma μ_pullback_left_snd (R S : Over X) :
+    (LaxMonoidal.μ (Over.pullback f) R S).left ≫ pullback.snd _ _ =
+      pullback.snd _ _ ≫ pullback.snd _ _ := by
+  rw [Monoidal.μ_of_cartesianMonoidalCategory,
+    ← cancel_epi (prodComparisonIso (Over.pullback f) R S).hom.left,
+    ← Over.comp_left_assoc, Iso.hom_inv_id]
+  simp [CartesianMonoidalCategory.prodComparison]
+
+@[simp]
+lemma μ_pullback_left_fst_fst' (g₁ : Y ⟶ X) (g₂ : Z ⟶ X) :
+    (LaxMonoidal.μ (Over.pullback f) (.mk g₁) (.mk g₂)).left ≫
+      pullback.fst (pullback.fst g₁ g₂ ≫ g₁) f ≫ pullback.fst g₁ g₂ =
+        pullback.fst _ _ ≫ pullback.fst _ _ :=
+  μ_pullback_left_fst_fst ..
+
+@[simp]
+lemma μ_pullback_left_fst_snd' (g₁ : Y ⟶ X) (g₂ : Z ⟶ X) :
+    (LaxMonoidal.μ (Over.pullback f) (.mk g₁) (.mk g₂)).left ≫
+      pullback.fst (pullback.fst g₁ g₂ ≫ g₁) f ≫ pullback.snd g₁ g₂ =
+        pullback.snd _ _ ≫ pullback.fst _ _ :=
+  μ_pullback_left_fst_snd ..
+
+@[simp]
+lemma μ_pullback_left_snd' (g₁ : Y ⟶ X) (g₂ : Z ⟶ X) :
+    (LaxMonoidal.μ (Over.pullback f) (.mk g₁) (.mk g₂)).left ≫
+      pullback.snd (pullback.fst g₁ g₂ ≫ g₁) f =
+        pullback.snd _ _ ≫ pullback.snd _ _ := μ_pullback_left_snd ..
+
+attribute [local simp] monObjAsOverPullback_one in
+instance isMonHom_fst_id_right [MonObj (asOver R X)] :
+    IsMonHom <| asOverHom X <| pullback.fst (R ↘ X) (𝟙 X) where
+  one_hom := by ext; simp [monObjAsOverPullback_one]
+  mul_hom := by
+    ext
+    dsimp [monObjAsOverPullback_mul]
+    simp only [Category.assoc, limit.lift_π, PullbackCone.mk_pt, PullbackCone.mk_π_app]
+    simp only [← Category.assoc]
+    congr 1
+    ext <;> simp [OverClass.asOver]
+
+@[simp]
+lemma preservesTerminalIso_pullback (f : R ⟶ S) :
+    preservesTerminalIso (Over.pullback f) =
+      Over.isoMk (asIso (pullback.snd (𝟙 _) f)) (by simp) := by
+  ext1; exact toUnit_unique _ _
+
+@[simp]
+lemma prodComparisonIso_pullback_inv_left_fst_fst (f : X ⟶ Y) (A B : Over Y) :
+    (prodComparisonIso (Over.pullback f) A B).inv.left ≫
+      pullback.fst (pullback.fst A.hom B.hom ≫ A.hom) f ≫ pullback.fst _ _ =
+        pullback.fst (pullback.snd A.hom f) (pullback.snd B.hom f) ≫ pullback.fst _ _ := by
+  rw [← cancel_epi (prodComparisonIso (Over.pullback f) A B).hom.left,
+    Over.hom_left_inv_left_assoc]
+  simp [CartesianMonoidalCategory.prodComparison, fst]
+
+@[simp]
+lemma prodComparisonIso_pullback_Spec_inv_left_fst_fst' (f : X ⟶ Y) (gA : A ⟶ Y) (gB : B ⟶ Y) :
+    (prodComparisonIso (Over.pullback f) (.mk gA) (.mk gB)).inv.left ≫
+      pullback.fst (pullback.fst gA gB ≫ gA) f ≫ pullback.fst _ _ =
+        pullback.fst (pullback.snd gA f) (pullback.snd gB f) ≫ pullback.fst _ _ :=
+  prodComparisonIso_pullback_inv_left_fst_fst ..
+
+@[simp]
+lemma prodComparisonIso_pullback_inv_left_fst_snd' (f : X ⟶ Y) (gA : A ⟶ Y) (gB : B ⟶ Y) :
+    (prodComparisonIso (Over.pullback f) (.mk gA) (.mk gB)).inv.left ≫
+      pullback.fst (pullback.fst gA gB ≫ gA) f ≫ pullback.snd _ _ =
+        pullback.snd _ _ ≫ pullback.fst _ _ := by
+  rw [← cancel_epi (prodComparisonIso (Over.pullback f) _ _).hom.left,
+    Over.hom_left_inv_left_assoc]
+  simp [CartesianMonoidalCategory.prodComparison, snd]
+
+@[simp]
+lemma prodComparisonIso_pullback_inv_left_snd' (f : X ⟶ Y) (gA : A ⟶ Y) (gB : B ⟶ Y) :
+    (prodComparisonIso (Over.pullback f) (.mk gA) (.mk gB)).inv.left ≫
+      pullback.snd (pullback.fst gA gB ≫ gA) f = pullback.snd _ _ ≫ pullback.snd _ _ := by
+  rw [← cancel_epi (prodComparisonIso (Over.pullback f) _ _).hom.left,
+    Over.hom_left_inv_left_assoc]
+  simp [CartesianMonoidalCategory.prodComparison]
 
 end CategoryTheory.Over
