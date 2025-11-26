20ac114ce345e8bb04ac859e634c4e8cd50febed
mitchell-horner <29882987+mitchell-horner@users.noreply.github.com>
2025-11-26 21:11:10 +1100

    Replace `IsTuranMaximal.le_iff_eq` with `IsExtremal.le_iff_eq`

--- Mathlib/Combinatorics/SimpleGraph/Extremal/Turan.lean
+++ Mathlib/Combinatorics/SimpleGraph/Extremal/Turan.lean
@@ -57,12 +57,7 @@ def IsTuranMaximal (r : ℕ) : Prop := G.IsExtremal (CliqueFree · (r + 1))
 
 section Defs
 
-variable {H : SimpleGraph V} [DecidableRel H.Adj]
-
-lemma IsTuranMaximal.le_iff_eq (hG : G.IsTuranMaximal r) (hH : H.CliqueFree (r + 1)) :
-    G ≤ H ↔ G = H :=
-  ⟨fun hGH ↦ edgeFinset_inj.1 <|
-    eq_of_subset_of_card_le (edgeFinset_subset_edgeFinset.2 hGH) (hG.2 hH), le_of_eq⟩
+variable {H : SimpleGraph V}
 
 /-- The canonical `r + 1`-cliquefree Turán graph on `n` vertices. -/
 def turanGraph (n r : ℕ) : SimpleGraph (Fin n) where Adj v w := v % r ≠ w % r
@@ -106,7 +101,6 @@ theorem not_cliqueFree_of_isTuranMaximal (hn : r ≤ card V) (hG : G.IsTuranMaxi
   obtain ⟨a, -, b, -, hab, hGab⟩ : ∃ a ∈ K, ∃ b ∈ K, a ≠ b ∧ ¬ G.Adj a b := by
     simpa only [isNClique_iff, IsClique, Set.Pairwise, mem_coe, ne_eq, and_true, not_forall,
       exists_prop, exists_and_right] using h K
-  classical
   exact hGab <| le_sup_right.trans_eq ((hG.le_iff_eq <| h.sup_edge _ _).1 le_sup_left).symm <|
     (edge_adj ..).2 ⟨Or.inl ⟨rfl, rfl⟩, hab⟩
 
