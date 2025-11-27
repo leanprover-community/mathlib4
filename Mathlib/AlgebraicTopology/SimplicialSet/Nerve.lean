a3ad327fe97d943bda9d101d6ccc551491a87be0
Kim Morrison <kim@tqft.net>
2025-11-27 11:11:13 +0000

    fix bad merge?

--- Mathlib/AlgebraicTopology/SimplicialSet/Nerve.lean
+++ Mathlib/AlgebraicTopology/SimplicialSet/Nerve.lean
@@ -56,9 +56,9 @@ def nerveFunctor : Cat.{v, u} ⥤ SSet where
   map F := nerveMap F
 
 /-- The 0-simplices of the nerve of a category are equivalent to the objects of the category. -/
-def nerveEquiv (C : Type u) [Category.{v} C] : nerve C _⦋0⦌ ≃ C where
-  toFun f := f.obj ⟨0, by lia⟩
-  invFun f := (Functor.const _).obj f
+def nerveEquiv {C : Type u} [Category.{v} C] : ComposableArrows C 0 ≃ C where
+  toFun f := f.obj ⟨0, by omega⟩
+  invFun f := ComposableArrows.mk₀ f
   left_inv f := ComposableArrows.ext₀ rfl
 
 namespace nerve
