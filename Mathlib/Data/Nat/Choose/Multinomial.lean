fab48d2e4f80e3843a028cd8dd2f82198724fa36
ntlean <midff@math.uni-bonn.de>
2025-11-27 10:50:49 +0100

    suggested change

--- Mathlib/Data/Nat/Choose/Multinomial.lean
+++ Mathlib/Data/Nat/Choose/Multinomial.lean
@@ -156,9 +156,7 @@ theorem multinomial_eq_of_support_subset {f : α →₀ ℕ} {s : Finset α} (h 
   congr 1
   · simp [Finset.sum_subset h]
   · rw [Finset.prod_subset h]
-    intro x _
-    simp only [Finsupp.mem_support_iff, ne_eq, Decidable.not_not, Nat.factorial_eq_one]
-    grind
+    grind [Finsupp.mem_support_iff, Nat.factorial_eq_one]
 
 theorem multinomial_update (a : α) (f : α →₀ ℕ) :
     f.multinomial = (f.sum fun _ => id).choose (f a) * (f.update a 0).multinomial := by
