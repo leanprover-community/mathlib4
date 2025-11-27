10943bf825385cf6d88ff00939b419c0c6dcb2f8
bwangpj <70694994+bwangpj@users.noreply.github.com>
2025-11-27 08:50:32 -0500

    Update Subrepresentation.lean

--- Mathlib/RepresentationTheory/Subrepresentation.lean
+++ Mathlib/RepresentationTheory/Subrepresentation.lean
@@ -3,8 +3,10 @@ Copyright (c) 2025 FLT Project. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: FLT Project
 -/
-import Mathlib.RepresentationTheory.Basic
-import Mathlib.LinearAlgebra.Span.Defs
+module
+
+public import Mathlib.RepresentationTheory.Basic
+public import Mathlib.LinearAlgebra.Span.Defs
 
 /-!
 # Subrepresentations
@@ -12,6 +14,8 @@ import Mathlib.LinearAlgebra.Span.Defs
 This file defines subrepresentations of a group representation.
 
 -/
+
+@[expose] public section
 
 open Pointwise
 
