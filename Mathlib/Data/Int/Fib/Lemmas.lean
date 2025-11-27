c62a0b000579ecd3d8f312bbdd1716894fe43ac6
Monica Omar <2497250a@research.gla.ac.uk>
2025-11-27 21:48:26 +0000

    final final cleanup

--- Mathlib/Data/Int/Fib/Lemmas.lean
+++ Mathlib/Data/Int/Fib/Lemmas.lean
@@ -27,10 +27,10 @@ lemma fib_natCast_succ_mul_fib_natCast_pred_sub_fib_natCast_sq (n : ℕ) :
 /-- **Cassini's identity**: `fib (n + 1) * fib (n - 1) - fib n ^ 2 = (-1) ^ |n|`. -/
 public theorem fib_succ_mul_fib_pred_sub_fib_sq (n : ℤ) :
     fib (n + 1) * fib (n - 1) - fib n ^ 2 = (-1) ^ n.natAbs := by
-  if hn : n = 0 then simp [hn] else
   obtain ⟨n, (rfl | rfl)⟩ := n.eq_nat_or_neg
   · exact fib_natCast_succ_mul_fib_natCast_pred_sub_fib_natCast_sq n
-  · obtain ⟨i, rfl⟩ := Nat.exists_eq_add_one_of_ne_zero (by simpa using hn)
+  · if hn : n = 0 then simp [hn] else
+    obtain ⟨i, rfl⟩ := Nat.exists_eq_add_one_of_ne_zero (by simpa using hn)
     simp_rw [show -((i + 1 : ℕ) : ℤ) + 1 = -i by simp, sub_eq_add_neg, ← neg_add, fib_neg,
       natAbs_neg, natAbs_natCast, ← fib_natCast_succ_mul_fib_natCast_pred_sub_fib_natCast_sq]
     grind
@@ -42,13 +42,9 @@ public theorem fib_add_sq_sub_fib_mul_fib_add_two_mul (x a : ℤ) :
     _ = (fib x * fib (a + 1) + fib (x - 1) * fib a) ^ 2 -
           fib x * (fib x * (fib (a + 1) ^ 2 + fib a ^ 2) +
             fib (x - 1) * (fib a * fib (a + 1) + fib (a - 1) * fib a)) := by
-      congr 2
-      · simp only [fib_add, fib_one, mul_one, reduceAdd, fib_two]
-        ring
-      · simp_rw [add_comm x, two_mul]
-        simp only [fib_add, ← add_sub, sub_add_cancel, fib_one, mul_one, reduceAdd, fib_two]
-        grind
-    _ = fib a ^ 2 * (fib (x - 1) ^ 2 + fib x * fib (x - 1) - fib x ^ 2) := by grind [fib_add_two]
+      rw [add_comm x]
+      simp only [two_mul, fib_add, fib_one, mul_one, reduceAdd, fib_two, ← add_sub, sub_add_cancel]
+      ring
     _ = _ := by grind [fib_succ_mul_fib_pred_sub_fib_sq, fib_add_two]
 
 end Int
