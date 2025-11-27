2d82cee86e29676f400678f25f5cf130625ddc67
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2025-11-27 11:19:33 +0100

    mv lemmas on finrank

--- Mathlib/LinearAlgebra/SpecialLinearGroup.lean
+++ Mathlib/LinearAlgebra/SpecialLinearGroup.lean
@@ -40,6 +40,7 @@ associated with a finite basis of `V`.
 
 -/
 
+/-
 -- Four lemmas on rank, finrank
 -- TODO : move elsewhere
 theorem Module.rank_pos_iff_of_free {R M : Type*} [Ring R] [Nontrivial R]
@@ -50,7 +51,14 @@ theorem Module.rank_pos_iff_of_free {R M : Type*} [Ring R] [Nontrivial R]
   intro h'
   simp only [rank_subsingleton', lt_self_iff_false] at h
 
+#find_home! Module.rank_pos_iff_of_free
 theorem Module.rank_zero_iff_of_free {R M : Type*} [Ring R] [Nontrivial R]
+    [AddCommGroup M] [Module R M] [Module.Free R M] :
+    Module.rank R M = 0 ↔ Subsingleton M := by
+  rw [← not_nontrivial_iff_subsingleton, iff_not_comm,
+    ← Module.rank_pos_iff_of_free (R := R), pos_iff_ne_zero]
+
+theorem Module.rank_zero_iff_of_free' {R M : Type*} [Ring R] [Nontrivial R]
     [AddCommGroup M] [Module R M] [Module.Free R M] :
     Module.rank R M = 0 ↔ Subsingleton M := by
   rw [← not_nontrivial_iff_subsingleton, iff_not_comm,
@@ -59,6 +67,8 @@ theorem Module.rank_zero_iff_of_free {R M : Type*} [Ring R] [Nontrivial R]
 theorem Module.finrank_eq_zero_iff_of_free {R M : Type*} [CommRing R] [Nontrivial R]
     [AddCommGroup M] [Module R M] [Module.Free R M] [Module.Finite R M] :
     Module.finrank R M = 0 ↔ Subsingleton M := by
+  have : StrongRankCondition R := by
+    exact strongRankCondition_of_orzechProperty R
   have := Module.rank_lt_aleph0 R M
   rw [← not_le] at this
   simp [Module.finrank, this, Module.rank_zero_iff_of_free]
@@ -68,6 +78,7 @@ theorem Module.finrank_pos_iff_of_free {R M : Type*} [CommRing R] [Nontrivial R]
     0 < Module.finrank R M ↔ Nontrivial M := by
   rw [← not_subsingleton_iff_nontrivial, ← iff_not_comm]
   simp [Module.finrank_eq_zero_iff_of_free]
+-/
 
 @[expose] public section
 
@@ -544,7 +555,7 @@ open Subgroup Matrix Matrix.SpecialLinearGroup
 
 variable {n : Type*} [Fintype n] [DecidableEq n] {R : Type*} [CommRing R]
 
-variable [Nontrivial R]
+-- variable [Nontrivial R]
 variable {V : Type*} [AddCommGroup V] [Module R V] [Module.Free R V] [Module.Finite R V]
 variable {ι : Type*} [Fintype ι] [DecidableEq ι] (b : Module.Basis ι R V)
 
