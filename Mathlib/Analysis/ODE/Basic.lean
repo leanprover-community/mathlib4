4ea72eec9c6b104a857e37b663f96768d92545fd
Winston Yin <winstonyin@gmail.com>
2025-11-27 14:09:06 -0500

    fix import

--- Mathlib/Analysis/ODE/Basic.lean
+++ Mathlib/Analysis/ODE/Basic.lean
@@ -3,7 +3,9 @@ Copyright (c) 2023 Winston Yin. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Winston Yin
 -/
-import Mathlib.Analysis.Calculus.Deriv.Basic
+module
+
+public import Mathlib.Analysis.Calculus.Deriv.Basic
 
 /-!
 # Integral curves of vector fields on a Banach space
