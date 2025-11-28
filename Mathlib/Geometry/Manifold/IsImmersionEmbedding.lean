eeb7d8673022074a88f683c45396b0b4ac0f028e
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-11-28 12:43:42 +0100

    Remove the empty case for now

--- Mathlib/Geometry/Manifold/IsImmersionEmbedding.lean
+++ Mathlib/Geometry/Manifold/IsImmersionEmbedding.lean
@@ -556,12 +556,7 @@ Note that the proof contains a small formalisation-related subtlety: `F` can liv
 while being an immersion requires the existence of a complement in the same universe as
 the model normed space of `N`. This is solved by `smallComplement` and `smallEquiv`.
 -/
-lemma isImmersion (h : IsImmersionOfComplement F I J n f) : IsImmersion I J n f := by
-  by_cases! hM : IsEmpty M
-  · rw [IsImmersion]
-    -- TODO: need to find a dummy candidate here... or copy-paste the same smallness lemmas...
-    use sorry, sorry, sorry
-    exact fun x ↦ (IsEmpty.false x).elim
+lemma isImmersion [Nonempty M] (h : IsImmersionOfComplement F I J n f) : IsImmersion I J n f := by
   inhabit M
   let x : M := Inhabited.default
   use (h x).smallComplement, by infer_instance, by infer_instance
