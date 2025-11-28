c376679b9199871ba8298b4d8d40749da27d810c
Monica Omar <2497250a@research.gla.ac.uk>
2025-11-28 18:05:45 +0000

    var

--- Mathlib/Algebra/Central/End.lean
+++ Mathlib/Algebra/Central/End.lean
@@ -18,16 +18,16 @@ public import Mathlib.LinearAlgebra.FreeModule.Basic
 
 open Module Free
 
-variable {K V : Type*} [CommSemiring K] [AddCommMonoid V] [Module K V] [Free K V]
+variable {R V : Type*} [CommSemiring R] [AddCommMonoid V] [Module R V] [Free R V]
 
 /-- The center of endomorphisms on a vector space is trivial,
 in other words, it is a central algebra. -/
-instance Algebra.IsCentral.end : IsCentral K (End K V) where
+instance Algebra.IsCentral.end : IsCentral R (End R V) where
   out T hT := by
-    have h' (f : Dual K V) (y v : V) : f (T v) • y = f v • T y := by
+    have h' (f : Dual R V) (y v : V) : f (T v) • y = f v • T y := by
       simpa using congr($(Subalgebra.mem_center_iff.mp hT <| f.smulRight y) v)
     nontriviality V
     obtain ⟨x, hx⟩ := exists_ne (0 : V)
-    let b := chooseBasis K V
+    let b := chooseBasis R V
     have ⟨i, hi⟩ := not_forall.mp fun h ↦ b.repr.map_ne_zero_iff.mpr hx <| Finsupp.ext h
     exact ⟨b.coord i (T (b i)), LinearMap.ext fun y ↦ by simpa using h' (b.coord i) y (b i)⟩
