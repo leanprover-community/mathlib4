d400ee39ded815062ab883d745f9852a048d41df
Etienne Marion <baguettes_casino0c@icloud.com>
2025-11-28 20:58:41 +0100

    wip

--- Mathlib/Probability/Distributions/Gaussian/HasGaussianLaw.lean
+++ Mathlib/Probability/Distributions/Gaussian/HasGaussianLaw.lean
@@ -35,51 +35,6 @@ public section
 
 namespace ProbabilityTheory
 
-section HasLaw
-
-variable {𝓧} {m𝓧 : MeasurableSpace 𝓧} {X : Ω → 𝓧} {μ : Measure 𝓧} {P : Measure Ω}
-
-lemma hasLaw_map (hX : AEMeasurable X P) : HasLaw X (P.map X) P where
-  aemeasurable := hX
-  map_eq := rfl
-
-section dirac
-
-lemma HasLaw.ae_eq_of_dirac' {𝓧 : Type*} {m𝓧 : MeasurableSpace 𝓧} [MeasurableSingletonClass 𝓧]
-    {x : 𝓧} {X : Ω → 𝓧}
-    (hX : HasLaw X (.dirac x) P) : X =ᵐ[P] (fun _ ↦ x) := by
-  apply ae_of_ae_map (p := fun y ↦ y = x) hX.aemeasurable
-  rw [hX.map_eq, ae_dirac_iff]
-  simp
-
-lemma HasLaw.ae_eq_of_dirac {𝓧 : Type*} {m𝓧 : MeasurableSpace 𝓧} [MeasurableSingletonClass 𝓧]
-    {x : 𝓧} {X : Ω → 𝓧}
-    (hX : HasLaw X (.dirac x) P) : ∀ᵐ ω ∂P, X ω = x := hX.ae_eq_of_dirac'
-
-lemma HasLaw.ae_eq_const_of_gaussianReal {X : Ω → ℝ} {μ : ℝ} (hX : HasLaw X (gaussianReal μ 0) P) :
-    ∀ᵐ ω ∂P, X ω = μ := by
-  rw [gaussianReal_zero_var] at hX
-  exact hX.ae_eq_of_dirac
-
-end dirac
-
-variable {E : Type*} [NormedAddCommGroup E] [MeasurableSpace E] [BorelSpace E]
-    [SecondCountableTopology E] [IsProbabilityMeasure P] {X Y : Ω → E}
-
-lemma IndepFun.charFunDual_map_add_eq_mul [NormedSpace ℝ E]
-    (mX : AEMeasurable X P) (mY : AEMeasurable Y P) (hXY : IndepFun X Y P) :
-    charFunDual (P.map (X + Y)) = charFunDual (P.map X) * charFunDual (P.map Y) := by
-  ext L
-  rw [(hXY.hasLaw_add (hasLaw_map mX) (hasLaw_map mY)).map_eq, charFunDual_conv, Pi.mul_apply]
-
-lemma IndepFun.charFun_map_add_eq_mul [InnerProductSpace ℝ E]
-    (mX : AEMeasurable X P) (mY : AEMeasurable Y P) (hXY : IndepFun X Y P) :
-    charFun (P.map (X + Y)) = charFun (P.map X) * charFun (P.map Y) := by
-  ext t
-  rw [(hXY.hasLaw_add (hasLaw_map mX) (hasLaw_map mY)).map_eq, charFun_conv, Pi.mul_apply]
-
-end HasLaw
-
 section Basic
 
 variable [TopologicalSpace E] [AddCommMonoid E] [Module ℝ E] [mE : MeasurableSpace E]
