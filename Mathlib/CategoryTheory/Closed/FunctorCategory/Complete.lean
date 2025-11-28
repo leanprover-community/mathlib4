19b5e58354dcca4c918b9b8f10b5e8b56b4e6cdc
Dagur Asgeirsson <dagurtomas@gmail.com>
2025-11-28 08:55:56 -0700

    chore(CategoryTheory/Closed): deprecate modules

---
+++ Mathlib/CategoryTheory/Closed/FunctorCategory/Complete.lean
@@ -0,0 +1,5 @@
+module
+
+public import Mathlib.CategoryTheory.Monoidal.Closed.FunctorCategory.Complete
+
+deprecated_module (since := "2025-11-28")
---
+++ Mathlib/CategoryTheory/Closed/Monoidal.lean
@@ -0,0 +1,7 @@
+module
+
+public import Mathlib.CategoryTheory.Monoidal.Closed.Basic
+
+@[expose] public section
+
+deprecated_module (since := "2025-11-28")
