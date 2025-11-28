c2d7010c52c20577944da772b56db3b82d95d03c
Junyan Xu <junyanxu.math@gmail.com>
2025-11-28 13:53:49 +0100

    golfs

--- Mathlib/RingTheory/Valuation/AlgebraInstances.lean
+++ Mathlib/RingTheory/Valuation/AlgebraInstances.lean
@@ -33,7 +33,7 @@ variable {K : Type*} [Field K] (v : Valuation K ℤᵐ⁰) (L : Type*) [Field L]
 
 namespace ValuationSubring
 
-instance : Algebra v.valuationSubring L := fast_instance% inferInstance
+instance : Algebra v.valuationSubring L := inferInstance
 
 theorem algebraMap_injective : Injective (algebraMap v.valuationSubring L) :=
   (FaithfulSMul.algebraMap_injective K L).comp (IsFractionRing.injective _ _)
@@ -42,16 +42,17 @@ theorem isIntegral_of_mem_ringOfIntegers {x : L} (hx : x ∈ integralClosure v.v
     IsIntegral v.valuationSubring (⟨x, hx⟩ : integralClosure v.valuationSubring L) :=
   integralClosure.isIntegral ⟨x, hx⟩
 
-theorem isIntegral_of_mem_ringOfIntegers' {x : (integralClosure v.valuationSubring L)} :
+theorem isIntegral_of_mem_ringOfIntegers' {x : integralClosure v.valuationSubring L} :
     IsIntegral v.valuationSubring (x : integralClosure v.valuationSubring L) := by
   apply isIntegral_of_mem_ringOfIntegers
 
 variable (E : Type _) [Field E] [Algebra K E] [Algebra L E] [IsScalarTower K L E]
 
-instance : IsScalarTower v.valuationSubring L E := Subring.instIsScalarTowerSubtypeMem _
+instance : IsScalarTower v.valuationSubring L E := inferInstance
 
 /-- Given an algebra between two field extensions `L` and `E` of a field `K` with a valuation `v`,
   create an algebra between their two rings of integers. -/
+-- TODO: fix the smul field
 instance algebra :
     Algebra (integralClosure v.valuationSubring L) (integralClosure v.valuationSubring E) :=
   RingHom.toAlgebra
@@ -68,14 +69,12 @@ instance algebra :
   and a ring `R` satisfying `isIntegralClosure R v.valuationSubring L`. -/
 protected noncomputable def equiv (R : Type*) [CommRing R] [Algebra v.valuationSubring R]
     [Algebra R L] [IsScalarTower v.valuationSubring R L]
-    [IsIntegralClosure R v.valuationSubring L] : integralClosure v.valuationSubring L ≃+* R := by
-  have := IsScalarTower.subalgebra' (valuationSubring v) L L
-    (integralClosure (valuationSubring v) L)
-  exact (IsIntegralClosure.equiv v.valuationSubring R L
+    [IsIntegralClosure R v.valuationSubring L] : integralClosure v.valuationSubring L ≃+* R :=
+  (IsIntegralClosure.equiv v.valuationSubring R L
     (integralClosure v.valuationSubring L)).symm.toRingEquiv
 
 theorem integralClosure_algebraMap_injective :
     Injective (algebraMap v.valuationSubring (integralClosure v.valuationSubring L)) :=
-  FaithfulSMul.algebraMap_injective ↥v.valuationSubring ↥(integralClosure (↥v.valuationSubring) L)
+  FaithfulSMul.algebraMap_injective ..
 
 end ValuationSubring
