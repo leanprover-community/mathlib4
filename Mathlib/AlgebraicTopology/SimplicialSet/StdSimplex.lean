306a537d351873bd5c26aaced52462e47c18b76c
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-11-28 09:31:49 +0100

    feat(AlgebraicTopology): finite simplicial sets

--- Mathlib/AlgebraicTopology/SimplicialSet/StdSimplex.lean
+++ Mathlib/AlgebraicTopology/SimplicialSet/StdSimplex.lean
@@ -5,7 +5,7 @@ Authors: Johan Commelin, Kim Morrison, Adam Topaz, Joël Riou
 -/
 module
 
-public import Mathlib.AlgebraicTopology.SimplicialSet.Dimension
+public import Mathlib.AlgebraicTopology.SimplicialSet.Finite
 public import Mathlib.AlgebraicTopology.SimplicialSet.NerveNondegenerate
 public import Mathlib.Data.Fin.VecNotation
 public import Mathlib.Order.Fin.SuccAboveOrderIso
@@ -322,6 +322,21 @@ instance (n : ℕ) : (Δ[n] : SSet.{u}).HasDimensionLE n where
     have := SimplexCategory.len_le_of_mono (objEquiv x)
     dsimp at this
     cutsat
+
+instance (n : SimplexCategory) (d : SimplexCategoryᵒᵖ) :
+    Finite ((stdSimplex.{u}.obj n).obj d) := by
+  rw [objEquiv.finite_iff]
+  infer_instance
+
+instance (n : SimplexCategory) : (stdSimplex.{u}.obj n).Finite := by
+  induction n using SimplexCategory.rec with | _ n
+  exact finite_of_hasDimensionLT _ (n + 1) inferInstance
+
+instance {X : SSet.{u}} {n : ℕ} (x : X _⦋n⦌) :
+    SSet.Finite (Subcomplex.ofSimplex x) := by
+  obtain ⟨f, rfl⟩ := yonedaEquiv.surjective x
+  rw [← Subcomplex.range_eq_ofSimplex]
+  infer_instance
 
 end stdSimplex
 
