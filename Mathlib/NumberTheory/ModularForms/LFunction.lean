1d1250857e43530510f49a1c8e5f6bcc6f4d6e93
David Loeffler <d.loeffler.01@cantab.net>
2025-11-28 14:30:13 +0100

    add public flags

--- Mathlib/NumberTheory/ModularForms/LFunction.lean
+++ Mathlib/NumberTheory/ModularForms/LFunction.lean
@@ -5,10 +5,10 @@ Authors: David Loeffler
 -/
 module
 
-import Mathlib.NumberTheory.ModularForms.Bounds
-import Mathlib.NumberTheory.LSeries.AbstractFuncEq
-import Mathlib.NumberTheory.LSeries.MellinEqDirichlet
-import Mathlib.Analysis.PSeries
+public import Mathlib.NumberTheory.ModularForms.Bounds
+public import Mathlib.NumberTheory.LSeries.AbstractFuncEq
+public import Mathlib.NumberTheory.LSeries.MellinEqDirichlet
+public import Mathlib.Analysis.PSeries
 
 /-!
 # The `L`-function of a modular form
