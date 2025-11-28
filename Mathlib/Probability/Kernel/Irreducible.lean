36a42d11f66b88a3259079e29d56bda65a3d82c2
bwangpj <70694994+bwangpj@users.noreply.github.com>
2025-11-28 15:00:04 -0500

    Update

--- Mathlib/Probability/Kernel/Irreducible.lean
+++ Mathlib/Probability/Kernel/Irreducible.lean
@@ -11,8 +11,8 @@ public import Mathlib.Probability.Kernel.Composition.Comp
 # Irreducibility of kernels
 
 A kernel `κ : Kernel α α` is `φ`-irreducible, for a given measure `φ` on `α`,
-if for every measurable set `A` with positive measure under `φ`,
-there exists a positive integer `n` such that for every `a : α`, we have `(κ ^ n) a A > 0`.
+if for every measurable set `A` with positive measure under `φ`, and for every `a : α`,
+there exists a positive integer `n` such that we have `(κ ^ n) a A > 0`.
 
 When the kernel `κ` is the transition kernel of a Markov chain,
 this precisely means that the Markov chain is `φ`-irreducible,
@@ -23,16 +23,11 @@ that is, there is a positive probability of reaching any
 
 * `ProbabilityTheory.Kernel.IsIrreducible`:
   irreducibility of a given kernel with respect to a measure `φ`.
-* `ProbabilityTheory.Kernel.IsStronglyIrreducible`:
-  strong irreducibility of a given kernel with respect to a measure `φ`.
 
 ## Main statements
 
 * `isIrreducible_of_le_measure`: If a kernel `κ` is irreducible with respect to a measure `φ₂`,
   then it is also irreducible with respect to any measure `φ₁` with `φ₁ ≤ φ₂`.
-* `isStronglyIrreducible_of_le_measure`: If a kernel `κ` is strongly irreducible
-  with respect to a measure `φ₂`,
-  then it is also strongly irreducible with respect to any measure `φ₁` with `φ₁ ≤ φ₂`.
 
 ## References
 
@@ -53,53 +48,34 @@ variable {α β : Type*} {mα : MeasurableSpace α} {mβ : MeasurableSpace β}
 
 namespace Kernel
 
-/-- `φ`-irreducibility of a kernel `κ` (w.r.t. a measure `φ`).
+/-- A kernel `κ : Kernel α α` is `φ`-irreducible (w.r.t. a given measure `φ` on `α`),
+if for every measurable set `A` with positive measure under `φ`,
+and for every `a : α`, there exists an integer `n` such that `(κ ^ n) a A > 0`.
 Ref. *Meyn-Tweedie* Proposition 4.2.1(ii), page 89 -/
 @[mk_iff]
 class IsIrreducible (φ : Measure α) (κ : Kernel α α) : Prop where
   irreducible ⦃A⦄ (hA : MeasurableSet A) (hφA : φ A > 0) a :
     ∃ (n : ℕ), (κ ^ n) a A > 0
 
-/-- Strongly `φ`-irreducibility of a kernel `κ` (w.r.t. a measure `φ`).
-Ref. https://www.stats.ox.ac.uk/~rebeschi/teaching/AdvSim/18/notes/notes4.pdf, Definition 3.1 -/
-@[mk_iff]
-class IsStronglyIrreducible (φ : Measure α) (κ : Kernel α α) : Prop where
-  strongly_irreducible ⦃A⦄ (hA : MeasurableSet A) (hφA : φ A > 0) a : κ a A > 0
-
-instance {φ : Measure α} {κ : Kernel α α} [IsStronglyIrreducible φ κ] :
-    IsIrreducible φ κ :=
-  { irreducible := fun _ hs hsp a ↦ by
-      use 1; simpa using IsStronglyIrreducible.strongly_irreducible hs hsp a }
-
 instance {φ : Measure α} [Subsingleton α] :
-    IsStronglyIrreducible φ Kernel.id :=
-  { strongly_irreducible := fun s hs hsp a ↦ by
-      have ha : a ∈ s :=
-        Subsingleton.mem_iff_nonempty.mpr
-          <| MeasureTheory.nonempty_of_measure_ne_zero (μ := φ) (ne_of_lt hsp).symm
-      simp [id_apply, ha] }
+    IsIrreducible φ Kernel.id where
+  irreducible s hs hsp a := by
+    use 1;
+    have ha : a ∈ s :=
+      Subsingleton.mem_iff_nonempty.mpr
+        <| MeasureTheory.nonempty_of_measure_ne_zero (μ := φ) (ne_of_lt hsp).symm
+    simp [id_apply, ha]
 
 instance {c : ℝ≥0∞} {φ : Measure α} {κ : Kernel α α} [hκ : IsIrreducible φ κ] :
-    IsIrreducible (c • φ) κ :=
-  { irreducible := fun s hs hsp ↦ by
-      simpa using hκ.irreducible hs <| by simp_all }
-
-instance {c : ℝ≥0∞} {φ : Measure α} {κ : Kernel α α} [hκ : IsStronglyIrreducible φ κ] :
-    IsStronglyIrreducible (c • φ) κ :=
-  { strongly_irreducible := fun s hs hsp ↦ by
-      simpa using hκ.strongly_irreducible hs <| by simp_all }
+    IsIrreducible (c • φ) κ where
+  irreducible s hs hsp := by
+    simpa using hκ.irreducible hs <| by simp_all
 
 lemma isIrreducible_of_le_measure {φ₁ φ₂ : Measure α} (hφ : φ₁ ≤ φ₂)
     {κ : Kernel α α} [hκ : IsIrreducible φ₂ κ] :
     IsIrreducible φ₁ κ :=
   { irreducible := fun s hs hsp ↦ by
       simpa using hκ.irreducible hs <| Std.lt_of_lt_of_le hsp (hφ s) }
-
-lemma isStronglyIrreducible_of_le_measure {φ₁ φ₂ : Measure α} (hφ : φ₁ ≤ φ₂)
-    {κ : Kernel α α} [hκ : IsStronglyIrreducible φ₂ κ] :
-    IsStronglyIrreducible φ₁ κ :=
-  { strongly_irreducible := fun s hs hsp ↦ by
-      simpa using hκ.strongly_irreducible hs <| Std.lt_of_lt_of_le hsp (hφ s) }
 
 end Kernel
 
