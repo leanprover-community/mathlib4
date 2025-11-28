59b7043082ed96b1a5ae12e3693af5ed7b6c035b
Monica Omar <2497250a@research.gla.ac.uk>
2025-11-28 18:04:03 +0000

    semiring

--- Mathlib/Algebra/Central/End.lean
+++ Mathlib/Algebra/Central/End.lean
@@ -16,17 +16,18 @@ public import Mathlib.LinearAlgebra.FreeModule.Basic
 
 @[expose] public section
 
-open Module
+open Module Free
 
-variable {K V : Type*} [Semifield K] [AddCommMonoid V] [Module K V] [Free K V]
+variable {K V : Type*} [CommSemiring K] [AddCommMonoid V] [Module K V] [Free K V]
 
 /-- The center of endomorphisms on a vector space is trivial,
 in other words, it is a central algebra. -/
 instance Algebra.IsCentral.end : IsCentral K (End K V) where
   out T hT := by
-    have h' (f : V →ₗ[K] K) (y v : V) : f (T v) • y = f v • T y := by
+    have h' (f : Dual K V) (y v : V) : f (T v) • y = f v • T y := by
       simpa using congr($(Subalgebra.mem_center_iff.mp hT <| f.smulRight y) v)
     nontriviality V
     obtain ⟨x, hx⟩ := exists_ne (0 : V)
-    obtain ⟨f, hf⟩ := Free.exists_dual_eq_one K hx
-    exact ⟨f (T x), LinearMap.ext fun _ => by simp [h', hf]⟩
+    let b := chooseBasis K V
+    have ⟨i, hi⟩ := not_forall.mp fun h ↦ b.repr.map_ne_zero_iff.mpr hx <| Finsupp.ext h
+    exact ⟨b.coord i (T (b i)), LinearMap.ext fun y ↦ by simpa using h' (b.coord i) y (b i)⟩
