692d418ab682c602337e879ce78970ceb09abb1d
Etienne Marion <baguettes_casino0c@icloud.com>
2025-11-29 12:52:36 +0100

    fix imports

--- Mathlib/Probability/Moments/Variance.lean
+++ Mathlib/Probability/Moments/Variance.lean
@@ -6,10 +6,10 @@ Authors: Sébastien Gouëzel, Kexing Ying
 module
 
 public import Mathlib.Probability.Moments.Covariance
+public import Mathlib.Probability.Notation
 
 import Mathlib.MeasureTheory.Function.LpSeminorm.Prod
 import Mathlib.Probability.Independence.Integrable
-import Mathlib.Probability.Notation
 
 /-!
 # Variance of random variables
