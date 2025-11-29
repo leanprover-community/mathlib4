2e0f5a9cbfcade2eddc1fc3eb299f68c98dc5cad
Whysoserioushah <109107491+Whysoserioushah@users.noreply.github.com>
2025-11-29 12:58:47 +0000

    Update Mathlib/LinearAlgebra/Matrix/Module.lean
    Co-authored-by: Andrew Yang <36414270+erdOne@users.noreply.github.com>

--- Mathlib/LinearAlgebra/Matrix/Module.lean
+++ Mathlib/LinearAlgebra/Matrix/Module.lean
@@ -32,7 +32,7 @@ variable {ι R M N P : Type*} [Ring R] [Fintype ι] [DecidableEq ι] [AddCommGro
 namespace Matrix.Module
 
 /-- `Mⁿ` is a `Mₙ(R)` module, note that this creates a diamond when `M` is `Matrix ι ι R` or when
-  `M` is `R`. (The intended name in lemmas is `smulVec`.) -/
+  `M` is `R`. -/
 scoped instance matrixModule : Module (Matrix ι ι R) (ι → M) where
   smul N v i := ∑ j : ι, N i j • v j
   one_smul v := funext fun i ↦ show ∑ _, _ = _ by simp [one_apply]
