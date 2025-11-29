1866b77a7f63d6e4dba445095c2728e1c21559a3
Monica Omar <2497250a@research.gla.ac.uk>
2025-11-29 16:44:07 +0000

    move

--- Mathlib/Algebra/Central/End.lean
+++ Mathlib/Algebra/Central/End.lean
@@ -5,23 +5,23 @@ Authors: Monica Omar
 -/
 module
 
-public import Mathlib.Algebra.Azumaya.Defs
-public import Mathlib.Algebra.Central.TensorProduct
+public import Mathlib.Algebra.Central.Defs
+public import Mathlib.LinearAlgebra.FreeModule.Basic
 
 /-!
 # `Module.End R M` is a central algebra
 
-This file shows that the algebra of endomorphisms on free modules is central,
-and, as a corollary, that Azumaya algebras are central.
+This file shows that the algebra of endomorphisms on a free module is central.
 -/
 
 open Module Free
 
+namespace Algebra.IsCentral
 variable {R M : Type*} [CommSemiring R] [AddCommMonoid M] [Module R M] [Free R M]
 
 /-- The center of endomorphisms on a free module is trivial,
 in other words, it is a central algebra. -/
-public instance Algebra.IsCentral.end : IsCentral R (End R M) where
+public instance : IsCentral R (End R M) where
   out T hT := by
     nontriviality M
     let b := chooseBasis R M
@@ -29,8 +29,4 @@ public instance Algebra.IsCentral.end : IsCentral R (End R M) where
     refine ⟨b.coord i (T (b i)), LinearMap.ext fun y ↦ ?_⟩
     simpa using congr($(Subalgebra.mem_center_iff.mp hT <| (b.coord i).smulRight y) (b i))
 
-/-- An Azumaya algebra is a central algebra. -/
-public instance Algebra.IsCentral.azumaya {A : Type*} [Semiring A] [Algebra R A]
-    [IsAzumaya R A] [Free R A] [FaithfulSMul R Aᵐᵒᵖ] : IsCentral R A :=
-  have := of_algEquiv R _ _ (AlgEquiv.ofBijective (.mulLeftRight R A) IsAzumaya.bij).symm
-  left_of_tensor R A Aᵐᵒᵖ <| FaithfulSMul.algebraMap_injective _ _
+end Algebra.IsCentral
