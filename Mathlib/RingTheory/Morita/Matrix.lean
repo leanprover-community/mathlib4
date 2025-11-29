8ee3dadf55e1cb67addafde9ff67bfaae8267577
Whysoserioushah <yx3021@ic.ac.uk>
2025-11-29 12:14:59 +0000

    fix build

--- Mathlib/RingTheory/Morita/Matrix.lean
+++ Mathlib/RingTheory/Morita/Matrix.lean
@@ -3,10 +3,14 @@ Copyright (c) 2025 Yunzhou Xie. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yunzhou Xie, Jujian Zhang
 -/
-import Mathlib.Algebra.Category.ModuleCat.Basic
-import Mathlib.LinearAlgebra.Matrix.Module
+module
+
+public import Mathlib.Algebra.Category.ModuleCat.Basic
+public import Mathlib.LinearAlgebra.Matrix.Module
 
 /-!
+# Morita Equivalece between `R` and `Mₙ(R)`
+
 ## Main definitions
 - `ModuleCat.toMatrixModCat`: The functor from `Mod-R` to `Mod-Mₙ(R)` induced by
   `LinearMap.mapModule` and `Matrix.matrixModule`.
@@ -14,6 +18,8 @@ import Mathlib.LinearAlgebra.Matrix.Module
 ## TODO (Edison)
 - Prove `R` and `Mₙ(R)` are morita-equivalent.
 -/
+
+@[expose] public section
 
 universe u
 
@@ -26,6 +32,6 @@ open CategoryTheory Matrix.Module
 @[simps]
 def ModuleCat.toMatrixModCat : ModuleCat R ⥤ ModuleCat (Matrix ι ι R) where
   obj M := ModuleCat.of (Matrix ι ι R) (ι → M)
-  map f := ModuleCat.ofHom f.hom.mapMatrixModule
-  map_id _ := ModuleCat.hom_ext LinearMap.id_mapMatrixModule
-  map_comp f g := ModuleCat.hom_ext (LinearMap.comp_mapMatrixModule f.hom g.hom)
+  map f := ModuleCat.ofHom <| f.hom.mapMatrixModule ι
+  map_id _ := ModuleCat.hom_ext <| LinearMap.mapMatrixModule_id
+  map_comp f g := ModuleCat.hom_ext (LinearMap.mapMatrixModule_comp f.hom g.hom)
