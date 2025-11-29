9a728cb0edaa54318878e5694e6a4446fc3623b3
Alastair Irving <29164966+ajirving@users.noreply.github.com>
2025-11-29 17:41:15 +0000

    feat(NumberTheory): define Chebyshev functions
    Defines the Chebyshev psi and theta functions and proves some simple results.

---
+++ Mathlib/NumberTheory/Chebyshev.lean
@@ -0,0 +1,76 @@
+/-
+Copyright (c) 2025 Alastair Irving. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Alastair Irving, Ruben Van de Velde
+-/
+module
+
+public import Mathlib.NumberTheory.VonMangoldt
+
+/-!
+# Chebyshev functions
+
+This file defines the Chebyshev functions `theta` and `psi`.
+These  give logarithmically weighted sums of primes and prime powers.
+
+## Main definitions
+
+- `Chebyshev.psi` gives the sum of `ArithmeticFunction.vonMangoldt`
+- `Chebyshev.theta` gives the sum of `log p` over primes
+
+## Notation
+
+We introduce the scoped notations `θ` and `ψ` for the Chebyshev functions.
+
+## References
+
+Parts of this file were upstreamed from the PrimeNumberTheoremAnd project by Kontorovich et al, https://github.com/alexKontorovich/PrimeNumberTheoremAnd.
+
+## TODOS
+
+- Upstream the results relating `theta` and `psi` to each other and to the prime counting function.
+- Prove Chebyshev's estimates.
+-/
+@[expose] public section
+
+open Nat hiding log
+open Finset Real
+open ArithmeticFunction hiding log
+
+namespace Chebyshev
+
+noncomputable def psi (x : ℝ) :=
+    ∑ n ∈ Ioc 0 ⌊ x⌋₊, Λ n
+
+scoped notation "ψ" => Chebyshev.psi
+
+noncomputable def theta (x : ℝ) :=
+    ∑ p ∈ Ioc 0 ⌊x⌋₊ with p.Prime, log p
+
+scoped notation "θ" => Chebyshev.theta
+
+theorem psi_nonneg (x : ℝ) : 0 ≤ ψ x :=
+  sum_nonneg fun _ _ ↦ (by simp)
+
+theorem theta_nonneg (x : ℝ) : 0 ≤ θ x := by
+  refine sum_nonneg fun n hn ↦ log_nonneg ?_
+  simp_all
+  linarith
+
+theorem psi_eq_zero_of_lt_two {x : ℝ} (hx : x < 2) : ψ x = 0 := by
+  apply sum_eq_zero fun n hn ↦ ?_
+  simp only [mem_Ioc] at hn
+  convert vonMangoldt_apply_one
+  have := lt_of_le_of_lt (le_floor_iff' hn.1.ne.symm |>.mp hn.2) hx
+  norm_cast at this
+  linarith
+
+theorem theta_eq_zero_of_lt_two {x : ℝ} (hx : x < 2) : θ x = 0 := by
+  apply sum_eq_zero fun n hn ↦ ?_
+  convert log_one
+  simp only [mem_filter, mem_Ioc] at hn
+  have := lt_of_le_of_lt (le_floor_iff' hn.1.1.ne.symm |>.mp hn.1.2) hx
+  norm_cast at ⊢ this
+  linarith
+
+end Chebyshev
