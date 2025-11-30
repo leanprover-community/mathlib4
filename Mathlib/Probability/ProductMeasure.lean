fa3ba7e06a66b1ebc58160d4d735b33f3d852676
David Ledvinka <dledvinka@Davids-Mac-mini.local>
2025-11-30 20:14:07 +0000

    rough done

--- Mathlib/Probability/ProductMeasure.lean
+++ Mathlib/Probability/ProductMeasure.lean
@@ -398,7 +398,6 @@ theorem eq_infinitePi {ν : Measure (Π i, X i)}
     · exact .univ
   · exact .univ_pi ht
 
--- TODO: add a version for an infinite product
 lemma infinitePi_pi {s : Finset ι} {t : (i : ι) → Set (X i)}
     (mt : ∀ i ∈ s, MeasurableSet (t i)) :
     infinitePi μ (Set.pi s t) = ∏ i ∈ s, (μ i) (t i) := by
@@ -410,12 +409,81 @@ lemma infinitePi_pi {s : Finset ι} {t : (i : ι) → Set (X i)}
   · exact measurable_restrict _
   · exact .univ_pi fun i ↦ mt i.1 i.2
 
--- TODO: add a version for infinite `ι`. See TODO on `infinitePi_pi`.
+-- -- Move to proper location!
+open Classical in
+lemma restrict_preimage' {I : Set ι} (s : Finset I) (t : (i : I) → Set (X i))
+  : I.restrict ⁻¹' Set.pi s t =
+      Set.pi (s.image Subtype.val) (fun i ↦ if h : i ∈ I then t ⟨i, h⟩ else .univ) := by
+  grind
+
+theorem infinitePi_map_restrict' {I : Set ι} :
+    (infinitePi μ).map I.restrict = infinitePi fun i : I ↦ μ i := by
+  apply eq_infinitePi
+  intro s t ht
+  classical
+  rw [map_apply (by fun_prop), restrict_preimage', infinitePi_pi _ (by measurability)]
+  · simp
+  · exact MeasurableSet.pi s.countable_toSet (by measurability)
+
+-- Move to proper location!
+@[simp] theorem apply_le_one {Ω : Type*} {mΩ : MeasurableSpace Ω}
+    {μ : Measure Ω} [IsProbabilityMeasure μ] (s : Set Ω) : μ s ≤ 1 := by
+  grw [measure_mono (Set.subset_univ _), measure_univ]
+
+-- Move to proper location!
+theorem pi_iUnion_eq_iInter_pi {ι ι' : Type*} {α : ι → Type*} (s : ι' → Set ι)
+    (t : (i : ι) → Set (α i)) : (⋃ i, s i).pi t = ⋂ i, (s i).pi t := by
+  ext f
+  simp
+  grind
+
+-- Move to proper location!
+theorem union_all_finset_eq_set {ι : Type*} (s : Set ι) :
+    ⋃ s' : Finset s, Subtype.val '' (s' : Set s) = s := by
+  ext x
+  simp only [Set.mem_iUnion, Set.mem_image, SetLike.mem_coe, Subtype.exists,
+    exists_and_right, exists_eq_right]
+  exact ⟨fun ⟨_, hx, _⟩ ↦ hx, fun hx ↦ ⟨{⟨x, hx⟩}, hx, by simp⟩⟩
+
+open Classical in
+lemma infinitePi_pi' {s : Set ι} (hs : Countable s) {t : (i : ι) → Set (X i)}
+    (mt : ∀ i ∈ s, MeasurableSet (t i)) :
+    infinitePi μ (Set.pi s t) = ∏' i : s, (μ i) (t i) := by
+  wlog s_ne : Nonempty s
+  · simp [Set.not_nonempty_iff_eq_empty'.mp s_ne]
+  apply tendsto_nhds_unique (f := fun s' : Finset s ↦ ∏ i ∈ s', (μ i) (t i)) (l := atTop)
+  · conv in ∏ _ ∈ _, _ =>
+      rw [← infinitePi_pi _ (by measurability), ← infinitePi_map_restrict', map_apply
+        (by fun_prop) (by apply MeasurableSet.pi (countable_toSet _) (by measurability)),
+        restrict_preimage']
+      simp only [coe_image, dite_eq_ite]
+    have : s.pi t
+      = ⋂ s' : Finset s,
+        (Subtype.val '' (s': Set s)).pi (fun i ↦ if i ∈ s then t i else Set.univ) := by
+      rw [← pi_iUnion_eq_iInter_pi, union_all_finset_eq_set]
+      grind
+    rw [this]
+    apply tendsto_measure_iInter_atTop
+    · refine fun s' ↦ MeasurableSet.nullMeasurableSet (MeasurableSet.pi ?_ (by measurability))
+      exact (Finset.countable_toSet _).image _
+    · intro _ _ h
+      simpa using Set.pi_mono' (by simp) (Set.image_mono h)
+    · exact ⟨{Nonempty.some s_ne}, by simp⟩
+  · rw [ENNReal.tprod_eq_iInf_prod (by simp)]
+    exact tendsto_atTop_iInf (prod_anti_set_of_le_one' (by simp))
+
 @[simp]
 lemma infinitePi_singleton [Fintype ι] [∀ i, MeasurableSingletonClass (X i)] (f : ∀ i, X i) :
     infinitePi μ {f} = ∏ i, μ i {f i} := by
   simpa [Set.univ_pi_singleton] using
     infinitePi_pi μ (s := .univ) (t := fun i ↦ {f i}) fun _ _ ↦ .singleton _
+
+@[simp]
+lemma infinitePi_singleton' [hι : Countable ι] [∀ i, MeasurableSingletonClass (X i)]
+    (f : ∀ i, X i) : infinitePi μ {f} = ∏' i, μ i {f i} := by
+  rw [← Set.univ_pi_singleton, infinitePi_pi', tprod_univ (f := fun i ↦ μ i {f i})]
+  · simp [Set.countable_univ_iff, hι]
+  · measurability
 
 @[simp] lemma infinitePi_dirac (f : ∀ i, X i) : infinitePi (fun i ↦ dirac (f i)) = dirac f :=
   .symm <| eq_infinitePi _ <| by simp +contextual [MeasurableSet.pi, Finset.countable_toSet]
