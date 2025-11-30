d1ef9e86c709a133057dc501802f38a7f89a80da
Etienne Marion <baguettes_casino0c@icloud.com>
2025-11-30 17:53:03 +0100

    wip

--- Mathlib/Probability/Distributions/Gaussian/IsGaussianProcess/Basic.lean
+++ Mathlib/Probability/Distributions/Gaussian/IsGaussianProcess/Basic.lean
@@ -19,7 +19,7 @@ import Mathlib.Probability.Process.FiniteDimensionalLaws
 
 public section
 
-open MeasureTheory InnerProductSpace
+open MeasureTheory InnerProductSpace Finset
 
 open scoped ENNReal NNReal RealInnerProductSpace
 
@@ -35,17 +35,12 @@ lemma isProbabilityMeasure (hX : IsGaussianProcess X P) :
     IsProbabilityMeasure P :=
   hX.hasGaussianLaw Classical.ofNonempty |>.isProbabilityMeasure
 
-lemma aemeasurable (hX : IsGaussianProcess X P) (t : T) :
-    AEMeasurable (X t) P := by
-  by_contra h
-  have := (hX.hasGaussianLaw {t}).isGaussian_map
-  rw [Measure.map_of_not_aemeasurable] at this
-  · exact this.toIsProbabilityMeasure.ne_zero _ rfl
-  · rw [aemeasurable_pi_iff]
-    push_neg
-    exact ⟨⟨t, by simp⟩, h⟩
+lemma aemeasurable (hX : IsGaussianProcess X P) (t : T) : AEMeasurable (X t) P :=
+  AEMeasurable.of_map_ne_zero
+    (hX.hasGaussianLaw {t}).isGaussian_map.toIsProbabilityMeasure.ne_zero |>.eval ⟨t, by simp⟩
 
-lemma modification (hX : IsGaussianProcess X P) (hXY : ∀ t, X t =ᵐ[P] Y t) :
+/-- A modification of a Gaussian process is a Gaussian Process -/
+lemma congr (hX : IsGaussianProcess X P) (hXY : ∀ t, X t =ᵐ[P] Y t) :
     IsGaussianProcess Y P where
   hasGaussianLaw I := by
     constructor
@@ -56,12 +51,13 @@ end Basic
 
 variable [NormedAddCommGroup E] [NormedSpace ℝ E] [MeasurableSpace E] [BorelSpace E]
 
-lemma hasGaussianLaw_eval (hX : IsGaussianProcess X P) (t : T) :
-    HasGaussianLaw (X t) P := by
+section Maps
+
+lemma hasGaussianLaw_eval (hX : IsGaussianProcess X P) (t : T) : HasGaussianLaw (X t) P := by
   have : X t = (ContinuousLinearMap.proj (R := ℝ) ⟨t, by simp⟩) ∘
     (fun ω ↦ ({t} : Finset T).restrict (X · ω)) := by ext; simp
   rw [this]
-  exact (hX.hasGaussianLaw _).map _
+  exact (hX.hasGaussianLaw {t}).map (.proj ⟨t, by simp⟩)
 
 variable [SecondCountableTopology E]
 
@@ -70,66 +66,78 @@ lemma hasGaussianLaw_prodMk (hX : IsGaussianProcess X P)
   classical
   exact (hX.hasGaussianLaw {s, t}).prodMk ⟨s, by simp⟩ ⟨t, by simp⟩
 
-lemma hasGaussianLaw_sub (hX : IsGaussianProcess X P)
-    {s t : T} : HasGaussianLaw (X s - X t) P := hX.hasGaussianLaw_prodMk.sub
+lemma hasGaussianLaw_add (hX : IsGaussianProcess X P) {s t : T} :
+    HasGaussianLaw (X s + X t) P := hX.hasGaussianLaw_prodMk.add
 
-lemma hasGaussianLaw_fun_sub
-    (hX : IsGaussianProcess X P) {s t : T} : HasGaussianLaw (fun ω ↦ X s ω - X t ω) P :=
-  hX.hasGaussianLaw_sub
+lemma hasGaussianLaw_fun_add (hX : IsGaussianProcess X P) {s t : T} :
+    HasGaussianLaw (fun ω ↦ X s ω + X t ω) P := hX.hasGaussianLaw_add
 
-lemma hasGaussianLaw_increments
-    (hX : IsGaussianProcess X P) {n : ℕ} {t : Fin (n + 1) → T} :
+lemma hasGaussianLaw_sub (hX : IsGaussianProcess X P) {s t : T} :
+    HasGaussianLaw (X s - X t) P := hX.hasGaussianLaw_prodMk.sub
+
+lemma hasGaussianLaw_fun_sub (hX : IsGaussianProcess X P) {s t : T} :
+    HasGaussianLaw (fun ω ↦ X s ω - X t ω) P := hX.hasGaussianLaw_sub
+
+lemma hasGaussianLaw_sum (hX : IsGaussianProcess X P) {I : Finset T} :
+    HasGaussianLaw (∑ i ∈ I, X i) P := by
+  convert (hX.hasGaussianLaw I).sum
+  simp [I.sum_attach X]
+
+lemma hasGaussianLaw_fun_sum (hX : IsGaussianProcess X P) {I : Finset T} :
+    HasGaussianLaw (fun ω ↦ ∑ i ∈ I, X i ω) P := by
+  convert hX.hasGaussianLaw_sum (I := I)
+  simp
+
+/-- The increments of a Gaussian process are Gaussian. -/
+lemma hasGaussianLaw_increments (hX : IsGaussianProcess X P) {n : ℕ} {t : Fin (n + 1) → T} :
     HasGaussianLaw (fun ω (i : Fin n) ↦ X (t i.succ) ω - X (t i.castSucc) ω) P := by
   classical
-  let L : ((Finset.univ.image t) → E) →L[ℝ] Fin n → E :=
+  let L : ((univ.image t) → E) →L[ℝ] Fin n → E :=
     { toFun x i := x ⟨t i.succ, by simp⟩ - x ⟨t i.castSucc, by simp⟩
       map_add' x y := by ext; simp; abel
       map_smul' m x := by ext; simp; module
       cont := by fun_prop }
-  have : (fun ω i ↦ X (t i.succ) ω - X (t i.castSucc) ω) =
-      L ∘ fun ω ↦ (Finset.univ.image t).restrict (X · ω) := by ext; simp [L]
-  rw [this]
-  exact (hX.hasGaussianLaw _).map _
+  exact (hX.hasGaussianLaw _).map L
 
-lemma indepFun [CompleteSpace E] {X : S → Ω → E} {Y : T → Ω → E}
-    (h : IsGaussianProcess (Sum.elim X Y) P) (hX : ∀ s, Measurable (X s))
-    (hY : ∀ t, Measurable (Y t))
-    (h' : ∀ s t (L₁ L₂ : StrongDual ℝ E), cov[L₁ ∘ X s, L₂ ∘ Y t; P] = 0) :
+end Maps
+
+section Independence
+
+variable [SecondCountableTopology E] [CompleteSpace E] {X : S → Ω → E}
+
+lemma indepFun {X : S → Ω → E} {Y : T → Ω → E} (hXY : IsGaussianProcess (Sum.elim X Y) P)
+    (hX : ∀ s, Measurable (X s)) (hY : ∀ t, Measurable (Y t))
+    (h : ∀ s t (L₁ L₂ : StrongDual ℝ E), cov[L₁ ∘ X s, L₂ ∘ Y t; P] = 0) :
     IndepFun (fun ω s ↦ X s ω) (fun ω t ↦ Y t ω) P := by
-  have := h.isProbabilityMeasure
+  have := hXY.isProbabilityMeasure
   refine IndepFun.process_indepFun_process hX hY fun I J ↦
     HasGaussianLaw.indepFun_of_covariance_strongDual ?_ fun L₁ L₂ ↦ ?_
   · let L : (I.disjSum J → E) →L[ℝ] (I → E) × (J → E) :=
-      { toFun x := (fun s ↦ x ⟨Sum.inl s, Finset.inl_mem_disjSum.2 s.2⟩,
-          fun t ↦ x ⟨Sum.inr t, Finset.inr_mem_disjSum.2 t.2⟩)
+      { toFun x := (fun s ↦ x ⟨Sum.inl s, inl_mem_disjSum.2 s.2⟩,
+          fun t ↦ x ⟨Sum.inr t, inr_mem_disjSum.2 t.2⟩)
         map_add' x y := by ext <;> simp
         map_smul' c x := by ext <;> simp }
-    have : (fun ω ↦ (fun i : I ↦ X i ω, fun j : J ↦ Y j ω)) =
-        L ∘ (fun ω ↦ (I.disjSum J).restrict (Sum.elim X Y · ω)) := by
-      ext <;> simp [L]
-    rw [this]
-    exact (h.hasGaussianLaw _).map _
+    exact (hXY.hasGaussianLaw _).map L
   classical
   have h1 : L₁ ∘ (fun ω i ↦ X i ω) = ∑ i : I, (L₁ ∘L .single ℝ _ i) ∘ X i := by
     ext ω
-    simp only [Function.comp_apply, ← L₁.sum_comp_single, Finset.univ_eq_attach, Finset.sum_apply]
+    simp only [Function.comp_apply, ← L₁.sum_comp_single, univ_eq_attach, sum_apply]
   have h2 : L₂ ∘ (fun ω j ↦ Y j ω) = ∑ j : J, (L₂ ∘L .single ℝ _ j) ∘ Y j := by
     ext ω
-    simp only [Function.comp_apply, ← L₂.sum_comp_single, Finset.univ_eq_attach, Finset.sum_apply]
+    simp only [Function.comp_apply, ← L₂.sum_comp_single, univ_eq_attach, sum_apply]
   rw [h1, h2, covariance_sum_sum]
-  · exact Finset.sum_eq_zero fun i _ ↦ Finset.sum_eq_zero fun j _ ↦ h' ..
-  · exact fun s ↦ ((h.hasGaussianLaw_eval (.inl s)).map _).memLp_two
-  · exact fun t ↦ ((h.hasGaussianLaw_eval (.inr t)).map _).memLp_two
+  · exact sum_eq_zero fun i _ ↦ sum_eq_zero fun j _ ↦ h ..
+  · exact fun s ↦ ((hXY.hasGaussianLaw_eval (.inl s)).map _).memLp_two
+  · exact fun t ↦ ((hXY.hasGaussianLaw_eval (.inr t)).map _).memLp_two
 
-lemma iIndepFun [CompleteSpace E] {S : T → Type*}
-    {X : (t : T) → (s : S t) → Ω → E}
-    (h : IsGaussianProcess (fun (p : (t : T) × S t) ω ↦ X p.1 p.2 ω) P)
-    (hX : ∀ t s, Measurable (X t s))
-    (h' : ∀ t₁ t₂, t₁ ≠ t₂ → ∀ (s₁ : S t₁) (s₂ : S t₂) (L₁ L₂ : StrongDual ℝ E),
+lemma iIndepFun {S : T → Type*} {X : (t : T) → (s : S t) → Ω → E}
+    (hX : IsGaussianProcess (fun (p : (t : T) × S t) ω ↦ X p.1 p.2 ω) P)
+    (mX : ∀ t s, Measurable (X t s))
+    (h : ∀ t₁ t₂, t₁ ≠ t₂ → ∀ (s₁ : S t₁) (s₂ : S t₂) (L₁ L₂ : StrongDual ℝ E),
       cov[L₁ ∘ X t₁ s₁, L₂ ∘ X t₂ s₂; P] = 0) :
     iIndepFun (fun t ω s ↦ X t s ω) P := by
-  have := h.isProbabilityMeasure
-  refine iIndepFun.iIndepFun_process hX fun I J ↦
+  have := hX.isProbabilityMeasure
+  refine iIndepFun.iIndepFun_process mX fun I J ↦
     HasGaussianLaw.iIndepFun_of_covariance_strongDual ?_ fun i j hij L₁ L₂ ↦ ?_
   · classical
     let L : (I.sigma (fun i ↦ if hi : i ∈ I then J ⟨i, hi⟩ else ∅) → E) →L[ℝ] (i : I) → J i → E :=
@@ -137,23 +145,18 @@ lemma iIndepFun [CompleteSpace E] {S : T → Type*}
         map_add' x y := by ext; simp
         map_smul' c x := by ext; simp
         cont := by fun_prop }
-    have : (fun ω (i : I) (j : J i) ↦ X i j ω) =
-        L ∘ (fun ω ↦ (I.sigma (fun i ↦ if hi : i ∈ I then J ⟨i, hi⟩ else ∅)).restrict
-          (fun p ↦ X p.1 p.2 ω)) := by
-      ext; simp [L]
-    rw [this]
-    exact (h.hasGaussianLaw _).map _
+    exact (hX.hasGaussianLaw _).map L
   classical
   have h1 : L₁ ∘ (fun ω k ↦ X i k ω) = ∑ k : J i, (L₁ ∘L .single ℝ _ k) ∘ X i k := by
     ext ω
-    simp only [Function.comp_apply, ← L₁.sum_comp_single, Finset.univ_eq_attach, Finset.sum_apply]
+    simp only [Function.comp_apply, ← L₁.sum_comp_single, univ_eq_attach, sum_apply]
   have h2 : L₂ ∘ (fun ω k ↦ X j k ω) = ∑ k : J j, (L₂ ∘L .single ℝ _ k) ∘ X j k := by
     ext ω
-    simp only [Function.comp_apply, ← L₂.sum_comp_single, Finset.univ_eq_attach, Finset.sum_apply]
+    simp only [Function.comp_apply, ← L₂.sum_comp_single, univ_eq_attach, sum_apply]
   rw [h1, h2, covariance_sum_sum]
-  · exact Finset.sum_eq_zero fun _ _ ↦ Finset.sum_eq_zero fun _ _ ↦ h' i j (by simpa) ..
-  · exact fun k ↦ ((h.hasGaussianLaw_eval ⟨i, k⟩).map _).memLp_two
-  · exact fun k ↦ ((h.hasGaussianLaw_eval ⟨j, k⟩).map _).memLp_two
+  · exact sum_eq_zero fun _ _ ↦ sum_eq_zero fun _ _ ↦ h i j (by simpa) ..
+  · exact fun k ↦ ((hX.hasGaussianLaw_eval ⟨i, k⟩).map _).memLp_two
+  · exact fun k ↦ ((hX.hasGaussianLaw_eval ⟨j, k⟩).map _).memLp_two
 
 lemma indepFun'
     {E : Type*} [NormedAddCommGroup E] [InnerProductSpace ℝ E] [MeasurableSpace E] [BorelSpace E]
@@ -194,6 +197,8 @@ lemma iIndepFun'' {S : T → Type*}
   h.iIndepFun' hX fun _ _ h'' _ _ _ _ ↦ by
     simp [mul_comm, covariance_const_mul_left, covariance_const_mul_right, h' _ _ h'']
 
+end Independence
+
 /-- If a stochastic process `Y` is such that for `s`, `Y s` can be written as a linear
 combination of finitely many values of a Gaussian process, then `Y` is a Gaussian process. -/
 lemma of_isGaussianProcess (hX : IsGaussianProcess X P)
@@ -205,7 +210,7 @@ lemma of_isGaussianProcess (hX : IsGaussianProcess X P)
     choose J L hL using h
     classical
     let K : (I.biUnion J → E) →L[ℝ] I → F :=
-      { toFun x s := L s (fun t ↦ x ⟨t.1, Finset.mem_biUnion.2 ⟨s.1, s.2, t.2⟩⟩)
+      { toFun x s := L s (fun t ↦ x ⟨t.1, mem_biUnion.2 ⟨s.1, s.2, t.2⟩⟩)
         map_add' x y := by ext; simp [← Pi.add_def]
         map_smul' c x := by ext; simp [← Pi.smul_def]
         cont := by fun_prop }
