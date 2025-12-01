4d5db01bc907e2034144df51d7b2371e42299796
stepanholub <holub@karlin.mff.cuni.cz>
2025-12-01 08:47:50 +0100

    module and public removed

--- Mathlib/Data/List/PeriodicityLemma.lean
+++ Mathlib/Data/List/PeriodicityLemma.lean
@@ -3,10 +3,9 @@ Copyright (c) 2025 Štěpán Holub. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Štěpán Holub
 -/
-module
 
-public import Mathlib.Algebra.Order.Group.Nat
-public import Mathlib.Data.Nat.Lattice
+import Mathlib.Algebra.Order.Group.Nat
+import Mathlib.Data.Nat.Lattice
 
 /-! # Periods of words (Lists)
 
