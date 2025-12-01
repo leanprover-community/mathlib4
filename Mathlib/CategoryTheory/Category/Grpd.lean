c868c21b7c80336b51546897078758f7e30d5b0f
Calle Sönne <calle.sonne@gmail.com>
2025-12-01 18:58:05 +0000

    fixing

--- Mathlib/CategoryTheory/Category/Grpd.lean
+++ Mathlib/CategoryTheory/Category/Grpd.lean
@@ -33,7 +33,7 @@ universe v u
 
 namespace CategoryTheory
 
-open scoped Bicategory
+open Bicategory InducedBicategory
 
 -- intended to be used with explicit universe parameters
 /-- Bicategory of groupoids. -/
@@ -84,129 +84,125 @@ instance bicategory : Bicategory.{max v u, max v u} Grpd.{v, u} :=
 --   pentagon := fun {_} {_} {_} {_} {_}=> Functor.pentagon
 --   triangle {_} {_} {_} := Functor.triangle
 
-instance bicategory.strict : Bicategory.Strict Grpd.{v, u} := inferInstance
-
--- TODO: should be automatic?
-/-- `Grpd` is a strict bicategory. -/
-instance bicategory.strict : Bicategory.Strict Grpd.{v, u} where
-  id_comp {C} {D} F := by cases F; rfl
-  comp_id {C} {D} F := by cases F; rfl
-  assoc := by intros; rfl
+instance bicategory.strict : Bicategory.Strict Grpd.{v, u} :=
+  inferInstanceAs (Bicategory.Strict (InducedBicategory _ toCat))
 
 /-- Category structure on `Grpd` -/
 instance category : LargeCategory.{max v u} Grpd.{v, u} :=
   StrictBicategory.category Grpd.{v, u}
 
-@[ext]
-theorem natTrans_ext {C D : Grpd} {F G : C ⟶ D} {η θ : F ⟶ G} (h : η.app = θ.app) :
-    η = θ := NatTrans.ext' h
+-- TODO: fix following once Cat API PR lands
 
-@[simp]
-theorem id_obj {C : Grpd} (X : C) : (𝟙 C : C ⥤ C).obj X = X :=
-  rfl
+-- @[ext]
+-- theorem natTrans_ext {C D : Grpd} {F G : C ⟶ D} {η θ : F ⟶ G} (h : η.hom.app = θ.hom.app) :
+--     η = θ := NatTrans.ext' h
 
-@[simp]
-theorem id_map {C : Grpd} {X Y : C} (f : X ⟶ Y) : (𝟙 C : C ⥤ C).map f = f :=
-  rfl
+-- @[simp]
+-- theorem id_obj {C : Grpd} (X : C) : (𝟙 C : C ⥤ C).obj X = X :=
+--   rfl
 
-@[simp]
-theorem comp_obj {C D E : Grpd} (F : C ⟶ D) (G : D ⟶ E) (X : C) :
-    (F ≫ G).obj X = G.obj (F.obj X) :=
-  rfl
+-- @[simp]
+-- theorem id_map {C : Grpd} {X Y : C} (f : X ⟶ Y) : (𝟙 C : C ⥤ C).map f = f :=
+--   rfl
 
-@[simp]
-theorem comp_map {C D E : Grpd} (F : C ⟶ D) (G : D ⟶ E) {X Y : C} (f : X ⟶ Y) :
-    (F ≫ G).map f = G.map (F.map f) :=
-  rfl
+-- @[simp]
+-- theorem comp_obj {C D E : Grpd} (F : C ⟶ D) (G : D ⟶ E) (X : C) :
+--     (F ≫ G).obj X = G.obj (F.obj X) :=
+--   rfl
 
-@[simp]
-theorem id_app {C D : Grpd} (F : C ⟶ D) (X : C) : (𝟙 F : F ⟶ F).app X = 𝟙 (F.obj X) := rfl
+-- @[simp]
+-- theorem comp_map {C D E : Grpd} (F : C ⟶ D) (G : D ⟶ E) {X Y : C} (f : X ⟶ Y) :
+--     (F ≫ G).map f = G.map (F.map f) :=
+--   rfl
 
-@[simp]
-theorem comp_app {C D : Grpd} {F G H : C ⟶ D} (α : F ⟶ G) (β : G ⟶ H) (X : C) :
-    (α ≫ β).app X = α.app X ≫ β.app X := rfl
+-- @[simp]
+-- theorem id_app {C D : Grpd} (F : C ⟶ D) (X : C) : (𝟙 F : F ⟶ F).app X = 𝟙 (F.obj X) := rfl
 
-@[simp]
-lemma whiskerLeft_app {C D E : Grpd} (F : C ⟶ D) {G H : D ⟶ E} (η : G ⟶ H) (X : C) :
-    (F ◁ η).app X = η.app (F.obj X) :=
-  rfl
+-- @[simp]
+-- theorem comp_app {C D : Grpd} {F G H : C ⟶ D} (α : F ⟶ G) (β : G ⟶ H) (X : C) :
+--     (α ≫ β).app X = α.app X ≫ β.app X := rfl
 
-@[simp]
-lemma whiskerRight_app {C D E : Grpd} {F G : C ⟶ D} (H : D ⟶ E) (η : F ⟶ G) (X : C) :
-    (η ▷ H).app X = H.map (η.app X) :=
-  rfl
+-- @[simp]
+-- lemma whiskerLeft_app {C D E : Grpd} (F : C ⟶ D) {G H : D ⟶ E} (η : G ⟶ H) (X : C) :
+--     (F ◁ η).app X = η.app (F.obj X) :=
+--   rfl
 
-@[simp]
-theorem eqToHom_app {C D : Grpd} (F G : C ⟶ D) (h : F = G) (X : C) :
-    (eqToHom h).app X = eqToHom (Functor.congr_obj h X) :=
-  CategoryTheory.eqToHom_app h X
+-- @[simp]
+-- lemma whiskerRight_app {C D E : Grpd} {F G : C ⟶ D} (H : D ⟶ E) (η : F ⟶ G) (X : C) :
+--     (η ▷ H).app X = H.map (η.app X) :=
+--   rfl
 
-lemma leftUnitor_hom_app {B C : Grpd} (F : B ⟶ C) (X : B) : (λ_ F).hom.app X = eqToHom (by simp) :=
-  rfl
+-- @[simp]
+-- theorem eqToHom_app {C D : Grpd} (F G : C ⟶ D) (h : F = G) (X : C) :
+--     (eqToHom h).app X = eqToHom (Functor.congr_obj h X) :=
+--   CategoryTheory.eqToHom_app h X
 
-lemma leftUnitor_inv_app {B C : Grpd} (F : B ⟶ C) (X : B) : (λ_ F).inv.app X = eqToHom (by simp) :=
-  rfl
+-- lemma leftUnitor_hom_app {B C : Grpd} (F : B ⟶ C) (X : B) : (λ_ F).hom.app X = eqToHom (by simp) :=
+--   rfl
 
-lemma rightUnitor_hom_app {B C : Grpd} (F : B ⟶ C) (X : B) :
-    (ρ_ F).hom.app X = eqToHom (by simp) :=
-  rfl
+-- lemma leftUnitor_inv_app {B C : Grpd} (F : B ⟶ C) (X : B) : (λ_ F).inv.app X = eqToHom (by simp) :=
+--   rfl
 
-lemma rightUnitor_inv_app {B C : Grpd} (F : B ⟶ C) (X : B) :
-    (ρ_ F).inv.app X = eqToHom (by simp) :=
-  rfl
+-- lemma rightUnitor_hom_app {B C : Grpd} (F : B ⟶ C) (X : B) :
+--     (ρ_ F).hom.app X = eqToHom (by simp) :=
+--   rfl
 
-lemma associator_hom_app {B C D E : Grpd} (F : B ⟶ C) (G : C ⟶ D) (H : D ⟶ E) (X : B) :
-    (α_ F G H).hom.app X = eqToHom (by simp) :=
-  rfl
+-- lemma rightUnitor_inv_app {B C : Grpd} (F : B ⟶ C) (X : B) :
+--     (ρ_ F).inv.app X = eqToHom (by simp) :=
+--   rfl
 
-lemma associator_inv_app {B C D E : Grpd} (F : B ⟶ C) (G : C ⟶ D) (H : D ⟶ E) (X : B) :
-    (α_ F G H).inv.app X = eqToHom (by simp) :=
-  rfl
+-- lemma associator_hom_app {B C D E : Grpd} (F : B ⟶ C) (G : C ⟶ D) (H : D ⟶ E) (X : B) :
+--     (α_ F G H).hom.app X = eqToHom (by simp) :=
+--   rfl
 
-/-- The identity in the category of groupoids equals the identity functor. -/
-theorem id_eq_id (X : Grpd) : 𝟙 X = 𝟭 X := rfl
+-- lemma associator_inv_app {B C D E : Grpd} (F : B ⟶ C) (G : C ⟶ D) (H : D ⟶ E) (X : B) :
+--     (α_ F G H).inv.app X = eqToHom (by simp) :=
+--   rfl
 
-/-- Composition in the category of groupoids equals functor composition. -/
-theorem comp_eq_comp {X Y Z : Grpd} (F : X ⟶ Y) (G : Y ⟶ Z) : F ≫ G = F ⋙ G := rfl
+-- /-- The identity in the category of groupoids equals the identity functor. -/
+-- theorem id_eq_id (X : Grpd) : 𝟙 X = 𝟭 X := rfl
+
+-- /-- Composition in the category of groupoids equals functor composition. -/
+-- theorem comp_eq_comp {X Y Z : Grpd} (F : X ⟶ Y) (G : Y ⟶ Z) : F ≫ G = F ⋙ G := rfl
 
 section
 
-attribute [-simp] eqToIso_refl
-
 /-- Forgetting pseudofunctor to `Cat`. -/
-def forgetToCat : Grpd.{v, u} ⥤ᵖ Cat.{v, u} where
-  obj C := Cat.of C
-  map := id
-  map₂ := id
-  mapId C := eqToIso rfl
-  mapComp C D := eqToIso rfl
+-- TODO: check simp lemmas after API PR lands
+@[simps!]
+abbrev forget : StrictPseudofunctor Grpd.{v, u} Cat.{v, u} :=
+  InducedBicategory.forget
+
+instance forget_full : forget.toFunctor.Full := InducedBicategory.forget_full
+
+instance forget_faithful : forget.toFunctor.Faithful := InducedBicategory.forget_faithful
 
 end
 
-instance forgetToCatIsStrict : forgetToCat.IsStrict where
-
-instance forgetToCat_full : forgetToCat.toFunctor.Full where map_surjective f := ⟨f, rfl⟩
-
-instance forgetToCat_faithful : forgetToCat.toFunctor.Faithful where
-
 /-- Functor that gets the set of objects of a groupoid. It is not
 called `forget`, because it is not a faithful functor. -/
+@[simps]
 def objects : Grpd.{v, u} ⥤ Type u where
-  obj := Bundled.α
-  map F := F.obj
+  obj := Grpd.carrier
+  map F := F.hom.obj
 
 section Products
 
 /-- Construct the product over an indexed family of groupoids, as a fan. -/
+@[simps!]
 def piLimitFan ⦃J : Type u⦄ (F : J → Grpd.{u, u}) : Limits.Fan F :=
-  Limits.Fan.mk (@of (∀ j : J, F j) _) fun j => CategoryTheory.Pi.eval _ j
+  Limits.Fan.mk (@of (∀ j : J, F j) _) fun j =>
+    InducedBicategory.mkHom <| CategoryTheory.Pi.eval _ j
+
+-- TODO: fix following once API PR lands
 
 /-- The product fan over an indexed family of groupoids, is a limit cone. -/
+@[simps!]
 def piLimitFanIsLimit ⦃J : Type u⦄ (F : J → Grpd.{u, u}) : Limits.IsLimit (piLimitFan F) :=
-  Limits.mkFanLimit (piLimitFan F) (fun s => Functor.pi' fun j => s.proj j)
+  Limits.mkFanLimit (piLimitFan F) (fun s => mkHom <| Functor.pi' fun j => (s.proj j).hom)
     (by
       intros
-      dsimp only [piLimitFan]
+      dsimp [piLimitFan]
       simp [comp_eq_comp])
     (by
       intro s m w
