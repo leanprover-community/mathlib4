a30cd4a1bde8c07cf70474361b6f780d02a31ab8
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-12-01 14:05:50 +0100

    Add boilerplate

---
+++ Mathlib/Analysis/Distribution/WeakDeriv.lean
@@ -0,0 +1,16 @@
+/-
+Copyright (c) 2025 Filippo A. E. Nuccio, Michael Rothgang. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Filippo A. E. Nuccio, Michael Rothgang
+-/
+module
+
+public import Mathlib.Analysis.Distribution.TestFunction
+
+/-!
+# Weak derivatives of distributions
+-/
+
+@[expose] public section
+
+-- Now the fun starts.
