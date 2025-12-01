086fcfc125b332e81725e029a57978b28dd96f90
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2025-12-01 11:03:32 +0000

    doc(GroupTheory/GroupAction/SubMulAction/OfStabilizer): fix documentation and add missing (#32291)
    As noticed by @harahu , the documentation of `GroupTheory.GroupAction.SubMulAction.OfStabilizer` was not fully aligned with the definitions in the file, one lemma was actually missing, another one was in another file.
    
    We fix that.

--- Mathlib/GroupTheory/GroupAction/SubMulAction/OfFixingSubgroup.lean
+++ Mathlib/GroupTheory/GroupAction/SubMulAction/OfFixingSubgroup.lean
@@ -14,27 +14,29 @@ public import Mathlib.Tactic.Group
 /-!
 # SubMulActions on complements of invariant subsets
 
-- We define `SubMulAction` of an invariant subset in various contexts,
-especially stabilizers and fixing subgroups : `SubMulAction_of_compl`,
-`SubMulAction_of_stabilizer`, `SubMulAction_of_fixingSubgroup`.
+Given a `MulAction` of `G` on `α` and `s : Set α`,
+
+* `SubMulAction.ofFixingSubgroup` is the action
+  of `FixingSubgroup G s` on the complement `sᶜ` of `s`.
 
 - We define equivariant maps that relate various of these `SubMulAction`s
 and permit to manipulate them in a relatively smooth way:
 
-  * `SubMulAction.ofFixingSubgroupEmpty_equivariantMap`:
-  the identity map, when the set is the empty set.
+  * `SubMulAction.ofFixingSubgroup_equivariantMap`:
+  the identity map from `sᶜ` to `α`, as an equivariant map
+  relative to the injection of `FixingSubgroup G s` into `G`.
 
   * `SubMulAction.fixingSubgroupInsertEquiv M a s` : the
   multiplicative equivalence between `fixingSubgroup M (insert a s)`
   and `fixingSubgroup (stabilizer M a) s`
 
   * `SubMulAction.ofFixingSubgroup_insert_map` : the equivariant
-  map between `SubMulAction.ofFixingSubgroup M (insert a s)`
-  and `SubMulAction.ofFixingSubgroup (stabilizer M a) s`.
+  map between `SubMulAction.ofFixingSubgroup M (Set.insert a s)`
+  and `SubMulAction.ofFixingSubgroup (MulAction.stabilizer M a) s`.
 
   * `SubMulAction.fixingSubgroupEquivFixingSubgroup`:
-  the multiplicative equivalence between `SubMulAction.fixingSubgroup M s`
-  and `SubMulAction.fixingSubgroup M t` induced by `g : M`
+  the multiplicative equivalence between `SubMulAction.ofFixingSubgroup M s`
+  and `SubMulAction.ofFixingSubgroup M t` induced by `g : M`
   such that `g • t = s`.
 
   * `SubMulAction.conjMap_ofFixingSubgroup`:
