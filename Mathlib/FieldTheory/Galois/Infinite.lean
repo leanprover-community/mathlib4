e7525c96aa5d3d5d557f39ccc17a4413efe6c803
bwangpj <70694994+bwangpj@users.noreply.github.com>
2025-12-02 18:41:32 -0500

    Update Infinite.lean

--- Mathlib/FieldTheory/Galois/Infinite.lean
+++ Mathlib/FieldTheory/Galois/Infinite.lean
@@ -224,7 +224,7 @@ open IntermediateField in
 /-- If `H` is a closed normal subgroup of `Gal(K / k)`,
 then `Gal(fixedField H / k)` is isomorphic to `Gal(K / k) ⧸ H`. -/
 noncomputable def normalAutEquivQuotient [IsGalois k K]
-    (H : ClosedSubgroup Gal(K/k)) [Subgroup.Normal H.1] :
+    (H : ClosedSubgroup Gal(K/k)) [H.Normal] :
     Gal(K/k) ⧸ H.1 ≃* Gal(fixedField H.1/k) :=
   (QuotientGroup.quotientMulEquivOfEq ((fixingSubgroup_fixedField H).symm.trans
   (fixedField H.1).restrictNormalHom_ker.symm)).trans <|
@@ -234,7 +234,7 @@ noncomputable def normalAutEquivQuotient [IsGalois k K]
 
 open IntermediateField in
 lemma normalAutEquivQuotient_apply [IsGalois k K]
-    (H : ClosedSubgroup Gal(K/k)) [Subgroup.Normal H.1] (σ : Gal(K/k)) :
+    (H : ClosedSubgroup Gal(K/k)) [H.Normal] (σ : Gal(K/k)) :
     normalAutEquivQuotient H σ = (restrictNormalHom (fixedField H.1)) σ := rfl
 
 open IntermediateField in
