0c83ddc6bb82f520ddcf6603ed561093add1eb7f
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-12-02 09:13:25 +0100

    feat(AlgebraicTopology): relative morphisms of simplicial sets

--- Mathlib.lean
+++ Mathlib.lean
@@ -1428,6 +1428,7 @@ public import Mathlib.AlgebraicTopology.SimplicialSet.NonDegenerateSimplices
 public import Mathlib.AlgebraicTopology.SimplicialSet.NonDegenerateSimplicesSubcomplex
 public import Mathlib.AlgebraicTopology.SimplicialSet.Op
 public import Mathlib.AlgebraicTopology.SimplicialSet.Path
+public import Mathlib.AlgebraicTopology.SimplicialSet.RelativeMorphism
 public import Mathlib.AlgebraicTopology.SimplicialSet.Simplices
 public import Mathlib.AlgebraicTopology.SimplicialSet.StdSimplex
 public import Mathlib.AlgebraicTopology.SimplicialSet.StrictSegal
