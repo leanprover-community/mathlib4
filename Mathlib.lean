df1c5ca06981003be2e3d49b7de4a520fd509d2a
bjornsolheim <133647506+bjornsolheim@users.noreply.github.com>
2025-12-02 15:31:07 +0100

    added Simplicial.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -4179,6 +4179,7 @@ public import Mathlib.FieldTheory.Tower
 public import Mathlib.Geometry.Convex.Cone.Basic
 public import Mathlib.Geometry.Convex.Cone.Dual
 public import Mathlib.Geometry.Convex.Cone.Pointed
+public import Mathlib.Geometry.Convex.Cone.Simplicial
 public import Mathlib.Geometry.Convex.Cone.TensorProduct
 public import Mathlib.Geometry.Euclidean.Altitude
 public import Mathlib.Geometry.Euclidean.Angle.Bisector
