0a714ca6e684022951077787a26de2dacaaa7d1e
Remy Degenne <remydegenne@gmail.com>
2025-12-02 13:41:23 +0100

    add MemLp lemma

--- Mathlib/Probability/Moments/SubGaussian.lean
+++ Mathlib/Probability/Moments/SubGaussian.lean
@@ -692,6 +692,9 @@ lemma integrableExpSet_eq_univ (hX : HasSubgaussianMGF X c μ) :
   ext t
   simpa using hX.integrable_exp_mul t
 
+lemma memLp (hX : HasSubgaussianMGF X c μ) (p : ℝ≥0) : MemLp X p μ :=
+  memLp_of_mem_interior_integrableExpSet (by simp [integrableExpSet_eq_univ hX]) p
+
 lemma integrable (hX : HasSubgaussianMGF X c μ) : Integrable X μ :=
   integrable_of_mem_interior_integrableExpSet (by simp [integrableExpSet_eq_univ hX])
 
