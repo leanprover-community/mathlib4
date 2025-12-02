2b4ac8ee8ef6589020777f9ac0be0439c9421a13
Garmelon <11077553+Garmelon@users.noreply.github.com>
2025-12-02 19:39:24 +0000

    ci: decommission speedcenter (#32358)
    Since the speedcenter has been shut down and replaced by https://radar.lean-lang.org/, this PR updates or removes all the speedcenter-related infrastructure I could find in mathlib.
    
    The most significant change is that the `bench-after-CI` label no longer has any effect. Instead, radar respects the existing `awaiting-CI` tag for `!bench` commands. The workflow that replaces `bench-after-CI` is now:
    1. Tag your PR with `awaiting-CI` (otherwise the `!bench` will start executing immediately)
    2. Send a `!bench`/`!radar` command
    
    Co-authored-by: Joscha <joscha@plugh.de>

--- lakefile.lean
+++ lakefile.lean
@@ -138,7 +138,7 @@ lean_exe «check_title_labels» where
   srcDir := "scripts"
 
 /--
-`lake exe pole` queries the Mathlib speedcenter for build times for the current commit,
+`lake exe pole` queries radar for build times for the current commit,
 and then calculates the longest pole
 (i.e. the sequence of files you would be waiting for during a infinite parallelism build).
 -/
