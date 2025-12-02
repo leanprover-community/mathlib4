44510ecf2264343d6dbac5edb60fb2b2e277d099
Yuval Filmus <filmus.yuval@gmail.com>
2025-12-02 17:39:19 +0200

    Defined arcosh and proved that it is an inverse over the appropriate domains

--- Mathlib/Analysis/SpecialFunctions/Arsinh.lean
+++ Mathlib/Analysis/SpecialFunctions/Arsinh.lean
@@ -13,10 +13,13 @@ public import Mathlib.Analysis.SpecialFunctions.Log.Basic
 
 In this file we prove that sinh is bijective and hence has an
 inverse, arsinh.
+We also define a principal inverse of cosh, arcosh.
 
 ## Main definitions
 
 - `Real.arsinh`: The inverse function of `Real.sinh`.
+
+- `Real.arcosh`: The inverse function of `Real.cosh`.
 
 - `Real.sinhEquiv`, `Real.sinhOrderIso`, `Real.sinhHomeomorph`: `Real.sinh` as an `Equiv`,
   `OrderIso`, and `Homeomorph`, respectively.
@@ -32,9 +35,12 @@ inverse, arsinh.
   continuous, differentiable, and continuously differentiable; we also provide dot notation
   convenience lemmas like `Filter.Tendsto.arsinh` and `ContDiffAt.arsinh`.
 
+- `Real.cos_arcosh`, `Real.arcosh_cosh`: `Real.arcosh` is an inverse of `Real.cosh`
+
 ## Tags
 
 arsinh, arcsinh, argsinh, asinh, sinh injective, sinh bijective, sinh surjective
+arcosh, arccosh, argcosh, acosh
 -/
 
 @[expose] public section
@@ -175,6 +181,36 @@ theorem contDiff_arsinh {n : ℕ∞} : ContDiff ℝ n arsinh :=
 @[continuity]
 theorem continuous_arsinh : Continuous arsinh :=
   sinhHomeomorph.symm.continuous
+
+/-- `arcosh` is defined using a logarithm, `arcosh x = log (x + sqrt(x^2 - 1))`. -/
+@[pp_nodot]
+def arcosh (x : ℝ) :=
+  log (x + √(x ^ 2 - 1))
+
+theorem exp_arcosh (x : ℝ) (hx : 1 ≤ x) : exp (arcosh x) = x + √(x ^ 2 - 1) := by
+  apply exp_log
+  positivity
+
+/-- `arcosh` is the right inverse of `cosh` over [1, ∞). -/
+@[simp]
+theorem cosh_arcosh (x : ℝ) (hx : 1 ≤ x) : cosh (arcosh x) = x := by
+  unfold arcosh
+  have inv : (x + √(x ^ 2 - 1))⁻¹ = x - √(x ^ 2 - 1) := by
+    apply inv_eq_of_mul_eq_one_right
+    rw [← pow_two_sub_pow_two, sq_sqrt (sub_nonneg_of_le (one_le_pow₀ hx)), sub_sub_cancel]
+  rw [cosh_eq, exp_neg, exp_log (by positivity), inv]
+  ring
+
+/-- `arcosh` is the left inverse of `cosh` over [0, ∞). -/
+@[simp]
+theorem arcosh_cosh (x : ℝ) (hx : 0 ≤ x) : arcosh (cosh x) = x := by
+  unfold arcosh
+  apply exp_eq_exp.mp
+  rw [exp_log (by positivity)]
+  apply add_eq_of_eq_sub'
+  rw [exp_sub_cosh]
+  apply (sq_eq_sq₀ (by positivity) (by positivity)).mp
+  rw [← sinh_sq, sq_sqrt (by positivity)]
 
 end Real
 
