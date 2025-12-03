a158305894f8afbdb7758d5d382aa83d5d8c51e5
thorimur <thomasmurrills@gmail.com>
2025-12-03 16:50:30 -0500

    chore: turn on via init

--- Mathlib/Tactic/Linter/UnusedInstancesInType.lean
+++ Mathlib/Tactic/Linter/UnusedInstancesInType.lean
@@ -243,7 +243,7 @@ which are not used in the remainder of the type. If so, it suggests modifying th
 This linter fires only on theorems. (This includes `lemma`s and `instance`s of `Prop` classes.)
 -/
 register_option linter.unusedFintypeInType : Bool := {
-  defValue := true
+  defValue := false
   descr := "enable the unused `Fintype` instance linter, which lints against `Fintype` \
     instances in the hypotheses of theorems which are not used in the type, and can therefore be \
     replaced by a hypothesis of `Finite` or removed entirely."
