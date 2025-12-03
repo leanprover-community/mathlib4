41e023a9c2c3d438d11960b6102e89104086d64f
fbarroero <fbarroero@gmail.com>
2025-12-03 20:47:04 +0100

    Implement suggestions

--- Mathlib/NumberTheory/MahlerMeasure.lean
+++ Mathlib/NumberTheory/MahlerMeasure.lean
@@ -33,44 +33,33 @@ private lemma degree_map_int_castRingHom (p : ℤ[X]) :
 lemma one_le_mahlerMeasure_of_ne_zero {p : ℤ[X]} (hp : p ≠ 0) :
     1 ≤ (p.map (castRingHom ℂ)).mahlerMeasure := by
   apply le_trans _ (p.map (castRingHom ℂ)).leading_coeff_le_mahlerMeasure
-  rw [leadingCoeff, p.degree_map_int_castRingHom]
-  simp only [coeff_map, coeff_natDegree, eq_intCast, Complex.norm_intCast]
-  exact_mod_cast one_le_abs <| leadingCoeff_ne_zero.mpr hp
+  rw [leadingCoeff_map_of_injective (castRingHom ℂ).injective_int, eq_intCast]
+  norm_cast
+  exact one_le_abs <| leadingCoeff_ne_zero.mpr hp
 
 section Northcott
 
 variable {n : ℕ} {B₁ B₂ : Fin (n + 1) → ℝ}
 
 local notation3 "BoxPoly" =>
-  {p : ℤ[X] | p.natDegree ≤ n ∧ ∀ i : Fin (n + 1), B₁ i ≤ p.coeff i ∧ p.coeff i ≤ B₂ i}
+  {p : ℤ[X] | p.natDegree < n + 1 ∧ ∀ i, B₁ i ≤ p.coeff i ∧ p.coeff i ≤ B₂ i}
 
 open Finset in
 theorem card_eq_of_natDegree_le_of_coeff_le (h_B : ∀ i, ⌈B₁ i⌉ ≤ ⌊B₂ i⌋) :
-    Nat.card BoxPoly = ∏ i : Fin (n + 1), (⌊B₂ i⌋ - ⌈B₁ i⌉ + 1).toNat  := by
-  let Btop := fun i ↦ ⌊B₂ i⌋
-  let Bbot := fun i ↦ ⌈B₁ i⌉
-  let Box := Icc Bbot Btop
-  let f : BoxPoly → Box := fun p => ⟨toFn (n + 1) p , by
-    simp only [mem_Icc, Box, Bbot, Btop]
-    refine ⟨fun i ↦ ceil_le.mpr (p.property.2 i).1, fun i ↦ le_floor.mpr (p.property.2 i).2⟩⟩
-  let g : Box → BoxPoly := fun p => ⟨ofFn (n + 1) p, by
-    refine ⟨Nat.le_of_lt_add_one <| ofFn_natDegree_lt (Nat.le_add_left 1 n) p.val, ?_⟩
-    intro i
-    obtain ⟨_, prop⟩ := p
-    simp only [mem_Icc, Box] at prop
-    simp only [Fin.is_lt, ofFn_coeff_eq_val_of_lt, Fin.eta]
-    refine ⟨ceil_le.mp (prop.1 i), le_floor.mp (prop.2 i)⟩⟩
-  have hfBij : f.Bijective := by
-    refine Function.bijective_iff_has_inverse.mpr ⟨g, ?_, ?_⟩
-    · intro p
-      apply Subtype.ext
-      simp [f, g, ofFn_comp_toFn_eq_id_of_natDegree_lt <| Nat.lt_succ_iff.mpr p.property.1]
-    · intro ⟨_, _⟩
-      ext
-      simp [toFn, f, g]
-  rw [Nat.card_eq_of_bijective f hfBij, Nat.card_eq_finsetCard Box, Pi.card_Icc]
-  simp_rw [card_Icc]
-  grind
+    Nat.card BoxPoly = ∏ i, (⌊B₂ i⌋ - ⌈B₁ i⌉ + 1).toNat := by
+  let e : BoxPoly ≃ Icc (⌈B₁ ·⌉) (⌊B₂ ·⌋) := {
+    toFun p := ⟨toFn (n + 1) p, by
+      have prop := p.property.2
+      simpa using ⟨fun i ↦ ceil_le.mpr (prop i).1, fun i ↦ le_floor.mpr (prop i).2⟩⟩
+    invFun p := ⟨ofFn (n + 1) p, by
+      refine ⟨ofFn_natDegree_lt (Nat.le_add_left 1 n) p.val, fun i ↦ ?_⟩
+      have prop := mem_Icc.mp p.property
+      simpa using ⟨ceil_le.mp (prop.1 i), le_floor.mp (prop.2 i)⟩⟩
+    left_inv p := by grind [ofFn_comp_toFn_eq_id_of_natDegree_lt]
+    right_inv p := by grind [toFn_comp_ofFn_eq_id]
+  }
+  rw [Nat.card_congr e, Nat.card_eq_finsetCard, Pi.card_Icc]
+  grind [card_Icc]
 
 open Nat NNReal in
 /-- An upper bound on the number of integer polynomials of degree at most `n` and Mahler measure at
@@ -78,21 +67,20 @@ most `B`, also known as Northcott's Theorem. -/
 theorem card_mahlerMeasure_le_prod (n : ℕ) (B : ℝ≥0) :
     Nat.card {p : ℤ[X] | p.natDegree ≤ n ∧ (p.map (Int.castRingHom ℂ)).mahlerMeasure ≤ B} ≤
     ∏ i : Fin (n + 1), (2 * ⌊n.choose i * B⌋₊ + 1) := by
+  simp_rw [← Nat.lt_add_one_iff (n := n)]
   have h_card :
-      Nat.card {p : ℤ[X] | p.natDegree ≤ n ∧ ∀ i : Fin (n + 1), ‖p.coeff i‖ ≤ n.choose i * B} =
+      Nat.card {p : ℤ[X] | p.natDegree < n + 1 ∧ ∀ i : Fin (n + 1), ‖p.coeff i‖ ≤ n.choose i * B} =
       ∏ i : Fin (n + 1), (2 * ⌊n.choose i * B⌋₊ + 1) := by
-    have h_B (i : Fin (n + 1)) : ⌈- (n.choose i * B  : ℝ)⌉ ≤ ⌊(n.choose i * B : ℝ)⌋ := by
+    have h_B (i : Fin (n + 1)) : ⌈-(n.choose i * B  : ℝ)⌉ ≤ ⌊(n.choose i * B : ℝ)⌋ := by
       simp only [ceil_neg, neg_le_self_iff, floor_nonneg]
       positivity
-    conv => enter [1,1,1,1]; ext p; enter [2]; ext i; rw [norm_eq_abs, abs_le]
+    conv => enter [1, 1, 1, 1, p, 2, i]; rw [norm_eq_abs, abs_le]
     rw [card_eq_of_natDegree_le_of_coeff_le h_B]
     simp only [ceil_neg, sub_neg_eq_add, ← two_mul]
-    apply congr_arg
-    ext i
+    apply Finset.prod_congr rfl fun i _ ↦ ?_
     zify
     rw [toNat_of_nonneg (by positivity), ← Int.natCast_floor_eq_floor (by positivity)]
-    simp
-    rfl
+    norm_cast
   rw [← h_card]
   apply card_mono <| finite_of_card_ne_zero _
   · gcongr with p hp
@@ -100,13 +88,10 @@ theorem card_mahlerMeasure_le_prod (n : ℕ) (B : ℝ≥0) :
     rw [show ‖p.coeff d‖ = ‖(p.map (Int.castRingHom ℂ)).coeff d‖ by aesop]
     apply le_trans <| (p.map (Int.castRingHom ℂ)).norm_coeff_le_choose_mul_mahlerMeasure d
     gcongr
-    · exact mahlerMeasure_nonneg (map (Int.castRingHom ℂ) p)
-    · rw [p.degree_map_int_castRingHom]
-      by_cases hd : d ≤ p.natDegree
-      · exact choose_le_choose d hp
-      · grind [choose_ne_zero_iff, choose_zero_right]
+    · exact mahlerMeasure_nonneg _
+    · exact choose_le_choose _ <| by rw [p.degree_map_int_castRingHom]; grind
   · rw [h_card, Finset.prod_ne_zero_iff]
-    omega
+    grind
 
 end Northcott
 
