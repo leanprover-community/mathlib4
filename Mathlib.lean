1b6c732d51150ba1ea9ee9e0a8f4f2f660785005
Rémy Degenne <4094732+RemyDegenne@users.noreply.github.com>
2025-12-03 13:44:49 +0000

    feat: products of identically distributed random variables (#31545)
    If `X` and `Y` are independent random variables, `Z` and `W` are independent random variables, such that `X` and `Z` are identically distributed and `Y` and `W` are identically distributed, then the pairs `(X, Y)`  and `(Z, W)` are identically distributed.
    
    Also add a similar result for products of countably many random variables.
    
    From the LeanBandits project.
    
    Co-authored-by: Remy Degenne <remydegenne@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -12812,6 +12812,7 @@ public import Mathlib.Probability.Distributions.Uniform
 public import Mathlib.Probability.HasLaw
 public import Mathlib.Probability.HasLawExists
 public import Mathlib.Probability.IdentDistrib
+public import Mathlib.Probability.IdentDistribIndep
 public import Mathlib.Probability.Independence.Basic
 public import Mathlib.Probability.Independence.BoundedContinuousFunction
 public import Mathlib.Probability.Independence.CharacteristicFunction
