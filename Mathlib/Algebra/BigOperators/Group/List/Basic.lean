e7d59fecc806a53314bd15929e6995d0b47afa45
Yongshun Ye <ShreckYe@gmail.com>
2025-12-03 18:08:18 +0800

    Adjust the formatting

--- Mathlib/Algebra/BigOperators/Group/List/Basic.lean
+++ Mathlib/Algebra/BigOperators/Group/List/Basic.lean
@@ -228,8 +228,7 @@ theorem CommMonoid.prod_insertIdx {i} (h : i ≤ l.length) : (l.insertIdx i a).p
   List.prod_insertIdx h (fun a' _ ↦ Commute.all a a')
 
 @[to_additive (attr := simp)]
-theorem CommMonoid.mul_prod_eraseIdx {i} (h : i < l.length) :
-    l[i] * (l.eraseIdx i).prod = l.prod :=
+theorem CommMonoid.mul_prod_eraseIdx {i} (h : i < l.length) : l[i] * (l.eraseIdx i).prod = l.prod :=
   List.mul_prod_eraseIdx h (fun a' _ ↦ Commute.all l[i] a')
 
 @[to_additive (attr := simp)]
