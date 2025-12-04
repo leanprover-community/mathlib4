34ae68c2c72d07f2dff8d6e239da7e5641d0795e
thorimur <68410468+thorimur@users.noreply.github.com>
2025-12-04 15:27:29 -0500

    chore: docs tweaks

--- Mathlib/Lean/Linter.lean
+++ Mathlib/Lean/Linter.lean
@@ -9,7 +9,7 @@ public meta import Lean.Elab.Command
 public meta import Lean.Linter.Basic
 
 /-
-# Additional utilities for the `Linter` API
+# Additional utilities and boilerplate for the `Linter` API
 -/
 
 public meta section
@@ -23,10 +23,12 @@ variable {m : Type → Type} [Monad m] [MonadOptions m] [MonadEnv m]
 /--
 Runs a `CommandElabM` action when the provided linter option is `true`.
 
-This function assumes you have already called `withSetOptionIn`: use `whenLinterActivated`
-to do this automatically.
+This function assumes you have already called `withSetOptionIn`; use `whenLinterActivated`
+to do so automatically. At the start of linter code, `whenLinterActivated` should be preferred when
+possible.
 
-Note: this definition is marked as `@[macro_inline]`, so it is okay to supply it with a linter option which has been registered in the same module.
+Note: this definition is marked as `@[macro_inline]`, so it is okay to supply it with a linter
+option which has been registered in the same module.
 -/
 @[expose, macro_inline]
 def whenLinterOption (opt : Lean.Option Bool) (x : m Unit) : m Unit := do
@@ -35,7 +37,8 @@ def whenLinterOption (opt : Lean.Option Bool) (x : m Unit) : m Unit := do
 /--
 Runs a `CommandElabM` action when the provided linter option is `false`.
 
-Note: this definition is marked as `@[macro_inline]`, so it is okay to supply it with a linter option which has been registered in the same module.
+Note: this definition is marked as `@[macro_inline]`, so it is okay to supply it with a linter
+option which has been registered in the same module.
 -/
 @[expose, macro_inline]
 def whenNotLinterOption (opt : Lean.Option Bool) (x : m Unit) : m Unit := do
@@ -54,7 +57,8 @@ def myLinter : Linter where
     ...
 ```
 
-Note: this definition is marked as `@[macro_inline]`, so it is okay to supply it with a linter option which has been registered in the same module.
+Note: this definition is marked as `@[macro_inline]`, so it is okay to supply it with a linter
+option which has been registered in the same module.
 -/
 @[expose, macro_inline]
 def whenLinterActivated (opt : Lean.Option Bool) (x : CommandElab) (breakOnError := true) :
