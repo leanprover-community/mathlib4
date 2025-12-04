b5a781398d1e72df3c0ec43ebc40a4c706958844
Alastair Irving <29164966+ajirving@users.noreply.github.com>
2025-12-04 20:05:26 +0000

    Relating theta to pi

--- Mathlib/NumberTheory/Chebyshev.lean
+++ Mathlib/NumberTheory/Chebyshev.lean
@@ -5,6 +5,9 @@ Authors: Alastair Irving, Ruben Van de Velde
 -/
 module
 
+public import Mathlib.Algebra.Order.Floor.Semifield
+public import Mathlib.NumberTheory.AbelSummation
+public import Mathlib.NumberTheory.PrimeCounting
 public import Mathlib.NumberTheory.Primorial
 public import Mathlib.NumberTheory.ArithmeticFunction.VonMangoldt
 
@@ -43,6 +46,7 @@ Parts of this file were upstreamed from the PrimeNumberTheoremAnd project by Kon
 open Nat hiding log
 open Finset Real
 open ArithmeticFunction hiding log
+open scoped Nat.Prime
 
 namespace Chebyshev
 
@@ -127,5 +131,98 @@ theorem theta_le_log4_mul_x {x : ℝ} (hx : 0 ≤ x) : θ x ≤ log 4 * x := by
   rw [Real.log_pow, mul_comm]
   gcongr
   exact floor_le hx
+
+lemma deriv_log_inv {x : ℝ} (h0 : x ≠ 0) (h1 : x ≠ 1) (h2 : x ≠ -1) :
+    deriv (fun x => (log x)⁻¹) x = -x⁻¹ / (log x ^ 2) := by
+  rw [deriv_fun_inv'', deriv_log]
+  · fun_prop (disch := assumption)
+  exact log_ne_zero.mpr ⟨h0, h1, h2⟩
+
+open MeasureTheory in
+theorem integrable_theta_div (x : ℝ) :
+    IntegrableOn (fun t ↦ (θ t) / (t * log t ^ 2)) (Set.Icc 2 x) volume := by
+  conv => arg 1; ext; rw [theta, div_eq_mul_one_div, mul_comm, sum_filter]
+  apply integrableOn_mul_sum_Icc _ (by norm_num)
+  apply ContinuousOn.integrableOn_Icc
+  intro x hx
+  apply ContinuousAt.continuousWithinAt
+  have : x ≠ 0 := by linarith [hx.1]
+  have : x * log x ^ 2 ≠ 0 := by
+    apply mul_ne_zero this
+    apply pow_ne_zero _ <| log_ne_zero_of_pos_of_ne_one _ _ <;> linarith [hx.1]
+  fun_prop (disch := assumption)
+
+open MeasureTheory in
+theorem primeCounting_eq (x : ℝ) (hx : 2 ≤ x) :
+    π ⌊x⌋₊ = θ x / log x + ∫ t in Set.Icc 2 x, θ t / (t * log t ^ 2) := by
+  simp only [primeCounting, primeCounting', count_eq_card_filter_range]
+  rw [card_eq_sum_ones, range_succ_eq_Icc_zero]
+  rw [← add_sum_erase (a := 2) _ _ (by simp [prime_two, le_floor hx])]
+  trans 1 + ∑ x ∈ Ioc 2 ⌊x⌋₊ with x.Prime, 1
+  · norm_cast
+    congr
+    ext p
+    constructor <;> intro h
+    · simp_all only [mem_erase, mem_filter, mem_Icc, _root_.zero_le, true_and, mem_Ioc,
+      and_true]
+      exact lt_of_le_of_ne h.2.2.two_le h.1.symm
+    · simp_all only [mem_filter, mem_Ioc, mem_erase, mem_Icc, _root_.zero_le, and_self,
+      and_true]
+      exact h.1.1.ne.symm
+  rw [sum_filter]
+  let a : ℕ → ℝ := Set.indicator (setOf Nat.Prime) (fun n => log n)
+  trans 1 + ∑ n ∈ Ioc 2 ⌊x⌋₊, (1 / log n) * a n
+  · congr 1
+    apply sum_congr rfl fun n hn ↦ ?_
+    unfold a
+    split_ifs with h
+    · simp only [one_div, Set.mem_setOf_eq, h, Set.indicator_of_mem]
+      have : log n ≠ 0 := by
+        apply log_ne_zero_of_pos_of_ne_one <;> norm_cast
+        exacts [h.pos, h.ne_one]
+      field
+    · simp [h]
+  rw [(by simp : 2 = ⌊(2 : ℝ)⌋₊)]
+  rw [sum_mul_eq_sub_sub_integral_mul a (f := fun n ↦ 1 / log n) (by linarith) (by linarith)]
+  · have int_deriv (f : ℝ → ℝ) :
+      ∫ (u : ℝ) in Set.Ioc 2 x, deriv (fun x ↦ 1 / log x) u * f u =
+      ∫ (u : ℝ) in Set.Icc 2 x, f u * -(u * log u ^ 2)⁻¹ := by
+      rw [← integral_Icc_eq_integral_Ioc]
+      apply setIntegral_congr_ae measurableSet_Icc
+      refine Filter.Eventually.of_forall (fun u hu => ?_)
+      simp only [one_div, mul_inv_rev, mul_neg]
+      rw [deriv_log_inv]
+      · ring
+      all_goals linarith [hu.1]
+    rw [int_deriv]
+    have : log 2 ≠ 0 := by simp; linarith
+    simp [a, Set.indicator_apply, sum_filter, show Icc 0 2 = {0, 1, 2} by ext; simp; omega,
+      prime_two, theta_eq_sum_Icc, this, MeasureTheory.integral_neg]
+    ring_nf!
+    congr
+    ext
+    ring
+  · intro z hz
+    have : z ≠ 0 := by linarith [hz.1]
+    have : log z ≠ 0 := by
+      apply log_ne_zero_of_pos_of_ne_one <;> linarith [hz.1]
+    fun_prop (disch := assumption)
+  · simp only [one_div]
+    have : ∀ y ∈ Set.Icc 2 x, deriv (fun x ↦ (log x)⁻¹) y = -(y * log y ^ 2)⁻¹:= by
+      intro y hy
+      rw [deriv_log_inv, mul_inv, ← div_eq_mul_inv, neg_div]
+      all_goals linarith [hy.1]
+    apply ContinuousOn.integrableOn_Icc
+    intro z hz
+    apply ContinuousWithinAt.congr (f := fun x => - (x * log x ^ 2)⁻¹)
+    · apply ContinuousWithinAt.neg
+      apply ContinuousAt.continuousWithinAt
+      have : z ≠ 0 := by linarith [hz.1]
+      have : z * (log z) ^ 2 ≠ 0 := by
+        apply mul_ne_zero this <| pow_ne_zero _ <| log_ne_zero.mpr ⟨_, _, _⟩
+        all_goals linarith [hz.1]
+      fun_prop (disch := assumption)
+    · apply this
+    · apply this z hz
 
 end Chebyshev
