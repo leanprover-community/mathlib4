baccd54ff4b721d47347cda70e547bdc59df9817
Junyan Xu <junyanxu.math@gmail.com>
2025-12-05 15:16:16 +0100

    try priority high

--- Mathlib/Algebra/Group/Defs.lean
+++ Mathlib/Algebra/Group/Defs.lean
@@ -748,7 +748,8 @@ class AddCommMonoid (M : Type u) extends AddMonoid M, AddCommSemigroup M
 @[to_additive]
 class CommMonoid (M : Type u) extends Monoid M, CommSemigroup M
 
-@[to_additive] instance (M) [CommMonoid M] : IsDedekindFiniteMonoid M := inferInstance
+@[to_additive] instance (priority := high) (M) [CommMonoid M] :
+    IsDedekindFiniteMonoid M := inferInstance
 
 section LeftCancelMonoid
 
