d8b96c2ba8c7bd63473ac103f3e9f1d4467a1505
morrison-daniel <39346894+morrison-daniel@users.noreply.github.com>
2025-12-04 19:14:49 -0800

    clean up DirectSum

--- Mathlib/LinearAlgebra/Multilinear/DirectSum.lean
+++ Mathlib/LinearAlgebra/Multilinear/DirectSum.lean
@@ -25,23 +25,22 @@ This file describes multilinear maps on direct sums.
 
 namespace MultilinearMap
 
-open DirectSum LinearMap Function
+open DirectSum
 
-variable (R : Type*) [CommSemiring R]
-variable {ι : Type*} [Fintype ι] [DecidableEq ι]
-variable (κ : ι → Type*) [(i : ι) → DecidableEq (κ i)]
-variable {M : (i : ι) → κ i → Type*} {M' : Type*}
-variable [Π i (j : κ i), AddCommMonoid (M i j)] [AddCommMonoid M']
-variable [Π i (j : κ i), Module R (M i j)] [Module R M']
+variable {R ι M' : Type*} {κ : ι → Type*} {M : (i : ι) → κ i → Type*}
+variable [CommSemiring R] [Fintype ι] [(i : ι) → DecidableEq (κ i)]
+variable [∀ i (j : κ i), AddCommMonoid (M i j)] [AddCommMonoid M']
+variable [∀ i (j : κ i), Module R (M i j)] [Module R M']
 
-omit [DecidableEq ι] in
 /-- Two multilinear maps from direct sums are equal if they agree on the generators. -/
 @[ext]
 theorem directSum_ext ⦃f g : MultilinearMap R (fun i ↦ ⨁ j : κ i, M i j) M'⦄
     (h : ∀ p : Π i, κ i,
-      f.compLinearMap (fun i => DirectSum.lof R _ _ (p i)) =
-      g.compLinearMap (fun i => DirectSum.lof R _ _ (p i))) : f = g :=
+      f.compLinearMap (fun i => DirectSum.lof _ _ _ (p i)) =
+      g.compLinearMap (fun i => DirectSum.lof _ _ _ (p i))) : f = g :=
   dfinsupp_ext h
+
+variable [DecidableEq ι]
 
 /-- The linear equivalence between families indexed by `p : Π i : ι, κ i` of multilinear maps
 on the `fun i ↦ M i (p i)` and the space of multilinear map on `fun i ↦ ⨁ j : κ i, M i j`. -/
@@ -54,7 +53,7 @@ def fromDirectSumEquiv :
 theorem fromDirectSumEquiv_lof
     (f : Π (p : Π i, κ i), MultilinearMap R (fun i ↦ M i (p i)) M')
     (p : Π i, κ i) (x : Π i, M i (p i)) :
-    fromDirectSumEquiv R κ f (fun i => DirectSum.lof R (κ i) _ (p i) (x i)) = f p x :=
+    fromDirectSumEquiv f (fun i => lof R _ _ _ (x i)) = f p x :=
   fromDFinsuppEquiv_single _ _ _
 
 /-- Prefer using `fromDirectSumEquiv_lof` where possible. -/
@@ -62,14 +61,14 @@ theorem fromDirectSumEquiv_apply
     [Π i (j : κ i) (x : M i j), Decidable (x ≠ 0)]
     (f : Π (p : Π i, κ i), MultilinearMap R (fun i ↦ M i (p i)) M')
     (x : Π i, ⨁ (j : κ i), M i j) :
-    fromDirectSumEquiv R κ f x =
+    fromDirectSumEquiv f x =
       ∑ p ∈ Fintype.piFinset (fun i ↦ (x i).support), f p (fun i ↦ x i (p i)) :=
   fromDFinsuppEquiv_apply _ _
 
 @[simp]
 theorem fromDirectSumEquiv_symm_apply (f : MultilinearMap R (fun i ↦ ⨁ j : κ i, M i j) M')
     (p : Π i, κ i) :
-    (fromDirectSumEquiv R κ).symm f p = f.compLinearMap (fun i ↦ DirectSum.lof R (κ i) _ (p i)) :=
+    fromDirectSumEquiv.symm f p = f.compLinearMap (fun i ↦ DirectSum.lof _ _ _ (p i)) :=
   rfl
 
 end MultilinearMap
