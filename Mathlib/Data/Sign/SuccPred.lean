1fa7e9edb59b3820b3603770d2e05c2e4a8169ce
Violeta Hernández <vi.hdz.p@gmail.com>
2025-12-04 18:27:40 -0600

    add simp lemmas

--- Mathlib/Data/Sign/SuccPred.lean
+++ Mathlib/Data/Sign/SuccPred.lean
@@ -16,6 +16,10 @@ This file defines the `SuccOrder` and `PredOrder` instances for `SignType`.
 
 @[expose] public section
 
+open Order
+
+namespace SignType
+
 instance : SuccOrder SignType where
   succ
   | -1 => 0
@@ -23,6 +27,10 @@ instance : SuccOrder SignType where
   le_succ := by decide
   max_of_succ_le := by unfold IsMax; decide
   succ_le_of_lt := by decide
+
+@[simp] theorem succ_neg_one : succ -1 = 0 := rfl
+@[simp] theorem succ_zero : succ 0 = 1 := rfl
+@[simp] theorem succ_one : succ 1 = 1 := rfl
 
 -- TODO: use `to_dual`
 instance : PredOrder SignType where
@@ -32,3 +40,9 @@ instance : PredOrder SignType where
   pred_le := by decide
   min_of_le_pred := by unfold IsMin; decide
   le_pred_of_lt := by decide
+
+@[simp] theorem pred_neg_one : pred -1 = -1 := rfl
+@[simp] theorem pred_zero : pred 0 = -1 := rfl
+@[simp] theorem pred_one : pred 1 = 0 := rfl
+
+end SignType
