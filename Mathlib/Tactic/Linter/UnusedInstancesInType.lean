e7e517b36e4a2c069f371ccd5509f1a538477186
thorimur <68410468+thorimur@users.noreply.github.com>
2025-12-05 21:08:35 -0500

    temp: profile

--- Mathlib/Tactic/Linter/UnusedInstancesInType.lean
+++ Mathlib/Tactic/Linter/UnusedInstancesInType.lean
@@ -265,21 +265,21 @@ def unusedDecidableInType : Linter where
         | _ => pure () -- invalid option value, should be caught during elaboration
     unless override || getLinterValue linter.unusedDecidableInType (← getLinterOptions) do
       return
-    -- let opts := (← getOptions).setNat `profiler.threshold 50 |>.setBool `profiler true
-    -- profileitM Exception "uiit" opts do
-    cmd.logUnusedInstancesInDeclsWhere
-      /- Theorems in the `Decidable` namespace such as `Decidable.eq_or_ne` are allowed to depend
-      on decidable instances without using them in the type. -/
-      (declFilter := (!(`Decidable).isPrefixOf ·.name))
-      isDecidableVariant
-      fun _ thm unusedParams => do
-        logLint linter.unusedDecidableInType (← getRef) m!"\
-          {thm.name.unusedInstancesMsg unusedParams}\n\n\
-          Consider removing \
-          {if unusedParams.size = 1 then "this hypothesis" else "these hypotheses"} \
-          and using `classical` in the proof instead. \
-          For terms, consider using `open scoped Classical in` at the term level (not the command \
-          level)."
+    let opts := (← getOptions).setNat `profiler.threshold 50 |>.setBool `profiler true
+    profileitM Exception "uiit" opts do
+      cmd.logUnusedInstancesInDeclsWhere
+        /- Theorems in the `Decidable` namespace such as `Decidable.eq_or_ne` are allowed to depend
+        on decidable instances without using them in the type. -/
+        (declFilter := (!(`Decidable).isPrefixOf ·.name))
+        isDecidableVariant
+        fun _ thm unusedParams => do
+          logLint linter.unusedDecidableInType (← getRef) m!"\
+            {thm.name.unusedInstancesMsg unusedParams}\n\n\
+            Consider removing \
+            {if unusedParams.size = 1 then "this hypothesis" else "these hypotheses"} \
+            and using `classical` in the proof instead. \
+            For terms, consider using `open scoped Classical in` at the term level (not the command \
+            level)."
 
 initialize addLinter unusedDecidableInType
 
