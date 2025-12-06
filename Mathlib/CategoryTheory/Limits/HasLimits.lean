8dfcb6bc5ffd0dccd138036efb892dd54836f5ea
euprunin <178733547+euprunin@users.noreply.github.com>
2025-12-06 21:34:12 +0000

    chore(CategoryTheory/Limits): remove use of `erw` in `limit.pre_π` and `limit.post_π` (#32518)

--- Mathlib/CategoryTheory/Limits/HasLimits.lean
+++ Mathlib/CategoryTheory/Limits/HasLimits.lean
@@ -361,8 +361,7 @@ def limit.pre : limit F ⟶ limit (E ⋙ F) :=
 
 @[reassoc (attr := simp)]
 theorem limit.pre_π (k : K) : limit.pre F E ≫ limit.π (E ⋙ F) k = limit.π F (E.obj k) := by
-  erw [IsLimit.fac]
-  rfl
+  simp [limit.pre]
 
 @[simp]
 theorem limit.lift_pre (c : Cone F) :
@@ -401,8 +400,7 @@ def limit.post : G.obj (limit F) ⟶ limit (F ⋙ G) :=
 
 @[reassoc (attr := simp)]
 theorem limit.post_π (j : J) : limit.post F G ≫ limit.π (F ⋙ G) j = G.map (limit.π F j) := by
-  erw [IsLimit.fac]
-  rfl
+  simp [limit.post]
 
 @[simp]
 theorem limit.lift_post (c : Cone F) :
