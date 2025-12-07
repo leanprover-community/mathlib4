dbcde05993f4b8a6348b20e91f528f2d717b0c15
Anishrajeev210 <37055371+Anishrajeev210@users.noreply.github.com>
2025-12-07 11:34:12 -0500

    Defined Type Space and proved properties

--- Mathlib/ModelTheory/Types.lean
+++ Mathlib/ModelTheory/Types.lean
@@ -1,11 +1,14 @@
 /-
 Copyright (c) 2022 Aaron Anderson. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Aaron Anderson
+Authors: Aaron Anderson, Anish Rajeev
 -/
 module
 
 public import Mathlib.ModelTheory.Satisfiability
+public import Mathlib.Topology.Separation.Profinite
+public import Mathlib.Topology.Connected.Separation
+public import Mathlib.Topology.Baire.LocallyCompactRegular
 
 /-!
 # Type Spaces
@@ -20,6 +23,8 @@ This file defines the space of complete types over a first-order theory.
 - `FirstOrder.Language.Theory.typeOf` is the type of a given tuple.
 - `FirstOrder.Language.Theory.realizedTypes`: `T.realizedTypes M α` is the set of
   types in `T.CompleteType α` that are realized in `M` - that is, the type of some tuple in `M`.
+- `FirstOrder.Language.Theory.TypeSpace.basis` is the basis of the of the stone space of types
+  over `(n : ℕ)` variables
 
 ## Main Results
 
@@ -27,6 +32,8 @@ This file defines the space of complete types over a first-order theory.
   The space `T.CompleteType α` is nonempty exactly when `T` is satisfiable.
 - `FirstOrder.Language.Theory.CompleteType.exists_modelType_is_realized_in`: Every type is realized
   in some model.
+- `FirstOrder.Language.Theory.TypeSpace.basis` describes a topology that is totally separated,
+  compact, and a Baire space
 
 ## Implementation Notes
 
@@ -44,7 +51,7 @@ This file defines the space of complete types over a first-order theory.
 
 universe u v w w'
 
-open Cardinal Set FirstOrder
+open Cardinal Set FirstOrder TopologicalSpace
 
 namespace FirstOrder
 
@@ -204,6 +211,119 @@ theorem exists_modelType_is_realized_in (p : T.CompleteType α) :
   rfl
 
 end
+
+namespace TypeSpace
+
+variable {T : L.Theory}
+
+/-- Defines the basis for the Type Space and defines a function from formulas to basic
+  clopen sets -/
+def basis {n : ℕ} : Set (Set (T.CompleteType (Fin n))) := {U | ∃ φ, U = {s | φ ∈ s}}
+def BasicOpen {n : ℕ} (φ : L[[Fin n]].Sentence) : Set (T.CompleteType (Fin n)) :=
+  {s : T.CompleteType (Fin n) | φ ∈ s}
+
+lemma finite_inter {n : ℕ} : FiniteInter (α := T.CompleteType (Fin n)) basis := by
+  constructor
+  · use ⊤
+    ext x
+    simp
+    exact Theory.CompleteType.mem_of_models x (fun _ _ _ a ↦ a)
+  · rintro s ⟨φ, rfl⟩ t ⟨ψ, rfl⟩
+    use φ ⊓ ψ
+    ext t
+    simp
+    simp_rw[← SetLike.mem_coe, t.isMaximal.mem_iff_models (φ ⊓ ψ), t.isMaximal.mem_iff_models φ,
+            t.isMaximal.mem_iff_models ψ, Theory.models_sentence_iff]
+    constructor
+    · rintro ⟨a, b⟩ M
+      exact Formula.realize_inf.2 ⟨a M, b M⟩
+    · intro a
+      rw [←forall_and]
+      intro M
+      exact Formula.realize_inf.1 (a M)
+
+lemma basic_open_compl {n : ℕ} (T : L.Theory) (φ : L[[Fin n]].Sentence) :
+                       BasicOpen (T := T) φ.not = (BasicOpen φ)ᶜ := by
+                          ext p ; exact p.not_mem_iff φ
+
+instance (n : ℕ) : TopologicalSpace (T.CompleteType (Fin n)) := generateFrom basis
+
+instance {n : ℕ} : T0Space (T.CompleteType (Fin n)) := by
+  rw [t0Space_iff_inseparable]
+  intro x y a
+  apply SetLike.ext
+  intro φ
+  replace h := inseparable_iff_forall_isOpen.1 a
+  have b : IsOpen (X := (T.CompleteType (Fin n))) (BasicOpen φ) := by
+    apply TopologicalSpace.GenerateOpen.basic
+    use φ ; trivial
+  exact h (BasicOpen φ) b
+
+instance {n : ℕ} : TotallySeparatedSpace (T.CompleteType (Fin n)) := by
+  apply totallySeparatedSpace_of_t0_of_basis_clopen
+  have a := isTopologicalBasis_of_subbasis_of_finiteInter
+            (α := T.CompleteType (Fin n)) (s := basis) (by trivial) finite_inter
+  apply IsTopologicalBasis.of_isOpen_of_subset
+  · intro _ h ; exact h.2
+  · exact a
+  · intro s ⟨φ, b⟩
+    constructor
+    · have h : IsOpen sᶜ := by
+        replace b : s = BasicOpen φ := b
+        rw[b, ←basic_open_compl]
+        apply TopologicalSpace.GenerateOpen.basic
+        exact ⟨φ.not, by trivial⟩
+      rw[←compl_compl s]
+      exact isClosed_compl_iff.2 h
+    · apply TopologicalSpace.GenerateOpen.basic ; use φ
+
+instance {n : ℕ} : T2Space (T.CompleteType (Fin n)) := TotallySeparatedSpace.t2Space
+
+instance {n : ℕ} : CompactSpace (T.CompleteType (Fin n)) := by
+  constructor
+  rw[isCompact_iff_ultrafilter_le_nhds]
+  intros F _
+  let x : L[[Fin n]].Theory := {φ | BasicOpen φ ∈ F}
+  have a : (L.lhomWithConstants (Fin n)).onTheory T ⊆ x := by
+    intro φ a
+    have b : BasicOpen (T := T) φ = Set.univ := by
+      ext y
+      simp
+      exact y.subset a
+    rw[Set.mem_setOf_eq, b]
+    exact Filter.univ_mem
+  have b : Theory.IsMaximal x := by
+    rw[Theory.IsMaximal]
+    constructor
+    · rw[Theory.isSatisfiable_iff_isFinitelySatisfiable, Theory.IsFinitelySatisfiable]
+      intro φᵢ a
+      have b : ∀ φ ∈ φᵢ, BasicOpen φ ∈ F.toFilter := by
+        intro _ b
+        exact a b
+      rw[←Filter.biInter_finset_mem φᵢ] at b
+      obtain ⟨y, c⟩ := F.neBot.nonempty_of_mem b
+      have d : SetLike.coe φᵢ ⊆ y.toTheory := by
+        rwa[Set.mem_iInter₂] at c
+      exact Theory.IsSatisfiable.mono y.isMaximal'.1 d
+    · intro φ
+      rcases Ultrafilter.mem_or_compl_mem F (BasicOpen φ) with  a | b
+      · exact Or.inl a
+      · rw[←basic_open_compl T φ] at b
+        exact Or.inr b
+  let p : T.CompleteType (Fin n) := ⟨x, a, b⟩
+  use p
+  constructor
+  · trivial
+  · rw [nhds_generateFrom]
+    apply le_iInf₂
+    intro _ a
+    simp
+    obtain ⟨_, rfl⟩ := a.2
+    exact a.1
+
+instance {n : ℕ} : BaireSpace (T.CompleteType (Fin n)) := BaireSpace.of_t2Space_locallyCompactSpace
+
+end TypeSpace
 
 end Theory
 
