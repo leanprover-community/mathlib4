297aceba0174604d57c439891ed9b49453119578
Yuval Filmus <filmus.yuval@gmail.com>
2025-12-07 11:48:27 +0200

    Update Mathlib/Algebra/Polynomial/Roots.lean
    Co-authored-by: Laurance Lau <60111599+LLaurance@users.noreply.github.com>

--- Mathlib/Algebra/Polynomial/Roots.lean
+++ Mathlib/Algebra/Polynomial/Roots.lean
@@ -289,16 +289,9 @@ theorem card_roots_X_pow_sub_C {n : ℕ} (hn : 0 < n) (a : R) :
 theorem roots_eq_of_degree {S : Finset R}
     (hS : ∀ x ∈ S, p.eval x = 0) (hcard : S.card = p.degree) : p.roots = S.val := by
   have hp : p ≠ 0 := by contrapose! hcard; simp [hcard]
-  symm; apply Multiset.eq_of_le_of_card_le
-  · apply Finset.val_le_iff_val_subset.mpr
-    intro x hx
-    apply (p.mem_roots hp).mpr
-    apply hS
-    exact hx
-  · change p.roots.card ≤ S.card
-    have := p.card_roots hp
-    rw [←hcard, Nat.cast_le] at this
-    exact this
+  exact (Multiset.eq_of_le_of_card_le
+    (Finset.val_le_iff_val_subset.mpr (fun x hx ↦ (p.mem_roots hp).mpr (hS x hx)))
+    ((WithBot.coe_le hcard.symm).mp (p.card_roots hp))).symm
 
 section NthRoots
 
