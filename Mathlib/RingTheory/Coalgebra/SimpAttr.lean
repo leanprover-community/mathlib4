636a5e4f3e49c56c111c97aeea10946f888600dc
Yaël Dillies <yael.dillies@gmail.com>
2025-12-07 15:01:23 +0100

    fix docstring

--- Mathlib/RingTheory/Coalgebra/SimpAttr.lean
+++ Mathlib/RingTheory/Coalgebra/SimpAttr.lean
@@ -1,13 +1,13 @@
 module
 
 public import Mathlib.Init
-public import Lean.Meta.Tactic.Simp.SimpTheorems
-public import Lean.Meta.Tactic.Simp.RegisterCommand
-public import Lean.LabelAttribute
+import Lean.Meta.Tactic.Simp.SimpTheorems
+import Lean.Meta.Tactic.Simp.RegisterCommand
+import Lean.LabelAttribute
 
 public meta section
 
-/- `coassoc_simps` is a simp set useful to prove tautologies on coalgebras.
+/-- `coassoc_simps` is a simp set useful to prove tautologies on coalgebras.
 It mimics the strategy of `mon_tauto`.
 See the docstring of `mon_tauto` for a more detailed overview. -/
 register_simp_attr coassoc_simps
