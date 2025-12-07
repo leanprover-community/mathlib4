91e0ff2f5cf5f900b7f77a8072e999a57d42ed56
Success Moses <syko2021@gmail.com>
2025-12-07 09:51:11 +0000

    copyright

--- Mathlib/Topology/EMetricSpace/Arclength.lean
+++ Mathlib/Topology/EMetricSpace/Arclength.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2023 Junyan Xu. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Junyan Xu
+Authors: Junyan Xu, Success Moses
 -/
 module
 
