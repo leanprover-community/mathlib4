a9cd9a381c49684280625e421749f136d004ad8a
morrison-daniel <39346894+morrison-daniel@users.noreply.github.com>
2025-12-06 21:56:35 -0800

    cleanup and repair Basis

--- Mathlib/LinearAlgebra/PiTensorProduct/Basis.lean
+++ Mathlib/LinearAlgebra/PiTensorProduct/Basis.lean
@@ -5,7 +5,9 @@ Authors: Daniel Morrison, Sophie Morel
 -/
 module
 
-public import Mathlib
+public import Mathlib.GroupTheory.MonoidLocalization.Basic
+public import Mathlib.LinearAlgebra.Finsupp.VectorSpace
+public import Mathlib.LinearAlgebra.PiTensorProduct.Finsupp
 
 /-!
 # Basis for `PiTensorProduct`
@@ -21,31 +23,31 @@ attribute [local ext] PiTensorProduct.ext
 
 open LinearMap PiTensorProduct Module TensorProduct
 
-variable {ι R : Type*} [CommSemiring R]
+variable {ι R : Type*} [CommSemiring R] [DecidableEq R]
 variable {M : ι → Type*} [∀ i, AddCommMonoid (M i)] [∀ i, Module R (M i)]
 variable {κ : ι → Type*}
-variable [Fintype ι] [DecidableEq ι] [(i : ι) → DecidableEq (κ i)] [(x : R) → Decidable (x ≠ 0)]
+variable [Fintype ι] [DecidableEq ι] [(i : ι) → DecidableEq (κ i)]
 
 /-- Let `ι` be a `Fintype` and `M` be a family of modules indexed by `ι`. If `b i : κ i → M i`
 is a basis for every `i` in `ι`, then `fun (p : Π i, κ i) ↦ ⨂ₜ[R] i, b i (p i)` is a basis
 of `⨂[R] i, M i`.
 -/
-def Basis.piTensorProduct (b : Π i, Basis (κ i) R (M i)) :
+noncomputable def Basis.piTensorProduct (b : Π i, Basis (κ i) R (M i)) :
     Basis (Π i, κ i) R (⨂[R] i, M i) :=
   Finsupp.basisSingleOne.map
     ((PiTensorProduct.congr (fun i ↦ (b i).repr)).trans <|
-        (finsuppPiTensorProduct R _ _).trans <|
-          Finsupp.lcongr (Equiv.refl _) (constantBaseRingEquiv _ R).toLinearEquiv).symm
+      ofFinsuppEquiv.trans <|
+      Finsupp.lcongr (Equiv.refl _) (constantBaseRingEquiv _ R).toLinearEquiv).symm
 
 theorem Basis.piTensorProduct_apply (b : Π i, Basis (κ i) R (M i)) (p : Π i, κ i) :
     Basis.piTensorProduct b p = ⨂ₜ[R] i, (b i) (p i) := by
   simp only [piTensorProduct, LinearEquiv.trans_symm, Finsupp.lcongr_symm, Equiv.refl_symm,
-    AlgEquiv.toLinearEquiv_symm, map_apply, Finsupp.coe_basisSingleOne, LinearEquiv.trans_apply,
-    Finsupp.lcongr_single, Equiv.refl_apply, AlgEquiv.toLinearEquiv_apply, _root_.map_one]
+    Basis.map_apply, Finsupp.coe_basisSingleOne, LinearEquiv.trans_apply, Finsupp.lcongr_single,
+    Equiv.refl_apply, AlgEquiv.coe_symm_toLinearEquiv, map_one]
   apply LinearEquiv.injective (PiTensorProduct.congr (fun i ↦ (b i).repr))
-  simp only [LinearEquiv.apply_symm_apply, congr_tprod, repr_self]
-  apply LinearEquiv.injective (finsuppPiTensorProduct R κ fun _ ↦ R)
-  simp only [LinearEquiv.apply_symm_apply, finsuppPiTensorProduct_tprod_single]
+  simp only [LinearEquiv.apply_symm_apply, congr_tprod, Basis.repr_self]
+  apply LinearEquiv.injective ofFinsuppEquiv
+  simp only [LinearEquiv.apply_symm_apply, ofFinsuppEquiv_tprod_single]
   rfl
 
 end PiTensorProduct
