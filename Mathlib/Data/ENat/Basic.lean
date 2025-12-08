2476bed95c78bff22083abb53cf6cdb7b52bf310
Yaël Dillies <yael.dillies@gmail.com>
2025-12-08 17:57:33 +0100

    update deprecation

--- Mathlib/Data/ENat/Basic.lean
+++ Mathlib/Data/ENat/Basic.lean
@@ -306,7 +306,7 @@ theorem lt_coe_add_one_iff {m : ℕ∞} {n : ℕ} : m < n + 1 ↔ m ≤ n :=
 theorem le_coe_iff {n : ℕ∞} {k : ℕ} : n ≤ ↑k ↔ ∃ (n₀ : ℕ), n = n₀ ∧ n₀ ≤ k :=
   WithTop.le_coe_iff
 
-@[deprecated not_neg (since := "2025-12-03")]
+@[deprecated not_lt_zero (since := "2025-12-03")]
 protected lemma not_lt_zero (n : ℕ∞) : ¬ n < 0 := not_lt_zero
 
 @[simp]
