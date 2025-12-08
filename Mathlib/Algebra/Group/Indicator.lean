72eecf709bafb65a0e9b4797c050139f53f27c4c
Thomas Zhu <thomas.zhu.sh@gmail.com>
2025-12-08 03:48:20 -0500

    add new results

--- Mathlib/Algebra/Group/Indicator.lean
+++ Mathlib/Algebra/Group/Indicator.lean
@@ -205,6 +205,6 @@ end Group
 end Set
 
 @[to_additive]
-theorem MonoidHom.map_mulIndicator {M N : Type*} [MulOneClass M] [MulOneClass N] (f : M →* N)
+theorem map_mulIndicator {M N F : Type*} [One M] [One N] [FunLike F M N] [OneHomClass F M N] (f : F)
     (s : Set α) (g : α → M) (x : α) : f (s.mulIndicator g x) = s.mulIndicator (f ∘ g) x := by
   simp [Set.mulIndicator_comp_of_one]
