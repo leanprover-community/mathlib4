840635cf42cf7374a5be771c73214425cf760400
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2025-12-08 12:33:48 +0100

    add module keyword

--- Mathlib/Probability/Kernel/Representation.lean
+++ Mathlib/Probability/Kernel/Representation.lean
@@ -4,6 +4,8 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Gaëtan Serré, Rémy Degenne
 -/
 
+module
+
 import Mathlib.Analysis.SpecialFunctions.Sigmoid
 import Mathlib.MeasureTheory.Constructions.UnitInterval
 import Mathlib.Order.CompletePartialOrder
