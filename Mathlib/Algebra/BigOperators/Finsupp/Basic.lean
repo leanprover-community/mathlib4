bc71ee76ed508adc6a6dcd409ed598beddba27e5
Thomas Zhu <29544653+hanwenzhu@users.noreply.github.com>
2025-12-08 11:36:51 +0000

    feat(Finsupp): remove unused conditions of `Finset.sum_apply''` (#32564)
    This PR removes two unused conditions of `Finset.sum_apply''`.

--- Mathlib/Algebra/BigOperators/Finsupp/Basic.lean
+++ Mathlib/Algebra/BigOperators/Finsupp/Basic.lean
@@ -584,18 +584,13 @@ At the time of writing Mathlib does not have a typeclass to express the conditio
 that addition on a `FunLike` type is pointwise; hence this is asserted via explicit hypotheses. -/
 theorem Finsupp.sum_apply'' {A F : Type*} [AddZeroClass A] [AddCommMonoid F] [FunLike F γ B]
     (g : ι →₀ A) (k : ι → A → F) (x : γ)
-    (hg0 : ∀ (i : ι), k i 0 = 0) (hgadd : ∀ (i : ι) (a₁ a₂ : A), k i (a₁ + a₂) = k i a₁ + k i a₂)
     (h0 : (0 : F) x = 0) (hadd : ∀ (f g : F), (f + g : F) x = f x + g x) :
     g.sum k x = g.sum (fun i a ↦ k i a x) := by
-  induction g using Finsupp.induction with
-  | zero => simp [h0]
-  | single_add i a f hf ha ih =>
-    rw [Finsupp.sum_add_index' hg0 hgadd, Finsupp.sum_add_index', hadd, ih]
-    · congr 1
-      rw [Finsupp.sum_single_index (hg0 i), Finsupp.sum_single_index]
-      simp [hg0, h0]
-    · simp [hg0, h0]
-    · simp [hgadd, hadd]
+  classical
+  unfold Finsupp.sum
+  induction g.support using Finset.induction with
+  | empty => simp [h0]
+  | insert i s hi ih => simp [sum_insert hi, hadd, ih]
 
 @[deprecated "use instead `sum_finset_sum_index` (with equality reversed)" (since := "2025-11-07")]
 theorem Finsupp.sum_sum_index' (h0 : ∀ i, t i 0 = 0) (h1 : ∀ i x y, t i (x + y) = t i x + t i y) :
