943e6787d0b535ab80c751be83a93cff5708858d
Christian Merten <136261474+chrisflav@users.noreply.github.com>
2025-12-08 15:13:24 +0000

    feat(RingTheory/Smooth): smooth algebra over Noetherian base is flat (#32064)
    Co-authored by: Judith Ludwig <ludwig@mathi.uni-heidelberg.de>

--- Mathlib.lean
+++ Mathlib.lean
@@ -13451,7 +13451,9 @@ public import Mathlib.RingTheory.SimpleRing.Defs
 public import Mathlib.RingTheory.SimpleRing.Field
 public import Mathlib.RingTheory.SimpleRing.Matrix
 public import Mathlib.RingTheory.SimpleRing.Principal
+public import Mathlib.RingTheory.Smooth.AdicCompletion
 public import Mathlib.RingTheory.Smooth.Basic
+public import Mathlib.RingTheory.Smooth.Flat
 public import Mathlib.RingTheory.Smooth.Kaehler
 public import Mathlib.RingTheory.Smooth.Local
 public import Mathlib.RingTheory.Smooth.Locus
