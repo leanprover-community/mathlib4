21c372c75e361eed86f428508db9ab0dee322b6a
Matthew Ballard <matt@mrb.email>
2025-12-09 13:43:48 -0500

    upsteam `Int.cast_ite` to `Basic` following the situation with `Nat.cast_ite`

--- Mathlib/Data/Matrix/Diagonal.lean
+++ Mathlib/Data/Matrix/Diagonal.lean
@@ -5,13 +5,11 @@ Authors: Ellen Arlt, Blair Shi, Sean Leather, Mario Carneiro, Johan Commelin, Lu
 -/
 module
 
-public import Mathlib.Data.Int.Cast.Basic
+public import Mathlib.Data.Int.Cast.Lemmas
 public import Mathlib.Data.Int.Cast.Pi
 public import Mathlib.Data.Nat.Cast.Basic
 public import Mathlib.LinearAlgebra.Matrix.Defs
 public import Mathlib.Logic.Embedding.Basic
-
-import Mathlib.Data.Int.Cast.Lemmas
 
 /-!
 # Diagonal matrices
