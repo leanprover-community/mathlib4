a6dbf7696e2524d640581ffacbcac769fbaa4c36
thorimur <68410468+thorimur@users.noreply.github.com>
2025-12-08 22:27:09 -0500

    Revert "temp: just see what `isInternalDetail` yields"
    This reverts commit 66fe6147666d9aa55563a477b390a5a84522a682.

--- Mathlib/Tactic/Linter/PrivateModule.lean
+++ Mathlib/Tactic/Linter/PrivateModule.lean
@@ -88,7 +88,7 @@ def privateModule : Linter where run stx := do
           -- Ignore private, reserved, and autoparam names; see implementation notes
           if !isPrivateName decl
               && !isReservedName (← getEnv) decl
-              && !decl.isInternalDetail then
+              && !(`_auto).isPrefixOf decl then
             return
         -- Lint if all names are private:
         let topOfFileRef := Syntax.atom (.synthetic ⟨0⟩ ⟨0⟩) ""
