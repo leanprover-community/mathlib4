81f9187173bcbd10f5eae9932740c94d643bd4d3
Stefan Kebekus <kebekus@users.noreply.github.com>
2025-12-10 08:04:31 +0100

    Update Basic.lean

--- Mathlib/Analysis/Calculus/Deriv/Basic.lean
+++ Mathlib/Analysis/Calculus/Deriv/Basic.lean
@@ -608,7 +608,7 @@ protected theorem Filter.EventuallyEq.deriv (h : f₁ =ᶠ[𝓝 x] f) : deriv f�
   h.eventuallyEq_nhds.mono fun _ h => h.deriv_eq
 
 theorem Filter.EventuallyEq.nhdsNE_deriv (h : f₁ =ᶠ[𝓝[≠] x] f) : deriv f₁ =ᶠ[𝓝[≠] x] deriv f := by
-  rw [Filter.EventuallyEq, ← eventually_nhdsNE_eventually_nhds_iff_eventually_nhdsNE] at *
+  rw [Filter.EventuallyEq, ← eventually_nhdsNE_eventually_nhds_iff] at *
   filter_upwards [h] with y hy
   apply Filter.EventuallyEq.deriv hy
 
