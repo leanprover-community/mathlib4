6ac425cbc6f5b08cc02a0378826197e0b047e0a7
Wenrong Zou <141128015+WenrongZou@users.noreply.github.com>
2025-12-11 00:55:40 +0100

    Update Mathlib/Algebra/Category/ModuleCat/Sheaf/Quasicoherent.lean
    Co-authored-by: Dagur Asgeirsson <dagurtomas@gmail.com>

--- Mathlib/Algebra/Category/ModuleCat/Sheaf/Quasicoherent.lean
+++ Mathlib/Algebra/Category/ModuleCat/Sheaf/Quasicoherent.lean
@@ -64,9 +64,8 @@ variable {C : Type u'} [Category.{v'} C] {J : GrothendieckTopology C} {R : Sheaf
   [HasSheafify J AddCommGrpCat] [J.WEqualsLocallyBijective AddCommGrpCat]
   [J.HasSheafCompose (forget₂ RingCat AddCommGrpCat)] {ι σ : Type u}
 
-/-- Given two morphism of sheaf of `R`-module `f : free ι ⟶ free σ` and `g : free σ ⟶ M`
-satisfying `H : f ≫ g = 0` and `IsColimit (CokernelCofork.ofπ g H)`, then this sequence defines
-a `Presentation M`. -/
+/-- Given two morphisms of sheaves of `R`-modules `f : free ι ⟶ free σ` and `g : free σ ⟶ M`
+satisfying `H : f ≫ g = 0` and `IsColimit (CokernelCofork.ofπ g H)`, we obtain a `Presentation M`. -/
 def presentationOfIsCokernelFree {M : SheafOfModules.{u} R}
     (f : free ι ⟶ free σ) (g : free σ ⟶ M) (H : f ≫ g = 0)
     (H' : IsColimit (CokernelCofork.ofπ g H)) : Presentation M :=
