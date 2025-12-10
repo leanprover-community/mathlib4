7b2616b971c50f4629d5b2cc508167774ad2eb9d
Stefan Kebekus <kebekus@users.noreply.github.com>
2025-12-10 08:22:51 +0100

    Revert "feat: Meromorphic functions are measurable"
    This reverts commit 2efdcfd003e33ff0ff9beaf59cb380f31ed9d38b.

--- Mathlib/Analysis/Meromorphic/Basic.lean
+++ Mathlib/Analysis/Meromorphic/Basic.lean
@@ -7,7 +7,6 @@ module
 
 public import Mathlib.Analysis.Analytic.IsolatedZeros
 public import Mathlib.Analysis.Calculus.Deriv.ZPow
-public import Mathlib.MeasureTheory.Constructions.BorelSpace.Basic
 
 /-!
 # Meromorphic functions
@@ -20,9 +19,9 @@ Main statements:
   and `g` analytic at `z₀`.
 -/
 
-open Filter Set
-
 @[expose] public section
+
+open Filter
 
 open scoped Topology
 
@@ -597,36 +596,5 @@ theorem eventually_codiscreteWithin_analyticAt
   apply Filter.mem_of_superset ((h x hx).eventually_analyticAt)
   intro x hx
   simp [hx]
-
-/--
-The singular set of a meromorphic function is countable.
--/
-theorem countable_compl_analyticAt [SecondCountableTopology 𝕜] [CompleteSpace E]
-    (h : MeromorphicOn f U) :
-    ({z | AnalyticAt 𝕜 f z}ᶜ ∩ U).Countable := by
-  classical
-  have : DiscreteTopology ↑({z | AnalyticAt 𝕜 f z}ᶜ ∩ U) := by
-    apply isDiscrete_iff_discreteTopology.1 (isDiscrete_of_codiscreteWithin _)
-    simpa using eventually_codiscreteWithin_analyticAt f h
-  apply countable_of_Lindelof_of_discrete
-
-/--
-Meromorphic functions are measurable.
--/
-theorem meromorphic_measurable [MeasurableSpace 𝕜] [SecondCountableTopology 𝕜] [BorelSpace 𝕜]
-    [MeasurableSpace E] [CompleteSpace E] [BorelSpace E] (h : MeromorphicOn f Set.univ) :
-    Measurable f := by
-  set s := {z : 𝕜 | AnalyticAt 𝕜 f z}
-  have h₁ : sᶜ.Countable  := by simpa using h.countable_compl_analyticAt
-  have h₂ : IsOpen s := isOpen_analyticAt 𝕜 f
-  have h₃ : ContinuousOn f s := fun z hz ↦ hz.continuousAt.continuousWithinAt
-  apply measurable_of_isOpen
-  intro V hV
-  rw [(by aesop : f ⁻¹' V = (f ⁻¹' V ∩ s) ∪ (f ⁻¹' V ∩ sᶜ))]
-  apply MeasurableSet.union (IsOpen.measurableSet _) (h₁.mono inter_subset_right).measurableSet
-  rw [isOpen_iff_mem_nhds] at *
-  intro x a
-  simp_all only [mem_setOf_eq, mem_inter_iff, mem_preimage, inter_mem_iff, and_true, s]
-  apply h₃.continuousAt (h₂ x a.2) (hV (f x) a.1)
 
 end MeromorphicOn
