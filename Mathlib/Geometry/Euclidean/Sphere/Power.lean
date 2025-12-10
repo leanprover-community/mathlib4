249495e2d0816fe8a1fff2999092d3be8dc7142f
Scarlett-le <735979178@qq.com>
2025-12-10 16:01:51 +0800

    docs: add missing author to Sphere.Power

--- Mathlib/Geometry/Euclidean/Sphere/Power.lean
+++ Mathlib/Geometry/Euclidean/Sphere/Power.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2021 Manuel Candales. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Manuel Candales, Benjamin Davidson
+Authors: Manuel Candales, Benjamin Davidson, Li Jiale
 -/
 module
 
