14437b2319e93e1ccc847432bcebb1389015c1f6
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2025-12-12 00:19:59 +0100

    fix

--- Mathlib/Tactic/Propose.lean
+++ Mathlib/Tactic/Propose.lean
@@ -8,4 +8,6 @@ public meta import Mathlib.Lean.Meta.Basic
 public meta import Batteries.Util.Cache
 public meta import Mathlib.Tactic.Core
 
+import Mathlib.Tactic.Linter.DeprecatedModule
+
 deprecated_module (since := "2025-11-25")
