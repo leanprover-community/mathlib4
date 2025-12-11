840988c595ab4795e09ddda0b36687d07553c48f
Dagur Asgeirsson <dagurtomas@gmail.com>
2025-12-11 10:50:38 -0700

    undo module deprecation

--- Mathlib/CategoryTheory/Subpresheaf/Basic.lean
+++
@@ -1,5 +0,0 @@
-module
-
-public import Mathlib.CategoryTheory.Subfunctor.Basic
-
-deprecated_module (since := "2025-12-11")
--- Mathlib/CategoryTheory/Subpresheaf/Image.lean
+++
@@ -1,5 +0,0 @@
-module
-
-public import Mathlib.CategoryTheory.Subfunctor.Image
-
-deprecated_module (since := "2025-12-11")
