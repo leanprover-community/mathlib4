0d5edcf11f4053cc04390326f68bde2d022a2926
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2025-12-12 00:06:59 +0100

    chore: deprecate removed modules

---
+++ Mathlib/Tactic/Propose.lean
@@ -0,0 +1,11 @@
+module
+
+public meta import Lean.Meta.Tactic.TryThis
+public meta import Lean.Meta.Tactic.SolveByElim
+public meta import Mathlib.Lean.Expr.Basic
+public meta import Mathlib.Lean.Meta
+public meta import Mathlib.Lean.Meta.Basic
+public meta import Batteries.Util.Cache
+public meta import Mathlib.Tactic.Core
+
+deprecated_module (since := "2025-11-25")
