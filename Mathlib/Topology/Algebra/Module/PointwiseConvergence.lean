d9bb9fd75c951389a85fbeab948e327cdfb95e56
Moritz Doll <moritz.doll@googlemail.com>
2025-12-12 09:47:57 +1100

    Update Mathlib/Topology/Algebra/Module/PointwiseConvergence.lean
    Co-authored-by: Oliver Nash <7734364+ocfnash@users.noreply.github.com>

--- Mathlib/Topology/Algebra/Module/PointwiseConvergence.lean
+++ Mathlib/Topology/Algebra/Module/PointwiseConvergence.lean
@@ -73,9 +73,6 @@ instance [T2Space F] : T2Space (E →SLₚₜ[σ] F) :=
 instance continuousEvalConst : ContinuousEvalConst (E →SLₚₜ[σ] F) E F :=
   UniformConvergenceCLM.continuousEvalConst _ _ _ Set.sUnion_finite_eq_univ
 
-instance [T2Space F] : T2Space (E →SLₚₜ[σ] F) :=
-  UniformConvergenceCLM.t2Space _ _ _ Set.sUnion_finite_eq_univ
-
 protected theorem hasBasis_nhds_zero_of_basis
     {ι : Type*} {p : ι → Prop} {b : ι → Set F} (h : (𝓝 0 : Filter F).HasBasis p b) :
     (𝓝 (0 : E →SLₚₜ[σ] F)).HasBasis (fun Si : Set E × ι => Finite Si.1 ∧ p Si.2)
