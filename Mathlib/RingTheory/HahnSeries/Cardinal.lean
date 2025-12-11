b67fbfa3fb143d314f99c97323054594174015da
Violeta Hernández <vi.hdz.p@gmail.com>
2025-12-11 10:20:01 -0600

    Update Cardinal.lean

--- Mathlib/RingTheory/HahnSeries/Cardinal.lean
+++ Mathlib/RingTheory/HahnSeries/Cardinal.lean
@@ -34,19 +34,19 @@ section Zero
 variable [Zero R]
 
 /-- The cardinality of the support of a Hahn series. -/
-def card (x : HahnSeries Γ R) : Cardinal :=
+def card (x : R⟦Γ⟧) : Cardinal :=
   #x.support
 
-theorem card_congr [Zero S] {x : HahnSeries Γ R} {y : HahnSeries Γ S} (h : x.support = y.support) :
+theorem card_congr [Zero S] {x : R⟦Γ⟧} {y : S⟦Γ⟧} (h : x.support = y.support) :
     x.card = y.card := by
   simp_rw [card, h]
 
-theorem card_mono [Zero S] {x : HahnSeries Γ R} {y : HahnSeries Γ S} (h : x.support ⊆ y.support) :
+theorem card_mono [Zero S] {x : R⟦Γ⟧} {y : S⟦Γ⟧} (h : x.support ⊆ y.support) :
     x.card ≤ y.card :=
   mk_le_mk_of_subset h
 
 @[simp]
-theorem card_zero : card (0 : HahnSeries Γ R) = 0 := by
+theorem card_zero : card (0 : R⟦Γ⟧) = 0 := by
   simp [card]
 
 theorem card_single_of_ne (a : Γ) {r : R} (h : r ≠ 0) : card (single a r) = 1 := by
@@ -55,33 +55,33 @@ theorem card_single_of_ne (a : Γ) {r : R} (h : r ≠ 0) : card (single a r) = 1
 theorem card_single_le (a : Γ) (r : R) : card (single a r) ≤ 1 :=
   (mk_le_mk_of_subset support_single_subset).trans_eq (mk_singleton a)
 
-theorem card_map_le [Zero S] (x : HahnSeries Γ R) (f : ZeroHom R S) : (x.map f).card ≤ x.card :=
+theorem card_map_le [Zero S] (x : R⟦Γ⟧) (f : ZeroHom R S) : (x.map f).card ≤ x.card :=
   card_mono <| support_map_subset ..
 
-theorem card_truncLT_le [DecidableLT Γ] (x : HahnSeries Γ R) (c : Γ) :
+theorem card_truncLT_le [DecidableLT Γ] (x : R⟦Γ⟧) (c : Γ) :
     (truncLT c x).card ≤ x.card :=
   card_mono <| support_truncLT_subset ..
 
-theorem card_smul_le (s : S) (x : HahnSeries Γ R) [SMulZeroClass S R] : (s • x).card ≤ x.card :=
+theorem card_smul_le (s : S) (x : R⟦Γ⟧) [SMulZeroClass S R] : (s • x).card ≤ x.card :=
   card_mono <| support_smul_subset ..
 
 end Zero
 
-theorem card_neg_le [NegZeroClass R] (x : HahnSeries Γ R) : (-x).card ≤ x.card :=
+theorem card_neg_le [NegZeroClass R] (x : R⟦Γ⟧) : (-x).card ≤ x.card :=
   card_mono <| support_neg_subset ..
 
-theorem card_add_le [AddMonoid R] (x y : HahnSeries Γ R) : (x + y).card ≤ x.card + y.card :=
+theorem card_add_le [AddMonoid R] (x y : R⟦Γ⟧) : (x + y).card ≤ x.card + y.card :=
   (mk_le_mk_of_subset (support_add_subset ..)).trans (mk_union_le ..)
 
 @[simp]
-theorem card_neg [AddGroup R] (x : HahnSeries Γ R) : (-x).card = x.card :=
+theorem card_neg [AddGroup R] (x : R⟦Γ⟧) : (-x).card = x.card :=
   card_congr support_neg
 
-theorem card_sub_le [AddGroup R] (x y : HahnSeries Γ R) : (x - y).card ≤ x.card + y.card :=
+theorem card_sub_le [AddGroup R] (x y : R⟦Γ⟧) : (x - y).card ≤ x.card + y.card :=
   (mk_le_mk_of_subset (support_sub_subset ..)).trans (mk_union_le ..)
 
 theorem card_mul_le [AddCommMonoid Γ] [IsOrderedCancelAddMonoid Γ] [NonUnitalNonAssocSemiring R]
-    (x y : HahnSeries Γ R) : (x * y).card ≤ x.card * y.card :=
+    (x y : R⟦Γ⟧) : (x * y).card ≤ x.card * y.card :=
   (mk_le_mk_of_subset (support_mul_subset ..)).trans mk_add_le
 
 end HahnSeries
