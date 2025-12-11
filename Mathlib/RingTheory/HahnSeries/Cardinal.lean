dc7b585e8be97e27703745c15a2f9489945e14b8
Violeta Hernández <vi.hdz.p@gmail.com>
2025-12-11 10:15:13 -0600

    Update Cardinal.lean

--- Mathlib/RingTheory/HahnSeries/Cardinal.lean
+++ Mathlib/RingTheory/HahnSeries/Cardinal.lean
@@ -36,19 +36,19 @@ section Zero
 variable [Zero R]
 
 /-- The cardinality of the support of a Hahn series. -/
-def card (x : HahnSeries Γ R) : Cardinal :=
+def card (x : R⟦Γ⟧) : Cardinal :=
   #x.support
 
-theorem card_congr [Zero S] {x : HahnSeries Γ R} {y : HahnSeries Γ S} (h : x.support = y.support) :
+theorem card_congr [Zero S] {x : R⟦Γ⟧} {y : S⟦Γ⟧} (h : x.support = y.support) :
     x.card = y.card := by
   simp_rw [card, h]
 
-theorem card_mono [Zero S] {x : HahnSeries Γ R} {y : HahnSeries Γ S} (h : x.support ⊆ y.support) :
+theorem card_mono [Zero S] {x : R⟦Γ⟧} {y : S⟦Γ⟧} (h : x.support ⊆ y.support) :
     x.card ≤ y.card :=
   mk_le_mk_of_subset h
 
 @[simp]
-theorem card_zero : card (0 : HahnSeries Γ R) = 0 := by
+theorem card_zero : card (0 : R⟦Γ⟧) = 0 := by
   simp [card]
 
 theorem card_single_of_ne (a : Γ) {r : R} (h : r ≠ 0) : card (single a r) = 1 := by
@@ -58,54 +58,54 @@ theorem card_single_le (a : Γ) (r : R) : card (single a r) ≤ 1 :=
   (mk_le_mk_of_subset support_single_subset).trans_eq (mk_singleton a)
 
 @[simp]
-theorem card_one_le [Zero Γ] [One R] : card (1 : HahnSeries Γ R) ≤ 1 :=
+theorem card_one_le [Zero Γ] [One R] : card (1 : R⟦Γ⟧) ≤ 1 :=
   card_single_le ..
 
 @[simp]
-theorem card_one [Zero Γ] [One R] [NeZero (1 : R)] : card (1 : HahnSeries Γ R) = 1 :=
+theorem card_one [Zero Γ] [One R] [NeZero (1 : R)] : card (1 : R⟦Γ⟧) = 1 :=
   card_single_of_ne _ one_ne_zero
 
-theorem card_map_le [Zero S] (x : HahnSeries Γ R) (f : ZeroHom R S) : (x.map f).card ≤ x.card :=
+theorem card_map_le [Zero S] (x : R⟦Γ⟧) (f : ZeroHom R S) : (x.map f).card ≤ x.card :=
   card_mono <| support_map_subset ..
 
-theorem card_truncLT_le [DecidableLT Γ] (x : HahnSeries Γ R) (c : Γ) :
+theorem card_truncLT_le [DecidableLT Γ] (x : R⟦Γ⟧) (c : Γ) :
     (truncLT c x).card ≤ x.card :=
   card_mono <| support_truncLT_subset ..
 
-theorem card_smul_le (s : S) (x : HahnSeries Γ R) [SMulZeroClass S R] : (s • x).card ≤ x.card :=
+theorem card_smul_le (s : S) (x : R⟦Γ⟧) [SMulZeroClass S R] : (s • x).card ≤ x.card :=
   card_mono <| support_smul_subset ..
 
 end Zero
 
-theorem card_neg_le [NegZeroClass R] (x : HahnSeries Γ R) : (-x).card ≤ x.card :=
+theorem card_neg_le [NegZeroClass R] (x : R⟦Γ⟧) : (-x).card ≤ x.card :=
   card_mono <| support_neg_subset ..
 
-theorem card_add_le [AddMonoid R] (x y : HahnSeries Γ R) : (x + y).card ≤ x.card + y.card :=
+theorem card_add_le [AddMonoid R] (x y : R⟦Γ⟧) : (x + y).card ≤ x.card + y.card :=
   (mk_le_mk_of_subset (support_add_subset ..)).trans (mk_union_le ..)
 
 @[simp]
-theorem card_neg [AddGroup R] (x : HahnSeries Γ R) : (-x).card = x.card :=
+theorem card_neg [AddGroup R] (x : R⟦Γ⟧) : (-x).card = x.card :=
   card_congr support_neg
 
-theorem card_sub_le [AddGroup R] (x y : HahnSeries Γ R) : (x - y).card ≤ x.card + y.card :=
+theorem card_sub_le [AddGroup R] (x y : R⟦Γ⟧) : (x - y).card ≤ x.card + y.card :=
   (mk_le_mk_of_subset (support_sub_subset ..)).trans (mk_union_le ..)
 
 theorem card_mul_le [AddCommMonoid Γ] [IsOrderedCancelAddMonoid Γ] [NonUnitalNonAssocSemiring R]
-    (x y : HahnSeries Γ R) : (x * y).card ≤ x.card * y.card :=
+    (x y : R⟦Γ⟧) : (x * y).card ≤ x.card * y.card :=
   (mk_le_mk_of_subset (support_mul_subset ..)).trans mk_add_le
 
 theorem card_single_mul_le [AddCommMonoid Γ] [IsOrderedCancelAddMonoid Γ]
-    [NonUnitalNonAssocSemiring R] (x : HahnSeries Γ R) (a : Γ) (r : R) :
+    [NonUnitalNonAssocSemiring R] (x : R⟦Γ⟧) (a : Γ) (r : R) :
     (single a r * x).card ≤ x.card := by
   simpa using (card_mul_le ..).trans (mul_le_mul_left (card_single_le ..) _)
 
 theorem card_mul_single_le [AddCommMonoid Γ] [IsOrderedCancelAddMonoid Γ]
-    [NonUnitalNonAssocSemiring R] (x : HahnSeries Γ R) (a : Γ) (r : R) :
+    [NonUnitalNonAssocSemiring R] (x : R⟦Γ⟧) (a : Γ) (r : R) :
     (x * single a r).card ≤ x.card := by
   simpa using (card_mul_le ..).trans (mul_le_mul_right (card_single_le ..) _)
 
 theorem card_pow_le [AddCommMonoid Γ] [IsOrderedCancelAddMonoid Γ] [Semiring R]
-    (x : HahnSeries Γ R) (n : ℕ) : (x ^ n).card ≤ x.card ^ n := by
+    (x : R⟦Γ⟧) (n : ℕ) : (x ^ n).card ≤ x.card ^ n := by
   induction n with
   | zero => simp
   | succ n IH =>
@@ -123,7 +123,7 @@ section LinearOrder
 variable [LinearOrder Γ]
 
 theorem card_hsum_powers_le [AddCommMonoid Γ] [IsOrderedCancelAddMonoid Γ] [CommRing R]
-    (x : HahnSeries Γ R) : (SummableFamily.powers x).hsum.card ≤ max ℵ₀ x.card := by
+    (x : R⟦Γ⟧) : (SummableFamily.powers x).hsum.card ≤ max ℵ₀ x.card := by
   rw [← lift_uzero (card _)]
   refine (sum_pow_le_max_aleph0 _).trans' <| (card_hsum_le _).trans <| sum_le_sum _ _ fun i ↦ ?_
   rw [SummableFamily.powers_toFun]
@@ -131,7 +131,7 @@ theorem card_hsum_powers_le [AddCommMonoid Γ] [IsOrderedCancelAddMonoid Γ] [Co
   · exact card_pow_le ..
   · cases i <;> simp
 
-theorem card_inv_le [AddCommGroup Γ] [IsOrderedAddMonoid Γ] [Field R] (x : HahnSeries Γ R) :
+theorem card_inv_le [AddCommGroup Γ] [IsOrderedAddMonoid Γ] [Field R] (x : R⟦Γ⟧) :
     x⁻¹.card ≤ max ℵ₀ x.card := by
   obtain rfl | hx := eq_or_ne x 0
   · simp
@@ -141,7 +141,7 @@ theorem card_inv_le [AddCommGroup Γ] [IsOrderedAddMonoid Γ] [Field R] (x : Hah
   refine (card_single_mul_le _ (-x.order) x.leadingCoeff⁻¹).trans' <| card_mono fun _ ↦ ?_
   aesop (add simp [coeff_single_mul])
 
-theorem card_div_le [AddCommGroup Γ] [IsOrderedAddMonoid Γ] [Field R] (x y : HahnSeries Γ R) :
+theorem card_div_le [AddCommGroup Γ] [IsOrderedAddMonoid Γ] [Field R] (x y : R⟦Γ⟧) :
     (x / y).card ≤ x.card * max ℵ₀ y.card :=
   (card_mul_le ..).trans <| mul_le_mul_right (card_inv_le y) _
 
@@ -157,13 +157,13 @@ variable [PartialOrder Γ] [AddMonoid R] [hκ : Fact (ℵ₀ ≤ κ)]
 variable (Γ R) in
 /-- The `κ`-bounded submonoid of Hahn series with cardinal less than `κ`. -/
 @[simps!]
-def cardLTAddSubmonoid : AddSubmonoid (HahnSeries Γ R) where
+def cardLTAddSubmonoid : AddSubmonoid R⟦Γ⟧ where
   carrier := {x | x.card < κ}
   zero_mem' := by simpa using aleph0_pos.trans_le hκ.out
   add_mem' hx hy := (card_add_le ..).trans_lt <| add_lt_of_lt hκ.out hx hy
 
 @[simp]
-theorem mem_cardLTAddSubmonoid {x : HahnSeries Γ R} : x ∈ cardLTAddSubmonoid Γ R κ ↔ x.card < κ :=
+theorem mem_cardLTAddSubmonoid {x : R⟦Γ⟧} : x ∈ cardLTAddSubmonoid Γ R κ ↔ x.card < κ :=
   .rfl
 
 end AddMonoid
@@ -174,12 +174,12 @@ variable [PartialOrder Γ] [AddGroup R] [hκ : Fact (ℵ₀ ≤ κ)]
 variable (Γ R) in
 /-- The `κ`-bounded subgroup of Hahn series with cardinal less than `κ`. -/
 @[simps!]
-def cardLTAddSubgroup : AddSubgroup (HahnSeries Γ R) where
+def cardLTAddSubgroup : AddSubgroup R⟦Γ⟧ where
   neg_mem' := by simp
   __ := cardLTAddSubmonoid Γ R κ
 
 @[simp]
-theorem mem_cardLTAddSubgroup {x : HahnSeries Γ R} : x ∈ cardLTAddSubgroup Γ R κ ↔ x.card < κ :=
+theorem mem_cardLTAddSubgroup {x : R⟦Γ⟧} : x ∈ cardLTAddSubgroup Γ R κ ↔ x.card < κ :=
   .rfl
 
 end AddGroup
@@ -190,13 +190,13 @@ variable [PartialOrder Γ] [AddCommMonoid Γ] [IsOrderedCancelAddMonoid Γ] [Rin
 
 variable (Γ R) in
 /-- The `κ`-bounded subring of Hahn series with cardinal less than `c`. -/
-def cardLTSubring : Subring (HahnSeries Γ R) where
+def cardLTSubring : Subring R⟦Γ⟧ where
   one_mem' := card_one_le.trans_lt <| one_lt_aleph0.trans_le hκ.out
   mul_mem' hx hy := (card_mul_le ..).trans_lt <| mul_lt_of_lt hκ.out hx hy
   __ := cardLTAddSubgroup Γ R κ
 
 @[simp]
-theorem mem_cardLTSubring {x : HahnSeries Γ R} : x ∈ cardLTSubring Γ R κ ↔ x.card < κ :=
+theorem mem_cardLTSubring {x : R⟦Γ⟧} : x ∈ cardLTSubring Γ R κ ↔ x.card < κ :=
   .rfl
 
 end Subring
@@ -207,12 +207,12 @@ variable [LinearOrder Γ] [AddCommGroup Γ] [IsOrderedAddMonoid Γ] [Field R] [h
 variable (Γ R) in
 /-- The `κ`-bounded subfield of Hahn series with cardinal less than `c`. -/
 @[simps!]
-def cardLTSubfield : Subfield (HahnSeries Γ R) where
+def cardLTSubfield : Subfield R⟦Γ⟧ where
   inv_mem' _ _ := (card_inv_le _).trans_lt <| by simpa [hκ.out]
   __ := have : Fact (ℵ₀ ≤ κ) := ⟨hκ.out.le⟩; cardLTSubring Γ R κ
 
 @[simp]
-theorem mem_cardLTSubfield {x : HahnSeries Γ R} : x ∈ cardLTSubfield Γ R κ ↔ x.card < κ :=
+theorem mem_cardLTSubfield {x : R⟦Γ⟧} : x ∈ cardLTSubfield Γ R κ ↔ x.card < κ :=
   .rfl
 
 end Subfield
