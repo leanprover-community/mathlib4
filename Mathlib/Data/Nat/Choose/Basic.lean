ef033c5b5293b782b9444df76037f64ed6eef357
Batixx <s59fpern@uni-bonn.de>
2025-12-12 19:46:11 +0100

    fix sum_bernoulli proof

--- Mathlib/Data/Nat/Choose/Basic.lean
+++ Mathlib/Data/Nat/Choose/Basic.lean
@@ -163,7 +163,7 @@ theorem choose_mul (n : ℕ) {k s : ℕ} (hsk : s ≤ k) :
   · rw [choose_eq_zero_of_lt hkn, Nat.zero_mul]
     by_cases hns : n < s
     · rw [choose_eq_zero_of_lt hns, Nat.zero_mul]
-    rw [choose_eq_zero_of_lt (Nat.sub_lt_sub_right (Nat.not_lt.mp hns) hkn), Nat.mul_zero]
+    · rw [choose_eq_zero_of_lt (Nat.sub_lt_sub_right (Nat.not_lt.mp hns) hkn), Nat.mul_zero]
   have h : 0 < (n - k)! * (k - s)! * s ! := by apply_rules [factorial_pos, Nat.mul_pos]
   apply Nat.mul_right_cancel h
   calc
