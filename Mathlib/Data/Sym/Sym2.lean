2794d85ee927e9ab260b26b7841475cdf998dcec
Oliver Nash <7734364+ocfnash@users.noreply.github.com>
2025-12-12 17:18:06 +0000

    Apply suggestion from @ocfnash
    No good deed goes unpunished

--- Mathlib/Data/Sym/Sym2.lean
+++ Mathlib/Data/Sym/Sym2.lean
@@ -142,9 +142,7 @@ protected def rec {motive : Sym2 α → Sort*}
     (z : Sym2 α) : motive z :=
   Quot.rec f h z
 
-/--
-A dependent recursion principle for `Sym2` that uses
-[heterogeneous equality](lean-manual://section HEq). -/
+/-- A dependent recursion principle for `Sym2` that uses heterogeneous equality. -/
 @[elab_as_elim]
 protected def hrec {motive : Sym2 α → Sort*}
     (f : (p : α × α) → motive (Sym2.mk p))
