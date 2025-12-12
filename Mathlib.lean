9a8ec2b93d127477a9c62424aa0ac1bd42d472c0
Joël Riou <37772949+joelriou@users.noreply.github.com>
2025-12-12 14:37:32 +0000

    feat(AlgebraicTopology/SimplicialSet): structures to study homotopy groups of Kan complexes (#32347)
    We introduce a structure `SSet.PtSimplex` which consists of morphisms `Δ[n] ⟶ X` which are constant to a certain `x : X _⦋0⦌` on the boundary. Homotopy groups of Kan complexes will be defined as a quotient of this. We introduce two structures `PtSimplex.RelStruct` and `PtSimplex.MulStruct` which will be important in the study of homotopy groups of Kan complexes.
    
    From https://github.com/joelriou/topcat-model-category

--- Mathlib.lean
+++ Mathlib.lean
@@ -8629,6 +8629,7 @@ public import Mathlib.AlgebraicTopology.SimplicialSet.HomotopyCat
 public import Mathlib.AlgebraicTopology.SimplicialSet.Horn
 public import Mathlib.AlgebraicTopology.SimplicialSet.HornColimits
 public import Mathlib.AlgebraicTopology.SimplicialSet.KanComplex
+public import Mathlib.AlgebraicTopology.SimplicialSet.KanComplex.MulStruct
 public import Mathlib.AlgebraicTopology.SimplicialSet.Monoidal
 public import Mathlib.AlgebraicTopology.SimplicialSet.Monomorphisms
 public import Mathlib.AlgebraicTopology.SimplicialSet.Nerve
