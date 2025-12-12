8bdb8eab1b27f015c167f642879de8528fc3b4e6
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2025-12-12 13:15:52 +0100

    correct Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -4628,8 +4628,7 @@ public import Mathlib.LinearAlgebra.Matrix.Symmetric
 public import Mathlib.LinearAlgebra.Matrix.ToLin
 public import Mathlib.LinearAlgebra.Matrix.ToLinearEquiv
 public import Mathlib.LinearAlgebra.Matrix.Trace
-public import Mathlib.LinearAlgebra.Matrix.Transvection.Basic
-public import Mathlib.LinearAlgebra.Matrix.Transvection.Generation
+public import Mathlib.LinearAlgebra.Matrix.Transvection
 public import Mathlib.LinearAlgebra.Matrix.Unique
 public import Mathlib.LinearAlgebra.Matrix.Vec
 public import Mathlib.LinearAlgebra.Matrix.ZPow
