2eebe2aa223d1262dd30f63eb2a630f70242b659
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-12-12 20:45:50 +0100

    whitespace

--- Mathlib/Algebra/Homology/DerivedCategory/KInjective.lean
+++ Mathlib/Algebra/Homology/DerivedCategory/KInjective.lean
@@ -47,7 +47,7 @@ lemma bijective_toSmallShiftedHom_of_isKInjective [L.IsKInjective] :
     Function.Bijective (toSmallShiftedHom.{w} (K := K) (L := L) (n := n)) := by
   letI := HasDerivedCategory.standard C
   rw [← Function.Bijective.of_comp_iff'
-    (SmallShiftedHom.equiv _ DerivedCategory.Q).bijective,
+      (SmallShiftedHom.equiv _ DerivedCategory.Q).bijective,
     ← Function.Bijective.of_comp_iff' (Iso.homCongr ((quotientCompQhIso C).symm.app K)
       ((Q.commShiftIso n).symm.app L ≪≫ (quotientCompQhIso C).symm.app (L⟦n⟧))).bijective]
   convert (CochainComplex.IsKInjective.Qh_map_bijective _ _).comp (toHom_bijective K L n)
