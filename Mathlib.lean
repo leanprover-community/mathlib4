fef3a469f463ed8dfb11e6f6a285da3e99dd0e6e
Joël Riou <37772949+joelriou@users.noreply.github.com>
2025-12-12 14:05:39 +0000

    feat(AlgebraicTopology): finite simplicial sets (#32202)
    A simplicial set `X` is finite (`SSet.Finite`) if it has finitely many nondegenerate simplices. Equivalently, `X` is finite dimensional and the type of `n`-simplices of `X` is finite for any `n`.

--- Mathlib.lean
+++ Mathlib.lean
@@ -8623,6 +8623,7 @@ public import Mathlib.AlgebraicTopology.SimplicialSet.CompStructTruncated
 public import Mathlib.AlgebraicTopology.SimplicialSet.Coskeletal
 public import Mathlib.AlgebraicTopology.SimplicialSet.Degenerate
 public import Mathlib.AlgebraicTopology.SimplicialSet.Dimension
+public import Mathlib.AlgebraicTopology.SimplicialSet.Finite
 public import Mathlib.AlgebraicTopology.SimplicialSet.HomotopyCat
 public import Mathlib.AlgebraicTopology.SimplicialSet.Horn
 public import Mathlib.AlgebraicTopology.SimplicialSet.HornColimits
