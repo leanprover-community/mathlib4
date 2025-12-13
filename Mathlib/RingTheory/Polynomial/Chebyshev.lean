2dfa45a7e8818286a3e567a7cf9a8293bde4bfda
Yuval Filmus <filmus.yuval@gmail.com>
2025-12-13 23:44:16 +0200

    Update Mathlib/RingTheory/Polynomial/Chebyshev.lean
    Co-authored-by: Andrew Yang <36414270+erdOne@users.noreply.github.com>

--- Mathlib/RingTheory/Polynomial/Chebyshev.lean
+++ Mathlib/RingTheory/Polynomial/Chebyshev.lean
@@ -355,7 +355,7 @@ theorem degree_U_neg_one : (U R (-1)).degree = ⊥ := by simp
 
 theorem natDegree_U_neg_one : (U R (-1)).natDegree = 0 := by simp
 
-theorem degree_U_ne_neg_one [IsDomain R] [NeZero (2 : R)] (n : ℤ) (hn : n ≠ -1) :
+theorem degree_U_of_ne_neg_one [IsDomain R] [NeZero (2 : R)] (n : ℤ) (hn : n ≠ -1) :
     (U R n).degree = ↑((n + 1).natAbs - 1) := by
   obtain ⟨m, rfl | rfl⟩ := n.eq_nat_or_neg
   case inl => rw [degree_U_natCast R m]; norm_cast
