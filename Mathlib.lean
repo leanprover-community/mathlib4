5734b4a316c1c0b4ee9324c503e6e5062ab054d1
Christian Merten <136261474+chrisflav@users.noreply.github.com>
2025-12-13 07:56:05 +0000

    feat(RingTheory/StandardSmooth): presentation independent characterization (#31081)
    We show that if `S` is of finite presentation over `R` such that `H¹(S/R) = 0` and `Ω[S⁄R]` is free on `{d sᵢ}ᵢ` for some `sᵢ : S`, then `S` is `R`-standard smooth. The converse of this is already in mathlib.
    
    In the next PR, we will deduce from this that smooth is equivalent to locally standard smooth.
    
    From Pi1.

--- Mathlib.lean
+++ Mathlib.lean
@@ -13455,6 +13455,7 @@ public import Mathlib.RingTheory.Smooth.NoetherianDescent
 public import Mathlib.RingTheory.Smooth.Pi
 public import Mathlib.RingTheory.Smooth.StandardSmooth
 public import Mathlib.RingTheory.Smooth.StandardSmoothCotangent
+public import Mathlib.RingTheory.Smooth.StandardSmoothOfFree
 public import Mathlib.RingTheory.Spectrum.Maximal.Basic
 public import Mathlib.RingTheory.Spectrum.Maximal.Defs
 public import Mathlib.RingTheory.Spectrum.Maximal.Localization
