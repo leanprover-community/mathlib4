bade1d807e904ff045794087e20e59a9b522e2c2
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-12-13 11:40:42 +0100

    Fix linter

--- Mathlib/Analysis/Calculus/ContDiff/Basic.lean
+++ Mathlib/Analysis/Calculus/ContDiff/Basic.lean
@@ -1522,3 +1522,5 @@ theorem ContDiff.iterate_deriv' (n : ℕ) :
   | k + 1, _, hf => ContDiff.iterate_deriv' _ k (contDiff_succ_iff_deriv.mp hf).2.2
 
 end deriv
+
+set_option linter.style.longLine 1700
