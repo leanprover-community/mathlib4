565adf85684354ea7b917deb4a66dd582133e561
Andrew Yang <36414270+erdOne@users.noreply.github.com>
2025-12-13 01:00:54 +0000

    Update QuasiFinite.lean

--- Mathlib/RingTheory/Etale/QuasiFinite.lean
+++ Mathlib/RingTheory/Etale/QuasiFinite.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2025 Andrew Yang. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Andrew Yang
+-/
 module
 
 public import Mathlib.RingTheory.Polynomial.UniversalFactorizationRing
