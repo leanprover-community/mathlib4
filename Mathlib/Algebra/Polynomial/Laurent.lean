25b347d13cb840e01e0a3da03d1af858d0cac355
Yaël Dillies <yael.dillies@gmail.com>
2025-12-13 13:43:00 +0100

    fix

--- Mathlib/Algebra/Polynomial/Laurent.lean
+++ Mathlib/Algebra/Polynomial/Laurent.lean
@@ -592,7 +592,7 @@ def invert : R[T;T⁻¹] ≃ₐ[R] R[T;T⁻¹] := AddMonoidAlgebra.domCongr R R 
 @[simp] lemma invert_T (n : ℤ) : invert (T n : R[T;T⁻¹]) = T (-n) :=
   AddMonoidAlgebra.domCongr_single _ _ _ _ _
 
-@[simp] lemma invert_apply (f : R[T;T⁻¹]) (n : ℤ) : invert f n = f (-n) := rfl
+@[simp] lemma invert_apply (f : R[T;T⁻¹]) (n : ℤ) : invert f n = f (-n) := by simp [invert]
 
 @[simp] lemma invert_comp_C : invert ∘ (@C R _) = C := by ext; simp
 
