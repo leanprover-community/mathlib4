eea6419c2109c6a9a243247a83f5dfaa46c34092
ooovi <79147175+ooovi@users.noreply.github.com>
2025-12-14 20:24:25 +0000

    feat(Combinatorics/SimpleGraph): edge labellings (#30047)
    Add edge labellings for `SimpleGraph` as well as some basic lemmata.
    
    Co-authored-by: ovi <ovi@posteo.de>
    Co-authored-by: Bhavik Mehta <bhavikmehta8@gmail.com>
    Co-authored-by: Oliver Nash <7734364+ocfnash@users.noreply.github.com>

--- Mathlib/Combinatorics/SimpleGraph/Triangle/Counting.lean
+++ Mathlib/Combinatorics/SimpleGraph/Triangle/Counting.lean
@@ -128,7 +128,7 @@ lemma triangle_counting'
     rw [Nat.cast_add]
     gcongr
   rintro a _ b _ t
-  rw [Function.onFun, disjoint_left]
+  rw [Function.onFun, Finset.disjoint_left]
   simp only [Prod.forall, mem_image, not_exists, Prod.mk_inj,
     exists_imp, and_imp, not_and]
   aesop
@@ -141,7 +141,7 @@ private lemma triple_eq_triple_of_mem (hst : Disjoint s t) (hsu : Disjoint s u) 
     (x₁, y₁, z₁) = (x₂, y₂, z₂) := by
   simp only [Finset.Subset.antisymm_iff, subset_iff, mem_insert, mem_singleton, forall_eq_or_imp,
     forall_eq] at h
-  grind [disjoint_left]
+  grind [Finset.disjoint_left]
 
 variable [Fintype α]
 
