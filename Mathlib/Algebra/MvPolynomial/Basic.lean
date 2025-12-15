e5acb35922f5fcf8155a0a4154b31b3bdca36db1
Hagb (Junyu Guo 郭俊余) <hagb@hagb.name>
2025-12-15 11:48:46 +0000

    feat(Algebra/MvPolynomial/Basic): add `MvPolynomial.support_C` (#32876)
    Similar with `MvPolynomial.support_monomial`, while for `MvPolynomial.C`.

--- Mathlib/Algebra/MvPolynomial/Basic.lean
+++ Mathlib/Algebra/MvPolynomial/Basic.lean
@@ -509,6 +509,10 @@ theorem support_monomial [h : Decidable (a = 0)] :
   rw [← Subsingleton.elim (Classical.decEq R a 0) h]
   rfl
 
+lemma support_C (c : R) [h : Decidable (c = 0)] :
+    (C (σ := σ) c).support = if c = 0 then ∅ else {0} :=
+  support_monomial
+
 theorem support_monomial_subset : (monomial s a).support ⊆ {s} :=
   support_single_subset
 
