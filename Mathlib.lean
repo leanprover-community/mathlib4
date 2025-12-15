7429d41c853e11ce48d2b0e5968e0b09b1f14624
Joël Riou <37772949+joelriou@users.noreply.github.com>
2025-12-15 10:15:33 +0000

    feat(AlgebraicTopology): finite colimits of finite simplicial sets are finite (#32265)
    From https://github.com/joelriou/topcat-model-category

--- Mathlib.lean
+++ Mathlib.lean
@@ -1421,6 +1421,7 @@ public import Mathlib.AlgebraicTopology.SimplicialSet.Coskeletal
 public import Mathlib.AlgebraicTopology.SimplicialSet.Degenerate
 public import Mathlib.AlgebraicTopology.SimplicialSet.Dimension
 public import Mathlib.AlgebraicTopology.SimplicialSet.Finite
+public import Mathlib.AlgebraicTopology.SimplicialSet.FiniteColimits
 public import Mathlib.AlgebraicTopology.SimplicialSet.HomotopyCat
 public import Mathlib.AlgebraicTopology.SimplicialSet.Horn
 public import Mathlib.AlgebraicTopology.SimplicialSet.HornColimits
