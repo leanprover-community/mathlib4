56ec12b647f8dfcc1bc6cdcaea7e13e96c91f3a2
Joël Riou <37772949+joelriou@users.noreply.github.com>
2025-12-15 09:35:13 +0000

    feat(AlgebraicTopology): binary products of standard simplices (#32237)
    We start the study of nondegenerate simplices in `Δ[p] ⊗ Δ[q]`. In particular, we show that it is of dimension `≤ p + q`.
    
    From https://github.com/joelriou/topcat-model-category

--- Mathlib.lean
+++ Mathlib.lean
@@ -8642,6 +8642,7 @@ public import Mathlib.AlgebraicTopology.SimplicialSet.NonDegenerateSimplices
 public import Mathlib.AlgebraicTopology.SimplicialSet.NonDegenerateSimplicesSubcomplex
 public import Mathlib.AlgebraicTopology.SimplicialSet.Op
 public import Mathlib.AlgebraicTopology.SimplicialSet.Path
+public import Mathlib.AlgebraicTopology.SimplicialSet.ProdStdSimplex
 public import Mathlib.AlgebraicTopology.SimplicialSet.RelativeMorphism
 public import Mathlib.AlgebraicTopology.SimplicialSet.Simplices
 public import Mathlib.AlgebraicTopology.SimplicialSet.Skeleton
