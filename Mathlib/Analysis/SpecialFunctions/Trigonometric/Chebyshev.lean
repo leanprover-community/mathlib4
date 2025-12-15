650d5c3e1fe9a2ed8dd2aebf01607c61a8e3cdc9
Yuval Filmus <filmus.yuval@gmail.com>
2025-12-16 00:18:39 +0200

    Update Mathlib/Analysis/SpecialFunctions/Trigonometric/Chebyshev.lean
    Co-authored-by: Dagur Asgeirsson <dagurtomas@gmail.com>

--- Mathlib/Analysis/SpecialFunctions/Trigonometric/Chebyshev.lean
+++ Mathlib/Analysis/SpecialFunctions/Trigonometric/Chebyshev.lean
@@ -1,3 +1,5 @@
-import Mathlib.Analysis.SpecialFunctions.Trigonometric.Chebyshev.Basic
+module
+
+public import Mathlib.Analysis.SpecialFunctions.Trigonometric.Chebyshev.Basic
 
 deprecated_module (since := "2025-12-15")
