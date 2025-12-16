2c799c6952a841b2e650721088efd62f9559807a
Jonas van der Schaaf <niet@jonasvanderschaaf.nl>
2025-12-16 14:51:15 +0100

    Move file

--- Mathlib/CategoryTheory/Sites/PreservesColimits.lean
+++ Mathlib/CategoryTheory/Sites/PreservesColimits.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2025 Jonas van der Schaaf. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Jonas van der Schaaf
+Authors: Dagur Asgeirsson, Jonas van der Schaaf
 -/
 module
 
