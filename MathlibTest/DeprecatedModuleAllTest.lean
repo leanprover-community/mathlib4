d3786390f95c4b1eb333a75ec5ecd4c8dd6a362a
Kim Morrison <kim@tqft.net>
2025-12-17 23:54:23 +0000

    fix(Test): add missing blank line in DeprecatedModuleAllTest guard_msgs

--- MathlibTest/DeprecatedModuleAllTest.lean
+++ MathlibTest/DeprecatedModuleAllTest.lean
@@ -9,6 +9,7 @@ warning: Testing public import deprecation
 import Mathlib.Tactic.Linter.DocPrime
 import Mathlib.Tactic.Linter.DocString
 
+
 Note: This linter can be disabled with `set_option linter.deprecated.module false`
 -/
 #guard_msgs in
