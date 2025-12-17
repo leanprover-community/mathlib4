9e6b3156b1d8281a9e8abcebd242292eb432925a
Yaël Dillies <yael.dillies@gmail.com>
2025-12-17 18:21:25 +0100

    cleanups

--- Mathlib/Probability/Distributions/SetBernoulli.lean
+++ Mathlib/Probability/Distributions/SetBernoulli.lean
@@ -18,12 +18,17 @@ For a set `u : Set ι` and `p` between `0` and `1`, this is the measure on `Set 
 ## Notation
 
 `setBer(u, p)` is the product of `p`-Bernoulli distributions on `u`.
+
+## TODO
+
+It is painful to convert from `unitInterval` to `ENNReal`. Should we introduce a coercion or
+explicit operation (like `unitInterval.toNNReal`, note the lack of dot notation!)?
 -/
 
 public section
 
 open MeasureTheory Measure unitInterval
-open scoped Finset
+open scoped ENNReal Finset
 
 namespace ProbabilityTheory
 variable {ι Ω : Type*} {m : MeasurableSpace Ω} {X Y : Ω → Set ι} {s u : Set ι} {i j : ι} {p q : I}
@@ -35,30 +40,28 @@ on `Set V` such that each element of `u` is taken with probability `p`, and the 
 `u` are never taken. -/
 @[expose]
 noncomputable def setBernoulli : Measure (Set ι) :=
-  .comap (fun s i ↦ i ∈ s) <| infinitePi fun i ↦
+  .comap (fun s i ↦ i ∈ s) <| infinitePi fun i : ι ↦
     toNNReal p • dirac (i ∈ u) + toNNReal (σ p) • dirac False
 
 @[inherit_doc] scoped notation "setBer(" u ", " p ")" => setBernoulli u p
 
 instance : IsProbabilityMeasure setBer(u, p) :=
   MeasurableEquiv.setOf.symm.measurableEmbedding.isProbabilityMeasure_comap <| .of_forall fun P ↦
-    ⟨{a | P a}, rfl⟩
+    ⟨{i | P i}, rfl⟩
 
 variable (u p) in
 lemma setBernoulli_eq_map :
-    setBer(u, p) = .map (fun p ↦ {i | p i})
-      (infinitePi fun i ↦ toNNReal p • dirac (i ∈ u) + toNNReal (σ p) • dirac False) :=
+    setBer(u, p) = .map (fun p : ι → Prop ↦ {i | p i})
+      (infinitePi fun i : ι ↦ toNNReal p • dirac (i ∈ u) + toNNReal (σ p) • dirac False) :=
   MeasurableEquiv.setOf.comap_symm
 
 lemma setBernoulli_apply (S : Set (Set ι)) :
-    setBer(u, p) S = (infinitePi fun a ↦ toNNReal p • dirac (a ∈ u) + toNNReal (σ p) • dirac False)
-      ((fun t a ↦ a ∈ t) '' S) := by
-  convert MeasurableEquiv.setOf.symm.measurableEmbedding.comap_apply ..
+    setBer(u, p) S = (infinitePi fun i ↦ toNNReal p • dirac (i ∈ u) + toNNReal (σ p) • dirac False)
+      ((fun t i ↦ i ∈ t) '' S) := MeasurableEquiv.setOf.symm.measurableEmbedding.comap_apply ..
 
 lemma setBernoulli_apply' (S : Set (Set ι)) :
-    setBer(u, p) S = (infinitePi fun a ↦ toNNReal p • dirac (a ∈ u) + toNNReal (σ p) • dirac False)
-      ((fun p ↦ {a | p a}) ⁻¹' S) := by
-  convert MeasurableEquiv.setOf.symm.comap_apply ..
+    setBer(u, p) S = (infinitePi fun i ↦ toNNReal p • dirac (i ∈ u) + toNNReal (σ p) • dirac False)
+      ((fun p ↦ {i | p i}) ⁻¹' S) := MeasurableEquiv.setOf.symm.comap_apply ..
 
 variable (u) in
 @[simp] lemma setBernoulli_zero : setBer(u, 0) = dirac ∅ := by simp [setBernoulli_eq_map]
@@ -74,19 +77,16 @@ lemma setBernoulli_ae_subset : ∀ᵐ s ∂setBer(u, p), s ⊆ u := by
   change _ = _
   simp only [Set.compl_setOf, Set.not_subset_iff_exists_mem_notMem, Set.setOf_exists, Set.setOf_and,
     measure_iUnion_null_iff]
-  rintro a
-  by_cases ha : a ∈ u
+  rintro i
+  by_cases hi : i ∈ u
   · simp [*]
   calc
-    setBer(u, p) ({s | a ∈ s} ∩ {s | a ∉ u})
-    _ = setBer(u, p) {s | a ∈ s} := by simp [ha]
-    _ = infinitePi (fun a ↦ toNNReal p • dirac (a ∈ u) + toNNReal (σ p) • dirac False)
-            (cylinder {a} {fun _ ↦ True}) := by
-      rw [setBernoulli_apply']
-      congr!
-      ext
-      simp [funext_iff]
-    _ = 0 := by simp [infinitePi_cylinder _ (.singleton _), ha]
+    setBer(u, p) ({s | i ∈ s} ∩ {s | i ∉ u})
+    _ = setBer(u, p) {s | i ∈ s} := by simp [hi]
+    _ = infinitePi (fun i ↦ toNNReal p • dirac (i ∈ u) + toNNReal (σ p) • dirac False)
+          (cylinder {i} {fun _ ↦ True}) := by
+      rw [setBernoulli_apply']; congr!; ext; simp [funext_iff]
+    _ = 0 := by simp [infinitePi_cylinder, hi]
 
 variable (u p) in
 @[simp] lemma setBernoulli_singleton (hsu : s ⊆ u) (hu : u.Finite) :
@@ -95,11 +95,11 @@ variable (u p) in
   lift u to Finset ι using hu
   calc
     setBer(u, p) {s}
-    _ = ∏' i, ((if i ∈ u ↔ i ∈ s then (toNNReal p : ENNReal) else 0) +
-          if i ∈ s then 0 else (toNNReal (σ p) : ENNReal)) := by
+    _ = ∏' i, ((if i ∈ u ↔ i ∈ s then (toNNReal p : ℝ≥0∞) else 0) +
+          if i ∈ s then 0 else (toNNReal (σ p) : ℝ≥0∞)) := by
       simp [setBernoulli_apply, Set.image_singleton, Set.indicator]
       simp [ENNReal.smul_def]
-    _ = ∏ i ∈ u, (if i ∈ s then (toNNReal p : ENNReal) else (toNNReal (σ p) : ENNReal)) := by
+    _ = ∏ i ∈ u, (if i ∈ s then (toNNReal p : ℝ≥0∞) else (toNNReal (σ p) : ℝ≥0∞)) := by
       rw [tprod_eq_prod, Finset.prod_congr rfl] <;>
         simp +contextual [ite_add_ite, mt (@hsu _), ← ENNReal.coe_add]
     _ = toNNReal p ^ s.ncard * toNNReal (σ p) ^ (↑u \ s).ncard := by
