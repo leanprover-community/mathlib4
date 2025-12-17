f915d5adc10a1776d568a2f16a605e063fdccf5f
Kim Morrison <kim@tqft.net>
2025-12-17 02:26:27 +0000

    fix: add module declaration

--- Mathlib/CategoryTheory/Monoidal/Rigid/Functor.lean
+++ Mathlib/CategoryTheory/Monoidal/Rigid/Functor.lean
@@ -3,6 +3,8 @@ Copyright (c) 2025 Kim Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kim Morrison
 -/
+module
+
 import Mathlib.CategoryTheory.Monoidal.Rigid.Basic
 import Mathlib.CategoryTheory.Monoidal.Opposite
 
