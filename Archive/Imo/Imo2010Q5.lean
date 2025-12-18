8c3accf3deee53d96e5ce9e65af9d75ef43aa22c
Parcly Taxel <reddeloostw@gmail.com>
2025-12-18 12:15:57 +0800

    reduce imports further

--- Archive/Imo/Imo2010Q5.lean
+++ Archive/Imo/Imo2010Q5.lean
@@ -3,11 +3,12 @@ Copyright (c) 2025 Jeremy Tan. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jeremy Tan
 -/
+import Mathlib.Algebra.Group.Fin.Basic
 import Mathlib.Algebra.Order.Pi
 import Mathlib.Algebra.Order.Ring.Nat
 import Mathlib.Algebra.Order.Sub.Basic
 import Mathlib.Algebra.Order.Sub.Prod
-import Mathlib.Data.ZMod.Defs
+import Mathlib.Data.Fintype.Basic
 
 /-!
 # IMO 2010 Q5
