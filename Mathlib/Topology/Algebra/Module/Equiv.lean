7b8ef6ecf56e2fff6c8ad7d4f8b18d6f2dc96678
Yury G. Kudryashov <urkud@urkud.name>
2025-12-18 08:15:20 -0600

    Apply suggestion from @themathqueen
    Co-authored-by: Monica Omar <23701951+themathqueen@users.noreply.github.com>

--- Mathlib/Topology/Algebra/Module/Equiv.lean
+++ Mathlib/Topology/Algebra/Module/Equiv.lean
@@ -1234,10 +1234,8 @@ protected theorem inverse (hf : f.IsInvertible) : f.inverse.IsInvertible := by
 
 protected theorem of_isInvertible_inverse (hf : f.inverse.IsInvertible) : f.IsInvertible := by
   by_contra H
-  rw [inverse, dif_neg H, isInvertible_zero_iff] at hf
-  cases hf
-  obtain rfl : f = 0 := Subsingleton.elim _ _
-  simp_all [isInvertible_zero_iff]
+  obtain ⟨_, _⟩ : Subsingleton M ∧ Subsingleton M₂ := by simpa [inverse, H] using hf
+  simp_all [Subsingleton.elim f 0]
 
 @[simp]
 theorem _root_.ContinuousLinearMap.isInvertible_inverse_iff :
