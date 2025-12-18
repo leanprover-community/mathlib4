b71ea474c77b347ad4207182b36797244e64d70b
Aaron Liu <aaronliu2008@outlook.com>
2025-12-18 14:20:42 +0000

    feat(Topology): Completely Regular Space iff Uniformizable (#32080)
    This PR continues the work from #24096.
    
    Original PR: https://github.com/leanprover-community/mathlib4/pull/24096
    
    Co-authored-by: Junyan Xu <junyanxu.math@gmail.com>
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- Mathlib/Data/Rel.lean
+++ Mathlib/Data/Rel.lean
@@ -381,6 +381,9 @@ instance isRefl_univ : SetRel.IsRefl (.univ : SetRel α α) where
 instance isRefl_inter [R₁.IsRefl] [R₂.IsRefl] : (R₁ ∩ R₂).IsRefl where
   refl _ := ⟨R₁.rfl, R₂.rfl⟩
 
+instance IsRefl.comp [R₁.IsRefl] [R₂.IsRefl] : (R₁.comp R₂).IsRefl where
+  refl _ := ⟨_, R₁.rfl, R₂.rfl⟩
+
 protected lemma IsRefl.sInter {ℛ : Set <| SetRel α α} (hℛ : ∀ R ∈ ℛ, R.IsRefl) :
     SetRel.IsRefl (⋂₀ ℛ) where
   refl _a R hR := (hℛ R hR).refl _
