eb3e554c30257569bfe230fe959102af613de12b
David Ledvinka <dledvinka.ledvinka@mail.utoronto.ca>
2025-12-18 20:05:44 +0000

    partial progress

--- Mathlib/MeasureTheory/Function/ConditionalLExpectation.lean
+++ Mathlib/MeasureTheory/Function/ConditionalLExpectation.lean
@@ -13,6 +13,7 @@ noncomputable irreducible_def condLExp (mΩ : MeasurableSpace Ω) (P : Measure[m
     (X : Ω → ℝ≥0∞) : Ω → ℝ≥0∞ :=
   if hm : mΩ ≤ mΩ₀ then
     if _ : SigmaFinite (P.trim hm) then
+      if _ : Measurable[mΩ] X then X else
       ∂((P.withDensity X).trim hm)/∂(P.trim hm)
     else 0
   else 0
@@ -40,20 +41,93 @@ theorem condLExp_of_not_le (hm_not : ¬mΩ ≤ mΩ₀) : P⁻[X|mΩ] = 0 := by
 theorem condLExp_of_not_sigmaFinite (hm : mΩ ≤ mΩ₀) (hμm_not : ¬SigmaFinite (P.trim hm)) :
     P⁻[X|mΩ] = 0 := by rwa [condLExp, dif_pos hm, dif_neg]
 
+theorem condLExp_eq_self (hm : mΩ ≤ mΩ₀) (P : Measure[mΩ₀] Ω) [hσ : SigmaFinite (P.trim hm)]
+    {X : Ω → ℝ≥0∞} (hX : Measurable[mΩ] X) : P⁻[X|mΩ] = X := by
+  simp [condLExp, hm, hσ, hX]
+
+theorem condLExp_of_not_sub_sigma_measurable (hm : mΩ ≤ mΩ₀) (P : Measure[mΩ₀] Ω)
+    [hσ : SigmaFinite (P.trim hm)] {X : Ω → ℝ≥0∞} (hX : ¬Measurable[mΩ] X) :
+    P⁻[X|mΩ] = ∂((P.withDensity X).trim hm)/∂(P.trim hm) := by
+  simp [condLExp, hm, hσ, hX]
+
 theorem measurable_condLExp (mΩ : MeasurableSpace Ω) (P : Measure[mΩ₀] Ω) (X : Ω → ℝ≥0∞) :
     Measurable[mΩ] P⁻[X|mΩ] := by
   by_cases hm : mΩ ≤ mΩ₀
-  · by_cases h : SigmaFinite (P.trim hm) <;> simpa [condLExp, hm, h] using (by fun_prop)
-  · simpa [condLExp, hm] using (by fun_prop)
+  · by_cases hσ : SigmaFinite (P.trim hm)
+    · by_cases hX : Measurable[mΩ] X
+      · simp [condLExp_eq_self hm, hX]
+      simp [condLExp_of_not_sub_sigma_measurable hm _ hX, measurable_rnDeriv]
+    simp [condLExp_of_not_sigmaFinite hm hσ, measurable_zero]
+  simp [condLExp_of_not_le hm, measurable_zero]
 
-theorem lintegral_condLExp {mΩ : MeasurableSpace Ω} (hm : mΩ ≤ mΩ₀) (P : Measure[mΩ₀] Ω)
-     [hσ : SigmaFinite (P.trim hm)] (X : Ω → ℝ≥0∞) (s : Set Ω) (hs : MeasurableSet[mΩ] s) :
+variable (hm : mΩ ≤ mΩ₀)
+
+theorem setLIntegral_condLExp (P : Measure[mΩ₀] Ω) [hσ : SigmaFinite (P.trim hm)]
+    (X : Ω → ℝ≥0∞) {s : Set Ω} (hs : MeasurableSet[mΩ] s) :
     ∫⁻ ω in s, P⁻[X|mΩ] ω ∂P = ∫⁻ ω in s, X ω ∂P := by
+  by_cases hX : Measurable[mΩ] X
+  · simp [condLExp_eq_self hm _ hX]
   have h := AbsolutelyContinuous.trim (withDensity_absolutelyContinuous P X) hm
   have : SFinite ((P.withDensity X).trim hm) := sFinite_of_absolutelyContinuous h
-  simp only [condLExp, hm, hσ, ↓reduceDIte,]
-  rw [ ← lintegral_indicator (hm s hs), ← lintegral_trim hm (by measurability),
-    lintegral_indicator hs, setLIntegral_rnDeriv' h hs, trim_measurableSet_eq hm hs,
-    withDensity_apply _ (hm s hs)]
+  rw [condLExp_of_not_sub_sigma_measurable hm _ hX, ← lintegral_indicator (hm s hs),
+    ← lintegral_trim hm (by measurability), lintegral_indicator hs, setLIntegral_rnDeriv' h hs,
+    trim_measurableSet_eq hm hs, withDensity_apply _ (hm s hs)]
+
+theorem lintegral_condLExp (P : Measure[mΩ₀] Ω) [hσ : SigmaFinite (P.trim hm)] (X : Ω → ℝ≥0∞) :
+    ∫⁻ ω, P⁻[X|mΩ] ω ∂P = ∫⁻ ω, X ω ∂P := by
+  simpa [← setLIntegral_univ] using setLIntegral_condLExp _ _ _ .univ
+
+theorem eq_condLExp_ae (P : Measure[mΩ₀] Ω) [hσ : SigmaFinite (P.trim hm)]
+    (X : Ω → ℝ≥0∞) {Y : Ω → ℝ≥0∞} (hY : Measurable[mΩ] Y)
+    (hXY : ∀ s, MeasurableSet[mΩ] s → ∫⁻ ω in s, P⁻[X|mΩ] ω ∂P = ∫⁻ ω in s, Y ω ∂P) :
+    Y =ᵐ[P] P⁻[X|mΩ] := by
+  --change P {ω | Y ω ≠ P⁻[X|mΩ] ω} = 0
+  have h₁ : MeasurableSet[mΩ] {ω | Y ω > P⁻[X|mΩ] ω} := by sorry
+  have h₂ : MeasurableSet[mΩ] {ω | Y ω < P⁻[X|mΩ] ω} := by sorry
+  --have h₃ := lintegral_condLExp hm P X h₁
+  --have h₄ := lintegral_condLExp hm P X h₂
+  sorry
+
+theorem condLExp_const (P : Measure[mΩ₀] Ω) [hσ : SigmaFinite (P.trim hm)] (c : ℝ≥0∞) :
+    P⁻[fun _ : Ω ↦ c|mΩ] = fun _ ↦ c := condLExp_eq_self _ _ (measurable_const)
+
+@[gcongr]
+theorem condLExp_congr_ae (P : Measure[mΩ₀] Ω)
+    {X Y : Ω → ℝ≥0∞} (hXY : X =ᵐ[P] Y) : P⁻[X|mΩ] =ᵐ[P] P⁻[Y|mΩ] := by
+  by_cases hm : mΩ ≤ mΩ₀
+  · by_cases hσ : SigmaFinite (P.trim hm)
+    · refine eq_condLExp_ae _ _ _ (measurable_condLExp _ _ _) (fun s hs ↦ ?_)
+      repeat rw [setLIntegral_condLExp _ _ _ hs]
+      apply setLIntegral_congr_fun_ae (hm s hs)
+      filter_upwards [hXY] using (fun _ h _ ↦ h.symm)
+    simp [condLExp_of_not_sigmaFinite hm hσ]
+  simp [condLExp_of_not_le hm]
+
+@[gcongr]
+theorem condLExp_congr_ae_trim (P : Measure[mΩ₀] Ω)
+    {X Y : Ω → ℝ≥0∞} (hXY : X =ᵐ[P] Y) : P⁻[X|mΩ] =ᵐ[P.trim hm] P⁻[Y|mΩ] := by
+  apply ae_eq_trim_of_measurable hm (measurable_condLExp _ _ X) (measurable_condLExp _ _ Y)
+  exact condLExp_congr_ae P hXY
+
+-- we need `measurable_bot_iff`
+#check MeasurableSpace.SeparatesPoints
+-- similar proof should work where we rule out the not `FiniteMeasure` case
+theorem condLExp_bot' (X : Ω → ℝ≥0∞) :
+    P⁻[X|⊥] = fun _ => (P .univ)⁻¹ • ∫⁻ ω, X ω ∂P := by sorry
+
+theorem condLExp_bot [IsProbabilityMeasure P] (X : Ω → ℝ≥0∞) :
+    P⁻[X|⊥] = fun _ => ∫⁻ ω, X ω ∂P := by sorry
+
+-- condLExp_mono
+
+-- condLExp_add
+
+-- condLExp_finset_sum
+
+-- condLExp_smul
+
+-- Add docs
+
+-- Good for first PR?
 
 end MeasureTheory
