4b55b612592af7a1f627a80ca55fd02f6313db55
Yury G. Kudryashov <urkud@urkud.name>
2025-12-17 20:42:48 -0600

    feat(Topology/Algebra/Module): lemmas about `IsInvertible`

--- Mathlib/Topology/Algebra/Module/Equiv.lean
+++ Mathlib/Topology/Algebra/Module/Equiv.lean
@@ -1204,6 +1204,48 @@ theorem ringInverse_eq_inverse : Ring.inverse = inverse (R := R) (M := M) := by
   rw [← ringInverse_eq_inverse]
   exact Ring.inverse_one _
 
+namespace IsInvertible
+
+variable {f : M →L[R] M₂}
+
+@[simp]
+theorem self_comp_inverse (hf : f.IsInvertible) : f ∘L f.inverse = .id _ _ := by
+  rcases hf with ⟨e, rfl⟩
+  simp
+
+@[simp]
+theorem inverse_comp_self (hf : f.IsInvertible) : f.inverse ∘L f = .id _ _ := by
+  rcases hf with ⟨e, rfl⟩
+  simp
+
+protected theorem bijective (hf : f.IsInvertible) : Function.Bijective f := by
+  rcases hf with ⟨e, rfl⟩
+  simp [ContinuousLinearEquiv.bijective]
+
+protected theorem injective (hf : f.IsInvertible) : Function.Injective f :=
+  hf.bijective.injective
+
+protected theorem surjective (hf : f.IsInvertible) : Function.Surjective f :=
+  hf.bijective.surjective
+
+protected theorem inverse (hf : f.IsInvertible) : f.inverse.IsInvertible := by
+  rcases hf with ⟨e, rfl⟩
+  simp
+
+protected theorem of_isInvertible_inverse (hf : f.inverse.IsInvertible) : f.IsInvertible := by
+  by_contra H
+  rw [inverse, dif_neg H, isInvertible_zero_iff] at hf
+  cases hf
+  obtain rfl : f = 0 := Subsingleton.elim _ _
+  simp_all [isInvertible_zero_iff]
+
+@[simp]
+theorem _root_.ContinuousLinearMap.isInvertible_inverse_iff :
+    f.inverse.IsInvertible ↔ f.IsInvertible :=
+  ⟨.of_isInvertible_inverse, .inverse⟩
+
+end IsInvertible
+
 /-- Composition of a map on a product with the exchange of the product factors -/
 theorem coprod_comp_prodComm [ContinuousAdd M] (f : M₂ →L[R] M) (g : M₃ →L[R] M) :
     f.coprod g ∘L ContinuousLinearEquiv.prodComm R M₃ M₂ = g.coprod f := by
