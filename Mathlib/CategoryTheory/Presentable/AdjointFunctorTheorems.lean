e6e906aa61e5919c46e61cfe373ebafc835ec04e
Dagur Asgeirsson <dagurtomas@gmail.com>
2025-12-19 15:32:32 -0700

    golf

--- Mathlib/CategoryTheory/Presentable/AdjointFunctorTheorems.lean
+++ Mathlib/CategoryTheory/Presentable/AdjointFunctorTheorems.lean
@@ -19,25 +19,23 @@ lemma presentableAdjointFunctorTheorem₁ (L : C ⥤ D) [PreservesColimits L] [H
 
 lemma presentableAdjointFunctorTheorem₂ (R : C ⥤ D) [IsAccessible.{v} R] [PreservesLimits R]
     [HasLimits C] : R.IsRightAdjoint := by
-  apply isRightAdjoint_of_preservesLimits_of_solutionSetCondition
-  intro A
+  refine isRightAdjoint_of_preservesLimits_of_solutionSetCondition _ fun A ↦ ?_
   obtain ⟨κ₁, _, ⟨h⟩⟩ := IsAccessible.exists_cardinal (F := R)
   obtain ⟨κ₀₁, _, h₁⟩ := IsLocallyPresentable.exists_cardinal (C := C)
   obtain ⟨κ₀₂, _, h₂⟩ := IsLocallyPresentable.exists_cardinal (C := D)
-  have hh : ∃ (κ₀ : Cardinal.{v}) (_ : Fact κ₀.IsRegular), IsCardinalLocallyPresentable C κ₀ ∧
-      IsCardinalLocallyPresentable D κ₀ := by
+  obtain ⟨κ₀, _, hC, hD⟩ : ∃ (κ₀ : Cardinal.{v}) (_ : Fact κ₀.IsRegular),
+      IsCardinalLocallyPresentable C κ₀ ∧ IsCardinalLocallyPresentable D κ₀ := by
     have : Fact (κ₀₁ ⊔ κ₀₂).IsRegular := ⟨iteInduction (fun a ↦ Fact.out) (fun a ↦ Fact.out)⟩
     have h₀₁ : κ₀₁ ≤ κ₀₁ ⊔ κ₀₂ := by simp
     have h₀₂ : κ₀₂ ≤ κ₀₁ ⊔ κ₀₂ := by simp
     refine ⟨κ₀₁ ⊔ κ₀₂, inferInstance, ?_, ?_⟩
     · exact IsCardinalLocallyPresentable.of_le _ h₀₁
     · exact IsCardinalLocallyPresentable.of_le _ h₀₂
-  obtain ⟨κ₀, _, hC, hD⟩ := hh
-  have : ∃ (κ : Cardinal.{v}) (_ : Fact κ.IsRegular), κ₀ ≤ κ ∧ κ₁ ≤ κ ∧
+  obtain ⟨P, _, ⟨le, h⟩⟩ := hD.1
+  obtain ⟨J, _, cf, ⟨⟨⟨diag, ι, hc⟩, hx⟩⟩⟩ := h A
+  obtain ⟨κ', hκ', lt⟩ := HasCardinalLT.exists_regular_cardinal (Arrow J)
+  obtain ⟨κ, _, h₀, h₁, ⟨_⟩⟩ : ∃ (κ : Cardinal.{v}) (_ : Fact κ.IsRegular), κ₀ ≤ κ ∧ κ₁ ≤ κ ∧
       isCardinalPresentable D κ A := by
-    obtain ⟨P, _, ⟨le, h⟩⟩ := hD.1
-    obtain ⟨J, _, cf, ⟨⟨⟨diag, ι, hc⟩, hx⟩⟩⟩  := h A
-    obtain ⟨κ', hκ', lt⟩ := HasCardinalLT.exists_regular_cardinal (Arrow J)
     have : Fact (κ₀ ⊔ κ₁ ⊔ κ').IsRegular :=
       ⟨iteInduction (fun a ↦ hκ') (fun a ↦ iteInduction (fun a ↦ Fact.out) (fun a ↦ Fact.out))⟩
     have hκ₀ : κ₀ ≤ κ₀ ⊔ κ₁ ⊔ κ' := by simp
@@ -48,15 +46,11 @@ lemma presentableAdjointFunctorTheorem₂ (R : C ⥤ D) [IsAccessible.{v} R] [Pr
       apply isCardinalPresentable_of_le _ hκ₀
     exact ⟨κ₀ ⊔ κ₁ ⊔ κ', inferInstance, hκ₀, hκ₁,
       isCardinalPresentable_of_isColimit _ hc _ (lt.of_le (by simp))⟩
-  obtain ⟨κ, _, h₀, h₁, ⟨_⟩⟩ := this
-  have hC : IsCardinalLocallyPresentable C κ := IsCardinalLocallyPresentable.of_le _ h₀
-  obtain ⟨P, _, ⟨_, h⟩⟩ := hC.1
+  obtain ⟨P, _, ⟨_, h⟩⟩ := (IsCardinalLocallyPresentable.of_le C h₀).1
   obtain ⟨Q, _, hQP, hPQ⟩ := ObjectProperty.EssentiallySmall.exists_small_le P
-  let ι : Type v := Shrink (Subtype Q)
-  refine ⟨(X : ι) × (A ⟶ R.obj ((equivShrink _).symm X).val),
-    fun X ↦ ((equivShrink _).symm X.fst).val, fun X ↦ X.snd, ?_⟩
-  intro X f
-  obtain ⟨J, _, _, ⟨⟨⟨diag, ι, hc⟩, hx⟩⟩⟩  := h X
+  refine ⟨(X : Shrink (Subtype Q)) × (A ⟶ R.obj ((equivShrink _).symm X).val),
+    fun X ↦ ((equivShrink _).symm X.fst).val, fun X ↦ X.snd, fun X f ↦ ?_⟩
+  obtain ⟨J, _, _, ⟨⟨⟨diag, ι, hc⟩, hx⟩⟩⟩ := h X
   have : IsCardinalFiltered J κ₁ := IsCardinalFiltered.of_le J h₁
   replace hc := isColimitOfPreserves (coyoneda.obj ⟨A⟩) (isColimitOfPreserves R hc)
   obtain ⟨j, hj, w⟩ := Types.jointly_surjective_of_isColimit hc f
