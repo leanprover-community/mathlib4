bbf5d49df0dea0f2781887e3939ef7751c282327
Joël Riou <37772949+joelriou@users.noreply.github.com>
2025-12-19 13:08:44 +0000

    feat(Algebra/Homology): equivOfIsKInjective (#32814)
    We construct a bijection `CohomologyClass K L n ≃ SmallShiftedHom.{w} (HomologicalComplex.quasiIso C (.up ℤ)) K L n` when `K` and `L` are cochain complexes, and `L` is `K`-injective, or `K` is `K`-projective.

--- Mathlib/Algebra/Homology/DerivedCategory/KInjective.lean
+++ Mathlib/Algebra/Homology/DerivedCategory/KInjective.lean
@@ -5,7 +5,7 @@ Authors: Joël Riou
 -/
 module
 
-public import Mathlib.Algebra.Homology.DerivedCategory.Basic
+public import Mathlib.Algebra.Homology.DerivedCategory.SmallShiftedHom
 public import Mathlib.Algebra.Homology.HomotopyCategory.KInjective
 
 /-!
@@ -27,9 +27,44 @@ open CategoryTheory
 
 variable {C : Type u} [Category.{v} C] [Abelian C]
 
-lemma CochainComplex.IsKInjective.Qh_map_bijective [HasDerivedCategory C]
+open CategoryTheory Localization DerivedCategory
+
+namespace CochainComplex
+
+lemma IsKInjective.Qh_map_bijective [HasDerivedCategory C]
     (K : HomotopyCategory C (ComplexShape.up ℤ))
     (L : CochainComplex C ℤ) [L.IsKInjective] :
     Function.Bijective (DerivedCategory.Qh.map :
       (K ⟶ (HomotopyCategory.quotient _ _).obj L) → _ ) :=
   (CochainComplex.IsKInjective.rightOrthogonal L).map_bijective_of_isTriangulated _ _
+
+namespace HomComplex.CohomologyClass
+
+variable (K L : CochainComplex C ℤ) (n : ℤ)
+  [HasSmallLocalizedShiftedHom.{w} (HomologicalComplex.quasiIso C (.up ℤ)) ℤ K L]
+
+lemma bijective_toSmallShiftedHom_of_isKInjective [L.IsKInjective] :
+    Function.Bijective (toSmallShiftedHom.{w} (K := K) (L := L) (n := n)) := by
+  letI := HasDerivedCategory.standard C
+  rw [← Function.Bijective.of_comp_iff'
+      (SmallShiftedHom.equiv _ DerivedCategory.Q).bijective,
+    ← Function.Bijective.of_comp_iff' (Iso.homCongr ((quotientCompQhIso C).symm.app K)
+      ((Q.commShiftIso n).symm.app L ≪≫ (quotientCompQhIso C).symm.app (L⟦n⟧))).bijective]
+  convert (CochainComplex.IsKInjective.Qh_map_bijective _ _).comp (toHom_bijective K L n)
+  ext x
+  obtain ⟨x, rfl⟩ := x.mk_surjective
+  simp [toHom_mk, ShiftedHom.map]
+
+variable {K L n} in
+/-- When `L` is a K-injective cochain complex, cohomology classes
+in `CohomologyClass K L n` identify to elements in a type `SmallShiftedHom` relatively
+to quasi-isomorphisms. -/
+@[simps! -isSimp]
+noncomputable def equivOfIsKInjective [L.IsKInjective] :
+    CohomologyClass K L n ≃
+      SmallShiftedHom.{w} (HomologicalComplex.quasiIso C (.up ℤ)) K L n :=
+  Equiv.ofBijective _ (bijective_toSmallShiftedHom_of_isKInjective _ _ _)
+
+end HomComplex.CohomologyClass
+
+end CochainComplex
