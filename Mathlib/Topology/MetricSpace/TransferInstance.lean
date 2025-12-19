c5aaaf260fff1a8c98fe06348fc205e82529826d
Attila Gáspár <58485900+gasparattila@users.noreply.github.com>
2025-12-19 20:25:34 +0100

    feat: transferring a metric space along a `Homeomorph` or `UniformEquiv`

--- Mathlib/Topology/MetricSpace/TransferInstance.lean
+++ Mathlib/Topology/MetricSpace/TransferInstance.lean
@@ -6,9 +6,10 @@ Authors: Michael Rothgang
 module
 
 public import Mathlib.Topology.MetricSpace.Basic
+public import Mathlib.Topology.UniformSpace.Equiv
 
 /-!
-# Transfer metric space structures across `Equiv`s
+# Transfer metric space structures across `Equiv`s, `Homeomorph`s and `UniformEquiv`s
 
 In this file, we transfer a distance and (pseudo-)metric space structure across an equivalence.
 
@@ -34,3 +35,31 @@ protected abbrev metricSpace [MetricSpace β] (e : α ≃ β) : MetricSpace α :
   .induced e e.injective ‹_›
 
 end Equiv
+
+namespace Homeomorph
+
+variable [TopologicalSpace α]
+
+/-- Transfer a `PseudoMetricSpace` across a `Homeomorph`, keeping the existing topology. -/
+protected abbrev pseudometricSpace [PseudoMetricSpace β] (e : α ≃ₜ β) : PseudoMetricSpace α :=
+  .replaceTopology (.induced e ‹_›) e.induced_eq.symm
+
+/-- Transfer a `MetricSpace` across a `Homeomorph`, keeping the existing topology. -/
+protected abbrev metricSpace [MetricSpace β] (e : α ≃ₜ β) : MetricSpace α :=
+  .replaceTopology (.induced e e.injective ‹_›) e.induced_eq.symm
+
+end Homeomorph
+
+namespace UniformEquiv
+
+variable [UniformSpace α]
+
+/-- Transfer a `PseudoMetricSpace` across a `UniformEquiv`, keeping the existing uniformity. -/
+protected abbrev pseudometricSpace [PseudoMetricSpace β] (e : α ≃ᵤ β) : PseudoMetricSpace α :=
+  .replaceUniformity (.induced e ‹_›) (congrArg _ e.comap_eq.symm)
+
+/-- Transfer a `MetricSpace` across a `UniformEquiv`, keeping the existing uniformity. -/
+protected abbrev metricSpace [MetricSpace β] (e : α ≃ᵤ β) : MetricSpace α :=
+  .replaceUniformity (.induced e e.injective ‹_›) (congrArg _ e.comap_eq.symm)
+
+end UniformEquiv
