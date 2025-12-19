2ba213ecf26c984334dd1a05ec012774262d9494
Yaël Dillies <yael.dillies@gmail.com>
2025-12-19 15:53:39 +0000

    feat: an irreducible element of a submonoid isn't zero (#32433)
    From ClassFieldTheory

--- Mathlib/Algebra/GroupWithZero/NonZeroDivisors.lean
+++ Mathlib/Algebra/GroupWithZero/NonZeroDivisors.lean
@@ -37,6 +37,10 @@ assert_not_exists Ring
 
 open Function
 
+lemma Irreducible.coe_ne_zero {M₀ S : Type*} [MonoidWithZero M₀] [SetLike S M₀]
+    [SubmonoidClass S M₀] {s : S} {x : s} (hx : Irreducible x) : (x : M₀) ≠ 0 :=
+  fun h ↦ hx.1 <| by simpa using hx.2 (a := x) (b := x) (by ext; simp [h])
+
 section
 variable (M₀ : Type*) [MonoidWithZero M₀] {x : M₀}
 
