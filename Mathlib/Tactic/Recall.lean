abcaf80e28730bdd7f9f4b3fa51ea4b29d129417
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2025-12-20 00:50:34 +0100

    revert change for `recall`

--- Mathlib/Tactic/Recall.lean
+++ Mathlib/Tactic/Recall.lean
@@ -44,7 +44,7 @@ open Lean Meta Elab Command Term
 elab_rules : command
   | `(recall $id $sig:optDeclSig $[$val?]?) => withoutModifyingEnv do
     let declName := id.getId
-    addTermInfo' id (← mkConstWithLevelParams declName) (isBinder := true)
+    addConstInfo id declName
     let info ← getConstInfo declName
     let declConst : Expr := mkConst declName <| info.levelParams.map Level.param
     let newId := ({ namePrefix := declName : DeclNameGenerator }.mkUniqueName (← getEnv) `recall).1
