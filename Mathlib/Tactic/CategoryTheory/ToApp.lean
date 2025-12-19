5ef4d8edcd340db28192b4734d150638e15439c1
Kim Morrison <kim@tqft.net>
2025-12-19 02:00:04 +0000

    more adapatation notes

--- Mathlib/Tactic/CategoryTheory/ToApp.lean
+++ Mathlib/Tactic/CategoryTheory/ToApp.lean
@@ -93,8 +93,13 @@ def toCatExpr (e : Expr) : MetaM Expr := do
   let value ← apprec 0 value
   return value
 
+#adaptation_note
+/--
+`toNatTrans_congr` was marked `private` in #31807,
+but we have removed this when disabling `set_option backward.privateInPublic` as a global option.
+-/
 universe v u in
-private def toNatTrans_congr {C D : Cat.{v, u}} {F G : C ⟶ D} {η θ : F ⟶ G} (h : η = θ) :
+def toNatTrans_congr {C D : Cat.{v, u}} {F G : C ⟶ D} {η θ : F ⟶ G} (h : η = θ) :
   η.toNatTrans = θ.toNatTrans := congr(($h).toNatTrans)
 
 /--
