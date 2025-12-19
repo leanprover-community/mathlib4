9d8015fae7bb377eaa59803579d1b182f275c580
Jeremy Tan Jie Rui <54175463+Parcly-Taxel@users.noreply.github.com>
2025-12-19 01:57:49 +0000

    feat: a simple unit-distance embedding of the Heawood graph (#32684)
    Cf. [Zulip](https://leanprover.zulipchat.com/#narrow/channel/287929-mathlib4/topic/A.20simple.20unit-distance.20embedding.20of.20the.20Heawood.20graph/near/562930187). The embedding is depicted below, with vertex labels as in the formalisation.
    
    <img width="692" height="1045" alt="heawood-ud" src="https://github.com/user-attachments/assets/c1ae2fce-318b-4442-9332-9fb68cd6ce7f" />
    
    Co-authored-by: Parcly Taxel <reddeloostw@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -10488,6 +10488,7 @@ public import Mathlib.Combinatorics.SimpleGraph.Triangle.Removal
 public import Mathlib.Combinatorics.SimpleGraph.Triangle.Tripartite
 public import Mathlib.Combinatorics.SimpleGraph.Turan
 public import Mathlib.Combinatorics.SimpleGraph.Tutte
+public import Mathlib.Combinatorics.SimpleGraph.UnitDistance.Basic
 public import Mathlib.Combinatorics.SimpleGraph.UniversalVerts
 public import Mathlib.Combinatorics.SimpleGraph.Walk
 public import Mathlib.Combinatorics.SimpleGraph.Walks.Basic
