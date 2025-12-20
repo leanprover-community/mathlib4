13be159de16571a3d4f6bad60109237e0b58512a
Batixx <s59fpern@uni-bonn.de>
2025-12-20 08:16:37 +0100

    update mathlib

--- Mathlib.lean
+++ Mathlib.lean
@@ -3750,6 +3750,7 @@ public import Mathlib.Data.Nat.Choose.Cast
 public import Mathlib.Data.Nat.Choose.Central
 public import Mathlib.Data.Nat.Choose.Dvd
 public import Mathlib.Data.Nat.Choose.Factorization
+public import Mathlib.Data.Nat.Choose.Inversion
 public import Mathlib.Data.Nat.Choose.Lucas
 public import Mathlib.Data.Nat.Choose.Mul
 public import Mathlib.Data.Nat.Choose.Multinomial
