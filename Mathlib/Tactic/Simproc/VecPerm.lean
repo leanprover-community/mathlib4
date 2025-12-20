4ef44fd97ea4fc1da1ab13fa997746832f578b95
Paul Lezeau <paul.lezeau@gmail.com>
2025-12-20 16:59:02 +0100

    Rename stuff and document

--- Mathlib/Tactic/Simproc/VecPerm.lean
+++ Mathlib/Tactic/Simproc/VecPerm.lean
@@ -12,29 +12,47 @@ open Lean Elab Meta Simp Qq
 
 namespace FinVec
 
-private partial def mkList {u : Level} {α : Q(Type u)} {n : Q(ℕ)}
+/--
+Takes an expression representing a vector `Fin n → α` and returns the corresponding
+list `List α`. Fails if the vector is not constucted using `Matrix.vecCons` and `Matrix.vecEmpty`.
+-/
+private partial def listOfVecQ {u : Level} {α : Q(Type u)} {n : Q(ℕ)}
     (vec : Q(Fin $n → $α)) : MetaM (Option <| List Q($α)) := do
   match n, vec with
   | ~q(Nat.succ $m), ~q(Matrix.vecCons $lastOut $prevVec) =>
-    let some prevList ← mkList prevVec | return none
+    let some prevList ← listOfVecQ prevVec | return none
     return some <| lastOut :: prevList
   | ~q(0), ~q(Matrix.vecEmpty) => return some []
 
-private partial def mkVec {u : Level} {α : Q(Type u)}
+/--
+Takes an expression representing a list of elements of type `α` and outputs the corresponding
+vector `Fin n → α`.
+-/
+private partial def vecOfListQ {u : Level} {α : Q(Type u)}
     (n : Q(ℕ)) (vec : Q(List $α)) : MetaM Q(Fin $n → $α) := do
   match n, vec with
   | ~q(Nat.succ $prev), ~q($head :: $rest) =>
-    let prevVec ← mkVec prev rest
+    let prevVec ← vecOfListQ prev rest
     return q(Matrix.vecCons $head $prevVec)
   | ~q(0), ~q([]) => return q(Matrix.vecEmpty)
 
+/--
+Given a list `l` of elements `α` and a list `perm` of indices (as natural numbers), outputs
+the list whose `i`th entry is `l[perm[i]]`. If `perm[i]` is out of bounds, we simply move
+to the next `i`.
+In the case where `perm ~ [0, ..., l.length-1]`, this is just computing the permutation of `l`
+represented by `perm`.
+-/
 private def permList {α : Type*} (vec : List α) (perm : List Nat) : List α :=
   perm.foldr (init := []) fun head current ↦
     match vec[head]? with
     | some entry =>  entry :: current
     | none => current
 
-private def permAsList (n : Q(ℕ)) (vn : ℕ) (perm : Q(Fin $n → Fin $n)) :
+/-- Given an expression representing a vector `perm : Fin n → Fin n`, computes the corresponding
+list of term of type `Fin n`. This is meant to be used when `perm corresponds to a permutation
+of `Fin n`, e.g. `perm = Equiv.swap 0 1`, etc. -/
+private def listOfVecFinQ (n : Q(ℕ)) (vn : ℕ) (perm : Q(Fin $n → Fin $n)) :
     MetaM (Option <| List (Fin vn)) :=
   try
     let _ ← synthInstanceQ q(NeZero $n)
@@ -46,7 +64,6 @@ private def permAsList (n : Q(ℕ)) (vn : ℕ) (perm : Q(Fin $n → Fin $n)) :
     return some out
   catch _ =>
     return none
-
 
 /--
 The `vecPerm` simproc computes the new entries of a vector after applying a permutation to them.
@@ -60,12 +77,12 @@ simproc_decl vecPerm (_ ∘ _) := fun e ↦ do
   let ⟨_, ~q(Fin $n → $α), ~q($v ∘ $p)⟩ ← inferTypeQ' e | return .continue
   let ⟨_, ~q(Fin $n → $α), ~q($v)⟩ ← inferTypeQ' v| return .continue
   let some qp ← Qq.checkTypeQ p q(Fin $n → Fin $n) | return .continue
-  let some unpermList ← mkList (α := α) (n:= n) v | return .continue
+  let some unpermList ← listOfVecQ (α := α) (n:= n) v | return .continue
   let vn := unpermList.length
-  let some permAsList ← permAsList n vn qp | return .continue
+  let some permAsList ← listOfVecFinQ n vn qp | return .continue
   let outAsList := permList unpermList permAsList
   let outAsListQ := outAsList.foldr (fun head list  ↦ q($head :: $list)) q([])
-  let out ← mkVec n outAsListQ
+  let out ← vecOfListQ n outAsListQ
   let pf ← mkAppM ``FinVec.etaExpand_eq #[e]
   let pf ← mkAppM ``Eq.symm #[pf]
   let result : Result := {expr := out, proof? := some pf}
