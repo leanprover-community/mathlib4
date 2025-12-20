d4fcba026e31c261255db50e938ac5c5ef8240f6
Jovan Gerbscheid <56355248+JovanGerb@users.noreply.github.com>
2025-12-20 00:00:17 +0000

    fix: don't use `addConstInfo` on constant defining syntax (#33067)
    As pointed out by `fpvandoorn`, `addConstInfo` doesn't use `isBinder := true`, so the information that this constant is defined at the syntax is not available in the VSCode UI. So we revert back to using `addTermInfo'`.

--- Mathlib/Tactic/HigherOrder.lean
+++ Mathlib/Tactic/HigherOrder.lean
@@ -95,7 +95,7 @@ def higherOrderGetParam (thm : Name) (stx : Syntax) : AttrM Name := do
           type := hot
           value := prf }
       addDeclarationRangesFromSyntax hothmName (← getRef) ref
-      addConstInfo ref hothmName
+      addTermInfo' ref (← mkConstWithLevelParams hothmName) (isBinder := true)
       let hsm := simpExtension.getState (← getEnv) |>.lemmaNames.contains (.decl thm)
       if hsm then
         addSimpTheorem simpExtension hothmName true false .global 1000
