35717a808a30ef0bef69e63238df9494af725607
Bingyu Xia <71547343+BryceT233@users.noreply.github.com>
2025-12-22 10:59:02 +0800

    Add files via upload

--- Mathlib/RingTheory/MvPowerSeries/rename.lean
+++ Mathlib/RingTheory/MvPowerSeries/rename.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Bingyu Xia
 -/
 
-import Mathlib
+import Mathlib.RingTheory.MvPowerSeries.Basic
 
 /-!
 # Renaming variables of power series
