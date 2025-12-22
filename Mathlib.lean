0fa56eda99ca7bfc60dde89af38f7dc763e11bed
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2025-12-22 09:02:14 +0000

    feat(RingTheory/MvPolynomial/IrrQuadratic): irreducibility of sum X_i Y_i (#31161)
    The quadratic polynomial $$\sum_i c_i X_i Y_i$$ is irreducible, provided the following three conditions holds:
    * the ground ring is a domain
    * there are at least 2 nonzero $$c_i$$
    * only units of $$R$$ divide all $$c_i$$.
    
    This was used in the initial proof that the transvections have determinant 1. Now,
    a more general result is proved there and this PR is no more needed.
    I believe that it remains of independent interest (irreducibility of quadrics in algebraic geometry) but the maintainers might want that the general one.
    
    Co-authored-by: Johan Commelin <johan@commelin.net>
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -13319,6 +13319,7 @@ public import Mathlib.RingTheory.MvPolynomial.FreeCommRing
 public import Mathlib.RingTheory.MvPolynomial.Groebner
 public import Mathlib.RingTheory.MvPolynomial.Homogeneous
 public import Mathlib.RingTheory.MvPolynomial.Ideal
+public import Mathlib.RingTheory.MvPolynomial.IrreducibleQuadratic
 public import Mathlib.RingTheory.MvPolynomial.Localization
 public import Mathlib.RingTheory.MvPolynomial.MonomialOrder
 public import Mathlib.RingTheory.MvPolynomial.MonomialOrder.DegLex
