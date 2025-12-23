d23d094c3fb626a655518beb6a5911da8c8f0f83
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-12-23 15:31:59 +0100

    chore: disable the linter in a few buggy cases for now --- should fix these before landing!

--- Mathlib/Data/Finset/Slice.lean
+++ Mathlib/Data/Finset/Slice.lean
@@ -121,6 +121,11 @@ def slice (𝒜 : Finset (Finset α)) (r : ℕ) : Finset (Finset α) := {A ∈ �
 @[inherit_doc]
 scoped[Finset] infixl:90 " # " => Finset.slice
 
+-- TODO: a bug in the linter causes non-desired outside around Finset.slice; diagnose and fix!
+-- (or disable the linter properly around this construct)
+set_option linter.style.commandStart false
+
+
 /-- `A` is in the `r`-th slice of `𝒜` iff it's in `𝒜` and has cardinality `r`. -/
 theorem mem_slice : A ∈ 𝒜 # r ↔ A ∈ 𝒜 ∧ #A = r :=
   mem_filter
--- Mathlib/Topology/FiberBundle/Constructions.lean
+++ Mathlib/Topology/FiberBundle/Constructions.lean
@@ -54,6 +54,7 @@ theorem isInducing_toProd : IsInducing (TotalSpace.toProd B F) :=
 def homeomorphProd : TotalSpace F (Trivial B F) ≃ₜ B × F :=
   (TotalSpace.toProd _ _).toHomeomorphOfIsInducing (isInducing_toProd B F)
 
+set_option linter.style.commandStart false -- linter false positive, TODO fix!
 /-- Local trivialization for trivial bundle. -/
 @[simps!]
 def trivialization : Trivialization F (π F (Bundle.Trivial B F)) where
@@ -135,6 +136,7 @@ def Prod.toFun' : TotalSpace (F₁ × F₂) (E₁ ×ᵇ E₂) → B × F₁ × F
 
 variable {e₁ e₂}
 
+set_option linter.style.commandStart false -- linter false positive, TODO fix!
 theorem Prod.continuous_to_fun : ContinuousOn (Prod.toFun' e₁ e₂)
     (π (F₁ × F₂) (E₁ ×ᵇ E₂) ⁻¹' (e₁.baseSet ∩ e₂.baseSet)) := by
   let f₁ : TotalSpace (F₁ × F₂) (E₁ ×ᵇ E₂) → TotalSpace F₁ E₁ × TotalSpace F₂ E₂ :=
@@ -241,6 +243,7 @@ variable [∀ x, Zero (E₁ x)] [∀ x, Zero (E₂ x)] [∀ x : B, TopologicalSp
   trivialization_mem_atlas' b :=
     ⟨trivializationAt F₁ E₁ b, trivializationAt F₂ E₂ b, inferInstance, inferInstance, rfl⟩
 
+set_option linter.style.commandStart false -- linter false positive, TODO fix!
 instance {e₁ : Trivialization F₁ (π F₁ E₁)} {e₂ : Trivialization F₂ (π F₂ E₂)}
     [MemTrivializationAtlas e₁] [MemTrivializationAtlas e₂] :
     MemTrivializationAtlas (e₁.prod e₂ : Trivialization (F₁ × F₂) (π (F₁ × F₂) (E₁ ×ᵇ E₂))) where
@@ -271,6 +274,7 @@ the projections to the base and the map to the original bundle are continuous. -
 instance Pullback.TotalSpace.topologicalSpace : TopologicalSpace (TotalSpace F (f *ᵖ E)) :=
   pullbackTopology F E f
 
+set_option linter.style.commandStart false -- linter false positive, TODO fix!
 theorem Pullback.continuous_proj (f : B' → B) : Continuous (π F (f *ᵖ E)) := by
   rw [continuous_iff_le_induced, Pullback.TotalSpace.topologicalSpace, pullbackTopology_def]
   exact inf_le_left
