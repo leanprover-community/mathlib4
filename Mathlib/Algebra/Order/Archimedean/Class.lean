196dc1f2a6edb224eab21d71a6d7dda8a6dd794b
Michael Rothgang <10105016+grunweg@users.noreply.github.com>
2025-12-24 10:09:34 +0000

    chore: replace by_contra! and contrapose! with plain versions (#31600)
    When the push_neg step in these tactics does nothing, write just `by_contra` resp. `contrapose`.
    
    Not exhaustive.

--- Mathlib/Algebra/Order/Archimedean/Class.lean
+++ Mathlib/Algebra/Order/Archimedean/Class.lean
@@ -395,8 +395,8 @@ theorem mk_prod {ι : Type*} [LinearOrder ι] {s : Finset ι} (hnonempty : s.Non
     have hminmem : s.min' hs ∈ (Finset.cons i s hi) :=
       Finset.mem_cons_of_mem (Finset.min'_mem _ _)
     have hne : mk (a i) ≠ mk (a (s.min' hs)) := by
-      by_contra!
-      obtain eq := hmono.injOn (by simp) hminmem this
+      by_contra h
+      obtain eq := hmono.injOn (by simp) hminmem h
       rw [eq] at hi
       exact hi (Finset.min'_mem _ hs)
     rw [← ih] at hne
