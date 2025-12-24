bf5b518e7892454ad9b2a029988a6342712eca51
mkaratarakis <mixkarat@gmail.com>
2025-12-24 14:43:31 +0100

    linters are causing issues

--- Mathlib/Analysis/Analytic/Order.lean
+++ Mathlib/Analysis/Analytic/Order.lean
@@ -329,21 +329,6 @@ theorem analyticOrderNatAt_pow (hf : AnalyticAt 𝕜 f z₀) (n : ℕ) :
     analyticOrderNatAt (f ^ n) z₀ = n • analyticOrderNatAt f z₀ := by
   simp [analyticOrderNatAt, analyticOrderAt_pow, hf]
 
-open Set AnalyticAt AnalyticOnNhd
-
-lemma zero_iff_order_inf [PreconnectedSpace 𝕜] (f : 𝕜 → E) (z : 𝕜) (hf : ∀ z, AnalyticAt 𝕜 f z) :
-  (∀ z, f z = 0) ↔ analyticOrderAt f z = ⊤ := by
-  constructor
-  · intro H
-    refine (analyticOrderAt_eq_top).2 <| (frequently_eq_iff_eventually_eq (hf z) analyticAt_const).1
-        (Filter.Frequently.of_forall H)
-  · intros hr
-    rw [@analyticOrderAt_eq_top 𝕜 _ _ _ _ f z,
-        ← frequently_eq_iff_eventually_eq (hf z) (analyticAt_const)] at hr
-    intros z
-    exact (eqOn_zero_of_preconnected_of_frequently_eq_zero (fun x hx => by aesop)
-      (isPreconnected_univ) trivial hr) trivial
-
 end NontriviallyNormedField
 
 /-!
@@ -461,5 +446,19 @@ theorem preimage_zero_mem_codiscrete [ConnectedSpace 𝕜] {x : 𝕜} (hf : Anal
     (hx : f x ≠ 0) :
     f ⁻¹' {0}ᶜ ∈ codiscrete 𝕜 :=
   hf.preimage_zero_mem_codiscreteWithin hx trivial isConnected_univ
+
+lemma zero_iff_order_inf [PreconnectedSpace 𝕜] (f : 𝕜 → E) (z : 𝕜) (hf : ∀ z, AnalyticAt 𝕜 f z) :
+  (∀ z, f z = 0) ↔ analyticOrderAt f z = ⊤ := by
+  constructor
+  · intro H
+    refine (analyticOrderAt_eq_top).2 <|
+      (AnalyticAt.frequently_eq_iff_eventually_eq (hf z) analyticAt_const).1
+      (Filter.Frequently.of_forall H)
+  · intros hr
+    rw [@analyticOrderAt_eq_top 𝕜 _ _ _ _ f z,
+        ← AnalyticAt.frequently_eq_iff_eventually_eq (hf z) (analyticAt_const)] at hr
+    intros z
+    exact (eqOn_zero_of_preconnected_of_frequently_eq_zero (fun x hx => by aesop)
+      (isPreconnected_univ) trivial hr) trivial
 
 end AnalyticOnNhd
