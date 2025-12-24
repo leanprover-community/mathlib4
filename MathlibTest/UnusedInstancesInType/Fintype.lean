156473c765233c51bc50db5048b3f41cfe52542c
thorimur <68410468+thorimur@users.noreply.github.com>
2025-12-24 11:17:34 -0500

    chore: remove outdated comment

--- MathlibTest/UnusedInstancesInType/Fintype.lean
+++ MathlibTest/UnusedInstancesInType/Fintype.lean
@@ -39,7 +39,6 @@ Note: This linter can be disabled with `set_option linter.unusedFintypeInType fa
 theorem foo₂ (a : Type) [∀ α : Type, Fintype α] (_ : Unit) [Fintype a] : True :=
   trivial
 
--- TODO: why the newline + indentation in the pretty-printing of the forall?
 /--
 warning: `foo₃` has the hypotheses:
   • [(α : Type) → Fintype α] (#2)
