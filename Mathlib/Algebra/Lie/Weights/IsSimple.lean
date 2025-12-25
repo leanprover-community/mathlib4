fe523550780468944737f87e232f6664fa9f8aaf
Janos Wolosz <janos.wolosz@gmail.com>
2025-12-25 06:56:31 +0100

    Renaming aux lemma

--- Mathlib/Algebra/Lie/Weights/IsSimple.lean
+++ Mathlib/Algebra/Lie/Weights/IsSimple.lean
@@ -266,7 +266,7 @@ noncomputable def invtSubmoduleToLieIdeal (q : Submodule K (Dual K H))
       ⨆ α : {α : Weight K H L // ↑α ∈ q ∧ α.IsNonZero}, sl2SubmoduleOfRoot α.2.2 :=
   rfl
 
-lemma H_le_invtSubmoduleToLieIdeal_top :
+private lemma H_le_invtSubmoduleToLieIdeal_top_aux :
     (H.toLieSubmodule : Submodule K L) ≤
       invtSubmoduleToLieIdeal (⊤ : Submodule K (Module.Dual K H)) (by simp) := by
   have h_span : Submodule.span K (Set.range (coroot : Weight K H L → H)) = ⊤ :=
@@ -294,7 +294,7 @@ lemma H_le_invtSubmoduleToLieIdeal_top :
 @[simp] lemma invtSubmoduleToLieIdeal_top :
     invtSubmoduleToLieIdeal (⊤ : Submodule K (Module.Dual K H)) (by simp) = ⊤ := by
   set I := invtSubmoduleToLieIdeal (⊤ : Submodule K (Module.Dual K H)) (by simp)
-  have h₁ := H_le_invtSubmoduleToLieIdeal_top (H := H)
+  have h₁ := H_le_invtSubmoduleToLieIdeal_top_aux (H := H)
   have h₂ : ∀ α : H.root, (rootSpace H α : Submodule K L) ≤ (I : Submodule K L) := fun ⟨α, hα⟩ =>
     (le_of_le_of_eq (le_sup_of_le_left le_sup_left) (sl2SubmoduleOfRoot_eq_sup α _).symm).trans
       (le_iSup_of_le ⟨α, trivial, (Finset.mem_filter.mp hα).2⟩ le_rfl)
