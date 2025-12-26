2c61b9c0c44675b741aabf56c02a53191118d6de
Scott Carnahan <128885296+ScottCarnahan@users.noreply.github.com>
2025-12-26 12:44:19 +0900

    Lie structure on finsupps

--- Mathlib/Algebra/Lie/Loop.lean
+++ Mathlib/Algebra/Lie/Loop.lean
@@ -6,8 +6,8 @@ Authors: Scott Carnahan
 module
 
 public import Mathlib.Algebra.Lie.BaseChange
-public import Mathlib.Algebra.Lie.InvariantForm
-public import Mathlib.Algebra.Lie.Extension
+--public import Mathlib.Algebra.Lie.InvariantForm
+--public import Mathlib.Algebra.Lie.Extension
 public import Mathlib.Algebra.Polynomial.Laurent
 public import Mathlib.LinearAlgebra.TensorProduct.Basis
 
@@ -62,8 +62,19 @@ namespace LoopAlgebra
 instance instLieRing : LieRing (LoopAlgebra R L) :=
   ExtendScalars.instLieRing R (LaurentPolynomial R) L
 
-instance instLieAlgebra : LieAlgebra R (LoopAlgebra R L) :=
-  LieAlgebra.RestrictScalars.lieAlgebra R (LaurentPolynomial R) (LaurentPolynomial R ⊗[R] L)
+instance instLieAlgebra : LieAlgebra (LaurentPolynomial R) (LoopAlgebra R L) :=
+  ExtendScalars.instLieAlgebra R (LaurentPolynomial R) L
+
+--instance : LieAlgebra R (LoopAlgebra R L) :=
+--  LieAlgebra.RestrictScalars.lieAlgebra R (LaurentPolynomial R) (LaurentPolynomial R ⊗[R] L)
+
+--#synth Module R (LoopAlgebra R L) --succeeds
+--#synth LieAlgebra R (LoopAlgebra R L) --fails
+
+instance instLieModule : LieModule (LaurentPolynomial R) (LoopAlgebra R L) (LoopAlgebra R L) :=
+    lieAlgebraSelfModule (L := LoopAlgebra R L)
+
+--#synth LieModule R (LoopAlgebra R L) (LoopAlgebra R L) --fails
 
 /-- The linear map taking `x` to `T ^ n ⊗ x`. -/
 def monomial (n : ℤ) : L →ₗ[R] LoopAlgebra R L :=
@@ -106,6 +117,52 @@ lemma monomial_injective (n : ℤ) : Function.Injective (monomial R L n) := by
   rw [← toFinsupp_symm_single]
   exact (EmbeddingLike.comp_injective _ (toFinsupp R L).symm).mpr (Finsupp.single_injective n)
 
+/-- A Lie ring structure on finitely supported functions on a Lie algebra `L`. -/
+def finsuppLieRing : LieRing (ℤ →₀ L) where
+  bracket x y := toFinsupp R L ⁅(toFinsupp R L).symm x, (toFinsupp R L).symm y⁆
+  add_lie := by simp
+  lie_add := by simp
+  lie_self := by simp
+  leibniz_lie := by simp
+
+@[simp]
+lemma finsuppLieRing_apply (x y : ℤ →₀ L) :
+    letI := finsuppLieRing R L
+    ⁅x, y⁆ = (toFinsupp R L) ⁅(toFinsupp R L).symm x, (toFinsupp R L).symm y⁆ := rfl
+
+lemma bracketHom (x y : LoopAlgebra R L) :
+    letI := finsuppLieRing R L
+    ⁅(toFinsupp R L) x, (toFinsupp R L) y⁆ = toFinsupp R L ⁅x, y⁆ := by
+  simp
+
+/-- The scalar multiplication of Laurent polynomials on finsupps. -/
+@[simps]
+def laurentSMul : SMul (LaurentPolynomial R) (ℤ →₀ L) where
+  smul r x := toFinsupp R L (r • ((toFinsupp R L).symm x))
+
+/-- The `R[T,T⁻¹]`-Lie algebra structure on finsupp. -/
+def finsuppLieAlgebra :
+    letI := finsuppLieRing R L
+    LieAlgebra (LaurentPolynomial R) (ℤ →₀ L) :=
+  letI := finsuppLieRing R L
+  { smul r x := (laurentSMul R L).smul r x
+    one_smul a := by ext; simp
+    mul_smul r s x := by ext; simp [← mul_smul]
+    smul_zero := by simp
+    smul_add := by simp
+    add_smul r s x := by ext; simp [add_smul]
+    zero_smul := by simp
+    lie_smul r x y := by
+      ext n
+      simp [laurentSMul_smul] }
+
+/-- The `R`-Lie algebra structure on finsupp. -/
+def finsuppRestrictLieAlgebra :
+    letI := finsuppLieRing R L
+    LieAlgebra R (ℤ →₀ L) :=
+  letI := finsuppLieRing R L
+  letI := finsuppLieAlgebra R L
+  LieAlgebra.RestrictScalars.lieAlgebra R (LaurentPolynomial R) (ℤ →₀ L)
 
 /-!
 /-- The evaluation representation, given by composing a representation with the evaluation map
@@ -121,7 +178,7 @@ lemma monomial_injective (n : ℤ) : Function.Injective (monomial R L n) := by
 -/
 
 open Pointwise in
-lemma finite_support_add {α A : Type*} [AddMonoid A] {f g : α → A} (hf : Finite f.support)
+lemma finite_support_add {α A : Type*} [AddZeroClass A] {f g : α → A} (hf : Finite f.support)
     (hg : Finite g.support) :
     Finite (f + g).support := by
   refine Finite.Set.subset (f.support ∪ g.support) ?_
@@ -129,13 +186,13 @@ lemma finite_support_add {α A : Type*} [AddMonoid A] {f g : α → A} (hf : Fin
   contrapose! hn
   simp only [Set.mem_union, Function.mem_support, ne_eq, not_or, not_not] at hn
   simp [hn.1, hn.2]
---#find_home! ofSupportFiniteAdd --Mathlib.Data.Finsupp.Fintype
 
 lemma add_finsupp {α A : Type*} [AddMonoid A] {f g : α → A} (hf : Finite f.support)
     (hg : Finite g.support) :
     Finsupp.ofSupportFinite f hf + Finsupp.ofSupportFinite g hg =
       Finsupp.ofSupportFinite (f + g) (finite_support_add hf hg) := by
   ext; simp [Finsupp.add_apply, Finsupp.ofSupportFinite_coe]
+--#find_home! add_finsupp --[Mathlib.Algebra.Group.Finsupp]
 
 section CentralExt
 
@@ -179,6 +236,14 @@ def residuePairingFinsupp (Φ : LinearMap.BilinForm R L) :
     rw [smul_finsum' _ (residuePairing_finite_support R L Φ x _), finsum_congr]
     intro k
     simp [mul_left_comm]
+/-
+/-- A cochain on finsupp -/
+def residuePairingCochain (Φ : LinearMap.BilinForm R L) :
+    letI := finsuppLieRing R L
+    letI := finsuppRestrictLieAlgebra R L
+    LieModule.Cohomology.twoCochain R (ℤ →₀ L) (TrivialLieModule R (LoopAlgebra R L) R) where
+  val := (residuePairingFinsupp R L Φ).compr₂
+    ((TrivialLieModule.equiv R (LoopAlgebra R L) R).symm.toLinearMap)
 
 /-- The residue pairing on a Loop algebra, with values in a trivial module. -/
 def residuePairingLoop (Φ : LinearMap.BilinForm R L) :
@@ -187,7 +252,16 @@ def residuePairingLoop (Φ : LinearMap.BilinForm R L) :
     ((TrivialLieModule.equiv R (LoopAlgebra R L) R).symm.toLinearMap)).compl₂
     (toFinsupp R L).toLinearMap).comp (toFinsupp R L).toLinearMap
 
-/-
+/-- A 2-cocycle on a loop algebra given by an invariant bilinear form. -/
+def twoCocycle_of_Bilinear' (Φ : LinearMap.BilinForm R L)
+    (hΦ : LinearMap.BilinForm.lieInvariant L Φ) :
+    LieModule.Cohomology.twoCochain R (LoopAlgebra R L)
+      (TrivialLieModule R (LoopAlgebra R L) R) where
+  val := residuePairingLoop R L Φ
+  property := by
+    sorry
+
+
 /-- A 2-cocycle on a loop algebra given by an invariant bilinear form. -/
 def twoCocycle_of_Bilinear (Φ : LinearMap.BilinForm R L)
     (hΦ : LinearMap.BilinForm.lieInvariant L Φ) :
