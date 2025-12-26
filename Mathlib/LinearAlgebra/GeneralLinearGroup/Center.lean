ccc4af01f0432799de89a91a482737ee348e7949
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2025-12-26 16:19:36 +0100

    progress

--- Mathlib/LinearAlgebra/GeneralLinearGroup/Center.lean
+++ Mathlib/LinearAlgebra/GeneralLinearGroup/Center.lean
@@ -9,7 +9,7 @@ module
 public import Mathlib.LinearAlgebra.Center
 
 /-!
-# Center of the group of linear endomorphisms
+# Center of the group of linear automorphisms
 
 -/
 
@@ -19,11 +19,23 @@ open Module LinearMap LinearEquiv Set
 
 variable {R V : Type*}
 
+theorem Module.Basis.index_nontrivial_iff
+    {R : Type*} [Semiring R] [StrongRankCondition R]
+    {M : Type*} [AddCommMonoid M] [Module R M] [Nontrivial M]
+    {ι : Type*} (b : Basis ι R M) :
+    Nontrivial ι ↔ finrank R M ≠ 1 := by
+  simp [← not_iff_not, ne_eq, not_nontrivial_iff_subsingleton,
+    Module.finrank_eq_nat_card_basis b, Nat.card_eq_one_iff_unique,
+    b.index_nonempty]
+
+
 namespace LinearEquiv
 
-theorem isUnit_ratio
-    [Semiring R] [AddCommMonoid V] [Module R V] [Free R V] [Nontrivial V]
-    {f : V ≃ₗ[R] V} {a : R} (hfa : ∀ x, f x = a • x) :
+section
+
+variable [Semiring R] [AddCommMonoid V] [Module R V] [Free R V] [Nontrivial V]
+
+theorem isUnit_ratio {f : V ≃ₗ[R] V} {a : R} (hfa : ∀ x, f x = a • x) :
     IsUnit a := by
   rw [isUnit_iff_exists]
   let ι := Free.ChooseBasisIndex R V
@@ -47,12 +59,15 @@ theorem isUnit_ratio
     simpa using (hfa 1).symm
   simpa using congr(b.coord i $((this)))
 
+end
+
+variable [Ring R] [AddCommGroup V] [Module R V]
+    {ι : Type*}
+
 /-- A linear automorphism of a free module of rank at least 2
 that commutes with transvections consists of homotheties with central ratio. -/
-theorem commute_transvections_iff_of_basis
-    [Ring R] [AddCommGroup V] [Module R V]
-    {ι : Type*} [Nontrivial ι] (b : Basis ι R V)
-    {f : V ≃ₗ[R] V}
+theorem commute_transvections_iff_of_basis [Nontrivial ι]
+    (b : Basis ι R V) {f : V ≃ₗ[R] V}
     (hcomm : ∀ i j (r : R) (hij : i ≠ j),
       Commute f (LinearEquiv.transvection (f := b.coord i) (v := r • b j) (by
       simp [Finsupp.single_eq_of_ne hij]))) :
@@ -67,13 +82,27 @@ theorem commute_transvections_iff_of_basis
   refine ⟨a, hfa.1, ?_, fun x ↦ by simpa using hfa.2 x⟩
   apply isUnit_ratio (f := f) (fun x ↦ by simpa using hfa.2 x)
 
+/-- A linear automorphism of a free module of rank at least 2
+that commutes with transvections consists of homotheties with central invertible ratio. -/
+theorem commute_transvections_iff [StrongRankCondition R] [Free R V]
+    (hV : finrank R V ≠ 1) {f : V ≃ₗ[R] V}
+    (hcomm : ∀ (l : Dual R V) (v : V) (hlv : l v = 0), Commute f (LinearEquiv.transvection hlv)) :
+    ∃ a ∈ Subring.center R, IsUnit a ∧ ∀ x, f x = a • x := by
+  let ι := Free.ChooseBasisIndex R V
+  let b : Basis ι R V := Free.chooseBasis R V
+  rcases subsingleton_or_nontrivial V
+  · refine ⟨1, by simp, by simp, fun x ↦ Subsingleton.allEq _ _⟩
+  rw [← b.index_nontrivial_iff] at hV
+  exact commute_transvections_iff_of_basis b (fun _ _ _ _ ↦ hcomm _ ..)
+
+
 /-- The center of linear automorphisms of a free module of rank ≠ 1
 consists of homotheties with central ratio.
 
 This version requires a basis with a nontrivial type of indices.
 Under `StrongRankCondition R`, use `LinearEquiv.mem_center_iff`. -/
-theorem mem_center_iff_of_basis [Ring R] [AddCommGroup V] [Module R V]
-    {ι : Type*} [Nontrivial ι] (b : Basis ι R V) {f : V ≃ₗ[R] V} :
+theorem mem_center_iff_of_basis [Nontrivial ι]
+    (b : Basis ι R V) {f : V ≃ₗ[R] V} :
     f ∈ Subgroup.center (V ≃ₗ[R] V) ↔
       ∃ a ∈ Subring.center R, IsUnit a ∧ ∀ x : V, f x = a • x  := by
   constructor
@@ -91,11 +120,10 @@ theorem mem_center_iff_of_basis [Ring R] [AddCommGroup V] [Module R V]
 /-- The center of linear automorphisms of a free module of rank ≠ 1
 consists of homotheties with central ratio.
 
-This version requires `StrongRankCondition R`. Otherwise, use `LinearEquiv.mem_center_iff`. -/
-theorem mem_center_iff
-    [Ring R] [StrongRankCondition R]
-    [AddCommGroup V] [Module R V] [Free R V] (hV : finrank R V ≠ 1)
-    {f : V ≃ₗ[R] V} :
+This version requires `StrongRankCondition R`.
+Otherwise, use `LinearEquiv.mem_center_iff_of_basis`. -/
+theorem mem_center_iff [StrongRankCondition R] [Free R V]
+    (hV : finrank R V ≠ 1) {f : V ≃ₗ[R] V} :
     f ∈ Subgroup.center (V ≃ₗ[R] V) ↔
       ∃ a ∈ Subring.center R, IsUnit a ∧ ∀ x : V, f x = a • x  := by
   let ι := Free.ChooseBasisIndex R V
@@ -107,18 +135,8 @@ theorem mem_center_iff
     · rintro ⟨a, hcomm, hunt, hf⟩
       simp only [Subgroup.mem_center_iff]
       intro; ext; simp [hf]
-  rcases subsingleton_or_nontrivial ι with hι | hι
-  · exfalso
-    contrapose hV
-    have : Nonempty ι := inferInstance
-    have : Fintype ι := Fintype.ofFinite ι
-    rw [finrank_eq_card_basis b, ← Nat.card_eq_fintype_card,
-    Nat.card_eq_one_iff_unique]
-    aesop
+  rw [← b.index_nontrivial_iff] at hV
   exact mem_center_iff_of_basis b
-
-variable [Ring R] [StrongRankCondition R]
-    [AddCommGroup V] [Module R V] [Free R V]
 
 end LinearEquiv
 
@@ -169,9 +187,5 @@ theorem mem_center_iff
     rintro ⟨a, ha, hfa⟩
     rw [Semigroup.mem_center_iff]
     aesop (add norm hfa)
-
-def centerMap [Semiring R] [AddCommMonoid V] [Module R V] :
-    Subgroup.center Rˣ →* Subgroup.center (V →ₗ[R] V)ˣ :=
-  sorry
 
 end LinearMap.GeneralLinearGroup
