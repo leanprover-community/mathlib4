7178389f00054b1888a804f3529be498c8a5a608
Harald Husum <harald.husum@gmail.com>
2025-12-26 22:32:42 +0000

    doc(Combinatorics): fix file refs (#33272)
    Fix some stale documentation file refs.

--- Mathlib/Combinatorics/Matroid/Rank/ENat.lean
+++ Mathlib/Combinatorics/Matroid/Rank/ENat.lean
@@ -46,7 +46,7 @@ which is why mathlib defines matroids using bases/independence. )
 It is natural to ask if equicardinality of bases holds if 'cardinality' refers to
 a term in `Cardinal` instead of `ℕ∞`, but the answer is that it doesn't.
 The cardinal-valued rank functions `Matroid.cRank` and `Matroid.cRk` are defined in
-`Mathlib/Data/Matroid/Rank/Cardinal.lean`, but have less desirable properties in general.
+`Mathlib/Combinatorics/Matroid/Rank/Cardinal.lean`, but have less desirable properties in general.
 See the module docstring of that file for a discussion.
 
 ## Implementation Details
