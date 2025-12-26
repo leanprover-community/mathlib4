66cbd93c4c62309dbdadbb8271318ced97c9aa78
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2025-12-26 01:35:05 +0100

    initial commit

--- Mathlib/Algebra/Central/End.lean
+++ Mathlib/Algebra/Central/End.lean
@@ -11,13 +11,47 @@ public import Mathlib.LinearAlgebra.FreeModule.Basic
 /-!
 # `Module.End R M` is a central algebra
 
-This file shows that the algebra of endomorphisms on a free module is central.
+This file shows that the algebra of endomorphisms on a free module
+over a commutative semiring is central.
+
+* `Module.End.exists_apply_eq_smul`:
+in the noncommutative case, the center of `End R M` consists of homotheties with central ratio.
+
 -/
 
 open Module Free
 
 namespace Algebra.IsCentral
-variable {R M : Type*} [CommSemiring R] [AddCommMonoid M] [Module R M] [Free R M]
+
+open Module.Free Module.Basis
+
+variable {R M : Type*} [AddCommMonoid M]
+
+section
+
+variable [Semiring R] [Module R M] [Free R M]
+
+/-- The center of endomorphisms on a free module
+consists of homotheties with central ratio. -/
+theorem _root_.Module.End.exists_apply_eq_smul
+    (f : End R M) (hf : f ∈ Subsemiring.center (End R M)) :
+    ∃ a ∈ Subsemiring.center R, ∀ x, f x = a • x := by
+  rcases subsingleton_or_nontrivial M
+  · refine ⟨1, by simp, ?_⟩
+    intro; apply Subsingleton.allEq
+  let b := Free.chooseBasis R M
+  let i := b.index_nonempty.some
+  simp_rw [Subsemiring.mem_center_iff] at hf ⊢
+  suffices _ by
+    refine ⟨b.coord i (f (b i)),
+      fun r ↦ by simpa using congr(b.coord i $(this (r • b i))),
+      this⟩
+  intro y
+  simpa using congr($(hf ((b.coord i).smulRight y)) (b i)).symm
+
+end
+
+variable [CommSemiring R] [Module R M] [Free R M]
 
 /-- The center of endomorphisms on a free module is trivial,
 in other words, it is a central algebra. -/
