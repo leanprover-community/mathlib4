3a9d815c36ae68217b977b02849befa615a0388f
Snir Broshi <26556598+SnirBroshi@users.noreply.github.com>
2025-12-27 12:44:24 +0200

    no reason to use `@` in `abbrev IsSymm`

--- Mathlib/Order/Defs/Unbundled.lean
+++ Mathlib/Order/Defs/Unbundled.lean
@@ -37,7 +37,7 @@ class IsRefl (α : Sort*) (r : α → α → Prop) : Prop where
 
 /-- `IsSymm X r` means the binary relation `r` on `X` is symmetric. -/
 @[deprecated Std.Symm (since := "2025-12-26")]
-abbrev IsSymm (α : Sort*) (r : α → α → Prop) : Prop := @Std.Symm α r
+abbrev IsSymm (α : Sort*) (r : α → α → Prop) : Prop := Std.Symm r
 
 /-- `IsAsymm X r` means that the binary relation `r` on `X` is asymmetric, that is,
 `r a b → ¬ r b a`. -/
