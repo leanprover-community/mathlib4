f2c1d6286ce53a41cb734228297a76864cefc5a4
Whysoserioushah <yx3021@ic.ac.uk>
2025-12-28 01:37:25 +0800

    minimport

--- Mathlib/RingTheory/Morita/Matrix.lean
+++ Mathlib/RingTheory/Morita/Matrix.lean
@@ -6,9 +6,9 @@ Authors: Yunzhou Xie, Jujian Zhang
 module
 
 public import Mathlib.Algebra.Category.ModuleCat.Basic
-public import Mathlib.LinearAlgebra.Matrix.Module
 public import Mathlib.Data.Matrix.Basis
-public import Mathlib.Algebra.Algebra.Tower
+public import Mathlib.LinearAlgebra.Matrix.Module
+
 
 /-!
 # Morita Equivalece between `R` and `Mₙ(R)`
