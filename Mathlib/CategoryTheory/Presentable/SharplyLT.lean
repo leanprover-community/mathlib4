fc6fa637a669786a47c152bb81ae927430a2c63a
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-12-27 15:37:54 +0100

    wip

--- Mathlib/CategoryTheory/Presentable/SharplyLT.lean
+++ Mathlib/CategoryTheory/Presentable/SharplyLT.lean
@@ -5,10 +5,12 @@ Authors: Joël Riou
 -/
 module
 
+public import Mathlib.CategoryTheory.ObjectProperty.ColimitsCardinalClosure
 public import Mathlib.CategoryTheory.Presentable.CardinalDirectedPoset
 
 /-!
 # Sharply smaller regular cardinals
+
 -/
 
 @[expose] public section
@@ -31,11 +33,45 @@ namespace SharplyLT
 
 lemma le (h : SharplyLT κ₁ κ₂) : κ₁ ≤ κ₂ := h.lt.le
 
+section
+
+/-- Whan `k₁` is sharply smaller than `κ₂`, and `C` is a `κ₁`-accessible category, this
+is a property of objects which allows to show that `C` is a `κ₂`-accessible category.
+This property is defined as the closure of `κ₁`-presentable objects under
+colimits of shape `J` for categories `J` such that `Arrow J` is of cardinality `< κ₂`. -/
+def generator (_ : SharplyLT κ₁ κ₂) (C : Type u) [Category.{v} C] :
+    ObjectProperty C :=
+  (isCardinalPresentable C κ₁).colimitsCardinalClosure κ₂
+
+variable (h : SharplyLT κ₁ κ₂) (C : Type u) [Category.{v} C]
+
+lemma generator_le_sCardinalPresentable [LocallySmall.{w} C] :
+    h.generator C ≤ isCardinalPresentable C κ₂ :=
+  ObjectProperty.colimitsCardinalClosure_le _ _
+    (fun _ _ hJ ↦ isClosedUnderColimitsOfShape_isCardinalPresentable C hJ)
+    (isCardinalPresentable_monotone _ h.le)
+
+instance [IsCardinalAccessibleCategory C κ₁] :
+    ObjectProperty.EssentiallySmall.{w} (h.generator C) := by
+  dsimp [generator]
+  infer_instance
+
+variable [IsCardinalAccessibleCategory C κ₁]
+
+lemma isCardinalFilteredGenerator :
+    (h.generator C).IsCardinalFilteredGenerator κ₂ where
+  le_isCardinalPresentable := h.generator_le_sCardinalPresentable C
+  exists_colimitsOfShape X := by
+    sorry
+
+end
+
 lemma isCardinalAccessible (h : SharplyLT κ₁ κ₂)
     (C : Type u) [Category.{v} C] [IsCardinalAccessibleCategory C κ₁] :
     IsCardinalAccessibleCategory C κ₂ where
   toHasCardinalFilteredColimits := .of_le C h.le
-  exists_generator := sorry
+  exists_generator :=
+    ⟨_, inferInstance, h.isCardinalFilteredGenerator C⟩
 
 lemma trans (h₁₂ : SharplyLT κ₁ κ₂) {κ₃ : Cardinal.{w}} [Fact κ₃.IsRegular]
     (h₂₃ : SharplyLT κ₂ κ₃) :
