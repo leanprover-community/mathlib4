eab2df5ac3fa7b7a4472f9439b143bc443cb47e6
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2025-12-27 23:22:21 +0100

    delete Transvection.Generation (postponed to later PR)

--- Mathlib.lean
+++ Mathlib.lean
@@ -4764,7 +4764,6 @@ public import Mathlib.LinearAlgebra.TensorProduct.Tower
 public import Mathlib.LinearAlgebra.TensorProduct.Vanishing
 public import Mathlib.LinearAlgebra.Trace
 public import Mathlib.LinearAlgebra.Transvection.Basic
-public import Mathlib.LinearAlgebra.Transvection.Generation
 public import Mathlib.LinearAlgebra.UnitaryGroup
 public import Mathlib.LinearAlgebra.Vandermonde
 public import Mathlib.Logic.Basic
