bb2ccf3316fa8556db3843268734c9185adcd487
Frédéric Dupuis <dupuisf@iro.umontreal.ca>
2025-12-30 15:43:14 -0700

    fix file

--- Mathlib/Analysis/Convex/Majorization.lean
+++ Mathlib/Analysis/Convex/Majorization.lean
@@ -67,13 +67,13 @@ lemma decSum_of_le_card {k : ℕ} {x : n → R} (hk : k ≤ card n) :
 noncomputable def incSumSet (k : ℕ) (x : n → R) :=
     if hk : k ≤ card n then
       Classical.choose (exists_min'_image (powersetCard k (univ : Finset n))
-        (fun s => ∑ i ∈ s, x i) (by grind [= powersetCard_nonempty]))
+        (fun s => ∑ i ∈ s, x i) (by grind))
     else ∅
 
 @[simp]
 lemma card_incSumSet (k : ℕ) (x : n → R) (hk : k ≤ card n) : (incSumSet k x).card = k := by
   obtain ⟨hs₁, hs₂⟩ := Classical.choose_spec (exists_min'_image (powersetCard k (univ : Finset n))
-        (fun s => ∑ i ∈ s, x i) (by grind [= powersetCard_nonempty]))
+        (fun s => ∑ i ∈ s, x i) (by grind))
   simp only [mem_powersetCard, subset_univ, true_and, incSumSet, hk, ↓reduceDIte] at ⊢ hs₁
   grind
 
@@ -98,14 +98,15 @@ lemma sum_incSumSet_le (k : ℕ) (x : n → R) (hk : k ≤ card n) (t : Finset n
   rw [← hs₂]
   exact Finset.min'_le _ _ (by grind)
 
-lemma le_of_mem_incSumSet [DecidableEq n] [IsStrictOrderedRing R] (k : ℕ) (x : n → R) (hk : k ≤ card n) (i : n)
-    (hi : i ∈ incSumSet k x) (j : n) (hj : j ∈ (incSumSet k x)ᶜ) : x i ≤ x j := by
+lemma le_of_mem_incSumSet [DecidableEq n] [IsStrictOrderedRing R] (k : ℕ) (x : n → R)
+    (hk : k ≤ card n) (i : n) (hi : i ∈ incSumSet k x) (j : n) (hj : j ∈ (incSumSet k x)ᶜ) :
+    x i ≤ x j := by
   by_contra hcontra
   let s := incSumSet k x
   let t : Finset n := insert j (s.erase i)
   have hs : s = insert i (s.erase i) := by grind
   have tcard : t.card = k := by
-    have : j ∉ s.erase i := by rw [mem_erase]; grind
+    have : j ∉ s.erase i := by rw [mem_erase]; grind [Finset.mem_compl]
     rw [card_insert_of_notMem this, card_erase_of_mem hi]
     grind
   have hsum : ∑ l ∈ t, x l < ∑ l ∈ s, x l := by
@@ -151,14 +152,15 @@ lemma sum_decSumSet_le (k : ℕ) (x : n → R) (hk : k ≤ card n) (t : Finset n
   rw [← hs₂]
   exact Finset.le_max' _ _ (by grind)
 
-lemma ge_of_mem_decSumSet [DecidableEq n] [IsStrictOrderedRing R] (k : ℕ) (x : n → R) (hk : k ≤ card n) (i : n)
-    (hi : i ∈ decSumSet k x) (j : n) (hj : j ∈ (decSumSet k x)ᶜ) : x j ≤ x i := by
+lemma ge_of_mem_decSumSet [DecidableEq n] [IsStrictOrderedRing R] (k : ℕ) (x : n → R)
+    (hk : k ≤ card n) (i : n) (hi : i ∈ decSumSet k x) (j : n) (hj : j ∈ (decSumSet k x)ᶜ) :
+    x j ≤ x i := by
   by_contra hcontra
   let s := decSumSet k x
   let t : Finset n := insert j (s.erase i)
   have hs : s = insert i (s.erase i) := by grind
   have tcard : t.card = k := by
-    have : j ∉ s.erase i := by rw [mem_erase]; grind
+    have : j ∉ s.erase i := by rw [mem_erase]; grind [Finset.mem_compl]
     rw [card_insert_of_notMem this, card_erase_of_mem hi]
     grind
   have hsum : ∑ l ∈ s, x l < ∑ l ∈ t, x l := by
@@ -174,7 +176,8 @@ lemma exists_eq_incSum (k : ℕ) (x : n → R) (hk : k ≤ card n) :
       ∧ incSum k x = ∑ i ∈ s, x i
       ∧ ∀ t : Finset n, t.card = k → ∑ i ∈ s, x i ≤ ∑ i ∈ t, x i := by
   rw [incSum_of_le_card hk]
-  obtain ⟨s, hs₁, hs₂⟩ := exists_min'_image (powersetCard k (univ : Finset n)) (fun s => ∑ i ∈ s, x i) (by grind)
+  obtain ⟨s, hs₁, hs₂⟩ := exists_min'_image (powersetCard k (univ : Finset n))
+      (fun s => ∑ i ∈ s, x i) (by grind)
   refine ⟨s, ?_, hs₂, ?_⟩
   · exact mem_powersetCard_univ.mp hs₁
   · intro t ht
@@ -185,7 +188,8 @@ lemma exists_eq_decSum (k : ℕ) (x : n → R) (hk : k ≤ card n) :
     ∃ s : Finset n, s.card = k ∧ decSum k x = ∑ i ∈ s, x i
       ∧ ∀ t : Finset n, t.card = k → ∑ i ∈ t, x i ≤ ∑ i ∈ s, x i := by
   rw [decSum_of_le_card hk]
-  obtain ⟨s, hs₁, hs₂⟩ := exists_max'_image (powersetCard k (univ : Finset n)) (fun s => ∑ i ∈ s, x i) (by grind)
+  obtain ⟨s, hs₁, hs₂⟩ := exists_max'_image (powersetCard k (univ : Finset n))
+      (fun s => ∑ i ∈ s, x i) (by grind)
   refine ⟨s, ?_, hs₂, ?_⟩
   · exact mem_powersetCard_univ.mp hs₁
   · intro t ht
@@ -248,11 +252,11 @@ lemma decSum_add_incSum [IsStrictOrderedRing R] (k : ℕ) (x : n → R) :
     obtain ⟨t, ht₁, ht₂, ht₃⟩ := exists_eq_incSum (card n - k) x (by grind)
     rw [hs₂, ht₂, ← Finset.sum_add_sum_compl s]
     have h₁ : ∑ i ∈ t, x i ≤ ∑ i ∈ sᶜ, x i := by
-      exact ht₃ sᶜ (by grind)
+      exact ht₃ sᶜ (by grind [Finset.card_compl])
     have h₂ : ∑ i ∈ sᶜ, x i ≤ ∑ i ∈ t, x i := by
       have hs' := Finset.sum_add_sum_compl s x
       have ht' := Finset.sum_add_sum_compl t x
-      specialize hs₃ tᶜ (by grind)
+      specialize hs₃ tᶜ (by grind [Finset.card_compl])
       grind
     grind only
   · have h₁ : decSum k x = ∑ i, x i := by grind [decSum_of_ge_card]
@@ -331,13 +335,13 @@ lemma decSum_comp_equiv (k : ℕ) (x : n → R) (e : m ≃ n) : decSum k (x ∘ 
 lemma decSum_of_antitone [IsOrderedRing R] {n : ℕ} (k : ℕ) (x : Fin n → R) (hx : Antitone x)
     (hk0 : 0 < k) (hkn : k < n) : decSum k x = ∑ i ≤ ⟨k - 1, by grind⟩, x i := by
   let s : Finset (Fin n) := Iic ⟨k - 1, by grind⟩
-  rw [decSum_of_le_card (by grind)]
+  rw [decSum_of_le_card (by grind [Fintype.card_fin])]
   rw [max'_eq_iff]
   refine ⟨?_, ?_⟩
   · rw [mem_image]
     refine ⟨s, ?_, rfl⟩
     rw [mem_powersetCard_univ]
-    grind
+    grind [Fin.card_Iic]
   · intro b hb
     rw [mem_image] at hb
     obtain ⟨a, ha₁, ha₂⟩ := hb
@@ -347,11 +351,12 @@ lemma decSum_of_antitone [IsOrderedRing R] {n : ℕ} (k : ℕ) (x : Fin n → R)
     · simp [has]
     have ha : a = (a ∩ s) ∪ (a \ s) := by grind
     have hs : s = (a ∩ s) ∪ (s \ a) := by grind
-    have hcard : (a \ s).card = (s \ a).card := by rw [card_sdiff_eq_card_sdiff_iff]; grind
-    rw [ha, sum_union (by grind)]
-    conv_rhs => rw [hs, sum_union (by grind)]
+    have hcard : (a \ s).card = (s \ a).card := by
+      rw [card_sdiff_eq_card_sdiff_iff]; grind [Fin.card_Iic]
+    rw [ha, sum_union (by grind [Finset.disjoint_iff_ne])]
+    conv_rhs => rw [hs, sum_union (by grind [Finset.disjoint_iff_ne])]
     gcongr ?_ + ?_
-    let K := ((a \ s).image x).max' <| by grind
+    let K := ((a \ s).image x).max' <| by grind [Finset.image_nonempty]
     calc _ ≤ (a \ s).card • K := by
               apply Finset.sum_le_card_nsmul
               intro i hi
