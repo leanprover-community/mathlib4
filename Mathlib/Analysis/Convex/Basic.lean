65a21d7030b4852725db229e3c4227d940068f48
SnirBroshi <26556598+SnirBroshi@users.noreply.github.com>
2025-12-30 23:52:52 +0000

    feat(Order/Monotone/Defs): weaken hypothesis of `injective_of_le_imp_le` (#33111)
    `injective_of_le_imp_le` requires `∀ x y, f x ≤ f y → x ≤ y`, but `∀ x y, f x = f y → x ≤ y` is sufficient.
    
    Also rename the weakened`injective_of_le_imp_le` to `Function.Injective.of_eq_imp_le` and `injective_of_lt_imp_ne` to `Function.Injective.of_lt_imp_ne` to allow for dot notation.

--- Mathlib/Analysis/Convex/Basic.lean
+++ Mathlib/Analysis/Convex/Basic.lean
@@ -509,7 +509,7 @@ theorem Convex.semilinear_image {s : Set E} (hs : Convex 𝕜 s) (hσ : ∀ {s t
   obtain ⟨t, rfl⟩ : ∃ t : 𝕜, σ t = b := RingHomSurjective.is_surjective ..
   refine ⟨r • x + t • y, hs hx hy (by simp_all [(@hσ 0 r).mp]) (by simp_all [(@hσ 0 t).mp])
     ?_, by simp⟩
-  apply_fun σ using injective_of_le_imp_le _ hσ.mp
+  apply_fun σ using Function.Injective.of_eq_imp_le (hσ.mp ·.le)
   simpa
 
 end SemilinearMap
