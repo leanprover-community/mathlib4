d471fff0739257aff1ca869755434f726f172217
Parcly Taxel <reddeloostw@gmail.com>
2025-12-31 18:01:38 +0800

    chore: remove June 2025 deprecated declarations

--- Mathlib/Algebra/Star/NonUnitalSubalgebra.lean
+++ Mathlib/Algebra/Star/NonUnitalSubalgebra.lean
@@ -623,9 +623,6 @@ theorem mem_starClosure (S : NonUnitalSubalgebra R A) {x : A} :
 theorem starClosure_toNonUnitalSubalgebra (S : NonUnitalSubalgebra R A) :
     S.starClosure.toNonUnitalSubalgebra = S ⊔ star S := rfl
 
-@[deprecated (since := "2025-06-17")] alias
-  starClosure_toNonunitalSubalgebra := starClosure_toNonUnitalSubalgebra
-
 theorem starClosure_le {S₁ : NonUnitalSubalgebra R A} {S₂ : NonUnitalStarSubalgebra R A}
     (h : S₁ ≤ S₂.toNonUnitalSubalgebra) : S₁.starClosure ≤ S₂ :=
   NonUnitalStarSubalgebra.toNonUnitalSubalgebra_le_iff.1 <|
--- Mathlib/Deprecated/RingHom.lean
+++ Mathlib/Deprecated/RingHom.lean
@@ -29,24 +29,11 @@ section
 
 variable {_ : NonAssocSemiring α} {_ : NonAssocSemiring β} (f : α →+* β)
 
-/-- `f : α →+* β` has a trivial codomain iff its range is `{0}`. -/
-@[deprecated "Use range_eq_singleton_iff and codomain_trivial_iff_range_trivial"
-    (since := "2025-06-09")]
-theorem codomain_trivial_iff_range_eq_singleton_zero : (0 : β) = 1 ↔ Set.range f = {0} :=
-  f.codomain_trivial_iff_range_trivial.trans
-    ⟨fun h =>
-      Set.ext fun y => ⟨fun ⟨x, hx⟩ => by simp [← hx, h x], fun hy => ⟨0, by simpa using hy.symm⟩⟩,
-      fun h x => Set.mem_singleton_iff.mp (h ▸ Set.mem_range_self x)⟩
-
 end
 
 section Semiring
 
 variable [Semiring α] [Semiring β]
-@[deprecated map_dvd (since := "2025-06-09")]
-protected theorem map_dvd (f : α →+* β) {a b : α} : a ∣ b → f a ∣ f b :=
-  map_dvd f
-
 end Semiring
 
 end RingHom
