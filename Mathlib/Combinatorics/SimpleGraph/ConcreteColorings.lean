d923574de4f2e6e356dfcc5a5ae1c1edd53bc6d7
Nick Adfor <NickAdfor@outlook.com>
2025-12-31 13:35:07 +0800

    Update ConcreteColorings.lean

--- Mathlib/Combinatorics/SimpleGraph/ConcreteColorings.lean
+++ Mathlib/Combinatorics/SimpleGraph/ConcreteColorings.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2023 Iván Renison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Iván Renison, Yue Sun, Jiayi Huang, Nick Adfor, Aristotle AI
+Authors: Iván Renison, Yue Sun, Nick Adfor, Aristotle AI
 -/
 module
 
