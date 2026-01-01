82f8f14d3e3043aceb9b77022d62d14c36e104aa
anivegesana <anirudh.vegesana@gmail.com>
2026-01-01 18:07:13 -0500

    Remove IsUnipotent definition

--- Mathlib/RingTheory/Nilpotent/Exp.lean
+++ Mathlib/RingTheory/Nilpotent/Exp.lean
@@ -199,11 +199,10 @@ theorem exp_smul {G : Type*} [Monoid G] [MulSemiringAction G A]
     exp (g • a) = g • exp a :=
   (map_exp ha (MulSemiringAction.toRingHom G A g)).symm
 
-theorem exp_isUnipotent {a : A} (ha : IsNilpotent a) : IsUnipotent (exp a) := by
+theorem exp_sub_one_isNilpotent {a : A} (ha : IsNilpotent a) : IsNilpotent ((exp a) - 1) := by
   cases subsingleton_or_nontrivial A; · exact isNilpotent_of_subsingleton
   rw [exp, ← Nat.sub_add_cancel (pos_nilpotencyClass_iff.mpr ha), Finset.sum_range_succ',
-    Nat.factorial_zero, Nat.cast_one, inv_one, pow_zero, one_smul,
-    add_one_isUnipotent_iff_isNilpotent]
+    Nat.factorial_zero, Nat.cast_one, inv_one, pow_zero, one_smul, add_sub_cancel_right]
   exact Commute.isNilpotent_sum (by intros; apply smul; exact pow_of_pos ha (by positivity)) (by
     intros
     have h (n : ℕ) : (n.factorial : ℚ)⁻¹ ≠ 0 := by norm_num; exact Nat.factorial_ne_zero n
