5ed4d63370fb7533ebce446da10f0499f23300b4
Monica Omar <2497250a@research.gla.ac.uk>
2026-01-01 13:51:11 +0000

    update doc

--- Mathlib/Analysis/InnerProductSpace/LinearMap.lean
+++ Mathlib/Analysis/InnerProductSpace/LinearMap.lean
@@ -300,7 +300,10 @@ open ContinuousLinearMap
 variable (𝕜) in
 /-- A rank-one operator on an inner product space is given by `x ↦ y ↦ z ↦ ⟪y, z⟫ • x`.
 
-This is also sometimes referred to as an outer product of vectors on a Hilbert space. -/
+This is also sometimes referred to as an outer product of vectors on a Hilbert space.\
+This corresponds to the matrix outer product `Matrix.vecMulVec`, see
+`InnerProductSpace.toMatrix_rankOne` and `Matrix.symm_toEuclideanLin_rankOne` in
+`Mathlib/Analysis/InnerProductSpace/PiL2.lean`. -/
 noncomputable def rankOne : E →L[𝕜] F →L⋆[𝕜] F →L[𝕜] E :=
   .flip <| .comp (.smulRightL 𝕜 _ _) (innerSL 𝕜)
 
