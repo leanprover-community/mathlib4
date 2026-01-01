1328a9409e9eac5145c6c49490eb7f51bf0888c3
Monica Omar <23701951+themathqueen@users.noreply.github.com>
2026-01-01 00:31:50 +0000

    feat(Analysis/InnerProductSpace/Positive): `f.symm.IsPositive` iff `f.IsPositive` (#33352)
    ... for linear equivalence `f`.

--- Mathlib/Analysis/InnerProductSpace/Symmetric.lean
+++ Mathlib/Analysis/InnerProductSpace/Symmetric.lean
@@ -220,6 +220,12 @@ theorem isSymmetric_linearIsometryEquiv_conj_iff {F : Type*} [SeminormedAddCommG
 
 end LinearMap
 
+theorem LinearMap.IsSymmetric.toLinearMap_symm {T : E ≃ₗ[𝕜] E} (hT : T.IsSymmetric) :
+    T.symm.IsSymmetric := fun x y ↦ by simpa using hT (T.symm x) (T.symm y) |>.symm
+
+@[simp] theorem LinearEquiv.isSymmetric_symm_iff {T : E ≃ₗ[𝕜] E} :
+    T.symm.IsSymmetric ↔ T.IsSymmetric := ⟨.toLinearMap_symm, .toLinearMap_symm⟩
+
 end Seminormed
 
 section Normed
