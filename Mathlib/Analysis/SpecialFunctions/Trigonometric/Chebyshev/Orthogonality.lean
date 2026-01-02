4999da99a207dbce4323806824418c32dc7363cf
Yuval Filmus <filmus.yuval@gmail.com>
2026-01-02 12:10:03 +0200

    Formulate sample theorem

---
+++ Mathlib/Analysis/SpecialFunctions/Trigonometric/Chebyshev/Orthogonality.lean
@@ -0,0 +1,28 @@
+/-
+Copyright (c) 2026 Yuval Filmus. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Yuval Filmus
+-/
+module
+
+public import Mathlib.RingTheory.Polynomial.Chebyshev
+public import Mathlib.Data.Real.Basic
+public import Mathlib.MeasureTheory.Integral.Bochner.Basic
+public import Mathlib.MeasureTheory.Measure.Lebesgue.Basic
+--public import Mathlib.MeasureTheory.Measure.WithDensity
+import Mathlib.Analysis.SpecialFunctions.Trigonometric.Chebyshev.Basic
+
+@[expose] public section
+
+namespace Polynomial.Chebyshev
+
+open Real MeasureTheory
+
+noncomputable abbrev chebDensT (x : ℝ) : ENNReal :=
+    if x ∈ Set.Ioo (-1) 1 then ((√(1 - x ^ 2))⁻¹).toNNReal else 0
+
+theorem integral_T_mul_T_of_ne {n m : ℤ} (h : n ≠ m) :
+    ∫ x, (T ℝ n).eval x * (T ℝ m).eval x ∂volume.withDensity chebDensT = 0 := by
+  sorry
+
+end Polynomial.Chebyshev
