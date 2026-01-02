1532a265db5fe3292cfd446d5d2103aa95788cc4
Bolton Bailey <bolton.bailey@gmail.com>
2026-01-01 20:49:16 -0800

    Update Mathlib/Algebra/MvPolynomial/NoZeroDivisors.lean
    Co-authored-by: Riccardo Brasca <riccardo.brasca@gmail.com>

--- Mathlib/Algebra/MvPolynomial/NoZeroDivisors.lean
+++ Mathlib/Algebra/MvPolynomial/NoZeroDivisors.lean
@@ -46,19 +46,16 @@ lemma degreeOf_mul_eq (hp : p ≠ 0) (hq : q ≠ 0) :
 lemma degreeOf_prod_eq {ι : Type*} (s : Finset ι) (f : ι → MvPolynomial σ R)
     (h : ∀ i ∈ s, f i ≠ 0) :
     degreeOf n (∏ i ∈ s, f i) = ∑ i ∈ s, degreeOf n (f i) := by
-  by_cases nontrivial : Nontrivial (MvPolynomial σ R)
+  rcases subsingleton_or_nontrivial (MvPolynomial σ R) with nontrivial | nontrivial
+  · simp [Subsingleton.eq_zero]
   · classical
     induction s using Finset.induction_on with
     | empty => simp
     | insert a s a_not_mem ih =>
-      simp only [Finset.mem_insert, ne_eq, forall_eq_or_imp] at h
+      simp only [mem_insert, ne_eq, forall_eq_or_imp] at h
       obtain ⟨ha, hs⟩ := h
-      specialize ih hs
-      simp only [a_not_mem, not_false_eq_true, Finset.prod_insert, Finset.sum_insert]
-      rw [degreeOf_mul_eq ha (by rw [Finset.prod_ne_zero_iff]; exact hs), ih]
-  · push_neg at nontrivial
-    have (x : MvPolynomial σ R) : x = 0 := Subsingleton.eq_zero x
-    simp only [degreeOf_zero, Finset.sum_const_zero, this]
+      simp [a_not_mem, not_false_eq_true, prod_insert, sum_insert, degreeOf_mul_eq ha
+        (by rw [prod_ne_zero_iff]; exact hs), ih hs]
 
 theorem degreeOf_pow_eq (i : σ) (p : MvPolynomial σ R) (n : ℕ) (hp : p ≠ 0) :
     degreeOf i (p ^ n) = n * degreeOf i p := by
