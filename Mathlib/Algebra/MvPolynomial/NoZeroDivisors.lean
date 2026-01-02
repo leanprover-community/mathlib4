ef6fc90ea01c2965faaf92ecb4bf3b66eeee9a79
Bolton Bailey <bolton.bailey@gmail.com>
2026-01-01 20:49:43 -0800

    Update Mathlib/Algebra/MvPolynomial/NoZeroDivisors.lean
    Co-authored-by: Riccardo Brasca <riccardo.brasca@gmail.com>

--- Mathlib/Algebra/MvPolynomial/NoZeroDivisors.lean
+++ Mathlib/Algebra/MvPolynomial/NoZeroDivisors.lean
@@ -100,8 +100,7 @@ theorem dvd_C_iff_exists {f : MvPolynomial σ R}
       rw [totalDegree_eq_zero_iff_eq_C] at this
       refine ⟨?_, this⟩
       rw [this, C_dvd_iff_dvd_coeff] at hf
-      convert hf 0
-      simp
+      simpa using hf 0
     apply Nat.eq_zero_of_le_zero
     convert totalDegree_le_of_dvd_of_isDomain hf (by simp [ha])
     simp
