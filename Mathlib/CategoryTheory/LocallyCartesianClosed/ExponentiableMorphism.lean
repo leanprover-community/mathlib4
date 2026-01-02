de259e1f516dfb03735fa929f460d63c4b037ccc
Joël Riou <37772949+joelriou@users.noreply.github.com>
2026-01-02 18:27:03 +0100

    Apply suggestion from @joelriou

--- Mathlib/CategoryTheory/LocallyCartesianClosed/ExponentiableMorphism.lean
+++ Mathlib/CategoryTheory/LocallyCartesianClosed/ExponentiableMorphism.lean
@@ -80,6 +80,7 @@ theorem ev_naturality {X Y : Over I} (g : X ⟶ Y) :
     (pullback f).map ((pushforward f).map g) ≫ (ev f).app Y = (ev f).app X ≫ g :=
   ev f |>.naturality g
 
+@[reassoc]
 theorem coev_naturality {X Y : Over J} (g : X ⟶ Y) :
     g ≫ (coev f).app Y = (coev f).app X ≫ (pushforward f).map ((pullback f).map g) :=
   coev f |>.naturality g
