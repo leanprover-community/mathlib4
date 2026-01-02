2e4e51dd48560bebdd0481d72b86670286f56603
Li Xuanji <xuanji@gmail.com>
2026-01-02 19:36:18 +0000

    chore: Fix Dihedral comments (#33483)
    Fix incorrect comments

--- Mathlib/GroupTheory/SpecificGroups/Dihedral.lean
+++ Mathlib/GroupTheory/SpecificGroups/Dihedral.lean
@@ -176,7 +176,7 @@ theorem r_one_pow_n : r (1 : ZMod n) ^ n = 1 := by
 theorem sr_mul_self (i : ZMod n) : sr i * sr i = 1 := by
   simp
 
-/-- If `0 < n`, then `sr i` has order 2.
+/-- `sr i` has order 2.
 -/
 @[simp]
 theorem orderOf_sr (i : ZMod n) : orderOf (sr i) = 2 := by
@@ -184,7 +184,7 @@ theorem orderOf_sr (i : ZMod n) : orderOf (sr i) = 2 := by
   · rw [sq, sr_mul_self]
   · simp [← r_zero]
 
-/-- If `0 < n`, then `r 1` has order `n`.
+/-- `r 1` has order `n`.
 -/
 @[simp]
 theorem orderOf_r_one : orderOf (r 1 : DihedralGroup n) = n := by
@@ -203,7 +203,7 @@ theorem orderOf_r_one : orderOf (r 1 : DihedralGroup n) = n := by
     rw [← ZMod.val_eq_zero, ZMod.val_natCast, Nat.mod_eq_of_lt h] at h2
     exact absurd h2.symm (orderOf_pos _).ne
 
-/-- If `0 < n`, then `i : ZMod n` has order `n / gcd n i`.
+/-- If `0 < n`, then `r i` has order `n / gcd n i`.
 -/
 theorem orderOf_r [NeZero n] (i : ZMod n) : orderOf (r i) = n / Nat.gcd n i.val := by
   conv_lhs => rw [← ZMod.natCast_zmod_val i]
