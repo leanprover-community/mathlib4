2aeee93d17c1f281a30a9c881597c4e566ebb956
Gregory Wickham <gwickham99@gmail.com>
2026-01-02 09:38:56 -0500

    removed unnecessary definition

--- Mathlib/Analysis/CStarAlgebra/GNSConstruction/Hom.lean
+++ Mathlib/Analysis/CStarAlgebra/GNSConstruction/Hom.lean
@@ -26,8 +26,8 @@ into the algebra of bounded operators on a Hilbert space.
 open scoped ComplexOrder
 open Complex ContinuousLinearMap UniformSpace Completion
 
-variable {A : Type*} [NonUnitalCStarAlgebra A] [PartialOrder A] [StarOrderedRing A]
-variable (f : A →ₚ[ℂ] ℂ)
+variable {A : Type*} [NonUnitalCStarAlgebra A] [PartialOrder A] [StarOrderedRing A] (f : A →ₚ[ℂ] ℂ)
+
 namespace PositiveLinearMap
 
 @[simps!]
@@ -44,21 +44,15 @@ noncomputable def const_mul_GNS (a : A) : f.GNS →L[ℂ] f.GNS :=
       _ ≤ f (‖a‖ ^ 2 • star (f.ofGNS x) * f.ofGNS x) := by simpa using OrderHomClass.mono f this
       _ = _ := by simp [← Complex.coe_smul, GNS_norm_sq, smul_mul_assoc]
 
-noncomputable
-def A_mul_GNS : A →ₗ[ℂ] f.GNS →L[ℂ] f.GNS where
-  toFun a := f.const_mul_GNS a
-  map_add' _ _ := by ext b; simp [const_mul_GNS]
-  map_smul' _ _ := by ext b; simp [const_mul_GNS]
-
 @[simps!]
 noncomputable def π_ofA (a : A) : f.GNS_HilbertSpace →L[ℂ] f.GNS_HilbertSpace :=
-  mapCLM (f.A_mul_GNS a)
+  mapCLM (f.const_mul_GNS a)
 
 @[simp]
-lemma A_mul_GNS_prod_eq_comp (a b : A) :
-    f.A_mul_GNS (a * b) = f.A_mul_GNS (a) ∘ f.A_mul_GNS (b) := by
+lemma const_mul_GNS_prod_eq_comp (a b : A) :
+    f.const_mul_GNS (a * b) = f.const_mul_GNS (a) ∘ f.const_mul_GNS (b) := by
   ext c
-  simp [A_mul_GNS, const_mul_GNS, mul_assoc]
+  simp [mul_assoc]
 
 noncomputable def π : NonUnitalStarAlgHom ℂ A (f.GNS_HilbertSpace →L[ℂ] f.GNS_HilbertSpace) where
   toFun := f.π_ofA
@@ -67,36 +61,31 @@ noncomputable def π : NonUnitalStarAlgHom ℂ A (f.GNS_HilbertSpace →L[ℂ] f
     induction x using Completion.induction_on with
     | hp => apply isClosed_eq <;> fun_prop
     | ih x
-    simp [map_coe, ContinuousLinearMap.uniformContinuous, UniformContinuous.const_smul _ r]
+    simp [ContinuousLinearMap.uniformContinuous, map_coe, smul_mul_assoc]
   map_zero' := by
     ext b
-    dsimp [π_ofA]
-    simp only [map_zero]
     induction b using Completion.induction_on with
     | hp => apply isClosed_eq <;> fun_prop
     | ih b
-    dsimp [A_mul_GNS, const_mul_GNS]
     simp [map_coe, ContinuousLinearMap.uniformContinuous]
     rfl
   map_add' x y := by
     ext c
-    simp only [π_ofA, add_apply]
     induction c using Completion.induction_on with
       | hp => apply isClosed_eq <;> fun_prop
       | ih c
-    simp [map_coe, ContinuousLinearMap.uniformContinuous]
-    norm_cast
+    simp [ContinuousLinearMap.uniformContinuous, map_coe, add_mul, map_add, Completion.coe_add]
   map_mul' a b := by
     ext c
-    simp only [π_ofA, mapCLM, coe_mk', LinearMap.coe_mk, AddHom.coe_mk, ContinuousLinearMap.coe_mul,
-      Function.comp_apply]
     induction c using Completion.induction_on with
       | hp => apply isClosed_eq <;> fun_prop
       | ih c
-    simp only [A_mul_GNS_prod_eq_comp, ContinuousLinearMap.uniformContinuous, map_coe]
+    simp only [π_ofA, mapCLM, coe_mk', LinearMap.coe_mk, AddHom.coe_mk, ContinuousLinearMap.coe_mul,
+      Function.comp_apply, const_mul_GNS_prod_eq_comp, ContinuousLinearMap.uniformContinuous,
+      map_coe, const_mul_GNS_apply, ofGNS_toGNS]
     rw [map_coe]
     · simp
-    exact ContinuousLinearMap.uniformContinuous ((f.A_mul_GNS a).comp (f.A_mul_GNS b))
+    exact ContinuousLinearMap.uniformContinuous ((f.const_mul_GNS a).comp (f.const_mul_GNS b))
   map_star' a := by
     refine (eq_adjoint_iff (π_ofA f (star a)) (π_ofA f a)).mpr ?_
     intro x y
@@ -104,29 +93,28 @@ noncomputable def π : NonUnitalStarAlgHom ℂ A (f.GNS_HilbertSpace →L[ℂ] f
     | hp => apply isClosed_eq <;> fun_prop
     | ih x y
     simp only [π_ofA_apply]
-    rw [map_coe (ContinuousLinearMap.uniformContinuous (f.A_mul_GNS a)),
-      map_coe (ContinuousLinearMap.uniformContinuous (f.A_mul_GNS (star a)))]
-    simp [GNS_inner_def, A_mul_GNS, mul_assoc]
+    rw [map_coe (ContinuousLinearMap.uniformContinuous (f.const_mul_GNS a)),
+      map_coe (ContinuousLinearMap.uniformContinuous (f.const_mul_GNS (star a)))]
+    simp [GNS_inner_def, const_mul_GNS, mul_assoc]
 
-variable {A : Type*} [CStarAlgebra A] [PartialOrder A] [StarOrderedRing A]
-variable (f : A →ₚ[ℂ] ℂ)
+variable {A : Type*} [CStarAlgebra A] [PartialOrder A] [StarOrderedRing A] (f : A →ₚ[ℂ] ℂ)
 
 noncomputable def π_unital : StarAlgHom ℂ A (f.GNS_HilbertSpace →L[ℂ] f.GNS_HilbertSpace) where
   toFun := f.π
   map_one' := by
     ext b
-    dsimp [π, π_ofA]
     induction b using Completion.induction_on with
     | hp => apply isClosed_eq <;> fun_prop
     | ih b
-    rw [map_coe (ContinuousLinearMap.uniformContinuous (f.A_mul_GNS 1))]
-    simp_all [A_mul_GNS, const_mul_GNS]
+    dsimp [π, π_ofA]
+    rw [map_coe (ContinuousLinearMap.uniformContinuous (f.const_mul_GNS 1))]
+    simp [const_mul_GNS, const_mul_GNS]
   commutes' r := by
     dsimp [π]
     simp only [← RingHom.smulOneHom_eq_algebraMap, RingHom.smulOneHom_apply, π_ofA, mapCLM]
     congr
     ext c
-    simp [A_mul_GNS, const_mul_GNS]
+    simp [const_mul_GNS, const_mul_GNS]
   map_mul' := map_mul f.π
   map_zero' := map_zero f.π
   map_add' := map_add f.π
