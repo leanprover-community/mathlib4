464d7c2f4e77eb10f8d90cad8494bcdaa149c308
Gregory Wickham <gwickham99@gmail.com>
2026-01-02 09:55:29 -0500

    Combining GNS files into one file

--- Mathlib/Analysis/CStarAlgebra/GNS.lean
+++ Mathlib/Analysis/CStarAlgebra/GNS.lean
@@ -5,30 +5,95 @@ Authors: Gregory Wickham
 -/
 module
 
-public import Mathlib.Analysis.CStarAlgebra.GNSConstruction.Defs
+public import Mathlib.Analysis.CStarAlgebra.PositiveLinearMap
 public import Mathlib.Analysis.InnerProductSpace.Adjoint
+public import Mathlib.Analysis.InnerProductSpace.Completion
 
 /-!
-# The *-homomorphism of the GNS construction
+# The GNS (Gelfand-Naimark-Segal) construction
 
-In this file we define both the unital and non-unital ⋆-homomorphisms from a C⋆-algebra `A`
-into the algebra of bounded operators on a Hilbert space.
+In this file we do the "construction" part of the GNS construction. We construct a Hilbert space
+from an arbitrary C*-algebra so that it is *-isomorphic to a subalgebra of the bounded operators on
+the Hilbert space.
 
 ## Main results
 
+- `f.GNS` : a type synonym of `A` that "forgets" the norm of `A` and bundles in a fixed
+  linear functional `f` so that we can construct an inner product and inner product-induced norm.
+- `f.GNS_HilbertSpace` : the Hilbert space completion of `f.GNS`.
 - `f.π` : The non-unital *-homomorphism from a non-unital `A` into the bounded linear operators on
   `f.GNS_HilbertSpace`.
 - `f.π_unital` : The unital *-homomorphism from a unital `A` into the bounded linear operators on
   `f.GNS_HilbertSpace`.
-
 -/
+
 @[expose] public section
 open scoped ComplexOrder
 open Complex ContinuousLinearMap UniformSpace Completion
 
-variable {A : Type*} [NonUnitalCStarAlgebra A] [PartialOrder A] [StarOrderedRing A] (f : A →ₚ[ℂ] ℂ)
+variable {A : Type*} [NonUnitalCStarAlgebra A] [PartialOrder A] (f : A →ₚ[ℂ] ℂ)
 
 namespace PositiveLinearMap
+
+set_option linter.unusedVariables false in
+/-- The GNS space on a non-unital C⋆-algebra with a positive linear functional. This erases the norm
+on `A`, while remainaing structurally equivalent via the `LinearEquivalence`, `toGNS`.
+-/
+@[nolint unusedArguments]
+def GNS (f : A →ₚ[ℂ] ℂ) := A
+
+instance : AddCommGroup f.GNS := inferInstanceAs (AddCommGroup A)
+instance : Module ℂ f.GNS := inferInstanceAs (Module ℂ A)
+
+/-- The map from the C⋆-algebra to the GNS space, as a linear equivalence. -/
+def toGNS : A ≃ₗ[ℂ] f.GNS := LinearEquiv.refl ℂ _
+
+/-- The map from the GNS space to the C⋆-algebra, as a linear equivalence. -/
+def ofGNS : f.GNS ≃ₗ[ℂ] A := (f.toGNS).symm
+
+@[simp]
+lemma toGNS_ofGNS (a : f.GNS) : f.toGNS (f.ofGNS a) = a := rfl
+
+@[simp]
+lemma ofGNS_toGNS (a : A) : f.ofGNS (f.toGNS a) = a := rfl
+
+@[simp]
+lemma symm_ofGNS : (f.ofGNS).symm = f.toGNS := rfl
+
+@[simp]
+lemma symm_toGNS : (f.toGNS).symm = f.ofGNS := rfl
+
+variable [StarOrderedRing A]
+
+def preInnerProdSpace : PreInnerProductSpace.Core ℂ f.GNS where
+  inner a b := f (star (f.ofGNS a) * f.ofGNS b)
+  conj_inner_symm := by simp [← Complex.star_def, ← map_star f]
+  re_inner_nonneg _ := RCLike.nonneg_iff.mp (f.map_nonneg (star_mul_self_nonneg _)) |>.1
+  add_left _ _ _ := by rw [map_add, star_add, add_mul, map_add]
+  smul_left := by simp [smul_mul_assoc]
+
+noncomputable instance : SeminormedAddCommGroup f.GNS :=
+  InnerProductSpace.Core.toSeminormedAddCommGroup (c := f.preInnerProdSpace)
+noncomputable instance : InnerProductSpace ℂ f.GNS :=
+  InnerProductSpace.ofCore f.preInnerProdSpace
+
+lemma GNS_inner_def (a b : f.GNS) :
+    inner ℂ a b = f (star (f.ofGNS a) * f.ofGNS b) := rfl
+
+lemma GNS_norm_def (a : f.GNS) :
+    ‖a‖ = (f (star (f.ofGNS a) * f.ofGNS a)).re.sqrt := rfl
+
+lemma GNS_norm_sq (a : f.GNS) :
+    ‖a‖ ^ 2 = (f (star (f.ofGNS a) * f.ofGNS a)) := by
+  have : 0 ≤ f (star (f.ofGNS a) * f.ofGNS a) := map_nonneg f <| star_mul_self_nonneg _
+  rw [GNS_norm_def, ← ofReal_pow, Real.sq_sqrt]
+  · rw [conj_eq_iff_re.mp this.star_eq]
+  · rwa [re_nonneg_iff_nonneg this.isSelfAdjoint]
+
+/--
+The Hilbert space constructed from a positive linear functional on a C⋆-algebra.
+-/
+abbrev GNS_HilbertSpace := UniformSpace.Completion f.GNS
 
 @[simps!]
 noncomputable def GNS_op (a : A) : f.GNS →L[ℂ] f.GNS :=
@@ -48,7 +113,7 @@ noncomputable def GNS_op (a : A) : f.GNS →L[ℂ] f.GNS :=
 noncomputable def π_ofA (a : A) : f.GNS_HilbertSpace →L[ℂ] f.GNS_HilbertSpace := mapCLM (f.GNS_op a)
 
 @[simp]
-lemma const_mul_GNS_prod_eq_comp (a b : A) : f.GNS_op (a * b) = f.GNS_op (a) ∘ f.GNS_op (b) := by
+lemma GNS_op_prod_eq_comp (a b : A) : f.GNS_op (a * b) = f.GNS_op (a) ∘ f.GNS_op (b) := by
   ext c; simp [mul_assoc]
 
 noncomputable def π : NonUnitalStarAlgHom ℂ A (f.GNS_HilbertSpace →L[ℂ] f.GNS_HilbertSpace) where
