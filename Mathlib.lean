5e7ebc96ca661e3067410ef39d0decfdefee27eb
Sina Hazratpour <sinahazratpour@gmail.com>
2026-01-02 18:07:19 +0000

    feat(CategoryTheory): Exponentiable morphisms (#31332)
    We define an exponentiable morphism `f : I ⟶ J` to be a morphism with a chosen pullback functor
    `Over J ⥤ Over I` together with a right adjoint, called the pushforward functor.
    
    We use this PR in the PR branch `lccc-basic`.  
    
    ```mermaid
    graph TD
      A[ChosenPullbacksAlong.lean] --> A'[Over.lean] 
      A' --> B[Sections.lean]
      A --> C[ExponentiableMorphism.lean]
      B --> D[Basic.lean]
      C --> D
      D --> E[Types.lean]
      E --> F[Presheaves.lean]
      D --> G[Beck-Chevalley.lean]
    
      %% Define highlight style
      classDef highlight fill:#ffe599,stroke:#d4a017,stroke-width:2px;
    
      %% Apply to one node
      class C highlight;
    ```
    
    Co-authored-by: Joël Riou <37772949+joelriou@users.noreply.github.com>
    Co-authored-by: Dagur Asgeirsson <dagurtomas@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -2762,6 +2762,7 @@ public import Mathlib.CategoryTheory.Localization.StructuredArrow
 public import Mathlib.CategoryTheory.Localization.Triangulated
 public import Mathlib.CategoryTheory.Localization.Trifunctor
 public import Mathlib.CategoryTheory.LocallyCartesianClosed.ChosenPullbacksAlong
+public import Mathlib.CategoryTheory.LocallyCartesianClosed.ExponentiableMorphism
 public import Mathlib.CategoryTheory.LocallyCartesianClosed.Over
 public import Mathlib.CategoryTheory.LocallyDirected
 public import Mathlib.CategoryTheory.MarkovCategory.Basic
