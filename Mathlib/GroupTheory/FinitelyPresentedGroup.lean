572c818ff0372f192c4b37965ae4cd2d2e148293
Hang Lu Su <hanglu.su@pm.me>
2026-01-03 21:19:05 +0100

    re-ord mpr direction of `isFinitelyPresented_iff_fintype` proof

--- Mathlib/GroupTheory/FinitelyPresentedGroup.lean
+++ Mathlib/GroupTheory/FinitelyPresentedGroup.lean
@@ -71,16 +71,18 @@ lemma isFinitelyPresented_iff_fintype {G : Type*} [Group G] :
       have hf'surj := hfsurj.comp iso.surjective
       constructor
       · exact hf'surj
-      · convert IsNormalClosureFG.map iso.symm.toMonoidHom iso.symm.surjective f.ker hfkernel
-        ext x
-        simp only [Subgroup.mem_map, MonoidHom.mem_ker, f', MonoidHom.comp_apply]
-        constructor
-        · intro hx
-          use iso x, hx
-          simp only [MulEquiv.toMonoidHom_eq_coe, MonoidHom.coe_coe, MulEquiv.symm_apply_apply]
-        · intro ⟨y, hy, hyx⟩
-          rw [← hyx]
-          simpa using hy
+      · have hfker_eq : f'.ker = f.ker.map iso.symm := by
+          ext x
+          simp only [Subgroup.mem_map, MonoidHom.mem_ker, f', MonoidHom.comp_apply]
+          constructor
+          · intro hx
+            use iso x, hx
+            simp only [MonoidHom.coe_coe, MulEquiv.symm_apply_apply]
+          · intro ⟨y, hy, hyx⟩
+            rw [← hyx]
+            simpa using hy
+        rw [hfker_eq]
+        exact IsNormalClosureFG.map iso.symm.toMonoidHom iso.symm.surjective f.ker hfkernel
 
 variable (G : Type) [Group G] (g : G)
 
