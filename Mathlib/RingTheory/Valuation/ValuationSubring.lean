2b9fd2e5496f8969777dc1b6484068f73710b38e
Xavier Genereux <xaviergenereux@hotmail.com>
2026-01-05 12:57:34 -0500

    add docstring

--- Mathlib/RingTheory/Valuation/ValuationSubring.lean
+++ Mathlib/RingTheory/Valuation/ValuationSubring.lean
@@ -95,6 +95,8 @@ theorem mem_top (x : K) : x ∈ (⊤ : ValuationSubring K) :=
 theorem le_top : A ≤ ⊤ := fun _a _ha => mem_top _
 
 variable (K) in
+/-- If `K` is a field, then so is `K` viewed as a valuation subring
+of itself. (That is, `⊤ : ValuationSubring K`.) -/
 noncomputable abbrev fieldTop : Field (⊤ : ValuationSubring K) := by
   apply Field.ofIsUnitOrEqZero
   intro ⟨a, ha⟩
