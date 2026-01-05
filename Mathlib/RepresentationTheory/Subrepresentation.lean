156e362ffa0c3df70d11c4abb5294633baa3802e
Violeta Hernández <vi.hdz.p@gmail.com>
2026-01-05 14:20:06 -0600

    Update Subrepresentation.lean

--- Mathlib/RepresentationTheory/Subrepresentation.lean
+++ Mathlib/RepresentationTheory/Subrepresentation.lean
@@ -57,17 +57,14 @@ def toRepresentation (ρ' : Subrepresentation ρ) : Representation A G ρ'.toSub
 
 instance : Max (Subrepresentation ρ) where
   max ρ₁ ρ₂ := .mk (ρ₁.toSubmodule ⊔ ρ₂.toSubmodule) <| by
-      simp only [Submodule.forall_mem_sup, map_add]
-      intro g x₁ hx₁ x₂ hx₂
-      exact Submodule.mem_sup.mpr
-        ⟨ρ g x₁, ρ₁.apply_mem_toSubmodule g hx₁, ρ g x₂, ρ₂.apply_mem_toSubmodule g hx₂, rfl⟩
+    simp only [Submodule.forall_mem_sup, map_add]
+    exact fun g x₁ hx₁ x₂ hx₂ ↦ Submodule.mem_sup.mpr
+      ⟨ρ g x₁, ρ₁.apply_mem_toSubmodule g hx₁, ρ g x₂, ρ₂.apply_mem_toSubmodule g hx₂, rfl⟩
 
 instance : Min (Subrepresentation ρ) where
   min ρ₁ ρ₂ := .mk (ρ₁.toSubmodule ⊓ ρ₂.toSubmodule) <| by
-      simp only [Submodule.mem_inf, and_imp]
-      rintro g x hx₁ hx₂
-      exact ⟨ρ₁.apply_mem_toSubmodule g hx₁, ρ₂.apply_mem_toSubmodule g hx₂⟩
-
+    simp only [Submodule.mem_inf, and_imp]
+    exact fun g x hx₁ hx₂ ↦ ⟨ρ₁.apply_mem_toSubmodule g hx₁, ρ₂.apply_mem_toSubmodule g hx₂⟩
 
 @[simp, norm_cast]
 lemma coe_sup (ρ₁ ρ₂ : Subrepresentation ρ) : ↑(ρ₁ ⊔ ρ₂) = (ρ₁ : Set W) + (ρ₂ : Set W) :=
