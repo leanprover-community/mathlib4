7d20121ad6e9e56e0956e5a0ada02f65e5fa36f8
Yoh Tanimoto <hoyt@jcom.home.ne.jp>
2026-01-05 04:28:25 +0100

    Update Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -1838,6 +1838,7 @@ public import Mathlib.Analysis.InnerProductSpace.Rayleigh
 public import Mathlib.Analysis.InnerProductSpace.Semisimple
 public import Mathlib.Analysis.InnerProductSpace.Spectrum
 public import Mathlib.Analysis.InnerProductSpace.StarOrder
+public import Mathlib.Analysis.InnerProductSpace.StandardSubspace
 public import Mathlib.Analysis.InnerProductSpace.Subspace
 public import Mathlib.Analysis.InnerProductSpace.Symmetric
 public import Mathlib.Analysis.InnerProductSpace.TensorProduct
