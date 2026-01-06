b60b62c64085a3e58430cdf0503614abcf1d5865
leo <leonid.ryvkin@rub.de>
2026-01-07 00:17:43 +0100

    added module keyword and public section

--- Mathlib/Algebra/LieRinehartAlgebra/Defs.lean
+++ Mathlib/Algebra/LieRinehartAlgebra/Defs.lean
@@ -3,7 +3,7 @@ Copyright (c) 2025 Sven Holtrop and Leonid Ryvkin. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Sven Holtrop, Leonid Ryvkin
 -/
-
+module
 
 import Mathlib.RingTheory.Derivation.Lie
 
