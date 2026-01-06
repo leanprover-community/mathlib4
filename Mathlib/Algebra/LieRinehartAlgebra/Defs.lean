e65c6e2eda03e3c4944c7ef53176e5385eb7014b
leo <leonid.ryvkin@rub.de>
2026-01-06 23:21:21 +0100

    simplified the exposition in the file

--- Mathlib/Algebra/LieRinehartAlgebra/Defs.lean
+++ Mathlib/Algebra/LieRinehartAlgebra/Defs.lean
@@ -9,18 +9,21 @@ import Mathlib.RingTheory.Derivation.Lie
 
 /-!
 # Lie Rinehart algebras
-This file defines Lie-Rinehart algebras and their morphisms.
+This file defines Lie-Rinehart algebras and their morphisms. It also shows that the derivations of
+a commutative algebra over a commutative Ring form such a Lie-Rinehart algebra.
 Lie Rinehart algebras appear in differential geometry as section spaces of Lie algebroids and
 singular foliations. The typical Cartan calculus of differential geometry can be restated fully in
 terms of the Chevalley-Eilenberg algebra of a Lie-Rinehart algebra.
 
 ## References
 
-*
+* Rinehart, G. S., Differential forms on general commutative algebras. Zbl 0113.26204
+Trans. Am. Math. Soc. 108, 195-222 (1963).
 
 ## Tags
 
 Lie-Rinehart algebra
+Derivation
 -/
 
 
@@ -51,33 +54,26 @@ variable {A : Type*} [CommRing A] [Algebra R A]
 
 instance instDerLieRingModule : LieRingModule (Derivation R A A) A where
 bracket := fun X a ↦ X ( a )
-add_lie := by simp only [Derivation.coe_add, Pi.add_apply, implies_true]
-lie_add := by simp only [map_add, implies_true]
-leibniz_lie := by exact fun x y m ↦ Eq.symm (add_eq_of_eq_sub rfl)
+add_lie := by simp
+lie_add := by simp
+leibniz_lie := fun _ _ _ ↦ Eq.symm (add_eq_of_eq_sub rfl)
 
 instance : LieModule R (Derivation R A A) A where
-smul_lie := by exact fun t x m ↦ rfl
-lie_smul := by intros r X a; exact X.map_smul_of_tower r a
+smul_lie := fun _ _ _ ↦ rfl
+lie_smul := fun _ _ _ ↦ Derivation.map_smul_of_tower _ _ _
 
 @[simp]
-lemma bracketmul (X : Derivation R A A) (a : A) : ⁅ X ,a ⁆ = X a := by rfl
+lemma bracketmul (X : Derivation R A A) (a : A) : ⁅ X ,a ⁆ = X a := rfl
 
 
-/-- The derivations of an Algebra form a LieRinehartAlgebra themselves
+/-- The derivations of a commutative Algebra themselves form a LieRinehart-Algebra
 -/
 instance : (LieRinehartAlgebra R A (Derivation R A A)) := {
-  left_linearity := by exact fun a b x ↦ rfl
-  leibnizA := by
-    intros X a m
-    simp only [smul_eq_mul, bracketmul, Derivation.leibniz, add_right_inj]
-    exact CommMonoid.mul_comm m (X a)
-  leibnizL := by
-    intros X a Y
+  left_linearity := fun _ _ _ ↦ rfl
+  leibnizA := by simp [CommMonoid.mul_comm]
+  leibnizL := fun _ _ _ ↦ by
     ext b
-    simp only [bracketmul, Derivation.coe_add, Derivation.coe_smul, Pi.add_apply, Pi.smul_apply,
-      smul_eq_mul]
-    repeat rw [Derivation.commutator_apply]
-    simp only [Derivation.coe_smul, Pi.smul_apply, smul_eq_mul, Derivation.leibniz]
+    simp [Derivation.commutator_apply]
     ring
 }
 
@@ -123,12 +119,11 @@ notation:25 L " →ₗ⁅" σ:25 "⁆ " L':0 => LieRinehartAlgebra.Hom σ L L'
 instance : CoeFun (L →ₗ⁅σ⁆ L') (fun _ => L → L') := ⟨fun f => f.toLinearMap⟩
 
 @[simp]
-lemma lem_map_lie (f : L →ₗ⁅σ⁆ L') (x y : L) : f (⁅x,y⁆) = ⁅ f (x), f (y) ⁆ :=
-by exact f.map_lie' x y
+lemma lem_map_lie (f : L →ₗ⁅σ⁆ L') (x y : L) : f (⁅x,y⁆) = ⁅ f (x), f (y) ⁆ := f.map_lie' x y
 
 @[simp]
 lemma lem_anchorcomp (f : L →ₗ⁅σ⁆ L') (l : L) (a : A ): σ (⁅l, a⁆)  =  ⁅f (l), σ (a)⁆ :=
-by exact f.anchorcomp a l
+  f.anchorcomp a l
 
 
 /-- Recovers the Lie algebra morphism underlying a Lie-Rinehart algbera homomorophism
@@ -136,13 +131,7 @@ by exact f.anchorcomp a l
 def Hom.toLieHom (f : L →ₗ⁅σ⁆ L') : L →ₗ⁅R⁆ L' := {
   toFun := f.toFun
   map_add' := f.map_add'
-  map_smul' := by
-    intros r l
-    simp only [AlgHom.toRingHom_eq_coe, AddHom.toFun_eq_coe, LinearMap.coe_toAddHom,
-      RingHom.id_apply]
-    rw [←(IsScalarTower.algebraMap_smul A r l)]
-    rw [f.map_smulₛₗ]
-    simp only [AlgHom.toRingHom_eq_coe, RingHom.coe_coe, AlgHom.commutes, algebraMap_smul]
+  map_smul' := fun _ _ ↦ by simp [←IsScalarTower.algebraMap_smul A]
   map_lie' := by apply f.map_lie'
 }
 
@@ -150,7 +139,7 @@ def Hom.toLieHom (f : L →ₗ⁅σ⁆ L') : L →ₗ⁅R⁆ L' := {
 are the same function
 -/
 @[simp]
-theorem ModHomeqLieHom (f : L →ₗ⁅σ⁆ L') (x : L) : f.toLinearMap x = (f.toLieHom) x := by rfl
+lemma ModHomeqLieHom (f : L →ₗ⁅σ⁆ L') (x : L) : f.toLinearMap x = (f.toLieHom) x := rfl
 
 
 /-- The composition of Lie Rinehart algebra homomorphisms is again a homomorphism
@@ -160,12 +149,13 @@ def comp (f : L →ₗ⁅σ⁆ L') (g : L' →ₗ⁅σ'⁆ L'') : L →ₗ⁅(Al
     toLinearMap := by
       haveI h: RingHomCompTriple σ.toRingHom σ'.toRingHom (σ'.comp σ).toRingHom := ⟨rfl⟩
       exact g.toLinearMap.comp f.toLinearMap
-    map_lie' := by
-      intros x y
-      dsimp only [AlgHom.toRingHom_eq_coe, LinearMap.coe_comp, Function.comp_apply]
+    map_lie' := fun _ _ ↦ by
+      dsimp
       repeat rw [ModHomeqLieHom]
-      simp only [LieHom.map_lie]
-    anchorcomp := by intros l a; dsimp; repeat rw [←lem_anchorcomp]
+      simp
+    anchorcomp := fun _ _ ↦ by
+      dsimp
+      repeat rw [←lem_anchorcomp]
     }
 
 
@@ -174,12 +164,9 @@ variable (R) in
 Later this will become simply `ρ(a)`
 -/
 abbrev derivOf (x : L) : Derivation R A A := Derivation.mk' ((LieModule.toEnd R L A) x)
-  (by
-    intros a b;
-    dsimp only [LieModule.toEnd_apply_apply, smul_eq_mul]
-    repeat rw [←smul_eq_mul]
-    rw [LieRinehartAlgebra.leibnizA R x a b]
-    simp only [smul_eq_mul, add_right_inj]
+  (fun a b ↦ by
+    simp only [← smul_eq_mul, LieModule.toEnd_apply_apply, LieRinehartAlgebra.leibnizA R,
+      add_right_inj]
     exact CommMonoid.mul_comm ⁅x, a⁆ b
   )
 
@@ -192,27 +179,9 @@ def anchor [LieRing L] [Module A L] [LieAlgebra R L] [IsScalarTower R A L]
 [LieRingModule L A] [LieModule R L A] [LieRinehartAlgebra R A L] : L →ₗ⁅AlgHom.id R A⁆
 (Derivation R A A) := {
   toFun := derivOf R
-  map_add' := by
-    intros x y
-    ext a
-    simp only [Derivation.coe_mk', map_add, LinearMap.add_apply, LieModule.toEnd_apply_apply,
-      Derivation.coe_add, Pi.add_apply]
-  map_smul' := by
-    intros a X
-    ext b
-    simp only [AlgHom.toRingHom_eq_coe, AlgHom.id_toRingHom, RingHom.id_apply,
-      Derivation.coe_smul, Pi.smul_apply, smul_eq_mul]
-    apply LieRinehartAlgebra.left_linearity R
-  map_lie' := by
-    intros x y
-    ext a
-    simp only [AlgHom.toRingHom_eq_coe, AlgHom.id_toRingHom, LinearMap.coe_mk, AddHom.coe_mk,
-      Derivation.coe_mk', LieHom.map_lie, LieHom.lie_apply, LieModule.toEnd_apply_apply,
-      Module.End.lie_apply]
-    rw [Derivation.commutator_apply]
-    dsimp only [Derivation.coe_mk', LieModule.toEnd_apply_apply]
-  anchorcomp := by simp only [AlgHom.coe_id, id_eq, AlgHom.toRingHom_eq_coe, AlgHom.id_toRingHom,
-    LinearMap.coe_mk, AddHom.coe_mk, bracketmul, Derivation.coe_mk', LieModule.toEnd_apply_apply,
-    implies_true]
+  map_add' := fun _ _ ↦by ext a; simp
+  map_smul' := fun _ _ ↦ by ext a; simp [LieRinehartAlgebra.left_linearity R]
+  map_lie' := fun _ _ ↦ by ext a; simp [Derivation.commutator_apply]
+  anchorcomp := by simp
 }
 end LieRinehartAlgebra
