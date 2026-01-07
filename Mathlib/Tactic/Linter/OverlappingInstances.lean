a2594a12dd26bc242f2488a8cf9a23340ad8c356
thorimur <68410468+thorimur@users.noreply.github.com>
2026-01-06 23:26:36 -0500

    refactor: return data from `findOverlappingDataInstances`, remove env linter

--- Mathlib/Tactic/Linter/OverlappingInstances.lean
+++ Mathlib/Tactic/Linter/OverlappingInstances.lean
@@ -39,34 +39,36 @@ private partial def getStructureDataProjections (e : Expr) (acc : Array Expr := 
     else
       return acc
 
-/-- Run the overlapping instances linter on `declName`. -/
-def findDuplicateDataInstances (declName : Name) : CoreM (Option MessageData) := MetaM.run' do
-  Core.checkSystem "overlappingInstances"
-  forallTelescope (← getConstInfo declName).type fun _ _ ↦ do
-  let mut result := none
-  let mut insts : Std.HashMap Expr Expr := {}
-  for { fvar, .. } in ← getLocalInstances do
-    unless (← fvar.fvarId!.getBinderInfo).isInstImplicit do continue
-    let projClasses ← forallTelescope (← inferType fvar) fun xs _ ↦ do
-      (← getStructureDataProjections (mkAppN fvar xs) |>.run' {}).mapM (mkForallFVars xs)
+/-- An overlap between two local instances. This is introduced for readability, as all fields are
+`Expr`s. -/
+structure Overlap where
+  /-- A local instance free variable whose data-carrying projections overlap with `fvar₂`. -/
+  fvar₁ : Expr × Bool
+  /-- A local instance free variable whose data-carrying projections overlap with `fvar₁`. -/
+  fvar₂ : Expr × Bool
+  /-- A type class on which `fvar₁` and `fvar₂`'s data-carrying projections overlap. -/
+  overlap : Expr
 
-    for cls in projClasses do
-      if let some fvar' := insts[cls]? then
-        let type ← inferType fvar; let type' ← inferType fvar'
-        if type == type' then
-          result := m!"{result.getD m!""}There are multiple different instances of `{type}`\n\n"
-          break
-        else
-          result := m!"{result.getD m!""}`{type}` and `{type'}` both imply `{cls}`\n\n"
+/-- Find data-carrying overlaps between the current local instances of the `MetaM` context. -/
+def findOverlappingDataInstances : MetaM (Array Overlap) := do
+  let mut overlaps : Array Overlap := #[]
+  /- The `Bool` indicates whether the given class key is exactly the type of the associated `fvar`
+  value. This is used for error reporting. -/
+  let mut insts : Std.HashMap Expr (Expr × Bool) := {}
+  for { fvar := fvar₁, .. } in ← getLocalInstances do
+    unless (← fvar₁.fvarId!.getBinderInfo).isInstImplicit do continue
+    let projClasses ← forallTelescope (← inferType fvar₁) fun xs _ ↦ do
+      (← getStructureDataProjections (mkAppN fvar₁ xs) |>.run' {}).mapM (mkForallFVars xs)
+    for h : clsIdx in 0...projClasses.size do
+      let cls := projClasses[clsIdx]
+      if let some (fvar₂, isTypeOfFVar₂) := insts[cls]? then
+        overlaps := overlaps.push {
+            fvar₁ := (fvar₁, clsIdx = 0)
+            fvar₂ := (fvar₂, isTypeOfFVar₂)
+            overlap := cls }
+        if clsIdx = 0 && isTypeOfFVar₂ then
+          break -- Don't consider further projections of this local instance
       else
-        insts := insts.insert cls fvar
-  result.mapM addMessageContextFull
+        insts := insts.insert cls (fvar₁, clsIdx = 0)
+  return overlaps
 
-/-- A linter for declarations which have instance hypotheses that have overlapping data. -/
-@[env_linter] def overlappingInstances : Lint.Linter where
-  noErrorsFound := "No declarations have overlapping instance arguments"
-  errorsFound := "Some declarations have overlapping instance arguments"
-  test := fun declName => do
-    if declName.isInternal then return none
-    if congrKindsExt.contains (← getEnv) declName then return none
-    findDuplicateDataInstances declName
