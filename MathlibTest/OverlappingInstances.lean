e4cc55efb7463576446dc419ad4b8d3f6efff8ea
thorimur <68410468+thorimur@users.noreply.github.com>
2026-01-07 00:10:20 -0500

    test: overlapping instances (first pass; all pass, but not comprehensive)

---
+++ MathlibTest/OverlappingInstances.lean
@@ -0,0 +1,87 @@
+module
+
+import Mathlib.Tactic.Linter.OverlappingInstances
+
+set_option linter.overlappingInstances true
+
+namespace Lean
+
+class Bar (α : Type) where
+  a : α
+
+class Baz (β : Type) where
+  b : β
+
+class Baq (β : Type) where
+  b : β
+
+class Foo (α) (β) extends Bar α, Baz β
+
+
+class Foo' (α) (β) extends Bar α, Baq β
+
+/--
+error: unsolved goals
+inst✝¹ inst✝ : Add Nat
+⊢ [Add Nat] → [Add Nat] → Bool
+---
+warning: The declaration `foo` has instance hypotheses that overlap on data-carrying components.
+
+There are 4 instances of `[Add Nat]`.
+-/
+#guard_msgs in
+def foo [Add Nat] [Add Nat] : [Add Nat] → [Add Nat] → Bool := by
+  skip
+
+
+/--
+warning: The declaration `foo'` has instance hypotheses that overlap on data-carrying components.
+
+`[Bar Nat]` is provided by `[Foo' Nat String]` and `[Foo Nat Bool]`.
+-/
+#guard_msgs in
+def foo' [Foo Nat Bool] [Foo' Nat String] : Bool := by
+  exact true
+
+
+/--
+warning: The declaration `foo''` has instance hypotheses that overlap on data-carrying components.
+
+`[Bar Nat]` is provided by `[Foo Nat Bool]` and `[Foo' Nat String]`.
+
+There are 2 instances of `[Foo Nat Bool]`.
+-/
+#guard_msgs in
+def foo'' [Foo Nat Bool] [Foo Nat Bool] [Foo' Nat String] : Bool := true
+
+/--
+warning: The declaration `foo'''` has instance hypotheses that overlap on data-carrying components.
+
+There are 2 instances of `[Foo Nat Bool]`.
+-/
+#guard_msgs in
+def foo''' [Foo Nat Bool] [Foo Nat Bool] : Bool := true
+
+/--
+error: Failed to infer type of definition `foo''''`
+---
+warning: The declaration `foo''''` has instance hypotheses that overlap on data-carrying components.
+
+There is an instance of `[Bar Nat]` in the local context, but it is also provided by `[Foo Nat Bool]`.
+
+There are 2 instances of `[Foo Nat Bool]`.
+-/
+#guard_msgs in
+def foo'''' [Foo Nat Bool] [Foo Nat Bool] [Bar Nat] := sorry
+
+-- Correct? Might not have `Bar Nat` if we can't provide `α`, but might if we can.
+-- Only needs `(usedOnly := true)` in `mkForallFVars` to change behavior.
+/--
+error: unsolved goals
+inst✝¹ : Foo Nat Bool
+inst✝ : (α : Type) → Foo' Nat α
+⊢ Bool
+-/
+#guard_msgs in
+def fooForall [Foo Nat Bool] [∀ α, Foo' Nat α] : Bool := by
+  skip
