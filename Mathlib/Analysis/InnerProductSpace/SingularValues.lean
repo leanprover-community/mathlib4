6bb2ded89cd2265c82389be73265c19fe4d87cd9
Niels Voss <code@nielsvoss.com>
2026-01-07 12:20:23 -0800

    Fix more indentation in docstring for LinearMap.singularValues
    Co-authored-by: Jireh Loreaux <loreaujy@gmail.com>

--- Mathlib/Analysis/InnerProductSpace/SingularValues.lean
+++ Mathlib/Analysis/InnerProductSpace/SingularValues.lean
@@ -56,12 +56,12 @@ We take the last approach for the following reasons:
 converting between any two of the other definitions is more inconvenient because it involves
 multiple `Fin` types.
 - If you prefer a definition where there are `k` singular values, you can treat the singular values
-after `k` as junk values.
-Not having to prove that `i < k` when getting the `i`th singular value has similar advantages to
-not having to prove that `y ≠ 0` when calculating `x / y`.
+  after `k` as junk values.
+  Not having to prove that `i < k` when getting the `i`th singular value has similar advantages to
+  not having to prove that `y ≠ 0` when calculating `x / y`.
 - This API coincides with a potential future API for approximation numbers, which are a
-generalization of singular values to continuous linear maps between possibly-infinite-dimensional
-normed vector spaces.
+  generalization of singular values to continuous linear maps between possibly-infinite-dimensional
+  normed vector spaces.
 -/
 public noncomputable def singularValues : ℕ →₀ ℝ≥0 :=
   Finsupp.embDomain Fin.valEmbedding <|
