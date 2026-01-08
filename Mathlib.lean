fe772cb42eb9726dc1956d653ae391184b9499eb
Sebi-Kumar <140582760+Sebi-Kumar@users.noreply.github.com>
2026-01-08 11:26:11 +0000

    feat(Topology): add definition for subpaths (#27261)
    Define subpaths as restrictions of paths to subintervals, reparameterized to have domain
    `[0, 1]` and possibly with a reverse of direction. Prove their basic properties.
    
    This serves as an alternative to `Path.truncate` which is useful for the explicit construction of certain homotopies, in particular regarding the concatenation of subpaths.
    
    Co-authored-by: Johan Commelin <johan@commelin.net>

--- Mathlib.lean
+++ Mathlib.lean
@@ -14587,6 +14587,7 @@ public import Mathlib.Topology.Specialization
 public import Mathlib.Topology.Spectral.Basic
 public import Mathlib.Topology.Spectral.Hom
 public import Mathlib.Topology.Spectral.Prespectral
+public import Mathlib.Topology.Subpath
 public import Mathlib.Topology.TietzeExtension
 public import Mathlib.Topology.Ultrafilter
 public import Mathlib.Topology.UniformSpace.AbsoluteValue
