9cb47c9e1ec9e9e169f175525dd73766633dcfc1
Andrew Yang <the.erd.one@gmail.com>
2026-01-08 02:35:50 +0000

    address comments

--- Mathlib/RingTheory/Smooth/Field.lean
+++ Mathlib/RingTheory/Smooth/Field.lean
@@ -22,39 +22,39 @@ In particular finitely generated field extensions over perfect fields are smooth
 variable {K L ι : Type*} [Field L] [Field K] [Algebra K L]
 
 open scoped IntermediateField.algebraAdjoinAdjoin in
-lemma Algebra.FormallySmooth.adjoin_of_isTranscendenceBasis {v : ι → L}
-    (hb : IsTranscendenceBasis K v) :
+lemma Algebra.FormallySmooth.adjoin_of_algebraicIndependent {v : ι → L}
+    (hb : AlgebraicIndependent K v) :
     Algebra.FormallySmooth K (IntermediateField.adjoin K (Set.range v)) := by
   have : Algebra.FormallySmooth K (adjoin K (Set.range v)) :=
-    .of_equiv hb.1.aevalEquiv
+    .of_equiv hb.aevalEquiv
   have : Algebra.FormallySmooth (adjoin K (Set.range v))
       (IntermediateField.adjoin K (Set.range v)) :=
     .of_isLocalization (nonZeroDivisors _)
   exact .comp _ (adjoin K (Set.range v)) _
 
 /-- Purely transcendental extensions are formally smooth. -/
-lemma Algebra.FormallySmooth.of_isTranscendenceBasis {v : ι → L}
-    (hb : IsTranscendenceBasis K v) (hb' : IntermediateField.adjoin K (Set.range v) = ⊤) :
+lemma Algebra.FormallySmooth.of_algebraicIndependent {v : ι → L}
+    (hb : AlgebraicIndependent K v) (hb' : IntermediateField.adjoin K (Set.range v) = ⊤) :
     Algebra.FormallySmooth K L := by
-  have := Algebra.FormallySmooth.adjoin_of_isTranscendenceBasis hb
+  have := Algebra.FormallySmooth.adjoin_of_algebraicIndependent hb
   rw [hb'] at this
   exact .of_equiv IntermediateField.topEquiv
 
 /-- Separably generated extensions are formally smooth. -/
-lemma Algebra.FormallySmooth.of_isTranscendenceBasis_of_isSeparable [EssFiniteType K L]
-    {v : ι → L} (hb : IsTranscendenceBasis K v)
+lemma Algebra.FormallySmooth.of_algebraicIndependent_of_isSeparable [EssFiniteType K L]
+    {v : ι → L} (hb : AlgebraicIndependent K v)
     [Algebra.IsSeparable (IntermediateField.adjoin K (Set.range v)) L] :
     Algebra.FormallySmooth K L := by
-  have := Algebra.FormallySmooth.adjoin_of_isTranscendenceBasis hb
+  have := Algebra.FormallySmooth.adjoin_of_algebraicIndependent hb
   have : EssFiniteType (IntermediateField.adjoin K (Set.range v)) L :=
     .of_comp K _ _
   have : FormallyEtale (IntermediateField.adjoin K (Set.range v)) L :=
     (FormallyEtale.iff_isSeparable _ _).mpr inferInstance
   exact .comp _ (IntermediateField.adjoin K (Set.range v)) _
 
-lemma Algebra.FormallySmooth.of_perfectField [PerfectField K] [Algebra.EssFiniteType K L] :
-    Algebra.FormallySmooth K L := by
+instance (priority := low) Algebra.FormallySmooth.of_perfectField
+    [PerfectField K] [Algebra.EssFiniteType K L] : Algebra.FormallySmooth K L := by
   obtain ⟨s, hs, H⟩ := exists_isTranscendenceBasis_and_isSeparable_of_perfectField K L
   have : Algebra.IsSeparable (↥(IntermediateField.adjoin K (Set.range ((↑) : s → L)))) L := by
     convert H <;> simp
-  refine .of_isTranscendenceBasis_of_isSeparable hs
+  exact .of_algebraicIndependent_of_isSeparable hs.1
