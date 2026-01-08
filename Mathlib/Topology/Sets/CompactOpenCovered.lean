6366f80ab6b8e2da84ad15ab56145f84d37a81b2
Christian Merten <christian@merten.dev>
2026-01-08 14:46:58 +0100

    golf by andrew

--- Mathlib/Topology/Sets/CompactOpenCovered.lean
+++ Mathlib/Topology/Sets/CompactOpenCovered.lean
@@ -85,27 +85,25 @@ lemma iff_isCompactOpenCovered_sigmaMk :
     refine ⟨s, hs, fun i hi ↦ ⟨t i, this i hi⟩, fun i _ ↦ hc i, ?_⟩
     simp_rw [coe_mk, ← heq, ← heq', Set.image_sigma_eq_iUnion, Function.comp_apply]
 
-lemma of_iUnion_eq_of_finite (s : Set (Set S)) (hs : ⋃ t ∈ s, t = U) (hf : s.Finite)
-    (H : ∀ t ∈ s, IsCompactOpenCovered f t) : IsCompactOpenCovered f U := by
+lemma of_iUnion_eq_of_finite {κ : Type*} [Finite κ] (s : κ → Set S) (hs : ⋃ i, s i = U)
+    (H : ∀ i, IsCompactOpenCovered f (s i)) : IsCompactOpenCovered f U := by
   rw [iff_isCompactOpenCovered_sigmaMk, iff_of_unique]
-  have (t) (h : t ∈ s) : ∃ (V : Opens (Σ i, X i)),
-      IsCompact V.1 ∧ (fun p ↦ f p.fst p.snd) '' V.carrier = t := by
-    have := H t h
-    rwa [iff_isCompactOpenCovered_sigmaMk, iff_of_unique] at this
+  have (i : κ) : ∃ (V : Opens (Σ i, X i)), IsCompact V.1 ∧ (f _ ·.snd) '' V.1 = s i := by
+    convert H i; rw [iff_isCompactOpenCovered_sigmaMk, iff_of_unique]
   choose V hVeq hVc using this
-  refine ⟨⨆ (t : s), V t t.2, ?_, ?_⟩
-  · simp only [Opens.iSup_mk, Opens.carrier_eq_coe, Opens.coe_mk]
-    have : Finite s := hf
-    exact isCompact_iUnion (fun _ ↦ hVeq _ _)
-  · simp [Set.image_iUnion, ← hs]
-    simp_all
+  exact ⟨⨆ i, V i, by simpa using isCompact_iUnion hVeq, by simp_all [Set.image_iUnion, ← hs]⟩
 
-lemma of_iUnion_eq_of_isCompact [TopologicalSpace S] {U : Set S} (hU : IsCompact U)
+lemma of_biUnion_eq_of_finite (s : Set (Set S)) (hs : ⋃ t ∈ s, t = U) (hf : s.Finite)
+    (H : ∀ t ∈ s, IsCompactOpenCovered f t) : IsCompactOpenCovered f U := by
+  have := hf.to_subtype
+  exact of_iUnion_eq_of_finite (fun i : s ↦ i.1) (by simpa) (by simpa)
+
+lemma of_biUnion_eq_of_isCompact [TopologicalSpace S] {U : Set S} (hU : IsCompact U)
     (s : Set (Opens S)) (hs : ⋃ t ∈ s, t = U) (H : ∀ t ∈ s, IsCompactOpenCovered f t) :
     IsCompactOpenCovered f U := by
   classical
   obtain ⟨t, ht⟩ := hU.elim_finite_subcover (fun V : s ↦ V.1) (fun V ↦ V.1.2) (by simp [← hs])
-  refine of_iUnion_eq_of_finite (SetLike.coe '' (t.image Subtype.val : Set (Opens S))) ?_ ?_ ?_
+  refine of_biUnion_eq_of_finite (SetLike.coe '' (t.image Subtype.val : Set (Opens S))) ?_ ?_ ?_
   · exact subset_antisymm (fun x h ↦ by aesop) (subset_trans ht <| by simp)
   · exact Set.toFinite _
   · grind
@@ -114,38 +112,22 @@ lemma of_isCompact_of_forall_exists [TopologicalSpace S] {U : Set S} (hU : IsCom
     (H : ∀ x ∈ U, ∃ t ⊆ U, x ∈ t ∧ IsOpen t ∧ IsCompactOpenCovered f t) :
     IsCompactOpenCovered f U := by
   choose Us hU' hUx hUo hU'' using H
-  refine of_iUnion_eq_of_isCompact hU { Us x h | (x : S) (h : x ∈ U) } ?_ ?_
+  refine of_biUnion_eq_of_isCompact hU { Us x h | (x : S) (h : x ∈ U) } ?_ ?_
   · refine subset_antisymm (fun x ↦ ?_) fun x hx ↦ ?_
-    · simp only [Set.mem_setOf_eq, Set.iUnion_exists, Set.mem_iUnion, SetLike.mem_coe, exists_prop,
-        exists_and_right, forall_exists_index, and_imp]
-      intro V y hy heq hyV
-      apply hU' y hy
-      rwa [heq]
+    · simp [Opens.«forall»]
+      aesop
     · simpa using ⟨⟨Us x hx, hUo _ _⟩, ⟨x, by simpa⟩, hUx _ _⟩
-  · simp only [Set.mem_setOf_eq, forall_exists_index]
-    intro t x hx heq
-    rw [← heq]
-    exact hU'' _ _
+  · simp [Opens.«forall»]
+    grind
+
+lemma image {i : ι} (V : Opens (X i)) (hV : IsCompact (X := X i) V) :
+    IsCompactOpenCovered f (f i '' V) := by
+  refine ⟨{i}, Set.finite_singleton i, fun j hj ↦ hj ▸ V, by rintro i rfl; simpa, by simp⟩
 
 lemma of_finite {U : Set S} {κ : Type*} [Finite κ] (a : κ → ι) (V : ∀ k, Opens (X (a k)))
     (hV : ∀ k, IsCompact (V k).1) (hU : ⋃ k, f (a k) '' V k = U) :
-    IsCompactOpenCovered f U := by
-  refine ⟨Set.range a, Set.finite_range a, fun i hi ↦ ⨆ (k : κ) (h : i = a k), h ▸ V k,
-      fun i hi ↦ ?_, ?_⟩
-  · simp only [iSup_mk, carrier_eq_coe, coe_mk]
-    refine isCompact_iUnion fun i ↦ isCompact_iUnion ?_
-    rintro rfl
-    exact hV _
-  · simp only [Set.mem_range, iSup_mk, carrier_eq_coe, coe_mk, Set.iUnion_exists,
-      Set.iUnion_iUnion_eq', ← hU]
-    ext x
-    simp_rw [Set.image_iUnion, Set.mem_iUnion]
-    refine ⟨fun ⟨i, j, h, y, hy, hyx⟩ ↦ ⟨j, h ▸ y, ?_, ?_⟩, fun ⟨m, y, h, e⟩ ↦ ⟨m, m, rfl, y, h, e⟩⟩
-    · simp only [SetLike.mem_coe] at hy ⊢
-      convert hy using 1 <;> try rw [h]
-      repeat simp
-    · rw [← hyx]
-      congr 1 <;> simp [h]
+    IsCompactOpenCovered f U :=
+  of_iUnion_eq_of_finite _ hU (fun _ ↦ .image _ (hV _))
 
 /-- If `U` is compact-open covered and the `X i` have a basis of compact opens,
 `U` can be written as the union of images of elements of the basis. -/
