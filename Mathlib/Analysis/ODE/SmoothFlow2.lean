488c53ac2a0230237388ca0f90630fcace8f681f
Winston Yin <winstonyin@gmail.com>
2026-01-08 12:42:57 -0500

    refactor

---
+++ Mathlib/Analysis/ODE/SmoothFlow2.lean
@@ -0,0 +1,33 @@
+/-
+Copyright (c) 2025 Winston Yin. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Winston Yin
+-/
+module
+
+public import Mathlib.Analysis.ODE.PicardLindelof
+public import Mathlib.Analysis.Calculus.ImplicitContDiff
+
+/-!
+# Smooth dependence on initial condition
+
+We prove that the solution of a $C^n$ vector field has $C^n$ dependence on the initial condition.
+
+## Main definitions and results
+
+
+
+## Implementation notes
+
+
+
+## Tags
+
+differential equation, dynamical system, initial value problem
+
+-/
+
+@[expose] public section
+
+open Function intervalIntegral MeasureTheory Metric Set
+open scoped Nat NNReal Topology
