311d7fe145e903249d1c47f9c9afcfb916c8ef9e
Yury G. Kudryashov <188813+urkud@users.noreply.github.com>
2026-01-08 17:39:12 +0000

    feat(DifferentialForm): exterior derivative applied to vector fields (#30886)
    Prove a formula for evaluation of the exterior derivative of a differential form on a tuple of vector fields,
    see https://en.wikipedia.org/wiki/Exterior_derivative#In_terms_of_invariant_formula
    
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1589,6 +1589,7 @@ public import Mathlib.Analysis.Calculus.Deriv.ZPow
 public import Mathlib.Analysis.Calculus.DerivativeTest
 public import Mathlib.Analysis.Calculus.DiffContOnCl
 public import Mathlib.Analysis.Calculus.DifferentialForm.Basic
+public import Mathlib.Analysis.Calculus.DifferentialForm.VectorField
 public import Mathlib.Analysis.Calculus.FDeriv.Add
 public import Mathlib.Analysis.Calculus.FDeriv.Affine
 public import Mathlib.Analysis.Calculus.FDeriv.Analytic
