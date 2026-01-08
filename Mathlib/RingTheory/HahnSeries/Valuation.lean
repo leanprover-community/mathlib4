a566c06e4b6f7f7a2fcd5bd5e3430d4dedd338bf
Yaël Dillies <yael.dillies@gmail.com>
2026-01-08 11:26:13 +0000

    refactor: make `LinearOrderedCommMonoidWithZero`s cancellative (#31749)
    It appears that the only interesting `LinearOrderedCommMonoidWithZero`s are the ones for which every non-zero element is regular. This PR therefore strenghtens `LinearOrderedCommMonoidWithZero`, and similarly for `LinearOrderedAddCommMonoidWithTop`, its additive counterpart.
    
    Here are the instances we lose:
    * `ENNReal`: multiplication by `\top` isn't injective
    * `ERealᵒᵈ`: multiplication by `\top` isn't injective
    * `Cardinal`: multiplication by anything but 1 isn't injective
    * `ArchimedeanClass R` where `R` is a non-strict ordered ring
    * the pathological counterexample of a `LinearOrderedCommMonoidWithZero` where two positive numbers multiply to zero
    
    I believe these are all fine to lose since they had no mathematical motivation (we never use those as targets of valuations).
    
    There is an argument to be made that we could keep the existing typeclass as is and instead have the one I'm currently replacing it with under the name `LinearOrderedCancelCommMonoidWithZero`. There are at least two reasons I do not want to do this:
    1. We have no evidence that the weaker typeclass is actually useful
    2. `LinearOrderedCommMonoidWithZero`, weak or strong version, extends both the algebra and order hierarchy, which is something we want to move away from. Therefore, if anything, we should not be adding more of those typeclasses.
    
    This will be used in a later PR to replace `pow_eq_one_iff` with `IsMulTorsionFree.pow_eq_one_iff_left`.
    
    [Zulip](https://leanprover.zulipchat.com/#narrow/channel/287929-mathlib4/topic/Should.20linear.20ordered.20comm.20monoids.20with.20zero.20be.20torsion-free)

--- Mathlib/RingTheory/HahnSeries/Valuation.lean
+++ Mathlib/RingTheory/HahnSeries/Valuation.lean
@@ -35,10 +35,10 @@ variable {Γ R : Type*}
 namespace HahnSeries
 
 section Valuation
+variable [AddCancelCommMonoid Γ] [LinearOrder Γ] [IsOrderedCancelAddMonoid Γ] [Ring R] [IsDomain R]
 
-variable (Γ R) [AddCommMonoid Γ] [LinearOrder Γ] [IsOrderedCancelAddMonoid Γ] [Ring R] [IsDomain R]
-
-/-- The additive valuation on `R⟦Γ⟧`, returning the smallest index at which
+variable (Γ R) in
+/-- The additive valuation on `R⟦Γ⟧` returning the smallest index at which
   a Hahn Series has a nonzero coefficient, or `⊤` for the 0 series. -/
 def addVal : AddValuation R⟦Γ⟧ (WithTop Γ) :=
   AddValuation.of orderTop orderTop_zero (orderTop_one) (fun _ _ => min_orderTop_le_orderTop_add)
@@ -48,8 +48,6 @@ def addVal : AddValuation R⟦Γ⟧ (WithTop Γ) :=
     rw [← order_eq_orderTop_of_ne_zero hx, ← order_eq_orderTop_of_ne_zero hy,
       ← order_eq_orderTop_of_ne_zero (mul_ne_zero hx hy), ← WithTop.coe_add, WithTop.coe_eq_coe,
       order_mul hx hy]
-
-variable {Γ R}
 
 theorem addVal_apply {x : R⟦Γ⟧} : addVal Γ R x = x.orderTop :=
   AddValuation.of_apply _
