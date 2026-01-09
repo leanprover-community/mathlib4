3bc0f4d03930f94c310b27781fa7f8ee3badb738
Andrew Yang <the.erd.one@gmail.com>
2026-01-09 20:12:30 +0000

    add

--- Mathlib/RingTheory/QuasiFinite/Basic.lean
+++ Mathlib/RingTheory/QuasiFinite/Basic.lean
@@ -10,12 +10,9 @@ public import Mathlib.RingTheory.Artinian.Ring
 public import Mathlib.RingTheory.FiniteStability
 public import Mathlib.RingTheory.Finiteness.NilpotentKer
 public import Mathlib.RingTheory.Jacobson.Artinian
-public import Mathlib.RingTheory.LocalRing.ResidueField.Fiber
-public import Mathlib.RingTheory.Localization.Submodule
+public import Mathlib.RingTheory.LocalRing.ResidueField.Polynomial
 public import Mathlib.RingTheory.Spectrum.Prime.Jacobson
 public import Mathlib.RingTheory.TensorProduct.Pi
-public import Mathlib.RingTheory.TensorProduct.Quotient
-public import Mathlib.Topology.DiscreteSubset
 
 /-!
 # Quasi-finite algebras
@@ -511,3 +508,66 @@ lemma _root_.Ideal.exists_not_mem_forall_mem_of_ne_of_liesOver
 end QuasiFiniteAt
 
 end Algebra
+
+namespace Polynomial
+
+lemma not_quasiFiniteAt (P : Ideal R[X]) [P.IsPrime] : ¬ Algebra.QuasiFiniteAt R P := by
+  intro H
+  wlog hR : IsField R
+  · let p := P.under R
+    obtain ⟨Q, hQ⟩ := (PrimeSpectrum.preimageEquivFiber R R[X]
+        ⟨p, inferInstance⟩).symm.surjective ⟨⟨P, ‹_›⟩, rfl⟩
+    have inst : Algebra.QuasiFiniteAt p.ResidueField Q.asIdeal :=
+      .baseChange P Q.asIdeal congr($(hQ.symm).1.1)
+    exact this (Q.asIdeal.comap (polyEquivTensor' R p.ResidueField).toRingHom)
+      inferInstance (Field.toIsField _)
+  let := hR.toField
+  rw [Algebra.QuasiFiniteAt, Algebra.QuasiFinite.iff_of_isArtinianRing] at H
+  have := Module.Finite.of_injective
+    (IsScalarTower.toAlgHom R R[X] (Localization.AtPrime P)).toLinearMap
+    (IsLocalization.injective _ P.primeCompl_le_nonZeroDivisors)
+  exact transcendental_X R (Algebra.IsIntegral.isIntegral X).isAlgebraic
+
+lemma map_under_lt_comap_of_quasiFiniteAt
+    (f : R[X] →ₐ[R] S) (P : Ideal S) [P.IsPrime] [Algebra.QuasiFiniteAt R P] :
+    (P.under R).map C < P.comap (f : R[X] →+* S) := by
+  let := f.toRingHom.toAlgebra
+  refine lt_of_le_of_ne (Ideal.map_le_iff_le_comap.mpr ?_) fun e ↦ ?_
+  · rw [Ideal.comap_comap, ← algebraMap_eq, f.comp_algebraMap]
+  have : Module.Finite (P.under R).ResidueField (P.under R[X]).ResidueField :=
+    .of_injective (IsScalarTower.toAlgHom _ _ P.ResidueField).toLinearMap
+      (algebraMap (P.under R[X]).ResidueField P.ResidueField).injective
+  have : Module.Finite (P.under R).ResidueField (RatFunc (P.under R).ResidueField) :=
+    .of_surjective (residueFieldMapCAlgEquiv _ (P.under _) e.symm).toLinearMap
+      (residueFieldMapCAlgEquiv _ (P.under _) e.symm).surjective
+  have : Algebra.IsIntegral (P.under R).ResidueField (RatFunc (P.under R).ResidueField) :=
+    inferInstance
+  exact RatFunc.transcendental_X (K := (P.under R).ResidueField)
+    (Algebra.IsIntegral.isIntegral _).isAlgebraic
+
+lemma ker_le_map_C_of_surjective_of_quasiFiniteAt
+    (f : R[X] →ₐ[R] S) (hf : Function.Surjective f)
+    (P : Ideal S) [P.IsPrime] [Algebra.QuasiFiniteAt R P]
+    (p : Ideal R) [P.LiesOver p] : ¬ RingHom.ker f ≤ p.map C := by
+  intro H
+  let := f.toRingHom.toAlgebra
+  have : p.IsPrime := P.over_def p ▸ inferInstance
+  letI : Algebra (R[X] ⊗[R] p.ResidueField) (R[X] ⊗[R] p.ResidueField ⊗[R[X]]
+    (R[X] ⧸ RingHom.ker f)) := Algebra.TensorProduct.leftAlgebra
+  have : IsScalarTower R (R[X] ⊗[R] p.ResidueField)
+      (R[X] ⊗[R] p.ResidueField ⊗[R[X]] (R[X] ⧸ RingHom.ker f)) :=
+    TensorProduct.isScalarTower_left
+  have H' : (RingHom.ker f).map (mapRingHom (algebraMap R p.ResidueField)) = ⊥ := by
+    rw [← le_bot_iff, Ideal.map_le_iff_le_comap]
+    intro x hx
+    simpa [Polynomial.ext_iff, Ideal.mem_map_C_iff] using H hx
+  let g' : p.ResidueField[X] ≃ₐ[p.ResidueField] p.Fiber S :=
+    .trans ((AlgEquiv.quotientBot _ _).symm.trans (Ideal.quotientEquivAlgOfEq _ H'.symm))
+      (Polynomial.fiberEquivQuotient f hf _).symm
+  obtain ⟨Q, hQ⟩ := (PrimeSpectrum.preimageEquivFiber _ _
+      ⟨p, inferInstance⟩).symm.surjective ⟨⟨P, ‹_›⟩, PrimeSpectrum.ext (P.over_def p).symm⟩
+  have inst : Algebra.QuasiFiniteAt p.ResidueField Q.asIdeal :=
+    .baseChange P Q.asIdeal congr($(hQ.symm).1.1)
+  exact Polynomial.not_quasiFiniteAt (Q.asIdeal.comap g'.toRingHom) inferInstance
+
+end Polynomial
