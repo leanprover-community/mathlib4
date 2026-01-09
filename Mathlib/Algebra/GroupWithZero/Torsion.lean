a086802d0103abe47f092a98aa0e147e8738ac72
Junyan Xu <junyanxu.math@gmail.com>
2026-01-09 01:30:48 +0100

    fix

--- Mathlib/Algebra/GroupWithZero/Torsion.lean
+++ Mathlib/Algebra/GroupWithZero/Torsion.lean
@@ -27,6 +27,7 @@ theorem IsMulTorsionFree.mk' (ih : ∀ x ≠ 0, ∀ y ≠ 0, ∀ n ≠ 0, (x ^ n
   refine ⟨fun n hn x y hxy ↦ ?_⟩
   by_cases h : x ≠ 0 ∧ y ≠ 0
   · exact ih x h.1 y h.2 n hn hxy
+  have : IsReduced M := inferInstance
   grind [eq_zero_of_pow_eq_zero, zero_pow]
 
 variable [UniqueFactorizationMonoid M] [NormalizationMonoid M] [IsMulTorsionFree Mˣ]
