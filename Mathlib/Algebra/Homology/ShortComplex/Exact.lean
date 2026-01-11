07cdf386ebb494203a9bba60c7e5e2dce1c08e58
Joël Riou <joel.riou@universite-paris-saclay.fr>
2026-01-11 19:04:33 +0100

    fix

--- Mathlib/Algebra/Homology/ShortComplex/Exact.lean
+++ Mathlib/Algebra/Homology/ShortComplex/Exact.lean
@@ -236,15 +236,15 @@ lemma exact_map_iff_of_faithful [S.HasHomology]
     (F : C ⥤ D) [F.PreservesZeroMorphisms] [F.PreservesLeftHomologyOf S]
     [F.PreservesRightHomologyOf S] [F.Faithful] :
     (S.map F).Exact ↔ S.Exact := by
-    constructor
-    · intro h
-      rw [S.leftHomologyData.exact_iff, IsZero.iff_id_eq_zero]
-      rw [(S.leftHomologyData.map F).exact_iff, IsZero.iff_id_eq_zero,
-        LeftHomologyData.map_H] at h
-      apply F.map_injective
-      rw [F.map_id, F.map_zero, h]
-    · intro h
-      exact h.map F
+  constructor
+  · intro h
+    rw [S.leftHomologyData.exact_iff, IsZero.iff_id_eq_zero]
+    rw [(S.leftHomologyData.map F).exact_iff, IsZero.iff_id_eq_zero,
+      LeftHomologyData.map_H] at h
+    apply F.map_injective
+    rw [F.map_id, F.map_zero, h]
+  · intro h
+    exact h.map F
 
 variable {S}
 
