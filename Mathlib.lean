d976a728bf4ec71b48c520374abef77b78edf9a9
Yuval Filmus <filmus.yuval@gmail.com>
2026-01-11 14:16:36 +0200

    Create branch

--- Mathlib.lean
+++ Mathlib.lean
@@ -2166,6 +2166,7 @@ public import Mathlib.Analysis.SpecialFunctions.Trigonometric.Basic
 public import Mathlib.Analysis.SpecialFunctions.Trigonometric.Bounds
 public import Mathlib.Analysis.SpecialFunctions.Trigonometric.Chebyshev
 public import Mathlib.Analysis.SpecialFunctions.Trigonometric.Chebyshev.Basic
+public import Mathlib.Analysis.SpecialFunctions.Trigonometric.Chebyshev.ChebyshevGauss
 public import Mathlib.Analysis.SpecialFunctions.Trigonometric.Chebyshev.Orthogonality
 public import Mathlib.Analysis.SpecialFunctions.Trigonometric.Chebyshev.RootsExtrema
 public import Mathlib.Analysis.SpecialFunctions.Trigonometric.Complex
---
+++ Mathlib/Analysis/SpecialFunctions/Trigonometric/Chebyshev/ChebyshevGauss.lean
@@ -0,0 +1,18 @@
+/-
+Copyright (c) 2026 Yuval Filmus. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Yuval Filmus
+-/
+
+module
+
+public import Mathlib.Analysis.SpecialFunctions.Trigonometric.Chebyshev.Orthogonality
+
+/-!
+TODO
+-/
+public section
+
+namespace Polynomial.Chebyshev
+
+end Polynomial.Chebyshev
