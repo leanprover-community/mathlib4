299c1624c6934607c1cb0a8150b38567c7964621
euprunin <euprunin@users.noreply.github.com>
2026-01-12 00:58:17 +0100

    chore: golf and improve compilation time

--- Mathlib/Data/List/Count.lean
+++ Mathlib/Data/List/Count.lean
@@ -38,8 +38,7 @@ theorem count_lt_length_iff {a : α} : l.count a < l.length ↔ ∃ b ∈ l, b �
 
 lemma countP_erase (p : α → Bool) (l : List α) (a : α) :
     countP p (l.erase a) = countP p l - if a ∈ l ∧ p a then 1 else 0 := by
-  rw [countP_eq_length_filter, countP_eq_length_filter, ← erase_filter, length_erase]
-  aesop
+  grind [countP_eq_length_filter]
 
 lemma count_diff (a : α) (l₁ : List α) :
     ∀ l₂, count a (l₁.diff l₂) = count a l₁ - count a l₂
--- Mathlib/Data/Set/Pairwise/Basic.lean
+++ Mathlib/Data/Set/Pairwise/Basic.lean
@@ -132,8 +132,7 @@ theorem pairwise_eq_iff_exists_eq [Nonempty ι] (s : Set α) (f : α → ι) :
 theorem pairwise_union :
     (s ∪ t).Pairwise r ↔
     s.Pairwise r ∧ t.Pairwise r ∧ ∀ a ∈ s, ∀ b ∈ t, a ≠ b → r a b ∧ r b a := by
-  simp only [Set.Pairwise, mem_union, or_imp, forall_and]
-  aesop
+  grind [Set.Pairwise]
 
 theorem pairwise_union_of_symmetric (hr : Symmetric r) :
     (s ∪ t).Pairwise r ↔ s.Pairwise r ∧ t.Pairwise r ∧ ∀ a ∈ s, ∀ b ∈ t, a ≠ b → r a b :=
