1c4b42077a9441ef2c68d3bfae12bc8d83136e4c
Violeta Hernández <vi.hdz.p@gmail.com>
2026-01-11 08:55:31 -0600

    strengthen to iffs

--- Mathlib/Algebra/Order/Archimedean/Class.lean
+++ Mathlib/Algebra/Order/Archimedean/Class.lean
@@ -379,21 +379,39 @@ theorem mk_left_le_mk_div (hab : mk a ≤ mk b) : mk a ≤ mk (a / b) := by
 theorem mk_right_le_mk_div (hba : mk b ≤ mk a) : mk b ≤ mk (a / b) := by
   simpa [div_eq_mul_inv, hba] using mk_right_le_mk_mul (a := a) (b := b⁻¹)
 
-@[to_additive]
-theorem mk_le_mk_of_mul_left (h : mk a ≤ mk (a * b)) : mk a ≤ mk b := by
-  simpa using mk_left_le_mk_div h
+@[to_additive (attr := simp)]
+theorem mk_left_le_mk_mul_iff : mk a ≤ mk (a * b) ↔ mk a ≤ mk b where
+  mp h := by simpa using mk_left_le_mk_div h
+  mpr := mk_left_le_mk_mul
 
-@[to_additive]
-theorem mk_le_mk_of_mul_right (h : mk a ≤ mk (b * a)) : mk a ≤ mk b :=
-  mk_le_mk_of_mul_left (mul_comm a b ▸ h)
+@[to_additive (attr := simp)]
+theorem mk_right_le_mk_mul_iff : mk b ≤ mk (a * b) ↔ mk b ≤ mk a := by
+  rw [mul_comm, mk_left_le_mk_mul_iff]
 
-@[to_additive]
-theorem mk_le_mk_of_div_left (h : mk a ≤ mk (a / b)) : mk a ≤ mk b := by
-  simpa using mk_left_le_mk_div h
+@[to_additive (attr := simp)]
+theorem mk_left_le_mk_div_iff : mk a ≤ mk (a / b) ↔ mk a ≤ mk b where
+  mp h := by simpa using mk_left_le_mk_div h
+  mpr := mk_left_le_mk_div
 
-@[to_additive]
-theorem mk_le_mk_of_div_right (h : mk a ≤ mk (b / a)) : mk a ≤ mk b :=
-  mk_le_mk_of_div_left (mk_div_comm a b ▸ h)
+@[to_additive (attr := simp)]
+theorem mk_right_le_mk_div_iff : mk b ≤ mk (a / b) ↔ mk b ≤ mk a := by
+  rw [mk_div_comm, mk_left_le_mk_div_iff]
+
+@[to_additive (attr := simp)]
+theorem mk_mul_lt_mk_left_iff : mk (a * b) < mk a ↔ mk b < mk a :=
+  le_iff_le_iff_lt_iff_lt.1 mk_left_le_mk_mul_iff
+
+@[to_additive (attr := simp)]
+theorem mk_mul_lt_mk_right_iff : mk (a * b) < mk b ↔ mk a < mk b :=
+  le_iff_le_iff_lt_iff_lt.1 mk_right_le_mk_mul_iff
+
+@[to_additive (attr := simp)]
+theorem mk_div_lt_mk_left_iff : mk (a / b) < mk a ↔ mk b < mk a :=
+  le_iff_le_iff_lt_iff_lt.1 mk_left_le_mk_div_iff
+
+@[to_additive (attr := simp)]
+theorem mk_div_lt_mk_right_iff : mk (a / b) < mk b ↔ mk a < mk b :=
+  le_iff_le_iff_lt_iff_lt.1 mk_right_le_mk_div_iff
 
 @[to_additive]
 theorem mk_mul_eq_mk_left (h : mk a < mk b) : mk (a * b) = mk a := by
