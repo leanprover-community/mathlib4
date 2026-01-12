12b05a68fbca28e0280425b9240e68fd4553b99b
Joël Riou <joel.riou@universite-paris-saclay.fr>
2026-01-12 15:02:16 +0100

    feat(CategoryTheory/ComposableArrows): API for the composition of four arrows

--- Mathlib.lean
+++ Mathlib.lean
@@ -2379,6 +2379,7 @@ public import Mathlib.CategoryTheory.Comma.StructuredArrow.Final
 public import Mathlib.CategoryTheory.Comma.StructuredArrow.Functor
 public import Mathlib.CategoryTheory.Comma.StructuredArrow.Small
 public import Mathlib.CategoryTheory.ComposableArrows.Basic
+public import Mathlib.CategoryTheory.ComposableArrows.Four
 public import Mathlib.CategoryTheory.ComposableArrows.One
 public import Mathlib.CategoryTheory.ComposableArrows.Three
 public import Mathlib.CategoryTheory.ComposableArrows.Two
