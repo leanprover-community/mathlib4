41835268917529c2dc59ebd45672f1e55c372163
Michael Rothgang <rothgang@math.uni-bonn.de>
2026-01-12 22:37:57 +0100

    clearly true fixes

--- Mathlib/CategoryTheory/MorphismProperty/FunctorCategory.lean
+++ Mathlib/CategoryTheory/MorphismProperty/FunctorCategory.lean
@@ -57,7 +57,7 @@ instance IsStableUnderColimitsOfShape.functorCategory
     (W.functorCategory J).IsStableUnderColimitsOfShape K where
   condition X₁ X₂ _ _ hc₁ hc₂ f hf φ hφ j :=
     MorphismProperty.colimitsOfShape_le _
-      (colimitsOfShape.mk' (X₁ ⋙ (evaluation _ _ ).obj j) (X₂ ⋙ (evaluation _ _ ).obj j)
+      (colimitsOfShape.mk' (X₁ ⋙ (evaluation _ _).obj j) (X₂ ⋙ (evaluation _ _).obj j)
       _ _ (isColimitOfPreserves _ hc₁) (isColimitOfPreserves _ hc₂) (Functor.whiskerRight f _)
       (fun k ↦ hf k j) (φ.app j) (fun k ↦ congr_app (hφ k) j))
 
