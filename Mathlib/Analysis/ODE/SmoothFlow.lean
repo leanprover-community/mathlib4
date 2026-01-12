1fad8a76c3c981b83353709f240169b12a9ea02c
Winston Yin <winstonyin@gmail.com>
2026-01-12 15:56:40 -0500

    integralCM

--- Mathlib/Analysis/ODE/SmoothFlow.lean
+++ Mathlib/Analysis/ODE/SmoothFlow.lean
@@ -19,6 +19,8 @@ open scoped Nat NNReal Topology
 
 namespace SmoothFlow
 
+noncomputable section
+
 variable
   {E : Type*} [NormedAddCommGroup E] [NormedSpace ℝ E]
 
@@ -30,7 +32,25 @@ This helps us work with the space of continuous curves `C(Icc tmin tmax, E)`. We
 `C(Icc tmin tmax, E)` instead of the junk value pattern on `ℝ → E` because we need the space of
 curves to be a complete normed space.
 -/
-noncomputable def compProj {tmin tmax : ℝ} (t₀ : Icc tmin tmax) (α : C(Icc tmin tmax, E)) : ℝ → E :=
+def compProj {tmin tmax : ℝ} (t₀ : Icc tmin tmax) (α : C(Icc tmin tmax, E)) : ℝ → E :=
   fun t ↦ α (projIcc tmin tmax (le_trans t₀.2.1 t₀.2.2) t)
+
+/--
+The integral
+$$\int_{t₀}^t g(\alpha(\tau))(d\alpha_1(\tau),\cdots,d\alpha_n(\tau)) \,d\tau,$$
+where `g : x → E [×n]→L[ℝ] E` has the same type as the `n`-th iterated derivative of `f : E → E`.
+This is defined so that its derivative with respect to `α` will yield the same integral expression,
+but with `n` replaced by `n + 1` and `g` replaced by its derivative.
+-/
+def integralFun {n : ℕ} (g : E → E [×n]→L[ℝ] E) {tmin tmax : ℝ} (t₀ : Icc tmin tmax)
+    (α : C(Icc tmin tmax, E)) (dα : Fin n → C(Icc tmin tmax, E)) (t : Icc tmin tmax) : E :=
+  ∫ τ in t₀..t, g (compProj t₀ α τ) (fun i ↦ compProj t₀ (dα i) τ)
+
+def integralCM {n : ℕ} (g : E → E [×n]→L[ℝ] E) {tmin tmax : ℝ} (t₀ : Icc tmin tmax)
+    (α : C(Icc tmin tmax, E)) (dα : Fin n → C(Icc tmin tmax, E)) : C(Icc tmin tmax, E) where
+  toFun := integralFun g t₀ α dα
+  continuous_toFun := sorry
+
+end
 
 end SmoothFlow
