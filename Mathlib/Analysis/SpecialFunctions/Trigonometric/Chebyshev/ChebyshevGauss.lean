fcf04b32374ef62696207cab20a8a2985a6a677f
Yuval Filmus <filmus.yuval@gmail.com>
2026-01-12 12:33:32 +0200

    Strengthen results

--- Mathlib/Analysis/SpecialFunctions/Trigonometric/Chebyshev/ChebyshevGauss.lean
+++ Mathlib/Analysis/SpecialFunctions/Trigonometric/Chebyshev/ChebyshevGauss.lean
@@ -31,38 +31,37 @@ public section
 namespace Polynomial.Chebyshev
 
 open Real Polynomial Finset
+open Complex (exp I)
 
-private lemma exp_sub_one_ne_zero {n : ℕ} {k : ℤ} (hn : n ≠ 0)
-  (hk₀ : k ≠ 0) (hk₁ : k.natAbs < 2 * n) : Complex.exp (k / n * π * Complex.I) ≠ 1 := by
+private lemma exp_sub_one_ne_zero {n : ℕ} {k : ℤ} (hn : n ≠ 0) (hk : ¬ (2 * n : ℤ) ∣ k) :
+  exp (k / n * π * I) ≠ 1 := by
   have : (n : ℂ) ≠ 0 := by aesop
-  by_contra!
-  obtain ⟨m, hx⟩ := Complex.exp_eq_one_iff.mp this
-  have h : k = m * (2 * n) := by
+  contrapose! hk
+  obtain ⟨m, hx⟩ := Complex.exp_eq_one_iff.mp hk
+  have h : k = 2 * n * m := by
     apply (@Int.cast_inj ℂ _ _).mp
-    linear_combination (norm := (push_cast; field)) hx * (n / π / Complex.I)
-  have : m < 1 := Int.lt_of_mul_lt_mul_right (a := 2 * n) (by grind) (by positivity)
-  have : -1 < m := Int.lt_of_mul_lt_mul_right (a := 2 * n) (by grind) (by positivity)
-  grind
+    linear_combination (norm := (push_cast; field)) hx * (n / π / I)
+  use m
 
-private theorem sum_exp {n : ℕ} {k : ℤ} (hn : n ≠ 0) (hk₀ : k ≠ 0) (hk₁ : k.natAbs < 2 * n) :
-    ∑ i ∈ range n, Complex.exp ((k * ((2 * i + 1) / (2 * n) * π)) * Complex.I) =
-    (Complex.exp (k / (2 * n) * π * Complex.I) / (Complex.exp (k / n * π * Complex.I) - 1)) *
+private theorem sum_exp {n : ℕ} {k : ℤ} (hn : n ≠ 0) (hk : ¬ (2 * n : ℤ) ∣ k) :
+    ∑ i ∈ range n, exp ((k * ((2 * i + 1) / (2 * n) * π)) * I) =
+    (exp (k / (2 * n) * π * I) / (exp (k / n * π * I) - 1)) *
     ((-1) ^ k - 1) := by
   have : (n : ℂ) ≠ 0 := by aesop
-  suffices (∑ i ∈ range n, Complex.exp ((k * ((2 * i + 1) / (2 * n) * π)) * Complex.I)) *
-    Complex.exp (-(k / (2 * n) * π * Complex.I)) * (Complex.exp (k / n * π * Complex.I) - 1) =
+  suffices (∑ i ∈ range n, exp ((k * ((2 * i + 1) / (2 * n) * π)) * I)) *
+    exp (-(k / (2 * n) * π * I)) * (exp (k / n * π * I) - 1) =
     (-1) ^ k - 1 by
     rw [Complex.exp_neg] at this
     have hf {s a b t : ℂ} (h : s * a⁻¹ * b = t) (ha : a ≠ 0) (hb : b ≠ 0) : s = a / b * t := by
       linear_combination (norm := field) h * a / b
     apply hf this (Complex.exp_ne_zero _) (by grind [exp_sub_one_ne_zero])
-  convert geom_sum_mul (Complex.exp (k / n * π * Complex.I)) n using 1
+  convert geom_sum_mul (exp (k / n * π * I)) n using 1
   · congr 1
     rw [sum_mul]
     congr! 1 with i hi
     rw [← Complex.exp_nat_mul, ← Complex.exp_add]
     grind
-  · rw [← Complex.exp_nat_mul, show (n * (k / n * π * Complex.I)) = k * (π * Complex.I) by field,
+  · rw [← Complex.exp_nat_mul, show (n * (k / n * π * I)) = k * (π * I) by field,
       Complex.exp_int_mul, Complex.exp_pi_mul_I]
 
 /-- Weighted sum of P (x) where x goes over cos ((2 * i + 1) / (2 * n) * π) for 0 ≤ i < n. -/
@@ -87,7 +86,7 @@ theorem sumZeroes_T_zero {n : ℕ} (hn : n ≠ 0) :
     sumZeroes n (T ℝ 0) = π := by
   simp [sumZeroes, show π / n * n = π by field]
 
-theorem sumZeroes_T_of_ne_zero {n : ℕ} {k : ℤ} (hk₀ : k ≠ 0) (hk₁ : k.natAbs < 2 * n) :
+theorem sumZeroes_T_of_not_dvd {n : ℕ} {k : ℤ} (hk : ¬ (2 * n : ℤ) ∣ k) :
     sumZeroes n (T ℝ k) = 0 := by
   wlog! hn : n ≠ 0
   · simp [sumZeroes, hn]
@@ -99,16 +98,15 @@ theorem sumZeroes_T_of_ne_zero {n : ℕ} {k : ℤ} (hk₀ : k ≠ 0) (hk₁ : k.
   suffices ∑ i ∈ range n, 2 * Complex.cos (k * ((2 * i + 1) / (2 * n) * π)) = 0 by aesop
   simp_rw [Complex.two_cos, ← neg_mul, ← Int.cast_neg]
   have : (-1 : ℂ) ^ (-k) = (-1) ^ k := by rw [← Int.cast_negOnePow, ← Int.cast_negOnePow]; simp
-  rw [sum_add_distrib, sum_exp hn (by grind) (by grind), sum_exp hn (by grind) (by grind),
+  rw [sum_add_distrib, sum_exp hn hk, sum_exp hn (by aesop),
     Int.cast_neg, neg_div, neg_mul, neg_mul, Complex.exp_neg,
     neg_div, neg_mul, neg_mul, Complex.exp_neg, this, ← add_mul, mul_eq_zero_of_left]
-  set z := Complex.exp (k / (2 * n) * π * Complex.I) with hz
-  have hz₂ : Complex.exp (k / n * π * Complex.I) = z ^ 2 := by
+  set z := exp (k / (2 * n) * π * I) with hz
+  have hz₂ : exp (k / n * π * I) = z ^ 2 := by
     rw [hz, ← Complex.exp_nat_mul]; grind
   rw [hz₂, ← inv_pow z 2]
-  have : z ≠ 0 := by grind [Complex.exp_ne_zero]
-  have : z ^ 2 ≠ 1 := by grind [exp_sub_one_ne_zero]
-  field [show (z ^ 2 - 1 ≠ 0) ∧ (1 - z ^ 2 ≠ 0) by grind]
+  field [show z ≠ 0 by grind [Complex.exp_ne_zero],
+    show (z ^ 2 - 1 ≠ 0) ∧ (1 - z ^ 2 ≠ 0) by grind [exp_sub_one_ne_zero]]
 
 -- This probably belongs somewhere else
 theorem poly_eq_sum_of_deg {F : Type*} [Field F] {n : ℕ} {P : F[X]} {Q : Fin n → F[X]}
@@ -170,8 +168,11 @@ theorem integral_eq_sumZeroes {n : ℕ} {P : ℝ[X]} (hn : n ≠ 0) (hP : P.degr
     by_cases i.val = 0
     case pos hi => rw [hi, Nat.cast_zero, integral_T_real_zero, sumZeroes_T_zero hn]
     case neg hi =>
-      rw [integral_T_real_of_ne_zero (by grind),
-        sumZeroes_T_of_ne_zero (by grind) (by convert i.isLt)]
+      have : ¬ (2 * n : ℤ) ∣ i := by
+        refine (Int.not_dvd_iff_lt_mul_succ _ (by grind)).mpr ⟨0, ⟨by grind, ?_⟩⟩
+        rw_mod_cast [zero_add, mul_one]
+        exact i.isLt
+      rw [integral_T_real_of_ne_zero (by grind), sumZeroes_T_of_not_dvd this]
   · simp_rw [← eval_smul]
     exact fun i hi => integrable_poly_T _
 
