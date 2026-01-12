3022877d38e614dd06534545bf00465b15edd7e9
Yury G. Kudryashov <urkud@urkud.name>
2026-01-11 20:16:43 -0600

    Update Mathlib/Algebra/ModEq.lean
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- Mathlib/Algebra/ModEq.lean
+++ Mathlib/Algebra/ModEq.lean
@@ -158,7 +158,6 @@ end ModEq
 end AddCancelCommMonoid
 
 section AddCommGroup
-
 variable {G : Type*} [AddCommGroup G] {p a a₁ a₂ b b₁ b₂ c : G} {n : ℕ} {z : ℤ}
 
 theorem modEq_iff_zsmul : a ≡ b [PMOD p] ↔ ∃ m : ℤ, m • p = b - a := by
