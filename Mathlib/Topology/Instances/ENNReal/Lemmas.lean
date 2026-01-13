f3cec3a56f5f1e9735f7249e897efb8c45e77fab
Simon <Citronhat@gmail.com>
2026-01-13 15:40:19 +0000

    feat: generalize ENNReal.tendsto_inv_iff

--- Mathlib/Topology/Instances/ENNReal/Lemmas.lean
+++ Mathlib/Topology/Instances/ENNReal/Lemmas.lean
@@ -465,10 +465,10 @@ protected theorem continuous_zpow : ∀ n : ℤ, Continuous (· ^ n : ℝ≥0∞
   | (n : ℕ) => mod_cast ENNReal.continuous_pow n
   | .negSucc n => by simpa using (ENNReal.continuous_pow _).inv
 
-@[simp] -- TODO: generalize to `[InvolutiveInv _] [ContinuousInv _]`
+@[simp]
 protected theorem tendsto_inv_iff {f : Filter α} {m : α → ℝ≥0∞} {a : ℝ≥0∞} :
     Tendsto (fun x => (m x)⁻¹) f (𝓝 a⁻¹) ↔ Tendsto m f (𝓝 a) :=
-  ⟨fun h => by simpa only [inv_inv] using Tendsto.inv h, Tendsto.inv⟩
+  tendsto_inv_iff
 
 protected theorem Tendsto.div {f : Filter α} {ma : α → ℝ≥0∞} {mb : α → ℝ≥0∞} {a b : ℝ≥0∞}
     (hma : Tendsto ma f (𝓝 a)) (ha : a ≠ 0 ∨ b ≠ 0) (hmb : Tendsto mb f (𝓝 b))
