7db0f67373dc828c8d16d1a630435357220a8d0a
Andrew Yang <the.erd.one@gmail.com>
2026-01-13 06:47:43 +0000

    fix

--- Mathlib/AlgebraicGeometry/Morphisms/QuasiFinite.lean
+++ Mathlib/AlgebraicGeometry/Morphisms/QuasiFinite.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Andrew Yang
 -/
 import Mathlib.AlgebraicGeometry.Normalization
+import Mathlib.AlgebraicGeometry.Morphisms.Finite
 import Mathlib.RingTheory.ZariskiMain
 import Mathlib.RingTheory.RingHom.QuasiFinite
 
