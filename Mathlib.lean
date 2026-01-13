fc2de2f6c15139b7c6e4a021fd3373b453d7c5a3
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2026-01-13 15:04:03 +0100

    conclude the proof and add docstring

--- Mathlib.lean
+++ Mathlib.lean
@@ -4780,6 +4780,7 @@ public import Mathlib.LinearAlgebra.SesquilinearForm.Star
 public import Mathlib.LinearAlgebra.Span.Basic
 public import Mathlib.LinearAlgebra.Span.Defs
 public import Mathlib.LinearAlgebra.SpecialLinearGroup
+public import Mathlib.LinearAlgebra.SpecialLinearGroup.Simple
 public import Mathlib.LinearAlgebra.StdBasis
 public import Mathlib.LinearAlgebra.SymmetricAlgebra.Basic
 public import Mathlib.LinearAlgebra.SymmetricAlgebra.Basis
