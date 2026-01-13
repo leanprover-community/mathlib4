ba31b870a8d6ee2a28acef032327b419d1d51a6c
Yaël Dillies <yael.dillies@gmail.com>
2026-01-13 15:40:23 +0100

    append

--- Mathlib/Data/List/Rotate.lean
+++ Mathlib/Data/List/Rotate.lean
@@ -459,7 +459,8 @@ theorem IsRotated.map {β : Type*} {l₁ l₂ : List α} (h : l₁ ~r l₂) (f :
   rw [map_rotate]
   use n
 
-lemma isRotated_cons_concat : a :: l ~r l.concat a := by simpa using isRotated_append (l := [a])
+lemma isRotated_cons_append_singleton : a :: l ~r l ++ [a] := by
+  simpa using isRotated_append (l := [a])
 
 theorem IsRotated.cons_getLast_dropLast
     (L : List α) (hL : L ≠ []) : L.getLast hL :: L.dropLast ~r L := by
