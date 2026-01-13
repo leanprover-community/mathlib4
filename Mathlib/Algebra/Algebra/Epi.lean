8e586a32f352b454b52112898bbee1feb338bd10
Oliver Nash <github@olivernash.org>
2026-01-13 20:49:59 +0000

    Feedback from CR

--- Mathlib/Algebra/Algebra/Epi.lean
+++ Mathlib/Algebra/Algebra/Epi.lean
@@ -8,7 +8,7 @@ module
 public import Mathlib.Algebra.Algebra.Bilinear
 public import Mathlib.LinearAlgebra.TensorProduct.Tower
 public import Mathlib.RingTheory.Localization.FractionRing
-public import Mathlib.RingTheory.TensorProduct.Basic
+public import Mathlib.RingTheory.TensorProduct.Finite
 
 /-!
 # Algebras which are commutative ring epimorphisms
@@ -19,11 +19,11 @@ public import Mathlib.RingTheory.TensorProduct.Basic
 noncomputable section
 open Function TensorProduct
 
-variable (R A : Type*) [CommSemiring R] [CommSemiring A]
-
 namespace Algebra
 
-variable [Algebra R A]
+section Semiring
+
+variable (R A : Type*) [CommSemiring R] [Semiring A] [Algebra R A]
 
 /-- A commutative `R`-algebra `A` is epi, if the multiplication map `A ⊗[R] A → A` is injective. -/
 protected class IsEpi : Prop where
@@ -49,6 +49,8 @@ lemma isEpi_iff_forall_one_tmul_eq :
   obtain ⟨b, rfl⟩ := h' y
   aesop
 
+end Semiring
+
 -- TODO Generalise to any localization
 instance (R A : Type*) [CommRing R] [IsDomain R] [Field A] [Algebra R A] [IsFractionRing R A] :
     Algebra.IsEpi R A := by
@@ -63,18 +65,46 @@ instance (R A : Type*) [CommRing R] [IsDomain R] [Field A] [Algebra R A] [IsFrac
      _ = (f a / f b) ⊗ₜ[R] (b • (1 / f b)) := by rw [smul_tmul]
      _ = (f a / f b) ⊗ₜ[R] 1 := by rw [smul_def, mul_div_cancel₀ _ hb]
 
-variable [Algebra.IsEpi R A]
+section Ring
+
+variable {R A : Type*} [CommRing R] [Ring A] [Algebra R A] [Algebra.IsEpi R A]
+
+lemma _root_.RingHom.surjective_of_tmul_eq_tmul_of_finite [Module.Finite R A] :
+    Surjective (algebraMap R A) := by
+  let R' := (Algebra.linearMap R A).range
+  rcases subsingleton_or_nontrivial (A ⧸ R') with h | _
+  · rwa [Submodule.Quotient.subsingleton_iff, LinearMap.range_eq_top] at h
+  have : Subsingleton ((A ⧸ R') ⊗[R] (A ⧸ R')) := by
+    refine subsingleton_of_forall_eq 0 fun y ↦ ?_
+    induction y with
+    | zero => rfl
+    | add a b e₁ e₂ => rwa [e₁, zero_add]
+    | tmul x y =>
+      obtain ⟨x, rfl⟩ := R'.mkQ_surjective x
+      obtain ⟨y, rfl⟩ := R'.mkQ_surjective y
+      obtain ⟨s, hs⟩ : ∃ s, 1 ⊗ₜ[R] s = x ⊗ₜ[R] y := by
+        use x * y
+        trans x ⊗ₜ 1 * 1 ⊗ₜ y
+        · simp [(isEpi_iff_forall_one_tmul_eq R A).mp]
+        · simp
+      have : R'.mkQ 1 = 0 := (Submodule.Quotient.mk_eq_zero R').mpr ⟨1, map_one (algebraMap R A)⟩
+      rw [← map_tmul R'.mkQ R'.mkQ, ← hs, map_tmul, this, zero_tmul]
+  cases false_of_nontrivial_of_subsingleton ((A ⧸ R') ⊗[R] (A ⧸ R'))
+
+end Ring
+
+section CommSemiring
+
+variable (R A : Type*) [CommSemiring R] [CommSemiring A] [Algebra R A] [Algebra.IsEpi R A]
 
 variable {A} in
 lemma tmul_comm (a b : A) :
-    a ⊗ₜ[R] b = b ⊗ₜ[R] a :=
-  calc a ⊗ₜ[R] b = a • (1 ⊗ₜ[R] b) := by rw [tmul_eq_smul_one_tmul]
-              _ = a • (b ⊗ₜ[R] 1) := by rw [(isEpi_iff_forall_one_tmul_eq R A).mp inferInstance b]
-              _ = a • (b • (1 ⊗ₜ[R] 1)) := by rw [tmul_eq_smul_one_tmul]
-              _ = b • (a • (1 ⊗ₜ[R] 1)) := by rw [smul_comm]
-              _ = b • (a ⊗ₜ[R] 1) := by rw [← tmul_eq_smul_one_tmul]
-              _ = b • (1 ⊗ₜ[R] a) := by rw [(isEpi_iff_forall_one_tmul_eq R A).mp inferInstance a]
-              _ = b ⊗ₜ[R] a := by rw [← tmul_eq_smul_one_tmul]
+    a ⊗ₜ[R] b = b ⊗ₜ[R] a := by
+  have (a b : A) := calc a ⊗ₜ[R] b
+      = a • (1 ⊗ₜ[R] b) := by rw [tmul_eq_smul_one_tmul]
+    _ = a • (b ⊗ₜ[R] 1) := by rw [(isEpi_iff_forall_one_tmul_eq R A).mp inferInstance b]
+    _ = a • (b • (1 ⊗ₜ[R] 1)) := by rw [tmul_eq_smul_one_tmul]
+  rw [this a b, this b a, smul_comm]
 
 section Module
 
@@ -128,5 +158,7 @@ def _root_.TensorProduct.lid' : A ⊗[R] M ≃ₗ[A] M :=
   (TensorProduct.lid' R A M).injective <| by simp
 
 end Module
+
+end CommSemiring
 
 end Algebra
