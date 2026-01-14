7e5757a027fe751c3bfa4c8206ba7fe5d1c2c581
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2026-01-14 09:25:23 +0100

    delete use of AddCommMonoid

--- Mathlib/Algebra/Ring/Subring/Basic.lean
+++ Mathlib/Algebra/Ring/Subring/Basic.lean
@@ -1085,12 +1085,12 @@ instance center.smulCommClass_right : SMulCommClass R (center R) R :=
   Subsemiring.center.smulCommClass_right
 
 /-- The center of a semiring acts commutatively on any `R`-module -/
-instance {M : Type*} [AddCommMonoid M] [MulAction R M] :
+instance {M : Type*} [MulAction R M] :
     SMulCommClass R (Subring.center R) M :=
    inferInstanceAs <| SMulCommClass R ↥(Submonoid.center R) M
 
 /-- The center of a semiring acts commutatively on any `R`-module -/
-instance {M : Type*} [AddCommMonoid M] [MulAction R M] :
+instance {M : Type*} [MulAction R M] :
     SMulCommClass (Subring.center R) R M :=
    inferInstanceAs <| SMulCommClass ↥(Submonoid.center R) R M
 
