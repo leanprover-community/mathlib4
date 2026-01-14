d4b1004485f79a858d145772c6b3eaa625419f8b
IlPreteRosso <will1541286313@gmail.com>
2026-01-14 17:41:17 -0500

    docstring

--- Mathlib/Topology/Algebra/InfiniteSum/DiscreteConvolution.lean
+++ Mathlib/Topology/Algebra/InfiniteSum/DiscreteConvolution.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2025 Fengyang Wang. All rights reserved.
+Copyright (c) 2026 Fengyang Wang. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Fengyang Wang
 -/
