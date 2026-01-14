91538d3c5d6c344cdf44e06df088d8e7d8ee60ba
Seewoo Lee <seewoo5@gmail.com>
2026-01-14 09:14:33 -0800

    add author

--- Mathlib/GroupTheory/SpecificGroups/Dihedral.lean
+++ Mathlib/GroupTheory/SpecificGroups/Dihedral.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2020 Shing Tak Lam. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Shing Tak Lam
+Authors: Shing Tak Lam, Seewoo Lee
 -/
 module
 
