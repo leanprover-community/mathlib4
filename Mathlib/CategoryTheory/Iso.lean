17f5efb600394a8666a4cb05496bc0b927d25881
Jovan Gerbscheid <56355248+JovanGerb@users.noreply.github.com>
2026-01-14 11:11:45 +0000

    chore(CategoryTheory/Iso): address FIXME (#33945)
    This comment in the code complains about a limitation of `@[reassoc]`, but it seems `reassoc` has improved since.

--- Mathlib/CategoryTheory/Iso.lean
+++ Mathlib/CategoryTheory/Iso.lean
@@ -245,31 +245,13 @@ noncomputable def inv (f : X ⟶ Y) [I : IsIso f] : Y ⟶ X :=
 
 namespace IsIso
 
-@[simp, grind =]
+@[reassoc (attr := simp), grind =]
 theorem hom_inv_id (f : X ⟶ Y) [I : IsIso f] : f ≫ inv f = 𝟙 X :=
   (Classical.choose_spec I.1).left
 
-@[simp, grind =]
+@[reassoc (attr := simp), grind =]
 theorem inv_hom_id (f : X ⟶ Y) [I : IsIso f] : inv f ≫ f = 𝟙 Y :=
   (Classical.choose_spec I.1).right
-
--- FIXME putting @[reassoc] on the `hom_inv_id` above somehow unfolds `inv`
--- This happens even if we make `inv` irreducible!
--- I don't understand how this is happening: it is likely a bug.
-
--- attribute [reassoc] hom_inv_id inv_hom_id
--- #print hom_inv_id_assoc
---   theorem CategoryTheory.IsIso.hom_inv_id_assoc {X Y : C} (f : X ⟶ Y) [I : IsIso f]
---     {Z : C} (h : X ⟶ Z),
---     f ≫ Classical.choose (_ : Exists fun inv ↦ f ≫ inv = 𝟙 X ∧ inv ≫ f = 𝟙 Y) ≫ h = h := ...
-
-@[simp]
-theorem hom_inv_id_assoc (f : X ⟶ Y) [I : IsIso f] {Z} (g : X ⟶ Z) : f ≫ inv f ≫ g = g := by
-  simp [← Category.assoc]
-
-@[simp]
-theorem inv_hom_id_assoc (f : X ⟶ Y) [I : IsIso f] {Z} (g : Y ⟶ Z) : inv f ≫ f ≫ g = g := by
-  simp [← Category.assoc]
 
 end IsIso
 
