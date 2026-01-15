52dffaeb39874d0d278d0666aaeb62fa31dfe5eb
Sebastian Ullrich <sebasti@nullri.ch>
2026-01-15 15:23:44 +0000

    try !Elab.async

--- lakefile.lean
+++ lakefile.lean
@@ -43,7 +43,8 @@ abbrev mathlibOnlyLinters : Array LeanOption := #[
 abbrev mathlibLeanOptions := #[
     ⟨`pp.unicode.fun, true⟩, -- pretty-prints `fun a ↦ b`
     ⟨`autoImplicit, false⟩,
-    ⟨`maxSynthPendingDepth, .ofNat 3⟩
+    ⟨`maxSynthPendingDepth, .ofNat 3⟩,
+    ⟨`Elab.async, false⟩
   ] ++ -- options that are used in `lake build`
     mathlibOnlyLinters.map fun s ↦ { s with name := `weak ++ s.name }
 
