f2b0505c068ad9eeceeac81f2bc86debaefe35f3
Anne C.A. Baanen <vierkantor@vierkantor.com>
2026-01-15 18:26:32 +0100

    Add a test that Verso docstrings are able to produce messages

--- MathlibTest/VersoHeader.lean
+++ MathlibTest/VersoHeader.lean
@@ -15,3 +15,21 @@ set_option doc.verso true in
 set_option linter.style.header true in
 set_option doc.verso true in
 def foo : Nat := 37
+
+
+-- Make sure Verso is indeed enabled when we do `set_option docs.verso true`.
+
+/--
+warning: Code block could be more specific.
+
+Hint: Insert a specific kind of code block:
+  ```l̲e̲a̲n̲
+-/
+#guard_msgs in
+set_option doc.verso true in
+/--
+```
+#check foo
+```
+-/
+def bar : Nat := 37 * 2
