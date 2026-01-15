3a4945ab13914077801036235690b6746f1eb14a
Newell Jensen <newell.jensen@gmail.com>
2026-01-15 01:41:00 -0800

    Trim imports

--- Mathlib/Topology/MetricSpace/Cover.lean
+++ Mathlib/Topology/MetricSpace/Cover.lean
@@ -5,7 +5,6 @@ Authors: Yaël Dillies
 -/
 module
 
-public import Mathlib.Data.Rel.Cover
 public import Mathlib.Topology.MetricSpace.MetricSeparated
 public import Mathlib.Topology.MetricSpace.Thickening
 
