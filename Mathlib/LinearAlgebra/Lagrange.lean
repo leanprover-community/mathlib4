5cd9d3c2e8986190f9d0b5518eda9cad3b65168a
Yuval Filmus <filmus.yuval@gmail.com>
2026-01-15 11:10:15 +0000

    feat(LinearAlgebra/Lagrange): refactored proof of leadingCoeff_eq_sum (#33194)
    The proof of `leadingCoeff_eq_sum` was slightly refactored.
    A subsequent PR will use the refactored parts to prove a formula for the iterated derivative of a polynomial at a point.

--- Mathlib/LinearAlgebra/Lagrange.lean
+++ Mathlib/LinearAlgebra/Lagrange.lean
@@ -449,31 +449,29 @@ theorem interpolate_eq_add_interpolate_erase (hvs : Set.InjOn v s) (hi : i ∈ s
     sdiff_singleton_eq_erase]
   exact insert_subset_iff.mpr ⟨hi, singleton_subset_iff.mpr hj⟩
 
-open scoped Classical in
+@[deprecated eq_interpolate (since := "2026-01-14")]
 theorem interpolate_poly_eq_self
     (hvs : Set.InjOn v s) {P : Polynomial F} (hP : P.degree < s.card) :
     interpolate s v (fun i => P.eval (v i)) = P := (eq_interpolate hvs hP).symm
 
-theorem leadingCoeff_eq_sum
-    (hvs : Set.InjOn v s) {P : Polynomial F} (hP : s.card = P.degree + 1) :
-    P.leadingCoeff = ∑ i ∈ s, (P.eval (v i)) / ∏ j ∈ s.erase i, ((v i) - (v j)) := by
-  have P_degree : P.degree = ↑(s.card - 1) := by
-    cases h : P.degree
-    case bot => simp_all
-    case coe d =>
-      rw [Nat.cast_withBot] at hP ⊢
-      suffices #s = d + 1 by grind
-      rw [h] at hP
-      simp [← WithBot.coe_inj, hP]
-  have P_natDegree : P.natDegree = s.card - 1 := natDegree_eq_of_degree_eq_some P_degree
-  have s_card : s.card > 0 := by by_contra! h; simp_all
-  have hP' : P.degree < s.card := by grind [Nat.cast_lt]
-  rw [leadingCoeff, P_natDegree]
-  rw (occs := [1]) [← interpolate_poly_eq_self hvs hP']
+theorem coeff_eq_sum
+    (hvs : Set.InjOn v s) {P : Polynomial F} (hP : P.degree < #s) :
+    P.coeff (#s - 1) = ∑ i ∈ s, (P.eval (v i)) / ∏ j ∈ s.erase i, (v i - v j) := by
+  rw (occs := [1]) [eq_interpolate hvs hP]
   rw [interpolate_apply, finset_sum_coeff]
   congr! with i hi
   rw [coeff_C_mul, ← natDegree_basis hvs hi, ← leadingCoeff, leadingCoeff_basis hvs hi]
   field_simp
+
+theorem leadingCoeff_eq_sum
+    (hvs : Set.InjOn v s) {P : Polynomial F} (hP : #s = P.degree + 1) :
+    P.leadingCoeff = ∑ i ∈ s, (P.eval (v i)) / ∏ j ∈ s.erase i, (v i - v j) := by
+  lift P.degree to ℕ using (by contrapose! hP; simp [hP]) with deg hdeg
+  rw [← WithBot.coe_one, ← WithBot.coe_add] at hP
+  replace hP : #s = deg + 1 := WithBot.coe_eq_coe.mp hP
+  have hdegree : P.degree = ↑(#s - 1) := hdeg.symm.trans (WithBot.coe_eq_coe.mpr (by grind))
+  rw [leadingCoeff, natDegree_eq_of_degree_eq_some hdegree]
+  exact coeff_eq_sum hvs (by rw [hdegree]; norm_cast; omega)
 
 end Interpolate
 
