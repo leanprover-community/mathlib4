a719f64bbbd12ddc4c3d291ab01c6fc448b60cee
Moritz Doll <moritz.doll@googlemail.com>
2026-01-16 13:55:50 +1100

    deprecations

--- Mathlib.lean
+++ Mathlib.lean
@@ -1787,6 +1787,8 @@ public import Mathlib.Analysis.Distribution.AEEqOfIntegralContDiff
 public import Mathlib.Analysis.Distribution.ContDiffMapSupportedIn
 public import Mathlib.Analysis.Distribution.DerivNotation
 public import Mathlib.Analysis.Distribution.Distribution
+public import Mathlib.Analysis.Distribution.FourierSchwartz
+public import Mathlib.Analysis.Distribution.SchwartzSpace
 public import Mathlib.Analysis.Distribution.SchwartzSpace.Basic
 public import Mathlib.Analysis.Distribution.SchwartzSpace.Deriv
 public import Mathlib.Analysis.Distribution.SchwartzSpace.Fourier
---
+++ Mathlib/Analysis/Distribution/FourierSchwartz.lean
@@ -0,0 +1,5 @@
+module
+
+public import Mathlib.Analysis.Distribution.SchwartzSpace.Fourier
+
+deprecated_module (since := "2026-01-16")
