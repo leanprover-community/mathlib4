6ed2a6a4baae116c56fd2516b78277f581328ec2
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2026-01-16 03:23:53 +0000

    fix name swap

--- Mathlib/CategoryTheory/Iso.lean
+++ Mathlib/CategoryTheory/Iso.lean
@@ -279,14 +279,14 @@ noncomputable def asIso (f : X ⟶ Y) [IsIso f] : X ≅ Y :=
 -- Porting note: the `IsIso f` argument had been instance implicit,
 -- but we've changed it to implicit as a `rw` in `Mathlib/CategoryTheory/Closed/Functor.lean`
 -- was failing to generate it by typeclass search.
-@[to_dual (attr := simp) asIso'_hom]
+@[to_dual (attr := simp) asIso'_inv]
 theorem asIso_hom (f : X ⟶ Y) [IsIso f] : (asIso f).hom = f :=
   rfl
 
 -- Porting note: the `IsIso f` argument had been instance implicit,
 -- but we've changed it to implicit as a `rw` in `Mathlib/CategoryTheory/Closed/Functor.lean`
 -- was failing to generate it by typeclass search.
-@[to_dual (attr := simp) asIso'_inv]
+@[to_dual (attr := simp) asIso'_hom]
 theorem asIso_inv (f : X ⟶ Y) [IsIso f] : (asIso f).inv = inv f :=
   rfl
 
