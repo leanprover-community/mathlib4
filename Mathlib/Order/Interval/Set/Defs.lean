e90e87b3b03267c3b2aa0b65a5f5ad3b8a0869c2
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2026-01-16 13:42:27 +0000

    remove use cases from this PR

--- Mathlib/Order/Interval/Set/Defs.lean
+++ Mathlib/Order/Interval/Set/Defs.lean
@@ -32,52 +32,52 @@ namespace Set
 variable {α : Type*} [Preorder α] {a b x : α}
 
 /-- `Ioo a b` is the left-open right-open interval $(a, b)$. -/
-@[to_dual self (reorder := a b)]
 def Ioo (a b : α) := { x | a < x ∧ x < b }
 
-to_dual_insert_cast Ioo := by grind
-
-@[to_dual mem_Ioo', simp, grind =] theorem mem_Ioo : x ∈ Ioo a b ↔ a < x ∧ x < b := Iff.rfl
-@[to_dual Ioo_def'] theorem Ioo_def (a b : α) : { x | a < x ∧ x < b } = Ioo a b := rfl
+@[simp, grind =] theorem mem_Ioo : x ∈ Ioo a b ↔ a < x ∧ x < b := Iff.rfl
+theorem Ioo_def (a b : α) : { x | a < x ∧ x < b } = Ioo a b := rfl
 
 /-- `Ico a b` is the left-closed right-open interval $[a, b)$. -/
 def Ico (a b : α) := { x | a ≤ x ∧ x < b }
 
-/-- `Ioc a b` is the left-open right-closed interval $(a, b]$. -/
-@[to_dual existing (reorder := a b)]
-def Ioc (a b : α) := { x | a < x ∧ x ≤ b }
-
-to_dual_insert_cast Ico := by grind
-to_dual_insert_cast Ioc := by grind
-
-@[to_dual mem_Ioc', simp, grind =] theorem mem_Ico : x ∈ Ico a b ↔ a ≤ x ∧ x < b := Iff.rfl
-@[to_dual Ioc_def'] theorem Ico_def (a b : α) : { x | a ≤ x ∧ x < b } = Ico a b := rfl
-
-@[to_dual mem_Ico', simp, grind =] theorem mem_Ioc : x ∈ Ioc a b ↔ a < x ∧ x ≤ b := Iff.rfl
-@[to_dual Ico_def'] theorem Ioc_def (a b : α) : { x | a < x ∧ x ≤ b } = Ioc a b := rfl
-
-/-- `Icc a b` is the left-closed right-closed interval $[a, b]$. -/
-@[to_dual self (reorder := a b)]
-def Icc (a b : α) := { x | a ≤ x ∧ x ≤ b }
-
-to_dual_insert_cast Icc := by grind
-
-@[to_dual mem_Icc', simp, grind =] theorem mem_Icc : x ∈ Icc a b ↔ a ≤ x ∧ x ≤ b := Iff.rfl
-@[to_dual Icc_def'] theorem Icc_def (a b : α) : { x | a ≤ x ∧ x ≤ b } = Icc a b := rfl
+@[simp, grind =] theorem mem_Ico : x ∈ Ico a b ↔ a ≤ x ∧ x < b := Iff.rfl
+theorem Ico_def (a b : α) : { x | a ≤ x ∧ x < b } = Ico a b := rfl
 
 /-- `Iio b` is the left-infinite right-open interval $(-∞, b)$. -/
-@[to_dual /-- `Ioi a` is the left-open right-infinite interval $(a, ∞)$. -/]
 def Iio (b : α) := { x | x < b }
 
-@[to_dual (attr := simp, grind =)] theorem mem_Iio : x ∈ Iio b ↔ x < b := Iff.rfl
-@[to_dual] theorem Iio_def (a : α) : { x | x < a } = Iio a := rfl
+@[simp, grind =] theorem mem_Iio : x ∈ Iio b ↔ x < b := Iff.rfl
+theorem Iio_def (a : α) : { x | x < a } = Iio a := rfl
+
+/-- `Icc a b` is the left-closed right-closed interval $[a, b]$. -/
+def Icc (a b : α) := { x | a ≤ x ∧ x ≤ b }
+
+@[simp, grind =] theorem mem_Icc : x ∈ Icc a b ↔ a ≤ x ∧ x ≤ b := Iff.rfl
+theorem Icc_def (a b : α) : { x | a ≤ x ∧ x ≤ b } = Icc a b := rfl
 
 /-- `Iic b` is the left-infinite right-closed interval $(-∞, b]$. -/
-@[to_dual /-- `Ici a` is the left-closed right-infinite interval $[a, ∞)$. -/]
 def Iic (b : α) := { x | x ≤ b }
 
-@[to_dual (attr := simp, grind =)] theorem mem_Iic : x ∈ Iic b ↔ x ≤ b := Iff.rfl
-@[to_dual] theorem Iic_def (b : α) : { x | x ≤ b } = Iic b := rfl
+@[simp, grind =] theorem mem_Iic : x ∈ Iic b ↔ x ≤ b := Iff.rfl
+theorem Iic_def (b : α) : { x | x ≤ b } = Iic b := rfl
+
+/-- `Ioc a b` is the left-open right-closed interval $(a, b]$. -/
+def Ioc (a b : α) := { x | a < x ∧ x ≤ b }
+
+@[simp, grind =] theorem mem_Ioc : x ∈ Ioc a b ↔ a < x ∧ x ≤ b := Iff.rfl
+theorem Ioc_def (a b : α) : { x | a < x ∧ x ≤ b } = Ioc a b := rfl
+
+/-- `Ici a` is the left-closed right-infinite interval $[a, ∞)$. -/
+def Ici (a : α) := { x | a ≤ x }
+
+@[simp, grind =] theorem mem_Ici : x ∈ Ici a ↔ a ≤ x := Iff.rfl
+theorem Ici_def (a : α) : { x | a ≤ x } = Ici a := rfl
+
+/-- `Ioi a` is the left-open right-infinite interval $(a, ∞)$. -/
+def Ioi (a : α) := { x | a < x }
+
+@[simp, grind =] theorem mem_Ioi : x ∈ Ioi a ↔ a < x := Iff.rfl
+theorem Ioi_def (a : α) : { x | a < x } = Ioi a := rfl
 
 /-- We say that a set `s : Set α` is `OrdConnected` if for all `x y ∈ s` it includes the
 interval `[[x, y]]`. If `α` is a `DenselyOrdered` `ConditionallyCompleteLinearOrder` with
@@ -86,7 +86,5 @@ the `OrderTopology`, then this condition is equivalent to `IsPreconnected s`. If
 class OrdConnected (s : Set α) : Prop where
   /-- `s : Set α` is `OrdConnected` if for all `x y ∈ s` it includes the interval `[[x, y]]`. -/
   out' ⦃x : α⦄ (hx : x ∈ s) ⦃y : α⦄ (hy : y ∈ s) : Icc x y ⊆ s
-
-attribute [to_dual self (reorder := x y, hx hy)] OrdConnected.out'
 
 end Set
