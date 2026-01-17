670f4943d5a508683678ca8ee727f9b75dd7ab41
jonat <jonathanreich100@gmail.com>
2026-01-17 15:44:06 +1100

    feat(LinearAlgebra/FiniteDimensional): add finrank_ker_of_surjective

--- Mathlib/LinearAlgebra/FiniteDimensional/Lemmas.lean
+++ Mathlib/LinearAlgebra/FiniteDimensional/Lemmas.lean
@@ -138,6 +138,13 @@ theorem finrank_range_add_finrank_ker [FiniteDimensional K V] (f : V →ₗ[K] V
   rw [← f.quotKerEquivRange.finrank_eq]
   exact Submodule.finrank_quotient_add_finrank _
 
+
+theorem finrank_ker_of_surjective [FiniteDimensional K V] {f : V →ₗ[K] V₂}
+    (hf : Function.Surjective f) : finrank K (LinearMap.ker f) = finrank K V - finrank K V₂ := by
+  have := f.finrank_range_add_finrank_ker
+  rw [f.range_eq_top.mpr hf, finrank_top] at this
+  omega
+
 lemma ker_ne_bot_of_finrank_lt [FiniteDimensional K V] [FiniteDimensional K V₂] {f : V →ₗ[K] V₂}
     (h : finrank K V₂ < finrank K V) :
     LinearMap.ker f ≠ ⊥ := by
