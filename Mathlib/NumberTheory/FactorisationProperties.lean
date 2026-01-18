9400ad86a3347f45531e28b34119e1e94dab4fdd
metakunt <zugvzuzuguih@eclipso.me>
2026-01-18 16:07:22 +0100

    Infinite abundant numbers

--- Mathlib/NumberTheory/FactorisationProperties.lean
+++ Mathlib/NumberTheory/FactorisationProperties.lean
@@ -5,7 +5,10 @@ Authors: Colin Jones
 -/
 module
 
+public import Mathlib.Algebra.Group.Action.Defs
 public import Mathlib.Algebra.Ring.GeomSum
+public import Mathlib.Algebra.Group.Pointwise.Finset.Scalar
+public import Mathlib.Data.Finset.NatDivisors
 public import Mathlib.NumberTheory.Divisors
 public import Mathlib.Tactic.FinCases
 public import Mathlib.Tactic.Linarith
@@ -70,61 +73,49 @@ def Pseudoperfect (n : ℕ) : Prop :=
 def Weird (n : ℕ) : Prop := Abundant n ∧ ¬ Pseudoperfect n
 
 theorem not_pseudoperfect_iff_forall :
-    ¬ Pseudoperfect n ↔ n = 0 ∨ ∀ s ⊆ properDivisors n, ∑ i ∈ s, i ≠ n := by
-  rw [Pseudoperfect, not_and_or]
-  simp only [not_lt, nonpos_iff_eq_zero, not_exists, not_and, ne_eq]
+    ¬ Pseudoperfect n ↔ n = 0 ∨ ∀ s ⊆ properDivisors n, ∑ i ∈ s, i ≠ n := by grind [Pseudoperfect]
 
 theorem deficient_one : Deficient 1 := zero_lt_one
 theorem deficient_two : Deficient 2 := one_lt_two
 theorem deficient_three : Deficient 3 := by norm_num [Deficient]
 
 theorem abundant_twelve : Abundant 12 := by
-  rw [Abundant, show properDivisors 12 = {1,2,3,4,6} by rfl]
-  simp
+  simp [Abundant, show properDivisors 12 = {1,2,3,4,6} by rfl]
 
 theorem weird_seventy : Weird 70 := by
-  rw [Weird, Abundant, not_pseudoperfect_iff_forall]
-  have h : properDivisors 70 = {1, 2, 5, 7, 10, 14, 35} := by rfl
+  rw [Weird, Abundant, not_pseudoperfect_iff_forall, show properDivisors 70 =
+      {1, 2, 5, 7, 10, 14, 35} by rfl]
   constructor
-  · rw [h]
-    repeat norm_num
-  · rw [h]
-    right
+  · simp
+  · right
     intro s hs
     have hs' := mem_powerset.mpr hs
     fin_cases hs' <;> decide
 
 lemma deficient_iff_not_abundant_and_not_perfect (hn : n ≠ 0) :
     Deficient n ↔ ¬ Abundant n ∧ ¬ Perfect n := by
-  dsimp only [Perfect, Abundant, Deficient]
-  lia
+  grind [Perfect, Abundant, Deficient]
 
 lemma perfect_iff_not_abundant_and_not_deficient (hn : 0 ≠ n) :
     Perfect n ↔ ¬ Abundant n ∧ ¬ Deficient n := by
-  dsimp only [Perfect, Abundant, Deficient]
-  lia
+  grind [Perfect, Abundant, Deficient]
 
 lemma abundant_iff_not_perfect_and_not_deficient (hn : 0 ≠ n) :
     Abundant n ↔ ¬ Perfect n ∧ ¬ Deficient n := by
-  dsimp only [Perfect, Abundant, Deficient]
-  lia
+  grind [Perfect, Abundant, Deficient]
 
 /-- A positive natural number is either deficient, perfect, or abundant -/
 theorem deficient_or_perfect_or_abundant (hn : 0 ≠ n) :
     Deficient n ∨ Abundant n ∨ Perfect n := by
-  dsimp only [Perfect, Abundant, Deficient]
-  lia
+  grind [Perfect, Abundant, Deficient]
 
 theorem Perfect.pseudoperfect (h : Perfect n) : Pseudoperfect n :=
-  ⟨h.2, ⟨properDivisors n, ⟨fun ⦃_⦄ a ↦ a, h.1⟩⟩⟩
+  ⟨h.2, ⟨properDivisors n, ⟨fun _ a ↦ a, h.1⟩⟩⟩
 
 theorem Prime.not_abundant (h : Prime n) : ¬ Abundant n :=
   fun h1 ↦ (h.one_lt.trans h1).ne' (sum_properDivisors_eq_one_iff_prime.mpr h)
 
-theorem Prime.not_weird (h : Prime n) : ¬ Weird n := by
-  simp only [Nat.Weird, not_and_or]
-  left
-  exact h.not_abundant
+theorem Prime.not_weird (h : Prime n) : ¬ Weird n := by grind [Weird, h.not_abundant]
 
 theorem Prime.not_pseudoperfect (h : Prime p) : ¬ Pseudoperfect p := by
   simp_rw [not_pseudoperfect_iff_forall, ← mem_powerset,
@@ -132,7 +123,7 @@ theorem Prime.not_pseudoperfect (h : Prime p) : ¬ Pseudoperfect p := by
   refine Or.inr (fun s hs ↦ ?_)
   fin_cases hs <;>
   simp only [sum_empty, sum_singleton] <;>
-  linarith [Prime.one_lt h]
+  grind [Prime.one_lt h]
 
 theorem Prime.not_perfect (h : Prime p) : ¬ Perfect p := by
   have h1 := Prime.not_pseudoperfect h
@@ -156,7 +147,7 @@ theorem Prime.deficient_pow (h : Prime n) : Deficient (n ^ m) := by
         · rw [← hy]
           exact (Nat.pow_lt_pow_iff_right (Prime.two_le h)).mpr hx
     have h2 : ∑ i ∈ image (fun x => n ^ x) (range m), i = ∑ i ∈ range m, n ^ i := by
-      rw [Finset.sum_image]
+      rw [sum_image]
       rintro x _ y _
       apply pow_injective_of_not_isUnit h.not_isUnit <| Prime.ne_zero h
     rw [Deficient, h1, h2]
@@ -170,9 +161,7 @@ theorem _root_.IsPrimePow.deficient (h : IsPrimePow n) : Deficient n := by
   obtain ⟨p, k, hp, -, rfl⟩ := h
   exact hp.nat_prime.deficient_pow
 
-theorem Prime.deficient (h : Prime n) : Deficient n := by
-  rw [← pow_one n]
-  exact h.deficient_pow
+theorem Prime.deficient (h : Prime n) : Deficient n := (pow_one n) ▸ h.deficient_pow
 
 /-- There exists infinitely many deficient numbers -/
 theorem infinite_deficient : {n : ℕ | n.Deficient}.Infinite := by
@@ -195,7 +184,52 @@ theorem infinite_odd_deficient : {n : ℕ | Odd n ∧ n.Deficient}.Infinite := b
   rw [Set.infinite_iff_exists_gt]
   intro n
   obtain ⟨p, ⟨_, h2⟩⟩ := exists_infinite_primes (max (n + 1) 3)
-  exact ⟨p, Set.mem_setOf.mpr ⟨Prime.odd_of_ne_two h2 (Ne.symm (ne_of_lt (by omega))),
-    Prime.deficient h2⟩, by omega⟩
+  exact ⟨p, Set.mem_setOf.mpr ⟨Prime.odd_of_ne_two h2 (Ne.symm (ne_of_lt (by grind))),
+    Prime.deficient h2⟩, by grind⟩
+
+set_option maxRecDepth 2000 in
+theorem abundant_945 : Abundant 945 := by
+  simp [Abundant, show properDivisors 945 =
+      {1, 3, 5, 7, 9, 15, 21, 27, 35, 45, 63, 105, 135, 189, 315} from rfl]
+
+theorem abundant_iff_sum_divisors : Abundant n ↔ 2 * n < ∑ i ∈ n.divisors, i := by
+  grind [Abundant, sum_divisors_eq_sum_properDivisors_add_self]
+
+theorem abundant_mul [NeZero m] (h : Abundant n) : Abundant (m * n) := by
+  letI : SMul ℕ (Finset ℕ) := Finset.smulFinset
+  rw [abundant_iff_sum_divisors] at *
+  have h1 : 0 < m := pos_of_neZero m
+  calc
+    _ = m * (2 * n) := by grind
+    _ < m • ∑ i ∈ n.divisors, i := (Nat.mul_lt_mul_left h1).mpr h
+    _ = ∑ i ∈ (m • n.divisors), i := by
+      let f := Function.Embedding.mk (fun x : ℕ => m • x) (by
+        intro y z ho
+        simp only [smul_eq_mul, mul_eq_mul_left_iff] at ho
+        grind)
+      have ht : ∑ i ∈ n.divisors.map f, i = ∑ i ∈ n.divisors, m • i := sum_map n.divisors f id
+      rw [← sum_nsmul, ← ht]
+      congr
+      ext z
+      simp [f, mem_smul_finset]
+    _ ≤ _ := by
+      apply sum_le_sum_of_subset
+      intro i h2
+      rw [mem_smul_finset] at h2
+      obtain ⟨ j, ⟨ hd , hj ⟩ ⟩ := h2
+      rw [← hj, divisors_mul, smul_eq_mul]
+      exact mul_mem_mul (mem_divisors_self _ (by grind)) hd
+
+theorem infinite_even_abundant : {n : ℕ | Even n ∧ n.Abundant}.Infinite := by
+  rw [Set.infinite_iff_exists_gt]
+  intro a
+  use (2 * (a + 1)) * 945
+  grind [abundant_mul (m := (2 * (a + 1))) abundant_945]
+
+theorem infinite_odd_abundant : {n : ℕ | Odd n ∧ n.Abundant}.Infinite := by
+  rw [Set.infinite_iff_exists_gt]
+  intro a
+  use (2 * a + 1) * 945
+  grind [abundant_mul (m := (2 * a + 1)) abundant_945]
 
 end Nat
