44d96dd4c63334d8ef8399cb6325b6a72f34fdbc
staroperator <staroperator92@gmail.com>
2026-01-17 22:03:08 -0500

    add

--- Mathlib/LinearAlgebra/Basis/Cardinality.lean
+++ Mathlib/LinearAlgebra/Basis/Cardinality.lean
@@ -116,7 +116,7 @@ theorem infinite_basis_le_maximal_linearIndependent' {ι : Type w} (b : Basis ι
     Cardinal.lift.{w'} #ι ≤ Cardinal.lift.{w} #κ := by
   let Φ := fun k : κ => (b.repr (v k)).support
   have w₁ : #ι ≤ #(Set.range Φ) := by
-    apply Cardinal.le_range_of_union_finset_eq_top
+    apply Cardinal.le_range_of_union_finset_eq_univ
     exact union_support_maximal_linearIndependent_eq_range_basis b v i m
   have w₂ : Cardinal.lift.{w'} #(Set.range Φ) ≤ Cardinal.lift.{w} #κ := Cardinal.mk_range_le_lift
   exact (Cardinal.lift_le.mpr w₁).trans w₂
