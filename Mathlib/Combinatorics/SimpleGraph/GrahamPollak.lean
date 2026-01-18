49f2079517ca9c1d71960dfd2b72523b53040192
Julian Berman <Julian@GrayVines.com>
2026-01-18 12:49:57 -0500

    Move other lemmas.

--- Mathlib/Combinatorics/SimpleGraph/GrahamPollak.lean
+++ Mathlib/Combinatorics/SimpleGraph/GrahamPollak.lean
@@ -19,58 +19,9 @@ public import Mathlib.Data.Matrix.ColumnRowPartitioned
 
 @[expose] public section
 
-namespace SimpleGraph
+namespace SimpleGraph.completeGraph
 
-variable {V : Type*} {G : SimpleGraph V} {u v : V} {α ι : Type*} {a : α} {f : V → ι}
-
-def IsCompleteBipartiteWith (left : Set V) : Prop := G.IsCompleteMultipartiteWith (· ∈ left)
-
-namespace IsCompleteBipartiteWith
-
-variable {left : Set V} (C : G.IsCompleteBipartiteWith left)
-include C
-
-lemma isCompleteMultipartiteWith : G.IsCompleteMultipartiteWith (· ∈ left) := C
-
-lemma adj_iff_not_mem (hv : v ∈ left) : G.Adj v u ↔ u ∉ left := by
-  simp [C.isCompleteMultipartiteWith.adj_iff_ne, hv]
-
-lemma adj_iff_mem (hv : v ∉ left) : G.Adj v u ↔ u ∈ left := by
-  simp [C.isCompleteMultipartiteWith.adj_iff_ne, hv]
-
-lemma neighborSet_eq_of_mem_left (hv : v ∈ left) : G.neighborSet v = leftᶜ := by
-  grind [C.isCompleteMultipartiteWith.neighborSet_eq]
-
-lemma neighborSet_eq_of_not_mem_left (hv : v ∉ left) : G.neighborSet v = left := by
-  grind [C.isCompleteMultipartiteWith.neighborSet_eq]
-
-lemma bipartite : G.IsBipartiteWith left leftᶜ := by
-  refine ⟨disjoint_compl_right, fun v u hadj ↦ ?_⟩
-  grind [C.isCompleteMultipartiteWith.adj_iff_ne.mp hadj]
-
-section finite
-
-variable [Fintype ↑left] [Fintype ↑(G.neighborSet v)]
-
-lemma neighborFinset_eq_of_mem_left [Fintype ↑leftᶜ] (hv : v ∈ left.toFinset) :
-    G.neighborFinset v = leftᶜ.toFinset := by
-  grind [neighborFinset_def, neighborSet_eq_of_mem_left]
-
-lemma neighborFinset_eq_of_not_mem_left (hv : v ∉ left.toFinset) :
-    G.neighborFinset v = left.toFinset := by
-  grind [neighborFinset_def, neighborSet_eq_of_not_mem_left]
-
-end finite
-
-end IsCompleteBipartiteWith
-
-variable (G) in
-def IsCompleteBipartite :=
-  ∃ left, G.IsCompleteBipartiteWith left
-
-section finite
-
-variable [Fintype V] [Fintype α] {𝓁 : TopEdgeLabeling V α}
+variable {V α : Type*} [Fintype V] {G : SimpleGraph V} [Fintype α] {𝓁 : TopEdgeLabeling V α}
 
 open Finset Fintype LinearMap in
 open scoped Matrix in
@@ -86,19 +37,19 @@ theorem card_le_card_add_one_of_forall_IsCompleteBipartite
   classical
   rcases subsingleton_or_nontrivial V
   · grind [card_le_one_iff_subsingleton]
-  by_contra! h
-  let M : Matrix (Fin 1 ⊕ α) V ℝ := Matrix.fromRows
-    (Matrix.replicateCol V ![1])
-    (Matrix.of fun m n ↦ (completeBipartiteOf m).choose.indicator 1 n)
-  obtain ⟨c, hc, hc_nezero⟩ : ∃ x ∈ ker M.toLin', x ≠ 0 := (ker _).ne_bot_iff.mp <| by
-    apply ker_ne_bot_of_finrank_lt
-    simp only [Module.finrank_fintype_fun_eq_card, card_sum, card_unique]
-    grind
-  have h_sum : ∑ v, c v = 0 := by
-    have : (M *ᵥ c) (.inl 0) = 0 := by simp_all
-    simp only [M, Matrix.mulVec, dotProduct] at this
-    aesop
-  · have h_disjoint (u : V) :
+  · by_contra! h
+    let M : Matrix (Fin 1 ⊕ α) V ℝ := Matrix.fromRows
+      (Matrix.replicateCol V ![1])
+      (Matrix.of fun m n ↦ (completeBipartiteOf m).choose.indicator 1 n)
+    obtain ⟨c, hc, hc_nezero⟩ : ∃ x ∈ ker M.toLin', x ≠ 0 := (ker _).ne_bot_iff.mp <| by
+      apply ker_ne_bot_of_finrank_lt
+      simp only [Module.finrank_fintype_fun_eq_card, card_sum, card_unique]
+      grind
+    have h_sum : ∑ v, c v = 0 := by
+      have : (M *ᵥ c) (.inl 0) = 0 := by simp_all
+      simp only [M, Matrix.mulVec, dotProduct] at this
+      aesop
+    have h_disjoint (u : V) :
         ((univ : Finset α) : Set α).PairwiseDisjoint (𝓁.labelGraph · |>.neighborFinset u) := by
       intro
       grind [Finset.disjoint_left, mem_neighborFinset, 𝓁.labelGraph_adj]
@@ -107,8 +58,9 @@ theorem card_le_card_add_one_of_forall_IsCompleteBipartite
       ext v
       simp only [mem_erase, Finset.mem_univ, Finset.mem_biUnion, mem_neighborFinset]
       exact ⟨fun _ ↦ ⟨𝓁 ⟨s(u, v), by tauto⟩, by tauto⟩,
-             by grind only [TopEdgeLabeling.labelGraph_adj]⟩
-    have h_sq : ∑ v, c v ^ 2 = -∑ u, ∑ v ∈ univ.erase u, c u * c v := by simp [← mul_sum, sq, h_sum]
+            by grind only [TopEdgeLabeling.labelGraph_adj]⟩
+    have h_sq : ∑ v, c v ^ 2 = -∑ u, ∑ v ∈ univ.erase u, c u * c v :=
+      by simp [← mul_sum, sq, h_sum]
     have h_zero : ∑ v, c v ^ 2 = 0 := by
       rw [h_sq,
           Finset.sum_congr rfl fun u _ ↦ by rw [h_partition, sum_biUnion <| h_disjoint _],
@@ -144,6 +96,4 @@ theorem card_le_card_add_one_of_forall_IsCompleteBipartite
     simp only [Finset.sum_eq_zero_iff_of_nonneg (fun _ _ ↦ sq_nonneg _), sq_eq_zero_iff] at h_zero
     bound
 
-end finite
-
-end SimpleGraph
+end SimpleGraph.completeGraph
