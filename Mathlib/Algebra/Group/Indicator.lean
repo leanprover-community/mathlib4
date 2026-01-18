7d6d861103bf15eed3e815bab4f693b3a5ee3c6c
Yury G. Kudryashov <urkud@urkud.name>
2026-01-18 00:32:35 -0600

    Make it `@[simp]`, fix compile

--- Mathlib/Algebra/Group/Indicator.lean
+++ Mathlib/Algebra/Group/Indicator.lean
@@ -196,12 +196,12 @@ theorem apply_mulIndicator_symmDiff {g : G → β} (hg : ∀ x, g x⁻¹ = g x)
 
 end Group
 
-/- Relationship with Pi.mulSingle/Pi.single -/
+/-! ### Relationship with `Pi.mulSingle`/`Pi.single` -/
 
 variable {ι : Type*} [DecidableEq ι] {M : Type*} [One M]
 
 /-- On non-dependent functions, `Set.mulIndicator` on a singleton set equals `Pi.mulSingle`. -/
-@[to_additive
+@[to_additive (attr := simp)
   /-- On non-dependent functions, `Set.indicator` on a singleton set equals `Pi.single`. -/]
 theorem mulIndicator_singleton (i : ι) (f : ι → M) :
     Set.mulIndicator {i} f = Pi.mulSingle i (f i) := by
