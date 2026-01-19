719a6b3eefcac47eb2ee9d357529b74eece4ce05
pfaffelh <p.p@stochastik.uni-freiburg.de>
2026-01-19 15:51:34 +0100

    small change

--- Mathlib/Data/Set/Accumulate.lean
+++ Mathlib/Data/Set/Accumulate.lean
@@ -3,6 +3,7 @@ Copyright (c) 2020 Floris van Doorn. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Floris van Doorn, Peter Pfaffelhuber
 -/
+
 module
 
 public import Mathlib.Data.Nat.Lattice
