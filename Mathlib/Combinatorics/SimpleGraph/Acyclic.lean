50649c39e8282be0328d4eeb44c99a917348088b
sunyue <enthusiastical996@gmail.com>
2026-01-21 15:13:16 +0800

    Let G be a tree of order n ≥ 2. Then G has at least two pendent vertices.

--- Mathlib/Combinatorics/SimpleGraph/Acyclic.lean
+++ Mathlib/Combinatorics/SimpleGraph/Acyclic.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2022 Kyle Miller. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Kyle Miller , Yue Sun and Jiayi Huang
+Authors: Kyle Miller , Yue Sun , Jiayi Huang
 -/
 module
 
