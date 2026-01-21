fcd587442ff2555adad6089e4dd75f77074d2e5a
Eric Wieser <efw@google.com>
2026-01-21 00:54:12 +0000

    better API

--- Mathlib/Topology/MetricSpace/Congruence.lean
+++ Mathlib/Topology/MetricSpace/Congruence.lean
@@ -97,16 +97,19 @@ lemma index_map (h : v₁ ≅ v₂) (f : ι' → ι) : (v₁ ∘ f) ≅ (v₂ �
   simpa [(EquivLike.toEquiv f).right_inv i₁, (EquivLike.toEquiv f).right_inv i₂]
     using edist_eq h ((EquivLike.toEquiv f).symm i₁) ((EquivLike.toEquiv f).symm i₂)
 
-lemma comp_isometry {f : P₁ → P₂} (hf : Isometry f) : f ∘ v₁ ≅ v₁ :=
-  fun _ _ ↦ hf _ _
+lemma comp_left {f : P₁ → P₃} (hf : Isometry f) (h : v₁ ≅ v₂) : f ∘ v₁ ≅ v₂ :=
+  .trans (fun _ _ ↦ hf _ _) h
+
+lemma comp_right {f : P₂ → P₃} (hf : Isometry f) (h : v₁ ≅ v₂) : v₁ ≅ f ∘ v₂ :=
+  .trans h (.symm <| fun _ _ ↦ hf _ _)
 
 @[simp]
-lemma comp_left_isometry_iff {f : P₁ → P₃} (hf : Isometry f) : f ∘ v₁ ≅ v₂ ↔ v₁ ≅ v₂ :=
-  ⟨(comp_isometry hf).symm.trans, (comp_isometry hf).trans⟩
+lemma comp_left_iff {f : P₁ → P₃} (hf : Isometry f) : f ∘ v₁ ≅ v₂ ↔ v₁ ≅ v₂ :=
+  ⟨.trans <| .comp_right hf (.refl _), .comp_left hf⟩
 
 @[simp]
-lemma comp_right_isometry_iff {f : P₂ → P₃} (hf : Isometry f) : v₁ ≅ f ∘ v₂ ↔ v₁ ≅ v₂ := by
-  rw [congruent_comm, comp_left_isometry_iff hf, congruent_comm]
+lemma comp_right_iff {f : P₂ → P₃} (hf : Isometry f) : v₁ ≅ f ∘ v₂ ↔ v₁ ≅ v₂ := by
+  rw [congruent_comm, comp_left_iff hf, congruent_comm]
 
 end Congruent
 
