19f1cc1c110d5666973c99bf3afe79daa6c901ff
leibniz-rs <matteo.cipollina@yahoo.it>
2026-01-21 12:40:52 +0100

    Update Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -7247,6 +7247,7 @@ public import Mathlib.Topology.Maps.Proper.Basic
 public import Mathlib.Topology.Maps.Proper.CompactlyGenerated
 public import Mathlib.Topology.Maps.Proper.UniversallyClosed
 public import Mathlib.Topology.MetricSpace.Algebra
+public import Mathlib.Topology.MetricSpace.Annulus
 public import Mathlib.Topology.MetricSpace.Antilipschitz
 public import Mathlib.Topology.MetricSpace.Basic
 public import Mathlib.Topology.MetricSpace.Bilipschitz
