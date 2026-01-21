ae76bb14952b0bbf4110d4d2eabd7362ad28a7d6
Chris Birkbeck <c.birkbeck@uea.ac.uk>
2026-01-21 14:43:35 +0000

    save

--- Mathlib/NumberTheory/ModularForms/DedekindEta.lean
+++ Mathlib/NumberTheory/ModularForms/DedekindEta.lean
@@ -124,4 +124,35 @@ theorem differentiableAt_eta_of_mem_upperHalfPlaneSet {z : ℂ} (hz : z ∈ ℍ�
   filter_upwards with b
   simpa [Finset.prod_fn] using DifferentiableOn.finset_prod (by fun_prop)
 
+lemma logDeriv_q_term (z : ℍ) : logDeriv (𝕢 24) ↑z  =  2 * ↑π * I / 24 := by
+  have : (𝕢 24) = (fun z ↦ cexp (z)) ∘ (fun z => (2 * ↑π * I / 24) * z)  := by
+    ext y
+    simp only [Periodic.qParam, ofReal_ofNat, comp_apply]
+    ring_nf
+  rw [this, logDeriv_comp (by fun_prop) (by fun_prop), deriv_const_mul _ (by fun_prop)]
+  simp only [LogDeriv_exp, Pi.one_apply, deriv_id'', mul_one, one_mul]
+
+lemma eta_logDeriv (z : ℍ) : logDeriv ModularForm.eta z = (π * I / 12) * E2 z := by
+  unfold ModularForm.eta
+  rw [logDeriv_mul (UpperHalfPlane.coe z) (by simp [ne_eq, exp_ne_zero, not_false_eq_true,
+    Periodic.qParam]) (eta_tprod_ne_zero z.2) (by fun_prop) (differentiableAt_eta_tprod z.2)]
+  have HG := logDeriv_tprod_eq_tsum isOpen_upperHalfPlaneSet (x := z)
+    (f := fun n x => 1 - eta_q n x) (fun i ↦ one_sub_eta_q_ne_zero i z.2)
+    (by simp_rw [eta_q_eq_pow]; fun_prop) (summable_log_deriv_one_sub_eta_q z.2)
+    (multipliableLocallyUniformlyOn_eta ) (eta_tprod_ne_zero z.2)
+  rw [show z.1 = UpperHalfPlane.coe z by rfl] at HG
+  simp only [logDeriv_q_term z, HG, tsum_logDeriv_eta_q z, E2, one_div,
+    mul_inv_rev, Pi.smul_apply, smul_eq_mul]
+  rw [G2_q_exp, riemannZeta_two, ← tsum_pow_div_one_sub_eq_tsum_sigma
+    (by apply UpperHalfPlane.norm_exp_two_pi_I_lt_one z), mul_sub, sub_eq_add_neg, mul_add]
+  congr 1
+  · field_simp
+    ring
+  · field_simp [tsum_pnat_eq_tsum_succ (f := fun n ↦ n * cexp (2 * π * I * z) ^ n
+      / (1 - cexp (2 * π * I * z) ^ n )), eta_q_eq_pow]
+    simp_rw [← tsum_mul_left, ← tsum_mul_right, ← tsum_neg]
+    congr
+    ext n
+    ring_nf
+
 end ModularForm
