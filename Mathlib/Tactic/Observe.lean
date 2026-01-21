47f880c7b3da1782d7b9003f524bbd27d7783a35
Anne Baanen <2116570+Vierkantor@users.noreply.github.com>
2026-01-21 19:02:42 +0000

    chore(Tactic/Observe): improve `observe` docstring (#34198)
    This PR (re)writes the docstring for the `observe` tactic to consistently match the [official style guide](https://github.com/leanprover/lean4/blob/master/doc/style.md#tactics), to make sure it is complete while not getting too long.
    
    It turns out there is the syntax `observe hp : p using e1, e2, ...` which does not seem to do anything. (This syntax has been there since the Lean 3 port.) I left a TODO comment, but we can also drop the syntax altogether.
    
    Co-authored-by: Anne C.A. Baanen <vierkantor@vierkantor.com>

--- Mathlib/Tactic/Observe.lean
+++ Mathlib/Tactic/Observe.lean
@@ -22,14 +22,16 @@ namespace Mathlib.Tactic.LibrarySearch
 
 open Lean Meta Elab Tactic Meta.Tactic.TryThis LibrarySearch
 
-/-- `observe hp : p` asserts the proposition `p`, and tries to prove it using `exact?`.
+/-- `observe hp : p` asserts the proposition `p` as a hypothesis named `hp`, and tries to prove it
+using `exact?`.
 If no proof is found, the tactic fails.
 In other words, this tactic is equivalent to `have hp : p := by exact?`.
 
-If `hp` is omitted, then the placeholder `this` is used.
-
-The variant `observe? hp : p` will emit a trace message of the form `have hp : p := proof_term`.
-This may be particularly useful to speed up proofs. -/
+* `observe : p` uses the name `this` for the new hypothesis.
+* `observe? hp : p` will emit a trace message of the form `have hp : p := proof_term`.
+  This may be particularly useful to speed up proofs.
+-/
+-- TODO: the syntax `observe hp : p using e1, e2, ...` exists but does not appear to do anything.
 syntax (name := observe) "observe" "?"? (ppSpace ident)? " : " term
   (" using " (colGt term),+)? : tactic
 
