5724eb99115af72ec9325b930061ef47798126ed
leibniz-rs <matteo.cipollina@yahoo.it>
2026-01-21 15:07:52 +0100

    Update IntervalSucc.lean

--- Mathlib/Order/SuccPred/IntervalSucc.lean
+++ Mathlib/Order/SuccPred/IntervalSucc.lean
@@ -56,20 +56,14 @@ theorem biUnion_Ici_Ioc_map_succ [SuccOrder α] [IsSuccArchimedean α] [LinearOr
 theorem iUnion_Ico_map_succ_eq_Ici [OrderBot α] [SuccOrder α] [IsSuccArchimedean α] [LinearOrder β]
     {f : α → β} (hf : ∀ a, f ⊥ ≤ f a) (h2f : ¬BddAbove (range f)) :
     (⋃ a : α, Ico (f a) (f (succ a))) = Ici (f ⊥) := by
-  have hf' : ∀ i ∈ Ici (⊥ : α), f ⊥ ≤ f i := fun i _ => hf i
-  have h2f' : ¬BddAbove (f '' Ici (⊥ : α)) := by
-    simpa [Set.image_univ, Set.Ici_bot] using h2f
-  simpa [Set.Ici_bot] using
-    (biUnion_Ici_Ico_map_succ (α := α) (β := β) (f := f) (a := ⊥) hf' h2f')
+  have h2f' : ¬BddAbove (f '' Ici (⊥ : α)) := by simpa [Set.image_univ, Set.Ici_bot] using h2f
+  simpa [Set.Ici_bot] using (biUnion_Ici_Ico_map_succ (a := (⊥ : α)) (fun i _ ↦ hf i) h2f')
 
 theorem iUnion_Ioc_map_succ_eq_Ioi [OrderBot α] [SuccOrder α] [IsSuccArchimedean α] [LinearOrder β]
     {f : α → β} (hf : ∀ a, f ⊥ ≤ f a) (h2f : ¬BddAbove (range f)) :
     (⋃ a : α, Ioc (f a) (f (succ a))) = Ioi (f ⊥) := by
-  have hf' : ∀ i ∈ Ici (⊥ : α), f ⊥ ≤ f i := fun i _ => hf i
-  have h2f' : ¬BddAbove (f '' Ici (⊥ : α)) := by
-    simpa [Set.image_univ, Set.Ici_bot] using h2f
-  simpa [Set.Ici_bot] using
-    (biUnion_Ici_Ioc_map_succ (α := α) (β := β) (f := f) (a := ⊥) hf' h2f')
+  have h2f' : ¬BddAbove (f '' Ici (⊥ : α)) := by simpa [Set.image_univ, Set.Ici_bot] using h2f
+  simpa [Set.Ici_bot] using (biUnion_Ici_Ioc_map_succ (a := (⊥ : α)) (fun i _ ↦ hf i) h2f')
 
 namespace Monotone
 
