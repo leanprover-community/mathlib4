003dc90e550dfe078c944cf08f7d1e3d700ea1ce
Matteo Cipollina <matteo.cipollina@yahoo.it>
2026-01-21 15:34:23 +0100

    Update Mathlib/Topology/MetricSpace/Annulus.lean
    Co-authored-by: Michael Rothgang <rothgang@math.uni-bonn.de>

--- Mathlib/Topology/MetricSpace/Annulus.lean
+++ Mathlib/Topology/MetricSpace/Annulus.lean
@@ -52,7 +52,9 @@ def annulusIoi (x : X) (r : ℝ) : Set X := (fun y : X => dist y x) ⁻¹' Ioi r
 /-- The exterior `{y | dist y x ∈ Set.Ici r}` (complement of `ball x r`). -/
 def annulusIci (x : X) (r : ℝ) : Set X := (fun y : X => dist y x) ⁻¹' Ici r
 
-lemma annulusIoo_eq {x : X} {r R : ℝ} :
+variable {x : X} {r R : ℝ}
+
+lemma annulusIoo_eq :
     annulusIoo x r R = ball x R ∩ (closedBall x r)ᶜ := by
   ext y
   simp [annulusIoo, Metric.ball, Metric.closedBall, and_comm]
