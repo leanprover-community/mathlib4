c2ea65fa62a599f3ba7132859eb7300a8878891a
bwangpj <70694994+bwangpj@users.noreply.github.com>
2026-01-21 10:22:50 +0800

    Update Equiv.lean

--- Mathlib/Topology/Algebra/Algebra/Equiv.lean
+++ Mathlib/Topology/Algebra/Algebra/Equiv.lean
@@ -131,7 +131,17 @@ instance : Coe (A ≃A[R] B) (A ≃L[R] B) := ⟨toContinuousLinearEquiv⟩
 
 @[simp]
 theorem toContinuousLinearEquiv_apply (e : A ≃A[R] B) (a : A) :
-  e.toContinuousLinearEquiv a = e a := rfl
+    e.toContinuousLinearEquiv a = e a := rfl
+
+@[simp]
+theorem toContinuousLinearEquiv_toContinuousLinearMap_eq (e : A ≃A[R] B) :
+    e.toContinuousLinearEquiv.toContinuousLinearMap
+    = e.toContinuousAlgHom.toContinuousLinearMap := rfl
+
+@[simp]
+theorem toContinuousLinearEquiv_toLinearEquiv_eq (e : A ≃A[R] B) :
+    e.toContinuousLinearEquiv.toLinearEquiv
+    = e.toAlgEquiv.toLinearEquiv := rfl
 
 theorem isOpenMap (e : A ≃A[R] B) : IsOpenMap e :=
   e.toHomeomorph.isOpenMap
@@ -194,6 +204,10 @@ theorem coe_refl : refl R A = ContinuousAlgHom.id R A := rfl
 @[simp]
 theorem coe_refl' : ⇑(refl R A) = id := rfl
 
+@[simp]
+theorem refl_toContinuousLinearEquiv :
+    (refl R A).toContinuousLinearEquiv = refl R A := rfl
+
 variable {R A}
 
 /-- The inverse of a continuous algebra equivalence. -/
@@ -225,6 +239,10 @@ theorem symm_toAlgEquiv (e : A ≃A[R] B) : e.symm.toAlgEquiv = e.toAlgEquiv.sym
 @[simp]
 theorem symm_toHomeomorph (e : A ≃A[R] B) : e.symm.toHomeomorph = e.toHomeomorph.symm := rfl
 
+@[simp]
+theorem symm_toContinuousLinearEquiv (e : A ≃A[R] B) :
+    e.symm.toContinuousLinearEquiv = e.toContinuousLinearEquiv.symm := rfl
+
 theorem symm_map_nhds_eq (e : A ≃A[R] B) (a : A) : Filter.map e.symm (𝓝 (e a)) = 𝓝 a :=
   e.toHomeomorph.symm_map_nhds_eq a
 
@@ -239,6 +257,11 @@ def trans (e₁ : A ≃A[R] B) (e₂ : B ≃A[R] C) : A ≃A[R] C where
 theorem trans_toAlgEquiv (e₁ : A ≃A[R] B) (e₂ : B ≃A[R] C) :
     (e₁.trans e₂).toAlgEquiv = e₁.toAlgEquiv.trans e₂.toAlgEquiv :=
   rfl
+
+@[simp]
+theorem trans_toContinuousLinearEquiv (e₁ : A ≃A[R] B) (e₂ : B ≃A[R] C) :
+    (e₁.trans e₂).toContinuousLinearEquiv
+    = e₁.toContinuousLinearEquiv.trans e₂.toContinuousLinearEquiv := rfl
 
 @[simp]
 theorem trans_apply (e₁ : A ≃A[R] B) (e₂ : B ≃A[R] C) (a : A) :
