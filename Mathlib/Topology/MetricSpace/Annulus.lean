0438fa6fb8225420d81b8231b97745bd46236670
leibniz-rs <matteo.cipollina@yahoo.it>
2026-01-21 14:04:49 +0100

    Update Annulus.lean

--- Mathlib/Topology/MetricSpace/Annulus.lean
+++ Mathlib/Topology/MetricSpace/Annulus.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2024 James Sundstrom. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: James Sundstrom, Matteo Cipollina
+Authors: James Sundstrom
 -/
 module
 
