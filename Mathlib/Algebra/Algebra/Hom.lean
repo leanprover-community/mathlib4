eb57db99d7b1f9ed8e4dd2f5fedc8d5427b48ddb
Leonid Ryvkin <ryvkin@gmx.com>
2026-01-22 18:01:27 +0100

    Update Mathlib/Algebra/Algebra/Hom.lean
    Co-authored-by: Oliver Nash <7734364+ocfnash@users.noreply.github.com>

--- Mathlib/Algebra/Algebra/Hom.lean
+++ Mathlib/Algebra/Algebra/Hom.lean
@@ -280,7 +280,7 @@ theorem comp_assoc (φ₁ : C →ₐ[R] D) (φ₂ : B →ₐ[R] C) (φ₃ : A �
   rfl
 
 instance {φ₁ : B →ₐ[R] C} {φ₂ : A →ₐ[R] B} :
-    RingHomCompTriple (φ₂.toRingHom) φ₁.toRingHom  (φ₁.comp φ₂).toRingHom := ⟨rfl⟩
+    RingHomCompTriple φ₂.toRingHom φ₁.toRingHom (φ₁.comp φ₂).toRingHom := ⟨rfl⟩
 
 /-- R-Alg ⥤ R-Mod -/
 def toLinearMap : A →ₗ[R] B where
