df9fbaa418cb1e1c159c0cec5246f5dc6fbbdbd7
Parcly Taxel <reddeloostw@gmail.com>
2026-01-22 15:54:49 +0800

    there is room to add more about the AGM

--- Mathlib.lean
+++ Mathlib.lean
@@ -2109,7 +2109,7 @@ public import Mathlib.Analysis.Real.Pi.Leibniz
 public import Mathlib.Analysis.Real.Pi.Wallis
 public import Mathlib.Analysis.Real.Spectrum
 public import Mathlib.Analysis.Seminorm
-public import Mathlib.Analysis.SpecialFunctions.AGM
+public import Mathlib.Analysis.SpecialFunctions.AGM.Basic
 public import Mathlib.Analysis.SpecialFunctions.Arcosh
 public import Mathlib.Analysis.SpecialFunctions.Arsinh
 public import Mathlib.Analysis.SpecialFunctions.Artanh
