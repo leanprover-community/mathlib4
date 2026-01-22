bf2c719fc2783dcd879fcb78d56e98db9f477b36
Hang Lu Su <hanglu.su@pm.me>
2026-01-22 21:40:54 +0100

    changed import on `EckmannHilton.lean` to not import all `GroupTheory defs`

--- Mathlib/GroupTheory/EckmannHilton.lean
+++ Mathlib/GroupTheory/EckmannHilton.lean
@@ -5,8 +5,12 @@ Authors: Johan Commelin, Kenny Lau, Robert Y. Lewis
 -/
 module
 
-public import Mathlib.Algebra.Group.Defs
-
+public import Mathlib.GroupTheory.Exponent
+public import Mathlib.GroupTheory.FiniteAbelian.Basic
+public import Mathlib.GroupTheory.FiniteAbelian.Duality
+public import Mathlib.GroupTheory.Finiteness
+public import Mathlib.GroupTheory.FixedPointFree
+public import Mathlib.GroupTheory.Frattini
 /-!
 # Eckmann-Hilton argument
 
