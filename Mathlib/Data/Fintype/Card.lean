b8766be23fb3f2e94266085867e27fe49e9c3ac7
jonat <jonathanreich100@gmail.com>
2026-01-23 01:45:40 +1100

    refactor: move not_injective_of_card_lt to Card.lean next to card_le_of_injective

--- Mathlib/Data/Fintype/Card.lean
+++ Mathlib/Data/Fintype/Card.lean
@@ -229,6 +229,12 @@ variable [Fintype α] [Fintype β]
 theorem card_le_of_injective (f : α → β) (hf : Function.Injective f) : card α ≤ card β :=
   Finset.card_le_card_of_injOn f (fun _ _ => Finset.mem_univ _) fun _ _ _ _ h => hf h
 
+/-- If `card β < card α`, then `f : α → β` is not injective.
+This is the contrapositive of `card_le_of_injective`. -/
+theorem not_injective_of_card_lt (f : α → β) (h : card β < card α) :
+    ¬Function.Injective f := fun hinj =>
+  Nat.not_lt.mpr (card_le_of_injective f hinj) h
+
 theorem card_le_of_embedding (f : α ↪ β) : card α ≤ card β :=
   card_le_of_injective f f.2
 
