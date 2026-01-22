e72cbc48d181566ab8041b38b8a64f68c115fd0b
leibniz-rs <mcipollina@recognitionphysics.org>
2026-01-22 16:17:09 +0100

    Update Divisor.lean

--- Mathlib/Analysis/Complex/Divisor.lean
+++ Mathlib/Analysis/Complex/Divisor.lean
@@ -3,6 +3,7 @@ Copyright (c) 2026 Jonathan Washburn. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Matteo Cipollina, Jonathan Washburn
 -/
+
 import Mathlib.Analysis.Complex.CanonicalProduct
 import Mathlib.Analysis.Complex.DivisorIndex
 import Mathlib.Analysis.Complex.DivisorConvergence
