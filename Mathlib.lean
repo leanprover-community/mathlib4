c8ea13583a70170eb6554dfe6c0d0b577e74de7f
Brian-Nugent <bnugent@uw.edu>
2026-01-22 17:15:32 -0700

    fix(Mathlib.lean): reorder import for AddCommGrpCat in sheaves

--- Mathlib.lean
+++ Mathlib.lean
@@ -7406,8 +7406,8 @@ public import Mathlib.Topology.Sets.Compacts
 public import Mathlib.Topology.Sets.OpenCover
 public import Mathlib.Topology.Sets.Opens
 public import Mathlib.Topology.Sets.Order
-public import Mathlib.Topology.Sheaves.AddCommGrpCat
 public import Mathlib.Topology.Sets.VietorisTopology
+public import Mathlib.Topology.Sheaves.AddCommGrpCat
 public import Mathlib.Topology.Sheaves.Alexandrov
 public import Mathlib.Topology.Sheaves.CommRingCat
 public import Mathlib.Topology.Sheaves.Forget
