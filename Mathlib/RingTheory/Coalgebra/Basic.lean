f714513a7d797c889e3ba1ffb60d9c91dcc3a1b1
Monica Omar <2497250a@research.gla.ac.uk>
2026-01-23 14:23:50 +0000

    cleanup

--- Mathlib/RingTheory/Coalgebra/Basic.lean
+++ Mathlib/RingTheory/Coalgebra/Basic.lean
@@ -369,9 +369,12 @@ end DFinsupp
 
 namespace Finsupp
 variable (R : Type u) (ι : Type v) (A : Type w)
-variable [CommSemiring R] [AddCommMonoid A] [Module R A] [Coalgebra R A]
+variable [CommSemiring R] [AddCommMonoid A] [Module R A]
 
 open LinearMap
+
+section coalgebraStruct
+variable [CoalgebraStruct R A]
 
 noncomputable instance instCoalgebraStruct : CoalgebraStruct R (ι →₀ A) where
   comul := Finsupp.lsum R fun i =>
@@ -403,6 +406,10 @@ theorem comul_comp_lapply (i : ι) :
 
 @[simp] theorem counit_comp_lsingle (i : ι) : counit ∘ₗ (lsingle i : A →ₗ[R] _) = counit := by
   ext; simp
+
+end coalgebraStruct
+
+variable [Coalgebra R A]
 
 /-- The `R`-module whose elements are functions `ι → A` which are zero on all but finitely many
 elements of `ι` has a coalgebra structure. The coproduct `Δ` is given by `Δ(fᵢ a) = fᵢ a₁ ⊗ fᵢ a₂`
