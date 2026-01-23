bf29c24e485ffe95819341362b9050b90e7413a9
Salvatore Mercuri <47568553+smmercuri@users.noreply.github.com>
2026-01-23 15:39:18 +0000

    Update Mathlib/Analysis/Normed/Field/TransferInstance.lean
    Co-authored-by: Michael Rothgang <rothgang@math.uni-bonn.de>

--- Mathlib/Analysis/Normed/Field/TransferInstance.lean
+++ Mathlib/Analysis/Normed/Field/TransferInstance.lean
@@ -13,7 +13,7 @@ public import Mathlib.Topology.MetricSpace.TransferInstance
 /-!
 # Transfer normed algebraic structures across `Equiv`s
 
-In this file, we transfer a normed field across an equivalence.
+In this file, we transfer a normed field structure across an equivalence.
 This continues the pattern set in `Mathlib/Algebra/Module/TransferInstance.lean`.
 -/
 
