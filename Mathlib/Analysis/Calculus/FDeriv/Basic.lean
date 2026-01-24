7478da8a1332a9ce7241be4bb5cc831ba80f3cc2
Eric Wieser <efw@google.com>
2026-01-24 00:55:40 +0000

    exact

--- Mathlib/Analysis/Calculus/FDeriv/Basic.lean
+++ Mathlib/Analysis/Calculus/FDeriv/Basic.lean
@@ -638,7 +638,7 @@ theorem HasFDerivAtFilter.tendsto_nhds (hL : L ≤ 𝓝 x) (h : HasFDerivAtFilte
   have h_sub : Tendsto (fun z ↦ z - x) L (𝓝 0) := tendsto_sub_nhds_zero_iff.mpr hL
   have h_rem : Filter.Tendsto (fun z ↦ f z - f x - f' (z - x)) L (𝓝 0) := by
     rw [← isLittleOTVS_one (𝕜 := 𝕜)] at h_sub ⊢
-    refine h.1.trans h_sub
+    exact h.1.trans h_sub
   simpa using h_rem.add ((f'.continuous.tendsto 0).comp h_sub)
 
 theorem HasFDerivWithinAt.continuousWithinAt (h : HasFDerivWithinAt f f' s x) :
