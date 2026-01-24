ff4d38e34300c51c2b197bcae261c018f6ab6998
Rmal <97214596+CoolRmal@users.noreply.github.com>
2026-01-23 22:38:23 -0500

    resolve merge conflict

--- Mathlib/MeasureTheory/Integral/IntegralEqImproper.lean
+++ Mathlib/MeasureTheory/Integral/IntegralEqImproper.lean
@@ -615,7 +615,7 @@ theorem iicIntegral_tendsto_zero (b : ℝ) (hfi : IntegrableOn f (Iic b) μ)
     Tendsto (fun i => ∫ x in Iic (a i), f x ∂μ) l (𝓝 0) := by
   have : ∀ᶠ i in l, ∫ x in Iic b, f x ∂μ - ∫ x in a i..b, f x ∂μ = ∫ x in Iic (a i), f x ∂μ := by
     filter_upwards [ha.eventually_mem (Iic_mem_atBot b)] with i hi
-    rw [sub_eq_iff_sub_eq, intervalIntegral.integral_Iic_sub_Iic (hfi.mono_set
+    rw [sub_eq_iff_comm, intervalIntegral.integral_Iic_sub_Iic (hfi.mono_set
       (Iic_subset_Iic.2 hi)) hfi]
   exact Tendsto.congr' this (sub_self (∫ x in Iic b, f x ∂μ) ▸ (Tendsto.const_sub _ <|
     intervalIntegral_tendsto_integral_Iic b hfi ha))
