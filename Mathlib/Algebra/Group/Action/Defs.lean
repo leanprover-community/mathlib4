5310dca68ef72598b6fe248c56bf0af35c97c67e
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2026-01-24 22:58:25 +0000

    undeprecate Mul.toSMul and increase prio

--- Mathlib/Algebra/Group/Action/Defs.lean
+++ Mathlib/Algebra/Group/Action/Defs.lean
@@ -53,20 +53,9 @@ open Function (Injective Surjective)
 
 variable {M N G H α β γ δ : Type*}
 
-attribute [to_additive Add.toVAdd /-- See also `AddMonoid.toAddAction` -/] instSMulOfMul
-
--- see Note [lower instance priority]
-/-- See also `Monoid.toMulAction` and `MulZeroClass.toSMulWithZero`. -/
-@[deprecated instSMulOfMul (since := "2025-10-18")]
-def Mul.toSMul (α : Type*) [Mul α] : SMul α α := ⟨(· * ·)⟩
-
--- I don't know the right way to do this because `instSMulOfMul` has priority 910 in core
--- so let me make another instance of the same thing with high priority just for
--- benchmarking purposes
-
 /-- See also `Monoid.toMulAction` and `MulZeroClass.toSMulWithZero`. -/
 @[to_additive /-- See also `AddMonoid.toAddAction` -/]
-instance (priority := high) Mul.toSMul' (α : Type*) [Mul α] : SMul α α := ⟨(· * ·)⟩
+instance (priority := 1100) Mul.toSMul (α : Type*) [Mul α] : SMul α α := ⟨(· * ·)⟩
 
 /-- Like `Mul.toSMul`, but multiplies on the right.
 
