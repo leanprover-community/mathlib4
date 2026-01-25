d56a30e06bfd4426790fb317dfede9cd41f2bcd4
Christian Merten <136261474+chrisflav@users.noreply.github.com>
2026-01-25 15:24:39 +0000

    feat(AlgebraicGeometry): morphisms of finite type induce essentially of finite type stalk maps (#34101)

--- Mathlib.lean
+++ Mathlib.lean
@@ -6330,6 +6330,7 @@ public import Mathlib.RingTheory.Regular.Flat
 public import Mathlib.RingTheory.Regular.IsSMulRegular
 public import Mathlib.RingTheory.Regular.RegularSequence
 public import Mathlib.RingTheory.RingHom.Bijective
+public import Mathlib.RingTheory.RingHom.EssFiniteType
 public import Mathlib.RingTheory.RingHom.Etale
 public import Mathlib.RingTheory.RingHom.FaithfullyFlat
 public import Mathlib.RingTheory.RingHom.Finite
