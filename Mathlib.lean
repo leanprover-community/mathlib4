0cc5558042c39a705efe81654deb3004c943581a
Parcly Taxel <reddeloostw@gmail.com>
2026-01-25 13:36:50 +0800

    feat: integral representation of the AGM

--- Mathlib.lean
+++ Mathlib.lean
@@ -2114,6 +2114,8 @@ public import Mathlib.Analysis.Real.Pi.Leibniz
 public import Mathlib.Analysis.Real.Pi.Wallis
 public import Mathlib.Analysis.Real.Spectrum
 public import Mathlib.Analysis.Seminorm
+public import Mathlib.Analysis.SpecialFunctions.AGM.Basic
+public import Mathlib.Analysis.SpecialFunctions.AGM.Integral
 public import Mathlib.Analysis.SpecialFunctions.Arcosh
 public import Mathlib.Analysis.SpecialFunctions.Arsinh
 public import Mathlib.Analysis.SpecialFunctions.Artanh
