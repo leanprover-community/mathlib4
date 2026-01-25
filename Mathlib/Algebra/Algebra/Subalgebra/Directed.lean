fad07457dfddcaf8fbae7983286530d274d66e1e
Boris Bilich <17164378+bilichboris@users.noreply.github.com>
2026-01-25 13:01:35 +0000

    feat(Algebra/Algebra/Subalgebra): generalize iSupLift to T ≤ iSup K (#34114)
    This PR generalizes `Subalgebra.iSupLift` (in `Mathlib/Algebra/Algebra/Subalgebra/Directed.lean`) by weakening the hypothesis `hT : T = iSup K` to the more flexible assumption `hT : T ≤ iSup K`. 
    
    This closes a TODO comment in the file.

--- Mathlib/Algebra/Algebra/Subalgebra/Directed.lean
+++ Mathlib/Algebra/Algebra/Subalgebra/Directed.lean
@@ -40,55 +40,70 @@ theorem coe_iSup_of_directed (dir : Directed (· ≤ ·) K) : ↑(iSup K) = ⋃ 
 
 variable (K)
 
--- TODO: turn `hT` into an assumption `T ≤ iSup K`. That's what `Set.iUnionLift` needs
 /-- Define an algebra homomorphism on a directed supremum of subalgebras by defining
 it on each subalgebra, and proving that it agrees on the intersection of subalgebras. -/
 noncomputable def iSupLift (dir : Directed (· ≤ ·) K) (f : ∀ i, K i →ₐ[R] B)
     (hf : ∀ (i j : ι) (h : K i ≤ K j), f i = (f j).comp (inclusion h))
-    (T : Subalgebra R A) (hT : T = iSup K) : ↥T →ₐ[R] B :=
-  { toFun := Set.iUnionLift (fun i => ↑(K i)) (fun i x => f i x)
-        (fun i j x hxi hxj => by
-          let ⟨k, hik, hjk⟩ := dir i j
-          dsimp
-          rw [hf i k hik, hf j k hjk]
-          rfl)
-        (T : Set A) (by rw [hT, coe_iSup_of_directed dir])
-    map_one' := by apply Set.iUnionLift_const _ (fun _ => 1) <;> simp
-    map_zero' := by apply Set.iUnionLift_const _ (fun _ => 0) <;> simp
-    map_mul' := by
-      subst hT;
-      apply Set.iUnionLift_binary (coe_iSup_of_directed dir) dir _ (fun _ => (· * ·)) <;> simp
-    map_add' := by
-      subst hT;
-      apply Set.iUnionLift_binary (coe_iSup_of_directed dir) dir _ (fun _ => (· + ·)) <;> simp
-    commutes' := fun r => by apply Set.iUnionLift_const _ (fun _ => algebraMap R _ r) <;> simp }
+    (T : Subalgebra R A) (hT : T ≤ iSup K) : ↥T →ₐ[R] B :=
+by
+  let compat :
+      ∀ (i j) (x : A) (hxi : x ∈ (K i : Set A)) (hxj : x ∈ (K j : Set A)),
+        f i ⟨x, hxi⟩ = f j ⟨x, hxj⟩ := by
+    intro i j x hxi hxj
+    rcases dir i j with ⟨k, hik, hjk⟩
+    simp [hf i k hik, hf j k hjk]
+  let liftSup : ((iSup K : Subalgebra R A)) →ₐ[R] B :=
+    { toFun :=
+        Set.iUnionLift (fun i => ↑(K i)) (fun i x => f i x) compat
+          ((iSup K : Subalgebra R A) : Set A)
+          (le_of_eq <| coe_iSup_of_directed (K := K) dir)
+      map_one' := by
+        dsimp
+        exact Set.iUnionLift_const _ (fun i : ι => (1 : K i)) (fun _ => rfl) _ (by simp)
+      map_zero' := by
+        dsimp
+        exact Set.iUnionLift_const _ (fun i : ι => (0 : K i)) (fun _ => rfl) _ (by simp)
+      map_mul' := by
+        dsimp
+        apply Set.iUnionLift_binary (coe_iSup_of_directed (K := K) dir) dir _ (fun _ => (· * ·))
+        all_goals simp
+      map_add' := by
+        dsimp
+        apply Set.iUnionLift_binary (coe_iSup_of_directed (K := K) dir) dir _ (fun _ => (· + ·))
+        all_goals simp
+      commutes' := fun r => by
+        dsimp
+        exact
+          Set.iUnionLift_const _ (fun i : ι => algebraMap R (K i) r) (fun _ => rfl) _ (by simp) }
+  exact liftSup.comp (inclusion hT)
 
 
 @[simp]
 theorem iSupLift_inclusion {dir : Directed (· ≤ ·) K} {f : ∀ i, K i →ₐ[R] B}
     {hf : ∀ (i j : ι) (h : K i ≤ K j), f i = (f j).comp (inclusion h)}
-    {T : Subalgebra R A} {hT : T = iSup K} {i : ι} (x : K i) (h : K i ≤ T) :
+    {T : Subalgebra R A} {hT : T ≤ iSup K} {i : ι} (x : K i) (h : K i ≤ T) :
     iSupLift K dir f hf T hT (inclusion h x) = f i x := by
   dsimp [iSupLift, inclusion]
   rw [Set.iUnionLift_inclusion]
+  exact SetLike.coe_subset_coe.mpr fun ⦃x⦄ a ↦ hT (h a)
 
 @[simp]
 theorem iSupLift_comp_inclusion {dir : Directed (· ≤ ·) K} {f : ∀ i, K i →ₐ[R] B}
     {hf : ∀ (i j : ι) (h : K i ≤ K j), f i = (f j).comp (inclusion h)}
-    {T : Subalgebra R A} {hT : T = iSup K} {i : ι} (h : K i ≤ T) :
+    {T : Subalgebra R A} {hT : T ≤ iSup K} {i : ι} (h : K i ≤ T) :
     (iSupLift K dir f hf T hT).comp (inclusion h) = f i := by ext; simp
 
 @[simp]
 theorem iSupLift_mk {dir : Directed (· ≤ ·) K} {f : ∀ i, K i →ₐ[R] B}
     {hf : ∀ (i j : ι) (h : K i ≤ K j), f i = (f j).comp (inclusion h)}
-    {T : Subalgebra R A} {hT : T = iSup K} {i : ι} (x : K i) (hx : (x : A) ∈ T) :
+    {T : Subalgebra R A} {hT : T ≤ iSup K} {i : ι} (x : K i) (hx : (x : A) ∈ T) :
     iSupLift K dir f hf T hT ⟨x, hx⟩ = f i x := by
   dsimp [iSupLift, inclusion]
   rw [Set.iUnionLift_mk]
 
 theorem iSupLift_of_mem {dir : Directed (· ≤ ·) K} {f : ∀ i, K i →ₐ[R] B}
     {hf : ∀ (i j : ι) (h : K i ≤ K j), f i = (f j).comp (inclusion h)}
-    {T : Subalgebra R A} {hT : T = iSup K} {i : ι} (x : T) (hx : (x : A) ∈ K i) :
+    {T : Subalgebra R A} {hT : T ≤ iSup K} {i : ι} (x : T) (hx : (x : A) ∈ K i) :
     iSupLift K dir f hf T hT x = f i ⟨x, hx⟩ := by
   dsimp [iSupLift, inclusion]
   rw [Set.iUnionLift_of_mem]
