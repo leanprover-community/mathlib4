19acaef9d4ea03ae6df2dd32340236770eefbc70
LessnessRandomness <lessnessr@gmail.com>
2026-01-25 11:16:53 +0200

    Changed proof to Vlad Tsyrklevich's proof and also added IsBipartite.four_mul_encard_edgeSet_le back

--- Mathlib/Combinatorics/SimpleGraph/Bipartite.lean
+++ Mathlib/Combinatorics/SimpleGraph/Bipartite.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2025 Mitchell Horner. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Mitchell Horner
+Authors: Mitchell Horner, Vlad Tsyrklevich, Ilmārs Cīrulis
 -/
 module
 
@@ -447,133 +447,82 @@ end Between
 
 section
 
-variable {V : Type*} {G : SimpleGraph V}
+variable {V W₁ W₂ : Type*} (G : SimpleGraph V) {s t : Set V}
 
-private lemma delete_incidence_set_is_bipartite_with_of_is_bipartite_with {s t : Finset V} {a : V}
-    (hG : G.IsBipartiteWith (insert a ↑s) ↑t) :
-    (G.deleteIncidenceSet a).IsBipartiteWith ↑s ↑t :=
-  ⟨by grind [hG.disjoint], fun _ _ h ↦ by grind [G.deleteIncidenceSet_adj.mp h, hG.mem_of_adj]⟩
+theorem Sym2.eq_out (v : Sym2 V) : v = s(v.out.1, v.out.2) := Sym2.ext (by simp)
 
-private lemma edgeSet_decompose (a : V) :
-    G.edgeSet = (G.deleteIncidenceSet a).edgeSet ∪ G.incidenceSet a := by
-  rw [edgeSet_deleteIncidenceSet, Set.diff_union_self]
-  exact (Set.union_eq_self_of_subset_right (incidenceSet_subset G a)).symm
+theorem edgeSet_eq : G.edgeSet = { x | ∃ v w : V, x = s(v, w) ∧ G.Adj v w } := by
+  refine Set.ext fun x ↦ ⟨fun h ↦ ?_, by grind [mem_edgeSet]⟩
+  exact ⟨x.out.1, x.out.2, by simp [G.mem_edgeSet.mp <| Sym2.eq_out x ▸ h]⟩
 
-private lemma disjoint_edgeSet_decompose (a : V) :
-    Disjoint (G.deleteIncidenceSet a).edgeSet (G.incidenceSet a) := by
-  rw [Set.disjoint_iff_inter_eq_empty]
-  ext e
-  cases e with | h x y =>
-  simp only [deleteIncidenceSet, edgeSet_deleteEdges, Set.diff_inter_self, Set.mem_empty_iff_false]
+theorem completeBipartiteGraph_edgeSet : (completeBipartiteGraph W₁ W₂).edgeSet =
+    Set.range (fun x : W₁ × W₂ ↦ s(.inl x.1, .inr x.2)) := by
+  refine Set.ext fun x ↦ ⟨fun h ↦ ?_, fun h ↦ ?_⟩
+  · simp only [edgeSet_eq, completeBipartiteGraph_adj, Sum.exists, Sum.isRight_inl,
+      Bool.false_eq_true, and_false, Sum.isLeft_inl, and_true, false_or, exists_and_right,
+      Sum.isRight_inr, Sum.isLeft_inr, or_false, Set.mem_setOf_eq] at h
+    rcases h with ⟨_, _, _⟩ | ⟨_, _, _⟩ <;> simp_all
+  · obtain ⟨_, _, _⟩ := h
+    simp
 
-private lemma incidenceSet_ncard_le {s t : Finset V} {a : V}
-    (hG : G.IsBipartiteWith (insert a ↑s) ↑t) :
-    (G.incidenceSet a).Finite ∧ (G.incidenceSet a).ncard ≤ t.card := by
-  have h₁ : G.incidenceSet a ⊆ (fun u ↦ s(u, a)) '' { x | x ∈ t ∧ G.Adj a x} := by
-    intros e he
-    cases e with | h x y =>
-    simp only [incidenceSet, Set.mem_setOf_eq, mem_edgeSet, Sym2.mem_iff, Set.mem_image, Sym2.eq,
-      Sym2.rel_iff', Prod.mk.injEq, Prod.swap_prod_mk] at *
-    obtain ⟨hxy, hx | hy⟩ := he
-    · use y
-      simp only [hx, hxy, and_true, and_self, or_true]
-      have h₀ := hG.disjoint (x := {x})
-      have h₁ := hG.mem_of_adj hxy
-      simp only [Set.le_eq_subset, Set.singleton_subset_iff, Set.mem_insert_iff, SetLike.mem_coe,
-        Set.bot_eq_empty, Set.subset_empty_iff, Set.singleton_ne_empty, imp_false] at h₀ h₁
-      grind
-    · use x
-      simp only [hy, hxy.symm, and_self, true_or, and_true]
-      have h₀ := hG.disjoint (x := {y})
-      have h₁ := hG.mem_of_adj hxy
-      simp only [Set.le_eq_subset, Set.singleton_subset_iff, Set.mem_insert_iff, SetLike.mem_coe,
-        Set.bot_eq_empty, Set.subset_empty_iff, Set.singleton_ne_empty, imp_false] at h₀ h₁
-      grind
-  have h₂ : {x | x ∈ t ∧ G.Adj a x}.Finite := Set.Finite.inter_of_left (finite_toSet t) (G.Adj a)
-  have h₃ : (G.incidenceSet a).Finite := Set.Finite.of_surjOn (fun u ↦ s(u, a)) h₁ h₂
-  have h₄ : (G.incidenceSet a).ncard ≤ ((fun u ↦ s(u, a)) '' {x | x ∈ t ∧ G.Adj a x}).ncard :=
-    Set.ncard_le_ncard h₁ (Set.Finite.image (fun u ↦ s(u, a)) h₂)
-  have h₅ : ((fun u ↦ s(u, a)) '' {x | x ∈ t ∧ G.Adj a x}).ncard =
-            {x | x ∈ t ∧ G.Adj a x}.ncard := by
-    simp [Set.ncard_image_iff h₂]
-  have h₆ : ({x | x ∈ t ∧ G.Adj a x}).ncard ≤ (t : Set V).ncard :=
-    Set.ncard_inter_le_ncard_left _ _ (Set.finite_mem_finset t)
-  simp only [Set.ncard_coe_finset] at h₆
-  exact ⟨h₃, by grind⟩
+theorem completeBipartiteGraph_edgeSet_encard : (completeBipartiteGraph W₁ W₂).edgeSet.encard =
+    ENat.card W₁ * ENat.card W₂ := by
+  let g (x : W₁ × W₂) : Sym2 (W₁ ⊕ W₂) := s(.inl x.1, .inr x.2)
+  have : Function.Injective g := fun _ _ _ ↦ by grind
+  have := this.encard_image Set.univ
+  simpa [completeBipartiteGraph_edgeSet, this]
 
-/-- An upper bound on the cardinality of the edge set of a bipartite graph when it is formed
-by two finite vertex sets: the edge set is finite, and its maximum cardinality equals the product
-of the cardinalities of these two sets. The statement uses `Set.ncard`. -/
-theorem IsBipartiteWith.edgeSet_ncard_le_of_finsets {s t : Finset V}
-    (hG : G.IsBipartiteWith ↑s ↑t) :
-    G.edgeSet.Finite ∧ G.edgeSet.ncard ≤ s.card * t.card := by
-  revert hG G
-  induction s using Finset.cons_induction with
-  | empty =>
-    intros G hG
-    have hG₀ : G = ⊥ := by
-      ext x y
-      simp only [bot_adj, iff_false]
-      intros hxy
-      apply hG.mem_of_adj at hxy
-      simp at hxy
-    subst G; simp
-  | cons a s h iH =>
-    intros G hG
-    classical
-    simp only [Finset.cons_eq_insert, Finset.coe_insert] at hG
-    have hG' : (G.deleteIncidenceSet a).IsBipartiteWith ↑s ↑t :=
-      delete_incidence_set_is_bipartite_with_of_is_bipartite_with hG
-    obtain ⟨hG'₀, hG'₁⟩ := @iH (G.deleteIncidenceSet a) hG'
-    rw [edgeSet_decompose a]
-    simp only [hG'₀, Set.finite_union, Finset.cons_eq_insert, true_and]
-    obtain ⟨h₁, h₂⟩ := incidenceSet_ncard_le hG
-    refine ⟨h₁, ?_⟩
-    rw [Set.ncard_union_eq (hs := hG'₀) (ht := h₁)]
-    · simp only [h, not_false_eq_true, Finset.card_insert_of_notMem]
-      rw [Nat.succ_mul]
-      exact Nat.add_le_add hG'₁ h₂
-    · exact disjoint_edgeSet_decompose a
+theorem myembedding (hG : G.IsBipartiteWith s t) :
+    Nonempty (G.edgeSet ↪ (completeBipartiteGraph s t).edgeSet) := by
+  refine ⟨⟨fun ⟨x, hx⟩ ↦ ?_, fun _ _ _ ↦ ?_⟩⟩
+  · by_cases! h : x.out.1 ∈ s
+    · refine ⟨s(.inl ⟨x.out.1, h⟩, .inr ⟨x.out.2, ?_⟩), by simp⟩
+      grind [hG.disjoint, hG.mem_of_adj, edgeSet_eq, Sym2.eq_out]
+    · refine ⟨s(.inr ⟨x.out.1, ?_⟩, .inl ⟨x.out.2, ?_⟩), by simp⟩
+      · grind [hG.mem_of_adj <| G.mem_edgeSet.mpr <| Sym2.eq_out x ▸ hx]
+      · grind [hG.disjoint, hG.mem_of_adj, edgeSet_eq, Sym2.eq_out]
+  · grind [Sym2.eq_out]
 
 /-- An upper bound on the cardinality of the edge set of a bipartite graph when the vertex sets
 forming it may also be infinite: in that case as well, the upper bound is the product of
 the cardinalities of these two sets. The statement uses `Set.encard`. -/
-theorem IsBipartiteWith.encard_edgeSet_le {s t : Set V} (h : G.IsBipartiteWith s t) :
+theorem IsBipartiteWith.encard_edgeSet_le {s t : Set V} (hG : G.IsBipartiteWith s t) :
     G.edgeSet.encard ≤ s.encard * t.encard := by
-  by_cases hst : s.Finite ∧ t.Finite
-  · obtain ⟨hs, ht⟩ := hst
-    have inst_s := hs.fintype; have inst_t := ht.fintype
-    have hs' : s = ↑(s.toFinset) := by simp
-    have ht' : t = ↑(t.toFinset) := by simp
-    rw [hs', ht'] at h
-    apply IsBipartiteWith.edgeSet_ncard_le_of_finsets at h
-    obtain ⟨h₀, h₁⟩ := h
-    have h₂ : G.edgeSet.encard = ↑G.edgeSet.ncard := (Set.Finite.cast_ncard_eq h₀).symm
-    have h₃ : s.encard = ↑(s.toFinset.card) := Set.encard_eq_coe_toFinset_card s
-    have h₄ : t.encard = ↑(t.toFinset.card) := Set.encard_eq_coe_toFinset_card t
-    rw [h₂, h₃, h₄]
-    norm_cast
-  · obtain hs | ht : s.Infinite ∨ t.Infinite := by tauto
-    · simp only [hs, Set.Infinite.encard_eq]
-      by_cases ht₀ : t.encard = 0
-      · simp only [ht₀, mul_zero, nonpos_iff_eq_zero, Set.encard_eq_zero, edgeSet_eq_empty]
-        ext x y
-        simp only [bot_adj, iff_false]
-        intro hxy
-        apply h.mem_of_adj at hxy
-        simp only [Set.encard_eq_zero] at ht₀
-        simp [ht₀] at hxy
-      · simp [ht₀]
-    · simp only [ht, Set.Infinite.encard_eq]
-      by_cases hs₀ : s.encard = 0
-      · simp only [hs₀, zero_mul, nonpos_iff_eq_zero, Set.encard_eq_zero, edgeSet_eq_empty]
-        ext x y
-        simp only [bot_adj, iff_false]
-        intro hxy
-        apply h.mem_of_adj at hxy
-        simp only [Set.encard_eq_zero] at hs₀
-        simp [hs₀] at hxy
-      · simp [hs₀]
+  grw [Classical.choice (myembedding G hG) |>.encard_le, completeBipartiteGraph_edgeSet_encard]
+  simp
+
+/-- An upper bound on the cardinality of the edge set of a bipartite graph when the cardinality
+of the entire vertex set of the graph is known. That is, the cardinality of the edge set times `4`
+is less or equal to the square of the cardinality of the vertex set.
+The statement uses `Set.encard` and `ENat.card`. -/
+theorem IsBipartite.four_mul_encard_edgeSet_le (h : G.IsBipartite) :
+    4 * G.edgeSet.encard ≤ ENat.card V ^ 2 := by
+  rw [isBipartite_iff_exists_isBipartiteWith] at h
+  obtain ⟨s, t, h⟩ := h
+  have hG := IsBipartiteWith.encard_edgeSet_le G h
+  have h₀ : s.encard + t.encard ≤ ENat.card V := by
+    rw [← Set.encard_union_eq h.disjoint]
+    exact Set.encard_le_card
+  by_cases hv : Finite V
+  · have hs : s.Finite := Set.toFinite s
+    have ht : t.Finite := Set.toFinite t
+    rw [ENat.card_eq_coe_natCard V] at h₀ ⊢
+    have hs' : s.encard = ↑(s.ncard) := (Set.Finite.cast_ncard_eq hs).symm
+    have ht' : t.encard = ↑(t.ncard) := (Set.Finite.cast_ncard_eq ht).symm
+    rw [hs', ht'] at hG h₀
+    have h₁ : G.edgeSet.encard = ↑(G.edgeSet.ncard) := by
+      rw [Set.Finite.cast_ncard_eq]
+      exact Set.toFinite G.edgeSet
+    norm_cast at h₀
+    have h₂ : (s.ncard + t.ncard) ^ 2 ≤ Nat.card V ^ 2 :=
+      Nat.pow_le_pow_left h₀ 2
+    rw [h₁] at hG ⊢; norm_cast at *
+    have h₃ : 4 * s.ncard * t.ncard ≤ (s.ncard + t.ncard) ^ 2 :=
+      four_mul_le_pow_two_add s.ncard t.ncard
+    have h₄ : 4 * G.edgeSet.ncard ≤ 4 * s.ncard * t.ncard := by
+      rw [mul_assoc]; exact Nat.mul_le_mul_left 4 hG
+    exact Nat.le_trans (Nat.le_trans h₄ h₃) h₂
+  · simp at hv; simp
 
 end
 
