8181b8d588aad0a86e7f154e53e94836258fc940
Bingyu Xia <71547343+BryceT233@users.noreply.github.com>
2026-01-26 08:30:58 +0800

    fix simp

--- Mathlib/Algebra/Order/Group/Pointwise/CanonicallyOrdered.lean
+++ Mathlib/Algebra/Order/Group/Pointwise/CanonicallyOrdered.lean
@@ -23,7 +23,7 @@ namespace Set
 variable {α β : Type*} [Monoid α] [Monoid β]
 variable [Preorder β] [CanonicallyOrderedMul β] [MulRightMono β]
 
-@[to_additive, simp]
+@[to_additive (attr := simp)]
 theorem Ici_mul_Ici_eq_of_canonicallyOrdered {a b : β} :
     Ici a * Ici b = Ici (a * b) := by
   refine Subset.antisymm (Ici_mul_Ici_subset' ..) (subset_def ▸ fun c c_in ↦
@@ -31,14 +31,14 @@ theorem Ici_mul_Ici_eq_of_canonicallyOrdered {a b : β} :
   obtain ⟨d, hd⟩ := exists_mul_of_le <| mem_Ici.mp c_in
   exact ⟨b * d, by simp [← mul_assoc, hd]⟩
 
-@[to_additive, simp]
+@[to_additive (attr := simp)]
 theorem Ici_pow_eq_of_canonicallyOrdered {a : β} :
     ∀ n ≠ 0, Ici a ^ n = Ici (a ^ n)
   | 1, _ => by simp
   | n + 2, _ => by simp [pow_succ _ n.succ, Ici_pow_eq_of_canonicallyOrdered]
 
 omit [MulRightMono β] in
-@[to_additive, simp]
+@[to_additive (attr := simp)]
 lemma Ici_one_eq_univ_of_canonicallyOrdered : Set.Ici (1 : β) = Set.univ := by aesop
 
 end Set
