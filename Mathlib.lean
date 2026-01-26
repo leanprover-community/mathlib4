8685280a1fcf6a1ef8292fde042b848086abf164
Shuta Nakajima <njima@keio.jp>
2026-01-26 19:39:41 +0900

    feat(Probability): gaussian integration by parts

--- Mathlib.lean
+++ Mathlib.lean
@@ -5734,6 +5734,7 @@ public import Mathlib.Probability.Distributions.Gamma
 public import Mathlib.Probability.Distributions.Gaussian.Basic
 public import Mathlib.Probability.Distributions.Gaussian.CharFun
 public import Mathlib.Probability.Distributions.Gaussian.Fernique
+public import Mathlib.Probability.Distributions.Gaussian.GIP
 public import Mathlib.Probability.Distributions.Gaussian.Real
 public import Mathlib.Probability.Distributions.Geometric
 public import Mathlib.Probability.Distributions.Pareto
