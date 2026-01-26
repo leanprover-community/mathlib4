5649cc683277c514aedbb79ef5602818290dde2f
Jingting Wang <wangjt2020@163.com>
2026-01-26 17:34:56 +0800

    Update Mathlib/RepresentationTheory/Homological/GroupCohomology/NonAbelian.lean
    Co-authored-by: Kevin Buzzard <k.buzzard@imperial.ac.uk>

--- Mathlib/RepresentationTheory/Homological/GroupCohomology/NonAbelian.lean
+++ Mathlib/RepresentationTheory/Homological/GroupCohomology/NonAbelian.lean
@@ -12,7 +12,7 @@ public import Mathlib.RepresentationTheory.Homological.GroupCohomology.LongExact
 # Non-abelian group cohomology
 
 Let `G` be a group acting on another (not necessarily abelian) group `A`, in this file we define
-`H⁰(G, A)$` and `H¹(G, A)`, and prove some basic properties about it.
+`H⁰(G, A)$` and `H¹(G, A)`, and prove some basic properties about them.
 
 ## Main definitions
 
