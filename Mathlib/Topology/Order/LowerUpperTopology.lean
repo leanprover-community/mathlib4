2b4b9e1a7e15e619766b103605f73fba86d30ed0
Komyyy <pol_tta@outlook.jp>
2026-01-27 04:59:09 +0900

    style: better recursor arguments names for topology-equiped order types
    required for #34464

--- Mathlib/Topology/Order/LowerUpperTopology.lean
+++ Mathlib/Topology/Order/LowerUpperTopology.lean
@@ -95,8 +95,8 @@ theorem ofLower_inj {a b : WithLower α} : ofLower a = ofLower b ↔ a = b :=
 
 /-- A recursor for `WithLower`. Use as `induction x`. -/
 @[elab_as_elim, cases_eliminator, induction_eliminator]
-protected def rec {β : WithLower α → Sort*} (h : ∀ a, β (toLower a)) : ∀ a, β a := fun a =>
-  h (ofLower a)
+protected def rec {β : WithLower α → Sort*} (toLower : ∀ a, β (toLower a)) : ∀ a, β a := fun a =>
+  toLower (ofLower a)
 
 instance [Nonempty α] : Nonempty (WithLower α) := ‹Nonempty α›
 instance [Inhabited α] : Inhabited (WithLower α) := ‹Inhabited α›
@@ -148,8 +148,8 @@ lemma ofUpper_inj {a b : WithUpper α} : ofUpper a = ofUpper b ↔ a = b := Iff.
 
 /-- A recursor for `WithUpper`. Use as `induction x`. -/
 @[elab_as_elim, cases_eliminator, induction_eliminator]
-protected def rec {β : WithUpper α → Sort*} (h : ∀ a, β (toUpper a)) : ∀ a, β a := fun a =>
-  h (ofUpper a)
+protected def rec {β : WithUpper α → Sort*} (toUpper : ∀ a, β (toUpper a)) : ∀ a, β a := fun a =>
+  toUpper (ofUpper a)
 
 instance [Nonempty α] : Nonempty (WithUpper α) := ‹Nonempty α›
 instance [Inhabited α] : Inhabited (WithUpper α) := ‹Inhabited α›
