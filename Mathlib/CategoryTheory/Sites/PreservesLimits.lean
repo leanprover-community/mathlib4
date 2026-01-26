05b44c3a52fabff1c9cb2b2a33201235652d7cfd
Dagur Asgeirsson <dagurtomas@gmail.com>
2026-01-26 09:33:22 -0700

    2026

--- Mathlib/CategoryTheory/Sites/PreservesLimits.lean
+++ Mathlib/CategoryTheory/Sites/PreservesLimits.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2025 Jonas van der Schaaf. All rights reserved.
+Copyright (c) 2026 Jonas van der Schaaf. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Dagur Asgeirsson, Jonas van der Schaaf
 -/
