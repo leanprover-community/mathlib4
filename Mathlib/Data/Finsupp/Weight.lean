a07858fb73f6501c5683475037bf05b208514b38
Bingyu Xia <71547343+BryceT233@users.noreply.github.com>
2026-01-27 10:45:07 +0800

    simplify

--- Mathlib/Data/Finsupp/Weight.lean
+++ Mathlib/Data/Finsupp/Weight.lean
@@ -315,7 +315,7 @@ lemma nsmul_single_one_image {α : Type*} {n : ℕ} {s : Set α} :
     use x; split_ands
     · aesop
     · trans ↑f.support
-      · simpa [hx, SetLike.coe_subset_coe, Finset.le_eq_subset] using support_mono le_self_add
+      · simpa [hx] using support_mono le_self_add
       exact f_supp
     exact ⟨i, ⟨f_supp (Finset.mem_coe.mpr hi) , hx.symm⟩⟩
 
