823ebfc8bb8a5b5c9ebe82e8db36080a8d583c26
Arend Mellendijk <arend.mellendijk@gmail.com>
2026-01-27 14:50:35 +0100

    fix compare

--- Mathlib/Tactic/Ring/Compare.lean
+++ Mathlib/Tactic/Ring/Compare.lean
@@ -217,8 +217,8 @@ def proveLE (g : MVarId) : MetaM Unit := do
   let c ← Common.mkCache q($ics)
   let (⟨a, va, pa⟩, ⟨b, vb, pb⟩)
     ← AtomM.run .instances do
-      pure (← Common.eval (fun _ _ => ringCompute) rcℕ (ringCompute ics) c e₁,
-            ← Common.eval (fun _ _ => ringCompute) rcℕ (ringCompute ics) c e₂)
+      pure (← Common.eval ringCompute rcℕ (ringCompute ics) c e₁,
+            ← Common.eval ringCompute rcℕ (ringCompute ics) c e₂)
   match ← evalLE ics ipo sα va vb with
   | .ok p => g.assign q(le_congr $pa $p $pb)
   | .error e =>
@@ -245,8 +245,8 @@ def proveLT (g : MVarId) : MetaM Unit := do
   let c ← Common.mkCache q($ics)
   let (⟨a, va, pa⟩, ⟨b, vb, pb⟩)
     ← AtomM.run .instances do
-      pure (← Common.eval (fun _ _ => ringCompute) rcℕ (ringCompute ics) c e₁,
-            ← Common.eval (fun _ _ => ringCompute) rcℕ (ringCompute ics) c e₂)
+      pure (← Common.eval ringCompute rcℕ (ringCompute ics) c e₁,
+            ← Common.eval ringCompute rcℕ (ringCompute ics) c e₂)
   match ← evalLT ics ipo sα va vb with
   | .ok p => g.assign q(lt_congr $pa $p $pb)
   | .error e =>
