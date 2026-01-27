9f8bed7df3c3c8b85fefa1c553645d3cb386be9b
Bingyu Xia <71547343+BryceT233@users.noreply.github.com>
2026-01-27 09:50:21 +0800

    Add lemmas for toAdd image setMul and nsmul

--- Mathlib/Algebra/Group/TypeTags/Pointwise.lean
+++ Mathlib/Algebra/Group/TypeTags/Pointwise.lean
@@ -34,4 +34,17 @@ lemma ofAdd_image_nsmul (n : ℕ) (s : Set M) :
   | zero => simp; rfl
   | succ n IH => simp [succ_nsmul, pow_succ, IH]
 
+@[simp]
+lemma toAdd_image_setMul {M : Type*} [AddMonoid M] (s t : Set (Multiplicative M)) :
+    Multiplicative.toAdd '' (s * t) =
+      (Multiplicative.toAdd '' s) + (Multiplicative.toAdd '' t) := by
+  rw [← Set.image2_mul, Set.image_image2_distrib toAdd_mul, Set.image2_add]
+
+@[simp]
+lemma toAdd_image_nsmul {M : Type*} [AddMonoid M] (n : ℕ) (s : Set (Multiplicative M)) :
+    Multiplicative.toAdd '' (s ^ n) = n • (Multiplicative.toAdd '' s) := by
+  induction n with
+  | zero => simp; rfl
+  | succ n IH => simp [succ_nsmul, pow_succ, IH]
+
 end Multiplicative
