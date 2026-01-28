0415842f2d026ec47501ff420ab3bf9e878f8dc2
Hampus Nyberg <21244942+Maldooor@users.noreply.github.com>
2026-01-28 01:00:57 +0000

    feat: `star_finsuppProd` (#34486)

--- Mathlib/Algebra/Star/BigOperators.lean
+++ Mathlib/Algebra/Star/BigOperators.lean
@@ -5,8 +5,7 @@ Authors: Eric Wieser
 -/
 module
 
-public import Mathlib.Algebra.BigOperators.Group.Finset.Basic
-public import Mathlib.Algebra.Star.Basic
+public import Mathlib.Algebra.BigOperators.Finsupp.Basic
 public import Mathlib.Algebra.Star.SelfAdjoint
 
 /-! # Big-operators lemmas about `star` algebraic operations
@@ -30,3 +29,13 @@ theorem star_sum [AddCommMonoid R] [StarAddMonoid R] {α : Type*} (s : Finset α
 theorem isSelfAdjoint_sum {ι : Type*} [AddCommMonoid R] [StarAddMonoid R] (s : Finset ι)
     {x : ι → R} (h : ∀ i ∈ s, IsSelfAdjoint (x i)) : IsSelfAdjoint (∑ i ∈ s, x i) := by
   simpa [IsSelfAdjoint, star_sum] using Finset.sum_congr rfl fun _ hi => h _ hi
+
+@[simp]
+theorem star_finsuppSum {ι : Type*} {M : Type*} [Zero M] [AddCommMonoid R] [StarAddMonoid R]
+    (s : ι →₀ M) (f : ι → M → R) : star (s.sum f) = s.sum (fun i m ↦ star f i m) := by
+  simp [Finsupp.sum]
+
+@[simp]
+theorem star_finsuppProd {ι : Type*} {M : Type*} [Zero M] [CommMonoid R] [StarMul R]
+    (s : ι →₀ M) (f : ι → M → R) : star (s.prod f) = s.prod (fun i m ↦ star f i m) := by
+  simp [Finsupp.prod]
