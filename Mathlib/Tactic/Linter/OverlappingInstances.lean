f86ae520ae72356e423032095b8a08e3f7ad5b2c
thorimur <68410468+thorimur@users.noreply.github.com>
2026-01-27 22:00:05 -0500

    feat: first pass at deduplicating error messages

--- Mathlib/Tactic/Linter/OverlappingInstances.lean
+++ Mathlib/Tactic/Linter/OverlappingInstances.lean
@@ -21,28 +21,53 @@ public meta section
 
 namespace Mathlib.Tactic.OverlappingInstances
 
-/-- Given an instance `e`, conpute return all data carrying classes that are
-the type of `e` itself, or a child class. -/
-private partial def getStructureDataProjections (e : Expr) (acc : Array Expr := #[]) :
-    StateRefT NameSet MetaM (Array Expr) := do
+/-- Given an instance `e`, compute all data carrying classes that are
+the type of `e` itself, or a child class, together with the indices of the parents of each
+projection as they appear in this array. -/
+private partial def getStructureDataProjections (e : Expr) (acc : Array (List Nat × Expr) := #[])
+    (parentIdx? : Option Nat := none) :
+    StateRefT (NameMap (Option Nat)) MetaM (Array (List Nat × Expr)) := do
   let eType ← whnf (← inferType e)
   if ← isProp eType then return acc
   let .const structName us := eType.getForallBody.getAppFn
     | throwError "{e} is not an instance of a structure"
-  if (← get).contains structName then return acc
-  modify (·.insert structName)
-  let some info := getStructureInfo? (← getEnv) structName | return acc
-  info.parentInfo.foldlM (init := acc.push eType) fun acc info ↦ do
-    if ← isInstance info.projFn then
-      let proj := Expr.app (mkAppN (.const info.projFn us) eType.getAppArgs) e
-      getStructureDataProjections proj acc
-    else
-      return acc
+  if let some structIdx? := (← get).get? structName then
+    if let some structIdx := structIdx? then -- `structName` may not have actually been a structure
+      if let some parentIdx := parentIdx? then
+        /- `e` has already been recorded, but is being encountered as the child of a new parent at
+        `parentIdx`. Add this parent index to `e`'s original parent indices. -/
+        return acc.modify structIdx fun (parents, struct) => (parentIdx :: parents, struct)
+    return acc
+  if let some info := getStructureInfo? (← getEnv) structName then
+    let currentIdx := acc.size
+    -- Record the index at which this structure occurs in `acc`, so we can add to its parents later
+    -- if it is encountered as a projection of something else.
+    modify (·.insert structName currentIdx)
+    info.parentInfo.foldlM (init := acc.push (parentIdx?.toList, eType)) fun acc info ↦ do
+      if ← isInstance info.projFn then
+        let proj := Expr.app (mkAppN (.const info.projFn us) eType.getAppArgs) e
+        getStructureDataProjections proj acc currentIdx
+      else
+        return acc
+  else
+    -- Record that we've encountered this constant; however, it is not in the array.
+    modify (·.insert structName none)
+    return acc
+
+/-- Given an array of projection types paired with indices for their parents, returns `true` if `p`
+is true for the types at any of the starting indices or their transitive parents. -/
+private partial def hasParentP! (projections : Array (List Nat × Expr)) (p : Nat → Expr → Bool)
+    (startingIdxs : List Nat) : Bool :=
+  match startingIdxs with
+  | []          => false
+  | idx :: idxs =>
+    let (parentIdxs, expr) := projections[idx]!
+    p idx expr || hasParentP! projections p parentIdxs || hasParentP! projections p idxs
 
 /-- Stores the local instance overlaps per class. The keys are the class, and the values are local
 instances which have the class as a projection. The `Bool` value of each entry indicates whether
 its type is exactly the key class. We use an `ExprMap Bool` here instead of e.g. an
-`Array (Expr × Bool`) to ensure that each local instance is recorded only once. There may be
+`Array (Expr × Bool)` to ensure that each local instance is recorded only once. There may be
 assumed to be at least two local instances per class. -/
 abbrev Overlaps := ExprMap (ExprMap Bool)
 
@@ -51,31 +76,40 @@ def Overlaps.insert (cls : Expr) (fvar₁ fvar₂ : Expr × Bool) (overlaps : Ov
   overlaps.alter cls fun map? =>
     map?.getD ∅ |>.insert fvar₁.1 fvar₁.2 |>.insert fvar₂.1 fvar₂.2
 
+/-- Returns `true` iff `fvar₁` and `fvar₂` overlap on the `cls` projection typeclass. -/
+def Overlaps.containsOverlapOn (fvar₁ fvar₂ : Expr) (cls : Expr) (overlaps : Overlaps) : Bool :=
+  match overlaps[cls]? with
+  | none => false
+  | some overlap => overlap.contains fvar₁ && overlap.contains fvar₂
+
 /-- Find data-carrying overlaps between the current local instances of the `MetaM` context. -/
 def findOverlappingDataInstances : MetaM Overlaps := do
   let mut overlaps : Overlaps := {}
-  /- Records all instances encountered, and so is distinct from `overlaps`. The `Bool` indicates
-  whether the given class key is exactly the type of the associated `fvar` value. -/
+  /- Associates all (data-carrying) typeclasses encountered to the first fvar that had a projection
+  into this given typeclass. Since it records all typeclasses indiscriminately, it is distinct from
+  `overlaps`. The `Bool` indicates whether the given class key is exactly the type of the
+  associated `fvar` value. We use this for error reporting. -/
   let mut insts : Std.HashMap Expr (Expr × Bool) := {}
   for { fvar := fvar₁, .. } in ← getLocalInstances do
     unless (← fvar₁.fvarId!.getBinderInfo).isInstImplicit do continue
     let projClasses ← forallTelescope (← inferType fvar₁) fun xs _ ↦ do
-      (← getStructureDataProjections (mkAppN fvar₁ xs) |>.run' {}).mapM (mkForallFVars xs)
-    /- The fvars that are either projections of the current fvar or have projections equal to the
-    current fvar. In both cases we want to ignore further matches against these fvars.
-
-    For less verbose error reporting, we would ideally also ignore overlaps which share a parent;
-    we may eventually want a different data structure for `projClasses` for this. -/
-    let mut done : Array Expr := #[]
-    for h : clsIdx in 0...projClasses.size do
-      let cls := projClasses[clsIdx]
+      (← getStructureDataProjections (mkAppN fvar₁ xs) |>.run' {}).mapM fun (parentIdx?, expr) =>
+        return (parentIdx?, ← mkForallFVars xs expr)
+    for (parentIdxs, cls) in projClasses, idx in 0...* do
       if let some (fvar₂, isTypeOfFVar₂) := insts[cls]? then
-        unless done.contains fvar₂ do
-          overlaps := overlaps.insert cls (fvar₁, clsIdx = 0) (fvar₂, isTypeOfFVar₂)
-          if clsIdx = 0 || isTypeOfFVar₂ then
-            done := done.push fvar₂ -- Don't consider `fvar₂` any more
+        -- We have encountered a projection with this type already; we should now record an overlap,
+        -- unless it is (or will) be redundant.
+        -- Note that the actions in this branch are allowed to be "slow".
+        let shouldIgnoreCurrent (parentIdx : Nat) (parentClass : Expr) :=
+          -- If a parent further on in `projClasses` will overlap via `fvar₂`, ignore  this child.
+          -- Note that we can assume `false`, as only the first array element has `true`.
+          ((idx < parentIdx) && insts[parentClass]?.isEqSome (fvar₂, false))
+          -- If `fvar₁` and `fvar₂` already overlap on a parent, ignore this redundant overlap.
+            || overlaps.containsOverlapOn fvar₁ fvar₂ parentClass
+        unless hasParentP! projClasses shouldIgnoreCurrent parentIdxs do
+          overlaps := overlaps.insert cls (fvar₁, parentIdxs.isEmpty) (fvar₂, isTypeOfFVar₂)
       else
-        insts := insts.insert cls (fvar₁, clsIdx = 0)
+        insts := insts.insert cls (fvar₁, parentIdxs.isEmpty)
   return overlaps
 
 /-- Lints against data-carrying overlaps between instances in the local contexts of declarations. -/
@@ -98,7 +132,7 @@ decl of the same name in the local context. In the future, we should account for
 the name within a more appropriate message context. The type of this declaration is therefore
 subject to change.
 -/
-def _root_.Lean.Elab.ContextInfo.toDeclDescr (ctx : ContextInfo) : MessageData :=
+private def _root_.Lean.Elab.ContextInfo.toDeclDescr (ctx : ContextInfo) : MessageData :=
   if let some decl := ctx.parentDecl? then
     m!"declaration `{privateToUserName decl}`"
   else
