1e2785606074b21f9c9e8f79cd58da03fffb1a73
artie2000 <artem.khovanov@gmail.com>
2026-01-29 12:54:34 +0000

    upd

--- test.lean
+++ test.lean
@@ -18,4 +18,4 @@ variable {R S T ι : Type*}
 #synth FaithfulSMul (P.ofAlgEquiv e).Ring T
 -- master: Used 19452 heartbeats
 -- #33697: Used 22693 heartbeats
--- #34475: Used 15502 heartbeats
+-- #34475: Used 14597 heartbeats
