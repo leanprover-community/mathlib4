6a99d775acf25922936089b3417764248dffc039
Christian Merten <christian@merten.dev>
2026-01-31 16:59:42 +0100

    remove max u v

--- Mathlib/Algebra/Category/Grp/Ulift.lean
+++ Mathlib/Algebra/Category/Grp/Ulift.lean
@@ -153,9 +153,8 @@ instance : uliftFunctor.{u, v}.Full := uliftFunctorFullyFaithful.full
 @[to_additive]
 noncomputable instance uliftFunctor_preservesLimit {J : Type w} [Category.{w'} J]
     (K : J ⥤ CommGrpCat.{u}) : PreservesLimit K uliftFunctor.{v, u} where
-  preserves lc := ⟨isLimitOfReflects (forget CommGrpCat.{max u v})
-    (isLimitOfPreserves CategoryTheory.uliftFunctor.{v}
-      (isLimitOfPreserves (forget CommGrpCat) lc))⟩
+  preserves lc := ⟨isLimitOfReflects (forget CommGrpCat) <|
+    isLimitOfPreserves CategoryTheory.uliftFunctor.{v} (isLimitOfPreserves (forget CommGrpCat) lc)⟩
 
 @[to_additive]
 noncomputable instance uliftFunctor_preservesLimitsOfShape {J : Type w} [Category.{w'} J] :
