a6586761e2641f69204dae7f2692c7eb0884bd0c
danils@mit.edu <danils@mit.edu>
2026-01-31 18:41:24 -0500

    merge

--- Mathlib/Combinatorics/SimpleGraph/Konig/Konig.lean
+++ Mathlib/Combinatorics/SimpleGraph/Konig/Konig.lean
@@ -61,11 +61,7 @@ theorem min_size_cover_exists (G : SimpleGraph V) : ∃ C : Set V, IsMinSizeCove
   have S_nonempty : S.Nonempty := ⟨#Set.univ, ⟨Set.univ, isVertexCover_univ, rfl⟩⟩
   obtain ⟨a, ⟨C, ⟨hC, hcard⟩⟩, hmin⟩ := WellFounded.has_min Cardinal.lt_wf S S_nonempty
   refine ⟨C, ⟨hC, fun C' hC' ↦ ?_⟩⟩
-<<<<<<< HEAD
   set b : S := ⟨#C', ⟨C', ⟨hC', rfl⟩⟩⟩ with hb
-=======
-  set b: S := ⟨#C', ⟨C', ⟨hC', rfl⟩⟩⟩ with hb
->>>>>>> a264ad38e7 (merge conflicts)
   have := hb ▸ hcard ▸ (hmin b b.prop)
   simpa using this
 
@@ -112,11 +108,7 @@ lemma konig_finite_matching
     have hwG: w ∈ G'.verts := Or.inr (Or.inr ⟨CS, rfl⟩)
     obtain ⟨hadj, hnv, hnw⟩ := hf C hCS;
     have hadj_coe: G'.coe.Adj ⟨v, hvG⟩ ⟨w, hwG⟩ := Or.inr ⟨⟨C, hCS⟩, Or.inl ⟨rfl, rfl⟩⟩
-<<<<<<< HEAD
-    rcases hC _ _ hadj_coe <;> grind
-=======
     rcases hC hadj_coe <;> grind
->>>>>>> a264ad38e7 (merge conflicts)
   have h : #C ≤ #M.edgeSet := by
     obtain ⟨N', ⟨hN', hN'card⟩⟩ := konig_finite_graph (G := G'.coe) (hbi := hbi') ⟨hC, hCmin⟩
     let N : Subgraph G := subgraph_upcast N'
@@ -129,7 +121,7 @@ lemma konig_finite_matching
     intro v w hadj
     have hadj' : G'.Adj v w := Or.inl hadj
     have hadj'_coe : G'.coe.Adj ⟨v, G'.edge_vert hadj'⟩ ⟨w, G'.edge_vert hadj'.symm⟩ := hadj'
-    rcases hC hadj'_coe <;> grind
+    rcases hC _ _ hadj'_coe <;> grind
   have hfinC : #C < ℵ₀ := lt_of_le_of_lt h hfin
   suffices hsub: Subtype.val '' C ⊆ M.verts from ⟨hsub, heq_subtype ▸ h, by simpa⟩
   rintro v hvC
@@ -146,7 +138,7 @@ lemma konig_finite_matching
   have hvwM : v ∈ M.verts ∧ w ∈ M.verts := ⟨M.edge_vert hadj, M.edge_vert hadj.symm⟩
   have hvwG : v ∈ G'.verts ∧ w ∈ G'.verts := ⟨Or.inl hvwM.1, Or.inl hvwM.2⟩
   have : G'.coe.Adj ⟨v, hvwG.1⟩ ⟨w, hvwG.2⟩ := Or.inl hadj
-  rcases hC this with hC | hC
+  rcases hC _ _ this with hC | hC
   · use ⟨v, ⟨by simpa [hvwG] using hC, M.edge_vert hadj⟩⟩
     simpa [f, hvw] using hM.1.toEdge_eq_of_adj hvwM.1 hadj
   · use ⟨w, ⟨by simpa [hvwG] using hC, M.edge_vert hadj.symm⟩⟩
