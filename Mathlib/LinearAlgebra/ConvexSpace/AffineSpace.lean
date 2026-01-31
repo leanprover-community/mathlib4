9aedc2f40e6e71d5758c0756bcba10fe648be033
Kim Morrison <kim@tqft.net>
2026-01-31 12:45:32 +1100

    suggestions from botbaki review

--- Mathlib/LinearAlgebra/ConvexSpace/AffineSpace.lean
+++ Mathlib/LinearAlgebra/ConvexSpace/AffineSpace.lean
@@ -3,7 +3,9 @@ Copyright (c) 2026 Lean FRO, LLC. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kim Morrison
 -/
-import Mathlib.LinearAlgebra.ConvexSpace
+module
+
+public import Mathlib.LinearAlgebra.ConvexSpace
 import Mathlib.LinearAlgebra.AffineSpace.Combination
 
 /-!
@@ -27,17 +29,15 @@ variable [AddCommGroup V] [Module R V] [AddTorsor V P]
 namespace AddTorsor
 
 /-- The convex combination of points in an affine space, given a probability distribution. -/
-def convexCombination (s : StdSimplex R P) : P :=
+public def convexCombination (s : StdSimplex R P) : P :=
   s.weights.support.affineCombination R id s.weights
 
 theorem convexCombination_single (x : P) :
     convexCombination (StdSimplex.single x : StdSimplex R P) = x := by
   simp only [convexCombination, StdSimplex.single]
   rw [Finsupp.support_single_ne_zero _ one_ne_zero]
-  refine ({x} : Finset P).affineCombination_of_eq_one_of_eq_zero _ _
-    (Finset.mem_singleton_self x) Finsupp.single_eq_same ?_
-  intro j _ hne
-  exact Finsupp.single_eq_of_ne hne
+  exact ({x} : Finset P).affineCombination_of_eq_one_of_eq_zero _ _
+    (Finset.mem_singleton_self x) Finsupp.single_eq_same fun j _ hne => Finsupp.single_eq_of_ne hne
 
 theorem convexCombination_assoc (f : StdSimplex R (StdSimplex R P)) :
     convexCombination (f.map convexCombination) = convexCombination f.join := by
@@ -45,11 +45,11 @@ theorem convexCombination_assoc (f : StdSimplex R (StdSimplex R P)) :
   -- Choose a base point
   obtain ⟨b⟩ : Nonempty P := inferInstance
   -- Express both sides using weightedVSubOfPoint with base point b
-  have hL := @Finset.affineCombination_eq_weightedVSubOfPoint_vadd_of_sum_eq_one R V P _ _ _ _
-    P (f.map convexCombination).weights.support (f.map convexCombination).weights id
+  have hL := Finset.affineCombination_eq_weightedVSubOfPoint_vadd_of_sum_eq_one
+    (f.map convexCombination).weights.support (f.map convexCombination).weights id
     (f.map convexCombination).total b
-  have hR := @Finset.affineCombination_eq_weightedVSubOfPoint_vadd_of_sum_eq_one R V P _ _ _ _
-    P f.join.weights.support f.join.weights id f.join.total b
+  have hR := Finset.affineCombination_eq_weightedVSubOfPoint_vadd_of_sum_eq_one
+    f.join.weights.support f.join.weights id f.join.total b
   simp only [convexCombination, hL, hR]
   congr 1
   -- Now show the weighted vector sums are equal
