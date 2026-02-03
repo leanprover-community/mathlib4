1f66a410d62b2e18bdf259ae3f6c3f06c7ca36bb
Vasilii Nesterov <vas.nesterov63@gmail.com>
2026-02-03 10:40:48 +0300

    rename & add docs

--- Mathlib/Tactic/ComputeAsymptotics/Multiseries/Majorated.lean
+++ Mathlib/Tactic/ComputeAsymptotics/Multiseries/Majorated.lean
@@ -8,15 +8,15 @@ module
 public import Mathlib.Analysis.SpecialFunctions.Pow.Asymptotics
 
 /-!
-# `Majorated` predicate
+# `Majorized` predicate
 
-This file defines the `Majorated` predicate, along with a few basic lemmas.
+This file defines the `Majorized` predicate, along with a few basic lemmas.
 
 ## Main definitions
 
-* `Majorated f b exp` means that `f =o[atTop] (b ^ exp')` for any `exp' > exp`. This predicate is
-  used in the definition of the `PreMS.Approximates` predicate.
-
+* `Majorized f b exp` means that `f =o[atTop] (b ^ exp')` for any `exp' > exp`.
+  Intuitively, this means that the right order of `f` in terms of `b` is at most `b ^ exp`.
+  This predicate is used in the definition of the `PreMS.Approximates` predicate.
 -/
 
 @[expose] public section
@@ -25,28 +25,28 @@ namespace Tactic.ComputeAsymptotics
 
 open Topology Filter Asymptotics
 
-/-- `Majorated f g exp` for real functions `f` and `g` means that for any `exp' < exp`,
+/-- `Majorized f g exp` for real functions `f` and `g` means that for any `exp' < exp`,
 `f =o[atTop] g ^ exp'`. This is used to define the `PreMS.Approximates` predicate. The naming
-`Majorated` is non-standard because this notion is invented for the purposes of this tactic, and
+`Majorized` is non-standard because this notion is invented for the purposes of this tactic, and
 does not exists in literature. -/
-def Majorated (f b : ℝ → ℝ) (exp : ℝ) : Prop :=
+def Majorized (f b : ℝ → ℝ) (exp : ℝ) : Prop :=
   ∀ exp' > exp, f =o[atTop] (b ^ exp')
 
-namespace Majorated
+namespace Majorized
 
 variable {f g b : ℝ → ℝ} {exp : ℝ}
 
-/-- Replacing the first argument of `Majorated` by an eventually equal function preserves it. -/
-theorem of_eventuallyEq (h_eq : g =ᶠ[atTop] f) (h : Majorated f b exp) :
-    Majorated g b exp := by
-  simp only [Majorated] at *
+/-- Replacing the first argument of `Majorized` by an eventually equal function preserves it. -/
+theorem of_eventuallyEq (h_eq : g =ᶠ[atTop] f) (h : Majorized f b exp) :
+    Majorized g b exp := by
+  simp only [Majorized] at *
   intro exp' h_exp
   exact EventuallyEq.trans_isLittleO h_eq (h exp' h_exp)
 
-/-- For any function `f` tending to infinity, `f ^ exp` is majorated by `f` with exponent `exp`. -/
+/-- For any function `f` tending to infinity, `f ^ exp` is majorized by `f` with exponent `exp`. -/
 theorem self (h : Tendsto f atTop atTop) :
-    Majorated (f ^ exp) f exp := by
-  simp only [Majorated]
+    Majorized (f ^ exp) f exp := by
+  simp only [Majorized]
   intro exp' h_exp
   apply (isLittleO_iff_tendsto' _).mpr
   · have : (fun t ↦ f t ^ exp / f t ^ exp') =ᶠ[atTop] fun t ↦ (f t) ^ (exp - exp') :=
@@ -61,50 +61,50 @@ theorem self (h : Tendsto f atTop atTop) :
     absurd h2
     exact (Real.rpow_pos_of_pos h1 _).ne.symm
 
-/-- If `f` is majorated with exponent `exp₁`, then it is majorated with any `exp₂ ≥ exp₁`. -/
-theorem of_le {exp1 exp2 : ℝ} (h_lt : exp1 ≤ exp2) (h : Majorated f b exp1) :
-    Majorated f b exp2 := by
-  simp only [Majorated] at *
+/-- If `f` is majorized with exponent `exp₁`, then it is majorized with any `exp₂ ≥ exp₁`. -/
+theorem of_le {exp1 exp2 : ℝ} (h_lt : exp1 ≤ exp2) (h : Majorized f b exp1) :
+    Majorized f b exp2 := by
+  simp only [Majorized] at *
   exact fun exp' h_exp ↦ h _ (by linarith)
 
-/-- If `f` is majorated with a negative exponent, then it tends to zero. -/
-theorem tendsto_zero_of_neg (h_lt : exp < 0) (h : Majorated f b exp) :
+/-- If `f` is majorized with a negative exponent, then it tends to zero. -/
+theorem tendsto_zero_of_neg (h_lt : exp < 0) (h : Majorized f b exp) :
     Tendsto f atTop (𝓝 0) := by
-  simpa [Pi.pow_def, Majorated] using h 0 (by linarith)
+  simpa [Pi.pow_def, Majorized] using h 0 (by linarith)
 
-/-- Constants are majorated with exponent `exp = 0` by any functions which tends to infinity. -/
+/-- Constants are majorized with exponent `exp = 0` by any functions which tends to infinity. -/
 theorem const (h_tendsto : Tendsto b atTop atTop) {c : ℝ} :
-    Majorated (fun _ ↦ c) b 0 := by
+    Majorized (fun _ ↦ c) b 0 := by
   intro exp h_exp
   apply Asymptotics.isLittleO_const_left.mpr
   right
   apply tendsto_norm_atTop_atTop.comp
   exact (tendsto_rpow_atTop h_exp).comp h_tendsto
 
-/-- The zero function is majorated with any exponent. -/
-theorem zero : Majorated 0 b exp :=
+/-- The zero function is majorized with any exponent. -/
+theorem zero : Majorized 0 b exp :=
   fun _ _ ↦ Asymptotics.isLittleO_zero _ _
 
-/-- `c • f` is majorated with the same exponent as `f` for any constant `c`. -/
-theorem smul (h : Majorated f b exp) {c : ℝ} :
-    Majorated (c • f) b exp :=
+/-- `c • f` is majorized with the same exponent as `f` for any constant `c`. -/
+theorem smul (h : Majorized f b exp) {c : ℝ} :
+    Majorized (c • f) b exp :=
   fun exp h_exp ↦ (h exp h_exp).const_mul_left _
 
-/-- The sum of two functions that are majorated with exponents `f_exp` and `g_exp` is
-majorated with exponent `exp` whenever `exp ≥ max f_exp g_exp`. -/
-theorem add {f_exp g_exp : ℝ} (hf : Majorated f b f_exp)
-    (hg : Majorated g b g_exp) (hf_exp : f_exp ≤ exp) (hg_exp : g_exp ≤ exp) :
-    Majorated (f + g) b exp := by
-  simp only [Majorated] at *
+/-- The sum of two functions that are majorized with exponents `f_exp` and `g_exp` is
+majorized with exponent `exp` whenever `exp ≥ max f_exp g_exp`. -/
+theorem add {f_exp g_exp : ℝ} (hf : Majorized f b f_exp)
+    (hg : Majorized g b g_exp) (hf_exp : f_exp ≤ exp) (hg_exp : g_exp ≤ exp) :
+    Majorized (f + g) b exp := by
+  simp only [Majorized] at *
   intro exp' h_exp'
   exact (hf _ (by order)).add (hg _ (by order))
 
-/-- The product of two functions that are majorated with exponents `f_exp` and `g_exp` is
-majorated with exponent `f_exp + g_exp`. -/
-theorem mul {f_exp g_exp : ℝ} (hf : Majorated f b f_exp)
-    (hg : Majorated g b g_exp) (h_pos : ∀ᶠ t in atTop, 0 < b t) :
-    Majorated (f * g) b (f_exp + g_exp) := by
-  simp only [Majorated] at *
+/-- The product of two functions that are majorized with exponents `f_exp` and `g_exp` is
+majorized with exponent `f_exp + g_exp`. -/
+theorem mul {f_exp g_exp : ℝ} (hf : Majorized f b f_exp)
+    (hg : Majorized g b g_exp) (h_pos : ∀ᶠ t in atTop, 0 < b t) :
+    Majorized (f * g) b (f_exp + g_exp) := by
+  simp only [Majorized] at *
   intro exp h_exp
   let ε := (exp - f_exp - g_exp) / 2
   specialize hf (f_exp + ε) (by dsimp [ε]; linarith)
@@ -115,6 +115,6 @@ theorem mul {f_exp g_exp : ℝ} (hf : Majorated f b f_exp)
   simp only [Pi.pow_apply]
   conv_rhs => rw [show exp = (f_exp + ε) + (g_exp + ε) by dsimp [ε]; ring_nf, Real.rpow_add hx]
 
-end Majorated
+end Majorized
 
 end Tactic.ComputeAsymptotics
