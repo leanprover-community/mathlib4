1dffb324c114df29b13799307b28ac4ef7e3da5c
Andrew Yang <the.erd.one@gmail.com>
2026-02-04 11:43:26 +0000

    comments

--- Mathlib/RingTheory/Unramified/LocalStructure.lean
+++ Mathlib/RingTheory/Unramified/LocalStructure.lean
@@ -75,11 +75,11 @@ namespace Algebra.IsUnramifiedAt
 private theorem exist_HasStandardEtaleSurjectionOn_of_exists_adjoin_singleton_eq_top_aux₁
     (P : Ideal R) [P.IsPrime] (x : S) (hx : adjoin R {x} = ⊤) :
     (RingHom.ker (aeval (R := R) x).toRingHom).map (mapRingHom (algebraMap R P.ResidueField)) =
-      RingHom.ker (aeval (1 ⊗ₜ x : P.ResidueField ⊗[R] S)).toRingHom := by
+      RingHom.ker (aeval (1 ⊗ₜ x : P.Fiber S)).toRingHom := by
   have hx' : Function.Surjective (aeval (R := R) x) :=
     (AlgHom.range_eq_top _).mp ((adjoin_singleton_eq_range_aeval R x).symm.trans hx)
   let I := RingHom.ker (RingHomClass.toRingHom <| aeval (R := R) x)
-  let e : P.ResidueField ⊗[R] S ≃ₐ[P.ResidueField]
+  let e : P.Fiber S ≃ₐ[P.ResidueField]
       P.ResidueField[X] ⧸ (I.map (mapRingHom (algebraMap _ P.ResidueField))) :=
     Polynomial.fiberEquivQuotient (aeval (R := R) x) hx' _
   rw [← RingHom.ker_comp_of_injective _ (f := e.toRingHom) e.injective]
@@ -94,14 +94,14 @@ private theorem exist_HasStandardEtaleSurjectionOn_of_exists_adjoin_singleton_eq
 
 attribute [local instance] Algebra.TensorProduct.rightAlgebra in
 private theorem exist_HasStandardEtaleSurjectionOn_of_exists_adjoin_singleton_eq_top_aux₂
-    (P : Ideal R) [P.IsPrime] (Q : Ideal S) [Q.IsPrime]
+    {P : Ideal R} [P.IsPrime] {Q : Ideal S} [Q.IsPrime]
     [Q.LiesOver P] [Algebra.IsUnramifiedAt R Q] (x : S) (p : R[X])
     (hp₁ : Ideal.span {p.map (algebraMap R P.ResidueField)} =
       RingHom.ker (aeval ((1 : P.ResidueField) ⊗ₜ[R] x)).toRingHom)
     (hp₂ : Algebra.adjoin R {x} = ⊤) :
     ¬ minpoly P.ResidueField (algebraMap S Q.ResidueField x) ^ 2 ∣
       p.map (algebraMap R P.ResidueField) := by
-  let Q' : Ideal (P.ResidueField ⊗[R] S) :=
+  let Q' : Ideal (P.Fiber S) :=
     (PrimeSpectrum.primesOverOrderIsoFiber R S P ⟨Q, ‹_›, ‹_›⟩).asIdeal
   have : Q'.LiesOver Q := ⟨congr($((PrimeSpectrum.primesOverOrderIsoFiber R S P).symm_apply_apply
     ⟨Q, ‹_›, ‹_›⟩).1).symm⟩
@@ -119,6 +119,7 @@ private theorem exist_HasStandardEtaleSurjectionOn_of_exists_adjoin_singleton_eq
   · apply Algebra.algebra_ext; intros r; congr 1; ext x; simp [← IsScalarTower.algebraMap_apply]
   · simp [← Algebra.TensorProduct.right_algebraMap_apply, ← IsScalarTower.algebraMap_apply]
 
+attribute [local simp] aeval_algebraMap_apply in
 lemma exist_HasStandardEtaleSurjectionOn_of_exists_adjoin_singleton_eq_top
     [Module.Finite R S] (H : ∃ x : S, Algebra.adjoin R {x} = ⊤)
     (Q : Ideal S) [Q.IsPrime] [Algebra.IsUnramifiedAt R Q] :
@@ -126,74 +127,70 @@ lemma exist_HasStandardEtaleSurjectionOn_of_exists_adjoin_singleton_eq_top
   cases subsingleton_or_nontrivial S
   · cases Ideal.IsPrime.ne_top' (Subsingleton.elim Q ⊤)
   have := (algebraMap R S).domain_nontrivial
+  -- Suppose `S = R[X]/I` is finite (as an `R`-module), and `R`-unramified at a prime `Q`,
+  -- which lies over the prime `P` of `R`.
+  -- We shall show that `S[1/f]` has a surjection from a standard etale algebra for some `f ∉ Q`.
   let P := Q.under R
   obtain ⟨x, hx⟩ := H
+  have hRx : IsIntegral R x := Algebra.IsIntegral.isIntegral _
   let I := RingHom.ker (aeval (R := R) x).toRingHom
   have hx' : Function.Surjective (aeval (R := R) x) :=
     (AlgHom.range_eq_top _).mp ((adjoin_singleton_eq_range_aeval R x).symm.trans hx)
-  obtain ⟨p, hpI, hp⟩ := Ideal.exists_mem_span_singleton_map_residueField_eq P I
-  have hI' : I.map (mapRingHom (algebraMap R P.ResidueField)) =
-      RingHom.ker (aeval (1 ⊗ₜ x : P.ResidueField ⊗[R] S)).toRingHom :=
-    exist_HasStandardEtaleSurjectionOn_of_exists_adjoin_singleton_eq_top_aux₁ P x hx
-  have hmp₁ : minpoly P.ResidueField (algebraMap S Q.ResidueField x) ∣ p.map (algebraMap _ _) := by
-    rw [minpoly.dvd_iff, aeval_map_algebraMap, aeval_algebraMap_apply,
-      show aeval x p = 0 from RingHom.mem_ker.mp hpI, map_zero]
-  have hmp₂ :
-      ¬ minpoly P.ResidueField (algebraMap S Q.ResidueField x) ^ 2 ∣ p.map (algebraMap _ _) :=
-    exist_HasStandardEtaleSurjectionOn_of_exists_adjoin_singleton_eq_top_aux₂
-      P Q x p (hp.trans hI') hx
-  let q := minpoly R x ^ (p.natDegree + 2) + p
-  have ⟨w, h₁, h₂⟩ : ∃ w, q.map (algebraMap R P.ResidueField) =
-      p.map (algebraMap R P.ResidueField) * w ∧
-        ¬ minpoly P.ResidueField (algebraMap S Q.ResidueField x) ∣ w := by
-    obtain ⟨w, hw⟩ := Ideal.mem_span_singleton.mp
-      (hp.ge (Ideal.mem_map_of_mem _ (x := minpoly R x) (by simp [I])))
-    refine ⟨1 + w * (minpoly R x).map (algebraMap R P.ResidueField) ^ (p.natDegree + 1), ?_, ?_⟩
-    · simp_all [q]; grind
-    · rw [dvd_add_left (dvd_mul_of_dvd_right (dvd_pow
-        (by simp [minpoly.dvd_iff, aeval_algebraMap_apply]) (by simp)) _),
-        ← isUnit_iff_dvd_one]
-      exact (minpoly.prime (Algebra.IsIntegral.isIntegral _)).not_unit
-  have hq : q.Monic := by
-    rw [Monic, leadingCoeff_add_of_degree_lt']
-    · exact (minpoly.monic (Algebra.IsIntegral.isIntegral x)).pow _
-    · refine degree_lt_degree ?_
-      rw [natDegree_pow']
-      · refine ((Nat.lt_succ_self _).trans (Nat.lt_succ_self _)).trans_le ?_
-        exact Nat.le_mul_of_pos_right _ (minpoly.natDegree_pos (Algebra.IsIntegral.isIntegral x))
-      · simp [minpoly.monic (Algebra.IsIntegral.isIntegral x)]
-  have : Algebra.IsSeparable P.ResidueField Q.ResidueField :=
-    ((Algebra.isUnramifiedAt_iff_map_eq _ _ _).mp inferInstance).1
-  have hqx : aeval x q.derivative ∉ Q := by
-    rw [← Ideal.ker_algebraMap_residueField Q, RingHom.mem_ker, ← aeval_algebraMap_apply,
-      ← aeval_map_algebraMap P.ResidueField, ← derivative_map, h₁, ← minpoly.dvd_iff]
-    simp only [derivative_mul, derivative_map]
-    rw [dvd_add_left (dvd_mul_of_dvd_left hmp₁ _),
-      (minpoly.prime (Algebra.IsIntegral.isIntegral _)).dvd_mul, not_or]
-    refine ⟨fun h₃ ↦ hmp₂ ?_, h₂⟩
-    obtain ⟨c, hc⟩ := hmp₁
-    letI : derivative (minpoly P.ResidueField ((algebraMap S Q.ResidueField) x)) ≠ 0 := fun h ↦
-      (separable_iff_derivative_ne_zero (minpoly.prime (IsIntegral.isIntegral
-        (algebraMap S Q.ResidueField x))).irreducible).not_left.mpr h
-          (Algebra.IsSeparable.isSeparable P.ResidueField (algebraMap S Q.ResidueField x))
-    rw [← derivative_map, hc, derivative_mul, dvd_add_left (by simp),
-      (minpoly.prime (Algebra.IsIntegral.isIntegral _)).dvd_mul, dvd_derivative_iff] at h₃
-    obtain ⟨d, rfl⟩ := h₃.resolve_left this
-    exact ⟨d, by linear_combination hc⟩
-  let P : StandardEtalePair R := ⟨q, hq, q.derivative, 1, 0, 1, by simp⟩
-  have hP : P.HasMap (algebraMap _ (Localization.Away (aeval x q.derivative)) x) := by
-    constructor
-    · rw [aeval_algebraMap_apply, show aeval x P.f = 0 by simpa [P, q], map_zero]
-    · rw [aeval_algebraMap_apply]; exact IsLocalization.Away.algebraMap_isUnit _
-  have : Function.Surjective (P.lift _ hP) := by
-    intro a
+  -- It suffices to find some monic `q : R[X]` such that `q(x) = 0` and `q'(x) ∉ Q`.
+  suffices ∃ q : R[X], q.Monic ∧ aeval x q = 0 ∧ aeval x q.derivative ∉ Q by
+    -- Since if we have such a `q`, then `(R[X]/q)[1/q'] → S[1/q'(x)]` is the desired surjection.
+    obtain ⟨q, hq, hqx, hq'x⟩ := this
+    let P : StandardEtalePair R := ⟨q, hq, q.derivative, 1, 0, 1, by simp⟩
+    have hP : P.HasMap (algebraMap _ (Localization.Away (aeval x q.derivative)) x) :=
+      ⟨by simp_all [P], by simpa using IsLocalization.Away.algebraMap_isUnit _⟩
+    refine ⟨aeval x q.derivative, hq'x, .mk (P.lift _ hP) fun a ↦ ?_⟩
     obtain ⟨a, ⟨_, n, rfl⟩, rfl⟩ := IsLocalization.exists_mk'_eq (.powers (aeval x q.derivative)) a
     obtain ⟨a, rfl⟩ := hx' a
     refine ⟨Ideal.Quotient.mk _ (C a * .X ^ n), ?_⟩
     dsimp [StandardEtalePair.Ring, StandardEtalePair.lift]
     simp only [map_mul, map_pow, aeval_X, eval_mul, eval_pow, eval_C,
       ← Units.inv_pow_eq_pow_inv, Units.mul_inv_eq_iff_eq_mul]
-    simp [aeval_algebraMap_apply, ← map_pow, P]
-  exact ⟨aeval x q.derivative, hqx, .mk _ this⟩
+    simp [← map_pow, P]
+  -- Using the fact that `κ(P)[X]` is a PID, the image of `I` in `κ(P)[X]`
+  -- (i.e. the kernel of `κ(P)[X] → κ(P) ⊗[R] S`) is generated by a single polynomial `p ∈ I`.
+  obtain ⟨p, hpI, hp⟩ := Ideal.exists_mem_span_singleton_map_residueField_eq P I
+  have hI' : I.map (mapRingHom (algebraMap R P.ResidueField)) =
+      RingHom.ker (aeval (1 ⊗ₜ x : P.Fiber S)).toRingHom :=
+    exist_HasStandardEtaleSurjectionOn_of_exists_adjoin_singleton_eq_top_aux₁ P x hx
+  -- Let `x` denote the image of `X` in `S`,
+  -- and let `m` be the minimal polynomial of `x` (viewed as an element of `κ(Q)`) over `κ(P)`.
+  -- By unramified-ness we know that `m` divides `p` only once.
+  -- (via `Algebra.IsUnramifiedAt.not_minpoly_sq_dvd`).
+  let m := minpoly P.ResidueField (algebraMap S Q.ResidueField x)
+  have hm : Prime m := minpoly.prime (Algebra.IsIntegral.isIntegral _)
+  have hmp₁ : m ∣ p.map (algebraMap _ _) := by simp_all [m, I, minpoly.dvd_iff]
+  have hmp₂ : ¬ m ^ 2 ∣ p.map (algebraMap _ _) :=
+    exist_HasStandardEtaleSurjectionOn_of_exists_adjoin_singleton_eq_top_aux₂ x p (hp.trans hI') hx
+  -- But the issue is that `p` is not necessarily monic.
+  -- Let `q := M + p` for some monic `M ∈ I` with large enough degree (since `S` is `R`-finite).
+  -- I claim that `q` satisfies the desired properties.
+  let q := minpoly R x ^ (p.natDegree + 2) + p
+  refine ⟨q, ?_, by simpa [q], ?_⟩
+  · refine ((minpoly.monic hRx).pow _).add_of_left (degree_lt_degree ?_)
+    grw [natDegree_pow' (by simp [minpoly.monic hRx]),
+      ← Nat.le_mul_of_pos_right _ (minpoly.natDegree_pos hRx)]; lia
+  -- To show that `q'(x) ∉ Q`, we first show that `m` still divides `q` only once in `κ(P)[X]`.
+  have ⟨w, h₁, h₂⟩ : ∃ w, q.map (algebraMap R _) = p.map (algebraMap R _) * w ∧ ¬ m ∣ w := by
+    obtain ⟨w, hw⟩ := Ideal.mem_span_singleton.mp
+      (hp.ge (Ideal.mem_map_of_mem _ (x := minpoly R x) (by simp [I])))
+    refine ⟨1 + w * (minpoly R x).map (algebraMap R P.ResidueField) ^ (p.natDegree + 1), ?_, ?_⟩
+    · simp_all [q]; grind
+    · rw [dvd_add_left (dvd_mul_of_dvd_right (dvd_pow (by simp [m, minpoly.dvd_iff]) (by simp)) _),
+        ← isUnit_iff_dvd_one]
+      exact hm.not_unit
+  have hm' : derivative m ≠ 0 :=
+    have : Algebra.IsSeparable P.ResidueField Q.ResidueField :=
+      ((Algebra.isUnramifiedAt_iff_map_eq _ _ _).mp inferInstance).1
+    (separable_iff_derivative_ne_zero hm.irreducible).mp (Algebra.IsSeparable.isSeparable ..)
+  suffices ¬m ∣ derivative (q.map (algebraMap R _)) by
+    rwa [← Ideal.ker_algebraMap_residueField Q, RingHom.mem_ker, ← aeval_algebraMap_apply,
+      ← aeval_map_algebraMap P.ResidueField, ← derivative_map, ← minpoly.dvd_iff]
+  obtain ⟨c, hc⟩ := hmp₁
+  simp_all [hm.dvd_mul, dvd_add_left, pow_two, mul_dvd_mul_iff_left, hm.ne_zero]
 
 end Algebra.IsUnramifiedAt
