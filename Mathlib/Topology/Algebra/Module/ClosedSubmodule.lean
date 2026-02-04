4335add50f06f3f6bab70f54169efa0d33cc3028
Yoh Tanimoto <hoyt@jcom.home.ne.jp>
2026-02-04 08:08:54 +0100

    docstring

--- Mathlib/Topology/Algebra/Module/ClosedSubmodule.lean
+++ Mathlib/Topology/Algebra/Module/ClosedSubmodule.lean
@@ -310,8 +310,8 @@ namespace ClosedSubmodule
 
 variable (f : M ≃L[R] N)
 
-/-- A continuous equivalnce between `Module R M` and `Module R N` induces an equivalence between
-`ClosedSubmodule R M` and `ClosedSubmodule R M` through `map f`. -/
+/-- A continuous equivalnce `f` between modules `M` and `N` on `R` induces an equivalence between
+closed submodules in `M` and those in `N` through `map f`. -/
 def mapEquiv : ClosedSubmodule R M ≃ ClosedSubmodule R N where
   toFun s := ⟨s.toSubmodule.map f.toLinearMap, by simpa using s.isClosed⟩
   invFun t := ⟨t.toSubmodule.map f.symm.toLinearMap, by simpa using t.isClosed⟩
