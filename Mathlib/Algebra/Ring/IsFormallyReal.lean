4935b19a734a2fc75f862a15f782afa1a0b2ab2a
Artie Khovanov <artem.khovanov@gmail.com>
2026-02-05 02:29:01 +0000

    fix doc
    Add a TODO comment regarding aesop timeout in theorem.

--- Mathlib/Algebra/Ring/IsFormallyReal.lean
+++ Mathlib/Algebra/Ring/IsFormallyReal.lean
@@ -86,7 +86,7 @@ end AddSubsemigroup
 theorem AddSubsemigroup.closure_mul_self [AddMonoid R] [Mul R] :
     closure {x * x | x ≠ (0 : R)} = sumNonzeroSq R := by
   refine closure_eq_of_le (fun x hx ↦ by aesop) (fun x hx ↦ ?_)
-  -- TODO : fix aesop timeout and change to `induction hx <;> aesop
+  -- TODO : fix aesop timeout and change to `induction hx <;> aesop`
   induction hx with
   | sq ha => aesop
   | sq_add ha hs ih =>
