8ba8007e3a9ac46cdde78c48f59191f4a803dceb
Xavier Roblot <46200072+xroblot@users.noreply.github.com>
2026-02-05 09:22:37 +0100

    duplicate

--- Mathlib/GroupTheory/QuotientGroup/Defs.lean
+++ Mathlib/GroupTheory/QuotientGroup/Defs.lean
@@ -274,25 +274,6 @@ theorem lift_surjective_of_surjective (φ : G →* M) (hφ : Function.Surjective
     Function.Surjective (QuotientGroup.lift N φ HN) :=
   Quotient.lift_surjective _ _ hφ
 
-/-- A surjective group homomorphism `φ : G →* H` with `N = ker(φ)` descends (i.e. `lift`s) to a
-group isomorphism `G/N ≃* H`. -/
-@[to_additive /-- A surjective `AddGroup` homomorphism `φ : G →+ H` with `N = ker(φ)` descends
-(i.e. `lift`s) to an `AddGroup` isomorphism `G/N ≃+ H`. -/]
-noncomputable def liftEquiv {φ : G →* H} (hφ : Function.Surjective φ)
-    (HN : N = φ.ker) : G ⧸ N ≃* H :=
-  MulEquiv.ofBijective (QuotientGroup.lift N φ HN.le)
-    ⟨fun x y ↦ Quotient.inductionOn₂' x y fun a b (h : φ a = φ b) ↦ Quotient.sound' <|
-      by rw [leftRel_apply, HN, MonoidHom.mem_ker, φ.map_mul, ← h, φ.map_inv, inv_mul_cancel],
-    lift_surjective_of_surjective N φ hφ HN.le⟩
-
-@[to_additive (attr := simp)]
-theorem liftEquiv_mk {φ : G →* H} (hφ : Function.Surjective φ) (HN : N = φ.ker) (g : G) :
-    liftEquiv N hφ HN (g : Q) = φ g := rfl
-
-@[to_additive (attr := simp)]
-theorem liftEquiv_mk' {φ : G →* H} (hφ : Function.Surjective φ) (HN : N = φ.ker) (g : G) :
-    liftEquiv N hφ HN (mk g : Q) = φ g := rfl
-
 @[to_additive]
 theorem ker_lift (φ : G →* M) (HN : N ≤ φ.ker) :
     (QuotientGroup.lift N φ HN).ker = Subgroup.map (QuotientGroup.mk' N) φ.ker := by
