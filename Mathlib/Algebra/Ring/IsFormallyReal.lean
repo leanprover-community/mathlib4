fd25d8fd7b3c03241d9d33e50f0868b6f6ba3542
artie2000 <artem.khovanov@gmail.com>
2026-02-05 00:23:42 +0000

    fix

--- Mathlib/Algebra/Ring/IsFormallyReal.lean
+++ Mathlib/Algebra/Ring/IsFormallyReal.lean
@@ -86,9 +86,15 @@ end AddSubsemigroup
 theorem AddSubsemigroup.closure_mul_self [AddMonoid R] [Mul R] :
     closure {x * x | x ≠ (0 : R)} = sumNonzeroSq R := by
   refine closure_eq_of_le (fun x hx ↦ by aesop) (fun x hx ↦ ?_)
+  -- TODO : fix aesop timeout and change to `induction hx <;> aesop
   induction hx with
   | sq ha => aesop
-  | sq_add ha hs ih => aesop
+  | sq_add ha hs ih =>
+    -- `aesop` times out
+    apply add_mem
+    · apply AddSubsemigroup.mem_closure_of_mem
+      aesop
+    aesop
 
 end IsSumNonzeroSq
 
