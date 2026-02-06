620b3ff2c1272175dd8372f98b752be80d49982a
Floris van Doorn <fpvdoorn@gmail.com>
2026-02-06 19:56:58 +0100

    fix

--- Mathlib/Data/Finset/Lattice/Lemmas.lean
+++ Mathlib/Data/Finset/Lattice/Lemmas.lean
@@ -68,16 +68,16 @@ theorem Nonempty.inr {s t : Finset α} (h : t.Nonempty) : (s ∪ t).Nonempty :=
 theorem insert_eq (a : α) (s : Finset α) : insert a s = {a} ∪ s :=
   rfl
 
-/- We formulate `singleton_union` after `union_singleton` to ensure that `{x} ∪ {y}`
-simplifies to `{x, y}` and not `{y, x}`. -/
-
-@[simp, grind =]
-lemma union_singleton (x : α) (s : Finset α) : s ∪ {x} = insert x s := by
-  rw [Finset.union_comm]
-  rfl
-
 @[simp, grind =]
 lemma singleton_union (x : α) (s : Finset α) : {x} ∪ s = insert x s :=
+  rfl
+
+/- We lowe the simp-priority of `union_singleton` to ensure that `{x} ∪ {y}`
+simplifies to `{x, y}` and not `{y, x}`. -/
+
+@[simp 900, grind =]
+lemma union_singleton (x : α) (s : Finset α) : s ∪ {x} = insert x s := by
+  rw [Finset.union_comm]
   rfl
 
 @[simp, grind =]
