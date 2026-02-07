e5779c6ee468a9fcf4fa390ca21fb67dafad9d4e
Rida Hamadani <mridahamadani@gmail.com>
2026-02-07 18:48:38 +0100

    delete merged lemmas
    Remove theorems related to subwalks in SimpleGraph.

--- Mathlib/Combinatorics/SimpleGraph/Walks/Subwalks.lean
+++ Mathlib/Combinatorics/SimpleGraph/Walks/Subwalks.lean
@@ -237,35 +237,6 @@ theorem drop_isSubwalk_drop {u v n k} (p : G.Walk u v) (h : n ≤ k) :
     | zero => exact p.drop_zero ▸ (p.isSubwalk_rfl.copy rfl rfl p.getVert_zero.symm rfl).tail
     | succ _ ih => cases p <;> simp [drop, ih]
 
-theorem take_isSubwalk_take_succ {u v} (p : G.Walk u v) (n : ℕ) :
-    (p.take n).IsSubwalk (p.take (n + 1)) := by
-  cases p
-  · exact isSubwalk_take _ _
-  · cases n
-    · exact isSubwalk_of_append_left rfl
-    simp [getVert_cons_succ, take, isSubwalk_iff_support_isInfix, support_cons,
-      take_support_eq_support_take_succ, ← List.take_succ_cons, List.IsPrefix.isInfix]
-
-theorem take_isSubwalk_take {u v n k} (p : G.Walk u v) (h : n ≤ k) :
-    (p.take n).IsSubwalk (p.take k) := by
-  obtain ⟨t, rfl⟩ := Nat.exists_eq_add_of_le h
-  induction t with
-  | zero => rfl
-  | succ t ih => exact (ih (n.le_add_right t)).trans <| p.take_isSubwalk_take_succ (n + t)
-
-theorem drop_succ_isSubwalk_drop {u v} (p : G.Walk u v) (n : ℕ) :
-    (p.drop (n + 1)).IsSubwalk (p.drop n) := by
-  induction n generalizing p u with
-  | zero => exact p.drop_zero ▸ (p.isSubwalk_rfl.copy rfl rfl p.getVert_zero.symm rfl).tail
-  | succ _ ih => cases p <;> simp [drop, ih]
-
-theorem drop_isSubwalk_drop {u v n k} (p : G.Walk u v) (h : n ≤ k) :
-    (p.drop k).IsSubwalk (p.drop n) := by
-  obtain ⟨t, rfl⟩ := Nat.exists_eq_add_of_le h
-  induction t with
-  | zero => rfl
-  | succ t ih => exact (drop_succ_isSubwalk_drop p (n + t)).trans <| ih (n.le_add_right t)
-
 end Walk
 
 end SimpleGraph
