9e7f7354c20ffa08c33276d5beaf213ce44426bc
IntGrah <148660026+IntGrah@users.noreply.github.com>
2026-02-08 05:47:51 +0000

    Add deprecated

--- Mathlib/CategoryTheory/Limits/Types/Coproducts.lean
+++ Mathlib/CategoryTheory/Limits/Types/Coproducts.lean
@@ -198,6 +198,8 @@ noncomputable def initialIso : ⊥_ Type u ≅ PEmpty :=
 noncomputable def isInitialPEmpty : IsInitial (PEmpty : Type u) :=
   initialIsInitial.ofIso initialIso
 
+@[deprecated (since := "2026-02-08")] alias isInitialPunit := isInitialPEmpty
+
 /-- An object in `Type u` is initial if and only if it is empty. -/
 lemma initial_iff_empty (X : Type u) : Nonempty (IsInitial X) ↔ IsEmpty X := by
   constructor
