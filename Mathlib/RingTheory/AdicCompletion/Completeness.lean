eb91a0221ec0e8d7e882b8d6b509340d2b5b45bf
Bingyu Xia <71547343+BryceT233@users.noreply.github.com>
2026-02-08 11:20:44 +0800

    move a lemma

--- Mathlib/RingTheory/AdicCompletion/Completeness.lean
+++ Mathlib/RingTheory/AdicCompletion/Completeness.lean
@@ -71,29 +71,6 @@ theorem smul_top_eq_range_directSum (s : Set R) : (s • ⊤ : Submodule R M) =
     (DirectSum.toModule R s M (fun r ↦ LinearMap.lsmul R M r.val)).range := by
   simpa using image_smul_top_eq_range_directSum (M := M) s id
 
-open scoped Pointwise in
-theorem restrictScalars_image_smul_eq_smul_restrictScalars {S : Type*} [CommSemiring S] [Module S M]
-    [Algebra S R] [IsScalarTower S R M] (s : Set S) (N : Submodule R M) :
-      ((algebraMap S R) '' s • N).restrictScalars S = s • N.restrictScalars S := by
-  refine le_antisymm (fun x x_in ↦ ?_) (set_smul_le _ _ _ fun r x r_in x_in ↦ ?_)
-  · rw [restrictScalars_mem] at x_in
-    refine set_smul_inductionOn x x_in ?_ ?_ (fun _ _ _ _ h h' ↦  add_mem h h') (zero_mem _)
-    · rintro _ x ⟨r, r_in, rfl⟩ x_in
-      rw [IsScalarTower.algebraMap_smul R r]
-      exact mem_set_smul_of_mem_mem r_in x_in
-    · intro r y h h'
-      obtain ⟨c, c_supp, hc⟩ := (mem_set_smul ..).mp <| smul_mem _ r h
-      simp only [hc, Finsupp.sum, AddSubmonoidClass.coe_finset_sum, SetLike.val_smul]
-      refine sum_mem fun u u_in ↦ ?_
-      obtain ⟨u, u_in', rfl⟩ := c_supp (Finset.mem_coe.mpr u_in)
-      rw [IsScalarTower.algebraMap_smul]
-      refine mem_set_smul_of_mem_mem u_in' ?_
-      rw [restrictScalars_mem]
-      exact coe_mem (c ((algebraMap S R) u))
-  · rw [restrictScalars_mem] at x_in
-    rw [restrictScalars_mem, ← IsScalarTower.algebraMap_smul R r]
-    exact mem_set_smul_of_mem_mem (Set.mem_image_of_mem _ r_in) x_in
-
 end Submodule
 
 end preliminary
