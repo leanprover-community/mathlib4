6fe914c86b9a38a9fb35b357c6445bbf1dca44ab
thorimur <68410468+thorimur@users.noreply.github.com>
2026-02-07 22:30:57 -0500

    temp: reinstate debugging

--- Mathlib/Tactic/Linter/OverlappingInstances.lean
+++ Mathlib/Tactic/Linter/OverlappingInstances.lean
@@ -145,7 +145,10 @@ partial def findOverlappingDataInstances : MetaM Overlaps := do
         then `fvar` and `fvar₀` overlap (or will overlap) on some parent of the current `cls`.
         We should (only) record overlaps on the maximal parent class(es); the current overlap is
         therefore redundant, and we skip it. -/
-        unless hasAnyParentWhich isProjectionOfFVar₀ projClasses parentIdxs do
+        if hasAnyParentWhich isProjectionOfFVar₀ projClasses parentIdxs then
+          trace[debug] "  False alarm, parent will be recorded"
+        else
+          trace[debug] "  Recording overlap"
           overlaps := overlaps.pushOverlap (fvar₀, clsIsTypeOfFVar₀) cls (fvar, parentIdxs.isEmpty)
       else
         trace[debug] "• `{cls}`: not found."
