2ec23de7bed53bae3ce1f79eff39aa1ce067f320
Bingyu Xia <71547343+BryceT233@users.noreply.github.com>
2026-02-08 08:37:46 +0800

    apply suggestions and simplify (in progress)

--- Mathlib/RingTheory/AdicCompletion/Completeness.lean
+++ Mathlib/RingTheory/AdicCompletion/Completeness.lean
@@ -27,11 +27,11 @@ when the ideal `I` is finitely generated.
 
 ## Main results
 
-* `AdicCompletion.pow_smul_top_eq_eval_ker`: `I ^ n • AdicCompletion I M` is exactly the kernel
+* `AdicCompletion.pow_smul_top_eq_eval_ker`: `I ^ n • AdicCompletion I M` is exact the kernel
   of the evaluation map `eval I M n` when `I` is finitely generated.
 
-* `AdicCompletion.IsAdicComplete`: `AdicCompletion I M` is `I`-adically complete if `I` is
-  finitely generated.
+* `AdicCompletion.instIsAdicComplete`: The main instance showing that if `I` is finitely
+  generated, then `AdicCompletion I M` is `I`-adically complete.
 
 -/
 
@@ -46,8 +46,8 @@ variable {M : Type*} [AddCommGroup M] [Module R M]
 
 namespace Submodule
 
-open Pointwise Classical in
-theorem image_smul_top_eq_range_directSum {σ : Type*} (s : Set σ) (f : σ → R) :
+open scoped Pointwise in
+theorem image_smul_top_eq_range_directSum {σ : Type*} [DecidableEq σ] (s : Set σ) (f : σ → R) :
     (f '' s • ⊤ : Submodule R M) =
       (DirectSum.toModule R s M (fun r ↦ LinearMap.lsmul R M (f r.val))).range := by
   refine Submodule.ext (fun m ↦ ⟨fun h ↦ LinearMap.mem_range.mpr ?_, fun ⟨c, h⟩ ↦ ?_⟩)
@@ -71,27 +71,28 @@ theorem smul_top_eq_range_directSum (s : Set R) : (s • ⊤ : Submodule R M) =
     (DirectSum.toModule R s M (fun r ↦ LinearMap.lsmul R M r.val)).range := by
   simpa using image_smul_top_eq_range_directSum (M := M) s id
 
-variable (R) in
 open scoped Pointwise in
-theorem smul_top_eq_restrictScalars_image_smul_top {S : Type*} [CommSemiring S] [Module S M]
-    [Algebra S R] [IsScalarTower S R M] (s : Set S) : (s • ⊤ : Submodule S M) =
-        ((algebraMap S R) '' s • ⊤ : Submodule R M).restrictScalars S := by
-  refine le_antisymm (set_smul_le _ _ _ fun r x r_in _ ↦ ?_) ?_
-  · rw [restrictScalars_mem, ← IsScalarTower.algebraMap_smul R r]
-    exact mem_set_smul_of_mem_mem (Set.mem_image_of_mem _ r_in) mem_top
-  intro x x_in
-  rw [restrictScalars_mem] at x_in
-  refine set_smul_inductionOn x x_in ?_ ?_ (fun _ _ _ _ h h' ↦  add_mem h h') (zero_mem _)
-  · rintro _ x ⟨r, r_in, rfl⟩ _
-    rw [IsScalarTower.algebraMap_smul R r]
-    exact mem_set_smul_of_mem_mem r_in mem_top
-  · intro r x h h'
-    obtain ⟨c, c_supp, hc⟩ := (mem_set_smul ..).mp <| smul_mem _ r h
-    simp only [hc, Finsupp.sum, AddSubmonoidClass.coe_finset_sum, SetLike.val_smul]
-    refine sum_mem fun u u_in ↦ ?_
-    obtain ⟨u, u_in', rfl⟩ := c_supp (Finset.mem_coe.mpr u_in)
-    rw [IsScalarTower.algebraMap_smul]
-    exact mem_set_smul_of_mem_mem u_in' mem_top
+theorem restrictScalars_image_smul_eq_smul_restrictScalars {S : Type*} [CommSemiring S] [Module S M]
+    [Algebra S R] [IsScalarTower S R M] (s : Set S) (N : Submodule R M) :
+      ((algebraMap S R) '' s • N).restrictScalars S = s • N.restrictScalars S := by
+  refine le_antisymm (fun x x_in ↦ ?_) (set_smul_le _ _ _ fun r x r_in x_in ↦ ?_)
+  · rw [restrictScalars_mem] at x_in
+    refine set_smul_inductionOn x x_in ?_ ?_ (fun _ _ _ _ h h' ↦  add_mem h h') (zero_mem _)
+    · rintro _ x ⟨r, r_in, rfl⟩ x_in
+      rw [IsScalarTower.algebraMap_smul R r]
+      exact mem_set_smul_of_mem_mem r_in x_in
+    · intro r y h h'
+      obtain ⟨c, c_supp, hc⟩ := (mem_set_smul ..).mp <| smul_mem _ r h
+      simp only [hc, Finsupp.sum, AddSubmonoidClass.coe_finset_sum, SetLike.val_smul]
+      refine sum_mem fun u u_in ↦ ?_
+      obtain ⟨u, u_in', rfl⟩ := c_supp (Finset.mem_coe.mpr u_in)
+      rw [IsScalarTower.algebraMap_smul]
+      refine mem_set_smul_of_mem_mem u_in' ?_
+      rw [restrictScalars_mem]
+      exact coe_mem (c ((algebraMap S R) u))
+  · rw [restrictScalars_mem] at x_in
+    rw [restrictScalars_mem, ← IsScalarTower.algebraMap_smul R r]
+    exact mem_set_smul_of_mem_mem (Set.mem_image_of_mem _ r_in) x_in
 
 end Submodule
 
@@ -109,12 +110,13 @@ open Submodule
 variable (M) in
 /-- The canonical inclusion from the adic completion of `I ^ n • M` to
 the adic completion of `M`. -/
-def powSmulTopInclusion (n : ℕ) := (AdicCompletion.map I (topEquiv.toLinearMap ∘ₗ
-    ((I ^ n • (⊤ : Submodule R M)).inclusion le_top)))
+def powSmulTopInclusion (n : ℕ) : AdicCompletion I ↥(I ^ n • ⊤ : Submodule R M)
+    →ₗ[AdicCompletion I R] AdicCompletion I M :=
+  AdicCompletion.map I (I ^ n • (⊤ : Submodule R M)).subtype
 
-theorem powSmulTopInclusion_val_apply {a b c : ℕ} (h : c = a + b)
+theorem powSmulTopInclusion_val_apply {a b c : ℕ} (h : c = b + a)
     {x : AdicCompletion I ↥(I ^ a • ⊤ : Submodule R M)} : (powSmulTopInclusion I M a x).val c =
-      powSmulQuotInclusion I M h (x.val b) := by
+      powSmulQuotInclusion I M h ⊤ (x.val b) := by
   rw [← x.prop (show b ≤ c by lia), powSmulTopInclusion, map_val_apply]
   refine Quotient.induction_on _ (x.val c) fun z ↦ ?_
   simp [powSmulQuotInclusion]
@@ -136,19 +138,19 @@ theorem powSmulTopInclusion_injective (n : ℕ) :
     LinearMap.map_eq_zero_iff _ (powSmulQuotInclusion_injective ..)] at hx
   simp [hx]
 
-private lemma liftOfValZeroAux_exists {a b c : ℕ} {x : AdicCompletion I M} (h : c = a + b)
-    (ha : x.val a = 0) : ∃ t, powSmulQuotInclusion I M h t = x.val c := by
+private lemma liftOfValZeroAux_exists {a b c : ℕ} {x : AdicCompletion I M} (h : c = b + a)
+    (ha : x.val a = 0) : ∃ t, powSmulQuotInclusion I M h ⊤ t = x.val c := by
   simpa [← LinearMap.mem_range, powSmulQuotInclusion_range] using
     (val_apply_mem_smul_top_iff I (show a ≤ c by lia)).mpr ha
 
 /-- An auxillary lift function used in the definition of `liftOfValZero`.
 Use `liftOfValZero` instead. -/
-def liftOfValZeroAux {a b c : ℕ} {x : AdicCompletion I M} (h : c = a + b) (ha : x.val a = 0) :
+def liftOfValZeroAux {a b c : ℕ} {x : AdicCompletion I M} (h : c = b + a) (ha : x.val a = 0) :
     ↥(I ^ a • ⊤ : Submodule R M) ⧸ I ^ b • (⊤ : Submodule R ↥(I ^ a • ⊤ : Submodule R M)) :=
   Exists.choose (liftOfValZeroAux_exists I h ha)
 
-private lemma liftOfValZeroAux_prop {a b c : ℕ} {x : AdicCompletion I M} (h : c = a + b)
-    (ha : x.val a = 0) : (powSmulQuotInclusion I M h) (liftOfValZeroAux I h ha) = x.val c :=
+private lemma liftOfValZeroAux_prop {a b c : ℕ} {x : AdicCompletion I M} (h : c = b + a)
+    (ha : x.val a = 0) : (powSmulQuotInclusion I M h ⊤) (liftOfValZeroAux I h ha) = x.val c :=
   Exists.choose_spec (liftOfValZeroAux_exists I h ha)
 
 /-- Given an element `x` in the adic completion of `M` whose projection to `M / I ^ n • M` is zero,
@@ -156,19 +158,19 @@ private lemma liftOfValZeroAux_prop {a b c : ℕ} {x : AdicCompletion I M} (h : 
 @[no_expose]
 def liftOfValZero {n : ℕ} {x : AdicCompletion I M} (hxn : x.val n = 0) :
     AdicCompletion I ↥(I ^ n • (⊤ : Submodule R M)) where
-  val i := liftOfValZeroAux I (Eq.refl (n + i)) hxn
+  val i := liftOfValZeroAux I (Eq.refl (i + n)) hxn
   property {i j} h := by
     obtain ⟨k, rfl⟩ := Nat.exists_eq_add_of_le h
-    rw [← (powSmulQuotInclusion_injective I (by rfl)).eq_iff, liftOfValZeroAux_prop,
-      ← LinearMap.comp_apply, ← factorPow_powSmulQuotInclusion_comm I (by rfl)
-      (show n + (i + k) = n + i + k by ring), LinearMap.comp_apply, liftOfValZeroAux_prop]
+    rw [← (powSmulQuotInclusion_injective I (by rfl) ⊤).eq_iff, liftOfValZeroAux_prop,
+      ← LinearMap.comp_apply, transitionMap, ← factorPow_powSmulQuotInclusion_comm I (by rfl)
+      (show i + k + n = k + (i + n) by ring), LinearMap.comp_apply, liftOfValZeroAux_prop]
     exact x.prop (by lia)
 
 @[simp]
 theorem powSmulTopInclusion_liftOfValZero_apply {n : ℕ} {x : AdicCompletion I M}
     (hxn : x.val n = 0) : powSmulTopInclusion I M n (liftOfValZero I hxn) = x := by
   ext i; by_cases h : n ≤ i
-  · obtain ⟨k, rfl⟩ := Nat.exists_eq_add_of_le h
+  · obtain ⟨k, rfl⟩ := Nat.exists_eq_add_of_le' h
     rw [powSmulTopInclusion_val_apply _ (by rfl), liftOfValZero, liftOfValZeroAux_prop]
   replace h : i ≤ n := by lia
   rw [powSmulTopInclusion_val_apply_eq_zero _ h, ← x.prop h, hxn, _root_.map_zero]
@@ -191,14 +193,14 @@ theorem map_lsmul_eq_lsmul_of (r : R) : map I (LinearMap.lsmul R M r) =
     LinearMap.lsmul (AdicCompletion I R) (AdicCompletion I M) (of I R r) := by rfl
 
 variable {I} in
-@[stacks 05GG "(2)"]
 theorem pow_smul_top_eq_eval_ker {n : ℕ} (h : I.FG) : I ^ n • ⊤ = (eval I M n).ker := by
   classical
   refine le_antisymm (pow_smul_top_le_eval_ker ..) ?_
   replace h := Ideal.FG.pow (n := n) h
   rcases h with ⟨s, hs⟩
   simp only [← hs, span_smul_eq]
-  rw [smul_top_eq_restrictScalars_image_smul_top (AdicCompletion I R), show
+  rw [← restrictScalars_top R (AdicCompletion I R) (AdicCompletion I M),
+    ← restrictScalars_image_smul_eq_smul_restrictScalars (R := AdicCompletion I R), show
     ⇑(algebraMap R (AdicCompletion I R)) = of I R by rfl, ← powSmulTopInclusion_range_eq_eval_ker,
     restrictScalars_le, image_smul_top_eq_range_directSum]
   simp only [SetLike.coe_sort_coe, ← map_lsmul_eq_lsmul_of, ← map_toModule_sum_eq_toModule_map]
@@ -215,7 +217,7 @@ theorem pow_smul_top_eq_eval_ker {n : ℕ} (h : I.FG) : I ^ n • ⊤ = (eval I 
   exact ⟨u, by rw [powSmulTopInclusion, ← LinearMap.comp_apply, map_comp]; rfl⟩
 
 variable {I} in
-/-- `AdicCompletion I M` is adic complete when `I` is finitely generated. -/
+/-- `AdicCompletion I M` is complete when `I` is finitely generated. -/
 theorem isAdicComplete (h : I.FG) : IsAdicComplete I (AdicCompletion I M) where
   prec' x hx := by
     let L : AdicCompletion I M := {
