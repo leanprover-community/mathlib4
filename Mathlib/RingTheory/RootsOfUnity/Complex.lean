a2fa831732253cc2e8923253c8e9b669503ffafb
Pavel Grigorenko <GrigorenkoPV@ya.ru>
2026-02-08 22:44:32 +0300

    relax restriction on K

--- Mathlib/RingTheory/RootsOfUnity/Complex.lean
+++ Mathlib/RingTheory/RootsOfUnity/Complex.lean
@@ -215,19 +215,42 @@ section LowOrder
 
 open Complex
 
-variable {K : Type*} [Field K]
+variable {K : Type*} [CommRing K] [IsDomain K]
 
 /-
 Cubic roots of unity
 -/
 
-lemma cubic_cyclotomic_polynomial_roots_of_sq [NeZero (2 : K)] {z s : K}
-    (hs : s * s = -3) : z ^ 2 + z + 1 = 0 ↔ z = -(1 / 2) + s / 2 ∨ z = -(1 / 2) - s / 2 := by
-  suffices 1 * (z * z) + 1 * z + 1 = 0 ↔ z = -(1 / 2) + s / 2 ∨ z = -(1 / 2) - s / 2 by
+private lemma quadratic_roots_of_unity [Invertible (2 : K)] (z : K) :
+    z ^ 2 = 1 ↔ z = 1 ∨ z = -1 := by
+  have h := quadratic_eq_zero_iff' (show discrim (1 : K) 0 (-1) = 2 * 2  by rw[discrim]; norm_num) z
+  norm_num at h
+  conv_lhs at h => rw [← @add_left_cancel_iff _ _ _ (1 : K)]
+  conv_rhs at h =>
+    congr <;> rhs
+    · apply (show (2 : K) = 1 * 2 by norm_num)
+    · apply (show (-2 : K) = -1 * 2 by norm_num)
+  conv_rhs at h => congr <;> rw [mul_left_inj_of_invertible 2]
+  ring_nf at h
+  exact h
+
+private lemma quadratic_roots_of_minus_one (s : K) (hs : -1 = s * s) (z : K) :
+  -1 = z * z ↔ z = s ∨ z = -s := by
+  constructor
+  · intro hz
+    sorry
+  · grind
+
+
+lemma cubic_cyclotomic_polynomial_roots_of_sq [Invertible (2 : K)] {z s : K}
+    (hs : s * s = -3) : z ^ 2 + z + 1 = 0 ↔ z = (s - 1) * ⅟2 ∨ z = (-s - 1) * ⅟2 := by
+  suffices 1 * (z * z) + 1 * z + 1 = 0 ↔ z = (s - 1) * ⅟2 ∨ z = (-s - 1) * ⅟2 by
     grind
-  rw [quadratic_eq_zero_iff one_ne_zero (by rw [hs, discrim]; norm_num)]
+  rw [quadratic_eq_zero_iff' (by rw [hs, discrim]; norm_num)]
+  iterate 2 rw [← mul_right_eq_iff_eq_mul_invOf]
   ring_nf
 
+omit [IsDomain K] in
 lemma cubic_cyclotomic_polynomial_ne_zero_of_sq_ne {z : K} (h : ¬ IsSquare (-3 : K)) :
     z ^ 2 + z + 1 ≠ 0 := by
   suffices 1 * (z * z) + 1 * z + 1 ≠ 0 by
@@ -235,20 +258,27 @@ lemma cubic_cyclotomic_polynomial_ne_zero_of_sq_ne {z : K} (h : ¬ IsSquare (-3 
   apply quadratic_ne_zero_of_discrim_ne_sq
   grind only [discrim, pow_two, IsSquare]
 
-lemma cubic_roots_of_unity_of_sq_eq [NeZero (2 : K)] {s : K} (hs : s * s = -3) :
-    {z : K | z^3 = 1} = {1, -(1 / 2) + s / 2, -(1 / 2) - s / 2} := by
-  have H (z : K) : z ^ 3 - 1 = (z - 1) * (z ^ 2 + z + 1) := by ring
-  grind [cubic_cyclotomic_polynomial_roots_of_sq hs]
+lemma cubic_roots_of_unity_of_sq_eq [Invertible (2 : K)] {s : K} (hs : s * s = -3) :
+    {z : K | z^3 = 1} = {1, (-s - 1) * ⅟2, (s - 1) * ⅟2} := by
+  simp only [Set.ext_iff, Set.mem_setOf_eq, Set.mem_insert_iff, Set.mem_singleton_iff]
+  intro z
+  simp [(show z ^ 3 = (z - 1) * (z ^ 2 + z + 1) + 1 by ring)]
+  grind [cubic_cyclotomic_polynomial_roots_of_sq hs,
+    quadratic_eq_zero_iff' (show discrim 1 1 1 = s * s by rw[discrim, hs]; norm_num) z]
 
 lemma cubic_roots_of_unity_of_sq_ne (h : ¬ IsSquare (-3 : K)) : {z : K | z^3 = 1} = {1} := by
-  have H (z : K) : z ^ 3 - 1 = (z - 1) * (z ^ 2 + z + 1) := by ring
-  grind [cubic_cyclotomic_polynomial_ne_zero_of_sq_ne h]
+  rw [Set.eq_singleton_iff_unique_mem, Set.mem_setOf_eq]
+  constructor
+  · rw [one_pow]
+  · intro x
+    simp [Set.mem_setOf_eq, (show x ^ 3  = (x - 1) * (x ^ 2 + x + 1) + 1 by ring)]
+    grind [cubic_cyclotomic_polynomial_ne_zero_of_sq_ne h]
 
-example : {z : ℂ | z ^ 3 = 1} = {1, -(1 / 2) + √3 / 2 * I, -(1 / 2) - √3 / 2 * I} := by
+example : {z : ℂ | z ^ 3 = 1} = {1, -(1 / 2) - √3 / 2 * I, -(1 / 2) + √3 / 2 * I} := by
   have hs : (√3 * I) * (√3 * I) = -3 := by
     ring_nf
     rw [I_sq, ← ofReal_pow, Real.sq_sqrt zero_le_three, mul_neg, mul_one, ofReal_ofNat]
-  rw [cubic_roots_of_unity_of_sq_eq hs]
+  rw [cubic_roots_of_unity_of_sq_eq hs, (show ⅟(2 : ℂ) = 1 / 2 by simp)]
   ring_nf
 
 example : {z : ℝ | z ^ 3 = 1} = {1} := by
@@ -262,13 +292,18 @@ example : {z : ℝ | z ^ 3 = 1} = {1} := by
 Quartic roots of unity
 -/
 
-lemma quartic_roots_of_unity_of_sq_eq {s : K} (hs : s * s = -1) :
-    {z : K | z ^ 4 = 1} = {1, s, -1, -s} := by grind
+lemma quartic_roots_of_unity_of_sq_eq [Invertible (2 : K)] {s : K} (hs : s * s = -1) :
+    {z : K | z ^ 4 = 1} = {1, s, -1, -s} := by
+  simp only [Set.ext_iff, Set.mem_setOf_eq, Set.mem_insert_iff, Set.mem_singleton_iff]
+  intro z
+  have : z ^ 2 = -1 ↔ z = s ∨ z = -s := by grind only [pow_two, quadratic_roots_of_minus_one]
+  grind only [(show z ^ 4 = (z ^ 2) ^ 2 by ring), quadratic_roots_of_unity]
 
 lemma quartic_roots_of_unity_of_sq_ne (h : ¬ IsSquare (-1 : K)) :
     {z : K | z ^ 4 = 1} = {1, -1} := by
-  have H (z : K) : z ^ 4 - 1 = (z - 1) * (z + 1) * (z ^ 2 + 1) := by
-    ring_nf
+  simp only [Set.ext_iff, Set.mem_setOf_eq, Set.mem_insert_iff, Set.mem_singleton_iff]
+  intro z
+  simp [show z ^ 4 = (z - 1) * (z + 1) * (z ^ 2 + 1) + 1 by ring_nf]
   grind [IsSquare, pow_two]
 
 example : {z : ℂ | z ^ 4 = 1} = {1, I, -1, -I} := quartic_roots_of_unity_of_sq_eq I_mul_I
@@ -283,37 +318,48 @@ example : {z : ℝ | z ^ 4 = 1} = {1, -1} := by
 Quintic roots of unity
 -/
 
-lemma quintic_factorize_cyclotomic_polynomial [NeZero (4 : K)] {s t₁ t₂ : K} (hs : s * s = 5)
+omit [IsDomain K] in
+lemma quintic_factorize_cyclotomic_polynomial [Invertible (4 : K)] {s t₁ t₂ : K} (hs : s * s = 5)
     (ht₁ : t₁ * t₁ = -2 * (5 + s)) (ht₂ : t₂ * t₂ = -2 * (5 - s)) (z : K) :
-    z ^ 4 + z ^ 3 + z ^ 2 + z + 1 = (z - ( (s - 1) / 4 + t₁ / 4))
-                                  * (z - ( (s - 1) / 4 - t₁ / 4))
-                                  * (z - (-(s + 1) / 4 + t₂ / 4))
-                                  * (z - (-(s + 1) / 4 - t₂ / 4)) := by
+    z ^ 4 + z ^ 3 + z ^ 2 + z + 1 = (z - ( (s - 1) * ⅟4 + t₁ * ⅟4))
+                                  * (z - ( (s - 1) * ⅟4 - t₁ * ⅟4))
+                                  * (z - (-(s + 1) * ⅟4 + t₂ * ⅟4))
+                                  * (z - (-(s + 1) * ⅟4 - t₂ * ⅟4)) := by
   have s4 : s ^ 4 = 25 := by grind
   ring_nf
   rw [sq t₁, ht₁, sq t₂, ht₂, sq s, hs, s4]
   ring_nf
   rw [sq s, hs]
-  field
+  have h1 : ⅟(4 : K) * 4 = 1 := invOf_mul_self _
+  have h2 : ⅟(4 : K) ^ 2 * 16 = 1 := by grind only [(show (16 : K) = 4 * 4 by norm_num)]
+  have h3 : ⅟(4 : K) ^ 3 * 64 = 1 := by grind only [(show (64 : K) = 16 * 4 by norm_num)]
+  have h4 : ⅟(4 : K) ^ 4 * 256 = 1 := by grind only [(show (256 : K) = 64 * 4 by norm_num)]
+  grind
 
-lemma quintic_cyclotomic_polynomial_roots_of_sq [NeZero (4 : K)] {s t₁ t₂ : K} (hs : s * s = 5)
+
+lemma quintic_cyclotomic_polynomial_roots_of_sq [Invertible (4 : K)] {s t₁ t₂ : K} (hs : s * s = 5)
     (ht₁ : t₁ * t₁ = -2 * (5 + s)) (ht₂ : t₂ * t₂ = -2 * (5 - s)) :
     {z : K | z ^ 4 + z ^ 3 + z ^ 2 + z + 1 = 0} =
-      {(s - 1) / 4 + t₁ / 4,
-       (s - 1) / 4 - t₁ / 4,
-      -(s + 1) / 4 + t₂ / 4,
-      -(s + 1) / 4 - t₂ / 4} := by
+      {(s - 1) * ⅟4 + t₁ * ⅟4,
+       (s - 1) * ⅟4 - t₁ * ⅟4,
+      -(s + 1) * ⅟4 + t₂ * ⅟4,
+      -(s + 1) * ⅟4 - t₂ * ⅟4} := by
   ext
   simp [quintic_factorize_cyclotomic_polynomial hs ht₁ ht₂, sub_eq_zero, or_assoc]
 
-lemma quintic_roots_of_unity_of_sq [NeZero (4 : K)] {s t₁ t₂ : K} (hs : s * s = 5)
+lemma quintic_roots_of_unity_of_sq [Invertible (4 : K)] {s t₁ t₂ : K} (hs : s * s = 5)
     (ht₁ : t₁ * t₁ = -2 * (5 + s)) (ht₂ : t₂ * t₂ = -2 * (5 - s)) :
   {z : K | z^5 = 1} = {1,
-      (s - 1) / 4 + t₁ / 4,
-       (s - 1) / 4 - t₁ / 4,
-      -(s + 1) / 4 + t₂ / 4,
-      -(s + 1) / 4 - t₂ / 4} := by
-  rw [← Set.singleton_union, ← quintic_cyclotomic_polynomial_roots_of_sq hs ht₁ ht₂]
+      (s - 1) * ⅟4 + t₁ * ⅟4,
+       (s - 1) * ⅟4 - t₁ * ⅟4,
+      -(s + 1) * ⅟4 + t₂ * ⅟4,
+      -(s + 1) * ⅟4 - t₂ * ⅟4} := by
+  rw [← Set.singleton_union, ← quintic_cyclotomic_polynomial_roots_of_sq hs ht₁ ht₂, Set.ext_iff]
+  intro z
+  rw [Set.mem_setOf_eq,
+      show z ^ 5 = 1 ↔ z ^ 5 - 1 = 0 by grind only,
+      show z ^ 5 - 1 = (z ^ 4 + z ^ 3 + z ^ 2 + z + 1) * (z - 1) by ring_nf,
+      mul_eq_zero]
   grind
 
 example : {z : ℂ | z ^ 5 = 1} = {1,
@@ -342,6 +388,7 @@ example : {z : ℂ | z ^ 5 = 1} = {1,
     · rw [sub_nonneg]
       refine (Real.sqrt_le_left (Nat.ofNat_nonneg' 5)).mpr (by norm_num)
   rw [quintic_roots_of_unity_of_sq hs ht₁ ht₂]
+  norm_num
   ring_nf
 
 end LowOrder
