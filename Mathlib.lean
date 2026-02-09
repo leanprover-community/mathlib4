335bb1856b00666c5ca39b687dc828bcc46b1831
Christian Merten <136261474+chrisflav@users.noreply.github.com>
2026-02-09 14:35:12 +0000

    feat(Topology/Category): the standard Grothendieck topology on `TopCat` (#34979)
    We define the Grothendieck topology generated by families of jointly surjective open embeddings on `TopCat` and show it is subcanonical. This will be used to show that for a topological space `T`, the presheaf `U ↦ C(U, T)` on `Scheme` is a Zariski-sheaf.
    
    Co-authored by: Edward van de Meent <edwardvdmeent@gmail.com>
    
    From Proetale.

--- Mathlib.lean
+++ Mathlib.lean
@@ -7168,6 +7168,7 @@ public import Mathlib.Topology.Category.TopCat.Adjunctions
 public import Mathlib.Topology.Category.TopCat.Basic
 public import Mathlib.Topology.Category.TopCat.EffectiveEpi
 public import Mathlib.Topology.Category.TopCat.EpiMono
+public import Mathlib.Topology.Category.TopCat.GrothendieckTopology
 public import Mathlib.Topology.Category.TopCat.Limits.Basic
 public import Mathlib.Topology.Category.TopCat.Limits.Cofiltered
 public import Mathlib.Topology.Category.TopCat.Limits.Konig
