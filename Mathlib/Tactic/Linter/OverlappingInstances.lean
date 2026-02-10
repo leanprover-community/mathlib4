e41f371699ce85acaeeebff4ac2c3f7e8227cd60
thorimur <68410468+thorimur@users.noreply.github.com>
2026-02-10 18:57:20 -0500

    temp: bench old version

--- Mathlib/Tactic/Linter/OverlappingInstances.lean
+++ Mathlib/Tactic/Linter/OverlappingInstances.lean
@@ -318,31 +318,86 @@ open Batteries Tactic Lint
 
 namespace Environment
 
-/--
-Lints against data-carrying overlaps between instances in the local contexts of declarations.
-Only considers declarations which originate in modules with the given prefix.
--/
-def overlappingInstancesInModsWithPrefix (modPrefix : Name) : Batteries.Tactic.Lint.Linter where
-  noErrorsFound :=
-    m!"No declarations in modules with prefix `{modPrefix}` have overlapping instance arguments"
-  errorsFound :=
-    m!"Some declarations in modules with prefix `{modPrefix}` have overlapping instance arguments"
-  test declName := do
+section bench
+
+/-- Given an instance `e`, conpute return all data carrying classes that are
+the type of `e` itself, or a child class. -/
+private partial def getStructureDataProjections (e : Expr) (acc : Array Expr := #[]) :
+    StateRefT NameSet MetaM (Array Expr) := do
+  let eType ← whnf (← inferType e)
+  if ← isProp eType then return acc
+  let .const structName us := eType.getForallBody.getAppFn
+    | throwError "{e} is not an instance of a structure"
+  if (← get).contains structName then return acc
+  modify (·.insert structName)
+  let some info := getStructureInfo? (← getEnv) structName | return acc
+  info.parentInfo.foldlM (init := acc.push eType) fun acc info ↦ do
+    if ← isInstance info.projFn then
+      let proj := Expr.app (mkAppN (.const info.projFn us) eType.getAppArgs) e
+      getStructureDataProjections proj acc
+    else
+      return acc
+
+/-- Run the overlapping instances linter on `declName`. -/
+def findDuplicateDataInstances (declName : Name) : CoreM (Option MessageData) := MetaM.run' do
+  Core.checkSystem "overlappingInstances"
+  forallTelescope (← getConstInfo declName).type fun _ _ ↦ do
+  let mut result := none
+  let mut insts : Std.HashMap Expr Expr := {}
+  for { fvar, .. } in ← getLocalInstances do
+    unless (← fvar.fvarId!.getBinderInfo).isInstImplicit do continue
+    let projClasses ← forallTelescope (← inferType fvar) fun xs _ ↦ do
+      (← getStructureDataProjections (mkAppN fvar xs) |>.run' {}).mapM (mkForallFVars xs)
+
+    for cls in projClasses do
+      if let some fvar' := insts[cls]? then
+        let type ← inferType fvar; let type' ← inferType fvar'
+        if type == type' then
+          result := m!"{result.getD m!""}There are multiple different instances of `{type}`\n\n"
+          break
+        else
+          result := m!"{result.getD m!""}`{type}` and `{type'}` both imply `{cls}`\n\n"
+      else
+        insts := insts.insert cls fvar
+  result.mapM addMessageContextFull
+
+/-- A linter for declarations which have instance hypotheses that have overlapping data. -/
+@[env_linter] def overlappingInstancesInMathlib : Lint.Linter where
+  noErrorsFound := "No declarations have overlapping instance arguments"
+  errorsFound := "Some declarations have overlapping instance arguments"
+  test := fun declName => do
     if ← isAutoDecl declName then return none
     let some s ← findModuleOf? declName | throwError "Could not find module for {declName}"
-    unless modPrefix.isPrefixOf s do return none
-    MetaM.run' do
-      forallTelescope (← getConstInfo declName).type fun _ _ => do
-        let overlaps ← findOverlappingDataInstances
-        if overlaps.isEmpty then return none else
-          some <$> overlaps.toMsg m!"declaration {.ofConstName declName}"
+    unless (`Mathlib).isPrefixOf s do return none
+    findDuplicateDataInstances declName
 
-/--
-Lints against data-carrying overlaps between instances in the local contexts of declarations.
-Only considers declarations which originate in Mathlib.
--/
-@[env_linter]
-def overlappingInstancesInMathlib := overlappingInstancesInModsWithPrefix `Mathlib
+end bench
+
+-- /--
+-- Lints against data-carrying overlaps between instances in the local contexts of declarations.
+-- Only considers declarations which originate in modules with the given prefix.
+-- -/
+-- def overlappingInstancesInModsWithPrefix (modPrefix : Name) : Batteries.Tactic.Lint.Linter where
+--   noErrorsFound :=
+--     m!"No declarations in modules with prefix `{modPrefix}` have overlapping instance arguments"
+--   errorsFound :=
+--     m!"Some declarations in modules with prefix `{modPrefix}` have overlapping instance arguments"
+--   test declName := do
+--     if ← isAutoDecl declName then return none
+--     let some s ← findModuleOf? declName | throwError "Could not find module for {declName}"
+--     unless modPrefix.isPrefixOf s do return none
+--     MetaM.run' do
+--       forallTelescope (← getConstInfo declName).type fun _ _ => do
+--         let overlaps ← findOverlappingDataInstances
+--         if overlaps.isEmpty then return none else
+--           some <$> overlaps.toMsg m!"declaration {.ofConstName declName}"
+
+-- /--
+-- Lints against data-carrying overlaps between instances in the local contexts of declarations.
+-- Only considers declarations which originate in Mathlib.
+-- -/
+-- @[env_linter]
+-- def overlappingInstancesInMathlib := overlappingInstancesInModsWithPrefix `Mathlib
 
 end Environment
 
