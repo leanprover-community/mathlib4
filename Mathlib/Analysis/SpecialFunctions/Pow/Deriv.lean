9d409ef39fe7c6858fa102c14ff18f93456f77f3
tb65536 <thomas.l.browning@gmail.com>
2026-02-10 23:19:40 +0000

    fix

--- Mathlib/Analysis/SpecialFunctions/Pow/Deriv.lean
+++ Mathlib/Analysis/SpecialFunctions/Pow/Deriv.lean
@@ -235,9 +235,9 @@ theorem HasDerivWithinAt.cpow_const (hf : HasDerivWithinAt f f' s x)
 theorem Complex.derivWithin_const_cpow (hf : DifferentiableWithinAt ℂ f s x) (c : ℂ) :
     derivWithin (fun x ↦ c ^ f x) s x = log c * derivWithin f s x * c ^ f x := by
   by_cases h : UniqueDiffWithinAt ℂ s x
-  · rw [uniqueDiffWithinAt_iff_accPt, accPt_principal_iff_nhdsWithin] at h
-    by_cases hc : c = 0
-    · simp only [hc, log_zero, zero_mul]
+  · by_cases hc : c = 0
+    · rw [uniqueDiffWithinAt_iff_accPt, accPt_principal_iff_nhdsWithin] at h
+      simp only [hc, log_zero, zero_mul]
       apply derivWithin_zero_of_frequently_mem {0, 1} (mt Set.Infinite.of_accPt (by simp))
       simpa [zero_cpow_eq_iff, em']
     · rw [mul_comm, ← mul_assoc]
