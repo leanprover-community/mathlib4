a035bde254ae5ade5e555579a9e381663ed021c6
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2026-02-10 10:35:05 +0100

    fix

--- Mathlib/LinearAlgebra/Matrix/Gershgorin.lean
+++ Mathlib/LinearAlgebra/Matrix/Gershgorin.lean
@@ -64,7 +64,7 @@ theorem det_ne_zero_of_sum_row_lt_diag (h : ∀ k, ∑ j ∈ Finset.univ.erase k
     A.det ≠ 0 := by
   contrapose! h
   suffices ∃ k, 0 ∈ Metric.closedBall (A k k) (∑ j ∈ Finset.univ.erase k, ‖A k j‖) by
-    exact this.imp (fun a h ↦ by rwa [mem_closedBall_iff_norm', sub_zero] at h)
+    exact this.imp (fun a h ↦ by rwa [mem_closedBall_iff_norm', norm_neg_add, sub_zero] at h)
   refine eigenvalue_mem_ball ?_
   rw [Module.End.hasEigenvalue_iff, Module.End.eigenspace_zero, ne_comm]
   exact ne_of_lt (LinearMap.bot_lt_ker_of_det_eq_zero (by rwa [LinearMap.det_toLin']))
