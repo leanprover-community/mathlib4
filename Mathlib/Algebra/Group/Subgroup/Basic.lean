f2e3f5f8af723381da13f27869fa3f08e923bd87
Hang Lu Su <hanglu.su@pm.me>
2026-02-11 14:40:52 +0100

    fix indent

--- Mathlib/Algebra/Group/Subgroup/Basic.lean
+++ Mathlib/Algebra/Group/Subgroup/Basic.lean
@@ -498,11 +498,10 @@ theorem normalClosure_le_normal {N : Subgroup G} [N.Normal] (h : s ⊆ N) : norm
 
 /-- The normal closure of an empty set is the trivial subgroup. -/
 @[simp]
-lemma normalClosure_empty :
-  normalClosure (∅ : Set G) = (⊥ : Subgroup G) := by
-apply le_antisymm
-· exact Subgroup.normalClosure_le_normal (N := (⊥ : Subgroup G)) (by simp)
-· exact bot_le
+lemma normalClosure_empty : normalClosure (∅ : Set G) = (⊥ : Subgroup G) := by
+  apply le_antisymm
+  · exact Subgroup.normalClosure_le_normal (N := (⊥ : Subgroup G)) (by simp)
+  · exact bot_le
 
 theorem normalClosure_subset_iff {N : Subgroup G} [N.Normal] : s ⊆ N ↔ normalClosure s ≤ N :=
   ⟨normalClosure_le_normal, Set.Subset.trans subset_normalClosure⟩
