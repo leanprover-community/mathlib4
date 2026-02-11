e6c3350187b7c2a4efbd90a614518e254a9a6cee
daniel-carranza <carranzajdaniel@gmail.com>
2026-02-11 14:11:02 -0500

    add new file on tensor product of V-categories

--- Mathlib.lean
+++ Mathlib.lean
@@ -2468,6 +2468,7 @@ public import Mathlib.CategoryTheory.Enriched.Limits.HasConicalPullbacks
 public import Mathlib.CategoryTheory.Enriched.Limits.HasConicalTerminal
 public import Mathlib.CategoryTheory.Enriched.Opposite
 public import Mathlib.CategoryTheory.Enriched.Ordinary.Basic
+public import Mathlib.CategoryTheory.Enriched.TensorProductCategory
 public import Mathlib.CategoryTheory.EpiMono
 public import Mathlib.CategoryTheory.EqToHom
 public import Mathlib.CategoryTheory.Equivalence
