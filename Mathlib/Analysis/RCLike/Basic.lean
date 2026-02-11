7207bc87aaf6c5702896a4e3862dacafe6b18869
Alex Meiburg <alex.meiburg@gmail.com>
2026-02-10 21:00:33 -0500

    Update Basic.lean

--- Mathlib/Analysis/RCLike/Basic.lean
+++ Mathlib/Analysis/RCLike/Basic.lean
@@ -1168,7 +1168,7 @@ lemma lipschitzWith_im : LipschitzWith 1 (im (K := K)) := by
 @[simp] theorem map_from_real : map ℝ K = ofRealCLM := by ext; simp
 
 open scoped ComplexOrder in
-def instOrderClosedTopology : OrderClosedTopology K where
+lemma instOrderClosedTopology : OrderClosedTopology K where
   isClosed_le' := by
     conv in _ ≤ _ => rw [RCLike.le_iff_re_im]
     simp_rw [Set.setOf_and]
