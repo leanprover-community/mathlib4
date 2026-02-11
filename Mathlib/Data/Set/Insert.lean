0358a17449304bed850bb1ff30f9e746284b3ae4
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2026-02-11 17:11:50 +0000

    make import public

--- Mathlib/Data/Set/Insert.lean
+++ Mathlib/Data/Set/Insert.lean
@@ -6,8 +6,7 @@ Authors: Jeremy Avigad, Leonardo de Moura
 module
 
 public import Mathlib.Data.Set.Disjoint
-
-import Mathlib.Tactic.Simproc.ExistsAndEq
+public import Mathlib.Tactic.Simproc.ExistsAndEq
 
 /-!
 # Lemmas about insertion, singleton, and pairs
