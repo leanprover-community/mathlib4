3b0cf528c9406f593bb01fa3d6c91b665098814c
mkaratarakis <mixkarat@gmail.com>
2026-02-11 01:30:12 +0100

    review

--- Mathlib/Analysis/Analytic/Basic.lean
+++ Mathlib/Analysis/Analytic/Basic.lean
@@ -388,8 +388,8 @@ theorem HasFPowerSeriesWithinAt.mono_of_mem_nhdsWithin
     add_sub_cancel_left, hy, and_true] at h'y ⊢
   exact h'y.2
 
-lemma hasFPowerSeriesWithinAt_iff_of_nhds (f : E → F) (p : FormalMultilinearSeries 𝕜 E F) {U : Set E}
-    (hU : U ∈ 𝓝 x) :
+lemma hasFPowerSeriesWithinAt_iff_of_nhds (f : E → F) (p : FormalMultilinearSeries 𝕜 E F)
+    {U : Set E} (hU : U ∈ 𝓝 x) :
     HasFPowerSeriesWithinAt f p U x ↔ HasFPowerSeriesAt f p x := by
   rw [← hasFPowerSeriesWithinAt_univ]
   exact ⟨fun h ↦ h.mono_of_mem_nhdsWithin (mem_nhdsWithin_of_mem_nhds hU),
@@ -535,7 +535,7 @@ lemma AnalyticOn.mono {f : E → F} {s t : Set E} (h : AnalyticOn 𝕜 f t)
 lemma AnalyticOn.analyticAt {f : E → F} {z : E} {s : Set E} (hU : s ∈ 𝓝 z)
     (h : AnalyticOn 𝕜 f s) : AnalyticAt 𝕜 f z := by
   obtain ⟨p, hp⟩ := h z (mem_of_mem_nhds hU)
-  exact ⟨p, hasFPowerSeriesWithinAt_nhds_iff f p hU |>.mp hp⟩
+  exact ⟨p, hasFPowerSeriesWithinAt_iff_of_nhds f p hU |>.mp hp⟩
 
 /-!
 ### Composition with linear maps
