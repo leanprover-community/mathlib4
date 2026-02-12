b876526173f253322c9e3a38405e1396f1d4bfcf
Kevin Wilson <1527442+khwilson@users.noreply.github.com>
2026-02-12 20:44:21 +0000

    feat(Mathlib/Analysis/Polynomial/Fourier): Add Parseval's Identity specialized to Polynomials (#33507)
    Parseval's identity specialized to polynomials states that the sum of the norm squared coefficients is equal to the average of the norm of the polynomial squared on the circle. We prove this fact.
    
    Co-authored-by: Kevin H Wilson <khwilson@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -2122,6 +2122,7 @@ public import Mathlib.Analysis.PSeriesComplex
 public import Mathlib.Analysis.Polynomial.Basic
 public import Mathlib.Analysis.Polynomial.CauchyBound
 public import Mathlib.Analysis.Polynomial.Factorization
+public import Mathlib.Analysis.Polynomial.Fourier
 public import Mathlib.Analysis.Polynomial.MahlerMeasure
 public import Mathlib.Analysis.Polynomial.Norm
 public import Mathlib.Analysis.Quaternion
