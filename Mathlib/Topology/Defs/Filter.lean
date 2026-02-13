19c44ebe55b6b80c5c0063ba65221dc05a49d671
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2026-02-13 16:20:47 +0000

    remove new superfluous imports

--- Mathlib/Topology/Defs/Filter.lean
+++ Mathlib/Topology/Defs/Filter.lean
@@ -9,7 +9,6 @@ public import Mathlib.Topology.Defs.Basic
 public import Mathlib.Data.Setoid.Basic
 public import Mathlib.Order.Filter.Defs
 public import Mathlib.Tactic.IrreducibleDef
-public import Mathlib.Tactic.ToAdditive
 
 /-!
 # Definitions about filters in topological spaces
