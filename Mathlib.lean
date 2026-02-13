2a9588f5d0f9d414ecb501fbeff9469dd1305dcb
Violeta Hernández Palacios <vi.hdz.p@gmail.com>
2026-02-13 02:37:22 +0000

    feat: order topologies of successor orders (#32455)
    Co-authored-by: Jireh Loreaux <loreaujy@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -7481,6 +7481,7 @@ public import Mathlib.Topology.Order.ProjIcc
 public import Mathlib.Topology.Order.Real
 public import Mathlib.Topology.Order.Rolle
 public import Mathlib.Topology.Order.ScottTopology
+public import Mathlib.Topology.Order.SuccPred
 public import Mathlib.Topology.Order.T5
 public import Mathlib.Topology.Order.UpperLowerSetTopology
 public import Mathlib.Topology.Order.WithTop
