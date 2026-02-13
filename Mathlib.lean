00dc616909977609b57603ec375bedfde7edc278
Andrew Yang <the.erd.one@gmail.com>
2026-02-13 06:46:38 +0100

    oops

--- Mathlib.lean
+++ Mathlib.lean
@@ -1370,7 +1370,6 @@ public import Mathlib.AlgebraicGeometry.SpreadingOut
 public import Mathlib.AlgebraicGeometry.Stalk
 public import Mathlib.AlgebraicGeometry.StructureSheaf
 public import Mathlib.AlgebraicGeometry.ValuativeCriterion
-public import Mathlib.AlgebraicGeometry.ZariskisMainTheorem
 public import Mathlib.AlgebraicTopology.AlternatingFaceMapComplex
 public import Mathlib.AlgebraicTopology.CechNerve
 public import Mathlib.AlgebraicTopology.DoldKan.Compatibility
