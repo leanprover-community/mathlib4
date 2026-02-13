0d3bfb7fff900a3a0c51ec13db0693f4cbac29ca
Joël Riou <37772949+joelriou@users.noreply.github.com>
2026-02-13 12:38:02 +0000

    feat(CategoryTheory/Limits): relation between products and wide pullbacks (#35038)
    In this PR, we show that a wide pullback of arrows `objs j ⟶ B` exists when `B` is terminal and the product of the objects `obj j` exists.

--- Mathlib.lean
+++ Mathlib.lean
@@ -2624,6 +2624,7 @@ public import Mathlib.CategoryTheory.Limits.Constructions.Over.Connected
 public import Mathlib.CategoryTheory.Limits.Constructions.Over.Products
 public import Mathlib.CategoryTheory.Limits.Constructions.Pullbacks
 public import Mathlib.CategoryTheory.Limits.Constructions.WeaklyInitial
+public import Mathlib.CategoryTheory.Limits.Constructions.WidePullbackOfTerminal
 public import Mathlib.CategoryTheory.Limits.Constructions.ZeroObjects
 public import Mathlib.CategoryTheory.Limits.Creates
 public import Mathlib.CategoryTheory.Limits.Elements
