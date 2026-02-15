ab3dd28e6a2dfc2b227ce9dae67f50f58ede2d96
euprunin <euprunin@users.noreply.github.com>
2026-02-15 09:23:01 +0100

    golf further

--- Mathlib/Probability/Kernel/MeasurableIntegral.lean
+++ Mathlib/Probability/Kernel/MeasurableIntegral.lean
@@ -91,8 +91,7 @@ theorem StronglyMeasurable.integral_kernel_prod_right ⦃f : α → β → E⦄
     intro n; refine StronglyMeasurable.indicator ?_ (measurableSet_kernel_integrable hf)
     have : ∀ x, ((s' n x).range.filter fun x => x ≠ 0) ⊆ (s n).range := by
       intro
-      refine Finset.Subset.trans (Finset.filter_subset _ _) ?_
-      exact SimpleFunc.range_comp_subset_range _ _
+      exact Finset.Subset.trans (Finset.filter_subset _ _) (SimpleFunc.range_comp_subset_range _ _)
     simp only [SimpleFunc.integral_eq_sum_of_subset (this _)]
     refine Finset.stronglyMeasurable_fun_sum _ fun x _ => ?_
     refine (Measurable.ennreal_toReal ?_).stronglyMeasurable.smul_const _
