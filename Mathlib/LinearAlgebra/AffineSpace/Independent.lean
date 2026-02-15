73c53cb63233c79936c6480ba4a65e500eba8dbc
Zheng Chu <zcyemi@gmail.com>
2026-02-15 10:01:03 +0800

    Update Mathlib/LinearAlgebra/AffineSpace/Independent.lean
    Co-authored-by: Robin Carlier <57142648+robin-carlier@users.noreply.github.com>

--- Mathlib/LinearAlgebra/AffineSpace/Independent.lean
+++ Mathlib/LinearAlgebra/AffineSpace/Independent.lean
@@ -697,8 +697,9 @@ by the line map of the corresponding weights. -/
 theorem AffineIndependent.affineCombination_eq_lineMap_iff_weight_lineMap {p : ι → P}
     (ha : AffineIndependent k p) {w w₁ w₂ : ι → k} {s : Finset ι} (hw : ∑ i ∈ s, w i = 1)
     (hw₁ : ∑ i ∈ s, w₁ i = 1) (hw₂ : ∑ i ∈ s, w₂ i = 1) (c : k) :
-    s.affineCombination k p w = AffineMap.lineMap (s.affineCombination k p w₁)
-    (s.affineCombination k p w₂) c ↔ ∀ i ∈ s, w i = AffineMap.lineMap (w₁ i) (w₂ i) c := by
+    s.affineCombination k p w =
+      AffineMap.lineMap (s.affineCombination k p w₁) (s.affineCombination k p w₂) c ↔
+        ∀ i ∈ s, w i = AffineMap.lineMap (w₁ i) (w₂ i) c := by
   rw [← AffineMap.apply_lineMap, ha.affineCombination_eq_iff_eq hw]
   · simp [AffineMap.lineMap_apply]
   · simp [AffineMap.lineMap_apply, sum_add_distrib, ← mul_sum, hw₁, hw₂]
