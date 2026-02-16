ba9e45671f210063cffd44c36a2b746583630202
Raph-DG <raphaeldouglasgiles@gmail.com>
2026-02-16 17:30:08 +0100

    updated import format

--- Mathlib/RingTheory/OrderOfVanishing/Properties.lean
+++ Mathlib/RingTheory/OrderOfVanishing/Properties.lean
@@ -3,10 +3,12 @@ Copyright (c) 2025 Raphael Douglas Giles. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Raphael Douglas Giles
 -/
-import Mathlib.RingTheory.KrullDimension.NonZeroDivisors
-import Mathlib.RingTheory.Length
-import Mathlib.RingTheory.OrderOfVanishing.Basic
-import Mathlib.RingTheory.DiscreteValuationRing.Basic
+module
+
+public import Mathlib.RingTheory.KrullDimension.NonZeroDivisors
+public import Mathlib.RingTheory.Length
+public import Mathlib.RingTheory.OrderOfVanishing.Basic
+public import Mathlib.RingTheory.DiscreteValuationRing.Basic
 
 /-!
 # Order of vanishing properties
