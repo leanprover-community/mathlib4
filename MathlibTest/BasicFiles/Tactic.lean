ad618bae7996d50608df24c1e70eed090e304af8
Snir Broshi <26556598+SnirBroshi@users.noreply.github.com>
2026-02-16 16:57:27 +0200

    update tests since `#find_syntax` was fixed

--- MathlibTest/BasicFiles/Tactic.lean
+++ MathlibTest/BasicFiles/Tactic.lean
@@ -4,9 +4,9 @@ import Mathlib.Tactic
 
 /-! Checks that some utilities are available already when importing `Mathlib.Tactic`. -/
 
-/-- info: Found 0 uses among over 0 syntax declarations -/
-#guard_msgs in
-#find_syntax "↦" approx
+/-- info: Found 5 uses among over 2000 syntax declarations -/
+#guard_msgs (substring := true) in
+#find_syntax "∘" approx
 
 -- Guard against the shake tool modifying our imports
 /-- info: [public import Init, import Mathlib.Tactic] -/
