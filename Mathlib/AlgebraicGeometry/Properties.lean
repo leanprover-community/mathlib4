dc024e4799b9fe280f7926ef6251f8cb1ac7c474
Raph-DG <77658801+Raph-DG@users.noreply.github.com>
2026-02-17 03:31:29 +1100

    Update Mathlib/AlgebraicGeometry/Properties.lean
    Co-authored-by: Andrew Yang <36414270+erdOne@users.noreply.github.com>

--- Mathlib/AlgebraicGeometry/Properties.lean
+++ Mathlib/AlgebraicGeometry/Properties.lean
@@ -3,7 +3,6 @@ Copyright (c) 2021 Andrew Yang. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Andrew Yang
 -/
-
 module
 
 public import Mathlib.AlgebraicGeometry.AffineScheme
