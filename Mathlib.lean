54fc9ca346836ac4318bdc02dab5d125ff05531c
Chris Birkbeck <c.birkbeck@uea.ac.uk>
2026-02-17 12:42:40 +0000

    mk_all

--- Mathlib.lean
+++ Mathlib.lean
@@ -5370,6 +5370,7 @@ public import Mathlib.NumberTheory.ModularForms.Bounds
 public import Mathlib.NumberTheory.ModularForms.CongruenceSubgroups
 public import Mathlib.NumberTheory.ModularForms.Cusps
 public import Mathlib.NumberTheory.ModularForms.DedekindEta
+public import Mathlib.NumberTheory.ModularForms.Delta
 public import Mathlib.NumberTheory.ModularForms.EisensteinSeries.Basic
 public import Mathlib.NumberTheory.ModularForms.EisensteinSeries.Defs
 public import Mathlib.NumberTheory.ModularForms.EisensteinSeries.E2.Defs
