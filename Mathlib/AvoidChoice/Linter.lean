85a9725203091c7ecc60a3cb9848bfff22f719fd
Riccardo Brasca <riccardo.brasca@gmail.com>
2026-02-17 09:34:17 +0100

    ops again

--- Mathlib/AvoidChoice/Linter.lean
+++ Mathlib/AvoidChoice/Linter.lean
@@ -3,11 +3,10 @@ Copyright (c) 2025 Riccardo Brasca. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Riccardo Brasca, Damiano Testa
 -/
-module
 
-public import Lean.Util.CollectAxioms
-public import Mathlib.Tactic.DeclarationNames
-public import Mathlib.Init
+import Lean.Util.CollectAxioms
+import Mathlib.Tactic.DeclarationNames
+import Mathlib.Init
 
 /-!
 #  The "detectClassical" linter
