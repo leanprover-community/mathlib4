24f55f8b9b939595116e755e7879904195391cd6
daniel-carranza <carranzajdaniel@gmail.com>
2026-02-20 12:34:35 +0100

    renaming + added TODO comment to show naturality

--- Mathlib/CategoryTheory/Monoidal/Closed/InternalCurrying.lean
+++ Mathlib/CategoryTheory/Monoidal/Closed/InternalCurrying.lean
@@ -29,55 +29,59 @@ variable {C : Type u} [Category.{v} C] [MonoidalCategory C]
 
 /-- The currying operation taking a morphism `(z ⊗ y) ⟶ x` to a morphism `y ⟶ C(z, x)`,
   constructed as a morphism in `C` between internal homs. -/
+-- TODO: Prove naturality of this morphism (requires the appropriate instances of `[Closed _]` for
+-- objects in `Cᵒᵖ`).
 def ihomCurry (x y z : C) [Closed x] [Closed y] [Closed (x ⊗ y)] :
     (ihom (x ⊗ y)).obj z ⟶ (ihom y).obj ((ihom x).obj z) :=
   curry (curry ((α_ x y _).inv ≫ (ihom.ev _).app z))
 
-lemma internalHomCurry_uncurry_eq (x y z : C) [Closed x] [Closed y] [Closed (x ⊗ y)] :
-    uncurry (internalHomCurry x y z) = curry ((α_ x y _).inv ≫ (ihom.ev _).app z) :=
+lemma uncurry_ihomCurry (x y z : C) [Closed x] [Closed y] [Closed (x ⊗ y)] :
+    uncurry (ihomCurry x y z) = curry ((α_ x y _).inv ≫ (ihom.ev _).app z) :=
   uncurry_curry _
 
-lemma internalHomCurry_uncurry_uncurry_eq (x y z : C) [Closed x] [Closed y] [Closed (x ⊗ y)] :
-    uncurry (uncurry (internalHomCurry x y z)) = (α_ x y _).inv ≫ (ihom.ev _).app z := by
-  simp only [internalHomCurry_uncurry_eq, uncurry_curry]
+lemma uncurry_uncurry_ihomCurry (x y z : C) [Closed x] [Closed y] [Closed (x ⊗ y)] :
+    uncurry (uncurry (ihomCurry x y z)) = (α_ x y _).inv ≫ (ihom.ev _).app z := by
+  simp only [uncurry_ihomCurry, uncurry_curry]
 
 /-- The uncurrying operation taking a morphism `y ⟶ C(x, z)` to a morphism `(x ⊗ y) ⟶ z`,
   constructed as a morphism in `C` between internal homs. -/
-def internalHomUncurry (x y z : C) [Closed x] [Closed y] [Closed (x ⊗ y)] :
+-- TODO: Prove naturality of this morphism (requires the appropriate instances of `[Closed _]` for
+-- objects in `Cᵒᵖ`).
+def ihomUncurry (x y z : C) [Closed x] [Closed y] [Closed (x ⊗ y)] :
     (ihom y).obj ((ihom x).obj z) ⟶ (ihom (x ⊗ y)).obj z :=
   curry ((α_ x y _).hom ≫ x ◁ (ihom.ev y).app ((ihom x).obj z) ≫ (ihom.ev x).app z)
 
-lemma internalHomUncurry_uncurry_eq (x y z : C) [Closed x] [Closed y] [Closed (x ⊗ y)] :
-    uncurry (internalHomUncurry x y z) =
-    (α_ x y _).hom ≫ x ◁ (ihom.ev y).app ((ihom x).obj z) ≫ (ihom.ev x).app z :=
+lemma uncurry_ihomUncurry (x y z : C) [Closed x] [Closed y] [Closed (x ⊗ y)] :
+    uncurry (ihomUncurry x y z) = (α_ x y _).hom ≫ x ◁ (ihom.ev y).app ((ihom x).obj z) ≫
+    (ihom.ev x).app z :=
   uncurry_curry _
 
 @[reassoc (attr := simp)]
-theorem internalHom_uncurry_curry (x y z : C) [Closed x] [Closed y] [Closed (x ⊗ y)] :
-    internalHomUncurry x y z ≫ internalHomCurry x y z = 𝟙 _ := by
+theorem ihomUncurry_ihomCurry (x y z : C) [Closed x] [Closed y] [Closed (x ⊗ y)] :
+    ihomUncurry x y z ≫ ihomCurry x y z = 𝟙 _ := by
   apply uncurry_injective
   apply uncurry_injective
-  simp only [uncurry_natural_left, uncurry_id_eq_ev, internalHomCurry_uncurry_uncurry_eq]
-  rw [associator_inv_naturality_right_assoc, ← uncurry_eq, internalHomUncurry_uncurry_eq,
+  simp only [uncurry_natural_left, uncurry_id_eq_ev, uncurry_uncurry_ihomCurry]
+  rw [associator_inv_naturality_right_assoc, ← uncurry_eq, uncurry_ihomUncurry,
     Iso.inv_hom_id_assoc]
   exact rfl
 
 @[reassoc (attr := simp)]
-theorem internalHom_curry_uncurry (x y z : C) [Closed x] [Closed y] [Closed (x ⊗ y)] :
-    internalHomCurry x y z ≫ internalHomUncurry x y z = 𝟙 _ := by
+theorem ihomCurry_ihomUncurry (x y z : C) [Closed x] [Closed y] [Closed (x ⊗ y)] :
+    ihomCurry x y z ≫ ihomUncurry x y z = 𝟙 _ := by
   apply uncurry_injective
-  rw [uncurry_natural_left, uncurry_id_eq_ev, internalHomUncurry_uncurry_eq,
+  rw [uncurry_natural_left, uncurry_id_eq_ev, uncurry_ihomUncurry,
     associator_naturality_right_assoc, ← MonoidalCategory.whiskerLeft_comp_assoc, ← uncurry_eq,
-    ← uncurry_eq, internalHomCurry_uncurry_uncurry_eq, Iso.hom_inv_id_assoc _ _]
+    ← uncurry_eq, uncurry_uncurry_ihomCurry, Iso.hom_inv_id_assoc _ _]
 
 /-- The internal currying-uncurrying isomorphism `C(x ⊗ y, z) ≅ C(y, C(x, z))`. -/
 @[simps]
 def internalHomCurryIso (x y z : C) [Closed x] [Closed y] [Closed (x ⊗ y)] :
     (ihom (x ⊗ y)).obj z ≅ (ihom y).obj ((ihom x).obj z) where
-  hom := internalHomCurry x y z
-  inv := internalHomUncurry x y z
-  hom_inv_id := internalHom_curry_uncurry x y z
-  inv_hom_id := internalHom_uncurry_curry x y z
+  hom := ihomCurry x y z
+  inv := ihomUncurry x y z
+  hom_inv_id := ihomCurry_ihomUncurry x y z
+  inv_hom_id := ihomUncurry_ihomCurry x y z
 
 end CategoryTheory.MonoidalClosed
 
