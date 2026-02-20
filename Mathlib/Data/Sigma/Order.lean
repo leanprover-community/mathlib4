aa87459bcc9783953c6c5dc3e7886aac7fb7d28c
YellPika <yellpika@gmail.com>
2026-02-20 18:17:40 -0500

    chore(Data/Sigma/Order): apply suggestions

--- Mathlib/Data/Sigma/Order.lean
+++ Mathlib/Data/Sigma/Order.lean
@@ -92,6 +92,14 @@ theorem lt_def [∀ i, LT (α i)] {a b : Σ i, α i} : a < b ↔ ∃ h : a.1 = b
     rintro ⟨rfl : i = j, h⟩
     exact LT.fiber _ _ _ h
 
+/-- To prove something for all `a ≤ b`, it suffices to prove this for all `(i, x)` and `(i, y)`
+where `x ≤ y`. -/
+@[elab_as_elim]
+lemma le_rec [∀ i, LE (α i)] {P : ∀ (a b : Σ i, α i), a ≤ b → Prop}
+    (H : ∀ i x y, (h : x ≤ y) → P ⟨i, x⟩ ⟨i, y⟩ (mk_le_mk_iff.mpr h)) {a b} (h) : P a b h := by
+  rcases h with ⟨i, _, _, h⟩
+  exact H i _ _ h
+
 protected instance preorder [∀ i, Preorder (α i)] : Preorder (Σ i, α i) :=
   { le_refl := fun ⟨i, a⟩ => Sigma.LE.fiber i a a le_rfl,
     le_trans := by
@@ -117,14 +125,17 @@ instance [∀ i, Preorder (α i)] [∀ i, DenselyOrdered (α i)] : DenselyOrdere
     obtain ⟨c, ha, hb⟩ := exists_between h
     exact ⟨⟨i, c⟩, LT.fiber i a c ha, LT.fiber i c b hb⟩
 
+@[gcongr]
 lemma fst_mono [Preorder ι] [∀ i, Preorder (α i)] : Monotone (fst : (Σ i, α i) → ι) := by
   rintro _ _ ⟨_, _, _, _⟩
   rfl
 
+@[gcongr]
 lemma snd_mono {β : Type*} [Preorder β] : Monotone (snd : (_ : ι) × β → β) := by
   rintro _ _ ⟨_, _, _, h⟩
   exact h
 
+@[gcongr]
 lemma mk_mono [∀ i, Preorder (α i)] (i : ι) : Monotone (mk i : α i → Σ i, α i) := by
   simp only [Monotone, mk_le_mk_iff, imp_self, implies_true]
 
@@ -134,10 +145,8 @@ lemma rec_mono {δ β : Type*} [Preorder δ] [Preorder β] [∀ i, Preorder (α 
     Monotone (fun x ↦ Sigma.rec (motive := fun _ ↦ β) (g x) (f x)) := by
   intro i₁ i₂ hi
   dsimp only
-  specialize hf hi
-  generalize f i₁ = fi₁, f i₂ = fi₂ at *
-  rcases hf with ⟨i, _, _, hf⟩
-  exact hg i (Prod.GCongr.mk_le_mk hi hf)
+  refine le_rec (fun i _ _ hf => ?_) (hf hi)
+  exact hg i (Prod.mk_le_mk.mpr ⟨hi, hf⟩)
 
 /-! ### Lexicographical order on `Sigma` -/
 
