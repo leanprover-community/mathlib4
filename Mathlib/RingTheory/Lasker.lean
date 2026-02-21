699864e7ce6b4e7e393a98f3be14031960d46c17
tb65536 <thomas.l.browning@gmail.com>
2026-02-21 12:40:37 +0000

    merge

--- Mathlib/RingTheory/Lasker.lean
+++ Mathlib/RingTheory/Lasker.lean
@@ -6,11 +6,7 @@ Authors: Thomas Browning, Yakov Pechersky
 module
 
 public import Mathlib.Order.Irreducible
-public import Mathlib.RingTheory.Ideal.Colon
-public import Mathlib.RingTheory.Ideal.IsPrimary
-public import Mathlib.RingTheory.Noetherian.Defs
-public import Mathlib.RingTheory.IsPrimary
-public import Mathlib.RingTheory.Ideal.MinimalPrime.Basic
+public import Mathlib.RingTheory.Ideal.AssociatedPrime.Basic
 
 /-!
 # Lasker ring
@@ -119,21 +115,19 @@ lemma IsLasker.exists_isMinimalPrimaryDecomposition [DecidableEq (Submodule R M)
     exists_minimal_isPrimary_decomposition_of_isPrimary_decomposition hs1 hs2
   exact ⟨t, h1, h2, h3, h4⟩
 
--- TODO: rephrase in terms of `associatedPrimes` once the definition is changed to match.
 /-- The first uniqueness theorem for primary decomposition, Theorem 4.5 in Atiyah-Macdonald:
 In any minimal primary decomposition `I = ⨅ i, q_i`, the ideals `radical (q_i.colon M)` are exactly
 the associated primes of `I`. -/
-lemma IsMinimalPrimaryDecomposition.mem_image_radical_colon_iff [DecidableEq (Submodule R M)]
-    {N : Submodule R M} {t : Finset (Submodule R M)} (ht : IsMinimalPrimaryDecomposition N t)
-    {p : Ideal R} :
-    p ∈ (fun J : Submodule R M ↦ radical (J.colon .univ)) '' t ↔
-      IsPrime p ∧ ∃ x : M, p = radical (N.colon {x}) := by
+lemma IsMinimalPrimaryDecomposition.image_radical_eq_associated_primes [DecidableEq (Submodule R M)]
+    {N : Submodule R M} {t : Finset (Submodule R M)} (ht : IsMinimalPrimaryDecomposition N t) :
+    (fun J : (Submodule R M) ↦ (J.colon Set.univ).radical) '' t = N.associatedPrimes := by
   classical
   replace h x : radical (N.colon {x}) = (t.filter (x ∉ ·)).inf fun q ↦ radical (q.colon .univ) := by
     simp_rw [← ht.inf_eq, colon_finsetInf, ← radicalInfTopHom_apply, map_finset_inf,
       Function.comp_def, radicalInfTopHom_apply, id_eq]
     rw [Finset.inf_congr rfl (fun q hq ↦ (ht.primary hq).radical_colon_singleton_eq_ite x),
       Finset.inf_ite, Finset.inf_top, top_inf_eq]
+  ext p
   constructor
   · rintro ⟨q, hqt, rfl⟩
     obtain ⟨x, hxt, hxq⟩ := SetLike.not_le_iff_exists.mp (ht.minimal hqt)
