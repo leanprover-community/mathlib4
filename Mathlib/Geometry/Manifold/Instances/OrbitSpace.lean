86d0a963f1752891505a06af1c36854eb27886a3
pepamontero <pepamonterojimena@gmail.com>
2026-02-22 18:57:23 +0100

    added authors

--- Mathlib/Geometry/Manifold/Instances/OrbitSpace.lean
+++ Mathlib/Geometry/Manifold/Instances/OrbitSpace.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2026 <names>. All rights reserved.
+Copyright (c) 2026 Michael Rothgang, Pepa Montero, Archibald Browne. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: <names>
+Authors: Michael Rothgang, Pepa Montero, Archibald Browne.
 -/
 module
 
