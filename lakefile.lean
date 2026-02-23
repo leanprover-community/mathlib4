4a1478e012d8cd2e472c0e030d6a27fb8bee1950
Riccardo Brasca <riccardo.brasca@gmail.com>
2026-02-23 22:55:31 +0100

    maybe this

--- lakefile.lean
+++ lakefile.lean
@@ -6,7 +6,7 @@ open Lake DSL
 ## Mathlib dependencies on upstream projects
 -/
 
-require batteries from git "https://github.com/riccardobrasca/batteries" @ "less_choice"
+require batteries from git "https://github.com/riccardobrasca/batteries" @ "v1.1.0"
 require "leanprover-community" / "Qq" @ git "master"
 require "leanprover-community" / "aesop" @ git "master"
 require "leanprover-community" / "proofwidgets" @ git "v0.0.88" -- ProofWidgets should always be pinned to a specific version
