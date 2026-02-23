141c9543ea7650cb1dada9c3218f60bdb3fa3e72
Parcly Taxel <reddeloostw@gmail.com>
2026-02-23 16:06:04 +0800

    oops again

--- Mathlib.lean
+++ Mathlib.lean
@@ -6450,7 +6450,6 @@ public import Mathlib.RingTheory.QuasiFinite.Basic
 public import Mathlib.RingTheory.QuasiFinite.Polynomial
 public import Mathlib.RingTheory.QuasiFinite.Weakly
 public import Mathlib.RingTheory.QuotSMulTop
-public import Mathlib.RingTheory.Radical
 public import Mathlib.RingTheory.Radical.Basic
 public import Mathlib.RingTheory.Radical.NatInt
 public import Mathlib.RingTheory.ReesAlgebra
