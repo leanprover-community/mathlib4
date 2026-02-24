f6c7dd9ace9e775f8b6257428906603adb449523
David Loeffler <d.loeffler.01@cantab.net>
2026-02-24 20:40:57 +0000

    feat(LinearAlgebra/QuadraticForm): radical of a quadratic form (#34493)
    Define the radical of a quadratic form, and show that its dimension is invariant under equivalences.

--- Mathlib.lean
+++ Mathlib.lean
@@ -4857,6 +4857,7 @@ public import Mathlib.LinearAlgebra.QuadraticForm.Prod
 public import Mathlib.LinearAlgebra.QuadraticForm.QuadraticModuleCat
 public import Mathlib.LinearAlgebra.QuadraticForm.QuadraticModuleCat.Monoidal
 public import Mathlib.LinearAlgebra.QuadraticForm.QuadraticModuleCat.Symmetric
+public import Mathlib.LinearAlgebra.QuadraticForm.Radical
 public import Mathlib.LinearAlgebra.QuadraticForm.Real
 public import Mathlib.LinearAlgebra.QuadraticForm.TensorProduct
 public import Mathlib.LinearAlgebra.QuadraticForm.TensorProduct.Isometries
