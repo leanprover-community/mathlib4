a0c3fa4ed91e1e49c2b9ceb124838e5b6ceaef0c
Stefan Kebekus <kebekus@users.noreply.github.com>
2026-02-24 08:02:19 +0100

    Update Poisson.lean

--- Mathlib/Analysis/Complex/Poisson.lean
+++ Mathlib/Analysis/Complex/Poisson.lean
@@ -42,7 +42,7 @@ lemma herglotzRiesz_def (c w z : ℂ) :
 /--
 The Poisson kernel of integration.
 -/
-noncomputable def poisson (c w z : ℂ) : ℝ :=
+noncomputable def Poisson (c w z : ℂ) : ℝ :=
   (‖z - c‖ ^ 2 - ‖w - c‖ ^ 2) / ‖(z - c) - (w - c)‖ ^ 2
 
 lemma poisson_def (c w z : ℂ) :
@@ -251,9 +251,11 @@ theorem DiffContOnCl.circleAverage_re_herglotzRiesz_smul [CompleteSpace E] {c : 
     Real.circleAverage ((re ∘ HerglotzRiesz c w) • f) c R = f w := by
   rcases le_or_gt R 0 with hR | hR
   · simp_all [(ball_eq_empty).2 hR]
-    hf.comp (DifferentiableOn.diffContOnCl <| by fun_prop) (by intro; aesop) 
+  have h₁g : DiffContOnCl ℂ (fun z ↦ f (z + c)) (ball 0 R) :=
+    ⟨hf.1.comp (by fun_prop) (fun _ _ ↦ by aesop),
+      hf.2.comp (by fun_prop) (fun _ _ ↦ by simp_all [closure_ball _ (ne_of_lt hR).symm])⟩
   have h₂g : w - c ∈ ball 0 R := by simpa using hw
-  simpa [← circleAverage_map_add_const (c := c), HerglotzRiesz_def]
+  simpa [← circleAverage_map_add_const, herglotzRiesz_def]
     using circleAverage_re_smul_on_ball_zero h₁g h₂g
 
 /--
