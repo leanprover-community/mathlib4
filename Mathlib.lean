e4dfc54e5f56c33b1e2adf263760b84e259df288
yonggyuchoimath <63726570+yonggyuchoimath@users.noreply.github.com>
2026-02-24 22:10:57 +0900

    lake exe mk_all

--- Mathlib.lean
+++ Mathlib.lean
@@ -1277,7 +1277,6 @@ public import Mathlib.AlgebraicGeometry.Cover.Over
 public import Mathlib.AlgebraicGeometry.Cover.QuasiCompact
 public import Mathlib.AlgebraicGeometry.Cover.Sigma
 public import Mathlib.AlgebraicGeometry.EffectiveEpi
-public import Mathlib.AlgebraicGeometry.EllipticCurve.Affine
 public import Mathlib.AlgebraicGeometry.EllipticCurve.Affine.Basic
 public import Mathlib.AlgebraicGeometry.EllipticCurve.Affine.Formula
 public import Mathlib.AlgebraicGeometry.EllipticCurve.Affine.Point
