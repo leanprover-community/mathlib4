7a200d0f738f476309c4a02f36e27a81b24070c6
Aaron Liu <aaronliu2008@outlook.com>
2026-02-23 21:56:59 -0500

    fix list

--- Mathlib/RingTheory/Kaehler/Basic.lean
+++ Mathlib/RingTheory/Kaehler/Basic.lean
@@ -469,6 +469,7 @@ the relations:
 1. `dx + dy = d(x + y)`
 2. `x dy + y dx = d(x * y)`
 3. `dr = 0` for `r ∈ R`
+
 where `db` is the unit in the copy of `S` with index `b`.
 
 This is the kernel of the surjection
