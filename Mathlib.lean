0f3c8744d6544f953c42fe96f56e2c0e84d81905
Paul Lezeau <paul.lezeau@gmail.com>
2026-02-24 19:33:48 +0100

    mk_all

--- Mathlib.lean
+++ Mathlib.lean
@@ -2302,6 +2302,7 @@ public import Mathlib.CategoryTheory.Abelian.RightDerived
 public import Mathlib.CategoryTheory.Abelian.SerreClass.Basic
 public import Mathlib.CategoryTheory.Abelian.SerreClass.Bousfield
 public import Mathlib.CategoryTheory.Abelian.SerreClass.MorphismProperty
+public import Mathlib.CategoryTheory.Abelian.ShortExact
 public import Mathlib.CategoryTheory.Abelian.Subobject
 public import Mathlib.CategoryTheory.Abelian.Transfer
 public import Mathlib.CategoryTheory.Abelian.Yoneda
