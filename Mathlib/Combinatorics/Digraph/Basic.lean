5db163d0076ec38ff4ad3237205e95aea3423e3a
Shreyas <shreyasss94@gmail.com>
2026-02-24 21:22:51 +0100

    Add name to author list

--- Mathlib/Combinatorics/Digraph/Basic.lean
+++ Mathlib/Combinatorics/Digraph/Basic.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2024 Kyle Miller, Jack Cheverton. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Kyle Miller, Jack Cheverton, Jeremy Tan
+Authors: Kyle Miller, Jack Cheverton, Jeremy Tan, Shreyas Srinivas
 -/
 module
 
