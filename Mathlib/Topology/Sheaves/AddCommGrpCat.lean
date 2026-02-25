feca8db1c307ca226cec3665df6ab3b3d4b6f374
Brian-Nugent <bnugent@uw.edu>
2026-02-25 15:43:30 -0700

    Add definitions and theorems for flasque sheaves and sheaves of abelian groups

---
+++ Mathlib/Topology/Sheaves/AddCommGrpCat.lean
@@ -0,0 +1,54 @@
+/-
+Copyright (c) 2026 Brian Nugent. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Brian Nugent
+-/
+
+module
+
+public import Mathlib.Topology.Sheaves.Abelian
+
+/-!
+# Sheaves of abelian groups.
+
+Results for sheaves of abelian groups on topological spaces, in preparation for sheaf cohomology.
+- `TopCat.Sheaf.AddCommGrpCat.Γ` : `Γ U` is the functor `(Sheaf AddCommGrpCat X) ⥤ AddCommGrpCat`
+  that sends `𝓕` to `𝓕(U)` and and sends a morphism `f: 𝓕 ⟶ 𝓖` to `f(U): 𝓕(U) ⟶ 𝓖(U)`
+-/
+
+@[expose] public section
+
+universe u
+
+open TopologicalSpace Opposite CategoryTheory
+open scoped AlgebraicGeometry
+
+namespace TopCat
+
+variable {X : TopCat.{u}} {U V : Opens X}
+
+theorem Presheaf.addCommGrpCat_shortExact_app_zero {S : ShortComplex (Presheaf AddCommGrpCat.{u} X)}
+    {s : S.X₂.obj (op U)} (h : S.g.app (op U) s = 0) (hS : S.Exact) :
+    ∃(t : S.X₁.obj (op U)), S.f.app (op U) t = s := by
+  dsimp [Presheaf] at S
+  let F := (evaluation (Opens X)ᵒᵖ AddCommGrpCat.{u}).obj (op U)
+  apply (ShortComplex.ab_exact_iff (S.map F)).mp
+  · have := ((Functor.exact_tfae F).out 1 3).mpr
+    exact this ⟨inferInstance, inferInstance⟩ S hS
+  exact h
+
+namespace Sheaf.AddCommGrpCat
+
+lemma restrict_sum {F : Sheaf AddCommGrpCat X} (h : V ≤ U) (s t : F.val.obj (op U)) :
+    (s + t) |_ V = s |_V + t |_V := by
+  delta Presheaf.restrictOpen Presheaf.restrict
+  aesop_cat
+
+lemma shortExact_app_zero {S : ShortComplex (Sheaf AddCommGrpCat X)} (s : S.X₂.val.obj (op U))
+    (h : S.g.val.app (op U) s = 0) (hS : S.ShortExact) :
+    ∃(t : S.X₁.val.obj (op U)), S.f.val.app (op U) t = s := by
+  have := ((Functor.preservesFiniteLimits_tfae (forget AddCommGrpCat X)).out 1 3).mpr
+    (inferInstanceAs (Limits.PreservesFiniteLimits (forget AddCommGrpCat X)))
+  exact Presheaf.addCommGrpCat_shortExact_app_zero h (this S ⟨hS.1, hS.2⟩).left
+
+end TopCat.Sheaf.AddCommGrpCat
