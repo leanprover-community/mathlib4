7edf4d9dab06e2c79e010975d97d2e9d3c36a4a2
Xavier Roblot <46200072+xroblot@users.noreply.github.com>
2026-02-25 08:57:39 +0100

    1st commit

--- Mathlib/NumberTheory/NumberField/Cyclotomic/Galois.lean
+++ Mathlib/NumberTheory/NumberField/Cyclotomic/Galois.lean
@@ -7,6 +7,9 @@ module
 
 public import Mathlib.Data.ZMod.Units
 public import Mathlib.NumberTheory.Cyclotomic.Gal
+public import Mathlib.RingTheory.Invariant.Basic
+public import Mathlib.RingTheory.Ideal.Int
+public import Mathlib.FieldTheory.Finite.Extension
 
 /-!
 # Galois theory for cyclotomic fields
@@ -76,5 +79,60 @@ theorem galEquivZMod_restrictNormal_apply (h : m ∣ n) (σ : Gal(K/ℚ)) :
   rw [← map_pow, (hζ.pow_eq_one_iff_dvd _).mpr h, map_one]
 
 end restrict
+
+section stabilizer
+
+open Pointwise MulAction Ideal
+
+variable (p : ℕ) [hp : Fact (Nat.Prime p)] (P : Ideal (𝓞 K)) [hP₁ : P.IsMaximal]
+    [hP₂ : P.LiesOver (Ideal.span {(p : ℤ)})] (hn : p.Coprime n)
+
+attribute [local instance] Ideal.Quotient.field
+
+theorem galEquivZMod_mem_zpowers_of_mem_stabilizer {σ : Gal(K/ℚ)} (hσ : σ ∈ stabilizer Gal(K/ℚ) P) :
+    galEquivZMod n K σ ∈ Subgroup.zpowers (ZMod.unitOfCoprime p hn) := by
+  let 𝕜 := (𝓞 K) ⧸ P
+  have hζ := IsCyclotomicExtension.zeta_spec n ℚ K
+  let τ := IsFractionRing.stabilizerHom Gal(K/ℚ) (Ideal.span {(p : ℤ)}) P
+     (ℤ ⧸ span {(p : ℤ)}) 𝕜 ⟨σ, hσ⟩
+  have : CharP (ℤ ⧸ span {(p : ℤ)}) p := sorry
+  have : Finite 𝕜 := sorry -- use HasFiniteQuotients !
+  obtain ⟨i, hi⟩ := FiniteField.exists_forall_apply_eq_pow (ℤ ⧸ span {(p : ℤ)}) p 𝕜 τ
+  sorry
+
+#exit
+
+  have hζ := IsCyclotomicExtension.zeta_spec m ℚ K
+  let 𝕜 := (𝓞 K) ⧸ P
+  let τ := IsFractionRing.stabilizerHom Gal(K/ℚ) (Ideal.span {(p : ℤ)}) P
+     (ℤ ⧸ span {(p : ℤ)}) 𝕜 ⟨σ, hσ⟩
+  obtain ⟨i, hi⟩ := toto (ℤ ⧸ span {(p : ℤ)}) p 𝕜 τ
+  refine ⟨i, ?_⟩
+  dsimp only
+  have t₁ := hi (Ideal.Quotient.mk P hζ.toInteger)
+  simp only [τ] at t₁
+  have t₂ := IsFractionRing.stabilizerHom_apply_apply_mk Gal(K/ℚ) (Ideal.span {(p : ℤ)}) P
+    (ℤ ⧸ span {(p : ℤ)}) 𝕜 ⟨σ, hσ⟩ hζ.toInteger
+  simp only [Algebra.algebraMap_self, RingHomCompTriple.comp_apply, 𝕜] at t₂
+  have t₃ := hζ.toInteger_isPrimitiveRoot.idealQuotient_mk (I := P) ?_ ?_
+  · rw [t₂] at t₁
+    rw [galEquiv_smul_of_pow_eq m, map_pow, IsOfFinOrder.pow_inj_mod] at t₁
+    · rw [zpow_natCast]
+      rw [Units.ext_iff]
+      simp only [Units.val_pow_eq_pow_val, ZMod.coe_unitOfCoprime]
+      rw [← t₃.eq_orderOf] at t₁
+      rw [Int.card_ideal_quot] at t₁
+      rw [← ZMod.natCast_eq_natCast_iff'] at t₁
+      simp at t₁
+      exact t₁.symm
+    · apply IsPrimitiveRoot.isOfFinOrder t₃
+    · exact hζ.toInteger_isPrimitiveRoot.pow_eq_one
+  · rw [Ne.eq_def, absNorm_eq_one_iff]
+    exact IsPrime.ne_top'
+  · rw [Ideal.absNorm_eq_pow_inertiaDeg' _ hp.out]
+    apply Nat.Coprime.pow_left
+    exact hm
+
+end stabilizer
 
 end IsCyclotomicExtension.Rat
