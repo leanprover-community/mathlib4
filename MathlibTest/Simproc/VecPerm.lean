064b9407c29b7d24bbb7125f6db5c8cbb24112d6
Paul Lezeau <paul.lezeau@gmail.com>
2026-02-25 10:59:19 +0100

    guard_msgs in

--- MathlibTest/Simproc/VecPerm.lean
+++ MathlibTest/Simproc/VecPerm.lean
@@ -26,6 +26,8 @@ example : ![a, b, c, d] ∘ ![1, 3, 1, 3] = ![b, d, b, d] := by
 
 set_option linter.unusedSimpArgs false
 
+/-- warning: declaration uses `sorry` -/
+#guard_msgs in
 example (u v w x : Fin 4) : ![a, b, c, d] ∘ ![u, v, w, x] = ![b, d, b, d] := by
   simp [vecPerm]
   guard_target = ![a, b, c, d] ∘ ![u, v, w, x] = ![b, d, b, d]
