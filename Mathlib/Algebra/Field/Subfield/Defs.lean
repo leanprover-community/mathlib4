e92c20bcfd4c7357be77ce4e4a4f22bd69d1262a
Kim Morrison <kim@tqft.net>
2026-02-25 11:19:20 +0000

    chore: mark Submodule.toAddSubgroup implicit_reducible

--- Mathlib/Algebra/Field/Subfield/Defs.lean
+++ Mathlib/Algebra/Field/Subfield/Defs.lean
@@ -141,6 +141,7 @@ add_decl_doc Subfield.toSubring
 namespace Subfield
 
 /-- The underlying `AddSubgroup` of a subfield. -/
+@[implicit_reducible]
 def toAddSubgroup (s : Subfield K) : AddSubgroup K :=
   { s.toSubring.toAddSubgroup with }
 
