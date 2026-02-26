1f322034c43c221abcf27e4bb99da4114a51685f
Joël Riou <joel.riou@universite-paris-saclay.fr>
2026-02-26 11:31:26 +0100

    removed unnecessary code

--- Mathlib/CategoryTheory/ConcreteCategory/Elementwise.lean
+++ Mathlib/CategoryTheory/ConcreteCategory/Elementwise.lean
@@ -22,19 +22,3 @@ attribute [elementwise (attr := simp)] Cone.w limit.lift_π limit.w
   colimit.ι_desc colimit.w kernel.lift_ι cokernel.π_desc kernel.condition cokernel.condition
 
 attribute [elementwise] Cocone.w
-
-namespace CategoryTheory.Limits
-
-variable {C : Type*} [Category* C] {F : C ⥤ Type*}
-
-@[simp]
-lemma Cocone.w_apply' (c : Cocone F) {X Y : C} (f : X ⟶ Y) (x : F.obj X) :
-    c.ι.app Y (F.map f x) = c.ι.app X x :=
-  c.w_apply f x
-
-@[simp]
-lemma Cone.w_apply' (c : Cone F) {X Y : C} (f : X ⟶ Y) (x : c.pt) :
-    (F.map f) (c.π.app X x) = c.π.app Y x :=
-  c.w_apply f x
-
-end CategoryTheory.Limits
