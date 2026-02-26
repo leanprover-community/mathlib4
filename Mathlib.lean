3e84a1af308b96a1ca96a2014b4c902d41c23a67
Blake Farman <1136925+farmanb@users.noreply.github.com>
2026-02-26 21:07:23 +0000

    feat(CategoryTheory/Abelian/Preradical): introduce basic definition of preradicals (#35514)
    A **preradical** on an abelian category `C` is a monomorphism in the functor category `C ⥤ C`
    with codomain `𝟭 C`, i.e. an element of `MonoOver (𝟭 C)`.
    
    Co-authored-by: blake-farman <blake.farman@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -2300,6 +2300,7 @@ public import Mathlib.CategoryTheory.Abelian.LeftDerived
 public import Mathlib.CategoryTheory.Abelian.Monomorphisms
 public import Mathlib.CategoryTheory.Abelian.NonPreadditive
 public import Mathlib.CategoryTheory.Abelian.Opposite
+public import Mathlib.CategoryTheory.Abelian.Preradical.Basic
 public import Mathlib.CategoryTheory.Abelian.Projective.Basic
 public import Mathlib.CategoryTheory.Abelian.Projective.Dimension
 public import Mathlib.CategoryTheory.Abelian.Projective.Ext
