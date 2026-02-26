4e24c3db7fd6a772200a86dd61f3a2adc01bb48e
Yi.Yuan <kysyy1@126.com>
2026-02-26 18:05:19 +0800

    Update Mathlib/Probability/Distributions/Poisson.lean
    Co-authored-by: Etienne Marion <66847262+EtienneC30@users.noreply.github.com>

--- Mathlib/Probability/Distributions/Poisson.lean
+++ Mathlib/Probability/Distributions/Poisson.lean
@@ -41,7 +41,7 @@ lemma poissonPMFRealSum (r : ℝ≥0) : HasSum (fun n ↦ poissonPMFReal r n) 1 
   unfold poissonPMFReal
   apply (hasSum_mul_left_iff (exp_ne_zero r)).mp
   simp only [mul_one]
-  have : (fun i ↦ rexp r * (rexp (-r) * r ^ i / Nat.factorial i)) =
+  have : (fun i ↦ rexp r * (rexp (-r) * r ^ i / i.factorial)) =
       fun i ↦ r ^ i / i.factorial := by
     ext n
     rw [mul_div_assoc, exp_neg, ← mul_assoc, ← div_eq_mul_inv, div_self (exp_ne_zero r), one_mul]
