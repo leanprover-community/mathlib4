14f13972883fbbb3f7a862592ac8466fd3e9e9b8
Snir Broshi <26556598+SnirBroshi@users.noreply.github.com>
2026-02-27 19:42:03 +0200

    namespace everything

--- Mathlib/Data/Finset/Mex.lean
+++ Mathlib/Data/Finset/Mex.lean
@@ -25,27 +25,28 @@ public section
 
 open Set
 
-theorem sInf_compl_le_ncard {s : Set ℕ} (hfin : s.Finite) : sInf sᶜ ≤ s.ncard := by
+theorem Set.Finite.sInf_compl_le_ncard {s : Set ℕ} (hfin : s.Finite) : sInf sᶜ ≤ s.ncard := by
   rw [← ncard_Iio_nat <| sInf sᶜ]
   refine ncard_le_ncard ?_ hfin
   nth_rw 2 [← compl_compl s]
   apply Nat.notMem_of_lt_sInf
 
-theorem coe_sInf_compl_le_encard (s : Set ℕ) : sInf sᶜ ≤ s.encard := by
+theorem Set.coe_sInf_compl_le_encard (s : Set ℕ) : sInf sᶜ ≤ s.encard := by
   rcases s.finite_or_infinite with hfin | hinf
   · grw [← ncard_le_encard]
-    exact_mod_cast sInf_compl_le_ncard hfin
+    exact_mod_cast hfin.sInf_compl_le_ncard
   · rw [hinf.encard_eq]
     exact le_top
 
-theorem sInf_coe_compl_le_card (s : Finset ℕ) : sInf sᶜ ≤ s.card := by
-  grw [sInf_compl_le_ncard s.finite_toSet, ncard_coe_finset s]
+theorem Finset.sInf_coe_compl_le_card (s : Finset ℕ) : sInf sᶜ ≤ s.card := by
+  grw [s.finite_toSet.sInf_compl_le_ncard, ncard_coe_finset s]
 
 variable {α : Type*} [ConditionallyCompleteLinearOrderBot α]
 variable [AddMonoidWithOne α] [AddLeftMono α] [ZeroLEOneClass α] [CharZero α]
 
 set_option backward.isDefEq.respectTransparency false in
-theorem csInf_coe_compl_le_coe_ncard' {s : Set α} (hfin : s.Finite) : sInf sᶜ ≤ (s.ncard : α) := by
+theorem Set.Finite.csInf_coe_compl_le_coe_ncard' {s : Set α} (hfin : s.Finite) :
+    sInf sᶜ ≤ (s.ncard : α) := by
   rw [← csSup_Iic (a := s.ncard)]
   grw [← Monotone.csSup_image_le_csSup Nat.mono_cast nonempty_Iic bddAbove_Iic]
   refine csInf_le_csSup_of_nonempty_inter' ?_ <| finite_Iic _ |>.image _ |>.bddAbove
@@ -55,5 +56,5 @@ theorem csInf_coe_compl_le_coe_ncard' {s : Set α} (hfin : s.Finite) : sInf sᶜ
   lia
 
 set_option backward.isDefEq.respectTransparency false in
-theorem csInf_coe_compl_le_coe_card' (s : Finset α) : sInf sᶜ ≤ (s.card : α) := by
-  grw [csInf_coe_compl_le_coe_ncard' s.finite_toSet, ncard_coe_finset s]
+theorem Finset.csInf_coe_compl_le_coe_card' (s : Finset α) : sInf sᶜ ≤ (s.card : α) := by
+  grw [s.finite_toSet.csInf_coe_compl_le_coe_ncard', ncard_coe_finset s]
