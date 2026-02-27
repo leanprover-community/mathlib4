1c18698f66d80121cea252eed1c577f1e6108c1f
jano-wol <78079945+jano-wol@users.noreply.github.com>
2026-02-27 20:00:41 +0100

    Update Mathlib/Algebra/Lie/Weights/Cartan.lean
    Co-authored-by: Oliver Nash <7734364+ocfnash@users.noreply.github.com>

--- Mathlib/Algebra/Lie/Weights/Cartan.lean
+++ Mathlib/Algebra/Lie/Weights/Cartan.lean
@@ -300,8 +300,8 @@ lemma lieIdeal_eq_iSup_inf_genWeightSpace (I : LieIdeal K L) :
   eq_iSup_inf_genWeightSpace (N := I.restr H)
 
 lemma lieIdeal_eq_inf_cartan_sup_biSup_inf_rootSpace (I : LieIdeal K L) :
-    I.restr H = (I.restr H ⊓ H.toLieSubmodule) ⊔ ⨆ α : Weight K H L, ⨆ (_ : α.IsNonZero),
-    I.restr H ⊓ rootSpace H α := by
+    I.restr H = (I.restr H ⊓ H.toLieSubmodule) ⊔
+      ⨆ α : Weight K H L, ⨆ (_ : α.IsNonZero), I.restr H ⊓ rootSpace H α := by
   refine le_antisymm ?_ (sup_le inf_le_left (iSup₂_le fun _ _ ↦ inf_le_left))
   conv_lhs => rw [lieIdeal_eq_iSup_inf_genWeightSpace]
   exact iSup_le fun α ↦ by
