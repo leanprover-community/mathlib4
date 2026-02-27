95b42d026933cfdae147739efe32edf83bc697ec
Xavier Genereux <xaviergenereux@hotmail.com>
2026-02-27 21:30:33 +0100

    feat: IsAlgebraic.adjoin_singleton

---
+++ Mathlib/RingTheory/Adjoin/Polynomial/Bivariate.lean
@@ -0,0 +1,92 @@
+/-
+Copyright (c) 2026 Xavier Généreux, María Inés de Frutos Fernández. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Xavier Généreux, María Inés de Frutos Fernández
+-/
+module
+
+public import Mathlib.RingTheory.Adjoin.Polynomial.Transcendental
+public import Mathlib.Algebra.Polynomial.Bivariate
+public import Mathlib.Algebra.Ring.Defs
+
+/-!
+# Bivariate polynomials and adjoining transcendental elements
+
+## Main results
+
+* `IsAlgebraic.adjoin_singleton`:
+  Given two transcendental elements `a`, `b` over `R`, if one of them, say `a` is algebraic over
+  `R[b]` then `b` is algebraic over `R[a]`.
+-/
+
+@[expose] public section
+
+noncomputable section
+
+namespace Polynomial
+
+open Polynomial.Bivariate Algebra Transcendental
+
+variable {R A : Type*} [CommRing R]
+
+section Ring
+
+variable [Ring A] [Algebra R A]
+
+def Bivariate.equivAdjoinOfTranscendental (x : A) (hx : Transcendental R x) :
+    R[X][Y] ≃ₐ[R] (Algebra.adjoin R {x})[X] :=
+  mapAlgEquiv (equivPolynomialAdjoin x hx)
+
+theorem Bivariate.equivAdjoinOfTranscendental_apply (x : A) (hx : Transcendental R x)
+    (p : R[X][Y]) :
+  equivAdjoinOfTranscendental x hx p = mapAlgHom
+    ((Algebra.equivRangeAevalAdjoinSingleton R x).toAlgHom.comp (aeval x).rangeRestrict) p := rfl
+
+theorem Bivariate.equivAdjoinOfTranscendental_apply' (x : A) (hx : Transcendental R x)
+    (p : R[X][Y]) :
+  equivAdjoinOfTranscendental x hx p = mapAlgHom
+    ((Algebra.equivRangeAevalAdjoinSingleton R x).toAlgHom.comp (aeval x).rangeRestrict) p := rfl
+
+attribute [local instance] algebra in
+theorem Bivariate.equivAdjoinOfTranscendental_swap_eq_aeval (x : A) (p : R[X][Y])
+    (hx : Transcendental R x) :
+    equivAdjoinOfTranscendental x hx (swap p) =
+      aeval (C ⟨x, Algebra.self_mem_adjoin_singleton R x⟩) p := by
+  induction p using Polynomial.induction_on' with
+  | add => simp_all only [map_add]
+  | monomial n a =>
+    induction a using Polynomial.induction_on' with
+    | add p q => simp_all only [map_add]
+    | monomial => simp_all [C_mul_X_pow_eq_monomial, Bivariate.equivAdjoinOfTranscendental]
+
+end Ring
+
+section CommRing
+
+variable [CommRing A] [Algebra R A]
+
+variable {B : Type*} [CommRing B] [Algebra A B] [Algebra R B] [IsScalarTower R A B]
+
+attribute [local instance] Polynomial.algebra in
+theorem Bivariate.aeval_aeval_eq_aeval_equivAdjoinOfTranscendental (x : A) (y : B)
+    (hx : Transcendental R x) (p : R[X][Y]) :
+    aeval (algebraMap A B x) (aeval (C (⟨y, Algebra.self_mem_adjoin_singleton R y⟩ :
+      Algebra.adjoin R {y})) p) = aeval y (equivAdjoinOfTranscendental x hx p) := by
+  induction p using Polynomial.induction_on' with
+  | add p q hp hq => simp_all [map_add]
+  | monomial n a => simp_all [aeval_algebraMap_apply, Bivariate.equivAdjoinOfTranscendental]
+
+theorem _root_.IsAlgebraic.adjoin_singleton {x : A} {y : B} (hx : Transcendental R x)
+    (hy : Transcendental R y) (h : IsAlgebraic (Algebra.adjoin R {x}) y) :
+    IsAlgebraic (Algebra.adjoin R {y}) (algebraMap A B x) := by
+  obtain ⟨f, hnezero, halg⟩ := h
+  refine ⟨(equivAdjoinOfTranscendental y hy) (swap ((equivAdjoinOfTranscendental x hx).symm f)),
+    by simpa only [map_ne_zero_iff _ (AlgEquiv.injective _)], ?_⟩
+  rw [equivAdjoinOfTranscendental_swap_eq_aeval y _ hy]
+  simpa [aeval_aeval_eq_aeval_equivAdjoinOfTranscendental x y hx]
+
+end CommRing
+
+end Polynomial
+
+end
