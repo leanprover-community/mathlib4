ae225a414d068231403811d39af135fbdc90b20b
Joël Riou <37772949+joelriou@users.noreply.github.com>
2026-02-27 15:40:08 +0000

    feat(CategoryTheory/Sites): skyscraper sheaves (#35719)
    Let `Φ` be a point of a site `(C, J)`. In this file, we construct the skyscraper sheaf functor `skyscraperSheafFunctor : A ⥤ Sheaf J A` and show that it is a right adjoint to `Φ.sheafFiber : Sheaf J A ⥤ A`.

--- Mathlib.lean
+++ Mathlib.lean
@@ -3169,6 +3169,7 @@ public import Mathlib.CategoryTheory.Sites.Point.Basic
 public import Mathlib.CategoryTheory.Sites.Point.Category
 public import Mathlib.CategoryTheory.Sites.Point.Conservative
 public import Mathlib.CategoryTheory.Sites.Point.Over
+public import Mathlib.CategoryTheory.Sites.Point.Skyscraper
 public import Mathlib.CategoryTheory.Sites.Precoverage
 public import Mathlib.CategoryTheory.Sites.PrecoverageToGrothendieck
 public import Mathlib.CategoryTheory.Sites.Preserves
