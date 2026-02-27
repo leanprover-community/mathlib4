d36f444ced6367f91041a8be65bf519c1e0979b7
Snir Broshi <26556598+SnirBroshi@users.noreply.github.com>
2026-02-27 22:06:36 +0200

    define syntax for `#find_syntax` to find

--- MathlibTest/BasicFiles/Tactic.lean
+++ MathlibTest/BasicFiles/Tactic.lean
@@ -4,9 +4,10 @@ import Mathlib.Tactic
 
 /-! Checks that some utilities are available already when importing `Mathlib.Tactic`. -/
 
-/-- info: Found 5 uses among over 2000 syntax declarations -/
+@[inherit_doc] notation "test_find_syntax" => id
+/-- info: Found -/
 #guard_msgs (substring := true) in
-#find_syntax "∘" approx
+#find_syntax "test_find_syntax" approx
 
 -- Guard against the shake tool modifying our imports
 /-- info: [public import Init, import Mathlib.Tactic] -/
