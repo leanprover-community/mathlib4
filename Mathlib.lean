4a19ab228edf6f89c50d4ae98298e3531b477d9f
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2026-02-28 17:10:37 +0800

    remove unnecessary file

--- Mathlib.lean
+++ Mathlib.lean
@@ -6458,7 +6458,6 @@ public import Mathlib.RingTheory.Regular.IsSMulRegular
 public import Mathlib.RingTheory.Regular.RegularSequence
 public import Mathlib.RingTheory.RegularLocalRing.Basic
 public import Mathlib.RingTheory.RegularLocalRing.Defs
-public import Mathlib.RingTheory.RegularLocalRing.PowerSeries
 public import Mathlib.RingTheory.RingHom.Bijective
 public import Mathlib.RingTheory.RingHom.EssFiniteType
 public import Mathlib.RingTheory.RingHom.Etale
