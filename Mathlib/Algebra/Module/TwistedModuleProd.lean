e84dbe2587d452750347ff4860a629deb67d24fc
Yoh Tanimoto <hoyt@jcom.home.ne.jp>
2026-02-28 21:23:14 +0100

    Update TwistedModuleProd.lean

--- Mathlib/Algebra/Module/TwistedModuleProd.lean
+++ Mathlib/Algebra/Module/TwistedModuleProd.lean
@@ -23,55 +23,52 @@ open Complex
 
 section
 
-variable {R S : Type*} [Semiring R] [Semiring S] (σ : R →+* S)
-
 set_option linter.unusedVariables false
-/-- A `twistedModuleProd σ E mr F ms` or `E ×[σ|mr|ms] F` is a module structure on the product
+/-- A `TwistedModuleProd σ E F` or `E ×[σ|mr|ms] F` is a module structure on the product
 `E × F` with the `SMul` given by `s • ⟨x, y⟩ := ⟨s • x, σ s • y⟩`. -/
 @[ext]
-structure twistedModuleProd {R S : Type*} [Ring R] [Ring S] (σ : R →+* S) (E : Type*)
-    [AddCommGroup E] (mr : Module R E) (F : Type*) [AddCommGroup F] (ms : Module S F) where
+structure TwistedModuleProd {R S : Type*} [Ring R] [Ring S] (σ : R →+* S) (E : Type*)
+    [AddCommGroup E] [Module R E] (F : Type*) [AddCommGroup F] [Module S F] where
   /-- The first element of a pair. -/
   fst : E
   /-- The second element of a pair. -/
   snd : F
 
-@[inherit_doc] notation:25 E " ×[" σ:25 "|" mr:25 "|" ms:25 "] " F:0
-  => twistedModuleProd σ E mr F ms
+@[inherit_doc] notation:25 E " ×[" σ:25 "] " F:0 => TwistedModuleProd σ E F
 
-namespace twistedModuleProd
+namespace TwistedModuleProd
 
 variable {R S : Type*} [Ring R] [Ring S] (σ : R →+* S) {E : Type*} [AddCommGroup E]
-  [mr : Module R E] {F : Type*} [AddCommGroup F] [ms : Module S F]
+  [Module R E] {F : Type*} [AddCommGroup F] [Module S F]
 
-instance : Add (twistedModuleProd σ E mr F ms) where
+instance : Add (TwistedModuleProd σ E F) where
   add x y := mk (x.fst + y.fst) (x.snd + y.snd)
 
 @[simp]
-lemma add_fst (x y : twistedModuleProd σ E mr F ms) : (x + y).fst = x.fst + y.fst := rfl
+lemma add_fst (x y : TwistedModuleProd σ E F) : (x + y).fst = x.fst + y.fst := rfl
 
 @[simp]
-lemma add_snd (x y : twistedModuleProd σ E mr F ms) : (x + y).snd = x.snd + y.snd := rfl
+lemma add_snd (x y : TwistedModuleProd σ E F) : (x + y).snd = x.snd + y.snd := rfl
 
-instance : Neg (twistedModuleProd σ E mr F ms) where
+instance : Neg (TwistedModuleProd σ E F) where
   neg x := mk (-x.fst) (-x.snd)
 
 @[simp]
-lemma neg_fst (x : twistedModuleProd σ E mr F ms) : (-x).fst = -x.fst := rfl
+lemma neg_fst (x : TwistedModuleProd σ E F) : (-x).fst = -x.fst := rfl
 
 @[simp]
-lemma neg_snd (x : twistedModuleProd σ E mr F ms) : (-x).snd = -x.snd := rfl
+lemma neg_snd (x : TwistedModuleProd σ E F) : (-x).snd = -x.snd := rfl
 
-instance : Zero (twistedModuleProd σ E mr F ms) where
+instance : Zero (TwistedModuleProd σ E F) where
   zero := mk 0 0
 
 @[simp]
-lemma zero_fst : (0 : twistedModuleProd σ E mr F ms).fst = 0 := rfl
+lemma zero_fst : (0 : TwistedModuleProd σ E F).fst = 0 := rfl
 
 @[simp]
-lemma zero_snd : (0 : twistedModuleProd σ E mr F ms).snd = 0 := rfl
+lemma zero_snd : (0 : TwistedModuleProd σ E F).snd = 0 := rfl
 
-instance : AddCommGroup (twistedModuleProd σ E mr F ms) where
+instance : AddCommGroup (TwistedModuleProd σ E F) where
   add_assoc x y z := by ext <;> simpa using add_assoc _ _ _
   zero_add x := by ext <;> simp
   add_zero x := by ext <;> simp
@@ -85,16 +82,16 @@ instance : AddCommGroup (twistedModuleProd σ E mr F ms) where
   nsmul_succ n x := by ext <;> simp [add_smul]
   zsmul_neg' n x := by ext <;> simp [add_smul]
 
-instance : SMul R (twistedModuleProd σ E mr F ms) where
+instance : SMul R (TwistedModuleProd σ E F) where
   smul s x := mk (s • x.fst) (σ s • x.snd)
 
 @[simp]
-lemma smul_fst (s : R) (x : twistedModuleProd σ E mr F ms) : (s • x).fst = s • x.fst := rfl
+lemma smul_fst (s : R) (x : TwistedModuleProd σ E F) : (s • x).fst = s • x.fst := rfl
 
 @[simp]
-lemma smul_snd (s : R) (x : twistedModuleProd σ E mr F ms) : (s • x).snd = σ s • x.snd := rfl
+lemma smul_snd (s : R) (x : TwistedModuleProd σ E F) : (s • x).snd = σ s • x.snd := rfl
 
-instance : Module R (twistedModuleProd σ E mr F ms) where
+instance : Module R (TwistedModuleProd σ E F) where
   mul_smul s t x := by ext <;> simp [mul_smul]
   one_smul x := by ext <;> simp
   smul_zero s := by ext <;> simp
@@ -102,20 +99,22 @@ instance : Module R (twistedModuleProd σ E mr F ms) where
   add_smul s t x := by ext <;> simp [add_smul]
   zero_smul x := by ext <;> simp
 
-end twistedModuleProd
+end TwistedModuleProd
 
 end
 
 section
 
+open TwistedModuleProd
+
+-- the desired result. the second component is twisted.
 example :
-  Complex.I • (twistedModuleProd.mk 0 1 : ℂ ×[starRingEnd ℂ|Semiring.toModule|Semiring.toModule] ℂ)
-  = (twistedModuleProd.mk 0 (-Complex.I) : ℂ ×[starRingEnd ℂ|Semiring.toModule|Semiring.toModule] ℂ)
-  := by
+    Complex.I • (mk 0 1 : ℂ ×[starRingEnd ℂ] ℂ) = (mk 0 (-Complex.I) : ℂ ×[starRingEnd ℂ] ℂ) := by
   ext
   · simp
   · simp
 
+-- cf. the usual `Prod` structure.
 example :
   Complex.I • (⟨0, 1⟩ : ℂ × ℂ)
   = (⟨0, Complex.I⟩ : ℂ × ℂ)
@@ -125,6 +124,9 @@ example :
   · simp
 
 end
+
+-- below is a failed attempt to use a type synonym for `R`.
+-- it does not work because of multiple `SMul` instances.
 
 section
 
