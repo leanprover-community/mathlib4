1093819168908db2ac77af6218fd56119baf783c
Joël Riou <37772949+joelriou@users.noreply.github.com>
2026-02-28 11:04:50 +0000

    feat(CategoryTheory/Triangulated): abelian subcategories (#35387)
    This PR adds a criteria to show that an additive subcategory of a triangulated category is abelian. (This shall be used to show that the heart of a t-structure is an abelian category.)

--- Mathlib.lean
+++ Mathlib.lean
@@ -3254,6 +3254,7 @@ public import Mathlib.CategoryTheory.Triangulated.Pretriangulated
 public import Mathlib.CategoryTheory.Triangulated.Rotate
 public import Mathlib.CategoryTheory.Triangulated.SpectralObject
 public import Mathlib.CategoryTheory.Triangulated.Subcategory
+public import Mathlib.CategoryTheory.Triangulated.TStructure.AbelianSubcategory
 public import Mathlib.CategoryTheory.Triangulated.TStructure.Basic
 public import Mathlib.CategoryTheory.Triangulated.TStructure.Heart
 public import Mathlib.CategoryTheory.Triangulated.TStructure.TruncLTGE
