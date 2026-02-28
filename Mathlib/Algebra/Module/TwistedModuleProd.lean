7c903723f5e7feaab003d1acc55b1e1c8777fbb4
Yoh Tanimoto <hoyt@jcom.home.ne.jp>
2026-02-28 16:22:39 +0100

    Update TwistedModuleProd.lean

--- Mathlib/Algebra/Module/TwistedModuleProd.lean
+++ Mathlib/Algebra/Module/TwistedModuleProd.lean
@@ -17,8 +17,6 @@ TODO cf Prod.lean for module
 
 @[expose] public section
 
-variable {R S : Type*} [Semiring R] [Semiring S] (σ : R →+* S)
-
 set_option linter.unusedVariables false
 /-- A `WithTwist σ` with `σ : R →+* S` is a type synonym of `R`, where when there is `Module S F`,
 we define `Mpdule (WithTwist σ) F` by `r • x = (σ r) • x`. -/
@@ -26,32 +24,19 @@ def WithTwist {R S : Type*} [Semiring R] [Semiring S] (σ : R →+* S) := R
 
 namespace WithTwist
 
+variable {R S : Type*} [Semiring R] [Semiring S] (σ : R →+* S)
+
 instance : Semiring (WithTwist σ) := inferInstanceAs (Semiring R)
 
-variable (E : Type*) [AddCommMonoid E] [Module R E]
-  (F : Type*) [AddCommMonoid F] [Module S F]
+section
+
+variable {E : Type*} [SMul R E]
 
 instance : SMul (WithTwist σ) E where
   smul r x := (id r : R) • x
 
 instance : SMul R (WithTwist σ) where
   smul r x := r • (id x : R)
-
-instance : SMul (WithTwist σ) F where
-  smul r x := σ r • x
-
-@[simp]
-lemma smul_eq (s : WithTwist σ) (x : F) : s • x = σ s • x := rfl
-
-instance : Module (WithTwist σ) E := inferInstanceAs (Module R E)
-
-instance : Module (WithTwist σ) F where
-  mul_smul _ _ _ := by simp [mul_smul]
-  one_smul _ := by simp
-  smul_zero _ := by simp
-  smul_add _ _ _ := by simp [smul_add]
-  add_smul _ _ _ := by simp [add_smul]
-  zero_smul _ := by simp
 
 instance : Module R (WithTwist σ) where
   mul_smul _ _ _ := by simp [mul_assoc]
@@ -67,4 +52,38 @@ def toLinearMap : (WithTwist σ) →ₛₗ[σ] S where
   map_add' _ _ := by simp
   map_smul' _ _ := by simp
 
+end
+
+section
+
+variable {F : Type*} [SMul S F]
+
+instance : SMul (WithTwist σ) F where
+  smul r x := σ r • x
+
+@[simp]
+lemma smul_eq (s : WithTwist σ) (x : F) : s • x = σ s • x := rfl
+
+end
+
+section
+
+variable {E : Type*} [AddCommMonoid E] [Module R E]
+
+instance : Module (WithTwist σ) E := inferInstanceAs (Module R E)
+
+variable {F : Type*} [AddCommMonoid F] [Module S F]
+
+instance : Module (WithTwist σ) F where
+  mul_smul _ _ _ := by simp [mul_smul]
+  one_smul _ := by simp
+  smul_zero _ := by simp
+  smul_add _ _ _ := by simp [smul_add]
+  add_smul _ _ _ := by simp [add_smul]
+  zero_smul _ := by simp
+
+end
+
 end WithTwist
+
+end
