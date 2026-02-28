c3d8eae9110185eb6ce995b0758932416ee5674c
Snir Broshi <26556598+SnirBroshi@users.noreply.github.com>
2026-02-28 06:36:43 +0200

    some statements are true for `Hom`

--- Mathlib/Combinatorics/SimpleGraph/Maps.lean
+++ Mathlib/Combinatorics/SimpleGraph/Maps.lean
@@ -289,8 +289,22 @@ theorem map_adj {v w : V} (h : G.Adj v w) : G'.Adj (f v) (f w) :=
 theorem map_mem_edgeSet {e : Sym2 V} (h : e ∈ G.edgeSet) : e.map f ∈ G'.edgeSet :=
   Sym2.ind (fun _ _ => f.map_rel') e h
 
+theorem edgeSet_subset_preimage : G.edgeSet ⊆ Sym2.map f ⁻¹' G'.edgeSet :=
+  fun _ ↦ f.map_mem_edgeSet
+
+theorem image_edgeSet_subset : Sym2.map f '' G.edgeSet ⊆ G'.edgeSet :=
+  fun _e' ⟨_e, he, he'⟩ ↦ he' ▸ f.map_mem_edgeSet he
+
 theorem apply_mem_neighborSet {v w : V} (h : w ∈ G.neighborSet v) : f w ∈ G'.neighborSet (f v) :=
   map_adj f h
+
+variable (v) in
+theorem neighborSet_subset_preimage : G.neighborSet v ⊆ f ⁻¹' G'.neighborSet (f v) :=
+  fun _ ↦ f.apply_mem_neighborSet
+
+variable (v) in
+theorem image_neighborSet_subset : f '' G.neighborSet v ⊆ G'.neighborSet (f v) :=
+  fun _u' ⟨_u, hu, hu'⟩ ↦ hu' ▸ f.apply_mem_neighborSet hu
 
 /-- The map between edge sets induced by a homomorphism.
 The underlying map on edges is given by `Sym2.map`. -/
@@ -372,28 +386,24 @@ theorem map_mem_edgeSet_iff {e : Sym2 V} : e.map f ∈ G'.edgeSet ↔ e ∈ G.ed
 theorem preimage_edgeSet : Sym2.map f ⁻¹' G'.edgeSet = G.edgeSet :=
   Set.ext fun _ ↦ map_mem_edgeSet_iff f
 
-theorem image_edgeSet_subset : Sym2.map f '' G.edgeSet ⊆ G'.edgeSet := by
-  rintro v ⟨u, hu, rfl⟩
-  rwa [map_mem_edgeSet_iff]
-
 theorem image_edgeSet : Sym2.map f '' G.edgeSet = G'.edgeSet ∩ Set.range (Sym2.map f) := by
-  apply le_antisymm <| Set.subset_inter (image_edgeSet_subset f) (Set.image_subset_range ..)
-  rintro _ ⟨huv, ⟨u, v⟩, rfl⟩
+  apply le_antisymm
+  · exact Set.subset_inter (f.toHom.image_edgeSet_subset) (Set.image_subset_range ..)
+  · rintro _ ⟨huv, ⟨u, v⟩, rfl⟩
   exact ⟨s(u, v), map_adj_iff f |>.mp huv, rfl⟩
 
 theorem apply_mem_neighborSet_iff {v w : V} : f w ∈ G'.neighborSet (f v) ↔ w ∈ G.neighborSet v :=
   map_adj_iff f
 
+variable (v) in
 theorem preimage_neighborSet : f ⁻¹' G'.neighborSet (f v) = G.neighborSet v :=
   Set.ext fun _ ↦ apply_mem_neighborSet_iff f
 
-theorem image_neighborSet_subset : f '' G.neighborSet v ⊆ G'.neighborSet (f v) := by
-  rintro v ⟨u, hu, rfl⟩
-  rwa [apply_mem_neighborSet_iff]
-
+variable (v) in
 theorem image_neighborSet : f '' G.neighborSet v = G'.neighborSet (f v) ∩ Set.range f := by
-  apply le_antisymm <| Set.subset_inter (image_neighborSet_subset f) (Set.image_subset_range ..)
-  rintro _ ⟨huv, u, rfl⟩
+  apply le_antisymm
+  · exact Set.subset_inter (f.toHom.image_neighborSet_subset v) (Set.image_subset_range f _)
+  · rintro _ ⟨huv, u, rfl⟩
   exact ⟨u, f.map_adj_iff.mp huv, rfl⟩
 
 /-- A graph embedding induces an embedding of edge sets. -/
