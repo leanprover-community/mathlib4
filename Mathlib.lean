d708d9cd4ee49ca46e53594db3603830e20ca88a
Joël Riou <joel.riou@universite-paris-saclay.fr>
2026-03-01 19:20:07 +0000

    feat(Algebra/Homology): the mapping cocone

--- Mathlib.lean
+++ Mathlib.lean
@@ -616,6 +616,7 @@ public import Mathlib.Algebra.Homology.HomotopyCategory.HomComplexSingle
 public import Mathlib.Algebra.Homology.HomotopyCategory.HomologicalFunctor
 public import Mathlib.Algebra.Homology.HomotopyCategory.KInjective
 public import Mathlib.Algebra.Homology.HomotopyCategory.KProjective
+public import Mathlib.Algebra.Homology.HomotopyCategory.MappingCocone
 public import Mathlib.Algebra.Homology.HomotopyCategory.MappingCone
 public import Mathlib.Algebra.Homology.HomotopyCategory.Pretriangulated
 public import Mathlib.Algebra.Homology.HomotopyCategory.Shift
