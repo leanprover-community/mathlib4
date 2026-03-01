d6956d36a360e12d3a5fa58b16402616f9799891
Kim Morrison <kim@tqft.net>
2026-03-01 23:13:28 +0000

    fix

--- Mathlib/RingTheory/AdicCompletion/AsTensorProduct.lean
+++ Mathlib/RingTheory/AdicCompletion/AsTensorProduct.lean
@@ -61,7 +61,7 @@ def ofTensorProductBil : AdicCompletion I R →ₗ[AdicCompletion I R] M →ₗ[
     simp
   map_smul' r x := by
     apply LinearMap.ext
-    simp
+    simp [mul_smul]
 
 @[simp]
 private lemma ofTensorProductBil_apply_apply (r : AdicCompletion I R) (x : M) :
