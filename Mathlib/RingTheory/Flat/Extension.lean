6b36c9d898b6545f455a7c017772274d38bcb628
xyzw12345 <wangjt2020@163.com>
2026-03-01 15:10:41 +0800

    update trivial

--- Mathlib/RingTheory/Flat/Extension.lean
+++ Mathlib/RingTheory/Flat/Extension.lean
@@ -6,6 +6,7 @@ Authors: Nailin Guan
 module
 
 public import Mathlib.Algebra.Category.CommAlgCat.Basic
+public import Mathlib.Algebra.Algebra.Shrink
 public import Mathlib.CategoryTheory.SmallObject.Iteration.Nonempty
 public import Mathlib.RingTheory.Flat.Basic
 public import Mathlib.RingTheory.LocalRing.ResidueField.Basic
@@ -25,6 +26,27 @@ open IsLocalRing CategoryTheory SmallObject
 variable (R : Type u) [CommRing R] [IsLocalRing R] (K : Type v) [Field K]
   [Algebra (ResidueField R) K]
 
+section instances
+
+instance {S : Type*} [Semiring S] [Algebra R S] : IsLocalHom (AlgHom.id R S) := ⟨fun _ h ↦ h⟩
+
+instance {S₁ S₂ S₃ : Type*} [Semiring S₁] [Semiring S₂] [Semiring S₃] [Algebra R S₁] [Algebra R S₂]
+    [Algebra R S₃] (f : S₁ →ₐ[R] S₂) (g : S₂ →ₐ[R] S₃) [locf : IsLocalHom f] [locg : IsLocalHom g] :
+    IsLocalHom (g.comp f) :=
+  ⟨fun a ha ↦ locf.map_nonunit a (locg.map_nonunit (f a) ha)⟩
+
+omit [IsLocalRing R] in
+private lemma AlgHom.comp_toRingHom' {S₁ S₂ S₃ : Type*} [Semiring S₁] [Semiring S₂] [Semiring S₃]
+    [Algebra R S₁] [Algebra R S₂] [Algebra R S₃] (f : S₁ →ₐ[R] S₂) (g : S₂ →ₐ[R] S₃) :
+    (g.comp f) = (RingHomClass.toRingHom g).comp (RingHomClass.toRingHom f) := rfl
+
+instance [Small.{w} R] : IsLocalRing (Shrink R) :=
+  let := IsLocalHom.of_surjective (Shrink.ringEquiv R).symm.toRingHom
+    (Shrink.ringEquiv R).symm.surjective
+  IsLocalRing.of_surjective (Shrink.ringEquiv R).symm.toRingHom (Shrink.ringEquiv R).symm.surjective
+
+end instances
+
 structure FlatExtension where
   Ring : Type w
   [commRing : CommRing Ring]
@@ -35,29 +57,40 @@ structure FlatExtension where
   eqmap : maximalIdeal Ring = (maximalIdeal R).map (algebraMap R Ring)
   resembd : ResidueField Ring →ₐ[ResidueField R] K
 
-open FlatExtension
+namespace FlatExtension
 
 attribute [instance] commRing algebra isLocalRing isLocalHom
 
+noncomputable def trivial [Small.{w} R] : FlatExtension R K := by
+  let e : R ≃+* Shrink.{w} R := (Shrink.ringEquiv R).symm
+  letI : IsLocalHom (algebraMap R (Shrink.{w} R)) :=
+    IsLocalHom.of_surjective e.toRingHom e.surjective
+  refine
+    { Ring := Shrink.{w} R
+      flat := Module.Flat.of_linearEquiv (Shrink.linearEquiv R R)
+      eqmap := ?_
+      resembd := ?_}
+  · apply (IsLocalRing.eq_maximalIdeal _).symm
+    exact (Ideal.isMaximal_map_iff_of_bijective _ e.bijective).2 inferInstance
+  · let e : R ≃+* Shrink.{w} R := (Shrink.algEquiv R R).symm.toRingEquiv
+    let f : ResidueField (Shrink.{w} R) →+* K :=
+      (algebraMap (ResidueField R) K).comp (ResidueField.map (e.symm : Shrink.{w} R →+* R))
+    refine
+      { toRingHom := f
+        commutes' := ?_ }
+    intro x
+    obtain ⟨x, rfl⟩ := IsLocalRing.residue_surjective (R := R) x
+    simpa [f, e] using congrArg (algebraMap (ResidueField R) K)
+      (IsLocalRing.ResidueField.map_residue (f := (e.symm : Shrink.{w} R →+* R))
+        (r := algebraMap R (Shrink.{w} R) x))
+
 variable {R K} in
-structure FlatExtension.Hom (S₁ S₂ : FlatExtension.{w} R K) where
+structure Hom (S₁ S₂ : FlatExtension.{w} R K) where
   hom : S₁.Ring →ₐ[R] S₂.Ring
   [isLocalHom : IsLocalHom hom]
   comm : S₂.resembd.toRingHom.comp (ResidueField.map hom) = S₁.resembd.toRingHom
 
 attribute [instance] Hom.isLocalHom
-
-instance {S : Type*} [Ring S] [Algebra R S] : IsLocalHom (AlgHom.id R S) := ⟨fun _ h ↦ h⟩
-
-instance {S₁ S₂ S₃ : Type*} [Ring S₁] [Ring S₂] [Ring S₃] [Algebra R S₁] [Algebra R S₂]
-    [Algebra R S₃] (f : S₁ →ₐ[R] S₂) (g : S₂ →ₐ[R] S₃) [locf : IsLocalHom f] [locg : IsLocalHom g] :
-    IsLocalHom (g.comp f) :=
-  ⟨fun a ha ↦ locf.map_nonunit a (locg.map_nonunit (f a) ha)⟩
-
-omit [IsLocalRing R] in
-lemma AlgHom.comp_toRingHom' {S₁ S₂ S₃ : Type*} [Ring S₁] [Ring S₂] [Ring S₃] [Algebra R S₁]
-    [Algebra R S₂] [Algebra R S₃] (f : S₁ →ₐ[R] S₂) (g : S₂ →ₐ[R] S₃) :
-    (g.comp f) = (RingHomClass.toRingHom g).comp (RingHomClass.toRingHom f) := rfl
 
 instance : Category (FlatExtension R K) where
   Hom S₁ S₂ := FlatExtension.Hom S₁ S₂
@@ -66,10 +99,12 @@ instance : Category (FlatExtension R K) where
     rw [← f.comm, ← g.comm]
     simp [AlgHom.comp_toRingHom', ResidueField.map_comp, ← RingHom.comp_assoc]⟩
 
-def FlatExtension.SuccStruct : SuccStruct (FlatExtension.{w} R K) where
-  X₀ := sorry
+private noncomputable def SuccStruct [Small.{w} R] : SuccStruct (FlatExtension.{w} R K) where
+  X₀ := trivial R K
   succ := sorry
-  toSucc := sorry
+  toSucc X := sorry
+
+end FlatExtension
 
 lemma exists_isLocalHom_flat : ∃ (R' : Type (max u v)) (_ : CommRing R') (_ : IsLocalRing R')
     (_ : Algebra R R') (_ : IsLocalHom (algebraMap R R')), Module.Flat R R' ∧
