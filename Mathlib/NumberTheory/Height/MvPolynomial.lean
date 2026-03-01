b1c8274985c7ba1db5dd1f61fa80c3aae7d4b032
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2026-03-01 22:13:21 +0100

    add height bounds on \![x*y, x+y, 1]

--- Mathlib/NumberTheory/Height/MvPolynomial.lean
+++ Mathlib/NumberTheory/Height/MvPolynomial.lean
@@ -469,6 +469,163 @@ theorem logHeight_eval_ge' [Nonempty ι'] {M : ℕ} {q : ι × ι' → MvPolynom
       logHeight (fun j ↦ (p j).eval x) ≥ C + N * logHeight x :=
   ⟨_, logHeight_eval_ge hq⟩
 
+/-!
+### Bounds for the height of ![x*y, x+y, 1]
+
+We show that the multiplicative height of `![x*y, x+y, 1]` is bounded from above and from below
+by a positive constant times the product of the multiplicative heights of `x` and `y`
+(and the analogous statements for the logarithmic heights).
+
+The constants are unspecified here; with (likely considerably, but trivial) more work,
+we could make them explicit.
+-/
+
+section sym2
+
+open Function in
+lemma mulHeight₁_mul_mulHeight₁ (x y : K) :
+    mulHeight₁ x * mulHeight₁ y = mulHeight ![x * y, x, y, 1] := by
+  have hx : ![x, 1] ≠ 0 := ne_iff.mpr ⟨1, by simp⟩
+  have hy : ![y, 1] ≠ 0 := ne_iff.mpr ⟨1, by simp⟩
+  simp only [mulHeight₁_eq_mulHeight, ← mulHeight_fun_mul_eq hx hy]
+  convert mulHeight_comp_equiv finProdFinEquiv _ with a
+  fin_cases a <;> simp [finProdFinEquiv]
+
+open MvPolynomial
+
+lemma mulHeight_sym2_le :
+    ∃ C > 0, ∀  (x y : K), mulHeight ![x * y, x + y, 1] ≤ C * mulHeight₁ x * mulHeight₁ y := by
+  let p : Fin 3 → MvPolynomial (Fin 4) K := ![X 0, X 1 + X 2, X 3]
+  have hom i : (p i).IsHomogeneous 1 := by
+    fin_cases i <;> simp [p, isHomogeneous_X, IsHomogeneous.add]
+  obtain ⟨C, hC₀, hC⟩ := mulHeight_eval_le' (p := p) (N := 1) hom
+  refine ⟨C, hC₀, fun x y ↦ ?_⟩
+  rw [mul_assoc, mulHeight₁_mul_mulHeight₁, ← pow_one (mulHeight ![x * y, x, y, 1])]
+  convert hC ![x * y, x, y, 1] with a
+  fin_cases a <;> simp [p]
+
+lemma mulHeight_sym2_ge :
+    ∃ C > 0, ∀ (x y : K), mulHeight ![x * y, x + y, 1] ≥ C * mulHeight₁ x * mulHeight₁ y := by
+  let p : Fin 3 → MvPolynomial (Fin 4) K := ![X 0, X 1 + X 2, X 3]
+  let q : Fin 4 × Fin 3 → MvPolynomial (Fin 4) K :=
+    ![![X 0, 0, 0], ![0, X 1, -X 0], ![0, X 2, -X 0], ![0, 0, X 3]].uncurry
+  have hom a : (q a).IsHomogeneous 1 := by
+    fin_cases a <;> simp [q] <;>
+      grind only [!isHomogeneous_X, isHomogeneous_zero, IsHomogeneous.neg]
+  obtain ⟨C, hC₀, hC⟩ := mulHeight_eval_ge' (M := 1) (N := 1) hom
+  refine ⟨C, hC₀, fun x y ↦ ?_⟩
+  rw [mul_assoc, mulHeight₁_mul_mulHeight₁, ← pow_one (mulHeight ![x * y, x, y, 1])]
+  have H a : ∑ j, (eval ![x * y, x, y, 1]) (q (a, j)) * (eval ![x * y, x, y, 1]) (p j) =
+      ![x * y, x, y, 1] a ^ 2 := by
+    fin_cases a <;> simp [p, q, Fin.sum_univ_three] <;> ring
+  convert hC p (x := ![x * y, x, y, 1]) H with a
+  fin_cases a <;> simp [p]
+
+open Real
+
+variable (K)
+
+lemma logHeight_sym2_le :
+    ∃ C, ∀ (x y : K), logHeight ![x * y, x + y, 1] ≤ C + logHeight₁ x + logHeight₁ y := by
+  obtain ⟨c, hc₉, hc⟩ := mulHeight_sym2_le (K := K)
+  refine ⟨log c, fun x y ↦ ?_⟩
+  simp only [logHeight_eq_log_mulHeight, logHeight₁_eq_log_mulHeight₁]
+  pull (disch := positivity) log
+  exact log_le_log (by positivity) (hc x y)
+
+lemma logHeight_sym2_ge :
+    ∃ C, ∀  (x y : K), logHeight ![x * y, x + y, 1] ≥ C + logHeight₁ x + logHeight₁ y := by
+  obtain ⟨c, hc₉, hc⟩ := mulHeight_sym2_ge (K := K)
+  refine ⟨log c, fun x y ↦ ?_⟩
+  simp only [logHeight_eq_log_mulHeight, logHeight₁_eq_log_mulHeight₁]
+  pull (disch := positivity) log
+  exact log_le_log (by positivity) (hc x y)
+
+lemma abs_logHeight_sym2_sub_le :
+    ∃ C, ∀ (x y : K), |logHeight ![x * y, x + y, 1] - (logHeight₁ x + logHeight₁ y)| ≤ C := by
+  obtain ⟨C₁, hC₁⟩ := logHeight_sym2_le K
+  obtain ⟨C₂, hC₂⟩ := logHeight_sym2_ge K
+  exact ⟨max C₁ (-C₂), by grind⟩
+
+variable {K}
+
+@[simp]
+lemma logHeight_zero_right (x : K) : logHeight ![x, 0] = 0 := by
+  let e : Unit ⊕ Unit ≃ Fin 2 := {
+    toFun | .inl () => 0 | .inr () => 1
+    invFun | 0 => .inl () | 1 => .inr ()
+    left_inv := by grind
+    right_inv := by grind
+  }
+  have he : ![x, 0] ∘ e = Sum.elim (fun | () => x) (0 : Unit → K) := by
+    ext1 j
+    fin_cases j <;> simp [e]
+  rw [← logHeight_comp_equiv e, he, logHeight_sumElim_zero_eq, logHeight_eq_zero_of_subsingleton]
+
+@[simp]
+lemma logHeight_zero_zero_left (x : K) : logHeight ![0, 0, x] = 0 := by
+  let e : Unit ⊕ Fin 2 ≃ Fin 3 := {
+    toFun | .inr 0 => 0 | .inr 1 => 1 | .inl () => 2
+    invFun | 0 => .inr 0 | 1 => .inr 1 | 2 => .inl ()
+    left_inv := by grind
+    right_inv := by grind
+  }
+  have he : ![0, 0, x] ∘ e = Sum.elim (fun | () => x) (0 : Fin 2 → K) := by
+    ext1 j
+    fin_cases j <;> simp [e]
+  rw [← logHeight_comp_equiv e, he, logHeight_sumElim_zero_eq, logHeight_eq_zero_of_subsingleton]
+
+@[simp]
+lemma logHeight_x_y_zero (x y : K) : logHeight ![x, y, 0] = logHeight ![x, y] := by
+  let e : Fin 2 ⊕ Unit ≃ Fin 3 := {
+    toFun | .inl 0 => 0 | .inl 1 => 1 | .inr () => 2
+    invFun | 0 => .inl 0 | 1 => .inl 1 | 2 => .inr ()
+    left_inv := by grind
+    right_inv := by grind
+  }
+  have he : ![x, y, 0] ∘ e = Sum.elim ![x, y] (0 : Unit → K) := by
+    ext1 j
+    fin_cases j <;> simp [e]
+  rw [← logHeight_comp_equiv e, he, logHeight_sumElim_zero_eq]
+
+omit [AdmissibleAbsValues K] in
+private lemma ne_zero_of_tuple_ne_zero {a : K} (ha : ![a, 0] ≠ 0) : a ≠ 0 := by
+  obtain ⟨i, hi⟩ := Function.ne_iff.mp ha
+  fin_cases i
+  · simpa using hi
+  · simp at hi
+
+variable (K) in
+lemma abs_logHeight_sym2_sub_le' :
+    ∃ C, ∀ a b c d : K, ![a, b] ≠ 0 → ![c, d] ≠ 0 →
+      |logHeight ![a * c, a * d + b * c, b * d] - (logHeight ![a, b] + logHeight ![c, d])| ≤ C := by
+  obtain ⟨C, hC⟩ := abs_logHeight_sym2_sub_le K
+  refine ⟨C, fun a b c d hab hcd ↦ ?_⟩
+  have hC₀ : 0 ≤ C := by simpa using hC 0 0
+  rcases eq_or_ne b 0 with rfl | hb
+  · have ha : a ≠ 0 := ne_zero_of_tuple_ne_zero hab
+    simp only [Nat.succ_eq_add_one, Nat.reduceAdd, zero_mul, add_zero, logHeight_x_y_zero,
+      logHeight_zero_right, zero_add]
+    rw [← logHeight_smul_eq_logHeight (c := 1 / a) _ (by simp [ha])]
+    simpa [field, ha] using hC₀
+  rcases eq_or_ne d 0 with rfl | hd
+  · have hc : c ≠ 0 := ne_zero_of_tuple_ne_zero hcd
+    simp only [Nat.succ_eq_add_one, Nat.reduceAdd, mul_zero, zero_add, logHeight_x_y_zero,
+      logHeight_zero_right, add_zero, mul_comm _  c]
+    rw [← logHeight_smul_eq_logHeight (c := 1 / c) _ (by simp [hc])]
+    simpa [field, hc] using hC₀
+  simp only [← logHeight₁_div_eq_logHeight]
+  have H : logHeight ![a * c, a * d + b * c, b * d] =
+            logHeight ![a / b * (c / d), a / b + c / d, 1] := by
+    rw [← logHeight_smul_eq_logHeight (c := 1 / (b * d)) _ (by simp [hb, hd])]
+    congr
+    ext1 i
+    fin_cases i <;> simp <;> field
+  rw [H]
+  exact hC ..
+
+end sym2
+
 end Height
 
 end
