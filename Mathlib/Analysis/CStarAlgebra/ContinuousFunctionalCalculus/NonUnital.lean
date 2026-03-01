0f1fa70a6d12ed2d89376a5fc04e25244a1536d6
Monica Omar <2497250a@research.gla.ac.uk>
2026-03-01 13:06:15 +0000

    isIdempotentElem_iff_spec

--- Mathlib/Analysis/CStarAlgebra/ContinuousFunctionalCalculus/NonUnital.lean
+++ Mathlib/Analysis/CStarAlgebra/ContinuousFunctionalCalculus/NonUnital.lean
@@ -9,6 +9,7 @@ public import Mathlib.Algebra.Algebra.Spectrum.Quasispectrum
 public import Mathlib.Topology.ContinuousMap.Compact
 public import Mathlib.Analysis.CStarAlgebra.ContinuousFunctionalCalculus.Unital
 public import Mathlib.Topology.UniformSpace.CompactConvergence
+import Mathlib.FieldTheory.IsAlgClosed.Spectrum
 
 /-!
 # The continuous functional calculus for non-unital algebras
@@ -842,3 +843,18 @@ lemma cfcₙ_eq_cfc [ContinuousMapZero.UniqueHom R A] {f : R → R} {a : A}
   · simp [cfc_apply_of_not_predicate a ha, cfcₙ_apply_of_not_predicate (R := R) a ha]
 
 end UnitalToNonUnital
+
+theorem isIdempotentElem_iff_quasispectrum_subset (R : Type*) {A : Type*} {p : A → Prop} [Field R]
+    [StarRing R] [MetricSpace R] [IsTopologicalSemiring R] [ContinuousStar R] [NonUnitalRing A]
+    [StarRing A] [TopologicalSpace A] [Module R A] [IsScalarTower R A A] [SMulCommClass R A A]
+    [NonUnitalContinuousFunctionalCalculus R A p] (a : A) (ha : p a) :
+    IsIdempotentElem a ↔ quasispectrum R a ⊆ {0, 1} := by
+  refine ⟨IsIdempotentElem.quasispectrum_subset, fun h ↦ ?_⟩
+  rw [IsIdempotentElem, ← cfcₙ_id' R a, ← cfcₙ_mul _ _]
+  exact cfcₙ_congr fun x hx ↦ by grind
+
+theorem isIdempotentElem_iff_spectrum_subset (R : Type*) {A : Type*} {p : A → Prop} [Field R]
+    [StarRing R] [MetricSpace R] [IsTopologicalSemiring R] [ContinuousStar R] [Ring A]
+    [StarRing A] [TopologicalSpace A] [Algebra R A] [NonUnitalContinuousFunctionalCalculus R A p]
+    (a : A) (ha : p a) : IsIdempotentElem a ↔ spectrum R a ⊆ {0, 1} := by
+  grind [quasispectrum_eq_spectrum_union_zero, isIdempotentElem_iff_quasispectrum_subset R]
