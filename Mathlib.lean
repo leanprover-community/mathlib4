54644dda1d77ee218434b1e688510846237c05b5
Kenny Lau <kc_kennylau@yahoo.com.hk>
2026-03-02 11:18:31 +0000

    feat(Logic): graded functions (#30355)
    This PR defines a class of graded functions, called `GradedFunLike`. It is intended to be used in #30312 to characterise a class of graded ring homomorphisms, where it is invoked as `[GradedFunLike F 𝒜 ℬ] [RingHomClass F A B]`.
    
    Co-authored-by: Wrenna Robson <wrenna.robson@bristol.ac.uk>

--- Mathlib.lean
+++ Mathlib.lean
@@ -3739,6 +3739,7 @@ public import Mathlib.Data.FunLike.Basic
 public import Mathlib.Data.FunLike.Embedding
 public import Mathlib.Data.FunLike.Equiv
 public import Mathlib.Data.FunLike.Fintype
+public import Mathlib.Data.FunLike.Graded
 public import Mathlib.Data.Holor
 public import Mathlib.Data.Ineq
 public import Mathlib.Data.Int.AbsoluteValue
