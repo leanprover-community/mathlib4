1650afe98c7bf98248cf0497d7aee18b9d0765a2
mariainesdff <mariaines.dff@gmail.com>
2026-03-02 22:57:48 +0100

    another fix

--- Mathlib/NumberTheory/LocalField/Basic.lean
+++ Mathlib/NumberTheory/LocalField/Basic.lean
@@ -93,7 +93,8 @@ lemma isCompact_closedBall (γ : ValueGroupWithZero K) : IsCompact { x | valuati
   ext x
   simp only [Set.mem_setOf_eq, Homeomorph.coe_symm_toEquiv, Homeomorph.mulLeft₀_symm_apply, inv_div,
     Set.preimage_setOf_eq, map_mul, map_div₀, Valuation.restrict_le_iff]
-  rw [div_mul_eq_mul_div, div_le_iff₀ (by simp [hγ])]
+  rw [div_mul_eq_mul_div, div_le_iff₀ (by
+    simp [MonoidWithZeroHom.ValueGroup₀.restrict₀_apply, hγ])]
   simp only [IsValuativeTopology.v_eq_valuation, ← map_mul, Valuation.restrict_le_iff]
   simp [hr]
 
