78b007cb8f6fb871576c117882591acfeeddc845
Joël Riou <joel.riou@universite-paris-saclay.fr>
2026-03-02 20:30:31 +0000

    fixed page

--- Mathlib/Order/Fin/Clamp.lean
+++ Mathlib/Order/Fin/Clamp.lean
@@ -16,20 +16,12 @@ public import Mathlib.Order.MinMax
 
 namespace Fin
 
-<<<<<<< HEAD
-public lemma clamp_mono {m : ℕ} : Monotone (fun n ↦ clamp n m) := by
-=======
 public lemma clamp_monotone {m : ℕ} : Monotone (fun n ↦ clamp n m) := by
->>>>>>> origin/spectral-sequences-2-cycles-2
   intro a b h
   rw [le_iff_val_le_val]
   exact min_le_min_right m h
 
-<<<<<<< HEAD
-public lemma clamp_eq_last (n m : ℕ) (hnm : m ≤ n) :
-=======
 public lemma clamp_eq_last (n m : ℕ) (hmn : m ≤ n) :
->>>>>>> origin/spectral-sequences-2-cycles-2
     clamp n m = last _ := by
   ext
   simpa
