af9c9903943a8c233534091c872fa1c4ba546bd9
Monica Omar <2497250a@research.gla.ac.uk>
2026-03-02 17:28:52 +0000

    cleanup

--- Mathlib/Analysis/CStarAlgebra/Basic.lean
+++ Mathlib/Analysis/CStarAlgebra/Basic.lean
@@ -281,8 +281,7 @@ end SelfAdjoint
 theorem IsStarProjection.norm_le [NonUnitalNormedRing E] [StarRing E] [CStarRing E]
     (e : E) (he : IsStarProjection e) : ‖e‖ ≤ 1 := by
   suffices ‖e‖ * (‖e‖ - 1) = 0 by grind [sub_eq_zero]
-  rw [mul_sub, ← CStarRing.norm_star_mul_self, he.isSelfAdjoint.star_eq, he.isIdempotentElem.eq]
-  simp
+  simp [mul_sub, ← CStarRing.norm_star_mul_self, he.isSelfAdjoint.star_eq, he.isIdempotentElem.eq]
 
 section starₗᵢ
 
