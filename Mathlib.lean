f0d1c9bc8513100267cfc9902dd4a3d7d9c6dce4
Joël Riou <37772949+joelriou@users.noreply.github.com>
2026-03-02 12:12:42 +0000

    feat(Algebra/Homology): limits of degreewise eventually constant projective systems (#35935)

--- Mathlib.lean
+++ Mathlib.lean
@@ -603,6 +603,7 @@ public import Mathlib.Algebra.Homology.HomologicalComplex
 public import Mathlib.Algebra.Homology.HomologicalComplexAbelian
 public import Mathlib.Algebra.Homology.HomologicalComplexBiprod
 public import Mathlib.Algebra.Homology.HomologicalComplexLimits
+public import Mathlib.Algebra.Homology.HomologicalComplexLimitsEventuallyConstant
 public import Mathlib.Algebra.Homology.HomologySequence
 public import Mathlib.Algebra.Homology.HomologySequenceLemmas
 public import Mathlib.Algebra.Homology.Homotopy
