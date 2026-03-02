2c67377f4e8a4ded47ce697ff96b32f0ec4b3071
dhyan-aranha <dhyan.aranha@gmail.com>
2026-03-02 11:34:04 +0100

    Addressed all feedback as best I could

--- Counterexamples/AffineLineWithDoubledOrigin.lean
+++ Counterexamples/AffineLineWithDoubledOrigin.lean
@@ -1,9 +1,11 @@
 /-
 Copyright (c) 2024 Dhyan Aranha. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Dhyan Aranha
+Authors: Dhyan Aranha, Dion Lenjse
 -/
-import Mathlib
+import Mathlib.Algebra.AffineMonoid.Basic
+import Mathlib.AlgebraicGeometry.Morphisms.Proper
+import Mathlib.CategoryTheory.Limits.Types.Pushouts
 
 /-!
 # The Affine Line with Doubled Origin
@@ -16,8 +18,6 @@ open subset 𝔸¹ - 0. We show that over Spec(ℤ) it is not separated.
 
 * `affineLine`: The affine line 𝔸¹ over ℤ.
 * `affineLineWithDoubledOrigin`: The scheme constructed by gluing two copies of `affineLine`.
-* `isPullback_of_isPushout_of_isOpenImmersion` : A pushout of open immersion of schemes is
-   also a pullback.
 * `not_isSeparated`: A proof that the affine line with doubled origin is not a separated scheme.
 
 ## Implementation Notes
@@ -34,165 +34,7 @@ open Polynomial AlgebraicGeometry CategoryTheory CategoryTheory.Limits Scheme To
 open LocallyRingedSpace.IsOpenImmersion
 
 
-section TopCatLemmas
-
-/-!
-### Topological Lemmas
-
-These lemmas relate pushouts in `Scheme` to pushouts in `TopCat` via the forgetful functor.
--/
-
-/-- The forgetful functor to `TopCat` preserves pushouts. -/
-lemma pushouts_forget {Z X Y P : TopCat} (f : Z ⟶ X) (g : Z ⟶ Y) (inl : X ⟶ P) (inr : Y ⟶ P)
-    (h : IsPushout f g inl inr) :
-    IsPushout ((forget TopCat).map f) ((forget TopCat).map g)
-              ((forget TopCat).map inl) ((forget TopCat).map inr) :=
-  h.map (forget TopCat)
-
-/-- An open embedding in `TopCat` is a monomorphism. -/
-lemma openEmbedding_isMono {X Y : TopCat} (f : X ⟶ Y) (hf : IsOpenEmbedding f) : Mono f :=
-  (TopCat.mono_iff_injective f).mpr hf.injective
-
-/--
-In `TopCat`, the intersection of the ranges of the pushout inclusions equals the range of
-the composition with the base map.
--/
-lemma range_inter_top {Z X Y P : TopCat} (f : Z ⟶ X) (g : Z ⟶ Y) (inl : X ⟶ P) (inr : Y ⟶ P)
-    (hf : IsOpenEmbedding f) (hg : IsOpenEmbedding g) (h : IsPushout f g inl inr) :
-    Set.range inl ∩ Set.range inr = Set.range (f ≫ inl) := by
-  have forget_comm_sq := (pushouts_forget f g inl inr h).toCommSq.w
-  let P_cocone_in_set := PushoutCocone.mk ((forget TopCat).map inl) ((forget TopCat).map inr)
-     forget_comm_sq
-  have P_iscolim_in_set : IsColimit P_cocone_in_set :=
-    (pushouts_forget f g inl inr h).isColimit
-  have f_ismono := (openEmbedding_isMono f) hf
-  have g_ismono := (openEmbedding_isMono g) hg
-  have range_inter_forward : Set.range inl ∩ Set.range inr ⊆ Set.range (f ≫ inl) := by
-    intro p hp
-    have hinltype : ∃ x : (forget TopCat).obj X, (forget TopCat).map inl x = p := by
-      exact hp.1
-    have hinrtype : ∃ y : (forget TopCat).obj Y, (forget TopCat).map inr y = p := by
-      exact hp.2
-    rcases hinltype with ⟨xtype, _⟩
-    rcases hinrtype with ⟨ytype, _⟩
-    have h_inl_eq_inr_iff := Types.Pushout.inl_eq_inr_iff ((forget TopCat).map f)
-          ((forget TopCat).map g) xtype ytype
-    let types_pushout := Types.Pushout.isColimitCocone
-                ((forget TopCat).map f) ((forget TopCat).map g)
-    let iso := IsColimit.coconePointUniqueUpToIso types_pushout P_iscolim_in_set
-    have h_comm : Types.Pushout.inl ((forget TopCat).map f) ((forget TopCat).map g) xtype =
-              Types.Pushout.inr ((forget TopCat).map f) ((forget TopCat).map g) ytype := by
-      have hcomp1 : (iso.hom) ((Types.Pushout.inl ((forget TopCat).map f) ((forget TopCat).map g))
-        (xtype)) = ((forget TopCat).map inl) (xtype) := rfl
-      have hcomp2 : (iso.hom) ((Types.Pushout.inr ((forget TopCat).map f) ((forget TopCat).map g))
-         (ytype)) = ((forget TopCat).map inr) (ytype) := rfl
-      have almost : (iso.hom) ((Types.Pushout.inl ((forget TopCat).map f) ((forget TopCat).map g))
-        (xtype)) = (iso.hom) ((Types.Pushout.inr ((forget TopCat).map f) ((forget TopCat).map g))
-        (ytype)) := by
-        rw [hcomp1, hcomp2]
-        simp_all only [ConcreteCategory.forget_map_eq_coe]
-      exact (Iso.toEquiv iso).injective almost
-    cat_disch
-  have range_inter_reverse : Set.range (f ≫ inl) ⊆ Set.range inl ∩ Set.range inr := by
-    intro p hp
-    constructor
-    · simp_all only [TopCat.hom_comp, ContinuousMap.coe_comp, Set.mem_range, Function.comp_apply]
-      obtain ⟨z, _⟩ := hp
-      use (Hom.hom f) z
-    · simp_all only [TopCat.hom_comp, ContinuousMap.coe_comp, Set.mem_range, Function.comp_apply]
-      obtain ⟨z, _⟩ := hp
-      use (Hom.hom g) z
-      have h_apply : (f ≫ inl) z = (g ≫ inr) z := by rw [h.toCommSq.w]
-      simp_all only [TopCat.comp_app]
-  exact Eq.symm (Set.Subset.antisymm range_inter_reverse range_inter_forward)
-
-instance {X Y Z : Scheme} (f : Z ⟶ X) (g : Z ⟶ Y) [IsOpenImmersion f] [IsOpenImmersion g] :
-    PreservesColimit (span f g) (Scheme.forgetToTop) := by
-  change PreservesColimit _ (Scheme.forgetToLocallyRingedSpace ⋙
-    LocallyRingedSpace.forgetToSheafedSpace ⋙ _)
-  infer_instance
-
-/-- The forgetful functor from Schemes to TopCat preserves pushouts of open immersions. -/
-lemma pushout_in_TopCat {X Y Z T : Scheme} (f : Z ⟶ X) (g : Z ⟶ Y)
-    [IsOpenImmersion f] [IsOpenImmersion g] (inl : X ⟶ T) (inr : Y ⟶ T)
-    (h : IsPushout f g inl inr) :
-    IsPushout (forgetToTop.map f) (forgetToTop.map g) (forgetToTop.map inl)
-    (forgetToTop.map inr) :=
-  h.map Scheme.forgetToTop
-
-end TopCatLemmas
-
-
-/-- Pushout of open immersions are open immersions -/
-lemma inl_inr_isOpenImmersion {U X Y T : Scheme} (f : U ⟶ X) (g : U ⟶ Y)
-    (inl : X ⟶ T) (inr : Y ⟶ T) (h : IsPushout f g inl inr)
-    [IsOpenImmersion f] [IsOpenImmersion g] : IsOpenImmersion inl ∧ IsOpenImmersion inr := by
-  let iso := h.isoPushout
-  have inlpush : inl = pushout.inl _ _ ≫ iso.inv := h.inl_isoPushout_inv.symm
-  have inrpush : inr = pushout.inr _ _ ≫ iso.inv := h.inr_isoPushout_inv.symm
-  rw [inlpush, inrpush]
-  constructor
-  all_goals infer_instance
-
-
-/--
-The pushout square of open immersions is also a pullback square.
--/
-lemma isPullback_of_isPushout_of_isOpenImmersion {U X Y T : Scheme} (f : U ⟶ X) (g : U ⟶ Y)
-    (inl : X ⟶ T) (inr : Y ⟶ T) (h : IsPushout f g inl inr)
-    [IsOpenImmersion f] [IsOpenImmersion g] :
-    IsPullback f g inl inr := by
-  constructor
-  · have f_embedd_TopCat : IsOpenEmbedding (forgetToTop.map f) := f.isOpenEmbedding
-    have g_embedd_TopCat : IsOpenEmbedding (forgetToTop.map g) := g.isOpenEmbedding
-    let top_push : IsPushout (forgetToTop.map f) (forgetToTop.map g) (forgetToTop.map inl)
-        (forgetToTop.map inr) := h.map Scheme.forgetToTop
-    have inl_inj_base : Function.Injective inl.base := by
-      have := ((inl_inr_isOpenImmersion f g inl inr h).1.mono)
-      exact Hom.injective inl
-    have range_inter_top := range_inter_top (forgetToTop.map f) (forgetToTop.map g)
-      (forgetToTop.map inl) (forgetToTop.map inr) f_embedd_TopCat g_embedd_TopCat top_push
-    have range_inter : Set.range inl.base ∩ Set.range inr.base ⊆ Set.range (f ≫ inl).base :=
-      Eq.subset range_inter_top
-    constructor
-    fapply PullbackCone.IsLimit.mk
-    · intro s
-      have range_subset : Set.range s.fst.base ⊆ f.opensRange := by
-        rintro x ⟨w, rfl⟩
-        have mem_inter : (inl.base (s.fst.base w)) ∈ Set.range inl.base ∩ Set.range inr.base := by
-          refine ⟨Set.mem_range_self _, ?_⟩
-          have eq_base : (s.fst ≫ inl).base w = (s.snd ≫ inr).base w := by
-            rw [s.condition]
-          simp_all only [Hom.comp_base, TopCat.hom_comp, ContinuousMap.coe_comp,
-            ContinuousMap.comp_apply, Set.mem_range, exists_apply_eq_apply]
-        obtain ⟨u, hu⟩ := range_inter mem_inter
-        apply inl_inj_base at hu
-        rw [← hu]
-        exact Set.mem_range_self _
-      exact IsOpenImmersion.lift f s.fst range_subset
-    · intro s
-      simp only [IsOpenImmersion.lift_fac]
-    · intro s
-      simp only
-      have range_subset : Set.range s.fst.base ⊆ f.opensRange := by
-        rintro x ⟨w, rfl⟩
-        have mem_inter : (inl.base (s.fst.base w)) ∈ Set.range inl.base ∩ Set.range inr.base := by
-          refine ⟨Set.mem_range_self _, ?_⟩
-          have eq_base : (s.fst ≫ inl).base w = (s.snd ≫ inr).base w := by
-            rw [s.condition]
-          simp_all only [Hom.comp_base, TopCat.hom_comp, ContinuousMap.coe_comp,
-            ContinuousMap.comp_apply, Set.mem_range, exists_apply_eq_apply]
-        obtain ⟨u, hu⟩ := range_inter mem_inter
-        apply inl_inj_base at hu
-        rw [← hu]
-        exact Set.mem_range_self _
-      apply (inl_inr_isOpenImmersion f g inl inr h).2.mono.right_cancellation
-      simp only [Category.assoc]
-      rw [←h.toCommSq.w, ←Category.assoc, IsOpenImmersion.lift_fac f s.fst range_subset]
-      apply s.condition
-    · intro s m p q
-      exact IsOpenImmersion.lift_uniq f s.fst _ _ p
-  · exact h.toCommSq
+set_option backward.isDefEq.respectTransparency false
 
 
 section Diagonal
@@ -203,19 +45,17 @@ The diagonal morphism factored through the pullback of the product.
 lemma diag_eq_pullback_diagonal (Y : Scheme) :
     diag Y ≫ (prodIsoPullback Y Y).hom = pullback.diagonal (terminal.from Y) := by
   apply pullback.hom_ext
-  · simp only [Category.assoc, prodIsoPullback_hom_fst, limit.lift_π, BinaryFan.mk_pt,
-      BinaryFan.mk_fst, pullback.diagonal_fst]
+  · simp only [pullback.diagonal_fst]
+    simp_all only [Category.assoc, prodIsoPullback_hom_fst]
+    exact prod.lift_fst (𝟙 Y) (𝟙 Y)
   · simp only [Category.assoc, prodIsoPullback_hom_snd, limit.lift_π, BinaryFan.mk_pt,
       BinaryFan.mk_snd, pullback.diagonal_snd]
 
 /--
 The diagonal of a separated scheme is a closed immersion.
 -/
-lemma diag_isClosedImmersion (Y : Scheme) [IsSeparated (terminal.from Y)] :
-    IsClosedImmersion (diag Y) := by
-  have iso_closed : IsClosedImmersion (prodIsoPullback Y Y).hom := inferInstance
+instance (Y : Scheme) [Y.IsSeparated] : IsClosedImmersion (diag Y) := by
   have diag_pullback := diag_eq_pullback_diagonal Y
-  have rhs_closed : IsClosedImmersion (pullback.diagonal (terminal.from Y)) := inferInstance
   have comp_closed : IsClosedImmersion (diag Y ≫ (prodIsoPullback Y Y).hom) := by
     rw [diag_pullback]
     infer_instance
@@ -224,12 +64,12 @@ lemma diag_isClosedImmersion (Y : Scheme) [IsSeparated (terminal.from Y)] :
 /--
 The equalizer inclusion of two morphisms into a separated scheme is a closed immersion.
 -/
-instance isClosedImmersion_equalizer_iota {X Y : Scheme} [IsSeparated (terminal.from Y)]
+instance isClosedImmersion_equalizer_iota {X Y : Scheme} [Y.IsSeparated]
     (f g : X ⟶ Y) : IsClosedImmersion (equalizer.ι f g) := by
   refine MorphismProperty.of_isPullback (isPullback_equalizer_prod f g).flip ?_
   have h : diag Y = prod.lift (𝟙 Y) (𝟙 Y) := rfl
   rw [← h]
-  exact diag_isClosedImmersion Y
+  infer_instance
 
 end Diagonal
 
@@ -283,15 +123,18 @@ The **Affine Line with Doubled Origin**.
 Constructed as the pushout of two copies of $\mathbb{A}^1$ glued along
  $U = \mathbb{A}^1 \setminus \{0\}$.
 -/
-def affineLineWithDoubledOrigin : Scheme := pushout inclusion inclusion
+def affineLineWithDoubledOrigin : Scheme :=
+pushout inclusion inclusion
 
 namespace AffineLineWithDoubledOrigin
 
 /-- The inclusion of the first copy of $\mathbb{A}^1$. -/
-def f : affineLine ⟶ affineLineWithDoubledOrigin := pushout.inl inclusion inclusion
+def f : affineLine ⟶ affineLineWithDoubledOrigin :=
+pushout.inl inclusion inclusion
 
 /-- The inclusion of the second copy of $\mathbb{A}^1$. -/
-def g : affineLine ⟶ affineLineWithDoubledOrigin := pushout.inr inclusion inclusion
+def g : affineLine ⟶ affineLineWithDoubledOrigin :=
+pushout.inr inclusion inclusion
 
 
 /-- The defining pushout property. -/
@@ -349,7 +192,7 @@ lemma inclusion_eq_iso_comp_iota : inclusion = isoOpenToEqualizer.hom ≫ equali
     line_minusOrigin_isEqualizer (limit.isLimit _) WalkingParallelPair.zero
 
 /-- The inclusion $U \to \mathbb{A}^1$ is a closed immersion (if the scheme is separated). -/
-lemma inclusion_isClosedImmersion [IsSeparated (terminal.from affineLineWithDoubledOrigin)] :
+lemma inclusion_isClosedImmersion [affineLineWithDoubledOrigin.IsSeparated] :
     IsClosedImmersion inclusion := by
   rw [inclusion_eq_iso_comp_iota]
   have h₁ : IsClosedImmersion isoOpenToEqualizer.hom := inferInstance
@@ -364,7 +207,7 @@ instance : IrreducibleSpace affineLine := by
 $\mathbb{A}^1$ is not irreducible if the affine line with doubled origin is separated.
 This leads to a contradiction, proving the latter is not separated.
 -/
-lemma affineLine_not_irreducible [IsSeparated (terminal.from affineLineWithDoubledOrigin)] :
+lemma affineLine_not_irreducible [affineLineWithDoubledOrigin.IsSeparated] :
     ¬ (IrreducibleSpace affineLine) := by
   intro _
   have h₁ : IsOpen (Set.range inclusion.base) := IsOpenImmersion.isOpen_range inclusion
@@ -385,7 +228,7 @@ lemma affineLine_not_irreducible [IsSeparated (terminal.from affineLineWithDoubl
 /--
 **Main Theorem**: The Affine Line with Doubled Origin is not separated.
 -/
-theorem not_isSeparated : ¬ (IsSeparated (terminal.from affineLineWithDoubledOrigin)) := by
+theorem not_isSeparated : ¬ (affineLineWithDoubledOrigin.IsSeparated) := by
   intro h
   have : IrreducibleSpace affineLine := inferInstance
   have := affineLine_not_irreducible
