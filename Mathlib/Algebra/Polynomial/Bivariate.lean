f9510fa1127758e365da885c55d22f8fc3ad35a0
David Kurniadi Angdinata <dka31@cantab.ac.uk>
2026-03-03 23:48:43 +0000

    Fix simp

--- Mathlib/Algebra/Polynomial/Bivariate.lean
+++ Mathlib/Algebra/Polynomial/Bivariate.lean
@@ -228,7 +228,6 @@ the unique `R`-algebra homomorphism from `R[X][Y]` to `A` sending `X` to `x` and
 abbrev aevalAeval (x y : A) : R[X][Y] →ₐ[R] A :=
   aevalAevalEquiv R A ⟨x, y⟩
 
-@[simp]
 lemma aevalAevalEquiv_apply (xy : A × A) : aevalAevalEquiv R A xy = aevalAeval xy.1 xy.2 :=
   rfl
 
@@ -236,12 +235,10 @@ theorem coe_aevalAeval_eq_evalEval (x y : A) : ⇑(aevalAeval x y) = evalEval x 
   ext
   simp [aeval, aevalEquiv]
 
-@[simp]
 lemma aevalAeval_C (x y : A) (p : R[X]) : (C p).aevalAeval x y = aeval x p := by simp
 
 lemma aevalAeval_X (x y : A) : (C X : R[X][Y]).aevalAeval x y = x := by rw [aevalAeval_C, aeval_X]
 
-@[simp]
 lemma aevalAeval_Y (x y : A) : (Y : R[X][Y]).aevalAeval x y = y := by simp
 
 /-- The R-algebra automorphism given by `X ↦ Y` and `Y ↦ X`. -/
