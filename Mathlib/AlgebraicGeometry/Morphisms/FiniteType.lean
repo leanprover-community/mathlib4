2216933644b38ec3ea3fb4fccff3a40c7ab60adb
Andrew Yang <the.erd.one@gmail.com>
2026-03-03 17:58:42 +0000

    fix

--- Mathlib/AlgebraicGeometry/Morphisms/FiniteType.lean
+++ Mathlib/AlgebraicGeometry/Morphisms/FiniteType.lean
@@ -138,6 +138,7 @@ nonrec lemma LocallyOfFiniteType.jacobsonSpace
   have := PrimeSpectrum.isJacobsonRing_iff_jacobsonSpace.mpr ‹_›
   exact PrimeSpectrum.isJacobsonRing_iff_jacobsonSpace.mp (isJacobsonRing_of_finiteType (A := R))
 
+set_option backward.isDefEq.respectTransparency false in
 /--
 The category of affine schemes locally of finite type over a fixed base scheme is essentially small.
 TODO: extend this to (relatively) quasi-compact schemes.
