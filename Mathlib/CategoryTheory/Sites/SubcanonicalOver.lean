3fc7bb67447e072e4e890a79bf558abdf18a6961
Christian Merten <christian@merten.dev>
2026-03-03 16:12:25 +0100

    copyright

--- Mathlib/CategoryTheory/Sites/SubcanonicalOver.lean
+++ Mathlib/CategoryTheory/Sites/SubcanonicalOver.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2026 Christian Merten. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Christian Merten
+-/
 module
 
 public import Mathlib.CategoryTheory.Sites.Canonical
