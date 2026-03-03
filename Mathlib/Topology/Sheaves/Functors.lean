8c60b4acbf4abadeb519ca3713bb14d640e80e06
morel <smorel@math.princeton.edu>
2026-03-03 17:39:11 +0100

    clean code

--- Mathlib/Topology/Sheaves/Functors.lean
+++ Mathlib/Topology/Sheaves/Functors.lean
@@ -110,3 +110,39 @@ instance : (pushforward A f).IsRightAdjoint := (pullbackPushforwardAdjunction A 
 end Sheaf
 
 end TopCat
+
+namespace Topology.IsOpenEmbedding
+
+variable (A : Type*) [Category.{w} A] {FA : A → A → Type*} {CA : A → Type w}
+variable [∀ X Y, FunLike (FA X Y) (CA X) (CA Y)] [ConcreteCategory.{w} A FA] [HasColimits A]
+variable [HasLimits A] [PreservesLimits (CategoryTheory.forget A)]
+variable [PreservesFilteredColimits (CategoryTheory.forget A)]
+variable [(CategoryTheory.forget A).ReflectsIsomorphisms]
+
+variable {f : X ⟶ Y} (hf : IsOpenEmbedding f)
+
+open TopCat Sheaf
+
+def sheafPullback : Y.Sheaf A ⥤ X.Sheaf A :=
+  haveI := Topology.IsOpenEmbedding.functor_isContinuous hf
+  hf.isOpenMap.functor.sheafPushforwardContinuous _ _ _
+
+set_option backward.isDefEq.respectTransparency false in
+def sheafPullbackIso : Sheaf.pullback A f ≅ hf.sheafPullback A := by
+  refine Sheaf.pullbackIso A f ≪≫ (NatIso.ofComponents (fun F ↦ ?_) (fun u ↦ ?_))
+  · exact (presheafToSheaf (Opens.grothendieckTopology ↑X) A).mapIso
+      (hf.isOpenMap.pullbackIso.app _) ≪≫
+      (fullyFaithfulSheafToPresheaf (Opens.grothendieckTopology X) A).preimageIso
+      (isoSheafify (Opens.grothendieckTopology X)
+      (TopCat.Presheaf.isSheaf_of_isOpenEmbedding hf F.2)).symm
+  · dsimp
+    rw [← Functor.map_comp_assoc, hf.isOpenMap.pullbackIso.hom.naturality, Sheaf.hom_ext_iff]
+    simp only [Functor.whiskeringLeft_obj_obj, Functor.whiskeringLeft_obj_map, Functor.map_comp,
+      isoSheafify_inv, Category.assoc]
+    rw [Sheaf.comp_val, Sheaf.comp_val, Sheaf.comp_val, Sheaf.comp_val]
+    dsimp [sheafPullback, Functor.sheafPushforwardContinuous, Sheaf.forget]
+    simp only [sheafifyMap_sheafifyLift, sheafifyMap_sheafifyLift_assoc]
+    refine (sheafifyLift_unique _ _ _ _ ?_).symm
+    cat_disch
+
+end Topology.IsOpenEmbedding
