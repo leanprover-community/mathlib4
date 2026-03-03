c03b19adca86b8ddcc7a3e23a577afd9a9562afb
David Kurniadi Angdinata <dka31@cantab.ac.uk>
2026-03-03 21:44:16 +0000

    Redefine aeval and aevalAeval

--- Mathlib/Algebra/Polynomial/Bivariate.lean
+++ Mathlib/Algebra/Polynomial/Bivariate.lean
@@ -212,14 +212,29 @@ noncomputable section
 
 variable {R A : Type*} [CommSemiring R] [CommSemiring A] [Algebra R A]
 
-/-- `aevalAeval x y` is the `R`-algebra evaluation morphism sending a two-variable polynomial
-  `p : R[X][Y]` to `p(x,y)`. -/
+variable (R A) in
+/-- Given valuations `x` and `y` of the variables in an `R`-algebra `A`, the bijection induced by
+the unique `R`-algebra homomorphism from `R[X][Y]` to `A` sending `X` to `x` and `Y` to `y`. -/
+@[simps! apply_apply symm_apply symm_apply_fst symm_apply_snd]
+def aevalAevalEquiv : A × A ≃ (R[X][Y] →ₐ[R] A) where
+  toFun xy := aeval xy.fst |>.restrictScalars R |>.comp <|
+    let := Polynomial.algebra; aeval (R := R[X]) (C xy.snd) |>.restrictScalars R
+  invFun f := ⟨f <| C X, f Y⟩
+  left_inv f := by simp
+  right_inv f := algHom_ext' (by ext; simp) (by simp)
+
+/-- Given valuations `x` and `y` of the variables in an `R`-algebra `A`, `aevalAeval x y` is
+the unique `R`-algebra homomorphism from `R[X][Y]` to `A` sending `X` to `x` and `Y` to `y`. -/
 abbrev aevalAeval (x y : A) : R[X][Y] →ₐ[R] A :=
-  ((aeval x).restrictScalars R).comp
-    (letI := Polynomial.algebra; (aeval (R := R[X]) (C y)).restrictScalars R)
+  aevalAevalEquiv R A ⟨x, y⟩
+
+@[simp]
+lemma aevalAevalEquiv_apply (xy : A × A) : aevalAevalEquiv R A xy = aevalAeval xy.1 xy.2 :=
+  rfl
 
 theorem coe_aevalAeval_eq_evalEval (x y : A) : ⇑(aevalAeval x y) = evalEval x y := by
-  ext p; simp [aevalAeval, evalEval, aeval, Algebra.ofId]
+  ext
+  simp [aeval]
 
 @[simp]
 lemma aevalAeval_C (x y : A) (p : R[X]) : (C p).aevalAeval x y = aeval x p := by simp
@@ -228,15 +243,6 @@ lemma aevalAeval_X (x y : A) : (C X : R[X][Y]).aevalAeval x y = x := by rw [aeva
 
 @[simp]
 lemma aevalAeval_Y (x y : A) : (Y : R[X][Y]).aevalAeval x y = y := by simp
-
-variable (R A) in
-/-- The bijection `(R[X][Y] →ₐ[R] A) ≃ A × A` induced by `Polynomial.aevalAeval`. -/
-@[simps]
-noncomputable def Bivariate.algHomEquiv : (R[X][Y] →ₐ[R] A) ≃ A × A where
-  toFun f := ⟨f <| C X, f Y⟩
-  invFun xy := aevalAeval xy.fst xy.snd
-  left_inv _ := algHom_ext' (algHom_ext <| aevalAeval_X ..) <| aevalAeval_Y ..
-  right_inv _ := by simp_rw [aevalAeval_X, aevalAeval_Y]
 
 /-- The R-algebra automorphism given by `X ↦ Y` and `Y ↦ X`. -/
 def Bivariate.swap : R[X][Y] ≃ₐ[R] R[X][Y] := by
