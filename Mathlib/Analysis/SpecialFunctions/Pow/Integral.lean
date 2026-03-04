eda68794dc44336dee95b83d009d9dcacf1e5f6f
Moritz Doll <moritz.doll@googlemail.com>
2026-03-05 10:55:57 +1100

    cherry

--- Mathlib/Analysis/SpecialFunctions/Pow/Integral.lean
+++ Mathlib/Analysis/SpecialFunctions/Pow/Integral.lean
@@ -7,6 +7,8 @@ module
 
 public import Mathlib.Analysis.SpecialFunctions.Integrals.Basic
 public import Mathlib.MeasureTheory.Integral.Layercake
+public import Mathlib.MeasureTheory.Constructions.HaarToSphere
+public import Mathlib.Tactic.MoveAdd
 
 /-!
 # The integral of the real power of a nonnegative function
@@ -18,6 +20,8 @@ a representation of the integral of the p:th power of a nonnegative function `f`
 A variant of the formula with measures of sets of the form `{ω | f(ω) > t}` instead of
 `{ω | f(ω) ≥ t}` is also included.
 
+Moreover, we prove that `‖x‖ ^ (-d + ε)` is locally integrable.
+
 ## Main results
 
 * `MeasureTheory.lintegral_rpow_eq_lintegral_meas_le_mul` and
@@ -25,6 +29,8 @@ A variant of the formula with measures of sets of the form `{ω | f(ω) > t}` in
   other common special cases of the layer cake formulas, stating that for a nonnegative function `f`
   and `p > 0`, we have `∫ f(ω)ᵖ ∂μ(ω) = p * ∫ μ {ω | f(ω) ≥ t} * tᵖ⁻¹ dt` and
   `∫ f(ω)ᵖ ∂μ(ω) = p * ∫ μ {ω | f(ω) > t} * tᵖ⁻¹ dt`, respectively.
+* `MeasureTheory.locallyIntegrable_of_norm_le_rpow`:
+  a function that is dominated by `‖x‖ ^ (-d + ε)` is locally integrable.
 
 ## Tags
 
@@ -93,5 +99,47 @@ theorem lintegral_rpow_eq_lintegral_meas_lt_mul
   rw [ht]
 
 end LayercakeLT
+
+variable {E F : Type*} [NormedAddCommGroup E] [NormedSpace ℝ E] [FiniteDimensional ℝ E]
+  [MeasurableSpace E] [BorelSpace E]
+  [NormedAddCommGroup F]
+  {μ : Measure E} [μ.IsAddHaarMeasure]
+
+open Set Metric in
+lemma integrableOn_ball_of_norm_le_rpow (hd : 1 ≤ Module.finrank ℝ E) {f : E → F} {C α r : ℝ}
+    (hα : α < Module.finrank ℝ E) (hr : 0 < r)
+    (h_decay : ∀ x, ‖f x‖ ≤ C * ‖x‖ ^ (-α))
+    (h_meas : AEStronglyMeasurable f μ) :
+    IntegrableOn f (ball (0 : E) r) μ := by
+  haveI : Nontrivial E := by
+    apply Module.nontrivial_of_finrank_pos (R := ℝ)
+    positivity
+  have hint : IntegrableOn (fun y ↦ y ^ (Module.finrank ℝ E - 1) • (C * y ^ (-α))) (Ioo 0 r) := by
+    simp only [smul_eq_mul]
+    have h_rpow : IntegrableOn (fun y ↦ y ^ ((Module.finrank ℝ E : ℝ) - 1 - α)) (Ioo 0 r) := by
+      rw [intervalIntegral.integrableOn_Ioo_rpow_iff hr]
+      linarith
+    apply IntegrableOn.congr_fun (h_rpow.const_mul C) ?_ measurableSet_Ioo
+    intro y ⟨hy, _⟩
+    simp only
+    move_mul [C]
+    rw [← Real.rpow_natCast y (Module.finrank ℝ E - 1), ← Real.rpow_add hy]
+    congr
+    norm_cast
+  rw [← integrableOn_fun_norm_addHaar μ] at hint
+  apply Integrable.mono' hint h_meas.restrict
+  filter_upwards with x
+  exact h_decay x
+
+/-- A function that is dominated by `‖x‖ ^ (-d + ε)` is locally integrable -/
+theorem locallyIntegrable_of_norm_le_rpow (hdim : 1 ≤ Module.finrank ℝ E) {f : E → F} {C α : ℝ}
+    (hα : α < Module.finrank ℝ E)
+    (h_decay : ∀ x, ‖f x‖ ≤ C * ‖x‖ ^ (-α)) (h_meas : AEStronglyMeasurable f μ) :
+    LocallyIntegrable f μ := by
+  rw [locallyIntegrable_iff]
+  intro K hK
+  obtain ⟨R, hR_pos, hR⟩ := hK.isBounded.exists_pos_norm_lt
+  exact (integrableOn_ball_of_norm_le_rpow hdim hα hR_pos h_decay h_meas).mono_set
+    (mem_ball_zero_iff.mpr <| hR · ·)
 
 end MeasureTheory
