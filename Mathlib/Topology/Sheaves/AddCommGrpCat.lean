70d99ace6e16cd709e0db513cafb631a933d7c04
Brian-Nugent <bnugent@uw.edu>
2026-03-04 10:28:35 -0700

    fix: reorder arguments in Sheaf.addCommGrpCat_shortExact_app_zero for consistency

--- Mathlib/Topology/Sheaves/AddCommGrpCat.lean
+++ Mathlib/Topology/Sheaves/AddCommGrpCat.lean
@@ -43,7 +43,7 @@ lemma Presheaf.restrict_sum {V : Opens X} {F : Presheaf AddCommGrpCat X} (h : V 
   cat_disch
 
 lemma Sheaf.addCommGrpCat_shortExact_app_zero {S : ShortComplex (Sheaf AddCommGrpCat X)}
-    (hS : S.Exact) (s : S.X₂.val.obj (op U)) (h : S.g.val.app (op U) s = 0) (hf : Mono S.f) :
+    (hS : S.Exact) (hf : Mono S.f) (s : S.X₂.val.obj (op U)) (h : S.g.val.app (op U) s = 0) :
     ∃(t : S.X₁.val.obj (op U)), S.f.val.app (op U) t = s := by
   have := ((Functor.preservesFiniteLimits_tfae (forget AddCommGrpCat X)).out 1 3).mpr
     (inferInstanceAs (Limits.PreservesFiniteLimits (forget AddCommGrpCat X)))
