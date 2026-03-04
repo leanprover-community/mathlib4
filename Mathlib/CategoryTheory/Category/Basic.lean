566010d0d978353852190bec370f6b21f1ceebd6
Robin Carlier <57142648+robin-carlier@users.noreply.github.com>
2026-03-04 18:45:21 +0100

    test

--- Mathlib/CategoryTheory/Category/Basic.lean
+++ Mathlib/CategoryTheory/Category/Basic.lean
@@ -240,7 +240,7 @@ class Category (obj : Type u) : Type max u (v + 1) extends CategoryStruct.{v} ob
     cat_disch
 
 attribute [to_dual existing (attr := simp, grind =) id_comp] Category.comp_id
-attribute [simp, grind _=_] Category.assoc
+attribute [simp ↓, grind _=_] Category.assoc
 
 initialize_simps_projections Category (-Hom)
 
