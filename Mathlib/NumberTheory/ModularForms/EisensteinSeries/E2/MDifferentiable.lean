94a36f3bb2a0da157bc3e4032dc4217e2e733f2d
Chris Birkbeck <c.birkbeck@uea.ac.uk>
2026-03-04 14:42:45 +0000

    add gauss as co-author

--- Mathlib/NumberTheory/ModularForms/EisensteinSeries/E2/MDifferentiable.lean
+++ Mathlib/NumberTheory/ModularForms/EisensteinSeries/E2/MDifferentiable.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2026 Chris Birkbeck. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Chris Birkbeck
+Authors: Chris Birkbeck, Gauss AI (Math.Inc).
 -/
 
 module
