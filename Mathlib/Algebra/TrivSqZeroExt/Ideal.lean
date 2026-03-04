7c0aa26a8b2a22e8d8d0567b2fc2bac61b80dbde
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2026-03-04 09:37:50 +0000

    feat(Algebra/TrivSqZeroExt): the canonical ideal has square zero (#35690)
    - Mv `Mathlib/Algebra/TrivSqZeroExt.lean` to `Mathlib/Algebra/TrivSqZeroExt/Basic.lean`
    - Define `TrivSqZeroExt.kerIdeal`: the canonical ideal of `TrivSqZeroExt R M`
    - Prove `TrivSqZero.sqZero`: this ideal has square zero.
    
    co-authored with @mariainesdff
    
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>
    Co-authored-by: Oliver Nash <7734364+ocfnash@users.noreply.github.com>

---
+++ Mathlib/Algebra/TrivSqZeroExt/Ideal.lean
@@ -0,0 +1,44 @@
+/-
+Copyright (c) 2026 Antoine Chambert-Loir, María-Inés de Frutos-Fernández. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Antoine Chambert-Loir, María-Inés de Frutos-Fernández
+-/
+module
+
+public import Mathlib.Algebra.TrivSqZeroExt.Basic
+public import Mathlib.RingTheory.Ideal.Maps
+
+/-!
+# The square zero ideal of the trivial square-zero extension
+
+- `TrivSqZeroExt.kerIdeal`: the ideal in the trivial square-zero extension
+
+- `TrivSqZeroExt.kerIdeal_sq `: this ideal has square zero.
+
+-/
+
+@[expose] public section
+
+namespace TrivSqZeroExt
+
+open Ideal
+
+variable (R M : Type*)
+  [CommSemiring R] [AddCommMonoid M] [Module R M] [Module Rᵐᵒᵖ M] [IsCentralScalar R M]
+
+/-- The kernel of the `AlgHom` `fstHom R R M` -/
+def kerIdeal : Ideal (TrivSqZeroExt R M) := RingHom.ker (fstHom R R M)
+
+theorem mem_kerIdeal_iff_inr (x : TrivSqZeroExt R M) : x ∈ kerIdeal R M ↔ x = inr x.snd := by
+  obtain ⟨r, m⟩ := x
+  simp only [kerIdeal, RingHom.mem_ker, fstHom_apply, fst_mk]
+  exact ⟨fun hr => by rw [hr]; rfl, fun hrm => by rw [← fst_mk r m, hrm, fst_inr]⟩
+
+@[simp] theorem kerIdeal_sq : kerIdeal R M ^ 2 = ⊥ := by
+  simp only [pow_two, eq_bot_iff, mul_le, mem_kerIdeal_iff_inr]
+  rintro x hx y hy
+  rw [hx, hy, mem_bot, inr_mul_inr]
+
+end TrivSqZeroExt
+
+end
