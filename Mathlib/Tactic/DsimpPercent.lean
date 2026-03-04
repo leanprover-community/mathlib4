137590b4ffe9640ce5d9c277d239dfc9f091932e
Robin Carlier <57142648+robin-carlier@users.noreply.github.com>
2026-03-04 22:29:13 +0100

    Update Mathlib/Tactic/DsimpPercent.lean
    Co-authored-by: Jovan Gerbscheid <56355248+JovanGerb@users.noreply.github.com>

--- Mathlib/Tactic/DsimpPercent.lean
+++ Mathlib/Tactic/DsimpPercent.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2026 Robin Carlier. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Robin Carlier, Jovan Gerbstein
+Authors: Robin Carlier, Jovan Gerbscheid
 -/
 module
 
