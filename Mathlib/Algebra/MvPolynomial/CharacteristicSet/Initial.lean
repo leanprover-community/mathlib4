f36c9ea684f808308a367c8046a5ae4f308aca5b
Yuxuan Xiao <xiao-yuxuan@qq.com>
2026-03-05 17:35:00 +0800

    remove `initialOf_mul_C`

--- Mathlib/Algebra/MvPolynomial/CharacteristicSet/Initial.lean
+++ Mathlib/Algebra/MvPolynomial/CharacteristicSet/Initial.lean
@@ -501,9 +501,6 @@ variable [NoZeroDivisors R] (i : σ) (p : MvPolynomial σ R)
 @[simp] theorem initialOf_C_mul {r : R} : (C r * p).initialOf i = C r * p.initialOf i :=
   by simp only [initialOf_mul_eq, initialOf_C]
 
-@[simp] theorem initialOf_mul_C {r : R} : (p * C r).initialOf i = p.initialOf i * C r :=
-  by simp only [initialOf_mul_eq, initialOf_C]
-
 @[simp] theorem initialOf_smul {r : R} : (r • p).initialOf i = r • p.initialOf i := by
   rw [smul_eq_C_mul, smul_eq_C_mul, p.initialOf_C_mul i]
 
