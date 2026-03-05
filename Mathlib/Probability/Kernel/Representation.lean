3e9ce8fbe6e754f79c9f680300edacd4293635b5
Gaëtan Serré <56162277+gaetanserre@users.noreply.github.com>
2026-03-05 13:29:07 +0100

    Update Mathlib/Probability/Kernel/Representation.lean
    Co-authored-by: Etienne Marion <66847262+EtienneC30@users.noreply.github.com>

--- Mathlib/Probability/Kernel/Representation.lean
+++ Mathlib/Probability/Kernel/Representation.lean
@@ -3,7 +3,6 @@ Copyright (c) 2025 Gaëtan Serré. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Gaëtan Serré, Rémy Degenne
 -/
-
 module
 
 public import Mathlib.Analysis.SpecialFunctions.Sigmoid
