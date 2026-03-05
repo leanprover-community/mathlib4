d17b498bb1128e93a54903ece861ab59782c26db
Remy Degenne <remydegenne@gmail.com>
2026-03-05 17:38:48 +0100

    extract lemma

--- Mathlib/MeasureTheory/Measure/TightNormed.lean
+++ Mathlib/MeasureTheory/Measure/TightNormed.lean
@@ -261,20 +261,15 @@ lemma isTightMeasureSet_range_of_tendsto_limsup_inner_of_norm_eq_one
 lemma isTightMeasureSet_range_of_tendsto_limsup_measureReal_inner_of_norm_eq_one
     (h : ∀ y, ‖y‖ = 1 →
       Tendsto (fun r : ℝ ↦ limsup (fun n ↦ (μ n).real {x | r < ‖⟪y, x⟫_𝕜‖}) atTop) atTop (𝓝 0))
-    (C : ℝ≥0) (hμ : ∀ n, μ n .univ ≤ C) :
+    (C : ℝ≥0) (hμ : ∀ᶠ n in atTop, μ n .univ ≤ C) :
     IsTightMeasureSet (Set.range μ) := by
   refine isTightMeasureSet_range_of_tendsto_limsup_inner_of_norm_eq_one 𝕜 fun z hz ↦ ?_
   have h_ofReal (r : ℝ) : limsup (fun n ↦ μ n {x | r < ‖⟪z, x⟫_𝕜‖}) atTop
       = ENNReal.ofReal (limsup (fun n ↦ (μ n).real {x | r < ‖⟪z, x⟫_𝕜‖}) atTop) := by
-    simp_rw [measureReal_def, ENNReal.limsup_toReal_eq (by simp)
-      (.of_forall fun n ↦ (measure_mono (Set.subset_univ _)).trans (hμ n))]
-    rw [ENNReal.ofReal_toReal]
-    refine ne_top_of_le_ne_top (by simp : C ≠ ∞) ?_
-    refine limsup_le_of_le ?_ (.of_forall fun n ↦ (measure_mono (Set.subset_univ _)).trans (hμ n))
-    exact IsCoboundedUnder.of_frequently_ge <| .of_forall fun _ ↦ zero_le _
-  simp only [h_ofReal]
-  rw [← ENNReal.ofReal_zero]
-  exact ENNReal.tendsto_ofReal (h z hz)
+    simp_rw [measureReal_def]
+    rw [ENNReal.ofReal_limsup_toReal (C := C)]
+    filter_upwards [hμ] with n hμn using (measure_mono (Set.subset_univ _)).trans hμn
+  simpa only [h_ofReal, ← ENNReal.ofReal_zero] using ENNReal.tendsto_ofReal (h z hz)
 
 end InnerProductSpace
 
