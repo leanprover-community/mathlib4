e9b5e1a0a8e4788abb3a65f381351b3743298ac0
Kenny Lau <kc_kennylau@yahoo.com.hk>
2026-03-05 14:29:37 +0000

    feat(RingTheory): define R-linear graded algebra homomorphism (#30365)
    This PR defines R-linear graded algebra homomorphisms, which are R-algebra homomorphisms that preserve the grading. We also provide the basic properties (aka the "API").
    
    Zulip discussion: [#mathlib4 > redefine Graded Algebra](https://leanprover.zulipchat.com/#narrow/channel/287929-mathlib4/topic/redefine.20Graded.20Algebra/near/543847326)

--- Mathlib.lean
+++ Mathlib.lean
@@ -6199,6 +6199,7 @@ public import Mathlib.RingTheory.FractionalIdeal.Operations
 public import Mathlib.RingTheory.FreeCommRing
 public import Mathlib.RingTheory.FreeRing
 public import Mathlib.RingTheory.Frobenius
+public import Mathlib.RingTheory.GradedAlgebra.AlgHom
 public import Mathlib.RingTheory.GradedAlgebra.Basic
 public import Mathlib.RingTheory.GradedAlgebra.FiniteType
 public import Mathlib.RingTheory.GradedAlgebra.Homogeneous.Ideal
