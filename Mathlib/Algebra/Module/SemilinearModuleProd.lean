306c3aa985d7b0bb1da69848f5f095f1cc0519e0
Yoh Tanimoto <hoyt@jcom.home.ne.jp>
2026-03-05 23:29:12 +0100

    Update SemilinearModuleProd.lean

--- Mathlib/Algebra/Module/SemilinearModuleProd.lean
+++ Mathlib/Algebra/Module/SemilinearModuleProd.lean
@@ -5,29 +5,31 @@ Authors: Yoh Tanimoto
 -/
 module
 
-public import Mathlib.Algebra.Module.LinearMap.Defs
-public import Mathlib.Algebra.Star.Basic
-public import Mathlib.LinearAlgebra.Complex.Module
+public import Mathlib.LinearAlgebra.Span.Defs
 
 /-!
 # Twisted product module by a ring homomorphism
 
 For `Module R E` and `Module S F` and `σ : R →+* S`, we define the `R`-module structure
-on `E × F` given by `s • ⟨x, y⟩ := ⟨s • x, σ s • y⟩`. As an example, this gives a natural `ℂ`-linear
-space structure on the graph of antilinear operator.
+on a type synonym `E ×[σ] F` of `E × F` given by `s • ⟨x, y⟩ := ⟨s • x, σ s • y⟩`. As an example,
+this gives a natural `ℂ`-linear space structure on the graph of antilinear operator.
 
-Problem with defining a new `Semiring T` : there is a canonical instance `Module T (E × F)` and
-we want it to work.
-If we define either a type synonym or one-field structure, say `T`, we want that `Module T E` is
-the usual one while `Module T F` is the twisted one. However, in the case of interest (`E = F = ℂ`)
-with `σ = starRingEnd ℂ`, we are forced to use the usual instance `Module T ℂ`. So it seems
-necessary to define a new type for the module with twist and duplicate code.
+## Implementation notes
+
+The main application is the case where `Module ℂ E`, `Module ℂ F` and one has a antilinear operator
+`A : E →ₛₗ[starRingEnd ℂ] F`. The graph of `A` is a `ℂ`-module, but `ℂ` acts linearly on `E` while
+antilinearly on `F`. Such a graph cannot be a `Submodule ℂ (E × F)` because there is already the
+natural instance `Module ℂ (E × F)` where `ℂ` acts linearly on `F`.
+
+In order to implement this, note that defining a type synonym `R` of `ℂ` does not work, because one
+might want to take `E = F`, on which any `R : Ring` can have only one instance `Module R F`,
+in particular, there is already the canonical instance `Module R (E × F)`.
+This means that we cannot use the product module `E × F`, but we have to make a type synonym and
+duplicate code for `E ×[σ] F`.
 
 -/
 
 @[expose] public section
-
-open Complex
 
 set_option linter.unusedVariables false in
 /-- A `E ×[σ] F` or `E ×[σ] F` is a module structure on the product `E × F` with
@@ -209,104 +211,5 @@ theorem prod_sup_prod {p p' : Submodule R E} {q q' : Submodule S F} :
 end Submodule
 
 end SemilinearProdModule
-
-end
-
-section
-
-open Complex SemilinearProdModule
-
--- the desired result. the second component is twisted.
-example :
-    I • (mk 0 1 : ℂ ×[starRingEnd ℂ] ℂ) = (mk 0 (-I) : ℂ ×[starRingEnd ℂ] ℂ) := by
-  ext <;> simp
-
--- cf. the usual `Prod` structure.
-example : I • (⟨0, 1⟩ : ℂ × ℂ) = (⟨0, I⟩ : ℂ × ℂ) := by ext <;> simp
-
-end
-
--- below is a failed attempt to use a type synonym for `R`.
--- it does not work because of multiple `SMul` instances.
-
-section
-
-set_option linter.unusedVariables false in
-/-- A `WithTwist σ` with `σ : R →+* S` is a type synonym of `R`, where when there is `Module S F`,
-we define `Mpdule (WithTwist σ) F` by `r • x = (σ r) • x`. -/
-@[nolint unusedArguments]
-def WithTwist {R S : Type*} [Semiring R] [Semiring S] (σ : R →+* S) := R
-
-namespace WithTwist
-
-variable {R S : Type*} [Semiring R] [Semiring S] (σ : R →+* S)
-
-instance : Semiring (WithTwist σ) := inferInstanceAs (Semiring R)
-
-section
-
-variable {E : Type*} [SMul R E]
-
-instance : SMul (WithTwist σ) E where
-  smul r x := (id r : R) • x
-
-instance : SMul R (WithTwist σ) where
-  smul r x := r • (id x : R)
-
-instance : Module R (WithTwist σ) where
-  mul_smul _ _ _ := by simp [mul_assoc]
-  one_smul _ := by simp
-  smul_zero _ := by simp
-  smul_add _ _ _ := by simp [mul_add]
-  add_smul _ _ _ := by simp [add_mul]
-  zero_smul _ := by simp
-
-/-- The semilinear map `(WithTwist σ) →ₛₗ[σ] S` identical to `σ`. -/
-def toLinearMap : (WithTwist σ) →ₛₗ[σ] S where
-  toFun r := σ r
-  map_add' _ _ := by simp
-  map_smul' _ _ := by simp
-
-end
-
-section
-
-variable {F : Type*} [SMul S F]
-
-instance : SMul (WithTwist σ) F where
-  smul r x := σ r • x
-
-@[simp]
-lemma smul_eq (s : WithTwist σ) (x : F) : s • x = σ s • x := rfl
-
-end
-
-section
-
-variable {E : Type*} [AddCommMonoid E] [Module R E]
-
-instance : Module (WithTwist σ) E := inferInstanceAs (Module R E)
-
-variable {F : Type*} [AddCommMonoid F] [Module S F]
-
-instance : Module (WithTwist σ) F where
-  mul_smul _ _ _ := by simp [mul_smul]
-  one_smul _ := by simp
-  smul_zero _ := by simp
-  smul_add _ _ _ := by simp [smul_add]
-  add_smul _ _ _ := by simp [add_smul]
-  zero_smul _ := by simp
-
-end
-
-end WithTwist
-
-example :
-  Complex.I • (⟨0, 1⟩ : ℂ × (WithTwist ((starRingEnd ℂ))))
-  = (⟨0, Complex.I⟩ : ℂ × (WithTwist ((starRingEnd ℂ))))
-  := by
-  ext
-  · simp
-  · simp
 
 end
