a2af082337f2ab8ce176ea325dbb3bd2769094d6
Kenny Lau <kc_kennylau@yahoo.com.hk>
2026-03-05 15:18:54 +0000

    golf

--- Mathlib/RingTheory/Localization/Saturation.lean
+++ Mathlib/RingTheory/Localization/Saturation.lean
@@ -6,7 +6,7 @@ Authors: Kenny Lau
 module
 
 public import Mathlib.Algebra.Group.Submonoid.Saturation
-public import Mathlib.RingTheory.Localization.Defs
+public import Mathlib.RingTheory.Localization.Basic
 
 /-! # Localization and submonoid saturation
 
@@ -20,7 +20,7 @@ Crucially, the saturation of `S` is precisely the elements that become a unit in
 @[expose] public section
 
 namespace IsLocalization
-variable {R : Type*} [CommRing R] {S : Submonoid R} {A : Type*} [CommRing A] [Algebra R A]
+variable {R : Type*} [CommRing R] {S T : Submonoid R} {A : Type*} [CommRing A] [Algebra R A]
 
 open Submonoid
 
@@ -29,21 +29,13 @@ theorem mem_saturation_iff_isUnit_algebraMap [IsLocalization S A] {x : R} :
     x ∈ S.saturation ↔ IsUnit (algebraMap R A x) := by
   rw [IsLocalization.algebraMap_isUnit_iff S, Submonoid.mem_saturation_iff_exists_dvd]
 
-variable (S) in
-theorem of_saturation_eq [IsLocalization S A] (T : Submonoid R) (ih : S.saturation = T.saturation) :
-    IsLocalization T A := by
-  refine ⟨fun x ↦ ?_, fun y ↦ ?_, fun {x₁ x₂} eq ↦ ?_⟩
-  · rw [← mem_saturation_iff_isUnit_algebraMap S A, ih]
-    exact le_toSubmonoid_saturation x.2
-  · obtain ⟨⟨x₁, ⟨x₂, hx₂⟩⟩, h⟩ := IsLocalization.surj S y
-    obtain ⟨x₃, hx₃⟩ := mem_saturation_iff.mp <| ih ▸ le_toSubmonoid_saturation hx₂
-    exact ⟨(x₁ * x₃, ⟨_, hx₃⟩), by simpa [mul_assoc] using congr($h * algebraMap R A x₃)⟩
-  · obtain ⟨⟨x₃, hx₃⟩, eq⟩ := IsLocalization.exists_of_eq (M := S) eq
-    obtain ⟨x₄, hx₄⟩ := mem_saturation_iff.mp <| ih ▸ le_toSubmonoid_saturation hx₃
-    exact ⟨⟨_, hx₄⟩, by simpa [mul_right_comm] using congr($eq * x₄)⟩
-
 theorem saturation_iff : IsLocalization S.saturation.toSubmonoid A ↔ IsLocalization S A :=
-  ⟨fun h ↦ .of_saturation_eq S.saturation.toSubmonoid _ <| by simp,
-  fun h ↦ .of_saturation_eq S _ <| by simp⟩
+  .symm <| iff_of_le_of_exists_dvd _ _ le_toSubmonoid_saturation <| by
+    simp [mem_saturation_iff_exists_dvd]
+
+variable (S T) in
+theorem of_saturation_eq [IsLocalization S A] (ih : S.saturation = T.saturation) :
+    IsLocalization T A := by
+  rwa [← saturation_iff, ← ih, saturation_iff]
 
 end IsLocalization
