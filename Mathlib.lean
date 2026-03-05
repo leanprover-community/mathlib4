83f661975ca5e470737269e32dc1eeecfaf31c55
Jeremy Tan Jie Rui <54175463+Parcly-Taxel@users.noreply.github.com>
2026-03-05 11:23:24 +0000

    feat: radical lemmas for natural numbers and integers (#35673)
    These are needed as part of a project I'm doing for my PhD under Frank Stephan.
    
    Co-authored-by: Parcly Taxel <reddeloostw@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -6511,6 +6511,7 @@ public import Mathlib.RingTheory.QuasiFinite.Basic
 public import Mathlib.RingTheory.QuasiFinite.Polynomial
 public import Mathlib.RingTheory.QuasiFinite.Weakly
 public import Mathlib.RingTheory.QuotSMulTop
+public import Mathlib.RingTheory.Radical
 public import Mathlib.RingTheory.Radical.Basic
 public import Mathlib.RingTheory.Radical.NatInt
 public import Mathlib.RingTheory.ReesAlgebra
