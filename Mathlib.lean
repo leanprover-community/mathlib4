9dc021ee1d14ef0cbd0e2fce71d45fea22858bb6
Kenny Lau <kc_kennylau@yahoo.com.hk>
2026-03-05 14:25:39 +0000

    revert deletion

--- Mathlib.lean
+++ Mathlib.lean
@@ -3755,6 +3755,7 @@ public import Mathlib.Data.FunLike.Basic
 public import Mathlib.Data.FunLike.Embedding
 public import Mathlib.Data.FunLike.Equiv
 public import Mathlib.Data.FunLike.Fintype
+public import Mathlib.Data.FunLike.Graded
 public import Mathlib.Data.Holor
 public import Mathlib.Data.Ineq
 public import Mathlib.Data.Int.AbsoluteValue
