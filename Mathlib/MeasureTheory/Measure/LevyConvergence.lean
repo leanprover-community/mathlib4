bf22669c83d1147c902a628f3887005c0c484541
Etienne Marion <baguettes_casino0c@icloud.com>
2026-03-06 18:36:23 +0000

    wip

--- Mathlib/MeasureTheory/Measure/LevyConvergence.lean
+++ Mathlib/MeasureTheory/Measure/LevyConvergence.lean
@@ -150,28 +150,42 @@ and the integrals of elements of `A` with respect to `μ` converge to the integr
 with respect to `μ₀`, then `μ` converges weakly to `μ₀`. -/
 lemma ProbabilityMeasure.tendsto_of_tight_of_separatesPoints (𝕜 : Type*) [RCLike 𝕜]
     {E : Type*} [MetricSpace E] [CompleteSpace E] [SecondCountableTopology E]
-    [MeasurableSpace E] [BorelSpace E] {ι : Type*} {l : Filter ι} [l.IsCountablyGenerated]
+    [MeasurableSpace E] [BorelSpace E] {ι : Type*} {l : Filter ι}
+    [l.NeBot]
     {μ : ι → ProbabilityMeasure E} (h_tight : IsTightMeasureSet {(μ n : Measure E) | n})
     {μ₀ : ProbabilityMeasure E}
     {A : StarSubalgebra 𝕜 (E →ᵇ 𝕜)} (hA : (A.map (toContinuousMapStarₐ 𝕜)).SeparatesPoints)
     (heq : ∀ g ∈ A, Tendsto (fun n ↦ ∫ x, g x ∂(μ n)) l (𝓝 (∫ x, g x ∂μ₀))) :
     Tendsto μ l (𝓝 μ₀) := by
-  refine Filter.tendsto_of_subseq_tendsto fun ns hns ↦ ?_
+  rw [Filter.tendsto_iff_ultrafilter]
+  intro g hg
+  -- refine Filter.tendsto_of_subseq_tendsto fun ns hns ↦ ?_
   have h_compact : IsCompact (closure {μ n | n}) :=
     isCompact_closure_of_isTightMeasureSet (S := {μ n | n}) (by simpa using h_tight)
-  obtain ⟨μ', hμ'_mem, φ, hφ_mono, hφ_tendsto⟩ : ∃ μ' ∈ closure {μ n | n},
-      ∃ φ, StrictMono φ ∧ Tendsto ((fun n ↦ μ (ns n)) ∘ φ) atTop (𝓝 μ') :=
-    h_compact.tendsto_subseq fun n ↦ subset_closure ⟨ns n, rfl⟩
-  refine ⟨φ, ?_⟩
-  suffices μ' = μ₀ from this ▸ hφ_tendsto
+  have : g.map μ ≤ 𝓟 (closure {μ n | n}) := by
+    simp
+    apply hg
+    convert univ_mem
+    ext n
+    simp
+    refine subset_closure ?_
+    simp
+  obtain ⟨μ', h1, h2 : Filter.Tendsto μ g (𝓝 μ')⟩ := h_compact.ultrafilter_le_nhds (g.map μ) this
+
+  -- obtain ⟨μ', hμ'_mem, φ, hφ_mono, hφ_tendsto⟩ : ∃ μ' ∈ closure {μ n | n},
+  --     ∃ φ, StrictMono φ ∧ Tendsto ((fun n ↦ μ (ns n)) ∘ φ) atTop (𝓝 μ') :=
+  --   h_compact.tendsto_subseq fun n ↦ subset_closure ⟨ns n, rfl⟩
+  -- refine ⟨φ, ?_⟩
+
+  suffices μ' = μ₀ from this ▸ h2
   suffices (μ' : Measure E) = μ₀ by ext; rw [this]
   refine ext_of_forall_mem_subalgebra_integral_eq_of_pseudoEMetric_complete_countable hA
-    fun g hg ↦ ?_
-  specialize heq g hg
-  suffices Tendsto (fun n ↦ ∫ x, g x ∂(μ (ns (φ n)))) atTop (𝓝 (∫ x, g x ∂μ')) from
-    tendsto_nhds_unique this <| heq.comp (hns.comp hφ_mono.tendsto_atTop)
-  rw [ProbabilityMeasure.tendsto_iff_forall_integral_rclike_tendsto 𝕜] at hφ_tendsto
-  exact hφ_tendsto g
+    fun a ha ↦ ?_
+  specialize heq a ha
+  suffices Tendsto (fun n ↦ ∫ x, a x ∂(μ n)) g (𝓝 (∫ x, a x ∂μ')) from
+    tendsto_nhds_unique this (heq.comp hg)
+  rw [ProbabilityMeasure.tendsto_iff_forall_integral_rclike_tendsto 𝕜] at h2
+  exact h2 a
 
 variable {ι : Type*} {l : Filter ι} {μ₀ : ProbabilityMeasure E}
 
