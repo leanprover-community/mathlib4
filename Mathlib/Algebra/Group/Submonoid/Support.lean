e0eb32c7642c2d9ab9e99b77b1347028d778faa5
artie2000 <artem.khovanov@gmail.com>
2026-03-06 13:36:35 +0000

    update to new definition

--- Mathlib/Algebra/Group/Submonoid/Support.lean
+++ Mathlib/Algebra/Group/Submonoid/Support.lean
@@ -8,6 +8,11 @@ module
 public import Mathlib.Algebra.Group.Subgroup.Pointwise
 public import Mathlib.Algebra.Group.Subgroup.Lattice
 
+/-import Mathlib.Algebra.Group.Pointwise.Set.Basic
+import Mathlib.Algebra.Group.Subgroup.Ker
+import Mathlib.Algebra.Group.Subgroup.Pointwise
+import Mathlib.Algebra.Group.Submonoid.Membership-/
+
 import Mathlib.Tactic.ApplyFun
 
 /-!
@@ -26,35 +31,9 @@ The names for these concepts are taken from the theory of convex cones.
 * `AddSubmonoid.IsPointed`: typeclass for submonoids with zero support.
 * `AddSubmonoid.IsSpanning`: typeclass for submonoids generating the whole group.
 
-## Tags
-
-support, positive cone, group cone
-
 -/
 
-@[expose] public section
-
-namespace AddSubmonoid
-
-variable {G : Type*} [AddGroup G] (M : AddSubmonoid G)
-
-/-- Typeclass for submonoids of a group with zero support. -/
-class IsPointed (M : AddSubmonoid G) : Prop where
-  eq_zero_of_mem_of_neg_mem {x} (hx₁ : x ∈ M) (hx₂ : -x ∈ M) : x = 0 := by aesop
-
-export IsPointed (eq_zero_of_mem_of_neg_mem)
-
-attribute [aesop 50%, aesop safe forward (immediate := [hx₁])] eq_zero_of_mem_of_neg_mem
-
-/-- Typeclass for submonoids `M` of a group `G` such that `M` generates `G` as a subgroup. -/
-class IsSpanning (M : AddSubmonoid G) : Prop where
-  mem_or_neg_mem (M) (a : G) : a ∈ M ∨ -a ∈ M := by aesop
-
-export IsSpanning (mem_or_neg_mem)
-
-attribute [aesop safe, aesop safe forward] mem_or_neg_mem
-
-end AddSubmonoid
+-- TODO : add to_additive cleanups AddPointed -> Pointed, AddSpanning -> Spanning
 
 namespace Submonoid
 
@@ -86,58 +65,61 @@ theorem _root_.Subgroup.gc_toSubmonoid_mulSupport :
     GaloisConnection (α := Subgroup G) Subgroup.toSubmonoid mulSupport :=
   fun _ _ ↦ ⟨fun _ _ ↦ by aesop, fun h _ hx ↦ (h hx).1⟩
 
-/-- Typeclass for submonoids of a group with zero support. -/
-@[to_additive]
-class IsMulPointed (M : Submonoid G) : Prop where
-  eq_one_of_mem_of_inv_mem {x} (hx₁ : x ∈ M) (hx₂ : x⁻¹ ∈ M) : x = 1 := by aesop
+variable {M}
 
-export IsMulPointed (eq_one_of_mem_of_inv_mem)
+variable (M) in
+/-- A submonoid is pointed if it has zero support. -/
+@[to_additive IsPointed /-- A submonoid is pointed if it has zero support. -/]
+def IsMulPointed := ∀ x ∈ M, x⁻¹ ∈ M → x = 1
 
-attribute [aesop 50% apply, aesop safe forward (immediate := [hx₁])] eq_one_of_mem_of_inv_mem
+namespace IsMulPointed
 
-@[to_additive (attr := aesop safe forward (immediate := [hx₂]))]
+@[to_additive (attr := aesop 90%)]
+theorem mk (h : ∀ x ∈ M, x⁻¹ ∈ M → x = 1) : M.IsMulPointed := h -- for Aesop
+
+@[to_additive (attr := aesop safe forward (immediate := [hM, hx₁]))]
+theorem eq_one_of_mem_of_inv_mem (hM : M.IsMulPointed)
+    {x : G} (hx₁ : x ∈ M) (hx₂ : x⁻¹ ∈ M) : x = 1 := hM _ hx₁ hx₂
+
+@[to_additive (attr := aesop safe forward (immediate := [hM, hx₂]))]
 alias eq_one_of_mem_of_inv_mem₂ := eq_one_of_mem_of_inv_mem -- for Aesop
 
-@[to_additive (attr := simp)]
-theorem mulSupport_eq_bot [M.IsMulPointed] : M.mulSupport = ⊥ := by aesop
-
-variable {M} in
 @[to_additive]
-theorem IsMulPointed.of_mulSupport_eq_bot (h : M.mulSupport = ⊥) : M.IsMulPointed where
-  eq_one_of_mem_of_inv_mem {x} := by
+theorem _root_.isMulPointed_iff_mulSupport_eq_bot : M.IsMulPointed ↔ M.mulSupport = ⊥ where
+  mp := by aesop
+  mpr h := fun x ↦ by
     apply_fun (x ∈ ·) at h
     aesop
 
-@[to_additive]
-theorem isMulPointed_iff : M.IsMulPointed ↔ M.mulSupport = ⊥ where
-  mp _ := mulSupport_eq_bot _
-  mpr := IsMulPointed.of_mulSupport_eq_bot
-
-/-- Typeclass for submonoids `M` of a group `G` such that `M` generates `G` as a subgroup. -/
-@[to_additive]
-class IsMulSpanning {G : Type*} [Group G] (M : Submonoid G) : Prop where
-  mem_or_inv_mem (M) (a : G) : a ∈ M ∨ a⁻¹ ∈ M := by aesop
-
-export IsMulSpanning (mem_or_inv_mem)
-
-attribute [aesop safe forward, aesop safe apply] mem_or_inv_mem
-
-variable {M} in
-@[to_additive (attr := aesop 80%)]
-theorem inv_mem_of_notMem [M.IsMulSpanning] (x : G) (h : x ∉ M) : x⁻¹ ∈ M := by aesop
-
-variable {M} in
-@[to_additive (attr := aesop 50%)]
-theorem mem_of_inv_notMem [M.IsMulSpanning] (x : G) (h : x⁻¹ ∉ M) : x ∈ M := by aesop
-
-variable {M} in
-@[to_additive]
-theorem IsMulSpanning.of_le {N : Submonoid G} [M.IsMulSpanning] (h : M ≤ N) :
-    N.IsMulSpanning where
+@[to_additive (attr := simp)]
+alias ⟨mulSupport_eq_bot, _⟩ := isMulPointed_iff_mulSupport_eq_bot
 
 @[to_additive]
-theorem IsMulSpanning.maximal_isMulPointed [M.IsMulPointed] [M.IsMulSpanning] :
+alias ⟨_, of_mulSupport_eq_bot⟩ := isMulPointed_iff_mulSupport_eq_bot
+
+end IsMulPointed
+
+variable (M) in
+/-- A submonoid `M` of a group `G` is spanning if `M` generates `G` as a subgroup. -/
+@[to_additive IsSpanning
+/-- A submonoid `M` of a group `G` is spanning if `M` generates `G` as a subgroup. -/]
+def IsMulSpanning := ∀ a : G, a ∈ M ∨ a⁻¹ ∈ M
+
+namespace IsMulSpanning
+
+@[to_additive (attr := aesop 90%)]
+theorem mk (h : ∀ a : G, a ∈ M ∨ a⁻¹ ∈ M) : M.IsMulSpanning := h -- for Aesop
+
+@[to_additive (attr := aesop safe forward)]
+theorem mem_or_inv_mem (hM : M.IsMulSpanning) (a : G) : a ∈ M ∨ a⁻¹ ∈ M := by aesop
+
+@[to_additive]
+theorem of_le {N : Submonoid G} (hM : M.IsMulSpanning) (h : M ≤ N) :
+    N.IsMulSpanning := by aesop
+
+@[to_additive maximal_isPointed]
+theorem maximal_isMulPointed (hMp : M.IsMulPointed) (hMs : M.IsMulSpanning) :
     Maximal IsMulPointed M :=
-  ⟨inferInstance, fun N hN h ↦ by rw [SetLike.le_def] at h ⊢; aesop⟩
+  ⟨hMp, fun N hN h ↦ by rw [SetLike.le_def] at h ⊢; aesop⟩
 
-end Submonoid
+end Submonoid.IsMulSpanning
