a1f5005ff372cdfc184b95476702f4336981f7fe
Monica Omar <2497250a@research.gla.ac.uk>
2026-03-06 05:26:44 +0000

    will add anatole's proof after their pr

--- Mathlib/Analysis/Convex/Strict/Extreme.lean
+++ Mathlib/Analysis/Convex/Strict/Extreme.lean
@@ -12,7 +12,7 @@ public import Mathlib.Analysis.Convex.StrictConvexSpace
 
 This file collects some results of extreme points of sets in strictly convex spaces.
 In particular, we show that in a nontrivial strictly convex space,
-the sphere is contained in the extreme points of the closed ball. -/
+the sphere is equal to the extreme points of the closed ball. -/
 
 public section
 
@@ -40,39 +40,35 @@ lemma StrictConvexSpace.sphere_subset_extremePoints_closedBall' [Nontrivial A] {
   rw [← frontier_closedBall', frontier, closure_closedBall] at hx
   exact (_root_.strictConvex_closedBall ℝ _ _).mem_extremePoints_of_mem_sdiff_interior hx
 
-theorem not_mem_extremePoints_of_mem_interior {E : Type*} [AddCommGroup E] [Module ℝ E]
+theorem disjoint_interior_extremePoints {E : Type*} [AddCommGroup E] [Module ℝ E]
     [TopologicalSpace E] [ContinuousAdd E] [ContinuousSMul ℝ E] [Nontrivial E]
-    {S : Set E} {x : E} (hx : x ∈ interior S) : x ∉ extremePoints ℝ S := fun h ↦ by
+    (S : Set E) : Disjoint (interior S) (extremePoints ℝ S) := by
+  refine Set.disjoint_iff.mpr fun x ⟨x_int, x_ext⟩ ↦ ?_
   obtain ⟨v, hv⟩ := exists_ne (0 : E)
   let f (t : ℝ) : E := x + t • v
   obtain ⟨ε, hε, hεb⟩ : ∃ ε > 0, f '' ball 0 ε ⊆ interior S := by
     simp_rw [image_subset_iff]
     refine Metric.mem_nhds_iff.mp <| (by fun_prop : Continuous f).continuousAt ?_
     simp only [zero_smul, add_zero, interior_mem_nhds, f]
-    exact mem_interior_iff_mem_nhds.mp hx
+    exact mem_interior_iff_mem_nhds.mp x_int
   have : f (ε / 2) ≠ f (-ε / 2) := by
     simp only [ne_eq, neg_div, neg_smul, add_right_inj, f]
     rw [← sub_eq_zero, sub_neg_eq_add, ← two_smul ℝ]
     simp [hv, hε.ne']
-  have := mem_extremePoints.mp h |>.2 (f (ε / 2)) (interior_subset (hεb (by simp; grind)))
+  have := mem_extremePoints.mp x_ext |>.2 (f (ε / 2)) (interior_subset (hεb (by simp; grind)))
     (f (-ε / 2)) (interior_subset (hεb (by simp; grind))) ⟨2⁻¹, 2⁻¹, ?_⟩
   · grind
   · simp [← one_div, ← smul_add, f, neg_div, add_assoc, ← two_smul ℝ, smul_smul, mul_one_div_cancel]
 
-/-- In a nontrivial normed space, the extreme points of the closed unit ball is contained in
-the unit sphere. -/
-theorem extremePoints_closedUnitBall_subset_unitSphere [Nontrivial A] :
-    extremePoints ℝ (closedBall (0 : A) 1) ⊆ sphere 0 1 := fun x hx ↦ by
-  have h : x ≠ 0 := fun h ↦
-    not_mem_extremePoints_of_mem_interior (by simp [h, interior_closedBall]) hx
-  simp only [mem_extremePoints, mem_closedBall, dist_zero_right] at hx
-  by_contra!
-  refine h (@hx.2 (‖x‖⁻¹ • x) ?_ 0 (by simp) ⟨‖x‖, 1 - ‖x‖, by simp_all, ?_, ?_⟩).2.symm
-  on_goal 2 => grind [mem_sphere_zero_iff_norm]
-  all_goals simp [norm_smul, norm_ne_zero_iff.mpr h]
+/-- In a nontrivial normed space, the extreme points of the closed ball is contained in
+the sphere. -/
+theorem extremePoints_closedBall_subset_sphere [Nontrivial A] {r : ℝ} {x : A} :
+    extremePoints ℝ (closedBall x r) ⊆ sphere x r := by
+  rw [← closedBall_diff_ball, subset_diff, ← interior_closedBall' _]
+  exact ⟨extremePoints_subset, disjoint_interior_extremePoints _ |>.symm⟩
 
-theorem StrictConvexSpace.extremePoints_closedUnitBall_eq_unitSphere [Nontrivial A]
-    [StrictConvexSpace ℝ A] : extremePoints ℝ (closedBall (0 : A) 1) = sphere 0 1 :=
-  antisymm extremePoints_closedUnitBall_subset_unitSphere sphere_subset_extremePoints_closedBall'
+theorem StrictConvexSpace.extremePoints_closedBall_eq_sphere [Nontrivial A] {r : ℝ} {x : A}
+    [StrictConvexSpace ℝ A] : extremePoints ℝ (closedBall x r) = sphere x r :=
+  antisymm extremePoints_closedBall_subset_sphere sphere_subset_extremePoints_closedBall'
 
 end Normed
