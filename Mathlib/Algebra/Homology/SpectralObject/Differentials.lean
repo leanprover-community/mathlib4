8286beb1ad13571fa63bb66e995ce94acd11918c
Joël Riou <37772949+joelriou@users.noreply.github.com>
2026-03-06 22:09:26 +0100

    Update Mathlib/Algebra/Homology/SpectralObject/Differentials.lean
    Co-authored-by: smorel394 <67864981+smorel394@users.noreply.github.com>

--- Mathlib/Algebra/Homology/SpectralObject/Differentials.lean
+++ Mathlib/Algebra/Homology/SpectralObject/Differentials.lean
@@ -187,8 +187,7 @@ lemma Ψ_opcyclesMap_exact (hn₁ : n₀ + 1 = n₁ := by lia) :
   obtain ⟨A₂, π₂, _, z₂, hz₂⟩ :=
       (X.cokernelSequenceOpcycles_exact f₁ f₂₃ n₀ n₁ hn₁).exact_up_to_refinements z₁ (by
     dsimp
-    have H := X.p_opcyclesMap f₁ f₂ f₁ f₂₃
-      (threeδ₃Toδ₂ f₁ f₂ f₃ f₂₃ h₂₃) (𝟙 _) n₁ (by cat_disch)
+    have H := X.p_opcyclesMap f₁ f₂ f₁ f₂₃ (threeδ₃Toδ₂ f₁ f₂ f₃ f₂₃ h₂₃) (𝟙 _) n₁ (by cat_disch)
     rw [Functor.map_id, id_comp] at H
     rw [← H, ← reassoc_of% hz₁, hz₀, comp_zero])
   refine ⟨A₂, π₂ ≫ π₁, inferInstance, z₂ ≫ X.toCycles f₂ f₃ f₂₃ h₂₃ n₀, ?_⟩
