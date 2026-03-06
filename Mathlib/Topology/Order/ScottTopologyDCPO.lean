48d5ad173a0ce4185ae96856fd9f56bd94b619fd
Edwin Fernando <ef322@ic.ac.uk>
2026-03-06 14:50:18 +0000

    refactor: introduce an sSup based open_of_open_eq_basis

--- Mathlib/Topology/Order/ScottTopologyDCPO.lean
+++ Mathlib/Topology/Order/ScottTopologyDCPO.lean
@@ -12,14 +12,17 @@ public import Mathlib.Topology.Sets.Opens
 /-!
 # Scott Complete Partial Order
 
-Properties of Scott topologies over (Directed) `CompletePartialOrder`, and Algebraic DCPOs.
+Properties of Scott topologies over Pointed (Directed) `CompletePartialOrder`, and Algebraic DCPOs.
 
 ## Main Definitions
 - `Compact`: The element x is compact if for any directed subset A ⊆ D, whenever `x ≤ sSup A`,
   there is some a ∈ A s.t. x ≤ a already.
-- `AlgebraicDCPO`: an extension of `CompletePartialOrder` which is additionally "algebraic".
+- `AlgebraicDCPO`: an extension of pointed `CompletePartialOrder` which is additionally "algebraic".
+  Pointed is just `OrderBot`.
   Algebraicity has a correspondence with the 'inaccessibility of directed joins'
   property of Scott topologies, which can be seen in some of the proofs below.
+- `Sober`: An alternative equiavlent definition of a Sober topological space as one where
+  the unit map of the adjunction to Locales, `adjunctionTopToLocalePT`, is a homeomorphism.
 
 ## Main Statements
 - `specialization_iff_ge`: The original order and the specialization order induced by the Scott
@@ -28,6 +31,8 @@ Properties of Scott topologies over (Directed) `CompletePartialOrder`, and Algeb
   basis under the Scott Topology. Prop 3.5.2 in [reneta2024]
 - `open_eq_of_open_basis'`: Explicit construction an Open set from the basis mentioned above.
   Prop 2.3.6(2) [abramsky_gabbay_maibaum_1994]
+- `scott_is_sober`: Main result: Scott topologies over algebraic DCPOs are Sober.
+  Prop 3.5.3 in [reneta2024]. Prop 2.27 in [abramsky_gabbay_maibaum_1994]
 
 ## Notation
 * `e` `c` : elements in the a set, usually of an open set, often compact.
@@ -75,7 +80,7 @@ def CompactSet (α : Type*) [CompletePartialOrder α] := {x : α | IsCompactElem
 def CompactLowerSet (x : α) := Set.Iic x ∩ CompactSet α
 
 /-- Encodes notion of observable properties in programs (points are program semantics) -/
-class AlgebraicDCPO (α : Type*) extends CompletePartialOrder α where
+class AlgebraicDCPO (α : Type*) extends CompletePartialOrder α, OrderBot α where
   algebraic : ∀ x : α, (CompactLowerSet x).Nonempty ∧ DirectedOn (· ≤ ·) (CompactLowerSet x) ∧
     x = sSup (CompactLowerSet x)
 
@@ -162,9 +167,17 @@ lemma isOpen_of_basis {u : Set α} (hu : u ∈ Ici '' CompactSet α) : IsOpen u 
     · exact a_in_d
     · exact a_in_u
 
+/-- Subtype for compact elements -/
+abbrev CompactElement (α : Type*) [PartialOrder α] := {x : α // IsCompactElement x}
+
 /-- The upwards closure of a compact point which we know is open -/
-def IsCompactElement.toOpen {c : α} (hc : IsCompactElement c) : Opens α :=
-  ⟨Ici c, isOpen_of_basis <| Set.mem_image_of_mem Ici hc⟩
+abbrev Subtype.toOpen (c : CompactElement α) : Opens α :=
+  ⟨Ici c, isOpen_of_basis <| Set.mem_image_of_mem Ici c.prop⟩
+
+/-- The upwards closure of a compact point which we know is open.
+In this version the data is implicit -/
+abbrev IsCompactElement.toOpen {c : α} (hc : IsCompactElement c) : Opens α :=
+  (⟨c, hc⟩ : CompactElement α).toOpen
 
 /-- A helper. This can be made more straightforward by replacing `{u : Opens α}` with
 `{u : UpperSet}` and applying the additional lemma `Topology.IsScott.isUpperSet_of_isOpen`
@@ -245,27 +258,37 @@ lemma open_eq_open_of_basis (u : Set D) (hu : IsOpen u) :
     The weaker version is still useful as it is easier to use when sufficient.
     We don't reuse the previous result to prove this, since the proof turns out just as long -/
 lemma open_eq_open_of_basis' (u : Opens D) :
-    -- u = sSup ({ o | ∃ (c : D) (hc : c ∈ 𝕂 D), c ∈ u ∧ (o = ⟨c, hc⟩ᵘᵒ) }) := by
-    -- u = iSup (fun (c : {c : D // Compact c ∧ c ∈ u}) ↦  ⟨c.1, c.2.1⟩ᵘᵒ ) := by
-    u = ⨆ (c : D) (hc : IsCompactElement c) (_ : c ∈ u), hc.toOpen := by
+    u = ⨆ c : {c | IsCompactElement c ∧ c ∈ u}, IsCompactElement.toOpen c.2.1 := by
   ext e
   simp only [SetLike.mem_coe]
   constructor
   · intro e_in_u
     choose c hc₀ e_in_c' hc'₁ using exists_basis_mem_basis e u e_in_u u.isOpen
-    -- simp only [Opens.mem_iSup]
-    simp only [iSup_mk, carrier_eq_coe, mem_mk, mem_iUnion, SetLike.mem_coe, exists_prop,
-      exists_and_left]
-    use c
-    use Opens.mem_iff_Ici_subset.2 hc'₁
-    use hc₀
-    exact e_in_c'
+    simp only [coe_setOf, IsCompactElement.toOpen, mem_setOf_eq, iSup_mk, mem_mk, mem_iUnion,
+      mem_Ici, Subtype.exists, exists_prop]
+    exact ⟨c, ⟨hc₀, mem_iff_Ici_subset.2 hc'₁⟩, mem_Ici.1 e_in_c'⟩
   · rintro he
-    simp only [iSup_mk, carrier_eq_coe, mem_mk, mem_iUnion, SetLike.mem_coe, exists_prop,
-      exists_and_left] at he
-    obtain ⟨c, c_in_u, hc₀, he⟩ := he
+    simp only [coe_setOf, IsCompactElement.toOpen, mem_setOf_eq, iSup_mk, mem_mk, mem_iUnion,
+      mem_Ici, Subtype.exists, exists_prop] at he
+    obtain ⟨c, ⟨hc₀, c_in_u⟩, he⟩ := he
     rw [mem_iff_Ici_subset] at c_in_u
     apply Set.mem_of_mem_of_subset he c_in_u
+
+-- TODO: note to reviewers. the sSup formulation is the only result used later on
+-- The `iSup` version above can be deleted and the below proof restructured (which was
+-- indeed the original structure). Is it helpful to leave multiple versions of this statement here?
+
+/-- Version `of open_eq_open_of_basis'_sSup` using `sSup` rather than `iSup` -/
+lemma open_eq_open_of_basis'_sSup (u : Opens D) :
+    u = sSup ({ o | ∃ (c : D) (hc : IsCompactElement c), c ∈ u ∧ o = hc.toOpen }) := by
+  ext e
+  simp only [SetLike.mem_coe, Opens.mem_sSup, Set.mem_setOf_eq]
+  constructor
+  · intro e_in_u
+    choose c hc₀ e_in_c' hc'₁ using exists_basis_mem_basis e u e_in_u u.isOpen
+    exact ⟨hc₀.toOpen, ⟨c, hc₀, hc'₁ (Set.mem_Ici.2 le_rfl), rfl⟩, e_in_c'⟩
+  · rintro ⟨_, ⟨c, hc, c_in_u, rfl⟩, e_in_o⟩
+    exact (mem_iff_Ici_subset.mp c_in_u) e_in_o
 
 end AlgebraicDCPO
 end IsScott
