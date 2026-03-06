78c335482b8d675ce486fcb565335166399c0771
Rémy Degenne <4094732+RemyDegenne@users.noreply.github.com>
2026-03-06 15:35:15 +0000

    feat: Radon-Nikodym derivative of a map is a conditional expectation (#35089)
    The Radon-Nikodym derivative of the pushforward of two measures is written as a conditional expectation.
    This will be useful to prove the data processing inequality for the Kullback-Leibler divergence (or more generally f-divergences).
    
    Co-authored-by: Remy Degenne <remydegenne@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -5089,7 +5089,9 @@ public import Mathlib.MeasureTheory.Function.ConditionalExpectation.Basic
 public import Mathlib.MeasureTheory.Function.ConditionalExpectation.CondexpL1
 public import Mathlib.MeasureTheory.Function.ConditionalExpectation.CondexpL2
 public import Mathlib.MeasureTheory.Function.ConditionalExpectation.Indicator
+public import Mathlib.MeasureTheory.Function.ConditionalExpectation.LebesgueBochner
 public import Mathlib.MeasureTheory.Function.ConditionalExpectation.PullOut
+public import Mathlib.MeasureTheory.Function.ConditionalExpectation.RadonNikodym
 public import Mathlib.MeasureTheory.Function.ConditionalExpectation.Real
 public import Mathlib.MeasureTheory.Function.ConditionalExpectation.Unique
 public import Mathlib.MeasureTheory.Function.ConditionalLExpectation
