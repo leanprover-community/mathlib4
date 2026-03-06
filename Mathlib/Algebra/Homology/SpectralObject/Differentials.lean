cb589c5931f23dd3ac8ed38274c8743bcfb158da
Joël Riou <37772949+joelriou@users.noreply.github.com>
2026-03-06 11:20:57 +0000

    Update Mathlib/Algebra/Homology/SpectralObject/Differentials.lean
    Co-authored-by: Robin Carlier <57142648+robin-carlier@users.noreply.github.com>

--- Mathlib/Algebra/Homology/SpectralObject/Differentials.lean
+++ Mathlib/Algebra/Homology/SpectralObject/Differentials.lean
@@ -171,11 +171,10 @@ lemma cyclesMap_Ψ_exact (hn₁ : n₀ + 1 = n₁ := by lia) :
     (ShortComplex.mk _ _ (X.cyclesMap_Ψ f₁ f₂ f₃ f₁₂ h₁₂ f₂₃ h₂₃ n₀ n₁ hn₁)).Exact := by
   rw [ShortComplex.exact_iff_exact_up_to_refinements]
   intro A z hz
-  refine ⟨A, 𝟙 _, inferInstance, X.liftCycles f₁₂ f₃ n₀ n₁ hn₁ (z ≫ X.iCycles f₂ f₃ n₀) ?_, ?_⟩
-  · dsimp
-    rw [assoc, ← X.Ψ_fromOpcycles f₁ f₂ f₃ f₁₂ h₁₂ n₀ n₁ hn₁, reassoc_of% hz, zero_comp]
-  · dsimp
-    rw [← cancel_mono (X.iCycles f₂ f₃ n₀), id_comp, assoc,
+  refine ⟨A, 𝟙 _, inferInstance,
+    X.liftCycles f₁₂ f₃ n₀ n₁ hn₁ (z ≫ X.iCycles f₂ f₃ n₀) ?_, ?_⟩ <;> dsimp
+  · rw [assoc, ← X.Ψ_fromOpcycles f₁ f₂ f₃ f₁₂ h₁₂ n₀ n₁ hn₁, reassoc_of% hz, zero_comp]
+  · rw [← cancel_mono (X.iCycles f₂ f₃ n₀), id_comp, assoc,
       X.cyclesMap_i _ _ _ _ (threeδ₁Toδ₀ f₁ f₂ f₃ f₁₂ h₁₂) (𝟙 _) n₀ (by cat_disch) ,
       Functor.map_id, comp_id, liftCycles_i]
 
