3ade055a51cedb714705847984e4c2e829be54db
Rémy Degenne <remydegenne@gmail.com>
2026-03-06 11:34:33 +0100

    Update Mathlib/Probability/Process/Stopping.lean
    Co-authored-by: Etienne Marion <66847262+EtienneC30@users.noreply.github.com>

--- Mathlib/Probability/Process/Stopping.lean
+++ Mathlib/Probability/Process/Stopping.lean
@@ -376,7 +376,7 @@ protected lemma biInf [ConditionallyCompleteLinearOrderBot ι] [TopologicalSpace
     [OrderTopology ι] [DenselyOrdered ι] [FirstCountableTopology ι] [NoMaxOrder ι]
     {κ : Type*} {f : Filtration ι m} {τ : κ → Ω → WithTop ι} {s : Set κ} (hs : s.Countable)
     [f.IsRightContinuous] (hτ : ∀ n ∈ s, IsStoppingTime f (τ n)) :
-    IsStoppingTime f (fun ω ↦ ⨅ (n) (_ : n ∈ s), τ n ω) := by
+    IsStoppingTime f (fun ω ↦ ⨅ n ∈ s, τ n ω) := by
   refine isStoppingTime_of_measurableSet_lt_of_isRightContinuous <|
     fun i ↦ MeasurableSet.of_compl ?_
   rw [(_ : {ω | ⨅ n ∈ s, τ n ω < i}ᶜ = ⋂ n ∈ s, {ω | i ≤ τ n ω})]
