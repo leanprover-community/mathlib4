8bfb83bccfccd5086e1679bfb6934fe623a0e485
Salvatore Mercuri <salvatore.mercuri@imperial.ac.uk>
2026-03-06 22:01:43 +0000

    Set.BijOn.ncard_eq

--- Mathlib/Data/Set/Card.lean
+++ Mathlib/Data/Set/Card.lean
@@ -889,6 +889,9 @@ theorem ncard_range_of_injective (hf : Function.Injective f) :
     (range f).ncard = Nat.card α := by
   rw [← image_univ, ncard_image_of_injective univ hf, ncard_univ]
 
+theorem BijOn.ncard_eq {t : Set β} (h : Set.BijOn f s t) : s.ncard = t.ncard :=
+  ncard_congr _ h.mapsTo (fun _ _ ha hb heq ↦ h.injOn ha hb heq) fun _ ha ↦ bex_def.2 (h.surjOn ha)
+
 /-- A version of the pigeonhole principle for `Set`s rather than `Finset`s.
 
 See also `Finset.exists_ne_map_eq_of_card_lt_of_maps_to` and
