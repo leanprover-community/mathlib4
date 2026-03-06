5d9a9b4cab1edf30b119ffe3c741a7c4b0237604
Salvatore Mercuri <salvatore.mercuri@imperial.ac.uk>
2026-03-06 11:34:01 +0000

    fix test

--- MathlibTest/instance_diamonds/Analysis/Normed/Field/WithAbs.lean
+++ MathlibTest/instance_diamonds/Analysis/Normed/Field/WithAbs.lean
@@ -1,5 +1,5 @@
 import Mathlib
 
 example {R : Type*} [Field R] (v : AbsoluteValue R ℝ) :
-    (normedField v).toNormedCommRing.toNormedRing = normedRing v := by
+    (WithAbs.normedField v).toNormedCommRing.toNormedRing = WithAbs.normedRing v := by
   with_reducible_and_instances rfl
