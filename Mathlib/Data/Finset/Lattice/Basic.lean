d2d703d7b6e4f27c2bd8856fb06e68abdfe705ac
JadAbouHawili <jad-abou-hawili@hotmail.com>
2026-03-08 23:04:56 +0200

    Rename Finset API instances for clarity
    typo fixes

--- Mathlib/Data/Finset/Lattice/Basic.lean
+++ Mathlib/Data/Finset/Lattice/Basic.lean
@@ -22,16 +22,16 @@ In Lean, we use lattice notation to talk about things involving unions and inter
 `Mathlib/Order/Lattice.lean`. For the lattice structure on finsets, `⊥` is called `bot` with
 `⊥ = ∅` and `⊤` is called `top` with `⊤ = univ`.
 
-* `Finset.instHasSubsetFinset`: Lots of API about lattices, otherwise behaves as one would expect.
-* `Finset.instUnionFinset`: Defines `s ∪ t` (or `s ⊔ t`) as the union of `s` and `t`.
+* `Finset.instHasSubset`: Lots of API about lattices, otherwise behaves as one would expect.
+* `Finset.instUnion`: Defines `s ∪ t` (or `s ⊔ t`) as the union of `s` and `t`.
   See `Finset.sup`/`Finset.biUnion` for finite unions.
-* `Finset.instInterFinset`: Defines `s ∩ t` (or `s ⊓ t`) as the intersection of `s` and `t`.
+* `Finset.instInter`: Defines `s ∩ t` (or `s ⊓ t`) as the intersection of `s` and `t`.
   See `Finset.inf` for finite intersections.
 
 ### Operations on two or more finsets
 
-* `Finset.instUnionFinset`: see "The lattice structure on subsets of finsets"
-* `Finset.instInterFinset`: see "The lattice structure on subsets of finsets"
+* `Finset.instUnion`: see "The lattice structure on subsets of finsets"
+* `Finset.instInter`: see "The lattice structure on subsets of finsets"
 
 ## Tags
 
