8c79476d7073dcba3206a7aab9fe6711fca2ee16
Weiyi Wang <wwylele@gmail.com>
2026-03-08 16:28:43 -0400

    generalize VAddInvariantMeasure a bit

--- Mathlib/Geometry/Euclidean/Volume/Measure.lean
+++ Mathlib/Geometry/Euclidean/Volume/Measure.lean
@@ -169,10 +169,11 @@ theorem AffineSubspace.euclideanHausdorffMeasure_coe_image (d : ℕ) (s : Affine
 ### `μHE[d]` is translation invariant
 -/
 
-instance (d : ℕ) : VAddInvariantMeasure V P μHE[d] where
+instance {α : Type*} [AddGroup α] [AddTorsor α X] [IsIsometricVAdd α X] (d : ℕ) :
+    VAddInvariantMeasure α X μHE[d] where
   measure_preimage_vadd c s hs := by
     simp_rw [euclideanHausdorffMeasure_def, smul_apply, nnreal_smul_coe_apply]
-    have h : (0 : ℝ) ≤ d ∨ Function.Surjective fun (x : P) => -c +ᵥ x := by simp
+    have h : (0 : ℝ) ≤ d ∨ Function.Surjective fun (x : X) => -c +ᵥ x := by simp
     convert congr(volume.addHaarScalarFactor μH[d] * $(hausdorffMeasure_vadd (-c) h s))
     ext y
     simp [Set.mem_neg_vadd_set_iff]
