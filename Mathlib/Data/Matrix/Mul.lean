b40456857273e8595428941faf0a43c1b33e96da
Snir Broshi <26556598+SnirBroshi@users.noreply.github.com>
2026-03-08 04:42:20 +0200

    chore(Data/Matrix/Mul): fix references in docstrings

--- Mathlib/Data/Matrix/Mul.lean
+++ Mathlib/Data/Matrix/Mul.lean
@@ -691,7 +691,8 @@ variable [NonUnitalNonAssocSemiring α]
 /--
 `M *ᵥ v` (notation for `mulVec M v`) is the matrix-vector product of matrix `M` and vector `v`,
 where `v` is seen as a column vector.
-Put another way, `M *ᵥ v` is the vector whose entries are those of `M * col v` (see `col_mulVec`).
+Put another way, `M *ᵥ v` is the vector whose entries are those of `M * replicateCol ι v`
+(see `replicateCol_mulVec`).
 
 The notation has precedence 73, which comes immediately before ` ⬝ᵥ ` for `dotProduct`,
 so that `A *ᵥ v ⬝ᵥ B *ᵥ w` is parsed as `(A *ᵥ v) ⬝ᵥ (B *ᵥ w)`.
@@ -705,7 +706,8 @@ scoped infixr:73 " *ᵥ " => Matrix.mulVec
 /--
 `v ᵥ* M` (notation for `vecMul v M`) is the vector-matrix product of vector `v` and matrix `M`,
 where `v` is seen as a row vector.
-Put another way, `v ᵥ* M` is the vector whose entries are those of `row v * M` (see `row_vecMul`).
+Put another way, `v ᵥ* M` is the vector whose entries are those of `replicateRow ι v * M`
+(see `replicateRow_vecMul`).
 
 The notation has precedence 73, which comes immediately before ` ⬝ᵥ ` for `dotProduct`,
 so that `v ᵥ* A ⬝ᵥ w ᵥ* B` is parsed as `(v ᵥ* A) ⬝ᵥ (w ᵥ* B)`.
