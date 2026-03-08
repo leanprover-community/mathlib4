f73614c66993aef58ca7b66985e4c905453816da
Monica Omar <2497250a@research.gla.ac.uk>
2026-03-08 10:50:14 +0000

    isIdempotentElem_iff

--- Mathlib/LinearAlgebra/Matrix/WithConv.lean
+++ Mathlib/LinearAlgebra/Matrix/WithConv.lean
@@ -102,8 +102,7 @@ instance [CommSemiring β] [Semiring α] [Algebra β α] : Algebra β (WithConv 
 theorem Matrix.WithConv.IsIdempotentElem.isSelfAdjoint [Semiring α] [IsLeftCancelMulZero α]
     [StarRing α] {f : WithConv (Matrix m n α)} (hf : IsIdempotentElem f) : IsSelfAdjoint f := by
   simp_rw [IsIdempotentElem, WithConv.ext_iff, ← Matrix.ext_iff, convMul_def, hadamard_apply,
-    fun α ↦ show α * α = α ↔ IsIdempotentElem α from Iff.rfl,
-    IsIdempotentElem.iff_eq_zero_or_one] at hf
+    ← isIdempotentElem_iff, IsIdempotentElem.iff_eq_zero_or_one] at hf
   rw [IsSelfAdjoint, WithConv.ext_iff]
   ext i j
   obtain (h | h) := hf i j <;> simp_all
