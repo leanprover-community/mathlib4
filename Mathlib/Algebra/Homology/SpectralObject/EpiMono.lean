ed0b216c933474d83b1a9b9f30163104714bb473
Joël Riou <37772949+joelriou@users.noreply.github.com>
2026-03-08 11:52:32 +0100

    Update Mathlib/Algebra/Homology/SpectralObject/EpiMono.lean
    Co-authored-by: smorel394 <67864981+smorel394@users.noreply.github.com>

--- Mathlib/Algebra/Homology/SpectralObject/EpiMono.lean
+++ Mathlib/Algebra/Homology/SpectralObject/EpiMono.lean
@@ -45,10 +45,9 @@ lemma epi_map (α : mk₃ f₁ f₂ f₃ ⟶ mk₃ f₁ f₂ f₃') (n₀ n₁ n
     (hα₀ : α.app 0 = 𝟙 _ := by cat_disch) (hα₁ : α.app 1 = 𝟙 _ := by cat_disch)
     (hα₂ : α.app 2 = 𝟙 _ := by cat_disch)
     (hn₁ : n₀ + 1 = n₁ := by lia) (hn₂ : n₁ + 1 = n₂ := by lia) (hn₃ : n₂ + 1 = n₃ := by lia) :
-    Epi (X.map f₁ f₂ f₃ f₁ f₂ f₃' α n₀ n₁ n₂ hn₁ hn₂) := by
-  have := X.πE_map _ _ _ _ _ _ α (𝟙 _) n₀ n₁ n₂
-  rw [cyclesMap_id, id_comp] at this
-  exact epi_of_epi_fac this
+    Epi (X.map f₁ f₂ f₃ f₁ f₂ f₃' α n₀ n₁ n₂ hn₁ hn₂) :=
+  have : Epi (X.cyclesMap f₁ f₂ f₁ f₂ (𝟙 (mk₂ f₁ f₂)) n₁) := by rw [X.cyclesMap_id]; infer_instance
+  epi_of_epi_fac (X.πE_map _ _ _ _ _ _ α (𝟙 _) n₀ n₁ n₂ (by cat_disch) _ _)
 
 lemma mono_map (α : mk₃ f₁ f₂ f₃ ⟶ mk₃ f₁' f₂ f₃) (n₀ n₁ n₂ n₃ : ℤ)
     (hα₁ : α.app 1 = 𝟙 _ := by cat_disch) (hα₂ : α.app 2 = 𝟙 _ := by cat_disch)
