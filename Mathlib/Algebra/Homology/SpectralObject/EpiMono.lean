cee95f11e586eb9a8c027904157ff1b2cf2e411d
Joël Riou <37772949+joelriou@users.noreply.github.com>
2026-03-08 18:42:26 +0100

    Update Mathlib/Algebra/Homology/SpectralObject/EpiMono.lean
    Co-authored-by: Robin Carlier <57142648+robin-carlier@users.noreply.github.com>

--- Mathlib/Algebra/Homology/SpectralObject/EpiMono.lean
+++ Mathlib/Algebra/Homology/SpectralObject/EpiMono.lean
@@ -160,7 +160,7 @@ lemma mapFourδ₄Toδ₃'_comp (n₀ n₁ n₂ : ℤ)
     (hn₁ : n₀ + 1 = n₁ := by lia) (hn₂ : n₁ + 1 = n₂ := by lia) :
     X'.mapFourδ₄Toδ₃' i₀ i₁ i₂ i₃ i₄ hi₀₁ hi₁₂ hi₂₃ hi₃₄ n₀ n₁ n₂ hn₁ hn₂ ≫
       X'.mapFourδ₄Toδ₃' i₀ i₁ i₂ i₄ i₅ hi₀₁ hi₁₂ (hi₂₃.trans hi₃₄) hi₄₅ n₀ n₁ n₂ hn₁ hn₂ =
-    X'.mapFourδ₄Toδ₃' i₀ i₁ i₂ i₃ i₅ hi₀₁ hi₁₂ hi₂₃ (hi₃₄.trans hi₄₅) n₀ n₁ n₂ hn₁ hn₂ := by
+    X'.mapFourδ₄Toδ₃' i₀ i₁ i₂ i₃ i₅ hi₀₁ hi₁₂ hi₂₃ (hi₃₄.trans hi₄₅) n₀ n₁ n₂ hn₁ hn₂ :=
   (X'.map_comp (hn₁ := hn₁) (hn₂ := hn₂) ..).symm
 
 @[reassoc]
