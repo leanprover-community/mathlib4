bacbe35e1451f4243564861028ad4152cfbdfce8
Kexing Ying <kexing.ying@epfl.ch>
2026-03-09 14:36:26 +0100

    module

--- Mathlib/Probability/Process/LocalProperty.lean
+++ Mathlib/Probability/Process/LocalProperty.lean
@@ -3,7 +3,9 @@ Copyright (c) 2025 Rémy Degenne. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Rémy Degenne, Kexing Ying
 -/
-import Mathlib.Probability.Process.Stopping
+module
+
+public import Mathlib.Probability.Process.Stopping
 
 /-! # Local properties of processes
 
