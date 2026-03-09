cf030531197139709748a7e4c501716caa717565
Etienne Marion <baguettes_casino0c@icloud.com>
2026-03-09 19:08:42 +0100

    remove linter

--- Mathlib/Probability/Distributions/Poisson/PoissonLimitThm.lean
+++ Mathlib/Probability/Distributions/Poisson/PoissonLimitThm.lean
@@ -86,7 +86,6 @@ theorem tendsto_choose_mul_pow_of_tendsto_mul_atTop (hr : Tendsto (fun n => n * 
   refine Tendsto.inv₀ (.pow ?_ k) (by simp)
   · simpa using tendsto_const_nhds.sub (tendsto_zero_of_tendsto_mul_atTop hr)
 
-set_option linter.deprecated false in
 /--
 Another version of Poisson Limit Theorem: convergence of `PMF.binomial` to `poissonPMF` in `ℝ≥0∞`
 under the natural hypotheses (`∀ n, p n ≤ 1` and `r ≥ 0`).
@@ -95,11 +94,10 @@ under the natural hypotheses (`∀ n, p n ≤ 1` and `r ≥ 0`).
 lemma binomial_tendsto_poissonPMFReal_atTop {r : ℝ≥0} {p : ℕ → ℝ≥0} (h : ∀ n, p n ≤ 1)
     (hr : Tendsto (fun n => n * p n) atTop (𝓝 r)) :
     Tendsto (fun n ↦ PMF.binomial (p n) (h n) n (Fin.ofNat (n + 1) k))
-    atTop (𝓝 (poissonPMF r k)) := by
+    atTop (𝓝 (.ofReal (Real.exp (-r) * (r ^ k) / k.factorial))) := by
   have t1 : Tendsto (fun n => (ENNReal.ofReal (n.choose k * (p n) ^ k * (1 - p n) ^ (n - k) : ℝ)))
-    atTop (𝓝 (ENNReal.ofReal (poissonPMFReal r k))) :=
+    atTop (𝓝 (.ofReal (Real.exp (-r) * (r ^ k) / k.factorial))) :=
     tendsto_ofReal (tendsto_choose_mul_pow_of_tendsto_mul_atTop k (by norm_cast))
-  rw [poissonPMFReal_ofReal_eq_poissonPMF r k] at t1
   refine Tendsto.congr' ?_ t1
   simpa only [EventuallyEq, eventually_atTop, ge_iff_le] using
     ⟨k, fun b hb ↦ PMF.binomial_apply_of_le hb (h b)⟩
