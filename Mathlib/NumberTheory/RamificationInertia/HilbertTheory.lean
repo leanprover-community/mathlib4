f2b42bba12446fb17575139499021f420f63b35c
Xavier Roblot <46200072+xroblot@users.noreply.github.com>
2026-03-09 15:33:52 +0100

    clean up

--- Mathlib/NumberTheory/RamificationInertia/HilbertTheory.lean
+++ Mathlib/NumberTheory/RamificationInertia/HilbertTheory.lean
@@ -95,4 +95,16 @@ theorem IsInertiaField.of_isGaloisGroup [h : IsGaloisGroup (inertia G P) D L] :
     simp_rw [smul_div₀', subgroup_smul_def, ← algebraMap.smul', ← subgroup_smul_def,
       inertiaEquiv_symm_apply_smul]
 
+/-- Two decomposition fields are isomorphic. -/
+noncomputable abbrev IsDecompositionField.ringEquiv (D' : Type*) [Field D'] [Algebra D' L]
+    [IsDecompositionField K L P D] [IsDecompositionField K L P D'] :
+    D ≃+* D' :=
+  IsGaloisGroup.ringEquiv (stabilizer Gal(L/K) P) D D' L
+
+/-- Two inertia fields are isomorphic. -/
+noncomputable abbrev IsInertiaField.ringEquiv (E' : Type*) [Field E'] [Algebra E' L]
+    [h : IsInertiaField K L P E] [IsInertiaField K L P E'] :
+    E ≃+* E' :=
+  IsGaloisGroup.ringEquiv (inertia Gal(L/K) P) E E' L
+
 end basic
