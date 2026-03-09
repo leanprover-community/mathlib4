2e4a6a15a07f159f14275590d972b857afe7a134
Jireh Loreaux <loreaujy@gmail.com>
2026-03-09 16:53:26 -0500

    Update Mathlib/Analysis/CStarAlgebra/ContinuousFunctionalCalculus/RealImaginaryPart.lean
    Co-authored-by: Monica Omar <23701951+themathqueen@users.noreply.github.com>

--- Mathlib/Analysis/CStarAlgebra/ContinuousFunctionalCalculus/RealImaginaryPart.lean
+++ Mathlib/Analysis/CStarAlgebra/ContinuousFunctionalCalculus/RealImaginaryPart.lean
@@ -97,12 +97,79 @@ end NonUnital
 section Unital
 
 variable [Ring A] [StarRing A] [Algebra ℂ A] [StarModule ℂ A]
-  [ContinuousFunctionalCalculus ℂ A IsStarNormal] [ContinuousMapZero.UniqueHom ℂ A]
+  [ContinuousFunctionalCalculus ℂ A IsStarNormal]
 
+set_option backward.isDefEq.respectTransparency false in
 lemma cfc_re_id (a : A) (hp : IsStarNormal a := by cfc_tac) :
-    cfc (re · : ℂ → ℂ) a = ℜ a := by rw [← cfcₙ_eq_cfc, cfcₙ_re_id a]
+    cfc (re · : ℂ → ℂ) a = ℜ a := by
+  conv_rhs => rw [realPart_apply_coe, ← cfc_id' ℂ a, ← cfc_star, ← cfc_add .., ← cfc_smul ..]
+  refine cfc_congr fun x hx ↦ ?_
+  rw [Complex.re_eq_add_conj, ← smul_one_smul ℂ 2⁻¹]
+  simp [div_eq_inv_mul]
 
 lemma cfc_im_id (a : A) (hp : IsStarNormal a := by cfc_tac) :
-    cfc (im · : ℂ → ℂ) a = ℑ a := by rw [← cfcₙ_eq_cfc, cfcₙ_im_id a]
+    cfc (im · : ℂ → ℂ) a = ℑ a := by
+  suffices cfc (fun z : ℂ ↦ re z + I * im z) a = ℜ a + I • ℑ a by
+    rw [cfc_add .., cfc_const_mul .., cfc_re_id a] at this
+    simpa
+  simp [mul_comm I, re_add_im, cfc_id' .., realPart_add_I_smul_imaginaryPart]
+
+lemma spectrum_realPart (a : A) (ha : IsStarNormal a := by cfc_tac) :
+    spectrum ℂ (ℜ a : A) = (fun x ↦ (re x : ℂ)) '' (spectrum ℂ a) := by
+  rw [← cfc_re_id a, cfc_map_spectrum ..]
+
+set_option backward.isDefEq.respectTransparency false in
+lemma spectrum_realPart' (a : A) (ha : IsStarNormal a := by cfc_tac) :
+    spectrum ℝ (ℜ a : A) = re '' (spectrum ℂ a) := by
+  simp [← (ℜ a).2.spectrumRestricts.image, spectrum_realPart a, Set.image_image]
+
+lemma spectrum_imaginaryPart (a : A) (ha : IsStarNormal a := by cfc_tac) :
+    spectrum ℂ (ℑ a : A) = (fun x ↦ (im x : ℂ)) '' (spectrum ℂ a) := by
+  rw [← cfc_im_id a, cfc_map_spectrum ..]
+
+set_option backward.isDefEq.respectTransparency false in
+lemma spectrum_imaginaryPart' (a : A) (ha : IsStarNormal a := by cfc_tac) :
+    spectrum ℝ (ℑ a : A) = im '' (spectrum ℂ a) := by
+  simp [← (ℑ a).2.spectrumRestricts.image, spectrum_imaginaryPart a, Set.image_image]
+
+variable [ContinuousMap.UniqueHom ℂ A]
+
+lemma cfc_realPart (f : ℂ → ℂ) (a : A) (hf : ContinuousOn f (spectrum ℂ (ℜ a : A)) := by cfc_tac)
+    (ha : IsStarNormal a := by cfc_tac) :
+    cfc f (ℜ a : A) = cfc (fun x ↦ f (re x)) a := by
+  rw [spectrum_realPart a] at hf
+  rw [← cfc_re_id a, ← cfc_comp' ..]
+
+lemma cfc_imaginaryPart (f : ℂ → ℂ) (a : A)
+    (hf : ContinuousOn f (spectrum ℂ (ℑ a : A)) := by cfc_tac)
+    (ha : IsStarNormal a := by cfc_tac) :
+    cfc f (ℑ a : A) = cfc (fun x ↦ f (im x)) a := by
+  rw [spectrum_imaginaryPart a] at hf
+  rw [← cfc_im_id a, ← cfc_comp' ..]
+
+variable [T2Space A]
+
+lemma cfc_comp_re (f : ℝ → ℝ) (a : A)
+    (hf : ContinuousOn f (spectrum ℝ (ℜ a : A)) := by cfc_tac)
+    (ha : IsStarNormal a := by cfc_tac) :
+    cfc (fun x : ℂ ↦ f (re x)) a = cfc f (ℜ a : A) := by
+  have : ContinuousOn (fun x ↦ (f x.re) : ℂ → ℂ) ((re · : ℂ → ℂ) '' spectrum ℂ a) := by
+    rw [spectrum_realPart' a] at hf
+    refine continuous_ofReal.comp_continuousOn <| hf.comp (by fun_prop) ?_
+    simpa [Set.mapsTo_image_iff, Function.comp_def] using Set.mapsTo_image ..
+  conv_rhs =>
+    rw [cfc_real_eq_complex, ← cfc_re_id a, ← cfc_comp' ..]
+    simp
+
+lemma cfc_comp_im (f : ℝ → ℝ) (a : A) (hf : ContinuousOn f (spectrum ℝ (ℑ a : A)))
+    (ha : IsStarNormal a := by cfc_tac) :
+    cfc (fun x : ℂ ↦ f (im x)) a = cfc f (ℑ a : A) := by
+  have : ContinuousOn (fun x ↦ (f x.re) : ℂ → ℂ) ((im · : ℂ → ℂ) '' spectrum ℂ a) := by
+    rw [spectrum_imaginaryPart' a] at hf
+    refine continuous_ofReal.comp_continuousOn <| hf.comp (by fun_prop) ?_
+    simpa [Set.mapsTo_image_iff, Function.comp_def] using Set.mapsTo_image ..
+  conv_rhs =>
+    rw [cfc_real_eq_complex, ← cfc_im_id a, ← cfc_comp' ..]
+    simp
 
 end Unital
