leanprover-community/mathlib4

commit 86929b0c5bcdc7846fd6909d014b2dd1f05d0bbe

chore(Tactic/Convert): improve `convert` tactic family docstrings (#34840)
This PR (re)writes the docstring for the `convert`/`convert_to`/`ac_convert` tactics, to make sure they are complete without getting too long.

The existing docstring for `convert` was very detailed, which is good for e.g. a reference manual, but as a docstring it was hard to quickly find the relevance. I moved this to the module docs and wrote a new docstring that still lists the same features but in a structured order, referring to other tactics for the full picture.

Co-authored-by: Anne C.A. Baanen <vierkantor@vierkantor.com>

Mathlib/Tactic/Convert.lean
@@ -10,57 +10,7 @@ public import Mathlib.Tactic.CongrExclamation
 
 /-!
 # The `convert` tactic.
--/
 
-public meta section
-
-open Lean Meta Elab Tactic
-
-/--
-Close the goal `g` using `Eq.mp v e`,
-where `v` is a metavariable asserting that the type of `g` and `e` are equal.
-Then call `MVarId.congrN!` (also using local hypotheses and reflexivity) on `v`,
-and return the resulting goals.
-
-With `symm = true`, reverses the equality in `v`, and uses `Eq.mpr v e` instead.
-With `depth = some n`, calls `MVarId.congrN! n` instead, with `n` as the max recursion depth.
--/
-def Lean.MVarId.convert (e : Expr) (symm : Bool)
-    (depth : Option Nat := none) (config : Congr!.Config := {})
-    (patterns : List (TSyntax `rcasesPat) := []) (g : MVarId) :
-    MetaM (List MVarId) := g.withContext do
-  let src ← inferType e
-  let tgt ← g.getType
-  let v ← mkFreshExprMVar (← mkAppM ``Eq (if symm then #[src, tgt] else #[tgt, src]))
-  g.assign (← mkAppM (if symm then ``Eq.mp else ``Eq.mpr) #[v, e])
-  let m := v.mvarId!
-  m.congrN! depth config patterns
-
-/--
-Replaces the type of the local declaration `fvarId` with `typeNew`,
-using `Lean.MVarId.congrN!` to prove that the old type of `fvarId` is equal to `typeNew`.
-Uses `Lean.MVarId.replaceLocalDecl` to replace the type.
-Returns the new goal along with the side goals generated by `congrN!`.
-
-With `symm = true`, reverses the equality,
-changing the goal to prove `typeNew` is equal to `typeOld`.
-With `depth = some n`, calls `MVarId.congrN! n` instead, with `n` as the max recursion depth.
--/
-def Lean.MVarId.convertLocalDecl (g : MVarId) (fvarId : FVarId) (typeNew : Expr) (symm : Bool)
-    (depth : Option Nat := none) (config : Congr!.Config := {})
-    (patterns : List (TSyntax `rcasesPat) := []) :
-    MetaM (MVarId × List MVarId) := g.withContext do
-  let typeOld ← fvarId.getType
-  let v ← mkFreshExprMVar (← mkAppM ``Eq
-    (if symm then #[typeNew, typeOld] else #[typeOld, typeNew]))
-  let pf ← if symm then mkEqSymm v else pure v
-  let res ← g.replaceLocalDecl fvarId typeNew pf
-  let gs ← v.mvarId!.congrN! depth config patterns
-  return (res.mvarId, gs)
-
-namespace Mathlib.Tactic
-
-/--
 The `exact e` and `refine e` tactics require a term `e` whose type is
 definitionally equal to the goal. `convert e` is similar to `refine e`,
 but the type of `e` is not required to exactly match the
@@ -122,7 +72,107 @@ convert (config := {transparency := .default}) h
 ```
 These are passed to `congr!`. See `Congr!.Config` for options.
 -/
-syntax (name := convert) "convert" (Parser.Tactic.config)? " ←"? ppSpace term (" using " num)?
+
+public meta section
+
+open Lean Meta Elab Tactic
+
+/--
+Close the goal `g` using `Eq.mp v e`,
+where `v` is a metavariable asserting that the type of `g` and `e` are equal.
+Then call `MVarId.congrN!` (also using local hypotheses and reflexivity) on `v`,
+and return the resulting goals.
+
+With `symm = true`, reverses the equality in `v`, and uses `Eq.mpr v e` instead.
+With `depth = some n`, calls `MVarId.congrN! n` instead, with `n` as the max recursion depth.
+-/
+def Lean.MVarId.convert (e : Expr) (symm : Bool)
+    (depth : Option Nat := none) (config : Congr!.Config := {})
+    (patterns : List (TSyntax `rcasesPat) := []) (g : MVarId) :
+    MetaM (List MVarId) := g.withContext do
+  let src ← inferType e
+  let tgt ← g.getType
+  let v ← mkFreshExprMVar (← mkAppM ``Eq (if symm then #[src, tgt] else #[tgt, src]))
+  g.assign (← mkAppM (if symm then ``Eq.mp else ``Eq.mpr) #[v, e])
+  let m := v.mvarId!
+  m.congrN! depth config patterns
+
+/--
+Replaces the type of the local declaration `fvarId` with `typeNew`,
+using `Lean.MVarId.congrN!` to prove that the old type of `fvarId` is equal to `typeNew`.
+Uses `Lean.MVarId.replaceLocalDecl` to replace the type.
+Returns the new goal along with the side goals generated by `congrN!`.
+
+With `symm = true`, reverses the equality,
+changing the goal to prove `typeNew` is equal to `typeOld`.
+With `depth = some n`, calls `MVarId.congrN! n` instead, with `n` as the max recursion depth.
+-/
+def Lean.MVarId.convertLocalDecl (g : MVarId) (fvarId : FVarId) (typeNew : Expr) (symm : Bool)
+    (depth : Option Nat := none) (config : Congr!.Config := {})
+    (patterns : List (TSyntax `rcasesPat) := []) :
+    MetaM (MVarId × List MVarId) := g.withContext do
+  let typeOld ← fvarId.getType
+  let v ← mkFreshExprMVar (← mkAppM ``Eq
+    (if symm then #[typeNew, typeOld] else #[typeOld, typeNew]))
+  let pf ← if symm then mkEqSymm v else pure v
+  let res ← g.replaceLocalDecl fvarId typeNew pf
+  let gs ← v.mvarId!.congrN! depth config patterns
+  return (res.mvarId, gs)
+
+namespace Mathlib.Tactic
+
+/--
+`convert e`, where the term `e` is inferred to have type `t`, replaces the main goal `⊢ t'` with new
+goals for proving the equality `t' = t` using congruence. The goals are created like `congr!` would.
+Like `refine e`, any holes (`?_` or `?x`) in `e` that are not solved by unification are converted
+into new goals, using the hole's name, if any, as the goal case name.
+Like `congr!`, `convert` introduces variables while applying congruence rules. These can be
+pattern-matched, like `rintro` would, using the `with` keyword.
+
+See also `convert_to t`, where `t` specifies the expected type, instead of a proof term of type `t`.
+In other words, `convert_to t` works like `convert (?_ : t)`. Both tactics use the same options.
+
+* `convert ← e` creates equality goals in the opposite direction (with the goal type on the right).
+* `convert e using n`, where `n` is a positive numeral, controls the depth with which congruence is
+  applied. For example, if the main goal is `⊢ Prime (n + n + 1)` and `e : Prime (2 * n + 1)`, then
+  `convert e using 2` results in one goal, `⊢ n + n = 2 * n`, and `convert e using 3` (or more)
+  results in two (impossible) goals `⊢ HAdd.hAdd = HMul.hMul` and `⊢ n = 2`.
+  By default, the depth is unlimited.
+* `convert e with x ⟨y₁, y₂⟩ (z₁ | z₂)` names or pattern-matches the variables introduced by
+  congruence rules, like `rintro x ⟨y₁, y₂⟩ (z₁ | z₂)` would.
+* `convert (config := cfg) e` uses the configuration options in `cfg` to control the congruence
+  rules (see `Congr!.Config`).
+
+Examples:
+
+```lean
+-- `convert using` controls the depth of congruence.
+example {n : ℕ} (e : Prime (2 * n + 1)) :
+    Prime (n + n + 1) := by
+  convert e using 2
+  -- One goal: ⊢ n + n = 2 * n
+  ring
+```
+
+```lean
+-- `convert` can fail where `exact` succeeds.
+example (h : p 0) : p 1 := by
+  fail_if_success
+    convert h -- fails, left-over goal 1 = 0
+    done
+  exact h -- succeeds
+
+```lean
+-- `convert with` names introduced variables.
+example (p q : Nat → Prop) (h : ∀ ε > 0, p ε) :
+    ∀ ε > 0, q ε := by
+  convert h using 2 with ε hε
+  -- Goal now looks like:
+  -- hε : ε > 0
+  -- ⊢ q ε ↔ p ε
+  sorry
+-/
+syntax (name := convert) "convert" Lean.Parser.Tactic.optConfig " ←"? ppSpace term (" using " num)?
   (" with" (ppSpace colGt rintroPat)*)? : tactic
 
 /--
@@ -142,7 +192,7 @@ def elabTermForConvert (term : Syntax) (expectedType? : Option Expr) :
       return t
 
 elab_rules : tactic
-| `(tactic| convert $[$cfg:config]? $[←%$sym]? $term $[using $n]? $[with $ps?*]?) =>
+| `(tactic| convert $cfg $[←%$sym]? $term $[using $n]? $[with $ps?*]?) =>
   withMainContext do
     let config ← Congr!.elabConfig (mkOptionalNode cfg)
     let patterns := (Lean.Elab.Tactic.RCases.expandRIntroPats (ps?.getD #[])).toList
@@ -151,33 +201,32 @@ elab_rules : tactic
     liftMetaTactic fun g ↦
       return (← g.convert e sym.isSome (n.map (·.getNat)) config patterns) ++ gs
 
--- FIXME restore when `add_tactic_doc` is ported.
--- add_tactic_doc
--- { name       := "convert",
---   category   := doc_category.tactic,
---   decl_names := [`tactic.interactive.convert],
---   tags       := ["congruence"] }
-
 /--
-The `convert_to` tactic is for changing the type of the target or a local hypothesis,
-but unlike the `change` tactic it will generate equality proof obligations using `congr!`
-to resolve discrepancies.
+`convert_to t` on a goal `⊢ t'` changes the goal to `⊢ t` and adds new goals for proving the
+equality `t' = t` using congruence. The goals are created like `congr!` would.
+Any remaining congruence goals come before the main goal.
+Like `refine e`, any holes (`?_` or `?x`) in `t` that are not solved by unification are converted
+into new goals, using the hole's name, if any, as the goal case name.
+Like `congr!`, `convert_to` introduces variables while applying congruence rules. These can be
+pattern-matched, like `rintro` would, using the `with` keyword.
 
-* `convert_to ty` changes the target to `ty`
-* `convert_to ty using n` uses `congr! n` instead of `congr! 1`
-* `convert_to ty at h` changes the type of the local hypothesis `h` to `ty`.
-  Any remaining `congr!` goals come first.
+`convert e`, where `e` is a term of type `t`, uses `e` to close the new main goal. In other words,
+`convert e` works like `convert_to t; refine e`. Both tactics use the same options.
 
-Operating on the target, the tactic `convert_to ty using n`
-is the same as `convert (?_ : ty) using n`.
-The difference is that `convert_to` takes a type but `convert` takes a proof term.
-
-Except for it also being able to operate on local hypotheses,
-the syntax for `convert_to` is the same as for `convert`, and it has variations such as
-`convert_to ← g` and `convert_to (config := {transparency := .default}) g`.
-
-Note that `convert_to ty at h` may leave a copy of `h` if a later local hypotheses or the target
-depends on it, just like in `rw` or `simp`.
+* `convert_to ty at h` changes the type of the local hypothesis `h` to `ty`. If later local
+  hypotheses or the goal depend on `h`, then `convert_to t at h` may leave a copy of `h`.
+* `convert_to ← t` creates equality goals in the opposite direction (with the original goal type on
+  the right).
+* `convert_to t using n`, where `n` is a positive numeral, controls the depth with which congruence
+  is applied. For example, if the main goal is `⊢ Prime (n + n + 1)`,
+  then `convert_to Prime (2 * n + 1) using 2` results in one goal, `⊢ n + n = 2 * n`, and
+  `convert_to Prime (2 * n + 1) using 3` (or more) results in two (impossible) goals
+  `⊢ HAdd.hAdd = HMul.hMul` and `⊢ n = 2`.
+  The default value for `n` is 1.
+* `convert_to t with x ⟨y₁, y₂⟩ (z₁ | z₂)` names or pattern-matches the variables introduced by
+  congruence rules, like `rintro x ⟨y₁, y₂⟩ (z₁ | z₂)` would.
+* `convert_to (config := cfg) t` uses the configuration options in `cfg` to control the congruence
+  rules (see `Congr!.Config`).
 -/
 syntax (name := convertTo) "convert_to" (Parser.Tactic.config)? " ←"? ppSpace term (" using " num)?
   (" with" (ppSpace colGt rintroPat)*)? (Parser.Tactic.location)? : tactic
@@ -202,8 +251,24 @@ elab_rules : tactic
     (failed := fun _ ↦ throwError "convert_to failed")
 
 /--
-`ac_change g using n` is `convert_to g using n` followed by `ac_rfl`. It is useful for
-rearranging/reassociating e.g. sums:
+`ac_change t` on a goal `⊢ t'` changes the goal to `⊢ t` and adds new goals for proving the equality
+`t' = t` using congruence, then tries proving these goals by associativity and commutativity. The
+goals are created like `congr!` would.
+In other words, `ac_change t` is like `convert_to t` followed by `ac_refl`.
+
+Like `refine e`, any holes (`?_` or `?x`) in `t` that are not solved by unification are converted
+into new goals, using the hole's name, if any, as the goal case name.
+Like `congr!`, `convert_to` introduces variables while applying congruence rules. These can be
+pattern-matched, like `rintro` would, using the `with` keyword.
+
+* `ac_change t using n`, where `n` is a positive numeral, controls the depth with which congruence
+  is applied. For example, if the main goal is `⊢ Prime ((a * b + 1) + c)`,
+  then `ac_change Prime ((1 + a * b) + c) using 2` solves the side goals, and
+  `ac_change Prime ((1 + a * b) + c) using 3` (or more) results in two (impossible) goals
+  `⊢ 1 = a * b` and `⊢ a * b = 1`.
+  The default value for `n` is 1.
+
+Example:
 ```lean
 example (a b c d e f g N : ℕ) : (a + b) + (c + d) + (e + f) + g ≤ N := by
   ac_change a + d + e + f + c + g + b ≤ _

commit c287e212ea94add1c127a3a71932a782a488b909

chore(Tactic/Convert): fix docstring back-ticks (#36315)
This PR fixes a style mistake in the docstring of `convert` which was introduced in #34840.

Mathlib/Tactic/Convert.lean
@@ -152,9 +152,7 @@ example {n : ℕ} (e : Prime (2 * n + 1)) :
   convert e using 2
   -- One goal: ⊢ n + n = 2 * n
   ring
-```
 
-```lean
 -- `convert` can fail where `exact` succeeds.
 example (h : p 0) : p 1 := by
   fail_if_success
@@ -162,7 +160,6 @@ example (h : p 0) : p 1 := by
     done
   exact h -- succeeds
 
-```lean
 -- `convert with` names introduced variables.
 example (p q : Nat → Prop) (h : ∀ ε > 0, p ε) :
     ∀ ε > 0, q ε := by
@@ -171,6 +168,7 @@ example (p q : Nat → Prop) (h : ∀ ε > 0, p ε) :
   -- hε : ε > 0
   -- ⊢ q ε ↔ p ε
   sorry
+```
 -/
 syntax (name := convert) "convert" Lean.Parser.Tactic.optConfig " ←"? ppSpace term (" using " num)?
   (" with" (ppSpace colGt rintroPat)*)? : tactic