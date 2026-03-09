1d5103ab1be7c276779f90798f942faf95c13c66
Michael Rothgang <rothgang@math.uni-bonn.de>
2026-03-09 21:51:23 +0100

    wip(Curvature): indicate how global hypotheses on differentiability are hopefully not necessary

--- Mathlib/Geometry/Manifold/VectorBundle/CovariantDerivative/Curvature.lean
+++ Mathlib/Geometry/Manifold/VectorBundle/CovariantDerivative/Curvature.lean
@@ -29,6 +29,48 @@ variable {𝕜 : Type*} [NontriviallyNormedField 𝕜]
   {H : Type*} [TopologicalSpace H] {I : ModelWithCorners 𝕜 E H}
   {M : Type*} [TopologicalSpace M] [ChartedSpace H M] {x : M}
 
+/-! A preliminary lemma about -/
+section prelim
+
+variable (F : Type*) [NormedAddCommGroup F] [NormedSpace 𝕜 F]
+  {V : M → Type*} [TopologicalSpace (TotalSpace F V)]
+  [∀ x, AddCommGroup (V x)] [∀ x, Module 𝕜 (V x)]
+  [∀ x : M, TopologicalSpace (V x)]
+  [∀ x, IsTopologicalAddGroup (V x)] [∀ x, ContinuousSMul 𝕜 (V x)]
+  [FiberBundle F V]
+
+-- FIXME: does this require real bundles?
+lemma exists_contMDiff_of_one_form {k : WithTop ℕ∞}
+    {σ : Π x : M, V x} (hσ : CMDiffAt k (T% σ) x) :
+    ∃ σ' : (Π x : M, V x),
+      CMDiff k (T% σ') ∧ σ' x = σ x ∧ mfderiv% (T% σ') x = mfderiv% (T% σ) x := by
+  /- proof idea: assuming smooth bump functions, this becomes a local question
+  locally, convolve σ with a bump function of small support; should preserve σ x and mfderiv
+  does mathlib have this already? (Moritz Doll proved something similar, I think!)
+  -/
+  sorry
+
+-- We need one level more of agreement!
+
+-- TODO: might be a better definition of smoothness; proving the equivalence requires more work!
+variable {F} in
+lemma ContMDiffCovariantDerivativeOn.contMDiff' [IsManifold I 1 M] [VectorBundle 𝕜 F V]
+    {k : WithTop ℕ∞} {cov : (Π x : M, V x) → (Π x : M, TangentSpace I x →L[𝕜] V x)}
+    (hcov : IsCovariantDerivativeOn F cov) [hcov' : ContMDiffCovariantDerivativeOn F k cov univ]
+    {σ : Π x : M, V x} (hσ :CMDiffAt (k + 1) (T% σ) x) :
+    letI cov (x : M) : TotalSpace (E →L[𝕜] F) fun x ↦ TangentSpace I x →L[𝕜] V x := ⟨x, cov σ x⟩
+    ContMDiffAt I (I.prod 𝓘(𝕜, E →L[𝕜] F)) k cov x := by
+  obtain ⟨σ', hσ', heqs, hdiffx⟩ := exists_contMDiff_of_one_form F hσ
+  have aux := contMDiffOn_univ.mp (hcov'.contMDiff hσ'.contMDiffOn)
+  -- know: ∇ σ and ∇ σ' agree at x
+  have aux' := hcov.congr_of_eq_one_jet (Filter.univ_mem)
+    (hσ.mdifferentiableAt (by simp)) (hσ'.mdifferentiableAt (by simp))
+    heqs.symm hdiffx.symm -- TODO: choose direction for both equalities!
+  -- we need one more level, though!
+  sorry
+
+end prelim
+
 /-! ## Curvature tensor of an unbundled covariant derivative on `TM` on a set `s` -/
 namespace IsCovariantDerivativeOn
 
@@ -90,11 +132,10 @@ variable [IsManifold I (2 + 1) M] [IsManifold I (minSmoothness 𝕜 2) M]
 lemma aux
     (hcov : IsCovariantDerivativeOn E cov) [hcov' : ContMDiffCovariantDerivativeOn E 1 cov univ]
     {x : M} {Y Z σ : (x : M) → TangentSpace I x}
-    -- XXX: after rephrasing hcov', does CMDiffAt 2 (T% Z) x suffice?
-    (hσ : CMDiffAt 2 (T% σ) x) (hY : CMDiffAt 2 (T% Y) x) (hZ : CMDiff 2 (T% Z)) :
+    (hσ : CMDiffAt 2 (T% σ) x) (hY : CMDiffAt 2 (T% Y) x) (hZ : CMDiffAt 2 (T% Z) x) :
     (MDiffAt fun x ↦ TotalSpace.mk' E x ((cov Z x) (VectorField.mlieBracket I σ Y x))) x := by
   apply ContMDiffAt.mdifferentiableAt _ one_ne_zero
-  apply temp' hcov ?_ ((hcov'.contMDiff hZ.contMDiffOn).contMDiffAt (by simp))
+  apply temp' hcov ?_ (hcov'.contMDiff' hcov hZ)
   apply ContMDiffAt.mlieBracket_vectorField hσ hY _
   simp; sorry -- want sth with minSmoothness instead; otherwise too weak for general 𝕜
 
@@ -125,10 +166,10 @@ theorem curvatureTensorAux_tensorial₁ (hcov : IsCovariantDerivativeOn E cov) (
     -- corollaries, which occur as side goals several times
     have hZσ : MDiffAt (fun x ↦ TotalSpace.mk' E x (cov Z x (σ x))) x := by
       apply ContMDiffAt.mdifferentiableAt _ one_ne_zero
-      exact temp hcov hσ hZ ((hcov'.contMDiff hZ.contMDiffOn).contMDiffAt (by simp))
+      exact temp hcov hσ hZ (hcov'.contMDiff' hcov hZ')
     have hZσ' : MDiffAt (fun x ↦ TotalSpace.mk' E x (cov Z x (σ' x))) x := by
       apply ContMDiffAt.mdifferentiableAt _ one_ne_zero
-      exact temp hcov hσ' hZ ((hcov'.contMDiff hZ.contMDiffOn).contMDiffAt (by simp))
+      exact temp hcov hσ' hZ (hcov'.contMDiff' hcov hZ')
     have missing :
       (cov (fun x ↦ (cov Z x) (VectorField.mlieBracket I (σ + σ') Y x)) x) (Y x) =
         (cov (fun x ↦ (cov Z x) (VectorField.mlieBracket I σ Y x)) x) (Y x)
@@ -138,22 +179,22 @@ theorem curvatureTensorAux_tensorial₁ (hcov : IsCovariantDerivativeOn E cov) (
           ) x) (Y x)
       · congr 1
         sorry -- missing tensoriality lemma; first arguments are equal at x
-      · erw [hcov.add (aux hcov (hσ x) hY hZ) (aux hcov (hσ' x) hY hZ)]
+      · erw [hcov.add (aux hcov (hσ x) hY hZ') (aux hcov (hσ' x) hY hZ')]
         simp
     rw [hcov.sub]
     rotate_left
     · exact mdifferentiableAt_add_section hZσ hZσ'
     · apply ContMDiffAt.mdifferentiableAt _ one_ne_zero
-      apply temp' hcov ?_ ((hcov'.contMDiff hZ.contMDiffOn).contMDiffAt (by simp))
+      apply temp' hcov ?_ (hcov'.contMDiff' hcov hZ')
       apply ContMDiffAt.mlieBracket_vectorField (ContMDiff.add_section hσ hσ' ..) hY
       simp; sorry -- want sth with minSmoothness instead; otherwise too weak for general k
-    rw [hcov.sub hZσ (aux hcov (hσ x) hY hZ)]
+    rw [hcov.sub hZσ (aux hcov (hσ x) hY hZ')]
     dsimp
     erw [hcov.add hZσ hZσ']
     simp only [ContinuousLinearMap.add_apply]
     --set C := cov (fun x ↦ (cov Z x) (σ x)) x
     --set D := cov (fun x ↦ (cov Z x) (σ' x)) x
-    rw [hcov.sub hZσ' ((aux hcov (hσ' x) hY hZ))]
+    rw [hcov.sub hZσ' ((aux hcov (hσ' x) hY hZ'))]
     rw [missing]
     --set E := (cov (fun x ↦ (cov Z x) (VectorField.mlieBracket I σ Y x)) x) (Y x)
     dsimp
