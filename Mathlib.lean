f84529f2c639b6b2e2c5146a4da7d2ebe5347158
Moritz Doll <21366319+mcdoll@users.noreply.github.com>
2026-03-10 22:48:16 +0000

    feat(Analysis/Distribution): Fourier multiplier (#34099)
    We define Fourier multiplier with temperate growth functions acting on Schwartz functions as well as tempered distributions.
    We prove that the directional derivative and the Laplacian can be represented as Fourier multiplier.

--- Mathlib.lean
+++ Mathlib.lean
@@ -1863,6 +1863,7 @@ public import Mathlib.Analysis.Distribution.AEEqOfIntegralContDiff
 public import Mathlib.Analysis.Distribution.ContDiffMapSupportedIn
 public import Mathlib.Analysis.Distribution.DerivNotation
 public import Mathlib.Analysis.Distribution.Distribution
+public import Mathlib.Analysis.Distribution.FourierMultiplier
 public import Mathlib.Analysis.Distribution.FourierSchwartz
 public import Mathlib.Analysis.Distribution.SchwartzSpace
 public import Mathlib.Analysis.Distribution.SchwartzSpace.Basic
