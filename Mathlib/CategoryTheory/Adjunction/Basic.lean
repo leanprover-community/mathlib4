33faf64ad1a610876bce952391143c9a7190a850
Christian Merten <christian@merten.dev>
2026-03-11 20:25:47 +0100

    add suggested lemmas and fix

--- Mathlib/CategoryTheory/Adjunction/Basic.lean
+++ Mathlib/CategoryTheory/Adjunction/Basic.lean
@@ -505,6 +505,16 @@ def ofNatIsoLeft {F G : C ⥤ D} {H : D ⥤ C} (adj : F ⊣ H) (iso : F ≅ G) :
     simp [← Functor.comp_map]
   right_triangle_components := by simp [← Functor.map_comp]
 
+lemma homEquiv_ofNatIsoLeft_apply {F G : C ⥤ D} {H : D ⥤ C} (adj : F ⊣ H) (iso : F ≅ G)
+    {X : C} {Y : D} (f : G.obj X ⟶ Y) :
+    (ofNatIsoLeft adj iso).homEquiv X Y f = adj.homEquiv _ _ (iso.hom.app _ ≫ f) := by
+  simp
+
+lemma homEquiv_ofNatIsoLeft_symm_apply {F G : C ⥤ D} {H : D ⥤ C} (adj : F ⊣ H) (iso : F ≅ G)
+    {X : C} {Y : D} (f : X ⟶ H.obj Y) :
+    ((ofNatIsoLeft adj iso).homEquiv X Y).symm f = iso.inv.app _ ≫ (adj.homEquiv _ _).symm f := by
+  simp
+
 set_option backward.isDefEq.respectTransparency false in
 /-- Transport an adjunction along a natural isomorphism on the right. -/
 @[simps]
@@ -517,6 +527,17 @@ def ofNatIsoRight {F : C ⥤ D} {G H : D ⥤ C} (adj : F ⊣ G) (iso : G ≅ H) 
       assoc, ← iso.hom.naturality_assoc, ← iso.hom.naturality, unit_naturality_assoc,
       adj.right_triangle_components_assoc]
     simp
+
+lemma homEquiv_ofNatIsoRight_apply {F : C ⥤ D} {G H : D ⥤ C} (adj : F ⊣ G) (iso : G ≅ H)
+    {X : C} {Y : D} (f : F.obj X ⟶ Y) :
+    (ofNatIsoRight adj iso).homEquiv X Y f = adj.homEquiv _ _ f ≫ iso.hom.app _ := by
+  simp
+
+lemma homEquiv_ofNatIsoRight_symm_apply {F : C ⥤ D} {G H : D ⥤ C} (adj : F ⊣ G) (iso : G ≅ H)
+    {X : C} {Y : D} (f : X ⟶ H.obj Y) :
+    ((ofNatIsoRight adj iso).homEquiv X Y).symm f =
+      (adj.homEquiv _ _).symm (f ≫ iso.inv.app _) := by
+  simp
 
 set_option backward.isDefEq.respectTransparency false in
 /-- The isomorphism which an adjunction `F ⊣ G` induces on `G ⋙ yoneda`. This states that
