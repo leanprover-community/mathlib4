e9367ce99484975a033c659d3bab2c721a9fe3ce
blake-farman <blake.farman@gmail.com>
2026-03-11 12:41:27 -0500

    refactor: define radicals via IsIso and define Radical C as the full subcategory of Preradical C consisting of radical preradicals

--- Mathlib/CategoryTheory/Abelian/Preradical/Radical.lean
+++ Mathlib/CategoryTheory/Abelian/Preradical/Radical.lean
@@ -23,7 +23,7 @@ characterization of radicals in terms of the vanishing of `Φ.r` on `Φ.quotient
 ## Main definitions
 
 * `Preradical.IsRadical Φ` :
-  The proposition that a preradical `Φ` is radical, i.e. that `(Φ.colon Φ) ≅ Φ`.
+  The property that a preradical `Φ` is radical, i.e. that `(Φ.colon Φ) ≅ Φ`.
 
 * `Preradical.Radical C` :
   The type of radicals on `C`, given by a preradical together with a proof
@@ -48,41 +48,37 @@ preradical, radical, torsion theory, abelian
 namespace CategoryTheory.Abelian
 open CategoryTheory.Limits
 
-namespace Preradical
-
 variable {C : Type*} [Category C] [Abelian C]
 
+namespace Preradical
+
+variable (C)
 /-- A preradical `Φ` is *radical* if `Φ.colon Φ ≅ Φ`. -/
-class IsRadical (Φ : Preradical C) : Prop where
-  iso_self_colon : Nonempty ((Preradical.colon Φ Φ) ≅ Φ)
+def isRadical : ObjectProperty (Preradical C) :=
+  fun Φ ↦ IsIso (toColon Φ Φ)
 
-/-- A *radical* on `C` is a preradical together with a proof that it is radical
-in the sense of `IsRadical`. -/
-abbrev Radical := { Φ : Preradical C // IsRadical Φ }
-
-open Functor
+lemma isRadical_iff_isIso (Φ : Preradical C) :
+  isRadical C Φ ↔ IsIso (toColon Φ Φ) :=
+Iff.rfl
 
 /-- A preradical `Φ` is radical if and only if it `Φ` vanishes on the quotient `Φ.quotient`. -/
-theorem isRadical_iff {Φ : Preradical C} :
-    IsRadical Φ ↔ IsZero (Φ.quotient ⋙ Φ.r) := by
-  constructor
-  · intro h
-    obtain ⟨μ⟩ := h.iso_self_colon
-    refine IsZero.of_mono_eq_zero
-      (Φ.quotient.whiskerLeft Φ.ι ≫ (rightUnitor _).hom)
-      (zero_of_epi_comp (colonπ Φ Φ) ?_)
-    calc
-        _ = (colon Φ Φ).ι ≫ Φ.π :=
-          (isPullback_colon Φ Φ).w.symm
-        _ = μ.hom.hom.left ≫ Φ.ι ≫ Φ.π :=
-          (MonoOver.w_assoc μ.hom Φ.π).symm
-        _ = 0 := by
-          simp
-  · intro h
-    constructor
-    haveI := (isIso_toColon_iff.mpr h : IsIso (Φ.toColon Φ))
-    exact ⟨(asIso (Φ.toColon Φ)).symm⟩
+lemma isRadical_iff_isZero (Φ : Preradical C) :
+    isRadical C Φ ↔ IsZero (Φ.quotient ⋙ Φ.r) := by
+  rw [isRadical_iff_isIso, isIso_toColon_iff]
 
 end Preradical
+
+variable (C) in
+abbrev Radical := (Preradical.isRadical C).FullSubcategory
+
+namespace Radical
+
+instance (Φ : Radical C) : IsIso (Preradical.toColon Φ.obj Φ.obj) := Φ.property
+
+lemma isZero (Φ : Radical C) : IsZero (Φ.obj.quotient ⋙ Φ.obj.r) := by
+  rw [← Preradical.isRadical_iff_isZero, Preradical.isRadical_iff_isIso]
+  infer_instance
+
+end Radical
 
 end CategoryTheory.Abelian
