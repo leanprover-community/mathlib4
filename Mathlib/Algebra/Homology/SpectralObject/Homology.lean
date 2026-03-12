231d896976632effc6e15cf0d97597923f1022f7
Joël Riou <37772949+joelriou@users.noreply.github.com>
2026-03-12 21:28:06 +0100

    Update Mathlib/Algebra/Homology/SpectralObject/Homology.lean
    Co-authored-by: smorel394 <67864981+smorel394@users.noreply.github.com>

--- Mathlib/Algebra/Homology/SpectralObject/Homology.lean
+++ Mathlib/Algebra/Homology/SpectralObject/Homology.lean
@@ -90,21 +90,18 @@ set_option backward.isDefEq.respectTransparency false in
 lemma dKernelSequence_exact
     (hn₁ : n₀ + 1 = n₁ := by lia) (hn₂ : n₁ + 1 = n₂ := by lia) (hn₃ : n₂ + 1 = n₃ := by lia) :
     (X.dKernelSequence f₁ f₂ f₃ f₄ f₅ f₂₃ h₂₃ n₀ n₁ n₂ n₃ hn₁ hn₂ hn₃).Exact := by
-  rw [ShortComplex.exact_iff_exact_up_to_refinements]
-  intro A x₂ hx₂
-  dsimp at x₂ hx₂ ⊢
+  refine (ShortComplex.exact_iff_exact_up_to_refinements _).mpr (fun _ x₂ hx₂ ↦ ?_)
+  dsimp at hx₂ ⊢
   obtain ⟨A₁, π₁, _, y₂, hy₂⟩ :=
     surjective_up_to_refinements_of_epi (X.πE f₃ f₄ f₅ n₀ n₁ n₂) x₂
   have hy₂' := hy₂ =≫ (X.d f₁ f₂ f₃ f₄ f₅ n₀ n₁ n₂ n₃ ≫ X.ιE ..)
   simp only [assoc, reassoc_of% hx₂, zero_comp, comp_zero, πE_d_ιE] at hy₂'
   obtain ⟨A₂, π₂, _, y₁, hy₁⟩ :=
-    ((X.sequenceΨ_exact f₂ f₃ f₄ f₂₃ h₂₃ _ rfl n₁ n₂).exact 0).exact_up_to_refinements
-      y₂ hy₂'.symm
+    ((X.sequenceΨ_exact f₂ f₃ f₄ f₂₃ h₂₃ _ rfl n₁ n₂).exact 0).exact_up_to_refinements y₂ hy₂'.symm
   dsimp [sequenceΨ] at y₁ hy₁
   refine ⟨A₂, π₂ ≫ π₁, inferInstance, y₁ ≫ X.πE f₂₃ f₄ f₅ n₀ n₁ n₂, ?_⟩
-  rw [assoc, assoc, hy₂, reassoc_of% hy₁,
-    X.πE_map f₂₃ f₄ f₅ f₃ f₄ f₅ (fourδ₁Toδ₀ f₂ f₃ f₄ f₅ f₂₃ h₂₃)
-      (threeδ₁Toδ₀ f₂ f₃ f₄ f₂₃ h₂₃) n₀ n₁ n₂]
+  simp [hy₂, reassoc_of% hy₁, X.πE_map f₂₃ f₄ f₅ f₃ f₄ f₅ (fourδ₁Toδ₀ f₂ f₃ f₄ f₅ f₂₃ h₂₃)
+    (threeδ₁Toδ₀ f₂ f₃ f₄ f₂₃ h₂₃) n₀ n₁ n₂]
 
 end
 
