32e838c60f1d4ea7f75a77f87438f50095cdf96b
Yuma Mizuno <mizuno.y.aj@gmail.com>
2026-03-13 13:26:33 +0000

    update

--- MathlibTest/CategoryTheory/Bicategory/Basic.lean
+++ MathlibTest/CategoryTheory/Bicategory/Basic.lean
@@ -1,4 +1,5 @@
 import Mathlib.Tactic.CategoryTheory.Bicategory.Basic
+import Mathlib.CategoryTheory.Bicategory.Modification.Oplax
 
 open CategoryTheory Mathlib.Tactic BicategoryLike
 open Bicategory
@@ -19,3 +20,29 @@ example {f : a ⟶ b} {g : b ⟶ c} {h i : c ⟶ d} (η : h ⟶ i) :
 
 example {f g h : a ⟶ b} {η : f ⟶ g} {θ : g ⟶ h} : η ≫ θ = η ≫ θ := by
   bicategory
+
+section
+
+variable {B : Type u} [Bicategory.{w, v} B] {C : Type u} [Bicategory.{w, v} C]
+variable {F G H I : B ⥤ᵒᵖᴸ C}
+
+open Oplax.OplaxTrans
+
+example (η : F ⟶ G) {θ ι : G ⟶ H} (Γ : θ ⟶ ι) : η ≫ θ ⟶ η ≫ ι where
+  as := {
+    app a := η.app a ◁ Γ.as.app a
+    naturality {a b} f := by
+      dsimp only [categoryStruct_comp_app, categoryStruct_comp_naturality]
+      calc
+        _ =  𝟙 _ ⊗≫ ((F.map f ≫ η.app b) ◁ Γ.as.app b ≫ η.naturality f ▷ ι.app b) ⊗≫
+            η.app a ◁ ι.naturality f ⊗≫ 𝟙 _  := by
+          bicategory
+        _ = 𝟙 _ ⊗≫ η.naturality f ▷ _ ⊗≫ η.app a ◁ (G.map f ◁ Γ.as.app b ≫
+            ι.naturality f) ⊗≫ 𝟙 _ := by
+          rw [whisker_exchange]
+          bicategory
+        _ = _ := by
+          rw [Γ.as.naturality]
+          bicategory }
+
+end
