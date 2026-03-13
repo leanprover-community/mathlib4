1e86cfdbd284980b727be92915d343dbf79be3df
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2026-03-13 03:11:44 +0000

    fix

--- Mathlib/CategoryTheory/Functor/Category.lean
+++ Mathlib/CategoryTheory/Functor/Category.lean
@@ -57,7 +57,7 @@ instance Functor.category : Category.{max u₁ v₂} (C ⥤ D) where
 
 namespace NatTrans
 
-@[ext, grind ext]
+@[ext, grind ext, to_dual self]
 theorem ext' {α β : F ⟶ G} (w : α.app = β.app) : α = β := NatTrans.ext w
 
 @[simp, to_dual self]
