f160d83dae085d63351d6a08dc9b1906a45971df
Etienne Marion <baguettes_casino0c@icloud.com>
2026-03-13 09:06:40 +0100

    mk_all

--- Mathlib.lean
+++ Mathlib.lean
@@ -5935,6 +5935,7 @@ public import Mathlib.Order.Zorn
 public import Mathlib.Order.ZornAtoms
 public import Mathlib.Probability.BorelCantelli
 public import Mathlib.Probability.BrownianMotion.GaussianProjectiveFamily
+public import Mathlib.Probability.BrownianMotion.IsBrownian
 public import Mathlib.Probability.CDF
 public import Mathlib.Probability.Combinatorics.BinomialRandomGraph.Defs
 public import Mathlib.Probability.CondVar
