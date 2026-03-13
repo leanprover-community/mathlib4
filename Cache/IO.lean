75a0b3f02124e87301edb86a6dd52f23c68cdfba
Bryan Gin-ge Chen <bryangingechen@gmail.com>
2026-03-13 19:09:12 -0400

    ci: Revert "chore(cache): Update leantar to 0.1.18 (#36614)"
    CI is failing again after the update, so reverting for now.
    
    This reverts commit 7c885dc01c26f0eea1ac382e1a1dab674e966ff2.

--- Cache/IO.lean
+++ Cache/IO.lean
@@ -81,7 +81,7 @@ def CURLBIN :=
 
 /-- leantar version at https://github.com/digama0/leangz -/
 def LEANTARVERSION :=
-  "0.1.18"
+  "0.1.16"
 
 def EXE := if System.Platform.isWindows then ".exe" else ""
 
