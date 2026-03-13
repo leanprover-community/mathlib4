eec28ff12bf54417a0e1d124d594ea29d93d5574
Niels Voss <code@nielsvoss.com>
2026-03-13 01:21:46 -0700

    Add SingularValues.lean to Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -1934,6 +1934,7 @@ public import Mathlib.Analysis.InnerProductSpace.Projection.Submodule
 public import Mathlib.Analysis.InnerProductSpace.Rayleigh
 public import Mathlib.Analysis.InnerProductSpace.Reproducing
 public import Mathlib.Analysis.InnerProductSpace.Semisimple
+public import Mathlib.Analysis.InnerProductSpace.SingularValues
 public import Mathlib.Analysis.InnerProductSpace.Spectrum
 public import Mathlib.Analysis.InnerProductSpace.StarOrder
 public import Mathlib.Analysis.InnerProductSpace.Subspace
