c7e2b452af06e86557f2a0c69c68a1f614ce0285
tb65536 <thomas.l.browning@gmail.com>
2026-03-13 06:10:27 +0000

    explicit

--- Mathlib/Analysis/InnerProductSpace/Spectrum.lean
+++ Mathlib/Analysis/InnerProductSpace/Spectrum.lean
@@ -431,8 +431,8 @@ theorem orthogonalComplement_iSup_eigenspaces_eq_bot
 /-- The **Spectral Theorem** for compact self-adjoint operators: the eigenspaces of a compact
 self-adjoint operator are finite-dimensional. -/
 theorem finite_dimensional_eigenspace (hT : IsCompactOperator T) (μ : 𝕜) (hμ : μ ≠ 0) :
-    FiniteDimensional 𝕜 (eigenspace (T : Module.End 𝕜 E) μ) := by
-  have inv : ∀ x ∈ eigenspace (T : Module.End 𝕜 E) μ, T x ∈ eigenspace (T : Module.End 𝕜 E) μ := by
+    FiniteDimensional 𝕜 (eigenspace T.toLinearMap μ) := by
+  have inv : ∀ x ∈ eigenspace T.toLinearMap μ, T x ∈ eigenspace T.toLinearMap μ := by
     intro x hx
     rw [mem_eigenspace_iff, ContinuousLinearMap.coe_coe] at hx ⊢
     rw [hx, map_smul, hx]
