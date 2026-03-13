bc550c4a72bf8bff3d5faa8eade04d2d203a9b98
Niels Voss <code@nielsvoss.com>
2026-03-13 01:37:02 -0700

    Add copyright header

--- Mathlib/Analysis/InnerProductSpace/SingularValues.lean
+++ Mathlib/Analysis/InnerProductSpace/SingularValues.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2026 Niels Voss, Arnav Mehta. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Niels Voss, Arnav Mehta
+-/
 module
 
 public import Mathlib.Analysis.InnerProductSpace.Positive
