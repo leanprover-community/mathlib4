c71f51d20f173053ef7b13eb0488208fdf6a9d1b
Fabian Odermatt <odermatt_fabian@hotmail.com>
2026-03-14 18:34:14 +0100

    CI fail

--- Mathlib.lean
+++ Mathlib.lean
@@ -1432,7 +1432,6 @@ public import Mathlib.AlgebraicTopology.SimplicialSet.Finite
 public import Mathlib.AlgebraicTopology.SimplicialSet.FiniteColimits
 public import Mathlib.AlgebraicTopology.SimplicialSet.FiniteProd
 public import Mathlib.AlgebraicTopology.SimplicialSet.HoFunctorMonoidal
-public import Mathlib.AlgebraicTopology.SimplicialSet.Homotopy
 public import Mathlib.AlgebraicTopology.SimplicialSet.HomotopyCat
 public import Mathlib.AlgebraicTopology.SimplicialSet.Horn
 public import Mathlib.AlgebraicTopology.SimplicialSet.HornColimits
