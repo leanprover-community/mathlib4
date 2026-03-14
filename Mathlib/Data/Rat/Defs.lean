2b231bceb8e36a3eea86e29aae1a8ac308b49ff1
Riccardo Brasca <riccardo.brasca@gmail.com>
2026-03-14 20:42:38 +0100

    Rat.zero_ne_one

--- Mathlib/Data/Rat/Defs.lean
+++ Mathlib/Data/Rat/Defs.lean
@@ -148,7 +148,7 @@ variable (a b c : ℚ)
 lemma divInt_one_one : 1 /. 1 = 1 := by rw [divInt_one, Rat.intCast_one]
 
 protected theorem zero_ne_one : 0 ≠ (1 : ℚ) := by
-  rw [ne_comm, ← divInt_one_one, divInt_ne_zero] <;> lia
+  rw [ne_comm, ← divInt_one_one, divInt_ne_zero] <;> omega
 
 attribute [simp] mkRat_eq_zero
 
