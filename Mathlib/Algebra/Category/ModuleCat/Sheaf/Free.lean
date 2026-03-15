3a8c9b95b930359960afbf7eaffbc2c222c172ad
Joël Riou <joel.riou@universite-paris-saclay.fr>
2026-03-15 20:58:25 +0100

    feat(Algebra/Category/ModuleCat/Sheaf): free preserves colimits

--- Mathlib/Algebra/Category/ModuleCat/Sheaf/Free.lean
+++ Mathlib/Algebra/Category/ModuleCat/Sheaf/Free.lean
@@ -80,6 +80,11 @@ lemma freeHomEquiv_symm_comp {M N : SheafOfModules.{u} R} {I : Type u} (s : I �
 noncomputable abbrev freeSection {I : Type u} (i : I) : (free (R := R) I).sections :=
   (free (R := R) I).freeHomEquiv (𝟙 (free I)) i
 
+lemma freeHomEquiv_apply {M : SheafOfModules.{u} R} {I : Type u}
+    (f : free I ⟶ M) (i : I) :
+    freeHomEquiv M f i = sectionsMap f (freeSection i) :=
+  rfl
+
 lemma unitHomEquiv_symm_freeHomEquiv_apply
     {I : Type u} {M : SheafOfModules.{u} R} (f : free I ⟶ M) (i : I) :
     M.unitHomEquiv.symm (M.freeHomEquiv f i) = ιFree i ≫ f := by
@@ -104,6 +109,12 @@ lemma sectionMap_freeMap_freeSection (i : I) :
     sectionsMap (freeMap (R := R) f) (freeSection i) = freeSection (f i) := by
   simp [← freeHomEquiv_comp_apply]
 
+lemma sectionsMap_freeHomEquiv_symm_freeSection
+    {M : SheafOfModules.{u} R} (f : I → M.sections) (i : I) :
+    sectionsMap ((freeHomEquiv M).symm f) (freeSection i) = f i := by
+  obtain ⟨f, rfl⟩ := (freeHomEquiv M).surjective f
+  rw [Equiv.symm_apply_apply, freeHomEquiv_apply]
+
 @[reassoc (attr := simp)]
 lemma ιFree_freeMap (i : I) :
     ιFree (R := R) i ≫ freeMap f = ιFree (f i) := by
@@ -122,6 +133,46 @@ noncomputable def freeFunctor : Type u ⥤ SheafOfModules.{u} R where
   map f := freeMap f
   map_id X := (freeHomEquiv _).injective (by ext1 i; simp)
   map_comp {I J K} f g := (freeHomEquiv _).injective (by ext1; simp [freeHomEquiv_comp_apply])
+
+instance : PreservesColimitsOfSize.{v₂, u₂} (freeFunctor (R := R)) where
+  preservesColimitsOfShape {J} _ := ⟨fun {K} ↦ ⟨fun {c} hc ↦ ⟨by
+    replace hc := (Types.isColimit_iff_coconeTypesIsColimit c).1 ⟨hc⟩
+    let coconeTypes (s : Cocone (K ⋙ freeFunctor (R := R))) :
+        K.CoconeTypes :=
+      { pt := s.pt.sections
+        ι j := freeHomEquiv _ (s.ι.app j)
+        ι_naturality {j j'} f := by
+          funext x
+          dsimp
+          rw [← s.w f]
+          dsimp
+          rw [freeHomEquiv_comp_apply, freeHomEquiv_freeMap, Function.comp_apply,
+            freeHomEquiv_apply] }
+    let desc (s : Cocone (K ⋙ freeFunctor (R := R))) :
+        free c.pt ⟶ s.pt :=
+      (freeHomEquiv _).symm (hc.desc (coconeTypes s))
+    have fac (s : Cocone (K ⋙ freeFunctor (R := R))) (j : J) :
+        freeMap (c.ι.app j) ≫ desc s = s.ι.app j :=
+      (freeHomEquiv _).injective (by
+      funext x
+      dsimp [desc]
+      rw [freeHomEquiv_comp_apply, freeHomEquiv_freeMap, Function.comp_apply,
+        sectionsMap_freeHomEquiv_symm_freeSection]
+      apply hc.fac_apply)
+    exact {
+      desc := desc
+      fac := fac
+      uniq s m hm := (freeHomEquiv _).injective (by
+        funext x
+        obtain ⟨j, x, rfl⟩ := Functor.CoconeTypes.IsColimit.ι_jointly_surjective hc x
+        replace hm := congr_fun ((freeHomEquiv _).congr_arg (hm j)) x
+        dsimp at hm
+        rw [freeHomEquiv_comp_apply, freeHomEquiv_freeMap,
+          Function.comp_apply] at hm
+        rw [freeHomEquiv_apply, freeHomEquiv_apply,
+          sectionsMap_freeHomEquiv_symm_freeSection,
+          Functor.coconeTypesEquiv_symm_apply_ι,
+          dsimp% hc.fac_apply (coconeTypes s) j x, hm]) }⟩⟩⟩
 
 section
 
