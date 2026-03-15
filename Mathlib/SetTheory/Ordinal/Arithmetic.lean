2fda7ffbe86853a7d4543e8062bf190344a2acaa
Snir Broshi <26556598+SnirBroshi@users.noreply.github.com>
2026-03-15 08:18:51 +0200

    avoid `nsmul`

--- Mathlib/SetTheory/Ordinal/Arithmetic.lean
+++ Mathlib/SetTheory/Ordinal/Arithmetic.lean
@@ -535,25 +535,6 @@ theorem isSuccLimit_sub {a b : Ordinal} (ha : IsSuccPrelimit a) (h : b < a) :
   rw [add_succ]
   exact ha.succ_lt hc
 
-theorem addCommute_iff_nsmul {o₁ o₂ : Ordinal} :
-    AddCommute o₁ o₂ ↔ ∃ (o : Ordinal) (n₁ n₂ : ℕ), o₁ = n₁ • o ∧ o₂ = n₂ • o := by
-  refine ⟨fun hcomm ↦ ?_, ?_⟩
-  · induction h : o₁ + o₂ using WellFoundedLT.induction generalizing o₁ o₂ with | ind o ih
-    subst h
-    wlog hle : o₁ ≤ o₂
-    · grind [hcomm.symm]
-    rcases eq_or_ne o₁ 0 with (rfl | h₁)
-    · exact ⟨o₂, 0, 1, by simp, by simp [one_nsmul]⟩
-    let o₃ := o₂ - o₁
-    have hsub : o₁ + o₃ = o₂ := Ordinal.add_sub_cancel_of_le hle
-    have hcomm' : AddCommute o₁ o₃ := add_left_cancel (a := o₁) <| by grind
-    have hlt : o₁ + o₃ < o₁ + o₂ := by simpa [hsub, hcomm.eq] using h₁.pos
-    rcases ih _ hlt hcomm' rfl with ⟨o, n₁, n₃, hn₁, hn₃⟩
-    use o, n₁, n₁ + n₃, hn₁
-    rw [add_nsmul, ← hn₁, ← hn₃, hsub]
-  · rintro ⟨o, n₁, n₂, rfl, rfl⟩
-    rw [addCommute_iff_eq, ← add_nsmul, ← add_nsmul, add_comm]
-
 /-! ### Multiplication of ordinals -/
 
 /-- The multiplication of ordinals `o₁` and `o₂` is the (well-founded) lexicographic order on
@@ -797,12 +778,22 @@ theorem add_mul_of_isSuccLimit {a b c : Ordinal} (ba : b + a = a) (l : IsSuccLim
 
 theorem addCommute_iff_mul {o₁ o₂ : Ordinal} :
     AddCommute o₁ o₂ ↔ ∃ (o : Ordinal) (n₁ n₂ : ℕ), o₁ = o * n₁ ∧ o₂ = o * n₂ := by
-  rw [addCommute_iff_nsmul]
-  refine ⟨?_, ?_⟩
-    <;> rintro ⟨o, n₁, n₂, rfl, rfl⟩
-    <;> use o, n₁, n₂
-    <;> constructor
-    <;> rw [smul_eq_mul]
+  refine ⟨fun hcomm ↦ ?_, ?_⟩
+  · induction h : o₁ + o₂ using WellFoundedLT.induction generalizing o₁ o₂ with | ind o ih
+    subst h
+    wlog hle : o₁ ≤ o₂
+    · grind [hcomm.symm]
+    rcases eq_or_ne o₁ 0 with (rfl | h₁)
+    · exact ⟨o₂, 0, 1, by simp, by simp⟩
+    let o₃ := o₂ - o₁
+    have hsub : o₁ + o₃ = o₂ := Ordinal.add_sub_cancel_of_le hle
+    have hcomm' : AddCommute o₁ o₃ := add_left_cancel (a := o₁) <| by grind
+    have hlt : o₁ + o₃ < o₁ + o₂ := by simpa [hsub, hcomm.eq] using h₁.pos
+    rcases ih _ hlt hcomm' rfl with ⟨o, n₁, n₃, hn₁, hn₃⟩
+    use o, n₁, n₁ + n₃, hn₁
+    rw [Nat.cast_add, mul_add, ← hn₁, ← hn₃, hsub]
+  · rintro ⟨o, n₁, n₂, rfl, rfl⟩
+    rw [addCommute_iff_eq, ← mul_add, ← mul_add, ← Nat.cast_add, add_comm, Nat.cast_add]
 
 /-! ### Division on ordinals -/
 
