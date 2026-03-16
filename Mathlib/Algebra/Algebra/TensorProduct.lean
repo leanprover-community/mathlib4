238eb4b5719a8e5e388c9bff93343d0c8fa3faff
leo <leonid.ryvkin@rub.de>
2026-03-16 23:36:44 +0100

    typo correced

--- Mathlib/Algebra/Algebra/TensorProduct.lean
+++ Mathlib/Algebra/Algebra/TensorProduct.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2026 Leonid Ryvkin. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Leonid Ryvkin.
+Authors: Leonid Ryvkin
 -/
 module
 
