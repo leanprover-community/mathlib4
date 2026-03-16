72e3a1a7d6f3e02aa1d4c5ecd2f2719cb06e4150
Martin Winter <martin.winter.math@gmail.com>
2026-03-16 08:17:23 +0100

    Add copyright

--- Mathlib/Geometry/Convex/Cone/DualFinite.lean
+++ Mathlib/Geometry/Convex/Cone/DualFinite.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2026 Martin Winter. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Martin Winter
+-/
 module
 
 public import Mathlib.Geometry.Convex.Cone.Dual
