6fa9f4b64ec02fb32047acefe233368adf706aa4
Etienne Marion <baguettes_casino0c@icloud.com>
2026-03-16 11:06:02 +0100

    wip

---
+++ Mathlib/Probability/Independence/Process/HasIndepIncrements/IsGaussianProcess.lean
@@ -0,0 +1,146 @@
+/-
+Copyright (c) 2025 Etienne Marion. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Etienne Marion
+-/
+module
+
+public import Mathlib
+
+open MeasureTheory
+
+variable {T Ω E : Type*} {mΩ : MeasurableSpace Ω} {P : Measure Ω}
+
+namespace Finset
+
+/-! ### Indexing the elements of a finset in order -/
+
+variable [LinearOrder T] (I : Finset T)
+
+/-- Given a finite set `I : Finset T` of cardinality `n`, `ofFin : Fin #I → T`
+is the map `(t₁, ..., tₙ)`, where `t₁ < ... < tₙ` are the elements of `I`. -/
+noncomputable def ofFin (i : Fin #I) : T :=
+  (I.sort (· ≤ ·)).get (Fin.cast (I.length_sort (· ≤ ·)).symm i)
+
+lemma monotone_ofFin : Monotone I.ofFin :=
+  fun i j hij ↦ (I.pairwise_sort (· ≤ ·)).rel_get_of_le (by grind)
+
+lemma ofFin_mem (i : Fin #I) : I.ofFin i ∈ I := I.mem_sort (· ≤ ·) |>.1 <| List.get_mem _ _
+
+/-- Given a finite set `I : Finset T`, and `t : I`,
+`I.toFin t` returns the position of `t` in `I`. -/
+noncomputable def toFin (i : I) : Fin #I :=
+  haveI : NeZero #I := ⟨Nonempty.card_ne_zero ⟨i.1, i.2⟩⟩
+  Fin.ofNat #I ((I.sort (· ≤ ·)).idxOf i.1)
+
+@[simp]
+lemma ofFin_toFin (i : I) : I.ofFin (I.toFin i) = i.1 := by
+  rw [toFin, ofFin]
+  nth_rw 2 [← (I.sort (· ≤ ·)).idxOf_get (a := i.1)]
+  · congr
+    ext
+    simp only [Fin.ofNat_eq_cast, Fin.val_cast, Fin.val_natCast]
+    rw [Nat.mod_eq_of_lt]
+    rw [← I.length_sort (· ≤ ·)]
+    exact List.idxOf_lt_length_of_mem <| I.mem_sort (· ≤ ·) |>.2 i.2
+  exact List.idxOf_lt_length_of_mem <| I.mem_sort (· ≤ ·) |>.2 i.2
+
+@[simp]
+lemma toFin_ofFin (i : Fin #I) : I.toFin ⟨I.ofFin i, ofFin_mem I i⟩ = i := by
+  simp_rw [toFin, ofFin, List.get_idxOf (sort_nodup ..)]
+  simp
+
+section Bot
+
+variable [Bot T]
+
+/-- Given a finite set `I : Finset T` of cardinality `n`, `ofFin' : Fin (#I + 1) → T`
+is the map `(⊥, t₁, ..., tₙ)`, where `t₁ < ... < tₙ` are the elements of `I`. -/
+noncomputable def ofFin' (i : Fin (#I + 1)) : T :=
+  if h : i = 0
+    then ⊥
+    else I.ofFin (i.pred h)
+
+@[simp]
+lemma ofFin'_zero : I.ofFin' 0 = ⊥ := rfl
+
+lemma ofFin'_of_ne_zero (i : Fin (#I + 1)) (hi : i ≠ 0) :
+    I.ofFin' i = I.ofFin (i.pred hi) := by rw [ofFin', dif_neg hi]
+
+@[simp]
+lemma ofFin'_succ (i : Fin #I) :
+    I.ofFin' i.succ = I.ofFin i := by
+  rw [ofFin'_of_ne_zero, Fin.pred_succ]
+  simp
+
+lemma ofFin'_mem (i : Fin (#I + 1)) (hi : i ≠ 0) : I.ofFin' i ∈ I := by
+  rw [ofFin'_of_ne_zero _ _ hi]
+  exact I.ofFin_mem _
+
+end Bot
+
+lemma monotone_ofFin' [OrderBot T] : Monotone (I.ofFin') := by
+  intro i j hij
+  obtain rfl | hi := eq_or_ne i 0
+  · simp
+  rw [ofFin'_of_ne_zero I i hi, ofFin'_of_ne_zero I j (by grind)]
+  apply monotone_ofFin
+  simpa
+
+end Finset
+
+public section
+
+
+
+/-- `incrementsToRestrict I` is a continuous linear map `f` such that
+`f (xₜ₁, xₜ₂ - xₜ₁, ..., xₜₙ - xₜₙ₋₁) = (xₜ₁, ..., xₜₙ)`. -/
+noncomputable def incrementsToRestrict [LinearOrder T] (R : Type*) [Semiring R] [AddCommMonoid E]
+    [Module R E] [TopologicalSpace E] [ContinuousAdd E] (I : Finset T) :
+    (Fin #I → E) →L[R] (I → E) :=
+  { toFun x i := ∑ j ≤ I.toFin i, x j
+    map_add' x y := by ext; simp [sum_add_distrib]
+    map_smul' m x := by ext; simp [smul_sum]
+    cont := by fun_prop }
+
+lemma incrementsToRestrict_increments_ofFin'_ae_eq_restrict [LinearOrder T] (R : Type*) [OrderBot T]
+    [Semiring R] [AddCommGroup E] [Module R E] [TopologicalSpace E] [ContinuousAdd E]
+    {X : T → Ω → E} (h : ∀ᵐ ω ∂P, X ⊥ ω = 0) (I : Finset T) :
+    (fun ω ↦ I.restrict (X · ω)) =ᵐ[P]
+      (incrementsToRestrict R I) ∘
+        (fun ω i ↦ X (I.ofFin' i.succ) ω - X (I.ofFin' i.castSucc) ω) := by
+  filter_upwards [h] with ω hω
+  ext t
+  simp only [restrict, incrementsToRestrict, ContinuousLinearMap.coe_mk', LinearMap.coe_mk,
+    AddHom.coe_mk, Function.comp_apply]
+  rw [Fin.sum_Iic_sub (I.toFin t) (fun j ↦ X (I.ofFin' j) ω)]
+  simp [hω]
+
+/-- A stochastic process `X` with independent increments and such that `X t` is gaussian for
+all `t` is a Gaussian process. -/
+lemma HasIndepIncrements.isGaussianProcess [LinearOrder T] [OrderBot T]
+    [NormedAddCommGroup E] [NormedSpace ℝ E] [MeasurableSpace E] [BorelSpace E]
+    [SecondCountableTopology E] [CompleteSpace E]
+    {X : T → Ω → E} (law : ∀ t, HasGaussianLaw (X t) P) (h_bot : ∀ᵐ ω ∂P, X ⊥ ω = 0)
+    (incr : HasIndepIncrements X P) :
+    IsGaussianProcess X P where
+  hasGaussianLaw I := by
+    have := (law ⊥).isProbabilityMeasure
+    obtain rfl | hI := I.eq_empty_or_nonempty
+    · constructor
+      have : P.map (fun ω ↦ Finset.restrict ∅ fun x ↦ X x ω) = .dirac Classical.ofNonempty := by
+        ext s -
+        apply Subsingleton.set_cases (p := fun s ↦ Measure.map _ _ s = _)
+        · simp
+        simp only [measure_univ]
+        exact @measure_univ _ _ _
+          (Measure.isProbabilityMeasure_map (aemeasurable_pi_lambda _ fun _ ↦ (law _).aemeasurable))
+      rw [this]
+      infer_instance
+    have := incrementsToRestrict_increments_ofFin'_ae_eq_restrict ℝ h_bot I
+    refine @HasGaussianLaw.congr _ _ _ _ _ _ _ _ _ _ ?_ this.symm
+    refine HasGaussianLaw.map ?_ _
+    exact (incr _ _ (monotone_ofFin' I)).hasGaussianLaw fun i ↦
+      incr.indepFun_eval_sub bot_le
+        (monotone_ofFin' I (Fin.castSucc_le_succ i)) h_bot
+          |>.hasGaussianLaw_sub_of_sub (law _) (law _)
