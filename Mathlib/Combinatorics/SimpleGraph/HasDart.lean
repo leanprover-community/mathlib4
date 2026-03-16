7a76b3350786661c418f33f6bb69e40a7051a875
Jun2M <rnjsgurwns27@gmail.com>
2026-03-16 19:08:04 -0400

    minor cleanup

--- Mathlib/Combinatorics/SimpleGraph/HasDart.lean
+++ Mathlib/Combinatorics/SimpleGraph/HasDart.lean
@@ -16,12 +16,10 @@ In this file we make `SimpleGraph` an instance of `HasDart`.
 
 variable {α : Type*}
 
-variable {Gr : Type*} [HasDart α Gr] {G : Gr} {u v : α}
-
 instance : HasDart α (SimpleGraph α) where
   verts _ := Set.univ
   Adj G := G.Adj
-  left_mem_verts_of_adj _ {u _ : α} _ := Set.mem_univ u
-  right_mem_verts_of_adj _ {_ v : α} _ := Set.mem_univ v
   dart G u v := G.Adj u v
   nonempty_dart_iff_adj := nonempty_prop
+  left_mem_verts_of_adj _ {u _ : α} _ := Set.mem_univ u
+  right_mem_verts_of_adj _ {_ v : α} _ := Set.mem_univ v
