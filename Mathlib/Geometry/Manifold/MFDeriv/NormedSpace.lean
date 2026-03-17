8bf6ea3a4469bf28a4cd083e9906eca1789fcc3a
Michael Rothgang <rothgang@math.uni-bonn.de>
2026-03-17 20:22:26 +0100

    chore: trivially generalise extDerivFun to codomain normed spaces

--- Mathlib/Geometry/Manifold/MFDeriv/NormedSpace.lean
+++ Mathlib/Geometry/Manifold/MFDeriv/NormedSpace.lean
@@ -398,20 +398,20 @@ end smul
 /-! ### Exterior derivative of a scalar function -/
 
 /-- The exterior derivative of a scalar function on `M`, as a section of the cotangent bundle. -/
-noncomputable abbrev extDerivFun (g : M → 𝕜) :
-    Π x : M, TangentSpace I x →L[𝕜] 𝕜 :=
+noncomputable abbrev extDerivFun (g : M → F) :
+    Π x : M, TangentSpace I x →L[𝕜] F :=
   fun x ↦ (NormedSpace.fromTangentSpace <| g x).toContinuousLinearMap ∘L (mfderiv% g x)
 
 @[simp]
-lemma extDerivFun_add {g g' : M → 𝕜} {x : M} (hg : MDiffAt g x) (hg' : MDiffAt g' x) :
+lemma extDerivFun_add {g g' : M → F} {x : M} (hg : MDiffAt g x) (hg' : MDiffAt g' x) :
     extDerivFun (g + g') x = extDerivFun (I := I) g x + extDerivFun g' x := by
   simp [extDerivFun, mfderiv_add hg hg']
   congr
 
 @[simp]
-lemma extDerivFun_zero {x : M} : extDerivFun (I := I) (0 : M → 𝕜) x = 0 := by
-  have : extDerivFun (0 : M → 𝕜) x + extDerivFun (0 : M → 𝕜) x =
-      extDerivFun (I := I) (0 : M → 𝕜) x := by
+lemma extDerivFun_zero {x : M} : extDerivFun (I := I) (0 : M → F) x = 0 := by
+  have : extDerivFun (0 : M → F) x + extDerivFun (0 : M → F) x =
+      extDerivFun (I := I) (0 : M → F) x := by
     rw [← extDerivFun_add (by exact mdifferentiable_const ..) (by exact mdifferentiable_const ..)]
     simp
   simpa using this
