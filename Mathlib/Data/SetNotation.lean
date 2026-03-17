bc2565f1c8d5f154ba909184ca13f9568ac039b3
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2026-03-17 21:14:56 +0000

    new and improved elaborator

--- Mathlib/Data/SetNotation.lean
+++ Mathlib/Data/SetNotation.lean
@@ -6,6 +6,7 @@ Authors: Jovan Gerbscheid
 module
 
 public meta import Mathlib.Lean.PrettyPrinter.Delaborator
+public meta import Lean.Elab.Extra
 
 /-!
 # Set Notation
@@ -31,14 +32,46 @@ public meta section
 
 namespace Mathlib.Meta
 
-open Lean Elab Term PrettyPrinter.Delaborator SubExpr
+open Lean Meta Elab Term PrettyPrinter.Delaborator SubExpr
+
+/-- The `@[use_set_notation]` attribute marks that order notation on the given type should be
+pretty printed using set-style notation, i.e. `⊆` instead of `≤`. -/
+initialize setNotationExt : TagDeclarationExtension ← mkTagDeclarationExtension
+
+@[inherit_doc setNotationExt]
+initialize
+  registerBuiltinAttribute {
+    name  := `use_set_notation
+    descr := "use set notation for this type"
+    add   := fun declName _stx kind => do
+      unless kind == .global do
+        throwAttrMustBeGlobal `coe kind
+      modifyEnv (setNotationExt.tag · declName) }
+
+/-- Whether to use set notation for the given type or not. -/
+def useSetNotationFor (type : Expr) : MetaM Bool := do
+  let .const n _ := (← whnfR type).getAppFn | return false
+  return setNotationExt.isTagged (← getEnv) n
 
 /-! ## Elaboration -/
 
-private def mkSubsetStx (a b : Term) : CoreM Term := `($a ⊆ $b)
-private def mkSsubsetStx (a b : Term) : CoreM Term := `($a ⊂ $b)
-private def mkSupsetStx (a b : Term) : CoreM Term := `($a ⊇ $b)
-private def mkSsupsetStx (a b : Term) : CoreM Term := `($a ⊃ $b)
+/-- This relation is an implementation detail of the `⊆` elatorator. -/
+opaque SubsetElabAux.{u} {α : Type u} : α → α → Prop
+
+def elabSubsetLike (x y : Term) (le leCls sub subCls : Name) (expectedType? : Option Expr) :
+    TermElabM Expr := do
+  let rel ← `(SubsetElabAux $x $y)
+  let e ← withSynthesize (postpone := .yes) <| elabApp rel expectedType?
+  let mkApp3 (.const ``SubsetElabAux [u]) α x y := e
+    | throwError "unexpected result {e} when elaborating {rel}"
+  -- If the type cannot be determined yet, we postpone elaboration until it is known.
+  tryPostponeIfMVar α
+  if ← useSetNotationFor α then
+    let inst ← mkInstMVar <| .app (.const leCls [u]) α
+    return mkApp4 (.const le [u]) α inst x y
+  else
+    let inst ← mkInstMVar <| .app (.const subCls [u]) α
+    return mkApp4 (.const sub [u]) α inst x y
 
 /-- Subset relation: `a ⊆ b`, but overwritten  -/
 syntax:50 (name := subsetStx') (priority := high) term:51 " ⊆ " term:51 : term
@@ -49,46 +82,32 @@ syntax:50 (name := supsetStx') (priority := high) term:51 " ⊇ " term:51 : term
 /-- Strict superset relation: `a ⊃ b`  -/
 syntax:50 (name := ssupsetStx') (priority := high) term:51 " ⊃ " term:51 : term
 
-/-- First try to elaborate `stx₁`, and if that doesn't succeed immediately, fall back on
-elaborating `stx₂`.
-
-Note that we call `elabTermAndSynthesize` on `stx₁`, so if there are any stuck type class
-constraints due to not (yet) filled in metavariables, we will discard the `stx₁` option. -/
-private def elabTermOrTerm (stx₁ stx₂ : Term) (expectedType? : Option Expr) : TermElabM Expr := do
-  let s ← saveState
-  try
-    withoutErrToSorry do elabTermAndSynthesize stx₁ expectedType?
-  catch _ =>
-    s.restore
-    elabTerm stx₂ expectedType?
-
 /-- Elaborator for `x ⊆ y` notation. -/
 @[term_elab subsetStx']
-def elabSubsetStx' : TermElab := fun
-  | `($x ⊆ $y), expectedType? => do
-    -- Note we use `LE.le x y` instead of `x ≤ y` in order to avoid `binrel%`
-    elabTermOrTerm (← mkSubsetStx x y) (← `(LE.le $x $y)) expectedType?
+def elabSubsetStx' : TermElab
+  | `($x ⊆ $y), expectedType? =>
+    elabSubsetLike x y ``LE.le ``LE ``Subset ``HasSubset expectedType?
   | _, _ => throwUnsupportedSyntax
 
 /-- Elaborator for `x ⊂ y` notation. -/
 @[term_elab ssubsetStx']
-def elabSsubsetStx' : TermElab := fun
-  | `($x ⊂ $y), expectedType? => do
-    elabTermOrTerm (← mkSsubsetStx x y) (← `(LT.lt $x $y)) expectedType?
+def elabSsubsetStx' : TermElab
+  | `($x ⊂ $y), expectedType? =>
+    elabSubsetLike x y ``LT.lt ``LT ``SSubset ``HasSSubset expectedType?
   | _, _ => throwUnsupportedSyntax
 
 /-- Elaborator for `x ⊇ y` notation. -/
 @[term_elab supsetStx']
-def elabSupsetStx' : TermElab := fun
-  | `($x ⊇ $y), expectedType? => do
-    elabTermOrTerm (← mkSupsetStx x y) (← `(GE.ge $x $y)) expectedType?
+def elabSupsetStx' : TermElab
+  | `($x ⊇ $y), expectedType? =>
+    elabSubsetLike x y ``GE.ge ``LE ``Superset ``HasSubset expectedType?
   | _, _ => throwUnsupportedSyntax
 
 /-- Elaborator for `x ⊃ y` notation. -/
 @[term_elab ssupsetStx']
-def elabSsupsetStx' : TermElab := fun
-  | `($x ⊃ $y), expectedType? => do
-    elabTermOrTerm (← mkSsupsetStx x y) (← `(GT.gt $x $y)) expectedType?
+def elabSsupsetStx' : TermElab
+  | `($x ⊃ $y), expectedType? =>
+    elabSubsetLike x y ``GT.gt ``LT ``SSuperset ``HasSSubset expectedType?
   | _, _ => throwUnsupportedSyntax
 
 /-- Declare `∀ x ⊆ y, ...` as syntax for `∀ x, x ⊆ y → ...` and `∃ x ⊆ y, ...` as syntax for
@@ -110,59 +129,41 @@ binder_predicate (priority := high) x " ⊃ " y:term => `($x ⊃ $y)
 
 /-! ## Delaboration -/
 
-/-- The `@[use_set_notation]` attribute marks that order notation on the given type should be
-pretty printed using set-style notation, i.e. `⊆` instead of `≤`. -/
-initialize setNotationExt : TagDeclarationExtension ← mkTagDeclarationExtension
-
-@[inherit_doc setNotationExt]
-initialize
-  registerBuiltinAttribute {
-    name  := `use_set_notation
-    descr := "use set notation for this type"
-    add   := fun declName _stx kind => do
-      unless kind == .global do
-        throwAttrMustBeGlobal `coe kind
-      modifyEnv (setNotationExt.tag · declName) }
-
 /-- Delaborate `x ≤ y` into `x ⊆ y` if the type is tagged with `@[use_set_notation]`. -/
-@[delab app.LE.le]
+@[app_delab LE.le]
 def delabLe : Delab := whenNotPPOption getPPExplicit <| whenPPOption getPPNotation do
   let_expr LE.le α _ _ _ := ← getExpr | failure
-  let .const t _ := (← instantiateMVars α).getAppFn' | failure
-  unless setNotationExt.isTagged (← getEnv) t do failure
+  guard <| ← useSetNotationFor α
   let x ← withNaryArg 2 delab
   let y ← withNaryArg 3 delab
   let stx ← `($x ⊆ $y)
   annotateGoToDef stx `Mathlib.Meta.delabLe
 
 /-- Delaborate `x < y` into `x ⊂ y` if the type is tagged with `@[use_set_notation]`. -/
-@[delab app.LT.lt]
+@[app_delab LT.lt]
 def delabLt : Delab := whenNotPPOption getPPExplicit <| whenPPOption getPPNotation do
   let_expr LT.lt α _ _ _ := ← getExpr | failure
-  let .const t _ := (← instantiateMVars α).getAppFn' | failure
-  unless setNotationExt.isTagged (← getEnv) t do failure
+  guard <| ← useSetNotationFor α
   let x ← withNaryArg 2 delab
   let y ← withNaryArg 3 delab
   let stx ← `($x ⊂ $y)
   annotateGoToDef stx `Mathlib.Meta.delabLt
 
 /-- Delaborate `x ≥ y` into `x ⊇ y` if the type is tagged with `@[use_set_notation]`. -/
-@[delab app.GE.ge]
+@[app_delab GE.ge]
 def delabGe : Delab := whenNotPPOption getPPExplicit <| whenPPOption getPPNotation do
   let_expr GE.ge α _ _ _ := ← getExpr | failure
-  let .const t _ := (← instantiateMVars α).getAppFn' | failure
-  unless setNotationExt.isTagged (← getEnv) t do failure
+  guard <| ← useSetNotationFor α
   let x ← withNaryArg 2 delab
   let y ← withNaryArg 3 delab
   let stx ← `($x ⊇ $y)
   annotateGoToDef stx `Mathlib.Meta.delabGe
 
 /-- Delaborate `x > y` into `x ⊃ y` if the type is tagged with `@[use_set_notation]`. -/
-@[delab app.GT.gt]
+@[app_delab GT.gt]
 def delabGt : Delab := whenNotPPOption getPPExplicit <| whenPPOption getPPNotation do
   let_expr GT.gt α _ _ _ := ← getExpr | failure
-  let .const t _ := (← instantiateMVars α).getAppFn' | failure
-  unless setNotationExt.isTagged (← getEnv) t do failure
+  guard <| ← useSetNotationFor α
   let x ← withNaryArg 2 delab
   let y ← withNaryArg 3 delab
   let stx ← `($x ⊃ $y)
