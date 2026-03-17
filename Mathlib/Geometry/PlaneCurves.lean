f5c645dad7704a4a9b4e963d355fa3e54f71510e
michael-novak-math <michael.novak@mail.huji.ac.il>
2026-03-17 19:22:33 +0200

    Update Mathlib/Geometry/PlaneCurves.lean
    Co-authored-by: Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>

--- Mathlib/Geometry/PlaneCurves.lean
+++ Mathlib/Geometry/PlaneCurves.lean
@@ -101,7 +101,7 @@ def frameAt {I : Set ℝ} {c : ℝ → EuclideanSpace ℝ (Fin 2)} (hc : ∀ t �
     · intro i
       rcases eq_or_ne i 0 with h | h
       · simp only [h, Fin.isValue]; exact hc t ht
-      · have h' : i=1 := Fin.eq_one_of_ne_zero i h
+      · have h' : i = 1 := Fin.eq_one_of_ne_zero i h
         simp only [h', Fin.isValue]; exact normal_is_unit_of_unit_speed hc ht
     · intro i j hinej
       rcases (eq_or_ne i 0) with h | h
