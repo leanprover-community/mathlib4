8cafb5015dde11c51bb35f7abc0eac03c76c9d24
Rida Hamadani <mridahamadani@gmail.com>
2026-03-17 02:10:56 +0100

    Apply suggestions from code review
    Co-authored-by: Vlad Tsyrklevich <vlad902@gmail.com>

--- Mathlib/Data/List/Nodup.lean
+++ Mathlib/Data/List/Nodup.lean
@@ -241,7 +241,7 @@ theorem nodup_concat (l : List α) (u : α) : (l.concat u).Nodup ↔ u ∉ l ∧
   simp
 
 lemma Nodup.nodup_tail {l : List α} (h : Nodup l) : Nodup l.tail :=
-    (tail_sublist l).nodup h
+  (tail_sublist l).nodup h
 
 lemma nodup_tail_reverse (l : List α) (h : l[0]? = l.getLast?) :
     Nodup l.reverse.tail ↔ Nodup l.tail := by
