88d015d8a247b94dcf93c8d2a3e630c60d32de60
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2026-03-18 15:50:10 -0300

    Remove degree versions

--- Mathlib/Combinatorics/SimpleGraph/Connectivity/Connected.lean
+++ Mathlib/Combinatorics/SimpleGraph/Connectivity/Connected.lean
@@ -193,14 +193,6 @@ lemma Reachable.nonempty_neighborSet_right {G : SimpleGraph V} {u v : V} (huv : 
     (hreach : G.Reachable u v) : (G.neighborSet v).Nonempty :=
   hreach.symm.nonempty_neighborSet_left huv.symm
 
-lemma Reachable.degree_pos_left {G : SimpleGraph V} {u v : V} [Fintype (G.neighborSet u)]
-    (huv : u ≠ v) (hreach : G.Reachable u v) : 0 < G.degree u :=
-  degree_pos_iff_nonempty.mpr (hreach.nonempty_neighborSet_left huv)
-
-lemma Reachable.degree_pos_right {G : SimpleGraph V} {u v : V} [Fintype (G.neighborSet v)]
-    (huv : u ≠ v) (hreach : G.Reachable u v) : 0 < G.degree v :=
-  hreach.symm.degree_pos_left huv.symm
-
 lemma not_reachable_of_neighborSet_left_eq_empty {G : SimpleGraph V} {u v : V} (huv : u ≠ v)
     (hu : G.neighborSet u = ∅) : ¬G.Reachable u v :=
   (Reachable.nonempty_neighborSet_left huv).mt (Set.not_nonempty_iff_eq_empty.mpr hu)
@@ -208,15 +200,6 @@ lemma not_reachable_of_neighborSet_left_eq_empty {G : SimpleGraph V} {u v : V} (
 lemma not_reachable_of_right_neighborSet_eq_empty {G : SimpleGraph V} {u v : V} (huv : u ≠ v)
     (hv : G.neighborSet v = ∅) : ¬G.Reachable u v :=
   fun r ↦ not_reachable_of_neighborSet_left_eq_empty huv.symm hv r.symm
-
-lemma not_reachable_of_left_degree_zero {G : SimpleGraph V} {u v : V} [Fintype (G.neighborSet u)]
-    (huv : u ≠ v) (hu : G.degree u = 0) : ¬G.Reachable u v :=
-  (Reachable.degree_pos_left huv).mt (by simp [hu])
-
-lemma not_reachable_of_right_degree_zero {G : SimpleGraph V} {u v : V} [Fintype (G.neighborSet v)]
-    (huv : u ≠ v) (hu : G.degree v = 0) : ¬G.Reachable u v := by
-  rw [reachable_comm]
-  exact not_reachable_of_left_degree_zero huv.symm hu
 
 /-- The equivalence relation on vertices given by `SimpleGraph.Reachable`. -/
 @[implicit_reducible]
