8f72b00e51e3efe5fd78d7729b1eaf79f97d1430
Hang Lu Su <hanglu.su@pm.me>
2026-03-18 16:03:15 +0100

    wip still trying to figure out the core defs

---
+++ Mathlib/GroupTheory/Order/Orderable.lean
@@ -0,0 +1,35 @@
+/-
+Copyright (c) Hang Lu Su. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Hang Lu Su
+-/
+module
+
+public import Mathlib.Algebra.Group.Defs
+public import Mathlib.Algebra.Order.Monoid.Unbundled.Defs
+public import Mathlib.Order.Defs.Unbundled
+
+/-!
+# Left-orderable groups
+This file defines left-orderable groups.
+
+## Main declarations
+`IsLeftOrderable` defines when a group is left-orderable.
+
+## To-do
+* `IsRightOrderedGroup`, `IsRightOrderableGroup`
+* `BiOrderedGroup`, ``IsRightOrderedGroup`
+* Positive cone equivalences
+
+-/
+
+@[expose] public section
+
+/-- A left-ordered group `G` is a group equipped with a strict total order `<` such that
+left-multiplication preserves the inequality relation. -/
+abbrev IsLeftOrderedGroup (G : Type*) [Group G] [LT G] [IsStrictTotalOrder G (· < ·)] : Prop :=
+  MulLeftStrictMono G
+
+/-- A group is left-orderable if it admits some left-invariant strict total order. -/
+def IsLeftOrderableGroup (G : Type*) [Group G] : Prop :=
+  ∃ h : LT G, letI := h; ∃ _ : IsStrictTotalOrder G (· < ·), IsLeftOrderedGroup G
