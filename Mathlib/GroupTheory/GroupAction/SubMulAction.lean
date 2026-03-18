0e8d68b42ce94e22092fb706c68d8bb8ece61288
sharky564 <sharkesa@gmail.com>
2026-03-19 01:23:56 +1100

    Updated proofs using suggestions from review

--- Mathlib/GroupTheory/GroupAction/SubMulAction.lean
+++ Mathlib/GroupTheory/GroupAction/SubMulAction.lean
@@ -143,8 +143,8 @@ lemma smul_subset_self {S R M : Type*} [SetLike S M] [SMul R M] [SMulMemClass S 
 
 open scoped Pointwise in
 @[to_additive (attr := simp)]
-lemma units_smul {S R M : Type*} [SetLike S M] [Monoid R] [MulAction R M]
-    [SMulMemClass S R M] (s : S) (r : Rˣ) : r • s = (s : Set M) := by
+lemma units_smul {S R M : Type*} [SetLike S M] [Monoid R] [MulAction R M] [SMulMemClass S R M]
+    (s : S) (r : Rˣ) : r • s = (s : Set M) := by
   apply subset_antisymm (smul_subset_self _ s)
   rintro x hx
   exact ⟨r⁻¹ • x, SMulMemClass.smul_mem (↑r⁻¹ : R) hx, by simp [← Units.smul_def]⟩
