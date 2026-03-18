0f74530333ef124a92a72bff1316e734938dbf23
yuanyi-350 <kysyy1@126.com>
2026-03-18 09:42:23 +0800

    min_imports

--- Mathlib/Analysis/Calculus/FDeriv/Basic.lean
+++ Mathlib/Analysis/Calculus/FDeriv/Basic.lean
@@ -5,7 +5,6 @@ Authors: Jeremy Avigad, Sébastien Gouëzel, Yury Kudryashov
 -/
 module
 
-public import Mathlib.Analysis.Asymptotics.Defs
 public import Mathlib.Analysis.Asymptotics.AsymptoticEquivalent
 public import Mathlib.Analysis.Calculus.FDeriv.Defs
 public import Mathlib.Analysis.Normed.Operator.Asymptotics
