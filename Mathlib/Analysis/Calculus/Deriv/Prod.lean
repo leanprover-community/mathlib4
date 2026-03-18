29db69aeb98e3540a3a59fbcf684c83bd413221c
michael-novak-math <michael.novak@mail.huji.ac.il>
2026-03-18 13:32:21 +0200

    applying a fix for the `Fintype` warning (might be temporary)

--- Mathlib/Analysis/Calculus/Deriv/Prod.lean
+++ Mathlib/Analysis/Calculus/Deriv/Prod.lean
@@ -100,19 +100,21 @@ theorem deriv_pi (h : ∀ i, DifferentiableAt 𝕜 (fun x => φ x i) x) :
   simp only [deriv, fderiv_pi h]
   simp
 
-theorem hasDerivWithinAt_pi_piLp [Fintype ι] {p : ENNReal} [Fact (1 ≤ p)] {f : 𝕜 → PiLp p E'}
+theorem hasDerivWithinAt_pi_piLp [Finite ι] {p : ENNReal} [Fact (1 ≤ p)] {f : 𝕜 → PiLp p E'}
     {f' : PiLp p E'} {s : Set 𝕜} {x : 𝕜} :
+    letI : Fintype ι := Fintype.ofFinite ι
     HasDerivWithinAt f f' s x ↔ ∀ i, HasDerivWithinAt (fun t ↦ f t i) (f' i) s x := by
-  have := Fintype.ofFinite ι
+  letI : Fintype ι := Fintype.ofFinite ι
   have hd : HasDerivWithinAt f f' s x ↔ HasFDerivWithinAt f (ContinuousLinearMap.smulRight
                  (1 : 𝕜 →L[𝕜] 𝕜) f') s x := hasDerivWithinAt_iff_hasFDerivWithinAt
   rw [hd, hasFDerivWithinAt_piLp]
   congr! 3
 
-theorem hasDerivWithinAt_pi_euclidean [Fintype ι] {f : 𝕜 → EuclideanSpace 𝕜 ι}
+theorem hasDerivWithinAt_pi_euclidean [Finite ι] {f : 𝕜 → EuclideanSpace 𝕜 ι}
     {f' : EuclideanSpace 𝕜 ι} {s : Set 𝕜} {x : 𝕜} :
+    letI : Fintype ι := Fintype.ofFinite ι
     HasDerivWithinAt f f' s x ↔ ∀ i, HasDerivWithinAt (fun t ↦  f t i) (f' i) s x := by
-  have := Fintype.ofFinite ι
+  letI : Fintype ι := Fintype.ofFinite ι
   convert hasDerivWithinAt_pi_piLp using 1
   · infer_instance
   · exact ⟨by norm_num⟩
