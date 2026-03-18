f1b0e3f7230d1c5e8449b208f58f68265d97e3f4
Leonid Ryvkin <ryvkin@gmx.com>
2026-03-18 21:16:37 +0100

    Update Mathlib/Algebra/Lie/Derivation/BaseChange.lean
    Co-authored-by: Oliver Nash <7734364+ocfnash@users.noreply.github.com>

--- Mathlib/Algebra/Lie/Derivation/BaseChange.lean
+++ Mathlib/Algebra/Lie/Derivation/BaseChange.lean
@@ -32,51 +32,40 @@ variable {A : Type*} [CommRing A] [Algebra R A]
 variable {L : Type*} [LieRing L] [LieAlgebra R L]
 
 variable (L) in
-/-- The Lie Derivation of `A ⊗[R] L` induced by a derivation of `A` -/
-def BaseChangeDer (d : Derivation R A A) : LieDerivation R (A ⊗[R] L) (A ⊗[R] L) where
-  toFun := d.toLinearMap.rTensor L
-  map_add' := by simp
-  map_smul' := by simp
-  leibniz' x y := by
-    simp only [LinearMap.coe_mk, AddHom.coe_mk]
-    refine x.induction_on (by simp) ?_ ?_
-    · intros _ l
-      refine y.induction_on (by simp) ?_ ?_
-      · intros _ l'
-        rw [←sub_eq_zero]
-        simp only [LieAlgebra.ExtendScalars.bracket_tmul, LinearMap.rTensor_tmul,
-          Derivation.coeFn_coe, Derivation.leibniz, smul_eq_mul, add_tmul]
-        rw [←(lie_skew l' l), tmul_neg]
-        abel_nf
-      · intros _ _ h1 h2
-        rw [←sub_eq_zero]
-        simp [h1, h2]
-        abel_nf
-    · intros _ _ h1 h2
-      rw [←sub_eq_zero]
-      simp [h1, h2]
-      abel_nf
-
-@[simp]
-lemma BaseChangeDer_apply (d : Derivation R A A) (x : A ⊗[R] L) :
-  (BaseChangeDer L d) x = d.toLinearMap.rTensor L x := rfl
-
-variable (R A L) in
-/-- The assignment `BaseChangeDer` is a Lie algebra homomorphism -/
-def BaseChangeDerHom : (Derivation R A A) →ₗ⁅R⁆ (LieDerivation R (A ⊗[R] L) (A ⊗[R] L)) where
-  toFun := BaseChangeDer L
-  map_add' _ _ := by ext _; simp
-  map_smul' _ _ := by ext _; simp
+/-- A derivation of an associative `R-`algebra `A`, induces a Lie derivation of `A ⊗[R] L` for any
+Lie algebra `L` over `R`. -/
+def ofDerivation : Derivation R A A →ₗ⁅R⁆ LieDerivation R (A ⊗[R] L) (A ⊗[R] L) where
+  toFun d :=
+    { toFun := d.rTensor L
+      map_add' := by simp
+      map_smul' := by simp
+      leibniz' x y := by
+        -- Probably the below can be golfed (low priority)
+        simp only [LinearMap.coe_mk, AddHom.coe_mk]
+        refine x.induction_on (by simp) ?_ ?_
+        · intros _ l
+          refine y.induction_on (by simp) ?_ ?_
+          · intros _ l'
+            rw [←sub_eq_zero]
+            simp only [LieAlgebra.ExtendScalars.bracket_tmul, LinearMap.rTensor_tmul,
+              Derivation.coeFn_coe, Derivation.leibniz, smul_eq_mul, add_tmul]
+            rw [←(lie_skew l' l), tmul_neg]
+            abel_nf
+          · intros _ _ h1 h2
+            rw [←sub_eq_zero]
+            simp [h1, h2]
+            abel_nf
+        · intros _ _ h1 h2
+          rw [←sub_eq_zero]
+          simp [h1, h2]
+          abel_nf }
+  map_add' _ _ := by ext; simp
+  map_smul' _ _ := by ext; simp
   map_lie' {_ _} := by
     ext z
-    simp only [BaseChangeDer_apply, Derivation.commutator_coe_linear_map,
-      LieDerivation.lie_apply]
-    refine z.induction_on (by simp) ?_ ?_
-    · intros _ _
-      simp [sub_tmul]
-    · intros _ _ hx hy
-      simp [hx, hy]
-      abel_nf
+    refine z.induction_on (by simp) (by simp [sub_tmul]) (fun _ _ hx hy ↦ ?_)
+    simp_all
+    abel
 
 @[simp]
 lemma BaseChangeDerHom_apply (d : Derivation R A A) (x : A ⊗[R] L) :
