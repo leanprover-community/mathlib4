db0e46b8cfdf58eb8d7550eb479aee698136bc7f
Xavier Généreux <20421163+xgenereux@users.noreply.github.com>
2026-03-18 16:39:37 +0000

    feat(Adjoin/Polynomial/Bivariate): IsAlgebraic.adjoin_singleton (#35874)
    This PR builds up to the following theorem:
    
    - `IsAlgebraic.adjoin_singleton`:
      Given two transcendental elements `a`, `b` over `R`, if one of them, say `a`, is algebraic over
      `R[b]` then `b` is algebraic over `R[a]`.
    
    In particular, it uses maps between `R[a]` and `R[X]` as well of some of the Bivariate polynomial API.
    
    The results are necessary in preparation for the following: Given a function field `K` and `(y : K)` such that
    `(hy : Transcendental Fq y)` then we have:
    ```
    FiniteDimensional Fq⟮y⟯ K
    ```
    
    Co-authored-by: María Inés de Frutos Fernández <[mariaines.dff@gmail.com](mailto:mariaines.dff@gmail.com)>
    Co-authored-by: Xavier Genereux <xaviergenereux@hotmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -6130,6 +6130,7 @@ public import Mathlib.RingTheory.Adjoin.FGBaseChange
 public import Mathlib.RingTheory.Adjoin.Field
 public import Mathlib.RingTheory.Adjoin.Polynomial
 public import Mathlib.RingTheory.Adjoin.Polynomial.Basic
+public import Mathlib.RingTheory.Adjoin.Polynomial.Bivariate
 public import Mathlib.RingTheory.Adjoin.PowerBasis
 public import Mathlib.RingTheory.Adjoin.Singleton
 public import Mathlib.RingTheory.Adjoin.Tower
