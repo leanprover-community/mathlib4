ba6f398896782ef1adb3cbf7d8272edd3e38f47b
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2026-03-19 00:28:59 +0100

    Introduce several typeclasses for stability of `MorphismProperty`

--- Mathlib/Probability/Kernel/Category/Stoch.lean
+++ Mathlib/Probability/Kernel/Category/Stoch.lean
@@ -3,17 +3,17 @@ Copyright (c) 2026 Gaëtan Serré. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Gaëtan Serré
 -/
+
 module
 
+public import Mathlib.CategoryTheory.MarkovCategory.Basic
 public import Mathlib.CategoryTheory.Widesubcategory
-public import Mathlib.Combinatorics.Quiver.ReflQuiver
 public import Mathlib.MeasureTheory.Measure.WithDensity
 public import Mathlib.Probability.Kernel.Category.SFinKer
 public import Mathlib.Topology.Connected.Separation
 public import Mathlib.Topology.EMetricSpace.Paracompact
 public import Mathlib.Topology.MetricSpace.Polish
 public import Mathlib.Topology.Separation.Lemmas
-public import Mathlib.CategoryTheory.MarkovCategory.Basic
 
 /-!
 # Stoch
@@ -22,10 +22,9 @@ The category of measurable spaces with Markov kernels is a Markov category.
 
 ## Main declarations
 
-* `StochHom`: the morphism property selecting Markov kernels in `SFinKer`.
-* `Stoch`: the wide subcategory of `SFinKer` with Markov-kernel morphisms.
-* `MonoidalCategory Stoch`: `Stoch` is a monoidal category.
-* `MarkovCategory Stoch`: `Stoch` is a Markov category.
+`Stoch` is defined as the wide subcategory `WideSubcategory StochHom` of `SFinKer`, where
+`StochHom` selects Markov kernels, and this construction provides in particular the instance
+`MarkovCategory Stoch`.
 
 ## References
 * [A synthetic approach to
@@ -44,167 +43,54 @@ universe u
 /-- Morphism property selecting Markov kernels in `SFinKer`. -/
 abbrev StochHom : MorphismProperty SFinKer := fun _ _ κ => IsMarkovKernel κ.1
 
-instance : StochHom.IsMultiplicative where
+instance : StochHom.IsComonStable where
+  whiskerLeft X Y Z κ hκ := by
+    kernel_cat
+    infer_instance
+  whiskerRight κ hκ Y := by
+    kernel_cat
+    infer_instance
   id_mem X := by
     kernel_cat
     infer_instance
   comp_mem κ η hκ hη := by
     kernel_cat
     infer_instance
+  associator_hom X Y Z := by
+    kernel_cat
+    exact Kernel.IsMarkovKernel.map Kernel.id (by fun_prop)
+  associator_inv X Y Z := by
+    kernel_cat
+    exact Kernel.IsMarkovKernel.map Kernel.id (by fun_prop)
+  leftUnitor_hom X := by
+    kernel_cat
+    exact Kernel.IsMarkovKernel.map Kernel.id (by fun_prop)
+  leftUnitor_inv X := by
+    kernel_cat
+    exact Kernel.IsMarkovKernel.map Kernel.id (by fun_prop)
+  rightUnitor_hom X := by
+    kernel_cat
+    exact Kernel.IsMarkovKernel.map Kernel.id (by fun_prop)
+  rightUnitor_inv X := by
+    kernel_cat
+    exact Kernel.IsMarkovKernel.map Kernel.id (by fun_prop)
+  braiding_hom X Y := Kernel.instIsMarkovKernelProdSwap
+  braiding_inv X Y := Kernel.instIsMarkovKernelProdSwap
+  counit X := Kernel.instIsMarkovKernelPUnitDiscard
+  comul X := Kernel.instIsMarkovKernelProdCopy
 
 /-- `Stoch` is the wide subcategory of `SFinKer` with Markov-kernel morphisms. -/
 abbrev Stoch := WideSubcategory StochHom
 
-noncomputable section
-
-/-- `stoch_ext` unfolds the definition of morphisms in `Stoch`. -/
-macro "stoch_ext" : tactic =>
-  `(tactic| (rw [Subtype.ext_iff]; try simp only [WideSubcategory.comp_def]))
-
-instance : MonoidalCategoryStruct Stoch.{u} where
-  tensorObj X Y := ⟨X.obj ⊗ Y.obj⟩
-  whiskerLeft X Y Z κ := by
-    refine ⟨X.obj ◁ κ.1, ?_⟩
-    kernel_cat
-    have : IsMarkovKernel κ.1.1 := κ.2
-    simp only [Set.mem_setOf_eq]
-    infer_instance
-  whiskerRight κ Y := by
-    refine ⟨κ.1 ▷ Y.obj, ?_⟩
-    kernel_cat
-    have : IsMarkovKernel κ.1.1 := κ.2
-    simp only [Set.mem_setOf_eq]
-    infer_instance
-  tensorUnit := ⟨SFinKer.of PUnit⟩
-  associator X Y Z := by
-    refine ⟨⟨(α_ X.obj Y.obj Z.obj).hom, ?_⟩, ⟨(α_ X.obj Y.obj Z.obj).inv, ?_⟩, ?_, ?_⟩
-    · kernel_cat
-      exact Kernel.IsMarkovKernel.map Kernel.id (by fun_prop)
-    · kernel_cat
-      exact Kernel.IsMarkovKernel.map Kernel.id (by fun_prop)
-    all_goals
-    stoch_ext
-    simp
-  leftUnitor X := by
-    refine ⟨⟨(λ_ X.obj).hom, ?_⟩, ⟨(λ_ X.obj).inv, ?_⟩, ?_, ?_⟩
-    · kernel_cat
-      exact Kernel.IsMarkovKernel.map Kernel.id (by fun_prop)
-    · kernel_cat
-      exact Kernel.IsMarkovKernel.map Kernel.id (by fun_prop)
-    all_goals
-    stoch_ext
-    simp
-  rightUnitor X := by
-    refine ⟨⟨(ρ_ X.obj).hom, ?_⟩, ⟨(ρ_ X.obj).inv, ?_⟩, ?_, ?_⟩
-    · kernel_cat
-      exact Kernel.IsMarkovKernel.map Kernel.id (by fun_prop)
-    · kernel_cat
-      exact Kernel.IsMarkovKernel.map Kernel.id (by fun_prop)
-    all_goals
-    stoch_ext
-    simp
-
-instance : MonoidalCategory Stoch.{u} := by
-  refine Monoidal.induced (wideSubcategoryInclusion StochHom) ?_
-  refine ⟨fun X Y ↦ ?_, fun X Y Z κ ↦ ?_, fun κ Z ↦ ?_, fun κ η ↦ ?_, ?_, fun X Y Z ↦ ?_,
-    fun X ↦ ?_, fun X ↦ ?_⟩
-  · rfl
-  · simp only [wideSubcategoryInclusion.obj, wideSubcategoryInclusion.map, Iso.refl_inv,
-    Iso.refl_hom, Category.comp_id]
-    rw [Category.id_comp <| X.obj ◁ κ.1]
-    rfl
-  · simp only [wideSubcategoryInclusion.obj, wideSubcategoryInclusion.map, Iso.refl_inv,
-      Iso.refl_hom, Category.comp_id]
-    rw [Category.id_comp <| κ.1 ▷ Z.obj]
-    rfl
-  · simp only [wideSubcategoryInclusion.obj, wideSubcategoryInclusion.map, Iso.refl_inv,
-    Iso.refl_hom, Category.comp_id]
-    rw [Category.id_comp <| κ.1 ⊗ₘ η.1]
-    rfl
-  · rfl
-  · simp only [wideSubcategoryInclusion.obj, wideSubcategoryInclusion.map, Iso.refl_symm,
-    Iso.trans_refl, Iso.trans_assoc, Iso.trans_hom, Iso.refl_hom,
-    MonoidalCategory.tensorIso_hom, MonoidalCategory.tensorHom_id,
-    MonoidalCategory.whiskerRight_tensor, MonoidalCategory.id_whiskerRight, Category.id_comp,
-    Iso.inv_hom_id, Category.comp_id]
-    rw [MonoidalCategory.id_whiskerRight <| X.obj ⊗ Y.obj,
-      Category.id_comp <| (α_ X.obj Y.obj Z.obj).hom]
-    rw [show 𝟙 ((X ⊗ Y).obj ⊗ Z.obj) = 𝟙 ((X.obj ⊗ Y.obj) ⊗ Z.obj) by rfl,
-      Category.id_comp <| (α_ X.obj Y.obj Z.obj).hom]
-    rfl
-  · simp only [wideSubcategoryInclusion.obj, wideSubcategoryInclusion.map, Iso.refl_symm,
-    Iso.trans_assoc, Iso.trans_hom, Iso.refl_hom, MonoidalCategory.tensorIso_hom,
-    MonoidalCategory.tensorHom_id]
-    rw [MonoidalCategory.id_whiskerRight <| (𝟙_ SFinKer)]
-    rw [Category.id_comp (λ_ X.obj).hom]
-    rw [show 𝟙 ((𝟙_ Stoch).obj ⊗ X.obj) = 𝟙 (𝟙_ SFinKer ⊗ X.obj) by rfl,
-      Category.id_comp (λ_ X.obj).hom]
-    rfl
-  · simp only [wideSubcategoryInclusion.obj, wideSubcategoryInclusion.map, Iso.refl_symm,
-    Iso.trans_assoc, Iso.trans_hom, Iso.refl_hom, MonoidalCategory.tensorIso_hom,
-    MonoidalCategory.id_tensorHom]
-    rw [MonoidalCategory.whiskerLeft_id X.obj <| 𝟙_ SFinKer, Category.id_comp (ρ_ X.obj).hom]
-    rw [show 𝟙 (X.obj ⊗ (𝟙_ Stoch).obj) = 𝟙 (X.obj ⊗ 𝟙_ SFinKer) by rfl,
-      Category.id_comp (ρ_ X.obj).hom]
-    rfl
-
-open scoped ComonObj in
-instance {X : Stoch} : ComonObj X where
-  counit := by
-    refine ⟨ε[X.obj], ?_⟩
-    exact Kernel.instIsMarkovKernelPUnitDiscard
-  comul := by
-    refine ⟨Δ[X.obj], ?_⟩
-    exact Kernel.instIsMarkovKernelProdCopy
-  counit_comul := by
-    stoch_ext
-    exact ComonObj.counit_comul _
-  comul_counit := by
-    stoch_ext
-    exact ComonObj.comul_counit _
-  comul_assoc := by
-    stoch_ext
-    exact ComonObj.comul_assoc _
-
-instance : BraidedCategory Stoch.{u} where
-  braiding X Y := by
-    refine ⟨⟨(β_ X.obj Y.obj).hom, ?_⟩, ⟨(β_ X.obj Y.obj).inv, ?_⟩, ?_, ?_⟩
-    · exact Kernel.instIsMarkovKernelProdSwap
-    · exact Kernel.instIsMarkovKernelProdSwap
-    all_goals
-    stoch_ext
-    simp
-  braiding_naturality_right X Y Z κ := by
-    stoch_ext
-    exact BraidedCategory.braiding_naturality_right _ _
-  braiding_naturality_left κ X := by
-    stoch_ext
-    exact BraidedCategory.braiding_naturality_left _ _
-  hexagon_forward X Y Z := by
-    stoch_ext
-    exact BraidedCategory.hexagon_forward _ _ _
-  hexagon_reverse X Y Z := by
-    stoch_ext
-    exact BraidedCategory.hexagon_reverse _ _ _
-
-instance : SymmetricCategory Stoch.{u} where
-  symmetry X Y := by
-    stoch_ext
-    exact SymmetricCategory.symmetry _ _
-
-instance (X : Stoch) : IsCommComonObj X where
-  comul_comm := by
-    stoch_ext
-    exact IsCommComonObj.comul_comm _
-
+noncomputable
 instance : MarkovCategory Stoch.{u} where
   discard_natural κ := by
-    stoch_ext
+    widesubcat_ext
     kernel_cat
     have : IsMarkovKernel κ.1.1 := κ.2
     exact κ.1.1.comp_discard
   copy_tensor X Y := by
-    stoch_ext
+    widesubcat_ext
     dsimp [MonoidalCategory.tensorμ, ComonObj.comul, BraidedCategory.braiding]
     kernel_cat
     repeat rw [Kernel.id_map (by fun_prop)]
@@ -213,7 +99,7 @@ instance : MarkovCategory Stoch.{u} where
     repeat rw [Kernel.deterministic_comp_deterministic]
     congr 1
   discard_tensor X Y := by
-    stoch_ext
+    widesubcat_ext
     kernel_cat
     simp only [ComonObj.counit, Kernel.comp_id_parallelComp]
     rw [Kernel.id_map (by fun_prop), Kernel.deterministic_comp_eq_map]
@@ -221,7 +107,7 @@ instance : MarkovCategory Stoch.{u} where
     rw [Kernel.map_apply _ (by fun_prop), Kernel.parallelComp_apply]
     simp [Kernel.discard_apply]
   copy_unit := by
-    stoch_ext
+    widesubcat_ext
     dsimp [ComonObj.comul]
     kernel_cat
     ext x s hs
