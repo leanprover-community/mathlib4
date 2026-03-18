30c5a39cef607f8d7c9c07951e16e736d603abcf
Stefan Kebekus <kebekus@users.noreply.github.com>
2026-03-18 08:50:05 +0100

    Implement reviewer's suggestion

--- Mathlib/Analysis/Complex/CanonicalDecomposition.lean
+++ Mathlib/Analysis/Complex/CanonicalDecomposition.lean
@@ -25,15 +25,11 @@ TODO: Formulate the canonical decomposition.
 
 @[expose] public section
 
-open Complex ComplexConjugate Metric Real
+namespace Complex
+
+open ComplexConjugate Metric Real
 
 variable {R : ℝ} {w : ℂ}
-
-theorem nonneg_radius_of_mem_sphere {c : ℂ} (hw : w ∈ sphere c R) :
-    0 ≤ R := by
-  rw [mem_sphere_iff_norm] at hw
-  rw [← hw]
-  apply norm_nonneg
 
 /-!
 ## Canonical Factors
@@ -47,21 +43,21 @@ Given `R : ℝ` and `w : ℂ`, the canonical factor is the function
 `fun z ↦ (R ^ 2 - (conj w) * z) / (R * (z - w))`. In applications, on will typically consider a
 setting where `w ∈ ball 0 R`.
 -/
-noncomputable def CanonicalFactor (R : ℝ) (w : ℂ) : ℂ → ℂ :=
+noncomputable def canonicalFactor (R : ℝ) (w : ℂ) : ℂ → ℂ :=
   fun z ↦ (R ^ 2 - (conj w) * z) / (R * (z - w))
 
-lemma CanonicalFactor_def (R : ℝ) (w : ℂ) :
-    CanonicalFactor R w = fun z ↦ (R ^ 2 - (conj w) * z) / (R * (z - w)) :=
+lemma canonicalFactor_def (R : ℝ) (w : ℂ) :
+    canonicalFactor R w = fun z ↦ (R ^ 2 - (conj w) * z) / (R * (z - w)) :=
   rfl
 
-lemma CanonicalFactor_apply (R : ℝ) (w z : ℂ) :
-    CanonicalFactor R w z = (R ^ 2 - (conj w) * z) / (R * (z - w)) :=
+lemma canonicalFactor_apply (R : ℝ) (w z : ℂ) :
+    canonicalFactor R w z = (R ^ 2 - (conj w) * z) / (R * (z - w)) :=
   rfl
 
 @[simp]
-lemma CanonicalFactor_apply_self (R : ℝ) (w : ℂ) :
-    CanonicalFactor R w w = 0 := by
-  simp [CanonicalFactor_apply]
+lemma canonicalFactor_apply_self (R : ℝ) (w : ℂ) :
+    canonicalFactor R w w = 0 := by
+  simp [canonicalFactor_apply]
 
 /-!
 ### Regularity properties
@@ -71,9 +67,9 @@ variable (R w) in
 /--
 Canonical factors are meromorphic.
 -/
-theorem meromorphicOn_canonicalFactor : MeromorphicOn (CanonicalFactor R w) Set.univ := by
+theorem meromorphicOn_canonicalFactor : MeromorphicOn (canonicalFactor R w) Set.univ := by
   intro x hx
-  unfold CanonicalFactor
+  unfold canonicalFactor
   fun_prop
 
 open scoped ComplexOrder in
@@ -81,9 +77,9 @@ variable (R w) in
 /--
 The canonical factor `CanonicalFactor R w` is analytic on the complement of `w`.
 -/
-theorem analyticOnNhd_canonicalFactor : AnalyticOnNhd ℂ (CanonicalFactor R w) {w}ᶜ := by
+theorem analyticOnNhd_canonicalFactor : AnalyticOnNhd ℂ (canonicalFactor R w) {w}ᶜ := by
   intro x hx
-  rw [CanonicalFactor_def]
+  rw [canonicalFactor_def]
   obtain (rfl | h) := eq_or_ne R 0
   · simpa using analyticAt_const
   have : x - w ≠ 0 := by grind
@@ -93,8 +89,8 @@ theorem analyticOnNhd_canonicalFactor : AnalyticOnNhd ℂ (CanonicalFactor R w) 
 The canonical factor `CanonicalFactor R w` has a simple pole at `z = w`.
 -/
 theorem meromorphicOrderAt_canonicalFactor (h : w ∈ ball 0 R) :
-    meromorphicOrderAt (CanonicalFactor R w) w = -1 := by
-  unfold CanonicalFactor
+    meromorphicOrderAt (canonicalFactor R w) w = -1 := by
+  unfold canonicalFactor
   rw [fun_meromorphicOrderAt_div (by fun_prop) (by fun_prop),
     fun_meromorphicOrderAt_mul (by fun_prop) (by fun_prop)]
   have : meromorphicOrderAt (fun z ↦ ↑R ^ 2 - (starRingEnd ℂ) w * z) w = 0 := by
@@ -112,7 +108,7 @@ theorem meromorphicOrderAt_canonicalFactor (h : w ∈ ball 0 R) :
 Canonical factors are meromorphic in normal form.
 -/
 theorem meromorphicNFOn_canonicalFactor (h : w ∈ ball 0 R) :
-    MeromorphicNFOn (CanonicalFactor R w) Set.univ := by
+    MeromorphicNFOn (canonicalFactor R w) Set.univ := by
   intro z hz
   obtain (rfl | h₁) := eq_or_ne z w
   · rw [meromorphicNFAt_iff_analyticAt_or]
@@ -131,7 +127,7 @@ The canonical factor `CanonicalFactor R w` has no zeros inside the ball of radiu
 -/
 theorem canonicalFactor_ne_zero {z : ℂ} (hw : w ∈ ball 0 R) (h₁z : z ∈ closedBall 0 R)
     (h₂z : z ≠ w) :
-    CanonicalFactor R w z ≠ 0 := by
+    canonicalFactor R w z ≠ 0 := by
   obtain ⟨hR, hzw⟩ : 0 < R ∧ z - w ≠ 0 := by grind [mem_ball_zero_iff, norm_nonneg]
   simp only [mem_ball, dist_zero_right, mem_closedBall] at hw h₁z
   have h_num_ne_zero : R ^ 2 - conj w * z ≠ 0 := by
@@ -139,25 +135,20 @@ theorem canonicalFactor_ne_zero {z : ℂ} (hw : w ∈ ball 0 R) (h₁z : z ∈ c
     suffices ‖w‖ * ‖z‖ < R * R by simpa [sq]
     grw [h₁z]
     gcongr
-  rw [CanonicalFactor_apply]
+  rw [canonicalFactor_apply]
   positivity
-
-/--
-The canonical factor `CanonicalFactor R w` evalues to zero at `z = w`.
--/
-@[simp]
-theorem canonicalFactor_eval_center (R : ℝ) (w : ℂ) : CanonicalFactor R w w = 0 := by
-  simp [CanonicalFactor]
 
 open scoped ComplexOrder in
 /--
 The canonical factor `CanonicalFactor R w` takes values of norm one on `sphere 0 R`.
 -/
 theorem norm_canonicalFactor_eval_circle_eq_one {z : ℂ} (hw : w ∈ ball 0 R) (hz : z ∈ sphere 0 R) :
-    ‖CanonicalFactor R w z‖ = 1 := by
+    ‖canonicalFactor R w z‖ = 1 := by
   obtain ⟨hR, hzw⟩ : 0 < R ∧ z - w ≠ 0 := by
     grind [mem_ball_zero_iff, norm_nonneg, mem_sphere_zero_iff_norm]
-  rw [CanonicalFactor, norm_div, div_eq_iff (by rw [ne_eq, norm_eq_zero]; positivity), one_mul]
+  rw [canonicalFactor, norm_div, div_eq_iff (by rw [ne_eq, norm_eq_zero]; positivity), one_mul]
   obtain rfl := by simpa [mem_sphere_zero_iff_norm] using hz
   rw [← ofReal_pow, ← normSq_eq_norm_sq, normSq_eq_conj_mul_self, ← sub_mul, mul_comm _ z]
   simp [← map_sub]
+
+end Complex
