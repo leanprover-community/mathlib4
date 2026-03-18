b51fc64ee676ec7f59c64554e3b02caf6441eafe
Michael Rothgang <rothgang@math.uni-bonn.de>
2026-03-18 15:18:08 +0100

    Reduce output spew

--- MathlibTest/Continuity.lean
+++ MathlibTest/Continuity.lean
@@ -27,9 +27,23 @@ example {g : X → X} (y : Y) : Continuous ((fun _ ↦ y) ∘ g) := by continuit
 
 example {f : X → Y} (x : X) : Continuous (fun (_ : X) ↦ f x) := by continuity
 
+-- This test points at an internal error in aesop. With fewer imports, `continuity` succeeds.
+-- See https://leanprover.zulipchat.com/#narrow/channel/287929-mathlib4/topic/aesop.20error.20during.20proof.20reconstruction.2C.20goal.20not.20normalised/with/580202602.
+/-- error: aesop: internal error during proof reconstruction: goal 31 was not normalised. -/
+#guard_msgs in
 example (f₁ f₂ : X → Y) (hf₁ : Continuous f₁) (hf₂ : Continuous f₂)
     (g : Y → ℝ) (hg : Continuous g) : Continuous (fun x => (max (g (f₁ x)) (g (f₂ x))) + 1) := by
   continuity
+
+section
+
+attribute [-aesop] Continuous.comp'
+attribute [aesop (rule_sets := [Continuous]) safe apply] Continuous.comp'
+example (f₁ f₂ : X → Y) (hf₁ : Continuous f₁) (hf₂ : Continuous f₂)
+    (g : Y → ℝ) (hg : Continuous g) : Continuous (fun x => (max (g (f₁ x)) (g (f₂ x))) + 1) := by
+  continuity
+
+end
 
 example {κ ι : Type}
     (K : κ → Type) [∀ k, TopologicalSpace (K k)] (I : ι → Type) [∀ i, TopologicalSpace (I i)]
@@ -46,92 +60,10 @@ attribute [aesop (rule_sets := [Continuous]) unsafe 99% apply] Continuous.comp'
 
 /--
 error: tactic 'aesop' failed, failed to prove the goal. Some goals were not explored because the maximum rule application depth (30) was reached. Set option 'maxRuleApplicationDepth' to increase the limit.
-Initial goal:
-  W : Type ?u.507636
-  X : Type ?u.507639
-  Y : Type ?u.507642
-  Z : Type ?u.507645
-  inst✝⁴ : TopologicalSpace W
-  inst✝³ : TopologicalSpace X
-  inst✝² : TopologicalSpace Y
-  inst✝¹ : TopologicalSpace Z
-  I : Type ?u.507648
-  X' : I → Type ?u.507653
-  inst✝ : (i : I) → TopologicalSpace (X' i)
-  ⊢ Continuous fun x => rexp (max x (-x) + sin x) ^ 2
-Remaining goals after safe rules:
-  case h.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf
-  W : Type ?u.507636
-  X : Type ?u.507639
-  Y : Type ?u.507642
-  Z : Type ?u.507645
-  inst : TopologicalSpace W
-  inst_1 : TopologicalSpace X
-  inst_2 : TopologicalSpace Y
-  inst_3 : TopologicalSpace Z
-  I : Type ?u.507648
-  X' : I → Type ?u.507653
-  inst_4 : (i : I) → TopologicalSpace (X' i)
-  ⊢ Continuous fun x => (Complex.exp (↑(max x (-x)) + Complex.sin ↑x)).1
-The safe prefix was not fully expanded because the maximum number of rule applications (50) was reached.
 -/
-#guard_msgs in
+#guard_msgs (substring := true) in
 example : Continuous (fun x : ℝ => exp ((max x (-x)) + sin x)^2) := by
-  -- This invocation fails, with an error about maximal rule application depth being reached.
-  -- continuity itself does not support passing further options.
   continuity
-  sorry
-
-/--
-error: Tactic `simp` failed with a nested error:
-(deterministic) timeout at `whnf`, maximum number of heartbeats (200000) has been reached
-
-Note: Use `set_option maxHeartbeats <num>` to set the limit.
-
-Hint: Additional diagnostic information may be available using the `set_option diagnostics true` command.
--/
-#guard_msgs in
-example : Continuous (fun x : ℝ => exp ((max x (-x)) + sin x)^2) := by
-  -- Let's manually expand `continuity` to its aesop invocation, and pass that option.
-  -- Now we get an error about simp's maximal recursion depth being reached.
-  aesop (config := { terminal := true, maxRuleApplicationDepth := 200 }) (rule_sets := [Continuous])
-  sorry
-
-/--
-error: tactic 'aesop' failed, maximum number of rule applications (200) reached. Set the 'maxRuleApplications' option to increase the limit.
-Initial goal:
-  W : Type ?u.1782759
-  X : Type ?u.1782762
-  Y : Type ?u.1782765
-  Z : Type ?u.1782768
-  inst✝⁴ : TopologicalSpace W
-  inst✝³ : TopologicalSpace X
-  inst✝² : TopologicalSpace Y
-  inst✝¹ : TopologicalSpace Z
-  I : Type ?u.1782771
-  X' : I → Type ?u.1782776
-  inst✝ : (i : I) → TopologicalSpace (X' i)
-  ⊢ Continuous fun x => rexp (max x (-x) + sin x) ^ 2
-Remaining goals after safe rules:
-  case h.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf.hf
-  W : Type ?u.1782759
-  X : Type ?u.1782762
-  Y : Type ?u.1782765
-  Z : Type ?u.1782768
-  inst : TopologicalSpace W
-  inst_1 : TopologicalSpace X
-  inst_2 : TopologicalSpace Y
-  inst_3 : TopologicalSpace Z
-  I : Type ?u.1782771
-  X' : I → Type ?u.1782776
-  inst_4 : (i : I) → TopologicalSpace (X' i)
-  ⊢ Continuous fun x => (Complex.exp ↑(max x (-x) + sin x)).1
-The safe prefix was not fully expanded because the maximum number of rule applications (50) was reached.
--/
-#guard_msgs in
-example : Continuous (fun x : ℝ => exp ((max x (-x)) + sin x)^2) := by
-  -- If we disable simp, aesop fails to make meaningful progress.
-  aesop (config := { terminal := true, maxRuleApplicationDepth := 200, enableSimp := false }) (rule_sets := [Continuous])
   sorry
 
 -- If we make this a safe rule instead, the test succeeds quickly.
