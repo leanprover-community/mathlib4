d5efea98385c99669dd34b251f85d10a261d1a7f
Yaël Dillies <yael.dillies@gmail.com>
2026-03-18 15:31:56 +0100

    monomorphise

--- Mathlib/Probability/Distributions/Binomial.lean
+++ Mathlib/Probability/Distributions/Binomial.lean
@@ -41,43 +41,24 @@ namespace ProbabilityTheory
 variable {R Ω : Type*} [MeasurableSpace R] [AddMonoidWithOne R] {m : MeasurableSpace Ω}
   {P : Measure Ω} {X : Ω → R} {n : ℕ} {p : I}
 
-/-- The binomial probability distribution with parameter `p`.
-
-For convenience, this distribution is defined over any semiring.
-It is meant to be used on `ℕ` and `ℝ` mainly. -/
+/-- The binomial probability distribution with parameter `p`. -/
 @[expose]
-noncomputable def binomial (n : ℕ) (p : I) : Measure R :=
-  setBer(Set.Iio n, p).map (Nat.cast ∘ Set.ncard)
-
-/-- The binomial probability distribution with parameter `p` valued in the semiring `R`. -/
-scoped notation3 "Bin(" R' ", " n ", " p ")" => binomial (R := R') n p
+noncomputable def binomial (n : ℕ) (p : I) : Measure ℕ := setBer(Set.Iio n, p).map Set.ncard
 
 /-- The binomial probability distribution with parameter `p`. -/
-scoped notation3 "Bin(" n ", " p ")" => Bin(ℕ, n, p)
+scoped notation3 "Bin(" n ", " p ")" => binomial n p
 
-instance isProbabilityMeasure_binomial : IsProbabilityMeasure Bin(R, n, p) :=
+/-- The binomial probability distribution with parameter `p` valued in the semiring `R`. -/
+scoped notation3 "Bin(" R ", " n ", " p ")" => (binomial n p).map (Nat.cast : ℕ → R)
+
+instance isProbabilityMeasure_binomial : IsProbabilityMeasure Bin(n, p) :=
   Measure.isProbabilityMeasure_map <| by fun_prop
 
-@[simp] lemma map_natCast_binomial : Bin(n, p).map Nat.cast = Bin(R, n, p) :=
-  -- FIXME: Why can't `fun_prop` deal with the composition itself?
-  -- See https://leanprover.zulipchat.com/#narrow/channel/287929-mathlib4/topic/fun_prop.20fails.20Nat.2Ecast.20.E2.88.98.20Set.2Encard.20.3A.20Set.20.E2.84.95.20.E2.86.92.20.E2.84.95.20measurable/
-  Measure.map_map (by fun_prop) (.comp (by fun_prop) (by fun_prop))
-
-lemma hasLaw_binomial_natCast : HasLaw (Nat.cast : ℕ → R) Bin(R, n, p) Bin(n, p) where
-  map_eq := by simp
-  -- FIXME: Why doesn't `fun_prop` apply `Measurable.aemeasurable` itself?
-  aemeasurable := by refine Measurable.aemeasurable ?_; fun_prop
-
-lemma ae_mem_image_natCast_Iic_of_hasLaw_binomial [MeasurableSingletonClass R]
-    (hX : HasLaw X Bin(R, n, p) P) : ∀ᵐ ω ∂P, X ω ∈ Nat.cast '' Set.Iic n := by
-  have : MeasurableSet (Nat.cast (R := R) '' Set.Iic n) :=
-    ((Set.finite_Iic _).image _).measurableSet
-  rw [hX.ae_iff <| by simpa, binomial, ae_map_iff (by fun_prop) <| by exact this]
-  filter_upwards [setBernoulli_ae_subset] with s hs
-  exact Set.mem_image_of_mem _ <| by simpa using Set.ncard_le_ncard hs
-
 lemma ae_le_of_hasLaw_binomial {X : Ω → ℕ} (hX : HasLaw X Bin(n, p) P) : ∀ᵐ ω ∂P, X ω ≤ n := by
-  simpa using ae_mem_image_natCast_Iic_of_hasLaw_binomial hX
+  rw [hX.ae_iff (p := (· ≤ n)) <| by fun_prop, binomial,
+    ae_map_iff (by fun_prop) (Set.finite_Iic _).measurableSet]
+  filter_upwards [setBernoulli_ae_subset] with s hs
+  simpa using Set.ncard_le_ncard hs
 
 /-! ### Binomial random variables -/
 
