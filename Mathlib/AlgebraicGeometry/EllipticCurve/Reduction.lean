2adc08b7e74d1a76dd66489cfc8bfbfd13f31e76
tb65536 <thomas.l.browning@gmail.com>
2026-03-19 11:46:12 +0000

    merge

--- Mathlib/AlgebraicGeometry/EllipticCurve/Reduction.lean
+++ Mathlib/AlgebraicGeometry/EllipticCurve/Reduction.lean
@@ -151,61 +151,6 @@ lemma integralModel_Δ_eq (W : WeierstrassCurve K) [hW : IsIntegral R W] :
   conv_rhs => rw [← baseChange_integralModel_eq R W]
   simp [integralModel]
 
-lemma integralModel_a₁_eq (W : WeierstrassCurve K) [hW : IsIntegral R W] :
-    algebraMap R K (integralModel R W).a₁ = W.a₁ := by
-  conv_rhs => rw [← baseChange_integralModel_eq R W]
-  simp [integralModel]
-
-lemma integralModel_a₂_eq (W : WeierstrassCurve K) [hW : IsIntegral R W] :
-    algebraMap R K (integralModel R W).a₂ = W.a₂ := by
-  conv_rhs => rw [← baseChange_integralModel_eq R W]
-  simp [integralModel]
-
-lemma integralModel_a₃_eq (W : WeierstrassCurve K) [hW : IsIntegral R W] :
-    algebraMap R K (integralModel R W).a₃ = W.a₃ := by
-  conv_rhs => rw [← baseChange_integralModel_eq R W]
-  simp [integralModel]
-
-lemma integralModel_a₄_eq (W : WeierstrassCurve K) [hW : IsIntegral R W] :
-    algebraMap R K (integralModel R W).a₄ = W.a₄ := by
-  conv_rhs => rw [← baseChange_integralModel_eq R W]
-  simp [integralModel]
-
-lemma integralModel_a₆_eq (W : WeierstrassCurve K) [hW : IsIntegral R W] :
-    algebraMap R K (integralModel R W).a₆ = W.a₆ := by
-  conv_rhs => rw [← baseChange_integralModel_eq R W]
-  simp [integralModel]
-
-lemma integralModel_b₂_eq (W : WeierstrassCurve K) [hW : IsIntegral R W] :
-    algebraMap R K (integralModel R W).b₂ = W.b₂ := by
-  conv_rhs => rw [← baseChange_integralModel_eq R W]
-  simp [integralModel]
-
-lemma integralModel_b₄_eq (W : WeierstrassCurve K) [hW : IsIntegral R W] :
-    algebraMap R K (integralModel R W).b₄ = W.b₄ := by
-  conv_rhs => rw [← baseChange_integralModel_eq R W]
-  simp [integralModel]
-
-lemma integralModel_b₆_eq (W : WeierstrassCurve K) [hW : IsIntegral R W] :
-    algebraMap R K (integralModel R W).b₆ = W.b₆ := by
-  conv_rhs => rw [← baseChange_integralModel_eq R W]
-  simp [integralModel]
-
-lemma integralModel_b₈_eq (W : WeierstrassCurve K) [hW : IsIntegral R W] :
-    algebraMap R K (integralModel R W).b₈ = W.b₈ := by
-  conv_rhs => rw [← baseChange_integralModel_eq R W]
-  simp [integralModel]
-
-lemma integralModel_c₄_eq (W : WeierstrassCurve K) [hW : IsIntegral R W] :
-    algebraMap R K (integralModel R W).c₄ = W.c₄ := by
-  conv_rhs => rw [← baseChange_integralModel_eq R W]
-  simp [integralModel]
-
-lemma integralModel_c₆_eq (W : WeierstrassCurve K) [hW : IsIntegral R W] :
-    algebraMap R K (integralModel R W).c₆ = W.c₆ := by
-  conv_rhs => rw [← baseChange_integralModel_eq R W]
-  simp [integralModel]
-
 variable [IsDomain R] [ValuationRing R] [IsFractionRing R K]
 
 open ValuationRing
@@ -364,6 +309,20 @@ class HasAdditiveReduction (W : WeierstrassCurve K) : Prop extends IsMinimal R W
   badReduction : valuation K (maximalIdeal R) W.Δ < 1
   additiveReduction : valuation K (maximalIdeal R) W.c₄ < 1
 
+open Polynomial in
+/-- A minimal Weierstrass equation has split multiplicative reduction if and only if
+the polynomial `c₄ X ^ 2 + a₁ c₄ X + (54 b₆ - 3 b₂ b₄ - a₂ c₄)` splits in the residue field.
+
+To see how this polynomial arises, note that node `(x₀, y₀)` has `x₀ = (18 b₆ - b₂ b₄) / c₄`, and
+the reduction is split if and only if the tangent cone `X ^ 2 + a₁ T = 3 x₀ + a₂` splits.
+-/
+@[mk_iff]
+class HasSplitMultiplicativeReduction (W : WeierstrassCurve K) [IsMinimal R W] : Prop
+    extends W.HasMultiplicativeReduction R where
+  splitMultiplicativeReduction : letI I := W.integralModel R
+    Splits <| .map (algebraMap R (ResidueField R)) <|
+      C I.c₄ * X ^ 2 + C (I.a₁ * I.c₄) * X + C (54 * I.b₆ - 3 * I.b₂ * I.b₄ - I.a₂ * I.c₄)
+
 variable {W : WeierstrassCurve K}
 
 theorem hasGoodReduction_or_hasMultiplicativeReduction_or_hasAdditiveReduction [IsMinimal R W] :
@@ -395,41 +354,6 @@ theorem HasMultiplicativeReduction.not_hasAdditiveReduction (hW : W.HasMultiplic
 theorem HasAdditiveReduction.not_hasMultiplicativeReduction (hW : W.HasAdditiveReduction R) :
     ¬ W.HasMultiplicativeReduction R :=
   fun h ↦ hW.additiveReduction.ne h.multiplicativeReduction
-
-/-- A minimal Weierstrass equation has multiplicative reduction if and only if
-the valuation of its discriminant is less than 1 and the valuation of `a₄` equals 1. -/
-@[mk_iff]
-class IsMultiplicativeReduction (W : WeierstrassCurve K) [IsMinimal R W] : Prop where
-  badReduction : valuation K (maximalIdeal R) W.Δ < 1
-  multiplicativeReduction : valuation K (maximalIdeal R) W.c₄ = 1
-
-/-- A minimal Weierstrass equation has additive reduction if and only if
-the valuation of its discriminant is less than 1 and the valuation of `a₄` is less than 1. -/
-@[mk_iff]
-class IsAdditiveReduction (W : WeierstrassCurve K) [IsMinimal R W] : Prop where
-  badReduction : valuation K (maximalIdeal R) W.Δ < 1
-  additiveReduction : valuation K (maximalIdeal R) W.c₄ < 1
-
-theorem isGoodReduction_or_isMultiplicativeReduction_or_isAdditiveReduction
-    {W : WeierstrassCurve K} [IsMinimal R W] :
-    W.IsGoodReduction R ∨ W.IsMultiplicativeReduction R ∨ W.IsAdditiveReduction R := by
-  rw [isGoodReduction_iff, isMultiplicativeReduction_iff, isAdditiveReduction_iff,
-    ← integralModel_Δ_eq R W, ← integralModel_c₄_eq R W]
-  grind [valuation_le_one]
-
-open Polynomial in
-/-- A minimal Weierstrass equation has split multiplicative reduction if and only if
-the polynomial `c₄ X ^ 2 + a₁ c₄ X + (54 b₆ - 3 b₂ b₄ - a₂ c₄)` splits in the residue field.
-
-To see how this polynomial arises, note that node `(x₀, y₀)` has `x₀ = (18 b₆ - b₂ b₄) / c₄`, and
-the reduction is split if and only if the tangent cone `X ^ 2 + a₁ T = 3 x₀ + a₂` splits.
--/
-@[mk_iff]
-class IsSplitMultiplicativeReduction (W : WeierstrassCurve K) [IsMinimal R W] : Prop
-    extends W.IsMultiplicativeReduction R where
-  splitMultiplicativeReduction : letI I := W.integralModel R
-    Splits <| .map (algebraMap R (ResidueField R)) <|
-      C I.c₄ * X ^ 2 + C (I.a₁ * I.c₄) * X + C (54 * I.b₆ - 3 * I.b₂ * I.b₄ - I.a₂ * I.c₄)
 
 end Reduction
 
