333dc4efaa1b0dcb0bcbc827eb5d80a60564b63b
thorimur <68410468+thorimur@users.noreply.github.com>
2026-03-19 18:14:57 -0400

    feat: `for .. in` notation

--- Mathlib/Lean/MessageData/ForExprs.lean
+++ Mathlib/Lean/MessageData/ForExprs.lean
@@ -13,25 +13,31 @@ public import Lean.Meta.Basic
 /-!
 # Tools for extracting `Expr`s from `MessageData` nodes
 
-The main definition in this file is `Lean.MessageData.forExprs`,
-which locates `Expr` objects nested within a message.
+This file provides `for (ppCtx, e) in msg.exprs do` notation, which iterates through the
+expressions `e` in a `msg : MessageData`. The surrounding monad must support `BaseIO` to handle
+`.ofLazy` `MessageData` nodes. `e` may be interpreted in a `MetaM` context using `ppCtx.runMetaM e`.
 
 Some helpers are provided implemented in terms of this.
 -/
 
 public section
 
-/-- Iterate over all the expressions in a `MessageData`.
+namespace Lean.MessageData
 
-`σ` is a state, allowing this to emulate `ForIn`.
+universe u
+
+variable {m : Type → Type u} [Monad m] [MonadLiftT BaseIO m]
+
+/-- Iterate over all the expressions in a `MessageData`. Used to implement
+`for (ppCtx, e) in msg.exprs do` notation.
 
 `f` is run with `PPContext.runMetaM` with the appropriate context. -/
-partial def Lean.MessageData.forExprs {σ} (msg : MessageData) (s : σ)
-    (f : σ → Expr → MetaM (ForInStep σ)) : IO σ := do
+partial def forExprsIn {σ} (msg : MessageData) (s : σ)
+    (f : PPContext × Expr → σ → m (ForInStep σ)) : m σ := do
   return (← go ⟨.anonymous, []⟩ none s msg).value
 where
   go (nctx : NamingContext) (ctx? : Option MessageDataContext) (s : σ) :
-      MessageData → IO (ForInStep σ)
+      MessageData → m (ForInStep σ)
     | .withContext ctx m => go nctx (some ctx) s m
     | .withNamingContext nctx m => go nctx ctx? s m
     | .compose a b => do
@@ -57,20 +63,19 @@ where
       return .yield s'
     | .ofGoal m => do
       if let some ppCtx := ctx?.map (mkPPContext nctx) then
-        ppCtx.runMetaM (f s (.mvar m))
+        f (ppCtx, .mvar m) s
       else
         return .yield s
     | .ofFormatWithInfos fwi => do
       let some ppCtx := ctx?.map (mkPPContext nctx) | return .yield s
       goFmt fwi.infos s (fun s' i _ => do
         let some (.ofTermInfo i) := fwi.infos.get? i | return (.yield s', false)
-        let ri ← ppCtx.runMetaM (f s' i.expr)
-        return (ri, true)
+        return (← f (ppCtx, i.expr) s', true)
       ) fwi.fmt
   /-- Iterate over the tags of a `Format` using `f`. If `f` returns `true` as its second piece,
   do not recurse further into the tag.  -/
-  goFmt {σ} (infos) (s : σ) (f : σ → Nat → Format → IO (ForInStep σ × Bool)) :
-      Format → IO (ForInStep σ)
+  goFmt {σ} (infos) (s : σ) (f : σ → Nat → Format → m (ForInStep σ × Bool)) :
+      Format → m (ForInStep σ)
     | .tag n fmt => do
       let (rn, b) ← f s n fmt
       if b then return rn
@@ -84,16 +89,38 @@ where
       goFmt infos s f fmt2
     | .text _ | .align _ | .line | .nil => return .yield s
 
+/-- A wrapper structure for `MessageData` to enable `for (ppCtx, e) in msg.exprs do` notation. -/
+protected structure Exprs where
+  /-- The `MessageData` whose expressions will be iterated over. -/
+  msg : MessageData
+
+/-- `for (ppCtx, e) in msg.exprs do` iterates through the expressions in `MessageData` together
+with their `ppCtx : PPContext`. The `ppCtx` can be used to interpret the expression in a valid
+`MetaM` context via `ppCtx.runMetaM`.
+
+The monad must support `BaseIO` in order to interpret `.ofLazy` nodes in `MessageData`.
+
+Expressions without a valid `ppCtx` are skipped. -/
+def exprs (msg : MessageData) : MessageData.Exprs := ⟨msg⟩
+
+instance : ForIn m MessageData.Exprs (PPContext × Expr) where
+  forIn exprs := exprs.msg.forExprsIn
+
 /-- Find the expression in a message on which `f` does not return `none`. -/
-partial def Lean.MessageData.firstExpr? {α} (msg : MessageData) (f : Expr → MetaM (Option α)) :
-    IO (Option α) :=
-  msg.forExprs none fun s e => do
-    if let .some a ← f e then
-      return .done (some a)
-    return .yield s
+partial def firstExpr? {α} (msg : MessageData) (f : Expr → MetaM (Option α)) :
+    IO (Option α) := do
+  for (ppCtx, e) in msg.exprs do
+    let a@(some _) ← ppCtx.runMetaM (f e) | continue
+    return a
+  return none
 
 /-- Get all the expressions in a message, in order.
 
 If you need the context of the expressions, prefer to use `forExprs` directly. -/
-partial def Lean.MessageData.getExprs (msg : MessageData) : IO (Array Expr) :=
-  msg.forExprs #[] fun s e => return .yield (s.push e)
+partial def getExprs (msg : MessageData) : m (Array Expr) := do
+  let mut arr := #[]
+  for (_, e) in msg.exprs do
+    arr := arr.push e
+  return arr
+
+end Lean.MessageData
