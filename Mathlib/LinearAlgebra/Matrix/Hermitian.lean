89ac57afbf2d5d7171120a1ba0d0c0d2fba73727
Snir Broshi <26556598+SnirBroshi@users.noreply.github.com>
2026-03-19 08:33:36 +0200

    untag `IsHermitian.smul` with `@[simp]`
    Co-authored-by: Joël Riou <37772949+joelriou@users.noreply.github.com>

--- Mathlib/LinearAlgebra/Matrix/Hermitian.lean
+++ Mathlib/LinearAlgebra/Matrix/Hermitian.lean
@@ -224,7 +224,6 @@ section StarModule
 
 variable {R : Type*} [Star R] [Star α] [SMul R α] [StarModule R α]
 
-@[simp]
 theorem IsHermitian.smul {A : Matrix n n α} (h : A.IsHermitian) {k : R} (hk : IsSelfAdjoint k) :
     (k • A).IsHermitian := by
   rw [IsHermitian, conjTranspose_smul, hk.star_eq, h.eq]
