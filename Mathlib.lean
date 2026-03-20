02cc524e88be6f0d2349bfb36a4602eeee73afde
David Loeffler <d.loeffler.01@cantab.net>
2026-03-20 15:00:53 +0100

    update Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -7287,6 +7287,7 @@ public import Mathlib.Topology.Algebra.Polynomial
 public import Mathlib.Topology.Algebra.PontryaginDual
 public import Mathlib.Topology.Algebra.ProperAction.AddTorsor
 public import Mathlib.Topology.Algebra.ProperAction.Basic
+public import Mathlib.Topology.Algebra.ProperAction.Fiberwise
 public import Mathlib.Topology.Algebra.ProperAction.ProperlyDiscontinuous
 public import Mathlib.Topology.Algebra.ProperConstSMul
 public import Mathlib.Topology.Algebra.RestrictedProduct.Basic
