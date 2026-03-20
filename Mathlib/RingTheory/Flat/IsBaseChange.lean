114863f1ff6100789dff2b15188a6b70a73cea4f
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2026-03-20 11:11:02 +0800

    split file and fix doc

--- Mathlib/RingTheory/Flat/IsBaseChange.lean
+++ Mathlib/RingTheory/Flat/IsBaseChange.lean
@@ -12,8 +12,7 @@ public import Mathlib.RingTheory.Flat.Basic
 
 # Lemmas about IsBaseChange under Exact Sequences
 
-In this file, we show that cokernel preserves `IsBaseChange S` and when `S` is flat,
-kernel preserves `IsBaseChange S`.
+In this file, we show that kernel preserves `IsBaseChange S` and when `S` is flat.
 
 # Main Results
 
@@ -27,9 +26,6 @@ i₁         i₂         i₃
 |          |          |
 v          v          v
 N₁ --g₁--> N₂ --g₂--> N₃
-
-* `IsBaseChange.of_right_exact` : If `f₂` and `g₂` are surjective,
-  `i₁` and `i₂` is base change by `S`, then `i₃` is base change by `S`.
 
 * `IsBaseChange.of_left_exact` : If `S` is flat over `R`, `f₁` and `g₁` are injective,
   `i₂` and `i₃` is base change by `S`, then `i₁` is base change by `S`.
@@ -46,27 +42,11 @@ variable {M₁ M₂ M₃ N₁ N₂ N₃ : Type*} [AddCommGroup M₁] [AddCommGro
   [IsScalarTower R S N₁] [IsScalarTower R S N₂] [IsScalarTower R S N₃]
   (h₁ : M₁ →ₗ[R] N₁) (h₂ : M₂ →ₗ[R] N₂) (h₃ : M₃ →ₗ[R] N₃)
   {f : M₁ →ₗ[R] M₂} {g : M₂ →ₗ[R] M₃} {f' : N₁ →ₗ[S] N₂} {g' : N₂ →ₗ[S] N₃}
-  (comm1 : h₂.comp f = (f'.restrictScalars R).comp h₁)
-  (comm2 : h₃.comp g = (g'.restrictScalars R).comp h₂)
 
-include comm1 comm2 in
-lemma IsBaseChange.of_right_exact (isb1 : IsBaseChange S h₁) (isb2 : IsBaseChange S h₂)
-    (exac1 : Function.Exact f g) (surj1 : Function.Surjective g)
-    (exac2 : Function.Exact f' g') (surj2 : Function.Surjective g') : IsBaseChange S h₃ := by
-  change Function.Bijective _ at isb1 isb2 ⊢
-  refine LinearMap.bijective_of_surjective_of_bijective_of_right_exact
-    ((f.baseChange S).restrictScalars R) ((g.baseChange S).restrictScalars R)
-    (f'.restrictScalars R) (g'.restrictScalars R) _ _ _ ?_ ?_ ?_ exac2 isb1.2 isb2 ?_ surj2
-  · ext s m
-    simpa using congr(s • ($comm1 m)).symm
-  · ext s m
-    simpa using congr(s • ($comm2 m)).symm
-  · simpa [LinearMap.baseChange_eq_ltensor] using lTensor_exact S exac1 surj1
-  · simpa [LinearMap.baseChange_eq_ltensor] using LinearMap.lTensor_surjective S surj1
-
-include comm1 comm2 in
-lemma IsBaseChange.of_left_exact [Module.Flat R S] (isb2 : IsBaseChange S h₂)
-    (isb3 : IsBaseChange S h₃) (exac1 : Function.Exact f g) (inj1 : Function.Injective f)
+lemma IsBaseChange.of_left_exact (comm1 : h₂.comp f = (f'.restrictScalars R).comp h₁)
+    (comm2 : h₃.comp g = (g'.restrictScalars R).comp h₂) [Module.Flat R S]
+    (isb2 : IsBaseChange S h₂) (isb3 : IsBaseChange S h₃)
+    (exac1 : Function.Exact f g) (inj1 : Function.Injective f)
     (exac2 : Function.Exact f' g') (inj2 : Function.Injective f') : IsBaseChange S h₁ := by
   change Function.Bijective _ at isb2 isb3 ⊢
   refine LinearMap.bijective_of_bijective_of_injective_of_left_exact
@@ -79,23 +59,3 @@ lemma IsBaseChange.of_left_exact [Module.Flat R S] (isb2 : IsBaseChange S h₂)
   · simpa [LinearMap.baseChange_eq_ltensor] using Module.Flat.lTensor_exact S exac1
   · simpa [LinearMap.baseChange_eq_ltensor] using
       Module.Flat.lTensor_preserves_injective_linearMap f inj1
-
-lemma IsBaseChange.of_equiv_left (f : M₁ ≃ₗ[R] M₂) (f' : N₁ ≃ₗ[S] N₂)
-    (comm1 : h₂.comp f.toLinearMap = (f'.restrictScalars R).comp h₁)
-    (isb1 : IsBaseChange S h₁) : IsBaseChange S h₂ :=
-  IsBaseChange.of_right_exact S (f := (0 : Unit →ₗ[R] M₁)) (f' := (0 : Unit →ₗ[S] N₁))
-    (g := f) (g' := f') 0 h₁ h₂ (by simp) comm1 (show Function.Bijective _ from by simp) isb1
-      (fun y ↦ (by simpa using eq_comm)) f.bijective.2 (fun y ↦ (by simpa using eq_comm))
-        f'.bijective.2
-
-lemma IsBaseChange.of_equiv_right (f : M₁ ≃ₗ[R] M₂) (f' : N₁ ≃ₗ[S] N₂)
-    (comm1 : h₂.comp f.toLinearMap = (f'.restrictScalars R).comp h₁)
-    (isb2 : IsBaseChange S h₂) : IsBaseChange S h₁ := by
-  refine IsBaseChange.of_equiv_left S h₂ h₁ f.symm f'.symm (LinearMap.ext fun y ↦ ?_) isb2
-  obtain ⟨y, rfl⟩ := f.surjective y
-  exact f'.injective (by simpa using congr($comm1 y).symm)
-
-lemma IsBaseChange.comp_equiv {M1 M2 N : Type*} [AddCommGroup M1] [AddCommGroup M2] [AddCommGroup N]
-    [Module R M1] [Module R M2] [Module R N] [Module S N] [IsScalarTower R S N] (e : M1 ≃ₗ[R] M2)
-    (f : M2 →ₗ[R] N) (isb : IsBaseChange S f) : IsBaseChange S (f.comp e.toLinearMap) :=
-  IsBaseChange.of_equiv_right S (f.comp e.toLinearMap) f e 1 (LinearMap.ext fun y ↦ by simp) isb
