39f96e3aeb597ce9ebf16f57be2d46406a8581ba
vihdzp <vi.hdz.p@gmail.com>
2026-03-20 00:06:55 -0600

    golf

--- Mathlib/SetTheory/Ordinal/Arithmetic.lean
+++ Mathlib/SetTheory/Ordinal/Arithmetic.lean
@@ -928,11 +928,10 @@ theorem dvd_add_iff : ∀ {a b c : Ordinal}, a ∣ b → (a ∣ b + c ↔ a ∣ 
 theorem div_mul_cancel : ∀ {a b : Ordinal}, a ≠ 0 → a ∣ b → a * (b / a) = b
   | a, _, a0, ⟨b, rfl⟩ => by rw [mul_div_cancel _ a0]
 
-theorem le_of_dvd {a b : Ordinal} (hb : b ≠ 0) (ha : a ∣ b) : a ≤ b := by
-  obtain ⟨b, rfl⟩ := ha
-  apply le_mul_left _ (pos_of_ne_zero _)
-  contrapose! hb
-  rw [hb, mul_zero]
+theorem le_of_dvd {a b : Ordinal} (hb : b ≠ 0) (hdiv : a ∣ b) : a ≤ b := by
+  obtain ⟨b, rfl⟩ := hdiv
+  apply Ordinal.le_mul_left _ (pos_of_ne_zero _)
+  simp_all
 
 theorem dvd_antisymm {a b : Ordinal} (h₁ : a ∣ b) (h₂ : b ∣ a) : a = b :=
   if a0 : a = 0 then by subst a; exact (eq_zero_of_zero_dvd h₁).symm
