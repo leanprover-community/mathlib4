92b7f5580d4273564da4efbe4db0654c68d28b86
Adomas Baliuka <52975890+adomasbaliuka@users.noreply.github.com>
2026-03-20 13:19:55 +0100

    authors list
    Co-authored-by: Jon Eugster <eugster.jon@gmail.com>

--- Mathlib/Tactic/Linter/TextBased/UnicodeLinter.lean
+++ Mathlib/Tactic/Linter/TextBased/UnicodeLinter.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2024 Jon Eugster. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Adomas Baliuka, Jon Eugster, Michael Rothgang
+Authors: Adomas Baliuka, Jon Eugster
 -/
 module
 
