24aad224d911dcd90d1f2266cc741e92d777bb4d
Robin Carlier <57142648+robin-carlier@users.noreply.github.com>
2026-03-20 09:55:34 +0100

    chore: fix simp_nf

--- Mathlib/CategoryTheory/Limits/Types/Colimits.lean
+++ Mathlib/CategoryTheory/Limits/Types/Colimits.lean
@@ -174,7 +174,9 @@ theorem colimitEquivColimitType_apply (j : J) (x : F.obj j) :
   apply (colimitEquivColimitType F).symm.injective
   simp
 
-attribute [elementwise (attr := simp)] colimit.w colimit.ι_desc colimit.ι_map
+-- We don’t want to add `simp` to the original lemmas here
+attribute [elementwise] colimit.w colimit.ι_desc colimit.ι_map
+attribute [simp] colimit.w_apply colimit.ι_desc_apply colimit.ι_map_apply
 
 variable {F} in
 @[deprecated colimit.w_apply (since := "2026-03-06")]
