69e6abbe17214fe503498def04a21d5d922c731d
mariainesdff <mariaines.dff@gmail.com>
2026-03-20 15:10:31 +0100

    suggested change

--- Mathlib/Data/Nat/Choose/Multinomial.lean
+++ Mathlib/Data/Nat/Choose/Multinomial.lean
@@ -83,18 +83,20 @@ theorem multinomial_congr {f g : α → ℕ} (h : ∀ a ∈ s, f a = g a) :
   · rw [Finset.sum_congr rfl h]
   · exact Finset.prod_congr rfl fun a ha => by rw [h a ha]
 
-theorem multinomial_congr_of_sdiff [DecidableEq α] {f g : α → ℕ} {s t : Finset α}
-    (hst : s ⊆ t) (hg : ∀ a ∈ t \ s, g a = 0) (hfg : ∀ a ∈ s, f a = g a) :
+theorem multinomial_congr_of_eq_on_inter [DecidableEq α] {f g : α → ℕ} {s t : Finset α}
+    (hf : ∀ a ∈ s \ t, f a = 0) (hg : ∀ a ∈ t \ s, g a = 0) (hfg : ∀ a ∈ s ∩ t, f a = g a) :
     multinomial s f = multinomial t g := by
   rw [← Nat.mul_right_inj (prod_ne_zero_iff.mpr (fun x _ ↦ factorial_ne_zero (g x))),
-    multinomial_spec, ← sum_subset_zero_on_sdiff hst hg hfg, ← multinomial_spec s f]
+    multinomial_spec]
+  rw [prod_congr_of_eq_on_inter (g := fun a ↦ (f a)!) (s₂ := s) (by aesop) (by aesop) (by aesop),
+      multinomial_spec s f]
   congr 1
-  symm
-  apply prod_subset_one_on_sdiff hst
-  · intro x hx
-    rw [hg x hx, factorial_zero]
-  · intro x hx
-    rw [hfg x hx]
+  exact sum_congr_of_eq_on_inter (by grind) (by grind) (by grind)
+
+theorem multinomial_congr_of_sdiff [DecidableEq α] {f g : α → ℕ} {s t : Finset α}
+    (hst : s ⊆ t) (hg : ∀ a ∈ t \ s, g a = 0) (hfg : ∀ a ∈ s, f a = g a) :
+    multinomial s f = multinomial t g :=
+    multinomial_congr_of_eq_on_inter (by grind) hg (by grind)
 
 variable (s a) in
 theorem multinomial_single [DecidableEq α] :
