94acb1345e3767c673cd2cf656f86e25d5f4e6ec
Thomas R. Murrills <68410468+thorimur@users.noreply.github.com>
2026-03-19 21:44:43 -0400

    review: MessageData.ForExprs (#36880)
    This review of #36796 suggests implementing `for (ppCtx, e) in
    msgs.exprs do` notation by letting the caller handle `ppCtx` themselves,
    via an approach patterned after `Syntax.topDown`. This also means we can
    avoid running `MetaM` when we're simply getting the expressions.
    
    It also fixes issues in handling `FormatWithInfos` where `DelabTermInfo`
    (which extends `TermInfo`) was not handled and where the local context
    from the info node was ignored, and where `isExporting` was not updated.
    See comment for explanation.
    
    Also fixes some style issues, cleans up the logic `goFmt` (worth looking
    over to ensure it still makes sense!), and namespaces under
    `Lean.MessageData`. (Note: `goFmt` appears to be private by default.)
    
    ---
    <!-- Your PR title will become the first line of the commit message.
    
    In this box, the text above the `---` (if not empty) will be appended
    to the commit message, and can be used to give additional context or
    details. Please leave a blank newline before the `---`, otherwise GitHub
    will format the text above it as a title.
    
    For details on the "pull request lifecycle" in mathlib, please see:
    https://leanprover-community.github.io/contribute/index.html
    
    In particular, note that most reviewers will only notice your PR
    if it passes the continuous integration checks.
    Please ask for help on https://leanprover.zulipchat.com if needed.
    
    When merging, all the commits will be squashed into a single commit
    listing all co-authors.
    
    Co-authors in the squash commit are gathered from two sources:
    
    First, all authors of commits to this PR branch are included. Thus,
    one way to add co-authors is to include at least one commit authored by
    each co-author among the commits in the pull request. If necessary, you
    may create empty commits to indicate co-authorship, using commands like
    so:
    
    git commit --author="Author Name <author@email.com>" --allow-empty -m
    "add Author Name as coauthor"
    
    Second, co-authors can also be listed in lines at the very bottom of
    the commit message (that is, directly before the `---`) using the
    following format:
    
    Co-authored-by: Author Name <author@email.com>
    
    If you are moving or deleting declarations, please include these lines
    at the bottom of the commit message (before the `---`, and also before
    any "Co-authored-by" lines) using the following format:
    
    Moves:
    - Vector.* -> List.Vector.*
    - ...
    
    Deletions:
    - Nat.bit1_add_bit1
    - ...
    
    Any other comments you want to keep out of the PR commit should go
    below the `---`, and placed outside this HTML comment, or else they
    will be invisible to reviewers.
    
    If this PR depends on other PRs, please list them below this comment,
    using the following format:
    - [ ] depends on: #abc [optional extra text]
    - [ ] depends on: #xyz [optional extra text]
    
    -->
    
    [![Open in
    Gitpod](https://gitpod.io/button/open-in-gitpod.svg)](https://gitpod.io/from-referrer/)

--- Mathlib/Lean/MessageData/ForExprs.lean
+++ Mathlib/Lean/MessageData/ForExprs.lean
@@ -13,25 +13,30 @@ public import Lean.Meta.Basic
 /-!
 # Tools for extracting `Expr`s from `MessageData` nodes
 
-The main definition in this file is `Lean.MessageData.forExprs`,
-which locates `Expr` objects nested within a message.
+This file provides `for (ppCtx, e) in msg.exprs do` notation, which iterates through the
+expressions `e` in a `msg : MessageData`. The surrounding monad must support `BaseIO` to handle
+`.ofLazy` `MessageData` nodes. `e` may be interpreted in a `MetaM` context using `ppCtx.runMetaM e`.
 
 Some helpers are provided implemented in terms of this.
 -/
 
 public section
 
-/-- Iterate over all the expressions in a `MessageData`.
+namespace Lean.MessageData
 
-`σ` is a state, allowing this to emulate `ForIn`.
+universe u
 
-`f` is run with `PPContext.runMetaM` with the appropriate context. -/
-partial def Lean.MessageData.forExprs {σ} (msg : MessageData) (s : σ)
-    (f : σ → Expr → MetaM (ForInStep σ)) : IO σ := do
+variable {m : Type → Type u} [Monad m] [MonadLiftT BaseIO m]
+
+/-- Iterate over all the expressions in a `MessageData`. Used to implement
+`for (ppCtx, e) in msg.exprs do` notation, which should be preferred over using this declaration
+directly. -/
+partial def forExprsIn {σ} (msg : MessageData) (s : σ)
+    (f : PPContext × Expr → σ → m (ForInStep σ)) : m σ := do
   return (← go ⟨.anonymous, []⟩ none s msg).value
 where
   go (nctx : NamingContext) (ctx? : Option MessageDataContext) (s : σ) :
-      MessageData → IO (ForInStep σ)
+      MessageData → m (ForInStep σ)
     | .withContext ctx m => go nctx (some ctx) s m
     | .withNamingContext nctx m => go nctx ctx? s m
     | .compose a b => do
@@ -41,8 +46,7 @@ where
     | .ofLazy f _ => do
       let ppCtx? := ctx?.map (mkPPContext nctx)
       let dyn ← f ppCtx?
-      let some innerMsg := dyn.get? MessageData |
-        return .yield s
+      let some innerMsg := dyn.get? MessageData | return .yield s
       go nctx ctx? s innerMsg
     | .nest _ m | .group m | .tagged _ m => go nctx ctx? s m
     | .ofWidget _ alt => go nctx ctx? s alt
@@ -57,43 +61,73 @@ where
       return .yield s'
     | .ofGoal m => do
       if let some ppCtx := ctx?.map (mkPPContext nctx) then
-        ppCtx.runMetaM (f s (.mvar m))
+        -- See below; `.goal`s also go through `ctx.runMetaM` when being displayed.
+        f ({ ppCtx with env := ppCtx.env.setExporting false }, .mvar m) s
       else
         return .yield s
     | .ofFormatWithInfos fwi => do
       let some ppCtx := ctx?.map (mkPPContext nctx) | return .yield s
-      goFmt fwi.infos s (fun s' i _ => do
-        let some (.ofTermInfo i) := fwi.infos.get? i | return (.yield s', false)
-        let ri ← ppCtx.runMetaM (f s' i.expr)
-        return (ri, true)
-      ) fwi.fmt
-  /-- Iterate over the tags of a `Format` using `f`. If `f` returns `true` as its second piece,
-  do not recurse further into the tag.  -/
-  goFmt {σ} (infos) (s : σ) (f : σ → Nat → Format → IO (ForInStep σ × Bool)) :
-      Format → IO (ForInStep σ)
+      goFmt ppCtx fwi.infos s fwi.fmt
+  /-- Iterate over the tags of a `Format` using `f`. -/
+  goFmt (ppCtx : PPContext) (infos) (s : σ) : Format → m (ForInStep σ)
     | .tag n fmt => do
-      let (rn, b) ← f s n fmt
-      if b then return rn
-      let .yield s := rn | return rn
-      goFmt infos s f fmt
-    | .group fmt _ => goFmt infos s f fmt
-    | .nest _ fmt => goFmt infos s f fmt
+      match infos.get? n with
+      | some (.ofTermInfo { expr, lctx .. })
+      | some (.ofDelabTermInfo { expr, lctx .. }) => do
+        /- When displaying interactive expressions, lean creates a `ctx : Elab.ContextInfo` from
+        the surrounding `ppCtx`, abandoning the `ppCtx`'s `lctx` and using whatever `lctx` is
+        provided by the info. But we want to expose a uniform interface, so we try to create a
+        `ppCtx` such that `ppCtx.runMetaM` behaves the same as `ctx.runMetaM info.lctx`. This means
+        passing in the `lctx` from the info node and setting exporting to `false` (see
+        `ContextInfo.runCoreM`). We regard the other differences (e.g. `ngen`, `diag`) as
+        too minor to change.
+
+        See `Lean.Widget.Interactive` and `msgToInteractiveAux` to see how lean handles
+        `FormatWithInfos` in the language server. -/
+        let ppCtx := { ppCtx with lctx, env := ppCtx.env.setExporting false }
+        f (ppCtx, expr) s
+      | _ => goFmt ppCtx infos s fmt
+    | .group fmt _ => goFmt ppCtx infos s fmt
+    | .nest _ fmt => goFmt ppCtx infos s fmt
     | .append fmt1 fmt2 => do
-      let r1 ← goFmt infos s f fmt1
+      let r1 ← goFmt ppCtx infos s fmt1
       let .yield s := r1 | return r1
-      goFmt infos s f fmt2
+      goFmt ppCtx infos s fmt2
     | .text _ | .align _ | .line | .nil => return .yield s
 
+/-- A wrapper structure for `MessageData` to enable `for (ppCtx, e) in msg.exprs do` notation. -/
+protected structure Exprs where
+  /-- The `MessageData` whose expressions will be iterated over. -/
+  msg : MessageData
+
+/-- `for (ppCtx, e) in msg.exprs do` iterates through the expressions in `MessageData` together
+with their `ppCtx : PPContext`. The `ppCtx` can be used to interpret the expression in a valid
+`MetaM` context via `ppCtx.runMetaM`.
+
+The monad must support `BaseIO` in order to interpret `.ofLazy` nodes in `MessageData`.
+
+Expressions without a valid `ppCtx` are skipped. -/
+def exprs (msg : MessageData) : MessageData.Exprs := ⟨msg⟩
+
+instance : ForIn m MessageData.Exprs (PPContext × Expr) where
+  forIn exprs := exprs.msg.forExprsIn
+
 /-- Find the expression in a message on which `f` does not return `none`. -/
-partial def Lean.MessageData.firstExpr? {α} (msg : MessageData) (f : Expr → MetaM (Option α)) :
-    IO (Option α) :=
-  msg.forExprs none fun s e => do
-    if let .some a ← f e then
-      return .done (some a)
-    return .yield s
+partial def firstExpr? {α} (msg : MessageData) (f : Expr → MetaM (Option α)) :
+    IO (Option α) := do
+  for (ppCtx, e) in msg.exprs do
+    let a@(some _) ← ppCtx.runMetaM (f e) | continue
+    return a
+  return none
 
 /-- Get all the expressions in a message, in order.
 
-If you need the context of the expressions, prefer to use `forExprs` directly. -/
-partial def Lean.MessageData.getExprs (msg : MessageData) : IO (Array Expr) :=
-  msg.forExprs #[] fun s e => return .yield (s.push e)
+If you need the context of the expressions, prefer iterating over the expressions via
+`for (ppCtx, e) in msg.exprs do` directly. -/
+partial def getExprs (msg : MessageData) : m (Array Expr) := do
+  let mut arr := #[]
+  for (_, e) in msg.exprs do
+    arr := arr.push e
+  return arr
+
+end Lean.MessageData
