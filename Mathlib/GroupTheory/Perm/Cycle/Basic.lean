d56c334ec57dd22f42e5ee4f5ae81e80df09b048
Aaron Liu <aaronliu2008@outlook.com>
2026-03-19 22:04:21 -0400

    fix two files

--- Mathlib/GroupTheory/Perm/Cycle/Basic.lean
+++ Mathlib/GroupTheory/Perm/Cycle/Basic.lean
@@ -392,7 +392,7 @@ theorem isCycle_swap_mul_aux₂ {α : Type*} [DecidableEq α] :
     obtain hfxb | hfxb := eq_or_ne (f x) b
     · exact ⟨0, hfxb⟩
     obtain ⟨hfb, hbx⟩ : f b ≠ b ∧ b ≠ x := ne_and_ne_of_swap_mul_apply_ne_self hb
-    replace hb : (swap x (f.symm x) * f⁻¹) (f.symm b) ≠ f.symm b := by
+    replace hb : (swap x (f⁻¹ x) * f⁻¹) (f⁻¹ b) ≠ f⁻¹ b := by
       rw [mul_apply, swap_apply_def]
       split_ifs <;> simp [symm_apply_eq, eq_symm_apply] at * <;> tauto
     obtain ⟨i, hi⟩ := isCycle_swap_mul_aux₁ n hb <| by
@@ -400,7 +400,7 @@ theorem isCycle_swap_mul_aux₂ {α : Type*} [DecidableEq α] :
     refine ⟨-i, (swap x (f⁻¹ x) * f⁻¹).injective ?_⟩
     convert hi using 1
     · rw [zpow_neg, ← inv_zpow, ← mul_apply, mul_inv_rev, swap_inv, mul_swap_eq_swap_mul]
-      simp [swap_comm _ x, ← mul_apply, -coe_mul, ← inv_def, -coe_inv, ← inv_def, mul_assoc _ f⁻¹,
+      simp [swap_comm _ x, ← mul_apply, -coe_mul, -coe_inv, mul_assoc _ f⁻¹,
         ← mul_zpow_mul, mul_assoc _ _ f]
       simp
     · exact swap_apply_of_ne_of_ne (by simpa [eq_comm, eq_symm_apply, symm_apply_eq] using hfxb)
