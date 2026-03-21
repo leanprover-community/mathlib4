1bf29b4e48c3b7613f0b4227bb95832a12235a43
FMLJohn <128468682+FMLJohn@users.noreply.github.com>
2026-03-21 15:51:01 +0000

    Authors

--- Mathlib/Topology/ClosedBases.lean
+++ Mathlib/Topology/ClosedBases.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2026 Fangming Li. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Author: Fangming Li
+Authors: Fangming Li
 -/
 import Mathlib.Topology.Bases
 
