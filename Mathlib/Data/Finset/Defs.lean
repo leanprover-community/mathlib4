0d9bbb4fd221576c0ac4ffdc680d4fea55614a0d
JadAbouHawili <jad-abou-hawili@hotmail.com>
2026-03-21 14:43:18 +0200

    Update Mathlib/Data/Finset/Defs.lean
    Co-authored-by: Jon Eugster <eugster.jon@gmail.com>

--- Mathlib/Data/Finset/Defs.lean
+++ Mathlib/Data/Finset/Defs.lean
@@ -49,9 +49,7 @@ Most constructions involving `Finset`s have been split off to their own files.
 * `Finset`: Defines a type for the finite subsets of `α`.
   Constructing a `Finset` requires two pieces of data: `val`, a `Multiset α` of elements,
   and `nodup`, a proof that `val` has no duplicates.
-* `Finset.instMembership`: Defines membership `a ∈ (s : Finset α)`.(given by `Finset.instSetLike`)
-* `Finset.instSetLike`: Provides a coercion `s : Finset α` to `s : Set α`.
-* `Finset.instCoeSortFinsetType`: Coerce `s : Finset α` to the type of all `x ∈ s`.
+* `a ∈ (s : Finset α)` is defined through coercion to `Set α`.
 
 ## Tags
 
