c35faeebb468d48823191bdf85274d775b6d6a71
Jun2M <rnjsgurwns27@gmail.com>
2026-03-21 18:35:14 -0400

    mk_all

--- Mathlib.lean
+++ Mathlib.lean
@@ -3412,7 +3412,6 @@ public import Mathlib.Combinatorics.Enumerative.Schroder
 public import Mathlib.Combinatorics.Enumerative.Stirling
 public import Mathlib.Combinatorics.Extremal.RuzsaSzemeredi
 public import Mathlib.Combinatorics.Graph.Basic
-public import Mathlib.Combinatorics.Graph.GraphLike
 public import Mathlib.Combinatorics.Graph.Subgraph
 public import Mathlib.Combinatorics.GraphLike.Basic
 public import Mathlib.Combinatorics.HalesJewett
