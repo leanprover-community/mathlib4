1bb52214f0dacf74d63ff981a9151f0a28a0314c
Bingyu Xia <71547343+BryceT233@users.noreply.github.com>
2026-03-22 06:40:59 +0800

    rename

--- Mathlib/RingTheory/AdicCompletion/Completeness.lean
+++ Mathlib/RingTheory/AdicCompletion/Completeness.lean
@@ -145,7 +145,7 @@ theorem pow_smul_top_eq_ker_eval {n : ℕ} (h : I.FG) : I ^ n • ⊤ = (eval I 
   rw [← restrictScalars_top R (AdicCompletion I R) (AdicCompletion I M),
     ← restrictScalars_image_smul_eq (R := AdicCompletion I R), show
     ⇑(algebraMap R (AdicCompletion I R)) = of I R by rfl,
-    ← restrictScalars_ofPowSMul_range_eq_ker_eval, restrictScalars_le,
+    ← restrictScalars_range_ofPowSMul_eq_ker_eval, restrictScalars_le,
     image_smul_top_eq_range_lsum]
   simp only [SetLike.coe_sort_coe]
   rw [← LinearMap.range_comp_of_range_eq_top (f := (finsuppLEquivDirectSum ..).symm.toLinearMap)
