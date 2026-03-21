09801dfbcf97c6606d4fb4d5f0bbb72b486e4c3e
blake-farman <blake.farman@gmail.com>
2026-03-20 22:18:06 -0500

    Rewrote closure properties in terms of orthogonals.

--- Mathlib/CategoryTheory/Abelian/TorsionTheory/Basic.lean
+++ Mathlib/CategoryTheory/Abelian/TorsionTheory/Basic.lean
@@ -71,20 +71,6 @@ lemma le_leftOrthogonal_rightOrthogonal (P : ObjectProperty C) :
     P ≤ P.leftOrthogonal.rightOrthogonal := by
   simpa [Function.comp] using gc_rightOrthogonal_leftOrthogonal.l_u_le (OrderDual.toDual P)
 
-/-
-def PretorsionClass (Φ : Preradical C) : ObjectProperty C :=
-  fun X ↦ IsIso (Φ.ι.app X)
-
-def PretorsionFreeClass (Φ : Preradical C) : ObjectProperty C :=
-  fun X ↦ CategoryTheory.Limits.IsZero (Φ.r.obj X)
-
-example (X : C) (P : ObjectProperty C) : C := by
-  haveI : Limits.HasCoproducts C := by infer_instance
-  let f := fun Y : {Y : Subobject X // P Y} ↦ (Y.1 : C)
-  #check ∐ f
-  sorry
--/
-
 /-- A Torsion Theory in an abelian category consists of two classes, `T` and `F`, of
 torsion and torsion-free objects, respectively, such that `T` is the left orthogonal
 of `F` and `F` is the right orthogonal of `T`. -/
@@ -94,13 +80,13 @@ structure TorsionTheory (T F : ObjectProperty C) : Prop where
 
 namespace TorsionTheory
 
-lemma mem_torsion_iff {X : C} {T F : ObjectProperty C} (hTF : TorsionTheory T F) :
+/- lemma mem_torsion_iff {X : C} {T F : ObjectProperty C} (hTF : TorsionTheory T F) :
     T X ↔ F.leftOrthogonal X := by
   simp [hTF.torsion_eq_leftOrthogonal]
 
 lemma mem_free_iff {X : C} (T F : ObjectProperty C) (hTF : TorsionTheory T F) :
     F X ↔ T.rightOrthogonal X := by
-  simp [hTF.free_eq_rightOrthogonal]
+  simp [hTF.free_eq_rightOrthogonal] -/
 
 example (T F : ObjectProperty C) (hTF : TorsionTheory T F) (X : C) :
     T X ↔ (∀ {Y : C}, ∀ f : X ⟶ Y, F Y → f = 0) := by
@@ -109,14 +95,6 @@ example (T F : ObjectProperty C) (hTF : TorsionTheory T F) (X : C) :
 example (T F : ObjectProperty C) (hTF : TorsionTheory T F) (Y : C) :
     F Y ↔  (∀ {X : C}, ∀ f : X ⟶ Y, T X → f = 0) := by
   simpa [hTF.free_eq_rightOrthogonal] using ObjectProperty.rightOrthogonal_iff T Y
-
-lemma torsion_of_epi {T F : ObjectProperty C} (hTF : TorsionTheory T F)
-    {X Y : C} (hX : T X)
-    (f : X ⟶ Y) [Epi f] : T Y := by
-  apply hTF.mem_torsion_iff.mpr
-  intro Z g hZ
-  have hfg : f ≫ g = 0 := (hTF.mem_torsion_iff.mp hX (f ≫ g) hZ)
-  exact Limits.zero_of_epi_comp f hfg
 
 lemma torsionTheoryGeneratedBy (P : ObjectProperty C) :
     TorsionTheory P.rightOrthogonal.leftOrthogonal P.rightOrthogonal where
@@ -128,34 +106,56 @@ lemma torsionTheoryCogeneratedBy (P : ObjectProperty C) :
       torsion_eq_leftOrthogonal := by simp
       free_eq_rightOrthogonal := rfl
 
+lemma leftOrthogonal_of_epi (P : ObjectProperty C)
+    {X Y : C} (hX : P.leftOrthogonal X) (f : X ⟶ Y) [Epi f] : P.leftOrthogonal Y := by
+  intro Z g hZ
+  have hfg : f ≫ g = 0 := hX (f ≫ g) hZ
+  exact Limits.zero_of_epi_comp f hfg
+
+instance (P : ObjectProperty C) : (P.leftOrthogonal).IsClosedUnderQuotients where
+  prop_of_epi := fun f _ hX ↦ leftOrthogonal_of_epi P hX f
+
+instance (P : ObjectProperty C) : (P.leftOrthogonal).IsClosedUnderExtensions where
+  prop_X₂_of_shortExact := by
+    intro s hs hX₁ hX₃ Z k hZ
+    let t : Limits.CokernelCofork s.f :=
+      Limits.CokernelCofork.ofπ k (hX₁ (s.f ≫ k) hZ)
+    let l : s.X₃ ⟶ Z := hs.gIsCokernel.desc t
+    have hl : l = 0 := hX₃ l hZ
+    have hfac : s.g ≫ l = k :=
+      hs.gIsCokernel.fac t Limits.WalkingParallelPair.one
+    simp [← hfac, hl]
+
+-- TODO: This needs to be cleaned up.
+lemma isClosedUnderCoproducts_of_leftOrthogonal (P : ObjectProperty C) :
+    ∀ {J : Type w}, P.leftOrthogonal.IsClosedUnderColimitsOfShape (Discrete J) := by
+  intro J
+  refine { colimitsOfShape_le := ?_}
+  intro X ⟨hX⟩ Y f hY
+  apply hX.isColimit.hom_ext
+  intro j
+  simpa [comp_zero] using (hX.prop_diag_obj j) (hX.ι.app j ≫ f) hY
+
+/-
+  TODO: Should be able to use these pieces to prove that `P.leftOrthogonal` is closed under
+  colimits, when they exist.
+-/
+
+-- I think these will likely get removed once things are cleand up.
 lemma isClosedUnderQuotients_of_torsionTheory {T F : ObjectProperty C} (hTF : TorsionTheory T F) :
-    T.IsClosedUnderQuotients where
-      prop_of_epi := fun f _ hPX ↦ hTF.torsion_of_epi hPX f
+    T.IsClosedUnderQuotients := by
+      rw [hTF.torsion_eq_leftOrthogonal]
+      infer_instance
 
 lemma isClosedUnderExtensions_of_torsionTheory {T F : ObjectProperty C} (hTF : TorsionTheory T F) :
-    T.IsClosedUnderExtensions where
-      prop_X₂_of_shortExact := by
-        intro S hs hX₁ hX₃
-        apply hTF.mem_torsion_iff.mpr
-        intro Z k hZ
-        let t : Limits.CokernelCofork S.f :=
-          Limits.CokernelCofork.ofπ k  (hTF.mem_torsion_iff.mp hX₁ (S.f ≫ k) hZ)
-        let l : S.X₃ ⟶ Z := hs.gIsCokernel.desc t
-        have hl: l = 0 := hTF.mem_free_iff.mp hZ l hX₃
-        have hfac : S.g ≫ l = k :=
-          hs.gIsCokernel.fac t Limits.WalkingParallelPair.one
-        simp [← hfac, hl]
+    T.IsClosedUnderExtensions := by
+      rw [hTF.torsion_eq_leftOrthogonal]
+      infer_instance
 
 lemma isClosedUnderCoproducts_of_torsionTheory {T F : ObjectProperty C} (hTF : TorsionTheory T F) :
     ∀ {J : Type w}, T.IsClosedUnderColimitsOfShape (Discrete J) := by
   intro J
-  refine { colimitsOfShape_le := ?_}
-  intro X ⟨hX⟩
-  apply hTF.mem_torsion_iff.mpr
-  intro Y f hY
-  apply hX.isColimit.hom_ext
-  intro j
-  simpa [comp_zero] using hTF.mem_torsion_iff.mp (hX.prop_diag_obj j) (hX.ι.app j ≫ f) hY
+  simpa [hTF.torsion_eq_leftOrthogonal] using isClosedUnderCoproducts_of_leftOrthogonal F
 
 theorem isTorsionClass_iff {P : ObjectProperty C} [LocallySmall.{w} C] [WellPowered.{w} C]
     [HasCoproducts.{w} C] : (∃ F : ObjectProperty C, TorsionTheory P F) ↔
@@ -168,12 +168,58 @@ theorem isTorsionClass_iff {P : ObjectProperty C} [LocallySmall.{w} C] [WellPowe
   refine ⟨P.rightOrthogonal, ?_⟩
   refine { torsion_eq_leftOrthogonal := ?_, free_eq_rightOrthogonal := rfl }
   · refine le_antisymm (le_rightOrthogonal_leftOrthogonal P) ?_
-    · intro X hPX
-      let Y := CategoryTheory.Subobject.sSup {A : Subobject X | P (A : C)}
-      /- TODO: Prove that X/Y has property P. -/
+    · intro X hX
+      /-
+        Started off trying to ape the construction from Stenström, which goes roughly like this:
+        Assume `X : C` and `hX : P.rightOrthogonal.leftOrthogonal X`. Form the maximal `P`-subobject
+        of `X`, `Y`. Prove that `X/Y` lies in `P.rightOrthogonal` as follows. If `T : C` and
+        `P T`, then any morphism `α : T ⟶ X/Y` has image with property `P` because
+        `P.isClosedUnderQuotients`. But then the image of `α` corresponds to a subobject of `X` that
+        contains `Y` and has property `P`. Hence the corresponding subobject of `X` is `Y`, the
+        image of `α` is zero. This implies `α` is zero and thus `X/Y` lies in `P.rightOrthogonal`.
+        Now projection `X ⟶ X/Y` is zero by `hX`. Therefore `Y = X`.
+
+        Somewhere along the way, things went off the rails. Starting to think I'm using the wrong
+        components. Will revisit.
+      -/
+      let subobjs : Set (Subobject X) := {A : Subobject X | P (Subobject.underlying.obj A)}
+      let Y := CategoryTheory.Subobject.sSup subobjs
+      let I := equivShrink (Subobject X) '' subobjs
+      let f := Subobject.smallCoproductDesc subobjs -- Morphism coproduct over `subobjs` to `X`
+      let D : Discrete I ⥤ C := Discrete.functor fun j => ((equivShrink (Subobject X)).symm j : C)
+      have hPcoprod : P (colimit D) := by
+        haveI := hcoprod (J := I)
+        have hD : ∀ j, P (D.obj j) := by
+          intro j
+          rcases j with ⟨j⟩
+          change P (((equivShrink (Subobject X)).symm j : Subobject X) : C)
+          rcases j.2 with ⟨A, hA, hj⟩
+          have : ((equivShrink (Subobject X)).symm j : Subobject X) = A := by
+            simpa using (congrArg (Equiv.symm (equivShrink (Subobject X))) hj).symm
+          rw [this]
+          exact hA
+        simpa [D] using P.prop_colimit D hD
+      have hPimage : P (image f) := by
+        exact hquot.prop_of_epi (factorThruImage f) hPcoprod
+      have hPY : P (Subobject.underlying.obj Y) := by
+        dsimp [Y,f]
+        rw [Subobject.sSup]
+        let e : Subobject.underlying.obj
+            (Subobject.mk (Limits.image.ι (Subobject.smallCoproductDesc subobjs))) ≅
+            Limits.image (Subobject.smallCoproductDesc subobjs) :=
+        Subobject.underlyingIso (Limits.image.ι (Subobject.smallCoproductDesc subobjs))
+        exact P.prop_of_iso e.symm hPimage
+      let ses := ShortComplex.mk Y.arrow (cokernel.π Y.arrow) (cokernel.condition Y.arrow)
+      have hs : ses.ShortExact := {
+        exact := by
+          exact ShortComplex.exact_of_g_is_cokernel ses
+            (cokernelIsCokernel ses.f)
+        mono_f := by infer_instance
+        epi_g := by infer_instance
+      }
+      have hcok : P.rightOrthogonal (cokernel Y.arrow) := by
+        sorry
       sorry
-
-
 
 end TorsionTheory
 
