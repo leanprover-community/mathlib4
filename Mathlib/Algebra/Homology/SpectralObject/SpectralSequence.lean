1d1e99053ecb6c789b60009e0acbb6102190113b
Joël Riou <37772949+joelriou@users.noreply.github.com>
2026-03-22 13:48:49 +0100

    Update Mathlib/Algebra/Homology/SpectralObject/SpectralSequence.lean
    Co-authored-by: smorel394 <67864981+smorel394@users.noreply.github.com>

--- Mathlib/Algebra/Homology/SpectralObject/SpectralSequence.lean
+++ Mathlib/Algebra/Homology/SpectralObject/SpectralSequence.lean
@@ -662,8 +662,7 @@ example (r : ℤ) (hr : 2 ≤ r) (p q : ℤ) (hp : 0 < p) :
     IsZero ((Y.E₂SpectralSequence.page r).X ⟨p, q⟩) := by
   apply isZero_spectralSequence_page_X_of_isZero_H' _ _ _ hr
   apply Y.isZero₂_of_isThirdQuadrant
-  simp
-  lia
+  cat_disch
 
 /-- The `E₂` homologial spectral sequence indexed by `ℕ × ℕ` attached to
 a third quadrant spectral object indexed by `EInt`. -/
