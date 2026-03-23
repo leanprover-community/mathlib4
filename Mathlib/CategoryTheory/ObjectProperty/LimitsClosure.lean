686686b8e3ac674295ded4240a51f77268043cd0
vihdzp <vi.hdz.p@gmail.com>
2026-03-23 00:07:21 -0600

    fix

--- Mathlib/CategoryTheory/ObjectProperty/LimitsClosure.lean
+++ Mathlib/CategoryTheory/ObjectProperty/LimitsClosure.lean
@@ -171,7 +171,7 @@ lemma strictLimitsClosureStep_strictLimitsClosureIter_eq_self :
     obtain ⟨m, hm, hm'⟩ : ∃ (m : Ordinal.{w}) (hm : m < κ.ord), ∀ (j : J a), o j ≤ m := by
       refine ⟨⨆ j, o ((equivShrink.{w} (J a)).symm j),
           Ordinal.iSup_lt_ord ?_ (fun _ ↦ ho _), fun j ↦ ?_⟩
-      · rw [hκ.cof_eq, ← hasCardinalLT_iff_cardinal_mk_lt _ κ,
+      · rw [hκ.cof_ord_eq, ← hasCardinalLT_iff_cardinal_mk_lt _ κ,
           ← hasCardinalLT_iff_of_equiv (equivShrink.{w} (J a))]
         exact h a
       · obtain ⟨j, rfl⟩ := (equivShrink.{w} (J a)).symm.surjective j
