0d9b4376a7d87c0d0158b217b51cef50836c662e
Kim Morrison <kim@tqft.net>
2026-03-23 03:53:37 +0000

    add note about meta import

--- Mathlib/Tactic/Simproc/Divisors.lean
+++ Mathlib/Tactic/Simproc/Divisors.lean
@@ -5,7 +5,7 @@ Authors: Paul Lezeau, Bhavik Mehta
 -/
 module
 
-public meta import Mathlib.NumberTheory.Divisors
+public meta import Mathlib.NumberTheory.Divisors -- TODO: check if `meta` is still needed after https://github.com/leanprover/lean4/pull/13043
 public meta import Mathlib.Tactic.ToAdditive
 public import Mathlib.Util.Qq
 
