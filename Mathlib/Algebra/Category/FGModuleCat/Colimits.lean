7dfd47aa5ead5aa4c5ef322a94b412adde91b6f1
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2026-03-24 14:19:31 +0000

    fix

--- Mathlib/Algebra/Category/FGModuleCat/Colimits.lean
+++ Mathlib/Algebra/Category/FGModuleCat/Colimits.lean
@@ -42,7 +42,7 @@ of a finite coproduct. -/
 instance (F : J ⥤ FGModuleCat k) :
     Module.Finite k (colimit (F ⋙ forget₂ (FGModuleCat k) (ModuleCat.{v} k)) : ModuleCat.{v} k) :=
 have (j : J) : Module.Finite k ((F ⋙ forget₂ (FGModuleCat k) (ModuleCat.{v} k)).obj j) :=
-    inferInstanceAs Module.Finite k (F.obj j)
+    inferInstanceAs <| Module.Finite k (F.obj j)
   Module.Finite.of_surjective
     (colimitQuotientCoproduct (F ⋙ forget₂ (FGModuleCat k) (ModuleCat.{v} k))).hom
     ((ModuleCat.epi_iff_surjective _).1 inferInstance)
