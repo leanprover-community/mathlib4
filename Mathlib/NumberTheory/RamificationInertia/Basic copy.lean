48ab4db7944d2e8e4313fa47ff24a5a7731d2075
tb65536 <thomas.l.browning@gmail.com>
2026-03-24 14:08:12 +0000

    faithfully flat

--- Mathlib/NumberTheory/RamificationInertia/Basic copy.lean
+++ Mathlib/NumberTheory/RamificationInertia/Basic copy.lean
@@ -11,41 +11,75 @@ public import Mathlib.RingTheory.DedekindDomain.Ideal.Lemmas
 public import Mathlib.RingTheory.Finiteness.Quotient
 public import Mathlib.RingTheory.Ideal.Norm.AbsNorm
 public import Mathlib.RingTheory.Length
-public import Mathlib.RingTheory.Flat.Basic
+public import Mathlib.RingTheory.Flat.FaithfullyFlat.Algebra
+public import Mathlib.LinearAlgebra.TensorProduct.Tower
 
 /-!
-# Ramification index and inertia degree
-
-Given `P : Ideal S` lying over `p : Ideal R` for the ring extension `f : R →+* S`
-(assuming `P` and `p` are prime or maximal where needed),
-the **ramification index** `Ideal.ramificationIdx f p P` is the multiplicity of `P` in `map f p`,
-and the **inertia degree** `Ideal.inertiaDeg f p P` is the degree of the field extension
-`(S / P) : (R / p)`.
-
-## Main results
-
-The main theorem `Ideal.sum_ramification_inertia` states that for all coprime `P` lying over `p`,
-`Σ P, ramification_idx f p P * inertia_deg f p P` equals the degree of the field extension
-`Frac(S) : Frac(R)`.
-
-## Implementation notes
-
-Often the above theory is set up in the case where:
-* `R` is the ring of integers of a number field `K`,
-* `L` is a finite separable extension of `K`,
-* `S` is the integral closure of `R` in `L`,
-* `p` and `P` are maximal ideals,
-* `P` is an ideal lying over `p`
-We will try to relax the above hypotheses as much as possible.
-
-## Notation
-
-In this file, `e` stands for the ramification index and `f` for the inertia degree of `P` over `p`,
-leaving `p` and `P` implicit.
+# Ramification index
 
 -/
 
 @[expose] public section
+
+section temp
+
+open TensorProduct
+
+theorem Submodule.baseChange_mono {R M : Type*} (A : Type*) [CommSemiring R]
+    [Semiring A] [Algebra R A] [AddCommMonoid M] [Module R M]
+    {N N' : Submodule R M} (h : N ≤ N') :
+    N.baseChange A ≤ N'.baseChange A := by
+  rw [Submodule.baseChange, LinearMap.baseChange, ← Submodule.subtype_comp_inclusion N N' h,
+    ← LinearMap.id_comp LinearMap.id, TensorProduct.AlgebraTensorModule.map_comp]
+  apply LinearMap.range_comp_le_range
+
+@[simp]
+theorem Submodule.baseChange_le_iff {R M A : Type*} [CommRing R]
+    [Ring A] [Algebra R A] [Module.FaithfullyFlat R A] [AddCommGroup M] [Module R M]
+    {N N' : Submodule R M} :
+    N.baseChange A ≤ N'.baseChange A ↔ N ≤ N' := by
+  refine ⟨fun h ↦ ?_, Submodule.baseChange_mono A⟩
+  rwa [← N'.ker_mkQ, LinearMap.le_ker_iff_comp_subtype_eq_zero,
+    Module.FaithfullyFlat.zero_iff_lTensor_zero R A (N'.mkQ.comp N.subtype),
+    LinearMap.lTensor_comp, ← LinearMap.range_le_ker_iff, lTensor_mkQ, ← restrictScalars_le R]
+
+theorem Submodule.baseChange_inj {R M A : Type*} [CommRing R]
+    [Ring A] [Algebra R A] [Module.FaithfullyFlat R A] [AddCommGroup M] [Module R M]
+    {N N' : Submodule R M} :
+    N.baseChange A = N'.baseChange A ↔ N = N' := by
+  simp [le_antisymm_iff]
+
+theorem Submodule.baseChange_injective {R M A : Type*} [CommRing R]
+    [Ring A] [Algebra R A] [Module.FaithfullyFlat R A] [AddCommGroup M] [Module R M]
+    {N N' : Submodule R M} (h : N.baseChange A = N'.baseChange A) :
+    N = N' :=
+  Submodule.baseChange_inj.mp h
+
+variable (R M S : Type*) [CommRing R] [CommRing S] [Algebra R S] [Module.FaithfullyFlat R S]
+  [AddCommGroup M] [Module R M]
+
+/-- `Submodule.baseChange` as an order embedding. -/
+def Submodule.baseChangeOrderEmbedding [Module.FaithfullyFlat R S] :
+    Submodule R M ↪o Submodule S (S ⊗[R] M) where
+  toFun := Submodule.baseChange S
+  inj' _ _ := Submodule.baseChange_injective
+  map_rel_iff' := Submodule.baseChange_le_iff
+
+variable {R M S}
+
+theorem IsNoetherian.ofFaithfullyFlat (h : IsNoetherian S (S ⊗[R] M)) : IsNoetherian R M := by
+  rw [isNoetherian_iff'] at h ⊢
+  exact OrderEmbedding.wellFoundedGT (Submodule.baseChangeOrderEmbedding R M S)
+
+theorem IsArtinian.ofFaithfullyFlat (h : IsArtinian S (S ⊗[R] M)) : IsArtinian R M := by
+  rw [isArtinian_iff] at h ⊢
+  exact OrderEmbedding.wellFounded (Submodule.baseChangeOrderEmbedding R M S) h
+
+theorem IsFiniteLength.ofFaithfullyFlat (h : IsFiniteLength S (S ⊗[R] M)) : IsFiniteLength R M := by
+  rw [isFiniteLength_iff_isNoetherian_isArtinian] at h ⊢
+  exact h.imp IsNoetherian.ofFaithfullyFlat IsArtinian.ofFaithfullyFlat
+
+end temp
 
 namespace Ideal
 
@@ -60,9 +94,10 @@ noncomputable def ramificationIdx : ℕ :=
 theorem ramificationIdx_tower [r.LiesOver q] [q.LiesOver p]
     [Module.Flat (Localization.AtPrime q) (Localization.AtPrime r)] :
     p.ramificationIdx r = p.ramificationIdx q * q.ramificationIdx r := by
-  have : r.LiesOver p := LiesOver.trans r q p
+  have : r.LiesOver p := LiesOver.trans r q p -- this line might not be necessary
   let Sq := Localization.AtPrime q
   let Tr := Localization.AtPrime r
+  have : Module.FaithfullyFlat Sq Tr := Module.FaithfullyFlat.of_flat_of_isLocalHom
   by_cases h : IsFiniteLength Sq (Sq ⧸ p.map (algebraMap R Sq))
   · rw [isFiniteLength_iff_exists_compositionSeries] at h
     obtain ⟨s, hs0, hs1⟩ := h
@@ -71,10 +106,8 @@ theorem ramificationIdx_tower [r.LiesOver q] [q.LiesOver p]
       rw [← Module.length_ne_top_iff, not_ne_iff] at h this
       rw [ramificationIdx, ramificationIdx, ramificationIdx, h, this, ENat.toNat_top, zero_mul]
     contrapose! h
-    let ψ : Sq ⧸ p.map (algebraMap R Sq) →+* Tr ⧸ p.map (algebraMap R Tr) := by
-      refine Ideal.quotientMap _ (algebraMap Sq Tr) ?_
-      rw [Ideal.map_le_iff_le_comap, comap_comap, ← IsScalarTower.algebraMap_eq]
-      exact le_comap_map
-    sorry
+    let f := (Ideal.quotientEquivAlgOfEq Tr (by rw [map_map, ← IsScalarTower.algebraMap_eq])).trans
+      (Algebra.TensorProduct.quotIdealMapEquivTensorQuot Tr (p.map (algebraMap R Sq)))
+    exact (f.toLinearEquiv.isFiniteLength h).ofFaithfullyFlat
 
 end Ideal
