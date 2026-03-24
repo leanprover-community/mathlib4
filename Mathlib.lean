1b624cd2ec4b59ebfc4f2e87fcbb717ef03d4e53
Joël Riou <joel.riou@universite-paris-saclay.fr>
2026-03-24 14:13:34 +0100

    fix

--- Mathlib.lean
+++ Mathlib.lean
@@ -7826,6 +7826,5 @@ public import Mathlib.Util.TermReduce
 public import Mathlib.Util.TransImports
 public import Mathlib.Util.WhatsNew
 public import Mathlib.Util.WithWeakNamespace
-public import Mathlib.test
 
 set_option linter.style.longLine false
