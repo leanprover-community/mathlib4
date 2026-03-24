ce074838e8b5ca186e406f6393868697fa98f4cd
David Loeffler <d.loeffler.01@cantab.net>
2026-03-24 16:16:06 +0100

    Fix copyright for CompactlyGenerated.lean

--- Mathlib/Topology/Algebra/ProperAction/CompactlyGenerated.lean
+++ Mathlib/Topology/Algebra/ProperAction/CompactlyGenerated.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2024 Anatole Dedeker. All rights reserved.
+Copyright (c) 2024 Étienne Marion. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Etienne Marion
+Authors: Étienne Marion
 -/
 module
 
