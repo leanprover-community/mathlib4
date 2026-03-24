3941e0ec4f18cf352a96e3bcda6073a4e31eee5e
Whysoserioushah <yx3021@ic.ac.uk>
2026-03-24 11:23:12 +0000

    shake

--- Mathlib/RepresentationTheory/Rep/Res.lean
+++ Mathlib/RepresentationTheory/Rep/Res.lean
@@ -6,8 +6,6 @@ Authors: Edison Xie
 module
 
 public import Mathlib.RepresentationTheory.Rep.Basic
-public import Mathlib.CategoryTheory.Action.Monoidal
-public import Mathlib.CategoryTheory.Action.Limits
 
 /-!
 # Restriction of representations
