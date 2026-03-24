b17fb7d00a08934b82fbd16cabb0471909100c39
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2026-03-24 11:34:56 +0100

    refactor

--- Mathlib/Probability/Kernel/Category/Stoch.lean
+++ Mathlib/Probability/Kernel/Category/Stoch.lean
@@ -24,7 +24,6 @@ The category of measurable spaces with Markov kernels is a Markov category.
 ## References
 * [A synthetic approach to
 Markov kernels, conditional independence and theorems on sufficient statistics][fritz2020]
-
 -/
 
 @[expose] public section
