6bf10c21e686c30d97795ab79aa7226a4cb92b5c
Frédéric Dupuis <31101893+dupuisf@users.noreply.github.com>
2026-03-24 00:04:41 -0400

    Update Mathlib/Algebra/GroupWithZero/Commute.lean
    Co-authored-by: Jireh Loreaux <loreaujy@gmail.com>

--- Mathlib/Algebra/GroupWithZero/Commute.lean
+++ Mathlib/Algebra/GroupWithZero/Commute.lean
@@ -38,7 +38,7 @@ theorem mul_inverse_rev {M₀} [CommMonoidWithZero M₀] (a b : M₀) :
     (a * b)⁻¹ʳ = b⁻¹ʳ * a⁻¹ʳ :=
   mul_inverse_rev' (Commute.all _ _)
 
-lemma inverse_pow (r : M₀) : ∀ n : ℕ, r⁻¹ʳ ^ n= (r ^ n)⁻¹ʳ
+lemma inverse_pow (r : M₀) : ∀ n : ℕ, r⁻¹ʳ ^ n = (r ^ n)⁻¹ʳ
   | 0 => by rw [pow_zero, pow_zero, Ring.inverse_one]
   | n + 1 => by
     rw [pow_succ', pow_succ, Ring.mul_inverse_rev' ((Commute.refl r).pow_left n),
