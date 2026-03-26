f2af7486a9df5d15dbc3d044227ddcc866e161d0
thorimur <68410468+thorimur@users.noreply.github.com>
2026-03-26 16:51:17 -0400

    chore: get names from declrange state

--- Mathlib/Tactic/Linter/TestLinter.lean
+++ Mathlib/Tactic/Linter/TestLinter.lean
@@ -12,12 +12,26 @@ import Mathlib.Lean.Elab.InfoTree
 
 open Lean Elab
 
-namespace Mathlib.Tactic
+/--
+If `pos` is a `String.Pos`, then `getNamesFrom pos` returns the array of identifiers
+for the names of the declarations whose syntax begins in position at least `pos`.
+-/
+def getNamesAndRanges {m} [Monad m] [MonadEnv m] [MonadFileMap m] (pos : String.Pos.Raw) :
+    m (Array (Name × DeclarationRanges)) := do
+  -- declarations from parallelism branches should not be interesting here, so use `local`
+  let drs := declRangeExt.getState (asyncMode := .local) (← getEnv)
+  let fm ← getFileMap
+  let pos := fm.toPosition pos
+  let mut nms := #[]
+  for nmWithRange in drs do
+    unless nmWithRange.2.range.pos.lt pos do
+      nms := nms.push nmWithRange
+  return nms
 
 /-- Does work. -/
 def workLinter : Linter where
-  run _ := do
-    for t in ← getInfoTrees do
-      let _ := t.getDeclsByBody
+  run cmd := do
+    let some pos := cmd.getPos? | return
+    let _ ← getNamesAndRanges pos
 
 initialize addLinter workLinter
