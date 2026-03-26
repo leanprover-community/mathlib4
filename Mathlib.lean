86fbd2b78d8e6e676c0b15280c27e553060709a1
Hang Lu Su <homeowmorphism@pm.me>
2026-03-26 09:32:48 +0000

    feat(GroupTheory/FinitelyPresentedGroup): add finitely presented groups, minimal API version (#36996)
    We define the notions of 
    * being finitely presented and show it holds under isomorphism. 
    * being finitely generated as a normal subgroup, and show it holds under surjective homomorphism.

--- Mathlib.lean
+++ Mathlib.lean
@@ -4536,6 +4536,7 @@ public import Mathlib.GroupTheory.Exponent
 public import Mathlib.GroupTheory.FiniteAbelian.Basic
 public import Mathlib.GroupTheory.FiniteAbelian.Duality
 public import Mathlib.GroupTheory.FiniteIndexNormalSubgroup
+public import Mathlib.GroupTheory.FinitelyPresentedGroup
 public import Mathlib.GroupTheory.Finiteness
 public import Mathlib.GroupTheory.FixedPointFree
 public import Mathlib.GroupTheory.Focal
