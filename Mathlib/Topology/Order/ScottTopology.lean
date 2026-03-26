de4755d5eb311a7c41a1eb62b8dbbfb2d1193281
vihdzp <vi.hdz.p@gmail.com>
2026-03-26 11:01:52 -0600

    golf

--- Mathlib/Topology/Order/ScottTopology.lean
+++ Mathlib/Topology/Order/ScottTopology.lean
@@ -215,29 +215,25 @@ variable [PartialOrder α]
 theorem isOpen_iff_dirSupInacc [IsScottHausdorff α .univ] : IsOpen s ↔ DirSupInacc s where
   mp h := dirSupInaccOn_univ.1 <| Topology.IsScottHausdorff.dirSupInaccOn_of_isOpen h
   mpr h := by
-    rw [Topology.IsScottHausdorff.isOpen_iff (D := .univ)]
+    rw [IsScottHausdorff.isOpen_iff (D := .univ)]
     intro t _ ht₀ ht₁ a ha has
     have := h ht₀ ht₁ ha has
-    by_cases ht : a ∈ t
-    · refine ⟨a, ht, fun b ⟨hba, hbt⟩ ↦ ?_⟩
-      obtain rfl := (ha.1 hbt).antisymm hba
-      exact has
-    · by_contra! H
-      have H : ∀ b : t, ∃ c, b.1 ≤ c ∧ c ∈ t ∧ c ∉ s := by simpa [Set.not_subset, and_assoc] using H
-      choose f hf using H
-      have := ht₀.to_subtype
-      have hft : Set.range f ⊆ t := by grind
-      apply (h (Set.range_nonempty f) _ _ has).ne_empty
-      · aesop
-      · intro a ha b hb
-        obtain ⟨c, hc, _, _⟩ := ht₁ _ (hft ha) _ (hft hb)
-        have := hf ⟨c, hc⟩
-        grind
-      · exact ⟨upperBounds_mono_set hft ha.1,
-          fun b hb ↦ ha.2 fun c hc ↦ (hf ⟨c, hc⟩).1.trans (hb <| by simp)⟩
+    by_contra! H
+    have H : ∀ b : t, ∃ c, b.1 ≤ c ∧ c ∈ t ∧ c ∉ s := by simpa [not_subset, and_assoc] using H
+    choose f hf using H
+    have := ht₀.to_subtype
+    have hft : range f ⊆ t := by grind
+    apply (h (range_nonempty f) _ _ has).ne_empty
+    · aesop
+    · intro a ha b hb
+      obtain ⟨c, hc, _, _⟩ := ht₁ _ (hft ha) _ (hft hb)
+      have := hf ⟨c, hc⟩
+      grind
+    · exact ⟨upperBounds_mono_set hft ha.1,
+        fun b hb ↦ ha.2 fun c hc ↦ (hf ⟨c, hc⟩).1.trans (hb <| by simp)⟩
 
 theorem isClosed_iff_dirSupClosed [IsScottHausdorff α .univ] : IsClosed s ↔ DirSupClosed s := by
-  rw [← isOpen_compl_iff, Topology.IsScottHausdorff.isOpen_iff_dirSupInacc, dirSupInacc_compl]
+  rw [← isOpen_compl_iff, isOpen_iff_dirSupInacc, dirSupInacc_compl]
 
 end PartialOrder
 end IsScottHausdorff
