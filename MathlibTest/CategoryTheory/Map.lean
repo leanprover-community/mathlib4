3e8c6653458dd67c581a9ff95512d10b16e8f8b0
Dagur Asgeirsson <dagurtomas@gmail.com>
2026-03-25 22:09:02 -0600

    add reassoc test

--- MathlibTest/CategoryTheory/Map.lean
+++ MathlibTest/CategoryTheory/Map.lean
@@ -1,6 +1,7 @@
 module
 
 public import Mathlib.Tactic.CategoryTheory.Map
+public import Mathlib.Tactic.CategoryTheory.Reassoc
 
 open CategoryTheory
 
@@ -10,7 +11,7 @@ universe v₁ v₂ u₁ u₂
 
 variable {C : Type u₁} [Category.{v₁} C]
 
-@[map]
+@[reassoc (attr := map)]
 lemma comp_map {x y z : C} (f : x ⟶ y) (g : y ⟶ z) (h : x ⟶ z) (w : f ≫ g = h) :
     f ≫ g = h := w
 
@@ -20,6 +21,13 @@ info: Tests.Map.comp_map_map.{u✝, v✝, u_1, u_2} {C : Type u_1} [Category.{u_
 -/
 #guard_msgs in
 #check comp_map_map
+
+/--
+info: Tests.Map.comp_map_assoc_map.{u✝, v✝, u_1, u_2} {C : Type u_1} [Category.{u_2, u_1} C] {x y z : C} (f : x ⟶ y)
+  (g : y ⟶ z) (h : x ⟶ z) (w : f ≫ g = h) {Z : C} (h✝ : z ⟶ Z) {D : Type u✝} [instD : Category.{v✝, u✝} D] (F : C ⥤ D) :
+  F.map f ≫ F.map g ≫ F.map h✝ = F.map h ≫ F.map h✝-/
+#guard_msgs in
+#check comp_map_assoc_map
 
 /-!
 `map_of%` pushes `Functor.map` through an equality and applies `simp only [Functor.map_comp]` on each
