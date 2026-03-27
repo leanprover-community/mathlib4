9c405f33215dc4ddfb9a4bbd120f57417e6fb497
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2026-03-27 17:02:58 +0100

    simplifications

--- Mathlib/AlgebraicGeometry/EllipticCurve/Affine/Point.lean
+++ Mathlib/AlgebraicGeometry/EllipticCurve/Affine/Point.lean
@@ -650,12 +650,11 @@ instance : InvolutiveNeg W'.Point where
     · rfl
     · simp only [neg_some, negY_negY]
 
-lemma X_eq_iff {P Q : W.Point} {x₁ y₁ x₂ y₂ : F} {hP' : W.Nonsingular x₁ y₁}
-    {hQ' : W.Nonsingular x₂ y₂} (hP : P = some x₁ y₁ hP') (hQ : Q = some x₂ y₂ hQ') :
-    x₁ = x₂ ↔ P = Q ∨ P = -Q := by
+lemma X_eq_iff {x₁ y₁ x₂ y₂ : F} {h₁ : W.Nonsingular x₁ y₁} {h₂ : W.Nonsingular x₂ y₂} :
+    x₁ = x₂ ↔ some x₁ y₁ h₁ = some x₂ y₂ h₂ ∨ some x₁ y₁ h₁ = -some x₂ y₂ h₂ := by
   refine ⟨fun H ↦ ?_, fun H ↦ by grind [neg_some]⟩
-  simp_rw [hP, hQ, neg_some, some.injEq, ← and_or_left]
-  exact ⟨H, Y_eq_of_X_eq hP'.1 hQ'.1 H⟩
+  simp_rw [neg_some, some.injEq, ← and_or_left]
+  exact ⟨H, Y_eq_of_X_eq h₁.1 h₂.1 H⟩
 
 variable [DecidableEq F] [DecidableEq K] [DecidableEq L]
 
@@ -846,15 +845,16 @@ end Point
 /-!
 ### The x-coordinate map to ℙ¹
 
-We define the map from affine points of an affine Weierstrass curve over `R` to the projective line
+We define the map from points on an affine Weierstrass curve over `R` to the projective line
 by producing a coordinate vector in `Fin 2 → R` that represents the projective point.
 -/
 
 namespace Point
 
-/-- This map sends an affine point `P` on `W'` to a representative of its image on ℙ¹
-under the x-coordinate map. We take `![1, 0]` for the point at infinity and `![x, 1]`,
-where `x` is the x-coordinate of `P` for a finite point.
+/-- This map sends a point `P` on a Weierstrass curve `W'` in affine coordinates
+to a representative of its image on ℙ¹ under the x-coordinate map.
+We take `![1, 0]` for the point at infinity and `![x, 1]`,
+where `x` is the x-coordinate of `P`, for an affine point.
 
 We define it in the general setting of a commutative base ring, even though the definition
 of points in this setting is not really correct. For Weierstrass curves over fields, this
@@ -885,9 +885,9 @@ lemma eq_or_eq_neg_of_xRep_eq_xRep {P Q : W.Point} (h : P.xRep = Q.xRep) : P = Q
   | 0, 0 => exact .inl rfl
   | 0, some .. => simp [xRep] at h
   | some .., 0 => simp [xRep] at h
-  | some x₁ _ _, some x₂ _ _ =>
+  | some x₁ .., some x₂ .. =>
     simp only [xRep, Matrix.vecCons_inj, and_true] at h
-    exact (X_eq_iff rfl rfl).mp h
+    exact X_eq_iff.mp h
 
 lemma xRep_eq_xRep_iff {P Q : W.Point} : P.xRep = Q.xRep ↔ P = Q ∨ P = -Q := by
   refine ⟨eq_or_eq_neg_of_xRep_eq_xRep, fun H ↦ ?_⟩
