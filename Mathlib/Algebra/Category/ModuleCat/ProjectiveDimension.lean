d9086c5e297f0a935df83b7df3943ece443abb05
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2026-03-27 22:23:01 +0800

    golf variable

--- Mathlib/Algebra/Category/ModuleCat/ProjectiveDimension.lean
+++ Mathlib/Algebra/Category/ModuleCat/ProjectiveDimension.lean
@@ -20,7 +20,7 @@ public import Mathlib.CategoryTheory.Abelian.Projective.Dimension
 
 universe v v' u u'
 
-variable {R : Type u} [CommRing R]
+variable {R : Type u} [CommRing R] [Small.{v} R]
 
 open CategoryTheory Abelian Module
 
@@ -28,18 +28,19 @@ namespace CategoryTheory
 
 section
 
-variable {R' : Type u'} [CommRing R'] (e : R ≃+* R')
+variable {R' : Type u'} [CommRing R'] [Small.{v'} R'] (e : R ≃+* R')
+
+variable {M : ModuleCat.{v} R} {N : ModuleCat.{v'} R'}
 
 set_option backward.isDefEq.respectTransparency false in
 attribute [local instance] RingHomInvPair.of_ringEquiv in
-lemma hasProjectiveDimensionLE_of_semiLinearEquiv [Small.{v} R] [Small.{v'} R']
-    {M : ModuleCat.{v} R} {N : ModuleCat.{v'} R'} (e' : M ≃ₛₗ[RingHomClass.toRingHom e] N)
-    (n : ℕ) [h : HasProjectiveDimensionLE M n] : HasProjectiveDimensionLE N n := by
+lemma hasProjectiveDimensionLE_of_semiLinearEquiv (e' : M ≃ₛₗ[RingHomClass.toRingHom e] N)
+    (n : ℕ) [HasProjectiveDimensionLE M n] : HasProjectiveDimensionLE N n := by
   induction n generalizing M N e' with
   | zero =>
-    simp only [HasProjectiveDimensionLE, zero_add,
-      ← projective_iff_hasProjectiveDimensionLT_one] at h ⊢
-    rw [← IsProjective.iff_projective] at h ⊢
+    have : HasProjectiveDimensionLE M 0 := ‹_›
+    simp only [HasProjectiveDimensionLE, zero_add] at this ⊢
+    rw [← projective_iff_hasProjectiveDimensionLT_one, ← IsProjective.iff_projective] at this ⊢
     exact Projective.of_equiv e'
   | succ n ih =>
     let S := M.projectiveShortComplex
@@ -58,12 +59,11 @@ lemma hasProjectiveDimensionLE_of_semiLinearEquiv [Small.{v} R] [Small.{v'} R']
     rw [Submodule.comap_equiv_eq_map_symm] at this
     let eker : S.X₁ ≃ₛₗ[RingHomClass.toRingHom e] S'.X₁ :=
       (LinearEquiv.ofEq _ _ this).trans (e2.symm.submoduleMap S'.g.hom.ker).symm
-    have := (S_exact.hasProjectiveDimensionLT_X₃_iff n inferInstance).mp h
+    have := (S_exact.hasProjectiveDimensionLT_X₃_iff n inferInstance).mp ‹_›
     exact (S'_exact.hasProjectiveDimensionLT_X₃_iff n inferInstance).mpr (ih eker)
 
 attribute [local instance] RingHomInvPair.of_ringEquiv in
-lemma projectiveDimension_eq_of_semiLinearEquiv [Small.{v} R] [Small.{v'} R']
-    {M : ModuleCat.{v} R} {N : ModuleCat.{v'} R'} (e' : M ≃ₛₗ[RingHomClass.toRingHom e] N) :
+lemma projectiveDimension_eq_of_semiLinearEquiv (e' : M ≃ₛₗ[RingHomClass.toRingHom e] N) :
     projectiveDimension M = projectiveDimension N := by
   refine eq_of_forall_ge_iff (fun N ↦ ?_)
   induction N with
@@ -82,13 +82,13 @@ end
 
 section
 
-lemma hasProjectiveDimensionLE_of_linearEquiv [Small.{v} R] [Small.{v'} R]
-    {M : ModuleCat.{v} R} {N : ModuleCat.{v'} R} (e' : M ≃ₗ[R] N)
-    (n : ℕ) [h : HasProjectiveDimensionLE M n] : HasProjectiveDimensionLE N n :=
-  hasProjectiveDimensionLE_of_semiLinearEquiv (RingEquiv.refl R) e' n
+variable [Small.{v'} R] {M : ModuleCat.{v} R} {N : ModuleCat.{v'} R}
 
-lemma projectiveDimension_eq_of_linearEquiv [Small.{v} R] [Small.{v'} R]
-    {M : ModuleCat.{v} R} {N : ModuleCat.{v'} R} (e : M ≃ₗ[R] N) :
+lemma hasProjectiveDimensionLE_of_linearEquiv (e : M ≃ₗ[R] N)
+    (n : ℕ) [HasProjectiveDimensionLE M n] : HasProjectiveDimensionLE N n :=
+  hasProjectiveDimensionLE_of_semiLinearEquiv (RingEquiv.refl R) e n
+
+lemma projectiveDimension_eq_of_linearEquiv (e : M ≃ₗ[R] N) :
     projectiveDimension M = projectiveDimension N :=
   projectiveDimension_eq_of_semiLinearEquiv (M := M) (N := N) (RingEquiv.refl R) e
 
