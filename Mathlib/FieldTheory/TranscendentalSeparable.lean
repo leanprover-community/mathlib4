f8f5894f0b703e3bfef7f0877f313811ffe76974
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2026-03-27 22:13:01 +0800

    Create TranscendentalSeparable.lean
    add basic definitions

---
+++ Mathlib/FieldTheory/TranscendentalSeparable.lean
@@ -0,0 +1,28 @@
+/-
+Copyright (c) 2026 Nailin Guan. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Nailin Guan
+-/
+module
+
+public import Mathlib.FieldTheory.Separable
+public import Mathlib.RingTheory.AlgebraicIndependent.TranscendenceBasis
+
+/-!
+# Transcendental separable extensions
+-/
+
+universe u v
+
+variable (R : Type u) (A : Type v) [CommRing R] [CommRing A] [Algebra R A]
+
+@[expose] public section
+
+class Algebra.IsSeparablyGenerated : Prop where
+  isSeparable' : ∃ (ι : Type v) (f : ι → A),
+    IsTranscendenceBasis R f ∧
+    Algebra.IsSeparable (Algebra.adjoin R (Set.range f)) A
+
+class Algebra.IsTranscendentalSeparable : Prop where
+  forall_isSeparablyGenerated : ∀ (A' : Subalgebra R A),
+    Algebra.EssFiniteType R A' → Algebra.IsSeparablyGenerated R A'
