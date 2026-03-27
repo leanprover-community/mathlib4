8779935e999afc986017b8ff0c40c42d8ef26c6f
Winston Yin <winstonyin@gmail.com>
2026-03-27 19:26:08 -0400

    fix

--- Mathlib/Algebra/BigOperators/Finprod.lean
+++ Mathlib/Algebra/BigOperators/Finprod.lean
@@ -609,7 +609,7 @@ lemma finprod_zero_le_one {M α : Type*} [CommMonoidWithZero M] [PartialOrder M]
     ∏ᶠ _ : α, (0 : M) ≤ 1 := by
   rw [← finprod_one (α := α)]
   by_cases H : (fun _ : α ↦ (0 : M)).HasFiniteMulSupport
-  · exact finprod_le_finprod H (fun _ ↦ le_rfl) (by fun_prop) fun _ ↦ zero_le_one
+  · exact finprod_le_finprod₀ H (fun _ ↦ le_rfl) (by fun_prop) fun _ ↦ zero_le_one
   · rw [finprod_of_not_hasFiniteMulSupport H]
     exact finprod_one.symm.le
 
