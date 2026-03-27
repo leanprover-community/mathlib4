41f6322f67f4192861dc49b6ce2fec64898438f5
Yuma Mizuno <mizuno.y.aj@gmail.com>
2026-03-27 14:26:51 +0000

    Update Basic.lean

--- Mathlib/Algebra/MvLaurentPolynomial/Basic.lean
+++ Mathlib/Algebra/MvLaurentPolynomial/Basic.lean
@@ -47,14 +47,14 @@ section CommSemiring
 variable [CommSemiring R] [AddCommGroup M] {p q : AddMonoidAlgebra R M}
 
 /-- The Laurent monomial with exponent vector `d`, expressed in the basis `v`. -/
-def monomial (v : Basis σ ℤ M) (d : σ →₀ ℤ) : R →ₗ[R] AddMonoidAlgebra R M :=
-  AddMonoidAlgebra.lsingle (v.repr.symm d)
+def monomial (v : σ → M) (d : σ →₀ ℤ) (r : R) : AddMonoidAlgebra R M :=
+  AddMonoidAlgebra.single (Finsupp.linearCombination ℤ v d) r
 
-theorem one_def (v : Basis σ ℤ M) : (1 : AddMonoidAlgebra R M) = monomial v 0 1 := by
+theorem one_def (v : σ → M) : (1 : AddMonoidAlgebra R M) = monomial v 0 1 := by
   simp [monomial, AddMonoidAlgebra.one_def]
 
-theorem single_eq_monomial (v : Basis σ ℤ M) (d : σ →₀ ℤ) (r : R) :
-    (AddMonoidAlgebra.single (v.repr.symm d) r : AddMonoidAlgebra R M) = monomial v d r :=
+theorem single_eq_monomial (v : σ → M) (d : σ →₀ ℤ) (r : R) :
+    AddMonoidAlgebra.single (Finsupp.linearCombination ℤ v d) r = monomial v d r := by
   rfl
 
 theorem mul_def (v : Basis σ ℤ M) :
@@ -62,17 +62,17 @@ theorem mul_def (v : Basis σ ℤ M) :
   simp [AddMonoidAlgebra.mul_def, monomial]
 
 /-- The Laurent variable corresponding to the basis vector indexed by `i`. -/
-abbrev X (v : σ → M) (i : σ) : AddMonoidAlgebra R M := AddMonoidAlgebra.single (v i) (1 : R)
+abbrev X (v : σ → M) (i : σ) : AddMonoidAlgebra R M := monomial v (Finsupp.single i 1) 1
 
-theorem X_eq_monomial (v : Basis σ ℤ M) (i : σ) : X v i = monomial v (Finsupp.single i 1) (1 : R) :=
-  rfl
+theorem X_eq_monomial (v : σ → M) (i : σ) :
+    X v i = monomial v (Finsupp.single i 1) (1 : R) := by
+  simp [X, monomial]
 
 theorem monomial_left_injective (v : Basis σ ℤ M) {r : R} (hr : r ≠ 0) :
-    Function.Injective fun d : σ →₀ ℤ => monomial v d r := by
+    Function.Injective fun d : σ →₀ ℤ ↦ monomial v d r := by
   intro d e h
-  exact v.repr.symm.injective (Finsupp.single_left_injective hr h)
+  exact v.repr.symm.injective (Finsupp.single_left_injective hr (by simpa using h))
 
-@[simp]
 theorem monomial_left_inj (v : Basis σ ℤ M) {d e : σ →₀ ℤ} {r : R} (hr : r ≠ 0) :
     monomial v d r = monomial v e r ↔ d = e :=
   (monomial_left_injective v hr).eq_iff
@@ -85,86 +85,69 @@ theorem X_injective (v : Basis σ ℤ M) [Nontrivial R] :
     Function.Injective (X v : σ → AddMonoidAlgebra R M) :=
   (monomial_left_injective v one_ne_zero).comp (Finsupp.single_left_injective (one_ne_zero))
 
-@[simp]
 theorem X_inj (v : Basis σ ℤ M) [Nontrivial R] {i j : σ} :
-    X v i = (X v j : AddMonoidAlgebra R M) ↔ i = j := by
-  constructor
-  · intro h
-    exact X_injective v h
-  · intro h
-    simp [h]
+    X v i = (X v j : AddMonoidAlgebra R M) ↔ i = j :=
+  ⟨fun h ↦ X_injective v h, fun h ↦ congrArg _ h⟩
 
-theorem monomial_pow (v : Basis σ ℤ M) (d : σ →₀ ℤ) (r : R) (n : ℕ) :
+theorem monomial_pow (v : σ → M) (d : σ →₀ ℤ) (r : R) (n : ℕ) :
     monomial v d r ^ n = monomial v (n • d) (r ^ n) := by
-  simp only [monomial, Basis.repr_symm_apply, lsingle_apply, single_pow,
-    LinearMap.map_smul_of_tower]
+  simp only [monomial, single_pow, LinearMap.map_smul_of_tower]
 
-theorem X_pow_eq_monomial (v : Basis σ ℤ M) (i : σ) (e : ℕ) :
+theorem X_pow_eq_monomial (v : σ → M) (i : σ) (e : ℕ) :
     X v i ^ e = monomial v (Finsupp.single i (e : ℤ)) (1 : R) := by
-  simp only [X, single_pow, one_pow]
-  simp [monomial]
+  simp only [X, monomial, Finsupp.linearCombination_single, one_smul, single_pow, one_pow,
+    natCast_zsmul]
 
 @[simp]
-theorem monomial_mul (v : Basis σ ℤ M) {d e : σ →₀ ℤ} {r s : R} :
+theorem monomial_mul (v : σ → M) {d e : σ →₀ ℤ} {r s : R} :
     monomial v d r * monomial v e s = monomial v (d + e) (r * s) := by
   simp [monomial, AddMonoidAlgebra.single_mul_single]
 
-@[simp]
-theorem monomial_mul_monomial_neg (v : Basis σ ℤ M) (d : σ →₀ ℤ) :
-    monomial v d (1 : R) * monomial v (-d) 1 = 1 := by
-  rw [monomial_mul, add_neg_cancel, one_mul, ← one_def v]
+theorem isUnit_monomial (v : σ → M) (d : σ →₀ ℤ) :
+    IsUnit (monomial v d (1 : R) : AddMonoidAlgebra R M) :=
+  ⟨⟨monomial v d 1, monomial v (-d) 1, by simp [← one_def v], by simp [← one_def v],⟩, rfl⟩
 
-@[simp]
-theorem monomial_neg_mul_monomial (v : Basis σ ℤ M) (d : σ →₀ ℤ) :
-    monomial v (-d) (1 : R) * monomial v d 1 = 1 := by
-  rw [mul_comm, monomial_mul_monomial_neg]
-
-theorem isUnit_monomial (v : Basis σ ℤ M) (d : σ →₀ ℤ) :
-    IsUnit (monomial v d (1 : R) : AddMonoidAlgebra R M) := by
-  refine ⟨⟨monomial v d 1, monomial v (-d) 1, monomial_mul_monomial_neg v d,
-      monomial_neg_mul_monomial v d⟩, rfl⟩
-
-theorem monomial_add_single (v : Basis σ ℤ M) (d : σ →₀ ℤ) (i : σ) (e : ℕ) (r : R) :
+theorem monomial_add_single (v : σ → M) (d : σ →₀ ℤ) (i : σ) (e : ℕ) (r : R) :
     monomial v (d + Finsupp.single i (e : ℤ)) r = monomial v d r * X v i ^ e := by
   rw [X_pow_eq_monomial, monomial_mul, mul_one]
 
-theorem monomial_single_add (v : Basis σ ℤ M) (d : σ →₀ ℤ) (i : σ) (e : ℕ) (r : R) :
+theorem monomial_single_add (v : σ → M) (d : σ →₀ ℤ) (i : σ) (e : ℕ) (r : R) :
     monomial v (Finsupp.single i (e : ℤ) + d) r = X v i ^ e * monomial v d r := by
   rw [X_pow_eq_monomial, monomial_mul, one_mul]
 
 @[simp]
-theorem monomial_zero (v : Basis σ ℤ M) {d : σ →₀ ℤ} : monomial v d (0 : R) = 0 := by
-  rw [monomial, AddMonoidAlgebra.lsingle_apply]
+theorem monomial_zero (v : σ → M) {d : σ →₀ ℤ} : monomial v d (0 : R) = 0 := by
+  rw [monomial]
   exact Finsupp.single_eq_zero.2 rfl
 
 @[simp]
-theorem monomial_eq_zero (v : Basis σ ℤ M) {d : σ →₀ ℤ} {r : R} :
+theorem monomial_eq_zero (v : σ → M) {d : σ →₀ ℤ} {r : R} :
     monomial v d r = 0 ↔ r = 0 := by
   simp [monomial]
 
 @[simp]
-theorem sum_monomial_eq {A : Type*} [AddCommMonoid A] (v : Basis σ ℤ M) {d : σ →₀ ℤ} {r : R}
-    {f : M → R → A} (h0 : f (v.repr.symm d) 0 = 0) :
-    (monomial v d r).sum f = f (v.repr.symm d) r := by
+theorem sum_monomial_eq {A : Type*} [AddCommMonoid A] (v : σ → M) {d : σ →₀ ℤ} {r : R}
+    {f : M → R → A} (h0 : f ((Finsupp.linearCombination ℤ v) d) 0 = 0) :
+    (monomial v d r).sum f = f (Finsupp.linearCombination ℤ v d) r := by
   simpa [monomial] using (Finsupp.sum_single_index h0 : _)
 
-theorem support_monomial (v : Basis σ ℤ M) {d : σ →₀ ℤ} {r : R} [Decidable (r = 0)] :
-    (monomial v d r : AddMonoidAlgebra R M).support = if r = 0 then ∅ else {v.repr.symm d} := by
+theorem support_monomial (v : σ → M) {d : σ →₀ ℤ} {r : R} [Decidable (r = 0)] :
+    (monomial v d r : AddMonoidAlgebra R M).support =
+      if r = 0 then ∅ else {Finsupp.linearCombination ℤ v d} := by
   by_cases hr : r = 0
   · simp [monomial, hr]
-  · simpa [monomial, hr] using Finsupp.support_single_ne_zero (v.repr.symm d) hr
+  · simpa [monomial, hr] using Finsupp.support_single_ne_zero (Finsupp.linearCombination ℤ v d) hr
 
-theorem support_monomial_subset (v : Basis σ ℤ M) (d : σ →₀ ℤ) (r : R) :
-    (monomial v d r : AddMonoidAlgebra R M).support ⊆ {v.repr.symm d} := by
+theorem support_monomial_subset (v : σ → M) (d : σ →₀ ℤ) (r : R) :
+    (monomial v d r : AddMonoidAlgebra R M).support ⊆ {Finsupp.linearCombination ℤ v d} := by
   classical
   by_cases hr : r = 0
   · simp [monomial, hr]
   · simp [support_monomial v, hr]
 
-@[simp]
-theorem X_ne_zero (v : Basis σ ℤ M) [Nontrivial R] (i : σ) :
-    X v i ≠ (0 : AddMonoidAlgebra R M) :=
-  (monomial_eq_zero v).not.2 one_ne_zero
+theorem X_ne_zero (v : σ → M) [Nontrivial R] (i : σ) :
+    X v i ≠ (0 : AddMonoidAlgebra R M) := by
+  simp
 
 section AsSum
 
