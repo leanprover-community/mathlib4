896918261d54e181309a42677655180dab492156
artie2000 <artem.khovanov@gmail.com>
2026-03-28 01:20:00 +0000

    initial commit

--- Mathlib/Algebra/Order/Group/Cone.lean
+++ Mathlib/Algebra/Order/Group/Cone.lean
@@ -5,30 +5,73 @@ Authors: Mario Carneiro, Kim Morrison, Artie Khovanov
 -/
 module
 
-public import Mathlib.Algebra.Group.Subgroup.Defs
+public import Mathlib.Algebra.Group.Submonoid.Support
 public import Mathlib.Algebra.Order.Group.Unbundled.Basic
 public import Mathlib.Algebra.Order.Monoid.Submonoid
 
 /-!
 # Construct ordered groups from groups with a specified positive cone.
 
-In this file we provide the structure `GroupCone` and the predicate `IsMaxCone`
-that encode axioms of `OrderedCommGroup` and `LinearOrderedCommGroup`
-in terms of the subset of non-negative elements.
+We demonstrate the equivalence between order structures on a group `G` and *positive cones*:
+submonoids `M` of `G` such that `M ∩ -M = 0`.
 
-We also provide constructors that convert between
-cones in groups and the corresponding ordered groups.
 -/
 
 @[expose] public section
 
+variable (G : Type*) [CommGroup G]
+
+@[to_additive]
+theorem Submonoid.oneLE.isMulPointed [PartialOrder G] [IsOrderedMonoid G] :
+    (oneLE G).IsMulPointed := by aesop (add simp ge_antisymm_iff)
+
+@[to_additive]
+theorem Submonoid.oneLE.isMulSpanning [LinearOrder G] [IsOrderedMonoid G] :
+    (oneLE G).IsMulSpanning := by aesop (add safe le_total)
+
+variable {G} {M : Submonoid G} (hM : M.IsMulPointed)
+
+/-- Construct a partial order by designating a submonoid with zero support in an abelian group. -/
+@[to_additive
+/-- Construct a partial order by designating a submonoid with zero support in an abelian group. -/]
+abbrev PartialOrder.mkOfSubmonoid : PartialOrder G where
+  le a b := b / a ∈ M
+  le_refl a := by simp [one_mem]
+  le_trans a b c nab nbc := by simpa using mul_mem nbc nab
+  le_antisymm a b nab nba := by
+    simpa [div_eq_one, eq_comm] using hM.eq_one_of_mem_of_inv_mem nab (by simpa using nba)
+
+variable {hM} in
+@[to_additive (attr := simp)]
+theorem PartialOrder.mkOfSubmonoid_le_iff {a b : G} :
+    (mkOfSubmonoid hM).le a b ↔ b / a ∈ M := .rfl
+
+@[to_additive]
+theorem IsOrderedMonoid.mkOfSubmonoid :
+    letI _ := PartialOrder.mkOfSubmonoid hM
+    IsOrderedMonoid G :=
+  letI _ := PartialOrder.mkOfSubmonoid hM
+  { mul_le_mul_left := fun a b nab c ↦ by simpa [· ≤ ·] using nab }
+
+/-- Construct a linear order by designating
+    a maximal submonoid with zero support in an abelian group. -/
+@[to_additive
+/-- Construct a linear order by designating
+    a maximal submonoid with zero support in an abelian group. -/]
+abbrev LinearOrder.mkOfSubmonoid (hMs : M.IsMulSpanning) [DecidablePred (· ∈ M)] :
+    LinearOrder G where
+  __ := PartialOrder.mkOfSubmonoid hM
+  le_total a b := by simpa using hMs.mem_or_inv_mem (b / a)
+  toDecidableLE _ := _
+
 /-- `AddGroupConeClass S G` says that `S` is a type of cones in `G`. -/
+@[deprecated "Unbundled to `AddSubmonoid.IsPointed`" (since := "2026-03-28")]
 class AddGroupConeClass (S : Type*) (G : outParam Type*) [AddCommGroup G] [SetLike S G] : Prop
     extends AddSubmonoidClass S G where
   eq_zero_of_mem_of_neg_mem {C : S} {a : G} : a ∈ C → -a ∈ C → a = 0
 
 /-- `GroupConeClass S G` says that `S` is a type of cones in `G`. -/
-@[to_additive]
+@[to_additive, deprecated "Unbundled to `Submonoid.IsMulPointed`" (since := "2026-03-28")]
 class GroupConeClass (S : Type*) (G : outParam Type*) [CommGroup G] [SetLike S G] : Prop
     extends SubmonoidClass S G where
   eq_one_of_mem_of_inv_mem {C : S} {a : G} : a ∈ C → a⁻¹ ∈ C → a = 1
@@ -40,6 +83,7 @@ export AddGroupConeClass (eq_zero_of_mem_of_neg_mem)
 does not contain both `a` and `-a` for any nonzero `a`.
 This is equivalent to being the set of non-negative elements of
 some order making the group into a partially ordered group. -/
+@[deprecated "Unbundled to `AddSubmonoid.IsPointed`" (since := "2026-03-28")]
 structure AddGroupCone (G : Type*) [AddCommGroup G] extends AddSubmonoid G where
   eq_zero_of_mem_of_neg_mem' {a} : a ∈ carrier → -a ∈ carrier → a = 0
 
@@ -47,19 +91,19 @@ structure AddGroupCone (G : Type*) [AddCommGroup G] extends AddSubmonoid G where
 does not contain both `a` and `a⁻¹` for any non-identity `a`.
 This is equivalent to being the set of elements that are at least 1 in
 some order making the group into a partially ordered group. -/
-@[to_additive]
+@[to_additive, deprecated "Unbundled to `Submonoid.IsMulPointed`" (since := "2026-03-28")]
 structure GroupCone (G : Type*) [CommGroup G] extends Submonoid G where
   eq_one_of_mem_of_inv_mem' {a} : a ∈ carrier → a⁻¹ ∈ carrier → a = 1
 
-@[to_additive]
+@[to_additive (attr := deprecated (since := "2026-03-28"))]
 instance GroupCone.instSetLike (G : Type*) [CommGroup G] : SetLike (GroupCone G) G where
   coe C := C.carrier
   coe_injective' p q h := by cases p; cases q; congr; exact SetLike.ext' h
 
-@[to_additive]
+@[to_additive (attr := deprecated (since := "2026-03-28"))]
 instance (G : Type*) [CommGroup G] : PartialOrder (GroupCone G) := .ofSetLike (GroupCone G) G
 
-@[to_additive]
+@[to_additive (attr := deprecated (since := "2026-03-28"))]
 instance GroupCone.instGroupConeClass (G : Type*) [CommGroup G] :
     GroupConeClass (GroupCone G) G where
   mul_mem {C} := C.mul_mem'
@@ -74,19 +118,20 @@ variable {H : Type*} [CommGroup H] [PartialOrder H] [IsOrderedMonoid H] {a : H}
 
 variable (H) in
 /-- The cone of elements that are at least 1. -/
-@[to_additive /-- The cone of non-negative elements. -/]
+@[to_additive (attr := deprecated Submonoid.oneLE.isMulPointed (since := "2026-03-28"))
+/-- The cone of non-negative elements. -/]
 def oneLE : GroupCone H where
   __ := Submonoid.oneLE H
   eq_one_of_mem_of_inv_mem' {a} := by simpa using ge_antisymm
 
-@[to_additive (attr := simp)]
+@[to_additive (attr := simp, deprecated (since := "2026-03-28"))]
 lemma oneLE_toSubmonoid : (oneLE H).toSubmonoid = .oneLE H := rfl
-@[to_additive (attr := simp)]
+@[to_additive (attr := simp, deprecated (since := "2026-03-28"))]
 lemma mem_oneLE : a ∈ oneLE H ↔ 1 ≤ a := Iff.rfl
-@[to_additive (attr := simp, norm_cast)]
+@[to_additive (attr := simp, norm_cast, deprecated (since := "2026-03-28"))]
 lemma coe_oneLE : oneLE H = {x : H | 1 ≤ x} := rfl
 
-@[to_additive]
+@[to_additive (attr := deprecated Submonoid.oneLE.isMulSpanning (since := "2026-03-28"))]
 instance oneLE.hasMemOrInvMem {H : Type*} [CommGroup H] [LinearOrder H] [IsOrderedMonoid H] :
     HasMemOrInvMem (oneLE H) where
   mem_or_inv_mem := by simpa using le_total 1
@@ -96,7 +141,8 @@ end GroupCone
 variable {S G : Type*} [CommGroup G] [SetLike S G] (C : S)
 
 /-- Construct a partial order by designating a cone in an abelian group. -/
-@[to_additive /-- Construct a partial order by designating a cone in an abelian group. -/]
+@[to_additive (attr := deprecated PartialOrder.mkOfSubmonoid (since := "2026-03-28"))
+/-- Construct a partial order by designating a cone in an abelian group. -/]
 abbrev PartialOrder.mkOfGroupCone [GroupConeClass S G] : PartialOrder G where
   le a b := b / a ∈ C
   le_refl a := by simp [one_mem]
@@ -104,13 +150,14 @@ abbrev PartialOrder.mkOfGroupCone [GroupConeClass S G] : PartialOrder G where
   le_antisymm a b nab nba := by
     simpa [div_eq_one, eq_comm] using eq_one_of_mem_of_inv_mem nab (by simpa using nba)
 
-@[to_additive (attr := simp)]
+@[to_additive (attr := simp, deprecated PartialOrder.mkOfSubmonoid_le_iff (since := "2026-03-28"))]
 lemma PartialOrder.mkOfGroupCone_le_iff {S G : Type*} [CommGroup G] [SetLike S G]
     [GroupConeClass S G] {C : S} {a b : G} :
     (mkOfGroupCone C).le a b ↔ b / a ∈ C := Iff.rfl
 
 /-- Construct a linear order by designating a maximal cone in an abelian group. -/
-@[to_additive /-- Construct a linear order by designating a maximal cone in an abelian group. -/]
+@[to_additive (attr := deprecated LinearOrder.mkOfSubmonoid (since := "2026-03-28"))
+/-- Construct a linear order by designating a maximal cone in an abelian group. -/]
 abbrev LinearOrder.mkOfGroupCone
     [GroupConeClass S G] [HasMemOrInvMem C] [DecidablePred (· ∈ C)] : LinearOrder G where
   __ := PartialOrder.mkOfGroupCone C
@@ -118,7 +165,7 @@ abbrev LinearOrder.mkOfGroupCone
   toDecidableLE _ := _
 
 /-- Construct a partially ordered abelian group by designating a cone in an abelian group. -/
-@[to_additive
+@[to_additive (attr := deprecated IsOrderedMonoid.mkOfSubmonoid (since := "2026-03-28"))
   /-- Construct a partially ordered abelian group by designating a cone in an abelian group. -/]
 lemma IsOrderedMonoid.mkOfCone [GroupConeClass S G] :
     let _ : PartialOrder G := PartialOrder.mkOfGroupCone C
--- Mathlib/Algebra/Order/Ring/Cone.lean
+++ Mathlib/Algebra/Order/Ring/Cone.lean
@@ -11,17 +11,33 @@ public import Mathlib.Algebra.Ring.Subsemiring.Order
 /-!
 # Construct ordered rings from rings with a specified positive cone.
 
-In this file we provide the structure `RingCone`
-that encodes axioms of `OrderedRing` and `LinearOrderedRing`
-in terms of the subset of non-negative elements.
+We demonstrate the equivalence between order structures on a ring `R` and *positive cones*:
+subsemirings `S` of `R` such that `S ∩ -S = 0`.
 
-We also provide constructors that convert between
-cones in rings and the corresponding ordered rings.
 -/
 
 @[expose] public section
 
+variable (R : Type*) [Ring R]
+
+theorem Subsemiring.nonneg.isPointed [PartialOrder R] [IsOrderedRing R] :
+    (Subsemiring.nonneg R).IsPointed := AddSubmonoid.nonneg.isAddPointed R
+
+theorem Subsemiring.nonneg.isSpanning [LinearOrder R] [IsOrderedRing R] :
+    (Subsemiring.nonneg R).IsSpanning := AddSubmonoid.nonneg.isAddSpanning R
+
+variable {R} {S : Subsemiring R} (hS : S.IsPointed)
+
+theorem IsOrderedRing.mkOfSubsemiring :
+    letI _ := PartialOrder.mkOfAddSubmonoid hS
+    IsOrderedRing R :=
+  letI _ := PartialOrder.mkOfAddSubmonoid hS
+  haveI := IsOrderedAddMonoid.mkOfAddSubmonoid hS
+  haveI : ZeroLEOneClass R := ⟨by simp⟩
+  .of_mul_nonneg fun x y xnn ynn ↦ show _ ∈ S by simpa using Subsemiring.mul_mem _ xnn ynn
+
 /-- `RingConeClass S R` says that `S` is a type of cones in `R`. -/
+@[deprecated "Unbundled to `AddSubmonoid.IsPointed`" (since := "2026-03-28")]
 class RingConeClass (S : Type*) (R : outParam Type*) [Ring R] [SetLike S R] : Prop
     extends AddGroupConeClass S R, SubsemiringClass S R
 
@@ -29,17 +45,21 @@ class RingConeClass (S : Type*) (R : outParam Type*) [Ring R] [SetLike S R] : Pr
 does not contain both `a` and `-a` for any nonzero `a`.
 This is equivalent to being the set of non-negative elements of
 some order making the ring into a partially ordered ring. -/
+@[deprecated "Unbundled to `AddSubmonoid.IsPointed`" (since := "2026-03-28")]
 structure RingCone (R : Type*) [Ring R] extends Subsemiring R, AddGroupCone R
 
 /-- Interpret a cone in a ring as a cone in the underlying additive group. -/
 add_decl_doc RingCone.toAddGroupCone
 
+@[deprecated (since := "2026-03-28")]
 instance RingCone.instSetLike (R : Type*) [Ring R] : SetLike (RingCone R) R where
   coe C := C.carrier
   coe_injective' p q h := by cases p; cases q; congr; exact SetLike.ext' h
 
+@[deprecated (since := "2026-03-28")]
 instance (R : Type*) [Ring R] : PartialOrder (RingCone R) := .ofSetLike (RingCone R) R
 
+@[deprecated (since := "2026-03-28")]
 instance RingCone.instRingConeClass (R : Type*) [Ring R] :
     RingConeClass (RingCone R) R where
   add_mem {C} := C.add_mem'
@@ -48,12 +68,12 @@ instance RingCone.instRingConeClass (R : Type*) [Ring R] :
   one_mem {C} := C.one_mem'
   eq_zero_of_mem_of_neg_mem {C} := C.eq_zero_of_mem_of_neg_mem'
 
-@[simp]
+@[simp, deprecated (since := "2026-03-28")]
 theorem RingCone.mem_mk {R : Type*} [Ring R] {toSubsemiring : Subsemiring R}
     (eq_zero_of_mem_of_neg_mem) {x : R} :
     x ∈ mk toSubsemiring eq_zero_of_mem_of_neg_mem ↔ x ∈ toSubsemiring := .rfl
 
-@[simp]
+@[simp, deprecated (since := "2026-03-28")]
 theorem RingCone.coe_set_mk {R : Type*} [Ring R] {toSubsemiring : Subsemiring R}
     (eq_zero_of_mem_of_neg_mem) :
     (mk toSubsemiring eq_zero_of_mem_of_neg_mem : Set R) = toSubsemiring := rfl
@@ -64,15 +84,24 @@ variable {T : Type*} [Ring T] [PartialOrder T] [IsOrderedRing T] {a : T}
 
 variable (T) in
 /-- Construct a cone from the set of non-negative elements of a partially ordered ring. -/
+@[deprecated Subsemiring.nonneg.isPointed (since := "2026-03-28")]
 def nonneg : RingCone T where
   __ := Subsemiring.nonneg T
   eq_zero_of_mem_of_neg_mem' {a} := by simpa using ge_antisymm
 
-@[simp] lemma nonneg_toSubsemiring : (nonneg T).toSubsemiring = .nonneg T := rfl
-@[simp] lemma nonneg_toAddGroupCone : (nonneg T).toAddGroupCone = .nonneg T := rfl
-@[simp] lemma mem_nonneg : a ∈ nonneg T ↔ 0 ≤ a := Iff.rfl
-@[simp, norm_cast] lemma coe_nonneg : nonneg T = {x : T | 0 ≤ x} := rfl
+@[simp, deprecated (since := "2026-03-28")]
+lemma nonneg_toSubsemiring : (nonneg T).toSubsemiring = .nonneg T := rfl
 
+@[simp, deprecated (since := "2026-03-28")]
+lemma nonneg_toAddGroupCone : (nonneg T).toAddGroupCone = .nonneg T := rfl
+
+@[simp, deprecated (since := "2026-03-28")]
+lemma mem_nonneg : a ∈ nonneg T ↔ 0 ≤ a := Iff.rfl
+
+@[simp, deprecated (since := "2026-03-28")]
+lemma coe_nonneg : nonneg T = {x : T | 0 ≤ x} := rfl
+
+@[deprecated Subsemiring.nonneg.isSpanning (since := "2026-03-28")]
 instance nonneg.hasMemOrNegMem {T : Type*} [Ring T] [LinearOrder T] [IsOrderedRing T] :
     HasMemOrNegMem (nonneg T) where
   mem_or_neg_mem := mem_or_neg_mem (AddGroupCone.nonneg T)
@@ -82,6 +111,7 @@ end RingCone
 variable {S R : Type*} [Ring R] [SetLike S R] (C : S)
 
 /-- Construct a partially ordered ring by designating a cone in a ring. -/
+@[deprecated IsOrderedRing.mkOfSubsemiring (since := "2026-03-28")]
 lemma IsOrderedRing.mkOfCone [RingConeClass S R] :
     letI _ : PartialOrder R := .mkOfAddGroupCone C
     IsOrderedRing R :=
