f2a2e15ba285bd221695fd8ba318f872a444e638
Ivo Malinowski <35851409+imalinowskip@users.noreply.github.com>
2026-03-28 20:56:19 +0100

    add module docstring and copyright header

--- Mathlib/Probability/CramerWold.lean
+++ Mathlib/Probability/CramerWold.lean
@@ -1,3 +1,10 @@
+/-
+Copyright (c) 2026 Ivo Malinowski. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Ivo Malinowski
+-/
+module
+
 import Mathlib.Probability.CentralLimitTheorem
 import Mathlib.MeasureTheory.Function.SpecialFunctions.Inner
 import Mathlib.Topology.MetricSpace.Pseudo.Defs
