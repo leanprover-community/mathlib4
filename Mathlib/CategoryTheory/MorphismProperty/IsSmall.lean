40ec110abb7b55eb404b38972d37ec9cebe278cc
Joël Riou <joel.riou@universite-paris-saclay.fr>
2026-03-29 12:25:04 +0200

    removed sorry

--- Mathlib/CategoryTheory/MorphismProperty/IsSmall.lean
+++ Mathlib/CategoryTheory/MorphismProperty/IsSmall.lean
@@ -63,8 +63,15 @@ lemma isSmall_iff_eq_ofHoms :
 
 instance isSmall_iSup {α : Type*} (W : α → MorphismProperty C)
     [Small.{w} α] [∀ a, IsSmall.{w} (W a)] :
-    IsSmall.{w} (iSup W) := by
-  sorry
+    IsSmall.{w} (iSup W) where
+  small_toSet := by
+    rw [toSet_iSup]
+    refine small_of_surjective (f := fun (⟨i, f⟩ : Σ i, (W i).toSet) ↦
+      ⟨f, by rw [Set.mem_iUnion]; exact ⟨i, f.prop⟩⟩) ?_
+    rintro ⟨f, hf⟩
+    simp only [Set.mem_iUnion] at hf
+    obtain ⟨i, hf⟩ := hf
+    exact ⟨⟨i, ⟨_, hf⟩⟩, rfl⟩
 
 end MorphismProperty
 
