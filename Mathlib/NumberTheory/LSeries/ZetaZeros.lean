64d4de9beec59b33f3f6c58302bea878dabe1b57
Yu-Misaka <109365723+Yu-Misaka@users.noreply.github.com>
2026-03-29 11:09:20 +0800

    naming fix

--- Mathlib/NumberTheory/LSeries/ZetaZeros.lean
+++ Mathlib/NumberTheory/LSeries/ZetaZeros.lean
@@ -33,7 +33,7 @@ lemma riemannZeta_zeroes_codiscreteWithin_compl_one :
     rw [Complex.rank_real_complex]
     exact Cardinal.one_lt_two
 
-private lemma riemannZeta_zeroes_on_Compact_finite' {S : Set ℂ} (hS1 : IsCompact S)
+private lemma riemannZeta_zeroes_on_compact_finite' {S : Set ℂ} (hS1 : IsCompact S)
     (hS2 : 1 ∉ S) : (S ∩ riemannZeta ⁻¹' {0}).Finite := by
   have sub := Set.subset_compl_singleton_iff.mpr hS2
   refine IsCompact.finite (hS1.of_isClosed_subset ?_ Set.inter_subset_left) ?_
@@ -45,17 +45,17 @@ private lemma riemannZeta_zeroes_on_Compact_finite' {S : Set ℂ} (hS1 : IsCompa
       <| Set.inter_subset_inter_right _ sub
 
 /-- Any compact subset of `ℂ` contains only finitely many zeros of the Riemann zeta function. -/
-lemma riemannZeta_zeroes_on_Compact_finite {S : Set ℂ} (hS : IsCompact S) :
+lemma riemannZeta_zeroes_on_compact_finite {S : Set ℂ} (hS : IsCompact S) :
     (S ∩ riemannZeta ⁻¹' {0}).Finite := by
   obtain ⟨ε, hε_pos, hball⟩ := Metric.eventually_nhds_iff.1 <|
       eventually_nhdsWithin_iff.1 riemannZeta_eventually_ne_zero
-  have hball : ∀ s ∈ Metric.ball 1 ε, riemannZeta s ≠ 0 := fun s hs hzero ↦ by
+  have hball' : ∀ s ∈ Metric.ball 1 ε, riemannZeta s ≠ 0 := fun s hs hzero ↦ by
     by_cases h : s = 1
     · exact riemannZeta_one_ne_zero (h ▸ hzero)
     · exact hball (Metric.mem_ball.1 hs) (Set.mem_compl_singleton_iff.2 h) hzero
   have : S ∩ riemannZeta ⁻¹' {0} ⊆ (S ∩ (Metric.ball 1 ε)ᶜ) ∩ riemannZeta ⁻¹' {0} :=
-    fun s ⟨hs, hz⟩ ↦ ⟨⟨hs, fun hmem ↦ hball s hmem hz⟩, hz⟩
-  refine Set.Finite.subset (riemannZeta_zeroes_on_Compact_finite' ?_ ?_) this
+    fun s ⟨hs, hz⟩ ↦ ⟨⟨hs, fun hmem ↦ hball' s hmem hz⟩, hz⟩
+  refine Set.Finite.subset (riemannZeta_zeroes_on_compact_finite' ?_ ?_) this
   · exact hS.inter_right Metric.isOpen_ball.isClosed_compl
   · exact fun ⟨_, h⟩ ↦ h (Metric.mem_ball_self hε_pos)
 
