6f28b44827a86bb54e1a6bfafddea80848d03683
adomasbaliuka <52975890+adomasbaliuka@users.noreply.github.com>
2026-03-29 14:27:40 +0200

    scripts/extract-unique-nonascii: print excluded

--- scripts/extract-unique-nonascii.lean
+++ scripts/extract-unique-nonascii.lean
@@ -15,6 +15,10 @@ https://github.com/leanprover/vscode-lean4/blob/master/lean4-unicode-input/src/a
 Note: We should disallow any characters which cause right-to-left alignment.
 For more information about Unicode's bidirectional classes, see
 https://www.unicode.org/reports/tr44/tr44-34.html#Bidi_Class_Values
+When using this script, **please manually check** any newly added characters conform to the guidelines,
+see `Mathlib.Linter.TextBased.UnicodeLinter.withVSCodeAbbrev` and
+`Mathlib.Linter.TextBased.UnicodeLinter.othersInMathlib`.
+
 
 Note: this script will be used very rarely (if at all).
 Therefore we probably shouldn't register it in `lakefile.lean` to avoid clutter.
@@ -28,13 +32,17 @@ curl $URL | lake env lean --run scripts/extract-unique-nonascii.lean
 
 def Char.isAscii (c : Char) : Bool := c.toNat < 128
 
+def Char.manuallyExcluded (c : Char) : Bool :=
+   c ∈ [
+    '\u2001', -- \quad (U+2001) (due to being non-standard whitespace)
+    '\uFDFC', -- RIAL sign (U+FDFC) (due to triggering right-aligned text).
+    '\u060B', -- AFGHANI sign (U+060B) (due to triggering right-aligned text)
+    '\u0332' -- COMBINING LOW LINE (U+0332) (due to modifying characters around it)
+   ]
+
 /-- We deliberately exclude some characters. -/
 def Char.allowedNonAscii (c : Char) : Bool :=
-    !c.isAscii
-    && c != '\u2001' -- \quad (U+2001) (due to being non-standard whitespace)
-    && c != '\uFDFC' -- RIAL sign (U+FDFC) (due to triggering right-aligned text).
-    && c != '\u060B' -- AFGHANI sign (U+060B) (due to triggering right-aligned text)
-    && c != '\u0332' -- COMBINING LOW LINE (U+0332) (due to modifying characters around it)
+    !c.isAscii && !c.manuallyExcluded
 
 def reprCharsChunked (xs : List Char) (chunkSize : Nat := 16) : String :=
   let elems := xs.map (fun c => "'" ++ String.singleton c ++ "'")
@@ -47,6 +55,9 @@ def reprCharsChunked (xs : List Char) (chunkSize : Nat := 16) : String :=
 def main : IO Unit := do
   let stdin ← IO.getStdin
   let allText ← stdin.readToEnd
+  let manuallyExcluded := (allText.toList.filter Char.manuallyExcluded).eraseDups
+  IO.println s!"Manually excluded these carachters:"
+  IO.println s!"{reprCharsChunked manuallyExcluded}"
   let outputList := (allText.toList.filter Char.allowedNonAscii).eraseDups
-  IO.println s!"Found {outputList.length} unique non-ascii characters (dropping U+2001 and U+FDFC):"
+  IO.println s!"Found {outputList.length} valid unique non-ascii characters:"
   IO.println s!"{reprCharsChunked outputList}"
