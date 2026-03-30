891ef3aee58e5b37799748f7a1be6d3602884df5
Weiyi Wang <wwylele@gmail.com>
2026-03-30 18:38:34 -0400

    revert changes in Hermitian.lean and move to the next PR

--- Mathlib/Analysis/Matrix/Hermitian.lean
+++ Mathlib/Analysis/Matrix/Hermitian.lean
@@ -7,7 +7,6 @@ module
 
 public import Mathlib.Analysis.InnerProductSpace.PiL2
 public import Mathlib.LinearAlgebra.Matrix.Hermitian
-import Mathlib.Analysis.InnerProductSpace.Adjoint
 
 /-!
 # Hermitian matrices over ℝ and ℂ
@@ -39,21 +38,18 @@ lemma IsHermitian.coe_re_apply_self (h : A.IsHermitian) (i : n) : (re (A i i) : 
 lemma IsHermitian.coe_re_diag (h : A.IsHermitian) : (fun i => (re (A.diag i) : 𝕜)) = A.diag :=
   funext h.coe_re_apply_self
 
-/-- A matrix is Hermitian iff the corresponding linear map with an orthonormal basis is self
-adjoint. -/
-lemma isHermitian_iff_isSymmetric_of_orthonormalBasis [Fintype n] [DecidableEq n] {E : Type*}
-    [NormedAddCommGroup E] [InnerProductSpace 𝕜 E] (b : OrthonormalBasis n 𝕜 E) :
-    IsHermitian A ↔ (A.toLin b.toBasis b.toBasis).IsSymmetric := by
-  have : FiniteDimensional 𝕜 E := b.toBasis.finiteDimensional_of_finite
-  simp_rw [LinearMap.IsSymmetric, ← LinearMap.adjoint_inner_left, ← toLin_conjTranspose]
-  refine ⟨fun h _ _ ↦ by rw [h.eq], fun h ↦ ?_⟩
-  simpa using (LinearMap.ext fun x ↦ ext_inner_right _ (h x)).symm
-
-/-- A matrix is Hermitian iff the corresponding linear map on the Euclidean space is self adjoint.
--/
+/-- A matrix is Hermitian iff the corresponding linear map is self adjoint. -/
 lemma isHermitian_iff_isSymmetric [Fintype n] [DecidableEq n] :
-    IsHermitian A ↔ A.toEuclideanLin.IsSymmetric :=
-  isHermitian_iff_isSymmetric_of_orthonormalBasis (EuclideanSpace.basisFun n 𝕜)
+    IsHermitian A ↔ A.toEuclideanLin.IsSymmetric := by
+  rw [LinearMap.IsSymmetric, (WithLp.toLp_surjective _).forall₂]
+  simp only [toLpLin_toLp, Matrix.toLin'_apply, EuclideanSpace.inner_eq_star_dotProduct,
+    star_mulVec]
+  constructor
+  · rintro (h : Aᴴ = A) x y
+    rw [dotProduct_comm, ← dotProduct_mulVec, h, dotProduct_comm]
+  · intro h
+    ext i j
+    simpa [(Pi.single_star i 1).symm] using h (Pi.single i 1) (Pi.single j 1)
 
 lemma IsHermitian.im_star_dotProduct_mulVec_self [Fintype n] (hA : A.IsHermitian) (x : n → 𝕜) :
      RCLike.im (star x ⬝ᵥ A *ᵥ x) = 0 := by
