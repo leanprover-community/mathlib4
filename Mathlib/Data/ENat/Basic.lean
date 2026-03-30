9c628dac9302fb6c1bd54bed9d765046fec8d26a
Kim Morrison <kim@tqft.net>
2026-03-30 05:37:22 +0000

    bad merge

--- Mathlib/Data/ENat/Basic.lean
+++ Mathlib/Data/ENat/Basic.lean
@@ -43,34 +43,12 @@ open Function
 assert_not_exists Field
 
 deriving instance Nontrivial,
-  LinearOrder, Bot, Sub,
-  IsOrderedRing, CanonicallyOrderedAdd,
-  OrderBot, OrderTop, OrderedSub,
-  WellFoundedLT,
-  CharZero,
-  NoZeroDivisors,
-  ZeroLEOneClass
+  Add, Sub, LE, LT, Bot,
+  Preorder, LinearOrder, OrderTop, OrderBot, WellFoundedLT, SuccOrder,
+  AddMonoidWithOne, CommSemiring, LinearOrderedAddCommMonoidWithTop,
+  ZeroLEOneClass, OrderedSub, CanonicallyOrderedAdd, IsOrderedRing,
+  CharZero, NoZeroDivisors
   for ENat
-
-set_option backward.inferInstanceAs.wrap false in
-deriving instance LinearOrderedAddCommMonoidWithTop for ENat
-
-set_option backward.inferInstanceAs.wrap.data false in
-deriving instance SuccOrder for ENat
-
-#adaptation_note /-- Upon bumping to v4.29.0-rc3, we write out the `CommSemiring` instance rather
-than using `deriving`, to ensure that the `NatCast` instance is definitionally equal to the one
-expected by `grind`. The `deriving` mechanism produces a `NatCast` instance
-(`ENat.instNatCast`) that is not reducibly defeq to `Lean.Grind.Semiring.natCast`.
-See https://leanprover.zulipchat.com/#narrow/channel/113488-general/topic/backward.2EisDefEq.2ErespectTransparency/near/576566138
--/
-instance : CommSemiring ENat := {
-  __ := (inferInstance : CommSemiring (WithTop ℕ))
-  toNatCast := inferInstance
-}
-
--- Moving this before `CommSemiring ENat` causes a failure later (where?).
-deriving instance CanonicallyOrderedAdd for ENat
 
 namespace ENat
 
@@ -85,9 +63,6 @@ theorem coe_inj {a b : ℕ} : (a : ℕ∞) = b ↔ a = b := WithTop.coe_inj
 @[simp] theorem succ_coe (n : ℕ) : SuccOrder.succ (n : ℕ∞) = (n + 1 : ℕ) := WithTop.succ_coe
 
 @[simp] theorem succ_top : SuccOrder.succ (⊤ : ℕ∞) = ⊤ := rfl
-
-@[simp] theorem top_add {x : ℕ∞} : ⊤ + x = ⊤ := WithTop.top_add x
-@[simp] theorem add_top {x : ℕ∞} : x + ⊤ = ⊤ := WithTop.add_top x
 
 instance : SuccAddOrder ℕ∞ where
   succ_eq_add_one x := by cases x <;> simp
