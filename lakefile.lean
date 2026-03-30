7e8f61d9fdd42e126ea1a711a54495c1ab3c67b0
Wojciech Nawrocki <wjn@lean-fro.org>
2026-03-30 16:57:46 -0400

    chore: remove remaining special treatment

--- lakefile.lean
+++ lakefile.lean
@@ -9,11 +9,10 @@ open Lake DSL
 require "leanprover-community" / "batteries" @ git "main"
 require "leanprover-community" / "Qq" @ git "master"
 require "leanprover-community" / "aesop" @ git "master"
-require "leanprover-community" / "proofwidgets" @ git "v0.0.95-pre1"
+require "leanprover-community" / "proofwidgets" @ git "main"
   with NameMap.empty.insert `errorOnBuild
     "ProofWidgets failed to reuse pre-built JS code. \
-    Please remove the `.lake/` directory and try again. \
-    If this does not work, report your issue on the Lean Zulip."
+    Please report this issue on the Lean Zulip."
 require "leanprover-community" / "importGraph" @ git "main"
 require "leanprover-community" / "LeanSearchClient" @ git "main"
 require "leanprover-community" / "plausible" @ git "main"
