a914d88a82b635af06f97432bfb6d675adcd9ac0
Bingyu Xia <71547343+BryceT233@users.noreply.github.com>
2026-03-30 11:17:27 +0800

    Update Noetherian.lean

--- Mathlib/RingTheory/AdicCompletion/Noetherian.lean
+++ Mathlib/RingTheory/AdicCompletion/Noetherian.lean
@@ -37,10 +37,10 @@ lemma IsHausdorff.of_isTorsionFree [IsDomain R] [IsTorsionFree R M] (h : I ≠ �
 theorem IsHausdorff.of_isDomain [IsDomain R] (h : I ≠ ⊤) : IsHausdorff I R :=
   .of_isTorsionFree I R h
 
-instance (priority := 100) [IsArtinianRing R] [IsLocalRing R] :
-    IsAdicComplete (IsLocalRing.maximalIdeal R) R where
+instance (priority := 100) {A : Type*} [CommRing A] [IsArtinianRing A] [IsLocalRing A] :
+    IsAdicComplete (IsLocalRing.maximalIdeal A) A where
   prec' f hf := by
-    obtain ⟨n, hn⟩ := (isArtinianRing_iff_isNilpotent_maximalIdeal R).mp ‹_›
+    obtain ⟨n, hn⟩ := (isArtinianRing_iff_isNilpotent_maximalIdeal A).mp ‹_›
     use f n; intro m
     by_cases h : m ≤ n
     · exact hf h
