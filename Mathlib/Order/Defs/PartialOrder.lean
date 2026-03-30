71f042a325cb606c6a0f4a6bef1910acb08b692b
Snir Broshi <26556598+SnirBroshi@users.noreply.github.com>
2026-03-30 05:33:14 +0300

    feat(Order/Defs/PartialOrder): `(a < b ↔ b < a) ↔ (a ≤ b ↔ b ≤ a)` in `Preorder`s

--- Mathlib/Order/Defs/PartialOrder.lean
+++ Mathlib/Order/Defs/PartialOrder.lean
@@ -111,6 +111,15 @@ lemma le_of_lt_or_eq (h : a < b ∨ a = b) : a ≤ b := h.elim le_of_lt le_of_eq
 @[to_dual le_of_eq_or_lt']
 lemma le_of_eq_or_lt (h : a = b ∨ a < b) : a ≤ b := h.elim le_of_eq le_of_lt
 
+lemma lt_iff_lt_iff : (a < b ↔ b < a) ↔ (a ≤ b ↔ b ≤ a) := by
+  grind [lt_iff_le_not_ge]
+
+lemma lt_iff_le_iff : (a < b ↔ a ≤ b) ↔ (b < a ↔ b ≤ a) := by
+  grind [lt_iff_le_not_ge]
+
+lemma lt_iff_le_iff' : (a < b ↔ b ≤ a) ↔ (b < a ↔ a ≤ b) := by
+  grind [lt_iff_le_not_ge]
+
 instance instTransLE : @Trans α α α LE.le LE.le LE.le := ⟨le_trans⟩
 instance instTransLT : @Trans α α α LT.lt LT.lt LT.lt := ⟨lt_trans⟩
 instance instTransLTLE : @Trans α α α LT.lt LE.le LT.lt := ⟨lt_of_lt_of_le⟩
