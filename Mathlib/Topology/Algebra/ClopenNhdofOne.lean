bf17cb995fc2d919f9c4923635593698e12494cd
vihdzp <vi.hdz.p@gmail.com>
2026-03-31 10:37:39 -0600

    fix

--- Mathlib/Topology/Algebra/ClopenNhdofOne.lean
+++ Mathlib/Topology/Algebra/ClopenNhdofOne.lean
@@ -46,9 +46,9 @@ variable {G : Type*} [Group G] [TopologicalSpace G]
 theorem exist_openNormalSubgroup_sub_open_nhds_of_one
     {U : Set G} (UOpen : IsOpen U) (einU : 1 ∈ U) :
     ∃ H : OpenNormalSubgroup G, (H : Set G) ⊆ U := by
-  rcases ((Filter.HasBasis.mem_iff' ((nhds_basis_clopen (1 : G))) U).mp <|
+  rcases ((Filter.HasBasis.mem_iff' ((hasBasis_nhds_isClopen (1 : G))) U).mp <|
     mem_nhds_iff.mpr (by use U)) with ⟨W, hW, h⟩
-  rcases IsTopologicalGroup.exist_openNormalSubgroup_sub_clopen_nhds_of_one hW.2 hW.1 with ⟨H, hH⟩
+  rcases IsTopologicalGroup.exist_openNormalSubgroup_sub_clopen_nhds_of_one hW.1 hW.2 with ⟨H, hH⟩
   exact ⟨H, fun _ a ↦ h (hH a)⟩
 
 open scoped Pointwise in
