66e90e51f6b36d3853b39c3f994dda9308aec396
Frédéric Dupuis <dupuisf@iro.umontreal.ca>
2026-03-30 20:55:21 -0400

    add isUnit to lemma name

--- Mathlib/Algebra/Algebra/StrictPositivity.lean
+++ Mathlib/Algebra/Algebra/StrictPositivity.lean
@@ -103,8 +103,9 @@ theorem _root_.IsUnit.isStrictlyPositive_iff_conjugate_of_isSelfAdjoint (a b : A
   grind [hb.isStrictlyPositive_star_left_conjugate_iff]
 
 @[aesop safe apply]
-theorem conjugate_of_isSelfAdjoint (a b : A) (hb : IsUnit b) (hb₂ : IsSelfAdjoint b := by cfc_tac)
-    (ha : IsStrictlyPositive a := by cfc_tac) : IsStrictlyPositive (b * a * b) :=
+theorem conjugate_of_isUnit_of_isSelfAdjoint (a b : A) (hb : IsUnit b)
+    (hb₂ : IsSelfAdjoint b := by cfc_tac) (ha : IsStrictlyPositive a := by cfc_tac) :
+    IsStrictlyPositive (b * a * b) :=
   (hb.isStrictlyPositive_iff_conjugate_of_isSelfAdjoint _ _ hb₂).mpr ha
 
 end StarOrderedRing
