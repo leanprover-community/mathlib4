50060b4108cea2b5d6fbdc061374fa42b14f8c6e
Eric Wieser <wieser.eric@gmail.com>
2026-03-30 19:32:34 -0700

    chore: lemmas about `Matrix.submatrix` and `Matrix.kroneckerMap`

--- Mathlib/LinearAlgebra/Matrix/Kronecker.lean
+++ Mathlib/LinearAlgebra/Matrix/Kronecker.lean
@@ -77,6 +77,23 @@ theorem kroneckerMap_map (f : α → β → γ) (g : γ → γ') (A : Matrix l m
     (kroneckerMap f A B).map g = kroneckerMap (fun a b => g (f a b)) A B :=
   ext fun _ _ => rfl
 
+theorem kroneckerMap_submatrix_left
+    (f : α → β → γ) (A : Matrix l m α) (B : Matrix n p β) (r : l' → l) (c : m' → m) :
+    kroneckerMap f (A.submatrix r c) B = (kroneckerMap f A B).submatrix (Prod.map r id) (Prod.map c id) :=
+  rfl
+
+theorem kroneckerMap_submatrix_right
+    (f : α → β → γ) (A : Matrix l m α) (B : Matrix n p β) (r : n' → n) (c : p' → p) :
+    kroneckerMap f A (B.submatrix r c) = (kroneckerMap f A B).submatrix (Prod.map id r) (Prod.map id c) :=
+  rfl
+
+theorem kroneckerMap_submatrix_submatrix
+    (f : α → β → γ) (A : Matrix l m α) (B : Matrix n p β)
+    (r : l' → l) (c : m' → m) (r' : n' → n) (c' : p' → p) :
+    kroneckerMap f (A.submatrix r c) (B.submatrix r' c') =
+      (kroneckerMap f A B).submatrix (Prod.map r r') (Prod.map c c') :=
+  rfl
+
 @[simp]
 theorem kroneckerMap_zero_left [Zero α] [Zero γ] (f : α → β → γ) (hf : ∀ b, f 0 b = 0)
     (B : Matrix n p β) : kroneckerMap f (0 : Matrix l m α) B = 0 :=
