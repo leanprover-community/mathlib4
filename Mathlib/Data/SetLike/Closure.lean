db01d442e1781a91a1ee6e63db674cbd6a49b87a
artie2000 <artem.khovanov@gmail.com>
2026-03-31 17:34:52 +0100

    typo

--- Mathlib/Data/SetLike/Closure.lean
+++ Mathlib/Data/SetLike/Closure.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Artie Khovanov
 -/
 module
-l
+
 public import Mathlib.Data.SetLike.Basic
 public import Mathlib.Data.Set.Lattice
 
