8a17349f6ba541ed1b29e71d79da19c9602ec484
NoneMore <hellgoingup@outlook.com>
2026-03-31 14:52:30 +0800

    golf

--- Mathlib/SetTheory/Ordinal/FixedPointApproximants.lean
+++ Mathlib/SetTheory/Ordinal/FixedPointApproximants.lean
@@ -101,7 +101,7 @@ theorem apply_lfpApprox_le_lfpApprox_of_lt {a b : Ordinal} (h : a < b) :
 
 theorem lfpApprox_add_one (h : x ≤ f x) (a : Ordinal) :
     lfpApprox f x (a + 1) = f (lfpApprox f x a) := by
-  apply le_antisymm
+  refine le_antisymm ?_ (apply_lfpApprox_le_lfpApprox_of_lt f x (lt_add_one a))
   · conv => left; rw [lfpApprox]
     apply sSup_le
     simp only [lt_add_one_iff, exists_prop, Set.union_singleton,
@@ -113,7 +113,6 @@ theorem lfpApprox_add_one (h : x ≤ f x) (a : Ordinal) :
       exact le_lfpApprox f x
     · intro a' h
       apply f.2; apply lfpApprox_monotone; exact h
-  · exact apply_lfpApprox_le_lfpApprox_of_lt f x (lt_add_one a)
 
 theorem lfpApprox_of_isSuccLimit {a : Ordinal} (ha : Order.IsSuccLimit a) :
     lfpApprox f x a = ⨆ b : Set.Iio a, lfpApprox f x b := by
@@ -125,8 +124,7 @@ theorem lfpApprox_of_isSuccLimit {a : Ordinal} (ha : Order.IsSuccLimit a) :
   · exact (lfpApprox_zero f x).symm.trans_le
       (le_iSup (fun b : Set.Iio a => lfpApprox f x b) ⟨0, ha.bot_lt⟩)
   · intro b hb
-    trans lfpApprox f x (b+1)
-    · exact apply_lfpApprox_le_lfpApprox_of_lt f x (lt_add_one b)
+    apply (apply_lfpApprox_le_lfpApprox_of_lt f x (lt_add_one b)).trans
     · exact (le_iSup (fun c : Set.Iio a => lfpApprox f x c) ⟨b + 1, ha.succ_lt hb⟩)
 
 theorem lfpApprox_mono_left : Monotone (lfpApprox : (α →o α) → _) := by
