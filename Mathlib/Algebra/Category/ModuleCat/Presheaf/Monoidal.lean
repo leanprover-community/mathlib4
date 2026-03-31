6f44b49ba107d1d8d0b3787459b24c6389aa0715
Joël Riou <joel.riou@universite-paris-saclay.fr>
2026-03-31 17:30:40 +0200

    added simp lemmas

--- Mathlib/Algebra/Category/ModuleCat/Presheaf/Monoidal.lean
+++ Mathlib/Algebra/Category/ModuleCat/Presheaf/Monoidal.lean
@@ -139,10 +139,12 @@ noncomputable instance monoidalCategory :
   pentagon _ _ _ _ := by ext1; apply pentagon
   triangle _ _ := by ext1; apply triangle
 
+open BraidedCategory
+
 instance symmetricCategory :
     SymmetricCategory (PresheafOfModules.{u} (R ⋙ forget₂ _ _)) where
   braiding M₁ M₂ :=
-    isoMk (fun X ↦ BraidedCategory.braiding (C := ModuleCat (R.obj X)) (M₁.obj X) (M₂.obj X))
+    isoMk (fun X ↦ braiding (C := ModuleCat (R.obj X)) (M₁.obj X) (M₂.obj X))
       (fun _ _ f ↦ ModuleCat.MonoidalCategory.tensor_ext (fun _ _ ↦ rfl))
   braiding_naturality_right _ _ _ _ := by
     ext : 1; exact ModuleCat.MonoidalCategory.tensor_ext (fun _ _ ↦ rfl)
@@ -157,6 +159,78 @@ instance symmetricCategory :
   symmetry _ _ := by
     ext : 1
     apply SymmetricCategory.symmetry (C := ModuleCat (R.obj _))
+
+section
+
+variable (M₁ M₂ M₃ M₄ : PresheafOfModules.{u} (R ⋙ forget₂ _ _))
+
+lemma tensorObj_obj (X : Cᵒᵖ) :
+    (M₁ ⊗ M₂).obj X =
+      MonoidalCategory.tensorObj (C := ModuleCat (R.obj X)) (M₁.obj X) (M₂.obj X) := rfl
+
+attribute [local simp] tensorObj_obj
+
+variable {M₂ M₃} in
+@[simp]
+lemma whiskerLeft_app (f : M₂ ⟶ M₃) (X : Cᵒᵖ) :
+    dsimp% (M₁ ◁ f).app X = whiskerLeft (C := ModuleCat (R.obj X)) (M₁.obj X) (f.app X) :=
+  rfl
+
+variable {M₁ M₂} in
+@[simp]
+lemma whiskerRight_app (f : M₁ ⟶ M₂) (M₃ : PresheafOfModules.{u} (R ⋙ forget₂ _ _)) (X : Cᵒᵖ) :
+    dsimp% (f ▷ M₃).app X = whiskerRight (C := ModuleCat (R.obj X)) (f.app X) (M₃.obj X) := rfl
+
+variable {M₁ M₂ M₃ M₄} in
+@[simp]
+lemma tensorHom_app (f : M₁ ⟶ M₂) (g : M₃ ⟶ M₄) (X : Cᵒᵖ) :
+    dsimp% (f ⊗ₘ g).app X =
+      MonoidalCategory.tensorHom (C := ModuleCat (R.obj X)) (f.app X) (g.app X) := rfl
+
+@[simp]
+lemma leftUnitor_hom_app (X : Cᵒᵖ) :
+    dsimp% (λ_ M₁).hom.app X = (leftUnitor (C := ModuleCat (R.obj X)) (M₁.obj X)).hom :=
+  rfl
+
+@[simp]
+lemma leftUnitor_inv_app (X : Cᵒᵖ) :
+    dsimp% (λ_ M₁).inv.app X = (leftUnitor (C := ModuleCat (R.obj X)) (M₁.obj X)).inv := by
+  rfl
+
+@[simp]
+lemma rightUnitor_hom_app (X : Cᵒᵖ) :
+    dsimp% (ρ_ M₁).hom.app X = (rightUnitor (C := ModuleCat (R.obj X)) (M₁.obj X)).hom :=
+  rfl
+
+@[simp]
+lemma rightUnitor_inv_app (X : Cᵒᵖ) :
+    dsimp% (ρ_ M₁).inv.app X = (rightUnitor (C := ModuleCat (R.obj X)) (M₁.obj X)).inv :=
+  rfl
+
+@[simp]
+lemma associator_hom_app (X : Cᵒᵖ) :
+    (α_ M₁ M₂ M₃).hom.app X =
+      (associator (C := ModuleCat (R.obj X)) (M₁.obj X) (M₂.obj X) (M₃.obj X)).hom :=
+  rfl
+
+@[simp]
+lemma associator_inv_app (X : Cᵒᵖ) :
+    (α_ M₁ M₂ M₃).inv.app X =
+      (associator (C := ModuleCat (R.obj X)) (M₁.obj X) (M₂.obj X) (M₃.obj X)).inv :=
+  rfl
+
+@[simp]
+lemma braiding_hom_app (X : Cᵒᵖ) :
+    dsimp% (braiding M₁ M₂).hom.app X =
+      (braiding (C := ModuleCat (R.obj X)) (M₁.obj X) (M₂.obj X)).hom := by
+  rfl
+
+@[simp]
+lemma braiding_inv_app (X : Cᵒᵖ) :
+    dsimp% (braiding M₁ M₂).inv.app X =
+      (braiding (C := ModuleCat (R.obj X)) (M₁.obj X) (M₂.obj X)).inv := rfl
+
+end
 
 instance (F : PresheafOfModules.{u} (R ⋙ forget₂ _ _)) :
     PreservesColimitsOfSize.{u, u} (tensorLeft F) where
