6b450dd3d6d8b1d2ebb7d80e54fee3326973573a
Tomas Skrivan <tomass@sidefx.com>
2026-03-31 16:36:23 +0200

    more tests

--- MathlibTest/fun_prop_dev.lean
+++ MathlibTest/fun_prop_dev.lean
@@ -704,16 +704,21 @@ instance : FunLike (FooHom α) α (α → α) where
   coe f := f.toFun
   coe_injective' f g h := by cases f; cases g; congr
 
-
 @[fun_prop]
-theorem con_foohom {f : FooHom α} {β : Type*} {g₁ : β → α} (hg₁ : Con g₁)
-    {g₂ : β → α} (hg₂ : Con g₂) : Con fun x ↦ f (g₁ x) (g₂ x) :=
-  Con_comp _ _ f.cont' (prod_mk_Con _ _ hg₁ hg₂)
+theorem con_foohom' {β : Type*} {f : β → FooHom α} (hf : Con f) {g₁ : β → α} (hg₁ : Con g₁)
+    {g₂ : β → α} (hg₂ : Con g₂) : Con fun x ↦ (f x) (g₁ x) (g₂ x) := silentSorry
 
 example {f : FooHom α} : Con fun x => f x x := by fun_prop
 example {f : FooHom α} (y : α) : Con fun x => f x y := by fun_prop
 example {f : FooHom α} : Con fun x => f (f x x) x := by fun_prop
 example {f : FooHom (Fin 2 → α)} : Con fun x i => f (f (fun _ => x 0) x) x i := by fun_prop
 example {f : FooHom (Fin 2 → α)} (i) : Con fun x => f (f (fun _ => x 0) x) x i := by fun_prop
+
+example {f : α → FooHom α} (hf : Con (fun x : α×α×α => f x.1 x.2.1 x.2.2)) :
+    Con fun x ↦ f x x x := by
+  fun_prop
+example {f : α → FooHom α} (hf : Con f) : Con fun x ↦ f x (f x x x) x := by
+  fun_prop
+
 
 end BundledMorphismWithFunctionValues
