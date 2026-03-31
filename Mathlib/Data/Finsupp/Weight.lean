590e15bdbea3522dc20ab47fd7ac0d9c82170608
Bingyu Xia <71547343+BryceT233@users.noreply.github.com>
2026-04-01 06:44:59 +0800

    apply suggestions

--- Mathlib/Data/Finsupp/Weight.lean
+++ Mathlib/Data/Finsupp/Weight.lean
@@ -255,10 +255,8 @@ theorem finite_of_degree_le [Finite σ] (n : ℕ) :
   intro _
   simp only [Function.const_apply, ne_eq, one_ne_zero, not_false_eq_true]
 
-lemma finite_of_degree_lt [Finite σ] (n : ℕ) : {f : σ →₀ ℕ | degree f < n}.Finite := by
-  by_cases h : n = 0
-  · simp [h]
-  simpa [Nat.lt_iff_le_pred (show 0 < n by lia)] using finite_of_degree_le ..
+lemma finite_of_degree_lt [Finite σ] (n : ℕ) : {f : σ →₀ ℕ | degree f < n}.Finite :=
+  Set.Finite.subset (finite_of_degree_le n) (by grind)
 
 lemma range_single_one :
     Set.range (fun a : σ ↦ Finsupp.single a 1) = { d | d.degree = 1 } := by
