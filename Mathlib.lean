5f502f1e508ca81efb09aa0af492866efee1a419
Dagur Asgeirsson <dagurtomas@gmail.com>
2026-03-31 11:57:37 -0600

    mk_all

--- Mathlib.lean
+++ Mathlib.lean
@@ -3606,6 +3606,7 @@ public import Mathlib.Condensed.Light.InternallyProjective
 public import Mathlib.Condensed.Light.Limits
 public import Mathlib.Condensed.Light.Module
 public import Mathlib.Condensed.Light.Monoidal
+public import Mathlib.Condensed.Light.Sequence
 public import Mathlib.Condensed.Light.Small
 public import Mathlib.Condensed.Light.TopCatAdjunction
 public import Mathlib.Condensed.Light.TopComparison
