25563cc805762006800eedfc28ae9b1e713af6b1
agjftucker <agjftucker@gmail.com>
2026-04-01 17:57:31 +0100

    revert filenames in Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -1708,8 +1708,8 @@ public import Mathlib.Analysis.Calculus.FDeriv.WithLp
 public import Mathlib.Analysis.Calculus.FormalMultilinearSeries
 public import Mathlib.Analysis.Calculus.Gradient.Basic
 public import Mathlib.Analysis.Calculus.Implicit
+public import Mathlib.Analysis.Calculus.ImplicitContDiff
 public import Mathlib.Analysis.Calculus.ImplicitFunction.Bivariate
-public import Mathlib.Analysis.Calculus.ImplicitFunction.ContDiff
 public import Mathlib.Analysis.Calculus.ImplicitFunction.ProdDomain
 public import Mathlib.Analysis.Calculus.InverseFunctionTheorem.Analytic
 public import Mathlib.Analysis.Calculus.InverseFunctionTheorem.ApproximatesLinearOn
