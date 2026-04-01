e349b18573a192dc229406d369faed2a60589649
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2026-04-01 08:32:48 -0300

    Don't use let

--- Mathlib/Combinatorics/SimpleGraph/Sum.lean
+++ Mathlib/Combinatorics/SimpleGraph/Sum.lean
@@ -75,23 +75,16 @@ lemma not_adj_sum_inl_inr (v w) : ¬(G ⊕g H).Adj (.inl v) (.inr w) := by simp
 
 lemma not_reachable_sum_inl_inr (v w) : ¬(G ⊕g H).Reachable (.inl v) (.inr w) := by
   rintro ⟨p⟩
-  let sV : Set (V ⊕ W) := Sum.inl '' Set.univ
-  let sW : Set (V ⊕ W) := Sum.inr '' Set.univ
-  have hs : ∀x, ¬x ∈ sV ↔ x ∈ sW := by simp [sV, sW]
-  have hv : (.inl v) ∈ sV := by simp [sV]
-  have hw : (.inr w) ∉ sV := by simp [sV]
-  obtain ⟨⟨d, hadj⟩, _, hd1, hd2⟩ := p.exists_boundary_dart sV hv hw
+  have hs : ∀x : V ⊕ W, ¬x ∈ (.inl '' ⊤) ↔ x ∈ (.inr '' ⊤) := by simp
+  obtain ⟨⟨d, hadj⟩, _, hd1, hd2⟩ := p.exists_boundary_dart (.inl '' ⊤) (by simp) (by simp)
   simp only [hs] at hadj hd1 hd2
   obtain ⟨v', _, hv'⟩ := hd1
   obtain ⟨w', _, hw'⟩ := hd2
   rw [← hv', ← hw'] at hadj
   exact not_adj_sum_inl_inr _ _ hadj
 
-lemma not_preconnected_sum [Nonempty V] [Nonempty W] : ¬(G ⊕g H).Preconnected := by
-  let v : V := Classical.arbitrary _
-  let w : W := Classical.arbitrary _
-  intro h
-  exact not_reachable_sum_inl_inr v w (h _ _)
+lemma not_preconnected_sum [Nonempty V] [Nonempty W] : ¬(G ⊕g H).Preconnected :=
+  fun h ↦ not_reachable_sum_inl_inr (Classical.arbitrary _) (Classical.arbitrary _) (h ..)
 
 lemma not_connected_sum [Nonempty V] [Nonempty W] : ¬(G ⊕g H).Connected := by
   simp [connected_iff, not_preconnected_sum]
