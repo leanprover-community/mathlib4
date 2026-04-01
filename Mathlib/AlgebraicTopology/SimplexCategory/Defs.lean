49f704b715c2f203ec1a8d2617ea7ea79be0020e
Andrew Yang <36414270+erdOne@users.noreply.github.com>
2026-04-01 11:20:51 +0200

    Update Defs.lean

--- Mathlib/AlgebraicTopology/SimplexCategory/Defs.lean
+++ Mathlib/AlgebraicTopology/SimplexCategory/Defs.lean
@@ -75,7 +75,7 @@ attribute [irreducible] SimplexCategory
 
 -- TODO: Consider turning `SimplexCategory` a one field structure to make this reducibly equal.
 /-- The equality `len_mk` should be reducibly equal because they often appear in types. -/
-unif_hint len_mk_eq_self (n : ℕ) where ⊢ ⦋n⦌.len ≟ n
+unif_hint len_mk_eq_self (n : ℕ) where ⊢ len (.mk n) ≟ n
 
 open Simplicial
 
