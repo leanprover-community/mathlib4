6dd8d3fedb07450ce6fc350b651796ffa437cd68
Christian Merten <christian@merten.dev>
2026-04-01 15:20:47 +0200

    remove more deprecations

--- Mathlib/Topology/Connected/Clopen.lean
+++ Mathlib/Topology/Connected/Clopen.lean
@@ -484,19 +484,11 @@ theorem Topology.IsCoinducing.preimage_connectedComponent (hf : IsCoinducing f)
       _).subset_connectedComponent mem_connectedComponent).antisymm
     (hf.continuous.mapsTo_connectedComponent a)
 
-@[deprecated (since := "2026-03-21")]
-alias Topology.IsQuotientMap.preimage_connectedComponent :=
-  Topology.IsCoinducing.preimage_connectedComponent
-
 lemma Topology.IsCoinducing.image_connectedComponent {f : α → β} (hf : IsCoinducing f)
     (h_fibers : ∀ y : β, IsConnected (f ⁻¹' {y})) (a : α) :
     f '' connectedComponent a = connectedComponent (f a) := by
   rw [← hf.preimage_connectedComponent h_fibers,
     image_preimage_eq _ fun y ↦ (h_fibers y).nonempty]
-
-@[deprecated (since := "2026-03-21")]
-alias Topology.IsQuotientMap.image_connectedComponent :=
-  Topology.IsCoinducing.image_connectedComponent
 
 end Preconnected
 
