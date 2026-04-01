6018376c7f7ff4f0e9f856689dbb81fdb240066d
Christian Merten <christian@merten.dev>
2026-04-01 23:16:57 +0200

    fix

--- Mathlib/AlgebraicGeometry/Geometrically/Connected.lean
+++ Mathlib/AlgebraicGeometry/Geometrically/Connected.lean
@@ -23,11 +23,6 @@ of a geometrically connected scheme over a field.
   for all `Spec K ⟶ Y` with `K` a field, `X ×[Y] Spec K` is connected.
 - `GeometricallyConnected.iff_geometricallyConnected_fiber`: A scheme is geometrically connected
   over `S` iff the fibers of all `s : S` are geometrically connected.
-- `AlgebraicGeometry.GeometricallyConnected.connectedSpace`:
-  If `X` is geometrically connected and universally open (e.g. when flat + finite presentation),
-  over a connected scheme, then `X` is also connected.
-  In particular, the base change of a geometrically connected and universally open scheme to a
-  connected scheme is connected (by infer_instance).
 
 -/
 
@@ -35,59 +30,7 @@ of a geometrically connected scheme over a field.
 
 open CategoryTheory MorphismProperty Limits
 
-section
-
-@[simp]
-lemma Continuous.connectedComponentsMap_mk {X Y : Type*} [TopologicalSpace X] [TopologicalSpace Y]
-    {f : X → Y} (hf : Continuous f) (x : X) :
-    hf.connectedComponentsMap (.mk x) = .mk (f x) :=
-  rfl
-
-@[simp]
-lemma Continuous.connectedComponentsMap_surjective {X Y : Type*} [TopologicalSpace X]
-    [TopologicalSpace Y] {f : X → Y} (hf : Continuous f) (h : Function.Surjective f) :
-    Function.Surjective hf.connectedComponentsMap :=
-  Quotient.lift_surjective _ _ <| ConnectedComponents.surjective_coe.comp h
-
-lemma Topology.IsCoinducing.connectedComponentsMap_bijective {X Y : Type*} [TopologicalSpace X]
-    [TopologicalSpace Y] (f : X → Y) (hf : ∀ y, IsConnected (f ⁻¹' {y}))
-    (hf' : Topology.IsCoinducing f) :
-    Function.Bijective hf'.continuous.connectedComponentsMap := by
-  refine ⟨?_, Continuous.connectedComponentsMap_surjective _ (fun y ↦ (hf y).nonempty)⟩
-  intro x y hxy
-  obtain ⟨x, rfl⟩ := ConnectedComponents.surjective_coe x
-  obtain ⟨y, rfl⟩ := ConnectedComponents.surjective_coe y
-  simp at hxy
-  simp [hxy, ← hf'.preimage_connectedComponent hf]
-
-noncomputable
-def Topology.IsCoinducing.connectedComponentsEquiv {X Y : Type*} [TopologicalSpace X]
-    [TopologicalSpace Y] {f : X → Y} (hf : ∀ y, IsConnected (f ⁻¹' {y}))
-    (hf' : Topology.IsCoinducing f) :
-    ConnectedComponents X ≃ ConnectedComponents Y :=
-  .ofBijective _ (hf'.connectedComponentsMap_bijective _ hf)
-
-@[simp]
-lemma Topology.IsCoinducing.connectedComponentsEquiv_mk {X Y : Type*} [TopologicalSpace X]
-    [TopologicalSpace Y] {f : X → Y} (hf : ∀ y, IsConnected (f ⁻¹' {y}))
-    (hf' : Topology.IsCoinducing f) (x : X) :
-    hf'.connectedComponentsEquiv hf (.mk x) = .mk (f x) :=
-  rfl
-
-@[simp]
-lemma Topology.IsCoinducing.connectedComponentsEquiv_symm_mk_apply {X Y : Type*}
-    [TopologicalSpace X] [TopologicalSpace Y] {f : X → Y} (hf : ∀ y, IsConnected (f ⁻¹' {y}))
-    (hf' : Topology.IsCoinducing f) (x : X) :
-    (hf'.connectedComponentsEquiv hf).symm (.mk (f x)) = .mk x :=
-  (hf'.connectedComponentsEquiv hf).injective (by simp)
-
-end
-
 namespace AlgebraicGeometry
-
--- remove me
-instance : ObjectProperty.IsClosedUnderIsomorphisms (C := Scheme) (ConnectedSpace ·) :=
-  ⟨fun e ↦ e.hom.homeomorph.connectedSpace_iff.mp⟩
 
 variable {X Y Z S : Scheme} (f : X ⟶ S) (g : Y ⟶ S)
 
@@ -123,33 +66,32 @@ instance (s : S) [GeometricallyConnected f] : ConnectedSpace (f.fiber s) :=
 instance (priority := low) [GeometricallyConnected f] : Surjective f :=
   ⟨fun x ↦ ⟨_, (f.range_fiberι x).le ⟨Nonempty.some inferInstance, rfl⟩⟩⟩
 
-lemma Scheme.Hom.isConnected_preimage
-    [GeometricallyConnected f] (hf : IsOpenMap f)
-    {s : Set S} (hs : _root_.IsConnected s) : _root_.IsConnected (f ⁻¹' s) := by
-  wlog H : ∃ x, s = {x} generalizing s
-  · --refine hs.preimage_of_isConnected_fiber _ hf
-    --  (fun x ↦ (this isConnected_singleton ⟨_, rfl⟩).isPreirreducible) ?_
-    --rw [Set.range_eq_univ.mpr f.surjective, Set.inter_univ]
-    --exact hs.nonempty
-    sorry
-  obtain ⟨s, rfl⟩ := H
+lemma Scheme.Hom.isConnected_preimage_singleton [GeometricallyConnected f] (x : S) :
+    _root_.IsConnected (f ⁻¹' {x}) := by
   rw [← f.range_fiberι, ← Set.image_univ]
   exact isConnected_univ.image _ (f.fiberι _).continuous.continuousOn
 
-/-- If `f : X ⟶ S` is geometrically irreducible and open,
-then `f` induces an equivalence between the irreducible components of `X` and `S`. -/
+lemma Scheme.Hom.isConnected_preimage [GeometricallyConnected f] (hf : IsOpenMap f)
+    {s : Set S} (hs : _root_.IsConnected s) (hs' : IsClosed s) : _root_.IsConnected (f ⁻¹' s) := by
+  refine Topology.IsCoinducing.isConnected_preimage_of_isClosed f.isConnected_preimage_singleton
+    ?_ hs' hs
+  exact (hf.isQuotientMap f.continuous f.surjective).isCoinducing
+
+/-- If `f : X ⟶ S` is geometrically connected and open,
+then `f` induces an equivalence between the connected components of `X` and `S`. -/
 @[simps! apply]
 noncomputable
 def Scheme.Hom.connectedComponentsEquiv [GeometricallyConnected f] (hf : IsOpenMap f) :
     ConnectedComponents X ≃ ConnectedComponents S :=
   (hf.isQuotientMap f.continuous f.surjective).isCoinducing.connectedComponentsEquiv
-    fun _ ↦ f.isConnected_preimage hf isConnected_singleton
+    f.isConnected_preimage_singleton
 
-lemma GeometricallyConnected.connectedSpace
-    [GeometricallyConnected f] [ConnectedSpace S] (hf : IsOpenMap f) : ConnectedSpace X := by
+lemma GeometricallyConnected.connectedSpace [GeometricallyConnected f] [ConnectedSpace S]
+    (hf : IsOpenMap f) :
+    ConnectedSpace X := by
   simpa [connectedSpace_iff_univ] using f.isConnected_preimage hf isConnected_univ
 
-/-- If `X` is geometrically irreducible over a point, then it is irreducible. -/
+/-- If `X` is geometrically connected over a point, then it is connected. -/
 lemma GeometricallyConnected.connectedSpace_of_subsingleton
     [GeometricallyConnected f] [Subsingleton S] [Nonempty S] : ConnectedSpace X :=
   have : ConnectedSpace S := ⟨‹_›⟩
