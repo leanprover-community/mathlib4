7152469793e959b501849a852f05619ebb90a7d8
Yu-Misaka <109365723+Yu-Misaka@users.noreply.github.com>
2026-04-01 14:34:46 +0800

    zeroes -> zeros

--- Mathlib/NumberTheory/LSeries/ZetaZeros.lean
+++ Mathlib/NumberTheory/LSeries/ZetaZeros.lean
@@ -15,27 +15,27 @@ so that in particular any compact subset of `ℂ` contains only finitely many ze
 
 ## Main results
 
-* `isClosed_riemannZeta_zeroes`: `riemannZeta ⁻¹' {0}` is closed.
+* `isClosed_riemannZeta_zeros`: `riemannZeta ⁻¹' {0}` is closed.
 
-* `isDiscrete_riemannZeta_zeroes`: `riemannZeta ⁻¹' {0}` is discrete.
+* `isDiscrete_riemannZeta_zeros`: `riemannZeta ⁻¹' {0}` is discrete.
 
-* `riemannZeta_zeroes_on_Compact_finite`: for any compact set `S : Set ℂ`, the intersection
+* `riemannZeta_zeros_on_Compact_finite`: for any compact set `S : Set ℂ`, the intersection
   `S ∩ riemannZeta ⁻¹' {0}` is finite.
 -/
 
 public section
 
 /-- The complement of the zero set of `riemannZeta` is codiscrete within `{1}ᶜ`. -/
-private lemma riemannZeta_zeroes_codiscreteWithin_compl_one :
+private lemma riemannZeta_zeros_codiscreteWithin_compl_one :
     (riemannZeta ⁻¹' {0})ᶜ ∈ Filter.codiscreteWithin {1}ᶜ := by
   refine analyticOn_riemannZeta.preimage_zero_mem_codiscreteWithin (x := 2) ?_ (by grind) ?_
   · exact riemannZeta_ne_zero_of_one_le_re (by aesop)
   · exact isConnected_compl_singleton_of_one_lt_rank (by aesop) 1
 
 /-- The complement of the zero set of `riemannZeta` is codiscrete. -/
-private lemma compl_riemannZeta_zeroes_mem_codiscrete :
+private lemma compl_riemannZeta_zeros_mem_codiscrete :
     (riemannZeta ⁻¹' {0})ᶜ ∈ Filter.codiscrete ℂ := by
-  have := riemannZeta_zeroes_codiscreteWithin_compl_one
+  have := riemannZeta_zeros_codiscreteWithin_compl_one
   simp only [mem_codiscreteWithin, Set.mem_compl_iff, Set.mem_singleton_iff, sdiff_compl,
     Set.inf_eq_inter, Filter.disjoint_principal_right, mem_codiscrete, compl_compl] at this ⊢
   intro x
@@ -43,16 +43,16 @@ private lemma compl_riemannZeta_zeroes_mem_codiscrete :
   · exact riemannZeta_eventually_ne_zero_nhds_one.filter_mono nhdsWithin_le_nhds
   · exact Filter.mem_of_superset (this x hx) (by grind [riemannZeta_one_ne_zero])
 
-lemma isClosed_riemannZeta_zeroes : IsClosed (riemannZeta ⁻¹' {0}) :=
-  by simpa using (mem_codiscrete'.mp compl_riemannZeta_zeroes_mem_codiscrete).1
+lemma isClosed_riemannZeta_zeros : IsClosed (riemannZeta ⁻¹' {0}) :=
+  by simpa using (mem_codiscrete'.mp compl_riemannZeta_zeros_mem_codiscrete).1
 
-lemma isDiscrete_riemannZeta_zeroes : IsDiscrete (riemannZeta ⁻¹' {0}) :=
-  by simpa using (mem_codiscrete'.mp compl_riemannZeta_zeroes_mem_codiscrete).2
+lemma isDiscrete_riemannZeta_zeros : IsDiscrete (riemannZeta ⁻¹' {0}) :=
+  by simpa using (mem_codiscrete'.mp compl_riemannZeta_zeros_mem_codiscrete).2
 
 /-- Any compact subset of `ℂ` contains only finitely many zeros of the Riemann zeta function. -/
-lemma riemannZeta_zeroes_on_compact_finite {S : Set ℂ} (hS : IsCompact S) :
+lemma riemannZeta_zeros_on_compact_finite {S : Set ℂ} (hS : IsCompact S) :
     (S ∩ riemannZeta ⁻¹' {0}).Finite := by
-  apply (hS.inter_right isClosed_riemannZeta_zeroes).finite
-  exact isDiscrete_riemannZeta_zeroes.mono (by grind)
+  apply (hS.inter_right isClosed_riemannZeta_zeros).finite
+  exact isDiscrete_riemannZeta_zeros.mono (by grind)
 
 end
