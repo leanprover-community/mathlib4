d0e4bb47470b074931e9b88f3057651f33a2d310
Raph-DG <raphaeldouglasgiles@gmail.com>
2026-04-01 18:13:22 +0200

    Clean up

--- Mathlib/Topology/LocallyFinsupp/Pushforward.lean
+++ Mathlib/Topology/LocallyFinsupp/Pushforward.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2025 Raphael Douglas Giles. All rights reserved.
+Copyright (c) 2026 Raphael Douglas Giles. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Raphael Douglas Giles
 -/
@@ -13,15 +13,13 @@ public import Mathlib.Algebra.DirectSum.Decomposition
 public import Mathlib.Topology.Spectral.ConstructibleTopology
 
 /-!
-# Algebraic Cycles
+# Pushforward of functions with locally finite support
 
-In this file we define algebraic cycles on a scheme `X` with coefficients in a type `R` and provide
-some basic API for working with them. We define an algebraic cycle on a scheme `X` with
-coefficients in a type `R` to be functions `c : X → R` whose support is locally finite.
-
-Here we're making use of the equivalence between irreducible closed subsets of a scheme and their
-generic points in order to reuse the API in Function.locallyFinsupp, hence the slightly
-nonstandard definition.
+In this file we define the notion of the pushforward of a function with locally finite support
+between prespectral spaces. This is a nonstandard notion that arises because of our choice in
+mathlib to model algebraic cycles as functions with locally finite support.
+This makes it so that standard notions in the theory of cycles can be defined in more generality
+than usual.
 -/
 
 @[expose] public section
@@ -38,7 +36,8 @@ namespace locallyFinsupp
 section Zero
 variable [Zero R]
 /--
-The cycle containing a single point with a chosen coefficient
+The function taking `x` to some value `coeff`, and every other value to `0` as a
+function with locally finite support.
 -/
 noncomputable
 def single (x : X) (coeff : R) : Function.locallyFinsupp X R where
@@ -59,8 +58,7 @@ end Zero
 
 section map
 
-variable [Semiring R] {W : TopologicalSpace.Opens Y} (hW : IsCompact W.1)
-  (c : Function.locallyFinsupp X R)
+variable [Semiring R] {W : TopologicalSpace.Opens Y} (c : Function.locallyFinsupp X R)
 
 lemma preimageSupport_inter_subset : W.carrier ∩ {z | (preimageSupport f c z).Nonempty} ⊆
     f '' (f ⁻¹' ((W.carrier ∩ {z | (preimageSupport f c z).Nonempty})) ∩ c.support) := by
@@ -81,28 +79,23 @@ lemma preimageSupport_preimage_inter_subset : f ⁻¹' W.carrier ∩
     mem_singleton_iff, exists_and_right, exists_eq', true_and] at hp ⊢
   exact ⟨hp.1.1, hp.2⟩
 
-lemma preimage_inter_support_finite_of_isAffineOpen (hf : IsSpectralMap f) (hW : IsCompact W.1) :
-    (f ⁻¹' W.carrier ∩ c.support).Finite :=
-  LocallyFiniteSupport.finite_inter_support_of_isCompact c.locallyFiniteSupport <|
-  hf.2 W.is_open' hW
-
-lemma iUnion_preimage_inter_support_finite_of_isAffineOpen (hf : IsSpectralMap f)
-    (hW : IsCompact W.1) :
-    (⋃ _ : Y, f ⁻¹' W.carrier ∩ c.support).Finite := by
+lemma iUnion_preimage_inter_support_finite_of_isCompact (hf : IsSpectralMap f)
+    (hW : IsCompact W.1) : (⋃ _ : Y, f ⁻¹' W.carrier ∩ c.support).Finite := by
   suffices (f ⁻¹' W.carrier ∩ c.support).Finite by
     apply Finite.subset this
     intro a ha
     simp only [Opens.carrier_eq_coe, mem_iUnion, mem_inter_iff, mem_preimage,
       SetLike.mem_coe, Function.mem_support, ne_eq, exists_and_left, exists_const_iff] at ha ⊢
     exact ⟨ha.1, ha.2.2⟩
-  exact preimage_inter_support_finite_of_isAffineOpen c hf hW
+  exact LocallyFiniteSupport.finite_inter_support_of_isCompact c.locallyFiniteSupport <|
+    hf.2 W.is_open' hW
 
-lemma inter_nonempty_finite (hf : IsSpectralMap f) (hW : IsCompact W.1)
-   : (W.carrier ∩ {z : Y | (preimageSupport f c z).Nonempty}).Finite := by
+lemma inter_nonempty_finite (hf : IsSpectralMap f) (hW : IsCompact W.1) :
+    (W.carrier ∩ {z : Y | (preimageSupport f c z).Nonempty}).Finite := by
   refine Finite.subset (Finite.image _ ?_) (preimageSupport_inter_subset c)
-  rw[preimage_inter]
+  rw [preimage_inter]
   apply Finite.subset _ (preimageSupport_preimage_inter_subset c)
-  rw[inter_iUnion]
+  rw [inter_iUnion]
   suffices (⋃ i : Y, f ⁻¹' W.carrier ∩ c.support).Finite by
     apply Finite.subset this
     simp only [Opens.carrier_eq_coe, iUnion_subset_iff]
@@ -110,7 +103,7 @@ lemma inter_nonempty_finite (hf : IsSpectralMap f) (hW : IsCompact W.1)
     simp only [mem_inter_iff, mem_preimage, SetLike.mem_coe,
       Function.mem_support, ne_eq, mem_iUnion, exists_and_left, exists_const_iff] at hx ⊢
     exact ⟨hx.1, ⟨Nonempty.intro y, hx.2.2⟩⟩
-  exact iUnion_preimage_inter_support_finite_of_isAffineOpen c hf hW
+  exact iUnion_preimage_inter_support_finite_of_isCompact c hf hW
 
 variable {N : Type*} [PrespectralSpace Y]
 
@@ -143,14 +136,11 @@ lemma map_locally_finite (hf : IsSpectralMap f)
 
 variable (f) in
 /--
-The pushforward by a spectral map with quasicompact fibers with respect to a weight function `w`.
-This is mainly used when interpretting locally fin supp functions as algebraic cycles (in this case
-the weight function corresponds to a dimension or codimension function).
-
-
-
-NOTE: It is probably more sensible to define this for functions with compact fibers, rather than
-simply functions whose fibers have nonempty intersection with c.support. Maybe this way is fine too
+The pushforward of a function `c` of locally finite support
+by a spectral map whose fibers intersect `c` in finitely many places
+with respect to a weight function `w`. This is mainly used when interpretting locally fin supp
+functions as algebraic cycles (in this case the weight function corresponds to a dimension or
+codimension function).
 -/
 noncomputable
 def map (hf : IsSpectralMap f) (h : ∀ z, (preimageSupport f c z).Finite) :
@@ -176,16 +166,19 @@ lemma map_homogeneneous (s : Set X) (t : Set Y) (hc : c.support ⊆ s)
   specialize h x (hc hx.1.2)
   grind
 
+lemma preimageSupport_id (z : X) : (preimageSupport id c z).Finite := by
+  simp [preimageSupport, toFinite ({z} ∩ locallyFinsuppWithin.support c)]
+
 /--
 The pushforward of `c` along the identity morphism is `c`.
 -/
 @[simp]
-lemma map_id [PrespectralSpace X] (h : ∀ z, (preimageSupport id c z).Finite)
+lemma map_id [PrespectralSpace X]
     (hw : ∀ z : X, w z = 1) :
-    map id w c isSpectralMap_id h = c := by
+    map id w c isSpectralMap_id (preimageSupport_id c) = c := by
    ext z
-   have : (c z ≠ 0 ∧ (h z).toFinset = {z}) ∨
-          (c z = 0 ∧ (h z).toFinset = ∅) := by
+   have : (c z ≠ 0 ∧ (preimageSupport_id c z).toFinset = {z}) ∨
+          (c z = 0 ∧ (preimageSupport_id c z).toFinset = ∅) := by
     simp only [ne_eq, Finite.toFinset, preimageSupport, preimage_id_eq, id_eq, toFinset_eq_empty,
       singleton_inter_eq_empty,
       Function.mem_support, not_not, and_self]
@@ -194,7 +187,7 @@ lemma map_id [PrespectralSpace X] (h : ∀ z, (preimageSupport id c z).Finite)
       Finset.mem_singleton, and_iff_left_iff_imp]
     rintro rfl
     assumption
-   suffices (map id w c isSpectralMap_id h).toFun z = c.toFun z from this
+   suffices (map id w c isSpectralMap_id (preimageSupport_id c)).toFun z = c.toFun z from this
    obtain h | h := this
    all_goals simp only [map]
              rw[h.2]
