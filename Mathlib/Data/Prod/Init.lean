b9d9eae9331cfcf94955d4d4951837563d26b62a
Wrenna Robson <wren.robson@gmail.com>
2026-04-03 18:12:08 +0100

    Fix lint

--- Mathlib/Data/Prod/Init.lean
+++ Mathlib/Data/Prod/Init.lean
@@ -1,9 +1,11 @@
 /-
 Copyright (c) 2026 Wrenna Robson. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Wrena Robson
+Authors: Wrenna Robson
 -/
 module
+
+public import Mathlib.Init
 
 /-!
 
