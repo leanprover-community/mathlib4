6bedec6d467d5cb9e0850db88ec109f40469baf9
Sahan Wijetunga <99368343+sahanwijetunga@users.noreply.github.com>
2026-04-03 14:32:02 -0700

    Update Mathlib/LinearAlgebra/BilinearForm/IsometryEquiv.lean
    Co-authored-by: Robin Carlier <57142648+robin-carlier@users.noreply.github.com>

--- Mathlib/LinearAlgebra/BilinearForm/IsometryEquiv.lean
+++ Mathlib/LinearAlgebra/BilinearForm/IsometryEquiv.lean
@@ -129,5 +129,4 @@ def isometryEquivOfCompLinearEquiv (B : LinearMap.BilinForm R M) (f : M₁ ≃�
       simp only [comp_apply, LinearEquiv.coe_coe, LinearEquiv.toFun_eq_coe,
         f.apply_symm_apply] }
 
-end BilinForm
-end LinearMap
+end LinearMap.BilinForm
