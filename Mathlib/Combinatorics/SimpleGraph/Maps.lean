fbf616c5872495b0a263d76aa8d5718eefc553b3
Snir Broshi <26556598+SnirBroshi@users.noreply.github.com>
2026-04-04 21:47:31 +0300

    add `neighborSet_mono`

--- Mathlib/Combinatorics/SimpleGraph/Maps.lean
+++ Mathlib/Combinatorics/SimpleGraph/Maps.lean
@@ -344,6 +344,11 @@ theorem mapEdgeSet.injective (hinj : Function.Injective f) : Function.Injective 
   repeat rw [Subtype.mk_eq_mk]
   apply Sym2.map.injective hinj
 
+@[gcongr]
+theorem _root_.SimpleGraph.neighborSet_mono (hle : G₁ ≤ G₂) (v : V) :
+    G₁.neighborSet v ⊆ G₂.neighborSet v :=
+  neighborSet_subset_preimage v <| .ofLE hle
+
 /-- Every graph homomorphism from a complete graph is injective. -/
 theorem injective_of_top_hom (f : (⊤ : SimpleGraph V) →g G') : Function.Injective f := by
   intro v w h
