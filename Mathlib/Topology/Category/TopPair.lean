72126df2d0d28c76d5f1a552af76a1c74d098da0
Jacob Scharmberg <hi@jacobscharmberg.net>
2026-04-04 15:56:35 +0200

    change definition of TopPair to use MorphismProperty.Arrow

--- Mathlib/Topology/Category/TopPair.lean
+++ Mathlib/Topology/Category/TopPair.lean
@@ -12,7 +12,8 @@ public import Mathlib.Topology.Homotopy.Basic
 /-!
 # Topological Pairs
 
-In this file we introduce `TopPair`, the category of topological pairs.
+In this file we introduce `TopPair`, the category of topological pairs. It is defined as the
+category of arrows in `TopCat` which are topologically inducing.
 
 We provide the inclusion and diagonal functors `TopCat` ⥤ `TopPair` and show that they are left and
 right adjoint to the first projection functor, respectively.
@@ -27,154 +28,117 @@ open TopologicalSpace CategoryTheory unitInterval
 
 universe u
 
-/-- A pair of topological spaces consists of two topological spaces X and A and a map f : A → X
+/-- A pair of topological spaces consists of a morphism f : A ⟶ X in `TopCat`
 such that the topology of A is induced by f. -/
-structure TopPair where
-  /-- The first space of the pair -/
-  fst : TopCat
-  /-- The second space of the pair -/
-  snd : TopCat
-  /-- The map that induces the topology on A -/
-  map : snd ⟶ fst
-  isInducing_map : Topology.IsInducing map.hom
+abbrev TopPair :=
+  MorphismProperty.Arrow (fun (A X : TopCat) (f : A ⟶ X) ↦ Topology.IsInducing f.hom) ⊤ ⊤
 
 namespace TopPair
 
-/-- Constructor for a topological (X, A) pair where A ⊆ X. -/
-def ofSubset {X : TopCat} (A : Set X) : TopPair where
-  fst := X
-  snd := TopCat.of A
-  map := ⟨{ toFun := (↑) }⟩
-  isInducing_map := ⟨TopologicalSpace.ext rfl⟩
+variable {X Y : TopPair}
+
+/-- The first space of the pair -/
+abbrev fst : TopCat := X.right
+
+/-- The second space of the pair -/
+abbrev snd : TopCat := X.left
+
+/-- The map that induces the topology on A -/
+abbrev map : X.snd ⟶ X.fst := X.hom
+
+lemma isInducing_map (X : TopPair) : Topology.IsInducing X.map := X.prop
+
+/-- Construct a topological pair from its components. -/
+abbrev of {A X : TopCat} (f : A ⟶ X) (h : Topology.IsInducing f) : TopPair :=
+  MorphismProperty.Arrow.mk (P := fun (A X : TopCat) (f : A ⟶ X) ↦ Topology.IsInducing f.hom) f h
+
+/-- Constructor for a topological pair (X, A) where A ⊆ X. -/
+abbrev ofSubset {X : TopCat} (A : Set X) : TopPair :=
+  @TopPair.of (TopCat.of A) X ⟨{ toFun := Subtype.val }⟩ ⟨TopologicalSpace.ext rfl⟩
+
+/-- Construct a morphism in `TopPair` from its components. -/
+abbrev ofHom (f : X.fst ⟶ Y.fst) (g : X.snd ⟶ Y.snd) (w : g ≫ Y.map = X.map ≫ f := by cat_disch) :=
+  MorphismProperty.Arrow.homMk g f w
 
 variable {X Y Z : TopPair}
 
-/-- A morphism of a pair consists of a morphism between the first spaces and a morphism between the
-second spaces that fit in a commutative square with the maps of the pairs. -/
-@[ext]
-structure Hom (X Y : TopPair) where
-  /-- The map between the first spaces -/
-  fst : X.fst ⟶ Y.fst
-  /-- The map between the second spaces -/
-  snd : X.snd ⟶ Y.snd
-  /-- The proof that the two maps fit in the commutative square -/
-  snd_map : snd ≫ Y.map = X.map ≫ fst := by cat_disch
+/-- The map between the first spaces -/
+abbrev Hom.fst (f : X ⟶ Y) : X.fst ⟶ Y.fst := f.hom.right
 
-attribute [reassoc, elementwise] Hom.snd_map
+/-- The map between the second spaces -/
+abbrev Hom.snd (f : X ⟶ Y) : X.snd ⟶ Y.snd := f.hom.left
 
-@[simps]
-instance : Category TopPair where
-  Hom := Hom
-  id X := { fst := 𝟙 X.fst, snd := 𝟙 X.snd }
-  comp f g := ⟨f.fst ≫ g.fst, f.snd ≫ g.snd, (CommSq.horiz_comp ⟨f.snd_map⟩ ⟨g.snd_map⟩).w⟩
-
-/-- `TopPair` is equivalent to the full subcategory of the comma category with twice the identity
-functor of TopCat on the morphisms that are inducing. -/
-@[simps]
-def equivComma : TopPair ≌ MorphismProperty.Comma (𝟭 TopCat) (𝟭 TopCat)
-    (fun X Y f ↦ Topology.IsInducing (f : TopCat.Hom X Y)) ⊤ ⊤ where
-  functor.obj X :=
-    { left := X.snd
-      right := X.fst
-      hom := X.map
-      prop := X.isInducing_map }
-  functor.map f :=
-    { left := f.snd
-      right := f.fst
-      w := f.snd_map
-      prop_hom_left := by simp
-      prop_hom_right := by simp }
-  inverse.obj X :=
-    { fst := X.right
-      snd := X.left
-      map := X.hom
-      isInducing_map := X.prop }
-  inverse.map f := { fst := f.right, snd := f.left, snd_map := f.w }
-  unitIso := Iso.refl _
-  counitIso := Iso.refl _
+@[simp, reassoc, elementwise]
+lemma Hom.w {X Y : TopPair} (f : X ⟶ Y) :
+    f.hom.left ≫ Y.map = X.map ≫ f.hom.right :=
+  f.hom.w
 
 /-- The functor from topological pairs to topological spaces that forgets the second space, ie. the
 projection to the first space. -/
-@[simps]
-def proj₁ : TopPair ⥤ TopCat where
-  obj X := X.fst
-  map f := f.fst
+abbrev proj₁ : TopPair ⥤ TopCat :=
+  MorphismProperty.Arrow.forget _ _ _ ⋙ CategoryTheory.Arrow.rightFunc
 
 /-- The functor from topological pairs to topological spaces that forgets the first space, ie. the
 projection to the second space. -/
-@[simps]
-def proj₂ : TopPair ⥤ TopCat where
-  obj X := X.snd
-  map f := f.snd
+abbrev proj₂ : TopPair ⥤ TopCat :=
+  MorphismProperty.Arrow.forget _ _ _ ⋙ CategoryTheory.Arrow.leftFunc
 
 /-- The inclusion functor from topological spaces to topological pairs that sends a space X to
 (X, ∅). -/
 @[simps]
 def incl : TopCat ⥤ TopPair where
-  obj X := ⟨_, _, TopCat.isInitialPEmpty.to _, TopCat.IsInducing.empty X⟩
-  map f := ⟨f, 𝟙 (TopCat.of PEmpty), by ext x; induction x⟩
+  obj X := TopPair.of (TopCat.isInitialPEmpty.to X) (TopCat.IsInducing.empty _)
+  map f := TopPair.ofHom f (𝟙 _) <| by ext x; induction x
 
-/-- The functor from topological spaces to topological pairs that sends a space X to (X, X) with the
-identity morphism on X. -/
-@[simps]
-def diag : TopCat ⥤ TopPair where
-  obj X := ⟨_, _, 𝟙 X, Topology.IsInducing.id⟩
-  map f := { fst := f, snd := f }
-
-@[simps]
-instance : Inhabited TopPair := ⟨incl.obj TopCat.inhabited.default⟩
+/-- The functor from topological spaces to topological pairs that sends a space X to the identity
+morphism on X. -/
+abbrev diag : TopCat ⥤ TopPair where
+  obj X := TopPair.of (𝟙 X) Topology.IsInducing.id
+  map f := TopPair.ofHom f f
 
 /-- The inclusion functor is left adjoint to the projection to the first component. -/
 @[simps]
 def inclAdjProj₁ : incl ⊣ proj₁ where
   unit.app X := 𝟙 X
-  counit.app X :=
-    { fst := 𝟙 X.fst,
-      snd := TopCat.isInitialPEmpty.to _
-      snd_map := by ext x; induction x }
-  counit.naturality X Y f := Hom.ext (by simp) (by ext x; induction x)
-  left_triangle_components X := Hom.ext (by simp) (by cat_disch)
+  counit.app X := TopPair.ofHom (𝟙 X.fst) (TopCat.isInitialPEmpty.to X.snd)
 
 /-- The projection functor to the first component is left adjoint to the diagonal functor. -/
 @[simps]
 def proj₁AdjDiag : proj₁ ⊣ diag where
-  unit.app X := { fst := 𝟙 X.fst, snd := X.map }
-  unit.naturality X Y f := Hom.ext (by simp) f.snd_map
+  unit.app X := TopPair.ofHom (𝟙 X.fst) X.map
+  unit.naturality X Y f := MorphismProperty.Arrow.Hom.ext f.w (by cat_disch)
   counit.app X := 𝟙 X
 
-/-- The unique morphism (A, ∅) ⟶ (A, B) that is the identity on A. -/
-@[simps]
-def j (X : TopPair) : TopPair.incl.obj X.fst ⟶ X where
-  fst := 𝟙 X.fst
-  snd := TopCat.isInitialPEmpty.to _
-  snd_map := by ext x; induction x
+/-- The unique morphism (X, ∅) ⟶ (X, A) that is the identity on X. -/
+abbrev j (X : TopPair) : TopPair.incl.obj X.fst ⟶ X :=
+  TopPair.ofHom (𝟙 _) (TopCat.isInitialPEmpty.to _)
 
 /-- A homotopy of maps between topological pairs is a homotopy on the first space and a homotopy on
 the second space that fit in a commutative square with the maps of the pairs. -/
 @[ext]
 structure Homotopy (f g : X ⟶ Y) where
   /-- The homotopy on the first space. -/
-  fst : ContinuousMap.Homotopy f.fst.hom g.fst.hom
+  fst : ContinuousMap.Homotopy f.hom.right.hom g.hom.right.hom
   /-- The homotopy on the second space. -/
-  snd : ContinuousMap.Homotopy f.snd.hom g.snd.hom
+  snd : ContinuousMap.Homotopy f.hom.left.hom g.hom.left.hom
   /-- The proof that the homotopies fit into a commutative square with the maps of the pairs. -/
-  snd_map : TopCat.ofHom snd.toContinuousMap ≫ Y.map =
+  w : TopCat.ofHom snd.toContinuousMap ≫ Y.map =
     TopCat.ofHom (ContinuousMap.prodMap (ContinuousMap.id I) X.map.hom) ≫
       TopCat.ofHom fst.toContinuousMap := by cat_disch
 
-attribute [reassoc, elementwise] Homotopy.snd_map
+attribute [reassoc, elementwise] Homotopy.w
 
 namespace Homotopy
 
-attribute [local simp] Homotopy.snd_map_apply Hom.snd_map_apply
+attribute [local simp] Homotopy.w_apply Hom.w_apply
 
 /-- Given a morphism `f` of topological pairs, we can define a `Homotopy f f` by
 `ContinuousMap.Homotopy.refl` on the first and second components.
 -/
 @[simps]
 def refl (f : X ⟶ Y) : Homotopy f f where
-  fst := ContinuousMap.Homotopy.refl f.fst.hom
-  snd := ContinuousMap.Homotopy.refl f.snd.hom
+  fst := ContinuousMap.Homotopy.refl f.hom.right.hom
+  snd := ContinuousMap.Homotopy.refl f.hom.left.hom
 
 instance : Inhabited (Homotopy (𝟙 X) (𝟙 X)) :=
   ⟨Homotopy.refl _⟩
@@ -204,9 +168,9 @@ noncomputable def trans {f₀ f₁ f₂ : X ⟶ Y} (F : Homotopy f₀ f₁) (G :
     Homotopy f₀ f₂ where
   fst := F.fst.trans G.fst
   snd := F.snd.trans G.snd
-  snd_map := by
+  w := by
     ext
-    dsimp [ContinuousMap.Homotopy.trans_apply]
+    dsimp
     simp only [ContinuousMap.Homotopy.trans_apply, one_div]
     cat_disch
 
@@ -222,9 +186,9 @@ def comp {f₀ f₁ : X ⟶ Y} {g₀ g₁ : Y ⟶ Z} (G : Homotopy g₀ g₁) (F
     Homotopy (f₀ ≫ g₀) (f₁ ≫ g₁) where
   fst := G.fst.comp F.fst
   snd := G.snd.comp F.snd
-  snd_map := by
+  w := by
     ext
-    simp [F.snd_map_apply]
+    simp [F.w_apply]
 
 end Homotopy
 
