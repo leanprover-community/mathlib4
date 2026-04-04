aa68d5f44214eb6bc5e07cd09d1afa64ed75f7f9
Snir Broshi <26556598+SnirBroshi@users.noreply.github.com>
2026-04-04 23:32:39 +0300

    fix `VertexColoring.lean`

--- Mathlib/Combinatorics/SimpleGraph/Coloring/VertexColoring.lean
+++ Mathlib/Combinatorics/SimpleGraph/Coloring/VertexColoring.lean
@@ -440,7 +440,7 @@ lemma chromaticNumber_eq_iff_forall_surjective (hG : G.Colorable n) :
 
 theorem chromaticNumber_bot [Nonempty V] : (⊥ : SimpleGraph V).chromaticNumber = 1 := by
   have : (⊥ : SimpleGraph V).Colorable 1 := ⟨.mk 0 <| by simp⟩
-  exact this.chromaticNumber_le.antisymm <| Order.one_le_iff_pos.2 <| chromaticNumber_pos this
+  exact this.chromaticNumber_le.antisymm <| Order.one_le_iff_pos.2 this.chromaticNumber_pos
 
 @[simp]
 theorem chromaticNumber_top [Fintype V] : (⊤ : SimpleGraph V).chromaticNumber = Fintype.card V := by
