6535ea89890301a852d576ba635f02b059faf5f5
Michael Rothgang <rothgang@math.uni-bonn.de>
2026-04-05 19:24:05 +0200

    feat: also check for norm_num in the flexible linter
    Most other flexible tactics are terminal; dsimp is missing from this list
    (but perhaps less important in practice. Let's see how badly this fails!

--- Mathlib/Tactic/Linter/FlexibleLinter.lean
+++ Mathlib/Tactic/Linter/FlexibleLinter.lean
@@ -117,6 +117,10 @@ The prototypical non-flexible tactic `rw`.
 def flexible? : Syntax → Bool
   | .node _ ``Lean.Parser.Tactic.simp #[_, _, _, only?, _, _] => only?[0].getAtomVal != "only"
   | .node _ ``Lean.Parser.Tactic.simpAll #[_, _, _, only?, _] => only?[0].getAtomVal != "only"
+  | .node _ `Mathlib.Tactic.normNum args => true
+  -- dsimp could be added here, but this may be more disruptive in practice
+  -- NB. `simpa`, `grind` and `n(linarith)` are all flexible, but must be terminal
+  -- (so cannot produce linter errors).
   | _ => false
 
 end Mathlib.Linter
