f4fa2c1024c914ef0fa75150f153f443d4887dbd
vihdzp <vi.hdz.p@gmail.com>
2026-04-05 17:47:33 -0600

    finish

--- Mathlib.lean
+++ Mathlib.lean
@@ -6880,6 +6880,7 @@ public import Mathlib.SetTheory.Ordinal.Notation
 public import Mathlib.SetTheory.Ordinal.Principal
 public import Mathlib.SetTheory.Ordinal.Rank
 public import Mathlib.SetTheory.Ordinal.Topology
+public import Mathlib.SetTheory.Ordinal.Univ
 public import Mathlib.SetTheory.Ordinal.Veblen
 public import Mathlib.SetTheory.ZFC.Basic
 public import Mathlib.SetTheory.ZFC.Cardinal
