046a6038380626877e9cdc914c00c09d5614c65c
vihdzp <vi.hdz.p@gmail.com>
2026-04-05 10:42:33 -0600

    don't need to clean depr thm

--- Mathlib/SetTheory/Ordinal/Family.lean
+++ Mathlib/SetTheory/Ordinal/Family.lean
@@ -657,12 +657,12 @@ theorem lsub_typein (o : Ordinal) : lsub.{u, u} (typein (α := o.ToType) (· < �
       have h := h.trans_eq (type_toType o).symm
       simpa [typein_enum] using lt_lsub.{u, u} (typein (· < ·)) (enum (· < ·) ⟨_, h⟩))
 
+set_option linter.deprecated false in
 @[deprecated IsSuccPrelimit.sSup_Iio (since := "2026-03-27")]
 theorem iSup_typein_limit {o : Ordinal.{u}} (ho : ∀ a, a < o → succ a < o) :
     iSup (typein ((· < ·) : o.ToType → o.ToType → Prop)) = o := by
-  replace ho : IsSuccPrelimit o := by rwa [isSuccPrelimit_iff_succ_lt]
-  rw [iSup, PrincipalSeg.range_eq]
-  simpa [Iio_def] using ho.sSup_Iio
+  rw [(iSup_eq_lsub_iff.{u, u} (typein (· < ·))).2] <;> rw [lsub_typein o]
+  assumption
 
 @[deprecated csSup_Iic (since := "2026-03-27")]
 theorem iSup_typein_succ {o : Ordinal} :
