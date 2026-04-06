a4f7604d9866b02ecc85996c53a4bd7f7ad15d4c
Xavier Genereux <xaviergenereux@hotmail.com>
2026-04-06 17:57:42 -0400

    update mathlib (continuation)

--- Mathlib/RingTheory/Adjoin/Polynomial/Transcendental.lean
+++ Mathlib/RingTheory/Adjoin/Polynomial/Transcendental.lean
@@ -28,12 +28,11 @@ namespace Polynomial
 variable {p q : R[X]}
 
 variable (R) in
-set_option backward.isDefEq.respectTransparency false in
 /-- Given a transcendental element `s : S` over `R`, the `R`-algebra equivalence
-between `R[X]` and `Algebra.adjoin R {s}` given by sending `X` to `s`. -/
+between `R[X]` and `R[s]` given by sending `X` to `s`. -/
 noncomputable def algEquivOfTranscendental (h : Transcendental R s) :
-    R[X] ≃ₐ[R] (adjoin R {s}) :=
-  AlgEquiv.ofBijective (aeval ⟨s, self_mem_adjoin_singleton R s⟩) <| by
+    R[X] ≃ₐ[R] R[s] :=
+  AlgEquiv.ofBijective (aeval (s : R[s])) <| by
     refine ⟨transcendental_iff_injective.mp ?_, ?_⟩
     · rwa [Subalgebra.transcendental_iff_transcendental_val]
     rw [← AlgHom.range_eq_top, _root_.eq_top_iff]
@@ -43,19 +42,19 @@ noncomputable def algEquivOfTranscendental (h : Transcendental R s) :
 
 @[simp]
 theorem algEquivOfTranscendental_coe (h : Transcendental R s) :
-    (algEquivOfTranscendental R s h : R[X] →+* adjoin R {s}) =
-    aeval (R := R) (A := adjoin R {s}) s := rfl
+    (algEquivOfTranscendental R s h : R[X] →+* R[s]) = aeval (R := R) (A := R[s]) s :=
+  rfl
 
 @[simp]
 theorem algEquivOfTranscendental_apply (h : Transcendental R s) (f : R[X]) :
-    algEquivOfTranscendental R s h f = aeval (s : adjoin R {s}) f := rfl
+    algEquivOfTranscendental R s h f = aeval (s : R[s]) f := rfl
 
 lemma algEquivOfTranscendental_apply_X (h : Transcendental R s) :
-    algEquivOfTranscendental R s h X = (s : adjoin R {s}) := by simp
+    algEquivOfTranscendental R s h X = (s : R[s]) := by simp
 
 @[simp]
 theorem algEquivOfTranscendental_symm_aeval (h : Transcendental R s) (f : R[X]) :
-    (algEquivOfTranscendental R s h).symm (aeval (s : adjoin R {s}) f) = f :=
+    (algEquivOfTranscendental R s h).symm (aeval (s : R[s]) f) = f :=
   (algEquivOfTranscendental R s h).toEquiv.injective (by simp)
 
 @[simp]
@@ -65,7 +64,6 @@ theorem algEquivOfTranscendental_symm_gen (h : Transcendental R s) :
 
 open Ideal
 
-set_option backward.isDefEq.respectTransparency false in
 /-- Given a transcendental element `s`, we have an isomorphism
   `R[X] / span X ≃ₐ[R] R[s] / span {s}`. -/
 def algEquivQuotientXOfTranscendental (ht : Transcendental R s) :
@@ -76,7 +74,6 @@ def algEquivQuotientXOfTranscendental (ht : Transcendental R s) :
 theorem algEquivQuotientXOfTranscendental_apply (ht : Transcendental R s) :
     algEquivQuotientXOfTranscendental s ht p = algEquivOfTranscendental R s ht p := rfl
 
-set_option backward.isDefEq.respectTransparency false in
 @[simp]
 theorem adjoinTranscendentalquotientSpanXAlgEquiv_symm_apply (ht : Transcendental R s) :
     (algEquivQuotientXOfTranscendental s ht).symm (p.aeval s : adjoin R {s}) = p :=
@@ -88,7 +85,6 @@ namespace Algebra
 
 open Ideal Polynomial
 
-set_option backward.isDefEq.respectTransparency false in
 /-- Given a transcendental element `s`, we have an isomorphism `R[s] / span {s} ≃ₐ[R] R`.
 This map is equal to evaluation by 0, see _. -/
 def algEquivAdjoinQuotient (ht : Transcendental R s) :
@@ -100,11 +96,9 @@ theorem adjoin_irreducible_gen_of_transcendental (ht : Transcendental R s) [IsDo
   simpa [← MulEquiv.irreducible_iff (algEquivOfTranscendental R s ht)]
     using irreducible_X (R := R)
 
-set_option backward.isDefEq.respectTransparency false in
 def adjoinQuotGen (ht : Transcendental R s) :=
   (algEquivAdjoinQuotient s ht).toRingHom.comp (Ideal.Quotient.mk _)
 
-set_option backward.isDefEq.respectTransparency false in
 /-- Given a transcendental element `s`, we have a map `R[s] →ₐ[R] R` given
 This map is equal to evaluation by 0, see _. -/
 @[simp]
@@ -112,7 +106,6 @@ theorem adjoinQuotGen_apply_polynomial (p : R[X]) (ht : Transcendental R s) :
     (adjoinQuotGen s ht) (p.aeval s : adjoin R {s}) = p.eval 0 := by
   simp [adjoinQuotGen, algEquivAdjoinQuotient]
 
-set_option backward.isDefEq.respectTransparency false in
 theorem adjoinQuotGen_eq_zero_iff_gen_dvd (ht : Transcendental R s) (x : adjoin R {s}) :
     ((adjoinQuotGen s ht) x = 0 ↔ (s : adjoin R {s}) ∣ x) := by
   simp only [adjoinQuotGen, AlgEquiv.toRingEquiv_eq_coe, RingEquiv.toRingHom_eq_coe,
@@ -124,15 +117,19 @@ end Algebra
 open Polynomial
 
 theorem Transcendental.uniqueFactorizationMonoid_adjoin [UniqueFactorizationMonoid R] {s : S}
-      (h : Transcendental R s) : UniqueFactorizationMonoid (Algebra.adjoin R {s}) :=
+      (h : Transcendental R s) : UniqueFactorizationMonoid R[s] :=
   (algEquivOfTranscendental R s h).toMulEquiv.uniqueFactorizationMonoid inferInstance
 
+instance [UniqueFactorizationMonoid R] {s : S} [h : Fact (Transcendental R s)] :
+    UniqueFactorizationMonoid R[s] :=
+  (algEquivOfTranscendental R s h.out).toMulEquiv.uniqueFactorizationMonoid inferInstance
+
 theorem Transcendental.wfDvdMonoid_adjoin [UniqueFactorizationMonoid R]
-    (ht : Transcendental R s) : WfDvdMonoid (Algebra.adjoin R {s}) :=
+    (ht : Transcendental R s) : WfDvdMonoid R[s] :=
   (Transcendental.uniqueFactorizationMonoid_adjoin ht).toIsWellFounded
 
 instance [UniqueFactorizationMonoid R] {s : S} [h : Fact (Transcendental R s)] :
-    UniqueFactorizationMonoid (Algebra.adjoin R {s}) :=
+    UniqueFactorizationMonoid R[s] :=
   (Transcendental.uniqueFactorizationMonoid_adjoin h.out)
 
 end
