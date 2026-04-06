961658dae82a8765439a1fd56df0be440468e375
Parcly Taxel <reddeloostw@gmail.com>
2026-04-07 00:49:54 +0800

    reintroduce `OrderBot` instance for `IdemSemiring`

--- Mathlib/Algebra/Order/Kleene.lean
+++ Mathlib/Algebra/Order/Kleene.lean
@@ -165,6 +165,11 @@ instance (priority := 100) IdemSemiring.toMulLeftMono : MulLeftMono α :=
 instance (priority := 100) IdemSemiring.toMulRightMono : MulRightMono α :=
   ⟨fun a b c hbc ↦ add_eq_left_iff_le.1 <| by rw [← add_mul, hbc.add_eq_left]⟩
 
+-- See note [lower instance priority]
+instance (priority := 100) IdemSemiring.toOrderBot [IdemSemiring α] : OrderBot α where
+  bot := 0
+  bot_le := zero_le
+
 end IdemSemiring
 
 section KleeneAlgebra
