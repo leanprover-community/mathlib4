bf75643c54e30020f7420d73aa47d23ca7732888
SabrinaJewson <sejewson@gmail.com>
2026-04-07 21:06:55 +0100

    chore(Order/OrdContinuous): dualize
    Implement all the RightOrdContinuous functionality by dualizing the
    LeftOrdContinuous versions.

--- Mathlib/Tactic/Translate/ToDual.lean
+++ Mathlib/Tactic/Translate/ToDual.lean
@@ -243,6 +243,8 @@ def abbreviationDict : Std.HashMap String String := .ofList [
   ("directedOrder", "CodirectedOrder"),
   ("galoisInsertion", "GaloisCoinsertion"),
   ("galoisCoinsertion", "GaloisInsertion"),
+  ("leftOrdContinuous", "RightOrdContinuous"),
+  ("rightOrdContinuous", "LeftOrdContinuous"),
 
   ("neTop", "NeBot"),
   ("decidableSucc", "DecidablePred"),
