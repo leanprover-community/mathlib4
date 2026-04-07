df6850f70c9a5aef27cf03c9e58c20ef24bdddbe
Christian Merten <christian@merten.dev>
2026-04-07 23:47:46 +0200

    some move

--- Mathlib/CategoryTheory/Sites/Over.lean
+++ Mathlib/CategoryTheory/Sites/Over.lean
@@ -189,9 +189,7 @@ lemma overEquiv_functorPullback_post {D : Type*} [Category* D] (F : C ⥤ D) {X 
   refine le_antisymm ?_ ?_
   · dsimp [Sieve.overEquiv]
     rw [Sieve.functorPushforward_le_iff_le_functorPullback, ← Sieve.functorPullback_comp]
-    change _ ≤ Sieve.functorPullback (Over.post (X := X) F ⋙ Over.forget _)
-      (Sieve.functorPushforward (Over.forget (F.obj X)) S)
-    rw [Sieve.functorPullback_comp]
+    simp_rw [← CategoryTheory.Over.post_forget_eq_forget_post, Sieve.functorPullback_comp]
     exact Sieve.functorPullback_monotone _ _ (Sieve.le_functorPushforward_pullback _ _)
   · intro Z g hg
     rw [Sieve.overEquiv_iff]
@@ -204,9 +202,7 @@ lemma overEquiv_functorPushforward_post {D : Type*} [Category* D] (F : C ⥤ D) 
     (U : Over X) (S : Sieve U) :
     (Sieve.overEquiv _) (Sieve.functorPushforward (Over.post F) S) =
       Sieve.functorPushforward F ((Sieve.overEquiv _) S) := by
-  dsimp [Sieve.overEquiv, ← Sieve.functorPushforward_comp]
-  rw [← Sieve.functorPushforward_comp, ← Sieve.functorPushforward_comp]
-  rfl
+  simp [Sieve.overEquiv, ← Sieve.functorPushforward_comp, ← Over.post_forget_eq_forget_post]
 
 end Sieve
 
