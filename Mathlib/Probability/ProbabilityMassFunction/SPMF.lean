de466ea90ee96705406280f2b9109ea3edf772cc
Devon Tuma <dtumad@gmail.com>
2026-04-08 07:31:10 -0500

    Fix file header

--- Mathlib/Probability/ProbabilityMassFunction/SPMF.lean
+++ Mathlib/Probability/ProbabilityMassFunction/SPMF.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2017 Johannes Hölzl. All rights reserved.
+Copyright (c) 2026 Devon Tuma. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Johannes Hölzl, Devon Tuma
+Authors: Devon Tuma
 -/
 module
 
