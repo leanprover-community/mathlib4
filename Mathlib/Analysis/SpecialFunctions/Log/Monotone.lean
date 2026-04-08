98cf91e81127d50824e67feb43f789773f50f403
adomasbaliuka <52975890+adomasbaliuka@users.noreply.github.com>
2026-04-09 00:32:12 +0200

    uses Ici consistently
    instead of mix with set builder notation

--- Mathlib/Analysis/SpecialFunctions/Log/Monotone.lean
+++ Mathlib/Analysis/SpecialFunctions/Log/Monotone.lean
@@ -41,14 +41,13 @@ theorem log_mul_self_monotoneOn : MonotoneOn (fun x : ℝ => log x * x) { x | 1 
   grind [mul_log_strictMonoOn.monotoneOn, MonotoneOn.mono, show exp (-1) < 1 by norm_num]
 
 theorem mul_log_StrictAntiOn :
-    StrictAntiOn (fun x : ℝ ↦ x * log x) (Set.Icc 0 (exp (-1))) := by
+    StrictAntiOn (fun x : ℝ ↦ x * log x) <| .Icc 0 (exp (-1)) := by
   refine strictAntiOn_of_deriv_neg (convex_Icc ..) continuous_mul_log.continuousOn fun x hx ↦ ?_
   have hgt : x < rexp (-1) := by simp_all [interior_Icc, mem_Ioo]
   have hpos : 0 < x := by simp_all [interior_Icc, mem_Ioo]
   grind [deriv_mul_log, Real.log_lt_iff_lt_exp hpos |>.mpr hgt]
 
-theorem log_div_self_antitoneOn : AntitoneOn (fun x : ℝ ↦ log x / x) { x | exp 1 ≤ x } := by
-  simp only [AntitoneOn, mem_setOf_eq]
+theorem log_div_self_antitoneOn : AntitoneOn (fun x : ℝ ↦ log x / x) <| .Ici (exp 1) := by
   intro x hex y hey hxy
   have x_pos : 0 < x := (exp_pos 1).trans_le hex
   have y_pos : 0 < y := (exp_pos 1).trans_le hey
@@ -62,32 +61,27 @@ theorem log_div_self_antitoneOn : AntitoneOn (fun x : ℝ ↦ log x / x) { x | e
     _ = log x / x * y - log x := by ring
 
 theorem log_div_self_rpow_antitoneOn {a : ℝ} (ha : 0 < a) :
-    AntitoneOn (fun x : ℝ ↦ log x / x ^ a) { x | exp (1 / a) ≤ x } := by
-  simp only [AntitoneOn, mem_setOf_eq]
+    AntitoneOn (fun x : ℝ ↦ log x / x ^ a) <| .Ici (exp a⁻¹) := by
   intro x hex y _ hxy
-  have x_pos : 0 < x := lt_of_lt_of_le (exp_pos (1 / a)) hex
+  simp only
+  have x_pos : 0 < x := lt_of_lt_of_le (exp_pos a⁻¹) (le_of_le_of_eq hex rfl)
   have y_pos : 0 < y := by linarith
   nth_rw 1 [← rpow_one y]
   nth_rw 1 [← rpow_one x]
   rw [← div_self (ne_of_lt ha).symm, div_eq_mul_one_div a a, rpow_mul y_pos.le, rpow_mul x_pos.le,
     log_rpow (rpow_pos_of_pos y_pos a), log_rpow (rpow_pos_of_pos x_pos a), mul_div_assoc,
     mul_div_assoc, mul_le_mul_iff_right₀ (one_div_pos.mpr ha)]
-  refine log_div_self_antitoneOn ?_ ?_ ?_
-  · simp only [Set.mem_setOf_eq]
-    convert rpow_le_rpow _ hex (le_of_lt ha) using 1
+  refine log_div_self_antitoneOn ?_ ?_ (by gcongr)
+  · convert rpow_le_rpow (by positivity) hex (le_of_lt ha) using 1
     · rw [← exp_mul]
-      simp only [Real.exp_eq_exp]
-      field
-    positivity
-  · simp only [Set.mem_setOf_eq]
-    convert rpow_le_rpow _ (_root_.trans hex hxy) (le_of_lt ha) using 1
+      field_simp
+      rfl
+  · convert rpow_le_rpow (by positivity) (hex.trans hxy) (le_of_lt ha) using 1
     · rw [← exp_mul]
-      simp only [Real.exp_eq_exp]
-      field
-    positivity
-  gcongr
+      field_simp
+      rfl
 
-theorem log_div_sqrt_antitoneOn : AntitoneOn (fun x : ℝ ↦ log x / √x) { x | exp 2 ≤ x } := by
+theorem log_div_sqrt_antitoneOn : AntitoneOn (fun x : ℝ ↦ log x / √x) <| .Ici (exp 2) := by
   simp_rw [sqrt_eq_rpow]
   convert log_div_self_rpow_antitoneOn one_half_pos
   norm_num
