857919f39501b5485a76e47bf13c74bf6f591dbf
Moritz Doll <moritz.doll@googlemail.com>
2026-04-08 10:14:45 +1000

    mk_all

--- Mathlib.lean
+++ Mathlib.lean
@@ -3846,6 +3846,11 @@ public import Mathlib.Data.FunLike.Embedding
 public import Mathlib.Data.FunLike.Equiv
 public import Mathlib.Data.FunLike.Fintype
 public import Mathlib.Data.FunLike.Graded
+public import Mathlib.Data.FunLike.Group
+public import Mathlib.Data.FunLike.GroupWithZero
+public import Mathlib.Data.FunLike.Hom
+public import Mathlib.Data.FunLike.Module
+public import Mathlib.Data.FunLike.Ring
 public import Mathlib.Data.Holor
 public import Mathlib.Data.Ineq
 public import Mathlib.Data.Int.AbsoluteValue
