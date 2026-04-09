c550e8eb75cac6cab717307a29e280681e42c9a8
Jun2M <rnjsgurwns27@gmail.com>
2026-04-09 18:43:36 -0400

    Name change

--- Mathlib/Combinatorics/Graph/Connected/Component.lean
+++ Mathlib/Combinatorics/Graph/Connected/Component.lean
@@ -15,8 +15,9 @@ This file defines the connected components and connectedness of a graph.
 
 ## Main definitions
 
-* `IsCompOf`: a graph `H` is a component of `G` if it is a minimal nonempty closed subgraph of `G`.
-* `IsConnected`: a graph `G` is connected if it is a component of itself. Empty graph is not
+* `IsConnectedComponentOf`: a graph `H` is a component of `G` if it is a minimal nonempty closed
+  subgraph of `G`.
+* `Connected`: a graph `G` is connected if it is a component of itself. Empty graph is not
   considered connected.
 
 -/
@@ -27,55 +28,59 @@ variable {α β : Type*} {G H K : Graph α β}
 
 namespace Graph
 
-/-! ### Components -/
+/-! ### Connected components -/
 
-/-- A component of `G` is a minimal nonempty closed subgraph of `G`. -/
-@[expose] def IsCompOf (H G : Graph α β) : Prop := Minimal (fun H ↦ H ≤c G ∧ V(H).Nonempty) H
+/-- A connected component of `G` is a minimal nonempty closed subgraph of `G`. -/
+@[expose] def IsConnectedComponentOf (H G : Graph α β) : Prop :=
+  Minimal (fun H ↦ H ≤c G ∧ V(H).Nonempty) H
 
-namespace IsCompOf
+namespace IsConnectedComponentOf
 
-@[simp] lemma isClosedSubgraph (h : H.IsCompOf G) : H ≤c G := h.prop.1
-lemma isInducedSubgraph (h : H.IsCompOf G) : H ≤i G := h.isClosedSubgraph.isInducedSubgraph
-@[simp] lemma le (h : H.IsCompOf G) : H ≤ G := h.isClosedSubgraph.le
-@[simp, grind →] lemma nonempty (h : H.IsCompOf G) : V(H).Nonempty := h.prop.2
+@[simp] lemma isClosedSubgraph (h : H.IsConnectedComponentOf G) : H ≤c G := h.prop.1
+lemma isInducedSubgraph (h : H.IsConnectedComponentOf G) : H ≤i G :=
+  h.isClosedSubgraph.isInducedSubgraph
+@[simp] lemma le (h : H.IsConnectedComponentOf G) : H ≤ G := h.isClosedSubgraph.le
+@[grind →] lemma nonempty (h : H.IsConnectedComponentOf G) : V(H).Nonempty := h.prop.2
 
-lemma anti_right (hKH : K ≤ H) (hHG : H ≤ G) (h : K.IsCompOf G) : K.IsCompOf H :=
+lemma anti_right (hKH : K ≤ H) (hHG : H ≤ G) (h : K.IsConnectedComponentOf G) :
+    K.IsConnectedComponentOf H :=
   ⟨⟨h.isClosedSubgraph.anti_right hKH hHG, h.nonempty⟩, fun _ ⟨hK'H, hK'ne⟩ hK'K ↦
     h.le_of_le ⟨(hK'H.anti_right hK'K hKH).trans h.isClosedSubgraph, hK'ne⟩ hK'K⟩
 
-end IsCompOf
+end IsConnectedComponentOf
 
 /-! ### Connectedness -/
 
 /-- A graph is connected if it is a minimal closed subgraph of itself. -/
-protected def IsConnected (G : Graph α β) : Prop := G.IsCompOf G
+@[expose] protected def Connected (G : Graph α β) : Prop := G.IsConnectedComponentOf G
 
-lemma IsConnected.nonempty (hG : G.IsConnected) : V(G).Nonempty := IsCompOf.nonempty hG
+lemma Connected.nonempty (hG : G.Connected) : V(G).Nonempty := IsConnectedComponentOf.nonempty hG
 
 @[simp]
-lemma IsConnected.bot_not_isConnected : ¬ (⊥ : Graph α β).IsConnected := by
+lemma Connected.bot_not_Connected : ¬ (⊥ : Graph α β).Connected := by
   rintro h
   simpa using h.nonempty
 
-lemma isConnected_iff_forall_closed (hG : V(G).Nonempty) :
-    G.IsConnected ↔ ∀ ⦃H⦄, H ≤c G → V(H).Nonempty → H = G := by
+lemma Connected_iff_forall_closed (hG : V(G).Nonempty) :
+    G.Connected ↔ ∀ ⦃H⦄, H ≤c G → V(H).Nonempty → H = G := by
   refine ⟨fun h H hHG hHne ↦ ?_, fun h ↦ ⟨by simpa, fun H ⟨hle, hH⟩ _ ↦ (h hle hH).symm.le⟩⟩
-  rw [Graph.IsConnected] at h
+  rw [Graph.Connected] at h
   exact h.eq_of_le ⟨hHG, hHne⟩ hHG.le
 
-lemma isConnected_iff_forall_closed_ge (hG : V(G).Nonempty) :
-    G.IsConnected ↔ ∀ ⦃H⦄, H ≤c G → V(H).Nonempty → G ≤ H := by
-  rw [isConnected_iff_forall_closed hG]
+lemma Connected_iff_forall_closed_ge (hG : V(G).Nonempty) :
+    G.Connected ↔ ∀ ⦃H⦄, H ≤c G → V(H).Nonempty → G ≤ H := by
+  rw [Connected_iff_forall_closed hG]
   exact ⟨fun h H hle hne ↦ (h hle hne).symm.le, fun h H hle hne ↦ (h hle hne).antisymm' hle.le⟩
 
-lemma IsConnected.ext_of_isClosedSubgraph (hH : H ≤c G) (hne : V(H).Nonempty) (hG : G.IsConnected) :
+lemma Connected.ext_of_isClosedSubgraph (hH : H ≤c G) (hne : V(H).Nonempty) (hG : G.Connected) :
     H = G := by
-  rw [isConnected_iff_forall_closed (hne.mono hH.le.vertexSet_mono)] at hG
+  rw [Connected_iff_forall_closed (hne.mono hH.le.vertexSet_mono)] at hG
   exact hG hH hne
 
-lemma IsCompOf.isConnected (h : H.IsCompOf G) : H.IsConnected := h.anti_right le_rfl h.le
+lemma IsConnectedComponentOf.Connected (h : H.IsConnectedComponentOf G) : H.Connected :=
+  h.anti_right le_rfl h.le
 
-lemma IsConnected.exists_inc_notMem_of_lt (hlt : H < G) (hne : V(H).Nonempty) (hG : G.IsConnected) :
+lemma Connected.exists_inc_notMem_of_lt (hlt : H < G) (hne : V(H).Nonempty) (hG : G.Connected) :
     ∃ e x, G.Inc e x ∧ e ∉ E(H) ∧ x ∈ V(H) := by
   refine by_contra fun hcon ↦ hlt.ne <| hG.ext_of_isClosedSubgraph ?_ hne
   refine IsClosedSubgraph.mk' hlt.le fun e x hex hx ↦ ?_
