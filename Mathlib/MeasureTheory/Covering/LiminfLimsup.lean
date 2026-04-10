50af8ee4cecede614d6d4d617c295e661fb195bd
Yi.Yuan <kysyy1@126.com>
2026-04-10 16:51:06 +0800

    Update Mathlib/MeasureTheory/Covering/LiminfLimsup.lean
    Co-authored-by: Michael Rothgang <rothgang@math.uni-bonn.de>

--- Mathlib/MeasureTheory/Covering/LiminfLimsup.lean
+++ Mathlib/MeasureTheory/Covering/LiminfLimsup.lean
@@ -272,7 +272,7 @@ theorem blimsup_thickening_mul_ae_eq (p : ℕ → Prop) (s : ℕ → Set α) {M 
   have hq {u : ℕ → Set α} (hu : ∀ i, u i ≠ ∅ → 0 < r i) :
       blimsup u atTop p = blimsup u atTop q :=
     blimsup_congr' <| Eventually.of_forall fun i hi ↦ by simp [q, hu i hi]
-  rw [hq fun i hi => (thickening_nonempty_iff.1 <| nonempty_iff_ne_empty.2 hi).1,
+  rw [hq fun i hi ↦ (thickening_nonempty_iff.1 <| nonempty_iff_ne_empty.2 hi).1,
     hq fun i hi ↦ (mul_pos_iff_of_pos_left hM).1 <|
       (thickening_nonempty_iff.1 <| nonempty_iff_ne_empty.2 hi).1]
   exact blimsup_thickening_mul_ae_eq_aux μ q s hM r hr (Eventually.of_forall fun i hi => hi.2)
