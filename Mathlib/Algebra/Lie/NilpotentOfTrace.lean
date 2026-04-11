fe47d4758d730a0710374900fe839dde2b58dba6
Janos Wolosz <janos.wolosz@gmail.com>
2026-04-11 11:35:31 +0200

    Steps forward

--- Mathlib/Algebra/Lie/NilpotentOfTrace.lean
+++ Mathlib/Algebra/Lie/NilpotentOfTrace.lean
@@ -114,8 +114,8 @@ theorem isNilpotent_toEnd_of_mem_ker_traceForm {K L M : Type*}
   let E : Submodule ℚ K := Submodule.span ℚ (Set.range μ)
   suffices hs_zero : s = 0 by rw [hX_ns, hs_zero, add_zero]; exact hn_nil
   suffices h_f_zero : ∀ f : Module.Dual ℚ E, f = 0 by
-    have : Subsingleton E :=
-      (subsingleton_dual_iff ℚ).mp ⟨fun a b => by rw [h_f_zero a, h_f_zero b]⟩
+    have : Subsingleton E := (subsingleton_dual_iff ℚ).mp
+      ⟨fun a b => by rw [h_f_zero a, h_f_zero b]⟩
     refine hs_ss.eq_zero_iff_forall_eigenvalue.mpr fun ν hν => ?_
     have : Nontrivial (s.eigenspace ν) :=
       Submodule.nontrivial_iff_ne_bot.mpr (hasEigenvalue_iff.mp hν)
