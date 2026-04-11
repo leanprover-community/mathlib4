e45a3f770e87f44b94d9c025154d9a85afd6fa13
WilliamCoram <williamecoram@gmail.com>
2026-04-11 18:21:17 +0100

    placing lemma into Restricted with a TODO

--- Mathlib/RingTheory/MvPowerSeries/Restricted.lean
+++ Mathlib/RingTheory/MvPowerSeries/Restricted.lean
@@ -10,7 +10,6 @@ public import Mathlib.Analysis.Normed.Order.Lattice
 public import Mathlib.Analysis.RCLike.Basic
 public import Mathlib.RingTheory.MvPowerSeries.Basic
 public import Mathlib.Algebra.Order.Antidiag.Prod
-public import Mathlib.RingTheory.MvPowerSeries.test
 
 /-!
 # Multivariate restricted power series
@@ -68,6 +67,20 @@ lemma isRestricted.neg (c : σ → ℝ) {f : MvPowerSeries σ R} (hf : IsRestric
   simpa [IsRestricted] using hf
 
 open IsUltrametricDist
+
+/- TODO: Find a home for this lemma. -/
+lemma tendsto_sup'_antidiagonal_cofinite
+    {M R : Type*} [AddMonoid M] [Finset.HasAntidiagonal M] {f : M × M → R}
+    [LinearOrder R] {F : Filter R} (hf : Tendsto f cofinite F) :
+    Tendsto (fun a ↦ (Finset.antidiagonal a).sup' (Finset.nonempty_antidiagonal _) f)
+      cofinite F := by
+  intro U hU
+  refine ((((hf hU).image Prod.fst)).add ((hf hU).image Prod.snd)).subset ?_
+  simp only [Set.subset_def, Set.mem_compl_iff, Set.mem_preimage]
+  intro x hx
+  obtain ⟨i, hi, e⟩ := Finset.exists_mem_eq_sup' (Finset.nonempty_antidiagonal x) f
+  obtain rfl : i.1 + i.2 = x := by simpa using hi
+  exact Set.add_mem_add (by simpa using ⟨i.2, e ▸ hx⟩) (by simpa using ⟨i.1, e ▸ hx⟩)
 
 lemma tendsto_antidiagonal {M S : Type*} [AddMonoid M] [Finset.HasAntidiagonal M] [NormedRing S]
     [IsUltrametricDist S] {C : M → ℝ} (hC : ∀ a b, C (a + b) = C a * C b) {f g : M → S}
