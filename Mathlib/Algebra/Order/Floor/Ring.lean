579ae7034fd443cc1e84a9c3c7e91245a4e917b7
Gian Sanjaya <43656481+mortarsanjaya@users.noreply.github.com>
2026-04-12 16:53:03 -0400

    Ring: Golf some more proofs
    Again, the main goal is to weaken assumptions on the corresponding lemmas. But in doing that we obtained a shorter proof.

--- Mathlib/Algebra/Order/Floor/Ring.lean
+++ Mathlib/Algebra/Order/Floor/Ring.lean
@@ -509,66 +509,41 @@ theorem image_fract (s : Set R) : fract '' s = ⋃ m : ℤ, (fun x : R => x - m)
 
 section LinearOrderedField
 
-variable {k : Type*} [Field k] [LinearOrder k] [IsOrderedRing k] [FloorRing k] {b : k}
+variable {k : Type*} [Field k] [LinearOrder k] [FloorRing k]
+
+theorem fract_div_mul_self_add_zsmul_eq (a b : k) (ha : a ≠ 0) :
+    fract (b / a) * a + ⌊b / a⌋ • a = b := by
+  rw [zsmul_eq_mul, ← add_mul, fract_add_floor, div_mul_cancel₀ b ha]
+
+theorem fract_div_intCast_eq_div_intCast_mod [IsOrderedAddMonoid k] {m : ℤ} {n : ℕ} :
+    fract ((m : k) / n) = ↑(m % n) / n := by
+  rcases n.eq_zero_or_pos with rfl | hn
+  · simp
+  · have hn' : (n : k) ≠ 0 := by positivity
+    rw [eq_div_iff hn', fract, floor_div_natCast, floor_intCast,
+      sub_mul, div_mul_cancel₀ _ hn', sub_eq_iff_eq_add]
+    exact mod_cast (emod_add_ediv_mul m ↑n).symm
+
+theorem fract_div_natCast_eq_div_natCast_mod [IsOrderedAddMonoid k] {m n : ℕ} :
+    fract ((m : k) / n) = ↑(m % n) / n := by
+  have h : fract ((m : ℤ) / (n : k)) = ↑((m : ℤ) % n) / n :=
+    fract_div_intCast_eq_div_intCast_mod
+  rwa [cast_natCast, ← natCast_emod, cast_natCast] at h
+
+
+variable [IsOrderedRing k]
 
 theorem fract_div_mul_self_mem_Ico (a b : k) (ha : 0 < a) : fract (b / a) * a ∈ Ico 0 a :=
   ⟨(mul_nonneg_iff_of_pos_right ha).2 (fract_nonneg (b / a)),
     (mul_lt_iff_lt_one_left ha).2 (fract_lt_one (b / a))⟩
 
-omit [IsOrderedRing k] in
-theorem fract_div_mul_self_add_zsmul_eq (a b : k) (ha : a ≠ 0) :
-    fract (b / a) * a + ⌊b / a⌋ • a = b := by
-  rw [zsmul_eq_mul, ← add_mul, fract_add_floor, div_mul_cancel₀ b ha]
-
-theorem sub_floor_div_mul_nonneg (a : k) (hb : 0 < b) : 0 ≤ a - ⌊a / b⌋ * b :=
+theorem sub_floor_div_mul_nonneg (a : k) {b : k} (hb : 0 < b) : 0 ≤ a - ⌊a / b⌋ * b :=
   sub_nonneg_of_le <| (le_div_iff₀ hb).1 <| floor_le _
 
-theorem sub_floor_div_mul_lt (a : k) (hb : 0 < b) : a - ⌊a / b⌋ * b < b :=
+theorem sub_floor_div_mul_lt (a : k) {b : k} (hb : 0 < b) : a - ⌊a / b⌋ * b < b :=
   sub_lt_iff_lt_add.2 <| by
     rw [← one_add_mul, ← div_lt_iff₀ hb, add_comm]
     exact lt_floor_add_one _
-
-theorem fract_div_natCast_eq_div_natCast_mod {m n : ℕ} : fract ((m : k) / n) = ↑(m % n) / n := by
-  rcases n.eq_zero_or_pos with (rfl | hn)
-  · simp
-  have hn' : 0 < (n : k) := by
-    norm_cast
-  refine fract_eq_iff.mpr ⟨?_, ?_, m / n, ?_⟩
-  · positivity
-  · simpa only [div_lt_one hn', Nat.cast_lt] using m.mod_lt hn
-  · rw [sub_eq_iff_eq_add', ← mul_right_inj' hn'.ne', mul_div_cancel₀ _ hn'.ne', mul_add,
-      mul_div_cancel₀ _ hn'.ne']
-    norm_cast
-    rw [← Nat.cast_add, Nat.mod_add_div m n]
-
-theorem fract_div_intCast_eq_div_intCast_mod {m : ℤ} {n : ℕ} :
-    fract ((m : k) / n) = ↑(m % n) / n := by
-  rcases n.eq_zero_or_pos with (rfl | hn)
-  · simp
-  replace hn : 0 < (n : k) := by norm_cast
-  have : ∀ {l : ℤ}, 0 ≤ l → fract ((l : k) / n) = ↑(l % n) / n := by
-    intro l hl
-    obtain ⟨l₀, rfl | rfl⟩ := l.eq_nat_or_neg
-    · rw [cast_natCast, ← natCast_mod, cast_natCast, fract_div_natCast_eq_div_natCast_mod]
-    · rw [Right.nonneg_neg_iff, natCast_nonpos_iff] at hl
-      simp [hl]
-  obtain ⟨m₀, rfl | rfl⟩ := m.eq_nat_or_neg
-  · exact this (natCast_nonneg m₀)
-  let q := ⌈↑m₀ / (n : k)⌉
-  let m₁ := q * ↑n - (↑m₀ : ℤ)
-  have hm₁ : 0 ≤ m₁ := by
-    simpa [m₁, ← @cast_le k, ← div_le_iff₀ hn] using FloorRing.gc_ceil_coe.le_u_l _
-  calc
-    fract ((Int.cast (-(m₀ : ℤ)) : k) / (n : k))
-      = fract (-(m₀ : k) / n) := by simp
-    _ = fract ((m₁ : k) / n) := ?_
-    _ = Int.cast (m₁ % (n : ℤ)) / Nat.cast n := this hm₁
-    _ = Int.cast (-(↑m₀ : ℤ) % ↑n) / Nat.cast n := ?_
-  · rw [← fract_intCast_add q, ← mul_div_cancel_right₀ (q : k) hn.ne', ← add_div, ← sub_eq_add_neg]
-    simp [m₁]
-  · congr 2
-    simp only [m₁]
-    rw [sub_eq_add_neg, add_comm (q * ↑n), add_mul_emod_self_right]
 
 end LinearOrderedField
 
