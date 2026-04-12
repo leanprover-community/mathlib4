55db4c0c029779a9e02a5e48db9886f3cc66a4de
Martin Winter <martin.winter.math@gmail.com>
2026-04-12 23:45:58 +0200

    Remove Dual.lean from Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -808,7 +808,6 @@ public import Mathlib.Algebra.Module.SpanRankOperations
 public import Mathlib.Algebra.Module.Submodule.Basic
 public import Mathlib.Algebra.Module.Submodule.Bilinear
 public import Mathlib.Algebra.Module.Submodule.Defs
-public import Mathlib.Algebra.Module.Submodule.Dual
 public import Mathlib.Algebra.Module.Submodule.EqLocus
 public import Mathlib.Algebra.Module.Submodule.Equiv
 public import Mathlib.Algebra.Module.Submodule.Invariant
