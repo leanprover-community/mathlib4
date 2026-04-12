eb501d6d12d3ece3f84f36c65f41c3aef1ab6af5
Martin Winter <martin.winter.math@gmail.com>
2026-04-12 23:39:34 +0200

    Adjust license

--- Mathlib/LinearAlgebra/SesquilinearForm/Orthogonal.lean
+++ Mathlib/LinearAlgebra/SesquilinearForm/Orthogonal.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2026 Martin Winter. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Andreas Swerdlow
+Authors: Martin Winter
 -/
 module
 
