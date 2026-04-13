c33ff46acd903a00ba0d58bef74ecca2f56ba86c
Yi.Yuan <kysyy1@126.com>
2026-04-13 18:22:20 +0800

    Apply suggestion from @themathqueen
    Co-authored-by: Monica Omar <23701951+themathqueen@users.noreply.github.com>

--- Mathlib/Analysis/Convex/Approximation.lean
+++ Mathlib/Analysis/Convex/Approximation.lean
@@ -222,10 +222,9 @@ theorem univ_sSup_of_nat_affine_eq [HereditarilyLindelofSpace E]
       ∧ ⨆ i, re ∘ (l i) + const E (c i) = φ := by
   obtain ⟨l, c, hle, hsup⟩ := hφcv.sSup_of_nat_affine_eq (𝕜 := 𝕜) (s := univ) isClosed_univ
     (lowerSemicontinuousOn_univ_iff.2 hφc)
-  refine ⟨l, c, fun i x ↦ ?_, ?_⟩
-  · exact hle i ⟨x, trivial⟩
-  · ext x
-    simpa using congrFun hsup ⟨x, trivial⟩
+  refine ⟨l, c, fun i x ↦ hle i ⟨x, trivial⟩, ?_⟩
+  ext x
+  simpa using congrFun hsup ⟨x, trivial⟩
 
 end RCLike
 
