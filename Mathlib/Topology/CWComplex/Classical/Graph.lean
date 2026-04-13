e68decddaa949c5309dc36393e03b562b3f80072
Jun2M <rnjsgurwns27@gmail.com>
2026-04-13 11:06:18 -0400

    apply comments

--- Mathlib/Topology/CWComplex/Classical/Graph.lean
+++ Mathlib/Topology/CWComplex/Classical/Graph.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2025 Jun Kwon. All rights reserved.
+Copyright (c) 2026 Jun Kwon. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jun Kwon
 -/
@@ -35,7 +35,7 @@ def CWComplex.OneSkeletonGraph (C : Set X) [CWComplex C] : Graph (cell C 0) (cel
   isLink_symm _ _ x y h := by grind
   eq_or_eq_of_isLink_of_isLink e x y z w h1 h2 := by
     simp_rw [closedCell_zero_eq_singleton] at h1 h2
-    rw [h1] at h2; clear h1
+    rw [h1] at h2
     simp only [(RelCWComplex.map_zero_injective C).eq_iff, union_singleton, pair_eq_pair_iff] at h2
     tauto
   left_mem_of_isLink _ _ _ _ := mem_univ _
@@ -43,39 +43,38 @@ def CWComplex.OneSkeletonGraph (C : Set X) [CWComplex C] : Graph (cell C 0) (cel
     simp only [mem_univ, true_iff]
     exact cellFrontier_one_exists e
 
-namespace CWComplex
+namespace CWComplex.OneSkeletonGraph
 
 variable {C : Set X} [CWComplex C]
 
-lemma OneSkeletonGraph.isLink_iff_pair (e : cell C 1) (x y : cell C 0) :
+lemma isLink_iff_pair (e : cell C 1) (x y : cell C 0) :
     (OneSkeletonGraph C).IsLink e x y ↔ cellFrontier 1 e = {map 0 x ![], map 0 y ![]} := by
   rw [OneSkeletonGraph_isLink, closedCell_zero_eq_singleton, closedCell_zero_eq_singleton,
     singleton_union]
 
-lemma OneSkeletonGraph.exists_isLoopAt_iff (e : cell C 1) :
-    (∃ x : cell C 0, (OneSkeletonGraph C).IsLoopAt e x) ↔
+lemma exists_isLoopAt_iff (e : cell C 1) : (∃ x : cell C 0, (OneSkeletonGraph C).IsLoopAt e x) ↔
       ∃ x : cell C 0, cellFrontier 1 e = closedCell 0 x := by
   refine exists_congr fun x ↦ ?_
   change cellFrontier 1 e = closedCell 0 x ∪ closedCell 0 x ↔ _
   rw [union_self]
 
-lemma OneSkeletonGraph.exists_isLoopAt_iff_subsingleton (e : cell C 1) :
+lemma exists_isLoopAt_iff_subsingleton (e : cell C 1) :
     (∃ x : cell C 0, (OneSkeletonGraph C).IsLoopAt e x) ↔ (cellFrontier 1 e).Subsingleton := by
-  rw [OneSkeletonGraph.exists_isLoopAt_iff]
+  rw [exists_isLoopAt_iff]
   refine ⟨fun ⟨x, hx⟩ => by simp [closedCell_zero_eq_singleton, hx], fun h => ?_⟩
   obtain ⟨x, y, hxy⟩ := cellFrontier_one_exists e
   simp only [closedCell_zero_eq_singleton, union_singleton] at hxy
   obtain rfl : x = y := RelCWComplex.map_zero_injective C <| (hxy ▸ h) (by simp) (by simp)
   exact ⟨x, by simp [hxy, closedCell_zero_eq_singleton]⟩
 
-lemma OneSkeletonGraph.not_exists_isLoopAt_iff_nontrivial (e : cell C 1) :
+lemma not_exists_isLoopAt_iff_nontrivial (e : cell C 1) :
     ¬(∃ x : cell C 0, (OneSkeletonGraph C).IsLoopAt e x) ↔ (cellFrontier 1 e).Nontrivial :=
   (exists_isLoopAt_iff_subsingleton e).not.trans not_subsingleton_iff
 
 @[simp]
-lemma OneSkeletonGraph.adj_iff (x y : cell C 0) : (OneSkeletonGraph C).Adj x y ↔
+lemma adj_iff (x y : cell C 0) : (OneSkeletonGraph C).Adj x y ↔
     ∃ e : cell C 1, cellFrontier 1 e = closedCell 0 x ∪ closedCell 0 y := Iff.rfl
 
-end CWComplex
+end CWComplex.OneSkeletonGraph
 
 end Topology
