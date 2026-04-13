a76c3b08836c84e1e44d3931df21036c801358ac
Michael Rothgang <rothgang@math.uni-bonn.de>
2026-04-13 15:47:44 +0200

    Fix header linter: do we need this file at all?

--- Mathlib/Lean/Meta/WarningAsError.lean
+++ Mathlib/Lean/Meta/WarningAsError.lean
@@ -3,10 +3,9 @@ Copyright (c) 2025 Heather Macbeth. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Heather Macbeth
 -/
+module
 
--- Import this linter explicitly to ensure that
--- this file has a valid copyright header and module docstring.
-import Mathlib.Tactic.Linter.Header
+public import Mathlib.Init
 
 /-! # "Warning as error" wrapper
 
@@ -17,7 +16,7 @@ This file contains a wrapper `Lean.Meta.withWarningAsError : MetaM α → MetaM 
 open Lean Elab Term Command Linter
 
 /-- A wrapper for a `MetaM` program, causing it to fail if it produces unreported messages. -/
-def Lean.Meta.withWarningAsError {α : Type} (m : MetaM α) : MetaM α := do
+public def Lean.Meta.withWarningAsError {α : Type} (m : MetaM α) : MetaM α := do
   let a ← m
   let msgs := (← getThe Core.State).messages.unreported
   if msgs.isEmpty then
