13aa3a0a0859eae9180c26c67be4147a9e149b24
Pepa Montero Jimena <pepamonterojimena@gmail.com>
2026-04-13 22:13:20 +0200

    style: change copyright year
    Co-authored-by: Michael Rothgang <rothgang@math.uni-bonn.de>

--- Mathlib/Geometry/Manifold/Instances/OrbitSpace.lean
+++ Mathlib/Geometry/Manifold/Instances/OrbitSpace.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2026 Michael Rothgang, Pepa Montero, Archibald Browne. All rights reserved.
+Copyright (c) 2025 Michael Rothgang, Pepa Montero, Archibald Browne. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Michael Rothgang, Pepa Montero, Archibald Browne
 -/
