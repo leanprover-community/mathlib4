6d439df9026e5e3f5b2492ebb1f6cb37f27c292f
Jack McCarthy <37917934+Deicyde@users.noreply.github.com>
2026-04-13 17:46:40 -0400

    Added continuousAt_symm_prodMk_left.

--- Mathlib/Topology/FiberBundle/Trivialization.lean
+++ Mathlib/Topology/FiberBundle/Trivialization.lean
@@ -592,6 +592,21 @@ theorem continuousAt_proj (ex : x ∈ e.source) : ContinuousAt proj x :=
 theorem continuousOn_proj : ContinuousOn proj e.source :=
   continuousOn_of_forall_continuousAt fun _ ↦ e.continuousAt_proj
 
+/-- Fixing the fiber coordinate and varying the base point in a trivialization inverse is continuous
+at any point in the base set. -/
+theorem continuousAt_symm_prodMk_left {b : B} {v : F} (hb : b ∈ e.baseSet) :
+    ContinuousAt (fun q => e.toOpenPartialHomeomorph.symm (q, v)) b :=
+  ContinuousAt.comp (f := fun q => (q, v))
+    (e.toOpenPartialHomeomorph.continuousOn_symm.continuousAt
+      (e.toOpenPartialHomeomorph.open_target.mem_nhds (e.mem_target.mpr hb)))
+    (continuousAt_id.prodMk continuousAt_const)
+
+/-- Fixing the fiber coordinate and varying the base point in a trivialization inverse is continuous
+on the base set. -/
+theorem continuousOn_symm_prodMk_left {v : F} :
+    ContinuousOn (fun q => e.toOpenPartialHomeomorph.symm (q, v)) e.baseSet :=
+  fun _ hb => (e.continuousAt_symm_prodMk_left hb).continuousWithinAt
+
 /-- Pre-composition of a `Bundle.Trivialization` and a `Homeomorph`. -/
 protected def compHomeomorph {Z' : Type*} [TopologicalSpace Z'] (h : Z' ≃ₜ Z) :
     Trivialization F (proj ∘ h) where
