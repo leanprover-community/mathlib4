3eeeddeebbc2044dc82460bb5fcf0cc11d49717a
Raphael Douglas Giles <77658801+Raph-DG@users.noreply.github.com>
2026-04-13 21:02:55 +0200

    Apply suggestion from @riccardobrasca
    Co-authored-by: Riccardo Brasca <riccardo.brasca@gmail.com>

--- Mathlib/RingTheory/DedekindDomain/AdicValuation.lean
+++ Mathlib/RingTheory/DedekindDomain/AdicValuation.lean
@@ -333,7 +333,7 @@ theorem valuation_lt_one_iff_mem (r : R) :
   rw [valuation_of_algebraMap]; exact v.intValuation_lt_one_iff_mem r
 
 @[simp]
-theorem valuation_eq_one_iff_notMem (r : R) :
+theorem valuation_eq_one_iff_notMem {r : R} :
     v.valuation K (algebraMap R K r) = 1 ↔ r ∉ v.asIdeal := by
   rw [← HeightOneSpectrum.valuation_lt_one_iff_mem (K := K), le_antisymm_iff]
   simp [HeightOneSpectrum.valuation_le_one]
