ab22c0c087b531a6ed510014b762a8eaa4b7a166
Abel Doñate Muñoz <68061067+abeldonate@users.noreply.github.com>
2026-04-14 20:19:32 +0200

    Update Mathlib/RingTheory/Ideal/Maps.lean
    Co-authored-by: Hannah Scholz <70071345+scholzhannah@users.noreply.github.com>

--- Mathlib/RingTheory/Ideal/Maps.lean
+++ Mathlib/RingTheory/Ideal/Maps.lean
@@ -936,9 +936,7 @@ theorem annihilator_iSup (ι : Sort w) (f : ι → Submodule R M) :
 
 theorem annihilator_sup (N P : Submodule R M) :
     (N ⊔ P).annihilator = N.annihilator ⊓ P.annihilator := by
-  have := annihilator_iSup Bool (Bool.rec N P)
-  simp only [iSup_bool_eq, iInf_bool_eq] at this
-  rwa [sup_comm, inf_comm]
+  simpa [iSup_bool_eq, iInf_bool_eq] using annihilator_iSup Bool (Bool.rec P N)
 
 theorem le_annihilator_iff {N : Submodule R M} {I : Ideal R} : I ≤ annihilator N ↔ I • N = ⊥ := by
   simp_rw [← le_bot_iff, smul_le, SetLike.le_def, mem_annihilator]; rfl
