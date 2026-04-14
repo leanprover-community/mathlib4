fa4e95daeba7cd8f53c52b73f50ec6fa354ad3b0
JadAbouHawili <81527369+JadAbouHawili@users.noreply.github.com>
2026-04-14 12:50:13 +0300

    subsingleton_iff theorem

--- Mathlib/Data/Set/Subsingleton.lean
+++ Mathlib/Data/Set/Subsingleton.lean
@@ -63,6 +63,16 @@ theorem subsingleton_iff_singleton {x} (hx : x ∈ s) : s.Subsingleton ↔ s = {
 theorem Subsingleton.eq_empty_or_singleton (hs : s.Subsingleton) : s = ∅ ∨ ∃ x, s = {x} :=
   s.eq_empty_or_nonempty.elim Or.inl fun ⟨x, hx⟩ => Or.inr ⟨x, hs.eq_singleton_of_mem hx⟩
 
+theorem Subsingleton.subsingleton_iff_eq_empty_or_singleton  : s.Subsingleton ↔ s = ∅ ∨ ∃ x, s = {x} :=by
+  constructor
+  intro h ; exact Set.Subsingleton.eq_empty_or_singleton h
+  intro h
+  rcases h with h|h
+  rw [h] ; simp
+  obtain ⟨a,ha⟩ := h
+  subst ha
+  simp
+
 theorem Subsingleton.induction_on {p : Set α → Prop} (hs : s.Subsingleton) (he : p ∅)
     (h₁ : ∀ x, p {x}) : p s := by
   rcases hs.eq_empty_or_singleton with (rfl | ⟨x, rfl⟩)
