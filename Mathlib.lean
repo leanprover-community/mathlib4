3077f3359abc7dcdcc5045c50d5f6eaf51d49c0d
Nikolas Tapia <tapia@wias-berlin.de>
2026-04-15 10:52:41 +0200

    add missing file

--- Mathlib.lean
+++ Mathlib.lean
@@ -1848,6 +1848,7 @@ public import Mathlib.Analysis.Complex.ValueDistribution.LogCounting.Asymptotic
 public import Mathlib.Analysis.Complex.ValueDistribution.LogCounting.Basic
 public import Mathlib.Analysis.Complex.ValueDistribution.Proximity.Basic
 public import Mathlib.Analysis.ConstantSpeed
+public import Mathlib.Analysis.Controls.BoundedVariation
 public import Mathlib.Analysis.Controls.ControlOn
 public import Mathlib.Analysis.Controls.Defs
 public import Mathlib.Analysis.Convex.AmpleSet
