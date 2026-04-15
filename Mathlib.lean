37227330305a86b4864a7f4e53dd909201deb917
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2026-04-15 20:02:29 +0800

    restore change

--- Mathlib.lean
+++ Mathlib.lean
@@ -6275,7 +6275,6 @@ public import Mathlib.RingTheory.DedekindDomain.LinearDisjoint
 public import Mathlib.RingTheory.DedekindDomain.PID
 public import Mathlib.RingTheory.DedekindDomain.SInteger
 public import Mathlib.RingTheory.DedekindDomain.SelmerGroup
-public import Mathlib.RingTheory.Depth.Basic
 public import Mathlib.RingTheory.Derivation.Basic
 public import Mathlib.RingTheory.Derivation.DifferentialRing
 public import Mathlib.RingTheory.Derivation.Lie
