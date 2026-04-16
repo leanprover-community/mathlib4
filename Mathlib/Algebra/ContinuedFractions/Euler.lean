e89f97d853d695ab44a2c508a522f1f0d29bcc86
emlis <175462753+emlis42@users.noreply.github.com>
2026-04-16 22:45:38 +0800

    chore

--- Mathlib/Algebra/ContinuedFractions/Euler.lean
+++ Mathlib/Algebra/ContinuedFractions/Euler.lean
@@ -11,19 +11,19 @@ variable {K : Type*} (v : K) [Field K]
 variable {g : GenContFract K}
 
 def Euler (h : K) (ρ : Stream'.Seq K) : GenContFract K := ⟨h, ⟨
-  fun n => match n with
-  | 0 => ρ.get? n |>.map fun ρ => ⟨ρ, 1⟩
-  | _ => ρ.get? n |>.map fun ρ => ⟨-ρ, 1 + ρ⟩,
-  fun {n} h => match n with
+  fun n ↦ match n with
+  | 0 => ρ.get? n |>.map fun ρ ↦ ⟨ρ, 1⟩
+  | _ => ρ.get? n |>.map fun ρ ↦ ⟨-ρ, 1 + ρ⟩,
+  fun {n} h ↦ match n with
   | 0 => by simp_all
   | n + 1 => by simp at h; simpa using ρ.property h
 ⟩⟩
 
 private def toEulerAux (g : GenContFract K) : Stream'.Seq K := ⟨
-  fun n => match n with
-  | 0 => g.s.get? n |>.map fun c => c.a / c.b
-  | _ => g.s.get? n |>.map fun c => - c.a * g.dens (n - 1) / g.dens (n + 1),
-  fun {n} h => match n with
+  fun n ↦ match n with
+  | 0 => g.s.get? n |>.map fun c ↦ c.a / c.b
+  | _ => g.s.get? n |>.map fun c ↦ - c.a * g.dens (n - 1) / g.dens (n + 1),
+  fun {n} h ↦ match n with
   | 0 => by simp_all
   | n + 1 => by simp at h; simpa using g.s.property h
 ⟩
