fea65c76a2d62f34456d1f7de1397874a2e3c267
Ralf Stephan <ralf@ark.in-berlin.de>
2026-04-16 17:50:12 +0200

    module

--- Mathlib/NumberTheory/Height/Rat.lean
+++ Mathlib/NumberTheory/Height/Rat.lean
@@ -3,6 +3,8 @@ Copyright (c) 2026 Ralf Stephan. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Ralf Stephan
 -/
+module
+
 import Mathlib.NumberTheory.Height.NumberField
 
 /-!
