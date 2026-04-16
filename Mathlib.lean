f506d35fcfcaf6f662ce800b6cb3deb95461d870
Edward van de Meent <edwardvdmeent@gmail.com>
2026-04-16 23:14:54 +0200

    mk_all

--- Mathlib.lean
+++ Mathlib.lean
@@ -3128,8 +3128,8 @@ public import Mathlib.CategoryTheory.ObjectProperty.Small
 public import Mathlib.CategoryTheory.Opposites
 public import Mathlib.CategoryTheory.PEmpty
 public import Mathlib.CategoryTheory.PUnit
-public import Mathlib.CategoryTheory.PartialMap.PrePartialMap
 public import Mathlib.CategoryTheory.PartialMap.Defs
+public import Mathlib.CategoryTheory.PartialMap.PrePartialMap
 public import Mathlib.CategoryTheory.PathCategory.Basic
 public import Mathlib.CategoryTheory.PathCategory.MorphismProperty
 public import Mathlib.CategoryTheory.Pi.Basic
