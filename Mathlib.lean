d61755b35018e36753051283b0b55a33eea71b6f
Riccardo Brasca <riccardo.brasca@gmail.com>
2026-04-16 11:39:23 +0100

    fix this

--- Mathlib.lean
+++ Mathlib.lean
@@ -6248,7 +6248,8 @@ public import Mathlib.RingTheory.Bialgebra.MonoidAlgebra
 public import Mathlib.RingTheory.Bialgebra.TensorProduct
 public import Mathlib.RingTheory.Binomial
 public import Mathlib.RingTheory.ChainOfDivisors
-public import Mathlib.RingTheory.ClassGroup
+public import Mathlib.RingTheory.ClassGroup.Basic
+public import Mathlib.RingTheory.ClassGroup.Extension
 public import Mathlib.RingTheory.Coalgebra.Basic
 public import Mathlib.RingTheory.Coalgebra.CoassocSimps
 public import Mathlib.RingTheory.Coalgebra.Convolution
