c5088a9d23be88f6bac02c6e460638ed8d2b8839
emlis <175462753+emlis42@users.noreply.github.com>
2026-04-16 22:08:08 +0800

    chore

--- Mathlib/Algebra/ContinuedFractions/Euler.lean
+++ Mathlib/Algebra/ContinuedFractions/Euler.lean
@@ -12,20 +12,22 @@ variable {g : GenContFract K}
 
 def Euler (h : K) (ρ : Stream'.Seq K) : GenContFract K := ⟨h, ⟨
   fun n => match n with
-  | 0 => ρ.val n |>.map fun ρ => ⟨ρ, 1⟩
-  | _ => ρ.val n |>.map fun ρ => ⟨-ρ, 1 + ρ⟩,
+  | 0 => ρ.get? n |>.map fun ρ => ⟨ρ, 1⟩
+  | _ => ρ.get? n |>.map fun ρ => ⟨-ρ, 1 + ρ⟩,
   fun {n} h => match n with
-  | 0 => by simp at h; simp [h]
+  | 0 => by simp_all
   | n + 1 => by simp at h; simpa using ρ.property h
 ⟩⟩
-def toEulerAux (g : GenContFract K) : Stream'.Seq K := ⟨
+
+private def toEulerAux (g : GenContFract K) : Stream'.Seq K := ⟨
   fun n => match n with
   | 0 => g.s.get? n |>.map fun c => c.a / c.b
   | _ => g.s.get? n |>.map fun c => - c.a * g.dens (n - 1) / g.dens (n + 1),
   fun {n} h => match n with
-  | 0 => by simp at h; simp [h]
+  | 0 => by simp_all
   | n + 1 => by simp at h; simpa using g.s.property h
 ⟩
+
 def toEuler (g : GenContFract K) : GenContFract K := Euler g.h <| toEulerAux g
 
 end GenContFract
