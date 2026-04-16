502b0f77a85c32d2b020f0e0d271d4a6c9f53b3d
Edward van de Meent <edwardvdmeent@gmail.com>
2026-04-16 20:45:55 +0200

    split prepartialmap file

--- Mathlib.lean
+++ Mathlib.lean
@@ -3128,6 +3128,7 @@ public import Mathlib.CategoryTheory.ObjectProperty.Small
 public import Mathlib.CategoryTheory.Opposites
 public import Mathlib.CategoryTheory.PEmpty
 public import Mathlib.CategoryTheory.PUnit
+public import Mathlib.CategoryTheory.PartialMap.PrePartialMap
 public import Mathlib.CategoryTheory.PathCategory.Basic
 public import Mathlib.CategoryTheory.PathCategory.MorphismProperty
 public import Mathlib.CategoryTheory.Pi.Basic
