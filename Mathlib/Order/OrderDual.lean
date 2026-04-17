99264d64e74ca816bb178c388b73d5127c75f09d
Michael Rothgang <rothgang@math.uni-bonn.de>
2026-04-17 09:38:45 +0200

    Fix

--- Mathlib/Order/OrderDual.lean
+++ Mathlib/Order/OrderDual.lean
@@ -97,6 +97,7 @@ instance (α : Type*) [LinearOrder α] : LinearOrder αᵒᵈ where
     rfl
 
 -- TODO: This declaration really shouldn't exist.
+set_option linter.style.setOption false in
 set_option backward.inferInstanceAs.wrap.reuseSubInstances false in  -- otherwise we get an identity!
 /-- The opposite linear order to a given linear order -/
 @[implicit_reducible, deprecated "This declaration shouldn't have existed" (since := "2026-04-08")]
