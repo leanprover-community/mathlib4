4405f38eddba218b05336eec669cd8a707292ae4
ksenono <89638921+ksenono@users.noreply.github.com>
2026-04-17 10:09:44 -0400

    Update Mathlib/Combinatorics/SimpleGraph/Matching.lean
    Co-authored-by: Snir Broshi <26556598+SnirBroshi@users.noreply.github.com>

--- Mathlib/Combinatorics/SimpleGraph/Matching.lean
+++ Mathlib/Combinatorics/SimpleGraph/Matching.lean
@@ -228,9 +228,8 @@ lemma IsMatching.of_connected_pair {M : Subgraph G} (h : ∃ v w, M.verts = {v, 
   obtain ⟨v, w, hverts, hadj⟩ := h
   intro a ha
   simp_all only [Set.mem_insert_iff, Set.mem_singleton_iff]
-  have he : ∃ b : V, M.Adj a b :=
-    Or.elim ha (fun hav ↦ ⟨w, hav ▸ hadj⟩) (fun haw ↦ ⟨v, haw ▸ hadj.symm⟩)
-  obtain ⟨b, hadj⟩ := he
+  obtain ⟨b, hadj⟩ : ∃ b : V, M.Adj a b :=
+    ha.elim (fun hav ↦ ⟨w, hav ▸ hadj⟩) (fun haw ↦ ⟨v, haw ▸ hadj.symm⟩)
   refine ⟨b, ⟨hadj, fun b' hadj' => ?_⟩⟩
   have hb : b ∈ {v, w} := hverts ▸ M.edge_vert hadj.symm
   have hb' : b' ∈ {v, w} := hverts ▸ M.edge_vert hadj'.symm
