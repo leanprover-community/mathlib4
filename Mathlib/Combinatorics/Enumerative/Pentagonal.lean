e02f6d1d2b41c2947116cc0400bc31a0d8769cfc
Weiyi Wang <wwylele@gmail.com>
2026-04-17 18:39:04 -0400

    apply comments

--- Mathlib/Combinatorics/Enumerative/Pentagonal.lean
+++ Mathlib/Combinatorics/Enumerative/Pentagonal.lean
@@ -5,11 +5,7 @@ Authors: Weiyi Wang
 -/
 module
 
-public import Mathlib.Algebra.Order.Ring.Int
-public import Mathlib.Order.Interval.Set.Defs
-import Mathlib.Data.Int.SuccPred
-import Mathlib.Tactic.Linarith.Frontend
-import Mathlib.Tactic.LinearCombination
+public import Mathlib.Data.Int.SuccPred
 
 /-!
 # Pentagonal number
@@ -44,9 +40,7 @@ theorem pentagonal_neg (k : ℤ) : pentagonal (-k) = (k * (3 * k + 1) / 2).toNat
   grind [pentagonal_def]
 
 theorem natCast_pentagonal (k : ℤ) : (pentagonal k : ℤ) = k * (3 * k - 1) / 2 := by
-  simp only [pentagonal_def, Int.ofNat_toNat, sup_eq_left, Nat.ofNat_pos,
-    Int.ediv_nonneg_iff_of_pos]
-  rcases lt_or_ge 0 k with h | h <;> nlinarith
+  rcases k with (_ | _) | _ <;> grind [pentagonal_def]
 
 theorem two_mul_natCast_pentagonal (k : ℤ) : 2 * (pentagonal k : ℤ) = k * (3 * k - 1) := by
   rw [natCast_pentagonal]
@@ -57,30 +51,24 @@ theorem two_mul_natCast_pentagonal_neg (k : ℤ) : 2 * (pentagonal (-k) : ℤ) =
 
 theorem pentagonal_injective : Function.Injective pentagonal := by
   intro x y h
-  replace h := congr(2 * ($h : ℤ))
-  simp_rw [two_mul_natCast_pentagonal] at h
-  replace h : (3 * (x + y) - 1) * (x - y) = 0 := by linear_combination h
-  rcases mul_eq_zero.mp h with h | h <;> grind
+  replace h : (3 * (x + y) - 1) * (x - y) = 0 := by grind [two_mul_natCast_pentagonal]
+  cases mul_eq_zero.mp h <;> grind
 
 @[simp]
 theorem pentagonal_inj {x y : ℤ} : pentagonal x = pentagonal y ↔ x = y :=
   pentagonal_injective.eq_iff
 
 theorem pentagonal_lt_pentagonal_neg {k : ℤ} (h : 0 < k) : pentagonal k < pentagonal (-k) := by
-  zify
   grind [natCast_pentagonal]
 
 theorem pentagonal_neg_lt_pentagonal_add_one {k : ℤ} (h : 0 ≤ k) :
     pentagonal (-k) < pentagonal (k + 1) := by
-  zify
   grind [natCast_pentagonal]
 
 theorem pentagonal_strictMonoOn : StrictMonoOn pentagonal (Set.Ici 0) := by
   apply strictMonoOn_of_lt_add_one Set.ordConnected_Ici
-  zify
   grind [natCast_pentagonal]
 
 theorem pentagonal_strictAntiOn : StrictAntiOn pentagonal (Set.Iic 0) := by
   apply strictAntiOn_of_add_one_lt Set.ordConnected_Iic
-  zify
   grind [natCast_pentagonal]
