f7cd8644411adc25f30aa594161f7dc3729c0384
tb65536 <thomas.l.browning@gmail.com>
2026-04-17 17:07:53 +0100

    fix

--- Mathlib/RingTheory/LocalRing/Length.lean
+++ Mathlib/RingTheory/LocalRing/Length.lean
@@ -9,15 +9,15 @@ public import Mathlib.RingTheory.Flat.FaithfullyFlat.Algebra
 public import Mathlib.RingTheory.Length
 
 /-!
-# Lengths along flat extensions of local rings
+# Lengths along extensions of local rings
 
-This file proves results relating lengths along flat extensions of local rings.
+This file proves results relating lengths along extensions of local rings.
 
 ## Main results
+- `IsLocalRing.length_restrictScalars`: If `B/A` is an extension of local rings, and if `M`
+  is a `B`-module, then `ℓ_A(M) = ℓ_B(M) * [κ(B) : κ(A)]`.
 - `IsLocalRing.length_baseChange`: If `B/A` is a flat extension of local rings, and if `M` is an
   `A`-module, then `ℓ_B(B ⊗[A] M) = ℓ_A(M) * ℓ_B(B ⧸ m_A)`.
-- `IsLocalRing.length_restrictScalars`: If `B/A` is a flat extension of local rings, and if `M`
-  is a `B`-module, then `ℓ_A(M) = ℓ_B(M) * [κ(B) : κ(A)]`.
 -/
 
 public section
@@ -26,6 +26,53 @@ open IsLocalRing LinearMap Module Submodule TensorProduct AlgebraTensorModule
 
 variable {A B M : Type*} [CommRing A] [CommRing B] [IsLocalRing A] [IsLocalRing B] [Algebra A B]
   [IsLocalHom (algebraMap A B)] [AddCommGroup M] [Module A M]
+
+section tower
+
+variable [Module B M] [IsScalarTower A B M]
+
+variable (A) in
+theorem CovBy.length_restrictScalars {p q : Submodule B M} (h : p ⋖ q) :
+    length A q = Module.length A p + Module.length (ResidueField A) (ResidueField B) := by
+  let f : p →ₗ[B] q := inclusion h.le
+  have key : IsSimpleModule B (q ⧸ f.range) := by
+    rwa [range_inclusion, ← covBy_iff_quot_is_simple h.le]
+  obtain ⟨m, hm, ⟨e⟩⟩ := isSimpleModule_iff_quot_maximal.mp key
+  rw [eq_maximalIdeal hm] at e
+  -- `0 -> p -> q -> κ(B) -> 0` is exact, and length is additive
+  let g : q →ₗ[B] ResidueField B := e.comp f.range.mkQ
+  have : Function.Injective f := inclusion_injective _
+  have : Function.Surjective g := e.surjective.comp f.range.mkQ_surjective
+  have : Function.Exact f g := exact_iff.mpr ((e.ker_comp f.range.mkQ).trans f.range.ker_mkQ)
+  rw [length_eq_add_of_exact (f.restrictScalars A) (g.restrictScalars A)
+    (by simpa) (by simpa) (by simpa), Module.length_eq_of_surjective (M := ResidueField B)
+      (residue_surjective (R := A)), Module.length_eq_rank]
+
+variable (A B M) in
+/-- If `B/A` is an extension of local rings, and if `M` is a `B`-module, then
+`ℓ_A(M) = ℓ_B(M) * [κ(B) : κ(A)]`. -/
+theorem IsLocalRing.length_restrictScalars :
+    length A M = length B M * Module.length (ResidueField A) (ResidueField B) := by
+  by_cases h : IsFiniteLength B M
+  · obtain ⟨s, hs_bot, hs_top⟩ := isFiniteLength_iff_exists_compositionSeries.mp h
+    rw [← length_compositionSeries s hs_bot hs_top]
+    suffices ∀ k, length A (s k) = k * Module.length (ResidueField A) (ResidueField B) by
+      rw [← Fin.val_last s.length, ← this, ← RelSeries.last, hs_top]
+      exact length_top.symm
+    intro k
+    induction k using Fin.induction with
+    | zero => rw [← RelSeries.head, hs_bot]; simp
+    | succ i hi => simpa [hi, add_one_mul] using (s.step i).length_restrictScalars A
+  · have : ¬ IsFiniteLength A M := by
+      contrapose! h
+      rw [isFiniteLength_iff_isNoetherian_isArtinian] at h ⊢
+      exact h.imp (isNoetherian_of_tower A) (isArtinian_of_tower A)
+    rw [← length_ne_top_iff, not_ne_iff] at h this
+    have ne : length (ResidueField A) (ResidueField B) ≠ 0 := by
+      simpa [pos_iff_ne_zero] using Module.length_pos
+    rw [h, this, ENat.top_mul ne]
+
+end tower
 
 section flat
 
@@ -53,6 +100,8 @@ theorem CovBy.length_baseChange {p q : Submodule A M} (h : p ⋖ q) :
     (Algebra.TensorProduct.quotIdealMapEquivTensorQuot B (maximalIdeal A)).toLinearEquiv.length_eq]
 
 variable (A B M) in
+/-- If `B/A` is a flat extension of local rings, and if `M` is an `A`-module, then
+`ℓ_B(B ⊗[A] M) = ℓ_A(M) * ℓ_B(B ⧸ m_A)`. -/
 theorem IsLocalRing.length_baseChange :
     length B (B ⊗[A] M) = length A M * length B (B ⧸ (maximalIdeal A).map (algebraMap A B)) := by
   by_cases h : IsFiniteLength A M
@@ -77,48 +126,3 @@ theorem IsLocalRing.length_baseChange :
     rw [h, this, ENat.top_mul ne]
 
 end flat
-
-section tower
-
-variable [Module B M] [IsScalarTower A B M]
-
-variable (A) in
-theorem CovBy.length_restrictScalars {p q : Submodule B M} (h : p ⋖ q) :
-    length A q = Module.length A p + Module.length (ResidueField A) (ResidueField B) := by
-  let f : p →ₗ[B] q := inclusion h.le
-  have key : IsSimpleModule B (q ⧸ f.range) := by
-    rwa [range_inclusion, ← covBy_iff_quot_is_simple h.le]
-  obtain ⟨m, hm, ⟨e⟩⟩ := isSimpleModule_iff_quot_maximal.mp key
-  rw [eq_maximalIdeal hm] at e
-  -- `0 -> p -> q -> κ(B) -> 0` is exact, and length is additive
-  let g : q →ₗ[B] ResidueField B := e.comp f.range.mkQ
-  have : Function.Injective f := inclusion_injective _
-  have : Function.Surjective g := e.surjective.comp f.range.mkQ_surjective
-  have : Function.Exact f g := exact_iff.mpr ((e.ker_comp f.range.mkQ).trans f.range.ker_mkQ)
-  rw [length_eq_add_of_exact (f.restrictScalars A) (g.restrictScalars A)
-    (by simpa) (by simpa) (by simpa), Module.length_eq_of_surjective (M := ResidueField B)
-      (residue_surjective (R := A)), Module.length_eq_rank]
-
-variable (A B M) in
-theorem IsLocalRing.length_restrictScalars :
-    length A M = length B M * Module.length (ResidueField A) (ResidueField B) := by
-  by_cases h : IsFiniteLength B M
-  · obtain ⟨s, hs_bot, hs_top⟩ := isFiniteLength_iff_exists_compositionSeries.mp h
-    rw [← length_compositionSeries s hs_bot hs_top]
-    suffices ∀ k, length A (s k) = k * Module.length (ResidueField A) (ResidueField B) by
-      rw [← Fin.val_last s.length, ← this, ← RelSeries.last, hs_top]
-      exact length_top.symm
-    intro k
-    induction k using Fin.induction with
-    | zero => rw [← RelSeries.head, hs_bot]; simp
-    | succ i hi => simpa [hi, add_one_mul] using (s.step i).length_restrictScalars A
-  · have : ¬ IsFiniteLength A M := by
-      contrapose! h
-      rw [isFiniteLength_iff_isNoetherian_isArtinian] at h ⊢
-      exact h.imp (isNoetherian_of_tower A) (isArtinian_of_tower A)
-    rw [← length_ne_top_iff, not_ne_iff] at h this
-    have ne : length (ResidueField A) (ResidueField B) ≠ 0 := by
-      simpa [pos_iff_ne_zero] using Module.length_pos
-    rw [h, this, ENat.top_mul ne]
-
-end tower
