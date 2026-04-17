e8b89ebdf910fc5193e242cc2ba74be907970fe7
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2026-04-17 18:36:47 +0800

    add simps attr

--- Mathlib/Algebra/Homology/ShortComplex/ModuleCat.lean
+++ Mathlib/Algebra/Homology/ShortComplex/ModuleCat.lean
@@ -244,6 +244,7 @@ variable {M' N' L' : Type*} [AddCommGroup M'] [AddCommGroup N'] [AddCommGroup L'
 variable (eM : M ≃ₗ[R] M') (eN : N ≃ₗ[R] N') (eL : L ≃ₗ[R] L') (f : M' →ₗ[R] N') (g : N' →ₗ[R] L')
 
 /-- The short complex in `ModuleCat` obtained from conjugate by linear equivalences. -/
+@[simps! X₁ X₂ X₃ f g]
 def ModuleCat.shortComplexOfConj (eq0 : g ∘ₗ f = 0) :
     ShortComplex (ModuleCat.{v} R) :=
   ModuleCat.shortComplexOfCompEqZero ((eN.symm.comp f).comp eM.toLinearMap)
