5faa5b366cb0828a439bfa9304953ff928177196
David Gross <david.gross@thp.uni-koeln.de>
2026-04-17 10:56:44 +0200

    deprecate Algebra.Field.Power

--- Mathlib/Algebra/Field/Power.lean
+++ Mathlib/Algebra/Field/Power.lean
@@ -3,39 +3,8 @@ Copyright (c) 2014 Robert Y. Lewis. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Robert Y. Lewis, Leonardo de Moura, Johannes Hölzl, Mario Carneiro
 -/
-module
+module -- shake: keep-all
 
-public import Mathlib.Algebra.Ring.Int.Defs
-public import Mathlib.Algebra.Ring.Parity
+public import Mathlib.Algebra.Ring.Int.Parity
 
-/-!
-# Results about powers in fields or division rings.
-
-This file exists to ensure we can define `Field` with minimal imports,
-so it contains some lemmas about powers of elements which need imports
-beyond those needed for the basic definition.
--/
-
-public section
-
-variable {α : Type*}
-
-section DivisionMonoid
-
-variable [DivisionMonoid α] [HasDistribNeg α] {n : ℤ}
-
-theorem Odd.neg_zpow (h : Odd n) (a : α) : (-a) ^ n = -a ^ n := by
-  obtain ⟨k, rfl⟩ := h
-  cases k with
-  | ofNat k =>
-    rw [Int.ofNat_eq_natCast]
-    norm_cast
-    simp [pow_add]
-  | negSucc k =>
-    simp_rw [Int.negSucc_eq, show 2 * -(↑k + 1) + (1 : ℤ) = - (1 + k*2) by grind, _root_.zpow_neg]
-    norm_cast
-    simp [pow_add]
-
-theorem Odd.neg_one_zpow (h : Odd n) : (-1 : α) ^ n = -1 := by rw [h.neg_zpow, one_zpow]
-
-end DivisionMonoid
+deprecated_module (since := "2026-04-17")
