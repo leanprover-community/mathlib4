67f2f1cdddf152aa28448a2f93d5c8bc24bd4431
artie2000 <artem.khovanov@gmail.com>
2026-04-18 00:27:31 +0100

    fully deprecate

--- Mathlib/LinearAlgebra/FreeModule/StrongRankCondition.lean
+++ Mathlib/LinearAlgebra/FreeModule/StrongRankCondition.lean
@@ -3,47 +3,15 @@ Copyright (c) 2021 Riccardo Brasca. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Riccardo Brasca
 -/
-module
+module -- shake: keep-all
 
 public import Mathlib.RingTheory.FiniteType
 public import Mathlib.LinearAlgebra.InvariantBasisNumber
-
-/-!
-
-# Strong rank condition for commutative rings
-
-We prove that any nontrivial commutative ring satisfies `StrongRankCondition`, meaning that
-if there is an injective linear map `(Fin n → R) →ₗ[R] Fin m → R`, then `n ≤ m`. This implies that
-any commutative ring satisfies `InvariantBasisNumber`: the rank of a finitely generated free
-module is well defined.
-
-## Main result
-
-* `commRing_strongRankCondition R` : `R` has the `StrongRankCondition`.
-
-The `commRing_strongRankCondition` comes from `CommRing.orzechProperty`, proved in
-`Mathlib/RingTheory/FiniteType.lean`, which states that any commutative ring satisfies
-the `OrzechProperty`, that is, for any finitely generated
-`R`-module `M`, any surjective homomorphism `f : N → M` from a submodule `N` of `M` to `M`
-is injective.
-
-
-## References
-
-* [Orzech, Morris. *Onto endomorphisms are isomorphisms*][orzech1971]
-* [Djoković, D. Ž. *Epimorphisms of modules which must be isomorphisms*][djokovic1973]
-* [Ribenboim, Paulo. *Épimorphismes de modules qui sont nécessairement
-  des isomorphismes*][ribenboim1971]
-
--/
 
 deprecated_module (since := "2026-04-18")
 
 @[expose] public section
 
-
-variable (R : Type*) [CommRing R] [Nontrivial R]
-
-/-- Any nontrivial commutative ring satisfies the `StrongRankCondition`. -/
-instance (priority := 100) commRing_strongRankCondition : StrongRankCondition R :=
-  inferInstance
+@[deprecated "instance already exists" (since := "2026-04-18")]
+instance (priority := 100) commRing_strongRankCondition (R : Type*) [CommRing R] [Nontrivial R] :
+    StrongRankCondition R := inferInstance
