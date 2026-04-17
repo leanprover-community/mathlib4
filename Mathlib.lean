ff517753c5d08bb3b12ddb45b002beff652c0117
Joël Riou <37772949+joelriou@users.noreply.github.com>
2026-04-17 20:06:02 +0000

    chore(AlgebraicTopology): rename `SingularHomology.HomotopyInvarianceTopCat` (#37658)
    Following #37656, it is possible to rename `SingularHomology.HomotopyInvarianceTopCat` as `SingularHomology.HomotopyInvariance`.

--- Mathlib.lean
+++ Mathlib.lean
@@ -1563,7 +1563,7 @@ public import Mathlib.AlgebraicTopology.SimplicialSet.SubcomplexColimits
 public import Mathlib.AlgebraicTopology.SimplicialSet.SubcomplexEvaluation
 public import Mathlib.AlgebraicTopology.SimplicialSet.TopAdj
 public import Mathlib.AlgebraicTopology.SingularHomology.Basic
-public import Mathlib.AlgebraicTopology.SingularHomology.HomotopyInvarianceTopCat
+public import Mathlib.AlgebraicTopology.SingularHomology.HomotopyInvariance
 public import Mathlib.AlgebraicTopology.SingularSet
 public import Mathlib.AlgebraicTopology.TopologicalSimplex
 public import Mathlib.Analysis.AbsoluteValue.Equivalence
