ba9d4d9aeb8af7afe00231d02b8e5570ba30d945
Weiyi Wang <wwylele@gmail.com>
2026-04-17 14:30:18 -0400

    staging

--- Mathlib/Combinatorics/Enumerative/Pentagonal.lean
+++ Mathlib/Combinatorics/Enumerative/Pentagonal.lean
@@ -12,7 +12,27 @@ import Mathlib.Tactic.LinearCombination
 import Mathlib.Data.Int.SuccPred
 
 /-!
+# Pentagonal number
 
+This file introduces (generalized) pentagonal numbers $3k(k-1)/2$ for integer $k$.
+
+Some source, such as A001318 in the OEIS, orders generalized pentagonal numbers by indices
+$k = 0, 1, -1, 2, -2, \cdots$ to form a strictly monotone sequence. This file doesn't follow this
+convention, but implicitly shows the monotonicity by `pentagonal_lt_pentagonal_neg` and
+`pentagonal_neg_lt_pentagonal_add_one`.
+
+## Main definitions
+* `pentagonal`: pentagonal numbers as a function `ℤ → ℤ`.
+* `pentagonalNat`: pentagonal numbers as a function `ℤ → ℕ`, bundling in the fact that pentagonal
+  numbers are all non-negative.
+
+## TODO
+
+Show the relation between pentagonal numbers and partitions, including pentagonal number theorem.
+
+## References
+
+* https://en.wikipedia.org/wiki/Pentagonal_number
 -/
 
 public section
@@ -31,9 +51,7 @@ theorem two_pentagonal_neg (k : ℤ) : 2 * pentagonal (-k) = k * (3 * k + 1) := 
 
 theorem pentagonal_nonneg (k : ℤ) : 0 ≤ pentagonal k := by
   simp only [pentagonal, Nat.ofNat_pos, Int.ediv_nonneg_iff_of_pos]
-  obtain h | h := lt_or_ge 0 k
-  · exact mul_nonneg h.le (by linarith)
-  · exact mul_nonneg_of_nonpos_of_nonpos h (by linarith)
+  rcases lt_or_ge 0 k with h | h <;> nlinarith
 
 theorem pentagonal_injective : Function.Injective pentagonal := by
   intro x y h
@@ -44,7 +62,7 @@ theorem pentagonal_injective : Function.Injective pentagonal := by
 
 @[simp]
 theorem pentagonal_inj {x y : ℤ} : pentagonal x = pentagonal y ↔ x = y :=
-  ⟨fun h ↦ pentagonal_injective h, fun h ↦ congrArg _ h⟩
+  pentagonal_injective.eq_iff
 
 theorem pentagonal_lt_pentagonal_neg {k : ℤ} (h : 0 < k) : pentagonal k < pentagonal (-k) := by
   grind [pentagonal]
@@ -54,11 +72,11 @@ theorem pentagonal_neg_lt_pentagonal_add_one {k : ℤ} (h : 0 ≤ k) :
   grind [pentagonal]
 
 theorem pentagonal_strictMonoOn : StrictMonoOn pentagonal (Set.Ici 0) := by
-  refine strictMonoOn_of_lt_add_one Set.ordConnected_Ici fun k hm h h' ↦ ?_
+  apply strictMonoOn_of_lt_add_one Set.ordConnected_Ici
   grind [pentagonal]
 
 theorem pentagonal_strictAntiOn : StrictAntiOn pentagonal (Set.Iic 0) := by
-  refine strictAntiOn_of_add_one_lt Set.ordConnected_Iic fun k hm h h' ↦ ?_
+  apply strictAntiOn_of_add_one_lt Set.ordConnected_Iic
   grind [pentagonal]
 
 @[expose]
@@ -75,4 +93,4 @@ theorem pentagonalNat_injective : Function.Injective pentagonalNat := by
 
 @[simp]
 theorem pentagonalNat_inj {x y : ℤ} : pentagonalNat x = pentagonalNat y ↔ x = y :=
-  ⟨fun h ↦ pentagonalNat_injective h, fun h ↦ congrArg _ h⟩
+  pentagonalNat_injective.eq_iff
