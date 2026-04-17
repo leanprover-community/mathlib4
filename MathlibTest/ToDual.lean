e479388d393de50704b991888f076be362d259ef
Kim Morrison <kim@tqft.net>
2026-04-18 08:20:28 +1000

    fixes

--- MathlibTest/ToDual.lean
+++ MathlibTest/ToDual.lean
@@ -31,6 +31,7 @@ structure Lattice (α : Type) extends SemilatticeInf α, SemilatticeSup α
 
 attribute [to_dual existing] Lattice.toSemilatticeInf
 
+set_option linter.impossibleInstance false in
 -- we can reorder arguments of arguments in `SemilatticeInf.mk`
 @[to_dual]
 instance [Min α] (le_inf : ∀ a b c : α, a ≤ b → a ≤ c → a ≤ b ⊓ c) : SemilatticeInf α where
