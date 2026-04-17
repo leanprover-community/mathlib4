21dc716cbe8800f11b3351d92b45e0c2d32bd524
Samuel Oettl <86880972+samueloettl@users.noreply.github.com>
2026-04-17 11:24:53 +0200

    fixed naming (sugestion of lua-vr)

--- Mathlib/Dynamics/BirkhoffSum/Average.lean
+++ Mathlib/Dynamics/BirkhoffSum/Average.lean
@@ -77,7 +77,7 @@ theorem Function.IsFixedPt.birkhoffAverage_eq {f : α → α} {x : α} (h : IsFi
     (g : α → M) {n : ℕ} (hn : (n : R) ≠ 0) : birkhoffAverage R f g n x = g x := by
   rw [birkhoffAverage, h.birkhoffSum_eq, ← Nat.cast_smul_eq_nsmul R, inv_smul_smul₀ hn]
 
-theorem birkhoffAverage_const_eq₀ (f : α → α) (a : M) {n : ℕ} (hn : (n : R) ≠ 0) :
+theorem birkhoffAverage_const_eq' (f : α → α) (a : M) {n : ℕ} (hn : (n : R) ≠ 0) :
     birkhoffAverage R f (fun _ ↦ a) n = fun _ ↦ a := by
   ext x
   simp [hn, birkhoffAverage, birkhoffSum, ← Nat.cast_smul_eq_nsmul R]
@@ -89,7 +89,7 @@ theorem birkhoffAverage_const_eq (f : α → α) (a : M) (n : ℕ) :
   by_cases h : (n : R) = 0
   · ext
     simp [h, birkhoffAverage]
-  simp [h, birkhoffAverage_const_eq₀]
+  simp [h, birkhoffAverage_const_eq']
 
 lemma birkhoffAverage_add {f : α → α} {g g' : α → M} :
     birkhoffAverage R f (g + g') = birkhoffAverage R f g + birkhoffAverage R f g' := by
