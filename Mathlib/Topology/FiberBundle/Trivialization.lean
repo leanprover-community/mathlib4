94e0819e1b60a6e9470b79a19d5154b3616edc09
Jack McCarthy <37917934+Deicyde@users.noreply.github.com>
2026-04-18 14:06:14 -0400

    Changed to use OpenPartialHomeomorph.continuousAt_symm

--- Mathlib/Topology/FiberBundle/Trivialization.lean
+++ Mathlib/Topology/FiberBundle/Trivialization.lean
@@ -592,14 +592,10 @@ theorem continuousAt_proj (ex : x ∈ e.source) : ContinuousAt proj x :=
 theorem continuousOn_proj : ContinuousOn proj e.source :=
   continuousOn_of_forall_continuousAt fun _ ↦ e.continuousAt_proj
 
-/-- The inverse of a trivialization is continuous at any point in the target. -/
-theorem continuousAt_symm {p : B × F} (hp : p ∈ e.target) : ContinuousAt e.symm p :=
-  e.continuousOn_symm.continuousAt (e.open_target.mem_nhds hp)
-
 /-- For fixed `v ∈ F`, `x ↦ e.symm (x,v)` is continuous at any point in the base set. -/
 theorem continuousAt_symm_prodMk_left {b : B} {v : F} (hb : b ∈ e.baseSet) :
     ContinuousAt (fun x ↦ e.symm (x, v)) b :=
-  (e.continuousAt_symm (e.mem_target.mpr hb)).comp (by fun_prop)
+  (e.toOpenPartialHomeomorph.continuousAt_symm (e.mem_target.mpr hb)).comp (by fun_prop)
 
 /-- For fixed `v ∈ F`, `x ↦ e.symm (x,v)` is continuous on `e.baseSet`. -/
 theorem continuousOn_symm_prodMk_left {v : F} : ContinuousOn (fun x ↦ e.symm (x, v)) e.baseSet :=
