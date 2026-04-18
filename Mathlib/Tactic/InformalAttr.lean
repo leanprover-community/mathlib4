941ab76563ef5b38908bc7d80885df95adaaeac0
thorimur <68410468+thorimur@users.noreply.github.com>
2026-04-18 01:20:21 -0400

    feat: informal imported consts

---
+++ Mathlib/Tactic/InformalAttr.lean
@@ -0,0 +1,18 @@
+module
+
+public import Mathlib.Tactic.DatabaseAttributes
+public import Batteries
+public import Lean
+public import Std
+
+-- Generated with `#imported_informal?`
+attribute [informal "array"] Array
+attribute [informal "integer"] Int
+attribute [informal "list"] List
+attribute [informal "lazy list"] MLList
+attribute [informal "natural number"] Nat
+attribute [informal "rational number"] Rat
+attribute [informal "difference list"] Batteries.DList
+attribute [informal "red-black map"] Batteries.RBMap
+attribute [informal "red-black tree"] Batteries.RBSet
+attribute [informal "hash map"] Std.HashMap
