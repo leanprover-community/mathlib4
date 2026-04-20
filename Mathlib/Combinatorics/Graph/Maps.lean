fa5773e8d061831d20bdaaf4d4914608753a6f73
Jun2M <rnjsgurwns27@gmail.com>
2026-04-20 13:09:59 -0400

    Relation.map

--- Mathlib/Combinatorics/Graph/Maps.lean
+++ Mathlib/Combinatorics/Graph/Maps.lean
@@ -20,10 +20,9 @@ This file develops the basic theory of vertex maps on graphs `Graph α β`.
 
 public section
 
-variable {α α' α'' β : Type*} {G H : Graph α β} {G' H' : Graph α' β} {f g : α → α'}
-  {f' g' : α' → α''} {u v w : α} {e : β} {x y : α'} {X Y : Set α}
+variable {α α' α'' β : Type*} {G H : Graph α β} {f g : α → α'} {u v : α} {e : β} {x y : α'}
 
-open Set
+open Set Relation
 
 namespace Graph
 
@@ -31,23 +30,19 @@ section Map
 
 /-- Map `G : Graph α β` to a `Graph α' β` with the same edge set by applying a function `f : α → α'`
   to each vertex. Edges between identified vertices become loops. -/
-@[expose, simps (attr := grind =)]
+@[expose, simps! (attr := grind =)]
 def map (f : α → α') (G : Graph α β) : Graph α' β where
   vertexSet := f '' V(G)
   edgeSet := E(G)
-  IsLink e x' y' := ∃ x y, G.IsLink e x y ∧ x' = f x ∧ y' = f y
-  isLink_symm := by
-    rintro e he - - ⟨x, y, h, rfl, rfl⟩
-    exact ⟨y, x, h.symm, rfl, rfl⟩
+  IsLink e := Relation.Map (G.IsLink e) f f
+  isLink_symm _ he := map_symmetric (G.isLink_symm he) f
   eq_or_eq_of_isLink_of_isLink := by
     rintro e - - - - ⟨x, y, hxy, rfl, rfl⟩ ⟨z, w, hzw, rfl, rfl⟩
     obtain rfl | rfl := hxy.left_eq_or_eq hzw <;> simp
   edge_mem_iff_exists_isLink e := by
-    refine ⟨fun h ↦ ?_, ?_⟩
-    · obtain ⟨x, y, hxy⟩ := exists_isLink_of_mem_edgeSet h
-      exact ⟨_, _, _, _, hxy, rfl, rfl⟩
-    rintro ⟨-, -, x, y, h, rfl, rfl⟩
-    exact h.edge_mem
+    refine ⟨fun h ↦ ?_, fun ⟨_, _, _, _, h, _, _⟩ ↦ h.edge_mem⟩
+    obtain ⟨x, y, hxy⟩ := exists_isLink_of_mem_edgeSet h
+    exact ⟨_, _, _, _, hxy, rfl, rfl⟩
   left_mem_of_isLink := by
     rintro e - - ⟨x, y, h, rfl, rfl⟩
     exact Set.mem_image_of_mem _ h.left_mem
@@ -57,7 +52,7 @@ protected lemma IsLink.map (f : α → α') (h : G.IsLink e u v) : (G.map f).IsL
 
 @[simp]
 lemma map_inc (f : α → α') : (G.map f).Inc e x ↔ ∃ v, G.Inc e v ∧ x = f v := by
-  simp only [Inc, map_isLink]
+  simp only [Inc, map_isLink, map_apply]
   tauto
 
 protected lemma Inc.map (f : α → α') (h : G.Inc e v) : (G.map f).Inc e (f v) := by
@@ -66,14 +61,14 @@ protected lemma Inc.map (f : α → α') (h : G.Inc e v) : (G.map f).Inc e (f v)
 
 @[simp]
 lemma map_isLoopAt (f : α → α') :
-    (G.map f).IsLoopAt e x ↔ ∃ u v, G.IsLink e u v ∧ x = f u ∧ x = f v := Iff.rfl
+    (G.map f).IsLoopAt e x ↔ ∃ u v, G.IsLink e u v ∧ f u = x ∧ f v = x := Iff.rfl
 
 protected lemma IsLoopAt.map (f : α → α') (h : G.IsLoopAt e v) : (G.map f).IsLoopAt e (f v) :=
   IsLink.map f h
 
 @[simp]
-lemma map_adj (f : α → α') : (G.map f).Adj x y ↔ ∃ u v, G.Adj u v ∧ x = f u ∧ y = f v := by
-  simp only [Adj, map_isLink]
+lemma map_adj (f : α → α') : (G.map f).Adj x y ↔ Relation.Map G.Adj f f x y := by
+  simp only [Adj, map_isLink, map_apply]
   tauto
 
 protected lemma Adj.map (f : α → α') (h : G.Adj u v) : (G.map f).Adj (f u) (f v) := by
@@ -84,18 +79,13 @@ protected lemma Adj.map (f : α → α') (h : G.Adj u v) : (G.map f).Adj (f u) (
 
 @[simp]
 lemma map_map (f : α → α') (f' : α' → α'') : (G.map f).map f' = G.map (f' ∘ f) := by
-  ext a b c <;> simp
+  ext a b c <;> simp [map_apply]
 
 @[gcongr]
 protected lemma IsSubgraph.map (f : α → α') (h : G ≤ H) : G.map f ≤ H.map f where
-  vertexSet_mono v := by
-    simp only [vertexSet_map, mem_image, forall_exists_index, and_imp]
-    rintro u hu rfl
-    use u, h.vertexSet_mono hu
-  isLink_mono e x y := by
-    simp only [map_isLink, forall_exists_index, and_imp]
-    rintro a b hab rfl rfl
-    use a, b, h.isLink_mono hab
+  vertexSet_mono v := by grind [h.vertexSet_mono]
+  isLink_mono e := map_mono <| h.isLink_mono (e := e)
+alias map_mono := IsSubgraph.map
 
 @[gcongr]
 protected lemma IsSpanningSubgraph.map (f : α → α') (hsle : G ≤s H) : G.map f ≤s H.map f where
@@ -104,10 +94,8 @@ protected lemma IsSpanningSubgraph.map (f : α → α') (hsle : G ≤s H) : G.ma
 
 @[gcongr]
 lemma map_eq_of_eqOn (h : EqOn f g V(G)) : G.map f = G.map g := by
-  refine Graph.ext (by grind) fun e x y ↦ ⟨fun ⟨v, w, hvw, _, _⟩ ↦ ?_, fun ⟨v, w, hvw, _, _⟩ ↦ ?_⟩
-  <;> subst x y
-  · use v, w, hvw, h hvw.left_mem, h hvw.right_mem
-  · use v, w, hvw, (h hvw.left_mem).symm, (h hvw.right_mem).symm
+  refine Graph.ext (by grind) fun _ _ _ ↦ ⟨fun ⟨_, _, hvw, _, _⟩ ↦ ?_, fun ⟨_, _, hvw, _, _⟩ ↦ ?_⟩
+  <;> grind [h hvw.left_mem, h hvw.right_mem, hvw.map]
 
 end Map
 
