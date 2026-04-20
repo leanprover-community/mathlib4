5585a5303c6b67fa9f129a9f5375f0bab2d05b20
Gian Sanjaya <43656481+mortarsanjaya@users.noreply.github.com>
2026-04-20 17:44:48 -0400

    Update based on comments

--- Mathlib/Algebra/Order/Ring/Unbundled/Basic.lean
+++ Mathlib/Algebra/Order/Ring/Unbundled/Basic.lean
@@ -711,8 +711,9 @@ alias pow_two_nonneg := sq_nonneg
 lemma mul_self_nonneg [ExistsAddOfLE R] [PosMulMono R] [AddLeftMono R]
     (a : R) : 0 ≤ a * a := by simpa only [sq] using sq_nonneg a
 
-instance (priority := 100) [ExistsAddOfLE R] [PosMulMono R] [AddLeftMono R] : ZeroLEOneClass R :=
-  ⟨by simpa only [one_mul] using mul_self_nonneg (R := R) 1⟩
+instance (priority := 100) [ExistsAddOfLE R] [PosMulMono R] [AddLeftMono R] :
+    ZeroLEOneClass R where
+  zero_le_one := by simpa only [one_mul] using mul_self_nonneg (R := R) 1
 
 /-- The sum of two squares is zero iff both elements are zero. -/
 lemma mul_self_add_mul_self_eq_zero [NoZeroDivisors R]
@@ -789,8 +790,6 @@ lemma two_mul_le_add_of_sq_le_mul [ExistsAddOfLE R] [MulPosStrictMono R] [PosMul
   rw [mul_mul_mul_comm, ← pow_two r, two_mul, two_add_two_eq_four]
   grw [mul_le_mul_of_nonneg_left ht zero_le_four, ← mul_assoc, four_mul_le_sq_add a b, sq]
 
-/-- Binary and division-free **arithmetic mean-geometric mean inequality**
-(aka AM-GM inequality) for linearly ordered commutative semirings. -/
 @[deprecated two_mul_le_add_of_sq_le_mul (since := "2026-04-20")]
 lemma two_mul_le_add_of_sq_eq_mul [ExistsAddOfLE R] [MulPosStrictMono R] [PosMulStrictMono R]
     [AddLeftReflectLE R] [AddLeftMono R] {a b r : R}
