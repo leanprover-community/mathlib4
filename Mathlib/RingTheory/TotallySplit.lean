b41f28b7615d73d2ff8cf49e2bf22fd0701cd90e
Christian Merten <christian@merten.dev>
2026-04-21 16:23:57 +0200

    review suggestions

--- Mathlib/RingTheory/TotallySplit.lean
+++ Mathlib/RingTheory/TotallySplit.lean
@@ -6,15 +6,9 @@ Authors: Christian Merten
 module
 
 public import Mathlib.RingTheory.Etale.Pi
-public import Mathlib.RingTheory.Smooth.StandardSmoothCotangent
-public import Mathlib.RingTheory.Smooth.Flat
-public import Mathlib.RingTheory.Spectrum.Prime.FreeLocus
-public import Mathlib.RingTheory.TensorProduct.Pi
-public import Mathlib.RingTheory.Flat.FaithfullyFlat.Algebra
-public import Mathlib.RingTheory.Ideal.IdempotentFG
-public import Mathlib.RingTheory.Unramified.Basic
-public import Mathlib.RingTheory.Idempotents
 public import Mathlib.RingTheory.Flat.Rank
+public import Mathlib.RingTheory.Smooth.Flat
+public import Mathlib.RingTheory.TensorProduct.Pi
 
 /-!
 # Totally split algebras
@@ -89,18 +83,21 @@ instance [IsFiniteSplit R S] : Etale R S := by
 
 variable {n : ℕ} {R S : Type u} [CommRing R] [CommRing S] [Algebra R S]
 
-/-- If `S` is finite étale over `R` of (constant) rank `n`, there exists
+/--
+If `S` is finite étale over `R` of (constant) rank `n`, there exists
 a finite faithfully flat, étale `R`-algebra `T` such that `T ⊗[R] S` is split of rank `n`
-over `T`. -/
+over `T`.
+This is the commutative algebra version of
+[Lenstra, Galois theory for schemes, 5.10][lenstraGSchemes].
+-/
 lemma exists_tensorProduct_of_etale [Etale R S] [Module.Finite R S] {n : ℕ}
     (hn : Module.rankAtStalk (R := R) S = n) :
     ∃ (T : Type u) (_ : CommRing T) (_ : Algebra R T)
-      (_ : Module.FaithfullyFlat R T)
-      (_ : Module.Finite R T) (_ : Algebra.Etale R T),
+      (_ : Module.FaithfullyFlat R T) (_ : Module.Finite R T) (_ : Algebra.Etale R T),
       IsFiniteSplit T (T ⊗[R] S) := by
   induction n generalizing R S with
   | zero =>
-    use R, inferInstance, inferInstance, inferInstance, inferInstance
+    use R, inferInstance, inferInstance, inferInstance, inferInstance, inferInstance
     let e : R ⊗[R] S ≃ₐ[R] S := TensorProduct.lid R S
     have : IsFiniteSplit R S := by
       rw [Nat.cast_zero, Module.rankAtStalk_eq_zero_iff_subsingleton] at hn
@@ -108,12 +105,14 @@ lemma exists_tensorProduct_of_etale [Etale R S] [Module.Finite R S] {n : ℕ}
     apply IsFiniteSplit.of_algEquiv e.symm
   | succ n ih =>
     cases subsingleton_or_nontrivial R
-    · use R, inferInstance, inferInstance, inferInstance, inferInstance
+    · use R, inferInstance, inferInstance, inferInstance, inferInstance, inferInstance
       have : IsFiniteSplit R S := .of_subsingleton
       exact .of_algEquiv (TensorProduct.lid R S).symm
     have : Nontrivial S := by
       apply Module.nontrivial_of_rankAtStalk_pos (R := R)
       simp [hn]
+    /- Because `S` is unramified over `R`, there exists an `S`-algebra `U` such that
+    `S ⊗[R] S ≃ₐ[S] S × U`. -/
     obtain ⟨U, _, _, ⟨e⟩⟩ := Algebra.FormallyUnramified.exists_algEquiv_prod R S
     algebraize [RingHom.snd S U]
     have : IsScalarTower S (S × U) U := IsScalarTower.of_algebraMap_eq' rfl
@@ -128,25 +127,32 @@ lemma exists_tensorProduct_of_etale [Etale R S] [Module.Finite R S] {n : ℕ}
     have (p : PrimeSpectrum S) : Module.rankAtStalk (R := S) (S × U) p = n + 1 := by
       simp [Module.rankAtStalk_eq_of_equiv e.symm.toLinearEquiv, Module.rankAtStalk_baseChange, hn]
     simp_rw [Module.rankAtStalk_prod , Module.rankAtStalk_self, Pi.add_apply, Pi.one_apply] at this
+    /- Since the `S`-rank of `S × U = S ⊗[R] S` is `n + 1`, the `S`-rank of `U` is `n`,
+    so we may apply the induction hypothesis on `U`. -/
     have : Module.rankAtStalk (R := S) U = n := by
       ext p
       simp only [Pi.natCast_def, Nat.cast_id]
       grind
-    obtain ⟨V, _, _, _, _, hV⟩ := ih this
+    /- We obtain a finite étale, faithfully flat `S`-algebra `V` such that `V ⊗[S] U` is finite
+    split. We claim that `V` viewed as an `R`-algebra works. -/
+    obtain ⟨V, _, _, _, _, _, hV⟩ := ih this
     obtain ⟨n, ⟨f⟩⟩ := hV.nonempty_algEquiv_fun
     algebraize [(algebraMap S V).comp (algebraMap R S)]
-    let e := (Algebra.TensorProduct.cancelBaseChange R S V V S).symm.trans <|
-      (TensorProduct.congr AlgEquiv.refl e).trans <| (TensorProduct.prodRight S V V S U).trans <|
+    let e : V ⊗[R] S ≃ₐ[V] Unit ⊕ Fin n → V :=
+      (Algebra.TensorProduct.cancelBaseChange R S V V S).symm.trans <|
+        (TensorProduct.congr AlgEquiv.refl e).trans <|
+        (TensorProduct.prodRight S V V S U).trans <|
         (AlgEquiv.prodCongr (TensorProduct.rid S V V) f).trans <|
-      (AlgEquiv.prodCongr (AlgEquiv.funUnique _ _ _).symm AlgEquiv.refl).trans
+        (AlgEquiv.prodCongr (AlgEquiv.funUnique _ _ _).symm AlgEquiv.refl).trans
         (AlgEquiv.sumArrowEquivProdArrow Unit (Fin n) V V).symm
-    refine ⟨V, inferInstance, inferInstance, ?_, ?_, ?_⟩
+    refine ⟨V, inferInstance, inferInstance, ?_, ?_, ?_, ?_⟩
     · have : Module.FaithfullyFlat R S := by
         apply Module.FaithfullyFlat.of_comap_surjective
         rw [← PrimeSpectrum.rankAtStalk_pos_iff_comap_surjective]
         intro p
         simp [hn]
       exact Module.FaithfullyFlat.trans R S V
+    · exact Module.Finite.trans S V
     · exact Algebra.Etale.comp R S V
     · exact .of_algEquiv e.symm
 
--- Mathlib/RingTheory/Unramified/Basic.lean
+++ Mathlib/RingTheory/Unramified/Basic.lean
@@ -338,14 +338,14 @@ In particular, the diagonal is an open and closed immersion. -/
 lemma exists_algEquiv_prod (R S : Type u) [CommRing R] [CommRing S]
     [Algebra R S] [Algebra.EssFiniteType R S] [Algebra.FormallyUnramified R S] :
     ∃ (T : Type u) (_ : CommRing T) (_ : Algebra S T), Nonempty (S ⊗[R] S ≃ₐ[S] S × T) := by
-obtain ⟨e, he, hsp⟩ : ∃ e, IsIdempotentElem e ∧ KaehlerDifferential.ideal R S = S ⊗[R] S ∙ e :=
+  obtain ⟨e, he, hsp⟩ : ∃ e, IsIdempotentElem e ∧ KaehlerDifferential.ideal R S = S ⊗[R] S ∙ e :=
     (Ideal.isIdempotentElem_iff_of_fg _ (KaehlerDifferential.ideal_fg R S)).mp <|
       (Ideal.cotangent_subsingleton_iff _).mp <| inferInstanceAs <| Subsingleton Ω[S⁄R]
   let e₁ := AlgEquiv.prodQuotientOfIsIdempotentElem (R := S) he he.one_sub (by simp) (by simp [he])
   let e₂ : (S ⊗[R] S ⧸ Ideal.span {e}) ≃ₐ[S] S :=
-    ((Ideal.span {e}).quotientEquivAlgOfEq S hsp.symm).trans
-      (Ideal.quotientKerAlgEquivOfSurjective <|
-        (⟨· ⊗ₜ 1, by simp [Algebra.TensorProduct.lmul'']⟩))
+    ((Ideal.span {e}).quotientEquivAlgOfEq S hsp.symm).trans <|
+      Ideal.quotientKerAlgEquivOfSurjective <|
+        (⟨· ⊗ₜ 1, by simp [Algebra.TensorProduct.lmul'']⟩)
   exact ⟨(S ⊗[R] S) ⧸ Ideal.span {1 - e}, inferInstance, inferInstance,
     ⟨e₁.trans (.prodCongr e₂ .refl)⟩⟩
 
