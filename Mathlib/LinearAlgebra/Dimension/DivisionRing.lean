452fe9373e6e2fd133c3953c23e1632934f5df78
Wrenna Robson <wrenna.robson@bristol.ac.uk>
2026-04-21 16:30:50 +0100

    Restore Function.prod_apply in DivisionRing simp call

--- Mathlib/LinearAlgebra/Dimension/DivisionRing.lean
+++ Mathlib/LinearAlgebra/Dimension/DivisionRing.lean
@@ -97,7 +97,8 @@ theorem rank_add_rank_split (db : V₂ →ₗ[K] V) (eb : V₃ →ₗ[K] V) (cd 
     rintro ⟨d, e⟩
     have h := eq₂ d (-e)
     simp only [add_eq_zero_iff_eq_neg, LinearMap.prod_apply, mem_ker,
-      Prod.mk_inj, coprod_apply, map_neg, neg_apply, LinearMap.mem_range] at h ⊢
+      Prod.mk_inj, coprod_apply, map_neg, neg_apply, LinearMap.mem_range,
+      Function.prod_apply] at h ⊢
     grind
 
 end
