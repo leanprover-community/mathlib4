fa236e4d3d6b8de79fa60678c9548b57575180de
Christian Merten <christian@merten.dev>
2026-04-21 17:31:43 +0200

    mk_all

--- Mathlib.lean
+++ Mathlib.lean
@@ -6313,6 +6313,7 @@ public import Mathlib.RingTheory.DualNumber
 public import Mathlib.RingTheory.EssentialFiniteness
 public import Mathlib.RingTheory.Etale.Basic
 public import Mathlib.RingTheory.Etale.Field
+public import Mathlib.RingTheory.Etale.Finite
 public import Mathlib.RingTheory.Etale.Kaehler
 public import Mathlib.RingTheory.Etale.Locus
 public import Mathlib.RingTheory.Etale.Pi
