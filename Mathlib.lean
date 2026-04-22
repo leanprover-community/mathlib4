47abd1a05b617f5ea06f1af897749139b7efd6bb
vihdzp <vi.hdz.p@gmail.com>
2026-04-22 01:39:06 -0600

    here too

--- Mathlib.lean
+++ Mathlib.lean
@@ -6890,7 +6890,7 @@ public import Mathlib.SetTheory.Cardinal.Aleph
 public import Mathlib.SetTheory.Cardinal.Arithmetic
 public import Mathlib.SetTheory.Cardinal.Basic
 public import Mathlib.SetTheory.Cardinal.Cofinality
-public import Mathlib.SetTheory.Cardinal.Cofinality.Defs
+public import Mathlib.SetTheory.Cardinal.Cofinality.Basic
 public import Mathlib.SetTheory.Cardinal.Cofinality.Ordinal
 public import Mathlib.SetTheory.Cardinal.Continuum
 public import Mathlib.SetTheory.Cardinal.CountableCover
