55025a71fdbf89b7e714aa9afb5c1cb5b2e4a818
vihdzp <vi.hdz.p@gmail.com>
2026-04-22 04:54:31 -0600

    fix

--- Mathlib/SetTheory/Cardinal/Cofinality/Enum.lean
+++ Mathlib/SetTheory/Cardinal/Cofinality/Enum.lean
@@ -80,7 +80,6 @@ theorem cof_eq_card : cof α = #α := by
 theorem ord_cardinalMk : ord (#α) = typeLT α := by
   rw [← ord_cof_eq_type_lt, cof_eq_card]
 
-@[simp]
 theorem cof_ordinal : cof Ordinal.{u} = Cardinal.univ.{u, u + 1} := by
   simp [← Cardinal.univ_id]
 
