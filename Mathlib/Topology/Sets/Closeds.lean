14476fee2b9d28f91b031d7d1afafd3c985ef179
Raph-DG <raphaeldouglasgiles@gmail.com>
2026-04-22 21:34:47 +0200

    Fixed build errors

--- Mathlib/Topology/Sets/Closeds.lean
+++ Mathlib/Topology/Sets/Closeds.lean
@@ -530,17 +530,17 @@ to the irreducible closeds of `X` nontrivially intersecting the range of `f`.
 noncomputable
 def orderIsoOfIsOpenEmbedding (f : β → α) (h : IsOpenEmbedding f) :
     IrreducibleCloseds β ≃o {V : IrreducibleCloseds α | (f ⁻¹' V).Nonempty} where
-  toFun T := ⟨map f h.continuous T, preimage_closure_image_nonempty h.continuous T T.2.nonempty⟩
+  toFun T := ⟨map f h.continuous T, nonempty_preimage_closure_image h.continuous T T.2.nonempty⟩
   invFun V :=
     { carrier := f ⁻¹' V
       isIrreducible' := ⟨V.2, V.1.2.isPreirreducible.preimage h⟩
       isClosed' := V.1.3.preimage h.continuous }
   left_inv V := by
     ext
-    simp [h.isOpenMap.preimage_closure_image_eq_id h.injective h.continuous _ V.isClosed]
+    simp [h.isOpenMap.preimage_closure_image h.injective h.continuous _ V.isClosed]
   right_inv V := by
     ext
-    simp [closure_image_preimage_of_preIrreducible f h.isOpenMap V V.2 V.1.2.2 V.1.3]
+    simp [closure_image_preimage_of_isPreirreducible f h.isOpenMap V V.2 V.1.2.2 V.1.3]
   map_rel_iff' {a b} := by
     refine ⟨fun hle ↦ ?_, fun hle ↦ map_mono h.continuous hle⟩
     simpa [← h.isEmbedding.closure_eq_preimage_closure_image, a.isClosed.closure_eq,
