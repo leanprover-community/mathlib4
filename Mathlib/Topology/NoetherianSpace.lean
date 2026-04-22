aaee5ae1e0bc06af2e848dc15aca14fe96676e60
Raphael Douglas Giles <77658801+Raph-DG@users.noreply.github.com>
2026-04-22 19:32:08 +0200

    Update Mathlib/Topology/NoetherianSpace.lean
    Co-authored-by: Christian Merten <christian@merten.dev>

--- Mathlib/Topology/NoetherianSpace.lean
+++ Mathlib/Topology/NoetherianSpace.lean
@@ -214,7 +214,7 @@ theorem NoetherianSpace.exists_open_ne_empty_le_irreducibleComponent [Noetherian
     ∃ o : Set α, IsOpen o ∧ o.Nonempty ∧ o ≤ Z := by
   simpa using exists_isOpen_nonempty_subset_irreducibleComponent Z H
 
-lemma NoetherianSpace.subset {W V : Set α} [NoetherianSpace W]
+lemma NoetherianSpace.of_subset {W V : Set α} [NoetherianSpace W]
     (h : V ⊆ W) : NoetherianSpace V :=
   Topology.IsInducing.noetherianSpace (Topology.IsEmbedding.inclusion h).isInducing
 
