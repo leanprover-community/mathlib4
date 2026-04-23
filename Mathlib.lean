db0b1f39c010fb2fbfe530cf2b7fe99779fa7eac
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2026-04-23 22:35:39 +0800

    Update Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -6693,6 +6693,7 @@ public import Mathlib.RingTheory.Radical.Basic
 public import Mathlib.RingTheory.Radical.NatInt
 public import Mathlib.RingTheory.ReesAlgebra
 public import Mathlib.RingTheory.Regular.Category
+public import Mathlib.RingTheory.Regular.Depth
 public import Mathlib.RingTheory.Regular.Flat
 public import Mathlib.RingTheory.Regular.IsSMulRegular
 public import Mathlib.RingTheory.Regular.LinearMap
