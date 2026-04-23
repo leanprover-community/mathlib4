328f17e050bba598f8166695603de06294c05adb
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2026-04-23 22:23:20 +0800

    restore deprecation

---
+++ Mathlib/RingTheory/Regular/Depth.lean
@@ -0,0 +1,11 @@
+/-
+Copyright (c) 2025 Nailin Guan. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Nailin Guan, Yi Song
+-/
+module
+
+public import Mathlib.RingTheory.Regular.LinearMap
+public import Mathlib.Tactic.Linter.DeprecatedModule
+
+deprecated_module (since := "2026-04-14")
