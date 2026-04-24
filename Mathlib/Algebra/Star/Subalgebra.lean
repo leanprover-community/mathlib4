87a52f44bd248fe07184194b5dd2c3869269e5a7
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2026-04-24 22:39:27 +0200

    protect

--- Mathlib/Algebra/Star/Subalgebra.lean
+++ Mathlib/Algebra/Star/Subalgebra.lean
@@ -515,7 +515,7 @@ theorem _root_.Subalgebra.starClosure_eq_adjoin (S : Subalgebra R A) :
 @[elab_as_elim]
 theorem adjoin_induction {s : Set A} {p : (x : A) → x ∈ adjoin R s → Prop}
     (mem : ∀ (x) (h : x ∈ s), p x (subset_adjoin R s h))
-    (algebraMap : ∀ r, p (Algebra.algebraMap R _ r) (algebraMap_mem _ r))
+    (algebraMap : ∀ r, p (algebraMap R _ r) (algebraMap_mem _ r))
     (add : ∀ x y hx hy, p x hx → p y hy → p (x + y) (add_mem hx hy))
     (mul : ∀ x y hx hy, p x hx → p y hy → p (x * y) (mul_mem hx hy))
     (star : ∀ x hx, p x hx → p (star x) (star_mem hx))
