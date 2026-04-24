b9600c680e3e459446dc621406dca8537e07a6d8
Joël Riou <37772949+joelriou@users.noreply.github.com>
2026-04-24 15:22:52 +0000

    feat(CategoryTheory/Monoidal): definition of ring objects (#38366)
    This PR defines ring objects in cartesian monoidal categories.

--- Mathlib.lean
+++ Mathlib.lean
@@ -3075,6 +3075,7 @@ public import Mathlib.CategoryTheory.Monoidal.Rigid.Braided
 public import Mathlib.CategoryTheory.Monoidal.Rigid.Functor
 public import Mathlib.CategoryTheory.Monoidal.Rigid.FunctorCategory
 public import Mathlib.CategoryTheory.Monoidal.Rigid.OfEquivalence
+public import Mathlib.CategoryTheory.Monoidal.Ring
 public import Mathlib.CategoryTheory.Monoidal.Skeleton
 public import Mathlib.CategoryTheory.Monoidal.Subcategory
 public import Mathlib.CategoryTheory.Monoidal.Tor
