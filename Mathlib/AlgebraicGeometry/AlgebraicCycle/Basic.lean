1258f2c45e61ad4253ca9e0df69c4f03baff4677
Raph-DG <raphaeldouglasgiles@gmail.com>
2026-04-24 17:15:26 +0200

    Added more API and got rid of WeilDivisor file

--- Mathlib/AlgebraicGeometry/AlgebraicCycle/Basic.lean
+++ Mathlib/AlgebraicGeometry/AlgebraicCycle/Basic.lean
@@ -39,10 +39,9 @@ Here we're making use of the equivalence between irreducible closed subsets of a
 generic points in order to reuse the API in Function.locallyFinsupp, hence the slightly
 nonstandard definition.
 -/
-abbrev AlgebraicCycle (X : Scheme.{u}) (R : Type*) [Zero R] :=
-    Function.locallyFinsupp X R
+abbrev AlgebraicCycle (X : Scheme.{u}) (R : Type*) [Zero R] := Function.locallyFinsupp X R
 
-variable (f : X ⟶ Y) [Semiring R] (c : AlgebraicCycle X R) (x : X) (z : Y)
+variable (f : X ⟶ Y)
 
 noncomputable
 instance moduleResidueFieldExtension (x : X) :
@@ -57,12 +56,80 @@ from `κ(f x)` to `κ(x)`. We return a default value of zero when this degree is
 or undefined.
 -/
 noncomputable
-def _root_.AlgebraicGeometry.Scheme.Hom.degree : ℕ := @Module.finrank
+def _root_.AlgebraicGeometry.Scheme.Hom.degree (x : X) : ℕ := @Module.finrank
     (IsLocalRing.ResidueField (Y.presheaf.stalk (f.base x)))
     (IsLocalRing.ResidueField (X.presheaf.stalk x)) _ _ _
 
 namespace AlgebraicCycle
+section restrict
 
+variable [Zero R]
+    (D : AlgebraicCycle X R) (t : Set X) [DecidablePred (· ∈ t)]
+/--
+Restriction of an algebraic cycle to some set. This is distinct from `Function.locallyFinsuppWithin`
+because here we get something which is still just a locally finsupp function on the whole space.
+-/
+def restrict : AlgebraicCycle X R where
+  toFun z := if z ∈ t then D z else 0
+  supportWithinDomain' := by simp
+  supportLocallyFiniteWithinDomain' := by
+    intro z hz
+    obtain ⟨U, hU⟩ := D.supportLocallyFiniteWithinDomain z hz
+    use U, hU.1
+    apply Finite.subset hU.2
+    apply inter_subset_inter (Subset.refl U)
+    simp
+
+lemma restrict_apply (z : X) : D.restrict t z = if z ∈ t then D z else 0 := rfl
+
+@[simp]
+lemma restrict_eq_of_mem (z : X) (hz : z ∈ t) :
+    D.restrict t z = D z := dif_pos hz
+
+@[simp]
+lemma restrict_eq_zero_of_not_mem (z : X) (hz : z ∉ t) :
+    D.restrict t z = 0 := dif_neg hz
+
+lemma restrict_eqOn : Set.EqOn (D.restrict t) D t := by
+  intro _ _
+  simp_all
+
+lemma restrict_eqOn_compl : Set.EqOn (D.restrict t) 0 tᶜ := by
+  intro _ hx
+  simp_all
+
+@[simp]
+lemma restrict_eq_zero_of_eq_zero {z : X} (hD : D z = 0) : (D.restrict t) z = 0 := by
+  by_cases o : z ∈ t <;> simp_all
+
+variable {D t}
+lemma restrict_support_subset_support : (D.restrict t).support ⊆ D.support := by
+  simp_all only [Function.support_subset_iff, ne_eq, Function.mem_support]
+  intro x hx
+  contrapose hx
+  simp_all
+
+lemma restrict_support_subset : (D.restrict t).support ⊆ t := by
+  intro z
+  by_cases o : z ∈ t <;> simp_all
+
+lemma restrict_support_of_subset {X : Scheme.{u}} {R : Type*} [Zero R] {D : AlgebraicCycle X R}
+    {t s : Set X} [DecidablePred (· ∈ t)] (h : D.support ⊆ s) : (D.restrict t).support ⊆ s :=
+  Subset.trans restrict_support_subset_support h
+
+lemma restrict_support_subset_inter {X : Scheme.{u}} {R : Type*} [Zero R] {D : AlgebraicCycle X R}
+    {t s : Set X} [DecidablePred (· ∈ t)] (h : D.support ⊆ s) : (D.restrict t).support ⊆ s ∩ t :=
+  subset_inter_iff.mpr ⟨Subset.trans restrict_support_subset_support h, restrict_support_subset⟩
+
+@[simp]
+lemma restrict_univ {X : Scheme.{u}} {R : Type*} [Zero R] {D : AlgebraicCycle X R}
+    [DecidablePred (· ∈ (univ : Set X))] : D.restrict univ = D := by
+  ext z
+  simp
+
+end restrict
+
+variable [Semiring R] (c : AlgebraicCycle X R)
 /--
 Implementation detail for pushforward: function used to define the coefficient of the pushforward
 of a cycle `c` at a point `z = f x`, as in stacks `02R3`.
@@ -79,8 +146,8 @@ lemma _root_.AlgebraicGeometry.Scheme.Hom.preimageSupportFinite [QuasiCompact f]
 
 /--
 The pushforward of algebraic cycles with respect to a quasicompact morphism of schemes. The
-arguments `wx` and `wy` are certain weight functions used to calculate how the weights of the
-algebraic cycle should be adjusted to make the pushforward operation functorial. Typically in
+arguments `wx` and `wy` are gradings of the algebraic cycle, which is necessary information for the
+function determining how to adjust the coefficients of the cycle. Typically in
 applications these will be some notions of dimension or codimension. The most common notion of
 dimension is `Order.height`, and the most common notion of codimension is `Order.coheight`, though
 more sophisticated notions exist in the literature which are useful when sufficient
@@ -91,5 +158,32 @@ def pushforward [QuasiCompact f] {N : Type*} [DecidableEq N] (c : AlgebraicCycle
     (wx : X → N) (wy : Y → N) : AlgebraicCycle Y R :=
   Function.locallyFinsupp.map f (Nat.cast (R := R) <| mapAux f wx wy ·) c
   f.isSpectralMap (f.preimageSupportFinite c)
+
+lemma homgeneous_ext {R : Type*} [Zero R] {D₁ D₂ : AlgebraicCycle X R}
+    {t : Set X} (hD₁ : D₁.support ⊆ t) (hD₂ : D₂.support ⊆ t)
+    (h : ∀ a ∈ t, D₁ a = D₂ a) : D₁ = D₂ :=
+  have h' : ∀ a, D₁ a = D₂ a := by
+    intro a
+    by_cases o : a ∈ t
+    · exact h a o
+    rw [support_subset_iff] at hD₁ hD₂
+    specialize hD₁ a
+    specialize hD₂ a
+    simp_all
+  DFunLike.ext _ _ h'
+
+lemma homogeneous_le_iff {R : Type*} [Zero R] [Preorder R] {D₁ D₂ : AlgebraicCycle X R}
+    {t : Set X} (hD₁ : D₁.support ⊆ t) (hD₂ : ∀ z ∈ tᶜ, D₂ z ≥ 0) :
+    D₁ ≤ D₂ ↔ ∀ z ∈ t, D₁ z ≤ D₂ z := by
+  peel with z
+  refine ⟨by tauto, fun m ↦ ?_⟩
+  by_cases o : z ∈ t
+  · exact m o
+  simp only [support_subset_iff, ne_eq] at hD₁
+  specialize hD₁ z
+  by_cases p : D₁ z = 0
+  · simp_all
+  specialize hD₁ p
+  contradiction
 
 end AlgebraicCycle
--- Mathlib/AlgebraicGeometry/AlgebraicCycle/Principal.lean
+++ Mathlib/AlgebraicGeometry/AlgebraicCycle/Principal.lean
@@ -7,13 +7,13 @@ public import Mathlib.Topology.NoetherianSpace
 /-!
 # Principal divisors
 
-In this file we develop the notion of a principal divisor associated with an element of the function
-field of a locally noetherian integral scheme.
+In this file we develop the notion of a principal Weil divisor associated with an element of the
+function field of a locally noetherian integral scheme.
 -/
 
 @[expose] public section
 
-open AlgebraicGeometry CategoryTheory Order TopologicalSpace Set Topology
+open AlgebraicGeometry Order TopologicalSpace Set Topology
 
 universe u
 variable {X : Scheme.{u}}
@@ -40,9 +40,12 @@ lemma div_locally_finite [IsIntegral X] [IsLocallyNoetherian X]
     quasiSober_of_quasisober_inter_isClosed_right W.1 U.2.isClosed_compl
   have : NoetherianSpace (W.1 ∩ (U : Set X)ᶜ : Set X) :=
     NoetherianSpace.noetherian_inter_left W.1 _
-  have hne : closure (W.1 ∩ (U : Set X)ᶜ) ≠ univ := fun h =>
-    compl_ne_univ.mpr ((Scheme.Opens.nonempty_iff _).mp hUne) <| univ_subset_iff.mp <|
-      h ▸ (closure_mono inter_subset_right).trans U.2.isClosed_compl.closure_eq.le
+  have hne : closure (W.1 ∩ (U : Set X)ᶜ) ≠ univ := by
+    intro h
+    have := (closure_mono (inter_subset_right (s := W.carrier) (t := (↑U)ᶜ))).trans
+        U.2.isClosed_compl.closure_eq.le
+    rw [h] at this
+    exact compl_ne_univ.mpr ((Scheme.Opens.nonempty_iff _).mp hUne) <| univ_subset_iff.mp <| this
   refine ⟨W, W.2.mem_nhds hzW,
     (NoetherianSpace.finite_coheight_one_of_closure_ne_univ hne).subset ?_⟩
   intro x ⟨hxW, hxsup⟩
@@ -54,7 +57,7 @@ lemma div_locally_finite [IsIntegral X] [IsLocallyNoetherian X]
 
 /--
 On an locally Noetherian integral scheme, given `f : X.functionField` and `hf : x ≠ 0`,
-we define the principal Weil divisor `f.divisor hf` as an algebraic cycle with coefficients in `ℤ`.
+we define the principal Weil divisor `div f hf` as an algebraic cycle with coefficients in `ℤ`.
 -/
 noncomputable
 def div [IsIntegral X] [nt : IsLocallyNoetherian X] (f : X.functionField) (hf : f ≠ 0) :
@@ -65,17 +68,6 @@ def div [IsIntegral X] [nt : IsLocallyNoetherian X] (f : X.functionField) (hf : 
   supportWithinDomain' := by simp
   supportLocallyFiniteWithinDomain' z _ := div_locally_finite f hf z
 
-lemma div_ext₁ [IsIntegral X] [IsLocallyNoetherian X] {f : X.functionField} {hf : f ≠ 0}
-    {D : AlgebraicCycle X ℤ} (h : ∀ (a : X) (ha : coheight a = 1), div f hf a = D a) :
-    div f hf = D := by
-  ext x
-  simp [div]
-
-  sorry
-
-lemma div_ext₂ [IsIntegral X] [IsLocallyNoetherian X] {f : X.functionField} {hf : f ≠ 0}
-    {D : AlgebraicCycle X ℤ} (h : ∀ (a : X) (ha : coheight a = 1), D a = div f hf a) : D = div f hf := sorry
-
 @[simp]
 lemma div_eq_zero_of_coheight_ne_one [IsIntegral X] [IsLocallyNoetherian X] (f : X.functionField)
     (hf : f ≠ 0) (Z : X) (hZ : coheight Z ≠ 1) : div f hf Z = 0 := dif_neg hZ
@@ -85,18 +77,8 @@ lemma div_eq_ord_of_coheight_eq_one [IsIntegral X] [IsLocallyNoetherian X] (f : 
     (hf : f ≠ 0) (Z : X) (hZ : coheight Z = 1) :
     div f hf Z = Multiplicative.toAdd (WithZero.unzero (Scheme.ord_ne_zero hZ hf)) := dif_pos hZ
 
-/--
-TODO: Generlize this beyond just being about cycles with coefficients in `ℤ`.
--/
-lemma div_le_iff [IsIntegral X] [IsLocallyNoetherian X] (f : X.functionField)
-    (hf : f ≠ 0) {D : AlgebraicCycle X ℤ} (hD : ∀ z : X, coheight z ≠ 1 → D z ≥ 0):
-    div f hf ≤ D ↔ ∀ z : X, coheight z = 1 → div f hf z ≤ D z :=
-  ⟨fun h z _ ↦ h z, fun h z ↦ if hz : coheight z = 1
-                              then h z hz
-                              else (by simp [div_eq_zero_of_coheight_ne_one _ _ _ hz, hD z hz])⟩
-
 @[simp]
-theorem div_homomorphism [IsIntegral X] [IsLocallyNoetherian X]
+theorem div_mul [IsIntegral X] [IsLocallyNoetherian X]
     (f : X.functionField) (hf : f ≠ 0) (g : X.functionField) (hg : g ≠ 0) :
     div (f * g) ((mul_ne_zero_iff_right hg).mpr hf) = div f hf + div g hg := by
   ext a
@@ -104,24 +86,46 @@ theorem div_homomorphism [IsIntegral X] [IsLocallyNoetherian X]
   simp only [div, map_mul]
   split_ifs <;> simp_all [unzero_mul]
 
-theorem div_of_isUnit [IsIntegral X] [IsNoetherian X] {U : X.Opens} [Nonempty U] (g : Γ(X, U))
-    (hg : IsUnit g) : div (X.germToFunctionField U g)
-    ((map_ne_zero_iff _ (germToFunctionField_injective X U)).mpr hg.ne_zero) = 0 := by
-  ext z
-  simp [div]
-  sorry
+/--
+The `div` construction gives a Weil divisor.
+-/
+theorem div_support [IsIntegral X] [IsLocallyNoetherian X] {f : X.functionField} {hf : f ≠ 0} :
+    (div f hf).support ⊆ {x : X | coheight x = 1} := by
+  intro z hz
+  simp only [Function.mem_support, ne_eq] at hz
+  contrapose hz
+  exact div_eq_zero_of_coheight_ne_one f hf z hz
 
+theorem div_eq_zero_of_isUnit [IsIntegral X] [IsLocallyNoetherian X] {U : X.Opens} [Nonempty U]
+    [DecidablePred (· ∈ U)] {g : Γ(X, U)} (hg : IsUnit g) : (div (X.germToFunctionField U g)
+    ((map_ne_zero_iff _ (germToFunctionField_injective X U)).mpr hg.ne_zero)).restrict U = 0 := by
+  apply AlgebraicCycle.homgeneous_ext (AlgebraicCycle.restrict_support_subset_inter div_support)
+    (by simp)
+  intro z hz
+  simp_all only [mem_inter_iff, mem_setOf_eq, SetLike.mem_coe, AlgebraicCycle.restrict_eq_of_mem,
+    div_eq_ord_of_coheight_eq_one, Function.locallyFinsuppWithin.coe_zero, Pi.zero_apply,
+    toAdd_eq_zero]
+  change _ = unzero (by simp : (1 : WithZero (Multiplicative ℤ)) ≠ 0)
+  apply unzero.congr_simp
+  apply ord_of_isUnit hg _ hz.2
+
+lemma div_eq_zero_of_isUnit_top
+    [IsIntegral X] [IsLocallyNoetherian X] {g : Γ(X, ⊤)} (hg : IsUnit g) :
+    haveI : Nonempty (⊤ : X.Opens) := (Opens.nonempty_iff ⊤).mpr univ_nonempty
+    div (X.germToFunctionField ⊤ g)
+    ((map_ne_zero_iff _ (germToFunctionField_injective X ⊤)).mpr hg.ne_zero) = 0 := by
+  classical
+  have : Nonempty (⊤ : X.Opens) := (Opens.nonempty_iff ⊤).mpr univ_nonempty
+  simp [← div_eq_zero_of_isUnit hg]
 
 @[simp]
-theorem div_neg [IsIntegral X] [IsLocallyNoetherian X]
-    (f : X.functionField) (hf : f ≠ 0) :
+theorem div_neg [IsIntegral X] [IsLocallyNoetherian X] (f : X.functionField) (hf : f ≠ 0) :
     div (- f) (neg_ne_zero.mpr hf) = div f hf := by
-  have : -f = -1 * f := neg_eq_neg_one_mul f
-  simp_rw [this]
-  rw [div_homomorphism (-1) (by simp) f hf]
-  simp
-
-  sorry
+  classical
+  simp_rw [neg_eq_neg_one_mul f]
+  rw [div_mul _ (by simp) _ hf, add_eq_right]
+  have : IsUnit (-1 : Γ(X, ⊤)) := by simp
+  simp [← div_eq_zero_of_isUnit_top this]
 
 end functionField
 end Scheme
