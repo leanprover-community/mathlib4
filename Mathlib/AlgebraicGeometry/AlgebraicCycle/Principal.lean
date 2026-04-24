d1a81fc2cde87c769da3a71f53e68a63e0bcda17
Raph-DG <raphaeldouglasgiles@gmail.com>
2026-04-24 17:25:31 +0200

    Added in copyright thing

--- Mathlib/AlgebraicGeometry/AlgebraicCycle/Principal.lean
+++ Mathlib/AlgebraicGeometry/AlgebraicCycle/Principal.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2026 Raphael Douglas Giles. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Raphael Douglas Giles
+-/
 module
 
 public import Mathlib.AlgebraicGeometry.AlgebraicCycle.Basic
