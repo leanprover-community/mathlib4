36a97460d7eaafa7edb0d949816ae846240d9fdc
Thomas Browning <13339017+tb65536@users.noreply.github.com>
2026-04-25 10:39:56 +0000

    feat(AlgebraicGeometry/EllipticCurve/LFunction): define the L-Function of an elliptic curve (#37771)
    This PR defines the L-Function of an elliptic curve.
    
    Co-authored-by: tb65536 <thomas.l.browning@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1330,6 +1330,7 @@ public import Mathlib.AlgebraicGeometry.EllipticCurve.IsomOfJ
 public import Mathlib.AlgebraicGeometry.EllipticCurve.Jacobian.Basic
 public import Mathlib.AlgebraicGeometry.EllipticCurve.Jacobian.Formula
 public import Mathlib.AlgebraicGeometry.EllipticCurve.Jacobian.Point
+public import Mathlib.AlgebraicGeometry.EllipticCurve.LFunction
 public import Mathlib.AlgebraicGeometry.EllipticCurve.ModelsWithJ
 public import Mathlib.AlgebraicGeometry.EllipticCurve.NormalForms
 public import Mathlib.AlgebraicGeometry.EllipticCurve.Projective.Basic
