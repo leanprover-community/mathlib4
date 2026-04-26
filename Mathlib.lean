7057d8fc3e6c2fe9f7b132af312e6703b67f61bb
Rmal <97214596+CoolRmal@users.noreply.github.com>
2026-04-25 20:07:48 -0400

    update

--- Mathlib.lean
+++ Mathlib.lean
@@ -5268,6 +5268,7 @@ public import Mathlib.MeasureTheory.Function.LpSpace.ContinuousFunctions
 public import Mathlib.MeasureTheory.Function.LpSpace.DomAct.Basic
 public import Mathlib.MeasureTheory.Function.LpSpace.DomAct.Continuous
 public import Mathlib.MeasureTheory.Function.LpSpace.Indicator
+public import Mathlib.MeasureTheory.Function.LusinContinuous
 public import Mathlib.MeasureTheory.Function.Piecewise
 public import Mathlib.MeasureTheory.Function.SimpleFunc
 public import Mathlib.MeasureTheory.Function.SimpleFuncDense
