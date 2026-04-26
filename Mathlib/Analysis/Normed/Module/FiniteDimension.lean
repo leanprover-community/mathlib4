6e5a70ed59bf8440bb959150835eff66d040a651
ADedecker <anatolededecker@gmail.com>
2026-04-26 13:27:49 +0200

    Remove import

--- Mathlib/Analysis/Normed/Module/FiniteDimension.lean
+++ Mathlib/Analysis/Normed/Module/FiniteDimension.lean
@@ -8,7 +8,6 @@ module
 public import Mathlib.Analysis.Asymptotics.AsymptoticEquivalent
 public import Mathlib.Analysis.Normed.Group.Lemmas
 public import Mathlib.Analysis.Normed.Affine.Isometry
-public import Mathlib.Analysis.Normed.Operator.Compact
 public import Mathlib.Analysis.Normed.Operator.NormedSpace
 public import Mathlib.Analysis.Normed.Module.RieszLemma
 public import Mathlib.Analysis.Normed.Module.Ball.Pointwise
