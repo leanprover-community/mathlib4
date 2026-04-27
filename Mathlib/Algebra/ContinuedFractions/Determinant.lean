c4906a2c0de5cdb66581131a57a5ed94dc449de5
emlis <175462753+emlis42@users.noreply.github.com>
2026-04-27 11:30:56 -0400

    golf

--- Mathlib/Algebra/ContinuedFractions/Determinant.lean
+++ Mathlib/Algebra/ContinuedFractions/Determinant.lean
@@ -48,14 +48,13 @@ private theorem determinant_aux (hyp : n = 0 ∨ ¬g.TerminatedAt (n - 1)) :
     let ppA := ppred_conts.a
     let ppB := ppred_conts.b
     -- let's change the goal to something more readable
-    change pA * conts.b - pB * conts.a = _
+    change pA * conts.b - pB * conts.a = ∏ i ∈ Finset.range (n + 1), -(g.partNums.get? i).getD 0
     have not_terminated_at_n : ¬TerminatedAt g n := Or.resolve_left hyp n.succ_ne_zero
     obtain ⟨gp, s_nth_eq⟩ : ∃ gp, g.s.get? n = some gp :=
       Option.ne_none_iff_exists'.1 not_terminated_at_n
     -- unfold the recurrence relation for `conts` once and simplify to derive the following
     suffices ppA * pB - ppB * pA = ∏ i ∈ Finset.range n, - (g.partNums.get? i).getD 0 by
-      rw [Finset.prod_range_succ, ← this, partNums, Stream'.Seq.map_get?, s_nth_eq]
-      simp only [Option.map_some, Option.getD_some, mul_neg]
+      rw [Finset.prod_range_succ, ← this, partNum_eq_s_a s_nth_eq, Option.getD_some]
       subst conts
       rw [contsAux_recurrence s_nth_eq ppred_conts_eq pred_conts_eq]
       ring
@@ -66,11 +65,11 @@ theorem determinant :
     g.nums n * g.dens (n + 1) - g.dens n * g.nums (n + 1)
       = ∏ i ∈ Finset.range (n + 1), - (g.partNums.get? i).getD 0 := by
   rcases em <| TerminatedAt g n with terminatedAt_n | not_terminatedAt_n
-  swap; · exact determinant_aux <| Or.inr <| not_terminatedAt_n
-  rw [dens_stable_of_terminated n.le_succ <| terminatedAt_n]
-  rw [nums_stable_of_terminated n.le_succ <| terminatedAt_n]
-  rw [Finset.prod_range_succ, partNums, Stream'.Seq.map_get?, terminatedAt_n]
-  grind
+  · grind [partNum_none_iff_s_none.mpr terminatedAt_n,
+      nums_stable_of_terminated n.le_succ terminatedAt_n,
+      dens_stable_of_terminated n.le_succ terminatedAt_n, Finset.prod_range_succ]
+  · exact determinant_aux <| Or.inr not_terminatedAt_n
+
 
 end GenContFract
 
@@ -84,13 +83,11 @@ theorem determinant (not_terminatedAt_n : ¬(↑s : GenContFract K).TerminatedAt
       (↑s : GenContFract K).dens n * (↑s : GenContFract K).nums (n + 1) = (-1) ^ (n + 1) := calc
   _ = ∏ i ∈ Finset.range (n + 1), - ((↑s : GenContFract K).partNums.get? i).getD 0 :=
     (↑s : GenContFract K).determinant
-  _ = ∏ i ∈ Finset.range (n + 1), -1 := by
-    apply Finset.prod_congr rfl fun i hi ↦ ?_
-    simp only [Finset.mem_range] at hi
+  _ = ∏ i ∈ Finset.range (n + 1), -1 := Finset.prod_congr rfl fun i hi ↦ by
+    rw [Finset.mem_range] at hi
     obtain ⟨gp, s_ith_eq⟩ : ∃ gp, (↑s : GenContFract K).s.get? i = some gp :=
       Option.ne_none_iff_exists'.1 <| mt (terminated_stable <| Nat.le_of_succ_le_succ hi) ‹_›
-    rw [partNums, Stream'.Seq.map_get?, s_ith_eq, Option.map_some]
-    rw [s.property i _ <| partNum_eq_s_a s_ith_eq, Option.getD_some]
+    rw [partNum_eq_s_a s_ith_eq, s.property i gp.a <| partNum_eq_s_a s_ith_eq, Option.getD_some]
   _ = (-1) ^ (n + 1) := by rw [Finset.prod_const, Finset.card_range]
 
 end SimpContFract
