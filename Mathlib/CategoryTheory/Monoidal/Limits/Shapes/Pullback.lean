9466ce9b7bddfb9b014c6e050e11f39c52f0802f
Dagur Asgeirsson <dagurtomas@gmail.com>
2026-04-27 09:16:20 -0600

    reorder file

--- Mathlib/CategoryTheory/Monoidal/Limits/Shapes/Pullback.lean
+++ Mathlib/CategoryTheory/Monoidal/Limits/Shapes/Pullback.lean
@@ -25,28 +25,6 @@ universe v u
 
 open CategoryTheory Limits MonoidalCategory
 
-variable {C : Type u} [Category.{v} C] [MonoidalCategory C]
-
-variable [HasPushouts C]
-  {W X Y Z : C} {f : X ⟶ Y} {g : X ⟶ Z}
-  (h : Y ⟶ W) (k : Z ⟶ W) (w : f ≫ h = g ≫ k) {Q : C}
-
-variable {A B X Y Z W : C} {f : A ⟶ B} {g : X ⟶ Y}
-
-@[reassoc]
-lemma CategoryTheory.MonoidalCategory.Limits.pushout.associator_naturality_left_condition
-    {h : Z ⊗ W ⟶ X} : f ▷ Z ▷ W ≫ (α_ B Z W).hom ≫ B ◁ h ≫ pushout.inl (f ▷ X) (A ◁ g) =
-      (α_ A Z W).hom ≫ A ◁ (h ≫ g) ≫ pushout.inr (f ▷ X) (A ◁ g) := by
-  rw [associator_naturality_left_assoc, ← whisker_exchange_assoc, pushout.condition,
-    ← MonoidalCategory.whiskerLeft_comp_assoc]
-
-@[reassoc]
-lemma CategoryTheory.MonoidalCategory.Limits.pushout.associator_inv_naturality_right_condition
-    {h : Z ⊗ W ⟶ A} : Z ◁ W ◁ g ≫ (α_ Z W Y).inv ≫ h ▷ Y ≫ pushout.inr (f ▷ X) (A ◁ g) =
-      (α_ Z W X).inv ≫ (h ≫ f) ▷ X ≫ pushout.inl (f ▷ X) (A ◁ g) := by
-  rw [associator_inv_naturality_right_assoc, whisker_exchange_assoc, ← pushout.condition,
-    ← comp_whiskerRight_assoc]
-
 attribute [specialize_map tensorLeft (suffix := "_tensorLeft")]
   IsPushout.inl_desc
   IsPushout.inr_desc
@@ -92,3 +70,25 @@ attribute [reassoc (attr := simp)]
   inr_comp_pushoutSymmetry_hom_tensorRight
 
 attribute [reassoc] pushout.condition_tensorRight
+
+variable {C : Type u} [Category.{v} C] [MonoidalCategory C]
+
+variable [HasPushouts C]
+  {W X Y Z : C} {f : X ⟶ Y} {g : X ⟶ Z}
+  (h : Y ⟶ W) (k : Z ⟶ W) (w : f ≫ h = g ≫ k) {Q : C}
+
+variable {A B X Y Z W : C} {f : A ⟶ B} {g : X ⟶ Y}
+
+@[reassoc]
+lemma CategoryTheory.MonoidalCategory.Limits.pushout.associator_naturality_left_condition
+    {h : Z ⊗ W ⟶ X} : f ▷ Z ▷ W ≫ (α_ B Z W).hom ≫ B ◁ h ≫ pushout.inl (f ▷ X) (A ◁ g) =
+      (α_ A Z W).hom ≫ A ◁ (h ≫ g) ≫ pushout.inr (f ▷ X) (A ◁ g) := by
+  rw [associator_naturality_left_assoc, ← whisker_exchange_assoc, pushout.condition,
+    ← MonoidalCategory.whiskerLeft_comp_assoc]
+
+@[reassoc]
+lemma CategoryTheory.MonoidalCategory.Limits.pushout.associator_inv_naturality_right_condition
+    {h : Z ⊗ W ⟶ A} : Z ◁ W ◁ g ≫ (α_ Z W Y).inv ≫ h ▷ Y ≫ pushout.inr (f ▷ X) (A ◁ g) =
+      (α_ Z W X).inv ≫ (h ≫ f) ▷ X ≫ pushout.inl (f ▷ X) (A ◁ g) := by
+  rw [associator_inv_naturality_right_assoc, whisker_exchange_assoc, ← pushout.condition,
+    ← comp_whiskerRight_assoc]
