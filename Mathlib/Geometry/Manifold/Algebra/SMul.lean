4f224c842f75729b095d2176e8e2b45b1354b57d
Ben Eltschig <b.eltschig@protonmail.com>
2026-04-27 05:07:03 +0200

    use `ℕ∞ω` notation

--- Mathlib/Geometry/Manifold/Algebra/SMul.lean
+++ Mathlib/Geometry/Manifold/Algebra/SMul.lean
@@ -29,7 +29,7 @@ additive action on a Cⁿ manifold `M`, one can use the typeclasses
 class ContMDiffVAdd {𝕜 : Type*} [NontriviallyNormedField 𝕜] {H : Type*} [TopologicalSpace H]
     {E : Type*} [NormedAddCommGroup E] [NormedSpace 𝕜 E] (I : ModelWithCorners 𝕜 E H)
     {H' : Type*} [TopologicalSpace H'] {E' : Type*} [NormedAddCommGroup E'] [NormedSpace 𝕜 E']
-    (I' : ModelWithCorners 𝕜 E' H') (n : WithTop ℕ∞)
+    (I' : ModelWithCorners 𝕜 E' H') (n : ℕ∞ω)
     (G : Type*) [TopologicalSpace G] [ChartedSpace H G]
     (M : Type*) [TopologicalSpace M] [ChartedSpace H' M] [VAdd G M] : Prop where
   contMDiff_vadd : ContMDiff (I.prod I') I' n fun p : G × M ↦ p.1 +ᵥ p.2
@@ -45,7 +45,7 @@ manifold `M`, one can use the typeclasses
 class ContMDiffSMul {𝕜 : Type*} [NontriviallyNormedField 𝕜] {H : Type*} [TopologicalSpace H]
     {E : Type*} [NormedAddCommGroup E] [NormedSpace 𝕜 E] (I : ModelWithCorners 𝕜 E H)
     {H' : Type*} [TopologicalSpace H'] {E' : Type*} [NormedAddCommGroup E'] [NormedSpace 𝕜 E']
-    (I' : ModelWithCorners 𝕜 E' H') (n : WithTop ℕ∞)
+    (I' : ModelWithCorners 𝕜 E' H') (n : ℕ∞ω)
     (G : Type*) [TopologicalSpace G] [ChartedSpace H G]
     (M : Type*) [TopologicalSpace M] [ChartedSpace H' M] [SMul G M] : Prop where
   contMDiff_smul : ContMDiff (I.prod I') I' n fun p : G × M ↦ p.1 • p.2
@@ -64,16 +64,16 @@ variable {𝕜 : Type*} [NontriviallyNormedField 𝕜] {H : Type*} [TopologicalS
   {N : Type*} [TopologicalSpace N] [ChartedSpace H'' N]
 
 @[to_additive]
-protected theorem ContMDiffSMul.of_le [SMul G M] {n m : WithTop ℕ∞} (h : n ≤ m)
+protected theorem ContMDiffSMul.of_le [SMul G M] {n m : ℕ∞ω} (h : n ≤ m)
     [ContMDiffSMul I I' m G M] : ContMDiffSMul I I' n G M := ⟨contMDiff_smul.of_le h⟩
 
 @[to_additive]
-instance [SMul G M] {n : WithTop ℕ∞} [ContMDiffSMul I I' ∞ G M] [ENat.LEInfty n] :
+instance [SMul G M] {n : ℕ∞ω} [ContMDiffSMul I I' ∞ G M] [ENat.LEInfty n] :
     ContMDiffSMul I I' n G M :=
   .of_le ENat.LEInfty.out
 
 @[to_additive]
-instance [SMul G M] {n : WithTop ℕ∞} [ContMDiffSMul I I' ω G M] : ContMDiffSMul I I' n G M :=
+instance [SMul G M] {n : ℕ∞ω} [ContMDiffSMul I I' ω G M] : ContMDiffSMul I I' n G M :=
   .of_le le_top
 
 @[to_additive]
@@ -87,13 +87,13 @@ instance [SMul G M] [ContMDiffSMul I I' 2 G M] : ContMDiffSMul I I' 1 G M :=
 /-- If an action is Cⁿ for some `n`, it is also continuous. This has to be a theorem instead of an
 instance for technical reasons. -/
 @[to_additive]
-lemma ContMDiffSMul.continuousSMul [SMul G M] (n : WithTop ℕ∞) [ContMDiffSMul I I' n G M] :
+lemma ContMDiffSMul.continuousSMul [SMul G M] (n : ℕ∞ω) [ContMDiffSMul I I' n G M] :
     ContinuousSMul G M :=
   ⟨(contMDiff_smul (I := I) (I' := I') (n := n)).continuous⟩
 
 section
 
-variable [SMul G M] {n : WithTop ℕ∞} [ContMDiffSMul I I' n G M]
+variable [SMul G M] {n : ℕ∞ω} [ContMDiffSMul I I' n G M]
   {f : N → G} {g : N → M} {s : Set N} {x : N}
 
 @[to_additive]
@@ -117,7 +117,7 @@ theorem ContMDiff.smul (hf : ContMDiff I'' I n f) (hg : ContMDiff I'' I' n g) :
 end
 
 @[to_additive prod]
-instance ContMDiffSMul.prod [SMul G M] [SMul G N] {n : WithTop ℕ∞} [ContMDiffSMul I I' n G M]
+instance ContMDiffSMul.prod [SMul G M] [SMul G N] {n : ℕ∞ω} [ContMDiffSMul I I' n G M]
     [ContMDiffSMul I I'' n G N] : ContMDiffSMul I (I'.prod I'') n G (M × N) where
   contMDiff_smul := (contMDiff_fst.smul <| contMDiff_fst.comp contMDiff_snd).prodMk <|
       contMDiff_fst.smul <| contMDiff_snd.comp contMDiff_snd
@@ -125,7 +125,7 @@ instance ContMDiffSMul.prod [SMul G M] [SMul G N] {n : WithTop ℕ∞} [ContMDif
 /-- If `G` acts continuously differentiably on `G'` and `G'` acts continuously differentiably on
 `M`, then `G` acts continuously differentiably on `M`. -/
 lemma IsScalarTower.contMDiffSMul (G' : Type*) [TopologicalSpace G'] [ChartedSpace H'' G']
-    [Monoid G'] [SMul G G'] [MulAction G' M] [SMul G M] [IsScalarTower G G' M] {n : WithTop ℕ∞}
+    [Monoid G'] [SMul G G'] [MulAction G' M] [SMul G M] [IsScalarTower G G' M] {n : ℕ∞ω}
     [ContMDiffSMul I I'' n G G'] [ContMDiffSMul I'' I' n G' M] : ContMDiffSMul I I' n G M where
   contMDiff_smul := by
     suffices ContMDiff (I.prod I') I' n (fun p : G × M ↦ (p.1 • (1 : G')) • p.2) by simpa
@@ -134,7 +134,7 @@ lemma IsScalarTower.contMDiffSMul (G' : Type*) [TopologicalSpace G'] [ChartedSpa
 /-- If an action is continuously differentiable, then composing this action with a continuously
 differentiable homomorphism gives again a continuous action. -/
 @[to_additive]
-theorem MulAction.contMDiffSMul_compHom [Monoid G] [MulAction G M] {n : WithTop ℕ∞}
+theorem MulAction.contMDiffSMul_compHom [Monoid G] [MulAction G M] {n : ℕ∞ω}
     [ContMDiffSMul I I' n G M] {G' : Type*} [TopologicalSpace G'] [ChartedSpace H'' G'] [Monoid G']
     {f : G' →* G} (hf : ContMDiff I'' I n f) :
     letI : MulAction G' M := MulAction.compHom _ f
@@ -145,12 +145,12 @@ theorem MulAction.contMDiffSMul_compHom [Monoid G] [MulAction G M] {n : WithTop 
 /-- If a complete normed ring acts continuously differentiably on a manifold `M`, its submanifold
 of units does as well. -/
 instance Units.contMDiffSMul {R : Type*} [NormedRing R] [CompleteSpace R] [NormedAlgebra 𝕜 R]
-    [MulAction R M] {n : WithTop ℕ∞} [ContMDiffSMul 𝓘(𝕜, R) I' n R M] :
+    [MulAction R M] {n : ℕ∞ω} [ContMDiffSMul 𝓘(𝕜, R) I' n R M] :
     ContMDiffSMul 𝓘(𝕜, R) I' n Rˣ M :=
   MulAction.contMDiffSMul_compHom (f := coeHom R) contMDiff_val
 
 @[simps!]
-def Diffeomorph.modelWithCornersSelfProdComparison {n : WithTop ℕ∞} :
+def Diffeomorph.modelWithCornersSelfProdComparison {n : ℕ∞ω} :
     Diffeomorph 𝓘(𝕜, E × E') (𝓘(𝕜, E).prod 𝓘(𝕜, E')) (E × E') (E × E') n where
   toEquiv := .refl _
   contMDiff_toFun :=
@@ -160,19 +160,19 @@ def Diffeomorph.modelWithCornersSelfProdComparison {n : WithTop ℕ∞} :
     exact contMDiff_id
 
 /-- The scalar multiplication `𝕜 × E → E` of any normed vector space `E` over `𝕜` is smooth. -/
-instance {n : WithTop ℕ∞} : ContMDiffSMul 𝓘(𝕜) 𝓘(𝕜, E) n 𝕜 E where
+instance {n : ℕ∞ω} : ContMDiffSMul 𝓘(𝕜) 𝓘(𝕜, E) n 𝕜 E where
   contMDiff_smul := by
     rw [← Diffeomorph.modelWithCornersSelfProdComparison.contMDiff_comp_diffeomorph_iff le_rfl]
     exact contDiff_smul.contMDiff
 
 /-- The monoid `E →L[𝕜] E` of continuous linear endomorphisms of `E` acts smoothly on `E`. -/
-instance [CompleteSpace E] {n : WithTop ℕ∞} :
+instance [CompleteSpace E] {n : ℕ∞ω} :
     ContMDiffSMul 𝓘(𝕜, E →L[𝕜] E) 𝓘(𝕜, E) n (E →L[𝕜] E) E where
   contMDiff_smul := by
     rw [← Diffeomorph.modelWithCornersSelfProdComparison.contMDiff_comp_diffeomorph_iff le_rfl]
     exact isBoundedBilinearMap_apply.contDiff.contMDiff
 
 /-- The general linear group `(E →L[𝕜] E)ˣ` on `E` acts smoothly on `E`. -/
-example [CompleteSpace E] (n : WithTop ℕ∞) :
+example [CompleteSpace E] (n : ℕ∞ω) :
     ContMDiffSMul 𝓘(𝕜, E →L[𝕜] E) 𝓘(𝕜, E) n (E →L[𝕜] E)ˣ E :=
   inferInstance
