b59431402753b07149c78777f58a6e3c0b60d670
Joël Riou <37772949+joelriou@users.noreply.github.com>
2026-04-28 09:23:09 +0200

    Update Mathlib/AlgebraicTopology/SimplexCategory/DeltaZeroIter.lean
    Co-authored-by: Robin Carlier <57142648+robin-carlier@users.noreply.github.com>

--- Mathlib/AlgebraicTopology/SimplexCategory/DeltaZeroIter.lean
+++ Mathlib/AlgebraicTopology/SimplexCategory/DeltaZeroIter.lean
@@ -129,7 +129,7 @@ lemma σ₀Iter_coe_eq_of_lt (i : ℕ) {n m : ℕ}
 lemma σ₀Iter_coe_eq_of_ge (i : ℕ) {n m : ℕ}
     (j : Fin (m + 1)) (hi : n + i = m := by lia) (hj : i ≤ j.val := by grind) :
     dsimp% (σ₀Iter i hi j).val = j.val - i := by
-  dsimp [σ₀Iter, Hom.mk, ConcreteCategory.hom, Hom.toOrderHom, DFunLike.coe]
+  dsimp [σ₀Iter, Hom.mk, ConcreteCategory.hom, Hom.toOrderHom]
   rw [if_neg (by lia)]
 
 lemma σ₀Iter_coe_eq_of_le (i : ℕ) {n m : ℕ}
