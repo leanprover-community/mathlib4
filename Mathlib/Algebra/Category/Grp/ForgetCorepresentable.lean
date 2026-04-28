5cb4cfaa0d61a9d7b46a8f3d32cb9a68f5b8a02b
Brian-Nugent <bnugent@uw.edu>
2026-04-28 09:04:33 -0600

    change name to uliftZMultiplesHom_apply_add

--- Mathlib/Algebra/Category/Grp/ForgetCorepresentable.lean
+++ Mathlib/Algebra/Category/Grp/ForgetCorepresentable.lean
@@ -81,13 +81,13 @@ instance AddCommGrpCat.forget_isCorepresentable :
     (forget AddCommGrpCat.{u}).IsCorepresentable :=
   Functor.IsCorepresentable.mk' AddCommGrpCat.coyonedaObjIsoForget
 
-theorem uliftZMultiplesHom.map_add (G : Type u) [AddCommGroup G] (x y : G) :
-    (uliftZMultiplesHom G) (x + y) = ((uliftZMultiplesHom G) x) + ((uliftZMultiplesHom G) y) := by
+theorem uliftZMultiplesHom_apply_add (G : Type u) [AddCommGroup G] (x y : G) :
+    uliftZMultiplesHom G (x + y) = uliftZMultiplesHom G x + uliftZMultiplesHom G y := by
   ext
   simp_all only [uliftZMultiplesHom_apply_apply, smul_add, AddMonoidHom.add_apply]
 
 /-- The additive equivalence `(ℤ ⟶ G) ≃+ G` -/
 @[simps!]
 def AddCommGrpCat.uliftZMultiplesAddEquiv (G : AddCommGrpCat) : (of (ULift ℤ) ⟶ G) ≃+ G :=
-  AddEquiv.trans (@AddCommGrpCat.homAddEquiv (AddCommGrpCat.of (ULift ℤ)) G)
-    (AddEquiv.mk' (uliftZMultiplesHom G) (uliftZMultiplesHom.map_add G)).symm
+  AddCommGrpCat.homAddEquiv.trans
+    (AddEquiv.mk' (uliftZMultiplesHom G) (uliftZMultiplesHom_apply_add G)).symm
