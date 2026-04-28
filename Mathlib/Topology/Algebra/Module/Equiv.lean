234b93db46ba8521fc13edfd46715313df64bbca
sharky564 <sharkesa@gmail.com>
2026-04-29 02:45:35 +1000

    Added LinearEquiv.isHomeomorph_iff

--- Mathlib/Topology/Algebra/Module/Equiv.lean
+++ Mathlib/Topology/Algebra/Module/Equiv.lean
@@ -25,6 +25,15 @@ open scoped Ring
 
 universe u v w u'
 
+/-- A linear equivalence between topological modules is a homeomorphism if and only if it is
+continuous in both directions. -/
+theorem LinearEquiv.isHomeomorph_iff {R S : Type*} [Semiring R] [Semiring S]
+    {σ : R →+* S} {σ' : S →+* R} [RingHomInvPair σ σ'] [RingHomInvPair σ' σ]
+    {M : Type*} [TopologicalSpace M] [AddCommMonoid M] [Module R M]
+    {N : Type*} [TopologicalSpace N] [AddCommMonoid N] [Module S N]
+    (e : M ≃ₛₗ[σ] N) : IsHomeomorph e ↔ Continuous e ∧ Continuous e.symm :=
+  e.toEquiv.isHomeomorph_iff
+
 section
 
 /-- Continuous linear equivalences between modules. We only put the type classes that are necessary
