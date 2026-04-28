c601cd84869d46ae22f825a713dd922bb8547605
Dagur Asgeirsson <25623829+dagurtomas@users.noreply.github.com>
2026-04-28 06:25:57 +0000

    feat(CategoryTheory): define profunctors (#38085)
    Co-authored-by: Adrian Marti <adrianmarti06@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -3202,6 +3202,7 @@ public import Mathlib.CategoryTheory.Products.Associator
 public import Mathlib.CategoryTheory.Products.Basic
 public import Mathlib.CategoryTheory.Products.Bifunctor
 public import Mathlib.CategoryTheory.Products.Unitor
+public import Mathlib.CategoryTheory.Profunctor.Basic
 public import Mathlib.CategoryTheory.Quotient
 public import Mathlib.CategoryTheory.Quotient.Linear
 public import Mathlib.CategoryTheory.Quotient.LocallySmall
