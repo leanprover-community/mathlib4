1804a9da0a99987827a41f2dff2d51125cc30b20
Nailin Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2026-04-28 05:36:41 +0000

    chore(RingTheory): rename `RingTheory.Regular.Depth` (#38411)
    Rename `RingTheory.Regular.Depth` to `RingTheory.Regular.LinearMap`
    The file currently only contain lemmas useful for the development of the notion of depth of a module. It was previously designed for containing the definition of `depth`, but it was decided to put it into a different folder `RingTheory.Depth`.
    
    Deprecation module would be added in up coming #38028

--- Mathlib.lean
+++ Mathlib.lean
@@ -6726,9 +6726,9 @@ public import Mathlib.RingTheory.Radical.Basic
 public import Mathlib.RingTheory.Radical.NatInt
 public import Mathlib.RingTheory.ReesAlgebra
 public import Mathlib.RingTheory.Regular.Category
-public import Mathlib.RingTheory.Regular.Depth
 public import Mathlib.RingTheory.Regular.Flat
 public import Mathlib.RingTheory.Regular.IsSMulRegular
+public import Mathlib.RingTheory.Regular.LinearMap
 public import Mathlib.RingTheory.Regular.ProjectiveDimension
 public import Mathlib.RingTheory.Regular.RegularSequence
 public import Mathlib.RingTheory.RegularLocalRing.Defs
