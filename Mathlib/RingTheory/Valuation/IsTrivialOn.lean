7dfc0e32ac5f0ae313ca6fb2b2fa827340ead4e8
Xavier Genereux <xaviergenereux@hotmail.com>
2026-04-28 13:28:49 +0200

    rename vars

--- Mathlib/RingTheory/Valuation/IsTrivialOn.lean
+++ Mathlib/RingTheory/Valuation/IsTrivialOn.lean
@@ -15,15 +15,15 @@ public import Mathlib.RingTheory.Valuation.Basic
 This file contains additional results about `Valuation.IsTrivialOn` which is defined in
 `Mathlib.RingTheory.Valuation.Basic`.
 
-In what follows, we consider a `K`-algebra `L` and a valuation `v` over `L` which is trivial on `K`.
+In what follows, we consider a `A`-algebra `B` and a valuation `v` over `B` which is trivial on `A`.
 
 ## Main results
 * `valuation_aeval_eq_valuation_X_pow_natDegree_of_one_lt_valuation_X`: Let `p` be a polynomial
-over `K` evaluated at an element of `L`. We have the equality `v (p.aeval w) = v w ^ p.natDegree`.
-* `Valuation.transcendental_of_lt_one`: If `y : L` is such that `y ≠ 0` and `v y < 1`,
-then it is transcendental over `K`.
+over `A` evaluated at an element of `B`. We have the equality `v (p.aeval w) = v w ^ p.natDegree`.
+* `Valuation.transcendental_of_lt_one`: If `y : B` is such that `y ≠ 0` and `v y < 1`,
+then it is transcendental over `A`.
 Note that this means that any non zero element of the maximal ideal of `v.valuationSubring` is
-transcendental over `K`.
+transcendental over `A`.
 -/
 
 @[expose] public section
@@ -32,17 +32,17 @@ variable {Γ : Type*} [LinearOrderedCommGroupWithZero Γ]
 
 section Ring
 
-variable (K : Type*) [CommSemiring K]
-variable (L : Type*) [Ring L] [Algebra K L] {v : Valuation L Γ} [hv : v.IsTrivialOn K]
+variable (A : Type*) [CommSemiring A]
+variable (B : Type*) [Ring B] [Algebra A B] {v : Valuation B Γ} [hv : v.IsTrivialOn A]
 
 namespace Polynomial
 
-lemma valuation_aeval_monomial_eq_valuation_pow (w : L) (n : ℕ) {a : K} (ha : a ≠ 0) :
+lemma valuation_aeval_monomial_eq_valuation_pow (w : B) (n : ℕ) {a : A} (ha : a ≠ 0) :
     v ((monomial n a).aeval w) = (v w) ^ n := by
   simp [← C_mul_X_pow_eq_monomial, map_mul, map_pow, one_mul, hv.eq_one a ha]
 
-theorem valuation_aeval_eq_valuation_X_pow_natDegree_of_one_lt_valuation_X (w : L) (hpos : 1 < v w)
-    {p : Polynomial K} (hp : p ≠ 0) : v (p.aeval w) = v w ^ p.natDegree := by
+theorem valuation_aeval_eq_valuation_X_pow_natDegree_of_one_lt_valuation_X (w : B) (hpos : 1 < v w)
+    {p : Polynomial A} (hp : p ≠ 0) : v (p.aeval w) = v w ^ p.natDegree := by
   rw [← valuation_aeval_monomial_eq_valuation_pow _ _ _ _ (leadingCoeff_ne_zero.mpr hp)]
   nth_rw 1 [as_sum_range p, map_sum]
   apply Valuation.map_sum_eq_of_lt _ (by simp)
@@ -62,16 +62,16 @@ end Ring
 
 section Field
 
-variable (K : Type*) [CommRing K]
-variable (L : Type*) [Field L] [Algebra K L] {v : Valuation L Γ} [hv : v.IsTrivialOn K]
+variable (A : Type*) [CommRing A]
+variable (K : Type*) [Field K] [Algebra A K] {v : Valuation K Γ} [hv : v.IsTrivialOn A]
 
 open Polynomial
 
 /--
-For a `K`-algebra `L` and a valuation `v` over `L` which is trivial on `K`.
-If `y : L` is such that `y ≠ 0` and `v y < 1`, then it is transcendental over `K`. -/
-theorem Valuation.transcendental_of_ne_one (y : L) (h0 : y ≠ 0) (hy : v y ≠ 1) :
-    Transcendental K y := by
+For a `A`-algebra `K` and a valuation `v` over `K` which is trivial on `A`.
+If `y : K` is such that `y ≠ 0` and `v y < 1`, then it is transcendental over `A`. -/
+theorem Valuation.transcendental_of_ne_one (y : K) (h0 : y ≠ 0) (hy : v y ≠ 1) :
+    Transcendental A y := by
   wlog! hlt : 1 < v y generalizing v y
   · rw [Transcendental, ← IsAlgebraic.inv_iff]
     apply this (v := v) _ (by simpa) (by simpa)
@@ -79,7 +79,7 @@ theorem Valuation.transcendental_of_ne_one (y : L) (h0 : y ≠ 0) (hy : v y ≠ 
     exact lt_of_le_of_ne hlt hy
   simp_all only [ne_eq, Transcendental]
   by_contra!
-  replace ha : IsAlgebraic K y := .algebraMap this
+  replace ha : IsAlgebraic A y := .algebraMap this
   obtain ⟨p, hpnt, hp⟩ := ha
   suffices v y ^ p.natDegree = 0 by simp_all
   rw [← valuation_aeval_eq_valuation_X_pow_natDegree_of_one_lt_valuation_X _ _ _ hlt] <;> simp_all
