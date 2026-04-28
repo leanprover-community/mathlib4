f183aacac2d7e3532773bc4e208c93ffabb655f7
Xavier Genereux <xaviergenereux@hotmail.com>
2026-04-28 13:26:37 +0200

    suggested changes

--- Mathlib/RingTheory/Valuation/IsTrivialOn.lean
+++ Mathlib/RingTheory/Valuation/IsTrivialOn.lean
@@ -12,6 +12,9 @@ public import Mathlib.RingTheory.Valuation.Basic
 /-!
 # Basic lemmas on valuations that are trivial over a base ring
 
+This file contains additional results about `Valuation.IsTrivialOn` which is defined in
+`Mathlib.RingTheory.Valuation.Basic`.
+
 In what follows, we consider a `K`-algebra `L` and a valuation `v` over `L` which is trivial on `K`.
 
 ## Main results
@@ -25,11 +28,11 @@ transcendental over `K`.
 
 @[expose] public section
 
-variable (K : Type*) [CommRing K]
 variable {Γ : Type*} [LinearOrderedCommGroupWithZero Γ]
 
 section Ring
 
+variable (K : Type*) [CommSemiring K]
 variable (L : Type*) [Ring L] [Algebra K L] {v : Valuation L Γ} [hv : v.IsTrivialOn K]
 
 namespace Polynomial
@@ -40,7 +43,7 @@ lemma valuation_aeval_monomial_eq_valuation_pow (w : L) (n : ℕ) {a : K} (ha : 
 
 theorem valuation_aeval_eq_valuation_X_pow_natDegree_of_one_lt_valuation_X (w : L) (hpos : 1 < v w)
     {p : Polynomial K} (hp : p ≠ 0) : v (p.aeval w) = v w ^ p.natDegree := by
-  rw [← valuation_aeval_monomial_eq_valuation_pow _ _ _ _ ((leadingCoeff_ne_zero).mpr hp)]
+  rw [← valuation_aeval_monomial_eq_valuation_pow _ _ _ _ (leadingCoeff_ne_zero.mpr hp)]
   nth_rw 1 [as_sum_range p, map_sum]
   apply Valuation.map_sum_eq_of_lt _ (by simp)
   intro i hi
@@ -59,6 +62,7 @@ end Ring
 
 section Field
 
+variable (K : Type*) [CommRing K]
 variable (L : Type*) [Field L] [Algebra K L] {v : Valuation L Γ} [hv : v.IsTrivialOn K]
 
 open Polynomial
@@ -66,15 +70,18 @@ open Polynomial
 /--
 For a `K`-algebra `L` and a valuation `v` over `L` which is trivial on `K`.
 If `y : L` is such that `y ≠ 0` and `v y < 1`, then it is transcendental over `K`. -/
-theorem Valuation.transcendental_of_lt_one (y : L) (h0 : y ≠ 0)
-    (hy : v y < 1) : Transcendental K y := by
+theorem Valuation.transcendental_of_ne_one (y : L) (h0 : y ≠ 0) (hy : v y ≠ 1) :
+    Transcendental K y := by
+  wlog! hlt : 1 < v y generalizing v y
+  · rw [Transcendental, ← IsAlgebraic.inv_iff]
+    apply this (v := v) _ (by simpa) (by simpa)
+    rw [← val_lt_one_iff _ h0]
+    exact lt_of_le_of_ne hlt hy
   simp_all only [ne_eq, Transcendental]
   by_contra!
-  rw [val_lt_one_iff _ (by contrapose! h0; aesop)] at hy
   replace ha : IsAlgebraic K y := .algebraMap this
-  rw [← IsAlgebraic.inv_iff] at ha
   obtain ⟨p, hpnt, hp⟩ := ha
-  suffices v y⁻¹ ^ p.natDegree = 0 by simp_all
-  rw [← valuation_aeval_eq_valuation_X_pow_natDegree_of_one_lt_valuation_X _ _ _ hy] <;> simp_all
+  suffices v y ^ p.natDegree = 0 by simp_all
+  rw [← valuation_aeval_eq_valuation_X_pow_natDegree_of_one_lt_valuation_X _ _ _ hlt] <;> simp_all
 
 end Field
