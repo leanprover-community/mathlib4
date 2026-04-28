d42778d9b1849f2c23eb86c0ab8efbf55d1e6ab2
Pepa Montero Jimena <pepamonterojimena@gmail.com>
2026-04-28 08:56:22 +0000

    feat: quotients of charted spaces by free properly discontinuous group actions are charted spaces (#35653)
    Add topological preliminaries for properly discontinuous group actions, and use them to equip the orbit space `orbitRel.Quotient G M` with a `ChartedSpace` structure.
    
    In particular:
    - Show that the quotient map `Quotient.mk _ : M → orbitRel.Quotient G M` is a covering map and hence a local homeomorphism.
    - Construct chosen local inverses of the quotient map and establish properties over them.
    - Define a `ChartedSpace` instance on the quotient by composing these local inverses with charts of M.
    
    Future PRs will upgrade this to a smooth manifold structure (assuming a smooth action).
    
    This started as an ItaLean2025 project; here is the [Zulip thread](https://leanprover.zulipchat.com/#narrow/channel/541885-ItaLean-2025/topic/Projects.3A.20Quotient.20Manifolds/with/575121158) dedicated to it.
    
    Co-authored by: Michael Rothgang <rothgang@math.uni-bonn.de>
    Co-authored by: Archie Browne @archiebrowne
    Co-authored by: Enrique Diaz Blanco @qdiazblanco
    Co-authored by: Juanjo Madrigal @juanjomadrigal
    
    Co-authored-by: Oliver Nash <github@olivernash.org>

--- Mathlib.lean
+++ Mathlib.lean
@@ -4519,6 +4519,7 @@ public import Mathlib.Geometry.Manifold.GroupLieAlgebra
 public import Mathlib.Geometry.Manifold.HasGroupoid
 public import Mathlib.Geometry.Manifold.Immersion
 public import Mathlib.Geometry.Manifold.Instances.Icc
+public import Mathlib.Geometry.Manifold.Instances.Quotient
 public import Mathlib.Geometry.Manifold.Instances.Real
 public import Mathlib.Geometry.Manifold.Instances.Sphere
 public import Mathlib.Geometry.Manifold.Instances.UnitsOfNormedAlgebra
