9267d279acb3be28f56341926edf23e6f2c64881
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2026-04-29 00:29:43 +0800

    golf

--- Mathlib/RingTheory/RegularLocalRing/GlobalDimension.lean
+++ Mathlib/RingTheory/RegularLocalRing/GlobalDimension.lean
@@ -98,21 +98,15 @@ theorem IsRegularRing.globalDimension_eq_ringKrullDim [Small.{v} R] [IsRegularRi
   rcases subsingleton_or_nontrivial R with sub|ntr
   · rw [(globalDimension_eq_bot_iff R).mpr sub, ringKrullDim_eq_bot_of_subsingleton]
   · rw [globalDimension_eq_iSup_loclization_maximal]
-    let _ : Nonempty ((Subtype (Ideal.IsMaximal (α := R)))) :=
-      nonempty_subtype.mpr (Ideal.exists_maximal R)
+    have := nonempty_subtype.mpr (Ideal.exists_maximal R)
     let f := fun (x : Subtype (Ideal.IsMaximal (α := R))) ↦ x.1.height
     have bdd : BddAbove (Set.range f) := by
-      have : ringKrullDim R ≠ ⊥ :=
-        ne_bot_of_le_ne_bot WithBot.zero_ne_bot ringKrullDim_nonneg_of_nontrivial
-      use (ringKrullDim R).unbot this
-      refine mem_upperBounds.mpr (fun x ⟨y, hy⟩ ↦ ?_)
-      simpa [← hy, WithBot.le_unbot_iff] using
-        Ideal.height_le_ringKrullDim_of_ne_top y.2.isPrime.ne_top
+      use ⊤
+      simp [mem_upperBounds]
     rw [← Ideal.sup_height_of_maximal_eq_ringKrullDim, iSup_subtype', WithBot.coe_iSup bdd]
     apply le_antisymm
     · simp only [iSup_le_iff]
       intro p
-      have := isRegularRing_iff.mp ‹_› p.1
       have : Small.{v} (Localization.AtPrime p.1) :=
         small_of_surjective Localization.mkHom_surjective
       rw [IsRegularLocalRing.globalDimension_eq_ringKrullDim.{u, v} (Localization.AtPrime p.1),
@@ -120,7 +114,6 @@ theorem IsRegularRing.globalDimension_eq_ringKrullDim [Small.{v} R] [IsRegularRi
       exact le_iSup (fun i ↦ (f i : WithBot ℕ∞)) ⟨p.1, p.2⟩
     · simp only [iSup_le_iff]
       intro ⟨p, hp⟩
-      have := isRegularRing_iff.mp ‹_› p
       have : Small.{v} (Localization.AtPrime p) :=
         small_of_surjective Localization.mkHom_surjective
       simp only [f, ← IsLocalization.AtPrime.ringKrullDim_eq_height p (Localization.AtPrime p),
