3b003252bbada8d9ed3ba2a1ae4c1b640dfe6190
Jacob Scharmberg <hi@jacobscharmberg.net>
2026-04-29 12:37:00 +0200

    generalize to arbitrary `ComplexShape`s

--- Mathlib/AlgebraicTopology/EilenbergSteenrod.lean
+++ Mathlib/AlgebraicTopology/EilenbergSteenrod.lean
@@ -12,6 +12,7 @@ public import Mathlib.Combinatorics.Quiver.ReflQuiver
 public import Mathlib.Order.BourbakiWitt
 public import Mathlib.Order.CompletePartialOrder
 public import Mathlib.Topology.Category.TopPair
+public import Mathlib.Algebra.Homology.ComplexShape
 
 /-!
 # Eilenberg-Steenrod homology theories
@@ -80,148 +81,147 @@ end TopPair
 universe u v
 
 variable {C : Type v} [Category C] [Limits.HasZeroMorphisms C]
-  {Hₚ Hₚ' : ℕ → TopPair.{u} ⥤ C} {H H' : ℕ → TopCat.{u} ⥤ C}
-  (H_incl_Hₚ : ∀ n : ℕ, H n ≅ incl ⋙ Hₚ n) (H'_incl_Hₚ' : ∀ n : ℕ, H' n ≅ incl ⋙ Hₚ' n)
-  {δ : (Π m n, (Hₚ m) ⟶ proj₂ ⋙ H n)}
+  {ι : Type*} (c : ComplexShape ι)
+  {Hₚ Hₚ' : ι → TopPair.{u} ⥤ C} {H H' : ι → TopCat.{u} ⥤ C}
+  (H_incl_Hₚ : ∀ i : ι, H i ≅ incl ⋙ Hₚ i) (H'_incl_Hₚ' : ∀ i : ι, H' i ≅ incl ⋙ Hₚ' i)
+  {δ : (∀ i j, (Hₚ i) ⟶ proj₂ ⋙ H j)} {δₚ : (∀ i j, (Hₚ i) ⟶ proj₂ ⋙ incl ⋙ Hₚ j)}
 
 /-- An extraordinary Eilenberg-Steenrod homology theory requires the homotopy, excision, additivity,
 and exactness axioms. -/
 structure IsExtraordinaryEilenbergSteenrod
-    (δ_zero : ∀ n m : ℕ, m ≠ n + 1 → δ m n = 0 := by cat_disch) where
+    (shape_δ : ∀ i j : ι, ¬ c.Rel i j → δ i j = 0 := by cat_disch) where
   /-- Invariance of an extraordinary Eilenberg-Steenrod homology theory on homotopic maps. -/
   homotopy ⦃X Y : TopPair⦄ (f g : X ⟶ Y) (hfg : Homotopic f g) :
-      ∀ (n : ℕ), (Hₚ n).map f = (Hₚ n).map g := by cat_disch
+      ∀ (i : ι), (Hₚ i).map f = (Hₚ i).map g := by cat_disch
   /-- Excision axiom of an extraordinary Eilenberg-Steenrod homology theory. -/
   excision ⦃X U V : TopPair⦄ (f : U ⟶ X) (g : V ⟶ X) (hf : IsEmbedding f) (hg : IsEmbedding g)
       (hcompl : TopPair.IsCompl f g)
       (hU : closure (Set.range (Hom.fst f)) ⊆ interior (Set.range X.map)) :
-        ∀ n : ℕ, IsIso ((Hₚ n).map g) := by intro n; infer_instance
+        ∀ i : ι, IsIso ((Hₚ i).map g) := by intro i; infer_instance
   /-- An extraordinary Eilenberg-Steenrod homology functor preserves colimits. -/
-  additive (J : Type u) : ∀ (n : ℕ), Limits.PreservesColimitsOfShape (Discrete J) (H n) := by
-    intro n
+  additive (J : Type u) : ∀ (i : ι), Limits.PreservesColimitsOfShape (Discrete J) (H i) := by
+    intro i
     infer_instance
   /-- The long exact sequence of topological pairs in an extraordinary Eilenberg-Steenrod homology
   theory. -/
-  exact (X : TopPair) : ∀ (n : ℕ),
-      (ComposableArrows.mk₄ ((Hₚ (n + 1)).map X.j) ((δ (n + 1) n).app X) ((H n).map X.map)
-        ((H_incl_Hₚ n).hom.app X.fst ≫ (Hₚ n).map X.j)).Exact := by cat_disch
-
-/-- An Eilenberg-Steenrod homology theory is an extraordinary Eilenberg-Steenrod homology theory
-which additionally satisfies the dimension axiom. -/
-structure IsEilenbergSteenrod (δ_zero : ∀ n m : ℕ, m ≠ n + 1 → δ m n = 0 := by cat_disch)
-    extends IsExtraordinaryEilenbergSteenrod.{u} H_incl_Hₚ δ_zero where
-  /-- An Eilenberg-Steenrod homology theory is trivial on the terminal space for `n > 0`. -/
-  dimension : ∀ (n : ℕ) (_ : n ≠ 0), Limits.IsZero ((H n).obj (TopCat.of PUnit)) := by cat_disch
-
-section
-
-variable {δ : (Π m n, (Hₚ m) ⟶ proj₂ ⋙ incl ⋙ Hₚ n)}
+  exact (X : TopPair) : ∀ (i : ι),
+      (ComposableArrows.mk₄ ((Hₚ (c.next i)).map X.j) ((δ (c.next i) i).app X) ((H i).map X.map)
+        ((H_incl_Hₚ i).hom.app X.fst ≫ (Hₚ i).map X.j)).Exact := by cat_disch
 
 /-- An extraordinary Eilenberg-Steenrod homology theory purely on pairs is an an extraordinary
 Eilenberg-Steenrod homology theory where `H = TopPair.incl ⋙ Hₚ`. -/
 abbrev IsExtraordinaryEilenbergSteenrodₚ
-    (δ_zero : ∀ n m : ℕ, m ≠ n + 1 → δ m n = 0 := by cat_disch) :=
-  IsExtraordinaryEilenbergSteenrod.{u} (fun _ ↦ Iso.refl _) (δ := δ) δ_zero
-
-/-- An Eilenberg-Steenrod homology theory purely on pairs is an extraordinary Eilenberg-Steenrod
-homology theory purely on pairs which additionally satisfies the dimension axiom. -/
-abbrev IsEilenbergSteenrodₚ (δ_zero : ∀ n m : ℕ, m ≠ n + 1 → δ m n = 0 := by cat_disch) :=
-  IsEilenbergSteenrod.{u} (fun _ ↦ Iso.refl _) δ_zero
-
-end
+    (shape_δₚ : ∀ i j : ι, ¬ c.Rel i j → δₚ i j = 0 := by cat_disch) :=
+  IsExtraordinaryEilenbergSteenrod.{u} c (fun _ ↦ Iso.refl _) (δ := δₚ) shape_δₚ
 
 set_option backward.isDefEq.respectTransparency false in
-lemma isExtraordinaryEilenbergSteenrod_iff_of_iso (HₚIso : ∀ n : ℕ,  (Hₚ n) ≅ (Hₚ' n))
-    (δ_zero : ∀ n m : ℕ, m ≠ n + 1 → δ m n = 0) :
-      IsExtraordinaryEilenbergSteenrod.{u} H_incl_Hₚ δ_zero ↔
-      IsExtraordinaryEilenbergSteenrod.{u} H'_incl_Hₚ'
-        (δ := (fun m n ↦ (HₚIso m).inv ≫ δ m n ≫ Functor.whiskerLeft proj₂ (H_incl_Hₚ n).hom ≫
-          Functor.whiskerLeft proj₂ (Functor.whiskerLeft incl (HₚIso n).hom) ≫
-          Functor.whiskerLeft proj₂ (H'_incl_Hₚ' n).inv))
+lemma isExtraordinaryEilenbergSteenrod_iff_of_iso (HₚIso : ∀ i : ι,  (Hₚ i) ≅ (Hₚ' i))
+    (shape_δ : ∀ i j : ι, ¬ c.Rel i j → δ i j = 0 := by cat_disch) :
+      IsExtraordinaryEilenbergSteenrod.{u} c H_incl_Hₚ shape_δ ↔
+      IsExtraordinaryEilenbergSteenrod.{u} c H'_incl_Hₚ'
+        (δ := (fun i j ↦ (HₚIso i).inv ≫ δ i j ≫ Functor.whiskerLeft proj₂ (H_incl_Hₚ j).hom ≫
+          Functor.whiskerLeft proj₂ (Functor.whiskerLeft incl (HₚIso j).hom) ≫
+          Functor.whiskerLeft proj₂ (H'_incl_Hₚ' j).inv))
         (by cat_disch) where
   mp hEES := {
-    homotopy X Y f g hfg n := by
-      have := hEES.homotopy f g hfg n
-      apply (((HₚIso n).app X).cancel_iso_hom_left ((Hₚ' n).map f) ((Hₚ' n).map g)).mp
-      simp only [CategoryTheory.Iso.app_hom, ← (HₚIso n).hom.naturality]
+    homotopy X Y f g hfg i := by
+      have := hEES.homotopy f g hfg i
+      apply (((HₚIso i).app X).cancel_iso_hom_left ((Hₚ' i).map f) ((Hₚ' i).map g)).mp
+      simp only [CategoryTheory.Iso.app_hom, ← (HₚIso i).hom.naturality]
       cat_disch
     excision _ _ _ _ _ hf hg hcompl hU _ :=
       (NatIso.isIso_map_iff (HₚIso _) _).mp (hEES.excision _ _ hf hg hcompl hU _)
     additive _ _ := @Limits.preservesColimitsOfShape_of_natIso _ _ _ _ _ _ _ _
       ((H_incl_Hₚ _) ≪≫ Functor.isoWhiskerLeft incl (HₚIso _) ≪≫ (H'_incl_Hₚ' _).symm)
       (hEES.additive _ _)
-    exact X n := by
-      let pairSeq := ComposableArrows.mk₄ ((Hₚ (n + 1)).map X.j)
-        ((δ (n + 1) n).app X) ((H n).map X.map) ((H_incl_Hₚ n).hom.app X.fst ≫ (Hₚ n).map X.j)
-      let δ' : (Π m n, (Hₚ' m) ⟶ proj₂ ⋙ H' n) :=
-        (fun m n ↦ (HₚIso m).inv ≫ δ m n ≫ Functor.whiskerLeft proj₂ (H_incl_Hₚ n).hom ≫
-          Functor.whiskerLeft proj₂ (Functor.whiskerLeft incl (HₚIso n).hom) ≫
-          Functor.whiskerLeft proj₂ (H'_incl_Hₚ' n).inv)
-      let pairSeq' := ComposableArrows.mk₄ ((Hₚ' (n + 1)).map X.j) ((δ' (n + 1) n).app X)
-        ((H' n).map X.map) ((H'_incl_Hₚ' n).hom.app X.fst ≫ (Hₚ' n).map X.j)
+    exact X i := by
+      let pairSeq := ComposableArrows.mk₄ ((Hₚ (c.next i)).map X.j)
+        ((δ (c.next i) i).app X) ((H i).map X.map) ((H_incl_Hₚ i).hom.app X.fst ≫ (Hₚ i).map X.j)
+      let δ' : (∀ i j, (Hₚ' i) ⟶ proj₂ ⋙ H' j) :=
+        (fun i j ↦ (HₚIso i).inv ≫ δ i j ≫ Functor.whiskerLeft proj₂ (H_incl_Hₚ j).hom ≫
+          Functor.whiskerLeft proj₂ (Functor.whiskerLeft incl (HₚIso j).hom) ≫
+          Functor.whiskerLeft proj₂ (H'_incl_Hₚ' j).inv)
+      let pairSeq' := ComposableArrows.mk₄ ((Hₚ' (c.next i)).map X.j) ((δ' (c.next i) i).app X)
+        ((H' i).map X.map) ((H'_incl_Hₚ' i).hom.app X.fst ≫ (Hₚ' i).map X.j)
       have pairSeqIso : pairSeq ≅ pairSeq' :=
         ComposableArrows.isoMk₄
-          ((HₚIso (n + 1)).app _)
-          ((HₚIso (n + 1)).app _)
-          (((H_incl_Hₚ n) ≪≫ Functor.isoWhiskerLeft incl (HₚIso _) ≪≫ (H'_incl_Hₚ' n).symm).app _)
-          (((H_incl_Hₚ n) ≪≫ Functor.isoWhiskerLeft incl (HₚIso _) ≪≫ (H'_incl_Hₚ' n).symm).app _)
-          ((HₚIso n).app _)
+          ((HₚIso (c.next i)).app _)
+          ((HₚIso (c.next i)).app _)
+          (((H_incl_Hₚ i) ≪≫ Functor.isoWhiskerLeft incl (HₚIso _) ≪≫ (H'_incl_Hₚ' i).symm).app _)
+          (((H_incl_Hₚ i) ≪≫ Functor.isoWhiskerLeft incl (HₚIso _) ≪≫ (H'_incl_Hₚ' i).symm).app _)
+          ((HₚIso i).app _)
           (by cat_disch)
           (by simp [pairSeq, pairSeq', δ', ComposableArrows.Precomp.map])
           (by simp [pairSeq, pairSeq', ComposableArrows.Precomp.map,
-            ← (H'_incl_Hₚ' n).inv.naturality])
+            ← (H'_incl_Hₚ' i).inv.naturality])
           (by simp [pairSeq, pairSeq', ComposableArrows.Precomp.map])
       exact ComposableArrows.exact_of_iso pairSeqIso (hEES.exact _ _)
   }
   mpr hEES := {
-    homotopy X Y f g hfg n := by
-      have := hEES.homotopy f g hfg n
-      apply (((HₚIso n).symm.app X).cancel_iso_hom_left ((Hₚ n).map f) ((Hₚ n).map g)).mp
-      simp only [CategoryTheory.Iso.app_hom, ← (HₚIso n).symm.hom.naturality]
+    homotopy X Y f g hfg i := by
+      have := hEES.homotopy f g hfg i
+      apply (((HₚIso i).symm.app X).cancel_iso_hom_left ((Hₚ i).map f) ((Hₚ i).map g)).mp
+      simp only [CategoryTheory.Iso.app_hom, ← (HₚIso i).symm.hom.naturality]
       cat_disch
     excision _ _ _ _ _ hf hg hcompl hU _ :=
       (NatIso.isIso_map_iff (HₚIso _).symm _).mp (hEES.excision _ _ hf hg hcompl hU _)
     additive _ _ := @Limits.preservesColimitsOfShape_of_natIso _ _ _ _ _ _ _ _
       ((H'_incl_Hₚ' _) ≪≫ Functor.isoWhiskerLeft incl (HₚIso _).symm ≪≫(H_incl_Hₚ _).symm)
       (hEES.additive _ _)
-    exact X n := by
-      let pairSeq := ComposableArrows.mk₄ ((Hₚ (n + 1)).map X.j) ((δ (n + 1) n).app X)
-        ((H n).map X.map) ((H_incl_Hₚ n).hom.app X.fst ≫ (Hₚ n).map X.j)
-      let δ' : (Π m n, (Hₚ' m) ⟶ proj₂ ⋙ H' n) := (fun m n ↦ (HₚIso m).inv ≫ δ m n ≫
-        Functor.whiskerLeft proj₂ (H_incl_Hₚ n).hom ≫
-        Functor.whiskerLeft proj₂ (Functor.whiskerLeft incl (HₚIso n).hom) ≫
-        Functor.whiskerLeft proj₂ (H'_incl_Hₚ' n).inv)
-      let pairSeq' := ComposableArrows.mk₄ ((Hₚ' (n + 1)).map X.j) ((δ' (n + 1) n).app X)
-        ((H' n).map X.map) ((H'_incl_Hₚ' n).hom.app X.fst ≫ (Hₚ' n).map X.j)
+    exact X i := by
+      let pairSeq := ComposableArrows.mk₄ ((Hₚ (c.next i)).map X.j) ((δ (c.next i) i).app X)
+        ((H i).map X.map) ((H_incl_Hₚ i).hom.app X.fst ≫ (Hₚ i).map X.j)
+      let δ' : (∀ i j, (Hₚ' i) ⟶ proj₂ ⋙ H' j) := (fun i j ↦ (HₚIso i).inv ≫ δ i j ≫
+        Functor.whiskerLeft proj₂ (H_incl_Hₚ j).hom ≫
+        Functor.whiskerLeft proj₂ (Functor.whiskerLeft incl (HₚIso j).hom) ≫
+        Functor.whiskerLeft proj₂ (H'_incl_Hₚ' j).inv)
+      let pairSeq' := ComposableArrows.mk₄ ((Hₚ' (c.next i)).map X.j) ((δ' (c.next i) i).app X)
+        ((H' i).map X.map) ((H'_incl_Hₚ' i).hom.app X.fst ≫ (Hₚ' i).map X.j)
       have pairSeqIso : pairSeq ≅ pairSeq' :=
         ComposableArrows.isoMk₄
-          ((HₚIso (n + 1)).app _)
-          ((HₚIso (n + 1)).app _)
-          (((H_incl_Hₚ n) ≪≫ Functor.isoWhiskerLeft incl (HₚIso _) ≪≫ (H'_incl_Hₚ' n).symm).app _)
-          (((H_incl_Hₚ n) ≪≫ Functor.isoWhiskerLeft incl (HₚIso _) ≪≫ (H'_incl_Hₚ' n).symm).app _)
-          ((HₚIso n).app _)
+          ((HₚIso (c.next i)).app _)
+          ((HₚIso (c.next i)).app _)
+          (((H_incl_Hₚ i) ≪≫ Functor.isoWhiskerLeft incl (HₚIso _) ≪≫ (H'_incl_Hₚ' i).symm).app _)
+          (((H_incl_Hₚ i) ≪≫ Functor.isoWhiskerLeft incl (HₚIso _) ≪≫ (H'_incl_Hₚ' i).symm).app _)
+          ((HₚIso i).app _)
           (by cat_disch)
           (by simp [pairSeq, pairSeq', δ', ComposableArrows.Precomp.map])
           (by simp [pairSeq, pairSeq', ComposableArrows.Precomp.map,
-            ← (H'_incl_Hₚ' n).inv.naturality])
+            ← (H'_incl_Hₚ' i).inv.naturality])
           (by simp [pairSeq, pairSeq', ComposableArrows.Precomp.map])
       exact ComposableArrows.exact_of_iso pairSeqIso.symm (hEES.exact _ _)
   }
 
+variable {Hₚ Hₚ' : ℕ → TopPair.{u} ⥤ C} {H H' : ℕ → TopCat.{u} ⥤ C}
+  (H_incl_Hₚ : ∀ n : ℕ, H n ≅ incl ⋙ Hₚ n) (H'_incl_Hₚ' : ∀ n : ℕ, H' n ≅ incl ⋙ Hₚ' n)
+  {δ : (∀ m n, (Hₚ m) ⟶ proj₂ ⋙ H n)} {δₚ : (∀ m n, (Hₚ m) ⟶ proj₂ ⋙ incl ⋙ Hₚ n)}
+
+/-- An Eilenberg-Steenrod homology theory is an extraordinary Eilenberg-Steenrod homology theory
+which additionally satisfies the dimension axiom. -/
+structure IsEilenbergSteenrod (shape_δ : (∀ (m n : ℕ), ¬(ComplexShape.down ℕ).Rel m n → δ m n = 0)  := by cat_disch)
+    extends IsExtraordinaryEilenbergSteenrod.{u} (ComplexShape.down ℕ) H_incl_Hₚ shape_δ where
+  /-- An Eilenberg-Steenrod homology theory is trivial on the terminal space for `n > 0`. -/
+  dimension : ∀ (n : ℕ) (_ : n ≠ 0), Limits.IsZero ((H n).obj (TopCat.of PUnit)) := by cat_disch -- TODO: generalize beyond ComplexShape.down ℕ?
+
+/-- An Eilenberg-Steenrod homology theory purely on pairs is an extraordinary Eilenberg-Steenrod
+homology theory purely on pairs which additionally satisfies the dimension axiom. -/
+abbrev IsEilenbergSteenrodₚ (shape_δₚ : (∀ (m n : ℕ), ¬(ComplexShape.down ℕ).Rel m n → δₚ m n = 0)  := by cat_disch) :=
+  IsEilenbergSteenrod.{u} (fun _ ↦ Iso.refl _) shape_δₚ
+
 lemma isEilenbergSteenrod_iff_of_iso (HₚIso : ∀ n : ℕ,  (Hₚ n) ≅ (Hₚ' n))
-    (δ_zero : ∀ n m : ℕ, m ≠ n + 1 → δ m n = 0) : IsEilenbergSteenrod H_incl_Hₚ δ_zero ↔
+    (shape_δ : ∀ n m : ℕ, ¬ (ComplexShape.down ℕ).Rel n m → δ n m = 0 := by cat_disch) : IsEilenbergSteenrod H_incl_Hₚ shape_δ ↔
       IsEilenbergSteenrod H'_incl_Hₚ'
-        (δ := (fun m n ↦ (HₚIso m).inv ≫ δ m n ≫ Functor.whiskerLeft proj₂ (H_incl_Hₚ n).hom ≫
-          Functor.whiskerLeft proj₂ (Functor.whiskerLeft incl (HₚIso n).hom) ≫
-          Functor.whiskerLeft proj₂ (H'_incl_Hₚ' n).inv))
+        (δ := (fun n m ↦ (HₚIso n).inv ≫ δ n m ≫ Functor.whiskerLeft proj₂ (H_incl_Hₚ m).hom ≫
+          Functor.whiskerLeft proj₂ (Functor.whiskerLeft incl (HₚIso m).hom) ≫
+          Functor.whiskerLeft proj₂ (H'_incl_Hₚ' m).inv))
         (by cat_disch) where
   mp hES := {
-    1 := (isExtraordinaryEilenbergSteenrod_iff_of_iso H_incl_Hₚ H'_incl_Hₚ' HₚIso δ_zero).mp hES.1
+    1 := (isExtraordinaryEilenbergSteenrod_iff_of_iso (ComplexShape.down ℕ) H_incl_Hₚ H'_incl_Hₚ' HₚIso shape_δ).mp hES.1
     dimension n hn := (Iso.isZero_iff (((H_incl_Hₚ _) ≪≫ Functor.isoWhiskerLeft incl (HₚIso _) ≪≫
         (H'_incl_Hₚ' _).symm).app (TopCat.of PUnit))).mp (hES.dimension n hn)
   }
   mpr hES := {
-    1 := (isExtraordinaryEilenbergSteenrod_iff_of_iso H_incl_Hₚ H'_incl_Hₚ' HₚIso δ_zero).mpr hES.1
+    1 := (isExtraordinaryEilenbergSteenrod_iff_of_iso (ComplexShape.down ℕ) H_incl_Hₚ H'_incl_Hₚ' HₚIso shape_δ).mpr hES.1
     dimension n hn := (Iso.isZero_iff (((H'_incl_Hₚ' _) ≪≫
         Functor.isoWhiskerLeft incl (HₚIso _).symm ≪≫ (H_incl_Hₚ _).symm).app (TopCat.of PUnit))).mp
       (hES.dimension n hn)
