e5554d7d0fccee0db2ace31324430589da562e73
Andrew Yang <36414270+erdOne@users.noreply.github.com>
2026-04-30 17:15:23 +0100

    Update Mathlib/LinearAlgebra/Dimension/FreeAndStrongRankCondition.lean
    Co-authored-by: Thomas Browning <tb65536@users.noreply.github.com>

--- Mathlib/LinearAlgebra/Dimension/FreeAndStrongRankCondition.lean
+++ Mathlib/LinearAlgebra/Dimension/FreeAndStrongRankCondition.lean
@@ -297,12 +297,9 @@ attribute [simp] bot_eq_top_of_finrank_eq_one bot_eq_top_of_rank_eq_one
 
 lemma _root_.Algebra.finrank_eq_one_iff_bijective_algebraMap [Module.Free F E] :
     Module.finrank F E = 1 ↔ Function.Bijective (algebraMap F E) := by
-  cases subsingleton_or_nontrivial E
-  · simp only [finrank_eq_zero_of_subsingleton, zero_ne_one, false_iff]
-    have : Nontrivial F := nontrivial_of_invariantBasisNumber F
-    exact fun h ↦ not_nontrivial E h.injective.nontrivial
-  rw [← Subalgebra.bot_eq_top_iff_finrank_eq_one, Function.Bijective,
-    Algebra.surjective_algebraMap_iff, eq_comm]
-  simp [FaithfulSMul.algebraMap_injective]
+  refine ⟨?_, Module.finrank_of_bijective_algebraMap⟩
+  nontriviality E
+  refine fun h ↦ ⟨FaithfulSMul.algebraMap_injective F E, ?_⟩
+  rwa [Algebra.surjective_algebraMap_iff, eq_comm, Subalgebra.bot_eq_top_iff_finrank_eq_one]
 
 end Subalgebra
