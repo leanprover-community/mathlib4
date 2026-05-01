beec1f6188ada563a63bd82176410f65dbf9c1d2
Martin Winter <martin.winter.math@gmail.com>
2026-05-01 09:11:58 +0200

    mk_all

--- Mathlib.lean
+++ Mathlib.lean
@@ -4292,6 +4292,7 @@ public import Mathlib.Data.Set.SymmDiff
 public import Mathlib.Data.Set.UnionLift
 public import Mathlib.Data.SetLike.Basic
 public import Mathlib.Data.SetLike.Fintype
+public import Mathlib.Data.SetLike.Pointwise
 public import Mathlib.Data.Setoid.Basic
 public import Mathlib.Data.Setoid.Partition
 public import Mathlib.Data.Setoid.Partition.Card
