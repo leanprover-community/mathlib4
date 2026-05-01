854dcde54cea40774b4494ac3ad44eba77531126
Yongle Hu <140475041+mbkybky@users.noreply.github.com>
2026-05-01 23:00:03 +0800

    remove instance name

--- Mathlib/Algebra/Module/StablyFree/Basic.lean
+++ Mathlib/Algebra/Module/StablyFree/Basic.lean
@@ -68,10 +68,10 @@ instance [Free R M] : IsStablyFree R M :=
   ⟨PUnit, inferInstance, inferInstance, inferInstance, inferInstance, inferInstance⟩
 
 theorem IsStablyFree.of_free_prod [Module.Finite R N] [Free R N] [Free R (M × N)] :
-    IsStablyFree R M := by
+    IsStablyFree R M :=
   have : Small.{u} N := Module.Finite.small.{u} R N
   let +nondep eN : N ≃ₗ[R] Shrink.{u} N := (Shrink.linearEquiv R N).symm
-  exact ⟨Shrink.{u} N, inferInstance, inferInstance, Module.Finite.equiv eN,
+  ⟨Shrink.{u} N, inferInstance, inferInstance, Module.Finite.equiv eN,
     Free.of_equiv eN, Free.of_equiv ((LinearEquiv.refl R M).prodCongr eN)⟩
 
 theorem IsStablyFree.of_free_prod' [Module.Finite R N] [Free R N] [Free R (N × M)] :
@@ -79,7 +79,7 @@ theorem IsStablyFree.of_free_prod' [Module.Finite R N] [Free R N] [Free R (N × 
   have : Free R (M × N) := Free.of_equiv (LinearEquiv.prodComm R N M)
   .of_free_prod R M N
 
-instance (priority := low) IsStablyFree.projective [IsStablyFree R M] : Projective R M := by
+instance (priority := low) [IsStablyFree R M] : Projective R M := by
   obtain ⟨N, _, _, _, _, _⟩ := IsStablyFree.exist_free_prod R M
   exact Projective.of_split (LinearMap.inl R M N) (LinearMap.fst R M N) (LinearMap.ext fun _ ↦ rfl)
 
