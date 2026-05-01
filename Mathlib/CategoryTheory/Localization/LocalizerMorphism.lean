63c492914a018b2cd602e1035f5e8bba31ec9bba
Joël Riou <joel.riou@universite-paris-saclay.fr>
2026-05-01 11:56:59 +0200

    feat(CategoryTheory): localizer morphisms which induces fully faithful functors on localized categories

--- Mathlib/CategoryTheory/Localization/LocalizerMorphism.lean
+++ Mathlib/CategoryTheory/Localization/LocalizerMorphism.lean
@@ -136,6 +136,29 @@ lemma isEquivalence_iff : G.IsEquivalence ↔ G'.IsEquivalence :=
   ⟨fun _ => Φ.isEquivalence_imp L₁ L₂ G L₁' L₂' G',
     fun _ => Φ.isEquivalence_imp L₁' L₂' G' L₁ L₂ G⟩
 
+/-- If a localizer morphism induces a fully faithful functor on some choice of
+localized categories, it will be so for any choice of localized categories. -/
+noncomputable def fullyFaithfulImp (hG : G.FullyFaithful) : G'.FullyFaithful :=
+  let E₁ := Localization.uniq L₁ L₁' W₁
+  let E₂ := Localization.uniq L₂ L₂' W₂
+  let e : L₁ ⋙ G ⋙ E₂.functor ≅ L₁ ⋙ E₁.functor ⋙ G' :=
+    calc
+      L₁ ⋙ G ⋙ E₂.functor ≅ Φ.functor ⋙ L₂ ⋙ E₂.functor :=
+          (associator _ _ _).symm ≪≫
+            isoWhiskerRight (CatCommSq.iso Φ.functor L₁ L₂ G).symm E₂.functor ≪≫
+            associator _ _ _
+      _ ≅ Φ.functor ⋙ L₂' := isoWhiskerLeft Φ.functor (compUniqFunctor L₂ L₂' W₂)
+      _ ≅ L₁' ⋙ G' := CatCommSq.iso Φ.functor L₁' L₂' G'
+      _ ≅ L₁ ⋙ E₁.functor ⋙ G' :=
+            isoWhiskerRight (compUniqFunctor L₁ L₁' W₁).symm G' ≪≫ associator _ _ _
+  (E₁.fullyFaithfulInverse.comp (hG.comp E₂.fullyFaithfulFunctor)).ofIso
+    ((isoWhiskerLeft (E₁.inverse) (liftNatIso L₁ W₁ _ _ (G ⋙ E₂.functor) (E₁.functor ⋙ G') e) ≪≫
+    (associator _ _ _).symm ≪≫ isoWhiskerRight E₁.counitIso G' ≪≫ G'.leftUnitor))
+
+lemma nonempty_fullyFaithful_iff : Nonempty G.FullyFaithful ↔ Nonempty G'.FullyFaithful :=
+  ⟨fun ⟨h⟩ => ⟨Φ.fullyFaithfulImp L₁ L₂ G L₁' L₂' G' h⟩,
+    fun ⟨h⟩ => ⟨Φ.fullyFaithfulImp L₁' L₂' G' L₁ L₂ G h⟩⟩
+
 end
 
 /-- Condition that a `LocalizerMorphism` induces an equivalence on the localized categories -/
@@ -233,6 +256,96 @@ instance IsLocalizedEquivalence.comp [Φ.IsLocalizedEquivalence]
   have : ((Φ.comp Ψ).functor ⋙ W₃.Q).IsLocalization W₁ :=
     Functor.IsLocalization.of_iso _ (Functor.associator _ _ _).symm
   of_isLocalization_of_isLocalization _ W₃.Q
+
+/-- Condition that a `LocalizerMorphism` induces a fully faithful functor
+on the localized categories -/
+class IsLocalizedFullyFaithful : Prop where
+  /-- the induced functor on the constructed localized categories is fully faithful -/
+  nonempty_fullyFaithful : Nonempty (Φ.localizedFunctor W₁.Q W₂.Q).FullyFaithful
+
+lemma IsLocalizedFullyFaithful.mk' [CatCommSq Φ.functor L₁ L₂ G] (hG : G.FullyFaithful) :
+    Φ.IsLocalizedFullyFaithful where
+  nonempty_fullyFaithful := by
+    rw [Φ.nonempty_fullyFaithful_iff W₁.Q W₂.Q (Φ.localizedFunctor W₁.Q W₂.Q) L₁ L₂ G]
+    exact ⟨hG⟩
+
+instance [Φ.IsLocalizedEquivalence] : Φ.IsLocalizedFullyFaithful where
+  nonempty_fullyFaithful := ⟨Functor.FullyFaithful.ofFullyFaithful _⟩
+
+/-- If a `LocalizerMorphism` becomes a fully faithful after localization, then any compatible
+functor between the localized categories is fully faithful. -/
+noncomputable irreducible_def fullyFaithful
+    [h : Φ.IsLocalizedFullyFaithful] [CatCommSq Φ.functor L₁ L₂ G] :
+    G.FullyFaithful := (Nonempty.some (by
+  rw [Φ.nonempty_fullyFaithful_iff L₁ L₂ G W₁.Q W₂.Q (Φ.localizedFunctor W₁.Q W₂.Q)]
+  exact h.nonempty_fullyFaithful))
+
+lemma faithful [Φ.IsLocalizedFullyFaithful] [CatCommSq Φ.functor L₁ L₂ G] :
+    G.Faithful :=
+  (Φ.fullyFaithful L₁ L₂ G).faithful
+
+lemma full [Φ.IsLocalizedFullyFaithful] [CatCommSq Φ.functor L₁ L₂ G] :
+    G.Full :=
+  (Φ.fullyFaithful L₁ L₂ G).full
+
+/-- If a `LocalizerMorphism` becomes fully faithful after localization,
+then the induced functor on the localized categories is fully faithful. -/
+noncomputable irreducible_def fullyFaithfulLocalizedFunctor [Φ.IsLocalizedFullyFaithful] :
+    (Φ.localizedFunctor L₁ L₂).FullyFaithful :=
+  Φ.fullyFaithful L₁ L₂ _
+
+instance [Φ.IsLocalizedFullyFaithful] : (Φ.localizedFunctor L₁ L₂).Full :=
+  Φ.full L₁ L₂ _
+
+instance [Φ.IsLocalizedFullyFaithful] : (Φ.localizedFunctor L₁ L₂).Faithful :=
+  Φ.faithful L₁ L₂ _
+
+instance [Φ.IsLocalizedFullyFaithful] : Φ.op.IsLocalizedFullyFaithful := by
+  let G := Φ.localizedFunctor W₁.Q W₂.Q
+  letI : CatCommSq Φ.op.functor W₁.Q.op W₂.Q.op G.op :=
+    ⟨NatIso.op (CatCommSq.iso Φ.functor W₁.Q W₂.Q G).symm⟩
+  exact IsLocalizedFullyFaithful.mk' Φ.op W₁.Q.op W₂.Q.op G.op
+    (Φ.fullyFaithful W₁.Q W₂.Q G).op
+
+/-- Assume that a localizer morphism `Φ : LocalizerMorphism W₁ W₂` induces
+a fully faithful functor on the localized categories.
+If `L₂ : C₂ ⥤ D₂` is a localization functor for `W₂` and we have a
+factorization `iso : Φ.functor ⋙ L₂ ≅ L₁ ⋙ F` as an essentially surjective
+functor `L₁ : C₁ ⥤ D₁` followed by a fully faithful functor `F : D₁ ⥤ D₂`,
+then `L₁` is a localization functor for `W₁`. -/
+lemma isLocalization_of_isLocalizedFullyFaithful
+    [Φ.IsLocalizedFullyFaithful] {L₂ : C₂ ⥤ D₂} [L₂.IsLocalization W₂]
+    {L₁ : C₁ ⥤ D₁} {F : D₁ ⥤ D₂}
+    (iso : Φ.functor ⋙ L₂ ≅ L₁ ⋙ F)
+    [F.Full] [F.Faithful] [L₁.EssSurj] :
+    L₁.IsLocalization W₁ := by
+  have h : W₁.IsInvertedBy L₁ := fun _ _ f hf ↦ by
+    rw [← isIso_iff_of_reflects_iso  _ F]
+    exact ((MorphismProperty.isomorphisms _).arrow_mk_iso_iff
+      (Arrow.isoOfNatIso iso f)).1 (Localization.inverts L₂ W₂ _ (Φ.map _ hf))
+  let G := Localization.lift L₁ h W₁.Q
+  let e : W₁.Q ⋙ G ≅ L₁ := Localization.fac L₁ h W₁.Q
+  letI : CatCommSq Φ.functor W₁.Q L₂ (G ⋙ F) :=
+    ⟨iso ≪≫ isoWhiskerRight e.symm _ ≪≫ associator _ _ _⟩
+  have hG : G.FullyFaithful := Functor.FullyFaithful.ofCompFaithful
+    (Φ.fullyFaithful W₁.Q L₂ (G ⋙ F))
+  have := hG.full
+  have := hG.faithful
+  have : G.EssSurj :=
+    ⟨fun X ↦ ⟨W₁.Q.obj (L₁.objPreimage X), ⟨e.app _ ≪≫ L₁.objObjPreimageIso X⟩⟩⟩
+  have : G.IsEquivalence := { }
+  exact IsLocalization.of_equivalence_target W₁.Q W₁ L₁ G.asEquivalence e
+
+instance IsLocalizedFullyFaithful.comp
+    (Ψ : LocalizerMorphism W₂ W₃)
+    [Φ.IsLocalizedFullyFaithful] [Ψ.IsLocalizedFullyFaithful] :
+    (Φ.comp Ψ).IsLocalizedFullyFaithful :=
+  letI : CatCommSq (Φ.comp Ψ).functor W₁.Q W₃.Q
+      (Φ.localizedFunctor W₁.Q W₂.Q ⋙ Ψ.localizedFunctor W₂.Q W₃.Q) :=
+    CatCommSq.hComp _ _ _ W₂.Q _ _ _
+  IsLocalizedFullyFaithful.mk' _ W₁.Q W₃.Q _
+    ((Φ.fullyFaithfulLocalizedFunctor W₁.Q W₂.Q).comp
+      (Ψ.fullyFaithfulLocalizedFunctor W₂.Q W₃.Q))
 
 /-- The localizer morphism from `W₁.arrow` to `W₂.arrow` that is induced by
 `Φ : LocalizerMorphism W₁ W₂`. -/
