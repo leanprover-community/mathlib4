a98e98621e8c0fc49bdc729ca6eecc8558a46da3
Joël Riou <joel.riou@universite-paris-saclay.fr>
2026-05-02 23:14:34 +0200

    one more test

--- MathlibTest/CategoryTheory/FunctorAssoc.lean
+++ MathlibTest/CategoryTheory/FunctorAssoc.lean
@@ -1,12 +1,12 @@
 import Mathlib.CategoryTheory.Functor.Basic
 
-namespace CategoryTheory
+namespace CategoryTheory.Functor
 
 variable {C₁ C₂ C₃ C₄ : Type*}
   [Category* C₁] [Category* C₂] [Category* C₃] [Category* C₄]
 
 class Foo (F : C₁ ⥤ C₂) where
-  data : ℕ
+  data (F) : ℕ
 
 variable (F : C₁ ⥤ C₂) (G : C₂ ⥤ C₃) (H : C₃ ⥤ C₄)
 
@@ -17,28 +17,43 @@ for functors such that an instance for a composition of functors
 can be inferred from an instance for each of the functors.
 Taking into account the placement of parentheses, we want to allow
 `(F ⋙ G) ⋙ H` and `F ⋙ (G ⋙ H)` to have their own instances
-(even though they are usually propositionally equal). The following
-tests ensure that for the typeclass `Foo`, no instance
-is inferred on `(F ⋙ G) ⋙ H` from an instance on `F ⋙ (G ⋙ H)`,
+(even though they are usually propositionally equal).
+-/
+
+/-! The two following tests ensure that for the typeclass `Foo`,
+no instance is inferred on `(F ⋙ G) ⋙ H` from an instance on `F ⋙ (G ⋙ H)`,
 and vice versa.
 -/
 
 /--
 error: failed to synthesize instance of type class
-  Foo ((F ⋙ G) ⋙ H)
+  ((F ⋙ G) ⋙ H).Foo
 
 Hint: Type class instance resolution failures can be inspected with the `set_option trace.Meta.synthInstance true` command.
 -/
 #guard_msgs in
-example [Foo (F ⋙ (G ⋙ H))] : Foo ((F ⋙ G) ⋙ H) := inferInstance
+example [(F ⋙ (G ⋙ H)).Foo] : ((F ⋙ G) ⋙ H).Foo := inferInstance
 
 /--
 error: failed to synthesize instance of type class
-  Foo (F ⋙ G ⋙ H)
+  (F ⋙ G ⋙ H).Foo
 
 Hint: Type class instance resolution failures can be inspected with the `set_option trace.Meta.synthInstance true` command.
 -/
 #guard_msgs in
-example [Foo ((F ⋙ G) ⋙ H)] : Foo (F ⋙ (G ⋙ H)) := inferInstance
+example [((F ⋙ G) ⋙ H).Foo] : (F ⋙ (G ⋙ H)).Foo := inferInstance
 
-end CategoryTheory
+/-! Assuming a `Foo` instance for a composition of functors can be obtained from a
+`Foo` instance on each functor (by taking the sum in `ℕ`), we check that the
+instances obtained on `(F ⋙ G) ⋙ H` and `F ⋙ (G ⋙ H)` are equal,
+but not defeq (because `Nat.assoc` is not a defeq in general). -/
+
+instance [F.Foo] [G.Foo] : (F ⋙ G).Foo where
+  data := Foo.data F + Foo.data G
+
+/- This must succeed with `Nat.add_assoc` as a proof. It would be problematic
+if `rfl` worked. -/
+example [F.Foo] [G.Foo] [H.Foo] :
+    Foo.data ((F ⋙ G) ⋙ H) = Foo.data (F ⋙ (G ⋙ H)) := Nat.add_assoc _ _ _
+
+end CategoryTheory.Functor
