7e8b344982586cdb126b04b791442a432b178218
thorimur <68410468+thorimur@users.noreply.github.com>
2026-05-03 19:38:20 -0400

    chore: long lines

--- Mathlib/Util/Scope.lean
+++ Mathlib/Util/Scope.lean
@@ -12,9 +12,15 @@ public meta import Aesop.Util.Basic -- Name.ofComponents...
 /-!
 # Scope manipulation tools
 
-This module provides utilities for fully capturing the effectful parts of the current scope (namespaces, open declarations, options, etc.) and transporting it between modules. Specifically, it provides `#scope`, which reifies the semantically-relevant parts of the current scope into portable syntax capturing the current scope that may be moved between files.
+This module provides utilities for fully capturing the effectful parts of the current scope
+(namespaces, open declarations, options, etc.) and transporting it between modules. Specifically,
+it provides `#scope`, which reifies the semantically-relevant parts of the current scope into
+portable syntax capturing the current scope that may be moved between files.
 
-This is not intended to capture `section` behavior, nor is it intended to be used for user-friendly visibility into the current scope structure. The reified scope specification is primarily intended to be easy to act on with metaprogramming automation. In this sense it is not trying to replace `#where`.
+This is not intended to capture `section` behavior, nor is it intended to be used for user-friendly
+visibility into the current scope structure. The reified scope specification is primarily intended
+to be easy to act on with metaprogramming automation. In this sense it is not trying to replace
+`#where`.
 
 ## TODO
 
@@ -46,15 +52,20 @@ syntax reifiedOpenDecl := ppSpace colGt
 only renders fully-resolved namespaces. Surrounded by `()` when `hiding` is present. Uses `→` to
 render the mappings produced by `open ns renaming from → to, ...` and
 `open ns (id₁ id₂ ...)`. -/
-syntax reifiedOpenStx := withPosition(atomic("open" notFollowedBy("scoped")) ppIndent(reifiedOpenDecl*))
+syntax reifiedOpenStx := withPosition(
+  atomic("open" notFollowedBy("scoped")) ppIndent(reifiedOpenDecl*))
 
-/-- Renders the open scopes that are not accounted for by generic `open`s. Prefixes identifiers with `@` to show the fully-resolved name. -/
+/-- Renders the open scopes that are not accounted for by generic `open`s. Prefixes identifiers
+with `@` to show the fully-resolved name. -/
 syntax reifiedOpenScopedStx := withPosition("open " "scoped"
   ppIndent((ppSpace colGt reifiedSimpleOpenIdent)*))
 
 -- Parser of convenience, since we handle these together.
--- TODO: technically this may be influenced by open scopes. It needs to incorporate that in the parser.
-syntax reifiedVarStx := Parser.Command.variable (ppLine Parser.Command.include)? (ppLine Parser.Command.omit)?
+-- TODO: technically this may be influenced by open scopes.
+-- It needs to incorporate that in the parser.
+syntax reifiedVarStx := Parser.Command.variable
+  (ppLine Parser.Command.include)?
+  (ppLine Parser.Command.omit)?
 
 syntax reifiedOptionKeyValue := ppSpace colGt ident ppSpace optionValue
 /-- `set_options key₁ val₁, key₂ val₂, ...` renders the options set in a single line. -/
@@ -194,7 +205,9 @@ section openDecls
 /-- Lean may duplicate open declarations occasionally due to leanprover/lean4#13353. This function
 deduplicates exactly-duplicated `OpenDecl`s by removing the later occurrences. -/
 @[inline] private def deduplicateOpenDecls (openDecls : List OpenDecl) : List OpenDecl :=
-  /- Note that the innermost openDecls come first and affect name resolution first due to `eraseDups` affecting resolved ids by first occurrence (corresponding to later occurrences in openDecls) -/
+  /- Note that the innermost openDecls come first and affect name resolution first due to
+  `eraseDups` affecting resolved ids by first occurrence (corresponding to later occurrences in
+  openDecls) -/
   -- TODO: find something more efficient, which means basically just about anything else.
   openDecls.reverse.eraseDups.reverse
 
@@ -222,7 +235,8 @@ section openScoped
 @[inline] def _root_.Lean.NameSet.minus (current minus : NameSet) : NameSet := Id.run do
   if minus.isEmpty then current else current.filter (!minus.contains ·)
 
-/-- An extension we can trust to always be present, whose active scopes reflect the result of `open`s. -/
+/-- An extension we can trust to always be present, whose active scopes reflect the result of
+`open`s. -/
 @[inline] def scopeTestExt := Parser.parserExtension.ext
 
 def _root_.Lean.Environment.activeScopes (env : Environment) : NameSet :=
@@ -230,14 +244,16 @@ def _root_.Lean.Environment.activeScopes (env : Environment) : NameSet :=
   | s :: _ => s.activeScopes
   | _ => {}
 
-/-- Gets the activated scopes in a (standard) scoped env extension that are *not* implied by the current namespace and open decls. I.e., the extra `open scoped` that exist somewhere. -/
+/-- Gets the activated scopes in a (standard) scoped env extension that are *not* implied by the
+current namespace and open decls. I.e., the extra `open scoped` that exist somewhere. -/
 protected def _root_.Lean.Environment.extraScoped (env : Environment)
     (currNamespace : Name) (openDecls : List OpenDecl) : NameSet := Id.run do
   -- Note that each `open` that adds `.simple` activates the corresponding scopes.
   let impliedScopes : NameSet := openDecls.foldl (init := {}) fun
     | acc, .simple ns _ => if ns.isAnonymous then acc else acc.insert ns
     | acc, _ => acc
-  -- When `namespace ns` happened (or whatever sequence of `namespace`s), `addScopes` traversed all prefixes of `ns`. So we expect those to be there.
+  -- When `namespace ns` happened (or whatever sequence of `namespace`s), `addScopes` traversed all
+  -- prefixes of `ns`. So we expect those to be there.
   -- Note that `addScope` in `elabNamespace` does not add to the open decls, so this is necessary.
   let impliedScopes := currNamespace.foldrPrefix (init := impliedScopes) fun n acc =>
     if n.isAnonymous then acc else acc.insert n
@@ -344,7 +360,10 @@ def reifyScope : CommandElabM (TSyntax ``scopeStx) := do
     $[$opens]?
     $[$extraScoped]?
     $[$setOptions]?
-    $[$variables]?) -- TODO: technically the variable parsing could change if a scope is opened earlier. This is probably important...it'll mean (1) detecting if any variable syntax is scoped (2) writing a more detailed parser for `scopeStx` that opens the named scopes!
+    $[$variables]?)
+    /- TODO: technically the variable parsing could change if a scope is opened earlier. This is
+    probably important...it'll mean (1) detecting if any variable syntax is scoped (2) writing a
+    more detailed parser for `scopeStx` that opens the named scopes! -/
 
     -- We also could account for `open (scoped) ... in variable` but it would have to be ad-hoc.
 
@@ -360,3 +379,5 @@ elab_rules : command
     let stx ← `(command|#scope%$tk $scopeStx')
     liftCoreM <| Meta.Tactic.TryThis.addSuggestion tk (origSpan? := (← getRef)) stx
       (diffGranularity := .word)
+
+end Mathlib.Tactic.Scope
