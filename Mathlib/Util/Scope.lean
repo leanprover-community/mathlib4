9671fe6d73170eb99e8648601bd90e022dec2555
thorimur <68410468+thorimur@users.noreply.github.com>
2026-05-04 02:03:34 -0400

    chore: `scope` token

--- Mathlib/Util/Scope.lean
+++ Mathlib/Util/Scope.lean
@@ -76,7 +76,7 @@ syntax reifiedSetOptionsStx := withPosition("set_options " ppIndent(reifiedOptio
 /--
 A scope specification of the form
 ```
-(@[expose])? (public)? (noncomputable)? (section)? (scope)?
+(@[expose])? (public)? (noncomputable)? (section)? scope
   (universe ...)?
   (namespace ...)?
   (open @id₁ @id₂ ...)?
