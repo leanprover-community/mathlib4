5eb46b6bf305d62f31faa73870caa9cb554953b7
tb65536 <thomas.l.browning@gmail.com>
2026-05-04 22:18:11 +0100

    merge

--- Mathlib/NumberTheory/RamificationInertia/Basic copy.lean
+++ Mathlib/NumberTheory/RamificationInertia/Basic copy.lean
@@ -18,7 +18,8 @@ public import Mathlib.RingTheory.Flat.Localization
 public import Mathlib.RingTheory.QuasiFinite.Basic
 public import Mathlib.RingTheory.Ideal.Quotient.Nilpotent
 public import Mathlib.RingTheory.SurjectiveOnStalks
-public import Mathlib.NumberTheory.RamificationInertia.Inertia
+public import Mathlib.RingTheory.RamificationInertia.Inertia
+public import Mathlib.RingTheory.RamificationInertia.Ramification
 public import Mathlib.FieldTheory.Galois.IsGaloisGroup
 public import Mathlib.RingTheory.LocalProperties.Projective
 public import Mathlib.RingTheory.LocalRing.Module
@@ -57,126 +58,6 @@ lemma _root_.Ideal.finrank_fiber_eq_finrank {R M : Type*} [CommRing R] [AddCommG
 open Ideal
 
 namespace Ideal
-
--- PRed
-section ramification_inertia
-
-variable {S : Type*} [CommRing S] (q : Ideal S) (R : Type*) [CommRing R] [Algebra R S]
-
-section ramificationIdx
-
-open Classical in
-/-- An alternate definition of ramification index. -/
-noncomputable def ramificationIdx' : ℕ :=
-  if _ : q.IsPrime then
-    letI Sq := Localization.AtPrime q
-    (Module.length Sq (Sq ⧸ (q.under R).map (algebraMap R Sq))).toNat
-  else 0
-
-theorem ramificationIdx'_def [q.IsPrime] :
-    letI Sq := Localization.AtPrime q
-    q.ramificationIdx' R = (Module.length Sq (Sq ⧸ (q.under R).map (algebraMap R Sq))).toNat :=
-  dif_pos _
-
-theorem ramificationIdx'_of_not_isPrime (hq : ¬ q.IsPrime) : q.ramificationIdx' R = 0 :=
-  dif_neg hq
-
-theorem ramificationIdx'_eq {R S : Type*} [CommRing R] [CommRing S] [Algebra R S]
-    (p : Ideal R) (q : Ideal S) [h : q.LiesOver p] [q.IsPrime] :
-    letI Sq := Localization.AtPrime q
-    q.ramificationIdx' R = (Module.length Sq (Sq ⧸ p.map (algebraMap R Sq))).toNat := by
-  rw [ramificationIdx'_def, h.over]
-
-/-- See `ramificationIdx'_tower'` for a version that does not assume primality. -/
-theorem ramificationIdx'_tower
-    {R S T : Type*} [CommRing R] [CommRing S] [CommRing T]
-    [Algebra R S] [Algebra R T] [Algebra S T] [IsScalarTower R S T]
-    (q : Ideal S) [q.IsPrime] (r : Ideal T) [r.IsPrime] [r.LiesOver q]
-    [Algebra (Localization.AtPrime q) (Localization.AtPrime r)]
-    [Localization.AtPrime.IsLiesOverAlgebra q r]
-    [Module.Flat (Localization.AtPrime q) (Localization.AtPrime r)] :
-    r.ramificationIdx' R = q.ramificationIdx' R * r.ramificationIdx' S := by
-  have : q.LiesOver (r.under R) := LiesOver.tower_bot r q (r.under R)
-  rw [ramificationIdx'_def, ramificationIdx'_eq (r.under R), ramificationIdx'_eq q,
-    ← ENat.toNat_mul]
-  congr
-  set Sq := Localization.AtPrime q
-  set Tr := Localization.AtPrime r
-  have := IsLocalRing.length_baseChange
-    (A := Sq) (B := Tr) (M := Sq ⧸ (r.under R).map (algebraMap R Sq))
-  rw [← Localization.AtPrime.map_eq_maximalIdeal, map_map, ← IsScalarTower.algebraMap_eq] at this
-  convert this
-  let f := (Ideal.quotientEquivAlgOfEq Tr (by rw [map_map, ← IsScalarTower.algebraMap_eq])).trans
-    (Algebra.TensorProduct.quotIdealMapEquivTensorQuot Tr ((r.under R).map (algebraMap R Sq)))
-  exact f.toLinearEquiv.length_eq
-
-theorem ramificationIdx'_tower'
-    {R S T : Type*} [CommRing R] [CommRing S] [CommRing T]
-    [Algebra R S] [Algebra R T] [Algebra S T] [IsScalarTower R S T]
-    (q : Ideal S) (r : Ideal T) [h : r.LiesOver q] [Module.Flat S T] :
-    r.ramificationIdx' R = q.ramificationIdx' R * r.ramificationIdx' S := by
-  by_cases hr : r.IsPrime
-  · have : q.IsPrime := by rw [h.over]; exact IsPrime.under S r -- should be lemma
-    let := Localization.AtPrime.algebraOfLiesOver q r
-    apply ramificationIdx'_tower
-  · rw [ramificationIdx'_of_not_isPrime r R hr, ramificationIdx'_of_not_isPrime r S hr, mul_zero]
-
-end ramificationIdx
-
-section inertiaDeg
-
-open Classical in
-/-- An alternate definition of inertia degree. -/
-noncomputable def inertiaDeg' : ℕ :=
-  if _ : q.IsPrime then
-    letI := Localization.AtPrime.algebraOfLiesOver (q.under R) q
-    Module.finrank (q.under R).ResidueField q.ResidueField else 0
-
-theorem inertiaDeg'_def [q.IsPrime]
-    [Algebra (Localization.AtPrime (q.under R)) (Localization.AtPrime q)]
-    [Localization.AtPrime.IsLiesOverAlgebra (q.under R) q] :
-    q.inertiaDeg' R = Module.finrank (q.under R).ResidueField q.ResidueField := by
-  convert dif_pos _
-  apply Algebra.algebra_ext
-  rw [← DFunLike.ext_iff]
-  exact Localization.AtPrime.IsLiesOverAlgebra.algebraMap_eq
-
-theorem inertiaDeg'_of_not_isPrime (hq : ¬ q.IsPrime) : q.inertiaDeg' R = 0 :=
-  dif_neg hq
-
-theorem inertiaDeg'_eq {R S : Type*} [CommRing R] [CommRing S] [Algebra R S]
-    (p : Ideal R) (q : Ideal S) [h : q.LiesOver p] [q.IsPrime] [p.IsPrime]
-    [Algebra (Localization.AtPrime p) (Localization.AtPrime q)]
-    [Localization.AtPrime.IsLiesOverAlgebra p q] :
-    q.inertiaDeg' R = Module.finrank p.ResidueField q.ResidueField := by
-  let : Algebra (Localization.AtPrime (q.under R)) (Localization.AtPrime q) :=
-    Localization.AtPrime.algebraOfLiesOver (q.under R) q
-  rw [inertiaDeg'_def]
-  have := Ideal.over_def q p
-  subst this
-  convert rfl
-  apply Algebra.algebra_ext
-  rw [← DFunLike.ext_iff]
-  exact Localization.AtPrime.IsLiesOverAlgebra.algebraMap_eq
-
-theorem inertiaDeg'_tower'
-    {R S T : Type*} [CommRing R] [CommRing S] [CommRing T]
-    [Algebra R S] [Algebra R T] [Algebra S T] [IsScalarTower R S T]
-    (q : Ideal S) (r : Ideal T) [h : r.LiesOver q] [Module.Flat S T] :
-    r.inertiaDeg' R = q.inertiaDeg' R * r.inertiaDeg' S := by
-  by_cases hr : r.IsPrime
-  · have : q.IsPrime := by rw [h.over]; exact IsPrime.under S r -- should be lemma
-    have : q.LiesOver (r.under R) := LiesOver.tower_bot r q (r.under R)
-    let := Localization.AtPrime.algebraOfLiesOver (r.under R) r
-    let := Localization.AtPrime.algebraOfLiesOver (r.under R) q
-    let := Localization.AtPrime.algebraOfLiesOver q r
-    rw [inertiaDeg'_eq (r.under R), inertiaDeg'_eq (r.under R), inertiaDeg'_eq q, eq_comm]
-    apply Module.finrank_mul_finrank
-  · rw [inertiaDeg'_of_not_isPrime r R hr, inertiaDeg'_of_not_isPrime r S hr, mul_zero]
-
-end inertiaDeg
-
-end ramification_inertia
 
 variable {R S : Type*} [CommRing R] [CommRing S] [Algebra R S] (p : Ideal R) [p.IsPrime]
 
@@ -232,7 +113,8 @@ noncomputable def foo8 (q : Ideal (p.Fiber S)) [q.IsPrime]
         ← Localization.AtPrime.map_eq_maximalIdeal]
   letI q' : Ideal (Sp ⧸ pSp) := q.comap e₁
   haveI : Algebra.algebraMapSubmonoid (S ⧸ pS) r.primeCompl = (under (S ⧸ pS) q').primeCompl :=
-    algebraMapSubmonoid_primeCompl_of_liesOver_surjective (q'.under (S ⧸ pS)) r Ideal.Quotient.mk_surjective
+    algebraMapSubmonoid_primeCompl_of_liesOver_surjective
+      (q'.under (S ⧸ pS)) r Ideal.Quotient.mk_surjective
   haveI : IsLocalization (Algebra.algebraMapSubmonoid (S ⧸ pS) r.primeCompl)
       (Localization.AtPrime q') := by
     convert IsLocalization.isLocalization_isLocalization_atPrime_isLocalization
@@ -272,7 +154,7 @@ noncomputable def foo8 (q : Ideal (p.Fiber S)) [q.IsPrime]
 theorem foo3 (q : Ideal (p.Fiber S)) [q.IsPrime]
     [Algebra (Localization.AtPrime p)
       (Localization.AtPrime (q.comap Algebra.TensorProduct.includeRight))]
-    [Localization.AtPrime.IsLiesOverAlgebra p (q.comap Algebra.TensorProduct.includeRight)]:
+    [Localization.AtPrime.IsLiesOverAlgebra p (q.comap Algebra.TensorProduct.includeRight)] :
     letI r := q.comap Algebra.TensorProduct.includeRight
     letI Sr := Localization.AtPrime r
     Module.length (Localization.AtPrime p) (Localization.AtPrime q) =
@@ -321,10 +203,5 @@ theorem sum_ramification_inertia''
   let := FractionRing.liftAlgebra R (FractionRing S)
   rw [← sum_ramification_inertia', (IsGaloisGroup.toFractionRing G R S).card_eq_finrank,
     Algebra.IsAlgebraic.finrank_of_isFractionRing R (FractionRing R) S (FractionRing S)]
-
-open NumberField in
-example {K L : Type*} [Field K] [Field L] [Algebra K L] [NumberField K] [NumberField L] :
-    Module.Flat (𝓞 K) (𝓞 L) :=
-  inferInstance
 
 end Ideal
