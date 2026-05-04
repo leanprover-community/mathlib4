c29e3d22e69c28dfc67d37302d060c07c9ede7a6
thorimur <68410468+thorimur@users.noreply.github.com>
2026-05-03 22:32:10 -0400

    chore: copyright

--- Mathlib/Util/Scope.lean
+++ Mathlib/Util/Scope.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2026 Thomas R. Murrills. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Thomas R. Murrills
+-/
 module
 
 public meta import Mathlib.Lean.Elab.Options
