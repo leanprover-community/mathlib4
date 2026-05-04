b53544c1495acc0358ac3220d898ac96cacee3fc
Yaël Dillies <yael.dillies@gmail.com>
2026-05-04 15:38:50 +0000

    chore: tag (almost) all `mono` lemmas as `@[gcongr]` (#38793)
    The few exceptions are the lemmas about `Set` and `Finset` which would make `gcongr` use `≤` instead of `⊆`, and `measure_mono_ae` which feels like a bigger than I want to undertake here, as well as a few lemmas that `gcongr` doesn't want to eat.
    
    From AddCombi

--- Mathlib/Data/NNReal/Defs.lean
+++ Mathlib/Data/NNReal/Defs.lean
@@ -334,7 +334,7 @@ set_option backward.privateInPublic true in
 @[simp, norm_cast] lemma coe_le_one : (r : ℝ) ≤ 1 ↔ r ≤ 1 := by rw [← coe_le_coe, coe_one]
 @[simp, norm_cast] lemma coe_lt_one : (r : ℝ) < 1 ↔ r < 1 := by rw [← coe_lt_coe, coe_one]
 
-@[mono] lemma coe_mono : Monotone ((↑) : ℝ≥0 → ℝ) := fun _ _ => NNReal.coe_le_coe.2
+@[gcongr, mono] lemma coe_mono : Monotone ((↑) : ℝ≥0 → ℝ) := fun _ _ => NNReal.coe_le_coe.2
 
 protected theorem _root_.Real.toNNReal_monotone : Monotone Real.toNNReal := fun _ _ h =>
   max_le_max_right _ h
