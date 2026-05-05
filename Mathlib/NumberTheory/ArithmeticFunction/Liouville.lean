f38ab798de4bdc61a33c54038315da247d7efe35
tb65536 <thomas.l.browning@gmail.com>
2026-05-05 18:05:12 +0100

    add

---
+++ Mathlib/NumberTheory/ArithmeticFunction/Liouville.lean
@@ -0,0 +1,53 @@
+/-
+Copyright (c) 2026 Thomas Browning. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Thomas Browning
+-/
+module
+
+public import Mathlib.Algebra.Order.Ring.Abs
+public import Mathlib.NumberTheory.ArithmeticFunction.Misc
+
+/-!
+# The Liouville Function
+
+This file defines the Liouville function `λ(n)`.
+
+## Main Definitions
+
+* `ArithmeticFunction.liouville` is the Liouville function `λ(n)` defined to be `1` is `n` has an
+even number of prime factors (counting multiplicity) and `-1` otherwise.
+-/
+
+@[expose] public section
+
+open Finset Nat
+
+namespace ArithmeticFunction
+
+/-- The Liouville function `λ(n)` defined to be `1` is `n` has an even number of prime factors
+(counting multiplicity) and `-1` otherwise. -/
+def liouville : ArithmeticFunction ℤ where
+  toFun n := if n = 0 then 0 else (-1) ^ cardFactors n
+  map_zero' := by simp
+
+theorem liouville_apply {n : ℕ} (h : n ≠ 0) : liouville n = (-1) ^ cardFactors n :=
+  if_neg h
+
+theorem liouville_ne_zero {n : ℕ} (h : n ≠ 0) : liouville n ≠ 0 := by
+  simp [liouville_apply h]
+
+theorem liouville_apply_one : liouville 1 = 1 := by
+  simp [liouville_apply]
+
+theorem liouville_apply_mul (m n : ℕ) : liouville (m * n) = liouville m * liouville n := by
+  by_cases hm : m = 0
+  · simp [hm]
+  by_cases hn : n = 0
+  · simp [hn]
+  simp [liouville_apply, cardFactors_mul, hm, hn, pow_add]
+
+theorem isMultiplicative_liouville : IsMultiplicative liouville := by
+  refine ⟨liouville_apply_one, fun {m n} _ ↦ liouville_apply_mul m n⟩
+
+end ArithmeticFunction
