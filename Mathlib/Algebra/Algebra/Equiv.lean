561f421986b06c84c343d97b76cf39c512346067
Salvatore Mercuri <salvatore.mercuri@imperial.ac.uk>
2026-05-05 15:11:14 +0100

    remove comment

--- Mathlib/Algebra/Algebra/Equiv.lean
+++ Mathlib/Algebra/Algebra/Equiv.lean
@@ -71,18 +71,7 @@ end AlgEquivClass
 
 namespace AlgEquiv
 
--- universe uR uA₁ uA₂ uA₃ uA₁' uA₂' uA₃'
--- variable {R : Type uR}
--- variable {A₁ : Type uA₁} {A₂ : Type uA₂} {A₃ : Type uA₃}
--- variable {A₁' : Type uA₁'} {A₂' : Type uA₂'} {A₃' : Type uA₃'}
-
 section Semiring
-
--- variable [CommSemiring R] [Semiring A₁] [Semiring A₂] [Semiring A₃]
--- variable [Semiring A₁'] [Semiring A₂'] [Semiring A₃']
--- variable [Algebra R A₁] [Algebra R A₂] [Algebra R A₃]
--- variable [Algebra R A₁'] [Algebra R A₂'] [Algebra R A₃']
--- variable (e : A₁ ≃ₐ[R] A₂)
 
 variable {R : Type uR} [CommSemiring R]
 variable {A : Type uA} {B : Type uB} [Semiring A] [Semiring B]
