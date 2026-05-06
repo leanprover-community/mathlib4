ef7b4965e3d21a8992aee81fc8a8343786dfdabf
Christian Merten <christian@merten.dev>
2026-05-06 21:18:22 +0200

    add name

--- Mathlib/CategoryTheory/Monoidal/Cartesian/Mod.lean
+++ Mathlib/CategoryTheory/Monoidal/Cartesian/Mod.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2025 Paul Lezeau. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Paul Lezeau
+Authors: Paul Lezeau, Christian Merten
 -/
 module
 
