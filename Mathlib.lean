b732855363804c80ab9077c288240c7d6d8d9d30
Joël Riou <37772949+joelriou@users.noreply.github.com>
2026-05-06 00:44:29 +0000

    feat(CategoryTheory/Shift): lifting of single functors (#38632)
    Let `C`, `D` and `E` be categories. Let `A` be an additive monoid. Assume that `D` and `E` have shifts by `A` and that we have a fully faithful functor `G : D ⥤ A` which commutes with shifts. Given `F : SingleFunctors C E A`, and a family of functors `Φ a : C ⥤ D` with isomorphisms `Φ a ⋙ G ≅ F.functor a` for all `a : A`, we lift `F` in `SingleFunctor C D A`.

--- Mathlib.lean
+++ Mathlib.lean
@@ -3243,6 +3243,7 @@ public import Mathlib.CategoryTheory.Shift.ShiftSequence
 public import Mathlib.CategoryTheory.Shift.ShiftedHom
 public import Mathlib.CategoryTheory.Shift.ShiftedHomOpposite
 public import Mathlib.CategoryTheory.Shift.SingleFunctors
+public import Mathlib.CategoryTheory.Shift.SingleFunctorsLift
 public import Mathlib.CategoryTheory.Shift.Twist
 public import Mathlib.CategoryTheory.ShrinkYoneda
 public import Mathlib.CategoryTheory.Sigma.Basic
