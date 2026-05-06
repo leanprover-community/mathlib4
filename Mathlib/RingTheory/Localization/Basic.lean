9f8b156e1c1b7bd88ae833ec65c60f33b24c021c
Christian Merten <christian@merten.dev>
2026-05-06 16:40:51 +0200

    fix?

--- Mathlib/RingTheory/Localization/Basic.lean
+++ Mathlib/RingTheory/Localization/Basic.lean
@@ -511,12 +511,17 @@ noncomputable def localizationAlgebra : Algebra Rₘ Sₘ :=
 noncomputable instance : Algebra (Localization M)
     (Localization (Algebra.algebraMapSubmonoid S M)) := localizationAlgebra M S
 
-instance isScalarTower_localizationAlgebra [Algebra R Sₘ] [IsScalarTower R S Sₘ] :
+-- This is not an instance, because the discrimination tree key is `IsScalarTower _ _ _ _ _ _`, so
+-- it would cause significant slowdowns.
+lemma isScalarTower_localizationAlgebra [Algebra R Sₘ] [IsScalarTower R S Sₘ] :
     letI : Algebra Rₘ Sₘ := localizationAlgebra M S
     IsScalarTower R Rₘ Sₘ :=
   letI : Algebra Rₘ Sₘ := localizationAlgebra M S
   .of_algebraMap_eq' <| by
     simp [RingHom.algebraMap_toAlgebra, IsScalarTower.algebraMap_eq R S Sₘ]
+
+instance : IsScalarTower R (Localization M) (Localization (Algebra.algebraMapSubmonoid S M)) :=
+  isScalarTower_localizationAlgebra _ _
 
 end
 
