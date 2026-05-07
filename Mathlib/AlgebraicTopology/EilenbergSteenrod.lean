403ce289d2ee2a0cd5f3d53db287d6cf764d9de0
Jacob Scharmberg <hi@jacobscharmberg.net>
2026-05-07 11:37:40 +0200

    inline surjectivity proof into iso proof

--- Mathlib/AlgebraicTopology/EilenbergSteenrod.lean
+++ Mathlib/AlgebraicTopology/EilenbergSteenrod.lean
@@ -98,37 +98,33 @@ protected def forget (i : ι) : HomologyPretheory C c ⥤ TopPair.{u} ⥤ C wher
 
 end HomologyPretheory
 
-/-- Under the assumptions of excision, the map of the pair `U` is in fact surjective. -/
-lemma surjective_of_isCompl_closure ⦃X U V : TopPair⦄ (f : U ⟶ X) (g : V ⟶ X) (hf : IsEmbedding f)
-    (hcompl : TopPair.IsCompl f g)
-    (hU : closure (Set.range (Hom.fst f)) ⊆ interior (Set.range X.map)) :
-      Function.Surjective U.map := by
-  rw [← Set.range_eq_univ, Set.Subset.antisymm_iff]
-  use (by simp)
-  rw [← Set.image_subset_image_iff hf.fst.injective]
-  have h₀ : Set.range (Hom.fst f) ⊆ Hom.fst f '' Set.range U.map ∪ Hom.fst g '' Set.range V.map :=
-    by
-    simp only [← Set.range_comp, ← CategoryTheory.hom_comp]
-    simp only [← Arrow.w, CategoryTheory.hom_comp, Set.range_comp, ← Set.image_union,
-      ← Set.sup_eq_union, codisjoint_iff.mp hcompl.snd.codisjoint, Set.top_eq_univ, Set.image_univ]
-    calc
-      Set.range (Hom.fst f) ⊆ closure (Set.range (Hom.fst f)) := subset_closure
-      _ ⊆ interior (Set.range X.map) := hU
-      _ ⊆ Set.range X.map := interior_subset
-  have h₁ : Disjoint (Set.range (Hom.fst f)) (Hom.fst g '' Set.range V.map) := by
-    rw [Set.disjoint_iff, ← Set.disjoint_iff_inter_eq_empty.mp hcompl.fst.disjoint]
-    grind
-  simp [Disjoint.subset_left_of_subset_union h₀ h₁]
-
 set_option backward.isDefEq.respectTransparency false in
 /-- Under the assumptions of excision, the map of the pair `U` is an isomorphism. -/
 lemma isIso_of_isCompl_closure ⦃X U V : TopPair⦄ (f : U ⟶ X) (g : V ⟶ X) (hf : IsEmbedding f)
     (hcompl : TopPair.IsCompl f g)
     (hU : closure (Set.range (Hom.fst f)) ⊆ interior (Set.range X.map)) : IsIso U.map := by
+  have surjective_U : Function.Surjective U.map := by
+    rw [← Set.range_eq_univ, Set.Subset.antisymm_iff]
+    use (by simp)
+    rw [← Set.image_subset_image_iff hf.fst.injective]
+    have h₀ : Set.range (Hom.fst f) ⊆ Hom.fst f '' Set.range U.map ∪ Hom.fst g '' Set.range V.map :=
+      by
+      simp only [← Set.range_comp, ← CategoryTheory.hom_comp]
+      simp only [← Arrow.w, CategoryTheory.hom_comp, Set.range_comp, ← Set.image_union,
+        ← Set.sup_eq_union, codisjoint_iff.mp hcompl.snd.codisjoint, Set.top_eq_univ,
+        Set.image_univ]
+      calc
+        Set.range (Hom.fst f) ⊆ closure (Set.range (Hom.fst f)) := subset_closure
+        _ ⊆ interior (Set.range X.map) := hU
+        _ ⊆ Set.range X.map := interior_subset
+    have h₁ : Disjoint (Set.range (Hom.fst f)) (Hom.fst g '' Set.range V.map) := by
+      rw [Set.disjoint_iff, ← Set.disjoint_iff_inter_eq_empty.mp hcompl.fst.disjoint]
+      grind
+    simp [Disjoint.subset_left_of_subset_union h₀ h₁]
   apply TopCat.isIso_of_bijective_of_isOpenMap _
-    ⟨U.prop.injective, surjective_of_isCompl_closure f g hf hcompl hU⟩
+    ⟨U.prop.injective, surjective_U⟩
   apply Topology.IsInducing.isOpenMap U.prop.isInducing
-  simp [Function.Surjective.range_eq (surjective_of_isCompl_closure f g hf hcompl hU)]
+  simp [Function.Surjective.range_eq surjective_U]
 
 end TopPair
 
