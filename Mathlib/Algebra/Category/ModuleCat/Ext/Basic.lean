9e4d0e2081f2898cc69d8a6744bbb6ee26d4e8ad
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2026-05-07 20:42:42 +0800

    fix naming

--- Mathlib/Algebra/Category/ModuleCat/Ext/Basic.lean
+++ Mathlib/Algebra/Category/ModuleCat/Ext/Basic.lean
@@ -32,7 +32,7 @@ namespace CategoryTheory.Abelian
 variable [Small.{v} R] {M N : ModuleCat.{v} R}
 
 set_option backward.isDefEq.respectTransparency false in
-lemma Ext.smul_id_postcomp_eq_zero_of_mem_annihilator {r : R} (mem_ann : r ∈ Module.annihilator R N)
+lemma Ext.postcomp_smul_id_eq_zero_of_mem_annihilator {r : R} (mem_ann : r ∈ Module.annihilator R N)
     (n : ℕ) : AddCommGrpCat.ofHom ((Ext.mk₀ (r • (𝟙 M))).postcomp N (add_zero n)) = 0 := by
   ext h
   have : r • (𝟙 N) = 0 := by
@@ -41,7 +41,7 @@ lemma Ext.smul_id_postcomp_eq_zero_of_mem_annihilator {r : R} (mem_ann : r ∈ M
   simp [Ext.mk₀_smul, this, smul_eq]
 
 set_option backward.isDefEq.respectTransparency false in
-lemma Ext.smul_id_postcomp_mono_iff (r : R) (i : ℕ) :
+lemma Ext.postcomp_smul_id_mono_iff (r : R) (i : ℕ) :
     Mono (AddCommGrpCat.ofHom ((Ext.mk₀ (r • 𝟙 M)).postcomp N (add_zero i))) ↔
     IsSMulRegular (Ext N M i) r := by
   simp only [IsSMulRegular, AddCommGrpCat.mono_iff_injective]
