8535ebb694ebed8cca9be7e2f468a990c81f56e0
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2026-05-08 12:31:40 +0800

    add doc

--- Mathlib/RingTheory/Ideal/MinimalPrime/Basic.lean
+++ Mathlib/RingTheory/Ideal/MinimalPrime/Basic.lean
@@ -36,6 +36,7 @@ section
 
 variable {R S : Type*} [CommSemiring R] [CommSemiring S] (I J : Ideal R)
 
+/-- The predicate for describing an ideal is minimal prime over certain ideal `I`. -/
 protected def Ideal.IsMinimalPrime (p : Ideal R) : Prop := Minimal (fun q ↦ q.IsPrime ∧ I ≤ q) p
 
 lemma Ideal.IsMinimalPrime.isPrime {p : Ideal R} (h : I.IsMinimalPrime p) : p.IsPrime := h.1.1
