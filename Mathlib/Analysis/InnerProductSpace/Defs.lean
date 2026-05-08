259f640a40dc3ab5d80e1e8b3c2664ca46901838
Yaël Dillies <yael.dillies@gmail.com>
2026-05-08 09:32:51 +0200

    feat(Analysis): the trivial inner product space Provide the `InnerProductSpace 𝕜 PUnit` instance.
    From MeanFourier

--- Mathlib/Analysis/InnerProductSpace/Defs.lean
+++ Mathlib/Analysis/InnerProductSpace/Defs.lean
@@ -601,3 +601,17 @@ structure HilbertSpace (𝕜 E : Type*) [RCLike 𝕜]
   [NormedAddCommGroup E] [InnerProductSpace 𝕜 E] [CompleteSpace E]
 
 end
+
+namespace PUnit
+variable {𝕜 : Type*} [RCLike 𝕜]
+
+instance : InnerProductSpace 𝕜 PUnit where
+  inner _ _ := 0
+  norm_sq_eq_re_inner := by simp
+  conj_inner_symm := by simp
+  add_left := by simp
+  smul_left := by simp
+
+@[simp] lemma inner_eq_zero (x y : PUnit) : inner 𝕜 x y = 0 := rfl
+
+end PUnit
