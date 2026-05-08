32674dc10e3e81fe408402b3520921195e1d0d0e
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2026-05-08 19:16:19 +0200

    add another lemma

--- Mathlib/NumberTheory/NumberField/Completion/FinitePlace.lean
+++ Mathlib/NumberTheory/NumberField/Completion/FinitePlace.lean
@@ -400,6 +400,14 @@ theorem hasFiniteMulSupport {x : K} (h_x_nezero : x ≠ 0) :
 
 @[deprecated (since := "2026-03-03")] alias mulSupport_finite := hasFiniteMulSupport
 
+lemma hasFiniteMulSupport_fun_pow_multiplicity {M : Type*} [CommMonoid M] {I : Ideal (𝓞 K)}
+    (hI : I ≠ ⊥) (f : Ideal (𝓞 K) → M) :
+    (fun v : FinitePlace K ↦
+      (f v.maximalIdeal.asIdeal) ^ multiplicity v.maximalIdeal.asIdeal I).HasFiniteMulSupport := by
+  simp only [← count_normalizedFactors_eq_multiplicity hI]
+  exact Multiset.hasFiniteMulSupport_fun_pow_count (UniqueFactorizationMonoid.normalizedFactors I) f
+    |>.fun_comp_of_injective asIdeal_maximalIdeal_injective
+
 protected
 lemma add_le (v : FinitePlace K) (x y : K) :
     v (x + y) ≤ max (v x) (v y) := by
