caa8ff87c021f49fd2c0b6d6eb4ac8b23f72f41d
Rmal <97214596+CoolRmal@users.noreply.github.com>
2026-05-08 22:15:07 -0400

    Update Integral.lean

--- Mathlib/MeasureTheory/VectorMeasure/Integral.lean
+++ Mathlib/MeasureTheory/VectorMeasure/Integral.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2025 Yoh Tanimoto. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Yoh Tanimoto
+Authors: Yoh Tanimoto, Yongxi Lin
 -/
 module
 
