26c4381a5076743fda14205120db86e9e4d6c01a
Jovan Gerbscheid <56355248+JovanGerb@users.noreply.github.com>
2026-05-09 23:47:38 +0000

    feat(Translate): locally modify name guessing dictionaries (#37808)
    This PR adds the feature to `to_dual` and `to_additive` that you can now locally modify the name translation dictionary.
    
    With this change, we run the risk of having the automated translations being harder to predict. For this reason, the changes to the dictionary do not persisted through imports. Instead, the change lasts until the end of the file (though it ignores sections).
    
    See https://leanprover.zulipchat.com/#narrow/channel/287929-mathlib4/topic/Order.20dual.20tactic/near/580834166

--- Mathlib/Tactic/Translate/ToAdditive.lean
+++ Mathlib/Tactic/Translate/ToAdditive.lean
@@ -387,13 +387,17 @@ def abbreviationDict : Std.HashMap String String := .ofList [
   ("modObj", "AddModObj"),
   ("yonedaMon", "yonedaAddMon")]
 
+@[inherit_doc GuessName.GuessNameExt]
+initialize guessNameExt : GuessName.GuessNameExt ←
+  GuessName.registerGuessNameExt { nameDict, abbreviationDict }
+
 /-- The bundle of environment extensions for `to_additive` -/
 def data : TranslateData where
   ignoreArgsAttr; doTranslateAttr; translations
   attrName := `to_additive
   changeNumeral := true
   isDual := false
-  guessNameData := { nameDict, abbreviationDict }
+  guessNameExt
 
 initialize registerBuiltinAttribute {
     name := `to_additive
@@ -410,5 +414,10 @@ into the `to_additive` dictionary. This is useful for translating namespaces tha
 have a corresponding translated declaration. -/
 elab "insert_to_additive_translation" src:ident tgt:ident : command => do
   translations.add src.getId { translation := tgt.getId }
+
+/-- `to_additive_name_hint src tgt` lets `to_additive` translate the name segment `src` to `tgt`
+for the rest of the file current. `src` and `tgt` should both be capitalized. -/
+elab "to_additive_name_hint" src:ident tgt:ident : command => do
+  guessNameExt.addTranslation src tgt
 
 end Mathlib.Tactic.ToAdditive
