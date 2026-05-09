4aae0228ed9bf464f326862daf437336e0876c85
Haoen Feng <haoenfeng5@gmail.com>
2026-05-09 21:24:44 +0800

    chore: run lake exe mk_all to update Mathlib.lean imports

--- Mathlib.lean
+++ Mathlib.lean
@@ -1727,8 +1727,8 @@ public import Mathlib.Analysis.Calculus.DerivativeTest
 public import Mathlib.Analysis.Calculus.DiffContOnCl
 public import Mathlib.Analysis.Calculus.DifferentialForm.Basic
 public import Mathlib.Analysis.Calculus.DifferentialForm.BoxStokes
-public import Mathlib.Analysis.Calculus.DifferentialForm.HalfSpaceStokes
 public import Mathlib.Analysis.Calculus.DifferentialForm.FullSpaceStokes
+public import Mathlib.Analysis.Calculus.DifferentialForm.HalfSpaceStokes
 public import Mathlib.Analysis.Calculus.DifferentialForm.VectorField
 public import Mathlib.Analysis.Calculus.FDeriv.Add
 public import Mathlib.Analysis.Calculus.FDeriv.Affine
