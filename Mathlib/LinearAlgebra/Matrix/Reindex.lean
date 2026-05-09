a3c88adb3d6ff17f8500bd4bfcf47a6109ebaac7
Snir Broshi <26556598+SnirBroshi@users.noreply.github.com>
2026-05-09 07:23:27 +0300

    feat(LinearAlgebra/Matrix/Reindex): `AddEquiv` and `RingEquiv`

--- Mathlib/LinearAlgebra/Matrix/Reindex.lean
+++ Mathlib/LinearAlgebra/Matrix/Reindex.lean
@@ -15,9 +15,11 @@ to an `m'` by `n'` matrix, as long as `m ≃ m'` and `n ≃ n'`.
 
 ## Main definitions
 
+* `Matrix.reindexAddEquiv R`: `Matrix.reindex` as an `AddEquiv` between `R`-matrices.
+* `Matrix.reindexRingEquiv R`: `Matrix.reindex` as a `RingEquiv` between `R`-matrices.
 * `Matrix.reindexLinearEquiv R A`: `Matrix.reindex` is an `R`-linear equivalence between
   `A`-matrices.
-* `Matrix.reindexAlgEquiv R`: `Matrix.reindex` is an `R`-algebra equivalence between `R`-matrices.
+* `Matrix.reindexAlgEquiv R A`: `Matrix.reindex` is an `R`-algebra equivalence between `A`-matrices.
 
 ## Tags
 
@@ -35,6 +37,30 @@ open Equiv Matrix
 variable {l m n o : Type*} {l' m' n' o' : Type*} {m'' n'' : Type*}
 variable (R A : Type*)
 
+section Add
+
+variable [Add R]
+
+/-- `Matrix.reindex` as an `AddEquiv` between `R`-matrices. -/
+@[simps!]
+def reindexAddEquiv (eₘ : m ≃ m') (eₙ : n ≃ n') : Matrix m n R ≃+ Matrix m' n' R where
+  __ := reindex eₘ eₙ
+  map_add' _ _ := rfl
+
+end Add
+
+section Mul
+
+variable [Fintype m] [Fintype n] [Mul R] [AddCommMonoid R]
+
+/-- `Matrix.reindex` as a `RingEquiv` between `R`-matrices. -/
+@[simps!]
+def reindexRingEquiv (e : m ≃ n) : Matrix m m R ≃+* Matrix n n R where
+  __ := reindexAddEquiv R e e
+  map_mul' A B := submatrix_mul_equiv A B .. |>.symm
+
+end Mul
+
 section AddCommMonoid
 
 variable [Semiring R] [AddCommMonoid A] [Module R A]
@@ -42,8 +68,7 @@ variable [Semiring R] [AddCommMonoid A] [Module R A]
 /-- The natural map that reindexes a matrix's rows and columns with equivalent types,
 `Matrix.reindex`, is a linear equivalence. -/
 def reindexLinearEquiv (eₘ : m ≃ m') (eₙ : n ≃ n') : Matrix m n A ≃ₗ[R] Matrix m' n' A where
-  __ := reindex eₘ eₙ
-  map_add' _ _ := rfl
+  __ := reindexAddEquiv A eₘ eₙ
   map_smul' _ _ := rfl
 
 @[simp]
@@ -113,9 +138,8 @@ variable [CommSemiring R] [Fintype n] [Fintype m] [DecidableEq m] [DecidableEq n
 map that reindexes a matrix's rows and columns with equivalent types,
 `Matrix.reindex`, is an equivalence of algebras. -/
 def reindexAlgEquiv (e : m ≃ n) : Matrix m m A ≃ₐ[R] Matrix n n A where
-  __ := reindexLinearEquiv A A e e
-  map_mul' a b := reindexLinearEquiv_mul A A e e e a b |>.symm
-  commutes' _ := by simp [algebraMap, Algebra.algebraMap]
+  __ := reindexRingEquiv A e
+  commutes' _ := by simp [algebraMap]
 
 @[simp]
 theorem reindexAlgEquiv_apply (e : m ≃ n) (M : Matrix m m A) :
