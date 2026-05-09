8c19d14663fdd6a42073e7fcad386efdbfb4abf3
Yi.Yuan <kysyy1@126.com>
2026-05-09 12:59:33 +0800

    6

--- Mathlib/Analysis/Distribution/FourierSchwartz.lean
+++ Mathlib/Analysis/Distribution/FourierSchwartz.lean
@@ -1,7 +1,5 @@
 module -- shake: keep-all
 
-public import Mathlib.Analysis.SpecialFunctions.Bernstein
-public import Mathlib.Analysis.SpecialFunctions.Gamma.Basic
-public import Mathlib.Data.Nat.Factorial.DoubleFactorial
+import Mathlib.Data.Nat.Factorial.DoubleFactorial
 
 deprecated_module (since := "2026-01-19")
