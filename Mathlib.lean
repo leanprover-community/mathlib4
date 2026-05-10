38ee61f392a4a69934fc233be4c4add6eb0b124f
Joël Riou <37772949+joelriou@users.noreply.github.com>
2026-05-10 09:17:37 +0000

    feat(CategoryTheory/Monoidal): Yoneda embedding of ring objects (#36913)
    (This series of PR mostly builds on previous work by the Toric project.)

--- Mathlib.lean
+++ Mathlib.lean
@@ -3039,6 +3039,7 @@ public import Mathlib.CategoryTheory.Monoidal.Cartesian.Mon
 public import Mathlib.CategoryTheory.Monoidal.Cartesian.Mon_
 public import Mathlib.CategoryTheory.Monoidal.Cartesian.Normal
 public import Mathlib.CategoryTheory.Monoidal.Cartesian.Over
+public import Mathlib.CategoryTheory.Monoidal.Cartesian.Ring
 public import Mathlib.CategoryTheory.Monoidal.Cartesian.ShrinkYoneda
 public import Mathlib.CategoryTheory.Monoidal.Category
 public import Mathlib.CategoryTheory.Monoidal.Center
