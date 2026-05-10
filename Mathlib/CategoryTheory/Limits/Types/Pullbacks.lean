22a36e0039b6467aa40b9a1196b33a995dce56ae
Jacob Reinhold <5241441+jcreinhold@users.noreply.github.com>
2026-05-09 21:30:08 -0400

    chore: remove unnecessary set_option for backward.isDefEq.respectTransparency

--- Mathlib/CategoryTheory/Limits/Types/Pullbacks.lean
+++ Mathlib/CategoryTheory/Limits/Types/Pullbacks.lean
@@ -233,7 +233,6 @@ lemma isPullback_iff :
     exact ⟨⟨w⟩, ⟨IsLimit.ofIsoLimit ((Types.pullbackLimitCone r b).isLimit)
       (PullbackCone.ext (asIso φ)).symm⟩⟩
 
-set_option backward.isDefEq.respectTransparency false in
 /-- If `A = f ⁻¹' B`, then the canonical square through the subtype inclusions
 is a pullback in `Type u`. -/
 lemma isPullback_of_eq_setPreimage {X Y : Type u} (f : X ⟶ Y) (B : Set Y) {A : Set X}
