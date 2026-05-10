d7787edbef3a3c0ddaa457dcec9cc118a711265b
Weiyi Wang <wwylele@gmail.com>
2026-05-10 15:49:22 -0400

    chore(Algebra/LinearRecurrence): remove a defeq abuse

--- Mathlib/Algebra/LinearRecurrence.lean
+++ Mathlib/Algebra/LinearRecurrence.lean
@@ -155,11 +155,11 @@ theorem repr_basis_eq (u : E.solSpace) :
 theorem repr_basis_apply (u : E.solSpace) (n : Fin E.order) : E.basis.repr u n = u.val n :=
   rfl
 
-set_option backward.isDefEq.respectTransparency false in
 /-- Two solutions are equal iff their initial conditions are equal. -/
 theorem eq_iff_eqOn_range_order (u v : ℕ → R) (hu : E.IsSolution u) (hv : E.IsSolution v) :
     u = v ↔ Set.EqOn u v ↑(range E.order) := by
-  rw [← Subtype.mk.injEq u hu v hv, ← E.basis.repr.injective.eq_iff]
+  rw [← Subtype.mk.injEq u ((is_sol_iff_mem_solSpace _ _).mp hu) v
+    ((is_sol_iff_mem_solSpace _ _).mp hv), ← E.basis.repr.injective.eq_iff]
   constructor
   · exact fun h n hn ↦ congr($h ⟨n, Finset.mem_range.mp hn⟩)
   · exact fun h ↦ Finsupp.ext fun n ↦ h <| Finset.mem_range.mpr n.prop
