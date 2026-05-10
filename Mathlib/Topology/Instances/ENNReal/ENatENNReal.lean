5368102af644760ecc1f8a7860c7d66f1d887bed
Weiyi Wang <wwylele@gmail.com>
2026-05-10 00:04:27 -0400

    feat(Topology): Enat.toENNReal is a closed embedding

---
+++ Mathlib/Topology/Instances/ENNReal/ENatENNReal.lean
@@ -0,0 +1,33 @@
+/-
+Copyright (c) 2026 Weiyi Wang. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Weiyi Wang
+-/
+module
+
+public import Mathlib.Data.Real.ENatENNReal
+public import Mathlib.Topology.Instances.ENat
+public import Mathlib.Topology.Instances.ENNReal.Lemmas
+import Mathlib.Algebra.Order.Floor.Extended
+
+/-!
+# Topology lemma for `ENat.toENNReal`
+
+This file shows `ENat.toENNReal` is a closed embedding.
+-/
+
+public section
+
+namespace ENat
+
+theorem continuous_toENNReal : Continuous toENNReal := by
+  refine OrderTopology.continuous_iff.mpr fun a ↦ ⟨?_, ?_⟩
+  · have : toENNReal ⁻¹' Set.Ioi a = Set.Ioi ⌊a⌋ₑ := by ext; simp
+    simpa [this] using isOpen_Ioi
+  · have : toENNReal ⁻¹' Set.Iio a = Set.Iio ⌈a⌉ₑ := by ext; simp
+    simpa [this] using isOpen_Iio
+
+theorem isClosedEmbedding_toENNReal : Topology.IsClosedEmbedding toENNReal :=
+  continuous_toENNReal.isClosedEmbedding toENNReal_strictMono.injective
+
+end ENat
