50abf8d4302a562b663fc8a3adb9b0e5633b17e8
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2026-05-10 21:31:10 +0800

    golf more

--- Mathlib/Algebra/Category/ModuleCat/Ext/Baer.lean
+++ Mathlib/Algebra/Category/ModuleCat/Ext/Baer.lean
@@ -65,10 +65,9 @@ lemma ext_quotient_one_subsingleton_iff [Small.{v} R] (M : ModuleCat.{v} R) (I :
     (Shrink.linearEquiv R (R ⧸ I)) _ _  exact.linearMap_comp_eq_zero
   have S_exact : S.ShortExact :=
     ModuleCat.shortComplexOfConj_shortExact _ _ _ _ _ exact I.subtype_injective I.mkQ_surjective
-  have : Projective S.X₂ := by dsimp [S]; infer_instance
+  rw [ext_one_subsingleton_iff_of_projective M S S_exact (by dsimp [S]; infer_instance)]
   --Reduce the vanishing of `Ext (R ⧸ I) M 1` to surjectivity of `Ext R M 0 → Ext I M 0`,
   --but with shrink involved.
-  apply (ext_one_subsingleton_iff_of_projective M S S_exact this).trans
   refine ⟨fun h ↦ fun g ↦ ?_, fun h ↦ fun e ↦ ?_⟩
   · obtain ⟨f', hf'⟩ := h (Ext.mk₀ (ModuleCat.ofHom (g.comp (Shrink.linearEquiv R I).toLinearMap)))
     rw [Ext.bilinearComp_apply_apply, ← Ext.mk₀_addEquiv₀_apply f', Ext.mk₀_comp_mk₀] at hf'
