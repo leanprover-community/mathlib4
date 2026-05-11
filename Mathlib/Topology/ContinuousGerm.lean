8c393b6dd6a7c82c08960256218467ed9dc38135
Ben Eltschig <b.eltschig@protonmail.com>
2026-05-11 19:36:30 +0200

    remove `Filter.Germ.ContinuousAt`

--- Mathlib/Topology/ContinuousGerm.lean
+++ Mathlib/Topology/ContinuousGerm.lean
@@ -45,62 +45,35 @@ open Filter
 
 variable {X Y Z : Type*} [TopologicalSpace X] [TopologicalSpace Y] [TopologicalSpace Z]
 
-/-- A germ `f` at `x` is called continuous at `x` if it is represented by functions that are
-continuous at `x`.
-
-Note that this is weaker than being represented by a function that is continuous on an open
-neighbourhood around `x`: the functions representing `x` may still have discontinuities arbitrarily
-close to `x`. -/
-protected def Filter.Germ.ContinuousAt {x : X} (f : Germ (𝓝 x) Y) : Prop :=
-  Germ.liftOn f (fun f ↦ ContinuousAt f x) fun _ _ h ↦ (continuousAt_congr h).eq
-
-lemma ContinuousAt.germ {x : X} {f : X → Y} (hf : ContinuousAt f x) :
-    (f : Germ (𝓝 x) Y).ContinuousAt :=
-  hf
-
-@[simp]
-lemma Filter.Germ.continuousAt_coe_iff {x : X} (f : X → Y) :
-    (f : Germ (𝓝 x) Y).ContinuousAt ↔ ContinuousAt f x :=
-  Iff.rfl
-
-lemma Filter.Germ.continuousAt_iff_tendsto {x : X} {f : Germ (𝓝 x) Y} :
-    f.ContinuousAt ↔ f.Tendsto (𝓝 f.value) := by
-  obtain ⟨f, rfl⟩ := Quotient.exists_rep f
-  rfl
-
-lemma Filter.Germ.ContinuousAt.tendsto {x : X} {f : Germ (𝓝 x) Y} (hf : f.ContinuousAt) :
-    f.Tendsto (𝓝 f.value) :=
-  continuousAt_iff_tendsto.1 hf
-
 /-- A germ `f` at `x` is called continuous if it is represented by functions that are continuous
 near (i.e. on some open neighbourhood of) `x`.
 
 Note that this is weaker than being represented by a globally continuous function, even for germs
 between nice spaces like compact subsets of ℝ². -/
 protected def Filter.Germ.Continuous {x : X} (f : Germ (𝓝 x) Y) : Prop :=
-  RestrictGermPredicate (fun _ f ↦ f.ContinuousAt) {x} x f
+  RestrictGermPredicate (fun _ f ↦ f.Tendsto (𝓝 f.value)) {x} x f
 
 lemma Continuous.germ {f : X → Y} (hf : Continuous f) (x : X) : (f : Germ (𝓝 x) Y).Continuous :=
-  forall_restrictGermPredicate_of_forall (fun _ ↦ hf.continuousAt.germ) x
+  forall_restrictGermPredicate_of_forall (fun _ ↦ hf.continuousAt) x
 
 /-- A germ at `x` is continuous iff all functions representing it are continuous at all points
 close enough to `x`. -/
 lemma Filter.Germ.continuous_iff_forall {x : X} {f : Germ (𝓝 x) Y} :
     f.Continuous ↔ ∀ f' : X → Y, f = f' → ∀ᶠ x' in 𝓝 x, ContinuousAt f' x' := by
-  simp [Germ.Continuous, restrictGermPredicate_iff_forall]
+  simp [Germ.Continuous, restrictGermPredicate_iff_forall, ContinuousAt]
 
 /-- A germ at `x` is continuous iff at least one function representing it is continuous at all
 points close enough to `x`. -/
 lemma Filter.Germ.continuous_iff_exists {x : X} {f : Germ (𝓝 x) Y} :
     f.Continuous ↔ ∃ f' : X → Y, f = f' ∧ ∀ᶠ x' in 𝓝 x, ContinuousAt f' x' := by
-  simp [Germ.Continuous, restrictGermPredicate_iff_exists]
+  simp [Germ.Continuous, restrictGermPredicate_iff_exists, ContinuousAt]
 
 /-- The germ of a function `f` at `x` is continuous iff `f` is continuous at all points
 close enough to `x`. -/
 @[simp]
 lemma Filter.Germ.continuous_coe_iff {x : X} {f : X → Y} :
     (f : Germ (𝓝 x) Y).Continuous ↔ ∀ᶠ x' in 𝓝 x, ContinuousAt f x' := by
-  simp [Germ.Continuous]
+  simp [Germ.Continuous, ContinuousAt]
 
 /-- The germ of a function `f` at `x` is continuous iff `f` is continuous on a neighbourhood of
 `x`. -/
@@ -109,8 +82,8 @@ lemma Filter.Germ.continuous_coe_iff' {x : X} {f : X → Y} :
   simp [eventually_continuousAt_iff]
 
 /-- Every continuous germ at `x` is in particular continuous at `x`. -/
-lemma Filter.Germ.Continuous.continuousAt {x : X} {f : Germ (𝓝 x) Y} (hf : f.Continuous) :
-    f.ContinuousAt := by
+lemma Filter.Germ.Continuous.tendsto {x : X} {f : Germ (𝓝 x) Y} (hf : f.Continuous) :
+    f.Tendsto (𝓝 f.value) := by
   revert hf
   exact f.inductionOn fun f hf ↦ (continuous_coe_iff.1 hf).self_of_nhds
 
@@ -139,7 +112,7 @@ lemma ContinuousGerm.inductionOn {x : X} (f : ContinuousGerm x Y) {p : Continuou
 `y`. -/
 def ContinuousGerm.comp {x : X} {y : Y} (f : ContinuousGerm y Z) (g : ContinuousGerm x Y)
     (h : g.toGerm.value = y) : ContinuousGerm x Z where
-  toGerm := f.toGerm.compTendsto' g.toGerm (h ▸ g.continuous.continuousAt.tendsto)
+  toGerm := f.toGerm.compTendsto' g.toGerm (h ▸ g.continuous.tendsto)
   continuous := f.inductionOn fun f hf ↦ g.continuous.map <| by simpa [h] using hf
 
 @[simp]
