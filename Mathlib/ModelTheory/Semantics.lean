9d8914e09818f421c32b988bf2cc5ec76242ab5e
iosephusferrum <yusufdemir12610@gmail.com>
2026-05-12 10:23:43 +0300

    simplified proofs for new semantics lemmas

--- Mathlib/ModelTheory/Semantics.lean
+++ Mathlib/ModelTheory/Semantics.lean
@@ -592,9 +592,8 @@ theorem realize_relabel_finSumFinEquiv_symm {m n : ℕ} (φ : L.Formula (Fin (m 
     {v : Fin m → M} {xs : Fin n → M} :
     (BoundedFormula.relabel (fun i => finSumFinEquiv.symm i) φ).Realize v xs ↔
       φ.Realize (Sum.elim v xs ∘ finSumFinEquiv.symm) := by
-  rw [BoundedFormula.realize_relabel, Formula.Realize]
-  have hbound : (xs ∘ Fin.natAdd n : Fin 0 → M) = default := by ext i; exact i.elim0
-  rw [hbound]
+  rw [BoundedFormula.realize_relabel, Formula.Realize,
+    Subsingleton.elim (xs ∘ Fin.natAdd n : Fin 0 → M) default]
   rfl
 
 /-- A specialized version of `Formula.realize_relabel_finSumFinEquiv_symm` for quantifying
@@ -604,19 +603,10 @@ theorem realize_relabel_finSumFinEquiv_symm_snoc {m : ℕ} (φ : L.Formula (Fin 
     (BoundedFormula.relabel (fun i => finSumFinEquiv.symm i) φ).Realize v (Fin.snoc default x) ↔
       φ.Realize (Fin.snoc v x) := by
   rw [realize_relabel_finSumFinEquiv_symm]
-  have hsnoc :
-      Sum.elim v (Fin.snoc default x) ∘ (fun i => finSumFinEquiv.symm i) = Fin.snoc v x := by
-    ext i
-    refine Fin.addCases (fun i => ?_) (fun j => ?_) i
-    · simp only [Function.comp_apply, finSumFinEquiv_symm_apply_castAdd, Sum.elim_inl]
-      change v i = (@Fin.snoc m (fun _ => M) v x) i.castSucc
-      rw [Fin.snoc_castSucc]
-    · have hj : j = 0 := Fin.ext (Nat.le_zero.mp (Nat.lt_succ_iff.mp j.isLt))
-      subst hj
-      simp only [Function.comp_apply, finSumFinEquiv_symm_apply_natAdd, Sum.elim_inr, Fin.snoc_zero]
-      change x = (@Fin.snoc m (fun _ => M) v x) (Fin.last m)
-      rw [Fin.snoc_last]
-  rw [hsnoc]
+  refine iff_of_eq (congrArg _ (funext fun i => ?_))
+  induction i using Fin.lastCases with
+  | last => simp [Fin.snoc_zero]
+  | cast i => simp
 
 @[simp]
 theorem realize_equal {t₁ t₂ : L.Term α} {x : α → M} :
