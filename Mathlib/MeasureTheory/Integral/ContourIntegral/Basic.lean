983132ec61ae99365a0e3e9002e59102a36e0657
Yury G. Kudryashov <urkud@urkud.name>
2026-05-12 10:09:09 -0500

    Fix, add docs

--- Mathlib/MeasureTheory/Integral/ContourIntegral/Basic.lean
+++ Mathlib/MeasureTheory/Integral/ContourIntegral/Basic.lean
@@ -1,6 +1,20 @@
+/-
+Copyright (c) 2026 Yury Kudryashov. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Yury Kudryashov
+-/
 module
-
 public import Mathlib.MeasureTheory.Integral.CurveIntegral.Basic
+
+/-!
+# Contour integrals
+
+In this file we specialize the definition of `curveIntegral` to paths in `ℂ`.
+In this case, we integrate functions instead of 1-forms,
+interpreting a function `f` as the 1-form `f(z)dz`,
+written as `ContinuousLinearMap.toSpanSingleton ℂ ∘ f`.
+-/
+
 
 @[expose] public noncomputable section
 
@@ -9,10 +23,17 @@ open scoped unitInterval Convex
 
 variable {E : Type*} [NormedAddCommGroup E] [NormedSpace ℂ E] {a b : ℂ}
 
+/-- A function `f : ℂ → E` is *contour integrable* along a path `γ`,
+if the 1-form $f(z)\,dz$ is curve integrable along `γ`.
+
+Equivalently, this means that `φ(t)=γ'(t)•f(γ(t))` is interval integrable on `[0, 1]`. -/
 def ContourIntegrable (f : ℂ → E) (γ : Path a b) :=
   CurveIntegrable (.toSpanSingleton ℂ ∘ f) γ
 
-/-- TODO -/
+/-- The *contour integral* of a function `f : ℂ → E` along a path `γ`,
+defined as the curve integral of $f(z)\,dz$ along `γ`.
+
+Equivalently, this is given by the integral of `γ'(t)•f(t)` along `[0, 1]`. -/
 def contourIntegral (f : ℂ → E) (γ : Path a b) : E :=
   ∫ᶜ x in γ, .toSpanSingleton ℂ (f x)
 
@@ -30,7 +51,6 @@ theorem contourIntegral_eq_intervalIntegral_derivWithin_smul (f : ℂ → E) (γ
 theorem contourIntegral_eq_intervalIntegral_deriv_smul (f : ℂ → E) (γ : Path a b) :
     ∫ꟲ x in γ, f x = ∫ t in 0..1, deriv γ.extend t • f (γ.extend t) := by
   simp [contourIntegral, curveIntegral_eq_intervalIntegral_deriv]
-
 
 /-!
 ### Operations on paths
@@ -121,9 +141,11 @@ section Algebra
 variable {f f₁ f₂ : ℂ → E} {γ : Path a b} {t : ℝ}
 
 @[to_fun]
-protected theorem ContourIntegrable.add (h₁ : ContourIntegrable f₁ γ) (h₂ : ContourIntegrable f₂ γ) :
+protected theorem ContourIntegrable.add (h₁ : ContourIntegrable f₁ γ)
+    (h₂ : ContourIntegrable f₂ γ) :
     ContourIntegrable (f₁ + f₂) γ := by
-  simpa [ContourIntegrable] using CurveIntegrable.add h₁ h₂
+  simpa [ContourIntegrable, Function.comp_def, ContinuousLinearMap.toSpanSingleton_add]
+    using CurveIntegrable.add h₁ h₂
 
 theorem contourIntegral_add (h₁ : ContourIntegrable f₁ γ) (h₂ : ContourIntegrable f₂ γ) :
     contourIntegral (f₁ + f₂) γ = ∫ꟲ x in γ, f₁ x + ∫ꟲ x in γ, f₂ x := by
@@ -133,7 +155,7 @@ theorem contourIntegral_fun_add (h₁ : ContourIntegrable f₁ γ) (h₂ : Conto
     ∫ꟲ x in γ, (f₁ x + f₂ x) = ∫ꟲ x in γ, f₁ x + ∫ꟲ x in γ, f₂ x :=
   contourIntegral_add h₁ h₂
 
-@[to_fun]
+@[to_fun (attr := simp)]
 theorem ContourIntegrable.zero : ContourIntegrable (0 : ℂ → E) γ := by
   simp [ContourIntegrable]
 
@@ -143,7 +165,6 @@ theorem contourIntegral_zero : contourIntegral (0 : ℂ → E) γ = 0 := by simp
 @[simp]
 theorem contourIntegral_fun_zero : ∫ꟲ _ in γ, (0 : E) = 0 := contourIntegral_zero
 
--- TODO: add `ContinuousLinearMap.toSpanSingleton_neg`
 @[to_fun]
 theorem ContourIntegrable.neg (h : ContourIntegrable f γ) : ContourIntegrable (-f) γ := by
   simpa [ContourIntegrable, Function.comp_def, ContinuousLinearMap.toSpanSingleton_neg]
@@ -159,12 +180,13 @@ theorem contourIntegrable_fun_neg_iff : ContourIntegrable (-f ·) γ ↔ Contour
 
 @[simp]
 theorem contourIntegral_neg : contourIntegral (-f) γ = -∫ꟲ x in γ, f x := by
-  simp [contourIntegral]
+  simp [contourIntegral, ContinuousLinearMap.toSpanSingleton_neg]
 
 @[simp]
 theorem contourIntegral_fun_neg : ∫ꟲ x in γ, -f x = -∫ꟲ x in γ, f x := contourIntegral_neg
 
-protected theorem ContourIntegrable.sub (h₁ : ContourIntegrable f₁ γ) (h₂ : ContourIntegrable f₂ γ) :
+protected theorem ContourIntegrable.sub (h₁ : ContourIntegrable f₁ γ)
+    (h₂ : ContourIntegrable f₂ γ) :
     ContourIntegrable (f₁ - f₂) γ :=
   sub_eq_add_neg f₁ f₂ ▸ h₁.add h₂.neg
 
@@ -176,134 +198,23 @@ theorem contourIntegral_fun_sub (h₁ : ContourIntegrable f₁ γ) (h₂ : Conto
     ∫ꟲ x in γ, (f₁ x - f₂ x) = ∫ꟲ x in γ, f₁ x - ∫ꟲ x in γ, f₂ x :=
   contourIntegral_sub h₁ h₂
 
-
-section RestrictScalars
-
-variable {𝕝 : Type*} [RCLike 𝕝] [NormedSpace 𝕝 F] [NormedSpace 𝕝 E]
-  [LinearMap.CompatibleSMul E F 𝕝 𝕜]
+variable {𝕜 : Type*} [RCLike 𝕜] [NormedSpace 𝕜 E] [SMulCommClass ℂ 𝕜 E] {c : 𝕜}
 
 @[simp]
-theorem contourIntegralFun_restrictScalars :
-    contourIntegralFun (fun t ↦ (f t).restrictScalars 𝕝) γ = contourIntegralFun f γ := by
-  ext
-  letI : NormedSpace ℝ E := .restrictScalars ℝ 𝕜 E
-  simp [contourIntegralFun_def]
+theorem contourIntegrable_smul_iff :
+    ContourIntegrable (c • f) γ ↔ c = 0 ∨ ContourIntegrable f γ := by
+  simp [ContourIntegrable, Function.comp_def, ContinuousLinearMap.toSpanSingleton_smul,
+    curveIntegrable_fun_smul_iff]
 
-@[simp]
-theorem contourIntegrable_restrictScalars_iff :
-    ContourIntegrable (fun t ↦ (f t).restrictScalars 𝕝) γ ↔ ContourIntegrable f γ := by
-  simp [ContourIntegrable]
-
-@[simp]
-theorem contourIntegral_restrictScalars :
-    ∫ꟲ x in γ, (f x).restrictScalars 𝕝 = ∫ꟲ x in γ, f x := by
-  letI : NormedSpace ℝ F := .restrictScalars ℝ 𝕜 F
-  simp [contourIntegral_def]
-
-end RestrictScalars
-
-variable {𝕝 : Type*} [RCLike 𝕝] [NormedSpace 𝕝 F] [SMulCommClass 𝕜 𝕝 F] {c : 𝕝}
-
-@[simp]
-theorem contourIntegralFun_smul : contourIntegralFun (c • f) γ = c • contourIntegralFun f γ := by
-  ext
-  simp [contourIntegralFun]
-
-theorem ContourIntegrable.smul (h : ContourIntegrable f γ) :
+theorem ContourIntegrable.smul (h : ContourIntegrable f γ) (c : 𝕜) :
     ContourIntegrable (c • f) γ := by
-  simpa [ContourIntegrable] using IntervalIntegrable.smul h c
-
-@[simp]
-theorem contourIntegrable_smul_iff : ContourIntegrable (c • f) γ ↔ c = 0 ∨ ContourIntegrable f γ := by
-  rcases eq_or_ne c 0 with rfl | hc
-  · simp [ContourIntegrable.zero]
-  · simp only [hc, false_or]
-    refine ⟨fun h ↦ ?_, .smul⟩
-    simpa [hc] using h.smul (c := c⁻¹)
+  simp [h]
 
 @[simp]
 theorem contourIntegral_smul : contourIntegral (c • f) γ = c • contourIntegral f γ := by
-  letI : NormedSpace ℝ F := .restrictScalars ℝ 𝕜 F
-  simp [contourIntegral_def, intervalIntegral.integral_smul]
+  simp [contourIntegral, ContinuousLinearMap.toSpanSingleton_smul]
 
 @[simp]
 theorem contourIntegral_fun_smul : ∫ꟲ x in γ, c • f x = c • ∫ꟲ x in γ, f x := contourIntegral_smul
 
 end Algebra
-
-section FDeriv
-
-variable {𝕜 E F : Type*} [RCLike 𝕜] [NormedAddCommGroup E] [NormedSpace 𝕜 E]
-  [NormedAddCommGroup F] [NormedSpace 𝕜 F] [CompleteSpace F]
-  {a b : E} {s : Set E} {f : ℂ → E}
-
-/-!
-### Derivative of the curve integral w.r.t. the right endpoint
-
-In this section we prove that the integral of `f` along `[a -[ℝ] b]`, as a function of `b`,
-has derivative `f a` at `b = a`.
-We provide several versions of this theorem, for `HasFDerivWithinAt` and `HasFDerivAt`,
-as well as for continuity near a point and for continuity on the whole set or space.
-
-Note that we take the derivative at the left endpoint of the segment.
-Similar facts about the derivative at a different point are true
-provided that `f` is a closed 1-form (formalization WIP, see #24019).
--/
-
-/-- The integral of `f` along `[a -[ℝ] b]`, as a function of `b`, has derivative `f a` at `b = a`.
-This is a `HasFDerivWithinAt` version assuming that `f` is continuous within a convex set `s`
-in a neighborhood of `a` within `s`. -/
-theorem HasFDerivWithinAt.contourIntegral_segment_source' (hs : Convex ℝ s)
-    (hf : ∀ᶠ x in 𝓝[s] a, ContinuousWithinAt f s x) (ha : a ∈ s) :
-    HasFDerivWithinAt (∫ꟲ x in .segment a ·, f x) (f a) s a := by
-  /- Given `ε > 0`, take a number `δ > 0` such that `f` is continuous on `ball a δ ∩ s`
-  and `‖f z - f a‖ ≤ ε` on this set.
-  Then for `b ∈ ball a δ ∩ s`, we have
-  `‖(∫ꟲ x in .segment a b, f x) - f a (b - a)‖
-    = ‖(∫ꟲ x in .segment a b, f x) - ∫ꟲ x in .segment a b, f a‖
-    ≤ ∫ x in 0..1, ‖f x - f a‖ * ‖b - a‖
-    ≤ ε * ‖b - a‖`
-  -/
-  simp only [hasFDerivWithinAt_iff_isLittleO, Path.segment_same, contourIntegral_refl, sub_zero,
-    Asymptotics.isLittleO_iff]
-  intro ε hε
-  obtain ⟨δ, hδ₀, hδ⟩ : ∃ δ > 0,
-      ball a δ ∩ s ⊆ {z | ContinuousWithinAt f s z ∧ dist (f z) (f a) ≤ ε} := by
-    rw [← Metric.mem_nhdsWithin_iff, setOf_and, inter_mem_iff]
-    exact ⟨hf, (hf.self_of_nhdsWithin ha).eventually <| closedBall_mem_nhds _ hε⟩
-  rw [eventually_nhdsWithin_iff]
-  filter_upwards [Metric.ball_mem_nhds _ hδ₀] with b hb hbs
-  have hsub : [a -[ℝ] b] ⊆ ball a δ ∩ s :=
-    ((convex_ball _ _).inter hs).segment_subset (by simp [*]) (by simp [*])
-  rw [← contourIntegral_segment_const, ← contourIntegral_fun_sub]
-  · refine norm_contourIntegral_segment_le fun z hz ↦ ?_
-    simpa [dist_eq_norm] using (hδ (hsub hz)).2
-  · rw [contourIntegrable_segment]
-    refine ContinuousOn.intervalIntegrable_of_Icc zero_le_one fun t ht ↦ ?_
-    refine ((hδ ?_).1.eval_const _).comp AffineMap.lineMap_continuous.continuousWithinAt ?_
-    · exact hsub <| lineMap_mem_segment ℝ a b ht
-    · rw [mapsTo_iff_image_subset, ← segment_eq_image_lineMap]
-      exact hs.segment_subset ha hbs
-  · rw [contourIntegrable_segment]
-    exact intervalIntegrable_const
-
-/-- The integral of `f` along `[a -[ℝ] b]`, as a function of `b`, has derivative `f a` at `b = a`.
-This is a `HasFDerivWithinAt` version assuming that `f` is continuous on `s`. -/
-theorem HasFDerivWithinAt.contourIntegral_segment_source (hs : Convex ℝ s) (hf : ContinuousOn f s)
-    (ha : a ∈ s) : HasFDerivWithinAt (∫ꟲ x in .segment a ·, f x) (f a) s a :=
-  .contourIntegral_segment_source' hs (mem_of_superset self_mem_nhdsWithin hf) ha
-
-/-- The integral of `f` along `[a -[ℝ] b]`, as a function of `b`, has derivative `f a` at `b = a`.
-This is a `HasFDerivAt` version assuming that `f` is continuous in a neighborhood of `a`. -/
-theorem HasFDerivAt.contourIntegral_segment_source' (hf : ∀ᶠ z in 𝓝 a, ContinuousAt f z) :
-    HasFDerivAt (∫ꟲ x in .segment a ·, f x) (f a) a :=
-  HasFDerivWithinAt.contourIntegral_segment_source' convex_univ
-    (by simpa only [nhdsWithin_univ, continuousWithinAt_univ]) (mem_univ _) |>.hasFDerivAt_of_univ
-
-/-- The integral of `f` along `[a -[ℝ] b]`, as a function of `b`, has derivative `f a` at `b = a`.
-This is a `HasFDerivAt` version assuming that `f` is continuous on the whole space. -/
-theorem HasFDerivAt.contourIntegral_segment_source (hf : Continuous f) :
-    HasFDerivAt (∫ꟲ x in .segment a ·, f x) (f a) a :=
-  .contourIntegral_segment_source' <| .of_forall fun _ ↦ hf.continuousAt
-
-end FDeriv
