488cd5df41b8137b274bb57c9d01133bf9ad22b2
Tomas Skrivan <skrivantomas@seznam.cz>
2026-05-13 23:35:10 +0200

    clean up the code

--- Mathlib/Tactic/RunAutoParam.lean
+++ Mathlib/Tactic/RunAutoParam.lean
@@ -1,11 +1,24 @@
-import Lean
--- import Mathlib
+/-
+Copyright (c) 2026 Tomas Skrivan. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Tomas Skrivan
+-/
 
+import Lean
+
+/-!
+# Tactic `run_auto_param` is designed to solve goals in the form `autoParam P tac` by executing
+tactic `tac` on the goal `P`. It is mainly meant to be used as a discharger for other tactics like
+`simp` or `fun_prop`.
+-/
 open Lean Meta Elab Tactic
 
 namespace Mathlib.Meta
 
-private def tacticToDischarge' (tacticCode : Syntax) : TacticM (IO.Ref Term.State × (Expr → MetaM (Option Expr))) := do
+/-- Turns tactic syntax into `Expr → MetaM (Option Expr)`. This function is a direct copy of
+`simp`'s `tacticToDischarge`.-/
+private def tacticToDischarge' (tacticCode : Syntax) :
+    TacticM (IO.Ref Term.State × (Expr → MetaM (Option Expr))) := do
   let tacticCode : TSyntax `Lean.Parser.Tactic.tacticSeq := ⟨tacticCode⟩
   let tacticCode ← `(tactic| try ($tacticCode:tacticSeq))
   let ref ← IO.mkRef (← getThe Term.State)
@@ -15,8 +28,9 @@ private def tacticToDischarge' (tacticCode : Syntax) : TacticM (IO.Ref Term.Stat
     let s ← ref.get
     let runTac? : TermElabM (Option Expr) :=
       try
-        /- We must only save messages and info tree changes. Recall that `simp` uses temporary metavariables (`withNewMCtxDepth`).
-           So, we must not save references to them at `Term.State`.
+        /- We must only save messages and info tree changes. Recall that `simp` uses temporary
+           metavariables (`withNewMCtxDepth`). So, we must not save references to them at
+           `Term.State`.
         -/
         Term.withoutModifyingElabMetaStateWithInfo do
           Term.withSynthesize (postpone := .no) do
@@ -33,9 +47,12 @@ private def tacticToDischarge' (tacticCode : Syntax) : TacticM (IO.Ref Term.Stat
     return result?
   return (ref, disch)
 
+/-- Tactic `run_auto_param` is designed to solve goals in the form `autoParam P tac` by executing
+tactic `tac` on the goal `P`. It is mainly meant to be used as a discharger for other tactics like
+`simp` or `fun_prop`. -/
 syntax (name:=runAutoParamStx) "run_auto_param" : tactic
 
-@[tactic runAutoParamStx]
+@[tactic runAutoParamStx, inherit_doc runAutoParamStx]
 def runAutoParam : Tactic := fun _ => do
 
   let goal ← getMainGoal
