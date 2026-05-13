3e6b505cf5f06a12d2220457655e4cb361801b0c
Moritz Doll <moritz.doll@googlemail.com>
2026-05-13 17:55:44 -0400

    found one more assumption

--- MathlibTest/FunProp.lean
+++ MathlibTest/FunProp.lean
@@ -92,7 +92,7 @@ private theorem t1 : (5: ℕ) + (1 : ℕ∞) ≤ (12 : WithTop ℕ∞) := by nor
 
 example {f : ℝ → ℝ} (hf : ContDiff ℝ 12 f) :
     Differentiable ℝ (iteratedDeriv 5 (fun x ↦ f (2 * (f (x + x))) + x)) := by
-  fun_prop (disch := (exact t1))
+  fun_prop (disch := exact t1)
 
 -- This example used to panic due to loose bvars before #31001.
 -- TODO: this still fails because `fun_prop` cannot use `hl`.
