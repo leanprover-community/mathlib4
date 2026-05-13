9104db695487aee50381dfcadb53bf6e3b4e841b
Jun2M <rnjsgurwns27@gmail.com>
2026-05-13 11:29:57 -0400

    rm Step

--- Mathlib/Combinatorics/SimpleGraph/GraphLike.lean
+++ Mathlib/Combinatorics/SimpleGraph/GraphLike.lean
@@ -5,7 +5,7 @@ Authors: Jun Kwon
 -/
 module
 
-public import Mathlib.Combinatorics.GraphLike.Symm
+public import Mathlib.Combinatorics.GraphLike.Basic
 public import Mathlib.Combinatorics.SimpleGraph.Basic
 
 /-!
@@ -26,21 +26,25 @@ instance : noMultiEdgeSymmGraphLike V (V × V) (Sym2 V) (SimpleGraph V) where
   verts _ := Set.univ
   darts G := { (u, v) | G.Adj u v }
   edges G := G.edgeSet
-  src := Prod.fst
-  tgt := Prod.snd
-  edge d := s(d.fst, d.snd)
-  inv := Prod.swap
-  inv_invol := Prod.swap_swap
-  inv_src d := by simp
-  inv_tgt d := by simp
+  source G s := s.fst
+  target G s := s.snd
+  edge G s := s(s.fst, s.snd)
+  inv s := Prod.swap s
+  inv_invol _ := rfl
+  inv_source G _ := rfl
+  inv_target G _ := rfl
+  source_mem_of_darts _ _ _ := Set.mem_univ _
+  target_mem_of_darts _ _ _ := Set.mem_univ _
+  edge_mem_of_darts G d hd := by simpa using hd
+  src_tgt_inj _ := Function.injective_id
   inv_ne G d hd := by grind [hd.ne]
   inv_mem_darts_iff G d := by simp [G.adj_comm]
-  src_mem_of_darts _ _ _ := Set.mem_univ _
-  tgt_mem_of_darts _ _ _ := Set.mem_univ _
-  src_tgt_inj := Function.injective_id
-  edge_mem_of_darts G d hd := by simpa using hd
   edge_eq_edge_iff G d d' hd hd' := by grind
   Adj G := G.Adj
   exists_darts_iff_adj := by simp
+
+attribute [simp] source_def target_def edge_def inv_def
+
+@[simp] lemma toProd_eq (d : D(G)) : toProd d = d.val := rfl
 
 end SimpleGraph
