c49a048e8ee478083324cee11f570c345d530e32
Nailin Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2026-05-13 16:45:52 +0000

    chore(RingTheory): rearrange lemma about square of `IsLocalRing.maximalIdeal` (#39292)

--- Mathlib.lean
+++ Mathlib.lean
@@ -6608,6 +6608,7 @@ public import Mathlib.RingTheory.LocalRing.Length
 public import Mathlib.RingTheory.LocalRing.LocalSubring
 public import Mathlib.RingTheory.LocalRing.MaximalIdeal.Basic
 public import Mathlib.RingTheory.LocalRing.MaximalIdeal.Defs
+public import Mathlib.RingTheory.LocalRing.MaximalIdeal.Square
 public import Mathlib.RingTheory.LocalRing.Module
 public import Mathlib.RingTheory.LocalRing.NonLocalRing
 public import Mathlib.RingTheory.LocalRing.Quotient
