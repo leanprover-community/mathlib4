3c58317e087b5836de8be880b9cdd9f3d8aa935a
Jun2M <rnjsgurwns27@gmail.com>
2026-05-13 11:38:39 -0400

    notation

--- Mathlib/Combinatorics/GraphLike/Basic.lean
+++ Mathlib/Combinatorics/GraphLike/Basic.lean
@@ -115,26 +115,27 @@ graph-like structures.
 
 /-- A graph-like structure with no multi-edge. This includes `SimpleGraph` and `Digraph`. -/
 class NoMultiEdgeGraphLike (V D E : outParam Type*) (Gr : Type*) extends GraphLike V D E Gr where
-  protected src_tgt_inj (G : Gr) : Function.Injective (fun d ↦ (source G d, target G d))
+  protected source_target_inj (G : Gr) : Function.Injective (fun d ↦ (source G d, target G d))
 
 variable [NoMultiEdgeGraphLike V D E Gr]
 
-lemma dart_eq_of_src_tgt_eq {d₁ d₂ : D} (h : source G d₁ = source G d₂)
+lemma dart_eq_of_source_target_eq {d₁ d₂ : D} (h : source G d₁ = source G d₂)
     (h' : target G d₁ = target G d₂) : d₁ = d₂ := by
-  apply NoMultiEdgeGraphLike.src_tgt_inj G
+  apply NoMultiEdgeGraphLike.source_target_inj G
   grind
 
-lemma src_tgt_inj (d₁ d₂ : D) : source G d₁ = source G d₂ ∧ target G d₁ = target G d₂ ↔ d₁ = d₂ :=
-  ⟨fun h => dart_eq_of_src_tgt_eq h.1 h.2, by grind⟩
+lemma source_target_inj (d₁ d₂ : D) :
+    source G d₁ = source G d₂ ∧ target G d₁ = target G d₂ ↔ d₁ = d₂ :=
+  ⟨fun h => dart_eq_of_source_target_eq h.1 h.2, by grind⟩
 
 @[simp]
-lemma mem_darts_iff_adj : d ∈ darts G ↔ Adj G (source G d) (target G d) := by
+lemma mem_darts_iff_adj : d ∈ D(G) ↔ Adj G (source G d) (target G d) := by
   rw [← exists_darts_iff_adj]
   refine ⟨fun h => (by use d), fun ⟨d', hd', hs, ht⟩ => ?_⟩
-  obtain rfl := src_tgt_inj d' d |>.mp ⟨hs, ht⟩
+  obtain rfl := source_target_inj d' d |>.mp ⟨hs, ht⟩
   exact hd'
 
-instance [DecidableRel (Adj G)] : DecidablePred (· ∈ darts G) :=
+instance [DecidableRel (Adj G)] : DecidablePred (· ∈ D(G)) :=
   fun d => decidable_of_iff (Adj G (source G d) (target G d)) mem_darts_iff_adj.symm
 
 end noMultiEdgeGraphLike
@@ -160,8 +161,7 @@ attribute [grind →] inv_ne
 
 variable [SymmGraphLike V D E Gr]
 
-lemma inv_mem_darts (hd : d ∈ darts G) : inv Gr d ∈ darts G :=
-  inv_mem_darts_iff.mpr hd
+lemma inv_mem_darts (hd : d ∈ D(G)) : inv Gr d ∈ D(G) := inv_mem_darts_iff.mpr hd
 
 instance : Std.Symm (Adj G) where
   symm _ _ h := by
@@ -173,45 +173,45 @@ instance : Std.Symm (Adj G) where
 
 lemma adj_comm : Adj G v w ↔ Adj G w v := ⟨symm_of (Adj G), symm_of (Adj G)⟩
 
-lemma adj_tgt_src (hd : d ∈ darts G) : Adj G (target G d) (source G d) :=
-  symm_of (Adj G) (adj_source_target hd)
+lemma adj_target_source (hd : d ∈ D(G)) : Adj G (target G d) (source G d) :=
+  (adj_source_target hd).symm
 
 /-- The two vertices of the dart as an unordered pair. -/
-@[expose] def dartSym2 (d : darts G) : Sym2 V := s(source G d.val, target G d.val)
+@[expose] def dartSym2 (d : D(G)) : Sym2 V := s(source G d.val, target G d.val)
 
 @[simp]
-theorem dartSym2_mk (h : d ∈ darts G) : dartSym2 (⟨d, h⟩ : darts G) = s(source G d, target G d) :=
+theorem dartSym2_mk (h : d ∈ D(G)) : dartSym2 (⟨d, h⟩ : D(G)) = s(source G d, target G d) :=
   rfl
 
 /-- The dart with reversed orientation from a given dart. -/
-@[expose] def dartSymm (d : darts G) : darts G := ⟨inv Gr d.val, inv_mem_darts_iff.mpr d.prop⟩
+@[expose] def dartSymm (d : D(G)) : D(G) := ⟨inv Gr d.val, inv_mem_darts_iff.mpr d.prop⟩
 
 @[simp]
-theorem dartSymm_mk (h : d ∈ darts G) : dartSymm (⟨d, h⟩) = ⟨inv Gr d, inv_mem_darts_iff.mpr h⟩ :=
+theorem dartSymm_mk (h : d ∈ D(G)) : dartSymm (⟨d, h⟩) = ⟨inv Gr d, inv_mem_darts_iff.mpr h⟩ :=
   rfl
 
 @[simp]
-theorem dartSym2_inv (d : darts G) : dartSym2 (dartSymm d) = dartSym2 d := by
+theorem dartSym2_inv (d : D(G)) : dartSym2 (dartSymm d) = dartSym2 d := by
   simp [dartSym2, dartSymm]
 
 @[simp]
-theorem dartSym2_comp_inv : dartSym2 ∘ dartSymm = (dartSym2 : darts G → Sym2 _) :=
+theorem dartSym2_comp_inv : dartSym2 ∘ dartSymm = (dartSym2 : D(G) → Sym2 _) :=
   funext dartSym2_inv
 
 @[simp]
-theorem dartSymm_dartSymm (d : darts G) : dartSymm (dartSymm d) = d :=
+theorem dartSymm_dartSymm (d : D(G)) : dartSymm (dartSymm d) = d :=
   darts_ext _ _ <| by simp [dartSymm]
 
 @[simp]
-theorem dartSymm_involutive : Function.Involutive (dartSymm : darts G → darts G) :=
+theorem dartSymm_involutive : Function.Involutive (dartSymm : D(G) → D(G)) :=
   dartSymm_dartSymm
 
-theorem dartSym2_eq_mk'_iff {d : darts G} :
+theorem dartSym2_eq_mk'_iff {d : D(G)} :
     dartSym2 d = s(u, v) ↔ toProd d = (u, v) ∨ toProd d = (v, u) := by
   obtain ⟨p, hp⟩ := d
   simp [toProd]
 
-theorem dartSym2_eq_mk'_iff' {d : darts G} : dartSym2 d = s(u, v) ↔
+theorem dartSym2_eq_mk'_iff' {d : D(G)} : dartSym2 d = s(u, v) ↔
     source G d.val = u ∧ target G d.val = v ∨ source G d.val = v ∧ target G d.val = u := by
   obtain ⟨p, hp⟩ := d
   simp
@@ -228,7 +228,7 @@ class noMultiEdgeSymmGraphLike (V D E : outParam Type*) (Gr : Type*) extends
     SymmGraphLike V D E Gr, NoMultiEdgeGraphLike V D E Gr  where
 
 @[simp]
-theorem dartSym2_eq_iff [noMultiEdgeSymmGraphLike V D E Gr] (d₁ d₂ : darts G) :
+theorem dartSym2_eq_iff [noMultiEdgeSymmGraphLike V D E Gr] (d₁ d₂ : D(G)) :
     dartSym2 d₁ = dartSym2 d₂ ↔ d₁ = d₂ ∨ d₁ = dartSymm d₂ := by
   obtain ⟨p, hp⟩ := d₁
   obtain ⟨q, hq⟩ := d₂
@@ -236,8 +236,8 @@ theorem dartSym2_eq_iff [noMultiEdgeSymmGraphLike V D E Gr] (d₁ d₂ : darts G
     Subtype.mk.injEq, dartSymm_mk]
   refine ⟨?_, by rintro (rfl | rfl) <;> simp⟩
   rintro (⟨h1, h2⟩ | ⟨h1, h2⟩)
-  · exact Or.inl <| dart_eq_of_src_tgt_eq h1 h2
-  exact Or.inr <| dart_eq_of_src_tgt_eq (inv_source G q ▸ h1) (inv_target G q ▸ h2)
+  · exact Or.inl <| dart_eq_of_source_target_eq h1 h2
+  exact Or.inr <| dart_eq_of_source_target_eq (inv_source G q ▸ h1) (inv_target G q ▸ h2)
 
 end noMultiEdgeGraphLike
 
