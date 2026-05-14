9b5e4cd411b74f83bb799d257c86876c14f209b8
Paul Lezeau <paul.lezeau@gmail.com>
2026-05-13 23:00:46 -0400

    better name

--- Mathlib/Order/Filter/Basic.lean
+++ Mathlib/Order/Filter/Basic.lean
@@ -947,7 +947,7 @@ theorem EventuallyEq.filter_mono {l l' : Filter α} {f g : α → β} (h₁ : f 
     f =ᶠ[l'] g :=
   h₂ h₁
 
-lemma EventuallyEq.comap {α β γ : Type*} {F : Filter β} {f g : β → γ} (h : α → β)
+lemma EventuallyEq.comp_comap {α β γ : Type*} {F : Filter β} {f g : β → γ} (h : α → β)
     (hfg : f =ᶠ[F] g) : f.comp h =ᶠ[comap h F] g.comp h := by
   use (disch := aesop) {x : β | f x = g x}
 
