e73a47967dfe2525e6a980e634f8e839f87fa46d
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2026-05-14 20:33:37 +0200

    add keep-all to all Tactic.Linter modules

--- Mathlib/Tactic/Linter/UnusedInstancesInType.lean
+++ Mathlib/Tactic/Linter/UnusedInstancesInType.lean
@@ -3,7 +3,7 @@ Copyright (c) 2025 Thomas R. Murrills. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Thomas R. Murrills
 -/
-module
+module -- shake: keep-all
 
 public meta import Mathlib.Lean.Expr.Basic
 public meta import Mathlib.Lean.Environment
--- Mathlib/Tactic/Linter/UnusedTacticExtension.lean
+++ Mathlib/Tactic/Linter/UnusedTacticExtension.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Damiano Testa
 -/
 
-module
+module -- shake: keep-all
 
 -- Import this linter explicitly to ensure that
 -- this file has a valid copyright header and module docstring.
