1ea6113e4bb2a309092bb6eec076f09e0b6e1ed8
Brian-Nugent <bnugent@uw.edu>
2026-05-14 16:40:07 -0600

    Delete old version and run #min_imports

--- Mathlib/Algebra/Category/ModuleCat/Sheaf/LocallyFree.lean
+++ Mathlib/Algebra/Category/ModuleCat/Sheaf/LocallyFree.lean
@@ -5,7 +5,7 @@ Authors: Brian Nugent
 -/
 module
 
-public import Mathlib
+public import Mathlib.Algebra.Category.ModuleCat.Sheaf.Quasicoherent
 
 /-!
 # Locally Free Sheaves
@@ -16,7 +16,7 @@ A sheaf of modules is locally free if it is locally isomorphic to a free module.
 
 @[expose] public section
 
-universe w u v₁ v₂ u₁ u₂
+universe u v₁ u₁
 
 open CategoryTheory Limits
 
@@ -31,88 +31,62 @@ section
 
 variable [HasWeakSheafify J AddCommGrpCat.{u}] [J.WEqualsLocallyBijective AddCommGrpCat.{u}]
   [J.HasSheafCompose (forget₂ RingCat.{u} AddCommGrpCat.{u})]
-
-@[simps]
-def free.generatingSections (I : Type u) : (free (R := R) I).GeneratingSections where
-  I := I
-  s (i) := freeSection i
-  epi := by
-    simp only [Equiv.symm_apply_apply]
-    infer_instance
-
-@[simp]
-lemma free.generatingSections_π_id (I : Type u) :
-    (free.generatingSections (R := R) I).π = 𝟙 (free I) :=
-  Equiv.symm_apply_apply (free I).freeHomEquiv _
-
-instance free.generatingSections.π_isIso (I : Type u) :
-    IsIso (free.generatingSections (R := R) I).π := by
-  simp only [generatingSections_I, generatingSections_π_id]
-  infer_instance
-
-end
-
-variable [HasSheafify J AddCommGrpCat.{u}] [J.WEqualsLocallyBijective AddCommGrpCat.{u}]
-  [J.HasSheafCompose (forget₂ RingCat.{u} AddCommGrpCat.{u})]
   [∀ X, (J.over X).HasSheafCompose (forget₂ RingCat.{u} AddCommGrpCat.{u})]
-  [∀ X, HasSheafify (J.over X) AddCommGrpCat.{u}]
+  [∀ X, HasWeakSheafify (J.over X) AddCommGrpCat.{u}]
   [∀ X, (J.over X).WEqualsLocallyBijective AddCommGrpCat.{u}]
 
-structure LocallyFreeData (M : SheafOfModules.{u} R) where
-  /-- the index type of the covering -/
-  I : Type w
-  /-- a family of objects which cover the terminal object -/
-  X : I → C
-  coversTop : J.CoversTop X
-  /-- the index type for the iso -/
-  ι : I → Type u
-  /-- an isomorphism from a free module to `M.over (X i)` for any `i : I` -/
-  freeIso (i : I) : free (ι i) ≅ M.over (X i)
+namespace LocalGeneratorsData
+
+/-- Local generator data `q` is locally free data if all of the natural morphisms
+`free (q.generators i).I ⟶ M.over (q.X i)` are isomorphisms -/
+class IsLocallyFreeData {M : SheafOfModules.{u} R} (q : M.LocalGeneratorsData) : Prop where
+  iso : ∀ i, IsIso (q.generators i).π
+
+attribute [instance] IsLocallyFreeData.iso
+
+end LocalGeneratorsData
+
+/-- A sheaf of modules is locally free if there exists locally free data for it. -/
+class IsLocallyFree (M : SheafOfModules.{u} R) : Prop where
+  nonempty_locallyFreeData : ∃ q : M.LocalGeneratorsData, q.IsLocallyFreeData
+
+end
 
 section
 
-open ZeroObject
+variable [∀ X, (J.over X).HasSheafCompose (forget₂ RingCat.{u} AddCommGrpCat.{u})]
+  [∀ X, HasSheafify (J.over X) AddCommGrpCat.{u}]
+  [∀ X, (J.over X).WEqualsLocallyBijective AddCommGrpCat.{u}]
 
+namespace LocalGeneratorsData
+
+/-- Given locally free data, this is the quasiCoherentData where there are no relations. -/
 @[simps]
-def zero_generatingSections : (0 : SheafOfModules.{u} R).GeneratingSections where
-  I := ULift Empty
-  s (i) := Empty.rec (fun _ => sections 0) i.down
-  epi := inferInstance
-
-end
-
-@[simps]
-def free.presentation (I : Type u) : (free (R := R) I).Presentation where
-  generators := free.generatingSections I
-  relations := (GeneratingSections.equivOfIso (kernel.ofMono _)).symm zero_generatingSections
-
-namespace LocallyFreeData
-
-@[simps]
-def quasiCoherentData {M : SheafOfModules.{u} R} (q : M.LocallyFreeData) :
+def quasiCoherentData {M : SheafOfModules.{u} R} (q : M.LocalGeneratorsData) [q.IsLocallyFreeData] :
     M.QuasicoherentData where
   I := q.I
   X := q.X
   coversTop := q.coversTop
-  presentation (i) := Presentation.ofIsIso (q.freeIso i).hom (free.presentation (q.ι i))
+  presentation (i) := {
+    generators := q.generators i
+    relations := {
+      I := ULift Empty
+      s (j) := Empty.rec (fun _ => (kernel (q.generators i).π).sections) j.down
+      epi := IsZero.epi (IsZero.of_iso (isZero_zero _) (Limits.kernel.ofMono _)) _
+    }
+  }
 
-end LocallyFreeData
+@[simp]
+lemma quasiCoherentData_localGeneratorsData {M : SheafOfModules.{u} R}
+    (q : M.LocalGeneratorsData) [q.IsLocallyFreeData] :
+    q.quasiCoherentData.localGeneratorsData = q := rfl
 
-def LocalGeneratorsData.locallyFreeData {M : SheafOfModules.{u} R} (q : M.LocalGeneratorsData)
-    (h : ∀ i : q.I, IsIso (q.generators i).π) : M.LocallyFreeData where
-  I := q.I
-  X := q.X
-  coversTop := q.coversTop
-  ι (i) := (q.generators i).I
-  freeIso (i) := asIso (q.generators i).π
+end LocalGeneratorsData
 
-example {M : SheafOfModules.{u} R} (q : M.LocalGeneratorsData)
-    (h : ∀ i : q.I, IsIso (q.generators i).π) :
-    (q.locallyFreeData h).quasiCoherentData.localGeneratorsData = q := sorry
-
-class IsLocallyFree (M : SheafOfModules.{u} R) : Prop where
-  nonempty_locallyFreeData : Nonempty (LocallyFreeData.{u₁} M) := by infer_instance
-
-end SheafOfModules
+instance (M : SheafOfModules.{u} R) [h : M.IsLocallyFree] : M.IsQuasicoherent :=
+  have := h.nonempty_locallyFreeData.choose_spec
+  h.nonempty_locallyFreeData.choose.quasiCoherentData.isQuasicoherent
 
 end
+
+end SheafOfModules
