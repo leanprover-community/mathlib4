4c1017d40014affbd1e25055ba88b70011e25d24
Xavier Roblot <46200072+xroblot@users.noreply.github.com>
2026-05-14 18:21:39 +0200

    fix name

--- Mathlib/RingTheory/Ideal/GoingUp.lean
+++ Mathlib/RingTheory/Ideal/GoingUp.lean
@@ -447,11 +447,10 @@ end IsIntegral
 
 end Ideal
 
-instance toto (A B : Type*) [CommRing A] [CommRing B] [IsDomain B] [Algebra A B]
+instance (A B : Type*) [CommRing A] [CommRing B] [IsDomain B] [Algebra A B]
     [Algebra.IsIntegral A B] [FaithfulSMul A B] [Module.Flat A B] :
     Module.FaithfullyFlat A B := by
   refine Module.FaithfullyFlat.of_comap_surjective fun P ↦ ?_
   obtain ⟨P, hP₁, hP₂⟩ := Ideal.exists_ideal_over_prime_of_isIntegral_of_isDomain P.1 (S := B)
     (by simp [(RingHom.injective_iff_ker_eq_bot _).mp (FaithfulSMul.algebraMap_injective A B)])
   exact ⟨⟨P, hP₁⟩, PrimeSpectrum.ext_iff.mpr hP₂⟩
-
