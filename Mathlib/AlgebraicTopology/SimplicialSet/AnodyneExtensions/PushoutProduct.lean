da8315e0e0b710be4d117e39dbea9d07f86d777d
Joël Riou <joel.riou@universite-paris-saclay.fr>
2026-05-14 12:07:46 +0200

    added coauthor

--- Mathlib/AlgebraicTopology/SimplicialSet/AnodyneExtensions/PushoutProduct.lean
+++ Mathlib/AlgebraicTopology/SimplicialSet/AnodyneExtensions/PushoutProduct.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2026 Joël Riou. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Joël Riou
+Authors: Joël Riou, Jack McKoen
 -/
 module
 
