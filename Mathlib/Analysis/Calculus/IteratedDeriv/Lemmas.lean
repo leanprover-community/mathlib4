70886e06ffb42b77323ebecf3a74f2fa63c8e283
Michael Rothgang <rothgang@math.uni-bonn.de>
2026-05-16 01:10:20 +0200

    Update Mathlib/Analysis/Calculus/IteratedDeriv/Lemmas.lean
    Co-authored-by: Jovan Gerbscheid <56355248+JovanGerb@users.noreply.github.com>

--- Mathlib/Analysis/Calculus/IteratedDeriv/Lemmas.lean
+++ Mathlib/Analysis/Calculus/IteratedDeriv/Lemmas.lean
@@ -208,7 +208,7 @@ theorem iteratedDerivWithin_comp_const_smul (hf : ContDiffOn 𝕜 n f s) (c : �
     rw [iteratedDerivWithin_succ, derivWithin_congr h₀ (ih hx hf.of_succ),
       derivWithin_fun_const_smul (c ^ n) h₂, iteratedDerivWithin_succ,
       ← Function.comp_def,
-      derivWithin.scomp x h₁ (differentiableWithinAt_id.const_mul _) hs,
+      derivWithin.scomp x h₁ (by fun_prop) hs,
       derivWithin_const_mul _ differentiableWithinAt_id, derivWithin_id' _ _ (h _ hx),
       smul_smul, mul_one, pow_succ]
 
