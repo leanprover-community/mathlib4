2dbff497c5983732d660337876c73f8697f56503
Joël Riou <joel.riou@universite-paris-saclay.fr>
2026-05-15 14:11:05 +0200

    docstring

--- Mathlib/AlgebraicTopology/SimplicialSet/WeaklyPolyhedralLike.lean
+++ Mathlib/AlgebraicTopology/SimplicialSet/WeaklyPolyhedralLike.lean
@@ -8,10 +8,21 @@ module
 public import Mathlib.AlgebraicTopology.SimplicialSet.ProdStdSimplex
 
 /-!
-# ...
+# Weakly polyhedral like simplicial sets
+
+In this file, we introduce a typeclass `SSet.IsWeaklyPolyhedralLike` for a
+simplicial set `X : SSet`: it says that for any non-degenerate simplex
+`x : X _⦋n⦌`, the corresponding morphism `Δ[n] ⟶ X` is a monomorphism.
+This notion is useful in the context of the study of the subdivision
+functor (TODO @joelriou).
+
+The condition `IsWeaklyPolyhedralLike` is a weaker condition compared
+to the notion of "polyhedral complex" which appears in the article
+*Simplicial approximation* by Jardine, and which says that there
+exists a monomorphism `X ⟶ nerve T` where `T` is a partially ordered type.
 
 ## References
-* [Sean Moss, *Another approach to the Kan-Quillen model structure*][moss-2020]
+* [J. F. Jardine, *Simplicial approximation*][jardine-2004]
 
 -/
 
@@ -35,38 +46,22 @@ lemma IsWeaklyPolyhedralLike.mono' [X.IsWeaklyPolyhedralLike]
     {n : ℕ} (x : X _⦋n⦌) (hx : x ∈ X.nonDegenerate n) :
     Mono (yonedaEquiv.symm x) := mono ⟨x, hx⟩
 
-lemma IsWeaklyPolyhedralLike.of_iso (e : X ≅ Y) [X.IsWeaklyPolyhedralLike] :
-    Y.IsWeaklyPolyhedralLike where
+lemma IsWeaklyPolyhedralLike.of_mono (f : X ⟶ Y) [Mono f] [Y.IsWeaklyPolyhedralLike] :
+    X.IsWeaklyPolyhedralLike where
   mono := by
-    intro n ⟨y, hy⟩
-    obtain ⟨x, rfl⟩ := (ConcreteCategory.bijective_of_isIso (e.hom.app _)).surjective y
-    rw [nonDegenerate_iff_of_mono] at hy
-    have := mono' x hy
-    rw [← SSet.yonedaEquiv_symm_comp]
-    infer_instance
+    intro n ⟨x, hx⟩
+    rw [← nonDegenerate_iff_of_mono f] at hx
+    have := mono' _ hx
+    rw [← SSet.yonedaEquiv_symm_comp] at this
+    exact mono_of_mono _ f
 
-lemma nonDegenerate_δ [X.IsWeaklyPolyhedralLike]
-    {n : ℕ} {x : X _⦋n + 1⦌} (hx : x ∈ X.nonDegenerate _)
-    (i : Fin (n + 2)) :
-    X.δ i x ∈ X.nonDegenerate _ := by
-  have : Mono (yonedaEquiv.symm x) := IsWeaklyPolyhedralLike.mono ⟨x, hx⟩
-  have : X.δ i x = (yonedaEquiv.symm x).app _
-    (stdSimplex.objEquiv.symm (SimplexCategory.δ i)) := rfl
-  rw [this, nonDegenerate_iff_of_mono, stdSimplex.mem_nonDegenerate_iff_mono,
-    Equiv.apply_symm_apply]
-  infer_instance
+lemma IsWeaklyPolyhedralLike.of_iso (e : X ≅ Y) [X.IsWeaklyPolyhedralLike] :
+    Y.IsWeaklyPolyhedralLike :=
+  .of_mono e.inv
 
-lemma IsWeaklyPolyhedralLike.δ_injective [X.IsWeaklyPolyhedralLike]
-    {n : ℕ} (x : X _⦋n + 1⦌) (hx : x ∈ X.nonDegenerate _)
-    (i j : Fin (n + 2)) (hij : X.δ i x = X.δ j x) : i = j := by
-  have := mono' x hx
-  change (yonedaEquiv.symm x).app _
-    (stdSimplex.objEquiv.symm (SimplexCategory.δ i)) =
-    (yonedaEquiv.symm x).app _
-      (stdSimplex.objEquiv.symm (SimplexCategory.δ j)) at hij
-  replace hij := injective_of_mono ((yonedaEquiv.symm x).app _) hij
-  simp only [EmbeddingLike.apply_eq_iff_eq] at hij
-  exact SimplexCategory.δ_injective hij
+instance (A : X.Subcomplex) [X.IsWeaklyPolyhedralLike] :
+    (A : SSet).IsWeaklyPolyhedralLike :=
+  .of_mono A.ι
 
 instance (T : Type*) [PartialOrder T] : (nerve T).IsWeaklyPolyhedralLike where
   mono := by
@@ -83,5 +78,29 @@ instance (n : SimplexCategory) : (stdSimplex.{u}.obj n).IsWeaklyPolyhedralLike :
 instance (n m : SimplexCategory) :
     (stdSimplex.{u}.obj n ⊗ stdSimplex.obj m).IsWeaklyPolyhedralLike :=
   IsWeaklyPolyhedralLike.of_iso (prodStdSimplex.isoNerve _ _).symm
+
+lemma nonDegenerate_δ [X.IsWeaklyPolyhedralLike]
+    {n : ℕ} {x : X _⦋n + 1⦌} (hx : x ∈ X.nonDegenerate _)
+    (i : Fin (n + 2)) :
+    X.δ i x ∈ X.nonDegenerate _ := by
+  have := IsWeaklyPolyhedralLike.mono' x hx
+  have : X.δ i x = (yonedaEquiv.symm x).app _
+    (stdSimplex.objEquiv.symm (SimplexCategory.δ i)) := rfl
+  rw [this, nonDegenerate_iff_of_mono, stdSimplex.mem_nonDegenerate_iff_mono,
+    Equiv.apply_symm_apply]
+  infer_instance
+
+lemma IsWeaklyPolyhedralLike.δ_injective [X.IsWeaklyPolyhedralLike]
+    {n : ℕ} (x : X _⦋n + 1⦌) (hx : x ∈ X.nonDegenerate _)
+    (i j : Fin (n + 2)) (hij : X.δ i x = X.δ j x) : i = j := by
+  have := mono' x hx
+  change
+    (yonedaEquiv.symm x).app _
+      (stdSimplex.objEquiv.symm (SimplexCategory.δ i)) =
+    (yonedaEquiv.symm x).app _
+      (stdSimplex.objEquiv.symm (SimplexCategory.δ j)) at hij
+  replace hij := injective_of_mono ((yonedaEquiv.symm x).app _) hij
+  simp only [EmbeddingLike.apply_eq_iff_eq] at hij
+  exact SimplexCategory.δ_injective hij
 
 end SSet
