c87428aec92fdabadc3d6f8c78edab4a4262ec83
Yaël Dillies <yael.dillies@gmail.com>
2026-05-15 11:10:58 +0200

    use `WithConv`

--- Mathlib/RingTheory/Bialgebra/Convolution.lean
+++ Mathlib/RingTheory/Bialgebra/Convolution.lean
@@ -34,67 +34,70 @@ variable {R A B C : Type*} [CommSemiring R]
 namespace AlgHom
 variable [CommSemiring A] [CommSemiring B] [Semiring C] [Bialgebra R C] [Algebra R A]
 
-instance : One (C →ₐ[R] A) where one := (Algebra.ofId R A).comp <| counitAlgHom R C
-instance : Mul (C →ₐ[R] A) where
-  mul f g := .comp (lmul' R) <| .comp (Algebra.TensorProduct.map f g) <| comulAlgHom R C
+instance : One (WithConv <| C →ₐ[R] A) where
+  one := toConv <| (Algebra.ofId R A).comp <| counitAlgHom R C
 
-instance : Pow (C →ₐ[R] A) ℕ := ⟨fun f n ↦ npowRec n f⟩
+instance : Mul (WithConv <| C →ₐ[R] A) where
+  mul f g := toConv <| .comp (lmul' R) <| .comp (map f.ofConv g.ofConv) <| comulAlgHom R C
 
-lemma convOne_def : (1 : C →ₐ[R] A) = (Algebra.ofId R A).comp (counitAlgHom ..) := rfl
-lemma convMul_def (f g : C →ₐ[R] A) :
-    f * g = (.comp (lmul' R) <| .comp (Algebra.TensorProduct.map f g) <| comulAlgHom R C) := rfl
+instance : Pow (WithConv <| C →ₐ[R] A) ℕ := ⟨fun f n ↦ npowRec n f⟩
 
-private lemma convPow_succ (f : C →ₐ[R] A) (n : ℕ) : f ^ (n + 1) = (f ^ n) * f := rfl
+lemma convOne_def : 1 = toConv ((Algebra.ofId R A).comp (counitAlgHom R C)) := rfl
+lemma convMul_def (f g : WithConv <| C →ₐ[R] A) :
+    f * g = toConv (.comp (lmul' R) <| .comp (map f.ofConv g.ofConv) <| comulAlgHom R C) := rfl
 
-lemma convOne_apply (c : C) : (1 : C →ₐ[R] A) c = algebraMap R A (counit c) := rfl
+private lemma convPow_succ (f : WithConv <| C →ₐ[R] A) (n : ℕ) : f ^ (n + 1) = (f ^ n) * f := rfl
 
-lemma convMul_apply (f g : C →ₐ[R] A) (c : C) :
-    (f * g) c = Algebra.TensorProduct.lift f g (fun _ _ ↦ .all _ _) (comul c) := by
+lemma convOne_apply (c : C) : (1 : WithConv <| C →ₐ[R] A) c = algebraMap R A (counit c) := rfl
+
+lemma convMul_apply (f g : WithConv <| C →ₐ[R] A) (c : C) :
+    (f * g) c = lift f.ofConv g.ofConv (fun _ _ ↦ .all ..) (comul c) := by
   simp only [convMul_def, coe_comp, Function.comp_apply, Bialgebra.comulAlgHom_apply]
   rw [← comp_apply]
   congr 1
   ext <;> simp
 
-lemma toLinearMap_convOne : toConv (1 : C →ₐ[R] A).toLinearMap = 1 := rfl
-lemma toLinearMap_convMul (f g : C →ₐ[R] A) :
-    toConv (f * g).toLinearMap = toConv f.toLinearMap * toConv g.toLinearMap := rfl
+lemma toLinearMap_convOne : toConv (1 : WithConv <| C →ₐ[R] A).ofConv.toLinearMap = 1 := rfl
+lemma toLinearMap_convMul (f g : WithConv <| C →ₐ[R] A) :
+    toConv (f * g).ofConv.toLinearMap = toConv f.ofConv.toLinearMap * toConv g.ofConv.toLinearMap :=
+  rfl
 
-lemma toLinearMap_convPow (f : C →ₐ[R] A) :
-    ∀ n : ℕ, toConv (f ^ n).toLinearMap = toConv f.toLinearMap ^ n
+lemma toLinearMap_convPow (f : WithConv <| C →ₐ[R] A) :
+    ∀ n : ℕ, toConv (f ^ n).ofConv.toLinearMap = toConv f.ofConv.toLinearMap ^ n
   | 0 => rfl
   | n + 1 => by simp only [convPow_succ, toLinearMap_convMul, toLinearMap_convPow, pow_succ]
 
-lemma convMul_distrib_comp [Bialgebra R B] (f g : C →ₐ A) (h : B →ₐc[R] C) :
-    AlgHom.comp (f * g) (h : B →ₐ[R] C) = (.comp f h) * (.comp g h) := calc
-  _ = (.comp (lmul' R) <| .comp (Algebra.TensorProduct.map f g) <|
-      .comp (Algebra.TensorProduct.map (h : B →ₐ[R] C) (h : B →ₐ[R] C)) (comulAlgHom R B)) := by
+lemma convMul_distrib_comp [Bialgebra R B] (f g : WithConv <| C →ₐ A) (h : B →ₐc[R] C) :
+    AlgHom.comp (f * g).ofConv (h : B →ₐ[R] C) =
+      ofConv (toConv (f.ofConv.comp h) * toConv (g.ofConv.comp h)) := calc
+  _ = (.comp (lmul' R) <| .comp (map f.ofConv g.ofConv) <|
+      .comp (map (h : B →ₐ[R] C) (h : B →ₐ[R] C)) (comulAlgHom R B)) := by
     simp [convMul_def, comp_assoc]
   _ = (.comp (lmul' R) <|
-      .comp (Algebra.TensorProduct.map (.comp f h) (.comp g h)) (comulAlgHom R B)) := by
+      .comp (map (f.ofConv.comp h) (g.ofConv.comp h)) (comulAlgHom R B)) := by
     rw [Algebra.TensorProduct.map_comp]
     simp [comp_assoc]
   _ = _ := by simp [convMul_def]
 
-lemma comp_convMul_distrib [Algebra R B] (f g : C →ₐ[R] A) (h : A →ₐ[R] B) :
-    h.comp (f * g) = h.comp f * h.comp g := by
+lemma comp_convMul_distrib [Algebra R B] (f g : WithConv <| C →ₐ[R] A) (h : A →ₐ[R] B) :
+    h.comp (f * g).ofConv = ofConv (toConv (h.comp f.ofConv) * toConv (h.comp g.ofConv)) := by
   apply toLinearMap_injective
   apply WithConv.toConv_injective
   simp only [AlgHom.comp_toLinearMap]
-  rw [show (f * g).toLinearMap = (toConv f.toLinearMap * toConv g.toLinearMap).ofConv from
+  rw [show (f * g).ofConv.toLinearMap =
+    (toConv f.ofConv.toLinearMap * toConv g.ofConv.toLinearMap).ofConv from
         congr_arg WithConv.ofConv (toLinearMap_convMul f g),
       LinearMap.algHom_comp_convMul_distrib]
-  simp only [WithConv.toConv_ofConv]
-  rw [toLinearMap_convMul (h.comp f) (h.comp g)]
-  simp [AlgHom.comp_toLinearMap]
+  simp [toLinearMap_convMul]
 
-instance : Monoid (C →ₐ[R] A) :=
-  (toConv_injective.comp toLinearMap_injective).monoid _
+instance : Monoid (WithConv <| C →ₐ[R] A) :=
+  (toConv_injective.comp <| toLinearMap_injective.comp ofConv_injective).monoid _
     toLinearMap_convOne toLinearMap_convMul toLinearMap_convPow
 
 variable [IsCocomm R C]
 
-instance : CommMonoid (C →ₐ[R] A) :=
-  (toConv_injective.comp toLinearMap_injective).commMonoid _
+instance : CommMonoid (WithConv <| C →ₐ[R] A) :=
+  (toConv_injective.comp <| toLinearMap_injective.comp ofConv_injective).commMonoid _
     toLinearMap_convOne toLinearMap_convMul toLinearMap_convPow
 
 end AlgHom
@@ -102,38 +105,41 @@ end AlgHom
 namespace BialgHom
 variable [CommSemiring A] [CommSemiring C] [Bialgebra R A] [Bialgebra R C]
 
-instance : One (C →ₐc[R] A) where one := (unitBialgHom R A).comp <| counitBialgHom R C
+instance : One (WithConv <| C →ₐc[R] A) where
+  one := toConv <| (unitBialgHom R A).comp <| counitBialgHom R C
 
-lemma convOne_def : (1 : C →ₐc[R] A) = (unitBialgHom R A).comp (counitBialgHom ..) := rfl
+lemma convOne_def : 1 = toConv ((unitBialgHom R A).comp (counitBialgHom R C)) := rfl
 
-@[simp] lemma convOne_apply (c : C) : (1 : C →ₐc[R] A) c = algebraMap R A (counit c) := rfl
+@[simp]
+lemma convOne_apply (c : C) : (1 : WithConv <| C →ₐc[R] A) c = algebraMap R A (counit c) := rfl
 
-lemma toLinearMap_convOne : toConv (1 : C →ₐc[R] A).toLinearMap = 1 := rfl
+lemma toLinearMap_convOne : toConv (1 : WithConv <| C →ₐc[R] A).ofConv.toLinearMap = 1 := rfl
 
 variable [IsCocomm R C]
 
-instance : Mul (C →ₐc[R] A) where
-  mul f g := .comp (mulBialgHom R A) <| .comp (Bialgebra.TensorProduct.map f g) <| comulBialgHom R C
+instance : Mul (WithConv <| C →ₐc[R] A) where
+  mul f g := toConv <| .comp (mulBialgHom R A) <| .comp (map f.ofConv g.ofConv) <| comulBialgHom R C
 
-instance : Pow (C →ₐc[R] A) ℕ := ⟨fun f n ↦ npowRec n f⟩
+instance : Pow (WithConv <| C →ₐc[R] A) ℕ := ⟨fun f n ↦ npowRec n f⟩
 
-lemma convMul_def (f g : C →ₐc[R] A) :
+lemma convMul_def (f g : WithConv <| C →ₐc[R] A) :
     f * g =
-      (.comp (mulBialgHom R A) <| .comp (Bialgebra.TensorProduct.map f g) <| comulBialgHom R C) :=
+      toConv (.comp (mulBialgHom R A) <| .comp (map f.ofConv g.ofConv) <| comulBialgHom R C) :=
   rfl
 
-private lemma convPow_succ (f : C →ₐc[R] A) (n : ℕ) : f ^ (n + 1) = (f ^ n) * f := rfl
+private lemma convPow_succ (f : WithConv <| C →ₐc[R] A) (n : ℕ) : f ^ (n + 1) = (f ^ n) * f := rfl
 
-lemma toLinearMap_convMul (f g : C →ₐc[R] A) :
-    toConv (f * g).toLinearMap = toConv f.toLinearMap * toConv g.toLinearMap := rfl
+lemma toLinearMap_convMul (f g : WithConv <| C →ₐc[R] A) :
+    toConv (f * g).ofConv.toLinearMap = toConv f.ofConv.toLinearMap * toConv g.ofConv.toLinearMap :=
+  rfl
 
-lemma toLinearMap_convPow (f : C →ₐc[R] A) :
-    ∀ n, toConv (f ^ n).toLinearMap = toConv f.toLinearMap ^ n
+lemma toLinearMap_convPow (f : WithConv <| C →ₐc[R] A) :
+    ∀ n, toConv (f ^ n).ofConv.toLinearMap = toConv f.ofConv.toLinearMap ^ n
   | 0 => rfl
   | n + 1 => by simp only [convPow_succ, pow_succ, toLinearMap_convMul, toLinearMap_convPow]
 
-instance : CommMonoid (C →ₐc[R] A) :=
-  (toConv_injective.comp coe_linearMap_injective).commMonoid _
+instance : CommMonoid (WithConv <| C →ₐc[R] A) :=
+  (toConv_injective.comp <| coe_linearMap_injective.comp ofConv_injective).commMonoid _
     toLinearMap_convOne toLinearMap_convMul toLinearMap_convPow
 
 end BialgHom
