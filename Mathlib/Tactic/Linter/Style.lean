5594aaf857977e203a73b56f4fb8cfa1d172a0d9
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2026-05-16 21:06:25 +0200

    after shake Mathlib.Init

--- Mathlib/Tactic/Linter/Style.lean
+++ Mathlib/Tactic/Linter/Style.lean
@@ -12,6 +12,7 @@ public meta import Lean.Server.InfoUtils
 public meta import Mathlib.Tactic.Linter.Header  -- shake: keep
 public import Lean.Parser.Command
 public import Mathlib.Tactic.DeclarationNames
+import Lean.Parser.Module.Syntax
 
 /-!
 ## Style linters
