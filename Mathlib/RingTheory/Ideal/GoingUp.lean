1a37d807e5263837812243956f968693fe3b9566
Xavier Roblot <46200072+xroblot@users.noreply.github.com>
2026-05-16 12:30:15 +0200

    find home

--- Mathlib/RingTheory/Ideal/GoingUp.lean
+++ Mathlib/RingTheory/Ideal/GoingUp.lean
@@ -5,7 +5,8 @@ Authors: Anne Baanen, Yongle Hu
 -/
 module
 
-public import Mathlib.RingTheory.Flat.FaithfullyFlat.Algebra
+public import Mathlib.RingTheory.Ideal.Over
+public import Mathlib.RingTheory.Localization.AtPrime.Basic
 public import Mathlib.RingTheory.Localization.Integral
 
 /-!
@@ -446,11 +447,3 @@ lemma primesOver_bot [Module.IsTorsionFree A B] [IsDomain A] [IsDomain B] :
 end IsIntegral
 
 end Ideal
-
-instance (A B : Type*) [CommRing A] [CommRing B] [IsDomain B] [Algebra A B]
-    [Algebra.IsIntegral A B] [FaithfulSMul A B] [Module.Flat A B] :
-    Module.FaithfullyFlat A B := by
-  refine Module.FaithfullyFlat.of_comap_surjective fun P ↦ ?_
-  obtain ⟨P, hP₁, hP₂⟩ := Ideal.exists_ideal_over_prime_of_isIntegral_of_isDomain P.1 (S := B)
-    (by simp [(RingHom.injective_iff_ker_eq_bot _).mp (FaithfulSMul.algebraMap_injective A B)])
-  exact ⟨⟨P, hP₁⟩, PrimeSpectrum.ext_iff.mpr hP₂⟩
