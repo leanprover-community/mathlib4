a6e3e66ef74e53d7f76a9feee75263143f22c55d
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2026-05-16 21:51:26 +0200

    after shaking&fixing Mathlib.Data.FunLike.Basic

--- Mathlib/Data/FunLike/Basic.lean
+++ Mathlib/Data/FunLike/Basic.lean
@@ -5,12 +5,12 @@ Authors: Anne Baanen
 -/
 module
 
-public meta import Lean.Meta.CoeAttr
-public import Mathlib.Logic.Function.Basic
-public import Mathlib.Logic.Unique
-public import Mathlib.Util.CompileInductive
+public import Mathlib.Logic.IsEmpty.Defs
+import Mathlib.Logic.Basic
+import Mathlib.Logic.Unique
 public import Mathlib.Tactic.Simps.NotationClass
-public import Mathlib.Tactic.SplitIfs
+import Mathlib.Tactic.SplitIfs
+import Mathlib.Util.CompileInductive
 
 /-!
 # Typeclass for a type `F` with an injective map to `A → B`
