49b839921586eff1600eb90d3b225dddec598701
Tadd Yuchi Mao <44916381+tdM05@users.noreply.github.com>
2026-05-16 18:12:09 -0400

    Apply suggestion from @YanYablonovskiy
    Co-authored-by: Yan Yablonovskiy <yablonovskiy.yan@gmail.com>

--- Mathlib/Algebra/Group/Subgroup/ZPowers/Lemmas.lean
+++ Mathlib/Algebra/Group/Subgroup/ZPowers/Lemmas.lean
@@ -70,12 +70,8 @@ lemma range_nsmulAddMonoidHom (n : ℕ) : (nsmulAddMonoidHom n).range = zmultipl
   simp [mem_zmultiples_iff, dvd_def]
   grind
 
-theorem closure_eq_zmultiples (a b : ℤ) : closure {a, b} = zmultiples (a.gcd b : ℤ) := by
   apply le_antisymm
-  · rw [closure_le]
-    rintro x (rfl | rfl)
-    · exact mem_zmultiples_iff.mpr <| gcd_dvd_left x b
-    · exact mem_zmultiples_iff.mpr <| gcd_dvd_right a x
+  · grind [mem_zmultiples_iff, SetLike.mem_coe, gcd_dvd_left, gcd_dvd_right, closure_le]
   · simp [zmultiples_le, mem_closure_pair, gcd_eq_gcd_ab, mul_comm]
 
 end Int
