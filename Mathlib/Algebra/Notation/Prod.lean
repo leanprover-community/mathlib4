a0d090135f1f7205e8e8166b947f1961b370c4d8
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2026-05-16 21:59:50 +0200

    after shake Mathlib.Algebra.Notation.Prod

--- Mathlib/Algebra/Notation/Prod.lean
+++ Mathlib/Algebra/Notation/Prod.lean
@@ -6,7 +6,7 @@ Authors: Simon Hudon, Patrick Massot, Yury Kudryashov
 module
 
 public import Mathlib.Algebra.Notation.Defs
-public import Mathlib.Data.Prod.Basic
+import Mathlib.Data.Prod.Basic
 
 /-!
 # Arithmetic operators on (pairwise) product types
