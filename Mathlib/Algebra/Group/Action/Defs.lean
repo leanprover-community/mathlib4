de82fa9c94ccb98e95e036c806350fe85f43ce0a
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2026-05-16 22:40:45 +0200

    after shake Mathlib.Algebra.Group.Action.Defs

--- Mathlib/Algebra/Group/Action/Defs.lean
+++ Mathlib/Algebra/Group/Action/Defs.lean
@@ -7,8 +7,9 @@ module
 
 public import Mathlib.Algebra.Group.Commute.Defs
 public import Mathlib.Algebra.Opposites
-public import Mathlib.Tactic.Spread
 public import Mathlib.Logic.Function.Iterate
+import Mathlib.Tactic.SimpRw
+import Mathlib.Tactic.Spread
 
 /-!
 # Definitions of group actions
