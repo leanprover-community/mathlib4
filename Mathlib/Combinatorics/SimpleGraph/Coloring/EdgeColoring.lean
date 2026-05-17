247144e807ab8f62084c16348978720a6f41fe21
yiyang he <dannyhe652@proton.me>
2026-05-17 20:54:49 +0800

    fix: use Fintype G.edgeSet instead of Fintype V for chromaticIndex

--- Mathlib/Combinatorics/SimpleGraph/Coloring/EdgeColoring.lean
+++ Mathlib/Combinatorics/SimpleGraph/Coloring/EdgeColoring.lean
@@ -36,8 +36,9 @@ def edgeColoring := G.lineGraph.Coloring α
 /-- A graph is edge colorable with `n` colors if its line graph is `n`-colorable. -/
 def edgeColorable (n : ℕ) : Prop := G.lineGraph.Colorable n
 
-/-- The chromatic index of `G` is the chromatic number of its line graph. -/
-noncomputable def chromaticIndex [Fintype V] : ℕ :=
+/-- The chromatic index of `G` is the chromatic number of its line graph,
+converted to a natural number (or 0 if infinite). -/
+noncomputable def chromaticIndex [Fintype G.edgeSet] : ℕ :=
   G.lineGraph.chromaticNumber.toNat
 
 end vizing
