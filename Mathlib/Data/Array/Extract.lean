c477b571584ae069a007354dd3c5a552edcc1d23
Bryan Gin-ge Chen <bryangingechen@gmail.com>
2026-05-17 10:43:43 -0400

    deprecate empty file (using date of previous deprecations)

--- Mathlib/Data/Array/Extract.lean
+++ Mathlib/Data/Array/Extract.lean
@@ -3,21 +3,9 @@ Copyright (c) 2024 Jiecheng Zhao. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jiecheng Zhao
 -/
-module
+module -- shake: keep-all
 
 public import Mathlib.Init
 public import Batteries.Data.Array.Lemmas
-/-!
-# Lemmas about `Array.extract`
 
-Some useful lemmas about Array.extract
--/
-
-public section
-
-universe u
-variable {α : Type u} {i : Nat}
-
-namespace Array
-
-end Array
+deprecated_module (since := "2025-11-03")
