8ffae4e96a9f66296a10536dffd2e32194ecc8ba
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2026-05-17 20:44:57 +0800

    fix

--- Mathlib/RingTheory/RegularLocalRing/Polynomial.lean
+++ Mathlib/RingTheory/RegularLocalRing/Polynomial.lean
@@ -45,8 +45,8 @@ lemma Polynomial.localization_at_comap_maximal_isRegularRing_isRegularRing
         Set.image_comp, ← Ideal.map_span, ← Ideal.map_span]
       simp only [Ideal.span, (maximalIdeal R).span_generators, algebraMap_eq, ← eq,
         Localization.AtPrime.map_eq_maximalIdeal]
-    simp only [← maximalIdeal_height_eq_ringKrullDim, ← IsLocalization.height_comap p.primeCompl,
-      IsLocalization.AtPrime.comap_maximalIdeal _ p, ge_iff_le]
+    simp only [← maximalIdeal_height_eq_ringKrullDim, ← IsLocalization.height_under p.primeCompl,
+      IsLocalization.AtPrime.under_maximalIdeal _ p, ge_iff_le]
     apply le_trans _ (WithBot.coe_le_coe.mpr ht1)
     simp only [maximalIdeal_height_eq_ringKrullDim, ← reg, Nat.cast_le, ← this,
       ← Submodule.FG.generators_ncard fg]
@@ -72,10 +72,9 @@ lemma Polynomial.localization_at_comap_maximal_isRegularRing_isRegularRing
         congr
         exact (maximalIdeal R).span_generators.symm
     simp only [← Localization.AtPrime.map_eq_maximalIdeal, peq, Ideal.map_span]
-    rw [← maximalIdeal_height_eq_ringKrullDim, ← IsLocalization.height_comap p.primeCompl,
-      IsLocalization.AtPrime.comap_maximalIdeal _ p, Ideal.height_eq_primeHeight]
-    apply le_trans _ (WithBot.coe_le_coe.mpr (Ideal.primeHeight_add_one_le_of_lt lt))
-    rw [← Ideal.height_eq_primeHeight]
+    rw [← maximalIdeal_height_eq_ringKrullDim, ← IsLocalization.height_under p.primeCompl,
+      ← Ideal.under_def, IsLocalization.AtPrime.under_maximalIdeal _ p]
+    apply le_trans _ (WithBot.coe_le_coe.mpr (Ideal.height_add_one_le_of_lt_of_isPrime lt))
     apply le_trans _ (WithBot.coe_le_coe.mpr (add_le_add_left ht 1))
     rw [WithBot.coe_add, maximalIdeal_height_eq_ringKrullDim, WithBot.coe_one, ← reg,
       ← Nat.cast_one, ← Nat.cast_add, Nat.cast_le]
@@ -101,18 +100,17 @@ theorem Polynomial.isRegularRing_of_isRegularRing [IsRegularRing R] : IsRegularR
   have : IsLocalization.AtPrime (Localization.AtPrime pS) p := by
     convert IsLocalization.isLocalization_isLocalization_atPrime_isLocalization pc
       (Localization.AtPrime pS) pS
-    exact (IsLocalization.comap_map_of_isPrime_disjoint pc _ ‹_› disj).symm
+    exact (IsLocalization.under_map_of_isPrime_disjoint pc _ ‹_› disj).symm
   have := isRegularRing_iff.mp ‹_› q
   have eq : comap C pS = maximalIdeal (Localization.AtPrime q) := by
-    rw [← IsLocalization.map_comap q.primeCompl _ (comap C pS),
-      ← IsLocalization.map_comap q.primeCompl _ (maximalIdeal (Localization.AtPrime q))]
+    rw [← IsLocalization.map_under q.primeCompl _ (comap C pS),
+      ← IsLocalization.map_under q.primeCompl _ (maximalIdeal (Localization.AtPrime q))]
     simp only [comap_comap, S, pS]
     rw [← Polynomial.algebraMap_eq (R := Localization.AtPrime q),
       ← IsScalarTower.algebraMap_eq R (Localization.AtPrime q) (Localization.AtPrime q)[X],
       IsScalarTower.algebraMap_eq R R[X] (Localization.AtPrime q)[X], ← comap_comap,
-      IsLocalization.comap_map_of_isPrime_disjoint pc _ ‹_› disj,
-      IsLocalization.AtPrime.comap_maximalIdeal (Localization.AtPrime q) q]
-    rfl
+      ← Ideal.under_def R[X], IsLocalization.under_map_of_isPrime_disjoint pc _ ‹_› disj]
+    simp [q, IsLocalization.AtPrime.under_maximalIdeal (Localization.AtPrime q) q]
   have := localization_at_comap_maximal_isRegularRing_isRegularRing (Localization.AtPrime q) pS eq
   exact IsRegularLocalRing.of_ringEquiv (IsLocalization.algEquiv p.primeCompl
     (Localization.AtPrime pS) (Localization.AtPrime p)).toRingEquiv
