f5bbf82bed9df263608ecaec00170b22f36a244c
Michael Rothgang <rothgang@math.uni-bonn.de>
2026-05-18 10:09:55 +0200

    Remove more to_fun workaround

--- Mathlib/Geometry/Manifold/MFDeriv/NormedSpace.lean
+++ Mathlib/Geometry/Manifold/MFDeriv/NormedSpace.lean
@@ -591,31 +591,27 @@ end Manifold
 lemma mvfderivWithin_const (c : F) {x : M} : mvfderiv[s] (fun _ : M ↦ c) x = 0 := by
   simp [mvfderivWithin, mfderivWithin_const]
 
-@[simp, to_fun]
+@[simp, to_fun mvfderivWithin_fun_add]
 lemma mvfderivWithin_add {g g' : M → F} {x : M}
     (hg : MDiffAt[s] g x) (hg' : MDiffAt[s] g' x) (hs : UniqueMDiffWithinAt I s x) :
     mvfderiv[s] (g + g') x = mvfderiv[s] g x + mvfderiv[s] g' x := by
   simp [mvfderivWithin, mfderivWithin_add hg hg' hs]
   rfl
--- TODO: specify as name for to_fun instead, after #34279
-alias mvfderivWithin_fun_add := fun_mvfderivWithin_add
 
-@[simp, to_fun]
+@[simp, to_fun mvfderivWithin_fun_sub]
 lemma mvfderivWithin_sub {g g' : M → F} {x : M}
     (hg : MDiffAt[s] g x) (hg' : MDiffAt[s] g' x) (hs : UniqueMDiffWithinAt I s x) :
     mvfderiv[s] (g - g') x = mvfderiv[s] g x - mvfderiv[s] g' x := by
   simp [mvfderivWithin, mfderivWithin_sub hg hg' hs]
   rfl
--- TODO: specify as name for to_fun instead, after #34279
-alias mvfderivWithin_fun_sub := fun_mvfderivWithin_sub
 
-@[simp, to_fun]
+@[simp, to_fun mvfderivWithin_fun_neg]
 lemma mvfderivWithin_neg {g : M → F} {x : M} (hs : UniqueMDiffWithinAt I s x) :
     mvfderiv[s] (-g) x = -mvfderiv[s] g x := by
   simp [mvfderivWithin, mfderivWithin_neg hs]
   rfl
 
-@[simp, to_fun]
+@[simp, to_fun mvfderivWithin_fun_smul]
 lemma mvfderivWithin_smul {a : M → 𝕜} (ha : MDiffAt[s] a x) {g : M → F} (hg : MDiffAt[s] g x)
     (hs : UniqueMDiffWithinAt I s x) :
     mvfderiv[s] (a • g) x =
@@ -625,17 +621,12 @@ lemma mvfderivWithin_smul {a : M → 𝕜} (ha : MDiffAt[s] a x) {g : M → F} (
     fromTangentSpace_mfderivWithin_smul_apply ha hg hs]
   rfl
 
--- TODO: specify as name for to_fun instead, after #34279
-alias mvfderivWithin_fun_smul := fun_mvfderivWithin_smul
-
-@[simp, to_fun]
+@[simp, to_fun mvfderivWithin_fun_mul]
 lemma mvfderivWithin_mul {f g : M → 𝕜} {x : M} (hf : MDiffAt f x) (hg : MDiffAt g x) :
     mvfderiv% (f * g) x = f x • mvfderiv% g x + (g x) • (mvfderiv% f x) := by
   ext v
   simp [mvfderiv, -Pi.smul_apply', -Pi.smul_apply, ← smul_eq_mul, mfderiv_smul hf hg]
   simp [mul_comm _ (g x)]
-
-alias mvfderivWithin_fun_mul := fun_mvfderivWithin_mul
 
 @[simp]
 lemma mvfderivWithin_zero {s : Set M} (hs : UniqueMDiffWithinAt I s x) :
