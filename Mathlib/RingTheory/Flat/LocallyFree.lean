3498c5b19ca543c4190fd93c16bcd86c7c1624f0
Yongle Hu <140475041+mbkybky@users.noreply.github.com>
2026-05-18 23:57:29 +0800

    Update LocallyFree.lean

--- Mathlib/RingTheory/Flat/LocallyFree.lean
+++ Mathlib/RingTheory/Flat/LocallyFree.lean
@@ -9,7 +9,7 @@ public import Mathlib.RingTheory.PicardGroup
 public import Mathlib.RingTheory.Spectrum.Prime.FreeLocus
 
 /-!
-This file proves that a finite flat `R`-module `M` is locally free if `rankAtStalk M` is constant.
+# A finite flat module `M` is locally free if `rankAtStalk M` is constant
 -/
 
 public section
@@ -23,23 +23,12 @@ open LocalizedModule
 
 attribute [local instance] Module.free_of_flat_of_isLocalRing
 
-lemma localizedMap_bijective_of_surjective_of_rankAtStalk_eq (a : R) {φ : M →ₗ[R] N}
-    (hφs : Function.Surjective (LocalizedModule.map (Submonoid.powers a) φ))
+lemma bijective_of_surjective_of_rankAtStalk_eq {φ : M →ₗ[R] N} (hs : Function.Surjective φ)
     (h : ∀ (m : Ideal R) [m.IsMaximal],
       rankAtStalk M ⟨m, inferInstance⟩ = rankAtStalk N ⟨m, inferInstance⟩) :
-    Function.Bijective (LocalizedModule.map (Submonoid.powers a) φ) := by
-  let Mₐ := LocalizedModule.Away a M
-  let Nₐ := LocalizedModule.Away a N
-  refine bijective_of_localized_maximal (map (Submonoid.powers a) φ) <| fun m _ ↦
-    bijective_of_surjective_of_finite_of_free_of_finrank_eq ?_
-      (LocalizedModule.map_surjective m.primeCompl (map (Submonoid.powers a) φ) hφs)
-  change rankAtStalk Mₐ ⟨m, inferInstance⟩ = rankAtStalk Nₐ ⟨m, inferInstance⟩
-  rw [rankAtStalk_isBaseChange (LocalizedModule.isBaseChange (Submonoid.powers a) M)]
-  rw [rankAtStalk_isBaseChange (LocalizedModule.isBaseChange (Submonoid.powers a) N)]
-  obtain ⟨𝔪, _, hm𝔪⟩ : ∃ 𝔪 : Ideal R, 𝔪.IsMaximal ∧ PrimeSpectrum.comap
-      (algebraMap R (Localization (Submonoid.powers a))) ⟨m, inferInstance⟩ ≤ 𝔪 :=
-    (m.comap (algebraMap R (Localization.Away a))).exists_le_maximal Ideal.IsPrime.ne_top'
-  simp [rankAtStalk_eq_of_le_of_finite_of_flat' _ hm𝔪, h 𝔪]
+    Function.Bijective φ :=
+  bijective_of_localized_maximal φ <| fun m _ ↦
+    bijective_of_surjective_of_finite_of_free_of_finrank_eq (h m) (map_surjective m.primeCompl φ hs)
 
 variable (M) in
 /-- Let `M` be a finite flat `R`-module, `p` be a prime ideal of `R`. If `rankAtStalk M` is
@@ -64,7 +53,12 @@ theorem Free.away_of_finite_of_flat_of_rankAtStalk_constant (p : Ideal R) [p.IsP
       free_of_isLocalizedModule (Submonoid.powers a) (mkLinearMap (Submonoid.powers a) (Fin n →₀ R))
     let φₐ : LocalizedModule.Away a (Fin n →₀ R) →ₗ[Localization.Away a] LocalizedModule.Away a M :=
       LocalizedModule.map (Submonoid.powers a) φ
-    exact ⟨a, hap, Module.Free.of_equiv <| LinearEquiv.ofBijective φₐ <|
-      localizedMap_bijective_of_surjective_of_rankAtStalk_eq a hφas <| fun m _ ↦ by simp [n, h m]⟩
+    refine ⟨a, hap, Module.Free.of_equiv <| LinearEquiv.ofBijective φₐ <|
+      bijective_of_surjective_of_rankAtStalk_eq hφas <| fun m _ ↦ ?_⟩
+    obtain ⟨𝔪, _, hm𝔪⟩ : ∃ 𝔪 : Ideal R, 𝔪.IsMaximal ∧ PrimeSpectrum.comap
+        (algebraMap R (Localization (Submonoid.powers a))) ⟨m, inferInstance⟩ ≤ 𝔪 :=
+      (m.comap (algebraMap R (Localization.Away a))).exists_le_maximal Ideal.IsPrime.ne_top'
+    simp [rankAtStalk_isBaseChange (LocalizedModule.isBaseChange (Submonoid.powers a) _),
+      rankAtStalk_eq_of_le_of_finite_of_flat' M hm𝔪, h 𝔪, n]
 
 end Module
