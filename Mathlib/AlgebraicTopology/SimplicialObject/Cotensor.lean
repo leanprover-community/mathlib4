83382ebf4c4d9f999972c629f8ad95f5d691ef89
Christian Merten <christian@merten.dev>
2026-05-18 12:59:11 +0200

    add documentation

--- Mathlib/AlgebraicTopology/SimplicialObject/Cotensor.lean
+++ Mathlib/AlgebraicTopology/SimplicialObject/Cotensor.lean
@@ -1,9 +1,24 @@
+/-
+Copyright (c) 2026 Robin Carlier, Christian Merten. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Robin Carlier, Christian Merten
+-/
 import Mathlib
+
+/-!
+# Bracket and hypercovers
+
+In this file we define a bracket operation `X[K]` for a semi-simplicial object `X`
+and a nonsingular simplicial set `K`. Using this we define the notion of
+a hypercover.
+
+This is a rough draft
+-/
 
 open CategoryTheory Limits AlgebraicTopology SimplicialObject
   Simplicial
 
-universe u w
+universe w v u
 
 variable {C : Type*} [Category* C] (X : SimplicialObject C) (K : SSet.{w})
 
@@ -66,6 +81,20 @@ def liftComp {C : Type*} [Category* C] (F : C ⥤ SimplexCategory)
     lift F h ⋙ toSimplexCategory ≅ F :=
   NatIso.ofComponents (fun X ↦ Iso.refl _)
 
+set_option backward.isDefEq.respectTransparency false in
+lemma homOfMono_id (n : ℕ) :
+    homOfMono (𝟙 <| toSimplexCategory.obj ⦋n⦌ₛ) = 𝟙 (⦋n⦌ₛ) :=
+  rfl
+
+set_option backward.isDefEq.respectTransparency false in
+def δ {n} (i : Fin (n + 2)) : ⦋n⦌ₛ ⟶ ⦋n + 1⦌ₛ :=
+  homOfMono (SimplexCategory.δ i)
+
+@[simp]
+lemma toSimplexCategory_map_δ {n} (i : Fin (n + 2)) :
+    toSimplexCategory.map (δ i) = SimplexCategory.δ i :=
+  rfl
+
 end SemiSimplexCategory
 
 namespace CategoryTheory
@@ -78,6 +107,10 @@ set_option quotPrecheck false in
 scoped[Simplicial]
   notation3:1000 X " _⦋" n "⦌ₛ" =>
       (X : CategoryTheory.SemisimplicialObject _).obj (Opposite.op (SemiSimplexCategory.mk n))
+
+def SemisimplicialObject.δ (X : SemisimplicialObject C) {n : ℕ} (i : Fin (n + 2)) :
+    X _⦋n + 1⦌ₛ ⟶ X _⦋n⦌ₛ :=
+  X.map (SemiSimplexCategory.δ i).op
 
 end CategoryTheory
 
@@ -434,32 +467,13 @@ def functorLift (E : Type*) [Category* E] :
   lift G h := lift G h
   liftIso G h := liftComp _
 
---set_option backward.isDefEq.respectTransparency false in
---@[simps]
---def functorLift' (E : Type*) [Category* E] :
---    FunctorLift' (toSimplexCategory) (fun G ↦ ∀ ⦃X Y : E⦄ (f : X ⟶ Y), Mono (G.map f)) where
---  lift.obj G := lift G.obj G.property
---  lift.map {G G'} f :=
---    { app X := by
---        dsimp [lift]
---        let f : toSimplexCategory.obj ⦋(G.obj.obj X).len⦌ₛ ⟶
---            toSimplexCategory.obj ⦋(G'.obj.obj X).len⦌ₛ :=
---          f.hom.app _
---        have : Mono f := by
---          sorry
---        apply homOfMono f
---      , naturality := sorry }
---  liftIso := sorry
---  --liftIso G h := liftComp _
-
-set_option backward.isDefEq.respectTransparency false in
-lemma homOfMono_id (n : ℕ) :
-    homOfMono (𝟙 <| toSimplexCategory.obj ⦋n⦌ₛ) = 𝟙 (⦋n⦌ₛ) :=
-  rfl
-
 end SemiSimplexCategory
 
 namespace CategoryTheory.SemisimplicialObject
+
+/-!
+## Bracket for semi-simplicial objects
+-/
 
 variable (X : SemisimplicialObject C) (K : SSet.{u})
 
@@ -478,12 +492,16 @@ noncomputable abbrev bracketDiag' (X : SemisimplicialObject C) (K : SSet.{u}) [K
     rw [Quiver.Hom.unop_op, ← unop_mono_iff]
     infer_instance
 
--- example [K.Nonsingular] : X.bracketDiag K = X.bracketDiag' K := rfl
-
 abbrev HasBracket [K.Nonsingular] : Prop := HasLimit (X.bracketDiag K)
 
 variable [K.Nonsingular]
 
+/--
+The bracket `X[K]` of a semi-simplicial object `X` with a non-singular
+simplicial set `K`.
+Note: In the case where `X` is simplicial, we have a more natural construction
+`SimplicialObject.bracket` below.
+-/
 noncomputable abbrev bracket [X.HasBracket K] : C :=
   limit (X.bracketDiag K)
 
@@ -574,18 +592,217 @@ lemma bracketMap_id [X.HasBracket K] : X.bracketMap (𝟙 K) = 𝟙 _ := by
     Category.id_comp]
   rfl
 
+lemma _root_.CategoryTheory.Functor.isEmpty_elements {C : Type*} [Category* C] {F : C ⥤ Type*}
+    (h : IsInitial F) :
+    IsEmpty F.Elements := by
+  constructor
+  intro x
+  have : IsEmpty (F.obj x.1) := by
+    rw [← Types.initial_iff_empty]
+    constructor
+    exact .isInitialObj ((evaluation _ _).obj x.1) _ h
+  apply this.elim' x.2
+
+noncomputable def isTerminalBracketOfIsInitial [X.HasBracket K] (h : IsInitial K) :
+    IsTerminal (X.bracket K) :=
+  haveI : IsEmpty K.Elements := K.isEmpty_elements h
+  haveI : IsEmpty K.nonDegenerateElements.FullSubcategory := K.nonDegenerateElements.ι.obj.isEmpty
+  (Limits.isLimitEquivIsTerminalOfIsEmpty _ _) (limit.isLimit _)
+
+noncomputable def isInitialBoundaryZero : IsInitial (∂Δ[0] : SSet.{u}) := by
+  apply Nonempty.some
+  rw [boundary_zero]
+  exact ⟨SSet.Subcomplex.isInitialBot⟩
+
+lemma _root_.SSet.yonedaEquiv_mem_nonDegenerate_of_mono (n : ℕ) {K : SSet.{u}} (f : Δ[n] ⟶ K)
+    [Mono f] :
+    SSet.yonedaEquiv f ∈ nonDegenerate _ n :=
+  (nonDegenerate_iff_of_mono f (stdSimplex.objEquiv.symm (𝟙 _))).mpr
+    (stdSimplex.objEquiv_symm_id_mem_nonDegenerate n)
+
+@[simps]
+noncomputable
+def bracketFunctor {J : Type*} [Category* J] (F : J ⥤ SSet.{u}) [∀ j, Nonsingular (F.obj j)]
+    [∀ j, X.HasBracket (F.obj j)] [∀ ⦃i j : J⦄ (f : i ⟶ j), Mono (F.map f)] :
+    Jᵒᵖ ⥤ C where
+  obj j := X.bracket (F.obj j.unop)
+  map f := X.bracketMap (F.map f.unop)
+
+set_option backward.isDefEq.respectTransparency false in
+@[simps]
+noncomputable
+def bracketCone {J : Type*} [Category* J] (F : J ⥤ SSet.{u}) [∀ j, Nonsingular (F.obj j)]
+    [∀ j, X.HasBracket (F.obj j)] [∀ ⦃i j : J⦄ (f : i ⟶ j), Mono (F.map f)]
+    (c : Cocone F) [c.pt.Nonsingular] [X.HasBracket c.pt]
+    [∀ j, Mono (c.ι.app (Opposite.unop j))] :
+    Cone (X.bracketFunctor F) where
+  pt := X.bracket c.pt
+  π.app j := X.bracketMap (c.ι.app j.unop)
+  π.naturality j k f := by simp [← bracketMap_comp]
+
+set_option backward.isDefEq.respectTransparency false in
+-- Note: The folĺowing proof is painful, because `X[-]` is not fully functorial, when
+-- `X` is only semi-simplicial.
+/-- `X[-]` sends colimits to limits. -/
+noncomputable
+def isLimitBracketCone {J : Type*} [Category* J] (F : J ⥤ SSet.{u}) [∀ j, Nonsingular (F.obj j)]
+    [∀ j, X.HasBracket (F.obj j)] [∀ ⦃i j : J⦄ (f : i ⟶ j), Mono (F.map f)]
+    (c : Cocone F) [c.pt.Nonsingular] [X.HasBracket c.pt]
+    [∀ j, Mono (c.ι.app (Opposite.unop j))] (hc : IsColimit c) :
+    IsLimit (X.bracketCone F c) where
+  lift s := by
+    dsimp [bracket]
+    let t : Cone (X.bracketDiag c.pt) :=
+      { pt := s.pt
+        π.app := fun ⟨⟨⟨⟨n⟩⟩, xp⟩, hx⟩ ↦ by
+          dsimp
+          let cn := Functor.mapCocone ((evaluation (SimplexCategoryᵒᵖ) (Type u)).obj (.op ⦋n⦌)) c
+          let x := (Functor.coconeTypesEquiv _).symm cn
+          have : x.IsColimit := sorry
+          let aa : (F ⋙ evaluation SimplexCategoryᵒᵖ (Type u) _⦋n⦌).CoconeTypes :=
+            {
+              pt := s.pt ⟶ X _⦋n⦌ₛ
+              ι j := by
+                dsimp
+                sorry
+                --dsimp at x
+                --exact s.π.app (.op j) ≫ limit.π _ ⟨⟨_, x⟩, by dsimp⟩ ≫ _
+              ι_naturality := sorry
+            }
+          let : c.pt _⦋n⦌ → (s.pt ⟶ X _⦋n⦌ₛ) :=
+            this.desc aa
+          -- let sapp (j) := s.π.app j ≫ limit.π _ ⟨⟨.op ⦋n⦌, _⟩, sorry⟩
+          -- dsimp at sapp
+          apply this
+          exact xp
+        π.naturality := sorry
+      }
+    exact limit.lift _ t
+  fac := sorry
+  uniq := sorry
+
+def inclNonDegenerateBoundaryOne :
+    Discrete WalkingPair ⥤ (∂Δ[1] : SSet.{u}).nonDegenerateElements.FullSubcategory :=
+  Discrete.functor fun x ↦ match x with
+  | .left =>
+    ⟨⟨_, uliftYonedaEquiv (boundary.ι (n := 0) 0)⟩,
+      SSet.yonedaEquiv_mem_nonDegenerate_of_mono _ _⟩
+  | .right =>
+    ⟨⟨_, uliftYonedaEquiv (boundary.ι (n := 0) 1)⟩,
+      SSet.yonedaEquiv_mem_nonDegenerate_of_mono _ _⟩
+
+def inclNonDegenerateBoundaryOne' :
+    Discrete WalkingPair ⥤ (∂Δ[1] : SSet.{u}).nonDegenerateElements.FullSubcategory :=
+  Discrete.functor fun x ↦ match x with
+  | .left =>
+    ⟨⟨_, uliftYonedaEquiv (boundary.ι (n := 0) 0)⟩,
+      SSet.yonedaEquiv_mem_nonDegenerate_of_mono _ _⟩
+  | .right =>
+    ⟨⟨_, uliftYonedaEquiv (boundary.ι (n := 0) 1)⟩,
+      SSet.yonedaEquiv_mem_nonDegenerate_of_mono _ _⟩
+
+instance : inclNonDegenerateBoundaryOne.Initial := by
+  sorry
+
 end CategoryTheory.SemisimplicialObject
 
 namespace CategoryTheory
 
 variable (X : SemisimplicialObject C)
 
+/--
+A semi-simplicial object in a category `C` with finite limits is a `P`-hypercover
+if the natural map `X[Δⁿ] ⟶ X[∂Δⁿ]` satisfies `P`.
+
+To obtain the usual notion of a hypercover on a site `C` with topology `J`, we
+take a semi-simplicial object `H` in `FormalCoproduct C` and ask
+for the induced semi-simplical object in `Cᵒᵖ ⥤ Type _` to be a `P`-hypercover, where
+`P` is the class of "covering morphisms" (which SGA calls "morphisme couvrant"):
+A morphism of presheafs `K ⟶ L` is covering if it becomes an epimorphism after
+sheafification.
+(see `GrothendieckTopology.couvrant` below)
+
+The `1`-truncation of a hypercover in the above sense induces a `1`-hypercover
+(see `CategoryTheory.GrothendieckTopology.OneHypercover.ofIsHypercover`).
+-/
 structure MorphismProperty.IsHypercover [HasFiniteLimits C] (P : MorphismProperty C) : Prop where
   prop_bracketMap (n : ℕ) : P (X.bracketMap <| (∂Δ[n] : (Δ[n] : SSet.{0}).Subcomplex).ι)
+
+variable (J : GrothendieckTopology C)
+
+variable {A : Type*} [Category* A]
+
+def ObjectProperty.isPrelocal (P : ObjectProperty C) : MorphismProperty C := fun _ _ f =>
+  ∀ Z, P Z → Function.Injective (fun (g : _ ⟶ Z) ↦ f ≫ g)
+
+/-- Covering morphisms in the sense of SGA. -/
+def GrothendieckTopology.couvrant : MorphismProperty (Cᵒᵖ ⥤ A) :=
+  ObjectProperty.isPrelocal (Presheaf.IsSheaf J)
+
+namespace Limits.FormalCoproduct
+
+def objIsoOfEq (X : FormalCoproduct.{w} C) {i j : X.I} (hij : i = j) :
+    X.obj i ≅ X.obj j :=
+  eqToIso (by rw [hij])
+
+@[simp]
+lemma objIsoOfEq_rfl (X : FormalCoproduct.{w} C) (i : X.I) :
+    X.objIsoOfEq (rfl : i = i) = Iso.refl _ :=
+  rfl
+
+@[simp]
+lemma objIsoOfEq_trans (X : FormalCoproduct.{w} C) {i j k : X.I}
+    (hij : i = j) (hjk : j = k) :
+    X.objIsoOfEq hij ≪≫ X.objIsoOfEq hjk = X.objIsoOfEq (hij.trans hjk) := by
+  subst hij hjk
+  simp
+
+@[simp]
+lemma objIsoOfEq_symm (X : FormalCoproduct.{w} C) {i j : X.I}
+    (hij : i = j) :
+    (X.objIsoOfEq hij).symm = X.objIsoOfEq hij.symm := by
+  subst hij
+  simp
+
+variable {C : Type*} [Category.{v} C]
+
+noncomputable def uliftYoneda : FormalCoproduct.{w} C ⥤ Cᵒᵖ ⥤ Type (max w v) :=
+  (eval _ _).obj CategoryTheory.uliftYoneda
+
+end Limits.FormalCoproduct
+
+/-- The `1`-truncation of a semi-simplical object in `FormalCoproduct.{w} (Over S)`
+induces a pre-`1`-hypercover of `S`. -/
+@[simps]
+def SemisimplicialObject.preOneHypercover {S : C}
+    (H : SemisimplicialObject (FormalCoproduct.{w} (Over S))) : PreOneHypercover.{w} S where
+  I₀ := (H _⦋0⦌ₛ).I
+  X i := ((H _⦋0⦌ₛ).obj i).left
+  f i := ((H _⦋0⦌ₛ).obj i).hom
+  I₁ i j := { x : (H _⦋1⦌ₛ).I // (H.δ 0).f x = i ∧ (H.δ 1).f x = j }
+  Y _ _ k := ((H _⦋1⦌ₛ).obj k).left
+  p₁ i j k := ((H.δ 0).φ k.1).left ≫ ((H _⦋0⦌ₛ).objIsoOfEq k.2.1).hom.left
+  p₂ i j k := ((H.δ 1).φ k.1).left ≫ ((H _⦋0⦌ₛ).objIsoOfEq k.2.2).hom.left
+  w := by simp
+
+/-- The `1`-truncation of a hypercover induces a `1`-hypercover. -/
+def GrothendieckTopology.OneHypercover.ofIsHypercover {S : C}
+    (H : SemisimplicialObject (FormalCoproduct.{w} (Over S)))
+    (h : (J.over S).couvrant.IsHypercover <| H ⋙ FormalCoproduct.uliftYoneda) :
+    J.OneHypercover S where
+  __ := H.preOneHypercover
+  mem₀ := by
+    have := h.prop_bracketMap 0
+    sorry
+  mem₁ := sorry
 
 end CategoryTheory
 
 namespace CategoryTheory.SimplicialObject
+
+/-!
+## Bracket for simplicial objects
+-/
 
 abbrev bracketDiag' : K.Elements ⥤ C :=
   FunctorLift.bracketDiag .id X K (𝟭 _) <| by tauto
@@ -597,6 +814,7 @@ example : bracketDiag' X K = bracketDiag X K := rfl
 
 abbrev HasBracket : Prop := HasLimit (X.bracketDiag K)
 
+/-- The bracket `X[K]` of a simplical object `X` with a simplicial set `K`. -/
 noncomputable abbrev bracket [X.HasBracket K] : C :=
   limit (X.bracketDiag K)
 
