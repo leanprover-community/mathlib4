97ccf40ffa8af5a25eefcf9b102ad71e30284458
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2026-05-18 15:56:36 +0200

    reduce priority for two more instances

--- Mathlib/CategoryTheory/Monoidal/Internal/Types/CommGrp_.lean
+++ Mathlib/CategoryTheory/Monoidal/Internal/Types/CommGrp_.lean
@@ -27,7 +27,8 @@ open CategoryTheory MonObj ConcreteCategory
 
 namespace CommGrpTypeEquivalenceCommGrp
 
-instance commGrpCommGroup (A : Type u) [GrpObj A] [IsCommMonObj A] : CommGroup A :=
+instance (priority := 900) commGrpCommGroup (A : Type u) [GrpObj A] [IsCommMonObj A] :
+    CommGroup A :=
   { GrpTypeEquivalenceGrp.grpGroup A with
     mul_comm := fun x y => by convert congr_hom (CC := fun X ↦ X) (IsCommMonObj.mul_comm A) (y, x) }
 
