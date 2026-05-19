7531e4e3b2ad857487278d9439272384c3d9a462
AlexBrodbelt <apbrodbelt@gmail.com>
2026-05-19 12:36:42 +0200

    follow modifications from review

--- Mathlib/Data/Finite/Option.lean
+++ Mathlib/Data/Finite/Option.lean
@@ -6,17 +6,18 @@ Authors: Alex Brodbelt, Eric Wieser
 module
 
 public import Mathlib.Data.Fintype.Option
-public import Mathlib.Logic.Equiv.Fin.Basic
+import Mathlib.Logic.Equiv.Fin.Basic
 
 /-!
 # Finiteness conditions for `Option` types
 -/
 
-@[expose] public section
-/-- The `Option` type on a type is finite if and only if the underlying type is finite -/
+public section
+
+/-- The `Option` type on a type is finite if and only if the underlying type is finite. -/
 @[simp]
 theorem Option.finite_iff {α : Type*} : Finite (Option α) ↔ Finite α where
-  mpr _ := instFiniteOption
+  mpr _ := inferInstance
   mp
   | @Finite.intro _ 0 e => (e none).elim0
   | @Finite.intro _ (n + 1) e => ⟨(e.trans (finSuccEquiv n)).removeNone⟩
