81b6c468a201107d81fdc00bdeb8f5ff65edbff5
AlexBrodbelt <apbrodbelt@gmail.com>
2026-05-19 11:05:09 +0200

    feat(Mathlib/Data/Finite/Subtype): Subtype of elements not equal to a term is finite iff type is finite

---
+++ Mathlib/Data/Finite/Subtype.lean
@@ -0,0 +1,17 @@
+/-
+Copyright (c) 2026 Alex Brodbelt. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Alex Brodbelt and Eric Wieser
+-/
+module
+
+public import Mathlib.Data.Finite.Option
+
+@[expose] public section
+
+@[simp]
+theorem Subtype.finite_ne_iff {α : Type*} (a₀ : α) : Finite {a // a ≠ a₀} ↔ Finite α := by
+  classical
+  rw [← (Equiv.optionSubtypeNe a₀).finite_iff, Option.finite_iff]
+
+#min_imports
