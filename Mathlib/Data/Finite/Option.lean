a88c8cce5bc40ce6c40484b1e86a86a5621ee4e9
AlexBrodbelt <apbrodbelt@gmail.com>
2026-05-19 12:04:18 +0200

    added docstrings and comments to files

--- Mathlib/Data/Finite/Option.lean
+++ Mathlib/Data/Finite/Option.lean
@@ -1,15 +1,19 @@
 /-
 Copyright (c) 2026 Alex Brodbelt. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Alex Brodbelt and Eric Wieser
+Authors: Alex Brodbelt, Eric Wieser
 -/
 module
 
 public import Mathlib.Data.Fintype.Option
 public import Mathlib.Logic.Equiv.Fin.Basic
 
-@[expose] public section
+/-!
+# Finiteness conditions for `Option` types
+-/
 
+@[expose] public section
+/-- The `Option` type on a type is finite if and only if the underlying type is finite -/
 @[simp]
 theorem Option.finite_iff {α : Type*} : Finite (Option α) ↔ Finite α where
   mpr _ := instFiniteOption
