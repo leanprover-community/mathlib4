f4297c9604bc38268fbb82e45517fc15f509290e
AlexBrodbelt <apbrodbelt@gmail.com>
2026-05-19 12:01:20 +0200

    add file to Mathlib.lean and add docstrings

--- Mathlib/Data/Finite/Subtype.lean
+++ Mathlib/Data/Finite/Subtype.lean
@@ -1,14 +1,19 @@
 /-
 Copyright (c) 2026 Alex Brodbelt. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Alex Brodbelt and Eric Wieser
+Authors: Alex Brodbelt, Eric Wieser
 -/
 module
 
 public import Mathlib.Data.Finite.Option
 
+/-!
+# Finitiness conditions on subtypes
+-/
+
 @[expose] public section
 
+/-- The subtype of terms not equal to a given term is finite if and only if the type is finite -/
 @[simp]
 theorem Subtype.finite_ne_iff {α : Type*} (a₀ : α) : Finite {a // a ≠ a₀} ↔ Finite α := by
   classical
