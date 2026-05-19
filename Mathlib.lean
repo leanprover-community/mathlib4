ee770a5dee872e9b038d95a8fa089832ba87fe9a
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2026-05-20 00:46:46 +0200

    adjust Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -6472,6 +6472,7 @@ public import Mathlib.RingTheory.FractionalIdeal.Operations
 public import Mathlib.RingTheory.FreeCommRing
 public import Mathlib.RingTheory.FreeRing
 public import Mathlib.RingTheory.Frobenius
+public import Mathlib.RingTheory.GeneralizedEuclidean
 public import Mathlib.RingTheory.GradedAlgebra.AlgHom
 public import Mathlib.RingTheory.GradedAlgebra.Basic
 public import Mathlib.RingTheory.GradedAlgebra.FiniteType
