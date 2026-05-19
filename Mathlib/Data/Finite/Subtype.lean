bb97a2a2e6af1ae5d1f488faa19fe0710b224306
AlexBrodbelt <apbrodbelt@gmail.com>
2026-05-19 12:42:49 +0200

    fix typos and styling

--- Mathlib/Data/Finite/Subtype.lean
+++ Mathlib/Data/Finite/Subtype.lean
@@ -8,12 +8,12 @@ module
 public import Mathlib.Data.Finite.Option
 
 /-!
-# Finitiness conditions on subtypes
+# `Finite`ness conditions on subtypes
 -/
 
 @[expose] public section
 
-/-- The subtype of terms not equal to a given term is finite if and only if the type is finite -/
+/-- The subtype of terms not equal to a given term is finite if and only if the type is finite. -/
 @[simp]
 theorem Subtype.finite_ne_iff {α : Type*} (a₀ : α) : Finite {a // a ≠ a₀} ↔ Finite α := by
   classical
