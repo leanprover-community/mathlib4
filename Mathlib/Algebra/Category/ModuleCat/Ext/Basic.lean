f6be0b059848ae0f523482114e24955964d0b0b4
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2026-05-19 13:36:24 +0800

    golf

--- Mathlib/Algebra/Category/ModuleCat/Ext/Basic.lean
+++ Mathlib/Algebra/Category/ModuleCat/Ext/Basic.lean
@@ -31,7 +31,6 @@ namespace CategoryTheory.Abelian
 
 variable [Small.{v} R] {M N : ModuleCat.{v} R}
 
-set_option backward.isDefEq.respectTransparency false in
 lemma Ext.postcomp_smul_id_eq_zero_of_mem_annihilator {r : R} (mem_ann : r ∈ Module.annihilator R N)
     (n : ℕ) : AddCommGrpCat.ofHom ((Ext.mk₀ (r • 𝟙 M)).postcomp N (add_zero n)) = 0 := by
   ext h
@@ -40,7 +39,6 @@ lemma Ext.postcomp_smul_id_eq_zero_of_mem_annihilator {r : R} (mem_ann : r ∈ M
   have smul_eq : r • h = (Ext.mk₀ (r • 𝟙 N)).comp h (zero_add n) := by simp [Ext.mk₀_smul]
   simp [Ext.mk₀_smul, this, smul_eq]
 
-set_option backward.isDefEq.respectTransparency false in
 lemma Ext.postcomp_smul_id_mono_iff (r : R) (i : ℕ) :
     Mono (AddCommGrpCat.ofHom ((Ext.mk₀ (r • 𝟙 M)).postcomp N (add_zero i))) ↔
     IsSMulRegular (Ext N M i) r := by
