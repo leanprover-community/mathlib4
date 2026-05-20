898e96492a83e94098eeb3455dc4f81be1fc53cd
Dagur Asgeirsson <dagurtomas@gmail.com>
2026-05-20 09:39:20 -0600

    feat(CategoryTheory): the bicategory of profunctors

--- Mathlib.lean
+++ Mathlib.lean
@@ -3243,6 +3243,7 @@ public import Mathlib.CategoryTheory.Products.Basic
 public import Mathlib.CategoryTheory.Products.Bifunctor
 public import Mathlib.CategoryTheory.Products.Unitor
 public import Mathlib.CategoryTheory.Profunctor.Basic
+public import Mathlib.CategoryTheory.Profunctor.Bicategory
 public import Mathlib.CategoryTheory.Profunctor.Comp
 public import Mathlib.CategoryTheory.Quotient
 public import Mathlib.CategoryTheory.Quotient.Linear
