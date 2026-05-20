f4730bf1242effd1944bec1438a5060784d6b949
Snir Broshi <26556598+SnirBroshi@users.noreply.github.com>
2026-05-21 02:34:09 +0300

    feat(LinearAlgebra/Matrix/Nondegenerate): generalize `det ≠ 0 → Nondegenerate` to non-domains with `det ∈ nonZeroDivisors`

--- Mathlib/LinearAlgebra/Matrix/Nondegenerate.lean
+++ Mathlib/LinearAlgebra/Matrix/Nondegenerate.lean
@@ -45,8 +45,7 @@ structure Nondegenerate (M : Matrix m n R) : Prop where
 
 end Finite
 
-variable {m n R A : Type*} [CommRing R] [Fintype m] [Fintype n] [CommRing A] [IsDomain A]
-  {M : Matrix m n R}
+variable {m n R A : Type*} [CommRing R] [Fintype m] [Fintype n] [CommRing A] {M : Matrix m n R}
 
 lemma separatingRight_def : M.SeparatingRight ↔ (∀ w, (∀ v, v ⬝ᵥ M *ᵥ w = 0) → w = 0) := by
   refine forall_congr' fun w ↦ ⟨fun hM hw ↦ hM ?_, fun hM hw ↦ hM ?_⟩ <;>
@@ -85,30 +84,43 @@ theorem Nondegenerate.exists_not_ortho_of_ne_zero' {M : Matrix m n R} (hM : Nond
 section Determinant
 variable [DecidableEq m] {M : Matrix m m A}
 
+theorem nondegenerate_of_det_mem_nonZeroDivisors (hM : M.det ∈ nonZeroDivisors A) :
+    Nondegenerate M := by
+  refine nondegenerate_def.mpr ⟨fun v h ↦ ?_, fun w h ↦ ?_⟩
+  · ext i
+    apply mem_nonZeroDivisors_iff_left.mp hM
+    simpa using h <| M.cramer <| Pi.single i 1
+  · ext i
+    apply mem_nonZeroDivisors_iff_left.mp hM
+    have h := h <| Pi.single i 1 ᵥ* M.adjugate
+    rw [dotProduct_mulVec, vecMul_vecMul, adjugate_mul, dotProduct] at h
+    simpa [one_apply] using h
+
 /-- If `M` is square and has nonzero determinant, then `M` as a bilinear form on `n → A` is
 nondegenerate. The "iff" implication, `nondegenerate_iff_det_ne_zero`, is proved in a later file.
 
 See also `BilinForm.nondegenerateOfDetNeZero'` and `BilinForm.nondegenerateOfDetNeZero`.
 -/
-theorem nondegenerate_of_det_ne_zero (hM : M.det ≠ 0) : Nondegenerate M := by
-  refine nondegenerate_def.mpr ⟨fun v h ↦ ?_, fun w h ↦ ?_⟩
-  · ext i
-    specialize h (M.cramer (Pi.single i 1))
-    simp_all
-  · ext i
-    contrapose! h
-    use Pi.single i 1 ᵥ* M.adjugate
-    rw [dotProduct_mulVec, vecMul_vecMul, adjugate_mul]
-    simp_all [dotProduct, smul_apply, smul_eq_mul, Matrix.one_apply]
+theorem nondegenerate_of_det_ne_zero [NoZeroDivisors A] (hM : M.det ≠ 0) : Nondegenerate M :=
+  nondegenerate_of_det_mem_nonZeroDivisors <| mem_nonZeroDivisors_of_ne_zero hM
 
-theorem eq_zero_of_vecMul_eq_zero (hM : M.det ≠ 0) {v : m → A}
-    (hv : v ᵥ* M = 0) : v = 0 :=
-  (nondegenerate_of_det_ne_zero hM).eq_zero_of_ortho fun w => by
+theorem eq_zero_of_vecMul_eq_zero_of_det_mem_nonZeroDivisors (hM : M.det ∈ nonZeroDivisors A)
+    {v : m → A} (hv : v ᵥ* M = 0) : v = 0 :=
+  nondegenerate_of_det_mem_nonZeroDivisors hM |>.eq_zero_of_ortho fun w ↦ by
     rw [dotProduct_mulVec, hv, zero_dotProduct]
 
-theorem eq_zero_of_mulVec_eq_zero (hM : M.det ≠ 0) {v : m → A}
+theorem eq_zero_of_vecMul_eq_zero [NoZeroDivisors A] (hM : M.det ≠ 0) {v : m → A}
+    (hv : v ᵥ* M = 0) : v = 0 :=
+  eq_zero_of_vecMul_eq_zero_of_det_mem_nonZeroDivisors (mem_nonZeroDivisors_of_ne_zero hM) hv
+
+theorem eq_zero_of_mulVec_eq_zero_of_det_mem_nonZeroDivisors (hM : M.det ∈ nonZeroDivisors A)
+    {v : m → A} (hv : M *ᵥ v = 0) : v = 0 :=
+  eq_zero_of_vecMul_eq_zero_of_det_mem_nonZeroDivisors (by rwa [det_transpose]) <|
+    vecMul_transpose M v |>.trans hv
+
+theorem eq_zero_of_mulVec_eq_zero [NoZeroDivisors A] (hM : M.det ≠ 0) {v : m → A}
     (hv : M *ᵥ v = 0) : v = 0 :=
-  eq_zero_of_vecMul_eq_zero (by rwa [det_transpose]) ((vecMul_transpose M v).trans hv)
+  eq_zero_of_mulVec_eq_zero_of_det_mem_nonZeroDivisors (mem_nonZeroDivisors_of_ne_zero hM) hv
 
 end Determinant
 
