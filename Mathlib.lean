4bea9a03947c9ac55d25ab35c5491c9e07ad538e
Raph-DG <raphaeldouglasgiles@gmail.com>
2026-05-21 21:32:26 +0200

    Update Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -7718,6 +7718,7 @@ public import Mathlib.Topology.LocallyConstant.Algebra
 public import Mathlib.Topology.LocallyConstant.Basic
 public import Mathlib.Topology.LocallyFinite
 public import Mathlib.Topology.LocallyFinsupp
+public import Mathlib.Topology.LocallyFinsupp.Pushforward
 public import Mathlib.Topology.Maps.Basic
 public import Mathlib.Topology.Maps.OpenQuotient
 public import Mathlib.Topology.Maps.Proper.Basic
