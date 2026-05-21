b458cd6f32d8e3254c6ece663362ae8c89a28de4
Moritz Doll <moritz.doll@googlemail.com>
2026-05-21 16:01:18 -0400

    fix more

--- Archive/Sensitivity.lean
+++ Archive/Sensitivity.lean
@@ -285,7 +285,7 @@ using only the addition of `V`. -/
 set_option backward.isDefEq.respectTransparency false in
 theorem f_squared (v : V n) : (f n) (f n v) = (n : ℝ) • v := by
   induction n with
-  | zero => simp only [Nat.cast_zero, zero_smul, f_zero, zero_apply]
+  | zero => simp only [Nat.cast_zero, zero_smul, f_zero, LinearMap.zero_apply]
   | succ n IH =>
     cases v; rw [f_succ_apply, f_succ_apply]; simp [IH, add_smul (n : ℝ) 1, add_assoc]; abel
 
