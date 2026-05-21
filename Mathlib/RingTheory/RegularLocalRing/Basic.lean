02e7b8c8f5d546bc87a250ee92d4b49772c9db53
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2026-05-21 14:33:46 +0800

    fix imports

--- Mathlib/RingTheory/RegularLocalRing/Basic.lean
+++ Mathlib/RingTheory/RegularLocalRing/Basic.lean
@@ -6,12 +6,9 @@ Authors: Nailin Guan
 module
 
 public import Mathlib.LinearAlgebra.Dimension.OrzechProperty
-public import Mathlib.RingTheory.DiscreteValuationRing.TFAE
 public import Mathlib.RingTheory.LocalRing.MaximalIdeal.Square
-public import Mathlib.RingTheory.RegularLocalRing.Defs
-public import Mathlib.RingTheory.Ideal.KrullsHeightTheorem
-public import Mathlib.RingTheory.KrullDimension.Field
 public import Mathlib.RingTheory.Regular.RegularSequence
+public import Mathlib.RingTheory.RegularLocalRing.Defs
 
 /-!
 
