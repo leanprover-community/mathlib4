6b09cba21b9209638a8c407d282ec3e84e3bebe5
ADedecker <anatolededecker@gmail.com>
2026-05-21 21:16:46 +0200

    chore(Topology/Algebra/Module/LinearMap): deprecate_module

--- Mathlib.lean
+++ Mathlib.lean
@@ -7474,6 +7474,7 @@ public import Mathlib.Topology.Algebra.Module.Determinant
 public import Mathlib.Topology.Algebra.Module.Equiv
 public import Mathlib.Topology.Algebra.Module.FiniteDimension
 public import Mathlib.Topology.Algebra.Module.FiniteDimensionBilinear
+public import Mathlib.Topology.Algebra.Module.LinearMap
 public import Mathlib.Topology.Algebra.Module.LinearPMap
 public import Mathlib.Topology.Algebra.Module.LocallyConvex
 public import Mathlib.Topology.Algebra.Module.ModuleTopology
