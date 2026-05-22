a8a24c138124febae4120255cb44045eda2f06d2
Sorrachai Yingchareonthawornchai <sorrachai.cp@gmail.com>
2026-05-22 16:23:53 +0200

    rename branch

--- Mathlib/Data/Tree/Basic.lean
+++ Mathlib/Data/Tree/Basic.lean
@@ -126,98 +126,6 @@ def right : BinaryTree α → BinaryTree α
 /-- A node with `Unit` data -/
 scoped infixr:65 " △ " => BinaryTree.node ()
 
-/-- A notation for a tree node -/
-scoped notation:65 l:66 " △[" v "] " r:66 => BinaryTree.node v l r
-
-/--
-BinaryTree membership, typically accessed via the `∈` operator.
-
-`a ∈ t` means that `a` is an element of the binary tree `t`.
-Elements are compared according to Lean's logical equality.
-
-Examples:
-* `a ∈ ((nil △[x] nil) △[y] nil) ↔ a = x ∨ a = y`
--/
-inductive Mem (a : α) : BinaryTree α → Prop
-| node  {l r}   : Mem a (l △[a] r)
-| left  (b : α) {l r} : Mem a l → Mem a (l △[b] r)
-| right (b : α) {l r} : Mem a r → Mem a (l △[b] r)
-
-/-- Defines the `∈` notation for `BinaryTree`. -/
-instance : Membership α (BinaryTree α) where
-  mem l a := Mem a l
-
-theorem mem_singleton_iff (a x : ℕ) : a ∈ ((nil △[x] nil)) ↔ a = x := by
-  constructor
-  · intro h
-    cases h <;> trivial
-  · intro h
-    subst h
-    apply Mem.node
-
-/-- In a binary tree, `contains` operation traverses over the tree and make equality check. -/
-def contains [BEq α] (t : BinaryTree α) (a : α) :  Bool := match t with
-  | .nil        => false
-  | l △[b] r => a == b || l.contains a  || r.contains a
-
-/-- `contains` is sound and complete with respect to `Mem`. -/
-theorem contains_iff [BEq α] [LawfulBEq α]
-  (a : α) (t : BinaryTree α) : t.contains a = true ↔ a ∈ t := by
-  fun_induction contains
-  · simp_all only [Bool.false_eq_true, false_iff]
-    false_or_by_contra
-    expose_names
-    cases h
-  · expose_names
-    simp only [Bool.or_eq_true, beq_iff_eq, ih2, ih1]
-    constructor
-    · rintro ( ⟨h | h⟩ | h)
-      · subst h; exact .node
-      · exact .left a_1 h
-      · exact .right a_1 h
-    · intro h
-      cases h
-      · grind
-      · left
-        right
-        assumption
-      · right; assumption
-
-instance [BEq α] [LawfulBEq α] (a : α) (t : BinaryTree α) : Decidable (a ∈ t)  :=
-  decidable_of_iff (t.contains a = true) (t.contains_iff a)
-
-/-- An implementation of checking whether a boolean predicate holds for every node in the tree.
-    Traversal order: current node, then left subtree, then right subtree. -/
-def all (p : α → Bool) : BinaryTree α → Bool
-  | nil        => true
-  | l △[b] r => p b && all p l && all p r
-
-theorem all_iff (p : α → Prop) [DecidablePred p]
-  (t : BinaryTree α) : t.all (fun x ↦ p x) = true ↔ ∀ x ∈ t, p x := by
-  fun_induction all
-  · simp only [true_iff]; intros; contradiction
-  · expose_names
-    simp only [Bool.and_eq_true, decide_eq_true_eq, ih2, ih1]
-    constructor
-    · rintro ⟨⟨h1,h3⟩,h2⟩ x hx
-      cases hx
-      · assumption
-      · expose_names
-        apply h3
-        exact h
-      · expose_names
-        apply h2
-        exact h
-    · intro h
-      exact ⟨⟨h a .node,
-        fun x m => h x (.left a m)⟩,
-        fun x m => h x (.right a m)⟩
-
-/-- `Decidable` instance for bounded universal quantification over a `BinaryTree`. -/
-instance decidableBAll (p : α → Prop) [DecidablePred p] (t : BinaryTree α) :
-    Decidable (∀ x ∈ t, p x) :=
-      decidable_of_iff (t.all (fun x => p x)) (all_iff p t)
-
 /-- Induction principle for `BinaryTree Unit`s -/
 @[elab_as_elim]
 def unitRecOn {motive : BinaryTree Unit → Sort*} (t : BinaryTree Unit) (base : motive nil)
@@ -227,21 +135,5 @@ def unitRecOn {motive : BinaryTree Unit → Sort*} (t : BinaryTree Unit) (base :
 theorem left_node_right_eq_self : ∀ {x : BinaryTree Unit} (_hx : x ≠ nil), x.left △ x.right = x
   | nil, h => by trivial
   | node _ _ _, _ => rfl  -- Porting note: `a △ b` no longer works in pattern matching
-
-
-/-- Inorder traversal into a list: left → node → right. -/
-def toListInOrder : BinaryTree α → List α
-  | nil          => []
-  | l △[v] r  => l.toListInOrder ++ [v] ++ r.toListInOrder
-
-/-- Preorder traversal  into a list: node → left → right. -/
-def toListPreOrder : BinaryTree α → List α
-  | nil          => []
-  | l △[v] r  => [v] ++ l.toListPreOrder ++ r.toListPreOrder
-
-/-- Postorder traversal  into a list: left → right → node. -/
-def toListPostOrder : BinaryTree α → List α
-  | nil          => []
-  | l △[v] r  => l.toListPostOrder ++ r.toListPostOrder ++ [v]
 
 end BinaryTree
