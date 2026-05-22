fb28163f883192734e550a64bc89dac7335c94bb
Riccardo Brasca <riccardo.brasca@gmail.com>
2026-05-22 11:15:03 +0100

    extendedHom_eq_one_of_forall_isPrincipal

--- Mathlib/RingTheory/ClassGroup/ExtendedHom.lean
+++ Mathlib/RingTheory/ClassGroup/ExtendedHom.lean
@@ -46,7 +46,6 @@ noncomputable def extendedHom : ClassGroup A →* ClassGroup B :=
 lemma extendedHom_quotientMk (α : (FractionalIdeal A⁰ (FractionRing A))ˣ) :
     extendedHom A B (QuotientGroup.mk α) = QuotientGroup.mk
       (Units.map (FractionalIdeal.extendedHom (FractionRing B) B).toMonoidHom α) := by
-  rw [extendedHom]
   rfl
 
 @[simp]
@@ -56,21 +55,30 @@ theorem extendedHom_mk (I : (FractionalIdeal A⁰ (FractionRing A))ˣ) :
   rw [← ClassGroup.Quot_mk_eq_mk, ← ClassGroup.Quot_mk_eq_mk]
   exact extendedHom_quotientMk A B I
 
+/-- The extension of a nonzero integral ideal along an injective extension of domains. -/
+abbrev extendedIdeal (I : (Ideal A)⁰) : (Ideal B)⁰ :=
+  ⟨I.1.map (algebraMap A B), mem_nonZeroDivisors_iff_ne_zero.mpr <|
+    (Ideal.map_eq_bot_iff_of_injective (FaithfulSMul.algebraMap_injective A B)).not.mpr
+      (mem_nonZeroDivisors_iff_ne_zero.mp I.2)⟩
+
+@[simp]
+theorem extendedIdeal_extendedIdeal (C : Type*) [CommRing C] [IsDomain C] [Algebra B C]
+    [Algebra A C] [IsScalarTower A B C] [Module.IsTorsionFree B C]
+    [Module.IsTorsionFree A C] (I : (Ideal A)⁰) :
+    extendedIdeal B C (extendedIdeal A B I) = extendedIdeal A C I := by
+  simp [Ideal.map_map, IsScalarTower.algebraMap_eq A B C]
+
 variable [IsDedekindDomain A]
 
-/-- `ClassGroup.mk0` factors through the canonical quotient projection
-on `(FractionalIdeal A⁰ (FractionRing A))ˣ`. -/
+/-- `ClassGroup.mk0` factors through the canonical quotient projection on
+`(FractionalIdeal A⁰ (FractionRing A))ˣ`. -/
 lemma mk0_eq_quotientMk (I : (Ideal A)⁰) :
     (ClassGroup.mk0 I : ClassGroup A) =
       QuotientGroup.mk (FractionalIdeal.mk0 (FractionRing A) I) :=
   (ClassGroup.mk_mk0 (K := FractionRing A) I).symm.trans (ClassGroup.Quot_mk_eq_mk _).symm
 
 lemma extendedHom_mk0 [IsDedekindDomain B] (I : (Ideal A)⁰) :
-    extendedHom A B (ClassGroup.mk0 I) = ClassGroup.mk0 ⟨I.1.map (algebraMap A B),
-      mem_nonZeroDivisors_iff_ne_zero.mpr <|
-        (Ideal.map_eq_bot_iff_of_injective
-          (FaithfulSMul.algebraMap_injective A B)).not.mpr
-        (mem_nonZeroDivisors_iff_ne_zero.mp I.2)⟩ := by
+    extendedHom A B (ClassGroup.mk0 I) = ClassGroup.mk0 (extendedIdeal A B I) := by
   rw [mk0_eq_quotientMk, mk0_eq_quotientMk, extendedHom_quotientMk]
   congr; ext : 1
   exact FractionalIdeal.extendedHom_coeIdeal_eq_map (L := FractionRing B) (B := B) _
@@ -78,8 +86,31 @@ lemma extendedHom_mk0 [IsDedekindDomain B] (I : (Ideal A)⁰) :
 theorem extendedHom_mk0' (I : (Ideal A)⁰) :
     extendedHom A B (ClassGroup.mk0 I) =
       ClassGroup.mk (Units.map (FractionalIdeal.extendedHom (FractionRing B) B).toMonoidHom
-        (FractionalIdeal.mk0 (FractionRing A) I)) := by
+      (FractionalIdeal.mk0 (FractionRing A) I)) := by
   rw [← ClassGroup.mk_mk0 (FractionRing A), extendedHom_mk]
+
+@[simp]
+theorem extendedHom_comp_apply (C : Type*) [CommRing C] [IsDomain C] [Algebra B C]
+    [Algebra A C] [IsScalarTower A B C] [Module.IsTorsionFree B C]
+    [Module.IsTorsionFree A C] [IsDedekindDomain B] [IsDedekindDomain C] (x : ClassGroup A) :
+    extendedHom B C (extendedHom A B x) = extendedHom A C x := by
+  obtain ⟨I, rfl⟩ := ClassGroup.mk0_surjective x
+  rw [extendedHom_mk0 A B I, extendedHom_mk0 B C (extendedIdeal A B I),
+    extendedHom_mk0 A C I, extendedIdeal_extendedIdeal]
+
+theorem extendedHom_comp (C : Type*) [CommRing C] [IsDomain C] [Algebra B C] [Algebra A C]
+    [IsScalarTower A B C] [Module.IsTorsionFree B C] [Module.IsTorsionFree A C]
+    [IsDedekindDomain B] [IsDedekindDomain C] :
+    (extendedHom B C).comp (extendedHom A B) = extendedHom A C := by
+  ext x
+  exact extendedHom_comp_apply A B C x
+
+theorem extendedHom_eq_one_of_forall_isPrincipal [IsDedekindDomain B]
+    (h : ∀ I : (Ideal A), (I.map (algebraMap A B)).IsPrincipal) : extendedHom A B = 1 := by
+  ext x
+  obtain ⟨I, rfl⟩ := ClassGroup.mk0_surjective x
+  rw [extendedHom_mk0, MonoidHom.one_apply]
+  exact (ClassGroup.mk0_eq_one_iff (extendedIdeal A B I).2).mpr (by simpa using h I)
 
 end ClassGroup
 
