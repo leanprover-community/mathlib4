506ed18876d86a4128d1afcb5e4ba931ec2ac332
drocta <madaco.madaco@gmail.com>
2026-05-21 19:43:52 -0700

    add @[to_additive] on instance of NormedGroup for DirectLimit, and remove the separate instance (and namespace) specific to the NormedAddGroup version

--- Mathlib/Analysis/Normed/Group/DirectLimit.lean
+++ Mathlib/Analysis/Normed/Group/DirectLimit.lean
@@ -53,29 +53,12 @@ variable [∀ i, NormedGroup (G i)]
 variable [∀ i j h, MonoidHomClass (T h) (G i) (G j)]
 variable [∀ i j h, IsometryClass (T h) (G i) (G j)]
 
+@[to_additive]
 noncomputable instance : NormedGroup (DirectLimit G f) where
   __ := (inferInstance : SeminormedGroup (DirectLimit G f))
   __ := (inferInstance : MetricSpace (DirectLimit G f))
 
 end NormedGroup
-
-namespace NormedAddGroup
-
-variable [∀ i, NormedAddGroup (G i)]
-variable [∀ i j h, AddMonoidHomClass (T h) (G i) (G j)]
-variable [∀ i j h, IsometryClass (T h) (G i) (G j)]
-
-noncomputable instance instNormedAddGroup : NormedAddGroup (DirectLimit G f) where
-  norm := DirectLimit.lift f (ih := fun i x ↦ ‖x‖) (fun i j h x ↦ by
-    simpa [NormedAddGroup.dist_eq] using (IsometryClass.dist_eq (f i j h) 0 x).symm)
-  dist_eq := DirectLimit.induction₂ f (fun i x y ↦ by
-    rw [dist_def, NormedAddGroup.dist_eq, neg_def, add_def, DirectLimit.lift_def])
-
-lemma norm_def (i : ι) (x : G i) : ‖(⟦⟨i, x⟩⟧ : DirectLimit G f)‖ = ‖x‖ := by
-  change DirectLimit.lift f (ih := fun i x ↦ ‖x‖) _ ⟦⟨i, x⟩⟧ = ‖x‖
-  apply DirectLimit.lift_def
-
-end NormedAddGroup
 
 namespace NormedAddCommGroup
 
@@ -84,7 +67,7 @@ variable [∀ i j h, AddMonoidHomClass (T h) (G i) (G j)]
 variable [∀ i j h, IsometryClass (T h) (G i) (G j)]
 
 noncomputable instance instNormedAddCommGroup : NormedAddCommGroup (DirectLimit G f) where
-  __ := NormedAddGroup.instNormedAddGroup
+  __ := (inferInstance : NormedAddGroup (DirectLimit G f))
   __ := (inferInstance : AddCommGroup (DirectLimit G f))
 
 end NormedAddCommGroup
