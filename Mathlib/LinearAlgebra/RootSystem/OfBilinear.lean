98c315721a1087ce5cd57ea2d4aa0ae4d319a669
Aaron Liu <aaronliu2008@outlook.com>
2026-05-23 16:13:09 -0400

    fix stuff

--- Mathlib/LinearAlgebra/RootSystem/OfBilinear.lean
+++ Mathlib/LinearAlgebra/RootSystem/OfBilinear.lean
@@ -160,8 +160,8 @@ def ofBilinear [IsReflexive R M] (B : M →ₗ[R] M →ₗ[R] R) (hNB : LinearMa
       right_inv := by
         intro y
         simp [involutive_reflection (coroot_apply_self B x.2) y] }
-  reflectionPerm_root x y := by
-    simp [Module.reflection_apply]
+  reflectionPerm_root := by
+    simp [coe_setOf, Module.reflection_apply]
   reflectionPerm_coroot x y := by
     simp only [coe_setOf, mem_setOf_eq, Embedding.coeFn_mk, Embedding.subtype_apply,
       Dual.eval_apply, Equiv.coe_fn_mk]
