3d224a2f89fa5701eccc501af4e894775bebcb8d
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2026-05-24 19:34:43 +0200

    fix ...Group.InfiniteSum

--- Mathlib/Analysis/Normed/Group/InfiniteSum.lean
+++ Mathlib/Analysis/Normed/Group/InfiniteSum.lean
@@ -40,8 +40,8 @@ open Topology ENNReal NNReal
 
 open Finset Filter Metric
 
-variable {ι α E F ε : Type*} [SeminormedAddCommGroup E] [SeminormedAddCommGroup F]
-  [TopologicalSpace ε] [ESeminormedAddCommMonoid ε]
+variable {ι α E F ε : Type*} [AddCommGroup E] [SeminormedAddCommGroup E] [AddCommGroup F]
+  [SeminormedAddCommGroup F] [TopologicalSpace ε] [AddCommMonoid ε] [ESeminormedAddCommMonoid ε]
 
 theorem cauchySeq_finset_iff_vanishing_norm {f : ι → E} :
     (CauchySeq fun s : Finset ι => ∑ i ∈ s, f i) ↔
