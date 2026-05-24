b04aafce88bde29caee10e8862506710e05ba640
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2026-05-24 22:16:06 +0200

    fix ...Operator.Completeness

--- Mathlib/Analysis/Normed/Operator/Completeness.lean
+++ Mathlib/Analysis/Normed/Operator/Completeness.lean
@@ -24,7 +24,7 @@ open Filter hiding map_smul
 open scoped NNReal Topology Uniformity
 
 -- the `ₗ` subscript variables are for special cases about linear (as opposed to semilinear) maps
-variable {𝕜 𝕜₂ E F Fₗ : Type*}
+variable {𝕜 𝕜₂ E F Fₗ : Type*} [AddCommGroup E] [AddCommGroup F] [AddCommGroup Fₗ]
 variable [NormedAddCommGroup E] [NormedAddCommGroup F] [NormedAddCommGroup Fₗ]
 
 variable [NontriviallyNormedField 𝕜] [NontriviallyNormedField 𝕜₂]
@@ -35,7 +35,8 @@ namespace ContinuousLinearMap
 
 section Completeness
 
-variable {E' : Type*} [SeminormedAddCommGroup E'] [NormedSpace 𝕜 E'] [RingHomIsometric σ₁₂]
+variable {E' : Type*} [AddCommGroup E'] [SeminormedAddCommGroup E'] [NormedSpace 𝕜 E']
+  [RingHomIsometric σ₁₂]
 
 /-- Construct a bundled continuous (semi)linear map from a map `f : E → F` and a proof of the fact
 that it belongs to the closure of the image of a bounded set `s : Set (E →SL[σ₁₂] F)` under coercion
