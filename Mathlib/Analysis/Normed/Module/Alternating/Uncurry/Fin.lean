c22fc5e006a0b99cd151d55fa9753028391625fa
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2026-05-24 23:24:51 +0200

    fix ...Module.Alternating.Uncurry.Fin

--- Mathlib/Analysis/Normed/Module/Alternating/Uncurry/Fin.lean
+++ Mathlib/Analysis/Normed/Module/Alternating/Uncurry/Fin.lean
@@ -50,9 +50,9 @@ open Fin Function
 namespace ContinuousAlternatingMap
 
 variable {𝕜 E F G : Type*} [NontriviallyNormedField 𝕜]
-  [NormedAddCommGroup E] [NormedSpace 𝕜 E]
-  [NormedAddCommGroup F] [NormedSpace 𝕜 F]
-  [NormedAddCommGroup G] [NormedSpace 𝕜 G]
+  [AddCommGroup E] [NormedAddCommGroup E] [NormedSpace 𝕜 E]
+  [AddCommGroup F] [NormedAddCommGroup F] [NormedSpace 𝕜 F]
+  [AddCommGroup G] [NormedAddCommGroup G] [NormedSpace 𝕜 G]
   {n : ℕ}
 
 /-- If `f` is a continuous `(n + 1)`-multilinear alternating map, `x` is an element of the domain,
