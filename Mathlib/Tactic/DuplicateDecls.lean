3caf62d1f0344158cfe9467b72c4615517637c0d
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2026-05-24 04:13:18 +0100

    make it a module

--- Mathlib/Tactic/DuplicateDecls.lean
+++ Mathlib/Tactic/DuplicateDecls.lean
@@ -3,7 +3,9 @@ Copyright (c) 2026 Jovan Gerbscheid. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jovan Gerbscheid
 -/
-import Mathlib.Init
+module
+
+public import Mathlib.Init
 
 /-!
 # A tool for finding duplicate declarations
