ae4265723423f5784777ae8f8435b04aa7314e0c
Vlad Tsyrklevich <vlad@tsyrklevi.ch>
2026-05-24 13:52:13 +0200

    delete unnecessary import

--- Mathlib/Algebra/Module/Projective.lean
+++ Mathlib/Algebra/Module/Projective.lean
@@ -6,7 +6,6 @@ Authors: Kevin Buzzard, Antoine Labelle
 module
 
 public import Mathlib.Algebra.Module.Shrink
-public import Mathlib.LinearAlgebra.DirectSum.Basis
 public import Mathlib.LinearAlgebra.TensorProduct.Basis
 public import Mathlib.Logic.UnivLE
 
