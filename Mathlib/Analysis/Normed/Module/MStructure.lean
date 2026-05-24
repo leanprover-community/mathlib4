97c97f38f30eb59ca2d862c687cc355ee0123be6
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2026-05-24 18:07:27 +0200

    fix ...Module.MStructure

--- Mathlib/Analysis/Normed/Module/MStructure.lean
+++ Mathlib/Analysis/Normed/Module/MStructure.lean
@@ -63,7 +63,7 @@ M-summand, M-projection, L-summand, L-projection, M-ideal, M-structure
 
 @[expose] public section
 
-variable (X : Type*) [NormedAddCommGroup X]
+variable (X : Type*) [AddCommGroup X] [NormedAddCommGroup X]
 variable {M : Type*} [Ring M] [Module M X]
 
 
