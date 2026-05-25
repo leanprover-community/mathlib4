4b5a9a6c2b4a005775eccc6a979b4a9a18a3592f
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2026-05-25 15:33:02 +0200

    fix Analysis.Calculus.ContDiff.Comp

--- Mathlib/Analysis/Calculus/ContDiff/Comp.lean
+++ Mathlib/Analysis/Calculus/ContDiff/Comp.lean
@@ -39,11 +39,12 @@ open Set Filter Function
 
 open scoped Topology ContDiff
 
-attribute [local instance 1001] NormedAddCommGroup.toAddCommGroup AddCommGroup.toAddCommMonoid
+attribute [local instance 1001] AddCommGroup.toAddCommMonoid
 
-variable {𝕜 E F G : Type*} [NontriviallyNormedField 𝕜] [NormedAddCommGroup E] [NormedSpace 𝕜 E]
-  [NormedAddCommGroup F] [NormedSpace 𝕜 F] [NormedAddCommGroup G] [NormedSpace 𝕜 G]
-  {X : Type*} [NormedAddCommGroup X] [NormedSpace 𝕜 X] {s t : Set E} {f : E → F}
+variable {𝕜 E F G : Type*} [NontriviallyNormedField 𝕜] [AddCommGroup E] [NormedAddCommGroup E]
+  [NormedSpace 𝕜 E] [AddCommGroup F] [NormedAddCommGroup F] [NormedSpace 𝕜 F] [AddCommGroup G]
+  [NormedAddCommGroup G] [NormedSpace 𝕜 G]
+  {X : Type*} [AddCommGroup X] [NormedAddCommGroup X] [NormedSpace 𝕜 X] {s t : Set E} {f : E → F}
   {g : F → G} {x x₀ : E} {m n : ℕ∞ω}
 
 section comp
@@ -419,7 +420,8 @@ theorem contDiff_prod_iff (f : E → F × G) :
 section NAry
 
 variable {E₁ E₂ E₃ : Type*}
-variable [NormedAddCommGroup E₁] [NormedAddCommGroup E₂] [NormedAddCommGroup E₃]
+variable [AddCommGroup E₁] [NormedAddCommGroup E₁] [AddCommGroup E₂] [NormedAddCommGroup E₂]
+  [AddCommGroup E₃] [NormedAddCommGroup E₃]
   [NormedSpace 𝕜 E₁] [NormedSpace 𝕜 E₂] [NormedSpace 𝕜 E₃]
 
 theorem ContDiff.comp₂ {g : E₁ × E₂ → G} {f₁ : F → E₁} {f₂ : F → E₂} (hg : ContDiff 𝕜 n g)
