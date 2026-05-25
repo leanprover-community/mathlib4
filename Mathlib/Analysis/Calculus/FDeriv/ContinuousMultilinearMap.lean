98cab37393fda16065ac8bd5e27f2a543fecbd4c
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2026-05-25 14:59:53 +0200

    fix Analysis.Calculus.FDeriv.ContinuousMultilinearMap

--- Mathlib/Analysis/Calculus/FDeriv/ContinuousMultilinearMap.lean
+++ Mathlib/Analysis/Calculus/FDeriv/ContinuousMultilinearMap.lean
@@ -42,10 +42,10 @@ public section
 
 variable {𝕜 ι E : Type*} {F G : ι → Type*} {H : Type*}
   [NontriviallyNormedField 𝕜]
-  [NormedAddCommGroup E] [NormedSpace 𝕜 E]
-  [∀ i, NormedAddCommGroup (F i)] [∀ i, NormedSpace 𝕜 (F i)]
-  [∀ i, NormedAddCommGroup (G i)] [∀ i, NormedSpace 𝕜 (G i)]
-  [NormedAddCommGroup H] [NormedSpace 𝕜 H]
+  [AddCommGroup E] [NormedAddCommGroup E] [NormedSpace 𝕜 E]
+  [∀ i, AddCommGroup (F i)] [∀ i, NormedAddCommGroup (F i)] [∀ i, NormedSpace 𝕜 (F i)]
+  [∀ i, AddCommGroup (G i)] [∀ i, NormedAddCommGroup (G i)] [∀ i, NormedSpace 𝕜 (G i)]
+  [AddCommGroup H] [NormedAddCommGroup H] [NormedSpace 𝕜 H]
   {f : E → ContinuousMultilinearMap 𝕜 G H} {f' : E →L[𝕜] ContinuousMultilinearMap 𝕜 G H}
   {g : ∀ i, E → F i →L[𝕜] G i} {g' : ∀ i, E →L[𝕜] F i →L[𝕜] G i}
   {s : Set E} {x : E}
