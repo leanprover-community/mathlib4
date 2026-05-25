9dc9863cfb11d8fb8043bdaf70a51086e90d4441
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2026-05-25 13:23:51 +0200

    fix Analysis.Calculus.Implicit

--- Mathlib/Analysis/Calculus/Implicit.lean
+++ Mathlib/Analysis/Calculus/Implicit.lean
@@ -97,9 +97,9 @@ needs to have a complete control over the choice of the implicit function.
 * the derivatives are surjective;
 * the kernels of the derivatives are complementary subspaces of `E`. -/
 structure ImplicitFunctionData (𝕜 : Type*) [NontriviallyNormedField 𝕜] (E : Type*)
-    [NormedAddCommGroup E] [NormedSpace 𝕜 E] [CompleteSpace E] (F : Type*) [NormedAddCommGroup F]
-    [NormedSpace 𝕜 F] [CompleteSpace F] (G : Type*) [NormedAddCommGroup G] [NormedSpace 𝕜 G]
-    [CompleteSpace G] where
+    [AddCommGroup E] [NormedAddCommGroup E] [NormedSpace 𝕜 E] [CompleteSpace E] (F : Type*)
+    [AddCommGroup F] [NormedAddCommGroup F] [NormedSpace 𝕜 F] [CompleteSpace F] (G : Type*)
+    [AddCommGroup G] [NormedAddCommGroup G] [NormedSpace 𝕜 G] [CompleteSpace G] where
   /-- Left function -/
   leftFun : E → F
   /-- Derivative of the left function -/
@@ -118,10 +118,10 @@ structure ImplicitFunctionData (𝕜 : Type*) [NontriviallyNormedField 𝕜] (E 
 
 namespace ImplicitFunctionData
 
-variable {𝕜 : Type*} [NontriviallyNormedField 𝕜] {E : Type*} [NormedAddCommGroup E]
-  [NormedSpace 𝕜 E] [CompleteSpace E] {F : Type*} [NormedAddCommGroup F] [NormedSpace 𝕜 F]
-  [CompleteSpace F] {G : Type*} [NormedAddCommGroup G] [NormedSpace 𝕜 G] [CompleteSpace G]
-  (φ : ImplicitFunctionData 𝕜 E F G)
+variable {𝕜 : Type*} [NontriviallyNormedField 𝕜] {E : Type*} [AddCommGroup E] [NormedAddCommGroup E]
+  [NormedSpace 𝕜 E] [CompleteSpace E] {F : Type*} [AddCommGroup F] [NormedAddCommGroup F]
+  [NormedSpace 𝕜 F] [CompleteSpace F] {G : Type*} [AddCommGroup G] [NormedAddCommGroup G]
+  [NormedSpace 𝕜 G] [CompleteSpace G] (φ : ImplicitFunctionData 𝕜 E F G)
 
 /-- The function given by `x ↦ (leftFun x, rightFun x)`. -/
 def prodFun (x : E) : F × G :=
@@ -317,9 +317,9 @@ Note that a map with these properties is not unique. E.g., different choices of 
 complementary to `ker f'` lead to different maps `φ`.
 -/
 
-variable {𝕜 : Type*} [NontriviallyNormedField 𝕜] {E : Type*} [NormedAddCommGroup E]
-  [NormedSpace 𝕜 E] [CompleteSpace E] {F : Type*} [NormedAddCommGroup F] [NormedSpace 𝕜 F]
-  [CompleteSpace F] {f : E → F} {f' : E →L[𝕜] F} {a : E}
+variable {𝕜 : Type*} [NontriviallyNormedField 𝕜] {E : Type*} [AddCommGroup E] [NormedAddCommGroup E]
+  [NormedSpace 𝕜 E] [CompleteSpace E] {F : Type*} [AddCommGroup F] [NormedAddCommGroup F]
+  [NormedSpace 𝕜 F] [CompleteSpace F] {f : E → F} {f' : E →L[𝕜] F} {a : E}
 
 section Defs
 
@@ -458,9 +458,10 @@ complementary to `ker f'` lead to different maps `φ`.
 
 section FiniteDimensional
 
-variable {𝕜 : Type*} [NontriviallyNormedField 𝕜] [CompleteSpace 𝕜] {E : Type*}
-  [NormedAddCommGroup E] [NormedSpace 𝕜 E] [CompleteSpace E] {F : Type*} [NormedAddCommGroup F]
-  [NormedSpace 𝕜 F] [FiniteDimensional 𝕜 F] (f : E → F) (f' : E →L[𝕜] F) {a : E}
+variable {𝕜 : Type*} [NontriviallyNormedField 𝕜] [CompleteSpace 𝕜] {E : Type*} [AddCommGroup E]
+  [NormedAddCommGroup E] [NormedSpace 𝕜 E] [CompleteSpace E] {F : Type*} [AddCommGroup F]
+  [NormedAddCommGroup F] [NormedSpace 𝕜 F] [FiniteDimensional 𝕜 F] (f : E → F)
+  (f' : E →L[𝕜] F) {a : E}
 
 /-- Given a map `f : E → F` to a finite-dimensional space with a surjective derivative `f'`,
 returns an open partial homeomorphism between `E` and `F × ker f'`. -/
