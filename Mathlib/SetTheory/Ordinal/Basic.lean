2688d88ecb9e181151ac21b7d7f7dab5cb464d89
vihdzp <vi.hdz.p@gmail.com>
2026-05-24 19:30:28 -0600

    3

--- Mathlib/SetTheory/Ordinal/Basic.lean
+++ Mathlib/SetTheory/Ordinal/Basic.lean
@@ -146,7 +146,7 @@ def type (r : α → α → Prop) [wo : IsWellOrder α r] : Ordinal :=
   ⟦⟨α, r, wo⟩⟧
 
 /-- `typeLT α` is an abbreviation for the order type of the `<` relation of `α`. -/
-scoped notation "typeLT " α:70 => @Ordinal.type α (· < ·) inferInstance
+scoped notation3 "typeLT " α:70 => @Ordinal.type α (· < ·) inferInstance
 
 instance zero : Zero Ordinal :=
   ⟨type <| @emptyRelation PEmpty⟩
