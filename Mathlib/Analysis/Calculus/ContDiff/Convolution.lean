24d517cb94110a5f3986b1d3d3c4518c26f91262
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2026-05-25 22:30:43 +0200

    fix Analysis.Calculus.ContDiff.Convolution

--- Mathlib/Analysis/Calculus/ContDiff/Convolution.lean
+++ Mathlib/Analysis/Calculus/ContDiff/Convolution.lean
@@ -36,7 +36,8 @@ universe u𝕜 uG uE uE' uE'' uF uF' uF'' uP
 variable {𝕜 : Type u𝕜} {G : Type uG} {E : Type uE} {E' : Type uE'} {E'' : Type uE''} {F : Type uF}
   {F' : Type uF'} {F'' : Type uF''} {P : Type uP}
 
-variable [NormedAddCommGroup E] [NormedAddCommGroup E'] [NormedAddCommGroup E'']
+variable [AddCommGroup E] [NormedAddCommGroup E] [AddCommGroup E'] [NormedAddCommGroup E']
+  [AddCommGroup E''] [NormedAddCommGroup E''] [AddCommGroup F]
   [NormedAddCommGroup F] {f f' : G → E} {g g' : G → E'} {x x' : G} {y y' : E}
 
 namespace MeasureTheory
@@ -53,7 +54,7 @@ variable {n : ℕ∞}
 variable [MeasurableSpace G] {μ ν : Measure G}
 variable (L : E →L[𝕜] E' →L[𝕜] F)
 
-variable [NormedAddCommGroup G] [BorelSpace G]
+variable [AddCommGroup G] [NormedAddCommGroup G] [BorelSpace G]
 
 variable [NormedSpace 𝕜 G] [SFinite μ] [IsAddLeftInvariant μ]
 
@@ -130,8 +131,8 @@ end Real
 section WithParam
 
 variable [RCLike 𝕜] [NormedSpace 𝕜 E] [NormedSpace 𝕜 E'] [NormedSpace 𝕜 E''] [NormedSpace ℝ F]
-  [NormedSpace 𝕜 F] [MeasurableSpace G] [NormedAddCommGroup G] [BorelSpace G]
-  [NormedSpace 𝕜 G] [NormedAddCommGroup P] [NormedSpace 𝕜 P] {μ : Measure G}
+  [NormedSpace 𝕜 F] [MeasurableSpace G] [AddCommGroup G] [NormedAddCommGroup G] [BorelSpace G]
+  [NormedSpace 𝕜 G] [AddCommGroup P] [NormedAddCommGroup P] [NormedSpace 𝕜 P] {μ : Measure G}
   (L : E →L[𝕜] E' →L[𝕜] F)
 
 /-- The derivative of the convolution `f * g` is given by `f * Dg`, when `f` is locally integrable
@@ -279,10 +280,11 @@ parameter space `P` (and the compact support `k` is independent of the parameter
 In this version, all the types belong to the same universe (to get an induction working in the
 proof). Use instead `contDiffOn_convolution_right_with_param`, which removes this restriction. -/
 theorem contDiffOn_convolution_right_with_param_aux {G : Type uP} {E' : Type uP} {F : Type uP}
-    {P : Type uP} [NormedAddCommGroup E'] [NormedAddCommGroup F] [NormedSpace 𝕜 E']
-    [NormedSpace ℝ F] [NormedSpace 𝕜 F] [MeasurableSpace G]
-    {μ : Measure G}
-    [NormedAddCommGroup G] [BorelSpace G] [NormedSpace 𝕜 G] [NormedAddCommGroup P] [NormedSpace 𝕜 P]
+    {P : Type uP} [AddCommGroup E'] [NormedAddCommGroup E'] [AddCommGroup F] [NormedAddCommGroup F]
+    [NormedSpace 𝕜 E'] [NormedSpace ℝ F] [NormedSpace 𝕜 F] [MeasurableSpace G]
+    {μ : Measure G} [AddCommGroup G]
+    [NormedAddCommGroup G] [BorelSpace G] [NormedSpace 𝕜 G] [AddCommGroup P] [NormedAddCommGroup P]
+    [NormedSpace 𝕜 P]
     {f : G → E} {n : ℕ∞} (L : E →L[𝕜] E' →L[𝕜] F) {g : P → G → E'} {s : Set P} {k : Set G}
     (hs : IsOpen s) (hk : IsCompact k) (hgs : ∀ p, ∀ x, p ∈ s → x ∉ k → g p x = 0)
     (hf : LocallyIntegrable f μ) (hg : ContDiffOn 𝕜 n ↿g (s ×ˢ univ)) :
