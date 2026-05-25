8c5fd1f6d5cc6e971da717b1733490131876e31a
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2026-05-25 21:41:48 +0200

    fix Geometry.Euclidean.Similarity

--- Mathlib/Geometry/Euclidean/Similarity.lean
+++ Mathlib/Geometry/Euclidean/Similarity.lean
@@ -26,7 +26,7 @@ open Similar NNReal Affine
 namespace EuclideanGeometry
 
 variable {ι V₁ V₂ P₁ P₂ : Type*}
-  [NormedAddCommGroup V₁] [NormedAddCommGroup V₂]
+  [AddCommGroup V₁] [NormedAddCommGroup V₁] [AddCommGroup V₂] [NormedAddCommGroup V₂]
   [InnerProductSpace ℝ V₁] [InnerProductSpace ℝ V₂]
   [MetricSpace P₁] [MetricSpace P₂]
   [NormedAddTorsor V₁ P₁] [NormedAddTorsor V₂ P₂]
