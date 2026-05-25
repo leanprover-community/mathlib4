6169d7f26927a56db35fb2a41630c0f6b49cfcfd
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2026-05-25 17:58:47 +0200

    fix Geometry.Manifold.ContMDiff.Defs

--- Mathlib/Geometry/Manifold/ContMDiff/Defs.lean
+++ Mathlib/Geometry/Manifold/ContMDiff/Defs.lean
@@ -57,15 +57,15 @@ open scoped Topology Manifold ContDiff
 
 variable {𝕜 : Type*} [NontriviallyNormedField 𝕜]
   -- Prerequisite typeclasses to say that `M` is a manifold over the pair `(E, H)`
-  {E : Type*}
+  {E : Type*} [AddCommGroup E]
   [NormedAddCommGroup E] [NormedSpace 𝕜 E] {H : Type*} [TopologicalSpace H]
   {I : ModelWithCorners 𝕜 E H} {M : Type*} [TopologicalSpace M] [ChartedSpace H M]
   -- Prerequisite typeclasses to say that `M'` is a manifold over the pair `(E', H')`
-  {E' : Type*}
+  {E' : Type*} [AddCommGroup E']
   [NormedAddCommGroup E'] [NormedSpace 𝕜 E'] {H' : Type*} [TopologicalSpace H']
   {I' : ModelWithCorners 𝕜 E' H'} {M' : Type*} [TopologicalSpace M'] [ChartedSpace H' M']
   -- Prerequisite typeclasses to say that `M''` is a manifold over the pair `(E'', H'')`
-  {E'' : Type*}
+  {E'' : Type*} [AddCommGroup E'']
   [NormedAddCommGroup E''] [NormedSpace 𝕜 E''] {H'' : Type*} [TopologicalSpace H'']
   {I'' : ModelWithCorners 𝕜 E'' H''} {M'' : Type*} [TopologicalSpace M''] [ChartedSpace H'' M'']
   -- declare functions, sets, points and smoothness indices
