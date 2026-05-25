b03b67c22c60e5fe990e6b240e1ed533fa51e60e
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2026-05-25 15:19:02 +0200

    fix Analysis.Calculus.ContDiff.Basic

--- Mathlib/Analysis/Calculus/ContDiff/Basic.lean
+++ Mathlib/Analysis/Calculus/ContDiff/Basic.lean
@@ -25,10 +25,11 @@ open Set Fin Filter Function
 
 open scoped Topology ContDiff
 
-attribute [local instance 1001] NormedAddCommGroup.toAddCommGroup AddCommGroup.toAddCommMonoid
+attribute [local instance 1001] AddCommGroup.toAddCommMonoid
 
-variable {𝕜 E F G : Type*} [NontriviallyNormedField 𝕜] [NormedAddCommGroup E] [NormedSpace 𝕜 E]
-  [NormedAddCommGroup F] [NormedSpace 𝕜 F] [NormedAddCommGroup G] [NormedSpace 𝕜 G]
+variable {𝕜 E F G : Type*} [NontriviallyNormedField 𝕜] [AddCommGroup E] [NormedAddCommGroup E]
+  [NormedSpace 𝕜 E] [AddCommGroup F] [NormedAddCommGroup F] [NormedSpace 𝕜 F] [AddCommGroup G]
+  [NormedAddCommGroup G] [NormedSpace 𝕜 G]
   {s t : Set E} {f : E → F} {x : E} {b : E × F → G} {m n : ℕ∞ω}
   {p : E → FormalMultilinearSeries 𝕜 E F}
 
