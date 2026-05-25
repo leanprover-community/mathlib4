bd32b05fffaac6dd004ce4b1848f140b19c1b6e1
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2026-05-25 21:32:27 +0200

    fix Geometry.Manifold.ContMDiff.NormedSpace

--- Mathlib/Geometry/Manifold/ContMDiff/NormedSpace.lean
+++ Mathlib/Geometry/Manifold/ContMDiff/NormedSpace.lean
@@ -27,16 +27,17 @@ open scoped Topology Manifold
 
 variable {𝕜 : Type*} [NontriviallyNormedField 𝕜]
   -- declare a charted space `M` over the pair `(E, H)`.
-  {E : Type*}
+  {E : Type*} [AddCommGroup E]
   [NormedAddCommGroup E] [NormedSpace 𝕜 E] {H : Type*} [TopologicalSpace H]
   {I : ModelWithCorners 𝕜 E H} {M : Type*} [TopologicalSpace M] [ChartedSpace H M]
   -- declare normed spaces `E'`, `F`, `F'`, `F₁`, `F₂`, `F₃`, `F₄`.
-  {E' : Type*} [NormedAddCommGroup E'] [NormedSpace 𝕜 E']
-  {F : Type*} [NormedAddCommGroup F] [NormedSpace 𝕜 F]
-  {F' : Type*} [NormedAddCommGroup F'] [NormedSpace 𝕜 F']
-  {F₁ : Type*} [NormedAddCommGroup F₁] [NormedSpace 𝕜 F₁] {F₂ : Type*} [NormedAddCommGroup F₂]
-  [NormedSpace 𝕜 F₂] {F₃ : Type*} [NormedAddCommGroup F₃] [NormedSpace 𝕜 F₃] {F₄ : Type*}
-  [NormedAddCommGroup F₄] [NormedSpace 𝕜 F₄]
+  {E' : Type*} [AddCommGroup E'] [NormedAddCommGroup E'] [NormedSpace 𝕜 E']
+  {F : Type*} [AddCommGroup F] [NormedAddCommGroup F] [NormedSpace 𝕜 F]
+  {F' : Type*} [AddCommGroup F'] [NormedAddCommGroup F'] [NormedSpace 𝕜 F']
+  {F₁ : Type*} [AddCommGroup F₁] [NormedAddCommGroup F₁] [NormedSpace 𝕜 F₁]
+  {F₂ : Type*} [AddCommGroup F₂] [NormedAddCommGroup F₂] [NormedSpace 𝕜 F₂]
+  {F₃ : Type*} [AddCommGroup F₃] [NormedAddCommGroup F₃] [NormedSpace 𝕜 F₃]
+  {F₄ : Type*} [AddCommGroup F₄] [NormedAddCommGroup F₄] [NormedSpace 𝕜 F₄]
   -- declare functions, sets, points and smoothness indices
   {s : Set M} {x : M} {n : WithTop ℕ∞}
 
