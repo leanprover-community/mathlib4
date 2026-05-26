e2090f1232f4cb6c6c0119e71404db7b272a6c2f
Johan Commelin <johan@commelin.net>
2026-05-26 11:33:03 +0200

    Apply suggestion from @jcommelin

--- Mathlib/Tactic/StacksAttribute.lean
+++ Mathlib/Tactic/StacksAttribute.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2024 Damiano Testa. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Damiano Testa
+-/
 module
 
 public import Mathlib.Tactic.CrossRefAttribute
