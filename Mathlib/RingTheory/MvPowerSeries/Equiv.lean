045ca38d97eb717fa7c4e4e151f51367dc0e8ea6
WenrongZou <wenrongzou@outlook.com>
2026-05-27 13:27:12 +0200

    comment change

--- Mathlib/RingTheory/MvPowerSeries/Equiv.lean
+++ Mathlib/RingTheory/MvPowerSeries/Equiv.lean
@@ -189,14 +189,14 @@ theorem toMvPowerSeries_injective (i : σ) : Function.Injective (toMvPowerSeries
 
 section CommRing
 
-variable {R : Type*} [CommRing R] {f : PowerSeries R} (i : σ) (r : R) (p : R⟦X⟧)
+variable {R : Type*} [CommRing R] {f : R⟦X⟧} {i : σ}
 
 theorem toMvPowerSeries_eq_subst : f.toMvPowerSeries i = f.subst (MvPowerSeries.X i) := by
   rw [toMvPowerSeries_apply, MvPowerSeries.rename_eq_subst, comp_def, subst]
 
-lemma toMvPowerSeries_coeff_eq_zero {d : σ →₀ ℕ} {s : σ} (hd : d s = 0) (hf : f.constantCoeff = 0) :
-    ((PowerSeries.toMvPowerSeries s) f).coeff d = 0 := by classical
-  have : (.subst (.X (R := R) ∘ fun x ↦ s) f) = f.subst (.X s) := rfl
+lemma toMvPowerSeries_coeff_eq_zero {d : σ →₀ ℕ} (hd : d i = 0) (hf : f.constantCoeff = 0) :
+    ((PowerSeries.toMvPowerSeries i) f).coeff d = 0 := by classical
+  have : (.subst (.X (R := R) ∘ fun x ↦ i) f) = f.subst (.X i) := rfl
   rw [toMvPowerSeries_apply, MvPowerSeries.rename_eq_subst, this, coeff_subst (HasSubst.X _),
     finsum_eq_zero_of_forall_eq_zero]
   simp only [MvPowerSeries.X_pow_eq, MvPowerSeries.coeff_monomial, smul_eq_mul, mul_ite, mul_one,
@@ -212,7 +212,7 @@ theorem _root_.MvPowerSeries.HasSubst.toMvPowerSeries (hf : f.constantCoeff = 0)
     contrapose
     simpa using fun hd ↦ toMvPowerSeries_coeff_eq_zero hd hf
 
-theorem toMvPowerSeries_val {a : σ → MvPowerSeries τ R} (i : σ) (ha : MvPowerSeries.HasSubst a) :
+theorem toMvPowerSeries_val {a : σ → MvPowerSeries τ R} (ha : MvPowerSeries.HasSubst a) :
     (f.toMvPowerSeries i).subst a = f.subst (a i) := by
   rw [toMvPowerSeries_eq_subst, subst, MvPowerSeries.subst_comp_subst_apply
     (HasSubst.const (HasSubst.X _)) ha, MvPowerSeries.subst_X ha, subst]
