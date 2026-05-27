c9dc3bdd25e3d95772fd8ca82933d8250eecadd2
vihdzp <vi.hdz.p@gmail.com>
2026-05-27 13:41:50 -0600

    fix

--- Mathlib/Analysis/Calculus/ContDiff/Bounds.lean
+++ Mathlib/Analysis/Calculus/ContDiff/Bounds.lean
@@ -366,7 +366,7 @@ theorem norm_iteratedFDerivWithin_comp_le_aux {Fu Gu : Type u} [NormedAddCommGro
   have M : (n : ℕ∞ω) < n.succ := Nat.cast_lt.2 n.lt_succ_self
   have Cnonneg : 0 ≤ C := (norm_nonneg _).trans (hC 0 bot_le)
   have Dnonneg : 0 ≤ D := by
-    have : 1 ≤ n + 1 := by simp only [le_add_iff_nonneg_left, zero_le']
+    have : 1 ≤ n + 1 := by simp only [le_add_iff_nonneg_left, zero_le]
     simpa only [pow_one] using (norm_nonneg _).trans (hD 1 le_rfl this)
   -- use the inductive assumption to bound the derivatives of `g' ∘ f`.
   have I : ∀ i ∈ Finset.range (n + 1),
--- Mathlib/RingTheory/Valuation/Basic.lean
+++ Mathlib/RingTheory/Valuation/Basic.lean
@@ -174,7 +174,7 @@ theorem map_sum_le {ι : Type*} {s : Finset ι} {f : ι → R} {g : Γ₀} (hf :
     v (∑ i ∈ s, f i) ≤ g := by
   classical
   refine
-    Finset.induction_on s (fun _ => v.map_zero ▸ zero_le')
+    Finset.induction_on s (fun _ => v.map_zero ▸ zero_le)
       (fun a s has ih hf => ?_) hf
   rw [Finset.forall_mem_insert] at hf; rw [Finset.sum_insert has]
   exact v.map_add_le hf.1 (ih hf.2)
@@ -427,7 +427,7 @@ theorem val_eq_one_iff (v : Valuation K Γ₀) {x : K} : v x = 1 ↔ v x⁻¹ = 
 
 theorem val_le_one_or_val_inv_lt_one (v : Valuation K Γ₀) (x : K) : v x ≤ 1 ∨ v x⁻¹ < 1 := by
   by_cases h : x = 0
-  · simp only [h, map_zero, zero_le', inv_zero, zero_lt_one, or_self]
+  · simp only [h, map_zero, zero_le, inv_zero, zero_lt_one, or_self]
   · simp only [← one_lt_val_iff v h, le_or_gt]
 
 /--
@@ -436,7 +436,7 @@ in `x` and `x⁻¹`.
 -/
 theorem val_le_one_or_val_inv_le_one (v : Valuation K Γ₀) (x : K) : v x ≤ 1 ∨ v x⁻¹ ≤ 1 := by
   by_cases h : x = 0
-  · simp only [h, map_zero, zero_le', inv_zero, or_self]
+  · simp only [h, map_zero, zero_le, inv_zero, or_self]
   · simp only [← one_le_val_iff v h, le_total]
 
 /-- The subgroup of elements whose valuation is less than or equal to a certain value. -/
@@ -693,7 +693,7 @@ variable {B : Type*} {A : Type*} [CommSemiring A] [Ring B] [Algebra A B] (v : Va
 
 @[simp]
 theorem IsTrivialOn.valuation_algebraMap_le_one (a : A) : v (algebraMap A B a) ≤ 1 := by
-  by_cases a = 0 <;> grind [zero_le']
+  by_cases a = 0 <;> grind [zero_le]
 
 end IsTrivialOn
 
