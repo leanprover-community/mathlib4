c24b24545ec08cd9a0cd1fff24ae926efbccb84a
Andrew Z <1497456+z-tech@users.noreply.github.com>
2026-05-27 17:31:14 +0200

    Corradi's intersection lemma

--- Mathlib.lean
+++ Mathlib.lean
@@ -3578,6 +3578,7 @@ public import Mathlib.Combinatorics.Schnirelmann
 public import Mathlib.Combinatorics.SetFamily.AhlswedeZhang
 public import Mathlib.Combinatorics.SetFamily.Compression.Down
 public import Mathlib.Combinatorics.SetFamily.Compression.UV
+public import Mathlib.Combinatorics.SetFamily.Corradi
 public import Mathlib.Combinatorics.SetFamily.FourFunctions
 public import Mathlib.Combinatorics.SetFamily.HarrisKleitman
 public import Mathlib.Combinatorics.SetFamily.Intersecting
