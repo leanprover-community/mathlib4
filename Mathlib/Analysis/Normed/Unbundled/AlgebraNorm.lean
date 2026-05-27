cc8e1dc7cd4d9fb4f7532a27a72a05de26c5398d
yuanyi-350 <kysyy1@126.com>
2026-05-27 08:43:26 +0800

    refactor(Analysis): golf AlgebraNorm

--- Mathlib/Analysis/Normed/Unbundled/AlgebraNorm.lean
+++ Mathlib/Analysis/Normed/Unbundled/AlgebraNorm.lean
@@ -214,12 +214,8 @@ variable {R : Type*} [NonAssocRing R]
 set_option linter.style.whitespace false in -- manual alignment is not recognised
 /-- The ring norm underlying a multiplicative ring norm. -/
 def toRingNorm (f : MulRingNorm R) : RingNorm R where
-  toFun       := f
-  map_zero'   := f.map_zero'
-  add_le'     := f.add_le'
-  neg'        := f.neg'
+  __ := f
   mul_le' x y := le_of_eq (f.map_mul' x y)
-  eq_zero_of_map_eq_zero' := f.eq_zero_of_map_eq_zero'
 
 /-- A multiplicative ring norm is power-multiplicative. -/
 theorem isPowMul {A : Type*} [Ring A] (f : MulRingNorm A) : IsPowMul f := fun x n hn => by
