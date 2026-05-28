be3ad677b64786967bf376eb346b3bd0fe41cd97
Michael Rothgang <rothgang@math.uni-bonn.de>
2026-05-28 12:56:10 +0200

    Oops

--- Mathlib/Analysis/Real/Sqrt.lean
+++ Mathlib/Analysis/Real/Sqrt.lean
@@ -1,4 +1,4 @@
-`/-
+/-
 Copyright (c) 2020 Mario Carneiro. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro, Floris van Doorn, Yury Kudryashov
