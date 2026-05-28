35f530772a3b3b4072d3ff9df34584bcf409a5c4
Kim Morrison <kim@tqft.net>
2026-05-28 22:56:39 +0000

    bad merge

--- Mathlib/NumberTheory/ModularForms/DimensionFormulas/LevelOne.lean
+++ Mathlib/NumberTheory/ModularForms/DimensionFormulas/LevelOne.lean
@@ -6,5 +6,7 @@ Authors: Chris Birkbeck
 module -- shake: keep-all
 
 public import Mathlib.NumberTheory.ModularForms.LevelOne.DimensionFormula
+public import Mathlib.Tactic.Linter.DeprecatedModule
 
-deprecated_module (since := "2026-05-06")
+deprecated_module "Use `Mathlib.NumberTheory.ModularForms.LevelOne.DimensionFormula` instead."
+  (since := "2026-05-06")
