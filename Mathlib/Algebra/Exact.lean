93d026c47d9ba99e106a4839efff5fd58cb5acf1
ADedecker <anatolededecker@gmail.com>
2026-05-29 12:10:09 +0200

    chore(Algebra/Exact): deprecate module

---
+++ Mathlib/Algebra/Exact.lean
@@ -0,0 +1,10 @@
+/-
+Copyright (c) 2023 Antoine Chambert-Loir. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Antoine Chambert-Loir
+-/
+module -- shake: keep-all
+
+public import Mathlib.Algebra.Exact.Basic
+
+deprecated_module (since := "2026-05-29")
