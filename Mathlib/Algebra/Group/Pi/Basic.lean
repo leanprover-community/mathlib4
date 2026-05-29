c27eddb0b6bd0cb09ce17096c99f95c95eed5e01
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2026-05-29 17:16:57 +0100

    wip

--- Mathlib/Algebra/Group/Pi/Basic.lean
+++ Mathlib/Algebra/Group/Pi/Basic.lean
@@ -44,32 +44,33 @@ variable (x y : ∀ i, f i) (i : I)
 namespace Pi
 
 @[to_additive]
-instance isMulCommutative [∀ i, Mul (f i)] [∀ i, IsMulCommutative (f i)] :
+instance (priority := 1100) isMulCommutative [∀ i, Mul (f i)] [∀ i, IsMulCommutative (f i)] :
     IsMulCommutative (∀ i, f i) where
   is_comm.comm _ _ := by ext; apply mul_comm'
 
 @[to_additive]
-instance commMagma [∀ i, CommMagma (f i)] : CommMagma (∀ i, f i) where
+instance (priority := 1100) commMagma [∀ i, CommMagma (f i)] : CommMagma (∀ i, f i) where
   mul_comm _ _ := by ext; apply mul_comm
 
 @[to_additive]
-instance semigroup [∀ i, Semigroup (f i)] : Semigroup (∀ i, f i) where
+instance (priority := 1100) semigroup [∀ i, Semigroup (f i)] : Semigroup (∀ i, f i) where
   mul_assoc := by intros; ext; exact mul_assoc _ _ _
 
 @[to_additive]
-instance commSemigroup [∀ i, CommSemigroup (f i)] : CommSemigroup (∀ i, f i) where
+instance (priority := 1100) commSemigroup [∀ i, CommSemigroup (f i)] :
+    CommSemigroup (∀ i, f i) where
 
 @[to_additive]
-instance mulOneClass [∀ i, MulOneClass (f i)] : MulOneClass (∀ i, f i) where
+instance (priority := 1100) mulOneClass [∀ i, MulOneClass (f i)] : MulOneClass (∀ i, f i) where
   one_mul := by intros; ext; exact one_mul _
   mul_one := by intros; ext; exact mul_one _
 
 @[to_additive]
-instance invOneClass [∀ i, InvOneClass (f i)] : InvOneClass (∀ i, f i) where
+instance (priority := 1100) invOneClass [∀ i, InvOneClass (f i)] : InvOneClass (∀ i, f i) where
   inv_one := by ext; exact inv_one
 
 @[to_additive]
-instance monoid [∀ i, Monoid (f i)] : Monoid (∀ i, f i) where
+instance (priority := 1100) monoid [∀ i, Monoid (f i)] : Monoid (∀ i, f i) where
   __ := semigroup
   __ := mulOneClass
   npow := fun n x i => x i ^ n
@@ -77,10 +78,10 @@ instance monoid [∀ i, Monoid (f i)] : Monoid (∀ i, f i) where
   npow_succ := by intros; ext; exact Monoid.npow_succ _ _
 
 @[to_additive]
-instance commMonoid [∀ i, CommMonoid (f i)] : CommMonoid (∀ i, f i) where
+instance (priority := 1100) commMonoid [∀ i, CommMonoid (f i)] : CommMonoid (∀ i, f i) where
 
 @[to_additive Pi.subNegMonoid]
-instance divInvMonoid [∀ i, DivInvMonoid (f i)] : DivInvMonoid (∀ i, f i) where
+instance (priority := 1100) divInvMonoid [∀ i, DivInvMonoid (f i)] : DivInvMonoid (∀ i, f i) where
   zpow := fun z x i => x i ^ z
   div_eq_mul_inv := by intros; ext; exact div_eq_mul_inv _ _
   zpow_zero' := by intros; ext; exact DivInvMonoid.zpow_zero' _
@@ -88,64 +89,74 @@ instance divInvMonoid [∀ i, DivInvMonoid (f i)] : DivInvMonoid (∀ i, f i) wh
   zpow_neg' := by intros; ext; exact DivInvMonoid.zpow_neg' _ _
 
 @[to_additive]
-instance divInvOneMonoid [∀ i, DivInvOneMonoid (f i)] : DivInvOneMonoid (∀ i, f i) where
+instance (priority := 1100) divInvOneMonoid [∀ i, DivInvOneMonoid (f i)] :
+    DivInvOneMonoid (∀ i, f i) where
   inv_one := by ext; exact inv_one
 
 @[to_additive]
-instance involutiveInv [∀ i, InvolutiveInv (f i)] : InvolutiveInv (∀ i, f i) where
+instance (priority := 1100) involutiveInv [∀ i, InvolutiveInv (f i)] :
+    InvolutiveInv (∀ i, f i) where
   inv_inv := by intros; ext; exact inv_inv _
 
 @[to_additive]
-instance divisionMonoid [∀ i, DivisionMonoid (f i)] : DivisionMonoid (∀ i, f i) where
+instance (priority := 1100) divisionMonoid [∀ i, DivisionMonoid (f i)] :
+    DivisionMonoid (∀ i, f i) where
   __ := divInvMonoid
   __ := involutiveInv
   mul_inv_rev := by intros; ext; exact mul_inv_rev _ _
   inv_eq_of_mul := by intro _ _ h; ext; exact DivisionMonoid.inv_eq_of_mul _ _ (congrFun h _)
 
 @[to_additive instSubtractionCommMonoid]
-instance divisionCommMonoid [∀ i, DivisionCommMonoid (f i)] : DivisionCommMonoid (∀ i, f i) :=
+instance (priority := 1100) divisionCommMonoid [∀ i, DivisionCommMonoid (f i)] :
+    DivisionCommMonoid (∀ i, f i) :=
   { divisionMonoid, commSemigroup with }
 
 @[to_additive]
-instance group [∀ i, Group (f i)] : Group (∀ i, f i) where
+instance (priority := 1100) group [∀ i, Group (f i)] : Group (∀ i, f i) where
   inv_mul_cancel := by intros; ext; exact inv_mul_cancel _
 
 @[to_additive]
-instance commGroup [∀ i, CommGroup (f i)] : CommGroup (∀ i, f i) := { group, commMonoid with }
+instance (priority := 1100) commGroup [∀ i, CommGroup (f i)] : CommGroup (∀ i, f i) :=
+  { group, commMonoid with }
 
-@[to_additive] instance instIsLeftCancelMul [∀ i, Mul (f i)] [∀ i, IsLeftCancelMul (f i)] :
-    IsLeftCancelMul (∀ i, f i) where
+@[to_additive] instance (priority := 1100) instIsLeftCancelMul [∀ i, Mul (f i)]
+    [∀ i, IsLeftCancelMul (f i)] : IsLeftCancelMul (∀ i, f i) where
   mul_left_cancel _ _ _ h := funext fun _ ↦ mul_left_cancel (congr_fun h _)
 
-@[to_additive] instance instIsRightCancelMul [∀ i, Mul (f i)] [∀ i, IsRightCancelMul (f i)] :
-    IsRightCancelMul (∀ i, f i) where
+@[to_additive] instance (priority := 1100) instIsRightCancelMul [∀ i, Mul (f i)]
+    [∀ i, IsRightCancelMul (f i)] : IsRightCancelMul (∀ i, f i) where
   mul_right_cancel _ _ _ h := funext fun _ ↦ mul_right_cancel (congr_fun h _)
 
-@[to_additive] instance instIsCancelMul [∀ i, Mul (f i)] [∀ i, IsCancelMul (f i)] :
-    IsCancelMul (∀ i, f i) where
+@[to_additive] instance (priority := 1100) instIsCancelMul [∀ i, Mul (f i)]
+    [∀ i, IsCancelMul (f i)] : IsCancelMul (∀ i, f i) where
 
 @[to_additive]
-instance leftCancelSemigroup [∀ i, LeftCancelSemigroup (f i)] : LeftCancelSemigroup (∀ i, f i) :=
+instance (priority := 1100) leftCancelSemigroup [∀ i, LeftCancelSemigroup (f i)] :
+    LeftCancelSemigroup (∀ i, f i) :=
   { semigroup with mul_left_cancel := fun _ _ _ => mul_left_cancel }
 
 @[to_additive]
-instance rightCancelSemigroup [∀ i, RightCancelSemigroup (f i)] : RightCancelSemigroup (∀ i, f i) :=
+instance (priority := 1100) rightCancelSemigroup [∀ i, RightCancelSemigroup (f i)] :
+    RightCancelSemigroup (∀ i, f i) :=
   { semigroup with mul_right_cancel := fun _ _ _ => mul_right_cancel }
 
 @[to_additive]
-instance leftCancelMonoid [∀ i, LeftCancelMonoid (f i)] : LeftCancelMonoid (∀ i, f i) :=
+instance (priority := 1100) leftCancelMonoid [∀ i, LeftCancelMonoid (f i)] :
+    LeftCancelMonoid (∀ i, f i) :=
   { leftCancelSemigroup, monoid with }
 
 @[to_additive]
-instance rightCancelMonoid [∀ i, RightCancelMonoid (f i)] : RightCancelMonoid (∀ i, f i) :=
+instance (priority := 1100) rightCancelMonoid [∀ i, RightCancelMonoid (f i)] :
+    RightCancelMonoid (∀ i, f i) :=
   { rightCancelSemigroup, monoid with }
 
 @[to_additive]
-instance cancelMonoid [∀ i, CancelMonoid (f i)] : CancelMonoid (∀ i, f i) :=
+instance (priority := 1100) cancelMonoid [∀ i, CancelMonoid (f i)] : CancelMonoid (∀ i, f i) :=
   { leftCancelMonoid, rightCancelMonoid with }
 
 @[to_additive]
-instance cancelCommMonoid [∀ i, CancelCommMonoid (f i)] : CancelCommMonoid (∀ i, f i) :=
+instance (priority := 1100) cancelCommMonoid [∀ i, CancelCommMonoid (f i)] :
+    CancelCommMonoid (∀ i, f i) :=
   { leftCancelMonoid, commMonoid with }
 
 end Pi
