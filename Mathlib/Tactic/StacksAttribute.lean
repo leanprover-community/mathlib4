253a703c2ad7dee8e1a7e408c21fcd06c8734723
Johan Commelin <johan@commelin.net>
2026-05-29 10:08:50 +0200

    missing import?

--- Mathlib/Tactic/StacksAttribute.lean
+++ Mathlib/Tactic/StacksAttribute.lean
@@ -1,10 +1,6 @@
-/-
-Copyright (c) 2024 Damiano Testa. All rights reserved.
-Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Damiano Testa
--/
-module
+module -- shake: keep-all
 
 public import Mathlib.Tactic.CrossRefAttribute
+public import Mathlib.Tactic.Linter.DeprecatedModule
 
 deprecated_module (since := "2026-05-11")
