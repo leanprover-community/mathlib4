0a4861d5862a6155b72b91213c490af822379dbf
AlexBrodbelt <apbrodbelt@gmail.com>
2026-05-29 14:13:26 +0200

    implemented improvements using new changes to group

--- Mathlib/Tactic/Group.lean
+++ Mathlib/Tactic/Group.lean
@@ -67,13 +67,153 @@ example {G : Type} [Group G] (a b c d : G) (h : c = (a*b^2)*((b*b)⁻¹*a⁻¹)*
   group      -- which then normalized and closed
 ```
 -/
+/-
+/-
+Copyright (c) 2020 Patrick Massot. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Thomas Browning, Patrick Massot
+-/
+module
+
+public import Mathlib.Algebra.Group.Commutator  -- shake: keep (tactic dependency)
+public import Mathlib.Algebra.Order.Sub.Basic  -- shake: keep (tactic dependency)
+public meta import Mathlib.Tactic.FailIfNoProgress
+public import Mathlib.Tactic.Ring
+
+/-!
+# `group` tactic
+
+Normalizes expressions in the language of groups. The basic idea is to use the simplifier
+to put everything into a product of group powers (`zpow` which takes a group element and an
+integer), then simplify the exponents using the `ring` tactic. The process needs to be repeated
+since `ring` can normalize an exponent to zero, leading to a factor that can be removed
+before collecting exponents again. The simplifier step also uses some extra lemmas to avoid
+some `ring` invocations.
+
+## Tags
+
+group theory
+-/
+
+public meta section
+
+namespace Mathlib.Tactic.Group
+
+open Lean
+open Lean.Meta
+open Lean.Parser.Tactic
+open Lean.Elab.Tactic
+
+-- The next three lemmas are not general purpose lemmas, they are intended for use only by
+-- the `group` tactic.
+@[to_additive]
+theorem zpow_trick {G : Type*} [Group G] (a b : G) (n m : ℤ) :
+    a * b ^ n * b ^ m = a * b ^ (n + m) := by rw [mul_assoc, ← zpow_add]
+
+@[to_additive]
+theorem zpow_trick_one {G : Type*} [Group G] (a b : G) (m : ℤ) :
+    a * b * b ^ m = a * b ^ (m + 1) := by rw [mul_assoc, mul_self_zpow]
+
+@[to_additive]
+theorem zpow_trick_one' {G : Type*} [Group G] (a b : G) (n : ℤ) :
+    a * b ^ n * b = a * b ^ (n + 1) := by rw [mul_assoc, mul_zpow_self]
+
+/-- Auxiliary tactic for the `group` tactic. Calls the simplifier only. -/
+syntax (name := aux_group₁) "aux_group₁" (location)? : tactic
+
+macro_rules
+| `(tactic| aux_group₁ $[at $location]?) =>
+  `(tactic| simp -decide -failIfUnchanged only
+    [commutatorElement_def, mul_one, one_mul,
+      ← zpow_neg_one, ← zpow_natCast, ← zpow_mul,
+      Int.natCast_add, Int.natCast_mul,
+      Int.mul_neg, Int.neg_mul, neg_neg,
+      one_zpow, zpow_zero, zpow_one, mul_zpow_neg_one,
+      ↓← mul_assoc,
+      ← zpow_add, ← zpow_add_one, ← zpow_one_add, zpow_trick, zpow_trick_one, zpow_trick_one',
+      tsub_self, sub_self, add_neg_cancel, neg_add_cancel]
+  $[at $location]?)
+
+/-- Auxiliary tactic for the `group` tactic. Calls `ring_nf` to normalize exponents. -/
+syntax (name := aux_group₂) "aux_group₂" (location)? : tactic
+
+macro_rules
+| `(tactic| aux_group₂ $[at $location]?) =>
+  `(tactic| ring_nf -failIfUnchanged $[at $location]?)
+
+/-- Auxiliary tactic for the `group` tactic. Applies cancellation theorem on the *right* -/
+syntax (name := aux_group₃) "aux_group₃" (location)? : tactic
+
+macro_rules
+| `(tactic| aux_group₃ $[at $location]?) =>
+  `(tactic| simp -decide -failIfUnchanged only [mul_left_inj] $[at $location]?)
+
+/-- Auxiliary tactic for the `group` tactic. Applies cancellation theorem on the *left* -/
+syntax (name := aux_group₄) "aux_group₄" (location)? : tactic
+
+macro_rules
+| `(tactic| aux_group₄ $[at $location]?) =>
+  `(tactic| simp -decide -failIfUnchanged only [↓mul_assoc, mul_right_inj] $[at $location]?)
+
+/-- Auxiliary tactic for the `group` tactic. Applies post-processing converting (·)^(-1) to (·)⁻¹
+and associates the expression to the left -/
+syntax (name := aux_group₅) "aux_group₅" (location)? : tactic
+
+macro_rules
+| `(tactic| aux_group₅ $[at $location]?) =>
+  `(tactic| simp -decide -failIfUnchanged only [zpow_neg _ 1, zpow_one, ↓← mul_assoc]
+  $[at $location]?)
+
+/-- `group` normalizes expressions in multiplicative groups that occur in the goal. `group` does not
+assume commutativity, instead using only the group axioms without any information about which group
+is manipulated. If the goal is an equality, and after normalization the two sides are equal, `group`
+closes the goal.
+
+For additive commutative groups, use the `abel` tactic instead.
+
+* `group at l1 l2 ...` normalizes at the given locations.
+
+Example:
+```lean
+example {G : Type} [Group G] (a b c d : G) (h : c = (a*b^2)*((b*b)⁻¹*a⁻¹)*d) : a*c*d⁻¹ = a := by
+  group at h -- normalizes `h` which becomes `h : c = d`
+  rw [h]     -- the goal is now `a*d*d⁻¹ = a`
+  group      -- which then normalized and closed
+```
+-/
 syntax (name := group) "group" (location)? : tactic
 
 macro_rules
 | `(tactic| group $[$loc]?) =>
-  `(tactic| first
-    | repeat1 fail_if_no_progress
-        (simp -decide -failIfUnchanged only
+  `(tactic|
+    -- Call the simplifier and simplify exponents
+    repeat
+      (fail_if_no_progress
+      (aux_group₁ $[$loc]? <;> aux_group₂ $[$loc]?));
+    -- Once the expression has been simplified, apply left/right cancellation
+    <;> repeat
+      (fail_if_no_progress
+      (aux_group₃ $[$loc]? <;> aux_group₄ $[$loc]?)
+      );
+    -- Apply post-processing converting terms of the form (·)^(-1) to terms of the form (·)⁻¹
+    <;> aux_group₅ $[$loc]?
+)
+
+end Mathlib.Tactic.Group
+
+/-!
+We register `group` with the `hint` tactic.
+-/
+
+register_hint 900 group
+
+-/
+syntax (name := group) "group" (location)? : tactic
+
+macro_rules
+| `(tactic| group $[$loc]?) => do
+  let baseSimpTac ← `(tactic |
+    simp -decide -failIfUnchanged only
           [commutatorElement_def, mul_one, one_mul,
             ← zpow_neg_one, ← zpow_natCast, ← zpow_mul,
             Int.natCast_add, Int.natCast_mul,
@@ -83,8 +223,27 @@ macro_rules
             ← zpow_add, ← zpow_add_one, ← zpow_one_add,
             _zpow_trick, _zpow_trick_one, _zpow_trick_one',
             tsub_self, sub_self, add_neg_cancel, neg_add_cancel]
-          $[$loc]?
-        <;> ring_nf (ifUnchanged := .silent) $[$loc]?)
+          $[$loc]?)
+  -- call the `ring_nf` tactic to simplify exponents
+  let simpExpTac ← `(tactic |
+    ring_nf (ifUnchanged := .silent) $[$loc]?)
+  -- tactic block for applying cancellation on the *right*
+  let rightCancelTac ← `(tactic |
+    simp -decide -failIfUnchanged only [mul_left_inj] $[$loc]?)
+  -- tactic block for applying cancellation on the *left*
+  let leftCancelTac ← `(tactic |
+    simp -decide -failIfUnchanged only [↓mul_assoc, mul_right_inj] $[$loc]?)
+  -- post-processing tactic for cleaning up expressions of the form ( · )^(-1) to ( · )⁻¹
+  let cleanUpTac ← `(tactic |
+    simp -decide -failIfUnchanged only [zpow_neg _ 1, zpow_one, ↓← mul_assoc] $[$loc]?)
+  -- normalise the expression, apply left and right cancellation and then clean up the expression
+  -- after the simplifying the expression
+  `(tactic| first
+    | fail_if_no_progress (
+      repeat (fail_if_no_progress ($baseSimpTac <;> $simpExpTac));
+      <;> repeat (fail_if_no_progress ($rightCancelTac <;> $leftCancelTac));
+      <;> $cleanUpTac
+    )
     | fail "`group` made no progress")
 
 end Mathlib.Tactic.Group
