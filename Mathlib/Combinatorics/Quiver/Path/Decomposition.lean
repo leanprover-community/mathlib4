282e6f8992287048ecf23e4b97f54c9c4fba3641
Matteo Cipollina <matteo.cipollina@yahoo.it>
2026-05-29 17:53:34 +0200

    fix date

--- Mathlib/Combinatorics/Quiver/Path/Decomposition.lean
+++ Mathlib/Combinatorics/Quiver/Path/Decomposition.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2025 Matteo Cipollina. All rights reserved.
+Copyright (c) 2026 Matteo Cipollina. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Matteo Cipollina
 -/
