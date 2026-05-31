08a42afe87a59263621028e801a55fdb1e5524de
Hagb (Junyu Guo 郭俊余) <hagb@hagb.name>
2026-05-31 11:11:31 +0800

    Update copyright header

--- Mathlib/Algebra/MvPolynomial/SupDegree.lean
+++ Mathlib/Algebra/MvPolynomial/SupDegree.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2026 Johannes Hölzl, Junyu Guo. All rights reserved.
+Copyright (c) 2026 Damiano Testa, Junyu Guo. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Johannes Hölzl, Johan Commelin, Mario Carneiro, Junyu Guo
+Authors: Damiano Testa, Junyu Guo
 -/
 module
 
