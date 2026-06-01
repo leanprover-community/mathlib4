1e14d1eb40b40020b604e8366140072b361c6631
Yaël Dillies <yael.dillies@gmail.com>
2026-06-01 09:15:20 +0200

    lint

--- Mathlib/RingTheory/Coalgebra/Basic.lean
+++ Mathlib/RingTheory/Coalgebra/Basic.lean
@@ -89,8 +89,9 @@ namespace Coalgebra
 variable {R : Type u} {A : Type v} {ι : Type*} {κ Λ : ι → Type*}
 variable [CommSemiring R] [AddCommMonoid A] [Module R A] [Coalgebra R A] {a : A}
 
-@[deprecated "The indexing type is now unbundled" (since := "2026-05-31")]
-protected abbrev Repr.ι (_repr : Repr R a ι) : Type _ := ι
+/-- The indexing type of a representation of `comul a` -/
+@[nolint unusedArgs, deprecated "The indexing type is now unbundled" (since := "2026-05-31")]
+protected abbrev Repr.ι (repr : Repr R a ι) : Type _ := ι
 
 @[simp]
 theorem coassoc_apply (a : A) :
