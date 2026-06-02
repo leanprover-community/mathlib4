bf9127fd31cafa39ab45315c4023fe0b109c58a7
Sebastien Gouezel <sebastien.gouezel@univ-rennes1.fr>
2026-06-02 08:08:19 +0200

    Update Mathlib/MeasureTheory/Function/LpSpace/Basic.lean
    Co-authored-by: Moritz Doll <moritz.doll@googlemail.com>

--- Mathlib/MeasureTheory/Function/LpSpace/Basic.lean
+++ Mathlib/MeasureTheory/Function/LpSpace/Basic.lean
@@ -204,7 +204,7 @@ theorem coeFn_finsetSum {ι : Type*} (s : Finset ι) (f : ι → Lp E p μ) :
   simp [AEEqFun.coeFn_finsetSum]
 
 theorem coeFn_finsetSum_fun {ι : Type*} (s : Finset ι) (f : ι → Lp E p μ) :
-    ⇑(∑ i ∈ s, f i) =ᵐ[μ] fun x ↦ ∑ i ∈ s, f i x:= by
+    ⇑(∑ i ∈ s, f i) =ᵐ[μ] fun x ↦ ∑ i ∈ s, f i x := by
   grw [coeFn_finsetSum]
   filter_upwards with x using by simp
 
