1560704ea6bccefed508ec4cf881da1a621c8e31
Sebastien Gouezel <sebastien.gouezel@univ-rennes1.fr>
2026-06-02 18:08:32 +0200

    Update Mathlib/MeasureTheory/Measure/MeasureSpace.lean
    Co-authored-by: Etienne Marion <66847262+EtienneC30@users.noreply.github.com>

--- Mathlib/MeasureTheory/Measure/MeasureSpace.lean
+++ Mathlib/MeasureTheory/Measure/MeasureSpace.lean
@@ -1089,9 +1089,7 @@ instance [SMul R ℝ≥0∞] [IsScalarTower R ℝ≥0∞ ℝ≥0∞] [CovariantC
 
 instance [SMul R ℝ≥0∞] [LE R] [IsScalarTower R ℝ≥0∞ ℝ≥0∞] [IsOrderedSMul R ℝ≥0∞] :
     IsOrderedSMul R (Measure α) where
-  smul_le_smul_left μ ν hμν a s := by
-    simp only [smul_apply]
-    gcongr
+  smul_le_smul_left μ ν hμν a s := by gcongr
   smul_le_smul_right a b hab μ s := by
     simp only [smul_apply]
     gcongr
