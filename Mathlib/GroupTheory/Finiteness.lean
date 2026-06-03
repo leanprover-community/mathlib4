dbffbce3d638dc02435315b427e876843159e559
tb65536 <thomas.l.browning@gmail.com>
2026-06-02 18:50:18 -0700

    fix

--- Mathlib/GroupTheory/Finiteness.lean
+++ Mathlib/GroupTheory/Finiteness.lean
@@ -219,6 +219,13 @@ instance Monoid.fg_of_addMonoid_fg {M : Type*} [AddMonoid M] [AddMonoid.FG M] :
     Monoid.FG (Multiplicative M) :=
   AddMonoid.fg_iff_mul_fg.1 ‹_›
 
+-- This was previously a global instance,
+-- but it doesn't appear to be used and has been implicated in slow typeclass resolutions.
+@[to_additive]
+lemma Monoid.fg_of_finite [Finite M] : Monoid.FG M := by
+  cases nonempty_fintype M
+  exact ⟨⟨Finset.univ, by rw [Finset.coe_univ]; exact Submonoid.closure_univ⟩⟩
+
 end Monoid
 
 @[to_additive]
