64a72dfa78824c26f0348519395f05960081b1c7
thorimur <68410468+thorimur@users.noreply.github.com>
2026-06-04 10:39:55 -0400

    feat: fulfillBodyPromises

--- Mathlib/Tactic/Linter/Declaration/Run.lean
+++ Mathlib/Tactic/Linter/Declaration/Run.lean
@@ -13,24 +13,44 @@ namespace Mathlib.Tactic.Linter
 
 public meta section
 
-def runDeclarationLinters (cmd : Syntax) : CommandElabM Unit := do
+def fulfillBodyPromises (trees : PersistentArray InfoTree)
+    (bodies : NameMap (IO.Promise BodyWithContext)) :
+    BaseIO (Array (Option Name × BodyWithContext)) := do
+  let mut extraBodies := #[]
+  for t in trees do
+    extraBodies ← t.foldDeclBodyInfosM (init := extraBodies) fun body ctx info extra => do
+      if let some name := ctx.parentDecl? then
+        if let some p := bodies.get? name then
+          p.resolve { body, ctx, info : BodyWithContext }
+          return extra
+        else
+          return extra.push (some name, { body, ctx, info : BodyWithContext })
+      else
+        return extra.push (none, { body, ctx, info : BodyWithContext })
+  return extraBodies
+
+def fulfillBodyPromisesAsync (trees : PersistentArray InfoTree)
+    (bodies : NameMap (IO.Promise BodyWithContext)) :
+    BaseIO (Task (Array (Option Name × BodyWithContext))) :=
+  BaseIO.asTask <| fulfillBodyPromises trees bodies
+
+def runDeclarationLintersAsync (cmd : Syntax) : CommandElabM Unit := do
   let some pos := cmd.getPos? | throwError "Could not find position for syntax:{indentD cmd}"
   let env ← getEnv
   let decls := pos.getDeclsAfter env (← getFileMap)
-  let mut data : Declarations ← decls.foldlM (init := {}) fun acc name => do
+  let mut data : Declarations := {}
+  let mut bodies : NameMap (IO.Promise BodyWithContext) := {}
+  for decl in decls do
     -- TODO: replace with just `isAutoDecl` after batteries#1831
-    let isAuto ← pure (isReservedName (← getEnv) name) <||>
-      liftCoreM (Batteries.Tactic.Lint.isAutoDecl (privateToUserName name))
-    return acc.insert name { name, isAuto : DeclarationData }
-  -- Special-case `example`
-  if cmd.isExample then
-    data := data.insert `_example { name := `_example, isAuto := false }
-  for t in ← getInfoTrees do
-    data := t.foldDeclBodyInfos (init := data) fun body ctx info data =>
-      if let some name := ctx.parentDecl? then
-        data.setBodyWithContext body ctx info name
-      else
-        data
+    let isAuto ← pure (isReservedName (← getEnv) decl) <||>
+      liftCoreM (Batteries.Tactic.Lint.isAutoDecl (privateToUserName decl))
+    let body ← IO.Promise.new
+    bodies := bodies.insert decl body
+    data := data.insert decl { name := decl, body := body.result?, isAuto : DeclarationData }
+  -- Should we special case `example` instead here? I suspect not...
+  let extraBodiesTask ← fulfillBodyPromisesAsync (← getInfoTrees) bodies
+
   let declLinters := sorry
-  for linter in declLinters do
-    try linter cmd decls catch _ => finally (reset)
+  -- for linter in declLinters do
+  --   sorry
+    -- try linter cmd decls catch _ => finally (reset)
