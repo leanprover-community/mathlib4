5cca3cd49aa5db9377459353bfab713bc70abeec
Michael Rothgang <rothgang@math.uni-bonn.de>
2026-06-04 12:34:40 +0200

    Apply suggestion from @Vierkantor
    Co-authored-by: Anne Baanen <Vierkantor@users.noreply.github.com>

--- Mathlib/AlgebraicGeometry/Modules/Tilde.lean
+++ Mathlib/AlgebraicGeometry/Modules/Tilde.lean
@@ -134,8 +134,8 @@ lemma restrictAppIso_smul_Spec {S : CommRingCat.{u}} (f : R ⟶ S)
     simp [Iso.cancel_iso_inv_left, Hom.app_eq_appLE]
     rfl
   rw [this]
-set_option linter.dupNamespace false in
 
+set_option linter.dupNamespace false in
 @[deprecated (since := "2026-06-04")]
 alias Scheme.Modules.restrictAppIso_smul_Spec := restrictAppIso_smul_Spec
 
