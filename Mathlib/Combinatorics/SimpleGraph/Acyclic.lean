8d27f533e131bd95c797918d88ac1eecaca16086
Yaël Dillies <yael.dillies@gmail.com>
2026-06-04 19:29:23 +0200

    revert
    Co-authored-by: Bhavik Mehta <bhavikmehta8@gmail.com>

--- Mathlib/Combinatorics/SimpleGraph/Acyclic.lean
+++ Mathlib/Combinatorics/SimpleGraph/Acyclic.lean
@@ -505,7 +505,7 @@ lemma isTree_iff_connected_and_card [Finite V] :
   classical
   refine ⟨fun h ↦ ⟨h.connected, by simpa [edgeFinset] using h.card_edgeFinset⟩,
     fun ⟨h₁, h₂⟩ ↦ ⟨h₁, ?_⟩⟩
-  simp_rw [isAcyclic_iff_forall_isBridge, Sym2.forall]
+  simp_rw [isAcyclic_iff_forall_adj_isBridge]
   refine fun x y h ↦ by_contra fun hbr ↦
     (h₁.connected_delete_edge_of_not_isBridge hbr).card_vert_le_card_edgeSet_add_one.not_gt ?_
   rw [Nat.card_eq_fintype_card, ← edgeFinset_card, ← h₂, Nat.card_eq_fintype_card,
