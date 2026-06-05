b95a482710546acf206982df27136b2f74473bf8
Michael Rothgang <rothgang@math.uni-bonn.de>
2026-06-05 15:12:29 +0200

    chore: three other deprecations Found by #find_deleted_files 1000; with that, everything has one!

--- Mathlib/LinearAlgebra/ConvexSpace/AffineSpace.lean
+++ Mathlib/LinearAlgebra/ConvexSpace/AffineSpace.lean
@@ -1,13 +1,6 @@
 module
 
-public import Mathlib.Algebra.BigOperators.Finsupp.Basic
-public import Mathlib.Algebra.Module.Defs
-public import Mathlib.Algebra.Order.BigOperators.Group.Finset
-public import Mathlib.Algebra.Order.Ring.Defs
-public import Mathlib.Data.Finsupp.SMulWithZero
-public import Mathlib.Tactic.Bound
-public import Mathlib.Data.Finsupp.SMul
-public import Mathlib.Data.Finsupp.Order
+public import Mathlib.LinearAlgebra.ConvexSpace
 public import Mathlib.LinearAlgebra.AffineSpace.Combination
 public import Mathlib.LinearAlgebra.AffineSpace.AffineMap
 
