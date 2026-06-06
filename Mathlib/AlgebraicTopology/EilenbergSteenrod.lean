44ab2dd8a03221de721f67d0007d2d224127dfa8
Jacob Scharmberg <hi@jacobscharmberg.net>
2026-06-06 15:30:51 +0200

    fix formatting

--- Mathlib/AlgebraicTopology/EilenbergSteenrod.lean
+++ Mathlib/AlgebraicTopology/EilenbergSteenrod.lean
@@ -3,7 +3,6 @@ Copyright (c) 2026 Jakob Scharmberg. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jakob Scharmberg
 -/
-
 module
 
 public import Mathlib.Algebra.Homology.ComplexShape
