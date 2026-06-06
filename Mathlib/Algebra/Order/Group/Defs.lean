3cf8c70649d8247f4c2ecd14389035c0f8e65d5a
Hagb (Junyu Guo) <hagb@hagb.name>
2026-06-06 16:23:49 +0800

    improve doc-string

--- Mathlib/Algebra/Order/Group/Defs.lean
+++ Mathlib/Algebra/Order/Group/Defs.lean
@@ -58,13 +58,13 @@ instance (priority := 100) IsOrderedMonoid.toIsOrderedCancelMonoid
 /-- Assuming `α` equipped with `LinearOrder` is `CancelCommMonoid` and `IsOrderedMonoid`, it is
 also be `IsOrderedCancelMonoid`.
 
-TODO: make it an `instance`. To avoid slowdown, it is not an instance currently. See
+TODO: make it an `instance`. To avoid slowdown, it was not an instance when it was submitted. See
 https://github.com/leanprover-community/mathlib4/pull/32828. -/
 @[implicit_reducible, to_additive IsOrderedAddMonoid.toIsOrderedCancelAddMonoid'
   /-- Assuming `α` equipped with `LinearOrder` is `AddCancelCommMonoid` and `IsAddOrderedMonoid`, it
   is also `IsAddOrderedCancelMonoid`.
 
-  TODO: make it an `instance`. To avoid slowdown, it is not an instance currently. See
+  TODO: make it an `instance`. To avoid slowdown, it was not an instance when it was submitted. See
   https://github.com/leanprover-community/mathlib4/pull/32828. -/]
 def IsOrderedMonoid.toIsOrderedCancelMonoid'
     [CancelCommMonoid α] [LinearOrder α] [IsOrderedMonoid α] : IsOrderedCancelMonoid α where
