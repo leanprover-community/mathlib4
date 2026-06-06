83a643013d3a97ebcee3177fd6463ea06aa109e9
Batixx <s59fpern@uni-bonn.de>
2026-06-06 06:42:18 +0200

    chore(NumberTheory/Padics): remove an `erw`

--- Mathlib/NumberTheory/Padics/PadicNumbers.lean
+++ Mathlib/NumberTheory/Padics/PadicNumbers.lean
@@ -593,7 +593,6 @@ theorem coe_zero : (↑(0 : ℚ) : ℚ_[p]) = 0 := rfl
 end Completion
 
 end Padic
-
 /-- The rational-valued `p`-adic norm on `ℚ_[p]` is lifted from the norm on Cauchy sequences. The
 canonical form of this function is the normed space instance, with notation `‖ ‖`. -/
 def padicNormE {p : ℕ} [hp : Fact p.Prime] : AbsoluteValue ℚ_[p] ℚ where
@@ -601,8 +600,8 @@ def padicNormE {p : ℕ} [hp : Fact p.Prime] : AbsoluteValue ℚ_[p] ℚ where
   map_mul' q r := Quotient.inductionOn₂ q r <| PadicSeq.norm_mul
   nonneg' q := Quotient.inductionOn q <| PadicSeq.norm_nonneg
   eq_zero' q := Quotient.inductionOn q fun r ↦ by
-    erw [Padic.zero_def, Quotient.eq]
-    exact PadicSeq.norm_zero_iff r
+    rw [Padic.zero_def, Quotient.lift_mk, PadicSeq.norm_zero_iff r]
+    exact Quotient.eq.symm
   add_le' q r := by
     trans
       max ((Quotient.lift PadicSeq.norm <| @PadicSeq.norm_equiv _ _) q)
