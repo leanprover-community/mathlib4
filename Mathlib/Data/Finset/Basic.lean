cced4103c8dbad073e69fe563d0a739ddf401316
Eric Wieser <wieser.eric@gmail.com>
2026-06-07 06:54:01 +0000

    feat: `choose_eq_iff` lemmas for `Finset` and `Multiset`

--- Mathlib/Data/Finset/Basic.lean
+++ Mathlib/Data/Finset/Basic.lean
@@ -569,6 +569,9 @@ theorem choose_property (hp : ∃! a, a ∈ l ∧ p a) : p (l.choose p hp) :=
 
 grind_pattern choose_property => l.choose p hp
 
+theorem choose_eq_iff (hp : ∃! a, a ∈ l ∧ p a) {a : α} : choose p l hp = a ↔ a ∈ l ∧ p a :=
+  ⟨fun h => h ▸ choose_spec p l hp, hp.unique (choose_spec p l hp)⟩
+
 end Choose
 
 end Finset
