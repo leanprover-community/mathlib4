98e4a292e494ae09383bffa7f22f5843afa01257
Remy Degenne <remydegenne@gmail.com>
2026-06-07 16:42:42 +0200

    add tv for vector measures

--- Mathlib/InformationTheory/TotalVariation/Basic.lean
+++ Mathlib/InformationTheory/TotalVariation/Basic.lean
@@ -12,6 +12,8 @@ public import Mathlib.MeasureTheory.VectorMeasure.Variation.Defs
 
 ## Main definitions
 
+* `vecTVDist μ ν`: total variation distance between two vector measures, defined as the value on
+  the universal set of the variation of `μ - ν`.
 * `tvDist μ ν`: total variation distance, defined as the value on the universal set of
   the variation of `μ - ν`, in which both measures are seen as signed measures.
 
@@ -36,14 +38,18 @@ namespace InformationTheory
 variable {𝓧 : Type*} {m𝓧 : MeasurableSpace 𝓧}
   {μ ν : Measure 𝓧} [IsFiniteMeasure μ] [IsFiniteMeasure ν]
 
+/-- Total variation distance between two vector measures. -/
+noncomputable def vecTVDist {M : Type*} [NormedAddCommGroup M]
+    (μ ν : VectorMeasure 𝓧 M) : ℝ≥0∞ := (μ - ν).variation Set.univ
+
 /-- Total variation distance between two finite measures. -/
 noncomputable def tvDist (μ ν : Measure 𝓧) [IsFiniteMeasure μ] [IsFiniteMeasure ν] : ℝ :=
-  ((μ.toSignedMeasure - ν.toSignedMeasure).variation Set.univ).toReal
+  (vecTVDist μ.toSignedMeasure ν.toSignedMeasure).toReal
 
 lemma tvDist_eq_iSup_finPartition_abs :
     tvDist μ ν = ⨆ (P : Finpartition (⟨.univ, .univ⟩ : Subtype (MeasurableSet (α := 𝓧)))),
       ∑ p ∈ P.parts, |μ.real p - ν.real p| := by
-  simp only [tvDist, VectorMeasure.variation, preVariation, ennrealPreVariation,
+  simp only [tvDist, vecTVDist, VectorMeasure.variation, preVariation, ennrealPreVariation,
     VectorMeasure.coe_sub, Pi.sub_apply, Measure.toSignedMeasure_apply,
     VectorMeasure.ennrealToMeasure_apply .univ, preVariationFun, MeasurableSet.univ, ↓reduceDIte]
   rw [ENNReal.toReal_iSup (fun _ ↦ by simp)]
