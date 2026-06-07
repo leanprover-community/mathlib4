4df25829492984dcdcc5f6e56e503ea6ee72740d
Snir Broshi <26556598+SnirBroshi@users.noreply.github.com>
2026-06-07 20:33:35 +0300

    fix import after merge

--- Mathlib/Combinatorics/SimpleGraph/Coloring/Greedy.lean
+++ Mathlib/Combinatorics/SimpleGraph/Coloring/Greedy.lean
@@ -5,7 +5,7 @@ Authors: Snir Broshi
 -/
 module
 
-public import Mathlib.Combinatorics.SimpleGraph.Coloring.VertexColoring
+public import Mathlib.Combinatorics.SimpleGraph.Coloring.Vertex
 public import Mathlib.SetTheory.Ordinal.Arithmetic
 
 /-!
