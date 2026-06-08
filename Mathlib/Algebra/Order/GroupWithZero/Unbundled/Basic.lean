0dc03464ef993d51de688f5ca6be41e8f9e62feb
Xavier Roblot <46200072+xroblot@users.noreply.github.com>
2026-06-08 13:37:48 +0000

    feat(RamificationInertia): splitting in the decomposition field (#35802)
    We add results about the splitting of the prime `p` in the decomposition field `D` of `P`, prime over `p`, in a Galois extension `L/K`. Specifically, for `𝓟D` the prime of `D` below `P`:
      - `P` is the unique prime of `B` above `𝓟D`
      -  the ramification index (resp. inertia degree) of `𝓟D` in `L` equals that of `p` in `L`
      - `𝓟D` is unramified over `p`
       
      ---
    <!-- Your PR title will become the first line of the commit message.
    
    In this box, the text above the `---` (if not empty) will be appended
    to the commit message, and can be used to give additional context or
    details. Please leave a blank newline before the `---`, otherwise GitHub
    will format the text above it as a title.
    
    For details on the "pull request lifecycle" in mathlib, please see:
    https://leanprover-community.github.io/contribute/index.html
    
    In particular, note that most reviewers will only notice your PR
    if it passes the continuous integration checks.
    Please ask for help on https://leanprover.zulipchat.com if needed.
    
    When merging, all the commits will be squashed into a single commit
    listing all co-authors.
    
    Co-authors in the squash commit are gathered from two sources:
    
    First, all authors of commits to this PR branch are included. Thus,
    one way to add co-authors is to include at least one commit authored by
    each co-author among the commits in the pull request. If necessary, you
    may create empty commits to indicate co-authorship, using commands like so:
    
    git commit --author="Author Name <author@email.com>" --allow-empty -m "add Author Name as coauthor"
    
    Second, co-authors can also be listed in lines at the very bottom of
    the commit message (that is, directly before the `---`) using the following format:
    
    
    If you are moving or deleting declarations, please include these lines
    at the bottom of the commit message (before the `---`, and also before
    any "Co-authored-by" lines) using the following format:
    
    Moves:
    - Vector.* -> List.Vector.*
    - ...
    
    Deletions:
    - Nat.bit1_add_bit1
    - ...
    
    Any other comments you want to keep out of the PR commit should go
    below the `---`, and placed outside this HTML comment, or else they
    will be invisible to reviewers.
    
    If this PR depends on other PRs, please list them below this comment,
    using the following format:
    - [ ] depends on: #abc [optional extra text]
    - [ ] depends on: #xyz [optional extra text]
    
    -->
    
    - [x] depends on: #35798
    - [x] depends on: #35405
    - [x] depends on: #35371
    
    
    [![Open in Gitpod](https://gitpod.io/button/open-in-gitpod.svg)](https://gitpod.io/from-referrer/)
    
    Co-authored-by: Author Name <author@email.com>

--- Mathlib/Algebra/Order/GroupWithZero/Unbundled/Basic.lean
+++ Mathlib/Algebra/Order/GroupWithZero/Unbundled/Basic.lean
@@ -201,6 +201,17 @@ theorem mul_eq_mul_iff_eq_and_eq_of_pos' [PosMulStrictMono α] [MulPosStrictMono
   · exact (mul_lt_mul_of_pos_right hab c0).trans_le (mul_le_mul_of_nonneg_left hcd b0.le)
   · exact (mul_le_mul_of_nonneg_right hab c0.le).trans_lt (mul_lt_mul_of_pos_left hcd b0)
 
+theorem eq_and_eq_of_pos_of_le_of_mul_le_mul [PosMulReflectLE α] [MulPosReflectLE α]
+    [PosMulMono α] [MulPosMono α] (ha : 0 < a) (hc : 0 < c) (hab : a ≤ b) (hcd : c ≤ d)
+    (h : b * d ≤ a * c) : a = b ∧ c = d := by
+  refine ⟨le_antisymm hab ?_, le_antisymm hcd ?_⟩
+  · grw [hcd] at h
+    · exact le_of_mul_le_mul_of_pos_right h <| hc.trans_le hcd
+    · exact ha.le
+  · grw [hab] at h
+    · exact le_of_mul_le_mul_of_pos_left h <| ha.trans_le hab
+    · exact hc.le
+
 end PartialOrder
 
 section LinearOrder
