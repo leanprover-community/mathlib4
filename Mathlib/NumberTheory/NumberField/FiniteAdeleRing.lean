0a18eb2a346f3302d30ba5d3cb745180d61be530
Salvatore Mercuri <salvatore.mercuri@imperial.ac.uk>
2026-06-08 10:01:43 +0100

    fix

--- Mathlib/NumberTheory/NumberField/FiniteAdeleRing.lean
+++ Mathlib/NumberTheory/NumberField/FiniteAdeleRing.lean
@@ -47,7 +47,7 @@ open Filter HeightOneSpectrum Valued in
 private theorem hasProd_zero_subset_one_lt_valued {x : FiniteAdeleRing (𝓞 K) K} (hx : ¬IsUnit x)
     (hx₀ : ∀ v, x v ≠ 0) : HasProd (fun v : {v | Valued.v (x v) < 1} ↦ ‖x v‖) 0 :=
   have hx := infinite_valued_ne_one_of_not_isUnit (by simpa using hx₀) hx
-  have hx_prop : {v | 1 < Valued.v (x v)}.Finite := finite_valued_lt_one x
+  have hx_prop : {v | 1 < Valued.v (x v)}.Finite := finite_valued_one_lt x
   have hx_inf : {v | Valued.v (x v) < 1}.Infinite := (hx.diff hx_prop).mono (by grind)
   have : atTop.Tendsto (fun s : Finset {v | Valued.v (x v) < 1} ↦ (∏ v ∈ s, ‖x v‖)⁻¹) atTop := by
     have h_le (S : Finset {v | Valued.v (x v) < 1}) : 2 ^ S.card ≤ (∏ v ∈ S, ‖x v‖)⁻¹ := by
