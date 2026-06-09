22b3cbdd9e65706d004dd872fcc46f25847b8bbd
tb65536 <thomas.l.browning@gmail.com>
2026-06-09 00:24:11 -0700

    add

--- Mathlib/FieldTheory/Perfect.lean
+++ Mathlib/FieldTheory/Perfect.lean
@@ -342,7 +342,7 @@ instance Algebra.IsAlgebraic.isSeparable_of_perfectField {K L : Type*} [Field K]
     minpoly.irreducible (Algebra.IsIntegral.isIntegral x)⟩
 
 /-- If `L / K` is an algebraic extension, `K` is a perfect field, then so is `L`. -/
-theorem Algebra.IsAlgebraic.perfectField {K L : Type*} [Field K] [Field L] [Algebra K L]
+theorem Algebra.IsAlgebraic.perfectField (K : Type*) {L : Type*} [Field K] [Field L] [Algebra K L]
     [Algebra.IsAlgebraic K L] [PerfectField K] : PerfectField L := ⟨fun {f} hf ↦ by
   obtain ⟨_, _, hi, h⟩ := hf.exists_dvd_monic_irreducible_of_isIntegral (K := K)
   exact (PerfectField.separable_of_irreducible hi).map |>.of_dvd h⟩
@@ -350,7 +350,7 @@ theorem Algebra.IsAlgebraic.perfectField {K L : Type*} [Field K] [Field L] [Alge
 theorem PerfectField.of_ringEquiv {K L : Type*} [Field K] [Field L] (h : K ≃+* L) [PerfectField K] :
     PerfectField L := by
   let := h.toRingHom.toAlgebra
-  exact Algebra.IsAlgebraic.perfectField (K := K)
+  exact Algebra.IsAlgebraic.perfectField
 
 namespace Polynomial
 
