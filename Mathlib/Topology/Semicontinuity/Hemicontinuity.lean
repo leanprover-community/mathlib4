7cf9fc40a12bdcf900349137e2378d8ca90a9c1d
Kevin H Wilson <khwilson@gmail.com>
2026-06-08 22:12:52 -0400

    reduce imports

--- Mathlib/Topology/Semicontinuity/Hemicontinuity.lean
+++ Mathlib/Topology/Semicontinuity/Hemicontinuity.lean
@@ -10,7 +10,7 @@ public import Mathlib.Topology.NhdsWithin
 public import Mathlib.Topology.Separation.Regular
 public import Mathlib.Topology.Defs.Sequences
 public import Mathlib.Topology.UniformSpace.Closeds
-public import Mathlib.Topology.UniformSpace.Compact
+import Mathlib.Topology.UniformSpace.Compact
 public import Mathlib.Topology.UniformSpace.UniformConvergence
 import Mathlib.Topology.Sequences
 
