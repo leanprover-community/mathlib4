7e8f30a86b80dbae79fbd60e783e600c925db40e
Thomas Browning <tb65536@users.noreply.github.com>
2026-06-09 09:42:12 -0700

    Update Mathlib/FieldTheory/Perfect.lean
    Co-authored-by: Riccardo Brasca <riccardo.brasca@gmail.com>

--- Mathlib/FieldTheory/Perfect.lean
+++ Mathlib/FieldTheory/Perfect.lean
@@ -348,9 +348,9 @@ theorem Algebra.IsAlgebraic.perfectField (K : Type*) {L : Type*} [Field K] [Fiel
   exact (PerfectField.separable_of_irreducible hi).map |>.of_dvd h⟩
 
 theorem PerfectField.of_ringEquiv {K L : Type*} [Field K] [Field L] (h : K ≃+* L) [PerfectField K] :
-    PerfectField L := by
+    PerfectField L :=
   let := h.toRingHom.toAlgebra
-  exact Algebra.IsAlgebraic.perfectField K
+  Algebra.IsAlgebraic.perfectField K
 
 namespace Polynomial
 
