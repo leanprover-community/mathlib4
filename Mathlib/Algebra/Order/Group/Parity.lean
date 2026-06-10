4a882228be5e950aba9cf34965f3e9cf7d4fa08d
Yaël Dillies <yael.dillies@gmail.com>
2026-06-10 11:49:45 +0200

    feat(Algebra): the max of two squares is a square Also use `Even.pow_of_ne_zero` wherever possible.
    From APAP

---
+++ Mathlib/Algebra/Order/Group/Parity.lean
@@ -0,0 +1,26 @@
+/-
+Copyright (c) 2026 Yaël Dillies. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Yaël Dillies
+-/
+module
+
+public import Mathlib.Algebra.Group.Even
+public import Mathlib.Order.Lattice
+
+/-!
+# Parity in ordered monoids
+-/
+
+public section
+
+section
+variable {M : Type*} [LinearOrder M] [Monoid M] {a b : M}
+
+@[to_additive]
+protected lemma IsSquare.max (ha : IsSquare a) (hb : IsSquare b) : IsSquare (max a b) := by grind
+
+@[to_additive]
+protected lemma IsSquare.min (ha : IsSquare a) (hb : IsSquare b) : IsSquare (min a b) := by grind
+
+end
