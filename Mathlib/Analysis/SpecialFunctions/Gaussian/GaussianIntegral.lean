16f8ab9771452ecdc584f900f9766f8d0c3362ed
Yi.Yuan <kysyy1@126.com>
2026-06-10 22:28:38 +0800

    fix warning

--- Mathlib/Analysis/SpecialFunctions/Gaussian/GaussianIntegral.lean
+++ Mathlib/Analysis/SpecialFunctions/Gaussian/GaussianIntegral.lean
@@ -299,9 +299,9 @@ theorem integral_gaussian_complex_Ioi {b : ℂ} (hb : 0 < re b) :
       simpa [f] using (integral_comp_neg_Ioi 0 f).symm
     _ = ∫ x : ℝ in Ioi 0, f x :=
       setIntegral_congr_fun measurableSet_Ioi fun _ _ ↦ (by simp [f])
-  rw [← integral_add_compl (s := Ioi 0) (by simp) (integrable_cexp_neg_mul_sq hb), compl_Ioi, h_eq, ← mul_two]
-    at full_integral
-  exact (eq_div_iff two_ne_zero).2 (by simpa [mul_comm] using full_integral)
+  rw [← integral_add_compl (s := Ioi 0) (by simp) (integrable_cexp_neg_mul_sq hb), compl_Ioi, h_eq,
+    ← mul_two] at full_integral
+  exact (eq_div_iff two_ne_zero).2 (by simpa using full_integral)
 
 -- The Gaussian integral on the half-line, `∫ x in Ioi 0, exp (-b * x^2)`, for real `b`.
 theorem integral_gaussian_Ioi (b : ℝ) :
