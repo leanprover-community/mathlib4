3df4fa1e8d8fff299062ded16ba73befbfb3be09
Jireh Loreaux <loreaujy@gmail.com>
2026-06-10 17:07:06 -0500

    mk_all

--- Mathlib.lean
+++ Mathlib.lean
@@ -7524,6 +7524,7 @@ public import Mathlib.Topology.Algebra.Module.Determinant
 public import Mathlib.Topology.Algebra.Module.Equiv
 public import Mathlib.Topology.Algebra.Module.FiniteDimension
 public import Mathlib.Topology.Algebra.Module.FiniteDimensionBilinear
+public import Mathlib.Topology.Algebra.Module.IsWeak
 public import Mathlib.Topology.Algebra.Module.LinearMap
 public import Mathlib.Topology.Algebra.Module.LinearMapPiProd
 public import Mathlib.Topology.Algebra.Module.LinearPMap
