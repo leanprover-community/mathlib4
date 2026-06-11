dd7008c853f3fe7ae7d8d64321e5a40d8037a336
TheGoedeDoel <jrozmarynowycz@gmail.com>
2026-06-12 00:21:10 +0200

    Improve definition of SubmonoidFunctor.IsLocalization
    Rename several things.
    
    Add IsLocalization instances.

--- Mathlib/Algebra/Category/ModuleCat/LocalizedFunctor.lean
+++ Mathlib/Algebra/Category/ModuleCat/LocalizedFunctor.lean
@@ -9,6 +9,7 @@ public import Mathlib.Algebra.Category.Ring.Basic
 public import Mathlib.Algebra.Module.LocalizedModule.IsLocalization
 public import Mathlib.CategoryTheory.Subfunctor.SubmonoidFunctor
 public import Mathlib.Combinatorics.Quiver.ReflQuiver
+public import Mathlib.Tactic.Algebraize
 
 /-!
 ## Functors of Localized Rings
@@ -35,19 +36,15 @@ open CategoryTheory CommRingCat
 
 namespace CategoryTheory
 
-instance : HasForget₂ CommRingCat.{u} MonCat.{u} where
-  forget₂ := Functor.comp (forget₂ CommRingCat.{u} CommMonCat.{u})
-    (forget₂ CommMonCat.{u} MonCat.{u})
-  forget_comp := rfl
-
 variable {C : Type u₁} [Category.{v₁} C] {R : C ⥤ CommRingCat.{u}}
-  (S : SubmonoidFunctor (R ⋙ (forget₂ CommRingCat.{u} MonCat.{u})))
+  (S : SubmonoidFunctor (R ⋙ (forget₂ CommRingCat.{u} CommMonCat.{u}) ⋙
+  (forget₂ CommMonCat.{u} MonCat.{u})))
 
 /-- The associated submonoid `S(U)` of `R(U)` recovered after the forgetful functor -/
 abbrev SubmonoidFunctor.ringObj (U : C) : Submonoid (R.obj U) := S.obj U
 
 /-- The functor of submonoids mapping property recovered after the forgetful functor -/
-lemma SubmonoidFunctor.ringMap {X Y : C} (i : X ⟶ Y) (s : ringObj S X) :
+lemma SubmonoidFunctor.map_mem_ringObj {X Y : C} (i : X ⟶ Y) (s : ringObj S X) :
     R.map i s ∈ ringObj S Y := S.map i s.2
 
 variable (R) in
@@ -59,41 +56,30 @@ noncomputable abbrev LocalizationFunctor : C ⥤ CommRingCat where
   obj _ := of (Localization (S.ringObj _))
   map {U V} i:= ofHom (IsLocalization.map (Localization
     (S.ringObj V)) (R.map i).hom (S.map i))
-  map_id := by cat_disch
   map_comp i j := by
     ext x
     simpa [Functor.map_comp, hom_comp, RingHom.coe_comp] using
       Eq.symm (IsLocalization.map_map (S.map i) (S.map j) x)
 
-/-- `S.isLocalization p` implies `p : R ⟶ R'` is the localization map and `R'` is the localized
+instance {U : C} : IsLocalization (S.ringObj U) ((LocalizationFunctor R S).obj U) := inferInstance
+
+/-- `S.IsLocalization p` implies `p : R ⟶ R'` is the localization map and `R'` is the localized
 functor `LocalizationFunctor R S` up to isomorphism. -/
-@[mk_iff] class SubmonoidFunctor.isLocalization {R' : C ⥤ CommRingCat} (p : R ⟶ R') : Prop where
-  map_units (p) (U : C) (y : S.ringObj U) : IsUnit ((p.app U).hom y)
-  surj (p) (U : C) (z : R'.obj U) : ∃ x : (R.obj U) × (S.ringObj U), z * (p.app U).hom x.2 =
-    (p.app U).hom x.1
-  exists_of_eq {U : C} {x y} : (p.app U).hom x = (p.app U).hom y → ∃ c : S.ringObj U, c * x = c * y
+@[mk_iff] class SubmonoidFunctor.IsLocalization {R' : C ⥤ CommRingCat} (p : R ⟶ R') : Prop where
+  toIsLocalizationMap (U : C) : (S.ringObj U).IsLocalizationMap (p.app U)
 
-attribute [nolint docBlame] SubmonoidFunctor.isLocalization.map_units
-  SubmonoidFunctor.isLocalization.surj SubmonoidFunctor.isLocalization.exists_of_eq
-
-lemma isLocalizationMap_of_isLocalization {R' : C ⥤ CommRingCat} (p : R ⟶ R') [S.isLocalization p]
-    {U : C} : (S.ringObj U).IsLocalizationMap (p.app U) :=
-  ⟨SubmonoidFunctor.isLocalization.map_units _ _, SubmonoidFunctor.isLocalization.surj _ _ ,
-    SubmonoidFunctor.isLocalization.exists_of_eq⟩
+instance {U : C} {R' : C ⥤ CommRingCat} (p : R ⟶ R') [S.IsLocalization p] :
+    letI := (p.app U).hom.toAlgebra;  IsLocalization (S.ringObj U) (R'.obj U) := by
+  apply letI := (p.app U).hom.toAlgebra; IsLocalization'.mk
+  apply SubmonoidFunctor.IsLocalization.toIsLocalizationMap
 
 namespace LocalizationFunctor
 
-lemma IsLocalization.eq_iff_exists {R' : C ⥤ CommRingCat} (p : R ⟶ R') [S.isLocalization p]
+lemma IsLocalization.eq_iff_exists {R' : C ⥤ CommRingCat} (p : R ⟶ R') [S.IsLocalization p]
     {U : C} {x₁ x₂ : R.obj U} :
       (p.app U).hom x₁ = (p.app U).hom x₂ ↔ ∃ c : S.ringObj U, c * x₁ = c * x₂ :=
-  Iff.intro SubmonoidFunctor.isLocalization.exists_of_eq fun ⟨c, h⟩ ↦ by
-    apply_fun (p.app U).hom at h
-    rename_i h₁
-    simp_all only [Subtype.exists, exists_prop]
-    simp_rw [map_mul] at h
-    obtain h₂ := (SubmonoidFunctor.isLocalization.map_units p U c)
-    apply IsUnit.mul_left_cancel h₂
-    apply h
+Submonoid.LocalizationMap.eq_iff_exists (Submonoid.LocalizationMap.mk (p.app U).hom.toMulHom
+  (SubmonoidFunctor.IsLocalization.toIsLocalizationMap U))
 
 /-- The canonical map from `R` to its localization. -/
 noncomputable abbrev ι : R ⟶ LocalizationFunctor R S where
@@ -102,10 +88,8 @@ noncomputable abbrev ι : R ⟶ LocalizationFunctor R S where
 
 lemma ι_apply (U : C) (r : R.obj U) : ((ι S).app U).hom r = Localization.mk r 1 := by rfl
 
-instance : S.isLocalization (ι S) where
-  map_units U x :=  _root_.IsLocalization.map_units (Localization (S.ringObj U)) x
-  surj U z := _root_.IsLocalization.surj (S.ringObj U) z
-  exists_of_eq :=  _root_.IsLocalization.exists_of_eq
+instance : S.IsLocalization (ι S) where
+  toIsLocalizationMap _ := (_root_.IsLocalization'.toIsLocalizationMap)
 
 lemma mk_eq_quotmk {R : Type*} [CommRing R] {S : Submonoid R} (r : R) (s : S) :
     (Quot.mk ⇑(OreLocalization.oreEqv S R) (r, s)) = IsLocalization.mk' (Localization S)  r s := by
@@ -131,7 +115,7 @@ noncomputable def lift' {R' : C ⥤ CommRingCat} (p : R ⟶ R')
     rw [mk_eq_quotmk, IsLocalization.map_mk', IsLocalization.lift_mk']
     exact
       (Submonoid.LocalizationMap.mul_inv_left (h V)
-        ⟨((R.map i).hom) ↑s, S.ringMap i s⟩
+        ⟨((R.map i).hom) ↑s, S.map_mem_ringObj i s⟩
           ((p.app V).hom ((R.map i).hom r))
             ((R'.map i).hom
               ((IsLocalization.lift (h U))
@@ -159,13 +143,14 @@ lemma lift'_comp {R' : C ⥤ CommRingCat} (p : R ⟶ R')
 
 section
 
-variable {R' : C ⥤ CommRingCat} (p : R ⟶ R') [S.isLocalization p]
+variable {R' : C ⥤ CommRingCat} (p : R ⟶ R') [S.IsLocalization p]
+
 
 /-- The canonical isomorphism from the localization functor to any pair `(R', p : R ⟶ R')`
 satisfying the universal property. -/
 noncomputable def fromLocalization :
     LocalizationFunctor R S ⟶ R' :=
-  lift' S p (by intro U x; apply (SubmonoidFunctor.isLocalization.map_units p U x))
+  lift' S p (by intro U x; apply (SubmonoidFunctor.IsLocalization.toIsLocalizationMap U).map_units)
 
 lemma fromLocalization.inj {U : C} :
     Function.Injective ((fromLocalization S p).app U).hom := fun x y eq1 => by
@@ -192,12 +177,13 @@ lemma fromLocalization.inj {U : C} :
   aesop
 
 lemma fromLocalization.surj {U : C} : Function.Surjective ((fromLocalization S p).app U).hom :=
-  fun z => let ⟨⟨m, (s : S.ringObj U)⟩, eq1⟩ := SubmonoidFunctor.isLocalization.surj p U z
-  ⟨IsLocalization.mk' ((LocalizationFunctor R S).obj U) m s, by
-    dsimp only [fromLocalization, lift']
-    simp only [ConcreteCategory.hom_ofHom]
-    rw [IsLocalization.lift_mk', ← eq1, Submonoid.LocalizationMap.mul_inv_left, mul_comm];
-    simp only [RingHom.toMonoidHom_eq_coe, MonoidHom.coe_coe]⟩
+  fun z => let ⟨⟨m, (s : S.ringObj U)⟩, eq1⟩ :=
+  (@SubmonoidFunctor.IsLocalization.toIsLocalizationMap _ _ _ _ _ p _ U).surj z
+    ⟨IsLocalization.mk' ((LocalizationFunctor R S).obj U) m s, by
+      dsimp only [fromLocalization, lift']
+      simp only [ConcreteCategory.hom_ofHom]
+      rw [IsLocalization.lift_mk', ← eq1, Submonoid.LocalizationMap.mul_inv_left, mul_comm];
+      simp only [RingHom.toMonoidHom_eq_coe, MonoidHom.coe_coe]⟩
 
 lemma fromLocalization.bij {U : C} : Function.Bijective ((fromLocalization S p).app U).hom :=
   ⟨fromLocalization.inj S p, fromLocalization.surj S p⟩
@@ -217,21 +203,22 @@ noncomputable def isoApp (U : C) : (LocalizationFunctor R S).obj U ≅ R'.obj U 
   asIso ((fromLocalization S p).app U)
 
 theorem iso_inv_apply_aux {U : C} (r : R'.obj U) : ((iso S p).inv.app U) r = Localization.mk
-    ((SubmonoidFunctor.isLocalization.surj p U r).choose : (R.obj U) × (S.ringObj U)).1
-      (SubmonoidFunctor.isLocalization.surj p U r).choose.2 := by
+    (((@SubmonoidFunctor.IsLocalization.toIsLocalizationMap _ _ _ _ _ p _ U).surj r).choose :
+      (R.obj U) × (S.ringObj U)).1
+      ((@SubmonoidFunctor.IsLocalization.toIsLocalizationMap _ _ _ _ _ p _ U).surj r).choose.2 := by
   apply_fun ((iso S p).hom.app U).hom using
     RingEquiv.injective (CategoryTheory.Iso.commRingCatIsoToRingEquiv (isoApp S p U))
   rw [Iso.inv_hom_id_app_apply]
   simp only [iso, fromLocalization, asIso_hom, lift', ConcreteCategory.hom_ofHom]
   rw [mk_eq_mk, IsLocalization.lift_mk', Units.eq_mul_inv_iff_mul_eq, IsUnit.coe_liftRight,
     RingHom.toMonoidHom_eq_coe, MonoidHom.restrict_apply, MonoidHom.coe_coe,
-    (SubmonoidFunctor.isLocalization.surj _ _ _).choose_spec]
+    ((@SubmonoidFunctor.IsLocalization.toIsLocalizationMap _ _ _ _ _ p _ U).surj _).choose_spec]
 
 end
 
 section
 
-variable {R' R'' : C ⥤ CommRingCat} (p : R ⟶ R') [S.isLocalization p] (q : R ⟶ R'')
+variable {R' R'' : C ⥤ CommRingCat} (p : R ⟶ R') [S.IsLocalization p] (q : R ⟶ R'')
 
 /-- If `R : C ⥤ CommRingCat` and `p : R ⟶ R'` form a localization of `R`, then for any morphism
 `q : R ⟶ R''` such that `(q.app U)` maps all elements of `S(U)` to a unit, then there is a
