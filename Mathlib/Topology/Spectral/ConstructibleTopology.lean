43b142edc2fd6250e5e60ec2ad2282e2f5266537
FMLJohn <128468682+FMLJohn@users.noreply.github.com>
2026-06-12 21:58:02 +0100

    recover

--- Mathlib/Topology/Spectral/ConstructibleTopology.lean
+++ Mathlib/Topology/Spectral/ConstructibleTopology.lean
@@ -161,8 +161,8 @@ instance compactSpace_withConstructibleTopology [CompactSpace X] [QuasiSober X]
     · apply isCompact_sInter_of_subset_constructibleTopologySubbasis _ (hA₁'.sdiff.union hA₂'.sdiff)
       grind [hB.prop, Set.union_subset_iff]
     · rw [← Set.sInter_union]
-      refine hB.prop.2.1 (_ ∪ F) ?_ <| (hA₁'.diff.union hA₂'.diff).union hF
-      grind [Set.diff_singleton_subset_iff, Set.union_subset_iff]
+      refine hB.prop.2.1 (_ ∪ F) ?_ <| (hA₁'.sdiff.union hA₂'.sdiff).union hF
+      grind [Set.sdiff_singleton_subset_iff, Set.union_subset_iff]
 
 instance t2Space_constructibleTopology₁ [T0Space X]
     [CompactSpace X] [QuasiSeparatedSpace X] [PrespectralSpace X] :
@@ -192,5 +192,3 @@ instance t2Space_withConstructibleTopology₁ [T0Space X]
     (constructibleTopology X) t2Space_constructibleTopology₁ WithTopology.ofTopology
     (WithTopology.ofTopology_injective (constructibleTopology X))
     (WithTopology.continuous_ofTopology (constructibleTopology X))
-      refine hB.prop.2.1 (_ ∪ F) ?_ <| (hA₁'.sdiff.union hA₂'.sdiff).union hF
-      grind [Set.sdiff_singleton_subset_iff, Set.union_subset_iff]
