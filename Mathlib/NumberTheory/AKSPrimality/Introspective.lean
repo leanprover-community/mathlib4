eb6dcaa0f1fe81dcf6e93d7518491a681f9620b1
metakunt <m@k.nt>
2026-06-13 11:33:20 +0200

    Refactor definition

--- Mathlib/NumberTheory/AKSPrimality/Introspective.lean
+++ Mathlib/NumberTheory/AKSPrimality/Introspective.lean
@@ -5,7 +5,10 @@ Authors: metakunt
 -/
 module
 
-public import Mathlib.RingTheory.RootsOfUnity.PrimitiveRoots
+public import Mathlib.Algebra.CharP.Quotient
+public import Mathlib.FieldTheory.Finite.Basic
+public import Mathlib.RingTheory.Polynomial.Cyclotomic.Basic
+
 /-!
 # Introspective relation
 
@@ -25,76 +28,160 @@ This defines the main relation for the proof as defined in their original paper.
 prime number, polynomial prime number test, AKS, Agrawal-Kayal-Saxena, Introspective
 -/
 
-
-
-variable {K : Type*} [CommRing K] [IsDomain K]
-
 @[expose] public section Introspective
 
-open Polynomial Nat
+open Polynomial Nat Ideal Ideal.Quotient
 
-/-- The introspective relation, currently only useful for the proof of the AKS primality theorem. -/
-def Introspective (f : K[X]) (n : ℕ) (r : ℕ) : Prop :=
-  ∀ μ ∈ (primitiveRoots r K), f.eval (μ ^ n) = f.eval μ ^ n
+def Introspective {n : ℕ} (f : (ZMod n)[X]) (e r : ℕ) : Prop :=
+  mk (span ({(X : (ZMod n)[X]) ^ r - C 1})) (f ^ e) =
+  mk (span ({(X : (ZMod n)[X]) ^ r - C 1})) (f.comp (X ^ e))
+
 
 namespace Introspective
 
-variable {r : ℕ}
+variable {b d r p a n e : ℕ} {f g : (ZMod n)[X]}
 
-@[simp]
-protected theorem zero {n : ℕ} {f : K[X]} : Introspective f n 0 := by
-  simp [Introspective]
+theorem dvd_of_introspective (h : Introspective f e r) (hd : p ∣ n) :
+    Introspective (f.map (ZMod.castHom hd _)) e r := by
+  simp only [Introspective] at *
+  let g := lift (span {(X : (ZMod n)[X]) ^ r - C 1}) (RingHom.comp (mk (span
+      ({(X : (ZMod p)[X]) ^ r - C 1})))  (Polynomial.mapRingHom (ZMod.castHom hd (ZMod p)))) (by
+    intro a ha
+    simp only [RingHom.coe_comp, coe_mapRingHom, Function.comp_apply]
+    apply eq_zero_iff_mem.mpr
+    simp only [Ideal.mem_span_singleton'] at *
+    obtain ⟨ b , hb ⟩ := ha
+    use b.map (ZMod.castHom hd _)
+    simp [← hb])
+  convert congrArg g h
+  · simp [g]
+  · simp only [lift_mk, RingHom.coe_comp, coe_mapRingHom, Function.comp_apply, g, map_comp]
+    congr
+    simp
 
-@[simp]
-protected theorem one {f : K[X]} : Introspective f 1 r := by
-  simp [Introspective]
-
-protected theorem X_sub_C {p a : ℕ} [ExpChar K p] : Introspective (X - C (a : K)) p r := by
+theorem aeval_of_primitive_roots {K : Type*} [CommRing K] [IsDomain K] [Algebra (ZMod n) K]
+    (h : Introspective f e r) : ∀ μ ∈ (primitiveRoots r K), f.aeval μ ^ e = f.aeval (μ ^ e) := by
   intro μ hμ
-  simp only [eval_sub, eval_X, eval_C]
-  change (frobenius K p) μ - _ = (frobenius K p) (μ - a)
+  simp only [Introspective] at h
+  let g := lift (S := K) (span ({(X : (ZMod n)[X]) ^ r - C 1})) (aeval (R := (ZMod n)) μ) (by
+    intro a ha
+    simp only [RingHom.coe_coe]
+    obtain ⟨ l , ee ⟩ := mem_span_singleton.mp ha
+    grind [aeval_sub, aeval_X, (isPrimitiveRoot_of_mem_primitiveRoots hμ).pow_eq_one])
+  exact (Iff.mp (Eq.congr (by simp [g]) (by simp [g, aeval_comp]))) (congrArg g h)
+
+@[simp]
+protected theorem one : Introspective f 1 r := by
+  simp [Introspective]
+
+protected theorem X_sub_C {a : ℕ} [Fact n.Prime] :
+    Introspective ((X : (ZMod n)[X]) - C (a : ZMod n)) n r := by
+  simp only [Introspective]
+  apply Ideal.Quotient.eq.mpr
+  convert Ideal.zero_mem _
+  suffices ((X : (ZMod n)[X]) - C (a : ZMod n)) ^ n = ((X : (ZMod n)[X]) ^ n) - C (a : ZMod n) by
+    simp_all
+  change (frobenius _ n) _ = (frobenius _ n) _ - _
   simp
 
 /-- The product of two polynomials is introspective. -/
-protected theorem mul {n : ℕ} {f g : K[X]} (hf : Introspective f n r)
-    (hg : Introspective g n r) : Introspective (f * g) n r := by
-  intro μ hm
-  simp only [eval_mul, hf μ hm, hg μ hm]
-  ring
+protected theorem mul (hf : Introspective f e r) (hg : Introspective g e r) :
+    Introspective (f * g) e r := by
+  simp_all only [Introspective, map_pow, map_mul, mul_comp]
+  rw [← hf, ← hg]
+  apply Ideal.Quotient.eq.mpr
+  convert Ideal.zero_mem _
+  grind [mul_pow]
 
-theorem of_frobenius_map {p n : ℕ} [ExpChar K p] {f : K[X]} (hm : f.map (frobenius K p) = f)
-    (hf : Introspective (f.map (frobenius K p)) (p * n) r) : Introspective f n r := by
-  intro μ hμ
-  specialize hf μ hμ
-  simp_rw [pow_mul', ← frobenius_def, eval_map_apply, hm] at hf
-  exact frobenius_inj K p hf
+theorem of_mul {m : ℕ} (h : Introspective ((X : (ZMod p)[X]) - C (a : (ZMod p))) (m * p) r)
+    [Fact p.Prime] (hcprm : p.Coprime r) :
+    Introspective ((X : (ZMod p)[X]) - C (a : (ZMod p))) m r := by
+  have hp : p.Prime := Fact.out
+  simp only [Introspective] at h ⊢
+  set g : (ZMod p)[X] := X - C (a : ZMod p)
+  have rn0 : r ≠ 0 := by grind [coprime_zero_right, prime_one_false]
+  rw [pow_mul] at h
+  haveI :  IsReduced ((ZMod p)[X] ⧸ span {(X : (ZMod p)[X]) ^ r - C 1}) := by
+    apply (isRadical_iff_quotient_reduced _).mp
+    apply (Ideal.isRadical_iff_pow_one_lt 2 (by grind)).mpr
+    intro s hs
+    rw [Ideal.mem_span_singleton] at *
+    refine (Squarefree.dvd_pow_iff_dvd ?_ (by lia)).mp hs
+    apply Separable.squarefree
+    apply separable_X_pow_sub_C
+    · apply ((cast_zero (R:= ZMod p)) ▸ (ZMod.natCast_eq_natCast_iff r 0 p).mp).mt
+      apply modEq_zero_iff_dvd.mp.mt
+      exact (Nat.Prime.coprime_iff_not_dvd hp).mp hcprm
+    · simp
+  have ddd := CharP.quotient' p (Ideal.span {(X : (ZMod p)[X]) ^ r - C 1}) (by
+    intro z hz
+    by_contra!
+    obtain ⟨ y, hy ⟩ := Ideal.mem_span_singleton'.mp hz
+    by_cases hc : y = 0
+    · grind
+    · have _ :  (z : (ZMod p)[X]).natDegree = 0 := by simp
+      have _ : r ≤ (z : (ZMod p)[X]).natDegree := by
+        rw [← hy, natDegree_mul]
+        · suffices ((X : (ZMod p)[X]) ^ r - C 1).natDegree = r by lia
+          exact natDegree_X_pow_sub_C
+        · exact hc
+        exact X_pow_sub_C_ne_zero (show 0 < r by lia) _
+      grind)
+  simp only [map_pow] at h
+  replace h : (frobenius _ p) _ = _ := h
+  have hrh : mk (span {(X : (ZMod p)[X]) ^ r - C 1}) (g.comp (X ^ (m * p))) =
+      frobenius _ p (mk (Ideal.span {(X : (ZMod p)[X]) ^ r - C 1}) (g.comp (X ^ m))) := by
+    simp only [frobenius, RingHom.coe_mk, powMonoidHom_apply]
+    rw[← map_pow]
+    congr
+    simp only [sub_comp, X_comp, g]
+    nth_rw 1 [← ZMod.pow_card (a: ZMod p)]
+    simp only [C_comp]
+    simp only [C_pow, pow_mul]
+    change (frobenius _ _) _  - (frobenius _ _) _= (frobenius _ _ ) _
+    rw [← RingHom.map_sub]
+  rw [hrh] at h
+  exact (frobenius_inj _ _) h
 
-protected theorem div {p a n : ℕ} [ExpChar K p] (h : Introspective (X - C (a : K)) n r)
-    (hd : p ∣ n) : Introspective (X - C (a : K)) (n / p) r := by
-  have _ : map (frobenius K p) (X - C (a : K)) = X - C (a : K) := by simp
-  grind [of_frobenius_map, Nat.mul_div_cancel' hd]
-
-protected theorem eval_pow {μ : K} {f : K[X]} {n : ℕ} (h : IsPrimitiveRoot μ r) (hz : r ≠ 0)
-    (hi : Introspective f n r) : f.eval (μ ^ n) = f.eval μ ^ n :=
-  hi μ ((mem_primitiveRoots (Nat.ne_zero_iff_zero_lt.mp hz)).mpr h)
+protected theorem div (h : Introspective ((X : (ZMod p)[X]) - C (a : (ZMod p))) n r) [Fact p.Prime]
+    (hd : p ∣ n) (hcprm : p.Coprime r) :
+    Introspective ((X : (ZMod p)[X]) - C (a : (ZMod p))) (n / p) r := by
+  grind [of_mul, Nat.div_mul_cancel hd]
 
 /-- The product of coprime exponents is Introspective. -/
-theorem mul_of_coprime {d e : ℕ} {f : K[X]} (hf : Introspective f e r)
+theorem mul_of_coprime {d e : ℕ} (hf : Introspective f e r)
     (hg : Introspective f d r) (h : e.Coprime r) : Introspective f (e * d) r := by
   by_cases hr : r = 0
   · grind
-  · intro μ hm
-    have mu : μ ^ e ∈ primitiveRoots r K := by
-      have hl : 0 < r := by lia
-      simp only [mem_primitiveRoots hl] at ⊢ hm
-      exact IsPrimitiveRoot.pow_of_coprime hm e h
-    rw [pow_mul, hg (μ ^ e) mu, hf μ hm]
-    ring
+  · simp only [Introspective] at *
+    set I := mk (span {(X : (ZMod n)[X]) ^ r - C 1})
+    have ⟨ w, hw ⟩ := mem_span_singleton.mp (Ideal.Quotient.eq.mp hg)
+    have hw2 := congrArg₂ comp hw (Eq.refl (X ^ e))
+    simp only [sub_comp, pow_comp, map_one, mul_comp, X_comp, one_comp, comp_assoc] at hw2
+    obtain ⟨ z, hz ⟩  : ((X : (ZMod n)[X]) ^ r - 1 ) ∣ ((X ^ e) ^ r - 1) := by
+      simp only [← pow_mul]
+      by_cases he : 1 < e
+      · exact X_pow_sub_one_mul_cyclotomic_dvd_X_pow_sub_one_of_dvd _ (by
+          simp only [mem_properDivisors, dvd_mul_left, true_and]
+          exact lt_mul_left (by lia) (by lia)) |> dvd_of_mul_right_dvd
+      -- I feel like grind should handle this case split.
+      · by_cases he2 : e = 0
+        · simp [he2]
+        · grind [dvd_refl]
+    calc
+      _ = I (f ^ e) ^ d := by simp [pow_mul]
+      _ = _ := congrArg₂ HPow.hPow hf (Eq.refl d)
+      _ = _ := by
+        simp only [pow_mul]
+        apply Ideal.Quotient.eq.mpr
+        apply mem_span_singleton.mpr
+        use z * w.comp (X ^ e)
+        grind
 
 /-- Necessary condition for the auxilliary proof. -/
-theorem of_multiset {p n b : ℕ} [ExpChar K p] (d e : ℕ) (s : Multiset (Fin b)) (hcprm : n.Coprime r)
-    (hs : ∀ x : Fin b, Introspective (ofMultiset {(x.val : K)}) n r) (hdiv : p ∣ n) :
-    Introspective (ofMultiset (s.map fun x ↦ (x.val : K))) (p ^ d * (n / p) ^ e) r := by
+theorem of_multiset (s : Multiset (Fin b)) (hcprm : n.Coprime r) [Fact p.Prime]
+    (hs : ∀ x : Fin b, Introspective (ofMultiset {(x.val : (ZMod p))}) n r) (hdiv : p ∣ n) :
+    Introspective (ofMultiset (s.map fun x ↦ (x.val : (ZMod p)))) (p ^ d * (n / p) ^ e) r := by
   simp only [ofMultiset_apply]
   have hcprm2 := Coprime.coprime_mul_right (Eq.symm (Nat.mul_div_cancel' hdiv) ▸ hcprm)
   induction s using Multiset.induction_on with
@@ -116,7 +203,7 @@ theorem of_multiset {p n b : ℕ} [ExpChar K p] (d e : ℕ) (s : Multiset (Fin b
         refine mul_of_coprime ?_ hi ?_
         · have hsx := hs x
           simp only [ofMultiset_apply, Multiset.map_singleton, Multiset.prod_singleton] at hsx
-          exact Introspective.div hsx hdiv
+          exact Introspective.div hsx hdiv hcprm2
         · exact Coprime.coprime_div_left hcprm hdiv
     · exact Coprime.pow_left d hcprm2
 
