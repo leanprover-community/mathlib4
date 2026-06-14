13c9652f889b6a2498e61c0034abc8ab2a45b849
Albert Smith <chicbrt@gmail.com>
2026-06-15 01:18:38 +0800

    doc

--- Mathlib/Algebra/Module/Submodule/Map.lean
+++ Mathlib/Algebra/Module/Submodule/Map.lean
@@ -276,13 +276,14 @@ theorem map_subtype_submoduleOf (p q : Submodule R M) :
     map q.subtype (p.submoduleOf q) = (p ⊓ q :) := by
   ext; simp [submoduleOf]
 
+/-- `p` as a submodule of `q` is isomorphic to `p ⊓ q`. -/
 def submoduleOfEquivInf (p q : Submodule R M) : p.submoduleOf q ≃ₗ[R] (p ⊓ q :) where
   toFun m := ⟨m.1, m.2, coe_mem m.1⟩
   invFun m := ⟨⟨m.1, m.2.2⟩, m.2.1⟩
   map_add' _ _ := rfl
   map_smul' _ _ := rfl
 
-/-- If `p ≤ q`, then `p` as a subgroup of `q` is isomorphic to `p`. -/
+/-- If `p ≤ q`, then `p` as a submodule of `q` is isomorphic to `p`. -/
 def submoduleOfEquivOfLe {p q : Submodule R M} (h : p ≤ q) : p.submoduleOf q ≃ₗ[R] p where
   toFun m := ⟨m.1, m.2⟩
   invFun m := ⟨⟨m.1, h m.2⟩, m.2⟩
