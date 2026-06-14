8c506d33913b90fa0786bbfc6ec32aa3d15ab572
Mai Zhang <zhangmai19@foxmail.com>
2026-06-14 12:37:08 +0800

    fix(Analysis/Convex): add missing module declaration

--- Mathlib/Analysis/Convex/ShapleyFolkman.lean
+++ Mathlib/Analysis/Convex/ShapleyFolkman.lean
@@ -3,6 +3,8 @@ Copyright (c) 2026 Mai Zhang. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mai Zhang
 -/
+module
+
 import Mathlib.Algebra.BigOperators.Field
 import Mathlib.Analysis.Convex.Caratheodory
 import Mathlib.Data.Real.Basic
