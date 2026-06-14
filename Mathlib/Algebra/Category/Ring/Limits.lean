670d3eb5abae5b27b30df7fdcd3286bf0548c1be
Jacob Reinhold <5241441+jcreinhold@users.noreply.github.com>
2026-06-14 06:29:39 -0400

    feat(Ring/Limits): simplify instance definition for forget₂CommMon_preservesLimits

--- Mathlib/Algebra/Category/Ring/Limits.lean
+++ Mathlib/Algebra/Category/Ring/Limits.lean
@@ -535,14 +535,11 @@ instance forget_preservesLimitsOfSize [UnivLE.{v, u}] :
 instance forget_preservesLimits : PreservesLimits (forget CommRingCat.{u}) :=
   CommRingCat.forget_preservesLimitsOfSize.{u, u}
 
-instance forget₂CommMon_preservesLimits :
-    PreservesLimits (forget₂ CommRingCat CommMonCat.{u}) := by
-  have : ReflectsLimitsOfSize.{u, u} (forget CommMonCat.{u}) :=
+instance forget₂CommMon_preservesLimits :  PreservesLimits (forget₂ CommRingCat CommMonCat) :=
+  have : ReflectsLimitsOfSize (forget CommMonCat) :=
     reflectsLimits_of_reflectsIsomorphisms
-  have : PreservesLimitsOfSize.{u, u}
-      (forget₂ CommRingCat CommMonCat.{u} ⋙ forget CommMonCat.{u}) :=
+  have : PreservesLimitsOfSize (forget₂ CommRingCat CommMonCat ⋙ forget CommMonCat) :=
     preservesLimits_of_natIso (eqToIso HasForget₂.forget_comp).symm
-  exact preservesLimits_of_reflects_of_preserves
-    (forget₂ CommRingCat CommMonCat.{u}) (forget CommMonCat.{u})
+  preservesLimits_of_reflects_of_preserves (forget₂ CommRingCat CommMonCat) (forget CommMonCat)
 
 end CommRingCat
