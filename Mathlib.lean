cf0700c243aca3cf5b1841785617c6ff6dadbcd3
tb65536 <thomas.l.browning@gmail.com>
2026-06-15 12:08:01 +0100

    move

--- Mathlib.lean
+++ Mathlib.lean
@@ -3550,7 +3550,7 @@ public import Mathlib.Combinatorics.Hall.Basic
 public import Mathlib.Combinatorics.Hall.Finite
 public import Mathlib.Combinatorics.Hindman
 public import Mathlib.Combinatorics.Hypergraph.Basic
-public import Mathlib.Combinatorics.Hypergraph.Coloring
+public import Mathlib.Combinatorics.Hypergraph.Coloring.Edge
 public import Mathlib.Combinatorics.KatonaCircle
 public import Mathlib.Combinatorics.Matroid.Basic
 public import Mathlib.Combinatorics.Matroid.Circuit
