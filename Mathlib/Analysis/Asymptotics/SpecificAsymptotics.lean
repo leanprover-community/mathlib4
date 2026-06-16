9854f74b9774a77208828a9ed2b47aea0ef8675a
Stefan Kebekus <kebekus@users.noreply.github.com>
2026-06-16 14:02:24 +0200

    Apply suggestions from code review
    Co-authored-by: Chris Henson <46805207+chenson2018@users.noreply.github.com>

--- Mathlib/Analysis/Asymptotics/SpecificAsymptotics.lean
+++ Mathlib/Analysis/Asymptotics/SpecificAsymptotics.lean
@@ -230,21 +230,20 @@ theorem Continuous.isBounded_range_iff_isBigO_atTop_atBot {f : ℝ → E} (hf : 
     all_goals
     · rw [isBigO_iff]
       obtain ⟨C, hC⟩ := H.exists_pos_norm_le
-      exact ⟨C, by aesop⟩
+      exact ⟨C, by simp_all⟩
   · obtain ⟨C₁, M₁, hC₁⟩ : ∃ C₁ M₁, ∀ x ≥ M₁, ‖f x‖ ≤ C₁ := by simp_all [isBigO_iff]
     obtain ⟨C₂, M₂, hC₂⟩ : ∃ C₂ M₂, ∀ x ≤ M₂, ‖f x‖ ≤ C₂ := by simp_all [isBigO_iff]
     obtain ⟨C₃, hC₃⟩ : ∃ C₃, ∀ x ∈ Icc M₂ M₁, ‖f x‖ ≤ C₃ :=
       isCompact_Icc.exists_bound_of_continuousOn hf.continuousOn
     rw [isBounded_iff_forall_norm_le]
-    refine ⟨max C₁ (max C₂ C₃), forall_mem_range.2 fun x ↦ ?_⟩
-    rcases le_total x M₁ with hx₁ | hx₁ <;> rcases le_total x M₂ with hx₂ | hx₂ <;> aesop
+    exact ⟨max C₁ (max C₂ C₃), by grind⟩
 
 /-- An even function that is `O(1)` at `atTop` is also `O(1)` at `atBot`. -/
 theorem Function.Even.isBigO_atBot_of_isBigO_atTop {f : ℝ → E} (heven : Function.Even f)
     (h : f =O[atTop] (1 : ℝ → ℝ)) : f =O[atBot] (1 : ℝ → ℝ) := by
-  simp_all only [isBigO_iff, Pi.one_apply, norm_one, mul_one, eventually_atTop, eventually_atBot]
-  obtain ⟨c, a, h⟩ := h
-  exact ⟨c, -a, fun b hb ↦ by simpa [heven b] using h (-b) (by linarith)⟩
+  obtain ⟨c, a, h⟩ : ∃ c a, ∀ b, a ≤ b → ‖f b‖ ≤ c := by simpa [isBigO_iff] using h
+  simp only [isBigO_iff, Pi.one_apply, norm_one, eventually_atBot]
+  exact ⟨c, -a, by grind [Function.Even]⟩
 
 /-- A continuous even function `f : ℝ → E` has bounded range if and only if `f =O[atTop] 1`. -/
 theorem Continuous.isBounded_range_iff_isBigO_atTop_of_even {f : ℝ → E} (hf : Continuous f)
