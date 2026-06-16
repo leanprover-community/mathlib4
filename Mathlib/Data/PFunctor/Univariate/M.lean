87664f53f6bf4b4551580dbdc74e5dda86046d8c
nrs <32944119+nrs-status@users.noreply.github.com>
2026-06-16 17:46:50 +0000

    Update the example's declarations with descriptions

--- Mathlib/Data/PFunctor/Univariate/M.lean
+++ Mathlib/Data/PFunctor/Univariate/M.lean
@@ -15,17 +15,20 @@ as the greatest fixpoint of a polynomial functor.
 
 Example usage:
 ```
+/-- Shape type for the polynomial functor of lists and streams. -/
 inductive ListA (α : Type u)
 | nil
 | cons : α -> ListA α
 deriving DecidableEq, Repr
 
+-- Polynomial functor for lists. Its smallest fixpoint, (PFunctor.W ·), produces the usual inductive type for lists; its greatest fixpoint, (PFunctor.M ·), produces the coinductive type of streams.
 def ListPFunctor.{u} (α : Type u) : PFunctor.{u, u} where
   A := ListA α
   B
   | .nil => PEmpty
   | .cons _ => PUnit
 
+-- A stream whose ith value is the ith natural number. 
 def range : (ListPFunctor Nat).M :=
   PFunctor.M.corec
     (X := Nat)
@@ -37,18 +40,13 @@ info: [ListA.cons 0,
  ListA.cons 1,
  ListA.cons 2,
  ListA.cons 3,
- ListA.cons 4,
- ListA.cons 5,
- ListA.cons 6,
- ListA.cons 7,
- ListA.cons 8,
- ListA.cons 9]
+ ListA.cons 4]
 -/
 #guard_msgs in
 #eval List.iterate (fun x =>
   match h : x.head with
   | .nil => .mk ⟨.nil, nofun⟩ -- not reached in this example
-  | .cons _ => x.children (h ▸ .unit)) range 10
+  | .cons _ => x.children (h ▸ .unit)) range 5
   |>.map (·.head)
 ```
 -/
