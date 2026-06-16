c3d69a7a9937a3e2dfa90de333cd8f2d81e8153b
Terence Tao <tao@math.ucla.edu>
2026-06-16 11:04:45 -0700

    restore accidental comment end tag deletion

--- Mathlib/Analysis/SumIntegralComparisons.lean
+++ Mathlib/Analysis/SumIntegralComparisons.lean
@@ -2,6 +2,7 @@
 Copyright (c) 2022 Kevin H. Wilson. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kevin H. Wilson
+-/
 module
 
 public import Mathlib.MeasureTheory.Integral.IntervalIntegral.Basic
