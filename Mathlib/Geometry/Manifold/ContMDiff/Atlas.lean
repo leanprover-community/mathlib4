58c0154f6efbbe3ba62b7fdcea8335d73e52cfad
Michael Rothgang <rothgang@math.uni-bonn.de>
2026-06-16 14:07:06 +0200

    Rename; make I explicit; deprecate

--- Mathlib/Geometry/Manifold/ContMDiff/Atlas.lean
+++ Mathlib/Geometry/Manifold/ContMDiff/Atlas.lean
@@ -45,17 +45,20 @@ variable {𝕜 : Type*} [NontriviallyNormedField 𝕜]
 
 section Atlas
 
-theorem contMDiff_model : ContMDiff I 𝓘(𝕜, E) n I := by
+variable (I) in
+theorem ModelWithCorners.contMDiff : ContMDiff I 𝓘(𝕜, E) n I := by
   intro x
   refine contMDiffAt_iff.mpr ⟨I.continuousAt, ?_⟩
   simpa using contDiffWithinAt_id.congr (fun y hy ↦ by simp [hy]) (by simp)
-alias ModelWithCorners.contMDiff := contMDiff_model -- XXX: which one is better?
+@[deprecated (since := "2026-06-16")] alias contMDiff_model := ModelWithCorners.contMDiff
 
-theorem contMDiffOn_model_symm : ContMDiffOn 𝓘(𝕜, E) I n I.symm (range I) := by
+variable (I) in
+theorem ModelWithCorners.contMDiffOn_symm : ContMDiffOn 𝓘(𝕜, E) I n I.symm (range I) := by
   intro x hx
   apply contMDiffWithinAt_iff.mpr ⟨by fun_prop, ?_⟩
   simpa using contDiffWithinAt_id.congr (fun y hy ↦ by simp [hy]) (by simp [hx])
-alias ModelWithCorners.contMDiffOn_symm := contMDiffOn_model_symm -- XXX: which one is better?
+@[deprecated (since := "2026-06-16")]
+alias contMDiffOn_model_symm := ModelWithCorners.contMDiffOn_symm
 
 /-- An atlas member is `C^n` for any `n`. -/
 theorem contMDiffOn_of_mem_maximalAtlas (h : e ∈ maximalAtlas I n M) :
@@ -87,7 +90,7 @@ theorem contMDiffOn_chart_symm [IsManifold I n M] :
 
 theorem contMDiffAt_extend {x : M} (he : e ∈ maximalAtlas I n M) (hx : x ∈ e.source) :
     ContMDiffAt I 𝓘(𝕜, E) n (e.extend I) x :=
-  (contMDiff_model _).comp x <| contMDiffAt_of_mem_maximalAtlas he hx
+  (I.contMDiff _).comp x <| contMDiffAt_of_mem_maximalAtlas he hx
 
 theorem contMDiffOn_extend (he : e ∈ maximalAtlas I n M) :
     ContMDiffOn I 𝓘(𝕜, E) n (e.extend I) e.source :=
@@ -110,7 +113,7 @@ theorem contMDiffOn_extChartAt [IsManifold I n M] :
 theorem contMDiffOn_extend_symm (he : e ∈ maximalAtlas I n M) :
     ContMDiffOn 𝓘(𝕜, E) I n (e.extend I).symm (I '' e.target) := by
   refine (contMDiffOn_symm_of_mem_maximalAtlas he).comp
-    (contMDiffOn_model_symm.mono <| image_subset_range _ _) ?_
+    (I.contMDiffOn_symm.mono <| image_subset_range _ _) ?_
   simp_rw [image_subset_iff, PartialEquiv.restr_coe_symm, I.toPartialEquiv_coe_symm,
     preimage_preimage, I.left_inv, preimage_id']; rfl
 
