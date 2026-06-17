3c95ef89cceb932dce69174763d31e64b72b6dd5
AntoineduFresne <antoine@du-fresne.ch>
2026-06-18 00:22:08 +0200

    update

--- Mathlib/Order/Dilworth.lean
+++ Mathlib/Order/Dilworth.lean
@@ -4,6 +4,8 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Antoine du Fresne von Hohenesche
 -/
 
+module
+
 import Mathlib.Order.Antichain
 import Mathlib.Order.Preorder.Chain
 import Mathlib.Order.Preorder.Finite
