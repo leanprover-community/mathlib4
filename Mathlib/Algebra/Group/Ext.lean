1927358d83a02fcec7a5bf43d7eae946e22e3967
Yakov Pechersky <ffxen158@gmail.com>
2026-06-18 15:09:19 -0400

    Ext.lean

--- Mathlib/Algebra/Group/Ext.lean
+++ Mathlib/Algebra/Group/Ext.lean
@@ -71,7 +71,7 @@ theorem Monoid.ext {M : Type u} ⦃m₁ m₂ : Monoid M⦄
   have : m₁.toMulOneClass = m₂.toMulOneClass := MulOneClass.ext h_mul
   have h₁ : m₁.one = m₂.one := congr_arg (·.one) (this)
   have h₂ : m₁.toSemigroup = m₂.toSemigroup := Semigroup.ext h_mul
-  let f : @MonoidHom M M m₁.toMulOneClass m₂.toMulOneClass :=
+  let f : @MonoidHom M M m₁.toMulOne m₂.toMulOne :=
     @MonoidHom.mk _ _ (_) _ (@OneHom.mk _ _ (_) _ id h₁)
       (fun x y => congr_fun (congr_fun h_mul x) y)
   have : m₁.npow = m₂.npow := by
