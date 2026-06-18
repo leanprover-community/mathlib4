ca5515acedeb8a8dedc2872589680b4761fd3adb
teorth <tao@math.ucla.edu>
2026-06-18 08:06:48 -0700

    Update Mathlib/Analysis/Complex/ExponentialBounds.lean
    Co-authored-by: Monica Omar <23701951+themathqueen@users.noreply.github.com>

--- Mathlib/Analysis/Complex/ExponentialBounds.lean
+++ Mathlib/Analysis/Complex/ExponentialBounds.lean
@@ -124,7 +124,6 @@ theorem log_five_gt_d9 : 1.6094379123 < log 5 :=
 theorem log_five_lt_d9 : log 5 < 1.6094379126 :=
   lt_of_le_of_lt (sub_le_iff_le_add.1 (abs_sub_le_iff.1 log_five_near_10).1) (by norm_num)
 
-theorem log_ten_eq : log 10 = log 2 + log 5 := by
-  norm_num [← log_mul]
+theorem log_ten_eq : log 10 = log 2 + log 5 := by norm_num [← log_mul]
 
 end Real
