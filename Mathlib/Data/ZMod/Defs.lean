ea0af546583fed948363511b14c8a79a4d21cc1e
Yakov Pechersky <ffxen158@gmail.com>
2026-06-18 15:29:44 -0400

    fix zmod

--- Mathlib/Data/ZMod/Defs.lean
+++ Mathlib/Data/ZMod/Defs.lean
@@ -205,16 +205,16 @@ instance commRing (n : ℕ) : CommRing (ZMod n) where
     ((inferInstance : CommRing ℤ)).nsmul_zero
     fun n => ((inferInstance : CommRing (Fin n.succ))).nsmul_zero
   nsmul_succ := Nat.casesOn n
-    (inferInstanceAs (CommRing ℤ)).nsmul_succ
-    fun n => (inferInstanceAs (CommRing (Fin n.succ))).nsmul_succ
+    (inferInstance : CommRing ℤ).nsmul_succ
+    fun n => (inferInstance : CommRing (Fin n.succ)).nsmul_succ
   psmul := Nat.casesOn n
-    (inferInstanceAs (CommRing ℤ)).psmul fun n => (inferInstanceAs (CommRing (Fin n.succ))).psmul
+    (inferInstance : CommRing ℤ).psmul fun n => (inferInstance : CommRing (Fin n.succ)).psmul
   psmul_one := Nat.casesOn n
-    (inferInstanceAs (CommRing ℤ)).psmul_one
-    fun n => (inferInstanceAs (CommRing (Fin n.succ))).psmul_one
+    (inferInstance : CommRing ℤ).psmul_one
+    fun n => (inferInstance: CommRing (Fin n.succ)).psmul_one
   psmul_succ := Nat.casesOn n
-    (inferInstanceAs (CommRing ℤ)).psmul_succ
-    fun n => (inferInstanceAs (CommRing (Fin n.succ))).psmul_succ
+    (inferInstance : CommRing ℤ).psmul_succ
+    fun n => (inferInstance : CommRing (Fin n.succ)).psmul_succ
   neg_add_cancel := Nat.casesOn n (@neg_add_cancel Int _) fun n => @neg_add_cancel (Fin n.succ) _
   add_comm := Nat.casesOn n (@add_comm Int _) fun n => @add_comm (Fin n.succ) _
   mul := Nat.casesOn n (@Mul.mul Int _) fun n => @Mul.mul (Fin n.succ) _
@@ -241,16 +241,16 @@ instance commRing (n : ℕ) : CommRing (ZMod n) where
     ((inferInstance : CommRing ℤ)).npow_zero
     fun n => ((inferInstance : CommRing (Fin n.succ))).npow_zero
   npow_succ := Nat.casesOn n
-    (inferInstanceAs (CommRing ℤ)).npow_succ
-    fun n => (inferInstanceAs (CommRing (Fin n.succ))).npow_succ
+    (inferInstance : CommRing ℤ).npow_succ
+    fun n => (inferInstance : CommRing (Fin n.succ)).npow_succ
   ppow := Nat.casesOn n
-    (inferInstanceAs (CommRing ℤ)).ppow fun n => (inferInstanceAs (CommRing (Fin n.succ))).ppow
+    (inferInstance : CommRing ℤ).ppow fun n => (inferInstance : CommRing (Fin n.succ)).ppow
   ppow_one := Nat.casesOn n
-    (inferInstanceAs (CommRing ℤ)).ppow_one
-    fun n => (inferInstanceAs (CommRing (Fin n.succ))).ppow_one
+    (inferInstance : CommRing ℤ).ppow_one
+    fun n => (inferInstance : CommRing (Fin n.succ)).ppow_one
   ppow_succ := Nat.casesOn n
-    (inferInstanceAs (CommRing ℤ)).ppow_succ
-    fun n => (inferInstanceAs (CommRing (Fin n.succ))).ppow_succ
+    (inferInstance : CommRing ℤ).ppow_succ
+    fun n => (inferInstance : CommRing (Fin n.succ)).ppow_succ
 
 instance inhabited (n : ℕ) : Inhabited (ZMod n) :=
   ⟨0⟩
