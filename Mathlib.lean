b964a070814569bbe8c00618ff007da737a8b6ca
WilliamCoram <williamecoram@gmail.com>
2026-06-19 13:52:58 +0100

    add torsor added?

--- Mathlib.lean
+++ Mathlib.lean
@@ -7627,6 +7627,7 @@ public import Mathlib.Topology.Algebra.Order.UpperLower
 public import Mathlib.Topology.Algebra.Polynomial
 public import Mathlib.Topology.Algebra.PontryaginDual
 public import Mathlib.Topology.Algebra.PowerBounded
+public import Mathlib.Topology.Algebra.ProperAction.AddTorsor
 public import Mathlib.Topology.Algebra.ProperAction.Basic
 public import Mathlib.Topology.Algebra.ProperAction.CompactlyGenerated
 public import Mathlib.Topology.Algebra.ProperAction.ProperlyDiscontinuous
