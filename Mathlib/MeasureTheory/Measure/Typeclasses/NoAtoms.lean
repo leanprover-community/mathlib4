4271fb82491c591056d379f80c17b61e5d881cfb
Leo Diedering <leo.diedering@web.de>
2026-06-19 15:57:07 +0200

    Add deprecation warning for NoAtoms.lean

---
+++ Mathlib/MeasureTheory/Measure/Typeclasses/NoAtoms.lean
@@ -0,0 +1,7 @@
+/-! # NoAtoms
+This file is deprecated. Please use `Mathlib.MeasureTheory.Measure.Typeclasses.NullSingletonClass`
+instead.
+-/
+
+deprecated_module "use Mathlib.MeasureTheory.Measure.Typeclasses.NullSingletonClass instead"
+  (since := "2026-06-19")
