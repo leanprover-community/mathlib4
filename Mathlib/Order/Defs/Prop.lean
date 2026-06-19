c1f0b96fb9d9ee62f297b99777bf25695ff04b64
Snir Broshi <26556598+SnirBroshi@users.noreply.github.com>
2026-06-19 20:46:08 +0300

    better copyright header

--- Mathlib/Order/Defs/Prop.lean
+++ Mathlib/Order/Defs/Prop.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2026 Snir Broshi. All rights reserved.
+Copyright (c) 2014 Jeremy Avigad. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Snir Broshi
+Authors: Jeremy Avigad, Mario Carneiro, Johannes Hölzl, Yury Kudryashov
 -/
 module
 
