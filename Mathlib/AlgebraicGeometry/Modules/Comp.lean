779f7354b82e129deeb0625024dedd90658d3a59
Brian-Nugent <bnugent@uw.edu>
2026-06-21 13:33:50 -0600

    More finagling

--- Mathlib/AlgebraicGeometry/Modules/Comp.lean
+++ Mathlib/AlgebraicGeometry/Modules/Comp.lean
@@ -34,6 +34,93 @@ abbrev extendScalarsCompTildeFunctorIso :
 
 variable (S : Submonoid R)
 
+open scoped ModuleCat.Algebra in
 def localizedModuleFunctorExtendScalarsIso :
-    ModuleCat.localizedModuleFunctor S ≅
-      ModuleCat.extendScalars (algebraMap R (Localization S)) := sorry
+    ModuleCat.localizedModuleFunctor.{u} S ≅
+      ModuleCat.extendScalars (algebraMap R (Localization S)) := by
+  letI : IsScalarTower R (Localization S)
+      ((ModuleCat.restrictScalars (algebraMap R (Localization S))).obj
+        (ModuleCat.of (Localization S) (Localization S))) :=
+    IsScalarTower.of_algebraMap_smul fun _ _ => rfl
+  let e (M : ModuleCat.{u} R) :
+      (ModuleCat.localizedModuleFunctor S).obj M ≃ₗ[Localization S]
+        (ModuleCat.extendScalars (algebraMap R (Localization S))).obj M :=
+    (Shrink.linearEquiv (Localization S) (LocalizedModule S M)).trans <|
+      (LocalizedModule.equivTensorProduct S M).trans <|
+        (TensorProduct.AlgebraTensorModule.congr (LinearEquiv.refl _ _) (LinearEquiv.refl R M) :
+          TensorProduct R (Localization S) M ≃ₗ[Localization S]
+            (ModuleCat.extendScalars (algebraMap R (Localization S))).obj M)
+  have e_apply_mk (M : ModuleCat.{u} R) (m : M) :
+      e M (M.localizedModuleMkLinearMap S m) =
+        (show (ModuleCat.extendScalars (algebraMap R (Localization S))).obj M from
+        (TensorProduct.mk R
+          ((ModuleCat.restrictScalars (algebraMap R (Localization S))).obj
+            (ModuleCat.of (Localization S) (Localization S))) M
+          (show (ModuleCat.restrictScalars (algebraMap R (Localization S))).obj
+            (ModuleCat.of (Localization S) (Localization S)) from (1 : Localization S))) m) := by
+    dsimp [e, ModuleCat.localizedModuleMkLinearMap]
+    have hshrink :
+        (Shrink.linearEquiv (Localization S) (LocalizedModule S M))
+          (((Shrink.linearEquiv R (LocalizedModule S M)).symm.toLinearMap.comp
+            (LocalizedModule.mkLinearMap S M)) m) =
+          (LocalizedModule.mkLinearMap S M) m := by
+      change (equivShrink (LocalizedModule S M)).symm
+          (equivShrink (LocalizedModule S M) ((LocalizedModule.mkLinearMap S M) m)) =
+        (LocalizedModule.mkLinearMap S M) m
+      simp
+    change
+      (show (ModuleCat.extendScalars (algebraMap R (Localization S))).obj M from
+      ((TensorProduct.AlgebraTensorModule.congr (LinearEquiv.refl _ _) (LinearEquiv.refl R M) :
+          TensorProduct R (Localization S) M ≃ₗ[Localization S]
+            (ModuleCat.extendScalars (algebraMap R (Localization S))).obj M))
+        ((LocalizedModule.equivTensorProduct S M)
+          ((Shrink.linearEquiv (Localization S) (LocalizedModule S M))
+            (((Shrink.linearEquiv R (LocalizedModule S M)).symm.toLinearMap.comp
+              (LocalizedModule.mkLinearMap S M)) m)))) =
+      (show (ModuleCat.extendScalars (algebraMap R (Localization S))).obj M from
+      (TensorProduct.mk R
+        ((ModuleCat.restrictScalars (algebraMap R (Localization S))).obj
+          (ModuleCat.of (Localization S) (Localization S))) M
+        (show (ModuleCat.restrictScalars (algebraMap R (Localization S))).obj
+          (ModuleCat.of (Localization S) (Localization S)) from (1 : Localization S))) m)
+    rw [hshrink]
+    simp [Localization.mk_one_eq_algebraMap, LinearEquiv.refl_apply]
+    rfl
+  refine NatIso.ofComponents (fun M => LinearEquiv.toModuleIso <| e M) ?_
+  intro X Y g
+  letI : Module R ((ModuleCat.localizedModuleFunctor S).obj X) :=
+    inferInstanceAs (Module R (X.localizedModule S))
+  letI : IsScalarTower R (Localization S) ((ModuleCat.localizedModuleFunctor S).obj X) :=
+    inferInstanceAs (IsScalarTower R (Localization S) (X.localizedModule S))
+  letI : Module R ((ModuleCat.localizedModuleFunctor S).obj Y) :=
+    inferInstanceAs (Module R (Y.localizedModule S))
+  letI : IsScalarTower R (Localization S) ((ModuleCat.localizedModuleFunctor S).obj Y) :=
+    inferInstanceAs (IsScalarTower R (Localization S) (Y.localizedModule S))
+  letI : Module R ((ModuleCat.extendScalars (algebraMap R (Localization S))).obj Y) :=
+    Module.compHom _ (algebraMap R (Localization S))
+  letI : IsScalarTower R (Localization S)
+      ((ModuleCat.extendScalars (algebraMap R (Localization S))).obj Y) :=
+    IsScalarTower.of_algebraMap_smul fun _ _ => rfl
+  let targetMap : Y →ₗ[R]
+      (ModuleCat.extendScalars (algebraMap R (Localization S))).obj Y :=
+    ((e Y).restrictScalars R).toLinearMap.comp (Y.localizedModuleMkLinearMap S)
+  haveI : IsLocalizedModule S targetMap := by
+    dsimp [targetMap]
+    exact IsLocalizedModule.of_linearEquiv
+      (S := S) (f := Y.localizedModuleMkLinearMap S) ((e Y).restrictScalars R)
+  apply ModuleCat.hom_ext
+  apply LinearMap.restrictScalars_injective R
+  apply IsLocalizedModule.linearMap_ext S (X.localizedModuleMkLinearMap S) targetMap
+  ext m
+  change (e Y)
+      (((ModuleCat.localizedModuleFunctor S).map g) (X.localizedModuleMkLinearMap S m)) =
+    ((ModuleCat.extendScalars (algebraMap R (Localization S))).map g)
+      ((e X) (X.localizedModuleMkLinearMap S m))
+  simp only [ModuleCat.localizedModuleFunctor, ModuleCat.localizedModuleMap,
+    ModuleCat.hom_ofHom, IsLocalizedModule.mapExtendScalars_apply_apply,
+    IsLocalizedModule.map_apply]
+  change (e Y) (show Y.localizedModule S from Y.localizedModuleMkLinearMap S (g m)) =
+    ((ModuleCat.extendScalars (algebraMap R (Localization S))).map g)
+      ((e X) (show X.localizedModule S from X.localizedModuleMkLinearMap S m))
+  rw [e_apply_mk Y (g m), e_apply_mk X m]
+  rfl
