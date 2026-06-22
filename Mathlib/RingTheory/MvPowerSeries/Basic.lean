9eb370c41a1a8f6e36728bd370169d8180449252
Justus Springer <50165510+justus-springer@users.noreply.github.com>
2026-06-22 15:28:10 +0100

    Apply suggestions from code review
    Co-authored-by: Noah Walker <noahwalker3.14@gmail.com>

--- Mathlib/RingTheory/MvPowerSeries/Basic.lean
+++ Mathlib/RingTheory/MvPowerSeries/Basic.lean
@@ -354,7 +354,7 @@ theorem coeff_zero_C (a : R) : coeff (0 : σ →₀ ℕ) (C a) = a :=
 theorem coeff_C_of_ne_zero {n : σ →₀ ℕ} (h : n ≠ 0) {a : R} : coeff n (C a) = 0 := by
   classical rw [coeff_C, if_neg h]
 
--- The inteded use case of this theorem is for `m = 1` (often useful for `pderiv`).
+-- The intended use case of this theorem is for `m = 1` (often useful for `pderiv`).
 @[simp]
 theorem coeff_add_single_C {m : ℕ} [NeZero m] {n : σ →₀ ℕ} {a : R} {i : σ} :
     coeff (n + single i m) (C a) = 0 :=
