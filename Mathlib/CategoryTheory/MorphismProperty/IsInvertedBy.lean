96b8d7404c40ba4be45032ab750f42d14c5eae18
Joël Riou <37772949+joelriou@users.noreply.github.com>
2026-06-22 13:41:13 +0200

    Update Mathlib/CategoryTheory/MorphismProperty/IsInvertedBy.lean
    Co-authored-by: Christian Merten <christian@merten.dev>

--- Mathlib/CategoryTheory/MorphismProperty/IsInvertedBy.lean
+++ Mathlib/CategoryTheory/MorphismProperty/IsInvertedBy.lean
@@ -173,7 +173,7 @@ lemma IsInvertedBy.map_iff {C₁ C₂ C₃ : Type*} [Category* C₁] [Category* 
     (W.map F).IsInvertedBy G ↔ W.IsInvertedBy (F ⋙ G) := by
   simp only [IsInvertedBy.iff_map_le_isomorphisms, map_map]
 
-lemma isomorphisms_isInvertedBy (F : C ⥤ D) : (isomorphisms C).IsInvertedBy F := by
+lemma isInvertedBy_isomorphisms (F : C ⥤ D) : (isomorphisms C).IsInvertedBy F := by
   intro _ _ _ hf
   simp only [isomorphisms.iff] at hf
   infer_instance
