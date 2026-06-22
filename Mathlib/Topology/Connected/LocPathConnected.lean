ce497500e536d7ab3f08bfe13a377a8577506594
Batixx <s59fpern@uni-bonn.de>
2026-06-22 05:13:35 +0200

    shake

--- Mathlib/Topology/Connected/LocPathConnected.lean
+++ Mathlib/Topology/Connected/LocPathConnected.lean
@@ -3,7 +3,7 @@ Copyright (c) 2020 Patrick Massot. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Patrick Massot, Ben Eltschig
 -/
-module
+module  -- shake: keep-all
 
 public import Mathlib.Topology.Connected.LocallyPathConnected
 
