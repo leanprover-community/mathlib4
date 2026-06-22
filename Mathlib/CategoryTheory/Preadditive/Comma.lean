7233ec9aabf4a89cfdbfc1a06f435cc9fb6f6b53
morel <sophie.morel@ens-lyon.fr>
2026-06-22 14:46:57 +0200

    feedback

--- Mathlib/CategoryTheory/Preadditive/Comma.lean
+++ Mathlib/CategoryTheory/Preadditive/Comma.lean
@@ -37,32 +37,17 @@ variable {u v : Comma L R}
 
 section Comma
 
-instance : Add (u ⟶ v) where
+@[simps!]
+instance CommaMorphismAdd : Add (u ⟶ v) where
   add α β := CommaMorphism.mk (α.left + β.left) (α.right + β.right) (by simp)
 
-@[simp]
-lemma CommMorphism.add_left (α β : u ⟶ v) : (α + β).left = α.left + β.left := rfl
-
-@[simp]
-lemma CommaMorphism.add_right (α β : u ⟶ v) : (α + β).right = α.right + β.right := rfl
-
-instance : Zero (u ⟶ v) where
+@[simps!]
+instance CommaMorphismZero : Zero (u ⟶ v) where
   zero := CommaMorphism.mk 0 0
 
-@[simp]
-lemma CommaMorphism.zero_left : (0 : u ⟶ v).left = 0 := rfl
-
-@[simp]
-lemma CommaMorphism.zero_right : (0 : u ⟶ v).right = 0 := rfl
-
-instance : Neg (u ⟶ v) where
+@[simps!]
+instance CommaMorphismNeg : Neg (u ⟶ v) where
   neg α := CommaMorphism.mk (- α.left) (- α.right)
-
-@[simp]
-lemma CommaMorphism.neg_left (α : u ⟶ v) : (- α).left = - α.left := rfl
-
-@[simp]
-lemma CommaMorphism.neg_right (α : u ⟶ v) : (- α).right = - α.right := rfl
 
 instance : AddCommGroup (u ⟶ v) where
   add_assoc _ _ _ := by ext <;> simp [add_assoc]
@@ -70,13 +55,24 @@ instance : AddCommGroup (u ⟶ v) where
   add_zero _ := by cat_disch
   add_comm _ _ := by ext <;> simp [add_comm]
   neg_add_cancel _ := by cat_disch
-  nsmul := nsmulRec
-  zsmul := zsmulRec
+  nsmul n α:= CommaMorphism.mk (n • α.left) (n • α.right)
+    (by simp [Functor.map_nsmul, Preadditive.comp_nsmul, Preadditive.nsmul_comp])
+  zsmul n α := CommaMorphism.mk (n • α.left) (n • α.right)
+    (by simp [Functor.map_zsmul, Preadditive.comp_zsmul, Preadditive.zsmul_comp])
+  nsmul_zero := by cat_disch
+  nsmul_succ _ _ := by ext <;> dsimp <;> simp [add_nsmul]
+  zsmul_zero' := by cat_disch
+  zsmul_succ' _ _ := by ext <;> dsimp <;> simp [add_zsmul]
+  zsmul_neg' _ _ := by ext <;> dsimp <;> simp [add_nsmul, add_zsmul]
 
 /-- If we have additive functors `L : A ⥤ T` and `R : B ⥤ T` between preadditive categories,
 then the category `Comma L R` is preadditive.
 -/
 instance : Preadditive (Comma L R) where
+
+instance : (Comma.fst L R).Additive where
+
+instance : (Comma.snd L R).Additive where
 
 end Comma
 
@@ -85,6 +81,12 @@ section Arrow
 /-- If a category `T` is preadditive, then so is its category of arrows.
 -/
 instance : Preadditive (Arrow T) := inferInstanceAs (Preadditive (Comma (𝟭 T) (𝟭 T)))
+
+instance : (Arrow.leftFunc (C := T)).Additive :=
+  inferInstanceAs ((Comma.fst (𝟭 T) (𝟭 T))).Additive
+
+instance : (Arrow.rightFunc (C := T)).Additive :=
+  inferInstanceAs ((Comma.snd (𝟭 T) (𝟭 T))).Additive
 
 variable {u v : Arrow T}
 
