8c1bb5b5f11a80c235cc8662ea81c189d2ab8d2b
Ondřej Čertík <ondrej@certik.us>
2026-06-23 16:45:24 -0600

    Update Mathlib/MeasureTheory/Integral/IntervalIntegral/LebesgueDifferentiationThm.lean
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- Mathlib/MeasureTheory/Integral/IntervalIntegral/LebesgueDifferentiationThm.lean
+++ Mathlib/MeasureTheory/Integral/IntervalIntegral/LebesgueDifferentiationThm.lean
@@ -67,7 +67,7 @@ theorem IntervalIntegrable.ae_hasDerivAt_integral {E : Type*} [NormedAddCommGrou
     (hf : IntervalIntegrable f volume a b) :
     ∀ᵐ x, x ∈ uIcc a b → ∀ c ∈ uIcc a b, HasDerivAt (fun x => ∫ (t : ℝ) in c..x, f t) (f x) x := by
   wlog hab : a ≤ b
-  · exact uIcc_comm b a ▸ @this E _ _ _ f b a hf.symm (by linarith)
+  · exact uIcc_comm b a ▸ this hf.symm (by linarith)
   rw [uIcc_of_le hab]
   have h₁ : ∀ᵐ x, x ≠ a := by simp [ae_iff, measure_singleton]
   have h₂ : ∀ᵐ x, x ≠ b := by simp [ae_iff, measure_singleton]
