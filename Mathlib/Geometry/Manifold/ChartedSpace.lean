611350d0f2307ba8559b6e0ffc79edb7d90dcf0d
bergschaf <christian.k@netcom-mail.de>
2026-06-23 08:53:28 +0200

    add explicit H

--- Mathlib/Geometry/Manifold/ChartedSpace.lean
+++ Mathlib/Geometry/Manifold/ChartedSpace.lean
@@ -595,6 +595,7 @@ def IsLocalHomeomorph.chartedSpace
     ChartedSpace H M' :=
   hf.chartedSpaceOfRightInverse hf'.hasRightInverse.choose_spec
 
+variable (H) in
 /-- Given a homeomorphism `f : M ≃ₜ M'`, endow `M'` with a `ChartedSpace` structure by pushing
 forward the `ChartedSpace` structure from `M`. -/
 @[implicit_reducible]
