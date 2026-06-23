2650435a2a909fb233879a1eacd150d467b4b007
rshlyakh <157648681+rshlyakh@users.noreply.github.com>
2026-06-23 09:24:32 -0700

    Apply proof suggestions from review
    Co-authored-by: Riccardo Brasca <riccardo.brasca@gmail.com>

--- Mathlib/RingTheory/Ideal/HasGoingUp.lean
+++ Mathlib/RingTheory/Ideal/HasGoingUp.lean
@@ -47,22 +47,18 @@ lemma Ideal.exists_ideal_ge_liesOver_of_le [Algebra.HasGoingUp R S]
     {p q : Ideal R} [p.IsPrime] [q.IsPrime] (P : Ideal S) [P.IsPrime] [P.LiesOver p]
     (hle : p ≤ q) :
     ∃ Q ≥ P, Q.IsPrime ∧ Q.LiesOver q := by
-  by_cases h : p = q
-  · subst h
-    use P
-  · have := P.over_def p
-    subst this
+  rcases eq_or_ne p q with rfl | h
+  · use P
+  · rw [P.over_def p] at hle h
     exact Algebra.HasGoingUp.exists_ideal_ge_liesOver_of_lt P (lt_of_le_of_ne hle h)
 
 lemma Ideal.exists_ideal_gt_liesOver_of_lt [Algebra.HasGoingUp R S]
     {p q : Ideal R} [p.IsPrime] [q.IsPrime] (P : Ideal S) [P.IsPrime] [P.LiesOver p]
     (hpq : p < q) :
     ∃ Q > P, Q.IsPrime ∧ Q.LiesOver q := by
-  obtain ⟨Q, hPQ, _, _⟩ := P.exists_ideal_ge_liesOver_of_le (p := p) (q := q) hpq.le
-  refine ⟨Q, ?_, inferInstance, inferInstance⟩
-  by_contra hc
-  have : P = Q := eq_of_le_of_not_lt hPQ hc
-  subst this
+  obtain ⟨Q, hPQ, hQ, hQq⟩ := P.exists_ideal_ge_liesOver_of_le (p := p) (q := q) hpq.le
+  refine ⟨Q, lt_of_le_of_ne hPQ fun h ↦ ?_, hQ, hQq⟩
+  subst Q
   simp [P.over_def p, P.over_def q] at hpq
 
 /-- This generalizes `exists_ideal_over_prime_of_isIntegral_of_isPrime`
@@ -77,22 +73,13 @@ lemma Ideal.exists_ltSeries_of_hasGoingUp [Algebra.HasGoingUp R S]
       List.map (PrimeSpectrum.comap (algebraMap R S)) (L.toList) = l.toList := by
   induction l using RelSeries.inductionOn generalizing P with
   | singleton q =>
-    use RelSeries.singleton _ ⟨P, inferInstance⟩
-    simp only [RelSeries.singleton_length, RelSeries.head_singleton,
-      RelSeries.toList_singleton, List.map_cons, List.map_nil,
-      List.cons.injEq, and_true, true_and]
-    ext : 1
-    simpa using lo.over.symm
+    refine ⟨RelSeries.singleton _ ⟨P, inferInstance⟩, rfl, rfl, ?_⟩
+    simpa [PrimeSpectrum.ext_iff] using lo.over.symm
   | cons l q lt ih =>
-    simp only [RelSeries.head_cons] at lo
-    obtain ⟨Q, PQlt, hQ, Qlo⟩ :=
-      Ideal.exists_ideal_gt_liesOver_of_lt P lt
-    obtain ⟨L, len, head, spec⟩ := ih Q
-    use L.cons ⟨P, inferInstance⟩ (by simp_all only [Set.mem_setOf_eq, gt_iff_lt]; exact PQlt)
-    simp only [RelSeries.cons_length, add_left_inj,
-      RelSeries.head_cons, true_and]
-    exact ⟨len,
-      by simpa [spec] using PrimeSpectrum.ext_iff.mpr lo.over.symm⟩
+    refine ⟨L.cons ⟨P, inferInstance⟩ (by
+      simp_all only [Set.mem_setOf_eq, gt_iff_lt]
+      exact PQlt), by simpa using len, rfl, ?_⟩
+    simpa [spec, PrimeSpectrum.ext_iff] using lo.over.symm
 
 namespace Algebra.HasGoingUp
 
