952907f9a958f4649d601861371de4c0d8c55e61
Sidharth Hariharan <64404030+thefundamentaltheor3m@users.noreply.github.com>
2026-06-23 11:44:15 -0700

    Update Mathlib/Analysis/Complex/CauchyIntegral.lean
    Co-authored-by: Monica Omar <23701951+themathqueen@users.noreply.github.com>

--- Mathlib/Analysis/Complex/CauchyIntegral.lean
+++ Mathlib/Analysis/Complex/CauchyIntegral.lean
@@ -813,8 +813,7 @@ lemma integral_boundary_rect_eq_zero_eventually_atTop_of_differentiable_on_off_c
       (∫ (x : ℝ) in x₁..x₂, f (x + y * I))
         - (∫ (x : ℝ) in x₁..x₂, f (x + m * I))
         + (I • ∫ (t : ℝ) in y..m, f (x₂ + t * I))
-        - (I • ∫ (t : ℝ) in y..m, f (x₁ + t * I)))
-    =ᶠ[atTop] (fun (_ : ℝ) ↦ 0) := by
+        - (I • ∫ (t : ℝ) in y..m, f (x₁ + t * I))) =ᶠ[atTop] (fun (_ : ℝ) ↦ 0) := by
   filter_upwards [eventually_ge_atTop y] with m hm using hzero y hcont s hs hdiff m hm
 
 end Eventually_Eq_Zero
