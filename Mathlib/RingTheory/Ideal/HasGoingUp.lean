506d175e1a09f3fc1d2a89f225e068f20dcf4011
rshlyakh <robert.shlyakhtenko@icloud.com>
2026-06-23 10:44:49 -0700

    Fixed review suggestions, removed unused imports

--- Mathlib/RingTheory/Ideal/HasGoingUp.lean
+++ Mathlib/RingTheory/Ideal/HasGoingUp.lean
@@ -6,7 +6,6 @@ Authors: Robert Shlyakhtenko
 
 module
 
-public import Mathlib.Order.RelSeries
 public import Mathlib.RingTheory.Spectrum.Prime.Topology
 
 /-!
@@ -38,23 +37,25 @@ The condition only asks for `<` which is easier to prove, use
 class Algebra.HasGoingUp
     (R S : Type*) [CommRing R] [CommRing S] [Algebra R S] : Prop where
   exists_ideal_ge_liesOver_of_lt {q : Ideal R} [q.IsPrime] (P : Ideal S) [P.IsPrime] :
-    P.under R < q → ∃ P ≤ Q, Q.IsPrime ∧ Q.LiesOver q
+    P.under R < q → ∃ Q, P ≤ Q ∧ Q.IsPrime ∧ Q.LiesOver q
 
 variable {R S : Type*} [CommRing R] [CommRing S] [Algebra R S]
 
-lemma Ideal.exists_ideal_ge_liesOver_of_le [Algebra.HasGoingUp R S]
+namespace Ideal
+
+lemma exists_ideal_ge_liesOver_of_le [Algebra.HasGoingUp R S]
     {p q : Ideal R} [p.IsPrime] [q.IsPrime] (P : Ideal S) [P.IsPrime] [P.LiesOver p]
     (hle : p ≤ q) :
-    ∃ Q ≥ P, Q.IsPrime ∧ Q.LiesOver q := by
+    ∃ Q, P ≤ Q ∧ Q.IsPrime ∧ Q.LiesOver q := by
   rcases eq_or_ne p q with rfl | h
   · use P
   · rw [P.over_def p] at hle h
     exact Algebra.HasGoingUp.exists_ideal_ge_liesOver_of_lt P (lt_of_le_of_ne hle h)
 
-lemma Ideal.exists_ideal_gt_liesOver_of_lt [Algebra.HasGoingUp R S]
+lemma exists_ideal_gt_liesOver_of_lt [Algebra.HasGoingUp R S]
     {p q : Ideal R} [p.IsPrime] [q.IsPrime] (P : Ideal S) [P.IsPrime] [P.LiesOver p]
     (hpq : p < q) :
-    ∃ Q > P, Q.IsPrime ∧ Q.LiesOver q := by
+    ∃ Q, P < Q ∧ Q.IsPrime ∧ Q.LiesOver q := by
   obtain ⟨Q, hPQ, hQ, hQq⟩ := P.exists_ideal_ge_liesOver_of_le (p := p) (q := q) hpq.le
   refine ⟨Q, lt_of_le_of_ne hPQ fun h ↦ ?_, hQ, hQq⟩
   subst Q
@@ -62,7 +63,7 @@ lemma Ideal.exists_ideal_gt_liesOver_of_lt [Algebra.HasGoingUp R S]
 
 /-- This generalizes `exists_ideal_over_prime_of_isIntegral_of_isPrime`
 to arbitrary length chains. -/
-lemma Ideal.exists_ltSeries_of_hasGoingUp [Algebra.HasGoingUp R S]
+lemma exists_ltSeries_of_hasGoingUp [Algebra.HasGoingUp R S]
     (l : LTSeries (PrimeSpectrum R))
     (P : Ideal S) [P.IsPrime]
     [lo : P.LiesOver (RelSeries.head l).asIdeal] :
@@ -75,10 +76,16 @@ lemma Ideal.exists_ltSeries_of_hasGoingUp [Algebra.HasGoingUp R S]
     refine ⟨RelSeries.singleton _ ⟨P, inferInstance⟩, rfl, rfl, ?_⟩
     simpa [PrimeSpectrum.ext_iff] using lo.over.symm
   | cons l q lt ih =>
+    simp only [RelSeries.head_cons] at lo
+    obtain ⟨Q, PQlt, hQ, Qlo⟩ :=
+      Ideal.exists_ideal_gt_liesOver_of_lt P lt
+    obtain ⟨L, len, head, spec⟩ := ih Q
     refine ⟨L.cons ⟨P, inferInstance⟩ (by
-      simp_all only [Set.mem_setOf_eq, gt_iff_lt]
+      simp_all only [Set.mem_setOf_eq]
       exact PQlt), by simpa using len, rfl, ?_⟩
     simpa [spec, PrimeSpectrum.ext_iff] using lo.over.symm
+
+end Ideal
 
 namespace Algebra.HasGoingUp
 
