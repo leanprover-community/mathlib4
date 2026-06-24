9d75588f80d0ea4b848d15131f2748a834a63783
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2026-06-24 14:12:00 +0200

    chore: remove tangent space backward transparency

--- Mathlib/Geometry/Manifold/IsManifold/Basic.lean
+++ Mathlib/Geometry/Manifold/IsManifold/Basic.lean
@@ -1055,15 +1055,14 @@ variable {𝕜 : Type*} [NontriviallyNormedField 𝕜]
   {H : Type*} [TopologicalSpace H] (I : ModelWithCorners 𝕜 E H)
   {M : Type*} [TopologicalSpace M] [ChartedSpace H M] {x : M}
 
-set_option backward.isDefEq.respectTransparency false in
 /-- Identifying the tangent space at a normed space with the normed space itself.
 This canonical identification (which, in mathlib, is implemented using an abuse of definitional
 equality) is very prevalent in a number of places: this device allows making it explicit. -/
 def NormedSpace.fromTangentSpace (v : E) : TangentSpace 𝓘(𝕜, E) v ≃L[𝕜] E where
   toFun v := v
   invFun v := v
-  map_add' := by simp
-  map_smul' := by simp
+  map_add' _ _ := rfl
+  map_smul' _ _ := rfl
 
 instance : Inhabited (TangentSpace I x) := ⟨0⟩
 
