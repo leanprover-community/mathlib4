27cb1e6cabfceeabb274b671710908c76a346f4f
Batixx <s59fpern@uni-bonn.de>
2026-06-24 16:42:43 +0200

    chore: remove unused `linter.unusedVariables` exception

--- Mathlib/NumberTheory/NumberField/Cyclotomic/Basic.lean
+++ Mathlib/NumberTheory/NumberField/Cyclotomic/Basic.lean
@@ -220,9 +220,6 @@ theorem integralPowerBasisOfPrimePow_gen [hcycl : IsCyclotomicExtension {p ^ k} 
     simp only [adjoinEquivRingOfIntegersOfPrimePow_apply, IsIntegralClosure.algebraMap_lift]
     rfl
 
-set_option linter.unusedVariables false in
-/- We name `hcycl` so it can be used as a named argument, but this is unused in the declaration
-otherwise, so we need to disable the linter. -/
 @[simp]
 theorem integralPowerBasisOfPrimePow_dim [hcycl : IsCyclotomicExtension {p ^ k} ℚ K]
     (hζ : IsPrimitiveRoot ζ (p ^ k)) : hζ.integralPowerBasisOfPrimePow.dim = φ (p ^ k) := by
