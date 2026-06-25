5b57e9f0c671a12e7ab7539dc0f3661b568acb7f
Whysoserioushah <109107491+Whysoserioushah@users.noreply.github.com>
2026-06-25 20:39:23 +0100

    Update Mathlib/RepresentationTheory/Homological/GroupCohomology/Functoriality.lean
    Co-authored-by: Thomas Browning <tb65536@users.noreply.github.com>

--- Mathlib/RepresentationTheory/Homological/GroupCohomology/Functoriality.lean
+++ Mathlib/RepresentationTheory/Homological/GroupCohomology/Functoriality.lean
@@ -178,9 +178,9 @@ noncomputable def mapIso (e : G ≃* H) (e' : B.V ≃ₗ[k] A.V)
     (he : ∀ g, e' ∘ₗ B.ρ g = A.ρ (e g) ∘ₗ e') (n : ℕ) :
     groupCohomology B n ≅ groupCohomology A n where
   hom := groupCohomology.map e.symm (ofHom ⟨e'.toLinearMap, fun h ↦ by simp [he]⟩) n
-  inv := groupCohomology.map e (ofHom ⟨e'.symm.toLinearMap, fun g ↦
-    e'.eq_comp_toLinearMap_iff _ _|>.1 <| by
-      simp [LinearMap.comp_assoc, ← he g]⟩) n
+  inv := groupCohomology.map e (ofHom ⟨e'.symm.toLinearMap, fun g ↦ by
+    rw [e'.toLinearMap_symm_comp_eq, ← LinearMap.comp_assoc]
+    simp [he, LinearMap.comp_assoc]⟩) n
   hom_inv_id := by
     rw [← groupCohomology.map_comp, ← groupCohomology.map_id]
     refine map_congr (by simp) (by simp [res_id]) n
