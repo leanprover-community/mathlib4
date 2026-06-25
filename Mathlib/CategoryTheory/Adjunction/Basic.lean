fc61229654c8e42a191dfbfc99fc3df619a69b14
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2026-06-25 03:43:10 +0100

    fix

--- Mathlib/CategoryTheory/Adjunction/Basic.lean
+++ Mathlib/CategoryTheory/Adjunction/Basic.lean
@@ -514,16 +514,29 @@ def ofNatIsoLeft {F G : C ⥤ D} {H : D ⥤ C} (adj : F ⊣ H) (iso : F ≅ G) :
     simp [← Functor.comp_map]
   right_triangle_components := by simp [← Functor.map_comp]
 
-@[to_dual]
+@[to_dual none]
 lemma homEquiv_ofNatIsoLeft_apply {F G : C ⥤ D} {H : D ⥤ C} (adj : F ⊣ H) (iso : F ≅ G)
     {X : C} {Y : D} (f : G.obj X ⟶ Y) :
     (ofNatIsoLeft adj iso).homEquiv X Y f = adj.homEquiv _ _ (iso.hom.app _ ≫ f) := by
   simp
 
-@[to_dual]
+@[to_dual none]
 lemma homEquiv_ofNatIsoLeft_symm_apply {F G : C ⥤ D} {H : D ⥤ C} (adj : F ⊣ H) (iso : F ≅ G)
     {X : C} {Y : D} (f : X ⟶ H.obj Y) :
     ((ofNatIsoLeft adj iso).homEquiv X Y).symm f = iso.inv.app _ ≫ (adj.homEquiv _ _).symm f := by
+  simp
+
+@[to_dual none]
+lemma homEquiv_ofNatIsoRight_apply {F : C ⥤ D} {G H : D ⥤ C} (adj : F ⊣ G) (iso : G ≅ H)
+    {X : C} {Y : D} (f : F.obj X ⟶ Y) :
+    (ofNatIsoRight adj iso).homEquiv X Y f = adj.homEquiv _ _ f ≫ iso.hom.app _ := by
+  simp
+
+@[to_dual none]
+lemma homEquiv_ofNatIsoRight_symm_apply {F : C ⥤ D} {G H : D ⥤ C} (adj : F ⊣ G) (iso : G ≅ H)
+    {X : C} {Y : D} (f : X ⟶ H.obj Y) :
+    ((ofNatIsoRight adj iso).homEquiv X Y).symm f =
+      (adj.homEquiv _ _).symm (f ≫ iso.inv.app _) := by
   simp
 
 /-- The isomorphism which an adjunction `F ⊣ G` induces on `G ⋙ yoneda`. This states that
