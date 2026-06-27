8801670248e2bdf094a5427e3bf811bd0233d50a
Jack McCarthy <37917934+Deicyde@users.noreply.github.com>
2026-06-27 15:13:38 -0400

    Rewrote docstring

--- Mathlib/Geometry/Manifold/Category/MfldCat/OfModel.lean
+++ Mathlib/Geometry/Manifold/Category/MfldCat/OfModel.lean
@@ -11,8 +11,8 @@ public import Mathlib.Topology.Category.TopCat.Basic
 /-!
 # The category of `C^n` manifolds modeled on `I`
 
-`ModelWithCorners.MfldCat.{u} I n` is the category of bundled `C^n` manifolds modeled on a fixed
-`I : ModelWithCorners 𝕜 E H`.
+This file defines the bundled category `ModelWithCorners.MfldCat.{u} I n` of `C^n` manifolds modeled
+on a fixed `I : ModelWithCorners 𝕜 E H`, along with the rogetful functor to `TopCat`.
 
 ## Future work
 * Show that `ModelWithCorners.MfldCat I n` has coproducts given by disjoint unions.
