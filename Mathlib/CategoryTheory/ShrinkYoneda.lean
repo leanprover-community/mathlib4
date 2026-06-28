6419e846f4b8f9fc591a3eedec5393552ae4d774
Ben Eltschig <b.eltschig@protonmail.com>
2026-06-29 01:57:50 +0200

    WIP: switch to approach using points

--- Mathlib/CategoryTheory/ShrinkYoneda.lean
+++ Mathlib/CategoryTheory/ShrinkYoneda.lean
@@ -69,6 +69,8 @@ end FunctorToTypes
 
 variable [LocallySmall.{w} C]
 
+section Yoneda
+
 set_option backward.defeqAttrib.useBackward true in
 instance (X : C) : FunctorToTypes.Small.{w} (yoneda.obj X) :=
   fun _ ↦ by dsimp; infer_instance
@@ -248,5 +250,192 @@ def shrinkYonedaRepresentableBy (X : C) : (shrinkYoneda.{w}.obj X).Representable
 
 instance (X : C) : (shrinkYoneda.{w}.obj X).IsRepresentable :=
   (shrinkYonedaRepresentableBy X).isRepresentable
+
+end Yoneda
+
+section Coyoneda
+
+set_option backward.defeqAttrib.useBackward true in
+instance (X : Cᵒᵖ) : FunctorToTypes.Small.{w} (coyoneda.obj X) :=
+  fun _ ↦ by dsimp; infer_instance
+
+/-- The co-Yoneda embedding `Cᵒᵖ ⥤ C ⥤ Type w` for a locally `w`-small category `C`. -/
+@[simps -isSimp obj map, pp_with_univ]
+noncomputable def shrinkCoyoneda :
+    Cᵒᵖ ⥤ C ⥤ Type w where
+  obj X := FunctorToTypes.shrink (coyoneda.obj X)
+  map f := FunctorToTypes.shrinkMap (coyoneda.map f)
+
+/-- The type `(shrinkCoyoneda.obj X).obj Y` is equivalent to `X.unop ⟶ Y`. -/
+noncomputable def shrinkCoyonedaObjObjEquiv {X : Cᵒᵖ} {Y : C} :
+    ((shrinkCoyoneda.{w}.obj X).obj Y) ≃ (X.unop ⟶ Y) :=
+  (equivShrink _).symm
+
+set_option backward.defeqAttrib.useBackward true in
+set_option backward.isDefEq.respectTransparency false in
+lemma shrinkCoyoneda_obj_map_shrinkCoyonedaObjObjEquiv_symm
+    {X : Cᵒᵖ} {Y Y' : C} (g : Y ⟶ Y') (f : X.unop ⟶ Y) :
+    (shrinkCoyoneda.obj _).map g (shrinkCoyonedaObjObjEquiv.symm f) =
+      shrinkCoyonedaObjObjEquiv.symm (f ≫ g) := by
+  simp [shrinkCoyoneda, shrinkCoyonedaObjObjEquiv]
+
+lemma shrinkCoyonedaObjObjEquiv_symm_comp {X Y Y' : C} (g : Y' ⟶ Y) (f : Y ⟶ X) :
+    shrinkCoyonedaObjObjEquiv.symm (g ≫ f) =
+    (shrinkCoyoneda.obj _).map f (shrinkCoyonedaObjObjEquiv.symm g) :=
+  (shrinkCoyoneda_obj_map_shrinkCoyonedaObjObjEquiv_symm f g).symm
+
+set_option backward.defeqAttrib.useBackward true in
+set_option backward.isDefEq.respectTransparency false in
+lemma shrinkCoyoneda_map_app_shrinkCoyonedaObjObjEquiv_symm
+    {X X' : Cᵒᵖ} {Y : C} (f : X.unop ⟶ Y) (g : X ⟶ X') :
+    (shrinkCoyoneda.map g).app _ (shrinkCoyonedaObjObjEquiv.symm f) =
+      shrinkCoyonedaObjObjEquiv.symm (g.unop ≫ f) := by
+  simp [shrinkCoyoneda, shrinkCoyonedaObjObjEquiv]
+
+set_option backward.defeqAttrib.useBackward true in
+set_option backward.isDefEq.respectTransparency false in
+@[reassoc]
+lemma shrinkCoyonedaObjObjEquiv_map_app
+    {X X' : Cᵒᵖ} {Y : C} (f : (shrinkCoyoneda.{w, v, u}.obj X).obj Y) (g : X ⟶ X') :
+    shrinkCoyonedaObjObjEquiv ((shrinkCoyoneda.map g).app Y f) =
+      g.unop ≫ shrinkCoyonedaObjObjEquiv f := by
+  simp [shrinkCoyoneda, shrinkCoyonedaObjObjEquiv]
+
+set_option backward.defeqAttrib.useBackward true in
+set_option backward.isDefEq.respectTransparency false in
+@[reassoc]
+lemma shrinkCoyonedaObjObjEquiv_obj_map {X : Cᵒᵖ} {Y Y' : C} (g : Y ⟶ Y')
+    (f : (shrinkCoyoneda.{w}.obj X).obj Y) :
+    shrinkCoyonedaObjObjEquiv ((shrinkCoyoneda.{w}.obj X).map g f) =
+      shrinkCoyonedaObjObjEquiv f ≫ g := by
+  simp [shrinkCoyonedaObjObjEquiv, shrinkCoyoneda]
+
+set_option backward.isDefEq.respectTransparency false in
+/-- The type of natural transformations `shrinkCoyoneda.{w}.obj X ⟶ P`
+with `X : Cᵒᵖ` and `P : C ⥤ Type w` is equivalent to `P.obj (op X)`. -/
+noncomputable def shrinkCoyonedaEquiv {X : Cᵒᵖ} {P : C ⥤ Type w} :
+    (shrinkCoyoneda.{w}.obj X ⟶ P) ≃ P.obj X.unop where
+  toFun τ := τ.app _ (equivShrink.{w} _ (𝟙 X.unop))
+  invFun x :=
+    { app Y := ↾fun f ↦ P.map ((equivShrink.{w} _).symm f) x
+      naturality Y Z g := by ext; simp [shrinkCoyoneda] }
+  left_inv τ := by
+    ext Y f
+    obtain ⟨f, rfl⟩ := (equivShrink _).surjective f
+    simpa [shrinkCoyoneda] using ((τ.naturality_apply f) (equivShrink _ (𝟙 X.unop))).symm
+  right_inv x := by simp
+
+set_option backward.defeqAttrib.useBackward true in
+set_option backward.isDefEq.respectTransparency false in
+lemma map_shrinkCoyonedaEquiv {X Y : Cᵒᵖ} {P : C ⥤ Type w} (f : shrinkCoyoneda.obj X ⟶ P)
+    (g : Y ⟶ X) : P.map g.unop (shrinkCoyonedaEquiv f) =
+      f.app Y.unop (shrinkCoyonedaObjObjEquiv.symm g.unop) := by
+  simp [shrinkCoyonedaObjObjEquiv, shrinkCoyonedaEquiv, shrinkCoyoneda,
+    ← comp_apply, ← NatTrans.naturality]
+
+set_option backward.defeqAttrib.useBackward true in
+set_option backward.isDefEq.respectTransparency false in
+lemma shrinkCoyonedaEquiv_shrinkCoyoneda_map {X Y : Cᵒᵖ} (f : X ⟶ Y) :
+    shrinkCoyonedaEquiv (shrinkCoyoneda.{w}.map f) = shrinkCoyonedaObjObjEquiv.symm f.unop := by
+  simp [shrinkCoyonedaEquiv, shrinkCoyoneda, shrinkCoyonedaObjObjEquiv]
+
+lemma shrinkCoyonedaEquiv_comp {X : Cᵒᵖ} {P Q : C ⥤ Type w} (α : shrinkCoyoneda.obj X ⟶ P)
+    (β : P ⟶ Q) :
+    shrinkCoyonedaEquiv (α ≫ β) = β.app _ (shrinkCoyonedaEquiv α) := by
+  simp [shrinkCoyonedaEquiv]
+
+set_option backward.isDefEq.respectTransparency false in
+lemma shrinkCoyonedaEquiv_naturality {X Y : Cᵒᵖ} {P : C ⥤ Type w}
+    (f : shrinkCoyoneda.obj X ⟶ P) (g : Y ⟶ X) :
+    P.map g.unop (shrinkCoyonedaEquiv f) = shrinkCoyonedaEquiv (shrinkCoyoneda.map g ≫ f) := by
+  simpa [shrinkCoyonedaEquiv, shrinkCoyoneda]
+    using (f.naturality_apply g.unop ((equivShrink _) (𝟙 _))).symm
+
+@[reassoc]
+lemma shrinkCoyonedaEquiv_symm_map {X Y : C} (f : X ⟶ Y) {P : C ⥤ Type w} (t : P.obj X) :
+    shrinkCoyonedaEquiv.symm (P.map f t) =
+      shrinkCoyoneda.map f.op ≫ shrinkCoyonedaEquiv.symm t :=
+  shrinkCoyonedaEquiv.injective (by
+    obtain ⟨t, rfl⟩ := shrinkCoyonedaEquiv.surjective t
+    rw [← shrinkCoyonedaEquiv_naturality]
+    simp)
+
+lemma shrinkCoyonedaEquiv_symm_app_shrinkCoyonedaObjObjEquiv_symm {X : Cᵒᵖ} {P : C ⥤ Type w}
+    (s : P.obj X.unop) {Y : Cᵒᵖ} (f : Y ⟶ X) :
+    (shrinkCoyonedaEquiv.symm s).app Y.unop (shrinkCoyonedaObjObjEquiv.symm f.unop) =
+      P.map f.unop s := by
+  obtain ⟨g, rfl⟩ := shrinkCoyonedaEquiv.surjective s
+  simp [map_shrinkCoyonedaEquiv]
+
+variable (C) in
+/-- The functor `shrinkCoyoneda : Cᵒᵖ ⥤ C ⥤ Type w` for a locally `w`-small category `C`
+is fully faithful. -/
+noncomputable def fullyFaithfulShrinkCoyoneda :
+    (shrinkCoyoneda.{w} (C := C)).FullyFaithful where
+  preimage f := (shrinkCoyonedaObjObjEquiv (shrinkCoyonedaEquiv f)).op
+  map_preimage f := by
+    obtain ⟨f, rfl⟩ := shrinkCoyonedaEquiv.symm.surjective f
+    cat_disch
+  preimage_map f := by simp [shrinkCoyonedaEquiv_shrinkCoyoneda_map]
+
+instance : (shrinkCoyoneda.{w} (C := C)).Faithful := (fullyFaithfulShrinkCoyoneda C).faithful
+
+instance : (shrinkCoyoneda.{w} (C := C)).Full := (fullyFaithfulShrinkCoyoneda C).full
+
+set_option backward.defeqAttrib.useBackward true in
+/-- `shrinkCoyoneda` at the morphism universe level is `coyoneda`. -/
+@[simps! hom_app inv_app]
+noncomputable
+def shrinkCoyonedaIsoCoyoneda : shrinkCoyoneda.{v} ≅ coyoneda (C := C) :=
+  NatIso.ofComponents
+    (fun X ↦ NatIso.ofComponents (fun Y ↦ shrinkCoyonedaObjObjEquiv.toIso)
+      (by intros; ext; simp [shrinkCoyonedaObjObjEquiv_obj_map]))
+    (by intros; ext; simp [shrinkCoyonedaObjObjEquiv_map_app])
+
+set_option backward.defeqAttrib.useBackward true in
+/-- `shrinkCoyoneda` is compatible with `uliftFunctor`. -/
+noncomputable
+def shrinkCoyonedaUliftFunctorIso [LocallySmall.{max w w'} C] :
+    shrinkCoyoneda.{w} ⋙ (Functor.whiskeringRight Cᵒᵖ _ _).obj uliftFunctor.{w', w} ≅
+      shrinkCoyoneda :=
+  NatIso.ofComponents
+    (fun X ↦ FunctorToTypes.shrinkCompUliftFunctorIso.{w, v} (coyoneda.obj X))
+    fun _ ↦ by ext; simp [shrinkCoyoneda]
+
+/-- `uliftYoneda` identifies to `shrinkCoyoneda`. -/
+noncomputable def uliftYonedaIsoShrinkCoyoneda :
+    uliftCoyoneda.{w'} (C := C) ≅ shrinkCoyoneda.{max w' v} :=
+  NatIso.ofComponents (fun X ↦ NatIso.ofComponents
+    (fun Y ↦ (Equiv.ulift.trans shrinkCoyonedaObjObjEquiv.symm).toIso) (fun f ↦ by
+      ext
+      exact (shrinkCoyoneda_obj_map_shrinkCoyonedaObjObjEquiv_symm _ _).symm)) (fun g ↦ by
+      ext
+      exact (shrinkCoyoneda_map_app_shrinkCoyonedaObjObjEquiv_symm _ _).symm)
+
+set_option backward.defeqAttrib.useBackward true in
+/-- The functor `shrinkCoyoneda.{w}` followed by the evaluation
+at `Y : C` and `uliftFunctor.{v}` identifies to `yoneda.obj Y` followed
+by `uliftFunctor.{w}`. -/
+noncomputable def shrinkCoyonedaCompEvaluationCompUliftFunctorIsoUliftFunctor (Y : C) :
+    shrinkCoyoneda.{w} ⋙ (evaluation C _).obj Y ⋙ uliftFunctor.{v} ≅
+      yoneda.obj Y ⋙ uliftFunctor.{w} :=
+  NatIso.ofComponents (fun X ↦ (Equiv.ulift.trans
+    (shrinkCoyonedaObjObjEquiv.trans Equiv.ulift.symm)).toIso) (fun f ↦ by
+      ext ⟨g⟩
+      obtain ⟨g, rfl⟩ := shrinkCoyonedaObjObjEquiv.symm.surjective g
+      simp [shrinkCoyoneda_map_app_shrinkCoyonedaObjObjEquiv_symm])
+
+/-- `shrinkCoyoneda.obj X` is corepresented by `X`. -/
+@[simps]
+noncomputable
+def shrinkCoyonedaRepresentableBy (X : Cᵒᵖ) :
+    (shrinkCoyoneda.{w}.obj X).CorepresentableBy X.unop where
+  homEquiv := shrinkCoyonedaObjObjEquiv.symm
+  homEquiv_comp f g := shrinkCoyonedaObjObjEquiv_symm_comp g f
+
+instance (X : Cᵒᵖ) : (shrinkCoyoneda.{w}.obj X).IsCorepresentable :=
+  (shrinkCoyonedaRepresentableBy X).isCorepresentable
+
+end Coyoneda
 
 end CategoryTheory
--- Mathlib/CategoryTheory/Sites/LocalSite.lean
+++ Mathlib/CategoryTheory/Sites/LocalSite.lean
@@ -6,7 +6,9 @@ Authors: Ben Eltschig
 module
 
 public import Mathlib.CategoryTheory.Adjunction.Triple
+public import Mathlib.CategoryTheory.Limits.Elements
 public import Mathlib.CategoryTheory.Sites.GlobalSections
+public import Mathlib.CategoryTheory.Sites.Point.Skyscraper
 
 /-!
 # Local sites
@@ -18,10 +20,14 @@ See https://ncatlab.org/nlab/show/local+site.
 
 ## Main definitions / results
 * `J.IsLocalSite`: typeclass stating that (C,J) is a local site.
-* `coconstantSheaf`: the coconstant sheaf functor `Type w ⥤ Sheaf J (Type max v w)` defined on any
-  local site.
-* `ΓCoconstantSheafAdj`: the adjunction between the global sections functor `Γ` and
-  `coconstantSheaf`.
+* `IsLocalSite.point J`: the canonical point of any local site, whose fibre functor is given by
+  the coyoneda embedding of the terminal object and extends to the global sections functors on
+  presheaves and sheaves.
+* `coconstantSheaf J A`: the coconstant sheaf functor `A ⥤ Sheaf J A` for any local site (C,J) and
+  sufficiently nice target category `A`, defined as the skyscraper sheaf functor of the canonical
+  point.
+* `ΓCoconstantSheafAdj J A`: the adjunction between the global sections functor `Γ J A` and
+  `coconstantSheaf J A`.
 * `fullyFaithfulCoconstantSheaf`: `coconstantSheaf` is fully faithful.
 * `fullyFaithfulConstantSheaf`: on local sites, the constant sheaf functor is fully faithful.
 
@@ -29,7 +35,7 @@ All together this shows that for local sites `Sheaf J (Type max u v w)` forms a 
 since we don't yet have local topoi this can't be stated yet.
 -/
 
-universe w u v
+universe w u v u' v'
 
 @[expose] public section
 
@@ -45,7 +51,7 @@ class GrothendieckTopology.IsLocalSite extends HasTerminal C where
   eq_top_of_mem : ∀ S ∈ J (⊤_ C), S = ⊤
 
 /-- On a local site, every covering sieve contains every morphism from the terminal object. -/
-lemma LocalSite.from_terminal_mem_of_mem [J.IsLocalSite] {X : C} (f : ⊤_ C ⟶ X) {S : Sieve X}
+lemma IsLocalSite.from_terminal_mem_of_mem [J.IsLocalSite] {X : C} (f : ⊤_ C ⟶ X) {S : Sieve X}
     (hS : S ∈ J X) : S.arrows f :=
   (S.mem_iff_pullback_eq_top f).2 <| IsLocalSite.eq_top_of_mem _ <| J.pullback_stable f hS
 
@@ -53,142 +59,166 @@ lemma LocalSite.from_terminal_mem_of_mem [J.IsLocalSite] {X : C} (f : ⊤_ C ⟶
 instance {C : Type u} [Category.{v} C] [HasTerminal C] : (trivial C).IsLocalSite where
   eq_top_of_mem _ := trivial_covering.1
 
-/-- The functor that sends any type `A` to the functor `Cᵒᵖ → Type _` that sends any `X : C`
-to the type of all functions `(⊤_ C ⟶ X) → A`. This can be defined on any site with a terminal
-object, but has values in sheaves in the case of local sites. -/
-@[simps!]
-noncomputable def Presheaf.coconst {C : Type u} [Category.{v} C] [HasTerminal C] :
-    Type w ⥤ (Cᵒᵖ ⥤ Type max v w) where
-  obj A := {
-    obj X := ((⊤_ C ⟶ X.unop) → A)
-    map f := TypeCat.ofHom fun g g' ↦ g (g' ≫ f.unop) }
-  map f := {
-    app X := TypeCat.ofHom fun g ↦ ConcreteCategory.hom f ∘ g }
+/-- Every local site has a canonical point, given as a fibre functor by the coyoneda embedding of
+the terminal object `⊤_ C`. -/
+noncomputable def IsLocalSite.point [LocallySmall.{w} C] [J.IsLocalSite] : Point.{w} J where
+  fiber := shrinkCoyoneda.obj (op (⊤_ C))
+  jointly_surjective := fun R hR x ↦
+    ⟨(⊤_ C), shrinkCoyonedaObjObjEquiv x,
+      (IsLocalSite.from_terminal_mem_of_mem J (shrinkCoyonedaObjObjEquiv x) hR),
+          shrinkCoyonedaObjObjEquiv.symm (𝟙 _), by
+        rw [shrinkCoyoneda_obj_map_shrinkCoyonedaObjObjEquiv_symm]
+        simp⟩
+
+lemma shrinkCoyoneda_obj_map [LocallySmall.{w} C]
+    {X : Cᵒᵖ} {Y Y' : C} (g : Y ⟶ Y') (f : (shrinkCoyoneda.obj X).obj Y) :
+    (shrinkCoyoneda.obj _).map g f =
+      shrinkCoyonedaObjObjEquiv.symm (shrinkCoyonedaObjObjEquiv f ≫ g) :=
+  rfl
+
+/-- The canonical initial object of `(shrinkCoyoneda.{w}.obj (op X)).Elements`, given
+by `𝟙 X : X ⟶ X`. -/
+noncomputable def shrinkCoyonedaElementsInitial [LocallySmall.{w} C] (X : C) :
+  (shrinkCoyoneda.{w}.obj (op X)).Elements :=
+    ⟨X, shrinkCoyonedaObjObjEquiv.symm (𝟙 X)⟩
 
 set_option backward.isDefEq.respectTransparency false in
-/-- On local sites, `Presheaf.coconst'` actually takes values in sheaves. -/
-lemma Presheaf.coconst_isSheaf [J.IsLocalSite] (X : Type w) : IsSheaf J (coconst.obj X) := by
-  refine (isSheaf_iff_isSheaf_of_type J _).2 fun Y S hS f hf ↦ ?_
-  refine ⟨fun g ↦ f g (LocalSite.from_terminal_mem_of_mem J g hS) (𝟙 _), ?_, ?_⟩
-  · refine fun Z g hg ↦ funext fun x ↦ ?_
-    exact (congrFun (f.comp_of_compatible S hf hg x) _).trans (congrArg (f g hg) <| by simp)
-  · exact fun g hg ↦ funext fun h : (⊤_ C ⟶ Y) ↦ Eq.trans (by simp [Presheaf.coconst]) <|
-      congrFun (hg h ((LocalSite.from_terminal_mem_of_mem J h hS))) _
-
-/-- The right adjoint to the global sections functor that exists over any local site.
-Takes a type `X` to the sheaf that sends each `Y : C` to the type of functions `Y → X`. -/
-noncomputable def IsLocalSite.coconstantSheaf [J.IsLocalSite] :
-    Type w ⥤ Sheaf J (Type (max v w)) where
-  obj X := ⟨Presheaf.coconst.obj X, Presheaf.coconst_isSheaf J X⟩
-  map f := ⟨Presheaf.coconst.map f⟩
-  map_id _ := rfl
-  map_comp _ _ := rfl
+/-- `shrinkCoyonedaElementsInitial X` is indeed initial. -/
+noncomputable def shrinkCoyonedaElementsInitialIsInitial [LocallySmall.{w} C] (X : C) :
+    IsInitial (shrinkCoyonedaElementsInitial X) where
+  desc s := ⟨shrinkCoyonedaObjObjEquiv s.pt.snd, by
+    simp only [asEmptyCocone, shrinkCoyonedaElementsInitial]
+    rw [← shrinkCoyonedaObjObjEquiv_symm_comp]
+    simp⟩
+  uniq := by
+    intro s ⟨(f : X ⟶ _), hf⟩ _
+    ext1
+    simp only [shrinkCoyoneda_obj_map, asEmptyCocone, shrinkCoyonedaElementsInitial,
+      Equiv.apply_symm_apply, Category.id_comp] at hf
+    exact (Equiv.symm_apply_eq _).1 hf
 
 set_option backward.isDefEq.respectTransparency false in
+/-- The fibre of any presheaf `P : Cᵒᵖ ⥤ A` at `IsLocalSite.point J` is just `P` evaluated at
+the terminal object. -/
+noncomputable def IsLocalSite.pointPresheafFiberIso [LocallySmall.{w} C] [J.IsLocalSite]
+    {A : Type u'} [Category.{v', u'} A] [Limits.HasColimitsOfSize.{w, w, v', u'} A] (P : Cᵒᵖ ⥤ A) :
+    (IsLocalSite.point J).presheafFiber.obj P ≅ P.obj (op (⊤_ C)) :=
+  (colimit.isColimit _).coconePointUniqueUpToIso
+    (colimitOfDiagramTerminal (shrinkCoyonedaElementsInitialIsInitial (⊤_ C)).op _)
+
+set_option backward.isDefEq.respectTransparency false in
+lemma IsColimit.map_comp_coconePointUniqueUpToIso {J : Type*} [Category* J] {F G : J ⥤ C}
+    {s : Cocone F} {t t' : Cocone G} (hs : IsColimit s) (ht : IsColimit t)
+    (ht' : IsColimit t') (α : F ⟶ G) :
+    hs.map t α ≫ (ht.coconePointUniqueUpToIso ht').hom = hs.map t' α :=
+  hs.uniq (((Cocone.precompose α).obj t')) _ fun i ↦ by simp
+
+set_option backward.isDefEq.respectTransparency false in
+lemma IsColimit.coconePointUniqueUpToIso_comp_map {J : Type*} [Category* J] {F G : J ⥤ C}
+    {s' s : Cocone F} (hs' : IsColimit s') (hs : IsColimit s) (t : Cocone G) (α : F ⟶ G) :
+    (hs'.coconePointUniqueUpToIso hs).hom ≫ hs.map t α = hs'.map t α :=
+  hs'.uniq (((Cocone.precompose α).obj t)) _ fun i ↦ by simp
+
+lemma IsColimit.coconePointUniqueUpToIso_naturality {J : Type*} [Category* J] {F G : J ⥤ C}
+    {s s' : Cocone F} {t t' : Cocone G} (hs : IsColimit s) (hs' : IsColimit s') (ht : IsColimit t)
+    (ht' : IsColimit t') (α : F ⟶ G) :
+    hs.map t α ≫ (ht.coconePointUniqueUpToIso ht').hom =
+      (hs.coconePointUniqueUpToIso hs').hom ≫ hs'.map t' α := by
+  rw [map_comp_coconePointUniqueUpToIso, coconePointUniqueUpToIso_comp_map]
+
+set_option backward.isDefEq.respectTransparency false in
+lemma IsLocalSite.pointPresheafFiberIso_naturality [LocallySmall.{w} C] [J.IsLocalSite]
+    {A : Type u'} [Category.{v', u'} A] [Limits.HasColimitsOfSize.{w, w, v', u'} A]
+    {P P' : Cᵒᵖ ⥤ A} (F : P ⟶ P') :
+    (point J).presheafFiber.map F ≫ (pointPresheafFiberIso J P').hom =
+      (pointPresheafFiberIso J P).hom ≫ F.app (op (⊤_ C)) := by
+  refine (IsColimit.coconePointUniqueUpToIso_naturality _
+    (colimitOfDiagramTerminal (shrinkCoyonedaElementsInitialIsInitial (⊤_ C)).op _) _ _
+      (((CategoryOfElements.π (shrinkCoyoneda.{w}.obj (op (⊤_ C)))).op.whiskerLeft F))).trans ?_
+  congr
+  simp only [IsColimit.map, colimitOfDiagramTerminal, Cocone.precompose_obj_ι, NatTrans.comp_app,
+    Functor.whiskerLeft_app, coconeOfDiagramTerminal_ι_app, IsTerminal.from_self, Functor.map_id]
+  exact Category.comp_id _
+
+/-- The presheaf fibre functor of `IsLocalSite.point J` is given by evaluation at the terminal
+object. -/
+noncomputable def IsLocalSite.pointPresheafFiberNatIso [LocallySmall.{w} C] [J.IsLocalSite]
+    (A : Type u') [Category.{v', u'} A] [Limits.HasColimitsOfSize.{w, w, v', u'} A] :
+    ((IsLocalSite.point J).presheafFiber : _ ⥤ A) ≅ (evaluation _ _).obj (op (⊤_ C)) :=
+  NatIso.ofComponents (pointPresheafFiberIso J) fun F ↦ pointPresheafFiberIso_naturality J F
+
+/-- The sheaf fibre functor of `IsLocalSite.point J` is the global sections functor. -/
+noncomputable def IsLocalSite.pointSheafFiberIso [LocallySmall.{w} C] [J.IsLocalSite]
+    (A : Type u') [Category.{v', u'} A] [Limits.HasColimitsOfSize.{w, w, v', u'} A]
+    [HasWeakSheafify J A] : (IsLocalSite.point J).sheafFiber ≅ Γ J A :=
+  ((sheafToPresheaf J A).isoWhiskerLeft (pointPresheafFiberNatIso J A)).trans
+    (ΓNatIsoSheafSections J A terminalIsTerminal).symm
+
+/-- The right adjoint to the global sections functor that exists over any local site. This is
+implemented as the skyscraper functor associated to `IsLocalSite.point.{w} J`, but can be thought of
+as taking any object `X : A` to the sheaf that sends each `Y : C` to the product over copies of `A`
+indexed by the points `⊤_ C ⟶ Y` of `Y`. -/
+noncomputable def IsLocalSite.coconstantSheaf [LocallySmall.{w} C] [J.IsLocalSite]
+    (A : Type u') [Category.{v', u'} A] [Limits.HasColimitsOfSize.{w, w, v', u'} A]
+    [HasProducts.{w} A] : A ⥤ Sheaf J A :=
+  (point.{w} J).skyscraperSheafFunctor
+
+noncomputable example [J.IsLocalSite] : Type max v w ⥤ Sheaf J (Type (max v w)) :=
+  IsLocalSite.coconstantSheaf.{max v w} J (Type max v w)
+
+variable [LocallySmall.{w} C] [J.IsLocalSite]
+    (A : Type u') [Category.{v', u'} A] [Limits.HasColimitsOfSize.{w, w, v', u'} A]
+    [HasProducts.{w} A] [HasWeakSheafify J A]
+
 /-- On local sites, the global sections functor `Γ` is left-adjoint to the coconstant functor. -/
-noncomputable def IsLocalSite.ΓCoconstantSheafAdj [J.IsLocalSite] :
-    Γ J (Type max u v w) ⊣ coconstantSheaf J := by
-  refine Adjunction.ofNatIsoLeft ?_ (ΓNatIsoSheafSections J _ terminalIsTerminal).symm
-  exact {
-    unit := {
-      app X := ⟨{
-        app Y := TypeCat.ofHom fun (x : X.obj.obj Y) y ↦ X.obj.map (op y) x
-        naturality Y Z f := by
-          ext (x : X.obj.obj Y)
-          dsimp [coconstantSheaf, Presheaf.coconst, Functor.comp]
-          ext z
-          exact (Functor.map_comp_apply X.obj _ _ x).symm
-      }⟩
-      naturality X Y f := by
-        ext Z (x : X.obj.obj Z)
-        dsimp [coconstantSheaf, Presheaf.coconst, Functor.comp]
-        ext z
-        exact (NatTrans.naturality_apply f.hom _ x).symm
-    }
-    counit := { app X := TypeCat.ofHom fun f ↦ f (𝟙 _) }
-    left_triangle_components X := by
-      ext (x : X.obj.obj _)
-      exact ConcreteCategory.congr_hom (X.obj.map_id _) x
-    right_triangle_components X := by
-      ext Y (f : _ → _)
-      dsimp [coconstantSheaf, Presheaf.coconst]
-      ext y
-      simp }
+noncomputable def IsLocalSite.ΓCoconstantSheafAdj' : Γ J A ⊣ coconstantSheaf J A :=
+  (point.{w} J).skyscraperSheafAdjunction.ofNatIsoLeft (pointSheafFiberIso J A)
 
-instance [J.IsLocalSite] : (IsLocalSite.coconstantSheaf.{max u v w} J).IsRightAdjoint :=
-  ⟨Γ J _, ⟨IsLocalSite.ΓCoconstantSheafAdj J⟩⟩
+instance : (Γ J A).IsLeftAdjoint :=
+  ⟨IsLocalSite.coconstantSheaf.{w} J A, ⟨IsLocalSite.ΓCoconstantSheafAdj' J A⟩⟩
+
+instance : (IsLocalSite.coconstantSheaf.{w} J A).IsRightAdjoint :=
+  ⟨Γ J A, ⟨IsLocalSite.ΓCoconstantSheafAdj' J A⟩⟩
 
 /-- The global sections of the coconstant sheaf on a type are naturally isomorphic to that type. -/
-noncomputable def coconstantSheafΓNatIsoId [J.IsLocalSite] :
-    IsLocalSite.coconstantSheaf J ⋙ Γ J _ ≅ 𝟭 (Type max u v w) := by
+noncomputable def coconstantSheafΓNatIsoId' :
+    IsLocalSite.coconstantSheaf J A ⋙ Γ J A ≅ 𝟭 A := by
   refine (Functor.isoWhiskerLeft _ (ΓNatIsoSheafSections J _ terminalIsTerminal)).trans ?_
-  exact (NatIso.ofComponents (fun X ↦ {
-    hom := TypeCat.ofHom fun x _ ↦ x
-    inv := TypeCat.ofHom fun f ↦ by exact f (𝟙 (⊤_ C))
-    inv_hom_id := by
-      dsimp [IsLocalSite.coconstantSheaf, Presheaf.coconst]
-      ext x
-      dsimp at ⊢
-      exact funext fun x ↦ congr_arg _ <| Subsingleton.elim _ _
-  })).symm
+  refine NatIso.ofComponents (fun X ↦ @productUniqueIso
+    (((IsLocalSite.point J).fiber.obj (⊤_ C))) _ _ (equivShrink (⊤_ C ⟶ ⊤_ C)).symm.unique _) ?_
+  intro X Y f
+  simp only [IsLocalSite.coconstantSheaf, Point.skyscraperSheafFunctor,
+    Point.skyscraperPresheafFunctor, Functor.comp, Functor.id_obj, Functor.flip_obj_map,
+    ObjectProperty.ι_obj, ObjectProperty.ι_map, Functor.flip_map_app, piFunctor_map_app,
+    productUniqueIso_hom, Functor.id_map]
+  exact Pi.map_π _ _
 
 /-- `coconstantSheaf` is fully faithful. -/
-noncomputable def fullyFaithfulCoconstantSheaf [J.IsLocalSite] :
-    (IsLocalSite.coconstantSheaf.{max u v w} J).FullyFaithful :=
-  (IsLocalSite.ΓCoconstantSheafAdj J).fullyFaithfulROfCompIsoId (coconstantSheafΓNatIsoId J)
+noncomputable def fullyFaithfulCoconstantSheaf :
+    (IsLocalSite.coconstantSheaf.{w} J A).FullyFaithful :=
+  (IsLocalSite.ΓCoconstantSheafAdj' J A).fullyFaithfulROfCompIsoId (coconstantSheafΓNatIsoId' J A)
 
-instance [J.IsLocalSite] : (IsLocalSite.coconstantSheaf.{max u v} J).Full :=
-  (fullyFaithfulCoconstantSheaf J).full
+instance : (IsLocalSite.coconstantSheaf.{w} J A).Full :=
+  (fullyFaithfulCoconstantSheaf J A).full
 
-instance [J.IsLocalSite] : (IsLocalSite.coconstantSheaf.{max u v} J).Faithful :=
-  (fullyFaithfulCoconstantSheaf J).faithful
+instance : (IsLocalSite.coconstantSheaf.{w} J A).Faithful :=
+  (fullyFaithfulCoconstantSheaf J A).faithful
+
+/-- The adjoint triple `constantSheaf J A ⊣ Γ J A ⊣ coconstantSheaf J A` on any local site. -/
+noncomputable abbrev IsLocalSite.constantΓCoconstantTriple :
+    Adjunction.Triple (constantSheaf J A) (Γ J A) (coconstantSheaf.{w} J A) where
+  adj₁ := constantSheafΓAdj J A
+  adj₂ := ΓCoconstantSheafAdj' J A
 
 /-- On local sites, the constant sheaf functor is fully faithful. -/
-noncomputable def fullyFaithfulConstantSheaf [HasWeakSheafify J (Type max u v w)] [J.IsLocalSite] :
-    (constantSheaf J (Type max u v w)).FullyFaithful :=
-  (Adjunction.Triple.mk (constantSheafΓAdj J _)
-    (IsLocalSite.ΓCoconstantSheafAdj J)).fullyFaithfulEquiv.symm <| fullyFaithfulCoconstantSheaf J
+noncomputable def fullyFaithfulConstantSheaf : (constantSheaf J A).FullyFaithful :=
+  (IsLocalSite.constantΓCoconstantTriple J A).fullyFaithfulEquiv.symm <|
+    fullyFaithfulCoconstantSheaf.{w} J A
 
-instance [HasWeakSheafify J (Type max u v w)] [J.IsLocalSite] :
-    (constantSheaf J (Type max u v w)).Full :=
-  (fullyFaithfulConstantSheaf J).full
+instance : (constantSheaf J A).Full :=
+  (fullyFaithfulConstantSheaf.{w} J A).full
 
-instance [HasWeakSheafify J (Type max u v w)] [J.IsLocalSite] :
-    (constantSheaf J (Type max u v w)).Faithful :=
-  (fullyFaithfulConstantSheaf J).faithful
-
-open List in
-/-- For any site with a terminal object, the following are equivalent:
-* the site is local, i.e. the only covering sieve of the terminal object is the trivial one
-* every covering sieve contains all morphisms from the terminal object
-* the coconstant presheaf on the empty type is a sheaf
-* every coconstant presheaf is a sheaf. -/
-protected theorem GrothendieckTopology.IsLocalSite.tfae [HasTerminal C] :
-    TFAE [J.IsLocalSite,
-      ∀ X : C, ∀ S ∈ J X, ∀ x : ⊤_ C ⟶ X, S x,
-      Presieve.IsSheaf J (Presheaf.coconst.{max u v}.obj PEmpty),
-      ∀ X : Type max u v, Presieve.IsSheaf J (Presheaf.coconst.obj X)] := by
-  tfae_have 2 → 1 := fun h ↦ ⟨fun S hS ↦ S.id_mem_iff_eq_top.1 <| h _ S hS _⟩
-  tfae_have 1 → 2 := fun h X S hS f ↦ by
-    simpa using Sieve.id_mem_iff_eq_top.2 <| h.eq_top_of_mem _ <| J.pullback_stable f hS
-  tfae_have 3 → 1 := fun h ↦ ⟨fun S hS ↦ by
-    replace h : IsEmpty (Presieve.FamilyOfElements
-        (Presheaf.coconst.{max u v}.obj PEmpty) S.arrows) := by
-      have : IsEmpty ((Presheaf.coconst.{max u v}.obj PEmpty).obj (op (⊤_ C))) := by
-        dsimp [Presheaf.coconst]; exact isEmpty_fun.2 ⟨⟨𝟙 _⟩, inferInstance⟩
-      have {X : C} : Subsingleton ((Presheaf.coconst.{max u v}.obj PEmpty).obj (op X)) :=
-        Pi.instSubsingleton
-      refine not_nonempty_iff.1 fun ⟨x⟩ ↦ IsEmpty.false (h S hS x ?_).choose
-      exact fun _ _ _ _ _ _ _ _ _ _ ↦ Subsingleton.elim _ _
-    replace ⟨X, f, hf, h⟩ : ∃ X, ∃ f : X ⟶ ⊤_ C, S f ∧
-        IsEmpty ((Presheaf.coconst.{max u v}.obj PEmpty).obj (op X)) := by
-      by_contra! h'; exact h.false fun X f hf ↦ (h' X f hf).some
-    let ⟨(g : _ ⟶ _)⟩ := (isEmpty_fun.1 h).1
-    refine S.id_mem_iff_eq_top.1 ?_
-    convert S.downward_closed hf g
-    exact Subsingleton.elim _ _⟩
-  tfae_have 4 → 3 := fun h ↦ h _
-  tfae_have 1 → 4 := fun _ _ ↦ (isSheaf_iff_isSheaf_of_type _ _).1 <| Presheaf.coconst_isSheaf J _
-  tfae_finish
+instance : (constantSheaf J A).Faithful :=
+  (fullyFaithfulConstantSheaf.{w} J A).faithful
 
 end CategoryTheory
