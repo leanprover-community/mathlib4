c5765d827f21cd3b2149186780530f3b89c54e4b
Yakov Pechersky <ffxen158@gmail.com>
2026-06-29 11:11:15 -0400

    discrete topology on pnat, declared earlier

--- Mathlib/Topology/Order.lean
+++ Mathlib/Topology/Order.lean
@@ -570,6 +570,9 @@ instance : DiscreteTopology Bool := ⟨rfl⟩
 instance : TopologicalSpace ℕ := ⊥
 instance : DiscreteTopology ℕ := ⟨rfl⟩
 
+instance : TopologicalSpace ℕ+ := ⊥
+instance : DiscreteTopology ℕ+ := ⟨rfl⟩
+
 instance : TopologicalSpace ℤ := ⊥
 instance : DiscreteTopology ℤ := ⟨rfl⟩
 
