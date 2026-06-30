f21777134ce2e79eb6aa01f98a35c42fee463d18
Joël Riou <joel.riou@universite-paris-saclay.fr>
2026-06-30 11:35:48 +0200

    feat(CategoryTheory/Presentable): constructor for `IsCardinalPresentable`

--- Mathlib/CategoryTheory/Presentable/Basic.lean
+++ Mathlib/CategoryTheory/Presentable/Basic.lean
@@ -237,6 +237,33 @@ lemma isCardinalPresentable_iff_of_isEquivalence
   · intro
     infer_instance
 
+variable {X κ} in
+set_option backward.isDefEq.respectTransparency false in
+set_option backward.defeqAttrib.useBackward true in
+open IsFiltered in
+lemma IsCardinalPresentable.mk
+    (hX : ∀ (J : Type w) (_ : Category.{w} J) (_ : IsCardinalFiltered J κ)
+      (F : J ⥤ C) (c : Cocone F) (_ : IsColimit c),
+      (∀ (g : X ⟶ c.pt), ∃ (j : J) (f : X ⟶ F.obj j), f ≫ c.ι.app j = g) ∧
+      (∀ (j : J) (f₁ f₂ : X ⟶ F.obj j) (_ : f₁ ≫ c.ι.app j = f₂ ≫ c.ι.app j),
+        ∃ (j' : J) (a : j ⟶ j'), f₁ ≫ F.map a = f₂ ≫ F.map a)) :
+    IsCardinalPresentable X κ where
+  preservesColimitOfShape J _ _ :=
+    ⟨fun {F} ↦ ⟨fun {c} hc ↦ by
+      have := isFiltered_of_isCardinalFiltered J κ
+      rw [Types.isColimit_iff_coconeTypesIsColimit]
+      refine ⟨fun f₁ f₂ hf ↦ ?_, fun g ↦ ?_⟩
+      · obtain ⟨j₁, f₁, rfl⟩ := Functor.ιColimitType_jointly_surjective _ f₁
+        obtain ⟨j₂, f₂, rfl⟩ := Functor.ιColimitType_jointly_surjective _ f₂
+        dsimp at f₁ f₂ hf
+        obtain ⟨j', a, ha⟩ := (hX J _ inferInstance F c hc).2 _ (f₁ ≫ F.map (leftToMax j₁ j₂))
+          (f₂ ≫ F.map (rightToMax j₁ j₂)) (by simpa)
+        simp only [Category.assoc] at ha
+        exact Functor.ιColimitType_eq_of_map_eq_map _ _ _
+          (leftToMax j₁ j₂ ≫ a) (rightToMax j₁ j₂ ≫ a) (by simpa)
+      · obtain ⟨j, f, rfl⟩ := (hX J _ inferInstance F c hc).1 g
+        exact ⟨Functor.ιColimitType _ j f, rfl⟩⟩⟩
+
 section
 
 variable {J : Type*} [Category* J] {D : J ⥤ C}
