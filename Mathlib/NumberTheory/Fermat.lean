ea8213d16afc715cc59f0d74913ffee9094defa9
Batixx <s59fpern@uni-bonn.de>
2026-06-30 07:07:45 +0200

    simplify

--- Mathlib/NumberTheory/Fermat.lean
+++ Mathlib/NumberTheory/Fermat.lean
@@ -131,12 +131,11 @@ theorem pow_of_pow_add_prime {a n : ℕ} (ha : 1 < a) (hn : n ≠ 0) (hP : (a ^ 
 lemma pepin_primality (n : ℕ) (h : 3 ^ (2 ^ (2 ^ n - 1)) = (-1 : ZMod (fermatNumber n))) :
     (fermatNumber n).Prime := by
   have := Fact.mk (two_lt_fermatNumber n)
+  unfold fermatNumber at h this
   have key : 2 ^ n = 2 ^ n - 1 + 1 := (Nat.sub_add_cancel Nat.one_le_two_pow).symm
   apply lucas_primality (p := 2 ^ (2 ^ n) + 1) (a := 3)
-  · unfold fermatNumber at h
-    rw [Nat.add_sub_cancel, key, pow_succ, pow_mul, ← pow_succ, ← key, h, neg_one_pow_two]
+  · rw [Nat.add_sub_cancel, key, pow_succ, pow_mul, ← pow_succ, ← key, h, neg_one_pow_two]
   · intro p hp1 hp2
-    unfold fermatNumber at h this
     rw [Nat.add_sub_cancel, (Nat.prime_dvd_prime_iff_eq hp1 prime_two).mp (hp1.dvd_of_dvd_pow hp2),
         key, pow_succ, Nat.mul_div_cancel _ two_pos, ← pow_succ, ← key, h]
     exact neg_one_ne_one
