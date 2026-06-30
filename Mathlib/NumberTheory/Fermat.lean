18ac73b646a41dc20f37785a3fa0664d8fe0bf16
Batixx <s59fpern@uni-bonn.de>
2026-06-30 07:17:54 +0200

    .

--- Mathlib/NumberTheory/Fermat.lean
+++ Mathlib/NumberTheory/Fermat.lean
@@ -134,7 +134,7 @@ lemma pepin_primality (n : ℕ) (h : 3 ^ (2 ^ (2 ^ n - 1)) = (-1 : ZMod (fermatN
   unfold fermatNumber at h this
   have key : 2 ^ n = 2 ^ n - 1 + 1 := (Nat.sub_add_cancel Nat.one_le_two_pow).symm
   apply lucas_primality (p := 2 ^ (2 ^ n) + 1) (a := 3)
-  · rw [Nat.add_sub_cancel, key, pow_succ, pow_mul, ← pow_succ, ← key, h, neg_one_pow_two]
+  · rw [Nat.add_sub_cancel, key, pow_succ, pow_mul, ← pow_succ, ← key, h, neg_one_sq]
   · intro p hp1 hp2
     rw [Nat.add_sub_cancel, (Nat.prime_dvd_prime_iff_eq hp1 prime_two).mp (hp1.dvd_of_dvd_pow hp2),
         key, pow_succ, Nat.mul_div_cancel _ two_pos, ← pow_succ, ← key, h]
