bd464a02a5edf85c523c8b41b5910ec232923189
Joël Riou <joel.riou@universite-paris-saclay.fr>
2026-06-30 10:17:29 +0200

    fix

--- Mathlib/CategoryTheory/Presentable/Comma.lean
+++ Mathlib/CategoryTheory/Presentable/Comma.lean
@@ -3,6 +3,7 @@ Copyright (c) 2024 Joël Riou. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joël Riou
 -/
+module
 
 import Mathlib.CategoryTheory.Presentable.Dense
 import Mathlib.CategoryTheory.Presentable.LocallyPresentable
