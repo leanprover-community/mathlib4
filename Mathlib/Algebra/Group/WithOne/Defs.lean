3593dc481819f87147d397713d21143e1a1764c4
Raph-DG <raphaeldouglasgiles@gmail.com>
2026-06-30 13:46:08 +0200

    Fixed to_additive warnings

--- Mathlib/Algebra/Group/WithOne/Defs.lean
+++ Mathlib/Algebra/Group/WithOne/Defs.lean
@@ -125,15 +125,15 @@ lemma recOneCoe_coe {motive : WithOne α → Sort*} (h₁ h₂) (a : α) :
   rfl
 
 /-- Deconstruct an `x : WithOne α` to the underlying value in `α`, given a proof that `x ≠ 1`. -/
-@[to_additive unzero
+@[to_additive
 /-- Deconstruct an `x : WithZero α` to the underlying value in `α`, given a proof that `x ≠ 0`. -/]
 def unone : ∀ {x : WithOne α}, x ≠ 1 → α | (x : α), _ => x
 
-@[to_additive (attr := simp) unzero_coe]
+@[to_additive (attr := simp)]
 theorem unone_coe {x : α} (hx : (x : WithOne α) ≠ 1) : unone hx = x :=
   rfl
 
-@[to_additive (attr := simp) coe_unzero]
+@[to_additive (attr := simp)]
 lemma coe_unone : ∀ {x : WithOne α} (hx : x ≠ 1), unone hx = x
   | (x : α), _ => rfl
 
