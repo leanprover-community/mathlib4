8e899a3e6d26c02e7846b519b9b0f70bb4981e54
Yannis Monbru <yannis.monbru@orange.fr>
2026-07-01 13:40:22 +0200

    modification following the review of J.Riou

--- Mathlib/Topology/Sets/BaseChangeNhds.lean
+++ Mathlib/Topology/Sets/BaseChangeNhds.lean
@@ -29,38 +29,50 @@ variable {α : Type*} [TopologicalSpace α]
 
 namespace Opens
 
-/-- For U an open subset included in a open subset V, there is
-a map sending compacts inside U to the ones inside V -/
+/-- For `U` an open subset included in a open subset `V`, there is
+a map sending compacts inside `U` to the ones inside `V` -/
 def baseChangeCompactInsd {U V : Opens α} (h : U ⟶ V) : U.compactInsd → V.compactInsd :=
-  fun ⟨K,hK⟩ ↦ ⟨K, fun _ hx ↦ Set.mem_of_subset_of_mem (leOfHom h) (hK hx)⟩
+  fun ⟨K, hK⟩ ↦ ⟨K, fun _ hx ↦ Set.mem_of_subset_of_mem (leOfHom h) (hK hx)⟩
 
-lemma monoBaseChangeCompactInsd {U V : Opens α} (h : U ⟶ V) : Monotone <| baseChangeCompactInsd h :=
+lemma baseChangeCompactInsd_mono {U V : Opens α} (h : U ⟶ V) : Monotone <| baseChangeCompactInsd h :=
   fun _ _ hKL _ hx ↦ SetLike.mem_coe.mpr (hKL hx)
+
+@[simp]
+lemma baseChangeCompactInsd_comp {U V W : Opens α} (h : U ⟶ V) (k : V ⟶ W) (K : U.compactInsd) :
+  baseChangeCompactInsd (h ≫ k) K = baseChangeCompactInsd k (baseChangeCompactInsd h K) := by rfl
+
+@[simp]
+lemma baseChangeOpenNhds_id {U : Opens α} (K : U.compactInsd) :
+  baseChangeCompactInsd (𝟙 U) K = K := by rfl
 
 end Opens
 
 namespace Compacts
 
-/-- For K a compact subset included in a compact subset L, there
-is a map sending open neighbourhoods of L to the ones of K -/
+/-- For `K` a compact subset included in a compact subset `L`, there
+is a map sending open neighbourhoods of `L` to the ones of `K` -/
 def baseChangeOpenNhds {K L : Compacts α} (h : K ⟶ L) : L.openNhds → K.openNhds :=
-  fun ⟨U,hU⟩ ↦ ⟨U, fun _ hx ↦ Set.mem_of_subset_of_mem hU (leOfHom h hx)⟩
+  fun ⟨U, hU⟩ ↦ ⟨U, fun _ hx ↦ Set.mem_of_subset_of_mem hU (leOfHom h hx)⟩
 
-lemma monoBaseChangeOpenNhds {K L : Compacts α} (h : K ⟶ L) : Monotone <| baseChangeOpenNhds h :=
+lemma baseChangeOpenNhds_mono {K L : Compacts α} (h : K ⟶ L) : Monotone <| baseChangeOpenNhds h :=
   fun _ _ hUV _ hx ↦ SetLike.mem_coe.mpr (hUV hx)
 
 @[simp]
 lemma baseChangeOpenNhds_comp {K L M : Compacts α} (h : K ⟶ L) (k : L ⟶ M) (U : M.openNhds) :
   baseChangeOpenNhds (h ≫ k) U = baseChangeOpenNhds h (baseChangeOpenNhds k U) := by rfl
 
-/-- The evidence that ⊥ is initial among the open Nhds of ⊥ -/
-def isInitialElemOpensOpenNhdsBot : IsInitial (⊥ : (⊥ : Compacts α).openNhds) := .ofUniqueHom
+@[simp]
+lemma baseChangeOpenNhds_id {K : Compacts α} (U : K.openNhds) :
+  baseChangeOpenNhds (𝟙 K) U = U := by rfl
+
+/-- The evidence that `⊥` is initial among the compact open neighbourhoods of `⊥` -/
+def isInitialBotOpensOpenNhdsBot : IsInitial (⊥ : (⊥ : Compacts α).openNhds) := .ofUniqueHom
   (fun _ ↦ homOfLE <| by tauto)
   (fun _ _ ↦ eq_of_comp_right_eq <| by tauto)
 
 instance {K : Compacts α} [T2Space α] [LocallyCompactSpace α] :
-    K.mono_oRcNhds_to_openNhds.functor.Initial := by
-  apply (Monotone.initial_functor_iff _).2
+    K.oRcNhdsToOpenNhds_mono.functor.Initial := by
+  rw [Monotone.initial_functor_iff]
   intro U
   obtain ⟨L, h1, h2, h3⟩ := exists_compact_between K.isCompact U.val.isOpen U.property
   use ⟨⟨interior L, isOpen_interior⟩,
@@ -69,8 +81,8 @@ instance {K : Compacts α} [T2Space α] [LocallyCompactSpace α] :
     h2⟩⟩
   exact Subset.trans interior_subset h3
 
-instance {K : Compacts α} [T2Space α] : K.mono_oRcNhds_to_compactNhds.functor.Initial := by
-  apply (Monotone.initial_functor_iff _).2
+instance {K : Compacts α} [T2Space α] : K.oRcNhdsToCompactNhds_mono.functor.Initial := by
+  rw [Monotone.initial_functor_iff]
   intro L
   obtain ⟨U, h1, h2⟩ := exists_open_nhds_sub_compact_nhds L
   have h3 : closure (U : Set α) ⊆ L := (IsClosed.closure_subset_iff
