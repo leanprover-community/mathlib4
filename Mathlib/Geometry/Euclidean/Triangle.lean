c2be0f264cba9b92705d22689c144c42201f8d1e
Oliver Nash <github@olivernash.org>
2026-07-01 13:06:38 +0100

    Avoid unfolding `angle` by adding missing lemmas
    Specifically the additions are:
    - `angle_smul_right_of_pos`
    - `angle_smul_left_of_pos`
    
    This allows improving two existing proofs which were also unfolding
    and were somewhat brittle.
    
    Also some minor style tweaks.

--- Mathlib/Geometry/Euclidean/Triangle.lean
+++ Mathlib/Geometry/Euclidean/Triangle.lean
@@ -361,10 +361,10 @@ lemma angle_add_of_ne_of_ne {a b c p : P} (hb : a ≠ b) (hc : a ≠ c) (hp : Wb
 with no nondegeneracy assumption on the point `P`. -/
 lemma angle_add_angle_eq_of_sbtw {a c p x : P} (hx : Sbtw ℝ a x c) :
     ∠ a p x + ∠ x p c = ∠ a p c := by
-  by_cases hpa : p = a
-  · subst p; simp [(hx.angle_eq_right x).symm.trans (angle_self_of_ne hx.ne_left)]
-  by_cases hpc : p = c
-  · subst p; simp [(hx.symm.angle_eq_right a).trans (angle_self_of_ne hx.left_ne_right)]
+  rcases eq_or_ne p a with rfl | hpa
+  · simp [(hx.angle_eq_right x).symm.trans (angle_self_of_ne hx.ne_left)]
+  rcases eq_or_ne p c with rfl | hpc
+  · simp [(hx.symm.angle_eq_right a).trans (angle_self_of_ne hx.left_ne_right)]
   exact angle_add_of_ne_of_ne hpa hpc hx.wbtw
 
 /-- If `B` lies on the same ray from `P` as a point `X` strictly between `A` and `C`,
@@ -372,17 +372,13 @@ then `∠ A P B + ∠ B P C = ∠ A P C`. -/
 theorem angle_add_angle_eq_of_sbtw_of_sameRay {a b c p x : P}
     (hx : Sbtw ℝ a x c) (hxb : SameRay ℝ (x -ᵥ p) (b -ᵥ p)) (hb : b ≠ p) :
     ∠ a p b + ∠ b p c = ∠ a p c := by
-  by_cases hxp : x = p
-  · subst x
-    have hpi : ∠ a p c = π := hx.angle₁₂₃_eq_pi
+  rcases eq_or_ne p x with rfl | hpx
+  · have hpi : ∠ a p c = π := hx.angle₁₂₃_eq_pi
     rw [hpi, angle_comm a p b]
     exact angle_add_angle_eq_pi_of_angle_eq_pi b hpi
-  obtain ⟨r, hr, hrb⟩ :=
-    (exists_pos_left_iff_sameRay (vsub_ne_zero.2 hxp) (vsub_ne_zero.2 hb)).2 hxb
-  have hab : ∠ a p b = ∠ a p x := by
-    unfold EuclideanGeometry.angle; rw [← hrb]; exact angle_smul_right_of_pos _ _ hr
-  have hbc : ∠ b p c = ∠ x p c := by
-    unfold EuclideanGeometry.angle; rw [← hrb]; exact angle_smul_left_of_pos _ _ hr
+  obtain ⟨r, hr, hrb⟩ := (exists_pos_left_iff_sameRay (by aesop) (by aesop)).2 hxb
+  have hab : ∠ a p b = ∠ a p x := angle_smul_right_of_pos a hr hrb
+  have hbc : ∠ b p c = ∠ x p c := angle_smul_left_of_pos c hr hrb
   rw [hab, hbc]
   exact angle_add_angle_eq_of_sbtw hx
 
