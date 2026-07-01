b217f65b3d6652bdf30146af74cebaabcd593904
smorel394 <67864981+smorel394@users.noreply.github.com>
2026-07-01 12:28:06 +0000

    feat(CategoryTheory/Limits/WeakLimits): weak limits (#41075)
    Define weak limits and add some minimal API for them. Also consider the special cases of weak equalizers, weak kernels and weak pullbacks. Prove that a category with weak equalizers and pullbacks has weak pullbacks, and that a preadditive category has weak equalizers if and only if it has weak kernels.
    
    If `F : J ⥤ C` is a functor and `c : Cone F`, we say that `c` is a weak limit of `F` if every cone over `F` admits a (not necessarily unique) morphism to `c`. In other words, a weak limit satisfies the same "versal property" as a limit, without the uniqueness condition. In particular, weak limits are not unique, and they are not functorial.
    
    TODO:
    * Prove that a triangulated category has weak kernels: this is #41162.
    * If `C` is an additive category with weak limits, then its cokernel completion (also TODO) is an abelian category.
    
    Co-authored-by: morel <sophie.morel@ens-lyon.fr>
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -2994,6 +2994,10 @@ public import Mathlib.CategoryTheory.Limits.Types.Pushouts
 public import Mathlib.CategoryTheory.Limits.Types.Yoneda
 public import Mathlib.CategoryTheory.Limits.Unit
 public import Mathlib.CategoryTheory.Limits.VanKampen
+public import Mathlib.CategoryTheory.Limits.WeakLimits.Basic
+public import Mathlib.CategoryTheory.Limits.WeakLimits.WeakEqualizers
+public import Mathlib.CategoryTheory.Limits.WeakLimits.WeakKernels
+public import Mathlib.CategoryTheory.Limits.WeakLimits.WeakPullbacks
 public import Mathlib.CategoryTheory.Limits.Yoneda
 public import Mathlib.CategoryTheory.Linear.Basic
 public import Mathlib.CategoryTheory.Linear.FunctorCategory
