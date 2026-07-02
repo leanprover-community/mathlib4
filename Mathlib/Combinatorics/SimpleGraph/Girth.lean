db7b2011d5500318b89c7a185d67be5afa43a2dd
Rida Hamadani <mridahamadani@gmail.com>
2026-07-02 11:33:15 +0200

    remove name from authors
    Removed Rida Hamadani as an author from the Girth.lean file.

--- Mathlib/Combinatorics/SimpleGraph/Girth.lean
+++ Mathlib/Combinatorics/SimpleGraph/Girth.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2023 Yaël Dillies. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Yaël Dillies, Rida Hamadani
+Authors: Yaël Dillies
 -/
 module
 
