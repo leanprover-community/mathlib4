f42448dc9f09d5bef78c2952838d48c69a52e386
Yakov Pechersky <ffxen158@gmail.com>
2026-07-02 18:03:56 -0400

    remove duplicate Commute

--- Mathlib/Algebra/Group/PPow/Basic.lean
+++ Mathlib/Algebra/Group/PPow/Basic.lean
@@ -45,22 +45,6 @@ lemma ppow_mul_comm' (x : M) (n : ℕ+) : x ^ n * x = x * x ^ n := by
 lemma ppow_mul (x : M) (n m : ℕ+) : x ^ (n * m) = (x ^ n) ^ m :=
   m.recOn (by simp) fun k hk => by simp [mul_add, ppow_add, hk]
 
-@[to_additive]
-lemma Commute.ppow_left {x y : M} (h : Commute x y) (n : ℕ+) : Commute (x ^ n) y :=
-  n.recOn ((ppow_one x).symm ▸ h) fun k hk => ppow_succ x k ▸ hk.mul_left h
-
-@[to_additive]
-lemma Commute.ppow_right {x y : M} (h : Commute x y) (n : ℕ+) : Commute x (y ^ n) :=
-  (h.symm.ppow_left n).symm
-
-@[to_additive Commute.psmul_add]
-lemma Commute.mul_ppow {x y : M} (h : Commute x y) (n : ℕ+) : (x * y) ^ n = x ^ n * y ^ n :=
-  n.recOn (by simp) fun k hk => by
-    simp only [ppow_succ, hk, mul_assoc]
-    rw [← mul_assoc x, ← mul_assoc (y ^ k)]
-    congr 2
-    exact (h.symm.ppow_left k).eq
-
 end Semigroup
 
 section CommSemigroup
