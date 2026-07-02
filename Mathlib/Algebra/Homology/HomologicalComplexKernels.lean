e13ffc86eea445ba2c4e9ceccd1b3430f7da3f14
Joël Riou <joel.riou@universite-paris-saclay.fr>
2026-07-02 16:49:12 +0200

    wip

--- Mathlib/Algebra/Homology/HomologicalComplexKernels.lean
+++ Mathlib/Algebra/Homology/HomologicalComplexKernels.lean
@@ -36,4 +36,22 @@ lemma hasCokernel_of_hasCokernel_f [∀ i, HasCokernel (f.f i)] : HasCokernel f 
       (parallelPair.ext (Iso.refl _) (Iso.refl _))
   ⟨_, isColimitCoconeOfHasColimitEval _⟩
 
+set_option backward.defeqAttrib.useBackward true in
+lemma eval_preservesKernel_of_hasKernel_f [∀ i, HasKernel (f.f i)] (i : ι) :
+    PreservesLimit (parallelPair f 0) (eval C c i) :=
+  have (i : ι) : HasLimit (parallelPair f 0 ⋙ eval C c i) :=
+    hasLimit_of_iso (F := (parallelPair (f.f i) 0))
+      (parallelPair.ext (Iso.refl _) (Iso.refl _))
+  preservesLimit_of_preserves_limit_cone
+    (isLimitConeOfHasLimitEval _) (limit.isLimit _)
+
+set_option backward.defeqAttrib.useBackward true in
+lemma eval_preservesCokernel_of_hasCokernel_f [∀ i, HasCokernel (f.f i)] (i : ι) :
+    PreservesColimit (parallelPair f 0) (eval C c i) :=
+  have (i : ι) : HasColimit (parallelPair f 0 ⋙ eval C c i) :=
+    hasColimit_of_iso (F := (parallelPair (f.f i) 0))
+      (parallelPair.ext (Iso.refl _) (Iso.refl _))
+  preservesColimit_of_preserves_colimit_cocone
+    (isColimitCoconeOfHasColimitEval _) (colimit.isColimit _)
+
 end HomologicalComplex
