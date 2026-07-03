f31e5ef8365d3bcd574a202ce3242128783384ef
Etienne Marion <baguettes_casino0c@icloud.com>
2026-07-03 09:15:48 +0200

    wip

--- Mathlib/Probability/Distributions/Bernoulli.lean
+++ Mathlib/Probability/Distributions/Bernoulli.lean
@@ -140,6 +140,15 @@ lemma bernoulliMeasure_real_apply_of_notMem_of_notMem (p : I) {s : Set X}
   classical
   simp_all [bernoulliMeasure_real_apply]
 
+@[simp]
+lemma bernoulliMeasure_real_apply_singleton_left [MeasurableSingletonClass X] (p : I) (h : x ≠ y) :
+    Ber(x, y, p).real {x} = p :=
+  bernoulliMeasure_real_apply_of_mem_of_notMem p (by simp) (by simp) (by grind)
+
+lemma bernoulliMeasure_real_apply_singleton_right [MeasurableSingletonClass X] (p : I) (h : x ≠ y) :
+    Ber(x, y, p).real {y} = 1 - p := by
+  simp [h]
+
 instance : IsProbabilityMeasure Ber(x, y, p) where
   measure_univ := by simp [bernoulliMeasure_def]
 
