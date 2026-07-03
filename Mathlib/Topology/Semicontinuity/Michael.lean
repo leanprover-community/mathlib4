4b07898726a610737bf8b2f81c3eedae9ac1a207
Kevin H Wilson <khwilson@gmail.com>
2026-07-03 10:29:08 -0400

    pull out iteration lemma

--- Mathlib/Topology/Semicontinuity/Michael.lean
+++ Mathlib/Topology/Semicontinuity/Michael.lean
@@ -78,6 +78,29 @@ lemma HasOpenLowerSections.exists_continuous_selection (hf : HasOpenLowerSection
     (hf_convex y).finsum_mem (fun i ↦ φ.nonneg i y) (φ.sum_eq_one (mem_univ y)) fun x' hx' ↦
       hφ x' (subset_tsupport _ hx')⟩
 
+omit [ContinuousAdd β] in
+variable [IsTopologicalAddGroup β] in
+/-- **Michael's selection theorem (iteration):** An approximate continuous selection `g` to a
+correspondence `f` with open lower sections and convex values can be refined to an approximate
+continuous selection `h` whose values `h x` are both closer to `f x` and are close to `g x`. -/
+lemma LowerHemicontinuous.exists_continuous_selection_refine
+    (hf : LowerHemicontinuous f) (hf_convex : ∀ x, Convex ℝ (f x))
+    {g : α → β} (hg : Continuous g) {W V : Set β} (hW_open : IsOpen W) (hW_convex : Convex ℝ W)
+    (hV_open : IsOpen V) (hV_convex : Convex ℝ V) (hW_zero : 0 ∈ W) (hV_symm : V = -V)
+    (hgV : ∀ x, g x ∈ f x + V) :
+      ∃ h : α → β, Continuous h ∧ (∀ x, h x ∈ f x + W) ∧ (∀ x, h x ∈ {g x} + V) := by
+  have h₁ : HasOpenLowerSections fun x ↦ (f x + W) ∩ ({g x} + V) := by
+    apply hf.hasOpenCGraph_of_add_isOpen hW_open |>.hasOpenLowerSections.inter
+    exact hg.lowerHemicontinuous.hasOpenCGraph_of_add_isOpen hV_open |>.hasOpenLowerSections
+  have h₂ (x : α) : ((f x + W) ∩ ({g x} + V)).Nonempty := by
+    obtain ⟨a, ha, b, hb, hab⟩ := hgV x
+    refine ⟨g x + (-b), ?_, ?_⟩
+    · refine ⟨a, ha, 0, by grind⟩
+    · refine ⟨g x, rfl, -b, by rw [hV_symm]; exact Set.neg_mem_neg.mpr hb, by simp⟩
+  have h₃ (x : α) : Convex ℝ ((f x + W) ∩ ({g x} + V)) :=
+    (hf_convex x).add hW_convex |>.inter <| (convex_singleton _).add hV_convex
+  grind [h₁.exists_continuous_selection h₂ h₃]
+
 end approximate
 
 section michael
@@ -101,34 +124,18 @@ theorem LowerHemicontinuous.exists_continuous_selection (hf : LowerHemicontinuou
       (fun x ↦ (hf_convex x).add (hV.2 0).2.1.2)
   obtain ⟨h, hh_cont, hh_mem, hh_mem_ball⟩ : ∃ h : ℕ → α → β, (∀ n, Continuous (h n)) ∧
       (∀ n x, h n x ∈ f x + (V n)) ∧
-      (∀ n x, h (n + 1) x - h n x ∈ V n) := by
+      (∀ n x, h (n + 1) x ∈ {h n x} + V n) := by
     let P (n : ℕ) (h : α → β) := Continuous h ∧ ∀ x, h x ∈ f x + (V n)
-    have step : ∀ n hn, P n hn →
-        ∃ h', P (n + 1) h' ∧ ∀ x, (h' x) - (hn x) ∈ V n := by
-      intro n hn hn_prop
-      have : HasOpenLowerSections (fun x ↦ ((f x + V (n + 1)) ∩ ({hn x} + V n))) := by
-        apply HasOpenLowerSections.inter
-        · exact hf.hasOpenCGraph_of_add_isOpen (hV.2 (n + 1)).1 |>.hasOpenLowerSections
-        · exact hn_prop.1.lowerHemicontinuous.hasOpenCGraph_of_add_isOpen (hV.2 n).1
-            |>.hasOpenLowerSections
-      obtain ⟨h', hh'_cont, hh'_mem⟩ := this.exists_continuous_selection (by
-          intro x
-          obtain ⟨a, ha, v, hv, hav⟩ := (hn_prop.2 x)
-          exact ⟨hn x + (-v), ⟨a, ha, 0, mem_of_mem_nhds (hV.1.mem (n + 1)), by simp [← hav]⟩,
-              by simp [(hV.2 n).2.1.1.neg_mem_iff.mpr hv]⟩)
-        fun x ↦ ((hf_convex x).add (hV.2 (n + 1)).2.1.2).inter ((convex_singleton _).add
-          (hV.2 n).2.1.2)
-      use h'
-      refine ⟨⟨hh'_cont, fun x ↦ (hh'_mem x).1⟩, fun x ↦ ?_⟩
-      obtain ⟨_, rfl, v, hv, hv'⟩ := (hh'_mem x).2
-      simp [← hv', hv]
-    choose! F hF using step
+    choose! F hF using fun n hn (hn_prop : P n hn) =>
+      hf.exists_continuous_selection_refine hf_convex hn_prop.1
+        (hV.2 (n + 1)).1 (hV.2 (n + 1)).2.1.2 (hV.2 n).1 (hV.2 n).2.1.2
+        (mem_of_mem_nhds (hV.1.mem (n + 1))) (hV.2 n).2.1.1.neg_eq.symm hn_prop.2
     use Nat.rec g F
     rw [← forall_and, ← forall_and]
     intro n
     induction n with
-    | zero => simp [hg_cont, hg_mem, hF, P]
-    | succ n ih => simp [ih, P, hF]
+    | zero => simp [hg_cont, hg_mem, hF, P, -singleton_add]
+    | succ n ih => simp [ih, P, hF, -singleton_add]
   -- Prove the sequence is uniformly cauchy. The (necessarily continuous) limit is a selection
   have hUnif : UniformCauchySeqOn h Filter.atTop univ := by
     intro U hU
@@ -139,7 +146,9 @@ theorem LowerHemicontinuous.exists_continuous_selection (hf : LowerHemicontinuou
         intro k j x _; simp only [Nat.add_zero, sub_self]; exact mem_of_mem_nhds (hV.1.mem k)
       | succ m ih =>
         intro k j x hj
-        have h1 : h (j + 1) x - h j x ∈ V (k + 1) := hV.1.antitone hj (hh_mem_ball j x)
+        obtain ⟨_, rfl, v, hv, hv'⟩ := hh_mem_ball j x
+        have h1 : h (j + 1) x - h j x ∈ V (k + 1) := by
+          simpa [← hv'] using hV.1.antitone hj hv
         have h2 : h (j + 1 + m) x - h (j + 1) x ∈ V (k + 1) := ih (k + 1) (j + 1) x (by omega)
         convert (hV.2 k).2.2.1 (Set.add_mem_add h1 h2) using 1
         abel_nf
