23fc186f8296531cb9842f1a72568bb63b4f53f3
Joël Riou <joel.riou@universite-paris-saclay.fr>
2026-07-03 08:14:43 +0200

    the long exact sequence

--- Mathlib/AlgebraicTopology/SimplicialSet/Homology/Relative.lean
+++ Mathlib/AlgebraicTopology/SimplicialSet/Homology/Relative.lean
@@ -5,7 +5,9 @@ Authors: Joël Riou, Andrew Yang
 -/
 module
 
+public import Mathlib.Algebra.Homology.HomologicalComplexAbelian
 public import Mathlib.Algebra.Homology.HomologicalComplexKernels
+public import Mathlib.Algebra.Homology.HomologySequence
 public import Mathlib.AlgebraicTopology.SimplicialSet.Homology.Basic
 public import Mathlib.AlgebraicTopology.SimplicialSet.SSetPair
 public import Mathlib.CategoryTheory.Limits.FunctorCategory.Shapes.Kernels
@@ -25,6 +27,8 @@ open Simplicial CategoryTheory Limits Opposite
 universe w v u
 
 namespace SSetPair
+
+section
 
 variable (C : Type u) [Category.{v} C] [HasCoproducts.{w} C] [Preadditive C]
 
@@ -54,14 +58,31 @@ noncomputable abbrev chainComplexFunctorLeftToRight :
   Functor.whiskerLeft _ ((Functor.whiskeringLeft ..).map
     (Functor.whiskerLeft _ Arrow.leftToRight))
 
+instance (R : C) (P : SSetPair.{w}) (n : ℕ) :
+    Mono ((SSet.chainComplexMap P.hom R).f n) :=
+  inferInstanceAs (Mono ((sigmaConst.obj R).map (P.hom.app (op ⦋n⦌))))
+
+set_option backward.defeqAttrib.useBackward true in
+instance (R : C) (P : SSetPair.{w}) (n : ℕ) :
+    dsimp% Mono ((SSet.chainComplexMap P.hom R).f n) :=
+  inferInstanceAs (Mono ((SSet.chainComplexMap P.hom R).f n))
+
+instance (R : C) (P : SSetPair.{w}) :
+    Mono (SSet.chainComplexMap P.hom R) :=
+  HomologicalComplex.mono_of_mono_f _ inferInstance
+
+instance (R : C) (P : SSetPair.{w}) :
+    dsimp% Mono (SSet.chainComplexMap P.hom R) :=
+  HomologicalComplex.mono_of_mono_f _ inferInstance
+
 set_option backward.defeqAttrib.useBackward true in
 instance (R : C) (P : SSetPair.{w}) (n : ℕ) :
     Mono ((((chainComplexFunctorLeftToRight C).app R).app P).f n) :=
-  inferInstanceAs (Mono ((sigmaConst.obj R).map (P.hom.app (op ⦋n⦌))))
+  inferInstanceAs (Mono ((SSet.chainComplexMap P.hom R).f n))
 
 instance (R : C) (P : SSetPair.{w}) :
     Mono (((chainComplexFunctorLeftToRight C).app R).app P) :=
-  HomologicalComplex.mono_of_mono_f _ inferInstance
+  inferInstanceAs (Mono (SSet.chainComplexMap P.hom R))
 
 instance (R : C) : Mono ((chainComplexFunctorLeftToRight C).app R) :=
   NatTrans.mono_of_mono_app _
@@ -174,7 +195,7 @@ instance (R : C) (P : SSetPair.{w}) (n : ℕ) :
       (HomologicalComplex.eval C _ n) :=
   HomologicalComplex.eval_preservesCokernel_of_hasCokernel_f ..
 
-variable {C} (P : SSetPair.{w}) (R : C)
+variable {C} (P P' P'' : SSetPair.{w}) (f : P ⟶ P') (g : P' ⟶ P'') (R : C)
 
 /-- The chain complex of a pair of simplicial sets
 with coefficients in `R : C` (e.g. `C := Ab` and `R := ℤ`.) -/
@@ -252,5 +273,114 @@ noncomputable def isColimitCokernelCoforkChainComplexX (n : ℕ) :
 
 instance (n : ℕ) : Epi ((P.chainComplexπ R).f n) :=
   Cofork.IsColimit.epi (P.isColimitCokernelCoforkChainComplexX R n)
+
+/-- Given a pair of simplicial sets corresponding to a monomorphism `i : X ⟶ Y`,
+this is the (short exact) short complex which relates
+the chain complex of `X`, of `Y` and of the pair. -/
+@[simps]
+noncomputable def chainComplexShortComplex : ShortComplex (ChainComplex C ℕ) :=
+    ShortComplex.mk _ _ (P.chainComplex_condition R)
+
+set_option backward.defeqAttrib.useBackward true in
+instance : Mono (P.chainComplexShortComplex R).f := by dsimp; infer_instance
+
+set_option backward.defeqAttrib.useBackward true in
+instance : Epi (P.chainComplexShortComplex R).g := by dsimp; infer_instance
+
+variable {P P'} in
+/-- The morphism of simplicial chain complexes induces by a morphism
+of simplicial sets. -/
+noncomputable abbrev chainComplexMap : P.chainComplex R ⟶ P'.chainComplex R :=
+  ((SSetPair.chainComplexFunctor C).obj R).map f
+
+section
+
+variable [CategoryWithHomology C]
+
+/-- The relative simplicial homology with coefficients in `R : C` in degree `n`
+of a pair of simplicial sets. -/
+protected noncomputable abbrev homology (n : ℕ) : C := (P.chainComplex R).homology n
+
+variable {P P'} in
+/-- The morphism in relative simplicial homology that is induced by a morphism
+of pairs of simplicial sets. -/
+protected noncomputable abbrev homologyMap (n : ℕ) : P.homology R n ⟶ P'.homology R n :=
+  HomologicalComplex.homologyMap (chainComplexMap f R) n
+
+@[simp]
+lemma homologyMap_id (n : ℕ) : SSetPair.homologyMap (𝟙 P) R n = 𝟙 _ := by
+  simp [SSetPair.homologyMap]
+
+variable {P P' P''} in
+@[reassoc]
+lemma homologyMap_comp (n : ℕ) :
+    SSetPair.homologyMap (f ≫ g) R n =
+      SSetPair.homologyMap f R n ≫ SSetPair.homologyMap g R n := by
+  simp [SSetPair.homologyMap, HomologicalComplex.homologyMap_comp]
+
+attribute [local simp] homologyMap_comp in
+/-- The relative simplicial homology functor in degree `n` with coefficients in `R : C`. -/
+@[simps]
+noncomputable def homologyFunctor (n : ℕ) : SSetPair.{w} ⥤ C where
+  obj P := P.homology R n
+  map f := SSetPair.homologyMap f R n
+
+/-- Given a pair of simplicial sets corresponding to a monomorphism `i : X ⟶ Y`,
+this is the morphism from the homology of `Y` to the relative homology of the pair. -/
+noncomputable abbrev homologyπ (n : ℕ) : P.right.homology R n ⟶ P.homology R n :=
+  HomologicalComplex.homologyMap (P.chainComplexπ R) n
+
+set_option backward.isDefEq.respectTransparency false in
+@[reassoc (attr := simp)]
+lemma homologyMap_hom_homologyπ (n : ℕ) :
+    SSet.homologyMap P.hom R n ≫ P.homologyπ R n = 0 := by
+  simp [← HomologicalComplex.homologyMap_comp]
+
+end
+
+end
+
+section
+
+variable {A : Type u} [Category.{v} A] [HasCoproducts.{w} A] [Abelian A]
+  (P : SSetPair.{w}) (R : A)
+
+lemma shortExact_chainComplexShortComplex :
+    (P.chainComplexShortComplex R).ShortExact where
+  exact := ShortComplex.exact_of_g_is_cokernel _
+    (isColimitCokernelCoforkChainComplex ..)
+
+/-- Given a pair of simplicial sets corresponding to a monomorphism `i : X ⟶ Y`,
+this is the connecting morphism from the homology of the pair to the homology of `X`. -/
+noncomputable def homologyδ (n m : ℕ) (h : m + 1 = n := by lia) :
+    P.homology R n ⟶ P.left.homology R m :=
+  (P.shortExact_chainComplexShortComplex R).δ n m (by simpa)
+
+@[reassoc (attr := simp)]
+lemma homologyδ_comp (n m : ℕ) (h : m + 1 = n := by lia) :
+    P.homologyδ R n m h ≫ SSet.homologyMap P.hom R m = 0 :=
+  (P.shortExact_chainComplexShortComplex R).δ_comp n m (by simpa)
+
+@[reassoc (attr := simp)]
+lemma comp_homologyδ (n m : ℕ) (h : m + 1 = n := by lia) :
+    P.homologyπ R n ≫ P.homologyδ R n m h = 0 :=
+  (P.shortExact_chainComplexShortComplex R).comp_δ n m (by simpa)
+
+lemma homology_exact₁ (n m : ℕ) (h : m + 1 = n := by lia) :
+    (ShortComplex.mk _ _ (P.homologyδ_comp R n m h)).Exact :=
+  (P.shortExact_chainComplexShortComplex R).homology_exact₁ n m h
+
+lemma homology_exact₂ (n : ℕ) :
+    (ShortComplex.mk _ _ (P.homologyMap_hom_homologyπ R n)).Exact :=
+  (P.shortExact_chainComplexShortComplex R).homology_exact₂ n
+
+lemma homology_exact₃ (n m : ℕ) (h : m + 1 = n := by lia) :
+    (ShortComplex.mk _ _ (P.comp_homologyδ R n m h)).Exact :=
+  (P.shortExact_chainComplexShortComplex R).homology_exact₃ n m h
+
+instance : Epi (P.homologyπ R 0) :=
+  HomologicalComplex.epi_homologyMap_of_epi_of_not_rel _ _ (by simp)
+
+end
 
 end SSetPair
