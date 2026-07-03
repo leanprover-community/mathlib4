1f9a3832d9557f07bae187d21567f380a66a6be7
Terence Tao <tao@math.ucla.edu>
2026-07-03 11:46:06 -0700

    fix improper integrals

--- Mathlib/Analysis/SpecialFunctions/ImproperIntegrals.lean
+++ Mathlib/Analysis/SpecialFunctions/ImproperIntegrals.lean
@@ -294,7 +294,7 @@ theorem integrableOn_Ioi_inv_div_log_sq {c : ℝ} (hc : 1 < c) :
     IntegrableOn (fun t ↦ t⁻¹ / (log t)^2) (.Ioi c) volume := by
   apply integrableOn_Ioi_deriv_of_nonneg' _ _ tendsto_log_atTop.inv_tendsto_atTop.neg
   · intro t _
-    convert! (hasDerivAt_inv_log (by grind : 0 < t) (by grind)).neg using 1
+    convert! (hasDerivAt_inv_log (by grind : t ≠ 0) (by grind) (by grind)).neg using 1
     field
   · intro t _
     have : 0 < t := by grind
@@ -307,7 +307,7 @@ theorem integral_Ioi_inv_divlog_sq {c : ℝ} (hc : 1 < c) :
     (integrableOn_Ioi_inv_div_log_sq hc) ?_ using 1
   · simp
   · intro t _
-    convert! (hasDerivAt_inv_log (by grind : 0 < t) (by grind)).neg using 1
+    convert! (hasDerivAt_inv_log (by grind : t ≠ 0) (by grind) (by grind)).neg using 1
     field
   convert! tendsto_log_atTop.inv_tendsto_atTop.neg using 1
   simp
