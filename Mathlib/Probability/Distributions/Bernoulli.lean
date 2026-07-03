b5afd545dc8493391a5015a8a5eb14d7e856d032
Etienne Marion <baguettes_casino0c@icloud.com>
2026-07-03 09:26:42 +0200

    change proof

--- Mathlib/Probability/Distributions/Bernoulli.lean
+++ Mathlib/Probability/Distributions/Bernoulli.lean
@@ -143,8 +143,8 @@ lemma bernoulliMeasure_real_apply_of_notMem_of_notMem (p : I) {s : Set X}
 
 @[simp]
 lemma bernoulliMeasure_real_apply_singleton_left [MeasurableSingletonClass X] (p : I) (h : x ≠ y) :
-    Ber(x, y, p).real {x} = p :=
-  bernoulliMeasure_real_apply_of_mem_of_notMem p (by simp) (by simp) (by grind)
+    Ber(x, y, p).real {x} = p := by
+  simp [h.symm]
 
 lemma bernoulliMeasure_real_apply_singleton_right [MeasurableSingletonClass X] (p : I) (h : x ≠ y) :
     Ber(x, y, p).real {y} = 1 - p := by
