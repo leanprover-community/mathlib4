067472e2e92a9306e999015012e86420d39cfbf3
ymonbru <yannis.monbru@orange.fr>
2026-07-03 13:41:10 +0200

    --

--- Mathlib/Topology/Sheaves/KSheaf.lean
+++ Mathlib/Topology/Sheaves/KSheaf.lean
@@ -76,7 +76,7 @@ structure of cocone over the diagramm defined by the `P.obj (op (closure (U : Se
 for `U` an open
 neighbourhood of `K` -/
 def coconeOfClosureOfOpens (P : KPresheaf A X) (K : Compacts X) :=
-  Cocone.whisker K.oRcNhdsToCompactNhds_mono.functor.op <| P.coconeOfCompacts K
+  Cocone.whisker K.openRcNhdsToCompactNhds_mono.functor.op <| P.coconeOfCompacts K
 
 variable [T2Space X]
 
@@ -86,7 +86,7 @@ a recipi to build maps from `P.obj(op K)` by only using the open relatively
 comapct neighbourhoods and not all the compacts neighbourhoods. -/
 noncomputable def mapOfOpenClosure (P : KPresheaf A X) (K : Compacts X)
     (h : (IsColimit (P.coconeOfCompacts K))) {G : (K.openRcNhds)ᵒᵖ ⥤ A} (t : Cocone G)
-    (α : (K.oRcNhdsToCompactNhds_mono.functor.op ⋙ (Subtype.mono_coe _).functor.op ⋙ P) ⟶ G) :
+    (α : (K.openRcNhdsToCompactNhds_mono.functor.op ⋙ (Subtype.mono_coe _).functor.op ⋙ P) ⟶ G) :
     P.obj (op K) ⟶ t.pt :=
   ((Functor.Final.isColimitWhiskerEquiv _ _).invFun h ).map t α
 
@@ -128,8 +128,8 @@ set_option backward.isDefEq.respectTransparency false in
 all the compacts neighbourhoods. -/
 noncomputable def mapOfOpenClosure (P : KSheaf A X) (K : Compacts X) {G : (K.openRcNhds)ᵒᵖ ⥤ A}
     (t : Cocone G)
-    (α : (K.oRcNhdsToCompactNhds_mono.functor.op ⋙ (Subtype.mono_coe _).functor.op ⋙ P.obj) ⟶ G) :
-    P.obj.obj (op K) ⟶ t.pt :=
+    (α : (K.openRcNhdsToCompactNhds_mono.functor.op ⋙ (Subtype.mono_coe _).functor.op ⋙ P.obj) ⟶ G)
+    : P.obj.obj (op K) ⟶ t.pt :=
   ((Functor.Final.isColimitWhiskerEquiv _ _).invFun
   (Classical.choice <| P.property.nonempty_isColimit_coconeOfCompacts K) ).map t α
 
