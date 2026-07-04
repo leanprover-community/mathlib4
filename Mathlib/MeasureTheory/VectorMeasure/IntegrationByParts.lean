0fdc4be316bfd17e4936da0280b63ae5e939fac7
Sebastien Gouezel <sebastien.gouezel@univ-rennes1.fr>
2026-07-04 09:41:30 +0200

    ext Icc lemma

--- Mathlib/MeasureTheory/VectorMeasure/IntegrationByParts.lean
+++ Mathlib/MeasureTheory/VectorMeasure/IntegrationByParts.lean
@@ -32,10 +32,46 @@ variable {α : Type*} [LinearOrder α] [DenselyOrdered α] [TopologicalSpace α]
 
 namespace MeasureTheory
 
+omit [NormedSpace ℝ E] [CompleteSpace E] in
+/-- Two vector measures which agree on closed intervals are equal. -/
+theorem VectorMeasure.ext_of_Icc {α : Type*} [TopologicalSpace α] {m : MeasurableSpace α}
+    [SecondCountableTopology α] [LinearOrder α] [OrderTopology α] [BorelSpace α]
+    (μ ν : VectorMeasure α E) (hμ : ∀ ⦃a b⦄, a ≤ b → μ (Icc a b) = ν (Icc a b)) : μ = ν := by
+  rcases isEmpty_or_nonempty α with hα | hα
+  · ext s hs
+    have : s = ∅ := Subsingleton.elim _ _
+    simp [this]
+  apply VectorMeasure.ext_of_generateFrom _ _
+    (BorelSpace.measurable_eq.trans (borel_eq_generateFrom_Icc α)) (isPiSystem_Icc id id); swap
+  · rintro s ⟨l, u, hlu, rfl⟩
+    exact hμ hlu
+  obtain ⟨u, u_mono, hu⟩ : ∃ u : ℕ → α, Monotone u ∧ Tendsto u atTop atTop :=
+    Filter.exists_seq_monotone_tendsto_atTop_atTop _
+  obtain ⟨v, v_mono, hv⟩ : ∃ v : ℕ → α, Antitone v ∧ Tendsto v atTop atBot :=
+    Filter.exists_seq_antitone_tendsto_atTop_atBot _
+  have : ⋃ n, Icc (v n) (u n) = univ := by
+    apply eq_univ_iff_forall.2 (fun x ↦ ?_)
+    simp only [mem_iUnion, mem_Icc]
+    exact ((tendsto_atBot.1 hv x).and (tendsto_atTop.1 hu x)).exists
+  rw [← this]
+  have M : Monotone (fun n ↦ Icc (v n) (u n)) := by
+    intro m n hmn x hx
+    grind [Monotone, Antitone]
+  apply tendsto_nhds_unique (VectorMeasure.tendsto_vectorMeasure_iUnion_atTop_nat M (v := μ)
+    (fun n ↦ measurableSet_Icc))
+  have A a b : μ (Icc a b) = ν (Icc a b) := by
+    rcases lt_or_ge b a with hab | hab
+    · simp [hab]
+    · exact hμ hab
+  simp only [A]
+  exact VectorMeasure.tendsto_vectorMeasure_iUnion_atTop_nat M (fun n ↦ measurableSet_Icc)
+
 lemma foo (hf : BoundedVariationOn f univ) (hg : BoundedVariationOn g univ)
     {B : E →L[ℝ] F →L[ℝ] G} :
     (hf.bilinear_comp hg B).vectorMeasure = hf.vectorMeasure.withDensity g.rightLim B.flip
       + hg.vectorMeasure.withDensity f.leftLim B := by
-  sorry
+  apply VectorMeasure.ext_of_Icc _ _ (fun a b hab ↦ ?_)
+
+
 
 end MeasureTheory
