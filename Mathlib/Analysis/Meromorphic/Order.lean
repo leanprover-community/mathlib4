b8d85acdc7ded9e6a0a6ea27bfc3527448559b16
Stefan Kebekus <kebekus@users.noreply.github.com>
2026-07-05 17:18:56 +0200

    Update Mathlib/Analysis/Meromorphic/Order.lean
    Co-authored-by: Monica Omar <23701951+themathqueen@users.noreply.github.com>

--- Mathlib/Analysis/Meromorphic/Order.lean
+++ Mathlib/Analysis/Meromorphic/Order.lean
@@ -298,7 +298,7 @@ theorem MeromorphicAt.meromorphicOrderAt_nonneg_iff_exists_analyticAt_and_eventu
     0 ≤ meromorphicOrderAt f x ↔ ∃ g : 𝕜 → E, AnalyticAt 𝕜 g x ∧ f =ᶠ[𝓝[≠] x] g := by
   refine ⟨fun nneg ↦ ?_, fun ⟨g, hg₁, hg₂⟩ ↦ ?_⟩
   · by_cases h' : meromorphicOrderAt f x = ⊤
-    · exact ⟨0, ⟨analyticAt_const, meromorphicOrderAt_eq_top_iff.mp h'⟩⟩
+    · exact ⟨0, analyticAt_const, meromorphicOrderAt_eq_top_iff.mp h'⟩
     obtain ⟨n, h₀ : meromorphicOrderAt f x = n⟩ := Option.ne_none_iff_exists'.mp h'
     obtain ⟨g, hg, -, hfg⟩ := (meromorphicOrderAt_eq_int_iff hf).mp h₀
     refine ⟨fun z ↦ (z - x) ^ n • g z, ?_, hfg⟩
