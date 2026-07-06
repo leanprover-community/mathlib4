f4891375146ba04c0feea00865edcd52484a645a
Michael Rothgang <rothgang@math.uni-bonn.de>
2026-07-06 13:49:23 +0200

    change

--- Mathlib/CategoryTheory/Sites/LocallySurjective.lean
+++ Mathlib/CategoryTheory/Sites/LocallySurjective.lean
@@ -5,7 +5,8 @@ Authors: Andrew Yang, Joël Riou
 -/
 module
 
-public import Mathlib.CategoryTheory.Sites.Subsheaf
+-- TODO investigate: commenting this import makes mathlib 300 * 10⁹ instructions slow -- why?
+-- public import Mathlib.CategoryTheory.Sites.Subsheaf
 public import Mathlib.CategoryTheory.Sites.CompatibleSheafification
 public import Mathlib.CategoryTheory.Sites.LocallyInjective
 public import Mathlib.CategoryTheory.ShrinkYoneda
