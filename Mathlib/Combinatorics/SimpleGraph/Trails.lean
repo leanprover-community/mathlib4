20779939058b589234fad47accf5785b994084b7
Snir Broshi <26556598+SnirBroshi@users.noreply.github.com>
2026-07-07 19:05:22 +0300

    chore(Combinatorics/SimpleGraph/Trails): golf and cleanup

--- Mathlib/Combinatorics/SimpleGraph/Trails.lean
+++ Mathlib/Combinatorics/SimpleGraph/Trails.lean
@@ -9,7 +9,6 @@ public import Mathlib.Algebra.Ring.Parity
 public import Mathlib.Combinatorics.SimpleGraph.Paths
 
 /-!
-
 # Trails and Eulerian trails
 
 This module contains additional theory about trails, including Eulerian trails (also known
@@ -33,7 +32,6 @@ as Eulerian circuits).
 ## Tags
 
 Eulerian trails
-
 -/
 
 @[expose] public section
@@ -41,118 +39,85 @@ Eulerian trails
 
 namespace SimpleGraph
 
-variable {V : Type*} {G : SimpleGraph V}
+variable {V : Type*} {G : SimpleGraph V} {u v x : V}
 
 namespace Walk
 
 /-- The edges of a trail as a finset, since each edge in a trail appears exactly once. -/
-abbrev IsTrail.edgesFinset {u v : V} {p : G.Walk u v} (h : p.IsTrail) : Finset (Sym2 V) :=
+abbrev IsTrail.edgesFinset {p : G.Walk u v} (h : p.IsTrail) : Finset (Sym2 V) :=
   ⟨p.edges, h.edges_nodup⟩
 
 variable [DecidableEq V]
 
-theorem IsTrail.even_countP_edges_iff {u v : V} {p : G.Walk u v} (ht : p.IsTrail) (x : V) :
-    Even (p.edges.countP fun e => x ∈ e) ↔ u ≠ v → x ≠ u ∧ x ≠ v := by
+theorem IsTrail.even_countP_edges_iff {p : G.Walk u v} (ht : p.IsTrail) (x : V) :
+    Even (p.edges.countP fun e ↦ x ∈ e) ↔ u ≠ v → x ≠ u ∧ x ≠ v := by
   induction p with
   | nil => simp
-  | cons huv p ih =>
-    rw [isTrail_cons] at ht
-    specialize ih ht.1
-    simp only [List.countP_cons, Ne, edges_cons, Sym2.mem_iff]
-    split_ifs with h
-    · rw [decide_eq_true_eq] at h
-      obtain (rfl | rfl) := h
-      · rw [Nat.even_add_one, ih]
-        simp only [huv.ne, imp_false, Ne, not_false_iff, true_and, not_forall,
-          Classical.not_not, exists_prop, not_true, false_and,
-          and_iff_right_iff_imp]
-        rintro rfl rfl
-        exact G.loopless.irrefl _ huv
-      · have := huv.ne; grind
-    · grind
+  | cons huv p ih => grind [isTrail_cons, edges_cons, G.irrefl]
 
 /-- An *Eulerian trail* (also known as an "Eulerian path") is a walk
 `p` that visits every edge exactly once.  The lemma `SimpleGraph.Walk.IsEulerian.IsTrail` shows
 that these are trails.
 
 Combine with `p.IsCircuit` to get an Eulerian circuit (also known as an "Eulerian cycle"). -/
-def IsEulerian {u v : V} (p : G.Walk u v) : Prop :=
+def IsEulerian (p : G.Walk u v) : Prop :=
   ∀ e, e ∈ G.edgeSet → p.edges.count e = 1
 
-theorem IsEulerian.isTrail {u v : V} {p : G.Walk u v} (h : p.IsEulerian) : p.IsTrail := by
-  rw [isTrail_def, List.nodup_iff_count_le_one]
-  intro e
-  by_cases he : e ∈ p.edges
-  · exact (h e (edges_subset_edgeSet _ he)).le
-  · simp [List.count_eq_zero_of_not_mem he]
+theorem IsEulerian.isTrail {p : G.Walk u v} (h : p.IsEulerian) : p.IsTrail := by
+  rw [isTrail_def, List.nodup_iff_count_eq_one]
+  exact (h · <| p.edges_subset_edgeSet ·)
 
-theorem IsEulerian.mem_edges_iff {u v : V} {p : G.Walk u v} (h : p.IsEulerian) {e : Sym2 V} :
+theorem IsEulerian.mem_edges_iff {p : G.Walk u v} (h : p.IsEulerian) {e : Sym2 V} :
     e ∈ p.edges ↔ e ∈ G.edgeSet :=
-  ⟨fun h => p.edges_subset_edgeSet h,
-   fun he => by simpa [Nat.succ_le_iff] using (h e he).ge⟩
+  ⟨fun h ↦ p.edges_subset_edgeSet h, fun he ↦ by simpa using (h e he).ge⟩
 
 /-- The edge set of an Eulerian graph is finite. -/
 @[implicit_reducible]
-def IsEulerian.fintypeEdgeSet {u v : V} {p : G.Walk u v} (h : p.IsEulerian) :
-    Fintype G.edgeSet :=
-  Fintype.ofFinset h.isTrail.edgesFinset fun e => by
-    simp only [Finset.mem_mk, Multiset.mem_coe, h.mem_edges_iff]
+def IsEulerian.fintypeEdgeSet {p : G.Walk u v} (h : p.IsEulerian) : Fintype G.edgeSet :=
+  .ofFinset h.isTrail.edgesFinset <| by simp [h.mem_edges_iff]
 
-theorem IsTrail.isEulerian_of_forall_mem {u v : V} {p : G.Walk u v} (h : p.IsTrail)
-    (hc : ∀ e, e ∈ G.edgeSet → e ∈ p.edges) : p.IsEulerian := fun e he =>
-  List.count_eq_one_of_mem h.edges_nodup (hc e he)
+theorem IsTrail.isEulerian_of_forall_mem {p : G.Walk u v} (h : p.IsTrail)
+    (hc : ∀ e, e ∈ G.edgeSet → e ∈ p.edges) : p.IsEulerian :=
+  fun e he ↦ List.count_eq_one_of_mem h.edges_nodup (hc e he)
 
-theorem isEulerian_iff {u v : V} (p : G.Walk u v) :
-    p.IsEulerian ↔ p.IsTrail ∧ ∀ e, e ∈ G.edgeSet → e ∈ p.edges := by
-  constructor
-  · intro h
-    exact ⟨h.isTrail, fun _ => h.mem_edges_iff.mpr⟩
-  · rintro ⟨h, hl⟩
-    exact h.isEulerian_of_forall_mem hl
+theorem isEulerian_iff (p : G.Walk u v) :
+    p.IsEulerian ↔ p.IsTrail ∧ ∀ e, e ∈ G.edgeSet → e ∈ p.edges where
+  mp h := ⟨h.isTrail, fun _ ↦ h.mem_edges_iff.mpr⟩
+  mpr := fun ⟨h, hl⟩ ↦ h.isEulerian_of_forall_mem hl
 
-theorem IsTrail.isEulerian_iff {u v : V} {p : G.Walk u v} (hp : p.IsTrail) :
-    p.IsEulerian ↔ p.edgeSet = G.edgeSet :=
-  ⟨fun h ↦ Set.Subset.antisymm p.edges_subset_edgeSet (p.isEulerian_iff.mp h).2,
-   fun h ↦ p.isEulerian_iff.mpr ⟨hp, by simp [← h]⟩⟩
+theorem IsTrail.isEulerian_iff {p : G.Walk u v} (hp : p.IsTrail) :
+    p.IsEulerian ↔ p.edgeSet = G.edgeSet where
+  mp h := p.edgeSet_subset_edgeSet.antisymm (p.isEulerian_iff.mp h).2
+  mpr h := p.isEulerian_iff.mpr ⟨hp, by simp [← h]⟩
 
-theorem IsEulerian.edgeSet_eq {u v : V} {p : G.Walk u v} (h : p.IsEulerian) :
-    p.edgeSet = G.edgeSet := by
+theorem IsEulerian.edgeSet_eq {p : G.Walk u v} (h : p.IsEulerian) : p.edgeSet = G.edgeSet := by
   rwa [← h.isTrail.isEulerian_iff]
 
-theorem IsEulerian.edgesFinset_eq [Fintype G.edgeSet] {u v : V} {p : G.Walk u v}
-    (h : p.IsEulerian) : h.isTrail.edgesFinset = G.edgeFinset := by
+theorem IsEulerian.edgesFinset_eq [Fintype G.edgeSet] {p : G.Walk u v} (h : p.IsEulerian) :
+    h.isTrail.edgesFinset = G.edgeFinset := by
   ext e
   simp [h.mem_edges_iff]
 
-theorem IsEulerian.even_degree_iff {x u v : V} {p : G.Walk u v} (ht : p.IsEulerian) [Fintype V]
+theorem IsEulerian.even_degree_iff {p : G.Walk u v} (ht : p.IsEulerian) [Fintype V]
     [DecidableRel G.Adj] : Even (G.degree x) ↔ u ≠ v → x ≠ u ∧ x ≠ v := by
-  convert! ht.isTrail.even_countP_edges_iff x
-  rw [← Multiset.coe_countP, Multiset.countP_eq_card_filter, ← card_incidenceFinset_eq_degree]
-  change Multiset.card _ = _
-  congr 1
-  convert_to! _ = (ht.isTrail.edgesFinset.filter (x ∈ ·)).val
-  rw [ht.edgesFinset_eq, G.incidenceFinset_eq_filter x]
+  rw [← ht.isTrail.even_countP_edges_iff, ← Multiset.coe_countP, Multiset.countP_eq_card_filter,
+    ← card_incidenceFinset_eq_degree, incidenceFinset_eq_filter, ← ht.edgesFinset_eq]
+  simp [Finset.card_def]
 
-theorem IsEulerian.card_filter_odd_degree [Fintype V] [DecidableRel G.Adj] {u v : V}
+theorem IsEulerian.card_filter_odd_degree [Fintype V] [DecidableRel G.Adj]
     {p : G.Walk u v} (ht : p.IsEulerian) {s} (h : s = ({ v | Odd (G.degree v) } : Finset V)) :
     s.card = 0 ∨ s.card = 2 := by
   subst s
-  simp only [← Nat.not_even_iff_odd, Finset.card_eq_zero]
-  simp only [ht.even_degree_iff, Ne, not_forall, not_and, Classical.not_not, exists_prop]
+  simp_rw [← Nat.not_even_iff_odd, ht.even_degree_iff]
   obtain rfl | hn := eq_or_ne u v
   · simp
-  · right
-    convert_to _ = ({u, v} : Finset V).card
-    · simp [hn]
-    · congr
-      ext x
-      simp [hn, imp_iff_not_or]
+  · grind [Finset.card_pair hn, congrArg Finset.card]
 
-theorem IsEulerian.card_odd_degree [Fintype V] [DecidableRel G.Adj] {u v : V} {p : G.Walk u v}
+theorem IsEulerian.card_odd_degree [Fintype V] [DecidableRel G.Adj] {p : G.Walk u v}
     (ht : p.IsEulerian) :
     Fintype.card { v | Odd (G.degree v) } = 0 ∨ Fintype.card { v | Odd (G.degree v) } = 2 := by
   rw [← Set.toFinset_card]
-  apply IsEulerian.card_filter_odd_degree ht
+  apply ht.card_filter_odd_degree
   simp
 
 end Walk
