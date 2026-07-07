57dc94cca1cc82d3858f953b029746b19cbfb4b4
NoahW314 <noahwalker3.14@gmail.com>
2026-07-07 12:07:45 -0500

    `{Left,Right}CancelMulZero` are dedekind-finite

--- Mathlib/Algebra/GroupWithZero/Basic.lean
+++ Mathlib/Algebra/GroupWithZero/Basic.lean
@@ -325,6 +325,22 @@ theorem eq_zero_of_mul_eq_self_left [IsRightCancelMulZero M₀] (h₁ : b ≠ 1)
     a = 0 :=
   Classical.byContradiction fun ha => h₁ <| mul_right_cancel₀ ha <| h₂.symm ▸ (one_mul a).symm
 
+variable {M₀ : Type*} [MonoidWithZero M₀]
+
+instance [IsLeftCancelMulZero M₀] : IsDedekindFiniteMonoid M₀ where
+  mul_eq_one_symm h := by
+    rcases subsingleton_or_nontrivial M₀ with hM | hM
+    · exact Subsingleton.elim _ _
+    exact (IsLeftCancelMulZero.mul_left_cancel_of_ne_zero
+      (left_ne_zero_of_mul_eq_one h)).mul_eq_one_symm h
+
+instance [IsRightCancelMulZero M₀] : IsDedekindFiniteMonoid M₀ where
+  mul_eq_one_symm h := by
+    rcases subsingleton_or_nontrivial M₀ with hM | hM
+    · exact Subsingleton.elim _ _
+    exact (IsRightCancelMulZero.mul_right_cancel_of_ne_zero
+      (right_ne_zero_of_mul_eq_one h)).mul_eq_one_symm h
+
 end CancelMonoidWithZero
 
 section GroupWithZero
