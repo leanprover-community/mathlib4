aacc1ec205528f95d639244ba158dc3522387321
Yakov Pechersky <ffxen158@gmail.com>
2026-07-08 10:05:42 -0400

    generalize SMul, unlocks TestFunction
    and generalize more in contdiffmapsupportin

--- Mathlib/Analysis/Distribution/ContDiffMapSupportedIn.lean
+++ Mathlib/Analysis/Distribution/ContDiffMapSupportedIn.lean
@@ -233,14 +233,13 @@ instance : IsSubApply 𝓓^{n}_{K}(E, F) E F where
 
 @[deprecated (since := "2026-06-15")] alias coe_sub := FunLike.coe_sub
 
-instance instSMul {R} [AddSemigroup R] [DistribSMul R F] [SMulCommClass ℝ R F]
-    [ContinuousConstSMul R F] :
+instance instSMul {R} [Add R] [DistribSMul R F] [SMulCommClass ℝ R F] [ContinuousConstSMul R F] :
     SMul R 𝓓^{n}_{K}(E, F) where
   smul c f := .mk (c • (f : E → F)) (f.contDiff.const_smul c) <| by
     rw [← smul_zero c]
     exact f.zero_on_compl.comp_left
 
-instance {R} [AddSemigroup R] [DistribSMul R F] [SMulCommClass ℝ R F] [ContinuousConstSMul R F] :
+instance {R} [Add R] [DistribSMul R F] [SMulCommClass ℝ R F] [ContinuousConstSMul R F] :
     IsSMulApply R 𝓓^{n}_{K}(E, F) E F where
   smul_apply _ _ _ := rfl
 
