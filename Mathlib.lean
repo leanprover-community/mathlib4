d45663d02f179c449d17bb065acc845b8051ec1a
Justus Springer <50165510+justus-springer@users.noreply.github.com>
2026-07-10 02:16:48 +0000

    feat(AlgebraicGeometry/Birational): composition of rational maps (#39445)
    Define composition of partial and rational maps.
    
    - [x] depends on: #39442
    - [x] depends on: #39443
    - [x] depends on: #39317
    - [x] depends on: #40189

--- Mathlib.lean
+++ Mathlib.lean
@@ -1350,6 +1350,7 @@ public import Mathlib.AlgebraicGeometry.AlgClosed.Basic
 public import Mathlib.AlgebraicGeometry.AlgebraicCycle.Basic
 public import Mathlib.AlgebraicGeometry.Artinian
 public import Mathlib.AlgebraicGeometry.Birational.Birational
+public import Mathlib.AlgebraicGeometry.Birational.Composition
 public import Mathlib.AlgebraicGeometry.Birational.Dominant
 public import Mathlib.AlgebraicGeometry.Birational.RationalMap
 public import Mathlib.AlgebraicGeometry.ColimitsOver
