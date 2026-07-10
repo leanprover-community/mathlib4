fb688d6dc799a562d63d23c0f64aa8dbd4b687c0
ADedecker <anatolededecker@gmail.com>
2026-07-10 12:51:37 +0200

    Good start on documentation

--- Mathlib/Analysis/FredholmOperator.lean
+++ Mathlib/Analysis/FredholmOperator.lean
@@ -13,6 +13,12 @@ public import Mathlib.Algebra.Module.LinearMap.Index
 
 /-!
 # Fredholm operators between topological vector spaces
+
+Fix `𝕜` a complete `NontriviallyNormedField`, and `E`, `F` be two Hausdorff topological vector
+spaces over `𝕜`.
+
+In this file, we define what it means for a continuous linear map `T : E →L[𝕜] F` to be
+a **Fredholm operator**.
 -/
 
 @[expose] public noncomputable section
@@ -35,6 +41,14 @@ section DefTFAE
 
 section IsFredholm
 
+/-- A continuous linear map `u : E →L[𝕜] F` is a **Fredholm operator** if it is strict,
+its range is closed and has finite codimension, and its kernel is (topologically) complemented and
+has finite dimension.
+
+See also `isFredholm_tfae` for other equivalent characterizations.
+We will also prove later (not in Mathlib yet) that for maps between Banach (or even Fréchet)
+spaces, all the conditions follow from the kernel and cokernel having finite dimension.
+-/
 structure IsFredholm (u : E →L[𝕜] F) : Prop where
   isStrictMap : IsStrictMap u
   isClosed_range : IsClosed (u.range : Set F)
@@ -53,15 +67,38 @@ end IsFredholm
 section FredholmDecomposition
 
 variable (𝕜 E) in
+/-- A **Fredholm decomposition** of a topological vector space `E` is the data of two subspaces
+`X₀` and `X₁` which are topological complements, and where `X₀` is finite dimensional.
+
+Note that we purposefully use the index `₀` for the "inessential" (i.e finite dimensional)
+part of the decomposition. -/
 structure FredholmDecomposition where
   X₀ : Submodule 𝕜 E
   X₁ : Submodule 𝕜 E
   isTopCompl : IsTopCompl X₁ X₀
-  fin_dim_X₀ : FiniteDimensional 𝕜 X₀
+  finite_X₀ : FiniteDimensional 𝕜 X₀
 
+/-- Given a fredhom decomposition `dec` of the space `E`, `dec.proj` is the (continuous linear)
+projection onto the "essential part" `dec.X₁` along the "inessential part" `dec.X₀`.
+This is a Fredholm operator. -/
 abbrev FredholmDecomposition.proj (dec : FredholmDecomposition 𝕜 E) :
     E →L[𝕜] dec.X₁ := dec.X₁.projectionOntoL dec.X₀ dec.isTopCompl
 
+/-- Let `u : E →L[𝕜] F` be a continuous linear map. A **Fredholm package** for `u` is the data of
+Fredholm decompositions `dec_dom` and `dec_codom` of `E` and `F` respectively, together with
+a continuous linear equivalence `equiv : dec_dom.X₁ ≃ₗ[𝕜] dec_codom.X₁` between the "essential"
+(i.e finite codimension) parts of these decompositions, such that `u` equals the composition
+`u = dec_codom.X₁.subtypeL ∘L equiv ∘L dec_dom.proj`. In other words, in these
+"essential ⊕ inessential" decompositions, the matrix of `u` is
+```
+equiv 0
+0     0
+```
+
+We will show in `isFredholm_tfae` that an operator is Fredholm if and only if it admits
+a Fredholm package. In practice, the condition that `u` is Fredholm is always easier to
+prove, so if you need a Fredholm package you should probably get it from
+`IsFredholm.nonempty_fredholmPackage` or `IsFredholm.fredholmPackage`. -/
 structure FredholmPackage (u : E →L[𝕜] F) where
   dec_dom : FredholmDecomposition 𝕜 E
   dec_codom : FredholmDecomposition 𝕜 F
@@ -75,18 +112,20 @@ lemma FredholmPackage.range_eq {u : E →L[𝕜] F} (pkg : FredholmPackage u) :
     u.range = pkg.dec_codom.X₁ := by
   simp [pkg.eq_equiv, range_comp]
 
+/-- The data of a Fredholm package for `u` determines a canonical quasi-inverse of `u`. -/
 def FredholmPackage.quasiInverse {u : E →L[𝕜] F} (pkg : FredholmPackage u) :
     F →L[𝕜] E :=
   pkg.dec_dom.X₁.subtypeL ∘L pkg.equiv.symm ∘L pkg.dec_codom.proj
 
+/-- The data of a Fredholm package for `u` determines a canonical quasi-inverse of `u`. -/
 lemma FredholmPackage.isQuasiInverse {u : E →L[𝕜] F} (pkg : FredholmPackage u) :
     u.IsQuasiInverse pkg.quasiInverse := by
   nth_rw 1 [pkg.eq_equiv, quasiInverse]
   have hdom : IsQuasiInverse pkg.dec_dom.X₁.subtype pkg.dec_dom.proj :=
-    have := pkg.dec_dom.fin_dim_X₀
+    have := pkg.dec_dom.finite_X₀
     isQuasiInverse_subtype_projectionOnto _
   have hcodom : IsQuasiInverse pkg.dec_codom.X₁.subtype pkg.dec_codom.proj :=
-    have := pkg.dec_codom.fin_dim_X₀
+    have := pkg.dec_codom.finite_X₀
     isQuasiInverse_subtype_projectionOnto _
   refine .of_comp_left hcodom.symm <| .of_comp_right hdom ?_
   simp_rw [FredholmDecomposition.proj, toLinearMap_comp, toLinearMap_subtypeL,
@@ -128,7 +167,7 @@ variable [T2Space F] in
 subspaces `E₁` and `F₁`. Then `u` is Fredholm.
 
 In fact it is enough to assume that the restriction `E₁ →L[𝕜] F₁` is Fredholm, see
-`IsFredholm.of_restrict`. -/
+`IsFredholm.of_restrict` (not in Mathlib yet). -/
 theorem IsFredholm.of_isInvertible_restrict {u : E →L[𝕜] F}
     {E₁ : Submodule 𝕜 E} (E₁_closed : IsClosed (E₁ : Set E)) [E₁_coFG : E₁.CoFG]
     {F₁ : Submodule 𝕜 F} (F₁_closed : IsClosed (F₁ : Set F)) [F₁_coFG : F₁.CoFG]
@@ -154,6 +193,13 @@ theorem IsFredholm.of_isInvertible_restrict {u : E →L[𝕜] F}
   · exact .of_disjoint_of_finiteDimensional_quotient E₁_closed disj.symm
 
 omit [ContinuousSMul 𝕜 E] in
+/-- Let `u : E →L[𝕜] F` be a Fredholm operator. Given `dom₁` (resp. `codom₀`) be an arbitrary
+topological complement of `u.ker` (resp. `u.range`), we get a `FredholmPackage` for `u`
+by considering the decompositions `E = dom₁ ⊕ u.ker`, `F = u.range ⊕ codom₀`, and the isomorphism
+`dom₁ ≃L[𝕜] u.range` induced by `u`.
+
+If you need control over the decompositions, this is the primary way to get a `FredholmPackage`.
+Otherwise, see `IsFredholm.nonempty_fredholmPackage`. -/
 def IsFredholm.fredholmPackage {u : E →L[𝕜] F}
     (u_fred : IsFredholm u) {dom₁ : Submodule 𝕜 E} {codom₀ : Submodule 𝕜 F}
     (h_dom : IsTopCompl u.ker dom₁) (h_codom : IsTopCompl u.range codom₀) :
@@ -162,12 +208,12 @@ def IsFredholm.fredholmPackage {u : E →L[𝕜] F}
     X₀ := u.ker
     X₁ := dom₁
     isTopCompl := h_dom.symm
-    fin_dim_X₀ := u_fred.finite_ker  }
+    finite_X₀ := u_fred.finite_ker }
   dec_codom := {
     X₀ := codom₀
     X₁ := u.range
     isTopCompl := h_codom
-    fin_dim_X₀ := .of_fg <| u_fred.finite_coker.fg_of_isCompl h_codom.isCompl  }
+    finite_X₀ := .of_fg <| u_fred.finite_coker.fg_of_isCompl h_codom.isCompl  }
   equiv :=
     letI Φ : dom₁ ≃L[𝕜] E ⧸ u.ker := u.ker.quotientEquivOfIsTopCompl dom₁ h_dom |>.symm
     letI Ψ : (E ⧸ u.ker) ≃L[𝕜] u.range := .quotKerEquivRange u.toLinearMap u_fred.isStrictMap
@@ -180,7 +226,11 @@ def IsFredholm.fredholmPackage {u : E →L[𝕜] F}
       simp [hx, projection_apply_of_mem_left, ContinuousLinearEquiv.quotKerEquivRange]
 
 omit [ContinuousSMul 𝕜 E] in
-theorem IsFredholm.nonempty_fredholmDecomposition {u : E →L[𝕜] F}
+/-- Every Fredholm operator admits a `FredholmPackage`.
+
+This is the primary way to get a `FredholmPackage` if you don't need control of the decompositions.
+If you do, see `IsFredholm.fredholmPackage`. -/
+theorem IsFredholm.nonempty_fredholmPackage {u : E →L[𝕜] F}
     (u_fred : IsFredholm u) : Nonempty (FredholmPackage u) := by
   obtain ⟨codom₂, h_codom⟩ := u_fred.closedComplemented_range.exists_isTopCompl
   obtain ⟨dom₁, h_dom⟩ := u_fred.closedComplemented_ker.exists_isTopCompl
@@ -188,7 +238,7 @@ theorem IsFredholm.nonempty_fredholmDecomposition {u : E →L[𝕜] F}
 
 variable [T2Space E] [T2Space F]
 
-theorem isFredholmTFAE (u : E →L[𝕜] F) : List.TFAE
+theorem isFredholm_tfae (u : E →L[𝕜] F) : List.TFAE
     [
       IsFredholm u,
       ∃ v : F →L[𝕜] E, u.IsQuasiInverse v,
@@ -198,7 +248,7 @@ theorem isFredholmTFAE (u : E →L[𝕜] F) : List.TFAE
         ∃ h : MapsTo u E₁ F₁, (u.restrict h).IsInvertible,
       Nonempty (FredholmPackage u)
     ] := by
-  tfae_have 1 → 4 := IsFredholm.nonempty_fredholmDecomposition
+  tfae_have 1 → 4 := IsFredholm.nonempty_fredholmPackage
   tfae_have 4 → 2 := by
     rintro ⟨dec⟩
     exact ⟨dec.quasiInverse, dec.isQuasiInverse⟩
@@ -209,6 +259,10 @@ theorem isFredholmTFAE (u : E →L[𝕜] F) : List.TFAE
     rintro ⟨E₁, F₁, E₁_closed, F₁_closed, E₁_coFG, F₁_coFG, u_mapsto, u_invertible⟩
     exact .of_isInvertible_restrict E₁_closed F₁_closed u_mapsto u_invertible
   tfae_finish
+
+theorem FredholmPackage.isFredholm {u : E →L[𝕜] F} (pkg : FredholmPackage u) :
+    IsFredholm u :=
+  isFredholm_tfae u |>.out 3 0 |>.mp (Nonempty.intro pkg)
 
 end DefTFAE
 
