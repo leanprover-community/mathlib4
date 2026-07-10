e0394640a9b58504a907066ad37cafea3a8b75f7
Anatole Dedecker <anatolededecker@gmail.com>
2026-07-10 20:06:16 +0000

    feat: more API specific to strict group homs (#41238)
    * Move the section of `Topology.Maps.Strict.Basic` about group homs to a new file `Topology.Maps.Strict.Group`
    * add more criterions for strictness of group homs
    * the first isomorphism theorem holds for strict group homs, yielding a [ContinuousMulEquiv](https://leanprover-community.github.io/mathlib4_docs/Mathlib/Topology/Algebra/ContinuousMonoidHom.html#ContinuousMulEquiv)
    * various tweaks to namespaces and variables throughout the two files.

--- Mathlib.lean
+++ Mathlib.lean
@@ -7986,6 +7986,7 @@ public import Mathlib.Topology.Maps.Proper.Basic
 public import Mathlib.Topology.Maps.Proper.CompactlyGenerated
 public import Mathlib.Topology.Maps.Proper.UniversallyClosed
 public import Mathlib.Topology.Maps.Strict.Basic
+public import Mathlib.Topology.Maps.Strict.Group
 public import Mathlib.Topology.MetricSpace.Algebra
 public import Mathlib.Topology.MetricSpace.Antilipschitz
 public import Mathlib.Topology.MetricSpace.Basic
