44d850421fbbf6721af2c24b2f8a46c8fcb47189
Felix Pernegger <s59fpern@uni-bonn.de>
2026-07-11 22:05:18 +0200

    Update Mathlib/FieldTheory/LinearDisjoint.lean
    Co-authored-by: Aaron Liu <aaronliu2008@outlook.com>

--- Mathlib/FieldTheory/LinearDisjoint.lean
+++ Mathlib/FieldTheory/LinearDisjoint.lean
@@ -629,7 +629,6 @@ theorem isDomain' {A B : Type*} [Field A] [Algebra F A] [Field B] [Algebra F B]
 /-- If `A ⊗[F] L` is a field, then `A` and `L` are linearly disjoint over `F`. -/
 theorem of_isField (H : IsField (A ⊗[F] L)) : A.LinearDisjoint L := by
   apply Subalgebra.LinearDisjoint.of_isField
-  -- need these otherwise the `exact` will stuck at typeclass
   exact Algebra.TensorProduct.congr (AlgEquiv.refl : A ≃ₐ[F] A)
     (AlgEquiv.ofInjective (IsScalarTower.toAlgHom F L E) (RingHom.injective _))
       |>.symm.toMulEquiv.isField H
