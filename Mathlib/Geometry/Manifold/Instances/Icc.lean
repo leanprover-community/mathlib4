5c8e2e24ee2232131109a1c386e75adaac609681
Michael Rothgang <rothgang@math.uni-bonn.de>
2026-07-11 14:40:04 +0200

    Fix one flexible linter warning. Is this better?

--- Mathlib/Geometry/Manifold/Instances/Icc.lean
+++ Mathlib/Geometry/Manifold/Instances/Icc.lean
@@ -87,8 +87,10 @@ lemma isImmersionOfComplement_subtype_coe_Icc :
       (chartAt (EuclideanHalfSpace 1) z) (Homeomorph.addLeft (-x)).toOpenPartialHomeomorph
       (mem_chart_source _ z) (by simp [Homeomorph.addLeft]) (chart_mem_maximalAtlas _) ?_; swap
     · apply OpenPartialHomeomorph.mem_maximalAtlas_of_contMDiffOn
-      all_goals
-        simp [Homeomorph.addLeft, contMDiffOn_iff_contDiffOn]
+      · suffices ContDiffOn ℝ n (fun x_1 ↦ -x + x_1) univ by simpa [contMDiffOn_iff_contDiffOn]
+        fun_prop
+      · suffices ContDiffOn ℝ n (fun x_1 ↦ x + x_1) univ by
+          simpa [contMDiffOn_iff_contDiffOn, Homeomorph.addLeft]
         fun_prop
     intro z' hz'
     simp [hz, IccLeftChart, modelWithCornersEuclideanHalfSpace]
