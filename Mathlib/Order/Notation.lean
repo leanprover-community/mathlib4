e36b981cbe8f1782e955393c368fe2fce324f524
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2026-07-11 10:02:12 -0400

    fix(Order/Notation): unify at correct transparency in sup/inf delaborators

--- Mathlib/Order/Notation.lean
+++ Mathlib/Order/Notation.lean
@@ -100,13 +100,12 @@ private meta def hasLinearOrder (u : Level) (α : Q(Type u)) (cls : Q(Type u →
     MetaM Bool := do
   try
     withNewMCtxDepth do
-    -- `isDefEq` may call type class search to instantiate `mvar`, so we need the local instances
-    -- In Lean 4.19 the pretty printer clears local instances, so we re-add them here.
-    -- TODO(Jovan): remove
+    -- `isDefEq` may call type class search to instantiate `mvar`, so we need the local instances.
+    -- Unfortunately, we need to set the local instances manually.
     withLocalInstances (← getLCtx).decls.toList.reduceOption do
       let mvar ← mkFreshExprMVarQ q($(linearOrderExpr u) $α) (kind := .synthetic)
       let inst' : Q($cls $α) := q($toCls $α $mvar)
-      isDefEq inst inst'
+      withReducibleAndInstances <| isDefEq inst inst'
   catch _ =>
     -- For instance, if `LinearOrder` is not yet imported.
     return false
