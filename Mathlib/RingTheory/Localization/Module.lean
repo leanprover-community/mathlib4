f8f08edb469a5d55cf45677043c1bda0617f5bbe
tb65536 <thomas.l.browning@gmail.com>
2026-07-13 14:56:58 -0400

    import

--- Mathlib/RingTheory/Localization/Module.lean
+++ Mathlib/RingTheory/Localization/Module.lean
@@ -7,7 +7,6 @@ module
 
 public import Mathlib.Algebra.Module.LocalizedModule.IsLocalization
 public import Mathlib.LinearAlgebra.Basis.Basic
-public import Mathlib.LinearAlgebra.Dimension.Finrank
 public import Mathlib.RingTheory.Localization.FractionRing
 public import Mathlib.RingTheory.Localization.Integer
 
