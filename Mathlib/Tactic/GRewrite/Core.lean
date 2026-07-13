addfd30bdc9b408fbec336cddcbca3535be24f3a
Jovan Gerbscheid <56355248+JovanGerb@users.noreply.github.com>
2026-07-13 19:01:37 +0000

    feat(GRewrite): support universe changing rewrites (#40338)
    This PR allows `grw` to deal with relations that are heterogenous (the two sides don't have the same type).
    
    I'm slightly surprised that this works :). I added a basic test showing that the polymorphic relations on cardinals can be used in `grw`, as proposed in https://leanprover.zulipchat.com/#narrow/channel/287929-mathlib4/topic/Universe-heterogeneous.20ordinal.20and.20cardinal.20relations/with/495593531

--- Mathlib/Tactic/GRewrite/Core.lean
+++ Mathlib/Tactic/GRewrite/Core.lean
@@ -176,11 +176,20 @@ def GRewriteLemma.apply (lem : GRewriteLemma) (goal : MVarId) (symm : Bool)
 /-- Create the `gcongr` goal corresponding to rewriting `e` by relation `rel?`,
 so that we can apply `gcongr` lemmas to it. -/
 def makeGCongrGoal (rel? : Option Expr) (e : Expr) (forward : Bool) : MetaM (Expr × Expr) := do
-  let mkRel := if let some rel := rel? then mkApp2 rel else (.forallE `_a · · .default)
-  -- Assume that the two arguments of `rel` have the same type.
-  let mvar ← mkFreshExprMVar (← inferType e)
-  let target := if forward then mkRel e mvar else mkRel mvar e
-  return (mvar, ← mkFreshExprMVar target)
+  if let some rel := rel? then
+    let .forallE _ d₁ (.forallE _ d₂ _ _) _ ← whnf (← inferType rel) | throwFunctionExpected rel
+    -- note that `@[gcongr]`'s checks should prevent this happening
+    if d₂.hasLooseBVars then throwError "grw: {rel} is a dependent relation"
+    if forward then
+      let mvar ← mkFreshExprMVar d₂
+      return (mvar, ← mkFreshExprMVar <| mkApp2 rel e mvar)
+    else
+      let mvar ← mkFreshExprMVar d₁
+      return (mvar, ← mkFreshExprMVar <| mkApp2 rel mvar e)
+  else
+    let mvar ← mkFreshTypeMVar
+    let target := if forward then .forallE `_a e mvar .default else .forallE `_a mvar e .default
+    return (mvar, ← mkFreshExprMVar (some target))
 
 /-- Version of `getRel` that also returns the expression of the relation. -/
 def getRel' (e : Expr) : Option (Name × Option Expr × Expr × Expr) :=
--- MathlibTest/Tactic/GRewrite.lean
+++ MathlibTest/Tactic/GRewrite.lean
@@ -500,3 +500,45 @@ example (h₁ : a < b) : Set.Ici b ⊆ Set.Ioi a := by
   grw [h₁]
 
 end strict
+
+section universePolymorphic
+
+universe u v w w'
+
+axiom Cardinal : Type u
+
+axiom liftEq : Cardinal.{u} → Cardinal.{v} → Prop
+axiom liftLE : Cardinal.{u} → Cardinal.{v} → Prop
+
+variable {a : Cardinal.{u}} {b : Cardinal.{v}} {c : Cardinal.{w}} {d : Cardinal.{w'}}
+
+@[gcongr]
+axiom liftLE_imp_liftLE_of_liftLE_of_liftLE (h₁ : liftLE a b) (h₂ : liftLE c d) :
+    liftLE b c → liftLE a d
+
+@[refl]
+axiom liftEq_rfl : liftEq a a
+
+@[symm]
+axiom liftEq.comm (h : liftEq a b) : liftEq b a
+
+axiom liftLE_of_liftEq (h : liftEq a b) : liftLE a b
+
+@[refl]
+theorem liftLE_rfl : liftLE a a := liftLE_of_liftEq liftEq_rfl
+namespace Mathlib.Tactic.GCongr
+
+/-- See if the term is `AntisymmRel r a b` and the goal is `r a b`. -/
+@[gcongr_forward]
+public meta def exactLiftLEOfLiftEq : ForwardExt where
+  eval h goal := do goal.assignIfDefEq (← Lean.Meta.mkAppM ``liftLE_of_liftEq #[h])
+
+end Mathlib.Tactic.GCongr
+
+example (h : liftEq a b) (h' : liftLE b c) : liftLE a c := by
+  grw [h, h']
+
+example (h : liftEq b a) (h' : liftLE b c) : liftLE a c := by
+  grw [← h, ← h']
+
+end universePolymorphic
