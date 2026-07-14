31d5c0a9efda277706fc6c9930a6eb3ca086180b
Paul Cadman <git@paulcadman.dev>
2026-07-14 10:07:15 +0100

    Remove unused LinearAlgebra.Matrix.Determinant.Bird umbrella module

--- Mathlib/LinearAlgebra/Matrix/Determinant/Bird.lean
+++
@@ -1,8 +0,0 @@
-/-
-Copyright (c) 2026 Paul Cadman. All rights reserved.
-Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Paul Cadman
--/
-module
-
-public import Mathlib.LinearAlgebra.Matrix.Determinant.Bird.Defs
