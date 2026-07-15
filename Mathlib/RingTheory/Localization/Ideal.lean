92563c7b27533eb35c3f47617519eb76d8cb24fe
Batixx <s59fpern@uni-bonn.de>
2026-07-15 15:34:55 +0200

    more

--- Mathlib/RingTheory/Localization/Ideal.lean
+++ Mathlib/RingTheory/Localization/Ideal.lean
@@ -350,7 +350,8 @@ theorem bot_lt_under_prime [IsDomain R] (hM : M ≤ R⁰) (p : Ideal S) [hpp : p
     (hp0 : p ≠ ⊥) : ⊥ < p.under R := by
   have : IsDomain S := isDomain_of_le_nonZeroDivisors _ hM
   rw [← Ideal.comap_bot_of_injective (algebraMap R S) (IsLocalization.injective _ hM)]
-  exact
+  -- faster than `exact`
+  convert!
     (orderIsoOfPrime M S).lt_iff_lt.mpr
       (show (⟨⊥, Ideal.isPrime_bot⟩ : { p : Ideal S // p.IsPrime }) < ⟨p, hpp⟩ from hp0.bot_lt)
 
