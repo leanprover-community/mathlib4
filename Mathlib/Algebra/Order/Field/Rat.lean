e25c6759c792d0ddd4ab568360cacd7105c96fb7
Yaël Dillies <yael.dillies@gmail.com>
2026-07-17 09:02:57 +0200

    feat(Algebra/Order): `NNRat.cast_sub`

--- Mathlib/Algebra/Order/Field/Rat.lean
+++ Mathlib/Algebra/Order/Field/Rat.lean
@@ -7,7 +7,9 @@ module
 
 public import Mathlib.Algebra.Field.Rat
 public import Mathlib.Algebra.Order.Nonneg.Field
-public import Mathlib.Algebra.Order.Ring.Rat
+public import Mathlib.Algebra.Order.Ring.NNRat
+
+import Mathlib.Data.Rat.Cast.CharZero
 
 /-!
 # The rational numbers form a linear ordered field
@@ -25,4 +27,12 @@ rat, rationals, field, ℚ, numerator, denominator, num, denom
 
 public section
 
+namespace NNRat
+variable {K : Type*} [DivisionRing K] [CharZero K]
+
 deriving instance LinearOrderedCommGroupWithZero for NNRat
+
+@[simp] lemma cast_sub {p q : ℚ≥0} (h : p ≤ q) : (↑(q - p) : K) = q - p := by
+  rw [eq_sub_iff_add_eq]; norm_cast; exact tsub_add_cancel_of_le h
+
+end NNRat
