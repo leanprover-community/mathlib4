3dd97c0683a926abc9b0f7f9758a6bea75101593
Bolton Bailey <bolton.bailey@gmail.com>
2026-07-18 15:06:26 -0400

    mk_all

--- Mathlib/Tactic.lean
+++ Mathlib/Tactic.lean
@@ -268,7 +268,6 @@ public import Mathlib.Tactic.Polynomial.Core
 public import Mathlib.Tactic.Polyrith
 public import Mathlib.Tactic.Positivity
 public import Mathlib.Tactic.Positivity.Auto
-public import Mathlib.Tactic.Positivity.AutoTest
 public import Mathlib.Tactic.Positivity.Basic
 public import Mathlib.Tactic.Positivity.Core
 public import Mathlib.Tactic.Positivity.Finset
