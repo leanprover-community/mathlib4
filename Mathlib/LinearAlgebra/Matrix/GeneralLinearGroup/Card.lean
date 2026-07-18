acb979752f788f4cada74e289df24c553bbf2d23
Nicola Falciola <nicola.falciola@gmail.com>
2026-07-18 21:36:03 +0200

    Update Mathlib/LinearAlgebra/Matrix/GeneralLinearGroup/Card.lean
    Co-authored-by: Felix Pernegger <s59fpern@uni-bonn.de>

--- Mathlib/LinearAlgebra/Matrix/GeneralLinearGroup/Card.lean
+++ Mathlib/LinearAlgebra/Matrix/GeneralLinearGroup/Card.lean
@@ -61,11 +61,10 @@ end LinearIndependent
 
 namespace Matrix
 
-open Nat in
-/-- The cardinal of the matrix -/
+/-- The cardinal of a matrix. -/
 theorem card_matrix {m n α} [Finite m] [Finite n] :
   Nat.card (Matrix m n α) = Nat.card α ^ (Nat.card n * Nat.card m) := by
-  simp [Matrix, card_fun, ← pow_mul]
+  simp [Matrix, Nat.card_fun, ← pow_mul]
 
 section field
 
