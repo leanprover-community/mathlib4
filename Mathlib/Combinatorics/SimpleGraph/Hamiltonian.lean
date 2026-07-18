f2016e70478a265946eb42d394b928c30af08dd6
Snir Broshi <26556598+SnirBroshi@users.noreply.github.com>
2026-07-18 05:31:06 +0300

    add myself to Hamiltonian file authors

--- Mathlib/Combinatorics/SimpleGraph/Hamiltonian.lean
+++ Mathlib/Combinatorics/SimpleGraph/Hamiltonian.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2023 Bhavik Mehta, Rishi Mehta, Linus Sommer. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Bhavik Mehta, Rishi Mehta, Linus Sommer, Yue Sun
+Authors: Bhavik Mehta, Rishi Mehta, Linus Sommer, Yue Sun, Snir Broshi
 -/
 module
 
