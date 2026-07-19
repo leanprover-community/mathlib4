a3603a88f42ef4c96231ad1217762d5302e663bc
Yongxi Lin <aaronlin@andrew.cmu.edu>
2026-07-19 05:28:13 -0400

    change docstring

--- Mathlib/Topology/Closure.lean
+++ Mathlib/Topology/Closure.lean
@@ -421,7 +421,7 @@ theorem Dense.exists_mem_open (hs : Dense s) {U : Set X} (ho : IsOpen U)
   let ⟨x, hx⟩ := hs.inter_open_nonempty U ho hne
   ⟨x, hx.2, hx.1⟩
 
-/-- A dense set contains every point whose singleton is open, that is, every isolated point. -/
+/-- A dense set contains every isolated point. -/
 theorem Dense.mem_of_isOpen_singleton (hs : Dense s) (hx : IsOpen {x}) : x ∈ s := by
   obtain ⟨y, hys, hy⟩ := hs.exists_mem_open hx ⟨x, rfl⟩
   exact mem_singleton_iff.1 hy ▸ hys
