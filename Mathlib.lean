c2ea7ca1e55ede82135eb79555926975d87bd264
Paul Cadman <92877+paulcadman@users.noreply.github.com>
2026-07-19 02:25:22 +0000

    feat: add theorem that Bird's determinant algorithm computes Matrix.det (#41160)
    This PR adds:
    
    ```lean
    theorem det_eq_birdDet {n : Nat} (A : Array R) (hA : A.size = n * n) :
        Matrix.det (Matrix.ofArray (m := n) (n := n) A hA) = birdDet n A 
    ```
    
    showing that Bird's determinant algorithm computes `Matrix.det`.
    
    The PR also adds:
    - `BirdDet.Spec.birdDet` - an alternative implementation of Bird's algorithm that's stated in terms of `Matrix` instead of `Array`.
    
    Co-authored-by: Oliver Nash <github@olivernash.org>
    Co-authored-by: Oliver Nash <7734364+ocfnash@users.noreply.github.com>
    Co-authored-by: Johan Commelin <johan@commelin.net>

--- Mathlib.lean
+++ Mathlib.lean
@@ -5113,6 +5113,7 @@ public import Mathlib.LinearAlgebra.Matrix.Circulant
 public import Mathlib.LinearAlgebra.Matrix.ConjTranspose
 public import Mathlib.LinearAlgebra.Matrix.Defs
 public import Mathlib.LinearAlgebra.Matrix.Determinant.Basic
+public import Mathlib.LinearAlgebra.Matrix.Determinant.Bird.Correctness
 public import Mathlib.LinearAlgebra.Matrix.Determinant.Bird.Defs
 public import Mathlib.LinearAlgebra.Matrix.Determinant.Misc
 public import Mathlib.LinearAlgebra.Matrix.Determinant.TotallyUnimodular
