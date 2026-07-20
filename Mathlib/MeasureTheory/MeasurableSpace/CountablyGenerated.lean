80a92f6e251512a3cc466b2010b8c47d0fa29945
Ran Tao <rant2@andrew.cmu.edu>
2026-07-19 20:20:43 -0400

    add measurableSet_graph

--- Mathlib/MeasureTheory/MeasurableSpace/CountablyGenerated.lean
+++ Mathlib/MeasureTheory/MeasurableSpace/CountablyGenerated.lean
@@ -453,6 +453,25 @@ theorem measurableSingletonClass_of_countablySeparated
   rw [← finj.preimage_image {x}, image_singleton]
   exact fmeas <| MeasurableSet.singleton _
 
+/-- A measurable function into a countably separated space has a measurable graph. -/
+@[measurability]
+theorem _root_.Measurable.measurableSet_graph {X Y : Type*}
+    [MeasurableSpace X]
+    [MeasurableSpace Y] [cs : MeasurableSpace.CountablySeparated Y]
+    {f : X → Y} (measf : Measurable f) :
+    MeasurableSet { (x, y) : X × Y | y = f x } := by
+  let ⟨V, Vctbl, Vmeas, Vsep⟩ := cs.countably_separated
+  let A : Set (Set (X × Y)) := (fun v => { (x, y) : X × Y | x ∈ f ⁻¹' v ↔ y ∈ v }) '' V
+  have : { (x, y) : X × Y | y = f x } = ⋂₀ A := by
+    ext x
+    have := Vsep (f x.1) (Set.mem_univ _) x.2 (Set.mem_univ _)
+    simp [A]; grind
+  rw [this]
+  apply MeasurableSet.sInter (Vctbl.image _)
+  simp only [Set.mem_preimage, Set.mem_image, forall_exists_index, and_imp,
+    forall_apply_eq_imp_iff₂, measurableSet_setOfPred]
+  fun_prop (disch := measurability)
+
 end SeparatesPoints
 
 section MeasurableMemPartition
