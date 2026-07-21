04ae4b6a5930dc1951cf354c1e6ae6152f9f8b76
Weiyi Wang <wwylele@gmail.com>
2026-07-21 16:51:23 -0400

    address comments except mod_cast issue

--- Mathlib/Analysis/SpecialFunctions/Trigonometric/Complex.lean
+++ Mathlib/Analysis/SpecialFunctions/Trigonometric/Complex.lean
@@ -26,7 +26,7 @@ noncomputable section
 
 namespace Complex
 
-open Set Filter
+open Set Filter Finset
 
 open scoped Real
 
@@ -254,44 +254,42 @@ theorem range_sin : Set.range sin = Set.univ :=
   sin_surjective.range_eq
 
 theorem sin_mul_sum_sin (n : ℕ) (a b : ℂ) :
-    sin (a / 2) * ∑ i ∈ Finset.range n, sin (a * i + b) =
-      sin (n * a / 2) * sin ((n - 1) * a / 2 + b) := by
+    sin (a / 2) * ∑ i ∈ range n, sin (a * i + b) = sin (n * a / 2) * sin ((n - 1) * a / 2 + b) := by
   apply mul_left_cancel₀ (show (-2 : ℂ) ≠ 0 by simp)
-  simp_rw [← mul_assoc, Finset.mul_sum]
-  convert_to ∑ i ∈ Finset.range n,
-    -2 * sin (((a * ((i + 1 : ℕ) - 1 / 2) + b) + -(a * (i - 1 / 2) + b)) / 2) *
-    sin (((a * ((i + 1 : ℕ) - 1 / 2) + b) - -(a * (i - 1 / 2) + b)) / 2) =
-    -2 * sin (n * a / 2) * sin ((n - 1) * a / 2 + b)
-  · push_cast
-    ring_nf
-  simp_rw [← cos_sub_cos, cos_neg]
-  rw [Finset.sum_range_sub (fun i ↦ cos (a * (i - 1 / 2) + b)) n, cos_sub_cos]
-  ring_nf
+  simp_rw [← mul_assoc, mul_sum]
+  calc
+    ∑ i ∈ range n, -2 * sin (a / 2) * sin (a * i + b)
+      = ∑ x ∈ range n, (cos (a * (↑(x + 1) - 1 / 2) + b) - cos (-(a * (x - 1 / 2) + b))) := by
+      congr! 1 with x hx
+      rw [cos_sub_cos]
+      push_cast
+      ring_nf
+    _ = -2 * sin (n * a / 2) * sin ((n - 1) * a / 2 + b) := by
+      simp_rw [cos_neg, sum_range_sub (fun i ↦ cos (a * (i - 1 / 2) + b)), cos_sub_cos]
+      ring_nf
 
 theorem sum_sin (n : ℕ) {a : ℂ} (h : ∀ k : ℤ, a ≠ k * (2 * π)) (b : ℂ) :
-    ∑ i ∈ Finset.range n, sin (a * i + b) =
-      sin (n * a / 2) * sin ((n - 1) * a / 2 + b) / sin (a / 2) := by
+    ∑ i ∈ range n, sin (a * i + b) = sin (n * a / 2) * sin ((n - 1) * a / 2 + b) / sin (a / 2) := by
   rw [← sin_mul_sum_sin]
   grind [sin_ne_zero_iff]
 
 theorem sin_mul_sum_cos (n : ℕ) (a b : ℂ) :
-    sin (a / 2) * ∑ i ∈ Finset.range n, cos (a * i + b) =
-      sin (n * a / 2) * cos ((n - 1) * a / 2 + b) := by
+    sin (a / 2) * ∑ i ∈ range n, cos (a * i + b) = sin (n * a / 2) * cos ((n - 1) * a / 2 + b) := by
   apply mul_left_cancel₀ (show (2 : ℂ) ≠ 0 by simp)
-  simp_rw [← mul_assoc, Finset.mul_sum]
-  convert_to ∑ i ∈ Finset.range n,
-    2 * sin (((a * ((i + 1 : ℕ) - 1 / 2) + b) - (a * (i - 1 / 2) + b)) / 2) *
-    cos (((a * ((i + 1 : ℕ) - 1 / 2) + b) + (a * (i - 1 / 2) + b)) / 2) =
-    2 * sin (n * a / 2) * cos ((n - 1) * a / 2 + b)
-  · push_cast
-    ring_nf
-  simp_rw [← sin_sub_sin]
-  rw [Finset.sum_range_sub (fun i ↦ sin (a * (i - 1 / 2) + b)) n, sin_sub_sin]
-  ring_nf
+  simp_rw [← mul_assoc, mul_sum]
+  calc
+    ∑ i ∈ range n, 2 * sin (a / 2) * cos (a * i + b)
+      = ∑ x ∈ range n, (sin (a * (↑(x + 1) - 1 / 2) + b) - sin (a * (x - 1 / 2) + b)) := by
+      congr! 1 with x hx
+      rw [sin_sub_sin]
+      push_cast
+      ring_nf
+    _ = 2 * sin (n * a / 2) * cos ((n - 1) * a / 2 + b) := by
+      simp_rw [sum_range_sub (fun i ↦ sin (a * (i - 1 / 2) + b)), sin_sub_sin]
+      ring_nf
 
 theorem sum_cos (n : ℕ) {a : ℂ} (h : ∀ k : ℤ, a ≠ k * (2 * π)) (b : ℂ) :
-    ∑ i ∈ Finset.range n, cos (a * i + b) =
-      sin (n * a / 2) * cos ((n - 1) * a / 2 + b) / sin (a / 2) := by
+    ∑ i ∈ range n, cos (a * i + b) = sin (n * a / 2) * cos ((n - 1) * a / 2 + b) / sin (a / 2) := by
   rw [← sin_mul_sum_cos]
   grind [sin_ne_zero_iff]
 
@@ -300,6 +298,7 @@ end Complex
 namespace Real
 
 open scoped Real
+open Finset
 
 theorem cos_eq_zero_iff {θ : ℝ} : cos θ = 0 ↔ ∃ k : ℤ, θ = (2 * k + 1) * π / 2 :=
   mod_cast @Complex.cos_eq_zero_iff θ
@@ -377,24 +376,20 @@ theorem tan_eq_one_sub_tan_half_sq_div_one_add_tan_half_sq (x : ℝ) :
   mod_cast @Complex.tan_eq_one_sub_tan_half_sq_div_one_add_tan_half_sq x
 
 theorem sin_mul_sum_sin (n : ℕ) (a b : ℝ) :
-    sin (a / 2) * ∑ i ∈ Finset.range n, sin (a * i + b) =
-      sin (n * a / 2) * sin ((n - 1) * a / 2 + b) := by
+    sin (a / 2) * ∑ i ∈ range n, sin (a * i + b) = sin (n * a / 2) * sin ((n - 1) * a / 2 + b) := by
   exact_mod_cast congr($(Complex.sin_mul_sum_sin n a b).re)
 
 theorem sum_sin (n : ℕ) {a : ℝ} (h : ∀ k : ℤ, a ≠ k * (2 * π)) (b : ℝ) :
-    ∑ i ∈ Finset.range n, sin (a * i + b) =
-      sin (n * a / 2) * sin ((n - 1) * a / 2 + b) / sin (a / 2) := by
+    ∑ i ∈ range n, sin (a * i + b) = sin (n * a / 2) * sin ((n - 1) * a / 2 + b) / sin (a / 2) := by
   have h := Complex.sum_sin n (a := a) (by exact_mod_cast h) b
   exact_mod_cast congr($(h).re)
 
 theorem sin_mul_sum_cos (n : ℕ) (a b : ℝ) :
-    sin (a / 2) * ∑ i ∈ Finset.range n, cos (a * i + b) =
-      sin (n * a / 2) * cos ((n - 1) * a / 2 + b) := by
+    sin (a / 2) * ∑ i ∈ range n, cos (a * i + b) = sin (n * a / 2) * cos ((n - 1) * a / 2 + b) := by
   exact_mod_cast congr($(Complex.sin_mul_sum_cos n a b).re)
 
 theorem sum_cos (n : ℕ) {a : ℝ} (h : ∀ k : ℤ, a ≠ k * (2 * π)) (b : ℝ) :
-    ∑ i ∈ Finset.range n, cos (a * i + b) =
-      sin (n * a / 2) * cos ((n - 1) * a / 2 + b) / sin (a / 2) := by
+    ∑ i ∈ range n, cos (a * i + b) = sin (n * a / 2) * cos ((n - 1) * a / 2 + b) / sin (a / 2) := by
   have h := Complex.sum_cos n (a := a) (by exact_mod_cast h) b
   exact_mod_cast congr($(h).re)
 
