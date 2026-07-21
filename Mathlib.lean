a7cc4d4324d24c7a8a2205f324524ced9cf6f073
peakpoint <27919714+peakpoint@users.noreply.github.com>
2026-07-20 21:28:10 -0700

    feat: `Set.Finite.sigma`

--- Mathlib.lean
+++ Mathlib.lean
@@ -4357,6 +4357,7 @@ public import Mathlib.Data.Set.Finite.List
 public import Mathlib.Data.Set.Finite.Monad
 public import Mathlib.Data.Set.Finite.Powerset
 public import Mathlib.Data.Set.Finite.Range
+public import Mathlib.Data.Set.Finite.Sigma
 public import Mathlib.Data.Set.FiniteExhaustion
 public import Mathlib.Data.Set.Function
 public import Mathlib.Data.Set.Functor
