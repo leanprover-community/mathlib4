c61c11d389d8702f5f699e339b73db2319dc38d9
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2026-07-21 12:57:04 +0800

    extend doc

--- Mathlib/Algebra/Category/ModuleCat/ProjectiveDimension.lean
+++ Mathlib/Algebra/Category/ModuleCat/ProjectiveDimension.lean
@@ -14,6 +14,26 @@ public import Mathlib.CategoryTheory.Abelian.Projective.Dimension
 
 # Projective Dimension in ModuleCat
 
+This file deals with preservation of `projectiveDimension` in (semi) linear equivalences.
+Previously we only know this for linear equivalence within same universe level, now it works with
+all universe level where the ring `R` is small.
+
+## Main Results
+
+* `ModuleCat.hasProjectiveDimensionLE_of_semiLinearEquiv`: a module `N` satisfy
+  `HasProjectiveDimensionLE N n` if it is semi-linear equivalent to a module `M` that
+  `HasProjectiveDimensionLE M n`.
+
+* `ModuleCat.projectiveDimension_eq_of_semiLinearEquiv`: `projectiveDimension` is preserved
+  under arbitrary semi-linear equivalence.
+
+* `ModuleCat.hasProjectiveDimensionLE_of_linearEquiv`: a module `N` satisfy
+  `HasProjectiveDimensionLE N n` if it is linear equivalent to a module `M` that
+  `HasProjectiveDimensionLE M n`.
+
+* `ModuleCat.projectiveDimension_eq_of_linearEquiv`: `projectiveDimension` is preserved
+  under arbitrary linear equivalence.
+
 -/
 
 public section
