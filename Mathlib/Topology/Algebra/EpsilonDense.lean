86c9085cb40d8dc302efe0552d63bba2e506426f
WilliamCoram <williamecoram@gmail.com>
2026-07-21 20:08:38 +0100

    test foo lemma

--- Mathlib/Topology/Algebra/EpsilonDense.lean
+++ Mathlib/Topology/Algebra/EpsilonDense.lean
@@ -64,4 +64,28 @@ lemma dense_epsilonDense (ε : ℝ) (h1 : 0 < ε) (h2 : ε < 1) (h : epsilonDens
     Dense (H : Set G) := by
   simp [dense_iff_closure_eq, Metric.closure_eq, epsilonDense_dist_eq_zero H h1 h2 h]
 
+lemma foo (ε : ℝ) (h1 : 0 < ε) (h2 : ε < 1) (h : ∀ g : G, ∃ h : H, ‖g⁻¹ * (h : G)‖ ≤ ε * ‖g‖) :
+    Dense (H : Set G) := by
+  simp_rw [dense_iff_closure_eq]
+  suffices ∀ g : G, infDist g H = 0 by
+    ext x
+    simp [Metric.mem_closure_iff_infDist_zero]
+    grind
+  intro g
+  by_contra
+  suffices ∃ h₁ : H, ‖g⁻¹ * h₁‖ < ε⁻¹ * infDist g H by
+    obtain ⟨h₁, h₁_lt⟩ := this
+    obtain ⟨h₂, h₂_le⟩ := h ((h₁ : G)⁻¹ * g)
+    suffices dist g (↑h₁ * ↑h₂) < infDist g H by
+      grind [infDist_le_dist_of_mem (x := g) (s := H) (y := h₁ * h₂) (by aesop)]
+    calc _ = ‖((h₁ : G)⁻¹ * g)⁻¹ * h₂‖ := by rw [dist_eq]; group
+         _ ≤ ε * ‖(h₁ : G)⁻¹ * g‖ := h₂_le
+         _ = ε * ‖g⁻¹ * (h₁ : G)‖ := by rw [← dist_eq, dist_comm, dist_eq]
+         _ < ε * (ε⁻¹ * Metric.infDist g H) := by gcongr
+         _ = infDist g H := by grind
+  suffices infDist g ↑H < ε⁻¹ * infDist g ↑H by
+    obtain ⟨y, hy, _⟩ := (infDist_lt_iff ⟨1, H.one_mem⟩).mp this
+    exact ⟨⟨y, hy⟩, by rwa [← dist_eq]⟩
+  exact (lt_mul_iff_one_lt_left (by grind [infDist_nonneg])).mpr ((one_lt_inv₀ h1).mpr h2)
+
 end SeminormedGroup
