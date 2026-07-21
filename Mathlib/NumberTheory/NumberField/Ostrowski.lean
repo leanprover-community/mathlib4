746a7cfe29630826ea6cce25b37141d21c22051e
fbarroero <fbarroero@gmail.com>
2026-07-21 15:05:43 +0200

    first commit

---
+++ Mathlib/NumberTheory/NumberField/Ostrowski.lean
@@ -0,0 +1,51 @@
+/-
+Copyright (c) 2026 Fabrizio Barroero. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Fabrizio Barroero
+-/
+module
+
+public import Mathlib.Algebra.AffineMonoid.Basic
+public import Mathlib.Algebra.Order.Ring.Star
+public import Mathlib.Data.Int.Star
+public import Mathlib.NumberTheory.NumberField.ClassNumber
+public import Mathlib.NumberTheory.NumberField.Completion.FinitePlace
+public import Mathlib.RingTheory.DedekindDomain.SInteger
+public import Mathlib.RingTheory.Henselian
+public import Mathlib.RingTheory.RegularLocalRing.Defs
+public import Mathlib.RingTheory.Simpl
+
+/-!
+# Ostrowski’s Theorem for number fields
+
+Ostrowski's Theorem for number fields and non-archimedean absolute values:
+every non-archimedean absolute value on `K` is equivalent to .
+
+## Main results
+
+## TODO
+
+Archimedean case.
+
+## References
+
+* [K. Conrad, *Ostrowski for number fields*][conradnumbfield]
+
+## Tags
+
+absolute value, number field, Ostrowski's theorem
+-/
+
+@[expose] public section
+
+section Non_archimedean
+
+/-!
+### The non-archimedean case
+
+Every bounded absolute value on `K` is equivalent to .
+-/
+
+
+
+end Non_archimedean
