548640e529155f3157690a6ee0e0db3753e73338
Monica Omar <23701951+themathqueen@users.noreply.github.com>
2026-07-22 20:01:55 +0100

    mk_all

--- Mathlib.lean
+++ Mathlib.lean
@@ -6625,7 +6625,7 @@ public import Mathlib.RingTheory.FractionalIdeal.Operations
 public import Mathlib.RingTheory.FreeCommRing
 public import Mathlib.RingTheory.FreeRing
 public import Mathlib.RingTheory.Frobenius
-public import Mathlib.RingTheory.FrobeniusAlgebra
+public import Mathlib.RingTheory.FrobeniusAlgebra.IsFrobenius
 public import Mathlib.RingTheory.GradedAlgebra.AlgHom
 public import Mathlib.RingTheory.GradedAlgebra.Basic
 public import Mathlib.RingTheory.GradedAlgebra.FiniteType
