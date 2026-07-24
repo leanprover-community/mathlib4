96356c715b3362abade2ec1dc39dd6eba5dcc553
mitchell-horner <29882987+mitchell-horner@users.noreply.github.com>
2026-07-24 22:36:17 +1000

    the supersaturation theorem

--- Mathlib.lean
+++ Mathlib.lean
@@ -3669,6 +3669,7 @@ public import Mathlib.Combinatorics.SimpleGraph.Ends.Defs
 public import Mathlib.Combinatorics.SimpleGraph.Ends.Properties
 public import Mathlib.Combinatorics.SimpleGraph.Extremal.Basic
 public import Mathlib.Combinatorics.SimpleGraph.Extremal.ErdosStoneSimonovits
+public import Mathlib.Combinatorics.SimpleGraph.Extremal.Supersaturation
 public import Mathlib.Combinatorics.SimpleGraph.Extremal.Turan
 public import Mathlib.Combinatorics.SimpleGraph.Extremal.TuranDensity
 public import Mathlib.Combinatorics.SimpleGraph.Extremal.Zarankiewicz
