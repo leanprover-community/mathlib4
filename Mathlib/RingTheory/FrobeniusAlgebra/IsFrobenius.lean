f8438a122214c2a6a3f11b1d46c8e49c9bf5c3b9
Monica Omar <23701951+themathqueen@users.noreply.github.com>
2026-07-24 20:11:23 +0100

    some abbreviations

--- Mathlib/RingTheory/FrobeniusAlgebra/IsFrobenius.lean
+++ Mathlib/RingTheory/FrobeniusAlgebra/IsFrobenius.lean
@@ -39,30 +39,31 @@ local notation3 "γ⁻¹" => (TensorProduct.rid K _).symm.toLinearMap
 local notation "rT" => rTensor
 local notation "lT" => lTensor
 
-/-- The coevaluation of a Frobenius algebra. -/
-private noncomputable abbrev coev : K →ₗ[K] A ⊗[K] A :=
-  lT A (equivDual K A).symm ∘ₗ coevaluation K A
+/-- The natural left-sided comultiplication. -/
+noncomputable abbrev comulLeft : A →ₗ[K] A ⊗[K] A :=
+  lT A μ[K] ∘ₗ α ∘ₗ rT A (lT A (equivDual K A).symm ∘ₗ coevaluation K A) ∘ₗ β⁻¹
 
-/-- `dual` and `coev` satisfy the left snake equations. -/
-private lemma dual_comp_right_comp_rid :
-    lT A (dual ∘ₗ μ[K]) ∘ₗ α ∘ₗ rT A (coev K A) = TensorProduct.comm K K A := calc
+/-- The natural right-sided comultiplication. -/
+noncomputable abbrev comulRight : A →ₗ[K] A ⊗[K] A :=
+  rT A μ[K] ∘ₗ α⁻¹ ∘ₗ lT A (lT A (equivDual K A).symm ∘ₗ coevaluation K A) ∘ₗ γ⁻¹
+
+lemma lTensor_dual_comp_comulLeft_comp_lid :
+    lT A dual ∘ₗ comulLeft K A ∘ₗ β = TensorProduct.comm K K A := calc
   _ = lT A (contractLeft K A) ∘ₗ α ∘ₗ rT A (coevaluation K A) := by
-    ext; simp [coev, coevaluation_apply_one, sum_tmul, mul'_apply]
+    ext; simp [coevaluation_apply_one, sum_tmul, mul'_apply]
   _ = _ := by ext; simp [contractLeft_assoc_coevaluation']
 
-/-- `dual` and `coev` satisfy the right snake equations. -/
-private lemma dual_comp_left_comp_lid :
-    rT A (dual ∘ₗ μ[K]) ∘ₗ α⁻¹ ∘ₗ lT A (coev K A) = TensorProduct.comm K A K := calc
+lemma rTensor_comp_comulRight_comp_rid :
+    rT A dual ∘ₗ comulRight K A ∘ₗ γ = TensorProduct.comm K A K := calc
   _ = lT K (equivDual K A).symm ∘ₗ
       (rT (Dual K A) (contractLeft K A) ∘ₗ α⁻¹ ∘ₗ lT (Dual K A) (coevaluation K A)) ∘ₗ
       rT K (equivDual K A) := by
-    ext; simp [coev, coevaluation_apply_one, tmul_sum, mul'_apply]
+    ext; simp [coevaluation_apply_one, tmul_sum, mul'_apply]
   _ = _ := by ext; simp [contractLeft_assoc_coevaluation]
 
-/-- The comultiplication satisfies the Frobenius equation. -/
-private lemma left_eq_right_aux :
-    lT A μ[K] ∘ₗ α ∘ₗ rT A (coev K A) ∘ₗ β⁻¹ = rT A μ[K] ∘ₗ α⁻¹ ∘ₗ lT A (coev K A) ∘ₗ γ⁻¹ := by
-  obtain ⟨S, hS⟩ := exists_finset (coev K A 1)
+/-- The left-sided and right-sided comultiplication maps are equal. -/
+lemma comulLeft_eq_comulRight : comulLeft K A = comulRight K A := by
+  obtain ⟨S, hS⟩ := exists_finset ((lT A (equivDual K A).symm.toLinearMap ∘ₗ coevaluation K A) 1)
   ext a
   calc
     _ = ∑ p ∈ S, p.1 ⊗ₜ[K] (p.2 * a) := by simp [hS, sum_tmul]
@@ -73,37 +74,39 @@ private lemma left_eq_right_aux :
       · rfl
       intro s hs
       have : ∑ x ∈ S, dual (R := K) (s.2 * a * x.1) • x.2 = s.2 * a := by
-        simpa [hS, tmul_sum] using congr(β ($(dual_comp_left_comp_lid K A) ((s.2 * a) ⊗ₜ[K] 1)))
+        simpa [hS, tmul_sum] using
+          congr(β ($(rTensor_comp_comulRight_comp_rid K A) ((s.2 * a) ⊗ₜ[K] 1)))
       rw [← this]
       simp [tmul_sum, smul_tmul', mul_assoc]
       rfl
     _ = ∑ q ∈ S, (a * q.1) ⊗ₜ[K] q.2 := by
       have (a : A) : ∑ x ∈ S, dual (R := K) (x.2 * a) • x.1 = a := by
-        simpa [hS, tmul_sum, sum_tmul] using congr(γ ($(dual_comp_right_comp_rid K A) (1 ⊗ₜ[K] a)))
+        simpa [hS, tmul_sum, sum_tmul] using
+          congr(γ ($(lTensor_dual_comp_comulLeft_comp_lid K A) (1 ⊗ₜ[K] a)))
       simp_rw [this]
     _ = _ := by simp [hS, tmul_sum]
 
-/-- A Frobenius algebra induces a coalgebra. -/
+/-- A Frobenius algebra induces a coalgebra that satisfies the Frobenius equations
+(see `isFrobenius_toCoalgebra`).
+**Note** that this is not an instance since there can be other non equivalent coalgebras
+on your algebra. -/
 noncomputable abbrev toCoalgebra : Coalgebra K A where
-  comul := lT A μ[K] ∘ₗ α ∘ₗ rT A (lT A (equivDual K A).symm ∘ₗ coevaluation K A) ∘ₗ β⁻¹
+  comul := comulLeft K A
   counit := dual
   coassoc := by
     ext
-    nth_rw 2 3 [left_eq_right_aux]
+    nth_rw 2 3 [comulLeft_eq_comulRight]
     simp only [coe_comp, LinearEquiv.coe_coe, Function.comp_apply,
       rid_symm_apply, lTensor_tmul, coevaluation_apply_one, tmul_sum, map_sum, assoc_symm_tmul,
       rTensor_tmul, mul'_apply, lid_symm_apply, sum_tmul, assoc_tmul, mul_assoc]
     rw [Finset.sum_comm]
   rTensor_counit_comp_comul := by
-    rw [left_eq_right_aux]
-    simp only [← comp_assoc, ← rTensor_comp]
-    nth_rw 2 [comp_assoc]
-    rw [dual_comp_left_comp_lid]
+    rw [comulLeft_eq_comulRight, ← (TensorProduct.rid K _).eq_comp_toLinearMap_iff, comp_assoc,
+      rTensor_comp_comulRight_comp_rid]
     ext; simp
   lTensor_counit_comp_comul := by
-    simp only [← comp_assoc, ← lTensor_comp]
-    nth_rw 2 [comp_assoc]
-    rw [dual_comp_right_comp_rid]
+    rw [← (TensorProduct.lid K _).eq_comp_toLinearMap_iff, comp_assoc,
+      lTensor_dual_comp_comulLeft_comp_lid]
     ext; simp
 
 attribute [local instance] toCoalgebra in
@@ -111,7 +114,7 @@ attribute [local instance] toCoalgebra in
 theorem isFrobenius_toCoalgebra : (toCoalgebra K A).IsFrobenius K A where
   left_eq_right := by
     simp only [Coalgebra.IsFrobenius.left, Coalgebra.IsFrobenius.right, CoalgebraStruct.comul]
-    nth_rw 1 [left_eq_right_aux]
+    nth_rw 1 [comulLeft_eq_comulRight]
     ext
     simp [coevaluation_apply_one, tmul_sum, sum_tmul]
 
