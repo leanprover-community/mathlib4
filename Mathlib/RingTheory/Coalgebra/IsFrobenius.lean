1dcd6da752447711f5eeb7b00377ff438ccb0744
Monica Omar <23701951+themathqueen@users.noreply.github.com>
2026-07-24 20:31:22 +0100

    better names

--- Mathlib/RingTheory/Coalgebra/IsFrobenius.lean
+++ Mathlib/RingTheory/Coalgebra/IsFrobenius.lean
@@ -120,7 +120,7 @@ variable [IsFrobenius R A]
 instance _root_.CommSemiring.toIsFrobenius : IsFrobenius R R where
   left_eq_right := by ext; simp [left_def, right_def]
 
-lemma left_eq : left R A = δ ∘ₗ μ[R] := by
+lemma left_eq_comul_comp_mul : left R A = δ ∘ₗ μ[R] := by
   have h := ‹IsFrobenius R A›.left_eq_right
   simp only [left, lTensor, rTensor, right] at h ⊢
   calc
@@ -133,7 +133,8 @@ lemma left_eq : left R A = δ ∘ₗ μ[R] := by
     _ = β ∘ₗ lT R (δ ∘ₗ μ) ∘ₗ α ∘ₗ rT A (rT A ε ∘ₗ δ) := by simp only [coassoc_simps]
     _ = δ ∘ₗ μ := by simp only [coassoc_simps, CoassocSimps.map_counit_comp_comul_left]
 
-lemma right_eq : right R A = δ ∘ₗ μ[R] := by rw [← left_eq_right, left_eq]
+lemma right_eq_comul_comp_mul : right R A = δ ∘ₗ μ[R] := by
+  rw [← left_eq_right, left_eq_comul_comp_mul]
 
 -- TODO: show `IsFrobenius R (A ⊗ B)` and `IsFrobenius R (A × B)`
 -- should be easy, but annoying
@@ -150,12 +151,13 @@ variable {A : Type*} [NonAssocSemiring A] [Module R A] [Coalgebra R A]
 private lemma sum_counit_mul_left_smul_of_comul_one {S : Finset (A × A)}
     (hS : δ (1 : A) = ∑ i ∈ S, i.1 ⊗ₜ[R] i.2) (a : A) :
     ∑ x ∈ S, (ε : _ →ₗ[R] _) (a * x.1) • x.2 = a := by
-  simpa [hS, tmul_sum, right_def] using congr(β (rT A ε ($right_eq (a ⊗ₜ[R] 1))))
+  simpa [hS, tmul_sum, right_def] using congr(β (rT A ε ($right_eq_comul_comp_mul (a ⊗ₜ[R] 1))))
 
 private lemma sum_counit_mul_right_smul_of_comul_one {S : Finset (A × A)}
     (hS : δ (1 : A) = ∑ i ∈ S, i.1 ⊗ₜ[R] i.2) (a : A) :
     ∑ x ∈ S, (ε : _ →ₗ[R] _) (x.2 * a) • x.1 = a := by
-  simpa [hS, sum_tmul, left_def] using congr(TensorProduct.rid R A (lT A ε ($left_eq (1 ⊗ₜ[R] a))))
+  simpa [hS, sum_tmul, left_def] using
+    congr(TensorProduct.rid R A (lT A ε ($left_eq_comul_comp_mul (1 ⊗ₜ[R] a))))
 
 instance instFinite : Module.Finite R A := by
   have ⟨S, hS⟩ := exists_finset (R := R) (δ (1 : A))
@@ -214,7 +216,7 @@ version.
 (This is sometimes known as the left snake equation.) -/
 lemma lTensor_counit_comp_left_comp_rTensor_algebraLinearMap :
     lT A ε ∘ₗ left R A ∘ₗ rT A η[R] = (TensorProduct.comm _ _ _).toLinearMap := by
-  ext; simp [left_eq]
+  ext; simp [left_eq_comul_comp_mul]
 
 /-- Composing the right Frobenius equation with `Coalgebra.counit` and `Algebra.linearMap`.
 See `lTensor_counit_comp_left_comp_rTensor_algebraLinearMap` for the left Frobenius equation
@@ -223,7 +225,7 @@ version.
 (This is sometimes known as the right snake equation.) -/
 lemma rTensor_counit_comp_right_comp_lTensor_algebraLinearMap :
     rT A ε ∘ₗ right R A ∘ₗ lT A η[R] = (TensorProduct.comm _ _ _).toLinearMap := by
-  ext; simp [right_eq]
+  ext; simp [right_eq_comul_comp_mul]
 
 end Algebra
 
@@ -239,7 +241,7 @@ variable {A : Type*} [Semiring A] [Bialgebra R A] [IsFrobenius R A]
 
 @[simp] lemma comul_apply_eq_of_isFrobenius (a : A) : δ a = a ⊗ₜ[R] 1 := by
   simpa [Algebra.TensorProduct.one_def, IsFrobenius.right_def] using
-    congr($IsFrobenius.right_eq (a ⊗ₜ[R] 1)).symm
+    congr($IsFrobenius.right_eq_comul_comp_mul (a ⊗ₜ[R] 1)).symm
 
 lemma comul_eq_of_isFrobenius : δ = (TensorProduct.mk R A A).flip 1 :=
   ext comul_apply_eq_of_isFrobenius
