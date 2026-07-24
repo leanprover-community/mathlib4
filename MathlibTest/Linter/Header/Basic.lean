ded14cffba2cebada0aae350191f751228718493
Oliver Butterley <51876429+oliver-butterley@users.noreply.github.com>
2026-07-24 15:06:38 +0100

    Apply suggestions from code review
    Co-authored-by: Jon Eugster <eugster.jon@gmail.com>

--- MathlibTest/Linter/Header/Basic.lean
+++ MathlibTest/Linter/Header/Basic.lean
@@ -58,7 +58,7 @@ It logs details of what the linter would report if the `cop` is "malformed".
 elab "#check_copyright " copStx:str : command => do
   let cop := copStx.getString
   let offset := copStx.raw.getPos?.get!.increaseBy 1
-  let expectedLicense := Mathlib.Linter.linter.style.header.license.defValue
+  let expectedLicense := Mathlib.Linter.linter.style.header.license.get (← getOptions)
   for (s, m) in Mathlib.Linter.copyrightHeaderChecks cop expectedLicense do
     if let some rg := s.getRange? then
       logInfoAt (.ofRange ({start := rg.start.offsetBy offset, stop := rg.stop.offsetBy offset}))
--- MathlibTest/Linter/Header/License.lean
+++ MathlibTest/Linter/Header/License.lean
@@ -6,7 +6,8 @@ Authors: Oliver Butterley
 
 import Mathlib.Tactic.Linter.Header
 
-/- Test that the expected copyright license line is configurable via `linter.style.header.license`
+/-!
+Test that the expected copyright license line is configurable via `linter.style.header.license`
 (default: the Apache LICENSE line). -/
 
 /--
