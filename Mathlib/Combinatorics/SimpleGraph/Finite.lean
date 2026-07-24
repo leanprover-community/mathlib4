35a6c36ed76666ac6cf9ea738ed8aefacb5cba56
mitchell-horner <29882987+mitchell-horner@users.noreply.github.com>
2026-07-24 21:38:24 +1000

    edges of an induced subgraph as a filter

--- Mathlib/Combinatorics/SimpleGraph/Finite.lean
+++ Mathlib/Combinatorics/SimpleGraph/Finite.lean
@@ -682,6 +682,26 @@ theorem le_minDegree_induce_of_support_subset (h : G.support ⊆ s) :
 
 end Support
 
+section InduceEdgeFinset
+
+variable [DecidableEq V] [Fintype V] [DecidableRel G.Adj]
+
+theorem filter_edgeFinset_toFinset_subset (s : Finset V) :
+    {e ∈ G.edgeFinset | e.toFinset ⊆ s} = G.edgeFinset ∩ s.sym2 := by
+  simp [subset_iff, ← mem_sym2_iff, filter_mem_eq_inter]
+
+/-- The edges whose vertices lie in `s` are in bijection with the edges of the induced
+subgraph `G.induce s`. -/
+theorem card_filter_edgeFinset_toFinset_subset (s : Finset V) :
+    #{e ∈ G.edgeFinset | e.toFinset ⊆ s} = #(G.induce ↑s).edgeFinset := by
+  have h := congrArg Finset.card (map_edgeFinset_induce (s := (↑s : Set V)) (G := G))
+  rw [card_map, toFinset_coe] at h
+  rw [filter_edgeFinset_toFinset_subset]
+  convert h.symm using 1
+  congr!
+
+end InduceEdgeFinset
+
 section Map
 
 variable [Fintype V] {W : Type*} [Fintype W] [DecidableEq W]
