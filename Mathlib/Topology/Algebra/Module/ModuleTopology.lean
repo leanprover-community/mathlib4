44962f3cabf0b4d1c505d26de3ebccd15eadd56b
TJHeeringa <heeringa@protonmail.ch>
2026-07-25 16:55:00 +0200

    Fix import errors

--- Mathlib/Topology/Algebra/Module/ModuleTopology.lean
+++ Mathlib/Topology/Algebra/Module/ModuleTopology.lean
@@ -6,6 +6,7 @@ Authors: Kevin Buzzard, Will Sawin
 module
 
 public import Mathlib.Topology.Algebra.Module.Equiv.Basic
+public import Mathlib.Topology.Algebra.Module.Equiv.Pi
 public import Mathlib.RingTheory.Finiteness.Cardinality
 public import Mathlib.Algebra.Algebra.Bilinear
 public import Mathlib.Algebra.Group.Basic
