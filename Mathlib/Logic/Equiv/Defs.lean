93cfd58facee3c7bb4d9904e12bbaf7f56648f15
TJHeeringa <heeringa@protonmail.ch>
2026-07-26 20:14:48 +0200

    Degrind apply_eq_iff_eq_symm_apply for logic Equiv

--- Mathlib/Logic/Equiv/Defs.lean
+++ Mathlib/Logic/Equiv/Defs.lean
@@ -299,7 +299,8 @@ theorem symm_apply_eq {α β} (e : α ≃ β) {x y} : e.symm x = y ↔ x = e y :
 theorem eq_symm_apply {α β} (e : α ≃ β) {x y} : y = e.symm x ↔ e y = x := by grind
 
 @[deprecated eq_symm_apply (since := "2026-07-26")]
-theorem apply_eq_iff_eq_symm_apply {x : α} {y : β} (f : α ≃ β) : f x = y ↔ x = f.symm y := by grind
+theorem apply_eq_iff_eq_symm_apply {x : α} {y : β} (f : α ≃ β) : f x = y ↔ x = f.symm y :=
+  f.eq_symm_apply.symm
 
 @[simp, grind =] theorem symm_symm (e : α ≃ β) : e.symm.symm = e := rfl
 
