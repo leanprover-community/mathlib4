db6a2e87660d91f2f063f0dad07fe6324ea859ea
Lambert <lambertacampo@gmail.com>
2026-07-27 00:22:10 +0200

    remove abbreviation for principal ideal

--- Mathlib/RingTheory/Noetherian/InjectiveModule.lean
+++ Mathlib/RingTheory/Noetherian/InjectiveModule.lean
@@ -56,13 +56,11 @@ private lemma ascending_ideal_b (f : R) : Monotone (ideal_b f) := by
 set_option backward.privateInPublic true in
 private abbrev ideal_b_order_hom (f : R) : ℕ →o Ideal R := ⟨ideal_b f, ascending_ideal_b f⟩
 
-private abbrev ideal_p (f : R) (i : ℕ) : Ideal R := Ideal.span {f ^ i}
-
 set_option backward.privateInPublic true in
 private lemma exists_divide_by_fn_map (f : R) (n : ℕ) (r : ℕ)
     (hr : ∀ (i : ℕ), ideal_b f (r + i) = ideal_b f r)
     {M : Type u} [AddCommGroup M] [Module R M] (y : M) :
-    ∃ phi : ideal_p f (r + n) →ₗ[R] M,
+    ∃ phi : Ideal.span {f^(r + n)} →ₗ[R] M,
       phi ⟨f ^ (r + n), Ideal.mem_span_singleton_self _⟩ = f ^ r • y := by
         have hker0 : ideal_b f r ≤ LinearMap.ker (LinearMap.toSpanSingleton R M (f ^ r • y)) := by
           intro c hc
@@ -76,7 +74,7 @@ private lemma exists_divide_by_fn_map (f : R) (n : ℕ) (r : ℕ)
             (LinearMap.toSpanSingleton R M (f ^ r • y)) hker).comp
           (Ideal.quotTorsionOfEquivSpanSingleton R R (f ^ (r + n))).symm.toLinearMap, ?_⟩
         have hmk : (Ideal.quotTorsionOfEquivSpanSingleton R R (f ^ (r + n))).symm
-            (⟨f ^ (r + n), Ideal.mem_span_singleton_self _⟩ : ideal_p f (r + n))
+            (⟨f ^ (r + n), Ideal.mem_span_singleton_self _⟩ : Ideal.span {f^(r + n)})
             = Submodule.Quotient.mk (1 : R) := by
           rw [LinearEquiv.symm_apply_eq, Ideal.quotTorsionOfEquivSpanSingleton_apply_mk, one_smul]
         change (Submodule.liftQ (ideal_b f (r + n))
@@ -102,31 +100,31 @@ theorem Module.surjective_of_isLocalizedModule_of_baer {I : Type u}
     [AddCommGroup I] [Module R I] (hI : Module.Baer R I) (f : R) {I' : Type u} [AddCommGroup I']
     [Module R I'] (g : I →ₗ[R] I') [IsLocalizedModule (Submonoid.powers f) g] :
     Function.Surjective g := by
-      intro x
-     obtain ⟨n, a, ha⟩ : ∃ (n : ℕ) (a : I), g a = f ^ n • x := by
-        obtain ⟨⟨a, s⟩, hs⟩ := IsLocalizedModule.surj (Submonoid.powers f) g x
-        choose n hn using (Submonoid.mem_powers_iff s.1 f).1 s.2
-        use n, a
-        exact hn ▸ hs.symm
-      choose r hr using stabilize_ideal_b f
-      choose phi hphi using exists_divide_by_fn_map f n r hr a
-      choose psi hpsi using hI (ideal_p f (r + n)) phi
-      let z := psi 1
-      have hz : f ^ (r + n) • z = f ^ r • a := by
-        rw [← psi.map_smul (f ^ (r + n)) (1 : R), smul_eq_mul, mul_one, ← hphi]
-        exact hpsi _ _
-      have hz2 : f ^ (r + n) • (g z) = f ^ r • (f ^ n • x) := by
-        rw [← ha, ← g.map_smul, ← g.map_smul]
-        exact congrArg g hz
-      use z
-      have s_mem : f ^ (r + n) ∈ Submonoid.powers f :=
-        (Submonoid.mem_powers_iff (f ^ (r + n)) f).mpr ⟨r + n, rfl⟩
-      have hbij : Function.Bijective
-          (algebraMap R (Module.End R I') (⟨f ^ (r + n), s_mem⟩ : Submonoid.powers f)) :=
-        (Module.End.isUnit_iff _).mp (IsLocalizedModule.map_units g ⟨f ^ (r + n), s_mem⟩)
-      apply hbij.1
-      change f ^ (r + n) • (g z) = f ^ (r + n) • x
-      rw [hz2, smul_smul, ← pow_add]
+  intro x
+  obtain ⟨n, a, ha⟩ : ∃ (n : ℕ) (a : I), g a = f ^ n • x := by
+    obtain ⟨⟨a, s⟩, hs⟩ := IsLocalizedModule.surj (Submonoid.powers f) g x
+    choose n hn using (Submonoid.mem_powers_iff s.1 f).1 s.2
+    use n, a
+    exact hn ▸ hs.symm
+  choose r hr using stabilize_ideal_b f
+  choose phi hphi using exists_divide_by_fn_map f n r hr a
+  choose psi hpsi using hI (Ideal.span {f^(r + n)}) phi
+  let z := psi 1
+  have hz : f ^ (r + n) • z = f ^ r • a := by
+    rw [← psi.map_smul (f ^ (r + n)) (1 : R), smul_eq_mul, mul_one, ← hphi]
+    exact hpsi _ _
+  have hz2 : f ^ (r + n) • (g z) = f ^ r • (f ^ n • x) := by
+    rw [← ha, ← g.map_smul, ← g.map_smul]
+    exact congrArg g hz
+  use z
+  have s_mem : f ^ (r + n) ∈ Submonoid.powers f :=
+    (Submonoid.mem_powers_iff (f ^ (r + n)) f).mpr ⟨r + n, rfl⟩
+  have hbij : Function.Bijective
+    (algebraMap R (Module.End R I') (⟨f ^ (r + n), s_mem⟩ : Submonoid.powers f)) :=
+    (Module.End.isUnit_iff _).mp (IsLocalizedModule.map_units g ⟨f ^ (r + n), s_mem⟩)
+  apply hbij.1
+  change f ^ (r + n) • (g z) = f ^ (r + n) • x
+  rw [hz2, smul_smul, ← pow_add]
 
 /-- **Hartshorne III. Lemma 3.3.** Let `R` be a Noetherian ring and `I` an injective `R`-module.
 For any `f : R`, the localization map `I → I_f` is surjective. -/
