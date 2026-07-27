0782d6b4389e932956166372636b183c3abf3f0b
Kim Morrison <kim@tqft.net>
2026-07-27 04:18:16 +0000

    chore: use nightly ProofWidgets

--- lakefile.lean
+++ lakefile.lean
@@ -10,7 +10,7 @@ require "leanprover-community" / "batteries" @ git "nightly-testing"
 require "leanprover-community" / "Qq" @ git "master"
 
 require "leanprover-community" / "aesop" @ git "master"
-require "leanprover-community" / "proofwidgets" @ git "main"
+require "leanprover-community" / "proofwidgets" @ git "nightly-testing"
   with NameMap.empty.insert `errorOnBuild
     "ProofWidgets failed to reuse pre-built JS code. \
     Please report this issue on the Lean Zulip."
