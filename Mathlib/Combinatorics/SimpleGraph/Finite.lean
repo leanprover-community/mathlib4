22c6708a6d356615e18d120b451d5ad53031e25b
mitchell-horner <29882987+mitchell-horner@users.noreply.github.com>
2026-07-27 22:20:28 +1000

    Move to `section Support`

--- Mathlib/Combinatorics/SimpleGraph/Finite.lean
+++ Mathlib/Combinatorics/SimpleGraph/Finite.lean
@@ -680,19 +680,13 @@ theorem le_minDegree_induce_of_support_subset (h : G.support ⊆ s) :
   grw [G.minDegree_le_degree v, degree_induce_of_neighborSet_subset]
   grw [neighborSet_subset_support, h]
 
-end Support
-
-section InduceEdgeFinset
-
-variable [DecidableEq V] [Fintype V] [DecidableRel G.Adj]
-
-theorem filter_edgeFinset_toFinset_subset (s : Finset V) :
+theorem filter_edgeFinset_toFinset_subset [DecidableEq V] (s : Finset V) :
     {e ∈ G.edgeFinset | e.toFinset ⊆ s} = G.edgeFinset ∩ s.sym2 := by
   simp [subset_iff, ← mem_sym2_iff, filter_mem_eq_inter]
 
 /-- The edges whose vertices lie in `s` are in bijection with the edges of the induced
 subgraph `G.induce s`. -/
-theorem card_filter_edgeFinset_toFinset_subset (s : Finset V) :
+theorem card_filter_edgeFinset_toFinset_subset [DecidableEq V] (s : Finset V) :
     #{e ∈ G.edgeFinset | e.toFinset ⊆ s} = #(G.induce ↑s).edgeFinset := by
   have h := congrArg Finset.card (map_edgeFinset_induce (s := (↑s : Set V)) (G := G))
   rw [card_map, toFinset_coe] at h
@@ -700,7 +694,7 @@ theorem card_filter_edgeFinset_toFinset_subset (s : Finset V) :
   convert h.symm using 1
   congr!
 
-end InduceEdgeFinset
+end Support
 
 section Map
 
