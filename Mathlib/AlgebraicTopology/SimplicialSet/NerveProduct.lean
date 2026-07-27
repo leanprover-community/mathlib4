ac9800b417b53c33d797559c04d12d8fed0e08f2
Dennis Sweeney <sweeney.427@osu.edu>
2026-07-27 02:53:48 -0400

    remove lemmas that follow from monoidal functor properties

--- Mathlib/AlgebraicTopology/SimplicialSet/NerveProduct.lean
+++ Mathlib/AlgebraicTopology/SimplicialSet/NerveProduct.lean
@@ -30,22 +30,6 @@ def nerveProdIso (C₁ C₂ : Type u) [Category.{v} C₁] [Category.{v} C₂] :
     nerve (C₁ × C₂) ≅ nerve C₁ ⊗ nerve C₂ :=
   NatIso.ofComponents (fun n ↦ (ComposableArrows.prodEquiv C₁ C₂ n.unop.len).toIso)
 
-section
-variable {C₁ C₂ : Type u} [Category.{v} C₁] [Category.{v} C₂]
-variable {D₁ D₂ : Type u} [Category.{v} D₁] [Category.{v} D₂]
-
-lemma nerveOfProdMap_prod_nerveMap (F₁ : C₁ ⥤ D₁) (F₂ : C₂ ⥤ D₂) :
-    (nerveProdIso C₁ C₂).inv ≫ nerveMap (F₁.prod F₂) ≫ (nerveProdIso D₁ D₂).hom =
-      nerveMap F₁ ⊗ₘ nerveMap F₂ := rfl
-
-variable {E₁ E₂ : Type u} [Category.{v} E₁] [Category.{v} E₂]
-
-lemma nerve_of_product_interchange (F₁ : C₁ ⥤ D₁) (F₂ : C₂ ⥤ D₂) (G₁ : D₁ ⥤ E₁) (G₂ : D₂ ⥤ E₂) :
-    (nerveMap F₁ ⊗ₘ nerveMap F₂) ≫ (nerveMap G₁ ⊗ₘ nerveMap G₂) =
-      nerveMap (F₁ ⋙ G₁) ⊗ₘ nerveMap (F₂ ⋙ G₂) := rfl
-
-end
-
 instance : Functor.Monoidal nerveFunctor where
   δ C₁ C₂ := (nerveProdIso C₁ C₂).hom
   μ C₁ C₂ := (nerveProdIso C₁ C₂).inv
