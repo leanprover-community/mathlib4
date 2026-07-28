271ff204af083207ea0212fd8fc61b23709a1209
tb65536 <thomas.l.browning@gmail.com>
2026-07-28 21:48:27 +0100

    fix

--- Mathlib/GroupTheory/Finiteness.lean
+++ Mathlib/GroupTheory/Finiteness.lean
@@ -10,7 +10,6 @@ public import Mathlib.Algebra.Group.Pointwise.Set.Finite
 public import Mathlib.Algebra.Group.Subgroup.Pointwise
 public import Mathlib.Algebra.Group.Subgroup.ZPowers.Basic
 public import Mathlib.Algebra.Group.Submonoid.BigOperators
-public import Mathlib.Algebra.Group.Subsemigroup.Operations
 public import Mathlib.GroupTheory.FreeGroup.Basic
 public import Mathlib.GroupTheory.QuotientGroup.Defs
 
