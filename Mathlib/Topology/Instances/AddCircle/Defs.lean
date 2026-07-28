9342b1e9bb686da8f61d6a958c154ccd60e29195
Evgenia Karunus <lakesare@gmail.com>
2026-07-28 20:31:54 +0200

    `theorem equivAddCircle_eq` - upstream

--- Mathlib/Topology/Instances/AddCircle/Defs.lean
+++ Mathlib/Topology/Instances/AddCircle/Defs.lean
@@ -515,6 +515,13 @@ theorem equivAddCircle_symm_apply_mk (hp : p ≠ 0) (hq : q ≠ 0) (x : 𝕜) :
     (equivAddCircle p q hp hq).symm (x : 𝕜) = (x * (q⁻¹ * p) : 𝕜) :=
   rfl
 
+theorem equivAddCircle_eq [LinearOrder 𝕜] [IsOrderedAddMonoid 𝕜] [Archimedean 𝕜]
+    [hp : Fact (0 < p)] (hq : q ≠ 0) :
+    ⇑(equivAddCircle p q hp.out.ne' hq)
+      = fun x ↦ ((equivIco p 0 x : 𝕜) * (p⁻¹ * q) : AddCircle q) := by
+  ext x
+  grind [coe_equivIco, equivAddCircle_apply_mk]
+
 section
 variable [LinearOrder 𝕜] [IsStrictOrderedRing 𝕜] [TopologicalSpace 𝕜] [OrderTopology 𝕜]
 
