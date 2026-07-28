9671b6f69de0ec61d9efbe0cb57319bf9b5aa028
Vlad Tsyrklevich <vlad@tsyrklevi.ch>
2026-07-28 10:18:25 +0200

    Generalize to magmas and plumb throughout the algebra hierarchy

--- Mathlib/Algebra/Algebra/NonUnitalSubalgebra.lean
+++ Mathlib/Algebra/Algebra/NonUnitalSubalgebra.lean
@@ -1136,6 +1136,11 @@ variable {R A}
 theorem mem_center_iff {a : A} : a ∈ center R A ↔ ∀ b : A, b * a = a * b :=
   Subsemigroup.mem_center_iff
 
+theorem map_center_le_center {B F} [NonUnitalNonAssocSemiring B] [Module R B]
+    [IsScalarTower R B B] [SMulCommClass R B B] [FunLike F A B] [NonUnitalAlgHomClass F R A B]
+    {f : F} (hf : Function.Surjective f) : map f (center R A) ≤ center R B :=
+  NonUnitalSubsemiring.map_center_le_center hf
+
 end Center
 
 section Centralizer
--- Mathlib/Algebra/Ring/Subsemiring/Basic.lean
+++ Mathlib/Algebra/Ring/Subsemiring/Basic.lean
@@ -302,6 +302,18 @@ theorem mem_center_iff {R} [Semiring R] {z : R} : z ∈ center R ↔ ∀ g, g * 
 instance decidableMemCenter {R} [Semiring R] [DecidableEq R] [Fintype R] :
     DecidablePred (· ∈ center R) := fun _ => decidable_of_iff' _ mem_center_iff
 
+theorem map_center_le_center {F} [FunLike F R S] [RingHomClass F R S] {f : F}
+    (hf : Function.Surjective f) : map f (center R) ≤ center S :=
+  NonUnitalSubsemiring.map_center_le_center hf
+
+theorem center_le_comap_center {F} [FunLike F R S] [RingHomClass F R S] {f : F}
+    (hf : Function.Surjective f) : center R ≤ comap f (center S) :=
+  map_le_iff_le_comap.mp (map_center_le_center hf)
+
+@[simp]
+theorem map_center_eq (f : R ≃+* S) : map f (center R) = center S :=
+  SetLike.coe_injective Set.image_center_eq
+
 @[simp]
 theorem center_eq_top (R) [CommSemiring R] : center R = ⊤ :=
   SetLike.coe_injective (Set.center_eq_univ R)
