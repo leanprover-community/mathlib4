de4f6df9e4450badabd0e400b17e610ac1380079
Jireh Loreaux <loreaujy@gmail.com>
2026-07-28 18:00:51 -0500

    copyright

--- Mathlib/Topology/Algebra/Module/ContinuousLinearMap/Positive.lean
+++ Mathlib/Topology/Algebra/Module/ContinuousLinearMap/Positive.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2026 Jireh Loreaux. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Jireh Loreaux
+-/
 module
 
 public import Mathlib.Topology.Algebra.Module.ContinuousLinearMap.Basic
