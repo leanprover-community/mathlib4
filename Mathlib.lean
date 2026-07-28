fc982ac4322b06d6e604880d98b099c70172cde1
Jack McKoen <104791831+mckoen@users.noreply.github.com>
2026-07-28 10:56:58 -0600

    scrap

--- Mathlib.lean
+++ Mathlib.lean
@@ -3177,6 +3177,8 @@ public import Mathlib.CategoryTheory.Monoidal.Rigid.Functor
 public import Mathlib.CategoryTheory.Monoidal.Rigid.FunctorCategory
 public import Mathlib.CategoryTheory.Monoidal.Rigid.OfEquivalence
 public import Mathlib.CategoryTheory.Monoidal.Rigid.Pivotal
+public import Mathlib.CategoryTheory.Monoidal.Rigid.Spherical
+public import Mathlib.CategoryTheory.Monoidal.Rigid.Trace
 public import Mathlib.CategoryTheory.Monoidal.Ring
 public import Mathlib.CategoryTheory.Monoidal.Skeleton
 public import Mathlib.CategoryTheory.Monoidal.Subcategory
