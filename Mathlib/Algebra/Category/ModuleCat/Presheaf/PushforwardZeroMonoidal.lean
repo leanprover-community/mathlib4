dc15f66107661025c5d92cc917dec9617132f7d6
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2026-07-28 10:12:09 +0100

    apply changes from code review

--- Mathlib/Algebra/Category/ModuleCat/Presheaf/PushforwardZeroMonoidal.lean
+++ Mathlib/Algebra/Category/ModuleCat/Presheaf/PushforwardZeroMonoidal.lean
@@ -36,13 +36,13 @@ noncomputable instance : (pushforward₀OfCommRingCat F R).Monoidal :=
       -- using `Iso.refl _` for `μIso` directly hurts kernel typechecking
       μIso _ _ := isoMk (fun _ ↦ Iso.refl _) (fun _ _ _ ↦ tensor_ext fun _ _ ↦ rfl)
       associativity _ _ _ := by
-        ext1 x
+        ext1
         exact tensor_ext₃' fun m₁ m₂ m₃ ↦ rfl
       left_unitality _ := by
-        ext1 x
+        ext1
         exact tensor_ext fun m₁ m₂ ↦ rfl
       right_unitality _ := by
-        ext1 x
+        ext1
         exact tensor_ext fun m₁ m₂ ↦ rfl
     }
 
