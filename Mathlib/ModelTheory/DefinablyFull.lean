825f4f63cb5d7173f6a3b0316ad3ef747ae44e65
NoneMore <hellgoingup@outlook.com>
2026-07-29 01:43:08 +0800

    update copyright information and author name

--- Mathlib/ModelTheory/DefinablyFull.lean
+++ Mathlib/ModelTheory/DefinablyFull.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2026 NoneMore. All rights reserved.
+Copyright (c) 2026 Zikang Yu. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: NoneMore
+Authors: Zikang Yu
 -/
 module
 
