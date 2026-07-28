ecfe73d92040b0ab4877e491324fa54c6f1e4d47
Jack McKoen <104791831+mckoen@users.noreply.github.com>
2026-07-28 10:56:22 -0600

    mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -3176,6 +3176,7 @@ public import Mathlib.CategoryTheory.Monoidal.Rigid.Braided
 public import Mathlib.CategoryTheory.Monoidal.Rigid.Functor
 public import Mathlib.CategoryTheory.Monoidal.Rigid.FunctorCategory
 public import Mathlib.CategoryTheory.Monoidal.Rigid.OfEquivalence
+public import Mathlib.CategoryTheory.Monoidal.Rigid.Pivotal
 public import Mathlib.CategoryTheory.Monoidal.Ring
 public import Mathlib.CategoryTheory.Monoidal.Skeleton
 public import Mathlib.CategoryTheory.Monoidal.Subcategory
