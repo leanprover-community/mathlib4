n-fug/Lean4

refactor: make `Set` into a one-field structure

Mathlib/MeasureTheory/MeasurableSpace/Constructions.lean
@@ -870,7 +870,9 @@ variable [MeasurableSpace α] {p q : α → Prop}
 @[simp] theorem measurable_mem : Measurable (· ∈ s) ↔ MeasurableSet s :=
   measurableSet_setOfPred.symm
 
-alias ⟨_, Measurable.setOf⟩ := measurableSet_setOf
+@[simp] theorem measurable_mem : Measurable (· ∈ s) ↔ MeasurableSet s := measurableSet_ofPred.symm
+
+alias ⟨_, Measurable.setOf⟩ := measurableSet_ofPred
 
 @[fun_prop]
 alias ⟨_, MeasurableSet.mem⟩ := measurable_mem

leanprover-community/mathlib4

Mathlib/MeasureTheory/MeasurableSpace/Constructions.lean
@@ -870,7 +870,9 @@ variable [MeasurableSpace α] {p q : α → Prop}
 @[simp] theorem measurable_mem : Measurable (· ∈ s) ↔ MeasurableSet s :=
   measurableSet_setOfPred.symm
 
-alias ⟨_, Measurable.setOf⟩ := measurableSet_setOfPred
+@[simp] theorem measurable_mem : Measurable (· ∈ s) ↔ MeasurableSet s := measurableSet_ofPred.symm
+
+alias ⟨_, Measurable.setOf⟩ := measurableSet_ofPredPred
 
 @[fun_prop]
 alias ⟨_, MeasurableSet.mem⟩ := measurable_mem