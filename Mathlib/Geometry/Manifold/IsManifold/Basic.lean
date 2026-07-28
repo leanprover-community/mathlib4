20eb89e329908a7071c2b004ac66fe8d44a81741
Michael Rothgang <rothgang@math.uni-bonn.de>
2026-07-28 23:25:56 +0200

    chore(Geometry/Manifold): avoid some underscore soup

--- Mathlib/Geometry/Manifold/IsManifold/Basic.lean
+++ Mathlib/Geometry/Manifold/IsManifold/Basic.lean
@@ -990,7 +990,8 @@ theorem OpenPartialHomeomorph.isManifold_singleton
     {𝕜 : Type*} [NontriviallyNormedField 𝕜] {E : Type*} [NormedAddCommGroup E] [NormedSpace 𝕜 E]
     {H : Type*} [TopologicalSpace H] {I : ModelWithCorners 𝕜 E H} {n : ℕ∞ω}
     {M : Type*} [TopologicalSpace M] (e : OpenPartialHomeomorph M H) (h : e.source = Set.univ) :
-    @IsManifold 𝕜 _ E _ _ H _ I n M _ (e.singletonChartedSpace h) :=
+    letI := e.singletonChartedSpace h
+    IsManifold I n M :=
   @IsManifold.mk' _ _ _ _ _ _ _ _ _ _ _ (id _) <|
     e.singleton_hasGroupoid h (contDiffGroupoid n I)
 
@@ -998,7 +999,8 @@ theorem Topology.IsOpenEmbedding.isManifold_singleton {𝕜 E H : Type*}
     [NontriviallyNormedField 𝕜] [NormedAddCommGroup E] [NormedSpace 𝕜 E] [TopologicalSpace H]
     {I : ModelWithCorners 𝕜 E H} {n : ℕ∞ω}
     {M : Type*} [TopologicalSpace M] [Nonempty M] {f : M → H} (h : IsOpenEmbedding f) :
-    @IsManifold 𝕜 _ E _ _ H _ I n M _ h.singletonChartedSpace :=
+    letI := h.singletonChartedSpace
+    IsManifold I n M :=
   (h.toOpenPartialHomeomorph f).isManifold_singleton (by simp)
 
 namespace TopologicalSpace.Opens
