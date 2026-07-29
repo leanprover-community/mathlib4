f910f9b5f46383cccba746b359b1aa10aef404e7
Moritz Doll <moritz.doll@googlemail.com>
2026-07-29 21:05:48 +1000

    docstring

--- Mathlib/Data/FunLike/Group.lean
+++ Mathlib/Data/FunLike/Group.lean
@@ -279,7 +279,8 @@ end GroupInstances
 section TorsionFree
 
 /-- A type `F` that satisfies `FunLike F α β` is torsion-free if `β` is torsion-free. -/
-@[to_additive /-- A `FunLike` type torsion-free if `β` is torsion-free. -/]
+@[to_additive /-- A type `F` that satisfies `FunLike F α β` is torsion-free if `β` is
+torsion-free. -/]
 protected theorem isMulTorsionFree [Monoid β] [Monoid F] [IsOneApply F α β] [IsMulApply F α β]
     [IsMulTorsionFree β] : IsMulTorsionFree F :=
   DFunLike.coe_injective.isMulTorsionFree (coeMonoidHom F α β)
