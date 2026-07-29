f76a1eeb8a994355bb1e7197c11cd91ef08a77b5
Monica Omar <23701951+themathqueen@users.noreply.github.com>
2026-07-29 21:20:50 +0100

    cleanup golfs

--- Mathlib/Analysis/CStarAlgebra/Extreme.lean
+++ Mathlib/Analysis/CStarAlgebra/Extreme.lean
@@ -203,44 +203,38 @@ variable {𝕜 E : Type*} [Semiring 𝕜] [StarRing 𝕜] [PartialOrder 𝕜] [S
 
 open Metric Set
 
-@[simp] lemma star_segment (x y : E) :
-    star (segment 𝕜 x y) = segment 𝕜 (star x) (star y) := by
-  ext a
-  simp only [mem_star, segment, exists_and_left, mem_ofPred_eq]
-  refine ⟨?_, ?_⟩ <;> rintro ⟨h, x₁, w, x₂, h₁, h₂⟩
-    <;> use star h, by simpa, star w, by simpa, by simp [← star_add, h₁]
-  · simp [← star_add, ← star_smul, h₂]
-  · simp [← h₂]
+@[simp] lemma star_segment (x y : E) : star (segment 𝕜 x y) = segment 𝕜 (star x) (star y) := by
+  suffices ∀ x y : E, segment 𝕜 x y ⊆ star (segment 𝕜 (star x) (star y)) from
+    le_antisymm (by simpa [star_subset] using this x y) (by simpa using this (star x) (star y))
+  refine fun x y a ⟨b, c, hb, hc, hbc, ha⟩ ↦ ⟨star b, star c, by simpa, by simpa, ?_, ?_⟩
+  · simp [← star_add, hbc]
+  simpa [← star_add, ← star_smul]
 
 @[simp] lemma star_openSegment (x y : E) :
     star (openSegment 𝕜 x y) = openSegment 𝕜 (star x) (star y) := by
-  ext a
-  simp only [mem_star, openSegment, exists_and_left, mem_ofPred_eq]
-  refine ⟨?_, ?_⟩ <;> rintro ⟨h, x₁, w, x₂, h₁, h₂⟩
-    <;> use star h, by simpa, star w, by simpa, by simp [← star_add, h₁]
-  · simp [← star_add, ← star_smul, h₂]
-  · simp [← h₂]
+  suffices ∀ x y : E, openSegment 𝕜 x y ⊆ star (openSegment 𝕜 (star x) (star y)) from
+    le_antisymm (by simpa [star_subset] using this x y) (by simpa using this (star x) (star y))
+  refine fun x y a ⟨b, c, hb, hc, hbc, ha⟩ ↦ ⟨star b, star c, by simpa, by simpa, ?_, ?_⟩
+  · simp [← star_add, hbc]
+  simpa [← star_add, ← star_smul]
 
 @[simp] theorem star_extremePoints (s : Set E) :
     star (extremePoints 𝕜 s) = extremePoints 𝕜 (star s) := by
-  ext
-  simp only [mem_star, mem_extremePoints_iff_left, and_congr_right_iff]
-  refine fun a ↦ ⟨?_, ?_⟩ <;> intro  h x₁ _ x₂ _ _ <;> rw [← star_inj]
-  on_goal 2 => rw [star_star]
-  all_goals apply h _ (by simpa) (star x₂) (by simpa); simpa [← star_openSegment]
+  suffices ∀ s : Set E, extremePoints 𝕜 s ⊆ star (extremePoints 𝕜 (star s)) from
+    le_antisymm (by simpa [star_subset] using this s) (by simpa using this (star s))
+  refine fun s a ⟨ha, h⟩ ↦ ⟨by simpa, fun _ h₁ _ h₂ ↦ ?_⟩
+  simpa [← mem_star, ← star_inj (y := star a)] using h h₁ h₂
 
 variable {A : Type*} [SeminormedAddCommGroup A] [StarAddMonoid A] [NormedStarGroup A]
 
-@[simp] theorem star_ball (x : A) (s : ℝ) :
-    star (ball x s) = ball (star x) s := by
+@[simp] theorem star_ball (x : A) (s : ℝ) : star (ball x s) = ball (star x) s := by
   ext; simp [dist_eq_norm, ← norm_star (star _ - _)]
 
 @[simp] theorem star_closedBall (x : A) (s : ℝ) :
     star (closedBall x s) = closedBall (star x) s := by
   ext; simp [dist_eq_norm, ← norm_star (star _ - _)]
 
-@[simp] theorem star_sphere (x : A) (s : ℝ) :
-    star (sphere x s) = sphere (star x) s := by
+@[simp] theorem star_sphere (x : A) (s : ℝ) : star (sphere x s) = sphere (star x) s := by
   ext; simp [← norm_star (star _ - _)]
 
 end star_balls
