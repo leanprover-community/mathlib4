818f8aeb55302d2bd40067fe0ecc81870563c772
fbarroero <fbarroero@gmail.com>
2026-07-30 16:26:07 +0200

    fix

--- Mathlib/NumberTheory/NumberField/Ostrowski.lean
+++ Mathlib/NumberTheory/NumberField/Ostrowski.lean
@@ -13,7 +13,7 @@ public import Mathlib.RingTheory.DedekindDomain.SInteger
 
 Ostrowski's Theorem for number fields:
 every nontrivial absolute value on `K` is equivalent to either a `v`-adic absolute value for some
-`v : HeightOneSpectrum (𝓞 K)` or to some archimedean absolute value induced by an embedding of `K`
+`v : HeightOneSpectrum (𝓞 K)` or to an archimedean absolute value induced by an embedding of `K`
 into `ℂ`.
 
 ## Main results
@@ -43,7 +43,7 @@ section Nonarchimedean
 /-!
 ### The non-archimedean case
 
-Every bounded absolute value on `K` is equivalent to a `v`-adic absolute value for some
+Every non-archimedean absolute value on `K` is equivalent to a `v`-adic absolute value for some
 `v : HeightOneSpectrum (𝓞 K)`. -/
 
 open IsDedekindDomain HeightOneSpectrum WithZeroMulInt NumberField NNReal
@@ -57,11 +57,8 @@ lemma exists_num_denom_adicAbv_eq_one {α : K} {v : HeightOneSpectrum (𝓞 K)}
     {b : ℝ≥0} (hb : 1 < b) (h_abs : v.adicAbv hb α ≤ 1) :
   ∃ x y : 𝓞 K, α = x / y ∧ v.adicAbv hb (y : K) = 1 := by
   let S : Set (HeightOneSpectrum (𝓞 K)) := {v}ᶜ
-  have mem : α ∈ S.integer K := by
-    intro _ hw
-    simp_all [S, (toNNReal_le_one_iff hb).mp h_abs]
-  letI : Fact (IsMulTorsion (ClassGroup (𝓞 K))) := fact_iff.mpr isMulTorsion_of_finite
-  let γ : S.integer K := ⟨α, mem⟩
+  let γ : S.integer K := ⟨α, fun _ _ ↦ by simp_all [S, (toNNReal_le_one_iff hb).mp h_abs]⟩
+  let : Fact (IsMulTorsion (ClassGroup (𝓞 K))) := fact_iff.mpr isMulTorsion_of_finite
   obtain ⟨⟨x, ⟨y, hy_away, hy_nzd⟩⟩, h⟩ := IsLocalization.surj S.submonoid γ
   refine ⟨x, y, ?_, by simpa [adicAbv_coe_eq_one_iff, S] using hy_away⟩
   rw [eq_div_iff <| IsFractionRing.to_map_ne_zero_of_mem_nonZeroDivisors hy_nzd]
@@ -90,7 +87,7 @@ lemma RingOfIntegers.absoluteValue_le_one (x : 𝓞 K) : f x ≤ 1 := by
   have ht : Filter.atTop.Tendsto (fun k : ℕ ↦ C ^ (1 / (k : ℝ))) (nhds 1) := by
     simpa using tendsto_const_nhds.rpow (tendsto_one_div_atTop_nhds_zero_nat)
       (Or.inl <| ne_of_gt <| lt_of_lt_of_le zero_lt_one (by simpa using hC 1))
-  exact ge_of_tendsto ht <| Filter.eventually_atTop.2 ⟨1, fun k hk ↦ hx_root (ne_of_gt hk)⟩
+  exact ge_of_tendsto ht <| Filter.eventually_atTop.2 ⟨1, fun _ hk ↦ hx_root (ne_of_gt hk)⟩
 
 include hf_nonarch hf_nontriv in
 /-- The open unit ball in `𝓞 K` is a non-zero prime ideal of `𝓞 K`. -/
@@ -102,7 +99,7 @@ def maximalIdeal : HeightOneSpectrum (𝓞 K) where
     smul_mem' := by
       simpa [Set.mem_ofPred_eq] using
         (fun (c x : 𝓞 K) hx ↦ mul_lt_one_of_nonneg_of_lt_one_right
-            (RingOfIntegers.absoluteValue_le_one f hf_nonarch c) (apply_nonneg f ↑x) hx)
+        (RingOfIntegers.absoluteValue_le_one f hf_nonarch c) (apply_nonneg f ↑x) hx)
   }
   isPrime := by
       rw [Ideal.isPrime_iff]
@@ -111,7 +108,7 @@ def maximalIdeal : HeightOneSpectrum (𝓞 K) where
         change ¬f 1 < 1
         simp
       · change ∀ x y : 𝓞 K, f (x * y) < 1 → f x < 1 ∨ f y < 1
-        intro x y hxy
+        intro _ _ hxy
         rw [map_mul] at hxy
         by_contra! h
         linarith [one_le_mul_of_one_le_of_one_le h.1 h.2]
@@ -133,8 +130,8 @@ theorem exists_heightOneSpectrum_eq_adicAbv (hf_nontriv : f.IsNontrivial) :
     ∃! P : HeightOneSpectrum (𝓞 K), ∃ b, ∃ hb : 1 < b, f = adicAbv P hb := by
   let P := maximalIdeal f hf_nonarch hf_nontriv
   use P
-  rcases intValuation_exists_uniformizer P with ⟨π, hπ⟩
-  have hπv_ne : P.valuation K (π : K) ≠ 0 := by simp [valuation_of_algebraMap, hπ]
+  rcases P.intValuation_exists_uniformizer with ⟨π, hπ⟩
+  have hπv_ne : P.valuation K π ≠ 0 := by simp [valuation_of_algebraMap, hπ]
   have hπ_pos : 0 < f π := by grind [AbsoluteValue.pos_iff]
   let b : ℝ≥0 := ⟨(f π)⁻¹, by positivity⟩
   have hb : 1 < b := by
@@ -143,16 +140,16 @@ theorem exists_heightOneSpectrum_eq_adicAbv (hf_nontriv : f.IsNontrivial) :
     simp [← intValuation_lt_one_iff_mem, hπ, ← WithZero.exp_zero, -WithZero.exp_neg]
   have f_eq_one_of_adicAbv_eq_one {x : K} (hx : P.adicAbv hb x = 1) : f x = 1 := by
     obtain ⟨y, z, rfl, hz⟩ := exists_num_denom_adicAbv_eq_one hb (le_of_eq hx)
-    have int {x : 𝓞 K} (hx : P.adicAbv hb (x : K) = 1) : f x = 1 := by
+    have h {x : 𝓞 K} (hx : P.adicAbv hb (x : K) = 1) : f x = 1 := by
       rw [adicAbv_coe_eq_one_iff] at hx
       exact le_antisymm (RingOfIntegers.absoluteValue_le_one f hf_nonarch x) (le_of_not_gt hx)
     have hy : P.adicAbv hb (y : K) = 1 := by simpa [map_div₀, hz] using hx
-    simp [map_div₀, int hy, int hz]
+    simp [map_div₀, h hy, h hz]
   constructor
   · use b, hb
     ext x
-    by_cases hx : x = 0
-    · simp [hx]
+    rcases eq_or_ne x 0 with (rfl | hx)
+    · simp
     have hxv_ne : P.valuation K x ≠ 0 := (P.valuation K).ne_zero_iff.mpr hx
     have coe_b : (b : ℝ) = (f π)⁻¹ := rfl
     simp only [IsDedekindDomain.HeightOneSpectrum.adicAbv, adicAbvDef, AbsoluteValue.coe_mk,
@@ -161,7 +158,7 @@ theorem exists_heightOneSpectrum_eq_adicAbv (hf_nontriv : f.IsNontrivial) :
     rw [← map_zpow₀, ← map_mul]
     apply f_eq_one_of_adicAbv_eq_one
     have : (WithZero.unzero hπv_ne).toAdd = -1 := by
-      simp [valuation_of_algebraMap, hπ, WithZero.toAdd_unzero]
+      simp [valuation_of_algebraMap, hπ, WithZero.toAdd_unzero_eq_log]
     simp [IsDedekindDomain.HeightOneSpectrum.adicAbv, adicAbvDef, coe_b,
       WithZeroMulInt.toNNReal_neg_apply _ hπv_ne, WithZeroMulInt.toNNReal_neg_apply _ hxv_ne, this,
       zpow_ne_zero _ hπ_pos.ne']
