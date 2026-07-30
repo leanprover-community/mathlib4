b9965759442d6268b2446ad74551b7e6b1ef14b3
vihdzp <vi.hdz.p@gmail.com>
2026-07-30 16:56:01 -0600

    fix lint

--- Mathlib/Data/ENat/Basic.lean
+++ Mathlib/Data/ENat/Basic.lean
@@ -257,12 +257,14 @@ protected theorem add_lt_add_of_le_of_lt : a ≠ ⊤ → a ≤ b → c < d → a
 protected theorem add_lt_add_of_lt_of_le : c ≠ ⊤ → a < b → c ≤ d → a + c < b + d :=
   WithTop.add_lt_add_of_lt_of_le
 
-@[simp, norm_cast]
+-- Duplicate of `Nat.cast_lt`, but requires less imports.
+@[simp 900, norm_cast]
 lemma natCast_lt_natCast {n m : ℕ} : (n : ℕ∞) < (m : ℕ∞) ↔ n < m := WithTop.coe_lt_coe
 
 @[deprecated (since := "2026-07-17")] alias coe_lt_coe := natCast_lt_natCast
 
-@[simp, norm_cast]
+-- Duplicate of `Nat.cast_le`, but requires less imports.
+@[simp 900, norm_cast]
 lemma natCast_le_natCast {n m : ℕ} : (n : ℕ∞) ≤ (m : ℕ∞) ↔ n ≤ m := WithTop.coe_le_coe
 
 @[deprecated (since := "2026-07-17")] alias coe_le_coe := natCast_le_natCast
