c854586e5eeaca3b359ca37b9908077ff6edc827
Yongxi Lin <aaronlin@andrew.cmu.edu>
2026-07-29 19:47:51 -0700

    docs(Topology): note GO-space generalization

--- Mathlib/Topology/Order/Separable.lean
+++ Mathlib/Topology/Order/Separable.lean
@@ -21,6 +21,8 @@ In this file we prove some results about a separable linearly ordered topologica
   points of a subset that are isolated in the subspace topology form a countable set.
 * `Set.separableSpace`: a separable linearly ordered topological space is hereditarily separable.
 
+TODO: Generalize the hereditary separability result to separable GO-spaces.
+
 -/
 
 public section
