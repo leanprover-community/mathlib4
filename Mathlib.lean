39801a187be338ec848691bb9f929346e80f65a2
Junye Ji <jijunye1@outlook.com>
2026-07-30 10:28:40 -0700

    Regenerate Mathlib imports

--- Mathlib.lean
+++ Mathlib.lean
@@ -5135,9 +5135,9 @@ public import Mathlib.LinearAlgebra.Matrix.Diagonal
 public import Mathlib.LinearAlgebra.Matrix.DotProduct
 public import Mathlib.LinearAlgebra.Matrix.Dual
 public import Mathlib.LinearAlgebra.Matrix.DualNumber
-public import Mathlib.LinearAlgebra.Matrix.Elementary
 public import Mathlib.LinearAlgebra.Matrix.Echelon.Basic
 public import Mathlib.LinearAlgebra.Matrix.Echelon.Pivot
+public import Mathlib.LinearAlgebra.Matrix.Elementary
 public import Mathlib.LinearAlgebra.Matrix.FiniteDimensional
 public import Mathlib.LinearAlgebra.Matrix.FixedDetMatrices
 public import Mathlib.LinearAlgebra.Matrix.GeneralLinearGroup.Basic
