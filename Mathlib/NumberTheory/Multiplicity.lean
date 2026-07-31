6e9068f6481537fd2e2f0d09f23954767edf623c
Xavier Roblot <46200072+xroblot@users.noreply.github.com>
2026-07-31 15:38:42 +0900

    relocate Int square-mod-four lemmas to Ring/Int/Parity

--- Mathlib/NumberTheory/Multiplicity.lean
+++ Mathlib/NumberTheory/Multiplicity.lean
@@ -247,14 +247,6 @@ theorem pow_two_pow_sub_pow_two_pow [CommRing R] {x y : R} (n : ℕ) :
     rw [Nat.succ_eq_add_one]
     ring
 
-theorem Int.sq_mod_four_eq_one_of_odd {x : ℤ} : Odd x → x ^ 2 % 4 = 1 := by
-  intro hx
-  unfold Odd at hx
-  rcases hx with ⟨_, rfl⟩
-  ring_nf
-  rw [add_assoc, ← add_mul, Int.add_mul_emod_self_right]
-  decide
-
 lemma Int.eight_dvd_sq_sub_one_of_odd {k : ℤ} (hk : Odd k) : 8 ∣ k ^ 2 - 1 := by
   rcases hk with ⟨m, rfl⟩
   have eq : (2 * m + 1) ^ 2 - 1 = 4 * (m * (m + 1)) := by ring
