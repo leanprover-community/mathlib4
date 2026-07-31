a60f6b1c453e4b660d2b446a420d64f5a8e96545
Felix Pernegger <s59fpern@uni-bonn.de>
2026-07-31 03:08:32 +0200

    Apply suggestion from @felixpernegger

--- Mathlib/Topology/SmallInductiveDimension.lean
+++ Mathlib/Topology/SmallInductiveDimension.lean
@@ -155,6 +155,7 @@ theorem smallInductiveDimension_eq (n : ℕ)
 theorem smallInductiveDimension_eq_bot : smallInductiveDimension X = ⊥ ↔ IsEmpty X := by
   simp_rw [← hasSmallInductiveDimensionLT_zero_iff, ← smallInductiveDimension_lt_iff,
     WithBot.lt_coe_bot.symm, bot_eq_zero', Nat.cast_zero, WithBot.coe_zero]
+
 variable (X) in
 @[simp]
 theorem smallInductiveDimension_of_isEmpty [IsEmpty X] : smallInductiveDimension X = ⊥ :=
