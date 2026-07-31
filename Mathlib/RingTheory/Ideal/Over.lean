3a228035b874c2a99e6ee0f11004dd0797c00b9d
tb65536 <thomas.l.browning@gmail.com>
2026-07-31 17:16:01 +0100

    review

--- Mathlib/RingTheory/Ideal/Over.lean
+++ Mathlib/RingTheory/Ideal/Over.lean
@@ -389,9 +389,9 @@ abbrev primesOver.mk (P : Ideal B) [hPp : P.IsPrime] [hp : P.LiesOver p] : prime
   ⟨P, ⟨hPp, hp⟩⟩
 
 variable {p} in
-theorem ne_bot_of_mem_primesOver {S : Type*} [Semiring S] [Algebra R S]
-    [FaithfulSMul R S] {p : Ideal R} (hp : p ≠ ⊥) {P : Ideal S} (hP : P ∈ p.primesOver S) :
-    P ≠ ⊥ := by have : P.LiesOver p := hP.2; exact ne_bot_of_liesOver_of_ne_bot hp P
+theorem ne_bot_of_mem_primesOver [FaithfulSMul A B] (hp : p ≠ ⊥) {P : Ideal B}
+    (hP : P ∈ p.primesOver B) : P ≠ ⊥ := by
+  have : P.LiesOver p := hP.2; exact ne_bot_of_liesOver_of_ne_bot hp P
 
 end primesOver
 
