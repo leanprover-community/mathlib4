012c7fb29fc98b98b62eab5128b106bd89fe8526
Jason Hickey <jason@karyk.com>
2026-07-31 08:01:13 -0700

    Name the module Counterexamples.JacobianConjecture

--- Counterexamples/JacobianConjecture.lean
+++ Counterexamples/JacobianConjecture.lean
@@ -3,7 +3,7 @@ Copyright (c) 2026 Jason Hickey. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jason Hickey
 -/
-module
+module Counterexamples.JacobianConjecture
 
 public import Mathlib.Algebra.Algebra.Rat
 public import Mathlib.Algebra.MvPolynomial.Monad
