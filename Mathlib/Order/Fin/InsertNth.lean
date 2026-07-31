dade58ffd5fa2fb7c0e254cb92d1c2bd2995e692
Joël Riou <joel.riou@universite-paris-saclay.fr>
2026-07-31 10:37:50 +0530

    fix

--- Mathlib/Order/Fin/InsertNth.lean
+++ Mathlib/Order/Fin/InsertNth.lean
@@ -36,6 +36,7 @@ lemma strictMono_insertNth_zero
   · simpa
   · simpa using hf i
 
+set_option backward.isDefEq.respectTransparency false in
 lemma insertNth_monotone
     {f : Fin (n + 1) → α} (hf : Monotone f) (i : Fin n) (x : α)
     (hx₁ : f i.castSucc ≤ x) (hx₂ : x ≤ f i.succ) :
@@ -57,6 +58,7 @@ lemma insertNth_monotone
         insertNth_apply_above (by simpa using hj.le)]
       simpa using hf j.castSucc_le_succ
 
+set_option backward.isDefEq.respectTransparency false in
 lemma strictMono_insertNth
     {f : Fin (n + 1) → α} (hf : StrictMono f) (i : Fin n) (x : α)
     (hx₁ : f i.castSucc < x) (hx₂ : x < f i.succ) :
