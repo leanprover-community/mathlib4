bad26521ed9998a8f60684ebc6ad8a4aca9cba83
Aaron Liu <aaronliu2008@outlook.com>
2026-08-01 22:47:39 -0400

    unindent

--- Mathlib/RingTheory/Unramified/Dedekind.lean
+++ Mathlib/RingTheory/Unramified/Dedekind.lean
@@ -23,29 +23,28 @@ variable (A B : Type*) [CommRing A] [CommRing B] [Algebra A B] [Module.Finite A 
 include A in
 theorem IsDedekindDomain.of_formallyUnramified : IsDedekindDomain B :=
   have noetherian := IsNoetherianRing.of_finite A B
-  isDedekindDomain_iff_isDiscreteValuationRing_atPrime.2
-    ⟨noetherian, by
-      intro q hq hqp
-      let q' := IsLocalRing.maximalIdeal (Localization.AtPrime q)
-      suffices q'.IsPrincipal from ((IsDiscreteValuationRing.TFAE (Localization.AtPrime q)
-        (IsLocalization.AtPrime.not_isField B hq (Localization.AtPrime q))).out 4 0).mp this
-      let p := q.under A
-      let := Localization.AtPrime.algebraOfLiesOver p q
-      have : p.IsMaximal := (hqp.under A).isMaximal (q.under_ne_bot A hq)
-      let : Field (A ⧸ p) := Ideal.Quotient.field p
-      have := IsArtinianRing.of_finite (A ⧸ p) (B ⧸ p.map (algebraMap A B))
-      suffices q' = (p.map (algebraMap A B)).map (algebraMap B (Localization.AtPrime q)) by
-        rw [this, Ideal.map_map, ← IsScalarTower.algebraMap_eq,
-          IsScalarTower.algebraMap_eq A (Localization.AtPrime p) (Localization.AtPrime q),
-          ← Ideal.map_map]
-        infer_instance
-      rw [← (Algebra.FormallyUnramified.isRadical_map_isMaximal A B p).radical,
-        IsLocalization.map_radical q.primeCompl,
-        IsLocalization.AtPrime.radical_map_of_mem_minimalPrimes (Localization.AtPrime q) q,
-        Localization.AtPrime.map_eq_maximalIdeal]
-      rw [Ideal.minimalPrimes_eq_comap]
-      exact ⟨q.map (Ideal.Quotient.mk (p.map (algebraMap A B))),
-        IsArtinianRing.mem_minimalPrimes bot_le, Ideal.comap_map_mk Ideal.map_comap_le⟩⟩
+  isDedekindDomain_iff_isDiscreteValuationRing_atPrime.2 ⟨noetherian, by
+    intro q hq hqp
+    let q' := IsLocalRing.maximalIdeal (Localization.AtPrime q)
+    suffices q'.IsPrincipal from ((IsDiscreteValuationRing.TFAE (Localization.AtPrime q)
+      (IsLocalization.AtPrime.not_isField B hq (Localization.AtPrime q))).out 4 0).mp this
+    let p := q.under A
+    let := Localization.AtPrime.algebraOfLiesOver p q
+    have : p.IsMaximal := (hqp.under A).isMaximal (q.under_ne_bot A hq)
+    let : Field (A ⧸ p) := Ideal.Quotient.field p
+    have := IsArtinianRing.of_finite (A ⧸ p) (B ⧸ p.map (algebraMap A B))
+    suffices q' = (p.map (algebraMap A B)).map (algebraMap B (Localization.AtPrime q)) by
+      rw [this, Ideal.map_map, ← IsScalarTower.algebraMap_eq,
+        IsScalarTower.algebraMap_eq A (Localization.AtPrime p) (Localization.AtPrime q),
+        ← Ideal.map_map]
+      infer_instance
+    rw [← (Algebra.FormallyUnramified.isRadical_map_isMaximal A B p).radical,
+      IsLocalization.map_radical q.primeCompl,
+      IsLocalization.AtPrime.radical_map_of_mem_minimalPrimes (Localization.AtPrime q) q,
+      Localization.AtPrime.map_eq_maximalIdeal]
+    rw [Ideal.minimalPrimes_eq_comap]
+    exact ⟨q.map (Ideal.Quotient.mk (p.map (algebraMap A B))),
+      IsArtinianRing.mem_minimalPrimes bot_le, Ideal.comap_map_mk Ideal.map_comap_le⟩⟩
 
 @[deprecated (since := "2026-08-01")]
 alias isDedekindDomainDvr.of_forallyUnramified := IsDedekindDomain.of_formallyUnramified
