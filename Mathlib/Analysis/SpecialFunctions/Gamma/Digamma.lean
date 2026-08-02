3257213b522580917671a035f7b6000e8335f373
Terence Tao <tao@math.ucla.edu>
2026-08-02 07:06:05 -0700

    one final golf

--- Mathlib/Analysis/SpecialFunctions/Gamma/Digamma.lean
+++ Mathlib/Analysis/SpecialFunctions/Gamma/Digamma.lean
@@ -68,8 +68,8 @@ theorem digamma_one_sub {s : ℂ} (hs : ∀ n : ℤ, s ≠ n) :
   have (m : ℕ) : s ≠ -m := by simpa using hs (-m)
   have (m : ℕ) : 1 - s ≠ -m := fun _ ↦ hs (1 + m) (by push_cast; grind)
   have := congrArg (logDeriv · s) (funext Gamma_mul_Gamma_one_sub)
-  rw [logDeriv_mul, logDeriv_div, (by rfl : (Gamma <| 1 - ·) = Gamma ∘ (1 - ·)),
-    ← Function.comp_def sin, logDeriv_comp, logDeriv_comp] at this
+  rw [logDeriv_mul, logDeriv_div, ← Function.comp_def Gamma, ← Function.comp_def sin,
+    logDeriv_comp, logDeriv_comp] at this
     <;> try first | fun_prop | grind [sin_eq_zero_iff, ofReal_ne_zero, pi_ne_zero, Gamma_ne_zero]
   simp [digamma_def] at this ⊢
   grind
