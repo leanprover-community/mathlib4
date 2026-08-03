395a4efafa26660e593f9fbb470b63ef46419073
tb65536 <thomas.l.browning@gmail.com>
2026-08-03 21:47:00 +0100

    Remove

--- Mathlib/NumberTheory/Padics/MahlerBasis.lean
+++ Mathlib/NumberTheory/Padics/MahlerBasis.lean
@@ -70,9 +70,6 @@ lemma norm_ascPochhammer_le (k : ℕ) (x : ℤ_[p]) :
     ← Ring.factorial_nsmul_multichoose_eq_ascPochhammer, smul_eq_mul, Nat.cast_mul, norm_mul]
   exact mul_le_of_le_one_right (norm_nonneg _) (norm_le_one _)
 
-instance : IsAddTorsionFree ℤ_[p] := by
-  infer_instance
-
 set_option backward.isDefEq.respectTransparency false in
 /-- The p-adic integers are a binomial ring, i.e. a ring where binomial coefficients make sense. -/
 noncomputable instance instBinomialRing : BinomialRing ℤ_[p] where
