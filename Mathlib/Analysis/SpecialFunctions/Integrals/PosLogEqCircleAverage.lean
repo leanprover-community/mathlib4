0db3f3c072749a18215862124c54ff6515ac7766
yuanyi-350 <kysyy1@126.com>
2026-08-04 19:14:59 +0800

    fix ci

--- Mathlib/Analysis/SpecialFunctions/Integrals/PosLogEqCircleAverage.lean
+++ Mathlib/Analysis/SpecialFunctions/Integrals/PosLogEqCircleAverage.lean
@@ -20,7 +20,7 @@ the circle average of `log ‖· - a‖` over the unit circle.
 
 public section
 
-open Filter Interval intervalIntegral MeasureTheory Metric Real InnerProductSpace
+open Filter Interval intervalIntegral MeasureTheory Metric Real
 
 variable {a c : ℂ} {R : ℝ}
 
@@ -55,7 +55,7 @@ theorem circleAverage_log_norm_sub_const₀ (h : ‖a‖ < 1) : circleAverage (l
     _ = ‖1 - z⁻¹ * a‖ := by field_simp
   _ = 0 := by
     rw [circleAverage_zero_one_congr_inv (f := fun x ↦ log ‖1 - x * a‖),
-      HarmonicOnNhd.circleAverage_eq, zero_mul, sub_zero,
+      InnerProductSpace.HarmonicOnNhd.circleAverage_eq, zero_mul, sub_zero,
       CStarRing.norm_of_mem_unitary (unitary ℂ).one_mem, log_one]
     intro x hx
     have : ‖x * a‖ < 1 := by
@@ -159,7 +159,7 @@ If `a : ℂ` has norm greater than one, then `circleAverage (log ‖· - a‖) 0
 @[simp]
 theorem circleAverage_log_norm_sub_const₂ (h : 1 < ‖a‖) :
     circleAverage (log ‖· - a‖) 0 1 = log ‖a‖ := by
-  rw [HarmonicOnNhd.circleAverage_eq, zero_sub, norm_neg]
+  rw [InnerProductSpace.HarmonicOnNhd.circleAverage_eq, zero_sub, norm_neg]
   intro x hx
   apply AnalyticAt.harmonicAt_log_norm (by fun_prop)
   rw [sub_ne_zero]
