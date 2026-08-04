2805bc128c5926e5b2f61e560f88b2a13045bbd5
Gaëtan Serré <56162277+gaetanserre@users.noreply.github.com>
2026-08-04 17:09:10 +0200

    Update Mathlib/Probability/Kernel/Deterministic.lean
    Co-authored-by: Yongxi (Aaron) Lin <97214596+CoolRmal@users.noreply.github.com>

--- Mathlib/Probability/Kernel/Deterministic.lean
+++ Mathlib/Probability/Kernel/Deterministic.lean
@@ -185,14 +185,9 @@ instance (κ : Kernel α β) [IsDeterministic κ] : IsSFiniteKernel κ := by
     by_contra! h
     let : IsFiniteKernel κ := ⟨⟨0, by simp, h⟩⟩
     exact hκ inferInstance
-  have h := DFunLike.congr_fun κ.parallelComp_self_comp_copy a
-  simp_all only [not_false_eq_true, parallelComp_of_not_isSFiniteKernel_left, zero_comp, zero_apply]
-  replace h := DFunLike.congr_fun h Set.univ
-  rw [comp_apply'] at h
-  · simp_rw [copy_apply, Measure.dirac_apply' _ MeasurableSet.univ, indicator_univ] at h
-    simp only [Measure.coe_zero, Pi.zero_apply, Pi.one_apply, MeasureTheory.lintegral_const,
-      one_mul] at h
-    exact ha.ne h
-  exact MeasurableSet.univ
+  have h := DFunLike.congr_fun (DFunLike.congr_fun κ.parallelComp_self_comp_copy a) (univ ×ˢ univ)
+  simp only [parallelComp_of_not_isSFiniteKernel_left κ hκ, zero_comp, zero_apply,
+    copy_comp_apply_prod κ a .univ .univ, inter_self] at h
+  exact ha.ne h
 
 end ProbabilityTheory.Kernel
