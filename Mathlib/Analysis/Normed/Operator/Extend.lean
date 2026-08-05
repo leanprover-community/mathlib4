1e737752f39addd1584e11b2dbedd38ee6c9be99
TJHeeringa <heeringa@protonmail.ch>
2026-08-05 11:54:28 +0200

    Update docstring

--- Mathlib/Analysis/Normed/Operator/Extend.lean
+++ Mathlib/Analysis/Normed/Operator/Extend.lean
@@ -23,8 +23,6 @@ subspace to the entire Banach space.
 * `LinearMap.extendOfIsometry`: Extend a linear map `f : E →ₛₗ[𝕜] F` between normed spaces to a
   linear isometry `Eₗ →ₗᵢ[𝕜] Fₗ` between Banach spaces with a dense map `e₁ : E →ₗ[𝕜] Eₗ`, a linear
   map `e₂ : F →ₗ[𝕜] Fₗ` and the corresponding norm estimate.
-
-Moreover, we can extend a linear equivalence:
 * `LinearEquiv.extend`: Extend a linear equivalence between normed spaces to a continuous linear
   equivalence between Banach spaces with two dense maps `e₁` and `e₂` and the corresponding norm
   estimates.
