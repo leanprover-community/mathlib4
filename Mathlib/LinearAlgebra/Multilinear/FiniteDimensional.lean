3c630666482e2f9115125a248abc08142a921cd4
gloges <gerg1995@gmail.com>
2026-08-07 20:18:23 +0900

    finitedim to semilinear

--- Mathlib/LinearAlgebra/Multilinear/FiniteDimensional.lean
+++ Mathlib/LinearAlgebra/Multilinear/FiniteDimensional.lean
@@ -32,21 +32,22 @@ variable [Module.Finite R M₂] [Module.Free R M₂]
 
 private theorem free_and_finite_fin (n : ℕ) (N : Fin n → Type*) [∀ i, AddCommGroup (N i)]
     [∀ i, Module R (N i)] [∀ i, Module.Finite R (N i)] [∀ i, Module.Free R (N i)] :
-    Module.Free R (MultilinearMap R N M₂) ∧ Module.Finite R (MultilinearMap R N M₂) := by
+    Module.Free R (MultilinearMap (.id R) N M₂) ∧
+      Module.Finite R (MultilinearMap (.id R) N M₂) := by
   induction n with
   | zero =>
     have : IsEmpty (Fin Nat.zero) := inferInstanceAs (IsEmpty (Fin 0))
     exact
-      ⟨Module.Free.of_equiv (constLinearEquivOfIsEmpty R R N M₂),
-        Module.Finite.equiv (constLinearEquivOfIsEmpty R R N M₂)⟩
+      ⟨Module.Free.of_equiv (constLinearEquivOfIsEmpty R N M₂ _),
+        Module.Finite.equiv (constLinearEquivOfIsEmpty R N M₂ _)⟩
   | succ n ih =>
     suffices
-      Module.Free R (N 0 →ₗ[R] MultilinearMap R (fun i : Fin n => N i.succ) M₂) ∧
-        Module.Finite R (N 0 →ₗ[R] MultilinearMap R (fun i : Fin n => N i.succ) M₂) by
+      Module.Free R (N 0 →ₗ[R] MultilinearMap (.id R) (fun i : Fin n => N i.succ) M₂) ∧
+        Module.Finite R (N 0 →ₗ[R] MultilinearMap (.id R) (fun i : Fin n => N i.succ) M₂) by
       cases this
       exact
-        ⟨Module.Free.of_equiv (multilinearCurryLeftEquiv R N M₂).symm,
-          Module.Finite.equiv (multilinearCurryLeftEquiv R N M₂).symm⟩
+        ⟨Module.Free.of_equiv (multilinearCurryLeftEquiv N M₂ _).symm,
+          Module.Finite.equiv (multilinearCurryLeftEquiv N M₂ _).symm⟩
     cases ih fun i => N i.succ
     exact ⟨Module.Free.linearMap _ _ _ _, Module.Finite.linearMap _ _ _ _⟩
 
@@ -55,18 +56,19 @@ variable [∀ i, Module.Finite R (M₁ i)] [∀ i, Module.Free R (M₁ i)]
 
 -- the induction requires us to show both at once
 private theorem free_and_finite :
-    Module.Free R (MultilinearMap R M₁ M₂) ∧ Module.Finite R (MultilinearMap R M₁ M₂) := by
+    Module.Free R (MultilinearMap (.id R) M₁ M₂) ∧
+      Module.Finite R (MultilinearMap (.id R) M₁ M₂) := by
   cases nonempty_fintype ι
   have := @free_and_finite_fin R M₂ _ _ _ _ _ (Fintype.card ι)
     (fun x => M₁ ((Fintype.equivFin ι).symm x))
   obtain ⟨l, r⟩ := this
-  have e := domDomCongrLinearEquiv' R R M₁ M₂ (Fintype.equivFin ι)
+  have e := domDomCongrLinearEquiv' R M₁ M₂ (.id R) (Fintype.equivFin ι)
   exact ⟨Module.Free.of_equiv e.symm, Module.Finite.equiv e.symm⟩
 
-instance _root_.Module.Finite.multilinearMap : Module.Finite R (MultilinearMap R M₁ M₂) :=
+instance _root_.Module.Finite.multilinearMap : Module.Finite R (MultilinearMap (.id R) M₁ M₂) :=
   free_and_finite.2
 
-instance _root_.Module.Free.multilinearMap : Module.Free R (MultilinearMap R M₁ M₂) :=
+instance _root_.Module.Free.multilinearMap : Module.Free R (MultilinearMap (.id R) M₁ M₂) :=
   free_and_finite.1
 
 end MultilinearMap
