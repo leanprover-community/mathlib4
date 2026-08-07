d1dde30f350135282f715397cdcef9e970322e64
Jireh Loreaux <loreaujy@gmail.com>
2026-08-07 12:43:41 -0500

    refactor(Analysis/CStarAlgebra/ContinuousFunctionalCalculus/Order): clean up hypotheses

--- Mathlib/Analysis/CStarAlgebra/Module/Constructions.lean
+++ Mathlib/Analysis/CStarAlgebra/Module/Constructions.lean
@@ -272,7 +272,7 @@ lemma norm_apply_le_norm (x : C⋆ᵐᵒᵈ(A, Π i, E i)) (i : ι) : ‖x i‖ 
   let _ : NormedAddCommGroup C⋆ᵐᵒᵈ(A, Π i, E i) := normedAddCommGroup A
   refine abs_le_of_sq_le_sq' ?_ (by positivity) |>.2
   rw [pi_norm_sq, norm_sq_eq A]
-  refine CStarAlgebra.norm_le_norm_of_nonneg_of_le inner_self_nonneg ?_
+  refine CStarAlgebra.norm_le_norm_of_nonneg_of_le ?_ inner_self_nonneg
   exact Finset.single_le_sum (fun j _ ↦ inner_self_nonneg (A := A) (x := x j)) (Finset.mem_univ i)
 
 open Finset in
