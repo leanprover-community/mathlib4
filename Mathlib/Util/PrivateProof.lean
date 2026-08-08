31af55bd3d1bf296e7c181054d84aa36e3be982f
thorimur <68410468+thorimur@users.noreply.github.com>
2026-08-08 14:19:18 -0400

    chore: tweak error message

--- Mathlib/Util/PrivateProof.lean
+++ Mathlib/Util/PrivateProof.lean
@@ -60,21 +60,24 @@ elab_rules : term <= ty
     -- If definitely not a prop, log an error and proceed. We log errors at the (ambient) term
     -- instead of the `private` token, since the term is "at fault".
     if ← notM (isProp ty) <&&> return !(← instantiateMVars <|← inferType ty).hasMVar then
-      logError m!"`private` can only wrap proofs; the expected type is not a `Prop`.\
-        {indentD ty} : {← inferType ty}"
+      logError m!"`private` can only wrap proofs, but \
+        the expected type is not a `Prop`.\
+        {indentD ty} : {← inferType ty}\n\n\
+        Use `private_decl%` to wrap a non-proof term in an auxiliary definition."
       Term.elabTerm t ty (implicitLambda := false)
     else
       let e ← instantiateMVars <|← withoutExporting do withSynthesize do
         Term.elabTermEnsuringType t ty (implicitLambda := false)
       if !(← isProp ty) then
         let knownToBe? := if (← instantiateMVars ty).hasMVar then " known to be" else ""
-        logError m!"`private` can only wrap proofs; \
+        logError m!"`private` can only wrap proofs, but \
           the expected type of `{e}` is not{knownToBe?} a `Prop`.\
-          {indentD ty} : {← inferType ty}"
+          {indentD ty} : {← inferType ty}\n\n\
+          Use `private_decl%` to wrap a non-proof term in an auxiliary definition."
         return e
       else if e.isFVar then
         logWarningAt tk m!"`private` is unnecessary, since the resulting expression is just a free \
-          variable:{indentD e}"
+          variable:{indentD e} : {ty}"
         return e
       else
         mkAuxTheorem ty e (zetaDelta := true) (cache := !e.hasSorry)
