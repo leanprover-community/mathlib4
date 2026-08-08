df25f40d2c594c285d6b5b9c5f6fb34487700ef9
Snir Broshi <26556598+SnirBroshi@users.noreply.github.com>
2026-08-08 08:29:18 +0300

    `PartialOrder` version

--- Mathlib/Order/Interval/Set/Disjoint.lean
+++ Mathlib/Order/Interval/Set/Disjoint.lean
@@ -185,6 +185,20 @@ theorem IsGLB.biUnion_Ici_eq_Ici (a_glb : IsGLB s a) (a_mem : a ∈ s) : ⋃ x �
 
 end Preorder
 
+section PartialOrder
+
+variable [PartialOrder α] {s : Set α} {a : α}
+
+@[to_dual]
+theorem biUnion_Ici_eq_Ici_iff : ⋃ x ∈ s, Ici x = Ici a ↔ IsLeast s a := by
+  refine ⟨fun h ↦ ?_, IsLeast.biUnion_Ici_eq_Ici⟩
+  have hleast := h ▸ isLeast_Ici
+  have hlb : a ∈ lowerBounds s := fun b hbs ↦ hleast.right <| mem_iUnion₂_of_mem hbs self_mem_Ici
+  have ⟨b, hbs, hba⟩ := Set.mem_iUnion₂.mp hleast.left
+  exact ⟨hba.antisymm (hlb hbs) ▸ hbs, hlb⟩
+
+end PartialOrder
+
 section LinearOrder
 
 variable [LinearOrder α] {s : Set α} {a : α} {f : ι → α}
