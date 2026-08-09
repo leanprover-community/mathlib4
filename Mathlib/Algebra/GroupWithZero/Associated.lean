0caa4dc2189ca152973809424e791f833183eb93
Bingyu Xia <71547343+BryceT233@users.noreply.github.com>
2026-08-09 08:24:47 +0800

    apply suggestions

--- Mathlib/Algebra/GroupWithZero/Associated.lean
+++ Mathlib/Algebra/GroupWithZero/Associated.lean
@@ -233,21 +233,19 @@ protected theorem Associated.prime [CommMonoidWithZero M] {p q : M} (h : p ~ᵤ 
       intro a b
       exact hp.dvd_or_dvd⟩⟩
 
-lemma Associated.isRelPrime_left [CommMonoid M] {a b c : M} (assoc : Associated a b)
-    (h : IsRelPrime a c) : IsRelPrime b c := by
-  rcases assoc with ⟨u, hu⟩
-  rwa [← hu, isRelPrime_mul_unit_right_left u.isUnit]
+lemma Associated.isRelPrime_left [Monoid M] {a b c : M} (assoc : Associated a b)
+    (h : IsRelPrime a c) : IsRelPrime b c :=
+  fun _ hb hc ↦ h (assoc.dvd_iff_dvd_right.mpr hb) hc
 
-lemma Associated.isRelPrime_iff_left [CommMonoid M] {a b c : M} (assoc : Associated a b) :
+lemma Associated.isRelPrime_iff_left [Monoid M] {a b c : M} (assoc : Associated a b) :
     IsRelPrime a c ↔ IsRelPrime b c :=
   ⟨fun h ↦ isRelPrime_left assoc h, fun h ↦ isRelPrime_left assoc.symm h⟩
 
-lemma Associated.isRelPrime_right [CommMonoid M] {a b c : M} (assoc : Associated a b)
-    (h : IsRelPrime c a) : IsRelPrime c b:= by
-  rcases assoc with ⟨u, hu⟩
-  rwa [← hu, isRelPrime_mul_unit_right_right u.isUnit]
+lemma Associated.isRelPrime_right [Monoid M] {a b c : M} (assoc : Associated a b)
+    (h : IsRelPrime c a) : IsRelPrime c b:=
+  fun _ hc hb ↦ h hc (assoc.dvd_iff_dvd_right.mpr hb)
 
-lemma Associated.isRelPrime_iff_right [CommMonoid M] {a b c : M} (assoc : Associated a b) :
+lemma Associated.isRelPrime_iff_right [Monoid M] {a b c : M} (assoc : Associated a b) :
     IsRelPrime c a ↔ IsRelPrime c b :=
   ⟨fun h ↦ isRelPrime_right assoc h, fun h ↦ isRelPrime_right assoc.symm h⟩
 
