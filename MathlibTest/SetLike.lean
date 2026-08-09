fb18b1b97dc2968bf13c2f563cdd3a7d2becb7f1
artie2000 <artem.khovanov@gmail.com>
2026-08-09 22:22:20 +0100

    document

--- MathlibTest/SetLike.lean
+++ MathlibTest/SetLike.lean
@@ -55,7 +55,7 @@ example [Monoid M] (x y z : M) (S₁ S₂ : Submonoid M) (h : S₁ ≤ S₂) (hx
 example [Monoid M] (x y z : M) (S₁ S₂ : Submonoid M) (h : S₁ ≤ S₂) (hx : x ∈ S₁)
     (hy : y ∈ S₁) (hz : z ∈ S₂) :
     x * y * z ∈ S₁ ⊔ S₂ := by
-  grind [membership]
+  grind [membership] -- weird bug: `mem_of_le_of_mem` fails to E-match
 
 example [Monoid M] (x y z : M) (S : Submonoid M) (hxy : x * y ∈ S) (hz : z ∈ S) :
     z * (x * y) ∈ S := by
