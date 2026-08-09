be39520383ef2e761b6c9cf62f077483b02c960a
Snir Broshi <26556598+SnirBroshi@users.noreply.github.com>
2026-08-09 14:58:22 +0300

    antichains

--- Mathlib/Order/Antichain.lean
+++ Mathlib/Order/Antichain.lean
@@ -414,6 +414,10 @@ protected theorem symm (h : IsMaxAntichain r s) : IsMaxAntichain (flip r) s :=
 
 end IsMaxAntichain
 
+theorem maximal_isAntichain_iff : Maximal (IsAntichain r) s ↔ IsMaxAntichain r s where
+  mp h := ⟨h.prop, fun _ ht hst ↦ hst.antisymm <| h.le_of_ge ht hst⟩
+  mpr h := ⟨h.isAntichain, fun _ ht hst ↦ h.right ht hst |>.ge⟩
+
 end General
 
 /-! ### Weak antichains -/
