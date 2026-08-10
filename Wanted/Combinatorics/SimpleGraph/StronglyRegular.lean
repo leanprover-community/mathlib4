f869b92ef2369188622700b98c56a6505adc93a9
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2026-08-10 10:25:00 +0100

    Conway graph called G not g (as per mathlib before fix)

--- Wanted/Combinatorics/SimpleGraph/StronglyRegular.lean
+++ Wanted/Combinatorics/SimpleGraph/StronglyRegular.lean
@@ -7,7 +7,7 @@ namespace SimpleGraph
 /-- **Conway's 99-graph problem** (from https://oeis.org/A248380/a248380.pdf)
 can be reformulated as the existence of a strongly regular graph with params (99, 14, 1, 2).
 This is an open problem, and has no known proof of existence. -/
-proof_wanted conway_99 : ∃ (α : Type) (_ : Fintype α) (g : SimpleGraph α) (_ : DecidableRel g.Adj),
-    IsSRGWith g 99 14 1 2
+proof_wanted conway_99 : ∃ (α : Type) (_ : Fintype α) (G : SimpleGraph α) (_ : DecidableRel G.Adj),
+    IsSRGWith G 99 14 1 2
 
 end SimpleGraph
