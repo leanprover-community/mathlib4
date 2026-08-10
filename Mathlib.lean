5c5760a8b462bc8a77125a1edbdaebb2d72f514b
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2026-08-10 20:14:32 +0200

    feat: a measurable space structure on matrices

--- Mathlib.lean
+++ Mathlib.lean
@@ -5467,6 +5467,7 @@ public import Mathlib.MeasureTheory.Function.LpSpace.DomAct.Basic
 public import Mathlib.MeasureTheory.Function.LpSpace.DomAct.Continuous
 public import Mathlib.MeasureTheory.Function.LpSpace.Indicator
 public import Mathlib.MeasureTheory.Function.LpSpace.InfiniteSum
+public import Mathlib.MeasureTheory.Function.Matrix
 public import Mathlib.MeasureTheory.Function.Piecewise
 public import Mathlib.MeasureTheory.Function.SimpleFunc
 public import Mathlib.MeasureTheory.Function.SimpleFuncDense
