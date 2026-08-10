ad19de4f1ce999f9c752e6650deb2f540a8c413a
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2026-08-10 10:51:38 +0100

    better comment

--- Wanted/Order/KrullDimension.lean
+++ Wanted/Order/KrullDimension.lean
@@ -10,8 +10,11 @@ public import Mathlib.Order.KrullDimension
 
 namespace Order
 
-/- These two lemmas could possibly be used to simplify the calculations in
-`Mathlib/Order/KrullDimension.lean`, especially once the `Set.encard` api is richer. -/
+/-
+These two lemmas could possibly be used to simplify the calculations in
+the `Concrete calculations` section of `Mathlib/Order/KrullDimension.lean`, especially once
+the `Set.encard` api is richer.
+-/
 
 proof_wanted height_of_linearOrder {α : Type*} [LinearOrder α] (a : α) :
   height a = (Set.Iio a).encard
