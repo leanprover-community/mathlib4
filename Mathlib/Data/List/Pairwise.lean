0dba393fff8061706c135facc433e93eea0fe562
Yongxi (Aaron) Lin <97214596+CoolRmal@users.noreply.github.com>
2026-08-09 18:46:10 -0700

    Update Mathlib/Data/List/Pairwise.lean
    Co-authored-by: Snir Broshi <26556598+SnirBroshi@users.noreply.github.com>

--- Mathlib/Data/List/Pairwise.lean
+++ Mathlib/Data/List/Pairwise.lean
@@ -71,8 +71,8 @@ theorem pairwise_of_reflexive_of_forall_ne [Std.Refl R] (h : ∀ a ∈ l, ∀ b 
     apply h <;> try (apply hab.subset; simp)
     exact heq
 
-theorem pairwise_append_of_mem [Std.Symm R] [IsTrans α R] {a b : α} {l₁ l₂ : List α} (ha : a ∈ l₁)
-    (hb : b ∈ l₂) : (l₁ ++ l₂).Pairwise R ↔ R a b ∧ l₁.Pairwise R ∧ l₂.Pairwise R := by
+theorem pairwise_append_of_mem [Std.Symm R] [IsTrans α R] (ha : a ∈ l₁) (hb : b ∈ l₂) :
+    (l₁ ++ l₂).Pairwise R ↔ R a b ∧ l₁.Pairwise R ∧ l₂.Pairwise R := by
   rw [pairwise_append, ← and_rotate, and_congr_left_iff, and_imp]
   refine fun h₁ h₂ ↦ ⟨fun h ↦ h a ha b hb, ?_⟩
   refine fun hab x hx y hy ↦ _root_.trans (_root_.trans ?_ hab) ?_
