a36a5c400f48160004176c8f432c5b828f3b81a6
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2026-08-10 10:16:06 +0100

    convex cone tidyup

--- Wanted/Analysis/Convex/Cone/Basic.lean
+++ Wanted/Analysis/Convex/Cone/Basic.lean
@@ -2,15 +2,14 @@ module
 
 public import Mathlib.Analysis.Convex.Cone.Basic
 
-variable {𝕜 E : Type*} [AddCommMonoid E] [TopologicalSpace E]
+variable {𝕜 E : Type*} [AddCommGroup E] [TopologicalSpace E]
 
 namespace ConvexCone
 variable [Semifield 𝕜] [LinearOrder 𝕜] [Module 𝕜 E] {s : Set E}
 
 -- TODO: This result generalises to `G`-submodules in the sense of the Zulip thread linked below.
 
--- FIXME: This is necessary for the proof below but triggers the `unusedSectionVars` linter.
--- variable [IsStrictOrderedRing 𝕜] [IsTopologicalAddGroup E] in
+variable [IsStrictOrderedRing 𝕜] [IsTopologicalAddGroup E] in
 /-- This is true essentially by `Submodule.span_eq_iUnion_nat`, except that `Submodule` currently
 doesn't support that use case. See
 https://leanprover.zulipchat.com/#narrow/channel/116395-maths/topic/G-submodules/with/514426583 -/
