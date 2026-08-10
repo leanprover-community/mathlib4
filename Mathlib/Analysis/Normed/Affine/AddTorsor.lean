5b30f1570ed7b345237370b05afb36abea3ed898
Weiyi Wang <wwylele@gmail.com>
2026-08-10 13:55:20 +0000

    feat(Analysis/Normed/Affine): mapping dist with homothety (#41910)

--- Mathlib/Analysis/Normed/Affine/AddTorsor.lean
+++ Mathlib/Analysis/Normed/Affine/AddTorsor.lean
@@ -38,6 +38,19 @@ variable {𝕜 : Type*} [NormedField 𝕜] [NormedSpace 𝕜 V] [NormedSpace �
 open AffineMap
 
 @[simp]
+theorem dist_homothety (p₁ p₂ p : P) (c : 𝕜) :
+    dist (homothety p c p₁) (homothety p c p₂) = ‖c‖ * dist p₁ p₂ := by
+  simp [dist_eq_norm_vsub, ← (homothety p c).linearMap_vsub, homothety_linear, norm_smul]
+
+@[simp]
+theorem nndist_homothety (p₁ p₂ p : P) (c : 𝕜) :
+    nndist (homothety p c p₁) (homothety p c p₂) = ‖c‖₊ * nndist p₁ p₂ :=
+  NNReal.eq <| dist_homothety p₁ p₂ p c
+
+theorem lipschitzWith_homothety (p : P) (c : 𝕜) : LipschitzWith ‖c‖₊ (homothety p c) :=
+  LipschitzWith.of_dist_le_mul fun p₁ p₂ ↦ (dist_homothety p₁ p₂ p c).le
+
+@[simp]
 theorem dist_center_homothety (p₁ p₂ : P) (c : 𝕜) :
     dist p₁ (homothety p₁ c p₂) = ‖c‖ * dist p₁ p₂ := by
   simp [homothety_def, norm_smul, ← dist_eq_norm_vsub, dist_comm]
