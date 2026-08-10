5f7192b9c397d5b220c13cec3721b18f2d4c9c92
tb65536 <thomas.l.browning@gmail.com>
2026-08-10 22:23:38 +0100

    clean

--- Mathlib/Analysis/Normed/Algebra/SpectralRadiusLimit.lean
+++ Mathlib/Analysis/Normed/Algebra/SpectralRadiusLimit.lean
@@ -28,6 +28,18 @@ basic properties.
 
 @[expose] public section
 
+theorem Finite.ciInf_le_iff {ι α : Type*} [Finite ι] [Nonempty ι]
+    [ConditionallyCompleteLinearOrder α] {a : α}
+    {f : ι → α} : ⨅ i, f i ≤ a ↔ ∃ x, f x ≤ a :=
+  ⟨fun h ↦ (exists_eq_ciInf_of_finite).imp fun _ hi ↦ hi.trans_le h,
+    fun ⟨i, h⟩ ↦ (ciInf_le f i).trans h⟩
+
+theorem Finite.lt_ciInf_iff {ι α : Type*} [Finite ι] [Nonempty ι]
+    [ConditionallyCompleteLinearOrder α] {a : α}
+    {f : ι → α} : a < ⨅ i, f i ↔ ∀ x, a < f x := by
+  contrapose!
+  exact Finite.ciInf_le_iff
+
 open scoped Topology
 
 variable {A : Type*}
@@ -48,8 +60,25 @@ theorem tendsto_spectralRadiusLim (a : A) :
 
 @[bound]
 theorem spectralRadiusLim_nonneg (a : A) : 0 ≤ spectralRadiusLim a :=
-  isClosed_Ici.mem_of_tendsto (tendsto_spectralRadiusLim a)
-    (.of_forall fun k ↦ by rw [Set.mem_Ici]; positivity)
+  ge_of_tendsto' (tendsto_spectralRadiusLim a) fun _ ↦ by positivity
+
+namespace Mathlib.Meta.Positivity
+
+open Lean Meta Qq Function
+/-- The `positivity` extension which identifies expressions of the form `spectralRadiusLim a`. -/
+@[positivity spectralRadiusLim _]
+meta def evalSpectralRadiusLim : PositivityExt where eval {u α} _ pα? e :=
+  match pα? with | none => pure .none | some _ => do
+  match u, α, e with
+  | 0, ~q(ℝ), ~q(@spectralRadiusLim $A $KA $a) =>
+    assertInstancesCommute
+    pure (.nonnegative q(spectralRadiusLim_nonneg $a))
+  | _, _, _ => throwError "not spectralRadiusLim"
+
+end Mathlib.Meta.Positivity
+
+example (a : A) : 0 ≤ spectralRadiusLim a := by
+  positivity
 
 theorem spectralRadiusLim_le_norm (a : A) : spectralRadiusLim a ≤ ‖a‖ := by
   refine le_of_tendsto (tendsto_spectralRadiusLim a)
@@ -57,10 +86,10 @@ theorem spectralRadiusLim_le_norm (a : A) : spectralRadiusLim a ≤ ‖a‖ := b
   grw [norm_pow_le' a hn, ← Real.rpow_natCast_mul (by positivity),
     mul_inv_cancel₀ (by positivity), Real.rpow_one]
 
-theorem spectralRadiusLim_pow_of_ne_zero (a : A) (n : ℕ) (hn : n ≠ 0) :
+theorem spectralRadiusLim_pow_of_ne_zero (a : A) {n : ℕ} (hn : n ≠ 0) :
     spectralRadiusLim (a ^ n) = spectralRadiusLim a ^ n := by
-  refine tendsto_nhds_unique (tendsto_spectralRadiusLim (a ^ n)) ((tendsto_spectralRadiusLim a).comp
-    (strictMono_mul_left_of_pos hn.pos).tendsto_atTop |>.pow n |>.congr fun k ↦ ?_)
+  refine tendsto_nhds_unique ((tendsto_spectralRadiusLim (a ^ n)).congr fun k ↦ ?_)
+    (((tendsto_spectralRadiusLim a).comp (strictMono_mul_left_of_pos hn.pos).tendsto_atTop).pow n)
   rw [Function.comp_apply, Nat.cast_mul, mul_inv_rev,
     ← Real.rpow_mul_natCast (by positivity), inv_mul_cancel_right₀ (by simpa), pow_mul]
 
@@ -68,64 +97,45 @@ theorem spectralRadiusLim_pow [NormOneClass A] (a : A) (n : ℕ) :
     spectralRadiusLim (a ^ n) = spectralRadiusLim a ^ n := by
   by_cases hn : n = 0
   · simpa [hn, eq_comm] using tendsto_spectralRadiusLim (1 : A)
-  · exact spectralRadiusLim_pow_of_ne_zero a n hn
+  · exact spectralRadiusLim_pow_of_ne_zero a hn
 
-theorem spectralRadiusLim_le' (a : A) (n : ℕ) (hn : n ≠ 0) :
+theorem spectralRadiusLim_le_norm_pow (a : A) {n : ℕ} (hn : n ≠ 0) :
     spectralRadiusLim a ≤ ‖a ^ n‖ ^ (n : ℝ)⁻¹ := by
-  grw [← spectralRadiusLim_le_norm, spectralRadiusLim_pow_of_ne_zero a n hn,
-    ← Real.rpow_natCast_mul, mul_inv_cancel₀ (by simpa), Real.rpow_one] <;>
-    apply spectralRadiusLim_nonneg
+  grw [← spectralRadiusLim_le_norm, spectralRadiusLim_pow_of_ne_zero a hn,
+    ← Real.rpow_natCast_mul (by positivity), mul_inv_cancel₀ (by simpa), Real.rpow_one]
 
 theorem exists_le_spectralRadiusLim (a : A) (ε : ℝ) (hε0 : 0 < ε) :
     ∃ C > 0, ∀ n, ‖a ^ n‖ ≤ C * (spectralRadiusLim a + ε) ^ n := by
   obtain ⟨n, hn⟩ := Filter.eventually_atTop.mp ((tendsto_spectralRadiusLim a).eventually_le_const
     ((lt_add_iff_pos_right (spectralRadiusLim a)).mpr hε0))
-  use max 1 (⨆ k : Set.Iic n, ‖a ^ k.val‖ / (spectralRadiusLim a + ε) ^ k.val)
-  constructor
-  · positivity
-  · intro k
-    rcases le_or_gt k n with hk | hk
-    · rw [← div_le_iff₀]
-      · exact le_max_of_le_right (le_ciSup_of_le (Finite.bddAbove_range _) ⟨k, hk⟩ le_rfl)
-      · apply pow_pos
-        grind [spectralRadiusLim_nonneg]
-    · grw [← hn k hk.le, ← le_max_left, one_mul, ← Real.rpow_mul_natCast (by positivity),
-        inv_mul_cancel₀ (by simp; grind), Real.rpow_one]
-
-theorem Set.Finite.lt_ciInf_iff' {ι α : Type*} [ConditionallyCompleteLinearOrder α] {a : α}
-    {s : Set ι} {f : ι → α} (hs : s.Finite) (h : s.Nonempty) :
-    a < ⨅ i ∈ s, f i ↔ ∀ x ∈ s, a < f x := by
-  sorry
-
-theorem Finite.lt_ciInf_iff {ι α : Type*} [Finite ι] [Nonempty ι]
-    [ConditionallyCompleteLinearOrder α] {a : α}
-    {f : ι → α} : a < ⨅ i, f i ↔ ∀ x, a < f x := by
-  simpa using Set.finite_univ.lt_ciInf_iff' (by simp)
+  refine ⟨max 1 (⨆ k : Set.Iic n, ‖a ^ k.val‖ / (spectralRadiusLim a + ε) ^ k.val),
+    by positivity, fun k ↦ ?_⟩
+  rcases le_or_gt k n with hk | hk
+  · grw [← div_le_iff₀ (by positivity), ← le_max_right]
+    exact le_ciSup_of_le (Finite.bddAbove_range _) ⟨k, hk⟩ le_rfl
+  · grw [← le_max_left, one_mul, ← hn k hk.le, ← Real.rpow_mul_natCast (by positivity),
+      inv_mul_cancel₀ (by grind [Nat.cast_eq_zero]), Real.rpow_one]
 
 theorem exists_spectralRadiusLim_le (a : A) (ε : ℝ) (hε0 : 0 < ε) (hε : ε < spectralRadiusLim a) :
     ∃ C > 0, ∀ n, C * (spectralRadiusLim a - ε) ^ n ≤ ‖a ^ n‖ := by
   obtain ⟨n, hn⟩ := Filter.eventually_atTop.mp ((tendsto_spectralRadiusLim a).eventually_const_le
     (sub_lt_self (spectralRadiusLim a) hε0))
-  use min 1 (⨅ k : Set.Iic n, ‖a ^ k.val‖ / (spectralRadiusLim a - ε) ^ k.val)
-  constructor
+  refine ⟨min 1 (⨅ k : Set.Iic n, ‖a ^ k.val‖ / (spectralRadiusLim a - ε) ^ k.val), ?_, fun k ↦ ?_⟩
   · have h : 0 < spectralRadiusLim a := hε0.trans hε
     refine lt_min one_pos (Finite.lt_ciInf_iff.mpr fun x ↦ div_pos ?_ (by positivity))
     contrapose! h
     by_cases hx : x.val = 0
     · rw [hx, pow_zero] at h
       grw [spectralRadiusLim_le_norm, ← one_mul a, norm_mul_le, h, zero_mul]
-    · grw [spectralRadiusLim_le' a x hx, h, Real.zero_rpow (by simpa)]
-  · intro k
-    rcases le_or_gt k n with hk | hk
-    · rw [← le_div_iff₀ (by positivity)]
-      exact min_le_of_right_le (ciInf_le_of_le (Finite.bddBelow_range _) ⟨k, hk⟩ le_rfl)
+    · grw [spectralRadiusLim_le_norm_pow a hx, h, Real.zero_rpow (by simpa)]
+  · rcases le_or_gt k n with hk | hk
+    · grw [← le_div_iff₀ (by positivity), min_le_right]
+      exact ciInf_le_of_le (Finite.bddBelow_range _) ⟨k, hk⟩ le_rfl
     · grw [min_le_left, one_mul, hn k hk.le, ← Real.rpow_mul_natCast (by positivity),
         inv_mul_cancel₀ (by simp; grind), Real.rpow_one] <;> positivity
 
 theorem Commute.spectralRadiusLim_add_le {a b : A} (hc : Commute a b) :
     spectralRadiusLim (a + b) ≤ spectralRadiusLim a + spectralRadiusLim b := by
-  have := spectralRadiusLim_nonneg a
-  have := spectralRadiusLim_nonneg b
   suffices ∀ ε > 0, ε / 3 < spectralRadiusLim (a + b) → spectralRadiusLim (a + b) - ε / 3 ≤
       (spectralRadiusLim a + ε / 3) + (spectralRadiusLim b + ε / 3) from
     le_of_forall_pos_le_add fun ε hε ↦ by grind [spectralRadiusLim_nonneg]
