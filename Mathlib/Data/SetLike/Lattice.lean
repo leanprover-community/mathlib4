dd0ab336b26930e01f2b84838fdd12d8ab48ed59
artie2000 <artem.khovanov@gmail.com>
2026-08-11 18:31:45 +0100

    fix licence

--- Mathlib/Data/SetLike/Lattice.lean
+++ Mathlib/Data/SetLike/Lattice.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2026 Artie Khovanov. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Eric Wieser
+Authors: Artie Khovanov
 -/
 module
 
