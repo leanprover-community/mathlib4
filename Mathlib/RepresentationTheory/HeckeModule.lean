f07757c030f1c330438119b6ba19669fc7ae2b43
Jiaxi Mo <jiaximo1998@163.com>
2026-08-13 05:09:57 +0800

    Prove HeckeBimodule is isomorphic to the free module k[HeckeCoset]

--- Mathlib/RepresentationTheory/HeckeModule.lean
+++ Mathlib/RepresentationTheory/HeckeModule.lean
@@ -22,8 +22,9 @@ attribute [local instance] Subgroup.fintypeQuotientOfFiniteIndex
 
 namespace Representation
 
-variable {G : Type*} [Group G]
+
 variable {k : Type*} [CommRing k]
+variable {G : Type*} [Group G]
 variable {V : Type*} [AddCommGroup V] [Module k V]
 variable {W : Type*} [AddCommGroup W] [Module k W]
 
@@ -58,84 +59,88 @@ abbrev HeckeBimodule₁ (H₁ H₂ : Subgroup G) := HeckeModule₁ H₁ (ind H�
 section cosetVector
 
 /-- The unit vector supported on the left coset `gH`. -/
-abbrev cosetVectorMk (H : Subgroup G) (g : G) :
+abbrev cosetVectorMk (g : G) (H : Subgroup G) :
     IndV H.subtype (trivial k H k) := IndV.mk H.subtype (trivial k H k) g⁻¹ 1
 
 -- `H.subtype h` is not a simp normal form so we need additional simp lemmas.
 @[simp]
 lemma cosetVectorMk_mem_mul_eq {H : Subgroup G} {h : G} (hh : h ∈ H) (g : G) :
-    cosetVectorMk k H (g * h) = cosetVectorMk k H g := by
+    cosetVectorMk k (g * h) H = cosetVectorMk k g H := by
   convert IndV.mk_map_inv_mul H.subtype (trivial k H k) ⟨h, hh⟩ g⁻¹ 1 <;> simp
 
 @[simp]
 lemma cosetVectorMk_mem_eq {H : Subgroup G} {h : G} (hh : h ∈ H) :
-    cosetVectorMk k H h = cosetVectorMk k H 1 := by
+    cosetVectorMk k h H = cosetVectorMk k 1 H := by
   rw [← one_mul h]
   exact cosetVectorMk_mem_mul_eq k hh 1
 
 @[simp 2000]
 lemma ind_apply_cosetVectorMk {H : Subgroup G} (g x : G) :
-    ind H.subtype (trivial k H k) g (cosetVectorMk k H x) = cosetVectorMk k H (g * x) := by
+    ind H.subtype (trivial k H k) g (cosetVectorMk k x H) = cosetVectorMk k (g * x) H := by
   simp [cosetVectorMk]
 
 lemma cosetVectorMk_eq_ind_apply (H : Subgroup G) (g : G) :
-    cosetVectorMk k H g = ind H.subtype (trivial k H k) g (cosetVectorMk k H 1) := by
+    cosetVectorMk k g H = ind H.subtype (trivial k H k) g (cosetVectorMk k 1 H) := by
   simp
 
 lemma smul_cosetVectorMk_eq {H : Subgroup G} (g : G) (r : k) :
-    r • cosetVectorMk k H g = IndV.mk H.subtype (trivial k H k) g⁻¹ r := by
+    r • cosetVectorMk k g H = IndV.mk H.subtype (trivial k H k) g⁻¹ r := by
   simp [← map_smul]
 
 lemma ind_trivial.cosetVectorMk_one.surjective {H : Subgroup G} {v : V}
-    (f : IntertwiningMap ρ (ind H.subtype (trivial k H k))) (h : f v = cosetVectorMk k H 1) :
+    (f : IntertwiningMap ρ (ind H.subtype (trivial k H k))) (h : f v = cosetVectorMk k 1 H) :
     Function.Surjective f :=
   fun x => IndV.induction_on x
     (fun g r => ⟨ρ g⁻¹ (r • v), by simp [IntertwiningMap.isIntertwining, h, smul_cosetVectorMk_eq]⟩)
     (fun _ _ ⟨x, hx⟩ ⟨y, hy⟩ => ⟨x + y, by simp [hx, hy]⟩)
 
-def cosetVector (H : Subgroup G) :
+def cosetVector {H : Subgroup G} :
     G ⧸ H → IndV H.subtype (trivial k H k) :=
-  Quotient.lift (fun g => cosetVectorMk k H g) fun x _ h => by
+  Quotient.lift (fun g => cosetVectorMk k g H) fun x _ h => by
     simpa [eq_comm] using cosetVectorMk_mem_mul_eq k (QuotientGroup.leftRel_apply.mp h) x
 
-lemma cosetVectorMk_eq_cosetVector_mk (H : Subgroup G) (g : G) :
-    cosetVectorMk k H g = cosetVector k H ⟦g⟧ :=
+lemma cosetVectorMk_eq_cosetVector_mk (g : G) :
+    cosetVectorMk k g H = cosetVector k ⟦g⟧ :=
   rfl
 
-lemma cosetVectorMk_out_eq_cosetVector (H : Subgroup G) (x : G ⧸ H) :
-    cosetVectorMk k H x.out = cosetVector k H x := by
+lemma cosetVectorMk_out_eq_cosetVector (x : G ⧸ H) :
+    cosetVectorMk k x.out H = cosetVector k x := by
   simp [cosetVectorMk_eq_cosetVector_mk]
 
 @[simp]
-lemma ind_apply_cosetVector (H : Subgroup G) (g : G) (x : G ⧸ H) :
-    ind H.subtype (trivial k H k) g (cosetVector k H x) = cosetVector k H (g • x) := by
+lemma ind_apply_cosetVector (g : G) (x : G ⧸ H) :
+    ind H.subtype (trivial k H k) g (cosetVector k x) = cosetVector k (g • x) := by
   rw [← cosetVectorMk_out_eq_cosetVector, ind_apply_cosetVectorMk, cosetVectorMk_eq_cosetVector_mk]
   exact congrArg _ (MulAction.Quotient.mk_smul_out H g x)
 
-@[simp]
-lemma ind_apply_cosetVector_one (H : Subgroup G) (g : H) :
-    ind H.subtype (trivial k H k) g (cosetVector k H ⟦1⟧) = cosetVector k H ⟦1⟧ := by
-  simp [← cosetVectorMk_eq_cosetVector_mk]
-
-abbrev cosetVectorLinearMap (H : Subgroup G) :
-    IndV H.subtype (trivial k H k) →ₗ[k] (G ⧸ H →₀ k) :=
-  IndV.lift H.subtype (trivial k H k)
-    (fun g => LinearMap.toSpanSingleton k _ (Finsupp.single ⟦g⁻¹⟧ 1))
+def indTrivialToOfMulActionMap :
+    IntertwiningMap (ind H.subtype (trivial k H k)) (ofMulAction k G (G ⧸ H)) where
+  toLinearMap := IndV.lift H.subtype (trivial k H k)
+    (fun g => LinearMap.toSpanSingleton k _ (MonoidAlgebra.single ⟦g⁻¹⟧ 1))
     (by intros; congr 3; exact Quotient.sound (QuotientGroup.leftRel_apply.mpr (by simp)))
+  isIntertwining' _ := by ext; simp
 
-abbrev cosetVectorLinearInv (H : Subgroup G) :
-    (G ⧸ H →₀ k) →ₗ[k] IndV H.subtype (trivial k H k) :=
-  Finsupp.lsum k fun x => LinearMap.toSpanSingleton k _ (cosetVector k H x)
+@[simp]
+lemma indTrivialToOfMulActionMap_apply_cosetVector (x : G ⧸ H) :
+    (indTrivialToOfMulActionMap k H) (cosetVector k x) = MonoidAlgebra.single x 1 := by
+  simp [indTrivialToOfMulActionMap, ← cosetVectorMk_out_eq_cosetVector]
 
-def cosetVectorLinearEquiv (H : Subgroup G) :
-    IndV H.subtype (trivial k H k) ≃ₗ[k] (G ⧸ H →₀ k) where
-  toLinearMap := cosetVectorLinearMap k H
-  invFun := cosetVectorLinearInv k H
+@[simps!]
+def indTrivialToOfMulActionInv :
+    IntertwiningMap (ofMulAction k G (G ⧸ H)) (ind H.subtype (trivial k H k)) where
+  toLinearMap := (Finsupp.lsum k fun x => LinearMap.toSpanSingleton k _ (cosetVector k x)) ∘ₗ
+    (MonoidAlgebra.coeffLinearEquiv k).toLinearMap
+  isIntertwining' _ := by ext; simp
+
+def indTrivialOfMulActionEquiv :
+    (ind H.subtype (trivial k H k)).Equiv (ofMulAction k G (G ⧸ H)) where
+  toIntertwiningMap := indTrivialToOfMulActionMap k H
+  invFun := (indTrivialToOfMulActionInv k H).toLinearMap
   left_inv x := IndV.induction_on x
-    (by simp [← cosetVectorMk_eq_cosetVector_mk, smul_cosetVectorMk_eq])
+    (by simp [indTrivialToOfMulActionMap, ← cosetVectorMk_eq_cosetVector_mk, smul_cosetVectorMk_eq])
     (by simp_all [map_add])
-  right_inv x := Finsupp.induction x (by simp) fun g => Quotient.inductionOn g
-    (by simp [map_add, ← cosetVectorMk_eq_cosetVector_mk])
+  right_inv x := MonoidAlgebra.induction x (by simp) fun g => Quotient.inductionOn g
+    (by simp [indTrivialToOfMulActionMap, map_add, ← cosetVectorMk_eq_cosetVector_mk])
 
 end cosetVector
 
@@ -145,15 +150,15 @@ section HeckeModule₁
 
 @[ext]
 lemma HeckeModule₁.ext {H : Subgroup G} (f g : HeckeModule₁ H ρ)
-    (h : f (cosetVectorMk k H 1) = g (cosetVectorMk k H 1)) : f = g := by
+    (h : f (cosetVectorMk k 1 H) = g (cosetVectorMk k 1 H)) : f = g := by
   ext x
   rw [← inv_inv x]
-  change f (cosetVectorMk k H x⁻¹) = g (cosetVectorMk k H x⁻¹)
+  change f (cosetVectorMk k x⁻¹ H) = g (cosetVectorMk k x⁻¹ H)
   rw [cosetVectorMk_eq_ind_apply]
   simp only [IntertwiningMap.isIntertwining, h]
 
 /-- Construct elements of the standard Hecke module from `H`-invariants. -/
-def HeckeModule₁.invariantsMk (H : Subgroup G) :
+def HeckeModule₁.invariantsMk :
     invariants (ρ.comp H.subtype) →ₗ[k] HeckeModule₁ H ρ where
   toFun v := ind.lift H.subtype
     ⟨LinearMap.toSpanSingleton k _ v.val, fun g => by ext; simpa [eq_comm] using v.prop g⟩
@@ -161,22 +166,15 @@ def HeckeModule₁.invariantsMk (H : Subgroup G) :
   map_smul' _ _ := by ext; simp
 
 @[simp]
-lemma HeckeModule₁.invariantsMk_apply (H : Subgroup G) (v : invariants (ρ.comp H.subtype)) (g : G) :
-    HeckeModule₁.invariantsMk ρ H v (cosetVectorMk k H g) = ρ g v := by
+lemma HeckeModule₁.invariantsMk_apply (v : invariants (ρ.comp H.subtype)) (g : G) :
+    HeckeModule₁.invariantsMk H ρ v (cosetVectorMk k g H) = ρ g v := by
   simp [invariantsMk]
 
-@[simp]
-lemma HeckeModule₁.invariantsMk_eq_iff {H : Subgroup G}
-    {v₁ v₂ : invariants (ρ.comp H.subtype)} :
-  HeckeModule₁.invariantsMk ρ H v₁ = HeckeModule₁.invariantsMk ρ H v₂ ↔ v₁ = v₂ :=
-  ⟨fun h => by simpa using congrArg (fun f => f (cosetVectorMk k H 1)) h, fun h => by
-    simp [h]⟩
-
 /-- `HeckeModule₁.invariantsMk` is a linear equivalence. -/
-def invariantsHeckeModule₁Equiv (H : Subgroup G) :
+def invariantsHeckeModule₁Equiv :
     invariants (ρ.comp H.subtype) ≃ₗ[k] HeckeModule₁ H ρ where
-  toLinearMap := HeckeModule₁.invariantsMk ρ H
-  invFun f := ⟨f (cosetVectorMk k H 1), by simp [← IntertwiningMap.isIntertwining]⟩
+  toLinearMap := HeckeModule₁.invariantsMk H ρ
+  invFun f := ⟨f (cosetVectorMk k 1 H), by simp [← IntertwiningMap.isIntertwining]⟩
   left_inv _ := by simp
   right_inv _ := by ext; simp
 
@@ -184,9 +182,9 @@ end HeckeModule₁
 
 variable (k) (H₁ : Subgroup G) (g : G) (H₂ : Subgroup G) (g' : G) (H₃ : Subgroup G)
 
-section HeckeTriple
+open Pointwise MonoidAlgebra
 
-open Pointwise
+section HeckeTriple
 
 lemma mem_conjAct_pointwise_smul_iff {H : Subgroup G} {g x : G} :
     x ∈ ConjAct.toConjAct g • H ↔ g⁻¹ * x * g ∈ H := by
@@ -195,6 +193,11 @@ lemma mem_conjAct_pointwise_smul_iff {H : Subgroup G} {g x : G} :
 
 abbrev DecompQuotient := H₁ ⧸ ((ConjAct.toConjAct g) • H₂).subgroupOf H₁
 
+lemma DecompQuotient.mul_mk_leftCoset_eq_iff (x x' : H₁) :
+    (⟦x.val * g⟧ : G ⧸ H₂) = ⟦x'.val * g⟧ ↔ ⟦x⟧ = (⟦x'⟧ : DecompQuotient H₁ g H₂) := by
+  simp [Quotient.eq, QuotientGroup.leftRel_apply, mul_assoc, Subgroup.mem_subgroupOf,
+    mem_conjAct_pointwise_smul_iff]
+
 def DecompQuotient.toLeftCoset :
     DecompQuotient H₁ g H₂ → G ⧸ H₂ :=
   Quotient.lift (fun x => ⟦x * g⟧) fun _ _ h => by
@@ -202,15 +205,7 @@ def DecompQuotient.toLeftCoset :
     have := (QuotientGroup.leftRel_apply.mp h)
     simpa [mul_assoc] using mem_conjAct_pointwise_smul_iff.mp this
 
-lemma DecompQuotient.mem_mul_mk_leftCoset_eq_iff (x x' : H₁) :
-    (⟦x.val * g⟧ : G ⧸ H₂) = ⟦x'.val * g⟧ ↔ x⁻¹ * x' ∈ ((ConjAct.toConjAct g) • H₂).subgroupOf H₁ :=
-  ⟨ fun h => by
-      rw [Subgroup.mem_subgroupOf, mem_conjAct_pointwise_smul_iff]
-      simpa [mul_assoc] using QuotientGroup.leftRel_apply.mp (Quotient.exact h),
-    fun h => by
-      rw [Quotient.eq, QuotientGroup.leftRel_apply]
-      simpa [mul_assoc] using mem_conjAct_pointwise_smul_iff.mp h⟩
-
+@[simp]
 lemma DecompQuotient.toLeftCoset_apply (x : DecompQuotient H₁ g H₂) :
     DecompQuotient.toLeftCoset H₁ g H₂ x = ⟦x.out * g⟧ := by
   nth_rw 1 [← Quotient.out_eq x]
@@ -275,30 +270,40 @@ abbrev HeckeSet.Setoid : Setoid (HeckeSet H₁ H₂) :=
 
 def HeckeCoset := Quotient (HeckeSet.Setoid H₁ H₂)
 
-def HeckeCosetModule := HeckeCoset H₁ H₂ →₀ k
-
-abbrev sumCosetVector (H : Subgroup G) {ι : Type*} [Fintype ι] (s : ι → G ⧸ H) :
-    IndV H.subtype (trivial k H k) := ∑ x : ι, cosetVector k H (s x)
+abbrev HeckeCosetModule := HeckeCoset H₁ H₂ →₀ k
 
 abbrev HeckeCosetVectorMk [hg : IsHeckeTriple H₁ g H₂] :
     IndV H₂.subtype (trivial k H₂ k) :=
-  ∑ x, cosetVector k H₂ (DecompQuotient.toLeftCoset H₁ g H₂ x)
+  ∑ x, cosetVector k (DecompQuotient.toLeftCoset H₁ g H₂ x)
+
+variable {H₁ H₂}
+
+lemma HeckeCoset.mk_eq_iff (x y : HeckeSet H₁ H₂) :
+    (⟦x⟧ : HeckeCoset H₁ H₂) = ⟦y⟧ ↔ ∃ (h₁ : H₁) (h₂ : H₂), y.val = h₁ * x.val * h₂ := by
+  simpa [Quotient.eq] using DoubleCoset.rel_iff
+
+lemma HeckeCoset.leftCoset_ne_if_ne {x y : HeckeCoset H₁ H₂} (hxy : x ≠ y) (h₁ : H₁) :
+    (⟦h₁ * x.out⟧ : G ⧸ H₂) ≠ ⟦(y.out : G)⟧ := by
+  intro h
+  apply hxy
+  sorry
 
 def HeckeCosetVector : HeckeCoset H₁ H₂ → IndV H₂.subtype (trivial k H₂ k) :=
-  fun h => ∑ x, cosetVector k H₂ (DecompQuotient.toLeftCoset H₁ h.out H₂ x)
+  fun h => ∑ x, cosetVector k (DecompQuotient.toLeftCoset H₁ h.out H₂ x)
 
 lemma HeckeCosetVectorMk_out_eq_HeckeCosetVector (x : HeckeCoset H₁ H₂) :
-    HeckeCosetVectorMk k H₁ x.out H₂ = HeckeCosetVector k H₁ H₂ x := by
+    HeckeCosetVectorMk k H₁ x.out H₂ = HeckeCosetVector k x := by
   rfl
 
-end HeckeTriple
-
-section HeckeBimodule₁'
-
-abbrev HeckeBimodule₁' := invariants ((ind H₂.subtype (trivial k H₂ k)).comp H₁.subtype)
-
-def HeckeBimodule₁HeckeBimodule₁'Equiv : HeckeBimodule₁ k H₂ H₁ ≃ₗ[k] HeckeBimodule₁' k H₂ H₁ :=
-  (invariantsHeckeModule₁Equiv (ind H₁.subtype (trivial k H₁ k)) H₂).symm
+lemma HeckeCosetVector_isInvariant (x : HeckeCoset H₁ H₂) (h₁ : H₁) :
+    ind H₂.subtype (trivial k H₂ k) h₁ (HeckeCosetVector k x) = HeckeCosetVector k x := by
+  simp only [HeckeCosetVector, DecompQuotient.toLeftCoset_apply, map_sum, ind_apply_cosetVector]
+  exact Fintype.sum_equiv (MulAction.toPerm h₁) _ _ fun y => by
+    congr 1
+    simp only [MulAction.Quotient.smul_mk, smul_eq_mul, ← mul_assoc]
+    change ⟦(h₁ * y.out : H₁) * (x.out : G)⟧ = _
+    rw [DecompQuotient.mul_mk_leftCoset_eq_iff]
+    simp [← MulAction.Quotient.mk_smul_out]
 
 lemma HeckeCoset.out_leftCoset.injective :
     Function.Injective (fun x : HeckeCoset H₁ H₂ => (⟦x.out⟧ : G ⧸ H₂)) := by
@@ -307,36 +312,82 @@ lemma HeckeCoset.out_leftCoset.injective :
   exact Quotient.sound <| DoubleCoset.rel_iff.mpr
     ⟨1, by simp, _, QuotientGroup.leftRel_apply.mp (Quotient.exact h), by simp⟩
 
-def HeckeBimodule₁'HeckeCosetModuleLinearMap :
-    HeckeBimodule₁' k H₁ H₂ →ₗ[k] HeckeCoset H₁ H₂ →₀ k :=
-  Finsupp.lcomapDomain (fun x => ⟦x.out.val⟧) (HeckeCoset.out_leftCoset.injective H₁ H₂) ∘ₗ
-    cosetVectorLinearMap k H₂ ∘ₗ (HeckeBimodule₁' k H₁ H₂).subtype
+end HeckeTriple
 
-def HeckeBimodule₁'HeckeCosetModuleLinearInv :
-    (HeckeCoset H₁ H₂ →₀ k) →ₗ[k] HeckeBimodule₁' k H₁ H₂ where
-  toFun := Finsupp.lsum k fun x => LinearMap.toSpanSingleton k _ ⟨(HeckeCosetVector k H₁ H₂ x),
-    fun h₁ => by
-      rw [← HeckeCosetVectorMk_out_eq_HeckeCosetVector]
-      simp only [MonoidHom.coe_comp, Subgroup.coe_subtype, Function.comp_apply, HeckeCosetVectorMk,
-        DecompQuotient.toLeftCoset_apply, ← cosetVectorMk_eq_cosetVector_mk, map_sum,
-        ind_apply_cosetVectorMk]
-      exact Fintype.sum_equiv (MulAction.toPerm h₁) _ _ fun _ => by
-        simp only [← mul_assoc, ← MulMemClass.coe_mul H₁, cosetVectorMk_eq_cosetVector_mk]
-        congr 1
-        rw [DecompQuotient.mem_mul_mk_leftCoset_eq_iff]
-        apply QuotientGroup.leftRel_apply.mp
-        exact Quotient.exact <| by simp [← MulAction.Quotient.mk_smul_out]⟩
-  map_add' := by simp [map_add]
-  map_smul' := by simp
+section bimodule₁
 
-def HeckeBimodule₁'HeckeCosetModuleLinearEquiv :
-    HeckeBimodule₁' k H₁ H₂ ≃ₗ[k] HeckeCoset H₁ H₂ →₀ k where
-  toLinearMap := HeckeBimodule₁'HeckeCosetModuleLinearMap k H₁ H₂
-  invFun := HeckeBimodule₁'HeckeCosetModuleLinearInv k H₁ H₂
-  left_inv _ := by ext; simp; sorry
-  right_inv _ := by ext; simp; sorry
+namespace HeckeBimodule₁
 
-end HeckeBimodule₁'
+variable {H₁ H₂} in
+abbrev mk (x : HeckeCoset H₁ H₂) : HeckeBimodule₁ k H₁ H₂ :=
+  HeckeModule₁.invariantsMk H₁ _ ⟨HeckeCosetVector k x, HeckeCosetVector_isInvariant k x⟩
+
+def toHeckeCosetModuleLinearMap :
+    HeckeBimodule₁ k H₁ H₂ →ₗ[k] k[HeckeCoset H₁ H₂] where
+  toFun f := MonoidAlgebra.comapDomain (fun x => ⟦x.out.val⟧) (HeckeCoset.out_leftCoset.injective)
+    (indTrivialToOfMulActionMap k H₂ (f (cosetVectorMk k 1 H₁)))
+  map_add' := by simp
+  map_smul' _ _ := by ext; simp
+
+@[simp]
+lemma toHeckeCosetModuleLinearMap_apply_HeckeCosetVector (x : HeckeCoset H₁ H₂) :
+    (toHeckeCosetModuleLinearMap k H₁ H₂) (HeckeBimodule₁.mk k x) = single x 1
+  := by classical
+  ext y
+  simp only [toHeckeCosetModuleLinearMap, LinearMap.coe_mk, AddHom.coe_mk,
+    HeckeModule₁.invariantsMk_apply, map_one, Module.End.one_apply, coeff_comapDomain,
+    Finsupp.comapDomain_apply, coeff_single]
+  simp only [HeckeCosetVector, DecompQuotient.toLeftCoset_apply, map_sum,
+    indTrivialToOfMulActionMap_apply_cosetVector, coeff_sum, coeff_single, Finsupp.coe_finsetSum,
+    Finset.sum_apply]
+  by_cases hxy : x = y
+  · have (g : G) (z : DecompQuotient H₁ g H₂) : (⟦(z.out : G) * g⟧ : G ⧸ H₂) = ⟦g⟧ ↔ z = ⟦1⟧ := by
+      constructor
+      · rw [← Quotient.out_eq' z, ← DecompQuotient.mul_mk_leftCoset_eq_iff]
+        simp
+      · rintro rfl
+        nth_rw 4 [← one_mul g]
+        change ⟦(⟦1⟧.out: H₁) * g⟧ = ⟦(1 : H₁) * g⟧
+        rw [DecompQuotient.mul_mk_leftCoset_eq_iff]
+        simp
+    simp [← hxy, Finsupp.single_apply, this]
+  · simp [hxy, HeckeCoset.leftCoset_ne_if_ne]
+
+def toHeckeCosetModuleLinearInv :
+    k[HeckeCoset H₁ H₂] →ₗ[k] HeckeBimodule₁ k H₁ H₂ :=
+  (MonoidAlgebra.basis (HeckeCoset H₁ H₂) k).constr k fun x => HeckeBimodule₁.mk k x
+
+@[simp]
+lemma toHeckeCosetModuleLinearInv_apply_single (x : HeckeCoset H₁ H₂) :
+    ((toHeckeCosetModuleLinearInv k H₁ H₂) (single x 1)) = HeckeBimodule₁.mk k x := by
+  simp [toHeckeCosetModuleLinearInv, ← MonoidAlgebra.basis_apply]
+
+variable {k H₁ H₂} in
+lemma induction_on (f : HeckeBimodule₁ k H₁ H₂) {p : HeckeBimodule₁ k H₁ H₂ → Prop}
+    (mk : ∀ (g : HeckeCoset H₁ H₂), p (HeckeBimodule₁.mk k g))
+    (smul : ∀ (x : HeckeBimodule₁ k H₁ H₂) (r : k), p x → p (r • x))
+    (add : ∀ x y, p x → p y → p (x + y)) : p f := by
+  sorry
+
+def toHeckeCosetModuleLinearEquiv :
+    HeckeBimodule₁ k H₁ H₂ ≃ₗ[k] k[HeckeCoset H₁ H₂] where
+  toLinearMap := toHeckeCosetModuleLinearMap k H₁ H₂
+  invFun := toHeckeCosetModuleLinearInv k H₁ H₂
+  left_inv f := induction_on f
+    (p := fun x => (toHeckeCosetModuleLinearInv _ _ _) (toHeckeCosetModuleLinearMap _ _ _ x) = x)
+    (by simp) (fun _ _ h => by simp [h]) (fun _ _ hx hy => by simp [hx, hy])
+  right_inv x := induction_linear x (by simp) (fun _ _ h h' => by nth_rw 2 [← h, ← h']; simp) <| by
+    intro _ r
+    rw [← mul_one r, ← MonoidAlgebra.smul_single', map_smul]
+    simp
+
+end HeckeBimodule₁
+
+end bimodule₁
+
+section HeckeOperator
+
+end HeckeOperator
 
 end Hecke
 
