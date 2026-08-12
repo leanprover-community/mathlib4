eefe479c59addd11852996ba8f7d045048cf85a9
Moritz Firsching <firsching@google.com>
2026-08-12 09:03:00 +0200

    feat(SetTheory/Cardinal/Arithmetic): properties of `sdiff`
    ported from Formal Conjectures ForMathlib original pr:
    https://github.com/google-deepmind/formal-conjectures/issues/1496
    
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- Mathlib/SetTheory/Cardinal/Arithmetic.lean
+++ Mathlib/SetTheory/Cardinal/Arithmetic.lean
@@ -820,6 +820,30 @@ theorem mk_bounded_subset_le {α : Type u} (s : Set α) (c : Cardinal.{u}) :
 
 end computing
 
+/-! ### Properties of `sdiff` -/
+section sdiff
+variable {α : Type*} {s t : Set α}
+
+@[simp]
+lemma mk_sdiff_eq_left' (hs : s.Infinite) (hst : #↑(s ∩ t) < #s) : #↑(s \ t) = #s := by
+  refine (mk_le_mk_of_subset Set.sdiff_subset).eq_of_not_lt
+    fun h ↦ (add_lt_of_lt (by simpa) hst h).not_ge ?_
+  grw [← mk_union_le .., Set.inter_union_sdiff]
+
+@[simp]
+lemma mk_sdiff_eq_left (hs : s.Infinite) (hts : #t < #s) : #↑(s \ t) = #s :=
+  mk_sdiff_eq_left' hs <| hts.trans_le' <| mk_subtype_mono Set.inter_subset_right
+
+@[simp]
+lemma mk_sdiff_eq_left_of_finite' (hs : s.Infinite) (hst : (s ∩ t).Finite) : #↑(s \ t) = #s :=
+  mk_sdiff_eq_left' hs <| (aleph0_le_mk_set.2 hs).trans_lt' <| by simpa
+
+@[simp]
+lemma mk_sdiff_eq_left_of_finite (hs : s.Infinite) (ht : t.Finite) : #↑(s \ t) = #s :=
+  mk_sdiff_eq_left hs <| (aleph0_le_mk_set.2 hs).trans_lt' <| by simpa
+
+end sdiff
+
 /-! ### Properties of `compl` -/
 section compl
 
