7eccc66d626cc21a1a452af56ab03e5db91a00bd
ADedecker <anatolededecker@gmail.com>
2026-08-12 23:21:12 +0200

    fixes

--- Mathlib/Topology/Algebra/Module/ContinuousLinearMap/Invertible.lean
+++ Mathlib/Topology/Algebra/Module/ContinuousLinearMap/Invertible.lean
@@ -232,16 +232,18 @@ protected theorem of_isInvertible_inverse (hf : f.inverse.IsInvertible) : f.IsIn
   obtain ⟨_, _⟩ : Subsingleton M₂ ∧ Subsingleton M := by simpa [inverse, H] using hf
   simp_all [Subsingleton.elim f 0]
 
+end IsInvertible
+
 @[simp]
-theorem _root_.ContinuousLinearMap.isInvertible_inverse_iff :
+theorem isInvertible_inverse_iff {f : M →L[R] M₂} :
     f.inverse.IsInvertible ↔ f.IsInvertible :=
   ⟨.of_isInvertible_inverse, .inverse⟩
 
-theorem isInvertible_iff_isHomeomorph :
+theorem isInvertible_iff_isHomeomorph {f : M →L[R] M₂} :
     f.IsInvertible ↔ IsHomeomorph f :=
   ⟨fun ⟨e, he⟩ ↦ he ▸ e.isHomeomorph,
     fun hf ↦ ⟨.ofIsHomeomorph (.ofBijective f.toLinearMap hf.bijective) hf, rfl⟩⟩
 
-end IsInvertible
+alias ⟨IsInvertible.isHomeomorph, IsInvertible.of_isHomeomorph⟩ := isInvertible_iff_isHomeomorph
 
 end ContinuousLinearMap
