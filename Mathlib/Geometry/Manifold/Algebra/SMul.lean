0ee1f9f1ad06c1af8656f19fadd546ed0b990c77
qdiazblanco <qdiazblanco@gmail.com>
2026-08-12 18:10:50 +0200

    add my name to authors :)

--- Mathlib/Geometry/Manifold/Algebra/SMul.lean
+++ Mathlib/Geometry/Manifold/Algebra/SMul.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2026 Ben Eltschig. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Ben Eltschig, Pepa Montero
+Authors: Ben Eltschig, Pepa Montero, Enrique Díaz Blanco
 -/
 module
 
