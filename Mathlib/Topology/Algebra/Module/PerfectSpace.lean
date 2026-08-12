1340fc600420ea24065934f7b62bb1f303e7194b
Alex Meiburg <timeroot.alex@gmail.com>
2026-08-12 14:55:15 +0000

    Fix perfectSpace_of_module for new PerfectSpace definition

--- Mathlib/Topology/Algebra/Module/PerfectSpace.lean
+++ Mathlib/Topology/Algebra/Module/PerfectSpace.lean
@@ -26,9 +26,9 @@ lemma perfectSpace_of_module : PerfectSpace E := by
   have A : Tendsto (fun (n : ℕ) ↦ x + r ^ n • c) atTop (𝓝 (x + (0 : 𝕜) • c)) := by
     apply Tendsto.add tendsto_const_nhds
     exact (tendsto_pow_atTop_nhds_zero_of_norm_lt_one hr).smul tendsto_const_nhds
-  have B : Tendsto (fun (n : ℕ) ↦ x + r ^ n • c) atTop (𝓝[univ \ {x}] x) := by
+  have B : Tendsto (fun (n : ℕ) ↦ x + r ^ n • c) atTop (𝓝[≠] x) := by
     simp only [zero_smul, add_zero] at A
     simp [tendsto_nhdsWithin_iff, A, hc, norm_pos_iff.mp hr₀]
-  exact accPt_principal_iff_nhdsWithin.mpr ((atTop_neBot.map _).mono B)
+  exact (atTop_neBot.map _).mono B
 
 instance : PerfectSpace 𝕜 := perfectSpace_of_module 𝕜 𝕜
