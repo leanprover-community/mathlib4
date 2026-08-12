48605aa722667baf91cfea356f1b124134494c1f
Stefan Kebekus <kebekus@users.noreply.github.com>
2026-08-12 15:54:13 +0200

    Implement referee's suggestion.

--- Mathlib/Algebra/Polynomial/Roots.lean
+++ Mathlib/Algebra/Polynomial/Roots.lean
@@ -144,10 +144,15 @@ theorem finite_setOfPred_isRoot {p : R[X]} (hp : p ≠ 0) : Set.Finite { x | IsR
 
 @[deprecated (since := "2026-07-09")] alias finite_setOf_isRoot := finite_setOfPred_isRoot
 
+/-- Nonzero polynomials have no roots away from a cofinite set. -/
+lemma eventually_cofinite_not_isRoot {p : R[X]} (hp : p ≠ 0) :
+    ∀ᶠ x in Filter.cofinite, ¬p.IsRoot x :=
+  (finite_setOfPred_isRoot hp).compl_mem_cofinite
+
 /-- Nonzero polynomials are nonzero away from a cofinite set. -/
 lemma eventually_eval_ne_zero_cofinite {p : R[X]} (hp : p ≠ 0) :
-    ∀ᶠ x in Filter.cofinite, p.eval x ≠ 0 := by
-  simpa [Filter.eventually_cofinite] using finite_setOfPred_isRoot hp
+    ∀ᶠ x in Filter.cofinite, p.eval x ≠ 0 :=
+  eventually_cofinite_not_isRoot hp
 
 theorem eq_zero_of_infinite_isRoot (p : R[X]) (h : Set.Infinite { x | IsRoot p x }) : p = 0 :=
   not_imp_comm.mp finite_setOfPred_isRoot h
