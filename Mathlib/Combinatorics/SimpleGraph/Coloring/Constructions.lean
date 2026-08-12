d6e16d9f297a68ae9379d5431c569da9a261d8a8
Snir Broshi <26556598+SnirBroshi@users.noreply.github.com>
2026-08-12 05:23:47 +0300

    `by_contra` instead of `Classical.by_contradiction`

--- Mathlib/Combinatorics/SimpleGraph/Coloring/Constructions.lean
+++ Mathlib/Combinatorics/SimpleGraph/Coloring/Constructions.lean
@@ -73,9 +73,9 @@ theorem Coloring.odd_length_iff_not_congr {α} {G : SimpleGraph α}
   tauto
 
 theorem Walk.three_le_chromaticNumber_of_odd_loop {α} {G : SimpleGraph α} {u : α} (p : G.Walk u u)
-    (hOdd : Odd p.length) : 3 ≤ G.chromaticNumber := Classical.by_contradiction <| by
-  intro h
-  have h' : G.chromaticNumber ≤ 2 := Order.le_of_lt_add_one <| not_le.mp h
+    (hOdd : Odd p.length) : 3 ≤ G.chromaticNumber := by
+  by_contra! h
+  have h' : G.chromaticNumber ≤ 2 := Order.le_of_lt_add_one h
   let c : G.Coloring (Fin 2) := (chromaticNumber_le_iff_colorable.mp h').some
   let c' : G.Coloring Bool := recolorOfEquiv G finTwoEquiv c
   have : ¬c' u ↔ c' u := (c'.odd_length_iff_not_congr p).mp hOdd
