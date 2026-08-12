01f7a00e1cd8a2609e4ec4fee11e5b2bddf8c4e1
Oliver Nash <7734364+ocfnash@users.noreply.github.com>
2026-08-12 17:11:51 +0000

    feat: a Lie algebra is determined by its root system (#42071)

--- Mathlib/Algebra/Lie/Derivation/Basic.lean
+++ Mathlib/Algebra/Lie/Derivation/Basic.lean
@@ -391,7 +391,7 @@ end Inner
 
 section ExpNilpotent
 
-variable {R L : Type*} [CommRing R] [LieRing L] [LieAlgebra R L] [LieAlgebra ℚ L]
+variable {R L : Type*} [CommRing R] [LieRing L] [LieAlgebra R L] [Module ℚ L]
   (D : LieDerivation R L L)
 
 /-- In characteristic zero, the exponential of a nilpotent derivation is a Lie algebra
