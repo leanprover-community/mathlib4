a842fe72122702f7f1cc0e1c1d22cd138465e042
Batixx <s59fpern@uni-bonn.de>
2026-08-13 02:56:26 +0200

    fix

--- Mathlib/Topology/EMetricSpace/Lipschitz.lean
+++ Mathlib/Topology/EMetricSpace/Lipschitz.lean
@@ -137,7 +137,7 @@ open Metric
 variable [TopologicalSpace α] [WeakPseudoEMetricSpace α] [TopologicalSpace β]
   [WeakPseudoEMetricSpace β] [TopologicalSpace γ] [WeakPseudoEMetricSpace γ]
   [PseudoEMetricSpace δ] [PseudoEMetricSpace τ] [PseudoEMetricSpace ζ]
-variable {K : ℝ≥0} {f : α → β} (g : δ → τ) {x y : α} {r : ℝ≥0∞} {s : Set α}
+variable {K : ℝ≥0} {f : α → β} {g : δ → τ} {x y : α} {r : ℝ≥0∞} {s : Set α}
 
 protected theorem lipschitzOnWith (h : LipschitzWith K f) : LipschitzOnWith K f s :=
   fun x _ y _ => h x y
