638a69b836fcae65249c76893d38c3f218777e99
Xavier Roblot <46200072+xroblot@users.noreply.github.com>
2026-08-13 10:49:47 +0900

    clean up

--- Mathlib/Algebra/QuadraticAlgebra/Basic.lean
+++ Mathlib/Algebra/QuadraticAlgebra/Basic.lean
@@ -82,10 +82,6 @@ theorem omega_mul_omega_eq_algebraMap :
   simp [omega_mul_omega_eq_add, Algebra.algebraMap_eq_smul_one]
 
 @[simp]
-theorem basis_apply_one : (basis a b) 1 = ω := by
-  ext <;> simp [basis]
-
-@[simp]
 theorem omega_mul_mk (x y : R) : (ω : QuadraticAlgebra R a b) * ⟨x, y⟩ = ⟨a * y, x + b * y⟩ := by
   ext <;> simp
 
