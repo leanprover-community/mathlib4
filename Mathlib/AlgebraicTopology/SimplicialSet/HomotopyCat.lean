5524f3a332fb6e44eed8dba0aa974bb9359d0700
Joël Riou <joel.riou@universite-paris-saclay.fr>
2026-08-14 12:16:52 +0530

    typo

--- Mathlib/AlgebraicTopology/SimplicialSet/HomotopyCat.lean
+++ Mathlib/AlgebraicTopology/SimplicialSet/HomotopyCat.lean
@@ -350,7 +350,7 @@ lemma morphismProperty_eq_top {W : MorphismProperty V.HomotopyCategory}
 
 /-- Induction principle for proving a property for all the morphisms
 in the homotopy category of a `2`-truncated simplicial set: it suffices
-to show that the property hold for morphisms induced by edges and that
+to show that the property holds for morphisms induced by edges and that
 the property is stable under composition. -/
 @[elab_as_elim, cases_eliminator, induction_eliminator]
 lemma hom_rec {motive : ∀ {x y : V.HomotopyCategory}, (x ⟶ y) → Prop}
