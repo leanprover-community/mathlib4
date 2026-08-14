f60ad279897567a8d42124aec09346a65c210e78
ADedecker <anatolededecker@gmail.com>
2026-08-14 16:41:28 +0200

    split in anticipation

--- Mathlib/Analysis/Normed/Operator/Fredholm/Open.lean
+++ Mathlib/Analysis/Normed/Operator/Fredholm/Open.lean
@@ -23,30 +23,41 @@ namespace ContinuousLinearMap
 
 variable {𝕜 E F : Type*} [NontriviallyNormedField 𝕜]
     [NormedAddCommGroup E] [NormedAddCommGroup F] [NormedSpace 𝕜 E] [NormedSpace 𝕜 F]
-    [CompleteSpace 𝕜] [CompleteSpace E]
+    [CompleteSpace E]
 
-/-- If `T` is a Fredholm operators between two Banach spaces, then every operator `S` close
-enough to `T` (in operator norm) is also Fredholm. -/
-protected theorem IsFredholm.eventually
-    {T : E →L[𝕜] F} (hT : T.IsFredholm) : ∀ᶠ S in 𝓝 T, S.IsFredholm := by
-  obtain ⟨pkg⟩ := hT.nonempty_fredholmPackage
+/-- Let `T : E → F` be a Fredholm operator between two Banach spaces, and choose a `FredholmPackage`
+for `T`; that is, fix topological decompositions `E = E₁ ⊕ E₀` and `F = F₁ ⊕ F₀`, where `E₀` and
+`F₀` are finite dimensional, and such that in these decompositions we have
+$T = \begin{pmatrix} α₀ & 0 \cr 0 & 0$ with `α₀` invertible.
+
+Then, for $S = \begin{pmatrix} α & β \cr γ & δ$ close enough to `T` (in operator norm), we have
+that `α` is invertible. -/
+theorem FredholmPackage.eventually_isInvertible
+    {T : E →L[𝕜] F} (pkg : T.FredholmPackage) :
+    ∀ᶠ S in 𝓝 T, (pkg.decCodom.proj ∘L S ∘L pkg.decDom.X₁.subtypeL).IsInvertible := by
   have : CompleteSpace pkg.decDom.X₁ := pkg.decDom.isTopCompl.isClosed.isComplete.completeSpace_coe
-  have := pkg.decDom.cofg_X₁
   let Φ (S : E →L[𝕜] F) : (pkg.decDom.X₁ →L[𝕜] pkg.decCodom.X₁) :=
     pkg.decCodom.proj ∘L S ∘L pkg.decDom.X₁.subtypeL
   have Φ_cont : Continuous Φ := by fun_prop
   have Φ_T_inv : (Φ T).IsInvertible := ⟨pkg.equiv, by ext; simp [Φ, pkg.eq_equiv]⟩
-  have : ∀ᶠ S in 𝓝 T, (Φ S).IsInvertible := Φ_cont.tendsto T |>.eventually Φ_T_inv.eventually
-  filter_upwards [this] with S Φ_S_inv
+  exact Φ_cont.tendsto T |>.eventually Φ_T_inv.eventually
+
+/-- If `T` is a Fredholm operators between two Banach spaces, then every operator `S` close
+enough to `T` (in operator norm) is also Fredholm. -/
+protected theorem IsFredholm.eventually [CompleteSpace 𝕜]
+    {T : E →L[𝕜] F} (hT : T.IsFredholm) : ∀ᶠ S in 𝓝 T, S.IsFredholm := by
+  obtain ⟨pkg⟩ := hT.nonempty_fredholmPackage
+  filter_upwards [pkg.eventually_isInvertible] with S h_inv
   have A : IsFredholm pkg.decDom.X₁.subtypeL :=
+    have := pkg.decDom.cofg_X₁
     pkg.decDom.X₁.isFredholm_subtypeL pkg.decDom.isTopCompl.isClosed
   have B : IsFredholm pkg.decCodom.proj := pkg.decCodom.isFredholm_proj
   rw [← A.comp_iff_left, ← B.comp_iff_right]
-  exact Φ_S_inv.isFredholm
+  exact h_inv.isFredholm
 
 /-- The set of Fredholm operators between two Banach spaces is open (for the operator norm)
 in the space of continuous linear maps. -/
-theorem isOpen_setOfPred_isFredholm : IsOpen {T : E →L[𝕜] F | T.IsFredholm} :=
+theorem isOpen_setOfPred_isFredholm [CompleteSpace 𝕜] : IsOpen {T : E →L[𝕜] F | T.IsFredholm} :=
   isOpen_iff_mem_nhds.mpr fun _ ↦ IsFredholm.eventually
 
 end ContinuousLinearMap
