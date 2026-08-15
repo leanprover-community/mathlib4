c3a9118cfbcbd39ace85667a5f19095b844db983
Weiyi Wang <wwylele@gmail.com>
2026-08-15 17:11:38 -0400

    bessel function deriv

--- Mathlib/Analysis/SpecialFunctions/Bessel.lean
+++ Mathlib/Analysis/SpecialFunctions/Bessel.lean
@@ -111,4 +111,111 @@ theorem analyticAt_besselJ_int (a : ℤ) (x : ℂ) : AnalyticAt ℂ (J(a)) x := 
 theorem besselJ_zero (a : ℂ) : J(a) 0 = if a = 0 then 1 else 0 := by
   split_ifs with h <;> simp [besselJ, h, regularizedHGFunCoeff]
 
+theorem self_mul_besselJ (a : ℂ) (x : ℂ) :
+    2 * a * J(a) x = x * J(a - 1) x + x * J(a + 1) x := by
+  by_cases h : x = 0
+  · simp [h, besselJ_zero]
+  have h : x / 2 ≠ 0 := by simp_all
+  unfold besselJ
+  calc
+    _ = 2 * (x / 2) ^ a * (a * F₀₁(a) (-(x / 2) ^ 2)) := by
+      ring
+    _ = 2 * (x / 2) ^ a * F₀₁(a - 1) (-(x / 2) ^ 2) +
+        2 * (x / 2) ^ a * (x / 2) ^ 2 * F₀₁(a + 1) (-(x / 2) ^ 2) := by
+      rw [← sub_eq_iff_eq_add.mpr (mul_regularizedHGFun_zero_singleton a (-(x / 2) ^ 2))]
+      ring_nf
+    _ = 2 * ((x / 2) ^ (a - 1) * (x / 2) ^ (1 : ℂ)) * F₀₁(a - 1) (-(x / 2) ^ 2) +
+        2 * (x / 2) ^ (a + 1) * (x / 2) * F₀₁(a + 1) (-(x / 2) ^ 2) := by
+      rw [← cpow_add _ _ h, sub_add_cancel, cpow_add _ _ h, cpow_one]
+      ring
+    _ = _ := by
+      rw [cpow_one]
+      ring
+
+theorem mul_deriv_besselJ_eq_besselJ_add_one (a : ℂ) {x : ℂ} (h : x ∈ slitPlane) :
+    x * deriv J(a) x = a * J(a) x - x * J(a + 1) x := by
+  have hx2 : x / 2 ∈ slitPlane := by simpa [slitPlane] using h
+  have hx0 : x / 2 ≠ 0 := by
+    intro h
+    simp_all
+  unfold besselJ
+  calc
+    _ = x * (deriv (fun x ↦ (x / 2) ^ a) x * F₀₁(a) (-(x / 2) ^ 2) +
+        (x / 2) ^ a * deriv ((fun x ↦ F₀₁(a) x) ∘ fun x ↦ -(x / 2) ^ 2) x) := by
+      rw [deriv_fun_mul (by fun_prop) (by fun_prop)]
+      rfl
+    _ = 2 * (x / 2) * deriv (fun x ↦ (x / 2) ^ a) x * F₀₁(a) (-(x / 2) ^ 2) + x *
+        (x / 2) ^ a * (F₀₁(a + 1) (-(x / 2) ^ 2) * (-x / 2)) := by
+      rw [deriv_comp _ (by fun_prop) (by fun_prop), deriv_regularizedHGFun (by simp)]
+      simp
+      ring
+    _ = (a * (x / 2) ^ a) * F₀₁(a) (-(x / 2) ^ 2) + x *
+        (x / 2) ^ a * (F₀₁(a + 1) (-(x / 2) ^ 2) * (-x / 2)) := by
+      congrm ?_ * _ + _
+      rw [_root_.deriv_cpow_const (by fun_prop) (by exact hx2)]
+      trans 2 * a * ((x / 2) ^ (a - 1) * (x / 2) ^ (1 : ℂ) * deriv (fun x ↦ x / 2) x)
+      · norm_cast
+        ring
+      rw [← cpow_add _ _ hx0]
+      simp
+      ring
+    _ = _ := by
+      rw [cpow_add _ _ hx0, cpow_one]
+      ring
+
+theorem mul_deriv_besselJ_eq_besselJ_sub_one (a : ℂ) {x : ℂ} (h : x ∈ slitPlane) :
+    x * deriv J(a) x = x * J(a - 1) x - a * J(a) x := by
+  linear_combination self_mul_besselJ a x + mul_deriv_besselJ_eq_besselJ_add_one a h
+
+theorem two_mul_deriv_besselJ (a : ℂ) {x : ℂ} (h : x ∈ slitPlane) :
+    2 * deriv J(a) x = J(a - 1) x - J(a + 1) x := by
+  have hx0 : x ≠ 0 := by
+    intro h
+    simp_all
+  rw [← mul_left_inj' hx0]
+  linear_combination mul_deriv_besselJ_eq_besselJ_sub_one a h +
+    mul_deriv_besselJ_eq_besselJ_add_one a h
+
+theorem besselJ_neg_one (x : ℂ) : J(-1) x = -J(1) x := by
+  simpa using besselJ_neg_int 1 x
+
+open Topology
+
+theorem two_mul_deriv_besselJ_int (a : ℤ) (x : ℂ) :
+    2 * deriv J(a) x = J(a - 1) x - J(a + 1) x := by
+  wlog! hx0 : x ≠ 0
+  · suffices (fun _ ↦ 2) * deriv J(a) =ᶠ[𝓝 0] J(↑(a - 1)) - J(↑(a + 1)) by
+      simpa [hx0] using this.eq_of_nhds
+    rw [← ContinuousAt.eventuallyEq_nhds_iff_eventuallyEq_nhdsNE
+      (AnalyticAt.continuousAt (𝕜 := ℂ) (by fun_prop))
+      (AnalyticAt.continuousAt (𝕜 := ℂ) (by fun_prop))]
+    refine eventuallyEq_nhdsWithin_of_eqOn fun x hx ↦ ?_
+    simpa using this a x (by simpa using hx)
+  by_cases hx : x ∈ slitPlane
+  · exact two_mul_deriv_besselJ a hx
+  have hx' : -x ∈ slitPlane := by
+    contrapose! hx0
+    rw [Complex.ext_iff]
+    simp_all [slitPlane]
+    grind
+  convert congr(- $(two_mul_deriv_besselJ (-a) hx'))
+  · trans 2 * (deriv ((fun x ↦ J(a) (-x)) ∘ (-·)) x)
+    · simp [Function.comp_def]
+    simp_rw [← besselJ_neg_comm]
+    norm_cast
+    rw [deriv_comp _ (by fun_prop) (by fun_prop)]
+    simp
+  · norm_cast
+    simp_rw [← besselJ_neg_comm]
+    simp
+    ring_nf
+
+theorem mul_deriv_besselJ_eq_besselJ_add_one_int (a : ℤ) (x : ℂ) :
+    x * deriv J(a) x = a * J(a) x - x * J(a + 1) x := by
+  linear_combination x * two_mul_deriv_besselJ_int a x / 2 - self_mul_besselJ a x / 2
+
+theorem mul_deriv_besselJ_eq_besselJ_sub_one_int (a : ℤ) (x : ℂ) :
+    x * deriv J(a) x = x * J(a - 1) x - a * J(a) x := by
+  linear_combination self_mul_besselJ a x + mul_deriv_besselJ_eq_besselJ_add_one_int a x
+
 end Complex
