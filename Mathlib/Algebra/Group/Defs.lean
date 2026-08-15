80b91a1c4f919894d3401bf572dd665f846dd5e3
Chris Henson <chrishenson.net@gmail.com>
2026-08-15 17:45:04 +0100

    chore: remove outdated FIXME for congr

--- Mathlib/Algebra/Group/Defs.lean
+++ Mathlib/Algebra/Group/Defs.lean
@@ -402,9 +402,7 @@ class MulOneClass (M : Type u) extends MulOne M where
 @[to_additive (attr := ext)]
 theorem MulOneClass.ext {M : Type u} : ∀ ⦃m₁ m₂ : MulOneClass M⦄, m₁.mul = m₂.mul → m₁ = m₂ := by
   rintro @⟨@⟨⟨one₁⟩, ⟨mul₁⟩⟩, one_mul₁, mul_one₁⟩ @⟨@⟨⟨one₂⟩, ⟨mul₂⟩⟩, one_mul₂, mul_one₂⟩ ⟨rfl⟩
-  -- FIXME (See https://github.com/leanprover/lean4/issues/1711)
-  -- congr
-  suffices one₁ = one₂ by cases this; rfl
+  congr
   exact (one_mul₂ one₁).symm.trans (mul_one₁ one₂)
 
 section MulOneClass
