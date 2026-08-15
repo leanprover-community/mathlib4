d7dd2eec0511be351323c67d8a0f98cd7cf922dc
Xavier Roblot <46200072+xroblot@users.noreply.github.com>
2026-08-15 14:34:06 +0900

    remove sandbox

--- Mathlib/Sandbox.lean
+++
@@ -1,7 +0,0 @@
-module
-
-public import Mathlib.RingTheory.Ideal.Norm.AbsNorm
-
--- `Infinite S` should be gone from the unprimed version now.
-#check @Ideal.exists_prime_and_absNorm_eq_pow
-#check @Ideal.exists_prime_and_absNorm_eq_pow'
