da14e39ed59ace43eb751f95f1841b2e2f4de631
Joël Riou <37772949+joelriou@users.noreply.github.com>
2026-08-15 08:01:30 +0000

    feat(CategoryTheory/Limits): weighted limits commute with (co)limits in the weight variable (#41146)
    This is an adaptation of code from https://github.com/joelriou/reedy in order to allow uses both for Reedy categories and applications to hypercovers #41125. In particular, the proof of the preservations of limits with respect to the weight variable (which was obtained in https://github.com/joelriou/reedy in the most favourable case) is proven here in a similar manner as in #41125.
    
    
    - [x] depends on: #41163
    
    Co-authored-by: Yun Liu

--- Mathlib.lean
+++ Mathlib.lean
@@ -3027,6 +3027,7 @@ public import Mathlib.CategoryTheory.Limits.WeakLimits.WeakEqualizers
 public import Mathlib.CategoryTheory.Limits.WeakLimits.WeakKernels
 public import Mathlib.CategoryTheory.Limits.WeakLimits.WeakPullbacks
 public import Mathlib.CategoryTheory.Limits.Weighted.HasWeightedLimit
+public import Mathlib.CategoryTheory.Limits.Weighted.PreservesLimit
 public import Mathlib.CategoryTheory.Limits.Yoneda
 public import Mathlib.CategoryTheory.Linear.Basic
 public import Mathlib.CategoryTheory.Linear.FunctorCategory
