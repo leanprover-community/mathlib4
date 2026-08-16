cb2499b790bac0f748170dd30fa8b97ea2e425cb
JadAbouHawili <jad-abou-hawili@hotmail.com>
2026-08-16 21:22:59 +0300

    Update Mathlib/Combinatorics/SimpleGraph/Connectivity/EdgeConnectivity.lean
    Co-authored-by: Snir Broshi <26556598+SnirBroshi@users.noreply.github.com>

--- Mathlib/Combinatorics/SimpleGraph/Connectivity/EdgeConnectivity.lean
+++ Mathlib/Combinatorics/SimpleGraph/Connectivity/EdgeConnectivity.lean
@@ -220,8 +220,8 @@ theorem edgeReachability_of_Reachable (G : SimpleGraph V) (u v : V)
   specialize h' 1
   grind [isEdgeReachable_one, Nat.cast_one, iSup_le_iff]
 
-theorem IsEdgeReachable.le_edgeReachability
-    (h : G.IsEdgeReachable k u v) : k ≤ G.edgeReachability u v :=
+theorem IsEdgeReachable.le_edgeReachability (h : G.IsEdgeReachable k u v) :
+    k ≤ G.edgeReachability u v :=
   le_iSup₂ (α := ℕ∞) k h
 
 theorem IsEdgeConnected.le_edgeConnectivity (h : G.IsEdgeConnected k) : k ≤ G.edgeConnectivity :=
