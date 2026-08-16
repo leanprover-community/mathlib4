78d9a14a37345c0ac2089e5150a8297d02c74d1a
rshlyakh <157648681+rshlyakh@users.noreply.github.com>
2026-08-16 09:15:08 -0700

    chore: remove unneeded parentheses
    Co-authored-by: Weiyi Wang <wwylele@gmail.com>

--- Mathlib/Algebra/MonoidAlgebra/Grading.lean
+++ Mathlib/Algebra/MonoidAlgebra/Grading.lean
@@ -177,9 +177,8 @@ theorem grade.decompose_single (i : ι) (r : R) :
   decomposeAux_single _ _ _
 
 /-- The component of degree `a` in the decomposition of `f` is its `a`-term. -/
-@[simp] lemma grade.decompose_apply [DecidableEq M]
-  (f : AddMonoidAlgebra R M) (a : M) :
-    ((DirectSum.decompose (grade R) f) a) = single a (f.coeff a) := by
+@[simp] lemma grade.decompose_apply [DecidableEq M] (f : AddMonoidAlgebra R M) (a : M) :
+    DirectSum.decompose (grade R) f a = single a (f.coeff a) := by
   induction f using induction_linear with
   | zero => simp
   | add x y hx hy => simp [decompose_add, hx, hy]
