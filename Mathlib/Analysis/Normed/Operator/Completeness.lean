96b1833365889df8c4aa54d147d50aa9b4fdba8b
TJHeeringa <16029718+TJHeeringa@users.noreply.github.com>
2026-08-17 18:17:10 +0000

    chore(Analysis): rename CLM.lipschitz, Isometry.lipschitz, Isometry.antilipschitz  (#42828)
    This PR renames
    
    - `ContinuousLinearMap.lipschitz` -> `ContinuousLinearMap.lipschitzWith`
    - `Isometry.lipschitz` -> `Isometry.lipschitzWith`
    - `Isometry.antilipschitz` -> `Isometry.antilipschitzWith`

--- Mathlib/Analysis/Normed/Operator/Completeness.lean
+++ Mathlib/Analysis/Normed/Operator/Completeness.lean
@@ -93,7 +93,7 @@ pointwise convergence is precompact: its closure is a compact set. -/
 theorem isCompact_closure_image_coe_of_bounded [ProperSpace F] {s : Set (E' →SL[σ₁₂] F)}
     (hb : IsBounded s) : IsCompact (closure (((↑) : (E' →SL[σ₁₂] F) → E' → F) '' s)) :=
   have : ∀ x, IsCompact (closure (apply' F σ₁₂ x '' s)) := fun x =>
-    ((apply' F σ₁₂ x).lipschitz.isBounded_image hb).isCompact_closure
+    ((apply' F σ₁₂ x).lipschitzWith.isBounded_image hb).isCompact_closure
   (isCompact_pi_infinite this).closure_of_subset
     (image_subset_iff.2 fun _ hg _ => subset_closure <| mem_image_of_mem _ hg)
 
