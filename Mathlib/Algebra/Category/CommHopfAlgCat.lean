4e4367b4e505bb9be7e204ce2e79b55302fe6fcd
thorimur <68410468+thorimur@users.noreply.github.com>
2026-08-17 14:35:56 -0400

    ditto

--- Mathlib/Algebra/Category/CommHopfAlgCat.lean
+++ Mathlib/Algebra/Category/CommHopfAlgCat.lean
@@ -62,7 +62,7 @@ instance : Category (CommHopfAlgCat.{v} R) where
 
 instance : ConcreteCategory (CommHopfAlgCat.{v} R) (· →ₐc[R] ·) where
   hom := Hom.hom'
-  ofHom := Hom._mkInternal
+  ofHom := Hom.mk
 
 /-- Turn a morphism in `CommHopfAlgCat` back into a `BialgHom`. -/
 abbrev Hom.hom (f : Hom A B) := ConcreteCategory.hom (C := CommHopfAlgCat R) f
