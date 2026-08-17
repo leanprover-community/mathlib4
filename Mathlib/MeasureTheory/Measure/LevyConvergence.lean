0ee0d93631c98168116281a592f85b6459fa031b
Ivo Malinowski <35851409+imalinowskip@users.noreply.github.com>
2026-08-17 21:55:59 +0100

    simplified proof
    Co-authored-by: Etienne Marion <66847262+EtienneC30@users.noreply.github.com>

--- Mathlib/MeasureTheory/Measure/LevyConvergence.lean
+++ Mathlib/MeasureTheory/Measure/LevyConvergence.lean
@@ -230,7 +230,7 @@ lemma tendstoInDistribution_of_tendsto_charFun
     (hX : ∀ n, AEMeasurable (X n) (P n)) (hX' : AEMeasurable X' P')
     (h : ∀ t : E, Tendsto (fun n ↦ charFun ((P n).map (X n)) t) atTop (𝓝 (charFun (P'.map X') t))) :
     TendstoInDistribution X atTop X' P P' where
-  forall_aemeasurable n := (hX n)
+  forall_aemeasurable := hX
   tendsto := by
     apply ProbabilityMeasure.tendsto_of_tendsto_charFun
     simpa only [ProbabilityMeasure.coe_mk] using h
