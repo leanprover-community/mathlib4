55f86e9a28f3e45261d7a911a565e72b00eda54d
vihdzp <vi.hdz.p@gmail.com>
2026-08-18 06:13:08 -0600

    start

--- Mathlib/Topology/Metrizable/Basic.lean
+++ Mathlib/Topology/Metrizable/Basic.lean
@@ -18,7 +18,7 @@ see `Mathlib/Topology/Metrizable/Uniformity.lean`.
 -/
 
 -- don't import the real numbers
-assert_not_exists Real
+assert_not_exists DivisionSemiring IsOrderedRing
 
 public section
 
