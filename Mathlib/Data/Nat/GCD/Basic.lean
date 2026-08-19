1ff5c18a05db81ea6e7350c4715a73d2ed386bd4
Xavier Roblot <46200072+xroblot@users.noreply.github.com>
2026-08-19 18:45:37 +0200

    review

--- Mathlib/Data/Nat/GCD/Basic.lean
+++ Mathlib/Data/Nat/GCD/Basic.lean
@@ -40,7 +40,7 @@ theorem gcd_right_comm (a b c : ℕ) : gcd (gcd a b) c = gcd (gcd a c) b := by
   rw [gcd_assoc, gcd_assoc, gcd_comm b c]
 
 theorem gcd_gcd_gcd_right (a b c : ℕ) : (a.gcd c).gcd (b.gcd c) = (a.gcd b).gcd c := by
-  rw [gcd_assoc, ← gcd_assoc c b c, gcd_comm c b, gcd_assoc b c c, gcd_self, ← gcd_assoc]
+  rw [gcd_left_comm, gcd_gcd_self_left_right, gcd_left_comm, gcd_assoc]
 
 theorem gcd_gcd_gcd_left (a b c : ℕ) : (c.gcd a).gcd (c.gcd b) = c.gcd (a.gcd b) := by
   rw [gcd_comm c a, gcd_comm c b, gcd_gcd_gcd_right, gcd_comm]
--- Mathlib/Data/ZMod/QuotientGroup.lean
+++ Mathlib/Data/ZMod/QuotientGroup.lean
@@ -49,13 +49,13 @@ def quotientZMultiplesEquivZMod (a : ℤ) : ℤ ⧸ AddSubgroup.zmultiples a ≃
 lemma index_zmultiples (a : ℤ) : (AddSubgroup.zmultiples a).index = a.natAbs := by
   rw [AddSubgroup.index, Nat.card_congr (quotientZMultiplesEquivZMod a).toEquiv, Nat.card_zmod]
 
+open AddSubgroup in
 /-- The relative index of `zmultiples a` in `zmultiples b` (as subgroups of `ℤ`, `a b : ℕ`),
 multiplied by `gcd a b`, is `a`. -/
 lemma relIndex_zmultiples_mul (a b : ℕ) :
-    (AddSubgroup.zmultiples (a : ℤ)).relIndex (AddSubgroup.zmultiples (b : ℤ)) * a.gcd b = a := by
-  rw [show a.gcd b = ((a : ℤ).gcd (b : ℤ) : ℤ).natAbs by simp [Int.gcd_natCast_natCast],
-    ← AddSubgroup.relIndex_sup_left, ← index_zmultiples, ← Int.zmultiples_sup,
-    AddSubgroup.relIndex_mul_index le_sup_left, index_zmultiples, Int.natAbs_natCast]
+    (zmultiples (a : ℤ)).relIndex (zmultiples (b : ℤ)) * a.gcd b = a := by
+  rw [show a.gcd b = (zmultiples (a : ℤ) ⊔ zmultiples (b : ℤ)).index by simp [Int.zmultiples_sup],
+    ← relIndex_sup_left, relIndex_mul_index le_sup_left, index_zmultiples, Int.natAbs_natCast]
 
 end Int
 
