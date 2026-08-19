5143e40d3c8f6d2abbe82439664c17343e5385dc
Dennj Osele <dennj.osele@gmail.com>
2026-08-19 01:45:30 +0100

    move ToType-related changes to a separate PR

--- Mathlib/CategoryTheory/Presentable/IsCardinalFiltered.lean
+++ Mathlib/CategoryTheory/Presentable/IsCardinalFiltered.lean
@@ -197,12 +197,11 @@ lemma isCardinalFiltered_preorder (J : Type w) [Preorder J]
       { app a := homOfLE (hj a)
         naturality _ _ _ := rfl }⟩
 
+set_option backward.isDefEq.respectTransparency.types false in
 instance (κ : Cardinal.{w}) [hκ : Fact κ.IsRegular] :
     IsCardinalFiltered κ.ord.ToType κ :=
   isCardinalFiltered_preorder _ _ (fun ι f hs ↦ by
-    have h : Function.Surjective (fun i ↦ (⟨f i, i, rfl⟩ : Set.range f)) := by
-      rintro ⟨_, i, rfl⟩
-      exact ⟨i, rfl⟩
+    have h : Function.Surjective (fun i ↦ (⟨f i, i, rfl⟩ : Set.range f)) := fun _ ↦ by aesop
     contrapose! hs
     rw [← hκ.out.cof_ord, ← Ordinal.cof_toType]
     refine (Order.cof_le fun j ↦ ?_).trans (Cardinal.mk_le_of_surjective h)
