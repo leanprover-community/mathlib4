e111c21cb6e02526b45decc578df7c57019d07e1
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2026-08-19 17:16:23 +0200

    `continuous_uncurry_toEuclideanCLM`

--- Mathlib/Analysis/CStarAlgebra/Matrix.lean
+++ Mathlib/Analysis/CStarAlgebra/Matrix.lean
@@ -107,6 +107,19 @@ def toEuclideanCLM :
       map_mul' := fun _ _ ↦ rfl
       map_star' := adjoint_toContinuousLinearMap }
 
+@[fun_prop]
+lemma continuous_uncurry_toEuclideanCLM :
+    Continuous (fun (S, x) ↦ toEuclideanCLM (n := n) (𝕜 := 𝕜) S x) := by
+  refine Continuous.comp (by fun_prop) <| continuous_pi fun i ↦ ?_
+  simp only [LinearEquiv.toEquiv_symm, Equiv.symm_symm, Equiv.invFun_as_coe,
+    LinearEquiv.coe_symm_toEquiv, toMatrix'_symm, OrthonormalBasis.coe_toBasis_repr,
+    LinearEquiv.coe_coe, LinearEquiv.trans_apply, LinearIsometryEquiv.coe_toLinearEquiv,
+    linearEquiv_apply, AddEquiv.toEquiv_eq_coe, Equiv.toFun_as_coe, EquivLike.coe_coe,
+    addEquiv_apply, toLin'_apply, LinearEquiv.symm_mk, coe_mk, AddHom.coe_mk, LinearEquiv.coe_mk,
+    LinearEquiv.coe_toEquiv, Finsupp.linearEquivFunOnFinite_apply,
+    Finsupp.equivFunOnFinite_symm_apply_apply]
+  fun_prop
+
 lemma coe_toEuclideanCLM_eq_toEuclideanLin (A : Matrix n n 𝕜) :
     (toEuclideanCLM (n := n) (𝕜 := 𝕜) A : _ →ₗ[𝕜] _) = toEuclideanLin A :=
   rfl
