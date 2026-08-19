899a0a87730ce16d2a94273f70737164973355a5
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2026-08-19 16:47:36 +0200

    feat(Gaussian/Multivariate): `multivariateGaussian` is measurable w.r.t. its parameters

--- Mathlib/Analysis/Matrix/MeasurableSpace.lean
+++ Mathlib/Analysis/Matrix/MeasurableSpace.lean
@@ -5,8 +5,9 @@ Authors: Gaëtan Serré
 -/
 module
 
-public import Mathlib.MeasureTheory.Constructions.BorelSpace.Basic
-public import Mathlib.Topology.Instances.Matrix
+public import Mathlib.Analysis.CStarAlgebra.Matrix
+public import Mathlib.Analysis.Normed.Lp.MeasurableSpace
+public import Mathlib.MeasureTheory.Constructions.BorelSpace.Complex
 
 /-!
 # Measurable space structure on Matrices
@@ -52,8 +53,28 @@ variable (m n α : Type*) [MeasurableSpace α]
 lemma measurable_of : Measurable <| Matrix.of (m := m) (n := n) (α := α) :=
   measurable_id
 
+section BorelSpace
+
 instance [Countable m] [Countable n] [TopologicalSpace α] [SecondCountableTopology α]
   [BorelSpace α] : BorelSpace (Matrix m n α) := inferInstanceAs <| BorelSpace (m → n → α)
+
+@[fun_prop]
+lemma measurable_toEuclideanCLM {𝕜 : Type*} [RCLike 𝕜] [ContinuousAdd 𝕜] [ContinuousMul 𝕜]
+    [BorelSpace 𝕜] [Fintype m] [DecidableEq m] :
+    Measurable (fun (S, x) ↦ toEuclideanCLM (n := m) (𝕜 := 𝕜) S x) := by
+  apply Continuous.measurable
+  refine Continuous.comp ?_ <| continuous_pi fun i ↦ ?_
+  · fun_prop
+  · simp only [LinearEquiv.toEquiv_symm, Equiv.symm_symm, Equiv.invFun_as_coe,
+    LinearEquiv.coe_symm_toEquiv, LinearMap.toMatrix'_symm, OrthonormalBasis.coe_toBasis_repr,
+    LinearEquiv.coe_coe, LinearEquiv.trans_apply, LinearIsometryEquiv.coe_toLinearEquiv,
+    WithLp.linearEquiv_apply, AddEquiv.toEquiv_eq_coe, Equiv.toFun_as_coe, EquivLike.coe_coe,
+    WithLp.addEquiv_apply, toLin'_apply, LinearEquiv.symm_mk, LinearMap.coe_mk, AddHom.coe_mk,
+    LinearEquiv.coe_mk, LinearEquiv.coe_toEquiv, Finsupp.linearEquivFunOnFinite_apply,
+    Finsupp.equivFunOnFinite_symm_apply_apply]
+    fun_prop
+
+end BorelSpace
 
 /-- The map from `m → n → α` to `Matrix m n α` as a measurable equivalence. -/
 protected def ofMeasurableEquiv : (m → n → α) ≃ᵐ (Matrix m n α) where
