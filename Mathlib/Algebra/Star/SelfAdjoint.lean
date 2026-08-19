92242d81d48af2ddc0758c27f750c4ebc9d7669b
Jireh Loreaux <loreaujy@gmail.com>
2026-08-19 13:39:55 -0500

    remove more

--- Mathlib/Algebra/Star/SelfAdjoint.lean
+++ Mathlib/Algebra/Star/SelfAdjoint.lean
@@ -116,16 +116,15 @@ theorem map {F R S : Type*} [Star R] [Star S] [FunLike F R S] [StarHomClass F R 
     {x : R} (f : F) (hx : IsSelfAdjoint x) : IsSelfAdjoint (f x) :=
   show star (f x) = f x from map_star f x ▸ congr_arg f hx
 
-lemma of_map {F R S : Type*} [AddMonoid R] [AddMonoid S] [StarAddMonoid R] [StarAddMonoid S]
-    [FunLike F R S] [StarHomClass F R S] [AddMonoidHomClass F R S] (f : F)
+lemma of_map {F R S : Type*} [Star R] [Star S]
+    [FunLike F R S] [StarHomClass F R S] (f : F)
     {x : R} (hf : Function.Injective f) (hx : IsSelfAdjoint (f x)) :
     IsSelfAdjoint x :=
   hf <| by simp [map_star, hx.star_eq]
 
 lemma _root_.Function.Injective.isSelfAdjoint_apply_iff
-    {F R S : Type*} [AddMonoid R] [AddMonoid S] [StarAddMonoid R] [StarAddMonoid S]
-    [FunLike F R S] [StarHomClass F R S] [AddMonoidHomClass F R S] (f : F)
-    {x : R} (hf : Function.Injective f) :
+    {F R S : Type*} [Star R] [Star S] [FunLike F R S] [StarHomClass F R S]
+    (f : F) {x : R} (hf : Function.Injective f) :
     IsSelfAdjoint (f x) ↔ IsSelfAdjoint x :=
   ⟨.of_map f hf, .map f⟩
 
