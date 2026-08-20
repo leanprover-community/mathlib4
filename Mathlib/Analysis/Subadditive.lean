f1fd052371e256d56528b93956e1b4ef44d2617e
tb65536 <thomas.l.browning@gmail.com>
2026-08-20 22:12:07 +0100

    golf

--- Mathlib/Analysis/Subadditive.lean
+++ Mathlib/Analysis/Subadditive.lean
@@ -5,7 +5,7 @@ Authors: Sébastien Gouëzel
 -/
 module
 
-public import Mathlib.Analysis.SpecialFunctions.Pow.Real
+public import Mathlib.Analysis.SpecialFunctions.Pow.Continuity
 
 /-!
 # Convergence of subadditive sequences
@@ -33,6 +33,67 @@ for all `m, n`. -/
 def Submultiplicative {α β : Type*} [Add α] [Mul β] [LE β] (u : α → β) : Prop :=
   ∀ m n, u (m + n) ≤ u m * u n
 
+namespace Submultiplicative
+
+variable {u : ℕ → ℝ} (h : Submultiplicative u)
+
+/-- The limit of the nth roots of a submultiplicative sequence. The fact that the nth roots indeed
+converge to this limit is given in `Submultiplicative.tendsto_lim`. -/
+protected def lim (_h : Submultiplicative u) :=
+  sInf ((fun n : ℕ ↦ u n ^ (n : ℝ)⁻¹) '' Ici 1)
+
+theorem lim_le_rpow (hbdd : ∀ n, 0 ≤ u n) {n : ℕ} (hn : n ≠ 0) : h.lim ≤ u n ^ (n : ℝ)⁻¹ := by
+  refine csInf_le ⟨0, ?_⟩ ⟨n, hn.pos, rfl⟩
+  rintro - ⟨n, hn, rfl⟩
+  exact Real.rpow_nonneg (hbdd n) n⁻¹
+
+include h in
+theorem apply_mul_add_le (k n r) (hbdd : 0 ≤ u n) : u (k * n + r) ≤ u n ^ k * u r := by
+  induction k with
+  | zero => simp
+  | succ k IH => grw [add_one_mul, add_right_comm, h (k * n + r) n, IH, mul_right_comm, pow_succ]
+
+include h in
+theorem eventually_rpow_lt_of_rpow_lt (hbdd : ∀ k, 0 ≤ u k) {L : ℝ} {n : ℕ} (hn : n ≠ 0)
+    (hL : u n ^ (n : ℝ)⁻¹ < L) : ∀ᶠ p in atTop, u p ^ (p : ℝ)⁻¹ < L := by
+  /- It suffices to prove the statement for each arithmetic progression `(n * · + r)`. -/
+  refine .atTop_of_arithmetic hn fun r hrn ↦ ?_
+  /- `(u n ^ x * u r) ^ (x * n + r)⁻¹` tends to `u n ^ n⁻¹ < L`, hence
+  `(u n ^ x * u r) ^ (x * n + r)⁻¹ < L` for sufficiently large `x`. -/
+  by_cases hur : u r = 0
+  · replace hur (m : ℕ) (hrm : r ≤ m) : u m = 0 := by grind [le_antisymm, h r (m - r)]
+    refine (eventually_ne_atTop 0).mono fun m hm ↦ ?_
+    rw [hur n hrn.le, Real.zero_rpow (by simpa)] at hL
+    rwa [hur (n * m + r) (r.le_add_left (n * m)), Real.zero_rpow] at ⊢
+    rw [ne_eq, inv_eq_zero, Nat.cast_eq_zero, Nat.add_eq_zero_iff, mul_eq_zero]
+    grind
+  have A : Tendsto (fun x : ℝ ↦ (u n * u r ^ x⁻¹) ^ (n + r / x)⁻¹) atTop _ :=
+    (Real.continuousAt_rpow _ (by simp [hn.pos])).tendsto.comp <|
+      (((((Real.continuous_const_rpow hur).tendsto' 0 1 (u r).rpow_zero).comp
+        tendsto_inv_atTop_zero).const_mul (u n)).prodMk_nhds
+          ((tendsto_const_nhds.add (tendsto_const_nhds.div_atTop tendsto_id)).inv₀ (by simpa)))
+  have B : Tendsto (fun x : ℝ ↦ (u n ^ x * u r) ^ (x * n + r)⁻¹) atTop (𝓝 (u n ^ (n : ℝ)⁻¹)) := by
+    rw [mul_one, add_zero] at A
+    refine A.congr' <| (eventually_ne_atTop 0).mono fun x hx ↦ ?_
+    rw [add_div' _ _ _ hx, inv_div, div_eq_mul_inv, Real.rpow_mul (by bound),
+      Real.mul_rpow (hbdd n) (by bound), Real.rpow_inv_rpow (hbdd r) hx, mul_comm _ x]
+  refine ((B.comp tendsto_natCast_atTop_atTop).eventually (gt_mem_nhds hL)).mono fun k hk ↦ ?_
+  grw [mul_comm, h.apply_mul_add_le k n r (hbdd n)]
+  · simpa using hk
+  · exact hbdd (k * n +r)
+
+/-- Fekete's lemma for nonnegative submultiplicative sequences:
+The nth roots of a submultiplicative sequence converge. -/
+theorem tendsto_lim (hbdd : ∀ n, 0 ≤ u n) : Tendsto (fun n ↦ u n ^ (n : ℝ)⁻¹) atTop (𝓝 h.lim) := by
+  refine tendsto_order.mpr ⟨fun l hl ↦ ?_, fun L hL ↦ ?_⟩
+  · exact (eventually_ne_atTop 0).mono fun n hn ↦ hl.trans_le (h.lim_le_rpow hbdd hn)
+  · obtain ⟨n, npos, hn⟩ : ∃ n : ℕ, 0 < n ∧ u n ^ (n : ℝ)⁻¹ < L := by
+      obtain ⟨-, ⟨n, hn, rfl⟩, xL⟩ := exists_lt_of_csInf_lt (by simp) hL
+      exact ⟨n, hn, xL⟩
+    exact h.eventually_rpow_lt_of_rpow_lt hbdd npos.ne' hn
+
+end Submultiplicative
+
 namespace Subadditive
 
 variable {u : ℕ → ℝ} (h : Subadditive u)
@@ -43,12 +104,14 @@ this limit is given in `Subadditive.tendsto_lim` -/
 protected def lim (_h : Subadditive u) :=
   sInf ((fun n : ℕ => u n / n) '' Ici 1)
 
+@[deprecated "No longer needed." (since := "2026-08-20")]
 theorem lim_le_div (hbdd : BddBelow (range fun n => u n / n)) {n : ℕ} (hn : n ≠ 0) :
     h.lim ≤ u n / n := by
   rw [Subadditive.lim]
   exact csInf_le (hbdd.mono <| image_subset_range _ _) ⟨n, hn.bot_lt, rfl⟩
 
 include h in
+@[deprecated "No longer needed." (since := "2026-08-20")]
 theorem apply_mul_add_le (k n r) : u (k * n + r) ≤ k * u n + u r := by
   induction k with
   | zero => simp only [Nat.cast_zero, zero_mul, zero_add]; rfl
@@ -60,6 +123,7 @@ theorem apply_mul_add_le (k n r) : u (k * n + r) ≤ k * u n + u r := by
       _ = (k + 1 : ℕ) * u n + u r := by simp; ring
 
 include h in
+@[deprecated "No longer needed." (since := "2026-08-20")]
 theorem eventually_div_lt_of_div_lt {L : ℝ} {n : ℕ} (hn : n ≠ 0) (hL : u n / n < L) :
     ∀ᶠ p in atTop, u p / p < L := by
   /- It suffices to prove the statement for each arithmetic progression `(n * · + r)`. -/
@@ -82,83 +146,23 @@ theorem eventually_div_lt_of_div_lt {L : ℝ} {n : ℕ} (hn : n ≠ 0) (hL : u n
   gcongr
   apply h.apply_mul_add_le
 
+include h in
+theorem submultiplicative_exp : Submultiplicative fun n ↦ (u n).exp :=
+  fun a b ↦ (Real.exp_le_exp_of_le (h a b)).trans_eq  (Real.exp_add (u a) (u b))
+
 /-- Fekete's lemma: a subadditive sequence which is bounded below converges. -/
 theorem tendsto_lim (hbdd : BddBelow (range fun n => u n / n)) :
     Tendsto (fun n => u n / n) atTop (𝓝 h.lim) := by
-  refine tendsto_order.2 ⟨fun l hl => ?_, fun L hL => ?_⟩
-  · refine eventually_atTop.2
-      ⟨1, fun n hn => hl.trans_le (h.lim_le_div hbdd (zero_lt_one.trans_le hn).ne')⟩
-  · obtain ⟨n, npos, hn⟩ : ∃ n : ℕ, 0 < n ∧ u n / n < L := by
-      rw [Subadditive.lim] at hL
-      rcases exists_lt_of_csInf_lt (by simp) hL with ⟨x, hx, xL⟩
-      rcases (mem_image _ _ _).1 hx with ⟨n, hn, rfl⟩
-      exact ⟨n, zero_lt_one.trans_le hn, xL⟩
-    exact h.eventually_div_lt_of_div_lt npos.ne' hn
-
-include h in
-theorem tendsto_atBot (hbdd : ¬ BddBelow (range fun n ↦ u n / n)) :
-    Tendsto (fun n ↦ u n / n) atTop atBot := by
-  simp_rw [tendsto_atTop_atBot, ← eventually_atTop]
-  intro L
-  obtain ⟨-, ⟨n, rfl⟩, hn⟩ := not_bddBelow_iff.mp hbdd (min L 0)
-  by_cases hn0 : n = 0
-  · simp [hn0] at hn
-  · exact (eventually_div_lt_of_div_lt h hn0 hn).mono (by grind)
+  have h0 n : (u n).exp ^ (n : ℝ)⁻¹ = (u n / n).exp := by rw [← Real.exp_mul, div_eq_mul_inv]
+  have key := h.submultiplicative_exp.tendsto_lim (by bound)
+  suffices h.lim.exp = h.submultiplicative_exp.lim by
+    rw [← this] at key
+    exact (key.congr h0).of_tendsto_comp (g := Real.exp) (by simp)
+  let : Inhabited (Ioi (0 : ℝ)) := ⟨1, by simp⟩
+  let : ConditionallyCompleteLinearOrder (Ioi (0 : ℝ)) :=
+    ordConnectedSubsetConditionallyCompleteLinearOrder (Ioi (0 : ℝ))
+  simp_rw [Subadditive.lim, Submultiplicative.lim, h0]
+  rw [Real.exp_monotone.map_csInf_of_continuousAt Real.continuous_exp.continuousAt
+    Nonempty.of_subtype (hbdd.mono (image_subset_range _ _)), Set.image_image]
 
 end Subadditive
-
-namespace Submultiplicative
-
-variable {u : ℕ → ℝ} (h : Submultiplicative u)
-
-/-- The limit of the nth roots of a submultiplicative sequence. The fact that the nth roots indeed
-converge to this limit is given in `Submultiplicative.tendsto_lim`. -/
-protected def lim (_h : Submultiplicative u) :=
-  sInf ((fun n : ℕ ↦ u n ^ (n : ℝ)⁻¹) '' Ici 1)
-
-/-- Fekete's lemma for nonnegative submultiplicative sequences:
-The nth roots of a submultiplicative sequence converge. -/
-theorem tendsto_lim (hbdd : ∀ n, 0 ≤ u n) : Tendsto (fun n ↦ u n ^ (n : ℝ)⁻¹) atTop (𝓝 h.lim) := by
-  by_cases! hu : ∃ n, u n ≤ 0
-  · obtain ⟨n, hu⟩ := hu
-    replace hu m (hm : m ≥ n) : u m = 0 := by grind [le_antisymm, h n (m - n)]
-    have h0 : n + 1 ≠ (0 : ℝ) := by grind
-    have h1 : h.lim = 0 := by
-      rw [Submultiplicative.lim]
-      refine csInf_eq_of_forall_ge_of_forall_gt_exists_lt ⟨0, n + 1, by simp, by simp [hu, h0]⟩ ?_
-        fun _ _ ↦ ⟨u (n + 1) ^ (n + 1 : ℝ)⁻¹, ⟨n + 1, by simp⟩, by simpa [hu, h0]⟩
-      rintro - ⟨n, hn, rfl⟩
-      positivity [hbdd n]
-    apply tendsto_nhds_of_eventually_eq
-    rw [eventually_atTop, h1]
-    refine ⟨n + 1, fun m hm ↦ ?_⟩
-    simp [hu m (by grind), show m ≠ 0 by grind]
-  · have key : Subadditive fun n ↦ (u n).log :=
-      fun a b ↦ (Real.log_le_log (hu (a + b)) (h a b)).trans_eq (Real.log_mul (hu a).ne' (hu b).ne')
-    have h0 n : u n ^ (n : ℝ)⁻¹ = ((u n).log / n).exp := by
-      rw [Real.rpow_def_of_pos (hu n), Real.exp_eq_exp, div_eq_mul_inv]
-    simp_rw [h0]
-    by_cases h' : BddBelow (range fun n ↦ (u n).log / n)
-    · suffices h.lim = key.lim.exp by
-        rw [this]
-        exact Real.continuous_exp.continuousAt.tendsto.comp (key.tendsto_lim h')
-      let : Inhabited (Ioi (0 : ℝ)) := ⟨1, by simp⟩
-      let : ConditionallyCompleteLinearOrder (Ioi (0 : ℝ)) :=
-        ordConnectedSubsetConditionallyCompleteLinearOrder (Ioi (0 : ℝ))
-      simp_rw [Subadditive.lim, Submultiplicative.lim, h0]
-      rw [Real.exp_monotone.map_csInf_of_continuousAt Real.continuous_exp.continuousAt
-        Nonempty.of_subtype (h'.mono (image_subset_range _ _)), Set.image_image]
-    · suffices h.lim = 0 by
-        rw [this]
-        exact Real.tendsto_exp_atBot.comp (key.tendsto_atBot h')
-      simp_rw [Submultiplicative.lim, h0]
-      apply csInf_eq_of_forall_ge_of_forall_gt_exists_lt Nonempty.of_subtype
-      · rintro - ⟨n, hn, rfl⟩
-        positivity
-      · intro ε hε
-        obtain ⟨-, ⟨n, rfl⟩, hn⟩ := (not_bddBelow_iff.mp h') (min 0 ε.log)
-        refine exists_mem_image.mpr ⟨n, (lt_inf_iff.mp hn).imp ?_ (Real.lt_log_iff_exp_lt hε).mp⟩
-        contrapose!
-        simp +contextual
-
-end Submultiplicative
