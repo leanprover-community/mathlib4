a56f15137bee5018d1f3280f8f398c4f7454f45d
tb65536 <thomas.l.browning@gmail.com>
2026-08-21 14:23:26 +0100

    rm

--- Mathlib/Analysis/Matrix/Normed.lean
+++ Mathlib/Analysis/Matrix/Normed.lean
@@ -331,7 +331,6 @@ theorem linfty_opNorm_blockDiagonal [DecidableEq l] (M : l → Matrix m n α) :
     ‖blockDiagonal M‖ = ‖M‖ :=
   congr_arg ((↑) : ℝ≥0 → ℝ) <| linfty_opNNNorm_blockDiagonal M
 
-
 end SeminormedAddCommGroup
 
 section NonUnitalSeminormedRing
