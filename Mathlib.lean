03fece463e60921394b04da8e35eaf132e4e1ad8
David Loeffler <d.loeffler.01@cantab.net>
2026-08-21 17:12:09 +0200

    refactor(MeasureTheory/Integral): split SetToL1.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -5591,6 +5591,11 @@ public import Mathlib.MeasureTheory.Integral.RieszMarkovKakutani.Basic
 public import Mathlib.MeasureTheory.Integral.RieszMarkovKakutani.NNReal
 public import Mathlib.MeasureTheory.Integral.RieszMarkovKakutani.Real
 public import Mathlib.MeasureTheory.Integral.SetToL1
+public import Mathlib.MeasureTheory.Integral.SetToL1.ChangeMeasure
+public import Mathlib.MeasureTheory.Integral.SetToL1.DominatedConvergence
+public import Mathlib.MeasureTheory.Integral.SetToL1.Function
+public import Mathlib.MeasureTheory.Integral.SetToL1.L1
+public import Mathlib.MeasureTheory.Integral.SetToL1.SimpleFunc
 public import Mathlib.MeasureTheory.Integral.TorusIntegral
 public import Mathlib.MeasureTheory.MeasurableSpace.Basic
 public import Mathlib.MeasureTheory.MeasurableSpace.Card
