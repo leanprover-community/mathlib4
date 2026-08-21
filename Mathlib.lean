415e632953ce0ca682683da082a417f642fdc5e0
Joël Riou <37772949+joelriou@users.noreply.github.com>
2026-08-21 09:34:11 +0000

    feat(CategoryTheory/Presentable): the uniformization theorem (#31018)
    The main result in this PR is `IsCardinalAccessibleCategory.uniformization` which says that if `F : C ⥤ D` is an accessible functor between accessible categories, there exists a regular cardinal `κ` such that `C` and `D` are `κ`-accessible categories, and `F` is a `κ`-accessible functor which preserves `κ`-presentable objects.

--- Mathlib.lean
+++ Mathlib.lean
@@ -3335,6 +3335,7 @@ public import Mathlib.CategoryTheory.Presentable.SharplyLT.Basic
 public import Mathlib.CategoryTheory.Presentable.SharplyLT.Lemmas
 public import Mathlib.CategoryTheory.Presentable.StrongGenerator
 public import Mathlib.CategoryTheory.Presentable.Type
+public import Mathlib.CategoryTheory.Presentable.Uniformization
 public import Mathlib.CategoryTheory.Products.Associator
 public import Mathlib.CategoryTheory.Products.Basic
 public import Mathlib.CategoryTheory.Products.Bifunctor
