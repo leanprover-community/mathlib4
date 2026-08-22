32574675acc0a01ded658d921002e45b6f8e1d2c
David Loeffler <d.loeffler.01@cantab.net>
2026-08-22 09:06:15 +0200

    chore(Data/Set): deprecation stub for PR 43030

---
+++ Mathlib/Data/Set/Lattice.lean
@@ -0,0 +1,9 @@
+module
+
+public import Mathlib.Data.Set.Lattice.Bounded
+public import Mathlib.Data.Set.Lattice.Disjoint
+public import Mathlib.Data.Set.Lattice.Image
+public import Mathlib.Data.Set.Lattice.Indexed
+public import Mathlib.Data.Set.Lattice.Order
+
+deprecated_module (since := "2026-08-22")
