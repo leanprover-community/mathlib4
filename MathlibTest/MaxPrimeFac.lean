faae8395c2d3ec5a627210d68ad32288e411404b
Bo Cowgill <bo.cowgill@gmail.com>
2026-08-21 22:28:26 -0600

    refactor(Data/Nat/MaxFac): simplify greatest prime factor API

--- MathlibTest/MaxPrimeFac.lean
+++
@@ -1,8 +0,0 @@
-import Mathlib.Data.Nat.MaxPrimeFac
-
-#guard Nat.maxPrimeFac 0 = 0
-#guard Nat.maxPrimeFac 1 = 1
-#guard Nat.maxPrimeFac 12 = 3
-#guard Nat.maxPrimeFac 97 = 97
-#guard Nat.maxPrimeFac 125 = 5
-#guard Nat.maxPrimeFac 360 = 5
