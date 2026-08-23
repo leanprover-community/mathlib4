8601a1b4bbed83b07c54cce2d0bdbfbc9e58e918
Aaron Liu <aaronliu2008@outlook.com>
2026-08-22 21:07:01 -0400

    docstring hide coercion again

--- Mathlib/Data/PFunctor/Univariate/Basic.lean
+++ Mathlib/Data/PFunctor/Univariate/Basic.lean
@@ -61,7 +61,7 @@ of the definitional equality between `P α` and `Σ x : P.A, P.B x → α`. -/
 @[implicit_reducible, match_pattern]
 def Obj.mk (a : P.A) (f : P.B a → α) : P α := ⟨a, f⟩
 
-/-- To prove a theorem about `t : P.Obj α` it suffices to
+/-- To prove a theorem about `t : P α` it suffices to
 prove it for `P.Obj.mk a f` for all possible values of `a` and `f`. -/
 @[implicit_reducible, elab_as_elim, induction_eliminator, cases_eliminator]
 def Obj.rec {motive : P α → Sort*} (mk : ∀ a f, motive (.mk a f)) : ∀ t, motive t :=
