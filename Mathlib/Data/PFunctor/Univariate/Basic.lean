d8bee3699b7d0cca3b7158d27ffdec2b0128bba4
Aaron Liu <aaronliu2008@outlook.com>
2026-08-22 21:14:13 -0400

    long line

--- Mathlib/Data/PFunctor/Univariate/Basic.lean
+++ Mathlib/Data/PFunctor/Univariate/Basic.lean
@@ -56,8 +56,8 @@ variable {P}
 
 /-- Make an element of `P α` from a "shape" `a : P.A` and a family of elements `f : P.B a → α`.
 
-Important: You should use `PFunctor.Obj.mk` instead of the anonymous constructor `⟨_, _⟩` to avoid abuse
-of the definitional equality between `P α` and `Σ x : P.A, P.B x → α`. -/
+Important: You should use `PFunctor.Obj.mk` instead of the anonymous constructor `⟨_, _⟩`
+to avoid abuse of the definitional equality between `P α` and `Σ x : P.A, P.B x → α`. -/
 @[implicit_reducible, match_pattern]
 def Obj.mk (a : P.A) (f : P.B a → α) : P α := ⟨a, f⟩
 
