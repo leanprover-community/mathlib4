0f268c70e4412c1f55383b7cd1269f11fe7275fd
Remy Degenne <remydegenne@gmail.com>
2026-08-24 12:57:03 +0200

    review

--- Mathlib/Topology/MetricSpace/CoveringExponent.lean
+++ Mathlib/Topology/MetricSpace/CoveringExponent.lean
@@ -52,9 +52,7 @@ lemma HasCoveringExponent.coveringNumber_lt_top (h : HasCoveringExponent A c d) 
   · suffices (coveringNumber ε A : ℝ≥0∞) < ∞ by norm_cast at this
     calc (coveringNumber ε A : ℝ≥0∞)
     _ ≤ c * (ε : ℝ≥0∞)⁻¹ ^ d := h.coveringNumber_le _ hε_le
-    _ < ∞ := by
-      refine ENNReal.mul_lt_top hc.lt_top ?_
-      exact ENNReal.rpow_lt_top_of_nonneg hd (by simp [hε_ne])
+    _ < ∞ := by finiteness
   · calc coveringNumber ε A
     _ ≤ 1 := coveringNumber_le_one_of_ediam_le (not_le.mp hε_le).le
     _ < ⊤ := by simp
@@ -65,32 +63,22 @@ lemma HasCoveringExponent.coveringNumber_ne_top (h : HasCoveringExponent A c d) 
   (h.coveringNumber_lt_top hε_ne hc hd).ne
 
 lemma HasCoveringExponent.subset (h : HasCoveringExponent A c d) (hBA : B ⊆ A) (hd : 0 ≤ d) :
-    HasCoveringExponent B (2 ^ d * c) d := by
-  constructor
-  · exact lt_of_le_of_lt (Metric.ediam_mono hBA) h.ediam_lt_top
-  intro ε hε_le
-  by_cases hdA : d = 0 ∧ Metric.ediam A = ∞
-  · simp only [hdA.1, ENNReal.rpow_zero, one_mul, mul_one]
-    replace h := h.coveringNumber_le 0 (by simp)
-    simp only [hdA.1, ENNReal.rpow_zero, mul_one] at h
+    HasCoveringExponent B (2 ^ d * c) d where
+  ediam_lt_top := lt_of_le_of_lt (Metric.ediam_mono hBA) h.ediam_lt_top
+  coveringNumber_le ε hε_le :=
     calc (coveringNumber ε B : ℝ≥0∞)
-    _ ≤ coveringNumber 0 B := mod_cast coveringNumber_anti zero_le
-    _ ≤ coveringNumber (0 / 2) A := mod_cast coveringNumber_subset_le hBA
-    _ = coveringNumber 0 A := by simp
-    _ ≤ c := h
-  push Not at hdA
-  calc (coveringNumber ε B : ℝ≥0∞)
-  _ ≤ coveringNumber (ε / 2) A := mod_cast coveringNumber_subset_le hBA
-  _ ≤ c * (ε / 2 : ℝ≥0∞)⁻¹ ^ d := by
-    replace h := h.coveringNumber_le (ε / 2) ?_
-    · simpa using h
-    · simp only [ne_eq, OfNat.ofNat_ne_zero, not_false_eq_true, ENNReal.coe_div, ENNReal.coe_ofNat]
-      calc (ε / 2 : ℝ≥0∞) ≤ ε := ENNReal.half_le_self
-      _ ≤ Metric.ediam B := hε_le
-      _ ≤ Metric.ediam A := Metric.ediam_mono hBA
-  _ = 2 ^ d * c * (ε : ℝ≥0∞)⁻¹ ^ d := by
-    rw [div_eq_mul_inv, ENNReal.mul_inv (by simp) (by simp), inv_inv,
-      ENNReal.mul_rpow_of_nonneg _ _ hd]
-    ring
+    _ ≤ coveringNumber (ε / 2) A := mod_cast coveringNumber_subset_le hBA
+    _ ≤ c * (ε / 2 : ℝ≥0∞)⁻¹ ^ d := by
+      replace h := h.coveringNumber_le (ε / 2) ?_
+      · simpa using h
+      · simp only [ne_eq, OfNat.ofNat_ne_zero, not_false_eq_true, ENNReal.coe_div,
+          ENNReal.coe_ofNat]
+        calc (ε / 2 : ℝ≥0∞) ≤ ε := ENNReal.half_le_self
+        _ ≤ Metric.ediam B := hε_le
+        _ ≤ Metric.ediam A := Metric.ediam_mono hBA
+    _ = 2 ^ d * c * (ε : ℝ≥0∞)⁻¹ ^ d := by
+      rw [div_eq_mul_inv, ENNReal.mul_inv (by simp) (by simp), inv_inv,
+        ENNReal.mul_rpow_of_nonneg _ _ hd]
+      ring
 
 end Metric
