fd465a11fced85b1402afebda9a7241e6b291eed
Remy Degenne <remydegenne@gmail.com>
2026-08-24 10:19:50 +0200

    use to_fun

--- Mathlib/MeasureTheory/Order/Group/Lattice.lean
+++ Mathlib/MeasureTheory/Order/Group/Lattice.lean
@@ -28,23 +28,13 @@ variable [DivInvMonoid α] [MeasurableSup α]
 theorem measurable_oneLePart : Measurable (oneLePart : α → α) :=
   measurable_sup_const _
 
-@[to_additive (attr := fun_prop)]
+@[to_fun (attr := to_additive (attr := fun_prop))]
 protected theorem Measurable.oneLePart (hf : Measurable f) :
-    Measurable fun x ↦ (f x)⁺ᵐ :=
-  measurable_oneLePart.comp hf
+    Measurable f⁺ᵐ := measurable_oneLePart.comp hf
 
-@[to_additive (attr := fun_prop)]
-protected theorem Measurable.oneLePart' (hf : Measurable f) :
-    Measurable f⁺ᵐ := hf.oneLePart
-
-@[to_additive (attr := fun_prop)]
+@[to_fun (attr := to_additive (attr := fun_prop))]
 protected theorem AEMeasurable.oneLePart {μ : MeasureTheory.Measure β} (hf : AEMeasurable f μ) :
-    AEMeasurable (fun x ↦ (f x)⁺ᵐ) μ :=
-  hf.sup_const 1
-
-@[to_additive (attr := fun_prop)]
-protected theorem AEMeasurable.oneLePart' {μ : MeasureTheory.Measure β} (hf : AEMeasurable f μ) :
-    AEMeasurable f⁺ᵐ μ := hf.oneLePart
+    AEMeasurable f⁺ᵐ μ := hf.sup_const 1
 
 variable [MeasurableInv α]
 
@@ -52,23 +42,13 @@ variable [MeasurableInv α]
 theorem measurable_leOnePart : Measurable (leOnePart : α → α) :=
   (measurable_sup_const _).comp measurable_inv
 
-@[to_additive (attr := fun_prop)]
+@[to_fun (attr := to_additive (attr := fun_prop))]
 protected theorem Measurable.leOnePart (hf : Measurable f) :
-    Measurable fun x ↦ (f x)⁻ᵐ :=
-  measurable_leOnePart.comp hf
+    Measurable f⁻ᵐ := measurable_leOnePart.comp hf
 
-@[to_additive (attr := fun_prop)]
-protected theorem Measurable.leOnePart' (hf : Measurable f) :
-    Measurable f⁻ᵐ := hf.leOnePart
-
-@[to_additive (attr := fun_prop)]
+@[to_fun (attr := to_additive (attr := fun_prop))]
 protected theorem AEMeasurable.leOnePart {μ : MeasureTheory.Measure β} (hf : AEMeasurable f μ) :
-    AEMeasurable (fun x ↦ (f x)⁻ᵐ) μ :=
-  hf.inv.sup_const 1
-
-@[to_additive (attr := fun_prop)]
-protected theorem AEMeasurable.leOnePart' {μ : MeasureTheory.Measure β} (hf : AEMeasurable f μ) :
-    AEMeasurable f⁻ᵐ μ := hf.leOnePart
+    AEMeasurable f⁻ᵐ μ := hf.inv.sup_const 1
 
 end DivInvMonoid
 
