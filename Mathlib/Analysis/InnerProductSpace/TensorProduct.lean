6166858a84e83fd6c319f70260205787cc99b9e5
TJHeeringa <heeringa@protonmail.ch>
2026-08-24 14:32:54 +0200

    Revert line-removal next to imports

--- Mathlib/Analysis/InnerProductSpace/TensorProduct.lean
+++ Mathlib/Analysis/InnerProductSpace/TensorProduct.lean
@@ -10,6 +10,7 @@ public import Mathlib.RingTheory.TensorProduct.Finite
 import Mathlib.Analysis.InnerProductSpace.GramMatrix
 import Mathlib.Analysis.InnerProductSpace.Positive
 import Mathlib.Algebra.Order.Star.Real
+
 /-!
 
 # Inner product space structure on tensor product spaces
