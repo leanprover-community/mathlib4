e4259f8c9a791ecb74418e16f8027f00ec92d4e6
TJHeeringa <heeringa@protonmail.ch>
2026-08-25 10:56:20 +0200

    Add docstring for .toLinearEquivMonoidHom

--- Mathlib/Analysis/InnerProductSpace/TensorProduct.lean
+++ Mathlib/Analysis/InnerProductSpace/TensorProduct.lean
@@ -773,6 +773,8 @@ lemma _root_.ContinuousLinearEquiv.toLinearEquiv_inv (f : E ≃L[𝕜] E) :
 lemma _root_.ContinuousLinearEquiv.toLinearEquiv_mul (f g : E ≃L[𝕜] E) :
     (f * g).toLinearEquiv = f * g := rfl
 
+/-- `ContinuousLinearEquiv.toLinearEquiv` as a multiplicative monoid homomorphism `MonoidHom`. -/
+@[simps]
 def _root_.ContinuousLinearEquiv.toLinearEquivMonoidHom : (E ≃L[𝕜] E) →* (E ≃ₗ[𝕜] E) where
   toFun := ContinuousLinearEquiv.toLinearEquiv
   map_one' := rfl
