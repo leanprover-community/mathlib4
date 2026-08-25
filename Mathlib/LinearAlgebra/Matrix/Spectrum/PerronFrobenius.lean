6f19d4e787d49d25d77109fcbd9462faf81b90e5
Michail Karatarakis <40603357+mkaratarakis@users.noreply.github.com>
2026-08-25 16:50:13 +0200

    update info

--- Mathlib/LinearAlgebra/Matrix/Spectrum/PerronFrobenius.lean
+++ Mathlib/LinearAlgebra/Matrix/Spectrum/PerronFrobenius.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2025 Matteo Cipollina. All rights reserved.
+Copyright (c) 2025 Matteo Cipollina, Michail Karatarakis. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Matteo Cipollina, Michail Karatarakis
 -/
