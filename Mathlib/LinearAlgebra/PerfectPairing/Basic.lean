75567c8587bebf78b01fcdaf6cea8c9a66baca4c
Monica Omar <23701951+themathqueen@users.noreply.github.com>
2026-08-26 19:16:49 +0100

    Remove unused variable in Field section
    Removed unused variable 'x' from the section on Fields.

--- Mathlib/LinearAlgebra/PerfectPairing/Basic.lean
+++ Mathlib/LinearAlgebra/PerfectPairing/Basic.lean
@@ -109,7 +109,7 @@ end CommSemiring
 
 section Field
 variable {K M N : Type*} [Field K] [AddCommGroup M] [AddCommGroup N]
-  [Module K M] [Module K N] {p : M →ₗ[K] N →ₗ[K] K} {x : M} {y : N}
+  [Module K M] [Module K N] {p : M →ₗ[K] N →ₗ[K] K}
 
 /-- If the coefficients are a field, and one of the spaces is finite-dimensional, it is sufficient
 to check only injectivity instead of bijectivity of the bilinear pairing. -/
