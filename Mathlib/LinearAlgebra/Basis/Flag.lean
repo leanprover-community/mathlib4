8055267b9b00b56dabf9618302c1357efc154c46
Matteo Cipollina <matteo.cipollina@yahoo.it>
2026-08-26 17:26:29 +0200

    Update Flag.lean

--- Mathlib/LinearAlgebra/Basis/Flag.lean
+++ Mathlib/LinearAlgebra/Basis/Flag.lean
@@ -8,7 +8,6 @@ module
 public import Mathlib.Data.Fin.FlagRange
 public import Mathlib.LinearAlgebra.Basis.Fin
 public import Mathlib.LinearAlgebra.Dual.Basis
-public import Mathlib.Tactic.NormNum.Basic
 
 /-!
 # Flag of submodules defined by a basis
