7ebb324153a24e274d54750c7080bdb1ff3eb32a
Raph-DG <raphaeldouglasgiles@gmail.com>
2026-08-26 13:21:06 +0000

    Stopped using deprecated lemma

--- Mathlib/Topology/KrullDimension.lean
+++ Mathlib/Topology/KrullDimension.lean
@@ -104,7 +104,7 @@ def coheightZeroSetOrderIsoIrreducibleComponents [QuasiSober X] [T0Space X] :
     { Equiv.subtypeUnivEquiv fun _ ↦ trivial with map_rel_iff' := Iff.rfl }
   have : {x : X | coheight x = 0} = {x : X | Maximal ⊤ x} := by simp [maximal_true_iff_isMax]
   rw [irreducibleComponents_eq_maximals_closed, this]
-  exact OrderIso.mapSetOfMaximal <| OrderIso.trans
+  exact OrderIso.mapSetOfPredMaximal <| OrderIso.trans
     (OrderIso.trans univIso (irreducibleSetEquivPoints (α := X)).symm) <|
     TopologicalSpace.IrreducibleCloseds.orderIsoSubtype' X
 
