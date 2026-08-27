8a01ebaa95b4829074de5fb0b212b6c6caec6e06
NoahW314 <noahwalker3.14@gmail.com>
2026-08-26 23:37:31 -0500

    divisible R-modules over a PID are injective

--- Mathlib/Algebra/Category/Grp/Injective.lean
+++ Mathlib/Algebra/Category/Grp/Injective.lean
@@ -32,19 +32,6 @@ universe u
 
 variable (A : Type u) [AddCommGroup A]
 
-theorem Module.Baer.of_divisible [DivisibleBy A ℤ] : Module.Baer ℤ A := fun I g ↦ by
-  rcases IsPrincipalIdealRing.principal I with ⟨m, rfl⟩
-  obtain rfl | h0 := eq_or_ne m 0
-  · refine ⟨0, fun n hn ↦ ?_⟩
-    rw [Submodule.span_zero_singleton] at hn
-    subst hn
-    exact (map_zero g).symm
-  let gₘ := g ⟨m, Submodule.subset_span (Set.mem_singleton _)⟩
-  refine ⟨LinearMap.toSpanSingleton ℤ A (DivisibleBy.div gₘ m), fun n hn ↦ ?_⟩
-  rcases Submodule.mem_span_singleton.mp hn with ⟨n, rfl⟩
-  rw [map_zsmul, LinearMap.toSpanSingleton_apply, DivisibleBy.div_cancel gₘ h0, ← map_zsmul g,
-    SetLike.mk_smul_mk]
-
 namespace AddCommGrpCat
 
 theorem injective_as_module_iff : Injective (ModuleCat.of ℤ A) ↔
@@ -54,6 +41,6 @@ theorem injective_as_module_iff : Injective (ModuleCat.of ℤ A) ↔
 instance injective_of_divisible [DivisibleBy A ℤ] :
     Injective (C := AddCommGrpCat) (AddCommGrpCat.of A) :=
   (injective_as_module_iff A).mp <|
-    Module.injective_object_of_injective_module (inj := (Module.Baer.of_divisible A).injective)
+    Module.injective_object_of_injective_module (inj := (Module.Baer.of_divisible ℤ A).injective)
 
 end AddCommGrpCat
