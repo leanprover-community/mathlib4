d8883c4071cd0b10c9bcfb6b9fee2c6889c564b2
WenrongZou <141128015+WenrongZou@users.noreply.github.com>
2026-08-27 18:24:39 +0800

    fix

--- Mathlib/RingTheory/MvPowerSeries/Expand.lean
+++ Mathlib/RingTheory/MvPowerSeries/Expand.lean
@@ -260,7 +260,7 @@ theorem _root_.FiniteField.MvPowerSeries.expand_card {K : Type*} [Field K] [Fint
     f.expand (Fintype.card K) Fintype.card_ne_zero = f ^ (Fintype.card K) := by
   obtain ⟨p, hp⟩ := CharP.exists K
   rcases FiniteField.card K p with ⟨⟨n, npos⟩, ⟨hp, hn⟩⟩
-  haveI : Fact p.Prime := ⟨hp⟩
+  have : Fact p.Prime := ⟨hp⟩
   simp_rw [hn]
   rw [← MvPowerSeries.map_iterateFrobenius_expand _ (NeZero.ne' p).symm, iterateFrobenius_eq_pow,
     FiniteField.frobenius_pow hn, RingHom.one_def, MvPowerSeries.map_id, RingHom.id_apply]
