1b647cbac0b7899f32f3a7f25db27aa7953f47e3
Brian-Nugent <bnugent@uw.edu>
2026-08-28 12:06:16 -0600

    Refactor Monoidal.lean

--- Mathlib/Algebra/Category/ModuleCat/Presheaf/Monoidal.lean
+++ Mathlib/Algebra/Category/ModuleCat/Presheaf/Monoidal.lean
@@ -5,7 +5,7 @@ Authors: Dagur Asgeirsson, Jack McKoen, Joël Riou
 -/
 module
 
-public import Mathlib.Algebra.Category.ModuleCat.Presheaf.Colimits
+public import Mathlib.Algebra.Category.ModuleCat.Presheaf.OfCommRing
 public import Mathlib.Algebra.Category.ModuleCat.Monoidal.Closed
 
 /-!
@@ -13,7 +13,7 @@ public import Mathlib.Algebra.Category.ModuleCat.Monoidal.Closed
 
 Given a presheaf of commutative rings `R : Cᵒᵖ ⥤ CommRingCat`, we construct
 the monoidal category structure on the category of presheaves of modules
-`PresheafOfModules (R ⋙ forget₂ _ _)`. The tensor product `M₁ ⊗ M₂` is defined
+`PresheafOfModulesOfCommRing R`. The tensor product `M₁ ⊗ M₂` is defined
 as the presheaf of modules which sends `X : Cᵒᵖ` to `M₁.obj X ⊗ M₂.obj X`.
 
 ## Notes
@@ -31,14 +31,11 @@ universe v u v₁ u₁
 
 variable {C : Type*} [Category* C] {R : Cᵒᵖ ⥤ CommRingCat.{u}}
 
-instance (X : Cᵒᵖ) : CommRing ((R ⋙ forget₂ _ RingCat).obj X) :=
-  inferInstanceAs (CommRing (R.obj X))
-
-namespace PresheafOfModules
+namespace PresheafOfModulesOfCommRing
 
 namespace Monoidal
 
-variable (M₁ M₂ M₃ M₄ : PresheafOfModules.{u} (R ⋙ forget₂ _ _))
+variable (M₁ M₂ M₃ M₄ : PresheafOfModulesOfCommRing.{u} R)
 
 set_option backward.isDefEq.respectTransparency false in
 /-- Auxiliary definition for `tensorObj`. -/
@@ -60,18 +57,17 @@ set_option backward.defeqAttrib.useBackward true in
 set_option backward.isDefEq.respectTransparency false in
 /-- The tensor product of two presheaves of modules. -/
 @[simps obj]
-noncomputable def tensorObj : PresheafOfModules (R ⋙ forget₂ _ _) where
-  obj X := M₁.obj X ⊗ M₂.obj X
-  map f := tensorObjMap M₁ M₂ f
-  map_id X := ModuleCat.MonoidalCategory.tensor_ext (by
-    intro m₁ m₂
-    dsimp [tensorObjMap]
-    simp
-    rfl) -- `ModuleCat.restrictScalarsId'App_inv_apply` doesn't get picked up due to type mismatch
-  map_comp f g := ModuleCat.MonoidalCategory.tensor_ext (by
-    intro m₁ m₂
-    dsimp [tensorObjMap]
-    simp +instances)
+noncomputable def tensorObj : PresheafOfModulesOfCommRing R :=
+  mk (fun X ↦ M₁.obj X ⊗ M₂.obj X)
+    (fun f ↦ tensorObjMap M₁ M₂ f)
+    (fun X ↦ ModuleCat.MonoidalCategory.tensor_ext (by
+      intro m₁ m₂
+      dsimp [tensorObjMap]
+      simp))
+    (fun f g ↦ ModuleCat.MonoidalCategory.tensor_ext (by
+      intro m₁ m₂
+      dsimp [tensorObjMap]
+      simp +instances))
 
 variable {M₁ M₂ M₃ M₄}
 
@@ -86,23 +82,23 @@ set_option backward.defeqAttrib.useBackward true in
 set_option backward.isDefEq.respectTransparency false in
 /-- The tensor product of two morphisms of presheaves of modules. -/
 @[simps]
-noncomputable def tensorHom (f : M₁ ⟶ M₂) (g : M₃ ⟶ M₄) : tensorObj M₁ M₃ ⟶ tensorObj M₂ M₄ where
-  app X := f.app X ⊗ₘ g.app X
-  naturality {X Y} φ := ModuleCat.MonoidalCategory.tensor_ext (fun m₁ m₃ ↦ by
-    dsimp
-    rw [tensorObj_map_tmul]
-    -- Need `erw` because of the type mismatch in `map` and the tensor product.
-    erw [ModuleCat.MonoidalCategory.tensorHom_tmul, tensorObj_map_tmul]
-    rw [naturality_apply, naturality_apply]
-    simp)
+noncomputable def tensorHom (f : M₁ ⟶ M₂) (g : M₃ ⟶ M₄) :
+    tensorObj M₁ M₃ ⟶ tensorObj M₂ M₄ :=
+  mkHom (fun X ↦ f.app X ⊗ₘ g.app X)
+    (fun φ ↦ ModuleCat.MonoidalCategory.tensor_ext (fun m₁ m₃ ↦ by
+      dsimp
+      rw [tensorObj_map_tmul]
+      -- Need `erw` because of the type mismatch in `map` and the tensor product.
+      erw [ModuleCat.MonoidalCategory.tensorHom_tmul, tensorObj_map_tmul]
+      erw [PresheafOfModules.naturality_apply, PresheafOfModules.naturality_apply]
+      simp))
 
 end Monoidal
 
 open Monoidal
 
-open ModuleCat.MonoidalCategory in
 noncomputable instance monoidalCategoryStruct :
-    MonoidalCategoryStruct (PresheafOfModules.{u} (R ⋙ forget₂ _ _)) where
+    MonoidalCategoryStruct (PresheafOfModulesOfCommRing.{u} R) where
   tensorObj := tensorObj
   whiskerLeft _ _ _ g := tensorHom (𝟙 _) g
   whiskerRight f _ := tensorHom f (𝟙 _)
@@ -113,16 +109,18 @@ noncomputable instance monoidalCategoryStruct :
   leftUnitor M := Iso.symm (isoMk (fun _ ↦ (λ_ _).symm) (fun X Y f ↦ by
     ext m
     dsimp [CommRingCat.forgetToRingCat_obj]
-    erw [leftUnitor_inv_apply, leftUnitor_inv_apply, tensorObj_map_tmul, (R.map f).hom.map_one]
+    erw [ModuleCat.MonoidalCategory.leftUnitor_inv_apply,
+      ModuleCat.MonoidalCategory.leftUnitor_inv_apply, tensorObj_map_tmul, (R.map f).hom.map_one]
     rfl))
   rightUnitor M := Iso.symm (isoMk (fun _ ↦ (ρ_ _).symm) (fun X Y f ↦ by
     ext m
     dsimp [CommRingCat.forgetToRingCat_obj]
-    erw [rightUnitor_inv_apply, rightUnitor_inv_apply, tensorObj_map_tmul, (R.map f).hom.map_one]
+    erw [ModuleCat.MonoidalCategory.rightUnitor_inv_apply,
+      ModuleCat.MonoidalCategory.rightUnitor_inv_apply, tensorObj_map_tmul, (R.map f).hom.map_one]
     rfl))
 
 noncomputable instance monoidalCategory :
-    MonoidalCategory (PresheafOfModules.{u} (R ⋙ forget₂ _ _)) where
+    MonoidalCategory (PresheafOfModulesOfCommRing.{u} R) where
   tensorHom_def _ _ := by ext1; apply tensorHom_def
   id_tensorHom_id _ _ := by ext1; apply id_tensorHom_id
   tensorHom_comp_tensorHom _ _ _ _ := by ext1; apply tensorHom_comp_tensorHom
@@ -141,7 +139,7 @@ noncomputable instance monoidalCategory :
 open BraidedCategory
 
 noncomputable instance symmetricCategory :
-    SymmetricCategory (PresheafOfModules.{u} (R ⋙ forget₂ _ _)) where
+    SymmetricCategory (PresheafOfModulesOfCommRing.{u} R) where
   braiding M₁ M₂ :=
     isoMk (fun X ↦ braiding (C := ModuleCat (R.obj X)) (M₁.obj X) (M₂.obj X))
       (fun _ _ f ↦ ModuleCat.MonoidalCategory.tensor_ext (fun _ _ ↦ rfl))
@@ -163,7 +161,7 @@ noncomputable instance symmetricCategory :
 
 section
 
-variable (M₁ M₂ M₃ M₄ : PresheafOfModules.{u} (R ⋙ forget₂ _ _))
+variable (M₁ M₂ M₃ M₄ : PresheafOfModulesOfCommRing.{u} R)
 
 lemma tensorObj_obj (X : Cᵒᵖ) :
     (M₁ ⊗ M₂).obj X =
@@ -179,7 +177,8 @@ lemma whiskerLeft_app (f : M₂ ⟶ M₃) (X : Cᵒᵖ) :
 
 variable {M₁ M₂} in
 @[simp]
-lemma whiskerRight_app (f : M₁ ⟶ M₂) (M₃ : PresheafOfModules.{u} (R ⋙ forget₂ _ _)) (X : Cᵒᵖ) :
+lemma whiskerRight_app (f : M₁ ⟶ M₂) (M₃ : PresheafOfModulesOfCommRing.{u} R)
+    (X : Cᵒᵖ) :
     dsimp% (f ▷ M₃).app X = whiskerRight (C := ModuleCat (R.obj X)) (f.app X) (M₃.obj X) := rfl
 
 variable {M₁ M₂ M₃ M₄} in
@@ -233,14 +232,14 @@ lemma braiding_inv_app (X : Cᵒᵖ) :
 
 end
 
-instance (F : PresheafOfModules.{u} (R ⋙ forget₂ _ _)) :
+instance (F : PresheafOfModulesOfCommRing.{u} R) :
     PreservesColimitsOfSize.{u, u} (tensorLeft F) where
-  preservesColimitsOfShape := ⟨⟨fun hc ↦ ⟨evaluationJointlyReflectsColimits _ _
+  preservesColimitsOfShape := ⟨⟨fun hc ↦ ⟨PresheafOfModules.evaluationJointlyReflectsColimits _ _
       (fun X ↦ isColimitOfPreserves (tensorLeft (show ModuleCat (R.obj X) from F.obj X))
-        (isColimitOfPreserves (evaluation _ X) hc))⟩⟩⟩
+        (isColimitOfPreserves (PresheafOfModules.evaluation _ X) hc))⟩⟩⟩
 
-instance (F : PresheafOfModules.{u} (R ⋙ forget₂ _ _)) :
+instance (F : PresheafOfModulesOfCommRing.{u} R) :
     PreservesColimitsOfSize.{u, u} (tensorRight F) :=
   preservesColimits_of_natIso (tensorLeftIsoTensorRight F)
 
-end PresheafOfModules
+end PresheafOfModulesOfCommRing
