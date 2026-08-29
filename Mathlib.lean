5de7817f2c4bc97cc057d6bd74bbfa79ad84f0b8
Joël Riou <37772949+joelriou@users.noreply.github.com>
2026-08-29 18:09:14 +0000

    feat(CategoryTheory/Presentable): comma categories are `κ`-accessible (#41068)
    This is one of the main technical results about accessible categories.

--- Mathlib.lean
+++ Mathlib.lean
@@ -3336,6 +3336,7 @@ public import Mathlib.CategoryTheory.Presentable.CardinalDirectedPoset
 public import Mathlib.CategoryTheory.Presentable.CardinalFilteredPresentation
 public import Mathlib.CategoryTheory.Presentable.CardinalPure
 public import Mathlib.CategoryTheory.Presentable.ColimitPresentation
+public import Mathlib.CategoryTheory.Presentable.Comma
 public import Mathlib.CategoryTheory.Presentable.Dense
 public import Mathlib.CategoryTheory.Presentable.Directed
 public import Mathlib.CategoryTheory.Presentable.EssentiallyLarge
