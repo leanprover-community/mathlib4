b047574880cca54fb2e90aa714706cfc1205245b
Brian Nugent <b.nugent@utah.edu>
2026-08-29 16:00:36 +0000

    feat(Algebra/Category/ModuleCat): add abbrevs for Presheaves of Modules over commutative rings (#43192)
    Currently in mathlib, to define a presheaf of modules over a commutative ring, one 
    writes 
    ```
    variable  {C : Type*} [Category* C] (R : Cᵒᵖ ⥤ CommRingCat.{u}) (F : PresheafOfModules.{v} (R ⋙ forget₂ _ _))
    ```
    
    The problem with this formulation is that many operations do not preserve the shape of `(R ⋙ forget₂ _ _)`. For 
    example, [PresheafOfModules.pushforward₀](https://leanprover-community.github.io/mathlib4_docs/Mathlib/Algebra/Category/ModuleCat/Presheaf/Pushforward.html#PresheafOfModules.pushforward%E2%82%80) has type `PresheafOfModules (R ⋙ forget₂ _ _) ⥤ PresheafOfModules (F.op ⋙ R ⋙ forget₂ _ _)` when applied directly. This is why [PresheafOfModules.pushforward₀OfCommRingCat](https://leanprover-community.github.io/mathlib4_docs/Mathlib/Algebra/Category/ModuleCat/Presheaf/Pushforward.html#PresheafOfModules.pushforward%E2%82%80OfCommRingCat) was introduced. There are now around a dozen of these comm ring specific abbrevs in open PRs. In this PR, I introduce `PresheafOfModulesOfCommRing` with the goal of having a namespace to put all of these as well as making it easier to avoid making mistakes. 
    
    For an example of this being used, see #43193.
    
    Co-authored-by: Brian-Nugent <bnugent@uw.edu>

--- Mathlib.lean
+++ Mathlib.lean
@@ -189,6 +189,7 @@ public import Mathlib.Algebra.Category.ModuleCat.Presheaf.Free
 public import Mathlib.Algebra.Category.ModuleCat.Presheaf.Generator
 public import Mathlib.Algebra.Category.ModuleCat.Presheaf.Limits
 public import Mathlib.Algebra.Category.ModuleCat.Presheaf.Monoidal
+public import Mathlib.Algebra.Category.ModuleCat.Presheaf.OfCommRing
 public import Mathlib.Algebra.Category.ModuleCat.Presheaf.Pullback
 public import Mathlib.Algebra.Category.ModuleCat.Presheaf.Pushforward
 public import Mathlib.Algebra.Category.ModuleCat.Presheaf.PushforwardZeroMonoidal
