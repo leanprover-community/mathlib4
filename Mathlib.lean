9acf97e67fdbb82c50d496574a2ea9a061c76ba3
Joël Riou <joel.riou@universite-paris-saclay.fr>
2026-08-29 18:16:43 +0200

    wip

--- Mathlib.lean
+++ Mathlib.lean
@@ -4657,6 +4657,7 @@ public import Mathlib.Geometry.Convex.ConvexSpace.AffineMap
 public import Mathlib.Geometry.Convex.ConvexSpace.AffineSpace
 public import Mathlib.Geometry.Convex.ConvexSpace.Defs
 public import Mathlib.Geometry.Convex.ConvexSpace.Module
+public import Mathlib.Geometry.Convex.ConvexSpace.PathConnectedSpace
 public import Mathlib.Geometry.Convex.ConvexSpace.Prod
 public import Mathlib.Geometry.Convex.ConvexSpace.Topology
 public import Mathlib.Geometry.Convex.Hull
