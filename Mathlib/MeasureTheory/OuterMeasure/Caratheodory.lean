37ea417e7708c0c4fafa9f1abb5f97ba2947d875
Theodore Meek <theomeek@uw.edu>
2026-08-30 14:27:11 -0400

    Apply @CoolRaml's suggestion to proof golf
    Co-authored-by: Yongxi (Aaron) Lin <97214596+CoolRmal@users.noreply.github.com>

--- Mathlib/MeasureTheory/OuterMeasure/Caratheodory.lean
+++ Mathlib/MeasureTheory/OuterMeasure/Caratheodory.lean
@@ -64,9 +64,7 @@ theorem isCaratheodory_empty : IsCaratheodory m ∅ := by simp [IsCaratheodory, 
 
 /-- Sets with zero outer measure are Carathéodory-measurable. -/
 lemma isCaratheodory_of_measure_eq_zero (h : m s = 0) : m.IsCaratheodory s := by
-  rw [isCaratheodory_iff_le']
-  intro t
-  simp [measure_mono_null inter_subset_left h, measure_mono sdiff_subset]
+  simp [isCaratheodory_iff_le', measure_mono_null inter_subset_left h, measure_mono sdiff_subset]
 
 theorem isCaratheodory_compl : IsCaratheodory m s₁ → IsCaratheodory m s₁ᶜ := by
   simp [IsCaratheodory, sdiff_eq, add_comm]
