8dac57fe3f6475a53ec4a122eb2e565945d7a978
Bingyu Xia <71547343+BryceT233@users.noreply.github.com>
2026-08-30 09:14:43 +0800

    add a missing lemma

--- Mathlib/Algebra/Order/Antidiag/Prod.lean
+++ Mathlib/Algebra/Order/Antidiag/Prod.lean
@@ -126,6 +126,10 @@ theorem map_swap_mulAntidiagonal [CommMagma A] [HasMulAntidiagonal A] {n : A} :
     (mulAntidiagonal n).map ⟨Prod.swap, Prod.swap_injective⟩ = mulAntidiagonal n :=
   map_prodComm_mulAntidiagonal
 
+@[to_additive (attr := simp)]
+lemma coe_mulAntidiagonal_eq_preimage_singleton [CommMagma A] [HasMulAntidiagonal A] (a : A) :
+    (mulAntidiagonal a : Set (A × A)) = ((fun p ↦ p.1 * p.2) ⁻¹' {a}) := by ext; simp
+
 section CancelMonoid
 
 variable [CancelMonoid A] [HasMulAntidiagonal A] {p q : A × A} {n : A}
