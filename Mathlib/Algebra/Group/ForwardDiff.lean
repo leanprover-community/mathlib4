1e88181e49ce52c0074ba9414c5e1c2a5fb496b8
Monica Omar <23701951+themathqueen@users.noreply.github.com>
2026-08-31 18:45:10 +0100

    Update Mathlib/Algebra/Group/ForwardDiff.lean

--- Mathlib/Algebra/Group/ForwardDiff.lean
+++ Mathlib/Algebra/Group/ForwardDiff.lean
@@ -301,13 +301,11 @@ theorem fwdDiff_iter_sum_mul_pow_eq_zero {n : ℕ} (P : ℕ → R) :
 /--
 A summation formula expressing `∑ k < n, f (y + k • h)` in terms of iterated forward differences.
 -/
-theorem sum_range_shift_eq_sum_fwdDiff_iter (f : M → G) (y : M) {n : ℕ} (hn : 0 < n) :
+theorem sum_range_shift_eq_sum_fwdDiff_iter (f : M → G) (y : M) (n : ℕ) :
     ∑ k ∈ range n, f (y + k • h) = ∑ k ∈ range (n + 1), n.choose (k + 1) • Δ_[h]^[k] f y := by
-  simp_rw [shift_eq_sum_fwdDiff_iter]
-  have sum_extend_inner_range : ∑ x ∈ range n, ∑ k ∈ range (x + 1), (x.choose k) • Δ_[h]^[k] f y =
-      ∑ k ∈ range n, ∑ x ∈ Ico k n, (x.choose k) • (Δ_[h]^[k]) f y := by
-    rw [range_eq_Ico, sum_Ico_Ico_comm]; simp
-  simp only [sum_extend_inner_range, ← sum_smul]
-  rw [sum_range_add]; simp only [range_one, sum_singleton, add_zero, choose_succ_self, zero_nsmul]
-  apply sum_congr rfl; intro k hk
-  rw [show n = n - 1 + 1 by omega, Ico_add_one_right_eq_Icc, Nat.sum_Icc_choose]
+  induction n with
+  | zero => simp
+  | succ n IH =>
+    simp_rw [sum_range_succ, choose_succ_self, IH, shift_eq_sum_fwdDiff_iter h f n y,
+      choose_succ_succ', add_smul, sum_add_distrib, sum_range_succ]
+    grind
