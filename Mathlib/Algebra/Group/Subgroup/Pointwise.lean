6fb4972c8e517e92e277de088db4755e2385d6f1
Justus Springer <50165510+justus-springer@users.noreply.github.com>
2026-08-31 15:42:16 +0200

    Update Mathlib/Algebra/Group/Subgroup/Pointwise.lean
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- Mathlib/Algebra/Group/Subgroup/Pointwise.lean
+++ Mathlib/Algebra/Group/Subgroup/Pointwise.lean
@@ -43,7 +43,7 @@ variable {α G A S : Type*}
 theorem inv_coe_set [InvolutiveInv G] [SetLike S G] [InvMemClass S G] {H : S} : (H : Set G)⁻¹ = H :=
   Set.ext fun _ => inv_mem_iff
 
-@[to_additive]
+@[to_additive (attr := simp)]
 theorem InvMemClass.isSelfInv [InvolutiveInv G] [SetLike S G] [InvMemClass S G] (H : S) :
     IsSelfInv (H : Set G) :=
   inv_coe_set
