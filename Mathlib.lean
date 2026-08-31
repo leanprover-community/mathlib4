3d36759b506fba8c45353eda8149341334f15599
Riccardo Brasca <riccardo.brasca@gmail.com>
2026-08-31 19:39:59 +0200

    feat: add Algebra.Polynomial.Laurent.Roots

--- Mathlib.lean
+++ Mathlib.lean
@@ -1188,6 +1188,7 @@ public import Mathlib.Algebra.Polynomial.Homogenize
 public import Mathlib.Algebra.Polynomial.Identities
 public import Mathlib.Algebra.Polynomial.Inductions
 public import Mathlib.Algebra.Polynomial.Laurent
+public import Mathlib.Algebra.Polynomial.Laurent.Roots
 public import Mathlib.Algebra.Polynomial.Lifts
 public import Mathlib.Algebra.Polynomial.Mirror
 public import Mathlib.Algebra.Polynomial.Module.AEval
