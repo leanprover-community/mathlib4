bcb6f994802f8dd3ebda725da2c1417ac2509e8b
Thomas R. Murrills <68410468+thorimur@users.noreply.github.com>
2026-08-31 19:24:51 -0400

    Update Mathlib/Tactic/NormNum/Prime.lean
    Co-authored-by: Jon Eugster <eugster.jon@gmail.com>

--- Mathlib/Tactic/NormNum/Prime.lean
+++ Mathlib/Tactic/NormNum/Prime.lean
@@ -123,9 +123,9 @@ theorem isNat_minFac_4 : {n n' k : ℕ} →
 /-- The core of the `norm_num` extension which normalizes expressions of the form `minFac n`.
 This is public for use in the `Nat.primeFactorsList_ofNat` simproc. -/
 partial def evalMinFac.core (n : Q(ℕ)) (nn : Q(ℕ)) (pn : Q(IsNat $n $nn))
-  (n' : ℕ) : MetaM (Result q(Nat.minFac $n)) := do
-    let sℕ : Q(AddMonoidWithOne ℕ) := q(Nat.instAddMonoidWithOne)
-    let rec aux (ek : Q(ℕ)) (prf : Q(MinFacHelper $nn $ek)) :
+    (n' : ℕ) : MetaM (Result q(Nat.minFac $n)) := do
+  let sℕ : Q(AddMonoidWithOne ℕ) := q(Nat.instAddMonoidWithOne)
+  let rec aux (ek : Q(ℕ)) (prf : Q(MinFacHelper $nn $ek)) :
       (c : Q(ℕ)) × Q(IsNat (Nat.minFac $n) $c) :=
       let k := ek.natLit!
       -- remark: `deriveBool q($nn < $ek * $ek)` is 2x slower than the following test.
