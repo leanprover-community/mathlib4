9f159124cf06bfaf3cc64139b2073c8d2d17524d
Jiaxi Mo <jiaximo1998@163.com>
2026-09-01 14:10:29 +0800

    add HeckeBimodule

---
+++ Mathlib/RepresentationTheory/Hecke/Multiplicity.lean
@@ -0,0 +1,98 @@
+/-
+Copyright (c) 2026 Chris Birkbeck. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Chris Birkbeck, Jiaxi Mo
+-/
+module
+
+public import Mathlib.RepresentationTheory.Hecke.LeftFiniteDoubleCoset
+public import Mathlib.Data.Finsupp.Defs
+
+/-!
+# Multiplicity of convolution product
+
+-/
+
+@[expose] public section
+
+variable {G : Type*} [Group G] {H₁ H₂ H₃ : Subgroup G}
+open DoubleCoset
+
+namespace DoubleCoset.LeftDecompQuotient
+
+lemma snd_eq_of_fst_eq {g g' d : G} {i : LeftDecompQuotient H₁ H₂ g}
+    {j₁ j₂ : LeftDecompQuotient H₂ H₃ g'}
+    (h₁ : (i.out * g * (j₁.out * g') : G ⧸ H₃) = (d : G ⧸ H₃))
+    (h₂ : (i.out * g * (j₂.out * g') : G ⧸ H₃) = (d : G ⧸ H₃)) :
+    j₁ = j₂ := by
+  apply toLeftCoset_injective
+  have h := h₁.trans h₂.symm
+  simp only [toLeftCoset_apply, QuotientGroup.eq] at h ⊢
+  simpa [mul_assoc] using h
+
+lemma nat_card_fiber (H₁ H₂ : Subgroup G) (x y : G) [Decidable (mk H₁ H₂ x = mk H₁ H₂ y)] :
+    Nat.card {j : LeftDecompQuotient H₁ H₂ x | ((j.out : G) * x : G ⧸ H₂) = (y : G ⧸ H₂)} =
+      if mk H₁ H₂ x = mk H₁ H₂ y then 1 else 0 := by
+  split_ifs with hxy
+  · apply Nat.card_eq_one_iff_unique.mpr
+    constructor
+    · exact ⟨fun i j ↦ Subtype.ext <| toLeftCoset_injective <| by
+        simpa [toLeftCoset_apply] using i.2.trans j.2.symm⟩
+    · obtain ⟨i, hi⟩ := mem_range_toLeftCoset_iff.mpr hxy
+      exact ⟨⟨i, by simpa [toLeftCoset_apply] using hi⟩⟩
+  · have : IsEmpty {j : LeftDecompQuotient H₁ H₂ x | ((j.out : G) * x : G ⧸ H₂) = (y : G ⧸ H₂)} :=
+      ⟨fun i => hxy <| mem_range_toLeftCoset_iff.mp ⟨i.1, by rw [toLeftCoset_apply]; exact i.2⟩⟩
+    exact Nat.card_of_isEmpty
+
+lemma nat_card_fiber_helper (H₁ : Subgroup G) (x y z : G) :
+    (x * y : G ⧸ H₁) = z ↔ y = (x⁻¹ * z : G ⧸ H₁) := by
+  simp only [← smul_eq_mul, ← MulAction.Quotient.smul_mk, ← inv_smul_eq_iff, inv_inv]
+
+end DoubleCoset.LeftDecompQuotient
+
+namespace DoubleCoset₀
+
+/-- The map sending a pair of coset representatives `(σᵢ, τⱼ)` to the mixed double coset
+`H₁ (σᵢ g₁ τⱼ g₂) H₃` of their product. -/
+noncomputable def mulMap (x : DoubleCoset₀ H₁ H₂) (y : DoubleCoset₀ H₂ H₃)
+    (p : LeftDecompQuotient H₁ H₂ x.rep × LeftDecompQuotient H₂ H₃ y.rep) : DoubleCoset₀ H₁ H₃ :=
+  mk H₁ H₃ (p.1.out * x.rep * p.2.out * y.rep)
+
+lemma mulMap_eq_of_mk_eq (x : DoubleCoset₀ H₁ H₂) (y : DoubleCoset₀ H₂ H₃)
+    (z : DoubleCoset₀ H₁ H₃) {p : LeftDecompQuotient H₁ H₂ x.rep × LeftDecompQuotient H₂ H₃ y.rep}
+    (h : (p.1.out * x.rep * (p.2.out * y.rep) : G ⧸ H₃) = (z.rep : G ⧸ H₃)) :
+    x.mulMap y p = z := by
+  rw [← DoubleCoset₀.mk_rep z]
+  apply DoubleCoset₀.mk_eq_iff.mpr
+  exact ⟨1, H₁.one_mem, ((p.1.out * x.rep * p.2.out * y.rep)⁻¹ * z.rep), by
+    simpa [mul_assoc] using QuotientGroup.eq.mp h, by simp [mul_assoc]⟩
+
+/-- Shimura's multiplicity descended to Hecke double cosets. -/
+noncomputable def multiplicity (x : DoubleCoset₀ H₁ H₂) (y : DoubleCoset₀ H₂ H₃) :
+    DoubleCoset₀ H₁ H₃ →₀ ℕ :=
+  Finsupp.ofSupportFinite
+    (fun z => Nat.card {p : LeftDecompQuotient H₁ H₂ x.rep × LeftDecompQuotient H₂ H₃ y.rep |
+      (p.1.out * x.rep * (p.2.out * y.rep) : G ⧸ H₃) = (z.rep : G ⧸ H₃)}) <| by
+    classical
+    refine (Finset.univ.image (x.mulMap y)).finite_toSet.subset ?_
+    intro z hz
+    simp only [Function.mem_support, Nat.card_ne_zero] at hz
+    obtain ⟨⟨p, hp⟩, _⟩ := hz
+    exact Finset.mem_image.mpr ⟨p, Finset.mem_univ p, mulMap_eq_of_mk_eq x y z hp⟩
+
+lemma multiplicity_apply (x : DoubleCoset₀ H₁ H₂) (y : DoubleCoset₀ H₂ H₃)
+    (z : DoubleCoset₀ H₁ H₃) :
+    x.multiplicity y z =
+      Nat.card {p : LeftDecompQuotient H₁ H₂ x.rep × LeftDecompQuotient H₂ H₃ y.rep |
+        (p.1.out * x.rep * (p.2.out * y.rep) : G ⧸ H₃) = (z.rep : G ⧸ H₃)} := rfl
+
+lemma multiplicity_mk_mk_mk
+    (g : G) [IsLeftFinite H₁ H₂ g]
+    (h : G) [IsLeftFinite H₂ H₃ h]
+    (d : G) [IsLeftFinite H₁ H₃ d] :
+    multiplicity (mk H₁ H₂ g) (mk H₂ H₃ h) (mk H₁ H₃ d)
+      = Nat.card { p : LeftDecompQuotient H₁ H₂ g × LeftDecompQuotient H₂ H₃ h |
+          (p.1.out * g * (p.2.out * h) : G ⧸ H₃) = (d : G ⧸ H₃)} := by
+  sorry
+
+end DoubleCoset₀
--- Mathlib/RepresentationTheory/Hecke/Unimodular.lean
+++ Mathlib/RepresentationTheory/Hecke/Unimodular.lean
@@ -5,10 +5,11 @@ Authors: Jiaxi Mo
 -/
 module
 
-public import Mathlib.NumberTheory.HeckeRing.HeckeCoset.Multiplicity
+public import Mathlib.RepresentationTheory.Hecke.LeftFiniteDoubleCoset
+public import Mathlib.RepresentationTheory.Hecke.Multiplicity
 
 /-!
-# Unimodular
+# Unimodular condition for subgroups
 
 -/
 
@@ -16,12 +17,12 @@ public import Mathlib.NumberTheory.HeckeRing.HeckeCoset.Multiplicity
 
 open DoubleCoset
 
-variable {G : Type*} [Group G] {H H₁ H₂ : Subgroup G}
+variable {G : Type*} [Group G] {H H₁ H₂ H₃ : Subgroup G}
 
 namespace DoubleCoset
 
 /-- The map sending `H₁gH₂` to `H₂g⁻¹H₁`. -/
-def inv (x : DoubleCoset.Quotient (H₁ : Set G) (H₂ : Set G)) :
+def Quotient.inv (x : DoubleCoset.Quotient (H₁ : Set G) (H₂ : Set G)) :
     DoubleCoset.Quotient (H₂ : Set G) (H₁ : Set G) :=
   Quotient.liftOn x (fun x => DoubleCoset.mk H₂ H₁ x⁻¹) fun a b hab => by
     obtain ⟨h₁, hh₁, h₂, hh₂, heq⟩ := DoubleCoset.rel_iff.mp hab
@@ -30,53 +31,60 @@ def inv (x : DoubleCoset.Quotient (H₁ : Set G) (H₂ : Set G)) :
 
 @[simp]
 lemma inv_mk (g : G) :
-    inv (mk H₁ H₂ g) = mk H₂ H₁ g⁻¹ := by
+    (mk H₁ H₂ g).inv = mk H₂ H₁ g⁻¹ := by
   rfl
 
+@[simp]
 lemma mk_inv_out (x : DoubleCoset.Quotient (H₁ : Set G) (H₂ : Set G)) :
-    mk H₂ H₁ x.out⁻¹ = inv x := by
-  simp [← inv_mk, DoubleCoset.out_eq']
+    mk H₂ H₁ x.out⁻¹ = x.inv := by
+  rw [← inv_mk, DoubleCoset.out_eq']
 
 @[simp]
 lemma inv_inv (x : DoubleCoset.Quotient (H₁ : Set G) (H₂ : Set G)) :
-    inv (inv x) = x :=
+    x.inv.inv = x :=
   Quotient.inductionOn x fun x => by simp
 
 end DoubleCoset
 
 /-- A subgroup `H` is called Hecke unimodular if `Nat.card HgH = Nat.card Hg⁻¹H` for any `g`. -/
-class IsHeckeUnimodular (H : Subgroup G) : Prop where
+class Subgroup.IsHeckeUnimodular (H : Subgroup G) : Prop where
   degree_eq_inv_degree : ∀ (x : DoubleCoset.Quotient (H : Set G) (H : Set G)),
-    x.degree = (inv x).degree
+    x.degree = (x.inv).degree
 
-instance [IsHeckeUnimodular H] (g : G) [IsHeckeFinite H H g] :
-    IsHeckeFinite H H g⁻¹ := by
-  rw [isHeckeFinite_iff, ←inv_mk g, ← IsHeckeUnimodular.degree_eq_inv_degree]
+instance [H.IsHeckeUnimodular] (g : G) [IsLeftFinite H H g] :
+    IsLeftFinite H H g⁻¹ := by
+  rw [isLeftFinite_iff, ←inv_mk g, ← Subgroup.IsHeckeUnimodular.degree_eq_inv_degree]
   simp [mk_degree]
+
+@[simp]
+lemma DoubleCoset.inv_degree [H.IsHeckeUnimodular]
+    (x : DoubleCoset.Quotient (H : Set G) (H : Set G)) :
+    x.inv.degree = x.degree :=
+  (Subgroup.IsHeckeUnimodular.degree_eq_inv_degree x).symm
 
 namespace DoubleCoset₀
 
-variable [IsHeckeUnimodular H]
+variable [H.IsHeckeUnimodular]
 
 /-- The map sending `HgH` to `Hg⁻¹H`. -/
 def inv (x : DoubleCoset₀ H H) :
-    DoubleCoset₀ H H := ⟨DoubleCoset.inv x, by simp [← IsHeckeUnimodular.degree_eq_inv_degree]⟩
+    DoubleCoset₀ H H := ⟨x.val.inv, by simp⟩
 
 lemma coe_inv (x : DoubleCoset₀ H H) :
-    x.inv = DoubleCoset.inv x.val := rfl
+    x.inv = x.val.inv := rfl
 
 @[simp]
-lemma inv_mk (g : G) [IsHeckeFinite H H g] :
+lemma inv_mk (g : G) [IsLeftFinite H H g] :
     (mk H H g).inv = mk H H g⁻¹ := rfl
 
 lemma mk_inv_rep (x : DoubleCoset₀ H H) :
     mk H H x.rep⁻¹ = x.inv  := by
-  simp [inv, ← DoubleCoset.mk_inv_out, rep_eq_out]
+  rw [← inv_mk, mk_rep]
 
 @[simp]
 lemma inv_inv (x : DoubleCoset₀ H H) :
-    x.inv.inv = x :=
-  induction_on x (p := fun x => x.inv.inv = x) fun g => by simp
+    x.inv.inv = x := by
+  simp [inv]
 
 lemma inv_eq_iff {x y : DoubleCoset₀ H H} :
     x.inv = y ↔ x = y.inv :=
@@ -85,24 +93,16 @@ lemma inv_eq_iff {x y : DoubleCoset₀ H H} :
 @[simp]
 lemma inv_degree (x : DoubleCoset₀ H H) :
     x.inv.degree = x.degree := by
-  rw [← mk_rep x, mk_degree, ← DoubleCoset.mk_degree, IsHeckeUnimodular.degree_eq_inv_degree]
-  simp [inv, mk_degree, DoubleCoset.mk_degree]
-
-lemma diag_one_inv_eq_self :
-    (mk H H 1).inv = (mk H H 1) := by
-  simp
+  simp [inv, degree]
 
 private lemma multiplicity_self_inv_mk_one_eq_degree (x : DoubleCoset₀ H H) :
     x.multiplicity x.inv (mk H H 1) = x.degree := by
-  simp only  [multiplicity_apply]
-  obtain ⟨h₁, hh₁, h₂, hh₂, hinv⟩ := mk_eq_iff.mp
-    (show mk H H x.rep⁻¹ = mk H H x.inv.rep by simp [mk_inv_rep])
-  let j : DecompQuotient H H x.inv.rep := QuotientGroup.mk ⟨h₁⁻¹, H.inv_mem hh₁⟩
+  rw [multiplicity_apply, ← mk_rep x, mk_degree, mk_rep]
   have hrep : DoubleCoset.mk H H x.inv.rep = DoubleCoset.mk H H x.rep⁻¹ := by
     rw [← coe_mk, ← coe_mk, mk_inv_rep, mk_rep]
-  obtain ⟨j, hj⟩ := DoubleCoset.DecompQuotient.exists_toLeftCoset_of_mk_eq hrep
-  simp only [DoubleCoset.DecompQuotient.toLeftCoset_apply] at hj
-  have heq (i : DecompQuotient H H x.rep) :
+  obtain ⟨j, hj⟩ := DoubleCoset.LeftDecompQuotient.mem_range_toLeftCoset_iff.mpr hrep
+  simp only [DoubleCoset.LeftDecompQuotient.toLeftCoset_apply] at hj
+  have heq (i : LeftDecompQuotient H H x.rep) :
       (i.out * x.rep * (j.out * x.inv.rep) : G ⧸ H)
         = ((mk H H 1).rep : G ⧸ H) := by
     calc
@@ -111,31 +111,23 @@ private lemma multiplicity_self_inv_mk_one_eq_degree (x : DoubleCoset₀ H H) :
           congrArg (fun q : G ⧸ H => (i.out * x.rep : G) • q) hj
       _ = _ := by
         simpa [QuotientGroup.eq (a := i.out.val)] using H.mul_mem (H.inv_mem i.out.prop) (by simp)
-  let equiv :
-      {p : DecompQuotient H H x.rep × DecompQuotient H H x.inv.rep |
-        ((p.1.out * x.rep * (p.2.out * x.inv.rep) : G) :  G ⧸ H) = ((mk H H 1).rep : G ⧸ H)}
-      ≃ DecompQuotient H H x.rep :=
+  exact Nat.card_congr
     { toFun p := p.1.1
       invFun i := ⟨(i, j), heq i⟩
       left_inv p := Subtype.ext
-        (Prod.ext rfl (DoubleCoset.DecompQuotient.snd_eq_of_fst_eq (heq p.1.1) p.prop))
+        (Prod.ext rfl (DoubleCoset.LeftDecompQuotient.snd_eq_of_fst_eq (heq p.1.1) p.prop))
       right_inv _ := rfl}
-  simpa [degree_eq_rep] using Nat.card_congr equiv
 
 private lemma multiplicity_ne_self_inv_mk_one_eq_zero {x y : DoubleCoset₀ H H} (h : y ≠ x.inv) :
     x.multiplicity y (mk H H 1) = 0 := by
   simp only [multiplicity_apply]
   by_contra hne
+  apply h
   obtain ⟨p, hp⟩ := (Nat.card_ne_zero.mp hne).left
   simp only [Set.mem_ofPred_eq, ← mul_assoc, QuotientGroup.eq, mul_inv_rev] at hp
-  have heq : y = x.inv := by
-    rw [← DoubleCoset₀.mk_rep y, ← mk_inv_rep]
-    apply mk_eq_iff.mpr
-    have : y.rep⁻¹ * p.2.out⁻¹ * x.rep⁻¹ ∈ H := by
-      simpa [mul_assoc] using
-        H.mul_mem hp (H.mul_mem (H.inv_mem (DoubleCoset₀.diag_mk_one_rep_mem H)) p.1.out.prop)
-    exact ⟨p.2.out, p.2.out.prop, (y.rep⁻¹ * p.2.out⁻¹ * x.rep⁻¹), this, by simp [mul_assoc]⟩
-  exact h heq
+  rw [← mk_rep y, ← mk_inv_rep]
+  refine mk_eq_iff.mpr ⟨_, p.2.out.prop, y.rep⁻¹ * p.2.out⁻¹ * x.rep⁻¹, ?_, by simp [mul_assoc]⟩
+  simpa [mul_assoc] using H.mul_mem hp (H.mul_mem (H.inv_mem (diag_mk_one_rep_mem H)) p.1.out.prop)
 
 @[simp]
 lemma multiplicity_apply_one {x y : DoubleCoset₀ H H} [Decidable (y = x.inv)] :
