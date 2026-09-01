9355c5bfc2cc7c24db5bc5b35a2577a8bf577a7a
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2026-09-01 11:36:56 +0200

    feat(AlgebraicGeometry/EllipticCurve/Affine/AddSubMap): add the main property of addSubMap and sym2x

--- Mathlib/AlgebraicGeometry/EllipticCurve/Affine/AddSubMap.lean
+++ Mathlib/AlgebraicGeometry/EllipticCurve/Affine/AddSubMap.lean
@@ -169,6 +169,16 @@ lemma sym2x_some_some {x y x' y' : R} (h : W'.Nonsingular x y) (h' : W'.Nonsingu
     (some x y h).sym2x (some x' y' h') = ![x * x', x + x', 1] := by
   simp [sym2x]
 
+/-- `sym2x` in terms of the projective `xRep` coordinates. -/
+lemma sym2x_eq (P Q : W'.Point) :
+    P.sym2x Q = ![P.xRep 0 * Q.xRep 0, P.xRep 0 * Q.xRep 1 + P.xRep 1 * Q.xRep 0,
+      P.xRep 1 * Q.xRep 1] := by
+  match P, Q with
+  | 0, 0 => simp [xRep_zero]
+  | 0, .some x y h => simp [xRep_zero, xRep_some]
+  | .some x y h, 0 => simp [xRep_zero, xRep_some]
+  | .some x y h, .some x' y' h' => simp [xRep_some]
+
 lemma sym2x_ne_zero [Nontrivial R] (P Q : W'.Point) : P.sym2x Q ≠ 0 := by
   cases P <;> cases Q <;> simp [sym2x, xRep]
 
@@ -180,6 +190,81 @@ lemma sym2x_neg_left (P Q : W'.Point) : (-P).sym2x Q = P.sym2x Q := by
 
 lemma sym2x_neg_right (P Q : W'.Point) : P.sym2x (-Q) = P.sym2x Q := by
   simp [sym2x]
+
+open Nat in
+private lemma sym2x_P_P_eq_addSubMap (P : W'.Point) :
+    sym2x P P = fun i ↦ (addSubMap W' i).eval <| P.sym2x 0 := by
+  match P with
+  | 0 =>
+    simp only [sym2x_zero_zero, succ_eq_add_one, reduceAdd, addSubMap, Fin.isValue]
+    ext i : 1
+    fin_cases i <;> simp
+  | some .. =>
+    simp only [sym2x_some_some, succ_eq_add_one, reduceAdd, sym2x_some_zero, addSubMap, Fin.isValue]
+    ext i : 1
+    fin_cases i <;> simp [pow_two, two_mul]
+
+section Field
+
+variable {F : Type*} [Field F] [DecidableEq F] {W : Affine F}
+
+private lemma sym2x_P_add_P_zero (P : W.Point) :
+    ∃ t : F, t ≠ 0 ∧ t • sym2x (P + P) 0 = fun i ↦ (addSubMap W i).eval <| P.sym2x P := by
+  match P with
+  | 0 =>
+    refine ⟨1, one_ne_zero, ?_⟩
+    rw [add_zero, sym2x_zero_zero, one_smul, addSubMap]
+    ext i : 1
+    fin_cases i <;> simp
+  | some x y h =>
+    have Heq := (W.equation_iff x y).mp h.1
+    have Hrs : (fun i ↦ (addSubMap W i).eval <| (some x y h).sym2x (some x y h)) =
+          ![x ^ 4 - W.b₄ * x ^ 2 - 2 * W.b₆ * x - W.b₈,
+            4 * x ^ 3 + W.b₂ * x ^ 2 + 2 * W.b₄ * x + W.b₆, 0] := by
+      ext i : 1
+      fin_cases i <;> simp [addSubMap] <;> ring
+    rw [Hrs]
+    by_cases! H : y = W.negY x y
+    · have H' := (den_duplication_eq_zero_iff h.1).mpr H
+      rw [H', add_self_of_Y_eq H, sym2x_zero_zero]
+      refine ⟨_, den_duplication_ne_zero_or_num_duplication_ne_zero h |>.neg_resolve_left H', ?_⟩
+      simp
+    · have H' := (den_duplication_eq_zero_iff h.1).not.mpr H
+      refine ⟨_, H', ?_⟩
+      simp [sym2x_eq, xRep_add_self_of_Y_ne h H, mul_div_cancel₀ _ H']
+
+/-- `sym2x (P + Q) (P - Q)` is equal, up to scaling by a nonzero constant, to `addSubMap W`
+applied to `sym2x P Q`. -/
+lemma sym2x_add_sub_eq_addSubMap_sym2x (P Q : W.Point) :
+    ∃ t : F, t ≠ 0 ∧ t • sym2x (P + Q) (P - Q) = fun i ↦ (addSubMap W i).eval <| sym2x P Q := by
+  rcases eq_or_ne P Q with rfl | hPQ
+  · simpa using P.sym2x_P_add_P_zero
+  rcases eq_or_ne Q (-P) with rfl | hPQ'
+  · simpa [sym2x_neg_right, sym2x_comm 0] using P.sym2x_P_add_P_zero
+  match P, Q with
+  | P, 0 =>  exact ⟨1, one_ne_zero, by simpa using P.sym2x_P_P_eq_addSubMap⟩
+  | 0, Q =>
+    refine ⟨1, one_ne_zero, ?_⟩
+    simpa [sym2x_neg_right, sym2x_comm _ Q] using Q.sym2x_P_P_eq_addSubMap
+  | some xP yP hP, some xQ yQ hQ =>
+    have hxPQ : xP ≠ xQ := fun Heq ↦ by grind only [X_eq_iff.mp Heq]
+    have Hrs : (fun i ↦ (addSubMap W i).eval <| (some xP yP hP).sym2x (some xQ yQ hQ)) =
+        ![(xP * xQ) ^ 2 - W.b₄ * (xP * xQ) - W.b₆ * (xP + xQ) - W.b₈,
+          2 * (xP + xQ) * (xP * xQ) + W.b₂ * (xP * xQ) + W.b₄ * (xP + xQ) + W.b₆,
+          (xP - xQ) ^ 2] := by
+      ext i : 1
+      fin_cases i <;> simp [addSubMap]
+      ring
+    have : xP - xQ ≠ 0 := sub_ne_zero_of_ne hxPQ
+    refine ⟨(xP - xQ) ^ 2, pow_ne_zero 2 this, ?_⟩
+    -- The following relations are needed for the `grobner` calls below.
+    have HeqP := (W.equation_iff xP yP).mp hP.1
+    have HeqQ := (W.equation_iff xQ yQ).mp hQ.1
+    rw [Hrs, sym2x_eq, xRep_add_of_X_ne hP hQ hxPQ, xRep_sub_of_X_ne hP hQ hxPQ, b₂, b₄, b₆, b₈]
+    ext i : 1
+    fin_cases i <;> simp [field] <;> grobner
+
+end Field
 
 end WeierstrassCurve.Affine.Point
 
