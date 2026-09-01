81160f466b2be8f7166133ebbb015a160378ebcd
metakunt <m@k.nt>
2026-09-01 19:27:34 +0200

    Add tb to author

--- Mathlib/NumberTheory/AKSPrimality/Introspective.lean
+++ Mathlib/NumberTheory/AKSPrimality/Introspective.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2026 metakunt. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: metakunt
+Authors: metakunt, Thomas Browning
 -/
 module
 
