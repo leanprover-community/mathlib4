ac58f3599995e8b963f5d7a32c2698cb70f93b0e
Monica Omar <23701951+themathqueen@users.noreply.github.com>
2026-09-01 14:12:40 +0100

    wrong file

--- Mathlib.lean
+++ Mathlib.lean
@@ -2061,7 +2061,6 @@ public import Mathlib.Analysis.InnerProductSpace.Calculus
 public import Mathlib.Analysis.InnerProductSpace.CanonicalTensor
 public import Mathlib.Analysis.InnerProductSpace.Coalgebra
 public import Mathlib.Analysis.InnerProductSpace.Completion
-public import Mathlib.Analysis.InnerProductSpace.Complexification.basic2
 public import Mathlib.Analysis.InnerProductSpace.ConformalLinearMap
 public import Mathlib.Analysis.InnerProductSpace.Continuous
 public import Mathlib.Analysis.InnerProductSpace.Convex
