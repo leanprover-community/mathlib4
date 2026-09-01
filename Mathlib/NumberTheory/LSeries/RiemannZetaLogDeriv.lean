bf66ea116fb529b973782b5aa187ceed485c71ff
Terence Tao <tao@math.ucla.edu>
2026-09-01 16:53:13 -0700

    golf

--- Mathlib/NumberTheory/LSeries/RiemannZetaLogDeriv.lean
+++ Mathlib/NumberTheory/LSeries/RiemannZetaLogDeriv.lean
@@ -35,21 +35,20 @@ theorem logDeriv_riemannZeta_one_sub {s : ℂ} (hs : ∀ n : ℤ, s ≠ n) (hz :
   have h2π : 2 * (π : ℂ) ≠ 0 := mod_cast by positivity
   have (m : ℕ) : s ≠ -m := mod_cast hs (-m)
   have : s ≠ 1 := mod_cast hs 1
-  have : cos (π * s / 2) ≠ 0 := by
-    rw [Ne, cos_eq_zero_iff]
-    exact fun ⟨k, _⟩ ↦ hs (2 * k + 1) (mod_cast by grind)
+  have : cos (π * s / 2) ≠ 0 :=
+    mt cos_eq_zero_iff.mp fun ⟨k, _⟩ ↦ hs (2 * k + 1) (mod_cast by grind)
   have : logDeriv (fun z ↦ (2 * (π : ℂ)) ^ (-z)) s = -log (2 * π) := by
     simp [logDeriv_apply, ((hasDerivAt_neg' s).const_cpow (Or.inl h2π)).deriv, field]
   have : logDeriv (fun z ↦ cos (π * z / 2)) s = -(π / 2 * tan (π * s / 2)) := by
     simp [logDeriv_apply, (by simpa using ((hasDerivAt_id s).const_mul _).div_const 2 :
       HasDerivAt ((π : ℂ) * · / 2) (π / 2) s).ccos.deriv, tan_eq_sin_div_cos, field]
   have := ((hasDerivAt_neg' s).const_cpow (Or.inl h2π)).differentiableAt
-  have : (fun z ↦ riemannZeta (1 - z)) =ᶠ[𝓝 s]
+  have : riemannZeta ∘ (1 - ·) =ᶠ[𝓝 s]
       fun z ↦ 2 * (2 * π) ^ (-z) * Gamma z * cos (π * z / 2) * riemannZeta z := by
     filter_upwards [isClosed_range_intCast.isOpen_compl.mem_nhds (by grind)] with z hz
     rw [Set.mem_compl_iff, Set.mem_range, not_exists] at hz
     exact riemannZeta_one_sub (fun n h ↦ hz (-n) (by simp [h])) (by grind [hz 1])
   have := (logDeriv_congr_nhds this).eq_of_nhds
-  rw [(by rfl : (fun _ ↦ _) = riemannZeta ∘ (1 - ·)), logDeriv_comp, logDeriv_fun_mul,
-    logDeriv_fun_mul, logDeriv_fun_mul, logDeriv_fun_mul, ← digamma_def] at this
+  rw [logDeriv_comp, logDeriv_fun_mul, logDeriv_fun_mul, logDeriv_fun_mul, logDeriv_fun_mul,
+    ← digamma_def] at this
   <;> try first | fun_prop | simp_all <;> grind [differentiableAt_riemannZeta, Gamma_ne_zero, hs 0]
