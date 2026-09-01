d86bd34c8008758a3421e10ea05caf09bdb5afec
Snir Broshi <26556598+SnirBroshi@users.noreply.github.com>
2026-09-01 07:03:31 +0300

    `Order.Interval.Finset.Card` -> `Order.Interval.Set.Card`

--- Mathlib.lean
+++ Mathlib.lean
@@ -6259,7 +6259,6 @@ public import Mathlib.Order.InitialSeg
 public import Mathlib.Order.Interval.Basic
 public import Mathlib.Order.Interval.Finset.Basic
 public import Mathlib.Order.Interval.Finset.Box
-public import Mathlib.Order.Interval.Finset.Card
 public import Mathlib.Order.Interval.Finset.Defs
 public import Mathlib.Order.Interval.Finset.DenselyOrdered
 public import Mathlib.Order.Interval.Finset.Fin
@@ -6270,6 +6269,7 @@ public import Mathlib.Order.Interval.Finset.SuccPred
 public import Mathlib.Order.Interval.Lex
 public import Mathlib.Order.Interval.Multiset
 public import Mathlib.Order.Interval.Set.Basic
+public import Mathlib.Order.Interval.Set.Card
 public import Mathlib.Order.Interval.Set.Defs
 public import Mathlib.Order.Interval.Set.Disjoint
 public import Mathlib.Order.Interval.Set.Fin
