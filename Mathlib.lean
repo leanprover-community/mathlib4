9b60a13363264cde08a3614d1bc7e49ab312c179
metakunt <m@k.nt>
2026-09-02 20:08:09 +0200

    remove mapCyclotomic

--- Mathlib.lean
+++ Mathlib.lean
@@ -6617,7 +6617,6 @@ public import Mathlib.RingTheory.Adjoin.Singleton
 public import Mathlib.RingTheory.Adjoin.Tower
 public import Mathlib.RingTheory.AdjoinRoot.Basic
 public import Mathlib.RingTheory.AdjoinRoot.Instances
-public import Mathlib.RingTheory.AdjoinRoot.MapCyclotomic
 public import Mathlib.RingTheory.AlgebraTower
 public import Mathlib.RingTheory.Algebraic.Basic
 public import Mathlib.RingTheory.Algebraic.Cardinality
