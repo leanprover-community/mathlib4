27cc50d8e3f58e037577f8714c0b14e076d70fac
Michael Rothgang <rothgang@math.uni-bonn.de>
2026-09-03 01:40:23 +0200

    Fix

--- Mathlib/Algebra/Star/StarAlgHom.lean
+++ Mathlib/Algebra/Star/StarAlgHom.lean
@@ -667,16 +667,15 @@ instance (priority := 100) (F R A B : Type*) [CommSemiring R] [Semiring A]
 
 /-- Turn an element of a type `F` satisfying `AlgEquivClass F R A B` and `StarHomClass F A B` into
 an actual `StarAlgEquiv`. -/
-@[coe]
-def StarAlgEquiv.ofClass {F R A B : Type*} [Add A] [Mul A] [SMul R A] [Star A] [Add B] [Mul B] [SMul R B]
-    [Star B] [EquivLike F A B] [NonUnitalAlgEquivClass F R A B] [StarHomClass F A B]
+def StarAlgEquiv.ofClass {F R A B : Type*}
+    [Add A] [Mul A] [SMul R A] [Star A] [Add B] [Mul B] [SMul R B] [Star B]
+    [EquivLike F A B] [NonUnitalAlgEquivClass F R A B] [StarHomClass F A B]
     (f : F) : A ≃⋆ₐ[R] B :=
   { (RingEquivClass.toRingEquiv f : A ≃+* B) with
     map_star' := map_star f
     map_smul' := map_smul f }
 
-@[deprecated (since := "2026-09-02")] alias StarAlgEquivClass.toStarAlgEquiv := StarAlgEquiv.ofClass
-
+@[deprecated (since := "2026-09-03")] alias StarAlgEquivClass.toStarAlgEquiv := StarAlgEquiv.ofClass
 
 namespace StarAlgEquiv
 
