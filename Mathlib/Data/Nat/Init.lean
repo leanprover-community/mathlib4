2502c0ba2e3b0e261bf8e310b2aae9bc07eedae8
Bolton Bailey <bolton.bailey@gmail.com>
2026-09-02 12:23:59 -0700

    feat(Data/Nat/Init): dvd_div_of_mul_dvd_right

--- Mathlib/Data/Nat/Init.lean
+++ Mathlib/Data/Nat/Init.lean
@@ -381,6 +381,13 @@ protected theorem not_two_dvd_bit1 (n : ℕ) : ¬2 ∣ 2 * n + 1 := by
 /-- A natural number `m` divides the sum `n + m` if and only if `m` divides `n`. -/
 @[simp] protected lemma dvd_add_self_right : m ∣ n + m ↔ m ∣ n := Nat.dvd_add_left (Nat.dvd_refl m)
 
+/-- Alias of `Nat.dvd_div_of_mul_dvd`, to specify which factor is divided out.
+So that `Nat.dvd_div_of_mul_dvd_right` can have a symmetrical name. -/
+alias dvd_div_of_mul_dvd_left := dvd_div_of_mul_dvd
+
+lemma dvd_div_of_mul_dvd_right (h : a * b ∣ c) : a ∣ c / b :=
+  dvd_div_of_mul_dvd_left (by rwa [Nat.mul_comm] at h)
+
 /-- Two natural numbers are equal if and only if they have the same multiples. -/
 lemma dvd_right_iff_eq : (∀ a : ℕ, m ∣ a ↔ n ∣ a) ↔ m = n :=
   ⟨fun h => Nat.dvd_antisymm ((h _).mpr (Nat.dvd_refl _)) ((h _).mp (Nat.dvd_refl _)),
