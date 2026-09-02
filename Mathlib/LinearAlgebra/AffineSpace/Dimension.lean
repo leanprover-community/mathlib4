48d990e254f77e1f220c8ba93c10a256c3ce699c
Vlad Tsyrklevich <vlad@tsyrklevi.ch>
2026-09-02 11:49:08 +0200

    findim -> finDim

--- Mathlib/LinearAlgebra/AffineSpace/Dimension.lean
+++ Mathlib/LinearAlgebra/AffineSpace/Dimension.lean
@@ -18,7 +18,7 @@ is similary defined using `Module.finrank`.
 ## Main definitions
 
 * `AffineSubspace.dim`: Dimension expressed as `WithBot Cardinal`
-* `AffineSubspace.findim`: Dimension expressed as `WithBot ℕ` with a junk value of 0 for infinite
+* `AffineSubspace.finDim`: Dimension expressed as `WithBot ℕ` with a junk value of 0 for infinite
   dimensional spaces.
 -/
 
@@ -45,8 +45,8 @@ noncomputable def dim (s : AffineSubspace R A) : WithBot Cardinal :=
 
 /-- The dimension of `s` is equal to `⊥` if `s = ⊥`, and otherwise it is equal to the finite
 dimension of `s` interpreted as a linear space. Note that this inherits `Module.finrank`s junk
-value: `AffineSubspace.findim s = 0` for infinite dimensional subspaces. -/
-noncomputable def findim (s : AffineSubspace R A) : WithBot ℕ :=
+value: `AffineSubspace.finDim s = 0` for infinite dimensional subspaces. -/
+noncomputable def finDim (s : AffineSubspace R A) : WithBot ℕ :=
   WithBot.map Cardinal.toNat (dim s)
 
 @[simp]
@@ -54,8 +54,8 @@ theorem dim_bot : dim (⊥ : AffineSubspace R A) = ⊥ := by
   simp [dim]
 
 @[simp]
-theorem findim_bot : findim (⊥ : AffineSubspace R A) = ⊥ := by
-  simp [findim]
+theorem finDim_bot : finDim (⊥ : AffineSubspace R A) = ⊥ := by
+  simp [finDim]
 
 @[simp]
 theorem dim_singleton [Nontrivial R] (x : A) : dim ({x} : AffineSubspace R A) = 0 := by
@@ -64,39 +64,39 @@ theorem dim_singleton [Nontrivial R] (x : A) : dim ({x} : AffineSubspace R A) = 
   simp
 
 @[simp]
-theorem findim_singleton [Nontrivial R] (x : A) : findim ({x} : AffineSubspace R A) = 0 := by
-  simp [findim]
+theorem finDim_singleton [Nontrivial R] (x : A) : finDim ({x} : AffineSubspace R A) = 0 := by
+  simp [finDim]
 
 @[simp]
 theorem dim_eq_bot_iff : dim s = ⊥ ↔ s = ⊥ := by
   simp [dim]
 
 @[simp]
-theorem findim_eq_bot_iff : findim s = ⊥ ↔ s = ⊥ := by
-  simp [findim]
+theorem finDim_eq_bot_iff : finDim s = ⊥ ↔ s = ⊥ := by
+  simp [finDim]
 
 theorem dim_ne_bot_iff : dim s ≠ ⊥ ↔ (s : Set A).Nonempty := by
   contrapose!
   simp
 
-theorem findim_ne_bot_iff : findim s ≠ ⊥ ↔ (s : Set A).Nonempty := by
+theorem finDim_ne_bot_iff : finDim s ≠ ⊥ ↔ (s : Set A).Nonempty := by
   contrapose!
   simp
 
 theorem dim_eq_rank (h : s ≠ ⊥) : dim s = Module.rank R s.direction := by
   simpa [dim]
 
-theorem findim_eq_finrank (h : s ≠ ⊥) : findim s = Module.finrank R s.direction := by
-  simp [findim, dim_eq_rank h]
+theorem finDim_eq_finrank (h : s ≠ ⊥) : finDim s = Module.finrank R s.direction := by
+  simp [finDim, dim_eq_rank h]
   norm_cast
 
 @[simp]
-theorem findim_eq_finrank_of_not_finite [Module.Free R s.direction] [StrongRankCondition R]
-    (h : ¬Module.Finite R s.direction) : findim s = 0 := by
+theorem finDim_eq_finrank_of_not_finite [Module.Free R s.direction] [StrongRankCondition R]
+    (h : ¬Module.Finite R s.direction) : finDim s = 0 := by
   by_cases hs : s = ⊥
   · rw [hs, direction_bot] at h
     exact False.elim <| h <| Module.Finite.bot ..
-  rw [findim_eq_finrank hs, Nat.cast_eq_zero]
+  rw [finDim_eq_finrank hs, Nat.cast_eq_zero]
   exact Module.finrank_of_not_finite h
 
 @[simp]
@@ -114,19 +114,19 @@ theorem finite_iff_dim_lt_aleph0 [StrongRankCondition R] (s : AffineSubspace R A
     infer_instance
   · exact Module.rank_lt_aleph0_iff.mp (by simpa [dim_eq_rank hs] using h)
 
-theorem finite_of_findim_ne_zero [StrongRankCondition R] (s : AffineSubspace R A)
-    [Module.Free R s.direction] (h : findim s ≠ 0) : Module.Finite R s.direction := by
+theorem finite_of_finDim_ne_zero [StrongRankCondition R] (s : AffineSubspace R A)
+    [Module.Free R s.direction] (h : finDim s ≠ 0) : Module.Finite R s.direction := by
   rcases eq_or_ne s ⊥ with rfl | hs
   · rw [direction_bot]
     infer_instance
-  exact Module.finite_of_finrank_pos <| Nat.pos_of_ne_zero (by simpa [findim_eq_finrank hs] using h)
+  exact Module.finite_of_finrank_pos <| Nat.pos_of_ne_zero (by simpa [finDim_eq_finrank hs] using h)
 
-theorem findim_eq_map_dim_toNat : findim s = (dim s).map Cardinal.toNat := by
-  simp [findim]
+theorem finDim_eq_map_dim_toNat : finDim s = (dim s).map Cardinal.toNat := by
+  simp [finDim]
 
-theorem dim_eq_findim_unbot (hs : s ≠ ⊥) [StrongRankCondition R] [Module.Finite R s.direction] :
-    dim s = (findim s).unbot (by simpa) := by
-  simp only [dim_eq_rank hs, findim, WithBot.map_coe, WithBot.unbot_coe]
+theorem dim_eq_finDim_unbot (hs : s ≠ ⊥) [StrongRankCondition R] [Module.Finite R s.direction] :
+    dim s = (finDim s).unbot (by simpa) := by
+  simp only [dim_eq_rank hs, finDim, WithBot.map_coe, WithBot.unbot_coe]
   norm_cast
   exact Cardinal.cast_toNat_of_lt_aleph0 (Module.rank_lt_aleph0 _ _) |>.symm
 
@@ -138,11 +138,11 @@ theorem dim_mono (h : s ≤ t) : dim s ≤ dim t := by
     Submodule.rank_mono (direction_le h)]
 
 @[mono]
-theorem findim_mono [StrongRankCondition R] [Module.Finite R t.direction] (h : s ≤ t) :
-    findim s ≤ findim t := by
+theorem finDim_mono [StrongRankCondition R] [Module.Finite R t.direction] (h : s ≤ t) :
+    finDim s ≤ finDim t := by
   by_cases hs : s = ⊥
   · simp [hs]
-  simp [findim_eq_finrank hs, findim_eq_finrank (ne_bot_of_le_ne_bot hs h),
+  simp [finDim_eq_finrank hs, finDim_eq_finrank (ne_bot_of_le_ne_bot hs h),
     Submodule.finrank_mono (direction_le h)]
 
 theorem map_lift_dim_map_le (f : A →ᵃ[R] A') (s : AffineSubspace R A) :
@@ -153,11 +153,11 @@ theorem map_lift_dim_map_le (f : A →ᵃ[R] A') (s : AffineSubspace R A) :
   rw [dim_eq_rank hs, dim_eq_rank (by simp_all), map_direction]
   simp [lift_rank_map_le]
 
-theorem findim_map_le_findim [StrongRankCondition R] (f : A →ᵃ[R] A') (s : AffineSubspace R A)
-    [Module.Finite R s.direction] : (map f s).findim ≤ s.findim := by
+theorem finDim_map_le_finDim [StrongRankCondition R] (f : A →ᵃ[R] A') (s : AffineSubspace R A)
+    [Module.Finite R s.direction] : (map f s).finDim ≤ s.finDim := by
   by_cases hs : (map f s) = ⊥
   · simp [hs]
-  rw [findim_eq_finrank hs, findim_eq_finrank (by simp_all), map_direction]
+  rw [finDim_eq_finrank hs, finDim_eq_finrank (by simp_all), map_direction]
   simp [Submodule.finrank_map_le]
 
 theorem map_lift_dim_map_eq_of_injective {f : A →ᵃ[R] A'} (hf : Function.Injective f)
@@ -171,11 +171,11 @@ theorem map_lift_dim_map_eq_of_injective {f : A →ᵃ[R] A'} (hf : Function.Inj
   refine LinearEquiv.lift_rank_eq <| (Submodule.equivMapOfInjective _ ?_ _).symm
   exact f.linear_injective_iff.mpr hf
 
-theorem findim_map_eq_findim_of_injective {f : A →ᵃ[R] A'} (hf : Function.Injective f)
-    (s : AffineSubspace R A) : (map f s).findim = s.findim := by
+theorem finDim_map_eq_finDim_of_injective {f : A →ᵃ[R] A'} (hf : Function.Injective f)
+    (s : AffineSubspace R A) : (map f s).finDim = s.finDim := by
   by_cases hs : map f s = ⊥
   · simp_all
-  rw [findim_eq_finrank hs, findim_eq_finrank (by simp_all), map_direction]
+  rw [finDim_eq_finrank hs, finDim_eq_finrank (by simp_all), map_direction]
   norm_cast
   refine LinearEquiv.finrank_eq <| (Submodule.equivMapOfInjective _ ?_ _).symm
   exact f.linear_injective_iff.mpr hf
@@ -188,12 +188,12 @@ theorem dim_le_zero_iff_subsingleton [IsDomain R] [Module.IsTorsionFree R s.dire
   simp [dim_eq_rank, hs, rank_zero_iff, Submodule.subsingleton_iff_eq_bot]
 
 @[simp]
-theorem findim_le_zero_iff_subsingleton [StrongRankCondition R] [IsDomain R]
+theorem finDim_le_zero_iff_subsingleton [StrongRankCondition R] [IsDomain R]
     [Module.IsTorsionFree R s.direction] [Module.Finite R s.direction] :
-    findim s ≤ 0 ↔ (s : Set A).Subsingleton := by
+    finDim s ≤ 0 ↔ (s : Set A).Subsingleton := by
   by_cases hs : s = ⊥
   · simp [hs]
-  simp [findim_eq_finrank, hs, Module.finrank_zero_iff, Submodule.subsingleton_iff_eq_bot]
+  simp [finDim_eq_finrank, hs, Module.finrank_zero_iff, Submodule.subsingleton_iff_eq_bot]
 
 end Ring
 
@@ -203,10 +203,10 @@ variable [DivisionRing R] [Module R V]
 variable {s t : AffineSubspace R A}
 
 @[mono]
-theorem findim_strictMono [Module.Finite R t.direction] (h : s < t) : findim s < findim t := by
+theorem finDim_strictMono [Module.Finite R t.direction] (h : s < t) : finDim s < finDim t := by
   by_cases hs : s = ⊥
   · simp_all [bot_lt_iff_ne_bot]
-  rw [findim_eq_finrank hs, findim_eq_finrank (ne_bot_of_gt h), Nat.cast_lt]
+  rw [finDim_eq_finrank hs, finDim_eq_finrank (ne_bot_of_gt h), Nat.cast_lt]
   refine Submodule.finrank_lt_finrank_of_lt (direction_lt_of_nonempty h ?_)
   exact (nonempty_iff_ne_bot _).mpr hs
 
