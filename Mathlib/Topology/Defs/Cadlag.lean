7d548bb04d79bd3f4f30568bf2d2ca17a584aa90
Etienne Marion <baguettes_casino0c@icloud.com>
2026-09-02 18:35:42 +0200

    authors

--- Mathlib/Topology/Defs/Cadlag.lean
+++ Mathlib/Topology/Defs/Cadlag.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2025 Rémy Degenne. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Rémy Degenne, Yongxi Lin, Etienne Marion, Kexing Ying
+Authors: Rémy Degenne, Nick Kuhn, Yongxi Lin, Rohit Manokaran, Etienne Marion, Kexing Ying
 -/
 module
 
