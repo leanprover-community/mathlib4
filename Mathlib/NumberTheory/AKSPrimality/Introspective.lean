47de13f199c8eef81b05ce7d363a1d742403ad95
metakunt <166170055+metakunt@users.noreply.github.com>
2026-09-02 05:37:34 +0200

    Update Mathlib/NumberTheory/AKSPrimality/Introspective.lean
    Co-authored-by: Thomas Browning <tb65536@users.noreply.github.com>

--- Mathlib/NumberTheory/AKSPrimality/Introspective.lean
+++ Mathlib/NumberTheory/AKSPrimality/Introspective.lean
@@ -56,12 +56,9 @@ theorem dvd_aeval_sub {A : Type*} [CommRing A] [Algebra R A] (x : A) (h : Intros
   rw [Introspective.iff_dvd] at h
   simpa [aeval_comp] using map_dvd (aeval x) h
 
-theorem aeval_of_primitive_roots {K : Type*} [CommRing K] [IsDomain K] [Algebra R K]
-    (h : Introspective f e r) : ∀ μ ∈ (primitiveRoots r K), f.aeval μ ^ e = f.aeval (μ ^ e) := by
-  intro μ hμ
-  obtain ⟨g, hg⟩ := dvd_aeval_sub μ h
-  suffices μ ^ r - 1 = 0 by grind
-  simp [(isPrimitiveRoot_of_mem_primitiveRoots hμ).pow_eq_one]
+theorem aeval_of_pow_eq_one {A : Type*} [CommRing A] [Algebra R A]
+    (h : Introspective f e r) {μ : A} (hμ : μ ^ r = 1) : f.aeval μ ^ e = f.aeval (μ ^ e) := by
+  simpa [hμ, sub_eq_zero] using h.dvd_aeval_sub μ
 
 @[simp]
 protected theorem one (f : R[X]) : Introspective f 1 r := by
