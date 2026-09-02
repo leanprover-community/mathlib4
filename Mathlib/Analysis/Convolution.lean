6d3b8486807d319fb6e38c13a18f5a5d960e9d14
Evgenia Karunus <lakesare@gmail.com>
2026-09-02 20:31:56 +0100

    Loreaux - move the lemma to avoid excessive imports

--- Mathlib/Analysis/Convolution.lean
+++ Mathlib/Analysis/Convolution.lean
@@ -6,6 +6,7 @@ Authors: Floris van Doorn
 module
 
 public import Mathlib.MeasureTheory.Integral.Prod
+public import Mathlib.MeasureTheory.Function.Holder
 public import Mathlib.MeasureTheory.Function.LocallyIntegrable
 public import Mathlib.MeasureTheory.Group.Integral
 public import Mathlib.MeasureTheory.Group.Prod
--- Mathlib/MeasureTheory/Function/Holder.lean
+++ Mathlib/MeasureTheory/Function/Holder.lean
@@ -41,6 +41,11 @@ variable {α 𝕜 E F G : Type*} {m : MeasurableSpace α} {μ : Measure α}
     [NormedSpace 𝕜 E] [NormedSpace 𝕜 F] [NormedSpace 𝕜 G]
     (B : E →L[𝕜] F →L[𝕜] G)
 
+theorem MeasureTheory.eLpNorm_le_enorm_mul_eLpNorm_mul_eLpNorm {f : α → E} {g : α → F}
+    (hf : AEStronglyMeasurable f μ) (hg : AEStronglyMeasurable g μ) :
+    eLpNorm (fun a ↦ B (f a) (g a)) r μ ≤ ‖B‖ₑ * eLpNorm f p μ * eLpNorm g q μ :=
+  eLpNorm_le_eLpNorm_mul_eLpNorm_of_enorm hf hg (B ·) ‖B‖ₑ (.of_forall fun _ ↦ B.le_opENorm₂ ..)
+
 namespace ContinuousLinearMap
 
 variable (r) in
