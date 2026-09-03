8b9e11c580520a7a033cc602494150e90fa9cd32
Michael Rothgang <rothgang@math.uni-bonn.de>
2026-09-03 17:24:27 +0200

    Initial tests pass, with a hacky fix

--- MathlibTest/Linter/BundledMorphismClasses.lean
+++ MathlibTest/Linter/BundledMorphismClasses.lean
@@ -10,4 +10,24 @@ variable {R M N : Type*} [Semiring R] [AddCommMonoid M] [AddCommMonoid N] [Modul
 def LinearMap.foo (_f : M →ₗ[R] N) : ℕ := 37
 
 -- A definition taking in a LinearMapClass argument is not.
+
+-- Note: the linter does not fire here, because the typeclass assumption is not actually used.
 def LinearMap.bar (_f : F) : ℕ := 37
+
+-- But the linter fires on this declaration.
+def LinearMap.baz (f : F) : M →ₗ[R] N := f
+
+/--
+error: -- Found 1 error in 3 declarations (plus 0 automatically generated ones) in the current file with 1 linters
+
+/- The `defsWithMorphismClass` linter reports:
+FOUND definitions with a bundled morphism argument.
+This linter can be disabled with `@[nolint defsWithMorphismClass]`. -/
+#check @LinearMap.baz /- The definition `LinearMap.baz` takes a `LinearMapClass` argument.
+Per https://github.com/leanprover-community/mathlib4/issues/31365, this is (usually) a bad idea:
+please change the definition to take in a `LinearMap` argument instead.
+Note that this linter has false positives if a LinearMapClass is just coerced to a function.
+Note: the 'proper' linter check doesn't fire here; there's still a bug to fix! -/
+-/
+#guard_msgs in
+#lint only defsWithMorphismClass
