cf4cf80e10bf6331ff4e89ac63e7c6fa0cbcff84
Michael Rothgang <rothgang@math.uni-bonn.de>
2026-09-03 15:55:09 +0200

    Basic test: not run yet

--- MathlibTest/Linter/BundledMorphismClasses.lean
+++ MathlibTest/Linter/BundledMorphismClasses.lean
@@ -1,5 +1,13 @@
 module
 
 import Mathlib.Tactic.Linter.BundledMorphismClasses
+import Mathlib.Algebra.Module.LinearMap.Defs
 
--- TODO: insert a test here!
+variable {R M N : Type*} [Semiring R] [AddCommMonoid M] [AddCommMonoid N] [Module R M] [Module R N]
+  {F : Type*} [FunLike F M N] [LinearMapClass F R M N]
+
+-- A definition taking in a linear map: all is fine.
+def LinearMap.foo (_f : M →ₗ[R] N) : ℕ := 37
+
+-- A definition taking in a LinearMapClass argument is not.
+def LinearMap.bar (_f : F) : ℕ := 37
