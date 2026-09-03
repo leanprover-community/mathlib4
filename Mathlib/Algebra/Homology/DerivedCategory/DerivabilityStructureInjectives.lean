3ebd0176d36009ac89d477a4ca8920902565155f
Joël Riou <joel.riou@universite-paris-saclay.fr>
2026-09-03 16:10:01 +0200

    moved instance

--- Mathlib/Algebra/Homology/DerivedCategory/DerivabilityStructureInjectives.lean
+++ Mathlib/Algebra/Homology/DerivedCategory/DerivabilityStructureInjectives.lean
@@ -228,20 +228,6 @@ instance :
   rw [inverseImage_quasiIso_mapCochainComplexPlus_injectiveObjectι]
   infer_instance
 
-set_option backward.isDefEq.respectTransparency false in
-open HomologicalComplex in
-instance (L : Plus C ⥤ H) [L.IsLocalization (quasiIso C)] :
-    (quotient C ⋙ L).IsLocalization (CochainComplex.Plus.quasiIso C) := by
-  refine Functor.IsLocalization.comp _ _
-    ((homotopyEquivalences C (.up ℤ)).inverseImage (CochainComplex.Plus.ι C))
-    (quasiIso C) _ ?_ ?_ ?_
-  · intro _ _ f hf
-    refine Localization.inverts L (quasiIso C) _ ?_
-    simpa [quasiIso, quotient_map_mem_quasiIso_iff]
-  · intro K L f hf
-    exact homotopyEquivalences_le_quasiIso _ _ _ hf
-  · rw [quasiIso_map_quotient_eq_quasiIso]
-
 namespace isRightDerivabilityStructure
 
 /-! The following private definitions are used to deduce that
