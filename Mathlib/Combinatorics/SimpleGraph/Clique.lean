64271f36a819239e5584bde24f48c1339fba50de
JadAbouHawili <jad-abou-hawili@hotmail.com>
2026-09-03 20:23:03 +0300

    Update Mathlib/Combinatorics/SimpleGraph/Clique.lean
    Co-authored-by: Snir Broshi <26556598+SnirBroshi@users.noreply.github.com>

--- Mathlib/Combinatorics/SimpleGraph/Clique.lean
+++ Mathlib/Combinatorics/SimpleGraph/Clique.lean
@@ -1096,7 +1096,7 @@ theorem indepNum_eq_zero [Finite α] : G.indepNum = 0 ↔ IsEmpty α := by
     have one_indep : G.IsNIndepSet 1 {a} := by simp
     have : 1 ≤ G.indepNum  := IsIndepSet.card_le_indepNum one_indep.isIndepSet
     grind
-  · have : ∀ (s : Finset α ) , s = ∅ := fun s ↦ eq_empty_of_forall_notMem fun x a ↦ h x
+  · have : ∀ (s : Finset α), s = ∅ := fun s ↦ eq_empty_of_forall_notMem fun x a ↦ h x
     simp [indepNum, this, exists_const]
 
 end IndepNumber
