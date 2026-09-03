5762b8188bf50c6dd25b4f16e549cc9e48e73b1a
Thomas R. Murrills <68410468+thorimur@users.noreply.github.com>
2026-09-03 17:31:52 +0000

    feat: linter for internal constructors (#42883)
    This PR lints against using internal constructors during elaboration (i.e. those with a name component that starts with `_`, e.g. `_mkInternal`).
    
    This allows us to preserve defeq properties (such as in category theory) and avoid `set_option backward.privateInPublic false` while still preventing uses downstream through `⟨_⟩`.
    
    This linter does not fire in the file in which the internal constructor is defined.

--- Mathlib.lean
+++ Mathlib.lean
@@ -7583,6 +7583,7 @@ public import Mathlib.Tactic.Linter.HashCommandLinter
 public import Mathlib.Tactic.Linter.HaveILetI
 public import Mathlib.Tactic.Linter.HaveLetLinter
 public import Mathlib.Tactic.Linter.Header
+public import Mathlib.Tactic.Linter.InternalConstructor
 public import Mathlib.Tactic.Linter.Lint
 public import Mathlib.Tactic.Linter.MinImports
 public import Mathlib.Tactic.Linter.Multigoal
