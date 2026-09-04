d3de6af393ee972b875e5aa19773608f45c6791e
Whning0513 <whning@zju.edu.cn>
2026-09-04 20:44:47 +0800

    Add lemmas for List.toChunks

--- Mathlib.lean
+++ Mathlib.lean
@@ -4229,6 +4229,7 @@ public import Mathlib.Data.List.Sym
 public import Mathlib.Data.List.TFAE
 public import Mathlib.Data.List.TakeDrop
 public import Mathlib.Data.List.TakeWhile
+public import Mathlib.Data.List.ToChunks
 public import Mathlib.Data.List.ToFinsupp
 public import Mathlib.Data.List.Triplewise
 public import Mathlib.Data.List.Zip
