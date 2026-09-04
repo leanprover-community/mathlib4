18a0cdca71b21b79a270049fee831727ea92332c
JX-Mo <jiaximo1998@163.com>
2026-09-04 15:18:36 +0800

    Inline helper `invariantsMk_apply` reduces 40% elaboration effort

--- Mathlib/RepresentationTheory/Hecke/Basic.lean
+++ Mathlib/RepresentationTheory/Hecke/Basic.lean
@@ -32,41 +32,49 @@ open MonoidAlgebra
 
 variable (k) {H} in
 /-- The basis vector in `k[G ⧸ H]` of a left coset. -/
-noncomputable abbrev cosetVector (x : G ⧸ H) : k[G ⧸ H] := single x 1
+noncomputable def cosetVector (x : G ⧸ H) : k[G ⧸ H] := single x 1
 
 @[simp]
-lemma cosetVector_mem_eq (h : H) :
+lemma ofMulAction_apply_cosetVector (g : G) (x : G ⧸ H) :
+    ofMulAction k G (G ⧸ H) g (cosetVector k x)  =  cosetVector k (g • x) := by
+  simp [cosetVector]
+
+@[simp]
+lemma cosetVector_mem (h : H) :
     cosetVector k h = cosetVector k ((1 : G) : G ⧸ H) := by
   congr 1
   simp [QuotientGroup.eq]
+
+@[simp]
+lemma coeff_cosetVector (x : G ⧸ H) :
+    (cosetVector k x).coeff = Finsupp.single x (1 : k) := rfl
 
 @[ext]
 lemma HeckeModule.ext {H : Subgroup G} (f g : HeckeModule H ρ)
     (h : f (cosetVector k (1 : G)) = g (cosetVector k (1 : G))) : f = g := by
   ext x
-  simpa [← IntertwiningMap.isIntertwining] using congrArg (ρ x.out) h
+  simpa [← IntertwiningMap.isIntertwining, cosetVector] using congrArg (ρ x.out) h
 
 /-- Evaluation at the trivial coset gives a linear equivalence between the `H`-invariants of `ρ` and
 the Hecke module `Hom_G(k[G ⧸ H], ρ)`. -/
 @[simps symm_apply]
 noncomputable def HeckeModule.invariantsEquiv :
-    invariants (ρ.comp H.subtype) ≃ₗ[k] HeckeModule H ρ where
-  toLinearMap :=
-    { toFun v :=
-        ⟨ Finsupp.lift V k (G ⧸ H)
-          ( fun x => Quotient.liftOn x (fun x => ρ x v) (fun a b hab => by
-              let h : H := ⟨a⁻¹ * b, QuotientGroup.leftRel_apply.mp hab⟩
-              have : ρ h v = v := v.2 h
-              nth_rw 1 [← this]
-              simp [h])) ∘ₗ (MonoidAlgebra.coeffLinearEquiv k).toLinearMap,
-          fun _ => by
-            ext z
-            simpa using Quotient.inductionOn z (by simp [MulAction.Quotient.smul_mk])⟩
-      map_add' _ _ := by ext; simp
-      map_smul' _ _ := by ext; simp}
-  invFun f := ⟨f (cosetVector k (1 : G)), by simp [← IntertwiningMap.isIntertwining]⟩
-  left_inv _ := by simp
-  right_inv _ := by ext; simp
+    invariants (ρ.comp H.subtype) ≃ₗ[k] HeckeModule H ρ :=
+  let invariantsMk (v :invariants (ρ.comp H.subtype)) : HeckeModule H ρ :=
+    ⟨ Finsupp.linearCombination k (Quotient.lift (fun x => ρ x v) (fun a b hab => by
+        nth_rw 1 [← v.2 ⟨a⁻¹ * b, QuotientGroup.leftRel_apply.mp hab⟩]
+        simp)) ∘ₗ (MonoidAlgebra.coeffLinearEquiv k).toLinearMap,
+      fun _ => by ext z; simpa using Quotient.inductionOn z (by simp)⟩
+  have invariantsMk_apply (v : invariants (ρ.comp H.subtype)) :
+      invariantsMk v (cosetVector k (1 : G)) = v := by
+    simp [invariantsMk]
+  { toLinearMap :=
+    { toFun := invariantsMk
+      map_add' _ _ := by ext; simp [invariantsMk_apply]
+      map_smul' _ _ := by ext; simp [invariantsMk_apply]}
+    invFun f := ⟨f (cosetVector k (1 : G)), by simp [← IntertwiningMap.isIntertwining]⟩
+    left_inv _ := Subtype.ext (invariantsMk_apply _)
+    right_inv _ := by ext; exact invariantsMk_apply _}
 
 @[simp]
 lemma HeckeModule.invariantsEquiv_apply (g : G) (v : invariants (ρ.comp H.subtype)) :
