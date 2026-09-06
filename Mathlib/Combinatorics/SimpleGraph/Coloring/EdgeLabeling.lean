2283674d1afece7eb6434d6c48feb5729285ceb3
Bhavik Mehta <bhavikmehta8@gmail.com>
2026-09-06 19:49:17 +0100

    Apply suggestion from @SnirBroshi
    Co-authored-by: Snir Broshi <26556598+SnirBroshi@users.noreply.github.com>

--- Mathlib/Combinatorics/SimpleGraph/Coloring/EdgeLabeling.lean
+++ Mathlib/Combinatorics/SimpleGraph/Coloring/EdgeLabeling.lean
@@ -213,7 +213,7 @@ theorem TopEdgeLabeling.labelGraph_toTopEdgeLabeling [DecidableEq V]
 namespace EdgeLabeling
 
 /-- The predicate `C.MonochromaticBetween X Y k` says every edge between `X` and `Y` is labelled
-`k` by the labeling `C`. -/
+`k` by the labelling `C`. -/
 def MonochromaticBetween (C : EdgeLabeling G K) (X Y : Set V) (k : K) : Prop :=
   ∀ ⦃x⦄, x ∈ X → ∀ ⦃y⦄, y ∈ Y → (h : G.Adj x y) → C.get x y h = k
 
