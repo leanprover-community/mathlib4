22f13dfecb3d07eafa893b8c09219052ce80c49b
Michael Rothgang <rothgang@math.uni-bonn.de>
2026-09-07 22:23:50 +0200

    All squashed

--- Mathlib/Algebra/Algebra/Unitization.lean
+++ Mathlib/Algebra/Algebra/Unitization.lean
@@ -790,8 +790,8 @@ variable [Semiring C] [Algebra R C] [StarRing C]
 /-- See note [partially-applied ext lemmas] -/
 @[ext]
 theorem starAlgHom_ext {φ ψ : Unitization R A →⋆ₐ[R] C}
-    (h : (φ : Unitization R A →⋆ₙₐ[R] C).comp (Unitization.inrNonUnitalStarAlgHom R A) =
-      (ψ : Unitization R A →⋆ₙₐ[R] C).comp (Unitization.inrNonUnitalStarAlgHom R A)) :
+    (h : (NonUnitalStarAlgHom.ofClass φ).comp (Unitization.inrNonUnitalStarAlgHom R A) =
+      (NonUnitalStarAlgHom.ofClass ψ).comp (Unitization.inrNonUnitalStarAlgHom R A)) :
     φ = ψ :=
   Unitization.algHom_ext'' <| DFunLike.congr_fun h
 
