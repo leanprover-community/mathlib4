58012b6e62d2703120b7ca95260294d4cc55dbdc
Kim Morrison <kim@tqft.net>
2026-09-07 12:29:32 +0000

    test(RealRootCount): synchronize test source headers

--- MathlibTest/Sturm.lean
+++ MathlibTest/Sturm.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2026 Lean FRO, LLC. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Kim Morrison
+-/
+
 import Mathlib.Analysis.Polynomial.Sturm.Certificate
 import Mathlib.Tactic.NormNum
 
