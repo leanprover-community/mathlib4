c9e900c8cb0ac8f2a478a6ff8d5b4a1c5d409272
JX-Mo <jiaximo1998@163.com>
2026-09-07 23:49:10 +0800

    Update Mathlib/RepresentationTheory/Subrepresentation.lean
    Co-authored-by: Johan Commelin <johan@commelin.net>

--- Mathlib/RepresentationTheory/Subrepresentation.lean
+++ Mathlib/RepresentationTheory/Subrepresentation.lean
@@ -59,8 +59,8 @@ def toRepresentation (ρ' : Subrepresentation ρ) : Representation A G ρ'.toSub
 
 @[simp]
 lemma toRepresentation_apply_mk {ρ' : Subrepresentation ρ} {g : G} {v w : W} {hv : v ∈ ρ'}
-    {hw : w ∈ ρ'}
-    : ρ'.toRepresentation g ⟨v, hv⟩ = ⟨w, hw⟩ ↔ ρ g v = w := by
+    {hw : w ∈ ρ'} :
+    ρ'.toRepresentation g ⟨v, hv⟩ = ⟨w, hw⟩ ↔ ρ g v = w := by
   rw [Subtype.ext_iff]; rfl
 
 lemma toRepresentation_apply_coe {ρ' : Subrepresentation ρ} {g : G} {v w : ρ'.toSubmodule}
