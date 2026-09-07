b9262ebeb16543e10c36957f46edf1c0b75428f5
Eric Wieser <wieser.eric@gmail.com>
2026-09-07 23:04:08 +0100

    Update Mathlib/Algebra/CharP/Pi.lean
    Co-authored-by: Monica Omar <23701951+themathqueen@users.noreply.github.com>

--- Mathlib/Algebra/CharP/Pi.lean
+++ Mathlib/Algebra/CharP/Pi.lean
@@ -19,7 +19,7 @@ universe u v
 section
 variable {ι : Type*} {α : ι → Type*}
 
-theorem CharZero.pi (i : ι) [∀ i, AddMonoidWithOne (α i)] [CharZero (α i)] :
+theorem CharZero.pi (i : ι) [Π i, AddMonoidWithOne (α i)] [CharZero (α i)] :
     CharZero (Π i, α i) where
   cast_injective _ _ h := Nat.cast_injective <| congrFun h i
 
