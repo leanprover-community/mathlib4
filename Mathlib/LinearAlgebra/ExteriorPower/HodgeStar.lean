510134b14f565894ad88de5288242810b432ba0e
Kirill Kondrashov <kondrashovks1989@gmail.com>
2026-09-07 16:37:26 +0200

    Update authors

--- Mathlib/LinearAlgebra/ExteriorPower/HodgeStar.lean
+++ Mathlib/LinearAlgebra/ExteriorPower/HodgeStar.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2026 Kirill Kondrashov. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Kirill Kondrashov
+Authors: Kirill Kondrashov, Oliver Nash
 -/
 module
 
