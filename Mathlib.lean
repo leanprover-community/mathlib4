27a211ec467d2af4051fadd34c6a088548ab7e9d
abhijitaj1997 <abhijit.aj.1997@gmail.com>
2026-09-08 01:28:51 +0200

    regenerate import files

--- Mathlib.lean
+++ Mathlib.lean
@@ -3192,6 +3192,7 @@ public import Mathlib.CategoryTheory.Monoidal.Cartesian.FunctorCategory
 public import Mathlib.CategoryTheory.Monoidal.Cartesian.Grp
 public import Mathlib.CategoryTheory.Monoidal.Cartesian.GrpLimits
 public import Mathlib.CategoryTheory.Monoidal.Cartesian.Grp_
+public import Mathlib.CategoryTheory.Monoidal.Cartesian.Hom
 public import Mathlib.CategoryTheory.Monoidal.Cartesian.InfSemilattice
 public import Mathlib.CategoryTheory.Monoidal.Cartesian.Mod
 public import Mathlib.CategoryTheory.Monoidal.Cartesian.Mod_
