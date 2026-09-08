0407eee32c38576a23f1976f3617bd8db0665d7f
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2026-09-08 23:45:01 +0200

    revert Ordnode

--- Mathlib/Data/Ordmap/Ordnode.lean
+++ Mathlib/Data/Ordmap/Ordnode.lean
@@ -5,12 +5,10 @@ Authors: Mario Carneiro
 -/
 module
 
-public import Batteries.Data.List.Lemmas
 public import Mathlib.Order.Compare
 public import Mathlib.Data.Nat.PSub
 public import Mathlib.Data.Tree.Basic
 public import Batteries.Data.List.Basic
-public import Mathlib.Util.CompileInductive
 
 /-!
 # Ordered sets
