b9607e6cef7c01b2d6508b947bc517e06cfe39ae
Adam Topaz <adamtopaz@users.noreply.github.com>
2026-09-08 14:43:09 -0600

    chore(FieldTheory/AbsoluteGaloisGroup): use separable closure

--- Mathlib/FieldTheory/AbsoluteGaloisGroup.lean
+++ Mathlib/FieldTheory/AbsoluteGaloisGroup.lean
@@ -5,7 +5,7 @@ Authors: María Inés de Frutos-Fernández
 -/
 module
 
-public import Mathlib.FieldTheory.IsAlgClosed.AlgebraicClosure
+public import Mathlib.FieldTheory.IsSepClosed
 public import Mathlib.FieldTheory.KrullTopology
 public import Mathlib.Topology.Algebra.Group.TopologicalAbelianization
 
@@ -15,8 +15,8 @@ public import Mathlib.Topology.Algebra.Group.TopologicalAbelianization
 We define the absolute Galois group of a field `K` and its topological abelianization.
 
 ## Main definitions
-- `Field.absoluteGaloisGroup` : The Galois group of the field extension `K^al/K`, where `K^al` is an
-  algebraic closure of `K`.
+- `Field.absoluteGaloisGroup` : The Galois group of the field extension `K^sep/K`,
+  where `K^sep` is a separable closure of `K`.
 - `Field.absoluteGaloisGroupAbelianization` : The topological abelianization of
   `Field.absoluteGaloisGroup K`, that is, the quotient of `Field.absoluteGaloisGroup K` by the
   topological closure of its commutator subgroup.
@@ -26,7 +26,7 @@ We define the absolute Galois group of a field `K` and its topological abelianiz
   commutator of `absoluteGaloisGroup` is a normal subgroup.
 
 ## Tags
-field, algebraic closure, galois group, abelianization
+field, separable closure, galois group, abelianization
 
 -/
 
@@ -38,9 +38,9 @@ variable (K L : Type*) [Field K] [Field L]
 
 /-! ### The absolute Galois group -/
 
-/-- The absolute Galois group of `K`, defined as the Galois group of the field extension `K^al/K`,
-  where `K^al` is an algebraic closure of `K`. -/
-def absoluteGaloisGroup := AlgebraicClosure K ≃ₐ[K] AlgebraicClosure K
+/-- The absolute Galois group of `K`, defined as the Galois group of the field extension `K^sep/K`,
+  where `K^sep` is a separable closure of `K`. -/
+def absoluteGaloisGroup := SeparableClosure K ≃ₐ[K] SeparableClosure K
 deriving Group, TopologicalSpace, IsTopologicalGroup
 
 /-- `absoluteGaloisGroup` is a topological space with the Krull topology. -/
@@ -50,11 +50,11 @@ local notation "G_K" => absoluteGaloisGroup
 
 section
 
-variable [Algebra K L] [Algebra (AlgebraicClosure K) (AlgebraicClosure L)]
-  [IsScalarTower K (AlgebraicClosure K) (AlgebraicClosure L)]
+variable [Algebra K L] [Algebra (SeparableClosure K) (SeparableClosure L)]
+  [IsScalarTower K (SeparableClosure K) (SeparableClosure L)]
 
 open IntermediateField in
-/-- A commuting square of two fields and their algebraic closures induces a continuous homomorphism
+/-- A commuting square of two fields and their separable closures induces a continuous homomorphism
 of their absolute Galois groups. -/
 @[simps!]
 noncomputable def absoluteGaloisGroup.mapOfAlgebra : G_K L →ₜ* G_K K :=
@@ -62,14 +62,14 @@ noncomputable def absoluteGaloisGroup.mapOfAlgebra : G_K L →ₜ* G_K K :=
   { __ := F
     continuous_toFun := by
       classical
-      let f := IsScalarTower.toAlgHom K (AlgebraicClosure K) (AlgebraicClosure L)
+      let f := IsScalarTower.toAlgHom K (SeparableClosure K) (SeparableClosure L)
       apply continuous_of_continuousAt_one F
       rw [ContinuousAt, map_one]
-      refine ((galGroupBasis L (AlgebraicClosure L)).nhds_one_hasBasis.tendsto_iff
-        (galGroupBasis K (AlgebraicClosure K)).nhds_one_hasBasis).mpr ?_
+      refine ((galGroupBasis L (SeparableClosure L)).nhds_one_hasBasis.tendsto_iff
+        (galGroupBasis K (SeparableClosure K)).nhds_one_hasBasis).mpr ?_
       rintro _ ⟨_, ⟨F, hF : FiniteDimensional _ _, rfl⟩, rfl⟩
       refine ⟨_, ⟨_, ⟨adjoin L (F.map f), ?_, rfl⟩, rfl⟩, fun σ hσ x ↦ ?_⟩
-      · suffices Algebra.EssFiniteType L (adjoin L (F.map f : Set (AlgebraicClosure L))) by
+      · suffices Algebra.EssFiniteType L (adjoin L (F.map f : Set (SeparableClosure L))) by
           apply Algebra.finite_of_essFiniteType_of_isAlgebraic
         replace hF : Algebra.EssFiniteType K F := inferInstance
         rw [essFiniteType_iff] at hF ⊢
@@ -77,18 +77,18 @@ noncomputable def absoluteGaloisGroup.mapOfAlgebra : G_K L →ₜ* G_K K :=
         use s.image f
         rw [adjoin_map, adjoin_adjoin_right, Finset.coe_image]
       · exact f.injective <| ((σ.restrictScalarsHom K).restrictNormal_commutes
-          (AlgebraicClosure K) x).trans <| hσ ⟨f x, subset_adjoin _ _ ⟨_, x.2, rfl⟩⟩ }
+          (SeparableClosure K) x).trans <| hσ ⟨f x, subset_adjoin _ _ ⟨_, x.2, rfl⟩⟩ }
 
 end
 
 variable {K L} in
 /-- An embedding of fields induces a continuous homomorphism of absolute Galois groups.
-Note that this depends on an arbitrary choice of embedding of the algebraic closures. -/
+Note that this depends on an arbitrary choice of embedding of the separable closures. -/
 @[simps!]
 noncomputable def absoluteGaloisGroup.map (f : K →+* L) : G_K L →ₜ* G_K K :=
   letI : Algebra K L := f.toAlgebra
-  letI g : AlgebraicClosure K →ₐ[K] AlgebraicClosure L := IsAlgClosed.lift
-  letI : Algebra (AlgebraicClosure K) (AlgebraicClosure L) := g.toAlgebra
+  letI g : SeparableClosure K →ₐ[K] SeparableClosure L := IsSepClosed.lift
+  letI : Algebra (SeparableClosure K) (SeparableClosure L) := g.toAlgebra
   absoluteGaloisGroup.mapOfAlgebra K L
 
 /-! ### The topological abelianization of the absolute Galois group -/
