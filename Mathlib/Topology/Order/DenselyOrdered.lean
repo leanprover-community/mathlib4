ca97a8a64ea7431d947724f849677ec501e34a98
Etienne Marion <baguettes_casino0c@icloud.com>
2026-09-08 16:30:56 +0200

    done

--- Mathlib/Topology/Order/DenselyOrdered.lean
+++ Mathlib/Topology/Order/DenselyOrdered.lean
@@ -28,6 +28,8 @@ variable [TopologicalSpace α] [LinearOrder α] [OrderTopology α] [DenselyOrder
 
 /-- The closure of the interval `(a, +∞)` is the closed interval `[a, +∞)`, unless `a` is a top
 element. -/
+@[to_dual /-- The closure of the interval `(-∞, a)` is the closed interval `(∞, a]`,
+unless `a` is a bot element. -/]
 theorem closure_Ioi' {a : α} (h : (Ioi a).Nonempty) : closure (Ioi a) = Ici a := by
   apply Subset.antisymm
   · exact closure_minimal Ioi_subset_Ici_self isClosed_Ici
@@ -35,39 +37,28 @@ theorem closure_Ioi' {a : α} (h : (Ioi a).Nonempty) : closure (Ioi a) = Ici a :
     exact isGLB_Ioi.mem_closure h
 
 /-- The closure of the interval `(a, +∞)` is the closed interval `[a, +∞)`. -/
-@[simp]
+@[to_dual (attr := simp) /-- The closure of the interval `(-∞, a)` is the interval `(-∞, a]`. -/]
 theorem closure_Ioi (a : α) [NoMaxOrder α] : closure (Ioi a) = Ici a :=
   closure_Ioi' nonempty_Ioi
 
-/-- The closure of the interval `(-∞, a)` is the closed interval `(-∞, a]`, unless `a` is a bottom
-element. -/
-theorem closure_Iio' (h : (Iio a).Nonempty) : closure (Iio a) = Iic a :=
-  closure_Ioi' (α := αᵒᵈ) h
-
-/-- The closure of the interval `(-∞, a)` is the interval `(-∞, a]`. -/
-@[simp]
-theorem closure_Iio (a : α) [NoMinOrder α] : closure (Iio a) = Iic a :=
-  closure_Iio' nonempty_Iio
-
+@[to_dual]
 theorem IsMax.of_disjoint_nhds_Ioi {x : α} {u : Set α} (hu : u ∈ nhds x)
     (hd : Disjoint u (Set.Ioi x)) : IsMax x := by
   by_contra hx
   exact (mem_closure_iff_nhds.mp (closure_Ioi' (not_isMax_iff.mp hx) ▸ self_mem_Ici) u hu).ne_empty
     (disjoint_iff.mp hd)
 
+@[to_dual]
 theorem IsMin.of_disjoint_nhds_Iio {x : α} {u : Set α} (hu : u ∈ nhds x)
     (hd : Disjoint u (Set.Iio x)) : IsMin x :=
   IsMax.of_disjoint_nhds_Ioi (α := αᵒᵈ) hu hd
 
+@[to_dual]
 theorem nonempty_nhds_inter_Ioi {x : α} {u : Set α} (hu : u ∈ nhds x) (hx : ¬IsMax x) :
     (u ∩ Set.Ioi x).Nonempty := by
   by_contra h
   exact hx (IsMax.of_disjoint_nhds_Ioi hu (Set.disjoint_iff_inter_eq_empty.mpr
     (Set.not_nonempty_iff_eq_empty.mp h)))
-
-theorem nonempty_nhds_inter_Iio {x : α} {u : Set α} (hu : u ∈ nhds x) (hx : ¬IsMin x) :
-    (u ∩ Set.Iio x).Nonempty :=
-  nonempty_nhds_inter_Ioi (α := αᵒᵈ) hu hx
 
 /-- The closure of the open interval `(a, b)` is the closed interval `[a, b]`. -/
 @[simp]
@@ -106,44 +97,30 @@ theorem closure_Ico {a b : α} (hab : a ≠ b) : closure (Ico a b) = Icc a b := 
   · apply Subset.trans _ (closure_mono Ioo_subset_Ico_self)
     rw [closure_Ioo hab]
 
-@[simp]
+@[to_dual (attr := simp)]
 theorem interior_Ici' {a : α} (ha : (Iio a).Nonempty) : interior (Ici a) = Ioi a := by
   rw [← compl_Iio, interior_compl, closure_Iio' ha, compl_Iic]
 
+@[to_dual]
 theorem interior_Ici [NoMinOrder α] {a : α} : interior (Ici a) = Ioi a :=
   interior_Ici' nonempty_Iio
 
-@[simp]
-theorem interior_Iic' {a : α} (ha : (Ioi a).Nonempty) : interior (Iic a) = Iio a :=
-  interior_Ici' (α := αᵒᵈ) ha
-
-theorem interior_Iic [NoMaxOrder α] {a : α} : interior (Iic a) = Iio a :=
-  interior_Iic' nonempty_Ioi
-
-@[simp]
+@[simp]--@[to_dual self (reorder := 6 7, 8 9), simp]
 theorem interior_Icc [NoMinOrder α] [NoMaxOrder α] {a b : α} : interior (Icc a b) = Ioo a b := by
   rw [← Ici_inter_Iic, interior_inter, interior_Ici, interior_Iic, Ioi_inter_Iio]
 
-@[simp]
+@[simp]--@[to_dual self (reorder := 6 7, 8 9), simp]
 theorem Icc_mem_nhds_iff [NoMinOrder α] [NoMaxOrder α] {a b x : α} :
     Icc a b ∈ 𝓝 x ↔ x ∈ Ioo a b := by
   rw [← interior_Icc, mem_interior_iff_mem_nhds]
 
-@[simp]
+@[to_dual (attr := simp)]
 theorem interior_Ico [NoMinOrder α] {a b : α} : interior (Ico a b) = Ioo a b := by
   rw [← Ici_inter_Iio, interior_inter, interior_Ici, interior_Iio, Ioi_inter_Iio]
 
-@[simp]
+@[to_dual (attr := simp)]
 theorem Ico_mem_nhds_iff [NoMinOrder α] {a b x : α} : Ico a b ∈ 𝓝 x ↔ x ∈ Ioo a b := by
   rw [← interior_Ico, mem_interior_iff_mem_nhds]
-
-@[simp]
-theorem interior_Ioc [NoMaxOrder α] {a b : α} : interior (Ioc a b) = Ioo a b := by
-  rw [← Ioi_inter_Iic, interior_inter, interior_Ioi, interior_Iic, Ioi_inter_Iio]
-
-@[simp]
-theorem Ioc_mem_nhds_iff [NoMaxOrder α] {a b x : α} : Ioc a b ∈ 𝓝 x ↔ x ∈ Ioo a b := by
-  rw [← interior_Ioc, mem_interior_iff_mem_nhds]
 
 theorem closure_interior_Icc {a b : α} (h : a ≠ b) : closure (interior (Icc a b)) = Icc a b :=
   (closure_minimal interior_subset isClosed_Icc).antisymm <|
@@ -161,37 +138,26 @@ theorem Ioc_subset_closure_interior (a b : α) : Ioc a b ⊆ closure (interior (
       _ ⊆ closure (interior (Ioc a b)) :=
         closure_mono (interior_maximal Ioo_subset_Ioc_self isOpen_Ioo)
 
+@[to_dual existing]
 theorem Ico_subset_closure_interior (a b : α) : Ico a b ⊆ closure (interior (Ico a b)) := by
   simpa only [Ioc_toDual] using!
     Ioc_subset_closure_interior (OrderDual.toDual b) (OrderDual.toDual a)
 
-@[simp]
+@[to_dual (attr := simp)]
 theorem frontier_Ici' {a : α} (ha : (Iio a).Nonempty) : frontier (Ici a) = {a} := by
   simp [frontier, ha]
 
+@[to_dual]
 theorem frontier_Ici [NoMinOrder α] {a : α} : frontier (Ici a) = {a} :=
   frontier_Ici' nonempty_Iio
 
-@[simp]
-theorem frontier_Iic' {a : α} (ha : (Ioi a).Nonempty) : frontier (Iic a) = {a} := by
-  simp [frontier, ha]
-
-theorem frontier_Iic [NoMaxOrder α] {a : α} : frontier (Iic a) = {a} :=
-  frontier_Iic' nonempty_Ioi
-
-@[simp]
+@[to_dual (attr := simp)]
 theorem frontier_Ioi' {a : α} (ha : (Ioi a).Nonempty) : frontier (Ioi a) = {a} := by
   simp [frontier, closure_Ioi' ha]
 
+@[to_dual]
 theorem frontier_Ioi [NoMaxOrder α] {a : α} : frontier (Ioi a) = {a} :=
   frontier_Ioi' nonempty_Ioi
-
-@[simp]
-theorem frontier_Iio' {a : α} (ha : (Iio a).Nonempty) : frontier (Iio a) = {a} := by
-  simp [frontier, closure_Iio' ha]
-
-theorem frontier_Iio [NoMinOrder α] {a : α} : frontier (Iio a) = {a} :=
-  frontier_Iio' nonempty_Iio
 
 @[simp]
 theorem frontier_Icc [NoMinOrder α] [NoMaxOrder α] {a b : α} (h : a ≤ b) :
@@ -209,41 +175,29 @@ theorem frontier_Ico [NoMinOrder α] {a b : α} (h : a < b) : frontier (Ico a b)
 theorem frontier_Ioc [NoMaxOrder α] {a b : α} (h : a < b) : frontier (Ioc a b) = {a, b} := by
   rw [frontier, closure_Ioc h.ne, interior_Ioc, Icc_sdiff_Ioo_same h.le]
 
+@[to_dual]
 theorem nhdsWithin_Ioi_neBot' {a b : α} (H₁ : (Ioi a).Nonempty) (H₂ : a ≤ b) :
     NeBot (𝓝[Ioi a] b) :=
   mem_closure_iff_nhdsWithin_neBot.1 <| by rwa [closure_Ioi' H₁]
 
+@[to_dual]
 theorem nhdsWithin_Ioi_neBot [NoMaxOrder α] {a b : α} (H : a ≤ b) : NeBot (𝓝[Ioi a] b) :=
   nhdsWithin_Ioi_neBot' nonempty_Ioi H
 
+@[to_dual nhdsLT_neBot_of_exists_lt]
 theorem nhdsGT_neBot_of_exists_gt {a : α} (H : ∃ b, a < b) : NeBot (𝓝[>] a) :=
   nhdsWithin_Ioi_neBot' H (le_refl a)
 
+@[to_dual]
 instance nhdsGT_neBot [NoMaxOrder α] (a : α) : NeBot (𝓝[>] a) := nhdsWithin_Ioi_neBot le_rfl
 
-theorem nhdsWithin_Iio_neBot' {b c : α} (H₁ : (Iio c).Nonempty) (H₂ : b ≤ c) :
-    NeBot (𝓝[Iio c] b) :=
-  mem_closure_iff_nhdsWithin_neBot.1 <| by rwa [closure_Iio' H₁]
-
-theorem nhdsWithin_Iio_neBot [NoMinOrder α] {a b : α} (H : a ≤ b) : NeBot (𝓝[Iio b] a) :=
-  nhdsWithin_Iio_neBot' nonempty_Iio H
-
-theorem nhdsLT_neBot_of_exists_lt {b : α} (H : ∃ a, a < b) : NeBot (𝓝[<] b) :=
-  nhdsWithin_Iio_neBot' H (le_refl b)
-
-instance nhdsLT_neBot [NoMinOrder α] (a : α) : NeBot (𝓝[<] a) := nhdsWithin_Iio_neBot (le_refl a)
-
+@[to_dual left_nhdsWithin_Ioc_neBot]
 theorem right_nhdsWithin_Ico_neBot {a b : α} (H : a < b) : NeBot (𝓝[Ico a b] b) :=
   (isLUB_Ico H).nhdsWithin_neBot (nonempty_Ico.2 H)
 
-theorem left_nhdsWithin_Ioc_neBot {a b : α} (H : a < b) : NeBot (𝓝[Ioc a b] a) :=
-  (isGLB_Ioc H).nhdsWithin_neBot (nonempty_Ioc.2 H)
-
+@[to_dual right_nhdsWithin_Ioo_neBot]
 theorem left_nhdsWithin_Ioo_neBot {a b : α} (H : a < b) : NeBot (𝓝[Ioo a b] a) :=
   (isGLB_Ioo H).nhdsWithin_neBot (nonempty_Ioo.2 H)
-
-theorem right_nhdsWithin_Ioo_neBot {a b : α} (H : a < b) : NeBot (𝓝[Ioo a b] b) :=
-  (isLUB_Ioo H).nhdsWithin_neBot (nonempty_Ioo.2 H)
 
 instance (x : α) [Nontrivial α] : NeBot (𝓝[≠] x) := by
   refine forall_mem_nonempty_iff_neBot.1 fun s hs => ?_
@@ -298,7 +252,7 @@ theorem isClosed_Ico_iff {a b : α} : IsClosed (Set.Ico a b) ↔ b ≤ a := by
   exact this hab.le
 
 /-- `Set.Ioc a b` is only closed if it is empty. -/
-@[simp]
+@[to_dual existing, simp]
 theorem isClosed_Ioc_iff {a b : α} : IsClosed (Set.Ioc a b) ↔ b ≤ a := by
   refine ⟨fun h => le_of_not_gt fun hab => ?_, by simp_all⟩
   have := h.closure_eq
@@ -306,7 +260,7 @@ theorem isClosed_Ioc_iff {a b : α} : IsClosed (Set.Ioc a b) ↔ b ≤ a := by
   exact this hab.le
 
 /-- `Set.Ioo a b` is only closed if it is empty. -/
-@[simp]
+@[to_dual self, simp]
 theorem isClosed_Ioo_iff {a b : α} : IsClosed (Set.Ioo a b) ↔ b ≤ a := by
   refine ⟨fun h => le_of_not_gt fun hab => ?_, by simp_all⟩
   have := h.closure_eq
