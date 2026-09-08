ac83a6392f42cddc1cd9a44e8330c34885054b65
Aaron Liu <aaronliu2008@outlook.com>
2026-09-07 22:03:34 -0400

    doesn't exist

--- Mathlib.lean
+++ Mathlib.lean
@@ -3956,7 +3956,6 @@ public import Mathlib.Data.DFinsupp.Submonoid
 public import Mathlib.Data.DFinsupp.WellFounded
 public import Mathlib.Data.DList.Instances
 public import Mathlib.Data.Dyadic
-public import Mathlib.Data.Dyadic.IsLocalization
 public import Mathlib.Data.Dyadic.OrderedRing
 public import Mathlib.Data.Dyadic.ToReal
 public import Mathlib.Data.ENNReal.Action
