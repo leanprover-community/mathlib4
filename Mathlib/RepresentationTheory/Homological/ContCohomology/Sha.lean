b6f3b14a3a6b3c834f820451546d80cc099867c3
Whysoserioushah <yx3021@ic.ac.uk>
2026-09-08 10:56:17 +0100

    squeeze line

--- Mathlib/RepresentationTheory/Homological/ContCohomology/Sha.lean
+++ Mathlib/RepresentationTheory/Homological/ContCohomology/Sha.lean
@@ -26,8 +26,7 @@ variety over K, and `n = 1`, this recovers the classical definition of the Tate-
 universe u v
 
 variable {K : Type u} {V : Type v} [Field K] (f : V → Type u) [h : ∀ v : V, Field (f v)]
-  [h' : ∀ v : V, Algebra K (f v)] (A : TopRep ℤ (Field.absoluteGaloisGroup K))
-  (n : ℕ)
+  [h' : ∀ v : V, Algebra K (f v)] (A : TopRep ℤ (Field.absoluteGaloisGroup K)) (n : ℕ)
 
 open CategoryTheory
 
