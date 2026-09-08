8db47d24b78b57fa9ff4ba8c848c9574ec411098
Kim Morrison <kim@tqft.net>
2026-09-08 00:12:45 +0000

    build: satisfy dependency revision policy

--- lakefile.lean
+++ lakefile.lean
@@ -18,6 +18,11 @@ require "leanprover-community" / "importGraph" @ git "main"
 require "leanprover-community" / "LeanSearchClient" @ git "main"
 require "leanprover-community" / "plausible" @ git "main"
 
+require HexBasic from git "https://github.com/leanprover/hex-basic.git" @ "main"
+require HexArith from git "https://github.com/leanprover/hex-arith.git" @ "main"
+require HexModArith from git "https://github.com/leanprover/hex-mod-arith.git" @ "main"
+require HexPoly from git "https://github.com/leanprover/hex-poly.git" @ "main"
+require HexPolyZ from git "https://github.com/leanprover/hex-poly-z.git" @ "main"
 require HexRealRoots from git "https://github.com/leanprover/hex-real-roots.git" @ "main"
 
 
