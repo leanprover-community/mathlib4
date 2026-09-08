c92eedf0204d4b5e3b16d131ba5962bcaff066f5
metakunt <m@k.nt>
2026-09-08 20:41:38 +0200

    Optimise imports

--- Mathlib/RingTheory/LittleWedderburn.lean
+++ Mathlib/RingTheory/LittleWedderburn.lean
@@ -5,9 +5,11 @@ Authors: Johan Commelin, Eric Rodriguez
 -/
 module
 
-public import Mathlib.Algebra.GroupWithZero.Action.Center
-public import Mathlib.GroupTheory.ClassEquation
-public import Mathlib.RingTheory.Polynomial.Cyclotomic.Eval
+import Mathlib.Algebra.GroupWithZero.Action.Center
+public import Mathlib.Algebra.Field.IsField
+public import Mathlib.Basic.Finite.Defs
+import Mathlib.GroupTheory.ClassEquation
+import Mathlib.RingTheory.Polynomial.Cyclotomic.Eval
 
 /-!
 # Wedderburn's Little Theorem
