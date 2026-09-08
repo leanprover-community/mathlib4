5724928e24fe949066b93df422ff64412d65c4d2
Whysoserioushah <yx3021@ic.ac.uk>
2026-09-08 11:41:24 +0100

    formatting

--- Mathlib/RepresentationTheory/Homological/ContCohomology/Sha.lean
+++ Mathlib/RepresentationTheory/Homological/ContCohomology/Sha.lean
@@ -20,7 +20,10 @@ which induces maps between absolute Galois groups and hence maps between cohomol
 When `V` is the set of places of a global field `K`, `A` is the set of rational points of an abelian
 variety over K, and `n = 1`, this recovers the classical definition of the Tate-Shafarevich group.
 
-Reference: https://en.wikipedia.org/wiki/Tate%E2%80%93Shafarevich_group
+## Reference
+
+* [Wikipedia, *Tate–Shafarevich group*](https://en.wikipedia.org/wiki/Tate%E2%80%93Shafarevich_group)
+
 -/
 
 @[expose] public section
