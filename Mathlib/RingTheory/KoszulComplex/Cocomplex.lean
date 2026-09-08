463170e269e389e1dfc0dab36cb6cd6b54e3c4f0
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2026-09-08 17:14:37 +0200

    use grind

--- Mathlib/RingTheory/KoszulComplex/Cocomplex.lean
+++ Mathlib/RingTheory/KoszulComplex/Cocomplex.lean
@@ -165,6 +165,6 @@ lemma isZero_X_ofList_of_length_le (l : List R) (i : ℕ) (hi : l.length < i) :
     IsZero ((ofList l).X i) :=
   isZero_X_of_span_eq_top R l.get
   (Pi.basisFun R (Fin l.length)) (Pi.basisFun R (Fin l.length)).span_eq i
-  (by simpa [Nat.card_eq_fintype_card] using hi)
+  (by grind [Nat.card_eq_fintype_card, Fintype.card_fin])
 
 end koszulCocomplex
