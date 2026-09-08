f57407bcb30b11eafde237d9e15b4db58858f403
Michael Rothgang <rothgang@math.uni-bonn.de>
2026-09-08 22:53:15 +0200

    Update Mathlib/Algebra/Star/NonUnitalSubalgebra.lean
    Co-authored-by: Monica Omar <23701951+themathqueen@users.noreply.github.com>

--- Mathlib/Algebra/Star/NonUnitalSubalgebra.lean
+++ Mathlib/Algebra/Star/NonUnitalSubalgebra.lean
@@ -469,7 +469,7 @@ theorem range_comp_le_range (f : A →⋆ₙₐ[R] B) (g : B →⋆ₙₐ[R] C) 
 /-- Restrict the codomain of a non-unital star algebra homomorphism. -/
 def codRestrict (f : A →⋆ₙₐ[R] B) (S : NonUnitalStarSubalgebra R B) (hf : ∀ x, f x ∈ S) :
     A →⋆ₙₐ[R] S where
-  toNonUnitalAlgHom := NonUnitalAlgHom.codRestrict f S.toNonUnitalSubalgebra hf
+  toNonUnitalAlgHom := NonUnitalAlgHom.codRestrict f.toNonUnitalAlgHom S.toNonUnitalSubalgebra hf
   map_star' := fun a => Subtype.ext <| by exact map_star f a
 
 @[simp]
