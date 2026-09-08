4fc73894fbde7a60df7a594d408ca61cf203a24f
Aaron Liu <aaronliu2008@outlook.com>
2026-09-07 21:58:53 -0400

    fix import order 2

--- Mathlib.lean
+++ Mathlib.lean
@@ -3955,10 +3955,10 @@ public import Mathlib.Data.DFinsupp.Small
 public import Mathlib.Data.DFinsupp.Submonoid
 public import Mathlib.Data.DFinsupp.WellFounded
 public import Mathlib.Data.DList.Instances
+public import Mathlib.Data.Dyadic
 public import Mathlib.Data.Dyadic.IsLocalization
 public import Mathlib.Data.Dyadic.OrderedRing
 public import Mathlib.Data.Dyadic.ToReal
-public import Mathlib.Data.Dyadic
 public import Mathlib.Data.ENNReal.Action
 public import Mathlib.Data.ENNReal.Basic
 public import Mathlib.Data.ENNReal.BigOperators
