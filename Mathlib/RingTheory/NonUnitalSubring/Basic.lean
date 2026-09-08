583470bc90d126ea310b7e4ac5a9afbc9ab8f701
Michael Rothgang <rothgang@math.uni-bonn.de>
2026-09-09 00:54:40 +0200

    More definitions, found by the linter

--- Mathlib/RingTheory/NonUnitalSubring/Basic.lean
+++ Mathlib/RingTheory/NonUnitalSubring/Basic.lean
@@ -387,17 +387,16 @@ theorem mem_center_iff {z : R} : z ∈ center R ↔ ∀ g, g * z = z * g := Subs
 instance decidableMemCenter [DecidableEq R] [Fintype R] : DecidablePred (· ∈ center R) := fun _ =>
   decidable_of_iff' _ mem_center_iff
 
-theorem map_center_le_center {F} [FunLike F R S] [NonUnitalRingHomClass F R S] {f : F}
-    (hf : Function.Surjective f) : map f (center R) ≤ center S :=
+theorem map_center_le_center {f : R →ₙ+* S} (hf : Function.Surjective f) :
+    map f (center R) ≤ center S :=
   Set.image_center_subset hf
 
-theorem comap_center_le_center {F} [FunLike F R S] [NonUnitalRingHomClass F R S] {f : F}
-    (hf : Function.Injective f) : comap f (center S) ≤ center R :=
+theorem comap_center_le_center {f : R →ₙ+* S} (hf : Function.Injective f) :
+    comap f (center S) ≤ center R :=
   Set.preimage_center_subset hf
 
 @[simp]
-theorem map_center_eq {F} [EquivLike F R S] [RingEquivClass F R S] (f : F) :
-    map f (center R) = center S :=
+theorem map_center_eq (f : R ≃+* S) : map f (center R) = center S :=
   SetLike.coe_injective (Set.image_center_eq f)
 
 @[simp]
