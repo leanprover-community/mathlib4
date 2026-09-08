4a97aa5109a55b6cccfbe5b5d4f0228a09c40937
Michael Rothgang <rothgang@math.uni-bonn.de>
2026-09-08 10:21:54 +0200

    A few concretisations, clearly good

--- Mathlib/RingTheory/Nilpotent/Lemmas.lean
+++ Mathlib/RingTheory/Nilpotent/Lemmas.lean
@@ -27,8 +27,8 @@ open Function Module Set
 variable {R S : Type*} {x y : R}
 
 theorem RingHom.ker_isRadical_iff_reduced_of_surjective {S F} [CommSemiring R] [Semiring S]
-    [FunLike F R S] [RingHomClass F R S] {f : F} (hf : Function.Surjective f) :
-    (RingHom.ker f).IsRadical ↔ IsReduced S := by
+    [FunLike F R S] [RingHomClass F R S] {f : R →+* S} (hf : Function.Surjective f) :
+    f.ker.IsRadical ↔ IsReduced S := by
   simp_rw [isReduced_iff, hf.forall, IsNilpotent, ← map_pow, ← RingHom.mem_ker]
   rfl
 
