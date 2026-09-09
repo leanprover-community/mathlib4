749e95d3ccd39c8b77c6383c854a98e33f991bd5
metakunt <m@k.nt>
2026-09-09 18:02:49 +0200

    Fix Equiv.pairwise

--- Mathlib/Logic/Equiv/Pairwise.lean
+++ Mathlib/Logic/Equiv/Pairwise.lean
@@ -16,10 +16,10 @@ public section
 
 open scoped Function -- required for scoped `on` notation
 
-lemma EmbeddingLike.pairwise_comp {X : Type*} {Y : Type*} {F} [FunLike F Y X] [EmbeddingLike F Y X]
-    (f : F) {p : X → X → Prop} (h : Pairwise p) : Pairwise (p on f) :=
+lemma EmbeddingLike.pairwise'_comp {X : Type*} {Y : Type*} {F} [FunLike F Y X] [EmbeddingLike F Y X]
+    (f : F) {p : X → X → Prop} (h : Pairwise' p) : Pairwise' (p on f) :=
   h.comp_of_injective <| EmbeddingLike.injective f
 
-lemma EquivLike.pairwise_comp_iff {X : Type*} {Y : Type*} {F} [EquivLike F Y X]
-    (f : F) (p : X → X → Prop) : Pairwise (p on f) ↔ Pairwise p :=
+lemma EquivLike.pairwise'_comp_iff {X : Type*} {Y : Type*} {F} [EquivLike F Y X]
+    (f : F) (p : X → X → Prop) : Pairwise' (p on f) ↔ Pairwise' p :=
   (EquivLike.bijective f).pairwise_comp_iff
