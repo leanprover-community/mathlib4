fb1b65be7cf47e0e188398100798329e5c659ee0
Yi.Yuan <kysyy1@126.com>
2026-09-09 16:02:12 +0800

    Apply suggestion from @j-loreaux
    Co-authored-by: Jireh Loreaux <loreaujy@gmail.com>

--- Mathlib/Topology/Closure.lean
+++ Mathlib/Topology/Closure.lean
@@ -545,7 +545,7 @@ theorem frontier_union_subset (s t : Set X) :
     frontier (s ∪ t) ⊆ frontier s ∩ closure tᶜ ∪ closure sᶜ ∩ frontier t := by
   simpa only [frontier_compl, ← compl_union] using frontier_inter_subset sᶜ tᶜ
 
-lemma frontier_biUnion_subset {ι : Type*} (I : Finset ι) (s : ι → Set X) :
+lemma Finset.frontier_biUnion_subset {ι : Type*} (I : Finset ι) (s : ι → Set X) :
     frontier (⋃ i ∈ I, s i) ⊆ ⋃ i ∈ I, frontier (s i) := by
   classical induction I using Finset.induction_on with
   | empty => simp
