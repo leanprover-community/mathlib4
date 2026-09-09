0af50531642c3a12ae523451e366caa2c9a8f275
Super User <yuanyi-350>
2026-09-09 16:13:12 +0800

    apply suggestion

--- Mathlib/Topology/Closure.lean
+++ Mathlib/Topology/Closure.lean
@@ -547,11 +547,17 @@ theorem frontier_union_subset (s t : Set X) :
 
 lemma Finset.frontier_biUnion_subset {ι : Type*} (I : Finset ι) (s : ι → Set X) :
     frontier (⋃ i ∈ I, s i) ⊆ ⋃ i ∈ I, frontier (s i) := by
-  classical induction I using Finset.induction_on with
+  classical
+  induction I using Finset.induction_on with
   | empty => simp
   | insert i I hi ih =>
     simp only [Finset.mem_insert, iUnion_iUnion_eq_or_left]
     grind [frontier_union_subset]
+
+lemma Set.Finite.frontier_biUnion_subset {ι : Type*} {I : Set ι} (hI : I.Finite) (s : ι → Set X) :
+    frontier (⋃ i ∈ I, s i) ⊆ ⋃ i ∈ I, frontier (s i) := by
+  simpa only [hI.mem_toFinset] using hI.toFinset.frontier_biUnion_subset s
+
 theorem IsClosed.frontier_eq (hs : IsClosed s) : frontier s = s \ interior s := by
   rw [frontier, hs.closure_eq]
 
