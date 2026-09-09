f4adfe8c4d5525bad5574d5e29be9c3201af8844
artie2000 <artem.khovanov@gmail.com>
2026-09-09 19:35:39 +0100

    remove nonrec

--- Mathlib/Geometry/Convex/Cone/Pointed.lean
+++ Mathlib/Geometry/Convex/Cone/Pointed.lean
@@ -119,8 +119,8 @@ variable {C C₁ C₂ : PointedCone R E} {x : E} {r : R}
 @[ext] lemma ext (h : ∀ x, x ∈ C₁ ↔ x ∈ C₂) : C₁ = C₂ := SetLike.ext h
 
 @[aesop 90% (rule_sets := [SetLike])]
-nonrec lemma smul_mem (C : PointedCone R E) (hr : 0 ≤ r) (hx : x ∈ C) : r • x ∈ C :=
-  C.smul_mem ⟨r, hr⟩ hx
+lemma smul_mem (C : PointedCone R E) (hr : 0 ≤ r) (hx : x ∈ C) : r • x ∈ C :=
+  Submodule.smul_mem C ⟨r, hr⟩ hx
 
 lemma convex (C : PointedCone R E) : Convex R (C : Set E) :=
   convex_iff_add_mem.2 fun _ hx _ hy _ _ ha hb _ ↦ add_mem (C.smul_mem ha hx) (C.smul_mem hb hy)
