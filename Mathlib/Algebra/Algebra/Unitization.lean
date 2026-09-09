2bd05857d8f99a0ee277686671edeb451f040342
Michael Rothgang <rothgang@math.uni-bonn.de>
2026-09-09 12:10:06 +0200

    Update Mathlib/Algebra/Algebra/Unitization.lean
    Co-authored-by: Monica Omar <23701951+themathqueen@users.noreply.github.com>

--- Mathlib/Algebra/Algebra/Unitization.lean
+++ Mathlib/Algebra/Algebra/Unitization.lean
@@ -793,12 +793,7 @@ theorem starAlgHom_ext {φ ψ : Unitization R A →⋆ₐ[R] C}
     (h : φ.toNonUnitalStarAlgHom.comp (Unitization.inrNonUnitalStarAlgHom R A) =
       ψ.toNonUnitalStarAlgHom.comp (Unitization.inrNonUnitalStarAlgHom R A)) :
     φ = ψ :=
-  Unitization.algHom_ext'' <| by
-    intro a
-    -- TODO: can automation be improved to handle this better?
-    rw [← inrNonUnitalStarAlgHom_apply,
-      ← StarAlgHom.coe_toNonUnitalStarAlgHom, ← StarAlgHom.coe_toNonUnitalStarAlgHom,
-      ← NonUnitalStarAlgHom.comp_apply, ← NonUnitalStarAlgHom.comp_apply, h]
+  Unitization.algHom_ext'' <| DFunLike.congr_fun h
 
 variable [StarModule R C]
 
