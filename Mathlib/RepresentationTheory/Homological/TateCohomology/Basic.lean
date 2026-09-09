95b9c5abde86549d15d06e226ece65c92e656e1a
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2026-09-09 18:40:28 +0200

    final fix

--- Mathlib/RepresentationTheory/Homological/TateCohomology/Basic.lean
+++ Mathlib/RepresentationTheory/Homological/TateCohomology/Basic.lean
@@ -83,7 +83,8 @@ lemma Rep.comp_eq_zero : d₁₀ M ≫ M.norm.toModuleCatHom = 0 := by
 
 lemma Rep.d_comp_tateNorm : (inhomogeneousChains M).d 1 0 ≫ M.tateNorm = 0 := by
   simp only [tateNorm, ← Category.assoc, Preadditive.IsIso.comp_right_eq_zero]
-  simp [← comp_d₁₀_eq _]
+  rw [← comp_d₁₀_eq _]
+  simp
 
 /-- The Tate norm connecting complexes of inhomogeneous chains and cochains. -/
 @[simps]
@@ -110,7 +111,15 @@ lemma tateComplex_d_neg (n : ℕ) :
 def tateComplex.map (φ : X ⟶ Y) : tateComplex X ⟶ tateComplex Y := by
   refine CochainComplex.ConnectData.map _ _ (chainsMap (.id G) φ) (cochainsMap (.id G) φ) ?_
   ext
-  simp [Rep.tateNorm_eq, Representation.norm, Rep.hom_comm_apply]
+  simp only [tateComplexConnectData_d₀, Rep.tateNorm_eq, Representation.norm,
+    lsingle_comp_chainsMap_f_assoc, MonoidHom.coe_id, CompTriple.comp_eq, ModuleCat.ofHom_comp,
+    Category.assoc, ModuleCat.hom_comp, ConcreteCategory.hom_ofHom, LinearMap.coe_comp,
+    Representation.IntertwiningMap.coe_toLinearMap, Function.comp_apply,
+    cochainsMap_id_f_hom_eq_compLeft, Finsupp.coe_lsum, Finsupp.lsingle_apply, map_zero,
+    Finsupp.sum_single_index, LinearMap.compLeft_apply, LinearMap.pi_apply, LinearMap.coe_sum,
+    Finset.sum_apply, map_sum, Rep.hom_comm_apply]
+  rw [Finsupp.lsum_apply, Finsupp.lsingle_apply]
+  simp
 
 @[simp]
 lemma tateComplex.map_zero : tateComplex.map (0 : X ⟶ Y) = 0 := by cat_disch
