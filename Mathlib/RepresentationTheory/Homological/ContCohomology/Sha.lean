72025626969df6ef98eb3689b5ff45e63785a261
Whysoserioushah <yx3021@ic.ac.uk>
2026-09-09 10:20:10 +0100

    fix name

--- Mathlib/RepresentationTheory/Homological/ContCohomology/Sha.lean
+++ Mathlib/RepresentationTheory/Homological/ContCohomology/Sha.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2026 Yunzhou Xie. All rights reserved.
+Copyright (c) 2026 Edison Xie. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Edison Xie, David Kurniadi Angdinata
 -/
