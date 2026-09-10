794fd51973f40077c5f3ffd8ba8230e1ebe95034
tb65536 <thomas.l.browning@gmail.com>
2026-09-10 11:45:21 +0100

    fix

--- Mathlib/Algebra/Group/Equiv/TypeTags.lean
+++ Mathlib/Algebra/Group/Equiv/TypeTags.lean
@@ -8,7 +8,6 @@ module
 public import Mathlib.Algebra.Group.TypeTags.Hom
 public import Mathlib.Algebra.Group.Equiv.Defs
 public import Mathlib.Algebra.Notation.Prod
-public import Mathlib.Tactic.DefEqAbuse
 public import Mathlib.Tactic.Spread
 
 /-!
