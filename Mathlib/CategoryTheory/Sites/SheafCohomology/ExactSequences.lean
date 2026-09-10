f5674a843f2e9f9f1245f55f062e5d681f9d1af9
Brian Nugent <b.nugent@utah.edu>
2026-09-10 12:48:45 -0600

    Update Mathlib/CategoryTheory/Sites/SheafCohomology/ExactSequences.lean
    Co-authored-by: Joël Riou <37772949+joelriou@users.noreply.github.com>

--- Mathlib/CategoryTheory/Sites/SheafCohomology/ExactSequences.lean
+++ Mathlib/CategoryTheory/Sites/SheafCohomology/ExactSequences.lean
@@ -90,7 +90,6 @@ noncomputable abbrev longSequenceHom (h : n₀ + 1 = n₁ := by lia) :
     ext
     simp [← H.map_comp_apply, f.comm₁₂, f.comm₂₃, ← δ_naturality n₀ n₁ h h₁ h₂ f]
 
-set_option backward.isDefEq.respectTransparency false in
 @[simp]
 lemma longSequenceHom_id (h : n₀ + 1 = n₁ := by lia) :
     longSequenceHom n₀ n₁ h₁ h₁ (𝟙 _) h = 𝟙 _ := by
