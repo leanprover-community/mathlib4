2bb2a95acd185eac913a1aae8ccbdbcc8bd55ca9
Jireh Loreaux <loreaujy@gmail.com>
2026-09-10 16:42:32 -0500

    feat(Algebra/Star/SelfAdjoint): add a missing `pow` instance for `IsStarNormal`

--- Mathlib/Algebra/Star/SelfAdjoint.lean
+++ Mathlib/Algebra/Star/SelfAdjoint.lean
@@ -664,6 +664,10 @@ protected instance val_inv [Monoid R] [StarMul R] {x : Rˣ} [IsStarNormal (x : R
     IsStarNormal (↑x⁻¹ : R) where
   star_comm_self := by simpa [← Units.coe_star_inv, -Commute.units_val_iff] using star_comm_self
 
+protected instance pow [Monoid R] [StarMul R] {x : R} [IsStarNormal x] (n : ℕ) :
+    IsStarNormal (x ^ n) where
+  star_comm_self := star_pow x n ▸ (star_comm_self.pow_pow n n)
+
 protected instance map {F R S : Type*} [Mul R] [Star R] [Mul S] [Star S]
     [FunLike F R S] [MulHomClass F R S] [StarHomClass F R S] (f : F) (r : R) [hr : IsStarNormal r] :
     IsStarNormal (f r) where
