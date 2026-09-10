0d7bbf7abc1118486008e83c5834d34d226acdc9
Katerina Hristova <katherina.hristova@gmail.com>
2026-09-10 15:43:30 +0100

    apply review comments

--- Mathlib/Topology/MetricSpace/GromovProduct.lean
+++ Mathlib/Topology/MetricSpace/GromovProduct.lean
@@ -20,45 +20,58 @@ The Gromov product of `y` and `z` with respect to `x` in a pseudometric space is
 ## Tags
 
 Gromov product
+
+## Implementation notes
+
+We intentionally omit grind tags which would introduce `dist` terms in general proofs about
+`gromovProduct`, to avoid introducing the dist-theory.
+In proofs where it is useful to convert `gromovProduct` to a `dist`, users should feel free to add
+the relevant lemmas to the `grind [...]` list in an application.
 -/
 
 @[expose] public section
 
 namespace Metric
 
-variable {X : Type*} [PseudoMetricSpace X]
+variable {X : Type*} [PseudoMetricSpace X] (x y z : X)
 
 /-- The Gromov product of `y` and `z` with respect to `x`. -/
-noncomputable def gromovProduct (x y z : X) : ℝ := (dist x y + dist x z - dist y z) / 2
+noncomputable def gromovProduct : ℝ := (dist x y + dist x z - dist y z) / 2
 
-lemma gromovProduct_comm (x y z : X) : gromovProduct x y z = gromovProduct x z y := by
-  grind [gromovProduct, dist_comm]
+lemma gromovProduct_eq : gromovProduct x y z = (dist x y + dist x z - dist y z) / 2 := rfl
 
-lemma gromovProduct_nonneg (x y z : X) : 0 ≤ gromovProduct x y z := by
-  grind [gromovProduct, dist_triangle_left y z x]
+lemma gromovProduct_comm : gromovProduct x y z = gromovProduct x z y := by
+  grind [gromovProduct_eq, dist_comm]
 
-lemma gromovProduct_le_dist_left (x y z : X) : gromovProduct x y z ≤ dist x y := by
-  grind [gromovProduct, dist_triangle x y z]
+grind_pattern gromovProduct_comm => gromovProduct x y z where y =/= z
 
-lemma gromovProduct_le_dist_right (x y z : X) : gromovProduct x y z ≤ dist x z := by
-  rw [gromovProduct_comm]
-  exact gromovProduct_le_dist_left x z y
+@[grind! .]
+lemma gromovProduct_nonneg : 0 ≤ gromovProduct x y z := by
+  grind [gromovProduct_eq, dist_triangle_left y z x]
 
-@[simp]
-lemma gromovProduct_self_left (x y : X) : gromovProduct x x y = 0 := by
-  simp [gromovProduct]
+lemma gromovProduct_le_dist_left : gromovProduct x y z ≤ dist x y := by
+  grind [gromovProduct_eq, dist_triangle x y z]
 
-@[simp]
-lemma gromovProduct_self_right (x y : X) : gromovProduct x y x = 0 := by
+lemma gromovProduct_le_dist_right : gromovProduct x y z ≤ dist x z := by
+  grind [gromovProduct_le_dist_left]
+
+@[simp, grind =]
+lemma gromovProduct_self_left : gromovProduct x x y = 0 := by
+  simp [gromovProduct_eq]
+
+@[simp, grind =]
+lemma gromovProduct_self_right : gromovProduct x y x = 0 := by
   simp [gromovProduct, dist_comm]
 
 @[simp]
-lemma gromovProduct_self_centre (x y : X) : gromovProduct x y y = dist x y := by
-  simp [gromovProduct]
+lemma gromovProduct_self : gromovProduct x y y = dist x y := by
+  simp [gromovProduct_eq]
 
-@[simp]
-lemma gromovProduct_add_gromovProduct (x y z : X) : gromovProduct x y z + gromovProduct z y x =
-    dist x z := by
-  grind [gromovProduct, dist_comm]
+lemma gromovProduct_add_gromovProduct₁₃ : gromovProduct x y z + gromovProduct z y x = dist x z := by
+  grind [gromovProduct_eq, dist_comm]
+
+lemma gromovProduct_add_gromovProduct₁₂ : gromovProduct x y z + gromovProduct y x z = dist x y := by
+  grind [gromovProduct_eq, dist_comm]
+
 
 end Metric
