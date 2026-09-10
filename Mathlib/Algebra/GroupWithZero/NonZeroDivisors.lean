9ae8330b6795ad042bc4f88392334e04c799b84a
Eric Wieser <efw@google.com>
2026-09-10 14:46:15 +0000

    reduce import

--- Mathlib/Algebra/GroupWithZero/NonZeroDivisors.lean
+++ Mathlib/Algebra/GroupWithZero/NonZeroDivisors.lean
@@ -6,9 +6,9 @@ Authors: Kenny Lau, Devon Tuma, Oliver Nash
 module
 
 public import Mathlib.Algebra.Group.Submonoid.Membership
+public import Mathlib.Algebra.GroupWithZero.Action.Defs
 public import Mathlib.Algebra.GroupWithZero.Associated
 public import Mathlib.Algebra.GroupWithZero.Regular
-public import Mathlib.Algebra.GroupWithZero.Action.Basic
 public import Mathlib.Algebra.Regular.SMul
 public import Mathlib.Algebra.BigOperators.Group.Finset.Defs
 import Mathlib.Algebra.GroupWithZero.Action.Regular
