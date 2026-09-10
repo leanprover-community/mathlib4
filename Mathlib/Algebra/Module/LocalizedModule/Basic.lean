1a711245b8aa6cc25806e57c47c914357e5cd4cd
Jiedong Jiang <emailboxofjjd@163.com>
2026-09-10 05:57:57 +0200

    remove 1 sorry

--- Mathlib/Algebra/Module/LocalizedModule/Basic.lean
+++ Mathlib/Algebra/Module/LocalizedModule/Basic.lean
@@ -759,9 +759,8 @@ lemma IsLocalizedModule.of_restrictScalars (S : Submonoid R)
   map_units x := by
     obtain ⟨_, x, hx, rfl⟩ := x
     have := IsLocalizedModule.map_units (f.restrictScalars R) ⟨x, hx⟩
-    sorry /- proof was:
-    simp only [← IsScalarTower.algebraMap_apply, Module.End.isUnit_iff] at this ⊢
-    exact this -/
+    simp only [End.isUnit_iff, MonoidHom.coe_coe, ← IsScalarTower.algebraMap_apply] at this ⊢
+    exact this
   surj y := by
     obtain ⟨⟨x, t⟩, e⟩ := IsLocalizedModule.surj S (f.restrictScalars R) y
     exact ⟨⟨x, ⟨_, t, t.2, rfl⟩⟩, by simpa [Submonoid.smul_def] using e⟩
