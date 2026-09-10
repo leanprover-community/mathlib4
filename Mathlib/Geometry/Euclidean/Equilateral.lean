d046ac32c35bd5ee2fc3c90102385103af89fd1e
Laurance <60111599+LLaurance@users.noreply.github.com>
2026-09-11 06:42:59 +0800

    Update Mathlib/Geometry/Euclidean/Equilateral.lean
    Co-authored-by: Daniel Liao <omsidaniel@gmail.com>

--- Mathlib/Geometry/Euclidean/Equilateral.lean
+++ Mathlib/Geometry/Euclidean/Equilateral.lean
@@ -74,8 +74,8 @@ lemma dist_centroid_eq (h : s.Equilateral) :
 
 /-- The centroid is the circumcenter. -/
 lemma centroid_eq_circumcenter (h : s.Equilateral) : s.centroid = s.circumcenter :=
-    s.eq_circumcenter_of_dist_eq s.centroid_mem_affineSpan (r := dist (s.points 0) s.centroid)
-      fun _ ↦ h.dist_centroid_eq
+  s.eq_circumcenter_of_dist_eq s.centroid_mem_affineSpan (r := dist (s.points 0) s.centroid)
+    fun _ ↦ h.dist_centroid_eq
 
 /-- The centroid is the Monge point. -/
 lemma centroid_eq_mongePoint (h : s.Equilateral) : s.centroid = s.mongePoint := by
