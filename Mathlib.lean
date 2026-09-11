62fd6bb0e73d2ffee7f346fc8833dd9d54fb7f01
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2026-09-11 12:22:40 +0200

    migrate file

--- Mathlib.lean
+++ Mathlib.lean
@@ -6931,6 +6931,7 @@ public import Mathlib.RingTheory.Kaehler.Polynomial
 public import Mathlib.RingTheory.Kaehler.TensorProduct
 public import Mathlib.RingTheory.KoszulComplex.Cocomplex
 public import Mathlib.RingTheory.KoszulComplex.Complex
+public import Mathlib.RingTheory.KoszulComplex.Homotopy
 public import Mathlib.RingTheory.KrullDimension.Basic
 public import Mathlib.RingTheory.KrullDimension.Field
 public import Mathlib.RingTheory.KrullDimension.LocalRing
