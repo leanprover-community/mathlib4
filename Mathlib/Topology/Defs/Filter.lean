eda14bce396cafa008be0a0c8a0976e083b9db94
Snir Broshi <26556598+SnirBroshi@users.noreply.github.com>
2026-09-11 07:12:04 +0300

    backticks in `Specializes` docstring

--- Mathlib/Topology/Defs/Filter.lean
+++ Mathlib/Topology/Defs/Filter.lean
@@ -202,8 +202,8 @@ hold:
 * for any open set `s` we have `y ∈ s → x ∈ s`;
 * `y` is a cluster point of the filter `pure x = 𝓟 {x}`.
 
-This relation defines a preorder on `X`. If `X` is a T₀ space, then this preorder is a partial
-order. If `X` is a T₁ space, then this partial order is trivial : `x ⤳ y ↔ x = y`. -/
+This relation defines a `Preorder` on `X`. If `X` is a T₀ space, then this `Preorder` is a
+`PartialOrder`. If `X` is a T₁ space, then this `PartialOrder` is trivial: `x ⤳ y ↔ x = y`. -/
 def Specializes (x y : X) : Prop := 𝓝 x ≤ 𝓝 y
 
 @[inherit_doc]
