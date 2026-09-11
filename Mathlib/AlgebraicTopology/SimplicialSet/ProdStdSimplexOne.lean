13a16651a26228f2b61b7d18c8183440b1efdc86
Joël Riou <37772949+joelriou@users.noreply.github.com>
2026-09-11 19:37:05 +0200

    Update Mathlib/AlgebraicTopology/SimplicialSet/ProdStdSimplexOne.lean
    Co-authored-by: Robin Carlier <robin@rcarlier.dev>

--- Mathlib/AlgebraicTopology/SimplicialSet/ProdStdSimplexOne.lean
+++ Mathlib/AlgebraicTopology/SimplicialSet/ProdStdSimplexOne.lean
@@ -113,8 +113,7 @@ lemma mem_range_objEquiv_nonDegenerateEquiv₁_iff (i x : Fin (p + 1)) :
     dsimp% (x, 1) ∈ Set.range (prodStdSimplex.objEquiv (nonDegenerateEquiv i).val) ↔ i ≤ x := by
   constructor
   · intro ⟨y, hy⟩
-    have hy₁ := congr_arg Prod.fst hy
-    have hy₂ := congr_arg Prod.snd hy
+    obtain ⟨hy₁, hy₂⟩ := Prod.ext_iff.mp hy
     dsimp at hy₁ hy₂
     rw [stdSimplex.objMk₁_apply_eq_one_iff, Fin.succ_castSucc, Fin.castSucc_le_castSucc_iff] at hy₂
     rw [← hy₁, stdSimplex.objEquiv_symm_σ_apply, Fin.predAbove_of_succ_le _ _ hy₂,
