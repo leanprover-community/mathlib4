264b39c772a15817b8b611b6fa7a93b29d2fdea6
tb65536 <thomas.l.browning@gmail.com>
2026-09-11 23:29:50 +0100

    tweak

--- Mathlib/NumberTheory/SumTwoSquares.lean
+++ Mathlib/NumberTheory/SumTwoSquares.lean
@@ -207,7 +207,7 @@ section Main
 /-- A (positive) natural number `n` is a sum of two squares if and only if the exponent of
 every prime `q` such that `q % 4 = 3` in the prime factorization of `n` is even.
 (The assumption `0 < n` is not present, since for `n = 0`, both sides are satisfied;
-the right-hand side holds, since `multiplicity q 0 = 0` by definition.) -/
+the right-hand side holds, since `padicValNat q 0 = 0` by definition.) -/
 theorem Nat.eq_sq_add_sq_iff {n : ℕ} :
     (∃ x y, n = x ^ 2 + y ^ 2) ↔ ∀ q ∈ n.primeFactors, q % 4 = 3 → Even (padicValNat q n) := by
   simp_rw [padicValNat_def]
