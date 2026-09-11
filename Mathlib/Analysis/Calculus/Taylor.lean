356c0050ad46cf28bc08f04545e21440107f9bd8
Felix Pernegger <s59fpern@uni-bonn.de>
2026-09-11 16:20:18 +0200

    Apply batched suggestions from code review
    Co-authored-by: Felix Pernegger <s59fpern@uni-bonn.de>

--- Mathlib/Analysis/Calculus/Taylor.lean
+++ Mathlib/Analysis/Calculus/Taylor.lean
@@ -313,6 +313,8 @@ theorem taylor_mean_remainder {f : ℝ → ℝ} {g g' : ℝ → ℝ} {x x₀ : �
       (continuousOn_taylorWithinEval (uniqueDiffOn_Icc hx₁) hf)
       (fun _ hy => taylorWithinEval_hasDerivAt_Ioo x hx₁ hy hf hf')
     g g' gcont gdiff with ⟨y, hy, h⟩
+  use y, hy
+  -- The rest is simplifications and trivial calculations
   grind [uIoo, smul_eq_mul, taylorWithinEval_self]
 
 -- see https://github.com/leanprover-community/mathlib4/issues/29041
