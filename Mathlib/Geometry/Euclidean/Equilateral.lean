26d7f108d57d24ffe69f4658dfaf11b5f415157d
LLaurance <laurancelau.2022@berkeley.edu>
2026-09-11 21:48:39 +1000

    Update Equilateral.lean

--- Mathlib/Geometry/Euclidean/Equilateral.lean
+++ Mathlib/Geometry/Euclidean/Equilateral.lean
@@ -47,24 +47,26 @@ lemma mem_perpBisector (h : s.Equilateral) (hij : i ≠ j) (hik : i ≠ k) :
 
 section Center
 
+lemma dist_centroid_sq_eq (h : s.Equilateral) (hjk : j ≠ k) : dist (s.points i) s.centroid ^ 2 =
+    (n * dist (s.points j) (s.points k) ^ 2 - ∑ k, dist (s.points k) s.centroid ^ 2) / (n + 1) := by
+  obtain ⟨r, hr⟩ := h
+  have : ∑ l, inner ℝ (s.points i -ᵥ s.centroid) (s.points l -ᵥ s.centroid) = 0 := by
+    simp [← inner_sum, s.centroid_weighted_vsub_eq_zero]
+  have h l (hl : l ∈ Finset.univ.erase i) :
+      2 * inner ℝ (s.points i -ᵥ s.centroid) (s.points l -ᵥ s.centroid) =
+        ‖s.points i -ᵥ s.centroid‖ ^ 2 + ‖s.points l -ᵥ s.centroid‖ ^ 2 - r ^ 2 := by
+    grind [hr i l (Finset.ne_of_mem_erase hl).symm, dist_eq_norm_vsub, vsub_sub_vsub_cancel_right,
+      norm_sub_sq_real]
+  have := Finset.sum_congr rfl h
+  simp [← Finset.mul_sum, Finset.sum_sub_distrib, Finset.sum_add_distrib] at this
+  grind [dist_eq_norm_vsub]
+
 /-- The distance between any point and the centroid is constant. -/
 lemma dist_centroid_eq (h : s.Equilateral) :
     dist (s.points i) s.centroid = dist (s.points j) s.centroid := by
-  obtain ⟨r, hr⟩ := h
-  have mul_sq_norm_eq i : (n + 1) * ‖s.points i -ᵥ s.centroid‖ ^ 2 =
-      n * r ^ 2 - ∑ k, ‖s.points k -ᵥ s.centroid‖ ^ 2 := by
-    have : ∑ l, inner ℝ (s.points i -ᵥ s.centroid) (s.points l -ᵥ s.centroid) = 0 := by
-      simp [← inner_sum, s.centroid_weighted_vsub_eq_zero]
-    have h l (hl : l ∈ Finset.univ.erase i) :
-        2 * inner ℝ (s.points i -ᵥ s.centroid) (s.points l -ᵥ s.centroid) =
-          ‖s.points i -ᵥ s.centroid‖ ^ 2 + ‖s.points l -ᵥ s.centroid‖ ^ 2 - r ^ 2 := by
-      grind [hr i l (Finset.ne_of_mem_erase hl).symm, dist_eq_norm_vsub, vsub_sub_vsub_cancel_right,
-        norm_sub_sq_real]
-    have := Finset.sum_congr rfl h
-    simp [← Finset.mul_sum, Finset.sum_sub_distrib, Finset.sum_add_distrib] at this
-    grind
-  rw [dist_eq_norm_vsub, dist_eq_norm_vsub, ← sq_eq_sq₀ (norm_nonneg _) (norm_nonneg _),
-    ← mul_right_inj' n.cast_add_one_ne_zero, mul_sq_norm_eq, mul_sq_norm_eq]
+  by_cases hij : i = j
+  · rw [hij]
+  · rw [← sq_eq_sq₀ dist_nonneg dist_nonneg, h.dist_centroid_sq_eq hij, h.dist_centroid_sq_eq hij]
 
 /-- The centroid is the circumcenter. -/
 lemma centroid_eq_circumcenter (h : s.Equilateral) : s.centroid = s.circumcenter :=
