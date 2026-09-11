b6a37ad29b4ea2b48ee63ee9860b2e24c817026a
Matthew Spillman <matthewspillman1@gmail.com>
2026-09-10 23:17:11 -0400

    ditto

--- Mathlib/Geometry/Manifold/LocalDiffeomorph.lean
+++ Mathlib/Geometry/Manifold/LocalDiffeomorph.lean
@@ -41,7 +41,7 @@ diffeomorphism at every `x ∈ s`, and a **local diffeomorphism** iff it is a lo
   each differential `mfderiv I J n f x` is a continuous linear equivalence.
 
 * `IsLocalDiffeomorphAt_of_isInvertible_mderiv` (proved in `InverseFunctionTheorem.lean`): if `f` is
-  `C^n` an open set `U` and has bijective differential at an interior point `p ∈ U`, then
+  `C^n` on an open set `U` and has bijective differential at an interior point `p ∈ U`, then
   `f` is a local diffeomorphism at `p`.
 
 ## TODO
