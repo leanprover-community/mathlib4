af25e2dc5aef1051f1679d144a8947e47351965b
Oliver Nash <7734364+ocfnash@users.noreply.github.com>
2026-09-11 08:38:07 +0000

    feat: definition + API for Cartan matrix realisations (#43208)

--- Mathlib.lean
+++ Mathlib.lean
@@ -5244,6 +5244,7 @@ public import Mathlib.LinearAlgebra.Matrix.BilinearForm
 public import Mathlib.LinearAlgebra.Matrix.Block
 public import Mathlib.LinearAlgebra.Matrix.Cartan
 public import Mathlib.LinearAlgebra.Matrix.Cartan.Basic
+public import Mathlib.LinearAlgebra.Matrix.Cartan.Realisation
 public import Mathlib.LinearAlgebra.Matrix.CharP
 public import Mathlib.LinearAlgebra.Matrix.Charpoly.Basic
 public import Mathlib.LinearAlgebra.Matrix.Charpoly.Coeff
