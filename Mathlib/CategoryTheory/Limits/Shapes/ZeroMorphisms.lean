3c960ddeecddf29c46db039ac0fa8b57c6af7ab4
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2026-09-13 00:04:37 +0200

    dualize the stuff about products

--- Mathlib/CategoryTheory/Limits/Shapes/ZeroMorphisms.lean
+++ Mathlib/CategoryTheory/Limits/Shapes/ZeroMorphisms.lean
@@ -496,12 +496,9 @@ theorem image.ι_zero' [HasEqualizers C] {X Y : C} {f : X ⟶ Y} (h : f = 0) [Ha
 
 end Image
 
-/-- In the presence of zero morphisms, coprojections into a coproduct are (split) monomorphisms. -/
-instance isSplitMono_sigma_ι {β : Type u'} [HasZeroMorphisms C] (f : β → C)
-    [HasColimit (Discrete.functor f)] (b : β) : IsSplitMono (Sigma.ι f b) := by
-  classical exact IsSplitMono.mk' { retraction := Sigma.desc <| Pi.single b (𝟙 _) }
-
 /-- In the presence of zero morphisms, projections into a product are (split) epimorphisms. -/
+@[to_dual isSplitMono_sigma_ι
+/-- In the presence of zero morphisms, coprojections into a coproduct are (split) monomorphisms. -/]
 instance isSplitEpi_pi_π {β : Type u'} [HasZeroMorphisms C] (f : β → C)
     [HasLimit (Discrete.functor f)] (b : β) : IsSplitEpi (Pi.π f b) := by
   classical exact IsSplitEpi.mk' { section_ := Pi.lift <| Pi.single b (𝟙 _) }
@@ -581,14 +578,16 @@ section PiIota
 variable [HasZeroMorphisms C] {β : Type w} [DecidableEq β] (f : β → C) [HasProduct f]
 
 /-- In the presence of 0-morphism we can define an inclusion morphism into any product. -/
+@[to_dual π
+/-- In the presence of 0-morphisms we can define a projection morphism from any coproduct. -/]
 def Pi.ι (b : β) : f b ⟶ ∏ᶜ f :=
   Pi.lift (Function.update (fun _ ↦ 0) b (𝟙 _))
 
-@[reassoc (attr := simp), grind =]
+@[to_dual (attr := reassoc (attr := simp), grind =)]
 lemma Pi.ι_π_eq_id (b : β) : Pi.ι f b ≫ Pi.π f b = 𝟙 _ := by
   simp [Pi.ι]
 
-@[reassoc, grind =]
+@[to_dual (attr := reassoc, grind =)]
 lemma Pi.ι_π_of_ne {b c : β} (h : b ≠ c) : Pi.ι f b ≫ Pi.π f c = 0 := by
   simp [Pi.ι, Function.update_of_ne h.symm]
 
@@ -597,6 +596,7 @@ lemma Pi.ι_π (b c : β) :
     Pi.ι f b ≫ Pi.π f c = if h : b = c then eqToHom (congrArg f h) else 0 := by
   grind [CategoryTheory.eqToHom_refl]
 
+@[to_dual instEpiπ]
 instance (b : β) : Mono (Pi.ι f b) where
   right_cancellation _ _ e := by simpa using congrArg (· ≫ Pi.π f b) e
 
@@ -606,25 +606,10 @@ section SigmaPi
 
 variable [HasZeroMorphisms C] {β : Type w} [DecidableEq β] (f : β → C) [HasCoproduct f]
 
-/-- In the presence of 0-morphisms we can define a projection morphism from any coproduct. -/
-def Sigma.π (b : β) : ∐ f ⟶ f b :=
-  Limits.Sigma.desc (Function.update (fun _ ↦ 0) b (𝟙 _))
-
-@[reassoc (attr := simp), grind =]
-lemma Sigma.ι_π_eq_id (b : β) : Sigma.ι f b ≫ Sigma.π f b = 𝟙 _ := by
-  simp [Sigma.π]
-
-@[reassoc, grind =]
-lemma Sigma.ι_π_of_ne {b c : β} (h : b ≠ c) : Sigma.ι f b ≫ Sigma.π f c = 0 := by
-  simp [Sigma.π, Function.update_of_ne h]
-
 @[reassoc]
 theorem Sigma.ι_π (b c : β) :
     Sigma.ι f b ≫ Sigma.π f c = if h : b = c then eqToHom (congrArg f h) else 0 := by
   grind [CategoryTheory.eqToHom_refl]
-
-instance (b : β) : Epi (Sigma.π f b) where
-  left_cancellation _ _ e := by simpa using congrArg (Sigma.ι f b ≫ ·) e
 
 end SigmaPi
 
