bd2b16a6b9a6ccb234b44ff65a14071a32a24ebe
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2026-09-12 15:47:49 +0200

    more use cases

--- Mathlib/Order/Interval/Finset/Defs.lean
+++ Mathlib/Order/Interval/Finset/Defs.lean
@@ -306,10 +306,10 @@ theorem mem_Ioc : x ∈ Ioc a b ↔ a < x ∧ x ≤ b :=
 theorem mem_Ioo : x ∈ Ioo a b ↔ a < x ∧ x < b :=
   LocallyFiniteOrder.finset_mem_Ioo a b x
 
-@[to_dual existing mem_Icc] theorem mem_Icc' : x ∈ Icc a b ↔ x ≤ b ∧ a ≤ x := by grind
-@[to_dual existing mem_Ioc] theorem mem_Ico' : x ∈ Ico a b ↔ x < b ∧ a ≤ x := by grind
-@[to_dual existing mem_Ico] theorem mem_Ioc' : x ∈ Ioc a b ↔ x ≤ b ∧ a < x := by grind
-@[to_dual existing mem_Ioo] theorem mem_Ioo' : x ∈ Ioo a b ↔ x < b ∧ a < x := by grind
+to_dual_for mem_Icc := by grind
+to_dual_for mem_Ico := by grind
+to_dual_for mem_Ioc := by grind
+to_dual_for mem_Ioo := by grind
 
 @[simp, norm_cast, to_dual self]
 theorem coe_Icc (a b : α) : (Icc a b : Set α) = Set.Icc a b :=
