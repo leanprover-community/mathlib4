284b0c54bd0a3d907228aef65eea3b1d0b7cb2b7
LLaurance <laurancelau.2022@berkeley.edu>
2026-09-12 15:09:17 +1000

    lake exe mk_all

--- Mathlib.lean
+++ Mathlib.lean
@@ -4744,6 +4744,7 @@ public import Mathlib.Geometry.Euclidean.Basic
 public import Mathlib.Geometry.Euclidean.BritishFlag
 public import Mathlib.Geometry.Euclidean.Circumcenter
 public import Mathlib.Geometry.Euclidean.Congruence
+public import Mathlib.Geometry.Euclidean.Equilateral
 public import Mathlib.Geometry.Euclidean.Incenter
 public import Mathlib.Geometry.Euclidean.Inversion.Basic
 public import Mathlib.Geometry.Euclidean.Inversion.Calculus
