61fbecd2d4d4b4764f0a2699857cc658e9857397
Jiedong Jiang <emailboxofjjd@163.com>
2026-09-13 09:26:39 +0200

    fix linebreaks

--- Mathlib/Algebra/Ring/CentroidHom.lean
+++ Mathlib/Algebra/Ring/CentroidHom.lean
@@ -188,7 +188,8 @@ theorem comp_apply (g f : CentroidHom α) (a : α) : g.comp f a = g (f a) :=
   rfl
 
 @[simp, norm_cast]
-theorem ofClass_addMonoidHom_comp (g f : CentroidHom α) : (g.comp f : α →+ α) = (g : α →+ α).comp f :=
+theorem ofClass_addMonoidHom_comp (g f : CentroidHom α) :
+    (g.comp f : α →+ α) = (g : α →+ α).comp f :=
   rfl
 
 @[simp]
