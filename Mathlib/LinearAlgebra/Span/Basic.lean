a66431d376064edf99c8b3c97c93238a34a70071
Bhavik Mehta <bhavikmehta8@gmail.com>
2026-09-14 21:31:26 +0100

    Apply batched suggestions from code review
    Co-authored-by: Monica Omar <23701951+themathqueen@users.noreply.github.com>

--- Mathlib/LinearAlgebra/Span/Basic.lean
+++ Mathlib/LinearAlgebra/Span/Basic.lean
@@ -425,8 +425,6 @@ def prodEquiv (p : Submodule R M) (q : Submodule R M') : p.prod q ≃ₗ[R] p ×
   invFun y := ⟨(y.1.1, y.2.1), mem_prod.2 ⟨y.1.2, y.2.2⟩⟩
   map_add' _ _ := rfl
   map_smul' _ _ := rfl
-  left_inv _ := rfl
-  right_inv _ := rfl
 
 /-- If a bilinear map takes values in a submodule along two sets, then the same is true along
 the span of these sets. -/
