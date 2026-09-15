62ec6fa6a3289bc9dbb8b301110ed9eecd26de33
David Kurniadi Angdinata <dka31@cantab.ac.uk>
2026-09-15 10:51:01 +0200

    Replace A with J

--- Mathlib/RepresentationTheory/Homological/ContCohomology/Sha.lean
+++ Mathlib/RepresentationTheory/Homological/ContCohomology/Sha.lean
@@ -41,7 +41,7 @@ The second context is when `A` is the `Kˢ`-valued points of a group scheme `J` 
 variety. Then the Tate–Shafarevich group is usually defined as the intersection of the kernels of
 the maps `H¹(G_K, J(Kˢ)) → H¹(G_Kᵥ, J(Kᵥˢ))`. Note that in particular the module changes as well as
 the group, so technically this is not quite what is happening in this definition. However the
-inclusion `H¹(G_Kᵥ, A(Kᵥˢ)) → H¹(G_Kᵥ, A(Kˢ))` is an isomorphism by the Greenberg approximation
+inclusion `H¹(G_Kᵥ, J(Kᵥˢ)) → H¹(G_Kᵥ, J(Kˢ))` is an isomorphism by the Greenberg approximation
 theorem, so the definition in this file is still mathematically correct.
 See Remark I.3.10 of [Milne].
 
