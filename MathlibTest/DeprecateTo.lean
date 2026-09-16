1699c29c6edb03f60b927cbc8f83c5fc2c8f1d04
Snir Broshi <26556598+SnirBroshi@users.noreply.github.com>
2026-09-16 18:33:05 +0300

    chore(Tactic/DeprecateTo): rename `deprecate to` into `#deprecate_to`

--- MathlibTest/DeprecateTo.lean
+++ MathlibTest/DeprecateTo.lean
@@ -21,7 +21,7 @@ Try this:
   alias add_easy_deprecated := new_name_add
 -/
 #guard_msgs in
-deprecate to new_name_mul new_name_add "YYYY-MM-DD"
+#deprecate_to new_name_mul new_name_add "YYYY-MM-DD"
 /-- I also have a doc-string -/
 @[to_additive /-- With its additive doc-string -/]
 theorem mul_easy_deprecated : True := .intro
