f9e893ad3936cab0f983cb8b7e40df026dd647b7
Nicola Falciola <nicola.falciola@gmail.com>
2026-09-16 21:46:00 +0200

    Update Mathlib/GroupTheory/Index.lean
    Co-authored-by: Snir Broshi <26556598+SnirBroshi@users.noreply.github.com>

--- Mathlib/GroupTheory/Index.lean
+++ Mathlib/GroupTheory/Index.lean
@@ -298,8 +298,7 @@ theorem card_ker_mul_card_range (f : G →* G') : Nat.card f.ker * Nat.card f.ra
 @[to_additive]
 theorem card_ker_mul_card_of_surjective {f : G →* G'} (hf : Function.Surjective f) :
     Nat.card f.ker * Nat.card G' = Nat.card G := by
-  rw [← card_ker_mul_card_range f,  MonoidHom.range_eq_top.mpr hf,
-    Nat.card_congr topEquiv.toEquiv]
+  rw [← card_ker_mul_card_range f,  MonoidHom.range_eq_top.mpr hf, Nat.card_congr topEquiv.toEquiv]
 
 @[to_additive]
 theorem card_dvd_of_surjective (f : G →* G') (hf : Function.Surjective f) :
