cc5e312bc8a80bb9a483c7c9503c4ff22fa17213
manman4 <7023566+manman4@users.noreply.github.com>
2026-09-16 22:02:19 +0900

    chore(PowerSeries): use author name in header

--- Mathlib/RingTheory/PowerSeries/LagrangeInversion.lean
+++ Mathlib/RingTheory/PowerSeries/LagrangeInversion.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2026 manman4. All rights reserved.
+Copyright (c) 2026 Seiichi Manyama. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: manman4
+Authors: Seiichi Manyama
 -/
 module
 
