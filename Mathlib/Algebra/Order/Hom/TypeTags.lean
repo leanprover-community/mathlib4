d17aea27d29088b53a0ce88b64d4060b6ab83a6b
David Kurniadi Angdinata <dka31@cantab.ac.uk>
2026-09-17 15:01:56 +0200

    Remove instance name
    Co-authored-by: Snir Broshi <26556598+SnirBroshi@users.noreply.github.com>

--- Mathlib/Algebra/Order/Hom/TypeTags.lean
+++ Mathlib/Algebra/Order/Hom/TypeTags.lean
@@ -96,9 +96,8 @@ instance Additive.instUniqueOrderAddMonoidIso {G H : Type*}
     Unique (Additive G ≃+o Additive H) :=
   OrderMonoidIso.toAdditive.symm.unique
 
-instance Multiplicative.instUniqueOrderMonoidIso {G H : Type*}
-    [AddCommMonoid G] [PartialOrder G] [AddCommMonoid H] [PartialOrder H] [Unique (G ≃+o H)] :
-    Unique (Multiplicative G ≃*o Multiplicative H) :=
+instance {G H : Type*} [AddCommMonoid G] [PartialOrder G] [AddCommMonoid H] [PartialOrder H]
+    [Unique (G ≃+o H)] : Unique (Multiplicative G ≃*o Multiplicative H) :=
   OrderAddMonoidIso.toMultiplicative.symm.unique
 
 end TypeTags
