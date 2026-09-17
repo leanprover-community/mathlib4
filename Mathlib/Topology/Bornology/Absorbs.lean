a3c97e63d7a727d0c7d3be7637f01b43ee0fc57c
Kevin H Wilson <khwilson@gmail.com>
2026-09-17 17:43:50 -0400

    jireh comments

--- Mathlib/Topology/Bornology/Absorbs.lean
+++ Mathlib/Topology/Bornology/Absorbs.lean
@@ -8,6 +8,7 @@ module
 public import Mathlib.Algebra.GroupWithZero.Action.Pointwise.Set
 public import Mathlib.Algebra.Ring.Action.Pointwise.Set
 public import Mathlib.Topology.Bornology.Basic
+public import Mathlib.GroupTheory.GroupAction.Hom
 
 /-!
 # Absorption of sets
@@ -117,13 +118,13 @@ lemma _root_.absorbs_biUnion_finset {ι : Type*} {t : ι → Set α} {I : Finset
 
 protected alias ⟨_, biUnion_finset⟩ := absorbs_biUnion_finset
 
-protected theorem image {β : Type*} [SMul M β] {f : α → β}
-    (h : Absorbs M s t) (hf_smul : ∀ (a : M) (x : α), f (a • x) = a • f x) :
+protected theorem image {β F : Type*} [SMul M β] [FunLike F α β] [MulActionHomClass F M α β]
+    {f : F} (h : Absorbs M s t) :
     Absorbs M (f '' s) (f '' t) := by
   refine h.eventually.mono fun a (ha : t ⊆ a • s) => ?_
   rintro _ ⟨y, hy, rfl⟩
   obtain ⟨x, hx, rfl⟩ := ha hy
-  exact ⟨f x, ⟨x, hx, rfl⟩, (hf_smul a x).symm⟩
+  exact ⟨f x, ⟨x, hx, rfl⟩, (map_smul ..).symm⟩
 
 end SMul
 
@@ -236,12 +237,12 @@ theorem absorbs_finite (hs : Absorbent M s) (ht : t.Finite) : Absorbs M s t := b
   rw [← Set.biUnion_of_singleton t]
   exact .biUnion ht fun _ _ => hs.absorbs
 
-theorem image_of_surjective {β : Type*} [SMul M β] {f : α → β} (hf : Function.Surjective f)
-    (hf_smul : ∀ (a : M) (x : α), f (a • x) = a • f x) (hs : Absorbent M s) :
+theorem image_of_surjective {β F : Type*} [SMul M β] [FunLike F α β] [MulActionHomClass F M α β]
+    {f : F} (hf : Function.Surjective f) (hs : Absorbent M s) :
     Absorbent M (f '' s) := by
   intro y
   obtain ⟨x, rfl⟩ := hf y
-  simpa using (hs x).image hf_smul
+  simpa using (hs x).image
 
 end SMul
 
