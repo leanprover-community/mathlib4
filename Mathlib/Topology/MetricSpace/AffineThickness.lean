a7113e9aceb7ec6fd10523d72385ef600a61b0ab
Bolton Bailey <bolton.bailey@gmail.com>
2026-09-16 22:54:36 -0700

    add human name to author list.

--- Mathlib/Topology/MetricSpace/AffineThickness.lean
+++ Mathlib/Topology/MetricSpace/AffineThickness.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2026 Project Numina. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Numina Team
+Authors: Numina Team, Bolton Bailey
 -/
 module
 
