44ba35c6daa9d69aff8fed9fff9bbde17ded774d
Yi.Yuan <kysyy1@126.com>
2026-09-17 17:14:11 +0000

    feat(Topology/ContinuousMap): add coeFnCLM (#43746)
    used in https://github.com/LeanMachineLearning/LML/pull/246

--- Mathlib/Topology/ContinuousMap/Algebra.lean
+++ Mathlib/Topology/ContinuousMap/Algebra.lean
@@ -609,6 +609,15 @@ def coeFnLinearMap : C(α, M) →ₗ[R] α → M :=
   { (coeFnAddMonoidHom : C(α, M) →+ _) with
     map_smul' := coe_smul }
 
+/-- Coercion to a function as a `ContinuousLinearMap`. -/
+@[simps! apply]
+def coeFnCLM : C(α, M) →L[R] α → M where
+  __ := coeFnLinearMap R
+  cont := continuous_coeFun
+
+@[simp]
+lemma toLinearMap_coeFnCLM : (coeFnCLM R (α := α) (M := M)).toLinearMap = coeFnLinearMap R := rfl
+
 variable (M) in
 /-- Composition on the right by a continuous map, as a `ContinuousLinearMap`. -/
 @[simps]
