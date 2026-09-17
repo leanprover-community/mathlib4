e80335b2130f4fd9c7fb999d8f4406ea2f6eefb9
David Kurniadi Angdinata <dka31@cantab.ac.uk>
2026-09-17 15:01:32 +0200

    Remove instance name
    Co-authored-by: Snir Broshi <26556598+SnirBroshi@users.noreply.github.com>

--- Mathlib/Algebra/Homology/Additive.lean
+++ Mathlib/Algebra/Homology/Additive.lean
@@ -117,8 +117,7 @@ def Functor.mapHomologicalComplex (F : W₁ ⥤ W₂) [F.PreservesZeroMorphisms]
 instance (F : W₁ ⥤ W₂) [F.PreservesZeroMorphisms] (c : ComplexShape ι) :
     (F.mapHomologicalComplex c).PreservesZeroMorphisms where
 
-instance Functor.mapHomologicalComplex_additive (F : V ⥤ W) [F.Additive] (c : ComplexShape ι) :
-    (F.mapHomologicalComplex c).Additive where
+instance (F : V ⥤ W) [F.Additive] (c : ComplexShape ι) : (F.mapHomologicalComplex c).Additive where
 
 variable (W₁)
 
