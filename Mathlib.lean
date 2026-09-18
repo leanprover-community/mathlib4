ac23608fcc8cfe541c3967ce4424a4f3a31effa4
Dagur Asgeirsson <dagurtomas@gmail.com>
2026-09-18 14:26:48 +0200

    Regenerate Mathlib imports

--- Mathlib.lean
+++ Mathlib.lean
@@ -1,7 +1,5 @@
 module  -- shake: keep-all --deprecated_module: ignore
 
-public import Mathlib.CategoryTheory.Monoidal.Linear.FunctorCategory
-public import Mathlib.CategoryTheory.Monoidal.Preadditive.FunctorCategory
 public import Std
 public import Batteries
 public import Mathlib.Algebra.AddConstMap.Basic
@@ -3260,6 +3258,7 @@ public import Mathlib.CategoryTheory.Monoidal.Limits.HasLimits
 public import Mathlib.CategoryTheory.Monoidal.Limits.Preserves
 public import Mathlib.CategoryTheory.Monoidal.Limits.Shapes.Pullback
 public import Mathlib.CategoryTheory.Monoidal.Linear
+public import Mathlib.CategoryTheory.Monoidal.Linear.FunctorCategory
 public import Mathlib.CategoryTheory.Monoidal.Mod
 public import Mathlib.CategoryTheory.Monoidal.Mod_
 public import Mathlib.CategoryTheory.Monoidal.Mon
@@ -3271,6 +3270,7 @@ public import Mathlib.CategoryTheory.Monoidal.Opposite
 public import Mathlib.CategoryTheory.Monoidal.Opposite.Mon
 public import Mathlib.CategoryTheory.Monoidal.Opposite.Mon_
 public import Mathlib.CategoryTheory.Monoidal.Preadditive
+public import Mathlib.CategoryTheory.Monoidal.Preadditive.FunctorCategory
 public import Mathlib.CategoryTheory.Monoidal.PushoutProduct
 public import Mathlib.CategoryTheory.Monoidal.Rigid.Basic
 public import Mathlib.CategoryTheory.Monoidal.Rigid.Braided
