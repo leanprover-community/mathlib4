0ae02f97412ab9c6639d119f30e1f6d84f335428
Kiran S. Kedlaya <kskedl@gmail.com>
2026-09-18 06:34:17 -0700

    Update Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -4502,6 +4502,7 @@ public import Mathlib.Dynamics.Transitive
 public import Mathlib.FieldTheory.AbelRuffini
 public import Mathlib.FieldTheory.AbsoluteGaloisGroup
 public import Mathlib.FieldTheory.AlgebraicClosure
+public import Mathlib.FieldTheory.ArtinSchreierExtension
 public import Mathlib.FieldTheory.AxGrothendieck
 public import Mathlib.FieldTheory.CardinalEmb
 public import Mathlib.FieldTheory.Cardinality
