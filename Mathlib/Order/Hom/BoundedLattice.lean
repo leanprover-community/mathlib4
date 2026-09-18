7bdfc9170bc2d82f20e31a637f609074e45aac1b
twwar <tom.waring@unimelb.edu.au>
2026-09-19 00:26:44 +0200

    order iso hom congr

--- Mathlib/Order/Hom/BoundedLattice.lean
+++ Mathlib/Order/Hom/BoundedLattice.lean
@@ -291,6 +291,15 @@ theorem cancel_left {g : SupBotHom β γ} {f₁ f₂ : SupBotHom α β} (hg : In
     g.comp f₁ = g.comp f₂ ↔ f₁ = f₂ :=
   ⟨fun h => SupBotHom.ext fun a => hg <| by rw [← comp_apply, h, comp_apply], congr_arg _⟩
 
+@[to_dual (attr := simps)]
+def _root_.OrderIso.supBotHomCongr {α β γ δ : Type*} [SemilatticeSup α] [SemilatticeSup β]
+    [SemilatticeSup γ] [SemilatticeSup δ] [OrderBot α] [OrderBot β] [OrderBot γ] [OrderBot δ]
+    (e : α ≃o γ) (e' : β ≃o δ) : SupBotHom α β ≃ SupBotHom γ δ where
+  toFun f := (e' : SupBotHom β δ).comp (f.comp e.symm)
+  invFun f := (e'.symm : SupBotHom δ β).comp (f.comp e)
+  left_inv _ := by ext; simp
+  right_inv _ := by ext; simp
+
 end Sup
 
 variable [SemilatticeSup β] [OrderBot β]
@@ -471,6 +480,14 @@ theorem cancel_right {g₁ g₂ : BoundedLatticeHom β γ} {f : BoundedLatticeHo
 theorem cancel_left {g : BoundedLatticeHom β γ} {f₁ f₂ : BoundedLatticeHom α β} (hg : Injective g) :
     g.comp f₁ = g.comp f₂ ↔ f₁ = f₂ :=
   ⟨fun h => ext fun a => hg <| by rw [← comp_apply, h, comp_apply], congr_arg _⟩
+
+@[simps]
+def _root_.OrderIso.boundedLatticeHomCongr (e : α ≃o γ) (e' : β ≃o δ) :
+    BoundedLatticeHom α β ≃ BoundedLatticeHom γ δ where
+  toFun f := (e' : BoundedLatticeHom β δ).comp (f.comp e.symm)
+  invFun f := (e'.symm : BoundedLatticeHom δ β).comp (f.comp e)
+  left_inv _ := by ext; simp
+  right_inv _ := by ext; simp
 
 /-- `Subtype.val` as a `BoundedLatticeHom`. -/
 @[to_dual self (reorder := Pbot Ptop, Psup Pinf)]
--- Mathlib/Order/Hom/CompleteLattice.lean
+++ Mathlib/Order/Hom/CompleteLattice.lean
@@ -326,6 +326,17 @@ theorem coe_bot : ⇑(⊥ : sSupHom α β) = ⊥ :=
 theorem bot_apply (a : α) : (⊥ : sSupHom α β) a = ⊥ :=
   rfl
 
+variable {_ : CompleteLattice α}
+
+@[to_dual (attr := simps)]
+def _root_.OrderIso.sSupHomCongr {α β γ δ : Type*} [CompleteLattice α] [CompleteLattice β]
+    [CompleteLattice γ] [CompleteLattice δ] (e : α ≃o γ) (e' : β ≃o δ) :
+    sSupHom α β ≃ sSupHom γ δ where
+  toFun f := (e' : sSupHom β δ).comp (f.comp e.symm)
+  invFun f := (e'.symm : sSupHom δ β).comp (f.comp e)
+  left_inv _ := by ext; simp
+  right_inv _ := by ext; simp
+
 end sSupHom
 
 /-! ### Frame homomorphisms -/
@@ -428,6 +439,15 @@ theorem cancel_right {g₁ g₂ : FrameHom β γ} {f : FrameHom α β} (hf : Sur
 theorem cancel_left {g : FrameHom β γ} {f₁ f₂ : FrameHom α β} (hg : Injective g) :
     g.comp f₁ = g.comp f₂ ↔ f₁ = f₂ :=
   ⟨fun h => ext fun a => hg <| by rw [← comp_apply, h, comp_apply], congr_arg _⟩
+
+@[simps]
+def _root_.OrderIso.frameHomCongr {α β γ δ : Type*} [CompleteLattice α] [CompleteLattice β]
+    [CompleteLattice γ] [CompleteLattice δ] (e : α ≃o γ) (e' : β ≃o δ) :
+    FrameHom α β ≃ FrameHom γ δ where
+  toFun f := (e' : FrameHom β δ).comp (f.comp e.symm)
+  invFun f := (e'.symm : FrameHom δ β).comp (f.comp e)
+  left_inv _ := by ext; simp
+  right_inv _ := by ext; simp
 
 instance : PartialOrder (FrameHom α β) :=
   PartialOrder.lift _ DFunLike.coe_injective
@@ -539,6 +559,15 @@ theorem cancel_right {g₁ g₂ : CompleteLatticeHom β γ} {f : CompleteLattice
 theorem cancel_left {g : CompleteLatticeHom β γ} {f₁ f₂ : CompleteLatticeHom α β}
     (hg : Injective g) : g.comp f₁ = g.comp f₂ ↔ f₁ = f₂ :=
   ⟨fun h => ext fun a => hg <| by rw [← comp_apply, h, comp_apply], congr_arg _⟩
+
+@[simps]
+def _root_.OrderIso.completeLatticeHomCongr {α β γ δ : Type*} [CompleteLattice α]
+    [CompleteLattice β] [CompleteLattice γ] [CompleteLattice δ] (e : α ≃o γ) (e' : β ≃o δ) :
+    CompleteLatticeHom α β ≃ CompleteLatticeHom γ δ where
+  toFun f := (e' : CompleteLatticeHom β δ).comp (f.comp e.symm)
+  invFun f := (e'.symm : CompleteLatticeHom δ β).comp (f.comp e)
+  left_inv _ := by ext; simp
+  right_inv _ := by ext; simp
 
 end CompleteLatticeHom
 
