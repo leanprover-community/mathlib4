71dba4b24fd491d5dca20be416a93ef793b6d426
manman4 <7023566+manman4@users.noreply.github.com>
2026-09-18 23:42:49 +0900

    style(PowerSeries): group induction parameters

--- Mathlib/RingTheory/PowerSeries/LagrangeInversion.lean
+++ Mathlib/RingTheory/PowerSeries/LagrangeInversion.lean
@@ -86,7 +86,7 @@ variable {P Y : R⟦X⟧}
 variable (hY : Y = X * P.subst Y)
 include hY
 
-private theorem lagrange_inversion_coeff_pow_of_le (m : ℕ) (k : ℕ) (hk : k ≤ m + 1) :
+private theorem lagrange_inversion_coeff_pow_of_le (m k : ℕ) (hk : k ≤ m + 1) :
       (m + 1) • (Y ^ k).coeff (m + 1) = k • (P ^ (m + 1)).coeff (m + 1 - k) := by
   induction m using Nat.strong_induction_on generalizing k with
   | h m ih =>
