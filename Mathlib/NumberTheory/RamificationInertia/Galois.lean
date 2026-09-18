b628b47f724f3450ba3b0824480c997004faa2ac
tb65536 <thomas.l.browning@gmail.com>
2026-09-18 09:22:39 +0100

    fix

--- Mathlib/NumberTheory/RamificationInertia/Galois.lean
+++ Mathlib/NumberTheory/RamificationInertia/Galois.lean
@@ -263,10 +263,8 @@ theorem ncard_primesOver_mul_ncard_primesOver :
       obtain ⟨g, hg⟩ :=
         IsInvariant.exists_smul_of_under_eq A C GAC Q Q' ((Q.over_def p).symm.trans (Q'.over_def p))
       exact ⟨⟨g, key Q Q' g hg.symm⟩, by simpa [Subgroup.smul_def] using hg.symm⟩
-  have stabilizer_eq : stabilizer H Q = (stabilizer GAC Q).subgroupOf H := by
-    simp [Subgroup.ext_iff, Subgroup.mem_subgroupOf]
   rw [← IsInvariant.orbit_eq_primesOver A B G p P, ← index_stabilizer,
-    ← orbit_eq, ← index_stabilizer, stabilizer_eq, ← Subgroup.relIndex,
+    ← orbit_eq, ← index_stabilizer, ← stabilizer_subgroupOf, ← Subgroup.relIndex,
     ← IsInvariant.orbit_eq_primesOver A C GAC p Q, ← index_stabilizer,
     ← (stabilizer G P).index_comap_of_surjective (restrictHom_surjective GAC G A B C),
     mul_comm, Subgroup.relIndex_mul_index]
