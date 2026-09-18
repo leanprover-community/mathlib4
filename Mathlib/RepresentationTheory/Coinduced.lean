6c5141a660227750dffa92b517a3a82564948f11
JX-Mo <jiaximo1998@163.com>
2026-09-18 23:40:58 +0800

    fix

--- Mathlib/RepresentationTheory/Coinduced.lean
+++ Mathlib/RepresentationTheory/Coinduced.lean
@@ -82,7 +82,7 @@ def coind : Representation k H (coindV φ ρ) where
   toFun h := (LinearMap.funLeft _ _ (· * h)).restrict fun x hx => (mem_coindV φ ρ _).mpr <| by
       simp [(mem_coindV φ ρ _).mp hx, mul_assoc]
   map_one' := by ext; simp
-  map_mul' _ _:= by ext; simp [mul_assoc]
+  map_mul' _ _ := by ext; simp [mul_assoc]
 
 @[simp]
 lemma coind_apply_apply (h x : H) (f : coindV φ ρ) :
