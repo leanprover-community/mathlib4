0a856e4090192b5417d257cd69df88bd41441831
Nicola Falciola <nicola.falciola@gmail.com>
2026-09-18 16:34:35 +0200

    Apply suggestion from @tb65536
    Co-authored-by: Thomas Browning <tb65536@users.noreply.github.com>

--- Mathlib/GroupTheory/Index.lean
+++ Mathlib/GroupTheory/Index.lean
@@ -299,7 +299,7 @@ theorem card_ker_mul_card_range (f : G →* G') : Nat.card f.ker * Nat.card f.ra
 @[to_additive card_ker_mul_card_of_surjective]
 theorem card_ker_mul_card_of_surjective (hf : Surjective f) :
     Nat.card f.ker * Nat.card G' = Nat.card G := by
-  rw [← card_ker_mul_card_range f, f.range_eq_top.mpr hf, card_top]
+  rw [← card_ker_mul_card_range f, f.range_eq_top_of_surjective hf, card_top]
 
 @[to_additive]
 theorem card_dvd_of_surjective (hf : Surjective f) : Nat.card G' ∣ Nat.card G :=
