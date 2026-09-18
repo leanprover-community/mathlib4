a014543c716a64adab8d5b1364fc3865fdba996a
Joël Riou <joel.riou@universite-paris-saclay.fr>
2026-09-18 12:54:57 +0200

    remove dsimp%

--- Mathlib/Algebra/Category/ModuleCat/Presheaf.lean
+++ Mathlib/Algebra/Category/ModuleCat/Presheaf.lean
@@ -154,7 +154,7 @@ lemma presheaf_map_apply_coe {X Y : Cᵒᵖ} (f : X ⟶ Y) (x : M.obj X) :
 
 @[reassoc]
 lemma smul_map {U V : Cᵒᵖ} (f : U ⟶ V) (r : R.obj U) :
-    dsimp% (ModuleCat.smul _ r).asHom ≫ M.presheaf.map f =
+    (ModuleCat.smul _ r).asHom ≫ M.presheaf.map f =
       M.presheaf.map f ≫ (ModuleCat.smul _ (R.map f r)).asHom := by
   ext x
   exact (M.map f).hom.map_smul r x
