3fd1f90941d2a595761ab2a00c3b95d9f1e9b10d
artie2000 <artem.khovanov@gmail.com>
2026-09-18 15:03:18 +0100

    finish renaming

--- Mathlib/AlgebraicTopology/SimplicialComplex/Basic.lean
+++ Mathlib/AlgebraicTopology/SimplicialComplex/Basic.lean
@@ -81,7 +81,7 @@ instance : LE (PreAbstractSimplicialComplex ι) where
 instance : LT (PreAbstractSimplicialComplex ι) where
   lt K L := K.faces ⊂ L.faces
 
-instance : IsConcreteLE (PreAbstractSimplicialComplex ι) (Finset ι) where
+instance : IsMemLE (PreAbstractSimplicialComplex ι) (Finset ι) where
   le_iff := .rfl
 
 instance : PartialOrder (PreAbstractSimplicialComplex ι) :=
@@ -205,7 +205,7 @@ instance : LE (AbstractSimplicialComplex ι) where
 instance : LT (AbstractSimplicialComplex ι) where
   lt K L := K.faces ⊂ L.faces
 
-instance : IsConcreteLE (AbstractSimplicialComplex ι) (Finset ι) where
+instance : IsMemLE (AbstractSimplicialComplex ι) (Finset ι) where
   le_iff := .rfl
 
 instance : PartialOrder (AbstractSimplicialComplex ι) :=
