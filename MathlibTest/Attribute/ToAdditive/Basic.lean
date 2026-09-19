f92e81519b6fe07a829196254336db6cc8b5fafd
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2026-09-19 18:22:30 +0200

    make the test theorem have a type correct proof, as suggested

--- MathlibTest/Attribute/ToAdditive/Basic.lean
+++ MathlibTest/Attribute/ToAdditive/Basic.lean
@@ -1078,6 +1078,25 @@ section errors
 abbrev FakeMul (α : Type) := Mul α
 
 variable {α : Type}
+
+/--
+error: `@[to_additive]` failed to add declaration `add_rfl`.
+  The translated type is not type correct.
+Application type mismatch: The argument
+  inst✝
+has type
+  Add α
+but is expected to have type
+  FakeMul α
+in the application
+  @inferInstance (FakeMul α) inst✝
+
+For help, see the docstring of `to_additive`, section `Troubleshooting`.
+-/
+#guard_msgs in
+@[to_additive]
+theorem mul_rfl [Mul α] (a b : α) : haveI : FakeMul α := inferInstance; a * b = a * b := Eq.refl (a * b)
+
 /--
 error: `@[to_additive]` failed to add declaration `instFakeAddOfAddGroup`.
   The translated value does not have the translated type.
@@ -1092,7 +1111,7 @@ For help, see the docstring of `to_additive`, section `Troubleshooting`.
 -/
 #guard_msgs in
 @[to_additive]
-instance [Group α] : FakeMul α := ⟨(· * ·)⟩
+local instance [Group α] : FakeMul α := ⟨(· * ·)⟩
 
 /--
 error: `@[to_additive]` failed to add declaration `instAddOfAddGroup_mathlibTest`.
@@ -1111,24 +1130,6 @@ For help, see the docstring of `to_additive`, section `Troubleshooting`.
 #guard_msgs in
 @[to_additive]
 instance [Group α] : Mul α := ⟨(· * ·)⟩
-
-/--
-error: `@[to_additive]` failed to add declaration `fakeAdd_rfl`.
-  The translated type is not type correct.
-Application type mismatch: The argument
-  inst✝
-has type
-  FakeMul α
-but is expected to have type
-  Add α
-in the application
-  @instHAdd α inst✝
-
-For help, see the docstring of `to_additive`, section `Troubleshooting`.
--/
-#guard_msgs in
-@[to_additive]
-theorem fakeMul_rfl [FakeMul α] (a b : α) : a * b = a * b := rfl
 
 end errors
 
