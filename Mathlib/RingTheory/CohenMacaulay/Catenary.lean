dc7513d2ce7d2e66b70d77d0169631d08561b57f
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2026-09-20 16:16:17 +0200

    fix

--- Mathlib/RingTheory/CohenMacaulay/Catenary.lean
+++ Mathlib/RingTheory/CohenMacaulay/Catenary.lean
@@ -264,7 +264,8 @@ lemma isRegular_of_maximalIdeal_mem_ofList_minimalPrimes
             fun r hr ↦ map_nonunit (Ideal.Quotient.mk (x • (⊤ : Ideal R))) r
             (mem.1.2 (Ideal.subset_span (by simp [hr])))
         exact ne_top_of_le_ne_top Ideal.IsPrime.ne_top' (Ideal.span_le.mpr mem_max)
-      have := (quotient_regular_smul_top_isCohenMacaulay_iff_isCohenMacaulay R x xreg xmem).mp ‹_›
+      have :=
+        (quotient_isSMulRegular_smul_top_isCohenMacaulay_iff_isCohenMacaulay R x xreg xmem).mp ‹_›
       rw [← RingTheory.Sequence.isWeaklyRegular_map_algebraMap_iff R' R' rs']
       apply hn (rs'.map (algebraMap R R')) min _ (by simpa using len)
       have : ringKrullDim (QuotSMulTop x R) + 1 = ringKrullDim R := by
@@ -288,8 +289,8 @@ lemma isRegular_of_ofList_height_eq_length_of_isCohenMacaulayLocalRing [IsCohenM
 
 lemma Ideal.depth_le_height [IsLocalRing R] (I : Ideal R) (netop : I ≠ ⊤) :
     I.depth (ModuleCat.of R R) ≤ I.height := by
-  simp only [IsLocalRing.ideal_depth_eq_sSup_length_regular I netop (ModuleCat.of R R), exists_prop,
-    sSup_le_iff, Set.mem_ofPred_eq, forall_exists_index, and_imp]
+  simp only [IsLocalRing.ideal_depth_eq_sSup_length_isRegular I netop (ModuleCat.of R R),
+    exists_prop, sSup_le_iff, Set.mem_ofPred_eq, forall_exists_index, and_imp]
   intro n rs reg mem len
   simpa [← len, ← ofList_height_eq_length_of_isWeaklyRegular' rs reg.1
     (fun r hr ↦ le_maximalIdeal netop (mem r hr))] using! Ideal.height_mono (span_le.mpr mem)
@@ -317,7 +318,7 @@ lemma Ideal.exist_regular_sequence_length_eq_height [IsCohenMacaulayLocalRing R]
 lemma Ideal.depth_eq_height [IsCohenMacaulayLocalRing R] (I : Ideal R) (netop : I ≠ ⊤) :
     I.depth (ModuleCat.of R R) = I.height := by
   apply le_antisymm (I.depth_le_height netop)
-  rw [IsLocalRing.ideal_depth_eq_sSup_length_regular I netop]
+  rw [IsLocalRing.ideal_depth_eq_sSup_length_isRegular I netop]
   apply le_sSup
   rcases Ideal.exist_regular_sequence_length_eq_height I netop with ⟨rs, reg, mem, len⟩
   use rs
@@ -330,7 +331,7 @@ lemma Ideal.height_add_ringKrullDim_quotient_eq_ringKrullDim_of_isPrime [IsCohen
     ((isCohenMacaulayLocalRing_iff R).mp ‹_›)
   have ht_eq := Ideal.ofList_height_eq_length_of_isWeaklyRegular' rs reg.1 mem'
   rw [← len, (isCohenMacaulayLocalRing_def R).mp ‹_›,
-    ← depth_quotient_regular_sequence_add_length_eq_depth (ModuleCat.of R R) rs reg]
+    ← depth_quotient_isRegular_add_length_eq_depth (ModuleCat.of R R) rs reg]
   have ass : p ∈ associatedPrimes R (R ⧸ ofList rs • (⊤ : Ideal R)) := by
     apply Module.associatedPrimes.minimalPrimes_annihilator_subset_associatedPrimes
     simp only [smul_eq_mul, annihilator_quotient, mul_top]
