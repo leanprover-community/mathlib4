4b640ca715b1adf837534c140516cff9ab6a9416
JX-Mo <jiaximo1998@163.com>
2026-09-20 23:56:52 +0800

    Define noncomputable indMap for intertwining maps
    Add noncomputable indMap definition for intertwining maps.

--- Mathlib/RepresentationTheory/Induced.lean
+++ Mathlib/RepresentationTheory/Induced.lean
@@ -204,6 +204,7 @@ lemma indResHomEquiv_symm_apply_mk (f : ρ.IntertwiningMap (σ.comp φ)) (h : H)
     (indResHomEquiv φ ρ σ).symm f (indV.mk φ ρ h a) = σ h⁻¹ (f a) :=
   ind.lift_apply_mk φ f h a
 
+/-- tbd -/
 noncomputable def indMap (f : IntertwiningMap ρ τ) :
     (ind φ ρ).IntertwiningMap (ind φ τ) :=
   ind.lift φ ⟨indV.mk φ τ 1 ∘ₗ f, fun g => by ext; simp [IntertwiningMap.isIntertwining]⟩
