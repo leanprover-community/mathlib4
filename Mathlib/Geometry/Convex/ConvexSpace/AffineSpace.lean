d1585ae1bfcbc69aa8880d1432705322b00664d4
ovi <ovi@posteo.de>
2026-09-21 18:31:55 +0200

    nit

--- Mathlib/Geometry/Convex/ConvexSpace/AffineSpace.lean
+++ Mathlib/Geometry/Convex/ConvexSpace/AffineSpace.lean
@@ -5,7 +5,6 @@ Authors: Kim Morrison
 -/
 module
 
-public import Mathlib.Geometry.Convex.ConvexSpace.Defs
 public import Mathlib.Geometry.Convex.ConvexSpace.Module
 public import Mathlib.LinearAlgebra.AffineSpace.Combination
 public import Mathlib.LinearAlgebra.AffineSpace.AffineMap
