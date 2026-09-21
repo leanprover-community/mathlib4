f749c765cff081c1b7adada169e232fb3eccf919
ovi <ovi@posteo.de>
2026-09-21 14:17:42 +0200

    reviewed

--- Mathlib/LinearAlgebra/AffineSpace/AffineSubspace/Range.lean
+++ Mathlib/LinearAlgebra/AffineSpace/AffineSubspace/Range.lean
@@ -7,17 +7,20 @@ module
 
 public import Mathlib.LinearAlgebra.AffineSpace.AffineSubspace.Basic
 
-import Mathlib.LinearAlgebra.AffineSpace.AffineMap
-
 /-!
-# Range of linear maps
+# Range of affine maps
 
 The range of an affine map `f : P₁ →ᵃ[R] P₂` is an affine subspace of `P₂`.
+
+# Implementation note
+
+Follows `LinearMap.range`.
+
 -/
 
 @[expose] public section
 
-open Affine Module AffineMap
+open Affine Module AffineMap AffineSubspace
 
 section
 
@@ -40,16 +43,18 @@ def range : AffineSubspace R P₂ where
     intro c _ _ _ x₁ h₁ x₂ h₂ x₃ h₃
     exact ⟨c • (x₁ -ᵥ x₂) +ᵥ x₃, by simp [map_vadd, ← h₁, ← h₂, ← h₃]⟩
 
+/-- The range of an affine map is nonempty. -/
 instance instNonemptyRange : Nonempty (range f) :=
   Set.instNonemptyRange f
 
+@[simp, norm_cast]
 theorem coe_range : f.range = Set.range f := rfl
 
 @[simp]
 theorem mem_range (x : P₂) : x ∈ f.range ↔ ∃ (y : P₁), f y = x :=
   Iff.rfl
 
-theorem range_eq_copmap_top : f.range = .map f ⊤ := by ext; simp
+theorem range_eq_map_top : f.range = .map f ⊤ := by ext; simp
 
 theorem mem_range_self (x : P₁) : f x ∈ f.range := by simp
 
@@ -57,20 +62,7 @@ theorem mem_range_self (x : P₁) : f x ∈ f.range := by simp
 theorem range_id : (id R P₁).range = ⊤ := by ext; simp
 
 theorem range_direction_eq_linear_range : f.range.direction = f.linear.range := by
-  apply le_antisymm
-  · apply Submodule.span_le.mpr
-    intro _ ⟨p₁, h₁, p₂, h₂, h⟩
-    simp only [SetLike.mem_coe, mem_range] at h₁ h₂
-    obtain ⟨p₁, rfl⟩ := h₁
-    obtain ⟨p₂, rfl⟩ := h₂
-    exact ⟨p₁ -ᵥ p₂, h ▸ f.linearMap_vsub _ _⟩
-  · apply Submodule.span_le.mp
-    intro v hv
-    rw [Submodule.span_coe_eq_restrictScalars] at hv
-    obtain ⟨w, rfl⟩ := hv
-    apply Submodule.subset_span
-    simp only [Set.mem_vsub, SetLike.mem_coe, mem_range, exists_exists_eq_and]
-    exact ⟨(w +ᵥ Classical.arbitrary P₁), (Classical.arbitrary P₁), by simp⟩
+  rw [range_eq_map_top, map_direction, direction_top, Submodule.map_top]
 
 /-- Restrict the codomain of an affine map `f` to `f.range`. -/
 def rangeRestrict : P₁ →ᵃ[R] f.range where
@@ -79,23 +71,19 @@ def rangeRestrict : P₁ →ᵃ[R] f.range where
     f.linear.rangeRestrict
   map_vadd' _ _ := by ext; simp [map_vadd]
 
+theorem surjective_rangeRestrict : Function.Surjective ⇑f.rangeRestrict :=
+  fun ⟨_, y, rfl⟩ => ⟨y, rfl⟩
+
 @[simp]
 theorem range_rangeRestrict : f.rangeRestrict.range = ⊤ := by
   ext ⟨_, x, rfl⟩
   simpa [AffineSubspace.mem_top, iff_true] using ⟨x, rfl⟩
 
-theorem ker_rangeRestrict : f.rangeRestrict ⁻¹' ⊥ = f ⁻¹' ⊥ := by simp
-
-theorem surjective_rangeRestrict : Function.Surjective ⇑f.rangeRestrict := by
-  intro ⟨x, ⟨y, hy⟩⟩
-  use y; simp [rangeRestrict, hy]
-
 @[simp]
-lemma injective_rangeRestrict_iff :
-    (rangeRestrict f).toFun.Injective ↔ f.toFun.Injective := by
-  constructor <;> intro h _ _ hpq <;> apply h
-  · exact Subtype.ext hpq
-  · exact congrArg Subtype.val hpq
+theorem injective_rangeRestrict_iff :
+    Function.Injective (rangeRestrict f) ↔ Function.Injective f := by
+  convert (Function.Injective.of_comp_iff Subtype.val_injective f.rangeRestrict).symm
+  ext; simp [rangeRestrict]
 
 end AffineMap
 
