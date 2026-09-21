673fa11dde367f796696d2b5b063c1cffdf554aa
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2026-09-21 09:31:47 +0200

    golf

--- Mathlib/RingTheory/ReesAlgebra.lean
+++ Mathlib/RingTheory/ReesAlgebra.lean
@@ -144,18 +144,19 @@ lemma mem_map_algebraMap_reesAlgebra_iff (f : reesAlgebra I) :
     | smul r hr m hm =>
       simpa [pow_succ'] using Ideal.mul_mem_mul hr ((mem_reesAlgebra_iff I _).mp m.2 n)
     | add x hx y hy memx memy => simpa using add_mem memx memy
-  · have mem' (i : ℕ) {r : R} : r ∈ I ^ i → _ := reesAlgebra.monomial_mem.mpr
-    have mem (i : ℕ) := reesAlgebra.monomial_mem.mpr ((mem_reesAlgebra_iff I _).mp f.2 i)
+  · have mem (i : ℕ) : (monomial i) (f.1.coeff i) ∈ reesAlgebra I :=
+      reesAlgebra.monomial_mem.mpr ((mem_reesAlgebra_iff I _).mp f.2 i)
     have : f = ∑ i ∈ f.1.support, ⟨monomial i (f.1.coeff i), mem i⟩ :=
       SetCoe.ext (by simpa using f.1.as_sum_support)
     rw [this]
     apply sum_mem (fun i hi ↦ ?_)
-    have {r : R} (h' : r ∈ I * I ^ i) : ⟨monomial i r, mem' i (Ideal.mul_le_right h')⟩
-      ∈ I.map (algebraMap R (reesAlgebra I)) := by
+    have {r : R} (h' : r ∈ I * I ^ i) :
+      ⟨monomial i r, reesAlgebra.monomial_mem.mpr (Ideal.mul_le_right h')⟩
+        ∈ I.map (algebraMap R (reesAlgebra I)) := by
       induction h' using Submodule.mul_induction_on' with
       | mem_mul_mem s hs t ht =>
         simp_rw [← smul_eq_mul, ← smul_monomial]
-        rw [← SetLike.mk_smul_mk (hx := mem' i ht), Algebra.smul_def]
+        rw [← SetLike.mk_smul_mk (hx := reesAlgebra.monomial_mem.mpr ht), Algebra.smul_def]
         exact Ideal.mul_mem_right _ _ (Ideal.mem_map_of_mem _ hs)
       | add s1 hs1 s2 hs2 mem1 mem2 => simpa using add_mem mem1 mem2
     apply this
