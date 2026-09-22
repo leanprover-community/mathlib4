27afe46896624112caa151e741811b8d9a365510
Nicola Falciola <nicola.falciola@gmail.com>
2026-09-22 23:43:40 +0200

    Update SpecialLinearGroup.lean

--- Mathlib/LinearAlgebra/Matrix/SpecialLinearGroup.lean
+++ Mathlib/LinearAlgebra/Matrix/SpecialLinearGroup.lean
@@ -217,7 +217,7 @@ theorem toLin'_injective :
     Function.Injective ↑(toLin' : SpecialLinearGroup n R →* (n → R) ≃ₗ[R] n → R) := fun _ _ h =>
   Subtype.coe_injective <| Matrix.toLin'.injective <| LinearEquiv.toLinearMap_injective.eq_iff.mpr h
 
-variable {S : Type*} [CommRing S]
+variable {S T : Type*} [CommRing S] [CommRing T]
 
 /-- A ring homomorphism from `R` to `S` induces a group homomorphism from
 `SpecialLinearGroup n R` to `SpecialLinearGroup n S`. -/
@@ -229,6 +229,52 @@ def map (f : R →+* S) : SpecialLinearGroup n R →* SpecialLinearGroup n S whe
       simp [g.prop]⟩
   map_one' := Subtype.ext f.mapMatrix.map_one
   map_mul' x y := Subtype.ext <| f.mapMatrix.map_mul ↑ₘx ↑ₘy
+
+@[simp] lemma map_comp (f : R →+* S) (g : S →+* T) :
+    (map (n := n) g).comp (map f) = map (g.comp f) := rfl
+
+/-- A ring isomorphism `R ≃+* S` induces `SL(n, R) ≃* SL(n, S)`. -/
+@[simps!]
+def mapEquiv (e : R ≃+* S) : SpecialLinearGroup n R ≃* SpecialLinearGroup n S where
+  toFun := map e
+  invFun := map e.symm
+  left_inv A := by ext; simp
+  right_inv A := by ext; simp
+  map_mul' := map_mul _
+
+@[simp] lemma mapEquiv_refl : mapEquiv (.refl R) = .refl (SpecialLinearGroup n R) := rfl
+
+@[simp] lemma symm_mapEquiv (f : R ≃+* S) :
+    (mapEquiv (n := n) f).symm = mapEquiv f.symm := rfl
+
+@[simp] lemma mapEquiv_trans (f : R ≃+* S) (g : S ≃+* T) :
+    mapEquiv (n := n) (f.trans g) = (mapEquiv f).trans (mapEquiv g) := rfl
+
+@[simp] lemma toMonoidHom_mapEquiv (f : R ≃+* S) :
+    (mapEquiv (n := n) f : SpecialLinearGroup n R →* SpecialLinearGroup n S)
+      = map (f : R →+* S) := rfl
+
+@[simp]
+lemma coe_mapEquiv (e : R ≃+* S) (A : SpecialLinearGroup n R) :
+    (mapEquiv e A : Matrix n n S) = (e : R →+* S).mapMatrix A :=
+  rfl
+
+section Pi
+
+variable {ι : Type*} (R : ι → Type*) [Π i, CommRing (R i)]
+
+/-- The monoid equivalence between `SL n` of a product of rings,
+and the product of the `SL n` of each ring. -/
+@[simps!]
+def piEquiv : SpecialLinearGroup n (Π i, R i) ≃* Π i, SpecialLinearGroup n (R i) where
+  toFun A i := map (Pi.evalRingHom R i) A
+  invFun A := ⟨of fun a b i ↦ A i a b, funext fun i ↦
+    (RingHom.map_det (Pi.evalRingHom R i) (of fun a b i ↦ A i a b)).trans (A i).det_coe⟩
+  left_inv A := by ext; rfl
+  right_inv A := funext fun _ ↦ by ext; rfl
+  map_mul' A B := by ext; simp
+
+end Pi
 
 section center
 
