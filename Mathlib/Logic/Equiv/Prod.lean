050cdd2773ae10a4d3e9979bef8f34dee865fd65
Moritz Doll <moritz.doll@googlemail.com>
2026-09-23 20:35:56 +1000

    Update Mathlib/Logic/Equiv/Prod.lean
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- Mathlib/Logic/Equiv/Prod.lean
+++ Mathlib/Logic/Equiv/Prod.lean
@@ -98,8 +98,7 @@ theorem prodComm_symm (α β) : (prodComm α β).symm = prodComm β α :=
   rfl
 
 @[simp]
-theorem prodComm_trans_prodComm (α β) : (prodComm α β).trans (prodComm β α) = Equiv.refl _ :=
-  rfl
+theorem prodComm_trans_prodComm (α β) : (prodComm α β).trans (prodComm β α) = .refl _ := rfl
 
 /-- Type product is associative up to an equivalence. -/
 @[simps (attr := grind =)]
