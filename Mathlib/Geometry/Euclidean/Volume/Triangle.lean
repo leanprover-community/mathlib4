ca4f59d619f4f03ad86df3811b9657a1aaace210
Weiyi Wang <wwylele@gmail.com>
2026-09-23 14:12:09 -0400

    Update Mathlib/Geometry/Euclidean/Volume/Triangle.lean
    Co-authored-by: Snir Broshi <26556598+SnirBroshi@users.noreply.github.com>

--- Mathlib/Geometry/Euclidean/Volume/Triangle.lean
+++ Mathlib/Geometry/Euclidean/Volume/Triangle.lean
@@ -36,8 +36,7 @@ include h₁₂ h₁₃ h₂₃ in
 theorem volume_eq_height_mul : t.volume = 2⁻¹ * t.height i₁ * dist (t.points i₂) (t.points i₃) := by
   let e : Fin 3 ≃ Fin 3 := List.Nodup.getEquivOfForallMemList [i₁, i₂, i₃] (by grind) (by grind)
   let s := t.reindex e.symm
-  suffices s.volume = 2⁻¹ * s.height 0 * dist (s.points 1) (s.points 2) by
-    simpa [s, e, List.Nodup.getEquivOfForallMemList_apply [i₁, i₂, i₃]]
+  suffices s.volume = 2⁻¹ * s.height 0 * dist (s.points 1) (s.points 2) by simpa [s]
   simp [s.volume_eq 0, volume_eq_dist, faceOpposite_point_eq_point_succAbove]
 
 include h₁₂ h₁₃ h₂₃ in
