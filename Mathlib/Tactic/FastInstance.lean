leanprover-community/mathlib4

commit fde0cc508f5375f278f515cb2f50a34a545a4c5c

feat(Tactic/FastInstance): use expected type to fill in arguments (#36420)
This PR improves `fast_instance%` by letting it use the expected type to fill in arguments. This allows us to then define `inferInstanceAs% A` as a macro for `fast_instance% inferInstanceAs A`.

Mathlib/Tactic/FastInstance.lean
@@ -1,18 +1,17 @@
 /-
 Copyright (c) 2024 Eric Wieser. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Eric Wieser, Kyle Miller
+Authors: Eric Wieser, Kyle Miller, Jovan Gerbscheid
 -/
 module
 
-public meta import Lean.Elab.SyntheticMVars
 public import Mathlib.Init
-public meta import Std.Do
+
 /-!
-# The `fast_instance%` term elaborator
+# The `fast_instance%` and `inferInstanceAs%` term elaborators
 -/
 
-public meta section
+meta section
 
 namespace Mathlib.Elab.FastInstance
 
@@ -25,7 +24,7 @@ Throw an error for `makeFastInstance`. The trace is a list of fields.
 Note: with the current implementation, this might not be accurate for multi-structure types,
 since `makeFastInstance` just uses `ClassName.paramName` for the trace.
 -/
-private def error {α : Type _} (trace : Array Name) (m : MessageData) : MetaM α :=
+def error {α : Type _} (trace : Array Name) (m : MessageData) : MetaM α :=
   throwError "\
     {m}\n\n\
     Use `set_option trace.Elab.fast_instance true` to analyze the error.\n\n\
@@ -36,77 +35,81 @@ Core algorithm for normalizing instances.
 * Ideally, the term is replaced with a synthesized instance.
 * If not, it is reduced to a constructor
   and each instance implicit field is given the same treatment.
-  If the type is a structure, the algorithm throws an error;
-  we're more lenient with non-structure classes.
 
 Many reductions for typeclasses are done with reducible transparency, so the entire body
 is `withReducible` with some exceptions.
 -/
-private partial def makeFastInstance (provided : Expr) (trace : Array Name := #[]) :
+partial def makeFastInstance (inst expectedType : Expr) (trace : Array Name := #[]) :
     MetaM Expr := withReducible do
-  let ty ← inferType provided
-  withTraceNode `Elab.fast_instance (fun e => return m!"{exceptEmoji e} type: {ty}") do
-  let some className ← isClass? ty
-    | error trace m!"Can only be used for classes, but term has type{indentExpr ty}"
+  withTraceNode `Elab.fast_instance (fun e => return m!"{exceptEmoji e} type: {expectedType}") do
+  let some className ← isClass? expectedType
+    | error trace m!"Can only be used for classes, but type is{indentExpr expectedType}"
   trace[Elab.fast_instance] "class is {className}"
-  if ← withDefault <| Meta.isProp ty then
-    error trace m!"\
-      Provided instance{indentExpr provided}\n\
+  if ← isProp expectedType then
+    logWarning m!"Provided instance{indentExpr inst}\n\
       is a proof, which does not need normalization."
+    return inst
 
   -- Try to synthesize a total replacement for this term:
-  if let .some new ← trySynthInstance ty then
-    if ← withReducibleAndInstances <| isDefEq provided new then
+  if let .some new ← trySynthInstance expectedType then
+    if ← withDefault <| isDefEq inst new then
       trace[Elab.fast_instance] "replaced with synthesized instance"
       return new
     else
-      if ← withDefault <| isDefEq provided new then
-        error trace m!"\
-          Provided instance{indentExpr provided}\n\
-          is defeq only at default transparency to inferred instance{indentExpr new}"
-      else
-        error trace m!"\
-          Provided instance{indentExpr provided}\n\
-          is not defeq to inferred instance{indentExpr new}"
+      error trace m!"\
+        Provided instance{indentExpr inst}\n\
+        is not defeq to inferred instance{indentExpr new}"
   -- Otherwise, try to reduce it to a constructor.
   else
-    -- Telescope since it might be a family of instances.
-    forallTelescopeReducing ty fun tyArgs _ => do
-      let provided' ← withReducibleAndInstances <| whnf <| mkAppN provided tyArgs
-      let error' (m : MessageData) : MetaM Expr := do
-        if isStructure (← getEnv) className then
-          error trace m
+    (← whnfI inst).withApp fun f args => do
+    let error' (m : MessageData) : MetaM Expr := do
+      if isStructure (← getEnv) className then
+        error trace m
+      else
+        error trace m!"{m}\n\n\
+          This instance is not a structure and not canonical. \
+          Use a separate 'instance' command to define it."
+    let .const c _ := f
+      | error' m!"\
+          Provided instance does not reduce to a constructor application{indentExpr inst}"
+    let .ctorInfo ci ← getConstInfo c
+      | error' m!"\
+          Provided instance does not reduce to a constructor application{indentExpr inst}\n\
+          Reduces to an application of {c}."
+    let (mvars, bis, cls) ← forallMetaTelescope (← inferType f)
+    unless args.size == mvars.size do
+      -- This is an invalid term.
+      throwError "Incorrect number of arguments for constructor application `{f}`: {args}"
+    -- Unify the parameters
+    unless ← isDefEq expectedType cls do
+      throwError "`{expectedType}` does not unify with the conclusion of `{.ofConstName c}`"
+    -- TODO: use structure eta reduction when possible?
+    for i in ci.numParams...args.size do
+      let bi := bis[i]!
+      let mvarId := mvars[i]!.mvarId!
+      let mvarDecl ← mvarId.getDecl
+      let argExpectedType ← instantiateMVars mvarDecl.type
+      let arg := args[i]!
+      if ← isProp argExpectedType then
+        -- For proofs, create an auxiliary theorem of the expected type.
+        if ← withDefault <| isDefEq argExpectedType (← inferType arg) then
+          mvarId.assign <| ← mkAuxTheorem argExpectedType arg (zetaDelta := true)
         else
-          error trace m!"{m}\n\n\
-            This instance is not a structure and not canonical. \
-            Use a separate 'instance' command to define it."
-      let .const c .. := provided'.getAppFn
-        | error' m!"\
-            Provided instance does not reduce to a constructor application{indentExpr provided}"
-      let some (.ctorInfo ci) := (← getEnv).find? c
-        | error' m!"\
-            Provided instance does not reduce to a constructor application{indentExpr provided}\n\
-            Reduces to an application of {c}."
-      let mut args := provided'.getAppArgs
-      let params ← withDefault <| forallTelescopeReducing ci.type fun args _ =>
-        args.mapM fun arg => do
-          let recurse ← (return (← arg.fvarId!.getBinderInfo).isInstImplicit)
-                        <&&> not <$> Meta.isProof arg
-          return (recurse, ← arg.fvarId!.getUserName)
-      unless args.size == params.size do
-        -- This is an invalid term.
-        throwError "Incorrect number of arguments for constructor application{indentExpr provided'}"
-      for i in [ci.numParams:args.size] do
-        let (recurse, binderName) := params[i]!
-        if recurse then
-          let trace' := trace.push (className ++ binderName)
-          args := args.set! i (← makeFastInstance args[i]! (trace := trace'))
-      let provided' := mkAppN provided'.getAppFn args
-      mkLambdaFVars tyArgs provided'
+          throwError "Proof `{arg}` does not have expected type `{argExpectedType}`"
+      -- Recurse into instance arguments of the constructor
+      else if bi.isInstImplicit then
+        let trace' := trace.push (className ++ mvarDecl.userName)
+        mvarId.assign (← makeFastInstance arg argExpectedType (trace := trace'))
+      else
+        -- For data fields, make sure that the lambda binders have the right type.
+        forallTelescopeReducing argExpectedType fun xs _ ↦ do
+          mvarId.assign <| ← mkLambdaFVars xs (arg.beta xs)
+    return mkAppN f (← mvars.mapM instantiateMVars)
 
 /--
 `fast_instance% inst` takes an expression for a typeclass instance `inst`, and unfolds it into
-constructor applications that leverage existing instances.
+constructor applications that leverage existing instances. It uses the expected type to fill in
+the constructor applications and lambda binders of data fields.
 
 For instance, when used as
 ```lean
@@ -121,15 +124,24 @@ rather than having to break it down into smaller pieces.
 syntax (name := fastInstance) "fast_instance% " term : term
 
 @[term_elab fastInstance, inherit_doc fastInstance]
-def elabFastInstance : TermElab
-  | `(term| fast_instance%%$tk $arg), expectedType => do
-    let provided ← withSynthesize <| elabTerm arg expectedType
-    withRef tk do
-      try
-        makeFastInstance provided
-      catch e =>
-        logException e
-        return provided
+public def elabFastInstance : TermElab
+  | `(term| fast_instance% $arg), expectedType? => do
+    let inst ← withSynthesize <| elabTerm arg expectedType?
+    let expectedType ← expectedType?.getDM (inferType inst)
+    try
+      -- Telescope since it might be a family of instances.
+      forallTelescopeReducing expectedType fun xs expectedType => do
+        mkLambdaFVars xs <| ← withNewMCtxDepth <| makeFastInstance inst expectedType
+    catch e =>
+      logException e
+      return inst
   | _, _ => Elab.throwUnsupportedSyntax
+
+/-- `inferInstanceAs% A` is shorthand for `fast_instance% inferInstanceAs A`.
+This is preferred over `inferInstanceAs` when the instance can be reduced to
+constructor applications. In that case, the parameters of the constructors will be filled in
+using the expected type, so that the instance will unfold nicely during unification. -/
+macro "inferInstanceAs% " source:term : term =>
+  `(fast_instance% inferInstanceAs $source)
 
 end Mathlib.Elab.FastInstance

Mathlib/Algebra/Order/Ring/StandardPart.lean
@@ -145,7 +145,7 @@ def FiniteResidueField : Type _ :=
 namespace FiniteResidueField
 
 noncomputable instance : Field (FiniteResidueField K) :=
-  inferInstanceAs (Field (IsLocalRing.ResidueField _))
+  inferInstanceAs% Field (IsLocalRing.ResidueField _)
 
 private theorem ordConnected_preimage_mk' : ∀ x, Set.OrdConnected <| Quotient.mk
     (Submodule.quotientRel (IsLocalRing.maximalIdeal (FiniteElement K))) ⁻¹' {x} := by
@@ -198,12 +198,6 @@ theorem mk_lt_mk {x y : FiniteElement K} : mk x < mk y ↔ x < y ∧ mk x ≠ mk
 theorem lt_of_mk_lt_mk {x y : FiniteElement K} (h : mk x < mk y) : x < y :=
   (mk_lt_mk.1 h).1
 
-#adaptation_note /-- Upon bumping to v4.29.0-rc3, this was previously proved by
-`grind [mul_le_mul_of_nonneg_left]`, but `grind` fails due to `inferInstanceAs` leakage in the
-`Field (FiniteResidueField K)` instance causing a defeq mismatch between
-`DivisionMonoid.toDivInvOneMonoid.toInvOneClass.toInv` and `Field.toGrindField.toInv`.
-See https://leanprover.zulipchat.com/#narrow/channel/113488-general/topic/backward.2EisDefEq.2ErespectTransparency/near/576520256
--/
 private theorem mul_le_mul_of_nonneg_left' {x y z : FiniteResidueField K} (h : x ≤ y) (hz : 0 ≤ z) :
     z * x ≤ z * y := by
   induction x with | mk x
@@ -212,11 +206,7 @@ private theorem mul_le_mul_of_nonneg_left' {x y z : FiniteResidueField K} (h : x
   rw [← map_mul, ← map_mul]
   rw [← map_zero mk] at hz
   rw [mk_le_mk] at h hz ⊢
-  rcases h with h | h
-  · rcases hz with hz | hz
-    · simp [mul_le_mul_of_nonneg_left h hz]
-    · simp [← hz]
-  · simp [h]
+  grind [mul_le_mul_of_nonneg_left]
 
 instance : IsOrderedRing (FiniteResidueField K) where
   zero_le_one := mk.monotone' zero_le_one

Mathlib/NumberTheory/Padics/WithVal.lean
@@ -96,6 +96,7 @@ lemma isDenseInducing_cast_withVal : IsDenseInducing ((Rat.castHom ℚ_[p]).comp
   -- nhds_discrete causes timeouts on TC search
   simpa [-nhds_discrete] using Padic.denseRange_ratCast p _
 
+set_option backward.isDefEq.respectTransparency false in
 open Completion in
 open scoped Valued in
 /-- The `p`-adic numbers are isomorphic as a field to the completion of the rationals at

Mathlib/Order/BooleanAlgebra/Set.lean
@@ -8,6 +8,7 @@ module
 public import Mathlib.Data.Set.Insert
 public import Mathlib.Order.BooleanAlgebra.Basic
 public import Mathlib.Tactic.Tauto
+public import Mathlib.Tactic.FastInstance
 
 /-!
 # Boolean algebra of sets
@@ -33,18 +34,11 @@ open Function
 namespace Set
 variable {α β : Type*} {s s₁ s₂ t t₁ t₂ u : Set α} {a b : α}
 
-/- In #35959, to make sure that the defeq abuse between `α → Prop` and `Set α` does not leak from
-`inferInstanceAs (BooleanAlgebra (α → Prop))`, we define explicitly the data fields. This could be
-avoided by using a better elaborator `inferInstanceAs%` fixing the defeqs. -/
-instance instBooleanAlgebra : BooleanAlgebra (Set α) where
-  __ : DistribLattice (Set α) := inferInstance
-  __ : BooleanAlgebra (Set α) := inferInstanceAs (BooleanAlgebra (α → Prop))
-  compl := (·ᶜ)
-  sdiff := (· \ ·)
-  top := univ
-  bot := ∅
+instance : HImp (Set α) where
   himp s t := {x | x ∈ s → x ∈ t}
-  himp_eq s t := by ext; simp [imp_iff_or_not]
+
+instance instBooleanAlgebra : BooleanAlgebra (Set α) :=
+  fast_instance% { inferInstanceAs (BooleanAlgebra (α → Prop)) with }
 
 /-- See also `Set.sdiff_inter_right_comm`. -/
 lemma inter_diff_assoc (a b c : Set α) : (a ∩ b) \ c = a ∩ (b \ c) := inf_sdiff_assoc ..

MathlibTest/DefEqAbuse.lean
@@ -123,20 +123,7 @@ instance mySub₂MyAction {G : Type} [AddCommGroup G] (s : MySub₂ G) :
 def myOp {α : Type} [AddCommGroup α] [MyAction ℕ α] (x : α) : α :=
   -(MyAction.mySmul (R := ℕ) 1 x)
 
--- The warning output contains fvar IDs that vary between runs, so we just check it produces
--- a warning (not info or error).
--- It should produce something like:
-/-
-warning: #defeq_abuse: command fails with `backward.isDefEq.respectTransparency true` but succeeds with `false`.
-The following synthesis applications fail due to transparency:
-  ❌️ apply @mySub₂MyAction to MyAction ℕ ↥s
-    ❌️ s.toAddSubgroup =?= s.1
-    ❌️ s.toAddSubgroup =?= s.toAddSubmonoid
-    ❌️ s.toAddSubgroup.1 =?= s.1
-    ❌️ ↑s.toAddSubgroup =?= ↑s.toAddSubmonoid
--/
-#guard_msgs (drop warning) in
-#defeq_abuse in
+#guard_msgs in
 def testVirtualParent {G : Type} [AddCommGroup G] (s : MySub₂ G) (x : s) : s :=
   myOp x
 

MathlibTest/InferInstanceAsPercent.lean
@@ -1,0 +1,113 @@
+/-
+Copyright (c) 2026 Lean FRO, LLC. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Kim Morrison
+-/
+import Mathlib.Tactic.FastInstance
+
+/-!
+# Tests for `inferInstanceAs%`
+
+Demonstrates that `inferInstanceAs%` fixes type leakage in synthesized instances:
+when defining a typeclass instance for a type alias, the lambda binder domains
+use the alias type (not an internal unfolding).
+-/
+
+set_option pp.funBinderTypes true
+
+class MyInv (α : Type) where
+  myInv : α → α
+
+instance : MyInv Nat where
+  myInv n := n + 1
+
+def MyNat : Type := Nat
+
+-- `inferInstanceAs` leaks the source type `Nat` as the carrier
+@[implicit_reducible]
+def myNatInv_leaky : MyInv MyNat :=
+  inferInstanceAs (MyInv Nat)
+
+-- `inferInstanceAs%` fixes this — the carrier and lambda domain are `MyNat`
+instance myNatInv_fixed : MyInv MyNat :=
+  inferInstanceAs% (MyInv Nat)
+
+-- The binder type is `MyNat`:
+/--
+info: @[implicit_reducible] def myNatInv_fixed : MyInv MyNat :=
+{ myInv := fun (a : MyNat) => a + 1 }
+-/
+#guard_msgs in
+#print myNatInv_fixed
+
+-- Both instances should compute the same thing
+example : myNatInv_leaky.myInv (α := MyNat) (5 : Nat) = myNatInv_fixed.myInv (5 : Nat) := rfl
+
+/-! ## Deeper hierarchy: reproducing the grind failure pattern
+
+The original failure involved `Field (FiniteResidueField K)` defined via
+`inferInstanceAs (Field (IsLocalRing.ResidueField _))`. Deeply nested sub-instances
+(e.g. `DivisionMonoid.toDivInvOneMonoid.toInvOneClass.toInv`) had lambda domains
+referring to `IsLocalRing.ResidueField _` instead of `FiniteResidueField K`.
+This caused `isDefEq` failures at `instances` transparency — the level used by
+grind's `checkInst`.
+
+We reproduce the pattern with a smaller 3-level hierarchy and verify the
+transparency-level failure directly.
+-/
+
+class TestInv (α : Type) where
+  inv : α → α
+
+class TestDivInvMonoid (α : Type) extends TestInv α where
+  mul : α → α → α
+
+class TestField (α : Type) extends TestDivInvMonoid α where
+  add : α → α → α
+  neg : α → α
+
+instance : TestField Nat where
+  inv n := n
+  mul := Nat.mul
+  add := Nat.add
+  neg n := n
+
+def TestNat := Nat
+
+-- Direct instance: all lambda domains correctly use TestNat
+@[implicit_reducible]
+def testField_direct : TestField TestNat where
+  inv n := n
+  mul := Nat.mul
+  add := Nat.add
+  neg n := n
+
+-- Leaky: internal lambda domains use Nat instead of TestNat
+@[implicit_reducible]
+def testField_leaky : TestField TestNat := inferInstanceAs (TestField Nat)
+
+-- Fixed: inferInstanceAs% patches lambda domains to use TestNat
+@[implicit_reducible]
+def testField_fixed : TestField TestNat := inferInstanceAs% (TestField Nat)
+
+-- All three are defeq at default transparency (Nat = TestNat at this level).
+example : testField_leaky = testField_direct := rfl
+example : testField_fixed = testField_direct := rfl
+
+-- At `instances` transparency (the level grind's `checkInst` uses):
+-- the leaky instance is NOT defeq to a directly-defined instance,
+-- because lambda domains say `Nat` instead of `TestNat` and
+-- `TestNat := Nat` is not unfolded at this transparency.
+/--
+error: Tactic `rfl` failed: The left-hand side
+  testField_leaky
+is not definitionally equal to the right-hand side
+  testField_direct
+
+⊢ testField_leaky = testField_direct
+-/
+#guard_msgs in
+example : testField_leaky = testField_direct := by with_reducible_and_instances rfl
+
+-- The fixed instance IS defeq at `instances` transparency.
+example : testField_fixed = testField_direct := by with_reducible_and_instances rfl

MathlibTest/fast_instance.lean
@@ -117,7 +117,7 @@ abbrev dec1 : Decidable It := isTrue sorry
 #guard_msgs in
 def dec2 : Decidable It := isTrue sorry
 
-/-- info: @Dec.mk It (@isTrue It dec1._proof_1) : Dec It -/
+/-- info: @Dec.mk It (@isTrue It dec2._proof_1) : Dec It -/
 #guard_msgs in
 set_option pp.explicit true in
 #check fast_instance% { dec := dec1 : Dec It }

MathlibTest/slow_instances.lean
@@ -28,8 +28,9 @@ Hint: Additional diagnostic information may be available using the `set_option d
 #synth NoZeroSMulDivisors ℕ K
 
 variable {d : Type*} in
-set_option maxHeartbeats 3500 in -- uses about 2400 as of 2026-02-20,
+set_option maxHeartbeats 5000 in -- uses about 2400 as of 2026-02-20,
                                  -- and then about 3200 as of 2026-03-04
+                                 -- and then about 4400 as of 2026-03-10
 /--
 error: failed to synthesize
   Lean.Grind.OrderedAdd (Submodule ℂ (WithLp 2 (d → ℂ)))

commit ff7fef4c209dbe0b43bb25bdfac2a9ef7c3de6fe

fix(Tactic/FastInstance): telescope over instance families (#43203)
Fix `fast_instance%` to support for instance families (e.g. `Π i, Group (G i)` and `DecidableEq α`) (regression since #36420), both for the top-level instance and nested instances.

Mathlib/Tactic/FastInstance.lean
@@ -46,6 +46,9 @@ is `withReducible` with some exceptions.
 -/
 partial def makeFastInstance (inst expectedType : Expr) (root := true) (trace : Array Name := #[]) :
     MetaM Expr := withReducible do
+  -- Telescope since it might be a family of instances.
+  forallTelescopeReducing expectedType fun xs expectedType => do mkLambdaFVars xs <| ← do
+  let inst ← whnfI <| mkAppN inst xs
   withTraceNode `Elab.fast_instance (fun _ => return m!"type: {expectedType}") do
   let some className ← isClass? expectedType
     | error trace m!"Can only be used for classes, but type is{indentExpr expectedType}"
@@ -70,7 +73,7 @@ partial def makeFastInstance (inst expectedType : Expr) (root := true) (trace : 
         is not defeq to inferred instance{indentExpr new}"
   -- Otherwise, try to reduce it to a constructor.
   else
-    (← whnfI inst).withApp fun f args => do
+    inst.withApp fun f args => do
     let error' (m : MessageData) : MetaM Expr := do
       if isStructure (← getEnv) className then
         error trace m
@@ -138,16 +141,14 @@ public def elabFastInstance : TermElab
     let inst ← withSynthesize <| elabTerm arg expectedType?
     let expectedType ← expectedType?.getDM (inferType inst)
     try
-      -- Telescope since it might be a family of instances.
-      forallTelescopeReducing expectedType fun xs expectedType => do
-        mkLambdaFVars xs <| ← withNewMCtxDepth <| makeFastInstance inst expectedType
+      withNewMCtxDepth <| makeFastInstance inst expectedType
     catch e =>
       logException e
       return inst
   | _, _ => Elab.throwUnsupportedSyntax
 
-/-- `inferInstanceAs% A` is shorthand for `fast_instance% inferInstanceAs A`.
-This is preferred over `inferInstanceAs` when the instance can be reduced to
+/-- `inferInstanceAs% A` is shorthand for `fast_instance% _root_.inferInstanceAs A`.
+This is preferred over `_root_.inferInstanceAs` when the instance can be reduced to
 constructor applications. In that case, the parameters of the constructors will be filled in
 using the expected type, so that the instance will unfold nicely during unification. -/
 macro "inferInstanceAs% " source:term : term =>

MathlibTest/FastInstance.lean
@@ -82,7 +82,7 @@ set_option pp.explicit true in
 /-!
 Non-defeq error
 -/
-instance : Mul Nat := ⟨(· * · )⟩
+instance : Mul Nat := ⟨(· * ·)⟩
 
 /--
 warning: An instance of `Mul Nat` already exists.
@@ -141,6 +141,133 @@ info: @Dec.mk It dec2 : Dec It
 #guard_msgs in
 set_option pp.explicit true in
 #check fast_instance% { dec := dec2 : Dec It }
+
+/-! The provided instance does not reduce to a constructor application but it is defeq to what
+`inferInstance` would synthesize, so we allow it. -/
+/--
+info: let this := dec2;
+@Dec.mk It this : Dec It
+-/
+#guard_msgs in
+set_option pp.explicit true in
+#check let := dec2; fast_instance% { dec := dec2 : Dec It }
+
+class DecEq (α : Type*) where
+  [decEq : DecidableEq α]
+
+def UnitAlias : Type :=
+  Unit
+
+/-! The root is an instance family. -/
+example : DecidableEq UnitAlias :=
+  fast_instance% fun _ _ ↦ isTrue rfl
+
+/-! The root is an instance family, and the instances do not reduce to a constructor application. -/
+/--
+error: Provided instance does not reduce to a constructor application
+  sorry
+Reduces to an application of sorryAx.
+
+Use `set_option trace.Elab.fast_instance true` to analyze the error.
+
+Trace of fields visited: []
+-/
+#guard_msgs in
+example : DecidableEq UnitAlias :=
+  fast_instance% fun _ _ ↦ sorry
+
+/-! The root is an instance family, and the instances do not reduce to a constructor application,
+but are equal defeq to what `inferInstance` would synthesize, so we allow it with a warning. -/
+/--
+warning: An instance of `Decidable (a = b)` already exists.
+Please use `inferInstance` instead of `fast_instance%`
+
+Note: This linter can be disabled with `set_option linter.fast_instance_existing false`
+-/
+#guard_msgs in
+example : DecidableEq UnitAlias :=
+  let (a b : UnitAlias) : Decidable (a = b) := isTrue rfl
+  fast_instance% fun _ _ ↦ inferInstance
+
+/-! A nested instance field contains an instance family. -/
+example : DecEq UnitAlias :=
+  fast_instance% { decEq := fun _ _ ↦ isTrue rfl }
+
+/-! A nested instance field contains an instance family, and the instances do not reduce to a
+constructor application. -/
+/--
+error: Provided instance does not reduce to a constructor application
+  sorry
+Reduces to an application of sorryAx.
+
+Use `set_option trace.Elab.fast_instance true` to analyze the error.
+
+Trace of fields visited: [testing.DecEq.decEq]
+-/
+#guard_msgs in
+example : DecEq α :=
+  fast_instance% { decEq := fun a b ↦ sorry }
+
+/- A nested instance field contains an instance family, and the instances do not reduce to a
+constructor application, it is defeq to what `inferInstance` would synthesize so we allow it. -/
+#guard_msgs (drop warning) in
+example : DecEq α :=
+  let : DecidableEq α := fun a b ↦ sorry
+  fast_instance% { decEq := this : DecEq α }
+
+/-! The root is not a class. -/
+/--
+error: Can only be used for classes, but type is
+  Unit
+
+Use `set_option trace.Elab.fast_instance true` to analyze the error.
+
+Trace of fields visited: []
+-/
+#guard_msgs in
+example : Unit := fast_instance% ()
+
+class UnitClass where
+  [unit : Unit]
+
+/-! A nested instance field is not a class. -/
+/--
+error: Can only be used for classes, but type is
+  Unit
+
+Use `set_option trace.Elab.fast_instance true` to analyze the error.
+
+Trace of fields visited: [testing.UnitClass.unit]
+-/
+#guard_msgs in
+example : UnitClass := fast_instance% { unit := () }
+
+/-! The root is a family but not of classes. -/
+/--
+error: Can only be used for classes, but type is
+  Unit
+
+Use `set_option trace.Elab.fast_instance true` to analyze the error.
+
+Trace of fields visited: []
+-/
+#guard_msgs in
+example : Unit → Unit := fast_instance% fun _ ↦ ()
+
+class Func where
+  [func : Unit → Unit]
+
+/-! A nested instance field is a family but not of classes. -/
+/--
+error: Can only be used for classes, but type is
+  Unit
+
+Use `set_option trace.Elab.fast_instance true` to analyze the error.
+
+Trace of fields visited: [testing.Func.func]
+-/
+#guard_msgs in
+example : Func := fast_instance% { func := fun _ ↦ () }
 
 /-!
 Checking that proof fields whose types already match at instances transparency