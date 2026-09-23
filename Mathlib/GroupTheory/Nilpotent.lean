46ac26c854c3500b27485f8521a4f6852c0931c4
Xavier Roblot <46200072+xroblot@users.noreply.github.com>
2026-09-23 12:20:36 +0200

    1st commit

--- Mathlib/GroupTheory/Nilpotent.lean
+++ Mathlib/GroupTheory/Nilpotent.lean
@@ -953,10 +953,10 @@ instance (priority := 100) CommGroup.isNilpotent {G : Type*} [Group G] [IsMulCom
 
 /-- Abelian groups have nilpotency class at most one. -/
 @[to_additive /-- Abelian groups have nilpotency class at most one. -/]
-theorem CommGroup.nilpotencyClass_le_one {G : Type*} [CommGroup G] :
+theorem CommGroup.nilpotencyClass_le_one {G : Type*} [Group G] [IsMulCommutative G] :
     Group.nilpotencyClass G ≤ 1 := by
   rw [← upperCentralSeries_eq_top_iff_nilpotencyClass_le, upperCentralSeries_one]
-  apply CommGroup.center_eq_top
+  apply Subgroup.center_eq_top
 
 /-- Groups with nilpotency class at most one are abelian. -/
 @[to_additive /-- Additive groups with nilpotency class at most one are abelian. -/,
@@ -1179,14 +1179,18 @@ instance (priority := 100) IsNilpotent.to_isSolvable [h : IsNilpotent G] : Group
 
 /-- A simple nilpotent group is commutative. -/
 @[to_additive /-- A simple nilpotent additive group is commutative. -/]
-instance [IsSimpleGroup G] [IsNilpotent G] : CommGroup G :=
-  Group.commGroupOfCenterEqTop <|
-    (IsSimpleGroup.eq_bot_or_eq_top_of_normal (center G)).resolve_left
-      (Group.IsNilpotent.center_ne_bot G)
+instance [IsSimpleGroup G] [IsNilpotent G] : IsMulCommutative G :=
+  ⟨⟨fun a b ↦ (Subgroup.mem_center_iff.mp
+    ((Subgroup.eq_top_iff' _).mp
+      ((IsSimpleGroup.eq_bot_or_eq_top_of_normal (center G)).resolve_left
+        (Group.IsNilpotent.center_ne_bot G)) a) b).symm⟩⟩
 
 /-- A simple nilpotent group is cyclic. -/
 @[to_additive /-- A simple nilpotent additive group is cyclic. -/]
 instance [IsSimpleGroup G] [IsNilpotent G] : IsCyclic G :=
+  letI : CommGroup G := Group.commGroupOfCenterEqTop <|
+    (IsSimpleGroup.eq_bot_or_eq_top_of_normal (center G)).resolve_left
+      (Group.IsNilpotent.center_ne_bot G)
   inferInstance
 
 namespace Group
