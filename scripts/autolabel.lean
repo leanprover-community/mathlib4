805a6e5644d0159931aa9272709b25ff18ead8a4
Jon Eugster <eugster.jon@gmail.com>
2026-09-23 08:55:16 +0200

    review: dont exit on failing tests

--- scripts/autolabel.lean
+++ scripts/autolabel.lean
@@ -393,6 +393,7 @@ inductive GithubInteraction where
 
 open IO in
 def autoLabelCli (args : Cli.Parsed) : IO UInt32 := do
+  let mut exitCode : UInt32 := 0
   let force := args.hasFlag "force"
   let tool: GithubInteraction :=
     match ((args.flag? "pr").map (·.as! Nat)), args.hasFlag "gh", args.flag? "curl" with
@@ -402,7 +403,6 @@ def autoLabelCli (args : Cli.Parsed) : IO UInt32 := do
     | some pr, false, some curlFlag => .curl pr (curlFlag.as! String)
 
   -- test: validate that all paths in `mathlibLabelData` actually exist
-  let mut valid := true
   for label in mathlibLabels do
     let data := mathlibLabelData label
     for dir in data.dirs do
@@ -411,16 +411,14 @@ def autoLabelCli (args : Cli.Parsed) : IO UInt32 := do
           s!"Misformatted `{ ``AutoLabel.mathlibLabelData }`"
           s!"directory '{dir}' does not exist but is included by label '{label}'. \
           Please update `{ ``AutoLabel.mathlibLabelData }`!"
-        valid := false
+        exitCode := 2
     for dir in data.exclusions do
       unless ← FilePath.pathExists dir do
         println <| AutoLabel.githubAnnotation "warning" "scripts/autolabel.lean"
           s!"Misformatted `{ ``AutoLabel.mathlibLabelData }`"
           s!"directory '{dir}' does not exist but is excluded by label '{label}'. \
           Please update `{ ``AutoLabel.mathlibLabelData }`!"
-        valid := false
-  unless valid do
-    return 2
+        exitCode := 2
 
   -- test: validate that the labels cover all of the `Mathlib/` folder
   let notMatchedPaths ← findUncoveredPaths "Mathlib" (exceptions := mathlibUnlabelled)
@@ -431,7 +429,7 @@ def autoLabelCli (args : Cli.Parsed) : IO UInt32 := do
       s!"Incomplete `{ ``AutoLabel.mathlibLabelData }`"
       s!"the following paths inside `Mathlib/` are not covered \
       by any label: {notMatchedPaths} Please modify `AutoLabel.mathlibLabels` accordingly!"
-    return 3
+    exitCode := 3
 
   -- get the modified files
   let gitDiff ← IO.Process.run {
@@ -481,7 +479,7 @@ def autoLabelCli (args : Cli.Parsed) : IO UInt32 := do
       println s!"::notice::added label: {newLabels}"
     | .none =>
       println s!"::notice::github interaction disabled, not adding labels."
-  return 0
+  return exitCode
 
 end AutoLabel
 
