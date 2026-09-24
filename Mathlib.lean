dac78cb14706a45dbd109d94c88ce2c0dd68ba06
Blake Farman <farmanb@gmail.com>
2026-09-24 06:17:19 -0500

    Added OrthogonalClosed to Mathlib

--- Mathlib.lean
+++ Mathlib.lean
@@ -3345,6 +3345,7 @@ public import Mathlib.CategoryTheory.ObjectProperty.LimitsOfShape
 public import Mathlib.CategoryTheory.ObjectProperty.Local
 public import Mathlib.CategoryTheory.ObjectProperty.Opposite
 public import Mathlib.CategoryTheory.ObjectProperty.Orthogonal
+public import Mathlib.CategoryTheory.ObjectProperty.OrthogonalClosed
 public import Mathlib.CategoryTheory.ObjectProperty.Retract
 public import Mathlib.CategoryTheory.ObjectProperty.Shift
 public import Mathlib.CategoryTheory.ObjectProperty.ShiftAdditive
