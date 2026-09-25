d870b9068518a0870842d15a0cd42637ec30b587
TJHeeringa <16029718+TJHeeringa@users.noreply.github.com>
2026-09-25 14:20:42 +0000

    chore(Algebra/Module/Equiv): add deprecated module for `/Equiv.lean` (#42822)
    This PR marks the file ` Mathlib/Topology/Algebra/Module/Equiv.lean` as deprecated.

---
+++ Mathlib/Topology/Algebra/Module/Equiv.lean
@@ -0,0 +1,8 @@
+module -- shake: keep-all
+
+public import Mathlib.Topology.Algebra.Module.Equiv.Basic
+public import Mathlib.Topology.Algebra.Module.Equiv.Pi
+public import Mathlib.Topology.Algebra.Module.Equiv.Prod
+public import Mathlib.Topology.Algebra.Module.Equiv.Submodule
+
+deprecated_module (since := "2026-08-16")
