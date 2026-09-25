e41c28e8326358950acc9fd8f7f917225a94f6f4
tb65536 <thomas.l.browning@gmail.com>
2026-09-25 16:06:05 +0100

    Fix

--- Mathlib/GroupTheory/Commutator/Basic.lean
+++ Mathlib/GroupTheory/Commutator/Basic.lean
@@ -562,6 +562,10 @@ theorem Subgroup.Normal.quotient_commutative_iff_commutator_le {N : Subgroup G} 
   rw [← commutator_eq_bot_iff, _root_.commutator_def, ← QuotientGroup.range_mk',
     ← map_commutator_eq, map_eq_bot_iff, QuotientGroup.ker_mk']
 
+open IsMulCommutative in
+instance : IsMulCommutative (G ⧸ _root_.commutator G) :=
+  Subgroup.Normal.quotient_commutative_iff_commutator_le.mpr le_rfl
+
 /-- If `N` is a normal subgroup of `G` and `H` a commutative subgroup such that `H ⊔ N = ⊤`,
   then `N` contains `commutator G`. -/
 @[to_additive /-- If `N` is a normal additive subgroup of `G` and `H` a commutative additive
