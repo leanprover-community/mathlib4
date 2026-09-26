9c53aec4fc081b413e9cca14691a4f74bf7594c6
Jovan Gerbscheid <56355248+JovanGerb@users.noreply.github.com>
2026-09-26 01:27:27 +0100

    Update Mathlib/Tactic/Translate/Expr.lean
    Co-authored-by: Jon Eugster <eugster.jon@gmail.com>

--- Mathlib/Tactic/Translate/Expr.lean
+++ Mathlib/Tactic/Translate/Expr.lean
@@ -1,8 +1,7 @@
 /-
 Copyright (c) 2017 Mario Carneiro. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Mario Carneiro, Yury Kudryashov, Floris van Doorn, Jon Eugster, Bryan Gin-ge Chen,
-Jovan Gerbscheid
+Authors: Mario Carneiro, Yury Kudryashov, Floris van Doorn, Bryan Gin-ge Chen, Jovan Gerbscheid
 -/
 module
 
