801527f45d63de1a7e9d2454dcbeb92c1ee82eb2
Yaël Dillies <yael.dillies@gmail.com>
2026-09-26 17:10:24 +0200

    fix

--- Mathlib/RingTheory/Trace/Basic.lean
+++ Mathlib/RingTheory/Trace/Basic.lean
@@ -616,8 +616,9 @@ lemma Module.Basis.traceDual_powerBasis_eq (pb : PowerBasis K L) (i) :
   apply (algebraMap K (AlgebraicClosure K)).injective
   have := congr($(sum_smul_minpolyDiv_eq_X_pow (AlgebraicClosure K)
     pb.adjoin_gen_eq_top (r := j) (pb.finrank.symm ▸ j.prop)).coeff i)
-  simp only [Polynomial.map_smul, map_div₀, map_pow, RingHom.coe_coe, finsetSum_coeff, coeff_smul,
-    coeff_map, smul_eq_mul, coeff_X_pow, ← Fin.ext_iff, @eq_comm _ i] at this
+  simp only [Polynomial.map_smul, map_div₀, map_pow, RingHom.coe_coe, finsetSum_coeff,
+    AddMonoidAlgebra.coeff_smul_apply, coeff_map, smul_eq_mul, coeff_X_pow, ← Fin.ext_iff,
+    @eq_comm _ i] at this
   rw [PowerBasis.coe_basis]
   simp only [traceForm_apply, MonoidWithZeroHom.map_ite_one_zero]
   rw [← this, trace_eq_sum_embeddings (E := AlgebraicClosure K)]
